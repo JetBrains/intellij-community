f96e88fa129d19f2950dab6887c6be9a311a93cf
Eugene Vigdorchik <no_reply@jetbrains.com>
2004-12-04 16:52:02 +0300

    (no message)

--- plugins/InspectionGadgets/src/com/siyeh/ig/errorhandling/ThreadDeathRethrownInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/errorhandling/ThreadDeathRethrownInspection.java
@@ -35,8 +35,7 @@ public class ThreadDeathRethrownInspection extends StatementInspection {
             super.visitTryStatement(statement);
             final PsiParameter[] parameters = statement.getCatchBlockParameters();
             final PsiCodeBlock[] catchBlocks = statement.getCatchBlocks();
-            final int length = Math.min(parameters.length, catchBlocks.length);
-            for (int i = 0; i < length; i++) {
+            for (int i = 0; i < catchBlocks.length; i++) {
                 final PsiParameter parameter = parameters[i];
                 final PsiCodeBlock catchBlock = catchBlocks[i];
                 checkCatchBlock(parameter, catchBlock);
