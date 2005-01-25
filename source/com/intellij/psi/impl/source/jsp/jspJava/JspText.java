fc882747ac1f68af5c19f5f9428c4d57b34aff71
no_reply@jetbrains.com <Igor Kuralenok>
2005-01-25 21:59:54 +0300

    new  jspx xml update

--- source/com/intellij/psi/impl/source/jsp/jspJava/JspText.java
+++ source/com/intellij/psi/impl/source/jsp/jspJava/JspText.java
@@ -6,7 +6,7 @@ import com.intellij.psi.xml.XmlText;
 import com.intellij.util.CharTable;
 
 public class JspText extends LeafPsiElement {
-  private final XmlText myFollowingText;
+  private XmlText myFollowingText;
 
   public JspText(XmlText text, char[] buffer, int start, int end, CharTable table) {
     super(JspElementType.HOLDER_TEMPLATE_DATA, buffer, start, end, -1, table);
@@ -19,6 +19,10 @@ public class JspText extends LeafPsiElement {
 
   public void setText(String text) {
     super.setText(text);
+  }
+
+  public void setFollowingText(final XmlText followingText) {
+    myFollowingText = followingText;
   }
 
   public XmlText getFollowingText() {
