0e71d0582c842b283b8271652be28998a969eb7c
no_reply@jetbrains.com <Igor Kuralenok>
2005-01-27 20:40:05 +0300

    events sent for XmlText changes

--- source/com/intellij/psi/impl/source/xml/XmlTextImpl.java
+++ source/com/intellij/psi/impl/source/xml/XmlTextImpl.java
@@ -9,10 +9,14 @@ import com.intellij.openapi.fileTypes.LanguageFileType;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.impl.source.tree.TreeElement;
+import com.intellij.psi.impl.source.xml.aspect.XmlTextChanged;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.xml.*;
 import com.intellij.util.IncorrectOperationException;
 import com.intellij.xml.util.XmlTagTextUtil;
+import com.intellij.pom.PomModel;
+import com.intellij.pom.event.PomModelEvent;
+import com.intellij.pom.impl.PomTransactionBase;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -153,8 +157,17 @@ public class XmlTextImpl extends XmlElementImpl implements XmlText {
       delete();
       return;
     }
-    replaceAllChildrenToChildrenOf(firstEncodedElement.getTreeParent());
-    clearCaches();
+
+    final PomModel model = getProject().getModel();
+    final XmlAspect aspect = model.getModelAspect(XmlAspect.class);
+    model.runTransaction(new PomTransactionBase(this) {
+      public PomModelEvent run() throws IncorrectOperationException {
+        final String oldText = getText();
+        replaceAllChildrenToChildrenOf(firstEncodedElement.getTreeParent());
+        clearCaches();
+        return XmlTextChanged.createXmlTextChanged(model, XmlTextImpl.this, oldText);
+      }
+    }, aspect);
   }
 
   public XmlElement insertAtOffset(final XmlElement element, int physicalOffset) throws IncorrectOperationException{
