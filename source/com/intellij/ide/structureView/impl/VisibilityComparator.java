55df75967b10b1c6bfa6a0b466a2b175955ca56b
Olesya Smirnova <Olesya.Smirnova@jetbrains.com>
2005-01-28 17:07:07 +0300

    NPE fixed

--- source/com/intellij/ide/structureView/impl/VisibilityComparator.java
+++ source/com/intellij/ide/structureView/impl/VisibilityComparator.java
@@ -24,7 +24,7 @@ public class VisibilityComparator implements Comparator {
   public int compare(Object descriptor1, Object descriptor2) {
     int accessLevel1 = getAccessLevel(descriptor1);
     int accessLevel2 = getAccessLevel(descriptor2);
-    if (accessLevel1 == accessLevel2) {
+    if (accessLevel1 == accessLevel2 && myNextComparator != null) {
       return myNextComparator.compare(descriptor1, descriptor2);
     }
     return accessLevel2 - accessLevel1;
