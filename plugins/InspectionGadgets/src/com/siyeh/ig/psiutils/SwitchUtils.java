25a1897793c6f75fecaed567bda80b9a805ec48e
Dave Griffith <no_reply@jetbrains.com>
2005-02-01 17:43:07 +0300

    (no message)

---
+++ plugins/InspectionGadgets/src/com/siyeh/ig/psiutils/SwitchUtils.java
@@ -0,0 +1,25 @@
+package com.siyeh.ig.psiutils;
+
+import com.intellij.psi.PsiSwitchStatement;
+import com.intellij.psi.PsiCodeBlock;
+import com.intellij.psi.PsiStatement;
+import com.intellij.psi.PsiSwitchLabelStatement;
+
+public class SwitchUtils{
+    private SwitchUtils(){
+        super();
+    }
+
+    public static int calculateBranchCount(PsiSwitchStatement statement){
+    final PsiCodeBlock body = statement.getBody();
+        int branches = 0;
+        final PsiStatement[] statements = body.getStatements();
+        for (int i = 0; i < statements.length; i++) {
+            final PsiStatement child = statements[i];
+            if (child instanceof PsiSwitchLabelStatement) {
+                branches++;
+            }
+        }
+        return branches;
+    }
+}
