e277b9e46537dbf6e7d24bfee7745179420e028a
Dave Griffith <no_reply@jetbrains.com>
2005-02-04 23:04:56 +0300

    (no message)

--- plugins/InspectionGadgets/src/com/siyeh/ig/verbose/ForCanBeForeachInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/verbose/ForCanBeForeachInspection.java
@@ -355,7 +355,7 @@ public class ForCanBeForeachInspection extends StatementInspection {
             return false;
         }
         final PsiDeclarationStatement declaration = (PsiDeclarationStatement) initialization;
-        if (declaration.getDeclaredElements().length > 1) {
+        if (declaration.getDeclaredElements().length != 1) {
             return false;
         }
         final PsiLocalVariable indexVar = (PsiLocalVariable) declaration.getDeclaredElements()[0];
