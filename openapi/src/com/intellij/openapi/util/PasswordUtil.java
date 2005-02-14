1a1dfdabd3e7eccd98f0365bd62391cc99a08251
Olesya Smirnova <Olesya.Smirnova@jetbrains.com>
2005-02-14 18:52:38 +0300

    http://www.jetbrains.net/jira/browse/IDEADEV-239 fixed

--- openapi/src/com/intellij/openapi/util/PasswordUtil.java
+++ openapi/src/com/intellij/openapi/util/PasswordUtil.java
@@ -5,7 +5,7 @@
  * Copyright (c) 2000-2004 by JetBrains s.r.o. All Rights Reserved.
  * Use is subject to license terms.
  */
-package com.intellij.j2ee;
+package com.intellij.openapi.util;
 
 public class PasswordUtil {
   private PasswordUtil() {}
