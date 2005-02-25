1704b6ca37fb82701f92a29aaa72efafe70f3acf
Maxim Shafirov <max@jetbrains.com>
2005-02-25 22:19:37 +0300

    Documentation added.

--- openapi/src/com/intellij/openapi/diagnostic/SubmittedReportInfo.java
+++ openapi/src/com/intellij/openapi/diagnostic/SubmittedReportInfo.java
@@ -1,16 +1,23 @@
 package com.intellij.openapi.diagnostic;
 
 /**
- * Created by IntelliJ IDEA.
- * User: max
- * Date: Feb 17, 2005
- * Time: 9:42:38 PM
- * To change this template use File | Settings | File Templates.
+ * Simple bean representing error submission status.
  */
 public class SubmittedReportInfo {
   public static class SubmissionStatus {
+    /**
+     * Issue have been succesfully created
+     */
     public static SubmissionStatus NEW_ISSUE = new SubmissionStatus("NEW_ISSUE");
+
+    /**
+     * Issue user have been trying to submit is actually a duplicate of existing one
+     */
     public static SubmissionStatus DUPLICATE = new SubmissionStatus("DUPLICATE");
+
+    /**
+     * Submission failed. (For network connection reasons for example)
+     */
     public static SubmissionStatus FAILED = new SubmissionStatus("FAILED");
     private String myName;
 
@@ -27,6 +34,12 @@ public class SubmittedReportInfo {
   private String myLinkText;
   private SubmissionStatus myStatus;
 
+  /**
+   * Create new submission status bean
+   * @param URL url that points to newly created issue. Optional. Pass <code>null</code> value if N/A or failed
+   * @param linkText short text that UI interface pointing to the issue should have.
+   * @param status submission success/failure
+   */
   public SubmittedReportInfo(final String URL, final String linkText, final SubmissionStatus status) {
     myURL = URL;
     myLinkText = linkText;
