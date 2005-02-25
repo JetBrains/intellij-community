9e1ddea8aa625722d322267cc959ea4a479fbcf0
Maxim Shafirov <max@jetbrains.com>
2005-02-25 21:02:02 +0300

    Docs added.

--- extensions/source/com/intellij/openapi/extensions/PluginAware.java
+++ extensions/source/com/intellij/openapi/extensions/PluginAware.java
@@ -5,8 +5,13 @@
 package com.intellij.openapi.extensions;
 
 /**
+ * Extensions should implement this interface when it is important to find out what particular plugin has provided this extension.
  * @author akireyev
  */
 public interface PluginAware {
+  /**
+   * Called by extensions framework when extension is loaded from plugin.xml descriptor.
+   * @param pluginDescriptor descriptor of the plugin that provided this particular extension.
+   */
   void setPluginDescriptor(PluginDescriptor pluginDescriptor);
 }
