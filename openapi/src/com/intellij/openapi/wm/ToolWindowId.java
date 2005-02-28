d63c1c6f2612b8b695685a0646427cff5dd19397
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2005-02-28 18:47:47 +0300

    cosmetics to panel

--- openapi/src/com/intellij/openapi/wm/ToolWindowId.java
+++ openapi/src/com/intellij/openapi/wm/ToolWindowId.java
@@ -9,7 +9,7 @@ public interface ToolWindowId {
   String MESSAGES_WINDOW = "Messages";
   String PROJECT_VIEW = "Project";
   String STRUCTURE_VIEW = "Structure";
-  String FAVORITES_VIEW = "My Favorites";
+  String FAVORITES_VIEW = "Favorites";
   String ANT_BUILD = "Ant Build";
   String DEBUG = "Debug";
   String RUN = "Run";
