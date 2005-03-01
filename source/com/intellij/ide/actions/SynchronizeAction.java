3d681344f63bd2649bf784785995d6340c551211
Eugene Zhuravlev <jeka@jetbrains.com>
2005-03-01 21:05:47 +0300

    fix IDEADEV-178

--- source/com/intellij/ide/actions/SynchronizeAction.java
+++ source/com/intellij/ide/actions/SynchronizeAction.java
@@ -1,17 +1,27 @@
 
 package com.intellij.ide.actions;
 
+import com.intellij.ide.DataManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DataConstants;
+import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ex.ApplicationEx;
+import com.intellij.openapi.application.ex.ApplicationManagerEx;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFileManager;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.util.ProgressWindow;
+import com.intellij.util.concurrency.Semaphore;
 
 public class SynchronizeAction extends AnAction {
   private final boolean myAsynchronous;
 
   public SynchronizeAction() {
-    this(true);
+    this(false);
   }
 
   public SynchronizeAction(boolean asynchronous) {
@@ -21,16 +31,28 @@ public class SynchronizeAction extends AnAction {
   public void actionPerformed(AnActionEvent e) {
     FileDocumentManager.getInstance().saveAllDocuments();
     final VirtualFileManager vfMan = VirtualFileManager.getInstance();
-    Runnable synchronizeRunnable = new Runnable() {
+    final Runnable synchronizeRunnable = new Runnable() {
       public void run() {
         vfMan.refresh(myAsynchronous);
       }
     };
+    final ApplicationEx application = ApplicationManagerEx.getApplicationEx();
     if (myAsynchronous) {
-      ApplicationManager.getApplication().runReadAction(synchronizeRunnable);
+      application.runReadAction(synchronizeRunnable);
     }
     else {
-      ApplicationManager.getApplication().runWriteAction(synchronizeRunnable);
+      final Project project = (Project)DataManager.getInstance().getDataContext().getData(DataConstants.PROJECT);
+      application.runProcessWithProgressSynchronously(new Runnable() {
+        public void run() {
+          final ProgressIndicator pi = ProgressManager.getInstance().getProgressIndicator();
+          pi.setText("Synchronizing files...");
+          application.invokeAndWait(new Runnable() {
+            public void run() {
+              application.runWriteAction(synchronizeRunnable);
+            }
+          }, pi.getModalityState());
+        }
+      }, "", false, project);
     }
   }
 }
