301fae68239894b86841804f40f168c1e45b7400
Dave Griffith <no_reply@jetbrains.com>
2005-03-07 21:21:54 +0300

    (no message)

--- plugins/InspectionGadgets/src/com/siyeh/ig/fixes/AddSerialVersionUIDFix.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/fixes/AddSerialVersionUIDFix.java
@@ -1,17 +1,18 @@
 package com.siyeh.ig.fixes;
 
 import com.siyeh.ig.InspectionGadgetsFix;
+import com.siyeh.ig.serialization.SerialVersionUIDBuilder;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.ReadonlyStatusHandler;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.codeInspection.ProblemDescriptor;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiManager;
-import com.intellij.psi.PsiElementFactory;
-import com.intellij.psi.PsiField;
+import com.intellij.psi.*;
 import com.intellij.util.IncorrectOperationException;
 
 public class AddSerialVersionUIDFix extends InspectionGadgetsFix {
+    private static final Logger s_logger =
+            Logger.getInstance("MakeSerializableFix");
     public String getName() {
         return "Add serialVersionUIDField";
     }
@@ -19,15 +20,15 @@ public class AddSerialVersionUIDFix extends InspectionGadgetsFix {
     public void applyFix(Project project, ProblemDescriptor descriptor) {
         if (ReadonlyStatusHandler.getInstance(project).ensureFilesWritable(new VirtualFile[]{descriptor.getPsiElement().getContainingFile().getVirtualFile()}).hasReadonlyFiles()) return;
         final PsiElement classIdentifier = descriptor.getPsiElement();
-        final PsiElement aClass = classIdentifier.getParent();
+        final PsiClass aClass = (PsiClass) classIdentifier.getParent();
         try {
             final PsiManager psiManager = aClass.getManager();
             final PsiElementFactory elementFactory = psiManager.getElementFactory();
-            final long serialVersionUID = 1;
+            final long serialVersionUID = SerialVersionUIDBuilder.computeDefaultSUID(aClass);
             final PsiField field = elementFactory.createFieldFromText("private static final long serialVersionUID = "+ serialVersionUID+"L;", aClass);
             aClass.add(field);
         } catch (IncorrectOperationException e) {
-            e.printStackTrace();
+            s_logger.error(e);
         }
     }
 }
