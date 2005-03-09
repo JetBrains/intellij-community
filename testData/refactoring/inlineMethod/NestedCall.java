2deb42011fe57118e0dea76c6cf3577675c4dba8
Eugene Vigdorchik <no_reply@jetbrains.com>
2005-03-09 15:07:33 +0300

    (no message)

--- testData/refactoring/inlineMethod/NestedCall.java
+++ testData/refactoring/inlineMethod/NestedCall.java
@@ -1,7 +1,8 @@
 class NestedCall {
-  int foo(int p) {return p;}
-  int <caret>bar(int p) {return foo(p);}
-  {
-    bar(bar(0));
-  }
+    int foo(int p) { return p; }
+    int <caret>bar(int p) { return foo(p); }
+
+    {
+        bar(bar(0));
+    }
 }
