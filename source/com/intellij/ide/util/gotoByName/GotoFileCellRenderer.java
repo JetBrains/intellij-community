171721102b0d5d5b01e53d2cc068237e647308ff
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2005-03-14 21:57:24 +0300

    font in ctrl-shift-n dialog

--- source/com/intellij/ide/util/gotoByName/GotoFileCellRenderer.java
+++ source/com/intellij/ide/util/gotoByName/GotoFileCellRenderer.java
@@ -2,8 +2,18 @@ package com.intellij.ide.util.gotoByName;
 
 import com.intellij.ide.util.PsiElementListCellRenderer;
 import com.intellij.psi.*;
+import com.intellij.openapi.editor.colors.EditorColorsScheme;
+import com.intellij.openapi.editor.colors.EditorColorsManager;
+
+import java.awt.*;
 
 class GotoFileCellRenderer extends PsiElementListCellRenderer{
+  public GotoFileCellRenderer() {
+    EditorColorsScheme scheme = EditorColorsManager.getInstance().getGlobalScheme();
+    Font editorFont = new Font(scheme.getEditorFontName(), Font.PLAIN, scheme.getEditorFontSize());
+    setFont(editorFont);
+  }
+
   public String getElementText(PsiElement element) {
     final PsiFile file = (PsiFile)element;
     return file.getName();
