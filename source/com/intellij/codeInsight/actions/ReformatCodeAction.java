4376c52079f392e785cf45896f4312ba15a85c4d
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2005-03-26 12:06:58 +0300

    fix NPE in Fabrique corrected

--- source/com/intellij/codeInsight/actions/ReformatCodeAction.java
+++ source/com/intellij/codeInsight/actions/ReformatCodeAction.java
@@ -120,7 +120,7 @@ public class ReformatCodeAction extends AnAction {
     Editor editor = (Editor)dataContext.getData(DataConstants.EDITOR);
     if (editor != null){
       PsiFile file = PsiDocumentManager.getInstance(project).getPsiFile(editor.getDocument());
-      if (file == null) {
+      if (file == null || file.getVirtualFile() == null) {
         presentation.setEnabled(false);
         return;
       }
@@ -143,7 +143,7 @@ public class ReformatCodeAction extends AnAction {
       }
       if (!(element instanceof PsiDirectory)){
         PsiFile file = element.getContainingFile();
-        if (file == null || (!(file instanceof PsiJavaFile) && !(file instanceof XmlFile)) || (file.getVirtualFile() == null)){
+        if (file == null || (!(file instanceof PsiJavaFile) && !(file instanceof XmlFile))){
           presentation.setEnabled(false);
           return;
         }
