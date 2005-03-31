07551570f8d1b95388251991a046b322e6223922
Ann Oreshnikova <pti@jetbrains.com>
2005-03-31 17:28:46 +0400

    ScrollIncrement updated for the Plugins panel.

---
+++ source/com/intellij/openapi/wm/impl/welcomeScreen/PluginsPanel.java
@@ -0,0 +1,42 @@
+package com.intellij.openapi.wm.impl.welcomeScreen;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pti
+ * Date: Mar 29, 2005
+ * Time: 9:23:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PluginsPanel extends JPanel implements Scrollable{
+
+  private static final int VERTICAL_SCROLL_INCREMENT = 5;
+
+  public PluginsPanel(LayoutManager layout) {
+    super(layout);
+  }
+
+  public boolean getScrollableTracksViewportHeight() {
+    return false;
+  }
+
+  public boolean getScrollableTracksViewportWidth() {
+    return false;
+  }
+
+  public Dimension getPreferredScrollableViewportSize() {
+    return this.getPreferredSize();
+  }
+
+  public int getScrollableBlockIncrement(Rectangle visibleRect, int orientation, int direction) {
+    if (orientation == SwingConstants.VERTICAL) return VERTICAL_SCROLL_INCREMENT;
+    else return visibleRect.width;
+  }
+
+  public int getScrollableUnitIncrement(Rectangle visibleRect, int orientation, int direction) {
+    if (orientation == SwingConstants.VERTICAL) return VERTICAL_SCROLL_INCREMENT;
+    else return visibleRect.width;
+  }
+}
