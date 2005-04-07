7dd28f67848f0677c153139eeefda9eef810d700
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2005-04-07 21:47:15 +0400

    fix for exclude test sources

--- source/com/intellij/analysis/AnalysisScope.java
+++ source/com/intellij/analysis/AnalysisScope.java
@@ -39,7 +39,7 @@ public class AnalysisScope {
   private final int myType;
   private HashSet<VirtualFile> myFilesSet;
 
-  private boolean myIncludeTestSource = false;
+  private boolean myIncludeTestSource = true;
 
   public AnalysisScope(Project project) {
     myProject = project;
@@ -272,7 +272,7 @@ public class AnalysisScope {
       final FileIndex moduleFileIndex = ModuleRootManager.getInstance(myModule).getFileIndex();
       moduleFileIndex.iterateContent(new ContentIterator() {
         public boolean processFile(VirtualFile fileOrDir) {
-          if (moduleFileIndex.isContentJavaSourceFile(fileOrDir) && (myIncludeTestSource ? true : moduleFileIndex.isInTestSourceContent(fileOrDir))) {
+          if (moduleFileIndex.isContentJavaSourceFile(fileOrDir) && (myIncludeTestSource ? true : !moduleFileIndex.isInTestSourceContent(fileOrDir))) {
             PsiFile psiFile = PsiManager.getInstance(myModule.getProject()).findFile(fileOrDir);
             LOG.assertTrue(psiFile != null);
             psiFile.accept(visitor);
@@ -287,7 +287,7 @@ public class AnalysisScope {
         final FileIndex moduleFileIndex = ModuleRootManager.getInstance(module).getFileIndex();
         moduleFileIndex.iterateContent(new ContentIterator() {
           public boolean processFile(VirtualFile fileOrDir) {
-            if (moduleFileIndex.isContentJavaSourceFile(fileOrDir) && (myIncludeTestSource ? true : moduleFileIndex.isInTestSourceContent(fileOrDir))) {
+            if (moduleFileIndex.isContentJavaSourceFile(fileOrDir) && (myIncludeTestSource ? true : !moduleFileIndex.isInTestSourceContent(fileOrDir))) {
               PsiFile psiFile = PsiManager.getInstance(module.getProject()).findFile(fileOrDir);
               LOG.assertTrue(psiFile != null);
               psiFile.accept(visitor);
