46d41f1eb0796c015d45d42a6931a595234e0f56
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2005-04-14 18:58:04 +0400

    fix add customizations to the group end

--- source/com/intellij/ide/ui/customization/CustomizationUtil.java
+++ source/com/intellij/ide/ui/customization/CustomizationUtil.java
@@ -51,13 +51,13 @@ public class CustomizationUtil {
        if ((actionUrl.getParentGroup().equals(text) ||
             actionUrl.getParentGroup().equals(defaultGroupName) ||
             actionUrl.getParentGroup().equals(actionManager.getId(group))) &&
-           reordableChildren.size() > actionUrl.getAbsolutePosition()) {
+           reordableChildren.size() >= actionUrl.getAbsolutePosition()) {
          AnAction componentAction = actionUrl.getComponentAction();
          if (componentAction != null) {
            if (actionUrl.getActionType() == ActionUrl.ADDED) {
              reordableChildren.add(actionUrl.getAbsolutePosition(), componentAction);
            }
-           else if (actionUrl.getActionType() == ActionUrl.DELETED) {
+           else if (actionUrl.getActionType() == ActionUrl.DELETED && reordableChildren.size() != actionUrl.getAbsolutePosition()) {
              final AnAction anAction = reordableChildren.get(actionUrl.getAbsolutePosition());
              //for unnamed groups
              if (anAction.getTemplatePresentation().getText() == null && anAction instanceof ActionGroup) {
