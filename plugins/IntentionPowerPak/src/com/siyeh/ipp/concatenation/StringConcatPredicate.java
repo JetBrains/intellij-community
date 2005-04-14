439bc6ee8cc9d11b842ffd490d829d4c99e658ed
Maxim Shafirov <max@jetbrains.com>
2005-04-14 18:22:05 +0400

    IDEADEV-926

--- plugins/IntentionPowerPak/src/com/siyeh/ipp/concatenation/StringConcatPredicate.java
+++ plugins/IntentionPowerPak/src/com/siyeh/ipp/concatenation/StringConcatPredicate.java
@@ -4,49 +4,53 @@ import com.intellij.psi.*;
 import com.intellij.psi.tree.IElementType;
 import com.siyeh.ipp.base.PsiElementPredicate;
 
-class StringConcatPredicate implements PsiElementPredicate{
-    public boolean satisfiedBy(PsiElement element){
-        if(element instanceof PsiJavaToken){
-            final PsiJavaToken token = (PsiJavaToken) element;
-            final IElementType tokenType = token.getTokenType();
-            if(!tokenType.equals(JavaTokenType.PLUS)){
-                return false;
-            }
-        } else if(!(element instanceof PsiWhiteSpace)){
-            return false;
-        }
-        final PsiElement parent = element.getParent();
-        if(!(parent instanceof PsiBinaryExpression)){
-            return false;
-        }
-        final PsiBinaryExpression binaryExpression =
-                (PsiBinaryExpression) parent;
-        final PsiJavaToken sign = binaryExpression.getOperationSign();
-        if(sign == null){
-            return false;
-        }
-        final IElementType tokenType = sign.getTokenType();
-        if(!tokenType.equals(JavaTokenType.PLUS)){
-            return false;
-        }
-        final PsiExpression rOperand = binaryExpression.getROperand();
-        if(!(rOperand instanceof PsiLiteralExpression)){
-            return false;
-        }
-        final PsiExpression lhs = binaryExpression.getLOperand();
-        if(!isLeftSideLiteral(lhs)){
-            return false;
-        }
-        return true;
+class StringConcatPredicate implements PsiElementPredicate {
+  public boolean satisfiedBy(PsiElement element) {
+    if (element instanceof PsiJavaToken) {
+      final PsiJavaToken token = (PsiJavaToken)element;
+      final IElementType tokenType = token.getTokenType();
+      if (!tokenType.equals(JavaTokenType.PLUS)) {
+        return false;
+      }
     }
+    else if (!(element instanceof PsiWhiteSpace)) {
+      return false;
+    }
+    final PsiElement parent = element.getParent();
+    if (!(parent instanceof PsiBinaryExpression)) {
+      return false;
+    }
+    final PsiBinaryExpression binaryExpression =
+      (PsiBinaryExpression)parent;
+    final PsiJavaToken sign = binaryExpression.getOperationSign();
+    if (sign == null) {
+      return false;
+    }
+    final IElementType tokenType = sign.getTokenType();
+    if (!tokenType.equals(JavaTokenType.PLUS)) {
+      return false;
+    }
+    final PsiExpression rOperand = binaryExpression.getROperand();
+    if (!(rOperand instanceof PsiLiteralExpression)) {
+      return false;
+    }
+    final PsiExpression lhs = binaryExpression.getLOperand();
+    return expressionContainsStringLiteral(lhs);
+  }
 
-    private boolean isLeftSideLiteral(PsiExpression expression){
-        if(expression instanceof PsiBinaryExpression){
-            final PsiBinaryExpression binaryExpression =
-                    (PsiBinaryExpression) expression;
-            final PsiExpression rhs = binaryExpression.getROperand();
-            return isLeftSideLiteral(rhs);
-        }
-        return expression instanceof PsiLiteralExpression;
+  private static boolean expressionContainsStringLiteral(PsiExpression expression) {
+    if (expression instanceof PsiBinaryExpression) {
+      final PsiBinaryExpression binaryExpression =
+        (PsiBinaryExpression)expression;
+      final PsiExpression rhs = binaryExpression.getROperand();
+      final PsiExpression lhs = binaryExpression.getLOperand();
+      return expressionContainsStringLiteral(lhs) ||
+             expressionContainsStringLiteral(rhs);
     }
+    if (!(expression instanceof PsiLiteralExpression)) {
+      return false;
+    }
+    final PsiType expressionType = expression.getType();
+    return expressionType.equalsToText("java.lang.String");
+  }
 }
