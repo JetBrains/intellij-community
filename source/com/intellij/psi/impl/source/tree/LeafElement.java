35e983632fbe8e720284667896ac635fab6fcdc8
Maxim Shafirov <max@jetbrains.com>
2005-04-19 23:34:53 +0400

    Fixing failing tests.

--- source/com/intellij/psi/impl/source/tree/LeafElement.java
+++ source/com/intellij/psi/impl/source/tree/LeafElement.java
@@ -18,7 +18,7 @@ public abstract class LeafElement extends TreeElement {
   public abstract int copyTo(char[] buffer, int start);
 
   protected LeafElement(IElementType type) {
-    myState = myState | type.getIndex();
+    myState = type != null ? type.getIndex() : 0;
   }
 
   public final LeafElement findLeafElementAt(int offset) {
