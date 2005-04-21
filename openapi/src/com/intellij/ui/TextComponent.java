0a608525ed62b5b610689943b298b8121575c499
Eugene Vigdorchik <no_reply@jetbrains.com>
2005-04-21 17:54:45 +0400

    IDEADEV-956

--- openapi/src/com/intellij/ui/TextComponent.java
+++ openapi/src/com/intellij/ui/TextComponent.java
@@ -2,8 +2,9 @@ package com.intellij.ui;
 
 import javax.swing.*;
 
-/**
- * @author ven
+/*
+ * Copyright (c) 2000-2004 by JetBrains s.r.o. All Rights Reserved.
+ * Use is subject to license terms.
  */
 public interface TextComponent {
 
