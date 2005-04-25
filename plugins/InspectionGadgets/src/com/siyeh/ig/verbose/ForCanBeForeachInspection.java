6fcb83e86febbbf3f95451d9c417a799d37688b6
Alexey Kudravtsev <cdr@jetbrains.com>
2005-04-25 16:51:34 +0400

    NPEs fixed

--- plugins/InspectionGadgets/src/com/siyeh/ig/verbose/ForCanBeForeachInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/verbose/ForCanBeForeachInspection.java
@@ -378,7 +378,7 @@ public class ForCanBeForeachInspection extends StatementInspection{
             final String baseName;
             final CodeStyleManager codeStyleManager =
                     CodeStyleManager.getInstance(project);
-            if(containerName == null){
+            if(containerName != null){
                 baseName = StringUtils.createSingularFromName(containerName);
             } else{
 
@@ -534,9 +534,8 @@ public class ForCanBeForeachInspection extends StatementInspection{
         if(!"iterator".equals(initialCallName)){
             return false;
         }
-        final PsiExpression qualifier =
-                initialMethodExpression.getQualifierExpression();
-        final PsiType qualifierType = qualifier.getType();
+        final PsiExpression qualifier = initialMethodExpression.getQualifierExpression();
+        final PsiType qualifierType = qualifier == null ? null : qualifier.getType();
         if(!(qualifierType instanceof PsiClassType)){
             return false;
         }
