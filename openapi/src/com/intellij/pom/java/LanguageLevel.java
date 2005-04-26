675165bde07d5ab8f54e5626710cf1bb661b3628
Eugene Vigdorchik <no_reply@jetbrains.com>
2005-04-26 19:01:16 +0400

    enums and static imports forever!

--- openapi/src/com/intellij/pom/java/LanguageLevel.java
+++ openapi/src/com/intellij/pom/java/LanguageLevel.java
@@ -7,32 +7,27 @@ package com.intellij.pom.java;
 /**
  * @author dsl
  */
-public final class LanguageLevel implements Comparable<LanguageLevel> {
-  public static final LanguageLevel JDK_1_3 = new LanguageLevel("1.3", "1.3 ", 3, false, false);
-  public static final LanguageLevel JDK_1_4 = new LanguageLevel("1.4", "1.4 - 'assert' keyword", 4, true, false); // assert keyword
-  public static final LanguageLevel JDK_1_5 = new LanguageLevel("5.0", "5.0 - 'enum' keyword, autoboxing, etc.", 5, true, true); // enums etc.
+public enum LanguageLevel {
+  JDK_1_3 ("1.3", "1.3 ", false, false),
+  JDK_1_4 ("1.4", "1.4 - 'assert' keyword", true, false), // assert keyword
+  JDK_1_5 ("5.0", "5.0 - 'enum' keyword, autoboxing, etc.", true, true); // enums etc.
+  
   public static final LanguageLevel HIGHEST = JDK_1_5;
   private final String myId;
-  private final int myIndex;
   private final boolean myHasAssertKeyword;
   private final boolean myHasEnumKeywordAndAutoboxing;
   private final String myPresentableText;
 
 
-  private LanguageLevel(String id, String presentableText, int index, boolean hasAssertKeyword, boolean hasEnumKeywordAndAutoboxing) {
+  private LanguageLevel(String id, String presentableText, boolean hasAssertKeyword, boolean hasEnumKeywordAndAutoboxing) {
     myId = id;
-    myIndex = index;
     myHasAssertKeyword = hasAssertKeyword;
     myHasEnumKeywordAndAutoboxing = hasEnumKeywordAndAutoboxing;
     myPresentableText = presentableText;
   }
 
-  public int compareTo(LanguageLevel languageLevel) {
-    return myIndex - languageLevel.myIndex;
-  }
-
   public int getIndex() {
-    return myIndex;
+    return ordinal() + 3;  //Solely for backward compatibility
   }
 
   public boolean hasAssertKeyword() {
