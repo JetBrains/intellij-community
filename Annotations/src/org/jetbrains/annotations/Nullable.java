bca3ae2cd777f3355aad94e498b876a834e852d1
Maxim Shafirov <max@jetbrains.com>
2005-05-06 00:19:02 +0400

    Documentation added to Nullable and NotNull annotations.

--- Annotations/src/org/jetbrains/annotations/Nullable.java
+++ Annotations/src/org/jetbrains/annotations/Nullable.java
@@ -1,13 +1,15 @@
 package org.jetbrains.annotations;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-import java.lang.annotation.ElementType;
+import java.lang.annotation.*;
 
 /**
+ * An element annotated with Nullable claims <code>null</code> value is perfectly <em>valid</em>
+ * to return (for methods), pass to (parameters) and hold (local variables and fields).
+ * Apart from documentation puropses this annotation intended to be used by static analysis tools
+ * to validate against probable run-time errors and element contract violations.
  * @author max
  */
+@Documented
 @Retention(RetentionPolicy.CLASS)
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE})
 public @interface Nullable {
