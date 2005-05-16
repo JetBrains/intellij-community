92804fe1e6ece6b3b44ca1b32acea7761d444be4
Eugene Zhuravlev <jeka@jetbrains.com>
2005-05-16 17:31:19 +0400

    (no message)

--- runtimesource/com/intellij/rt/ant/execution/AntMain2.java
+++ runtimesource/com/intellij/rt/ant/execution/AntMain2.java
@@ -9,7 +9,18 @@ public final class AntMain2 {
 
   public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
     IdeaAntLogger2.guardStreams();
-    Class antMain = Class.forName("org.apache.tools.ant.Main");
+
+    // first try to use the new way of launching ant
+    try {
+      final Class antLauncher = Class.forName("org.apache.tools.ant.launch.Launcher");
+      antLauncher.getMethod("main", new Class[]{args.getClass()}).invoke(null, new Object[]{args});
+      return;
+    }
+    catch (ClassNotFoundException e) {
+      // ignore and try older variant
+    }
+
+    final Class antMain = Class.forName("org.apache.tools.ant.Main");
     antMain.getMethod("main", new Class[]{args.getClass()}).invoke(null, new Object[]{args});
   }
 }
