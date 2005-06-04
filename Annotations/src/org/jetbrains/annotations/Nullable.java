c00638f86b41022d10a138e0c81cfea798963913
Eugene Vigdorchik <no_reply@jetbrains.com>
2005-06-04 17:46:52 +0400

    (no message)

--- Annotations/src/org/jetbrains/annotations/Nullable.java
+++ Annotations/src/org/jetbrains/annotations/Nullable.java
@@ -5,8 +5,8 @@ import java.lang.annotation.*;
 /**
  * An element annotated with Nullable claims <code>null</code> value is perfectly <em>valid</em>
  * to return (for methods), pass to (parameters) and hold (local variables and fields).
- * Apart from documentation puropses this annotation intended to be used by static analysis tools
- * to validate against probable run-time errors and element contract violations.
+ * Apart from documentation purposes this annotation is intended to be used by static analysis tools
+ * to validate against probable runtime errors and element contract violations.
  * @author max
  */
 @Documented
