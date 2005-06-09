c46e976b66cca55a00d837d52a5459e2e276ebe0
Maxim Shafirov <max@jetbrains.com>
2005-06-09 17:19:52 +0400

    It's not possible to create initializers in interfaces.

--- plugins/IntentionPowerPak/src/com/siyeh/ipp/initialization/SplitDeclarationAndInitializationPredicate.java
+++ plugins/IntentionPowerPak/src/com/siyeh/ipp/initialization/SplitDeclarationAndInitializationPredicate.java
@@ -3,6 +3,7 @@ package com.siyeh.ipp.initialization;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiExpression;
 import com.intellij.psi.PsiField;
+import com.intellij.psi.PsiClass;
 import com.siyeh.ipp.base.PsiElementPredicate;
 import com.siyeh.ipp.psiutils.ErrorUtil;
 
@@ -14,6 +15,10 @@ public class SplitDeclarationAndInitializationPredicate
         }
         final PsiField field = (PsiField) element;
         if(ErrorUtil.containsError(field)){
+            return false;
+        }
+        final PsiClass aClass = field.getContainingClass();
+        if (aClass == null || aClass.isInterface()){
             return false;
         }
         final PsiExpression initializer = field.getInitializer();
