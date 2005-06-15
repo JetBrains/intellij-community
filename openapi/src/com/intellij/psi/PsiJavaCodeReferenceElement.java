e78d26799f1ddb808e368836ead46f097f20d9bc
Maxim Shafirov <max@jetbrains.com>
2005-06-15 12:56:38 +0400

    Some annotations

--- openapi/src/com/intellij/psi/PsiJavaCodeReferenceElement.java
+++ openapi/src/com/intellij/psi/PsiJavaCodeReferenceElement.java
@@ -4,18 +4,27 @@
  */
 package com.intellij.psi;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 public interface PsiJavaCodeReferenceElement extends PsiElement, PsiJavaReference {
   PsiJavaCodeReferenceElement[] EMPTY_ARRAY = new PsiJavaCodeReferenceElement[0];
 
+  @Nullable
   PsiElement getReferenceNameElement();
 
+  @NotNull
   PsiReferenceParameterList getParameterList();
 
+  @NotNull
   PsiType[] getTypeParameters();
 
+  @Nullable
   PsiElement getQualifier();
   boolean isQualified();
 
   String getQualifiedName();
+
+  @Nullable
   String getReferenceName();
 }
