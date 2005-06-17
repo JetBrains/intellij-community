6bd74f256b405a3aa60f4d64c91f7caa75bb1975
Olesya Smirnova <Olesya.Smirnova@jetbrains.com>
2005-06-17 19:34:22 +0400

    49350 fixed

--- source/com/intellij/ide/util/treeView/AbstractTreeBuilder.java
+++ source/com/intellij/ide/util/treeView/AbstractTreeBuilder.java
@@ -568,7 +568,7 @@ public abstract class AbstractTreeBuilder {
       for (int i = 0; i < parentNode.getChildCount(); i++) {
         DefaultMutableTreeNode node1 = (DefaultMutableTreeNode)parentNode.getChildAt(i);
         if (node == node1) continue;
-        if (((NodeDescriptor)node1.getUserObject()).getElement() == null) continue;
+        if ((node1.getUserObject() instanceof NodeDescriptor) && ((NodeDescriptor)node1.getUserObject()).getElement() == null) continue;
         if (myNodeComparator.compare(node, node1) > 0) newIndex++;
       }
 
