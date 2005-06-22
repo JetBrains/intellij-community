4a745c6bf07630b4f7bdb6189bda191252660d98
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2005-06-22 17:55:22 +0400

    http://www.jetbrains.net/jira/browse/IDEADEV-1537

--- source/com/intellij/application/options/ErrorHighlightingPanel.java
+++ source/com/intellij/application/options/ErrorHighlightingPanel.java
@@ -2,8 +2,8 @@ package com.intellij.application.options;
 
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer;
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzerSettings;
-import com.intellij.codeInsight.daemon.HighlightDisplayKey;
-import com.intellij.codeInspection.ex.*;
+import com.intellij.codeInspection.ex.InspectionProfileImpl;
+import com.intellij.codeInspection.ex.InspectionToolsPanel;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
@@ -22,40 +22,6 @@ public class ErrorHighlightingPanel extends InspectionToolsPanel {
   public ErrorHighlightingPanel() {
     super(DaemonCodeAnalyzerSettings.getInstance().getInspectionProfile().getName(), null);
     add(getAutoreparsePanel(), BorderLayout.NORTH);
-  }
-
-
-  protected void initDescriptors() {
-    super.initDescriptors();
-    InspectionTool[] tools = mySelectedProfile.getInspectionTools();
-    for (int i = 0; i < tools.length; i++) {
-      final InspectionTool tool = tools[i];
-      if (tool instanceof LocalInspectionToolWrapper) {
-        myDescriptors.add(new Descriptor(tool, mySelectedProfile));
-      }
-    }
-    addGeneralDescriptors();
-  }
-
-  private void addGeneralDescriptors() {
-    myDescriptors.add(new Descriptor(HighlightDisplayKey.DEPRECATED_SYMBOL, mySelectedProfile));
-    myDescriptors.add(new Descriptor(HighlightDisplayKey.UNUSED_IMPORT, mySelectedProfile));
-    myDescriptors.add(new Descriptor(HighlightDisplayKey.UNUSED_SYMBOL, mySelectedProfile));
-    myDescriptors.add(new Descriptor(HighlightDisplayKey.UNUSED_THROWS_DECL, mySelectedProfile));
-    myDescriptors.add(new Descriptor(HighlightDisplayKey.SILLY_ASSIGNMENT, mySelectedProfile));
-    myDescriptors.add(new Descriptor(HighlightDisplayKey.ACCESS_STATIC_VIA_INSTANCE, mySelectedProfile));
-    myDescriptors.add(new Descriptor(HighlightDisplayKey.WRONG_PACKAGE_STATEMENT, mySelectedProfile));
-    myDescriptors.add(new Descriptor(HighlightDisplayKey.ILLEGAL_DEPENDENCY, mySelectedProfile));
-    myDescriptors.add(new Descriptor(HighlightDisplayKey.JAVADOC_ERROR, mySelectedProfile));
-    myDescriptors.add(new Descriptor(HighlightDisplayKey.UNKNOWN_JAVADOC_TAG, mySelectedProfile));
-    
-    myDescriptors.add(new Descriptor(HighlightDisplayKey.UNKNOWN_HTML_ATTRIBUTES, mySelectedProfile));
-    myDescriptors.add(new Descriptor(HighlightDisplayKey.UNKNOWN_HTML_TAG, mySelectedProfile));
-    myDescriptors.add(new Descriptor(HighlightDisplayKey.REQUIRED_HTML_ATTRIBUTE, mySelectedProfile));
-    
-    myDescriptors.add(new Descriptor(HighlightDisplayKey.EJB_ERROR, mySelectedProfile));
-    myDescriptors.add(new Descriptor(HighlightDisplayKey.EJB_WARNING, mySelectedProfile));
-    myDescriptors.add(new Descriptor(HighlightDisplayKey.UNCHECKED_WARNING, mySelectedProfile));
   }
 
   private JPanel getAutoreparsePanel() {
