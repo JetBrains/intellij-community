dfe9ddd41a9447f7137a36cce29c53343767da12
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2005-06-22 18:30:42 +0400

    remove from favorites via ctrl-del

--- source/com/intellij/ide/favoritesTreeView/DeleteFromFavoritesAction.java
+++ source/com/intellij/ide/favoritesTreeView/DeleteFromFavoritesAction.java
@@ -3,6 +3,7 @@ package com.intellij.ide.favoritesTreeView;
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.IconLoader;
+import com.intellij.ide.util.treeView.AbstractTreeNode;
 
 /**
  * User: anna
@@ -13,27 +14,26 @@ public class DeleteFromFavoritesAction extends AnAction {
     super("Remove From Current Favorites", "Remove Selected Favorite", IconLoader.getIcon("/general/remove.png"));
   }
 
-  public void actionPerformed(AnActionEvent e) {    
+  public void actionPerformed(AnActionEvent e) {
     final DataContext dataContext = e.getDataContext();
     Project project = (Project)dataContext.getData(DataConstants.PROJECT);
     if (project == null) {
       return;
     }
-    if (e.getPlace().equals(ActionPlaces.FAVORITES_VIEW_POPUP) || e.getPlace().equals(ActionPlaces.FAVORITES_VIEW_TOOLBAR)) {
-      final FavoritesTreeViewPanel currentTreeViewPanel = FavoritesViewImpl.getInstance(project).getCurrentTreeViewPanel();
-      FavoritesTreeNodeDescriptor[] selectedNodeDescriptors = currentTreeViewPanel
-          .getSelectedNodeDescriptors();
-      removeNodes(selectedNodeDescriptors, project, currentTreeViewPanel.getName());
-    }
+    final FavoritesTreeViewPanel currentTreeViewPanel = FavoritesViewImpl.getInstance(project).getCurrentTreeViewPanel();
+    FavoritesTreeNodeDescriptor[] selectedNodeDescriptors = currentTreeViewPanel
+        .getSelectedNodeDescriptors();
+    removeNodes(selectedNodeDescriptors, project, currentTreeViewPanel.getName());
   }
 
   public static void removeNodes(final FavoritesTreeNodeDescriptor[] selectedNodeDescriptors, final Project project, String favoritesViewPane) {
     final FavoritesTreeViewPanel favoritesTreeViewPanel = FavoritesViewImpl.getInstance(project).getFavoritesTreeViewPanel(favoritesViewPane);
-    for (int i = 0; selectedNodeDescriptors != null && i < selectedNodeDescriptors.length; i++) {
-      FavoritesTreeNodeDescriptor selectedNodeDescriptor = selectedNodeDescriptors[i];
-      selectedNodeDescriptor = FavoritesTreeNodeDescriptor.getFavoritesRoot(selectedNodeDescriptor, project, favoritesViewPane);
-      if (selectedNodeDescriptor != null) {
-        favoritesTreeViewPanel.removeFromFavorites(selectedNodeDescriptor.getElement());
+    if (selectedNodeDescriptors != null) {
+      for (FavoritesTreeNodeDescriptor selectedNodeDescriptor : selectedNodeDescriptors) {
+        selectedNodeDescriptor = FavoritesTreeNodeDescriptor.getFavoritesRoot(selectedNodeDescriptor, project, favoritesViewPane);
+        if (selectedNodeDescriptor != null) {
+          favoritesTreeViewPanel.removeFromFavorites(selectedNodeDescriptor.getElement());
+        }
       }
     }
   }
@@ -49,8 +49,12 @@ public class DeleteFromFavoritesAction extends AnAction {
     }
     final FavoritesTreeNodeDescriptor[] selectedNodeDescriptors = FavoritesViewImpl.getInstance(project).getCurrentTreeViewPanel()
         .getSelectedNodeDescriptors();
-    e.getPresentation()
-      .setEnabled(selectedNodeDescriptors != null && selectedNodeDescriptors.length > 0 && !(selectedNodeDescriptors[0].getElement().getValue() instanceof String));
+    if (selectedNodeDescriptors == null || selectedNodeDescriptors.length == 0){
+      e.getPresentation().setEnabled(false);
+    } else {
+      final AbstractTreeNode node = selectedNodeDescriptors[0].getElement();
+      e.getPresentation().setEnabled(node != null && !(node.getValue() instanceof String));
+    }
   }
 
 
