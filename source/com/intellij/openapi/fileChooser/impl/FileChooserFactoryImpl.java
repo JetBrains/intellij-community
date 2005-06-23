32a029819b05df937d4d585ef87ccb1d94e2d06e
Eugene Zhuravlev <jeka@jetbrains.com>
2005-06-23 16:32:51 +0400

    (no message)

--- source/com/intellij/openapi/fileChooser/impl/FileChooserFactoryImpl.java
+++ source/com/intellij/openapi/fileChooser/impl/FileChooserFactoryImpl.java
@@ -6,6 +6,11 @@ import com.intellij.openapi.fileChooser.FileChooserDialog;
 import com.intellij.openapi.fileChooser.FileChooserFactory;
 import com.intellij.openapi.fileChooser.ex.FileChooserDialogImpl;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.actionSystem.DataConstants;
+import com.intellij.openapi.actionSystem.ex.DataConstantsEx;
+import com.intellij.openapi.module.Module;
+import com.intellij.ide.DataManager;
 
 import java.awt.*;
 
@@ -15,6 +20,10 @@ public class FileChooserFactoryImpl extends FileChooserFactory implements Applic
   }
 
   public FileChooserDialog createFileChooser(FileChooserDescriptor descriptor, Component parent) {
+    final DataContext dataContext = DataManager.getInstance().getDataContext(parent);
+    if (descriptor.getContextModule() == null) { // if not set
+      descriptor.setContextModule((Module)dataContext.getData(DataConstantsEx.MODULE_CONTEXT));
+    }
     return new FileChooserDialogImpl(descriptor, parent);
   }
 
