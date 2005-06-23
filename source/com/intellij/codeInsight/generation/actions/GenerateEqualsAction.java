3a635d11752803dc06d450e8c70d5c20793e5439
Eugene Vigdorchik <no_reply@jetbrains.com>
2005-06-23 12:46:02 +0400

    (no message)

--- source/com/intellij/codeInsight/generation/actions/GenerateEqualsAction.java
+++ source/com/intellij/codeInsight/generation/actions/GenerateEqualsAction.java
@@ -1,10 +1,10 @@
 package com.intellij.codeInsight.generation.actions;
 
 import com.intellij.codeInsight.generation.GenerateEqualsHandler;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiAnonymousClass;
 import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiFile;
 
 /**
  * @author dsl
@@ -16,7 +16,8 @@ public class GenerateEqualsAction extends BaseGenerateAction {
 
   protected PsiClass getTargetClass(Editor editor, PsiFile file) {
     final PsiClass targetClass = super.getTargetClass(editor, file);
-    if (targetClass == null || targetClass.isEnum()) return null;
+    if (targetClass == null || targetClass instanceof PsiAnonymousClass ||
+        targetClass.isEnum()) return null;
     return targetClass;
   }
 }
