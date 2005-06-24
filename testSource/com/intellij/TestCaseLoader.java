9e65487fdb693d5ee4042a475d72514f110c2980
Maxim Shafirov <max@jetbrains.com>
2005-06-25 00:49:57 +0400

    Bombed annotation moved to idea module (test sources)

--- testSource/com/intellij/TestCaseLoader.java
+++ testSource/com/intellij/TestCaseLoader.java
@@ -8,16 +8,16 @@
  */
 package com.intellij;
 
+import com.intellij.idea.Bombed;
 import com.intellij.idea.IdeaTestUtil;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-import org.jetbrains.annotations.Bombed;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.lang.reflect.Modifier;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
--- testSource/com/intellij/idea/Bombed.java
+++ testSource/com/intellij/idea/Bombed.java
@@ -1,9 +1,9 @@
-package org.jetbrains.annotations;
+package com.intellij.idea;
 
-import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
