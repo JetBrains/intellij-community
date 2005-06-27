2d2d5a2a31b1804a2f58e4a9df770e410283777f
Dave Griffith <no_reply@jetbrains.com>
2005-06-27 23:57:35 +0400

    Fix for IDEA-2122

--- plugins/InspectionGadgets/src/com/siyeh/ig/visibility/MethodOverloadsParentMethodInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/visibility/MethodOverloadsParentMethodInspection.java
@@ -12,60 +12,61 @@ import org.jetbrains.annotations.NotNull;
 import java.util.HashSet;
 import java.util.Set;
 
-public class MethodOverloadsParentMethodInspection extends MethodInspection {
+public class MethodOverloadsParentMethodInspection extends MethodInspection{
     private final RenameFix fix = new RenameFix();
 
     public String getID(){
         return "MethodOverloadsMethodOfSuperclass";
     }
-    public String getDisplayName() {
+
+    public String getDisplayName(){
         return "Method overloads method of superclass";
     }
 
-    public String getGroupDisplayName() {
+    public String getGroupDisplayName(){
         return GroupNames.VISIBILITY_GROUP_NAME;
     }
 
-    protected InspectionGadgetsFix buildFix(PsiElement location) {
+    protected InspectionGadgetsFix buildFix(PsiElement location){
         return fix;
     }
 
-    protected boolean buildQuickFixesOnlyForOnTheFlyErrors() {
+    protected boolean buildQuickFixesOnlyForOnTheFlyErrors(){
         return true;
     }
 
-    public String buildErrorString(PsiElement location) {
+    public String buildErrorString(PsiElement location){
         return "Method '#ref' overloads a compatible method of a superclass, when overriding might have been intended #loc";
     }
 
-    public BaseInspectionVisitor buildVisitor() {
+    public BaseInspectionVisitor buildVisitor(){
         return new MethodOverloadsParentMethodVisitor();
     }
 
-    private static class MethodOverloadsParentMethodVisitor extends BaseInspectionVisitor {
-
-        public void visitMethod(@NotNull PsiMethod method) {
+    private static class MethodOverloadsParentMethodVisitor
+            extends BaseInspectionVisitor{
+        public void visitMethod(@NotNull PsiMethod method){
             final PsiClass aClass = method.getContainingClass();
-            if (aClass == null) {
+            if(aClass == null){
                 return;
             }
-            if (method.hasModifierProperty(PsiModifier.PRIVATE)
-                    || method.hasModifierProperty(PsiModifier.STATIC)) {
+            if(method.hasModifierProperty(PsiModifier.PRIVATE)
+                    || method.hasModifierProperty(PsiModifier.STATIC)){
                 return;
             }
 
-            final PsiMethod[] superMethods = PsiSuperMethodUtil.findSuperMethods(method);
-            if(superMethods!=null && superMethods.length!=0)
-            {
+            final PsiMethod[] superMethods = PsiSuperMethodUtil
+                    .findSuperMethods(method);
+            if(superMethods != null && superMethods.length != 0){
                 return;
             }
             PsiClass ancestorClass = aClass.getSuperClass();
             final Set<PsiClass> visitedClasses = new HashSet<PsiClass>();
-            while (ancestorClass != null) {
-                if (!visitedClasses.add(ancestorClass)) {
+            while(ancestorClass != null){
+                if(!visitedClasses.add(ancestorClass)){
                     return;
                 }
-                if (methodOverloads(method, ancestorClass)) {
+                if(methodOverloads(method, ancestorClass)){
                     registerMethodError(method);
                     return;
                 }
@@ -73,22 +74,25 @@ public class MethodOverloadsParentMethodInspection extends MethodInspection {
             }
         }
 
-
-        private static boolean methodOverloads(PsiMethod meth, PsiClass ancestorClass) {
-            if (methodOverrides(meth, ancestorClass)) {
+        private static boolean methodOverloads(PsiMethod meth,
+                                               PsiClass ancestorClass){
+            if(methodOverrides(meth, ancestorClass)){
                 return false;
             }
             final String methName = meth.getName();
             final PsiParameterList parameterList = meth.getParameterList();
             final PsiParameter[] parameters = parameterList.getParameters();
-            final PsiMethod[] methods = ancestorClass.findMethodsByName(methName, false);
-            if (methods != null) {
+            final PsiMethod[] methods = ancestorClass
+                    .findMethodsByName(methName, false);
+            if(methods != null){
                 for(final PsiMethod testMethod : methods){
-                    if(!testMethod.hasModifierProperty(PsiModifier.PRIVATE)
-                            &&
-                            !testMethod.hasModifierProperty(PsiModifier.STATIC)){
-                        final PsiParameterList testParameterList = testMethod.getParameterList();
-                        final PsiParameter[] testParameters = testParameterList.getParameters();
+                    if(!testMethod.hasModifierProperty(PsiModifier.PRIVATE) &&
+                            !testMethod.hasModifierProperty(PsiModifier.STATIC) &&
+                            !isOverriddenInClass(testMethod, meth.getContainingClass())){
+                        final PsiParameterList testParameterList = testMethod
+                                .getParameterList();
+                        final PsiParameter[] testParameters = testParameterList
+                                .getParameters();
                         if(testParameters.length == parameters.length &&
                                 !parametersAreCompatible(parameters,
                                                          testParameters)){
@@ -100,23 +104,43 @@ public class MethodOverloadsParentMethodInspection extends MethodInspection {
             return false;
         }
 
-        private static boolean parametersAreCompatible(PsiParameter[] parameters, PsiParameter[] testParameters) {
-            for (int i = 0; i < parameters.length; i++) {
+        private static boolean isOverriddenInClass(PsiMethod method,
+                                                   PsiClass aClass){
+            final PsiMethod[] methods = aClass.getMethods();
+            for(PsiMethod testMethod : methods){
+                if(testMethod.getName().equals(method.getName()))
+                {
+                    final PsiMethod[] superMethods = PsiSuperMethodUtil
+                            .findSuperMethods(testMethod, true);
+                    for(final PsiMethod superMethod : superMethods){
+                        if(superMethod .equals(method)){
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        private static boolean parametersAreCompatible(
+                PsiParameter[] parameters, PsiParameter[] testParameters){
+            for(int i = 0; i < parameters.length; i++){
                 final PsiParameter parameter = parameters[i];
                 final PsiType parameterType = parameter.getType();
                 final PsiParameter testParameter = testParameters[i];
                 final PsiType testParameterType = testParameter.getType();
 
-                if (!parameterType.isAssignableFrom(testParameterType)) {
+                if(!parameterType.isAssignableFrom(testParameterType)){
                     return false;
                 }
             }
             return true;
         }
 
-        private static boolean methodOverrides(PsiMethod meth, PsiClass ancestorClass) {
-            // This code is generics aware.
-            final PsiMethod[] superMethods = PsiSuperMethodUtil.findSuperMethods(meth, true);
+        private static boolean methodOverrides(PsiMethod meth,
+                                               PsiClass ancestorClass){
+            final PsiMethod[] superMethods = PsiSuperMethodUtil
+                    .findSuperMethods(meth, true);
             for(final PsiMethod superMethod : superMethods){
                 if(ancestorClass.equals(superMethod.getContainingClass())){
                     return true;
