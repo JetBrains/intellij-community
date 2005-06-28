b12576899e3baa3c1964bf890c307328cc29d90c
Dave Griffith <no_reply@jetbrains.com>
2005-06-28 19:47:22 +0400

    Fix for IDEA-2122

--- plugins/InspectionGadgets/src/com/siyeh/ig/errorhandling/TooBroadCatchInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/errorhandling/TooBroadCatchInspection.java
@@ -25,23 +25,29 @@ public class TooBroadCatchInspection extends StatementInspection{
     }
 
     protected String buildErrorString(PsiElement location){
-        final PsiTryStatement tryStatement = PsiTreeUtil.getParentOfType(location,
-                                                                         PsiTryStatement.class);
+        final PsiTryStatement tryStatement = PsiTreeUtil
+                .getParentOfType(location,
+                                 PsiTryStatement.class);
         assert tryStatement != null;
         final PsiCodeBlock tryBlock = tryStatement.getTryBlock();
-        final Set<PsiType> exceptionsThrown = ExceptionUtils.calculateExceptionsThrown(tryBlock);
+        final Set<PsiType> exceptionsThrown = ExceptionUtils
+                .calculateExceptionsThrown(tryBlock);
         final int numExceptionsThrown = exceptionsThrown.size();
-        final Set<PsiType> exceptionsCaught = new HashSet<PsiType>(numExceptionsThrown);
+        final Set<PsiType> exceptionsCaught = new HashSet<PsiType>(
+                numExceptionsThrown);
 
-        final PsiParameter[] parameters = tryStatement.getCatchBlockParameters();
+        final PsiParameter[] parameters = tryStatement
+                .getCatchBlockParameters();
         final List<String> typesMasked = new ArrayList<String>();
         for(final PsiParameter parameter : parameters){
             if(parameter.equals(location.getParent())){
                 final PsiType typeCaught = parameter.getType();
-                for(Object aExceptionsThrown : exceptionsThrown){
-                    final PsiType typeThrown = (PsiType) aExceptionsThrown;
+                if(exceptionsThrown.contains(typeCaught)){
+                    continue;
+                }
+                for(PsiType typeThrown : exceptionsThrown){
                     if(exceptionsCaught.contains(typeThrown)){
-                        // don't do anything
+                        //don't do anything
                     } else if(typeCaught.equals(typeThrown)){
                         exceptionsCaught.add(typeCaught);
                     } else if(typeCaught.isAssignableFrom(typeThrown)){
@@ -61,13 +67,10 @@ public class TooBroadCatchInspection extends StatementInspection{
             }
             typesMaskedString += typesMasked.get(i);
         }
-        if(typesMasked.size() == 1)
-        {
+        if(typesMasked.size() == 1){
             return "Catch of #ref is too broad, masking exception " +
-                typesMaskedString + "  #loc";
-        }
-        else
-        {
+                    typesMaskedString + "  #loc";
+        } else{
             return "Catch of #ref is too broad, masking exceptions " +
                     typesMaskedString + "  #loc";
         }
@@ -79,28 +82,33 @@ public class TooBroadCatchInspection extends StatementInspection{
 
     private static class TooBroadCatchVisitor
             extends StatementInspectionVisitor{
-
         public void visitTryStatement(@NotNull PsiTryStatement statement){
             super.visitTryStatement(statement);
             final PsiCodeBlock tryBlock = statement.getTryBlock();
             if(tryBlock == null){
                 return;
             }
-            final Set<PsiType> exceptionsThrown = ExceptionUtils.calculateExceptionsThrown(tryBlock);
+            final Set<PsiType> exceptionsThrown = ExceptionUtils
+                    .calculateExceptionsThrown(tryBlock);
             final int numExceptionsThrown = exceptionsThrown.size();
-            final Set<PsiType> exceptionsCaught = new HashSet<PsiType>(numExceptionsThrown);
-            final PsiParameter[] parameters = statement.getCatchBlockParameters();
+            final Set<PsiType> exceptionsCaught = new HashSet<PsiType>(
+                    numExceptionsThrown);
+            final PsiParameter[] parameters = statement
+                    .getCatchBlockParameters();
             for(final PsiParameter parameter : parameters){
                 final PsiType typeCaught = parameter.getType();
-                for(Object aExceptionsThrown : exceptionsThrown){
-                    final PsiType typeThrown = (PsiType) aExceptionsThrown;
+                if(exceptionsThrown.contains(typeCaught)){
+                    continue; 
+                }
+                for(PsiType typeThrown : exceptionsThrown){
                     if(exceptionsCaught.contains(typeThrown)){
-                        // don't do anything
+                        //don't do anything
                     } else if(typeCaught.equals(typeThrown)){
                         exceptionsCaught.add(typeCaught);
                     } else if(typeCaught.isAssignableFrom(typeThrown)){
                         exceptionsCaught.add(typeCaught);
-                        final PsiTypeElement typeElement = parameter.getTypeElement();
+                        final PsiTypeElement typeElement = parameter
+                                .getTypeElement();
                         registerError(typeElement);
                         return;
                     }
