95d69045f5d2659109bf94e068354d1688e0b3a1
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2005-06-29 14:30:53 +0400

    add selection

--- source/com/intellij/codeInspection/ui/InspectionResultsView.java
+++ source/com/intellij/codeInspection/ui/InspectionResultsView.java
@@ -264,12 +264,9 @@ public class InspectionResultsView extends JPanel implements OccurenceNavigator,
       final String currentProfileName = manager.getCurrentProfile().getName();
       manager.setExternalProfile(myInspectionProfile);
       final InspectionCodeSettingsPanel dlg = new InspectionCodeSettingsPanel(manager, myScope);
-      final TreePath selectionPath = myTree.getSelectionPath();
-      if (selectionPath != null){
-        final Object node = selectionPath.getLastPathComponent();
-        if (node instanceof InspectionNode){
-          dlg.selectInspectionTool(((InspectionNode)node).getTool().getShortName());
-        }
+      final InspectionTool selectedTool = getSelectedTool();
+      if (selectedTool != null){        
+        dlg.selectInspectionTool(selectedTool.getShortName());
       }
       dlg.show();
       ApplicationManager.getApplication().invokeLater(new Runnable() {
