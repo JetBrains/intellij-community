2f225c9fa405b284e64b6c491205e7f1d67521fc
Alexey Kudravtsev <cdr@jetbrains.com>
2005-07-01 21:44:28 +0400

    IDEADEV-1534

--- source/com/intellij/codeInsight/daemon/impl/PostHighlightingPass.java
+++ source/com/intellij/codeInsight/daemon/impl/PostHighlightingPass.java
@@ -37,7 +37,6 @@ import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.source.codeStyle.CodeStyleManagerEx;
 import com.intellij.psi.impl.source.jsp.jspJava.JspxImportStatement;
-import com.intellij.psi.jsp.JspFile;
 import com.intellij.psi.search.PsiSearchHelper;
 import com.intellij.psi.util.MethodSignatureUtil;
 import com.intellij.psi.util.PsiUtil;
@@ -510,7 +509,7 @@ public class PostHighlightingPass extends TextEditorHighlightingPass {
     DaemonCodeAnalyzerImpl codeAnalyzer = (DaemonCodeAnalyzerImpl)DaemonCodeAnalyzer.getInstance(myProject);
     PsiFile file = PsiDocumentManager.getInstance(myProject).getPsiFile(myDocument);
     // dont optimize out imports in JSP since it can be included in other JSP
-    if (file == null || !codeAnalyzer.isHighlightingAvailable(file) || file instanceof JspFile) return false;
+    if (file == null || !codeAnalyzer.isHighlightingAvailable(file) || !(file instanceof PsiJavaFile)) return false;
 
     if (!codeAnalyzer.isErrorAnalyzingFinished(file)) return false;
     HighlightInfo[] errors = DaemonCodeAnalyzerImpl.getHighlights(myDocument, HighlightSeverity.ERROR, myProject);
