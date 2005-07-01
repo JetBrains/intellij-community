518354795272b950e2516bb460f982a4d4ee9aaf
Maxim Mossienko <Maxim.Mossienko@jetbrains.com>
2005-07-01 23:11:46 +0400

    import type from another schema is working properly

--- source/com/intellij/xml/impl/schema/XmlNSDescriptorImpl.java
+++ source/com/intellij/xml/impl/schema/XmlNSDescriptorImpl.java
@@ -247,7 +247,13 @@ public class XmlNSDescriptorImpl implements XmlNSDescriptor,Validator {
           return new ComplexTypeDescriptor(this, tag);
         }
       }
-      else if (equalsToSchemaName(tag, "include")) {
+      else if (equalsToSchemaName(tag, "include") ||
+               ( equalsToSchemaName(tag, "import") &&
+                 rootTag.getNamespaceByPrefix(
+                   XmlUtil.findPrefixByQualifiedName(name)
+                 ).equals(tag.getAttributeValue("namespace"))
+               )  
+              ) {
         final String schemaLocation = tag.getAttributeValue("schemaLocation");
         if (schemaLocation != null) {
           final XmlFile xmlFile = XmlUtil.findXmlFile(rootTag.getContainingFile(), schemaLocation);
