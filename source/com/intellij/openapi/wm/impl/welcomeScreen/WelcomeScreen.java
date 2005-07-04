70abbbb29c0a3a23695ead5aca864bec5bb23301
Ann Oreshnikova <pti@jetbrains.com>
2005-07-04 19:51:13 +0400

    Minor fix to Welcome screen.

--- source/com/intellij/openapi/wm/impl/welcomeScreen/WelcomeScreen.java
+++ source/com/intellij/openapi/wm/impl/welcomeScreen/WelcomeScreen.java
@@ -147,7 +147,7 @@ public class WelcomeScreen {
           button.onPress(e);
         }
       });
-      
+
       name.setForeground(CAPTION_COLOR);
       name.setFont(LINK_FONT);
       name.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
@@ -248,9 +248,9 @@ public class WelcomeScreen {
     // Create Welcome panel
     gBC = new GridBagConstraints(0, 0, 2, 1, 1, 0, GridBagConstraints.NORTHWEST, GridBagConstraints.HORIZONTAL, new Insets(10, 5, 0, 5), 0, 0);
     myWelcomePanel.add(topPanel, gBC);
-    gBC = new GridBagConstraints(0, 1, 1, 1, 1.4, 1, GridBagConstraints.NORTHWEST, GridBagConstraints.BOTH, new Insets(15, 15, 15, 0), 0, 0);
+    gBC = new GridBagConstraints(0, 1, 1, 1, 0.7, 1, GridBagConstraints.NORTHWEST, GridBagConstraints.BOTH, new Insets(15, 15, 15, 0), 0, 0);
     myWelcomePanel.add(myMainScrollPane, gBC);
-    gBC = new GridBagConstraints(1, 1, 1, 1, 0.6, 1, GridBagConstraints.NORTHWEST, GridBagConstraints.BOTH, new Insets(15, 15, 15, 15), 0, 0);
+    gBC = new GridBagConstraints(1, 1, 1, 1, 0.3, 1, GridBagConstraints.NORTHWEST, GridBagConstraints.BOTH, new Insets(15, 15, 15, 15), 0, 0);
     myWelcomePanel.add(myPluginsScrollPane, gBC);
   }
 
