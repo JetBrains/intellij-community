5d3f330de0c152075b28fb2b8f990bde97e4ffeb
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2005-07-06 19:23:08 +0400

    fix NPE

--- source/com/intellij/execution/impl/ConsoleViewImpl.java
+++ source/com/intellij/execution/impl/ConsoleViewImpl.java
@@ -338,7 +338,7 @@ public final class ConsoleViewImpl extends JPanel implements ConsoleView, DataPr
       final LogicalPosition pos = myEditor.getCaretModel().getLogicalPosition();
       final HyperlinkInfo info = getHyperlinkInfoByLineAndCol(pos.line, pos.column);
       final OpenFileDescriptor openFileDescriptor = info instanceof OpenFileHyperlinkInfo ? ((OpenFileHyperlinkInfo)info).getDescriptor() : null;
-      if (openFileDescriptor == null && !openFileDescriptor.getFile().isValid()) {
+      if (openFileDescriptor == null || !openFileDescriptor.getFile().isValid()) {
         return null;
       }
       return openFileDescriptor;
