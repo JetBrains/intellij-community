802f7dd0f59ecd1b2636458ba41861be115680bb
Dmitry Jemerov <yole@intellij.com>
2005-07-08 20:32:20 +0400

    update to JFlex 1.4.1; add -charat command line parameter to JFlex; include JFlex lib and sources in the -dev package; remove invalid JS lexer skeleton

--- openapi/src/com/intellij/lang/ParserDefinition.java
+++ openapi/src/com/intellij/lang/ParserDefinition.java
@@ -1,3 +1,34 @@
+/*
+ * Copyright (c) 2000-05 JetBrains s.r.o. All  Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * -Redistributions of source code must retain the above copyright
+ *  notice, this list of conditions and the following disclaimer.
+ *
+ * -Redistribution in binary form must reproduct the above copyright
+ *  notice, this list of conditions and the following disclaimer in
+ *  the documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of JetBrains or IntelliJ IDEA
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * This software is provided "AS IS," without a warranty of any kind. ALL
+ * EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, INCLUDING
+ * ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. JETBRAINS AND ITS LICENSORS SHALL NOT
+ * BE LIABLE FOR ANY DAMAGES OR LIABILITIES SUFFERED BY LICENSEE AS A RESULT
+ * OF OR RELATING TO USE, MODIFICATION OR DISTRIBUTION OF THE SOFTWARE OR ITS
+ * DERIVATIVES. IN NO EVENT WILL JETBRAINS OR ITS LICENSORS BE LIABLE FOR ANY LOST
+ * REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL,
+ * INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY
+ * OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE SOFTWARE, EVEN
+ * IF JETBRAINS HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ *
+ */
 package com.intellij.lang;
 
 import com.intellij.lexer.Lexer;
@@ -10,14 +41,25 @@ import com.intellij.psi.tree.TokenSet;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Created by IntelliJ IDEA.
- * User: max
- * Date: Jan 17, 2005
- * Time: 7:57:54 PM
- * To change this template use File | Settings | File Templates.
+ * Defines the implementation of a parser for a custom language.
  */
+
 public interface ParserDefinition {
+  /**
+   * Returns the lexer for lexing files in the specified project. This lexer does not need to support incremental relexing - it is always
+   * called for the entire file.
+   * @param project the project to which the lexer is connected.
+   * @return the lexer instance.
+   */
+  @NotNull
   Lexer createLexer(Project project);
+
+  /**
+   * Returns the parser for parsing files in the specified project.
+   * @param project the project to which the parser is connected.
+   * @return the parser instance.
+   */
+  @NotNull
   PsiParser createParser(Project project);
 
   IFileElementType getFileNodeType();
