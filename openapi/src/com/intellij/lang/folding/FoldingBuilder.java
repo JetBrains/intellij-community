917f08382118d08d789f99482c1dbfb964a511db
Dmitry Jemerov <yole@intellij.com>
2005-07-11 21:17:58 +0400

    javadocs for com.intellij.lang.folding

--- openapi/src/com/intellij/lang/folding/FoldingBuilder.java
+++ openapi/src/com/intellij/lang/folding/FoldingBuilder.java
@@ -1,19 +1,72 @@
+/*
+ * Copyright (c) 2000-05 JetBrains s.r.o. All  Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * -Redistributions of source code must retain the above copyright
+ *  notice, this list of conditions and the following disclaimer.
+ *
+ * -Redistribution in binary form must reproduct the above copyright
+ *  notice, this list of conditions and the following disclaimer in
+ *  the documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of JetBrains or IntelliJ IDEA
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * This software is provided "AS IS," without a warranty of any kind. ALL
+ * EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, INCLUDING
+ * ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. JETBRAINS AND ITS LICENSORS SHALL NOT
+ * BE LIABLE FOR ANY DAMAGES OR LIABILITIES SUFFERED BY LICENSEE AS A RESULT
+ * OF OR RELATING TO USE, MODIFICATION OR DISTRIBUTION OF THE SOFTWARE OR ITS
+ * DERIVATIVES. IN NO EVENT WILL JETBRAINS OR ITS LICENSORS BE LIABLE FOR ANY LOST
+ * REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL,
+ * INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY
+ * OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE SOFTWARE, EVEN
+ * IF JETBRAINS HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ *
+ */
 package com.intellij.lang.folding;
 
 import com.intellij.lang.ASTNode;
 import com.intellij.openapi.editor.Document;
 
 /**
- * Created by IntelliJ IDEA.
- * User: max
- * Date: Feb 1, 2005
- * Time: 12:10:19 AM
- * To change this template use File | Settings | File Templates.
+ * Allows a custom language plugin to define rules for folding code in the language handled
+ * by the plugin.
+ *
+ * @author max
+ * @see com.intellij.lang.Language#getFoldingBuilder()
  */
+
 public interface FoldingBuilder {
+  /**
+   * Builds the folding regions for the specified node in the AST tree and its children.
+   *
+   * @param node     the node for which folding is requested.
+   * @param document the document for which folding is built. Can be used to retrieve line
+   *                 numbers for folding regions.
+   * @return the array of folding descriptors.
+   */
   FoldingDescriptor[] buildFoldRegions(ASTNode node, Document document);
 
+  /**
+   * Returns the text which is displayed in the editor for the folding region related to the
+   * specified node when the folding region is collapsed.
+   *
+   * @param node the node for which the placeholder text is requested.
+   * @return the placeholder text.
+   */
   String getPlaceholderText(ASTNode node);
 
+  /**
+   * Returns the default collapsed state for the folding region related to the specified node.
+   *
+   * @param node the node for which the collapsed state is requested.
+   * @return true if the region is collapsed by default, false otherwise.
+   */
   boolean isCollapsedByDefault(ASTNode node);
 }
--- openapi/src/com/intellij/lang/folding/FoldingDescriptor.java
+++ openapi/src/com/intellij/lang/folding/FoldingDescriptor.java
@@ -1,14 +1,44 @@
+/*
+ * Copyright (c) 2000-05 JetBrains s.r.o. All  Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * -Redistributions of source code must retain the above copyright
+ *  notice, this list of conditions and the following disclaimer.
+ *
+ * -Redistribution in binary form must reproduct the above copyright
+ *  notice, this list of conditions and the following disclaimer in
+ *  the documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of JetBrains or IntelliJ IDEA
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * This software is provided "AS IS," without a warranty of any kind. ALL
+ * EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, INCLUDING
+ * ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. JETBRAINS AND ITS LICENSORS SHALL NOT
+ * BE LIABLE FOR ANY DAMAGES OR LIABILITIES SUFFERED BY LICENSEE AS A RESULT
+ * OF OR RELATING TO USE, MODIFICATION OR DISTRIBUTION OF THE SOFTWARE OR ITS
+ * DERIVATIVES. IN NO EVENT WILL JETBRAINS OR ITS LICENSORS BE LIABLE FOR ANY LOST
+ * REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL,
+ * INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY
+ * OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE SOFTWARE, EVEN
+ * IF JETBRAINS HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ *
+ */
 package com.intellij.lang.folding;
 
 import com.intellij.lang.ASTNode;
 import com.intellij.openapi.util.TextRange;
 
 /**
- * Created by IntelliJ IDEA.
- * User: max
- * Date: Feb 1, 2005
- * Time: 12:14:17 AM
- * To change this template use File | Settings | File Templates.
+ * Defines a single folding region in the code.
+ *
+ * @author max
+ * @see FoldingBuilder
  */
 public class FoldingDescriptor {
   public static final FoldingDescriptor[] EMPTY = new FoldingDescriptor[0];
@@ -16,15 +46,31 @@ public class FoldingDescriptor {
   private ASTNode myElement;
   private TextRange myRange;
 
+  /**
+   * Creates a folding region related to the specified AST node and covering the specified
+   * text range.
+   * @param node  The node to which the folding region is related. The node is then passed to
+   *              {@link FoldingBuilder#getPlaceholderText(com.intellij.lang.ASTNode)} and
+   *              {@link FoldingBuilder#isCollapsedByDefault(com.intellij.lang.ASTNode)}.
+   * @param range The folded text range.
+   */
   public FoldingDescriptor(final ASTNode node, final TextRange range) {
     myElement = node;
     myRange = range;
   }
 
+  /**
+   * Returns the node to which the folding region is related.
+   * @return the node to which the folding region is related.
+   */
   public ASTNode getElement() {
     return myElement;
   }
 
+  /**
+   * Returns the folded text range.
+   * @return the folded text range.
+   */
   public TextRange getRange() {
     return myRange;
   }
