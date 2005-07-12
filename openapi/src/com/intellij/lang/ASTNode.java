64bbf1bdcd38c0f71137d682a1768ec3675b53be
Dmitry Jemerov <yole@intellij.com>
2005-07-12 16:32:23 +0400

    Surround With { } for JavaScript

--- openapi/src/com/intellij/lang/ASTNode.java
+++ openapi/src/com/intellij/lang/ASTNode.java
@@ -44,6 +44,7 @@ import org.jetbrains.annotations.Nullable;
 /**
  * A node in the AST tree. The AST is an intermediate parsing tree created by {@link PsiBuilder},
  * out of which a PSI tree is then created.
+ *
  * @author max
  * @see PsiElement
  */
@@ -53,18 +54,21 @@ public interface ASTNode extends UserDataHolder {
 
   /**
    * Returns the type of this node.
+   *
    * @return the element type.
    */
   IElementType getElementType();
 
   /**
    * Returns the text of this node.
+   *
    * @return the node text.
    */
   String getText();
 
   /**
    * Checks if the specified character is present in the text of this node.
+   *
    * @param c the character to search for.
    * @return true if the character is found, false otherwise.
    */
@@ -72,30 +76,35 @@ public interface ASTNode extends UserDataHolder {
 
   /**
    * Returns the starting offset of the node text in the document.
+   *
    * @return the start offset.
    */
   int getStartOffset();
 
   /**
    * Returns the length of the node text.
+   *
    * @return the text length.
    */
   int getTextLength();
 
   /**
    * Returns the text range (a combination of starting offset in the document and length) for this node.
+   *
    * @return the text range.
    */
   TextRange getTextRange();
 
   /**
    * Returns the parent of this node in the tree.
+   *
    * @return the parent node.
    */
   ASTNode getTreeParent();
 
   /**
    * Returns the first child of this node in the tree.
+   *
    * @return the first child node.
    */
   @Nullable
@@ -103,6 +112,7 @@ public interface ASTNode extends UserDataHolder {
 
   /**
    * Returns the last child of this node in the tree.
+   *
    * @return the last child node.
    */
   @Nullable
@@ -110,6 +120,7 @@ public interface ASTNode extends UserDataHolder {
 
   /**
    * Returns the previous sibling of this node in the tree.
+   *
    * @return the previous sibling node.
    */
   @Nullable
@@ -117,6 +128,7 @@ public interface ASTNode extends UserDataHolder {
 
   /**
    * Returns the next sibling of this node in the tree.
+   *
    * @return the next sibling node.
    */
   @Nullable
@@ -125,27 +137,31 @@ public interface ASTNode extends UserDataHolder {
   /**
    * Returns the list of children of the specified node, optionally filtered by the
    * specified token type filter.
+   *
    * @param filter the token set used to filter the returned children, or null if
-   * all children should be returned.
+   *               all children should be returned.
    * @return the children array.
    */
   ASTNode[] getChildren(@Nullable TokenSet filter);
 
   /**
    * Adds the specified child node as the last child of this node.
+   *
    * @param child the child node to add.
    */
   void addChild(@NotNull ASTNode child);
 
   /**
    * Adds the specified child node at the specified position in the child list.
-   * @param child the child node to add.
+   *
+   * @param child        the child node to add.
    * @param anchorBefore the node before which the child node is inserted.
    */
   void addChild(@NotNull ASTNode child, ASTNode anchorBefore);
 
   /**
    * Removes the specified node from the list of children of this node.
+   *
    * @param child the child node to remove.
    */
   void removeChild(@NotNull ASTNode child);
@@ -153,13 +169,15 @@ public interface ASTNode extends UserDataHolder {
   /**
    * Removes a range of nodes from the list of children, starting with <code>firstNodeToRemove</code>,
    * up to and not including <code>firstNodeToKeep</code>.
+   *
    * @param firstNodeToRemove the first child node to remove from the tree.
-   * @param firstNodeToKeep the first child node to keep in the tree.
+   * @param firstNodeToKeep   the first child node to keep in the tree.
    */
   void removeRange(@NotNull ASTNode firstNodeToRemove, ASTNode firstNodeToKeep);
 
   /**
    * Replaces the specified child node with another node.
+   *
    * @param oldChild the child node to replace.
    * @param newChild the node to replace with.
    */
@@ -167,20 +185,24 @@ public interface ASTNode extends UserDataHolder {
 
   /**
    * Replaces all child nodes with the children of the specified node.
+   *
    * @param anotherParent the parent node whose children are used for replacement.
    */
   void replaceAllChildrenToChildrenOf(ASTNode anotherParent);
 
   /**
-   * Adds a range of nodes belonging to the same parent to the list of children of this node.
-   * @param firstChild the first node to add.
-   * @param lastChild the last node to add.
-   * @param anchorBefore the node before which the child nodes are inserted.
+   * Adds a range of nodes belonging to the same parent to the list of children of this node,
+   * starting with <code>firstChild</code>, up to and not including <code>firstChildToNotAdd</code>.
+   *
+   * @param firstChild         the first node to add.
+   * @param firstChildToNotAdd the first child node following firstChild which will not be added to the tree.
+   * @param anchorBefore       the node before which the child nodes are inserted.
    */
-  void addChildren(ASTNode firstChild, ASTNode lastChild, ASTNode anchorBefore);
+  void addChildren(ASTNode firstChild, ASTNode firstChildToNotAdd, ASTNode anchorBefore);
 
   /**
    * Creates and returns a deep copy of the AST tree part starting at this node.
+   *
    * @return the top node of the copied tree (as an ASTNode object)
    */
   Object clone();
@@ -188,12 +210,14 @@ public interface ASTNode extends UserDataHolder {
   /**
    * Creates a copy of the entire AST tree containing this node and returns a counterpart
    * of this node in the resulting tree.
+   *
    * @return the counterpart of this node in the copied tree.
    */
   ASTNode copyElement();
 
   /**
    * Finds a leaf child node at the specified offset from the start of the text range of this node.
+   *
    * @param offset the relative offset for which the child node is requested.
    * @return the child node, or null if none is found.
    */
@@ -202,16 +226,18 @@ public interface ASTNode extends UserDataHolder {
 
   /**
    * Returns a copyable user data object attached to this node.
+   *
    * @param key the key for accessing the user data object.
    * @return the user data object, or null if no such object is found in the current node.
    * @see #putCopyableUserData(com.intellij.openapi.util.Key, Object)
    */
   @Nullable
-  <T> T getCopyableUserData(Key<T> key);
+    <T> T getCopyableUserData(Key<T> key);
 
   /**
    * Attaches a copyable user data object to this node. Copyable user data objects are copied
    * when the AST tree nodes are copied.
+   *
    * @param key the key for accessing the user data object.
    * @see #getCopyableUserData(com.intellij.openapi.util.Key)
    */
@@ -219,6 +245,7 @@ public interface ASTNode extends UserDataHolder {
 
   /**
    * Returns the first child of the specified node which has the specified type.
+   *
    * @param type the type of the node to return.
    * @return the found node, or null if none was found.
    */
@@ -227,6 +254,7 @@ public interface ASTNode extends UserDataHolder {
 
   /**
    * Returns the PSI element for this node.
+   *
    * @return the PSI element.
    */
   PsiElement getPsi();
