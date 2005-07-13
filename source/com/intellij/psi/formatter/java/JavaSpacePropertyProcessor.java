1b06274794b08216ce4982935e2d6c164a2f4afe
Olesya Smirnova <Olesya.Smirnova@jetbrains.com>
2005-07-13 19:03:54 +0400

    http://www.jetbrains.net/jira/browse/IDEADEV-1795 fixed

--- source/com/intellij/psi/formatter/java/JavaSpacePropertyProcessor.java
+++ source/com/intellij/psi/formatter/java/JavaSpacePropertyProcessor.java
@@ -543,7 +543,7 @@ public class JavaSpacePropertyProcessor extends PsiElementVisitor {
       int space = spaceBeforeLbrace ? 1 : 0;
       return createNonLFSpace(space, false, dependantRange);
     }
-    else if (braceStyle == CodeStyleSettings.END_OF_LINE) {
+    else if (braceStyle == CodeStyleSettings.END_OF_LINE || braceStyle == CodeStyleSettings.NEXT_LINE_IF_WRAPPED) {
       int space = spaceBeforeLbrace ? 1 : 0;
       return createNonLFSpace(space, false, null);
     }
