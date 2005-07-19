42cf9dbdb06de5c510fffb4aaae3ec82969fc0f6
Maxim Shafirov <max@jetbrains.com>
2005-07-19 23:15:28 +0400

    Copyright headers updated to Apache 2.0 compliant license.

--- plugins/IntentionPowerPak/src/com/siyeh/ipp/constant/ConstantSubexpressionIntention.java
+++ plugins/IntentionPowerPak/src/com/siyeh/ipp/constant/ConstantSubexpressionIntention.java
@@ -3,6 +3,21 @@
  * Hengelosestraat 705, 7521PA Enschede
  * Created: Apr 14, 2005, 4:47:42 PM
  */
+/*
+ * Copyright 2003-2005 Dave Griffith
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.siyeh.ipp.constant;
 
 import com.intellij.openapi.util.text.StringUtil;
