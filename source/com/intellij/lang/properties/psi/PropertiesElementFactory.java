d1bd4a0c8c966ef535110cab78b02562dc45197e
Alexey Kudravtsev <cdr@jetbrains.com>
2005-08-01 21:26:27 +0400

    hang on i18n fixed

--- source/com/intellij/lang/properties/psi/PropertiesElementFactory.java
+++ source/com/intellij/lang/properties/psi/PropertiesElementFactory.java
@@ -32,13 +32,15 @@ public class PropertiesElementFactory {
     return name;
   }
 
-  private static String escapeChar(String name, char c) {
+  private static @NotNull String escapeChar(@NotNull String name, char c) {
+    int offset = 0;
     while (true) {
-      int i = name.indexOf(c);
+      int i = name.indexOf(c, offset);
       if (i == -1) return name;
       if (i == 0 || name.charAt(i-1) != '\\') {
         name = name.substring(0, i) + '\\' + name.substring(i);
       }
+      offset = i+2;
     }
   }
 }
