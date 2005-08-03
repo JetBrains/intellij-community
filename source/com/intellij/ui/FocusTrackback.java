81fc6eec603453d2027dd6381b24767971ee6c45
Kirill Maximov <kir@jetbrains.com>
2005-08-03 17:38:49 +0400

    Fix memory leak on Linux

--- source/com/intellij/ui/FocusTrackback.java
+++ source/com/intellij/ui/FocusTrackback.java
@@ -5,12 +5,15 @@ import com.intellij.openapi.application.ex.ApplicationEx;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.wm.impl.IdeFrame;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.WeakHashMap;
-import java.util.ArrayList;
 
 
 public class FocusTrackback {
@@ -22,49 +25,53 @@ public class FocusTrackback {
   private Window myRoot;
   private Component myFocusOwner;
 
-  private static Map<Window, ArrayList<FocusTrackback>> ourRootWindowToParentsStack = new WeakHashMap<Window, ArrayList<FocusTrackback>>();
+  private static final Map<Window, List<FocusTrackback>> ourRootWindowToParentsStack = new WeakHashMap<Window, List<FocusTrackback>>();
 
-  private Object myRequestor;
+  private String myRequestorName;
 
-  public FocusTrackback(Object requestor, Window parent) {
-    myRequestor = requestor;
+  public FocusTrackback(@NotNull Object requestor, Window parent) {
+    myRequestorName = requestor.toString();
     myParentWindow = parent;
 
-    if (ApplicationManager.getApplication().isUnitTestMode() || SystemInfo.isWindows) return;
+    if (ApplicationManager.getApplication().isUnitTestMode() || wrongOS()) return;
 
     register(parent);
 
-    final ArrayList<FocusTrackback> stack = getStackForRoot(myRoot);
+    final List<FocusTrackback> stack = getStackForRoot(myRoot);
     if (stack.indexOf(this) == 0) {
       final KeyboardFocusManager manager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
-      myFocusOwner = manager.getPermanentFocusOwner();
-      if (myFocusOwner == null) {
+      setFocusOwner(manager.getPermanentFocusOwner());
+      if (getFocusOwner() == null) {
         final Window window = manager.getActiveWindow();
         if (window instanceof Provider) {
-          myFocusOwner = ((Provider)window).getFocusTrackback().getFocusOwner();
+          setFocusOwner(((Provider)window).getFocusTrackback().getFocusOwner());
         }
       }
     } else {
-      myFocusOwner = stack.get(0).getFocusOwner();
+      setFocusOwner(stack.get(0).getFocusOwner());
     }
+  }
+
+  private boolean wrongOS() {
+    return SystemInfo.isWindows;
   }
 
   private void register(final Window parent) {
     myRoot = findUtlimateParent(parent);
-    ArrayList<FocusTrackback> stack = getCleanStackForRoot();
+    List<FocusTrackback> stack = getCleanStackForRoot();
     stack.remove(this);
     stack.add(this);
   }
 
-  private ArrayList<FocusTrackback> getCleanStackForRoot() {
-    ArrayList<FocusTrackback> stack = getStackForRoot(myRoot);
+  private List<FocusTrackback> getCleanStackForRoot() {
+    List<FocusTrackback> stack = getStackForRoot(myRoot);
 
-    final Object[] stackArray = stack.toArray();
-    for (int i = 0; i < stackArray.length; i++) {
-      FocusTrackback eachExisting = (FocusTrackback)stackArray[i];
+    final FocusTrackback[] stackArray = stack.toArray(new FocusTrackback[stack.size()]);
+    for (FocusTrackback eachExisting : stackArray) {
       if (eachExisting != null && eachExisting.isDead()) {
         eachExisting.dispose();
-      } else if (eachExisting == null) {
+      }
+      else if (eachExisting == null) {
         stack.remove(eachExisting);
       }
     }
@@ -72,7 +79,7 @@ public class FocusTrackback {
   }
 
   public void restoreFocus() {
-    if (!SystemInfo.isMac) return;
+    if (wrongOS()) return;
     SwingUtilities.invokeLater(new Runnable() {
       public void run() {
         _restoreFocus();
@@ -81,21 +88,21 @@ public class FocusTrackback {
   }
 
   private void _restoreFocus() {
-    if (myFocusOwner != null) {
-      final ArrayList<FocusTrackback> stack = getStackForRoot(myRoot);
+    final Component focusOwner = getFocusOwner();
+    if (focusOwner != null) {
+      final List<FocusTrackback> stack = getStackForRoot(myRoot);
 
       final boolean isLastInFocusChain = stack.contains(this) && stack.indexOf(this) == 0;
       if (isLastInFocusChain) {
         stack.clear();
-        myFocusOwner.requestFocus();
+        focusOwner.requestFocus();
       }
-
-      dispose();
     }
+    dispose();
   }
 
-  private ArrayList<FocusTrackback> getStackForRoot(final Window root) {
-    ArrayList<FocusTrackback> stack = ourRootWindowToParentsStack.get(root);
+  private List<FocusTrackback> getStackForRoot(final Window root) {
+    List<FocusTrackback> stack = ourRootWindowToParentsStack.get(root);
     if (stack == null) {
       stack = new ArrayList<FocusTrackback>();
       ourRootWindowToParentsStack.put(root, stack);
@@ -125,22 +132,26 @@ public class FocusTrackback {
     return root;
   }
 
+  @Nullable
   public Component getFocusOwner() {
     return myFocusOwner;
   }
 
   public String toString() {
-    return getClass().getName() + " requestor: " + myRequestor + " parent=" + myParentWindow;
+    return getClass().getName() + " requestor: " + myRequestorName + " parent=" + myParentWindow;
   }
 
   public void dispose() {
     getStackForRoot(myRoot).remove(this);
   }
 
-  public final boolean isDead() {
-    return myParentWindow != null ? !myParentWindow.isShowing() : true;
+  private boolean isDead() {
+    return myParentWindow == null || !myParentWindow.isShowing();
   }
 
+  private void setFocusOwner(final Component focusOwner) {
+    myFocusOwner = focusOwner;
+  }
 
   public static interface Provider {
     FocusTrackback getFocusTrackback();
