9dc4b48f8a2b21681ed3b64bcab3b127ff91824d
Dmitry Jemerov <yole@intellij.com>
2005-08-15 15:49:48 +0400

    i18n branch

--- source/com/intellij/featureStatistics/actions/ShowFeatureUsageStatisticsAction.java
+++ source/com/intellij/featureStatistics/actions/ShowFeatureUsageStatisticsAction.java
@@ -4,9 +4,13 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataConstants;
 import com.intellij.openapi.project.Project;
+import com.intellij.util.net.IOExceptionDialog;
+
+import java.io.IOException;
 
 public class ShowFeatureUsageStatisticsAction extends AnAction {
   public void actionPerformed(AnActionEvent e) {
+    new IOExceptionDialog(new IOException("Test IO Exception"), "IO Title", "IO Error Text").setVisible(true);
     new ShowFeatureUsageStatisticsDialog(getProject(e)).show();
   }
 
