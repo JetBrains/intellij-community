696b10a35e57a2a19bf1862eb7afad3fa784de91
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2005-08-17 15:12:44 +0400

    devkit internationalization + remove stored internal jdk from IdeaSdk to support multiple jres

---
+++ plugins/devkit/src/DevKitBundle.java
@@ -0,0 +1,31 @@
+package org.jetbrains.idea.devkit;
+
+import java.util.ResourceBundle;
+import java.util.MissingResourceException;
+import java.text.MessageFormat;
+
+/**
+ * User: anna
+ * Date: Aug 11, 2005
+ */
+public class DevKitBundle {
+  private static final ResourceBundle ourBundle = ResourceBundle.getBundle("org.jetbrains.idea.devkit.DevKitBundle");
+
+  private  DevKitBundle(){}
+
+  public static String message(String key, Object... params) {
+    String value;
+    try {
+      value = ourBundle.getString(key);
+    }
+    catch (MissingResourceException e) {
+      return "!" + key + "!";
+    }
+
+    if (params.length > 0) {
+      return MessageFormat.format(value, params);
+    }
+
+    return value;
+  }
+}
