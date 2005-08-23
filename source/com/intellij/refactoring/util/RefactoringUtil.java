ebd98939d24a3d333770fff708f5d8f7a1c251e9
Eugene Vigdorchik <no_reply@jetbrains.com>
2005-08-23 17:34:06 +0400

    wrong assert

--- source/com/intellij/refactoring/util/RefactoringUtil.java
+++ source/com/intellij/refactoring/util/RefactoringUtil.java
@@ -202,7 +202,7 @@ public class RefactoringUtil {
         public int compare(final UsageInfo usage1, final UsageInfo usage2) {
           final PsiElement element1 = usage1.getElement();
           final PsiElement element2 = usage2.getElement();
-          LOG.assertTrue(element1 != null && element2 != null);
+          if (element1 == null || element2 == null) return 0;
           return element2.getTextRange().getStartOffset() - element1.getTextRange().getStartOffset();
         }
       });
