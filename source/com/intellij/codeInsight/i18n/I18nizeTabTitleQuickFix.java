6883f2331afe55fc38376b38da3f6178b19f4be1
Dmitry Jemerov <yole@intellij.com>
2005-08-24 21:27:21 +0400

    i18n inspection visits tab titles in UI forms; fixes to com.intellij.application i18n

---
+++ source/com/intellij/codeInsight/i18n/I18nizeTabTitleQuickFix.java
@@ -0,0 +1,29 @@
+package com.intellij.codeInsight.i18n;
+
+import com.intellij.uiDesigner.lw.StringDescriptor;
+import com.intellij.uiDesigner.GuiEditor;
+import com.intellij.uiDesigner.RadComponent;
+import com.intellij.uiDesigner.RadTabbedPane;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: yole
+ * Date: 24.08.2005
+ * Time: 20:49:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class I18nizeTabTitleQuickFix extends I18nizeFormQuickFix {
+  public I18nizeTabTitleQuickFix(final GuiEditor editor, final String name, final RadComponent component) {
+    super(editor, name, component);
+  }
+
+  protected StringDescriptor getStringDescriptorValue() {
+    RadTabbedPane tabbedPane = (RadTabbedPane) myComponent.getParent();
+    return tabbedPane.getChildTitle(myComponent);
+  }
+
+  protected void setStringDescriptorValue(final StringDescriptor descriptor) throws Exception {
+    RadTabbedPane tabbedPane = (RadTabbedPane) myComponent.getParent();
+    tabbedPane.setChildTitle(myComponent, descriptor);
+  }
+}
