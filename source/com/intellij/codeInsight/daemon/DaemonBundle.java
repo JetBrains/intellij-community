d790c1c5598d5f22cbf9c4a36c22ca80d853ea13
Maxim Shafirov <max@jetbrains.com>
2005-08-29 16:59:03 +0400

    More daemon I18nization

---
+++ source/com/intellij/codeInsight/daemon/DaemonBundle.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2005 JetBrains s.r.o. All Rights Reserved.
+ */
+package com.intellij.codeInsight.daemon;
+
+import com.intellij.CommonBundle;
+
+import java.util.ResourceBundle;
+
+/**
+ * @author max
+ */
+public class DaemonBundle {
+  private static final ResourceBundle ourBundle = ResourceBundle.getBundle("com.intellij.codeInsight.daemon.DaemonBundle");
+
+  private DaemonBundle() {}
+
+  public static String message(String key, Object... params) {
+    return CommonBundle.message(ourBundle, key, params);
+  }
+}
