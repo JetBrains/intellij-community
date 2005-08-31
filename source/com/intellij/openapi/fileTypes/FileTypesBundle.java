54cdd7d488b2966266ab34563eda5c39aecbb05c
Dmitry Jemerov <yole@intellij.com>
2005-08-31 18:11:16 +0400

    com.intellij.openapi.fileTypes i18n

---
+++ source/com/intellij/openapi/fileTypes/FileTypesBundle.java
@@ -0,0 +1,24 @@
+package com.intellij.openapi.fileTypes;
+
+import org.jetbrains.annotations.PropertyKey;
+
+import java.util.ResourceBundle;
+
+import com.intellij.CommonBundle;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: yole
+ * Date: 31.08.2005
+ * Time: 17:41:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class FileTypesBundle {
+  private static final ResourceBundle ourBundle = ResourceBundle.getBundle("com.intellij.openapi.fileTypes.FileTypesBundle");
+
+  private FileTypesBundle() {}
+
+  public static String message(@PropertyKey String key, Object... params) {
+    return CommonBundle.message(ourBundle, key, params);
+  }
+}
