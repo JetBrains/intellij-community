ba1ce78cee01d85927c962dbe92d1b661a3a2309
Olesya Smirnova <Olesya.Smirnova@jetbrains.com>
2005-08-31 19:46:48 +0400

    diff i18ned

---
+++ openapi/src/com/intellij/openapi/diff/DiffBundle.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2005 JetBrains s.r.o. All Rights Reserved.
+ */
+package com.intellij.openapi.diff;
+
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.PropertyKey;
+import com.intellij.CommonBundle;
+
+import java.util.ResourceBundle;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: lesya
+ * Date: Aug 31, 2005
+ * Time: 6:02:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DiffBundle {
+  @NonNls protected static final String PATH_TO_BUNDLE = "com.intellij.openapi.diff.DiffBundle";
+  private final static ResourceBundle ourResourceBundle = ResourceBundle.getBundle(PATH_TO_BUNDLE);
+
+  public static String message(@PropertyKey String key, Object... params) {
+    return CommonBundle.message(ourResourceBundle, key, params);
+  }
+
+}
