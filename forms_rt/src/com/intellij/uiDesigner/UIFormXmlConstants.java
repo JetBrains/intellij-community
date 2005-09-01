288d5250eef296af8b01a8d5029487b20d4026f8
Dmitry Jemerov <yole@intellij.com>
2005-09-01 17:45:51 +0400

    checkbox for marking string values in UI designer forms as not needing i18n

--- forms_rt/src/com/intellij/uiDesigner/UIFormXmlConstants.java
+++ forms_rt/src/com/intellij/uiDesigner/UIFormXmlConstants.java
@@ -9,4 +9,11 @@ package com.intellij.uiDesigner;
  */
 public class UIFormXmlConstants {
   public static final String ATTRIBUTE_BIND_TO_CLASS = "bind-to-class";
+  public static final String ATTRIBUTE_VALUE = "value";
+  public static final String ATTRIBUTE_NOI18N = "noi18n";
+  public static final String ATTRIBUTE_RESOURCE_BUNDLE = "resource-bundle";
+  public static final String ATTRIBUTE_KEY = "key";
+  public static final String ATTRIBUTE_TITLE = "title";
+  public static final String ATTRIBUTE_TITLE_RESOURCE_BUNDLE = "title-resource-bundle";
+  public static final String ATTRIBUTE_TITLE_KEY = "title-key";
 }
