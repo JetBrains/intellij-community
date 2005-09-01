3d7d1df094b7dc798548797e894d70c3684c24b0
Dmitry Jemerov <yole@intellij.com>
2005-09-01 18:02:59 +0400

    com.intellij.javadoc i18n

---
+++ source/com/intellij/javadoc/JavadocBundle.java
@@ -0,0 +1,24 @@
+package com.intellij.javadoc;
+
+import org.jetbrains.annotations.PropertyKey;
+
+import java.util.ResourceBundle;
+
+import com.intellij.CommonBundle;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: yole
+ * Date: 01.09.2005
+ * Time: 14:37:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class JavadocBundle {
+  private static final ResourceBundle ourBundle = ResourceBundle.getBundle("com.intellij.javadoc.JavadocBundle");
+
+  private JavadocBundle() {}
+
+  public static String message(@PropertyKey String key, Object... params) {
+    return CommonBundle.message(ourBundle, key, params);
+  }
+}
