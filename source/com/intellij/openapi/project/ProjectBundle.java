821796a38b01650b9af94bcce57f6e8f7a021145
Dmitry Jemerov <yole@intellij.com>
2005-09-01 19:49:35 +0400

    com.intellij.openapi.project i18n

---
+++ source/com/intellij/openapi/project/ProjectBundle.java
@@ -0,0 +1,24 @@
+package com.intellij.openapi.project;
+
+import org.jetbrains.annotations.PropertyKey;
+
+import java.util.ResourceBundle;
+
+import com.intellij.CommonBundle;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: yole
+ * Date: 01.09.2005
+ * Time: 18:10:36
+ * To change this template use File | Settings | File Templates.
+ */
+public class ProjectBundle {
+  private static final ResourceBundle ourBundle = ResourceBundle.getBundle("com.intellij.openapi.project.ProjectBundle");
+
+  private ProjectBundle() {}
+
+  public static String message(@PropertyKey String key, Object... params) {
+    return CommonBundle.message(ourBundle, key, params);
+  }
+}
