9df1769dc976c08d71ec6290be735471ee384eef
Dmitry Jemerov <yole@intellij.com>
2005-09-02 17:01:05 +0400

    com.intellij.tools i18n

---
+++ source/com/intellij/tools/ToolsBundle.java
@@ -0,0 +1,24 @@
+package com.intellij.tools;
+
+import org.jetbrains.annotations.PropertyKey;
+
+import java.util.ResourceBundle;
+
+import com.intellij.CommonBundle;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: yole
+ * Date: 02.09.2005
+ * Time: 16:41:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class ToolsBundle {
+  private static final ResourceBundle ourBundle = ResourceBundle.getBundle("com.intellij.tools.ToolsBundle");
+
+  private ToolsBundle() {}
+
+  public static String message(@PropertyKey String key, Object... params) {
+    return CommonBundle.message(ourBundle, key, params);
+  }
+}
