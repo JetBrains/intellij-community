da0d548bda57d9013a4820a97feacb179e7ef130
Maxim Shafirov <max@jetbrains.com>
2005-09-05 19:44:37 +0400

    IDEADEV-2380

--- source/com/intellij/openapi/editor/actions/ToggleColumnModeAction.java
+++ source/com/intellij/openapi/editor/actions/ToggleColumnModeAction.java
@@ -12,14 +12,46 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataConstants;
 import com.intellij.openapi.actionSystem.ToggleAction;
 import com.intellij.openapi.editor.ex.EditorEx;
+import com.intellij.openapi.editor.SelectionModel;
+import com.intellij.openapi.editor.CaretModel;
+import com.intellij.openapi.editor.LogicalPosition;
 
 public class ToggleColumnModeAction extends ToggleAction {
 
   public void setSelected(AnActionEvent e, boolean state) {
-    if (isSelected(e) != state) {
-      getEditor(e).getSelectionModel().removeSelection();
+    final EditorEx editor = getEditor(e);
+    final SelectionModel selectionModel = editor.getSelectionModel();
+    if (state) {
+      boolean hasSelection = selectionModel.hasSelection();
+      int selStart = selectionModel.getSelectionStart();
+      int selEnd = selectionModel.getSelectionEnd();
+      final CaretModel caretModel = editor.getCaretModel();
+      LogicalPosition blockStart = selStart == caretModel.getOffset()
+                                   ? caretModel.getLogicalPosition()
+                                   : editor.offsetToLogicalPosition(selStart);
+      LogicalPosition blockEnd = selEnd == caretModel.getOffset()
+                                 ? caretModel.getLogicalPosition()
+                                 : editor.offsetToLogicalPosition(selEnd);
+      editor.setColumnMode(true);
+      if (hasSelection) {
+        selectionModel.setBlockSelection(blockStart, blockEnd);
+      }
+      else {
+        selectionModel.removeSelection();
+      }
     }
-    getEditor(e).setColumnMode(state);
+    else {
+      boolean hasSelection = selectionModel.hasBlockSelection();
+      int selStart = editor.logicalPositionToOffset(selectionModel.getBlockStart());
+      int selEnd = editor.logicalPositionToOffset(selectionModel.getBlockEnd());
+      editor.setColumnMode(false);
+      if (hasSelection) {
+        selectionModel.setSelection(selStart, selEnd);
+      }
+      else {
+        selectionModel.removeSelection();
+      }
+    }
   }
 
   public boolean isSelected(AnActionEvent e) {
