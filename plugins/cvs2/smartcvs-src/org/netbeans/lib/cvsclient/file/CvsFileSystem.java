98bc118fceaa1316eccf875cd793f276751a0253
Olesya Smirnova <Olesya.Smirnova@jetbrains.com>
2005-09-12 18:57:13 +0400

    CVS problem fixed

--- plugins/cvs2/smartcvs-src/org/netbeans/lib/cvsclient/file/CvsFileSystem.java
+++ plugins/cvs2/smartcvs-src/org/netbeans/lib/cvsclient/file/CvsFileSystem.java
@@ -83,7 +83,7 @@ public final class CvsFileSystem
             }
             return relativeRepositoryPath;
           } else {
-            return repository;
+            return repositoryPath;
           }
         }
 
