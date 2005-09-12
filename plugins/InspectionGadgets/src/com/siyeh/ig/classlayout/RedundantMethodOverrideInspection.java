24fbefd2540735f8d05c6f20abecdeeaa7888911
Bas Leijdekkers <basleijdekkers@gmail.com>
2005-09-12 13:42:02 +0400

    fix for IDEADEV-2368

---
+++ plugins/InspectionGadgets/src/com/siyeh/ig/classlayout/RedundantMethodOverrideInspection.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2005 Bas Leijdekkers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.siyeh.ig.classlayout;
+
+import com.intellij.codeInsight.daemon.GroupNames;
+import com.intellij.psi.PsiCodeBlock;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiModifierList;
+import com.siyeh.ig.BaseInspectionVisitor;
+import com.siyeh.ig.MethodInspection;
+import com.siyeh.ig.psiutils.EquivalenceChecker;
+import org.jetbrains.annotations.Nullable;
+
+public class RedundantMethodOverrideInspection extends MethodInspection {
+
+    public String getGroupDisplayName() {
+        return GroupNames.INHERITANCE_GROUP_NAME;
+    }
+
+    public String getDisplayName() {
+        return "Method is identical to super method";
+    }
+
+    @Nullable
+    protected String buildErrorString(PsiElement location) {
+        return "Method '#ref()' is identical to super method";
+    }
+
+    public BaseInspectionVisitor buildVisitor() {
+        return new RedundantMethodOverrideVisitor();
+    }
+
+    private static class RedundantMethodOverrideVisitor
+            extends BaseInspectionVisitor {
+
+        public void visitMethod(PsiMethod method) {
+            super.visitMethod(method);
+            final PsiCodeBlock body = method.getBody();
+            if (body == null) {
+                return;
+            }
+            final PsiMethod[] superMethods = method.findSuperMethods(false);
+            if (superMethods.length == 0) {
+                return;
+            }
+            final PsiMethod superMethod = superMethods[0];
+            final PsiCodeBlock superBody = superMethod.getBody();
+            if (superBody == null) {
+                return;
+            }
+            final PsiModifierList superModifierList =
+                    superMethod.getModifierList();
+            final PsiModifierList modifierList = method.getModifierList();
+            if (!EquivalenceChecker.modifierListsAreEquivalent(
+                    modifierList, superModifierList)) {
+                return;
+            }
+            if (!EquivalenceChecker.codeBlocksAreEquivalent(body, superBody)) {
+                return;
+            }
+            registerMethodError(method);
+        }
+    }
+}
