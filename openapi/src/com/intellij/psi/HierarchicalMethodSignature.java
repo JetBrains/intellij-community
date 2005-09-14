6454386ca818f327b24a73ac8eea07f347caf277
Eugene Vigdorchik <no_reply@jetbrains.com>
2005-09-14 17:49:39 +0400

    cleaner openapi exposed

--- openapi/src/com/intellij/psi/HierarchicalMethodSignature.java
+++ openapi/src/com/intellij/psi/HierarchicalMethodSignature.java
@@ -24,20 +24,12 @@ import java.util.Collections;
 /**
  * @author ven
  */
-public class HierarchicalMethodSignature extends MethodSignatureBackedByPsiMethod {
-  List<HierarchicalMethodSignature> mySupers;
+public abstract class HierarchicalMethodSignature extends MethodSignatureBackedByPsiMethod {
 
   public HierarchicalMethodSignature(final MethodSignatureBackedByPsiMethod signature) {
     super(signature.getMethod(), signature.getSubstitutor(), signature.isRaw(), signature.isInGenericContext(),
           signature.getParameterTypes(), signature.getTypeParameters());
   }
 
-  public List<HierarchicalMethodSignature> getSuperSignatures() {
-    return Collections.unmodifiableList(mySupers == null ? Collections.EMPTY_LIST : mySupers);
-  }
-
-  public void addSuperSignature(HierarchicalMethodSignature superSignatureHierarchical) {
-    if (mySupers == null) mySupers = new ArrayList<HierarchicalMethodSignature>(2);
-    mySupers.add(superSignatureHierarchical);
-  }
+  public abstract List<HierarchicalMethodSignature> getSuperSignatures();
 }
---
+++ source/com/intellij/psi/impl/source/HierarchicalMethodSignatureImpl.java
@@ -0,0 +1,29 @@
+package com.intellij.psi.impl.source;
+
+import com.intellij.psi.HierarchicalMethodSignature;
+import com.intellij.psi.util.MethodSignatureBackedByPsiMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+
+/**
+ * @author ven
+ */
+public class HierarchicalMethodSignatureImpl extends HierarchicalMethodSignature {
+  List<HierarchicalMethodSignature> mySupers;
+
+  public HierarchicalMethodSignatureImpl(final MethodSignatureBackedByPsiMethod signature) {
+    super(signature);
+  }
+
+  public void addSuperSignature(HierarchicalMethodSignature superSignatureHierarchical) {
+    if (mySupers == null) mySupers = new ArrayList<HierarchicalMethodSignature>(2);
+    mySupers.add(superSignatureHierarchical);
+  }
+
+  public List<HierarchicalMethodSignature> getSuperSignatures() {
+    return Collections.unmodifiableList(mySupers == null ? Collections.EMPTY_LIST : mySupers);
+  }
+
+}
