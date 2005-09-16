d033075d3cbee27c338198bf6cba2e4d4736e000
Maxim Mossienko <Maxim.Mossienko@jetbrains.com>
2005-09-16 18:34:31 +0400

    IOBE fixed

--- source/com/intellij/find/FindUtil.java
+++ source/com/intellij/find/FindUtil.java
@@ -215,7 +215,7 @@ public class FindUtil {
     else {
       editor.putUserData(KEY, null);
       offset = editor.getCaretModel().getOffset();
-      if (!model.isForward()) {
+      if (!model.isForward() && offset > 0 ) {
         offset--;
       }
     }
