ec8bc5b3079a7a29089b37e2949bf820a172c0db
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2005-09-26 11:44:21 +0400

    UI for exceptions in  i18n

---
+++ openapi/src/com/intellij/ui/AddDeleteListPanel.java
@@ -0,0 +1,81 @@
+package com.intellij.ui;
+
+import com.intellij.CommonBundle;
+
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User: anna
+ * Date: 22-Sep-2005
+ */
+public abstract class AddDeleteListPanel extends PanelWithButtons {
+  private String myTitle;
+  private JButton myAddButton = new JButton(CommonBundle.message("button.add"));
+  private JButton myDeleteButton = new JButton(CommonBundle.message("button.delete"));
+  private DefaultListModel myListModel = new DefaultListModel();
+  private JList myList = new JList(myListModel);
+
+  public AddDeleteListPanel(final String title,
+                            final List initialList) {
+    myTitle = title;
+    for (Object o : initialList) {
+      if (o != null) {
+        myListModel.addElement(o);
+      }
+    }
+    myList.setCellRenderer(getListCellRenderer());
+    myList.addListSelectionListener(new ListSelectionListener() {
+      public void valueChanged(ListSelectionEvent e) {
+        myDeleteButton.setEnabled(ListUtil.canRemoveSelectedItems(myList));
+      }
+    });
+    myAddButton.addActionListener(new ActionListener(){
+      public void actionPerformed(ActionEvent e) {
+        final Object itemToAdd = findItemToAdd();
+        if (itemToAdd != null){
+          myListModel.addElement(itemToAdd);
+          myList.setSelectedValue(itemToAdd, true);
+        }
+      }
+    });
+    myDeleteButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        ListUtil.removeSelectedItems(myList);
+      }
+    });
+    initPanel();
+  }
+
+  protected abstract Object findItemToAdd();
+
+  public Object [] getListItems(){
+    List<Object> items = new ArrayList<Object>();
+    for (int i = 0; i < myListModel.size(); i++){
+      items.add(myListModel.getElementAt(i));
+    }
+    return items.toArray();
+  }
+
+  protected String getLabelText() {
+    return myTitle;
+  }
+
+  protected JButton[] createButtons() {
+    return new JButton[]{myAddButton, myDeleteButton};
+  }
+
+  protected JComponent createMainComponent() {
+    if (!myListModel.isEmpty()) myList.setSelectedIndex(0);
+    return ScrollPaneFactory.createScrollPane(myList);
+  }
+
+  protected ListCellRenderer getListCellRenderer(){
+    return new DefaultListCellRenderer();
+  }
+}
