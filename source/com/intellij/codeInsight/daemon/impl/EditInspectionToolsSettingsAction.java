54d69a7d7db7629cae8f05e2a3189b6c48c95351
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2005-09-28 20:00:34 +0400

    correct suppress for descriptor provider inspections; exclude change of profile from edit tool settings during inspection process

---
+++ source/com/intellij/codeInsight/daemon/impl/EditInspectionToolsSettingsAction.java
@@ -0,0 +1,123 @@
+package com.intellij.codeInsight.daemon.impl;
+
+import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer;
+import com.intellij.codeInsight.daemon.DaemonCodeAnalyzerSettings;
+import com.intellij.codeInsight.daemon.HighlightDisplayKey;
+import com.intellij.codeInsight.intention.IntentionAction;
+import com.intellij.codeInspection.InspectionsBundle;
+import com.intellij.codeInspection.LocalInspectionTool;
+import com.intellij.codeInspection.ex.InspectionProfileImpl;
+import com.intellij.codeInspection.ex.InspectionToolsPanel;
+import com.intellij.openapi.application.ApplicationBundle;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.options.Configurable;
+import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.options.ShowSettingsUtil;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.util.IconLoader;
+import com.intellij.psi.PsiFile;
+import com.intellij.util.IncorrectOperationException;
+
+import javax.swing.*;
+
+/**
+ * User: anna
+ * Date: Feb 7, 2005
+ */
+public class EditInspectionToolsSettingsAction implements IntentionAction {
+  private String myDisplayName = null;
+  private String myShortName = null;
+
+  public EditInspectionToolsSettingsAction(LocalInspectionTool tool) {
+    myDisplayName = tool.getDisplayName();
+    myShortName = tool.getShortName();
+  }
+
+  public EditInspectionToolsSettingsAction(HighlightDisplayKey key) {
+    if (key != null) {
+      myDisplayName = HighlightDisplayKey.getDisplayNameByKey(key);
+      myShortName = key.toString();
+    }
+  }
+
+  public String getText() {
+    return InspectionsBundle.message("edit.options.of.reporter.inspection.text", myDisplayName);
+  }
+
+  public String getFamilyName() {
+    return InspectionsBundle.message("edit.options.of.reporter.inspection.family");
+  }
+
+  public boolean isAvailable(Project project, Editor editor, PsiFile file) {
+    return true;
+  }
+
+  public void invoke(Project project, Editor editor, PsiFile file) throws IncorrectOperationException {
+    InspectionProfileImpl inspectionProfile = DaemonCodeAnalyzerSettings.getInstance().getInspectionProfile(file);
+    editToolSettings(project, inspectionProfile, true);
+  }
+
+  public boolean editToolSettings(final Project project, final InspectionProfileImpl inspectionProfile, final boolean editorHighlighting) {
+    Project[] projects = ProjectManager.getInstance().getOpenProjects();
+    final boolean isOK = ShowSettingsUtil.getInstance().editConfigurable(project,
+                                                                         "#com.intellij.codeInsight.daemon.impl.EditInspectionToolsSettingsAction",
+                                                                         new Configurable(){
+                                                                           private InspectionToolsPanel myPanel = new InspectionToolsPanel(inspectionProfile.getName(),
+                                                                                                                                           project,
+                                                                                                                                           editorHighlighting);
+                                                                           public String getDisplayName() {
+                                                                             return ApplicationBundle.message("title.errors");
+                                                                           }
+
+                                                                           public Icon getIcon() {
+                                                                             return IconLoader.getIcon("/general/configurableErrorHighlighting.png");
+                                                                           }
+
+                                                                           public String getHelpTopic() {
+                                                                             return "preferences.errorHighlight";
+                                                                           }
+
+                                                                           public JComponent createComponent() {
+                                                                             if (myShortName != null) {
+                                                                               myPanel.selectInspectionTool(myShortName);
+                                                                             }
+                                                                             return myPanel;
+                                                                           }
+
+                                                                           public boolean isModified() {
+                                                                             return myPanel.isModified();
+                                                                           }
+
+                                                                           public void apply() throws ConfigurationException {
+                                                                             myPanel.apply();
+                                                                             final InspectionProfileImpl editedProfile = (InspectionProfileImpl) myPanel.getSelectedProfile().getParentProfile();
+                                                                             if (editorHighlighting){
+                                                                               DaemonCodeAnalyzerSettings.getInstance().setInspectionProfile(editedProfile);
+                                                                             }
+                                                                             inspectionProfile.copyFrom(editedProfile);
+                                                                           }
+
+                                                                           public void reset() {
+                                                                             myPanel.reset();
+                                                                           }
+
+                                                                           public void disposeUIResources() {
+                                                                             if (myPanel != null) {
+                                                                               myPanel.saveVisibleState();
+                                                                               myPanel = null;
+                                                                             }
+                                                                           }
+                                                                         });
+    if (isOK) {
+      for (Project project1 : projects) {
+        DaemonCodeAnalyzer.getInstance(project1).restart();
+      }
+    }
+    return isOK;
+  }
+
+  public boolean startInWriteAction() {
+    return false;
+  }
+}
