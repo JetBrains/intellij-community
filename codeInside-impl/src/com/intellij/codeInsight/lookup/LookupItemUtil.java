6711be4cb38f67af2ba547fb898948a4f6e424fd
Eugene Vigdorchik <no_reply@jetbrains.com>
2005-10-15 21:19:07 +0400

    eliminate wildcards bug

--- codeInside-impl/src/com/intellij/codeInsight/lookup/LookupItemUtil.java
+++ codeInside-impl/src/com/intellij/codeInsight/lookup/LookupItemUtil.java
@@ -133,7 +133,7 @@ public class LookupItemUtil{
       s = ((PsiExpression)object).getText();
     }
     else if (object instanceof PsiType) {
-      final PsiType type = CompletionUtil.eliminateWildcards((PsiType) object);
+      final PsiType type = (PsiType) object;
       if (type instanceof PsiPrimitiveType) {
         s = ((PsiType)object).getPresentableText();
       }
