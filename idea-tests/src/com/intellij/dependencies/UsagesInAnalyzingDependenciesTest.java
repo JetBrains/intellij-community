f8fb1edb0167c64be0c3f9b91bb038a4576820d6
Eugene Vigdorchik <no_reply@jetbrains.com>
2005-10-17 17:30:04 +0400

    remove deprecated UsageView

--- idea-tests/src/com/intellij/dependencies/UsagesInAnalyzingDependenciesTest.java
+++ idea-tests/src/com/intellij/dependencies/UsagesInAnalyzingDependenciesTest.java
@@ -1,26 +1,23 @@
 package com.intellij.dependencies;
 
 import com.intellij.analysis.AnalysisScope;
-import com.intellij.ide.highlighter.HighlighterFactory;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ex.PathManagerEx;
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.editor.ex.util.LexerEditorHighlighter;
 import com.intellij.openapi.projectRoots.impl.JavaSdkImpl;
 import com.intellij.packageDependencies.BackwardDependenciesBuilder;
 import com.intellij.packageDependencies.DependenciesBuilder;
 import com.intellij.packageDependencies.FindDependencyUtil;
 import com.intellij.packageDependencies.ForwardDependenciesBuilder;
 import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiPackage;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.testFramework.PsiTestCase;
 import com.intellij.testFramework.PsiTestUtil;
 import com.intellij.usageView.UsageInfo;
-import com.intellij.usageView.UsageTreeColorsScheme;
-import com.intellij.usageView.impl.UTUsageNode;
+import com.intellij.usages.TextChunk;
+import com.intellij.usages.Usage;
+import com.intellij.usages.UsageInfo2UsageAdapter;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -61,15 +58,23 @@ public class UsagesInAnalyzingDependenciesTest extends PsiTestCase{
     final PsiClass cClass = myPsiManager.findClass("com.b.C", GlobalSearchScope.allScope(myProject));
     searchIn.add(cClass.getContainingFile());
     final UsageInfo[] usagesInfos = FindDependencyUtil.findDependencies(builder, searchIn, searchFor);
+    final UsageInfo2UsageAdapter[] usages = UsageInfo2UsageAdapter.convert(usagesInfos);
     final String [] psiUsages = new String [usagesInfos.length];
     for (int i = 0; i < usagesInfos.length; i++) {
-      final PsiFile psiFile = usagesInfos[i].getElement().getContainingFile();
-      final Document document = getDocument(psiFile);
-      final LexerEditorHighlighter highlighter = getHighlighter(psiFile, document);
-      psiUsages[i] = new UTUsageNode(usagesInfos[i], psiFile, highlighter, document, false, false).toString();
+      psiUsages[i] = toString(usages[i]);
     }
     checkResult(new String []{"(4, 3) A myA = new A();", "(4, 15) A myA = new A();", "(6, 9) myA.aa();",
                               "(4, 3) A myA = new A();", "(4, 15) A myA = new A();", "(6, 9) myA.aa();"}, psiUsages);
+  }
+
+  private String toString(Usage usage) {
+    TextChunk[] textChunks = usage.getPresentation().getText();
+    StringBuffer result = new StringBuffer();
+    for (TextChunk textChunk : textChunks) {
+      result.append(textChunk);
+    }
+
+    return result.toString();
   }
 
    public void testBackwardPackageScope(){
@@ -84,12 +89,10 @@ public class UsagesInAnalyzingDependenciesTest extends PsiTestCase{
     final PsiClass cClass = myPsiManager.findClass("com.a.C", GlobalSearchScope.allScope(myProject));
     searchFor.add(cClass.getContainingFile());
     final UsageInfo[] usagesInfos = FindDependencyUtil.findBackwardDependencies(builder, searchIn, searchFor);
+    final UsageInfo2UsageAdapter[] usages = UsageInfo2UsageAdapter.convert(usagesInfos);
     final String [] psiUsages = new String [usagesInfos.length];
     for (int i = 0; i < usagesInfos.length; i++) {
-      final PsiFile psiFile = usagesInfos[i].getElement().getContainingFile();
-      final Document document = getDocument(psiFile);
-      final LexerEditorHighlighter highlighter = getHighlighter(psiFile, document);
-      psiUsages[i] = new UTUsageNode(usagesInfos[i], psiFile, highlighter, document, false, false).toString();
+      psiUsages[i] = toString(usages[i]);
     }
     checkResult(new String []{"(4, 3) A myA = new A();", "(4, 15) A myA = new A();", "(5, 3) C myC = new C();", "(5, 15) C myC = new C();", "(7, 9) myA.aa();", "(8, 9) myC.cc();"}, psiUsages);
   }
@@ -106,26 +109,12 @@ public class UsagesInAnalyzingDependenciesTest extends PsiTestCase{
     searchFor.add(bClass.getContainingFile());
 
     final UsageInfo[] usagesInfos = FindDependencyUtil.findDependencies(builder, searchIn, searchFor);
-
-    final PsiFile psiFile = aClass.getContainingFile();
-    final Document document = getDocument(psiFile);
-    final LexerEditorHighlighter highlighter = getHighlighter(psiFile, document);
+    final UsageInfo2UsageAdapter[] usages = UsageInfo2UsageAdapter.convert(usagesInfos);
     final String [] psiUsages = new String [usagesInfos.length];
     for (int i = 0; i < usagesInfos.length; i++) {
-      psiUsages[i] = new UTUsageNode(usagesInfos[i], psiFile, highlighter, document, false, false).toString();
+      psiUsages[i] = toString(usages[i]);
     }
     checkResult(new String []{"(2, 3) B myB = new B();", "(2, 15) B myB = new B();", "(4, 9) myB.bb();"}, psiUsages);
-  }
-
-  private LexerEditorHighlighter getHighlighter(final PsiFile psiFile, final Document document) {
-    final LexerEditorHighlighter highlighter = HighlighterFactory.createHighlighter(UsageTreeColorsScheme.getInstance().getScheme(),
-                                                                                    psiFile.getName(), myProject);
-    highlighter.setText(document.getCharsSequence());
-    return highlighter;
-  }
-
-  private Document getDocument(PsiFile psiFile){
-    return PsiDocumentManager.getInstance(myProject).getDocument(psiFile);
   }
 
   private void checkResult(final String[] usages, final String [] psiUsages) {
