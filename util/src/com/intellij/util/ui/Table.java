fadc91d1986e3d1d3a1db435af4b5bb3c8a1dac1
Olesya Smirnova <Olesya.Smirnova@jetbrains.com>
2005-10-19 21:40:17 +0400

    keyboard shortcuts support

--- util/src/com/intellij/util/ui/Table.java
+++ util/src/com/intellij/util/ui/Table.java
@@ -121,6 +121,8 @@ public class Table extends JTable {
       add(editorComp);
       editorComp.validate();
 
+      editorComp.requestFocusInWindow();
+
       setCellEditor(editor);
       setEditingRow(row);
       setEditingColumn(column);
