b6862f919e90d5c16c9db70fa9726195d2a3d9b7
Alexey Kudravtsev <cdr@jetbrains.com>
2005-10-26 17:28:45 +0400

    make ejb highlighting work

--- openapi/src/com/intellij/openapi/editor/colors/CodeInsightColors.java
+++ openapi/src/com/intellij/openapi/editor/colors/CodeInsightColors.java
@@ -18,9 +18,6 @@
  */
 package com.intellij.openapi.editor.colors;
 
-import com.intellij.openapi.editor.colors.ColorKey;
-import com.intellij.openapi.editor.colors.TextAttributesKey;
-
 public interface CodeInsightColors {
   TextAttributesKey WRONG_REFERENCES_ATTRIBUTES = TextAttributesKey.createTextAttributesKey("WRONG_REFERENCES_ATTRIBUTES");
   TextAttributesKey ERRORS_ATTRIBUTES = TextAttributesKey.createTextAttributesKey("ERRORS_ATTRIBUTES");
