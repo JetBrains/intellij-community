3cb24003cd128738ea2968f9e629c5e869785b62
greg <Gregory.Shrago@jetbrains.com>
2005-10-31 14:29:24 +0300

    hashCode & equals added

--- codeInsight/openapi/com/intellij/codeInsight/intention/EmptyIntentionAction.java
+++ codeInsight/openapi/com/intellij/codeInsight/intention/EmptyIntentionAction.java
@@ -45,4 +45,24 @@ public class EmptyIntentionAction implements IntentionAction{
   public boolean startInWriteAction() {
     return false;
   }
+
+  public boolean equals(final Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    final EmptyIntentionAction that = (EmptyIntentionAction)o;
+
+    if (myName != null ? !myName.equals(that.myName) : that.myName != null) return false;
+    if (myOptions != null ? !myOptions.equals(that.myOptions) : that.myOptions != null) return false;
+
+    return true;
+  }
+
+  public int hashCode() {
+    int result;
+    result = (myName != null ? myName.hashCode() : 0);
+    result = 29 * result + (myOptions != null ? myOptions.hashCode() : 0);
+    return result;
+  }
+
 }
