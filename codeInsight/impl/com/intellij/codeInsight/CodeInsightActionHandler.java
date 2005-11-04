6becaa74f08f3b9d3d206b012ac64d15f0b09e73
Eugene Vigdorchik <no_reply@jetbrains.com>
2005-11-04 13:42:01 +0300

    cleanup

--- codeInsight/impl/com/intellij/codeInsight/CodeInsightActionHandler.java
+++ codeInsight/impl/com/intellij/codeInsight/CodeInsightActionHandler.java
@@ -1,9 +1,8 @@
 package com.intellij.codeInsight;
 
-import com.intellij.psi.*;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
-import com.intellij.util.IncorrectOperationException;
+import com.intellij.psi.PsiFile;
 
 public interface CodeInsightActionHandler {
   void invoke(Project project, Editor editor, PsiFile file);
