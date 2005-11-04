5278628cad182f0cdb049afd4a8b5974aec205ce
Maxim Mossienko <Maxim.Mossienko@jetbrains.com>
2005-11-04 15:42:21 +0300

    NPE of null mimetype (IDEADEV-3094)

--- codeInsight/impl/com/intellij/codeInsight/daemon/impl/quickfix/FetchExtResourceAction.java
+++ codeInsight/impl/com/intellij/codeInsight/daemon/impl/quickfix/FetchExtResourceAction.java
@@ -374,7 +374,7 @@ public class FetchExtResourceAction extends BaseIntentionAction {
       InputStream in = urlConnection.getInputStream();
       String contentType = urlConnection.getContentType();
 
-      if (!ApplicationManager.getApplication().isUnitTestMode() && contentType.equals(HTML_MIME)) {
+      if (!ApplicationManager.getApplication().isUnitTestMode() && HTML_MIME.equals(contentType)) {
         ApplicationManager.getApplication().invokeLater(new Runnable() {
           public void run() {
             Messages.showMessageDialog(project,
