93ad236818b63a5cd8f6d2d7359d4b64d306326b
Peter Gromov <peter@jetbrains.com>
2005-11-09 18:36:43 +0300

    ValidatingComponent

--- openapi/src/com/intellij/ui/components/panels/ValidatingComponent.java
+++ openapi/src/com/intellij/ui/components/panels/ValidatingComponent.java
@@ -2,9 +2,7 @@
  * Copyright (c) 2000-2004 by JetBrains s.r.o. All Rights Reserved.
  * Use is subject to license terms.
  */
-package jetbrains.fabrique.ui.components;
-
-import com.intellij.ui.components.panels.NonOpaquePanel;
+package com.intellij.ui.components.panels;
 
 import javax.swing.*;
 import java.awt.*;
