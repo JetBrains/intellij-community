7e5e1ddb725ff759a20692eeee438fd9b2f44cc4
Maxim Shafirov <max@jetbrains.com>
2005-11-14 22:28:26 +0300

    Dependencies + css russian bundle moved to special module.

---
+++ action-system/impl/com/intellij/ide/impl/dataRules/UsageTargetsRule.java
@@ -0,0 +1,28 @@
+package com.intellij.ide.impl.dataRules;
+
+import com.intellij.find.FindManager;
+import com.intellij.find.findUsages.PsiElement2UsageTargetAdapter;
+import com.intellij.navigation.NavigationItem;
+import com.intellij.openapi.actionSystem.DataConstants;
+import com.intellij.openapi.actionSystem.DataProvider;
+import com.intellij.psi.PsiElement;
+import com.intellij.usages.UsageTarget;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: max
+ * Date: Dec 21, 2004
+ * Time: 3:59:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UsageTargetsRule implements GetDataRule {
+  public Object getData(DataProvider dataProvider) {
+    PsiElement psiElement = (PsiElement)dataProvider.getData(DataConstants.PSI_ELEMENT);
+    if (psiElement instanceof NavigationItem) {
+      if (!FindManager.getInstance(psiElement.getProject()).canFindUsages(psiElement)) return null;
+      return new UsageTarget[] {new PsiElement2UsageTargetAdapter(psiElement)};
+    }
+
+    return null;
+  }
+}
