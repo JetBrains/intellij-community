32e2846c9a9fc6aebad7343fc36625cb1ee4835f
Dmitry Jemerov <yole@intellij.com>
2005-11-22 21:14:58 +0300

    "Create field from parameter" action copies @Nullable annotation (IDEADEV-3284)

--- codeInsight/impl/com/intellij/codeInsight/intention/impl/CreateFieldFromParameterAction.java
+++ codeInsight/impl/com/intellij/codeInsight/intention/impl/CreateFieldFromParameterAction.java
@@ -2,6 +2,7 @@ package com.intellij.codeInsight.intention.impl;
 
 import com.intellij.codeInsight.CodeInsightBundle;
 import com.intellij.codeInsight.CodeInsightUtil;
+import com.intellij.codeInsight.AnnotationUtil;
 import com.intellij.codeInsight.intention.IntentionAction;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
@@ -160,6 +161,14 @@ public class CreateFieldFromParameterAction implements IntentionAction {
           PsiModifierList modifierList = field.getModifierList();
           modifierList.setModifierProperty(PsiModifier.STATIC, isMethodStatic);
           modifierList.setModifierProperty(PsiModifier.FINAL, isFinal);
+
+          PsiAnnotation[] annotations = myParameter.getAnnotations();
+          for(PsiAnnotation ann: annotations) {
+            if (AnnotationUtil.NULLABLE.equals(ann.getQualifiedName())) {
+              modifierList.addAfter(factory.createAnnotationFromText("@" + AnnotationUtil.NULLABLE, field), null);
+              break;
+            }
+          }
 
           PsiCodeBlock methodBody = method.getBody();
           PsiStatement[] statements = methodBody.getStatements();
