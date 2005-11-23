4894cc98cfdafb58a5b5bdceac48d27de4b00934
Vyacheslav Lukianov <lvo@jetbrains.com>
2005-11-23 16:17:51 +0300

    SpinAllocator and StringBuilderSpinAllocator classes implemented and used

---
+++ util/src/com/intellij/util/StringBuilderSpinAllocator.java
@@ -0,0 +1,30 @@
+package com.intellij.util;
+
+/**
+ * StringBuilderSpinAllocator reuses StringBuilder instances performing non-blocking allocation and dispose.
+ */
+public class StringBuilderSpinAllocator {
+
+  private static class Creator implements SpinAllocator.ICreator<StringBuilder> {
+    public StringBuilder createInstance() {
+      return new StringBuilder();
+    }
+  }
+
+  private static class Disposer implements SpinAllocator.IDisposer<StringBuilder> {
+    public void disposeInstance(final StringBuilder instance) {
+      instance.setLength(0);
+    }
+  }
+
+  private static SpinAllocator<StringBuilder> myAllocator =
+    new SpinAllocator<StringBuilder>(new Creator(), new Disposer());
+
+  public static StringBuilder alloc() {
+    return myAllocator.alloc();
+  }
+
+  public static void dispose(StringBuilder instance) {
+    myAllocator.dispose(instance);
+  }
+}
