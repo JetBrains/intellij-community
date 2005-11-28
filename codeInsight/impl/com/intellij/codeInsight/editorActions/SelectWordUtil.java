8cc4faad612ad77c14b156635e021d3b2d2f3bae
Maxim Mossienko <Maxim.Mossienko@jetbrains.com>
2005-11-28 20:35:36 +0300

    ctrl-w in XML CDATA sections (IDEADEV-3399)

--- codeInsight/impl/com/intellij/codeInsight/editorActions/SelectWordUtil.java
+++ codeInsight/impl/com/intellij/codeInsight/editorActions/SelectWordUtil.java
@@ -10,6 +10,7 @@ import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.util.text.LineTokenizer;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.source.jsp.jspJava.JspCodeBlock;
+import com.intellij.psi.impl.source.tree.CompositePsiElement;
 import com.intellij.psi.javadoc.PsiDocComment;
 import com.intellij.psi.javadoc.PsiDocTag;
 import com.intellij.psi.javadoc.PsiDocToken;
@@ -45,6 +46,7 @@ public class SelectWordUtil {
     new CaseStatementsSelectioner(),
     new HtmlSelectioner(),
     new XmlTagSelectioner(),
+    new XmlCDATAContentSelectioner(),
     new DtdSelectioner(),
     new XmlElementSelectioner(),
     new XmlTokenSelectioner(),
@@ -910,7 +912,43 @@ public class SelectWordUtil {
       return result;
     }
   }
-  
+
+  static class XmlCDATAContentSelectioner extends BasicSelectioner {
+    public boolean canSelect(PsiElement e) {
+      return e instanceof CompositePsiElement &&
+             ((CompositePsiElement)e).getElementType() == XmlElementType.XML_CDATA;
+    }
+
+    public List<TextRange> select(PsiElement e, CharSequence editorText, int cursorOffset, Editor editor) {
+      List<TextRange> result = super.select(e, editorText, cursorOffset, editor);
+      PsiElement[] children = e.getChildren();
+
+      PsiElement first = null;
+      PsiElement last = null;
+      for (PsiElement child : children) {
+        if (child instanceof XmlToken) {
+          XmlToken token = (XmlToken)child;
+          if (token.getTokenType() == XmlTokenType.XML_CDATA_START) {
+            first = token.getNextSibling();
+          }
+          if (token.getTokenType() == XmlTokenType.XML_CDATA_END) {
+            last = token.getPrevSibling();
+            break;
+          }
+        }
+      }
+
+      if (first != null && last != null) {
+        result.addAll(expandToWholeLine(editorText,
+                                        new TextRange(first.getTextRange().getStartOffset(),
+                                                      last.getTextRange().getEndOffset()),
+                                        false));
+      }
+
+      return result;
+    }
+  }
+
   static class XmlTagSelectioner extends BasicSelectioner {
     public boolean canSelect(PsiElement e) {
       return e instanceof XmlTag;
@@ -1007,10 +1045,10 @@ public class SelectWordUtil {
 
     public List<TextRange> select(PsiElement e, CharSequence editorText, int cursorOffset, Editor editor) {
       XmlToken token = (XmlToken)e;
-      
+
       if (token.getTokenType() != XmlTokenType.XML_DATA_CHARACTERS &&
           token.getTokenType() != XmlTokenType.XML_START_TAG_START &&
-          token.getTokenType() != XmlTokenType.XML_END_TAG_START 
+          token.getTokenType() != XmlTokenType.XML_END_TAG_START
         ) {
         List<TextRange> ranges = super.select(e, editorText, cursorOffset, editor);
         addWordSelection(editor.getSettings().isCamelWords(), editorText, cursorOffset, ranges);
