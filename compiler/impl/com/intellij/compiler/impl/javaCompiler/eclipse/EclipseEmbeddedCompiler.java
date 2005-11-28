f5e905fabac2782a793d354ad1f74dfd260d1855
Alexey Kudravtsev <cdr@jetbrains.com>
2005-11-28 19:42:27 +0300

    cache loaded compiler classs

--- compiler/impl/com/intellij/compiler/impl/javaCompiler/eclipse/EclipseEmbeddedCompiler.java
+++ compiler/impl/com/intellij/compiler/impl/javaCompiler/eclipse/EclipseEmbeddedCompiler.java
@@ -4,13 +4,13 @@ import com.intellij.compiler.OutputParser;
 import com.intellij.compiler.impl.javaCompiler.BackendCompiler;
 import com.intellij.compiler.impl.javaCompiler.ModuleChunk;
 import com.intellij.compiler.impl.javaCompiler.javac.JavacEmbeddedCompiler;
+import com.intellij.ide.plugins.PluginManager;
+import com.intellij.ide.plugins.cl.IdeaClassLoader;
 import com.intellij.openapi.compiler.CompileContext;
 import com.intellij.openapi.compiler.CompilerBundle;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.project.Project;
-import com.intellij.ide.plugins.cl.IdeaClassLoader;
-import com.intellij.ide.plugins.PluginManager;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -19,11 +19,11 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
-import java.lang.reflect.InvocationTargetException;
 
 
 public class EclipseEmbeddedCompiler implements BackendCompiler {
@@ -37,6 +37,12 @@ public class EclipseEmbeddedCompiler implements BackendCompiler {
   public EclipseEmbeddedCompiler(Project project) {
     myProject = project;
     myEclipseExternalCompiler = new EclipseCompiler(project);
+    try {
+      createCompileDriver();
+    }
+    catch (Exception e) {
+      LOG.error(e);
+    }
   }
 
   public boolean checkCompiler() {
@@ -85,7 +91,6 @@ public class EclipseEmbeddedCompiler implements BackendCompiler {
       if (canDelegate(name)) {
         return super.loadClass(name, resolve);
       }
-
       Class<?> c = findLoadedClass(name);
       if (c == null) {
         try {
@@ -102,45 +107,13 @@ public class EclipseEmbeddedCompiler implements BackendCompiler {
       return c;
     }
 
-    private static boolean canDelegate(final String name) {
+    private static boolean canDelegate(@NonNls final String name) {
       return !name.startsWith(EclipseCompilerDriver.class.getName()) && !name.startsWith("org.eclipse.");
     }
   }
+
   @NotNull
   public Process launchProcess(final ModuleChunk chunk, final String outputDir, final CompileContext compileContext) throws IOException {
-    URL jarUrl = new File(EclipseCompiler.PATH_TO_COMPILER_JAR).toURI().toURL();
-    final ClassLoader classLoader = PluginManager.class.getClassLoader();
-    final Class<? extends ClassLoader> pluginClass = classLoader.getClass();
-    if (!pluginClass.getName().equals(IdeaClassLoader.class.getName())) return null;
-    ArrayList<URL> urls = null;
-    try {
-      urls = new ArrayList<URL>((ArrayList<URL>)pluginClass.getDeclaredMethod("getUrls").invoke(classLoader));
-    }
-    catch (IllegalAccessException e) {
-      LOG.error(e);
-    }
-    catch (InvocationTargetException e) {
-      LOG.error(e);
-    }
-    catch (NoSuchMethodException e) {
-      LOG.error(e);
-    }
-    urls.add(jarUrl);
-    ClassLoader myclassLoader = new MyClassLoader(urls.toArray(new URL[urls.size()]), classLoader);
-    try {
-      String name = EclipseCompilerDriver.class.getName();
-      Class<?> aClass = myclassLoader.loadClass(name);
-      myEclipseCompilerDriver = (IEclipseCompilerDriver)aClass.newInstance();
-    }
-    catch (InstantiationException e) {
-      LOG.error(e);
-    }
-    catch (IllegalAccessException e) {
-      LOG.error(e);
-    }
-    catch (ClassNotFoundException e) {
-      LOG.error(e);
-    }
 
     final String[] commands = myEclipseExternalCompiler.createStartupCommand(chunk, compileContext, outputDir);
     final String[] modifiedCmds = JavacEmbeddedCompiler.createCommandsForEmbeddedCall(commands, EclipseCompiler.getCompilerClass());
@@ -185,5 +158,43 @@ public class EclipseEmbeddedCompiler implements BackendCompiler {
       }
     };
     return process;
+  }
+
+  private void createCompileDriver() throws Exception {
+    URL jarUrl = new File(EclipseCompiler.PATH_TO_COMPILER_JAR).toURI().toURL();
+    final ClassLoader classLoader = PluginManager.class.getClassLoader();
+    final Class<? extends ClassLoader> pluginClass = classLoader.getClass();
+    if (!pluginClass.getName().equals(IdeaClassLoader.class.getName())) {
+      throw new Exception();
+    }
+    ArrayList<URL> urls = null;
+    try {
+      urls = new ArrayList<URL>((ArrayList<URL>)pluginClass.getDeclaredMethod("getUrls").invoke(classLoader));
+    }
+    catch (IllegalAccessException e) {
+      LOG.error(e);
+    }
+    catch (InvocationTargetException e) {
+      LOG.error(e);
+    }
+    catch (NoSuchMethodException e) {
+      LOG.error(e);
+    }
+    urls.add(jarUrl);
+    ClassLoader myclassLoader = new MyClassLoader(urls.toArray(new URL[urls.size()]), classLoader);
+    try {
+      String name = EclipseCompilerDriver.class.getName();
+      Class<?> aClass = myclassLoader.loadClass(name);
+      myEclipseCompilerDriver = (IEclipseCompilerDriver)aClass.newInstance();
+    }
+    catch (InstantiationException e) {
+      LOG.error(e);
+    }
+    catch (IllegalAccessException e) {
+      LOG.error(e);
+    }
+    catch (ClassNotFoundException e) {
+      LOG.error(e);
+    }
   }
 }
