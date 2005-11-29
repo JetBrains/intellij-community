6483f56b718ac024a264669224991f57fa5ea1f5
Bas Leijdekkers <basleijdekkers@gmail.com>
2005-11-29 13:08:44 +0300

    fix for IDEADEV-2773

--- plugins/InspectionGadgets/src/com/siyeh/ig/j2me/SingleCharacterStartsWithInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/j2me/SingleCharacterStartsWithInspection.java
@@ -17,65 +17,119 @@ package com.siyeh.ig.j2me;
 
 import com.intellij.codeInsight.daemon.GroupNames;
 import com.intellij.psi.*;
+import com.intellij.openapi.project.Project;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.util.IncorrectOperationException;
+import com.siyeh.HardcodedMethodConstants;
+import com.siyeh.InspectionGadgetsBundle;
 import com.siyeh.ig.BaseInspectionVisitor;
 import com.siyeh.ig.ExpressionInspection;
+import com.siyeh.ig.InspectionGadgetsFix;
 import com.siyeh.ig.psiutils.TypeUtils;
-import com.siyeh.InspectionGadgetsBundle;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.NonNls;
 
 public class SingleCharacterStartsWithInspection extends ExpressionInspection {
 
     public String getDisplayName() {
-        return InspectionGadgetsBundle.message("single.character.startswith.display.name");
+        return InspectionGadgetsBundle.message(
+                "single.character.startswith.display.name");
     }
 
     public String getGroupDisplayName() {
         return GroupNames.J2ME_GROUP_NAME;
     }
 
+    @Nullable
+    protected InspectionGadgetsFix buildFix(PsiElement location) {
+        return new SingleCharacterStartsWithFix();
+    }
+
+    private static class SingleCharacterStartsWithFix
+            extends InspectionGadgetsFix {
+
+        public String getName() {
+            return InspectionGadgetsBundle.message(
+                    "single.character.startswith.quickfix");
+        }
+
+        protected void doFix(Project project, ProblemDescriptor descriptor)
+                throws IncorrectOperationException {
+            final PsiElement element = descriptor.getPsiElement();
+            final PsiReferenceExpression methodExpression =
+                    (PsiReferenceExpression)element.getParent();
+            final PsiMethodCallExpression methodCall =
+                    (PsiMethodCallExpression)methodExpression.getParent();
+            final PsiElement qualifier = methodExpression.getQualifier();
+            final PsiExpressionList argumentList = methodCall.getArgumentList();
+            final PsiExpression[] expressions = argumentList.getExpressions();
+            final PsiExpression expression = expressions[0];
+            final String expressionText = expression.getText();
+            final String character = expressionText.substring(1,
+                    expressionText.length() - 1);
+            if (qualifier == null) {
+                return;
+            }
+            final String qualifierText = qualifier.getText();
+            @NonNls final String newExpression;
+            final String referenceName = methodExpression.getReferenceName();
+            if (HardcodedMethodConstants.STARTS_WITH.equals(referenceName)) {
+                newExpression = qualifierText + ".length() > 0 && " +
+                        qualifierText + ".charAt(0) == '" + character + '\'';
+            } else {
+                newExpression = qualifierText + ".length() > 0 && " +
+                        qualifierText + ".charAt(" + qualifierText +
+                        ".length() - 1) == '" + character + '\'';
+            }
+            replaceExpression(methodCall, newExpression);
+        }
+    }
+
     public String buildErrorString(PsiElement location) {
-        return InspectionGadgetsBundle.message("single.character.startswith.problem.descriptor");
+        return InspectionGadgetsBundle.message(
+                "single.character.startswith.problem.descriptor");
     }
 
     public BaseInspectionVisitor buildVisitor() {
         return new SingleCharacterStartsWithVisitor();
     }
 
-    private static class SingleCharacterStartsWithVisitor extends BaseInspectionVisitor {
-      @NonNls private static final String STARTS_WITH_METHOD = "startsWith";
-      @NonNls private static final String ENDS_WITH_METHOD = "endsWith";
+    private static class SingleCharacterStartsWithVisitor
+            extends BaseInspectionVisitor {
 
-      public void visitMethodCallExpression(@NotNull PsiMethodCallExpression call) {
-          super.visitMethodCallExpression(call);
-          final PsiReferenceExpression methodExpression = call.getMethodExpression();
-          final String methodName = methodExpression.getReferenceName();
-          if (!STARTS_WITH_METHOD.equals(methodName) && !ENDS_WITH_METHOD.equals(methodName)) {
-              return;
-          }
-          final PsiExpressionList argumentList = call.getArgumentList();
-          if (argumentList == null) {
-              return;
-          }
-          final PsiExpression[] args = argumentList.getExpressions();
-          if (args.length != 1 && args.length != 2) {
-              return;
-          }
-          if (!isSingleCharacterStringLiteral(args[0])) {
-              return;
-          }
-          final PsiExpression qualifier = methodExpression.getQualifierExpression();
-          if (qualifier == null) {
-              return;
-          }
-          final PsiType type = qualifier.getType();
-          if (!TypeUtils.isJavaLangString(type)) {
-              return;
-          }
-          registerMethodCallError(call);
-      }
+        public void visitMethodCallExpression(
+                @NotNull PsiMethodCallExpression call) {
+            super.visitMethodCallExpression(call);
+            final PsiReferenceExpression methodExpression =
+                    call.getMethodExpression();
+            final String methodName = methodExpression.getReferenceName();
+            if (!HardcodedMethodConstants.STARTS_WITH.equals(methodName) &&
+                    !HardcodedMethodConstants.ENDS_WITH.equals(methodName)) {
+                return;
+            }
+            final PsiExpressionList argumentList = call.getArgumentList();
+            final PsiExpression[] args = argumentList.getExpressions();
+            if (args.length != 1 && args.length != 2) {
+                return;
+            }
+            if (!isSingleCharacterStringLiteral(args[0])) {
+                return;
+            }
+            final PsiExpression qualifier =
+                    methodExpression.getQualifierExpression();
+            if (qualifier == null) {
+                return;
+            }
+            final PsiType type = qualifier.getType();
+            if (!TypeUtils.isJavaLangString(type)) {
+                return;
+            }
+            registerMethodCallError(call);
+        }
 
-        private static boolean isSingleCharacterStringLiteral(PsiExpression arg) {
+        private static boolean isSingleCharacterStringLiteral(
+                PsiExpression arg) {
             final PsiType type = arg.getType();
             if (!TypeUtils.isJavaLangString(type)) {
                 return false;
