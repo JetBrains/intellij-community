73f1e319c78233615b51d6bd629ca84a0ed0361b
Alexey Pegov <Alexey.Pegov@jetbrains.com>
2005-11-30 15:32:59 +0300

    fix live templates NPE at the end of file

--- codeInsight/impl/com/intellij/codeInsight/template/impl/TemplateState.java
+++ codeInsight/impl/com/intellij/codeInsight/template/impl/TemplateState.java
@@ -158,7 +158,7 @@ public class TemplateState implements Disposable {
     int start = mySegments.getSegmentStart(segmentNumber);
     int end = mySegments.getSegmentEnd(segmentNumber);
     int length = myDocument.getTextLength();
-    if (start >= length || end > length) {
+    if (start > length || end > length) {
       return null;
     }
     return new TextResult(text.subSequence(start, end).toString());
