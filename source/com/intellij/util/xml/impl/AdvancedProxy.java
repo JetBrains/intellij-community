3d712d37191f0eadb89a203a76c0159054ca3573
Peter Gromov <peter@jetbrains.com>
2005-12-05 20:03:47 +0300

    Factory used

--- source/com/intellij/util/xml/impl/AdvancedProxy.java
+++ source/com/intellij/util/xml/impl/AdvancedProxy.java
@@ -3,31 +3,28 @@
  */
 package com.intellij.util.xml.impl;
 
-import com.intellij.util.containers.WeakValueHashMap;
 import com.intellij.openapi.util.Pair;
-import net.sf.cglib.core.CodeGenerationException;
+import com.intellij.util.containers.WeakValueHashMap;
 import net.sf.cglib.proxy.AdvancedEnhancer;
 import net.sf.cglib.proxy.Callback;
-import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.Factory;
 import net.sf.cglib.proxy.InvocationHandler;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
  * @author peter
  */
 public class AdvancedProxy {
-  private static final Map<Pair<Class, Class[]>, Class> ourProxyClasses = new HashMap<Pair<Class, Class[]>, Class>();
-  private static final Map<Object, InvocationHandler> ourInvocationHandlers = new WeakValueHashMap<Object, InvocationHandler>();
+  private static final Map<Pair<Class, Class[]>, Factory> ourFactories = new WeakValueHashMap<Pair<Class, Class[]>, Factory>();
 
   public static InvocationHandler getInvocationHandler(Object proxy) {
-    return ourInvocationHandlers.get(proxy);
+    return (InvocationHandler)((Factory) proxy).getCallback(0);
   }
 
   public static <T> T createProxy(final Class<T> superClass,
-                                 final InvocationHandler handler,
-                                 final Object... constructorArgs) {
+                                  final InvocationHandler handler,
+                                  final Object... constructorArgs) {
     if (superClass.isInterface()) {
       return (T) createProxy(null, new Class[]{superClass}, handler, constructorArgs);
     }
@@ -36,40 +33,27 @@ public class AdvancedProxy {
 
   public static <T> T createProxy(final Class<T> superClass,
                                   final Class[] interfaces,
-                                 final InvocationHandler handler,
-                                 final Object... constructorArgs) {
+                                  final InvocationHandler handler,
+                                  final Object... constructorArgs) {
     try {
-      Class clazz = getOrCreateProxyClass(superClass, interfaces);
-      Enhancer.registerStaticCallbacks(clazz, new Callback[]{ handler, null });
-      final T t = (T)clazz.getConstructors()[0].newInstance(constructorArgs);
-      ourInvocationHandlers.put(t, handler);
-      return t;
-    } catch (RuntimeException e) {
-      throw e;
-    } catch (Exception e) {
-      throw new CodeGenerationException(e);
+      final Pair<Class, Class[]> key = new Pair<Class, Class[]>(superClass, interfaces);
+      Factory factory = ourFactories.get(key);
+      if (factory != null) {
+        return (T) factory.newInstance(factory.getClass().getConstructors()[0].getParameterTypes(), constructorArgs, new Callback[]{handler});
+      }
+
+      AdvancedEnhancer e = new AdvancedEnhancer();
+      e.setSuperclass(superClass);
+      e.setInterfaces(interfaces);
+      e.setCallbackTypes(new Class[]{InvocationHandler.class});
+      factory = (Factory)e.createClass().getConstructors()[0].newInstance(constructorArgs);
+      factory.setCallback(0, handler);
+      ourFactories.put(key, factory);
+      return (T)factory;
+    }
+    catch (Exception e) {
+      throw new RuntimeException(e);
     }
   }
-
-  private static <T>Class getOrCreateProxyClass(final Class<T> superClass, final Class... interfaces) {
-    final Pair<Class, Class[]> key = new Pair<Class, Class[]>(superClass, interfaces);
-    Class proxyClass = ourProxyClasses.get(key);
-    if (proxyClass == null) {
-      proxyClass = getProxyClass(superClass, interfaces);
-      ourProxyClasses.put(key, proxyClass);
-    }
-    return proxyClass;
-  }
-
-  private static Class getProxyClass(final Class superClass, final Class... interfaces) {
-    AdvancedEnhancer e = new AdvancedEnhancer();
-    e.setSuperclass(superClass);
-    e.setInterfaces(interfaces);
-    e.setCallbackTypes(new Class[]{InvocationHandler.class});
-    e.setUseFactory(false);
-
-    return e.createClass();
-  }
-
 
 }
