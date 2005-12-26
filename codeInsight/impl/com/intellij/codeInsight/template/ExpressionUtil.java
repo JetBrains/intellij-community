931cffcfa8c478913681488083100884c306fb26
Eugene Vigdorchik <no_reply@jetbrains.com>
2005-12-26 16:04:13 +0300

    rollback

--- codeInsight/impl/com/intellij/codeInsight/template/ExpressionUtil.java
+++ codeInsight/impl/com/intellij/codeInsight/template/ExpressionUtil.java
@@ -2,7 +2,6 @@
 
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.*;
 import com.intellij.psi.codeStyle.CodeStyleManager;
 import com.intellij.psi.codeStyle.SuggestedNameInfo;
@@ -10,66 +9,51 @@ import com.intellij.psi.codeStyle.VariableKind;
 import com.intellij.psi.text.BlockSupport;
 import com.intellij.util.IncorrectOperationException;
 
-import java.util.Set;
-import java.util.HashSet;
-import java.util.Arrays;
+/**
+ * @author mike
+ */
+public class ExpressionUtil {
+  private static final Logger LOG = Logger.getInstance("#com.intellij.codeInsight.template.ExpressionUtil");
 
- /**
-  * @author mike
-  */
- public class ExpressionUtil {
-   private static final Logger LOG = Logger.getInstance("#com.intellij.codeInsight.template.ExpressionUtil");
+  public static String[] getNames(final ExpressionContext context) {
+    Project project = context.getProject();
+    int offset = context.getStartOffset();
 
-   public static String[] getNames(final ExpressionContext context) {
-     Project project = context.getProject();
-     int offset = context.getStartOffset();
+    PsiDocumentManager.getInstance(project).commitAllDocuments();
 
-     PsiDocumentManager.getInstance(project).commitAllDocuments();
+    String[] names = null;
+    PsiFile file = PsiDocumentManager.getInstance(project).getPsiFile(context.getEditor().getDocument());
+    PsiElement element = file.findElementAt(offset);
+    if (element instanceof PsiIdentifier){
+      names = getNamesForIdentifier(project, (PsiIdentifier)element);
+    }
+    else{
+      PsiFile fileCopy = (PsiFile)file.copy();
+      BlockSupport blockSupport = project.getComponent(BlockSupport.class);
+      try{
+        blockSupport.reparseRange(fileCopy, offset, offset, "xxx");
+      }
+      catch(IncorrectOperationException e){
+        LOG.error(e);
+      }
+      PsiElement identifierCopy = fileCopy.findElementAt(offset);
+      if (identifierCopy instanceof PsiIdentifier) {
+        names = getNamesForIdentifier(project, (PsiIdentifier)identifierCopy);
+      }
+    }
+    return names;
+  }
 
-     String[] names = null;
-     PsiFile file = PsiDocumentManager.getInstance(project).getPsiFile(context.getEditor().getDocument());
-     PsiElement element = file.findElementAt(offset);
-     if (element instanceof PsiIdentifier){
-       names = getNamesForIdentifier(project, (PsiIdentifier)element);
-     }
-     else{
-       PsiFile fileCopy = (PsiFile)file.copy();
-       BlockSupport blockSupport = project.getComponent(BlockSupport.class);
-       try{
-         blockSupport.reparseRange(fileCopy, offset, offset, "xxx");
-       }
-       catch(IncorrectOperationException e){
-         LOG.error(e);
-       }
-       PsiElement identifierCopy = fileCopy.findElementAt(offset);
-       if (identifierCopy instanceof PsiIdentifier) {
-         names = getNamesForIdentifier(project, (PsiIdentifier)identifierCopy);
-       }
-     }
-     return names;
-   }
-
-   private static String[] getNamesForIdentifier(Project project, PsiIdentifier identifier){
-     if (identifier.getParent() instanceof PsiVariable){
-       PsiVariable var = (PsiVariable)identifier.getParent();
-       if (var.getNameIdentifier().equals(identifier)) {
-         CodeStyleManager codeStyleManager = CodeStyleManager.getInstance(project);
-         VariableKind variableKind = codeStyleManager.getVariableKind(var);
-         SuggestedNameInfo suggestedInfo = codeStyleManager.suggestVariableName(variableKind, null, var.getInitializer(), var.getType());
-         if (var instanceof PsiParameter && ((PsiParameter)var).getDeclarationScope() instanceof PsiForeachStatement) {
-           final PsiExpression iteratedValue = ((PsiForeachStatement)((PsiParameter)var).getDeclarationScope()).getIteratedValue();
-           if (iteratedValue instanceof PsiReferenceExpression) {
-             final String referenceName = ((PsiReferenceExpression)iteratedValue).getReferenceName();
-             if (referenceName != null) {
-               final Set<String> names = new HashSet<String>(Arrays.asList(suggestedInfo.names));
-               names.add(StringUtil.unpluralize(referenceName));
-               return names.toArray(new String[names.size()]);
-             }
-           }
-         }
-         return suggestedInfo.names; //TODO: callback about choosen name
-       }
-     }
-     return null;
-   }
- }
+  private static String[] getNamesForIdentifier(Project project, PsiIdentifier identifier){
+    if (identifier.getParent() instanceof PsiVariable){
+      PsiVariable var = (PsiVariable)identifier.getParent();
+      if (var.getNameIdentifier().equals(identifier)){
+        CodeStyleManager codeStyleManager = CodeStyleManager.getInstance(project);
+        VariableKind variableKind = codeStyleManager.getVariableKind(var);
+        SuggestedNameInfo suggestedInfo = codeStyleManager.suggestVariableName(variableKind, null, var.getInitializer(), var.getType());
+        return suggestedInfo.names; //TODO: callback about choosen name
+      }
+    }
+    return null;
+  }
+}
