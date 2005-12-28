7789f9adbd88dad9965a317446d330b3f568bff7
Eugene Vigdorchik <no_reply@jetbrains.com>
2005-12-28 17:44:59 +0300

    IDEADEV-3728

--- refactoring/impl/com/intellij/refactoring/rename/naming/AutomaticVariableRenamer.java
+++ refactoring/impl/com/intellij/refactoring/rename/naming/AutomaticVariableRenamer.java
@@ -1,25 +1,48 @@
 package com.intellij.refactoring.rename.naming;
 
 import com.intellij.psi.*;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.codeStyle.CodeStyleManager;
 import com.intellij.usageView.UsageInfo;
 import com.intellij.refactoring.RefactoringBundle;
+import com.intellij.util.containers.HashSet;
+import com.intellij.openapi.util.text.StringUtil;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author dsl
  */
 public class AutomaticVariableRenamer extends AutomaticRenamer {
+  private Set<PsiVariable>myToUnpluralize = new HashSet<PsiVariable>();
   public AutomaticVariableRenamer(PsiClass aClass, String newClassName, List<UsageInfo> usages) {
     for (Iterator<UsageInfo> iterator = usages.iterator(); iterator.hasNext();) {
       final UsageInfo info = iterator.next();
-      if (info.getElement() instanceof PsiJavaCodeReferenceElement
-          && info.getElement().getParent() instanceof PsiTypeElement
-        &&  info.getElement().getParent().getParent() instanceof PsiVariable
-      ) {
-        myElements.add(((PsiVariable)info.getElement().getParent().getParent()));
+      final PsiElement element = info.getElement();
+      if (!(element instanceof PsiJavaCodeReferenceElement)) continue;
+      final PsiVariable variable = PsiTreeUtil.getParentOfType(element, PsiVariable.class);
+      if (variable == null) continue;
+      final PsiJavaCodeReferenceElement ref = variable.getTypeElement().getInnermostComponentReferenceElement();
+      if (ref == null) continue;
+      if (ref.equals(element)) {
+        myElements.add((variable));
+        if (variable.getType() instanceof PsiArrayType) {
+          myToUnpluralize.add(variable);
+        }
+      } else {
+        PsiType collectionType = variable.getManager().getElementFactory().createTypeByFQClassName("java.util.Collection", variable.getResolveScope());
+        if (!collectionType.isAssignableFrom(variable.getType())) continue;
+        final PsiTypeElement[] typeParameterElements = ref.getParameterList().getTypeParameterElements();
+        for (PsiTypeElement typeParameterElement : typeParameterElements) {
+          final PsiJavaCodeReferenceElement parameterRef = typeParameterElement.getInnermostComponentReferenceElement();
+          if (parameterRef != null && parameterRef.equals(element)) {
+            myElements.add((variable));
+            myToUnpluralize.add(variable);
+            break;
+          }
+        }
       }
     }
     suggestAllNames(aClass.getName(), newClassName);
@@ -39,12 +62,17 @@ public class AutomaticVariableRenamer extends AutomaticRenamer {
 
   public String nameToCanonicalName(String name, PsiNamedElement psiVariable) {
     final CodeStyleManager codeStyleManager = CodeStyleManager.getInstance(psiVariable.getManager());
-    return codeStyleManager.variableNameToPropertyName(name, codeStyleManager.getVariableKind((PsiVariable)psiVariable));
+    final String propertyName = codeStyleManager.variableNameToPropertyName(name, codeStyleManager.getVariableKind((PsiVariable)psiVariable));
+    if (myToUnpluralize.contains(psiVariable)) return StringUtil.unpluralize(propertyName);
+    return propertyName;
   }
 
   public String canonicalNameToName(String canonicalName, PsiNamedElement psiVariable) {
     final CodeStyleManager codeStyleManager = CodeStyleManager.getInstance(psiVariable.getManager());
-    return codeStyleManager.propertyNameToVariableName(canonicalName, codeStyleManager.getVariableKind((PsiVariable)psiVariable));
+    final String variableName =
+      codeStyleManager.propertyNameToVariableName(canonicalName, codeStyleManager.getVariableKind((PsiVariable)psiVariable));
+    if (myToUnpluralize.contains(psiVariable)) return StringUtil.pluralize(variableName);
+    return variableName;
   }
 
 }
