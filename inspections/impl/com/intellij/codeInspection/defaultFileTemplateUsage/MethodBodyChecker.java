730f25aeec6544a74947498406fec019703a10ef
Alexey Kudravtsev <cdr@jetbrains.com>
2006-01-06 21:18:10 +0300

    inspection performance

--- inspections/impl/com/intellij/codeInspection/defaultFileTemplateUsage/MethodBodyChecker.java
+++ inspections/impl/com/intellij/codeInspection/defaultFileTemplateUsage/MethodBodyChecker.java
@@ -1,29 +1,59 @@
 package com.intellij.codeInspection.defaultFileTemplateUsage;
 
-import com.intellij.psi.*;
-import com.intellij.psi.codeStyle.CodeStyleManager;
-import com.intellij.codeInspection.*;
 import com.intellij.codeInsight.PsiEquivalenceUtil;
-import com.intellij.codeInsight.generation.OverrideImplementUtil;
 import com.intellij.codeInsight.daemon.impl.quickfix.CreateFromUsageUtils;
-import com.intellij.openapi.util.Pair;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.util.IncorrectOperationException;
-import com.intellij.ide.fileTemplates.FileTemplateManager;
+import com.intellij.codeInsight.generation.OverrideImplementUtil;
+import com.intellij.codeInspection.*;
 import com.intellij.ide.fileTemplates.FileTemplate;
+import com.intellij.ide.fileTemplates.FileTemplateManager;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.util.Pair;
+import com.intellij.psi.*;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.codeStyle.CodeStyleManager;
+import com.intellij.util.IncorrectOperationException;
+import gnu.trove.THashMap;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.Comparator;
 
 /**
  * @author Alexey
  */
 public class MethodBodyChecker {
   private static final Logger LOG = Logger.getInstance("#com.intellij.codeInspection.defaultFileTemplateUsage.MethodBodyChecker");
+  private static final Map<Pair<PsiType,FileTemplate>, PsiMethod> TEMPLATE_METHOD_BODIES = new THashMap<Pair<PsiType, FileTemplate>, PsiMethod>();
+  private final static Project DEFAULT_PROJECT = ProjectManager.getInstance().getDefaultProject();
+  private final static PsiClassType OBJECT_TYPE = PsiType.getJavaLangObject(PsiManager.getInstance(DEFAULT_PROJECT), GlobalSearchScope.allScope(DEFAULT_PROJECT));
+
+  private static PsiMethod getTemplateMethod(PsiType returnType, List<HierarchicalMethodSignature> superSignatures, final PsiClass aClass) {
+    if (!(returnType instanceof PsiPrimitiveType)) {
+      returnType = OBJECT_TYPE;
+    }
+    try {
+      final FileTemplate fileTemplate = getMethodFileTemplate(superSignatures, true);
+      Pair<PsiType, FileTemplate> key = Pair.create(returnType, fileTemplate);
+      PsiMethod method = TEMPLATE_METHOD_BODIES.get(key);
+      if (method == null) {
+        method = PsiManager.getInstance(DEFAULT_PROJECT).getElementFactory().createMethod("x", returnType);
+        setupMethodBody(superSignatures, method, aClass, true);
+        TEMPLATE_METHOD_BODIES.put(key, method);
+      }
+      return method;
+    }
+    catch (IncorrectOperationException e) {
+      LOG.error(e);
+      return null;
+    }
+  }
+
   static void checkMethodBody(final PsiMethod method,
-                                      final InspectionManager manager,
-                                      final Collection<ProblemDescriptor> problemDescriptors) {
+                              final InspectionManager manager,
+                              final Collection<ProblemDescriptor> problemDescriptors) {
     PsiType returnType = method.getReturnType();
     if (method.isConstructor() || returnType == null) return;
     PsiCodeBlock body = method.getBody();
@@ -31,36 +61,52 @@ public class MethodBodyChecker {
     PsiClass aClass = method.getContainingClass();
     if (aClass == null) return;
     List<HierarchicalMethodSignature> superSignatures = method.getHierarchicalMethodSignature().getSuperSignatures();
-    try {
-      PsiMethod templateMethod = method.getManager().getElementFactory().createMethod("x", returnType);
-      setupMethodBody(superSignatures, templateMethod, aClass, true);
-      if (!PsiEquivalenceUtil.areElementsEquivalent(templateMethod.getBody(), body)) return;
+    final PsiMethod superMethod = superSignatures.size() ==0 ? null : superSignatures.get(0).getMethod();
+    final PsiMethod templateMethod = getTemplateMethod(returnType, superSignatures, aClass);
+    if (PsiEquivalenceUtil.areElementsEquivalent(body, templateMethod.getBody(), new Comparator<PsiElement>(){
+      public int compare(final PsiElement element1, final PsiElement element2) {
+        // templates may be different on super method name                              
+        if (element1 == superMethod && (element2 == templateMethod || element2 == null)) return 0;
+        return 1;
+      }
+    })) {
       Pair<? extends PsiElement, ? extends PsiElement> range = DefaultFileTemplateUsageInspection.getInteriorRange(body);
       final String description = InspectionsBundle.message("default.file.template.description");
-      ProblemDescriptor problem = manager.createProblemDescriptor(range.first, range.second,
-                                                                  description, ProblemHighlightType.GENERIC_ERROR_OR_WARNING, createMethodBodyQuickFix(method));
+      ProblemDescriptor problem = manager.createProblemDescriptor(range.first, range.second, description,
+                                                                  ProblemHighlightType.GENERIC_ERROR_OR_WARNING,
+                                                                  createMethodBodyQuickFix(method));
       problemDescriptors.add(problem);
-    }
-    catch (IncorrectOperationException e) {
-      LOG.error(e);
     }
   }
 
-  private static FileTemplate setupMethodBody(final List<HierarchicalMethodSignature> superSignatures,
-                                      final PsiMethod templateMethod,
-                                      final PsiClass aClass, final boolean useDefaultTemplate) throws IncorrectOperationException {
+  private static FileTemplate getMethodFileTemplate(final List<HierarchicalMethodSignature> superSignatures,
+                                                    final boolean useDefaultTemplate) throws IncorrectOperationException {
     FileTemplateManager templateManager = FileTemplateManager.getInstance();
     FileTemplate template;
     if (superSignatures.size() == 0) {
       String name = FileTemplateManager.TEMPLATE_FROM_USAGE_METHOD_BODY;
       template = useDefaultTemplate ? templateManager.getDefaultTemplate(name) : templateManager.getCodeTemplate(name);
-      CreateFromUsageUtils.setupMethodBody(templateMethod, aClass, template);
     }
     else {
       PsiMethod superMethod = superSignatures.get(0).getMethod();
       String name = superMethod.hasModifierProperty(PsiModifier.ABSTRACT) ?
                     FileTemplateManager.TEMPLATE_IMPLEMENTED_METHOD_BODY : FileTemplateManager.TEMPLATE_OVERRIDDEN_METHOD_BODY;
       template = useDefaultTemplate ? templateManager.getDefaultTemplate(name) : templateManager.getCodeTemplate(name);
+    }
+    return template;
+  }
+
+  private static final String NEW_METHOD_BODY_TEMPLATE_NAME = FileTemplateManager.getInstance().getDefaultTemplate(FileTemplateManager.TEMPLATE_FROM_USAGE_METHOD_BODY).getName();
+  private static FileTemplate setupMethodBody(final List<HierarchicalMethodSignature> superSignatures,
+                                              final PsiMethod templateMethod,
+                                              final PsiClass aClass,
+                                              final boolean useDefaultTemplate) throws IncorrectOperationException {
+    FileTemplate template = getMethodFileTemplate(superSignatures, useDefaultTemplate);
+    if (NEW_METHOD_BODY_TEMPLATE_NAME.equals(template.getName())) {
+      CreateFromUsageUtils.setupMethodBody(templateMethod, aClass, template);
+    }
+    else {
+      PsiMethod superMethod = superSignatures.get(0).getMethod();
       OverrideImplementUtil.setupMethodBody(templateMethod, superMethod, aClass,template);
     }
     return template;
--- testData/inspection/defaultFileTemplateUsage/defaultFile/src/x/X.java
+++ testData/inspection/defaultFileTemplateUsage/defaultFile/src/x/X.java
@@ -1,9 +1,5 @@
 package x;
-
-
 import java.io.File;
-
-
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,14 +9,18 @@ import java.io.File;
  * To change this template use File | Settings | File Templates.
  */
 public class X implements Runnable{
-    File f; //kkj lkkl jjkuufdffffjkkjjh kjh kjhj kkjh kjh
+    File f; //kkj lkkl jjkuufdffffjkkjjh kjh kjhj kkjh kjh  i k kj kj klj lkj lkj lkjl kj klkl kl
     {
         try {
             f=null;
         } catch (Exception e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
-        g();
+        try {
+            g();
+        } catch (Exception ex) {
+            ex.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
     }
 
     private void g() {
@@ -29,6 +29,10 @@ public class X implements Runnable{
 
     public int hashCode() {
         return super.hashCode();    //To change body of overridden methods use File | Settings | File Templates.
+    }
+        
+    public String toString() {
+        return super.toString();    //To change body of overridden methods use File | Settings | File Templates.
     }
 
     public void run() {
