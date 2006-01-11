7a775a7a192fb7e4b1ebbe017a1a5aacc4684a0f
Bas Leijdekkers <basleijdekkers@gmail.com>
2006-01-11 13:24:48 +0300

    fix for IDEADEV-4021

---
+++ plugins/InspectionGadgets/src/com/siyeh/ig/performance/SizeReplaceableByIsEmptyInspection.java
@@ -0,0 +1,198 @@
+/**
+ * (c) 2004 Carp Technologies BV
+ * Hengelosestraat 705, 7521PA Enschede
+ * Created: Jan 10, 2006, 9:39:41 PM
+ */
+package com.siyeh.ig.performance;
+
+import com.intellij.codeInsight.daemon.GroupNames;
+import com.intellij.psi.*;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.openapi.project.Project;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.util.IncorrectOperationException;
+import com.siyeh.HardcodedMethodConstants;
+import com.siyeh.InspectionGadgetsBundle;
+import com.siyeh.ig.BaseInspectionVisitor;
+import com.siyeh.ig.ExpressionInspection;
+import com.siyeh.ig.InspectionGadgetsFix;
+import com.siyeh.ig.psiutils.ComparisonUtils;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * @author <A href="bas@carp-technologies.nl">Bas Leijdekkers</a>
+ */
+public class SizeReplaceableByIsEmptyInspection extends ExpressionInspection {
+
+    String isEmptyCall = "";
+
+    public String getDisplayName() {
+        return InspectionGadgetsBundle.message(
+                "size.replaceable.by.isempty.display.name");
+    }
+
+    public String getGroupDisplayName() {
+        return GroupNames.PERFORMANCE_GROUP_NAME;
+    }
+
+    @Nullable
+    protected String buildErrorString(PsiElement location) {
+        return InspectionGadgetsBundle.message(
+                "size.replaceable.by.isempty.problem.descriptor", isEmptyCall);
+    }
+
+    @Nullable
+    protected InspectionGadgetsFix buildFix(PsiElement location) {
+        return new SizeReplaceableByIsEmptyFix();
+    }
+
+    private static class SizeReplaceableByIsEmptyFix
+            extends InspectionGadgetsFix {
+
+        public String getName() {
+            return InspectionGadgetsBundle.message(
+                    "size.replaceable.by.isempty.quickfix");
+        }
+
+        protected void doFix(Project project, ProblemDescriptor descriptor)
+                throws IncorrectOperationException {
+            final PsiBinaryExpression binaryExpression =
+                    (PsiBinaryExpression)descriptor.getPsiElement();
+            PsiExpression operand = binaryExpression.getLOperand();
+            String newExpression;
+            if (!(operand instanceof PsiMethodCallExpression)) {
+                operand = binaryExpression.getROperand();
+            }
+            if (!(operand instanceof PsiMethodCallExpression)) {
+                return;
+            }
+            final PsiMethodCallExpression methodCallExpression =
+                    (PsiMethodCallExpression)operand;
+            final PsiReferenceExpression methodExpression =
+                    methodCallExpression.getMethodExpression();
+            final PsiExpression qualifierExpression =
+                    methodExpression.getQualifierExpression();
+            if (qualifierExpression ==  null) {
+                return;
+            }
+            newExpression = qualifierExpression.getText();
+            final PsiJavaToken sign = binaryExpression.getOperationSign();
+            final IElementType tokenType = sign.getTokenType();
+            if (!JavaTokenType.EQEQ.equals(tokenType)) {
+                newExpression = '!' + newExpression;
+            }
+            newExpression += ".isEmpty()";
+            replaceExpression(binaryExpression, newExpression);
+        }
+    }
+
+    public BaseInspectionVisitor buildVisitor() {
+        return new SizeReplaceableByIsEmptyVisitor();
+    }
+
+    private class SizeReplaceableByIsEmptyVisitor
+            extends BaseInspectionVisitor {
+
+        public void visitBinaryExpression(PsiBinaryExpression expression) {
+            super.visitBinaryExpression(expression);
+            final PsiExpression rhs = expression.getROperand();
+            if (rhs == null) {
+                return;
+            }
+            if (!ComparisonUtils.isComparison(expression)) {
+                return;
+            }
+            final PsiExpression lhs = expression.getLOperand();
+            if (lhs instanceof PsiMethodCallExpression) {
+                final PsiJavaToken sign = expression.getOperationSign();
+                if (canBeReplacedByIsEmpty(lhs, sign, rhs, false)) {
+                    registerError(expression);
+                }
+            } else if (rhs instanceof PsiMethodCallExpression) {
+                final PsiJavaToken sign = expression.getOperationSign();
+                if (canBeReplacedByIsEmpty(rhs, sign, lhs, true)) {
+                    registerError(expression);
+                }
+            }
+        }
+
+        private boolean canBeReplacedByIsEmpty(
+                PsiExpression lhs, PsiJavaToken sign, PsiExpression rhs,
+                boolean flipped) {
+            final PsiMethodCallExpression callExpression =
+                    (PsiMethodCallExpression)lhs;
+            if (!isSizeCall(callExpression)) {
+                return false;
+            }
+            final PsiManager manager = rhs.getManager();
+            final PsiConstantEvaluationHelper constantEvaluationHelper =
+                    manager.getConstantEvaluationHelper();
+            final Object object =
+                    constantEvaluationHelper.computeConstantExpression(rhs);
+            if (!(object instanceof Integer)) {
+                return false;
+            }
+            final Integer integer = (Integer)object;
+            final int constant = integer.intValue();
+            if (constant != 0) {
+                return false;
+            }
+            final IElementType tokenType = sign.getTokenType();
+            if (JavaTokenType.EQEQ.equals(tokenType)) {
+                return true;
+            }
+            isEmptyCall = '!' + isEmptyCall;
+            if (JavaTokenType.NE.equals(tokenType)) {
+                return true;
+            } else if (flipped) {
+                if (JavaTokenType.LT.equals(tokenType)) {
+                    return true;
+                }
+            } else if (JavaTokenType.GT.equals(tokenType)) {
+                return true;
+            }
+            return false;
+        }
+
+        private boolean isSizeCall(
+                PsiMethodCallExpression callExpression) {
+            final PsiReferenceExpression methodExpression =
+                    callExpression.getMethodExpression();
+            final String referenceName = methodExpression.getReferenceName();
+            if (!HardcodedMethodConstants.SIZE.equals(referenceName)) {
+                return false;
+            }
+            final PsiExpressionList argumentList =
+                    callExpression.getArgumentList();
+            final PsiExpression[] expressions = argumentList.getExpressions();
+            if (expressions.length != 0) {
+                return false;
+            }
+            final PsiExpression qualifierExpression =
+                    methodExpression.getQualifierExpression();
+            if (qualifierExpression == null) {
+                return false;
+            }
+            isEmptyCall = qualifierExpression.getText() + ".isEmpty()";
+            final PsiType type = qualifierExpression.getType();
+            if (!(type instanceof PsiClassType)) {
+                return false;
+            }
+            final PsiClassType classType = (PsiClassType)type;
+            final PsiClass aClass = classType.resolve();
+            if (aClass == null) {
+                return false;
+            }
+            final PsiMethod[] methods =
+                    aClass.findMethodsByName("isEmpty", true);
+            for (PsiMethod method : methods) {
+                final PsiParameterList parameterList =
+                        method.getParameterList();
+                if (parameterList.getParameters().length == 0) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
