2ba3fd0c181a63ef248937e5c6d38858a687d351
Vyacheslav Lukianov <lvo@jetbrains.com>
2006-01-13 16:47:01 +0300

    marking file as dirty on allocating vector as well

--- util/src/com/intellij/util/io/PersistentStringEnumerator.java
+++ util/src/com/intellij/util/io/PersistentStringEnumerator.java
@@ -126,6 +126,7 @@ public class PersistentStringEnumerator {
   }
 
   private int allocVector() throws IOException {
+    markDirty(true);
     final int pos = (int)myStorage.length();
     myStorage.seek(pos);
     myStorage.put(EMPTY_VECTOR, 0, EMPTY_VECTOR.length);
