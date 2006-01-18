0a92393ddb6d3ac7fcb85c8c1c8d8abfe799a055
Maxim Mossienko <Maxim.Mossienko@jetbrains.com>
2006-01-18 19:24:56 +0300

    Completion trouble (IDEADEV-4065)

--- codeInsight/impl/com/intellij/codeInsight/completion/XmlCompletionData.java
+++ codeInsight/impl/com/intellij/codeInsight/completion/XmlCompletionData.java
@@ -318,6 +318,7 @@ public class XmlCompletionData extends CompletionData {
         template.addTextSegment("/>");
       } else if (completionChar == ' ') {
         template.addTextSegment(" ");
+        template.addEndVariable();
       }
 
       templateManager.startTemplate(editor, template);
