128126474756e6c42322573d9088d2430f27203b
Dmitry Jemerov <yole@intellij.com>
2006-01-18 22:15:56 +0300

    update javadocs for @NonNls and @PropertyKey

--- Annotations/src/org/jetbrains/annotations/PropertyKey.java
+++ Annotations/src/org/jetbrains/annotations/PropertyKey.java
@@ -18,11 +18,23 @@ package org.jetbrains.annotations;
 import java.lang.annotation.*;
 
 /**
+ * Specifies that a method parameter accepts arguments which must be valid property
+ * keys in a specific resource bundle. When a string literal which is not a property
+ * key in the specified bundle is passed as a parameter, IntelliJ IDEA highlights
+ * it as an error. The annotation is also used to provide completion in string literals
+ * passed as parameters.
+ *
  * @author max
  */
 @Documented
 @Retention(RetentionPolicy.CLASS)
 @Target({ElementType.PARAMETER})
 public @interface PropertyKey {
+  /**
+   * The full-qualified name of the resource bundle in which the property keys must
+   * be present. Consists of a full-qualified name of the package corresponding to the
+   * directory where the resource bundle is located and the base name of the resource
+   * bundle (with no locale specifier or extension), separated with a dot.
+   */
   String resourceBundle();
 }
