76249c383a778aa5745f60d4e0238679b7ad0723
Dmitry Jemerov <yole@intellij.com>
2006-01-20 16:04:09 +0300

    use EditorTextField for entering class name of component to add

--- ui-designer/impl/com/intellij/uiDesigner/palette/ComponentItemDialog.java
+++ ui-designer/impl/com/intellij/uiDesigner/palette/ComponentItemDialog.java
@@ -3,19 +3,20 @@ package com.intellij.uiDesigner.palette;
 import com.intellij.ide.util.TreeClassChooser;
 import com.intellij.ide.util.TreeClassChooserFactory;
 import com.intellij.ide.util.TreeFileChooser;
-import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.fileTypes.StdFileTypes;
+import com.intellij.openapi.module.impl.ModuleUtil;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.ComponentWithBrowseButton;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
-import com.intellij.openapi.module.impl.ModuleUtil;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiManager;
-import com.intellij.psi.PsiFile;
+import com.intellij.psi.*;
 import com.intellij.psi.search.GlobalSearchScope;
-import com.intellij.uiDesigner.UIDesignerBundle;
-import com.intellij.uiDesigner.ImageFileFilter;
+import com.intellij.ui.EditorTextField;
 import com.intellij.uiDesigner.GuiEditorUtil;
+import com.intellij.uiDesigner.ImageFileFilter;
+import com.intellij.uiDesigner.UIDesignerBundle;
 import com.intellij.uiDesigner.core.GridConstraints;
 import org.jetbrains.annotations.NotNull;
 
@@ -28,10 +29,9 @@ import java.awt.event.ActionListener;
  * @author Vladimir Kondratyev
  */
 public final class ComponentItemDialog extends DialogWrapper{
-  private static final Logger LOG = Logger.getInstance("#com.intellij.uiDesigner.palette.ComponentItemDialog");
-
   private JPanel myPanel;
-  private TextFieldWithBrowseButton myTfClassName;
+  private ComponentWithBrowseButton<EditorTextField> myTfClassName;
+  private Project myProject;
   private final ComponentItem myItemToBeEdited;
   private JLabel myLblClass;
   private JLabel myLblIcon;
@@ -42,34 +42,26 @@ public final class ComponentItemDialog extends DialogWrapper{
   private JCheckBox myChkVerCanShrink;
   private JCheckBox myChkVerCanGrow;
   private JCheckBox myChkVerWantGrow;
+  private JPanel myClassNamePlaceholder;
+  private EditorTextField myEditorTextField;
+  private Document myDocument;
 
   /**
    * @param itemToBeEdited item to be edited. If user closes dialog by "OK" button then
    */
   public ComponentItemDialog(final Project project, final Component parent, @NotNull ComponentItem itemToBeEdited) {
     super(parent, false);
+    myProject = project;
 
     myItemToBeEdited = itemToBeEdited;
 
-    myTfClassName.setText(myItemToBeEdited.getClassName());
+    myEditorTextField = new EditorTextField("", project, StdFileTypes.JAVA);
+    myEditorTextField.setFontInheritedFromLAF(true);
+    myTfClassName = new ComponentWithBrowseButton<EditorTextField>(myEditorTextField, new MyChooseClassActionListener(project));
+    setEditorText(myItemToBeEdited.getClassName());
     myTfClassName.getButton().setEnabled(!project.isDefault()); // chooser should not work in default project
-    myTfClassName.addActionListener(
-      new ActionListener() {
-        public void actionPerformed(final ActionEvent e) {
-          final TreeClassChooserFactory factory = TreeClassChooserFactory.getInstance(project);
-          final TreeClassChooser chooser = factory.createInheritanceClassChooser(
-            UIDesignerBundle.message("title.choose.component.class"),
-            GlobalSearchScope.allScope(project),
-            PsiManager.getInstance(project).findClass(JComponent.class.getName(), GlobalSearchScope.allScope(project)),
-            true, false, null);
-          chooser.showDialog();
-          final PsiClass result = chooser.getSelectedClass();
-          if (result != null) {
-            myTfClassName.setText(result.getQualifiedName());
-          }
-        }
-      }
-    );
+    myClassNamePlaceholder.setLayout(new BorderLayout());
+    myClassNamePlaceholder.add(myTfClassName, BorderLayout.CENTER);
 
     myTfIconPath.setText(myItemToBeEdited.getIconPath());
     myTfIconPath.addActionListener(new ActionListener() {
@@ -79,7 +71,7 @@ public final class ComponentItemDialog extends DialogWrapper{
         if (myItemToBeEdited.getIconPath() != null) {
           VirtualFile iconVFile = ModuleUtil.findResourceFileInProject(project, myItemToBeEdited.getIconPath());
           if (iconVFile != null) {
-            iconFile = PsiManager.getInstance(project).findFile(iconVFile); 
+            iconFile = PsiManager.getInstance(project).findFile(iconVFile);
           }
         }
         TreeFileChooser fileChooser = factory.createFileChooser(UIDesignerBundle.message("title.choose.icon.file"), iconFile,
@@ -116,11 +108,19 @@ public final class ComponentItemDialog extends DialogWrapper{
     init();
   }
 
+  private void setEditorText(final String className) {
+    final PsiManager manager = PsiManager.getInstance(myProject);
+    final PsiElementFactory factory = manager.getElementFactory();
+    PsiPackage defaultPackage = manager.findPackage("");
+    final PsiCodeFragment fragment = factory.createReferenceCodeFragment(className, defaultPackage, true, true);
+    myDocument = PsiDocumentManager.getInstance(manager.getProject()).getDocument(fragment);
+    myEditorTextField.setDocument(myDocument);
+  }
 
 
   protected void doOKAction() {
     // TODO[vova] implement validation
-    myItemToBeEdited.setClassName(myTfClassName.getText().trim());
+    myItemToBeEdited.setClassName(myDocument.getText().trim());
     myItemToBeEdited.setIconPath(myTfIconPath.getText().trim());
 
     // Horizontal size policy
@@ -151,10 +151,32 @@ public final class ComponentItemDialog extends DialogWrapper{
   }
 
   public JComponent getPreferredFocusedComponent() {
-    return myTfClassName.getTextField();
+    return myTfClassName.getChildComponent();
   }
 
   protected JComponent createCenterPanel() {
     return myPanel;
+  }
+
+  private class MyChooseClassActionListener implements ActionListener {
+    private final Project myProject;
+
+    public MyChooseClassActionListener(final Project project) {
+      myProject = project;
+    }
+
+    public void actionPerformed(final ActionEvent e) {
+      final TreeClassChooserFactory factory = TreeClassChooserFactory.getInstance(myProject);
+      final TreeClassChooser chooser = factory.createInheritanceClassChooser(
+        UIDesignerBundle.message("title.choose.component.class"),
+        GlobalSearchScope.allScope(myProject),
+        PsiManager.getInstance(myProject).findClass(JComponent.class.getName(), GlobalSearchScope.allScope(myProject)),
+        true, false, null);
+      chooser.showDialog();
+      final PsiClass result = chooser.getSelectedClass();
+      if (result != null) {
+        setEditorText(result.getQualifiedName());
+      }
+    }
   }
 }
