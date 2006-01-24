04a298fda7c4f4883d3f03358fd27cda31529806
Olesya Smirnova <Olesya.Smirnova@jetbrains.com>
2006-01-24 17:41:36 +0300

    IDEADEV-2873 fixed

--- source/com/intellij/psi/formatter/xml/AbstractXmlBlock.java
+++ source/com/intellij/psi/formatter/xml/AbstractXmlBlock.java
@@ -410,7 +410,7 @@ public abstract class AbstractXmlBlock extends AbstractBlock {
                                     final FormattingDocumentModel documentModel) {
     final PsiFile file = element.getContainingFile();
     final Language baseLanguage = ((JspFile)file).getViewProvider().getTemplateDataLanguage();
-    if (baseLanguage == StdLanguages.HTML) {
+    if (baseLanguage == StdLanguages.HTML || baseLanguage == StdLanguages.XHTML) {
       final PsiElement[] psiRoots = file.getPsiRoots();
       LOG.assertTrue(psiRoots.length == 4);
       final ASTNode rootNode = SourceTreeToPsiMap.psiElementToTree(psiRoots[0]);
