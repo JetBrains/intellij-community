32d8eafb75f23aadd1d9ad45d3a12c71f73fbc6c
Dmitry Jemerov <yole@intellij.com>
2006-02-13 12:55:03 +0300

    "Add Tab" in context menu (IDEADEV-4669)

---
+++ ui-designer/impl/com/intellij/uiDesigner/actions/AddTabAction.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2000-2006 JetBrains s.r.o. All Rights Reserved.
+ */
+
+package com.intellij.uiDesigner.actions;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.uiDesigner.designSurface.GuiEditor;
+import com.intellij.uiDesigner.designSurface.InsertComponentProcessor;
+import com.intellij.uiDesigner.radComponents.RadComponent;
+import com.intellij.uiDesigner.radComponents.RadTabbedPane;
+import com.intellij.uiDesigner.palette.Palette;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author yole
+ */
+public class AddTabAction extends AbstractGuiEditorAction {
+  public AddTabAction() {
+    super(true);
+  }
+
+  protected void actionPerformed(final GuiEditor editor, final List<RadComponent> selection, final AnActionEvent e) {
+    RadTabbedPane tabbedPane = (RadTabbedPane) selection.get(0);
+    Palette palette = Palette.getInstance(editor.getProject());
+    final RadComponent radComponent = InsertComponentProcessor.createInsertedComponent(editor, palette.getPanelItem());
+    tabbedPane.drop(null, new RadComponent[] { radComponent }, null, null);
+  }
+
+  @Override protected void update(@NotNull GuiEditor editor, final ArrayList<RadComponent> selection, final AnActionEvent e) {
+    e.getPresentation().setVisible(selection.size() == 1 && selection.get(0) instanceof RadTabbedPane);
+  }
+}
