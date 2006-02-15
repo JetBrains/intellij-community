214f1836d4a48815409ec48b0bbfd306922c6eef
Dmitry Jemerov <yole@intellij.com>
2006-02-15 15:07:24 +0300

    error -> warn

--- plugins-management/src/com/intellij/ide/plugins/PluginManager.java
+++ plugins-management/src/com/intellij/ide/plugins/PluginManager.java
@@ -659,23 +659,23 @@ public class PluginManager {
           }
         }
         else {
-          getLogger().error("Unknown classloader: " + loader.getClass().getName());
+          getLogger().warn("Unknown classloader: " + loader.getClass().getName());
         }
       }
       catch (ClassCastException e) {
-        getLogger().error("Unknown classloader [CCE]: " + e.getMessage());
+        getLogger().warn("Unknown classloader [CCE]: " + e.getMessage());
       }
       catch (ClassNotFoundException e) {
-        getLogger().error("Unknown classloader [CNFE]: " + loader.getClass().getName());
+        getLogger().warn("Unknown classloader [CNFE]: " + loader.getClass().getName());
       }
       catch (NoSuchMethodException e) {
-        getLogger().error("Unknown classloader [NSME]: " + e.getMessage());
+        getLogger().warn("Unknown classloader [NSME]: " + e.getMessage());
       }
       catch (IllegalAccessException e) {
-        getLogger().error("Unknown classloader [IAE]: " + e.getMessage());
+        getLogger().warn("Unknown classloader [IAE]: " + e.getMessage());
       }
       catch (InvocationTargetException e) {
-        getLogger().error("Unknown classloader [ITE]: " + e.getMessage());
+        getLogger().warn("Unknown classloader [ITE]: " + e.getMessage());
       }
     }
   }
