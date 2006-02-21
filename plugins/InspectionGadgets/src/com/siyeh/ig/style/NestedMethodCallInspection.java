21a46a46462520a9626bc29f241895715b49b6cb
Bas Leijdekkers <basleijdekkers@gmail.com>
2006-02-21 19:42:12 +0300

    improved quick fix

--- plugins/InspectionGadgets/src/com/siyeh/ig/style/NestedMethodCallInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/style/NestedMethodCallInspection.java
@@ -18,10 +18,12 @@ package com.siyeh.ig.style;
 import com.intellij.codeInsight.daemon.GroupNames;
 import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.psi.*;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.refactoring.RefactoringActionHandler;
 import com.intellij.refactoring.RefactoringActionHandlerFactory;
+import com.intellij.ide.DataManager;
 import com.siyeh.ig.BaseInspectionVisitor;
 import com.siyeh.ig.ExpressionInspection;
 import com.siyeh.ig.InspectionGadgetsFix;
@@ -33,94 +35,103 @@ import javax.swing.*;
 
 public class NestedMethodCallInspection extends ExpressionInspection {
 
-  /**
-   * @noinspection PublicField
-   */
-  public boolean m_ignoreFieldInitializations = true;
-  private final NestedMethodCallFix fix = new NestedMethodCallFix();
+    /**
+     * @noinspection PublicField
+     */
+    public boolean m_ignoreFieldInitializations = true;
 
-  public String getGroupDisplayName() {
-    return GroupNames.STYLE_GROUP_NAME;
-  }
-
-  public JComponent createOptionsPanel() {
-    return new SingleCheckboxOptionsPanel(InspectionGadgetsBundle.message("nested.method.call.ignore.option"),
-                                          this, "m_ignoreFieldInitializations");
-  }
-
-  public BaseInspectionVisitor buildVisitor() {
-    return new NestedMethodCallVisitor();
-  }
-
-  protected InspectionGadgetsFix buildFix(PsiElement location) {
-    return fix;
-  }
-
-  protected boolean buildQuickFixesOnlyForOnTheFlyErrors() {
-    return true;
-  }
-
-  private static class NestedMethodCallFix extends InspectionGadgetsFix {
-    public String getName() {
-      return InspectionGadgetsBundle.message("introduce.variable.quickfix");
+    public String getGroupDisplayName() {
+        return GroupNames.STYLE_GROUP_NAME;
     }
 
-    public void doFix(Project project, ProblemDescriptor descriptor) {
-      final RefactoringActionHandlerFactory factory = RefactoringActionHandlerFactory.getInstance();
-      final RefactoringActionHandler introduceHandler =
-        factory.createIntroduceVariableHandler();
-      final PsiElement methodNameElement = descriptor.getPsiElement();
-      final PsiElement methodExpression = methodNameElement.getParent();
-      assert methodExpression != null;
-      final PsiElement methodCallExpression = methodExpression.getParent();
-      introduceHandler.invoke(project, new PsiElement[]{methodCallExpression}, null);
+    public JComponent createOptionsPanel() {
+        return new SingleCheckboxOptionsPanel(
+                InspectionGadgetsBundle.message(
+                        "nested.method.call.ignore.option"),
+                this, "m_ignoreFieldInitializations");
     }
-  }
 
-  private class NestedMethodCallVisitor extends BaseInspectionVisitor {
+    public BaseInspectionVisitor buildVisitor() {
+        return new NestedMethodCallVisitor();
+    }
 
-    public void visitMethodCallExpression(@NotNull PsiMethodCallExpression expression) {
-      super.visitMethodCallExpression(expression);
-      PsiExpression outerExpression = expression;
-      while (outerExpression != null && outerExpression.getParent() instanceof PsiExpression) {
-        outerExpression = (PsiExpression)outerExpression.getParent();
-      }
-      if (outerExpression == null) {
-        return;
-      }
-      final PsiElement parent = outerExpression.getParent();
-      if (!(parent instanceof PsiExpressionList)) {
-        return;
-      }
-      final PsiElement grandParent = parent.getParent();
-      if (!(grandParent instanceof PsiCallExpression)) {
-        return;
-      }
-      if (grandParent instanceof PsiMethodCallExpression) {
+    protected InspectionGadgetsFix buildFix(PsiElement location) {
+        return new NestedMethodCallFix();
+    }
 
-        final PsiMethodCallExpression surroundingCall =
-          (PsiMethodCallExpression)grandParent;
-        final PsiReferenceExpression methodExpression =
-          surroundingCall.getMethodExpression();
-        final String callName = methodExpression.getReferenceName();
-        if (PsiKeyword.THIS.equals(callName) || PsiKeyword.SUPER.equals(callName)) {
-          return;     //ignore nested method calls at the start of a constructor,
-          //where they can't be extracted
+    protected boolean buildQuickFixesOnlyForOnTheFlyErrors() {
+        return true;
+    }
+
+    private static class NestedMethodCallFix extends InspectionGadgetsFix {
+
+        public String getName() {
+            return InspectionGadgetsBundle.message(
+                    "introduce.variable.quickfix");
         }
-      }
-      final PsiReferenceExpression reference =
-        expression.getMethodExpression();
-      if (reference == null) {
-        return;
-      }
-      if (m_ignoreFieldInitializations) {
-        final PsiElement field =
-          PsiTreeUtil.getParentOfType(expression, PsiField.class);
-        if (field != null) {
-          return;
+
+        public void doFix(Project project, ProblemDescriptor descriptor) {
+            final RefactoringActionHandlerFactory factory =
+                    RefactoringActionHandlerFactory.getInstance();
+            final RefactoringActionHandler introduceHandler =
+                    factory.createIntroduceVariableHandler();
+            final PsiElement methodNameElement = descriptor.getPsiElement();
+            final PsiElement methodExpression = methodNameElement.getParent();
+            if (methodExpression == null) {
+                return;
+            }
+            final PsiElement methodCallExpression =
+                    methodExpression.getParent();
+            final DataManager dataManager = DataManager.getInstance();
+            final DataContext dataContext = dataManager.getDataContext();
+            introduceHandler.invoke(project,
+                    new PsiElement[]{methodCallExpression}, dataContext);
         }
-      }
-      registerMethodCallError(expression);
     }
-  }
+
+    private class NestedMethodCallVisitor extends BaseInspectionVisitor {
+
+        public void visitMethodCallExpression(
+                @NotNull PsiMethodCallExpression expression) {
+            super.visitMethodCallExpression(expression);
+            PsiExpression outerExpression = expression;
+            while (outerExpression != null &&
+                    outerExpression.getParent() instanceof PsiExpression) {
+                outerExpression = (PsiExpression)outerExpression.getParent();
+            }
+            if (outerExpression == null) {
+                return;
+            }
+            final PsiElement parent = outerExpression.getParent();
+            if (!(parent instanceof PsiExpressionList)) {
+                return;
+            }
+            final PsiElement grandParent = parent.getParent();
+            if (!(grandParent instanceof PsiCallExpression)) {
+                return;
+            }
+            if (grandParent instanceof PsiMethodCallExpression) {
+
+                final PsiMethodCallExpression surroundingCall =
+                        (PsiMethodCallExpression)grandParent;
+                final PsiReferenceExpression methodExpression =
+                        surroundingCall.getMethodExpression();
+                final String callName = methodExpression.getReferenceName();
+                if (PsiKeyword.THIS.equals(callName) ||
+                        PsiKeyword.SUPER.equals(callName)) {
+                    //ignore nested method calls at the start of a constructor,
+                    //where they can't be extracted
+                    return;
+                }
+            }
+            if (m_ignoreFieldInitializations) {
+                final PsiElement field =
+                        PsiTreeUtil.getParentOfType(expression, PsiField.class);
+                if (field != null) {
+                    return;
+                }
+            }
+            registerMethodCallError(expression);
+        }
+    }
 }
