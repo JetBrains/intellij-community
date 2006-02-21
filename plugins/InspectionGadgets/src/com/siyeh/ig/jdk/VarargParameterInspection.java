5a387dd5d3a584f128af9b6146d04e1577392278
Bas Leijdekkers <basleijdekkers@gmail.com>
2006-02-22 02:29:09 +0300

    added quick fix for vararg method

--- plugins/InspectionGadgets/src/com/siyeh/ig/jdk/VarargParameterInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/jdk/VarargParameterInspection.java
@@ -17,6 +17,7 @@ package com.siyeh.ig.jdk;
 
 import com.intellij.codeInsight.daemon.GroupNames;
 import com.intellij.psi.*;
+import com.intellij.psi.codeStyle.CodeStyleManager;
 import com.intellij.psi.search.searches.ReferencesSearch;
 import com.intellij.openapi.project.Project;
 import com.intellij.codeInspection.ProblemDescriptor;
@@ -29,6 +30,8 @@ import com.siyeh.ig.InspectionGadgetsFix;
 import com.siyeh.InspectionGadgetsBundle;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
 
 public class VarargParameterInspection extends MethodInspection {
 
@@ -45,17 +48,16 @@ public class VarargParameterInspection extends MethodInspection {
         return GroupNames.JDK_GROUP_NAME;
     }
 
-
     @Nullable
     protected InspectionGadgetsFix buildFix(PsiElement location) {
-        //return new VarargParameterFix();
-        return null;
+        return new VarargParameterFix();
     }
 
     private static class VarargParameterFix extends InspectionGadgetsFix {
 
         public String getName() {
-            return "Change variable argument parameter to array parameter";
+            return InspectionGadgetsBundle.message(
+                    "variable.argument.method.quick.fix");
         }
 
         protected void doFix(Project project, ProblemDescriptor descriptor)
@@ -64,40 +66,96 @@ public class VarargParameterInspection extends MethodInspection {
             final PsiMethod method = (PsiMethod)element.getParent();
             final PsiParameterList parameterList = method.getParameterList();
             final PsiParameter[] parameters = parameterList.getParameters();
-
-            final Query<PsiReference> query = ReferencesSearch.search(method);
-            query.forEach(new Processor<PsiReference>() {
-                public boolean process(PsiReference reference) {
-                    final PsiReferenceExpression referenceExpression =
-                            (PsiReferenceExpression)reference.getElement();
-                    final PsiMethodCallExpression methodCallExpression =
-                            (PsiMethodCallExpression)
-                                    referenceExpression.getParent();
-                    final PsiExpressionList argumentList =
-                            methodCallExpression.getArgumentList();
-                    final PsiExpression[] arguments =
-                            argumentList.getExpressions();
-                    for (PsiExpression argument : arguments) {
-                        System.out.println(argument);
-                    }
-                    return true;
-                }
-            });
-
             final PsiParameter lastParameter =
                     parameters[parameters.length - 1];
-            if (lastParameter.isVarArgs()) {
-                final PsiEllipsisType type =
-                        (PsiEllipsisType)lastParameter.getType();
-                final PsiType arrayType = type.toArrayType();
-                final PsiManager psiManager = lastParameter.getManager();
-                final PsiElementFactory factory =
-                        psiManager.getElementFactory();
-                final PsiTypeElement newTypeElement =
-                        factory.createTypeElement(arrayType);
-                final PsiTypeElement typeElement =
-                        lastParameter.getTypeElement();
-                typeElement.replace(newTypeElement);
+            if (!lastParameter.isVarArgs()) {
+                return;
+            }
+            final PsiEllipsisType type =
+                    (PsiEllipsisType)lastParameter.getType();
+            final Query<PsiReference> query = ReferencesSearch.search(method);
+            final PsiType componentType = type.getComponentType();
+            final String typeText = componentType.getCanonicalText();
+            //query.forEach(new VarargProcessor(typeText, parameters.length - 1));
+            final VarargProcessor processor =
+                    new VarargProcessor(
+                            typeText, parameters.length - 1);
+            final Collection<PsiReference> references = query.findAll();
+            for (PsiReference reference : references) {
+                processor.process(reference);
+            }
+            final PsiType arrayType = type.toArrayType();
+            final PsiManager psiManager = lastParameter.getManager();
+            final PsiElementFactory factory = psiManager.getElementFactory();
+            final PsiTypeElement newTypeElement =
+                    factory.createTypeElement(arrayType);
+            final PsiTypeElement typeElement =
+                    lastParameter.getTypeElement();
+            typeElement.replace(newTypeElement);
+        }
+
+        private static class VarargProcessor
+                implements Processor<PsiReference> {
+
+            private final String arrayTypeText;
+            private final int indexOfFirstVarargArgument;
+
+            public VarargProcessor(String arrayTypeText,
+                                   int indexOfFirstVarargArgument) {
+                this.arrayTypeText = arrayTypeText;
+                this.indexOfFirstVarargArgument = indexOfFirstVarargArgument;
+            }
+
+            public boolean process(PsiReference reference) {
+                final PsiReferenceExpression referenceExpression =
+                        (PsiReferenceExpression)reference.getElement();
+                final PsiMethodCallExpression methodCallExpression =
+                        (PsiMethodCallExpression)
+                                referenceExpression.getParent();
+                final PsiExpressionList argumentList =
+                        methodCallExpression.getArgumentList();
+                final PsiExpression[] arguments =
+                        argumentList.getExpressions();
+                StringBuilder builder = new StringBuilder("new ");
+                builder.append(arrayTypeText);
+                builder.append("[]{");
+                if (arguments.length > indexOfFirstVarargArgument) {
+                    final PsiExpression firstArgument =
+                            arguments[indexOfFirstVarargArgument];
+                    final String firstArgumentText =
+                            firstArgument.getText();
+                    builder.append(firstArgumentText);
+                    for (int i = indexOfFirstVarargArgument + 1;
+                         i < arguments.length; i++) {
+                        final PsiExpression argument = arguments[i];
+                        builder.append(',');
+                        builder.append(argument.getText());
+                    }
+                }
+                builder.append('}');
+                final PsiManager manager = referenceExpression.getManager();
+                final PsiElementFactory factory = manager.getElementFactory();
+                try {
+                    final PsiExpression arrayExpression =
+                            factory.createExpressionFromText(builder.toString(),
+                                    referenceExpression);
+                    if (arguments.length > indexOfFirstVarargArgument) {
+                        final PsiExpression firstArgument =
+                                arguments[indexOfFirstVarargArgument];
+                        argumentList.deleteChildRange(firstArgument,
+                                arguments[arguments.length - 1]);
+                        argumentList.add(arrayExpression);
+                    } else {
+                        argumentList.add(arrayExpression);
+                    }
+                    final CodeStyleManager codeStyleManager =
+                            manager.getCodeStyleManager();
+                    codeStyleManager.shortenClassReferences(argumentList);
+                    codeStyleManager.reformat(argumentList);
+                } catch (IncorrectOperationException e) {
+                    throw new RuntimeException(e);
+                }
+                return true;
             }
         }
     }
