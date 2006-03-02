2b4a36956f0e912335b8f9c885123ba990f58485
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2006-03-02 20:08:50 +0300

    correct colors

--- openapi/src/com/intellij/codeHighlighting/HighlightDisplayLevel.java
+++ openapi/src/com/intellij/codeHighlighting/HighlightDisplayLevel.java
@@ -16,7 +16,7 @@
 package com.intellij.codeHighlighting;
 
 import com.intellij.lang.annotation.HighlightSeverity;
-import com.intellij.ui.LightColors;
+import com.intellij.openapi.editor.colors.CodeInsightColors;
 import com.intellij.util.ImageLoader;
 import com.intellij.util.containers.HashMap;
 import com.intellij.util.ui.EmptyIcon;
@@ -29,9 +29,9 @@ public class HighlightDisplayLevel {
   private static final Icon EMPTY = new EmptyIcon(12, 12);
   private static Map<String, HighlightDisplayLevel> ourMap = new HashMap<String, HighlightDisplayLevel>();
 
-  public static final HighlightDisplayLevel ERROR = new HighlightDisplayLevel(HighlightSeverity.ERROR, createIconByMask(LightColors.RED));
-  public static final HighlightDisplayLevel WARNING = new HighlightDisplayLevel(HighlightSeverity.WARNING, createIconByMask(LightColors.YELLOW));
-  public static final HighlightDisplayLevel DO_NOT_SHOW = new HighlightDisplayLevel(HighlightSeverity.INFORMATION, createIconByMask(LightColors.GREEN));
+  public static final HighlightDisplayLevel ERROR = new HighlightDisplayLevel(HighlightSeverity.ERROR, createIconByMask(CodeInsightColors.ERRORS_ATTRIBUTES.getDefaultAttributes().getErrorStripeColor()));
+  public static final HighlightDisplayLevel WARNING = new HighlightDisplayLevel(HighlightSeverity.WARNING, createIconByMask(CodeInsightColors.WARNINGS_ATTRIBUTES.getDefaultAttributes().getErrorStripeColor()));
+  public static final HighlightDisplayLevel DO_NOT_SHOW = new HighlightDisplayLevel(HighlightSeverity.INFORMATION, createIconByMask(new Color(30, 160, 0)));
 
   private final Icon myIcon;
   private final HighlightSeverity mySeverity;
