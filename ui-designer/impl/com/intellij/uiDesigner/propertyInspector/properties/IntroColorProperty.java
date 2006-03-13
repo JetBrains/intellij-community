1154adbd633a9547fa88327106817556b0dc65b6
Dmitry Jemerov <yole@intellij.com>
2006-03-13 20:01:48 +0300

    SnapShooter: import color properties

--- ui-designer/impl/com/intellij/uiDesigner/propertyInspector/properties/IntroColorProperty.java
+++ ui-designer/impl/com/intellij/uiDesigner/propertyInspector/properties/IntroColorProperty.java
@@ -1,20 +1,22 @@
 package com.intellij.uiDesigner.propertyInspector.properties;
 
+import com.intellij.openapi.util.Comparing;
+import com.intellij.uiDesigner.UIFormXmlConstants;
+import com.intellij.uiDesigner.XmlWriter;
+import com.intellij.uiDesigner.lw.ColorDescriptor;
 import com.intellij.uiDesigner.propertyInspector.IntrospectedProperty;
-import com.intellij.uiDesigner.propertyInspector.PropertyRenderer;
 import com.intellij.uiDesigner.propertyInspector.PropertyEditor;
+import com.intellij.uiDesigner.propertyInspector.PropertyRenderer;
 import com.intellij.uiDesigner.propertyInspector.editors.ColorEditor;
 import com.intellij.uiDesigner.propertyInspector.renderers.ColorRenderer;
-import com.intellij.uiDesigner.XmlWriter;
 import com.intellij.uiDesigner.radComponents.RadComponent;
-import com.intellij.uiDesigner.UIFormXmlConstants;
-import com.intellij.uiDesigner.lw.ColorDescriptor;
+import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.annotations.NonNls;
 
+import javax.swing.*;
+import java.awt.Color;
 import java.lang.reflect.Method;
-import java.awt.*;
 
 /**
  * @author yole
@@ -70,9 +72,24 @@ public class IntroColorProperty extends IntrospectedProperty {
     }
   }
 
-
   @Override public void resetValue(RadComponent component) throws Exception {
     super.resetValue(component);
     component.getDelegee().putClientProperty(CLIENT_PROPERTY_KEY_PREFIX + getName(), null);
+  }
+
+  @Override
+  public void importSnapshotValue(final JComponent component, final RadComponent radComponent) {
+    try {
+      if (component.getParent() != null) {
+        Color componentColor = (Color) myReadMethod.invoke(component, EMPTY_OBJECT_ARRAY);
+        Color parentColor = (Color) myReadMethod.invoke(component.getParent(), EMPTY_OBJECT_ARRAY);
+        if (componentColor != null && !Comparing.equal(componentColor, parentColor)) {
+          setValue(radComponent, new ColorDescriptor(componentColor));
+        }
+      }
+    }
+    catch (Exception e) {
+      // ignore
+    }
   }
 }
