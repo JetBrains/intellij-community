522899bb4405557dfb765c3f7ac772f798323613
Maxim Shafirov <max@jetbrains.com>
2006-03-15 19:44:48 +0300

    More proper fix for unversioned files on Windows.

---
+++ util/src/com/intellij/util/text/CaseInsensitiveStringHashingStrategy.java
@@ -0,0 +1,17 @@
+package com.intellij.util.text;
+
+import com.intellij.openapi.util.text.StringUtil;
+import gnu.trove.TObjectHashingStrategy;
+
+/**
+ * @author max
+ */
+public class CaseInsensitiveStringHashingStrategy implements TObjectHashingStrategy<String> {
+  public int computeHashCode(final String s) {
+    return StringUtil.stringHashCodeInsensitive(s);
+  }
+
+  public boolean equals(final String s1, final String s2) {
+    return s1.equalsIgnoreCase(s2);
+  }
+}
---
+++ util/src/com/intellij/util/text/FilePathHashingStrategy.java
@@ -0,0 +1,38 @@
+package com.intellij.util.text;
+
+import com.intellij.openapi.util.SystemInfo;
+import gnu.trove.TObjectHashingStrategy;
+
+import java.io.File;
+
+/**
+ * @author max
+ */
+public class FilePathHashingStrategy implements TObjectHashingStrategy<String> {
+  public int computeHashCode(final String s) {
+    int h = 0;
+    final int len = s.length();
+    for( int i = 0; i < len; i++) {
+      char c = normalizeChar(s.charAt(i));
+      h = 31*h + c;
+    }
+    return h;
+  }
+
+  private static char normalizeChar(final char cc) {
+    char c = SystemInfo.isFileSystemCaseSensitive ? cc : Character.toLowerCase(cc);
+    return c == File.separatorChar ? '/' : c;
+  }
+
+  public boolean equals(final String s1, final String s2) {
+    if(s1 == s2) return true;
+    if(s1 == null || s2 == null) return false;
+    if (s1.length() != s2.length()) return false;
+    int len = s1.length();
+    for (int i = 0; i < len; i++) {
+      if (normalizeChar(s1.charAt(i)) != normalizeChar(s2.charAt(i))) return false;
+    }
+
+    return true;
+  }
+}
