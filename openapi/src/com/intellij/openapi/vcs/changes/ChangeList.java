e5de7995a2c1f6345194988efae54bde33327e77
Maxim Shafirov <max@jetbrains.com>
2006-03-20 12:58:40 +0300

    Fix for ConcurrentModificationException

--- openapi/src/com/intellij/openapi/vcs/changes/ChangeList.java
+++ openapi/src/com/intellij/openapi/vcs/changes/ChangeList.java
@@ -30,7 +30,7 @@ public class ChangeList implements Cloneable {
     myDescription = description;
   }
 
-  public Collection<Change> getChanges() {
+  public synchronized Collection<Change> getChanges() {
     if (myReadChangesCache == null) {
       myReadChangesCache = new HashSet<Change>(myChanges);
       if (myOutdatedChanges != null) {
@@ -56,17 +56,17 @@ public class ChangeList implements Cloneable {
     myIsDefault = isDefault;
   }
 
-  void addChange(Change change) {
+  synchronized void addChange(Change change) {
     myReadChangesCache = null;
     myChanges.add(change);
   }
 
-  void removeChange(Change change) {
+  synchronized void removeChange(Change change) {
     myReadChangesCache = null;
     myChanges.remove(change);
   }
 
-  void startProcessingChanges(final VcsDirtyScope scope) {
+  synchronized void startProcessingChanges(final VcsDirtyScope scope) {
     myChangesBeforeUpdate = new HashSet<Change>(myChanges);
     myOutdatedChanges = new ArrayList<Change>();
     for (Change oldBoy : myChangesBeforeUpdate) {
@@ -98,7 +98,7 @@ public class ChangeList implements Cloneable {
     return false;
   }
 
-  boolean doneProcessingChanges() {
+  synchronized boolean doneProcessingChanges() {
     boolean changesDetected = !Comparing.equal(myChanges, myChangesBeforeUpdate);
     myOutdatedChanges = null;
     myReadChangesCache = null;
