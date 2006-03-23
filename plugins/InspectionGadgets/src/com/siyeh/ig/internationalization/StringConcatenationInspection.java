59796b4426067f63aff59e9920b9eae432826406
Bas Leijdekkers <basleijdekkers@gmail.com>
2006-03-23 12:30:32 +0300

    IDEADEV-5124

--- plugins/InspectionGadgets/src/com/siyeh/ig/internationalization/StringConcatenationInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/internationalization/StringConcatenationInspection.java
@@ -16,15 +16,31 @@
 package com.siyeh.ig.internationalization;
 
 import com.intellij.codeInsight.daemon.GroupNames;
+import com.intellij.codeInsight.CodeInsightBundle;
 import com.intellij.psi.*;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.tree.IElementType;
 import com.siyeh.ig.BaseInspectionVisitor;
 import com.siyeh.ig.ExpressionInspection;
+import com.siyeh.ig.ui.MultipleCheckboxOptionsPanel;
 import com.siyeh.ig.psiutils.TypeUtils;
 import com.siyeh.InspectionGadgetsBundle;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.JComponent;
 
 public class StringConcatenationInspection extends ExpressionInspection {
+
+    /** @noinspection PublicField*/
+    public boolean ignoreAsserts = false;
+
+    /** @noinspection PublicField*/
+    public boolean ignoreSystemOuts = false;
+
+    /** @noinspection PublicField*/
+    public boolean ignoreSystemErrs = false;
 
     public String getDisplayName() {
         return InspectionGadgetsBundle.message(
@@ -41,11 +57,27 @@ public class StringConcatenationInspection extends ExpressionInspection {
                 "string.concatenation.problem.descriptor");
     }
 
+    @Nullable
+    public JComponent createOptionsPanel() {
+        final MultipleCheckboxOptionsPanel optionsPanel =
+                new MultipleCheckboxOptionsPanel(this);
+        optionsPanel.addCheckbox(CodeInsightBundle.message(
+                "inspection.i18n.option.ignore.assert"),
+                "ignoreAsserts");
+        optionsPanel.addCheckbox(InspectionGadgetsBundle.message(
+                "string.concatenation.ignore.system.out.option"),
+                "ignoreSystemOuts");
+        optionsPanel.addCheckbox(InspectionGadgetsBundle.message(
+                "string.concatenation.ignore.system.err.option"),
+                "ignoreSystemErrs");
+        return optionsPanel;
+    }
+
     public BaseInspectionVisitor buildVisitor() {
         return new StringConcatenationVisitor();
     }
 
-    private static class StringConcatenationVisitor
+    private class StringConcatenationVisitor
             extends BaseInspectionVisitor {
 
         public void visitBinaryExpression(
@@ -67,8 +99,34 @@ public class StringConcatenationInspection extends ExpressionInspection {
             }
             final PsiType rhsType = rhs.getType();
             if(!TypeUtils.isJavaLangString(lhsType) &&
-                       !TypeUtils.isJavaLangString(rhsType)){
+               !TypeUtils.isJavaLangString(rhsType)){
                 return;
+            }
+            final PsiElement element =
+                    PsiTreeUtil.getParentOfType(expression,
+                            PsiAssertStatement.class,
+                            PsiMethodCallExpression.class);
+            if (ignoreAsserts && element instanceof PsiAssertStatement) {
+                return;
+            }
+            if (element instanceof PsiMethodCallExpression) {
+                final PsiMethodCallExpression methodCallExpression =
+                        (PsiMethodCallExpression)element;
+                final PsiReferenceExpression methodExpression =
+                        methodCallExpression.getMethodExpression();
+                @NonNls
+                final String canonicalText =
+                        methodExpression.getCanonicalText();
+                if (ignoreSystemOuts &&
+                    "System.out.println".equals(canonicalText) ||
+                        "System.out.print".equals(canonicalText)) {
+                    return;
+                }
+                if (ignoreSystemErrs &&
+                        "System.err.println".equals(canonicalText) ||
+                        "System.err.print".equals(canonicalText)) {
+                    return;
+                }
             }
             registerError(sign);
         }
