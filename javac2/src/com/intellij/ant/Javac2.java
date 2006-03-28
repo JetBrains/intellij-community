3eca1ff40f8143305dd1904e027967982dc469d5
Eugene Vigdorchik <no_reply@jetbrains.com>
2006-03-28 17:14:47 +0400

    add log compiler string

--- javac2/src/com/intellij/ant/Javac2.java
+++ javac2/src/com/intellij/ant/Javac2.java
@@ -44,6 +44,7 @@ public final class Javac2 extends Javac{
   protected void compile(){
     // compile java
     super.compile();
+    log("javac2, using compiler: " + getCompiler());
 
     // we instrument every file, because we cannot find which files should not be instrumented without dependency storage
     final ArrayList formsToInstrument = myFormFiles;
