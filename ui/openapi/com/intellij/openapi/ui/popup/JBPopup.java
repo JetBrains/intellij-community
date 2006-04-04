2a44373ffa6da4a0304c5a4ea0ad530e78073e71
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2006-04-04 20:15:22 +0400

    http://www.jetbrains.net/jira/browse/IDEADEV-5401

--- ui/openapi/com/intellij/openapi/ui/popup/JBPopup.java
+++ ui/openapi/com/intellij/openapi/ui/popup/JBPopup.java
@@ -5,6 +5,7 @@ import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.ui.awt.RelativePoint;
+import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
 
@@ -14,10 +15,10 @@ import java.awt.*;
 public interface JBPopup {
   void showUnderneathOf(Component componentUnder);
   void show(RelativePoint point);
-  void showInScreenCoordinates(Component owner, Point point);
+  void showInScreenCoordinates(@NotNull Component owner, Point point);
   void showInBestPositionFor(DataContext dataContext);
   void showInBestPositionFor(Editor editor);
-  void showInCenterOf(Component focusOwner);
+  void showInCenterOf(@NotNull Component focusOwner);
   void showCenteredInCurrentWindow(Project project);
 
   void cancel();
