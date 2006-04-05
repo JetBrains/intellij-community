f99c0e58380e8903bd0c24eb519bcc4b471a3b25
Mike Aizatsky <no_reply@jetbrains.com>
2006-04-05 20:27:25 +0400

    Repackaging designer

--- designer/src/com/intellij/designer/deprecated/StreamUtil.java
+++ designer/src/com/intellij/designer/deprecated/StreamUtil.java
@@ -2,13 +2,16 @@
  * Copyright (c) 2000-2004 by JetBrains s.r.o. All Rights Reserved.
  * Use is subject to license terms.
  */
-package jetbrains.fabrique.util;
+package com.intellij.designer.deprecated;
 
 import java.io.*;
 
 /**
  *
  * @author Oleg Melnik
+ */
+/**
+ * @deprecated
  */
 public class StreamUtil {
   /**
