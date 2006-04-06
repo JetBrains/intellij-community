29827968c67ac7c5fab3b4f4533ed62346945f4f
Peter Gromov <peter@jetbrains.com>
2006-04-06 14:25:48 +0400

    PerspectiveFileEditorProvider

---
+++ openapi/src/com/intellij/util/xml/ui/PerspectiveFileEditorProvider.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2000-2006 JetBrains s.r.o. All Rights Reserved.
+ */
+package com.intellij.util.xml.ui;
+
+import com.intellij.openapi.components.ApplicationComponent;
+import com.intellij.openapi.fileEditor.*;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jdom.Element;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author peter
+ */
+public abstract class PerspectiveFileEditorProvider implements WeighedFileEditorProvider, ApplicationComponent {
+  @NotNull
+  public abstract PerspectiveFileEditor createEditor(Project project, VirtualFile file);
+
+  public void disposeEditor(FileEditor editor) {
+    ((PerspectiveFileEditor)editor).dispose();
+  }
+
+  @NotNull
+  public FileEditorState readState(Element sourceElement, Project project, VirtualFile file) {
+    return new FileEditorState() {
+      public boolean canBeMergedWith(FileEditorState otherState, FileEditorStateLevel level) {
+        return true;
+      }
+    };
+  }
+
+  public void writeState(FileEditorState state, Project project, Element targetElement) {
+  }
+
+  @NotNull
+  @NonNls
+  public final String getEditorTypeId() {
+    return getComponentName();
+  }
+
+  @NotNull
+  public final FileEditorPolicy getPolicy() {
+    return FileEditorPolicy.PLACE_AFTER_DEFAULT_EDITOR;
+  }
+
+  @NonNls
+  public final String getComponentName() {
+    return getClass().getName();
+  }
+
+  public void initComponent() {
+  }
+
+  public void disposeComponent() {
+  }
+}
