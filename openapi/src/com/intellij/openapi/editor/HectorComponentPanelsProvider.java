01a41b41528ddf4aab39ebd5267eef5c0692104c
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2006-04-06 15:43:40 +0400

    api improvements

--- openapi/src/com/intellij/openapi/editor/HectorComponentPanelsProvider.java
+++ openapi/src/com/intellij/openapi/editor/HectorComponentPanelsProvider.java
@@ -4,6 +4,7 @@
 
 package com.intellij.openapi.editor;
 
+import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.options.UnnamedConfigurable;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
@@ -12,6 +13,6 @@ import org.jetbrains.annotations.Nullable;
 /**
  * @author nik
  */
-public interface HectorComponentPanelsProvider {
+public interface HectorComponentPanelsProvider extends ProjectComponent {
   @Nullable UnnamedConfigurable createConfigurable(@NotNull PsiFile file);
 }
