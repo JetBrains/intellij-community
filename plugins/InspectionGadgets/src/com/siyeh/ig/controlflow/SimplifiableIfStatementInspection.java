d2a0c8cd71361fe260df330d0416ed84a3cc62a7
Bas Leijdekkers <basleijdekkers@gmail.com>
2006-04-06 13:40:26 +0400

    fixed copyright statement

--- plugins/InspectionGadgets/src/com/siyeh/ig/controlflow/SimplifiableIfStatementInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/controlflow/SimplifiableIfStatementInspection.java
@@ -1,24 +1,35 @@
-/**
- * (c) 2006 Carp Technologies BV
- * Brouwerijstraat 1, 7523XC Enschede
- * Created: 20060310, 6:13:35 PM
+/*
+ * Copyright 2006 Bas Leijdekkers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.siyeh.ig.controlflow;
 
-import com.siyeh.ig.ExpressionInspection;
-import com.siyeh.ig.BaseInspectionVisitor;
-import com.siyeh.ig.InspectionGadgetsFix;
-import com.siyeh.ig.psiutils.BoolUtils;
-import com.siyeh.ig.psiutils.EquivalenceChecker;
-import com.siyeh.ig.psiutils.ControlFlowUtils;
-import com.siyeh.InspectionGadgetsBundle;
 import com.intellij.codeInsight.daemon.GroupNames;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.openapi.project.Project;
-import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.util.IncorrectOperationException;
+import com.siyeh.InspectionGadgetsBundle;
+import com.siyeh.ig.BaseInspectionVisitor;
+import com.siyeh.ig.ExpressionInspection;
+import com.siyeh.ig.InspectionGadgetsFix;
+import com.siyeh.ig.psiutils.BoolUtils;
+import com.siyeh.ig.psiutils.ControlFlowUtils;
+import com.siyeh.ig.psiutils.EquivalenceChecker;
+import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -50,6 +61,7 @@ public class SimplifiableIfStatementInspection
                 calculateReplacementStatement(statement));
     }
 
+    @NonNls
     static String calculateReplacementStatement(
             PsiIfStatement statement) {
         PsiStatement thenBranch = statement.getThenBranch();
