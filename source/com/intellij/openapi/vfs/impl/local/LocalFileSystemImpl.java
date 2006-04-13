7fa9377f8eff60e7b24076940b00ef3053992d5e
Eugene Vigdorchik <no_reply@jetbrains.com>
2006-04-13 19:05:51 +0400

    VFS findFileByPath() should not collect all children

--- source/com/intellij/openapi/vfs/impl/local/LocalFileSystemImpl.java
+++ source/com/intellij/openapi/vfs/impl/local/LocalFileSystemImpl.java
@@ -454,8 +454,8 @@ public final class LocalFileSystemImpl extends LocalFileSystem implements Applic
             getManager().addEventToFireByRefresh(action, asynchronous, modalityState);
 
           } else {
-            //TODO: isRecursive could be optimized
-            file.refreshInternal(true, modalityState, false, asynchronous);
+            //TODO: isRecursive could be optimized???
+            refresh(file, file.areChildrenCached(), false, modalityState, asynchronous, false);
           }
         }
 
