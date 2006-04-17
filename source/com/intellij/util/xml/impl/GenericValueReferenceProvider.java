c1ca079e82aa0bd6d9ed6a1fd75b56aac6281f2a
Peter Gromov <peter@jetbrains.com>
2006-04-17 17:47:33 +0400

    tests for standard DOM references

--- source/com/intellij/util/xml/impl/GenericValueReferenceProvider.java
+++ source/com/intellij/util/xml/impl/GenericValueReferenceProvider.java
@@ -3,8 +3,6 @@
  */
 package com.intellij.util.xml.impl;
 
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.impl.ModuleUtil;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiType;
@@ -13,12 +11,16 @@ import com.intellij.psi.impl.source.resolve.reference.ReferenceType;
 import com.intellij.psi.impl.source.resolve.reference.impl.GenericReference;
 import com.intellij.psi.scope.PsiScopeProcessor;
 import com.intellij.psi.util.PsiUtil;
-import com.intellij.psi.xml.XmlAttribute;
 import com.intellij.psi.xml.XmlTag;
-import com.intellij.util.xml.*;
-import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.Function;
+import com.intellij.util.containers.ContainerUtil;
+import com.intellij.util.xml.*;
+import com.intellij.util.xml.reflect.DomAttributeChildDescription;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author peter
@@ -26,75 +28,89 @@ import org.jetbrains.annotations.NotNull;
 public class GenericValueReferenceProvider implements PsiReferenceProvider {
   @NotNull
   public GenericReference[] getReferencesByElement(PsiElement element) {
-    if (!(element instanceof XmlTag || element instanceof XmlAttribute)) return GenericReference.EMPTY_ARRAY;
+    if (!(element instanceof XmlTag)) return GenericReference.EMPTY_ARRAY;
     PsiElement originalElement = element.getUserData(PsiUtil.ORIGINAL_KEY);
     if (originalElement != null){
       element = originalElement;
     }
-    final Module module = ModuleUtil.findModuleForPsiElement(element);
-    if (module == null) return GenericReference.EMPTY_ARRAY;
 
-    final XmlTag tag = element instanceof XmlTag ? (XmlTag) element : ((XmlAttribute) element).getParent();
-    if (element == tag && tag.getValue().getTextElements().length ==0) return GenericReference.EMPTY_ARRAY;
+    final XmlTag tag = (XmlTag) element;
 
-    final DomElement domElement = DomManager.getDomManager(module.getProject()).getDomElement(tag);
-    if (!(domElement instanceof GenericDomValue)) return GenericReference.EMPTY_ARRAY;
+    final DomElement domElement = DomManager.getDomManager(element.getManager().getProject()).getDomElement(tag);
+    if (domElement == null) return GenericReference.EMPTY_ARRAY;
 
+    List<GenericDomValueReference> result = new ArrayList<GenericDomValueReference>();
+    for (final DomAttributeChildDescription description : domElement.getGenericInfo().getAttributeChildrenDescriptions()) {
+      if (tag.getAttribute(description.getXmlElementName(), null) != null) {
+        final GenericDomValueReference reference = createReference(description.getDomAttributeValue(domElement));
+        if (reference != null) {
+          result.add(reference);
+        }
+      }
+    }
+
+    final GenericDomValueReference reference = createReference(domElement);
+    if (reference != null) {
+      result.add(reference);
+    }
+
+    return result.toArray(new GenericReference[result.size()]);
+  }
+
+  @Nullable
+  private GenericDomValueReference createReference(DomElement element) {
+    if (!(element instanceof GenericDomValue)) return null;
+
+    GenericDomValue domElement = (GenericDomValue) element;
     final Class parameter = DomUtil.getGenericValueType(domElement.getDomElementType());
-    if (PsiType.class.isAssignableFrom(parameter)) {
-      return new GenericReference[]{new PsiTypeReference(this, (GenericDomValue<PsiType>)domElement)};
-    }
-    if (PsiClass.class.isAssignableFrom(parameter)) {
-      return new GenericReference[]{new PsiClassReference(this, (GenericDomValue<PsiClass>)domElement)};
-    }
-    if (Integer.class.isAssignableFrom(parameter)) {
-      return new GenericReference[]{new GenericDomValueReference(this, (GenericDomValue) domElement) {
-        public Object[] getVariants() {
-          return new Object[]{"239", "42"};
-        }
-
-        public PsiElement resolveInner() {
-          return getValueElement();
-        }
+      if (PsiType.class.isAssignableFrom(parameter)) {
+        return new PsiTypeReference(this, (GenericDomValue<PsiType>)domElement);
       }
-      };
-    }
-    if (Enum.class.isAssignableFrom(parameter)) {
-      return new GenericReference[]{new GenericDomValueReference(this, (GenericDomValue) domElement) {
-        public Object[] getVariants() {
-          final Enum[] enumConstants = (Enum[])parameter.getEnumConstants();
-          return ContainerUtil.map2Array(enumConstants, String.class, new Function<Enum, String>() {
-            public String fun(final Enum s) {
-              return NamedEnumUtil.getEnumValueByElement(s);
-            }
-          });
-        }
-
-        public PsiElement resolveInner() {
-          return getValueElement();
-        }
+      if (PsiClass.class.isAssignableFrom(parameter)) {
+        return new PsiClassReference(this, (GenericDomValue<PsiClass>)domElement);
       }
-      };
-    }
-    if (Boolean.class.isAssignableFrom(parameter)) {
-      return new GenericReference[]{new GenericDomValueReference(this, (GenericDomValue) domElement) {
-        public Object[] getVariants() {
-          return new Object[]{"true", "false"};
-        }
+      if (Integer.class.isAssignableFrom(parameter)) {
+        return new GenericDomValueReference(this, domElement) {
+          public Object[] getVariants() {
+            return new Object[]{"239", "42"};
+          }
 
-        public PsiElement resolveInner() {
-          return getValueElement();
-        }
+          public PsiElement resolveInner() {
+            return getValueElement();
+          }
+        };
       }
-      };
-    }
+      if (Enum.class.isAssignableFrom(parameter)) {
+        return new GenericDomValueReference(this, domElement) {
+          public Object[] getVariants() {
+            final Enum[] enumConstants = (Enum[])parameter.getEnumConstants();
+            return ContainerUtil.map2Array(enumConstants, String.class, new Function<Enum, String>() {
+              public String fun(final Enum s) {
+                return NamedEnumUtil.getEnumValueByElement(s);
+              }
+            });
+          }
 
+          public PsiElement resolveInner() {
+            return getValueElement();
+          }
+        };
+      }
+      if (Boolean.class.isAssignableFrom(parameter)) {
+        return new GenericDomValueReference(this, domElement) {
+          public Object[] getVariants() {
+            return new Object[]{"true", "false"};
+          }
 
-    if (!String.class.isAssignableFrom(parameter)) {
-      return new GenericReference[]{new GenericDomValueReference(this, (GenericDomValue) domElement)};
-    }
-
-    return GenericReference.EMPTY_ARRAY;
+          public PsiElement resolveInner() {
+            return getValueElement();
+          }
+        };
+      }
+      if (!String.class.isAssignableFrom(parameter)) {
+        return new GenericDomValueReference(this, domElement);
+      }
+    return null;
   }
 
 
