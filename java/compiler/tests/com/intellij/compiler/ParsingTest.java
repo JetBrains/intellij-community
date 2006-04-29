1ddba48123fe8214e4f292fe78c4f1960886b292
Eugene Zhuravlev <jeka@jetbrains.com>
2006-04-29 16:35:05 +0400

    fix generic signature parsing

--- java/compiler/tests/com/intellij/compiler/ParsingTest.java
+++ java/compiler/tests/com/intellij/compiler/ParsingTest.java
@@ -4,10 +4,22 @@
  */
 package com.intellij.compiler;
 
+import com.intellij.compiler.classParsing.GenericMethodSignature;
+import com.intellij.compiler.classParsing.SignatureParsingException;
 import com.intellij.compiler.make.MakeUtil;
 import junit.framework.TestCase;
 
 public class ParsingTest extends TestCase{
+
+  public void testParseGenericSignature() {
+    try {
+      final GenericMethodSignature signature = GenericMethodSignature.parse("<T::Lcom/intellij/util/xml/GenericValue<*>;>(Ljava/util/Collection<TT;>;[Ljava/lang/String;)Ljava/lang/String;");
+      assertTrue(signature != null);
+    }
+    catch (SignatureParsingException e) {
+      assertTrue("NOT PARSED:" + e.getMessage(), false);
+    }
+  }
 
   public void testParseObjectType() {
     String parsed = null;
