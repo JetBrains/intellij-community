3dc2e031ff44cdc2099e9310d2e5ae904adf086c
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2006-05-03 15:53:23 +0400

    IDEADEV-5709

---
+++ web/source/com/intellij/jsf/actions/LibraryFilter.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2000-2006 JetBrains s.r.o. All Rights Reserved.
+ */
+
+package com.intellij.jsf.actions;
+
+import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.roots.libraries.Library;
+
+/**
+ * @author nik
+ */
+public interface LibraryFilter {
+
+  boolean accept(@NotNull Library library);
+
+}
