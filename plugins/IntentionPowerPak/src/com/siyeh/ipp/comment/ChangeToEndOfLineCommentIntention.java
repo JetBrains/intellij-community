a9d194da3bb593e9ad7b26b2b3a0f7fd8613a0b6
Bas Leijdekkers <basleijdekkers@gmail.com>
2006-05-15 19:31:05 +0400

    IDEADEV-5965

--- plugins/IntentionPowerPak/src/com/siyeh/ipp/comment/ChangeToEndOfLineCommentIntention.java
+++ plugins/IntentionPowerPak/src/com/siyeh/ipp/comment/ChangeToEndOfLineCommentIntention.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2005 Dave Griffith
+ * Copyright 2003-2006 Dave Griffith, Bas Leijdekkers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,37 +27,37 @@ import org.jetbrains.annotations.NotNull;
 
 public class ChangeToEndOfLineCommentIntention extends Intention {
 
-
-  @NotNull
-  protected PsiElementPredicate getElementPredicate() {
-    return new CStyleCommentPredicate();
-  }
-
-  public void processIntention(PsiElement element)
-    throws IncorrectOperationException {
-    final PsiComment comment = (PsiComment)element;
-    final PsiManager manager = comment.getManager();
-    final CodeStyleManager codeStyleManager = manager.getCodeStyleManager();
-    final PsiElement parent = comment.getParent();
-    assert parent != null;
-    final PsiElementFactory factory = manager.getElementFactory();
-    final String commentText = comment.getText();
-    final PsiElement whitespace = comment.getNextSibling();
-    final String text = commentText.substring(2, commentText.length() - 2);
-    final String[] lines = text.split("\n");
-
-    for (int i = lines.length - 1; i >= 1; i--) {
-      final PsiComment nextComment =
-        factory.createCommentFromText("//" + lines[i].trim(),
-                                      parent);
-      parent.addAfter(nextComment, comment);
-      if (whitespace != null) {
-        parent.addAfter(whitespace.copy(), comment);
-      }
+    @NotNull
+    protected PsiElementPredicate getElementPredicate() {
+        return new CStyleCommentPredicate();
     }
-    final PsiComment newComment =
-      factory.createCommentFromText("//" + lines[0], parent);
-    final PsiElement replacedComment = comment.replace(newComment);
-    codeStyleManager.reformat(replacedComment);
-  }
+
+    public void processIntention(PsiElement element)
+            throws IncorrectOperationException {
+        final PsiComment comment = (PsiComment)element;
+        final PsiManager manager = comment.getManager();
+        final CodeStyleManager codeStyleManager = manager.getCodeStyleManager();
+        final PsiElement parent = comment.getParent();
+        assert parent != null;
+        final PsiElementFactory factory = manager.getElementFactory();
+        final String commentText = comment.getText();
+        final PsiElement whitespace = comment.getNextSibling();
+        final String text = commentText.substring(2, commentText.length() - 2);
+        final String[] lines = text.split("\n");
+        for (int i = lines.length - 1; i >= 1; i--) {
+            final PsiComment nextComment =
+                    factory.createCommentFromText("//" + lines[i].trim(),
+                            parent);
+            parent.addAfter(nextComment, comment);
+            if (whitespace != null) {
+                final PsiElement newWhiteSpace =
+                        factory.createWhiteSpaceFromText(whitespace.getText());
+                parent.addAfter(newWhiteSpace, comment);
+            }
+        }
+        final PsiComment newComment =
+                factory.createCommentFromText("//" + lines[0], parent);
+        final PsiElement replacedComment = comment.replace(newComment);
+        codeStyleManager.reformat(replacedComment);
+    }
 }
