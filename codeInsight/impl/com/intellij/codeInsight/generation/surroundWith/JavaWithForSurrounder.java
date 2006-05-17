e20d400dd9b7abfaa78266e235368d832fede4d3
Eugene Vigdorchik <no_reply@jetbrains.com>
2006-05-17 18:38:27 +0400

    exception: PSI locked by document

--- codeInsight/impl/com/intellij/codeInsight/generation/surroundWith/JavaWithForSurrounder.java
+++ codeInsight/impl/com/intellij/codeInsight/generation/surroundWith/JavaWithForSurrounder.java
@@ -2,6 +2,7 @@
 package com.intellij.codeInsight.generation.surroundWith;
 
 import com.intellij.codeInsight.CodeInsightBundle;
+import com.intellij.codeInsight.CodeInsightUtil;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
@@ -35,6 +36,7 @@ class JavaWithForSurrounder extends JavaStatementsSurrounder{
     bodyBlock.addRange(statements[0], statements[statements.length - 1]);
     container.deleteChildRange(statements[0], statements[statements.length - 1]);
 
+    forStatement = CodeInsightUtil.forcePsiPosprocessAndRestoreElement(forStatement);
     TextRange range1 = forStatement.getInitialization().getTextRange();
     TextRange range3 = forStatement.getUpdate().getTextRange();
     editor.getDocument().deleteString(range1.getStartOffset(), range3.getEndOffset());
