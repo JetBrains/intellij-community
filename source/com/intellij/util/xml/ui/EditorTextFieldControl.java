d061c894c9b18dd7b7e1c0d681922f8d7385fb91
Peter Gromov <peter@jetbrains.com>
2006-05-19 16:18:07 +0400

    cleanup

--- source/com/intellij/util/xml/ui/EditorTextFieldControl.java
+++ source/com/intellij/util/xml/ui/EditorTextFieldControl.java
@@ -4,8 +4,6 @@
 package com.intellij.util.xml.ui;
 
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer;
-import com.intellij.lang.annotation.AnnotationHolder;
-import com.intellij.lang.annotation.Annotator;
 import com.intellij.lang.annotation.HighlightSeverity;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.Result;
@@ -18,7 +16,6 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Factory;
 import com.intellij.psi.PsiCodeFragment;
 import com.intellij.psi.PsiDocumentManager;
-import com.intellij.psi.PsiElement;
 import com.intellij.ui.EditorTextField;
 import com.intellij.util.xml.DomElement;
 import com.intellij.util.xml.highlighting.DomElementAnnotationsManager;
@@ -92,16 +89,6 @@ public abstract class EditorTextFieldControl<T extends JComponent> extends BaseC
       }
     });
 
-    final Annotator annotator = new Annotator() {
-      public void annotate(PsiElement psiElement, AnnotationHolder holder) {
-        final DomElement domElement = getDomElement();
-        if (domElement == null || !domElement.isValid()) return;
-        for (final DomElementProblemDescriptor problem : DomElementAnnotationsManager.getInstance(project).getProblems(domElement, true)) {
-          holder.createErrorAnnotation(psiElement.getContainingFile(), problem.getDescriptionTemplate());
-        }
-      }
-    };
-    //file.addAnnotator(annotator);
     if (myCommitOnEveryChange) {
       editorTextField.getDocument().addDocumentListener(myListener);
     }
@@ -146,12 +133,6 @@ public abstract class EditorTextFieldControl<T extends JComponent> extends BaseC
           background = getWarningBackground();
         }
         textField.setBackground(background);
-
-        //todo!!! tooltip text isn't shown
-        errorProblems.addAll(warningProblems);
-        final String tooltipText = TooltipUtils.getTooltipText(errorProblems);
-
-
 
         final Editor editor = textField.getEditor();
         if (editor != null && isCommitted()) {
