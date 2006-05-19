288e904a98e39a2267db80faa6582e159f09472d
Eugene Vigdorchik <no_reply@jetbrains.com>
2006-05-19 18:24:41 +0400

    filter scope must be present regardless of ignoreAccessScope

--- openapi/src/com/intellij/psi/search/searches/ReferencesSearch.java
+++ openapi/src/com/intellij/psi/search/searches/ReferencesSearch.java
@@ -6,6 +6,7 @@ package com.intellij.psi.search.searches;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
 import com.intellij.psi.search.SearchScope;
+import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.util.Query;
 import com.intellij.util.QueryFactory;
 
@@ -43,7 +44,7 @@ public class ReferencesSearch extends QueryFactory<PsiReference, ReferencesSearc
   }
 
   public static Query<PsiReference> search(PsiElement element) {
-    return search(element, element.getUseScope(), true);
+    return search(element, GlobalSearchScope.projectScope(element.getProject()), false);
   }
 
   public static Query<PsiReference> search(PsiElement element, SearchScope searchScope) {
