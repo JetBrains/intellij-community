6e7c40585ea1d935b7fc6c942662efdd65d17cac
Alexey Kudravtsev <cdr@jetbrains.com>
2006-05-19 18:12:15 +0400

    javadocs

--- openapi/src/com/intellij/psi/LanguageInjector.java
+++ openapi/src/com/intellij/psi/LanguageInjector.java
@@ -4,8 +4,16 @@ import com.intellij.lang.Language;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * @author max
+ * Describes logic for injecting language inside hosting PSI element.
+ * E.g. "inject XSLT language into all XML attributes named 'path' that sit inside XML tag prefixed with 'xsl:'"
+ * @see com.intellij.psi.PsiLanguageInjectionHost
+ * @see com.intellij.psi.PsiManager#registerLanguageInjector(LanguageInjector)
+ * @see com.intellij.psi.PsiManager#unregisterLanguageInjector(LanguageInjector)
  */
 public interface LanguageInjector {
+  /**
+   * @param host PSI element inside which your language will be injected
+   * @return language that should be injected in this particular place, or null if there is no any.
+   */
   @Nullable Language getLanguageToInject(PsiLanguageInjectionHost host);
 }
--- openapi/src/com/intellij/psi/PsiLanguageInjectionHost.java
+++ openapi/src/com/intellij/psi/PsiLanguageInjectionHost.java
@@ -5,13 +5,21 @@ import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.util.Pair;
 
 /**
- * @author max
+ * Marks psi element as (potentially) containing text in other language.
+ * Injected language PSI does not embed into the PSI tree of the hosting element,
+ * but is used by IDEA for highlighting, completion and other code insight actions.
+ * Currently, language can be injected into string literals, XML tag contents and XML attributes.
+ * In order to do the injection, you have to
+ * <ul>
+ * <li>Implement {@link com.intellij.psi.LanguageInjector} to describe exact place where injection should occur.</li>  
+ * <li>Register injection in {@link com.intellij.psi.PsiManager#registerLanguageInjector(LanguageInjector)} .</li>
+ * </ul>
  */
 public interface PsiLanguageInjectionHost extends PsiElement {
   /**
    * @return injected PSI element and text range inside host element where injection occurs.
    * For example, in string literals we might want to inject something inside double quotes.
-   * To express this, use <code>return Pair.create(injectedPsi, new TextRange(1, literalLength-2))</code>.
+   * To express this, use <code>return Pair.create(injectedPsi, new TextRange(1, textLength+1))</code>.
    */
   @Nullable
   Pair<PsiElement, TextRange> getInjectedPsi();
