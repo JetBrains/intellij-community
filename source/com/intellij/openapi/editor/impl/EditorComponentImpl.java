dcb9f83d160e16fbd5517f074e871dba7792c0e5
Maxim Shafirov <max@jetbrains.com>
2006-05-19 19:41:23 +0400

    Fix for non-file editors to be provided in data context.

--- source/com/intellij/openapi/editor/impl/EditorComponentImpl.java
+++ source/com/intellij/openapi/editor/impl/EditorComponentImpl.java
@@ -1,6 +1,7 @@
 package com.intellij.openapi.editor.impl;
 
 import com.intellij.ide.ui.UISettings;
+import com.intellij.openapi.actionSystem.DataConstants;
 import com.intellij.openapi.actionSystem.DataProvider;
 import com.intellij.openapi.actionSystem.ex.DataConstantsEx;
 import com.intellij.openapi.application.ApplicationManager;
@@ -34,11 +35,15 @@ public class EditorComponentImpl extends JComponent implements Scrollable, DataP
   public Object getData(String dataId) {
     if (myEditor.isRendererMode()) return null;
 
-    /*
     if (DataConstants.EDITOR.equals(dataId)) {
-      return myEditor;
+      if (myEditor.getVirtualFile() == null) {
+        return myEditor;
+      }
+      else {
+        return null;
+      }
     }
-    */
+
     if (DataConstantsEx.DELETE_ELEMENT_PROVIDER.equals(dataId)) {
       return myEditor.getDeleteProvider();
     }
