335376b80e0b7b201a33b4a395984fc07793d47e
Maxim Shafirov <max@jetbrains.com>
2006-05-26 20:26:05 +0400

    Attempt to make tests run faster.

--- idea-tests/src/com/intellij/TestAll.java
+++ idea-tests/src/com/intellij/TestAll.java
@@ -54,7 +54,7 @@ public class TestAll implements Test {
   private static int CHECK_MEMORY = 8;
   private static int FILTER_CLASSES = 16;
 
-  public static int ourMode = SAVE_MEMORY_SNAPSHOT | START_GUARD | RUN_GC | CHECK_MEMORY | FILTER_CLASSES;
+  public static int ourMode = SAVE_MEMORY_SNAPSHOT | START_GUARD /*| RUN_GC | CHECK_MEMORY*/ | FILTER_CLASSES;
   private int myLastTestTestMethodCount = 0;
   public static final int MAX_FAILURE_TEST_COUNT = 150;
 
