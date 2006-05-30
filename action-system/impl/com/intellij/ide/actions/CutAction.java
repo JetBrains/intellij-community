bdb49d6b8219bcf0ee329605bb1530eb785cd227
Alexey Kudravtsev <cdr@jetbrains.com>
2006-05-30 16:19:43 +0400

    IDEADEV-5922

--- action-system/impl/com/intellij/ide/actions/CutAction.java
+++ action-system/impl/com/intellij/ide/actions/CutAction.java
@@ -2,16 +2,13 @@
 package com.intellij.ide.actions;
 
 import com.intellij.ide.CutProvider;
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DataContext;
-import com.intellij.openapi.actionSystem.Presentation;
-import com.intellij.openapi.actionSystem.ex.DataConstantsEx;
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.project.Project;
 
 public class CutAction extends AnAction {
   public void actionPerformed(AnActionEvent e) {
     DataContext dataContext = e.getDataContext();
-    CutProvider provider = (CutProvider)dataContext.getData(DataConstantsEx.CUT_PROVIDER);
+    CutProvider provider = (CutProvider)dataContext.getData(DataConstants.CUT_PROVIDER);
     if (provider == null) return;
     provider.performCut(dataContext);
   }
@@ -19,7 +16,8 @@ public class CutAction extends AnAction {
   public void update(AnActionEvent event){
     Presentation presentation = event.getPresentation();
     DataContext dataContext = event.getDataContext();
-    CutProvider provider = (CutProvider)dataContext.getData(DataConstantsEx.CUT_PROVIDER);
-    presentation.setEnabled(provider != null && provider.isCutEnabled(dataContext));
+    CutProvider provider = (CutProvider)dataContext.getData(DataConstants.CUT_PROVIDER);
+    Project project = (Project)dataContext.getData(DataConstants.PROJECT);
+    presentation.setEnabled(project != null && project.isOpen() && provider != null && provider.isCutEnabled(dataContext));
   }
 }
