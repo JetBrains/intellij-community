5d06577802509a82332c466732c20e678905e84e
Maxim Shafirov <max@jetbrains.com>
2006-06-01 01:09:43 +0400

    Property bundles are no longer hard referenced from static fields. As ResourceBunder.getBundle() method uses soft caches it doesn't seem reasonable to have additional hard caches.

--- plugins/structuralsearch/source/com/intellij/structuralsearch/SSRBundle.java
+++ plugins/structuralsearch/source/com/intellij/structuralsearch/SSRBundle.java
@@ -7,11 +7,11 @@ import org.jetbrains.annotations.PropertyKey;
 import java.util.ResourceBundle;
 
 public class SSRBundle {
-  @NonNls private static final ResourceBundle ourBundle = ResourceBundle.getBundle("messages.SSRBundle");
+  @NonNls private static final String BUNDLE = "messages.SSRBundle";
 
   private SSRBundle() {}
 
-  public static String message(@NonNls @PropertyKey(resourceBundle = "messages.SSRBundle") String key, Object... params) {
-    return CommonBundle.message(ourBundle, key, params);
+  public static String message(@NonNls @PropertyKey(resourceBundle = BUNDLE) String key, Object... params) {
+    return CommonBundle.message(ResourceBundle.getBundle(BUNDLE), key, params);
   }
 }
