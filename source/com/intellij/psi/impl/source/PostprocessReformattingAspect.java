25b0874237e577c1a76b468396d6d2e44b7d7b65
Eugene Vigdorchik <no_reply@jetbrains.com>
2006-05-31 13:18:39 +0400

    Changed by IK. Locked by PSI enforced for delete events.

--- source/com/intellij/psi/impl/source/PostprocessReformattingAspect.java
+++ source/com/intellij/psi/impl/source/PostprocessReformattingAspect.java
@@ -161,7 +161,7 @@ public class PostprocessReformattingAspect implements PomModelAspect {
   }
 
   public boolean isViewProviderLocked(final FileViewProvider fileViewProvider) {
-    return myReformatElements.containsKey(fileViewProvider);
+    return myUpdatedProviders.contains(fileViewProvider);
   }
 
   public static PostprocessReformattingAspect getInstance(Project project) {
