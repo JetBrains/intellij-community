7ca4a3afc76d5a986e014942541fdec55f0a7223
Eugene Vigdorchik <no_reply@jetbrains.com>
2006-06-08 14:28:33 +0400

    cleanup

--- openapi/src/com/intellij/openapi/editor/ScrollType.java
+++ openapi/src/com/intellij/openapi/editor/ScrollType.java
@@ -15,22 +15,10 @@
  */
 package com.intellij.openapi.editor;
 
-import org.jetbrains.annotations.NonNls;
-
-public final class ScrollType {
-  public static final ScrollType RELATIVE = new ScrollType("RELATIVE");
-  public static final ScrollType CENTER = new ScrollType("CENTER");
-  public static final ScrollType MAKE_VISIBLE = new ScrollType("MAKE_VISIBLE");
-  public static final ScrollType CENTER_UP = new ScrollType("CENTER_UP");
-  public static final ScrollType CENTER_DOWN = new ScrollType("CENTER_DOWN");
-
-  private final String myDebugName;
-
-  private ScrollType(@NonNls String debugName) {
-    myDebugName = debugName;
-  }
-
-  public String toString() {
-    return myDebugName;
-  }
+public enum ScrollType {
+  RELATIVE,
+  CENTER,
+  MAKE_VISIBLE,
+  CENTER_UP,
+  CENTER_DOWN
 }
