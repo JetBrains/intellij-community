145ff04876eab4ab3bb5b32ba48098053a6f3447
Alexander Kitaev <no_reply@jetbrains.com>
2006-06-15 14:20:37 +0400

    missing file added

---
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/dialogs/RepositoryTreeModel.java
@@ -0,0 +1,87 @@
+package org.jetbrains.idea.svn.dialogs;
+
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeNode;
+
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNURL;
+import org.tmatesoft.svn.core.io.SVNRepository;
+import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
+import org.jetbrains.idea.svn.SvnVcs;
+
+public class RepositoryTreeModel extends DefaultTreeModel {
+
+  private SvnVcs myVCS;
+  private boolean myIsShowFiles;
+
+  public RepositoryTreeModel(SvnVcs vcs, boolean showFiles) {
+    super(null);
+    myVCS = vcs;
+    myIsShowFiles = showFiles;
+  }
+
+  public boolean isShowFiles() {
+    return myIsShowFiles;
+  }
+
+  public void setRoots(SVNURL[] urls) {
+    setRoot(new RepositoryTreeRootNode(this, urls));
+  }
+
+  public void setSingleRoot(SVNURL url) {
+    SVNRepository repos = createRepository(url);
+    setRoot(new RepositoryTreeNode(this, null, repos, url, url));
+  }
+
+  public boolean hasRoot(SVNURL url) {
+    if (getRoot()instanceof RepositoryTreeNode) {
+      return ((RepositoryTreeNode) getRoot()).getUserObject().equals(url);
+    }
+    RepositoryTreeRootNode root = (RepositoryTreeRootNode) getRoot();
+    for (int i = 0; i < root.getChildCount(); i++) {
+      RepositoryTreeNode node = (RepositoryTreeNode) root.getChildAt(i);
+      if (node.getUserObject().equals(url)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public void addRoot(SVNURL url) {
+    if (!hasRoot(url)) {
+      ((RepositoryTreeRootNode) getRoot()).addRoot(url);
+    }
+  }
+
+  public void removeRoot(SVNURL url) {
+    RepositoryTreeRootNode root = (RepositoryTreeRootNode) getRoot();
+    for (int i = 0; i < root.getChildCount(); i++) {
+      RepositoryTreeNode node = (RepositoryTreeNode) root.getChildAt(i);
+      if (node.getUserObject().equals(url)) {
+        root.remove(node);
+      }
+    }
+  }
+
+  public SVNURL[] getRoots() {
+    TreeNode root = (TreeNode) getRoot();
+    if (root instanceof RepositoryTreeNode) {
+      return new SVNURL[]{(SVNURL) ((RepositoryTreeNode) root).getUserObject()};
+    }
+    SVNURL[] roots = new SVNURL[root.getChildCount()];
+    for (int i = 0; i < roots.length; i++) {
+      roots[i] = (SVNURL) ((RepositoryTreeNode) root.getChildAt(i)).getUserObject();
+    }
+    return roots;
+  }
+
+  protected SVNRepository createRepository(SVNURL url) {
+    try {
+      return myVCS.createRepository(url.toString());
+    } catch (SVNException e) {
+      //
+    }
+    return null;
+  }
+
+}
