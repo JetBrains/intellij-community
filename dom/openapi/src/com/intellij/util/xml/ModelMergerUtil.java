348675f67dc271c7d07586722431c2b95cd7c987
greg <Gregory.Shrago@jetbrains.com>
2006-06-21 18:12:12 +0400

    persistent attributes & CMP methods/fields searchers; highlighting duplicate quickfixes issue fixes

--- dom/openapi/src/com/intellij/util/xml/ModelMergerUtil.java
+++ dom/openapi/src/com/intellij/util/xml/ModelMergerUtil.java
@@ -42,9 +42,9 @@ public class ModelMergerUtil {
     return (V)processor.getFoundValue();
   }
 
-  @Nullable
+  @NotNull
   public static <T, V> Collection<V> getImplementations(final T element, final Class<V> clazz) {
-    if (element == null) return null;
+    if (element == null) return Collections.emptyList();
     CommonProcessors.CollectProcessor<T> processor = new CommonProcessors.CollectProcessor<T>() {
       public boolean process(final T t) {
         return !ReflectionCache.isAssignable(clazz, t.getClass()) || super.process(t);
