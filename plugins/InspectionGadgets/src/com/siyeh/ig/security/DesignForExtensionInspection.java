265ff3e12feb6b4482e0ba9b4b31e7efe4bcc253
Bas Leijdekkers <basleijdekkers@gmail.com>
2006-06-23 16:34:37 +0400

    IDEADEV-6619

--- plugins/InspectionGadgets/src/com/siyeh/ig/security/DesignForExtensionInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/security/DesignForExtensionInspection.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2006 Dave Griffith
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.siyeh.ig.security;
 
 import com.intellij.codeInsight.daemon.GroupNames;
@@ -8,6 +24,11 @@ import com.siyeh.ig.MethodInspection;
 import org.jetbrains.annotations.NotNull;
 
 public class DesignForExtensionInspection extends MethodInspection {
+
+    public String getDisplayName() {
+        return InspectionGadgetsBundle.message(
+                "design.for.extension.display.name");
+    }
 
     public String getGroupDisplayName() {
         return GroupNames.SECURITY_GROUP_NAME;
@@ -28,42 +49,34 @@ public class DesignForExtensionInspection extends MethodInspection {
 
         public void visitMethod(PsiMethod method) {
             super.visitMethod(method);
-            if(method.isConstructor())
-            {
+            if(method.isConstructor()) {
                 return;
             }
             if(method.hasModifierProperty(PsiModifier.PRIVATE)||
                     method.hasModifierProperty(PsiModifier.FINAL) ||
                     method.hasModifierProperty(PsiModifier.ABSTRACT) ||
-                    method.hasModifierProperty(PsiModifier.STATIC))
-            {
+                    method.hasModifierProperty(PsiModifier.STATIC)) {
                 return;
             }
             final PsiClass containingClass = method.getContainingClass();
-            if(containingClass==null)
-            {
+            if(containingClass==null) {
                 return;
             }
-            if(containingClass.hasModifierProperty(PsiModifier.FINAL))
-            {
+            if(containingClass.hasModifierProperty(PsiModifier.FINAL)) {
                 return;
             }
-            if(containingClass.getName()==null)
-            {
-               return; //anonymous classes can't be overridden
+            if(containingClass.getName()==null) {
+                return; //anonymous classes can't be overridden
             }
             final PsiCodeBlock body = method.getBody();
-            if(body == null)
-            {
+            if(body == null) {
                 return;
             }
             final PsiStatement[] statements = body.getStatements();
-            if(statements.length!=0)
-            {
+            if(statements.length!=0) {
                 return;
             }
             registerMethodError(method);
         }
-
     }
 }
