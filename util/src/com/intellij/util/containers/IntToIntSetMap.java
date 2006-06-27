9c80e8728cdbb17e889f9bec75437837b2a6fa4a
Vyacheslav Lukianov <lvo@jetbrains.com>
2006-06-27 19:05:44 +0400

    aggregate changes for hastening purge

--- util/src/com/intellij/util/containers/IntToIntSetMap.java
+++ util/src/com/intellij/util/containers/IntToIntSetMap.java
@@ -1,38 +1,38 @@
 package com.intellij.util.containers;
 
+import gnu.trove.TIntHashSet;
 import gnu.trove.TIntIntHashMap;
 import gnu.trove.TIntObjectHashMap;
 
 /**
  * @author max
  */
-public class IntToIntArrayMap {
+public class IntToIntSetMap {
   private static final int[] EMPTY_INTS = new int[0];
   private TIntIntHashMap mySingle;
-  private TIntObjectHashMap<IntArrayList> myMulti;
+  private TIntObjectHashMap<TIntHashSet> myMulti;
 
 
-  public IntToIntArrayMap(int initialCapacity, float loadfactor) {
+  public IntToIntSetMap(int initialCapacity, float loadfactor) {
     mySingle = new TIntIntHashMap(initialCapacity, loadfactor);
-    myMulti = new TIntObjectHashMap<IntArrayList>(initialCapacity, loadfactor);
+    myMulti = new TIntObjectHashMap<TIntHashSet>(initialCapacity, loadfactor);
   }
 
   public void addOccurence(int key, int value) {
     if (mySingle.containsKey(key)) {
       int old = mySingle.get(key);
-      IntArrayList items = new IntArrayList(2);
+      TIntHashSet items = new TIntHashSet(3);
       items.add(old);
       items.add(value);
       mySingle.remove(key);
       myMulti.put(key, items);
       return;
     }
-
-    if (myMulti.containsKey(key)) {
-      myMulti.get(key).add(value);
+    final TIntHashSet items = myMulti.get(key);
+    if (items != null) {
+      items.add(value);
       return;
     }
-
     mySingle.put(key, value);
   }
 
@@ -41,19 +41,12 @@ public class IntToIntArrayMap {
       mySingle.remove(key);
       return;
     }
-
-    IntArrayList items = myMulti.get(key);
+    TIntHashSet items = myMulti.get(key);
     if (items != null) {
-      int idx = items.indexOf(value);
-      if (idx >= 0) {
-        items.remove(idx);
-        if (items.size() == 1) {
-          mySingle.put(key, items.get(0));
-          myMulti.remove(key);
-        }
-        else if (items.size() == 0) {
-          myMulti.remove(key);
-        }
+      items.remove(value);
+      if (items.size() == 1) {
+        mySingle.put(key, items.toArray()[0]);
+        myMulti.remove(key);
       }
     }
   }
@@ -62,8 +55,7 @@ public class IntToIntArrayMap {
     if (mySingle.containsKey(key)) {
       return new int[]{mySingle.get(key)};
     }
-
-    IntArrayList items = myMulti.get(key);
+    TIntHashSet items = myMulti.get(key);
     if (items == null) return EMPTY_INTS;
     return items.toArray();
   }
