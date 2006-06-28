909db0eb1fdf656c29c2985f79f72d704452cae7
Maxim Mossienko <Maxim.Mossienko@jetbrains.com>
2006-06-28 23:11:35 +0400

    test fixed

--- source/com/intellij/psi/impl/CachedValueImpl.java
+++ source/com/intellij/psi/impl/CachedValueImpl.java
@@ -119,7 +119,7 @@ public class CachedValueImpl<T> implements CachedValue<T> {
     List deps = new ArrayList();
     collectDependencies(timeStamps, deps, dependencies);
     if (myTrackValue) {
-      collectDependencies(timeStamps, deps, new Object[]{myValue});
+      collectDependencies(timeStamps, deps, new Object[]{myValue.get()});
     }
 
     myLastPsiTimeStamp = myManager.getModificationTracker().getModificationCount();
@@ -129,7 +129,7 @@ public class CachedValueImpl<T> implements CachedValue<T> {
 
   private void collectDependencies(TLongArrayList timeStamps, List resultingDeps, Object[] dependencies) {
     for (Object dependency : dependencies) {
-      if (dependency == null) continue;
+      if (dependency == null || dependency == NULL) continue;
       if (dependency.getClass().isArray()) {
         Object[] objects = (Object[])dependency;
         collectDependencies(timeStamps, resultingDeps, objects);
