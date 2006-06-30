5ca91b3453b1f52b8a9e15bc4bdd980d8a888e94
Eugene Vigdorchik <no_reply@jetbrains.com>
2006-06-30 15:29:38 +0400

    cleanup

--- UsageView/src/com/intellij/usages/UsageModelTracker.java
+++ UsageView/src/com/intellij/usages/UsageModelTracker.java
@@ -40,25 +40,7 @@ public class UsageModelTracker {
 
   public UsageModelTracker(Project project) {
     myProject = project;
-    myPsiListener = new PsiTreeChangeListener() {
-      public void beforeChildAddition(PsiTreeChangeEvent event) {
-      }
-
-      public void beforeChildRemoval(PsiTreeChangeEvent event) {
-      }
-
-      public void beforeChildReplacement(PsiTreeChangeEvent event) {
-      }
-
-      public void beforeChildMovement(PsiTreeChangeEvent event) {
-      }
-
-      public void beforeChildrenChange(PsiTreeChangeEvent event) {
-      }
-
-      public void beforePropertyChange(PsiTreeChangeEvent event) {
-      }
-
+    myPsiListener = new PsiTreeChangeAdapter() {
       public void childAdded(PsiTreeChangeEvent event) {
         if (!(event.getFile() instanceof PsiCodeFragment)) {
           fireModelChanged(false);
