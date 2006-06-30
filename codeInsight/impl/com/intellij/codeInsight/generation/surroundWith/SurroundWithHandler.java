14ecc262ec3f5e7adf1bbd7871317b7b7e46eb3d
Maxim Mossienko <Maxim.Mossienko@jetbrains.com>
2006-06-30 21:08:20 +0400

    "Surround with" does not work if user selected one or more spaces (IDEADEV-6764)

--- codeInsight/impl/com/intellij/codeInsight/generation/surroundWith/SurroundWithHandler.java
+++ codeInsight/impl/com/intellij/codeInsight/generation/surroundWith/SurroundWithHandler.java
@@ -3,10 +3,10 @@ package com.intellij.codeInsight.generation.surroundWith;
 import com.intellij.codeInsight.CodeInsightActionHandler;
 import com.intellij.codeInsight.CodeInsightBundle;
 import com.intellij.codeInsight.template.TemplateManager;
-import com.intellij.lang.surroundWith.SurroundDescriptor;
-import com.intellij.lang.surroundWith.Surrounder;
 import com.intellij.lang.Language;
 import com.intellij.lang.StdLanguages;
+import com.intellij.lang.surroundWith.SurroundDescriptor;
+import com.intellij.lang.surroundWith.Surrounder;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.LogicalPosition;
@@ -14,14 +14,11 @@ import com.intellij.openapi.editor.ScrollType;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
-import com.intellij.openapi.fileTypes.FileType;
-import com.intellij.openapi.fileTypes.StdFileTypes;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiWhiteSpace;
 import com.intellij.util.IncorrectOperationException;
-import org.jetbrains.annotations.NotNull;
 
 public class SurroundWithHandler implements CodeInsightActionHandler{
   private static final Logger LOG = Logger.getInstance("#com.intellij.codeInsight.generation.surroundWith.SurroundWithHandler");
@@ -35,14 +32,21 @@ public class SurroundWithHandler implements CodeInsightActionHandler{
     return true;
   }
 
-  public static boolean isLanguageWithWSSignificant(@NotNull PsiElement element) {
-    Language lang = element.getLanguage();
-    if (lang == StdLanguages.XML) lang = element.getParent().getLanguage();
+  public static boolean isLanguageWithWSSignificant(PsiElement element) {
+    return isLanguageWithWSSignificant(getLanguage(element));
+  }
 
+  public static boolean isLanguageWithWSSignificant(Language lang) {
     return lang == StdLanguages.HTML ||
            lang == StdLanguages.XHTML ||
            lang == StdLanguages.JSP ||
            lang == StdLanguages.JSPX;
+  }
+
+  private static Language getLanguage(PsiElement element) {
+    Language lang = element.getLanguage();
+    if (lang == StdLanguages.XML) lang = element.getParent().getLanguage();
+    return lang;
   }
 
   public void invoke(final Project project, final Editor editor, PsiFile file, Surrounder surrounder){
@@ -64,18 +68,22 @@ public class SurroundWithHandler implements CodeInsightActionHandler{
     PsiElement element2 = file.findElementAt(endOffset - 1);
 
     if (element1 == null || element2 == null) return;
-    Language lang = element1.getLanguage();
-    Language lang2 = element2.getLanguage();
+    Language lang = getLanguage(element1);
+    Language lang2 = getLanguage(element2);
 
-    if(lang != lang2) return;
-
-    if (element1 instanceof PsiWhiteSpace && isLanguageWithWSSignificant(element1) ) {
+    if (element1 instanceof PsiWhiteSpace && isLanguageWithWSSignificant(lang) ) {
       startOffset = element1.getTextRange().getEndOffset();
       element1 = file.findElementAt(startOffset);
     }
-    if (element2 instanceof PsiWhiteSpace && isLanguageWithWSSignificant(element2) ) {
+    if (element2 instanceof PsiWhiteSpace && isLanguageWithWSSignificant(lang2) ) {
       endOffset = element2.getTextRange().getStartOffset();
+      element2 = file.findElementAt(endOffset);
     }
+
+    lang = getLanguage(element1);
+    lang2 = getLanguage(element2);
+
+    if(lang != lang2) return;
 
     final SurroundDescriptor[] surroundDescriptors = element1.getLanguage().getSurroundDescriptors();
     if (surroundDescriptors.length == 0) return;
