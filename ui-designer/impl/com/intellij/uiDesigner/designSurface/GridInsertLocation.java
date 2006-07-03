93be5412b116f5dc5c8f7e40781005ee737a9112
Dmitry Jemerov <yole@intellij.com>
2006-07-03 14:20:23 +0400

    correct feedback rect positioning

--- ui-designer/impl/com/intellij/uiDesigner/designSurface/GridInsertLocation.java
+++ ui-designer/impl/com/intellij/uiDesigner/designSurface/GridInsertLocation.java
@@ -204,8 +204,8 @@ public class GridInsertLocation extends GridDropLocation {
       if (rcFeedback == null) {
         if (insertCol == layoutManager.getGridColumnCount(getContainer())-1 && myMode == GridInsertMode.ColumnAfter) {
           final Dimension initialSize = dragObject.getInitialSize(getContainer().getDelegee());
-          int remainingSize = getContainer().getDelegee().getWidth() - vGridLines [vGridLines.length-1];
           int feedbackX = vGridLines [vGridLines.length-1] + layoutManager.getGapCellSize(myContainer, false);
+          int remainingSize = getContainer().getDelegee().getWidth() - feedbackX;
           if (!dragObject.isHGrow() && remainingSize > initialSize.width) {
            if (dragObject.isVGrow() || initialSize.height > cellHeight) {
               rcFeedback = new Rectangle(feedbackX, hGridLines [insertRow], initialSize.width, cellHeight);
@@ -221,8 +221,8 @@ public class GridInsertLocation extends GridDropLocation {
         }
         else if (insertRow == layoutManager.getGridRowCount(getContainer())-1 && myMode == GridInsertMode.RowAfter) {
           final Dimension initialSize = dragObject.getInitialSize(getContainer().getDelegee());
-          int remainingSize = getContainer().getDelegee().getHeight() - hGridLines [hGridLines.length-1];
           int feedbackY = hGridLines [hGridLines.length-1] + layoutManager.getGapCellSize(myContainer, true);
+          int remainingSize = getContainer().getDelegee().getHeight() - feedbackY;
           if (!dragObject.isVGrow() && remainingSize > initialSize.height) {
             rcFeedback = new Rectangle(vGridLines [insertCol], feedbackY, cellWidth, initialSize.height);
           }
