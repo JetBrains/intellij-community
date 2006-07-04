afe6804a37e73781f2d9f89c7e573f4440749159
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2006-07-04 12:50:54 +0400

    add 'output' tab for 'plugin' modules

--- plugins/devkit/src/module/PluginModuleEditorsProvider.java
+++ plugins/devkit/src/module/PluginModuleEditorsProvider.java
@@ -20,13 +20,14 @@ import com.intellij.openapi.module.ModuleConfigurationEditor;
 import com.intellij.openapi.roots.ui.configuration.DefaultModuleConfigurationEditorFactory;
 import com.intellij.openapi.roots.ui.configuration.ModuleConfigurationEditorProvider;
 import com.intellij.openapi.roots.ui.configuration.ModuleConfigurationState;
-import com.intellij.openapi.roots.ui.configuration.ModulesProvider;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.idea.devkit.build.PluginModuleBuildConfEditor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class PluginModuleEditorsProvider implements ModuleComponent, ModuleConfigurationEditorProvider{
+  @NotNull
   public String getComponentName() {
     return "DevKit.PluginModuleEditorsProvider";
   }
@@ -34,14 +35,9 @@ public class PluginModuleEditorsProvider implements ModuleComponent, ModuleConfi
 
   public ModuleConfigurationEditor[] createEditors(ModuleConfigurationState state) {
     final DefaultModuleConfigurationEditorFactory editorFactory = DefaultModuleConfigurationEditorFactory.getInstance();
-    ModulesProvider provider = state.getModulesProvider();
     List<ModuleConfigurationEditor> editors = new ArrayList<ModuleConfigurationEditor>();
     editors.add(editorFactory.createModuleContentRootsEditor(state));
-    //editors.add(editorFactory.createLibrariesEditor(state));
-    //if (provider.getModules().length > 1) {
-    //  editors.add(editorFactory.createDependenciesEditor(state));
-    //}
-    //editors.add(editorFactory.createOrderEntriesEditor(state));
+    editors.add(editorFactory.createOutputEditor(state));
     editors.add(editorFactory.createClasspathEditor(state));
     editors.add(editorFactory.createJavadocEditor(state));
     editors.add(new PluginModuleBuildConfEditor(state));
