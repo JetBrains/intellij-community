d9da9b311283d0fee146de995e71529ff620ef4a
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2006-07-05 18:00:01 +0400

    STRUTS-23

--- dom/impl/src/com/intellij/util/xml/tree/actions/DeleteDomElement.java
+++ dom/impl/src/com/intellij/util/xml/tree/actions/DeleteDomElement.java
@@ -5,14 +5,15 @@
 package com.intellij.util.xml.tree.actions;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.application.Result;
 import com.intellij.openapi.application.ApplicationBundle;
+import com.intellij.openapi.application.Result;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.util.IconLoader;
 import com.intellij.ui.treeStructure.SimpleNode;
-import com.intellij.util.xml.ElementPresentation;
 import com.intellij.util.xml.DomElement;
+import com.intellij.util.xml.ElementPresentation;
 import com.intellij.util.xml.tree.BaseDomElementNode;
+import com.intellij.util.xml.tree.DomFileElementNode;
 import com.intellij.util.xml.tree.DomModelTreeView;
 
 /**
@@ -27,24 +28,27 @@ public class DeleteDomElement extends BaseDomTreeAction {
     super(treeView);
   }
 
-
   public void actionPerformed(AnActionEvent e, DomModelTreeView treeView) {
-    if (treeView.getTree().getSelectedNode()instanceof BaseDomElementNode) {
-      final BaseDomElementNode selectedNode = (BaseDomElementNode)treeView.getTree().getSelectedNode();
+    final SimpleNode selectedNode = treeView.getTree().getSelectedNode();
 
-      final DomElement domElement = selectedNode.getDomElement();
+    if (selectedNode instanceof BaseDomElementNode) {
+
+      final DomElement domElement = ((BaseDomElementNode)selectedNode).getDomElement();
       new WriteCommandAction(domElement.getManager().getProject(), domElement.getXmlTag().getContainingFile()) {
         protected void run(final Result result) throws Throwable {
-
-          selectedNode.getDomElement().undefine();
+          domElement.undefine();
         }
       }.execute();
     }
   }
 
-
   public void update(AnActionEvent e, DomModelTreeView treeView) {
     final SimpleNode selectedNode = treeView.getTree().getSelectedNode();
+
+    if (selectedNode instanceof DomFileElementNode) {
+      e.getPresentation().setVisible(false);
+      return;
+    }
 
     boolean enabled = false;
     if (selectedNode instanceof BaseDomElementNode) {
@@ -60,7 +64,8 @@ public class DeleteDomElement extends BaseDomTreeAction {
     if (enabled) {
       final ElementPresentation presentation = ((BaseDomElementNode)selectedNode).getDomElement().getPresentation();
 
-      e.getPresentation().setText(removeString + " " +presentation.getTypeName() + (presentation.getElementName() == null ? "" : ": " + presentation.getElementName()));
+      e.getPresentation().setText(removeString + " " + presentation.getTypeName() +
+                                  (presentation.getElementName() == null ? "" : ": " + presentation.getElementName()));
     }
     else {
       e.getPresentation().setText(removeString);
