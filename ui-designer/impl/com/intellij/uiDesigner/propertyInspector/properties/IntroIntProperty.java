9460087c42127e018c7837df0c96248643f15e99
Dmitry Jemerov <yole@intellij.com>
2006-07-05 13:05:06 +0400

    exclude selectedIndex property from snapshot import

--- ui-designer/impl/com/intellij/uiDesigner/propertyInspector/properties/IntroIntProperty.java
+++ ui-designer/impl/com/intellij/uiDesigner/propertyInspector/properties/IntroIntProperty.java
@@ -5,8 +5,12 @@ import com.intellij.uiDesigner.propertyInspector.PropertyEditor;
 import com.intellij.uiDesigner.propertyInspector.PropertyRenderer;
 import com.intellij.uiDesigner.propertyInspector.editors.IntEditor;
 import com.intellij.uiDesigner.propertyInspector.renderers.LabelPropertyRenderer;
+import com.intellij.uiDesigner.snapShooter.SnapshotContext;
+import com.intellij.uiDesigner.radComponents.RadComponent;
+import com.intellij.uiDesigner.SwingProperties;
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.*;
 import java.lang.reflect.Method;
 
 /**
@@ -45,5 +49,13 @@ public final class IntroIntProperty extends IntrospectedProperty<Integer> {
       myEditor = new IntEditor(Integer.MIN_VALUE);
     }
     return myEditor;
+  }
+
+  @Override
+  public void importSnapshotValue(final SnapshotContext context, final JComponent component, final RadComponent radComponent) {
+    // exclude property from snapshot import to avoid exceptions because of not imported model 
+    if (!getName().equals(SwingProperties.SELECTED_INDEX)) {
+      super.importSnapshotValue(context, component, radComponent);
+    }
   }
 }
