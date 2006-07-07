89c26adff6bbbedcd389b217d2b035767a6adfd9
Maxim Mossienko <Maxim.Mossienko@jetbrains.com>
2006-07-07 17:15:55 +0400

    names of JSP files not shown in find usages results (IDEADEV-6883)

--- UsageView/src/com/intellij/usages/impl/rules/NonJavaFileGroupingRule.java
+++ UsageView/src/com/intellij/usages/impl/rules/NonJavaFileGroupingRule.java
@@ -18,6 +18,7 @@ package com.intellij.usages.impl.rules;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiJavaFile;
+import com.intellij.psi.jsp.JspFile;
 import com.intellij.usages.Usage;
 import com.intellij.usages.UsageGroup;
 
@@ -30,7 +31,7 @@ public class NonJavaFileGroupingRule extends FileGroupingRule {
     final FileUsageGroup usageGroup = (FileUsageGroup)super.groupUsage(usage);
     if (usageGroup != null) {
       final PsiFile psiFile = usageGroup.getPsiFile();
-      if (psiFile instanceof PsiJavaFile) {
+      if (psiFile instanceof PsiJavaFile && !(psiFile instanceof JspFile)) {
         return null;
       }
     }
