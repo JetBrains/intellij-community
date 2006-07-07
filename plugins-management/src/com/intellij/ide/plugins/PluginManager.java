ec1a7f8983c36d248bdfe8a287c3c9b724301783
Olesya Smirnova <Olesya.Smirnova@jetbrains.com>
2006-07-07 18:22:22 +0400

    load plugins outside of home dir

--- plugins-management/src/com/intellij/ide/plugins/PluginManager.java
+++ plugins-management/src/com/intellij/ide/plugins/PluginManager.java
@@ -343,7 +343,8 @@ public class PluginManager {
         if ("file".equals(protocol)) {
           final File file = new File(url.getFile());
           final String canonicalPath = file.getCanonicalPath();
-          if (!canonicalPath.startsWith(homePath) || canonicalPath.endsWith(".jar")) continue;
+          //if (!canonicalPath.startsWith(homePath) || canonicalPath.endsWith(".jar")) continue;
+          if (!canonicalPath.startsWith(homePath)) continue;
           final IdeaPluginDescriptorImpl pluginDescriptor = loadDescriptor(file);
           if (pluginDescriptor != null && !result.contains(pluginDescriptor)) {
             result.add(pluginDescriptor);
