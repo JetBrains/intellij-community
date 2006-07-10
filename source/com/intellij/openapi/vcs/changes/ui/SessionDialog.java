e8e0f1189cc55cb8727afb1398208c6d5ae854ee
Olesya Smirnova <Olesya.Smirnova@jetbrains.com>
2006-07-10 12:03:02 +0400

    update OK button

--- source/com/intellij/openapi/vcs/changes/ui/SessionDialog.java
+++ source/com/intellij/openapi/vcs/changes/ui/SessionDialog.java
@@ -33,8 +33,8 @@ public class SessionDialog extends DialogWrapper {
     myChanges = changes;
     myCommitMessage = commitMessage;
     setTitle(title);
-    updateButtons();
     init();
+    updateButtons();
   }
 
 
