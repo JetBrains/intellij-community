607473e50e177cf3d2a66a4b696f9cc673d9ba1b
Maxim Mossienko <Maxim.Mossienko@jetbrains.com>
2006-07-12 18:25:46 +0400

    fixing compilation

--- plugins/InspectionGadgets/src/com/siyeh/ig/BaseInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/BaseInspection.java
@@ -17,10 +17,11 @@ package com.siyeh.ig;
 
 import com.intellij.codeInspection.LocalInspectionTool;
 import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.codeInspection.InspectionManager;
+import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.*;
 import com.siyeh.InspectionGadgetsBundle;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
@@ -57,6 +58,141 @@ public abstract class BaseInspection extends LocalInspectionTool {
     @Nullable
     protected InspectionGadgetsFix buildFix(PsiElement location) {
         return null;
+    }
+
+    @Nullable
+    public ProblemDescriptor[] checkFile(PsiFile file,
+                                         InspectionManager manager,
+                                         boolean isOnTheFly) {
+        /*
+        if (telemetryEnabled) {
+          initializeTelemetryIfNecessary();
+          final long start = System.currentTimeMillis();
+          try {
+            return doCheckFile(file, manager, isOnTheFly);
+          }
+          finally {
+            final long end = System.currentTimeMillis();
+            final String displayName = getDisplayName();
+            listener.reportRun(displayName, end - start);
+          }
+        }
+        else {
+          return doCheckFile(file, manager, isOnTheFly);
+        }
+        */
+
+        return null;
+    }
+
+    @Nullable
+    protected ProblemDescriptor[] doCheckFile(PsiFile file,
+                                              InspectionManager manager,
+                                              boolean isOnTheFly) {
+        return super.checkFile(file, manager, isOnTheFly);
+    }
+
+    @Nullable
+    public ProblemDescriptor[] checkMethod(PsiMethod method,
+                                           InspectionManager manager,
+                                           boolean isOnTheFly) {
+        /*
+        if (telemetryEnabled) {
+          initializeTelemetryIfNecessary();
+          final long start = System.currentTimeMillis();
+          try {
+            return doCheckMethod(method, manager, isOnTheFly);
+          }
+          finally {
+            final long end = System.currentTimeMillis();
+            final String displayName = getDisplayName();
+            listener.reportRun(displayName, end - start);
+          }
+        }
+        else {
+          return doCheckMethod(method, manager, isOnTheFly);
+        }
+        */
+
+        return null;
+    }
+
+    @Nullable
+    protected ProblemDescriptor[] doCheckMethod(PsiMethod method,
+                                                InspectionManager manager,
+                                                boolean isOnTheFly) {
+        return super.checkMethod(method, manager, isOnTheFly);
+    }
+
+    @Nullable
+    public ProblemDescriptor[] checkClass(PsiClass aClass,
+                                          InspectionManager manager,
+                                          boolean isOnTheFly) {
+        /*
+        if (telemetryEnabled) {
+          initializeTelemetryIfNecessary();
+          final long start = System.currentTimeMillis();
+          try {
+            return doCheckClass(aClass, manager, isOnTheFly);
+          }
+          finally {
+            final long end = System.currentTimeMillis();
+            final String name = getDisplayName();
+            listener.reportRun(name, end - start);
+          }
+        }
+        else {
+          return doCheckClass(aClass, manager, isOnTheFly);
+        }
+        */
+
+        return null;
+    }
+
+    @Nullable
+    protected ProblemDescriptor[] doCheckClass(PsiClass aClass,
+                                               InspectionManager manager,
+                                               boolean isOnTheFly) {
+        return super.checkClass(aClass, manager, isOnTheFly);
+    }
+
+    @Nullable
+    public ProblemDescriptor[] checkField(PsiField field,
+                                          InspectionManager manager,
+                                          boolean isOnTheFly) {
+        /*
+        if (telemetryEnabled) {
+          initializeTelemetryIfNecessary();
+          final long start = System.currentTimeMillis();
+          try {
+            return doCheckField(field, manager, isOnTheFly);
+          }
+          finally {
+            final long end = System.currentTimeMillis();
+            final String displayName = getDisplayName();
+            listener.reportRun(displayName, end - start);
+          }
+        }
+        else {
+          return doCheckField(field, manager, isOnTheFly);
+        }
+        */
+        return null;
+    }
+
+    @Nullable
+    protected ProblemDescriptor[] doCheckField(PsiField field,
+                                               InspectionManager manager,
+                                               boolean isOnTheFly) {
+        return super.checkField(field, manager, isOnTheFly);
+    }
+
+    protected BaseInspectionVisitor createVisitor(
+            InspectionManager inspectionManager, boolean onTheFly) {
+        final BaseInspectionVisitor visitor = buildVisitor();
+        visitor.setOnTheFly(onTheFly);
+        visitor.setInspection(this);
+        return visitor;
     }
 
     @Nullable
