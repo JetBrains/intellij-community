1d36d9367b9df32243600658bc4366079b8d4e31
Sergey Vasiliev <Sergey.Vasiliev@jetbrains.com>
2006-07-14 16:40:00 +0400

    fixed IDEADEV-6895

--- dom/impl/src/com/intellij/util/xml/ui/actions/generate/GenerateDomElementAction.java
+++ dom/impl/src/com/intellij/util/xml/ui/actions/generate/GenerateDomElementAction.java
@@ -4,8 +4,10 @@ import com.intellij.codeInsight.CodeInsightActionHandler;
 import com.intellij.codeInsight.generation.actions.BaseGenerateAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.Result;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.psi.PsiFile;
 import com.intellij.util.xml.DomElement;
 import com.intellij.util.xml.DomElementNavigationProvider;
@@ -18,15 +20,18 @@ public class GenerateDomElementAction extends BaseGenerateAction {
   private GenerateDomElementProvider myGenerateProvider;
 
   public GenerateDomElementAction(final GenerateDomElementProvider generateProvider) {
-   super(new CodeInsightActionHandler() {
-      public void invoke(Project project, Editor editor, PsiFile file) {
-        final DomElement element = generateProvider.generate(project, editor, file);
-
-        generateProvider.navigate(element);
+    super(new CodeInsightActionHandler() {
+      public void invoke(final Project project, final Editor editor, final PsiFile file) {
+        new WriteCommandAction(project, file) {
+          protected void run(final Result result) throws Throwable {
+            final DomElement element = generateProvider.generate(project, editor, file);
+            generateProvider.navigate(element);
+          }
+        }.execute();
       }
 
       public boolean startInWriteAction() {
-        return true;
+        return false;
       }
     });
 
