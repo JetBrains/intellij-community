587fd346cdb77c4fb42961ded69db7451dcdc085
Maxim Mossienko <Maxim.Mossienko@jetbrains.com>
2006-07-14 21:08:19 +0400

    fixing extract include test

--- refactoring/tests/com/intellij/refactoring/MoveClassTest.java
+++ refactoring/tests/com/intellij/refactoring/MoveClassTest.java
@@ -68,7 +68,6 @@ public class MoveClassTest extends CodeInsightTestCase {
     String rootAfter = root + "/after";
     VirtualFile rootDir2 = LocalFileSystem.getInstance().findFileByPath(rootAfter.replace(File.separatorChar, '/'));
     myProject.getComponent(PostprocessReformattingAspect.class).doPostponedFormatting();
-    FileDocumentManager.getInstance().saveAllDocuments();
     IdeaTestUtil.assertDirectoriesEqual(rootDir2, rootDir, MultiFileTestCase.CVS_FILE_FILTER);
   }
 
