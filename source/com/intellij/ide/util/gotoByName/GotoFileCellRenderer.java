40c3fbaaedd8d201909f63b02dfd571cd9f15197
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2006-07-17 20:02:02 +0400

    http://www.jetbrains.net/jira/browse/IDEADEV-6692

--- source/com/intellij/ide/util/gotoByName/GotoFileCellRenderer.java
+++ source/com/intellij/ide/util/gotoByName/GotoFileCellRenderer.java
@@ -1,18 +1,22 @@
 package com.intellij.ide.util.gotoByName;
 
 import com.intellij.ide.util.PsiElementListCellRenderer;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.colors.EditorColorsManager;
 import com.intellij.openapi.editor.colors.EditorColorsScheme;
-import com.intellij.openapi.vfs.VfsUtil;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.util.ui.FilePathSplittingPolicy;
 
 import java.awt.*;
+import java.io.File;
 
 public class GotoFileCellRenderer extends PsiElementListCellRenderer<PsiFile>{
   private final int myMaxWidth;
+  private static final Logger LOG = Logger.getInstance("#com.intellij.ide.util.gotoByName.GotoFileCellRenderer");
 
   public GotoFileCellRenderer(int maxSize) {
     myMaxWidth = maxSize;
@@ -27,11 +31,33 @@ public class GotoFileCellRenderer extends PsiElementListCellRenderer<PsiFile>{
 
   protected String getContainerText(PsiElement element, String name) {
     PsiFile file = (PsiFile)element;
-    final VirtualFile virtualFile = file.getContainingDirectory().getVirtualFile();
-    final String prefix = name + "     ";
-
-    String path = FilePathSplittingPolicy.SPLIT_BY_SEPARATOR.getOptimalTextForComponent(prefix, VfsUtil.virtualToIoFile(virtualFile), this, myMaxWidth);
+    final PsiDirectory psiDirectory = file.getContainingDirectory();
+    LOG.assertTrue(psiDirectory != null);
+    final VirtualFile virtualFile = psiDirectory.getVirtualFile();
+    final String relativePath = getRelativePath(virtualFile, element.getProject());
+    if (relativePath == null) return "( " + File.separator + " )";
+    String path = FilePathSplittingPolicy.SPLIT_BY_SEPARATOR.getOptimalTextForComponent(name + "          ", new File(relativePath), this, myMaxWidth);
     return "(" + path + ")";
+  }
+
+  private static String getRelativePath(final VirtualFile virtualFile, final Project project) {
+    String url = virtualFile.getPresentableUrl();
+    if (project == null) {
+      return url;
+    }
+    else {
+      final VirtualFile projectFile = project.getProjectFile();
+      if (projectFile != null) {
+        //noinspection ConstantConditions
+        final String projectHomeUrl = projectFile.getParent().getPresentableUrl();
+        if (url.startsWith(projectHomeUrl)) {
+          final String cont = url.substring(projectHomeUrl.length());
+          if (cont.length() == 0) return null;
+          url = "..." + cont;
+        }
+      }
+    }
+    return url;
   }
 
   protected int getIconFlags() {
