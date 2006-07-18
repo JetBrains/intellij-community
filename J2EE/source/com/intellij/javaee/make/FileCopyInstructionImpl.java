9ba3ef3519968dd6dae7d61156174eb915e3b88f
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2006-07-18 12:43:26 +0400

    remove unused method

--- J2EE/source/com/intellij/javaee/make/FileCopyInstructionImpl.java
+++ J2EE/source/com/intellij/javaee/make/FileCopyInstructionImpl.java
@@ -55,18 +55,6 @@ public class FileCopyInstructionImpl extends BuildInstructionBase implements Fil
     }
   }
 
-  public void addFilesToRefresh(File outputDir, Set<File> filesToRefresh) {
-    if (myChangedSet == null) {
-      final File to = MakeUtil.canonicalRelativePath(outputDir, getOutputRelativePath());
-      filesToRefresh.add(to);
-    }
-    else {
-      for (FileCopyInstructionImpl singleFileCopyInstruction : myChangedSet) {
-        singleFileCopyInstruction.addFilesToRefresh(outputDir, filesToRefresh);
-      }
-    }
-  }
-
   public boolean accept(BuildInstructionVisitor visitor) throws Exception {
     return visitor.visitFileCopyInstruction(this);
   }
