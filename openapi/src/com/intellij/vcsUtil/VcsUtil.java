6ad64c0f728799e62dc0a740544fb0538bf5f92e
Michael Gerasimov <lloix@jetbrains.com>
2006-07-20 14:24:17 +0400

    do not react if there is no configured Starteam project

--- openapi/src/com/intellij/vcsUtil/VcsUtil.java
+++ openapi/src/com/intellij/vcsUtil/VcsUtil.java
@@ -55,6 +55,8 @@ import java.util.List;
 @SuppressWarnings({"UtilityClassWithoutPrivateConstructor"})
 public class VcsUtil
 {
+  public VcsUtil() {}
+
   public static void markFileAsUpToDate(VirtualFile file, Project project) {
     markAsUpToDate(project, file.isDirectory(), file.getPath());
     FileStatusManager.getInstance(project).fileStatusChanged(file);
