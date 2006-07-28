19aa9a6378f3411e2eb3077bb8c6bd9dcde3eec4
Peter Gromov <peter@jetbrains.com>
2006-07-28 16:16:59 +0400

    IDEADEV-7537 Invorrect xml validation for BMP Bean and more

--- dom/impl/src/com/intellij/util/xml/highlighting/DomElementsHighlightingUtil.java
+++ dom/impl/src/com/intellij/util/xml/highlighting/DomElementsHighlightingUtil.java
@@ -9,13 +9,14 @@ import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.xml.XmlChildRole;
 import com.intellij.psi.xml.XmlTag;
+import com.intellij.util.SmartList;
 import com.intellij.util.xml.DomElement;
-import com.intellij.util.xml.GenericValue;
 import com.intellij.util.xml.GenericAttributeValue;
+import com.intellij.util.xml.GenericValue;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -24,30 +25,40 @@ import java.util.List;
 public class DomElementsHighlightingUtil {
 
   public static List<ProblemDescriptor> createProblemDescriptors(final InspectionManager manager, DomElementProblemDescriptor problemDescriptor) {
-    List<ProblemDescriptor>  descritors = new ArrayList<ProblemDescriptor>();
+    List<ProblemDescriptor>  descritors = new SmartList<ProblemDescriptor>();
     final DomElement domElement = problemDescriptor.getDomElement();
 
     final PsiElement psiElement = getPsiElement(domElement);
     if (psiElement != null) {
-      descritors.add(manager.createProblemDescriptor(psiElement, problemDescriptor.getDescriptionTemplate(),
-                                             problemDescriptor.getFixes(), ProblemHighlightType.GENERIC_ERROR_OR_WARNING));
+      final XmlTag tag = PsiTreeUtil.getParentOfType(psiElement, XmlTag.class, false);
+      if (tag != null && tag.getSubTags().length > 0) {
+        addDescriptionsToTagEnds(tag, descritors, manager, problemDescriptor);
+      } else {
+        descritors.add(manager.createProblemDescriptor(psiElement, problemDescriptor.getDescriptionTemplate(),
+                                                       problemDescriptor.getFixes(), ProblemHighlightType.GENERIC_ERROR_OR_WARNING));
+      }
     } else {
       final XmlTag tag = getParentXmlTag(domElement);
       if (tag != null) {
-        final ASTNode startNode = XmlChildRole.START_TAG_NAME_FINDER.findChild(tag.getNode());
-        final ASTNode endNode = XmlChildRole.CLOSING_TAG_NAME_FINDER.findChild(tag.getNode());
-
-        descritors.add(manager.createProblemDescriptor(startNode.getPsi(), problemDescriptor.getDescriptionTemplate(), problemDescriptor.getFixes(),
-                                               ProblemHighlightType.GENERIC_ERROR_OR_WARNING));
-
-        if (endNode != null && !startNode.equals(endNode)) {
-          descritors.add(manager.createProblemDescriptor(endNode.getPsi(), problemDescriptor.getDescriptionTemplate(), problemDescriptor.getFixes(),
-                                                 ProblemHighlightType.GENERIC_ERROR_OR_WARNING));
-
-        }
+        addDescriptionsToTagEnds(tag, descritors, manager, problemDescriptor);
       }
     }
     return descritors;
+  }
+
+  private static void addDescriptionsToTagEnds(final XmlTag tag, final List<ProblemDescriptor> descritors, final InspectionManager manager,
+                                               final DomElementProblemDescriptor problemDescriptor) {
+    final ASTNode startNode = XmlChildRole.START_TAG_NAME_FINDER.findChild(tag.getNode());
+    final ASTNode endNode = XmlChildRole.CLOSING_TAG_NAME_FINDER.findChild(tag.getNode());
+
+    descritors.add(manager.createProblemDescriptor(startNode.getPsi(), problemDescriptor.getDescriptionTemplate(), problemDescriptor.getFixes(),
+                                                   ProblemHighlightType.GENERIC_ERROR_OR_WARNING));
+
+    if (endNode != null && !startNode.equals(endNode)) {
+      descritors.add(manager.createProblemDescriptor(endNode.getPsi(), problemDescriptor.getDescriptionTemplate(), problemDescriptor.getFixes(),
+                                                     ProblemHighlightType.GENERIC_ERROR_OR_WARNING));
+
+    }
   }
 
 
