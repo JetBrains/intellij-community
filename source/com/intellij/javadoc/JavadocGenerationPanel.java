606c33d43d923a68a9f279eca34af443affa3235
Dmitry Jemerov <yole@intellij.com>
2006-08-28 14:43:07 +0400

    fix restoring settings in "generate javadoc" (IDEADEV-8743)

--- source/com/intellij/javadoc/JavadocGenerationPanel.java
+++ source/com/intellij/javadoc/JavadocGenerationPanel.java
@@ -89,13 +89,13 @@ final class JavadocGenerationPanel extends JPanel {
       myScopeSlider.setValue(1);
     }
     else if (PsiKeyword.PROTECTED.equals(scope)) {
-      myScopeSlider.setValue(3);
+      myScopeSlider.setValue(2);
     }
     else if (PsiKeyword.PRIVATE.equals(scope)) {
       myScopeSlider.setValue(4);
     }
     else {
-      myScopeSlider.setValue(2);
+      myScopeSlider.setValue(3);
     }
     handleSlider();
   }
