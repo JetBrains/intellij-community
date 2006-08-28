4894b5c6fbda7c8095aa2365b83110162bce6739
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2006-08-28 22:19:34 +0400

    http://www.jetbrains.net/jira/browse/IDEADEV-8329

--- source/com/intellij/packageDependencies/ForwardDependenciesBuilder.java
+++ source/com/intellij/packageDependencies/ForwardDependenciesBuilder.java
@@ -6,6 +6,7 @@ import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
@@ -47,7 +48,7 @@ public class ForwardDependenciesBuilder extends DependenciesBuilder {
             indicator.setText(AnalysisScopeBundle.message("package.dependencies.progress.text"));
             final VirtualFile virtualFile = file.getVirtualFile();
             if (virtualFile != null) {
-              indicator.setText2(virtualFile.getPresentableUrl());
+              indicator.setText2(VfsUtil.calcRelativeToProjectPath(virtualFile, getProject()));
             }
             indicator.setFraction(((double)++ myFileCount) / myTotalFileCount);
           }
