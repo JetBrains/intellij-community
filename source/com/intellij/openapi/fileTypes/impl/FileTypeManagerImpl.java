e9e6a6f987cf832fa71e208a92f29ca79ddabb4b
Maxim Shafirov <max@jetbrains.com>
2006-08-29 23:04:44 +0400

    Do not identify directories as belonging to some file type no matter what pattern their names match.

--- source/com/intellij/openapi/fileTypes/impl/FileTypeManagerImpl.java
+++ source/com/intellij/openapi/fileTypes/impl/FileTypeManagerImpl.java
@@ -147,6 +147,8 @@ public class FileTypeManagerImpl extends FileTypeManagerEx implements NamedJDOME
 
   @NotNull
   public FileType getFileTypeByFile(VirtualFile file) {
+    if (file.isDirectory()) return StdFileTypes.UNKNOWN;
+
     // first let file recognize its type
     for (FakeFileType fileType : mySpecialFileTypes) {
       if (fileType.isMyFileType(file)) return fileType;
