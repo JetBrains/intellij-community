5980779c924035393c60bb2f2cb22cd574c1d259
greg <Gregory.Shrago@jetbrains.com>
2006-08-29 18:58:14 +0400

    support units in libraries

--- dom/openapi/src/com/intellij/util/xml/AbstractConvertContext.java
+++ dom/openapi/src/com/intellij/util/xml/AbstractConvertContext.java
@@ -4,10 +4,12 @@
 package com.intellij.util.xml;
 
 import com.intellij.openapi.module.Module;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.xml.XmlFile;
 import com.intellij.psi.xml.XmlTag;
+import com.intellij.psi.xml.XmlElement;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -22,11 +24,33 @@ public abstract class AbstractConvertContext extends ConvertContext {
 
     final Module module = getModule();
     final PsiClass aClass;
-    if (module != null) {
-      aClass = file.getManager().findClass(name, GlobalSearchScope.moduleWithDependenciesAndLibrariesScope(module));
+    final XmlElement element = getInvocationElement().getXmlElement();
+    if (module == null && element != null && getPsiManager().isInProject(element)) {
+      final GlobalSearchScope baseScope = GlobalSearchScope.allScope(getPsiManager().getProject());
+      aClass = getPsiManager().findClass(name, new GlobalSearchScope() {
+
+        public boolean contains(VirtualFile file) {
+          return baseScope.contains(file);
+        }
+
+        public int compare(VirtualFile file1, VirtualFile file2) {
+          return baseScope.compare(file1, file2);
+        }
+
+        public boolean isSearchInModuleContent(Module aModule) {
+          return false;
+        }
+
+        public boolean isSearchInLibraries() {
+          return true;
+        }
+      });
+    }
+    else if (module != null) {
+      aClass = getPsiManager().findClass(name, GlobalSearchScope.moduleWithDependenciesAndLibrariesScope(module));
     }
     else {
-      aClass = file.getManager().findClass(name, file.getResolveScope());
+      aClass = getPsiManager().findClass(name, file.getResolveScope());
     }
     if (aClass != null) {
       assert aClass.isValid() : name;
