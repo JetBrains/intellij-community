63dee2c3bc21d09aefa2d28b7a9282fe87de858d
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2006-08-30 14:40:11 +0400

    fix dimensions

--- ui/openapi/com/intellij/openapi/ui/MasterDetailsComponent.java
+++ ui/openapi/com/intellij/openapi/ui/MasterDetailsComponent.java
@@ -127,7 +127,7 @@ public abstract class MasterDetailsComponent implements Configurable, JDOMExtern
     myScrollPane.setMaximumSize(new Dimension(150, -1));
     final JPanel panel = new JPanel(new BorderLayout()) {
       public Dimension getPreferredSize() {
-        return new Dimension(600, 800);
+        return new Dimension(800, 600);
       }
     };
     panel.add(myWholePanel, BorderLayout.CENTER);
