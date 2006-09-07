a9c40503045458a4153c5c320b972a329a3c4de9
Alexey Kudravtsev <cdr@jetbrains.com>
2006-09-07 13:55:38 +0400

    diagnostic

--- source/com/intellij/openapi/editor/actions/moveUpDown/LineRange.java
+++ source/com/intellij/openapi/editor/actions/moveUpDown/LineRange.java
@@ -2,9 +2,11 @@ package com.intellij.openapi.editor.actions.moveUpDown;
 
 import com.intellij.psi.PsiElement;
 import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.diagnostic.Logger;
 import org.jetbrains.annotations.NonNls;
 
 class LineRange {
+  private static final Logger LOG = Logger.getInstance("#com.intellij.openapi.editor.actions.moveUpDown.LineRange");
   final int startLine;
   final int endLine;
 
@@ -14,10 +16,13 @@ class LineRange {
   public LineRange(final int startLine, final int endLine) {
     this.startLine = startLine;
     this.endLine = endLine;
+    if (startLine > endLine) {
+      LOG.error("start > end: start=" + startLine+"; end="+endLine);
+    }
   }
   public LineRange(PsiElement startElement, PsiElement endElement, Document document) {
-    startLine = document.getLineNumber(startElement.getTextRange().getStartOffset());
-    endLine = document.getLineNumber(endElement.getTextRange().getEndOffset()) +1;
+    this(document.getLineNumber(startElement.getTextRange().getStartOffset()),
+         document.getLineNumber(endElement.getTextRange().getEndOffset()) + 1);
   }
 
   @NonNls
