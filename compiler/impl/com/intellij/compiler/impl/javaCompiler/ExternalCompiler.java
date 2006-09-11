5d33180ad5d3c3a6cccfb8edc2fa3489b6def99d
Eugene Zhuravlev <jeka@jetbrains.com>
2006-09-11 17:07:27 +0400

    use StringBuilder from the pool, don't create it each time

--- compiler/impl/com/intellij/compiler/impl/javaCompiler/ExternalCompiler.java
+++ compiler/impl/com/intellij/compiler/impl/javaCompiler/ExternalCompiler.java
@@ -2,10 +2,11 @@ package com.intellij.compiler.impl.javaCompiler;
 
 import com.intellij.openapi.compiler.CompileContext;
 import com.intellij.openapi.diagnostic.Logger;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.util.StringBuilderSpinAllocator;
 import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
 
-import java.io.*;
+import java.io.IOException;
 
 public abstract class ExternalCompiler implements BackendCompiler {
   private static final Logger LOG = Logger.getInstance("com.intellij.compiler.impl.javaCompiler.ExternalCompiler");
@@ -18,13 +19,18 @@ public abstract class ExternalCompiler implements BackendCompiler {
   public Process launchProcess(final ModuleChunk chunk, final String outputDir, final CompileContext compileContext) throws IOException {
     final String[] commands = createStartupCommand(chunk, compileContext, outputDir);
 
-    @NonNls final StringBuffer buf = new StringBuffer(16 * commands.length);
-    buf.append("Running compiler: ");
-    for (final String command : commands) {
-      buf.append(" ").append(command);
-    }
+    @NonNls final StringBuilder buf = StringBuilderSpinAllocator.alloc();
+    try {
+      buf.append("Running compiler: ");
+      for (final String command : commands) {
+        buf.append(" ").append(command);
+      }
 
-    LOG.info(buf.toString());
+      LOG.info(buf.toString());
+    }
+    finally {
+      StringBuilderSpinAllocator.dispose(buf);
+    }
 
     return Runtime.getRuntime().exec(commands);
   }
