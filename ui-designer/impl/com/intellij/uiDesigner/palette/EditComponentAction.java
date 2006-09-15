f400755494b902ed0bddcaca67bc491f78a66ee8
Dmitry Jemerov <yole@intellij.com>
2006-09-15 19:11:39 +0400

    when editing component, replace it in correct group (IDEADEV-6958)

--- ui-designer/impl/com/intellij/uiDesigner/palette/EditComponentAction.java
+++ ui-designer/impl/com/intellij/uiDesigner/palette/EditComponentAction.java
@@ -9,6 +9,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataConstants;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.WindowManager;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.uiDesigner.UIDesignerBundle;
 
 import java.awt.*;
@@ -17,11 +18,12 @@ import java.awt.*;
  * @author yole
  */
 public class EditComponentAction extends AnAction {
+  private static final Logger LOG = Logger.getInstance("#com.intellij.uiDesigner.palette.EditComponentAction");
+
   public void actionPerformed(AnActionEvent e) {
     Project project = (Project)e.getDataContext().getData(DataConstants.PROJECT);
     ComponentItem selectedItem = (ComponentItem) e.getDataContext().getData(ComponentItem.class.getName());
-    GroupItem groupItem = (GroupItem) e.getDataContext().getData(GroupItem.class.getName());
-    if (project == null || selectedItem == null || groupItem == null || selectedItem.isAnyComponent()) return;
+    if (project == null || selectedItem == null || selectedItem.isAnyComponent()) return;
 
     final ComponentItem itemToBeEdited = selectedItem.clone(); /*"Cancel" should work, so we need edit copy*/
     Window parentWindow = WindowManager.getInstance().suggestParentWindow(project);
@@ -32,13 +34,18 @@ public class EditComponentAction extends AnAction {
       return;
     }
 
+    GroupItem groupItem = null;
     Palette palette = Palette.getInstance(project);
     // If the itemToBeAdded is already in palette do nothing
     for(GroupItem group: palette.getGroups()) {
       if (group.containsItemCopy(selectedItem, itemToBeEdited.getClassName())){
         return;
       }
+      if (group.containsItemClass(selectedItem.getClassName())) {
+        groupItem = group;
+      }
     }
+    LOG.assertTrue(groupItem != null);        
 
     palette.replaceItem(groupItem, selectedItem, itemToBeEdited);
     palette.fireGroupsChanged();
