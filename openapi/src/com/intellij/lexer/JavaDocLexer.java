c20deae70a0d11913e1632048caae47f8874cb7e
Maxim Shafirov <max@jetbrains.com>
2006-09-17 17:40:37 +0400

    IDEADEV-9262

--- openapi/src/com/intellij/lexer/JavaDocLexer.java
+++ openapi/src/com/intellij/lexer/JavaDocLexer.java
@@ -141,8 +141,11 @@ public class JavaDocLexer extends MergingLexerAdapter {
         }
 
         final int state = myFlex.yystate();
-        if (state == _JavaDocLexer.COMMENT_DATA
-            || myTokenEndOffset < myBufferEndOffset && (myBuffer[myTokenEndOffset] == '@' || myBuffer[myTokenEndOffset] == '{')) {
+        if (state == _JavaDocLexer.COMMENT_DATA ||
+            myTokenEndOffset < myBufferEndOffset && (myBuffer[myTokenEndOffset] == '@' ||
+                                                     myBuffer[myTokenEndOffset] == '{' ||
+                                                     myBuffer[myTokenEndOffset] == '\"' ||
+                                                     myBuffer[myTokenEndOffset] == '<')) {
           myFlex.yybegin(_JavaDocLexer.COMMENT_DATA_START);
         }
 
