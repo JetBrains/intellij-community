3ba3ce69bd216744590d75d55e83833664e9ef7f
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2006-09-19 21:11:36 +0400

    unused import inspection description clarification

--- inspections/impl/com/intellij/codeInspection/unusedImport/UnusedImportLocalInspection.java
+++ inspections/impl/com/intellij/codeInspection/unusedImport/UnusedImportLocalInspection.java
@@ -4,9 +4,11 @@
 
 package com.intellij.codeInspection.unusedImport;
 
+import com.intellij.codeInsight.daemon.GroupNames;
 import com.intellij.codeInspection.InspectionsBundle;
 import com.intellij.codeInspection.ex.UnfairLocalInspectionTool;
 import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * User: anna
@@ -16,14 +18,17 @@ public class UnusedImportLocalInspection extends UnfairLocalInspectionTool {
   @NonNls public static final String SHORT_NAME = "UNUSED_IMPORT";
   public static final String DISPLAY_NAME = InspectionsBundle.message("unused.import");
 
+  @NotNull
   public String getGroupDisplayName() {
-    return "";
+    return GroupNames.IMPORTS_GROUP_NAME;
   }
 
+  @NotNull
   public String getDisplayName() {
     return DISPLAY_NAME;
   }
 
+  @NotNull
   @NonNls
   public String getShortName() {
     return SHORT_NAME;
