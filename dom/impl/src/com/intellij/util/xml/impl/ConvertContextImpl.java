4ff686aa6a38acd04b047a8ad551dd74c868f72f
greg <Gregory.Shrago@jetbrains.com>
2006-09-26 16:03:09 +0400

    integrate

--- dom/impl/src/com/intellij/util/xml/impl/ConvertContextImpl.java
+++ dom/impl/src/com/intellij/util/xml/impl/ConvertContextImpl.java
@@ -6,6 +6,7 @@ package com.intellij.util.xml.impl;
 import com.intellij.psi.PsiManager;
 import com.intellij.util.xml.AbstractConvertContext;
 import com.intellij.util.xml.DomElement;
+import com.intellij.openapi.module.Module;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -25,5 +26,13 @@ public class ConvertContextImpl extends AbstractConvertContext {
 
   public PsiManager getPsiManager() {
     return myHandler.getFile().getManager();
+  }
+
+  public Module getModule() {
+    final DomElement domElement = getInvocationElement();
+    if (domElement.getManager().isMockElement(domElement)) {
+      return getInvocationElement().getModule();
+    }
+    return super.getModule();
   }
 }
