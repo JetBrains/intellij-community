b59eeb029d93492b0eeb4d6b5886ec94e863407f
Maxim Shafirov <max@jetbrains.com>
2006-09-30 01:00:31 +0400

    A bit of dead-code.

--- refactoring/impl/com/intellij/refactoring/util/classMembers/MemberInfoTooltipManager.java
+++ refactoring/impl/com/intellij/refactoring/util/classMembers/MemberInfoTooltipManager.java
@@ -1,41 +1,64 @@
 package com.intellij.refactoring.util.classMembers;
 
+
+
 import com.intellij.util.containers.HashMap;
 
+
+
 /**
+
  * @author dsl
+
  */
+
 public class MemberInfoTooltipManager {
-  private final HashMap myTooltips;
+
+  private final HashMap<MemberInfo, String> myTooltips = new HashMap<MemberInfo, String>();
+
   private final TooltipProvider myProvider;
 
+
+
   public interface TooltipProvider {
+
     String getTooltip(MemberInfo memberInfo);
+
   }
 
-  public static class DefaultTooltipProvider implements TooltipProvider {
-    public static final DefaultTooltipProvider INSTANCE = new DefaultTooltipProvider();
-    private DefaultTooltipProvider() {}
-    public String getTooltip(MemberInfo memberInfo) {
-      return null;
-    }
-  }
+
 
   public MemberInfoTooltipManager(TooltipProvider provider) {
+
     myProvider = provider;
-    myTooltips = new HashMap();
+
   }
+
+
 
   public void invalidate() {
+
     myTooltips.clear();
+
   }
 
+
+
   public String getTooltip(MemberInfo member) {
+
     if(myTooltips.keySet().contains(member)) {
-      return (String) myTooltips.get(member);
+
+      return myTooltips.get(member);
+
     }
+
     String tooltip = myProvider.getTooltip(member);
+
     myTooltips.put(member, tooltip);
+
     return tooltip;
+
   }
+
 }
+
