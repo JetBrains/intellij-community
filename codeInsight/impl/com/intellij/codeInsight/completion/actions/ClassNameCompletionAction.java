0d5f6c45f87cda20880c2991a45709a4c1a6f32d
Maxim Shafirov <max@jetbrains.com>
2006-09-29 16:17:46 +0400

    NPE

--- codeInsight/impl/com/intellij/codeInsight/completion/actions/ClassNameCompletionAction.java
+++ codeInsight/impl/com/intellij/codeInsight/completion/actions/ClassNameCompletionAction.java
@@ -1,18 +1,18 @@
 package com.intellij.codeInsight.completion.actions;
 
 import com.intellij.codeInsight.CodeInsightActionHandler;
-import com.intellij.codeInsight.lookup.Lookup;
 import com.intellij.codeInsight.actions.BaseCodeInsightAction;
 import com.intellij.codeInsight.completion.ClassNameCompletionHandler;
+import com.intellij.codeInsight.lookup.Lookup;
+import com.intellij.codeInsight.lookup.LookupItem;
 import com.intellij.featureStatistics.FeatureUsageTracker;
+import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DataContext;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiJavaFile;
 import com.intellij.psi.PsiCodeFragment;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiJavaFile;
 import com.intellij.psi.xml.XmlFile;
 
 /**
@@ -33,7 +33,11 @@ public class ClassNameCompletionAction extends BaseCodeInsightAction {
   }
 
   public boolean isValidForLookup(Lookup lookup, DataContext context) {
-    final Object o = lookup.getCurrentItem().getObject();
+    if (lookup == null) return false;
+    final LookupItem item = lookup.getCurrentItem();
+    if (item == null) return false;
+    
+    final Object o = item.getObject();
     return o instanceof PsiElement && isValidForFile(null, null, ((PsiElement)o).getContainingFile());    
   }
 
