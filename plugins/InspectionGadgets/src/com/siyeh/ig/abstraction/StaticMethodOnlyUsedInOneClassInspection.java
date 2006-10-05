f0d2924f9150314baea406499e05484b3b556010
Bas Leijdekkers <basleijdekkers@gmail.com>
2006-10-06 01:55:18 +0400

    cleanup

--- plugins/InspectionGadgets/src/com/siyeh/ig/abstraction/StaticMethodOnlyUsedInOneClassInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/abstraction/StaticMethodOnlyUsedInOneClassInspection.java
@@ -1,7 +1,17 @@
-/**
- * (c) 2006 Carp Technologies BV
- * Brouwerijstraat 1, 7523XC Enschede
- * Created: 20060329, 11:55:52 AM
+/*
+ * Copyright 2006 Bas Leijdekkers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.siyeh.ig.abstraction;
 
@@ -76,7 +86,7 @@ public class StaticMethodOnlyUsedInOneClassInspection
                     "static.method.only.used.in.one.class.quickfix");
         }
 
-        protected void doFix(final Project project,
+        protected void doFix(@NotNull final Project project,
                              ProblemDescriptor descriptor)
                 throws IncorrectOperationException {
             final PsiElement location = descriptor.getPsiElement();
