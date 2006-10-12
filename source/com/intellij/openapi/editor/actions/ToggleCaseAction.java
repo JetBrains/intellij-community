f11fc64d6aa08257ca3ada42cf1814f157d39a3c
Maxim Shafirov <max@jetbrains.com>
2006-10-13 00:25:10 +0400

    IDEADEV-9665: smart toggle case rolled back. It doesn't seem people like it.

--- source/com/intellij/openapi/editor/actions/ToggleCaseAction.java
+++ source/com/intellij/openapi/editor/actions/ToggleCaseAction.java
@@ -22,7 +22,7 @@ public class ToggleCaseAction extends EditorAction {
   private static class Handler extends EditorWriteActionHandler {
     public void executeWriteAction(Editor editor, DataContext dataContext) {
       if (!editor.getSelectionModel().hasSelection()) {
-        editor.getSelectionModel().selectWordAtCaret(false);
+        editor.getSelectionModel().selectWordAtCaret(true);
       }
 
       int startOffset = editor.getSelectionModel().getSelectionStart();
@@ -35,9 +35,8 @@ public class ToggleCaseAction extends EditorAction {
         if (text.equals(builder.toString())) {
           toCase(builder, text, false);
         }
-        final String finalText = builder.toString();
-        editor.getDocument().replaceString(startOffset, endOffset, finalText);
-        editor.getSelectionModel().setSelection(startOffset, startOffset + finalText.length());
+        editor.getDocument().replaceString(startOffset, endOffset, builder.toString());
+        editor.getSelectionModel().setSelection(startOffset, endOffset);
       }
       finally {
         StringBuilderSpinAllocator.dispose(builder);
@@ -46,84 +45,15 @@ public class ToggleCaseAction extends EditorAction {
 
     private static void toCase(final StringBuilder builder, final String text, final boolean lower ) {
       builder.setLength(0);
-      if (isIdentifier(text)) {
-        if (lower) {
-          if (!isUnderscored(text)) {
-            builder.append(text);
-            return;
-          }
-
-          boolean prevIsUnderscore = false;
-          for (int i = 0; i < text.length(); i++) {
-            char c = text.charAt(i);
-            if (c == '_' && i > 0) {
-              prevIsUnderscore = true;
-              continue;
-            }
-
-            if (prevIsUnderscore) {
-              builder.append(Character.toUpperCase(c));
-              prevIsUnderscore = false;
-            }
-            else {
-              builder.append(Character.toLowerCase(c));
-            }
-          }
-        }
-        else {
-          if (!isCamelCased(text)) {
-            builder.append(text);
-            return;
-          }
-
-          boolean prevWasLower = false;
-          for (int i = 0; i < text.length(); i++) {
-            char c = text.charAt(i);
-            if (Character.isUpperCase(c)) {
-              if (prevWasLower) {
-                builder.append('_');
-                prevWasLower = false;
-              }
-            }
-            else if (Character.isLowerCase(c)) {
-              prevWasLower = true;
-            }
-            builder.append(Character.toUpperCase(c));
-          }
-        }
-      }
-      else {
-        boolean prevIsSlash = false;
-        for( int i = 0; i < text.length(); ++i) {
-          char c = text.charAt(i);
-          if( !prevIsSlash ) {
-            c = (lower) ? Character.toLowerCase(c) : Character.toUpperCase(c);
-          }
-          prevIsSlash = c == '\\';
-          builder.append(c);
-        }
-      }
-    }
-
-    private static boolean isUnderscored(String text) {
+      boolean prevIsSlash = false;
       for( int i = 0; i < text.length(); ++i) {
         char c = text.charAt(i);
-        if (c != '_' && !Character.isDigit(c) && !Character.isUpperCase(c)) return false;
+        if( !prevIsSlash ) {
+          c = (lower) ? Character.toLowerCase(c) : Character.toUpperCase(c);
+        }
+        prevIsSlash = c == '\\';
+        builder.append(c);
       }
-      return true;
-    }
-
-    private static boolean isCamelCased(String text) {
-      return text.indexOf('_') <= 0;
-    }
-
-    private static boolean isIdentifier(String text) {
-      if (!Character.isLetter(text.charAt(0))) return false;
-      for (int i = 0; i < text.length(); i++) {
-        if (!Character.isJavaIdentifierPart(text.charAt(i))) return false;
-      }
-
-      return true;
     }
   }
 }
