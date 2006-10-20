bf02f43999a07a1264fdcf35ef7269a72f03f9f1
Alexander Chernikov <Alexander.Chernikov@jetbrains.com>
2006-10-20 19:19:54 +0400

    IDEADEV-10792: map topics from the parent task to the respective dialogs.

---
+++ source/com/intellij/codeEditor/printing/HelpID.java
@@ -0,0 +1,15 @@
+package com.intellij.codeEditor.printing;
+
+import org.jetbrains.annotations.NonNls;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Alexander.Chernikov
+ * Date: 20.10.2006
+ * Time: 18:55:05
+ * To change this template use File | Settings | File Templates.
+ */
+public interface HelpID {
+  @NonNls String EXPORT_TO_HTML = "reference.file.exportToHtml";
+  @NonNls String PRINT = "reference.file.print";
+}
