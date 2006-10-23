d0b1d25f66c240b3afb6b2e10946f0590e31b744
Anton Makeev <Anton.Makeev@jetbrains.com>
2006-10-23 17:42:14 +0400

    my first test

--- LocalVcs/test/com/intellij/localvcs/LocalVcsTest.java
+++ LocalVcs/test/com/intellij/localvcs/LocalVcsTest.java
@@ -1,74 +1,49 @@
 package com.intellij.localvcs;
 
+import java.io.File;
+
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
 public class LocalVcsTest extends TempDirTestCase {
+  private File myFile;
   private LocalVcs myVcs;
 
   @Before
   public void setUp() {
-    myVcs = new LocalVcs(getTempDir());
-  }
-
-  @Test
-  public void testEmptyWorkingRevision() {
-    assertEquals(0, myVcs.getWorkingRevision().size());
-  }
-
-  @Test
-  public void testFiles() {
-    createFile("file");
-
-    DirectoryRevision rev = myVcs.getWorkingRevision();
-    assertEquals(1, rev.size());
-
-    Revision fileRev = rev.get(0);
-    assertEquals(WorkingFileRevision.class, fileRev.getClass());
-    assertEquals("file", fileRev.getName());
-  }
-
-  @Test
-  public void testDirs() {
-    createDir("dir");
-    createFile("dir/file");
-
-    DirectoryRevision rev = myVcs.getWorkingRevision();
-    assertEquals(1, rev.size());
-
-    assertEquals(WorkingDirectoryRevision.class, rev.get(0).getClass());
-
-    DirectoryRevision dirRev = ((DirectoryRevision)rev.get(0));
-    assertEquals("dir", dirRev.getName());
-
-    assertEquals(1, dirRev.size());
-
-    assertEquals(WorkingFileRevision.class, dirRev.get(0).getClass());
-    assertEquals("file", dirRev.get(0).getName());
+    myFile = createFile("file");
+    myVcs = new LocalVcs();
   }
 
   @Test
   public void testCommitting() {
-    assertTrue(myVcs.getRevisions().isEmpty());
+    myVcs.addFile(myFile, "");
+    assertFalse(myVcs.hasFile(myFile));
 
     myVcs.commit();
-
-    assertEquals(1, myVcs.getRevisions().size());
-    assertEquals(TEMP_DIR_NAME,
-                 myVcs.getRevisions().get(0).getName());
+    assertTrue(myVcs.hasFile(myFile));
   }
 
   @Test
-  public void testCommittingFiles() {
-    createFile("file");
+  public void testClearingUncommittedFiles() {
+    assertFalse(myVcs.isDirty());
+
+    myVcs.addFile(myFile, "");
+    assertTrue(myVcs.isDirty());
 
     myVcs.commit();
-    deleteFile("file");
 
-    StoredDirectoryRevision rev = myVcs.getRevisions().get(0);
+    assertFalse(myVcs.isDirty());
+  }
 
-    assertEquals(1, rev.size());
-    assertEquals("file", rev.get(0).getName());
+  @Test
+  public void testContent() {
+    myVcs.addFile(myFile, "content");
+    myVcs.commit();
+
+    myVcs.addFile(myFile, "new content");
+
+    assertEquals("content", myVcs.getFileContent(myFile));
   }
 }
