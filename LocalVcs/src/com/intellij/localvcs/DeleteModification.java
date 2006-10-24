09b5bdf49c66bfb77af3de1f397a53834edd42a0
Anton Makeev <Anton.Makeev@jetbrains.com>
2006-10-24 18:01:42 +0400

    reverting implemented

--- LocalVcs/src/com/intellij/localvcs/DeleteModification.java
+++ LocalVcs/src/com/intellij/localvcs/DeleteModification.java
@@ -2,15 +2,18 @@ package com.intellij.localvcs;
 
 class DeleteModification implements Modification {
   private String myName;
+  private String myContent;
 
   public DeleteModification(String name) {
     myName = name;
   }
 
   public void applyTo(Snapshot snapshot) {
+    myContent = snapshot.getFileRevision(myName).getContent();
     snapshot.doDeleteFile(myName);
   }
 
   public void revertOn(Snapshot snapshot) {
+    snapshot.doAddFile(myName, myContent);
   }
 }
