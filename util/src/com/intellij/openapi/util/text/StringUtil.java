0299e8d114b64d07dfd8fdd89d166f62316dd01c
Maxim Mossienko <Maxim.Mossienko@jetbrains.com>
2006-11-02 20:42:43 +0300

    Correctly fixed test

--- util/src/com/intellij/openapi/util/text/StringUtil.java
+++ util/src/com/intellij/openapi/util/text/StringUtil.java
@@ -534,7 +534,9 @@ public class StringUtil {
   @NotNull public static String capitalize(@NotNull String s) {
     if (s.length() == 0) return s;
     if (s.length() == 1) return s.toUpperCase();
-    if (s.length() > 1 && Character.isUpperCase(s.charAt(1))) return s;
+
+    // Per JavaBeans spec, Introspector.decaptalize
+    if (s.length() > 1 && Character.isUpperCase(s.charAt(0)) && Character.isUpperCase(s.charAt(1))) return s;
     return Character.toUpperCase(s.charAt(0)) + s.substring(1);
   }
 
