e6c892d99e44867fc59bed588c18134af38778c8
greg <Gregory.Shrago@jetbrains.com>
2006-11-03 13:24:52 +0300

    datasource JDBC types support primitive types support on schema import default column java type modification

--- J2EE/source/com/intellij/javaee/module/view/ComboBoxCellEditor.java
+++ J2EE/source/com/intellij/javaee/module/view/ComboBoxCellEditor.java
@@ -25,7 +25,7 @@ public abstract class ComboBoxCellEditor extends DefaultCellEditor {
   }
 
   public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
-    String servletName = (String)value;
+    String currentValue = (String)value;
     final JComboBox component = (JComboBox)super.getTableCellEditorComponent(table, value, isSelected, row, column);
     component.setBorder(null);
     component.removeAllItems();
@@ -34,16 +34,16 @@ public abstract class ComboBoxCellEditor extends DefaultCellEditor {
     for (int i = 0; i < items.size(); i++) {
       final String item = items.get(i);
       component.addItem(item);
-      if (Comparing.equal(item, servletName)) {
+      if (Comparing.equal(item, currentValue)) {
         selected = i;
       }
     }
     if (selected == -1) {
       component.setEditable(true);
-      component.setSelectedItem(servletName);
+      component.setSelectedItem(currentValue);
       component.setEditable(false);
     } else {
-      component.setSelectedItem(selected);
+      component.setSelectedIndex(selected);
     }
     component.setEditable(isComboboxEditable());
     return component;
