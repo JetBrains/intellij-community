52be6ea0d8e683a3271b1f6ca04c1c26909b5401
Dmitry Jemerov <yole@intellij.com>
2006-11-14 18:40:10 +0300

    avoid running UIManager static initializer

--- UIDesignerCore/src/com/intellij/uiDesigner/compiler/ColorPropertyCodeGenerator.java
+++ UIDesignerCore/src/com/intellij/uiDesigner/compiler/ColorPropertyCodeGenerator.java
@@ -15,12 +15,11 @@
  */
 package com.intellij.uiDesigner.compiler;
 
+import com.intellij.uiDesigner.lw.ColorDescriptor;
+import org.objectweb.asm.Type;
 import org.objectweb.asm.commons.GeneratorAdapter;
 import org.objectweb.asm.commons.Method;
-import org.objectweb.asm.Type;
-import com.intellij.uiDesigner.lw.ColorDescriptor;
 
-import javax.swing.*;
 import java.awt.*;
 
 /**
@@ -30,7 +29,7 @@ import java.awt.*;
 public class ColorPropertyCodeGenerator extends PropertyCodeGenerator {
   private static final Type ourColorType = Type.getType(Color.class);
   private static final Type ourObjectType = Type.getType(Object.class);
-  private static final Type ourUIManagerType = Type.getType(UIManager.class);
+  private static final Type ourUIManagerType = Type.getType("Ljavax/swing/UIManager;");
   private static final Type ourSystemColorType = Type.getType(SystemColor.class);
 
   private static final Method ourInitMethod = Method.getMethod("void <init>(int)");
