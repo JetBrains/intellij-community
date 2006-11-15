ccd187f62bef43ac3cc8aa5641847149ce9534ce
Eugene Zhuravlev <jeka@jetbrains.com>
2006-11-15 21:07:29 +0300

    fix NPE

--- debugger/impl/com/intellij/debugger/ui/impl/watch/InplaceEditor.java
+++ debugger/impl/com/intellij/debugger/ui/impl/watch/InplaceEditor.java
@@ -11,6 +11,7 @@ import com.intellij.debugger.DebuggerInvocationUtil;
 import com.intellij.execution.ExecutionManager;
 import com.intellij.execution.ui.RunContentDescriptor;
 import com.intellij.execution.ui.RunContentListener;
+import com.intellij.execution.ui.RunContentManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
@@ -20,43 +21,14 @@ import javax.swing.plaf.basic.ComboPopup;
 import javax.swing.tree.TreePath;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 
 public abstract class InplaceEditor implements AWTEventListener{
   private static final Logger LOG = Logger.getInstance("#com.intellij.debugger.ui.impl.watch.InplaceEditor");
   private final DebuggerTreeNodeImpl myNode;
   private JComponent myInplaceEditorComponent;
+  private final java.util.List<Runnable> myRemoveActions = new ArrayList<Runnable>();
 
-  private ComponentAdapter myComponentListener = new ComponentAdapter() {
-    public void componentResized(ComponentEvent e) {
-      DebuggerInvocationUtil.invokeLater(getProject(), new Runnable() {
-        public void run() {
-          if (!isShown()) {
-            return;
-          }
-          JTree tree = myNode.getTree();
-          JLayeredPane layeredPane = tree.getRootPane().getLayeredPane();
-          Rectangle bounds = getEditorBounds();
-          Point layeredPanePoint=SwingUtilities.convertPoint(tree, bounds.x, bounds.y,layeredPane);
-          myInplaceEditorComponent.setBounds(layeredPanePoint.x,layeredPanePoint.y, bounds.width, bounds.height);
-          myInplaceEditorComponent.revalidate();
-        }
-      });
-    }
-
-    public void componentHidden(ComponentEvent e) {
-      cancelEditing();
-    }
-  };
-
-  private RunContentListener myRunContentListener = new RunContentListener() {
-    public void contentSelected(RunContentDescriptor descriptor) {
-      cancelEditing();
-    }
-
-    public void contentRemoved(RunContentDescriptor descriptor) {
-      cancelEditing();
-    }
-  };
 
   protected abstract JComponent createInplaceEditorComponent();
 
@@ -78,21 +50,14 @@ public abstract class InplaceEditor implements AWTEventListener{
     if (!isShown()) {
       return;
     }
-    //ListenerUtil.removeKeyListener(getPreferredFocusedComponent(), this);
-    //ListenerUtil.removeKeyListener(myInplaceEditorComponent, this);
-    Toolkit.getDefaultToolkit().removeAWTEventListener(this);
-    ExecutionManager.getInstance(getProject()).getContentManager().removeRunContentListener(myRunContentListener);
-
-    DebuggerTree tree = myNode.getTree();
-    JRootPane rootPane = tree.getRootPane();
-    if (rootPane != null) {
-      JLayeredPane layeredPane = rootPane.getLayeredPane();
-      if(layeredPane != null) {
-        layeredPane.remove(myInplaceEditorComponent);
-      }
-      rootPane.removeComponentListener(myComponentListener);
+    for (Runnable action : myRemoveActions) {
+      action.run();
     }
+    myRemoveActions.clear();
+
     myInplaceEditorComponent = null;
+
+    final DebuggerTree tree = myNode.getTree();
     tree.repaint();
     tree.requestFocus();
   }
@@ -113,29 +78,83 @@ public abstract class InplaceEditor implements AWTEventListener{
   public void show() {
     LOG.assertTrue(myInplaceEditorComponent == null, "editor is not released");
     final DebuggerTree tree = myNode.getTree();
-    final JLayeredPane layeredPane = tree.getRootPane().getLayeredPane();
+    final JRootPane rootPane = tree.getRootPane();
+    if (rootPane == null) {
+      return;
+    }
+    final JLayeredPane layeredPane = rootPane.getLayeredPane();
 
     Rectangle bounds = getEditorBounds();
 
     Point layeredPanePoint = SwingUtilities.convertPoint(tree, bounds.x, bounds.y,layeredPane);
 
-    myInplaceEditorComponent = createInplaceEditorComponent();
-    LOG.assertTrue(myInplaceEditorComponent != null);
-    myInplaceEditorComponent.setBounds(
+    final JComponent inplaceEditorComponent = createInplaceEditorComponent();
+    myInplaceEditorComponent = inplaceEditorComponent;
+    LOG.assertTrue(inplaceEditorComponent != null);
+    inplaceEditorComponent.setBounds(
       layeredPanePoint.x,
       layeredPanePoint.y,
       bounds.width,
-      Math.max(bounds.height, myInplaceEditorComponent.getPreferredSize().height)
+      Math.max(bounds.height, inplaceEditorComponent.getPreferredSize().height)
     );
 
-    layeredPane.add(myInplaceEditorComponent,new Integer(250));
+    layeredPane.add(inplaceEditorComponent, new Integer(250));
 
-    myInplaceEditorComponent.validate();
-    myInplaceEditorComponent.paintImmediately(0,0,myInplaceEditorComponent.getWidth(),myInplaceEditorComponent.getHeight());
+    myRemoveActions.add(new Runnable() {
+      public void run() {
+        layeredPane.remove(inplaceEditorComponent);
+      }
+    });
+
+    inplaceEditorComponent.validate();
+    inplaceEditorComponent.paintImmediately(0,0,inplaceEditorComponent.getWidth(),inplaceEditorComponent.getHeight());
     getPreferredFocusedComponent().requestFocus();
 
-    tree.getRootPane().addComponentListener(myComponentListener);
-    ExecutionManager.getInstance(getProject()).getContentManager().addRunContentListener(myRunContentListener);
+    final ComponentAdapter componentListener = new ComponentAdapter() {
+      public void componentResized(ComponentEvent e) {
+        DebuggerInvocationUtil.invokeLater(getProject(), new Runnable() {
+          public void run() {
+            if (!isShown()) {
+              return;
+            }
+            JTree tree = myNode.getTree();
+            JLayeredPane layeredPane = tree.getRootPane().getLayeredPane();
+            Rectangle bounds = getEditorBounds();
+            Point layeredPanePoint = SwingUtilities.convertPoint(tree, bounds.x, bounds.y, layeredPane);
+            inplaceEditorComponent.setBounds(layeredPanePoint.x, layeredPanePoint.y, bounds.width, bounds.height);
+            inplaceEditorComponent.revalidate();
+          }
+        });
+      }
+
+      public void componentHidden(ComponentEvent e) {
+        cancelEditing();
+      }
+    };
+    rootPane.addComponentListener(componentListener);
+    myRemoveActions.add(new Runnable() {
+      public void run() {
+        rootPane.removeComponentListener(componentListener);
+      }
+    });
+
+    final RunContentManager contentManager = ExecutionManager.getInstance(getProject()).getContentManager();
+    final RunContentListener runContentListener = new RunContentListener() {
+      public void contentSelected(RunContentDescriptor descriptor) {
+        cancelEditing();
+      }
+
+      public void contentRemoved(RunContentDescriptor descriptor) {
+        cancelEditing();
+      }
+    };
+    contentManager.addRunContentListener(runContentListener);
+    myRemoveActions.add(new Runnable() {
+      public void run() {
+        contentManager.removeRunContentListener(runContentListener);
+      }
+    });
+
     final JComponent editorComponent = getEditorComponent();
     editorComponent.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT).put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "enterStroke");
     editorComponent.getActionMap().put("enterStroke", new AbstractAction() {
@@ -149,12 +168,21 @@ public abstract class InplaceEditor implements AWTEventListener{
         cancelEditing();
       }
     });
-    Toolkit.getDefaultToolkit().addAWTEventListener(this, AWTEvent.MOUSE_EVENT_MASK);
+    final Toolkit defaultToolkit = Toolkit.getDefaultToolkit();
+    defaultToolkit.addAWTEventListener(this, AWTEvent.MOUSE_EVENT_MASK);
+    myRemoveActions.add(new Runnable() {
+      public void run() {
+        defaultToolkit.removeAWTEventListener(InplaceEditor.this);
+      }
+    });
   }
 
   public void eventDispatched(AWTEvent event) {
+    if (!isShown()) {
+      return;
+    }
     MouseEvent mouseEvent = (MouseEvent)event;
-    if (mouseEvent.getClickCount() == 0 || !isShown()) {
+    if (mouseEvent.getClickCount() == 0) {
       return;
     }
     final Component sourceComponent = mouseEvent.getComponent();
