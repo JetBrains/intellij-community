b39c2eacc731d9b8e7064b5185a2b9e952e2f760
Hani Suleiman <no_reply@jetbrains.com>
2006-11-20 17:50:15 +0300

    Group browser looks in selected module only

--- plugins/testng/src/com/theoryinpractice/testng/GroupBrowser.java
+++ plugins/testng/src/com/theoryinpractice/testng/GroupBrowser.java
@@ -2,6 +2,7 @@ package com.theoryinpractice.testng;
 
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.module.Module;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.execution.junit2.configuration.BrowseModuleValueActionListener;
@@ -24,9 +25,9 @@ public class GroupBrowser extends BrowseModuleValueActionListener
 
     @Override
     protected String showDialog() {
-        Project project = editor.getModuleSelector().getProject();
-        //TODO narrow scope according to what's chosen in editor
-        PsiClass[] classes = TestNGUtil.getAllTestClasses(new TestClassFilter(GlobalSearchScope.projectScope(project), project, false));
+        Module module = editor.getModuleSelector().getModule();        
+        TestClassFilter filter = new TestClassFilter(GlobalSearchScope.moduleScope(module), module.getProject(), false);
+        PsiClass[] classes = TestNGUtil.getAllTestClasses(filter);
         if(classes.length == 0) {
             Messages.showMessageDialog(getField(), "No tests found in project", "Cannot Browse Groups", Messages.getInformationIcon());
             return null;
