8b3e4190d8bcf894271cc66ceeee4cb563befffe
Mike Aizatsky <no_reply@jetbrains.com>
2006-11-21 16:33:17 +0300

    Initial version of serializer

--- util/xmlb/src/com/intellij/util/xmlb/XmlSerializer.java
+++ util/xmlb/src/com/intellij/util/xmlb/XmlSerializer.java
@@ -1,69 +1,349 @@
 package com.intellij.util.xmlb;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
+import org.w3c.dom.*;
 
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.*;
 
 /**
  * @author mike
  */
 public class XmlSerializer {
     private static final String OPTION = "option";
+    private static final String VALUE = "value";
+    private Document document;
+
+
+    private XmlSerializer(Document document) {
+        this.document = document;
+    }
 
     public static Element serialize(Object object, Document document) throws XmlSerializationException {
+        return new XmlSerializer(document).serialize(object);
+    }
+
+    private Element serialize(Object object) throws XmlSerializationException {
         try {
-            Element element = document.createElement(getTagName(object));
-
-            Class<? extends Object> aClass = object.getClass();
-            serializeProperties(element, aClass, object);
-
-            return element;
+            return (Element) getValueBinding(object).serialize(object, document);
         } catch (Exception e) {
             throw new XmlSerializationException(e);
         }
     }
 
-    private static void serializeProperties(Element element, Class<? extends Object> aClass, Object object) throws IllegalAccessException {
+    private static Binding getValueBinding(Object value) throws Exception {
+        Class<? extends Object> aClass = value.getClass();
+        return getClassBinding(aClass);
+    }
+
+    private static Binding getClassBinding(Class<? extends Object> aClass) throws Exception {
+        if (aClass.isPrimitive()) return new PrimitiveValueBinding();
+        if (Number.class.isAssignableFrom(aClass)) return new PrimitiveValueBinding();
+        if (String.class.isAssignableFrom(aClass)) return new PrimitiveValueBinding();
+        if (Collection.class.isAssignableFrom(aClass)) return new CollectionBinding();
+        if (Map.class.isAssignableFrom(aClass)) return new MapBinding();
+
+        return new BeanToTagBinding(aClass);
+    }
+
+    private static <T> T findAnnotation(Annotation[] annotations, Class<T> aClass) {
+        if (annotations == null) return null;
+
+        for (Annotation annotation : annotations) {
+            if (aClass.isAssignableFrom(annotation.getClass())) return (T) annotation;
+        }
+        return null;
+    }
+
+    private interface Accessor {
+        Object read(Object o) throws Exception;
+
+        Annotation[] getAnnotations() throws Exception;
+
+        String getName();
+
+        Binding createBinding() throws Exception;
+    }
+
+    private static class FieldAccessor implements Accessor {
+        private final Field myField;
+
+
+        public FieldAccessor(Field myField) {
+            this.myField = myField;
+        }
+
+        public Object read(Object o) throws Exception {
+            return myField.get(o);
+        }
+
+        public Annotation[] getAnnotations() throws Exception {
+            return myField.getAnnotations();
+        }
+
+        public String getName() {
+            return myField.getName();
+        }
+
+        public Binding createBinding() throws Exception {
+            return createBindingByAccessor(this);
+        }
+    }
+
+    private static Binding createBindingByAccessor(Accessor accessor) throws Exception {
+        Tag tag = findAnnotation(accessor.getAnnotations(), Tag.class);
+        if (tag != null) return new TagBinding(accessor, tag);
+        return new OptionTagBinding(accessor);
+    }
+
+    private static class PropertyAccessor implements Accessor {
+        private final PropertyDescriptor myPropertyDescriptor;
+
+
+        public PropertyAccessor(PropertyDescriptor myPropertyDescriptor) {
+            this.myPropertyDescriptor = myPropertyDescriptor;
+        }
+
+
+        public Object read(Object o) throws Exception {
+            return myPropertyDescriptor.getReadMethod().invoke(o);
+        }
+
+        public Annotation[] getAnnotations() throws Exception {
+            List<Annotation> result = new ArrayList<Annotation>();
+
+            if (myPropertyDescriptor.getReadMethod() != null) {
+                result.addAll(Arrays.asList(myPropertyDescriptor.getReadMethod().getAnnotations()));
+            }
+
+            if (myPropertyDescriptor.getWriteMethod() != null) {
+                result.addAll(Arrays.asList(myPropertyDescriptor.getWriteMethod().getAnnotations()));
+            }
+
+            return result.toArray(new Annotation[result.size()]);
+        }
+
+        public String getName() {
+            return myPropertyDescriptor.getName();
+        }
+
+        public Binding createBinding() throws Exception {
+            return createBindingByAccessor(this);
+        }
+    }
+
+    private interface Binding {
+        Node serialize(Object o, Node context) throws Exception;
+    }
+
+    private static class BeanToTagBinding implements Binding {
+        private String myTagName;
+        private Binding[] myPropertyBindings;
+
+        public BeanToTagBinding(Class<? extends Object> beanClass) throws Exception {
+            myTagName = getTagName(beanClass);
+            myPropertyBindings = getPropertyBindings(beanClass);
+        }
+
+        private Binding[] getPropertyBindings(Class<? extends Object> beanClass) throws Exception {
+            List<Binding> bindings = new ArrayList<Binding>();
+            Accessor[] accessors = getAccessors(beanClass);
+
+            for (Accessor accessor : accessors) {
+                bindings.add(accessor.createBinding());
+            }
+
+            return bindings.toArray(new Binding[bindings.size()]);
+
+        }
+
+        public Node serialize(Object o, Node context) throws Exception {
+            Document ownerDocument = getOwnerDocument(context);
+            assert ownerDocument != null;
+            Element element = ownerDocument.createElement(myTagName);
+
+            for (Binding binding : myPropertyBindings) {
+                Node node = binding.serialize(o, element);
+                if (node != element) {
+                    element.appendChild(node);
+                }
+            }
+
+            return element;
+        }
+
+        private static String getTagName(Class<? extends Object> aClass) {
+            Tag tag = aClass.getAnnotation(Tag.class);
+            if (tag != null) return tag.name();
+
+            return aClass.getSimpleName();
+        }
+    }
+
+
+    private static Document getOwnerDocument(Node context) {
+        if (context instanceof Document) return (Document) context;
+        return context.getOwnerDocument();
+    }
+
+    private static Accessor[] getAccessors(Class<? extends Object> aClass) throws IntrospectionException {
+        List<Accessor> accessors = new ArrayList<Accessor>();
+
+        BeanInfo info = Introspector.getBeanInfo(aClass);
+
+        PropertyDescriptor[] propertyDescriptors = info.getPropertyDescriptors();
+        for (PropertyDescriptor propertyDescriptor : propertyDescriptors) {
+            if (propertyDescriptor.getName().equals("class")) continue;
+
+            accessors.add(new PropertyAccessor(propertyDescriptor));
+        }
+
         Field[] fields = aClass.getFields();
         for (Field field : fields) {
-            if (Modifier.isPublic(field.getModifiers())) {
-                Element option = element.getOwnerDocument().createElement(OPTION);
-
-                option.setAttribute("name", field.getName());
-                option.setAttribute("value", String.valueOf(field.get(object)));
-
-
-                element.appendChild(option);
+            int modifiers = field.getModifiers();
+            if (Modifier.isPublic(modifiers) && !Modifier.isStatic(modifiers)) {
+                accessors.add(new FieldAccessor(field));
             }
         }
+
+
+        return accessors.toArray(new Accessor[accessors.size()]);
     }
 
-    private static String getTagName(Object object) {
-        Class<? extends Object> aClass = object.getClass();
+    private static class OptionTagBinding implements Binding {
+        private Accessor accessor;
+        private String myName;
 
-        Tag tag = aClass.getAnnotation(Tag.class);
-        if (tag != null) return tag.name();
+        public OptionTagBinding(Accessor accessor) {
+            this.accessor = accessor;
+            myName = accessor.getName();
+        }
 
-        return aClass.getSimpleName();
+        public Node serialize(Object o, Node context) throws Exception {
+            Document ownerDocument = getOwnerDocument(context);
+            Element targetElement = ownerDocument.createElement(OPTION);
+            Object value = accessor.read(o);
+            if (value == null) return context;
+
+            targetElement.setAttribute("name", myName);
+
+            Node node = getValueBinding(value).serialize(value, targetElement);
+            if (node instanceof Text) {
+                Text text = (Text) node;
+                targetElement.setAttribute(VALUE, text.getWholeText());
+            }
+            else {
+                targetElement.appendChild(node);
+            }
+
+            return targetElement;
+        }
     }
 
-    public static class XmlSerializationException extends Exception {
+    private static class TagBinding implements Binding {
+        private Accessor accessor;
+        private Tag tagAnnotation;
 
-        public XmlSerializationException() {
+        public TagBinding(Accessor accessor, Tag tagAnnotation) {
+            this.accessor = accessor;
+            this.tagAnnotation = tagAnnotation;
         }
 
-        public XmlSerializationException(String message) {
-            super(message);
-        }
+        public Node serialize(Object o, Node context) throws Exception {
+            Document ownerDocument = getOwnerDocument(context);
+            Object value = accessor.read(o);
+            if (value == null) return context;
 
-        public XmlSerializationException(String message, Throwable cause) {
-            super(message, cause);
-        }
+            Element v = ownerDocument.createElement(tagAnnotation.name());
 
-        public XmlSerializationException(Throwable cause) {
-            super(cause);
+            Binding binding = getValueBinding(value);
+            Node node = binding.serialize(value, v);
+            v.appendChild(node);
+
+            return v;
+        }
+    }
+
+    private static class PrimitiveValueBinding implements Binding {
+        public Node serialize(Object o, Node context) throws Exception {
+            Document ownerDocument = getOwnerDocument(context);
+            return ownerDocument.createTextNode(String.valueOf(o));
+        }
+    }
+
+    private static class CollectionBinding implements Binding {
+        private static final String COLLECTION = "collection";
+
+        public Node serialize(Object o, Node context) throws Exception {
+            Collection collection = (Collection) o;
+            Document ownerDocument = getOwnerDocument(context);
+
+            Element c = ownerDocument.createElement(COLLECTION);
+            for (Object e : collection) {
+                Node node = getValueBinding(e).serialize(e, c);
+                if (node instanceof Text) {
+                    Text text = (Text) node;
+                    Element option = ownerDocument.createElement(OPTION);
+                    c.appendChild(option);
+                    option.setAttribute(VALUE, text.getWholeText());
+                }
+                else {
+                    c.appendChild(node);
+                }
+            }
+
+            return c;
+        }
+    }
+
+    private static class MapBinding implements Binding {
+        private static final String MAP = "map";
+        private static final String ENTRY = "entry";
+        private static final String KEY = "key";
+
+        public Node serialize(Object o, Node context) throws Exception {
+            Map map = (Map) o;
+
+            Document ownerDocument = getOwnerDocument(context);
+
+            Element m = ownerDocument.createElement(MAP);
+
+            for (Object k : map.keySet()) {
+                Object v = map.get(k);
+
+                Element entry = ownerDocument.createElement(ENTRY);
+                m.appendChild(entry);
+
+                Node kNode = getValueBinding(k).serialize(k, entry);
+                Node vNode = getValueBinding(v).serialize(v, entry);
+
+                if (kNode instanceof Text) {
+                    Text text = (Text) kNode;
+                    entry.setAttribute(KEY, text.getWholeText());
+                }
+                else {
+                    Element key = ownerDocument.createElement(KEY);
+                    entry.appendChild(key);
+                    key.appendChild(kNode);
+                }
+
+                if (vNode instanceof Text) {
+                    Text text = (Text) vNode;
+                    entry.setAttribute(VALUE, text.getWholeText());
+                }
+                else {
+                    Element value = ownerDocument.createElement(VALUE);
+                    entry.appendChild(value);
+                    value.appendChild(vNode);
+                }
+            }
+
+            return m;
         }
     }
 }
--- util/xmlb/testSrc/com/intellij/util/xmlb/XmlSerializerTest.java
+++ util/xmlb/testSrc/com/intellij/util/xmlb/XmlSerializerTest.java
@@ -10,9 +10,11 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.ByteArrayOutputStream;
+import java.util.*;
 
 /**
  * @author mike
@@ -44,9 +46,19 @@ public class XmlSerializerTest extends TestCase {
         public String STRING_V = "hello";
     }
     public void testPublicFieldSerialization() throws Exception {
+        BeanWithPublicFields bean = new BeanWithPublicFields();
+
         assertSerialization(
                 "<BeanWithPublicFields><option name=\"INT_V\" value=\"1\"/><option name=\"STRING_V\" value=\"hello\"/></BeanWithPublicFields>",
-                new BeanWithPublicFields()
+                bean
+        );
+
+        bean.INT_V = 2;
+        bean.STRING_V = "bye";
+
+        assertSerialization(
+                "<BeanWithPublicFields><option name=\"INT_V\" value=\"2\"/><option name=\"STRING_V\" value=\"bye\"/></BeanWithPublicFields>",
+                bean
         );
     }
 
@@ -55,29 +67,181 @@ public class XmlSerializerTest extends TestCase {
         public String NEW_S = "foo";
     }
     public void testPublicFieldSerializationWithInheritance() throws Exception {
+        BeanWithPublicFieldsDescendant bean = new BeanWithPublicFieldsDescendant();
+
         assertSerialization(
                 "<BeanWithPublicFieldsDescendant><option name=\"NEW_S\" value=\"foo\"/><option name=\"INT_V\" value=\"1\"/><option name=\"STRING_V\" value=\"hello\"/></BeanWithPublicFieldsDescendant>",
-                new BeanWithPublicFieldsDescendant()
+                bean
+        );
+
+        bean.INT_V = 2;
+        bean.STRING_V = "bye";
+        bean.NEW_S = "bar";
+
+        assertSerialization(
+                "<BeanWithPublicFieldsDescendant><option name=\"NEW_S\" value=\"bar\"/><option name=\"INT_V\" value=\"2\"/><option name=\"STRING_V\" value=\"bye\"/></BeanWithPublicFieldsDescendant>",
+                bean
         );
     }
 
+    public static class BeanWithSubBean {
+        public EmptyBeanWithCustomName BEAN1 = new EmptyBeanWithCustomName();
+        public BeanWithPublicFields BEAN2 = new BeanWithPublicFields();
+    }
     public void testSubBeanSerialization() throws Exception {
-        fail();
+        BeanWithSubBean bean = new BeanWithSubBean();
+        assertSerialization(
+                "<BeanWithSubBean><option name=\"BEAN1\"><Bean/></option><option name=\"BEAN2\"><BeanWithPublicFields><option name=\"INT_V\" value=\"1\"/><option name=\"STRING_V\" value=\"hello\"/></BeanWithPublicFields></option></BeanWithSubBean>",
+                bean
+        );
+        bean.BEAN2.INT_V = 2;
+        bean.BEAN2.STRING_V = "bye";
+
+        assertSerialization(
+                "<BeanWithSubBean><option name=\"BEAN1\"><Bean/></option><option name=\"BEAN2\"><BeanWithPublicFields><option name=\"INT_V\" value=\"2\"/><option name=\"STRING_V\" value=\"bye\"/></BeanWithPublicFields></option></BeanWithSubBean>",
+                bean
+        );
     }
 
+    public void testNullFieldValue() throws Exception {
+        BeanWithPublicFields bean1 = new BeanWithPublicFields();
+
+        assertSerialization(
+                "<BeanWithPublicFields><option name=\"INT_V\" value=\"1\"/><option name=\"STRING_V\" value=\"hello\"/></BeanWithPublicFields>",
+                bean1
+        );
+
+        bean1.STRING_V = null;
+
+        assertSerialization(
+                "<BeanWithPublicFields><option name=\"INT_V\" value=\"1\"/></BeanWithPublicFields>",
+                bean1
+        );
+
+        BeanWithSubBean bean2 = new BeanWithSubBean();
+        bean2.BEAN1 = null;
+        bean2.BEAN2 = null;
+
+        assertSerialization(
+                "<BeanWithSubBean/>",
+                bean2
+        );
+
+    }
+
+    public static class BeanWithList {
+        public List VALUES = Arrays.asList(new Object[] {"a", "b", "c"});
+    }
     public void testListSerialization() throws Exception {
-        fail();
+        BeanWithList bean = new BeanWithList();
+
+        assertSerialization(
+                "<BeanWithList><option name=\"VALUES\"><collection><option value=\"a\"/><option value=\"b\"/><option value=\"c\"/></collection></option></BeanWithList>",
+                bean
+        );
+
+        bean.VALUES = Arrays.asList(new Object[] {"1", "2", "3"});
+
+        assertSerialization(
+                "<BeanWithList><option name=\"VALUES\"><collection><option value=\"1\"/><option value=\"2\"/><option value=\"3\"/></collection></option></BeanWithList>",
+                bean
+        );
     }
 
+    public static class BeanWithSet{
+        public Set VALUES = new HashSet(Arrays.asList(new Object[] {"a", "b", "w"}));
+    }
     public void testSetSerialization() throws Exception {
-        fail();
+        BeanWithSet bean = new BeanWithSet();
+        assertSerialization(
+                "<BeanWithSet><option name=\"VALUES\"><collection><option value=\"w\"/><option value=\"a\"/><option value=\"b\"/></collection></option></BeanWithSet>",
+                bean
+        );
+        bean.VALUES = new HashSet(Arrays.asList(new Object[] {"1", "2", "3"}));
+
+        assertSerialization(
+                "<BeanWithSet><option name=\"VALUES\"><collection><option value=\"3\"/><option value=\"2\"/><option value=\"1\"/></collection></option></BeanWithSet>",
+                bean
+        );
+    }
+
+    public static class BeanWithMap{
+        public Map VALUES = new HashMap();
+        {
+            VALUES.put("a", "1");
+            VALUES.put("b", "2");
+            VALUES.put("c", "3");
+        }
     }
 
     public void testMapSerialization() throws Exception {
-        fail();
+        BeanWithMap bean = new BeanWithMap();
+        assertSerialization(
+                "<BeanWithMap><option name=\"VALUES\"><map><entry key=\"a\" value=\"1\"/><entry key=\"c\" value=\"3\"/><entry key=\"b\" value=\"2\"/></map></option></BeanWithMap>",
+                bean
+        );
+        bean.VALUES.clear();
+        bean.VALUES.put("1", "a");
+        bean.VALUES.put("2", "b");
+        bean.VALUES.put("3", "c");
+
+        assertSerialization(
+                "<BeanWithMap><option name=\"VALUES\"><map><entry key=\"3\" value=\"c\"/><entry key=\"2\" value=\"b\"/><entry key=\"1\" value=\"a\"/></map></option></BeanWithMap>",
+                bean
+        );
     }
 
-    private void assertSerialization(String expectedText, Object bean) throws ParserConfigurationException, TransformerException, XmlSerializer.XmlSerializationException {
+    public static class BeanWithProperty {
+        private String name = "James";
+
+        public String getName() {
+            return name;
+        }
+
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+    public void testPropertySerialization() throws Exception {
+        BeanWithProperty bean = new BeanWithProperty();
+
+        assertSerialization(
+                "<BeanWithProperty><option name=\"name\" value=\"James\"/></BeanWithProperty>",
+                bean
+        );
+
+        bean.setName("Bond");
+
+        assertSerialization(
+                "<BeanWithProperty><option name=\"name\" value=\"Bond\"/></BeanWithProperty>",
+                bean
+        );
+    }
+
+    public static class BeanWithFieldWithTagAnnotation {
+        @Tag(name = "name")
+        public String STRING_V = "hello";
+    }
+
+    public void testFieldWithTagAnnotation() throws Exception {
+        BeanWithFieldWithTagAnnotation bean = new BeanWithFieldWithTagAnnotation();
+
+        assertSerialization(
+                "<BeanWithFieldWithTagAnnotation><name>hello</name></BeanWithFieldWithTagAnnotation>",
+                bean
+        );
+
+        bean.STRING_V = "bye";
+
+        assertSerialization(
+                "<BeanWithFieldWithTagAnnotation><name>bye</name></BeanWithFieldWithTagAnnotation>",
+                bean
+        );
+    }
+
+    private void assertSerialization(String expectedText, Object bean)
+            throws ParserConfigurationException, TransformerException, XmlSerializationException {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder documentBuilder = factory.newDocumentBuilder();
         Document document = documentBuilder.newDocument();
@@ -92,14 +256,12 @@ public class XmlSerializerTest extends TestCase {
         StreamResult result = new StreamResult(byteArrayOutputStream);
         transformer.transform(source, result);
 
-        String actualString = new String(byteArrayOutputStream.toByteArray());
+        String actualString = new String(byteArrayOutputStream.toByteArray()).trim();
 
         if (!expectedText.startsWith(XML_PREFIX)) {
-            if (actualString.startsWith(XML_PREFIX)) actualString = actualString.substring(XML_PREFIX.length());
+            if (actualString.startsWith(XML_PREFIX)) actualString = actualString.substring(XML_PREFIX.length()).trim();
         }
 
         assertEquals(expectedText, actualString);
     }
-
-
 }
