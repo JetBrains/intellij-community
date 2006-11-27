beae78ea3996fea2fe75db848848360592d2e453
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2006-11-27 17:21:09 +0300

    error reporting enhanced

--- ExtendedApi/src/com/intellij/testFramework/ExpectedHighlightingData.java
+++ ExtendedApi/src/com/intellij/testFramework/ExpectedHighlightingData.java
@@ -147,7 +147,7 @@ public class ExpectedHighlightingData {
       if (closeTagMarker == null) {
         Pattern pat2 = Pattern.compile("(.*?)</" + marker + ">(.*)", Pattern.DOTALL);
         final Matcher matcher2 = pat2.matcher(rest);
-        LOG.assertTrue(matcher2.matches());
+        LOG.assertTrue(matcher2.matches(), "Cannot find closing </" + marker + ">");
         content = matcher2.group(1);
         endOffset = m.start(11) + matcher2.start(2);
       }
