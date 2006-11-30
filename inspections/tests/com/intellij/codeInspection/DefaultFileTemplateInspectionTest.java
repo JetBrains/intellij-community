7a48e91abbaa50d5f6d00ec81ead44fdbe24934e
Alexey Kudravtsev <cdr@jetbrains.com>
2006-11-30 18:49:09 +0300

    bombed

--- inspections/tests/com/intellij/codeInspection/DefaultFileTemplateInspectionTest.java
+++ inspections/tests/com/intellij/codeInspection/DefaultFileTemplateInspectionTest.java
@@ -2,8 +2,11 @@ package com.intellij.codeInspection;
 
 import com.intellij.codeInspection.defaultFileTemplateUsage.DefaultFileTemplateUsageInspection;
 import com.intellij.testFramework.InspectionTestCase;
+import com.intellij.idea.Bombed;
 
+import java.util.Calendar;
 
+@Bombed(year = 2006,month = Calendar.DECEMBER, day = 1,time = 14,user = "cdr")
 public class DefaultFileTemplateInspectionTest extends InspectionTestCase {
   protected void setUp() throws Exception {
     super.setUp();
