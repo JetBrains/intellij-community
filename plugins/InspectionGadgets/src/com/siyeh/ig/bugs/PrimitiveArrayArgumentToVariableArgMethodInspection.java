fa2aa109480b17e8ad95d5962229d1903bf14adb
Bas Leijdekkers <basleijdekkers@gmail.com>
2006-12-01 18:48:55 +0300

    IDEADEV-12394

--- plugins/InspectionGadgets/src/com/siyeh/ig/bugs/PrimitiveArrayArgumentToVariableArgMethodInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/bugs/PrimitiveArrayArgumentToVariableArgMethodInspection.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2006 Dave Griffith, Bas Leijdekkers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.siyeh.ig.bugs;
 
 import com.intellij.codeInsight.daemon.GroupNames;
@@ -42,18 +57,18 @@ public class PrimitiveArrayArgumentToVariableArgMethodInspection
         public void visitMethodCallExpression(
                 @NotNull PsiMethodCallExpression call) {
             super.visitMethodCallExpression(call);
-
             final LanguageLevel languageLevel = PsiUtil.getLanguageLevel(call);
             if (languageLevel.compareTo(LanguageLevel.JDK_1_5) < 0) {
                 return;
             }
             final PsiExpressionList argumentList = call.getArgumentList();
-            final PsiExpression[] args = argumentList.getExpressions();
-            if (args.length == 0) {
+            final PsiExpression[] arguments = argumentList.getExpressions();
+            if (arguments.length == 0) {
                 return;
             }
-            final PsiExpression lastArg = args[args.length - 1];
-            if (!isPrimitiveArray(lastArg)) {
+            final PsiExpression lastArgument = arguments[arguments.length - 1];
+            final PsiType argumentType = lastArgument.getType();
+            if (!isPrimitiveArrayType(argumentType)) {
                 return;
             }
             final PsiMethod method = call.resolveMethod();
@@ -61,24 +76,24 @@ public class PrimitiveArrayArgumentToVariableArgMethodInspection
                 return;
             }
             final PsiParameterList parameterList = method.getParameterList();
+            if (parameterList.getParametersCount() != arguments.length) {
+                return;
+            }
             final PsiParameter[] parameters = parameterList.getParameters();
-            if (parameters == null) {
-                return;
-            }
-            if (parameters.length != args.length) {
-                return;
-            }
             final PsiParameter lastParameter =
                     parameters[parameters.length - 1];
             if (!lastParameter.isVarArgs()) {
                 return;
             }
-            registerError(lastArg);
+            final PsiType parameterType = lastParameter.getType();
+            if (isPrimitiveArrayType(parameterType)) {
+                return;
+            }
+            registerError(lastArgument);
         }
     }
 
-    private static boolean isPrimitiveArray(PsiExpression exp) {
-        final PsiType type = exp.getType();
+    private static boolean isPrimitiveArrayType(PsiType type) {
         if (type == null) {
             return false;
         }
