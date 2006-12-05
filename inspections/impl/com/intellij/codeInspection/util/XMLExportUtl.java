320171cf171eaade40b6b5e626af34343cfca706
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2006-12-05 20:30:35 +0300

    add link to .iml file when inspection reports module problem

--- inspections/impl/com/intellij/codeInspection/util/XMLExportUtl.java
+++ inspections/impl/com/intellij/codeInspection/util/XMLExportUtl.java
@@ -22,33 +22,35 @@ import org.jdom.Element;
 
 @SuppressWarnings({"HardCodedStringLiteral"})
 public class XMLExportUtl {
-    private static final Logger LOG = Logger.getInstance("#com.intellij.codeInspection.util.XMLExportUtl");
+  private static final Logger LOG = Logger.getInstance("#com.intellij.codeInspection.util.XMLExportUtl");
 
-    private XMLExportUtl() {
-    }
+  private XMLExportUtl() {
+  }
 
-    public static Element createElement(RefEntity refEntity, Element parentNode, int actualLine, final TextRange range) {
+  public static Element createElement(RefEntity refEntity, Element parentNode, int actualLine, final TextRange range) {
     if (refEntity instanceof RefImplicitConstructor) {
       return createElement(refEntity.getOwner(), parentNode, actualLine, range);
     }
 
-    Element problem = new Element(InspectionsBundle.message("inspection.export.results.problem"));
+    Element problem = new Element("problem");
 
     if (refEntity instanceof RefElement) {
       final RefElement refElement = (RefElement)refEntity;
       PsiElement psiElement = refElement.getElement();
       PsiFile psiFile = psiElement.getContainingFile();
 
-      Element fileElement = new Element(InspectionsBundle.message("inspection.export.results.file"));
-      Element lineElement = new Element(InspectionsBundle.message("inspection.export.results.line"));
+      Element fileElement = new Element("file");
+      Element lineElement = new Element("line");
       final VirtualFile virtualFile = psiFile.getVirtualFile();
       LOG.assertTrue(virtualFile != null);
       fileElement.addContent(virtualFile.getUrl());
 
       if (actualLine == -1) {
-        Document document = PsiDocumentManager.getInstance(refElement.getRefManager().getProject()).getDocument(psiFile);
+        final Document document = PsiDocumentManager.getInstance(refElement.getRefManager().getProject()).getDocument(psiFile);
+        LOG.assertTrue(document != null);
         lineElement.addContent(String.valueOf(document.getLineNumber(psiElement.getTextOffset()) + 1));
-      } else {
+      }
+      else {
         lineElement.addContent(String.valueOf(actualLine));
       }
 
@@ -57,18 +59,28 @@ public class XMLExportUtl {
 
       new SmartRefElementPointerImpl(refElement, true).writeExternal(problem);
     }
+    else if (refEntity instanceof RefModule) {
+      final RefModule refModule = (RefModule)refEntity;
+      final VirtualFile moduleFile = refModule.getModule().getModuleFile();
+      final Element fileElement = new Element("file");
+      fileElement.addContent(moduleFile.getUrl());
+      problem.addContent(fileElement);
+    }
 
 
     if (refEntity instanceof RefMethod) {
-      RefMethod refMethod = (RefMethod) refEntity;
+      RefMethod refMethod = (RefMethod)refEntity;
       appendMethod(refMethod, problem);
-    } else if (refEntity instanceof RefField) {
-      RefField refField = (RefField) refEntity;
+    }
+    else if (refEntity instanceof RefField) {
+      RefField refField = (RefField)refEntity;
       appendField(refField, problem);
-    } else if (refEntity instanceof RefClass) {
+    }
+    else if (refEntity instanceof RefClass) {
       RefClass refClass = (RefClass)refEntity;
       appendClass(refClass, problem);
-    } else {
+    }
+    else {
       LOG.info("Unknown refElement: " + refEntity);
     }
     parentNode.addContent(problem);
@@ -84,27 +96,27 @@ public class XMLExportUtl {
 
     if (psiFile instanceof PsiJavaFile) {
       String packageName = ((PsiJavaFile)psiFile).getPackageName();
-      Element packageElement = new Element(InspectionsBundle.message("inspection.export.results.package"));
+      Element packageElement = new Element("package");
       packageElement.addContent(packageName.length() > 0 ? packageName : InspectionsBundle.message("inspection.export.results.default"));
       parentNode.addContent(packageElement);
     }
 
-    Element classElement = new Element(InspectionsBundle.message("inspection.export.results.class"));
+    Element classElement = new Element("class");
     if (psiDocComment != null) {
       PsiDocTag[] tags = psiDocComment.getTags();
       for (PsiDocTag tag : tags) {
-        if (InspectionsBundle.message("inspection.export.results.author").equals(tag.getName()) && tag.getValueElement() != null) {
-          classElement.setAttribute(InspectionsBundle.message("inspection.export.results.author"), tag.getValueElement().getText());
+        if ("author".equals(tag.getName()) && tag.getValueElement() != null) {
+          classElement.setAttribute("author", tag.getValueElement().getText());
         }
       }
     }
 
     String name = PsiFormatUtil.formatClass(psiClass, PsiFormatUtil.SHOW_NAME);
-    Element nameElement = new Element(InspectionsBundle.message("inspection.export.results.name"));
+    Element nameElement = new Element("name");
     nameElement.addContent(name);
     classElement.addContent(nameElement);
 
-    Element displayName = new Element(InspectionsBundle.message("inspection.export.results.display.name"));
+    Element displayName = new Element("display_name");
     final RefUtil refUtil = RefUtil.getInstance();
     displayName.addContent(refUtil.getQualifiedName(refClass));
     classElement.addContent(displayName);
@@ -118,22 +130,18 @@ public class XMLExportUtl {
   }
 
   private static void appendMethod(final RefMethod refMethod, Element parentNode) {
-    Element methodElement = new Element(refMethod.isConstructor() ? InspectionsBundle.message("inspection.export.results.constructor") : InspectionsBundle.message("inspection.export.results.method"));
+    Element methodElement = new Element(refMethod.isConstructor() ? "constructor" : "method");
 
-    PsiMethod psiMethod = (PsiMethod) refMethod.getElement();
-    String name = PsiFormatUtil.formatMethod(psiMethod, PsiSubstitutor.EMPTY, PsiFormatUtil.SHOW_NAME |
-                                                        PsiFormatUtil.SHOW_FQ_NAME |
-                                                        PsiFormatUtil.SHOW_TYPE |
-                                                        PsiFormatUtil.SHOW_PARAMETERS,
-                                             PsiFormatUtil.SHOW_NAME |
-                                             PsiFormatUtil.SHOW_TYPE
-    );
+    PsiMethod psiMethod = (PsiMethod)refMethod.getElement();
+    String name = PsiFormatUtil.formatMethod(psiMethod, PsiSubstitutor.EMPTY, PsiFormatUtil.SHOW_NAME | PsiFormatUtil.SHOW_FQ_NAME |
+                                                                              PsiFormatUtil.SHOW_TYPE | PsiFormatUtil.SHOW_PARAMETERS,
+                                                                              PsiFormatUtil.SHOW_NAME | PsiFormatUtil.SHOW_TYPE);
 
-    Element shortNameElement = new Element(InspectionsBundle.message("inspection.export.results.name"));
+    Element shortNameElement = new Element("name");
     shortNameElement.addContent(name);
     methodElement.addContent(shortNameElement);
 
-    Element displayName = new Element(InspectionsBundle.message("inspection.export.results.display.name"));
+    Element displayName = new Element("name");
     final RefUtil refUtil = RefUtil.getInstance();
     displayName.addContent(refUtil.getQualifiedName(refMethod));
     methodElement.addContent(displayName);
@@ -144,17 +152,15 @@ public class XMLExportUtl {
   }
 
   private static void appendField(final RefField refField, Element parentNode) {
-    Element fieldElement = new Element(InspectionsBundle.message("inspection.export.results.field"));
+    Element fieldElement = new Element("field");
     PsiField psiField = refField.getElement();
-    String name = PsiFormatUtil.formatVariable(psiField, PsiFormatUtil.SHOW_NAME |
-                                                         PsiFormatUtil.SHOW_TYPE,
-        PsiSubstitutor.EMPTY);
+    String name = PsiFormatUtil.formatVariable(psiField, PsiFormatUtil.SHOW_NAME | PsiFormatUtil.SHOW_TYPE, PsiSubstitutor.EMPTY);
 
-    Element shortNameElement = new Element(InspectionsBundle.message("inspection.export.results.name"));
+    Element shortNameElement = new Element("name");
     shortNameElement.addContent(name);
     fieldElement.addContent(shortNameElement);
 
-    Element displayName = new Element(InspectionsBundle.message("inspection.export.results.display.name"));
+    Element displayName = new Element("display_name");
     final RefUtil refUtil = RefUtil.getInstance();
     displayName.addContent(refUtil.getQualifiedName(refField));
     fieldElement.addContent(displayName);
