58ba4ad527011cf0eb33fbea901ea6e1ed133526
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2006-12-07 17:13:31 +0300

    javadocs

--- codeInsight/impl/com/intellij/codeInsight/daemon/QuickFixProvider.java
+++ codeInsight/impl/com/intellij/codeInsight/daemon/QuickFixProvider.java
@@ -1,10 +1,14 @@
 package com.intellij.codeInsight.daemon;
 
 import com.intellij.codeInsight.daemon.impl.HighlightInfo;
+import com.intellij.codeInsight.daemon.impl.quickfix.QuickFixAction;
+import com.intellij.codeInsight.intention.IntentionAction;
 import com.intellij.psi.PsiReference;
 
 /**
  * @author dyoma
+ * 
+ * @see QuickFixAction#registerQuickFixAction(HighlightInfo, IntentionAction)
  */
 public interface QuickFixProvider<T extends PsiReference> {
   QuickFixProvider NULL = new QuickFixProvider() {
