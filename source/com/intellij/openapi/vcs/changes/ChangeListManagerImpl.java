e0218517ac64a41ff4d1e434501bc0897442919c
Dmitry Jemerov <yole@intellij.com>
2006-12-12 20:18:32 +0300

    typo in code separating unversioned files by VCS (IDEADEV-12127)

--- source/com/intellij/openapi/vcs/changes/ChangeListManagerImpl.java
+++ source/com/intellij/openapi/vcs/changes/ChangeListManagerImpl.java
@@ -629,7 +629,7 @@ public class ChangeListManagerImpl extends ChangeListManager implements ProjectC
       public void process(final AbstractVcs vcs, final List<VirtualFile> items) {
         final CheckinEnvironment environment = vcs.getCheckinEnvironment();
         if (environment != null) {
-          exceptions.addAll(environment.scheduleUnversionedFilesForAddition(files));
+          exceptions.addAll(environment.scheduleUnversionedFilesForAddition(items));
         }
       }
     });
