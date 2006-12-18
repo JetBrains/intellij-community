ee331285b9ca441a11caba3ee73a01f91055b856
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2006-12-18 15:56:50 +0300

    DelimitedListConverter published

---
+++ dom/openapi/src/com/intellij/util/xml/converters/DelimitedListConverter.java
@@ -0,0 +1,168 @@
+/*
+ * Copyright (c) 2000-2006 JetBrains s.r.o. All Rights Reserved.
+ */
+
+/*
+ * Created by IntelliJ IDEA.
+ * User: Sergey.Vasiliev
+ * Date: Nov 13, 2006
+ * Time: 4:37:22 PM
+ */
+package com.intellij.util.xml.converters;
+
+import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.PsiReferenceBase;
+import com.intellij.util.xml.*;
+import com.intellij.codeInsight.daemon.EmptyResolveMessageProvider;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.NonNls;
+
+import java.util.List;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public abstract class DelimitedListConverter<T> extends ResolvingConverter<List<T>> implements CustomReferenceConverter<List<T>> {
+
+  private final String myDelimiters;
+
+  public DelimitedListConverter(@NonNls @NotNull String delimiters) {
+
+    myDelimiters = delimiters;
+  }
+
+  @Nullable
+  protected abstract T convertString(final @Nullable String string, final ConvertContext context);
+
+  @Nullable
+  protected abstract String toString(@Nullable final T t);
+
+
+  protected abstract Object[] getReferenceVariants(final ConvertContext context, GenericDomValue<List<T>> genericDomValue);
+
+  @Nullable
+  protected abstract PsiElement resolveReference(final T t);
+
+  protected abstract String getUnresolvedMessage(String value);
+
+  @NotNull
+  public Collection<? extends List<T>> getVariants(final ConvertContext context) {
+    return Collections.emptyList();
+  }
+
+  protected char getDefaultDelimiter() {
+    return myDelimiters.charAt(0);
+  }
+
+  public List<T> fromString(final String str, final ConvertContext context) {
+    List<T> values = new ArrayList<T>();
+
+    for (String s : StringUtil.tokenize(str, myDelimiters)) {
+      final T t = convertString(s, context);
+      if (t != null) {
+        values.add(t);
+      }
+    }
+    return values;
+  }
+
+  public String toString(final List<T> ts, final ConvertContext context) {
+    final StringBuffer buffer = new StringBuffer();
+    final char delimiter = getDefaultDelimiter();
+    for (T t : ts) {
+      final String s = toString(t);
+      if (s != null) {
+        if (buffer.length() == 0) {
+          buffer.append(delimiter);
+        }
+        buffer.append(s);
+      }
+    }
+    return buffer.toString();
+  }
+
+  protected int skipDelimiters(String s, int pos) {
+    while (pos < s.length()) {
+      final char ch = s.charAt(pos);
+      if (!isDelimiter(ch)) {
+        break;
+      }
+      pos++;
+    }
+    return pos;
+  }
+
+  protected boolean isDelimiter(char ch) {
+    return myDelimiters.indexOf(ch) != -1;
+  }
+
+  @NotNull
+  public PsiReference[] createReferences(final GenericDomValue<List<T>> genericDomValue,
+                                         final PsiElement element,
+                                         final ConvertContext context) {
+
+    final String text = genericDomValue.getStringValue();
+    if (text == null) {
+      return PsiReference.EMPTY_ARRAY;
+    }
+    final int shift = 1;
+    int start;
+    int pos = 0;
+
+    ArrayList<PsiReference> references = new ArrayList<PsiReference>();
+    do {
+      start = pos;
+      pos = skipDelimiters(text, pos);
+      if (pos == text.length()) {
+        if (references.size() == 0) {
+          references.add(createPsiReference(element, start + shift, pos + shift, context, genericDomValue));
+        }
+        break;
+      }
+      start = pos;
+      while (++pos < text.length() && !isDelimiter(text.charAt(pos))) {}
+      references.add(createPsiReference(element, start + shift, pos + shift, context, genericDomValue));
+      pos++;
+    } while(pos < text.length());
+
+    return references.toArray(new PsiReference[references.size()]);
+  }
+
+  @NotNull
+  protected PsiReference createPsiReference(final PsiElement element,
+                                            int start, int end,
+                                            final ConvertContext context,
+                                            final GenericDomValue<List<T>> genericDomValue) {
+    
+    return new MyPsiReference(element, new TextRange(start, end), context, genericDomValue);
+  }
+
+  private class MyPsiReference extends PsiReferenceBase<PsiElement> implements EmptyResolveMessageProvider {
+    private final ConvertContext myContext;
+    private final GenericDomValue<List<T>> myGenericDomValue;
+
+    public MyPsiReference(final PsiElement element, final TextRange range, final ConvertContext context, final GenericDomValue<List<T>> genericDomValue) {
+      super(element, range);
+      myContext = context;
+      myGenericDomValue = genericDomValue;
+    }
+
+    @Nullable
+    public PsiElement resolve() {
+      final String value = getValue();
+      return resolveReference(convertString(value, myContext));
+    }
+
+    public Object[] getVariants() {
+      return getReferenceVariants(myContext, myGenericDomValue);
+    }
+
+    public String getUnresolvedMessagePattern() {
+      return getUnresolvedMessage(getValue());
+    }
+  }
+}
