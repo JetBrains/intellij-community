574873ff07fa7aebe70a2ce84c9eec7274093773
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2006-12-20 15:49:47 +0300

    PsiMethod rendering support

--- dom/impl/src/com/intellij/util/xml/ElementPresentationManagerImpl.java
+++ dom/impl/src/com/intellij/util/xml/ElementPresentationManagerImpl.java
@@ -6,7 +6,12 @@ package com.intellij.util.xml;
 
 import com.intellij.codeInsight.lookup.LookupValueFactory;
 import com.intellij.openapi.components.ApplicationComponent;
+import com.intellij.openapi.util.Iconable;
 import com.intellij.util.Function;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiSubstitutor;
+import com.intellij.psi.PsiType;
+import com.intellij.psi.util.PsiFormatUtil;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
@@ -23,6 +28,20 @@ public class ElementPresentationManagerImpl extends ElementPresentationManager i
   public <T> Object[] createVariants(Collection<T> elements, Function<T, String> namer) {
     ArrayList<Object> result = new ArrayList<Object>(elements.size());
     for (T element: elements) {
+      if (element instanceof PsiMethod) {
+        PsiMethod method = (PsiMethod)element;
+        String tail = PsiFormatUtil.formatMethod(method,
+                                          PsiSubstitutor.EMPTY,
+                                          PsiFormatUtil.SHOW_PARAMETERS,
+                                          PsiFormatUtil.SHOW_NAME | PsiFormatUtil.SHOW_TYPE);
+
+        final PsiType returnType = method.getReturnType();
+        final Object value = LookupValueFactory.createLookupValueWithHintAndTail(method.getName(),
+                                     method.getIcon(Iconable.ICON_FLAG_VISIBILITY | Iconable.ICON_FLAG_READ_STATUS),
+                                     returnType == null ? null : returnType.getPresentableText(),
+                                     tail);
+        result.add(value);
+      }
       String name = namer.fun(element);
       if (name != null) {
         Icon icon = getIcon(element);
