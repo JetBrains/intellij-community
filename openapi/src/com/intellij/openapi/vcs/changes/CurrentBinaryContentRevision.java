073aad3f75ac70a49e3c19e6e7002f2a581f5f26
Dmitry Jemerov <yole@intellij.com>
2006-12-28 21:54:53 +0300

    cleanup

--- openapi/src/com/intellij/openapi/vcs/changes/CurrentBinaryContentRevision.java
+++ openapi/src/com/intellij/openapi/vcs/changes/CurrentBinaryContentRevision.java
@@ -19,23 +19,17 @@ package com.intellij.openapi.vcs.changes;
 
 import com.intellij.openapi.vcs.FilePath;
 import com.intellij.openapi.vcs.VcsException;
-import com.intellij.openapi.vcs.history.VcsRevisionNumber;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.fileEditor.FileDocumentManager;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 
 /**
- * @author max
+ * @author yole
  */
-public class CurrentBinaryContentRevision implements BinaryContentRevision {
-  private FilePath myFile;
-
+public class CurrentBinaryContentRevision extends CurrentContentRevision implements BinaryContentRevision {
   public CurrentBinaryContentRevision(final FilePath file) {
-    myFile = file;
+    super(file);
   }
 
   @Nullable
@@ -48,30 +42,5 @@ public class CurrentBinaryContentRevision implements BinaryContentRevision {
     catch (IOException e) {
       throw new VcsException(e);
     }
-  }
-
-  @Nullable
-  public VirtualFile getVirtualFile() {
-    final VirtualFile vFile = myFile.getVirtualFile();
-    if (vFile == null || !vFile.isValid()) return null;
-    return vFile;
-  }
-
-  @NotNull
-  public FilePath getFile() {
-    return myFile;
-  }
-
-  @NotNull
-  public VcsRevisionNumber getRevisionNumber() {
-    return VcsRevisionNumber.NULL;
-  }
-
-  @Nullable
-  public String getContent() throws VcsException {
-    final VirtualFile vFile = getVirtualFile();
-    if (vFile == null) return null;
-    final Document doc = FileDocumentManager.getInstance().getDocument(vFile);
-    return doc.getText();
   }
 }
