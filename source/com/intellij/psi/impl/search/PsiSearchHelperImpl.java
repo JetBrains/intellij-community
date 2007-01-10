ac38ba217d21f387bca2c44400f71a76c168b236
Alexey Kudravtsev <cdr@jetbrains.com>
2007-01-10 13:59:29 +0300

    pool size fixed

--- source/com/intellij/psi/impl/search/PsiSearchHelperImpl.java
+++ source/com/intellij/psi/impl/search/PsiSearchHelperImpl.java
@@ -533,7 +533,7 @@ public class PsiSearchHelperImpl implements PsiSearchHelper {
       }
 
       // if we are under write action, additional threads will never get read action
-      if (!application.isUnitTestMode() && !application.isWriteAccessAllowed() && POOL_SIZE > 1) {
+      if (!application.isUnitTestMode() && !application.isWriteAccessAllowed() && POOL_SIZE > 0) {
         final int chunkSize = Math.max(10, files.length / POOL_SIZE / 20); // make at least 20 chunks per proc to balance load
         ArrayList<Callable<Boolean>> callables = new ArrayList<Callable<Boolean>>(files.length/chunkSize);
         final AtomicInteger counter = new AtomicInteger(0);
