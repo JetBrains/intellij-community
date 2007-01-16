22ee18cd3abbf0071d1c48315f1073e8a879c0dc
Eugene Vigdorchik <no_reply@jetbrains.com>
2007-01-16 16:30:12 +0300

    test data

--- testData/refactoring/extractMethod/OxfordBug_after.java
+++ testData/refactoring/extractMethod/OxfordBug_after.java
@@ -2,12 +2,12 @@ class OxfordBug {
     private int f(int m, int n) throws Exception {
         int i = 0;
         while (i < n) {
-            i = newMethod(i, m, n);
+            i = newMethod(m, n, i);
         }
         return n;
     }
 
-    private int newMethod(int i, int m, int n) throws Exception {
+    private int newMethod(int m, int n, int i) throws Exception {
         i++;
         if (i == m) {
             n += m;
