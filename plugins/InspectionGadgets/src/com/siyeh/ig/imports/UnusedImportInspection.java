d4864d073478da97a43afb43a08b3a555bef8641
Bas Leijdekkers <basleijdekkers@gmail.com>
2007-02-01 18:19:14 +0300

    cleanup: abstract FileInspection class removed

--- plugins/InspectionGadgets/src/com/siyeh/ig/imports/UnusedImportInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/imports/UnusedImportInspection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2006 Dave Griffith, Bas Leijdekkers
+ * Copyright 2003-2007 Dave Griffith, Bas Leijdekkers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,13 +18,13 @@ package com.siyeh.ig.imports;
 import com.intellij.codeInsight.daemon.GroupNames;
 import com.intellij.psi.*;
 import com.siyeh.InspectionGadgetsBundle;
+import com.siyeh.ig.BaseInspection;
 import com.siyeh.ig.BaseInspectionVisitor;
-import com.siyeh.ig.FileInspection;
 import com.siyeh.ig.InspectionGadgetsFix;
 import com.siyeh.ig.fixes.DeleteImportFix;
 import org.jetbrains.annotations.NotNull;
 
-public class UnusedImportInspection extends FileInspection {
+public class UnusedImportInspection extends BaseInspection {
 
     @NotNull
     public String getDisplayName() {
--- plugins/InspectionGadgets/src/com/siyeh/ig/style/UnnecessarySemicolonInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/style/UnnecessarySemicolonInspection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2006 Dave Griffith, Bas Leijdekkers
+ * Copyright 2003-2007 Dave Griffith, Bas Leijdekkers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,14 +22,20 @@ import com.intellij.psi.*;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.IncorrectOperationException;
+import com.siyeh.InspectionGadgetsBundle;
+import com.siyeh.ig.BaseInspection;
 import com.siyeh.ig.BaseInspectionVisitor;
 import com.siyeh.ig.InspectionGadgetsFix;
-import com.siyeh.ig.FileInspection;
-import com.siyeh.InspectionGadgetsBundle;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class UnnecessarySemicolonInspection extends FileInspection{
+public class UnnecessarySemicolonInspection extends BaseInspection {
+
+    @NotNull
+    public String getDisplayName() {
+        return InspectionGadgetsBundle.message(
+                "unnecessary.semicolon.display.name");
+    }
 
     public String getGroupDisplayName(){
         return GroupNames.STYLE_GROUP_NAME;
@@ -178,6 +184,9 @@ public class UnnecessarySemicolonInspection extends FileInspection{
             final PsiElement parent = statement.getParent();
             if(parent instanceof PsiCodeBlock){
                 final PsiElement semicolon = statement.getFirstChild();
+                if (semicolon == null) {
+                    return;
+                }
                 registerError(semicolon);
             }
         }
