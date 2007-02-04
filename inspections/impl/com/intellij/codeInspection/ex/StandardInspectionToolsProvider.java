ffbb9565577fb58e54f4c0bccdaafd36136ff450
Eugene Vigdorchik <no_reply@jetbrains.com>
2007-02-04 14:59:01 +0300

    unnecessary type parameterization that could be simulated using covariant return types inspection

--- inspections/impl/com/intellij/codeInspection/ex/StandardInspectionToolsProvider.java
+++ inspections/impl/com/intellij/codeInspection/ex/StandardInspectionToolsProvider.java
@@ -26,6 +26,7 @@ import com.intellij.codeInspection.localCanBeFinal.LocalCanBeFinal;
 import com.intellij.codeInspection.miscGenerics.RedundantArrayForVarargsCallInspection;
 import com.intellij.codeInspection.miscGenerics.RedundantTypeArgsInspection;
 import com.intellij.codeInspection.miscGenerics.SuspiciousCollectionsMethodCallsInspection;
+import com.intellij.codeInspection.miscGenerics.UnnecessaryTypeParameterInspection;
 import com.intellij.codeInspection.nullable.NullableStuffInspection;
 import com.intellij.codeInspection.redundantCast.RedundantCastInspection;
 import com.intellij.codeInspection.sameParameterValue.SameParameterValueInspection;
@@ -82,6 +83,7 @@ public class StandardInspectionToolsProvider implements InspectionToolProvider, 
       DefUseInspection.class,
       RedundantCastInspection.class,
       RedundantTypeArgsInspection.class,
+      UnnecessaryTypeParameterInspection.class,
       RedundantArrayForVarargsCallInspection.class,
       SuspiciousCollectionsMethodCallsInspection.class,
       LocalCanBeFinal.class,
