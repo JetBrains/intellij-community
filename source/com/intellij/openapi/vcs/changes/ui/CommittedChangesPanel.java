199dc86399523b220008c3fe659845b7fe27fe88
Dmitry Jemerov <yole@intellij.com>
2007-02-06 21:22:24 +0300

    search in committed changes by description or committer name

--- source/com/intellij/openapi/vcs/changes/ui/CommittedChangesPanel.java
+++ source/com/intellij/openapi/vcs/changes/ui/CommittedChangesPanel.java
@@ -11,6 +11,7 @@
 package com.intellij.openapi.vcs.changes.ui;
 
 import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
@@ -19,10 +20,10 @@ import com.intellij.openapi.util.Ref;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vcs.CommittedChangesProvider;
 import com.intellij.openapi.vcs.VcsException;
-import com.intellij.openapi.vcs.versionBrowser.CommittedChangeList;
 import com.intellij.openapi.vcs.versionBrowser.ChangeBrowserSettings;
+import com.intellij.openapi.vcs.versionBrowser.CommittedChangeList;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.ui.FilterComponent;
 
 import javax.swing.*;
 import java.awt.*;
@@ -38,6 +39,8 @@ public class CommittedChangesPanel extends JPanel {
   private ChangeBrowserSettings mySettings;
   private VirtualFile myRoot;
   private int myMaxCount = 0;
+  private FilterComponent myFilterComponent = new MyFilterComponent();
+  private List<CommittedChangeList> myChangesFromProvider;
 
   public CommittedChangesPanel(Project project, final CommittedChangesProvider provider, final ChangeBrowserSettings settings) {
     super(new BorderLayout());
@@ -47,12 +50,14 @@ public class CommittedChangesPanel extends JPanel {
     myBrowser = new CommittedChangesBrowser(project, new CommittedChangesTableModel(new ArrayList<CommittedChangeList>()));
     add(myBrowser, BorderLayout.CENTER);
 
+    JPanel toolbarPanel = new JPanel(new BorderLayout());
     DefaultActionGroup toolbarActionGroup = new DefaultActionGroup();
     toolbarActionGroup.add(new RefreshAction());
     toolbarActionGroup.add(new FilterAction());
     ActionToolbar toolBar = ActionManager.getInstance().createActionToolbar(ActionPlaces.UNKNOWN, toolbarActionGroup, true);
-    myBrowser.addToolBar(toolBar.getComponent());
-    //myComponent.add(toolBar.getComponent(), BorderLayout.WEST);
+    toolbarPanel.add(toolBar.getComponent(), BorderLayout.WEST);
+    toolbarPanel.add(myFilterComponent, BorderLayout.EAST);
+    myBrowser.addToolBar(toolbarPanel);
   }
 
   public void setRoot(final VirtualFile root) {
@@ -68,14 +73,13 @@ public class CommittedChangesPanel extends JPanel {
     ProgressManager.getInstance().runProcessWithProgressSynchronously(new Runnable() {
       public void run() {
         try {
-          final List<CommittedChangeList> list;
           if (myRoot == null) {
-            list = myProvider.getAllCommittedChanges(mySettings, 50);
+            myChangesFromProvider = myProvider.getAllCommittedChanges(mySettings, 50);
           }
           else {
-            list = myProvider.getCommittedChanges(mySettings, myRoot, myMaxCount);
+            myChangesFromProvider = myProvider.getCommittedChanges(mySettings, myRoot, myMaxCount);
           }
-          myBrowser.setModel(new CommittedChangesTableModel(list, myProvider.getColumns()));
+          updateFilteredModel();
         }
         catch (VcsException ex) {
           refEx.set(ex);
@@ -86,6 +90,38 @@ public class CommittedChangesPanel extends JPanel {
       LOG.info(refEx.get());
       Messages.showErrorDialog(myProject, "Error refreshing view: " + StringUtil.join(refEx.get().getMessages(), "\n"), "Committed Changes");
     }
+  }
+
+  private void updateFilteredModel() {
+    if (myChangesFromProvider == null) {
+      return;
+    }
+    if (StringUtil.isEmpty(myFilterComponent.getFilter())) {
+      myBrowser.setModel(new CommittedChangesTableModel(myChangesFromProvider, myProvider.getColumns()));
+    }
+    else {
+      final String[] strings = myFilterComponent.getFilter().split(" ");
+      for(int i=0; i<strings.length; i++) {
+        strings [i] = strings [i].toLowerCase();
+      }
+      List<CommittedChangeList> filteredChanges = new ArrayList<CommittedChangeList>();
+      for(CommittedChangeList changeList: myChangesFromProvider) {
+        if (changeListMatches(changeList, strings)) {
+          filteredChanges.add(changeList);
+        }
+      }
+        myBrowser.setModel(new CommittedChangesTableModel(filteredChanges, myProvider.getColumns()));
+    }
+  }
+
+  private static boolean changeListMatches(final CommittedChangeList changeList, final String[] filterWords) {
+    for(String word: filterWords) {
+      if (changeList.getComment().toLowerCase().indexOf(word) >= 0 ||
+          changeList.getCommitterName().toLowerCase().indexOf(word) >= 0) {
+        return true;
+      }
+    }
+    return false;
   }
 
   private void setChangesFilter() {
@@ -114,6 +150,16 @@ public class CommittedChangesPanel extends JPanel {
 
     public void actionPerformed(AnActionEvent e) {
       setChangesFilter();
+    }
+  }
+
+  private class MyFilterComponent extends FilterComponent {
+    public MyFilterComponent() {
+      super("COMMITTED_CHANGES_FILTER_HISTORY", 20);
+    }
+
+    public void filter() {
+      updateFilteredModel();
     }
   }
 }
