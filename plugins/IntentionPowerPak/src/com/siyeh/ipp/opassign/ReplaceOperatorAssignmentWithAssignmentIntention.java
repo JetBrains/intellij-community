01bed24897393e3f9d6c5dcf740ea6f3eb66c73b
Bas Leijdekkers <basleijdekkers@gmail.com>
2007-02-08 20:19:37 +0300

    "Replace Operator Assignment with Assignment" intention

---
+++ plugins/IntentionPowerPak/src/com/siyeh/ipp/opassign/ReplaceOperatorAssignmentWithAssignmentIntention.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2007 Bas Leijdekkers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.siyeh.ipp.opassign;
+
+import com.intellij.psi.*;
+import com.intellij.util.IncorrectOperationException;
+import com.siyeh.ipp.base.MutablyNamedIntention;
+import com.siyeh.ipp.base.PsiElementPredicate;
+import com.siyeh.IntentionPowerPackBundle;
+import org.jetbrains.annotations.NotNull;
+
+public class ReplaceOperatorAssignmentWithAssignmentIntention
+        extends MutablyNamedIntention {
+
+    @NotNull
+    protected PsiElementPredicate getElementPredicate() {
+        return new OperatorAssignmentPredicate();
+    }
+
+    protected String getTextForElement(PsiElement element) {
+        final PsiAssignmentExpression assignmentExpression =
+                (PsiAssignmentExpression)element;
+        final PsiJavaToken sign = assignmentExpression.getOperationSign();
+        final String operator = sign.getText();
+        return IntentionPowerPackBundle.message(
+                "replace.operator.assignment.with.assignment.intention.name",
+                operator);
+    }
+
+    protected void processIntention(@NotNull PsiElement element)
+            throws IncorrectOperationException {
+        final PsiAssignmentExpression assignmentExpression =
+                (PsiAssignmentExpression)element;
+        final PsiJavaToken sign = assignmentExpression.getOperationSign();
+        final PsiExpression lhs = assignmentExpression.getLExpression();
+        final PsiExpression rhs = assignmentExpression.getRExpression();
+        final String operand = sign.getText();
+        final String newOperand = operand.substring(0, operand.length() - 2);
+        final String lhsText = lhs.getText();
+        final String rhsText;
+        if (rhs == null) {
+            rhsText = "";
+        } else {
+            rhsText = rhs.getText();
+        }
+        final String expString = lhsText + '=' + lhsText + newOperand + rhsText;
+        replaceExpression(expString, assignmentExpression);
+    }
+}
