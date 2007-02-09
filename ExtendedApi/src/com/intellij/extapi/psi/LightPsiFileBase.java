4783b2a5c9c0472ae69908c04a588fb5a02f5c6e
Maxim Mossienko <Maxim.Mossienko@jetbrains.com>
2007-02-09 18:28:19 +0300

    language dialect

--- ExtendedApi/src/com/intellij/extapi/psi/LightPsiFileBase.java
+++ ExtendedApi/src/com/intellij/extapi/psi/LightPsiFileBase.java
@@ -4,6 +4,8 @@ import com.intellij.psi.impl.source.LightPsiFileImpl;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.FileViewProvider;
 import com.intellij.lang.Language;
+import com.intellij.lang.LanguageDialect;
+import org.jetbrains.annotations.Nullable;
 
 public abstract class LightPsiFileBase extends LightPsiFileImpl {
   public LightPsiFileBase(final FileViewProvider provider, final Language language) {
@@ -16,5 +18,10 @@ public abstract class LightPsiFileBase extends LightPsiFileImpl {
 
   public void accept(PsiElementVisitor visitor) {
     visitor.visitFile(this);
+  }
+
+  @Nullable
+  public LanguageDialect getLanguageDialect() {
+    return null;
   }
 }
