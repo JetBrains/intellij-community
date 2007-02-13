9ed92dbcc1d2c0403ef430a41039ef45400987c9
Dmitry Jemerov <yole@intellij.com>
2007-02-13 14:14:16 +0300

    blame NoClassDefFound to correct plugin

--- action-system/impl/com/intellij/openapi/actionSystem/impl/ActionManagerImpl.java
+++ action-system/impl/com/intellij/openapi/actionSystem/impl/ActionManagerImpl.java
@@ -497,6 +497,10 @@ public final class ActionManagerImpl extends ActionManagerEx implements JDOMExte
       reportActionError(pluginId, "class with name \"" + className + "\" not found");
       return null;
     }
+    catch (NoClassDefFoundError e) {
+      reportActionError(pluginId, "class with name \"" + e.getMessage() + "\" not found");
+      return null;
+    }
     catch (Exception e) {
       final String message = "cannot create class \"" + className + "\"";
       if (pluginId == null) {
