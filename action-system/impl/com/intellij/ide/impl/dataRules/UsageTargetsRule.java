7fba5ed68e1b1694b54ebe1077ed516bb846ca88
Alexey Kudravtsev <cdr@jetbrains.com>
2007-02-13 21:06:45 +0300

    IDEADEV-14399

--- action-system/impl/com/intellij/ide/impl/dataRules/UsageTargetsRule.java
+++ action-system/impl/com/intellij/ide/impl/dataRules/UsageTargetsRule.java
@@ -5,26 +5,23 @@ import com.intellij.find.findUsages.PsiElement2UsageTargetAdapter;
 import com.intellij.navigation.NavigationItem;
 import com.intellij.openapi.actionSystem.DataConstants;
 import com.intellij.openapi.actionSystem.DataProvider;
+import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
 import com.intellij.usages.UsageTarget;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * Created by IntelliJ IDEA.
- * User: max
- * Date: Dec 21, 2004
- * Time: 3:59:35 PM
- * To change this template use File | Settings | File Templates.
+ * @author max
  */
 public class UsageTargetsRule implements GetDataRule {
   @Nullable
   public Object getData(DataProvider dataProvider) {
     PsiElement psiElement = (PsiElement)dataProvider.getData(DataConstants.PSI_ELEMENT);
+    if (psiElement instanceof PsiDirectory) psiElement = ((PsiDirectory)psiElement).getPackage();
     if (psiElement instanceof NavigationItem) {
       if (!FindManager.getInstance(psiElement.getProject()).canFindUsages(psiElement)) return null;
       return new UsageTarget[] {new PsiElement2UsageTargetAdapter(psiElement)};
     }
-
     return null;
   }
 }
