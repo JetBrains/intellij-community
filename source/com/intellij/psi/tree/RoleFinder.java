197461d2eee85dc0e9bbab26cf22985788135080
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2007-02-13 13:43:32 +0300

    @Nullable

--- source/com/intellij/psi/tree/RoleFinder.java
+++ source/com/intellij/psi/tree/RoleFinder.java
@@ -2,7 +2,9 @@ package com.intellij.psi.tree;
 
 import com.intellij.lang.ASTNode;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public interface RoleFinder {
+  @Nullable
   ASTNode findChild(@NotNull ASTNode parent);
 }
