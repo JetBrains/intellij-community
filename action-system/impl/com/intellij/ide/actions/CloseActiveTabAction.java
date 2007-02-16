7fcda20bc94e91204fe9e09795bea13f20824076
Dmitry Jemerov <yole@intellij.com>
2007-02-16 20:39:11 +0300

    disable action for contents which can't be closed (IDEADEV-14523)

--- action-system/impl/com/intellij/ide/actions/CloseActiveTabAction.java
+++ action-system/impl/com/intellij/ide/actions/CloseActiveTabAction.java
@@ -2,6 +2,7 @@ package com.intellij.ide.actions;
 
 import com.intellij.ui.content.ContentManagerUtil;
 import com.intellij.ui.content.ContentManager;
+import com.intellij.ui.content.Content;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.Presentation;
@@ -10,7 +11,10 @@ public class CloseActiveTabAction extends AnAction {
   public void actionPerformed(AnActionEvent e) {
     ContentManager contentManager = ContentManagerUtil.getContentManagerFromContext(e.getDataContext(), true);
     if (contentManager != null && contentManager.canCloseContents()) {
-      contentManager.removeContent(contentManager.getSelectedContent());
+      final Content selectedContent = contentManager.getSelectedContent();
+      if (selectedContent != null && selectedContent.isCloseable()) {
+        contentManager.removeContent(selectedContent);
+      }
     }
   }
 
