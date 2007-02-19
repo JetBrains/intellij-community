3d57b8ef8a982de733f56ec6f89583c72b1ae5eb
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2007-02-19 11:17:56 +0300

    one more try to delete outdated JspWhileStatement.java (from jsp-impl)

--- jsp/openapi/src/com/intellij/psi/impl/source/jsp/jspJava/JspWhileStatement_RenameMe.java
+++
@@ -1,9 +0,0 @@
-package com.intellij.psi.impl.source.jsp.jspJava;
-
-import com.intellij.psi.PsiWhileStatement;
-
-// ACHTUNG!!! Class with name JspWhileStatement already exist in module JEE
-
-// but in the same package!!!
-public interface JspWhileStatement_RenameMe extends PsiWhileStatement {
-}
