1515c3c4a0851c34798fd6f90d298f4eeef6bc95
Hani Suleiman <no_reply@jetbrains.com>
2007-02-20 18:08:33 +0300

    Fix junit intention not showing up

--- plugins/testng/src/com/theoryinpractice/testng/inspection/JUnitConvertTool.java
+++ plugins/testng/src/com/theoryinpractice/testng/inspection/JUnitConvertTool.java
@@ -4,30 +4,38 @@ import com.intellij.codeInspection.*;
 import com.intellij.psi.PsiClass;
 import com.theoryinpractice.testng.util.Intentions;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Hani Suleiman Date: Aug 3, 2005 Time: 3:34:56 AM
  */
 public class JUnitConvertTool extends LocalInspectionTool
 {
+    @NotNull
     @Override
     public String getGroupDisplayName() {
         return "TestNG";
     }
 
+    @NotNull
     @Override
     public String getDisplayName() {
         return "Convert JUnit Tests to TestNG";
     }
 
+    @NotNull
     @Override
     public String getShortName() {
         return "JUnitTestNG";
     }
 
+    public boolean isEnabledByDefault() {
+        return true;
+    }
+    
     @Override
     @Nullable
-    public ProblemDescriptor[] checkClass(PsiClass psiClass, InspectionManager manager, boolean isOnTheFly) {
+    public ProblemDescriptor[] checkClass(@NotNull PsiClass psiClass, @NotNull InspectionManager manager, boolean isOnTheFly) {
         if (isOnTheFly) return null;
         if (!psiClass.getContainingFile().isWritable()) return null;
         boolean isJUnit = Intentions.inheritsJUnitTestCase(psiClass) || Intentions.containsJunitAnnotions(psiClass);
