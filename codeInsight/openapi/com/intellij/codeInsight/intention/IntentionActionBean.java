1be9d311c05668d28c3a7c8ac1ca21e64e93de24
Mike Aizatsky <no_reply@jetbrains.com>
2007-02-26 21:31:00 +0300

    pluginawareness

--- codeInsight/openapi/com/intellij/codeInsight/intention/IntentionActionBean.java
+++ codeInsight/openapi/com/intellij/codeInsight/intention/IntentionActionBean.java
@@ -1,14 +1,17 @@
 package com.intellij.codeInsight.intention;
 
+import com.intellij.openapi.extensions.PluginAware;
+import com.intellij.openapi.extensions.PluginDescriptor;
 import com.thoughtworks.xstream.annotations.XStreamImplicitCollection;
 
 import java.util.List;
 
 @XStreamImplicitCollection(value="categories",item="category")
-public class IntentionActionBean {
+public class IntentionActionBean implements PluginAware {
   private String className;
   private List<String> categories;
   private String descriptionDirectoryName;
+  private PluginDescriptor myPluginDescriptor;
 
 
   public String getClassName() {
@@ -21,5 +24,9 @@ public class IntentionActionBean {
 
   public String getDescriptionDirectoryName() {
     return descriptionDirectoryName;
+  }
+
+  public void setPluginDescriptor(PluginDescriptor pluginDescriptor) {
+    myPluginDescriptor = pluginDescriptor;
   }
 }
