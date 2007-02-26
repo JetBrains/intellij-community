03b025f2a4193bbd9f880ebca289880847eabd99
Mike Aizatsky <no_reply@jetbrains.com>
2007-02-26 21:59:45 +0300

    XStream creation optimization

--- extensions/source/com/intellij/openapi/extensions/impl/ExtensionComponentAdapter.java
+++ extensions/source/com/intellij/openapi/extensions/impl/ExtensionComponentAdapter.java
@@ -18,9 +18,24 @@ package com.intellij.openapi.extensions.impl;
 import com.intellij.openapi.extensions.*;
 import com.intellij.util.pico.AssignableToComponentAdapter;
 import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.annotations.AnnotationProvider;
+import com.thoughtworks.xstream.annotations.AnnotationReflectionConverter;
 import com.thoughtworks.xstream.annotations.Annotations;
+import com.thoughtworks.xstream.converters.basic.*;
+import com.thoughtworks.xstream.converters.collections.*;
+import com.thoughtworks.xstream.converters.enums.EnumConverter;
+import com.thoughtworks.xstream.converters.enums.EnumMapConverter;
+import com.thoughtworks.xstream.converters.enums.EnumSetConverter;
+import com.thoughtworks.xstream.converters.extended.EncodedByteArrayConverter;
+import com.thoughtworks.xstream.converters.extended.FileConverter;
+import com.thoughtworks.xstream.converters.extended.GregorianCalendarConverter;
+import com.thoughtworks.xstream.converters.extended.LocaleConverter;
+import com.thoughtworks.xstream.converters.reflection.ExternalizableConverter;
+import com.thoughtworks.xstream.converters.reflection.ReflectionProvider;
+import com.thoughtworks.xstream.converters.reflection.SerializableConverter;
 import com.thoughtworks.xstream.core.util.CompositeClassLoader;
 import com.thoughtworks.xstream.io.xml.JDomReader;
+import com.thoughtworks.xstream.mapper.Mapper;
 import org.jdom.Element;
 import org.picocontainer.*;
 import org.picocontainer.defaults.AssignabilityRegistrationException;
@@ -71,7 +86,61 @@ public class ExtensionComponentAdapter implements ComponentAdapter, LoadingOrder
           classLoader.add(myPluginDescriptor.getPluginClassLoader());
         }
         //XStream xStream = new XStream(new PropertyReflectionProvider());
-        XStream xStream = new XStream();
+        XStream xStream = new XStream()
+        {
+          @Override
+          protected void setupConverters() {
+            final Mapper mapper = getMapper();
+            final ReflectionProvider reflectionProvider = getReflectionProvider();
+
+              registerConverter(new AnnotationReflectionConverter(mapper, reflectionProvider, new AnnotationProvider()), PRIORITY_LOW);
+              registerConverter(new SerializableConverter(mapper, reflectionProvider), PRIORITY_LOW);
+              registerConverter(new ExternalizableConverter(mapper), PRIORITY_LOW);
+
+              registerConverter(new NullConverter(), PRIORITY_VERY_HIGH);
+              registerConverter(new IntConverter(), PRIORITY_NORMAL);
+              registerConverter(new FloatConverter(), PRIORITY_NORMAL);
+              registerConverter(new DoubleConverter(), PRIORITY_NORMAL);
+              registerConverter(new LongConverter(), PRIORITY_NORMAL);
+              registerConverter(new ShortConverter(), PRIORITY_NORMAL);
+              registerConverter(new CharConverter(), PRIORITY_NORMAL);
+              registerConverter(new BooleanConverter(), PRIORITY_NORMAL);
+              registerConverter(new ByteConverter(), PRIORITY_NORMAL);
+
+              registerConverter(new StringConverter(), PRIORITY_NORMAL);
+              registerConverter(new StringBufferConverter(), PRIORITY_NORMAL);
+              registerConverter(new DateConverter(), PRIORITY_NORMAL);
+              registerConverter(new BitSetConverter(), PRIORITY_NORMAL);
+              registerConverter(new URLConverter(), PRIORITY_NORMAL);
+              registerConverter(new BigIntegerConverter(), PRIORITY_NORMAL);
+              registerConverter(new BigDecimalConverter(), PRIORITY_NORMAL);
+
+              registerConverter(new ArrayConverter(mapper), PRIORITY_NORMAL);
+              registerConverter(new CharArrayConverter(), PRIORITY_NORMAL);
+              registerConverter(new CollectionConverter(mapper), PRIORITY_NORMAL);
+              registerConverter(new MapConverter(mapper), PRIORITY_NORMAL);
+              registerConverter(new TreeMapConverter(mapper), PRIORITY_NORMAL);
+              registerConverter(new TreeSetConverter(mapper), PRIORITY_NORMAL);
+              registerConverter(new PropertiesConverter(), PRIORITY_NORMAL);
+              registerConverter(new EncodedByteArrayConverter(), PRIORITY_NORMAL);
+
+              registerConverter(new FileConverter(), PRIORITY_NORMAL);
+              /*
+              registerConverter(new DynamicProxyConverter(mapper, classLoaderReference), PRIORITY_NORMAL);
+              registerConverter(new JavaClassConverter(classLoaderReference), PRIORITY_NORMAL);
+              registerConverter(new JavaMethodConverter(classLoaderReference), PRIORITY_NORMAL);
+              */
+              registerConverter(new LocaleConverter(), PRIORITY_NORMAL);
+              registerConverter(new GregorianCalendarConverter(), PRIORITY_NORMAL);
+
+              registerConverter(new EnumConverter(), PRIORITY_NORMAL);
+              registerConverter(new EnumSetConverter(mapper), PRIORITY_NORMAL);
+              registerConverter(new EnumMapConverter(mapper), PRIORITY_NORMAL);
+
+
+              //registerConverter(new SelfStreamingInstanceChecker(reflectionConverter, this), PRIORITY_NORMAL);
+          }
+        };
         xStream.setClassLoader(classLoader);
         //xStream.registerConverter(new ElementConverter());
         if (componentInstance instanceof ReaderConfigurator) {
