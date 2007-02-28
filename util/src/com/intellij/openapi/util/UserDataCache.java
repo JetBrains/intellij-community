f0b9e2919e0dbe223fb7270595e37d6a145bde80
Mike Aizatsky <no_reply@jetbrains.com>
2007-02-28 21:09:33 +0300

    Desynchronizing file view provider

--- util/src/com/intellij/openapi/util/UserDataCache.java
+++ util/src/com/intellij/openapi/util/UserDataCache.java
@@ -45,5 +45,26 @@ public abstract class UserDataCache<T, Owner extends UserDataHolder, S> {
     return result;
   }
 
+  public final T getCached(Key<T> key, Owner owner) {
+    r.lock();
+
+    try {
+      return owner.getUserData(key);
+    }
+    finally {
+      r.unlock();
+    }
+  }
+
+  public final void clear(Key<T> key, Owner owner) {
+    w.lock();
+    try {
+      owner.putUserData(key, null);
+    }
+    finally{
+      w.unlock();
+    }
+  }
+
   protected abstract T compute(Owner owner, S s);
 }
