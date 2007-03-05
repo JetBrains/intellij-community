ef6fcbf8600389c41ee476d15d06e685f579ea83
Maxim Mossienko <Maxim.Mossienko@jetbrains.com>
2007-03-05 21:39:28 +0300

    Do correct validation of javascript namespaces (IDEADEV-14015) Need quickfix for unresolved ns (IDEADEV-15115)

--- source/com/intellij/psi/impl/source/tree/PsiCommentImpl.java
+++ source/com/intellij/psi/impl/source/tree/PsiCommentImpl.java
@@ -3,16 +3,18 @@ package com.intellij.psi.impl.source.tree;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.pom.tree.events.TreeChangeEvent;
+import com.intellij.pom.Navigatable;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.source.tree.injected.InjectedLanguageUtil;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.util.CharTable;
+import com.intellij.ide.util.EditSourceUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-public class PsiCommentImpl extends LeafPsiElement implements PsiComment, PsiJavaToken, PsiLanguageInjectionHost {
+public class PsiCommentImpl extends LeafPsiElement implements PsiComment, PsiJavaToken, PsiLanguageInjectionHost, Navigatable {
   public PsiCommentImpl(IElementType type, CharSequence buffer, int startOffset, int endOffset, CharTable table) {
     super(type, buffer, startOffset, endOffset, table);
   }
@@ -40,5 +42,17 @@ public class PsiCommentImpl extends LeafPsiElement implements PsiComment, PsiJav
         setText(text);
       }
     });
+  }
+
+  public void navigate(boolean requestFocus) {
+    EditSourceUtil.getDescriptor(this).navigate(true);
+  }
+
+  public boolean canNavigate() {
+    return EditSourceUtil.canNavigate(this); // js var maybe defined via comment annotation
+  }
+
+  public boolean canNavigateToSource() {
+    return canNavigate();
   }
 }
