46ca9382639ee2440a3587840e5e329977bc4264
Dmitry Krasilschikov <Dmitry.Krasilschikov@@jetbrains.com>
2007-03-14 16:24:42 +0300

    (no message)

--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/parser/parsing/statements/declaration/DeclarationStart.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/parser/parsing/statements/declaration/DeclarationStart.java
@@ -1,19 +1,18 @@
 package org.jetbrains.plugins.groovy.lang.parser.parsing.statements.declaration;
 
-import org.jetbrains.plugins.groovy.lang.parser.parsing.Construction;
-import org.jetbrains.plugins.groovy.lang.parser.parsing.util.ParserUtils;
-import org.jetbrains.plugins.groovy.lang.lexer.GroovyElementType;
 import com.intellij.lang.PsiBuilder;
 import com.intellij.psi.tree.IElementType;
+import org.jetbrains.plugins.groovy.lang.parser.parsing.Construction;
+import org.jetbrains.plugins.groovy.lang.parser.parsing.util.ParserUtils;
 
 /**
  * @autor: Dmitry.Krasilschikov
  * @date: 14.03.2007
  */
 public class DeclarationStart implements Construction {
-
     public static IElementType parse(PsiBuilder builder) {
         if (ParserUtils.getToken(builder, kDEF)) return kDEF;
+        
         return WRONGWAY;
     }
 }
