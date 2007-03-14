d089ae426d98826f9fc6fd7eb0055d2a9439f22a
Dmitry Krasilschikov <Dmitry.Krasilschikov@@jetbrains.com>
2007-03-14 19:14:09 +0300

    (no message)

--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/parser/parsing/statements/declaration/DeclarationStart.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/parser/parsing/statements/declaration/DeclarationStart.java
@@ -3,6 +3,10 @@ package org.jetbrains.plugins.groovy.lang.parser.parsing.statements.declaration;
 import com.intellij.lang.PsiBuilder;
 import com.intellij.psi.tree.IElementType;
 import org.jetbrains.plugins.groovy.lang.parser.parsing.Construction;
+import org.jetbrains.plugins.groovy.lang.parser.parsing.types.BuiltlnType;
+import org.jetbrains.plugins.groovy.lang.parser.parsing.types.QualifiedTypeName;
+import org.jetbrains.plugins.groovy.lang.parser.parsing.identifier.UpperCaseIdent;
+import org.jetbrains.plugins.groovy.lang.parser.parsing.identifier.Ident;
 import org.jetbrains.plugins.groovy.lang.parser.parsing.auxiliary.Modifier;
 import org.jetbrains.plugins.groovy.lang.parser.parsing.util.ParserUtils;
 
@@ -12,10 +16,32 @@ import org.jetbrains.plugins.groovy.lang.parser.parsing.util.ParserUtils;
  */
 public class DeclarationStart implements Construction {
   public static IElementType parse(PsiBuilder builder) {
+    IElementType elementType;
+
     if (ParserUtils.getToken(builder, kDEF)) return kDEF;
-    if (ParserUtils.lookAhead(builder, Modifier.first)) return Modifier.parse(builder);
-    if (ParserUtils.getToken(builder, mAT)) return mAT;
+
+    elementType = Modifier.parse(builder);
+    if (!WRONG_SET.contains(elementType)) return elementType;
+
+    PsiBuilder.Marker declStartMarker = builder.mark();
+    if (ParserUtils.getToken(builder, mAT)) {
+      elementType = Ident.parse(builder);
+
+      if (!WRONG_SET.contains(elementType)) {
+        declStartMarker.done(DECLARATION_START);
+        return DECLARATION_START;
+      } else {
+        declStartMarker.drop();
+      }
+    }
+
+    elementType = UpperCaseIdent.parse(builder);
+    if (!WRONG_SET.contains(elementType)) {
+      
+    }
 
     return WRONGWAY;
+
+
   }
 }
