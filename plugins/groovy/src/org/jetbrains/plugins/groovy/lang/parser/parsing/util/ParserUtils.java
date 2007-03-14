4e6a504b555a16134e1c8607d60a4c6762c1c926
Dmitry Krasilschikov <Dmitry.Krasilschikov@@jetbrains.com>
2007-03-14 16:47:12 +0300

    (no message)

--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/parser/parsing/util/ParserUtils.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/parser/parsing/util/ParserUtils.java
@@ -11,51 +11,51 @@ import com.intellij.psi.tree.IElementType;
  */
 public abstract class ParserUtils {
 
-    /**
-     * Auxiliary method for strict token appearance
-     *
-     * @param builder  current builder
-     * @param elem     given element
-     * @param errorMsg Message, that displays if element was not found; if errorMsg == null nothing displays
-     * @return true if element parsed
-     */
-    public static boolean getToken(PsiBuilder builder, IElementType elem, String errorMsg) {
-        if (elem.equals(builder.getTokenType())) {
-            builder.advanceLexer();
-            return true;
-        } else {
-            if (errorMsg != null)
-                builder.error(errorMsg);
-            return false;
-        }
+  /**
+   * Auxiliary method for strict token appearance
+   *
+   * @param builder  current builder
+   * @param elem     given element
+   * @param errorMsg Message, that displays if element was not found; if errorMsg == null nothing displays
+   * @return true if element parsed
+   */
+  public static boolean getToken(PsiBuilder builder, IElementType elem, String errorMsg) {
+    if (elem.equals(builder.getTokenType())) {
+      builder.advanceLexer();
+      return true;
+    } else {
+      if (errorMsg != null)
+        builder.error(errorMsg);
+      return false;
     }
+  }
 
-    /**
-     * Auxiliary method for construction like
-     * <BNF>
-     * token?
-     * </BNF>
-     * parsing
-     *
-     * @param builder current builder
-     * @param elem    given element
-     * @return true if element parsed
-     */
-    public static boolean getToken(PsiBuilder builder, IElementType elem) {
-        return getToken(builder, elem, null);
-    }
+  /**
+   * Auxiliary method for construction like
+   * <BNF>
+   * token?
+   * </BNF>
+   * parsing
+   *
+   * @param builder current builder
+   * @param elem    given element
+   * @return true if element parsed
+   */
+  public static boolean getToken(PsiBuilder builder, IElementType elem) {
+    return getToken(builder, elem, null);
+  }
 
   /**
    * Checks, that following element sequence is like given
    *
    * @param builder Given PsiBuilder
-   * @param elems Array of need elements in order
+   * @param elems   Array of need elements in order
    * @return true if following sequence is like a given
    */
-  public static boolean lookAhead(PsiBuilder builder, IElementType... elems ){
+  public static boolean lookAhead(PsiBuilder builder, IElementType... elems) {
     PsiBuilder.Marker rb = builder.mark();
     int i = 0;
-    for (; !builder.eof() && i < elems.length && elems[i].equals(builder.getTokenType()) ;) {
+    for (; !builder.eof() && i < elems.length && elems[i].equals(builder.getTokenType());) {
       builder.advanceLexer();
       i++;
     }
