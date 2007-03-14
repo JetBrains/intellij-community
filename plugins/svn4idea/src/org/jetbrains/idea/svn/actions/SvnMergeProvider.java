40ccac78a709a113d8af213c68f1266d2d101b4d
Dmitry Jemerov <yole@intellij.com>
2007-03-14 16:10:00 +0300

    showing progress directly from merge dialog breaks CVS (which shows UI from its loadRevisions() implementation); show progress directly in MergeProvider implementations instead

--- plugins/svn4idea/src/org/jetbrains/idea/svn/actions/SvnMergeProvider.java
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/actions/SvnMergeProvider.java
@@ -18,9 +18,12 @@ package org.jetbrains.idea.svn.actions;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vcs.VcsException;
+import com.intellij.openapi.vcs.VcsBundle;
 import com.intellij.openapi.vcs.merge.MergeData;
 import com.intellij.openapi.vcs.merge.MergeProvider;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.vcsUtil.VcsRunnable;
+import com.intellij.vcsUtil.VcsUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.idea.svn.SvnVcs;
 import org.jetbrains.idea.svn.SvnRevisionNumber;
@@ -34,11 +37,8 @@ import java.io.File;
 import java.io.IOException;
 
 /**
- * Created by IntelliJ IDEA.
- * User: lesya
- * Date: Jul 29, 2005
- * Time: 1:12:23 AM
- * To change this template use File | Settings | File Templates.
+ * @author lesya
+ * @author yole
  */
 public class SvnMergeProvider implements MergeProvider {
 
@@ -49,43 +49,48 @@ public class SvnMergeProvider implements MergeProvider {
   }
 
   @NotNull
-  public MergeData loadRevisions(VirtualFile file) throws VcsException {
-    MergeData data = new MergeData();
-    SvnVcs vcs = SvnVcs.getInstance(myProject);
-    File oldFile = null;
-    File newFile = null;
-    File workingFile = null;
-    SVNWCClient client;
-    try {
-      client = vcs.createWCClient();
-      SVNInfo info = client.doInfo(new File(file.getPath()), SVNRevision.WORKING);
-      if (info != null) {
-        oldFile = info.getConflictOldFile();
-        newFile = info.getConflictNewFile();
-        workingFile = info.getConflictWrkFile();
-        data.LAST_REVISION_NUMBER = new SvnRevisionNumber(info.getRevision());
+  public MergeData loadRevisions(final VirtualFile file) throws VcsException {
+    final MergeData data = new MergeData();
+    VcsRunnable runnable = new VcsRunnable() {
+      public void run() throws VcsException {
+        SvnVcs vcs = SvnVcs.getInstance(myProject);
+        File oldFile = null;
+        File newFile = null;
+        File workingFile = null;
+        SVNWCClient client;
+        try {
+          client = vcs.createWCClient();
+          SVNInfo info = client.doInfo(new File(file.getPath()), SVNRevision.WORKING);
+          if (info != null) {
+            oldFile = info.getConflictOldFile();
+            newFile = info.getConflictNewFile();
+            workingFile = info.getConflictWrkFile();
+            data.LAST_REVISION_NUMBER = new SvnRevisionNumber(info.getRevision());
+          }
+        }
+        catch (SVNException e) {
+          throw new VcsException(e);
+        }
+        if (oldFile == null || newFile == null || workingFile == null) {
+          ByteArrayOutputStream bos = new ByteArrayOutputStream();
+          try {
+            client.doGetFileContents(new File(file.getPath()), SVNRevision.UNDEFINED, SVNRevision.BASE, true, bos);
+          }
+          catch (SVNException e) {
+            //
+          }
+          data.ORIGINAL = bos.toByteArray();
+          data.LAST = bos.toByteArray();
+          data.CURRENT = readFile(new File(file.getPath()));
+        }
+        else {
+          data.ORIGINAL = readFile(oldFile);
+          data.LAST = readFile(newFile);
+          data.CURRENT = readFile(workingFile);
+        }
       }
-    }
-    catch (SVNException e) {
-      throw new VcsException(e);
-    }
-    if (oldFile == null || newFile == null || workingFile == null) {
-      ByteArrayOutputStream bos = new ByteArrayOutputStream();
-      try {
-        client.doGetFileContents(new File(file.getPath()), SVNRevision.UNDEFINED, SVNRevision.BASE, true, bos);
-      }
-      catch (SVNException e) {
-        //
-      }
-      data.ORIGINAL = bos.toByteArray();
-      data.LAST = bos.toByteArray();
-      data.CURRENT = readFile(new File(file.getPath()));
-    }
-    else {
-      data.ORIGINAL = readFile(oldFile);
-      data.LAST = readFile(newFile);
-      data.CURRENT = readFile(workingFile);
-    }
+    };
+    VcsUtil.runVcsProcessWithProgress(runnable, VcsBundle.message("multiple.file.merge.loading.progress.title"), false, myProject);
 
     return data;
   }
