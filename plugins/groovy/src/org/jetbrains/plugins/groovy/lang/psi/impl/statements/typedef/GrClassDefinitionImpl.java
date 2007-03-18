a7982161915643d9884a4977f6d15aade5d400f0
Dmitry Krasilschikov <Dmitry.Krasilschikov@@jetbrains.com>
2007-03-18 18:03:33 +0300

    added GrClassDefinition and GrInterfaceDefinition interfaces

--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/impl/statements/typedef/GrClassDefinitionImpl.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/impl/statements/typedef/GrClassDefinitionImpl.java
@@ -1,7 +1,7 @@
 package org.jetbrains.plugins.groovy.lang.psi.impl.statements.typedef;
 
 import org.jetbrains.plugins.groovy.lang.psi.impl.GroovyPsiElementImpl;
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.GrClassDef;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.GrClassDefinition;
 import org.jetbrains.annotations.NotNull;
 import com.intellij.lang.ASTNode;
 
@@ -9,7 +9,7 @@ import com.intellij.lang.ASTNode;
  * @autor: Dmitry.Krasilschikov
  * @date: 16.03.2007
  */
-public class GrClassDefinitionImpl extends GroovyPsiElementImpl implements GrClassDef {
+public class GrClassDefinitionImpl extends GroovyPsiElementImpl implements GrClassDefinition {
   public GrClassDefinitionImpl(@NotNull ASTNode node) {
     super(node);
   }
--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/impl/statements/typedef/GrInterfaceDefinitionImpl.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/impl/statements/typedef/GrInterfaceDefinitionImpl.java
@@ -1,7 +1,7 @@
 package org.jetbrains.plugins.groovy.lang.psi.impl.statements.typedef;
 
 import org.jetbrains.plugins.groovy.lang.psi.impl.GroovyPsiElementImpl;
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.GrInterfaceDef;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.GrInterfaceDefinition;
 import org.jetbrains.annotations.NotNull;
 import com.intellij.lang.ASTNode;
 
@@ -9,7 +9,7 @@ import com.intellij.lang.ASTNode;
  * @autor: Dmitry.Krasilschikov
  * @date: 16.03.2007
  */
-public class GrInterfaceDefinitionImpl extends GroovyPsiElementImpl implements GrInterfaceDef {
+public class GrInterfaceDefinitionImpl extends GroovyPsiElementImpl implements GrInterfaceDefinition {
   public GrInterfaceDefinitionImpl(@NotNull ASTNode node) {
     super(node);
   }
