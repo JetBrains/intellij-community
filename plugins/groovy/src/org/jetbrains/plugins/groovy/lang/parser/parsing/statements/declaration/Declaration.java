d0943b762173c43ec93e2955e33a08a22974e471
Dmitry Krasilschikov <Dmitry.Krasilschikov@@jetbrains.com>
2007-03-23 21:35:40 +0300

    (no message)

---
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/parser/parsing/statements/declaration/Declaration.java
@@ -0,0 +1,46 @@
+package org.jetbrains.plugins.groovy.lang.parser.parsing.statements.declaration;
+
+import com.intellij.lang.PsiBuilder;
+import com.intellij.psi.tree.IElementType;
+import org.jetbrains.plugins.groovy.lang.parser.parsing.types.TypeSpec;
+import org.jetbrains.plugins.groovy.lang.parser.parsing.auxiliary.modifiers.Modifiers;
+import org.jetbrains.plugins.groovy.lang.parser.GroovyElementTypes;
+import org.jetbrains.plugins.groovy.lang.lexer.GroovyElementType;
+import org.jetbrains.plugins.groovy.GroovyBundle;
+
+/**
+ * @autor: Dmitry.Krasilschikov
+ * @date: 14.03.2007
+ */
+
+/*
+ * Declaration ::= modifiers [TypeSpec] VariableDefinitions
+ *                  | TypeSpec VariableDefinitions
+ */
+
+public class Declaration implements GroovyElementTypes {
+  public static GroovyElementType parse(PsiBuilder builder) {
+    IElementType modifiers = Modifiers.parse(builder);
+
+    if (!tWRONG_SET.contains(modifiers)) {
+      TypeSpec.parse(builder);
+
+      if (tWRONG_SET.contains(VariableDefinitions.parse(builder))) {
+//        builder.error(GroovyBundle.message("variable.definitions.expected"));
+        return WRONGWAY;
+      }
+    } else {
+      if (tWRONG_SET.contains(TypeSpec.parse(builder))) {
+//        builder.error(GroovyBundle.message("type.specification.expected"));
+        return WRONGWAY;
+      }
+
+      if (tWRONG_SET.contains(VariableDefinitions.parse(builder))) {
+//        builder.error(GroovyBundle.message("variable.definitions.expected"));
+        return WRONGWAY;
+      }
+    }
+
+    return DECLARATION;
+  }
+}
