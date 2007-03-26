fd57c0d41e6c54a668662574a7c9019b1fdbb24c
Alexey Kudravtsev <cdr@jetbrains.com>
2007-03-26 22:10:29 +0400

    IDEADEV-13008

--- source/com/intellij/openapi/editor/actions/ToggleShowWhitespacesAction.java
+++ source/com/intellij/openapi/editor/actions/ToggleShowWhitespacesAction.java
@@ -9,30 +9,31 @@
 package com.intellij.openapi.editor.actions;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DataConstants;
+import com.intellij.openapi.actionSystem.DataKeys;
 import com.intellij.openapi.actionSystem.ToggleAction;
 import com.intellij.openapi.editor.Editor;
 
 public class ToggleShowWhitespacesAction extends ToggleAction {
-
   public void setSelected(AnActionEvent e, boolean state) {
     getEditor(e).getSettings().setWhitespacesShown(state);
     getEditor(e).getComponent().repaint();
   }
 
   public boolean isSelected(AnActionEvent e) {
-    return getEditor(e).getSettings().isWhitespacesShown();
+    Editor editor = getEditor(e);
+    return editor != null && editor.getSettings().isWhitespacesShown();
   }
 
-  private Editor getEditor(AnActionEvent e) {
-    return (Editor) e.getDataContext().getData(DataConstants.EDITOR);
+  private static Editor getEditor(AnActionEvent e) {
+    return e.getData(DataKeys.EDITOR);
   }
 
-  public void update(AnActionEvent e){
+  public void update(AnActionEvent e) {
     if (getEditor(e) == null) {
       e.getPresentation().setEnabled(false);
       e.getPresentation().setVisible(false);
-    } else {
+    }
+    else {
       e.getPresentation().setEnabled(true);
       e.getPresentation().setVisible(true);
       super.update(e);
