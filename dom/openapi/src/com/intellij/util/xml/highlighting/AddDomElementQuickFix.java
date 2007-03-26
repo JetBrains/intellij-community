ed05735ffc30117ba3b9c79101dee47b2a9850b5
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2007-03-26 17:25:31 +0400

    AddDomElementQuickFix

---
+++ dom/openapi/src/com/intellij/util/xml/highlighting/AddDomElementQuickFix.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2000-2007 JetBrains s.r.o. All Rights Reserved.
+ */
+
+package com.intellij.util.xml.highlighting;
+
+import com.intellij.codeInspection.LocalQuickFix;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.xml.XmlTag;
+import com.intellij.util.xml.DomBundle;
+import com.intellij.util.xml.DomElement;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author Dmitry Avdeev
+ */
+public class AddDomElementQuickFix<T extends DomElement> implements LocalQuickFix {
+
+  protected final T myElement;
+
+  public AddDomElementQuickFix(@NotNull T element) {
+    myElement = element;
+  }
+
+  @NotNull
+  public String getName() {
+    final String name = myElement.getXmlElementName();
+    return isTag() ?
+           DomBundle.message("add.element.fix.name", name) :
+           DomBundle.message("add.attribute.fix.name", name);
+  }
+
+  private boolean isTag() {
+    return myElement.getXmlElement() instanceof XmlTag;
+  }
+
+  @NotNull
+  public String getFamilyName() {
+    return DomBundle.message("quick.fixes.family");
+  }
+
+  public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
+    myElement.ensureXmlElementExists();
+  }
+}
