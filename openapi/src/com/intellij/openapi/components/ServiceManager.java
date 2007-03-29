b45879117a3e036578300c92588fd15da8eabe9d
Mike Aizatsky <no_reply@jetbrains.com>
2007-03-29 15:24:35 +0400

    Added javadoc

--- openapi/src/com/intellij/openapi/components/ServiceManager.java
+++ openapi/src/com/intellij/openapi/components/ServiceManager.java
@@ -4,6 +4,11 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
 
+
+/**
+ * For old-style components, the contract specifies a lifecycle: the component gets created and notified during the project opening process.
+ * For services, there's no such contract, so we don't even load the class implementing the service until someone requests it. 
+ */
 @SuppressWarnings({"unchecked"})
 public class ServiceManager {
   private ServiceManager() {
