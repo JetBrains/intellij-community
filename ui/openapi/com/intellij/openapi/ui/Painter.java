adacca53966efff61c5036f8f94df4fe2dd27dd4
Kirill Kalishev <Kirill.Kalishev@jetbrains.com>
2007-03-29 11:57:07 +0400

    duplicates form paint fix

--- ui/openapi/com/intellij/openapi/ui/Painter.java
+++ ui/openapi/com/intellij/openapi/ui/Painter.java
@@ -1,6 +1,10 @@
 package com.intellij.openapi.ui;
 
+import com.intellij.util.ObjectUtils;
+
 import java.awt.*;
+
+import org.jetbrains.annotations.Nullable;
 
 public interface Painter {
 
@@ -17,6 +21,21 @@ public interface Painter {
 
     public void setNeedsRepaint(final boolean needsRepaint) {
       myNeedsRepaint = needsRepaint;
+    }
+
+    @Nullable
+    public <T> T setNeedsRepaint(T oldValue, T newValue) {
+      if (!myNeedsRepaint) {
+        if (oldValue != null) {
+          myNeedsRepaint = !oldValue.equals(newValue);
+        } else if (newValue != null) {
+          myNeedsRepaint = !newValue.equals(oldValue);
+        } else {
+          myNeedsRepaint = false;
+        }
+      }
+
+      return newValue;
     }
 
     public final void paint(final Component component, final Graphics2D g) {
