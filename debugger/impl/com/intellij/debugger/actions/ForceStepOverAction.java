011c05672a81859ab68533ab22b76539ceb4c1ed
Maxim Shafirov <max@jetbrains.com>
2007-04-01 20:32:15 +0400

    Duplication removed.

--- debugger/impl/com/intellij/debugger/actions/ForceStepOverAction.java
+++ debugger/impl/com/intellij/debugger/actions/ForceStepOverAction.java
@@ -1,43 +1,19 @@
 package com.intellij.debugger.actions;
 
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DataConstants;
-import com.intellij.openapi.actionSystem.Presentation;
-import com.intellij.openapi.project.Project;
-import com.intellij.debugger.DebuggerManagerEx;
 import com.intellij.debugger.impl.DebuggerSession;
-import com.intellij.debugger.impl.DebuggerContextImpl;
-import com.intellij.debugger.DebuggerManagerEx;
-import com.intellij.debugger.engine.SuspendContextImpl;
+import com.intellij.openapi.actionSystem.AnActionEvent;
 
 /*
  * Copyright (c) 2000-2004 by JetBrains s.r.o. All Rights Reserved.
  * Use is subject to license terms.
  */
 
-public class ForceStepOverAction extends AnAction {
+public class ForceStepOverAction extends AbstractSteppingAction {
   public void actionPerformed(AnActionEvent e) {
-    Project project = (Project)e.getDataContext().getData(DataConstants.PROJECT);
-    if (project == null) {
-      return;
+    final DebuggerSession session = getSession(e);
+    if (session != null) {
+      session.stepOver(true);
     }
-    (DebuggerManagerEx.getInstanceEx(project)).getContext().getDebuggerSession().stepOver(true);
-  }
-
-  public void update(AnActionEvent event){
-    Presentation presentation = event.getPresentation();
-    Project project = (Project)event.getDataContext().getData(DataConstants.PROJECT);
-    if (project == null) {
-      presentation.setEnabled(false);
-      return;
-    }
-    final DebuggerContextImpl context = (DebuggerManagerEx.getInstanceEx(project)).getContext();
-    DebuggerSession debuggerSession = context.getDebuggerSession();
-    final boolean isPaused = debuggerSession != null && debuggerSession.isPaused();
-    final SuspendContextImpl suspendContext = context.getSuspendContext();
-    final boolean hasCurrentThread = suspendContext != null && suspendContext.getThread() != null;
-    presentation.setEnabled(isPaused && hasCurrentThread);
   }
 }
 
--- debugger/impl/com/intellij/debugger/actions/StepIntoAction.java
+++ debugger/impl/com/intellij/debugger/actions/StepIntoAction.java
@@ -1,36 +1,13 @@
 package com.intellij.debugger.actions;
 
-import com.intellij.debugger.DebuggerManagerEx;
-import com.intellij.debugger.engine.SuspendContextImpl;
-import com.intellij.debugger.impl.DebuggerContextImpl;
 import com.intellij.debugger.impl.DebuggerSession;
-import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DataConstants;
-import com.intellij.openapi.actionSystem.Presentation;
-import com.intellij.openapi.project.Project;
 
-public class StepIntoAction extends AnAction {
+public class StepIntoAction extends AbstractSteppingAction {
   public void actionPerformed(AnActionEvent e) {
-    Project project = (Project)e.getDataContext().getData(DataConstants.PROJECT);
-    if (project == null) {
-      return;
+    final DebuggerSession session = getSession(e);
+    if (session != null) {
+      session.stepInto(false, null);
     }
-    (DebuggerManagerEx.getInstanceEx(project)).getContext().getDebuggerSession().stepInto(false, null);
-  }
-
-  public void update(AnActionEvent event){
-    Presentation presentation = event.getPresentation();
-    Project project = (Project)event.getDataContext().getData(DataConstants.PROJECT);
-    if (project == null) {
-      presentation.setEnabled(false);
-      return;
-    }
-    final DebuggerContextImpl context = (DebuggerManagerEx.getInstanceEx(project)).getContext();
-    DebuggerSession debuggerSession = context.getDebuggerSession();
-    final boolean isPaused = debuggerSession != null && debuggerSession.isPaused();
-    final SuspendContextImpl suspendContext = context.getSuspendContext();
-    final boolean hasCurrentThread = suspendContext != null && suspendContext.getThread() != null;
-    presentation.setEnabled(isPaused && hasCurrentThread);
   }
 }
