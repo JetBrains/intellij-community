fcfa2e88be1ea637e71bdb82ba8590f1c10425a5
Maxim Mossienko <Maxim.Mossienko@jetbrains.com>
2007-04-14 15:05:30 +0400

    Meaningful assertion error message in VirtualFilePointer (IDEADEV-16210)

--- source/com/intellij/openapi/vfs/impl/VirtualFilePointerImpl.java
+++ source/com/intellij/openapi/vfs/impl/VirtualFilePointerImpl.java
@@ -22,14 +22,14 @@ public class VirtualFilePointerImpl extends UserDataHolderBase implements Virtua
   @NonNls private static final String ATTRIBUTE_URL = "url";
 
   VirtualFilePointerImpl(VirtualFile file, VirtualFileManager virtualFileManager) {
-    LOG.assertTrue(file != null);
+    LOG.assertTrue(file != null,"Virtual file should not be null!");
     myFile = file;
     myUrl = null;
     myVirtualFileManager = virtualFileManager;
   }
 
   VirtualFilePointerImpl(String url, VirtualFileManager virtualFileManager) {
-    LOG.assertTrue(url != null);
+    LOG.assertTrue(url != null, "Url should not be null!");
     myFile = null;
     myUrl = url;
     myVirtualFileManager = virtualFileManager;
