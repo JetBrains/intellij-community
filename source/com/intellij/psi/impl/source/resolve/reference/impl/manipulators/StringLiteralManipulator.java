ebc2533f2a642d6e6ac3f2eafecec651210a9ae8
greg <Gregory.Shrago@jetbrains.com>
2007-04-16 20:53:35 +0400

    minor fix

--- source/com/intellij/psi/impl/source/resolve/reference/impl/manipulators/StringLiteralManipulator.java
+++ source/com/intellij/psi/impl/source/resolve/reference/impl/manipulators/StringLiteralManipulator.java
@@ -21,6 +21,6 @@ public class StringLiteralManipulator extends AbstractElementManipulator<PsiLite
   }
 
   public TextRange getRangeInElement(final PsiLiteralExpression element) {
-    return new TextRange(1, element.getTextLength() - 1);
+    return new TextRange(1, Math.max(1, element.getTextLength() - 1));
   }
 }
