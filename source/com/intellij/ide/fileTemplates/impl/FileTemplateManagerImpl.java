3fa73243448a35bbb89f33caae37f6eb5418fbb7
Anton Makeev <Anton.Makeev@jetbrains.com>
2007-04-24 15:45:18 +0400

    compilation fixed

--- source/com/intellij/ide/fileTemplates/impl/FileTemplateManagerImpl.java
+++ source/com/intellij/ide/fileTemplates/impl/FileTemplateManagerImpl.java
@@ -17,6 +17,7 @@ import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileManager;
+import com.intellij.util.SystemProperties;
 import org.jdom.Element;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
