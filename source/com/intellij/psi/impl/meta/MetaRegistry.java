77c2d5b038edf425bc8d39d016b8d222ee41f326
Maxim Mossienko <Maxim.Mossienko@jetbrains.com>
2007-04-26 01:01:51 +0400

    Element resolving disappeared after Block Comment/Uncomment (IDEADEV-16453)

--- source/com/intellij/psi/impl/meta/MetaRegistry.java
+++ source/com/intellij/psi/impl/meta/MetaRegistry.java
@@ -6,6 +6,7 @@ import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.Key;
+import com.intellij.openapi.util.UserDataCache;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.filters.*;
 import com.intellij.psi.filters.position.NamespaceFilter;
@@ -131,7 +132,7 @@ public class MetaRegistry extends MetaDataRegistrar {
 
   }
 
-  public static final Key<SoftReference<CachedValue<PsiMetaDataBase>>> META_DATA_KEY = Key.create("META DATA KEY");
+  private static final Key<SoftReference<CachedValue<PsiMetaDataBase>>> META_DATA_KEY = Key.create("META DATA KEY");
 
   public static void bindDataToElement(final PsiElement element, final PsiMetaDataBase data){
     SoftReference<CachedValue<PsiMetaDataBase>> value = new SoftReference<CachedValue<PsiMetaDataBase>>(
@@ -144,64 +145,43 @@ public class MetaRegistry extends MetaDataRegistrar {
     element.putUserData(META_DATA_KEY, value);
   }
 
-  public MetaRegistry() {
-    // RegisterInPsi.metaData(this);
-  }
-
-  private final static SoftReference<CachedValue<PsiMetaDataBase>> NULL = new SoftReference<CachedValue<PsiMetaDataBase>>(null);
-
   public static PsiMetaData getMeta(final PsiElement element) {
     final PsiMetaDataBase base = getMetaBase(element);
     return base instanceof PsiMetaData ? (PsiMetaData)base : null;
   }
 
-  public static PsiMetaDataBase getMetaBase(final PsiElement element) {
-    ProgressManager.getInstance().checkCanceled();
-    PsiMetaDataBase ret = null;
-    SoftReference<CachedValue<PsiMetaDataBase>> value = element.getUserData(META_DATA_KEY);
-    if (value == null || (value != NULL && value.get() == null)) {
-      for (final MyBinding binding : ourBindings) {
-        try {
-          if (isAcceptable(binding.myFilter, element)) {
-            final PsiMetaDataBase data = binding.myDataClass.newInstance();
-            final CachedValue<PsiMetaDataBase> cachedValue = element.getManager().getCachedValuesManager()
-              .createCachedValue(new CachedValueProvider<PsiMetaDataBase>() {
-                public Result<PsiMetaDataBase> compute() {
-                  if (!isAcceptable(binding.myFilter, element)) {
-                    clearMetaForElement(element);
-                    final PsiMetaDataBase data1 = getMeta(element);
-                    if (data1 == null) return new Result<PsiMetaDataBase>(null);
-                    return new Result<PsiMetaDataBase>(data1, data1.getDependences());
-                  }
+  private static UserDataCache<SoftReference<CachedValue<PsiMetaDataBase>>, PsiElement, Object> ourCachedMetaCache =
+    new UserDataCache<SoftReference<CachedValue<PsiMetaDataBase>>, PsiElement, Object>() {
 
+      protected SoftReference<CachedValue<PsiMetaDataBase>> compute(final PsiElement element, Object p) {
+        final CachedValue<PsiMetaDataBase> cachedValue = element.getManager().getCachedValuesManager()
+        .createCachedValue(new CachedValueProvider<PsiMetaDataBase>() {
+          public Result<PsiMetaDataBase> compute() {
+            try {
+              for (final MyBinding binding : ourBindings) {
+                if (binding.myFilter.isClassAcceptable(element.getClass()) && binding.myFilter.isAcceptable(element, element.getParent())) {
+                  final PsiMetaDataBase data = binding.myDataClass.newInstance();
                   data.init(element);
                   return new Result<PsiMetaDataBase>(data, data.getDependences());
                 }
-              }, false);
-            value = new SoftReference<CachedValue<PsiMetaDataBase>>(cachedValue);
-            ret = cachedValue.getValue();
-            break;
+              }
+            } catch (IllegalAccessException iae) {
+              throw new RuntimeException(iae);
+            }
+            catch (InstantiationException ie) {
+              throw new RuntimeException(ie);
+            }
+
+            return new Result<PsiMetaDataBase>(null, element);
           }
-        }
-        catch (IllegalAccessException iae) {
-          throw new RuntimeException(iae);
-        }
-        catch (InstantiationException ie) {
-          throw new RuntimeException(ie);
-        }
+        }, false);
+        return new SoftReference<CachedValue<PsiMetaDataBase>>(cachedValue);
       }
-      element.putUserData(META_DATA_KEY, value != null ? value : NULL);
-    }
-    else if(value != NULL){
-      ret = value.get().getValue();
-    }
-
-    return ret;
-  }
-
-  private static boolean isAcceptable(final ElementFilter filter, final PsiElement element) {
-    return filter.isClassAcceptable(element.getClass()) &&
-        filter.isAcceptable(element, element.getParent());
+    };
+  
+  public static PsiMetaDataBase getMetaBase(final PsiElement element) {
+    ProgressManager.getInstance().checkCanceled();
+    return ourCachedMetaCache.get(META_DATA_KEY, element, null).get().getValue();
   }
 
   public static <T extends PsiMetaDataBase> void addMetadataBinding(ElementFilter filter, Class<T> aMetadataClass, Disposable parentDisposable) {
@@ -220,10 +200,6 @@ public class MetaRegistry extends MetaDataRegistrar {
 
   private static <T extends PsiMetaDataBase> void addBinding(final MyBinding binding) {
     ourBindings.add(0, binding);
-  }
-
-  public static void clearMetaForElement(PsiElement element) {
-    element.putUserData(META_DATA_KEY, null);
   }
 
   public <T extends PsiMetaDataBase> void registerMetaData(ElementFilter filter, Class<T> metadataDescriptorClass) {
