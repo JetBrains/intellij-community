a5f610f159efb61b209eb7dd068126ac8d297e38
Peter Gromov <peter@jetbrains.com>
2007-05-01 14:06:11 +0400

    remove deps

--- source/com/intellij/openapi/project/impl/convertors/Convertor34.java
+++ source/com/intellij/openapi/project/impl/convertors/Convertor34.java
@@ -1,7 +1,6 @@
 package com.intellij.openapi.project.impl.convertors;
 
 import com.intellij.ide.fileTemplates.FileTemplateManager;
-import com.intellij.javaee.J2EEFileTemplateNames;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.ProjectBundle;
