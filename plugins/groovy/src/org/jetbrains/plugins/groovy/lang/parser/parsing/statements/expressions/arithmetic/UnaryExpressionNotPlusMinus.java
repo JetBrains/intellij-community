94a4ad4b810c1da9f738c613508ee82c7d056ba2
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-05-02 15:28:30 +0400

    Fixed bug with type casts

--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/parser/parsing/statements/expressions/arithmetic/UnaryExpressionNotPlusMinus.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/parser/parsing/statements/expressions/arithmetic/UnaryExpressionNotPlusMinus.java
@@ -61,7 +61,7 @@ public class UnaryExpressionNotPlusMinus implements GroovyElementTypes {
     }
     if (TokenSets.BUILT_IN_TYPE.contains(builder.getTokenType()) ||
             mIDENT.equals(builder.getTokenType())) {
-      if (TypeSpec.parseStrict(builder).equals(WRONGWAY)) {
+      if (TypeSpec.parse(builder, false).equals(WRONGWAY)) {
         marker.rollbackTo();
         return WRONGWAY;
       }
