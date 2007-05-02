e6308a4d3aae73fa85c81703b86d7975337dfebd
Dmitry Krasilschikov <Dmitry.Krasilschikov@@jetbrains.com>
2007-05-02 21:50:49 +0400

    compiler go to the different process

---
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/compiler/GroovyCompilerProcess.java
@@ -0,0 +1,376 @@
+package org.jetbrains.plugins.groovy.compiler;
+
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.configurations.GeneralCommandLine;
+import com.intellij.ide.plugins.IdeaPluginDescriptor;
+import com.intellij.ide.plugins.PluginManager;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.PathManager;
+import com.intellij.openapi.compiler.*;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.extensions.PluginId;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.*;
+import com.intellij.openapi.vfs.JarFileSystem;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VfsUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiManager;
+import com.intellij.util.PathUtil;
+import com.intellij.util.PathsList;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.plugins.groovy.GroovyFileType;
+import org.jetbrains.plugins.groovy.compiler.rt.GroovycRunner;
+import org.jetbrains.plugins.groovy.config.GroovyGrailsConfiguration;
+import org.jetbrains.plugins.groovy.lang.psi.GroovyFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.*;
+
+/**
+ * @author: Dmitry.Krasilschikov
+ * @date: 16.04.2007
+ */
+
+public class GroovyCompilerProcess implements TranslatingCompiler {
+  private static final Logger LOG = Logger.getInstance("org.jetbrains.plugins.groovy.compiler.GroovyCompilerProcess");
+
+  private static final String GROOVYC_RUNNER_QUALIFIED_NAME = "org.jetbrains.plugins.groovy.compiler.rt.GroovycRunner";
+  private static final String JAVA_EXE = "java";
+  private static final String GROOVY_LANG_JAR = "groovy-1.0.jar";
+
+  private static final String GROOVY_LIB = "lib";
+  private static final String CLASS_PATH_LIST_SEPARATOR = File.pathSeparator;
+  private static final String antlrLibName = "antlr-2.7.5.jar";
+
+  @Nullable
+  public TranslatingCompiler.ExitStatus compile(final CompileContext compileContext, final VirtualFile[] virtualFiles) {
+    Set<TranslatingCompiler.OutputItem> compiledItems = new HashSet<TranslatingCompiler.OutputItem>();
+    Set<VirtualFile> allCompiling = new HashSet<VirtualFile>();
+
+    GeneralCommandLine commandLine;
+
+    Map<Module, Set<VirtualFile>> mapModulesToVirtualFiles = buildModuleToFilesMap(compileContext, virtualFiles);
+
+    for (Map.Entry<Module, Set<VirtualFile>> entry : mapModulesToVirtualFiles.entrySet()) {
+
+      commandLine = new GeneralCommandLine();
+      commandLine.setExePath(JAVA_EXE);
+
+      commandLine.addParameter("-cp");
+
+      String asmLibPath = PathManager.getLibPath() + File.separator + "asm.jar";
+
+      PluginId groovyPluginId = PluginManager.getPluginByClassName(getClass().getName());
+      IdeaPluginDescriptor ideaGroovyPluginDescriptor = PluginManager.getPlugin(groovyPluginId);
+
+      assert ideaGroovyPluginDescriptor != null;
+      String groovyPluginPath = ideaGroovyPluginDescriptor.getPath().getPath();
+      assert groovyPluginPath != null;
+
+      String antlrLib = groovyPluginPath + File.separator + GROOVY_LIB + File.separator + antlrLibName;
+      String groovyLangJarPath = GroovyGrailsConfiguration.getInstance().getGroovyInstallPath() + "\\" + GROOVY_LANG_JAR;
+
+      String myJarPath = PathUtil.getJarPathForClass(getClass());
+      commandLine.addParameter(new StringBuilder().append(myJarPath).
+          append(CLASS_PATH_LIST_SEPARATOR).
+          append(groovyLangJarPath).
+          append(CLASS_PATH_LIST_SEPARATOR).
+          append(antlrLib).
+          append(CLASS_PATH_LIST_SEPARATOR).
+          append(asmLibPath).toString());
+
+      commandLine.addParameter(GROOVYC_RUNNER_QUALIFIED_NAME);
+
+      try {
+        File fileWithParameters = File.createTempFile("toCompile", "");
+        fillFileWithGroovycParameters(entry.getKey(), entry.getValue(), fileWithParameters);
+
+        commandLine.addParameter(fileWithParameters.getPath());
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+
+      GroovycOSProcessHandler processHandler;
+
+      try {
+        processHandler = new GroovycOSProcessHandler(commandLine.createProcess(), commandLine.getCommandLineString());
+
+        processHandler.startNotify();
+        processHandler.waitFor();
+
+        Set<File> toRecompileFiles = processHandler.getToRecompileFiles();
+        VirtualFile[] toRecompileVirtualFiles = new VirtualFile[toRecompileFiles.size()];
+
+        VirtualFile toRecompileVirtualFile;
+
+        int i = 0;
+        for (File toRecompileFile : toRecompileFiles) {
+          toRecompileVirtualFile = LocalFileSystem.getInstance().findFileByIoFile(toRecompileFile);
+          toRecompileVirtualFiles[i] = toRecompileVirtualFile;
+          i++;
+        }
+
+        CompilerMessage[] compilerMessages = processHandler.getCompilerMessages().toArray(new CompilerMessage[0]);
+
+        for (final CompilerMessage compileMessage : compilerMessages) {
+          final CompilerMessageCategory category;
+          category = getMessageCategory(compileMessage);
+
+          String url = compileMessage.getUrl();
+
+
+          final GroovyFile[] myPsiFile = new GroovyFile[1];
+          final VirtualFile myFile;
+
+          try {
+            myFile = VfsUtil.findFileByURL(new URL("file://" + url));
+            assert myFile != null;
+
+
+            final Project project = VfsUtil.guessProjectForFile(myFile);
+            assert project != null;
+
+            ApplicationManager.getApplication().runReadAction(new Runnable() {
+              public void run() {
+                myPsiFile[0] = (GroovyFile) PsiManager.getInstance(project).findFile(myFile);
+              }
+            });
+
+            assert myPsiFile[0] != null;
+          } catch (MalformedURLException e) {
+            e.printStackTrace();
+          }
+
+          url = url.replace('\\', '/');
+          compileContext.addMessage(category, compileMessage.getMessage(), url, compileMessage.getLinenum(), compileMessage.getColomnnum());
+
+        }
+
+        StringBuffer unparsedBuffer = processHandler.getUnparsedOutput();
+        if (unparsedBuffer.length() != 0)
+          compileContext.addMessage(CompilerMessageCategory.ERROR, unparsedBuffer.toString(), null, -1, -1);
+
+        return new GroovyCompileExitStatus(processHandler.getSuccessfullyCompiled(), toRecompileVirtualFiles);
+
+//        if (exitStatus == null) return new GroovyCompileExitStatus(new HashSet<OutputItem>(), VirtualFile.EMPTY_ARRAY);
+      } catch (ExecutionException e) {
+        e.printStackTrace();
+      }
+    }
+
+    VirtualFile[] toRecompile = compiledItems.size() > 0 ?
+        VirtualFile.EMPTY_ARRAY :
+        allCompiling.toArray(new VirtualFile[allCompiling.size()]);
+
+    return new GroovyCompileExitStatus(compiledItems, toRecompile);
+  }
+
+  private CompilerMessageCategory getMessageCategory(CompilerMessage compilerMessage) {
+    String cathegory;
+    cathegory = compilerMessage.getCathegory();
+
+    if (MessageCollector.ERROR.equals(cathegory)) return CompilerMessageCategory.ERROR;
+    if (MessageCollector.INFORMATION.equals(cathegory)) return CompilerMessageCategory.INFORMATION;
+    if (MessageCollector.STATISTICS.equals(cathegory)) return CompilerMessageCategory.STATISTICS;
+    if (MessageCollector.WARNING.equals(cathegory)) return CompilerMessageCategory.WARNING;
+
+    return CompilerMessageCategory.ERROR;
+  }
+
+  class GroovyCompileExitStatus implements ExitStatus {
+    private OutputItem[] myCompiledItems;
+    private VirtualFile[] myToRecompile;
+
+    public GroovyCompileExitStatus(Set<TranslatingCompiler.OutputItem> compiledItems, VirtualFile[] toRecompile) {
+      myToRecompile = toRecompile;
+      myCompiledItems = compiledItems.toArray(new OutputItem[compiledItems.size()]);
+    }
+
+    public OutputItem[] getSuccessfullyCompiled() {
+      return myCompiledItems;
+    }
+
+    public VirtualFile[] getFilesToRecompile() {
+      return myToRecompile;
+    }
+  }
+
+  private void fillFileWithGroovycParameters(Module module, Set<VirtualFile> virtualFiles, File f) {
+
+    PrintStream printer = null;
+    try {
+      printer = new PrintStream(new FileOutputStream(f));
+
+/*    filename1
+*    filname2
+*    filname3
+*    ...
+*    filenameN
+*/
+
+      ModuleRootManager moduleRootManager = ModuleRootManager.getInstance(module);
+      //files
+      for (VirtualFile item : virtualFiles) {
+        if (!moduleRootManager.getFileIndex().isInTestSourceContent(item)) {
+          printer.print(GroovycRunner.SRC_FILE);
+        } else {
+          printer.print(GroovycRunner.TEST_FILE);
+        }
+        printer.println();
+        printer.print(item.getPath());
+        printer.println();
+      }
+
+      //classpath
+      printer.print(GroovycRunner.CLASSPATH);
+      printer.println();
+      printer.print(getCompilationClasspath(module).getPathsString());
+      printer.println();
+
+//output
+      boolean forTestSourceFolders;
+      String outputPath;
+
+//ordinary classes
+      forTestSourceFolders = false;
+      outputPath = CompilerPaths.getModuleOutputPath(module, forTestSourceFolders);
+      printer.print(GroovycRunner.OUTPUTPATH);
+      printer.println();
+      printer.print(outputPath);
+      printer.println();
+
+//test output
+      forTestSourceFolders = true;
+      outputPath = CompilerPaths.getModuleOutputPath(module, forTestSourceFolders);
+      printer.print(GroovycRunner.TEST_OUTPUTPATH);
+      printer.println();
+      printer.print(outputPath);
+      printer.println();
+
+//module name
+      printer.print(GroovycRunner.MODULE_NAME);
+      printer.println();
+      printer.print(module.getName());
+      printer.println();
+
+//source
+      printer.print(GroovycRunner.SRC_FOLDERS);
+      printer.println();
+      printer.print(getNonExcludedModuleSourceFolders(module).getPathsString());
+      printer.println();
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+
+      assert printer != null;
+      printer.close();
+    }
+  }
+
+  public boolean isCompilableFile(VirtualFile virtualFile, CompileContext compileContext) {
+    return GroovyFileType.GROOVY_FILE_TYPE.equals(virtualFile.getFileType());
+  }
+
+  @NotNull
+  public String getDescription() {
+    return "groovy compiler";
+  }
+
+  public boolean validateConfiguration(CompileScope compileScope) {
+    return true;
+  }
+
+  private PathsList getCompilationClasspath(Module module) {
+    ModuleRootManager rootManager = ModuleRootManager.getInstance(module);
+    OrderEntry[] entries = rootManager.getOrderEntries();
+    Set<VirtualFile> cpVFiles = new HashSet<VirtualFile>();
+
+    for (OrderEntry orderEntry : entries) {
+      cpVFiles.addAll(Arrays.asList(orderEntry.getFiles(OrderRootType.COMPILATION_CLASSES)));
+    }
+
+    StringBuffer path = new StringBuffer();
+
+    VirtualFile[] filesArray = cpVFiles.toArray(new VirtualFile[cpVFiles.size()]);
+    for (int i = 0; i < filesArray.length; i++) {
+      VirtualFile file = filesArray[i];
+      String filePath = file.getPath();
+
+      int jarSeparatorIndex = filePath.indexOf(JarFileSystem.JAR_SEPARATOR);
+      if (jarSeparatorIndex > 0) {         
+        filePath = filePath.substring(0, jarSeparatorIndex);
+      }
+      path.append(filePath);
+      
+      if (i < filesArray.length - 1) {
+        path.append(CLASS_PATH_LIST_SEPARATOR);
+      }
+    }
+
+    PathsList pathsList = new PathsList();
+    pathsList.add(path.toString());
+
+    return pathsList;
+  }
+
+  public PathsList getNonExcludedModuleSourceFolders(Module module) {
+    ContentEntry[] contentEntries = ModuleRootManager.getInstance(module).getContentEntries();
+    PathsList sourceFolders = findAllSourceFolders(contentEntries);
+    sourceFolders.getPathList().removeAll(findExcludedFolders(contentEntries));
+    return sourceFolders;
+  }
+
+  private PathsList findAllSourceFolders(ContentEntry[] contentEntries) {
+    PathsList sourceFolders = new PathsList();
+    for (ContentEntry contentEntry : contentEntries) {
+      for (SourceFolder folder : contentEntry.getSourceFolders()) {
+        VirtualFile file = folder.getFile();
+        assert file != null;
+        if (file.isDirectory() && file.isWritable()) {
+          sourceFolders.add(file);
+        }
+      }
+    }
+    return sourceFolders;
+  }
+
+  private Set<VirtualFile> findExcludedFolders(ContentEntry[] entries) {
+    Set<VirtualFile> excludedFolders = new HashSet<VirtualFile>();
+    for (ContentEntry entry : entries) {
+      excludedFolders.addAll(Arrays.asList(entry.getExcludeFolderFiles()));
+    }
+    return excludedFolders;
+  }
+
+  private static Map<Module, Set<VirtualFile>> buildModuleToFilesMap(final CompileContext context, final VirtualFile[] files) {
+    final Map<Module, Set<VirtualFile>> map = new HashMap<Module, Set<VirtualFile>>();
+    ApplicationManager.getApplication().runReadAction(new Runnable() {
+      public void run() {
+        for (VirtualFile file : files) {
+          final Module module = context.getModuleByFile(file);
+
+          if (module == null) {
+            continue;
+          }
+
+          Set<VirtualFile> moduleFiles = map.get(module);
+          if (moduleFiles == null) {
+            moduleFiles = new HashSet<VirtualFile>();
+            map.put(module, moduleFiles);
+          }
+          moduleFiles.add(file);
+        }
+      }
+    });
+    return map;
+  }
+}
---
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/compiler/GroovycOSProcessHandler.java
@@ -0,0 +1,225 @@
+package org.jetbrains.plugins.groovy.compiler;
+
+import com.intellij.execution.process.OSProcessHandler;
+import com.intellij.openapi.compiler.TranslatingCompiler;
+import com.intellij.openapi.util.Key;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.plugins.groovy.compiler.rt.GroovycRunner;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.ObjectInput;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+
+/**
+ * @author: Dmitry.Krasilschikov
+ * @date: 16.04.2007
+ */
+public class GroovycOSProcessHandler extends OSProcessHandler {
+  private Set<TranslatingCompiler.OutputItem> compiledFilesNames = new HashSet<TranslatingCompiler.OutputItem>();
+  private Set<File> toRecompileFiles = new HashSet<File>();
+  private List<CompilerMessage> compilerMessages = new ArrayList<CompilerMessage>();
+  private StringBuffer unparsedOutput = new StringBuffer();
+
+  public GroovycOSProcessHandler(Process process, String s) {
+    super(process, s);
+  }
+
+  public void notifyTextAvailable(final String text, final Key outputType) {
+    super.notifyTextAvailable(text, outputType);
+//    System.out.println("text: " + text);
+
+    parseOutput(text);
+  }
+
+  private final StringBuffer outputBuffer = new StringBuffer();
+
+  public void parseOutput(String text) {
+    InputStream inputStream;
+    ObjectInput objectInput = null;
+    TranslatingCompiler.ExitStatus exitStatus = null;
+
+    //todo: collect errors
+    text = text.trim();
+    if (text != null && !"".equals(text)) {
+      outputBuffer.append(text);
+
+      //compiled start marker have to be in the beginning on each string
+      if (outputBuffer.indexOf(GroovycRunner.COMPILED_START) != -1) {
+        unparsedOutput.setLength(0);
+
+        if (!(outputBuffer.indexOf(GroovycRunner.COMPILED_END) != -1)) {
+          return;
+        }
+
+        {
+          text = handleOutputBuffer(GroovycRunner.COMPILED_START, GroovycRunner.COMPILED_END);
+
+          StringTokenizer tokenizer = new StringTokenizer(text, GroovycRunner.SEPARATOR, false);
+
+          String token = "";
+          /*
+          * output path
+          * source file
+          * output root directory
+          */
+
+          String outputPath = "";
+          String sourceFile = "";
+          String outputRootDirectory = "";
+
+          if (tokenizer.hasMoreTokens()) {
+            token = tokenizer.nextToken();
+            outputPath = token;
+          }
+
+          if (tokenizer.hasMoreTokens()) {
+            token = tokenizer.nextToken();
+            sourceFile = token;
+          }
+
+          if (tokenizer.hasMoreTokens()) {
+            token = tokenizer.nextToken();
+            outputRootDirectory = token;
+          }
+
+          try {
+            compiledFilesNames.add(getOutputItem(outputPath, sourceFile, outputRootDirectory));
+          } catch (InvocationTargetException e) {
+            e.printStackTrace();
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+        }
+      } else if (outputBuffer.indexOf(GroovycRunner.TO_RECOMPILE_START) != -1) {
+        unparsedOutput.setLength(0);
+        if (!(outputBuffer.indexOf(GroovycRunner.TO_RECOMPILE_END) != -1)) {
+          return;
+        }
+
+        if (outputBuffer.indexOf(GroovycRunner.TO_RECOMPILE_END) != -1) {
+          text = handleOutputBuffer(GroovycRunner.TO_RECOMPILE_START, GroovycRunner.TO_RECOMPILE_END);
+
+          toRecompileFiles.add(new File(text));
+        }
+      }
+
+      /* Cathegory
+      * Message
+      * Url
+      * Linenum
+      * Colomnnum
+      */
+
+      else if (outputBuffer.indexOf(GroovycRunner.MESSAGES_START) != -1) {
+        unparsedOutput.setLength(0);
+        if (!(outputBuffer.indexOf(GroovycRunner.MESSAGES_END) != -1)) {
+          return;
+        }
+
+        text = handleOutputBuffer(GroovycRunner.MESSAGES_START, GroovycRunner.MESSAGES_END);
+
+        String cathegory = "";
+        String message = "";
+        String url = "";
+        String linenum = "";
+        String colomnnum = "";
+        String token;
+
+        final StringTokenizer compilerMessageTokenizer = new StringTokenizer(text, GroovycRunner.SEPARATOR, false);
+
+        if (compilerMessageTokenizer.hasMoreTokens()) {
+          token = compilerMessageTokenizer.nextToken();
+          cathegory = token;
+        }
+
+        if (compilerMessageTokenizer.hasMoreTokens()) {
+          token = compilerMessageTokenizer.nextToken();
+          message = token;
+        }
+
+        if (compilerMessageTokenizer.hasMoreTokens()) {
+          token = compilerMessageTokenizer.nextToken();
+          url = token;
+        }
+
+        if (compilerMessageTokenizer.hasMoreTokens()) {
+          token = compilerMessageTokenizer.nextToken();
+          linenum = token;
+        }
+
+        if (compilerMessageTokenizer.hasMoreTokens()) {
+          token = compilerMessageTokenizer.nextToken();
+          colomnnum = token;
+        }
+
+        int linenumInt;
+        int colomnnumInt;
+
+        try {
+          linenumInt = Integer.parseInt(linenum);
+          colomnnumInt = Integer.parseInt(colomnnum);
+        } catch (NumberFormatException e) {
+          e.printStackTrace();
+          linenumInt = 0;
+          colomnnumInt = 0;
+        }
+
+        compilerMessages.add(new CompilerMessage(cathegory, message, url, linenumInt, colomnnumInt));
+      } else {
+        if (outputBuffer.indexOf("Exception") != -1) unparsedOutput.append(outputBuffer);
+        outputBuffer.setLength(0);
+      }
+    }
+  }
+
+  private String handleOutputBuffer(String START_MARKER, String END_MARKER) {
+    String text;
+    text = outputBuffer.substring(
+        outputBuffer.indexOf(START_MARKER) + START_MARKER.length(),
+        outputBuffer.indexOf(END_MARKER)).toString();
+
+    outputBuffer.delete(
+              outputBuffer.indexOf(START_MARKER),
+              outputBuffer.indexOf(END_MARKER) + END_MARKER.length());
+
+    return text;
+  }
+
+  private TranslatingCompiler.OutputItem getOutputItem(final String outputPath, final String sourceFile, final String outputRootDir) throws InvocationTargetException, InterruptedException {
+
+    final VirtualFile sourceVirtualFile = LocalFileSystem.getInstance().findFileByIoFile(new File(sourceFile));
+
+    return new TranslatingCompiler.OutputItem() {
+      public String getOutputPath() {
+        return outputPath;
+      }
+
+      public VirtualFile getSourceFile() {
+        return sourceVirtualFile;
+      }
+
+      public String getOutputRootDirectory() {
+        return outputRootDir;
+      }
+    };
+  }
+
+  public Set<TranslatingCompiler.OutputItem> getSuccessfullyCompiled() {
+    return Collections.unmodifiableSet(compiledFilesNames);
+  }
+
+  public Set<File> getToRecompileFiles() {
+    return toRecompileFiles;
+  }
+
+  public List<CompilerMessage> getCompilerMessages() {
+    return compilerMessages;
+  }
+
+  public StringBuffer getUnparsedOutput() {
+    return unparsedOutput;
+  }
+}
