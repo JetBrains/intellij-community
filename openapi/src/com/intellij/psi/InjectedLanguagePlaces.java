3c3bf03302d3e3bca75912351b210363b8abd23c
Alexey Kudravtsev <cdr@jetbrains.com>
2007-05-03 21:34:50 +0400

    doc

--- openapi/src/com/intellij/psi/InjectedLanguagePlaces.java
+++ openapi/src/com/intellij/psi/InjectedLanguagePlaces.java
@@ -9,12 +9,15 @@ import org.jetbrains.annotations.NonNls;
 /**
  * Storage for places where PSI language being injected to.
  * @see com.intellij.psi.LanguageInjector for usage examples.
+ * @see com.intellij.psi.PsiLanguageInjectionHost
  */
 public interface InjectedLanguagePlaces {
   /**
-   * Informs IDEA of the language injected inside the host element.
+   * Informs IDEA of the language injected inside the host element, which must be instanceof {@link com.intellij.psi.PsiLanguageInjectionHost}
    * @param language to inject inside the host element.
    * @param rangeInsideHost where to inject the language. Offsets are relative to the host element text range.
+   *        E.g. for {@link com.intellij.psi.PsiLiteralExpression} it usually is <code>new TextRange(1, psiLiteral.getTextLength()-1)</code>,
+   *        for injecting the language in string literal inside double quotes.  
    * @param prefix Optional header to be handed on to the language parser before the host element text.
    *        Might be useful e.g. for making the text parsable or providing some context.
    * @param suffix Optional footer to be passed on to the language parser after the host element text.
