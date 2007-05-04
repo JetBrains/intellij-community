3583bb8e6398c125cdb01aa0337048eee2413f9f
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-05-04 22:04:59 +0400

    (no message)

--- plugins/groovy/src/org/jetbrains/plugins/groovy/annotator/GroovyAnnotator.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/annotator/GroovyAnnotator.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2007 JetBrains s.r.o.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.jetbrains.plugins.groovy.annotator;
 
 import com.intellij.codeInsight.intention.IntentionAction;
@@ -7,9 +22,9 @@ import com.intellij.lang.annotation.Annotation;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiManager;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.ui.LightweightHint;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.application.ApplicationManager;
 import org.jetbrains.plugins.groovy.GroovyBundle;
 import org.jetbrains.plugins.groovy.annotator.intentions.OuterImportsActionCreator;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.GrTypeDefinition;
@@ -39,18 +54,18 @@ public class GroovyAnnotator implements Annotator {
     }
   }
 
-  private void checkReferenceElement(PsiElement element, AnnotationHolder holder, GrTypeOrPackageReferenceElement refElement) {
+  private void checkReferenceElement(final PsiElement element, AnnotationHolder holder, final GrTypeOrPackageReferenceElement refElement) {
     if (!refElement.isSoft() && refElement.getReferenceName() != null) {
       final PsiElement resolved = refElement.resolve();
       if (resolved == null) {
         String message = GroovyBundle.message("cannot.resolve") + " " + refElement.getReferenceName();
-        final Annotation annotation = holder.createErrorAnnotation(element, message);
 
         // Register quickfix
-        for (IntentionAction action : OuterImportsActionCreator.getOuterImportFixes(refElement, annotation, refElement.getProject())) {
+        final Annotation annotation = holder.createErrorAnnotation(element, message);
+        final IntentionAction[] actions = OuterImportsActionCreator.getOuterImportFixes(refElement, annotation, refElement.getProject());
+        for (IntentionAction action : actions) {
           annotation.registerFix(action);
         }
-
         annotation.setHighlightType(ProblemHighlightType.LIKE_UNKNOWN_SYMBOL);
       }
     }
