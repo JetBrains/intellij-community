12d32f3624d29d713565b26b5606c929d115c1d5
Eugene Vigdorchik <no_reply@jetbrains.com>
2007-05-10 16:52:32 +0400

    cleanup

---
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/GroovyFile.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2000-2007 JetBrains s.r.o.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jetbrains.plugins.groovy.lang.psi;
+
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.GrTypeDefinition;
+import org.jetbrains.plugins.groovy.lang.psi.api.toplevel.packaging.GrPackageDefinition;
+import org.jetbrains.plugins.groovy.lang.psi.api.toplevel.GrTopStatement;
+import org.jetbrains.plugins.groovy.lang.psi.api.toplevel.imports.GrImportStatement;
+import org.jetbrains.annotations.NotNull;
+import com.intellij.psi.scope.PsiScopeProcessor;
+import com.intellij.psi.PsiSubstitutor;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiFile;
+
+/**
+ * @author ven
+ */
+public interface GroovyFile extends PsiFile {
+  GrTypeDefinition[] getTypeDefinitions();
+
+  @NotNull
+  String getPackageName();
+
+  GrPackageDefinition getPackageDefinition();
+
+  GrTopStatement[] getTopStatements();
+
+  GrImportStatement[] getImportStatements();
+
+  void addImportForClass(PsiClass aClass);
+}
