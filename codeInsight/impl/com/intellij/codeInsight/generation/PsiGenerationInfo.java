7f11893c51c15d96130abd48c43c1ad73680c765
greg <Gregory.Shrago@jetbrains.com>
2007-05-11 16:10:14 +0400

    merge existing members

--- codeInsight/impl/com/intellij/codeInsight/generation/PsiGenerationInfo.java
+++ codeInsight/impl/com/intellij/codeInsight/generation/PsiGenerationInfo.java
@@ -4,9 +4,7 @@
  */
 package com.intellij.codeInsight.generation;
 
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiMember;
+import com.intellij.psi.*;
 import com.intellij.psi.codeStyle.CodeStyleManager;
 import com.intellij.util.IncorrectOperationException;
 import org.jetbrains.annotations.NotNull;
@@ -15,19 +13,45 @@ import org.jetbrains.annotations.NotNull;
  * @author peter
  */
 public class PsiGenerationInfo<T extends PsiMember> implements GenerationInfo {
-  private T myMethod;
+  private T myMember;
 
-  public PsiGenerationInfo(@NotNull final T method) {
-    myMethod = method;
+  public PsiGenerationInfo(@NotNull final T member) {
+    myMember = member;
   }
 
   @NotNull
   public final T getPsiMember() {
-    return myMethod;
+    return myMember;
   }
 
   public void insert(PsiClass aClass, PsiElement anchor, boolean before) throws IncorrectOperationException {
-    PsiElement newMember = GenerateMembersUtil.insert(aClass, myMethod, anchor, before);
-    myMethod = (T)CodeStyleManager.getInstance(aClass.getProject()).shortenClassReferences(newMember);
+    final PsiMember existingMember;
+    if (myMember instanceof PsiField) {
+      existingMember = aClass.findFieldByName(myMember.getName(), false);
+    }
+    else if (myMember instanceof PsiMethod) {
+      existingMember = aClass.findMethodBySignature((PsiMethod)myMember, false);
+    }
+    else existingMember = null;
+    if (existingMember == null) {
+      PsiElement newMember = GenerateMembersUtil.insert(aClass, myMember, anchor, before);
+      myMember = (T)CodeStyleManager.getInstance(aClass.getProject()).shortenClassReferences(newMember);
+    }
+    else {
+      final PsiModifierList modifierList = myMember.getModifierList();
+      final PsiModifierList existingModifierList = existingMember.getModifierList();
+      if (modifierList != null && existingModifierList != null) {
+        final PsiAnnotation[] psiAnnotations = modifierList.getAnnotations();
+        PsiElement annoAnchor = existingModifierList.getAnnotations().length > 0 ? existingModifierList.getAnnotations()[0] : existingModifierList.getFirstChild();
+        if (psiAnnotations.length > 0) {
+          for (PsiAnnotation annotation : psiAnnotations) {
+            final PsiAnnotation existingAnno = existingModifierList.findAnnotation(annotation.getQualifiedName());
+            if (existingAnno != null) existingAnno.replace(annotation);
+            else existingModifierList.addBefore(annotation, annoAnchor);
+          }
+        }
+      }
+      myMember = (T)existingMember;
+    }
   }
 }
