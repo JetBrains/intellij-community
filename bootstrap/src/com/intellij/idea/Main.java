ac9c88313663f255a34c00d7d93e67b1e5cc8ad4
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2007-05-11 12:37:19 +0400

    command line application tests (fork per-test needed)

--- bootstrap/src/com/intellij/idea/Main.java
+++ bootstrap/src/com/intellij/idea/Main.java
@@ -25,10 +25,12 @@ public class Main {
     @NonNls final String diffAppCode = "diff";
     @NonNls final String antAppCode = "ant";
     @NonNls final String duplocateCode = "duplocate";
+    @NonNls final String traverseUI = "traverseUI";
     return args.length > 0 && (Comparing.strEqual(args[0], inspectAppCode) ||
                                Comparing.strEqual(args[0], diffAppCode) ||
                                Comparing.strEqual(args[0], antAppCode) ||
-                               Comparing.strEqual(args[0], duplocateCode));
+                               Comparing.strEqual(args[0], duplocateCode) ||
+                               Comparing.strEqual(args[0], traverseUI));
   }
 
   public static boolean isHeadless() {
