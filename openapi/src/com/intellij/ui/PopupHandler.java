c629f3a6746c5dcfa762f31df99aff41300de9db
Mike Aizatsky <no_reply@jetbrains.com>
2007-05-11 17:51:30 +0400

    compatibility fix

--- openapi/src/com/intellij/ui/PopupHandler.java
+++ openapi/src/com/intellij/ui/PopupHandler.java
@@ -56,12 +56,12 @@ public abstract class PopupHandler extends MouseAdapter {
     }
   }
 
-  public static void installPopupHandler(Component component, @NonNls String groupId, String place) {
+  public static void installPopupHandler(JComponent component, @NonNls String groupId, String place) {
     ActionGroup group = (ActionGroup)ActionManager.getInstance().getAction(groupId);
     installPopupHandler(component, group, place, ActionManager.getInstance());
   }
 
-  public static MouseListener installPopupHandler(Component component, final ActionGroup group, final String place, final ActionManager actionManager) {
+  public static MouseListener installPopupHandler(JComponent component, final ActionGroup group, final String place, final ActionManager actionManager) {
     if (ApplicationManager.getApplication() == null) return new MouseAdapter(){};
     PopupHandler popupHandler = new PopupHandler() {
       public void invokePopup(Component comp, int x, int y) {
