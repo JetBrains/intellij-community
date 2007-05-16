fbe180b85b791afa19854bf3def57a7945b27c07
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2007-05-16 11:16:50 +0400

    toString implemented in FacetTypeId

--- openapi/src/com/intellij/facet/FacetTypeId.java
+++ openapi/src/com/intellij/facet/FacetTypeId.java
@@ -4,8 +4,23 @@
 
 package com.intellij.facet;
 
+import org.jetbrains.annotations.NonNls;
+
 /**
  * @author nik
  */
 public final class FacetTypeId<F extends Facet> {
+  private final String myDebugName;
+
+  public FacetTypeId() {
+    this("unknown");
+  }
+
+  public FacetTypeId(@NonNls String debugName) {
+    myDebugName = debugName;
+  }
+
+  public String toString() {
+    return myDebugName;
+  }
 }
