d0001c68622302524e1d35d8c678c125ec8cd388
Alexey Kudravtsev <cdr@jetbrains.com>
2007-05-18 22:05:45 +0400

    bombed

--- lvcs/impl/test/com/intellij/localvcsintegr/revert/ChangeReverterTest.java
+++ lvcs/impl/test/com/intellij/localvcsintegr/revert/ChangeReverterTest.java
@@ -1,5 +1,6 @@
 package com.intellij.localvcsintegr.revert;
 
+import com.intellij.idea.Bombed;
 import com.intellij.localvcs.core.revisions.Revision;
 import com.intellij.localvcs.integration.Clock;
 import com.intellij.localvcs.integration.revert.ChangeReverter;
@@ -9,9 +10,11 @@ import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.vfs.VirtualFile;
 
 import java.io.IOException;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+@Bombed(month = Calendar.MAY, day = 21)
 public class ChangeReverterTest extends IntegrationTestCase {
   public void testRevertCreation() throws IOException {
     VirtualFile f = root.createChildData(null, "f.java");
