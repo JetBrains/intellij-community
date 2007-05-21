43422461f2598e4bcd808fef6dcf382c69e636e1
greg <Gregory.Shrago@jetbrains.com>
2007-05-21 16:53:03 +0400

    Hibernate attributes creation (TV/ERD)

--- openapi/src/com/intellij/psi/CommonClassNames.java
+++ openapi/src/com/intellij/psi/CommonClassNames.java
@@ -21,6 +21,9 @@ public interface CommonClassNames {
   @NonNls String JAVA_LANG_DEPRECATED = "java.lang.Deprecated";
   @NonNls String JAVA_LANG_ANNOTATION_INHERITED = "java.lang.annotation.Inherited";
 
+  @NonNls String JAVA_LANG_REFLECT_ARRAY = "java.lang.reflect.Array";
+
+  @NonNls String JAVA_UTIL_COLLECTION = "java.util.Collection";
   @NonNls String JAVA_UTIL_MAP = "java.util.Map";
   @NonNls String JAVA_UTIL_LIST = "java.util.List";
   @NonNls String JAVA_UTIL_SET = "java.util.Set";
