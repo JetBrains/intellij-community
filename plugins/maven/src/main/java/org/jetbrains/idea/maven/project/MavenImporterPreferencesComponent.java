ad7284bcca3bc51ba71a715d92139122f97f809a
Vladislav Kaznacheev <no_reply@jetbrains.com>
2007-05-29 20:59:15 +0400

    Redundant ProjectComponent removed

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/project/MavenImporterPreferencesComponent.java
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/project/MavenImporterPreferencesComponent.java
@@ -1,17 +1,15 @@
 package org.jetbrains.idea.maven.project;
 
 import com.intellij.openapi.components.PersistentStateComponent;
-import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
 import com.intellij.openapi.project.Project;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Vladislav.Kaznacheev
  */
 @State(name = "MavenImportPreferences", storages = {@Storage(id = "default", file = "$WORKSPACE_FILE$")})
-public class MavenImporterPreferencesComponent implements ProjectComponent, PersistentStateComponent<MavenImporterPreferences> {
+public class MavenImporterPreferencesComponent implements PersistentStateComponent<MavenImporterPreferences> {
 
   public static MavenImporterPreferencesComponent getInstance(final Project project) {
     return project.getComponent(MavenImporterPreferencesComponent.class);
@@ -25,22 +23,5 @@ public class MavenImporterPreferencesComponent implements ProjectComponent, Pers
 
   public void loadState(MavenImporterPreferences state) {
     preferences = state;
-  }
-
-  public void initComponent() {
-  }
-
-  public void disposeComponent() {
-  }
-
-  @NotNull
-  public String getComponentName() {
-    return "MavenImportPreferences";
-  }
-
-  public void projectOpened() {
-  }
-
-  public void projectClosed() {
   }
 }
