69a69513f77545aaaf93a11ff16e1104eb8c6ffa
Peter Gromov <peter@jetbrains.com>
2007-05-31 19:20:46 +0400

    don't parse iml files

--- dom/impl/src/com/intellij/util/xml/impl/DomManagerImpl.java
+++ dom/impl/src/com/intellij/util/xml/impl/DomManagerImpl.java
@@ -219,6 +219,7 @@ public final class DomManagerImpl extends DomManager implements ProjectComponent
   }
 
   private void processFileChange(final VirtualFile file) {
+    if (StdFileTypes.XML != file.getFileType()) return;
     processFileChange(PsiManager.getInstance(myProject).findFile(file));
   }
 
