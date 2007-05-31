6c53a595d9df811137842fbf2eed77548bac0601
Dmitry Jemerov <yole@intellij.com>
2007-05-31 11:05:46 +0400

    prepare for OpenAPI extraction (2): extract CompletionVariantPeer to keep implementation details and dependencies out of OpenAPI

--- codeInsight/impl/com/intellij/codeInsight/completion/CompletionVariant.java
+++ codeInsight/impl/com/intellij/codeInsight/completion/CompletionVariant.java
@@ -1,26 +1,18 @@
 package com.intellij.codeInsight.completion;
 
-import com.intellij.codeInsight.completion.scope.CompletionElement;
-import com.intellij.codeInsight.completion.scope.CompletionProcessor;
-import com.intellij.codeInsight.completion.simple.SimpleLookupItem;
-import com.intellij.codeInsight.lookup.LookupItem;
-import com.intellij.codeInsight.lookup.LookupItemUtil;
 import com.intellij.codeInsight.TailType;
-import com.intellij.openapi.util.Condition;
-import com.intellij.psi.*;
+import com.intellij.codeInsight.lookup.LookupItem;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementFactory;
+import com.intellij.psi.PsiReference;
 import com.intellij.psi.filters.ContextGetter;
 import com.intellij.psi.filters.ElementExtractorFilter;
 import com.intellij.psi.filters.ElementFilter;
 import com.intellij.psi.impl.source.jsp.jspJava.JspClassLevelDeclarationStatement;
-import com.intellij.psi.impl.source.resolve.reference.impl.PsiMultiReference;
-import com.intellij.psi.infos.CandidateInfo;
-import com.intellij.util.IncorrectOperationException;
 import com.intellij.util.ReflectionCache;
-import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.containers.HashMap;
 import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.io.Serializable;
 import java.util.*;
@@ -43,8 +35,11 @@ public class CompletionVariant {
   private InsertHandler myInsertHandler = null;
   private final Map<Object, Serializable> myItemProperties = new HashMap<Object, Serializable>();
   private boolean caseInsensitive;
+  private final CompletionVariantPeer myPeer;
 
-  public CompletionVariant(){}
+  public CompletionVariant() {
+    myPeer = new CompletionVariantPeerImpl(this);
+  }
 
   public CompletionVariant(Class scopeClass, ElementFilter position){
     includeScopeClass(scopeClass);
@@ -52,10 +47,12 @@ public class CompletionVariant {
       includeScopeClass(JspClassLevelDeclarationStatement.class); // hack for JSP completion
     }
     myPosition = position;
+    myPeer = new CompletionVariantPeerImpl(this);
   }
 
   public CompletionVariant(ElementFilter position){
     myPosition = position;
+    myPeer = new CompletionVariantPeerImpl(this);
   }
 
   public boolean isScopeAcceptable(PsiElement scope){
@@ -76,6 +73,10 @@ public class CompletionVariant {
 
   public void setItemProperty(Object id, Serializable value){
     myItemProperties.put(id, value);
+  }
+
+  public Map<Object, Serializable> getItemProperties() {
+    return myItemProperties;
   }
 
   public boolean isScopeClassFinal(Class scopeClass){
@@ -100,8 +101,7 @@ public class CompletionVariant {
     }
 
     if(ret){
-      for (final Object myScopeClassException : myScopeClassExceptions) {
-        final Class aClass = (Class)myScopeClassException;
+      for (final Class aClass: myScopeClassExceptions) {
         if (ReflectionCache.isAssignable(aClass, scopeClass)) {
           ret = false;
           break;
@@ -187,66 +187,6 @@ public class CompletionVariant {
     }
   }
 
-  @Nullable
-  private LookupItem addLookupItem(Set<LookupItem> set, CompletionVariantItem element, @NotNull Object completion, CompletionContext context){
-    LookupItem ret = LookupItemUtil.objectToLookupItem(completion);
-    if(ret == null) return null;
-
-    if(getInsertHandler() != null && !(ret instanceof SimpleLookupItem)){
-      ret.setAttribute(LookupItem.INSERT_HANDLER_ATTR, getInsertHandler());
-      ret.setTailType(TailType.UNKNOWN);
-    }
-    else if (element.myTailType != TailType.NONE) {
-      ret.setTailType(element.myTailType);
-    }
-
-    for (final Object key : myItemProperties.keySet()) {
-      if (key == LookupItem.FORCE_SHOW_FQN_ATTR && ret.getObject() instanceof PsiClass) {
-        @NonNls String packageName = ((PsiClass)ret.getObject()).getQualifiedName();
-        if (packageName != null && packageName.lastIndexOf('.') > 0) {
-          packageName = packageName.substring(0, packageName.lastIndexOf('.'));
-        }
-        else {
-          packageName = "";
-        }
-        if (packageName.length() == 0) {
-          packageName = "default package";
-        }
-
-        ret.setAttribute(LookupItem.TAIL_TEXT_ATTR, " (" + packageName + ")");
-        ret.setAttribute(LookupItem.TAIL_TEXT_SMALL_ATTR, "");
-      }
-      else {
-        if (completion instanceof PsiMember && key == LookupItem.FORCE_QUALIFY) {
-          final PsiMember completionElement = (PsiMember)completion;
-          final PsiClass containingClass = completionElement.getContainingClass();
-          if (containingClass != null) {
-            final String className = containingClass.getName();
-            ret.setLookupString(className + "." + ret.getLookupString());
-            ret.setAttribute(key, myItemProperties.get(key));
-          }
-        }
-        ret.setAttribute(key, myItemProperties.get(key));
-      }
-    }
-    final boolean itemCaseInsensitive = Boolean.TRUE.equals(ret.getAttribute(LookupItem.CASE_INSENSITIVE));
-    final String lookupString = ret.getLookupString();
-    if(CompletionUtil.checkName(lookupString, context, itemCaseInsensitive || caseInsensitive)){
-      if (itemCaseInsensitive) {
-        final String currentString = ret.getLookupString();
-        final String newString = handleCaseInsensitiveVariant(context.getPrefix(), currentString);
-        ret.setLookupString(newString);
-        if (ret.getObject() == currentString) {
-          ret.setObject(newString);
-        }
-      }
-      set.add(ret);
-      return ret;
-    }
-
-    return null;
-  }
-
   public void addKeywords(PsiElementFactory factory, Set<LookupItem> set, CompletionContext context, PsiElement position){
     for (final CompletionVariantItem ce : myCompletionsList) {
       final Object comp = ce.myCompletion;
@@ -255,48 +195,27 @@ public class CompletionVariant {
       }
 
       if (comp instanceof String) {
-        addKeyword(factory, set, ce, comp, context);
+        myPeer.addKeyword(factory, set, ce.myTailType, comp, context);
       }
       else if (comp instanceof ContextGetter) {
         final Object[] elements = ((ContextGetter)comp).get(position, context);
         for (Object element : elements) {
-          addLookupItem(set, ce, element, context);
+          myPeer.addLookupItem(set, ce.myTailType, element, context);
         }
       }
       // TODO: KeywordChooser -> ContextGetter
       else if (comp instanceof KeywordChooser) {
         final String[] keywords = ((KeywordChooser)comp).getKeywords(context, position);
         for (String keyword : keywords) {
-          addKeyword(factory, set, ce, keyword, context);
+          myPeer.addKeyword(factory, set, ce.myTailType, keyword, context);
         }
       }
     }
   }
 
-  private void addKeyword(PsiElementFactory factory, Set<LookupItem> set, final CompletionVariantItem ce, final Object comp, CompletionContext context){
-    for (final Object aSet : set) {
-      final LookupItem item = (LookupItem)aSet;
-      if (item.getObject().toString().equals(comp.toString())) {
-        return;
-      }
-    }
-    if(factory == null){
-      addLookupItem(set, ce, comp, context);
-    }
-    else{
-      try{
-        final PsiKeyword keyword = factory.createKeyword((String)comp);
-        addLookupItem(set, ce, keyword, context);
-      }
-      catch(IncorrectOperationException e){
-        addLookupItem(set, ce, comp, context);
-      }
-    }
-  }
-
   public boolean hasReferenceFilter(){
-    for (final Object aMyCompletionsList : myCompletionsList) {
-      if (((CompletionVariantItem)aMyCompletionsList).myCompletion instanceof ElementFilter) {
+    for (final CompletionVariantItem item: myCompletionsList) {
+      if (item.myCompletion instanceof ElementFilter) {
         return true;
       }
     }
@@ -304,9 +223,8 @@ public class CompletionVariant {
   }
 
   public boolean hasKeywordCompletions(){
-    for (final Object aMyCompletionsList : myCompletionsList) {
-      final Object completion = ((CompletionVariantItem)aMyCompletionsList).myCompletion;
-      if (!(completion instanceof ElementFilter)) {
+    for (final CompletionVariantItem item : myCompletionsList) {
+      if (!(item.myCompletion instanceof ElementFilter)) {
         return true;
       }
     }
@@ -315,93 +233,7 @@ public class CompletionVariant {
 
   protected void addReferenceCompletions(PsiReference reference, PsiElement position, Set<LookupItem> set,
                                          CompletionContext context, CompletionVariantItem item){
-    if(item.myCompletion instanceof ElementFilter){
-      final CompletionProcessor processor = new CompletionProcessor(context, position, (ElementFilter)item.myCompletion);
-
-      if (reference instanceof PsiMultiReference) {
-        int javaReferenceStart = -1;
-
-        PsiReference[] references = getReferences((PsiMultiReference)reference);
-
-        for (PsiReference ref : references) {
-          if (ref instanceof PsiJavaReference) {
-            int newStart = ref.getElement().getTextRange().getStartOffset() + ref.getRangeInElement().getStartOffset();
-            if (javaReferenceStart == -1) {
-              javaReferenceStart = newStart;
-            } else {
-              if (newStart == javaReferenceStart) continue;
-            }
-          }
-          addReferenceCompletions(ref, position, set, context, item);
-        }
-      }
-      else if(reference instanceof PsiJavaReference){
-        ((PsiJavaReference)reference).processVariants(processor);
-      }
-      else{
-        final Object[] completions = reference.getVariants();
-        if(completions == null) return;
-
-        for (Object completion : completions) {
-          if (completion instanceof PsiElement) {
-            processor.execute((PsiElement)completion, PsiSubstitutor.EMPTY);
-          }
-          else if (completion instanceof CandidateInfo) {
-            final CandidateInfo info = (CandidateInfo)completion;
-            if (info.isValidResult()) {
-              processor.execute(info.getElement(), PsiSubstitutor.EMPTY);
-            }
-          }
-          else {
-            addLookupItem(set, item, completion, context);
-          }
-        }
-      }
-
-      Collection<CompletionElement> results = processor.getResults();
-      if (results != null) {
-        for (CompletionElement element : results) {
-          final LookupItem lookupItem = addLookupItem(set, item, element.getElement(), context);
-          if (lookupItem != null) {
-            lookupItem.setAttribute(LookupItem.SUBSTITUTOR, element.getSubstitutor());
-            if (element.getQualifier() != null){
-              CompletionUtil.setQualifierType(lookupItem, element.getQualifier());
-            }
-          }
-        }
-      }
-    }
-  }
-
-  private static PsiReference[] getReferences(final PsiMultiReference multiReference) {
-    final PsiReference[] references = multiReference.getReferences();
-    final List<PsiReference> hard = ContainerUtil.findAll(references, new Condition<PsiReference>() {
-      public boolean value(final PsiReference object) {
-        return !object.isSoft();
-      }
-    });
-    if (!hard.isEmpty()) {
-      return hard.toArray(new PsiReference[hard.size()]);
-    }
-    return references;
-  }
-
-  private static String handleCaseInsensitiveVariant(final String prefix, final String uniqueText) {
-    final int length = prefix.length();
-    if (length == 0) return uniqueText;
-    boolean isAllLower = true;
-    boolean isAllUpper = true;
-    boolean sameCase = true;
-    for (int i = 0; i < length && (isAllLower || isAllUpper || sameCase); i++) {
-      final char c = prefix.charAt(i);
-      isAllLower = isAllLower && Character.isLowerCase(c);
-      isAllUpper = isAllUpper && Character.isUpperCase(c);
-      sameCase = sameCase && Character.isLowerCase(c) == Character.isLowerCase(uniqueText.charAt(i)); 
-    }
-    if (sameCase) return uniqueText;
-    if (isAllLower) return uniqueText.toLowerCase();
-    if (isAllUpper) return uniqueText.toUpperCase();
-    return uniqueText;
+    myPeer.addReferenceCompletions(reference, position, set, context, item.myCompletion, item.myTailType);
   }
 
 
