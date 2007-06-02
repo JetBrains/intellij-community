d537f8a9a378b507ff594947713d9df99ed3f563
Eugene Vigdorchik <no_reply@jetbrains.com>
2007-06-02 13:25:48 +0400

    type added

--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/impl/statements/expressions/arithmetic/GrRangeExprImpl.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/impl/statements/expressions/arithmetic/GrRangeExprImpl.java
@@ -3,16 +3,31 @@ package org.jetbrains.plugins.groovy.lang.psi.impl.statements.expressions.arithm
 
 import org.jetbrains.plugins.groovy.lang.psi.impl.statements.expressions.GrBinaryExpressionImpl;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.arithmetic.GrRangeExpression;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrExpression;
 import org.jetbrains.annotations.NotNull;
 import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiType;
+import com.intellij.psi.PsiElementFactory;
 
 /**
  * @author ilyas
  */
 public class GrRangeExprImpl extends GrBinaryExpressionImpl implements GrRangeExpression {
+  private static final String INTEGER_FQ_NAME = "java.lang.Integer";
+  private static final String INT_RANGE_FQ_NAME = "groovy.lang.IntRange";
+  private static final String OBJECT_RANGE_FQ_NAME = "groovy.lang.ObjectRange";
 
   public GrRangeExprImpl(@NotNull ASTNode node) {
     super(node);
+  }
+
+  public PsiType getType() {
+    PsiElementFactory factory = getManager().getElementFactory();
+    GrExpression lop = getLeftOperand();
+    if (lop != null && INTEGER_FQ_NAME.equals(lop.getType().getCanonicalText())) {
+      return factory.createTypeByFQClassName(INT_RANGE_FQ_NAME, getResolveScope());
+    }
+    return factory.createTypeByFQClassName(OBJECT_RANGE_FQ_NAME, getResolveScope());
   }
 
   public String toString() {
