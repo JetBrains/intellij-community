0e97f0f6649472f4e1abf5599d7b93d596b38f79
Bas Leijdekkers <basleijdekkers@gmail.com>
2007-06-04 12:52:26 +0400

    IDEADEV-17501

--- plugins/InspectionGadgets/src/com/siyeh/ig/global/MethodReturnAlwaysConstantInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/global/MethodReturnAlwaysConstantInspection.java
@@ -26,28 +26,31 @@ import com.intellij.psi.util.PsiUtil;
 import com.siyeh.InspectionGadgetsBundle;
 import com.siyeh.ig.BaseGlobalInspection;
 import com.siyeh.ig.psiutils.MethodInheritanceUtils;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import org.jetbrains.annotations.NotNull;
-
 public class MethodReturnAlwaysConstantInspection extends BaseGlobalInspection {
-    private static final Key<Boolean> ALWAYS_CONSTANT = Key.create("ALWAYS_CONSTANT");
+
+    private static final Key<Boolean> ALWAYS_CONSTANT =
+            Key.create("ALWAYS_CONSTANT");
 
     @NotNull
     public String getGroupDisplayName() {
         return GroupNames.CLASSLAYOUT_GROUP_NAME;
     }
 
-    public CommonProblemDescriptor[] checkElement(RefEntity refEntity, AnalysisScope scope, InspectionManager manager, GlobalInspectionContext globalContext) {
+    public CommonProblemDescriptor[] checkElement(
+            RefEntity refEntity, AnalysisScope scope, InspectionManager manager,
+            GlobalInspectionContext globalContext) {
         if (!(refEntity instanceof RefMethod)) {
             return null;
         }
         final RefMethod refMethod = (RefMethod) refEntity;
         final Boolean alreadyProcessed = refMethod.getUserData(ALWAYS_CONSTANT);
-        if (alreadyProcessed != null && alreadyProcessed) {
+        if (alreadyProcessed != null && alreadyProcessed.booleanValue()) {
             return null;
         }
         if (!(refMethod.getElement()instanceof PsiMethod)) {
@@ -60,25 +63,36 @@ public class MethodReturnAlwaysConstantInspection extends BaseGlobalInspection {
         if (!alwaysReturnsConstant(method)) {
             return null;
         }
-        final Set<RefMethod> siblingMethods = MethodInheritanceUtils.calculateSiblingMethods(refMethod);
+        final Set<RefMethod> siblingMethods =
+                MethodInheritanceUtils.calculateSiblingMethods(refMethod);
         for (RefMethod siblingMethod : siblingMethods) {
-            final PsiMethod siblingPsiMethod = (PsiMethod) siblingMethod.getElement();
-            if (method.getBody() != null && !alwaysReturnsConstant(siblingPsiMethod)) {
+            final PsiMethod siblingPsiMethod =
+                    (PsiMethod) siblingMethod.getElement();
+            if (method.getBody() != null &&
+                    !alwaysReturnsConstant(siblingPsiMethod)) {
                 return null;
             }
         }
         final List<ProblemDescriptor> out = new ArrayList<ProblemDescriptor>();
-        for (RefMethod siblingMethod : siblingMethods) {
-            final PsiMethod siblingPsiMethod = (PsiMethod) siblingMethod.getElement();
-            out.add(manager.createProblemDescriptor(siblingPsiMethod, InspectionGadgetsBundle.message(
-                    "method.return.always.constant.problem.descriptor"), (LocalQuickFix[]) null,
-                                                                         ProblemHighlightType.GENERIC_ERROR_OR_WARNING));
-            siblingMethod.putUserData(ALWAYS_CONSTANT, true);
+        for (RefMethod siblingRefMethod : siblingMethods) {
+            final PsiMethod siblingMethod =
+                    (PsiMethod) siblingRefMethod.getElement();
+            final PsiIdentifier identifier = siblingMethod.getNameIdentifier();
+            if (identifier == null) {
+                continue;
+            }
+            out.add(manager.createProblemDescriptor(identifier,
+                    InspectionGadgetsBundle.message(
+                            "method.return.always.constant.problem.descriptor"),
+                    (LocalQuickFix[]) null,
+                    ProblemHighlightType.GENERIC_ERROR_OR_WARNING));
+            siblingRefMethod.putUserData(ALWAYS_CONSTANT,
+                    Boolean.valueOf(true));
         }
         return out.toArray(new ProblemDescriptor[out.size()]);
     }
 
-    private boolean alwaysReturnsConstant(PsiMethod method) {
+    private static boolean alwaysReturnsConstant(PsiMethod method) {
         final PsiCodeBlock body = method.getBody();
         if (body == null) {
             return false;
@@ -91,7 +105,8 @@ public class MethodReturnAlwaysConstantInspection extends BaseGlobalInspection {
         if (!(statement instanceof PsiReturnStatement)) {
             return false;
         }
-        final PsiReturnStatement returnStatement = (PsiReturnStatement) statement;
+        final PsiReturnStatement returnStatement =
+                (PsiReturnStatement) statement;
         final PsiExpression value = returnStatement.getReturnValue();
         return value != null && PsiUtil.isConstantExpression(value);
     }
