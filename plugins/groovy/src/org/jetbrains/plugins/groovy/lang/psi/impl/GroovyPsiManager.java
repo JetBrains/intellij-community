b500f0f0d0df4faa42927931b5b95fd8b40cad02
Eugene Vigdorchik <no_reply@jetbrains.com>
2007-06-11 17:09:29 +0400

    fix configuration

--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/impl/GroovyPsiManager.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/impl/GroovyPsiManager.java
@@ -89,7 +89,11 @@ public class GroovyPsiManager implements ProjectComponent {
 
       public void rootsChanged(ModuleRootEvent event) {
         if (!ApplicationManager.getApplication().isUnitTestMode() && myProject.isInitialized()) {
-          fillDefaultGroovyMethods();
+          ApplicationManager.getApplication().invokeLater(new Runnable() {
+            public void run() {
+              fillDefaultGroovyMethods();
+            }
+          });
         }
       }
     };
