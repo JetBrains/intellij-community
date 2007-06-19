9f9c97779e6154f68a3af8b8bb91abdeca040d14
Dmitry Jemerov <yole@intellij.com>
2007-06-19 16:25:16 +0400

    inline to anonymous: mnemonics and dialog i18n (IDEADEV-17966)

--- refactoring/impl/com/intellij/refactoring/inline/InlineToAnonymousClassDialog.java
+++ refactoring/impl/com/intellij/refactoring/inline/InlineToAnonymousClassDialog.java
@@ -4,6 +4,7 @@ import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiCall;
 import com.intellij.psi.util.PsiFormatUtil;
+import com.intellij.refactoring.RefactoringBundle;
 
 /**
  * @author yole
@@ -17,25 +18,25 @@ public class InlineToAnonymousClassDialog extends InlineOptionsDialog {
     myClass = psiClass;
     myCallToInline = callToInline;
     myInvokedOnReference = (myCallToInline != null);
-    setTitle("Inline to Anonymous Class");
+    setTitle(RefactoringBundle.message("inline.to.anonymous.refactoring"));
     init();
   }
 
   protected String getNameLabelText() {
     String className = PsiFormatUtil.formatClass(myClass, PsiFormatUtil.SHOW_NAME);
-    return "Class " + className;
+    return RefactoringBundle.message("inline.to.anonymous.name.label", className);
   }
 
   protected String getBorderTitle() {
-    return "Inline";
+    return RefactoringBundle.message("inline.to.anonymous.border.title");
   }
 
   protected String getInlineAllText() {
-    return "All references and remove the class";
+    return RefactoringBundle.message("all.references.and.remove.the.class");
   }
 
   protected String getInlineThisText() {
-    return "This reference only and keep the class";
+    return RefactoringBundle.message("this.reference.only.and.keep.the.class");
   }
 
   protected boolean isInlineThis() {
