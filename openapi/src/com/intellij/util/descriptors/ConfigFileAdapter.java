1974242ec1d11f3200f0895b3b190ba8e22e81de
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2007-06-29 17:04:11 +0400

    jpa & hibernate configuration

--- openapi/src/com/intellij/util/descriptors/ConfigFileAdapter.java
+++ openapi/src/com/intellij/util/descriptors/ConfigFileAdapter.java
@@ -4,12 +4,18 @@ package com.intellij.util.descriptors;
  * @author Gregory.Shrago
  */
 public class ConfigFileAdapter implements ConfigFileListener {
+  protected void configChanged(ConfigFile configFile) {
+  }
+
   public void configFileAdded(ConfigFile configFile) {
+    configChanged(configFile);
   }
 
   public void configFileRemoved(ConfigFile configFile) {
+    configChanged(configFile);
   }
 
-  public void configFileChanged(ConfigFile descriptor) {
+  public void configFileChanged(ConfigFile configFile) {
+    configChanged(configFile);
   }
 }
