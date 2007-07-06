f1323e21696f421c8e7bd97df4a8d3c2337f9c3a
Vladislav Kaznacheev <no_reply@jetbrains.com>
2007-07-06 19:25:52 +0400

    Bug when binding fields with similar generic classes

--- util/src/com/intellij/util/xmlb/XmlSerializerImpl.java
+++ util/src/com/intellij/util/xmlb/XmlSerializerImpl.java
@@ -15,7 +15,7 @@ import java.util.*;
 class XmlSerializerImpl {
 
   private SerializationFilter filter;
-  private Map<Pair<Class, Accessor>, Binding> myBindings = new HashMap<Pair<Class, Accessor>, Binding>();
+  private Map<Pair<Type, Accessor>, Binding> myBindings = new HashMap<Pair<Type, Accessor>, Binding>();
 
 
   public XmlSerializerImpl(SerializationFilter filter) {
@@ -60,7 +60,7 @@ class XmlSerializerImpl {
 
 
   private Binding _getClassBinding(Class<?> aClass, Type originalType, final Accessor accessor) {
-    final Pair<Class, Accessor> p = new Pair<Class, Accessor>(aClass, accessor);
+    final Pair<Type, Accessor> p = new Pair<Type, Accessor>(originalType, accessor);
 
     Binding binding = myBindings.get(p);
 
