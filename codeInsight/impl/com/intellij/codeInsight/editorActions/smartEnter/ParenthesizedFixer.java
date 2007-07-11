1ed153728f56fd6fa16278f9fe1c1354c5fa224c
Peter Gromov <peter@jetbrains.com>
2007-07-11 18:14:38 +0400

    fix Schneider

--- codeInsight/impl/com/intellij/codeInsight/editorActions/smartEnter/ParenthesizedFixer.java
+++ codeInsight/impl/com/intellij/codeInsight/editorActions/smartEnter/ParenthesizedFixer.java
@@ -5,7 +5,6 @@
 package com.intellij.codeInsight.editorActions.smartEnter;
 
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiParenthesizedExpression;
 import com.intellij.util.IncorrectOperationException;
@@ -13,8 +12,8 @@ import com.intellij.util.IncorrectOperationException;
 public class ParenthesizedFixer implements Fixer {
   public void apply(Editor editor, SmartEnterProcessor processor, PsiElement psiElement) throws IncorrectOperationException {
     if (psiElement instanceof PsiParenthesizedExpression) {
-      String text = psiElement.getText();
-      if (!StringUtil.endsWithChar(text, ')')) {
+      final PsiElement lastChild = psiElement.getLastChild();
+      if (lastChild != null && !")".equals(lastChild.getText())) {
         editor.getDocument().insertString(psiElement.getTextRange().getEndOffset(), ")");
       }
     }
