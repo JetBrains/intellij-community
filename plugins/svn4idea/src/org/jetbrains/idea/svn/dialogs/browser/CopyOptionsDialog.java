2fe51299c3de59f2163d8bc5a383b36ac7b7e211
Dmitry Jemerov <yole@intellij.com>
2007-07-12 14:13:56 +0400

    snapshoot; provide disposer

--- plugins/svn4idea/src/org/jetbrains/idea/svn/dialogs/browser/CopyOptionsDialog.java
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/dialogs/browser/CopyOptionsDialog.java
@@ -2,7 +2,9 @@ package org.jetbrains.idea.svn.dialogs.browser;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.vcs.VcsConfiguration;
+import com.intellij.ui.DocumentAdapter;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.idea.svn.SvnVcs;
@@ -14,10 +16,8 @@ import org.tmatesoft.svn.core.internal.util.SVNPathUtil;
 
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -31,16 +31,58 @@ public class CopyOptionsDialog extends DialogWrapper {
   private JTextField myNameField;
   private JLabel myURLLabel;
   private RepositoryBrowserComponent myBrowser;
-  private SVNURL myRootURL;
   private JLabel myTargetURL;
+  private JComboBox myMessagesBox;
+  private JPanel myMainPanel;
 
   public CopyOptionsDialog(String title, Project project, SVNURL rootURL, SVNURL url) {
     super(project, true);
     myURL = url;
-    myRootURL = rootURL;
     myProject = project;
+
+    myURLLabel.setText(myURL.toString());
+    myBrowser.setRepositoryURL(rootURL, false);
+    myBrowser.addChangeListener(new TreeSelectionListener() {
+      public void valueChanged(TreeSelectionEvent e) {
+        update();
+      }
+    });
+
+    myNameField.setText(SVNPathUtil.tail(myURL.getPath()));
+    myNameField.selectAll();
+    myNameField.getDocument().addDocumentListener(new DocumentAdapter() {
+      protected void textChanged(final DocumentEvent e) {
+        update();
+      }
+    });
+
+    ArrayList<String> messages = VcsConfiguration.getInstance(myProject).getRecentMessages();
+    Collections.reverse(messages);
+    Object[] model = messages.toArray();
+    myMessagesBox.setModel(new DefaultComboBoxModel(model));
+    myMessagesBox.setRenderer(new MessageBoxCellRenderer());
+
+    String lastMessage = VcsConfiguration.getInstance(myProject).getLastNonEmptyCommitMessage();
+    if (lastMessage != null) {
+      myCommitMessage.setText(lastMessage);
+      myCommitMessage.selectAll();
+    }
+    myMessagesBox.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        myCommitMessage.setText(myMessagesBox.getSelectedItem().toString());
+        myCommitMessage.selectAll();
+      }
+    });
+
     setTitle(title);
     init();
+    update();
+  }
+
+  @Override
+  protected void dispose() {
+    super.dispose();
+    Disposer.dispose(myBrowser);
   }
 
   @NonNls
@@ -69,140 +111,11 @@ public class CopyOptionsDialog extends DialogWrapper {
 
   @Nullable
   protected JComponent createCenterPanel() {
-    JPanel panel = new JPanel(new GridBagLayout());
+    return myMainPanel;
+  }
 
-    GridBagConstraints gc = new GridBagConstraints();
-    gc.insets = new Insets(2, 2, 2, 2);
-    gc.gridwidth = 1;
-    gc.gridheight = 1;
-    gc.gridx = 0;
-    gc.gridy = 0;
-    gc.anchor = GridBagConstraints.WEST;
-    gc.fill = GridBagConstraints.NONE;
-    gc.weightx = 0;
-    gc.weighty = 0;
-
-    panel.add(new JLabel("Source URL:"), gc);
-    gc.gridx += 1;
-    gc.gridwidth = 2;
-    gc.weightx = 1;
-    gc.fill = GridBagConstraints.HORIZONTAL;
-    myURLLabel = new JLabel(myURL.toString());
-    myURLLabel.setFont(myURLLabel.getFont().deriveFont(Font.BOLD));
-    panel.add(myURLLabel, gc);
-
-    gc.gridy += 1;
-    gc.gridwidth = 3;
-    gc.gridx = 0;
-    gc.weightx = 0;
-    gc.fill = GridBagConstraints.NONE;
-    panel.add(new JLabel("Target Location:"), gc);
-    gc.gridy += 1;
-    gc.gridx = 0;
-    gc.weightx = 0;
-    gc.weighty = 0.5;
-    gc.gridwidth = 3;
-    gc.fill = GridBagConstraints.BOTH;
-
+  private void createUIComponents() {
     myBrowser = new RepositoryBrowserComponent(SvnVcs.getInstance(myProject));
-    myBrowser.setRepositoryURL(myRootURL, false);
-    myBrowser.addChangeListener(new TreeSelectionListener() {
-      public void valueChanged(TreeSelectionEvent e) {
-        update();
-      }
-    });
-    panel.add(myBrowser, gc);
-
-    gc.gridy += 1;
-    gc.gridx = 0;
-    gc.weightx = 0;
-    gc.weighty = 0;
-    gc.gridwidth = 1;
-    gc.fill = GridBagConstraints.NONE;
-    panel.add(new JLabel("Target Name:"), gc);
-    gc.gridx += 1;
-    gc.weightx = 1;
-    gc.gridwidth = 2;
-    gc.fill = GridBagConstraints.HORIZONTAL;
-    myNameField = new JTextField();
-    myNameField.setText(SVNPathUtil.tail(myURL.getPath()));
-    myNameField.selectAll();
-    myNameField.getDocument().addDocumentListener(new DocumentListener() {
-      public void insertUpdate(DocumentEvent e) {
-        update();
-      }
-      public void removeUpdate(DocumentEvent e) {
-        update();
-      }
-      public void changedUpdate(DocumentEvent e) {
-        update();
-      }
-    });
-    panel.add(myNameField, gc);
-    gc.gridy += 1;
-    gc.gridx = 0;
-    gc.weightx = 0;
-    gc.gridwidth = 1;
-    gc.fill = GridBagConstraints.NONE;
-    panel.add(new JLabel("Target URL: "), gc);
-    gc.gridx += 1;
-    gc.weightx = 1;
-    gc.gridwidth = 2;
-    gc.fill = GridBagConstraints.HORIZONTAL;
-    myTargetURL = new JLabel();
-    myTargetURL.setFont(myTargetURL.getFont().deriveFont(Font.BOLD));
-    panel.add(myTargetURL, gc);
-
-    gc.gridy += 1;
-    gc.gridx = 0;
-    gc.weightx = 0;
-    gc.gridwidth = 3;
-    gc.fill = GridBagConstraints.NONE;
-    panel.add(new JLabel("Commit Message:"), gc);
-    gc.gridy += 1;
-    gc.gridwidth = 3;
-    gc.gridx = 0;
-    gc.weightx = 1;
-    gc.weighty = .5;
-    gc.anchor = GridBagConstraints.NORTH;
-    gc.fill = GridBagConstraints.BOTH;
-
-    myCommitMessage = new JTextArea(5, 0);
-    panel.add(new JScrollPane(myCommitMessage), gc);
-
-    gc.gridy += 1;
-    gc.gridwidth = 3;
-    gc.gridx = 0;
-    gc.weightx = 0;
-    gc.weighty = 0;
-    gc.anchor = GridBagConstraints.NORTH;
-    gc.fill = GridBagConstraints.HORIZONTAL;
-    panel.add(new JLabel("Recent Messages: "), gc);
-    gc.gridy += 1;
-
-    ArrayList<String> messages = VcsConfiguration.getInstance(myProject).getRecentMessages();
-    if (messages != null) {
-      Collections.reverse(messages);
-    }
-    Object[] model = messages != null ? messages.toArray() : new Object[] {""};
-    final JComboBox messagesBox = new JComboBox(model);
-    messagesBox.setRenderer(new MessageBoxCellRenderer());
-    panel.add(messagesBox, gc);
-
-    String lastMessage = VcsConfiguration.getInstance(myProject).getLastNonEmptyCommitMessage();
-    if (lastMessage != null) {
-      myCommitMessage.setText(lastMessage);
-      myCommitMessage.selectAll();
-    }
-    messagesBox.addActionListener(new ActionListener() {
-
-      public void actionPerformed(ActionEvent e) {
-        myCommitMessage.setText(messagesBox.getSelectedItem().toString());
-        myCommitMessage.selectAll();
-      }
-    });
-    update();
-    return panel;
   }
 
   private void update() {
@@ -233,5 +146,4 @@ public class CopyOptionsDialog extends DialogWrapper {
   public JComponent getPreferredFocusedComponent() {
     return myNameField;
   }
-
 }
