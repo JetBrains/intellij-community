7430e318739e18bbb71838e63ae258161ffda38e
Dmitry Jemerov <yole@intellij.com>
2007-07-12 16:27:48 +0400

    extract more pieces to common framework

--- openapi/src/com/intellij/testFramework/AbstractVcsTestCase.java
+++ openapi/src/com/intellij/testFramework/AbstractVcsTestCase.java
@@ -6,9 +6,26 @@ import com.intellij.execution.process.ProcessEvent;
 import com.intellij.execution.process.ProcessOutputTypes;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Key;
+import com.intellij.openapi.util.Ref;
 import com.intellij.openapi.vfs.CharsetToolkit;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.vcs.ProjectLevelVcsManager;
+import com.intellij.openapi.vcs.AbstractVcs;
+import com.intellij.openapi.vcs.VcsConfiguration;
+import com.intellij.openapi.vcs.VcsShowConfirmationOption;
+import com.intellij.openapi.vcs.changes.ChangeListManager;
+import com.intellij.openapi.vcs.changes.VcsDirtyScopeManager;
+import com.intellij.openapi.vcs.changes.VcsDirtyScope;
+import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.testFramework.fixtures.IdeaProjectTestFixture;
+import com.intellij.testFramework.fixtures.TestFixtureBuilder;
+import com.intellij.testFramework.fixtures.IdeaTestFixtureFactory;
+import com.intellij.testFramework.builders.JavaModuleFixtureBuilder;
 import org.jetbrains.annotations.Nullable;
+import org.junit.Assert;
 
 import java.io.File;
 import java.io.IOException;
@@ -25,12 +42,17 @@ public class AbstractVcsTestCase {
   protected Project myProject;
   protected VirtualFile myWorkingCopyDir;
   protected File myClientBinaryPath;
+  protected IdeaProjectTestFixture myProjectFixture;
 
-  protected RunResult runClient(String exeName, @Nullable String stdin, String[] commandLine) throws IOException {
+  protected RunResult runClient(String exeName, @Nullable String stdin, @Nullable final File workingDir, String[] commandLine) throws IOException {
     final List<String> arguments = new ArrayList<String>();
     arguments.add(new File(myClientBinaryPath, exeName).toString());
     Collections.addAll(arguments, commandLine);
-    Process clientProcess = new ProcessBuilder().command(arguments).start();
+    final ProcessBuilder builder = new ProcessBuilder().command(arguments);
+    if (workingDir != null) {
+      builder.directory(workingDir);
+    }
+    Process clientProcess = builder.start();
 
     final RunResult result = new RunResult();
 
@@ -64,6 +86,97 @@ public class AbstractVcsTestCase {
     handler.waitFor();
     result.exitCode = clientProcess.exitValue();
     return result;
+  }
+
+  protected void initProject(final File clientRoot) throws Exception {
+    final TestFixtureBuilder<IdeaProjectTestFixture> testFixtureBuilder = IdeaTestFixtureFactory.getFixtureFactory().createFixtureBuilder();
+    myProjectFixture = testFixtureBuilder.getFixture();
+    testFixtureBuilder.addModule(JavaModuleFixtureBuilder.class).addContentRoot(clientRoot.toString());
+    myProjectFixture.setUp();
+    myProject = myProjectFixture.getProject();
+
+    ((ProjectComponent) ChangeListManager.getInstance(myProject)).projectOpened();
+    ((ProjectComponent) VcsDirtyScopeManager.getInstance(myProject)).projectOpened();
+
+    ApplicationManager.getApplication().runWriteAction(new Runnable() {
+      public void run() {
+        myWorkingCopyDir = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(clientRoot);
+        assert myWorkingCopyDir != null;
+      }
+    });
+  }
+
+  protected void activateVCS(final String vcsName) {
+    ProjectLevelVcsManager vcsManager = ProjectLevelVcsManager.getInstance(myProject);
+    vcsManager.setDirectoryMapping(myWorkingCopyDir.getPath(), vcsName);
+    vcsManager.updateActiveVcss();
+
+    AbstractVcs vcs = vcsManager.findVcsByName(vcsName);
+    Assert.assertEquals(1, vcsManager.getRootsUnderVcs(vcs).length);
+  }
+
+  protected VirtualFile createFileInCommand(final String name, @Nullable final String content) {
+    final Ref<VirtualFile> result = new Ref<VirtualFile>();
+    CommandProcessor.getInstance().executeCommand(myProject, new Runnable() {
+      public void run() {
+        try {
+          VirtualFile file = myWorkingCopyDir.createChildData(this, name);
+          if (content != null) {
+            file.setBinaryContent(CharsetToolkit.getUtf8Bytes(content));
+          }
+          result.set(file);
+        }
+        catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+    }, "", null);
+    return result.get();
+  }
+
+  protected void tearDownProject() throws Exception {
+    if (myProject != null) {
+      ((ProjectComponent) VcsDirtyScopeManager.getInstance(myProject)).projectClosed();
+      ((ProjectComponent) ChangeListManager.getInstance(myProject)).projectClosed();
+      ((ProjectComponent) ProjectLevelVcsManager.getInstance(myProject)).projectClosed();
+      myProject = null;
+    }
+    if (myProjectFixture != null) {
+      myProjectFixture.tearDown();
+      myProjectFixture = null;
+    }
+  }
+
+  protected void enableSilentOperation(final String vcsName, final VcsConfiguration.StandardConfirmation op) {
+    ProjectLevelVcsManager vcsManager = ProjectLevelVcsManager.getInstance(myProject);
+    final AbstractVcs vcs = vcsManager.findVcsByName(vcsName);
+    VcsShowConfirmationOption option = vcsManager.getStandardConfirmation(op, vcs);
+    option.setValue(VcsShowConfirmationOption.Value.DO_ACTION_SILENTLY);
+  }
+
+  protected void verify(final RunResult runResult) {
+    Assert.assertEquals(runResult.stdErr, 0, runResult.exitCode);
+  }
+
+  protected VcsDirtyScope getAllDirtyScope() {
+    VcsDirtyScopeManager dirtyScopeManager = VcsDirtyScopeManager.getInstance(myProject);
+    dirtyScopeManager.markEverythingDirty();
+    List<VcsDirtyScope> scopes = dirtyScopeManager.retrieveScopes();
+    Assert.assertEquals(1, scopes.size());
+    return scopes.get(0);
+  }
+
+  protected void renameFileInCommand(final VirtualFile fileToAdd, final String newName) {
+    CommandProcessor.getInstance().executeCommand(myProject, new Runnable() {
+      public void run() {
+        try {
+          fileToAdd.rename(this, newName);
+        }
+        catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+    }, "", null);
   }
 
   protected class RunResult {
