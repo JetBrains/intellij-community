62887855dd235620532afbfc9f89ca1f5542ca49
Dmitry Jemerov <yole@intellij.com>
2007-07-19 20:20:40 +0400

    use standard rollback action and standard "delete local copies of added files" option for CVS (IDEADEV-15972)

--- plugins/cvs2/source/com/intellij/cvsSupport2/checkinProject/CvsRollbacker.java
+++ plugins/cvs2/source/com/intellij/cvsSupport2/checkinProject/CvsRollbacker.java
@@ -1,14 +1,13 @@
 package com.intellij.cvsSupport2.checkinProject;
 
+import com.intellij.cvsSupport2.CvsUtil;
 import com.intellij.cvsSupport2.actions.RestoreFileAction;
 import com.intellij.cvsSupport2.actions.cvsContext.CvsContext;
 import com.intellij.cvsSupport2.actions.cvsContext.CvsContextAdapter;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.application.ModalityState;
+import com.intellij.cvsSupport2.util.CvsVfsUtil;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.command.CommandProcessor;
 
 import java.io.IOException;
 
@@ -33,50 +32,9 @@ public class CvsRollbacker {
     return restoreFile(parent, name);
   }
 
-  public boolean rollbackFileCreating(VirtualFile parent, String name) throws IOException {
-    return removeFile(parent, name);
-  }
-
-
-  private boolean removeFile(final VirtualFile parent, final String name) throws IOException {
-    final IOException[] exception = new IOException[1];
-    final boolean[] result = new boolean[1];
-
-    Runnable action = new Runnable() {
-      public void run() {
-        Runnable writeAction = new Runnable() {
-          public void run() {
-            ApplicationManager.getApplication().runWriteAction(new Runnable() {
-              public void run() {
-                final VirtualFile file = parent.findChild(name);
-                if (file == null) {
-                  result[0] = false;
-                  return;
-                }
-                try {
-                  file.delete(this);
-                }
-                catch (IOException e) {
-                  exception[0] = e;
-                  result[0] = false;
-                  return;
-                }
-                result[0] = true;
-              }
-            });
-          }
-        };
-        CommandProcessor.getInstance().executeCommand(myProject, writeAction, com.intellij.CvsBundle.message("command.name.rollback.file.creation"), null);
-      }
-    };
-    if (ApplicationManager.getApplication().isDispatchThread()) {
-      action.run();
-    }
-    else {
-      ApplicationManager.getApplication().invokeAndWait(action, ModalityState.defaultModalityState());
-    }
-    if (exception[0] != null) throw exception[0];
-    return result[0];
+  public static boolean rollbackFileCreating(VirtualFile parent, String name) throws IOException {
+    CvsUtil.removeEntryFor(CvsVfsUtil.getFileFor(parent, name));
+    return true;
   }
 
   private boolean restoreFile(final VirtualFile parent, String name) {
