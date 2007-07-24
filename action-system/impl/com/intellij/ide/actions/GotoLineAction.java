012a9fb9fb99f38a7fc4be0567205446baf1734c
Dmitry Jemerov <yole@intellij.com>
2007-07-24 14:51:20 +0400

    goto line works in diff (IDEADEV-19311)

--- action-system/impl/com/intellij/ide/actions/GotoLineAction.java
+++ action-system/impl/com/intellij/ide/actions/GotoLineAction.java
@@ -9,34 +9,42 @@ import com.intellij.openapi.fileEditor.ex.IdeDocumentHistory;
 import com.intellij.openapi.project.Project;
 
 public class GotoLineAction extends AnAction {
+  public GotoLineAction() {
+    setEnabledInModalContext(true);
+  }
+
   public void actionPerformed(AnActionEvent e) {
-    DataContext dataContext = e.getDataContext();
-    final Project project = (Project)dataContext.getData(DataConstants.PROJECT);
-    final Editor editor = (Editor)dataContext.getData(DataConstants.EDITOR);
-    CommandProcessor processor = CommandProcessor.getInstance();
-    processor.executeCommand(
-        project, new Runnable(){
-        public void run() {
-          GotoLineNumberDialog dialog = new GotoLineNumberDialog(project, editor);
-          dialog.show();
-          IdeDocumentHistory.getInstance(project).includeCurrentCommandAsNavigation();
-        }
-      },
-      IdeBundle.message("command.go.to.line"),
-      null
-    );
+    final Project project = e.getData(DataKeys.PROJECT);
+    final Editor editor = e.getData(DataKeys.EDITOR);
+    if (Boolean.TRUE.equals(e.getData(DataKeys.IS_MODAL_CONTEXT))) {
+      GotoLineNumberDialog dialog = new GotoLineNumberDialog(project, editor);
+      dialog.show();
+    }
+    else {
+      CommandProcessor processor = CommandProcessor.getInstance();
+      processor.executeCommand(
+          project, new Runnable(){
+          public void run() {
+            GotoLineNumberDialog dialog = new GotoLineNumberDialog(project, editor);
+            dialog.show();
+            IdeDocumentHistory.getInstance(project).includeCurrentCommandAsNavigation();
+          }
+        },
+        IdeBundle.message("command.go.to.line"),
+        null
+      );
+    }
   }
 
   public void update(AnActionEvent event){
     Presentation presentation = event.getPresentation();
-    DataContext dataContext = event.getDataContext();
-    Project project = (Project)dataContext.getData(DataConstants.PROJECT);
+    Project project = event.getData(DataKeys.PROJECT);
     if (project == null) {
       presentation.setEnabled(false);
       presentation.setVisible(false);
       return;
     }
-    Editor editor = (Editor)dataContext.getData(DataConstants.EDITOR);
+    Editor editor = event.getData(DataKeys.EDITOR);
     presentation.setEnabled(editor != null);
     presentation.setVisible(editor != null);
   }
