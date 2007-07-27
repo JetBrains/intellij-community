676334b390045d2a16f2c67de0b51f0baa2345a5
Maxim Shafirov <max@jetbrains.com>
2007-07-27 19:34:48 +0400

    ClassCastException fix

--- source/com/intellij/openapi/vfs/newvfs/impl/VirtualDirectoryImpl.java
+++ source/com/intellij/openapi/vfs/newvfs/impl/VirtualDirectoryImpl.java
@@ -168,21 +168,21 @@ public class VirtualDirectoryImpl extends VirtualFileSystemEntry {
   }
 
   @Nullable
-  private VirtualFile[] asArray() {
+  private synchronized VirtualFile[] asArray() {
     if (myChildren instanceof VirtualFile[]) return (VirtualFile[])myChildren;
     return null;
   }
 
   @Nullable
   @SuppressWarnings({"unchecked"})
-  private Map<String, VirtualFile> asMap() {
+  private synchronized Map<String, VirtualFile> asMap() {
     if (myChildren instanceof Map) return (Map<String, VirtualFile>)myChildren;
     return null;
   }
 
   @NotNull
   @SuppressWarnings({"unchecked"})
-  private Map<String, VirtualFile> ensureAsMap() {
+  private synchronized Map<String, VirtualFile> ensureAsMap() {
     assert !(myChildren instanceof VirtualFile[]);
 
     if (myChildren == null) {
