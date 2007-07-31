4e0977a8c15d93f45403cee5c8633f5538776fc4
Dmitry Jemerov <yole@intellij.com>
2007-07-31 18:42:57 +0400

    cleanup

--- refactoring/impl/com/intellij/refactoring/inheritanceToDelegation/InheritanceToDelegationUtil.java
+++ refactoring/impl/com/intellij/refactoring/inheritanceToDelegation/InheritanceToDelegationUtil.java
@@ -6,20 +6,21 @@ import com.intellij.psi.*;
  * @author dsl
  */
 public class InheritanceToDelegationUtil {
+  private InheritanceToDelegationUtil() {
+  }
+
   public static boolean isInnerClassNeeded(PsiClass aClass, PsiClass baseClass) {
     if(baseClass.isInterface()) return true;
     if(baseClass.hasModifierProperty(PsiModifier.ABSTRACT)) return true;
     PsiMethod[] methods = aClass.getMethods();
 
-    for (int i = 0; i < methods.length; i++) {
-      PsiMethod method = methods[i];
-
-      if(method.isConstructor() || method.hasModifierProperty(PsiModifier.PRIVATE)) continue;
+    for (PsiMethod method : methods) {
+      if (method.isConstructor() || method.hasModifierProperty(PsiModifier.PRIVATE)) continue;
       PsiMethod baseMethod = baseClass.findMethodBySignature(method, true);
-      if(baseMethod != null) {
+      if (baseMethod != null) {
         PsiClass containingClass = baseMethod.getContainingClass();
         String qName = containingClass.getQualifiedName();
-        if(qName == null || !"java.lang.Object".equals(qName)) return true;
+        if (qName == null || !"java.lang.Object".equals(qName)) return true;
       }
     }
     return false;
