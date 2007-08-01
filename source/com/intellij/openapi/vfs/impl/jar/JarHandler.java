35f373748d54b5dc86baa6c93d0ea7b04cdfb751
Maxim Shafirov <max@jetbrains.com>
2007-08-01 23:10:10 +0400

    IDEADEV-19766: JarHander.getInputStream() is now tolerant to not being closed.

--- source/com/intellij/openapi/vfs/impl/jar/JarHandler.java
+++ source/com/intellij/openapi/vfs/impl/jar/JarHandler.java
@@ -261,23 +261,25 @@ public class JarHandler implements FileSystemInterface {
     try {
       final ZipEntry entry = convertToEntry(file);
       if (entry == null) {
-        lock.unlock();
         return new ByteArrayInputStream(new byte[0]);
       }
 
       final ZipFile zip = getZip();
       assert zip != null;
 
-      return new BufferedInputStream(zip.getInputStream(entry)) {
-        public void close() throws IOException {
-          super.close();
-          lock.unlock();
-        }
-      };
+      ByteArrayOutputStream result = new ByteArrayOutputStream((int)entry.getSize());
+      InputStream nativeStream = new BufferedInputStream(zip.getInputStream(entry));
+      try {
+        FileUtil.copy(nativeStream, result);
+      }
+      finally {
+        nativeStream.close();
+      }
+
+      return new ByteArrayInputStream(result.toByteArray());
     }
-    catch (Throwable e) {
+    finally{
       lock.unlock();
-      throw new RuntimeException(e);
     }
   }
 
