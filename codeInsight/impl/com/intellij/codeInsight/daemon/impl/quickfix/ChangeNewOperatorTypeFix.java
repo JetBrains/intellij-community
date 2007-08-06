d8ab004a5e14a690cc8a536bffab2392afd2e091
Eugene Vigdorchik <no_reply@jetbrains.com>
2007-08-06 20:20:34 +0400

    IDEADEV-19948

--- codeInsight/impl/com/intellij/codeInsight/daemon/impl/quickfix/ChangeNewOperatorTypeFix.java
+++ codeInsight/impl/com/intellij/codeInsight/daemon/impl/quickfix/ChangeNewOperatorTypeFix.java
@@ -10,9 +10,13 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.*;
 import com.intellij.psi.util.PsiUtil;
+import com.intellij.psi.util.TypeConversionUtil;
 import com.intellij.util.IncorrectOperationException;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Iterator;
 
 public class ChangeNewOperatorTypeFix implements IntentionAction {
   private final PsiType myType;
@@ -33,11 +37,11 @@ public class ChangeNewOperatorTypeFix implements IntentionAction {
     return QuickFixBundle.message("change.new.operator.type.family");
   }
 
-  public boolean isAvailable(Project project, Editor editor, PsiFile file) {
+  public boolean isAvailable(@NotNull Project project, Editor editor, PsiFile file) {
     return myType.isValid() && myExpression.isValid() && myExpression.getManager().isInProject(myExpression);
   }
 
-  public void invoke(Project project, Editor editor, PsiFile file) throws IncorrectOperationException {
+  public void invoke(@NotNull Project project, Editor editor, PsiFile file) throws IncorrectOperationException {
     if (!CodeInsightUtil.prepareFileForWrite(file)) return;
     changeNewOperatorType(myExpression, myType, editor);
   }
@@ -79,7 +83,57 @@ public class ChangeNewOperatorTypeFix implements IntentionAction {
   public static void register(final HighlightInfo highlightInfo, PsiExpression expression, final PsiType lType) {
     expression = PsiUtil.deparenthesizeExpression(expression);
     if (!(expression instanceof PsiNewExpression)) return;
+    final PsiType rType = expression.getType();
+    PsiType newType = lType;
+    if (rType instanceof PsiClassType && newType instanceof PsiClassType) {
+      final PsiClassType.ClassResolveResult rResolveResult = ((PsiClassType)rType).resolveGenerics();
+      final PsiClass rClass = rResolveResult.getElement();
+      if (rClass != null) {
+        final PsiClassType.ClassResolveResult lResolveResult = ((PsiClassType)newType).resolveGenerics();
+        final PsiClass lClass = lResolveResult.getElement();
+        if (lClass != null) {
+          PsiSubstitutor substitutor = getInheritorSubstitutorForNewExpression(lClass, rClass, lResolveResult.getSubstitutor(), expression);
+          if (substitutor != null) {
+            newType = lClass.getManager().getElementFactory().createType(rClass, substitutor);
+          }
+        }
+      }
+    }
     PsiNewExpression newExpression = (PsiNewExpression)expression;
-    QuickFixAction.registerQuickFixAction(highlightInfo, new ChangeNewOperatorTypeFix(lType, newExpression));
+    QuickFixAction.registerQuickFixAction(highlightInfo, new ChangeNewOperatorTypeFix(newType, newExpression));
+  }
+
+  /* Guesswork
+  */
+  @Nullable
+  private static PsiSubstitutor getInheritorSubstitutorForNewExpression(final PsiClass baseClass, final PsiClass inheritor,
+                                                                       final PsiSubstitutor baseSubstitutor, final PsiElement context) {
+    PsiManager manager = baseClass.getManager();
+    final PsiResolveHelper resolveHelper = manager.getResolveHelper();
+    PsiSubstitutor superSubstitutor = TypeConversionUtil.getClassSubstitutor(baseClass, inheritor, PsiSubstitutor.EMPTY);
+    if (superSubstitutor == null) return null;
+    PsiSubstitutor inheritorSubstitutor = PsiSubstitutor.EMPTY;
+    final Iterator<PsiTypeParameter> inheritorParamIter = PsiUtil.typeParametersIterator(inheritor);
+    while (inheritorParamIter.hasNext()) {
+      PsiTypeParameter inheritorParameter = inheritorParamIter.next();
+
+      final Iterator<PsiTypeParameter> baseParamIter = PsiUtil.typeParametersIterator(baseClass);
+      while (baseParamIter.hasNext()) {
+        PsiTypeParameter baseParameter = baseParamIter.next();
+        final PsiType substituted = superSubstitutor.substitute(baseParameter);
+        PsiType arg = baseSubstitutor.substitute(baseParameter);
+        if (arg instanceof PsiWildcardType) arg = ((PsiWildcardType)arg).getBound();
+        PsiType substitution =
+          resolveHelper.getSubstitutionForTypeParameter(inheritorParameter, substituted, arg, true, PsiUtil.getLanguageLevel(context));
+        if (substitution == PsiType.NULL) continue;
+        if (substitution == null) {
+          return manager.getElementFactory().createRawSubstitutor(inheritor);
+        }
+        inheritorSubstitutor = inheritorSubstitutor.put(inheritorParameter, substitution);
+        break;
+      }
+    }
+
+    return inheritorSubstitutor;
   }
 }
