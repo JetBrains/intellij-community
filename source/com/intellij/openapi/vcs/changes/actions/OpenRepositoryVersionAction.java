5990911aa28665034667751930a32d331c875602
Dmitry Jemerov <yole@intellij.com>
2007-08-22 19:10:48 +0400

    disable "open repository version" for directories (IDEADEV-20352)

--- source/com/intellij/openapi/vcs/changes/actions/OpenRepositoryVersionAction.java
+++ source/com/intellij/openapi/vcs/changes/actions/OpenRepositoryVersionAction.java
@@ -3,13 +3,14 @@ package com.intellij.openapi.vcs.changes.actions;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.fileEditor.OpenFileDescriptor;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.IconLoader;
+import com.intellij.openapi.vcs.VcsBundle;
 import com.intellij.openapi.vcs.changes.Change;
 import com.intellij.openapi.vcs.changes.ContentRevision;
 import com.intellij.openapi.vcs.vfs.ContentRevisionVirtualFile;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.fileEditor.OpenFileDescriptor;
-import com.intellij.openapi.project.Project;
 import com.intellij.pom.Navigatable;
 
 /**
@@ -18,7 +19,7 @@ import com.intellij.pom.Navigatable;
 public class OpenRepositoryVersionAction extends AnAction {
   public OpenRepositoryVersionAction() {
     // TODO[yole]: real icon
-    super("Open Repository Version", "Open editor with selected revision of the file",
+    super(VcsBundle.message("open.repository.version.text"), VcsBundle.message("open.repository.version.description"),
           IconLoader.getIcon("/objectBrowser/showEditorHighlighting.png"));
   }
 
@@ -28,7 +29,7 @@ public class OpenRepositoryVersionAction extends AnAction {
     assert changes != null;
     for(Change change: changes) {
       ContentRevision revision = change.getAfterRevision();
-      if (revision == null) continue;
+      if (revision == null || revision.getFile().isDirectory()) continue;
       VirtualFile vFile = ContentRevisionVirtualFile.create(revision);
       Navigatable navigatable = new OpenFileDescriptor(project, vFile);
       navigatable.navigate(true);
@@ -38,6 +39,16 @@ public class OpenRepositoryVersionAction extends AnAction {
   public void update(final AnActionEvent e) {
     Project project = e.getData(DataKeys.PROJECT);
     Change[] changes = e.getData(DataKeys.CHANGES);
-    e.getPresentation().setEnabled(project != null && changes != null);
+    e.getPresentation().setEnabled(project != null && changes != null && hasValidChanges(changes));
+  }
+
+  private static boolean hasValidChanges(final Change[] changes) {
+    for(Change c: changes) {
+      final ContentRevision contentRevision = c.getAfterRevision();
+      if (contentRevision != null && !contentRevision.getFile().isDirectory()) {
+        return true;
+      }
+    }
+    return false;
   }
 }
