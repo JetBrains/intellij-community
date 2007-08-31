3055ab9789f39031537f44b66109807055ab23a5
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2007-08-31 14:16:17 +0400

    IDEADEV-20586; IDEADEV-20584

--- source/com/intellij/find/impl/HelpID.java
+++ source/com/intellij/find/impl/HelpID.java
@@ -7,11 +7,11 @@ import org.jetbrains.annotations.NonNls;
  *
  */
 public interface HelpID {
-  @NonNls String FIND_PACKAGE_USAGES = "find.findPackageUsages";
-  @NonNls String FIND_CLASS_USAGES = "find.findClassUsages";
-  @NonNls String FIND_METHOD_USAGES = "find.findMethodUsages";
-  @NonNls String FIND_OTHER_USAGES = "find.findOtherUsages";
-  @NonNls String FIND_THROW_USAGES = "find.findThrowUsages";
+  @NonNls String FIND_PACKAGE_USAGES = "reference.dialogs.findUsages.package";
+  @NonNls String FIND_CLASS_USAGES = "reference.dialogs.findUsages.class";
+  @NonNls String FIND_METHOD_USAGES = "reference.dialogs.findUsages.method";
+  @NonNls String FIND_OTHER_USAGES = "reference.dialogs.findUsages.other";
+  @NonNls String FIND_THROW_USAGES = "reference.dialogs.findUsages.throwUsages";
 
 
   @NonNls String FIND_IN_PROJECT = "find.findInProject";
@@ -19,6 +19,6 @@ public interface HelpID {
 
   @NonNls String FIND_OPTIONS = "find.findOptions";
   @NonNls String REPLACE_OPTIONS = "find.replaceOptions";
-  @NonNls String FIND_IN_PATH = "find.findInPath";
-  @NonNls String REPLACE_IN_PATH = "find.replaceInPath";
+  @NonNls String FIND_IN_PATH = "reference.dialogs.findinpath";
+  @NonNls String REPLACE_IN_PATH = "reference.dialogs.findinpath";
 }
