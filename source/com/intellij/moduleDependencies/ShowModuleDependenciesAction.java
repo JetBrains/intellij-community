45f10eeaffcc11d79cb8c10b42fdf000cf92e6d6
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2007-09-03 18:19:57 +0400

    tab title (IDEADEV-20685)

--- source/com/intellij/moduleDependencies/ShowModuleDependenciesAction.java
+++ source/com/intellij/moduleDependencies/ShowModuleDependenciesAction.java
@@ -1,5 +1,6 @@
 package com.intellij.moduleDependencies;
 
+import com.intellij.analysis.AnalysisScope;
 import com.intellij.analysis.AnalysisScopeBundle;
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.module.Module;
@@ -7,6 +8,7 @@ import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.module.ModuleUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.peer.PeerFactory;
 import com.intellij.psi.PsiElement;
 import com.intellij.ui.content.Content;
@@ -26,9 +28,11 @@ public class ShowModuleDependenciesAction extends AnAction{
       return;
     }
     ModulesDependenciesPanel panel;
+    AnalysisScope scope = new AnalysisScope(project);
     final Module[] modules = (Module[])dataContext.getData(DataConstants.MODULE_CONTEXT_ARRAY);
     if (modules != null){
       panel = new ModulesDependenciesPanel(project, modules);
+      scope = new AnalysisScope(modules);
     } else {
       final PsiElement element = (PsiElement)dataContext.getData(DataConstants.PSI_FILE);
       final Module module = element != null ? ModuleUtil.findModuleForPsiElement(element) : null;
@@ -38,6 +42,7 @@ public class ShowModuleDependenciesAction extends AnAction{
         if (dlg.isOK()){
           if (!dlg.useProjectScope()){
             panel = new ModulesDependenciesPanel(project, new Module[]{module});
+            scope = new AnalysisScope(module);
           } else {
             panel = new ModulesDependenciesPanel(project);
           }
@@ -52,7 +57,7 @@ public class ShowModuleDependenciesAction extends AnAction{
     Content content = PeerFactory.getInstance().getContentFactory().createContent(panel,
                                                                                   AnalysisScopeBundle.message(
                                                                                     "module.dependencies.toolwindow.title",
-                                                                                    project.getName()),
+                                                                                    StringUtil.decapitalize(scope.getDisplayName())),
                                                                                   false);
     content.setDisposer(panel);
     panel.setContent(content);
