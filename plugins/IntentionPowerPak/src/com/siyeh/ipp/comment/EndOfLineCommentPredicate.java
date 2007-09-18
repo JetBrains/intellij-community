3fa958e36ee397d68b29f619cc4cd26ad2a3cb4c
Bas Leijdekkers <basleijdekkers@gmail.com>
2007-09-18 20:25:53 +0400

    IDEADEV-15923

--- plugins/IntentionPowerPak/src/com/siyeh/ipp/comment/EndOfLineCommentPredicate.java
+++ plugins/IntentionPowerPak/src/com/siyeh/ipp/comment/EndOfLineCommentPredicate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2005 Dave Griffith
+ * Copyright 2003-2007 Dave Griffith, Bas Leijdekkers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,7 +22,13 @@ import com.intellij.psi.javadoc.PsiDocComment;
 import com.intellij.psi.tree.IElementType;
 import com.siyeh.ipp.base.PsiElementPredicate;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 class EndOfLineCommentPredicate implements PsiElementPredicate{
+
+    private static final Pattern NO_INSPECTION_PATTERN =
+            Pattern.compile("//[\t ]*noinspection .*");
 
     public boolean satisfiedBy(PsiElement element){
         if(!(element instanceof PsiComment)){  
@@ -33,6 +39,11 @@ class EndOfLineCommentPredicate implements PsiElementPredicate{
         }
         final PsiComment comment = (PsiComment) element;
         final IElementType type = comment.getTokenType();
-        return JavaTokenType.END_OF_LINE_COMMENT.equals(type);
+        if (!JavaTokenType.END_OF_LINE_COMMENT.equals(type)) {
+            return false;
+        }
+        final String text = comment.getText();
+        final Matcher matcher = NO_INSPECTION_PATTERN.matcher(text);
+        return !matcher.matches();
     }
 }
