3b2fd2576a431411e0e0937dafa0bdcd7b7d3828
Alexey Pegov <alexey.pegov@jetbrains.com>
2007-09-20 19:56:52 +0400

    IDEADEV-21237 Error message: File is not valid:virthost + IDEADEV-21271 IVFAE: FileAttribute.ensureFileIsValid

--- xml/impl/src/com/intellij/xml/breadcrumbs/BreadcrumbsComponent.java
+++ xml/impl/src/com/intellij/xml/breadcrumbs/BreadcrumbsComponent.java
@@ -171,12 +171,12 @@ public class BreadcrumbsComponent extends JComponent implements Disposable {
 
     //final PsiFile file = myFile.getViewProvider().getPsi(myLanguage);
     //return file != null ? file.getViewProvider().findElementAt(offset) : null;
-    return myFile.getViewProvider().findElementAt(offset);
+    return myFile.isValid() ? myFile.getViewProvider().findElementAt(offset) : null;
   }
 
   @Nullable
   private List<LineElement> getLineElements(@Nullable final PsiElement endElement) {
-    if (endElement == null) {
+    if (endElement == null || !endElement.isValid()) {
       return null;
     }
 
