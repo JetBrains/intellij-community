dee84ab2d38165964016d6e9ee9bbaadf4603373
Peter Gromov <peter@jetbrains.com>
2007-09-22 13:07:07 +0400

    suggest class literal inheritors

--- source/com/intellij/psi/filters/getters/ClassLiteralGetter.java
+++ source/com/intellij/psi/filters/getters/ClassLiteralGetter.java
@@ -1,6 +1,8 @@
 package com.intellij.psi.filters.getters;
 
+import com.intellij.codeInsight.CodeInsightUtil;
 import com.intellij.codeInsight.completion.CompletionContext;
+import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementFactory;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.psi.*;
@@ -8,10 +10,15 @@ import com.intellij.psi.filters.ContextGetter;
 import com.intellij.util.ArrayUtil;
 import com.intellij.util.IncorrectOperationException;
 import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClassLiteralGetter implements ContextGetter {
   private static final Logger LOG = Logger.getInstance("com.intellij.psi.filters.getters.ClassLiteralGetter");
   private ContextGetter myBaseGetter;
+  @NonNls private static final String DOT_CLASS = ".class";
 
   public ClassLiteralGetter(ContextGetter baseGetter) {
     myBaseGetter = baseGetter;
@@ -26,27 +33,42 @@ public class ClassLiteralGetter implements ContextGetter {
           final PsiTypeParameter[] typeParameters = psiClass.getTypeParameters();
           if (typeParameters.length == 1) {
             PsiType substitution = resolveResult.getSubstitutor().substitute(typeParameters[0]);
+            boolean addInheritors = false;
             if (substitution instanceof PsiWildcardType) {
-              substitution = ((PsiWildcardType)substitution).getBound();
+              final PsiWildcardType wildcardType = (PsiWildcardType)substitution;
+              substitution = wildcardType.getBound();
+              addInheritors = wildcardType.isExtends();
             }
-            if (substitution instanceof PsiClassType && !((PsiClassType)substitution).hasParameters() && !(((PsiClassType) substitution).resolve() instanceof PsiTypeParameter)) {
-              final @NonNls String suffix = ".class";
-              try {
-                final PsiManager manager = psiClass.getManager();
-                PsiExpression expr =
-                  manager.getElementFactory().createExpressionFromText(substitution.getCanonicalText() + suffix, context);
-                expr = (PsiExpression)manager.getCodeStyleManager().shortenClassReferences(expr);
-                return new Object[]{LookupElementFactory.getInstance().createLookupElement(expr, expr.getText()).setPriority(1)};
-              }
-              catch (IncorrectOperationException e) {
-                LOG.error(e);
+
+            final List<LookupElement<PsiExpression>> result = new ArrayList<LookupElement<PsiExpression>>();
+            createLookupElement(substitution, context, 2, result);
+            if (addInheritors && substitution != null && !CommonClassNames.JAVA_LANG_OBJECT.equals(substitution.getCanonicalText())) {
+              for (final PsiType type : CodeInsightUtil.addSubtypes(substitution, context, true)) {
+                createLookupElement(type, context, 1, result);
               }
             }
+            return result.toArray(new Object[result.size()]);
+
           }
         }
       }
     }
 
     return ArrayUtil.EMPTY_OBJECT_ARRAY;
+  }
+
+  private static void createLookupElement(@Nullable final PsiType type, final PsiElement context, final int priority, final List<LookupElement<PsiExpression>> list) {
+    if (type instanceof PsiClassType && !((PsiClassType)type).hasParameters() && !(((PsiClassType) type).resolve() instanceof PsiTypeParameter)) {
+      try {
+        final PsiManager manager = context.getManager();
+        PsiExpression expr =
+          manager.getElementFactory().createExpressionFromText(type.getCanonicalText() + DOT_CLASS, context);
+        expr = (PsiExpression)manager.getCodeStyleManager().shortenClassReferences(expr);
+        list.add(LookupElementFactory.getInstance().createLookupElement(expr, expr.getText()).setPriority(priority));
+      }
+      catch (IncorrectOperationException e) {
+        LOG.error(e);
+      }
+    }
   }
 }
