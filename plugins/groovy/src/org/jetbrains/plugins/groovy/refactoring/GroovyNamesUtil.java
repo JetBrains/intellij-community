fccbbddb7523e14b4ae78d7a71e2f5ac450225c9
Eugene Vigdorchik <no_reply@jetbrains.com>
2007-09-28 15:33:22 +0400

    cleanup

--- plugins/groovy/src/org/jetbrains/plugins/groovy/refactoring/GroovyNamesUtil.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/refactoring/GroovyNamesUtil.java
@@ -17,42 +17,33 @@ package org.jetbrains.plugins.groovy.refactoring;
 
 import com.intellij.lexer.Lexer;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.psi.PsiType;
 import org.jetbrains.plugins.groovy.lang.lexer.GroovyLexer;
 import org.jetbrains.plugins.groovy.lang.lexer.GroovyTokenTypes;
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrExpression;
-
-import java.util.ArrayList;
 
 /**
  * @author ven
  */
 public class GroovyNamesUtil {
-  private static final Object LOCK = new Object();
 
   public static boolean isIdentifier(String text) {
     ApplicationManager.getApplication().assertReadAccessAllowed();
     if (text == null) return false;
 
-    synchronized (LOCK) {
-      Lexer lexer = new GroovyLexer();
-      lexer.start(text, 0, text.length(), 0);
-      if (lexer.getTokenType() != GroovyTokenTypes.mIDENT) return false;
-      lexer.advance();
-      return lexer.getTokenType() == null;
-    }
+    Lexer lexer = new GroovyLexer();
+    lexer.start(text, 0, text.length(), 0);
+    if (lexer.getTokenType() != GroovyTokenTypes.mIDENT) return false;
+    lexer.advance();
+    return lexer.getTokenType() == null;
   }
 
   public static boolean isKeyword(String text) {
     ApplicationManager.getApplication().assertReadAccessAllowed();
 
-    synchronized (LOCK) {
-      Lexer lexer = new GroovyLexer();
-      lexer.start(text,0,text.length(),0);
-      if (lexer.getTokenType() == null || !GroovyTokenTypes.KEYWORDS.contains(lexer.getTokenType())) return false;
-      lexer.advance();
-      return lexer.getTokenType() == null;
-    }
+    Lexer lexer = new GroovyLexer();
+    lexer.start(text,0,text.length(),0);
+    if (lexer.getTokenType() == null || !GroovyTokenTypes.KEYWORDS.contains(lexer.getTokenType())) return false;
+    lexer.advance();
+    return lexer.getTokenType() == null;
   }
 
 
