2b8da7fef7110e9fae80309f450938856500a813
Maxim Shafirov <max@jetbrains.com>
2007-10-01 18:12:28 +0400

    IDEADEV-14869: getParent().getChildren().length == 0

--- codeInsight/impl/com/intellij/codeInsight/editorActions/SelectWordHandler.java
+++ codeInsight/impl/com/intellij/codeInsight/editorActions/SelectWordHandler.java
@@ -93,7 +93,7 @@ public class SelectWordHandler extends EditorActionHandler {
         final PsiElement parent = element.getParent();
         final PsiElement[] children = parent.getChildren();
 
-        if (children[children.length - 1] == element) {
+        if (children.length > 0 && children[children.length - 1] == element) {
           element = parent;
         }
         else {
