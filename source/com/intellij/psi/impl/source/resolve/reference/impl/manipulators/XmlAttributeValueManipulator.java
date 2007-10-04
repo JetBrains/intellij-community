c2af9332e2c5a5218a3db7ae34263d435dab5593
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2007-10-04 17:23:28 +0400

    IDEADEV-22003 [reviewed by maxim m.]

--- source/com/intellij/psi/impl/source/resolve/reference/impl/manipulators/XmlAttributeValueManipulator.java
+++ source/com/intellij/psi/impl/source/resolve/reference/impl/manipulators/XmlAttributeValueManipulator.java
@@ -1,19 +1,15 @@
 package com.intellij.psi.impl.source.resolve.reference.impl.manipulators;
 
+import com.intellij.lang.ASTNode;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.TextRange;
-import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.AbstractElementManipulator;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.impl.CheckUtil;
-import com.intellij.util.CharTable;
-import com.intellij.psi.impl.source.SourceTreeToPsiMap;
-import com.intellij.psi.AbstractElementManipulator;
-import com.intellij.psi.impl.source.tree.CompositeElement;
-import com.intellij.psi.impl.source.tree.Factory;
-import com.intellij.psi.impl.source.tree.LeafElement;
-import com.intellij.psi.impl.source.tree.SharedImplUtil;
-import com.intellij.psi.xml.XmlTokenType;
+import com.intellij.psi.impl.source.tree.*;
 import com.intellij.psi.xml.XmlAttributeValue;
+import com.intellij.psi.xml.XmlTokenType;
+import com.intellij.util.CharTable;
 import com.intellij.util.IncorrectOperationException;
 
 /**
@@ -25,37 +21,34 @@ import com.intellij.util.IncorrectOperationException;
  */
 public class XmlAttributeValueManipulator extends AbstractElementManipulator<XmlAttributeValue> {
   private static final Logger LOG = Logger.getInstance("#com.intellij.psi.impl.source.resolve.reference.impl.manipulators.XmlAttributeValueManipulator");
+
   public XmlAttributeValue handleContentChange(XmlAttributeValue element, TextRange range, String newContent) throws IncorrectOperationException{
-    final CompositeElement compositeElement = (CompositeElement)element.getNode();
-    PsiElement elementToReplace = element.getFirstChild().getNextSibling();
 
     CheckUtil.checkWritable(element);
+    final CompositeElement attrNode = (CompositeElement)element.getNode();
+    final ASTNode valueNode = attrNode.findLeafElementAt(range.getStartOffset());
+    final PsiElement elementToReplace = valueNode.getPsi();
+
     String text;
     try {
-      text = element.getText();
-      int offset = StringUtil.startsWithChar(text,'"') || StringUtil.startsWithChar(text,'\'') ? 1:0;
-      if (offset == 0) {
-        elementToReplace = element.getFirstChild();
-      }
-      
-      String textBeforeRange = text.substring(offset, range.getStartOffset());
-      String textAfterRange = text.substring(range.getEndOffset(), text.length() - offset);
+      text = elementToReplace.getText();
+      final int offsetInParent = elementToReplace.getStartOffsetInParent();
+      String textBeforeRange = text.substring(0, range.getStartOffset() - offsetInParent);
+      String textAfterRange = text.substring(range.getEndOffset()- offsetInParent, text.length());
       text = textBeforeRange + newContent + textAfterRange;
     } catch(StringIndexOutOfBoundsException e) {
       LOG.error("Range: " + range + " in text: '" + element.getText() + "'", e);
       throw e;
     }
-    final CharTable charTableByTree = SharedImplUtil.findCharTableByTree(compositeElement);
+    final CharTable charTableByTree = SharedImplUtil.findCharTableByTree(attrNode);
     final LeafElement newValueElement = Factory.createSingleLeafElement(
       XmlTokenType.XML_ATTRIBUTE_VALUE_TOKEN,
       text,
       0,
       text.length(), charTableByTree, element.getManager());
 
-
-    compositeElement.replaceChildInternal(SourceTreeToPsiMap.psiElementToTree(elementToReplace), newValueElement);
+    attrNode.replaceChildInternal(valueNode, newValueElement);
     return element;
-    //return ((XmlAttributeValueImpl) element).replaceRangeInText(range, newContent);
   }
 
   public TextRange getRangeInElement(final XmlAttributeValue xmlAttributeValue) {
