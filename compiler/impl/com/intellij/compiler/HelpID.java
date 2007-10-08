02eb9413b24db260600c0c95df8f153a723cf8b2
Dmitry Jemerov <yole@intellij.com>
2007-10-08 13:29:00 +0400

    [no reviewer] change help ID (IDEADEV-22220)

--- compiler/impl/com/intellij/compiler/HelpID.java
+++ compiler/impl/com/intellij/compiler/HelpID.java
@@ -6,6 +6,6 @@ import org.jetbrains.annotations.NonNls;
  *
  */
 public interface HelpID {
-  @NonNls  String  COMPILER = "compiling";
+  @NonNls  String  COMPILER = "reference.toolWindows.messages";
   @NonNls  String  GENERATE_ANT_BUILD = "editing.generateAntBuild";
 }
