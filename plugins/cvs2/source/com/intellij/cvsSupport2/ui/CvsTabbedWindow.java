31779766405220b7237d6c7cf4ffc508f547e90a
Dmitry Jemerov <yole@intellij.com>
2007-10-09 14:00:12 +0400

    [anna] ensure that ErrorTreeView is disposed (IDEADEV-22267)

--- plugins/cvs2/source/com/intellij/cvsSupport2/ui/CvsTabbedWindow.java
+++ plugins/cvs2/source/com/intellij/cvsSupport2/ui/CvsTabbedWindow.java
@@ -46,6 +46,10 @@ public class CvsTabbedWindow {
           EditorFactory.getInstance().releaseEditor(myOutput);
           myOutput = null;
         }
+        if (myErrorsView != null) {
+          myErrorsView.dispose();
+          myErrorsView = null;
+        }
 
         LOG.assertTrue(!myIsDisposed);
         try {
