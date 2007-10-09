d2b443f9556f86908e762988030eb7a0e1af4c99
Alexander Chernikov <Alexander.Chernikov@jetbrains.com>
2007-10-09 20:06:27 +0400

    Inline field help mapping corrected.

--- refactoring/impl/com/intellij/refactoring/HelpID.java
+++ refactoring/impl/com/intellij/refactoring/HelpID.java
@@ -35,7 +35,7 @@ public class HelpID {
   public static final String INLINE_METHOD      = "refactoring.inlineMethod";
   public static final String INLINE_CONSTRUCTOR = "refactoring.inlineConstructor";
   public static final String INLINE_VARIABLE    = "refactoring.inlineVariable";
-  public static final String INLINE_FIELD       = "reference.refactoring.inline.field";
+  public static final String INLINE_FIELD       = "refactoring.inlineField";
   public static final String INLINE_INCLUDE     = "refactoring.inlineInclude";
 
   public static final String MIGRATION          = "refactoring.migrate";
