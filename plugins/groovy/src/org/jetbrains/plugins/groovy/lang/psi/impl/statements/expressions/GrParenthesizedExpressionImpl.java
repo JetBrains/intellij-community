67dd238e2e92e175b87b966315f1da755194bc67
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-10-10 18:47:52 +0400

    testdata changed

--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/impl/statements/expressions/GrParenthesizedExpressionImpl.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/impl/statements/expressions/GrParenthesizedExpressionImpl.java
@@ -1,16 +1,5 @@
 /*
- * Copyright 2000-2007 JetBrains s.r.o.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright (c) 2007, Your Corporation. All Rights Reserved.
  */
 
 package org.jetbrains.plugins.groovy.lang.psi.impl.statements.expressions;
@@ -18,9 +7,9 @@ package org.jetbrains.plugins.groovy.lang.psi.impl.statements.expressions;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiType;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrParenthesizedExpression;
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrExpression;
 import org.jetbrains.plugins.groovy.lang.psi.GroovyElementVisitor;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrExpression;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrParenthesizedExpression;
 
 /**
  * @author ilyas
