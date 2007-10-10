6a05c6efea2c3b72e5b6ddd50123ed8e73624e05
Dmitry Jemerov <yole@intellij.com>
2007-10-10 19:44:57 +0400

    javadoc

--- openapi/src/com/intellij/openapi/vcs/rollback/RollbackEnvironment.java
+++ openapi/src/com/intellij/openapi/vcs/rollback/RollbackEnvironment.java
@@ -16,8 +16,8 @@
 
 package com.intellij.openapi.vcs.rollback;
 
-import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vcs.FilePath;
+import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vcs.changes.Change;
 import com.intellij.openapi.vfs.VirtualFile;
 
@@ -30,9 +30,49 @@ import java.util.List;
  * @since 7.0
  */
 public interface RollbackEnvironment {
+  /**
+   * Returns the name of operation which is shown in the UI (in menu item name, dialog title and button text).
+   *
+   * @return the user-readable name of operation (for example, "Rollback" or "Revert").
+   */
   String getRollbackOperationName();
+
+  /**
+   * Rolls back the specified changes.
+   *
+   * @param changes the changes to roll back.
+   * @return list of errors occurred, or an empty list if no errors occurred.
+   */
   List<VcsException> rollbackChanges(List<Change> changes);
+
+  /**
+   * Rolls back the deletion of files which have been deleted locally but not scheduled for deletion
+   * from VCS. The implementation of this method should get the current version of the listed files from VCS.
+   * You do not need to implement this method if you never report such files to
+   * {@link com.intellij.openapi.vcs.changes.ChangelistBuilder#processLocallyDeletedFile}.
+   *
+   * @param files the files to rollback deletion of.
+   * @return list of errors occurred, or an empty list if no errors occurred.
+   */
   List<VcsException> rollbackMissingFileDeletion(List<FilePath> files);
+
+  /**
+   * Rolls back the modifications of files which have been made writable but not properly checked out from VCS.
+   * You do not need to implement this method if you never report such files to
+   * {@link com.intellij.openapi.vcs.changes.ChangelistBuilder#processModifiedWithoutCheckout}.
+   *
+   * @param files the files to rollback.
+   * @return list of errors occurred, or an empty list if no errors occurred.
+   */
   List<VcsException> rollbackModifiedWithoutCheckout(List<VirtualFile> files);
+
+  /**
+   * This is called when the user performs an undo that returns a file to a state in which it was
+   * checked out or last saved. The implementation of this method can compare the current state of file
+   * with the base revision and undo the checkout if the file is identical. Implementing this method is
+   * optional.
+   *
+   * @param file the file to rollback.
+   */
   void rollbackIfUnchanged(VirtualFile file);
 }
