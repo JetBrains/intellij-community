7e84793f7c3983f85a7e826431fdb998e3ae88f9
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2007-10-17 14:43:36 +0400

    test for setup field initialization

---
+++ refactoring/tests/com/intellij/refactoring/IntroduceFieldWitSetUpInitializationTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2000-2007 JetBrains s.r.o. All Rights Reserved.
+ */
+
+/*
+ * User: anna
+ * Date: 17-Oct-2007
+ */
+package com.intellij.refactoring;
+
+import com.intellij.codeInsight.CodeInsightTestCase;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.roots.ModifiableRootModel;
+import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.roots.OrderRootType;
+import com.intellij.openapi.roots.libraries.Library;
+import com.intellij.openapi.roots.libraries.LibraryTable;
+import com.intellij.openapi.vfs.VfsUtil;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiExpression;
+import com.intellij.psi.PsiLocalVariable;
+import com.intellij.psi.PsiModifier;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.refactoring.introduceField.BaseExpressionToFieldHandler;
+import com.intellij.refactoring.introduceField.LocalToFieldHandler;
+import com.intellij.util.PathUtil;
+import junit.framework.TestCase;
+
+import java.io.File;
+
+public class IntroduceFieldWitSetUpInitializationTest extends CodeInsightTestCase {
+  protected Module createModule(final String path) {
+    final Module module = super.createModule(path);
+    ApplicationManager.getApplication().runWriteAction(new Runnable() {
+      public void run() {
+        final ModifiableRootModel model = ModuleRootManager.getInstance(module).getModifiableModel();
+        final LibraryTable.ModifiableModel modifiableModel = model.getModuleLibraryTable().getModifiableModel();
+        final Library library = modifiableModel.createLibrary("junit");
+        final Library.ModifiableModel libModel = library.getModifiableModel();
+        libModel.addRoot(VfsUtil.getUrlForLibraryRoot(new File(PathUtil.getJarPathForClass(TestCase.class))), OrderRootType.CLASSES);
+        libModel.commit();
+        model.commit();
+      }
+    });
+    return module;
+  }
+
+  protected boolean clearModelBeforeConfiguring() {
+    return false;
+  }
+
+  public void testInSetUp() throws Exception {
+    configureByFile("/refactoring/introduceField/before2.java");
+    final PsiLocalVariable local =
+      PsiTreeUtil.getParentOfType(getFile().findElementAt(getEditor().getCaretModel().getOffset()), PsiLocalVariable.class);
+    new LocalToFieldHandler(getProject(), false) {
+      protected BaseExpressionToFieldHandler.Settings showRefactoringDialog(final PsiClass aClass,
+                                                                            final PsiLocalVariable local,
+                                                                            final PsiExpression[] occurences,
+                                                                            final boolean isStatic) {
+        return new BaseExpressionToFieldHandler.Settings("i", true, false, false,
+                                                         BaseExpressionToFieldHandler.InitializationPlace.IN_SETUP_METHOD,
+                                                         PsiModifier.PRIVATE, local, local.getType(), true, null, false);
+      }
+    }.convertLocalToField(local, myEditor);
+    checkResultByFile("/refactoring/introduceField/after2.java");
+  }
+}
---
+++ testData/refactoring/introduceField/after2.java
@@ -0,0 +1,12 @@
+import junit.framework.TestCase;
+public class T extends TestCase {
+    private int i;
+
+    public void test() {
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        i = 9;
+    }
+}
