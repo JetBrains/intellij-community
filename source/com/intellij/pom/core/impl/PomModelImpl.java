8707c0fb29e30331293ebdfc943b17c3248ea08d
Maxim Shafirov <max@jetbrains.com>
2007-10-17 21:26:19 +0400

    [r: cdr] deadlock fix.

--- source/com/intellij/pom/core/impl/PomModelImpl.java
+++ source/com/intellij/pom/core/impl/PomModelImpl.java
@@ -144,7 +144,7 @@ public class PomModelImpl extends UserDataHolderBase implements PomModel {
 
   private final Stack<Pair<PomModelAspect, PomTransaction>> myBlockedAspects = new Stack<Pair<PomModelAspect, PomTransaction>>();
 
-  public synchronized void runTransaction(PomTransaction transaction) throws IncorrectOperationException{
+  public void runTransaction(PomTransaction transaction) throws IncorrectOperationException{
     List<Throwable> throwables = new ArrayList<Throwable>(0);
     synchronized(PsiLock.LOCK){
       final PomModelAspect aspect = transaction.getTransactionAspect();
