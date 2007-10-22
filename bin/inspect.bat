855995c71311227b5f03d1241c499fda420892fc
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2007-10-22 19:02:16 +0400

    J2EE isn't an inspection plugin -> dependant plugins are not loaded -> are disabled on next startup (IDEADEV-22187)

--- bin/inspect.bat
+++ bin/inspect.bat
@@ -1,3 +1,2 @@
 @echo off
-set REQUIRED_IDEA_JVM_ARGS=-Didea.load.plugins.category=inspection
 call idea.bat inspect %*
--- bin/inspect.sh
+++ bin/inspect.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-REQUIRED_JVM_ARGS=-Didea.load.plugins.category=inspection
 ./idea.sh inspect $*
