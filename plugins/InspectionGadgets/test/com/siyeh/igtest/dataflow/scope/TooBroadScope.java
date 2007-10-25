763beace5e5ebf6b61e79b274af70099ca4be6e1
Maxim Shafirov <max@jetbrains.com>
2007-10-25 16:58:53 +0400

    Fix test

--- plugins/InspectionGadgets/test/com/siyeh/igtest/dataflow/scope/TooBroadScope.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/dataflow/scope/TooBroadScope.java
@@ -1,8 +1,7 @@
 package com.siyeh.igtest.dataflow.scope;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.Collection; import java.util.List;
 
 public class TooBroadScope
 {
