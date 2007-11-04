0a7c9ad19d356f64dcfd1aee8717d0b5e200bb36
Bas Leijdekkers <basleijdekkers@gmail.com>
2007-11-04 11:05:38 +0300

    cleanup

--- plugins/InspectionGadgets/testsrc/com/siyeh/ig/dataflow/TooBroadScopeInspectionTest.java
+++ plugins/InspectionGadgets/testsrc/com/siyeh/ig/dataflow/TooBroadScopeInspectionTest.java
@@ -1,15 +1,7 @@
-/**
- * (c) 2006 Carp Technologies BV
- * Brouwerijstraat 1, 7523XC Enschede
- * Created: Sep 3, 2007
- */
 package com.siyeh.ig.dataflow;
 
 import com.IGInspectionTestCase;
 
-/**
- * @author <A href="bas@carp-technologies.nl">Bas Leijdekkers</a>
- */
 public class TooBroadScopeInspectionTest extends IGInspectionTestCase {
 
     public void test() throws Exception {
