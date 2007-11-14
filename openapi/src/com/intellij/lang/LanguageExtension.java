208275886d27e20e7dbc5886beded9f1ee053fa4
Maxim Shafirov <max@jetbrains.com>
2007-11-14 22:36:48 +0300

    You can't trust version control integration either.

--- openapi/src/com/intellij/lang/LanguageExtension.java
+++ openapi/src/com/intellij/lang/LanguageExtension.java
@@ -1,24 +1,40 @@
 /*
- * Copyright 2000-2007 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * @author max
  */
-
 package com.intellij.lang;
 
-import com.intellij.psi.PsiFile;
+import com.intellij.openapi.util.KeyedExtensionCollector;
+import org.jetbrains.annotations.NonNls;
 
-public interface LanguageExtension {
-  boolean isRelevantForFile(final PsiFile psi);
-  Language getLanguage();
+import java.util.List;
+
+public class LanguageExtension<T> extends KeyedExtensionCollector<T, Language> {
+  private final T myDefaultImplementation;
+
+  public LanguageExtension(@NonNls final String epName) {
+    this(epName, null);
+  }
+
+  public LanguageExtension(@NonNls final String epName, T defaultImplementation) {
+    super(epName);
+    myDefaultImplementation = defaultImplementation;
+  }
+
+  protected String keyToString(final Language key) {
+    return key.getID();
+  }
+
+  public T forLanguage(Language l) {
+    final List<T> extensions = forKey(l);
+    if (extensions.isEmpty()) {
+      return myDefaultImplementation;
+    }
+    else {
+      return extensions.get(0);
+    }
+  }
+
+  public List<T> allForLanguage(Language l) {
+    return forKey(l);
+  }
 }
