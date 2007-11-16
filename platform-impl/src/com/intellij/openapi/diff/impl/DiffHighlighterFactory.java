77adaefbbfbe90c7dacfca785076824a25de1026
Dmitry Jemerov <yole@intellij.com>
2007-11-16 16:53:49 +0300

    typo fix

--- platform-impl/src/com/intellij/openapi/diff/impl/DiffHighlighterFactory.java
+++ platform-impl/src/com/intellij/openapi/diff/impl/DiffHighlighterFactory.java
@@ -2,6 +2,6 @@ package com.intellij.openapi.diff.impl;
 
 import com.intellij.openapi.editor.highlighter.EditorHighlighter;
 
-public interface DiffHighliterFactory {
+public interface DiffHighlighterFactory {
   EditorHighlighter createHighlighter();
 }
