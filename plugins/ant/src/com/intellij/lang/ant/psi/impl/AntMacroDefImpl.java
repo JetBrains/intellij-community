ee724cd7d36238292c248968429290e88275f729
Eugene Zhuravlev <jeka@jetbrains.com>
2007-11-16 21:15:55 +0300

    fix macrodef tests

--- plugins/ant/src/com/intellij/lang/ant/psi/impl/AntMacroDefImpl.java
+++ plugins/ant/src/com/intellij/lang/ant/psi/impl/AntMacroDefImpl.java
@@ -7,6 +7,7 @@ import com.intellij.lang.ant.psi.introspection.AntTypeDefinition;
 import com.intellij.lang.ant.psi.introspection.AntTypeId;
 import com.intellij.lang.ant.psi.introspection.impl.AntTypeDefinitionImpl;
 import com.intellij.psi.PsiLock;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.xml.XmlTag;
 import com.intellij.util.StringBuilderSpinAllocator;
 import org.jetbrains.annotations.NonNls;
@@ -135,9 +136,17 @@ public class AntMacroDefImpl extends AntTaskImpl implements AntMacroDef {
       myMacroDefinition.setIsTask(true);
       myMacroDefinition.setDefiningElement(this);
     }
-    final AntStructuredElement antProject = getAntProject();
-    if (antProject != null) {
-      antProject.registerCustomType(myMacroDefinition);
+    final AntStructuredElement parent = getAntProject();
+    if (parent != null) {
+      parent.registerCustomType(myMacroDefinition);
+    }
+    // define itself as nested task for sequential
+    final AntAllTasksContainerImpl sequential = PsiTreeUtil.getChildOfType(this, AntAllTasksContainerImpl.class);
+    if (sequential != null) {
+      sequential.registerCustomType(myMacroDefinition);
+      for (final AntTypeId id : myMacroDefinition.getNestedElements()) {
+        sequential.registerCustomType(file.getBaseTypeDefinition(myMacroDefinition.getNestedClassName(id)));
+      }
     }
   }
 }
