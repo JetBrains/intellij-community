c2b06d44e439e17022da23dc2a231f99ef7ade37
Eugene Zhuravlev <jeka@jetbrains.com>
2007-11-16 18:28:39 +0300

    revert local change

--- java/compiler/tests/com/intellij/compiler/CommonTest.java
+++ java/compiler/tests/com/intellij/compiler/CommonTest.java
@@ -25,6 +25,4 @@ public class CommonTest extends CompilerTestCase {
   public void testChangeDefinitionToClass() throws Exception  {doTest();}
 
   public void testChangeDefinitionToClass2() throws Exception  {doTest();}
-  
-  public void testChangeDefinitionToInterface() throws Exception  {doTest();}
 }
