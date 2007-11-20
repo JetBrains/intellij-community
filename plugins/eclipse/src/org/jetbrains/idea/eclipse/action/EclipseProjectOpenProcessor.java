8989e96e94d8f064a2ea83e1b4318f5cd47a79c0
Maxim Shafirov <max@jetbrains.com>
2007-11-20 23:26:04 +0300

    Memory leak fix

--- plugins/eclipse/src/org/jetbrains/idea/eclipse/action/EclipseProjectOpenProcessor.java
+++ plugins/eclipse/src/org/jetbrains/idea/eclipse/action/EclipseProjectOpenProcessor.java
@@ -6,14 +6,14 @@ package org.jetbrains.idea.eclipse.action;
 
 import com.intellij.ide.util.projectWizard.WizardContext;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.projectImport.ProjectOpenProcessor;
+import com.intellij.projectImport.ProjectOpenProcessorBase;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.idea.eclipse.EclipseProjectModel;
 import org.jetbrains.idea.eclipse.EclipseXml;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-public class EclipseProjectOpenProcessor extends ProjectOpenProcessor {
+public class EclipseProjectOpenProcessor extends ProjectOpenProcessorBase {
   public EclipseProjectOpenProcessor(final EclipseImportBuilder builder) {
     super(builder);
   }
--- plugins/maven/src/main/java/org/jetbrains/idea/maven/project/action/MavenProjectOpenProcessor.java
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/project/action/MavenProjectOpenProcessor.java
@@ -7,16 +7,16 @@ package org.jetbrains.idea.maven.project.action;
 import com.intellij.ide.util.projectWizard.WizardContext;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.projectImport.ProjectOpenProcessor;
+import com.intellij.projectImport.ProjectOpenProcessorBase;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.idea.maven.core.util.MavenEnv;
 import org.jetbrains.idea.maven.project.MavenProjectModel;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MavenProjectOpenProcessor extends ProjectOpenProcessor {
+public class MavenProjectOpenProcessor extends ProjectOpenProcessorBase {
   public MavenProjectOpenProcessor(final MavenImportBuilder builder) {
     super(builder);
   }
