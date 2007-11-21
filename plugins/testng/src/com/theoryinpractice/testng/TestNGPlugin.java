30749b4714d3679ef1a16a02de7b3160757fd3c5
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2007-11-21 16:27:51 +0300

    by default register testng-1.0.dtd (IDEADEV-23399)

--- plugins/testng/src/com/theoryinpractice/testng/TestNGPlugin.java
+++ plugins/testng/src/com/theoryinpractice/testng/TestNGPlugin.java
@@ -4,15 +4,17 @@
  */
 package com.theoryinpractice.testng;
 
-import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.codeInsight.intention.IntentionManager;
+import com.intellij.javaee.ExternalResourceManager;
+import com.intellij.openapi.components.ApplicationComponent;
 import com.theoryinpractice.testng.intention.TestNGOrderEntryFix;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
 public class TestNGPlugin implements ApplicationComponent {
-  public TestNGPlugin(final IntentionManager manager) {
+  public TestNGPlugin(final IntentionManager manager, ExternalResourceManager externalResourceManager) {
     manager.addAction(new TestNGOrderEntryFix());
+    externalResourceManager.addStdResource("http://testng.org/testng-1.0.dtd", "/resources/standardSchemas/testng-1.0.dtd", getClass());
   }
 
   @NonNls
