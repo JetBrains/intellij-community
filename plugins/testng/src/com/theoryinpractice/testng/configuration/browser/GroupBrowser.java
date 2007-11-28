0099d7a888d84bd09eaf078e35333883b71db088
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2007-11-28 14:49:59 +0300

    NPE

--- plugins/testng/src/com/theoryinpractice/testng/configuration/browser/GroupBrowser.java
+++ plugins/testng/src/com/theoryinpractice/testng/configuration/browser/GroupBrowser.java
@@ -6,9 +6,10 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.search.GlobalSearchScope;
+import com.theoryinpractice.testng.configuration.TestNGConfigurationEditor;
 import com.theoryinpractice.testng.model.TestClassFilter;
 import com.theoryinpractice.testng.util.TestNGUtil;
-import com.theoryinpractice.testng.configuration.TestNGConfigurationEditor;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * @author Hani Suleiman
@@ -24,17 +25,24 @@ public class GroupBrowser extends BrowseModuleValueActionListener
         this.editor = editor;
     }
 
+    @Nullable
     @Override
     protected String showDialog() {
-        Module module = editor.getModuleSelector().getModule();        
-        TestClassFilter filter = new TestClassFilter(GlobalSearchScope.moduleScope(module), module.getProject(), false);
-        PsiClass[] classes = TestNGUtil.getAllTestClasses(filter);
-        if(classes.length == 0) {
-            Messages.showMessageDialog(getField(), "No tests found in project", "Cannot Browse Groups", Messages.getInformationIcon());
-            return null;
-        } else {
-            return GroupList.showDialog(classes, getField());
-        }
+      TestClassFilter filter;
+      Module module = editor.getModuleSelector().getModule();
+      if (module == null) {
+        filter = new TestClassFilter(GlobalSearchScope.projectScope(getProject()), getProject(), false);
+      }
+      else {
+        filter = new TestClassFilter(GlobalSearchScope.moduleScope(module), getProject(), false);
+      }
+      PsiClass[] classes = TestNGUtil.getAllTestClasses(filter);
+      if(classes.length == 0) {
+        Messages.showMessageDialog(getField(), "No tests found in project", "Cannot Browse Groups", Messages.getInformationIcon());
+        return null;
+      } else {
+        return GroupList.showDialog(classes, getField());
+      }
     }
 
 }
