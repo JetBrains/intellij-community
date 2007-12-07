78869042e181801517187674404e71d3429d515f
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2007-12-07 20:57:11 +0300

    GRVY-454

---
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/intentions/control/ConditionalToElvisIntention.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2000-2007 JetBrains s.r.o.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jetbrains.plugins.groovy.intentions.control;
+
+import com.intellij.psi.PsiElement;
+import com.intellij.util.IncorrectOperationException;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.intentions.base.Intention;
+import org.jetbrains.plugins.groovy.intentions.base.IntentionUtils;
+import org.jetbrains.plugins.groovy.intentions.base.PsiElementPredicate;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrConditionalExpression;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrExpression;
+
+/**
+ * @author ilyas
+ */
+public class ConditionalToElvisIntention extends Intention {
+  protected void processIntention(@NotNull PsiElement element) throws IncorrectOperationException {
+    final GrConditionalExpression expr =
+        (GrConditionalExpression) element;
+
+    final GrExpression thenExpression = expr.getThenBranch();
+    final GrExpression elseExpression = expr.getElseBranch();
+    assert elseExpression != null;
+    assert thenExpression != null;
+
+    final String newExpression = thenExpression.getText() + " ?: " + elseExpression.getText();
+    IntentionUtils.replaceExpression(newExpression, expr);
+
+  }
+
+  @NotNull
+  protected PsiElementPredicate getElementPredicate() {
+    return new ConditionalToElvisPredicate();
+  }
+}
