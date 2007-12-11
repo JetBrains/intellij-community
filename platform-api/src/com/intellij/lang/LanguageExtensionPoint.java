ba5b6563cb29e95aa1a1d3adf9b662eb1740bdeb
Maxim Shafirov <max@jetbrains.com>
2007-12-11 21:28:26 +0300

    assertion added.

--- platform-api/src/com/intellij/lang/LanguageExtensionPoint.java
+++ platform-api/src/com/intellij/lang/LanguageExtensionPoint.java
@@ -19,6 +19,9 @@ public class LanguageExtensionPoint<T> extends AbstractExtensionPointBean implem
 
   private final LazyInstance<T> myHandler = new LazyInstance<T>() {
     protected Class<T> getInstanceClass() throws ClassNotFoundException {
+      if (implementationClass == null) {
+        throw new RuntimeException("implementation class is not specified for unknown language extension point, language: " + language);
+      }
       return findClass(implementationClass);
     }
   };
