236af0903b763ea200a6ec063140dda42e94bd65
Maxim Shafirov <max@jetbrains.com>
2007-12-11 11:39:14 +0300

    Lots of 'already disposed' in tests log.

--- source/com/intellij/openapi/vcs/changes/VcsEventWatcher.java
+++ source/com/intellij/openapi/vcs/changes/VcsEventWatcher.java
@@ -1,7 +1,6 @@
 package com.intellij.openapi.vcs.changes;
 
 import com.intellij.ProjectTopics;
-import com.intellij.problems.WolfTheProblemSolver;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.components.ProjectComponent;
@@ -9,6 +8,7 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ModuleRootEvent;
 import com.intellij.openapi.roots.ModuleRootListener;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.problems.WolfTheProblemSolver;
 import com.intellij.util.messages.MessageBusConnection;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
@@ -34,6 +34,7 @@ public class VcsEventWatcher implements ProjectComponent {
       public void rootsChanged(ModuleRootEvent event) {
         ApplicationManager.getApplication().invokeLater(new Runnable() {
           public void run() {
+            if (myProject.isDisposed()) return;
             VcsDirtyScopeManager.getInstance(myProject).markEverythingDirty();
           }
         }, ModalityState.NON_MODAL);
