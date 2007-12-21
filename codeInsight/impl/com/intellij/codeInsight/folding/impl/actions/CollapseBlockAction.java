b3e10d8184bb1d0050b102feea52063aba8e3d0d
Maxim Shafirov <max@jetbrains.com>
2007-12-21 21:11:29 +0300

    wrong module

--- codeInsight/impl/com/intellij/codeInsight/folding/impl/actions/CollapseBlockAction.java
+++ codeInsight/impl/com/intellij/codeInsight/folding/impl/actions/CollapseBlockAction.java
@@ -1,7 +1,7 @@
 package com.intellij.codeInsight.folding.impl.actions;
 
-import com.intellij.codeInsight.actions.BaseCodeInsightAction;
 import com.intellij.codeInsight.CodeInsightActionHandler;
+import com.intellij.codeInsight.actions.BaseCodeInsightAction;
 import com.intellij.codeInsight.folding.impl.CollapseBlockHandler;
 
 /**
