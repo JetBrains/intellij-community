ecdba2a08cd32364175f2b6c9770b8675772b9c3
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2007-12-25 15:06:07 +0300

    xdebugger copyrights

--- xdebugger/openapi/com/intellij/xdebugger/XDebugSession.java
+++ xdebugger/openapi/com/intellij/xdebugger/XDebugSession.java
@@ -16,19 +16,13 @@
 
 package com.intellij.xdebugger;
 
-import com.intellij.openapi.project.Project;
-import com.intellij.xdebugger.breakpoints.XBreakpointManager;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * @author nik
  */
-public abstract class XDebuggerManager {
+public interface XDebugSession {
 
-  public static XDebuggerManager getInstance(@NotNull Project project) {
-    return project.getComponent(XDebuggerManager.class);
-  }
+  @NotNull XDebugProcess getDebugProcess();
 
-  @NotNull
-  public abstract XBreakpointManager getBreakpointManager();
 }
--- xdebugger/openapi/com/intellij/xdebugger/breakpoints/XBreakpointHandler.java
+++ xdebugger/openapi/com/intellij/xdebugger/breakpoints/XBreakpointHandler.java
@@ -16,22 +16,25 @@
 
 package com.intellij.xdebugger.breakpoints;
 
-import com.intellij.openapi.vfs.VirtualFile;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * @author nik
  */
-public abstract class XLineBreakpointType<P extends XBreakpointProperties> extends XBreakpointType<XLineBreakpoint<P>,P> {
-  protected XLineBreakpointType(@NonNls @NotNull final String id, @Nls @NotNull final String title) {
-    super(id, title);
+public abstract class XBreakpointHandler<B extends XBreakpoint<?>> {
+  public static final XBreakpointHandler<?>[] EMPTY_ARRAY = new XBreakpointHandler<?>[0];
+  private final Class<? extends XBreakpointType<B, ?>> myBreakpointTypeClass;
+
+  protected XBreakpointHandler(@NotNull Class<? extends XBreakpointType<B, ?>> breakpointTypeClass) {
+    myBreakpointTypeClass = breakpointTypeClass;
   }
 
-  public abstract boolean canPutAt(@NotNull VirtualFile file, int line);
+  public final Class<? extends XBreakpointType<B, ?>> getBreakpointTypeClass() {
+    return myBreakpointTypeClass;
+  }
 
-  public abstract P createBreakpointProperties(@NotNull VirtualFile file, int line);
+  public abstract void registerBreakpoint(@NotNull B breakpoint);
 
-  public abstract String getDisplayText(final XLineBreakpoint<P> breakpoint);
+  public abstract void unregisterBreakpoint(@NotNull B breakpoint);
+
 }
