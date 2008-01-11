399f523584645a4c97ea5c2b0f5af8d830aef8a1
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-01-11 16:57:54 +0300

    groovy-shell expression evaluator

--- plugins/groovy/src/org/jetbrains/plugins/groovy/debugger/GroovyCodeFragmentFactory.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/debugger/GroovyCodeFragmentFactory.java
@@ -15,21 +15,25 @@
 package org.jetbrains.plugins.groovy.debugger;
 
 import com.intellij.debugger.engine.evaluation.CodeFragmentFactory;
-import com.intellij.debugger.engine.evaluation.TextWithImports;
 import com.intellij.debugger.engine.evaluation.CodeFragmentKind;
-import com.intellij.psi.PsiCodeFragment;
-import com.intellij.psi.PsiElement;
+import com.intellij.debugger.engine.evaluation.TextWithImports;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiCodeFragment;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementFactory;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.containers.HashSet;
 import org.jetbrains.plugins.groovy.GroovyFileType;
 import org.jetbrains.plugins.groovy.lang.psi.GroovyElementFactory;
-import org.jetbrains.plugins.groovy.lang.psi.GroovyElementVisitor;
+import org.jetbrains.plugins.groovy.lang.psi.GroovyFile;
 import org.jetbrains.plugins.groovy.lang.psi.GroovyPsiElement;
 import org.jetbrains.plugins.groovy.lang.psi.GroovyRecursiveElementVisitor;
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrReferenceExpression;
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrVariable;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrField;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrVariable;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrReferenceExpression;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.GrTypeDefinition;
 
 import java.util.Set;
 
@@ -57,15 +61,32 @@ public class GroovyCodeFragmentFactory implements CodeFragmentFactory {
     });
 
     StringBuffer javaText = new StringBuffer();
-    javaText.append("java.util.Map m = new java.util.HashMap();\n");
+    javaText.append("java.util.Map m = org.codehaus.groovy.runtime.DefaultGroovyMethods.getProperties(this);\n");
     for (String local : locals) {
       javaText.append("m.put(\"").append(local).append("\", ").append(local).append(");\n");
     }
     javaText.append("groovy.lang.Binding b = new groovy.lang.Binding(m);\n");
     String finalEvalText = imports + "\n" + text;
-    javaText.append("new groovy.lang.GroovyShell(b).evaluate(\"").append(StringUtil.escapeStringCharacters(finalEvalText)).append("\");");
+    PsiClass contextClass = getContextClass(context);
+    assert contextClass != null;
+    String qName = contextClass.getQualifiedName();
+    javaText.append("org.codehaus.groovy.control.CompilerConfiguration c = new org.codehaus.groovy.control.CompilerConfiguration();\n");
+    //javaText.append("c.setScriptBaseClass(\"").append(qName).append("\");\n");
+    javaText.append("groovy.lang.Script s = new groovy.lang.GroovyShell(b, c).parse(\"").
+        append(StringUtil.escapeStringCharacters(finalEvalText)).append("\");\n");
+    javaText.append("s.run();");
 
-    return toEval.getManager().getElementFactory().createCodeBlockCodeFragment(javaText.toString(), null, true);
+    PsiElementFactory elementFactory = toEval.getManager().getElementFactory();
+    PsiCodeFragment result = elementFactory.createCodeBlockCodeFragment(javaText.toString(), null, true);
+    result.setThisType(elementFactory.createType(contextClass));
+    return result;
+  }
+
+  private PsiClass getContextClass(PsiElement context) {
+    GroovyPsiElement parent = PsiTreeUtil.getParentOfType(context, GrTypeDefinition.class, GroovyFile.class);
+    if (parent instanceof GrTypeDefinition) return (PsiClass) parent;
+    else if (parent instanceof GroovyFile) return ((GroovyFile) parent).getScriptClass();
+    return null;
   }
 
   public boolean isContextAccepted(PsiElement context) {
