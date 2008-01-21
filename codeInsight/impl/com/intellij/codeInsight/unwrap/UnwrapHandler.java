6de2dec4233e5d65d88bcc1ece46aba31e63a15e
Anton Makeev <Anton.Makeev@jetbrains.com>
2008-01-21 17:07:57 +0300

    CodeInsight: unwrap if/try

--- codeInsight/impl/com/intellij/codeInsight/unwrap/UnwrapHandler.java
+++ codeInsight/impl/com/intellij/codeInsight/unwrap/UnwrapHandler.java
@@ -7,16 +7,17 @@ import com.intellij.psi.*;
 import com.intellij.util.IncorrectOperationException;
 
 public class UnwrapHandler implements CodeInsightActionHandler {
+  private Project myProject;
+
   public boolean startInWriteAction() {
     return true;
   }
 
   public void invoke(Project project, Editor editor, PsiFile file) {
-    int offset = editor.getCaretModel().getOffset();
-    PsiElement el = file.findElementAt(offset);
-
+    myProject = project;
     try {
-      while(el != null && !dispatch(el)) {
+      PsiElement el = getSelectedElement(editor, file);
+      while (el != null && !dispatch(el)) {
         el = el.getParent();
       }
     }
@@ -25,35 +26,113 @@ public class UnwrapHandler implements CodeInsightActionHandler {
     }
   }
 
-  private boolean dispatch(PsiElement el) throws IncorrectOperationException {
-    if (el instanceof PsiIfStatement) {
-      uwrapIfStatement((PsiIfStatement)el);
-      return true;
-    }
-
-    return false;
+  private PsiElement getSelectedElement(Editor editor, PsiFile file) {
+    int offset = editor.getCaretModel().getOffset();
+    return file.findElementAt(offset);
   }
 
-  private void uwrapIfStatement(PsiIfStatement el) throws IncorrectOperationException {
-    extractStatements(el.getThenBranch(), el);
-    extractStatements(el.getElseBranch(), el);
+  private boolean dispatch(PsiElement el) throws IncorrectOperationException {
+    if (isElseBlock(el)) {
+      unwrapElse((PsiStatement)el);
+    }
+    else if (isElseKeyword(el)) {
+      PsiStatement elseBranch = ((PsiIfStatement)el.getParent()).getElseBranch();
+      if (elseBranch != null) unwrapElse(elseBranch);
+    }
+    else if (el instanceof PsiIfStatement) {
+      uwrapIf((PsiIfStatement)el);
+    }
+    else if (el instanceof PsiTryStatement) {
+      unwrapTry((PsiTryStatement)el);
+    }
+    else if (el instanceof PsiCatchSection) {
+      unwrapCatch((PsiCatchSection)el);
+    }
+    else {
+      return false;
+    }
+    return true;
+  }
+
+  private boolean isElseBlock(PsiElement el) {
+    PsiElement p = el.getParent();
+    return p instanceof PsiIfStatement && el == ((PsiIfStatement)p).getElseBranch();
+  }
+
+  private boolean isElseKeyword(PsiElement el) {
+    PsiElement p = el.getParent();
+    return p instanceof PsiIfStatement && el == ((PsiIfStatement)p).getElseElement();
+  }
+
+  private void uwrapIf(PsiIfStatement el) throws IncorrectOperationException {
+    PsiStatement then = el.getThenBranch();
+
+    if (then instanceof PsiBlockStatement) {
+      extractFromCodeBlock(((PsiBlockStatement)then).getCodeBlock(), el);
+    }
+    else if (then != null && !(then instanceof PsiEmptyStatement)) {
+      extract(new PsiElement[]{then}, el);
+    }
 
     el.delete();
   }
 
-  private void extractStatements(PsiStatement from, PsiStatement to) throws IncorrectOperationException {
-    if (from == null) return;
+  private void unwrapElse(PsiStatement el) throws IncorrectOperationException {
+    if (el instanceof PsiIfStatement) {
+      deleteSelectedElseIf((PsiIfStatement)el);
+    }
+    else {
+      el.delete();
+    }
+  }
 
-    PsiStatement[] statements = PsiStatement.EMPTY_ARRAY;
+  private void deleteSelectedElseIf(PsiIfStatement selectedBranch) throws IncorrectOperationException {
+    PsiIfStatement parentIf = (PsiIfStatement)selectedBranch.getParent();
+    PsiStatement childElse = selectedBranch.getElseBranch();
 
-    if (from instanceof PsiBlockStatement) {
-      statements = ((PsiBlockStatement)from).getCodeBlock().getStatements();
-    } else if (!(from instanceof PsiEmptyStatement)) {
-      statements = new PsiStatement[] { from };
+    if (childElse == null) {
+      selectedBranch.delete();
+      return;
     }
 
-    if (statements.length == 0) return;
+    parentIf.setElseBranch(copyElement(childElse));
+  }
 
-    to.getParent().addRangeBefore(statements[0], statements[statements.length - 1], to);
+  private PsiStatement copyElement(PsiStatement el) throws IncorrectOperationException {
+    // we can not call el.copy() for 'else' since it sets context to parent 'if'. This cause copy to be invalidated
+    // after parent 'if' removal in setElseBranch method.
+
+    PsiManager manager = PsiManager.getInstance(myProject);
+    PsiElementFactory factory = JavaPsiFacade.getInstance(manager.getProject()).getElementFactory();
+    return factory.createStatementFromText(el.getText(), null);
+  }
+
+  private void unwrapTry(PsiTryStatement el) throws IncorrectOperationException {
+    extractFromCodeBlock(el.getTryBlock(), el);
+    extractFromCodeBlock(el.getFinallyBlock(), el);
+
+    el.delete();
+  }
+
+  private void unwrapCatch(PsiCatchSection el) throws IncorrectOperationException {
+    PsiTryStatement tryEl = (PsiTryStatement)el.getParent();
+    if (tryEl.getCatchBlocks().length > 1) {
+      el.delete();
+    } else {
+      unwrapTry(tryEl);
+    }
+  }
+
+  private void extractFromCodeBlock(PsiCodeBlock block, PsiElement from) throws IncorrectOperationException {
+    if (block == null) return;
+    extract(block.getStatements(), from);
+  }
+
+  private void extract(PsiElement[] elements, PsiElement from) throws IncorrectOperationException {
+    if (elements.length == 0) return;
+
+    PsiElement first = elements[0];
+    PsiElement last = elements[elements.length - 1];
+    from.getParent().addRangeBefore(first, last, from);
   }
 }
