d43e4eea5588951e049f070aa7aa8c9a6d63c3ff
Peter Gromov <peter@jetbrains.com>
2008-01-21 20:46:08 +0300

    tests rewrite

--- lang-api/src/com/intellij/lang/LanguageAnnotators.java
+++ lang-api/src/com/intellij/lang/LanguageAnnotators.java
@@ -4,11 +4,13 @@
 package com.intellij.lang;
 
 import com.intellij.lang.annotation.Annotator;
+import org.jetbrains.annotations.NonNls;
 
 public class LanguageAnnotators extends LanguageExtension<Annotator> {
   public static final LanguageAnnotators INSTANCE = new LanguageAnnotators();
+  @NonNls public static final String EP_NAME = "com.intellij.annotator";
 
   private LanguageAnnotators() {
-    super("com.intellij.annotator");
+    super(EP_NAME);
   }
 }
