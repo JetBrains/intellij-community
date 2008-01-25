4185b5fe1b12bbb15c787c938be466992b9411cb
Maxim Shafirov <max@jetbrains.com>
2008-01-25 15:10:33 +0300

    Fix EnterActionTest

--- xml/impl/src/com/intellij/lang/xml/XmlFormattingModelBuilder.java
+++ xml/impl/src/com/intellij/lang/xml/XmlFormattingModelBuilder.java
@@ -8,8 +8,10 @@ import com.intellij.formatting.FormattingModelBuilder;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
+import com.intellij.psi.formatter.FormatterUtil;
 import com.intellij.psi.formatter.FormattingDocumentModelImpl;
 import com.intellij.psi.formatter.PsiBasedFormattingModel;
+import com.intellij.psi.formatter.XmlFormatterUtilHelper;
 import com.intellij.psi.formatter.xml.XmlBlock;
 import com.intellij.psi.formatter.xml.XmlPolicy;
 import com.intellij.psi.impl.source.SourceTreeToPsiMap;
@@ -18,8 +20,12 @@ import com.intellij.psi.impl.source.tree.TreeUtil;
 import org.jetbrains.annotations.NotNull;
 
 public class XmlFormattingModelBuilder implements FormattingModelBuilder {
+  static {
+    FormatterUtil.addHelper(new XmlFormatterUtilHelper());
+  }
+  
   @NotNull
-    public FormattingModel createModel(final PsiElement element, final CodeStyleSettings settings) {
+  public FormattingModel createModel(final PsiElement element, final CodeStyleSettings settings) {
     final ASTNode root = TreeUtil.getFileElement((TreeElement)SourceTreeToPsiMap.psiElementToTree(element));
     final FormattingDocumentModelImpl documentModel = FormattingDocumentModelImpl.createOn(element.getContainingFile());
     return new PsiBasedFormattingModel(element.getContainingFile(),
