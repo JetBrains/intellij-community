845e2d3f6837db43d4b91d1bdb5254665eb5fbe7
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2008-01-28 14:59:58 +0300

    eclipse file type registration IDEADEV-23885

--- plugins/eclipse/src/org/jetbrains/idea/eclipse/config/EclipseFileType.java
+++ plugins/eclipse/src/org/jetbrains/idea/eclipse/config/EclipseFileType.java
@@ -1,8 +1,9 @@
 package org.jetbrains.idea.eclipse.config;
 
-import com.intellij.lang.StdLanguages;
-import com.intellij.openapi.fileTypes.LanguageFileType;
+import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.util.IconLoader;
+import com.intellij.openapi.vfs.CharsetToolkit;
+import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -14,12 +15,9 @@ import javax.swing.*;
 /**
  * Author: Vladislav.Kaznacheev
  */
-public class EclipseFileType extends LanguageFileType {
+public class EclipseFileType implements FileType {
   public static final Icon eclipseIcon = IconLoader.getIcon("/images/eclipse.gif");
 
-  public EclipseFileType() {
-    super(StdLanguages.XML);
-  }
 
   @NotNull
   @NonNls
@@ -41,5 +39,17 @@ public class EclipseFileType extends LanguageFileType {
   @Nullable
   public Icon getIcon() {
     return eclipseIcon;
+  }
+
+  public boolean isBinary() {
+    return false;
+  }
+
+  public boolean isReadOnly() {
+    return false;
+  }
+
+  public String getCharset(@NotNull final VirtualFile file) {
+    return CharsetToolkit.UTF8;
   }
 }
