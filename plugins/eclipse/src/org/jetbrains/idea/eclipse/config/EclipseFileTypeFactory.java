ffd1f31d4944a5d16e4640e08d644ff6c773f883
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2008-01-28 13:58:59 +0300

    eclipse file type registration IDEADEV-23885

---
+++ plugins/eclipse/src/org/jetbrains/idea/eclipse/config/EclipseFileTypeFactory.java
@@ -0,0 +1,19 @@
+package org.jetbrains.idea.eclipse.config;
+
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.fileTypes.FileTypeFactory;
+import com.intellij.util.PairConsumer;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.idea.eclipse.EclipseXml;
+
+/**
+ * Author: Vladislav.Kaznacheev
+ */
+public class EclipseFileTypeFactory extends FileTypeFactory {
+  private final static FileType fileType = new EclipseFileType();
+
+  public void createFileTypes(final @NotNull PairConsumer<FileType, String> consumer) {
+    consumer.consume(fileType, EclipseXml.CLASSPATH_EXT);
+    consumer.consume(fileType, EclipseXml.PROJECT_EXT);
+  }
+}
