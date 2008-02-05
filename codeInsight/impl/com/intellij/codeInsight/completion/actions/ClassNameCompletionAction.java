967fe4e1625581db1c95dcd8148499559d0169a9
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2008-02-05 13:11:28 +0300

    enable in lookups

--- codeInsight/impl/com/intellij/codeInsight/completion/actions/ClassNameCompletionAction.java
+++ codeInsight/impl/com/intellij/codeInsight/completion/actions/ClassNameCompletionAction.java
@@ -3,13 +3,9 @@ package com.intellij.codeInsight.completion.actions;
 import com.intellij.codeInsight.CodeInsightActionHandler;
 import com.intellij.codeInsight.actions.BaseCodeInsightAction;
 import com.intellij.codeInsight.completion.ClassNameCompletionHandler;
-import com.intellij.codeInsight.lookup.Lookup;
-import com.intellij.codeInsight.lookup.LookupItem;
 import com.intellij.featureStatistics.FeatureUsageTracker;
-import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiElement;
 
 /**
  *
@@ -26,15 +22,6 @@ public class ClassNameCompletionAction extends BaseCodeInsightAction {
 
   protected CodeInsightActionHandler getHandler() {
     return new ClassNameCompletionHandler();
-  }
-
-  public boolean isValidForLookup(Lookup lookup, DataContext context) {
-    if (lookup == null) return false;
-    final LookupItem item = lookup.getCurrentItem();
-    if (item == null) return false;
-    
-    final Object o = item.getObject();
-    return o instanceof PsiElement && ((PsiElement) o).isValid() && isValidForFile(null, null, ((PsiElement)o).getContainingFile());
   }
 
   protected boolean isValidForLookup() {
