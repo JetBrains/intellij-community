c18b988102f4ea336eccc7425ecfc76686ff43d0
Dmitry Jemerov <yole@intellij.com>
2008-02-08 15:55:17 +0300

    cleanup

--- lang-impl/src/com/intellij/psi/tree/DefaultRoleFinder.java
+++ lang-impl/src/com/intellij/psi/tree/DefaultRoleFinder.java
@@ -1,26 +1,20 @@
 package com.intellij.psi.tree;
 
-import com.intellij.psi.impl.source.tree.TreeElement;
-import com.intellij.psi.impl.source.tree.CompositeElement;
-import com.intellij.psi.xml.XmlElementType;
-import com.intellij.psi.xml.XmlTokenType;
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.lang.ASTNode;
+import org.jetbrains.annotations.NotNull;
 
 public class DefaultRoleFinder implements RoleFinder{
-  private static final Logger LOG = Logger.getInstance("#com.intellij.psi.tree.DefaultRoleFinder");
   final IElementType[] myElementTypes;
 
   public DefaultRoleFinder(IElementType... elementType) {
     myElementTypes = elementType;
   }
 
-  public ASTNode findChild(ASTNode parent) {
+  public ASTNode findChild(@NotNull ASTNode parent) {
     ASTNode current = parent.getFirstChildNode();
     while(current != null){
-      for (int i = 0; i < myElementTypes.length; i++) {
-        final IElementType elementType = myElementTypes[i];
-        if(current.getElementType() == elementType) return current;
+      for (final IElementType elementType : myElementTypes) {
+        if (current.getElementType() == elementType) return current;
       }
       current = current.getTreeNext();
     }
