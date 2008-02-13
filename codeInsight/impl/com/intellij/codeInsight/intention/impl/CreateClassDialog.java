1aec7ff5246daf9031f91db827b615a002c00d7e
greg <Gregory.Shrago@jetbrains.com>
2008-02-13 13:26:29 +0300

    minor fix

--- codeInsight/impl/com/intellij/codeInsight/intention/impl/CreateClassDialog.java
+++ codeInsight/impl/com/intellij/codeInsight/intention/impl/CreateClassDialog.java
@@ -13,6 +13,7 @@ import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiManager;
@@ -124,7 +125,7 @@ public class CreateClassDialog extends DialogWrapper {
           getOKAction().setEnabled(JavaPsiFacade.getInstance(myProject).getNameHelper().isIdentifier(myTfClassName.getText()));
         }
       });
-      getOKAction().setEnabled(myClassName != null);
+      getOKAction().setEnabled(StringUtil.isNotEmpty(myClassName));
     }
 
     gbConstraints.gridx = 0;
