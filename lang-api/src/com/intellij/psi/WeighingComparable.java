fb2fa1f044427398e03ba930df4974b7e4ec4db4
Peter Gromov <peter@jetbrains.com>
2008-02-20 15:24:37 +0300

    CCE

--- lang-api/src/com/intellij/psi/WeighingComparable.java
+++ lang-api/src/com/intellij/psi/WeighingComparable.java
@@ -4,12 +4,17 @@
  */
 package com.intellij.psi;
 
-import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * @author peter
 */
 public class WeighingComparable<T,Loc> implements Comparable<WeighingComparable<T,Loc>> {
+  private static final Comparable NULL = new Comparable() {
+    public int compareTo(final Object o) {
+      throw new UnsupportedOperationException("Method compareTo is not yet implemented in " + getClass().getName());
+    }
+  };
   private final Comparable[] myComputedWeighs;
   private final T myElement;
   private final Loc myLocation;
@@ -26,18 +31,26 @@ public class WeighingComparable<T,Loc> implements Comparable<WeighingComparable<
     assert myWeighers == comparable.myWeighers;
 
     for (int i = 0; i < myComputedWeighs.length; i++) {
-      final int result = getWeight(i).compareTo(comparable.getWeight(i));
-      if (result != 0) return result;
+      final Comparable weight1 = getWeight(i);
+      final Comparable weight2 = comparable.getWeight(i);
+      if (weight1 == null ^ weight2 == null) {
+        return weight1 == null ? -1 : 1;
+      }
+
+      if (weight1 != null) {
+        final int result = weight1.compareTo(weight2);
+        if (result != 0) return result;
+      }
     }
     return 0;
   }
 
-  @NotNull
+  @Nullable
   private Comparable getWeight(final int index) {
     Comparable weight = myComputedWeighs[index];
     if (weight == null) {
       weight = myWeighers[index].weigh(myElement, myLocation);
-      if (weight == null) weight = 0;
+      if (weight == null) weight = NULL;
       myComputedWeighs[index] = weight;
     }
     return weight;
