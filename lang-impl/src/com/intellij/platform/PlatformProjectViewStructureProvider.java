80be3056612ebe71a731c2d88951ef675791ba4b
Dmitry Jemerov <yole@intellij.com>
2008-02-28 15:45:16 +0300

    don't show dummy nodes in pycharm project view

---
+++ lang-impl/src/com/intellij/platform/PlatformProjectViewStructureProvider.java
@@ -0,0 +1,34 @@
+package com.intellij.platform;
+
+import com.intellij.ide.projectView.TreeStructureProvider;
+import com.intellij.ide.projectView.ViewSettings;
+import com.intellij.ide.projectView.impl.nodes.ProjectViewModuleNode;
+import com.intellij.ide.projectView.impl.nodes.ProjectViewProjectNode;
+import com.intellij.ide.util.treeView.AbstractTreeNode;
+import com.intellij.psi.PsiElement;
+
+import java.util.Collection;
+
+/**
+ * @author yole
+ */
+public class PlatformProjectViewStructureProvider implements TreeStructureProvider {
+  public Collection<AbstractTreeNode> modify(final AbstractTreeNode parent, final Collection<AbstractTreeNode> children, final ViewSettings settings) {
+    if (parent instanceof ProjectViewProjectNode) {
+      for(AbstractTreeNode child: children) {
+        if (child instanceof ProjectViewModuleNode) {
+          return child.getChildren();
+        }
+      }
+    }
+    return children;
+  }
+
+  public Object getData(final Collection<AbstractTreeNode> selected, final String dataName) {
+    return null;
+  }
+
+  public PsiElement getTopLevelElement(final PsiElement element) {
+    return null;
+  }
+}
