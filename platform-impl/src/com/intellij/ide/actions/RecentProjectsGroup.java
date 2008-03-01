3f0de5885b8ca6413098f843b67b185dd10b6be1
Dmitry Jemerov <yole@intellij.com>
2008-03-01 15:54:56 +0300

    RecentProjectsGroup -> platform

--- platform-impl/src/com/intellij/ide/actions/RecentProjectsGroup.java
+++ platform-impl/src/com/intellij/ide/actions/RecentProjectsGroup.java
@@ -1,19 +1,21 @@
 package com.intellij.ide.actions;
 
-import com.intellij.ide.RecentProjectsManager;
+import com.intellij.ide.RecentProjectsManagerBase;
 import com.intellij.openapi.actionSystem.ActionGroup;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.Presentation;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class RecentProjectsGroup extends ActionGroup {
+  @NotNull
   public AnAction[] getChildren(@Nullable AnActionEvent e) {
-    return RecentProjectsManager.getInstance().getRecentProjectsActions(true);
+    return RecentProjectsManagerBase.getInstance().getRecentProjectsActions(true);
   }
 
   public void update(AnActionEvent event) {
     Presentation presentation = event.getPresentation();
-    presentation.setEnabled(RecentProjectsManager.getInstance().getRecentProjectsActions(true).length > 0);
+    presentation.setEnabled(RecentProjectsManagerBase.getInstance().getRecentProjectsActions(true).length > 0);
   }
 }
