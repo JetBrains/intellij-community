d08098d28d860ec5558cc043c4caedd08734d08c
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2008-03-06 19:03:00 +0300

    python completion test added

---
+++ python/testSrc/com/jetbrains/python/PythonCompletionTest.java
@@ -0,0 +1,27 @@
+/*
+ * User: anna
+ * Date: 06-Mar-2008
+ */
+package com.jetbrains.python;
+
+import com.intellij.codeInsight.completion.CodeCompletionHandler;
+import com.intellij.openapi.application.PathManager;
+import com.intellij.testFramework.LightCodeInsightTestCase;
+
+public class PythonCompletionTest extends LightCodeInsightTestCase{
+
+  private void doTest() throws Exception {
+    final String testName = getTestName(false);
+    configureByFile(testName + ".py");
+    new CodeCompletionHandler().invoke(getProject(), getEditor(), getFile());
+    checkResultByFile(testName + ".after.py");
+  }
+
+  protected String getTestDataPath() {
+    return PathManager.getHomePath() + "/plugins/python/testData/completion/";
+  }
+
+  public void testLocalVar() throws Exception {
+    doTest();
+  }
+}
