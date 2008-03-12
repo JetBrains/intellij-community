6afba0a1b56d96f4ce2d12a4b300c9dc3770a4c9
Bas Leijdekkers <basleijdekkers@gmail.com>
2008-03-12 18:36:50 +0300

    GRVY-1172

--- plugins/groovy/src/org/jetbrains/plugins/groovy/codeInspection/control/GroovyConstantIfStatementInspection.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/codeInspection/control/GroovyConstantIfStatementInspection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007-2008 Dave Griffith
+ * Copyright 2007-2008 Dave Griffith, Bas Leijdekkers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,94 +25,122 @@ import org.jetbrains.plugins.groovy.codeInspection.BaseInspection;
 import org.jetbrains.plugins.groovy.codeInspection.BaseInspectionVisitor;
 import org.jetbrains.plugins.groovy.codeInspection.GroovyFix;
 import org.jetbrains.plugins.groovy.lang.psi.api.auxiliary.GrCondition;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrBlockStatement;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrIfStatement;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrStatement;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.blocks.GrOpenBlock;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrExpression;
+import org.jetbrains.plugins.groovy.lang.psi.api.util.GrStatementOwner;
 
 public class GroovyConstantIfStatementInspection extends BaseInspection {
 
-    @NotNull
-    public String getGroupDisplayName() {
-        return CONTROL_FLOW;
-    }
+  @NotNull
+  public String getGroupDisplayName() {
+    return CONTROL_FLOW;
+  }
+
+  @NotNull
+  public String getDisplayName() {
+    return "Constant if statement";
+  }
+
+  public boolean isEnabledByDefault() {
+    return true;
+  }
+
+  @NotNull
+  protected String buildErrorString(Object... args) {
+    return "#ref statement can be simplified #loc";
+  }
+
+  public BaseInspectionVisitor buildVisitor() {
+    return new ConstantIfStatementVisitor();
+  }
+
+  public GroovyFix buildFix(PsiElement location) {
+    return new ConstantIfStatementFix();
+  }
+
+  private static class ConstantIfStatementFix extends GroovyFix {
 
     @NotNull
-    public String getDisplayName() {
-        return "Constant if statement";
+    public String getName() {
+      return "Simplify";
     }
 
-    public boolean isEnabledByDefault() {
-        return true;
-    }
-
-    @NotNull
-    protected String buildErrorString(Object... args) {
-        return "#ref statement can be simplified #loc";
-    }
-
-    public BaseInspectionVisitor buildVisitor() {
-        return new ConstantIfStatementVisitor();
-    }
-
-    public GroovyFix buildFix(PsiElement location) {
-        return new ConstantIfStatementFix();
-    }
-
-    private static class ConstantIfStatementFix extends GroovyFix {
-
-        @NotNull
-        public String getName() {
-            return "Simplify";
+    public void doFix(Project project, ProblemDescriptor descriptor)
+        throws IncorrectOperationException {
+      final PsiElement ifKeyword = descriptor.getPsiElement();
+      final GrIfStatement ifStatement = (GrIfStatement) ifKeyword.getParent();
+      assert ifStatement != null;
+      final GrStatement thenBranch = ifStatement.getThenBranch();
+      final GrStatement elseBranch = ifStatement.getElseBranch();
+      final GrExpression condition = (GrExpression) ifStatement.getCondition();
+      // todo still needs some handling for conflicting declarations
+      if (isFalse(condition)) {
+        if (elseBranch != null) {
+          replaceStatement(ifStatement, elseBranch);
+        } else {
+          ifStatement.delete();
         }
+      } else {
+        replaceStatement(ifStatement, thenBranch);
+      }
+    }
 
-        public void doFix(Project project, ProblemDescriptor descriptor)
-                throws IncorrectOperationException {
-            final PsiElement ifKeyword = descriptor.getPsiElement();
-            final GrIfStatement statement =
-                    (GrIfStatement) ifKeyword.getParent();
-            assert statement != null;
-            final GrStatement thenBranch = statement.getThenBranch();
-            final GrStatement elseBranch = statement.getElseBranch();
-            final GrExpression condition = (GrExpression) statement.getCondition();
-            if (isFalse(condition)) {
-                if (elseBranch != null) {
-                    replaceStatement(statement, elseBranch.getText());
-                } else {
-                    statement.delete();
-                }
-            } else {
-                replaceStatement(statement, thenBranch.getText());
+    /**
+     * unwraps surrounding blocks from new statement.
+     */
+    private void replaceStatement(GrStatement oldStatement, GrStatement newStatement) throws IncorrectOperationException {
+      if (newStatement instanceof GrBlockStatement) {
+        GrBlockStatement blockStatement = (GrBlockStatement) newStatement;
+        final GrOpenBlock openBlock = blockStatement.getBlock();
+        final GrStatement[] statements = openBlock.getStatements();
+        if (statements.length == 0) {
+          oldStatement.removeStatement();
+        } else {
+          final PsiElement parent = oldStatement.getParent();
+          if (parent instanceof GrStatementOwner) {
+            GrStatementOwner statementOwner = (GrStatementOwner) parent;
+            for (GrStatement statement : statements) {
+              statementOwner.addStatementBefore(statement, oldStatement);
             }
+            oldStatement.removeStatement();
+          }
         }
+      } else {
+        replaceStatement(oldStatement, newStatement.getText());
+      }
     }
+  }
 
-    private static class ConstantIfStatementVisitor
-            extends BaseInspectionVisitor {
+  private static class ConstantIfStatementVisitor
+      extends BaseInspectionVisitor {
 
-        public void visitIfStatement(GrIfStatement statement) {
-            super.visitIfStatement(statement);
-            final GrCondition condition = statement.getCondition();
-            if (!(condition instanceof GrExpression)) {
-                return;
-            }
-            final GrStatement thenBranch = statement.getThenBranch();
-            if (thenBranch == null) {
-                return;
-            }
-            final GrExpression conditionExpression = (GrExpression) condition;
-            if (isTrue(conditionExpression) || isFalse(conditionExpression)) {
-                registerStatementError(statement);
-            }
-        }
+    public void visitIfStatement(GrIfStatement statement) {
+      super.visitIfStatement(statement);
+      final GrCondition condition = statement.getCondition();
+      if (!(condition instanceof GrExpression)) {
+        return;
+      }
+      final GrStatement thenBranch = statement.getThenBranch();
+      if (thenBranch == null) {
+        return;
+      }
+      final GrExpression conditionExpression = (GrExpression) condition;
+      if (isTrue(conditionExpression) || isFalse(conditionExpression)) {
+        registerStatementError(statement);
+      }
     }
+  }
 
-    private static boolean isFalse(GrExpression expression) {
-        @NonNls final String text = expression.getText();
-        return "false".equals(text);
-    }
+  private static boolean isFalse(GrExpression expression) {
+    @NonNls final String text = expression.getText();
+    return "false".equals(text);
+  }
 
-    private static boolean isTrue(GrExpression expression) {
-        @NonNls final String text = expression.getText();
-        return "true".equals(text);
-    }
+  private static boolean isTrue(GrExpression expression) {
+    @NonNls final String text = expression.getText();
+    return "true".equals(text);
+  }
 }
