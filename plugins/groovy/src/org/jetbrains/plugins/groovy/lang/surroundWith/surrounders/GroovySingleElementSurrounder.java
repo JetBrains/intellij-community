3dcf7b9503e35e311dd40d0f9e9da3c1077c1827
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-03-12 16:27:38 +0300

    (no message)

--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/surroundWith/surrounders/GroovySingleElementSurrounder.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/surroundWith/surrounders/GroovySingleElementSurrounder.java
@@ -1,16 +1,8 @@
 package org.jetbrains.plugins.groovy.lang.surroundWith.surrounders;
 
 import com.intellij.lang.surroundWith.Surrounder;
-import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
-import com.intellij.openapi.util.TextRange;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.util.IncorrectOperationException;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.plugins.groovy.lang.psi.GroovyPsiElement;
-import org.jetbrains.plugins.groovy.lang.psi.GroovyPsiElementFactory;
 
 /**
  * User: Dmitry.Krasilschikov
@@ -18,10 +10,7 @@ import org.jetbrains.plugins.groovy.lang.psi.GroovyPsiElementFactory;
  */
 abstract public class GroovySingleElementSurrounder implements Surrounder {
   public boolean isApplicable(@NotNull PsiElement[] elements) {
-    if (elements.length == 0) return false;
-    if (elements.length == 1) return isApplicable(elements[0]);
-
-    return false;
+    return elements.length == 1 &&  isApplicable(elements[0]);
   }
 
   protected abstract boolean isApplicable(PsiElement element);
