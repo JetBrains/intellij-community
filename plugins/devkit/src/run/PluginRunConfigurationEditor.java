1e2819de90f0b4b3e3f488de425aef79ab26f832
Dmitry Jemerov <yole@intellij.com>
2008-03-19 14:57:31 +0300

    fix typo in method name; a number of IDEA UI components can now be correctly instantiated in design time

--- plugins/devkit/src/run/PluginRunConfigurationEditor.java
+++ plugins/devkit/src/run/PluginRunConfigurationEditor.java
@@ -139,11 +139,11 @@ public class PluginRunConfigurationEditor extends SettingsEditor<PluginRunConfig
     JPanel wholePanel = new JPanel(new GridBagLayout());
     myVMParameters.setText(DevKitBundle.message("vm.parameters"));
     myVMParameters.setComponent(new RawCommandLineEditor());
-    myVMParameters.getComponent().setDialodCaption(myVMParameters.getRawText());
+    myVMParameters.getComponent().setDialogCaption(myVMParameters.getRawText());
 
     myProgramParameters.setText(DevKitBundle.message("program.parameters"));
     myProgramParameters.setComponent(new RawCommandLineEditor());
-    myProgramParameters.getComponent().setDialodCaption(myProgramParameters.getRawText());
+    myProgramParameters.getComponent().setDialogCaption(myProgramParameters.getRawText());
 
     GridBagConstraints gc = new GridBagConstraints(0, GridBagConstraints.RELATIVE, 1, 1, 1, 0, GridBagConstraints.NORTHWEST, GridBagConstraints.HORIZONTAL, new Insets(5, 0, 5, 0), 0, 0);
     wholePanel.add(myVMParameters, gc);
