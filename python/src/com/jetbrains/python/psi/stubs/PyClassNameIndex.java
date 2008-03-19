31aa99cb4e3f5bf1d058aa343a463fcfdf91f1c1
Maxim Shafirov <max@jetbrains.com>
2008-03-19 20:20:37 +0300

    Index building in progress.

---
+++ python/src/com/jetbrains/python/psi/stubs/PyClassNameIndex.java
@@ -0,0 +1,16 @@
+/*
+ * @author max
+ */
+package com.jetbrains.python.psi.stubs;
+
+import com.intellij.psi.stubs.StringStubIndexExtension;
+import com.intellij.psi.stubs.StubIndexKey;
+import com.jetbrains.python.psi.PyClass;
+
+public class PyClassNameIndex extends StringStubIndexExtension<PyClass> {
+  public static final StubIndexKey<String,PyClass> KEY = new StubIndexKey<String, PyClass>("Py.class.shortName");
+
+  public StubIndexKey<String, PyClass> getKey() {
+    return KEY;
+  }
+}
---
+++ python/src/com/jetbrains/python/psi/stubs/PyFunctionNameIndex.java
@@ -0,0 +1,16 @@
+/*
+ * @author max
+ */
+package com.jetbrains.python.psi.stubs;
+
+import com.intellij.psi.stubs.StringStubIndexExtension;
+import com.intellij.psi.stubs.StubIndexKey;
+import com.jetbrains.python.psi.PyFunction;
+
+public class PyFunctionNameIndex extends StringStubIndexExtension<PyFunction> {
+  public static final StubIndexKey<String,PyFunction> KEY = new StubIndexKey<String, PyFunction>("Py.function.shortName");
+
+  public StubIndexKey<String, PyFunction> getKey() {
+    return KEY;
+  }
+}
