ab535e0a75a3e915fe4c4a6bdb1773cc9e151283
Eugene Zhuravlev <jeka@jetbrains.com>
2008-03-19 14:35:50 +0300

    make 'pause' work for suspendPolicy == 'thread'

--- debugger/impl/com/intellij/debugger/actions/PauseActionHandler.java
+++ debugger/impl/com/intellij/debugger/actions/PauseActionHandler.java
@@ -1,10 +1,12 @@
 package com.intellij.debugger.actions;
 
 import com.intellij.debugger.DebuggerManagerEx;
+import com.intellij.debugger.engine.SuspendContextImpl;
 import com.intellij.debugger.impl.DebuggerSession;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
 import com.intellij.xdebugger.impl.actions.DebuggerActionHandler;
+import com.sun.jdi.request.EventRequest;
 import org.jetbrains.annotations.NotNull;
 
 public class PauseActionHandler extends DebuggerActionHandler {
@@ -15,6 +17,13 @@ public class PauseActionHandler extends DebuggerActionHandler {
   public boolean isEnabled(@NotNull final Project project, final AnActionEvent event) {
     final DebuggerSession debuggerSession = DebuggerManagerEx.getInstanceEx(project).getContext().getDebuggerSession();
     return debuggerSession != null && !debuggerSession.getProcess().isPausePressed() &&
-           (debuggerSession.isEvaluating() || debuggerSession.isRunning());
+           (debuggerSession.isEvaluating() || 
+            debuggerSession.isRunning() || isSingleThreadSuspended(debuggerSession)
+           );
+  }
+
+  private boolean isSingleThreadSuspended(final DebuggerSession debuggerSession) {
+    final SuspendContextImpl suspendContext = debuggerSession.getContextManager().getContext().getSuspendContext();
+    return suspendContext != null && suspendContext.getSuspendPolicy() == EventRequest.SUSPEND_EVENT_THREAD;
   }
 }
