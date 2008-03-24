f451cf67cec8d09e614a3678eaf7c10a007e058a
Dmitry Jemerov <yole@intellij.com>
2008-03-24 13:30:07 +0300

    definitions search for Python

---
+++ python/src/com/jetbrains/python/psi/search/PyDefinitionsSearch.java
@@ -0,0 +1,24 @@
+package com.jetbrains.python.psi.search;
+
+import com.intellij.psi.PsiElement;
+import com.intellij.util.Processor;
+import com.intellij.util.QueryExecutor;
+import com.intellij.util.Query;
+import com.jetbrains.python.psi.PyClass;
+
+/**
+ * @author yole
+ */
+public class PyDefinitionsSearch implements QueryExecutor<PsiElement, PsiElement> {
+  public boolean execute(final PsiElement queryParameters, final Processor<PsiElement> consumer) {
+    if (queryParameters instanceof PyClass) {
+      final Query<PyClass> query = PyClassInheritorsSearch.search((PyClass)queryParameters);
+      return query.forEach(new Processor<PyClass>() {
+        public boolean process(final PyClass pyClass) {
+          return consumer.process(pyClass);
+        }
+      });
+    }
+    return true;
+  }
+}
