f6ac4de09c8b191e3f44140ec155f9beea348ef0
Eugene Vigdorchik <no_reply@jetbrains.com>
2008-03-29 14:55:22 +0300

    copyright added

--- plugins/groovy/src/org/jetbrains/plugins/groovy/annotator/intentions/dynamic/elements/DRootElement.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/annotator/intentions/dynamic/elements/DRootElement.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2000-2007 JetBrains s.r.o.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jetbrains.plugins.groovy.annotator.intentions.dynamic.elements;
 
 import org.jetbrains.annotations.Nullable;
@@ -11,7 +25,7 @@ import java.util.Set;
  * User: Dmitry.Krasilschikov
  * Date: 30.11.2007
  */
-public class DRootElement implements DElement {
+public class DRootElement {
   public Map<String, DClassElement> containingClasses = new HashMap<String, DClassElement>();
 
   public DRootElement() {
