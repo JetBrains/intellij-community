5ee849c9178284baf6c5014b1a8436b4200ae638
Maxim Shafirov <max@jetbrains.com>
2008-04-08 20:54:32 +0400

    Faster stub serialization plus library classes traversal.

--- python/src/com/jetbrains/python/psi/PyStubElementType.java
+++ python/src/com/jetbrains/python/psi/PyStubElementType.java
@@ -6,8 +6,8 @@ package com.jetbrains.python.psi;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.stubs.IStubElementType;
-import com.intellij.psi.stubs.StubElement;
 import com.intellij.psi.stubs.IndexSink;
+import com.intellij.psi.stubs.StubElement;
 import com.jetbrains.python.PythonFileType;
 import org.jetbrains.annotations.NonNls;
 
@@ -27,6 +27,6 @@ public abstract class PyStubElementType<StubT extends StubElement, PsiT extends 
   }
 
   public String getExternalId() {
-    return super.toString();
+    return "py." + super.toString();
   }
 }
