f3cd2876a367a31efdd2dc1200f43189f9d5e2be
Peter Gromov <peter@jetbrains.com>
2008-04-08 19:48:56 +0400

    access problem

--- xml/impl/src/com/intellij/lexer/HtmlHighlightingLexer.java
+++ xml/impl/src/com/intellij/lexer/HtmlHighlightingLexer.java
@@ -51,7 +51,7 @@ public class HtmlHighlightingLexer extends BaseHtmlLexer {
     }
   }
 
-  class ElEmbeddmentHandler implements TokenHandler {
+  public class ElEmbeddmentHandler implements TokenHandler {
     public void handleElement(Lexer lexer) {
       setEmbeddedLexer();
       if (embeddedLexer != null) {
