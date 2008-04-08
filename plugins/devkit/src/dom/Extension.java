c3341787ef94052f2700e55c139f1b6edfee08e6
Peter Gromov <peter@jetbrains.com>
2008-04-08 19:46:48 +0400

    id & order attrs support

--- plugins/devkit/src/dom/Extension.java
+++ plugins/devkit/src/dom/Extension.java
@@ -1,9 +1,17 @@
 package org.jetbrains.idea.devkit.dom;
 
 import com.intellij.util.xml.DomElement;
+import com.intellij.util.xml.GenericAttributeValue;
+import com.intellij.util.xml.NameValue;
 
 /**
  * @author mike
  */
 public interface Extension extends DomElement {
+
+  @NameValue
+  GenericAttributeValue<String> getId();
+
+  GenericAttributeValue<String> getOrder();
+
 }
