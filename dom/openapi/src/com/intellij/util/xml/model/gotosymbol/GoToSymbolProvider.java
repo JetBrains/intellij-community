e7405875584e493c19cfd687e32cfda6ab2f2f7d
Sergey Vasiliev <Sergey.Vasiliev@jetbrains.com>
2008-04-09 16:42:05 +0400

    supported goto symbol for seam components

---
+++ dom/openapi/src/com/intellij/util/xml/model/gotosymbol/GoToSymbolProvider.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright 2000-2006 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intellij.util.xml.model.gotosymbol;
+
+import com.intellij.extapi.psi.ASTWrapperPsiElement;
+import com.intellij.navigation.ChooseByNameContributor;
+import com.intellij.navigation.ItemPresentation;
+import com.intellij.navigation.NavigationItem;
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.xml.XmlElement;
+import com.intellij.util.xml.DomElement;
+import com.intellij.util.xml.ElementPresentationManager;
+import com.intellij.util.xml.GenericDomValue;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Base class for "Go To Symbol" contributors.
+ */
+public abstract class GoToSymbolProvider implements ChooseByNameContributor {
+
+  protected abstract void addNames(@NotNull Module module, Set<String> result);
+
+  protected abstract void addItems(@NotNull Module module, String name, List<NavigationItem> result);
+
+  protected abstract boolean acceptModule(final Module module);
+
+  protected static void addNewNames(@NotNull final List<? extends DomElement> elements, final Set<String> existingNames) {
+    for (DomElement name : elements) {
+      existingNames.add(name.getGenericInfo().getElementName(name));
+    }
+  }
+
+  public String[] getNames(final Project project, boolean includeNonProjectItems) {
+    Set<String> result = new HashSet<String>();
+    Module[] modules = ModuleManager.getInstance(project).getModules();
+    for (Module module : modules) {
+      if (acceptModule(module)) {
+        addNames(module, result);
+      }
+    }
+
+    return result.toArray(new String[result.size()]);
+  }
+
+  public NavigationItem[] getItemsByName(final String name, final String pattern, final Project project, boolean includeNonProjectItems) {
+    List<NavigationItem> result = new ArrayList<NavigationItem>();
+    Module[] modules = ModuleManager.getInstance(project).getModules();
+    for (Module module : modules) {
+      if (acceptModule(module)) {
+        addItems(module, name, result);
+      }
+    }
+
+    return result.toArray(new NavigationItem[result.size()]);
+  }
+
+  @Nullable
+  protected static NavigationItem createNavigationItem(final DomElement domElement) {
+    final GenericDomValue name = domElement.getGenericInfo().getNameDomElement(domElement);
+    assert name != null;
+    final XmlElement psiElement = name.getXmlElement();
+    final String value = name.getStringValue();
+    if (psiElement == null || value == null) {
+      return null;
+    }
+    final Icon icon = ElementPresentationManager.getIcon(domElement);
+    return createNavigationItem(psiElement, value, icon);
+  }
+
+  @NotNull
+  protected static NavigationItem createNavigationItem(@NotNull final PsiElement element,
+                                                       @NotNull @NonNls final String text,
+                                                       @Nullable final Icon icon) {
+    return new BaseNavigationItem(element, text, icon);
+  }
+
+
+  /**
+   * Wraps one entry to display in "Go To Symbol" dialog.
+   */
+  protected static class BaseNavigationItem extends ASTWrapperPsiElement {
+
+    private final PsiElement myPsiElement;
+    private final String myText;
+    private final Icon myIcon;
+
+    /**
+     * Creates a new display item.
+     *
+     * @param psiElement The PsiElement to navigate to.
+     * @param text       Text to show for this element.
+     * @param icon       Icon to show for this element.
+     */
+    protected BaseNavigationItem(@NotNull PsiElement psiElement, @NotNull @NonNls String text, @Nullable Icon icon) {
+      super(psiElement.getNode());
+      myPsiElement = psiElement;
+      myText = text;
+      myIcon = icon;
+    }
+
+    public Icon getIcon(int flags) {
+      return myIcon;
+    }
+
+    public ItemPresentation getPresentation() {
+      return new ItemPresentation() {
+
+        public String getPresentableText() {
+          return myText;
+        }
+
+        @Nullable
+        public String getLocationString() {
+          return '(' + myPsiElement.getContainingFile().getName() + ')';
+        }
+
+        @Nullable
+        public Icon getIcon(boolean open) {
+          return myIcon;
+        }
+
+        @Nullable
+        public TextAttributesKey getTextAttributesKey() {
+          return null;
+        }
+      };
+    }
+  }
+
+}
