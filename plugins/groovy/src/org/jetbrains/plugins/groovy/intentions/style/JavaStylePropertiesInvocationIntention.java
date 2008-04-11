c2640751e24be1fbaad567d598c96ef62195d965
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2008-04-11 16:15:05 +0400

    GRVY-1179

---
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/intentions/style/JavaStylePropertiesInvocationIntention.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2000-2008 JetBrains s.r.o.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jetbrains.plugins.groovy.intentions.style;
+
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.PsiElement;
+import com.intellij.util.IncorrectOperationException;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.intentions.base.Intention;
+import org.jetbrains.plugins.groovy.intentions.base.IntentionUtils;
+import org.jetbrains.plugins.groovy.intentions.base.PsiElementPredicate;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.arguments.GrArgumentList;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrExpression;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrReferenceExpression;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.path.GrMethodCallExpression;
+import static org.jetbrains.plugins.groovy.lang.psi.util.PsiElementUtil.*;
+
+/**
+ * @author ilyas
+ */
+public class JavaStylePropertiesInvocationIntention extends Intention {
+
+  protected void processIntention(@NotNull PsiElement element) throws IncorrectOperationException {
+    assert element instanceof GrMethodCallExpression;
+    GrMethodCallExpression call = ((GrMethodCallExpression) element);
+    GrExpression invoked = call.getInvokedExpression();
+    if (isGetterInvocation(call) && invoked instanceof GrReferenceExpression) {
+      String name = ((GrReferenceExpression) invoked).getName();
+      assert name != null;
+      name = StringUtil.trimStart(name, GETTER_PREFIX);
+      name = StringUtil.decapitalize(name);
+      replaceWithGetter(call, name);
+    } else if (isSetterInvocation(call) && invoked instanceof GrReferenceExpression) {
+      String name = ((GrReferenceExpression) invoked).getName();
+      assert name != null;
+      GrArgumentList args = call.getArgumentList();
+      assert args != null;
+      name = StringUtil.trimStart(name, SETTER_PREFIX);
+      name = StringUtil.decapitalize(name);
+      GrExpression value = args.getExpressionArguments()[0];
+      replaceWithSetter(call, name, value);
+    }
+  }
+
+  private static void replaceWithSetter(GrMethodCallExpression call, String name, GrExpression value) throws IncorrectOperationException {
+    GrReferenceExpression refExpr = (GrReferenceExpression) call.getInvokedExpression();
+    String oldNameStr = refExpr.getReferenceNameElement().getText();
+    String newRefExpr = StringUtil.trimEnd(refExpr.getText(), oldNameStr) + name;
+    IntentionUtils.replaceExpression(newRefExpr + " = " + value.getText(), call);
+  }
+
+  private static void replaceWithGetter(GrMethodCallExpression call, String name) throws IncorrectOperationException {
+    GrReferenceExpression refExpr = (GrReferenceExpression) call.getInvokedExpression();
+    String oldNameStr = refExpr.getReferenceNameElement().getText();
+    String newRefExpr = StringUtil.trimEnd(refExpr.getText(), oldNameStr) + name;
+    IntentionUtils.replaceExpression(newRefExpr, call);
+  }
+
+  @NotNull
+  protected PsiElementPredicate getElementPredicate() {
+    return new JavaPropertyInvocationPredicate();
+  }
+
+  private static class JavaPropertyInvocationPredicate implements PsiElementPredicate {
+    public boolean satisfiedBy(PsiElement element) {
+      if (!(element instanceof GrMethodCallExpression)) return false;
+      GrMethodCallExpression call = (GrMethodCallExpression) element;
+      return isPropertyAccessor(call);
+    }
+  }
+}
---
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/util/PsiElementUtil.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2000-2008 JetBrains s.r.o.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jetbrains.plugins.groovy.lang.psi.util;
+
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrExpression;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrReferenceExpression;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.path.GrMethodCallExpression;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.arguments.GrArgumentList;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.PsiMethod;
+
+/**
+ * @author ilyas
+ */
+public class PsiElementUtil {
+  public static final String GETTER_PREFIX = "get";
+  public static final String SETTER_PREFIX = "set";
+
+  public static boolean isPropertyAccessor(GrMethodCallExpression call) {
+    return isGetterInvocation(call) || isSetterInvocation(call);
+  }
+
+  public static boolean isSetterInvocation(GrMethodCallExpression call) {
+    GrExpression expr = call.getInvokedExpression();
+    if (!(expr instanceof GrReferenceExpression)) return false;
+
+    GrReferenceExpression refExpr = (GrReferenceExpression) expr;
+    String name = refExpr.getName();
+    if (name == null || !name.startsWith(SETTER_PREFIX)) return false;
+
+    name = StringUtil.trimStart(name, SETTER_PREFIX);
+    String propName = StringUtil.decapitalize(name);
+    if (propName.length() == 0 || name.equals(propName)) return false;
+
+    PsiMethod method = call.resolveMethod();
+    if (!PsiUtil.isSimplePropertySetter(method)) return false;
+
+    GrArgumentList args = call.getArgumentList();
+    return args != null &&
+        args.getExpressionArguments().length == 1 &&
+        args.getNamedArguments().length == 0;
+  }
+
+  public static boolean isGetterInvocation(GrMethodCallExpression call) {
+    GrExpression expr = call.getInvokedExpression();
+    if (!(expr instanceof GrReferenceExpression)) return false;
+
+    GrReferenceExpression refExpr = (GrReferenceExpression) expr;
+    String name = refExpr.getName();
+    if (name == null || !name.startsWith(GETTER_PREFIX)) return false;
+
+    name = StringUtil.trimStart(name, GETTER_PREFIX);
+    String propName = StringUtil.decapitalize(name);
+    if (propName.length() == 0 || name.equals(propName)) return false;
+
+
+    PsiMethod method = call.resolveMethod();
+    if (!PsiUtil.isSimplePropertyGetter(method)) return false;
+
+    GrArgumentList args = call.getArgumentList();
+    return args != null && args.getExpressionArguments().length == 0;
+  }
+
+
+}
