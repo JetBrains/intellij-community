0720d546e9a8b6eb2b017f5bcaa0015e7685506b
Dmitry Cheryasov <Dmitry.Cheryasov@jetbrains.com>
2008-04-11 16:23:23 +0400

    importing 'with_statement' not as first in list (demonstrative case for PY-1).

--- python/testData/psi/WithStatement2.py
+++ python/testData/psi/WithStatement2.py
@@ -1,8 +1,8 @@
 # with = 1 # legal identifier
 # with = 1
 with = 1
-# from __future__ import with_statement, nested_scopes
-from __future__ import with_statement, nested_scopes
+# from __future__ import nested_scopes, with_statement
+from __future__ import nested_scopes, with_statement
 # with x.y(z)[t] as y: pass
 with x.y(z)[t] as y: pass
 # with = 1 # now illegal
