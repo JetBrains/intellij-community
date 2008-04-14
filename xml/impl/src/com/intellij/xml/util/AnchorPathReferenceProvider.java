92bc3126e285675f48b9c3f4d3e9e7add7bffced
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2008-04-14 16:49:22 +0400

    anchor reference integrated with PathReferenceManager: fixed

--- xml/impl/src/com/intellij/xml/util/AnchorPathReferenceProvider.java
+++ xml/impl/src/com/intellij/xml/util/AnchorPathReferenceProvider.java
@@ -40,7 +40,7 @@ public class AnchorPathReferenceProvider implements PathReferenceProvider {
       final int i = elementText.indexOf('#', offset);
       if (i >= 0) {
         final int pos = elementText.indexOf('?', i);
-        final String anchor = pos == -1 ? elementText.substring(i + 1) : elementText.substring(i + 1, pos);
+        final String anchor = pos == -1 ? elementText.substring(i + 1, range.getEndOffset()) : elementText.substring(i + 1, pos);
         references.add(new AnchorReference(anchor, fileReference, psiElement, range.getStartOffset() + i, soft));
       }
     }
