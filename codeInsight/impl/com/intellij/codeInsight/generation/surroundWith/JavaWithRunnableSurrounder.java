4c3ede45c8b6462adf331f931b22094968c898f8
Irina Chernushina <Irina.Chernushina@jetbrains.com>
2008-04-24 18:09:28 +0400

    IDEADEV-23920 fixed ("Surrond with Runnable" should make locals/parameters final automatically if possible)

--- codeInsight/impl/com/intellij/codeInsight/generation/surroundWith/JavaWithRunnableSurrounder.java
+++ codeInsight/impl/com/intellij/codeInsight/generation/surroundWith/JavaWithRunnableSurrounder.java
@@ -7,8 +7,15 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.*;
 import com.intellij.psi.codeStyle.CodeStyleManager;
+import com.intellij.psi.search.LocalSearchScope;
+import com.intellij.psi.search.searches.ReferencesSearch;
+import com.intellij.psi.util.PsiUtil;
 import com.intellij.util.IncorrectOperationException;
 import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
 
 class JavaWithRunnableSurrounder extends JavaStatementsSurrounder{
   public String getTemplateDescription() {
@@ -44,24 +51,58 @@ class JavaWithRunnableSurrounder extends JavaStatementsSurrounder{
   private static void makeVariablesFinal(PsiElement scope, PsiCodeBlock body) throws IncorrectOperationException{
     //Q : check if variable may not be final (assigned twice)?
     PsiElement[] children = scope.getChildren();
+
     for (PsiElement child : children) {
       makeVariablesFinal(child, body);
+
       if (child instanceof PsiReferenceExpression) {
         if (child.getParent() instanceof PsiMethodCallExpression) continue;
-        if (child.getChildren().length != 1) continue;
+        if (PsiUtil.isAccessedForWriting((PsiReferenceExpression) child)) {
+          continue;
+        }
+
         PsiElement refElement = ((PsiReferenceExpression)child).resolve();
         if (refElement instanceof PsiLocalVariable || refElement instanceof PsiParameter) {
-          PsiVariable variable = (PsiVariable)refElement;
+          PsiVariable variable = (PsiVariable) refElement;
+          final PsiModifierList modifierList = variable.getModifierList();
+          if ((modifierList != null) && (modifierList.hasModifierProperty(PsiModifier.FINAL))) {
+            continue;
+          }
+
           PsiElement parent = variable.getParent();
+          PsiMethod enclosingMethod = null;
+
           while (parent != null) {
             if (parent.equals(body)) break;
+            if (parent instanceof PsiMethod) {
+              enclosingMethod = (PsiMethod) parent;
+            }
             parent = parent.getParent();
           }
-          if (parent == null) {
+          if ((parent == null) && canBeDeclaredFinal(variable, enclosingMethod)) {
             variable.getModifierList().setModifierProperty(PsiModifier.FINAL, true);
           }
         }
       }
     }
+  }
+
+  private static boolean canBeDeclaredFinal(@NotNull final PsiVariable variable, @Nullable final PsiElement scope) {
+    if (scope == null) {
+      return false;
+    }
+    final Collection<PsiReference> references = ReferencesSearch.search(variable, new LocalSearchScope(scope)).findAll();
+    boolean foundOnce = (variable.getInitializer() != null);
+    for (PsiReference reference : references) {
+      if (reference instanceof PsiReferenceExpression) {
+        if (PsiUtil.isAccessedForWriting((PsiReferenceExpression) reference)) {
+          if (foundOnce) {
+            return false;
+          }
+          foundOnce = true;
+        }
+      }
+    }
+    return true;
   }
 }
