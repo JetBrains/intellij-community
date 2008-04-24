efe843284ba3d17d64c26431d85371304ce0258f
Maxim Shafirov <max@jetbrains.com>
2008-04-24 15:08:44 +0400

    Attempt to speedup tests.

--- platform-impl/src/com/intellij/openapi/application/ex/DecodeDefaultsUtil.java
+++ platform-impl/src/com/intellij/openapi/application/ex/DecodeDefaultsUtil.java
@@ -9,13 +9,27 @@ import org.jetbrains.annotations.Nullable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DecodeDefaultsUtil {
   private static final Logger LOG = Logger.getInstance("#com.intellij.openapi.application.ex.DecodeDefaultsUtil");
+  private static Map<String, URL> myResourceCache = Collections.synchronizedMap(new HashMap<String, URL>());
 
   @NonNls private static final String XML_EXTENSION = ".xml";
 
   public static URL getDefaults(Object requestor, final String componentResourcePath) {
+    if (myResourceCache.containsKey(componentResourcePath)) {
+      return myResourceCache.get(componentResourcePath);
+    }
+
+    URL url = getDefaultsImpl(requestor, componentResourcePath);
+    myResourceCache.put(componentResourcePath, url);
+    return url;
+  }
+
+  private static URL getDefaultsImpl(final Object requestor, final String componentResourcePath) {
     boolean isPathAbsoulte = StringUtil.startsWithChar(componentResourcePath, '/');
     if (isPathAbsoulte) {
       return requestor.getClass().getResource(componentResourcePath + XML_EXTENSION);
