1edefda42fb7a6567187fd1201358cf67220bd43
Bas Leijdekkers <basleijdekkers@gmail.com>
2008-05-05 17:36:13 +0400

    IDEADEV-14704

---
+++ plugins/IntentionPowerPak/src/com/siyeh/ipp/concatenation/ReplaceConcatenationWithFormatStringIntention.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright 2008 Bas Leijdekkers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.siyeh.ipp.concatenation;
+
+import com.siyeh.ipp.base.Intention;
+import com.siyeh.ipp.base.PsiElementPredicate;
+import com.siyeh.ipp.psiutils.ConcatenationUtils;
+import com.intellij.psi.*;
+import com.intellij.util.IncorrectOperationException;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ReplaceConcatenationWithFormatStringIntention
+        extends Intention {
+
+    @NotNull
+    protected PsiElementPredicate getElementPredicate() {
+        return new ReplaceConcatenationWithFormatStringPredicate();
+    }
+
+    protected void processIntention(@NotNull PsiElement element)
+            throws IncorrectOperationException {
+        PsiBinaryExpression expression =
+                (PsiBinaryExpression)element;
+        PsiElement parent = expression.getParent();
+        while (ConcatenationUtils.isConcatenation(parent)) {
+            expression = (PsiBinaryExpression)parent;
+            assert expression != null;
+            parent = expression.getParent();
+        }
+
+        final StringBuilder formatString = new StringBuilder();
+        final List<PsiExpression> formatParameters = new ArrayList();
+        buildFormatString(expression, formatString, formatParameters);
+        if (replaceWithPrintfExpression(expression, formatString,
+                formatParameters)) {
+            return;
+        }
+        final StringBuilder newExpression = new StringBuilder();
+        newExpression.append("java.lang.String.format(\"");
+        newExpression.append(formatString);
+        newExpression.append('\"');
+        for (PsiExpression formatParameter : formatParameters) {
+            newExpression.append(", ");
+            newExpression.append(formatParameter.getText());
+        }
+        newExpression.append(")");
+        replaceExpression(newExpression.toString(), expression);
+    }
+
+    private static boolean replaceWithPrintfExpression(
+            PsiBinaryExpression expression,
+            CharSequence formatString, 
+            List<PsiExpression> formatParameters)
+            throws IncorrectOperationException {
+        final PsiElement expressionParent = expression.getParent();
+        if (!(expressionParent instanceof PsiExpressionList)) {
+            return false;
+        }
+        final PsiElement grandParent = expressionParent.getParent();
+        if (!(grandParent instanceof PsiMethodCallExpression)) {
+            return false;
+        }
+        final PsiMethodCallExpression methodCallExpression =
+                (PsiMethodCallExpression) grandParent;
+        final PsiReferenceExpression methodExpression =
+                methodCallExpression.getMethodExpression();
+        final String name = methodExpression.getReferenceName();
+        boolean insertNewline = false;
+        if ("println".equals(name)) {
+            insertNewline = true;
+        } else if ("print".equals(name)) {
+            insertNewline = false;
+        } else {
+            return false;
+        }
+        final PsiMethod method = methodCallExpression.resolveMethod();
+        if (method == null) {
+            return false;
+        }
+        final PsiClass containingClass = method.getContainingClass();
+        final String qualifiedName = containingClass.getQualifiedName();
+        if (!"java.io.PrintStream".equals(qualifiedName) &&
+                !"java.io.Printwriter".equals(qualifiedName)) {
+            return false;
+        }
+        final StringBuilder newExpression = new StringBuilder();
+        final PsiExpression qualifier =
+                methodExpression.getQualifierExpression();
+        if (qualifier != null) {
+            newExpression.append(qualifier.getText());
+            newExpression.append('.');
+        }
+        newExpression.append("printf(\"");
+        newExpression.append(formatString);
+        if (insertNewline) {
+            newExpression.append("\\n");
+        }
+        newExpression.append('\"');
+        for (PsiExpression formatParameter : formatParameters) {
+            newExpression.append(", ");
+            newExpression.append(formatParameter.getText());
+        }
+        newExpression.append(")");
+        replaceExpression(newExpression.toString(), methodCallExpression);
+        return true;
+    }
+
+    void buildFormatString(PsiBinaryExpression expression,
+                           StringBuilder formatString,
+                           List<PsiExpression> formatParameters) {
+        final PsiExpression lhs = expression.getLOperand();
+        appendFormatString(lhs, formatString, formatParameters);
+        final PsiExpression rhs = expression.getROperand();
+        if (rhs != null) {
+            appendFormatString(rhs, formatString, formatParameters);
+        }
+    }
+
+    private void appendFormatString(PsiExpression lhs,
+                                    StringBuilder formatString,
+                                    List<PsiExpression> formatParameters) {
+        if (lhs instanceof PsiLiteralExpression) {
+            final String text = lhs.getText();
+            final int length = text.length();
+            if (length > 2) {
+                formatString.append(text.substring(1, length -1));
+            }
+        } else if (lhs instanceof PsiBinaryExpression) {
+            final PsiBinaryExpression binaryExpression =
+                    (PsiBinaryExpression) lhs;
+            buildFormatString(binaryExpression, formatString, formatParameters);
+        } else {
+            formatString.append("%s");
+            formatParameters.add(lhs);
+        }
+    }
+}
