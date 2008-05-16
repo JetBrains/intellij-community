f45e749cd43b8040ae4102008fe48f9745da32de
Maxim Shafirov <max@jetbrains.com>
2008-05-16 15:40:59 +0400

    Fix IG tests.

--- plugins/InspectionGadgets/testsrc/com/IGInspectionTestCase.java
+++ plugins/InspectionGadgets/testsrc/com/IGInspectionTestCase.java
@@ -1,7 +1,10 @@
 package com;
 
+import com.intellij.codeInspection.LocalInspectionTool;
+import com.intellij.codeInspection.ex.LocalInspectionToolWrapper;
 import com.intellij.openapi.application.PathManager;
 import com.intellij.testFramework.InspectionTestCase;
+import org.jetbrains.annotations.NonNls;
 
 /**
  * @author Alexey
@@ -9,5 +12,9 @@ import com.intellij.testFramework.InspectionTestCase;
 public abstract class IGInspectionTestCase extends InspectionTestCase {
   protected String getTestDataPath() {
       return PathManager.getHomePath() + "/svnPlugins/InspectionGadgets/test";
+  }
+
+  public void doTest(@NonNls final String folderName, final LocalInspectionTool tool) throws Exception {
+    super.doTest(folderName, new LocalInspectionToolWrapper(tool), "java 1.5");
   }
 }
