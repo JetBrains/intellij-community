50dbf3276ef42e2cc9f0984f8e315aa64e2277a1
Maxim Shafirov <max@jetbrains.com>
2008-05-16 18:57:00 +0400

    Broken testdata for ExtractSuperClassTest fixed.

--- testData/refactoring/extractSuperClass/extendsLibraryClass/after/Test.java
+++ testData/refactoring/extractSuperClass/extendsLibraryClass/after/Test.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public abstract class Test extends ArrayList {
+public class Test extends ArrayList {
     public Test(int initialCapacity) {
         super(initialCapacity);
     }
