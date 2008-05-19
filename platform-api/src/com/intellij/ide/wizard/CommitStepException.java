2c0ecc6e59c18510f85cf06c9b8f4427efa85de0
Dmitry Jemerov <yole@intellij.com>
2008-05-19 18:24:26 +0400

    not final for TFS plugin

--- platform-api/src/com/intellij/ide/wizard/CommitStepException.java
+++ platform-api/src/com/intellij/ide/wizard/CommitStepException.java
@@ -18,7 +18,7 @@ package com.intellij.ide.wizard;
 /**
  * @author Vladimir Kondratyev
  */
-public final class CommitStepException extends Exception{
+public class CommitStepException extends Exception{
   public CommitStepException(final String message) {
     super(message);
   }
