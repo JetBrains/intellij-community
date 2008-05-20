47078e1c82849f3dce0253149749d944ff32a3a0
Bas Leijdekkers <basleijdekkers@gmail.com>
2008-05-20 17:45:45 +0400

    IDEADEV-26722 add make synchronized quick fix

--- plugins/InspectionGadgets/src/com/siyeh/ig/threading/NonSynchronizedMethodOverridesSynchronizedMethodInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/threading/NonSynchronizedMethodOverridesSynchronizedMethodInspection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2007 Dave Griffith, Bas Leijdekkers
+ * Copyright 2003-2008 Dave Griffith, Bas Leijdekkers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,6 +18,9 @@ package com.siyeh.ig.threading;
 import com.siyeh.InspectionGadgetsBundle;
 import com.siyeh.ig.BaseInspection;
 import com.siyeh.ig.BaseInspectionVisitor;
+import com.siyeh.ig.InspectionGadgetsFix;
+import com.siyeh.ig.fixes.ChangeModifierFix;
+import com.intellij.psi.PsiModifier;
 import org.jetbrains.annotations.NotNull;
 
 public class NonSynchronizedMethodOverridesSynchronizedMethodInspection
@@ -33,6 +36,11 @@ public class NonSynchronizedMethodOverridesSynchronizedMethodInspection
     protected String buildErrorString(Object... infos) {
         return InspectionGadgetsBundle.message(
                 "non.synchronized.method.overrides.synchronized.method.problem.descriptor");
+    }
+
+    @Override
+    protected InspectionGadgetsFix buildFix(Object... infos) {
+        return new ChangeModifierFix(PsiModifier.SYNCHRONIZED);
     }
 
     public BaseInspectionVisitor buildVisitor() {
