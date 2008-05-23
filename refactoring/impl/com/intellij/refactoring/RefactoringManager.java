09021c6f275dda16f33dc40f08b99390c2e432ae
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2008-05-23 19:12:53 +0400

    restore migration action

--- refactoring/impl/com/intellij/refactoring/RefactoringManager.java
+++ refactoring/impl/com/intellij/refactoring/RefactoringManager.java
@@ -3,11 +3,9 @@ package com.intellij.refactoring;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.refactoring.migration.MigrationManager;
-import com.intellij.refactoring.typeMigration.ui.TypeMigrationDialog;
 
 public class RefactoringManager {
   private MigrationManager myMigrateManager;
-  private TypeMigrationDialog myMigrationDialog;
 
   public static RefactoringManager getInstance(Project project) {
     return ServiceManager.getService(project, RefactoringManager.class);
@@ -15,22 +13,10 @@ public class RefactoringManager {
 
   public RefactoringManager(Project project) {
     myMigrateManager = new MigrationManager(project);
-    myMigrationDialog = null;
   }
 
   public MigrationManager getMigrateManager() {
     return myMigrateManager;
   }
 
-  public void startTypeMigrationSession(final TypeMigrationDialog d){
-    myMigrationDialog = d;
-  }
-
-  public TypeMigrationDialog getTypeMigrationDialog (){
-    return myMigrationDialog;
-  }
-
-  public void endTypeMigrationSession(){
-    myMigrationDialog = null;
-  }
 }
