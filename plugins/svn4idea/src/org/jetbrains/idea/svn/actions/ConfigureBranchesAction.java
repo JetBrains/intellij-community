36a9b3c758877c40292d8f3eb83083f1e94d316e
Irina Chernushina <Irina.Chernushina@jetbrains.com>
2008-05-27 16:08:01 +0400

    IDEADEV-26888 additional fix (NPE at org.jetbrains.idea.svn.dialogs.BranchConfigurationDialog.configureBranches)

--- plugins/svn4idea/src/org/jetbrains/idea/svn/actions/ConfigureBranchesAction.java
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/actions/ConfigureBranchesAction.java
@@ -39,7 +39,8 @@ public class ConfigureBranchesAction extends AnAction {
     final Project project = PlatformDataKeys.PROJECT.getData(e.getDataContext());
     final ChangeList[] cls = e.getData(VcsDataKeys.CHANGE_LISTS);
     if ((cls == null) || (cls.length == 0) ||
-        (! SvnVcs.getInstance(project).getName().equals(((CommittedChangeList) cls[0]).getVcs().getName()))) {
+        (! SvnVcs.getInstance(project).getName().equals(((CommittedChangeList) cls[0]).getVcs().getName())) ||
+        (((SvnChangeList) cls[0]).getVcsRoot() == null)) {
       return;
     }
     final SvnChangeList svnList = (SvnChangeList) cls[0];
