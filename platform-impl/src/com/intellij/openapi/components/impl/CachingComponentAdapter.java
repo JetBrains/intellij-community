2c7552c77ca8ec086f13cc4b8afae629b10c6fcd
Alexey Kudravtsev <cdr@jetbrains.com>
2008-05-27 18:21:07 +0400

    performance: use DCL instead of readwrite locks

--- platform-impl/src/com/intellij/openapi/components/impl/CachingComponentAdapter.java
+++ platform-impl/src/com/intellij/openapi/components/impl/CachingComponentAdapter.java
@@ -1,63 +1,44 @@
 package com.intellij.openapi.components.impl;
 
-import com.intellij.util.concurrency.JBLock;
-import com.intellij.util.concurrency.JBReentrantReadWriteLock;
-import com.intellij.util.concurrency.LockFactory;
-import org.picocontainer.*;
-import org.picocontainer.defaults.*;
+import org.picocontainer.ComponentAdapter;
+import org.picocontainer.PicoContainer;
+import org.picocontainer.PicoInitializationException;
+import org.picocontainer.PicoIntrospectionException;
+import org.picocontainer.defaults.AssignabilityRegistrationException;
+import org.picocontainer.defaults.DecoratingComponentAdapter;
+import org.picocontainer.defaults.LifecycleStrategy;
+import org.picocontainer.defaults.NotConcreteRegistrationException;
 
 /**
  * @author mike
  */
-public class CachingComponentAdapter extends DecoratingComponentAdapter implements LifecycleManager {
-
-  private ObjectReference instanceReference;
+public class CachingComponentAdapter extends DecoratingComponentAdapter {
+  private volatile Object cached;
   private boolean disposed;
   private boolean started;
   private boolean delegateHasLifecylce;
-  private JBReentrantReadWriteLock rw = LockFactory.createReadWriteLock();
-  private JBLock r = rw.readLock();
-  private JBLock w = rw.writeLock();
+  private final Object lock = new Object();
 
   public CachingComponentAdapter(ComponentAdapter delegate) {
-    this(delegate, new SimpleReference());
-  }
-
-  public CachingComponentAdapter(ComponentAdapter delegate, ObjectReference instanceReference) {
     super(delegate);
-    this.instanceReference = instanceReference;
     disposed = false;
     started = false;
     delegateHasLifecylce =
       delegate instanceof LifecycleStrategy && ((LifecycleStrategy)delegate).hasLifecycle(delegate.getComponentImplementation());
   }
 
-
   public Object getComponentInstance(PicoContainer container)
     throws PicoInitializationException, PicoIntrospectionException, AssignabilityRegistrationException, NotConcreteRegistrationException {
 
-    r.lock();
-    Object instance;
-    try {
-      instance = instanceReference.get();
-      if (instance != null) return instance;
+    Object o = cached;
+    if (o != null) return o;
+    synchronized (lock) {
+      o = cached;
+      if (o != null) return o;
+      o = super.getComponentInstance(container);
+      cached = o;
     }
-    finally {
-      r.unlock();
-    }
-
-    w.lock();
-    try {
-      instance = instanceReference.get();
-      if (instance == null) {
-        instance = super.getComponentInstance(container);
-        instanceReference.set(instance);
-      }
-      return instance;
-    }
-    finally {
-      w.unlock();
-    }
+    return o;
   }
 
   /**
