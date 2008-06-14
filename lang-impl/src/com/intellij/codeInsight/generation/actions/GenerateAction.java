ee8b10dfea1b5cfe11b1b80c6f463fb470f1bc5d
Kirill Kalishev <Kirill.Kalishev@jetbrains.com>
2008-06-14 16:35:32 +0400

    GenerateAction's show() is back to context sensetivity

--- lang-impl/src/com/intellij/codeInsight/generation/actions/GenerateAction.java
+++ lang-impl/src/com/intellij/codeInsight/generation/actions/GenerateAction.java
@@ -19,7 +19,7 @@ public class GenerateAction extends AnAction {
                                                           JBPopupFactory.ActionSelectionAid.SPEEDSEARCH,
                                                           false);
 
-    popup.showInFocusCenter();
+    popup.showInBestPositionFor(dataContext);
   }
 
   public void update(AnActionEvent event){
