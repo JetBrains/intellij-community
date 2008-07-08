f96001ba4ee7c7ce5900a15810a13580d939239f
Dmitry Jemerov <yole@intellij.com>
2008-07-08 17:34:34 +0400

    RenameCMPFieldTest fixed

--- lang-impl/src/com/intellij/refactoring/rename/RenameUtil.java
+++ lang-impl/src/com/intellij/refactoring/rename/RenameUtil.java
@@ -57,7 +57,7 @@ public class RenameUtil {
 
     if (searchInStringsAndComments && !(element instanceof PsiDirectory)) {
       String stringToSearch = ElementDescriptionUtil.getElementDescription(element, NonCodeSearchDescriptionLocation.STRINGS_AND_COMMENTS);
-      if (stringToSearch != null) {
+      if (stringToSearch != null && stringToSearch.length() > 0) {
         final String stringToReplace = getStringToReplace(element, newName, false, processor);
         TextOccurrencesUtil.UsageInfoFactory factory = new NonCodeUsageInfoFactory(element, stringToReplace);
         TextOccurrencesUtil.addUsagesInStringsAndComments(element, stringToSearch, result, factory);
