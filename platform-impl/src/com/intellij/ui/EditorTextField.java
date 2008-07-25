8c61786cafe6d5006bb926f9ddae275e609095ac
greg <Gregory.Shrago@jetbrains.com>
2008-07-25 20:20:05 +0400

    SQL dialect-related highlighting fix

--- platform-impl/src/com/intellij/ui/EditorTextField.java
+++ platform-impl/src/com/intellij/ui/EditorTextField.java
@@ -322,7 +322,7 @@ public class EditorTextField extends JPanel implements DocumentListener, TextCom
 
     setupEditorFont(editor);
 
-    if (myProject != null) {
+    if (myProject != null && myFileType != null) {
       editor.setHighlighter(EditorHighlighterFactory.getInstance().createEditorHighlighter(myProject, myFileType));
     }
     editor.getColorsScheme().setColor(EditorColors.CARET_ROW_COLOR, null);
