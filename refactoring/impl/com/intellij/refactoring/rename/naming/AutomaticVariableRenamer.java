f73fcdcc747f38bf4dd1bdc2cd91011828486f0c
Dmitry Jemerov <yole@intellij.com>
2008-08-02 17:24:41 +0400

    AutomaticVariableRenamer correctly processes multiple variables/fields in same declaration (IDEADEV-9213)

--- refactoring/impl/com/intellij/refactoring/rename/naming/AutomaticVariableRenamer.java
+++ refactoring/impl/com/intellij/refactoring/rename/naming/AutomaticVariableRenamer.java
@@ -1,15 +1,21 @@
 package com.intellij.refactoring.rename.naming;
 
+import com.intellij.lang.ASTNode;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.*;
 import com.intellij.psi.codeStyle.JavaCodeStyleManager;
+import com.intellij.psi.impl.source.tree.JavaElementType;
+import com.intellij.psi.impl.source.tree.StdTokenSets;
+import com.intellij.psi.impl.source.tree.TreeUtil;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.refactoring.RefactoringBundle;
 import com.intellij.usageView.UsageInfo;
 import com.intellij.util.containers.HashSet;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Set;
+import java.util.ArrayList;
 
 /**
  * @author dsl
@@ -21,34 +27,70 @@ public class AutomaticVariableRenamer extends AutomaticRenamer {
     for (final UsageInfo info : usages) {
       final PsiElement element = info.getElement();
       if (!(element instanceof PsiJavaCodeReferenceElement)) continue;
-      final PsiVariable variable = PsiTreeUtil.getParentOfType(element, PsiVariable.class);
-      if (variable == null) continue;
-      final PsiTypeElement typeElement = variable.getTypeElement();
-      if (typeElement == null) continue;
-      final PsiJavaCodeReferenceElement ref = typeElement.getInnermostComponentReferenceElement();
-      if (ref == null) continue;
-      if (ref.equals(element)) {
-        myElements.add(variable);
-        if (variable.getType() instanceof PsiArrayType) {
-          myToUnpluralize.add(variable);
+      final PsiDeclarationStatement statement = PsiTreeUtil.getParentOfType(element, PsiDeclarationStatement.class);
+      if (statement != null) {
+        for(PsiElement declaredElement: statement.getDeclaredElements()) {
+          if (declaredElement instanceof PsiVariable) {
+            checkRenameVariable(element, (PsiVariable) declaredElement);
+          }
         }
       }
       else {
-        PsiType collectionType = JavaPsiFacade.getInstance(variable.getProject()).getElementFactory()
-          .createTypeByFQClassName("java.util.Collection", variable.getResolveScope());
-        if (!collectionType.isAssignableFrom(variable.getType())) continue;
-        final PsiTypeElement[] typeParameterElements = ref.getParameterList().getTypeParameterElements();
-        for (PsiTypeElement typeParameterElement : typeParameterElements) {
-          final PsiJavaCodeReferenceElement parameterRef = typeParameterElement.getInnermostComponentReferenceElement();
-          if (parameterRef != null && parameterRef.equals(element)) {
-            myElements.add(variable);
-            myToUnpluralize.add(variable);
-            break;
+        PsiVariable variable = PsiTreeUtil.getParentOfType(element, PsiVariable.class);
+        if (variable != null) {
+          checkRenameVariable(element, variable);
+          if (variable instanceof PsiField) {
+            for(PsiField field: getFieldsInSameDeclaration((PsiField) variable)) {
+              checkRenameVariable(element, field);
+            }
           }
         }
       }
     }
     suggestAllNames(aClass.getName(), newClassName);
+  }
+
+  private static List<PsiField> getFieldsInSameDeclaration(final PsiField variable) {
+    List<PsiField> result = new ArrayList<PsiField>();
+    ASTNode node = variable.getNode();
+    if (node != null) {
+      while (true) {
+        ASTNode comma = TreeUtil.skipElements(node.getTreeNext(), StdTokenSets.WHITE_SPACE_OR_COMMENT_BIT_SET);
+        if (comma == null || comma.getElementType() != JavaTokenType.COMMA) break;
+        ASTNode nextField = TreeUtil.skipElements(comma.getTreeNext(), StdTokenSets.WHITE_SPACE_OR_COMMENT_BIT_SET);
+        if (nextField == null || nextField.getElementType() != JavaElementType.FIELD) break;
+        result.add((PsiField) nextField.getPsi());
+        node = nextField;
+      }
+    }
+    return result;
+  }
+
+  private void checkRenameVariable(final PsiElement element, final PsiVariable variable) {
+    final PsiTypeElement typeElement = variable.getTypeElement();
+    if (typeElement == null) return;
+    final PsiJavaCodeReferenceElement ref = typeElement.getInnermostComponentReferenceElement();
+    if (ref == null) return;
+    if (ref.equals(element)) {
+      myElements.add(variable);
+      if (variable.getType() instanceof PsiArrayType) {
+        myToUnpluralize.add(variable);
+      }
+    }
+    else {
+      PsiType collectionType = JavaPsiFacade.getInstance(variable.getProject()).getElementFactory()
+        .createTypeByFQClassName("java.util.Collection", variable.getResolveScope());
+      if (!collectionType.isAssignableFrom(variable.getType())) return;
+      final PsiTypeElement[] typeParameterElements = ref.getParameterList().getTypeParameterElements();
+      for (PsiTypeElement typeParameterElement : typeParameterElements) {
+        final PsiJavaCodeReferenceElement parameterRef = typeParameterElement.getInnermostComponentReferenceElement();
+        if (parameterRef != null && parameterRef.equals(element)) {
+          myElements.add(variable);
+          myToUnpluralize.add(variable);
+          break;
+        }
+      }
+    }
   }
 
   public String getDialogTitle() {
