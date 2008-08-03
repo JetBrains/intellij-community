20724a261224ee2559d11ac1fda9dace7f0c601f
Kirill Kalishev <Kirill.Kalishev@jetbrains.com>
2008-08-03 16:59:42 +0400

    IDEADEV-28772 - NPE: PasteFromX11Action$Handler.executeWriteAction

--- platform-impl/src/com/intellij/openapi/editor/actions/PasteFromX11Action.java
+++ platform-impl/src/com/intellij/openapi/editor/actions/PasteFromX11Action.java
@@ -4,13 +4,16 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.EditorModificationUtil;
 import com.intellij.openapi.editor.actionSystem.EditorAction;
 import com.intellij.openapi.editor.actionSystem.EditorWriteActionHandler;
 import com.intellij.openapi.editor.event.EditorMouseEventArea;
 import com.intellij.openapi.editor.ex.EditorEx;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.SystemInfo;
+import com.intellij.openapi.diagnostic.Logger;
 
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.Transferable;
@@ -21,6 +24,9 @@ import java.awt.event.MouseEvent;
  * Author: msk
  */
 public class PasteFromX11Action extends EditorAction {
+
+  private static final Logger LOG = Logger.getInstance("#com.intellij.openapi.editor.actions.PasteFromX11Action");
+
   public PasteFromX11Action() {
     super(new Handler());
   }
@@ -46,7 +52,17 @@ public class PasteFromX11Action extends EditorAction {
     public void executeWriteAction(Editor editor, DataContext dataContext) {
       final Clipboard clip = editor.getComponent().getToolkit().getSystemSelection();
       if (clip != null) {
-        final Transferable res = clip.getContents(null);
+        Transferable res = null;
+        try {
+          res = clip.getContents(null);
+        }
+        catch (Exception e) {
+          if (!ApplicationManager.getApplication().isHeadlessEnvironment()) {
+            LOG.info(e);
+            Messages.showErrorDialog(editor.getProject(), "Cannot paste from X11 clipboard: " + e.getLocalizedMessage(), "Cannot Paste");
+            return;
+          }
+        }
         editor.putUserData(EditorEx.LAST_PASTED_REGION, EditorModificationUtil.pasteFromTransferrable(res, editor));
       }
     }
