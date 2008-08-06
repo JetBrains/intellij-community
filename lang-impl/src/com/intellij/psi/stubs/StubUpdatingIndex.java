e82c0334795da0bbe27bb1fc6589e4808166ce97
Maxim Mossienko <Maxim.Mossienko@jetbrains.com>
2008-08-06 14:20:28 +0400

    caches version bumped - JS tree serialization updated

--- lang-impl/src/com/intellij/psi/stubs/StubUpdatingIndex.java
+++ lang-impl/src/com/intellij/psi/stubs/StubUpdatingIndex.java
@@ -40,7 +40,7 @@ public class StubUpdatingIndex implements CustomImplementationFileBasedIndexExte
   private static final Logger LOG = Logger.getInstance("#com.intellij.psi.stubs.StubUpdatingIndex");
 
   public static final ID<Integer, SerializedStubTree> INDEX_ID = ID.create("Stubs");
-  private static final int VERSION = 9;
+  private static final int VERSION = 10;
   private static final DataExternalizer<SerializedStubTree> KEY_EXTERNALIZER = new DataExternalizer<SerializedStubTree>() {
     public void save(final DataOutput out, final SerializedStubTree v) throws IOException {
       byte[] value = v.getBytes();
