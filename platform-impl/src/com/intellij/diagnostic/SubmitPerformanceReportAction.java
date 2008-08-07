9e9a93d71f9e626177d068899b6265e3e6fdba2b
Dmitry Jemerov <yole@intellij.com>
2008-08-07 10:04:33 +0400

    automatic upload of performance reports to FTP

--- platform-impl/src/com/intellij/diagnostic/SubmitPerformanceReportAction.java
+++ platform-impl/src/com/intellij/diagnostic/SubmitPerformanceReportAction.java
@@ -4,16 +4,22 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.application.ApplicationInfo;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.PathManager;
-import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.util.SystemProperties;
 import com.intellij.util.io.ZipUtil;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.io.File;
-import java.io.FileFilter;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -48,7 +54,74 @@ public class SubmitPerformanceReportAction extends AnAction {
                                "Submit Performance Report");
       return;
     }
-    Messages.showMessageDialog(project, "The performance report has been saved to " + reportPath, "Submit Performance Report",
-                               Messages.getInformationIcon());
+    int rc = Messages.showYesNoDialog(project, "The performance report has been saved to " + reportPath +
+                                               "\nWould you like to submit it to JetBrains?",
+                                      "Submit Performance Report",
+                                      Messages.getInformationIcon());
+    if (rc == 0) {
+      ProgressManager.getInstance().run(new Task.Backgroundable(project, "Uploading Performance Report") {
+        public void run(@NotNull final ProgressIndicator indicator) {
+          final String error = uploadFileToFTP(reportPath, "ftp.intellij.net", ".uploads", indicator);
+          if (error != null) {
+            ApplicationManager.getApplication().invokeLater(new Runnable() {
+              public void run() {
+                Messages.showErrorDialog(project, error, "Submit Performance Report");
+              }
+            });
+          }
+
+        }
+      });
+    }
+  }
+
+  @Nullable
+  private static String uploadFileToFTP(final File reportPath, @NonNls final String ftpSite, @NonNls final String directory,
+                                        final ProgressIndicator indicator) {
+    FTPClient ftp = new FTPClient();
+    try {
+      ftp.connect(ftpSite);
+      indicator.setText("Connected to server");
+      if (!ftp.login("anonymous", "anonymous@jetbrains.com")) {
+        return "Failed to login";
+      }
+      indicator.setText("Logged in");
+
+      // After connection attempt, you should check the reply code to verify
+      // success.
+      int reply = ftp.getReplyCode();
+
+      if (!FTPReply.isPositiveCompletion(reply)) {
+        ftp.disconnect();
+        return "FTP server refused connection";
+      }
+      if (!ftp.changeWorkingDirectory(directory)) {
+        return "Failed to change directory";
+      }
+      FileInputStream readStream = new FileInputStream(reportPath);
+      try {
+        if (!ftp.storeFile(reportPath.getName(), readStream)) {
+          return "Failed to upload file";
+        }
+      }
+      finally {
+        readStream.close();
+      }
+      ftp.logout();
+    }
+    catch (IOException e) {
+      return e.getMessage();
+    }
+    finally {
+      if (ftp.isConnected()) {
+        try {
+          ftp.disconnect();
+        }
+        catch (IOException ioe) {
+          // do nothing
+        }
+      }
+    }
+    return null;
   }
 }
