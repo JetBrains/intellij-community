ab888a48c56885a790c52105cca135e3416da04a
Constantine Plotnikov <Constantine.Plotnikov@jetbrains.com>
2008-08-14 13:17:21 +0400

    Fixed re-export in compile classpath.

--- compiler/impl/com/intellij/compiler/ant/ModuleChunkClasspath.java
+++ compiler/impl/com/intellij/compiler/ant/ModuleChunkClasspath.java
@@ -19,41 +19,60 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * Generate module chunk classspath. The class used to generate both runtime and compile time classpaths.
+ * Generator of module chunk classspath. The class used to generate both runtime and compile time classpaths.
+ *
  * @author Eugene Zhuravlev
- *         Date: Nov 22, 2004
  */
 public class ModuleChunkClasspath extends Path{
 
+  /**
+   * A constructor
+   * @param chunk a chunk to process
+   * @param genOptions a generation options
+   * @param generateRuntimeClasspath if true, runtime classpath is being generated. Otherwise a compile time classpath is constructed
+   */
+  @SuppressWarnings({"unchecked"})
   public ModuleChunkClasspath(final ModuleChunk chunk, final GenerationOptions genOptions, final boolean generateRuntimeClasspath) {
     super(generateRuntimeClasspath? BuildProperties.getRuntimeClasspathProperty(chunk.getName()) : BuildProperties.getClasspathProperty(chunk.getName()));
 
     final OrderedSet<ClasspathItem> pathItems = new OrderedSet<ClasspathItem>((TObjectHashingStrategy<ClasspathItem>)TObjectHashingStrategy.CANONICAL);
     final String moduleChunkBasedirProperty = BuildProperties.getModuleChunkBasedirProperty(chunk);
     final Module[] modules = chunk.getModules();
-    /// processed chunks (used only in runtime classpath), every chunk is referenced exactly once
+    // processed chunks (used only in runtime classpath), every chunk is referenced exactly once
     final Set<ModuleChunk> processedChunks = new HashSet<ModuleChunk>();
+    // pocessed modules
+    final Set<Module> processedModules = new HashSet<Module>();
     for (final Module module : modules) {
       new Object() {
-        /** pocessed modules */
-        final Set<Module> processed = new HashSet<Module>();
+        /**
+         * Process the module. The logic is different for compile-time case and runtime case.
+         * In the case of runtime, only directly referenced objects are included in classpath.
+         * Indirectly referenced are
+         *
+         * @param module a module to process.
+         * @param dependencyLevel is increased with every of recursion.
+         * @param isModuleExported if true the module is exported from the previous level
+         */
         public void processModule(final Module module, final int dependencyLevel, final boolean isModuleExported) {
-          if (processed.contains(module)) {
+          if (processedModules.contains(module)) {
+            // the module is already processed, nothing should be done
             return;
           }
-          processed.add(module);
+          if(dependencyLevel > 1 && !isModuleExported) {
+            // the module is not in exports and it is not directly included
+            return;
+          }
+          processedModules.add(module);
           for (final OrderEntry orderEntry : ModuleRootManager.getInstance(module).getOrderEntries()) {
             if (!orderEntry.isValid()) {
               continue;
             }
-            
             if (!generateRuntimeClasspath) {
               // needed for compilation classpath only
               if ((orderEntry instanceof ModuleSourceOrderEntry)) {
+                // this is the entry for outpath of the currently processed module
                 if (dependencyLevel == 0 || chunk.contains(module)) {
-                  continue;
-                }
-                if (dependencyLevel > 1 && !isModuleExported) {
+                  // the root module is never included
                   continue;
                 }
               }
@@ -61,12 +80,12 @@ public class ModuleChunkClasspath extends Path{
                 final boolean isExported = (orderEntry instanceof ExportableOrderEntry) && ((ExportableOrderEntry)orderEntry).isExported();
                 if (dependencyLevel > 0 && !isExported) {
                   if (!(orderEntry instanceof ModuleOrderEntry)) {
+                    // non-exported dependencies are excluded and not processed
                     continue;
                   }
                 }
               }
             }
-            
             if (orderEntry instanceof JdkOrderEntry) {
               if (genOptions.forceTargetJdk && !generateRuntimeClasspath) {
                 pathItems.add(new PathRefItem(BuildProperties.propertyRef(BuildProperties.getModuleChunkJdkClasspathProperty(chunk.getName()))));
@@ -77,13 +96,16 @@ public class ModuleChunkClasspath extends Path{
               final Module dependentModule = moduleOrderEntry.getModule();
               if (!chunk.contains(dependentModule)) {
                 if(generateRuntimeClasspath) {
+                  // in case of runtime classpath, just an referenced to corresponding classpath is created
                   final ModuleChunk depChunk = genOptions.getChunkByModule(dependentModule);
                   if(!processedChunks.contains(depChunk)) {
+                    // chunk references are included in the runtime classpath only once
                     processedChunks.add(depChunk);
                     pathItems.add(new PathRefItem(BuildProperties.getRuntimeClasspathProperty(depChunk.getName())));
                   }
                 }
                 else {
+                  // in case of compile classpath, the referenced module is processed recursively
                   processModule(dependentModule, dependencyLevel + 1, moduleOrderEntry.isExported());
                 }
               }
@@ -93,6 +115,7 @@ public class ModuleChunkClasspath extends Path{
               pathItems.add(new PathRefItem(BuildProperties.getLibraryPathId(libraryName)));
             }
             else {
+              // Module source entry?
               for (String url : getCompilationClasses(orderEntry, ((GenerationOptionsImpl)genOptions), generateRuntimeClasspath)) {
                 if (url.endsWith(JarFileSystem.JAR_SEPARATOR)) {
                   url = url.substring(0, url.length() - JarFileSystem.JAR_SEPARATOR.length());
@@ -109,10 +132,10 @@ public class ModuleChunkClasspath extends Path{
               }
             }
           }
-          processed.remove(module);
         }
       }.processModule(module, 0, false);
     }
+    // convert path items to generators
     for (final ClasspathItem pathItem : pathItems) {
       add(pathItem.toGenerator());
     }
@@ -130,25 +153,47 @@ public class ModuleChunkClasspath extends Path{
     return urls.toArray(new String[urls.size()]);
   }
 
+  /**
+   * The base class for an item in the class path. Instances of the subclasses are used instead
+   * of generators when building the class path content. The subclasses implement {@link Object#equals(Object)}
+   * and {@link Object#hashCode()} methods in order to eliminate duplicates when building classpath.
+   */
   private abstract static class ClasspathItem {
+    /** primary reference or path of the element */
     protected final String myValue;
 
+    /**
+     * A constructor
+     * @param value  primary value of the element
+     */
     public ClasspathItem(String value) {
       myValue = value;
     }
 
+    /**
+     * @return a generator for path elements.
+     */
     public abstract Generator toGenerator();
   }
 
+  /**
+   * This path element directly references some location.
+   */
   private static class PathElementItem extends ClasspathItem {
+    /**
+     * A constructor
+     * @param value a referenced location
+     */
     public PathElementItem(String value) {
       super(value);
     }
 
+    @Override
     public Generator toGenerator() {
       return new PathElement(myValue);
     }
 
+    @Override
     public boolean equals(Object o) {
       if (this == o) return true;
       if (!(o instanceof PathElementItem)) return false;
@@ -160,20 +205,30 @@ public class ModuleChunkClasspath extends Path{
       return true;
     }
 
+    @Override
     public int hashCode() {
       return myValue.hashCode();
     }
   }
 
+  /**
+   * This path element references a path
+   */
   private static class PathRefItem extends ClasspathItem {
+    /**
+     * A constructor
+     * @param value an indentifier of referenced classpath
+     */
     public PathRefItem(String value) {
       super(value);
     }
 
+    @Override
     public Generator toGenerator() {
       return new PathRef(myValue);
     }
 
+    @Override
     public boolean equals(Object o) {
       if (this == o) return true;
       if (!(o instanceof PathRefItem)) return false;
@@ -185,6 +240,7 @@ public class ModuleChunkClasspath extends Path{
       return true;
     }
 
+    @Override
     public int hashCode() {
       return myValue.hashCode();
     }
