4473c589c59f98d75d6a489a307a36355dcd32ca
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2008-08-22 18:46:18 +0400

    safe delete "extends" list: calc substitutor before import may be deleted

--- refactoring/impl/com/intellij/refactoring/safeDelete/usageInfo/SafeDeleteExtendsClassUsageInfo.java
+++ refactoring/impl/com/intellij/refactoring/safeDelete/usageInfo/SafeDeleteExtendsClassUsageInfo.java
@@ -11,10 +11,13 @@ import com.intellij.util.IncorrectOperationException;
 public class SafeDeleteExtendsClassUsageInfo extends SafeDeleteReferenceUsageInfo {
   private static final Logger LOG = Logger.getInstance("#com.intellij.refactoring.safeDelete.usageInfo.SafeDeleteExtendsClassUsageInfo");
   private final PsiClass myExtendingClass;
+  private final PsiSubstitutor mySubstitutor;
 
   public SafeDeleteExtendsClassUsageInfo(final PsiJavaCodeReferenceElement reference, PsiClass refClass, PsiClass extendingClass) {
     super(reference, refClass, true);
     myExtendingClass = extendingClass;
+    mySubstitutor = TypeConversionUtil.getClassSubstitutor(refClass, myExtendingClass, PsiSubstitutor.EMPTY);
+    LOG.assertTrue(mySubstitutor != null);
   }
 
   public PsiClass getReferencedElement() {
@@ -26,15 +29,14 @@ public class SafeDeleteExtendsClassUsageInfo extends SafeDeleteReferenceUsageInf
     LOG.assertTrue(parent instanceof PsiReferenceList);
     final PsiClass refClass = getReferencedElement();
     final PsiElementFactory elementFactory = JavaPsiFacade.getInstance(refClass.getProject()).getElementFactory();
-    final PsiSubstitutor substitutor = TypeConversionUtil.getClassSubstitutor(refClass, myExtendingClass, PsiSubstitutor.EMPTY);
-    LOG.assertTrue(substitutor != null);
+
     final PsiReferenceList extendsList = refClass.getExtendsList();
     final PsiReferenceList extendingImplementsList = myExtendingClass.getImplementsList();
     if (extendsList != null) {
       final PsiClassType[] referenceTypes = extendsList.getReferencedTypes();
       final PsiReferenceList listToAddExtends = refClass.isInterface() == myExtendingClass.isInterface() ? myExtendingClass.getExtendsList() : extendingImplementsList;
       for (PsiClassType referenceType : referenceTypes) {
-        listToAddExtends.add(elementFactory.createReferenceElementByType((PsiClassType)substitutor.substitute(referenceType)));
+        listToAddExtends.add(elementFactory.createReferenceElementByType((PsiClassType)mySubstitutor.substitute(referenceType)));
       }
     }
 
@@ -42,7 +44,7 @@ public class SafeDeleteExtendsClassUsageInfo extends SafeDeleteReferenceUsageInf
     if (implementsList != null) {
       final PsiClassType[] referenceTypes = implementsList.getReferencedTypes();
       for (PsiClassType referenceType : referenceTypes) {
-        extendingImplementsList.add(elementFactory.createReferenceElementByType((PsiClassType)substitutor.substitute(referenceType)));
+        extendingImplementsList.add(elementFactory.createReferenceElementByType((PsiClassType)mySubstitutor.substitute(referenceType)));
       }
     }
 
