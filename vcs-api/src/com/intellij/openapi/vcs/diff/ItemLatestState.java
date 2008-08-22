834e47bc0faa9b7d214e05cbb966fd61f8273872
Constantine Plotnikov <Constantine.Plotnikov@jetbrains.com>
2008-08-22 18:29:49 +0400

    Added javadoc to the class

--- vcs-api/src/com/intellij/openapi/vcs/diff/ItemLatestState.java
+++ vcs-api/src/com/intellij/openapi/vcs/diff/ItemLatestState.java
@@ -2,19 +2,37 @@ package com.intellij.openapi.vcs.diff;
 
 import com.intellij.openapi.vcs.history.VcsRevisionNumber;
 
+/**
+ * The latest state of the versioned item
+ */
 public class ItemLatestState {
+  /** version number */
   private final VcsRevisionNumber myNumber;
+  /** true if the item still exists in the remote reposistory */
   private final boolean myItemExists;
+
+  /**
+   * A constructor
+   *
+   * @param number version number
+   * @param itemExists true if the item still exists in the remote reposistory
+   */
 
   public ItemLatestState(final VcsRevisionNumber number, final boolean itemExists) {
     myNumber = number;
     myItemExists = itemExists;
   }
 
+  /**
+   * @return the latest version of the item
+   */
   public VcsRevisionNumber getNumber() {
     return myNumber;
   }
 
+  /**
+   * @return true if the item exists in the remote reposistory
+   */
   public boolean isItemExists() {
     return myItemExists;
   }
