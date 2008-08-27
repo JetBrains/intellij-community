3f56bc0d34f729c63c50be97a4623cda2ec29291
Dmitry Jemerov <yole@intellij.com>
2008-08-27 14:25:18 +0400

    refactor FileTypeFactory API; associate Rakefile with Ruby file type by default (RUBY-1756)

--- python/src/com/jetbrains/python/PythonFileTypeFactory.java
+++ python/src/com/jetbrains/python/PythonFileTypeFactory.java
@@ -1,8 +1,7 @@
 package com.jetbrains.python;
 
-import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.fileTypes.FileTypeConsumer;
 import com.intellij.openapi.fileTypes.FileTypeFactory;
-import com.intellij.util.PairConsumer;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
@@ -10,7 +9,7 @@ import org.jetbrains.annotations.NotNull;
  * @author yole
  */
 public class PythonFileTypeFactory extends FileTypeFactory {
-  public void createFileTypes(@NonNls @NotNull final PairConsumer<FileType, String> consumer) {
+  public void createFileTypes(@NonNls @NotNull final FileTypeConsumer consumer) {
     consumer.consume(PythonFileType.INSTANCE, "py");
   }
 }
