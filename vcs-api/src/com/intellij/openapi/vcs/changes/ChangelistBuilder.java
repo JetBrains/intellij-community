05040834df8fb7b3ae1994a9413091abb4cd97c8
Constantine Plotnikov <Constantine.Plotnikov@jetbrains.com>
2008-08-29 18:06:11 +0400

    Updated javadoc for some VCS interfaces

--- vcs-api/src/com/intellij/openapi/vcs/changes/ChangelistBuilder.java
+++ vcs-api/src/com/intellij/openapi/vcs/changes/ChangelistBuilder.java
@@ -21,23 +21,61 @@ import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.Nullable;
 
 /**
+ * Builder for the changes list in the file system. The instances of
+ * this class are used to collect changes that happened in the file system.
+ *
+ * @see com.intellij.openapi.vcs.changes.ChangeProvider#getChanges(VcsDirtyScope, ChangelistBuilder, com.intellij.openapi.progress.ProgressIndicator)
  * @author max
  */
 public interface ChangelistBuilder {
+  /**
+   * Process a change to the file. This method is used to report changes that
+   * version control system knows about.
+   *
+   * @param change a change to process.
+   */
   void processChange(Change change);
+
   void processChangeInList(Change change, @Nullable ChangeList changeList);
 
   /**
    * Put the given change into the change list with the given name.
    * If there is no such change list it is created.
    * This method allows not to refer to ChangeListManager for the LocalChangeList object.
-   * @param change Submitted change
+   *
+   * @param change         Submitted change
    * @param changeListName A name for a change list.
    */
-  void processChangeInList(Change change, String changeListName );
+  void processChangeInList(Change change, String changeListName);
+
+  /**
+   * Process a file that is not under version control.
+   *
+   * @param file a file to process
+   */
   void processUnversionedFile(VirtualFile file);
+
+  /**
+   * Process a file that was deleted locally, but version
+   * control has not been notified about removal yet.
+   *
+   * @param file a file to process
+   */
   void processLocallyDeletedFile(FilePath file);
+
+  /**
+   * Process the file that was modified without explicit checkout
+   * (if version control supports such behavior).
+   *
+   * @param file a file to process
+   */
   void processModifiedWithoutCheckout(VirtualFile file);
+
+  /**
+   * Process the file that is ignored by the version control.
+   *
+   * @param file an ignored file
+   */
   void processIgnoredFile(VirtualFile file);
 
   void processLockedFolder(VirtualFile file);
