f3e600121aecff1b36b5c43b1cf0da23bec67d4f
Alexey Kudravtsev <cdr@jetbrains.com>
2008-09-01 17:11:01 +0400

    more descriptive name

--- ExtendedApi/src/com/intellij/testFramework/IdeaTestCase.java
+++ ExtendedApi/src/com/intellij/testFramework/IdeaTestCase.java
@@ -154,7 +154,7 @@ import java.util.HashSet;
     LocalFileSystem.getInstance().refreshIoFiles(myFilesToDelete);
 
     myProject = myProjectManager.newProject(FileUtil.getNameWithoutExtension(projectFile), projectFile.getPath(), false, false);
-    myProject.putUserData(TESTCASE_NAME, getName());
+    myProject.putUserData(TESTCASE_NAME, getClass().getName() + "." + getName());
 
     setUpModule();
 
