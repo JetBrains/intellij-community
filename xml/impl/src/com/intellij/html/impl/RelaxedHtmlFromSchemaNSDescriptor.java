eef28af346e00ac44d84ac69795299d3a41562f6
Alexey Pegov <Alexey.Pegov@jetbrains.com>
2008-09-03 22:07:56 +0400

    IDEADEV-26043 logging added

--- xml/impl/src/com/intellij/html/impl/RelaxedHtmlFromSchemaNSDescriptor.java
+++ xml/impl/src/com/intellij/html/impl/RelaxedHtmlFromSchemaNSDescriptor.java
@@ -1,5 +1,7 @@
 package com.intellij.html.impl;
 
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.util.Key;
 import com.intellij.psi.xml.XmlDocument;
 import com.intellij.psi.xml.XmlTag;
 import com.intellij.util.ArrayUtil;
@@ -12,13 +14,20 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class RelaxedHtmlFromSchemaNSDescriptor extends XmlNSDescriptorImpl {
+  private static final Logger LOG = Logger.getInstance("#com.intellij.html.impl.RelaxedHtmlFromSchemaNSDescriptor");
+  private static final Key<Boolean> TEMP_DEBUG_KEY = Key.create("IDEADEV-26043");
+
   public XmlElementDescriptor getElementDescriptor(@NotNull XmlTag tag) {
     XmlElementDescriptor elementDescriptor = super.getElementDescriptor(tag);
 
-    if (elementDescriptor == null &&
-        !tag.getNamespace().equals(XmlUtil.XHTML_URI)
-       ) {
-      return new AnyXmlElementDescriptor(null,tag.getNSDescriptor(tag.getNamespace(),true));
+    if (elementDescriptor == null && !tag.getNamespace().equals(XmlUtil.XHTML_URI)) {
+      final Boolean data = tag.getUserData(TEMP_DEBUG_KEY);
+      if (data != null && !data.booleanValue()) {
+        LOG.error("Looks like we got an infinite loop for tag: " + tag.getText() + " and file: " + tag.getContainingFile().getText());
+      }
+
+      tag.putUserData(TEMP_DEBUG_KEY, data == null);
+      return new AnyXmlElementDescriptor(null, tag.getNSDescriptor(tag.getNamespace(), true));
     }
 
     return elementDescriptor;
@@ -30,10 +39,6 @@ public class RelaxedHtmlFromSchemaNSDescriptor extends XmlNSDescriptorImpl {
 
   @NotNull
   public XmlElementDescriptor[] getRootElementsDescriptors(@Nullable final XmlDocument doc) {
-    return ArrayUtil.mergeArrays(
-      super.getRootElementsDescriptors(doc),
-      HtmlUtil.getCustomTagDescriptors(doc),
-      XmlElementDescriptor.class
-    );
+    return ArrayUtil.mergeArrays(super.getRootElementsDescriptors(doc), HtmlUtil.getCustomTagDescriptors(doc), XmlElementDescriptor.class);
   }
 }
