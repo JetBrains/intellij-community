ab49fa23bd3640b4d2e13ed07c2655ea78d6bc26
Dmitry Jemerov <yole@intellij.com>
2008-09-04 18:46:47 +0400

    don't show "problem resolution" for empty list of fixes

--- lang-impl/src/com/intellij/codeInspection/ex/DescriptorComposer.java
+++ lang-impl/src/com/intellij/codeInspection/ex/DescriptorComposer.java
@@ -83,7 +83,7 @@ public class DescriptorComposer extends HTMLComposerImpl {
     if (refElement instanceof RefElement && !((RefElement)refElement).isValid()) return;
 
     final QuickFix[] fixes = descriptor.getFixes();
-    if (fixes != null) {
+    if (fixes != null && fixes.length > 0) {
       //noinspection HardCodedStringLiteral
       buf.append("<br><br>");
       appendHeading(buf, InspectionsBundle.message("inspection.problem.resolution"));
