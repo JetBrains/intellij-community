234cb1ed2ada8edcee1308d19ad189366e9ec4e7
Dmitry Jemerov <yole@intellij.com>
2008-09-08 21:08:06 +0400

    correct filtering of Ruby symbol contributors when includeNonProjectItems is enabled

---
+++ lang-api/src/com/intellij/psi/search/ProjectAndLibrariesScope.java
@@ -0,0 +1,76 @@
+package com.intellij.psi.search;
+
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.roots.*;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiBundle;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class ProjectAndLibrariesScope extends GlobalSearchScope {
+  protected final ProjectFileIndex myProjectFileIndex;
+
+  public ProjectAndLibrariesScope(final ProjectRootManager projectRootManager) {
+    myProjectFileIndex = projectRootManager.getFileIndex();
+  }
+
+  public boolean contains(VirtualFile file) {
+    return myProjectFileIndex.isInContent(file) ||
+           myProjectFileIndex.isInLibraryClasses(file) ||
+           myProjectFileIndex.isInLibrarySource(file);
+  }
+
+  public int compare(VirtualFile file1, VirtualFile file2) {
+    List<OrderEntry> entries1 = myProjectFileIndex.getOrderEntriesForFile(file1);
+    List<OrderEntry> entries2 = myProjectFileIndex.getOrderEntriesForFile(file2);
+    if (entries1.size() != entries2.size()) return 0;
+
+    int res = 0;
+    for (OrderEntry entry1 : entries1) {
+      Module module = entry1.getOwnerModule();
+      ModuleFileIndex moduleFileIndex = ModuleRootManager.getInstance(module).getFileIndex();
+      OrderEntry entry2 = moduleFileIndex.getOrderEntryForFile(file2);
+      if (entry2 == null) {
+        return 0;
+      }
+      else {
+        int aRes = entry2.compareTo(entry1);
+        if (aRes == 0) return 0;
+        if (res == 0) {
+          res = aRes;
+        }
+        else if (res != aRes) {
+          return 0;
+        }
+      }
+    }
+
+    return res;
+  }
+
+  public boolean isSearchInModuleContent(@NotNull Module aModule) {
+    return true;
+  }
+
+  public boolean isSearchInLibraries() {
+    return true;
+  }
+
+  public String getDisplayName() {
+    return PsiBundle.message("psi.search.scope.project.and.libraries");
+  }
+
+  @NotNull
+  public GlobalSearchScope intersectWith(@NotNull final GlobalSearchScope scope) {
+    return scope;
+  }
+
+  public GlobalSearchScope uniteWith(@NotNull final GlobalSearchScope scope) {
+    return this;
+  }
+
+  public String toString() {
+    return getDisplayName();
+  }
+}
--- lang-api/src/com/intellij/psi/search/ProjectScope.java
+++ lang-api/src/com/intellij/psi/search/ProjectScope.java
@@ -6,13 +6,12 @@ package com.intellij.psi.search;
 import com.intellij.injected.editor.VirtualFileWindow;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.*;
+import com.intellij.openapi.roots.ProjectFileIndex;
+import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiBundle;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.List;
 
 public class ProjectScope {
   private static final Key<GlobalSearchScope> ALL_SCOPE_KEY = new Key<GlobalSearchScope>("ALL_SCOPE_KEY");
@@ -30,68 +29,7 @@ public class ProjectScope {
         allScope = new EverythingGlobalScope();
       }
       else {
-        allScope = new GlobalSearchScope() {
-          final ProjectFileIndex myProjectFileIndex = projectRootManager.getFileIndex();
-
-          public boolean contains(VirtualFile file) {
-            return myProjectFileIndex.isInContent(file) ||
-                   myProjectFileIndex.isInLibraryClasses(file) ||
-                   myProjectFileIndex.isInLibrarySource(file);
-          }
-
-          public int compare(VirtualFile file1, VirtualFile file2) {
-            List<OrderEntry> entries1 = myProjectFileIndex.getOrderEntriesForFile(file1);
-            List<OrderEntry> entries2 = myProjectFileIndex.getOrderEntriesForFile(file2);
-            if (entries1.size() != entries2.size()) return 0;
-
-            int res = 0;
-            for (OrderEntry entry1 : entries1) {
-              Module module = entry1.getOwnerModule();
-              ModuleFileIndex moduleFileIndex = ModuleRootManager.getInstance(module).getFileIndex();
-              OrderEntry entry2 = moduleFileIndex.getOrderEntryForFile(file2);
-              if (entry2 == null) {
-                return 0;
-              }
-              else {
-                int aRes = entry2.compareTo(entry1);
-                if (aRes == 0) return 0;
-                if (res == 0) {
-                  res = aRes;
-                }
-                else if (res != aRes) {
-                  return 0;
-                }
-              }
-            }
-
-            return res;
-          }
-
-          public boolean isSearchInModuleContent(@NotNull Module aModule) {
-            return true;
-          }
-
-          public boolean isSearchInLibraries() {
-            return true;
-          }
-
-          public String getDisplayName() {
-            return PsiBundle.message("psi.search.scope.project.and.libraries");
-          }
-
-          @NotNull
-          public GlobalSearchScope intersectWith(@NotNull final GlobalSearchScope scope) {
-            return scope;
-          }
-
-          public GlobalSearchScope uniteWith(@NotNull final GlobalSearchScope scope) {
-            return this;
-          }
-
-          public String toString() {
-            return getDisplayName();
-          }
-        };
+        allScope = new ProjectAndLibrariesScope(projectRootManager);
       }
 
       project.putUserData(ALL_SCOPE_KEY, allScope);
@@ -143,4 +81,5 @@ public class ProjectScope {
     }
     return projectScope;
   }
+
 }
