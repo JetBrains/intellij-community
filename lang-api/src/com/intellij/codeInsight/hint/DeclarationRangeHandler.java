120941288021f847f0bc1b9da0a83af118cbcdcd
Dmitry Jemerov <yole@intellij.com>
2008-09-15 17:44:28 +0400

    declaration range handlers for Ruby (RUBY-1852)

--- lang-api/src/com/intellij/codeInsight/hint/DeclarationRangeHandler.java
+++ lang-api/src/com/intellij/codeInsight/hint/DeclarationRangeHandler.java
@@ -6,9 +6,19 @@ import com.intellij.psi.PsiElement;
 import com.intellij.util.MixinEP;
 import org.jetbrains.annotations.NotNull;
 
-public interface DeclarationRangeHandler {
+/**
+ * Returns the subset of the text range of the specified element which is considered its declaration.
+ * For example, the declaration range of a method includes its modifiers, return type, name and
+ * parameter list.
+ */
+public interface DeclarationRangeHandler<T extends PsiElement>{
   ExtensionPointName<MixinEP<DeclarationRangeHandler>> EP_NAME = ExtensionPointName.create("com.intellij.declarationRangeHandler");
-  
+
+  /**
+   * Returns the declaration range for the specified container.
+   * @param container the container
+   * @return the declaration range for it.
+   */
   @NotNull
-  TextRange getDeclarationRange(@NotNull PsiElement container);
+  TextRange getDeclarationRange(@NotNull T container);
 }
