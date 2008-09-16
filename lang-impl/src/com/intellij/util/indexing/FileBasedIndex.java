467238f8ab95a36b1eb4c544e9c6335f5fa1f879
Maxim Shafirov <max@jetbrains.com>
2008-09-16 14:18:45 +0400

    Race condition fixed.

--- lang-impl/src/com/intellij/util/indexing/FileBasedIndex.java
+++ lang-impl/src/com/intellij/util/indexing/FileBasedIndex.java
@@ -778,15 +778,19 @@ public class FileBasedIndex implements ApplicationComponent {
     assert index != null;
     
     index.update(inputId, currentFC, oldFC);
-    if (file.isValid()) {
-      if (currentFC != null) {
-        IndexingStamp.update(file, indexId, IndexInfrastructure.getIndexCreationStamp(indexId));
+    ApplicationManager.getApplication().runReadAction(new Runnable() {
+      public void run() {
+        if (file.isValid()) {
+          if (currentFC != null) {
+            IndexingStamp.update(file, indexId, IndexInfrastructure.getIndexCreationStamp(indexId));
+          }
+          else {
+            // mark the file as unindexed
+            IndexingStamp.update(file, indexId, -1L);
+          }
+        }
       }
-      else {
-        // mark the file as unindexed
-        IndexingStamp.update(file, indexId, -1L);
-      }
-    }
+    });
   }
 
   public static int getFileId(final VirtualFile file) {
