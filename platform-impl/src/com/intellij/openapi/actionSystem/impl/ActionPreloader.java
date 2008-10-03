61829a90239646ca66da862edd35834566cb3527
Dmitry Jemerov <yole@intellij.com>
2008-10-03 17:52:32 +0400

    do not preload actions in unit tests

--- platform-impl/src/com/intellij/openapi/actionSystem/impl/ActionPreloader.java
+++ platform-impl/src/com/intellij/openapi/actionSystem/impl/ActionPreloader.java
@@ -2,16 +2,19 @@ package com.intellij.openapi.actionSystem.impl;
 
 import com.intellij.openapi.startup.StartupManager;
 import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.application.ApplicationManager;
 
 /**
  * @author yole
  */
 public class ActionPreloader {
   public ActionPreloader(StartupManager manager) {
-    manager.registerPostStartupActivity(new Runnable() {
-      public void run() {
-        ((ActionManagerImpl)ActionManager.getInstance()).preloadActions();
-      }
-    });
+    if (!ApplicationManager.getApplication().isUnitTestMode()) {
+      manager.registerPostStartupActivity(new Runnable() {
+        public void run() {
+          ((ActionManagerImpl)ActionManager.getInstance()).preloadActions();
+        }
+      });
+    }
   }
 }
