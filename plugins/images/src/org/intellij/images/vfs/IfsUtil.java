11a393c7978bc1ede7828d371f321d4f1168d89a
Peter Gromov <peter@jetbrains.com>
2008-10-06 19:36:48 +0400

    IDEADEV-8362 All web mapped folder are displayed with dupes tails in Pictures frame

--- plugins/images/src/org/intellij/images/vfs/IfsUtil.java
+++ plugins/images/src/org/intellij/images/vfs/IfsUtil.java
@@ -20,7 +20,6 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectFileIndex;
 import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.util.Key;
-import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.reference.SoftReference;
@@ -35,7 +34,6 @@ import javax.imageio.ImageReader;
 import javax.imageio.stream.ImageInputStream;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Iterator;
@@ -46,102 +44,89 @@ import java.util.Iterator;
  * @author <a href="mailto:aefimov.box@gmail.com">Alexey Efimov</a>
  */
 public final class IfsUtil {
-    private static final Key<Long> TIMESTAMP_KEY = Key.create("Image.timeStamp");
-    private static final Key<String> FORMAT_KEY = Key.create("Image.format");
-    private static final Key<SoftReference<BufferedImage>> BUFFERED_IMAGE_REF_KEY = Key.create("Image.bufferedImage");
+  private static final Key<Long> TIMESTAMP_KEY = Key.create("Image.timeStamp");
+  private static final Key<String> FORMAT_KEY = Key.create("Image.format");
+  private static final Key<SoftReference<BufferedImage>> BUFFERED_IMAGE_REF_KEY = Key.create("Image.bufferedImage");
 
-    /**
-     * Load image data for file and put user data attributes into file.
-     *
-     * @param file File
-     * @return true if file image is loaded.
-     * @throws java.io.IOException if image can not be loaded
-     */
-    private static boolean refresh(@NotNull VirtualFile file) throws IOException {
-        Long loadedTimeStamp = file.getUserData(TIMESTAMP_KEY);
-        SoftReference<BufferedImage> imageRef = file.getUserData(BUFFERED_IMAGE_REF_KEY);
-        if (loadedTimeStamp == null || loadedTimeStamp.longValue() != file.getTimeStamp() || imageRef == null || imageRef.get() == null) {
+  /**
+   * Load image data for file and put user data attributes into file.
+   *
+   * @param file File
+   * @return true if file image is loaded.
+   * @throws java.io.IOException if image can not be loaded
+   */
+  private static boolean refresh(@NotNull VirtualFile file) throws IOException {
+    Long loadedTimeStamp = file.getUserData(TIMESTAMP_KEY);
+    SoftReference<BufferedImage> imageRef = file.getUserData(BUFFERED_IMAGE_REF_KEY);
+    if (loadedTimeStamp == null || loadedTimeStamp.longValue() != file.getTimeStamp() || imageRef == null || imageRef.get() == null) {
+      try {
+        final byte[] content = file.contentsToByteArray();
+        InputStream inputStream = new ByteArrayInputStream(content, 0, content.length);
+        ImageInputStream imageInputStream = ImageIO.createImageInputStream(inputStream);
+        try {
+          Iterator<ImageReader> imageReaders = ImageIO.getImageReaders(imageInputStream);
+          if (imageReaders.hasNext()) {
+            ImageReader imageReader = imageReaders.next();
             try {
-                final byte[] content = file.contentsToByteArray();
-                InputStream inputStream = new ByteArrayInputStream(content, 0, content.length);
-                ImageInputStream imageInputStream = ImageIO.createImageInputStream(inputStream);
-                try {
-                    Iterator<ImageReader> imageReaders = ImageIO.getImageReaders(imageInputStream);
-                    if (imageReaders.hasNext()) {
-                        ImageReader imageReader = imageReaders.next();
-                        try {
-                            file.putUserData(FORMAT_KEY, imageReader.getFormatName());
-                            ImageReadParam param = imageReader.getDefaultReadParam();
-                            imageReader.setInput(imageInputStream, true, true);
-                            int minIndex = imageReader.getMinIndex();
-                            BufferedImage image = imageReader.read(minIndex, param);
-                            file.putUserData(BUFFERED_IMAGE_REF_KEY, new SoftReference<BufferedImage>(image));
-                            return true;
-                        } finally {
-                            imageReader.dispose();
-                        }
-                    }
-                } finally {
-                    imageInputStream.close();
-                }
+              file.putUserData(FORMAT_KEY, imageReader.getFormatName());
+              ImageReadParam param = imageReader.getDefaultReadParam();
+              imageReader.setInput(imageInputStream, true, true);
+              int minIndex = imageReader.getMinIndex();
+              BufferedImage image = imageReader.read(minIndex, param);
+              file.putUserData(BUFFERED_IMAGE_REF_KEY, new SoftReference<BufferedImage>(image));
+              return true;
             } finally {
-                // We perform loading no more needed
-                file.putUserData(TIMESTAMP_KEY, file.getTimeStamp());
+              imageReader.dispose();
             }
+          }
+        } finally {
+          imageInputStream.close();
         }
-        return false;
+      } finally {
+        // We perform loading no more needed
+        file.putUserData(TIMESTAMP_KEY, file.getTimeStamp());
+      }
+    }
+    return false;
+  }
+
+  @Nullable
+  public static BufferedImage getImage(@NotNull VirtualFile file) throws IOException {
+    refresh(file);
+    SoftReference<BufferedImage> imageRef = file.getUserData(BUFFERED_IMAGE_REF_KEY);
+    return imageRef != null ? imageRef.get() : null;
+  }
+
+  @Nullable
+  public static String getFormat(@NotNull VirtualFile file) throws IOException {
+    refresh(file);
+    return file.getUserData(FORMAT_KEY);
+  }
+
+  public static String getReferencePath(Project project, VirtualFile file) {
+    final LogicalRoot logicalRoot = LogicalRootsManager.getLogicalRootsManager(project).findLogicalRoot(file);
+    if (logicalRoot != null) {
+      return getRelativePath(file, logicalRoot.getVirtualFile());
     }
 
-    @Nullable
-    public static BufferedImage getImage(@NotNull VirtualFile file) throws IOException {
-        refresh(file);
-        SoftReference<BufferedImage> imageRef = file.getUserData(BUFFERED_IMAGE_REF_KEY);
-        return imageRef != null ? imageRef.get() : null;
+    ProjectFileIndex fileIndex = ProjectRootManager.getInstance(project).getFileIndex();
+    VirtualFile sourceRoot = fileIndex.getSourceRootForFile(file);
+    if (sourceRoot != null) {
+      return getRelativePath(file, sourceRoot);
     }
 
-    @Nullable
-    public static String getFormat(@NotNull VirtualFile file) throws IOException {
-        refresh(file);
-        return file.getUserData(FORMAT_KEY);
+    VirtualFile root = fileIndex.getContentRootForFile(file);
+    if (root != null) {
+      return getRelativePath(file, root);
     }
 
-    public static String getReferencePath(Project project, VirtualFile file) {
-        File ioFile = VfsUtil.virtualToIoFile(file);
-        StringBuilder pathBuffer = new StringBuilder();
-        final LogicalRoot logicalRoot = LogicalRootsManager.getLogicalRootsManager(project).findLogicalRoot(file);
-        if (logicalRoot != null) {
-            File base = VfsUtil.virtualToIoFile(logicalRoot.getVirtualFile());
-            if (base.equals(ioFile)) {
-                pathBuffer.append(file.getPath());
-            } else {
-                pathBuffer.append("/");
-                pathBuffer.append(FileUtil.getRelativePath(base, ioFile));
-            }
-        }
-        ProjectRootManager projectRootManager = ProjectRootManager.getInstance(project);
-        ProjectFileIndex fileIndex = projectRootManager.getFileIndex();
-        VirtualFile sourceRoot = fileIndex.getSourceRootForFile(file);
-        if (sourceRoot != null) {
-            File base = VfsUtil.virtualToIoFile(sourceRoot);
-            if (base.equals(ioFile)) {
-                pathBuffer.append(file.getPath());
-            } else {
-                pathBuffer.append("/");
-                pathBuffer.append(FileUtil.getRelativePath(base, ioFile));
-            }
-        }
-        VirtualFile root = fileIndex.getContentRootForFile(file);
-        if (root != null) {
-            File base = VfsUtil.virtualToIoFile(root);
-            if (base.equals(ioFile)) {
-                pathBuffer.append(file.getPath());
-            } else {
-                pathBuffer.append("/");
-                pathBuffer.append(FileUtil.getRelativePath(base, ioFile));
-            }
-        } else {
-            pathBuffer.append(file.getPath());
-        }
-        return FileUtil.toSystemIndependentName(pathBuffer.toString());
+    return file.getPath();
+  }
+
+  private static String getRelativePath(final VirtualFile file, final VirtualFile root) {
+    if (root.equals(file)) {
+      return file.getPath();
     }
+    return "/" + VfsUtil.getRelativePath(file, root, '/');
+  }
 }
