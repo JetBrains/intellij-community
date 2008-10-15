f4a6f685e961c4d973d9f9232d0d9ef6d2ec7c3f
Alexey Kudravtsev <cdr@jetbrains.com>
2008-10-15 16:01:52 +0400

    bring test back

--- source/com/intellij/psi/impl/JavaPsiFacadeEx.java
+++ source/com/intellij/psi/impl/JavaPsiFacadeEx.java
@@ -6,11 +6,13 @@ package com.intellij.psi.impl;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFileFilter;
 import com.intellij.psi.JavaPsiFacade;
+import org.jetbrains.annotations.TestOnly;
 
 public abstract class JavaPsiFacadeEx extends JavaPsiFacade {
   public static JavaPsiFacadeEx getInstanceEx(Project project) {
     return (JavaPsiFacadeEx)getInstance(project);
   }
 
+  @TestOnly
   public abstract void setAssertOnFileLoadingFilter(final VirtualFileFilter filter);
 }
