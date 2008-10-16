5ef89073d00a19831b1fd89fdda8ff8315606f30
Alexey Pegov <Alexey.Pegov@jetbrains.com>
2008-10-16 17:50:48 +0400

    IDEADEV-7641 Ignore unmatched closed tags in html

--- plugins/IntelliLang/src/org/intellij/plugins/intelliLang/inject/CustomErrorElementFilter.java
+++ plugins/IntelliLang/src/org/intellij/plugins/intelliLang/inject/CustomErrorElementFilter.java
@@ -1,15 +1,20 @@
 package org.intellij.plugins.intelliLang.inject;
 
+import com.intellij.codeInsight.highlighting.HighlightErrorFilter;
 import com.intellij.lang.injection.InjectedLanguageManager;
-import com.intellij.openapi.util.Condition;
 import com.intellij.psi.PsiErrorElement;
 import com.intellij.psi.PsiLanguageInjectionHost;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Gregory.Shrago
  */
-public class CustomErrorElementFilter implements Condition<PsiErrorElement> {
-  public boolean value(final PsiErrorElement psiErrorElement) {
+public class CustomErrorElementFilter extends HighlightErrorFilter {
+  public boolean shouldHighlightErrorElement(@NotNull final PsiErrorElement element) {
+    return !value(element);
+  }
+
+  public static boolean value(final PsiErrorElement psiErrorElement) {
     final PsiLanguageInjectionHost host = InjectedLanguageManager.getInstance(psiErrorElement.getProject()).getInjectionHost(psiErrorElement);
     return host != null && Boolean.TRUE.equals(host.getUserData(CustomLanguageInjector.HAS_UNPARSABLE_FRAGMENTS));
   }
