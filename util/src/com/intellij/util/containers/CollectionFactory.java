5146786ad0d11d4d155d259d37af9c5391c0f3a3
Peter Gromov <peter@jetbrains.com>
2008-10-16 15:02:02 +0400

    support other platform components

---
+++ util/src/com/intellij/util/containers/CollectionFactory.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2000-2005 by JetBrains s.r.o. All Rights Reserved.
+ * Use is subject to license terms.
+ */
+package com.intellij.util.containers;
+
+import gnu.trove.THashSet;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Set;
+
+/**
+ * @author peter
+ */
+public class CollectionFactory {
+  private CollectionFactory() {
+  }
+
+  public static <T> Set<T> newTroveSet(T... elements) {
+    return newTroveSet(Arrays.asList(elements));
+  }
+
+  public static <T> Set<T> newTroveSet(Collection<T> elements) {
+    return new THashSet<T>(elements);
+  }
+
+  public static <T> T[] ar(T... elements) {
+    return elements;
+  }
+}
