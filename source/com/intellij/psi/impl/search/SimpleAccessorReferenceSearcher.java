4c227adeef1a984babf243ad5c9feb91ed1cd6cf
Alexey Kudravtsev <cdr@jetbrains.com>
2008-10-17 18:33:01 +0400

    IDEADEV-31471

--- source/com/intellij/psi/impl/search/SimpleAccessorReferenceSearcher.java
+++ source/com/intellij/psi/impl/search/SimpleAccessorReferenceSearcher.java
@@ -1,17 +1,18 @@
 package com.intellij.psi.impl.search;
 
-import com.intellij.psi.PsiReference;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiMethod;
-import com.intellij.psi.PsiManager;
-import com.intellij.psi.util.PropertyUtil;
-import com.intellij.psi.search.searches.ReferencesSearch;
-import com.intellij.psi.search.*;
-import com.intellij.util.QueryExecutor;
-import com.intellij.util.Processor;
-import com.intellij.openapi.fileTypes.StdFileTypes;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.fileTypes.StdFileTypes;
 import com.intellij.openapi.util.Computable;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.search.*;
+import com.intellij.psi.search.searches.ReferencesSearch;
+import com.intellij.psi.util.PropertyUtil;
+import com.intellij.util.Processor;
+import com.intellij.util.QueryExecutor;
 
 /**
  * @author ven
@@ -22,44 +23,38 @@ public class SimpleAccessorReferenceSearcher implements QueryExecutor<PsiReferen
     if (!(refElement instanceof PsiMethod)) return true;
     PsiMethod method = (PsiMethod)refElement;
     final String propertyName = PropertyUtil.getPropertyName(method);
-    if (propertyName != null) {
-      SearchScope searchScope = ApplicationManager.getApplication().runReadAction(new Computable<SearchScope>() {
-        public SearchScope compute() {
-          SearchScope searchScope = queryParameters.getEffectiveSearchScope();
-          if (searchScope instanceof GlobalSearchScope) {
-            searchScope = GlobalSearchScope.getScopeRestrictedByFileTypes((GlobalSearchScope)searchScope,
-                                                                          StdFileTypes.JSP,
-                                                                          StdFileTypes.JSPX,
-                                                                          StdFileTypes.XML,
-                                                                          StdFileTypes.XHTML);
-          }
-          return searchScope;
+    if (StringUtil.isEmptyOrSpaces(propertyName)) {
+      return true;
+    }
+    SearchScope searchScope = ApplicationManager.getApplication().runReadAction(new Computable<SearchScope>() {
+      public SearchScope compute() {
+        SearchScope searchScope = queryParameters.getEffectiveSearchScope();
+        if (searchScope instanceof GlobalSearchScope) {
+          searchScope = GlobalSearchScope.getScopeRestrictedByFileTypes((GlobalSearchScope)searchScope,
+                                                                        StdFileTypes.JSP,
+                                                                        StdFileTypes.JSPX,
+                                                                        StdFileTypes.XML,
+                                                                        StdFileTypes.XHTML);
         }
-      });
+        return searchScope;
+      }
+    });
 
-      final PsiSearchHelper helper = PsiManager.getInstance(refElement.getProject()).getSearchHelper();
-      final TextOccurenceProcessor processor = new TextOccurenceProcessor() {
-        public boolean execute(PsiElement element, int offsetInElement) {
-          final PsiReference[] refs = element.getReferences();
-          for (PsiReference ref : refs) {
-            if (ref.getRangeInElement().contains(offsetInElement)) {
-              if (ref.isReferenceTo(refElement)) {
-                return consumer.process(ref);
-              }
+    final PsiSearchHelper helper = PsiManager.getInstance(refElement.getProject()).getSearchHelper();
+    final TextOccurenceProcessor processor = new TextOccurenceProcessor() {
+      public boolean execute(PsiElement element, int offsetInElement) {
+        final PsiReference[] refs = element.getReferences();
+        for (PsiReference ref : refs) {
+          if (ref.getRangeInElement().contains(offsetInElement)) {
+            if (ref.isReferenceTo(refElement)) {
+              return consumer.process(ref);
             }
           }
-          return true;
         }
-      };
-
-      if (!helper.processElementsWithWord(processor,
-                                          searchScope,
-                                          propertyName,
-                                          UsageSearchContext.IN_FOREIGN_LANGUAGES,
-                                          false)) {
-        return false;
+        return true;
       }
-    }
-    return true;
+    };
+
+    return helper.processElementsWithWord(processor, searchScope, propertyName, UsageSearchContext.IN_FOREIGN_LANGUAGES, false);
   }
 }
