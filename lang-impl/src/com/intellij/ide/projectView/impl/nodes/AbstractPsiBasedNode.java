842a22caada49fa0ef721325faea5c44343038d0
Roman Chernyatchik <Roman.Chernyatchik@jetbrains.com>
2008-10-20 22:40:27 +0400

    method visibility was changed

--- lang-impl/src/com/intellij/ide/projectView/impl/nodes/AbstractPsiBasedNode.java
+++ lang-impl/src/com/intellij/ide/projectView/impl/nodes/AbstractPsiBasedNode.java
@@ -40,7 +40,7 @@ public abstract class AbstractPsiBasedNode<Value> extends ProjectViewNode<Value>
   }
 
   @Nullable
-  protected abstract PsiElement extractPsiFromValue();
+  public abstract PsiElement extractPsiFromValue();
   @Nullable
   protected abstract Collection<AbstractTreeNode> getChildrenImpl();
   protected abstract void updateImpl(final PresentationData data);
