ca1d512ac80a92f333bd755eb28ec40a83d8c97a
Dmitry Cheryasov <Dmitry.Cheryasov@jetbrains.com>
2008-10-22 10:14:39 +0400

    Fixes keyword completion somehow (PY-72, PY-76, PY-80). Still leaves much to be desired.

--- python/src/com/jetbrains/python/codeInsight/PyKeywordCompletionContributor.java
+++ python/src/com/jetbrains/python/codeInsight/PyKeywordCompletionContributor.java
@@ -11,6 +11,7 @@ import com.intellij.psi.filters.ElementFilter;
 import com.intellij.psi.filters.position.FilterPattern;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.ProcessingContext;
+import com.jetbrains.python.PyTokenTypes;
 import com.jetbrains.python.PythonLanguage;
 import com.jetbrains.python.psi.*;
 import org.jetbrains.annotations.NonNls;
@@ -26,18 +27,54 @@ public class PyKeywordCompletionContributor extends CompletionContributor {
     psiElement().afterLeaf(psiElement().andOr(psiElement().withText(".")))
   ;
 
+  protected static final PsiElementPattern.Capture<PsiElement> AFTER_DEF_OR_CLASS =
+    psiElement().afterLeaf(psiElement().andOr(
+        psiElement().withElementType(PyTokenTypes.DEF_KEYWORD), psiElement().withElementType(PyTokenTypes.CLASS_KEYWORD)
+    ))
+  ;
+
+  protected static final PsiElementPattern.Capture<PsiElement> IN_IMPORT_AFTER_REF =
+    psiElement().afterLeaf(psiElement().withElementType(PyTokenTypes.IDENTIFIER).inside(PyReferenceExpression.class).inside(PyImportElement.class))
+  ;
+
+  protected static final PsiElementPattern.Capture<PsiElement> IN_FROM_IMPORT_AFTER_REF =
+    psiElement().afterLeaf(psiElement().withElementType(PyTokenTypes.IDENTIFIER).inside(PyReferenceExpression.class).inside(PyFromImportStatement.class))
+  ;
+
+  protected static final PsiElementPattern.Capture<PsiElement> IN_LOOP_1 =
+    psiElement().andOr(
+      psiElement().inside(PyWhileStatement.class), psiElement().inside(PyForStatement.class)
+    ).andNot(psiElement().afterLeaf("for", "while")) // not working as expected: matches between "for" and ":" too.
+  ;
+
+  protected static final PsiElementPattern.Capture<PsiElement> IN_IF_COND =
+    psiElement().afterLeaf("if")
+  ;
+
+  protected static final PsiElementPattern.Capture<PsiElement> IN_EXPR_1 =
+    psiElement().inside(PyExpression.class)
+  ;
+
+
   /**
    * Allows element if it has a parent of given class(es).
    */
   protected static class ParentClassFilter implements ElementFilter {
     protected Class<? extends PsiElement>[] my_accepted_parents;
+    protected boolean my_inverted;
 
     protected ParentClassFilter(Class<? extends PsiElement>... parents) {
       my_accepted_parents = parents;
+      my_inverted = false;
+    }
+
+    protected ParentClassFilter(boolean straignt, Class<? extends PsiElement>... parents) {
+      my_accepted_parents = parents;
+      my_inverted = straignt;
     }
 
     public boolean isAcceptable(final Object element, final PsiElement context) {
-      return PsiTreeUtil.getParentOfType(context, my_accepted_parents) != null;
+      return /*my_inverted ^*/ (PsiTreeUtil.getParentOfType(context, my_accepted_parents) != null);
     }
 
     public boolean isClassAcceptable(final Class hintClass) {
@@ -47,9 +84,11 @@ public class PyKeywordCompletionContributor extends CompletionContributor {
 
   protected static class PrevSiblingClassFilter implements ElementFilter {
     protected Class<? extends PsiElement>[] my_accepted;
+    protected boolean my_straight;
 
     protected PrevSiblingClassFilter(Class<? extends PsiElement>... prevs) {
       my_accepted = prevs;
+      my_straight = true;
     }
 
     public boolean isAcceptable(final Object element, final PsiElement context) {
@@ -70,7 +109,9 @@ public class PyKeywordCompletionContributor extends CompletionContributor {
     }
   }
 
+  /*
   protected static final FilterPattern IN_LOOP = new FilterPattern(new ParentClassFilter(PyWhileStatement.class, PyForStatement.class));
+  */
   protected static final FilterPattern IN_TRY = new FilterPattern(new ParentClassFilter(PyTryExceptStatement.class));
   protected static final FilterPattern IN_DEF = new FilterPattern(new ParentClassFilter(PyFunction.class));
   protected static final FilterPattern IN_IF = new FilterPattern(new ParentClassFilter(PyIfStatement.class)); // TODO: handle "if. elif, but not else"
@@ -82,6 +123,7 @@ public class PyKeywordCompletionContributor extends CompletionContributor {
   protected static final FilterPattern AFTER_LOOP = new FilterPattern(new PrevSiblingClassFilter(PyWhileStatement.class, PyForStatement.class));
   protected static final FilterPattern AFTER_TRY = new FilterPattern(new PrevSiblingClassFilter(PyTryExceptStatement.class));
 
+  protected static final FilterPattern IN_PARAM_LIST = new FilterPattern(new ParentClassFilter(PyParameterList.class));
 
   protected static void addKeywords(@NonNls @NotNull String[] words, TailType tail, CompletionParameters parameters, final CompletionResultSet result) {
     final LookupElementFactory factory = LookupElementFactory.getInstance();
@@ -95,15 +137,16 @@ public class PyKeywordCompletionContributor extends CompletionContributor {
 
   protected void addBasic() {
     extend(
-      CompletionType.BASIC, psiElement().withLanguage(PythonLanguage.getInstance()).andNot(AFTER_QUALIFIED_REFERENCE).andNot(IN_IMPORT_STMT),
+      CompletionType.BASIC,
+      psiElement().withLanguage(PythonLanguage.getInstance()).andNot(AFTER_QUALIFIED_REFERENCE).andNot(IN_IMPORT_STMT).andNot(IN_PARAM_LIST).andNot(AFTER_DEF_OR_CLASS),
       new CompletionProvider<CompletionParameters>() {
         protected void addCompletions(
           @NotNull final CompletionParameters parameters, final ProcessingContext context, @NotNull final CompletionResultSet result
         ) {
           final @NonNls String[] space_strings = {
-            "assert", "def", "class", "for", "del", "exec", "from", "import", "lambda", "print", "raise", "while", "with"
+            "assert", "def", "class", "for", "del", "exec", "from", "if", "import", "lambda", "print", "raise", "while", "with"
           };
-          final @NonNls String[] colon_strings = {"if", "try"};
+          final @NonNls String[] colon_strings = {"try"};
           final @NonNls String[] just_strings = {"pass"};
           addKeywords(space_strings, TailType.SPACE, parameters, result);
           addKeywords(colon_strings, TailType.CASE_COLON, parameters, result);
@@ -115,7 +158,7 @@ public class PyKeywordCompletionContributor extends CompletionContributor {
 
   protected void addWithinLoops() {
     extend(
-      CompletionType.BASIC, psiElement().withLanguage(PythonLanguage.getInstance()).andNot(AFTER_QUALIFIED_REFERENCE).andOr(IN_LOOP, AFTER_LOOP),
+      CompletionType.BASIC, psiElement().withLanguage(PythonLanguage.getInstance()).andNot(AFTER_QUALIFIED_REFERENCE).andNot(IN_PARAM_LIST).andOr(IN_LOOP_1, AFTER_LOOP),
       new CompletionProvider<CompletionParameters>() {
         protected void addCompletions(
           @NotNull final CompletionParameters parameters, final ProcessingContext context, @NotNull final CompletionResultSet result
@@ -129,7 +172,7 @@ public class PyKeywordCompletionContributor extends CompletionContributor {
 
   protected void addWithinFuncs() {
     extend(
-      CompletionType.BASIC, psiElement().withLanguage(PythonLanguage.getInstance()).andNot(AFTER_QUALIFIED_REFERENCE).and(IN_DEF),
+      CompletionType.BASIC, psiElement().withLanguage(PythonLanguage.getInstance()).andNot(AFTER_QUALIFIED_REFERENCE).and(IN_DEF).andNot(AFTER_DEF_OR_CLASS),
       new CompletionProvider<CompletionParameters>() {
         protected void addCompletions(
           @NotNull final CompletionParameters parameters, final ProcessingContext context, @NotNull final CompletionResultSet result
@@ -174,7 +217,7 @@ public class PyKeywordCompletionContributor extends CompletionContributor {
   protected void addElse() {
     extend(
       CompletionType.BASIC, psiElement().withLanguage(PythonLanguage.getInstance())
-        .andNot(AFTER_QUALIFIED_REFERENCE).andOr(IN_LOOP, IN_IF, IN_TRY, AFTER_LOOP, AFTER_IF, AFTER_TRY),
+        .andNot(AFTER_QUALIFIED_REFERENCE).andOr(IN_LOOP_1, IN_IF, IN_TRY, AFTER_LOOP, AFTER_IF, AFTER_TRY),
       new CompletionProvider<CompletionParameters>() {
         protected void addCompletions(
           @NotNull final CompletionParameters parameters, final ProcessingContext context, @NotNull final CompletionResultSet result
@@ -188,7 +231,7 @@ public class PyKeywordCompletionContributor extends CompletionContributor {
 
   protected void addWithinExpr() {
     extend(
-      CompletionType.BASIC, psiElement().withLanguage(PythonLanguage.getInstance()).andNot(AFTER_QUALIFIED_REFERENCE).andOr(IN_EXPR),
+      CompletionType.BASIC, psiElement().withLanguage(PythonLanguage.getInstance()).andNot(AFTER_QUALIFIED_REFERENCE).andOr(IN_EXPR_1, IN_IF_COND),
       new CompletionProvider<CompletionParameters>() {
         protected void addCompletions(
           @NotNull final CompletionParameters parameters, final ProcessingContext context, @NotNull final CompletionResultSet result
@@ -203,12 +246,27 @@ public class PyKeywordCompletionContributor extends CompletionContributor {
   protected void addAs() {
     extend(
       CompletionType.BASIC, psiElement().withLanguage(PythonLanguage.getInstance())
-        .andNot(AFTER_QUALIFIED_REFERENCE).and( IN_IMPORT_ELT_OR_WITH),
+        .andNot(AFTER_QUALIFIED_REFERENCE).and(IN_IMPORT_AFTER_REF),
       new CompletionProvider<CompletionParameters>() {
         protected void addCompletions(
           @NotNull final CompletionParameters parameters, final ProcessingContext context, @NotNull final CompletionResultSet result
         ) {
           final @NonNls String[] space_strings = {"as"};
+          addKeywords(space_strings, TailType.SPACE, parameters, result);
+        }
+      }
+    );
+  }
+
+  protected void addImportInFrom() {
+    extend(
+      CompletionType.BASIC, psiElement().withLanguage(PythonLanguage.getInstance())
+        .andNot(AFTER_QUALIFIED_REFERENCE).and(IN_FROM_IMPORT_AFTER_REF),
+      new CompletionProvider<CompletionParameters>() {
+        protected void addCompletions(
+          @NotNull final CompletionParameters parameters, final ProcessingContext context, @NotNull final CompletionResultSet result
+        ) {
+          final @NonNls String[] space_strings = {"import"};
           addKeywords(space_strings, TailType.SPACE, parameters, result);
         }
       }
@@ -224,5 +282,6 @@ public class PyKeywordCompletionContributor extends CompletionContributor {
     addWithinExpr();
     addElse();
     addAs();
+    addImportInFrom();
   }
 }
