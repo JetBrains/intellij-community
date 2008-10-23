85ccf908e5074469c8b17dbb4b573de15d72e7c0
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2008-10-23 13:19:58 +0400

    icon replaced

--- source/com/intellij/openapi/roots/ui/configuration/packaging/PackedModuleOutputNode.java
+++ source/com/intellij/openapi/roots/ui/configuration/packaging/PackedModuleOutputNode.java
@@ -3,14 +3,18 @@ package com.intellij.openapi.roots.ui.configuration.packaging;
 import com.intellij.openapi.deployment.ModuleLink;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.ProjectBundle;
+import com.intellij.openapi.util.IconLoader;
 import com.intellij.ui.ColoredTreeCellRenderer;
 import com.intellij.ui.SimpleTextAttributes;
 import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
 
 /**
  * @author nik
  */
 class PackedModuleOutputNode extends ModuleOutputBaseNode {
+  private static final Icon MODULE_JAR_ICON = IconLoader.getIcon("/nodes/moduleJar.png");
   private final String myJarFileName;
 
   PackedModuleOutputNode(@NotNull ModuleLink moduleLink, String jarFileName, PackagingArtifact owner) {
@@ -25,9 +29,9 @@ class PackedModuleOutputNode extends ModuleOutputBaseNode {
 
   public void render(@NotNull final ColoredTreeCellRenderer renderer) {
     Module module = myModuleLink.getModule();
+    renderer.setIcon(MODULE_JAR_ICON);
     if (module != null) {
       renderer.append(myJarFileName, getMainAttributes());
-      renderer.setIcon(module.getModuleType().getNodeIcon(false));
       renderer.append(getComment(module.getName()), getCommentAttributes());
     }
     else {
