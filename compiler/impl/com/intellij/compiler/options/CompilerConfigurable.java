c8a0f9e5fa9fe26e2ffcdf41f2ef02b368cfd5ae
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2008-10-23 15:30:10 +0400

    cleanup

--- compiler/impl/com/intellij/compiler/options/CompilerConfigurable.java
+++ compiler/impl/com/intellij/compiler/options/CompilerConfigurable.java
@@ -25,10 +25,11 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-public class CompilerConfigurable extends SearchableConfigurable.Parent.Abstract {
+public class CompilerConfigurable implements SearchableConfigurable.Parent {
   private Project myProject;
   private static final Icon ICON = IconLoader.getIcon("/general/configurableCompiler.png");
   private CompilerUIConfigurable myCompilerUIConfigurable;
+  private Configurable[] myKids;
 
   public static CompilerConfigurable getInstance(Project project) {
     return ShowSettingsUtil.getInstance().findProjectConfigurable(project, CompilerConfigurable.class);
@@ -60,72 +61,77 @@ public class CompilerConfigurable extends SearchableConfigurable.Parent.Abstract
     return null;
   }
 
-  @Override
   public JComponent createComponent() {
     return myCompilerUIConfigurable.createComponent();
   }
 
-  @Override
   public boolean hasOwnContent() {
     return true;
   }
 
-  @Override
+  public boolean isToShowWhenChildIsShown() {
+    return true;
+  }
+
+  public boolean isVisible() {
+    return true;
+  }
+
   public boolean isModified() {
     return myCompilerUIConfigurable.isModified();
   }
 
-  @Override
   public void apply() throws ConfigurationException {
     myCompilerUIConfigurable.apply();
   }
 
-  @Override
   public void reset() {
     myCompilerUIConfigurable.reset();
   }
 
-  @Override
   public void disposeUIResources() {
     myCompilerUIConfigurable.disposeUIResources();
   }
 
-  protected Configurable[] buildConfigurables() {
-    List<Configurable> result = new ArrayList<Configurable>();
+  public Configurable[] getConfigurables() {
+    if (myKids == null) {
+      List<Configurable> kids = new ArrayList<Configurable>();
 
-    CompilerConfigurationImpl compilerConfiguration = (CompilerConfigurationImpl)CompilerConfiguration.getInstance(myProject);
-    final FileChooserDescriptor descriptor = new FileChooserDescriptor(true, true, false, false, false, true);
+      CompilerConfigurationImpl compilerConfiguration = (CompilerConfigurationImpl)CompilerConfiguration.getInstance(myProject);
+      final FileChooserDescriptor descriptor = new FileChooserDescriptor(true, true, false, false, false, true);
 
-    final ExcludedEntriesConfigurable excludes = new ExcludedEntriesConfigurable(myProject, descriptor, compilerConfiguration.getExcludedEntriesConfiguration()) {
-      public void apply() {
-        super.apply();
-        FileStatusManager.getInstance(myProject).fileStatusesChanged(); // refresh exclude from compile status
-        //ProjectView.getInstance(myProject).refresh();
-      }
-    };
-
-    result.add(createExcludesWrapper(excludes));
-
-    ArrayList<Configurable> additional = new ArrayList<Configurable>();
-
-    final CompilerSettingsFactory[] factories = Extensions.getExtensions(CompilerSettingsFactory.EP_NAME, myProject);
-    if (factories.length > 0) {
-      for (CompilerSettingsFactory factory : factories) {
-        additional.add(factory.create(myProject));
-      }
-      Collections.sort(additional, new Comparator<Configurable>() {
-        public int compare(final Configurable o1, final Configurable o2) {
-          return Comparing.compare(o1.getDisplayName(), o2.getDisplayName());
+      final ExcludedEntriesConfigurable excludes = new ExcludedEntriesConfigurable(myProject, descriptor, compilerConfiguration.getExcludedEntriesConfiguration()) {
+        public void apply() {
+          super.apply();
+          FileStatusManager.getInstance(myProject).fileStatusesChanged(); // refresh exclude from compile status
+          //ProjectView.getInstance(myProject).refresh();
         }
-      });
+      };
+
+      kids.add(createExcludesWrapper(excludes));
+
+      ArrayList<Configurable> additional = new ArrayList<Configurable>();
+
+      final CompilerSettingsFactory[] factories = Extensions.getExtensions(CompilerSettingsFactory.EP_NAME, myProject);
+      if (factories.length > 0) {
+        for (CompilerSettingsFactory factory : factories) {
+          additional.add(factory.create(myProject));
+        }
+        Collections.sort(additional, new Comparator<Configurable>() {
+          public int compare(final Configurable o1, final Configurable o2) {
+            return Comparing.compare(o1.getDisplayName(), o2.getDisplayName());
+          }
+        });
+      }
+
+      additional.add(0, new RmicConfigurable(RmicSettings.getInstance(myProject)));
+      additional.add(0, new JavaCompilersTab(myProject, compilerConfiguration.getRegisteredJavaCompilers(), compilerConfiguration.getDefaultCompiler()));
+
+      kids.addAll(additional);
+      myKids = kids.toArray(new Configurable[kids.size()]);
     }
 
-    additional.add(0, new RmicConfigurable(RmicSettings.getInstance(myProject)));
-    additional.add(0, new JavaCompilersTab(myProject, compilerConfiguration.getRegisteredJavaCompilers(), compilerConfiguration.getDefaultCompiler()));
-
-    result.addAll(additional);
-
-    return result.toArray(new Configurable[result.size()]);
+    return myKids;
 
   }
 
