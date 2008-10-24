aecfe0bf26f6de466e99dd98b6bdafa7223a1805
Ilya Sergey <Ilya.Sergey@@jetbrains.com>
2008-10-24 17:42:20 +0400

    reviewed: Gregory Shrago Groovy-specific class filter provided

---
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/debugger/filters/GroovyDebuggerClassFilterProvider.java
@@ -0,0 +1,39 @@
+package org.jetbrains.plugins.groovy.debugger.filters;
+
+import com.intellij.ui.classFilter.ClassFilter;
+import com.intellij.ui.classFilter.DebuggerClassFilterProvider;
+import com.intellij.util.Function;
+import com.intellij.util.containers.ContainerUtil;
+import org.jetbrains.annotations.NonNls;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author ilyas
+ */
+public class GroovyDebuggerClassFilterProvider implements DebuggerClassFilterProvider {
+
+  @NonNls private static final String[] PROHIBITED_CLASS_PATTERNS = {"sun.reflect.*", "groovy.lang.Meta*", "org.codehaus.groovy.runtime.*",
+  "org.codehaus.groovy.reflection.*"};
+
+  private static ClassFilter[] FITERS = ContainerUtil.map(PROHIBITED_CLASS_PATTERNS, new Function<String, ClassFilter>() {
+    public ClassFilter fun(final String s) {
+      return new ClassFilter(s);
+    }
+  }, new ClassFilter[0]);
+
+  public List<ClassFilter> getFilters() {
+
+    final GroovyDebuggerSettings settings = GroovyDebuggerSettings.getInstance();
+    final Boolean flag = settings.DEBUG_DISABLE_SPECIFIC_GROOVY_METHODS;
+    final ArrayList<ClassFilter> list = new ArrayList<ClassFilter>();
+    if (flag == null || flag.booleanValue()) {
+      list.addAll(Arrays.asList(FITERS));
+      return list;
+    }
+    return list;
+  }
+
+}
