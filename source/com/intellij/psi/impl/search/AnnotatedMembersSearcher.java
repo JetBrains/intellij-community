da7b5870310e85a434a243030793b48e145ecfac
Maxim Shafirov <max@jetbrains.com>
2008-10-31 20:53:38 +0300

    Another case of auto-recovery for faulty stub indices.

--- source/com/intellij/psi/impl/search/AnnotatedMembersSearcher.java
+++ source/com/intellij/psi/impl/search/AnnotatedMembersSearcher.java
@@ -3,14 +3,18 @@
  */
 package com.intellij.psi.impl.search;
 
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.PsiManagerImpl;
 import com.intellij.psi.impl.java.stubs.index.JavaAnnotationIndex;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.search.SearchScope;
 import com.intellij.psi.search.searches.AnnotatedMembersSearch;
+import com.intellij.psi.util.PsiUtil;
 import com.intellij.util.Processor;
 import com.intellij.util.QueryExecutor;
+import com.intellij.util.indexing.FileBasedIndex;
 
 import java.util.Collection;
 
@@ -18,6 +22,8 @@ import java.util.Collection;
  * @author max
  */
 public class AnnotatedMembersSearcher implements QueryExecutor<PsiMember, AnnotatedMembersSearch.Parameters> {
+  private static final Logger LOG = Logger.getInstance("#com.intellij.psi.impl.search.AnnotatedMembersSearcher");
+
   public boolean execute(final AnnotatedMembersSearch.Parameters p, final Processor<PsiMember> consumer) {
     final PsiClass annClass = p.getAnnotationClass();
     assert annClass.isAnnotationType() : "Annotation type should be passed to annotated members search";
@@ -31,7 +37,10 @@ public class AnnotatedMembersSearcher implements QueryExecutor<PsiMember, Annota
 
     final GlobalSearchScope scope = useScope instanceof GlobalSearchScope ? (GlobalSearchScope)useScope : null;
     final Collection<PsiAnnotation> annotations = JavaAnnotationIndex.getInstance().get(annClass.getName(), annClass.getProject(), scope);
-    for (PsiAnnotation ann : annotations) {
+    for (PsiElement elt : annotations) {
+      if (!notAnnotation(elt)) continue;
+
+      PsiAnnotation ann = (PsiAnnotation)elt;
       final PsiJavaCodeReferenceElement ref = ann.getNameReferenceElement();
       if (ref == null) continue;
 
@@ -49,6 +58,18 @@ public class AnnotatedMembersSearcher implements QueryExecutor<PsiMember, Annota
       if (!consumer.process(candidate)) {
         return false;
       }
+    }
+
+    return true;
+  }
+
+  private static boolean notAnnotation(final PsiElement found) {
+    if (found instanceof PsiAnnotation) return false;
+
+    VirtualFile faultyContainer = PsiUtil.getVirtualFile(found);
+    LOG.error("Non annotation in annotations list: " + faultyContainer);
+    if (faultyContainer != null && faultyContainer.isValid()) {
+      FileBasedIndex.getInstance().requestReindex(faultyContainer);
     }
 
     return true;
