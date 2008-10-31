8a74593ca26b777d39972c2b798999f8a4eeba74
Olesya Smirnova <Olesya.Smirnova@jetbrains.com>
2008-10-31 17:00:40 +0300

    move mappings to the vcs root configurable

--- vcs-impl/src/com/intellij/openapi/vcs/configurable/VcsManagerConfigurable.java
+++ vcs-impl/src/com/intellij/openapi/vcs/configurable/VcsManagerConfigurable.java
@@ -1,8 +1,8 @@
 package com.intellij.openapi.vcs.configurable;
 
 import com.intellij.openapi.options.Configurable;
-import com.intellij.openapi.options.SearchableConfigurable;
 import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.options.SearchableConfigurable;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.IconLoader;
 import com.intellij.openapi.vcs.AbstractVcs;
@@ -14,19 +14,55 @@ import org.jetbrains.annotations.Nls;
 
 import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
 public class VcsManagerConfigurable extends SearchableConfigurable.Parent.Abstract {
   public static final Icon ICON = IconLoader.getIcon("/general/configurableVcs.png");
   private final Project myProject;
+  private VcsDirectoryConfigurationPanel myMappings;
+  private VcsGeneralConfigurationPanel myGeneralPanel;
 
   public VcsManagerConfigurable(Project project) {
     myProject = project;
   }
 
-  public void moduleAdded() {
+  @Override
+  public JComponent createComponent() {
+    myMappings = new VcsDirectoryConfigurationPanel(myProject);
+    if (myGeneralPanel != null) {
+      addListenerToGeneralPanel();
+    }
+    return myMappings;
+  }
 
+  @Override
+  public boolean hasOwnContent() {
+    return true;
+  }
+
+  @Override
+  public boolean isModified() {
+    return myMappings.isModified();
+  }
+
+  @Override
+  public void apply() throws ConfigurationException {
+    super.apply();
+    myMappings.apply();
+  }
+
+  @Override
+  public void reset() {
+    super.reset();
+    myMappings.reset();
+  }
+
+  @Override
+  public void disposeUIResources() {
+    super.disposeUIResources();
+    myMappings = null;
   }
 
   public String getDisplayName() {
@@ -46,19 +82,25 @@ public class VcsManagerConfigurable extends SearchableConfigurable.Parent.Abstra
   }
 
   protected Configurable[] buildConfigurables() {
-    VcsDirectoryConfigurationPanel mappings = new VcsDirectoryConfigurationPanel(myProject);
-    final VcsGeneralConfigurationPanel generalPanel = new VcsGeneralConfigurationPanel(myProject);
-    generalPanel.updateAvailableOptions(mappings.getActiveVcses());
-    mappings.addVcsListener(new ModuleVcsListener() {
-      public void activeVcsSetChanged(Collection<AbstractVcs> activeVcses) {
-        generalPanel.updateAvailableOptions(activeVcses);
+    myGeneralPanel = new VcsGeneralConfigurationPanel(myProject){
+      @Override
+      public void disposeUIResources() {
+        super.disposeUIResources();
+        myGeneralPanel = null;
       }
-    });
+    };
+
+    if (myMappings != null) {
+      myGeneralPanel.updateAvailableOptions(myMappings.getActiveVcses());
+      addListenerToGeneralPanel();
+    }
+    else {
+      myGeneralPanel.updateAvailableOptions(Arrays.asList(ProjectLevelVcsManager.getInstance(myProject).getAllActiveVcss()));
+    }
 
     List<Configurable> result = new ArrayList<Configurable>();
 
-    result.add(mappings);
-    result.add(generalPanel);
+    result.add(myGeneralPanel);
     result.add(new VcsBackgroundOperationsConfigurationPanel(myProject));
     result.add(new IgnoredSettingsPanel(myProject));
     if (!myProject.isDefault()) {
@@ -74,6 +116,14 @@ public class VcsManagerConfigurable extends SearchableConfigurable.Parent.Abstra
 
     return result.toArray(new Configurable[result.size()]);
 
+  }
+
+  private void addListenerToGeneralPanel() {
+    myMappings.addVcsListener(new ModuleVcsListener() {
+      public void activeVcsSetChanged(Collection<AbstractVcs> activeVcses) {
+        myGeneralPanel.updateAvailableOptions(activeVcses);
+      }
+    });
   }
 
   private Configurable createVcsComposeConfigurable(final AbstractVcs[] vcses) {
