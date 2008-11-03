d5cb3078f1c9f9f5a80a9e82e32794c260d6f587
Maxim Mossienko <Maxim.Mossienko@jetbrains.com>
2008-11-03 22:59:07 +0300

    binary stub decompiler get project

--- lang-api/src/com/intellij/psi/stubs/BinaryFileStubBuilder.java
+++ lang-api/src/com/intellij/psi/stubs/BinaryFileStubBuilder.java
@@ -4,13 +4,14 @@
 package com.intellij.psi.stubs;
 
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.Nullable;
 
 public interface BinaryFileStubBuilder {
   boolean acceptsFile(final VirtualFile file);
 
   @Nullable
-  StubElement buildStubTree(final VirtualFile file, byte[] content);
+  StubElement buildStubTree(final VirtualFile file, byte[] content, final Project project);
 
   int getStubVersion();
 }
