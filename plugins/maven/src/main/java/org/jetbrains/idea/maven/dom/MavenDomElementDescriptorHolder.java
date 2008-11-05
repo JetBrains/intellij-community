f5366856e92d0529683e53db5a28a4461ea0de7b
Anton Makeev <Anton.Makeev@jetbrains.com>
2008-11-05 15:40:32 +0300

    Maven: memory leak fix

---
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/dom/MavenDomElementDescriptorHolder.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2000-2008 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jetbrains.idea.maven.dom;
+
+import com.intellij.javaee.ExternalResourceManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VfsUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.xml.XmlTag;
+import com.intellij.xml.XmlElementDescriptor;
+import com.intellij.xml.impl.schema.XmlNSDescriptorImpl;
+import org.jetbrains.idea.maven.utils.DummyProjectComponent;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MavenDomElementDescriptorHolder extends DummyProjectComponent {
+  private final Project myProject;
+  private volatile XmlNSDescriptorImpl myNSDescriptor;
+
+  public MavenDomElementDescriptorHolder(Project project) {
+    myProject = project;
+  }
+
+  public static MavenDomElementDescriptorHolder getInstance(Project project) {
+    return project.getComponent(MavenDomElementDescriptorHolder.class);
+  }
+
+  public XmlElementDescriptor getDescriptor(XmlTag tag) {
+    if (!MavenDomUtil.isPomFile(tag.getContainingFile())) return null;
+
+    if (myNSDescriptor == null) {
+      synchronized (this) {
+        if (myNSDescriptor == null) initDescriptor();
+      }
+    }
+    return myNSDescriptor.getElementDescriptor(tag.getName(), myNSDescriptor.getDefaultNamespace());
+  }
+
+  private void initDescriptor() {
+    myNSDescriptor = new XmlNSDescriptorImpl();
+
+    String schemaUrl = MavenSchemaRegistrar.MAVEN_SCHEMA_URL;
+    String location = ExternalResourceManager.getInstance().getResourceLocation(schemaUrl);
+    if (schemaUrl.equals(location)) return;
+
+    VirtualFile schema;
+    try {
+      schema = VfsUtil.findFileByURL(new URL(location));
+    }
+    catch (MalformedURLException ignore) {
+      return;
+    }
+
+    if (schema != null) {
+      PsiFile psiFile = PsiManager.getInstance(myProject).findFile(schema);
+      if (psiFile != null) myNSDescriptor.init(psiFile);
+    }
+  }
+}
--- plugins/maven/src/main/java/org/jetbrains/idea/maven/dom/MavenDomElementDescriptorProvider.java
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/dom/MavenDomElementDescriptorProvider.java
@@ -1,52 +1,11 @@
 package org.jetbrains.idea.maven.dom;
 
-import com.intellij.javaee.ExternalResourceManager;
-import com.intellij.openapi.vfs.VfsUtil;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiManager;
 import com.intellij.psi.impl.source.xml.XmlElementDescriptorProvider;
 import com.intellij.psi.xml.XmlTag;
 import com.intellij.xml.XmlElementDescriptor;
-import com.intellij.xml.impl.schema.XmlNSDescriptorImpl;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class MavenDomElementDescriptorProvider implements XmlElementDescriptorProvider {
-  public static XmlNSDescriptorImpl ourDescriptor;
-  public static Object ourLock = new Object();
-
   public XmlElementDescriptor getDescriptor(XmlTag tag) {
-    if (!MavenDomUtil.isPomFile(tag.getContainingFile())) return null;
-
-    if (ourDescriptor == null) {
-      synchronized (ourLock) {
-        if (ourDescriptor == null) initDescriptor(tag);
-      }
-    }
-    return ourDescriptor.getElementDescriptor(tag.getName(),
-                                              ourDescriptor.getDefaultNamespace());
-  }
-
-  private void initDescriptor(XmlTag tag) {
-    ourDescriptor = new XmlNSDescriptorImpl();
-
-    String schemaUrl = MavenSchemaRegistrar.MAVEN_SCHEMA_URL;
-    String location = ExternalResourceManager.getInstance().getResourceLocation(schemaUrl);
-    if (schemaUrl.equals(location)) return;
-
-    VirtualFile schema = null;
-    try {
-      schema = VfsUtil.findFileByURL(new URL(location));
-    }
-    catch (MalformedURLException ignore) {
-      return;
-    }
-
-    if (schema != null) {
-      PsiFile psiFile = PsiManager.getInstance(tag.getProject()).findFile(schema);
-      if(psiFile != null) ourDescriptor.init(psiFile);
-    }
+    return MavenDomElementDescriptorHolder.getInstance(tag.getProject()).getDescriptor(tag);
   }
 }
