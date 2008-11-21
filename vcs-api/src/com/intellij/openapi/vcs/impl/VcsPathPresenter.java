ecf6bd9a6691e08218e460bbe293b2edca2c0768
Irina Chernushina <Irina.Chernushina@jetbrains.com>
2008-11-21 12:59:52 +0300

    IDEADEV-32952 (AE: ComponentManagerImpl.getPicoContainer)

--- vcs-api/src/com/intellij/openapi/vcs/impl/VcsPathPresenter.java
+++ vcs-api/src/com/intellij/openapi/vcs/impl/VcsPathPresenter.java
@@ -1,9 +1,11 @@
 package com.intellij.openapi.vcs.impl;
 
-import com.intellij.openapi.project.Project;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ServiceManager;
-import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.vcs.changes.ContentRevision;
+import com.intellij.openapi.vfs.VirtualFile;
 
 /**
  * @author yole
@@ -11,6 +13,14 @@ import com.intellij.openapi.vcs.changes.ContentRevision;
 public abstract class VcsPathPresenter {
   public static VcsPathPresenter getInstance(Project project) {
     return ServiceManager.getService(project, VcsPathPresenter.class);
+  }
+
+  public static VcsPathPresenter getInstanceChecked(final Project project) {
+    return ApplicationManager.getApplication().runReadAction(new Computable<VcsPathPresenter>() {
+      public VcsPathPresenter compute() {
+        return ServiceManager.getService(project, VcsPathPresenter.class);
+      }
+    });
   }
 
   /**
