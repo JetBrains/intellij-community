7eb554a5a3d35ea374135eb7ff7571c4bfb1ddb0
Dmitry Jemerov <yole@intellij.com>
2008-11-27 14:23:06 +0300

    move DefaultTemplatePropertiesProvider to OpenAPI, provide javadoc

--- lang-api/src/com/intellij/ide/fileTemplates/DefaultTemplatePropertiesProvider.java
+++ lang-api/src/com/intellij/ide/fileTemplates/DefaultTemplatePropertiesProvider.java
@@ -6,10 +6,19 @@ import com.intellij.psi.PsiDirectory;
 import java.util.Properties;
 
 /**
+ * Provides default variables which are available in file templates.
+ *
  * @author yole
+ * @since 8.0
  */
 public interface DefaultTemplatePropertiesProvider {
   ExtensionPointName<DefaultTemplatePropertiesProvider> EP_NAME = ExtensionPointName.create("com.intellij.defaultTemplatePropertiesProvider");
 
+  /**
+   * Fills the default properties for a file which is created in the specified directory.
+   *
+   * @param directory the directory in which the file is created.
+   * @param props the map in which the defined properties should be stored.
+   */
   void fillProperties(PsiDirectory directory, Properties props);
 }
