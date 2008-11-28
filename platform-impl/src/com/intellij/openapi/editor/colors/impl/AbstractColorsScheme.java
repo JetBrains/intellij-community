7af1cbb4b3179bcdf690384740915c22763dd0d5
Dmitry Jemerov <yole@intellij.com>
2008-11-28 19:31:55 +0300

    remove debugging

--- platform-impl/src/com/intellij/openapi/editor/colors/impl/AbstractColorsScheme.java
+++ platform-impl/src/com/intellij/openapi/editor/colors/impl/AbstractColorsScheme.java
@@ -176,10 +176,6 @@ public abstract class AbstractColorsScheme implements EditorColorsScheme {
       }
     }
     initFonts();
-    System.out.println(new Font("Monaco", 0, 12).getFontName());
-    System.out.println(new Font("Monaco", 0, 12).getFamily());
-    System.out.println(new Font("Lucida Console", 0, 12).getFontName());
-    System.out.println(new Font("Lucida Console", 0, 12).getFamily());
   }
 
   private void readScheme(Element node) throws InvalidDataException {
