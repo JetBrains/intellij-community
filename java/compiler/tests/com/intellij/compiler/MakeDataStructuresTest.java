10512a7e020441a45e03748212760faac104d271
Eugene Zhuravlev <jeka@jetbrains.com>
2008-12-03 15:03:05 +0300

    faster compiler caches

--- java/compiler/tests/com/intellij/compiler/MakeDataStructuresTest.java
+++ java/compiler/tests/com/intellij/compiler/MakeDataStructuresTest.java
@@ -53,9 +53,6 @@ public class MakeDataStructuresTest extends TestCase{
 
     final String value = "dummy_value";
 
-    map.put(new DeclarationInfo(10), value);
-    assertTrue(value == map.get(new DeclarationInfo(10)));
-
     final FieldInfo fieldInfo = new FieldInfo(10, 20, -1, 30, ConstantValue.EMPTY_CONSTANT_VALUE, null, null);
     final MethodInfo methodInfo = new MethodInfo(10, 20, -1, 30, new int[0], false, null, null, null, null, null);
 
@@ -64,12 +61,6 @@ public class MakeDataStructuresTest extends TestCase{
 
     map.put(methodInfo, value);
     assertTrue(value == map.get(new MethodInfo(10, 20, -1, 30, new int[0], false, null, null, null, null, null)));
-
-    map.put(new MemberDeclarationInfo(10, fieldInfo), value);
-    assertTrue(value == map.get(new MemberDeclarationInfo(10, new FieldInfo(10, 20, -1, 30, ConstantValue.EMPTY_CONSTANT_VALUE, null, null))));
-
-    map.put(new MemberDeclarationInfo(10, methodInfo), value);
-    assertTrue(value == map.get(new MemberDeclarationInfo(10, new MethodInfo(10, 20, -1, 30, new int[0], false, null, null, null, null, null))));
   }
 
   public void testReferenceEquality() {
@@ -98,33 +89,5 @@ public class MakeDataStructuresTest extends TestCase{
 
     assertFalse(methodRef.equals(classRef));
     assertFalse(classRef.equals(methodRef));
-  }
-
-  public void testDeclarationEquality() {
-    final DeclarationInfo classDecl = new DeclarationInfo(10);
-    final DeclarationInfo classDecl2 = new DeclarationInfo(10);
-    final DeclarationInfo classDecl3 = new DeclarationInfo(70);
-    final MemberDeclarationInfo methodDecl = new MemberDeclarationInfo(10, new MethodInfo(20, 30, false));
-    final MemberDeclarationInfo methodDecl2 = new MemberDeclarationInfo(10, new MethodInfo(20, 30, false));
-    final MemberDeclarationInfo methodDecl3 = new MemberDeclarationInfo(10, new MethodInfo(20, 40, false));
-    final MemberDeclarationInfo methodDecl4 = new MemberDeclarationInfo(90, new MethodInfo(20, 40, false));
-
-    assertTrue(classDecl.equals(classDecl2));
-    assertTrue(classDecl2.equals(classDecl));
-
-    assertFalse(classDecl.equals(classDecl3));
-    assertFalse(classDecl3.equals(classDecl));
-
-    assertTrue(methodDecl.equals(methodDecl2));
-    assertTrue(methodDecl2.equals(methodDecl));
-
-    assertFalse(methodDecl.equals(methodDecl3));
-    assertFalse(methodDecl3.equals(methodDecl));
-
-    assertFalse(methodDecl.equals(methodDecl4));
-    assertFalse(methodDecl4.equals(methodDecl));
-
-    assertFalse(methodDecl.equals(classDecl));
-    assertFalse(classDecl.equals(methodDecl));
   }
 }
