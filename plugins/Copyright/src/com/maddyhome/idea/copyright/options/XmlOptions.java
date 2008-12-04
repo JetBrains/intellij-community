233226ab36edd3b97676511e134f0b71ddc352db
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2008-12-04 17:29:43 +0300

    copyright: update license

--- plugins/Copyright/src/com/maddyhome/idea/copyright/options/XmlOptions.java
+++ plugins/Copyright/src/com/maddyhome/idea/copyright/options/XmlOptions.java
@@ -1,23 +1,20 @@
-package com.maddyhome.idea.copyright.options;
-
 /*
- * Copyright - Copyright notice updater for IDEA
- * Copyright (C) 2004-2005 Rick Maddy. All Rights Reserved.
+ *  Copyright 2000-2007 JetBrains s.r.o.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
+
+package com.maddyhome.idea.copyright.options;
 
 /**
  */
