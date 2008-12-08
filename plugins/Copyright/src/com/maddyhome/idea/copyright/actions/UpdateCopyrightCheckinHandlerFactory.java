7db51fa95ad99190bc5705befdddeaab3c74bfbc
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2008-12-08 22:02:58 +0300

    copyright

--- plugins/Copyright/src/com/maddyhome/idea/copyright/actions/UpdateCopyrightCheckinHandlerFactory.java
+++ plugins/Copyright/src/com/maddyhome/idea/copyright/actions/UpdateCopyrightCheckinHandlerFactory.java
@@ -1,4 +1,20 @@
 /*
+ * Copyright 2000-2008 JetBrains s.r.o.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/*
 * User: anna
 * Date: 08-Dec-2008
 */
@@ -8,6 +24,7 @@ import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.RoamingType;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.vcs.CheckinProjectPanel;
 import com.intellij.openapi.vcs.changes.CommitExecutor;
 import com.intellij.openapi.vcs.checkin.CheckinHandler;
@@ -66,6 +83,7 @@ public class UpdateCopyrightCheckinHandlerFactory extends CheckinHandlerFactory 
       @Override
       public ReturnResult beforeCheckin(@Nullable CommitExecutor executor) {
         new UpdateCopyrightProcessor(panel.getProject(), null, getPsiFiles()).run();
+        FileDocumentManager.getInstance().saveAllDocuments();
         return super.beforeCheckin();
       }
 
