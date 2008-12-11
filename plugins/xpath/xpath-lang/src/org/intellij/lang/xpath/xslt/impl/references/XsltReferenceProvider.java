779f92bf5c3d1d9e66efd476de86ff80fadda276
Sascha Weinreuter <no_reply@jetbrains.com>
2008-12-11 13:27:48 +0300

    greenify

--- plugins/xpath/xpath-lang/src/org/intellij/lang/xpath/xslt/impl/references/XsltReferenceProvider.java
+++ plugins/xpath/xpath-lang/src/org/intellij/lang/xpath/xslt/impl/references/XsltReferenceProvider.java
@@ -1,17 +1,5 @@
 /*
- * Copyright 2005 Sascha Weinreuter
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright (c) 2008, Your Corporation. All Rights Reserved.
  */
 
 package org.intellij.lang.xpath.xslt.impl.references;
@@ -60,7 +48,7 @@ public class XsltReferenceProvider extends PsiReferenceProvider {
     private final XsltElementFactory myXsltElementFactory = XsltElementFactory.getInstance();
 
     public XsltReferenceProvider(Project project) {
-        this.myCacheManager = PsiManager.getInstance(project).getCachedValuesManager();
+        myCacheManager = PsiManager.getInstance(project).getCachedValuesManager();
     }
 
     @NotNull
@@ -87,7 +75,7 @@ public class XsltReferenceProvider extends PsiReferenceProvider {
         private final XmlAttribute myAttribute;
 
         ReferenceProvider(XmlAttribute attribute) {
-            this.myAttribute = attribute;
+            myAttribute = attribute;
         }
 
         public Result<PsiReference[]> compute() {
