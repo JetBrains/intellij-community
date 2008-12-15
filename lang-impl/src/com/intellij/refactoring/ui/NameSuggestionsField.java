3e8197c6e419ea7de1abe6cd89c884fe2cc65f8b
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2008-12-15 16:11:44 +0300

    restore initial name suggestion

--- lang-impl/src/com/intellij/refactoring/ui/NameSuggestionsField.java
+++ lang-impl/src/com/intellij/refactoring/ui/NameSuggestionsField.java
@@ -120,7 +120,11 @@ public class NameSuggestionsField extends JPanel {
       oldItemFromTextField.equals(oldSelectedItem) || oldItemFromTextField.trim().length() == 0;
     myComboBoxModel.setSuggestions(suggestions);
     if(suggestions.length > 0 && shouldUpdateTextField) {
-      comboBox.setSelectedItem(oldSelectedItem);
+      if (oldSelectedItem != null) {
+        comboBox.setSelectedItem(oldSelectedItem);
+      } else {
+        comboBox.setSelectedIndex(0);
+      }
     }
     else {
       comboBox.getEditor().setItem(oldItemFromTextField);
