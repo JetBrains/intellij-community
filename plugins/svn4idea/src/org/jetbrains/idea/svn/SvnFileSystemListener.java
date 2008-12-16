4be9ed0b152cf6bc81c5589c1f86449c8ab64419
Irina Chernushina <Irina.Chernushina@jetbrains.com>
2008-12-16 19:26:43 +0300

    SVN: NPE in file listener

--- plugins/svn4idea/src/org/jetbrains/idea/svn/SvnFileSystemListener.java
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/SvnFileSystemListener.java
@@ -744,10 +744,10 @@ public class SvnFileSystemListener implements LocalFileOperationsHandler, Comman
         else {
           singleFilePrompt = SvnBundle.message("confirmation.text.delete.file");
         }
-        filesToProcess = new ArrayList<FilePath>(vcsHelper.selectFilePathsToProcess(deletedFiles, SvnBundle.message("confirmation.title.delete.multiple.files"),
-                                                            null,
-                                                            SvnBundle.message("confirmation.title.delete.file"), singleFilePrompt,
-                                                            vcs.getDeleteConfirmation()));
+        final Collection<FilePath> files = vcsHelper
+          .selectFilePathsToProcess(deletedFiles, SvnBundle.message("confirmation.title.delete.multiple.files"), null,
+                                    SvnBundle.message("confirmation.title.delete.file"), singleFilePrompt, vcs.getDeleteConfirmation());
+        filesToProcess = (files == null) ? null : new ArrayList<FilePath>(files);
       }
       if (filesToProcess != null) {
         deletedFiles.removeAll(filesToProcess);
