9ae6e50f6386ea68ed55561d1ee1145e4d1414a3
Alexey Pegov <Alexey.Pegov@jetbrains.com>
2008-12-26 20:07:14 +0300

    classname file path completion advertisement added + minor fixes

--- lang-impl/src/com/intellij/codeInsight/completion/FilePathCompletionContributor.java
+++ lang-impl/src/com/intellij/codeInsight/completion/FilePathCompletionContributor.java
@@ -1,19 +1,23 @@
 package com.intellij.codeInsight.completion;
 
+import com.intellij.codeInsight.CodeInsightBundle;
 import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementPresentation;
 import com.intellij.codeInsight.lookup.LookupElementRenderer;
 import com.intellij.navigation.ChooseByNameContributor;
+import com.intellij.openapi.actionSystem.IdeActions;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.module.Module;
 import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.roots.ProjectFileIndex;
 import com.intellij.openapi.roots.ProjectRootManager;
-import com.intellij.openapi.roots.ModuleRootManager;
-import com.intellij.openapi.module.Module;
+import com.intellij.openapi.util.Computable;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vfs.VirtualFile;
 import static com.intellij.patterns.PlatformPatterns.psiElement;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.source.resolve.reference.impl.PsiMultiReference;
@@ -21,9 +25,9 @@ import com.intellij.psi.impl.source.resolve.reference.impl.providers.FileInfoMan
 import com.intellij.psi.impl.source.resolve.reference.impl.providers.FileReference;
 import com.intellij.psi.impl.source.resolve.reference.impl.providers.FileReferenceOwner;
 import com.intellij.psi.impl.source.resolve.reference.impl.providers.PsiFileSystemItemUtil;
-import com.intellij.psi.search.FilenameIndex;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.search.ProjectScope;
+import com.intellij.psi.search.FilenameIndex;
 import com.intellij.util.*;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -38,18 +42,46 @@ public class FilePathCompletionContributor extends CompletionContributor {
   private static final Logger LOG = Logger.getInstance("#com.intellij.codeInsight.completion.FilePathCompletionContributor");
 
   public FilePathCompletionContributor() {
-    extend(CompletionType.CLASS_NAME, psiElement(), new CompletionProvider<CompletionParameters>() {
+    extend(CompletionType.BASIC, psiElement(), new CompletionProvider<CompletionParameters>() {
       @Override
       protected void addCompletions(@NotNull CompletionParameters parameters,
                                     ProcessingContext context,
                                     @NotNull CompletionResultSet result) {
+        final String prefix = result.getPrefixMatcher().getPrefix();
+        if (prefix.length() == 0) {
+          return;
+        }
+
+        final PsiReference psiReference = parameters.getPosition().getContainingFile().findReferenceAt(parameters.getOffset());
+        if (getReference(psiReference) != null) {
+          final String shortcut = getActionShortcut(IdeActions.ACTION_CLASS_NAME_COMPLETION);
+          final CompletionService service = CompletionService.getCompletionService();
+          if (StringUtil.isEmpty(service.getAdvertisementText()) && shortcut != null) {
+            service.setAdvertisementText(CodeInsightBundle.message("class.completion.file.path", shortcut));
+          }
+        }
+      }
+    });
+
+    extend(CompletionType.CLASS_NAME, psiElement(), new CompletionProvider<CompletionParameters>(true, false) {
+      @Override
+      protected void addCompletions(@NotNull final CompletionParameters parameters,
+                                    ProcessingContext context,
+                                    @NotNull final CompletionResultSet result) {
         final String prefix = result.getPrefixMatcher().getPrefix();
         if (prefix.length() == 0) { // todo[spL]: no prefix no completion
           return;
         }
 
         final PsiElement e = parameters.getPosition();
-        final PsiReference psiReference = parameters.getOriginalFile().findReferenceAt(parameters.getOffset());
+
+
+        final PsiReference psiReference = ApplicationManager.getApplication().runReadAction(new Computable<PsiReference>() {
+          public PsiReference compute() {
+            //noinspection ConstantConditions
+            return parameters.getPosition().getContainingFile().findReferenceAt(parameters.getOffset());
+          }
+        });
 
         if (getReference(psiReference) != null) {
           final Project project = e.getProject();
@@ -81,28 +113,37 @@ public class FilePathCompletionContributor extends CompletionContributor {
               final LogicalRootType contextRootType = contextRoot.getType();
 
               final GlobalSearchScope scope = ProjectScope.getProjectScope(project);
-              for (String name : resultNames) {
+              for (final String name : resultNames) {
                 ProgressManager.getInstance().checkCanceled();
 
-                final PsiFile[] files = FilenameIndex.getFilesByName(project, name, scope);
+                final PsiFile[] files = ApplicationManager.getApplication().runReadAction(new Computable<PsiFile[]>() {
+                  public PsiFile[] compute() {
+                    return FilenameIndex.getFilesByName(project, name, scope);
+                  }
+                });
+
                 if (files.length > 0) {
-                  for (PsiFile file : files) {
-                    final VirtualFile virtualFile = file.getVirtualFile();
-                    if (virtualFile != null && virtualFile.isValid()) {
-                      final Module module = index.getModuleForFile(virtualFile);
-                      if (modules.contains(module)) {
-                        final LogicalRoot logicalRoot = logicalRootsManager.findLogicalRoot(virtualFile);
-                        if (logicalRoot != null && contextRootType == logicalRoot.getType()) {
-                          final VirtualFile _context = contextRoot == logicalRoot ? contextRootFile : project.getBaseDir();
-                          if (_context != null) {
-                            final PsiDirectory psiFile = psiManager.findDirectory(_context);
-                            if (psiFile != null) {
-                              result.addElement(new FilePathLookupItem(file, psiFile));
+                  for (final PsiFile file : files) {
+                    ApplicationManager.getApplication().runReadAction(new Runnable() {
+                      public void run() {
+                        final VirtualFile virtualFile = file.getVirtualFile();
+                        if (virtualFile != null && virtualFile.isValid()) {
+                          final Module module = index.getModuleForFile(virtualFile);
+                          if (modules.contains(module)) {
+                            final LogicalRoot logicalRoot = logicalRootsManager.findLogicalRoot(virtualFile);
+                            if (logicalRoot != null && contextRootType == logicalRoot.getType()) {
+                              final VirtualFile _context = contextRoot == logicalRoot ? contextRootFile : project.getBaseDir();
+                              if (_context != null) {
+                                final PsiDirectory psiFile = psiManager.findDirectory(_context);
+                                if (psiFile != null) {
+                                  result.addElement(new FilePathLookupItem(file, psiFile));
+                                }
+                              }
                             }
                           }
                         }
                       }
-                    }
+                    });
                   }
                 }
               }
@@ -118,7 +159,11 @@ public class FilePathCompletionContributor extends CompletionContributor {
     final ChooseByNameContributor[] nameContributors = ChooseByNameContributor.FILE_EP_NAME.getExtensions();
     for (final ChooseByNameContributor contributor : nameContributors) {
       try {
-        names.addAll(Arrays.asList(contributor.getNames(project, false)));
+        names.addAll(ApplicationManager.getApplication().runReadAction(new Computable<Collection<? extends String>>() {
+          public Collection<? extends String> compute() {
+            return Arrays.asList(contributor.getNames(project, false));
+          }
+        }));
       }
       catch (ProcessCanceledException ex) {
         // index corruption detected, ignore
@@ -168,6 +213,7 @@ public class FilePathCompletionContributor extends CompletionContributor {
       myPath = file.getVirtualFile().getPath();
 
       final String relative = PsiFileSystemItemUtil.getRelativePath(context, file);
+
       myRelativePath = relative == null ? null : relative.equals(myName) ? null : relative;
 
       myInfo = FileInfoManager.getFileAdditionalInfo(file);
@@ -204,7 +250,7 @@ public class FilePathCompletionContributor extends CompletionContributor {
 
           final PsiFile psiFile = _item.myFile;
           if (psiFile.isValid()) {
-            final PsiReference psiReference = context.getFile().findReferenceAt(context.getSelectionEndOffset());
+            final PsiReference psiReference = context.getFile().findReferenceAt(context.getStartOffset());
             final PsiReference fileReference = getReference(psiReference);
             LOG.assertTrue(fileReference != null);
 
