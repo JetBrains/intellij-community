858a81aedc3046b47b584345a10000b8e27da439
Dmitry Jemerov <yole@intellij.com>
2009-01-12 15:01:28 +0300

    hook restarter to "invalidate caches" action

--- platform-impl/src/com/intellij/ide/actions/InvalidateCachesAction.java
+++ platform-impl/src/com/intellij/ide/actions/InvalidateCachesAction.java
@@ -3,6 +3,9 @@ package com.intellij.ide.actions;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.application.Application;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ApplicationNamesInfo;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.newvfs.persistent.FSRecords;
 
@@ -12,8 +15,20 @@ import com.intellij.openapi.vfs.newvfs.persistent.FSRecords;
 public class InvalidateCachesAction extends AnAction {
   public void actionPerformed(AnActionEvent e) {
     FSRecords.invalidateCaches();
-    Messages.showMessageDialog(e.getData(PlatformDataKeys.PROJECT),
-                               "The caches have been invalidated and will be rebuilt on the next startup",
-                               "Invalidate Caches", Messages.getInformationIcon());
+    final Application app = ApplicationManager.getApplication();
+    if (app.isRestartCapable()) {
+      int rc = Messages.showYesNoDialog(e.getData(PlatformDataKeys.PROJECT),
+                                        "The caches have been invalidated and will be rebuilt on the next startup. Would you like to restart " +
+                                        ApplicationNamesInfo.getInstance().getFullProductName() + " now?",
+                                        "Invalidate Caches", Messages.getInformationIcon());
+      if (rc == 0) {
+        app.restart();
+      }
+    }
+    else {
+      Messages.showMessageDialog(e.getData(PlatformDataKeys.PROJECT),
+                                 "The caches have been invalidated and will be rebuilt on the next startup",
+                                 "Invalidate Caches", Messages.getInformationIcon());
+    }
   }
 }
