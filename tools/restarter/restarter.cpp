e3a057c2bcdb7123ad790961f293162065c0b2b5
Dmitry Jemerov <yole@intellij.com>
2009-01-12 19:26:30 +0300

    restarter fixes

--- tools/restarter/restarter.cpp
+++ tools/restarter/restarter.cpp
@@ -9,11 +9,13 @@ int _tmain(int argc, _TCHAR* argv[])
 	if (argc < 3) return 0;
 	int ppid = _ttoi(argv [1]);
 	HANDLE parent_process = OpenProcess(SYNCHRONIZE, FALSE, ppid);
-	if (!parent_process) return 0;
-	WaitForSingleObject(parent_process, INFINITE);
-	CloseHandle(parent_process);
+	if (parent_process)
+	{
+		WaitForSingleObject(parent_process, INFINITE);
+		CloseHandle(parent_process);
+	}
 
-	int rc = _texecv(argv [2], argv+3);
+	int rc = _texecv(argv [2], argv+2);
 	if (rc == -1)
 	{
 		_tprintf(_T("Error restarting process: errno is %d"), errno);
