f290264260e56d66baf0e103550110942bed51d8
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2009-01-27 14:43:51 +0300

    do not suggest dynamic classpath if -Djava.system.class.loader property is set

--- lang-api/src/com/intellij/execution/runners/ProgramRunnerUtil.java
+++ lang-api/src/com/intellij/execution/runners/ProgramRunnerUtil.java
@@ -31,14 +31,17 @@ public class ProgramRunnerUtil {
       LOG.assertTrue(false, message);
     }
     else {
-      if (message.contains("87") && e instanceof ProcessNotCreatedException && ((ProcessNotCreatedException)e).getCommandLine().getCommandLineString().length() > 1024 * 32 ) {
-        if (Messages.showYesNoDialog(project, message + "\nCommand line is too long. In order to reduce its length classpath file can be used. Would you like to enable classpath file mode for all run configurations of your project?", ExecutionBundle.message("run.error.message.title"), Messages.getErrorIcon()) ==
-            DialogWrapper.OK_EXIT_CODE) {
-          PropertiesComponent.getInstance(project).setValue("dynamic.classpath", "true");
+      if (message.contains("87") && e instanceof ProcessNotCreatedException) {
+        final String commandLineString = ((ProcessNotCreatedException)e).getCommandLine().getCommandLineString();
+        if (!commandLineString.contains("-Djava.system.class.loader") && commandLineString.length() > 1024 * 32) {
+          if (Messages.showYesNoDialog(project, message + "\nCommand line is too long. In order to reduce its length classpath file can be used. Would you like to enable classpath file mode for all run configurations of your project?", ExecutionBundle.message("run.error.message.title"), Messages.getErrorIcon()) ==
+              DialogWrapper.OK_EXIT_CODE) {
+            PropertiesComponent.getInstance(project).setValue("dynamic.classpath", "true");
+            return;
+          }
         }
-      } else {
-        Messages.showErrorDialog(project, message, ExecutionBundle.message("run.error.message.title"));
       }
+      Messages.showErrorDialog(project, message, ExecutionBundle.message("run.error.message.title"));
     }
   }
 
