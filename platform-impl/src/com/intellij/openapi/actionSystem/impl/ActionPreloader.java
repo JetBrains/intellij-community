1fe394acc9cce40231b41eb75d8e00182c0b5b60
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2009-02-02 19:17:56 +0300

    do not preload actions in headless mode

--- platform-impl/src/com/intellij/openapi/actionSystem/impl/ActionPreloader.java
+++ platform-impl/src/com/intellij/openapi/actionSystem/impl/ActionPreloader.java
@@ -1,15 +1,15 @@
 package com.intellij.openapi.actionSystem.impl;
 
-import com.intellij.openapi.startup.StartupManager;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.startup.StartupManager;
 
 /**
  * @author yole
  */
 public class ActionPreloader {
   public ActionPreloader(StartupManager manager) {
-    if (!ApplicationManager.getApplication().isUnitTestMode()) {
+    if (!ApplicationManager.getApplication().isUnitTestMode() && !ApplicationManager.getApplication().isHeadlessEnvironment()) {
       manager.registerPostStartupActivity(new Runnable() {
         public void run() {
           ((ActionManagerImpl)ActionManager.getInstance()).preloadActions();
