38badbf07a6a49c546530bd943dd29d6427eca31
Peter Gromov <peter@jetbrains.com>
2009-02-05 17:49:28 +0300

    full-blown .java support in library classes

--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/parser/GroovyParserDefinition.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/parser/GroovyParserDefinition.java
@@ -20,13 +20,11 @@ import com.intellij.lang.ParserDefinition;
 import static com.intellij.lang.ParserDefinition.SpaceRequirements.MAY;
 import static com.intellij.lang.ParserDefinition.SpaceRequirements.MUST_LINE_BREAK;
 import com.intellij.lang.PsiParser;
-import com.intellij.lang.java.JavaParserDefinition;
 import com.intellij.lexer.Lexer;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.FileViewProvider;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.impl.source.PsiPlainTextFileImpl;
 import com.intellij.psi.tree.IFileElementType;
 import com.intellij.psi.tree.TokenSet;
 import org.jetbrains.annotations.NotNull;
@@ -75,12 +73,7 @@ public class GroovyParserDefinition implements ParserDefinition {
   }
 
   public PsiFile createFile(FileViewProvider viewProvider) {
-    if (JavaParserDefinition.shouldCreateJavaFile(viewProvider)) {
-      return new GroovyFileImpl(viewProvider);
-    }
-    else {
-      return new PsiPlainTextFileImpl(viewProvider);
-    }
+    return new GroovyFileImpl(viewProvider);
   }
 
   public SpaceRequirements spaceExistanceTypeBetweenTokens(ASTNode left, ASTNode right) {
--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/impl/search/GrSourceFilterScope.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/impl/search/GrSourceFilterScope.java
@@ -1,15 +1,12 @@
 package org.jetbrains.plugins.groovy.lang.psi.impl.search;
 
-import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectFileIndex;
 import com.intellij.openapi.roots.ProjectRootManager;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.fileTypes.FileType;
-import com.intellij.openapi.fileTypes.StdFileTypes;
-import com.intellij.openapi.module.Module;
+import com.intellij.psi.search.GlobalSearchScope;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.plugins.groovy.GroovyFileType;
 
 /**
  * @author ilyas
@@ -24,10 +21,11 @@ public class GrSourceFilterScope extends GlobalSearchScope {
   }
 
   public boolean contains(final VirtualFile file) {
-    final FileType fileType = file.getFileType();
-    return (myDelegate == null || myDelegate.contains(file)) &&
-            (GroovyFileType.GROOVY_FILE_TYPE == fileType && myIndex.isInSourceContent(file) ||
-                    StdFileTypes.CLASS == fileType && myIndex.isInLibraryClasses(file));
+    if (myDelegate != null && !myDelegate.contains(file)) {
+      return false;
+    }
+    
+    return myIndex.isInSourceContent(file) || myIndex.isInLibraryClasses(file);
   }
 
   public int compare(final VirtualFile file1, final VirtualFile file2) {
