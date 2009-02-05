f89af080bf5f1e09600eb60953466be6ad88c1ff
Bas Leijdekkers <basleijdekkers@gmail.com>
2009-02-05 12:01:35 +0300

    IDEADEV-34498 (Non exception class name inspection quickfix fails)

--- plugins/InspectionGadgets/src/com/siyeh/ig/naming/NonExceptionNameEndsWithExceptionInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/naming/NonExceptionNameEndsWithExceptionInspection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2008 Dave Griffith, Bas Leijdekkers
+ * Copyright 2003-2009 Dave Griffith, Bas Leijdekkers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -84,6 +84,12 @@ public class NonExceptionNameEndsWithExceptionInspection
             final PsiJavaCodeReferenceElement reference =
                     factory.createReferenceElementByFQClassName(
                             "java.lang.Exception", scope);
+            final PsiJavaCodeReferenceElement[] referenceElements =
+                    extendsList.getReferenceElements();
+            for (PsiJavaCodeReferenceElement referenceElement :
+                    referenceElements) {
+                referenceElement.delete();
+            }
             extendsList.add(reference);
         }
     }
