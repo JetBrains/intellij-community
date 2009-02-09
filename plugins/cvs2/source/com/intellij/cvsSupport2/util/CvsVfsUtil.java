50970f457fb9b6b48518c01819d9b0c5e267a475
Dmitry Jemerov <yole@intellij.com>
2009-02-09 21:03:48 +0300

    CVS deadlock fixed (IDEADEV-34595)

--- plugins/cvs2/source/com/intellij/cvsSupport2/util/CvsVfsUtil.java
+++ plugins/cvs2/source/com/intellij/cvsSupport2/util/CvsVfsUtil.java
@@ -3,7 +3,6 @@ package com.intellij.cvsSupport2.util;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 
@@ -17,28 +16,16 @@ public class CvsVfsUtil {
   private static final Logger LOG = Logger.getInstance("#com.intellij.cvsSupport2.util.CvsVfsUtil");
 
   public static VirtualFile findFileByPath(final String path) {
-    final VirtualFile[] result = new VirtualFile[1];
-    ApplicationManager.getApplication().runReadAction(new Runnable() {
-      public void run() {
-        result[0] = LocalFileSystem.getInstance().findFileByPath(path);
-      }
-    });
-    return result[0];
+    return LocalFileSystem.getInstance().findFileByPath(path);
   }
 
   public static VirtualFile findChild(final VirtualFile file, final String name) {
-    final VirtualFile[] result = new VirtualFile[1];
-    ApplicationManager.getApplication().runReadAction(new Runnable() {
-      public void run() {
-        if (file == null) {
-          result[0] = LocalFileSystem.getInstance().findFileByIoFile(new File(name));
-        }
-        else {
-          result[0] = file.findChild(name);
-        }
-      }
-    });
-    return result[0];
+    if (file == null) {
+      return LocalFileSystem.getInstance().findFileByIoFile(new File(name));
+    }
+    else {
+      return file.findChild(name);
+    }
   }
 
   public static VirtualFile getParentFor(final File file) {
@@ -61,23 +48,7 @@ public class CvsVfsUtil {
 
   public static VirtualFile findFileByIoFile(final File file) {
     if (file == null) return null;
-    final VirtualFile[] result = new VirtualFile[1];
-
-    ApplicationManager.getApplication().runReadAction(new Runnable() {
-      public void run() {
-        result[0] = LocalFileSystem.getInstance().findFileByPath(file.getAbsolutePath().replace(File.separatorChar,
-                                                                                                '/'));
-      }
-    });
-
-    if (result[0] != null) return result[0];
-
-    ApplicationManager.getApplication().runReadAction(new Runnable() {
-      public void run() {
-        result[0] = LocalFileSystem.getInstance().findFileByIoFile(file);
-      }
-    });
-    return result[0];
+    return LocalFileSystem.getInstance().findFileByIoFile(file);
   }
 
   public static VirtualFile refreshAndFindFileByIoFile(final File file) {
@@ -108,44 +79,19 @@ public class CvsVfsUtil {
   }
 
   public static VirtualFile[] getChildrenOf(final VirtualFile directory) {
-    final VirtualFile[][] result = new VirtualFile[1][];
-    ApplicationManager.getApplication().runReadAction(new Runnable() {
-      public void run() {
-        if (directory.isValid()) {
-          result[0] = directory.getChildren();
-        }
-      }
-    });
-    return result[0];
+    return directory.isValid() ? directory.getChildren() : null;
   }
 
   public static long getTimeStamp(final VirtualFile file) {
-    final long[] result = new long[1];
-    ApplicationManager.getApplication().runReadAction(new Runnable() {
-      public void run() {
-        result[0] = file.getTimeStamp();
-      }
-    });
-    return result[0];
+    return file.getTimeStamp();
   }
 
   public static boolean isWritable(final VirtualFile virtualFile) {
-    final boolean[] result = new boolean[]{false};
-    ApplicationManager.getApplication().runReadAction(new Runnable() {
-      public void run() {
-        result[0] = virtualFile.isWritable();
-      }
-    });
-    return result[0];
-
+    return virtualFile.isWritable();
   }
 
   public static String getPresentablePathFor(final VirtualFile root) {
-    return ApplicationManager.getApplication().runReadAction(new Computable<String>() {
-      public String compute() {
-        return root.getPresentableUrl();
-      }
-    });
+    return root.getPresentableUrl();
   }
 
   public static VirtualFile refreshAndfFindChild(VirtualFile parent, String fileName) {
