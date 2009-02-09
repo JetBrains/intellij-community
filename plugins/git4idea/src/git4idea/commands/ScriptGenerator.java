0718f8c6a7dcbe43a508f7b48236e43fc9c258f7
Constantine Plotnikov <Constantine.Plotnikov@jetbrains.com>
2009-02-09 12:29:39 +0300

    git4idea: readded code lost in refactoring

--- plugins/git4idea/src/git4idea/commands/ScriptGenerator.java
+++ plugins/git4idea/src/git4idea/commands/ScriptGenerator.java
@@ -17,6 +17,7 @@ package git4idea.commands;
 
 import com.intellij.openapi.application.PathManager;
 import com.intellij.openapi.util.SystemInfo;
+import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.util.PathUtil;
 import org.jetbrains.annotations.NonNls;
 
@@ -153,6 +154,7 @@ public class ScriptGenerator {
     finally {
       out.close();
     }
+    FileUtil.setExectuableAttribute(scriptPath.getPath(), true);
     return scriptPath;
   }
 
