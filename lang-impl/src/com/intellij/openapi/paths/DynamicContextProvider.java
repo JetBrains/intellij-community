c6bb785c029d91d2201aa946a1abaf0f403db5a6
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2009-02-13 12:17:23 +0300

    javadoc

--- lang-impl/src/com/intellij/openapi/paths/DynamicContextProvider.java
+++ lang-impl/src/com/intellij/openapi/paths/DynamicContextProvider.java
@@ -10,5 +10,13 @@ public interface DynamicContextProvider {
 
   ExtensionPointName<DynamicContextProvider> EP_NAME = ExtensionPointName.create("com.intellij.dynamicContextProvider");
 
+  /**
+   * Returns starting position for file references
+   *
+   * @param element
+   * @param offset initial offset
+   * @param elementText
+   * @return -1 to suppress file references generation
+   */
   int getOffset(final PsiElement element, final int offset, final String elementText);
 }
