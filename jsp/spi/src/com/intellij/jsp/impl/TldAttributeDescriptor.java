c6cd0810a7a53dea5fdfbe7180fe59dd9bd446d1
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2009-02-20 10:03:55 +0300

    TldAttributeDescriptor.getMethodSignature() introduced

--- jsp/spi/src/com/intellij/jsp/impl/TldAttributeDescriptor.java
+++ jsp/spi/src/com/intellij/jsp/impl/TldAttributeDescriptor.java
@@ -1,6 +1,7 @@
 package com.intellij.jsp.impl;
 
 import com.intellij.xml.XmlAttributeDescriptor;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Created by IntelliJ IDEA.
@@ -10,5 +11,24 @@ import com.intellij.xml.XmlAttributeDescriptor;
  * To change this template use File | Settings | File Templates.
  */
 public interface TldAttributeDescriptor extends XmlAttributeDescriptor {
+
+  /**
+   * Indicates that attribute is a fragment
+   * @return
+   */
   boolean isIndirectSyntax();
+
+  /**
+   * Returns method signature for deferred methods
+   * @return
+   */
+  @Nullable
+  String getMethodSignature();
+
+  /**
+   * Returns value of "type" subtag
+   * @return
+   */
+  @Nullable
+  String getType();
 }
