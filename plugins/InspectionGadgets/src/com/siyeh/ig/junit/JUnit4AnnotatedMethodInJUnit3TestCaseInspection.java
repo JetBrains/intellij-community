4866dee9058e3e2e74b8fa4dc6b73fb8e6e3635a
Bas Leijdekkers <basleijdekkers@gmail.com>
2009-02-23 14:31:47 +0300

    fix "JUnit 4 test method in class extending JUnit 3 TestCase" inspection

--- plugins/InspectionGadgets/src/com/siyeh/ig/junit/JUnit4AnnotatedMethodInJUnit3TestCaseInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/junit/JUnit4AnnotatedMethodInJUnit3TestCaseInspection.java
@@ -64,7 +64,7 @@ public class JUnit4AnnotatedMethodInJUnit3TestCaseInspection extends
     private static class RemoveExtendsTestCaseFix extends InspectionGadgetsFix {
         private final String className;
 
-        public RemoveExtendsTestCaseFix(String className) {
+        RemoveExtendsTestCaseFix(String className) {
             this.className = className;
         }
 
@@ -137,7 +137,7 @@ public class JUnit4AnnotatedMethodInJUnit3TestCaseInspection extends
         @Override
         public void visitMethod(PsiMethod method) {
             super.visitMethod(method);
-            if (TestUtils.isJUnit4TestMethod(method)) {
+            if (!TestUtils.isJUnit4TestMethod(method)) {
                 return;
             }
             final PsiClass containingClass = method.getContainingClass();
