027c151de8f5db37c6870db97c1cfaa572673175
Peter Gromov <peter@jetbrains.com>
2009-03-13 14:07:39 +0300

    enable type hierarchy in groovy

---
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/GroovyTypeHierarchyProvider.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2000-2005 by JetBrains s.r.o. All Rights Reserved.
+ * Use is subject to license terms.
+ */
+package org.jetbrains.plugins.groovy.lang;
+
+import com.intellij.ide.hierarchy.type.JavaTypeHierarchyProvider;
+
+/**
+ * @author peter
+ */
+public class GroovyTypeHierarchyProvider extends JavaTypeHierarchyProvider{
+}
