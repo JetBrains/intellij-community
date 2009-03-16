b9a77b34ada9435ef3303ef26df114d1b80b5a6c
Eugene Zhuravlev <jeka@jetbrains.com>
2009-03-16 15:52:29 +0300

    cache tuning

--- compiler/impl/com/intellij/compiler/make/DependencyCache.java
+++ compiler/impl/com/intellij/compiler/make/DependencyCache.java
@@ -71,7 +71,7 @@ public class DependencyCache {
     try {
       if (myCache == null) {
         // base number of cached record views of each type
-        myCache = new Cache(myStoreDirectoryPath, 128);
+        myCache = new Cache(myStoreDirectoryPath, 512);
       }
 
       return myCache;
@@ -84,7 +84,7 @@ public class DependencyCache {
   public Cache getNewClassesCache() throws CacheCorruptedException {
     try {
       if (myNewClassesCache == null) {
-        myNewClassesCache = new Cache(myStoreDirectoryPath + "/tmp", 128);
+        myNewClassesCache = new Cache(myStoreDirectoryPath + "/tmp", 16);
       }
       return myNewClassesCache;
     }
