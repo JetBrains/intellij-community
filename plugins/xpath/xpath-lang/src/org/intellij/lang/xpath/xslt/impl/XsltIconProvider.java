961f3ae4f3fa31cdababbe3b85cf14bca3dcf543
Sascha Weinreuter <no_reply@jetbrains.com>
2009-03-18 13:00:12 +0300

    fixed "Loop in icons delegation" error for XSLT files (IDEADEV-35016)

--- plugins/xpath/xpath-lang/src/org/intellij/lang/xpath/xslt/impl/XsltIconProvider.java
+++ plugins/xpath/xpath-lang/src/org/intellij/lang/xpath/xslt/impl/XsltIconProvider.java
@@ -4,58 +4,70 @@
  */
 package org.intellij.lang.xpath.xslt.impl;
 
-import com.intellij.ide.IconProvider;
-import com.intellij.openapi.util.Key;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
-import gnu.trove.TIntObjectHashMap;
 import org.intellij.lang.xpath.xslt.XsltConfig;
 import org.intellij.lang.xpath.xslt.XsltSupport;
+
+import com.intellij.ide.FileIconPatcher;
+import com.intellij.openapi.components.ApplicationComponent;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Key;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+
+import gnu.trove.TIntObjectHashMap;
 
 import javax.swing.*;
 
 /**
  * @author peter
  */
-public class XsltIconProvider extends IconProvider {
-  private boolean active;
+public class XsltIconProvider implements ApplicationComponent, FileIconPatcher {
 
-  private static final Key<TIntObjectHashMap<Icon>> ICON_KEY = Key.create("MY_CUSTOM_ICON");
-  private final XsltConfig myConfig;
+    private static final Key<TIntObjectHashMap<Icon>> ICON_KEY = Key.create("XSLT_ICON");
+    private final XsltConfig myConfig;
 
-  public XsltIconProvider(XsltConfig config) {
-    myConfig = config;
-  }
+    public XsltIconProvider(XsltConfig config) {
+        myConfig = config;
+    }
 
-  @Nullable
-  public synchronized Icon getIcon(@NotNull PsiElement element, int flags) {
-      if (active || !myConfig.isEnabled()) return null;
+    public Icon patchIcon(Icon baseIcon, VirtualFile file, int flags, Project project) {
+        if (!myConfig.isEnabled()) return baseIcon;
 
-      active = true;
-      try {
-          TIntObjectHashMap<Icon> icons = element.getUserData(ICON_KEY);
-          if (icons != null) {
-              final Icon icon = icons.get(flags);
-              if (icon != null) {
-                  return icon;
-              }
-          }
-          if (element instanceof PsiFile) {
-              if (XsltSupport.isXsltFile((PsiFile)element)) {
-                  if (icons == null) {
-                      element.putUserData(ICON_KEY, icons = new TIntObjectHashMap<Icon>(3));
-                  }
-                  final Icon i = XsltSupport.createXsltIcon(element.getIcon(flags));
-                  icons.put(flags, i);
-                  return i;
-              }
-          }
-          return null;
-      } finally {
-          active = false;
-      }
-  }
+        final TIntObjectHashMap<Icon> icons = file.getUserData(ICON_KEY);
+        if (icons != null) {
+            final Icon icon = icons.get(flags);
+            if (icon != null) {
+                return icon;
+            }
+        }
 
+        final PsiFile element = PsiManager.getInstance(project).findFile(file);
+        if (element != null) {
+            if (XsltSupport.isXsltFile(element)) {
+                return cacheIcon(file, flags, icons, XsltSupport.createXsltIcon(baseIcon));
+            }
+        }
+        return baseIcon;
+    }
+
+    private static Icon cacheIcon(VirtualFile file, int flags, TIntObjectHashMap<Icon> icons, Icon icon) {
+        if (icons == null) {
+            file.putUserData(ICON_KEY, icons = new TIntObjectHashMap<Icon>(3));
+        }
+        icons.put(flags, icon);
+        return icon;
+    }
+
+    @NotNull
+    public String getComponentName() {
+        return "XSLT Icon Provider";
+    }
+
+    public void initComponent() {
+    }
+
+    public void disposeComponent() {
+    }
 }
