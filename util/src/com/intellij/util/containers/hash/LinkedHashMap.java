864af0b4fb090540b9a12d5989459694ff45afc7
Eugene Zhuravlev <jeka@jetbrains.com>
2009-03-25 15:16:34 +0300

    formatting

--- util/src/com/intellij/util/containers/hash/LinkedHashMap.java
+++ util/src/com/intellij/util/containers/hash/LinkedHashMap.java
@@ -1,671 +1,339 @@
 package com.intellij.util.containers.hash;
 
 
-
 import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
 
 
-
 public class LinkedHashMap<K, V> extends AbstractMap<K, V> implements Map<K, V> {
-
-
-
   private Entry<K, V>[] table;
-
   private Entry<K, V> top;
-
   private Entry<K, V> back;
-
   private int capacity;
-
   private int size;
-
   private final float loadFactor;
 
 
-
   public LinkedHashMap() {
-
     this(0);
-
   }
-
-
 
   public LinkedHashMap(int capacity) {
-
     this(capacity, HashUtil.DEFAULT_LOAD_FACTOR);
-
   }
-
-
 
   public LinkedHashMap(int capacity, float loadFactor) {
-
     this.loadFactor = loadFactor;
-
     clear(capacity);
-
   }
-
-
 
   public int size() {
-
     return size;
-
   }
-
-
 
   public boolean isEmpty() {
-
     return size() == 0;
-
   }
-
-
 
   public void clear() {
-
     clear(0);
-
   }
 
-
-
   public V get(final Object key) {
-
     final Entry<K, V>[] table = this.table;
-
     final int hash = HashUtil.hash(key);
-
     final int index = hash % table.length;
 
-
-
     for (Entry<K, V> e = table[index]; e != null; e = e.hashNext) {
-
       final K entryKey;
-
       if (e.keyHash == hash && ((entryKey = e.key) == key || entryKey.equals(key))) {
-
         moveToTop(e);
-
         return e.value;
 
       }
-
     }
-
-
-
     return null;
-
   }
-
-
 
   public V put(final K key, @NotNull final V value) {
-
     final Entry<K, V>[] table = this.table;
-
     final int hash = HashUtil.hash(key);
-
     final int index = hash % table.length;
-
-
-
     for (Entry<K, V> e = table[index]; e != null; e = e.hashNext) {
-
       final K entryKey;
-
       if (e.keyHash == hash && ((entryKey = e.key) == key || entryKey.equals(key))) {
-
         moveToTop(e);
-
         return e.setValue(value);
-
       }
-
     }
-
-
-
     final Entry<K, V> e = new Entry<K, V>(key, value);
-
     e.hashNext = table[index];
-
     table[index] = e;
-
     final Entry<K, V> top = this.top;
-
     e.next = top;
-
     if (top != null) {
-
       top.previous = e;
-
-    } else {
-
+    }
+    else {
       back = e;
-
     }
-
     this.top = e;
-
     size++;
-
-
-
     if (removeEldestEntry(back)) {
-
       remove(back.key);
-
-    } else if (size > capacity) {
-
-      rehash((int) (capacity * HashUtil.CAPACITY_MULTIPLE));
-
     }
-
+    else if (size > capacity) {
+      rehash((int)(capacity * HashUtil.CAPACITY_MULTIPLE));
+    }
     return null;
-
   }
-
-
 
   public boolean containsKey(final Object key) {
-
     return get(key) != null;
-
   }
-
-
 
   public V remove(final Object key) {
-
     final Entry<K, V>[] table = this.table;
-
     final int hash = HashUtil.hash(key);
-
     final int index = hash % table.length;
-
     Entry<K, V> e = table[index];
-
-
-
     if (e == null) return null;
-
-
-
     K entryKey;
-
     if (e.keyHash == hash && ((entryKey = e.key) == key || entryKey.equals(key))) {
-
       table[index] = e.hashNext;
-
-    } else {
-
-      for (; ;) {
-
-        final Entry<K, V> last = e;
-
-        e = e.hashNext;
-
-        if (e == null) return null;
-
-        if (e.keyHash == hash && ((entryKey = e.key) == key || entryKey.equals(key))) {
-
-          last.hashNext = e.hashNext;
-
-          break;
-
-        }
-
-      }
-
     }
-
+    else {
+      for (; ;) {
+        final Entry<K, V> last = e;
+        e = e.hashNext;
+        if (e == null) return null;
+        if (e.keyHash == hash && ((entryKey = e.key) == key || entryKey.equals(key))) {
+          last.hashNext = e.hashNext;
+          break;
+        }
+      }
+    }
     unlink(e);
-
     size--;
-
     return e.value;
-
   }
-
-
 
   public Set<K> keySet() {
-
     return new KeySet();
-
   }
-
-
 
   public Collection<V> values() {
-
     return new Values();
-
   }
-
-
 
   public Set<Map.Entry<K, V>> entrySet() {
-
     return new EntrySet();
-
   }
-
-
 
   protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
-
     return false;
-
   }
-
-
 
   private void init(int capacity) {
-
-    table = new Entry[HashUtil.adjustTableSize((int) (capacity / loadFactor))];
-
+    table = new Entry[HashUtil.adjustTableSize((int)(capacity / loadFactor))];
     top = back = null;
-
     this.capacity = capacity;
-
   }
-
-
 
   private void clear(int capacity) {
-
     if (capacity < HashUtil.MIN_CAPACITY) {
-
       capacity = HashUtil.MIN_CAPACITY;
-
     }
-
     init(capacity);
-
     size = 0;
-
   }
-
-
 
   private void moveToTop(final Entry<K, V> e) {
-
     final Entry<K, V> top = this.top;
-
     if (top != e) {
-
       final Entry<K, V> prev = e.previous;
-
       final Entry<K, V> next = e.next;
-
       prev.next = next;
-
       if (next != null) {
-
         next.previous = prev;
-
-      } else {
-
-        back = prev;
-
       }
-
+      else {
+        back = prev;
+      }
       top.previous = e;
-
       e.next = top;
-
       e.previous = null;
-
       this.top = e;
-
     }
-
   }
-
-
 
   private void unlink(final Entry<K, V> e) {
-
     final Entry<K, V> prev = e.previous;
-
     final Entry<K, V> next = e.next;
-
     if (prev != null) {
-
       prev.next = next;
-
-    } else {
-
+    }
+    else {
       top = next;
-
     }
-
     if (next != null) {
-
       next.previous = prev;
-
-    } else {
-
-      back = prev;
-
     }
-
+    else {
+      back = prev;
+    }
   }
-
-
 
   private void rehash(int capacity) {
-
-    table = new Entry[HashUtil.adjustTableSize((int) (capacity / loadFactor))];
-
+    table = new Entry[HashUtil.adjustTableSize((int)(capacity / loadFactor))];
     this.capacity = capacity;
-
     final Entry<K, V>[] table = this.table;
-
     final int tableLen = table.length;
-
     for (Entry<K, V> e = back; e != null; e = e.previous) {
-
       final int hash = e.keyHash % tableLen;
-
       e.hashNext = table[hash];
-
       table[hash] = e;
-
     }
-
   }
-
-
-
-
 
   private static class Entry<K, V> implements Map.Entry<K, V> {
 
-
-
     private final K key;
-
     private final int keyHash;
-
     private V value;
-
     private Entry<K, V> next;
-
     private Entry<K, V> previous;
-
     private Entry<K, V> hashNext;
 
-
-
     public Entry(final K key, final V value) {
-
       this.key = key;
-
       keyHash = HashUtil.hash(key);
-
       this.value = value;
-
     }
-
-
 
     public K getKey() {
-
       return key;
-
     }
-
-
 
     public V getValue() {
-
       return value;
-
     }
-
-
 
     public V setValue(final V value) {
-
       final V result = this.value;
-
       this.value = value;
-
       return result;
-
     }
-
   }
-
-
 
   private abstract class LinkedHashIterator<T> implements Iterator<T> {
 
-
-
     private Entry<K, V> e = top;
-
     private Entry<K, V> last;
 
-
-
     public boolean hasNext() {
-
       return e != null;
-
     }
-
-
 
     public void remove() {
-
       if (last == null) {
-
         throw new IllegalStateException();
-
       }
-
       LinkedHashMap.this.remove(last.key);
-
       last = null;
-
     }
-
-
 
     protected Entry<K, V> nextEntry() {
-
       final Entry<K, V> result = last = e;
-
       e = result.next;
-
       return result;
-
     }
-
   }
-
-
 
   private final class EntrySet extends AbstractSet<Map.Entry<K, V>> {
 
-
-
     public Iterator<Map.Entry<K, V>> iterator() {
-
       return new LinkedHashIterator<Map.Entry<K, V>>() {
-
         public Map.Entry<K, V> next() {
-
           return nextEntry();
-
         }
-
       };
-
     }
-
-
 
     public boolean contains(Object o) {
-
       if (!(o instanceof Map.Entry)) {
-
         return false;
-
       }
-
-      final Map.Entry<K, V> e = (Map.Entry<K, V>) o;
-
+      final Map.Entry<K, V> e = (Map.Entry<K, V>)o;
       final V value = get(e.getKey());
-
       return value != null && value.equals(e.getValue());
-
     }
-
-
 
     public boolean remove(Object o) {
-
       if (!(o instanceof Map.Entry)) {
-
         return false;
-
       }
-
-      final Map.Entry<K, V> e = (Map.Entry<K, V>) o;
-
+      final Map.Entry<K, V> e = (Map.Entry<K, V>)o;
       return LinkedHashMap.this.remove(e.getKey()) != null;
-
     }
-
-
 
     public int size() {
-
       return size;
-
     }
-
-
 
     public void clear() {
-
       LinkedHashMap.this.clear();
-
     }
-
   }
-
-
 
   private final class KeySet extends AbstractSet<K> {
 
-
-
     public Iterator<K> iterator() {
-
       return new LinkedHashIterator<K>() {
-
         public K next() {
-
           return nextEntry().key;
-
         }
-
       };
-
     }
-
-
 
     public int size() {
-
       return size;
-
     }
-
-
 
     public boolean contains(Object o) {
-
       return LinkedHashMap.this.containsKey(o);
-
     }
-
-
 
     public boolean remove(Object o) {
-
       return LinkedHashMap.this.remove(o) != null;
-
     }
-
-
 
     public void clear() {
-
       LinkedHashMap.this.clear();
-
     }
-
   }
-
-
 
   private final class Values extends AbstractCollection<V> {
 
-
-
     public Iterator<V> iterator() {
-
       return new LinkedHashIterator<V>() {
-
         public V next() {
-
           return nextEntry().value;
-
         }
-
       };
-
     }
-
-
 
     public int size() {
-
       return size;
-
     }
-
-
 
     public boolean contains(Object o) {
-
       return containsValue(o);
-
     }
-
-
 
     public void clear() {
-
       LinkedHashMap.this.clear();
-
     }
-
   }
-
 }
