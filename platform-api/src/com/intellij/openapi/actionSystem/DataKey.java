ba16816d9d5d30b563dd135a9f3c553025cdd4e3
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2009-03-25 16:49:25 +0300

    javadocs

--- platform-api/src/com/intellij/openapi/actionSystem/DataKey.java
+++ platform-api/src/com/intellij/openapi/actionSystem/DataKey.java
@@ -28,6 +28,10 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Map;
 import java.util.HashMap;
 
+/**
+ * @param <T>
+ * @see com.intellij.openapi.actionSystem.PlatformDataKeys
+ */
 public class DataKey<T> {
   private static final Map<String, DataKey> ourDataKeyIndex = new HashMap<String, DataKey>();
 
