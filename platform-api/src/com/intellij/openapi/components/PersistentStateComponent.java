4bb3be31e1dcafa001060c5e161df2df5dbc7a2d
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2009-03-25 10:47:15 +0300

    javadocs

--- platform-api/src/com/intellij/openapi/components/PersistentStateComponent.java
+++ platform-api/src/com/intellij/openapi/components/PersistentStateComponent.java
@@ -37,6 +37,7 @@ public interface PersistentStateComponent<T> {
    * to be called at any moment of its lifecycle. The method can and will be called several times, if
    * config files were externally changed while IDEA running.
    * @param state loaded component state
+   * @see com.intellij.util.xmlb.XmlSerializerUtil#copyBean(Object, Object) 
    */
   void loadState(T state);
 }
