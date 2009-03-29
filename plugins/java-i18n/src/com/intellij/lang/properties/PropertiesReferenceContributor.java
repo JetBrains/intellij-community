1eea25c1cbb3a1b5c0e8edd4fe751d321d9bdd6e
Dmitry Jemerov <yole@intellij.com>
2009-03-29 13:31:15 +0400

    non-hacky way to inject Java class references into property values

--- plugins/java-i18n/src/com/intellij/lang/properties/PropertiesReferenceContributor.java
+++ plugins/java-i18n/src/com/intellij/lang/properties/PropertiesReferenceContributor.java
@@ -5,10 +5,14 @@
 package com.intellij.lang.properties;
 
 import com.intellij.codeInsight.AnnotationUtil;
+import com.intellij.lang.properties.psi.impl.PropertyValueImpl;
+import com.intellij.patterns.PsiJavaPatterns;
 import static com.intellij.patterns.PsiJavaPatterns.literalExpression;
 import static com.intellij.patterns.PsiJavaPatterns.psiNameValuePair;
-import com.intellij.psi.PsiReferenceContributor;
-import com.intellij.psi.PsiReferenceRegistrar;
+import com.intellij.psi.*;
+import com.intellij.psi.impl.source.resolve.reference.impl.providers.JavaClassReferenceProvider;
+import com.intellij.util.ProcessingContext;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author peter
@@ -19,5 +23,20 @@ public class PropertiesReferenceContributor extends PsiReferenceContributor{
     registrar.registerReferenceProvider(literalExpression().withParent(
       psiNameValuePair().withName(AnnotationUtil.PROPERTY_KEY_RESOURCE_BUNDLE_PARAMETER)),
                                         new ResourceBundleReferenceProvider());
+
+    registrar.registerReferenceProvider(PsiJavaPatterns.psiElement(PropertyValueImpl.class), new PsiReferenceProvider() {
+      @NotNull
+      @Override
+      public PsiReference[] getReferencesByElement(@NotNull PsiElement element, @NotNull ProcessingContext context) {
+        String text = element.getText();
+        String[] words = text.split("\\s");
+        if (words.length == 0) return PsiReference.EMPTY_ARRAY;
+        return new JavaClassReferenceProvider(element.getProject()){
+          public boolean isSoft() {
+            return true;
+          }
+        }.getReferencesByString(words[0], element, 0);
+      }
+    });
   }
 }
--- plugins/properties/src/com/intellij/lang/properties/psi/impl/PropertyValueImpl.java
+++ plugins/properties/src/com/intellij/lang/properties/psi/impl/PropertyValueImpl.java
@@ -1,56 +1,23 @@
 package com.intellij.lang.properties.psi.impl;
 
-import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
-import com.intellij.psi.impl.source.resolve.reference.impl.providers.GenericReferenceProvider;
+import com.intellij.psi.impl.source.resolve.reference.ReferenceProvidersRegistry;
 import com.intellij.psi.impl.source.tree.LeafPsiElement;
 import com.intellij.psi.tree.IElementType;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
-
 /**
  * @author cdr
  */
 public class PropertyValueImpl extends LeafPsiElement {
-  private static final Constructor<?> ourJavaClassListReferenceProviderConstructor;
-  private static final Method ourJavaClassListReferenceProviderGetReferencesMethod;
-
-  static {  // TODO: make java spi
-    Constructor<?> javaClassListReferenceProviderConstructor = null;
-    Method javaClassListReferenceProviderGetReferencesMethod = null;
-    try {
-      Class c = Class.forName("com.intellij.psi.impl.source.resolve.reference.impl.providers.JavaClassListReferenceProvider");
-      javaClassListReferenceProviderConstructor = c.getConstructor(Project.class);
-      javaClassListReferenceProviderGetReferencesMethod = c.getMethod("getReferencesByString",String.class, PsiElement.class, int.class);
-    } catch (Exception ex) {}
-    finally {
-      ourJavaClassListReferenceProviderConstructor = javaClassListReferenceProviderConstructor;
-      ourJavaClassListReferenceProviderGetReferencesMethod = javaClassListReferenceProviderGetReferencesMethod;
-    }
-  }
-
   public PropertyValueImpl(IElementType type, CharSequence text) {
     super(type, text);
   }
 
   @NotNull
   public PsiReference[] getReferences() {
-    if (ourJavaClassListReferenceProviderConstructor != null) {
-      String text = getText();
-      String[] words = text.split("\\s");
-      if (words.length == 0) return PsiReference.EMPTY_ARRAY;
-      try {
-        GenericReferenceProvider referenceProvider = (GenericReferenceProvider)ourJavaClassListReferenceProviderConstructor.newInstance(getProject());
-        referenceProvider.setSoft(true);
-        return (PsiReference[])ourJavaClassListReferenceProviderGetReferencesMethod.invoke(referenceProvider, words[0], this, 0);
-      } catch (Throwable ex) {}
-    }
-
-    return PsiReference.EMPTY_ARRAY;
+    return ReferenceProvidersRegistry.getReferencesFromProviders(this, PropertyValueImpl.class);
   }
 
   public PsiReference getReference() {
