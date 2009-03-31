05aabf8a53dbcb05602d5aea94739b88964d84e9
Maxim Shafirov <max@jetbrains.com>
2009-03-31 15:43:37 +0400

    More c stubs.

--- lang-api/src/com/intellij/psi/stubs/NamedStubBase.java
+++ lang-api/src/com/intellij/psi/stubs/NamedStubBase.java
@@ -1,19 +1,24 @@
 package com.intellij.psi.stubs;
 
 import com.intellij.psi.PsiNamedElement;
+import com.intellij.util.io.StringRef;
 
 /**
  * @author yole
  */
 public abstract class NamedStubBase<T extends PsiNamedElement> extends StubBase<T> implements NamedStub<T> {
-  protected final String myName;
+  private final StringRef myName;
 
-  protected NamedStubBase(final StubElement parent, final IStubElementType elementType, final String name) {
+  protected NamedStubBase(StubElement parent, IStubElementType elementType, StringRef name) {
     super(parent, elementType);
     myName = name;
   }
 
+  protected NamedStubBase(final StubElement parent, final IStubElementType elementType, final String name) {
+    this(parent, elementType, StringRef.fromString(name));
+  }
+
   public String getName() {
-    return myName;
+    return myName.getString();
   }
 }
