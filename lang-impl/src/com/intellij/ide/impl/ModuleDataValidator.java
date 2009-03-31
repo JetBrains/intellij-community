1ca5b66f510d61f9e398eff4419b4c4683b4ad55
Dmitry Jemerov <yole@intellij.com>
2009-03-31 17:31:18 +0400

    modules validator

---
+++ lang-impl/src/com/intellij/ide/impl/ModuleDataValidator.java
@@ -0,0 +1,12 @@
+package com.intellij.ide.impl;
+
+import com.intellij.openapi.module.Module;
+
+/**
+ * @author yole
+ */
+public class ModuleDataValidator extends DataValidator<Module> {
+  public Module findInvalid(String dataId, Module data, Object dataSource) {
+    return data.isDisposed() ? null : data;
+  }
+}
