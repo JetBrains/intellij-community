b0b30979bf1ae961245acdc6f34a6d8cae2ae301
Dmitry Jemerov <yole@intellij.com>
2009-03-31 21:10:42 +0400

    assertTreeEqual -> PlatformTestUtil

--- testFramework/src/com/intellij/testFramework/PlatformTestUtil.java
+++ testFramework/src/com/intellij/testFramework/PlatformTestUtil.java
@@ -1,11 +1,19 @@
 package com.intellij.testFramework;
 
+import com.intellij.ide.util.treeView.AbstractTreeNode;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.extensions.ExtensionPoint;
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.extensions.Extensions;
 import com.intellij.openapi.extensions.ExtensionsArea;
 import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.util.text.StringUtil;
+import junit.framework.Assert;
+import org.jetbrains.annotations.NonNls;
+
+import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreePath;
 
 /**
  * @author yole
@@ -23,5 +31,72 @@ public class PlatformTestUtil {
         extensionPoint.unregisterExtension(t);
       }
     });
+  }
+
+  protected static String toString(Object node) {
+    if (node instanceof AbstractTreeNode) {
+      return ((AbstractTreeNode)node).getTestPresentation();
+    }
+    else if (node == null) {
+      return "NULL";
+    }
+    else {
+      return node.toString();
+    }
+  }
+
+  public static String print(JTree tree, boolean withSelection) {
+    StringBuffer buffer = new StringBuffer();
+    Object root = tree.getModel().getRoot();
+    printImpl(tree, root, buffer, 0, withSelection);
+    return buffer.toString();
+  }
+
+  private static void printImpl(JTree tree, Object root, StringBuffer buffer, int level, boolean withSelection) {
+    DefaultMutableTreeNode defaultMutableTreeNode = (DefaultMutableTreeNode)root;
+    boolean expanded = tree.isExpanded(new TreePath(defaultMutableTreeNode.getPath()));
+    StringUtil.repeatSymbol(buffer, ' ', level);
+    if (expanded && !defaultMutableTreeNode.isLeaf()) {
+      buffer.append("-");
+    }
+
+    if (!expanded && !defaultMutableTreeNode.isLeaf()) {
+      buffer.append("+");
+    }
+
+    final boolean selected = tree.getSelectionModel().isPathSelected(new TreePath(defaultMutableTreeNode.getPath()));
+
+    if (withSelection && selected) {
+      buffer.append("[");
+    }
+
+    final Object userObject = defaultMutableTreeNode.getUserObject();
+    if (userObject != null) {
+      buffer.append(toString(userObject));
+    }
+    else {
+      buffer.append(defaultMutableTreeNode);
+    }
+
+    if (withSelection && selected) {
+      buffer.append("]");
+    }
+
+    buffer.append("\n");
+    int childCount = tree.getModel().getChildCount(root);
+    if (expanded) {
+      for (int i = 0; i < childCount; i++) {
+        printImpl(tree, tree.getModel().getChild(root, i), buffer, level + 1, withSelection);
+      }
+    }
+  }
+
+  public static void assertTreeEqual(JTree tree, @NonNls String expected) {
+    assertTreeEqual(tree, expected, false);
+  }
+
+  public static void assertTreeEqual(JTree tree, String expected, boolean checkSelected) {
+    String treeStringPresentation = print(tree, checkSelected);
+    Assert.assertEquals(expected, treeStringPresentation);
   }
 }
