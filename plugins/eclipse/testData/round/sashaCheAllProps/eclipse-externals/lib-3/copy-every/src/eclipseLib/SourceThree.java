88d36a8f5784c1934a7f906a6fc440335433898f
Alexander Chernikov <Alexander.Chernikov@jetbrains.com>
2009-03-31 20:52:07 +0400

    Eclipse integration: round-trip test cases.

---
+++ plugins/eclipse/testData/round/sashaCheAllProps/eclipse-externals/lib-3/copy-every/src/eclipseLib/SourceThree.java
@@ -0,0 +1,14 @@
+package eclipseLib;
+
+/**
+ * Created in IntelliJ IDEA.
+ * By: Alexander.Chernikov
+ * When: 19.02.2009, 20:55:48
+ * Русский текст.
+ */
+public class SourceThree {
+	public static final String ELIB_CONST = "value 3";
+
+	public void elibMethod() {
+	}
+}
