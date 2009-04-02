c879d38787adac31eccfa9e731beb1f148e1ac37
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2009-04-02 13:00:38 +0400

    binding

---
+++ platform-impl/src/com/intellij/openapi/options/binding/BeanValueAccessor.java
@@ -0,0 +1,92 @@
+package com.intellij.openapi.options.binding;
+
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+
+/**
+ * @author Dmitry Avdeev
+*/
+public abstract class BeanValueAccessor extends ValueAccessor {
+
+  public static BeanValueAccessor createAccessor(Object bean, final String propertyName) {
+    Field[] fields = bean.getClass().getFields();
+    for (final Field field : fields) {
+      if (field.getName().equals(propertyName)) {
+        return new BeanValueAccessor(bean, propertyName) {
+          protected Object doGetValue() throws IllegalAccessException {
+            return field.get(myBean);
+          }
+
+          protected void doSetValue(Object value) throws IllegalAccessException {
+            field.set(myBean, value);
+          }
+
+          @Override
+          public Class getType() {
+            return field.getType();
+          }
+        };
+      }
+    }
+    try {
+      BeanInfo beanInfo = Introspector.getBeanInfo(bean.getClass());
+      for (final PropertyDescriptor descriptor : beanInfo.getPropertyDescriptors()) {
+        if (descriptor.getName().equals(propertyName)) {
+          return new BeanValueAccessor(bean, propertyName) {
+            @Override
+            protected Object doGetValue() throws Exception {
+              return descriptor.getReadMethod().invoke(myBean);
+            }
+
+            @Override
+            protected void doSetValue(Object value) throws Exception {
+              descriptor.getWriteMethod().invoke(myBean, value);
+            }
+
+            @Override
+            public Class getType() {
+              return descriptor.getPropertyType();
+            }
+          };
+        }
+      }
+      throw new IllegalArgumentException("Property " + propertyName + " not found in " + bean.getClass());
+    }
+    catch (IntrospectionException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  protected final Object myBean;
+  protected final String myPropertyName;
+
+  public BeanValueAccessor(Object bean, String propertyName) {
+    myBean = bean;
+    myPropertyName = propertyName;
+  }
+
+  public Object getValue() {
+    try {
+      return doGetValue();
+    }
+    catch (Exception e) {
+      throw new RuntimeException("Cannot access property " + myPropertyName, e);
+    }
+  }
+
+  public void setValue(Object value) {
+    try {
+      doSetValue(value);
+    }
+    catch (Exception e) {
+      throw new RuntimeException("Cannot access property " + myPropertyName, e);
+    }
+  }
+
+  protected abstract Object doGetValue() throws Exception;
+
+  protected abstract void doSetValue(Object value) throws Exception;
+}
