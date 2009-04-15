43b1f2189d125b78c8929bf98378c994e298a17f
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2009-04-15 18:22:33 +0400

    xml paste narrowed

--- xml/impl/src/com/intellij/codeInsight/editorActions/XmlCopyPastePreProcessor.java
+++ xml/impl/src/com/intellij/codeInsight/editorActions/XmlCopyPastePreProcessor.java
@@ -1,7 +1,6 @@
 package com.intellij.codeInsight.editorActions;
 
 import com.intellij.lang.ASTNode;
-import com.intellij.lang.xml.XMLLanguage;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.RawText;
@@ -9,10 +8,12 @@ import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.xml.XmlElementType;
-import com.intellij.psi.xml.XmlAttributeValue;
-import com.intellij.psi.impl.source.xml.behavior.EncodeEachSymbolPolicy;
 import com.intellij.psi.impl.source.tree.TreeUtil;
+import com.intellij.psi.impl.source.xml.behavior.EncodeEachSymbolPolicy;
+import com.intellij.psi.util.PsiUtilBase;
+import com.intellij.psi.xml.XmlAttributeValue;
+import com.intellij.psi.xml.XmlElement;
+import com.intellij.psi.xml.XmlElementType;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -31,13 +32,17 @@ public class XmlCopyPastePreProcessor implements CopyPastePreProcessor {
     final Document document = editor.getDocument();
     PsiDocumentManager.getInstance(project).commitDocument(document);
     int caretOffset = editor.getCaretModel().getOffset();
-    PsiElement element = file.findElementAt(caretOffset);
-    if (element != null && element.getLanguage() instanceof XMLLanguage) {
-      if (!(element instanceof XmlAttributeValue) && (text.indexOf('>') >= 0 || text.indexOf('<') >= 0)) {
-        return text;
+    PsiElement element = PsiUtilBase.getElementAtOffset(file, caretOffset);
+
+    if (element != null && element instanceof XmlElement) {
+      boolean hasMarkup = text.indexOf('>') >= 0 || text.indexOf('<') >= 0;
+      if (hasMarkup) {
+        if (!(element instanceof XmlAttributeValue)) {
+          return text;
+        }
       }
-      ASTNode cdata = TreeUtil.findParent(element.getNode(), XmlElementType.XML_CDATA);
-      if (cdata == null) {
+      if (TreeUtil.findParent(element.getNode(), XmlElementType.XML_CDATA) == null &&
+          TreeUtil.findParent(element.getNode(), XmlElementType.XML_COMMENT) == null) {
         ASTNode astNode = ENCODE_EACH_SYMBOL_POLICY.encodeXmlTextContents(text, element);
         return astNode.getTreeParent().getText();
       }
