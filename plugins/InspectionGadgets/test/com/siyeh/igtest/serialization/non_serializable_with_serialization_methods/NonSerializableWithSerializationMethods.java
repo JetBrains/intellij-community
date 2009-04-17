7954457f73730c75c88c0d6ccf4c99252c014878
Bas Leijdekkers <basleijdekkers@gmail.com>
2009-04-17 16:58:34 +0400

    fix test

--- plugins/InspectionGadgets/test/com/siyeh/igtest/serialization/non_serializable_with_serialization_methods/NonSerializableWithSerializationMethods.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/serialization/non_serializable_with_serialization_methods/NonSerializableWithSerializationMethods.java
@@ -1,20 +1,20 @@
 package com.siyeh.igtest.serialization.non_serializable_with_serialization_methods;
 
-import java.io.*;
+
 
 public class NonSerializableWithSerializationMethods
 {
     private static final long serialVersionUID = 1;
 
-    private void readObject(ObjectInputStream str)
+    private void readObject(java.io.ObjectInputStream str)
     {
 
     }
 
-    private void writeObject(ObjectOutputStream str)
+    private void writeObject(java.io.ObjectOutputStream str)
     {
         new Object() {
-            void readObject(ObjectInputStream x) {}
+            void readObject(java.io.ObjectInputStream x) {}
         };
     }
 }
