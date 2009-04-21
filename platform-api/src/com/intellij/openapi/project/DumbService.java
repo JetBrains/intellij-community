97dafae70b98600b9cec4976eb852e0da1abae7d
Peter Gromov <peter@jetbrains.com>
2009-04-21 14:19:26 +0400

    DumbService

---
+++ platform-api/src/com/intellij/openapi/project/DumbService.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2000-2005 by JetBrains s.r.o. All Rights Reserved.
+ * Use is subject to license terms.
+ */
+package com.intellij.openapi.project;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.components.ServiceManager;
+import com.intellij.util.concurrency.Semaphore;
+import com.intellij.util.messages.Topic;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author peter
+ */
+public abstract class DumbService {
+  public static final boolean UPDATE_IN_BACKGROUND = !ApplicationManager.getApplication().isUnitTestMode() &&
+                                                     "true".equals(System.getProperty("update.indices.in.background"));
+
+  public static final Topic<DumbModeListener> DUMB_MODE = new Topic<DumbModeListener>("dumb mode", DumbModeListener.class);
+
+  public abstract boolean isDumb();
+
+  public abstract void runWhenSmart(Runnable runnable);
+
+  public void waitForSmartMode() {
+    assert !ApplicationManager.getApplication().isDispatchThread();
+    assert !ApplicationManager.getApplication().isReadAccessAllowed();
+
+    final Semaphore semaphore = new Semaphore();
+    semaphore.down();
+    runWhenSmart(new Runnable() {
+          public void run() {
+            semaphore.up();
+          }
+        });
+    semaphore.waitFor();
+  }
+
+  public void smartInvokeLater(@NotNull final Runnable runnable) {
+    ApplicationManager.getApplication().invokeLater(new Runnable() {
+      public void run() {
+        runWhenSmart(runnable);
+      }
+    });
+  }
+
+  public static DumbService getInstance() {
+    return ServiceManager.getService(DumbService.class);
+  }
+
+
+  public interface DumbModeListener {
+
+    /**
+     * The events arrive on EDT before write action
+     */
+    void beforeEnteringDumbMode();
+
+    /**
+     * The event arrives on EDT inside write action
+     */
+    void enteredDumbMode();
+
+    /**
+     * The event arrives on EDT inside write action
+     */
+    void exitDumbMode();
+
+  }
+}
---
+++ platform-impl/src/com/intellij/openapi/project/DumbServiceImpl.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright (c) 2000-2005 by JetBrains s.r.o. All Rights Reserved.
+ * Use is subject to license terms.
+ */
+package com.intellij.openapi.project;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
+import com.intellij.openapi.util.Computable;
+import com.intellij.util.Consumer;
+import com.intellij.util.containers.Queue;
+import com.intellij.util.messages.MessageBus;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @author peter
+ */
+public class DumbServiceImpl extends DumbService {
+  //@NonNls private static final String NOTIFICATION_ID = "dumb-mode";
+  private final AtomicBoolean myDumb = new AtomicBoolean();
+  private final DumbModeListener myPublisher;
+  private final Queue<Runnable> myUpdateQueue = new Queue<Runnable>(5);
+  private final Queue<Runnable> myAfterUpdateQueue = new Queue<Runnable>(5);
+
+  public DumbServiceImpl(MessageBus bus) {
+    myPublisher = bus.syncPublisher(DUMB_MODE);
+  }
+
+  public boolean isDumb() {
+    return myDumb.get();
+  }
+
+  public static DumbServiceImpl getInstance() {
+    return (DumbServiceImpl)ServiceManager.getService(DumbService.class);
+  }
+
+  @Override
+  public void runWhenSmart(Runnable runnable) {
+    if (!isDumb()) {
+      runnable.run();
+    } else {
+      synchronized (myAfterUpdateQueue) {
+        myAfterUpdateQueue.addLast(runnable);
+      }
+    }
+  }
+
+  public void queueIndexUpdate(@Nullable final Project project, @NotNull final Consumer<ProgressIndicator> action) {
+    final Runnable update = new Runnable() {
+      public void run() {
+        if (project != null && project.isDisposed()) {
+          updateFinished(project);
+          return;
+        }
+
+        ProgressManager.getInstance().run(new Task.Backgroundable(project, "Updating indices", false) {
+          @Override
+          public void run(@NotNull final ProgressIndicator indicator) {
+            indicator.setIndeterminate(false);
+            indicator.setText("Indexing...");
+            try {
+              action.consume(indicator);
+            }
+            finally {
+              ApplicationManager.getApplication().invokeLater(new DumbAwareRunnable() {
+                public void run() {
+                  updateFinished(project);
+                }
+              });
+            }
+          }
+        });
+      }
+    };
+
+    invokeOnEDT(new Runnable() {
+      public void run() {
+        myPublisher.beforeEnteringDumbMode();
+
+        final boolean wasDumb = ApplicationManager.getApplication().runWriteAction(new Computable<Boolean>() {
+          public Boolean compute() {
+            final boolean wasDumb = myDumb.getAndSet(true);
+            if (!wasDumb) {
+              myPublisher.enteredDumbMode();
+            }
+            return wasDumb;
+          }
+        }).booleanValue();
+
+        if (wasDumb) {
+          myUpdateQueue.addLast(update);
+        } else {
+          update.run();
+        }
+      }
+    });
+  }
+
+  private static void invokeOnEDT(Runnable runnable) {
+    if (ApplicationManager.getApplication().isDispatchThread()) {
+      runnable.run();
+    } else {
+      ApplicationManager.getApplication().invokeLater(runnable);
+    }
+  }
+
+  private void updateFinished(Project project) {
+    if (!myUpdateQueue.isEmpty()) {
+      //run next dumb action
+      myUpdateQueue.pullFirst().run();
+      return;
+    }
+
+    //leave dumb mode
+    ApplicationManager.getApplication().runWriteAction(new Runnable() {
+      public void run() {
+        myDumb.set(false);
+        myPublisher.exitDumbMode();
+      }
+    });
+
+    if (project == null || project.isDisposed()) return;
+
+    while (true) {
+      final Runnable runnable;
+      synchronized (myAfterUpdateQueue) {
+        if (myAfterUpdateQueue.isEmpty()) {
+          break;
+        }
+
+        runnable = myAfterUpdateQueue.pullFirst();
+      }
+      runnable.run();
+    }
+  }
+
+
+}
