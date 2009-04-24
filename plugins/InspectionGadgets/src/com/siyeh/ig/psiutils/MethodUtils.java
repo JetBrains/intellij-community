f413d53d3d15119e24bc5a8651c1efc528c4605f
Maxim Shafirov <max@jetbrains.com>
2009-04-24 18:35:40 +0400

    Don't report equals(), hashCode(), toString() missing @Override annotations. Their supermethods will hardly ever miss from java.lang.Object().

--- plugins/InspectionGadgets/src/com/siyeh/ig/psiutils/MethodUtils.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/psiutils/MethodUtils.java
@@ -1,17 +1,17 @@
 /*
- * Copyright 2003-2007 Dave Griffith, Bas Leijdekkers
+ * Copyright 2000-2009 JetBrains s.r.o.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package com.siyeh.ig.psiutils;
 
@@ -49,6 +49,14 @@ public class MethodUtils{
         }
         return methodMatches(method, null, PsiType.INT,
                 HardcodedMethodConstants.HASH_CODE);
+    }
+
+    public static boolean isToString(@Nullable PsiMethod method){
+        if (method == null) {
+            return false;
+        }
+        return methodMatches(method, null, PsiType.getJavaLangString(method.getManager(), method.getResolveScope()),
+                HardcodedMethodConstants.TO_STRING);
     }
 
     public static boolean isEquals(@Nullable PsiMethod method){
