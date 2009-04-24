7d9463aaa954541a04b58e211563bacb5a3857ea
Maxim Shafirov <max@jetbrains.com>
2009-04-24 18:35:15 +0400

    Don't report equals(), hashCode(), toString() missing @Override annotations. Their supermethods will hardly ever miss from java.lang.Object().

--- plugins/InspectionGadgets/src/com/siyeh/ig/classlayout/MissingOverrideAnnotationInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/classlayout/MissingOverrideAnnotationInspection.java
@@ -1,17 +1,17 @@
 /*
- * Copyright 2003-2009 Dave Griffith, Bas Leijdekkers
+ * Copyright 2000-2009 JetBrains s.r.o.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package com.siyeh.ig.classlayout;
 
@@ -24,6 +24,7 @@ import com.siyeh.InspectionGadgetsBundle;
 import com.siyeh.ig.BaseInspection;
 import com.siyeh.ig.BaseInspectionVisitor;
 import com.siyeh.ig.InspectionGadgetsFix;
+import com.siyeh.ig.psiutils.MethodUtils;
 import org.jetbrains.annotations.NotNull;
 
 public class MissingOverrideAnnotationInspection extends BaseInspection {
@@ -117,6 +118,11 @@ public class MissingOverrideAnnotationInspection extends BaseInspection {
             } else if (!isJdk5Override(method)){
                 return;
             }
+
+            if(MethodUtils.isHashCode(method) || MethodUtils.isEquals(method) || MethodUtils.isToString(method)){
+                return;
+            }
+
             if(hasOverrideAnnotation(method)){
                 return;
             }
