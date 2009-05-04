287f79790c074ff2a966b8737eee8de68bb130f1
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2009-05-04 16:00:15 +0400

    Google App Engine: fix for 8.x

--- plugins/google-app-engine/source/com/intellij/appengine/server/instance/AppEngineServerInstance.java
+++ plugins/google-app-engine/source/com/intellij/appengine/server/instance/AppEngineServerInstance.java
@@ -1,13 +1,14 @@
 package com.intellij.appengine.server.instance;
 
 import com.intellij.debugger.DebuggerManager;
-import com.intellij.debugger.engine.DebugProcessAdapter;
 import com.intellij.debugger.engine.DebugProcess;
+import com.intellij.debugger.engine.DebugProcessAdapter;
 import com.intellij.debugger.engine.DefaultJSPPositionManager;
+import com.intellij.execution.process.OSProcessHandler;
 import com.intellij.execution.process.ProcessHandler;
+import com.intellij.javaee.facet.JavaeeFacetUtil;
 import com.intellij.javaee.run.configuration.CommonModel;
 import com.intellij.javaee.serverInstances.DefaultServerInstance;
-import com.intellij.javaee.facet.JavaeeFacetUtil;
 import com.intellij.openapi.project.Project;
 
 /**
@@ -20,6 +21,7 @@ public class AppEngineServerInstance extends DefaultServerInstance {
 
   @Override
   public void start(ProcessHandler processHandler) {
+    super.start(processHandler);
     final Project project = getCommonModel().getProject();
     DebuggerManager.getInstance(project).addDebugProcessListener(processHandler, new DebugProcessAdapter() {
       @Override
@@ -32,5 +34,15 @@ public class AppEngineServerInstance extends DefaultServerInstance {
         });
       }
     });
+  }
+
+  @Override
+  public void shutdown() {
+    super.shutdown();
+    ProcessHandler processHandler = getProcessHandler();
+    if (processHandler instanceof OSProcessHandler) {
+      //todo[nik] remove later. This fix is necessary only for IDEA 8.x
+      ((OSProcessHandler)processHandler).getProcess().destroy();
+    }
   }
 }
