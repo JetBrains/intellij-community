eb8c449854b37b9b2ab40f47d20aee60c8eb8ced
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2009-05-04 16:21:11 +0400

    IDEADEV-36535: Run configurations: temporary configuration limit value is not saved

--- lang-impl/src/com/intellij/execution/RunManagerConfig.java
+++ lang-impl/src/com/intellij/execution/RunManagerConfig.java
@@ -6,6 +6,7 @@ import com.intellij.execution.impl.RunManagerImpl;
 import com.intellij.execution.util.StoringPropertyContainer;
 import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.util.config.BooleanProperty;
+import org.jetbrains.annotations.NonNls;
 
 import java.util.Map;
 
@@ -13,12 +14,13 @@ public class RunManagerConfig {
   public static final String MAKE = ExecutionBundle.message("before.run.property.make");
   private static final BooleanProperty SHOW_SETTINGS = new BooleanProperty("showSettingsBeforeRunnig", true);
   private final StoringPropertyContainer myProperties;
+  private final PropertiesComponent myPropertiesComponent;
   private final RunManagerImpl myManager;
-
-  private int myRecentsLimit = 5;
+  @NonNls private static final String RECENTS_LIMIT = "recentsLimit";
 
   public RunManagerConfig(PropertiesComponent propertiesComponent,
                           RunManagerImpl manager) {
+    myPropertiesComponent = propertiesComponent;
     myManager = manager;
     myProperties = new StoringPropertyContainer("RunManagerConfig.", propertiesComponent);
   }
@@ -41,10 +43,15 @@ public class RunManagerConfig {
   }
 
   public int getRecentsLimit() {
-    return myRecentsLimit;
+    try {
+      return Integer.valueOf(myPropertiesComponent.getOrInit(RECENTS_LIMIT, "5")).intValue();
+    }
+    catch (NumberFormatException e) {
+      return 5;
+    }
   }
 
   public void setRecentsLimit(int recentsLimit) {
-    myRecentsLimit = recentsLimit;
+    myPropertiesComponent.setValue(RECENTS_LIMIT, Integer.toString(recentsLimit));
   }
 }
