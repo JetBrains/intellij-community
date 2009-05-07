6af11b5f2af9c94f9b5d0d8ad9ee44a1e67bd238
Peter Gromov <peter@jetbrains.com>
2009-05-07 21:18:17 +0400

    dumb-aware

--- platform-impl/src/com/intellij/ide/actions/ExitAction.java
+++ platform-impl/src/com/intellij/ide/actions/ExitAction.java
@@ -4,9 +4,10 @@ package com.intellij.ide.actions;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ex.ApplicationManagerEx;
+import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.util.SystemInfo;
 
-public class ExitAction extends AnAction {
+public class ExitAction extends AnAction implements DumbAware {
   public void update(AnActionEvent e) {
     e.getPresentation().setVisible(!SystemInfo.isMacSystemMenu);
   }
