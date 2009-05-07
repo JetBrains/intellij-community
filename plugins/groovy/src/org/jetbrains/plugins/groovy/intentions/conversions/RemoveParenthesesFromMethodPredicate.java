a707514d59850b9979fc7d42a22cac8da38e5a34
Dmitry Krasilschikov <Dmitry.Krasilschikov@@jetbrains.com>
2009-05-07 16:19:02 +0400

    Remove parenthesis intetion support fix GRVY-2023

--- plugins/groovy/src/org/jetbrains/plugins/groovy/intentions/conversions/RemoveParenthesesFromMethodPredicate.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/intentions/conversions/RemoveParenthesesFromMethodPredicate.java
@@ -17,12 +17,15 @@ package org.jetbrains.plugins.groovy.intentions.conversions;
 
 import com.intellij.psi.PsiElement;
 import org.jetbrains.plugins.groovy.intentions.base.PsiElementPredicate;
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.blocks.GrOpenBlock;
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.blocks.GrClosableBlock;
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.path.GrMethodCallExpression;
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrExpression;
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.arguments.GrArgumentList;
 import org.jetbrains.plugins.groovy.lang.psi.GroovyFile;
+import org.jetbrains.plugins.groovy.lang.psi.api.auxiliary.GrListOrMap;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.arguments.GrArgumentList;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.blocks.GrClosableBlock;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.blocks.GrOpenBlock;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrExpression;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrReferenceExpression;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrParenthesizedExpression;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.path.GrMethodCallExpression;
 
 class RemoveParenthesesFromMethodPredicate implements PsiElementPredicate {
 
@@ -30,16 +33,39 @@ class RemoveParenthesesFromMethodPredicate implements PsiElementPredicate {
     if (!(element instanceof GrMethodCallExpression)) {
       return false;
     }
-    final GrMethodCallExpression methodCallExpression = (GrMethodCallExpression) element;
+    final GrMethodCallExpression methodCallExpression = (GrMethodCallExpression)element;
     final GrArgumentList argumentList = methodCallExpression.getArgumentList();
-    if (argumentList ==  null) {
-      return false;
-    }
+    if (argumentList == null) return false;
+
     final GrExpression[] arguments = argumentList.getExpressionArguments();
-    if (arguments.length == 0) {
-      return false;
-    }
+    if (arguments.length == 0) return false;
+
+    GrExpression firstArg = arguments[0];
+    if (firstArg instanceof GrListOrMap) return false;
+
     final PsiElement parent = element.getParent();
+
+    if (firstArg instanceof GrMethodCallExpression) {
+      GrMethodCallExpression call = (GrMethodCallExpression)firstArg;
+
+      GrExpression invokedExpression = call.getInvokedExpression();
+
+      if (invokedExpression instanceof GrReferenceExpression) {
+        invokedExpression = getDeepestInvocationExpression(invokedExpression);
+      }
+
+      if (invokedExpression instanceof GrParenthesizedExpression) return false;
+    }
+
     return parent instanceof GrOpenBlock || parent instanceof GroovyFile || parent instanceof GrClosableBlock;
+  }
+
+  private GrExpression getDeepestInvocationExpression(GrExpression invokedExpression) {
+    if (invokedExpression instanceof GrReferenceExpression) {
+      GrReferenceExpression refElement = (GrReferenceExpression)invokedExpression;
+
+      return getDeepestInvocationExpression(refElement.getQualifierExpression());
+    }
+    return invokedExpression;
   }
 }
