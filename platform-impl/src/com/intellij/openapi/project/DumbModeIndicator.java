c7288502e7136157a2ad5c8a85d7b29eae979e39
Peter Gromov <peter@jetbrains.com>
2009-05-20 22:32:13 +0400

    replace nervous skull with a plain yellow stripe at the top indicating dumb mode

--- platform-impl/src/com/intellij/openapi/project/DumbModeIndicator.java
+++ platform-impl/src/com/intellij/openapi/project/DumbModeIndicator.java
@@ -4,100 +4,76 @@
  */
 package com.intellij.openapi.project;
 
-import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.ide.ui.UISettings;
+import com.intellij.openapi.wm.impl.IdeRootPaneNorthExtension;
 import com.intellij.openapi.ui.Messages;
-import com.intellij.openapi.util.IconLoader;
-import com.intellij.openapi.wm.StatusBar;
-import com.intellij.openapi.wm.StatusBarCustomComponentFactory;
-import com.intellij.util.Alarm;
-import com.intellij.util.containers.hash.HashSet;
-import com.intellij.util.ui.EmptyIcon;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.ui.LightColors;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.Set;
 
 /**
  * @author peter
  */
-class DumbModeIndicator extends StatusBarCustomComponentFactory<DumbModeIndicator.MyLabel> {
-  public static final Icon DUMB_MODE_ICON = IconLoader.findIcon("/debugger/killProcess.png");
-  private static final Icon EMPTY_ICON = new EmptyIcon(DUMB_MODE_ICON);
-  private static final int BLINKING_FREQUENCY = 500;
-  private final Set<MyLabel> myComponents = new HashSet<MyLabel>();
+class DumbModeIndicator extends IdeRootPaneNorthExtension {
+  private final JLabel myComponent = new JLabel("Index update is in progress...");
 
-
-  protected DumbModeIndicator() {
-    final Alarm alarm = new Alarm();
-    ApplicationManager.getApplication().getMessageBus().connect().subscribe(DumbService.DUMB_MODE, new DumbService.DumbModeListener() {
+  protected DumbModeIndicator(Project project) {
+    project.getMessageBus().connect().subscribe(DumbService.DUMB_MODE, new DumbService.DumbModeListener() {
       public void beforeEnteringDumbMode() {
       }
 
       public void enteredDumbMode() {
-        for (final MyLabel component : myComponents) {
-          component.setIcon(EMPTY_ICON);
-        }
-
-        alarm.addRequest(new Runnable() {
-          private long lastTime = System.currentTimeMillis();
-
-          public void run() {
-            final long curTime = System.currentTimeMillis();
-            boolean forceEmptyIcon = curTime - lastTime > 2 * BLINKING_FREQUENCY;
-            lastTime = curTime;
-
-            for (final MyLabel component : myComponents) {
-              component.setIcon(forceEmptyIcon || component.getIcon() == DUMB_MODE_ICON ? EMPTY_ICON : DUMB_MODE_ICON);
-            }
-            alarm.addRequest(this, BLINKING_FREQUENCY);
-          }
-        }, BLINKING_FREQUENCY);
+        myComponent.setVisible(true);
       }
 
       public void exitDumbMode() {
-        alarm.cancelAllRequests();
-        for (final MyLabel component : myComponents) {
-          component.setIcon(null);
-        }
+        myComponent.setVisible(false);
       }
     });
-  }
 
-  public MyLabel createComponent(@NotNull StatusBar statusBar) {
-    final MyLabel label = new MyLabel();
-    myComponents.add(label);
-    return label;
-  }
+    myComponent.setBorder(new EmptyBorder(3, 7, 3, 3));
+    myComponent.setOpaque(true);
+    myComponent.setBackground(LightColors.YELLOW);
 
-  @Override
-  public void disposeComponent(@NotNull StatusBar statusBar, @NotNull MyLabel c) {
-    myComponents.remove(c);
-  }
-
-  static class MyLabel extends JLabel {
-
-    private MyLabel() {
-      setToolTipText("Index update is in progress...");
-      addMouseListener(new MouseAdapter() {
+    myComponent.addMouseListener(new MouseAdapter() {
         @Override
         public void mouseReleased(MouseEvent e) {
           Messages.showMessageDialog("<html>" +
-                                     "Each time you see this icon in status bar, IntelliJ IDEA is indexing your source<br>" +
-                                     "and library files. This is needed for most smart functionality to work properly." +
+                                     "IntelliJ IDEA is now indexing your source and library files. These indices are<br>" +
+                                     "needed for most of the smart functionality to work properly." +
                                      "<p>" +
                                      "During this process some actions that require these indices won't be available,<br>" +
                                      "although you still can edit your files and work with VCS and file system.<br>" +
                                      "If you need smarter actions like Goto Declaration, Find Usages or refactorings,<br>" +
                                      "please wait until the update is finished. We appreciate your understanding." +
-                                     "</html>", "Don't panic!", DUMB_MODE_ICON);
+                                     "</html>", "Don't panic!", null);
         }
       });
-      setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-    }
+      myComponent.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+  }
 
+  public String getKey() {
+    return "dumb mode indicator";
+  }
+
+  public void uiSettingsChanged(UISettings settings) {
+  }
+
+  public void dispose() {
+  }
+
+  public JComponent getComponent() {
+    return myComponent;
+  }
+
+  public void installComponent(Project project, JPanel northPanel) {
+    northPanel.add(myComponent, new GridBagConstraints(0, 2, 2, 1, 1, 0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+
+    myComponent.setVisible(DumbService.getInstance().isDumb());
   }
 
 }
