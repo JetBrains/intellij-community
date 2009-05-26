fd1262ca2c92d0d3358905288525da18a412cc1b
Kirill Kalishev <Kirill.Kalishev@jetbrains.com>
2009-05-26 11:23:00 +0400

    "Hide side" fix when no editor open

--- platform-impl/src/com/intellij/openapi/wm/impl/ToolWindowManagerImpl.java
+++ platform-impl/src/com/intellij/openapi/wm/impl/ToolWindowManagerImpl.java
@@ -625,6 +625,12 @@ public final class ToolWindowManagerImpl extends ToolWindowManagerEx implements 
     deactivateToolWindowImpl(id, true, commandList);
 
     if (hideSide || info.isFloating()) {
+      final List<String> ids = myLayout.getVisibleIdsOn(info.getAnchor(), this);
+      for (String each : ids) {
+        myActiveStack.remove(each, true);
+      }
+
+
       while (!mySideStack.isEmpty(info.getAnchor())) {
         mySideStack.pop(info.getAnchor());
       }
