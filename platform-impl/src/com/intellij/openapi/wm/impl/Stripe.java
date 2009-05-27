358ba699a57f81bc876172da3d7bc58732534c10
Alexey Pegov <Alexey.Pegov@jetbrains.com>
2009-05-27 16:53:38 +0400

    IDEADEV-30516 Make a gap between vertical and horizontal stripe bars in top right and bottom right corners of main frame

--- platform-impl/src/com/intellij/openapi/wm/impl/Stripe.java
+++ platform-impl/src/com/intellij/openapi/wm/impl/Stripe.java
@@ -128,7 +128,7 @@ final class Stripe extends JPanel{
     if (toFitWith != null) {
       LayoutData layoutData = recomputeBounds(false, null, true);
       if (data.horizontal) {
-        gap = toFitWith.width - layoutData.size.width - data.eachX;
+        gap = toFitWith.width - myManager.getToolWindowsPane().getHorizontalInsetX() - layoutData.size.width - data.eachX ;
       }
       else {
         gap = toFitWith.height - layoutData.size.height - data.eachY;
