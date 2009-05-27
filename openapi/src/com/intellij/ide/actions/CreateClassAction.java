35e8ba2551472f52bf8b92de7e9a8debf69ee8ac
Peter Gromov <peter@jetbrains.com>
2009-05-27 21:10:23 +0400

    new class actions rework

--- openapi/src/com/intellij/ide/actions/CreateClassAction.java
+++ openapi/src/com/intellij/ide/actions/CreateClassAction.java
@@ -16,23 +16,30 @@
 
 package com.intellij.ide.actions;
 
+import com.intellij.codeInsight.lookup.LookupElement;
+import com.intellij.codeInsight.lookup.LookupElementFactoryImpl;
+import com.intellij.codeInsight.template.*;
 import com.intellij.ide.IdeBundle;
+import com.intellij.lang.ASTNode;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.LanguageLevelProjectExtension;
 import com.intellij.openapi.ui.Messages;
-import com.intellij.psi.JavaDirectoryService;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiDirectory;
-import com.intellij.psi.PsiElement;
+import com.intellij.pom.java.LanguageLevel;
+import com.intellij.psi.*;
 import com.intellij.util.Icons;
 import com.intellij.util.IncorrectOperationException;
+import com.intellij.util.ObjectUtils;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The standard "New Class" action.
  *
  * @since 5.1
  */
-public class CreateClassAction extends CreateInPackageActionBase {
+public class CreateClassAction extends CreateInPackageFromTemplateActionBase {
   public CreateClassAction() {
     super(IdeBundle.message("action.create.new.class"),
           IdeBundle.message("action.create.new.class"), Icons.CLASS_ICON);
@@ -60,12 +67,46 @@ public class CreateClassAction extends CreateInPackageActionBase {
     return IdeBundle.message("progress.creating.class", JavaDirectoryService.getInstance().getPackage(directory).getQualifiedName(), newName);
   }
 
-  protected void doCheckCreate(final PsiDirectory dir, final String className) throws IncorrectOperationException {
-    JavaDirectoryService.getInstance().checkCreateClass(dir, className);
+
+  protected PsiClass doCreateClass(final PsiDirectory dir, final String className) throws IncorrectOperationException {
+    return JavaDirectoryService.getInstance().createClass(dir, className);
   }
 
+  protected Template buildTemplate(PsiClass templateClass) {
+    final Project project = templateClass.getProject();
+    TemplateBuilder builder = new TemplateBuilder(templateClass.getContainingFile());
+    final ASTNode classToken = ObjectUtils.assertNotNull(templateClass.getNode()).findChildByType(JavaTokenType.CLASS_KEYWORD);
+    assert classToken != null;
+    builder.replaceElement(classToken.getPsi(), createTypeExpression(project), true);
+    builder.setEndVariableBefore(templateClass.getLBrace());
 
-  protected PsiClass doCreate(final PsiDirectory dir, final String className) throws IncorrectOperationException {
-    return JavaDirectoryService.getInstance().createClass(dir, className);
+    return builder.buildTemplate();
+  }
+
+  public static Expression createTypeExpression(final Project project) {
+    return new Expression() {
+      @Override
+      public Result calculateResult(ExpressionContext context) {
+        return new TextResult("class");
+      }
+
+      @Override
+      public Result calculateQuickResult(ExpressionContext context) {
+        return null;
+      }
+
+      @Override
+      public LookupElement[] calculateLookupItems(ExpressionContext context) {
+        final LookupElementFactoryImpl factory = LookupElementFactoryImpl.getInstance();
+        List<LookupElement> items = new ArrayList<LookupElement>();
+        items.add(factory.createLookupElement("class").setBold().setIcon(Icons.CLASS_ICON).setPriority(0));
+        items.add(factory.createLookupElement("interface").setBold().setIcon(Icons.INTERFACE_ICON).setPriority(0));
+        if (LanguageLevelProjectExtension.getInstance(project).getLanguageLevel().compareTo(LanguageLevel.JDK_1_5) >= 0) {
+          items.add(factory.createLookupElement("enum").setBold().setIcon(Icons.ENUM_ICON).setPriority(2));
+          items.add(factory.createLookupElement("@interface").setBold().setIcon(Icons.ANNOTATION_TYPE_ICON).setPriority(3));
+        }
+        return items.toArray(new LookupElement[items.size()]);
+      }
+    };
   }
 }
