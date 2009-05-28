9c3906eb16a5bbdf0513e2570aad09bed7d251e3
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2009-05-28 21:45:00 +0400

    register tools before profile is loaded

--- python/src/com/jetbrains/python/inspections/PythonPyInspectionToolProvider.java
+++ python/src/com/jetbrains/python/inspections/PythonPyInspectionToolProvider.java
@@ -1,15 +1,14 @@
 package com.jetbrains.python.inspections;
 
+import com.intellij.codeInspection.InspectionProfileEntry;
+import com.intellij.codeInspection.InspectionToolsFactory;
 import com.intellij.codeInspection.LocalInspectionTool;
 import com.intellij.codeInspection.ex.InspectionTool;
 import com.intellij.codeInspection.ex.InspectionToolRegistrar;
 import com.intellij.codeInspection.ex.LocalInspectionToolWrapper;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.components.ApplicationComponent;
-import com.intellij.openapi.util.Factory;
 import com.intellij.util.Function;
 import com.jetbrains.python.JythonManager;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.python.core.PyList;
 import org.python.core.PyObject;
@@ -17,7 +16,7 @@ import org.python.core.PyObject;
 /**
  * @author yole
  */
-public class PythonPyInspectionToolProvider implements ApplicationComponent {
+public class PythonPyInspectionToolProvider implements InspectionToolsFactory {
   public static PythonPyInspectionToolProvider getInstance() {
     return ApplicationManager.getApplication().getComponent(PythonPyInspectionToolProvider.class);
   }
@@ -26,34 +25,6 @@ public class PythonPyInspectionToolProvider implements ApplicationComponent {
 
   public PythonPyInspectionToolProvider(final InspectionToolRegistrar registrar) {
     myRegistrar = registrar;
-  }
-
-  @NotNull
-  public String getComponentName() {
-    return "PythonPyInspectionToolProvider";
-  }
-
-  public void initComponent() {
-    JythonManager manager = JythonManager.getInstance();
-    manager.execScriptFromResource("inspections/inspections.py");
-
-    final PyList pyList = (PyList) manager.eval("getAllInspections()");
-    int len = pyList.__len__();
-    for(int i=0; i<len; i++) {
-      final String inspectionToolName = pyList.__getitem__(i).toString();
-      myRegistrar.registerInspectionToolFactory(new PyInspectionToolFactory(inspectionToolName));
-    }
-    myRegistrar.registerInspectionToolProvider(new Function<String, InspectionTool>() {
-      @Nullable
-      public InspectionTool fun(String shortName) {
-        final LocalInspectionTool tool = createLocalInspectionTool(shortName);
-        return tool != null ? new LocalInspectionToolWrapper(tool) : null;
-      }
-    });
-  }
-
-  public void disposeComponent() {
-    //To change body of implemented methods use File | Settings | File Templates.
   }
 
   @Nullable
@@ -68,17 +39,25 @@ public class PythonPyInspectionToolProvider implements ApplicationComponent {
     }
   }
 
-  private static class PyInspectionToolFactory implements Factory<InspectionTool> {
-    private final String myInspectionToolName;
+  public InspectionProfileEntry[] createTools() {
+    myRegistrar.registerInspectionToolProvider(new Function<String, InspectionTool>() {
+      @Nullable
+      public InspectionTool fun(String shortName) {
+        final LocalInspectionTool tool = createLocalInspectionTool(shortName);
+        return tool != null ? new LocalInspectionToolWrapper(tool) : null;
+      }
+    });
 
-    public PyInspectionToolFactory(final String inspectionToolName) {
-      myInspectionToolName = inspectionToolName;
-    }
+    final JythonManager manager = JythonManager.getInstance();
+    manager.execScriptFromResource("inspections/inspections.py");
 
-    @Nullable
-    public InspectionTool create() {
-      final LocalInspectionTool tool = createLocalInspectionTool(myInspectionToolName);
-      return tool != null ? new LocalInspectionToolWrapper(tool) : null;
+    final PyList pyList = (PyList) manager.eval("getAllInspections()");
+    int len = pyList.__len__();
+    final InspectionProfileEntry[] profileEntries = new InspectionProfileEntry[len];
+    for(int i=0; i<len; i++) {
+      final String inspectionToolName = pyList.__getitem__(i).toString();
+      profileEntries[i] = createLocalInspectionTool(inspectionToolName);
     }
+    return profileEntries;
   }
 }
