3717fc5fdae174830f567570403b76df88f30bfe
Peter Gromov <peter@jetbrains.com>
2009-05-29 16:48:51 +0400

    stubs for .java & .groovy files in library classes aren't needed anymore

--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/impl/search/GrSourceFilterScope.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/impl/search/GrSourceFilterScope.java
@@ -1,5 +1,6 @@
 package org.jetbrains.plugins.groovy.lang.psi.impl.search;
 
+import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectFileIndex;
@@ -7,7 +8,7 @@ import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.search.GlobalSearchScope;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import org.jetbrains.plugins.groovy.GroovyFileType;
 
 /**
  * @author ilyas
@@ -22,16 +23,12 @@ public class GrSourceFilterScope extends GlobalSearchScope {
     myIndex = ProjectRootManager.getInstance(project).getFileIndex();
   }
 
-  public boolean contains(@Nullable final VirtualFile file) {
-    if (file == null) return false;
-
-    if (myDelegate != null && !myDelegate.contains(file)) {
-      return false;
+  public boolean contains(final VirtualFile file) {
+    final FileType fileType = file.getFileType();
+    return (myDelegate == null || myDelegate.contains(file)) &&
+            GroovyFileType.GROOVY_FILE_TYPE == fileType && myIndex.isInSourceContent(file);
     }
     
-    return myIndex.isInSourceContent(file) || myIndex.isInLibraryClasses(file);
-  }
-
   public int compare(final VirtualFile file1, final VirtualFile file2) {
     return myDelegate != null ? myDelegate.compare(file1, file2) : 0;
   }
