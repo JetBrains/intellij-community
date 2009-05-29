3faf346ce640539eb54ceb8af4c8a15c1c80e549
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2009-05-29 16:17:58 +0400

    run testng.xml in one click ( IDEADEV-37150)

--- plugins/testng/src/com/theoryinpractice/testng/configuration/TestNGConfigurationProducer.java
+++ plugins/testng/src/com/theoryinpractice/testng/configuration/TestNGConfigurationProducer.java
@@ -7,9 +7,6 @@ package com.theoryinpractice.testng.configuration;
 import com.intellij.execution.junit.JavaRuntimeConfigurationProducerBase;
 
 public abstract class TestNGConfigurationProducer extends JavaRuntimeConfigurationProducerBase implements Cloneable {
-  public static TestNGConfigurationProducer[] PROTOTYPES = new TestNGConfigurationProducer[] {
-    new TestNGInClassConfigurationProducer(), new TestNGPackageConfigurationProducer()
-  };
 
   public TestNGConfigurationProducer() {
     super(TestNGConfigurationType.getInstance());
---
+++ plugins/testng/src/com/theoryinpractice/testng/configuration/TestNGSuiteConfigurationProducer.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2000-2009 JetBrains s.r.o.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/*
+ * User: anna
+ * Date: 23-May-2007
+ */
+package com.theoryinpractice.testng.configuration;
+
+import com.intellij.execution.Location;
+import com.intellij.execution.RunnerAndConfigurationSettings;
+import com.intellij.execution.actions.ConfigurationContext;
+import com.intellij.execution.impl.RunnerAndConfigurationSettingsImpl;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.theoryinpractice.testng.model.TestType;
+import com.theoryinpractice.testng.util.TestNGUtil;
+import org.jetbrains.annotations.Nullable;
+
+public class TestNGSuiteConfigurationProducer extends TestNGConfigurationProducer{
+  private PsiElement myPsiElement = null;
+
+
+  public PsiElement getSourceElement() {
+    return myPsiElement;
+  }
+
+  @Nullable
+  protected RunnerAndConfigurationSettingsImpl createConfigurationByElement(Location location, ConfigurationContext context) {
+    final PsiElement element = location.getPsiElement();
+    final PsiFile containingFile = element.getContainingFile();
+    if (containingFile == null) return null;
+    final VirtualFile virtualFile = containingFile.getVirtualFile();
+    if (virtualFile == null) return null;
+    if (!TestNGUtil.isTestngXML(virtualFile)) return null;
+    myPsiElement = containingFile;
+    final Project project = location.getProject();
+    RunnerAndConfigurationSettings settings = cloneTemplateConfiguration(project, context);
+    final TestNGConfiguration configuration = (TestNGConfiguration)settings.getConfiguration();
+    setupConfigurationModule(context, configuration);
+    final Module originalModule = configuration.getConfigurationModule().getModule();
+    configuration.getPersistantData().SUITE_NAME = virtualFile.getPath();
+    configuration.getPersistantData().TEST_OBJECT = TestType.SUITE.getType();
+    configuration.restoreOriginalModule(originalModule);
+    settings.setName(configuration.getName());
+    copyStepsBeforeRun(project, configuration);
+    return (RunnerAndConfigurationSettingsImpl)settings;
+  }
+
+  public int compareTo(Object o) {
+    return PREFERED;
+  }
+}
