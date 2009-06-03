e7310b8d87fe4ed1cfc77afad2fcc0bf71e336f0
Bas Leijdekkers <basleijdekkers@gmail.com>
2009-06-03 16:23:37 +0400

    cleanup

--- plugins/IntentionPowerPak/src/com/siyeh/ipp/interfacetoclass/ConvertInterfaceToClassIntention.java
+++ plugins/IntentionPowerPak/src/com/siyeh/ipp/interfacetoclass/ConvertInterfaceToClassIntention.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2007 Bas Leijdekkers
+ * Copyright 2006-2009 Bas Leijdekkers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,11 +17,11 @@ package com.siyeh.ipp.interfacetoclass;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
-import com.intellij.psi.search.PsiSearchHelper;
 import com.intellij.psi.search.SearchScope;
 import com.intellij.psi.search.searches.ClassInheritorsSearch;
 import com.intellij.refactoring.util.CommonRefactoringUtil;
 import com.intellij.util.IncorrectOperationException;
+import com.intellij.util.Query;
 import com.siyeh.ipp.base.Intention;
 import com.siyeh.ipp.base.PsiElementPredicate;
 import org.jetbrains.annotations.NotNull;
@@ -31,123 +31,125 @@ import java.util.Collection;
 
 public class ConvertInterfaceToClassIntention extends Intention {
 
-	private static void changeInterfaceToClass(PsiClass anInterface)
-			throws IncorrectOperationException {
-		final PsiIdentifier nameIdentifier = anInterface.getNameIdentifier();
-		assert nameIdentifier != null;
-		final PsiElement whiteSpace = nameIdentifier.getPrevSibling();
-		assert whiteSpace != null;
-		final PsiElement interfaceToken = whiteSpace.getPrevSibling();
-		assert interfaceToken != null;
-		final PsiKeyword interfaceKeyword =
+    private static void changeInterfaceToClass(PsiClass anInterface)
+            throws IncorrectOperationException {
+        final PsiIdentifier nameIdentifier = anInterface.getNameIdentifier();
+        assert nameIdentifier != null;
+        final PsiElement whiteSpace = nameIdentifier.getPrevSibling();
+        assert whiteSpace != null;
+        final PsiElement interfaceToken = whiteSpace.getPrevSibling();
+        assert interfaceToken != null;
+        final PsiKeyword interfaceKeyword =
                 (PsiKeyword)interfaceToken.getOriginalElement();
-		final PsiManager manager = anInterface.getManager();
-          final PsiElementFactory factory = JavaPsiFacade.getInstance(manager.getProject()).getElementFactory();
-		final PsiKeyword classKeyword = factory.createKeyword("class");
-		interfaceKeyword.replace(classKeyword);
+        final Project project = anInterface.getProject();
+        final JavaPsiFacade psiFacade = JavaPsiFacade.getInstance(project);
+        final PsiElementFactory factory = psiFacade.getElementFactory();
+        final PsiKeyword classKeyword = factory.createKeyword("class");
+        interfaceKeyword.replace(classKeyword);
 
-		final PsiModifierList classModifierList = anInterface.getModifierList();
+        final PsiModifierList classModifierList = anInterface.getModifierList();
         if (classModifierList == null) {
             return;
         }
         classModifierList.setModifierProperty(PsiModifier.ABSTRACT, true);
-		final PsiElement parent = anInterface.getParent();
-		if (parent instanceof PsiClass) {
-			classModifierList.setModifierProperty(PsiModifier.STATIC, true);
-		}
-		final PsiMethod[] methods = anInterface.getMethods();
-		for (final PsiMethod method : methods) {
-			final PsiModifierList modifierList = method.getModifierList();
-			modifierList.setModifierProperty(PsiModifier.PUBLIC, true);
-			modifierList.setModifierProperty(PsiModifier.ABSTRACT, true);
-		}
-		final PsiField[] fields = anInterface.getFields();
-		for (final PsiField field : fields) {
-			final PsiModifierList modifierList = field.getModifierList();
+        final PsiElement parent = anInterface.getParent();
+        if (parent instanceof PsiClass) {
+            classModifierList.setModifierProperty(PsiModifier.STATIC, true);
+        }
+        final PsiMethod[] methods = anInterface.getMethods();
+        for (final PsiMethod method : methods) {
+            final PsiModifierList modifierList = method.getModifierList();
+            modifierList.setModifierProperty(PsiModifier.PUBLIC, true);
+            modifierList.setModifierProperty(PsiModifier.ABSTRACT, true);
+        }
+        final PsiField[] fields = anInterface.getFields();
+        for (final PsiField field : fields) {
+            final PsiModifierList modifierList = field.getModifierList();
             if (modifierList != null) {
                 modifierList.setModifierProperty(PsiModifier.PUBLIC, true);
                 modifierList.setModifierProperty(PsiModifier.STATIC, true);
                 modifierList.setModifierProperty(PsiModifier.FINAL, true);
             }
         }
-	}
+    }
 
-	protected void processIntention(@NotNull PsiElement element)
-			throws IncorrectOperationException {
-		final PsiClass anInterface = (PsiClass)element.getParent();
-        final boolean succes = moveSubClassImplementsToExtends(anInterface);
-        if (!succes) {
+    protected void processIntention(@NotNull PsiElement element)
+            throws IncorrectOperationException {
+        final PsiClass anInterface = (PsiClass)element.getParent();
+        final boolean success = moveSubClassImplementsToExtends(anInterface);
+        if (!success) {
             return;
         }
         changeInterfaceToClass(anInterface);
-		moveExtendsToImplements(anInterface);
-	}
+        moveExtendsToImplements(anInterface);
+    }
 
-	@NotNull
-	protected PsiElementPredicate getElementPredicate() {
-		return new ConvertInterfaceToClassPredicate();
-	}
+    @NotNull
+    protected PsiElementPredicate getElementPredicate() {
+        return new ConvertInterfaceToClassPredicate();
+    }
 
-	private static void moveExtendsToImplements(PsiClass anInterface)
-			throws IncorrectOperationException {
-		final PsiReferenceList extendsList = anInterface.getExtendsList();
-		final PsiReferenceList implementsList = anInterface.getImplementsList();
-		assert extendsList != null;
-		final PsiJavaCodeReferenceElement[] referenceElements =
-				extendsList.getReferenceElements();
-		for (PsiJavaCodeReferenceElement referenceElement : referenceElements) {
-			assert implementsList != null;
-			implementsList.add(referenceElement);
-			referenceElement.delete();
-		}
-	}
+    private static void moveExtendsToImplements(PsiClass anInterface)
+            throws IncorrectOperationException {
+        final PsiReferenceList extendsList = anInterface.getExtendsList();
+        final PsiReferenceList implementsList = anInterface.getImplementsList();
+        assert extendsList != null;
+        final PsiJavaCodeReferenceElement[] referenceElements =
+                extendsList.getReferenceElements();
+        for (PsiJavaCodeReferenceElement referenceElement : referenceElements) {
+            assert implementsList != null;
+            implementsList.add(referenceElement);
+            referenceElement.delete();
+        }
+    }
 
-	private static boolean moveSubClassImplementsToExtends(
+    private static boolean moveSubClassImplementsToExtends(
             PsiClass oldInterface) throws IncorrectOperationException {
-		final PsiManager psiManager = oldInterface.getManager();
-		final PsiSearchHelper searchHelper = psiManager.getSearchHelper();
-          final PsiElementFactory elementFactory = JavaPsiFacade.getInstance(psiManager.getProject()).getElementFactory();
-		final PsiJavaCodeReferenceElement oldInterfaceReference =
-				elementFactory.createClassReferenceElement(oldInterface);
-		final SearchScope searchScope = oldInterface.getUseScope();
-          final Collection<PsiClass> inheritors = ClassInheritorsSearch.search(oldInterface, searchScope, false).findAll();
         final Project project = oldInterface.getProject();
-        final boolean succes =
+        final JavaPsiFacade psiFacade = JavaPsiFacade.getInstance(project);
+        final PsiElementFactory elementFactory = psiFacade.getElementFactory();
+        final PsiJavaCodeReferenceElement oldInterfaceReference =
+                elementFactory.createClassReferenceElement(oldInterface);
+        final SearchScope searchScope = oldInterface.getUseScope();
+        final Query<PsiClass> query =
+                ClassInheritorsSearch.search(oldInterface, searchScope, false);
+        final Collection<PsiClass> inheritors = query.findAll();
+        final boolean success =
                 CommonRefactoringUtil.checkReadOnlyStatusRecursively(
                         project, inheritors, false);
-        if (!succes) {
+        if (!success) {
             return false;
         }
         for (PsiClass inheritor : inheritors) {
-			final PsiReferenceList implementsList =
+            final PsiReferenceList implementsList =
                     inheritor.getImplementsList();
-			final PsiReferenceList extendsList = inheritor.getExtendsList();
-			if (implementsList != null) {
-				moveReference(implementsList, extendsList,
-                              oldInterfaceReference);
-			}
-		}
+            final PsiReferenceList extendsList = inheritor.getExtendsList();
+            if (implementsList != null) {
+                moveReference(implementsList, extendsList,
+                        oldInterfaceReference);
+            }
+        }
         return true;
     }
 
-	private static void moveReference(
+    private static void moveReference(
             @NotNull PsiReferenceList source,
             @Nullable PsiReferenceList target,
             @NotNull PsiJavaCodeReferenceElement reference)
-			throws IncorrectOperationException {
-		final PsiJavaCodeReferenceElement[] implementsReferences =
-				source.getReferenceElements();
-		final String fqName = reference.getQualifiedName();
-		for (PsiJavaCodeReferenceElement implementsReference :
+            throws IncorrectOperationException {
+        final PsiJavaCodeReferenceElement[] implementsReferences =
+                source.getReferenceElements();
+        final String qualifiedName = reference.getQualifiedName();
+        for (PsiJavaCodeReferenceElement implementsReference :
                 implementsReferences) {
-			final String implementsReferenceFqName =
+            final String implementsReferenceQualifiedName =
                     implementsReference.getQualifiedName();
-			if (fqName.equals(implementsReferenceFqName)) {
-				if (target != null) {
-					target.add(implementsReference);
-				}
-				implementsReference.delete();
-			}
-		}
-	}
+            if (qualifiedName.equals(implementsReferenceQualifiedName)) {
+                if (target != null) {
+                    target.add(implementsReference);
+                }
+                implementsReference.delete();
+            }
+        }
+    }
 }
--- plugins/IntentionPowerPak/src/com/siyeh/ipp/parenthesis/UnnecessaryParenthesesPredicate.java
+++ plugins/IntentionPowerPak/src/com/siyeh/ipp/parenthesis/UnnecessaryParenthesesPredicate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2005 Dave Griffith
+ * Copyright 2003-2009 Dave Griffith, Bas Leijdekkers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,39 +23,39 @@ import com.siyeh.ipp.psiutils.ParenthesesUtils;
 
 class UnnecessaryParenthesesPredicate implements PsiElementPredicate{
 
-	public boolean satisfiedBy(PsiElement element){
-		if(!(element instanceof PsiParenthesizedExpression)){
-			return false;
-		}
-		if(ErrorUtil.containsError(element)){
-			return false;
-		}
-		final PsiParenthesizedExpression expression =
-				(PsiParenthesizedExpression) element;
-		final PsiElement parent = expression.getParent();
-		if(!(parent instanceof PsiExpression)){
-			return true;
-		}
-		final PsiExpression body = expression.getExpression();
-		if(body instanceof PsiParenthesizedExpression){
-			return true;
-		}
-		final int parentPrecendence =
+    public boolean satisfiedBy(PsiElement element){
+        if(!(element instanceof PsiParenthesizedExpression)){
+            return false;
+        }
+        if(ErrorUtil.containsError(element)){
+            return false;
+        }
+        final PsiParenthesizedExpression expression =
+                (PsiParenthesizedExpression) element;
+        final PsiElement parent = expression.getParent();
+        if(!(parent instanceof PsiExpression)){
+            return true;
+        }
+        final PsiExpression body = expression.getExpression();
+        if(body instanceof PsiParenthesizedExpression){
+            return true;
+        }
+        final int parentPrecedence =
                 ParenthesesUtils.getPrecedence((PsiExpression) parent);
-		final int childPrecendence = ParenthesesUtils.getPrecedence(body);
-		if(parentPrecendence > childPrecendence){
-			return true;
-		} else if(parentPrecendence == childPrecendence){
-			if(parent instanceof PsiBinaryExpression &&
-					body instanceof PsiBinaryExpression){
+        final int childPrecedence = ParenthesesUtils.getPrecedence(body);
+        if(parentPrecedence > childPrecedence){
+            return true;
+        } else if(parentPrecedence == childPrecedence){
+            if(parent instanceof PsiBinaryExpression &&
+                    body instanceof PsiBinaryExpression){
                 final PsiBinaryExpression binaryExpression =
                         (PsiBinaryExpression) parent;
                 final PsiJavaToken parentSign =
                         binaryExpression.getOperationSign();
-				final IElementType parentOperator = parentSign.getTokenType();
-				final PsiJavaToken childSign =
-						((PsiBinaryExpression) body).getOperationSign();
-				final IElementType childOperator = childSign.getTokenType();
+                final IElementType parentOperator = parentSign.getTokenType();
+                final PsiJavaToken childSign =
+                        ((PsiBinaryExpression) body).getOperationSign();
+                final IElementType childOperator = childSign.getTokenType();
                 if(!parentOperator.equals(childOperator)){
                     return false;
                 }
@@ -63,10 +63,10 @@ class UnnecessaryParenthesesPredicate implements PsiElementPredicate{
                 final PsiType bodyType = body.getType();
                 return parentType != null && parentType.equals(bodyType);
             } else{
-				return false;
-			}
-		} else{
-			return false;
-		}
-	}
+                return false;
+            }
+        } else{
+            return false;
+        }
+    }
 }
