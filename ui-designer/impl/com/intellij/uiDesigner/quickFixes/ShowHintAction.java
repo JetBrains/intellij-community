28468750761bc0c441ce4c13ed6e2a3ddb82f47b
Dmitry Jemerov <yole@intellij.com>
2009-06-05 22:31:13 +0400

    NPE (IDEADFEV-31442)

--- ui-designer/impl/com/intellij/uiDesigner/quickFixes/ShowHintAction.java
+++ ui-designer/impl/com/intellij/uiDesigner/quickFixes/ShowHintAction.java
@@ -1,6 +1,7 @@
 package com.intellij.uiDesigner.quickFixes;
 
 import com.intellij.openapi.actionSystem.*;
+import com.intellij.uiDesigner.designSurface.GuiEditor;
 import com.intellij.uiDesigner.propertyInspector.PropertyInspector;
 import com.intellij.uiDesigner.propertyInspector.UIDesignerToolWindowManager;
 import org.jetbrains.annotations.NotNull;
@@ -11,7 +12,7 @@ import javax.swing.*;
  * @author Anton Katilin
  * @author Vladimir Kondratyev
  */
-final class ShowHintAction extends AnAction{
+final class ShowHintAction extends AnAction {
   private final QuickFixManager myManager;
 
   public ShowHintAction(@NotNull final QuickFixManager manager, @NotNull final JComponent component) {
@@ -23,11 +24,14 @@ final class ShowHintAction extends AnAction{
   }
 
   public void actionPerformed(final AnActionEvent e) {
+    final GuiEditor editor = myManager.getEditor();
+    if (editor == null) return;
+
     // 1. Show light bulb
     myManager.showIntentionHint();
 
     // 2. Commit possible non committed value and show popup
-    final UIDesignerToolWindowManager manager = UIDesignerToolWindowManager.getInstance(myManager.getEditor().getProject());
+    final UIDesignerToolWindowManager manager = UIDesignerToolWindowManager.getInstance(editor.getProject());
     final PropertyInspector propertyInspector = manager.getPropertyInspector();
     if(propertyInspector != null && propertyInspector.isEditing()) {
       propertyInspector.stopEditing();
