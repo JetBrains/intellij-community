be8f9917f4fde49276f0c67f925b5f80085ea61d
Dmitry Jemerov <yole@intellij.com>
2009-06-05 19:59:42 +0400

    allow Alt-Shift-Left/Right shortcut to be used by plugins

--- platform-impl/src/com/intellij/openapi/fileEditor/impl/SelectPreviousEditorTabAction.java
+++ platform-impl/src/com/intellij/openapi/fileEditor/impl/SelectPreviousEditorTabAction.java
@@ -7,7 +7,15 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.DumbAware;
 
+// The only purpose of this action is to serve as placeholder for assigning keyboard shortcuts.
+// For actual tab switching code, see EditorComposite constructor.
 public class SelectPreviousEditorTabAction extends AnAction implements DumbAware {
   public void actionPerformed(final AnActionEvent e) {
+  }
+
+  @Override
+  public void update(AnActionEvent e) {
+    // allow plugins to use the same keyboard shortcut
+    e.getPresentation().setEnabled(false);
   }
 }
