4dc4f5bdd1bd4e8cfff4bd0681369f0669ea0dce
Maxim Shafirov <max@jetbrains.com>
2009-06-09 19:23:31 +0400

    sorting optimization

--- platform-impl/src/com/intellij/openapi/editor/impl/HighlighterList.java
+++ platform-impl/src/com/intellij/openapi/editor/impl/HighlighterList.java
@@ -4,18 +4,20 @@ import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.event.DocumentAdapter;
 import com.intellij.openapi.editor.event.DocumentEvent;
 import com.intellij.openapi.editor.markup.RangeHighlighter;
-import com.intellij.util.containers.ContainerUtil;
-import gnu.trove.THashSet;
+import com.intellij.util.containers.SortedList;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
 
 public abstract class HighlighterList {
-  private final List<RangeHighlighterImpl> mySegmentHighlighters = new ArrayList<RangeHighlighterImpl>();
-  private final Set<RangeHighlighter> myHighlightersSet = new THashSet<RangeHighlighter>();
+  private final List<RangeHighlighterImpl> mySegmentHighlighters = new SortedList<RangeHighlighterImpl>(MY_RANGE_COMPARATOR);
+
   private boolean myIsDirtied = false;
   private final DocumentAdapter myDocumentListener;
   private final Document myDoc;
   private int myLongestHighlighterLength = 0;
+
   private static final Comparator<RangeHighlighterImpl> MY_RANGE_COMPARATOR = new Comparator<RangeHighlighterImpl>() {
     public int compare(RangeHighlighterImpl r1, RangeHighlighterImpl r2) {
       if (r1.getAffectedAreaStartOffset() != r2.getAffectedAreaStartOffset()) {
@@ -51,19 +53,17 @@ public abstract class HighlighterList {
   private void sortMarkers() {
     assertDispatchThread();
     myLongestHighlighterLength = 0;
-    mySegmentHighlighters.clear();
-    Iterator<RangeHighlighter> iterator = myHighlightersSet.iterator();
+
+    Iterator<RangeHighlighterImpl> iterator = mySegmentHighlighters.iterator();
     while (iterator.hasNext()) {
       RangeHighlighter rangeHighlighter = iterator.next();
       if (rangeHighlighter.isValid()) {
-        mySegmentHighlighters.add((RangeHighlighterImpl)rangeHighlighter);
         myLongestHighlighterLength = Math.max(myLongestHighlighterLength, rangeHighlighter.getEndOffset() - rangeHighlighter.getStartOffset());
       }
       else {
         iterator.remove();
       }
     }
-    ContainerUtil.quickSort(mySegmentHighlighters, MY_RANGE_COMPARATOR);
 
     myIsDirtied = false;
   }
@@ -83,12 +83,14 @@ public abstract class HighlighterList {
   void addSegmentHighlighter(RangeHighlighter segmentHighlighter) {
     assertDispatchThread();
     myIsDirtied = true;
-    myHighlightersSet.add(segmentHighlighter);
+    mySegmentHighlighters.add((RangeHighlighterImpl)segmentHighlighter);
   }
 
   void removeSegmentHighlighter(RangeHighlighter segmentHighlighter) {
     assertDispatchThread();
     myIsDirtied = true;
-    myHighlightersSet.remove(segmentHighlighter);
+
+    //noinspection SuspiciousMethodCalls
+    mySegmentHighlighters.remove(segmentHighlighter);
   }
 }
