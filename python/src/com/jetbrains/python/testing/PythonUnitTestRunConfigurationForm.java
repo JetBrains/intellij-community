3977915dce5dc981c5c84a8e5dc0e902807da561
Dmitry Cheryasov <Dmitry.Cheryasov@jetbrains.com>
2009-06-09 07:46:35 +0400

    Show SDK selection panel in unit test run configuration.

--- python/src/com/jetbrains/python/testing/PythonUnitTestRunConfigurationForm.java
+++ python/src/com/jetbrains/python/testing/PythonUnitTestRunConfigurationForm.java
@@ -14,6 +14,7 @@ import static com.jetbrains.python.testing.PythonUnitTestRunConfiguration.TestTy
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.*;
 
 /**
  * @author Leonid Shalupov
@@ -44,6 +45,8 @@ public class PythonUnitTestRunConfigurationForm implements PythonUnitTestRunConf
 
     myConfiguration = configuration;
     myCommonOptionsForm = new PyCommonOptionsForm(configuration);
+    myCommonOptionsPlaceholder.add(myCommonOptionsForm.getMainPanel(), BorderLayout.CENTER);
+    
     initComponents();
   }
 
