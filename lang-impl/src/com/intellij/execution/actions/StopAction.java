d20086e791b6d7a9d761327f2bdef3a79708d2eb
greg <Gregory.Shrago@jetbrains.com>
2009-06-09 16:24:25 +0400

    fix wrong process being closed problem 2

--- lang-impl/src/com/intellij/execution/actions/StopAction.java
+++ lang-impl/src/com/intellij/execution/actions/StopAction.java
@@ -1,10 +1,14 @@
 package com.intellij.execution.actions;
 
+import com.intellij.execution.ExecutionManager;
 import com.intellij.execution.process.ProcessHandler;
 import com.intellij.execution.ui.RunContentDescriptor;
 import com.intellij.execution.ui.RunContentManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.Nullable;
 
 /*
  * Copyright (c) 2000-2004 by JetBrains s.r.o. All Rights Reserved.
@@ -13,8 +17,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 
 public class StopAction extends AnAction{
   public void actionPerformed(final AnActionEvent e) {
-    final RunContentDescriptor contentDescriptor = RunContentManager.RUN_CONTENT_DESCRIPTOR.getData(e.getDataContext());
-    final ProcessHandler processHandler = contentDescriptor == null ? null : contentDescriptor.getProcessHandler();
+    final ProcessHandler processHandler = getProcessHandler(e);
     if(processHandler == null) return;
     if(processHandler.detachIsDefault()) {
       processHandler.detachProcess();
@@ -24,9 +27,25 @@ public class StopAction extends AnAction{
   }
 
   public void update(final AnActionEvent e) {
-    final RunContentDescriptor contentDescriptor = RunContentManager.RUN_CONTENT_DESCRIPTOR.getData(e.getDataContext());
-    final ProcessHandler processHandler = contentDescriptor == null? null : contentDescriptor.getProcessHandler();
+    final ProcessHandler processHandler = getProcessHandler(e);
     boolean enable = processHandler != null && !processHandler.isProcessTerminating() && !processHandler.isProcessTerminated();
     e.getPresentation().setEnabled(enable);
+  }
+
+  @Nullable
+  private static ProcessHandler getProcessHandler(final AnActionEvent e) {
+    final RunContentDescriptor contentDescriptor = RunContentManager.RUN_CONTENT_DESCRIPTOR.getData(e.getDataContext());
+    final ProcessHandler processHandler;
+    if (contentDescriptor != null) {
+      // toolwindow case
+      processHandler = contentDescriptor.getProcessHandler();
+    }
+    else {
+      // main menu toolbar
+      final Project project = PlatformDataKeys.PROJECT.getData(e.getDataContext());
+      final RunContentDescriptor selectedContent = project == null? null : ExecutionManager.getInstance(project).getContentManager().getSelectedContent();
+      processHandler = selectedContent == null? null : selectedContent.getProcessHandler();
+    }
+    return processHandler;
   }
 }
