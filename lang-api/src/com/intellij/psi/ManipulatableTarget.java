76c39c90042c2b17c156c3aa8f9d113959cfd094
Peter Gromov <peter@jetbrains.com>
2009-06-16 16:02:22 +0400

    manipulatable pom target

---
+++ lang-api/src/com/intellij/psi/ManipulatableTarget.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2000-2009 JetBrains s.r.o.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.intellij.psi;
+
+import com.intellij.pom.PsiDeclaredTarget;
+import com.intellij.pom.PomRenameableTarget;
+import com.intellij.openapi.util.TextRange;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * @author peter
+ */
+public class ManipulatableTarget extends DelegatePsiTarget implements PsiDeclaredTarget, PomRenameableTarget<Object> {
+  public ManipulatableTarget(@NotNull PsiElement element) {
+    super(element);
+  }
+
+  public TextRange getNameIdentifierRange() {
+    return ElementManipulators.getValueTextRange(getNavigationElement());
+  }
+
+  public boolean isWritable() {
+    return getNavigationElement().isWritable();
+  }
+
+  @Nullable
+  public Object setName(@NotNull String newName) {
+    ElementManipulators.getManipulator(getNavigationElement()).handleContentChange(getNavigationElement(), newName);
+    return null;
+  }
+
+  public String getName() {
+    return ElementManipulators.getValueText(getNavigationElement());
+  }
+}
