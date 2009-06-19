76ecf73e58dbcd7c791cf7a499720257fd394f74
Dmitry Jemerov <yole@intellij.com>
2009-06-19 15:50:25 +0400

    simplified TextEditorBasedStructureViewModel API

--- python/src/com/jetbrains/python/structureView/PyStructureViewFactory.java
+++ python/src/com/jetbrains/python/structureView/PyStructureViewFactory.java
@@ -2,11 +2,15 @@ package com.jetbrains.python.structureView;
 
 import com.intellij.ide.structureView.StructureViewBuilder;
 import com.intellij.ide.structureView.StructureViewModel;
+import com.intellij.ide.structureView.StructureViewModelBase;
 import com.intellij.ide.structureView.TreeBasedStructureViewBuilder;
+import com.intellij.ide.util.treeView.smartTree.Sorter;
 import com.intellij.lang.PsiStructureViewFactory;
 import com.intellij.psi.PsiFile;
-import org.jetbrains.annotations.NotNull;
+import com.jetbrains.python.psi.PyClass;
 import com.jetbrains.python.psi.PyElement;
+import com.jetbrains.python.psi.PyFunction;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author yole
@@ -17,7 +21,9 @@ public class PyStructureViewFactory implements PsiStructureViewFactory {
 
       @NotNull
       public StructureViewModel createStructureViewModel() {
-        return new PyStructureViewModel((PyElement) psiFile);
+        return new StructureViewModelBase(psiFile, new PyStructureViewElement((PyElement) psiFile))
+          .withSorters(Sorter.ALPHA_SORTER)
+          .withSuitableClasses(PyFunction.class, PyClass.class);
       }
     };
   }
