57d1f9309963afed82756691fddee17fbe60fc8e
Anton Makeev <Anton.Makeev@jetbrains.com>
2009-06-24 21:03:29 +0400

    Maven: stack overflow fix (IDEADEV-38125)

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/project/MavenProjectsManager.java
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/project/MavenProjectsManager.java
@@ -247,7 +247,7 @@ public class MavenProjectsManager extends SimpleProjectComponent implements Pers
     myImportingQueue.makeDumbAware(myProject);
 
     mySchedulesQueue = new MavenMergingUpdateQueue(getComponentName() + ": Schedules queue", 1000, true, myProject);
-    mySchedulesQueue.setPassThrough(true);
+    mySchedulesQueue.setPassThrough(false);
   }
 
   private void listenForSettingsChanges() {
@@ -632,7 +632,7 @@ public class MavenProjectsManager extends SimpleProjectComponent implements Pers
     wrapper.set(new Runnable() {
       public void run() {
         if (!StartupManagerEx.getInstanceEx(myProject).postStartupActivityPassed()) {
-          mySchedulesQueue.queue(new Update(this) {
+          mySchedulesQueue.queue(new Update(MavenProjectsManager.this) {
             public void run() {
               wrapper.get().run();
             }
