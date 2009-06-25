89c6ad819cb5e6e2cb1c56093aa559daecba88e4
Peter Gromov <peter@jetbrains.com>
2009-06-25 15:55:10 +0400

    better groovy class exclusion stepping filters (stolen from Eclipse plugin guidelines)

--- plugins/groovy/src/org/jetbrains/plugins/groovy/debugger/filters/GroovyDebuggerClassFilterProvider.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/debugger/filters/GroovyDebuggerClassFilterProvider.java
@@ -2,9 +2,6 @@ package org.jetbrains.plugins.groovy.debugger.filters;
 
 import com.intellij.ui.classFilter.ClassFilter;
 import com.intellij.ui.classFilter.DebuggerClassFilterProvider;
-import com.intellij.util.Function;
-import com.intellij.util.containers.ContainerUtil;
-import org.jetbrains.annotations.NonNls;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,18 +11,7 @@ import java.util.List;
  * @author ilyas
  */
 public class GroovyDebuggerClassFilterProvider implements DebuggerClassFilterProvider {
-
-  @NonNls private static final String[] PROHIBITED_CLASS_PATTERNS =
-    {"org.codehaus.groovy.*",
-     "groovy.lang.Meta*", "groovy.lang.GroovyObjectSupport", "groovy.lang.GroovySystem", "groovy.lang.Binding", "groovy.lang.GroovyShell", "groovy.lang.Script*",
-     "groovy.ui.GroovyMain",
-     "groovy.lang.MissingPropertyException", "groovy.lang.GroovyRuntimeException"};
-
-  private static final ClassFilter[] FITERS = ContainerUtil.map(PROHIBITED_CLASS_PATTERNS, new Function<String, ClassFilter>() {
-    public ClassFilter fun(final String s) {
-      return new ClassFilter(s);
-    }
-  }, new ClassFilter[0]);
+  private static final ClassFilter[] FITERS = {new ClassFilter("org.codehaus.groovy.*"), new ClassFilter("groovy.*")};
 
   public List<ClassFilter> getFilters() {
 
