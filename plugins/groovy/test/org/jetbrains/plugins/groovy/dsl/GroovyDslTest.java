75136a8f8e5acd94fda7a2c27099d11a46f61a5b
Peter Gromov <peter@jetbrains.com>
2009-06-29 17:18:30 +0400

    groovy dsl test

---
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/dsl/GroovyDslTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2000-2009 JetBrains s.r.o.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.jetbrains.plugins.groovy.dsl;
+
+import com.intellij.testFramework.fixtures.JavaCodeInsightFixtureTestCase;
+
+/**
+ * @author peter
+ */
+public class GroovyDslTest extends JavaCodeInsightFixtureTestCase {
+
+  @Override
+  protected String getBasePath() {
+    return "/svnPlugins/groovy/testdata/groovy/dsl";
+  }
+
+  public void testCompleteMethod() throws Throwable { doTest(); }
+  public void testCompleteProperty() throws Throwable { doTest(); }
+
+  private void doTest() throws Throwable {
+    myFixture.testCompletion(getTestName(false) + ".gdsl", getTestName(false) + "_after.gdsl");
+  }
+}
