471094dbf2796edacf59b6468f35007c56cce439
Irina Chernushina <Irina.Chernushina@jetbrains.com>
2009-07-02 15:03:43 +0400

    IDEADEV-37856 (Unable to add content of .idea directory to SVN). .idea made visible in Changes | Local. but not in Project view

--- vcs-impl/src/com/intellij/openapi/vcs/changes/VcsGuess.java
+++ vcs-impl/src/com/intellij/openapi/vcs/changes/VcsGuess.java
@@ -1,10 +1,13 @@
 package com.intellij.openapi.vcs.changes;
 
+import com.intellij.openapi.components.StorageScheme;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ex.ProjectEx;
 import com.intellij.openapi.vcs.AbstractVcs;
 import com.intellij.openapi.vcs.FilePath;
 import com.intellij.openapi.vcs.impl.ExcludedFileIndex;
 import com.intellij.openapi.vcs.impl.ProjectLevelVcsManagerImpl;
+import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.Nullable;
 
@@ -44,13 +47,23 @@ class VcsGuess {
       return null;
     }
     if (myExcludedFileIndex.isInContent(validParent) || isFileInBaseDir(filePath) ||
-        myVcsManager.hasExplicitMapping(filePath)) {
+        myVcsManager.hasExplicitMapping(filePath) || isInDirectoryBasedRoot(validParent)) {
       if (myExcludedFileIndex.isExcludedFile(validParent)) {
         return null;
       }
       return myVcsManager.getVcsFor(validParent);
     }
     return null;
+  }
+
+  private boolean isInDirectoryBasedRoot(final VirtualFile file) {
+    if (file == null) return false;
+    final StorageScheme storageScheme = ((ProjectEx) myProject).getStateStore().getStorageScheme();
+    if (StorageScheme.DIRECTORY_BASED.equals(storageScheme)) {
+      final VirtualFile ideaDir = myProject.getBaseDir().findChild(Project.DIRECTORY_STORE_FOLDER);
+      return (ideaDir != null && ideaDir.isValid() && ideaDir.isDirectory() && VfsUtil.isAncestor(ideaDir, file, false));
+    }
+    return false;
   }
 
   private boolean isFileInBaseDir(final VirtualFile file) {
