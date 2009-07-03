aa4c7e5db1d37cc54904df902a8e7c44baad74ab
Dmitry Jemerov <yole@intellij.com>
2009-07-03 14:18:10 +0400

    recognize skipped tests; cleanup

--- python/helpers/pytest_teamcity.py
+++ python/helpers/pytest_teamcity.py
@@ -3,20 +3,24 @@ from pycharm.tcmessages import TeamcityServiceMessages
 
 messages = TeamcityServiceMessages(prepend_linebreak=True)
 
+def fspath_to_url(fspath):
+    return "file:///" + str(fspath).replace("\\", "/")
+
 def pytest_collectstart(collector):
-    url = "file:///" + str(collector.fspath).replace("\\", "/")
-    messages.testSuiteStarted(collector.name, location=url)
+    messages.testSuiteStarted(collector.name, location=fspath_to_url(collector.fspath))
 
 def pytest_runtest_makereport(item, call):
     if call.when == "setup":
         fspath, lineno, msg = item.reportinfo()
-        url = "file:///" + str(fspath).replace("\\", "/")
+        url = fspath_to_url(fspath)
         if lineno: url += ":" + str(lineno)
         messages.testStarted(item.name, location=url)
 
 def pytest_runtest_logreport(rep):
     if rep.failed:
         messages.testFailed(rep.item.name, details=rep.longrepr)
+    elif rep.skipped:
+        messages.testIgnored(rep.item.name)
     messages.testFinished(rep.item.name)
 
 def pytest_collectreport(rep):
