09b844292a77f8423eeaf40125dc49303fd9abc6
Dmitry Jemerov <yole@intellij.com>
2009-07-24 17:34:11 +0400

    disable action when no open project

--- source/com/intellij/internal/psiView/ViewerAction.java
+++ source/com/intellij/internal/psiView/ViewerAction.java
@@ -8,13 +8,19 @@ package com.intellij.internal.psiView;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
 
 public class ViewerAction extends AnAction implements DumbAware {
   public void actionPerformed(AnActionEvent e) {
     Project project = PlatformDataKeys.PROJECT.getData(e.getDataContext());
     PsiViewerDialog dialog = new PsiViewerDialog(project,false);
     dialog.show();
+  }
+
+  @Override
+  public void update(AnActionEvent e) {
+    Project project = PlatformDataKeys.PROJECT.getData(e.getDataContext());
+    e.getPresentation().setEnabled(project != null);
   }
 }
