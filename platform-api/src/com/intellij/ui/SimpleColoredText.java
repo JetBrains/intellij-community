80c46b281052225a1a7c351ecb9cf5d6bdcb64c1
Konstantin Bulenkov <kb@jetbrains.com>
2009-07-27 13:58:54 +0400

    - initial draft of language-independent UML

--- platform-api/src/com/intellij/ui/SimpleColoredText.java
+++ platform-api/src/com/intellij/ui/SimpleColoredText.java
@@ -1,17 +1,17 @@
 /*
- * Copyright 2000-2007 JetBrains s.r.o.
+ * Copyright 2000-2009 JetBrains s.r.o.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 package com.intellij.ui;
@@ -29,6 +29,11 @@ public class SimpleColoredText {
   public SimpleColoredText() {
     myTexts = new ArrayList<String>(3);
     myAttributes = new ArrayList<SimpleTextAttributes>(3);
+  }
+
+  public SimpleColoredText(@NotNull String fragment, @NotNull SimpleTextAttributes attributes) {
+    this();
+    append(fragment, attributes);
   }
 
   public void append(@NotNull String fragment, @NotNull SimpleTextAttributes attributes){
