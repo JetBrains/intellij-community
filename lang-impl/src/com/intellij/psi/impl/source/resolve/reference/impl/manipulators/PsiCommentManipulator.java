3ed8a5f5537f4bbc5cf38b1f9247f8e1bf578589
greg <Gregory.Shrago@jetbrains.com>
2009-08-05 02:10:40 +0400

    some comment manipulator hacks

--- lang-impl/src/com/intellij/psi/impl/source/resolve/reference/impl/manipulators/PsiCommentManipulator.java
+++ lang-impl/src/com/intellij/psi/impl/source/resolve/reference/impl/manipulators/PsiCommentManipulator.java
@@ -10,18 +10,27 @@ import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.IncorrectOperationException;
 
 /**
- * @author maxim.mossienko
+ * @author peter
  */
 public class PsiCommentManipulator extends AbstractElementManipulator<PsiComment> {
-  public PsiComment handleContentChange(PsiComment xmlToken, TextRange range, String newContent) throws IncorrectOperationException {
-    String oldText = xmlToken.getText();
+  public PsiComment handleContentChange(PsiComment psiComment, TextRange range, String newContent) throws IncorrectOperationException {
+    String oldText = psiComment.getText();
     String newText = oldText.substring(0, range.getStartOffset()) + newContent + oldText.substring(range.getEndOffset());
-    final FileType type = xmlToken.getContainingFile().getFileType();
+    final FileType type = psiComment.getContainingFile().getFileType();
     final PsiFile fromText =
-      PsiFileFactory.getInstance(xmlToken.getProject()).createFileFromText("__." + type.getDefaultExtension(), newText);
+      PsiFileFactory.getInstance(psiComment.getProject()).createFileFromText("__." + type.getDefaultExtension(), newText);
 
-    final PsiComment newElement = PsiTreeUtil.getParentOfType(fromText.findElementAt(0), xmlToken.getClass(), false);
+    final PsiComment newElement = PsiTreeUtil.getParentOfType(fromText.findElementAt(0), psiComment.getClass(), false);
     assert newElement != null;
-    return (PsiComment)xmlToken.replace(newElement);
+    return (PsiComment)psiComment.replace(newElement);
+  }
+
+  @Override
+  public TextRange getRangeInElement(final PsiComment element) {
+    final String text = element.getText();
+    if (text.startsWith("//")) return new TextRange(2, element.getTextLength());
+    if (text.startsWith("/**") && text.endsWith("*/")) return new TextRange(3, element.getTextLength()-2);
+    if (text.startsWith("/*") && text.endsWith("*/")) return new TextRange(2, element.getTextLength()-2);
+    return super.getRangeInElement(element);
   }
 }
