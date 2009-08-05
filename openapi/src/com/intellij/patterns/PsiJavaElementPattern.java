7aa9e0ef17d31bdf67acb2d3356c5902eb456649
Konstantin Bulenkov <kb@jetbrains.com>
2009-08-05 19:25:15 +0400

    annotationParam(fqn, "value") -> annotationParam(fqn)

--- openapi/src/com/intellij/patterns/PsiJavaElementPattern.java
+++ openapi/src/com/intellij/patterns/PsiJavaElementPattern.java
@@ -1,6 +1,17 @@
 /*
- * Copyright (c) 2000-2005 by JetBrains s.r.o. All Rights Reserved.
- * Use is subject to license terms.
+ * Copyright 2000-2009 JetBrains s.r.o.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package com.intellij.patterns;
 
@@ -13,6 +24,8 @@ import org.jetbrains.annotations.NotNull;
  * @author peter
  */
 public class PsiJavaElementPattern<T extends PsiElement,Self extends PsiJavaElementPattern<T,Self>> extends PsiElementPattern<T,Self> {
+  private static final @NonNls String VALUE = "value";
+
   public PsiJavaElementPattern(final Class<T> aClass) {
     super(aClass);
   }
@@ -23,6 +36,10 @@ public class PsiJavaElementPattern<T extends PsiElement,Self extends PsiJavaElem
 
   public Self annotationParam(@NonNls final String annotationQualifiedName, @NonNls final String parameterName) {
     return annotationParam(StandardPatterns.string().equalTo(annotationQualifiedName), parameterName);
+  }
+
+  public Self annotationParam(@NonNls final String annotationQualifiedName) {
+    return annotationParam(annotationQualifiedName, VALUE);
   }
 
   public Self annotationParam(final ElementPattern<String> annotationQualifiedName, @NonNls final String parameterName) {
