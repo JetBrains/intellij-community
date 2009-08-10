2823fd420a060c1e272adeffe01596d4ae3a42f3
greg <Gregory.Shrago@jetbrains.com>
2009-08-11 00:17:22 +0400

    another test fix

--- xml/impl/src/com/intellij/xml/util/documentation/XmlDocumentationProvider.java
+++ xml/impl/src/com/intellij/xml/util/documentation/XmlDocumentationProvider.java
@@ -50,12 +50,12 @@ public class XmlDocumentationProvider extends ExtensibleDocumentationProvider im
   @Nullable
   public String getQuickNavigateInfo(PsiElement element) {
     final String name = ElementDescriptionUtil.getElementDescription(element, UsageViewShortNameLocation.INSTANCE);
-    if (StringUtil.isNotEmpty(name)) return null;
+    if (StringUtil.isEmpty(name)) return null;
     final String typeName = ElementDescriptionUtil.getElementDescription(element, UsageViewTypeLocation.INSTANCE);
     final PsiFile file = element.getContainingFile();
     final StringBuilder sb = new StringBuilder();
     if (StringUtil.isNotEmpty(typeName)) sb.append(typeName).append(" ");
-    sb.append(name);
+    sb.append("\"").append(name).append("\"");
     if (file != null) {
       sb.append(" [").append(file.getName()).append("]");
     }
