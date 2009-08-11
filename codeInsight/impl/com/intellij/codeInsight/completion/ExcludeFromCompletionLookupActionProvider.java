d648a04a7891aab2b84ea5d65af5a6a938db467b
Peter Gromov <peter@jetbrains.com>
2009-08-11 20:12:28 +0400

    IDEADEV-39474 Support "Easy exclusion of classes" in Groovy editor

--- codeInsight/impl/com/intellij/codeInsight/completion/ExcludeFromCompletionLookupActionProvider.java
+++ codeInsight/impl/com/intellij/codeInsight/completion/ExcludeFromCompletionLookupActionProvider.java
@@ -10,6 +10,9 @@ import com.intellij.codeInsight.lookup.LookupActionProvider;
 import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementAction;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiType;
+import com.intellij.psi.util.PsiUtil;
 import com.intellij.util.Consumer;
 
 /**
@@ -17,11 +20,17 @@ import com.intellij.util.Consumer;
  */
 public class ExcludeFromCompletionLookupActionProvider implements LookupActionProvider {
   public void fillActions(LookupElement element, Lookup lookup, Consumer<LookupElementAction> consumer) {
-    if (element instanceof JavaPsiClassReferenceElement) {
-      final JavaPsiClassReferenceElement referenceElement = (JavaPsiClassReferenceElement)element;
-      final String qname = referenceElement.getQualifiedName();
+    final Object o = element.getObject();
+    PsiClass clazz = null;
+    if (o instanceof PsiType) {
+      clazz = PsiUtil.resolveClassInType((PsiType)o);
+    } else if (o instanceof PsiClass) {
+      clazz = (PsiClass)o;
+    }
+    if (clazz != null && clazz.isValid()) {
+      final String qname = clazz.getQualifiedName();
       if (qname != null) {
-        final Project project = referenceElement.getObject().getProject();
+        final Project project = clazz.getProject();
         for (final String s : AddImportAction.getAllExcludableStrings(qname)) {
           consumer.consume(new ExcludeFromCompletionAction(project, s));
         }
