10a9c7824d1e9f76bbea6710e7e98d2b324e24d4
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2009-08-17 13:27:58 +0400

    quick fix to remove unnecessary module dependency

--- inspections/impl/com/intellij/codeInspection/unnecessaryModuleDependency/UnnecessaryModuleDependencyInspection.java
+++ inspections/impl/com/intellij/codeInspection/unnecessaryModuleDependency/UnnecessaryModuleDependencyInspection.java
@@ -11,7 +11,12 @@ import com.intellij.codeInspection.reference.RefGraphAnnotator;
 import com.intellij.codeInspection.reference.RefManager;
 import com.intellij.codeInspection.reference.RefModule;
 import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ModifiableRootModel;
+import com.intellij.openapi.roots.ModuleOrderEntry;
 import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.roots.OrderEntry;
+import com.intellij.openapi.util.Comparing;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
@@ -36,10 +41,34 @@ public class UnnecessaryModuleDependencyInspection extends GlobalInspectionTool 
       final Module[] declaredDependencies = ModuleRootManager.getInstance(module).getDependencies();
       List<CommonProblemDescriptor> descriptors = new ArrayList<CommonProblemDescriptor>();
       final Set<Module> modules = refModule.getUserData(UnnecessaryModuleDependencyAnnotator.DEPENDENCIES);
-      for (Module dependency : declaredDependencies) {
+      for (final Module dependency : declaredDependencies) {
         if (scope.contains(dependency.getModuleFile())) { //external references are rejected -> annotator doesn't provide any information on them -> false positives
           if (modules == null || !modules.contains(dependency)) {
-            descriptors.add(manager.createProblemDescriptor(InspectionsBundle.message("unnecessary.module.dependency.problem.descriptor", module.getName(), dependency.getName())));
+            descriptors.add(manager.createProblemDescriptor(InspectionsBundle.message("unnecessary.module.dependency.problem.descriptor", module.getName(), dependency.getName()), new QuickFix(){
+              @NotNull
+              public String getName() {
+                return "Remove dependency";
+              }
+
+              @NotNull
+              public String getFamilyName() {
+                return getName();
+              }
+
+              public void applyFix(@NotNull Project project, @NotNull CommonProblemDescriptor descriptor) {
+                final ModifiableRootModel model = ModuleRootManager.getInstance(module).getModifiableModel();
+                for (OrderEntry entry : model.getOrderEntries()) {
+                  if (entry instanceof ModuleOrderEntry) {
+                    final Module mDependency = ((ModuleOrderEntry)entry).getModule();
+                    if (Comparing.equal(mDependency, dependency)) {
+                      model.removeOrderEntry(entry);
+                      break;
+                    }
+                  }
+                }
+                model.commit();
+              }
+            }));
           }
         }
       }
