588716fac5ea6079d3a87470135653517aadf223
greg <Gregory.Shrago@jetbrains.com>
2009-08-18 05:29:13 +0400

    fix direction

--- platform-api/src/com/intellij/ui/treeStructure/WeightBasedComparator.java
+++ platform-api/src/com/intellij/ui/treeStructure/WeightBasedComparator.java
@@ -44,6 +44,6 @@ public class WeightBasedComparator implements Comparator<NodeDescriptor> {
       if (s2 == null) return +1;
       return s1.compareToIgnoreCase(s2);
     }
-    return second.getWeight() - first.getWeight();
+    return first.getWeight() - second.getWeight();
   }
 }
