753211f8827866ca79657cb58c01e5e2dd85b04c
Alexey Pegov <Alexey.Pegov@jetbrains.com>
2009-08-20 21:06:51 +0400

    IDEADEV-39818 "Resume program" action should show "Debug" popup if no process is currently running

--- debugger/impl/com/intellij/debugger/actions/ResumeActionHandler.java
+++ debugger/impl/com/intellij/debugger/actions/ResumeActionHandler.java
@@ -3,6 +3,7 @@ package com.intellij.debugger.actions;
 
 import com.intellij.debugger.DebuggerManagerEx;
 import com.intellij.debugger.impl.DebuggerSession;
+import com.intellij.execution.actions.ChooseDebugConfigurationAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
 import com.intellij.xdebugger.impl.actions.DebuggerActionHandler;
@@ -10,11 +11,16 @@ import org.jetbrains.annotations.NotNull;
 
 public class ResumeActionHandler extends DebuggerActionHandler {
   public void perform(@NotNull final Project project, final AnActionEvent event) {
-    (DebuggerManagerEx.getInstanceEx(project)).getContext().getDebuggerSession().resume();
+    final DebuggerSession session = (DebuggerManagerEx.getInstanceEx(project)).getContext().getDebuggerSession();
+    if (session != null && session.isPaused()) {
+      session.resume();
+    } else {
+      new ChooseDebugConfigurationAction().actionPerformed(event);
+    }
   }
 
   public boolean isEnabled(@NotNull final Project project, final AnActionEvent event) {
     DebuggerSession debuggerSession = (DebuggerManagerEx.getInstanceEx(project)).getContext().getDebuggerSession();
-    return debuggerSession != null && debuggerSession.isPaused();
+    return debuggerSession == null || debuggerSession.isPaused();
   }
 }
