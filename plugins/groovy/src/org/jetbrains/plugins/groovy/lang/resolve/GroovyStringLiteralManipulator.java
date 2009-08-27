93d5ed67db39813267ba95074aba84c5cdb176e5
greg <Gregory.Shrago@jetbrains.com>
2009-08-27 22:30:56 +0400

    support multiline literals

--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/resolve/GroovyStringLiteralManipulator.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/resolve/GroovyStringLiteralManipulator.java
@@ -4,9 +4,9 @@ import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.AbstractElementManipulator;
 import com.intellij.util.IncorrectOperationException;
+import org.jetbrains.plugins.groovy.lang.psi.GroovyPsiElementFactory;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrExpression;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.literals.GrLiteral;
-import org.jetbrains.plugins.groovy.lang.psi.GroovyPsiElementFactory;
 
 public class GroovyStringLiteralManipulator extends AbstractElementManipulator<GrLiteral> {
   public GrLiteral handleContentChange(GrLiteral expr, TextRange range, String newContent) throws IncorrectOperationException {
@@ -19,6 +19,10 @@ public class GroovyStringLiteralManipulator extends AbstractElementManipulator<G
   }
 
   public TextRange getRangeInElement(final GrLiteral element) {
+    final String text = element.getText();
+    if (text.length() > 6 && text.startsWith("\"\"\"") && text.endsWith("\"\"\"")) {
+      return new TextRange(3, element.getTextLength() - 3);
+    }
     return new TextRange(1, Math.max(1, element.getTextLength() - 1));
   }
 }
