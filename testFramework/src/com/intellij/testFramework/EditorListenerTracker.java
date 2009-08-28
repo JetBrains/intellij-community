ac7e6a42dcbf60d26136fa0d6a0caaaa1befb6af
Alexey Kudravtsev <cdr@jetbrains.com>
2009-08-28 14:26:56 +0400

    clear

--- testFramework/src/com/intellij/testFramework/EditorListenerTracker.java
+++ testFramework/src/com/intellij/testFramework/EditorListenerTracker.java
@@ -24,28 +24,33 @@ public class EditorListenerTracker {
   }
 
   public void checkListenersLeak() {
-    // listeners may hang on default project
-    if (myDefaultProjectInitialized != ((ProjectManagerImpl)ProjectManager.getInstance()).isDefaultProjectInitialized()) return;
+    try {
+// listeners may hang on default project
+      if (myDefaultProjectInitialized != ((ProjectManagerImpl)ProjectManager.getInstance()).isDefaultProjectInitialized()) return;
 
-    EditorEventMulticasterImpl multicaster = (EditorEventMulticasterImpl)EditorFactory.getInstance().getEventMulticaster();
-    Map<Class, List> after = multicaster.getListeners();
-    Map<Class, List> leaked = new LinkedHashMap<Class, List>();
-    for (Map.Entry<Class, List> entry : after.entrySet()) {
-      Class aClass = entry.getKey();
-      List beforeList = before.get(aClass);
-      List afterList = entry.getValue();
-      if (beforeList != null) {
-        afterList.removeAll(beforeList);
+      EditorEventMulticasterImpl multicaster = (EditorEventMulticasterImpl)EditorFactory.getInstance().getEventMulticaster();
+      Map<Class, List> after = multicaster.getListeners();
+      Map<Class, List> leaked = new LinkedHashMap<Class, List>();
+      for (Map.Entry<Class, List> entry : after.entrySet()) {
+        Class aClass = entry.getKey();
+        List beforeList = before.get(aClass);
+        List afterList = entry.getValue();
+        if (beforeList != null) {
+          afterList.removeAll(beforeList);
+        }
+        if (!afterList.isEmpty()) {
+          leaked.put(aClass, afterList);
+        }
       }
-      if (!afterList.isEmpty()) {
-        leaked.put(aClass, afterList);
+
+      for (Map.Entry<Class, List> entry : leaked.entrySet()) {
+        Class aClass = entry.getKey();
+        List list = entry.getValue();
+        Assert.fail("Listeners leaked for " + aClass+":\n"+list);
       }
     }
-
-    for (Map.Entry<Class, List> entry : leaked.entrySet()) {
-      Class aClass = entry.getKey();
-      List list = entry.getValue();
-      Assert.fail("Listeners leaked for " + aClass+":\n"+list);
+    finally {
+      before.clear();
     }
   }
 }
