68b823a07089963201c92dcb596e4d5b8b4aab62
Alexey Ivanov <alexm@jetbrains.com>
2009-08-28 19:26:25 +0400

    minor error

--- python/src/com/jetbrains/python/actions/AddMethodQuickFix.java
+++ python/src/com/jetbrains/python/actions/AddMethodQuickFix.java
@@ -49,7 +49,7 @@ public class AddMethodQuickFix implements LocalQuickFix {
     String item_name = myIdentifier;
     if (cls != null && item_name != null) {
       PyStatementList cls_stmt_list = cls.getStatementList();
-      if (!CodeInsightUtilBase.preparePsiElementForWrite(problem_elt)) {
+      if (!CodeInsightUtilBase.preparePsiElementForWrite(cls_stmt_list)) {
         return;
       }
       Language language = cls.getLanguage();
