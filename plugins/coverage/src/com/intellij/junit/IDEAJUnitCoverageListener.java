4bedd4cf869e54774c2da82177ecdcb8e3f13d3c
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2009-08-31 18:28:56 +0400

    junit 3 vs 4 compatibility

--- plugins/coverage/src/com/intellij/junit/IDEAJUnitCoverageListener.java
+++ plugins/coverage/src/com/intellij/junit/IDEAJUnitCoverageListener.java
@@ -4,55 +4,25 @@
  */
 package com.intellij.junit;
 
-import com.intellij.junit4.JUnit4ReflectionUtil;
 import com.intellij.rt.execution.junit.IDEAJUnitListener;
-import junit.framework.AssertionFailedError;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import org.junit.runner.Description;
 import org.jetbrains.annotations.Nullable;
 
 @SuppressWarnings({"UnnecessaryFullyQualifiedName"})
-public class IDEAJUnitCoverageListener extends IDEAJUnitListener {
+public class IDEAJUnitCoverageListener implements IDEAJUnitListener {
 
-  public void addError(final Test test, final Throwable t) {
-  }
-
-  public void addFailure(final Test test, final AssertionFailedError t) {
-  }
-
-  public void startTest(final Test test) {
+  public void testStarted(String className, String methodName) {
     final Object data = getData();
-    if (data != null && test instanceof TestCase) {
-      ((com.intellij.rt.coverage.data.ProjectData)data).testStarted(test.getClass().getName() + "." + ((TestCase)test).getName());
-
+    if (data != null ) {
+      ((com.intellij.rt.coverage.data.ProjectData)data).testStarted(className + "." + methodName);
     }
   }
 
-  public void endTest(final Test test) {
-    final Object data = getData();
-    if (data != null && test instanceof TestCase) {
-      ((com.intellij.rt.coverage.data.ProjectData)data).testEnded(test.getClass().getName() + "." + ((TestCase)test).getName());
-    }
-  }
-
-  public void testStarted(Description description) {
+  public void testFinished(String className, String methodName) {
     final Object data = getData();
     if (data != null) {
-      ((com.intellij.rt.coverage.data.ProjectData)data)
-        .testStarted(JUnit4ReflectionUtil.getClassName(description) + "." + JUnit4ReflectionUtil.getMethodName(description));
+      ((com.intellij.rt.coverage.data.ProjectData)data).testEnded(className + "." + methodName);
     }
   }
-
-  public void testFinished(Description description) {
-    final Object data = getData();
-    if (data != null) {
-      ((com.intellij.rt.coverage.data.ProjectData)data)
-        .testEnded(JUnit4ReflectionUtil.getClassName(description) + "." + JUnit4ReflectionUtil.getMethodName(description));
-    }
-  }
-
-
   @Nullable
   private static Object getData() {
     try {
