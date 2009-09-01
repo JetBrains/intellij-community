b41ff8d03f67245ca9d54e13b4d6af97b7522709
Dmitry Jemerov <yole@intellij.com>
2009-09-01 17:21:43 +0400

    don't expose JavaEE interfaces in jsp-spi

--- java/jsp-spi/src/com/intellij/jsp/impl/JspElementDescriptor.java
+++ java/jsp-spi/src/com/intellij/jsp/impl/JspElementDescriptor.java
@@ -1,25 +1,9 @@
 package com.intellij.jsp.impl;
 
-import com.intellij.openapi.module.Module;
-import com.intellij.psi.xml.XmlTag;
 import com.intellij.xml.XmlElementDescriptor;
-import org.jetbrains.annotations.Nullable;
-
-import javax.servlet.jsp.tagext.TagExtraInfo;
 
 /**
- * Created by IntelliJ IDEA.
- * User: Maxim.Mossienko
- * Date: Mar 11, 2005
- * Time: 8:57:52 PM
- * To change this template use File | Settings | File Templates.
+ * @author Maxim.Mossienko
  */
 public interface JspElementDescriptor extends XmlElementDescriptor {
-  @Nullable
-  TagExtraInfo getExtraInfo(@Nullable Module module);
-  boolean isRequiredAttributeImplicitlyPresent(XmlTag tag,String attributeName);
-  @Nullable
-  XmlTag findVariableWithName(String name);
-
-  void resetClassloaderState();
 }
