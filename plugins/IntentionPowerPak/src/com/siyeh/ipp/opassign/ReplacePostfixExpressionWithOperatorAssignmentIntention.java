54788435b16adda22a360dbd6673713e3f80e1d9
Bas Leijdekkers <basleijdekkers@gmail.com>
2009-09-01 16:17:17 +0400

    IDEADEV-39999 (Replace Postfix Increment with Operator Assignment intention)

---
+++ plugins/IntentionPowerPak/src/com/siyeh/ipp/opassign/ReplacePostfixExpressionWithOperatorAssignmentIntention.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2009 Bas Leijdekkers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.siyeh.ipp.opassign;
+
+import com.siyeh.ipp.base.MutablyNamedIntention;
+import com.siyeh.ipp.base.PsiElementPredicate;
+import com.siyeh.IntentionPowerPackBundle;
+import com.intellij.psi.*;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.util.IncorrectOperationException;
+import org.jetbrains.annotations.NotNull;
+
+public class ReplacePostfixExpressionWithOperatorAssignmentIntention
+        extends MutablyNamedIntention {
+
+    @Override
+    protected String getTextForElement(PsiElement element) {
+        final PsiPostfixExpression postfixExpression =
+                (PsiPostfixExpression)element;
+        final PsiJavaToken sign = postfixExpression.getOperationSign();
+        final IElementType tokenType = sign.getTokenType();
+        final String replacementText;
+        if (JavaTokenType.PLUSPLUS.equals(tokenType)) {
+            replacementText = "+=";
+        } else {
+            replacementText = "-=";
+        }
+        final String signText = sign.getText();
+        return IntentionPowerPackBundle.message(
+                "replace.some.operator.with.other.intention.name",
+                signText, replacementText);
+    }
+
+    @NotNull
+    @Override
+    protected PsiElementPredicate getElementPredicate() {
+        return new ReplacePostfixExpressionWithOperatorAssignmentPredicate();
+    }
+
+    @Override
+    protected void processIntention(@NotNull PsiElement element)
+            throws IncorrectOperationException {
+        final PsiPostfixExpression postfixExpression =
+                (PsiPostfixExpression)element;
+        final PsiExpression operand = postfixExpression.getOperand();
+        final String operandText = operand.getText();
+        final IElementType tokenType =
+                postfixExpression.getOperationTokenType();
+        if (JavaTokenType.PLUSPLUS.equals(tokenType)) {
+            replaceExpression(operandText + "+=1", postfixExpression);
+        } else if (JavaTokenType.MINUSMINUS.equals(tokenType)) {
+            replaceExpression(operandText + "-=1", postfixExpression);
+        }
+    }
+}
