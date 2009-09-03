0a37db8a13fe42f8f4ca9c64e8f040d5e75ee14c
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2009-09-03 10:08:16 +0400

    EditorNotificationPanel extracted

---
+++ platform/platform-api/src/com/intellij/ui/EditorNotificationPanel.java
@@ -0,0 +1,58 @@
+package com.intellij.ui;
+
+import com.intellij.ide.DataManager;
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.ActionPlaces;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NonNls;
+
+import javax.swing.*;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import java.awt.*;
+
+/**
+ * @author Dmitry Avdeev
+ */
+public abstract class EditorNotificationPanel extends JPanel {
+
+  protected final JLabel myLabel = new JLabel();
+  private final JPanel myLinksPanel;
+
+  public EditorNotificationPanel() {
+    super(new BorderLayout());
+    setBackground(LightColors.YELLOW);
+    setBorder(BorderFactory.createEmptyBorder(0, 4, 0, 4));
+    add(myLabel, BorderLayout.CENTER);
+
+    myLinksPanel = new JPanel(new FlowLayout());
+    myLinksPanel.setBackground(LightColors.YELLOW);
+    add(myLinksPanel, BorderLayout.EAST);
+  }
+
+  protected void createActionLabel(final String text, @NonNls final String actionId) {
+    HyperlinkLabel label = new HyperlinkLabel(text, Color.BLUE, LightColors.YELLOW, Color.BLUE);
+    label.addHyperlinkListener(new HyperlinkListener() {
+      public void hyperlinkUpdate(final HyperlinkEvent e) {
+        if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+          executeAction(actionId);
+        }
+      }
+    });
+    myLinksPanel.add(label);
+  }
+
+  protected void executeAction(final String actionId) {
+    final AnAction action = ActionManager.getInstance().getAction(actionId);
+    final AnActionEvent event = new AnActionEvent(null, DataManager.getInstance().getDataContext(this), ActionPlaces.UNKNOWN,
+                                                  action.getTemplatePresentation(), ActionManager.getInstance(),
+                                                  0);
+    action.beforeActionPerformedUpdate(event);
+    action.update(event);
+
+    if (event.getPresentation().isEnabled() && event.getPresentation().isVisible()) {
+      action.actionPerformed(event);
+    }
+  }
+}
--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/committed/OutdatedVersionNotifier.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/committed/OutdatedVersionNotifier.java
@@ -1,10 +1,5 @@
 package com.intellij.openapi.vcs.changes.committed;
 
-import com.intellij.ide.DataManager;
-import com.intellij.openapi.actionSystem.ActionManager;
-import com.intellij.openapi.actionSystem.ActionPlaces;
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.diagnostic.Logger;
@@ -14,24 +9,19 @@ import com.intellij.openapi.fileEditor.FileEditorManagerEvent;
 import com.intellij.openapi.fileEditor.FileEditorManagerListener;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.Pair;
-import com.intellij.openapi.vcs.VcsBundle;
 import com.intellij.openapi.vcs.changes.Change;
 import com.intellij.openapi.vcs.versionBrowser.CommittedChangeList;
+import com.intellij.openapi.vcs.VcsBundle;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.ui.HyperlinkLabel;
-import com.intellij.ui.LightColors;
 import com.intellij.util.Consumer;
 import com.intellij.util.messages.MessageBus;
+import com.intellij.ui.EditorNotificationPanel;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import javax.swing.*;
-import javax.swing.event.HyperlinkEvent;
-import javax.swing.event.HyperlinkListener;
-import java.awt.*;
-import java.text.DateFormat;
 import java.util.List;
+import java.text.DateFormat;
 
 /**
  * @author yole
@@ -165,48 +155,15 @@ public class OutdatedVersionNotifier implements ProjectComponent {
     }
   }
 
-  private static class OutdatedRevisionPanel extends JPanel {
+  private static class OutdatedRevisionPanel extends EditorNotificationPanel {
     private CommittedChangeList myChangeList;
-    private final JLabel myLabel = new JLabel();
 
     public OutdatedRevisionPanel(CommittedChangeList changeList, final Change c) {
-      super(new BorderLayout());
-      setBackground(LightColors.YELLOW);
-      setBorder(BorderFactory.createEmptyBorder(0, 4, 0, 4));
+      super();
+      createActionLabel(VcsBundle.message("outdated.version.show.diff.action"), "Compare.LastVersion");
+      createActionLabel(VcsBundle.message("outdated.version.update.project.action"), "Vcs.UpdateProject");
       myChangeList = changeList;
       updateLabelText(c);
-      add(myLabel, BorderLayout.CENTER);
-
-      JPanel linksPanel = new JPanel(new FlowLayout());
-      linksPanel.setBackground(LightColors.YELLOW);
-      linksPanel.add(createActionLabel(VcsBundle.message("outdated.version.show.diff.action"), "Compare.LastVersion"));
-      linksPanel.add(createActionLabel(VcsBundle.message("outdated.version.update.project.action"), "Vcs.UpdateProject"));
-      add(linksPanel, BorderLayout.EAST);
-    }
-
-    private HyperlinkLabel createActionLabel(final String text, @NonNls final String actionId) {
-      HyperlinkLabel label = new HyperlinkLabel(text, Color.BLUE, LightColors.YELLOW, Color.BLUE);
-      label.addHyperlinkListener(new HyperlinkListener() {
-        public void hyperlinkUpdate(final HyperlinkEvent e) {
-          if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
-            executeAction(actionId);
-          }
-        }
-      });
-      return label;
-    }
-
-    private void executeAction(final String actionId) {
-      final AnAction action = ActionManager.getInstance().getAction(actionId);
-      final AnActionEvent event = new AnActionEvent(null, DataManager.getInstance().getDataContext(this), ActionPlaces.UNKNOWN,
-                                                    action.getTemplatePresentation(), ActionManager.getInstance(),
-                                                    0);
-      action.beforeActionPerformedUpdate(event);
-      action.update(event);
-
-      if (event.getPresentation().isEnabled() && event.getPresentation().isVisible()) {
-        action.actionPerformed(event);
-      }
     }
 
     private void updateLabelText(final Change c) {
