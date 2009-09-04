b422f99b6a9076b2ca5e0a93448be6029f47c153
Irina Chernushina <Irina.Chernushina@jetbrains.com>
2009-09-04 12:57:45 +0400

    VCS: do not look for valid parent twice; assertion-for-read-action-required fix

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/VcsGuess.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/VcsGuess.java
@@ -53,7 +53,7 @@ public class VcsGuess {
       return null;
     }
     if (myExcludedFileIndex.isInContent(validParent) || isFileInBaseDir(filePath) ||
-        myVcsManager.hasExplicitMapping(filePath) || isInDirectoryBasedRoot(validParent)) {
+        myVcsManager.hasExplicitMapping(validParent) || isInDirectoryBasedRoot(validParent)) {
       if (myExcludedFileIndex.isExcludedFile(validParent)) {
         return null;
       }
