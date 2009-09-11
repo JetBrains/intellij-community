f45c027312ee0576f10075044c281f85f373bc87
Dmitry Jemerov <yole@intellij.com>
2009-09-11 16:55:29 +0400

    test++

--- java/java-tests/testSrc/com/intellij/refactoring/RenameCollisionsTest.java
+++ java/java-tests/testSrc/com/intellij/refactoring/RenameCollisionsTest.java
@@ -1,32 +1,22 @@
 package com.intellij.refactoring;
 
+import com.intellij.JavaTestUtil;
 import com.intellij.codeInsight.TargetElementUtilBase;
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.openapi.projectRoots.impl.JavaSdkImpl;
 import com.intellij.psi.PsiElement;
 import com.intellij.refactoring.rename.RenameProcessor;
 import com.intellij.testFramework.LightCodeInsightTestCase;
-import com.intellij.openapi.projectRoots.Sdk;
-import com.intellij.openapi.projectRoots.impl.JavaSdkImpl;
-import com.intellij.openapi.roots.LanguageLevelProjectExtension;
-import com.intellij.pom.java.LanguageLevel;
 
 /**
  * @author sashache
  */
 public class RenameCollisionsTest extends LightCodeInsightTestCase {
   private static final String BASE_PATH = "/refactoring/renameCollisions/";
-  private LanguageLevel myPreviousLanguageLevel;
 
   @Override
-  protected void setUp() throws Exception {
-    super.setUp();
-    myPreviousLanguageLevel = LanguageLevelProjectExtension.getInstance(getJavaFacade().getProject()).getLanguageLevel();
-    LanguageLevelProjectExtension.getInstance(getJavaFacade().getProject()).setLanguageLevel(LanguageLevel.JDK_1_5);
-  }
-
-  @Override
-  protected void tearDown() throws Exception {
-    LanguageLevelProjectExtension.getInstance(getJavaFacade().getProject()).setLanguageLevel(myPreviousLanguageLevel);
-    super.tearDown();
+  protected String getTestDataPath() {
+    return JavaTestUtil.getJavaTestDataPath();
   }
 
   public void testRenameClassInnerToLocal() throws Exception {
