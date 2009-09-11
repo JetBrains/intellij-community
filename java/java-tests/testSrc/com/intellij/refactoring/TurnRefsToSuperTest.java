155e42a6bb3f76fa2654be54cdefed015a6bf9c8
Dmitry Jemerov <yole@intellij.com>
2009-09-11 23:02:15 +0400

    correct fix for case of testdata

--- java/java-tests/testSrc/com/intellij/refactoring/TurnRefsToSuperTest.java
+++ java/java-tests/testSrc/com/intellij/refactoring/TurnRefsToSuperTest.java
@@ -15,114 +15,115 @@ public class TurnRefsToSuperTest extends MultiFileTestCase {
   }
 
   public void testSuperClass() throws Exception {
-    doTest("AClass", "ASuper", true);
+    doTest("AClass", "ASuper", true, false);
   }
 
   public void testMethodFromSuper() throws Exception {
-    doTest("AClass", "ASuper", true);
+    doTest("AClass", "ASuper", true, false);
   }
 
   public void testRemoveImport() throws Exception {
-    doTest("pack1.AClass", "pack1.AnInterface", true);
+    doTest("pack1.AClass", "pack1.AnInterface", true, false);
   }
 
   public void testToArray() throws Exception {
-    doTest("A", "I", true);
+    doTest("A", "I", true, false);
   }
 
 
   public void testArrayElementAssignment() throws Exception {
-    doTest("C", "I", true);
+    doTest("C", "I", true, false);
   }
 
   public void testReturnValue() throws Exception {
-    doTest("A", "I", true);
+    doTest("A", "I", true, false);
   }
 
   public void testReturnValue2() throws Exception {
-    doTest("A", "I", true);
+    doTest("A", "I", true, false);
   }
 
   public void testCast() throws Exception {
-    doTest("A", "I", true);
+    doTest("A", "I", true, false);
   }
 
 
   public void testUseAsArg() throws Exception {
-    doTest("AClass", "I", true);
+    doTest("AClass", "I", true, false);
   }
 
   public void testClassUsage() throws Exception {
-    doTest("A", "I", true);
+    doTest("A", "I", true, false);
   }
 
   public void testInstanceOf() throws Exception {
-    doTest("A", "I", false);
+    doTest("A", "I", false, false);
   }
 
   public void testFieldTest() throws Exception {
-    doTest("Component1", "IDoSomething", false);
+    doTest("Component1", "IDoSomething", false, false);
   }
 
   public void testScr34000() throws Exception {
-    doTest("SimpleModel", "Model", false);
+    doTest("SimpleModel", "Model", false, false);
   }
 
   public void testScr34020() throws Exception {
-    doTest("java.util.List", "java.util.Collection", false);
+    doTest("java.util.List", "java.util.Collection", false, false);
   }
 
    public void testCommonInheritor() throws Exception {
-    doTest("Client.V", "Client.L", false);
+    doTest("Client.V", "Client.L", false, false);
   }
 
   public void testCommonInheritorFail() throws Exception {
-    doTest("Client.V", "Client.L", false);
+    doTest("Client.V", "Client.L", false, false);
   }
 
   public void testCommonInheritorResults() throws Exception {
-    doTest("Client.V", "Client.L", false);
+    doTest("Client.V", "Client.L", false, false);
   }
 
   public void testCommonInheritorResultsFail() throws Exception {
-    doTest("Client.V", "Client.L", false);
+    doTest("Client.V", "Client.L", false, false);
   }
 
   public void testCommonInheritorResultsFail2() throws Exception {
-    doTest("Client.V", "Client.L", false);
+    doTest("Client.V", "Client.L", false, false);
   }
 
 
   public void testIDEA6505() throws Exception {
-    doTest("Impl", "IB", false);
+    doTest("Impl", "IB", false, true);
   }
 
   public void testIDEADEV5517() throws Exception {
-    doTest("Xyz", "Xint", false);
+    doTest("Xyz", "Xint", false, true);
   }
 
   public void testIDEADEV5517Noop() throws Exception {
-    doTest("Xyz", "Xint", false);
+    doTest("Xyz", "Xint", false, true);
   }
 
   public void testIDEADEV6136() throws Exception {
-    doTest("A", "B", false);
+    doTest("A", "B", false, true);
   }
 
   public void testIDEADEV25669() throws Exception {
-    doTest("p.A", "p.Base", false);
+    doTest("p.A", "p.Base", false, true);
   }
 
   public void testIDEADEV23807() throws Exception {
-    doTest("B", "A", false);
+    doTest("B", "A", false, true);
   }
 
-  private void doTest(@NonNls final String className, @NonNls final String superClassName, final boolean replaceInstanceOf) throws Exception {
+  private void doTest(@NonNls final String className, @NonNls final String superClassName, final boolean replaceInstanceOf,
+                      final boolean lowercaseFirstLetter) throws Exception {
     doTest(new PerformAction() {
       public void performAction(VirtualFile rootDir, VirtualFile rootAfter) throws Exception {
         TurnRefsToSuperTest.this.performAction(className, superClassName, replaceInstanceOf);
       }
-    }, false);
+    }, lowercaseFirstLetter);
   }
 
   public String getTestRoot() {
