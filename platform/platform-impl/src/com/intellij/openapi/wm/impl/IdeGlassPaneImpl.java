ba7b27f7133e9bcf72a1620c560d970c0e86e744
Kirill Kalishev <Kirill.Kalishev@jetbrains.com>
2009-09-11 15:21:50 +0400

    IdeGlassPane: non-resizing popups fix

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/IdeGlassPaneImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/IdeGlassPaneImpl.java
@@ -220,7 +220,7 @@ private MouseEvent convertEvent(final MouseEvent e, final Component target) {
     IdeEventQueue queue = IdeEventQueue.getInstance();
     if (!queue.containsDispatcher(this) && myPreprocessorActive) {
       queue.addDispatcher(this, null);
-    } else if (queue.containsDispatcher(this) && myPreprocessorActive) {
+    } else if (queue.containsDispatcher(this) && !myPreprocessorActive) {
       queue.removeDispatcher(this);
     }
 
