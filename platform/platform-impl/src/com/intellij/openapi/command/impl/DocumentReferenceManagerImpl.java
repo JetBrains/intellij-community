64a159409d67c77193240775665c8db1a2e8af11
Alexey Kudravtsev <cdr@jetbrains.com>
2009-09-16 10:49:01 +0400

    memleaks

--- platform/platform-impl/src/com/intellij/openapi/command/impl/DocumentReferenceManagerImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/command/impl/DocumentReferenceManagerImpl.java
@@ -6,14 +6,19 @@ import com.intellij.openapi.command.undo.DocumentReferenceManager;
 import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.project.ProjectManagerAdapter;
 import com.intellij.openapi.vfs.*;
 import com.intellij.util.containers.WeakValueHashMap;
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.ref.Reference;
+import java.lang.ref.WeakReference;
 import java.util.Map;
 
 public class DocumentReferenceManagerImpl extends DocumentReferenceManager implements ApplicationComponent {
-  private final Map<Document, DocumentReference> myDocToRef = new WeakValueHashMap<Document, DocumentReference>();
+  private final Map<Reference<Document>, DocumentReference> myDocToRef = new WeakValueHashMap<Reference<Document>, DocumentReference>();
   private final Map<VirtualFile, DocumentReference> myFileToRef = new WeakValueHashMap<VirtualFile, DocumentReference>();
   private final Map<FilePath, DocumentReference> myDeletedFilePathToRef = new WeakValueHashMap<FilePath, DocumentReference>();
 
@@ -43,6 +48,11 @@ public class DocumentReferenceManagerImpl extends DocumentReferenceManager imple
         }
       }
     });
+    ProjectManager.getInstance().addProjectManagerListener(new ProjectManagerAdapter() {
+      @Override
+      public void projectOpened(Project project) {
+      }
+    });
   }
 
   public void disposeComponent() {
@@ -56,11 +66,24 @@ public class DocumentReferenceManagerImpl extends DocumentReferenceManager imple
     return file != null ? create(file) : doCreate(document);
   }
 
-  private DocumentReference doCreate(Document document) {
-    DocumentReference result = myDocToRef.get(document);
+  private DocumentReference doCreate(@NotNull final Document document) {
+    final int hashCode = document.hashCode();
+    Reference<Document> reference = new WeakReference<Document>(document){
+      @Override
+      public int hashCode() {
+        return hashCode;
+      }
+
+      @Override
+      public boolean equals(Object obj) {
+        Document mydoc = get();
+        return mydoc != null && obj instanceof Reference && ((Reference)obj).get() == mydoc;
+      }
+    };
+    DocumentReference result = myDocToRef.get(reference);
     if (result == null) {
       result = new DocumentReferenceByDocument(document);
-      myDocToRef.put(document, result);
+      myDocToRef.put(reference, result);
     }
     return result;
   }
