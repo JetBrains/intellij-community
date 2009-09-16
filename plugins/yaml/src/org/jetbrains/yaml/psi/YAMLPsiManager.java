25bb3a6d83166506c5253ca4a94caac39b7ddf73
Oleg Shpynov <Oleg.Shpynov@jetbrains.com>
2009-09-16 19:56:36 +0400

    RUBY-4996: Structure view updates fixed - YAMLPsiManager implemented

---
+++ plugins/yaml/src/org/jetbrains/yaml/psi/YAMLPsiManager.java
@@ -0,0 +1,114 @@
+package org.jetbrains.yaml.psi;
+
+import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiFileSystemItem;
+import com.intellij.psi.impl.PsiManagerImpl;
+import com.intellij.psi.impl.PsiModificationTrackerImpl;
+import com.intellij.psi.impl.PsiTreeChangeEventImpl;
+import com.intellij.psi.impl.PsiTreeChangePreprocessor;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author oleg
+ */
+public class YAMLPsiManager implements ProjectComponent, PsiTreeChangePreprocessor {
+  private PsiModificationTrackerImpl myModificationTracker;
+  protected Project myProject;
+  private final PsiManagerImpl myPsiManager;
+
+  public YAMLPsiManager(@NotNull final Project project, @NotNull final PsiManagerImpl psiManager) {
+    myProject = project;
+    myPsiManager = psiManager;
+  }
+
+  public void projectOpened() {
+  }
+
+  public void projectClosed() {
+  }
+
+  @NotNull
+  public String getComponentName() {
+    return "YAMLPsiManager";
+
+  }
+
+  public void initComponent() {
+    myModificationTracker = (PsiModificationTrackerImpl) myPsiManager.getModificationTracker();
+    myPsiManager.addTreeChangePreprocessor(this);
+  }
+
+  public void disposeComponent() {
+  }
+
+  public void treeChanged(final PsiTreeChangeEventImpl event) {
+    boolean changedInsideCodeBlock = false;
+
+    switch (event.getCode()) {
+      case BEFORE_CHILDREN_CHANGE:
+        if (event.getParent() instanceof PsiFile) {
+          changedInsideCodeBlock = true;
+          break; // May be caused by fake PSI event from PomTransaction. A real event will anyway follow.
+        }
+
+      case CHILDREN_CHANGED :
+        changedInsideCodeBlock = isInsideCodeBlock(event.getParent());
+      break;
+
+      case BEFORE_CHILD_ADDITION:
+      case BEFORE_CHILD_REMOVAL:
+      case CHILD_ADDED :
+      case CHILD_REMOVED :
+        changedInsideCodeBlock = isInsideCodeBlock(event.getParent());
+      break;
+
+      case BEFORE_PROPERTY_CHANGE:
+      case PROPERTY_CHANGED :
+        changedInsideCodeBlock = false;
+      break;
+
+      case BEFORE_CHILD_REPLACEMENT:
+      case CHILD_REPLACED :
+        changedInsideCodeBlock = isInsideCodeBlock(event.getParent());
+      break;
+
+      case BEFORE_CHILD_MOVEMENT:
+      case CHILD_MOVED :
+        changedInsideCodeBlock = isInsideCodeBlock(event.getOldParent()) && isInsideCodeBlock(event.getNewParent());
+      break;
+    }
+
+    if (!changedInsideCodeBlock) {
+      myModificationTracker.incOutOfCodeBlockModificationCounter();
+    }
+  }
+
+  private static boolean isInsideCodeBlock(PsiElement element) {
+    if (element instanceof PsiFileSystemItem) {
+      return false;
+    }
+
+    if (element == null || element.getParent() == null) return true;
+
+    while(true) {
+      if (element instanceof YAMLFile) {
+        return false;
+      }
+      if (element instanceof PsiFile || element instanceof PsiDirectory || element == null) {
+        return true;
+      }
+      PsiElement pparent = element.getParent();
+      if (!(pparent instanceof YAMLFile ||
+            pparent instanceof YAMLKeyValue ||
+            pparent instanceof YAMLCompoundValue ||
+            pparent instanceof YAMLDocument)) {
+        return true;
+      }
+      element = pparent;
+    }
+  }
+}
