b0b0ceb13dcbc808a69f4e67f6db0d6384a2dbc7
Maxim Shafirov <max@jetbrains.com>
2009-09-17 21:57:36 +0400

    IDEADEV-36036

--- plugins/InspectionGadgets/src/com/siyeh/ig/style/UnnecessaryAnnotationParenthesesInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/style/UnnecessaryAnnotationParenthesesInspection.java
@@ -1,28 +1,28 @@
 /*
- * Copyright 2009 Dave Bas Leijdekkers
+ * Copyright 2000-2009 JetBrains s.r.o.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package com.siyeh.ig.style;
 
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.*;
+import com.intellij.util.IncorrectOperationException;
+import com.siyeh.InspectionGadgetsBundle;
 import com.siyeh.ig.BaseInspection;
 import com.siyeh.ig.BaseInspectionVisitor;
 import com.siyeh.ig.InspectionGadgetsFix;
-import com.siyeh.InspectionGadgetsBundle;
-import com.intellij.psi.*;
-import com.intellij.openapi.project.Project;
-import com.intellij.codeInspection.ProblemDescriptor;
-import com.intellij.util.IncorrectOperationException;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 
@@ -82,6 +82,12 @@ public class UnnecessaryAnnotationParenthesesInspection extends BaseInspection {
         @Override
         public void visitAnnotation(PsiAnnotation annotation) {
             super.visitAnnotation(annotation);
+            final PsiJavaCodeReferenceElement reference =
+                    annotation.getNameReferenceElement();
+            if (reference == null) {
+                return;
+            }
+
             final PsiAnnotationParameterList parameterList =
                     annotation.getParameterList();
             final PsiElement[] children = parameterList.getChildren();
@@ -91,11 +97,6 @@ public class UnnecessaryAnnotationParenthesesInspection extends BaseInspection {
             final PsiNameValuePair[] nameValuePairs =
                     parameterList.getAttributes();
             if (nameValuePairs.length > 0) {
-                return;
-            }
-            final PsiJavaCodeReferenceElement reference =
-                    annotation.getNameReferenceElement();
-            if (reference == null) {
                 return;
             }
             final PsiElement target = reference.resolve();
