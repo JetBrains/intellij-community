f93362b838941815e4c9be6cff45a151b9ca8c92
Sergey Baranov <serge@jetbrains.com>
2009-09-22 22:04:48 +0400

    IDEADEV-39736: don't cache icons for exe and ico extensions on Windows as they may differ for each file

--- platform/lang-impl/src/com/intellij/openapi/fileTypes/impl/NativeFileIconProvider.java
+++ platform/lang-impl/src/com/intellij/openapi/fileTypes/impl/NativeFileIconProvider.java
@@ -4,6 +4,7 @@ import com.intellij.ide.FileIconProvider;
 import com.intellij.openapi.fileTypes.NativeFileType;
 import com.intellij.openapi.fileTypes.UnknownFileType;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.DeferredIconImpl;
 import com.intellij.util.Function;
@@ -11,8 +12,7 @@ import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author yole
@@ -20,6 +20,8 @@ import java.util.Map;
 public class NativeFileIconProvider implements FileIconProvider {
   private JFileChooser myFileChooser = new JFileChooser();
   private final Map<String, Icon> myIconCache = new HashMap<String, Icon>();
+  // on Windows .exe and .ico files provide their own icons which can differ for each file, don't cache them
+  private final Set<String> myNoCacheExtensions = SystemInfo.isWindows ? new HashSet<String>(Arrays.asList("exe", "ico")) : new HashSet<String>();
 
   public Icon getIcon(VirtualFile file, int flags, @Nullable Project project) {
     if (!(file.getFileType() instanceof NativeFileType) && !(file.getFileType() instanceof UnknownFileType)) {
@@ -28,7 +30,7 @@ public class NativeFileIconProvider implements FileIconProvider {
     final String ext = file.getExtension();
     Icon icon;
     synchronized (myIconCache) {
-      icon = ext != null ? myIconCache.get(ext) : null;
+      icon = ext != null && !myNoCacheExtensions.contains(ext) ? myIconCache.get(ext) : null;
     }
     if (icon != null) {
       return icon;
