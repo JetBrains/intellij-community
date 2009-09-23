da8cc3fc8a2829735d0bd256041d41e7ebce247b
Irina Chernushina <Irina.Chernushina@jetbrains.com>
2009-09-23 16:51:21 +0400

    fix compilation, sorry

--- plugins/git4idea/src/git4idea/changes/TestOutgoingAction.java
+++ plugins/git4idea/src/git4idea/changes/TestOutgoingAction.java
@@ -7,7 +7,10 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vcs.VcsOutgoingChangesProvider;
 import com.intellij.openapi.vcs.ProjectLevelVcsManager;
 import com.intellij.openapi.vcs.VcsException;
+import com.intellij.openapi.vcs.history.VcsRevisionNumber;
+import com.intellij.openapi.vcs.versionBrowser.CommittedChangeList;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.util.Pair;
 import git4idea.GitVcs;
 
 import java.util.List;
@@ -18,14 +21,14 @@ public class TestOutgoingAction extends AnAction {
     final Project project = PlatformDataKeys.PROJECT.getData(e.getDataContext());
     if (project == null) return;
     final GitVcs vcs = GitVcs.getInstance(project);
-    final VcsOutgoingChangesProvider provider = vcs.getOutgoingChangesProvider();
+    final VcsOutgoingChangesProvider<CommittedChangeList> provider = vcs.getOutgoingChangesProvider();
     final ProjectLevelVcsManager vcsManager = ProjectLevelVcsManager.getInstance(project);
     final VirtualFile[] roots = vcsManager.getRootsUnderVcs(vcs);
     if (roots == null) return;
     for (VirtualFile root : roots) {
       try {
-        final List list = provider.getOutgoingChanges(root, true);
-        System.out.println("list.size() = " + list.size());
+        final Pair<VcsRevisionNumber, List<CommittedChangeList>> pair = provider.getOutgoingChanges(root, true);
+        System.out.println("list.size() = " + pair.getSecond().size());
       }
       catch (VcsException e1) {
         e1.printStackTrace();
