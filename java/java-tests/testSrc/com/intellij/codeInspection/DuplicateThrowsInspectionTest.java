96ed6ab4dc8ca4be1a1022024db032f08a95b013
Dmitry Jemerov <yole@intellij.com>
2009-09-25 20:50:41 +0400

    moving tests to CE

--- java/java-tests/testSrc/com/intellij/codeInspection/DuplicateThrowsInspectionTest.java
+++ java/java-tests/testSrc/com/intellij/codeInspection/DuplicateThrowsInspectionTest.java
@@ -1,9 +1,15 @@
 package com.intellij.codeInspection;
 
+import com.intellij.JavaTestUtil;
 import com.intellij.codeInspection.duplicateThrows.DuplicateThrowsInspection;
 import com.intellij.testFramework.InspectionTestCase;
 
 public class DuplicateThrowsInspectionTest extends InspectionTestCase {
+  @Override
+  protected String getTestDataPath() {
+    return JavaTestUtil.getJavaTestDataPath() + "/inspection";
+  }
+
   private void doTest() throws Exception {
     doTest("duplicateThrows/" + getTestName(true), new DuplicateThrowsInspection());
   }
