be563a6e3d681f2569022086032c8a5a0e5cd0af
Dmitry Jemerov <yole@intellij.com>
2009-09-25 21:26:00 +0400

    moving tests to CE

--- java/java-tests/testSrc/com/intellij/codeInspection/SillyAssignmentTest.java
+++ java/java-tests/testSrc/com/intellij/codeInspection/SillyAssignmentTest.java
@@ -10,10 +10,16 @@
  */
 package com.intellij.codeInspection;
 
+import com.intellij.JavaTestUtil;
 import com.intellij.codeInspection.sillyAssignment.SillyAssignmentInspection;
 import com.intellij.testFramework.InspectionTestCase;
 
 public class SillyAssignmentTest extends InspectionTestCase {
+  @Override
+  protected String getTestDataPath() {
+    return JavaTestUtil.getJavaTestDataPath() + "/inspection";
+  }
+
   private void doTest() throws Exception {
     doTest("sillyAssignment/" + getTestName(true),  new SillyAssignmentInspection());
   }
