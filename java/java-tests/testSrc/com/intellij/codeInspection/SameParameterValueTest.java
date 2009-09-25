25140c50704514b36d8489bd8269952b3008a911
Dmitry Jemerov <yole@intellij.com>
2009-09-25 21:25:16 +0400

    moving tests to CE

--- java/java-tests/testSrc/com/intellij/codeInspection/SameParameterValueTest.java
+++ java/java-tests/testSrc/com/intellij/codeInspection/SameParameterValueTest.java
@@ -1,11 +1,16 @@
 package com.intellij.codeInspection;
 
+import com.intellij.JavaTestUtil;
 import com.intellij.codeInspection.sameParameterValue.SameParameterValueInspection;
 import com.intellij.testFramework.InspectionTestCase;
 
 public class SameParameterValueTest extends InspectionTestCase {
-
   private final SameParameterValueInspection myTool = new SameParameterValueInspection();
+
+  @Override
+  protected String getTestDataPath() {
+    return JavaTestUtil.getJavaTestDataPath() + "/inspection";
+  }
 
   private String getTestDir() {
     return "sameParameterValue/" + getTestName(false);
