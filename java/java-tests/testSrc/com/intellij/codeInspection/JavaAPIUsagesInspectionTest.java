6fef276b55e52ad1b7d4635a7d3921becac6c323
Dmitry Jemerov <yole@intellij.com>
2009-09-25 21:02:55 +0400

    moving tests to CE

--- java/java-tests/testSrc/com/intellij/codeInspection/JavaAPIUsagesInspectionTest.java
+++ java/java-tests/testSrc/com/intellij/codeInspection/JavaAPIUsagesInspectionTest.java
@@ -4,11 +4,16 @@
  */
 package com.intellij.codeInspection;
 
+import com.intellij.JavaTestUtil;
 import com.intellij.codeInspection.ex.LocalInspectionToolWrapper;
 import com.intellij.codeInspection.java15api.Java15APIUsageInspection;
 import com.intellij.testFramework.InspectionTestCase;
 
 public class JavaAPIUsagesInspectionTest extends InspectionTestCase {
+  @Override
+  protected String getTestDataPath() {
+    return JavaTestUtil.getJavaTestDataPath() + "/inspection";
+  }
 
   private void doTest() throws Exception {
     doTest("usage1.5/" + getTestName(false), new LocalInspectionToolWrapper(new Java15APIUsageInspection()), "java 1.5");
