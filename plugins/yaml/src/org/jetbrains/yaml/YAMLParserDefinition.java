27401e20d32f8d44bc4811968ee8410f5d640d10
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2009-09-29 21:41:01 +0400

    YAML Parser definition string literals fix

--- plugins/yaml/src/org/jetbrains/yaml/YAMLParserDefinition.java
+++ plugins/yaml/src/org/jetbrains/yaml/YAMLParserDefinition.java
@@ -48,7 +48,7 @@ public class YAMLParserDefinition implements ParserDefinition, YAMLElementTypes 
 
   @NotNull
   public TokenSet getStringLiteralElements() {
-    return TokenSet.create(YAMLTokenTypes.SCALAR_STRING, YAMLTokenTypes.SCALAR_DSTRING);
+    return TokenSet.create(YAMLTokenTypes.SCALAR_STRING, YAMLTokenTypes.SCALAR_DSTRING, YAMLTokenTypes.TEXT);
   }
 
   @NotNull
