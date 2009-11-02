e7cabd4603bb5269a8c4380075782f58adb220ae
Constantine Plotnikov <Constantine.Plotnikov@jetbrains.com>
2009-11-02 23:04:34 +0300

    git4idea: refactored rebase editor service to support other types of rebase editors

--- plugins/git4idea/src/git4idea/rebase/GitRebaseEditorHandler.java
+++ plugins/git4idea/src/git4idea/rebase/GitRebaseEditorHandler.java
@@ -15,142 +15,15 @@
  */
 package git4idea.rebase;
 
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Ref;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.ui.UIUtil;
-
-import java.io.Closeable;
-
 /**
- * The handler for rebase editor request. The handler shows {@link git4idea.rebase.GitRebaseEditor}
- * dialog with the specified file. If user accepts the changes, it saves file and returns 0,
- * otherwise it just returns error code.
+ * The interface
  */
-public class GitRebaseEditorHandler implements Closeable {
-  /**
-   * The logger
-   */
-  private final static Logger LOG = Logger.getInstance(GitRebaseEditorHandler.class.getName());
-  /**
-   * The service object that has created this handler
-   */
-  private final GitRebaseEditorService myService;
-  /**
-   * The context project
-   */
-  private final Project myProject;
-  /**
-   * The git repository root
-   */
-  private final VirtualFile myRoot;
-  /**
-   * The handler number
-   */
-  private final int myHandlerNo;
-  /**
-   * If true, the handler has been closed
-   */
-  private boolean myIsClosed;
-  /**
-   * Set to true after rebase edior was shown
-   */
-  private boolean myRebaseEditorShown = false;
-
-  /**
-   * The constructor from fields that is expected to be
-   * accessed only from {@link git4idea.rebase.GitRebaseEditorService}.
-   *
-   * @param service   the service object that has created this handler
-   * @param project   the context project
-   * @param root      the git repository root
-   * @param handlerNo the handler no for this editor
-   */
-  GitRebaseEditorHandler(final GitRebaseEditorService service, final Project project, final VirtualFile root, final int handlerNo) {
-    myService = service;
-    myProject = project;
-    myRoot = root;
-    myHandlerNo = handlerNo;
-  }
-
+public interface GitRebaseEditorHandler {
   /**
    * Edit commits request
    *
-   * @param path the path to eding
+   * @param path the path to editing
    * @return the exit code to be returned from editor
    */
-  public int editCommits(final String path) {
-    ensureOpen();
-    final Ref<Boolean> isSuccess = new Ref<Boolean>();
-    UIUtil.invokeAndWaitIfNeeded(new Runnable() {
-      public void run() {
-        try {
-          if (myRebaseEditorShown) {
-            GitRebaseUnstructuredEditor editor = new GitRebaseUnstructuredEditor(myProject, myRoot, path);
-            editor.show();
-            if (editor.isOK()) {
-              editor.save();
-              isSuccess.set(true);
-              return;
-            }
-            else {
-              isSuccess.set(false);
-            }
-          }
-          else {
-            setRebaseEditorShown();
-            GitRebaseEditor editor = new GitRebaseEditor(myProject, myRoot, path);
-            editor.show();
-            if (editor.isOK()) {
-              editor.save();
-              isSuccess.set(true);
-              return;
-            }
-            else {
-              editor.cancel();
-              isSuccess.set(true);
-            }
-          }
-        }
-        catch (Exception e) {
-          LOG.error("Failed to edit the git rebase file: " + path, e);
-        }
-        isSuccess.set(false);
-      }
-    });
-    return (isSuccess.isNull() || !isSuccess.get().booleanValue()) ? GitRebaseEditorMain.ERROR_EXIT_CODE : 0;
-  }
-
-  /**
-   * This method is invoked to indicate that this editor will be invoked in the rebase continuation action.
-   */
-  public void setRebaseEditorShown() {
-    myRebaseEditorShown = true;
-  }
-
-  /**
-   * Check that handler has not yet been closed
-   */
-  private void ensureOpen() {
-    if (myIsClosed) {
-      throw new IllegalStateException("The handler was already closed");
-    }
-  }
-
-  /**
-   * Stop using the handler
-   */
-  public void close() {
-    ensureOpen();
-    myIsClosed = true;
-    myService.unregisterHandler(myHandlerNo);
-  }
-
-  /**
-   * @return the handler number
-   */
-  public int getHandlerNo() {
-    return myHandlerNo;
-  }
+  int editCommits(String path);
 }
