57df3e115477fbf70dbfd703f430bd4b44cfc10e
nik <Nikolay.Chashnikov@jetbrains.com>
2009-11-17 15:32:37 +0300

    artifact editor: highlighting nodes with errors fixed

--- java/compiler/tests/com/intellij/compiler/artifacts/PackagingValidationTestCase.java
+++ java/compiler/tests/com/intellij/compiler/artifacts/PackagingValidationTestCase.java
@@ -38,7 +38,7 @@ public abstract class PackagingValidationTestCase extends PackagingElementsTestC
       super(new MockPackagingEditorContext());
     }
 
-    public void registerError(@NotNull String message, @Nullable PackagingElement<?> place, @Nullable ArtifactProblemQuickFix quickFix) {
+    public void registerError(@NotNull String message, @Nullable List<PackagingElement<?>> pathToPlace, @Nullable ArtifactProblemQuickFix quickFix) {
       myProblems.add(message);
       if (quickFix != null) {
         myQuickFixes.put(message, quickFix);
