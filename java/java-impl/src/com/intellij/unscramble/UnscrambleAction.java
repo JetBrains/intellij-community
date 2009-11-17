d6953a2120d1d11a996951cadbfc860977f0696a
Konstantin Bulenkov <kb@jetbrains.com>
2009-11-17 17:18:07 +0300

    remove stacktrace watcher

--- java/java-impl/src/com/intellij/unscramble/UnscrambleAction.java
+++ java/java-impl/src/com/intellij/unscramble/UnscrambleAction.java
@@ -19,17 +19,10 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.actionSystem.Presentation;
-import com.intellij.openapi.application.ex.ApplicationManagerEx;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 
 public final class UnscrambleAction extends AnAction implements DumbAware {
-  static {
-    if (ApplicationManagerEx.getApplicationEx().isInternal()) {
-      ApplicationManagerEx.getApplicationEx().addApplicationListener(new UnscrambleListener());
-    }
-  }
-
   public void actionPerformed(AnActionEvent e) {
     Project project = PlatformDataKeys.PROJECT.getData(e.getDataContext());
     UnscrambleDialog dialog = new UnscrambleDialog(project);
