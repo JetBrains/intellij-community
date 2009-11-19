ed9418e8a066d2672b395cdc4b444e547ac6b537
Dmitry Cheryasov <dmitry.cheryasov@jetbrains.com>
2009-11-19 02:20:45 +0200

    Added a test suite to include all actual tests in fail-fast order

---
+++ python/testSrc/com/jetbrains/python/PythonAllTestsSuite.java
@@ -0,0 +1,38 @@
+package com.jetbrains.python;
+
+import junit.framework.TestSuite;
+
+/**
+ * A suite to explicitly include all actual tests, in fail-fast order.
+ * User: dcheryasov
+ * Date: Nov 19, 2009 1:23:22 AM
+ */
+public class PythonAllTestsSuite {
+
+  public static final Class[] tests = {
+    PythonLexerTest.class,
+    PythonParsingTest.class,
+    IndentTest.class,
+    StatementPartsTest.class,
+    PythonHighlightingTest.class,
+    PyStubsTest.class,
+    PyResolveTest.class,
+    PyMultiFileResolveTest.class,
+    PyResolveCalleeTest.class,
+    PyToJavaResolveTest.class,
+    PythonCompletionTest.class,
+    PyInheritorsSearchTest.class,
+    PyParameterInfoTest.class,
+    PyDecoratorTest.class,
+    PyQuickDocTest.class,
+    PythonInspectionsTest.class,
+    PythonDemorganLawIntentionTest.class,
+    PySelectWordTest.class,
+  };
+
+  public static TestSuite suite() {
+    return new TestSuite(tests);
+  }
+
+  
+}
