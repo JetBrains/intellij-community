4e732880fce77a061470fe521d2727012281f054
Alexey Kudravtsev <cdr@intellij.com>
2009-11-24 13:40:04 +0300

    problem 17183

--- java/java-impl/src/com/intellij/slicer/DuplicateMap.java
+++ java/java-impl/src/com/intellij/slicer/DuplicateMap.java
@@ -15,6 +15,7 @@
  */
 package com.intellij.slicer;
 
+import com.intellij.usageView.UsageInfo;
 import gnu.trove.THashMap;
 import gnu.trove.TObjectHashingStrategy;
 
@@ -24,25 +25,17 @@ import java.util.Map;
 * @author cdr
 */ // rehash map on each PSI modification since SmartPsiPointer's hashCode() and equals() are changed
 class DuplicateMap {
-  private final Map<SliceUsage, SliceNode> myDuplicates = new THashMap<SliceUsage, SliceNode>(USAGEINFO_EQUALITY);
   private static final TObjectHashingStrategy<SliceUsage> USAGEINFO_EQUALITY = new TObjectHashingStrategy<SliceUsage>() {
     public int computeHashCode(SliceUsage object) {
-      return object.getUsageInfo().hashCode();
+      UsageInfo info = object.getUsageInfo();
+      return info.startOffset + info.endOffset * 31;
     }
 
     public boolean equals(SliceUsage o1, SliceUsage o2) {
       return o1.getUsageInfo().equals(o2.getUsageInfo());
     }
   };
-  private static final TObjectHashingStrategy<SliceNode> SLICE_NODE_EQUALITY = new TObjectHashingStrategy<SliceNode>() {
-    public int computeHashCode(SliceNode object) {
-      return object.getValue().getUsageInfo().hashCode();
-    }
-
-    public boolean equals(SliceNode o1, SliceNode o2) {
-      return o1.getValue().getUsageInfo().equals(o2.getValue().getUsageInfo());
-    }
-  };
+  private final Map<SliceUsage, SliceNode> myDuplicates = new THashMap<SliceUsage, SliceNode>(USAGEINFO_EQUALITY);
 
   public SliceNode putNodeCheckDupe(SliceNode node) {
     SliceUsage usage = node.getValue();
