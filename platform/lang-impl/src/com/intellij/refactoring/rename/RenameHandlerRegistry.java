6a81248729a417b812678d291572f80a113ee775
anna <anna.kozlova@jetbrains.com>
2009-12-03 12:39:33 +0300

    test fixed

--- platform/lang-impl/src/com/intellij/refactoring/rename/RenameHandlerRegistry.java
+++ platform/lang-impl/src/com/intellij/refactoring/rename/RenameHandlerRegistry.java
@@ -67,11 +67,13 @@ public class RenameHandlerRegistry {
     final Map<String, RenameHandler> availableHandlers = new TreeMap<String, RenameHandler>();
     for (RenameHandler renameHandler : Extensions.getExtensions(RenameHandler.EP_NAME)) {
       if (renameHandler.isRenaming(dataContext)) {
+        if (ApplicationManager.getApplication().isUnitTestMode()) return renameHandler;
         availableHandlers.put(getHandlerTitle(renameHandler), renameHandler);
       }
     }
     for (RenameHandler renameHandler : myHandlers) {
       if (renameHandler.isRenaming(dataContext)) {
+        if (ApplicationManager.getApplication().isUnitTestMode()) return renameHandler;
         availableHandlers.put(getHandlerTitle(renameHandler), renameHandler);
       }
     }
