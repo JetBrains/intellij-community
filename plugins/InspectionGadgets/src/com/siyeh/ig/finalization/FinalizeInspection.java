da6c0133c3ebdf6d66f12d1d06d332defa0f33f6
Bas Leijdekkers <leijdekkers@carp-technologies.nl>
2009-12-09 17:42:36 +0100

    IDEADEV-41933 Add option to ignore trivial finalize​​() methods to "​​​'​​finalize​​(​​​)' declaration" inspection

--- plugins/InspectionGadgets/src/com/siyeh/ig/finalization/FinalizeInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/finalization/FinalizeInspection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2007 Dave Griffith, Bas Leijdekkers
+ * Copyright 2003-2009 Dave Griffith, Bas Leijdekkers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,15 +15,21 @@
  */
 package com.siyeh.ig.finalization;
 
-import com.intellij.psi.PsiMethod;
-import com.intellij.psi.PsiParameterList;
+import com.intellij.codeInspection.ui.SingleCheckboxOptionsPanel;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.*;
 import com.siyeh.HardcodedMethodConstants;
 import com.siyeh.InspectionGadgetsBundle;
 import com.siyeh.ig.BaseInspection;
 import com.siyeh.ig.BaseInspectionVisitor;
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.*;
+
 public class FinalizeInspection extends BaseInspection {
+
+    @SuppressWarnings("PublicField")
+    public boolean ignoreTrivialFinalizers = true;
 
     @NotNull
     public String getID(){
@@ -42,11 +48,18 @@ public class FinalizeInspection extends BaseInspection {
                 "finalize.declaration.problem.descriptor");
     }
 
+    @Override
+    public JComponent createOptionsPanel() {
+        return new SingleCheckboxOptionsPanel(
+                InspectionGadgetsBundle.message("ignore.trivial.finalizers.option"), this,
+                "ignoreTrivialFinalizers");
+    }
+
     public BaseInspectionVisitor buildVisitor() {
         return new FinalizeDeclaredVisitor();
     }
 
-    private static class FinalizeDeclaredVisitor extends BaseInspectionVisitor {
+    private class FinalizeDeclaredVisitor extends BaseInspectionVisitor {
 
         @Override public void visitMethod(@NotNull PsiMethod method) {
             //note: no call to super;
@@ -58,7 +71,40 @@ public class FinalizeInspection extends BaseInspection {
             if (parameterList.getParametersCount() != 0) {
                 return;
             }
+            if (ignoreTrivialFinalizers && isTrivial(method)) {
+                return;
+            }
             registerMethodError(method);
+        }
+
+        private boolean isTrivial(PsiMethod method) {
+            final PsiCodeBlock body = method.getBody();
+            if (body == null) {
+                return true;
+            }
+            final PsiStatement[] statements = body.getStatements();
+            if (statements.length == 0) {
+                return true;
+            }
+            final Project project = method.getProject();
+            final JavaPsiFacade psiFacade =
+                    JavaPsiFacade.getInstance(project);
+            final PsiConstantEvaluationHelper evaluationHelper =
+                    psiFacade.getConstantEvaluationHelper();
+            for (PsiStatement statement : statements) {
+                if (!(statement instanceof PsiIfStatement)) {
+                    return false;
+                }
+                final PsiIfStatement ifStatement =
+                        (PsiIfStatement) statement;
+                final PsiExpression condition = ifStatement.getCondition();
+                final Object result =
+                        evaluationHelper.computeConstantExpression(condition);
+                if (result == null || !result.equals(Boolean.FALSE)) {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
