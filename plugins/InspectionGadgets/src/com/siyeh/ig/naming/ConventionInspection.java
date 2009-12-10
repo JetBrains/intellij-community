2e476669319c78ce047384a5657ba8a363976997
Bas Leijdekkers <leijdekkers@carp-technologies.nl>
2009-12-10 15:01:33 +0100

    IDEADEV-41943 (text entry fields in the configuration panels for inspections are _​extremely_ narrow on Mac OS X Leopard)

--- plugins/InspectionGadgets/src/com/siyeh/ig/naming/ConventionInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/naming/ConventionInspection.java
@@ -171,24 +171,20 @@ public abstract class ConventionInspection extends BaseInspection {
         final GridBagConstraints constraints = new GridBagConstraints();
         constraints.gridx = 0;
         constraints.gridy = 0;
-        constraints.insets.left = 4;
-        constraints.insets.right = 4;
-        constraints.insets.top = 2;
-        constraints.insets.bottom = 2;
+        constraints.weightx = 1.0;
         constraints.anchor = GridBagConstraints.EAST;
         panel.add(patternLabel, constraints);
 
         constraints.gridx = 1;
         constraints.gridy = 0;
         constraints.gridwidth = 3;
-        constraints.weightx = 1.0;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
         constraints.anchor = GridBagConstraints.WEST;
         panel.add(regexField, constraints);
 
         constraints.gridx = 0;
         constraints.gridy = 1;
         constraints.gridwidth = 1;
-        constraints.weightx = 0.0;
         constraints.anchor = GridBagConstraints.EAST;
         panel.add(minLengthLabel, constraints);
 
