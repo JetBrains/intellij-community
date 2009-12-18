8b5ebfc2f0d2dd33a4c5d504d9f8f0e312bab31d
Alexey Kudravtsev <cdr@intellij.com>
2009-12-18 14:37:31 +0300

    good code is red

--- java/java-impl/src/com/intellij/codeInsight/daemon/impl/analysis/GenericsHighlightUtil.java
+++ java/java-impl/src/com/intellij/codeInsight/daemon/impl/analysis/GenericsHighlightUtil.java
@@ -1079,10 +1079,9 @@ public class GenericsHighlightUtil {
       if (((PsiModifierListOwner)element).hasModifierProperty(PsiModifier.STATIC)) return null;
       PsiClass containingClass = ((PsiMember)element).getContainingClass();
       if (containingClass != null && PsiUtil.isRawSubstitutor(containingClass, resolveResult.getSubstitutor())) {
-        final String message;
-        message = element instanceof PsiClass
-                  ? JavaErrorMessages.message("generics.type.arguments.on.raw.type")
-                  : JavaErrorMessages.message("generics.type.arguments.on.raw.method");
+        final String message = element instanceof PsiClass
+                               ? JavaErrorMessages.message("generics.type.arguments.on.raw.type")
+                               : JavaErrorMessages.message("generics.type.arguments.on.raw.method");
 
         return HighlightInfo.createHighlightInfo(HighlightInfoType.ERROR, refParamList, message);
       }
@@ -1189,34 +1188,6 @@ public class GenericsHighlightUtil {
     if (superClass instanceof PsiTypeParameter) {
       return HighlightInfo.createHighlightInfo(HighlightInfoType.ERROR, toHighlight,
                                                JavaErrorMessages.message("class.cannot.inherit.from.its.type.parameter"));
-    }
-    return null;
-  }
-
-  public static HighlightInfo checkTopLevelMethodCallIntersectionTypeMaximalUpperBound(PsiMethodCallExpression methodCall,
-                                                                                       PsiReferenceExpression expression) {
-    PsiElement parent = methodCall.getParent();
-    if (parent instanceof PsiExpression) return null;
-    PsiType type = expression.getType();
-    if (!(type instanceof PsiIntersectionType)) {
-      return null;
-    }
-    PsiType[] conjuncts = ((PsiIntersectionType)type).getConjuncts();
-    PsiType lub = conjuncts[0];
-    for (int i = 1; i < conjuncts.length; i++) {
-      PsiType conjunct = conjuncts[i];
-      PsiType bound = GenericsUtil.getLeastUpperBound(lub, conjunct, methodCall.getManager());
-      if (bound == null || !TypeConversionUtil.isAssignable(lub, bound) || !TypeConversionUtil.isAssignable(conjunct, bound)) {
-        PsiMethod method = methodCall.resolveMethod();
-        if (method == null) return null;
-        String message = JavaErrorMessages.message("failed.to.find.unique.maximal.instance", 
-                                                   HighlightUtil.formatMethod(method),
-                  HighlightUtil.formatType(lub), HighlightUtil.formatType(conjunct));
-
-
-        return HighlightInfo.createHighlightInfo(HighlightInfoType.ERROR, expression, message);
-      }
-      lub = bound;
     }
     return null;
   }
