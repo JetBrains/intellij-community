7cc74acdcbf28592dbd03bfe41a295d495419a7b
Dmitry Jemerov <yole@jetbrains.com>
2009-12-24 17:15:10 +0300

    convert test to PyLightFixtureTestCase

--- python/testSrc/com/jetbrains/python/PythonDemorganLawIntentionTest.java
+++ python/testSrc/com/jetbrains/python/PythonDemorganLawIntentionTest.java
@@ -4,21 +4,29 @@
  */
 package com.jetbrains.python;
 
-import com.intellij.codeInsight.daemon.LightIntentionActionTestCase;
+import com.intellij.codeInsight.intention.IntentionAction;
+import com.jetbrains.python.fixtures.PyLightFixtureTestCase;
 
-public class PythonDemorganLawIntentionTest extends LightIntentionActionTestCase {
-  public void test() throws Exception { doAllTests(); }
-
-  protected String getBasePath() {
-    return "/demorgan";
+public class PythonDemorganLawIntentionTest extends PyLightFixtureTestCase {
+  public void test1() throws Exception {
+    doTest();
   }
 
+  public void test2() throws Exception {
+    doTest();
+  }
+
+  public void test3() throws Exception {
+    doTest();
+  }
+
+  private void doTest() throws Exception {
+    myFixture.configureByFile("before" + getTestName(false) + ".py");
+    final IntentionAction action = myFixture.findSingleIntention("DeMorgan Law");
+    myFixture.launchAction(action);
+
+  }
   protected String getTestDataPath() {
-    return PythonTestUtil.getTestDataPath() + "/intentions";
-  }
-
-  @Override
-  protected boolean shouldBeAvailableAfterExecution() {
-    return true;
+    return PythonTestUtil.getTestDataPath() + "/intentions/demorgan";
   }
 }
