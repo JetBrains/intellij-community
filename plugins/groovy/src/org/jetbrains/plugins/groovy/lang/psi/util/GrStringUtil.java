ca3f1e3b5f53c376ba70054abe654a78be40db29
Maxim Medvedev <maxim.medvedev@jetbrains.com>
2009-12-26 18:55:42 +0300

    Exception in 'Remove unnecessary braces in GString injection'

--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/util/GrStringUtil.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/util/GrStringUtil.java
@@ -223,11 +223,22 @@ public class GrStringUtil {
       return true;
     }
     final GroovyPsiElementFactory elementFactory = GroovyPsiElementFactory.getInstance(element.getProject());
-    final GrExpression gString = elementFactory.createExpressionFromText("\"$" + statements[0].getText() + nextChar + '"');
-    final GrReferenceExpression refExpr = (GrReferenceExpression)statements[0];
-    final PsiElement refExprCopy = ((GrStringInjection)gString.getChildren()[0]).getReferenceExpression();
+    final GrExpression gString;
+    try {
+      gString = elementFactory.createExpressionFromText("\"$" + statements[0].getText() + nextChar + '"');
+    }
+    catch (Exception e) {
+      return false;
+    }
+    if (!(gString instanceof GrString)) return false;
+
+    final PsiElement child = gString.getChildren()[0];
+    if (!(child instanceof GrStringInjection)) return false;
+
+    final PsiElement refExprCopy = ((GrStringInjection)child).getReferenceExpression();
     if (!(refExprCopy instanceof GrReferenceExpression)) return false;
 
+    final GrReferenceExpression refExpr = (GrReferenceExpression)statements[0];
     return Comparing.equal(refExpr.getName(), ((GrReferenceExpression)refExprCopy).getName());
   }
 
