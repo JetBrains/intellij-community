30ce81e0625411bdecce468be4a27a6ea0014e1f
Eugene Zhuravlev <jeka@intellij.com>
2010-01-15 19:03:02 +0300

    [ann] read whole classpath

--- java/java-runtime/src/com/intellij/rt/execution/CommandLineWrapper.java
+++ java/java-runtime/src/com/intellij/rt/execution/CommandLineWrapper.java
@@ -40,12 +40,14 @@ public class CommandLineWrapper {
     final File file = new File(args[0]);
     final BufferedReader reader = new BufferedReader(new FileReader(file));
     try {
-      final String fileName = reader.readLine();
-      try {
-        urls.add(new File(fileName).toURI().toURL());
-      }
-      catch (NoSuchMethodError e) {
-        urls.add(new File(fileName).toURL());
+      while (reader.ready()) {
+        final String fileName = reader.readLine();
+        try {
+          urls.add(new File(fileName).toURI().toURL());
+        }
+        catch (NoSuchMethodError e) {
+          urls.add(new File(fileName).toURL());
+        }
       }
     }
     finally {
