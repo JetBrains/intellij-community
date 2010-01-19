bfa3836e76407da1433528fe0a22fb8533a9b510
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2010-01-19 17:08:25 +0300

    Now TrackCoverageAction is Java-free!

--- plugins/coverage/src/com/intellij/coverage/JavaCoverageSupportProvider.java
+++ plugins/coverage/src/com/intellij/coverage/JavaCoverageSupportProvider.java
@@ -4,6 +4,12 @@ import com.intellij.execution.RunJavaConfiguration;
 import com.intellij.execution.configurations.ModuleBasedConfiguration;
 import com.intellij.execution.configurations.coverage.CoverageEnabledConfiguration;
 import com.intellij.execution.configurations.coverage.JavaCoverageEnabledConfiguration;
+import com.intellij.execution.testframework.AbstractTestProxy;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiMethod;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * @author Roman.Chernyatchik
@@ -14,6 +20,20 @@ public class JavaCoverageSupportProvider extends CoverageSupportProvider {
     if (conf instanceof RunJavaConfiguration) {
       return new JavaCoverageEnabledConfiguration(conf);
     }
+    return null;
+  }
+
+  @Nullable
+  @Override
+  public String getTestMethodName(@NotNull PsiElement element, @NotNull AbstractTestProxy proxy) {
+    if (element instanceof PsiMethod) {
+      final PsiMethod method = (PsiMethod)element;
+      final PsiClass psiClass = method.getContainingClass();
+      assert psiClass != null;
+
+      return psiClass.getQualifiedName() + "." + method.getName();
+    }
+
     return null;
   }
 }
