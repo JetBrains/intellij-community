186c08d284310c31cb0722eaed6b49ca5d54f31b
Alexey Kudravtsev <cdr@intellij.com>
2010-01-19 16:07:39 +0300

    javadoc

--- platform/lang-api/src/com/intellij/psi/LiteralTextEscaper.java
+++ platform/lang-api/src/com/intellij/psi/LiteralTextEscaper.java
@@ -33,8 +33,23 @@ public abstract class LiteralTextEscaper<T extends PsiLanguageInjectionHost> {
 
   /**
    * @param offsetInDecoded offset in the parsed injected file
-   * @param rangeInsideHost
-   * @return offset in the host PSI element, or -1 if offset is out of host range
+   * @param rangeInsideHost range where injection is performed,
+   *   E.g. if some language fragment xyz was injected into string literal expression "xyz", then rangeInsideHost = (1,4)
+   * @return offset in the host PSI element, or -1 if offset is out of host range.
+   * E.g. if some language fragment xyz was injected into string literal expression "xyz", then
+   *     getOffsetInHost(0)==1 (there is an 'x' at offset 0 in injected fragment,
+   *                            and that 'x' occurs in "xyz" string literal at offset 1
+   *                            since string literal expression "xyz" starts with double quote)
+   *     getOffsetInHost(1)==2
+   *     getOffsetInHost(2)==3
+   *     getOffsetInHost(3)==-1  (out of range)
+   *
+   * Similarly, for some language fragment xyz being injected into xml text inside xml tag 'tag': <tag>xyz</tag>
+   *     getOffsetInHost(0)==0 (there is an 'x' at offset 0 in injected fragment,
+   *                            and that 'x' occurs in xyz xml text at offset 0)
+   *     getOffsetInHost(1)==1
+   *     getOffsetInHost(2)==2
+   *     getOffsetInHost(3)==-1  (out of range)
    */
   public abstract int getOffsetInHost(int offsetInDecoded, @NotNull TextRange rangeInsideHost);
 
