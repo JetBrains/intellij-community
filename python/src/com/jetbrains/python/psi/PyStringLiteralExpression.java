b40bbe0bdd7dd895c41279abfe128e2b6b3ee008
Dmitry Jemerov <yole@jetbrains.com>
2010-01-19 18:25:40 +0300

    unused import

--- python/src/com/jetbrains/python/psi/PyStringLiteralExpression.java
+++ python/src/com/jetbrains/python/psi/PyStringLiteralExpression.java
@@ -3,7 +3,6 @@ package com.jetbrains.python.psi;
 import com.intellij.lang.ASTNode;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiLanguageInjectionHost;
-import com.jetbrains.python.psi.impl.EvaluatedTextRange;
 
 import java.util.List;
 
