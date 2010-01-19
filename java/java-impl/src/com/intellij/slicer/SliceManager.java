995493712ac5f78592e7a2f2e077c0230c1c66f7
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2010-01-19 12:04:34 +0300

    quick compilation fix

--- java/java-impl/src/com/intellij/slicer/SliceManager.java
+++ java/java-impl/src/com/intellij/slicer/SliceManager.java
@@ -126,7 +126,7 @@ public class SliceManager implements PersistentStateComponent<SliceManager.Bean>
     analysisUIOptions.ANALYZE_TEST_SOURCES = myStoredSettings.includeTestSources;
     AnalysisScope analysisScope = new AnalysisScope(element.getContainingFile());
     String name = module == null ? null : module.getName();
-    BaseAnalysisActionDialog dialog = new BaseAnalysisActionDialog(dialogTitle, "Analyze scope", myProject, analysisScope, name, true, analysisUIOptions);
+    BaseAnalysisActionDialog dialog = new BaseAnalysisActionDialog(dialogTitle, "Analyze scope", myProject, analysisScope, name, true, analysisUIOptions, element);
     dialog.show();
     if (!dialog.isOK()) return;
 
