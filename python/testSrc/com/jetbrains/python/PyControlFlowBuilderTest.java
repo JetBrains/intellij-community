f3afb977afabe10f79b775682f5239dece07ddcd
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2010-01-20 15:42:11 +0300

    Support for try-except statements

--- python/testSrc/com/jetbrains/python/PyControlFlowBuilderTest.java
+++ python/testSrc/com/jetbrains/python/PyControlFlowBuilderTest.java
@@ -58,6 +58,10 @@ public class PyControlFlowBuilderTest extends LightMarkedTestCase {
     doTest();
   }
 
+  public void testTry() throws Exception {
+    doTest();
+  }
+
   public void testFunction() throws Exception {
     final String testName = getTestName(false).toLowerCase();
     configureByFile(testName + ".py");
