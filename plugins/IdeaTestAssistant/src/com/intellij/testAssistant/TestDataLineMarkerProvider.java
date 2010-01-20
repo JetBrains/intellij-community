b2575424d1923f70b02f737cd6ec66d17f03bf81
Dmitry Jemerov <yole@jetbrains.com>
2010-01-20 21:36:56 +0300

    prompt to create missing testdata files

--- plugins/IdeaTestAssistant/src/com/intellij/testAssistant/TestDataLineMarkerProvider.java
+++ plugins/IdeaTestAssistant/src/com/intellij/testAssistant/TestDataLineMarkerProvider.java
@@ -5,25 +5,31 @@ import com.intellij.codeInsight.AnnotationUtil;
 import com.intellij.codeInsight.daemon.GutterIconNavigationHandler;
 import com.intellij.codeInsight.daemon.LineMarkerInfo;
 import com.intellij.codeInsight.daemon.LineMarkerProvider;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.fileTypes.FileTypeManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectFileIndex;
 import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.ui.popup.PopupChooserBuilder;
+import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.*;
 import com.intellij.ui.ColoredListCellRenderer;
 import com.intellij.ui.awt.RelativePoint;
+import com.intellij.util.ArrayUtil;
 import com.intellij.util.Icons;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.event.MouseEvent;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -106,8 +112,8 @@ public class TestDataLineMarkerProvider implements LineMarkerProvider {
       if (myFileNames.size() != 2) {
         return null;
       }
-      VirtualFile file1 = LocalFileSystem.getInstance().findFileByPath(myFileNames.get(0));
-      VirtualFile file2 = LocalFileSystem.getInstance().findFileByPath(myFileNames.get(1));
+      VirtualFile file1 = LocalFileSystem.getInstance().refreshAndFindFileByPath(myFileNames.get(0));
+      VirtualFile file2 = LocalFileSystem.getInstance().refreshAndFindFileByPath(myFileNames.get(1));
       if (file1 == null || file2 == null) {
         return null;
       }
@@ -126,13 +132,19 @@ public class TestDataLineMarkerProvider implements LineMarkerProvider {
       for (String fileName : myFileNames) {
         shortNames.add(new File(fileName).getName());
       }
+      final String CREATE_MISSING_OPTION = "Create Missing Files";
+      if (myFileNames.size() == 2) {
+        shortNames.add(CREATE_MISSING_OPTION);
+      }
       final JList list = new JList(shortNames.toArray(new String[shortNames.size()]));
       list.setCellRenderer(new ColoredListCellRenderer() {
         @Override
         protected void customizeCellRenderer(JList list, Object value, int index, boolean selected, boolean hasFocus) {
           String fileName = (String)value;
-          final FileType fileType = FileTypeManager.getInstance().getFileTypeByFileName(fileName);
-          setIcon(fileType.getIcon());
+          if (!fileName.equals(CREATE_MISSING_OPTION)) {
+            final FileType fileType = FileTypeManager.getInstance().getFileTypeByFileName(fileName);
+            setIcon(fileType.getIcon());
+          }
           append(fileName);
         }
       });
@@ -140,18 +152,60 @@ public class TestDataLineMarkerProvider implements LineMarkerProvider {
       builder.setItemChoosenCallback(new Runnable() {
         public void run() {
           final int[] indices = list.getSelectedIndices();
-          for (int index : indices) {
-            openFileByIndex(project, index);
+          if (ArrayUtil.indexOf(indices, myFileNames.size()) >= 0) {
+            createMissingFiles(project);
+          }
+          else {
+            for (int index : indices) {
+              openFileByIndex(project, index);
+            }
           }
         }
       }).createPopup().show(new RelativePoint(e));
     }
 
+    private void createMissingFiles(Project project) {
+      for (String name : myFileNames) {
+        if (LocalFileSystem.getInstance().refreshAndFindFileByPath(name) == null) {
+          createFileByName(project, name);
+        }
+      }
+      final TestDataGroupVirtualFile testDataGroup = getTestDataGroup();
+      if (testDataGroup != null) {
+        new OpenFileDescriptor(project, testDataGroup).navigate(true);
+      }
+    }
+
     private void openFileByIndex(final Project project, final int index) {
-      final VirtualFile file = LocalFileSystem.getInstance().findFileByPath(myFileNames.get(index));
+      final String path = myFileNames.get(index);
+      final VirtualFile file = LocalFileSystem.getInstance().refreshAndFindFileByPath(path);
       if (file != null) {
         new OpenFileDescriptor(project, file).navigate(true);
       }
+      else {
+        int rc = Messages.showYesNoDialog(project, "The referenced testdata file " + path + " does not exist. Would you like to create it?",
+                                          "Create Testdata File", Messages.getQuestionIcon());
+        if (rc == 0) {
+          VirtualFile vFile = createFileByName(project, path);
+          new OpenFileDescriptor(project, vFile).navigate(true);
+        }
+      }
+    }
+
+    private VirtualFile createFileByName(final Project project, final String path) {
+      return ApplicationManager.getApplication().runWriteAction(new Computable<VirtualFile>() {
+        public VirtualFile compute() {
+          try {
+            final File file = new File(path);
+            final VirtualFile parent = VfsUtil.createDirectories(file.getParent());
+            return parent.createChildData(this, file.getName());
+          }
+          catch (IOException e) {
+            Messages.showErrorDialog(project, e.getMessage(), "Create Testdata File");
+            return null;
+          }
+        }
+      });
     }
   }
 }
