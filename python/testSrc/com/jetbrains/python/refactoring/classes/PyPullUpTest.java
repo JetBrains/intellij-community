837ef22f6091842d1a208fbe77675eb73257e8ed
Dennis Ushakov <dennis.ushakov@jetbrains.com>
2010-01-25 16:15:43 +0300

    imports

--- python/testSrc/com/jetbrains/python/refactoring/classes/PyPullUpTest.java
+++ python/testSrc/com/jetbrains/python/refactoring/classes/PyPullUpTest.java
@@ -1,17 +1,10 @@
 package com.jetbrains.python.refactoring.classes;
 
-import com.intellij.openapi.project.Project;
-import com.intellij.psi.search.ProjectScope;
-import com.intellij.psi.stubs.StubIndex;
 import com.jetbrains.python.PythonTestUtil;
-import com.jetbrains.python.fixtures.PyLightFixtureTestCase;
 import com.jetbrains.python.psi.PyClass;
 import com.jetbrains.python.psi.PyElement;
-import com.jetbrains.python.psi.PyFunction;
-import com.jetbrains.python.psi.stubs.PyClassNameIndex;
 import com.jetbrains.python.refactoring.classes.pullUp.PyPullUpHelper;
 
-import java.util.Collection;
 import java.util.Collections;
 
 /**
