4ffc70d4b387355c1296ee8fc1606c86ef9993f5
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2010-02-02 19:48:18 +0300

    Unreachable code inspection uses control flow

--- python/src/com/jetbrains/python/inspections/PyUnreachableCodeInspection.java
+++ python/src/com/jetbrains/python/inspections/PyUnreachableCodeInspection.java
@@ -1,17 +1,20 @@
 package com.jetbrains.python.inspections;
 
+import com.intellij.codeInsight.controlflow.Instruction;
 import com.intellij.codeInspection.LocalInspectionTool;
 import com.intellij.codeInspection.ProblemsHolder;
-import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.jetbrains.python.PyBundle;
-import com.jetbrains.python.psi.*;
+import com.jetbrains.python.codeInsight.controlflow.ScopeOwner;
+import com.jetbrains.python.psi.PyExceptPart;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+
 /**
- * Detects code following 'return', 'break', 'continue', and 'raise'.
+ * Detects unreachable code using control flow graph
  */
 public class PyUnreachableCodeInspection extends LocalInspectionTool {
   @Nls
@@ -48,41 +51,47 @@ public class PyUnreachableCodeInspection extends LocalInspectionTool {
       super(holder);
     }
 
-    private void markFollowingStatementAsWrong(PsiElement node) {
-      //if (node.getParent() instanceof PyStatementList) { // check just in case?
-      PsiElement first_after_us = PyUtil.getFirstNonCommentAfter(node.getNextSibling());
-      if (
-        first_after_us instanceof PyElement && first_after_us.isValid() && first_after_us.isPhysical() &&
-        first_after_us.getTextLength() > 0 && ! "".equals(first_after_us.getText().trim())
-      ) {
-        getHolder().registerProblem(
-          first_after_us, PyBundle.message("INSP.unreachable.code"),
-          ProblemHighlightType.GENERIC_ERROR_OR_WARNING
-        );
+    @Override
+    public void visitElement(final PsiElement element) {
+      if (element instanceof ScopeOwner) {
+        // Look for decoupled components of control flow graph
+        final Instruction[] flow = ((ScopeOwner)element).getControlFlow().getInstructions();
+        final int[] colors = new int[flow.length];
+        for (int i = 0;i<flow.length;i++){
+          colors[i] = i;
+        }
+
+        boolean colorChanged;
+        do {
+          colorChanged = false;
+          for (Instruction instruction : flow) {
+            for (Instruction succ : instruction.allSucc()) {
+              if (colors[instruction.num()] < colors[succ.num()]){
+                colors[succ.num()] = colors[instruction.num()];
+                colorChanged = true;
+              }
+            }
+          }
+        } while (colorChanged);
+
+        int color = colors[0];
+        final boolean warned[] = new boolean[flow.length];
+        Arrays.fill(warned, false);
+        for (Instruction instruction : flow) {
+          final PsiElement e = instruction.getElement();
+          if (colors[instruction.num()] != color){
+            color = colors[instruction.num()];
+            if (!warned[color]){
+              warned[color] = true;
+              // Handle ensure parts
+              if (e instanceof PyExceptPart) {
+                continue;
+              }
+              registerProblem(e, PyBundle.message("INSP.unreachable.code"));
+            }
+          }
+        }
       }
     }
-
-    @Override
-    public void visitPyReturnStatement(PyReturnStatement node) {
-      super.visitPyElement(node);
-      markFollowingStatementAsWrong(node);
-    }
-
-    @Override
-    public void visitPyRaiseStatement(PyRaiseStatement node) {
-      markFollowingStatementAsWrong(node);
-    }
-
-    @Override
-    public void visitPyBreakStatement(PyBreakStatement node) {
-      markFollowingStatementAsWrong(node);
-    }
-
-    @Override
-    public void visitPyContinueStatement(PyContinueStatement node) {
-      markFollowingStatementAsWrong(node);
-    }
-
-
   }
 }
