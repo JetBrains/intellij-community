fae1fc898362972695b993bc697c273f55033c93
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2010-02-04 12:23:49 +0300

    PY-461: Extract Method doesn​'​t handle '​self' parameter

--- python/testData/refactoring/extractmethod/class.before.py
+++
@@ -1,5 +0,0 @@
-class Foo(X, Y, Z):
-    def __init__(self):
-        for base in self__class__.__bases__:
-            <selection>try: base.__init__(self)
-            except AttributeError: pass</selection>
