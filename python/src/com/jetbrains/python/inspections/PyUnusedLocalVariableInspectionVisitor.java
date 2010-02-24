5d39123a02ee63df360ec8bf92ba1c0d553ad5e8
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2010-02-24 12:42:00 +0300

    PY-559 Unused assignment false positive in case of nested scopes

---
+++ python/src/com/jetbrains/python/inspections/PyUnusedLocalVariableInspectionVisitor.java
@@ -0,0 +1,166 @@
+package com.jetbrains.python.inspections;
+
+import com.intellij.codeInsight.controlflow.ControlFlow;
+import com.intellij.codeInsight.controlflow.ControlFlowUtil;
+import com.intellij.codeInsight.controlflow.Instruction;
+import com.intellij.codeInspection.ProblemHighlightType;
+import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.ResolveResult;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.util.Function;
+import com.jetbrains.python.PyBundle;
+import com.jetbrains.python.codeInsight.controlflow.PyControlFlowUtil;
+import com.jetbrains.python.codeInsight.controlflow.ReadWriteInstruction;
+import com.jetbrains.python.codeInsight.controlflow.ScopeOwner;
+import com.jetbrains.python.psi.*;
+import com.jetbrains.python.psi.impl.PyAugAssignmentStatementNavigator;
+import com.jetbrains.python.psi.impl.PyImportStatementNavigator;
+import com.jetbrains.python.psi.impl.PyPsiUtils;
+import com.jetbrains.python.psi.search.PySuperMethodsSearch;
+
+import java.util.HashSet;
+
+/**
+ * @author oleg
+ */
+class PyUnusedLocalVariableInspectionVisitor extends PyInspectionVisitor {
+  private HashSet<PsiElement> myUnusedElements;
+
+  public PyUnusedLocalVariableInspectionVisitor(final ProblemsHolder holder) {
+    super(holder);
+    myUnusedElements = new HashSet<PsiElement>();
+  }
+
+  @Override
+  public void visitPyFile(final PyFile node) {
+    node.accept(new PyRecursiveElementVisitor() {
+      @Override
+      public void visitElement(PsiElement element) {
+        ProgressManager.checkCanceled();
+        super.visitElement(element);
+      }
+
+      @Override
+      public void visitPyFile(final PyFile node) {
+        processScope(node);
+        super.visitPyFile(node);
+      }
+
+      @Override
+      public void visitPyClass(final PyClass node) {
+        processScope(node);
+        super.visitPyClass(node);
+      }
+
+      @Override
+      public void visitPyFunction(final PyFunction node) {
+        processScope(node);
+        super.visitPyFunction(node);
+      }
+    });
+    registerProblems();
+  }
+
+
+  private void processScope(final ScopeOwner owner) {
+    // TODO[oleg] Do not show warning in python code expression mode (evaluate in debug or watches)
+
+    // If method overrides others do not mark parameters as unused if they are
+    boolean parametersCanBeUnused = false;
+    if (owner instanceof PyFunction) {
+      parametersCanBeUnused = PySuperMethodsSearch.search(((PyFunction)owner)).findFirst() != null;
+    }
+
+    final ControlFlow flow = owner.getControlFlow();
+    final Instruction[] instructions = flow.getInstructions();
+
+    // Iteration over write accesses
+    for (int i = 0; i < instructions.length; i++) {
+      final Instruction instruction = instructions[i];
+      if (instruction instanceof ReadWriteInstruction) {
+        final String name = ((ReadWriteInstruction)instruction).getName();
+        if (name == null) {
+          continue;
+        }
+        final PsiElement element = instruction.getElement();
+        // Ignore arguments of import statement
+        if (PyImportStatementNavigator.getImportStatementByElement(element) != null) {
+          continue;
+        }
+        final ReadWriteInstruction.ACCESS access = ((ReadWriteInstruction)instruction).getAccess();
+        // WriteAccess
+        if (access.isWriteAccess() && (parametersCanBeUnused || !(element != null && element.getParent() instanceof PyNamedParameter))) {
+          myUnusedElements.add(element);
+        }
+      }
+    }
+
+    // Iteration over read accesses
+    for (int i = 0; i < instructions.length; i++) {
+      final Instruction instruction = instructions[i];
+      if (instruction instanceof ReadWriteInstruction) {
+        final String name = ((ReadWriteInstruction)instruction).getName();
+        if (name == null) {
+          continue;
+        }
+        final PsiElement element = instruction.getElement();
+        final ReadWriteInstruction.ACCESS access = ((ReadWriteInstruction)instruction).getAccess();
+        // Read or self assign access
+        if (access.isReadAccess()) {
+          int number = i;
+          if (access == ReadWriteInstruction.ACCESS.READWRITE) {
+            final PyAugAssignmentStatement augAssignmentStatement = PyAugAssignmentStatementNavigator.getStatementByTarget(element);
+            number = ControlFlowUtil.findInstructionNumberByElement(instructions, augAssignmentStatement);
+          }
+
+          // Check out of scope resolve elements, processes nested scopes
+          if (element instanceof PyReferenceExpression){
+            boolean outOfScope = false;
+            for (ResolveResult result : ((PyReferenceExpression)element).multiResolve(false)) {
+              final PsiElement resolveElement = result.getElement();
+              if (!PsiTreeUtil.isAncestor(owner, resolveElement, false)){
+                outOfScope = true;
+                myUnusedElements.remove(resolveElement);
+              }
+            }
+            if (outOfScope){
+              continue;
+            }
+          }
+
+          PyControlFlowUtil
+            .iterateWriteAccessFor(name, number, instructions, new Function<ReadWriteInstruction, PyControlFlowUtil.Operation>() {
+              public PyControlFlowUtil.Operation fun(final ReadWriteInstruction rwInstr) {
+                final PsiElement instrElement = rwInstr.getElement();
+                myUnusedElements.remove(instrElement);
+                return PyControlFlowUtil.Operation.CONTINUE;
+              }
+            });
+        }
+      }
+    }
+  }
+
+  private void registerProblems() {
+    // Register problems
+    for (PsiElement element : myUnusedElements) {
+      final String name = element.getText();
+      // Ignore unused self parameters as obligatory
+      if (PyPsiUtils.isMethodContext(element) && "self".equals(name)) {
+        continue;
+      }
+      if (element instanceof PyNamedParameter) {
+        registerWarning(element, PyBundle.message("INSP.unused.locals.parameter.isnot.used", name));
+      }
+      else {
+        registerWarning(element, PyBundle.message("INSP.unused.locals.local.variable.isnot.used", name));
+      }
+    }
+  }
+
+  private void registerWarning(final PsiElement element, final String msg) {
+    registerProblem(element, msg, ProblemHighlightType.LIKE_UNUSED_SYMBOL, null);
+  }
+}
