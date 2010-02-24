d9e886fd25d1f581c4d2cddc43c6f11a45848c3e
anna <anna.kozlova@jetbrains.com>
2010-02-24 18:08:31 +0300

    cleanup

--- plugins/coverage/src/com/intellij/coverage/listeners/CoverageListener.java
+++ plugins/coverage/src/com/intellij/coverage/listeners/CoverageListener.java
@@ -5,12 +5,13 @@
 package com.intellij.coverage.listeners;
 
 public abstract class CoverageListener {
+  private static final Class[] EMPTY_CLASS_ARRAY = new Class[0];
   private Object myProjectData;
 
   protected Object getData() {
     try {
 
-     return Class.forName("com.intellij.rt.coverage.data.ProjectData").getMethod("getProjectData", new Class[0]).invoke(null);
+     return Class.forName("com.intellij.rt.coverage.data.ProjectData").getMethod("getProjectData", EMPTY_CLASS_ARRAY).invoke(null);
 
     }
     catch (Exception e) {
