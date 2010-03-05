d62a385d6597996a0c896f3ddf4b6845403f7135
Alexander Doroshko <adoroshko@swiftteams.com>
2010-03-05 13:17:35 +0300

    documentation is not always javadoc

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/utils/MavenConstants.java
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/utils/MavenConstants.java
@@ -27,9 +27,6 @@ public class MavenConstants {
   public static final String TYPE_TEST_JAR = "test-jar";
   public static final String TYPE_WAR = "war";
 
-  public static final String CLASSIFIER_JAVADOC = "javadoc";
-  public static final String CLASSIFIER_SOURCES = "sources";
-
   public static final String SCOPE_COMPILE = "compile";
   public static final String SCOPE_PROVIDEED = "provided";
   public static final String SCOPE_RUNTIME = "runtime";
