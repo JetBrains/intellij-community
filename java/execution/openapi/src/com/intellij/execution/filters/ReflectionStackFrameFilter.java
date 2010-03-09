89073ac97ad45eab853b32fe9815fe5a0b8e0a2a
peter <peter.gromov@jetbrains.com>
2010-03-09 12:53:30 +0000

    Method.invoke & Constructor.newInstance considered auxiliary

--- java/execution/openapi/src/com/intellij/execution/filters/ReflectionStackFrameFilter.java
+++ java/execution/openapi/src/com/intellij/execution/filters/ReflectionStackFrameFilter.java
@@ -5,6 +5,13 @@ package com.intellij.execution.filters;
  */
 public class ReflectionStackFrameFilter extends StackFrameFilter {
   public boolean isAuxiliaryFrame(String className, String methodName) {
+    if (className.equals("java.lang.reflect.Method") && methodName.equals("invoke")) {
+      return true;
+    }
+    if (className.equals("java.lang.reflect.Constructor") && methodName.equals("newInstance")) {
+      return true;
+    }
+
     return className.startsWith("sun.reflect.");
   }
 }
