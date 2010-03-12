2c099c9e498dcba03bb0852adfd3930c33a38a1a
Dmitry Jemerov <yole@jetbrains.com>
2010-03-12 17:28:56 +0300

    indent options for Python (PY-619)

---
+++ python/src/com/jetbrains/python/formatter/PyIndentOptionsProvider.java
@@ -0,0 +1,36 @@
+package com.jetbrains.python.formatter;
+
+import com.intellij.application.options.IndentOptionsEditor;
+import com.intellij.application.options.SmartIndentOptionsEditor;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.codeStyle.CodeStyleSettings;
+import com.intellij.psi.codeStyle.FileTypeIndentOptionsProvider;
+import com.jetbrains.python.PythonFileType;
+
+/**
+ * @author yole
+ */
+public class PyIndentOptionsProvider implements FileTypeIndentOptionsProvider {
+  public CodeStyleSettings.IndentOptions createIndentOptions() {
+    final CodeStyleSettings.IndentOptions indentOptions = new CodeStyleSettings.IndentOptions();
+    indentOptions.INDENT_SIZE = 4;
+    return indentOptions;
+  }
+
+  public FileType getFileType() {
+    return PythonFileType.INSTANCE;
+  }
+
+  public IndentOptionsEditor createOptionsEditor() {
+    return new SmartIndentOptionsEditor();
+  }
+
+  public String getPreviewText() {
+    return "def foo():\n"+
+           "    print 'bar'";
+  }
+
+  public void prepareForReformat(final PsiFile psiFile) {
+  }
+}
