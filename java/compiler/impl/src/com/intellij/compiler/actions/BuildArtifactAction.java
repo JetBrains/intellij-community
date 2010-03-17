7a4e117fe5cd88f0df2b264b3a9e2a2005c8533d
nik <Nikolay.Chashnikov@jetbrains.com>
2010-03-17 14:42:09 +0300

    separate 'build artifact' menu items replaced by 'build artifacts' dialog

--- java/compiler/impl/src/com/intellij/compiler/actions/BuildArtifactAction.java
+++ java/compiler/impl/src/com/intellij/compiler/actions/BuildArtifactAction.java
@@ -17,54 +17,86 @@ package com.intellij.compiler.actions;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.compiler.CompilerManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.openapi.fileChooser.FileChooserFactory;
-import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
-import com.intellij.openapi.fileChooser.FileChooserDialog;
-import com.intellij.openapi.fileChooser.FileChooserDescriptor;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.application.WriteAction;
-import com.intellij.openapi.application.Result;
-import com.intellij.openapi.compiler.CompilerManager;
-import com.intellij.openapi.compiler.CompilerBundle;
 import com.intellij.packaging.artifacts.Artifact;
 import com.intellij.packaging.artifacts.ArtifactManager;
-import com.intellij.packaging.artifacts.ModifiableArtifactModel;
 import com.intellij.packaging.impl.compiler.ArtifactCompileScope;
+import com.intellij.packaging.impl.compiler.ArtifactsWorkspaceSettings;
+import com.intellij.packaging.impl.ui.ChooseArtifactsDialog;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author nik
  */
 public class BuildArtifactAction extends AnAction {
-  private final Project myProject;
-  private Artifact myArtifact;
+  public BuildArtifactAction() {
+    super("Build Artifact");
+  }
 
-  public BuildArtifactAction(Project project, Artifact artifact) {
-    super(artifact.getName(), "Build Artifact '" + artifact.getName() + "'", artifact.getArtifactType().getIcon());
-    myProject = project;
-    myArtifact = artifact;
+  @Override
+  public void update(AnActionEvent e) {
+    final Project project = e.getData(PlatformDataKeys.PROJECT);
+    final Presentation presentation = e.getPresentation();
+    presentation.setEnabled(false);
+    if (project == null) {
+      return;
+    }
+    final List<Artifact> artifacts = getArtifactWithOutputPaths(project);
+    if (artifacts.isEmpty()) {
+      return;
+    }
+    presentation.setEnabled(true);
+    if (artifacts.size() == 1) {
+      presentation.setText("Build '" + StringUtil.first(artifacts.get(0).getName(), 40, true) + "' artifact");
+    }
+    else {
+      presentation.setText("Build Artifacts...");
+    }
+  }
+
+  private static List<Artifact> getArtifactWithOutputPaths(Project project) {
+    final List<Artifact> result = new ArrayList<Artifact>();
+    for (Artifact artifact : ArtifactManager.getInstance(project).getSortedArtifacts()) {
+      if (!StringUtil.isEmpty(artifact.getOutputPath())) {
+        result.add(artifact);
+      }
+    }
+    return result;
   }
 
   public void actionPerformed(AnActionEvent e) {
-    final String outputPath = myArtifact.getOutputPath();
-    if (StringUtil.isEmpty(outputPath)) {
-      final FileChooserDescriptor descriptor = FileChooserDescriptorFactory.createSingleFolderDescriptor();
-      descriptor.setTitle(CompilerBundle.message("dialog.title.output.directory.for.artifact"));
-      descriptor.setDescription(CompilerBundle.message("chooser.description.select.output.directory.for.0.artifact", myArtifact.getName()));
-      final FileChooserDialog chooser = FileChooserFactory.getInstance().createFileChooser(descriptor, myProject);
-      final VirtualFile[] files = chooser.choose(null, myProject);
-      if (files.length != 1) return;
-      final ModifiableArtifactModel model = ArtifactManager.getInstance(myProject).createModifiableModel();
-      model.getOrCreateModifiableArtifact(myArtifact).setOutputPath(files[0].getPath());
-      new WriteAction() {
-        protected void run(final Result result) {
-          model.commit();
-        }
-      }.execute();
+    final Project project = e.getData(PlatformDataKeys.PROJECT);
+    if (project == null) return;
+
+    final List<Artifact> artifacts = getArtifactWithOutputPaths(project);
+    if (artifacts.isEmpty()) return;
+
+    if (artifacts.size() == 1) {
+      buildArtifacts(project, artifacts);
+      return;
     }
-    CompilerManager.getInstance(myProject).make(ArtifactCompileScope.createArtifactsScope(myProject, Collections.singletonList(myArtifact)), null);
+
+    final ChooseArtifactsDialog dialog = new ChooseArtifactsDialog(project, artifacts, "Choose Artifacts to Build", "Selected artifacts will be built with all dependencies");
+    final List<Artifact> initialSelection = ArtifactsWorkspaceSettings.getInstance(project).getArtifactsToBuild();
+    if (!initialSelection.isEmpty()) {
+      dialog.selectElements(initialSelection);
+    }
+    dialog.show();
+    
+    if (dialog.isOK()) {
+      final List<Artifact> selected = dialog.getChosenElements();
+      ArtifactsWorkspaceSettings.getInstance(project).setArtifactsToBuild(selected);
+      buildArtifacts(project, selected);
+    }
+  }
+
+  private static void buildArtifacts(Project project, final List<Artifact> artifacts) {
+    CompilerManager.getInstance(project).make(ArtifactCompileScope.createArtifactsScope(project, artifacts), null);
   }
 }
