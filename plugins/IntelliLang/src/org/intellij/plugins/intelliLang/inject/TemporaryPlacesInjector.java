e390b5bbc94d93ec709caf97b2230d54e194db0d
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2010-03-23 02:29:37 +0300

    no need to stop the world iff there is no manipulator, no manipulator = no injection

--- plugins/IntelliLang/src/org/intellij/plugins/intelliLang/inject/TemporaryPlacesInjector.java
+++ plugins/IntelliLang/src/org/intellij/plugins/intelliLang/inject/TemporaryPlacesInjector.java
@@ -44,9 +44,10 @@ public class TemporaryPlacesInjector implements MultiHostInjector {
 
   public void getLanguagesToInject(@NotNull final MultiHostRegistrar registrar, @NotNull final PsiElement context) {
     final PsiLanguageInjectionHost host = (PsiLanguageInjectionHost)context;
-    final PsiLanguageInjectionHost originalHost = PsiUtilBase.getOriginalElement(host, host.getClass());
     final ElementManipulator<PsiLanguageInjectionHost> manipulator = ElementManipulators.getManipulator(host);
-    LOG.assertTrue(manipulator != null, "No manipulator for " + host);
+    if (manipulator == null) return; // TODO: [gregory] there is no need to stop the world !
+    //LOG.assertTrue(manipulator != null, "No manipulator for " + host);
+    final PsiLanguageInjectionHost originalHost = PsiUtilBase.getOriginalElement(host, host.getClass());
     final List<TemporaryPlacesRegistry.TemporaryPlace> list = TemporaryPlacesRegistry.getInstance(context.getProject()).getTempInjectionsSafe(originalHost);
     for (final TemporaryPlacesRegistry.TemporaryPlace place : list) {
       LOG.assertTrue(place.language != null, "Language should not be NULL");
