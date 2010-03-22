acf87101296f6ef747f99034bac4b0f5171872e8
Bas Leijdekkers <leijdekkers@carp-technologies.nl>
2010-03-22 11:52:11 +0100

    IDEA-52958 added "Remove leading zero to make decimal" and "Convert octal literal to decimal literal" fixes

--- plugins/InspectionGadgets/src/com/siyeh/ig/numeric/OctalLiteralInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/numeric/OctalLiteralInspection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2007 Dave Griffith, Bas Leijdekkers
+ * Copyright 2003-2010 Dave Griffith, Bas Leijdekkers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,36 +15,50 @@
  */
 package com.siyeh.ig.numeric;
 
-import com.intellij.psi.PsiLiteralExpression;
-import com.intellij.psi.PsiType;
+import com.intellij.psi.*;
 import com.siyeh.InspectionGadgetsBundle;
 import com.siyeh.ig.BaseInspection;
 import com.siyeh.ig.BaseInspectionVisitor;
+import com.siyeh.ig.InspectionGadgetsFix;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
 public class OctalLiteralInspection extends BaseInspection {
 
+    @Override
     @NotNull
     public String getID() {
         return "OctalInteger";
     }
 
+    @Override
     @NotNull
     public String getDisplayName() {
         return InspectionGadgetsBundle.message("octal.literal.display.name");
     }
 
+    @Override
     @NotNull
     protected String buildErrorString(Object... infos) {
         return InspectionGadgetsBundle.message(
                 "octal.literal.problem.descriptor");
     }
 
+    @Override
     public boolean isEnabledByDefault() {
         return true;
     }
 
+    @NotNull
+    @Override
+    protected InspectionGadgetsFix[] buildFixes(Object... infos) {
+        return new InspectionGadgetsFix[] {
+                new ConvertOctalLiteralToDecimalFix(),
+                new RemoveLeadingZeroFix()
+        };
+    }
+
+    @Override
     public BaseInspectionVisitor buildVisitor() {
         return new OctalLiteralVisitor();
     }
