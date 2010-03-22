6a4d39fde0a032c15dbaa7c90d040652c55106e9
Dmitry Jemerov <yole@jetbrains.com>
2010-03-22 16:30:34 +0300

    19351 - ISE: DialogWrapper.ensureEventDispatchThread (http://ea.jetbrains.com/browser/ea_problems/19351)

--- python/src/com/jetbrains/python/sdk/PythonSdkType.java
+++ python/src/com/jetbrains/python/sdk/PythonSdkType.java
@@ -8,6 +8,7 @@ import com.intellij.ide.DataManager;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.application.PathManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.fileChooser.FileChooserDescriptor;
@@ -452,7 +453,7 @@ public class PythonSdkType extends SdkType {
   private boolean switchPathToInterpreter(Sdk currentSdk, String... variants) {
     final Project project = PlatformDataKeys.PROJECT.getData(DataManager.getInstance().getDataContext());
     File sdk_file = new File(currentSdk.getHomePath());
-    String sdk_name = currentSdk.getName();
+    final String sdk_name = currentSdk.getName();
     boolean success = false;
     for (String interpreter : variants) {
       File binary = interpreter.startsWith("/")? new File(interpreter) : new File(sdk_file, interpreter);
@@ -468,10 +469,14 @@ public class PythonSdkType extends SdkType {
       }
     }
     if (!success) {
-      Messages.showWarningDialog(project,
-        "Failed to convert Python SDK '" + sdk_name + "'\nplease delete and re-create it",
-        "Converting Python SDK"
-      );
+      ApplicationManager.getApplication().invokeLater(new Runnable() {
+        public void run() {
+          Messages.showWarningDialog(project,
+            "Failed to convert Python SDK '" + sdk_name + "'\nplease delete and re-create it",
+            "Converting Python SDK"
+          );
+        }
+      }, ModalityState.NON_MODAL);
     }
     return success;
   }
