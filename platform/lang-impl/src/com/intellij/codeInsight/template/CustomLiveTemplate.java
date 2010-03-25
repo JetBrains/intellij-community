b6ef049584695a1af0c93dca022ded0667b3517d
Eugene.Kudelevsky <ekudel@gmail.com>
2010-03-25 21:10:13 +0300

    getTitle()

--- platform/lang-impl/src/com/intellij/codeInsight/template/CustomLiveTemplate.java
+++ platform/lang-impl/src/com/intellij/codeInsight/template/CustomLiveTemplate.java
@@ -34,4 +34,7 @@ public interface CustomLiveTemplate {
   void expand(String key, @NotNull CustomTemplateCallback callback, @Nullable TemplateInvokationListener listener);
 
   void wrap(String selection, @NotNull CustomTemplateCallback callback, @Nullable TemplateInvokationListener listener);
+
+  @NotNull
+  String getTitle();
 }
