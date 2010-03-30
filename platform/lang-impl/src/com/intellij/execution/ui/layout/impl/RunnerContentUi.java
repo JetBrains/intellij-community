0dabd5c818017060edac0c8b644346a23cdb89e6
Anton Makeev <Anton.Makeev@jetbrains.com>
2010-03-30 18:04:47 +0400

    reverted an imcomplete fix (on behalf of jeka)

--- platform/lang-impl/src/com/intellij/execution/ui/layout/impl/RunnerContentUi.java
+++ platform/lang-impl/src/com/intellij/execution/ui/layout/impl/RunnerContentUi.java
@@ -326,7 +326,7 @@ public class RunnerContentUi implements ContentUI, Disposable, CellTransform.Fac
           grid.processRemoveFromUi();
           removeGridIfNeeded(grid);
         }
-        myLayoutSettings.clearStateFor(event.getContent());
+        //myLayoutSettings.clearStateFor(event.getContent());
         updateTabsUI(false);
       }
 
