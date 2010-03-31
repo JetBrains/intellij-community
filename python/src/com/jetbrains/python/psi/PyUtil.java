05261eaf71b3f392f0d0463822d7d4e9aa0ed60f
Dmitry Cheryasov <dmitry.cheryasov@jetbrains.com>
2010-03-31 07:39:53 +0300

    Consider __new__ a static method in new-style classes (PY-282).

--- python/src/com/jetbrains/python/psi/PyUtil.java
+++ python/src/com/jetbrains/python/psi/PyUtil.java
@@ -619,6 +619,9 @@ public class PyUtil {
     String deconame = getTheOnlyBuiltinDecorator(function);
     if (PyNames.CLASSMETHOD.equals(deconame)) flags.add(CLASSMETHOD);
     else if (PyNames.STATICMETHOD.equals(deconame)) flags.add(STATICMETHOD);
+    // implicit classmethod __new__
+    PyClass cls = function.getContainingClass();
+    if (cls != null && cls.isNewStyleClass() && PyNames.NEW.equals(function.getName())) flags.add(CLASSMETHOD);
     //
     if (! flags.contains(CLASSMETHOD) && ! flags.contains(STATICMETHOD)) { // not set by decos, look for reassignment
       String func_name = function.getName();
