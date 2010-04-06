e0a24645c1b8a94e081348c26b2d1ded1612a099
Denis Zhdanov <Denis.Zhdanov@jetbrains.com>
2010-04-06 12:55:34 +0400

    Typo is corrected

--- java/testFramework/src/com/intellij/testFramework/LightIdeaTestCase.java
+++ java/testFramework/src/com/intellij/testFramework/LightIdeaTestCase.java
@@ -26,7 +26,7 @@ import org.jetbrains.annotations.NonNls;
  * A testcase that provides IDEA application and project. Note both are reused for each test run in the session so
  * be careful to return all the modification made to application and project components (such as settings) after
  * test is finished so other test aren't affected. The project is initialized with single module that have single
- * content&amp;source entry. For your convinience the project may be equipped with some mock JDK so your tests may
+ * content&amp;source entry. For your convenience the project may be equipped with some mock JDK so your tests may
  * refer to external classes. In order to enable this feature you have to have a folder named "mockJDK" under
  * idea installation home that is used for test running. Place src.zip under that folder. We'd suggest this is real mock
  * so it contains classes that is really needed in order to speed up tests startup.
