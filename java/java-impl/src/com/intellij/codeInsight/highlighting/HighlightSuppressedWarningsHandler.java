e8a6f8c5de6647337b4a8c27fe60ff7ac45c8aed
Alexey Kudravtsev <cdr@intellij.com>
2010-04-09 15:37:44 +0400

    reverted: do not start modal progress from within readaction

--- java/java-impl/src/com/intellij/codeInsight/highlighting/HighlightSuppressedWarningsHandler.java
+++ java/java-impl/src/com/intellij/codeInsight/highlighting/HighlightSuppressedWarningsHandler.java
@@ -32,9 +32,6 @@ import com.intellij.codeInspection.ex.LocalInspectionToolWrapper;
 import com.intellij.codeInspection.reference.RefManagerImpl;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.progress.ProgressManager;
-import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.openapi.ui.popup.PopupStep;
@@ -107,7 +104,7 @@ public class HighlightSuppressedWarningsHandler extends HighlightUsagesHandlerBa
   public void computeUsages(List<PsiLiteralExpression> targets) {
     final Project project = myTarget.getProject();
     final PsiElement parent = myTarget.getParent().getParent();
-    final LocalInspectionsPass pass = new LocalInspectionsPass(myFile, myFile.getViewProvider().getDocument(), parent.getTextRange().getStartOffset(), parent.getTextRange().getEndOffset());
+    LocalInspectionsPass pass = new LocalInspectionsPass(myFile, myFile.getViewProvider().getDocument(), parent.getTextRange().getStartOffset(), parent.getTextRange().getEndOffset());
     final InspectionProfile inspectionProfile =
       InspectionProjectProfileManager.getInstance(project).getInspectionProfile();
     for (PsiLiteralExpression target : targets) {
@@ -120,22 +117,13 @@ public class HighlightSuppressedWarningsHandler extends HighlightUsagesHandlerBa
           final GlobalInspectionContextImpl context = managerEx.createNewGlobalContext(false);
           tool.initialize(context);
           ((RefManagerImpl)context.getRefManager()).inspectionReadActionStarted();
-          ProgressManager.getInstance().run(new Task.Modal(project, "Collecting information...", true) {
-            @Override
-            public void run(@NotNull ProgressIndicator indicator) {
-              pass.doInspectInBatch(managerEx, Collections.<InspectionProfileEntry>singletonList(tool), false);
+          pass.doInspectInBatch(managerEx, Collections.<InspectionProfileEntry>singletonList(tool), false);
+          for (HighlightInfo info : pass.getInfos()) {
+            final PsiElement element = CollectHighlightsUtil.findCommonParent(myFile, info.startOffset, info.endOffset);
+            if (element != null) {
+              addOccurrence(element);
             }
-
-            @Override
-            public void onSuccess() {
-              for (HighlightInfo info : pass.getInfos()) {
-                final PsiElement element = CollectHighlightsUtil.findCommonParent(myFile, info.startOffset, info.endOffset);
-                if (element != null) {
-                  addOccurrence(element);
-                }
-              }
-            }
-          });
+          }
         }
       }
     }
