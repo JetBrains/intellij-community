3b2c6350cd3b3e1778ceb02c0ba5534c32e0d8a5
peter <peter.gromov@jetbrains.com>
2010-04-11 21:57:40 +0400

    IDEA-24721 Compilation with @Immutable class fails

--- plugins/groovy/test/org/jetbrains/plugins/groovy/compiler/GeneratorTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/compiler/GeneratorTest.java
@@ -60,6 +60,10 @@ public void testArrayType1() throws Throwable { doTest(); }
   public void testToGenerate1() throws Throwable { doTest(); }
   public void testVararg1() throws Throwable { doTest(); }
   public void testInaccessibleConstructor() throws Throwable { doTest(); }
+  public void testImmutableAnno() throws Throwable {
+    myFixture.addClass("package groovy.lang; public @interface Immutable {}");
+    doTest();
+  }
 
   @Override
   protected void tuneFixture(JavaModuleFixtureBuilder moduleBuilder) {
