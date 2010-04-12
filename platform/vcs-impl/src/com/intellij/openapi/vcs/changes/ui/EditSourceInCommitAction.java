d743c2cd3b4b801185a5211571361cca1ef4ef46
Konstantin Bulenkov <kb@jetbrains.com>
2010-04-13 00:06:24 +0400

    make shorter. avoid explicit array creation

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/EditSourceInCommitAction.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/EditSourceInCommitAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2010 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,7 +41,7 @@ public class EditSourceInCommitAction extends AnAction {
     if (navigatableArray != null && navigatableArray.length > 0) {
       SwingUtilities.invokeLater(new Runnable() {
         public void run() {
-          OpenSourceUtil.navigate(navigatableArray, true);
+          OpenSourceUtil.navigate(navigatableArray);
         }
       });
       myDialogWrapper.doCancelAction();
