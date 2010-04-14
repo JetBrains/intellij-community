4b006cd928e825e58144b46d8569d4dab0f25784
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2010-04-14 16:20:58 +0400

    Get rid of copy-paste round 1

--- python/src/com/jetbrains/python/console/RunPythonConsoleAction.java
+++ python/src/com/jetbrains/python/console/RunPythonConsoleAction.java
@@ -31,52 +31,37 @@ public class RunPythonConsoleAction extends AnAction implements DumbAware {
     getTemplatePresentation().setIcon(IconLoader.getIcon("/com/jetbrains/python/python.png"));
   }
 
-  @Override
-  public void update(final AnActionEvent e) {
-    e.getPresentation().setVisible(false);
-    e.getPresentation().setEnabled(false);
-    final Project project = e.getData(LangDataKeys.PROJECT);
-    if (project != null){
-      for (Module module : ModuleManager.getInstance(project).getModules()) {
-        e.getPresentation().setVisible(true);
-        if (PythonSdkType.findPythonSdk(module) != null){
-          e.getPresentation().setEnabled(true);
-          break;
+    @Override
+    public void update(final AnActionEvent e) {
+      e.getPresentation().setVisible(false);
+      e.getPresentation().setEnabled(false);
+      final Project project = e.getData(LangDataKeys.PROJECT);
+      if (project != null){
+        for (Module module : ModuleManager.getInstance(project).getModules()) {
+          e.getPresentation().setVisible(true);
+          if (PythonSdkType.findPythonSdk(module) != null){
+            e.getPresentation().setEnabled(true);
+            break;
+          }
         }
       }
     }
-  }
 
-  public void actionPerformed(final AnActionEvent e) {
-    final Project project = e.getData(LangDataKeys.PROJECT);
-    assert project != null : "Project is null";
-    Sdk sdk = null;
-    Module module = null;
-    for (Module m : ModuleManager.getInstance(project).getModules()) {
-      module = m;
-      sdk = PythonSdkType.findPythonSdk(module);
-      if (sdk != null){
-        break;
+    public void actionPerformed(final AnActionEvent e) {
+      final Project project = e.getData(LangDataKeys.PROJECT);
+      assert project != null : "Project is null";
+      Sdk sdk = null;
+      Module module = null;
+      for (Module m : ModuleManager.getInstance(project).getModules()) {
+        module = m;
+        sdk = PythonSdkType.findPythonSdk(module);
+        if (sdk != null){
+          break;
+        }
       }
+      assert module != null : "Module is null";
+      assert sdk != null : "Sdk is null";
+
+      PydevConsoleRunner.run(project, module, sdk);
     }
-    assert module != null : "Module is null";
-    assert sdk != null : "Sdk is null";
-    final String homePath = sdk.getHomePath();
-
-    PyConsoleRunner.run(project,
-                        PyBundle.message("python.console"),
-                        new CommandLineArgumentsProvider() {
-                          public String[] getArguments() {
-                            return new String[]{homePath, "-i"};
-                          }
-
-                          public boolean passParentEnvs() {
-                            return false;
-                          }
-
-                          public Map<String, String> getAdditionalEnvs() {
-                            return Collections.emptyMap();
-                          }
-                        }, DjangoUtil.getProjectRoot(module));
-  }
 }
