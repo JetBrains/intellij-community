7231b7aa19cdf2854039289c4dc51939713883dc
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2010-04-14 17:30:44 +0400

    PY-830 Do not highlight strings in python consoles as doc-comments

--- python/src/com/jetbrains/python/validation/DocStringAnnotator.java
+++ python/src/com/jetbrains/python/validation/DocStringAnnotator.java
@@ -3,6 +3,7 @@ package com.jetbrains.python.validation;
 import com.intellij.lang.annotation.Annotation;
 import com.jetbrains.python.PyHighlighter;
 import com.jetbrains.python.PythonDocStringFinder;
+import com.jetbrains.python.console.PydevConsoleRunner;
 import com.jetbrains.python.psi.PyClass;
 import com.jetbrains.python.psi.PyFile;
 import com.jetbrains.python.psi.PyFunction;
@@ -14,22 +15,25 @@ import com.jetbrains.python.psi.PyStringLiteralExpression;
 public class DocStringAnnotator extends PyAnnotator {
 
   @Override
-  public void visitPyFile(PyFile node) {
+  public void visitPyFile(final PyFile node) {
     annotateDocStringStmt(PythonDocStringFinder.find(node));
   }
 
   @Override
-  public void visitPyFunction(PyFunction node) {
+  public void visitPyFunction(final PyFunction node) {
     annotateDocStringStmt(PythonDocStringFinder.find(node.getStatementList()));
   }
 
   @Override
-  public void visitPyClass(PyClass node) {
+  public void visitPyClass(final PyClass node) {
     annotateDocStringStmt(PythonDocStringFinder.find(node.getStatementList()));
   }
 
-  private void annotateDocStringStmt(PyStringLiteralExpression stmt) {
+  private void annotateDocStringStmt(final PyStringLiteralExpression stmt) {
     if (stmt != null) {
+      if (PydevConsoleRunner.isInPydevConsole(stmt)){
+        return;
+      }
       Annotation ann = getHolder().createInfoAnnotation(stmt, null);
       ann.setTextAttributes(PyHighlighter.PY_DOC_COMMENT);
     }
