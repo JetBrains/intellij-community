e1bd744c95265fab5f983c88fb81ce578dea92c6
Dmitry Jemerov <yole@jetbrains.com>
2010-04-15 20:01:42 +0400

    usage type for untyped usages

--- python/src/com/jetbrains/python/findUsages/PyUsageTypeProvider.java
+++ python/src/com/jetbrains/python/findUsages/PyUsageTypeProvider.java
@@ -4,20 +4,25 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.usages.impl.rules.UsageType;
 import com.intellij.usages.impl.rules.UsageTypeProvider;
-import com.jetbrains.python.psi.PyElement;
-import com.jetbrains.python.psi.PyFromImportStatement;
-import com.jetbrains.python.psi.PyImportStatement;
+import com.jetbrains.python.psi.*;
 
 /**
  * @author yole
  */
 public class PyUsageTypeProvider implements UsageTypeProvider {
   private static final UsageType IN_IMPORT = new UsageType("Usage in import statement");
+  private static final UsageType UNTYPED = new UsageType("Untyped (probable) usage");
 
   public UsageType getUsageType(PsiElement element) {
     if (element instanceof PyElement) {
       if (PsiTreeUtil.getParentOfType(element, PyImportStatement.class, PyFromImportStatement.class) != null) {
         return IN_IMPORT;
+      }
+      if (element instanceof PyQualifiedExpression) {
+        final PyExpression qualifier = ((PyQualifiedExpression)element).getQualifier();
+        if (qualifier != null && qualifier.getType() == null) {
+          return UNTYPED;
+        }
       }
     }
     return null;
