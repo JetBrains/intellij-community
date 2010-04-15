749869ad94de11f5c5a35e54241ea4d86f74ee6f
anna <anna.kozlova@jetbrains.com>
2010-04-15 16:21:41 +0400

    type migration: boxing during array wrapping should be performed (IDEA-53591)

--- plugins/typeMigration/src/com/intellij/refactoring/typeMigration/rules/ElementToArrayConversionRule.java
+++ plugins/typeMigration/src/com/intellij/refactoring/typeMigration/rules/ElementToArrayConversionRule.java
@@ -5,6 +5,7 @@
 package com.intellij.refactoring.typeMigration.rules;
 
 import com.intellij.psi.*;
+import com.intellij.psi.util.PsiUtil;
 import com.intellij.psi.util.TypeConversionUtil;
 import com.intellij.refactoring.typeMigration.TypeConversionDescriptor;
 import com.intellij.refactoring.typeMigration.TypeMigrationLabeler;
@@ -13,9 +14,20 @@ public class ElementToArrayConversionRule extends TypeConversionRule{
   public TypeConversionDescriptor findConversion(final PsiType from, final PsiType to, final PsiMember member, final PsiExpression context,
                                                  final TypeMigrationLabeler labeler) {
     if (member == null && to instanceof PsiArrayType && TypeConversionUtil.isAssignable(((PsiArrayType)to).getComponentType(), from)) {
+      TypeConversionDescriptor wrapDescription =
+        new TypeConversionDescriptor("$qualifier$", "new " + from.getCanonicalText() + "[]{$qualifier$}", context);
+      if (((PsiArrayType)to).getComponentType() instanceof PsiClassType && from instanceof PsiPrimitiveType) {
+        final String boxedTypeName = ((PsiPrimitiveType)from).getBoxedTypeName();
+        final String normalizedArrayInitializer =
+          PsiUtil.getLanguageLevel(context).hasEnumKeywordAndAutoboxing() ? "$qualifier$" : boxedTypeName + ".valueOf($qualifier$)";
+        wrapDescription = new TypeConversionDescriptor("$qualifier$", "new " + boxedTypeName + "[]{" + normalizedArrayInitializer + "}", context);
+      }
       final PsiElement parent = context.getParent();
       if ((context instanceof PsiLiteralExpression || context instanceof PsiReferenceExpression) && parent instanceof PsiStatement) {
-        return new TypeConversionDescriptor("$qualifier$", "new " + from.getCanonicalText() + "[]{$qualifier$}", context);
+        return wrapDescription;
+      }
+      if (parent instanceof PsiAssignmentExpression && ((PsiAssignmentExpression)parent).getRExpression() == context) {
+        return wrapDescription;
       }
     }
     return null;
