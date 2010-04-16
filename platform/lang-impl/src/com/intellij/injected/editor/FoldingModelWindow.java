6e67f2398c6fe22ffda19f697bc269294f415a59
Alexey Kudravtsev <cdr@intellij.com>
2010-04-16 12:42:32 +0400

    initial folding in injected

--- platform/lang-impl/src/com/intellij/injected/editor/FoldingModelWindow.java
+++ platform/lang-impl/src/com/intellij/injected/editor/FoldingModelWindow.java
@@ -17,12 +17,16 @@
 package com.intellij.injected.editor;
 
 import com.intellij.openapi.editor.FoldRegion;
-import com.intellij.openapi.editor.markup.TextAttributes;
+import com.intellij.openapi.editor.FoldingGroup;
 import com.intellij.openapi.editor.ex.FoldingModelEx;
 import com.intellij.openapi.editor.impl.FoldRegionImpl;
+import com.intellij.openapi.editor.markup.TextAttributes;
+import com.intellij.openapi.util.Key;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
+import java.util.*;
+import java.util.List;
 
 /**
  * @author cdr
@@ -30,10 +34,12 @@ import java.awt.*;
 public class FoldingModelWindow implements FoldingModelEx{
   private final FoldingModelEx myDelegate;
   private final DocumentWindow myDocumentWindow;
+  private final EditorWindow myEditorWindow;
 
-  public FoldingModelWindow(FoldingModelEx delegate, DocumentWindow documentWindow) {
+  public FoldingModelWindow(@NotNull FoldingModelEx delegate, @NotNull DocumentWindow documentWindow, @NotNull EditorWindow editorWindow) {
     myDelegate = delegate;
     myDocumentWindow = documentWindow;
+    myEditorWindow = editorWindow;
   }
 
   public void setFoldingEnabled(boolean isEnabled) {
@@ -49,7 +55,15 @@ public class FoldingModelWindow implements FoldingModelEx{
   }
 
   public FoldRegion[] getAllFoldRegionsIncludingInvalid() {
-    return myDelegate.getAllFoldRegionsIncludingInvalid();
+    FoldRegion[] all = myDelegate.getAllFoldRegionsIncludingInvalid();
+    List<FoldRegion> result = new ArrayList<FoldRegion>();
+    for (FoldRegion region : all) {
+      FoldingRegionWindow window = region.getUserData(FOLD_REGION_WINDOW);
+      if (window != null && window.getEditor() == myEditorWindow) {
+        result.add(window);
+      }
+    }
+    return result.toArray(new FoldRegion[result.size()]);
   }
 
   public boolean intersectsRegion(int startOffset, int endOffset) {
@@ -59,22 +73,29 @@ public class FoldingModelWindow implements FoldingModelEx{
   }
 
   public FoldRegion addFoldRegion(int startOffset, int endOffset, @NotNull String placeholderText) {
-    return myDelegate.addFoldRegion(myDocumentWindow.injectedToHost(startOffset), myDocumentWindow.injectedToHost(endOffset), placeholderText);
+    FoldRegion region = createFoldRegion(startOffset, endOffset, placeholderText, null);
+    return addFoldRegion(region) ? region : null;
   }
 
   public boolean addFoldRegion(@NotNull final FoldRegion region) {
-    return myDelegate.addFoldRegion(new FoldRegionImpl(region.getEditor(), myDocumentWindow.injectedToHost(region.getStartOffset()),
-                                                       myDocumentWindow.injectedToHost(region.getEndOffset()), region.getPlaceholderText(),
-                                                       region.getGroup()));
+    return myDelegate.addFoldRegion(((FoldingRegionWindow)region).getDelegate());
   }
 
   public void removeFoldRegion(@NotNull FoldRegion region) {
-    myDelegate.removeFoldRegion(region);
+    myDelegate.removeFoldRegion(((FoldingRegionWindow)region).getDelegate());
   }
 
   @NotNull
   public FoldRegion[] getAllFoldRegions() {
-    return myDelegate.getAllFoldRegions();
+    FoldRegion[] all = myDelegate.getAllFoldRegions();
+    List<FoldRegion> result = new ArrayList<FoldRegion>();
+    for (FoldRegion region : all) {
+      FoldingRegionWindow window = region.getUserData(FOLD_REGION_WINDOW);
+      if (window != null && window.getEditor() == myEditorWindow) {
+        result.add(window);
+      }
+    }
+    return result.toArray(new FoldRegion[result.size()]);
   }
 
   public boolean isOffsetCollapsed(int offset) {
@@ -82,7 +103,8 @@ public class FoldingModelWindow implements FoldingModelEx{
   }
 
   public FoldRegion getCollapsedRegionAtOffset(int offset) {
-    return myDelegate.getCollapsedRegionAtOffset(myDocumentWindow.injectedToHost(offset));
+    FoldRegion host = myDelegate.getCollapsedRegionAtOffset(myDocumentWindow.injectedToHost(offset));
+    return host; //todo convert to window?
   }
 
   public void runBatchFoldingOperation(@NotNull Runnable operation) {
@@ -94,11 +116,12 @@ public class FoldingModelWindow implements FoldingModelEx{
   }
 
   public FoldRegion fetchOutermost(int offset) {
-    return null;
+    FoldRegion host = myDelegate.fetchOutermost(myDocumentWindow.injectedToHost(offset));
+    return host; //todo convert to window?
   }
 
   public int getLastCollapsedRegionBefore(int offset) {
-    return -1;
+    return -1; //todo implement
   }
 
   public TextAttributes getPlaceholderAttributes() {
@@ -106,6 +129,14 @@ public class FoldingModelWindow implements FoldingModelEx{
   }
 
   public FoldRegion[] fetchTopLevel() {
-    return FoldRegion.EMPTY_ARRAY;
+    return FoldRegion.EMPTY_ARRAY; //todo implement
+  }
+
+  private static final Key<FoldingRegionWindow> FOLD_REGION_WINDOW = Key.create("FOLD_REGION_WINDOW");
+  public FoldRegion createFoldRegion(int startOffset, int endOffset, @NotNull String placeholder, FoldingGroup group) {
+    FoldRegion hostRegion = myDelegate.createFoldRegion(myDocumentWindow.injectedToHost(startOffset), myDocumentWindow.injectedToHost(endOffset), placeholder, group);
+    FoldingRegionWindow window = new FoldingRegionWindow(myDocumentWindow, myEditorWindow, (FoldRegionImpl)hostRegion);
+    hostRegion.putUserData(FOLD_REGION_WINDOW, window);
+    return window;
   }
 }
