e25faeb08d442dff42bdbcf019ba493b51302a88
alexey.ivanov <alexey.ivanov@jetbrains.com>
2010-04-22 21:39:58 +0400

    test fix

--- python/testData/override/simple_after.py
+++ python/testData/override/simple_after.py
@@ -2,8 +2,8 @@ class A:
     def doStuff(self): pass
 
 class B(A):
-    def otherMethod(self, foo, bar):
-        print foo, bar
-
     def doStuff(self):
         <selection>pass</selection>
+
+    def otherMethod(self, foo, bar):
+        print foo, bar
