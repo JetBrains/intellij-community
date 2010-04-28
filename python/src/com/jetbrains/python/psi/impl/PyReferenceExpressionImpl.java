750df6de4b56430b0606335a9bc294b1440f2592
Dmitry Jemerov <yole@jetbrains.com>
2010-04-28 15:41:51 +0400

    correct handling of aug assignments when inferring types

--- python/src/com/jetbrains/python/psi/impl/PyReferenceExpressionImpl.java
+++ python/src/com/jetbrains/python/psi/impl/PyReferenceExpressionImpl.java
@@ -238,7 +238,9 @@ public class PyReferenceExpressionImpl extends PyElementImpl implements PyRefere
     if (target instanceof PyTargetExpression && context.allowDataFlow() && anchor != null) {
       final ScopeOwner scopeOwner = PsiTreeUtil.getParentOfType(anchor, ScopeOwner.class);
       if (scopeOwner != null && scopeOwner == PsiTreeUtil.getParentOfType(target, ScopeOwner.class)) {
-        final PyElement[] defs = PyDefUseUtil.getLatestDefs(scopeOwner, (PyTargetExpression) target, anchor);
+        PyAugAssignmentStatement augAssignment = PsiTreeUtil.getParentOfType(anchor, PyAugAssignmentStatement.class);
+        final PyElement[] defs = PyDefUseUtil.getLatestDefs(scopeOwner, (PyTargetExpression) target, 
+                                                            augAssignment != null ? augAssignment : anchor);
         if (defs.length > 0) {
           PyType type = getTypeIfExpr(defs [0], context);
           for (int i = 1; i < defs.length; i++) {
