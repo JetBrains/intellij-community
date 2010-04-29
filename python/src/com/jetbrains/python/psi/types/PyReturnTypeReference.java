7ededbc2ae2eec4a480fc623cfd38c2319d1f177
Dmitry Jemerov <yole@jetbrains.com>
2010-04-29 16:12:02 +0400

    @Nullable

--- python/src/com/jetbrains/python/psi/types/PyReturnTypeReference.java
+++ python/src/com/jetbrains/python/psi/types/PyReturnTypeReference.java
@@ -2,17 +2,20 @@ package com.jetbrains.python.psi.types;
 
 import com.intellij.psi.PsiElement;
 import com.jetbrains.python.psi.PyFunction;
+import com.jetbrains.python.psi.resolve.PyResolveContext;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * @author yole
  */
 public class PyReturnTypeReference extends PyTypeReferenceImpl {
-  private PyFunction myFunction;
+  private final PyFunction myFunction;
 
   public PyReturnTypeReference(PyFunction function) {
     myFunction = function;
   }
 
+  @Nullable
   public PyType resolve(PsiElement context) {
     return myFunction.getReturnType();
   }
