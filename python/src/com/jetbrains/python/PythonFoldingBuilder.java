e8e84f7132fe194716e82966f934eb4a6aa8e32a
Dmitry Jemerov <yole@jetbrains.com>
2010-04-30 21:33:57 +0400

    Python imports folding (PY-928)

--- python/src/com/jetbrains/python/PythonFoldingBuilder.java
+++ python/src/com/jetbrains/python/PythonFoldingBuilder.java
@@ -1,12 +1,15 @@
 package com.jetbrains.python;
 
+import com.intellij.codeInsight.folding.CodeFoldingSettings;
 import com.intellij.lang.ASTNode;
 import com.intellij.lang.folding.FoldingBuilder;
 import com.intellij.lang.folding.FoldingDescriptor;
 import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.TokenType;
 import com.intellij.psi.tree.IElementType;
+import com.jetbrains.python.psi.PyFileElementType;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -24,7 +27,28 @@ public class PythonFoldingBuilder implements FoldingBuilder, DumbAware {
   }
 
   private static void appendDescriptors(ASTNode node, List<FoldingDescriptor> descriptors) {
-    if (node.getElementType() == PyElementTypes.STATEMENT_LIST) {
+    if (node.getElementType() instanceof PyFileElementType) {
+      ASTNode firstImport = node.getFirstChildNode();
+      while(firstImport != null && !isImport(firstImport, false)) {
+        firstImport = firstImport.getTreeNext();
+      }
+      if (firstImport != null) {
+        ASTNode lastImport = firstImport.getTreeNext();
+        while(lastImport != null && isImport(lastImport.getTreeNext(), true)) {
+          lastImport = lastImport.getTreeNext();
+        }
+        if (lastImport != null) {
+          while (lastImport.getElementType() == TokenType.WHITE_SPACE) {
+            lastImport = lastImport.getTreePrev();
+          }
+          if (isImport(lastImport, false)) {
+            descriptors.add(new FoldingDescriptor(firstImport, new TextRange(firstImport.getStartOffset(),
+                                                                             lastImport.getTextRange().getEndOffset())));
+          }
+        }
+      }
+    }
+    else if (node.getElementType() == PyElementTypes.STATEMENT_LIST) {
       IElementType elType = node.getTreeParent().getElementType();
       if (elType == PyElementTypes.FUNCTION_DECLARATION || elType == PyElementTypes.CLASS_DECLARATION) {
         ASTNode colon = node.getTreeParent().findChildByType(PyTokenTypes.COLON);
@@ -48,11 +72,26 @@ public class PythonFoldingBuilder implements FoldingBuilder, DumbAware {
     }
   }
 
+  private static boolean isImport(ASTNode node, boolean orWhitespace) {
+    if (node == null) return false;
+    IElementType elementType = node.getElementType();
+    if (orWhitespace && elementType == TokenType.WHITE_SPACE) {
+      return true;
+    }
+    return elementType == PyElementTypes.IMPORT_STATEMENT || elementType == PyElementTypes.FROM_IMPORT_STATEMENT;
+  }
+
   public String getPlaceholderText(@NotNull ASTNode node) {
+    if (isImport(node, false)) {
+      return "import ...";
+    }
     return "...";
   }
 
   public boolean isCollapsedByDefault(@NotNull ASTNode node) {
+    if (isImport(node, false)) {
+      return CodeFoldingSettings.getInstance().COLLAPSE_IMPORTS;
+    }
     return false;
   }
 }
