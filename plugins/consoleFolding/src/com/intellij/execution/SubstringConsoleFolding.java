3711812f3debe946158b48b016e339c58d8c87f2
peter <peter.gromov@jetbrains.com>
2010-04-30 21:18:17 +0100

    ConsoleFolding plugin

---
+++ plugins/consoleFolding/src/com/intellij/execution/SubstringConsoleFolding.java
@@ -0,0 +1,24 @@
+package com.intellij.execution;
+
+import java.util.List;
+
+/**
+ * @author peter
+ */
+public class SubstringConsoleFolding extends ConsoleFolding {
+  private final ConsoleFoldingSettings mySettings;
+
+  public SubstringConsoleFolding(ConsoleFoldingSettings settings) {
+    mySettings = settings;
+  }
+
+  @Override
+  public boolean shouldFoldLine(String line) {
+    return mySettings.shouldFoldLine(line);
+  }
+
+  @Override
+  public String getPlaceholderText(List<String> lines) {
+    return " <" + lines.size() + " internal calls>";
+  }
+}
