1cd28a713437c115b68d49bca09d2958a366fac4
Denis Zhdanov <Denis.Zhdanov@jetbrains.com>
2010-04-30 19:40:36 +0400

    IDEA-52305 condensed code formatting for "flying geese" braces
    Test for 'flying geese' style has been removed because of requirements change

--- java/java-tests/testSrc/com/intellij/psi/formatter/java/JavaFormatterBracesTest.java
+++ java/java-tests/testSrc/com/intellij/psi/formatter/java/JavaFormatterBracesTest.java
@@ -15,8 +15,6 @@
  */
 package com.intellij.psi.formatter.java;
 
-import com.intellij.openapi.fileTypes.StdFileTypes;
-
 /**
  * Is intended to hold specific java formatting tests for 'braces placement' settings (
  * <code>Project Settings - Code Style - Alignment and Braces</code>).
@@ -25,98 +23,6 @@ import com.intellij.openapi.fileTypes.StdFileTypes;
  * @since Apr 27, 2010 6:39:24 PM
  */
 public class JavaFormatterBracesTest extends AbstractJavaFormatterTest {
-
-  public void testFlyingGeeseBraces() {
-    // Inspired by IDEA-52305
-
-    getSettings().USE_FLYING_GEESE_BRACES = true;
-    getSettings().FLYING_GEESE_BRACES_GAP = 1;
-    getSettings().BLANK_LINES_AROUND_METHOD = 0; // controls number of blank lines between instance initialization block and field. Very strange
-    getSettings().KEEP_SIMPLE_METHODS_IN_ONE_LINE = true; // allow methods like 'void foo() {}'
-    getSettings().getIndentOptions(StdFileTypes.JAVA).INDENT_SIZE = 2;
-
-    // Flying gees style for class initialization and instance initialization block when there are no fields/methods.
-    doTextTest(
-      "class FormattingTest {\n" +
-      "  {\n" +
-      "  } \n" +
-      "}",
-      "class FormattingTest { {\n" +
-      "} }"
-    );
-
-    // Flying gees style for class initialization and instance initialization block when there are fields after the block.
-    doTextTest(
-      "class FormattingTest {\n" +
-      "  {\n" +
-      "  } \n" +
-      "  int i;\n" +
-      "}",
-      "class FormattingTest {\n" +
-      "  {\n" +
-      "  } \n" +
-      "  int i;\n" +
-      "}"
-    );
-
-    // Flying gees style for class initialization and instance initialization block when there are fields before the block.
-    doTextTest(
-      "class FormattingTest {\n" +
-      "  int i;\n" +
-      "  {\n" +
-      "  } \n" +
-      "}",
-      "class FormattingTest {\n" +
-      "  int i;\n" +
-      "  {\n" +
-      "  } \n" +
-      "}"
-    );
-
-    // Flying gees style for class initialization and instance initialization block when there are methods after the block.
-    doTextTest(
-      "class FormattingTest {\n" +
-      "  {\n" +
-      "  } \n" +
-      "  void foo() {}\n" +
-      "}",
-      "class FormattingTest {\n" +
-      "  {\n" +
-      "  } \n" +
-      "  void foo() {}\n" +
-      "}"
-    );
-
-    // Flying gees style for class initialization and instance initialization block when there are methods before the block.
-    doTextTest(
-      "class FormattingTest {\n" +
-      "  void foo() {}\n" +
-      "  {\n" +
-      "  } \n" +
-      "}",
-      "class FormattingTest {\n" +
-      "  void foo() {}\n" +
-      "  {\n" +
-      "  } \n" +
-      "}"
-    );
-
-    // Flying gees style for class initialization and multiple instance initialization blocks.
-    doTextTest(
-      "class FormattingTest {\n" +
-      "  {\n" +
-      "  } \n" +
-      "  {\n" +
-      "  }\n" +
-      "}",
-      "class FormattingTest {\n" +
-      "  {\n" +
-      "  } \n" +
-      "  {\n" +
-      "  }\n" +
-      "}"
-    );
-  }
 
   public void testBracePositioningAtPreviousLine() throws Exception {
     // Inspired by IDEADEV-18529
