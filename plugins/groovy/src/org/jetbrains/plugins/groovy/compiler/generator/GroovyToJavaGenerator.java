80c482590407e918bca0389c41fec70f80530f92
peter <peter.gromov@jetbrains.com>
2010-05-03 16:18:38 +0100

    don't write modifiers to Groovy stubs that won't affect the compilation, or they would do it negatively

--- plugins/groovy/src/org/jetbrains/plugins/groovy/compiler/generator/GroovyToJavaGenerator.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/compiler/generator/GroovyToJavaGenerator.java
@@ -96,10 +96,6 @@ public class GroovyToJavaGenerator {
       PsiModifier.ABSTRACT,
       PsiModifier.FINAL,
       PsiModifier.NATIVE,
-      PsiModifier.SYNCHRONIZED,
-      PsiModifier.STRICTFP,
-      PsiModifier.TRANSIENT,
-      PsiModifier.VOLATILE
   };
 
   private static final CharSequence PREFIX_SEPARATOR = "/";
