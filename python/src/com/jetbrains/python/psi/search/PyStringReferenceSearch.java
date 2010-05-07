6c3e0ad7b2167cf8e9c62540d110a597f6a65f1c
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2010-05-07 22:24:24 +0400

    fixed django find usages and rename refactoring

---
+++ python/src/com/jetbrains/python/psi/search/PyStringReferenceSearch.java
@@ -0,0 +1,60 @@
+package com.jetbrains.python.psi.search;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.util.Computable;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.SearchScope;
+import com.intellij.psi.search.TextOccurenceProcessor;
+import com.intellij.psi.search.UsageSearchContext;
+import com.intellij.psi.search.searches.ReferencesSearch;
+import com.intellij.util.Processor;
+import com.intellij.util.QueryExecutor;
+import com.jetbrains.python.PythonFileType;
+import com.jetbrains.python.psi.PyElement;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author traff
+ */
+public class PyStringReferenceSearch implements QueryExecutor<PsiReference, ReferencesSearch.SearchParameters> {
+  public boolean execute(@NotNull final ReferencesSearch.SearchParameters params,
+                         @NotNull final Processor<PsiReference> consumer) {
+    final PsiElement element = params.getElementToSearch();
+    if (!(element instanceof PyElement)){
+      return true;
+    }
+    final String name = ApplicationManager.getApplication().runReadAction(new Computable<String>() {
+      public String compute() {
+        return ((PyElement)element).getName();
+      }
+    });
+    if (StringUtil.isEmpty(name)) {
+      return true;
+    }
+    SearchScope searchScope = params.getEffectiveSearchScope();
+    if (searchScope instanceof GlobalSearchScope) {
+      searchScope = GlobalSearchScope.getScopeRestrictedByFileTypes((GlobalSearchScope)searchScope, PythonFileType.INSTANCE);
+    }
+    
+    final TextOccurenceProcessor processor = new TextOccurenceProcessor() {
+      public boolean execute(PsiElement e, int offsetInElement) {
+        final PsiReference[] refs = e.getReferences();
+        for (PsiReference ref : refs) {
+          if (ref.getRangeInElement().contains(offsetInElement)) {
+            if (ref.isReferenceTo(element)) {
+              return consumer.process(ref);
+            }
+          }
+        }
+        return true;
+      }
+    };
+
+    return PsiManager.getInstance(element.getProject()).getSearchHelper().
+      processElementsWithWord(processor, searchScope, name, UsageSearchContext.IN_STRINGS, true);
+  }
+}
