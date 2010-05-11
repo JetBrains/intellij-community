1cd46240832bfedb71859372307d343fd413b474
Denis Zhdanov <Denis.Zhdanov@jetbrains.com>
2010-05-11 15:57:23 +0400

    IDEA-54765 Code Style -> Order of Members should have extra items for statics
    Test data is corrected in order to follow new static vs instance methods ordering policy

--- java/java-tests/testData/refactoring/inlineSuperClass/staticMembers/after/Test.java
+++ java/java-tests/testData/refactoring/inlineSuperClass/staticMembers/after/Test.java
@@ -1,12 +1,12 @@
 class Test {
     public static final String S = "";
 
+    static void foo(){
+       System.out.println(S);
+    }
+
     void bar() {
     System.out.println(Test.S);
     Test.foo();
   }
-
-    static void foo(){
-       System.out.println(S);
-    }
 }
