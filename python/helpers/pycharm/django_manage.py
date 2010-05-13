708dfd33333896d07b09edf5295cdeb4b2a022ee
Dmitry Jemerov <yole@jetbrains.com>
2010-05-13 15:40:40 +0400

    workaround to ensure correct getpass() operation in Django manage.py (PY-386)

--- python/helpers/pycharm/django_manage.py
+++ python/helpers/pycharm/django_manage.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
+import sys
 from django.core.management import execute_manager
 from pycharm import django_settings
+
 if __name__ == "__main__":
+    if sys.platform == 'win32':
+        import getpass
+        getpass.getpass = getpass.fallback_getpass
     execute_manager(django_settings)
