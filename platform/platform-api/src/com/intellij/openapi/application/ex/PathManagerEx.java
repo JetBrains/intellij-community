779ce66b079167d8dd5cb4f839981b8bcffb89ea
Denis Zhdanov <Denis.Zhdanov@jetbrains.com>
2010-05-14 16:58:42 +0400

    IDEA-54295 Unify test data path access for all IDEA tests
    Corrected algorithm of test data lookup strategy guessing

--- platform/platform-api/src/com/intellij/openapi/application/ex/PathManagerEx.java
+++ platform/platform-api/src/com/intellij/openapi/application/ex/PathManagerEx.java
@@ -25,14 +25,49 @@
 package com.intellij.openapi.application.ex;
 
 import com.intellij.openapi.application.PathManager;
+import com.intellij.testFramework.TestRunnerUtil;
+import com.intellij.util.containers.ConcurrentHashMap;
+import com.intellij.util.containers.ConcurrentHashSet;
+import junit.framework.TestCase;
 import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.util.*;
+import java.util.concurrent.ConcurrentMap;
 
 import static com.intellij.openapi.util.io.FileUtil.toSystemDependentName;
+import static java.util.Arrays.asList;
 
 public class PathManagerEx {
+
+  /**
+   * All IDEA project files may be logically divided by the following criteria:
+   * <ul>
+   *   <li>files that are contained at <code>'community'</code> directory;</li>
+   *   <li>all other files;</li>
+   * </ul>
+   * <p/>
+   * File location types implied by criteria mentioned above are enumerated here.
+   */
+  private enum FileSystemLocation {
+    ULTIMATE, COMMUNITY
+  }
+
+  /** Caches test data lookup strategy by class. */
+  private static final ConcurrentMap<Class, TestDataLookupStrategy> CLASS_STRATEGY_CACHE
+    = new ConcurrentHashMap<Class, TestDataLookupStrategy>();
+
+  /**
+   * Caches processed files.
+   * <p/>
+   * There is a possible case that particular file is processed but its test data lookup strategy is still not defined.
+   * <p/>
+   * We don't want to repeat the processing then during further requests for the same file, hence, we rememver paths of all
+   * processed files here.
+   */
+  private static final Set<String> PROCESSED_FILES = new ConcurrentHashSet<String>();
+
   private PathManagerEx() {
   }
 
@@ -83,17 +118,13 @@ public class PathManagerEx {
     = new EnumMap<TestDataLookupStrategy, List<String>>(TestDataLookupStrategy.class);
 
   static {
-    TEST_DATA_RELATIVE_PATHS.put(TestDataLookupStrategy.ULTIMATE, Collections.singletonList(toSystemDependentName("/testData")));
-
-    List<String> communityPathEndings = new ArrayList<String>();
-    String communityEnding = toSystemDependentName("java/java-tests/testData");
-    communityPathEndings.add(toSystemDependentName("community/" + communityEnding));
-    communityPathEndings.add(communityEnding);
-    TEST_DATA_RELATIVE_PATHS.put(TestDataLookupStrategy.COMMUNITY, communityPathEndings);
-
-    List<String> communityFromUltimatePathEndings = new ArrayList<String>(communityPathEndings);
-    Collections.reverse(communityFromUltimatePathEndings);
-    TEST_DATA_RELATIVE_PATHS.put(TestDataLookupStrategy.COMMUNITY_FROM_ULTIMATE, communityFromUltimatePathEndings);
+    TEST_DATA_RELATIVE_PATHS.put(TestDataLookupStrategy.ULTIMATE, Collections.singletonList(toSystemDependentName("testData")));
+    TEST_DATA_RELATIVE_PATHS.put(
+      TestDataLookupStrategy.COMMUNITY, Collections.singletonList(toSystemDependentName("java/java-tests/testData"))
+    );
+    TEST_DATA_RELATIVE_PATHS.put(
+      TestDataLookupStrategy.COMMUNITY_FROM_ULTIMATE, Collections.singletonList(toSystemDependentName("community/java/java-tests/testData"))
+    );
   }
 
   /**
@@ -149,6 +180,126 @@ public class PathManagerEx {
    * @return    guessed lookup strategy for the current execution; defaults to {@link TestDataLookupStrategy#ULTIMATE}
    */
   public static TestDataLookupStrategy guessTestDataLookupStrategy() {
+    TestDataLookupStrategy result = guessTestDataLookupStrategyOnClassLocation();
+    if (result == null) {
+      result = guessTestDataLookupStrategyOnDirectoryAvailability();
+    }
+    return result;
+  }
+
+  @SuppressWarnings({"ThrowableInstanceNeverThrown"})
+  @Nullable
+  private static TestDataLookupStrategy guessTestDataLookupStrategyOnClassLocation() {
+
+    // The general idea here is to find test class at the bottom of hierarchy and try to resolve test data lookup strategy
+    // against it. Rationale is that there is a possible case that, say, 'ultimate' test class extends basic test class
+    // that remains at 'community'. We want to perform the processing against 'ultimate' test class then.
+
+    Class<?> testClass = null;
+    for (StackTraceElement stackTraceElement : new Exception().getStackTrace()) {
+      Class<?> clazz = loadClass(stackTraceElement.getClassName());
+      if (isJUnitClass(clazz)) {
+        testClass = clazz;
+        continue;
+      }
+
+      // We consider that the latest test class that is not preceded by thread frame that contains test class
+      // as well is our target test class.
+      if (testClass != null) {
+        break;
+      }
+    }
+
+    return testClass == null ? null : determineLookupStrategy(testClass);
+  }
+
+  private static Class<?> loadClass(String className) {
+    Class<?> clazz = null;
+    ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+    ClassLoader definingClassLoader = PathManagerEx.class.getClassLoader();
+    ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
+
+    List<ClassLoader> classLoaders = asList(contextClassLoader, definingClassLoader, systemClassLoader);
+    for (ClassLoader classLoader : classLoaders) {
+      clazz = loadClass(className, classLoader);
+      if (clazz != null) {
+        break;
+      }
+    }
+
+    if (clazz == null) {
+      throw new IllegalStateException(String.format("Can't load class '%s'. Tried to do that via thread context class loader(%s), "
+                                                    + "defining class loader(%s) and system class loader(%s)",
+                                                    className, contextClassLoader, definingClassLoader, systemClassLoader));
+    }
+    return clazz;
+  }
+
+  @Nullable
+  private static Class<?> loadClass(String className, ClassLoader classLoader) {
+    try {
+      return Class.forName(className, true, classLoader);
+    }
+    catch (ClassNotFoundException e) {
+      return null;
+    }
+  }
+
+  private static boolean isJUnitClass(Class<?> clazz) {
+    return TestCase.class.isAssignableFrom(clazz) || TestRunnerUtil.isJUnit4TestClass(clazz);
+  }
+
+  @Nullable
+  public static TestDataLookupStrategy determineLookupStrategy(Class<?> clazz) {
+    FileSystemLocation projectLocation = parseProjectLocation();
+    if (projectLocation == FileSystemLocation.COMMUNITY) {
+      // There is no other options then.
+      return TestDataLookupStrategy.COMMUNITY;
+    }
+
+    // Check if resulting strategy is already cached for the target class.
+    TestDataLookupStrategy result = CLASS_STRATEGY_CACHE.get(clazz);
+    if (result != null) {
+      return result;
+    }
+
+    String targetFile = toSystemDependentName(clazz.getName().replace('.', '/') + ".java");
+    if (PROCESSED_FILES.contains(targetFile)) {
+      return null;
+    }
+
+    FileSystemLocation classFileLocation = new FileSystemLocationResolver(targetFile).resolve();
+    PROCESSED_FILES.add(targetFile);
+
+    if (classFileLocation == null) {
+      return null;
+    }
+
+    // We know that project location is ULTIMATE if control flow reaches this place.
+    result = classFileLocation == FileSystemLocation.COMMUNITY ? TestDataLookupStrategy.COMMUNITY_FROM_ULTIMATE
+                                                               : TestDataLookupStrategy.ULTIMATE;
+    CLASS_STRATEGY_CACHE.put(clazz, result);
+    return result;
+  }
+
+  /**
+   * Allows to determine project type by its file system location.
+   *
+   * @return    project type implied by its file system location
+   */
+  private static FileSystemLocation parseProjectLocation() {
+    return new File(PathManager.getHomePath(), "community").isDirectory() ? FileSystemLocation.ULTIMATE : FileSystemLocation.COMMUNITY;
+  }
+
+  /**
+   * Tries to check test data lookup strategy by target test data directories availability.
+   * <p/>
+   * Such an approach has a drawback that it doesn't work correctly at number of scenarios, e.g. when
+   * <code>'community'</code> test is executed under <code>'ultimate'</code> project.
+   *
+   * @return    test data lookup strategy based on target test data directories availability
+   */
+  private static TestDataLookupStrategy guessTestDataLookupStrategyOnDirectoryAvailability() {
     String homePath = PathManager.getHomePath();
     for (Map.Entry<TestDataLookupStrategy, List<String>> entry : TEST_DATA_RELATIVE_PATHS.entrySet()) {
       for (String relativePath : entry.getValue()) {
@@ -158,5 +309,54 @@ public class PathManagerEx {
       }
     }
     return TestDataLookupStrategy.ULTIMATE;
+  }
+
+  /**
+   * Allows to resolve {@link FileSystemLocation file type} by it's file system location.
+   */
+  private static class FileSystemLocationResolver {
+
+    private final String rootPath = PathManager.getHomePath();
+    private final String myTargetFile;
+    private FileSystemLocation myResult;
+
+    FileSystemLocationResolver(String targetFile) {
+      myTargetFile = targetFile;
+    }
+
+    /**
+     * @return    resolved file system location type for the target file defined at constructor if possible; <code>null</code> otherwise
+     */
+    @Nullable
+    public FileSystemLocation resolve() {
+      myResult = null;
+      dispatch(new File(rootPath));
+      return myResult;
+    }
+
+    public void dispatch(File dir) {
+      if (myResult != null) {
+        return;
+      }
+      for (File file : dir.listFiles()) {
+        if (myResult != null) {
+          return;
+        }
+        if (file.isDirectory()) {
+          dispatch(file);
+        }
+        else {
+          check(file);
+        }
+      }
+    }
+
+    public void check(File file) {
+      String path = file.getAbsolutePath();
+      if (path.endsWith(myTargetFile)) {
+        String targetPathSegment = path.substring(PathManager.getHomePath().length() + 1);
+        myResult = targetPathSegment.startsWith("community") ? FileSystemLocation.COMMUNITY : FileSystemLocation.ULTIMATE;
+      }
+    }
   }
 }
