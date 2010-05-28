7f5c1b0a8dd37a25a4e55a18fe34c69b0489abb7
Constantine Plotnikov <Constantine.Plotnikov@jetbrains.com>
2010-05-28 17:01:18 +0400

    password safe: Added diagnosic information reporting in the case of failed test

--- platform/platform-impl/testSrc/com/intellij/ide/passwordSafe/impl/providers/memory/MemoryPasswordSafeTest.java
+++ platform/platform-impl/testSrc/com/intellij/ide/passwordSafe/impl/providers/memory/MemoryPasswordSafeTest.java
@@ -15,8 +15,14 @@
  */
 package com.intellij.ide.passwordSafe.impl.providers.memory;
 
+import com.intellij.util.PathUtil;
 import org.junit.Assert;
 import org.junit.Test;
+
+import javax.crypto.KeyGenerator;
+import java.security.Provider;
+import java.security.Security;
+import java.util.Map;
 
 /**
  * The test for memory implementation of password safe
@@ -30,11 +36,35 @@ public class MemoryPasswordSafeTest {
    */
   @Test
   public void testMemory() throws Exception {
-    MemoryPasswordSafe p = new MemoryPasswordSafe();
-    Assert.assertNull(p.getPassword(null, MemoryPasswordSafeTest.class, "test"));
-    p.storePassword(null, MemoryPasswordSafeTest.class, "test", "TEST PASSWORD");
-    Assert.assertEquals("TEST PASSWORD", p.getPassword(null, MemoryPasswordSafeTest.class, "test"));
-    p.removePassword(null, MemoryPasswordSafeTest.class, "test");
-    Assert.assertNull(p.getPassword(null, MemoryPasswordSafeTest.class, "test"));
+    try {
+      MemoryPasswordSafe p = new MemoryPasswordSafe();
+      Assert.assertNull(p.getPassword(null, MemoryPasswordSafeTest.class, "test"));
+      p.storePassword(null, MemoryPasswordSafeTest.class, "test", "TEST PASSWORD");
+      Assert.assertEquals("TEST PASSWORD", p.getPassword(null, MemoryPasswordSafeTest.class, "test"));
+      p.removePassword(null, MemoryPasswordSafeTest.class, "test");
+      Assert.assertNull(p.getPassword(null, MemoryPasswordSafeTest.class, "test"));
+    } catch(Throwable t) {
+      throw new RuntimeException("Test failed: "+diagnosticInfo(), t);
+    }
+  }
+
+  /**
+   * @return diagnostic information for the JCE
+   */
+  public static String diagnosticInfo() {
+    StringBuilder b = new StringBuilder();
+    b.append("Security Diagnostic Info:");
+    b.append("\n java.home = ").append(System.getProperty("java.home"));
+    b.append("\n java.vm.version = ").append(System.getProperty("java.vm.version"));
+    b.append("\n java.class.path = ").append(System.getProperty("java.class.path"));
+    b.append("\n sun.boot.class.path = ").append(System.getProperty("sun.boot.class.path"));
+    b.append("\n java.ext.dirs = ").append(System.getProperty("java.ext.dirs"));
+    b.append("\n java.endorsed.dirs = ").append(System.getProperty("java.endorsed.dirs"));
+    b.append("\n Providers:");
+    for(Provider p : Security.getProviders()) {
+      b.append("\n  ").append(p.toString()).append(" info ").append(p.getInfo()).append(" class ").append(p.getClass().getName())
+        .append(" location ").append(PathUtil.getJarPathForClass(p.getClass()));
+    }
+    return b.toString();
   }
 }
