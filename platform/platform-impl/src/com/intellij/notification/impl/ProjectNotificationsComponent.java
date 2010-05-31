04a8009406f95e8b8837134f1da59a217a9b9ba3
Alexey Pegov <alexey.pegov@jetbrains.com>
2010-05-31 17:08:52 +0400

    fix notifications memleak

--- platform/platform-impl/src/com/intellij/notification/impl/ProjectNotificationsComponent.java
+++ platform/platform-impl/src/com/intellij/notification/impl/ProjectNotificationsComponent.java
@@ -22,9 +22,12 @@ import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.popup.Balloon;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.Disposable;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
 
 /**
  * @author spleaner
@@ -58,6 +61,11 @@ public class ProjectNotificationsComponent implements Notifications, ProjectComp
   }
 
   public void projectClosed() {
+    final Collection<Notification> collection = NotificationsManagerImpl.getNotificationsManagerImpl().getByType(null, myProject);
+    for (final Notification notification : collection) {
+      final Balloon balloon = notification.getBalloon();
+      if (balloon != null) balloon.hide();
+    }
   }
 
   private static boolean isDummyEnvironment() {
