bb3ad1f718f3a82dffaa3ab9498d684cbff639cc
Anton Makeev <Anton.Makeev@jetbrains.com>
2010-06-10 11:57:31 +0400

    Storage: rebuild on error + reclaiming data storage in linked and ref-counting storages

--- platform/util/src/com/intellij/util/io/storage/AbstractRecordsTable.java
+++ platform/util/src/com/intellij/util/io/storage/AbstractRecordsTable.java
@@ -34,7 +34,7 @@ public abstract class AbstractRecordsTable implements Disposable, Forceable {
   private static final int HEADER_VERSION_OFFSET = 4;
   protected static final int DEFAULT_HEADER_SIZE = 8;
 
-  private static final int VERSION = 5;
+  private static final int VERSION = 6;
   private static final int CONNECTED_MAGIC = 0x12ad34e4;
   private static final int SAFELY_CLOSED_MAGIC = 0x1f2f3f4f + VERSION;
 
