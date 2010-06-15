c00981f544b3d34942f1d9c7aaa5b7be45867a4e
peter <peter.gromov@jetbrains.com>
2010-06-15 23:44:03 +0400

    fix deadlock: don't cache GrVariable's enhanced types in CachedValue

--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/GrVariableEnhancer.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/GrVariableEnhancer.java
@@ -1,12 +1,7 @@
 package org.jetbrains.plugins.groovy.lang.psi;
 
 import com.intellij.openapi.extensions.ExtensionPointName;
-import com.intellij.openapi.util.Key;
 import com.intellij.psi.PsiType;
-import com.intellij.psi.util.CachedValue;
-import com.intellij.psi.util.CachedValueProvider;
-import com.intellij.psi.util.CachedValuesManager;
-import com.intellij.psi.util.PsiModificationTracker;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrVariable;
 
@@ -15,24 +10,20 @@ import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrVariable;
  */
 public abstract class GrVariableEnhancer {
   public static final ExtensionPointName<GrVariableEnhancer> EP_NAME = ExtensionPointName.create("org.intellij.groovy.variableEnhancer");
-  private static final Key<CachedValue<PsiType>> ENHANCED_TYPE_KEY = Key.create("GroovyEnhancedType");
 
   @Nullable
   public abstract PsiType getVariableType(GrVariable variable);
 
+  @Nullable
   public static PsiType getEnhancedType(final GrVariable variable) {
-    return CachedValuesManager.getManager(variable.getProject()).getCachedValue(variable, ENHANCED_TYPE_KEY, new CachedValueProvider<PsiType>() {
-          public Result<PsiType> compute() {
-            for (GrVariableEnhancer enhancer : GrVariableEnhancer.EP_NAME.getExtensions()) {
-              final PsiType type = enhancer.getVariableType(variable);
-              if (type != null) {
-                return Result.create(type, PsiModificationTracker.OUT_OF_CODE_BLOCK_MODIFICATION_COUNT);
-              }
-            }
+    for (GrVariableEnhancer enhancer : GrVariableEnhancer.EP_NAME.getExtensions()) {
+      final PsiType type = enhancer.getVariableType(variable);
+      if (type != null) {
+        return type;
+      }
+    }
 
-            return Result.create(null, PsiModificationTracker.OUT_OF_CODE_BLOCK_MODIFICATION_COUNT);
-          }
-        }, false);
+    return null;
   }
 
 }
