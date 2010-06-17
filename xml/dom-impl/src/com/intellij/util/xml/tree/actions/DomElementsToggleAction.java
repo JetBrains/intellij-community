fecc1a37423911c54e2d944058a22a4b15a4585c
peter <peter.gromov@jetbrains.com>
2010-06-18 01:51:03 +0400

    bring back the wrongly deleted classes

---
+++ xml/dom-impl/src/com/intellij/util/xml/tree/actions/DomElementsToggleAction.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2000-2009 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intellij.util.xml.tree.actions;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.ToggleAction;
+import com.intellij.openapi.util.IconLoader;
+import com.intellij.util.xml.ElementPresentationManager;
+import com.intellij.util.xml.TypeNameManager;
+import com.intellij.util.xml.DomUtil;
+import com.intellij.util.xml.tree.BaseDomElementNode;
+import com.intellij.util.xml.tree.DomModelTreeView;
+
+import javax.swing.*;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * User: Sergey.Vasiliev
+ */
+public class DomElementsToggleAction extends ToggleAction {
+  private final DomModelTreeView myTreeView;
+  private final Class myClass;
+  private final Icon myIcon;
+  private final String myText;
+
+
+  public DomElementsToggleAction(final DomModelTreeView treeView, final Class aClass) {
+    myTreeView = treeView;
+    myClass = aClass;
+
+    Icon myIcon = ElementPresentationManager.getIcon(myClass);
+    if (myIcon == null) {
+      myIcon = IconLoader.getIcon("/nodes/pointcut.png");
+    }
+    this.myIcon = myIcon;
+    myText = TypeNameManager.getTypeName(myClass);
+
+    if(getHiders() == null) DomUtil.getFile(myTreeView.getRootElement()).putUserData(BaseDomElementNode.TREE_NODES_HIDERS_KEY, new HashMap<Class, Boolean>());
+
+    if(getHiders().get(myClass) == null) getHiders().put(myClass, true);
+  }
+
+  public void update(final AnActionEvent e) {
+    super.update(e);
+
+    e.getPresentation().setIcon(myIcon);
+    e.getPresentation().setText((getHiders().get(myClass) ? "Hide ":"Show ")+myText);
+
+    e.getPresentation().setEnabled(getHiders() != null && getHiders().get(myClass)!=null);
+  }
+
+  public boolean isSelected(AnActionEvent e) {
+    return getHiders().get(myClass);
+  }
+
+  private Map<Class, Boolean> getHiders() {
+    return DomUtil.getFile(myTreeView.getRootElement()).getUserData(BaseDomElementNode.TREE_NODES_HIDERS_KEY);
+  }
+
+  public void setSelected(AnActionEvent e, boolean state) {
+    getHiders().put(myClass, state);
+    myTreeView.getBuilder().updateFromRoot();
+  }
+}
+
