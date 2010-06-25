211159524e55aebf5201924849e0458f68e38642
peter <peter.gromov@jetbrains.com>
2010-06-26 00:55:12 +0400

    NonCodeMembersContributor: invoked less often than NonCodeMembersProcessor => PROFIT

---
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/annotator/intentions/dynamic/DynamicMembersContributor.java
@@ -0,0 +1,31 @@
+package org.jetbrains.plugins.groovy.annotator.intentions.dynamic;
+
+import com.intellij.psi.*;
+import com.intellij.psi.scope.PsiScopeProcessor;
+import com.intellij.util.ProcessingContext;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.lang.resolve.NonCodeMembersContributor;
+import org.jetbrains.plugins.groovy.lang.resolve.ResolveUtil;
+
+/**
+ * @author peter
+ */
+public class DynamicMembersContributor extends NonCodeMembersContributor {
+  @Override
+  public void processDynamicElements(@NotNull PsiType qualifierType,
+                                     PsiScopeProcessor processor,
+                                     PsiElement place,
+                                     ResolveState state,
+                                     ProcessingContext ctx) {
+    final DynamicManager manager = DynamicManager.getInstance(place.getProject());
+    for (String qName : ResolveUtil.getAllSuperTypes(qualifierType, place, ctx).keySet()) {
+      for (PsiMethod method : manager.getMethods(qName)) {
+        if (!ResolveUtil.processElement(processor, method)) return;
+      }
+
+      for (PsiVariable var : manager.getProperties(qName)) {
+        if (!ResolveUtil.processElement(processor, var)) return;
+      }
+    }
+  }
+}
