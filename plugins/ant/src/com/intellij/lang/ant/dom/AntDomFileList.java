6faa0055b18c306482bdb3dfdb0217675a972557
Eugene Zhuravlev <jeka@intellij.com>
2010-06-28 21:44:21 +0400

    1. path-like structures support 2. documentation provider for ant's dom

--- plugins/ant/src/com/intellij/lang/ant/dom/AntDomFileList.java
+++ plugins/ant/src/com/intellij/lang/ant/dom/AntDomFileList.java
@@ -16,9 +16,17 @@
 package com.intellij.lang.ant.dom;
 
 import com.intellij.lang.ant.psi.AntFilesProvider;
+import com.intellij.psi.PsiFileSystemItem;
+import com.intellij.util.text.StringTokenizer;
+import com.intellij.util.xml.Attribute;
+import com.intellij.util.xml.Convert;
+import com.intellij.util.xml.GenericAttributeValue;
+import com.intellij.util.xml.SubTagList;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
@@ -28,8 +36,46 @@ import java.util.Set;
  *         Date: Jun 22, 2010
  */
 public abstract class AntDomFileList extends AntDomFilesProviderImpl{
+
+  @Attribute("dir")
+  @Convert(value = AntPathConverter.class)
+  public abstract GenericAttributeValue<PsiFileSystemItem> getDir();
+
+  @Attribute("files")
+  public abstract GenericAttributeValue<String> getFilesString();
+
+  @SubTagList("file")
+  public abstract List<AntDomNamedElement> getFiles(); // todo: add filename completion relative to the filelist's basedir
+
+  
+  @Nullable
+  protected AntDomPattern getAntPattern() {
+    return null; // not available for this data type
+  }
+
   @NotNull
-  public List<File> getFiles(Set<AntFilesProvider> processed) {
-    return Collections.emptyList(); // todo
+  protected List<File> getFiles(@Nullable AntDomPattern pattern, Set<AntFilesProvider> processed) {
+    final File root = getCanonicalFile(getDir().getStringValue());
+    if (root == null) {
+      return Collections.emptyList();
+    }
+
+    final ArrayList<File> files = new ArrayList<File>();
+
+    final String filenames = getFilesString().getStringValue();
+    if (filenames != null) {
+      final StringTokenizer tokenizer = new StringTokenizer(filenames, ", \t\n\r\f", false);
+      while (tokenizer.hasMoreTokens()) {
+        files.add(new File(root, tokenizer.nextToken()));
+      }
+    }
+
+    for (AntDomNamedElement child : getFiles()) {
+      final String fileName = child.getName().getStringValue();
+      if (fileName != null) {
+        files.add(new File(root, fileName));
+      }
+    }
+    return files;
   }
 }
