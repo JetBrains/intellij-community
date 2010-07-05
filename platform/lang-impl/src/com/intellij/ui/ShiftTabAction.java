901137d5e8a1b2b1e24b1847fd4d3236f75175cc
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2010-07-05 22:23:49 +0400

    moved LanguageTextField to lang-impl

--- platform/lang-impl/src/com/intellij/ui/ShiftTabAction.java
+++ platform/lang-impl/src/com/intellij/ui/ShiftTabAction.java
@@ -13,12 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.intellij.plugins.intelliLang.util;
+package com.intellij.ui;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CustomShortcutSet;
-import com.intellij.ui.EditorTextField;
 
 import javax.swing.*;
 import java.awt.*;
