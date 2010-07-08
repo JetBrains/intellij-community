767f63299d67a3a67a7cf5b27722e72d4eaf0570
peter <peter.gromov@jetbrains.com>
2010-07-08 15:19:55 +0100

    don't let light tests spoil language level for their comrades

--- plugins/typeMigration/test/com/intellij/codeInsight/ConvertToAtomicIntentionTest.java
+++ plugins/typeMigration/test/com/intellij/codeInsight/ConvertToAtomicIntentionTest.java
@@ -8,23 +8,8 @@ import com.intellij.codeInsight.daemon.quickFix.LightQuickFixTestCase;
 import com.intellij.openapi.application.PathManager;
 import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.projectRoots.impl.JavaSdkImpl;
-import com.intellij.openapi.roots.LanguageLevelProjectExtension;
-import com.intellij.pom.java.LanguageLevel;
 
 public class ConvertToAtomicIntentionTest extends LightQuickFixTestCase {
-  private LanguageLevel myPrevLanguageLevel;
-
-  protected void setUp() throws Exception {
-    super.setUp();
-
-    myPrevLanguageLevel = LanguageLevelProjectExtension.getInstance(getJavaFacade().getProject()).getLanguageLevel();
-    LanguageLevelProjectExtension.getInstance(getJavaFacade().getProject()).setLanguageLevel(LanguageLevel.JDK_1_5);
-  }
-
-  protected void tearDown() throws Exception {
-    LanguageLevelProjectExtension.getInstance(getJavaFacade().getProject()).setLanguageLevel(myPrevLanguageLevel);
-    super.tearDown();
-  }
 
   @Override
   protected boolean shouldBeAvailableAfterExecution() {
