fd93083bde15351926f84055d129416180d08df4
Dmitry Jemerov <yole@jetbrains.com>
2010-07-08 19:28:30 +0400

    fix bug in building class hierarchy structure (PY-236)

--- python/src/com/jetbrains/python/hierarchy/treestructures/PyTypeHierarchyTreeStructure.java
+++ python/src/com/jetbrains/python/hierarchy/treestructures/PyTypeHierarchyTreeStructure.java
@@ -8,10 +8,7 @@ import com.jetbrains.python.psi.PyUtil;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Created by IntelliJ IDEA.
- * User: Alexey.Ivanov
- * Date: Jul 31, 2009
- * Time: 6:29:00 PM
+ * @author Alexey.Ivanov
  */
 public class PyTypeHierarchyTreeStructure extends PySubTypesHierarchyTreeStructure {
   private static PyTypeHierarchyNodeDescriptor buildHierarchyElement(@NotNull final PyClass cl) {
@@ -21,7 +18,7 @@ public class PyTypeHierarchyTreeStructure extends PySubTypesHierarchyTreeStructu
       final PyClass superClass = superClasses[i];
       final PyTypeHierarchyNodeDescriptor newDescriptor = new PyTypeHierarchyNodeDescriptor(descriptor, superClass, false);
       if (descriptor != null) {
-        newDescriptor.setCachedChildren(new PyTypeHierarchyNodeDescriptor[]{newDescriptor});
+        descriptor.setCachedChildren(new PyTypeHierarchyNodeDescriptor[]{newDescriptor});
       }
       descriptor = newDescriptor;
     }
