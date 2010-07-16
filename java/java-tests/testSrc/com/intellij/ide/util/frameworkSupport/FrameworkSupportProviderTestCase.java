5a0bb8fef59f91c1b2dc1be1a92201ab798ef38e
nik <Nikolay.Chashnikov@jetbrains.com>
2010-07-16 13:16:42 +0400

    test fixed

--- java/java-tests/testSrc/com/intellij/ide/util/frameworkSupport/FrameworkSupportProviderTestCase.java
+++ java/java-tests/testSrc/com/intellij/ide/util/frameworkSupport/FrameworkSupportProviderTestCase.java
@@ -4,6 +4,7 @@ import com.intellij.facet.Facet;
 import com.intellij.facet.FacetManager;
 import com.intellij.facet.FacetTypeId;
 import com.intellij.facet.ui.FacetBasedFrameworkSupportProvider;
+import com.intellij.ide.util.newProjectWizard.FrameworkSupportNode;
 import com.intellij.ide.util.newProjectWizard.impl.FrameworkSupportCommunicator;
 import com.intellij.ide.util.newProjectWizard.impl.FrameworkSupportModelImpl;
 import com.intellij.openapi.roots.ModifiableRootModel;
@@ -21,32 +22,43 @@ import java.util.*;
  * @author nik
  */
 public abstract class FrameworkSupportProviderTestCase extends IdeaTestCase {
-  private FrameworkSupportModel myFrameworkSupportModel;
+  private FrameworkSupportModelImpl myFrameworkSupportModel;
   private Map<FrameworkSupportProvider, FrameworkSupportConfigurable> myConfigurables;
-  private Set<FrameworkSupportProvider> mySelected;
+  private Map<FrameworkSupportProvider, FrameworkSupportNode> myNodes;
 
   @Override
   protected void setUp() throws Exception {
     super.setUp();
     myFrameworkSupportModel = new FrameworkSupportModelImpl(getProject(), null);
+    myNodes = new HashMap<FrameworkSupportProvider, FrameworkSupportNode>();
+    for (FrameworkSupportProvider provider : FrameworkSupportProvider.EXTENSION_POINT.getExtensions()) {
+      final FrameworkSupportNode node = new FrameworkSupportNode(provider, null, myFrameworkSupportModel, null, getTestRootDisposable());
+      myNodes.put(provider, node);
+      myFrameworkSupportModel.registerComponent(provider, node);
+    }
     myConfigurables = new HashMap<FrameworkSupportProvider, FrameworkSupportConfigurable>();
-    mySelected = new HashSet<FrameworkSupportProvider>();
   }
 
   protected void addSupport() throws IOException {
     final VirtualFile root = getVirtualFile(createTempDir("contentRoot"));
     PsiTestUtil.addContentRoot(myModule, root);
     final ModifiableRootModel model = ModuleRootManager.getInstance(myModule).getModifiableModel();
-    List<FrameworkSupportConfigurable> selectedConfigurables = new ArrayList<FrameworkSupportConfigurable>();
-    for (FrameworkSupportProvider provider : mySelected) {
-      final FrameworkSupportConfigurable configurable = myConfigurables.get(provider);
-      configurable.addSupport(myModule, model, null);
-      selectedConfigurables.add(configurable);
+    try {
+      List<FrameworkSupportConfigurable> selectedConfigurables = new ArrayList<FrameworkSupportConfigurable>();
+      for (FrameworkSupportNode node : myNodes.values()) {
+        if (node.isChecked()) {
+          final FrameworkSupportConfigurable configurable = getOrCreateConfigurable(node.getProvider());
+          configurable.addSupport(myModule, model, null);
+          selectedConfigurables.add(configurable);
+        }
+      }
+      for (FrameworkSupportCommunicator communicator : FrameworkSupportCommunicator.EP_NAME.getExtensions()) {
+        communicator.onFrameworkSupportAdded(myModule, model, selectedConfigurables, myFrameworkSupportModel);
+      }
     }
-    for (FrameworkSupportCommunicator communicator : FrameworkSupportCommunicator.EP_NAME.getExtensions()) {
-      communicator.onFrameworkSupportAdded(myModule, model, selectedConfigurables, myFrameworkSupportModel);
+    finally {
+      model.commit();
     }
-    model.commit();
     for (FrameworkSupportConfigurable configurable : myConfigurables.values()) {
       Disposer.dispose(configurable);
     }
@@ -68,8 +80,8 @@ public abstract class FrameworkSupportProviderTestCase extends IdeaTestCase {
 
   protected FrameworkSupportConfigurable selectFramework(@NotNull FrameworkSupportProvider provider) {
     final FrameworkSupportConfigurable configurable = getOrCreateConfigurable(provider);
+    myNodes.get(provider).setChecked(true);
     configurable.onFrameworkSelectionChanged(true);
-    mySelected.add(provider);
     return configurable;
   }
 
