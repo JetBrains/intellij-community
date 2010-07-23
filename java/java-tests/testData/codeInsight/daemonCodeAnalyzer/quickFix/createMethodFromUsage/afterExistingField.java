5c430dd73a96af726e651b0a7f4c323fabe65967
anna <Anna.Kozlova@jetbrains.com>
2010-07-23 20:49:21 +0400

    create getter from usage: return type fixed when field with name already exist

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/createMethodFromUsage/afterExistingField.java
@@ -0,0 +1,11 @@
+// "Create Getter" "true"
+public class Test {
+    Integer field;
+    public foo() {
+        getField();
+    }
+
+    public Integer getField() {
+        return field;
+    }
+}
