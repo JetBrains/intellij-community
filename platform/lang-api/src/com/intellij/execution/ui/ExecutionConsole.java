c1dbbf34e8eb82fcc5fc87f3f2447a9466f6a43a
nik <Nikolay.Chashnikov@jetbrains.com>
2010-07-29 16:11:14 +0400

    IDEA-56925: Remote debugging broken (linux, tomcat)

--- platform/lang-api/src/com/intellij/execution/ui/ExecutionConsole.java
+++ platform/lang-api/src/com/intellij/execution/ui/ExecutionConsole.java
@@ -16,13 +16,12 @@
 package com.intellij.execution.ui;
 
 import com.intellij.openapi.ui.ComponentContainer;
-
-import javax.swing.*;
+import org.jetbrains.annotations.NonNls;
 
 /**
  * @author Eugene Zhuravlev
  *         Date: Apr 20, 2004
  */
 public interface ExecutionConsole extends ComponentContainer {
-
+  @NonNls String CONSOLE_CONTENT_ID = "ConsoleContent";
 }
