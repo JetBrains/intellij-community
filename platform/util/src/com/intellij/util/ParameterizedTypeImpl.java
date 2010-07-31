f01f8ff1a972fbd30cedf829bbac087ca6606309
peter <peter.gromov@jetbrains.com>
2010-07-31 19:43:58 +0100

    yet another test fix

--- platform/util/src/com/intellij/util/ParameterizedTypeImpl.java
+++ platform/util/src/com/intellij/util/ParameterizedTypeImpl.java
@@ -2,6 +2,7 @@ package com.intellij.util;
 
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.Arrays;
 
 /**
  * @author peter
@@ -28,5 +29,25 @@ public class ParameterizedTypeImpl implements ParameterizedType {
   @Override
   public Type getOwnerType() {
     return null;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof ParameterizedTypeImpl)) return false;
+
+    ParameterizedTypeImpl that = (ParameterizedTypeImpl)o;
+
+    if (!Arrays.equals(myArguments, that.myArguments)) return false;
+    if (!myRawType.equals(that.myRawType)) return false;
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = myRawType.hashCode();
+    result = 31 * result + Arrays.hashCode(myArguments);
+    return result;
   }
 }
