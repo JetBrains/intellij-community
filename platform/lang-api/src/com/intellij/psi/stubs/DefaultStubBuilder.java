9d5f3cb532247e26d9d1d102c981082cd5634419
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2010-08-01 18:02:14 +0400

    trying to decrease method appettite for locals to avoid SOE

--- platform/lang-api/src/com/intellij/psi/stubs/DefaultStubBuilder.java
+++ platform/lang-api/src/com/intellij/psi/stubs/DefaultStubBuilder.java
@@ -21,55 +21,51 @@ package com.intellij.psi.stubs;
 
 import com.intellij.lang.ASTNode;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.StubBasedPsiElement;
-import com.intellij.psi.StubBuilder;
+import com.intellij.psi.*;
 import com.intellij.psi.tree.IElementType;
 
 public class DefaultStubBuilder implements StubBuilder {
   private static final Logger LOG = Logger.getInstance("#com.intellij.psi.stubs.DefaultStubBuilder");
 
   public StubElement buildStubTree(final PsiFile file) {
-    return buildStubTreeFor(file, createStubForFile(file), this);
+    return buildStubTreeFor(file, createStubForFile(file));
   }
 
   protected StubElement createStubForFile(final PsiFile file) {
     return new PsiFileStubImpl(file);
   }
 
-  protected static StubElement buildStubTreeFor(PsiElement elt, StubElement parentStub, StubBuilder filter) {
-    StubElement stub = parentStub;
+  // Beware many recursive invokations of this method, the code below reuse locals and parameters to avoid SOE
+  protected StubElement buildStubTreeFor(PsiElement elt, StubElement parentStub) {
+    ASTNode node;
     IElementType eltType;
     if (elt instanceof StubBasedPsiElement) {
-      final IStubElementType type = ((StubBasedPsiElement)elt).getElementType();
+      eltType = ((StubBasedPsiElement)elt).getElementType();
 
-      if (type.shouldCreateStub(elt.getNode())) {
+      if (((IStubElementType)eltType).shouldCreateStub(elt.getNode())) {
         //noinspection unchecked
-        stub = type.createStub(elt, parentStub);
+        parentStub = ((IStubElementType)eltType).createStub(elt, parentStub);
       }
-      eltType = type;
     }
     else {
-      final ASTNode node = elt.getNode();
-      final IElementType type = node == null? null : node.getElementType();
-      if (type instanceof IStubElementType && ((IStubElementType)type).shouldCreateStub(node)) {
-        LOG.error("Non-StubBasedPsiElement requests stub creation. Stub type: " + type + ", PSI: " + elt);
-      }
-      eltType = type;
-    }
-
-    for (PsiElement child = elt.getFirstChild(); child != null; child = child.getNextSibling()) {
-      ASTNode childNode = child.getNode();
-      if (!filter.skipChildProcessingWhenBuildingStubs(eltType, childNode != null ? childNode.getElementType():null)) {
-        buildStubTreeFor(child, stub, filter);
+      node = elt.getNode();
+      eltType = node == null? null : node.getElementType();
+      if (eltType instanceof IStubElementType && ((IStubElementType)eltType).shouldCreateStub(node)) {
+        LOG.error("Non-StubBasedPsiElement requests stub creation. Stub type: " + eltType + ", PSI: " + elt);
       }
     }
 
-    return stub;
+    for (elt = elt.getFirstChild(); elt != null; elt = elt.getNextSibling()) {
+      node = elt.getNode();
+      if (!skipChildProcessingWhenBuildingStubs(eltType, node != null ? node.getElementType():null)) {
+        buildStubTreeFor(elt, parentStub);
+      }
+    }
+
+    return parentStub;
   }
 
-  protected static StubElement buildStubTreeFor(ASTNode node, StubElement parentStub, StubBuilder filter) {
+  protected StubElement buildStubTreeFor(ASTNode node, StubElement parentStub) {
     StubElement stub = parentStub;
     IElementType nodeType = node.getElementType();
 
@@ -87,8 +83,8 @@ public class DefaultStubBuilder implements StubBuilder {
     }
 
     for (ASTNode childNode = node.getFirstChildNode(); childNode != null; childNode = childNode.getTreeNext()) {
-      if (!filter.skipChildProcessingWhenBuildingStubs(nodeType, childNode.getElementType())) {
-        buildStubTreeFor(childNode, stub, filter);
+      if (!skipChildProcessingWhenBuildingStubs(nodeType, childNode.getElementType())) {
+        buildStubTreeFor(childNode, stub);
       }
     }
 
