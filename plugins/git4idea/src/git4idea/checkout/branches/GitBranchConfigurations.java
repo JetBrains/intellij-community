4565e2a473933e52f2273cf759617f1a8cefb3fa
Constantine Plotnikov <Constantine.Plotnikov@jetbrains.com>
2010-08-03 14:36:55 +0400

    git4idea: IDEA-57066: fixed non-tag commit dectection

--- plugins/git4idea/src/git4idea/checkout/branches/GitBranchConfigurations.java
+++ plugins/git4idea/src/git4idea/checkout/branches/GitBranchConfigurations.java
@@ -586,9 +586,9 @@ public class GitBranchConfigurations implements PersistentStateComponent<GitBran
       if (LOG.isDebugEnabled()) {
         LOG.debug("describe HEAD failed for root: " + root.getPath());
       }
-      GitSimpleHandler h = new GitSimpleHandler(myProject, root, GitCommand.SHOW);
+      GitSimpleHandler h = new GitSimpleHandler(myProject, root, GitCommand.LOG);
       h.setNoSSH(true);
-      h.addParameters("--pretty=format:%H", "HEAD");
+      h.addParameters("-n1", "--pretty=format:%H", "HEAD");
       try {
         return h.run().trim();
       }
@@ -601,7 +601,7 @@ public class GitBranchConfigurations implements PersistentStateComponent<GitBran
   /**
    * Detect possible configurations
    *
-   * @param roots
+   * @param roots the vcs roots used to detect configuraitons
    * @return a sorted list of branches
    * @throws VcsException if there is a problem with running git
    */
