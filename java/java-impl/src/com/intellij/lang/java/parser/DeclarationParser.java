87b0c2b795c9ee106b522a5d6563fb83cc564660
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2010-08-12 20:10:34 +0400

    New Java parser (incomplete methods parsing fixed)

--- java/java-impl/src/com/intellij/lang/java/parser/DeclarationParser.java
+++ java/java-impl/src/com/intellij/lang/java/parser/DeclarationParser.java
@@ -414,13 +414,7 @@ public class DeclarationParser {
     }
 
     final IElementType tokenType = builder.getTokenType();
-    if (tokenType == JavaTokenType.SEMICOLON) {
-      builder.advanceLexer();
-    }
-    else if (tokenType == JavaTokenType.LBRACE) {
-      StatementParser.parseCodeBlock(builder);
-    }
-    else {
+    if (tokenType != JavaTokenType.SEMICOLON && tokenType != JavaTokenType.LBRACE) {
       final PsiBuilder.Marker error = builder.mark();
       // heuristic: going to next line obviously means method signature is over, starting new method (actually, another one completion hack)
       final CharSequence text = builder.getOriginalText();
@@ -434,6 +428,12 @@ public class DeclarationParser {
         if (!expect(builder, APPEND_TO_METHOD_SET)) break;
       }
       error.error(JavaErrorMessages.message("expected.lbrace.or.semicolon"));
+    }
+
+    if (!expect(builder, JavaTokenType.SEMICOLON)) {
+      if (builder.getTokenType() == JavaTokenType.LBRACE) {
+        StatementParser.parseCodeBlock(builder);
+      }
     }
 
     declaration.done(anno ? JavaElementType.ANNOTATION_METHOD : JavaElementType.METHOD);
