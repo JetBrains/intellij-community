9fd2906108624470d3bc67218c4c86477fa58c5b
Anton Makeev <Anton.Makeev@jetbrains.com>
2010-08-19 13:48:43 +0400

    Code Style settings: refactoring

--- python/src/com/jetbrains/python/configuration/PyLanguageCodeStyleSettingsProvider.java
+++ python/src/com/jetbrains/python/configuration/PyLanguageCodeStyleSettingsProvider.java
@@ -3,17 +3,18 @@ package com.jetbrains.python.configuration;
 import com.intellij.application.options.codeStyle.LanguageCodeStyleSettingsProvider;
 import com.intellij.lang.Language;
 import com.intellij.openapi.application.ApplicationBundle;
-import com.intellij.psi.codeStyle.CodeStyleCustomizationsConsumer;
+import com.intellij.psi.codeStyle.CodeStyleSettingsCustomizable;
 import com.jetbrains.python.PythonLanguage;
 import com.jetbrains.python.formatter.PyCodeStyleSettings;
 import org.jetbrains.annotations.NotNull;
 
-import static com.intellij.psi.codeStyle.CodeStyleCustomizationsConsumer.*;
+import static com.intellij.psi.codeStyle.CodeStyleSettingsCustomizable.*;
 
 /**
  * @author yole
  */
 public class PyLanguageCodeStyleSettingsProvider extends LanguageCodeStyleSettingsProvider {
+  @NotNull
   @Override
   public Language getLanguage() {
     return PythonLanguage.getInstance();
@@ -21,48 +22,43 @@ public class PyLanguageCodeStyleSettingsProvider extends LanguageCodeStyleSettin
 
   @Override
   public String getCodeSample(@NotNull SettingsType settingsType) {
-    //noinspection EnumSwitchStatementWhichMissesCases
-    switch(settingsType) {
-      case SPACING_SETTINGS:
-        return SPACING_SETTINGS_PREVIEW;
-
-      case BLANK_LINE_SETTINGS:
-        return BLANK_LINES_SETTINGS_PREVIEW;
-
-      default:
-        return "";
-    }
-  }
-
-  public void customizeSpacingOptions(CodeStyleCustomizationsConsumer consumer) {
-    consumer.showStandardOptions("SPACE_BEFORE_METHOD_CALL_PARENTHESES",
-                                 "SPACE_AROUND_ASSIGNMENT_OPERATORS",
-                                 "SPACE_AROUND_LOGICAL_OPERATORS",
-                                 "SPACE_AROUND_EQUALITY_OPERATORS",
-                                 "SPACE_AROUND_RELATIONAL_OPERATORS",
-                                 "SPACE_AROUND_BITWISE_OPERATORS",
-                                 "SPACE_AROUND_ADDITIVE_OPERATORS",
-                                 "SPACE_AROUND_MULTIPLICATIVE_OPERATORS",
-                                 "SPACE_AROUND_SHIFT_OPERATORS",
-                                 "SPACE_WITHIN_METHOD_CALL_PARENTHESES",
-                                 "SPACE_WITHIN_BRACKETS",
-                                 "SPACE_AFTER_COMMA",
-                                 "SPACE_BEFORE_COMMA",
-                                "SPACE_BEFORE_SEMICOLON");
-    consumer.showCustomOption(PyCodeStyleSettings.class, "SPACE_BEFORE_LBRACKET", "Left bracket", BEFORE_PARENTHESES);    
-    consumer.showCustomOption(PyCodeStyleSettings.class, "SPACE_AROUND_EQ_IN_NAMED_PARAMETER", "Around = in named parameter", AROUND_OPERATORS);
-    consumer.showCustomOption(PyCodeStyleSettings.class, "SPACE_AROUND_EQ_IN_KEYWORD_ARGUMENT", "Around = in keyword argument", AROUND_OPERATORS);
-    consumer.showCustomOption(PyCodeStyleSettings.class, "SPACE_WITHIN_BRACES", "Within braces", WITHIN_PARENTHESES);
-    consumer.showCustomOption(PyCodeStyleSettings.class, "SPACE_BEFORE_COLON", ApplicationBundle.message("checkbox.spaces.before.colon"), OTHER);
+    if (settingsType == SettingsType.SPACING_SETTINGS) return SPACING_SETTINGS_PREVIEW;
+    if (settingsType == SettingsType.BLANK_LINES_SETTINGS) return BLANK_LINES_SETTINGS_PREVIEW;
+    return "";
   }
 
   @Override
-  public void customizeBlankLinesOptions(CodeStyleCustomizationsConsumer consumer) {
-    consumer.showStandardOptions("BLANK_LINES_AROUND_CLASS",
-                                 "BLANK_LINES_AROUND_METHOD",
-                                 "BLANK_LINES_AFTER_IMPORTS",
-                                 "KEEP_BLANK_LINES_IN_DECLARATIONS",
-                                 "KEEP_BLANK_LINES_IN_CODE");
+  public void customizeSettings(@NotNull CodeStyleSettingsCustomizable consumer, @NotNull SettingsType settingsType) {
+    if (settingsType == SettingsType.SPACING_SETTINGS) {
+      consumer.showStandardOptions("SPACE_BEFORE_METHOD_CALL_PARENTHESES",
+                                   "SPACE_AROUND_ASSIGNMENT_OPERATORS",
+                                   "SPACE_AROUND_LOGICAL_OPERATORS",
+                                   "SPACE_AROUND_EQUALITY_OPERATORS",
+                                   "SPACE_AROUND_RELATIONAL_OPERATORS",
+                                   "SPACE_AROUND_BITWISE_OPERATORS",
+                                   "SPACE_AROUND_ADDITIVE_OPERATORS",
+                                   "SPACE_AROUND_MULTIPLICATIVE_OPERATORS",
+                                   "SPACE_AROUND_SHIFT_OPERATORS",
+                                   "SPACE_WITHIN_METHOD_CALL_PARENTHESES",
+                                   "SPACE_WITHIN_BRACKETS",
+                                   "SPACE_AFTER_COMMA",
+                                   "SPACE_BEFORE_COMMA",
+                                  "SPACE_BEFORE_SEMICOLON");
+      consumer.showCustomOption(PyCodeStyleSettings.class, "SPACE_BEFORE_LBRACKET", "Left bracket", SPACES_BEFORE_PARENTHESES);
+      consumer.showCustomOption(PyCodeStyleSettings.class, "SPACE_AROUND_EQ_IN_NAMED_PARAMETER", "Around = in named parameter",
+                                SPACES_AROUND_OPERATORS);
+      consumer.showCustomOption(PyCodeStyleSettings.class, "SPACE_AROUND_EQ_IN_KEYWORD_ARGUMENT", "Around = in keyword argument",
+                                SPACES_AROUND_OPERATORS);
+      consumer.showCustomOption(PyCodeStyleSettings.class, "SPACE_WITHIN_BRACES", "Within braces", SPACES_WITHIN);
+      consumer.showCustomOption(PyCodeStyleSettings.class, "SPACE_BEFORE_COLON", ApplicationBundle.message("checkbox.spaces.before.colon"), SPACES_OTHER);
+    }
+    else if (settingsType == SettingsType.BLANK_LINES_SETTINGS) {
+      consumer.showStandardOptions("BLANK_LINES_AROUND_CLASS",
+                                   "BLANK_LINES_AROUND_METHOD",
+                                   "BLANK_LINES_AFTER_IMPORTS",
+                                   "KEEP_BLANK_LINES_IN_DECLARATIONS",
+                                   "KEEP_BLANK_LINES_IN_CODE");
+    }
   }
 
   private static String SPACING_SETTINGS_PREVIEW = "def settings_preview(argument, key=value):\n" +
