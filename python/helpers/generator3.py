623a31b438aacc406d6fe6c0c1211bcaa0ff76cd
Dmitry Jemerov <yole@jetbrains.com>
2010-08-23 16:24:13 +0400

    restore __init__ signature for skeleton classes

--- python/helpers/generator3.py
+++ python/helpers/generator3.py
@@ -894,7 +894,7 @@ class ModuleRedeclarator(object):
     SIG_DOC_NOTE = "restored from __doc__"
     SIG_DOC_UNRELIABLY = "NOTE: unreliably restored from __doc__ "
 
-    def restoreByDocString(self, signature_string, func_name, class_name, deco=None):
+    def restoreByDocString(self, signature_string, class_name, deco=None):
         """
         @param signature_string: parameter list extracted from the doc string.
         @param func_name: name of the function.
@@ -945,9 +945,10 @@ class ModuleRedeclarator(object):
         seq = makeNamesUnique(seq)
         return (seq, note)
 
-    def parseFuncDoc(self, func_doc, func_name, class_name, deco=None, sip_generated=False):
+    def parseFuncDoc(self, func_doc, func_id, func_name, class_name, deco=None, sip_generated=False):
         """
         @param func_doc: __doc__ of the function.
+        @param func_id: name to look for as identifier of the function in docstring
         @param func_name: name of the function.
         @param class_name: name of the containing class, or None
         @param deco: decorator to use
@@ -956,21 +957,21 @@ class ModuleRedeclarator(object):
         if sip_generated:
             overloads = []
             for l in func_doc.split('\n'):
-                signature = func_name + '('
+                signature = func_id + '('
                 i = l.find(signature)
                 if i >= 0:
                     overloads.append(l[i+len(signature):])
             if len(overloads) > 1:
-                param_lists = [self.restoreByDocString(s, func_name, class_name, deco)[0] for s in overloads]
+                param_lists = [self.restoreByDocString(s, class_name, deco)[0] for s in overloads]
                 spec = self.buildSignature(func_name, self.restoreParametersForOverloads(param_lists))
                 return (spec, "restored from __doc__ with multiple overloads")
 
         # find the first thing to look like a definition
-        prefix_re = re.compile(func_name + "\s*\(") # "foo(..."
+        prefix_re = re.compile(func_id + "\s*\(") # "foo(..."
         match = prefix_re.search(func_doc)
         # parse the part that looks right
         if match:
-            params, note = self.restoreByDocString(func_doc[match.end():], func_name, class_name, deco)
+            params, note = self.restoreByDocString(func_doc[match.end():], class_name, deco)
             spec = func_name + flatten(params)
             # if "NOTE" in note:
             # print "------\n", func_name, "@", match.end()
@@ -1141,7 +1142,9 @@ class ModuleRedeclarator(object):
                 funcdoc = p_func.__doc__
             sig_restored = False
             if isinstance(funcdoc, STR_TYPES):
-                (spec, more_notes) = self.parseFuncDoc(funcdoc, p_name, classname, deco, sip_generated)
+                (spec, more_notes) = self.parseFuncDoc(funcdoc, p_name, p_name, classname, deco, sip_generated)
+                if spec is None and p_name == '__init__' and classname:
+                    (spec, more_notes) = self.parseFuncDoc(funcdoc, classname, p_name, classname, deco, sip_generated)
                 sig_restored = spec is not None
                 if more_notes:
                     if sig_note:
@@ -1219,6 +1222,9 @@ class ModuleRedeclarator(object):
             # add fake __init__s to type and tuple to have the right sig
             if p_class in self.FAKE_BUILTIN_INITS:
                 methods["__init__"] = self.fake_builtin_init
+            elif '__init__' not in methods:
+                methods['__init__'] = getattr(p_class, '__init__')
+                
             #
             for item_name in sortedNoCase(methods.keys()):
                 item = methods[item_name]
