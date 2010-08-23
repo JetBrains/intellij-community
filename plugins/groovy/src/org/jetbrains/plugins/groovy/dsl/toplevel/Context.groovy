e0459a099552641ad1a78798de24fdf3acd4ee4b
peter <peter.gromov@jetbrains.com>
2010-08-23 13:29:53 +0100

    don't write gdsl applicability filters in slow groovy

--- plugins/groovy/src/org/jetbrains/plugins/groovy/dsl/toplevel/Context.groovy
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/dsl/toplevel/Context.groovy
@@ -1,20 +1,12 @@
 package org.jetbrains.plugins.groovy.dsl.toplevel
 
-import com.intellij.openapi.util.Pair
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.patterns.ElementPattern
-import com.intellij.patterns.ObjectPattern
-import com.intellij.psi.JavaPsiFacade
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
-import com.intellij.psi.PsiType
-import com.intellij.util.ProcessingContext
 import org.jetbrains.plugins.groovy.dsl.toplevel.scopes.Scope
-import org.jetbrains.plugins.groovy.lang.psi.impl.statements.expressions.TypesUtil
 import static com.intellij.patterns.PlatformPatterns.psiFile
 import static com.intellij.patterns.PlatformPatterns.virtualFile
 
-/**
+ /**
  * @author ilyas
  */
 class Context {
@@ -41,25 +33,11 @@ class Context {
     // ctype : <ctype>
     // Qualifier type to be augmented
     if (args.ctype instanceof String) {
-      addFilter getClassTypeFilter(args.ctype)
+      addFilter ClassContextFilter.subtypeOf(args.ctype)
     }
     else if (args.ctype instanceof ElementPattern) {
-      addFilter new ClassContextFilter(args.ctype)
+      addFilter ClassContextFilter.fromClassPattern(args.ctype)
     }
-  }
-
-  static ContextFilter getClassTypeFilter(final String ctype) {
-    return new ClassContextFilter(new ObjectPattern<Pair<PsiType, PsiElement>, ObjectPattern>(PsiType) {
-       @Override
-       boolean accepts(Object o, ProcessingContext context) {
-         if (o instanceof Pair && o.second instanceof PsiFile && o.first instanceof PsiType) {
-           def place = (PsiFile) o.second
-           PsiType myType = JavaPsiFacade.getElementFactory(place.project).createTypeFromText(ctype, place)
-           return TypesUtil.isAssignable(myType, (PsiType)o.first, place.manager, place.resolveScope, false)
-         }
-         return false
-       }
-    })
   }
 
   private void addFilter(ContextFilter cl) {
