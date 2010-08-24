a252ac95f1f47a97a7360975c87f0eaabca77102
irengrig <Irina.Chernushina@jetbrains.com>
2010-08-24 19:27:05 +0400

    IDEA-53288 Idea should support standard Subversion layout

--- plugins/svn4idea/src/org/jetbrains/idea/svn/dialogs/CheckoutDialog.java
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/dialogs/CheckoutDialog.java
@@ -26,7 +26,7 @@ public class CheckoutDialog extends RepositoryBrowserDialog {
   private final CheckoutProvider.Listener myListener;
 
   public CheckoutDialog(Project project, final CheckoutProvider.Listener listener) {
-    super(project, false);
+    super(project, false, null);
     myListener = listener;
   }
 
