6d5dbce7e71937513ca635f044b075705001ea92
Dmitry Cheryasov <dmitry.cheryasov@jetbrains.com>
2010-08-24 18:10:36 +0300

    Fix PY-1642 (wrong debug parameters order; a silly typo!)

--- python/src/com/jetbrains/python/run/PythonScriptCommandLineState.java
+++ python/src/com/jetbrains/python/run/PythonScriptCommandLineState.java
@@ -27,7 +27,7 @@ public class PythonScriptCommandLineState extends PythonCommandLineState {
     assert exe_options != null;
     exe_options.addParametersString(myConfig.getInterpreterOptions());
 
-    ParamsGroup script_parameters = parametersList.getParamsGroup(GROUP_EXE_OPTIONS);
+    ParamsGroup script_parameters = parametersList.getParamsGroup(GROUP_SCRIPT);
     assert script_parameters != null;
     if (!StringUtil.isEmptyOrSpaces(myConfig.getScriptName())) {
       script_parameters.addParameter(myConfig.getScriptName());
