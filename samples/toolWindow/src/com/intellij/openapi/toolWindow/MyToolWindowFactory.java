1d9815c34fbc683e07363c74331b4731b74c7524
Alexey <Alexey.Chursin@jetbrains.com>
2010-08-26 13:35:56 +0400

    The updated version of the toolWindow sample plugin.

--- samples/toolWindow/src/com/intellij/openapi/toolWindow/MyToolWindowFactory.java
+++ samples/toolWindow/src/com/intellij/openapi/toolWindow/MyToolWindowFactory.java
@@ -1,15 +1,12 @@
-package com.intellij.openapi.samples;
+package com.intellij.openapi.toolWindow;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
-import com.intellij.openapi.wm.ToolWindowEP;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
-import com.intellij.util.ui.UIUtil;
 
 import javax.swing.*;
-import java.awt.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,14 +18,14 @@ public class MyToolWindowFactory implements ToolWindowFactory {
 
     // Creates the tool window content.
     public void createToolWindowContent(Project project, ToolWindow toolWindow) {
-     ToolDialog myDialog = new ToolDialog();
-     myDialog.toolWin = toolWindow;
-     myDialog.currentDateTime();     
-     JPanel myContentPanel = myDialog.getPanel();
-     ContentFactory contentFactory =  ContentFactory.SERVICE.getInstance();
-     Content content = contentFactory.createContent(myContentPanel, "", false);
-     toolWindow.getContentManager().addContent(content);
-        
+        ToolDialog myDialog = new ToolDialog();
+        myDialog.toolWin = toolWindow;
+        myDialog.currentDateTime();
+        JPanel myContentPanel = myDialog.getPanel();
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        Content content = contentFactory.createContent(myContentPanel, "", false);
+        toolWindow.getContentManager().addContent(content);
+
     }
-    
+
 }
