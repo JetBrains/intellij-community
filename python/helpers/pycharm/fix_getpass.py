6ee70d7f7932c13d7a5414e6ffc5a0931e69d20f
Dmitry Jemerov <yole@jetbrains.com>
2010-08-26 16:54:37 +0400

    really fix python 2.5 compatibility

--- python/helpers/pycharm/fix_getpass.py
+++ python/helpers/pycharm/fix_getpass.py
@@ -7,6 +7,6 @@ def fixGetpass():
         fallback = getattr(getpass, 'fallback_getpass', None) # >= 2.6
         if not fallback:
             fallback = getpass.default_getpass # <= 2.5
-        getpass.getpass = getpass.fallback_getpass
-        warnings.simplefilter("ignore", category=getpass.GetPassWarning)
-
+        getpass.getpass = fallback
+        if hasattr(getpass, 'GetPassWarning'):
+            warnings.simplefilter("ignore", category=getpass.GetPassWarning)
