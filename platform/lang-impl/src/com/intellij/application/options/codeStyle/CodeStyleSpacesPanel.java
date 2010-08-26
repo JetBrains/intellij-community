bdc0cb14647d826bd04ac9a14a386ce2c543c4d8
Anton Makeev <Anton.Makeev@jetbrains.com>
2010-08-26 16:24:57 +0400

    CIDR Formatter: more spaces within

--- platform/lang-impl/src/com/intellij/application/options/codeStyle/CodeStyleSpacesPanel.java
+++ platform/lang-impl/src/com/intellij/application/options/codeStyle/CodeStyleSpacesPanel.java
@@ -93,10 +93,10 @@ public class CodeStyleSpacesPanel extends OptionTreeWithPreviewPanel {
     initBooleanField("SPACE_BEFORE_FINALLY_KEYWORD", ApplicationBundle.message("checkbox.spaces.finally.keyword"), SPACES_BEFORE_KEYWORD);
     initCustomOptions(SPACES_BEFORE_KEYWORD);
 
-    initBooleanField("SPACE_WITHIN_PARENTHESES", ApplicationBundle.message("checkbox.spaces.within.parentheses"), SPACES_WITHIN);
     initBooleanField("SPACE_WITHIN_BRACKETS", ApplicationBundle.message("checkbox.spaces.within.brackets"), SPACES_WITHIN);
     initBooleanField("SPACE_WITHIN_ARRAY_INITIALIZER_BRACES", ApplicationBundle.message("checkbox.spaces.within.array.initializer.braces"),
                      SPACES_WITHIN);
+    initBooleanField("SPACE_WITHIN_PARENTHESES", ApplicationBundle.message("checkbox.spaces.within.parentheses"), SPACES_WITHIN);
     initBooleanField("SPACE_WITHIN_METHOD_CALL_PARENTHESES", ApplicationBundle.message("checkbox.spaces.checkbox.spaces.method.call.parentheses"),
                      SPACES_WITHIN);
     initBooleanField("SPACE_WITHIN_METHOD_PARENTHESES", ApplicationBundle.message("checkbox.spaces.checkbox.spaces.method.declaration.parentheses"),
