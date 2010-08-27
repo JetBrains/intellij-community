410029fb3fcfec8f6f725aa9f0b6af8b415df736
Alexey <Alexey.Chursin@jetbrains.com>
2010-08-27 19:56:35 +0400

    The updated version of the toolWindow sample plugin.

--- samples/toolWindow/src/com/intellij/openapi/toolWindow/MyToolWindowFactory.java
+++ samples/toolWindow/src/com/intellij/openapi/toolWindow/MyToolWindowFactory.java
@@ -7,6 +7,9 @@ import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Calendar;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,15 +19,60 @@ import javax.swing.*;
  */
 public class MyToolWindowFactory implements ToolWindowFactory {
 
-    // Creates the tool window content.
+    private JButton refreshToolWindowButton;
+    private JButton hideToolWindowButton;
+    private JLabel currentDate;
+    private JLabel currentTime;
+    private JLabel timeZone;
+    private JPanel myToolWindowContent;
+    private ToolWindow myToolWindow;
+
+
+    public MyToolWindowFactory() {
+        hideToolWindowButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+             myToolWindow.hide(null);
+            }
+        });
+        refreshToolWindowButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               MyToolWindowFactory.this.currentDateTime();
+            }
+        });
+    }
+
+    // Create the tool window content.
     public void createToolWindowContent(Project project, ToolWindow toolWindow) {
-        ToolDialog myDialog = new ToolDialog();
-        myDialog.toolWin = toolWindow;
-        myDialog.currentDateTime();
-        JPanel myContentPanel = myDialog.getPanel();
+        myToolWindow = toolWindow;
+        this.currentDateTime();
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
-        Content content = contentFactory.createContent(myContentPanel, "", false);
+        Content content = contentFactory.createContent(myToolWindowContent, "", false);
         toolWindow.getContentManager().addContent(content);
+
+    }
+
+    public void currentDateTime() {
+        // Get current date and time
+        Calendar instance = Calendar.getInstance();
+        currentDate.setText(String.valueOf(instance.get(Calendar.DAY_OF_MONTH)) + "/"
+                + String.valueOf(instance.get(Calendar.MONTH) + 1) + "/" + String.valueOf(instance.get(Calendar.YEAR)));
+        currentDate.setIcon(new ImageIcon(getClass().getResource("/com/intellij/openapi/toolWindow/Calendar-icon.png")));
+        int min = instance.get(Calendar.MINUTE);
+        String strMin;
+        if (min < 10) {
+            strMin = "0" + String.valueOf(min);
+        } else {
+            strMin = String.valueOf(min);
+        }
+        currentTime.setText(instance.get(Calendar.HOUR_OF_DAY) + ":" + strMin);
+        currentTime.setIcon(new ImageIcon(getClass().getResource("/com/intellij/openapi/toolWindow/Time-icon.png")));
+        // Get time zone
+        long gmt_Offset = instance.get(Calendar.ZONE_OFFSET); // offset from GMT in milliseconds
+        String str_gmt_Offset = String.valueOf(gmt_Offset / 3600000);
+        str_gmt_Offset = (gmt_Offset > 0) ? "GMT + " + str_gmt_Offset : "GMT - " + str_gmt_Offset;
+        timeZone.setText(str_gmt_Offset);
+        timeZone.setIcon(new ImageIcon(getClass().getResource("/com/intellij/openapi/toolWindow/Time-zone-icon.png")));
+
 
     }
 
