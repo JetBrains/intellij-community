4b6d2277ec0ad52a36f7c38e8607ca220992ffa9
Dmitry Jemerov <yole@jetbrains.com>
2010-08-29 15:10:50 +0400

    delete method which has been moved away

--- python/src/com/jetbrains/python/buildout/config/psi/BuildoutPsiUtil.java
+++ python/src/com/jetbrains/python/buildout/config/psi/BuildoutPsiUtil.java
@@ -1,19 +1,13 @@
 package com.jetbrains.python.buildout.config.psi;
 
-import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiFileFactory;
 import com.intellij.psi.util.PsiTreeUtil;
-import com.jetbrains.python.buildout.BuildoutFacet;
-import com.jetbrains.python.buildout.config.BuildoutCfgLanguage;
 import com.jetbrains.python.buildout.config.psi.impl.BuildoutCfgFile;
 import com.jetbrains.python.buildout.config.psi.impl.BuildoutCfgOption;
 import com.jetbrains.python.buildout.config.psi.impl.BuildoutCfgSection;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.File;
 import java.util.List;
 
 /**
@@ -30,25 +24,6 @@ public class BuildoutPsiUtil {
       final BuildoutCfgSection section = configFile.findSectionByName(part);
       if (section != null && DJANGO_RECIPE.equals(section.getOptionValue(RECIPE))) {
         return section;
-      }
-    }
-    return null;
-  }
-
-  @Nullable
-  public static BuildoutCfgFile readBuildOutCfgFile(BuildoutFacet buildoutFacet) {
-    File cfg = buildoutFacet.getConfigFile();
-    if (cfg != null && cfg.exists()) {
-      try {
-        char[] scriptText = FileUtil.loadFileText(cfg);
-        final PsiFile configFile = PsiFileFactory
-          .getInstance(buildoutFacet.getModule().getProject()).createFileFromText("buildout.cfg",
-                                                                                  BuildoutCfgLanguage.INSTANCE, new String(scriptText));
-        if (configFile != null && configFile instanceof BuildoutCfgFile) {
-          return (BuildoutCfgFile)configFile;
-        }
-      }
-      catch (Exception e) {
       }
     }
     return null;
