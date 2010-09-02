be790663f47a62d5d502d3fa42a14d14fe600fbf
Sergey Evdokimov <sergey.evdokimov@jetbrains.com>
2010-09-02 18:12:59 +0400

    Move GspGroovyFileImpl.LightVariable to GrLightVariable.

---
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/impl/synthetic/GrLightVariable.java
@@ -0,0 +1,43 @@
+package org.jetbrains.plugins.groovy.lang.psi.impl.synthetic;
+
+import com.intellij.psi.*;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.lang.psi.impl.synthetic.GrImplicitVariableImpl;
+
+/**
+* @author sergey.evdokimov
+*/
+public class GrLightVariable extends GrImplicitVariableImpl implements NavigatablePsiElement {
+
+  private final PsiElement myElement;
+
+  public GrLightVariable(PsiModifierList modifierList,
+                       PsiManager manager,
+                       @NonNls String name,
+                       @NonNls @NotNull String type,
+                       PsiElement element) {
+    super(modifierList, manager, name, type, element);
+    this.myElement = element;
+  }
+
+  public GrLightVariable(PsiModifierList modifierList,
+                       PsiManager manager,
+                       @NonNls String name,
+                       @NotNull PsiType type,
+                       PsiElement element) {
+    super(modifierList, manager, new GrLightIdentifier(manager, name), type, false, element);
+    this.myElement = element;
+  }
+
+  @NotNull
+  @Override
+  public PsiElement getNavigationElement() {
+    return myElement;
+  }
+
+  @Override
+  public boolean isEquivalentTo(PsiElement another) {
+    return another == myElement || super.isEquivalentTo(another);
+  }
+}
