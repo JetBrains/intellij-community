4bdc92737c5c8b7d7d37b0a81369fda8b4aa370b
Sergey Evdokimov <sergey.evdokimov@jetbrains.com>
2010-09-09 18:24:08 +0400

    Fix: IDEA-40926 (Make GSP aware of model returned from controller)

--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/resolve/MethodTypeInferencer.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/resolve/MethodTypeInferencer.java
@@ -23,6 +23,7 @@ import org.jetbrains.plugins.groovy.lang.psi.api.statements.blocks.GrCodeBlock;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.branch.GrReturnStatement;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrExpression;
 import org.jetbrains.plugins.groovy.lang.psi.impl.statements.expressions.TypesUtil;
+import org.jetbrains.plugins.groovy.lang.psi.util.PsiUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,51 +40,27 @@ public class MethodTypeInferencer implements Computable<PsiType> {
 
   @Nullable
   public PsiType compute () {
-    List<GrReturnStatement> returns = new ArrayList<GrReturnStatement>();
-    collectReturns(myBlock, returns);
+    List<GrReturnStatement> returns = PsiUtil.collectReturns(myBlock);
 
     PsiType result = null;
     PsiManager manager = myBlock.getManager();
     for (GrReturnStatement returnStatement : returns) {
       GrExpression value = returnStatement.getReturnValue();
       if (value != null) {
-        result = upperBound(result, value.getType(), manager);
+        result = TypesUtil.getLeastUpperBoundNullable(result, value.getType(), manager);
       }
     }
 
     boolean isObject = returns.size() == 0;
 
-    GrStatement[] statements = myBlock.getStatements();
-    if (statements.length > 0) {
-      GrStatement last = statements[statements.length - 1];
-      if (last instanceof GrExpression) {
-        result = upperBound(((GrExpression) last).getType(), result, manager);
-        isObject = false;
-      }
+    GrStatement last = PsiUtil.getLastStatement(myBlock);
+    if (last instanceof GrExpression) {
+      result = TypesUtil.getLeastUpperBoundNullable(((GrExpression) last).getType(), result, manager);
+      isObject = false;
     }
 
     if (isObject) return TypesUtil.getJavaLangObject(myBlock);
     
     return result;
-  }
-
-  private static PsiType upperBound(PsiType type1, PsiType type2, PsiManager manager) {
-    if (type1 == null) return type2;
-    if (type2 == null) return type1;
-    if (type1.isAssignableFrom(type2)) return type1;
-    if (type2.isAssignableFrom(type1)) return type2;
-    return TypesUtil.getLeastUpperBound(type1, type2, manager);
-  }
-
-  private static void collectReturns(PsiElement element, List<GrReturnStatement> returns) {
-    if (element instanceof GrReturnStatement) {
-      returns.add((GrReturnStatement) element);
-    } else {
-      PsiElement child = element.getFirstChild();
-      while(child != null) {
-        collectReturns(child, returns);
-        child = child.getNextSibling();
-      }
-    }
   }
 }
