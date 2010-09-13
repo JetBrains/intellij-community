8e66303be157fe64df559c91a1e1dc51036fbede
Maxim Medvedev <maxim.medvedev@jetbrains.com>
2010-09-13 12:23:20 +0400

    IDEA-57867    Package declaration not updated when Groovy class is moved

--- plugins/groovy/src/org/jetbrains/plugins/groovy/refactoring/move/MoveGroovyScriptHandler.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/refactoring/move/MoveGroovyScriptHandler.java
@@ -29,7 +29,6 @@ import com.intellij.psi.PsiReference;
 import com.intellij.refactoring.HelpID;
 import com.intellij.refactoring.JavaRefactoringSettings;
 import com.intellij.refactoring.move.MoveCallback;
-import com.intellij.refactoring.move.moveClassesOrPackages.MoveClassesOrPackagesDialog;
 import com.intellij.refactoring.move.moveClassesOrPackages.MoveClassesOrPackagesHandlerBase;
 import com.intellij.refactoring.move.moveClassesOrPackages.MoveClassesOrPackagesImpl;
 import com.intellij.refactoring.util.CommonRefactoringUtil;
@@ -55,7 +54,7 @@ public class MoveGroovyScriptHandler extends MoveClassesOrPackagesHandlerBase {
     for (PsiElement element : elements) {
       if (!canMove(element)) return false;
     }
-    return super.canMove(elements, targetContainer);
+    return targetContainer == null || isValidTarget(targetContainer, elements);
   }
 
   private static boolean canMove(PsiElement element) {
@@ -68,7 +67,7 @@ public class MoveGroovyScriptHandler extends MoveClassesOrPackagesHandlerBase {
     }
 
     final PsiClass[] classes = file.getClasses();
-    return classes.length == 1 && classes[0] instanceof GroovyScriptClass;
+    return classes.length > 0 && classes[0] instanceof GroovyScriptClass;
   }
 
   @Override
@@ -100,7 +99,7 @@ public class MoveGroovyScriptHandler extends MoveClassesOrPackagesHandlerBase {
       PsiElement psiElement = elements[i];
       searchTextOccurences = TextOccurrencesUtil.isSearchTextOccurencesEnabled(psiElement);
     }
-    final MoveClassesOrPackagesDialog moveDialog =
+    final MoveGroovyScriptDialog moveDialog =
       new MoveGroovyScriptDialog(project, searchTextOccurences, elements, initialTargetElement, moveCallback);
 
     boolean searchInComments = JavaRefactoringSettings.getInstance().MOVE_SEARCH_IN_COMMENTS;
