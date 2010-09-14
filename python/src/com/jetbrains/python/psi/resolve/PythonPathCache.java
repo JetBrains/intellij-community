56616a986e437a7c680ff0023c8414f206bd61e2
Dmitry Jemerov <yole@jetbrains.com>
2010-09-14 18:25:54 +0400

    SDK path cache must be per-project

--- python/src/com/jetbrains/python/psi/resolve/PythonPathCache.java
+++ python/src/com/jetbrains/python/psi/resolve/PythonPathCache.java
@@ -14,6 +14,7 @@ import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.vfs.*;
 import com.intellij.psi.PsiElement;
+import com.intellij.util.containers.WeakHashMap;
 import com.jetbrains.python.psi.impl.PyQualifiedName;
 
 import java.util.HashMap;
@@ -24,7 +25,7 @@ import java.util.Map;
  * @author yole
  */
 public class PythonPathCache implements Disposable {
-  private static final Key<PythonPathCache> KEY = Key.create("PythonPathCache");
+  private static final Key<Map<Project, PythonPathCache>> KEY = Key.create("PythonPathCache");
 
   public static PythonPathCache getInstance(Module module) {
     return ModuleServiceManager.getService(module, PythonPathCache.class);
@@ -32,19 +33,28 @@ public class PythonPathCache implements Disposable {
 
   public static PythonPathCache getInstance(Project project, Sdk sdk) {
     synchronized (KEY) {
-      PythonPathCache cache = sdk.getUserData(KEY);
+      Map<Project, PythonPathCache> cacheMap = sdk.getUserData(KEY);
+      if (cacheMap == null) {
+        cacheMap = new WeakHashMap<Project, PythonPathCache>();
+        sdk.putUserData(KEY, cacheMap);
+      }
+      PythonPathCache cache = cacheMap.get(project);
       if (cache == null) {
         cache = new PythonPathCache(project, sdk);
-        sdk.putUserData(KEY, cache);
+        cacheMap.put(project, cache);
       }
       return cache;
     }
   }
 
+  private final Project myProject;
+  private final Sdk mySdk;
   private final Map<PyQualifiedName, List<PsiElement>> myCache = new HashMap<PyQualifiedName, List<PsiElement>>();
 
   @SuppressWarnings({"UnusedDeclaration"})
   public PythonPathCache(Module module) {
+    myProject = module.getProject();
+    mySdk = null;
     module.getMessageBus().connect().subscribe(ProjectTopics.PROJECT_ROOTS, new ModuleRootListener() {
       public void beforeRootsChange(ModuleRootEvent event) {
       }
@@ -58,9 +68,19 @@ public class PythonPathCache implements Disposable {
 
   @Override
   public void dispose() {
+    if (mySdk != null) {
+      synchronized (KEY) {
+        final Map<Project, PythonPathCache> cacheMap = mySdk.getUserData(KEY);
+        if (cacheMap != null) {
+          cacheMap.remove(myProject);
+        }
+      }
+    }
   }
 
   public PythonPathCache(Project project, final Sdk sdk) {
+    myProject = project;
+    mySdk = sdk;
     sdk.getRootProvider().addRootSetChangedListener(new RootProvider.RootSetChangedListener() {
       @Override
       public void rootSetChanged(RootProvider wrapper) {
