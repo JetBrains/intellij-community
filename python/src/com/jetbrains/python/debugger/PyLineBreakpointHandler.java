c7c3b423b5ee21819b8e7bc4e6e6c3bf2a806d46
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2010-09-20 17:49:50 +0400

    fixed breakpoint removing (PY-1288)

--- python/src/com/jetbrains/python/debugger/PyLineBreakpointHandler.java
+++ python/src/com/jetbrains/python/debugger/PyLineBreakpointHandler.java
@@ -1,15 +1,20 @@
 package com.jetbrains.python.debugger;
 
+import com.google.common.collect.Maps;
 import com.intellij.xdebugger.XSourcePosition;
 import com.intellij.xdebugger.breakpoints.XBreakpointHandler;
 import com.intellij.xdebugger.breakpoints.XBreakpointProperties;
 import com.intellij.xdebugger.breakpoints.XLineBreakpoint;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Map;
+
 
 public class PyLineBreakpointHandler extends XBreakpointHandler<XLineBreakpoint<XBreakpointProperties>> {
 
   private final PyDebugProcess myDebugProcess;
+
+  private final Map<XLineBreakpoint<XBreakpointProperties>, XSourcePosition> myBreakPointPositions = Maps.newHashMap();
 
   public PyLineBreakpointHandler(@NotNull final PyDebugProcess debugProcess) {
     super(PyLineBreakpointType.class);
@@ -20,13 +25,15 @@ public class PyLineBreakpointHandler extends XBreakpointHandler<XLineBreakpoint<
     final XSourcePosition position = breakpoint.getSourcePosition();
     if (position != null) {
       myDebugProcess.addBreakpoint(myDebugProcess.getPositionConverter().convert(position), breakpoint);
+      myBreakPointPositions.put(breakpoint, position);
     }
   }
 
   public void unregisterBreakpoint(@NotNull final XLineBreakpoint<XBreakpointProperties> breakpoint, final boolean temporary) {
-    final XSourcePosition position = breakpoint.getSourcePosition();
+    final XSourcePosition position = myBreakPointPositions.get(breakpoint);
     if (position != null) {
       myDebugProcess.removeBreakpoint(myDebugProcess.getPositionConverter().convert(position));
+      myBreakPointPositions.remove(breakpoint);
     }
   }
 
