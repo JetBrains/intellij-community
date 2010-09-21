14a50488d0f66c71fa3994dc5edcca7cb3a54c59
Anton Makeev <Anton.Makeev@jetbrains.com>
2010-09-21 17:51:48 +0400

    lh: npe fixed in vfs listener

--- platform/lvcs-impl/src/com/intellij/history/integration/LocalHistoryEventDispatcher.java
+++ platform/lvcs-impl/src/com/intellij/history/integration/LocalHistoryEventDispatcher.java
@@ -122,7 +122,9 @@ public class LocalHistoryEventDispatcher extends VirtualFileAdapter implements V
       VirtualFile f = e.getFile();
 
       boolean isVersioned = myGateway.isVersioned(f);
-      boolean wasVersioned = f.getUserData(WAS_VERSIONED_KEY);
+      Boolean wasVersioned = f.getUserData(WAS_VERSIONED_KEY);
+      if (wasVersioned == null) return;
+      f.putUserData(WAS_VERSIONED_KEY, null);
 
       if (!wasVersioned && !isVersioned) return;
 
@@ -149,7 +151,9 @@ public class LocalHistoryEventDispatcher extends VirtualFileAdapter implements V
     VirtualFile f = e.getFile();
 
     boolean isVersioned = myGateway.isVersioned(f);
-    boolean wasVersioned = f.getUserData(WAS_VERSIONED_KEY);
+    Boolean wasVersioned = f.getUserData(WAS_VERSIONED_KEY);
+    if (wasVersioned == null) return;
+    f.putUserData(WAS_VERSIONED_KEY, null);
 
     if (!wasVersioned && !isVersioned) return;
 
