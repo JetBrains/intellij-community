d31382825fde2329a51ec472d5eb479f89515ff7
Dmitry Jemerov <yole@jetbrains.com>
2010-09-21 17:53:07 +0400

    remove junk throws clauses

--- python/testSrc/com/jetbrains/python/refactoring/PyExtractMethodTest.java
+++ python/testSrc/com/jetbrains/python/refactoring/PyExtractMethodTest.java
@@ -14,7 +14,7 @@ public class PyExtractMethodTest extends LightMarkedTestCase {
   private void doTest(final String testPath,
                       final String name,
                       final String result,
-                      final Pair<String, String>... files2Create) throws Exception {
+                      final Pair<String, String>... files2Create) {
 
     // Create additional files
     for (Pair<String, String> pair : files2Create) {
@@ -40,108 +40,108 @@ public class PyExtractMethodTest extends LightMarkedTestCase {
     myFixture.checkResultByFile("/refactoring/extractmethod/" + result);
   }
 
-  public void testParameter() throws Throwable {
+  public void testParameter() {
     doTest("outEmpty/parameter.before.py", "bar", "outEmpty/parameter.after.py");
   }
 
-  public void testBreakAst() throws Throwable {
+  public void testBreakAst() {
     doTest("outEmpty/break_ast.before.py", "bar", "outEmpty/break_ast.after.py");
   }
 
-  public void testExpression() throws Throwable {
+  public void testExpression() {
     doTest("outEmpty/expression.before.py", "plus", "outEmpty/expression.after.py");
   }
 
-  public void testStatement() throws Throwable {
+  public void testStatement() {
     doTest("outEmpty/statement.before.py", "foo", "outEmpty/statement.after.py");
   }
 
-  public void testStatements() throws Throwable {
+  public void testStatements() {
     doTest("outEmpty/statements.before.py", "foo", "outEmpty/statements.after.py");
   }
 
-  public void testStatementReturn() throws Throwable {
+  public void testStatementReturn() {
     doTest("outEmpty/statement_return.before.py", "foo", "outEmpty/statement_return.after.py");
   }
 
-  public void testBinaryExpression() throws Throwable {
+  public void testBinaryExpression() {
     doTest("controlFlow/binary_expr.before.py", "foo", "controlFlow/binary_expr.after.py");
   }
 
-  public void testWhileOutput() throws Throwable {
+  public void testWhileOutput() {
     doTest("controlFlow/while_output.before.py", "bar", "controlFlow/while_output.after.py");
   }
 
-  public void testNameCollisionClass() throws Throwable {
+  public void testNameCollisionClass() {
     doTest("namesCollision/class.before.py", "hello", "Method name clashes with already existing method name");
   }
 
-  public void testNameCollisionFile() throws Throwable {
+  public void testNameCollisionFile() {
     doTest("namesCollision/file.before.py", "hello", "Method name clashes with already existing method name");
   }
 
-  public void testNameCollisionSuperClass() throws Throwable {
+  public void testNameCollisionSuperClass() {
     doTest("namesCollision/superclass.before.py", "hello", "Method name clashes with already existing method name");
   }
 
-  public void testOutNotEmptyStatements() throws Throwable {
+  public void testOutNotEmptyStatements() {
     doTest("outNotEmpty/statements.before.py", "sum_squares", "outNotEmpty/statements.after.py");
   }
 
-  public void testOutNotEmptyStatements2() throws Throwable {
+  public void testOutNotEmptyStatements2() {
     doTest("outNotEmpty/statements2.before.py", "sum_squares", "outNotEmpty/statements2.after.py");
   }
 
-  public void testComment() throws Throwable {
+  public void testComment() {
     doTest("comment.before.py", "bar", "comment.after.py");
   }
 
-  public void testFile() throws Throwable {
+  public void testFile() {
     doTest("file.before.py", "bar", "file.after.py");
   }
 
 
-  public void testMethod() throws Throwable {
+  public void testMethod() {
     doTest("context/method.before.py", "bar", "context/method.after.py");
   }
 
-  public void testMethodIndent() throws Throwable {
+  public void testMethodIndent() {
     doTest("context/methodindent.before.py", "bar", "context/methodindent.after.py");
   }
 
-  public void testMethodReturn() throws Throwable {
+  public void testMethodReturn() {
     doTest("context/methodreturn.before.py", "bar", "context/methodreturn.after.py");
   }
 
-  public void testWrongSelectionIfPart() throws Throwable {
+  public void testWrongSelectionIfPart() {
     doTest("wrongSelection/ifpart.before.py", "bar", "Cannot perform extract method using selected element(s)");
   }
 
-  public void testFromImportStatement() throws Throwable {
+  public void testFromImportStatement() {
     doTest("wrongSelection/fromimport.before.py", "bar", "Cannot perform refactoring with from import statement inside code block");
   }
 
-  public void testPy479() throws Throwable {
+  public void testPy479() {
     doTest("outEmpty/py479.before.py", "bar", "outEmpty/py479.after.py");
   }
 
-  public void testClass() throws Throwable {
+  public void testClass() {
     doTest("context/class.before.py", "bar", "context/class.after.py");
   }
 
-  public void testConditionalReturn() throws Throwable {
+  public void testConditionalReturn() {
     doTest("conditionalreturn.before.py", "bar", "Cannot perform refactoring when execution flow is interrupted");
   }
 
-  public void testReturnTuple() throws Throwable {
+  public void testReturnTuple() {
     doTest("return_tuple.before.py", "bar", "return_tuple.after.py");
   }
 
-  public void testComment2() throws Throwable {
+  public void testComment2() {
     doTest("comment2.before.py", "baz", "comment2.after.py");
   }
 
-  public void testElseBody() throws Throwable {
+  public void testElseBody() {
     doTest("elsebody.before.py", "baz", "elsebody.after.py");
   }
 
