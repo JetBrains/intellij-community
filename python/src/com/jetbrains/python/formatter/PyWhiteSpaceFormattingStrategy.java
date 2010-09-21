2d0baa5fb6316a37cd52e20898a42354c0b1c7f7
Denis Zhdanov <Denis.Zhdanov@jetbrains.com>
2010-09-21 15:59:07 +0400

    IDEA-58743 Formatter: Add ability to configure formatter's 'white space symbols'
    1. Composite white space strategy is introduced;
    2. Added ability to replace existing formatter strategies on per-language basis;
    3. Python-specific white space formatter strategy is configured;

--- python/src/com/jetbrains/python/formatter/PyWhiteSpaceFormattingStrategy.java
+++ python/src/com/jetbrains/python/formatter/PyWhiteSpaceFormattingStrategy.java
@@ -1,20 +1,13 @@
 package com.jetbrains.python.formatter;
 
-import com.intellij.formatting.WhiteSpaceFormattingStrategy;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.psi.formatter.StaticSymbolWhiteSpaceDefinitionStrategy;
 
 /**
  * @author yole
  */
-public class PyWhiteSpaceFormattingStrategy implements WhiteSpaceFormattingStrategy {
-  @Override
-  public int check(@NotNull CharSequence text, int start, int end) {
-    for (int i = start; i < end; i++) {
-      char c = text.charAt(i);
-      if (c != ' ' && c != '\t' && c != '\n' && c != '\\') {
-        return i;
-      }
-    }
-    return end;
+public class PyWhiteSpaceFormattingStrategy extends StaticSymbolWhiteSpaceDefinitionStrategy {
+
+  public PyWhiteSpaceFormattingStrategy() {
+    super('\\');
   }
 }
