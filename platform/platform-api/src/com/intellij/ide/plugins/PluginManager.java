572e5ffc060afb1d309334da4e017a8cec1180b1
Dmitry Jemerov <yole@jetbrains.com>
2010-09-23 17:47:05 +0400

    branch number 97

--- platform/platform-api/src/com/intellij/ide/plugins/PluginManager.java
+++ platform/platform-api/src/com/intellij/ide/plugins/PluginManager.java
@@ -718,11 +718,11 @@ public class PluginManager {
             ourBuildNumber = BuildNumber.fromString(new String(FileUtil.loadFileText(buildTxtFile)).trim());
           }
           else {
-            ourBuildNumber = BuildNumber.fromString("96.SNAPSHOT");
+            ourBuildNumber = BuildNumber.fromString("97.SNAPSHOT");
           }
         }
         catch (IOException e) {
-          ourBuildNumber = BuildNumber.fromString("96.SNAPSHOT");
+          ourBuildNumber = BuildNumber.fromString("97.SNAPSHOT");
         }
       }
     }
