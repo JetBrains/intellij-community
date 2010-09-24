8df527135eca0ac02e2160a2d66e434337c6ebc1
Alexey Kudravtsev <cdr@intellij.com>
2010-09-24 19:59:23 +0400

    injection tests fixed

--- platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/GeneralHighlightingPass.java
+++ platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/GeneralHighlightingPass.java
@@ -299,7 +299,7 @@ public class GeneralHighlightingPass extends ProgressableTextEditorHighlightingP
 
     for (PsiElement element : hosts) {
       progress.checkCanceled();
-      final List<PsiFile> destination = myPriorityRange.contains(element.getTextRange()) ? inside : outside;
+      final List<PsiFile> destination = myPriorityRange.intersects(element.getTextRange()) ? inside : outside;
       InjectedLanguageUtil.enumerate(element, myFile, new PsiLanguageInjectionHost.InjectedPsiVisitor() {
         public void visit(@NotNull PsiFile injectedPsi, @NotNull List<PsiLanguageInjectionHost.Shred> places) {
           destination.add(injectedPsi); // for concatenations there can be many injection hosts with only one injected PSI
