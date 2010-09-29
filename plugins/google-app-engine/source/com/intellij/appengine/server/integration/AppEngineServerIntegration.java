2915127050975970789ca4ec33a287304873dec4
nik <Nikolay.Chashnikov@jetbrains.com>
2010-09-29 13:26:10 +0400

    fixed too long combobox for app server selection in new module wizard

--- plugins/google-app-engine/source/com/intellij/appengine/server/integration/AppEngineServerIntegration.java
+++ plugins/google-app-engine/source/com/intellij/appengine/server/integration/AppEngineServerIntegration.java
@@ -1,17 +1,13 @@
 package com.intellij.appengine.server.integration;
 
-import com.intellij.facet.FacetTypeId;
+import com.intellij.appengine.util.AppEngineUtil;
 import com.intellij.javaee.appServerIntegrations.AppServerIntegration;
 import com.intellij.javaee.appServerIntegrations.ApplicationServerHelper;
-import com.intellij.javaee.facet.JavaeeFacet;
-import com.intellij.javaee.facet.JavaeeFacetUtil;
-import com.intellij.javaee.web.facet.WebFacet;
+import com.intellij.javaee.appServerIntegrations.ApplicationServerPersistentDataEditor;
 import com.intellij.javaee.openapi.ex.AppServerIntegrationsManager;
-import com.intellij.appengine.util.AppEngineUtil;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
-import java.util.Collection;
 
 /**
  * @author nik
@@ -33,6 +29,11 @@ public class AppEngineServerIntegration extends AppServerIntegration {
 
   public String getPresentableName() {
     return "Google App Engine Dev Server";
+  }
+
+  @Override
+  public ApplicationServerPersistentDataEditor createNewServerEditor() {
+    return myServerHelper.createConfigurable();
   }
 
   @NotNull
