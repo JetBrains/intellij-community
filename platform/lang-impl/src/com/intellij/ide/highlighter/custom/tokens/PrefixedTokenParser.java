6c0da2b0cf8308d87ae9da2227127977b10c63f0
peter.gromov <peter@jetbrains.com>
2010-09-30 19:37:41 +0400

    TokenParser interface replaced with its BaseTokenParser implementation; why have superfluous code?

--- platform/lang-impl/src/com/intellij/ide/highlighter/custom/tokens/PrefixedTokenParser.java
+++ platform/lang-impl/src/com/intellij/ide/highlighter/custom/tokens/PrefixedTokenParser.java
@@ -21,7 +21,7 @@ import com.intellij.psi.tree.IElementType;
 /**
  * @author dsl
  */
-public abstract class PrefixedTokenParser extends BaseTokenParser {
+public abstract class PrefixedTokenParser extends TokenParser {
   private final char[] myPrefix;
   private final IElementType myTokenType;
 
--- platform/lang-impl/src/com/intellij/ide/highlighter/custom/tokens/TokenParser.java
+++ platform/lang-impl/src/com/intellij/ide/highlighter/custom/tokens/TokenParser.java
@@ -16,14 +16,27 @@
 
 package com.intellij.ide.highlighter.custom.tokens;
 
-import com.intellij.ide.highlighter.custom.tokens.TokenInfo;
 
 /**
  * @author dsl
  */
-public interface TokenParser {
-  void setBuffer(CharSequence buffer, int startOffset, int endOffset);
-  boolean hasToken(int position);
-  void getTokenInfo(TokenInfo info);
-  int getSmartUpdateShift();
+public abstract class TokenParser {
+  protected CharSequence myBuffer;
+  protected int myStartOffset;
+  protected int myEndOffset;
+  protected final TokenInfo myTokenInfo = new TokenInfo();
+
+  public void setBuffer(CharSequence buffer, int startOffset, int endOffset) {
+    myBuffer = buffer;
+    myStartOffset = startOffset;
+    myEndOffset = endOffset;
+  }
+
+  public void getTokenInfo(TokenInfo info) {
+    info.updateData(myTokenInfo);
+  }
+
+  public abstract boolean hasToken(int position);
+
+  public abstract int getSmartUpdateShift();
 }
