38623bbe77ae0b5ae0c08b387f21e0b45d313e91
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2010-10-01 16:04:39 +0400

    tests fixed

--- xml/impl/src/com/intellij/xml/impl/dtd/XmlElementDescriptorImpl.java
+++ xml/impl/src/com/intellij/xml/impl/dtd/XmlElementDescriptorImpl.java
@@ -105,7 +105,7 @@ public class XmlElementDescriptorImpl extends BaseXmlElementDescriptorImpl imple
 
   // Read-only action
   protected final XmlElementDescriptor[] doCollectXmlDescriptors(final XmlTag context) {
-    final Set<XmlElementDescriptor> result = new HashSet<XmlElementDescriptor>();
+    final LinkedHashSet<XmlElementDescriptor> result = new LinkedHashSet<XmlElementDescriptor>();
     final XmlElementContentSpec contentSpecElement = myElementDecl.getContentSpecElement();
     final XmlNSDescriptor nsDescriptor = getNSDescriptor();
     final XmlNSDescriptor NSDescriptor = nsDescriptor != null? nsDescriptor:getNsDescriptorFrom(context);
