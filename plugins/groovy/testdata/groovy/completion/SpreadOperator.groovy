cc37493fa81bc2f7bb9b04b22c5ce87123ba773f
Maxim Medvedev <maxim.medvedev@jetbrains.com>
2010-10-01 13:57:08 +0400

    IDEA-58799    *. loses type of element for collections other than List

---
+++ plugins/groovy/testdata/groovy/completion/SpreadOperator.groovy
@@ -0,0 +1,7 @@
+class Foo {
+  def foo1(){}
+  def foo2(){}
+}
+
+def collection = new Collection<Foo>() {}
+collection*.fo<caret>
---
+++ plugins/groovy/testdata/resolve/method/spreadOperatorNotList/A.groovy
@@ -0,0 +1,7 @@
+class Foo {
+  def foo(){}
+}
+
+def col = new ArrayList<Foo>()
+
+col*.fo<ref>o()
