57a33492f21883add02546de8ba8a20ce578cd8a
Denis Zhdanov <Denis.Zhdanov@jetbrains.com>
2010-10-06 17:50:49 +0400

    IDEA-59220 IAE at com.intellij.openapi.editor.impl.DocumentImpl.a
    All debug info is commented

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/DocumentImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/DocumentImpl.java
@@ -470,7 +470,7 @@ public class DocumentImpl extends UserDataHolderBase implements DocumentEx {
     DocumentEvent event = new DocumentEventImpl(this, offset, oldString, newString, myModificationStamp, wholeTextReplaced);
     //System.out.printf("%nbefore change: offset=%d, old text='%s', new text='%s', document modification stamp=%d%nDocument:'%s'%n",
     //                  event.getOffset(), event.getOldFragment(), event.getNewFragment(), event.getDocument().getModificationStamp(),
-                      event.getDocument().getText());
+    //                  event.getDocument().getText());
     for (int i = myDocumentListeners.size() - 1; i >= 0; i--) {
       try {
         myDocumentListeners.get(i).beforeDocumentChange(event);
