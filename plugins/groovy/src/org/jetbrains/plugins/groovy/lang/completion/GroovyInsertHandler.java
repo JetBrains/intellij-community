0d1472b958ff2cb0432b6f37daa9627fcd1975d5
Maxim Medvedev <maxim.medvedev@jetbrains.com>
2010-10-07 14:49:22 +0400

    IDEA-59367    When completing top-level method calls, insert nothing instead of space and '('

--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/completion/GroovyInsertHandler.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/completion/GroovyInsertHandler.java
@@ -30,11 +30,13 @@ import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.plugins.groovy.lang.parser.GroovyElementTypes;
 import org.jetbrains.plugins.groovy.lang.psi.GrControlFlowOwner;
+import org.jetbrains.plugins.groovy.lang.psi.GroovyFile;
 import org.jetbrains.plugins.groovy.lang.psi.GroovyPsiElement;
 import org.jetbrains.plugins.groovy.lang.psi.api.auxiliary.modifiers.annotation.GrAnnotationNameValuePair;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrExpression;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrNewExpression;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrReferenceExpression;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.members.GrMethod;
 import org.jetbrains.plugins.groovy.lang.psi.api.toplevel.imports.GrImportStatement;
 import org.jetbrains.plugins.groovy.lang.psi.api.types.GrCodeReferenceElement;
 import org.jetbrains.plugins.groovy.lang.psi.util.PsiUtil;
@@ -86,7 +88,14 @@ public class GroovyInsertHandler implements InsertHandler<LookupElement> {
       PsiDocumentManager docManager = PsiDocumentManager.getInstance(method.getProject());
       docManager.commitDocument(document);
       PsiFile psiFile = docManager.getPsiFile(document);
-      if (isExpressionStatement(psiFile, context.getStartOffset()) && PsiType.VOID.equals(PsiUtil.getSmartReturnType(method)) && '(' != context.getCompletionChar() && parameters.length > 0) {
+      if (method.getParameterList().getParametersCount() > 0 && isTopLevelStatement(psiFile, context.getStartOffset())) {
+        return;
+      }
+      if (isExpressionStatement(psiFile, context.getStartOffset()) &&
+          (PsiType.VOID.equals(PsiUtil.getSmartReturnType(method)) ||
+           method instanceof GrMethod && ((GrMethod)method).getReturnTypeElementGroovy() == null) &&
+          '(' != context.getCompletionChar() &&
+          parameters.length > 0) {
         TailType.insertChar(editor, offset, ' ');
         return;
       }
@@ -129,6 +138,14 @@ public class GroovyInsertHandler implements InsertHandler<LookupElement> {
     }
 
     addTailType(item).processTail(context.getEditor(), context.getTailOffset());
+  }
+
+  private static boolean isTopLevelStatement(PsiFile psiFile, int offset) {
+    PsiElement elementAt = psiFile.findElementAt(offset);
+    if (elementAt == null) return false;
+    GrExpression expr = PsiTreeUtil.getParentOfType(elementAt, GrExpression.class);
+    if (expr == null) return false;
+    return expr.getParent() instanceof GroovyFile;
   }
 
   private static boolean isExpressionStatement(PsiFile psiFile, int offset) {
