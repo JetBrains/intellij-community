45a119d2683d2c2145eddec7c270a15be7fdf272
nik <Nikolay.Chashnikov@jetbrains.com>
2010-10-12 17:17:25 +0400

    extracted api for socket connection

---
+++ platform/platform-impl/src/com/intellij/util/io/socketConnection/ConnectionStatus.java
@@ -0,0 +1,8 @@
+package com.intellij.util.io.socketConnection;
+
+/**
+ * @author nik
+ */
+public enum ConnectionStatus {
+  NOT_CONNECTED, WAITING_FOR_CONNECTION, CONNECTED, DISCONNECTED, CONNECTION_FAILED
+}
