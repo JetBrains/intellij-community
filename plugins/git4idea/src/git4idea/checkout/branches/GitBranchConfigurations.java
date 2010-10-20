aa3cc8bc5a3a11ef2560679ae9f9fcaee7cd72b3
Kirill Likhodedov <kirill.likhodedov@jetbrains.com>
2010-10-20 16:29:19 +0400

    IDEA-59569 If current branch configuration wasn't once detected (for example, because git executable wasn't configured yet at that time), it wouldn't update anymore. Fixed to update it if it's null on GitBranchConfigurations component activation.

--- plugins/git4idea/src/git4idea/checkout/branches/GitBranchConfigurations.java
+++ plugins/git4idea/src/git4idea/checkout/branches/GitBranchConfigurations.java
@@ -499,23 +499,15 @@ public class GitBranchConfigurations implements PersistentStateComponent<GitBran
    */
   private void detectLocals() throws VcsException {
     synchronized (myStateLock) {
-      HashMap<VirtualFile, String> currents = new HashMap<VirtualFile, String>();
+      final HashMap<VirtualFile, String> rootToCurrentBranch = new HashMap<VirtualFile, String>();
       for (VirtualFile root : myGitRoots) {
         GitBranch current = GitBranch.current(myProject, root);
-        currents.put(root, current == null ? "" : current.getName());
+        rootToCurrentBranch.put(root, current == null ? "" : current.getName());
       }
+
       if (myConfigurations.isEmpty()) {
         detectLocalConfigurations(true);
-        for (GitBranchConfiguration configuration : myConfigurations.values()) {
-          boolean currentsMatched = true;
-          for (VirtualFile root : myGitRoots) {
-            currentsMatched &= currents.get(root).equals(configuration.getReference(root.getPath()));
-          }
-          if (currentsMatched) {
-            myCurrentConfiguration = configuration;
-            break;
-          }
-        }
+        updateCurrentConfiguration(rootToCurrentBranch);
         if (myCurrentConfiguration == null) {
           // the configuration does not matches any standard, there could be no configurations with spaces at this point
           // since it is not allowed branch name.
@@ -539,9 +531,29 @@ public class GitBranchConfigurations implements PersistentStateComponent<GitBran
             c.setReference(root.getPath(), describeRoot(root));
           }
         }
+      } else if (myCurrentConfiguration == null) {
+        updateCurrentConfiguration(rootToCurrentBranch);
       }
       fireCurrentConfigurationChanged();
       fireConfigurationsChanged();
+    }
+  }
+
+  /**
+   * Updates myCurrentConfiguration (displayed in the GitBranchesWidget) to the proper value from the list
+   * of all myConfigurations.
+   * @param rootToCurrentBranch Mapping from a git root to the name of current branch selected on this root.
+   */
+  private void updateCurrentConfiguration(HashMap<VirtualFile, String> rootToCurrentBranch) {
+    for (GitBranchConfiguration configuration : myConfigurations.values()) {
+      boolean currentsMatched = true;
+      for (VirtualFile root : myGitRoots) {
+        currentsMatched &= rootToCurrentBranch.get(root).equals(configuration.getReference(root.getPath()));
+      }
+      if (currentsMatched) {
+        myCurrentConfiguration = configuration;
+        break;
+      }
     }
   }
 
