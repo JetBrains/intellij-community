f971df97b26daf3ab019b566c21309c18f08e664
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2010-10-20 17:51:43 +0400

    django run: invocation of custom manage.py, usage of settings.py path from settings

--- python/helpers/pycharm/django_manage.py
+++ python/helpers/pycharm/django_manage.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 from pycharm.fix_getpass import fixGetpass
-from django.core.management import execute_manager
 from pycharm import django_settings
+import os
+from runpy import run_module
+
+manage_file = os.getenv('PYCHARM_DJANGO_MANAGE_MODULE')
+if not manage_file:
+    manage_file = 'manage'
 
 if __name__ == "__main__":
     fixGetpass()
-    execute_manager(django_settings)
+    run_module(manage_file)
