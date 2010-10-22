03bdfdaf6dfc9f1d63e656aacfca29d4dfe67dce
nik <Nikolay.Chashnikov@jetbrains.com>
2010-10-22 16:06:19 +0400

    IDEA-60099: Allow to disable default browser to run automatically in GWT projects and/or allow to set default one

---
+++ xml/impl/src/com/intellij/ide/browsers/BrowserSelector.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2000-2010 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.ide.browsers;
+
+import com.intellij.ide.BrowserSettings;
+import com.intellij.ide.ui.ListCellRendererWrapper;
+import com.intellij.openapi.options.ShowSettingsUtil;
+import com.intellij.openapi.util.IconLoader;
+import com.intellij.ui.ComboboxWithBrowseButton;
+import com.intellij.util.ArrayUtil;
+import com.intellij.util.Icons;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author nik
+ */
+public class BrowserSelector {
+  private ComboboxWithBrowseButton myBrowserComboWithBrowse;
+  private boolean myAllowDefaultBrowser;
+
+  public BrowserSelector(boolean allowDefaultBrowser) {
+    myAllowDefaultBrowser = allowDefaultBrowser;
+    myBrowserComboWithBrowse = new ComboboxWithBrowseButton();
+    myBrowserComboWithBrowse.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        final ShowSettingsUtil util = ShowSettingsUtil.getInstance();
+        util.editConfigurable(myBrowserComboWithBrowse, new BrowserSettings());
+
+        final BrowsersConfiguration.BrowserFamily selectedItem = getSelectedBrowser();
+        initBrowsersComboModel();
+        if (selectedItem != null) {
+          setSelectedBrowser(selectedItem);
+        }
+      }
+    });
+
+    final JComboBox comboBox = myBrowserComboWithBrowse.getComboBox();
+    comboBox.setRenderer(new ListCellRendererWrapper<BrowsersConfiguration.BrowserFamily>(comboBox.getRenderer()) {
+      @Override
+      public void customize(JList list,
+                            BrowsersConfiguration.BrowserFamily value,
+                            int index,
+                            boolean selected,
+                            boolean hasFocus) {
+        final Icon baseIcon = value != null ? value.getIcon() : Icons.WEB_ICON;
+        final Icon icon = myBrowserComboWithBrowse.isEnabled() ? baseIcon : IconLoader.getDisabledIcon(baseIcon);
+        setIcon(icon);
+        setText(value != null ? value.getName() : "Default");
+      }
+    });
+
+    initBrowsersComboModel();
+  }
+
+  public JComponent getMainComponent() {
+    return myBrowserComboWithBrowse;
+  }
+
+  private void initBrowsersComboModel() {
+    final BrowsersConfiguration.BrowserFamily[] browsers = BrowsersConfiguration.BrowserFamily.values();
+    final List<BrowsersConfiguration.BrowserFamily> activeBrowsers = new ArrayList<BrowsersConfiguration.BrowserFamily>();
+    if (myAllowDefaultBrowser) {
+      activeBrowsers.add(null);
+    }
+    final BrowsersConfiguration browsersConfiguration = BrowsersConfiguration.getInstance();
+    for (final BrowsersConfiguration.BrowserFamily browser : browsers) {
+      if (browsersConfiguration.getBrowserSettings(browser).isActive()) {
+        activeBrowsers.add(browser);
+      }
+    }
+
+    myBrowserComboWithBrowse.getComboBox().setModel(new DefaultComboBoxModel(ArrayUtil.toObjectArray(activeBrowsers)));
+  }
+
+  @Nullable
+  public BrowsersConfiguration.BrowserFamily getSelectedBrowser() {
+    return (BrowsersConfiguration.BrowserFamily)myBrowserComboWithBrowse.getComboBox().getSelectedItem();
+  }
+
+  @Nullable
+  public String getSelectedBrowserFamilyName() {
+    final BrowsersConfiguration.BrowserFamily browser = getSelectedBrowser();
+    return browser != null ? browser.getName() : null;
+  }
+
+  public void setSelectedBrowser(@Nullable BrowsersConfiguration.BrowserFamily selectedItem) {
+    myBrowserComboWithBrowse.getComboBox().setSelectedItem(selectedItem);
+  }
+}
