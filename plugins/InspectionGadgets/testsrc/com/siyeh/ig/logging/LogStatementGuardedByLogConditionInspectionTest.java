9b976363b4935977c4a9ddc992577119aa1a00ae
Bas Leijdekkers <leijdekkers@carp-technologies.nl>
2010-11-05 21:44:51 +0100

    test for IDEA-60701

---
+++ plugins/InspectionGadgets/testsrc/com/siyeh/ig/logging/LogStatementGuardedByLogConditionInspectionTest.java
@@ -0,0 +1,11 @@
+package com.siyeh.ig.logging;
+
+import com.IGInspectionTestCase;
+
+public class LogStatementGuardedByLogConditionInspectionTest extends IGInspectionTestCase {
+
+    public void test() throws Exception {
+        doTest("com/siyeh/igtest/logging/log_statement_guarded_by_log_condition",
+                new LogStatementGuardedByLogConditionInspection());
+    }
+}
