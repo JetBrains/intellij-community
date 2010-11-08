acecbf7f1c42325c8d7000cf9d4ce52fd1e9501e
Rustam.Vishnyakov <rustam.vishnyakov@gmail.com>
2010-11-08 16:30:55 +0300

    Allow editing JavaScript libraries only from Project Structure if possible

---
+++ platform/lang-impl/src/com/intellij/ide/scriptingContext/ui/ScriptingLibrariesPanelStub.java
@@ -0,0 +1,23 @@
+package com.intellij.ide.scriptingContext.ui;
+
+import javax.swing.*;
+
+/**
+ * @author Rustam Vishnyakov
+ */
+public class ScriptingLibrariesPanelStub {
+  private JPanel myMainPanel;
+
+  public JComponent getPanel() {
+    return myMainPanel;
+  }
+
+
+  public boolean isModified() {
+    return false;
+  }
+
+  public void resetTable() {
+    // Do nothing
+  }
+}
