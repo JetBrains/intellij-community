4a03d33c3a93498d7914065f4888f4858a5a9ea7
Alexey Kudravtsev <cdr@intellij.com>
2010-11-11 12:33:19 +0300

    IDEA-55575 _

--- plugins/structuralsearch/source/com/intellij/structuralsearch/impl/matcher/XmlMatchingVisitor.java
+++ plugins/structuralsearch/source/com/intellij/structuralsearch/impl/matcher/XmlMatchingVisitor.java
@@ -1,6 +1,5 @@
 package com.intellij.structuralsearch.impl.matcher;
 
-import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiWhiteSpace;
 import com.intellij.psi.XmlElementVisitor;
@@ -19,7 +18,7 @@ public class XmlMatchingVisitor extends XmlElementVisitor {
   private final GlobalMatchingVisitor myMatchingVisitor;
 
   public XmlMatchingVisitor(GlobalMatchingVisitor matchingVisitor) {
-    this.myMatchingVisitor = matchingVisitor;
+    myMatchingVisitor = matchingVisitor;
   }
 
   @Override
@@ -31,7 +30,7 @@ public class XmlMatchingVisitor extends XmlElementVisitor {
     final XmlAttribute another = (XmlAttribute)myMatchingVisitor.getElement();
     final boolean isTypedVar = myMatchingVisitor.getMatchContext().getPattern().isTypedVar(attribute.getName());
 
-    myMatchingVisitor.setResult((attribute.getName().equals(another.getName()) || isTypedVar));
+    myMatchingVisitor.setResult(attribute.getName().equals(another.getName()) || isTypedVar);
     if (myMatchingVisitor.getResult()) {
       myMatchingVisitor.setResult(myMatchingVisitor.match(attribute.getValueElement(), another.getValueElement()));
     }
@@ -44,18 +43,18 @@ public class XmlMatchingVisitor extends XmlElementVisitor {
 
   @Override public void visitXmlAttributeValue(XmlAttributeValue value) {
     final XmlAttributeValue another = (XmlAttributeValue)myMatchingVisitor.getElement();
-    final String text = StringUtil.stripQuotesAroundValue( value.getText() );
+    final String text = value.getValue();
 
     final boolean isTypedVar = myMatchingVisitor.getMatchContext().getPattern().isTypedVar(text);
     MatchingHandler handler;
 
     if (isTypedVar && (handler = myMatchingVisitor.getMatchContext().getPattern().getHandler( text )) instanceof SubstitutionHandler) {
       String text2 = another.getText();
-      int offset = (text2.length() > 0 && ( text2.charAt(0) == '"' || text2.charAt(0) == '\''))? 1:0;
+      int offset = text2.length() > 0 && ( text2.charAt(0) == '"' || text2.charAt(0) == '\'') ? 1:0;
       myMatchingVisitor.setResult(((SubstitutionHandler)handler).handle(another, offset, text2.length() - offset,
                                                                         myMatchingVisitor.getMatchContext()));
     } else {
-      myMatchingVisitor.setResult(text.equals(StringUtil.stripQuotesAroundValue(another.getText())));
+      myMatchingVisitor.setResult(text.equals(another.getValue()));
     }
   }
 
