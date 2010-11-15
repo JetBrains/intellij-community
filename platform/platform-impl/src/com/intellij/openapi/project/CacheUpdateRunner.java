f98b3bf3260aa408a5469e02e3e9aa37fa347c3f
peter <peter.gromov@jetbrains.com>
2010-11-15 23:52:20 +0300

    disable multithreaded indexing

--- platform/platform-impl/src/com/intellij/openapi/project/CacheUpdateRunner.java
+++ platform/platform-impl/src/com/intellij/openapi/project/CacheUpdateRunner.java
@@ -138,7 +138,7 @@ class CacheUpdateRunner {
       if (threadsCount == -1) {
         threadsCount = Math.min(Runtime.getRuntime().availableProcessors(), ApplicationManager.getApplication().isUnitTestMode() ? 1 : 2);
       }
-      if (threadsCount == 1) {
+      if (true) {
         Runnable process = new MyRunnable(innerIndicator, queue, isFinished, progressUpdater, processInReadAction, application);
         ProgressManager.getInstance().runProcess(process, innerIndicator);
       }
