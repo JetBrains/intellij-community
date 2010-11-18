707a2c2d4218aa2acf655184430ebdc9d9054ccc
peter.gromov <peter@jetbrains.com>
2010-11-18 21:09:16 +0300

    more realistic \n\t typing in tests

--- platform/lang-impl/src/com/intellij/codeInsight/lookup/impl/actions/ChooseItemReplaceAction.java
+++ platform/lang-impl/src/com/intellij/codeInsight/lookup/impl/actions/ChooseItemReplaceAction.java
@@ -26,7 +26,6 @@ import com.intellij.codeInsight.template.impl.TemplateImpl;
 import com.intellij.codeInsight.template.impl.TemplateSettings;
 import com.intellij.featureStatistics.FeatureUsageTracker;
 import com.intellij.openapi.actionSystem.DataContext;
-import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.actionSystem.EditorAction;
 import com.intellij.openapi.editor.actionSystem.EditorWriteActionHandler;
@@ -45,6 +44,26 @@ public class ChooseItemReplaceAction extends EditorAction {
       assert lookup != null;
       lookup.finishLookup(Lookup.REPLACE_SELECT_CHAR);
     }
+
+    @Override
+    public boolean isEnabled(Editor editor, DataContext dataContext) {
+      LookupImpl lookup = (LookupImpl)LookupManager.getActiveLookup(editor);
+      if (lookup != null) {
+        if (hasTemplatePrefix(lookup, TemplateSettings.TAB_CHAR)) {
+          return false;
+        }
+
+        if (lookup.isFocused()) {
+          return true;
+        }
+
+        if (lookup.isCompletion()) {
+          lookup.refreshUi(); // to bring the list model up to date
+          return !lookup.getItems().isEmpty();
+        }
+      }
+      return false;
+    }
   }
 
   static boolean hasTemplatePrefix(LookupImpl lookup, char shortcutChar) {
@@ -60,14 +79,4 @@ public class ChooseItemReplaceAction extends EditorAction {
     return template != null && shortcutChar == TemplateSettings.getInstance().getShortcutChar(template);
   }
 
-  public void update(Editor editor, Presentation presentation, DataContext dataContext){
-    LookupImpl lookup = (LookupImpl)LookupManager.getActiveLookup(editor);
-    if (lookup != null) {
-      lookup.refreshUi(); // to bring the list model up to date
-      presentation.setEnabled((lookup.isFocused() || lookup.isCompletion() && !lookup.getItems().isEmpty()) &&
-                              !hasTemplatePrefix(lookup, TemplateSettings.TAB_CHAR));
-    } else {
-      presentation.setEnabled(false);
-    }
-  }
 }
