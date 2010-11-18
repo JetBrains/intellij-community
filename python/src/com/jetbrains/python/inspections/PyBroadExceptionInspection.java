72867bd2a7a045e3d6ff566b70006391affe07d3
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2010-11-18 18:54:47 +0300

    fixed highlighting in BroadException inspection

--- python/src/com/jetbrains/python/inspections/PyBroadExceptionInspection.java
+++ python/src/com/jetbrains/python/inspections/PyBroadExceptionInspection.java
@@ -38,14 +38,14 @@ public class PyBroadExceptionInspection extends PyInspection {
     public void visitPyExceptBlock(final PyExceptPart node){
       PyExpression exceptClass= node.getExceptClass();
       if (exceptClass == null) {
-        registerProblem(node, "Too broad exception clause");
+        registerProblem(node.getFirstChild(), "Too broad exception clause");
       }
       if (exceptClass instanceof PyReferenceExpression) {
         PyReferenceExpression exceptClassRef = (PyReferenceExpression)exceptClass;
         PyType classRefType = myTypeEvalContext.getType(exceptClassRef);
         if (classRefType != null) {
           if (classRefType.isBuiltin())
-            registerProblem(node, "Too broad exception clause");
+            registerProblem(exceptClassRef, "Too broad exception clause");
         }
       }
 
