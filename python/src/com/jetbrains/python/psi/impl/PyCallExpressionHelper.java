e2fe7d668862d799b3abf6f951c44cf6047390e1
Dmitry Jemerov <yole@jetbrains.com>
2010-11-19 14:49:50 +0300

    typo fix: AIOOBE when mapping arguments

--- python/src/com/jetbrains/python/psi/impl/PyCallExpressionHelper.java
+++ python/src/com/jetbrains/python/psi/impl/PyCallExpressionHelper.java
@@ -152,7 +152,7 @@ public class PyCallExpressionHelper {
       final Callable callable = (Callable)resolved;
       int implicitOffset = getImplicitArgumentCount(callable, wrappedFlag, flags, is_by_instance);
       if (!isConstructorCall && PyNames.NEW.equals(callable.getName())) {
-        implicitOffset = Math.min(implicitOffset - 1, 0); // case of Class.__new__
+        implicitOffset = Math.max(implicitOffset - 1, 0); // case of Class.__new__
       }
       return new PyCallExpression.PyMarkedCallee(callable, flags, implicitOffset,
                                                  resolveResult != null ? resolveResult.isImplicit() : false);
