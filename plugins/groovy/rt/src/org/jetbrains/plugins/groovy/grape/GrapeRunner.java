111725a5c00b03a9190f7ba3d8f846063f3f9d44
peter.gromov <peter@jetbrains.com>
2010-11-22 20:19:39 +0300

    a more user-friendly message when @Grab dependency is not resolved

--- plugins/groovy/rt/src/org/jetbrains/plugins/groovy/grape/GrapeRunner.java
+++ plugins/groovy/rt/src/org/jetbrains/plugins/groovy/grape/GrapeRunner.java
@@ -1,8 +1,12 @@
 package org.jetbrains.plugins.groovy.grape;
 
 import groovy.lang.GroovyShell;
+import org.codehaus.groovy.control.MultipleCompilationErrorsException;
+import org.codehaus.groovy.control.messages.ExceptionMessage;
 
 import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * @author peter
@@ -17,6 +21,22 @@ public class GrapeRunner {
     final GroovyShell shell = new GroovyShell();
     try {
       shell.parse(args[0] + " import java.lang.*");
+    }
+    catch (MultipleCompilationErrorsException e) {
+      List errors = e.getErrorCollector().getErrors();
+      for (Iterator iterator = errors.iterator(); iterator.hasNext();) {
+        Object o = iterator.next();
+        if (o instanceof ExceptionMessage) {
+          Exception cause = ((ExceptionMessage)o).getCause();
+          String message = cause.getMessage();
+          if (message != null && message.startsWith("Error grabbing Grapes")) {
+            System.out.println(message);
+            return;
+          }
+        }
+      }
+      e.printStackTrace();
+      return;
     }
     catch (Throwable e) {
       e.printStackTrace();
