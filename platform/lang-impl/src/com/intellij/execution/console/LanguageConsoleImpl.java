97dc0c7efdead5ec5d44339ae31e810264bbba60
Gregory Shrago <Gregory.Shrago@jetbrains.com>
2010-11-24 17:27:13 +0300

    EA-23830 - IAE: RangeMarkerImpl.<init>

--- platform/lang-impl/src/com/intellij/execution/console/LanguageConsoleImpl.java
+++ platform/lang-impl/src/com/intellij/execution/console/LanguageConsoleImpl.java
@@ -369,12 +369,17 @@ public class LanguageConsoleImpl implements Disposable, TypeSafeDataProvider {
      //offset can be changed after text trimming after insert due to buffer constraints
     appendToHistoryDocument(history, text);
     int offset = history.getTextLength() - text.length();
-    final HighlighterIterator iterator = consoleEditor.getHighlighter().createIterator(0);
+    final int localOffset = textRange.getStartOffset();
+    final HighlighterIterator iterator = consoleEditor.getHighlighter().createIterator(localOffset);
+    final int localEndOffset = textRange.getEndOffset();
     while (!iterator.atEnd()) {
-      final int localOffset = textRange.getStartOffset();
-      final int start = Math.max(iterator.getStart(), localOffset) - localOffset;
-      final int end = Math.min(iterator.getEnd(), textRange.getEndOffset()) - localOffset;
-      markupModel.addRangeHighlighter(start + offset, end + offset, HighlighterLayer.SYNTAX, iterator.getTextAttributes(),
+      final int itStart = iterator.getStart();
+      if (itStart > localEndOffset) break;
+      final int itEnd = iterator.getEnd();
+      if (itEnd < localOffset) continue;
+      final int start = Math.max(itStart, localOffset) - localOffset + offset;
+      final int end = Math.min(itEnd, localEndOffset) - localOffset + offset;
+      markupModel.addRangeHighlighter(start, end, HighlighterLayer.SYNTAX, iterator.getTextAttributes(),
                                       HighlighterTargetArea.EXACT_RANGE);
 
       iterator.advance();
