6d9f136d30b2570c01ccb989512358601a64ca42
Dmitry Jemerov <yole@jetbrains.com>
2010-12-01 22:15:15 +0300

    take types from type annotations (PY-1750)

--- python/src/com/jetbrains/python/psi/PyAnnotation.java
+++ python/src/com/jetbrains/python/psi/PyAnnotation.java
@@ -1,7 +1,13 @@
 package com.jetbrains.python.psi;
 
+import org.jetbrains.annotations.Nullable;
+
 /**
  * @author yole
  */
 public interface PyAnnotation extends PyElement {
+  PyExpression getValue();
+
+  @Nullable
+  PyClass resolveToClass();
 }
--- python/src/com/jetbrains/python/psi/impl/PyAnnotationImpl.java
+++ python/src/com/jetbrains/python/psi/impl/PyAnnotationImpl.java
@@ -1,7 +1,12 @@
 package com.jetbrains.python.psi.impl;
 
 import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiPolyVariantReference;
 import com.jetbrains.python.psi.PyAnnotation;
+import com.jetbrains.python.psi.PyClass;
+import com.jetbrains.python.psi.PyExpression;
+import com.jetbrains.python.psi.PyReferenceExpression;
 
 /**
  * @author yole
@@ -9,5 +14,23 @@ import com.jetbrains.python.psi.PyAnnotation;
 public class PyAnnotationImpl extends PyElementImpl implements PyAnnotation {
   public PyAnnotationImpl(ASTNode astNode) {
     super(astNode);
+  }
+
+  @Override
+  public PyExpression getValue() {
+    return findChildByClass(PyExpression.class);
+  }
+
+  @Override
+  public PyClass resolveToClass() {
+    PyExpression expr = getValue();
+    if (expr instanceof PyReferenceExpression) {
+      final PsiPolyVariantReference reference = ((PyReferenceExpression)expr).getReference();
+      final PsiElement result = reference.resolve();
+      if (result instanceof PyClass) {
+        return (PyClass) result;
+      }
+    }
+    return null;
   }
 }
