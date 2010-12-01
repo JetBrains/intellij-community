71785b943681d720c638011e12994a9ee653a24a
Dmitry Jemerov <yole@jetbrains.com>
2010-12-01 22:24:54 +0300

    no completion after 'as' in import statements (PY-2384)

--- python/src/com/jetbrains/python/psi/impl/PyTargetReference.java
+++ python/src/com/jetbrains/python/psi/impl/PyTargetReference.java
@@ -2,6 +2,9 @@ package com.jetbrains.python.psi.impl;
 
 import com.intellij.psi.PsiElementResolveResult;
 import com.intellij.psi.ResolveResult;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.util.ArrayUtil;
+import com.jetbrains.python.psi.PyImportElement;
 import com.jetbrains.python.psi.PyQualifiedExpression;
 import com.jetbrains.python.psi.resolve.PyResolveContext;
 import org.jetbrains.annotations.NotNull;
@@ -23,5 +26,16 @@ public class PyTargetReference extends PyReferenceImpl {
     }
     // resolve to self if no other target found
     return new ResolveResult[] { new PsiElementResolveResult(myElement) };
+  }
+
+  @NotNull
+  @Override
+  public Object[] getVariants() {
+    final PyImportElement importElement = PsiTreeUtil.getParentOfType(myElement, PyImportElement.class);
+    // reference completion is useless in 'as' part of import statement (PY-2384)
+    if (importElement != null && myElement == importElement.getAsNameElement()) {
+      return ArrayUtil.EMPTY_OBJECT_ARRAY;
+    }
+    return super.getVariants();
   }
 }
