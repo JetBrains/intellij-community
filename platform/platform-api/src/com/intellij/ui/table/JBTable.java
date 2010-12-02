b7a9fbfdc6fbd299dd40a935dc81aa4389978256
Oleg Shpynov <oleg.shpynov@jebrains.com>
2010-12-02 18:17:57 +0300

    IDEA-61025 Item Tooltips: weird rendering artifacts in tables when hovering mouse over the cell with an expandable item [r=den]

--- platform/platform-api/src/com/intellij/ui/table/JBTable.java
+++ platform/platform-api/src/com/intellij/ui/table/JBTable.java
@@ -284,10 +284,10 @@ public class JBTable extends JTable implements ComponentWithEmptyText, Component
   public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
     final Component result = super.prepareRenderer(renderer, row, column);
     final boolean selected = myExpandableItemsHandler.getExpandedItems().contains(new TableCell(row, column));
+
     // Fix GTK background
     if (UIUtil.isUnderGTKLookAndFeel()) {
-      final Color background = selected ? UIUtil.getTreeSelectionBackground() : UIUtil.getTreeTextBackground();
-      UIUtil.changeBackGround(this, background);
+      UIUtil.changeBackGround(this, UIUtil.getTreeTextBackground());
     }
 
     if (!selected) return result;
