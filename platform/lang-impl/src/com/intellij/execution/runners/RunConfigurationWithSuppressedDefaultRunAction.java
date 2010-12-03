594be97b275fe928854e69674490276c5305d9b8
nik <Nikolay.Chashnikov@jetbrains.com>
2010-12-03 16:49:43 +0300

    disable 'run' action for ruby and python remote debug configurations [rev by A. Pegov]

--- platform/lang-impl/src/com/intellij/execution/runners/RunConfigurationWithSuppressedDefaultRunAction.java
+++ platform/lang-impl/src/com/intellij/execution/runners/RunConfigurationWithSuppressedDefaultRunAction.java
@@ -13,25 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.intellij.execution.runners;
 
-import com.intellij.execution.configurations.RunProfile;
-import com.intellij.execution.executors.DefaultRunExecutor;
-import org.jetbrains.annotations.NotNull;
-
 /**
- * @author AG
+ * @author nik
  */
-public class BasicProgramRunner extends DefaultProgramRunner {
-
-  @NotNull
-  @Override
-  public String getRunnerId() {
-    return "Basic";
-  }
-
-  public boolean canRun(@NotNull String executorId, @NotNull RunProfile profile) {
-    return DefaultRunExecutor.EXECUTOR_ID.equals(executorId) && !(profile instanceof RunConfigurationWithSuppressedDefaultRunAction);
-  }
+public interface RunConfigurationWithSuppressedDefaultRunAction {
 }
