97513e65eeaac95921bb250023369243651ccc4d
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2010-12-08 16:05:05 +0300

    [rev=yole] attempt to fix vanishing About dialog once again

--- platform/platform-impl/src/com/intellij/ide/actions/AboutAction.java
+++ platform/platform-impl/src/com/intellij/ide/actions/AboutAction.java
@@ -101,7 +101,7 @@ public class AboutAction extends AnAction implements DumbAware {
 
     final long showTime = System.currentTimeMillis();
     //final long delta = Patches.APPLE_BUG_ID_3716865 ? 100 : 0;
-    final long delta = 100; //reproducible on Windows too
+    final long delta = 500; //reproducible on Windows too
 
     dialog.addWindowFocusListener(new WindowFocusListener() {
       public void windowGainedFocus(WindowEvent e) {
