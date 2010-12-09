9606c69e423c6fa517c8d16ff0ce74cd2c553063
Oleg Shpynov <oleg.shpynov@jebrains.com>
2010-12-09 15:17:18 +0300

    refactoring

--- plugins/github/src/org/jetbrains/plugins/github/GithubRebaseAction.java
+++ plugins/github/src/org/jetbrains/plugins/github/GithubRebaseAction.java
@@ -49,7 +49,7 @@ public class GithubRebaseAction extends DumbAwareAction {
 
   public void update(AnActionEvent e) {
     final Project project = e.getData(PlatformDataKeys.PROJECT);
-    if (project == null || project.isDefault()){
+    if (project == null || project.isDefault()) {
       e.getPresentation().setEnabled(false);
       return;
     }
@@ -60,7 +60,7 @@ public class GithubRebaseAction extends DumbAwareAction {
   public void actionPerformed(final AnActionEvent e) {
     final Project project = e.getData(PlatformDataKeys.PROJECT);
     final VirtualFile[] roots = ProjectRootManager.getInstance(project).getContentRoots();
-    if (roots.length == 0){
+    if (roots.length == 0) {
       Messages.showErrorDialog(project, "Project doesn't have any project roots", "Cannot create new GitHub repository");
       return;
     }
@@ -72,34 +72,28 @@ public class GithubRebaseAction extends DumbAwareAction {
       return;
     }
 
-    // Check current branch
     try {
+    // Check current branch
       final GitBranch currentBranch = GitBranch.current(project, root);
-      if (currentBranch == null){
-
+      if (currentBranch == null) {
         Messages.showErrorDialog(project, "Cannot find current branch", "Cannot perform github rebase");
         return;
       }
-      if (!"master".equals(currentBranch.getName())){
-        Messages.showErrorDialog(project, "Cannot perform rebase with '" + currentBranch.getName() + "' branch.\nPlease switch to master", "Cannot perform github rebase");
+      if (!"master".equals(currentBranch.getName())) {
+        Messages.showErrorDialog(project, "Cannot perform rebase with '" + currentBranch.getName() + "' branch.\nPlease switch to master",
+                                 "Cannot perform github rebase");
         return;
       }
-    }
-    catch (VcsException e1) {
-      Messages.showErrorDialog(project, "Error happened during git operation: " + e1.getMessage(), "Cannot perform github rebase");
-      return;
-    }
 
-    // Check that given repository is properly configured git repository
-    GitRemote githubRemote = null;
-    try {
+      // Check that given repository is properly configured git repository
+      GitRemote githubRemote = null;
       final List<GitRemote> gitRemotes = GitRemote.list(project, root);
       if (gitRemotes.isEmpty()) {
         Messages.showErrorDialog(project, "Git repository doesn't have any remotes configured", "Cannot perform github rebase");
         return;
       }
       for (GitRemote gitRemote : gitRemotes) {
-        if (gitRemote.pushUrl().contains("git@github.com")){
+        if (gitRemote.pushUrl().contains("git@github.com")) {
           githubRemote = gitRemote;
           break;
         }
@@ -108,42 +102,44 @@ public class GithubRebaseAction extends DumbAwareAction {
         Messages.showErrorDialog(project, "Configured own github repository is not found", "Cannot perform github rebase");
         return;
       }
-    }
-    catch (VcsException e2) {
-      Messages.showErrorDialog(project, "Error happened during git operation: " + e2.getMessage(), "Cannot perform github rebase");
-      return;
-    }
 
-    final String pushUrl = githubRemote.pushUrl();
-    final String login = GithubSettings.getInstance().getLogin();
-    final int index = pushUrl.lastIndexOf(login);
-    if (index == -1){
-      Messages.showErrorDialog(project, "Github remote repository doesn't seem to be your own repository: " + pushUrl, "Cannot perform github rebase");
-      return;
-    }
-    String repoName = pushUrl.substring(index + login.length() + 1);
-    if (repoName.endsWith(".git")){
-      repoName = repoName.substring(0, repoName.length() - 4);
-    }
+      final String pushUrl = githubRemote.pushUrl();
+      final String login = GithubSettings.getInstance().getLogin();
+      final int index = pushUrl.lastIndexOf(login);
+      if (index == -1) {
+        Messages.showErrorDialog(project, "Github remote repository doesn't seem to be your own repository: " + pushUrl,
+                                 "Cannot perform github rebase");
+        return;
+      }
+      String repoName = pushUrl.substring(index + login.length() + 1);
+      if (repoName.endsWith(".git")) {
+        repoName = repoName.substring(0, repoName.length() - 4);
+      }
 
-    final RepositoryInfo repositoryInfo = GithubUtil.getDetailedRepositoryInfo(project, repoName);
-    if (repositoryInfo == null){
-      Messages.showErrorDialog(project, "Github repository doesn't seem to be your own repository: " + pushUrl, "Cannot perform github rebase");
-      return;
+      final RepositoryInfo repositoryInfo = GithubUtil.getDetailedRepositoryInfo(project, repoName);
+      if (repositoryInfo == null) {
+        Messages
+          .showErrorDialog(project, "Github repository doesn't seem to be your own repository: " + pushUrl, "Cannot perform github rebase");
+        return;
+      }
+
+      if (!repositoryInfo.isFork()) {
+        Messages.showErrorDialog(project, "Github repository '" + repoName + "' is not a forked one", "Cannot perform github rebase");
+        return;
+      }
+
+      final String parent = repositoryInfo.getParent();
+      LOG.assertTrue(parent != null, "Parent repository not found!");
+      final String parentRepoString = "git://github.com/" + parent + ".git";
+
+      BasicAction.saveAll();
+      int i = Messages.showYesNoDialog(project, "Perform rebase relative to '" + parentRepoString + "'?", "Github Rebase", Messages.getQuestionIcon());
+      if (i != Messages.OK) {
+        return;
+      }
     }
-
-    if (!repositoryInfo.isFork()){
-      Messages.showErrorDialog(project, "Github repository '" + repoName + "' is not a forked one", "Cannot perform github rebase");
-      return;
-    }
-
-    final String parent = repositoryInfo.getParent();
-    LOG.assertTrue(parent != null, "Parent repository not found!");
-    final String parentRepoString = "git://github.com/" + parent + ".git";
-
-    BasicAction.saveAll();
-    int i = Messages.showYesNoDialog(project, "Perform rebase relative to '" + parentRepoString + "'?", "Github Rebase", Messages.getQuestionIcon());
-    if (i != Messages.OK){
+    catch (VcsException e1) {
+      Messages.showErrorDialog(project, "Error happened during git operation: " + e1.getMessage(), "Cannot perform github rebase");
       return;
     }
   }
