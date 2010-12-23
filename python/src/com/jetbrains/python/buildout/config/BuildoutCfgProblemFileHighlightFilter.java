7ea8d8990f55b15c3ef00c134fdaad8a5e40e2e3
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2010-12-23 11:44:05 +0300

    added wave error highlighting to buidout.cfg files (PY-2525) (cherry picked from commit a82a5ab946cb78b2d69006412938b8d32fcd839b)

---
+++ python/src/com/jetbrains/python/buildout/config/BuildoutCfgProblemFileHighlightFilter.java
@@ -0,0 +1,17 @@
+package com.jetbrains.python.buildout.config;
+
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.fileTypes.FileTypeManager;
+import com.intellij.openapi.util.Condition;
+import com.intellij.openapi.vfs.VirtualFile;
+
+/**
+ * @author traff
+ */
+public class BuildoutCfgProblemFileHighlightFilter implements Condition<VirtualFile> {
+  @Override
+  public boolean value(VirtualFile virtualFile) {
+    final FileType fileType = FileTypeManager.getInstance().getFileTypeByFile(virtualFile);
+    return fileType == BuildoutCfgFileType.INSTANCE;
+  }
+}
