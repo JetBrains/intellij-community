b2ec0b617f7224a9ad7bc6b9be69cd93518890ae
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2010-12-23 11:40:49 +0300

    reregistering exceptions removed (cherry picked from commit 96a0ca212d634a6874e2f6248bbf43a8b6b36252)

--- python/src/com/jetbrains/python/debugger/PyLineBreakpointHandler.java
+++ python/src/com/jetbrains/python/debugger/PyLineBreakpointHandler.java
@@ -1,5 +1,6 @@
 package com.jetbrains.python.debugger;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.intellij.xdebugger.XSourcePosition;
 import com.intellij.xdebugger.breakpoints.XBreakpointHandler;
@@ -7,6 +8,7 @@ import com.intellij.xdebugger.breakpoints.XBreakpointProperties;
 import com.intellij.xdebugger.breakpoints.XLineBreakpoint;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.Map;
 
 
@@ -19,6 +21,14 @@ public class PyLineBreakpointHandler extends XBreakpointHandler<XLineBreakpoint<
   public PyLineBreakpointHandler(@NotNull final PyDebugProcess debugProcess) {
     super(PyLineBreakpointType.class);
     myDebugProcess = debugProcess;
+  }
+
+  public void reregisterBreakpoints() {
+    List<XLineBreakpoint<XBreakpointProperties>> breakpoints = Lists.newArrayList(myBreakPointPositions.keySet());
+    for (XLineBreakpoint<XBreakpointProperties> breakpoint : breakpoints) {
+      unregisterBreakpoint(breakpoint, false);
+      registerBreakpoint(breakpoint);
+    }
   }
 
   public void registerBreakpoint(@NotNull final XLineBreakpoint<XBreakpointProperties> breakpoint) {
@@ -36,5 +46,4 @@ public class PyLineBreakpointHandler extends XBreakpointHandler<XLineBreakpoint<
       myBreakPointPositions.remove(breakpoint);
     }
   }
-
 }
