ed90415da91ecdb46015d9b62e73e2818ed00723
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2010-12-30 13:47:54 +0300

    fixed PY-2608 Test runner does not draw test hierarchy for django versions < 1.2

--- python/helpers/pycharm/django_test_runner.py
+++ python/helpers/pycharm/django_test_runner.py
@@ -1,5 +1,44 @@
 from pycharm.tcunittest import TeamcityTestRunner
 from django.test.simple import *
+import unittest
+from django.test.testcases import TestCase
+
+def partition_suite(suite, classes, bins):
+    """
+    Partitions a test suite by test type.
+
+    classes is a sequence of types
+    bins is a sequence of TestSuites, one more than classes
+
+    Tests of type classes[i] are added to bins[i],
+    tests with no match found in classes are place in bins[-1]
+    """
+    for test in suite:
+        if isinstance(test, unittest.TestSuite):
+            partition_suite(test, classes, bins)
+        else:
+            for i in range(len(classes)):
+                if isinstance(test, classes[i]):
+                    bins[i].addTest(test)
+                    break
+            else:
+                bins[-1].addTest(test)
+
+def reorder_suite(suite, classes):
+    """
+    Reorders a test suite by test type.
+
+    classes is a sequence of types
+
+    All tests of type clases[0] are placed first, then tests of type classes[1], etc.
+    Tests with no match in classes are placed last.
+    """
+    class_count = len(classes)
+    bins = [unittest.TestSuite() for i in range(class_count+1)]
+    partition_suite(suite, classes, bins)
+    for i in range(class_count):
+        bins[0].addTests(bins[i+1])
+    return bins[0]
 
 def run_tests(test_labels, verbosity=1, interactive=True, extra_tests=[]):
     """
