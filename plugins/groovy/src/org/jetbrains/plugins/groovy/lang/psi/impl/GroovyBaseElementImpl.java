48f623bd44e402e34f6491123e24749efeeddfad
peter <peter.gromov@jetbrains.com>
2011-01-09 23:18:04 +0300

    cleanup

--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/impl/GroovyBaseElementImpl.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/impl/GroovyBaseElementImpl.java
@@ -26,15 +26,11 @@ import com.intellij.psi.impl.source.tree.CompositeElement;
 import com.intellij.psi.impl.source.tree.TreeElement;
 import com.intellij.psi.stubs.IStubElementType;
 import com.intellij.psi.stubs.StubElement;
-import com.intellij.psi.tree.TokenSet;
 import com.intellij.util.IncorrectOperationException;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.plugins.groovy.lang.lexer.TokenSets;
 import org.jetbrains.plugins.groovy.lang.psi.GroovyElementVisitor;
 import org.jetbrains.plugins.groovy.lang.psi.GroovyPsiElement;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrStatement;
-
-import java.util.Iterator;
 
 /**
  * @author ilyas
@@ -49,32 +45,6 @@ public class GroovyBaseElementImpl<T extends StubElement> extends StubBasedPsiEl
     super(node);
   }
 
-  public void removeElements(PsiElement[] elements) throws IncorrectOperationException {
-    ASTNode parentNode = getNode();
-    for (PsiElement element : elements) {
-      if (element.isValid()) {
-        ASTNode node = element.getNode();
-        if (node == null || node.getTreeParent() != parentNode) {
-          throw new IncorrectOperationException();
-        }
-        parentNode.removeChild(node);
-      }
-    }
-  }
-
-  public void removeStatement() throws IncorrectOperationException {
-    if (getParent() == null ||
-            getParent().getNode() == null) {
-      throw new IncorrectOperationException();
-    }
-    ASTNode parentNode = getParent().getNode();
-    ASTNode prevNode = getNode().getTreePrev();
-    parentNode.removeChild(this.getNode());
-    if (prevNode != null && TokenSets.SEPARATORS.contains(prevNode.getElementType())) {
-      parentNode.removeChild(prevNode);
-    }
-  }
-
   @Override
   public PsiElement getParent() {
     return getParentByStub();
@@ -86,43 +56,6 @@ public class GroovyBaseElementImpl<T extends StubElement> extends StubBasedPsiEl
       throw new PsiInvalidElementAccessException(this);
     }
     return (T)replace(newStmt);
-  }
-
-  public <T extends GroovyPsiElement> Iterable<T> childrenOfType(final TokenSet tokSet) {
-    return new Iterable<T>() {
-
-      public Iterator<T> iterator() {
-        return new Iterator<T>() {
-          private ASTNode findChild(ASTNode child) {
-            if (child == null) return null;
-
-            if (tokSet.contains(child.getElementType())) return child;
-
-            return findChild(child.getTreeNext());
-          }
-
-          PsiElement first = getFirstChild();
-
-          ASTNode n = first == null ? null : findChild(first.getNode());
-
-          public boolean hasNext() {
-            return n != null;
-          }
-
-          public T next() {
-            if (n == null) return null;
-            else {
-              final ASTNode res = n;
-              n = findChild(n.getTreeNext());
-              return (T) res.getPsi();
-            }
-          }
-
-          public void remove() {
-          }
-        };
-      }
-    };
   }
 
   public void accept(GroovyElementVisitor visitor) {
@@ -141,7 +74,7 @@ public class GroovyBaseElementImpl<T extends StubElement> extends StubBasedPsiEl
   }
 
   public PsiElement replace(@NotNull PsiElement newElement) throws IncorrectOperationException {
-    CompositeElement treeElement = calcTreeElement();
+    CompositeElement treeElement = (CompositeElement)getNode();
     assert treeElement.getTreeParent() != null;
     CheckUtil.checkWritable(this);
     TreeElement elementCopy = ChangeUtil.copyToElement(newElement);
@@ -150,7 +83,4 @@ public class GroovyBaseElementImpl<T extends StubElement> extends StubBasedPsiEl
     return SourceTreeToPsiMap.treeElementToPsi(elementCopy);
   }
 
-  protected CompositeElement calcTreeElement() {
-    return (CompositeElement)getNode();
-  }
 }
