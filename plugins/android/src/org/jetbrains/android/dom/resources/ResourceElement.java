19c962edd4968caf86a3fc36a94e12baa9fde4d4
Eugene Kudelevsky <Eugene.Kudelevsky@jetbrains.com>
2011-01-11 20:51:29 +0300

    clean up: inherit GenericDomValue interface

--- plugins/android/src/org/jetbrains/android/dom/resources/ResourceElement.java
+++ plugins/android/src/org/jetbrains/android/dom/resources/ResourceElement.java
@@ -16,17 +16,14 @@
 package org.jetbrains.android.dom.resources;
 
 import com.intellij.util.xml.GenericAttributeValue;
+import com.intellij.util.xml.GenericDomValue;
 import com.intellij.util.xml.Required;
 import org.jetbrains.android.dom.AndroidDomElement;
 
 /**
  * @author yole
  */
-public interface ResourceElement extends AndroidDomElement {
+public interface ResourceElement extends GenericDomValue<String>, AndroidDomElement {
   @Required
   GenericAttributeValue<String> getName();
-
-  String getValue();
-
-  void setValue(String s);
 }
