85308d7c746c5b978637938a1055e068d70148b8
irengrig <Irina.Chernushina@jetbrains.com>
2011-01-12 12:53:04 +0300

    IDEA-64028 "Compare with the Same Repository Version" should save changes to disk before invoking an external diff tool

--- platform/vcs-impl/src/com/intellij/openapi/vcs/actions/CompareWithSelectedRevisionAction.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/actions/CompareWithSelectedRevisionAction.java
@@ -17,6 +17,7 @@ package com.intellij.openapi.vcs.actions;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.popup.JBPopup;
 import com.intellij.openapi.ui.popup.PopupChooserBuilder;
@@ -111,6 +112,8 @@ public class CompareWithSelectedRevisionAction extends AbstractVcsAction {
 
   protected void actionPerformed(VcsContext vcsContext) {
     final VirtualFile file = vcsContext.getSelectedFiles()[0];
+    final FileDocumentManager fileDocumentManager = FileDocumentManager.getInstance();
+    fileDocumentManager.saveDocument(fileDocumentManager.getDocument(file));
     final Project project = vcsContext.getProject();
     final AbstractVcs vcs = ProjectLevelVcsManager.getInstance(project).getVcsFor(file);
     final VcsHistoryProvider vcsHistoryProvider = vcs.getVcsHistoryProvider();
