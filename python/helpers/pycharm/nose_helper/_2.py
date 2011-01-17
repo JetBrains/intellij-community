eb1cea039be998e062f92c37e64e7554e857f5f2
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2011-01-17 18:18:56 +0300

    fixed failed tests. removed unusable code.

--- python/helpers/pycharm/nose_helper/_2.py
+++ python/helpers/pycharm/nose_helper/_2.py
@@ -1,13 +1,2 @@
-from __future__ import print_function
-
-import ConfigParser as configparser
-import types
-try:
-    from cStringIO import StringIO
-except ImportError:
-    from StringIO import StringIO
-
-class_types = (type, types.ClassType)
-
 def reraise(exc_class, exc_val, tb):
     raise exc_class, exc_val, tb
--- python/helpers/pycharm/nose_helper/_3.py
+++ python/helpers/pycharm/nose_helper/_3.py
@@ -1,7 +1,2 @@
-import configparser
-from io import StringIO
-
-class_types = (type,)
-
 def reraise(exc_class, exc_val, tb):
     raise exc_class(exc_val).with_traceback(tb)
