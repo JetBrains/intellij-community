13fbe406331b7da15832a87905d2e280f320d369
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2011-01-18 14:34:06 +0300

    fixed SKIP parameter from docrunner

--- python/helpers/pycharm/docrunner.py
+++ python/helpers/pycharm/docrunner.py
@@ -93,6 +93,10 @@ class DocTestRunner(doctest.DocTestRunner):
                     else:
                         self.optionflags &= ~optionflag
 
+            if hasattr(doctest, 'SKIP'):
+              if self.optionflags & doctest.SKIP:
+                continue
+
             tries += 1
             if not quiet:
                 self.report_start(out, test, example)
