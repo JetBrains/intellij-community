2f007d256e27df07871807185608e8fa4641b0f2
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2011-01-24 19:01:37 +0300

    fixed PY-2334 Buildout custom manage.py for unit tests. (All custom stuff should be defined outside main block)

--- python/helpers/pycharm/django_test_manage.py
+++ python/helpers/pycharm/django_test_manage.py
@@ -1,46 +1,14 @@
 #!/usr/bin/env python
 from pycharm.fix_getpass import fixGetpass
-from pycharm import django_test_settings
+from pycharm import django_settings
 from django.core.management import execute_manager
 
-
-def setup_environ(settings_mod, original_settings_path=None):
-    if '__init__.py' in settings_mod.__file__:
-        p = os.path.dirname(settings_mod.__file__)
-    else:
-        p = settings_mod.__file__
-    project_directory, settings_filename = os.path.split(p)
-    if project_directory == os.curdir or not project_directory:
-        project_directory = os.getcwd()
-    project_name = os.path.basename(project_directory)
-
-    # Strip filename suffix to get the module name.
-    settings_name = os.path.splitext(settings_filename)[0]
-
-    # Strip $py for Jython compiled files (like settings$py.class)
-    if settings_name.endswith("$py"):
-        settings_name = settings_name[:-3]
-
-    # Set DJANGO_SETTINGS_MODULE appropriately.
-    if original_settings_path:
-        os.environ['DJANGO_SETTINGS_MODULE'] = original_settings_path
-    else:
-        os.environ['DJANGO_SETTINGS_MODULE'] = '%s.%s' % (project_name, settings_name)
-
-    # Import the project module. We add the parent directory to PYTHONPATH to
-    # avoid some of the path errors new users can have.
-    sys.path.append(os.path.join(project_directory, os.pardir))
-    project_module = import_module(project_name)
-    sys.path.pop()
-
-    return project_directory
-
-def execute_manager(settings_mod):
-    setup_environ(settings_mod)
-    utility = ManagementUtility(argv)
-    utility.execute()
-
+import os
+manage_file = os.getenv('PYCHARM_DJANGO_MANAGE_MODULE')
+if not manage_file:
+    manage_file = 'manage'
 
 if __name__ == "__main__":
+    __import__(manage_file)
     fixGetpass()
-    execute_manager(django_test_settings)
+    execute_manager(django_settings)
