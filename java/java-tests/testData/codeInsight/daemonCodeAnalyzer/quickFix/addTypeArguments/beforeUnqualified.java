58447c4832dd148a4145a82b904bf41a7230369f
anna <Anna.Kozlova@jetbrains.com>
2011-01-24 12:45:21 +0100

    add explicit type arguments: qualify method expression if needed (IDEA-64458)

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/addTypeArguments/beforeUnqualified.java
@@ -0,0 +1,7 @@
+// "Add explicit type arguments" "true"
+import java.util.*;
+class Test {
+    <T> List<T> f() { return new ArrayList<T>(); }
+    void g(List<Integer> a) {}
+    void someMethod() { g(f<caret>()); }
+}
