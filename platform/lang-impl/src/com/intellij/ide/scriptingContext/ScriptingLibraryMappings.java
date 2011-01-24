2a8810a6fd33b93e719602950040a372a8867d99
Rustam Vishnyakov <rvishnyakov@swiftteams.com>
2011-01-24 22:07:54 +0300

    Revert "JavaScript library files scope for completion (performance improvements for WI-4830)"
    This reverts commit 2ec7b6f5e6d4acf2ca222b00d9bfb99fa7c356a7.

--- platform/lang-impl/src/com/intellij/ide/scriptingContext/ScriptingLibraryMappings.java
+++ platform/lang-impl/src/com/intellij/ide/scriptingContext/ScriptingLibraryMappings.java
@@ -47,7 +47,6 @@ public class ScriptingLibraryMappings extends LanguagePerFileMappings<ScriptingL
   private final ScriptingLibraryManager myLibraryManager;
   private final Map<VirtualFile, CompoundLibrary> myCompoundLibMap = new HashMap<VirtualFile, CompoundLibrary>();
   private CompoundLibrary myProjectLibs = new CompoundLibrary();
-  private Set<VirtualFile> myProjectLibFiles;
 
   public ScriptingLibraryMappings(final Project project, final LibraryType libraryType) {
     super(project);
@@ -96,7 +95,6 @@ public class ScriptingLibraryMappings extends LanguagePerFileMappings<ScriptingL
   public void setMappings(Map<VirtualFile, ScriptingLibraryTable.LibraryModel> mappings) {
     super.setMappings(mappings);
     updateDependencies(mappings);
-    updateProjectLibraryFiles();
   }
 
   private static boolean dependencyExists(ModuleRootManager rootManager, Library library) {
@@ -173,7 +171,6 @@ public class ScriptingLibraryMappings extends LanguagePerFileMappings<ScriptingL
       setMapping(file, container);
     }
     updateDependencies(getMappings());
-    updateProjectLibraryFiles();
   }
   
   public boolean isAssociatedWith(VirtualFile file, String libName) {
@@ -403,28 +400,6 @@ public class ScriptingLibraryMappings extends LanguagePerFileMappings<ScriptingL
     }
     return isApplicable(libFile, srcFile.getParent());
   }
-
-  private void updateProjectLibraryFiles() {
-    myProjectLibFiles = new HashSet<VirtualFile>();
-    for (CompoundLibrary container : myCompoundLibMap.values()) {
-      for (ScriptingLibraryTable.LibraryModel libModel : container.getLibraries()) {
-        myProjectLibFiles.addAll(Arrays.asList(libModel.getSourceFiles()));
-        myProjectLibFiles.addAll(Arrays.asList(libModel.getCompactFiles()));
-      }
-    }
-    for (ScriptingLibraryTable.LibraryModel libModel : myProjectLibs.getLibraries()) {
-      myProjectLibFiles.addAll(Arrays.asList(libModel.getSourceFiles()));
-      myProjectLibFiles.addAll(Arrays.asList(libModel.getCompactFiles()));
-    }
-  }
-
-  public Set<VirtualFile> getProjectLibraryFiles() {
-    if (myProjectLibFiles == null) {
-      updateProjectLibraryFiles();
-    }
-    return myProjectLibFiles;
-  }
-  
 
 
 }
--- platform/lang-impl/src/com/intellij/openapi/roots/libraries/scripting/ScriptingLibrariesScope.java
+++ platform/lang-impl/src/com/intellij/openapi/roots/libraries/scripting/ScriptingLibrariesScope.java
@@ -30,7 +30,7 @@ public class ScriptingLibrariesScope extends GlobalSearchScope {
 
   private GlobalSearchScope myBaseScope;
   private ScriptingIndexableSetContributor myContributor;
-  protected Set<VirtualFile> myLibraryFiles;
+  private Set<VirtualFile> myLibraryFiles;
 
   public ScriptingLibrariesScope(GlobalSearchScope baseScope, Class<? extends IndexableSetContributor> providerClass) {
     super(baseScope.getProject());
@@ -38,16 +38,21 @@ public class ScriptingLibrariesScope extends GlobalSearchScope {
     IndexableSetContributor contributor = IndexableSetContributor.EP_NAME.findExtension(providerClass);
     if (contributor instanceof  ScriptingIndexableSetContributor) {
       myContributor = (ScriptingIndexableSetContributor)contributor;
-      setLibraryFiles();
+      updateLibraryFiles();
     }
   }
 
-  public void setLibraryFiles() {
+  public void updateLibraryFiles() {
     myLibraryFiles = myContributor.getLibraryFiles(myBaseScope.getProject());
   }
 
   public boolean contains(VirtualFile file) {
-    return myBaseScope.contains(file) | (myLibraryFiles == null ? false : myLibraryFiles.contains(file));
+    return myBaseScope.contains(file) | contributorContains(file);
+  }
+
+  private boolean contributorContains(VirtualFile file) {
+    if (myContributor == null) return false;
+    return myLibraryFiles.contains(file);
   }
 
   @Override
