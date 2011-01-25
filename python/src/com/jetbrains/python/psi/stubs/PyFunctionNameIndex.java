27aaddf95bd9fffe785890ddfbffe4105218b00d
Dmitry Jemerov <yole@jetbrains.com>
2011-01-25 15:50:53 +0100

    cleanup

--- python/src/com/jetbrains/python/psi/stubs/PyFunctionNameIndex.java
+++ python/src/com/jetbrains/python/psi/stubs/PyFunctionNameIndex.java
@@ -14,7 +14,7 @@ import com.jetbrains.python.psi.PyFunction;
 import java.util.Collection;
 
 public class PyFunctionNameIndex extends StringStubIndexExtension<PyFunction> {
-  public static final StubIndexKey<String,PyFunction> KEY = StubIndexKey.createIndexKey("Py.function.shortName");
+  public static final StubIndexKey<String, PyFunction> KEY = StubIndexKey.createIndexKey("Py.function.shortName");
 
   public StubIndexKey<String, PyFunction> getKey() {
     return KEY;
@@ -22,8 +22,8 @@ public class PyFunctionNameIndex extends StringStubIndexExtension<PyFunction> {
 
   public static Collection<PyFunction> find(String name, Project project, GlobalSearchScope scope) {
     return StubIndex.getInstance().get(KEY, name, project, scope);
-
   }
+
   public static Collection<PyFunction> find(String name, Project project) {
     return StubIndex.getInstance().get(KEY, name, project, ProjectScope.getAllScope(project));
   }
