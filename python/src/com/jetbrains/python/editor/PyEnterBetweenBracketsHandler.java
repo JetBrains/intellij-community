282a6491c740084893109b6b2f54ae754cf368c0
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2011-01-27 02:03:57 +0300

    fix compile

--- python/src/com/jetbrains/python/editor/PyEnterBetweenBracketsHandler.java
+++ python/src/com/jetbrains/python/editor/PyEnterBetweenBracketsHandler.java
@@ -6,7 +6,7 @@ import com.intellij.codeInsight.editorActions.enter.EnterBetweenBracesHandler;
  * @author yole
  */
 public class PyEnterBetweenBracketsHandler extends EnterBetweenBracesHandler {
-  @Override
+  // TODO yole, fix compile @Override
   protected boolean isBracePair(char c1, char c2) {
     return c1 == '[' && c2  == ']';
   }
