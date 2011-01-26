872ad54f14d8da428232d690a4c2049c5296b4b5
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2011-01-26 14:07:39 +0300

    fixed memory leak in ConvertVariadicParamIntention.

--- python/testData/intentions/afterConvertVariadicParam.py
+++ python/testData/intentions/afterConvertVariadicParam.py
@@ -1,2 +1,3 @@
-def foo(w, q = 2, foo=22, **kwargs):
+def foo(tmp, w, q = 2, foo=22, **kwargs):
+    a = tmp
     doSomething(foo)
--- python/testData/intentions/beforeConvertVariadicParam.py
+++ python/testData/intentions/beforeConvertVariadicParam.py
@@ -1,2 +1,3 @@
 def foo(w, <caret>q = 2, **kwargs):
+    a = kwargs['tmp']
     doSomething(kwargs.get('foo', 22))
