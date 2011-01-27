1a5a291880530a24513a76ec850ea29950ce45f6
Dmitry Jemerov <yole@jetbrains.com>
2011-01-27 12:11:23 +0100

    restore changes reverted because of failed push

--- python/src/com/jetbrains/python/editor/PyEnterBetweenBracketsHandler.java
+++ python/src/com/jetbrains/python/editor/PyEnterBetweenBracketsHandler.java
@@ -6,7 +6,7 @@ import com.intellij.codeInsight.editorActions.enter.EnterBetweenBracesHandler;
  * @author yole
  */
 public class PyEnterBetweenBracketsHandler extends EnterBetweenBracesHandler {
-  // TODO yole, fix compile @Override
+  @Override
   protected boolean isBracePair(char c1, char c2) {
     return c1 == '[' && c2  == ']';
   }
