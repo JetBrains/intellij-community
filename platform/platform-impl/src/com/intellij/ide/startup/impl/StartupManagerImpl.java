93742656e27e64f476d90f13569f651879e04806
Maxim Shafirov <max@jetbrains.com>
2011-01-31 20:48:28 +0300

    wording [r=kirillk]

--- platform/platform-impl/src/com/intellij/ide/startup/impl/StartupManagerImpl.java
+++ platform/platform-impl/src/com/intellij/ide/startup/impl/StartupManagerImpl.java
@@ -15,10 +15,13 @@
  */
 package com.intellij.ide.startup.impl;
 
+import com.intellij.ide.BrowserUtil;
 import com.intellij.ide.caches.CacheUpdater;
 import com.intellij.ide.startup.StartupManagerEx;
+import com.intellij.notification.*;
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ApplicationNamesInfo;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.progress.ProgressIndicator;
@@ -27,12 +30,15 @@ import com.intellij.openapi.project.DumbAwareRunnable;
 import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.DumbServiceImpl;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.SystemInfo;
+import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.vfs.VirtualFileManager;
 import com.intellij.util.io.storage.HeavyProcessLatch;
 import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.TestOnly;
 
+import javax.swing.event.HyperlinkEvent;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
@@ -143,6 +149,23 @@ public class StartupManagerImpl extends StartupManagerEx {
 
     if (!app.isUnitTestMode()) {
       VirtualFileManager.getInstance().refresh(!app.isHeadlessEnvironment());
+    }
+
+    if (SystemInfo.isMac && SystemInfo.is64Bit && "10.6".compareTo(SystemInfo.OS_VERSION) <= 0) {
+      if (Registry.is("ide.firstStartup")) {
+        String productName = ApplicationNamesInfo.getInstance().getProductName();
+        Notification n =
+          new Notification(Notifications.SYSTEM_MESSAGES_GROUP_ID, "Optimization Hint", productName + " is now running in 64-bit mode.<br>" +
+                                                                                               "You may reduce memory consumption by running it in 32-bit mode. " +
+                                                                                               "<a href=\"http://devnet.jetbrains.net/docs/DOC-1232\">Click for details</a>", NotificationType.INFORMATION, new NotificationListener() {
+            @Override
+            public void hyperlinkUpdate(@NotNull Notification notification, @NotNull HyperlinkEvent event) {
+              BrowserUtil.launchBrowser(event.getURL().toString());
+            }
+          });
+        Notifications.Bus.notify(n, NotificationDisplayType.STICKY_BALLOON, null);
+        Registry.get("ide.firstStartup").setValue(false);
+      }
     }
   }
 
