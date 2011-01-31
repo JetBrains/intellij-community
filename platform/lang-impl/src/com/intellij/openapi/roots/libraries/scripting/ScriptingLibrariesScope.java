49a9072ab4c52997c2e572b71f8f954ff2926171
Rustam.Vishnyakov <rustam.vishnyakov@gmail.com>
2011-01-31 13:35:24 +0300

    do not create scope in jsresolveutil to avoid leaks [by M.Mossienko]

--- platform/lang-impl/src/com/intellij/openapi/roots/libraries/scripting/ScriptingLibrariesScope.java
+++ platform/lang-impl/src/com/intellij/openapi/roots/libraries/scripting/ScriptingLibrariesScope.java
@@ -16,30 +16,55 @@
 package com.intellij.openapi.roots.libraries.scripting;
 
 import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.util.Key;
+import com.intellij.openapi.util.UserDataCache;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiManager;
 import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.util.CachedValue;
+import com.intellij.psi.util.CachedValueProvider;
 import com.intellij.util.indexing.IndexableSetContributor;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collections;
 import java.util.Set;
 
 /**
  * @author Rustam Vishnyakov
  */
 public class ScriptingLibrariesScope extends GlobalSearchScope {
+  private static final UserDataCache<CachedValue<Set<VirtualFile>>, Project, Class<? extends IndexableSetContributor>> ourAllLibFilesCache =
+    new UserDataCache<CachedValue<Set<VirtualFile>>, Project, Class<? extends IndexableSetContributor>>() {
+    @Override
+    protected CachedValue<Set<VirtualFile>> compute(final Project project, final Class<? extends IndexableSetContributor> p) {
+      return PsiManager.getInstance(project).getCachedValuesManager().createCachedValue(new CachedValueProvider<Set<VirtualFile>>() {
+        @Override
+        public Result<Set<VirtualFile>> compute() {
+          IndexableSetContributor contributor;
+          contributor = IndexableSetContributor.EP_NAME.findExtension(p);
+          Set<VirtualFile> result;
+          if (contributor instanceof ScriptingIndexableSetContributor) {
+            result = ((ScriptingIndexableSetContributor)contributor).getLibraryFiles(project);
+          } else {
+            result = Collections.emptySet();
+          }
+          return new Result<Set<VirtualFile>>(result, ProjectRootManager.getInstance(project));
+        }
+      }, false);
+    }
+  };
 
   private GlobalSearchScope myBaseScope;
   protected Set<VirtualFile> myLibraryFiles;
   private Set<VirtualFile> myAllLibraryFiles;
 
-  public ScriptingLibrariesScope(GlobalSearchScope baseScope, Class<? extends IndexableSetContributor> providerClass) {
+  public ScriptingLibrariesScope(GlobalSearchScope baseScope, Class<? extends IndexableSetContributor> providerClass, Key key) {
     super(baseScope.getProject());
     myBaseScope = baseScope;
-    IndexableSetContributor contributor = IndexableSetContributor.EP_NAME.findExtension(providerClass);
-    if (contributor instanceof ScriptingIndexableSetContributor) {
-      myAllLibraryFiles = ((ScriptingIndexableSetContributor)contributor).getLibraryFiles(myBaseScope.getProject());
-      setLibraryFiles();
-    }
+    myAllLibraryFiles = ourAllLibFilesCache.get(key, baseScope.getProject(), providerClass).getValue();
+    setLibraryFiles();
   }
 
   protected void setLibraryFiles() {
