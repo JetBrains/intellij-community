3ebd78eb2adfd3d047fd96ee06513e80bc543265
Konstantin Bulenkov <kb@jetbrains.com>
2011-01-31 13:05:04 +0300

    IDEA-64501 Context menu keyboard button shows context menu, but focus is still in editor [r=spleaner]

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorImpl.java
@@ -3353,7 +3353,10 @@ public final class EditorImpl extends UserDataHolderBase implements EditorEx, Hi
   }
 
   private void requestFocus() {
-    IdeFocusManager.getInstance(myProject).requestFocus(myEditorComponent, true);
+    final IdeFocusManager focusManager = IdeFocusManager.getInstance(myProject);
+    if (focusManager.getFocusOwner() != myEditorComponent) { //IDEA-64501
+      focusManager.requestFocus(myEditorComponent, true);
+    }
   }
 
   private void validateMousePointer(MouseEvent e) {
