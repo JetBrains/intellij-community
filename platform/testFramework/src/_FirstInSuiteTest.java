34bd24decb4b9b1df5076179ffb2cbcdc5c33a3d
Dmitry Jemerov <yole@jetbrains.com>
2011-01-31 13:16:25 +0100

    revert; the problem was elsewhere

--- platform/testFramework/src/_FirstInSuiteTest.java
+++ platform/testFramework/src/_FirstInSuiteTest.java
@@ -14,13 +14,13 @@
  * limitations under the License.
  */
 
-import junit.framework.TestCase;
+import com.intellij.testFramework.LightPlatformTestCase;
 
 /**
  * This is should be first test in all tests so we can measure how long tests are starting up.
  * @author max
  */
-public class _FirstInSuiteTest extends TestCase {
+public class _FirstInSuiteTest extends LightPlatformTestCase {
   public void testNothing() throws Exception {
 
   }
