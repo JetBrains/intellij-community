34261b84efd2eb8de117c6b7a262f2f7af30f4b5
anna <Anna.Kozlova@jetbrains.com>
2011-02-01 13:39:50 +0100

    coverage: read args from file; copy agent to the path without spaces (IDEA-64868); //arg file would be deleted on exit from IDEA

--- plugins/coverage/src/com/intellij/coverage/IDEACoverageRunner.java
+++ plugins/coverage/src/com/intellij/coverage/IDEACoverageRunner.java
@@ -7,6 +7,7 @@ package com.intellij.coverage;
 import com.intellij.execution.configurations.SimpleJavaParameters;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.SystemInfo;
+import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.rt.coverage.data.ProjectData;
 import com.intellij.rt.coverage.util.ProjectDataLoader;
 import com.intellij.util.PathUtil;
@@ -14,6 +15,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
+import java.io.IOException;
 
 public class IDEACoverageRunner extends JavaCoverageRunner {
   private static final Logger LOG = Logger.getInstance("#" + IDEACoverageRunner.class.getName());
@@ -25,28 +27,32 @@ public class IDEACoverageRunner extends JavaCoverageRunner {
   public void appendCoverageArgument(final String sessionDataFilePath, final String[] patterns, final SimpleJavaParameters javaParameters,
                                      final boolean collectLineInfo, final boolean isSampling) {
     StringBuffer argument = new StringBuffer("-javaagent:");
-    argument.append(PathUtil.getJarPathForClass(ProjectData.class));
+    final String agentPath = PathUtil.getJarPathForClass(ProjectData.class);
+    final String parentPath = handleSpacesInPath(agentPath);
+    argument.append(parentPath).append(File.separator).append(new File(agentPath).getName());
     argument.append("=");
-    if (SystemInfo.isWindows) {
-      argument.append("\\\"").append(sessionDataFilePath).append("\\\"");
-    }
-    else {
-      argument.append(sessionDataFilePath);
-    }
-    argument.append(" ").append(String.valueOf(collectLineInfo));
-    argument.append(" ").append(Boolean.FALSE.toString()); //append unloaded
-    argument.append(" ").append(Boolean.FALSE.toString()); //merge with existing
-    argument.append(" ").append(String.valueOf(isSampling));
-    if (patterns != null && patterns.length > 0) {
-      argument.append(" ");
+    try {
+      final File tempFile = FileUtil.createTempFile("coverage", "args");
+      tempFile.deleteOnExit();
+      write2file(tempFile, sessionDataFilePath);
+      write2file(tempFile, String.valueOf(collectLineInfo));
+      write2file(tempFile, Boolean.FALSE.toString());
+      write2file(tempFile, Boolean.FALSE.toString());
+      write2file(tempFile, String.valueOf(isSampling));
       for (String coveragePattern : patterns) {
         coveragePattern = coveragePattern.replace("$", "\\$").replace(".", "\\.").replaceAll("\\*", ".*");
         if (!coveragePattern.endsWith(".*")) { //include inner classes
           coveragePattern += "(\\$.*)*";
         }
-        argument.append(coveragePattern).append(" ");
+        write2file(tempFile, coveragePattern);
       }
+      argument.append(tempFile.getCanonicalPath());
     }
+    catch (IOException e) {
+      LOG.info("Coverage was not enabled", e);
+      return;
+    }
+
     javaParameters.getVMParametersList().add(argument.toString());
   }
 
--- plugins/coverage/src/com/intellij/coverage/JavaCoverageRunner.java
+++ plugins/coverage/src/com/intellij/coverage/JavaCoverageRunner.java
@@ -1,15 +1,22 @@
 package com.intellij.coverage;
 
 import com.intellij.execution.configurations.SimpleJavaParameters;
+import com.intellij.openapi.application.PathManager;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.util.SystemInfo;
+import com.intellij.openapi.util.io.FileUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * @author Roman.Chernyatchik
  */
 public abstract class JavaCoverageRunner extends CoverageRunner {
   private static final Logger LOG = Logger.getInstance("#" + JavaCoverageRunner.class.getName());
+  private static final String COVERAGE_AGENT_PATH = "coverage.lib.path";
 
   @Override
   public boolean acceptsCoverageEngine(@NotNull CoverageEngine engine) {
@@ -19,5 +26,43 @@ public abstract class JavaCoverageRunner extends CoverageRunner {
   public abstract void appendCoverageArgument(final String sessionDataFilePath, @Nullable final String[] patterns, final SimpleJavaParameters parameters,
                                               final boolean collectLineInfo, final boolean isSampling);
 
+  protected static String handleSpacesInPath(String agentPath) {
+    final String userDefined = System.getProperty(COVERAGE_AGENT_PATH);
+    if (userDefined != null && new File(userDefined).exists()) {
+      agentPath = userDefined;
+    } else {
+      agentPath = new File(agentPath).getParent();
+    }
+    if (!SystemInfo.isWindows && agentPath.contains(" ")) {
+      File dir = new File(PathManager.getSystemPath(), "coverageJars");
+      if (dir.getAbsolutePath().contains(" ")) {
+        try {
+          dir = FileUtil.createTempDirectory("coverage", "jars");
+          if (dir.getAbsolutePath().contains(" ")) {
+            LOG.info("Coverage agent not used since the agent path contains spaces: " + agentPath + "\n" +
+                     "One can move the agent libraries to a directory with no spaces in path and specify its path in idea.properties as " + COVERAGE_AGENT_PATH + "=<path>");
+            return agentPath;
+          }
+        }
+        catch (IOException e) {
+          LOG.info(e);
+          return agentPath;
+        }
+      }
 
+      try {
+        LOG.info("Coverage jars were copied to " + dir.getPath());
+        FileUtil.copyDir(new File(agentPath), dir);
+        return dir.getPath();
+      }
+      catch (IOException e) {
+        LOG.info(e);
+      }
+    }
+    return agentPath;
+  }
+
+  protected static void write2file(File tempFile, String arg) throws IOException {
+    FileUtil.writeToFile(tempFile, (arg + "\n").getBytes(), true);
+  }
 }
