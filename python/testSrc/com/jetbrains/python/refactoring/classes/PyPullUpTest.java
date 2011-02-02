deff48c064bca616c71948074a8151ae4514619c
Dmitry Jemerov <yole@jetbrains.com>
2011-02-02 16:33:45 +0100

    don't try to import references that resolve in the method being pulled up (PY-2810)

--- python/testSrc/com/jetbrains/python/refactoring/classes/PyPullUpTest.java
+++ python/testSrc/com/jetbrains/python/refactoring/classes/PyPullUpTest.java
@@ -30,13 +30,23 @@ public class PyPullUpTest extends PyClassRefactoringTest {
     doHelperTest("Boo", ".boo", "Foo");
   }
 
+  public void testMultiFile() {   // PY-2810
+    myFixture.copyDirectoryToProject("refactoring/pullup/multiFile/", "");
+    doPullUp("AnyClass", ".this_should_be_in_super", "SuperClass");
+    myFixture.checkResultByFile("SuperClass.py", "/refactoring/pullup/multiFile/SuperClass.after.py", true);
+  }
+
   private void doHelperTest(final String className, final String memberName, final String superClassName) {
     String baseName = "/refactoring/pullup/" + getTestName(true);
     myFixture.configureByFile(baseName + ".py");
+    doPullUp(className, memberName, superClassName);
+    myFixture.checkResultByFile(baseName + ".after.py");
+  }
+
+  private void doPullUp(String className, String memberName, String superClassName) {
     final PyClass clazz = findClass(className);
     final PyElement member = findMember(className, memberName);
     final PyClass superClass = findClass(superClassName);
     PyPullUpHelper.pullUp(clazz, Collections.singleton(new PyMemberInfo(member)), superClass);
-    myFixture.checkResultByFile(baseName + ".after.py");
   }
 }
