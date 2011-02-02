bd5fc794e0a5293601ec706affefeacdb60c04b5
Denis Zhdanov <Denis.Zhdanov@jetbrains.com>
2011-02-02 16:04:50 +0300

    IDEA-64964 Javadoc: Provide ability to configure IntelliJ IDEA to automatically insert closing tags during typing in javadoc
    1. Javadoc typed handler with 'auto insert closing tag' logic is added;
    2. Corresponding test is added;
    3. Corresponding GUI control is provided;

---
+++ java/java-impl/src/com/intellij/application/options/JavadocOptionsProvider.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2000-2011 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.application.options;
+
+import com.intellij.application.options.editor.EditorOptionsProvider;
+import com.intellij.codeInsight.CodeInsightSettings;
+import com.intellij.javadoc.JavadocBundle;
+import com.intellij.openapi.options.ConfigurationException;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+/**
+ * @author Denis Zhdanov
+ * @since 2/2/11 12:32 PM
+ */
+public class JavadocOptionsProvider implements EditorOptionsProvider {
+  
+  private JPanel myWholePanel;
+  private JCheckBox myAutoGenerateClosingTagCheckBox;
+
+  @NotNull
+  @Override
+  public String getId() {
+    return "editor.preferences.javadocOptions";
+  }
+
+  @Override
+  public Runnable enableSearch(String option) {
+    return null;
+  }
+
+  @Nls
+  @Override
+  public String getDisplayName() {
+    return JavadocBundle.message("javadoc.generate.message.title");
+  }
+
+  @Override
+  public Icon getIcon() {
+    return null;
+  }
+
+  @Override
+  public String getHelpTopic() {
+    return null;
+  }
+
+  @Override
+  public JComponent createComponent() {
+    return myWholePanel;
+  }
+
+  @Override
+  public boolean isModified() {
+    CodeInsightSettings settings = getSettings();
+    return myAutoGenerateClosingTagCheckBox.isSelected() ^ settings.JAVADOC_GENERATE_CLOSING_TAG;
+  }
+
+  @Override
+  public void apply() throws ConfigurationException {
+    getSettings().JAVADOC_GENERATE_CLOSING_TAG = myAutoGenerateClosingTagCheckBox.isSelected();
+  }
+
+  @Override
+  public void reset() {
+    myAutoGenerateClosingTagCheckBox.setSelected(getSettings().JAVADOC_GENERATE_CLOSING_TAG);
+  }
+
+  @Override
+  public void disposeUIResources() {
+  }
+  
+  private static CodeInsightSettings getSettings() {
+    return CodeInsightSettings.getInstance();
+  }
+}
---
+++ java/java-impl/testSrc/com/intellij/codeInsight/editorActions/JavadocTypedHandlerTest.java
@@ -0,0 +1,48 @@
+package com.intellij.codeInsight.editorActions;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Denis Zhdanov
+ * @since 02/02/2011
+ */
+public class JavadocTypedHandlerTest {
+
+  private static final String CARET_MARKER = "<caret>";
+  
+  @Test
+  public void correctEmptyTagStart() {
+    doTest("<first></first><second><caret>", "second");
+  }
+  
+  @Test
+  public void standaloneBracket() {
+    doTest("asdf ><caret>", null);
+  }
+
+  @Test
+  public void emptyElement() {
+    doTest("<tag/><caret>", null);
+  }
+
+  @Test
+  public void closingTag() {
+    doTest("<tag></tag><caret>", null);
+  }
+
+  @Test
+  public void startTagOnNewLine() {
+    doTest("<t\nag><caret>", null);
+  }
+  
+  private static void doTest(String text, String expected) {
+    StringBuilder normalized = new StringBuilder();
+    int offset = text.indexOf(CARET_MARKER);
+    normalized.append(text.substring(0, offset));
+    normalized.append(text.substring(offset + CARET_MARKER.length()));
+    CharSequence actual = JavadocTypedHandler.getTagName(normalized.toString(), offset);
+    assertEquals(expected, actual);
+  }
+}
