a25f95b49f1a488fffb8b1e1fe3e85a1996e320f
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2011-02-07 13:19:04 +0300

    fixed PY-2832 Python 3.x - "Statement can be replaced with function call ignores" arguments

--- python/src/com/jetbrains/python/actions/StatementEffectFunctionCallQuickFix.java
+++ python/src/com/jetbrains/python/actions/StatementEffectFunctionCallQuickFix.java
@@ -4,9 +4,11 @@ import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiWhiteSpace;
 import com.jetbrains.python.PyBundle;
 import com.jetbrains.python.psi.LanguageLevel;
 import com.jetbrains.python.psi.PyElementGenerator;
+import com.jetbrains.python.psi.PyExpression;
 import com.jetbrains.python.psi.PyReferenceExpression;
 import org.jetbrains.annotations.NotNull;
 
@@ -34,7 +36,31 @@ public class StatementEffectFunctionCallQuickFix implements LocalQuickFix {
     PsiElement expression = descriptor.getPsiElement();
     if (expression != null && expression.isWritable() && expression instanceof PyReferenceExpression) {
       PyElementGenerator elementGenerator = PyElementGenerator.getInstance(project);
-      expression.replace(elementGenerator.createCallExpression(LanguageLevel.forElement(expression), expression.getText()));
+      if ("print".equals(expression.getText()))
+        replacePrint(expression, elementGenerator);
+      else
+        expression.replace(elementGenerator.createCallExpression(LanguageLevel.forElement(expression), expression.getText()));
     }
+  }
+
+  private static void replacePrint(PsiElement expression, PyElementGenerator elementGenerator) {
+    StringBuilder stringBuilder = new StringBuilder("print (");
+
+    PsiElement whiteSpace = expression.getParent().getNextSibling();
+    PsiElement next = null;
+    if (whiteSpace instanceof PsiWhiteSpace) {
+      if (!whiteSpace.getText().contains("\n"))
+        next = whiteSpace.getNextSibling();
+    }
+    else
+      next = whiteSpace;
+
+    if (next != null) {
+      stringBuilder.append(next.getText());
+      next.delete();
+    }
+    stringBuilder.append(")");
+    expression.replace(elementGenerator.createFromText(LanguageLevel.forElement(expression), PyExpression.class,
+                                                       stringBuilder.toString()));
   }
 }
