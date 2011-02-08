b5e104be58a7096265ad9789db3c9b86022e61cc
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2011-02-08 14:21:01 +0300

    added quick fix for print replace with print() to compatibility inspection

---
+++ python/src/com/jetbrains/python/actions/CompatibilityPrintCallQuickFix.java
@@ -0,0 +1,51 @@
+package com.jetbrains.python.actions;
+
+import com.intellij.codeInspection.LocalQuickFix;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.jetbrains.python.PyBundle;
+import com.jetbrains.python.psi.LanguageLevel;
+import com.jetbrains.python.psi.PyElementGenerator;
+import com.jetbrains.python.psi.PyExpression;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * User: catherine
+ *
+ * QuickFix to replace statement that has no effect with function call
+ */
+public class CompatibilityPrintCallQuickFix implements LocalQuickFix {
+
+  public CompatibilityPrintCallQuickFix() {
+  }
+
+  @NotNull
+  public String getName() {
+    return PyBundle.message("QFIX.statement.effect");
+  }
+
+  @NotNull
+  public String getFamilyName() {
+    return PyBundle.message("INSP.GROUP.python");
+  }
+
+  public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
+    PsiElement expression = descriptor.getPsiElement();
+    PyElementGenerator elementGenerator = PyElementGenerator.getInstance(project);
+    replacePrint(expression, elementGenerator);
+  }
+
+  private static void replacePrint(PsiElement expression, PyElementGenerator elementGenerator) {
+    StringBuilder stringBuilder = new StringBuilder("print (");
+
+    PsiElement target = PsiTreeUtil.findChildOfType(expression, PyExpression.class);
+    if (target != null)
+      stringBuilder.append(target.getText());
+
+    stringBuilder.append(")");
+    expression.replace(elementGenerator.createFromText(LanguageLevel.forElement(expression), PyExpression.class,
+                                                       stringBuilder.toString()));
+  }
+}
