0ee2ba100baf0a8f8db7597e0ec331513e2ac005
anna <Anna.Kozlova@jetbrains.com>
2011-02-12 12:13:56 +0100

    coverage: convert jsp name to class name according to JspUtil from jasper compiler (IDEA-65421)

--- plugins/coverage/src/com/intellij/coverage/J2EECoverageEngine.java
+++ plugins/coverage/src/com/intellij/coverage/J2EECoverageEngine.java
@@ -4,8 +4,10 @@ import com.intellij.execution.configurations.RunConfigurationBase;
 import com.intellij.javaee.run.configuration.CommonModel;
 import com.intellij.javaee.web.WebUtil;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiClassOwner;
 import com.intellij.psi.PsiFile;
@@ -49,7 +51,8 @@ public class J2EECoverageEngine extends JavaCoverageEngine {
                 if (!webRoot.endsWith("/")) {
                   webRoot += "/";
                 }
-                result.add("org.apache.jsp" + webRoot.replace('/', '.') + virtualFile.getNameWithoutExtension() + "_" + virtualFile.getExtension());
+                result.add(
+                  "org.apache.jsp" + makeJavaIdentifier(webRoot + virtualFile.getName(), sourceFile.getProject()));
                 return result;
               }
             }
@@ -59,5 +62,48 @@ public class J2EECoverageEngine extends JavaCoverageEngine {
       });
     }
     return super.getQualifiedNames(sourceFile);
+  }
+
+  /**
+   * Converts the given identifier to a legal Java identifier
+   * {@see org.apache.jasper.compiler.JspUtil}
+   */
+  public static String makeJavaIdentifier(String identifier, Project project) {
+    StringBuffer modifiedIdentifier =
+      new StringBuffer(identifier.length());
+    if (!Character.isJavaIdentifierStart(identifier.charAt(0)) && identifier.charAt(0) != '/') {
+      modifiedIdentifier.append('_');
+    }
+    for (int i = 0; i < identifier.length(); i++) {
+      char ch = identifier.charAt(i);
+      if (ch == '/') {
+        modifiedIdentifier.append('.');
+      }
+      else if (Character.isJavaIdentifierPart(ch) && ch != '_') {
+        modifiedIdentifier.append(ch);
+      } else if (ch == '.') {
+        modifiedIdentifier.append('_');
+      }
+      else {
+        modifiedIdentifier.append(mangleChar(ch));
+      }
+    }
+    if (JavaPsiFacade.getInstance(project).getNameHelper().isKeyword(modifiedIdentifier.toString())) {
+      modifiedIdentifier.append('_');
+    }
+    return modifiedIdentifier.toString();
+  }
+
+  /**
+   * Mangle the specified character to create a legal Java class name.
+   */
+  public static String mangleChar(char ch) {
+    char[] result = new char[5];
+    result[0] = '_';
+    result[1] = Character.forDigit((ch >> 12) & 0xf, 16);
+    result[2] = Character.forDigit((ch >> 8) & 0xf, 16);
+    result[3] = Character.forDigit((ch >> 4) & 0xf, 16);
+    result[4] = Character.forDigit(ch & 0xf, 16);
+    return new String(result);
   }
 }
