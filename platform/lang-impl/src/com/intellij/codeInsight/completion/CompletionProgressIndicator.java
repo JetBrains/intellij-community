deec377e9668310838a61b5485e1e5a6294eb8a2
peter <peter@jetbrains.com>
2011-02-16 18:31:24 +0100

    use registry

--- platform/lang-impl/src/com/intellij/codeInsight/completion/CompletionProgressIndicator.java
+++ platform/lang-impl/src/com/intellij/codeInsight/completion/CompletionProgressIndicator.java
@@ -38,6 +38,7 @@ import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.patterns.ElementPattern;
 import com.intellij.psi.PsiFile;
@@ -102,7 +103,6 @@ public class CompletionProgressIndicator extends ProgressIndicatorBase implement
     }
   };
   private final Semaphore myDuringCompletionSemaphore = new Semaphore();
-  private static final boolean ourHintAutopopup = "true".equals(System.getProperty("hint.autopopup", "false"));
 
   private volatile int myCount;
 
@@ -336,7 +336,7 @@ public class CompletionProgressIndicator extends ProgressIndicatorBase implement
         }
       }
 
-      if (isAutopopupCompletion() && ourHintAutopopup) {
+      if (isAutopopupCompletion() && showHintAutopopup()) {
         myLookup.setHintMode(true);
       }
       else {
@@ -346,6 +346,10 @@ public class CompletionProgressIndicator extends ProgressIndicatorBase implement
     myLookup.refreshUi();
     hideAutopopupIfMeaningless();
     updateFocus();
+  }
+
+  private static boolean showHintAutopopup() {
+    return "true".equals(Registry.stringValue("hint.autopopup"));
   }
 
   final boolean isInsideIdentifier() {
@@ -466,7 +470,7 @@ public class CompletionProgressIndicator extends ProgressIndicatorBase implement
     if (isAutopopupCompletion() && !myLookup.isSelectionTouched() && !myLookup.isCalculating()) {
       myLookup.refreshUi();
       final List<LookupElement> items = myLookup.getItems();
-      if (!items.isEmpty() && ourHintAutopopup) {
+      if (!items.isEmpty() && showHintAutopopup()) {
         return false;
       }
 
