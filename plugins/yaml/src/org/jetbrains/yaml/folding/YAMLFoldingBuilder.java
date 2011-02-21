f2d57ab696b5c21b4fbf8b15481cef44b350a68c
Oleg Shpynov <oleg.shpynov@jebrains.com>
2011-02-21 14:15:01 +0300

    Revert "More consistent API"
    This reverts commit 3663cb4f416e065b7461aee296e9c7897cdb6d5e.

--- plugins/yaml/src/org/jetbrains/yaml/folding/YAMLFoldingBuilder.java
+++ plugins/yaml/src/org/jetbrains/yaml/folding/YAMLFoldingBuilder.java
@@ -5,7 +5,6 @@ import com.intellij.lang.folding.FoldingBuilder;
 import com.intellij.lang.folding.FoldingDescriptor;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.tree.TokenSet;
@@ -50,7 +49,7 @@ public class YAMLFoldingBuilder implements FoldingBuilder, DumbAware {
   }
 
   @Nullable
-  public String getPlaceholderText(@NotNull ASTNode node, TextRange range) {
+  public String getPlaceholderText(@NotNull ASTNode node) {
     final IElementType type = node.getElementType();
     if (type == YAMLElementTypes.DOCUMENT){
       return "---";
