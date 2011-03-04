d7171e05895126d56b5af25b5a97d4722bbb02c5
Sergey Evdokimov <sergey.evdokimov@jetbrains.com>
2011-03-04 15:38:03 +0300

    Add references to file-paths in BuildConfig.groovy.

---
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/patterns/GroovyAssignmentExpressionPattern.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2000-2011 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jetbrains.plugins.groovy.lang.psi.patterns;
+
+import com.intellij.patterns.ElementPattern;
+import com.intellij.patterns.PatternCondition;
+import com.intellij.util.ProcessingContext;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrAssignmentExpression;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrBinaryExpression;
+
+public class GroovyAssignmentExpressionPattern extends GroovyExpressionPattern<GrAssignmentExpression, GroovyAssignmentExpressionPattern> {
+  protected GroovyAssignmentExpressionPattern() {
+    super(GrAssignmentExpression.class);
+  }
+
+  public GroovyAssignmentExpressionPattern left(@NotNull final ElementPattern pattern) {
+    return with(new PatternCondition<GrAssignmentExpression>("left") {
+      public boolean accepts(@NotNull final GrAssignmentExpression psiBinaryExpression, final ProcessingContext context) {
+        return pattern.getCondition().accepts(psiBinaryExpression.getLValue(), context);
+      }
+    });
+  }
+
+  public GroovyAssignmentExpressionPattern right(@NotNull final ElementPattern pattern) {
+    return with(new PatternCondition<GrAssignmentExpression>("right") {
+      public boolean accepts(@NotNull final GrAssignmentExpression psiBinaryExpression, final ProcessingContext context) {
+        return pattern.getCondition().accepts(psiBinaryExpression.getRValue(), context);
+      }
+    });
+  }
+
+  public GroovyAssignmentExpressionPattern operation(final ElementPattern pattern) {
+    return with(new PatternCondition<GrAssignmentExpression>("operation") {
+      public boolean accepts(@NotNull final GrAssignmentExpression psiBinaryExpression, final ProcessingContext context) {
+        return pattern.getCondition().accepts(psiBinaryExpression.getOperationToken(), context);
+      }
+    });
+  }
+
+}
