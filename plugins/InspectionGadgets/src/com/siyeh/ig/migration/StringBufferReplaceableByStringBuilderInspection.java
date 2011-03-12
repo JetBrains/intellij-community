d6e1d50915c157508823851b6310fddbac02509b
Bas Leijdekkers <basleijdekkers@gmail.com>
2011-03-12 22:22:25 +0100

    move "'StringBuffer' may be 'StringBuilder'" to migration aids group

--- plugins/InspectionGadgets/src/com/siyeh/ig/migration/StringBufferReplaceableByStringBuilderInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/migration/StringBufferReplaceableByStringBuilderInspection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2010 Dave Griffith, Bas Leijdekkers
+ * Copyright 2003-2011 Dave Griffith, Bas Leijdekkers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.siyeh.ig.performance;
+package com.siyeh.ig.migration;
 
 import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.openapi.project.Project;
