a2c449009a0fdad2f223a859c6dff3c18e97c130
Gregory Shrago <Gregory.Shrago@jetbrains.com>
2011-03-21 18:56:04 +0300

    IDEA-66790 "Private field is never used" does not appear when @Resource annotation used

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/unusedDecls/InjectedAnno.java
@@ -0,0 +1,29 @@
+package javax.annotation;
+
+/*
+* Copyright 2000-2011 JetBrains s.r.o.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+@interface Resource {}
+class Test{
+  @Resource
+  private int <warning descr="Private field 'field' is assigned but never accessed">field</warning>;
+
+  @Resource
+  private int field2;
+
+  int f() {
+    return field2;
+  }
+}
