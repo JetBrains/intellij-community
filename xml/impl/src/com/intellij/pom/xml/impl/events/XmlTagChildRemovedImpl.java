990de8f39708dd0235fc7a5c272ab75eceb6e21a
Alexey Kudravtsev <cdr@intellij.com>
2011-03-22 18:58:51 +0300

    obsolete code

--- xml/impl/src/com/intellij/pom/xml/impl/events/XmlTagChildRemovedImpl.java
+++ xml/impl/src/com/intellij/pom/xml/impl/events/XmlTagChildRemovedImpl.java
@@ -15,17 +15,8 @@
  */
 package com.intellij.pom.xml.impl.events;
 
-import com.intellij.pom.PomModel;
-import com.intellij.pom.event.PomModelEvent;
-import com.intellij.pom.xml.XmlAspect;
 import com.intellij.pom.xml.XmlChangeVisitor;
-import com.intellij.pom.xml.events.XmlChange;
 import com.intellij.pom.xml.events.XmlTagChildRemoved;
-import com.intellij.pom.xml.impl.XmlAspectChangeSetImpl;
-import com.intellij.pom.xml.XmlChangeVisitor;
-import com.intellij.pom.xml.impl.XmlAspectChangeSetImpl;
-import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.psi.xml.XmlFile;
 import com.intellij.psi.xml.XmlTag;
 import com.intellij.psi.xml.XmlTagChild;
 
@@ -45,13 +36,6 @@ public class XmlTagChildRemovedImpl implements XmlTagChildRemoved {
     return myChild;
   }
 
-  public static PomModelEvent createXmlTagChildRemoved(PomModel source, XmlTag context, XmlTagChild treeElement) {
-    final PomModelEvent event = new PomModelEvent(source);
-    final XmlAspectChangeSetImpl xmlAspectChangeSet = new XmlAspectChangeSetImpl(source, PsiTreeUtil.getParentOfType(context, XmlFile.class));
-    xmlAspectChangeSet.add(new XmlTagChildRemovedImpl(context, treeElement));
-    event.registerChangeSet(source.getModelAspect(XmlAspect.class), xmlAspectChangeSet);
-    return event;
-  }
   @SuppressWarnings({"HardCodedStringLiteral"})
   public String toString() {
     return "child removed from " + getTag().getName() + " child: " + myChild.toString();
