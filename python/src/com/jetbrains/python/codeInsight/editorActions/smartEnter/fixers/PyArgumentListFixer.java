384a20364b67d209c61bc71e651994cadfd93e0b
Dmitry Jemerov <yole@jetbrains.com>
2011-03-31 21:49:54 +0200

    smart enter shouldn't insert parentheses into decorator calls (PY-3209)

--- python/src/com/jetbrains/python/codeInsight/editorActions/smartEnter/fixers/PyArgumentListFixer.java
+++ python/src/com/jetbrains/python/codeInsight/editorActions/smartEnter/fixers/PyArgumentListFixer.java
@@ -8,19 +8,17 @@ import com.jetbrains.python.PyTokenTypes;
 import com.jetbrains.python.codeInsight.editorActions.smartEnter.PySmartEnterProcessor;
 import com.jetbrains.python.psi.PyArgumentList;
 import com.jetbrains.python.psi.PyClass;
+import com.jetbrains.python.psi.PyDecorator;
 import com.jetbrains.python.psi.PyUtil;
 
 /**
- * Created by IntelliJ IDEA.
- * Author: Alexey.Ivanov
- * Date:   16.04.2010
- * Time:   19:15:36
+ * @author Alexey.Ivanov
  */
 public class PyArgumentListFixer implements PyFixer {
   public void apply(Editor editor, PySmartEnterProcessor processor, PsiElement psiElement) throws IncorrectOperationException {
     if (psiElement instanceof PyArgumentList) {
       final PsiElement rBrace = PyUtil.getChildByFilter(psiElement, PyTokenTypes.CLOSE_BRACES, 0);
-      if (psiElement.getContext() instanceof PyClass) {
+      if (psiElement.getParent() instanceof PyClass || psiElement.getParent() instanceof PyDecorator) {
         final PsiElement lBrace = PyUtil.getChildByFilter(psiElement, PyTokenTypes.OPEN_BRACES, 0);
         if (lBrace != null && rBrace == null) {
           final Document document = editor.getDocument();
