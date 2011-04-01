f23f4af81cd80c4fb9def84da7128274a1cb119c
Konstantin Bulenkov <kb@jetbrains.com>
2011-04-01 18:54:55 +0400

    Dir Diff very initial

---
+++ platform/vcs-impl/src/com/intellij/openapi/diff/impl/dir/TestDirDiffAction.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2000-2011 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.openapi.diff.impl.dir;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.diff.DirDiffManager;
+import com.intellij.openapi.fileChooser.FileChooserDescriptor;
+import com.intellij.openapi.fileChooser.FileChooserFactory;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+
+/**
+ * @author Konstantin Bulenkov
+ */
+public class TestDirDiffAction extends AnAction {
+  @Override
+  public void actionPerformed(AnActionEvent e) {
+    final Project project = e.getData(PlatformDataKeys.PROJECT);
+    if (project != null) {
+      final FileChooserDescriptor descriptor = new FileChooserDescriptor(false, true, false, false, false, false);
+      final VirtualFile[] files1 = FileChooserFactory.getInstance().createFileChooser(descriptor, project).choose(null, project);
+      final VirtualFile[] files2 = FileChooserFactory.getInstance().createFileChooser(descriptor, project).choose(null, project);
+      if (files1.length == 1 && files2.length == 1) {
+        final DirDiffManager diffManager = DirDiffManager.getInstance(project);
+        if (diffManager.canShow(files1[0], files2[0])) {
+          diffManager.showDiff(files1[0], files2[0]);
+        }
+      }
+    }
+  }
+}
