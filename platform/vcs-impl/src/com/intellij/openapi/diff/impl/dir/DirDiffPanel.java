5c69063fd7fd3131ede119a9f298f0d8b169a606
Konstantin Bulenkov <kb@jetbrains.com>
2011-04-04 16:46:45 +0400

    integrate diff viewer and editor components into dir diff panel

--- platform/vcs-impl/src/com/intellij/openapi/diff/impl/dir/DirDiffPanel.java
+++ platform/vcs-impl/src/com/intellij/openapi/diff/impl/dir/DirDiffPanel.java
@@ -15,10 +15,26 @@
  */
 package com.intellij.openapi.diff.impl.dir;
 
+import com.intellij.openapi.diff.DiffManager;
+import com.intellij.openapi.diff.DiffPanel;
+import com.intellij.openapi.diff.DiffRequest;
+import com.intellij.openapi.diff.SimpleDiffRequest;
+import com.intellij.openapi.diff.impl.DiffPanelImpl;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.EditorFactory;
+import com.intellij.openapi.editor.ex.EditorEx;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.table.JBTable;
 
 import javax.swing.*;
-import javax.swing.table.TableModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 /**
  * @author Konstantin Bulenkov
@@ -28,15 +44,93 @@ public class DirDiffPanel {
   private JBTable myTable;
   private JPanel myComponent;
   private JSplitPane mySplitPanel;
-  private final TableModel myModel;
+  private final DirDiffTableModel myModel;
+  private final DirDiffDialog myDialog;
+  private DiffPanel myDiffPanelComponent;
+  private EditorEx myEditor;
 
-  public DirDiffPanel(TableModel model) {
+  public DirDiffPanel(DirDiffTableModel model, DirDiffDialog dirDiffDialog) {
     myModel = model;
+    myDialog = dirDiffDialog;
     myTable.setModel(myModel);
     final DirDiffTableCellRenderer renderer = new DirDiffTableCellRenderer(myTable);
     myTable.setDefaultRenderer(Object.class, renderer);
-    myTable.setDefaultRenderer(String.class, renderer);
     myTable.getSelectionModel().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+    myTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+      @Override
+      public void valueChanged(ListSelectionEvent e) {
+        final DirDiffElement last = myModel.getElementAt(e.getLastIndex());
+        final DirDiffElement first = myModel.getElementAt(e.getFirstIndex());
+        if (last.isSeparator()) {
+          myTable.getSelectionModel().setLeadSelectionIndex(e.getFirstIndex());
+        }
+        else if (first.isSeparator()) {
+          myTable.getSelectionModel().setLeadSelectionIndex(e.getLastIndex());
+        }
+        else {
+          final DirDiffElement element = myModel.getElementAt(myTable.getSelectedRow());
+          final Project project = myModel.getProject();
+          if (element.getType() == DirDiffElement.ElementType.CHANGED) {
+            final DiffRequest request = SimpleDiffRequest.compareFiles(element.getSource(), element.getTarget(), project);
+            final DiffPanelImpl panel = (DiffPanelImpl)DiffManager.getInstance().createDiffPanel(myDialog.getWindow(), project);
+            panel.setIsRequestFocus(false);
+            panel.setDiffRequest(request);
+
+            clearDiffPanel();
+
+            myDiffPanelComponent = panel;
+            myDiffPanel.add(panel.getComponent(), BorderLayout.CENTER);
+          } else {
+            clearDiffPanel();
+
+            final VirtualFile file = element.getType() == DirDiffElement.ElementType.SOURCE ? element.getSource() : element.getTarget();
+            final Document document = FileDocumentManager.getInstance().getDocument(file);
+            myEditor = (EditorEx)EditorFactory.getInstance().createEditor(document, project, file, true);
+            myEditor.getSettings().setFoldingOutlineShown(false);
+            myDiffPanel.add(myEditor.getComponent(), BorderLayout.CENTER);
+            myDiffPanel.revalidate();
+          }
+        }
+      }
+    });
+    myTable.addKeyListener(new KeyAdapter() {
+      @Override
+      public void keyPressed(KeyEvent e) {
+        final int keyCode = e.getKeyCode();
+        final int rows = myTable.getRowCount();
+        int row = myTable.getSelectedRow();
+        if (keyCode == KeyEvent.VK_DOWN && row != rows - 1) {
+          row++;
+          if (myModel.getElementAt(row).isSeparator()) {
+            row++;
+          }
+        } else if (keyCode == KeyEvent.VK_UP && row != 0) {
+          row--;
+          if (myModel.getElementAt(row).isSeparator()) {
+            row--;
+          }
+        } else {
+          return;
+        }
+        if (0 <= row && row < rows && !myModel.getElementAt(row).isSeparator()) {
+          e.consume();
+          myTable.changeSelection(row, 3, false, false);
+        }
+      }
+    });
+  }
+
+  private void clearDiffPanel() {
+    if (myDiffPanelComponent != null) {
+      myDiffPanel.remove(myDiffPanelComponent.getComponent());
+      Disposer.dispose(((DiffPanelImpl)myDiffPanelComponent));
+      myDiffPanelComponent = null;
+    }
+    if (myEditor != null) {
+      myDiffPanel.remove(myEditor.getComponent());
+      EditorFactory.getInstance().releaseEditor(myEditor);
+      myEditor = null;
+    }
   }
 
   private void createUIComponents() {
@@ -52,5 +146,9 @@ public class DirDiffPanel {
 
   public JSplitPane getSplitPanel() {
     return mySplitPanel;
+  }
+
+  public void dispose() {
+    clearDiffPanel();
   }
 }
