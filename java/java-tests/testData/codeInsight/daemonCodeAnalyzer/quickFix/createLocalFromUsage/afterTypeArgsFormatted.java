dde2360a0cd77635ea29cf6546eada3368bdb0c4
anna <Anna.Kozlova@jetbrains.com>
2011-04-05 16:34:30 +0200

    keep spaces (IDEA-67541 )

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/createLocalFromUsage/afterTypeArgsFormatted.java
@@ -0,0 +1,12 @@
+// "Create Local Variable 'field'" "true"
+import java.util.*;
+class A {
+  void foo(Map<String, String> s){}
+
+  void bar() {
+      Map<String, String> field<caret>;
+      foo(field);
+  }
+
+}
+
