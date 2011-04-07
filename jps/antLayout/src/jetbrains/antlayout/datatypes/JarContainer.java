b89242abe6d5538b1b5b0b74be2ee642ca7b93aa
nik <Nikolay.Chashnikov@jetbrains.com>
2011-04-07 15:03:36 +0400

    added workaround for Ant bug in 1.8.0 and 1.8.1 versions regarding custom manifest (IDEA-66287)

--- jps/antLayout/src/jetbrains/antlayout/datatypes/JarContainer.java
+++ jps/antLayout/src/jetbrains/antlayout/datatypes/JarContainer.java
@@ -1,11 +1,8 @@
 package jetbrains.antlayout.datatypes;
 
-import org.apache.tools.ant.taskdefs.Zip;
-import org.apache.tools.ant.taskdefs.Jar;
-import org.apache.tools.ant.taskdefs.Manifest;
-import org.apache.tools.ant.taskdefs.ManifestException;
+import org.apache.tools.ant.Main;
+import org.apache.tools.ant.taskdefs.*;
 import org.apache.tools.ant.types.ZipFileSet;
-import org.apache.tools.ant.types.Path;
 
 import java.io.File;
 
@@ -13,34 +10,41 @@ import java.io.File;
  * @author max
  */
 public class JarContainer extends ZipContainer {
-    protected Zip createTask() {
-        Jar task = new Jar();
-        task.setTaskName("jar");
-        task.setWhenmanifestonly((Zip.WhenEmpty) Zip.WhenEmpty.getInstance(Zip.WhenEmpty.class, "skip"));
-        return task;
+  protected Zip createTask() {
+    String version = Main.getAntVersion();
+    Jar task;
+    if (version != null && (version.indexOf("1.8.0") != -1 || version.indexOf("1.8.1") != -1)) {
+      task = new PatchedJar();
     }
+    else {
+      task = new Jar();
+    }
+    task.setTaskName("jar");
+    task.setWhenmanifestonly((Zip.WhenEmpty) Zip.WhenEmpty.getInstance(Zip.WhenEmpty.class, "skip"));
+    return task;
+  }
 
-    public void setIndex(boolean flag) {
-        ((Jar) task).setIndex(flag);
-    }
+  public void setIndex(boolean flag) {
+    ((Jar) task).setIndex(flag);
+  }
 
-    public void setManifestEncoding(String manifestEncoding) {
-        ((Jar) task).setManifestEncoding(manifestEncoding);
-    }
+  public void setManifestEncoding(String manifestEncoding) {
+    ((Jar) task).setManifestEncoding(manifestEncoding);
+  }
 
-    public void setManifest(File manifestFile) {
-        ((Jar) task).setManifest(manifestFile);
-    }
+  public void setManifest(File manifestFile) {
+    ((Jar) task).setManifest(manifestFile);
+  }
 
-    public void setFilesetmanifest(Jar.FilesetManifestConfig config) {
-        ((Jar) task).setFilesetmanifest(config);
-    }
-    
-    public void addConfiguredManifest(Manifest newManifest) throws ManifestException {
-        ((Jar) task).addConfiguredManifest(newManifest);
-    }
- 
-    public void addMetainf(ZipFileSet fs) {
-        ((Jar) task).addMetainf(fs);
-    }
+  public void setFilesetmanifest(Jar.FilesetManifestConfig config) {
+    ((Jar) task).setFilesetmanifest(config);
+  }
+
+  public void addConfiguredManifest(Manifest newManifest) throws ManifestException {
+    ((Jar) task).addConfiguredManifest(newManifest);
+  }
+
+  public void addMetainf(ZipFileSet fs) {
+    ((Jar) task).addMetainf(fs);
+  }
 }
