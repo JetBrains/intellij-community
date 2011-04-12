8486f7aba2462f4925a63251b5bd39be3e865066
Gregory Shrago <Gregory.Shrago@jetbrains.com>
2011-04-12 18:16:42 +0400

    IDEA-67670 Inspection highlights incorrect.. (obsolete extra prefix/suffix handling removed)

--- plugins/IntelliLang/src/org/intellij/plugins/intelliLang/inject/InjectorUtils.java
+++ plugins/IntelliLang/src/org/intellij/plugins/intelliLang/inject/InjectorUtils.java
@@ -17,8 +17,6 @@
 package org.intellij.plugins.intelliLang.inject;
 
 import com.intellij.lang.Language;
-import com.intellij.lang.LanguageLiteralEscapers;
-import com.intellij.lang.LiteralEscaper;
 import com.intellij.lang.injection.MultiHostRegistrar;
 import com.intellij.openapi.extensions.Extensions;
 import com.intellij.openapi.util.Condition;
@@ -53,53 +51,6 @@ public class InjectorUtils {
   private InjectorUtils() {
   }
 
-  public static void addPlaceSafe(MultiHostRegistrar registrar, String prefix, String suffix, PsiLanguageInjectionHost host, TextRange textRange) {
-    registrar.addPlace(prefix, suffix, host, textRange);
-  }
-
-  // See http://www.jetbrains.net/jira/browse/IDEADEV-8302#action_111865
-  // This code assumes that for the injected language a single space character is a token separator
-  // that doesn't (significantly) change the semantics if added to the prefix/suffix
-  //
-  // NOTE: This does not work in all cases, such as string literals in JavaScript where a
-  // space character isn't a token separator. See also comments in IDEA-8561
-  public static void adjustPrefixAndSuffix(String text, StringBuilder prefix, StringBuilder suffix) {
-    if (prefix.length() > 0) {
-      if (!endsWithSpace(prefix) && !startsWithSpace(text)) {
-        prefix.append(" ");
-      }
-      else if (endsWithSpace(prefix) && startsWithSpace(text)) {
-        trim(prefix);
-      }
-    }
-    if (suffix.length() > 0) {
-      if (text.length() == 0) {
-        // avoid to stick whitespace from prefix and suffix together
-        trim(suffix);
-      }
-      else if (!startsWithSpace(suffix) && !endsWithSpace(text)) {
-        suffix.insert(0, " ");
-      }
-      else if (startsWithSpace(suffix) && endsWithSpace(text)) {
-        trim(suffix);
-      }
-    }
-  }
-
-  public static void trim(StringBuilder string) {
-    while (startsWithSpace(string)) string.deleteCharAt(0);
-    while (endsWithSpace(string)) string.deleteCharAt(string.length() - 1);
-  }
-
-  public static boolean startsWithSpace(CharSequence sequence) {
-    final int length = sequence.length();
-    return length > 0 && sequence.charAt(0) <= ' ';
-  }
-
-  public static boolean endsWithSpace(CharSequence sequence) {
-    final int length = sequence.length();
-    return length > 0 && sequence.charAt(length - 1) <= ' ';
-  }
 
   public static void registerInjection(Language language, List<Trinity<PsiLanguageInjectionHost, InjectedLanguage, TextRange>> list, PsiFile containingFile, MultiHostRegistrar registrar) {
     // if language isn't injected when length == 0, subsequent edits will not cause the language to be injected as well.
@@ -107,7 +58,6 @@ public class InjectorUtils {
     if (language == null/* && (pair.second.getLength() > 0*/) {
       return;
     }
-    final LiteralEscaper literalEscaper = LanguageLiteralEscapers.INSTANCE.forLanguage(containingFile.getLanguage());
     boolean injectionStarted = false;
     for (Trinity<PsiLanguageInjectionHost, InjectedLanguage, TextRange> trinity : list) {
       final PsiLanguageInjectionHost host = trinity.first;
@@ -120,21 +70,7 @@ public class InjectorUtils {
         registrar.startInjecting(language);
         injectionStarted = true;
       }
-      if (injectedLanguage.isDynamic()) {
-        // Only adjust prefix/suffix if it has been computed dynamically. Otherwise some other
-        // useful cases may break. This system is far from perfect still...
-        final StringBuilder prefix = new StringBuilder(injectedLanguage.getPrefix());
-        final StringBuilder suffix = new StringBuilder(injectedLanguage.getSuffix());
-        final String text = textRange.substring(host.getText());
-        final String result = literalEscaper != null ? literalEscaper.unescapeText(text) : text;
-
-        adjustPrefixAndSuffix(result, prefix, suffix);
-
-        addPlaceSafe(registrar, prefix.toString(), suffix.toString(), host, textRange);
-      }
-      else {
-        addPlaceSafe(registrar, injectedLanguage.getPrefix(), injectedLanguage.getSuffix(), host, textRange);
-      }
+      registrar.addPlace(injectedLanguage.getPrefix(), injectedLanguage.getSuffix(), host, textRange);
     }
     if (injectionStarted) {
       registrar.doneInjecting();
