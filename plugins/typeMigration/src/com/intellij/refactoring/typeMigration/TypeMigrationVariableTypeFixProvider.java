d79b8488570440cdfacd8b1a28586c401a97fd37
Alexey Kudravtsev <cdr@intellij.com>
2011-04-12 11:14:16 +0400

    PSI leaks via LocalQuickFix

--- plugins/typeMigration/src/com/intellij/refactoring/typeMigration/TypeMigrationVariableTypeFixProvider.java
+++ plugins/typeMigration/src/com/intellij/refactoring/typeMigration/TypeMigrationVariableTypeFixProvider.java
@@ -12,6 +12,7 @@ import com.intellij.openapi.command.undo.UndoUtil;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiType;
 import com.intellij.psi.PsiVariable;
@@ -28,20 +29,26 @@ public class TypeMigrationVariableTypeFixProvider implements ChangeVariableTypeQ
     return new IntentionAction[]{new VariableTypeFix(variable, toReturn) {
       @NotNull
       @Override
-      public String getName() {
-        return "Migrate \'" + getVariable().getName() + "\' type to \'" + getReturnType().getCanonicalText() + "\'";
+      public String getText() {
+        return "Migrate \'" + myName + "\' type to \'" + getReturnType().getCanonicalText() + "\'";
       }
 
       @Override
-      public void applyFix(Project project, PsiFile file, @Nullable Editor editor) {
-        if (!CodeInsightUtilBase.prepareFileForWrite(getVariable().getContainingFile())) return;
+      public void invoke(@NotNull Project project,
+                         @NotNull PsiFile file,
+                         @Nullable("is null when called from inspection") Editor editor,
+                         @NotNull PsiElement startElement,
+                         @NotNull PsiElement endElement) {
+        final PsiVariable myVariable = (PsiVariable)startElement;
+
+        if (!CodeInsightUtilBase.prepareFileForWrite(myVariable.getContainingFile())) return;
         try {
-          getVariable().normalizeDeclaration();
-          final TypeMigrationRules rules = new TypeMigrationRules(TypeMigrationLabeler.getElementType(getVariable()));
+          myVariable.normalizeDeclaration();
+          final TypeMigrationRules rules = new TypeMigrationRules(TypeMigrationLabeler.getElementType(myVariable));
           rules.setMigrationRootType(getReturnType());
           rules.setBoundScope(GlobalSearchScope.projectScope(project));
-          TypeMigrationProcessor.runHighlightingTypeMigration(project, editor, rules, getVariable());
-          JavaCodeStyleManager.getInstance(project).shortenClassReferences(getVariable());
+          TypeMigrationProcessor.runHighlightingTypeMigration(project, editor, rules, myVariable);
+          JavaCodeStyleManager.getInstance(project).shortenClassReferences(myVariable);
           UndoUtil.markPsiFileForUndo(file);
         }
         catch (IncorrectOperationException e) {
