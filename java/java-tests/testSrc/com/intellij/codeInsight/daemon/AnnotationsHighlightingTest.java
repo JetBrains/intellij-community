370794e5f703493abdb8414f365a96b562d99ea3
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2011-04-18 15:55:34 +0200

    Cleanup

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/AnnotationsHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/AnnotationsHighlightingTest.java
@@ -1,33 +1,31 @@
 package com.intellij.codeInsight.daemon;
 
-import com.intellij.openapi.projectRoots.Sdk;
-import com.intellij.openapi.projectRoots.impl.JavaSdkImpl;
 import org.jetbrains.annotations.NonNls;
 
 /**
  * @author ven
  */
 public class AnnotationsHighlightingTest extends LightDaemonAnalyzerTestCase {
-
-  @NonNls private static final String BASE_PATH = "/codeInsight/daemonCodeAnalyzer/annotations";
+  @NonNls
+  private static final String BASE_PATH = "/codeInsight/daemonCodeAnalyzer/annotations";
 
   private void doTest(boolean checkWarnings) throws Exception {
-    doTest(BASE_PATH + "/" + getTestName(false) + ".java", checkWarnings, false);
+    doTest(BASE_PATH + "/" + getTestName(true) + ".java", checkWarnings, false);
   }
 
-  public void testnotValueNameOmmited() throws Exception { doTest(false); }
-  public void testcannotFindMethod() throws Exception { doTest(false); }
-  public void testincompatibleType1() throws Exception { doTest(false); }
-  public void testincompatibleType2() throws Exception { doTest(false); }
-  public void testincompatibleType3() throws Exception { doTest(false); }
-  public void testincompatibleType4() throws Exception { doTest(false); }
-  public void testmissingAttribute() throws Exception { doTest(false); }
-  public void testduplicateAnnotation() throws Exception { doTest(false); }
-  public void testnonconstantInitializer() throws Exception { doTest(false); }
-  public void testinvalidType() throws Exception { doTest(false); }
-  public void testinapplicable() throws Exception { doTest(false); }
-  public void testduplicateAttribute() throws Exception { doTest(false); }
-  public void testduplicateTarget() throws Exception { doTest(false); }
+  public void testNotValueNameOmitted() throws Exception { doTest(false); }
+  public void testCannotFindMethod() throws Exception { doTest(false); }
+  public void testIncompatibleType1() throws Exception { doTest(false); }
+  public void testIncompatibleType2() throws Exception { doTest(false); }
+  public void testIncompatibleType3() throws Exception { doTest(false); }
+  public void testIncompatibleType4() throws Exception { doTest(false); }
+  public void testMissingAttribute() throws Exception { doTest(false); }
+  public void testDuplicateAnnotation() throws Exception { doTest(false); }
+  public void testNonConstantInitializer() throws Exception { doTest(false); }
+  public void testInvalidType() throws Exception { doTest(false); }
+  public void testInapplicable() throws Exception { doTest(false); }
+  public void testDuplicateAttribute() throws Exception { doTest(false); }
+  public void testDuplicateTarget() throws Exception { doTest(false); }
   public void testTypeAnnotations() throws Exception { doTest(false); }
 
   public void testInvalidPackageAnnotationTarget() throws Exception {
