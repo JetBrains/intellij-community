a5fb8b1e5fee792fa47e0eb1f7a39d5167a28055
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2011-04-21 13:11:44 +0400

    added paramenters recognized by sphinx

--- python/src/com/jetbrains/python/documentation/SphinxDocString.java
+++ python/src/com/jetbrains/python/documentation/SphinxDocString.java
@@ -7,7 +7,9 @@ import java.util.List;
  */
 public class SphinxDocString extends StructuredDocString {
   public static String[] KEYWORD_ARGUMENT_TAGS = new String[] { "keyword", "key" };
-  public static String[] ALL_TAGS = new String[] { ":param", ":keyword", ":type", ":raise", ":raises", ":var", ":cvar", ":ivar", ":returns", ":rtype" };
+  public static String[] ALL_TAGS = new String[] { ":param", ":parameter", ":arg", ":argument", ":keyword", ":key",
+                                                   ":type", ":raise", ":raises", ":var", ":cvar", ":ivar",
+                                                   ":return", ":returns", ":rtype", ":except", ":exception" };
 
   public SphinxDocString(String docstringText) {
     super(docstringText, ":");
