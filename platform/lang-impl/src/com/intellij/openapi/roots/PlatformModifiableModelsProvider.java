18695d9acc4059a737f9e8cae7bca7930bea32f2
Dmitry Jemerov <yole@jetbrains.com>
2011-04-26 21:21:36 +0200

    move ModifiableModelsProvider from ruby to platform

---
+++ platform/lang-impl/src/com/intellij/openapi/roots/PlatformModifiableModelsProvider.java
@@ -0,0 +1,26 @@
+package com.intellij.openapi.roots;
+
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.roots.libraries.LibraryTable;
+import com.intellij.openapi.roots.libraries.LibraryTablesRegistrar;
+
+/**
+ * @author Dennis.Ushakov
+ */
+public class PlatformModifiableModelsProvider implements ModifiableModelsProvider {
+  public ModifiableRootModel getModuleModifiableModel(final Module module) {
+    return ModuleRootManager.getInstance(module).getModifiableModel();
+  }
+
+  public void commitModuleModifiableModel(final ModifiableRootModel model) {
+    model.commit();
+  }
+
+  public void disposeModuleModifiableModel(final ModifiableRootModel model) {
+    model.dispose();
+  }
+
+  public LibraryTable.ModifiableModel getLibraryTableModifiableModel() {
+    return LibraryTablesRegistrar.getInstance().getLibraryTable().getModifiableModel();
+  }
+}
