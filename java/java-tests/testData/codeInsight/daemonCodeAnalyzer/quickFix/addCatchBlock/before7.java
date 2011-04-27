6cf67523bd87135b1e133550284f90716fbba67f
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2011-04-27 15:26:06 +0200

    [ann] IDEA-68850 (add empty try block if try statement is incomplete)

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/addCatchBlock/before7.java
@@ -0,0 +1,14 @@
+// "Add Catch Clause(s)" "true"
+class Test {
+    static class E1 extends Exception { }
+    static class E2 extends Exception { }
+
+    static class MyResource implements AutoCloseable {
+        public MyResource() throws E1 { }
+        public void close() throws E2 { }
+    }
+
+    void m() {
+        try (MyResource r = <caret>new MyResource())
+    }
+}
