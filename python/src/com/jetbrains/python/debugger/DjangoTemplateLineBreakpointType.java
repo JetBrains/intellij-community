c4c76464ab1927bef8a66564ad02d6118dd4db58
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2011-04-28 18:07:13 +0400

    Prohibited setting breakpoints to lines without Django tags.

--- python/src/com/jetbrains/python/debugger/DjangoTemplateLineBreakpointType.java
+++ python/src/com/jetbrains/python/debugger/DjangoTemplateLineBreakpointType.java
@@ -5,14 +5,13 @@ import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Ref;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiComment;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiWhiteSpace;
 import com.intellij.util.Processor;
 import com.intellij.xdebugger.XDebuggerUtil;
 import com.intellij.xdebugger.breakpoints.XBreakpointProperties;
 import com.intellij.xdebugger.breakpoints.XLineBreakpointType;
 import com.intellij.xdebugger.evaluation.XDebuggerEditorsProvider;
+import com.jetbrains.django.lang.template.parsing.DjangoTemplateTokenTypes;
 import com.jetbrains.django.util.DjangoUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -32,10 +31,13 @@ public class DjangoTemplateLineBreakpointType extends XLineBreakpointType<XBreak
       if (DjangoUtil.isDjangoTemplateDocument(document, project)) {
         XDebuggerUtil.getInstance().iterateLine(project, document, line, new Processor<PsiElement>() {
           public boolean process(PsiElement psiElement) {
-            if (psiElement instanceof PsiWhiteSpace || psiElement instanceof PsiComment) return true;
-            // Python debugger seems to be able to stop on pretty much everything
-            stoppable.set(true);
-            return false;
+            if (psiElement != null && (psiElement.getNode().getElementType() == DjangoTemplateTokenTypes.DJANGO_EXPRESSION_START ||
+                                       psiElement.getNode().getElementType() == DjangoTemplateTokenTypes.DJANGO_TAG_START)) {
+              stoppable.set(true);
+              return false;
+            } else {
+              return true;
+            }
           }
         });
       }
