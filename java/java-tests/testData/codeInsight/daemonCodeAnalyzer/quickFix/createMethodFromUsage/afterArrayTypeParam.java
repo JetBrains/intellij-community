2b98c3ab3529a3b1a0f34a3e3e4f62d393ce4d61
anna <Anna.Kozlova@jetbrains.com>
2011-05-03 13:31:29 +0200

    insert template: do not handle [] twice (IDEA-69106)

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/createMethodFromUsage/afterArrayTypeParam.java
@@ -0,0 +1,11 @@
+// "Create Method 'test'" "true"
+public class Test {
+    public Test() {
+        byte[] bytes = new byte[0];
+        test(bytes);
+    }
+
+    private void test(byte[] bytes) {
+        <selection>//To change body of created methods use File | Settings | File Templates.</selection>
+    }
+}
