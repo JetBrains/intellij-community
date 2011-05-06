eedd2998ac9d742ca3f4f4f5e74a0e095ad349fa
andrey zaytsev <andrey.zaytsev@jetbrains.com>
2011-05-06 17:20:37 +0400

    replace: fix from xena cherry-pick. use selection when starting IncrementalFind.

--- platform/lang-impl/src/com/intellij/openapi/editor/actions/IncrementalFindAction.java
+++ platform/lang-impl/src/com/intellij/openapi/editor/actions/IncrementalFindAction.java
@@ -57,7 +57,7 @@ public class IncrementalFindAction extends EditorAction {
             model = new FindModel();
             model.copyFrom(findManager.getFindInFileModel());
           }
-          FindUtil.configureFindModel(myReplace, myReplace ? editor.getSelectionModel().getSelectedText() : null, model);
+          FindUtil.configureFindModel(myReplace, editor.getSelectionModel().getSelectedText(), model);
           final EditorSearchComponent header = new EditorSearchComponent(editor, project, model);
           editor.setHeaderComponent(header);
           header.requestFocus();
