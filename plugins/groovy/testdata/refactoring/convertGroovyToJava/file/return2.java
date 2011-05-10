3b5872c1a0ff59dc8e326436fb29280a7d253fd3
peter <peter@jetbrains.com>
2011-05-10 10:17:39 +0200

    don't infer groovy method return types if they're written and unless you have to (e.g. tuples)

--- plugins/groovy/testdata/refactoring/convertGroovyToJava/file/return2.java
+++ plugins/groovy/testdata/refactoring/convertGroovyToJava/file/return2.java
@@ -8,7 +8,7 @@ return null;
 
 }
 
-public boolean foo() {
+public java.lang.Boolean foo() {
 java.lang.Integer a = 5;
 org.codehaus.groovy.runtime.DefaultGroovyMethods.times(a, new groovy.lang.Closure<java.lang.Integer>(this, this) {
 public java.lang.Integer doCall(java.lang.Object it) {
