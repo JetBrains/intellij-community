13d8d697a0cf3b817649dc16842e8be21756a146
Dmitry Cheryasov <dmitry.cheryasov@jetbrains.com>
2011-05-17 13:37:51 +0300

    PY-3567: _socket.error circular import in skeletons.

--- python/helpers/generator3.py
+++ python/helpers/generator3.py
@@ -24,7 +24,7 @@ but seemingly no one uses them in C extensions yet anyway.
 # * re.search-bound, ~30% time, in likes of builtins and _gtk with complex docstrings.
 # None of this can seemingly be easily helped. Maybe there's a simpler and faster parser library?
 
-VERSION = "1.88" # Must be a number-dot-number string, updated with each change that affects generated skeletons
+VERSION = "1.89" # Must be a number-dot-number string, updated with each change that affects generated skeletons
 # Note: DON'T FORGET TO UPDATE!
 
 import sys
@@ -1034,7 +1034,8 @@ class ModuleRedeclarator(object):
         "numpy.core._dotblas",
         "numpy.core.umath",
         "_collections",
-        "_functools"
+        "_functools",
+        "_socket", # .error, etc
     )
 
     # names that look genuinely exported but aren't.
