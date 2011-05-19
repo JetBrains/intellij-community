d1c9b3c691e0bb3ab8b62f4460b69d7d95a41bd1
anna <anna.kozlova@jetbrains.com>
2011-05-19 13:05:43 +0400

    diamonds: cannot use diamonds with explicit constructor types/disable checks on type args for non generics constructors (sic!?)(cherry picked from commit 097a3505972e9f921a353f2b030b015419e7b81f)[romka]

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/advHighlighting7/DiamondNeg12.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/advHighlighting7/DiamondNeg12.java
@@ -4,7 +4,7 @@ class Neg12 {
         <T> Foo(T t) {}
     }
 
-    Foo<Integer> fi1 = new <<error descr="Actual type argument and inferred type contradict each other">String</error>> Foo<>(1);
+    Foo<Integer> fi1 = new <String> Foo<<error descr="Cannot use diamonds with explicit type parameters for constructor"></error>>(1);
     Foo<Integer> fi2 = new <<error descr="Actual type argument and inferred type contradict each other">String</error>> Foo<Integer>(1);
     Foo<Integer> fi3 = new Foo<Integer>(1);
 }
