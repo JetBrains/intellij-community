2451a3a5a8593ec443c273519628eacfe3878932
Alexey Kudravtsev <cdr@intellij.com>
2011-05-24 19:17:49 +0400

    conserve memory by using 3 bits in flags to encode some widespread extensions

--- platform/util/src/com/intellij/openapi/util/text/StringUtil.java
+++ platform/util/src/com/intellij/openapi/util/text/StringUtil.java
@@ -1973,4 +1973,30 @@ public class StringUtil {
     }
     return true;
   }
+
+  public static boolean charsMatch(char c1, char c2, boolean ignoreCase) {
+    // duplicating String.equalsIgnoreCase logic
+    if (c1 == c2) {
+      return true;
+    }
+    if (ignoreCase) {
+      // If characters don't match but case may be ignored,
+      // try converting both characters to uppercase.
+      // If the results match, then the comparison scan should
+      // continue.
+      char u1 = Character.toUpperCase(c1);
+      char u2 = Character.toUpperCase(c2);
+      if (u1 == u2) {
+        return true;
+      }
+      // Unfortunately, conversion to uppercase does not work properly
+      // for the Georgian alphabet, which has strange rules about case
+      // conversion.  So we need to make one last check before
+      // exiting.
+      if (Character.toLowerCase(u1) == Character.toLowerCase(u2)) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
