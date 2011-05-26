919b0587520b08996d64b970b124d5d44824977e
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2011-05-26 21:04:08 +0400

    Bumped skeleton generator version for PY-3655

--- python/helpers/generator3.py
+++ python/helpers/generator3.py
@@ -24,7 +24,7 @@ but seemingly no one uses them in C extensions yet anyway.
 # * re.search-bound, ~30% time, in likes of builtins and _gtk with complex docstrings.
 # None of this can seemingly be easily helped. Maybe there's a simpler and faster parser library?
 
-VERSION = "1.92" # Must be a number-dot-number string, updated with each change that affects generated skeletons
+VERSION = "1.93" # Must be a number-dot-number string, updated with each change that affects generated skeletons
 # Note: DON'T FORGET TO UPDATE!
 
 import sys
