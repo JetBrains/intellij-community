10ecb6ce0dc2abc33363a6290f4747914f337b20
anna <anna.kozlova@jetbrains.com>
2011-05-30 16:08:46 +0400

    can be static extension condition: make it psiElement (class initializer is not PsiNamedElement though it can be static)

--- plugins/testng/src/com/theoryinpractice/testng/inspection/TestNGCanBeStaticExtension.java
+++ plugins/testng/src/com/theoryinpractice/testng/inspection/TestNGCanBeStaticExtension.java
@@ -21,13 +21,12 @@
 package com.theoryinpractice.testng.inspection;
 
 import com.intellij.openapi.util.Condition;
-import com.intellij.psi.PsiMember;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiMethod;
-import com.intellij.psi.PsiNamedElement;
 import com.theoryinpractice.testng.util.TestNGUtil;
 
-public class TestNGCanBeStaticExtension implements Condition<PsiNamedElement> {
-  public boolean value(PsiNamedElement member) {
+public class TestNGCanBeStaticExtension implements Condition<PsiElement> {
+  public boolean value(PsiElement member) {
     if (member instanceof PsiMethod) {
       PsiMethod method = (PsiMethod)member;
       return TestNGUtil.hasTest(method, false) || TestNGUtil.hasConfig(method);
