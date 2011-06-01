47d685b3b8a078aea86465ac044b44cad50991e6
Eugene Kudelevsky <Eugene.Kudelevsky@jetbrains.com>
2011-06-01 22:50:43 +0400

    support anonymization for php duplocator

--- plugins/structuralsearch/source/com/intellij/structuralsearch/duplicates/PhpDuplicatesProfile.java
+++ plugins/structuralsearch/source/com/intellij/structuralsearch/duplicates/PhpDuplicatesProfile.java
@@ -2,10 +2,10 @@ package com.intellij.structuralsearch.duplicates;
 
 import com.intellij.lang.Language;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.tree.TokenSet;
 import com.jetbrains.php.lang.PhpLanguage;
-import com.jetbrains.php.lang.psi.elements.Function;
-import com.jetbrains.php.lang.psi.elements.PhpExpression;
-import com.jetbrains.php.lang.psi.elements.Statement;
+import com.jetbrains.php.lang.lexer.PhpTokenTypes;
+import com.jetbrains.php.lang.psi.elements.*;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -26,5 +26,26 @@ public class PhpDuplicatesProfile extends SSRDuplicatesProfileBase {
   @Override
   public boolean isMyLanguage(@NotNull Language language) {
     return language.isKindOf(PhpLanguage.INSTANCE);
+  }
+
+  @Override
+  public PsiElementRole getRole(@NotNull PsiElement element) {
+    final PsiElement parent = element.getParent();
+
+    if (parent instanceof Variable && ((Variable)parent).getNameIdentifier() == element) {
+      return PsiElementRole.VARIABLE_NAME;
+    }
+    else if (parent instanceof Field && ((Field)parent).getNameIdentifier() == element) {
+      return PsiElementRole.FIELD_NAME;
+    }
+    else if (parent instanceof Function && ((Function)parent).getNameIdentifier() == element) {
+      return PsiElementRole.FUNCTION_NAME;
+    }
+    return null;
+  }
+
+  @Override
+  public TokenSet getLiterals() {
+    return PhpTokenTypes.tsCOMMON_SCALARS;
   }
 }
