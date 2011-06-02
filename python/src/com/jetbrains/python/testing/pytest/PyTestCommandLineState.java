67777ff86aeac31c0e299af0cacafbef59b7ef55
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2011-06-02 18:31:59 +0400

    added traceback filter for py.tests. PY-3781

--- python/src/com/jetbrains/python/testing/pytest/PyTestCommandLineState.java
+++ python/src/com/jetbrains/python/testing/pytest/PyTestCommandLineState.java
@@ -1,10 +1,16 @@
 package com.jetbrains.python.testing.pytest;
 
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.Executor;
 import com.intellij.execution.configurations.GeneralCommandLine;
 import com.intellij.execution.configurations.ParamsGroup;
+import com.intellij.execution.process.ProcessHandler;
 import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.execution.ui.ConsoleView;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.text.StringUtil;
 import com.jetbrains.python.testing.PythonTestCommandLineStateBase;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,5 +45,12 @@ public class PyTestCommandLineState extends PythonTestCommandLineStateBase {
       specs.add(keywords);
     }
     return specs;
+  }
+    @NotNull
+  protected ConsoleView createAndAttachConsole(Project project, ProcessHandler processHandler, Executor executor)
+    throws ExecutionException {
+    final ConsoleView consoleView = super.createAndAttachConsole(project, processHandler, executor);
+    consoleView.addMessageFilter(new PyTestTracebackFilter(project, myConfiguration.getWorkingDirectory()));
+    return consoleView;
   }
 }
