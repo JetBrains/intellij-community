bf1f7fd5c99d0c1d7a1a023f02b78f3a379281d4
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2011-06-03 13:58:10 +0400

    fixed [PY-3596] Output console of test runner prints \n\n as single n\

--- platform/smRunner/src/com/intellij/execution/testframework/sm/runner/OutputToGeneralTestEventsConverter.java
+++ platform/smRunner/src/com/intellij/execution/testframework/sm/runner/OutputToGeneralTestEventsConverter.java
@@ -49,6 +49,7 @@ public class OutputToGeneralTestEventsConverter implements ProcessOutputConsumer
   private final MyServiceMessageVisitor myServiceMessageVisitor;
   private final String myTestFrameworkName;
   private boolean myStdinSupportEnabled;
+  private boolean myPendingLineBreakFlag;
 
   private static class OutputChunk {
     private final Key myKey;
@@ -91,7 +92,7 @@ public class OutputToGeneralTestEventsConverter implements ProcessOutputConsumer
     // if text is multi line:
     if (textLength > 1 && text.substring(0, text.length() - 1).contains("\n")) {
       // some fake process handler may not auto-split text in lines
-      final String[] lines = StringUtil.splitByLines(text);
+      final List<String> lines = StringUtil.split(text, "\n", false);
       for (String line : lines) {
         processLine(line, outputType);
       }
@@ -117,6 +118,13 @@ public class OutputToGeneralTestEventsConverter implements ProcessOutputConsumer
    */
   public void flushBufferBeforeTerminating() {
     flushStdOutputBuffer();
+    if (myPendingLineBreakFlag) {
+      fireOnUncapturedLineBreak();
+    }
+  }
+
+  private void fireOnUncapturedLineBreak() {
+    fireOnUncapturedOutput("\n", ProcessOutputTypes.STDOUT);
   }
 
   public void dispose() {
@@ -192,18 +200,35 @@ public class OutputToGeneralTestEventsConverter implements ProcessOutputConsumer
   private void processConsistentText(final String text, final Key outputType, boolean tcLikeFakeOutput) {
     try {
       if (!processServiceMessages(text, outputType, myServiceMessageVisitor)) {
+        if (myPendingLineBreakFlag) {
+          // output type for line break isn't important
+          // we may use any, e.g. current one
+          fireOnUncapturedLineBreak();
+          myPendingLineBreakFlag = false;
+        }
         // Filters \n
-        if (text.equals("\n") && tcLikeFakeOutput) {
+        String outputToProcess = text;
+        if (tcLikeFakeOutput && text.endsWith("\n")) {
           // ServiceMessages protocol requires that every message
           // should start with new line, so such behaviour may led to generating
           // some number of useless \n.
           //
-          // This will not affect tests output because all
+          // IDEA process handler flush output by size or line break
+          // So:
+          //  1. "a\n\nb\n" -> ["a\n", "\n", "b\n"]
+          //  2. "a\n##teamcity[..]\n" -> ["a\n", "#teamcity[..]\n"]
+          // We need distinguish 1) and 2) cases, in 2) first linebreak is redundant and must be ignored
+          // in 2) linebreak must be considered as output
           // output will be in TestOutput message
-          return;
+          // Lets set myPendingLineBreakFlag if we meet "\n" and then ignore it or apply depending on
+          // next output chunk
+          myPendingLineBreakFlag = true;
+          outputToProcess = outputToProcess.substring(0, outputToProcess.length() - 1);
         }
         //fire current output
-        fireOnUncapturedOutput(text, outputType);
+        fireOnUncapturedOutput(outputToProcess, outputType);
+      } else {
+        myPendingLineBreakFlag = false;
       }
     }
     catch (ParseException e) {
@@ -319,6 +344,10 @@ public class OutputToGeneralTestEventsConverter implements ProcessOutputConsumer
 
   private void fireOnUncapturedOutput(final String text, final Key outputType) {
     assertNotNull(text);
+
+    if (StringUtil.isEmpty(text)) {
+      return;
+    }
 
     // local variable is used to prevent concurrent modification
     final GeneralTestEventsProcessor processor = myProcessor;
