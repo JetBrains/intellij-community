3ff76bec4e67f8c312e43601736de02b2df93066
anna <anna.kozlova@jetbrains.com>
2011-06-14 12:58:11 +0400

    EA-27726 - IOE: PsiJavaParserFacadeImpl.createTypeElementFromText

--- java/java-impl/src/com/intellij/refactoring/changeSignature/JavaChangeSignatureDetector.java
+++ java/java-impl/src/com/intellij/refactoring/changeSignature/JavaChangeSignatureDetector.java
@@ -81,7 +81,7 @@ public class JavaChangeSignatureDetector implements LanguageChangeSignatureDetec
         final MyJavaChangeInfo info = (MyJavaChangeInfo)changeInfo;
         if (!info.getMethod().equals(method)) return null;
         if (!info.equals(fromMethod)) {
-          createParametersInfo(element, parameterInfos, info);
+          if (!createParametersInfo(element, parameterInfos, info)) return null;
           if (info.isReturnTypeChanged()) {
             final String visibility = info.getNewVisibility();
             if (Comparing.strEqual(visibility, PsiModifier.PRIVATE) &&
@@ -138,9 +138,9 @@ public class JavaChangeSignatureDetector implements LanguageChangeSignatureDetec
     return null;
   }
 
-  private static void createParametersInfo(PsiElement element,
-                                           ParameterInfoImpl[] parameterInfos,
-                                           MyJavaChangeInfo info) {
+  private static boolean createParametersInfo(PsiElement element,
+                                              ParameterInfoImpl[] parameterInfos,
+                                              MyJavaChangeInfo info) {
 
     final JavaParameterInfo[] oldParameters = info.getNewParameters();
     final String[] oldParameterNames = info.getOldParameterNames();
@@ -178,12 +178,15 @@ public class JavaChangeSignatureDetector implements LanguageChangeSignatureDetec
             }
           }
         }
+        final CanonicalTypes.Type typeWrapper = parameterInfo.getTypeWrapper();
+        if (!typeWrapper.isValid()) return false;
         parameterInfos[i] = new ParameterInfoImpl(oldParameter != null ? oldParameter.getOldIndex() : - 1,
                                                   parameterInfo.getName(),
-                                                  parameterInfo.getTypeWrapper().getType(element, element.getManager()),
+                                                  typeWrapper.getType(element, element.getManager()),
                                                   null);
       }
     }
+    return true;
   }
 
 
