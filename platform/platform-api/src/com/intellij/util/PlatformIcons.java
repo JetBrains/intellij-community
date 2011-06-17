703a65fe354b89bb268392732cc16208dfb5d693
Konstantin Bulenkov <kb@jetbrains.com>
2011-06-17 11:38:31 +0400

    move to interface

---
+++ platform/platform-api/src/com/intellij/util/PlatformIcons.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright 2000-2011 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.util;
+
+import com.intellij.openapi.util.IconLoader;
+import org.jetbrains.annotations.NonNls;
+
+import javax.swing.*;
+
+/**
+ * @author Konstantin Bulenkov
+ */
+public interface PlatformIcons {
+  @NonNls String PUBLIC_ICON_PATH = "/nodes/c_public.png";
+  Icon PUBLIC_ICON = IconLoader.getIcon(PUBLIC_ICON_PATH);
+  Icon LOCKED_ICON = IconLoader.getIcon("/nodes/locked.png");
+  Icon PRIVATE_ICON = IconLoader.getIcon("/nodes/c_private.png");
+  Icon PROTECTED_ICON = IconLoader.getIcon("/nodes/c_protected.png");
+  Icon PACKAGE_LOCAL_ICON = IconLoader.getIcon("/nodes/c_plocal.png");
+  Icon PACKAGE_ICON = IconLoader.getIcon("/nodes/packageClosed.png");
+  Icon PACKAGE_OPEN_ICON = IconLoader.getIcon("/nodes/packageOpen.png");
+  Icon DIRECTORY_CLOSED_ICON = IconLoader.getIcon("/nodes/TreeClosed.png");
+  Icon DIRECTORY_OPEN_ICON = IconLoader.getIcon("/nodes/TreeOpen.png");
+  @NonNls String CLASS_ICON_PATH = "/nodes/class.png";
+  Icon CLASS_ICON = IconLoader.getIcon(CLASS_ICON_PATH);
+  Icon EXCEPTION_CLASS_ICON = IconLoader.getIcon("/nodes/exceptionClass.png");
+  Icon ANONYMOUS_CLASS_ICON = IconLoader.getIcon("/nodes/anonymousClass.png");
+  Icon ABSTRACT_CLASS_ICON = IconLoader.getIcon("/nodes/abstractClass.png");
+  Icon JUNIT_TEST_CLASS_ICON = IconLoader.getIcon("/nodes/junitTestClass.png");
+  Icon ANNOTATION_TYPE_ICON = IconLoader.getIcon("/nodes/annotationtype.png");
+  Icon ENUM_ICON = IconLoader.getIcon("/nodes/enum.png");
+  Icon INTERFACE_ICON = IconLoader.getIcon("/nodes/interface.png");
+  Icon METHOD_ICON = IconLoader.getIcon("/nodes/method.png");
+  Icon FUNCTION_ICON = IconLoader.getIcon("/nodes/function.png");
+  Icon ABSTRACT_METHOD_ICON = IconLoader.getIcon("/nodes/abstractMethod.png");
+  Icon FIELD_ICON = IconLoader.getIcon("/nodes/field.png");
+  Icon PARAMETER_ICON = IconLoader.getIcon("/nodes/parameter.png");
+  Icon VARIABLE_ICON = IconLoader.getIcon("/nodes/variable.png");
+  Icon XML_TAG_ICON = IconLoader.getIcon("/nodes/tag.png");
+  Icon ANT_TARGET_ICON = IconLoader.getIcon("/ant/target.png");
+  Icon ANT_META_TARGET_ICON = IconLoader.getIcon("/ant/metaTarget.png");
+  Icon LIBRARY_ICON = IconLoader.getIcon("/nodes/ppLib.png");
+  Icon WEB_ICON = IconLoader.getIcon("/nodes/ppWeb.png");
+  Icon JAR_ICON = IconLoader.getIcon("/nodes/ppJar.png");
+  Icon WEB_FOLDER_OPEN = IconLoader.getIcon("/nodes/webFolderOpen.png");
+  Icon WEB_FOLDER_CLOSED = IconLoader.getIcon("/nodes/webFolderClosed.png");
+  Icon FILE_ICON = IconLoader.getIcon("/nodes/ppFile.png");
+  Icon EJB_CLASS_ICON = IconLoader.getIcon("/javaee/ejbClass.png");
+  Icon EJB_INTERCEPTOR_CLASS_ICON = IconLoader.getIcon("/javaee/interceptorClass.png");
+  Icon EJB_HOME_INTERFACE_ICON = IconLoader.getIcon("/javaee/home.png");
+  Icon EJB_LOCAL_HOME_INTERFACE_ICON = IconLoader.getIcon("/javaee/localHome.png");
+  Icon EJB_LOCAL_INTERFACE_ICON = IconLoader.getIcon("/javaee/local.png");
+  Icon EJB_REMOTE_INTERFACE_ICON = IconLoader.getIcon("/javaee/remote.png");
+  Icon EJB_ICON = IconLoader.getIcon("/nodes/ejb.png");
+  Icon EJB_CREATE_METHOD_ICON = IconLoader.getIcon("/nodes/ejbCreateMethod.png");
+  Icon EJB_FINDER_METHOD_ICON = IconLoader.getIcon("/nodes/ejbFinderMethod.png");
+  Icon EJB_BUSINESS_METHOD_ICON = IconLoader.getIcon("/nodes/ejbBusinessMethod.png");
+  Icon EJB_CMP_FIELD_ICON = IconLoader.getIcon("/nodes/ejbCmpField.png");
+  Icon EJB_CMR_FIELD_ICON = IconLoader.getIcon("/nodes/ejbCmrField.png");
+  Icon EJB_PRIMARY_KEY_CLASS = IconLoader.getIcon("/nodes/ejbPrimaryKeyClass.png");
+  Icon EJB_REFERENCE = IconLoader.getIcon("/nodes/ejbReference.png");
+
+  Icon DATASOURCE_ICON = IconLoader.getIcon("/nodes/DataSource.png");
+  Icon DATASOURCE_DISABLED_ICON = IconLoader.getIcon("/nodes/DataSourceDisabled.png");
+  Icon DATASOURCE_TABLE_ICON = IconLoader.getIcon("/nodes/DataTables.png");
+  Icon DATASOURCE_VIEW_ICON = IconLoader.getIcon("/nodes/dataView.png");
+  Icon DATASOURCE_SEQUENCE_ICON = IconLoader.getIcon("/nodes/dataSequence.png");
+  Icon DATASOURCE_COLUMN_ICON = IconLoader.getIcon("/nodes/dataColumn.png");
+  Icon DATASOURCE_FK_COLUMN_ICON = IconLoader.getIcon("/nodes/dataFkColumn.png");
+  Icon DATASOURCE_PK_COLUMN_ICON = IconLoader.getIcon("/nodes/dataPkColumn.png");
+
+  Icon DATASOURCE_REMOTE_INSTANCE = IconLoader.getIcon("/nodes/addRemoteWeblogicInstance.png");
+  Icon EJB_FIELD_PK = IconLoader.getIcon("/nodes/fieldPK.png");
+  Icon VARIABLE_READ_ACCESS = IconLoader.getIcon("/nodes/read-access.png");
+  Icon VARIABLE_WRITE_ACCESS = IconLoader.getIcon("/nodes/write-access.png");
+  Icon VARIABLE_RW_ACCESS = IconLoader.getIcon("/nodes/rw-access.png");
+  Icon CUSTOM_FILE_ICON = IconLoader.getIcon("/fileTypes/custom.png");
+  Icon PROPERTY_ICON = IconLoader.getIcon("/nodes/property.png");
+  Icon ASPECT_ICON = IconLoader.getIcon("/nodes/aspect.png");
+  Icon POINTCUT_ICON = IconLoader.getIcon("/nodes/pointcut.png");
+  Icon ADVICE_ICON = IconLoader.getIcon("/nodes/advice.png");
+  Icon ERROR_INTRODUCTION_ICON = IconLoader.getIcon("/nodes/errorIntroduction.png");
+  Icon WARNING_INTRODUCTION_ICON = IconLoader.getIcon("/nodes/warningIntroduction.png");
+  Icon PARENTS_INTRODUCTION_ICON = IconLoader.getIcon("/nodes/parentsIntroduction.png");
+  Icon SOFTENING_INTRODUCTION_ICON = IconLoader.getIcon("/nodes/softeningIntroduction.png");
+  Icon JAVA_OUTSIDE_SOURCE_ICON = IconLoader.getIcon("/fileTypes/javaOutsideSource.png");
+  Icon EXCLUDED_FROM_COMPILE_ICON = IconLoader.getIcon("/nodes/excludedFromCompile.png");
+  Icon PROJECT_ICON = IconLoader.getIcon("/icon_small.png");
+  Icon UI_FORM_ICON = IconLoader.getIcon("/fileTypes/uiForm.png");
+  Icon JSP_ICON = IconLoader.getIcon("/fileTypes/jsp.png");
+  Icon SMALL_VCS_CONFIGURABLE = IconLoader.getIcon("/general/smallConfigurableVcs.png");
+  Icon VCS_SMALL_TAB = IconLoader.getIcon("/general/vcsSmallTab.png");
+  Icon GROUP_BY_PACKAGES = IconLoader.getIcon("/toolbar/folders.png");
+  Icon ADD_ICON = IconLoader.getIcon("/actions/include.png");
+  Icon DELETE_ICON = IconLoader.getIcon("/actions/exclude.png");
+  Icon MOVE_UP_ICON = IconLoader.getIcon("/actions/moveUp.png");
+  Icon MOVE_DOWN_ICON = IconLoader.getIcon("/actions/moveDown.png");
+  Icon DUPLICATE_ICON = IconLoader.getIcon("/general/copy.png");
+  Icon SELECT_ALL_ICON = IconLoader.getIcon("/actions/selectall.png");
+  Icon UNSELECT_ALL_ICON = IconLoader.getIcon("/actions/unselectall.png");
+  Icon PROPERTIES_ICON = IconLoader.getIcon("/actions/properties.png");
+  Icon SYNCHRONIZE_ICON = IconLoader.getIcon("/actions/sync.png");
+
+  Icon CHECK_ICON = IconLoader.getIcon("/actions/checked.png");
+  Icon CHECK_ICON_SELECTED = IconLoader.getIcon("/actions/checked_selected.png");
+  Icon CHECK_ICON_SMALL = IconLoader.getIcon("/actions/checked_small.png");
+  Icon CHECK_ICON_SMALL_SELECTED = IconLoader.getIcon("/actions/checked_small_selected.png");
+
+  Icon OPEN_EDIT_DIALOG_ICON = IconLoader.getIcon("/actions/showViewer.png");
+  Icon FLATTEN_PACKAGES_ICON = IconLoader.getIcon("/objectBrowser/flattenPackages.png");
+  Icon ADD_TO_SECTION_ICON = IconLoader.getIcon("/general/addItemToSection.png");
+  Icon EDIT_IN_SECTION_ICON = IconLoader.getIcon("/general/editItemInSection.png");
+  Icon TASK_ICON = IconLoader.getIcon("/ant/task.png");
+  Icon CLASS_INITIALIZER = IconLoader.getIcon("/nodes/classInitializer.png");
+
+  Icon OPENED_MODULE_GROUP_ICON = IconLoader.getIcon("/nodes/moduleGroupOpen.png");
+  Icon CLOSED_MODULE_GROUP_ICON = IconLoader.getIcon("/nodes/moduleGroupClosed.png");
+  Icon FOLDER_ICON = IconLoader.getIcon("/nodes/folder.png");
+  Icon SOURCE_FOLDERS_ICON = IconLoader.getIcon("/nodes/sourceFolder.png");
+  Icon TEST_SOURCE_FOLDER = IconLoader.getIcon("/nodes/testSourceFolder.png");
+
+  Icon MODULES_SOURCE_FOLDERS_ICON = IconLoader.getIcon("/modules/sourceRootClosed.png");
+  Icon MODULES_TEST_SOURCE_FOLDER = IconLoader.getIcon("/modules/testRootClosed.png");
+
+  Icon CONTENT_ROOT_ICON_OPEN = IconLoader.getIcon("/nodes/ModuleOpen.png");
+  Icon CONTENT_ROOT_ICON_CLOSED = IconLoader.getIcon("/nodes/ModuleClosed.png");
+
+  Icon UP_DOWN_ARROWS = IconLoader.getIcon("/ide/upDown.png");
+
+  Icon ADD_BIG = IconLoader.getIcon("/actions/addBig.png");
+  Icon REMOVE_BIG = IconLoader.getIcon("/actions/removeBig.png");
+  Icon UP_BIG = IconLoader.getIcon("/actions/upBig.png");
+  Icon DOWN_BIG = IconLoader.getIcon("/actions/downBig.png");
+}
