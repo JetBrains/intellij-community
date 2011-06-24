74e16105a3a87dcb6a8370b1b8bd9188e022fa0c
Bas Leijdekkers <basleijdekkers@gmail.com>
2011-06-24 14:46:15 +0200

    quickfix

--- plugins/InspectionGadgets/src/com/siyeh/ig/encapsulation/UseOfAnotherObjectsPrivateFieldInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/encapsulation/UseOfAnotherObjectsPrivateFieldInspection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2010 Dave Griffith, Bas Leijdekkers
+ * Copyright 2003-2011 Dave Griffith, Bas Leijdekkers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,12 +15,18 @@
  */
 package com.siyeh.ig.encapsulation;
 
+import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.codeInspection.ui.MultipleCheckboxOptionsPanel;
+import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
+import com.intellij.psi.util.PropertyUtil;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.util.PsiUtil;
+import com.intellij.util.IncorrectOperationException;
 import com.siyeh.InspectionGadgetsBundle;
 import com.siyeh.ig.BaseInspection;
 import com.siyeh.ig.BaseInspectionVisitor;
+import com.siyeh.ig.InspectionGadgetsFix;
 import com.siyeh.ig.psiutils.MethodUtils;
 import org.jetbrains.annotations.NotNull;
 
@@ -63,6 +69,93 @@ public class UseOfAnotherObjectsPrivateFieldInspection
         panel.addCheckbox(InspectionGadgetsBundle.message(
                 "ignore.accesses.from.equals.method"), "ignoreEquals");
         return panel;
+    }
+
+    @Override
+    protected InspectionGadgetsFix buildFix(Object... infos) {
+        final PsiField field = (PsiField) infos[0];
+        final String propertyName = field.getName();
+        final PsiReferenceExpression referenceExpression =
+                (PsiReferenceExpression) infos[1];
+        final boolean isStatic = field.hasModifierProperty(PsiModifier.STATIC);
+        final PsiClass containingClass = field.getContainingClass();
+        if (PsiUtil.isAccessedForReading(referenceExpression)) {
+            if (PsiUtil.isAccessedForWriting(referenceExpression)) {
+                return null;
+            }
+            final PsiMethod getter =
+                    PropertyUtil.findPropertyGetter(containingClass,
+                            propertyName, isStatic, true);
+            if (getter == null) {
+                return null;
+            }
+            return new UseOfAnotherObjectsPrivateFieldFix(getter);
+        } else if (PsiUtil.isAccessedForWriting(referenceExpression)) {
+            final PsiMethod setter =
+                    PropertyUtil.findPropertySetter(containingClass,
+                            propertyName, isStatic, true);
+            if (setter == null) {
+                return null;
+            }
+            return new UseOfAnotherObjectsPrivateFieldFix(setter);
+        }
+        return null;
+    }
+
+    private static class UseOfAnotherObjectsPrivateFieldFix
+            extends InspectionGadgetsFix {
+
+        private final PsiMethod method;
+
+        public UseOfAnotherObjectsPrivateFieldFix(PsiMethod method) {
+            this.method = method;
+        }
+
+        @NotNull
+        @Override
+        public String getName() {
+            return "Replace with call to '" + method.getName() + "'";
+        }
+
+        @Override
+        protected void doFix(Project project, ProblemDescriptor descriptor)
+                throws IncorrectOperationException {
+            final PsiElement element = descriptor.getPsiElement();
+            final PsiElement parent = element.getParent();
+            if (!(parent instanceof PsiReferenceExpression)) {
+                return;
+            }
+            final PsiReferenceExpression referenceExpression =
+                    (PsiReferenceExpression) parent;
+            final StringBuilder methodCallText = new StringBuilder();
+            final PsiExpression qualifierExpression =
+                    referenceExpression.getQualifierExpression();
+            if (qualifierExpression != null) {
+                methodCallText.append(qualifierExpression.getText());
+                methodCallText.append('.');
+            }
+            methodCallText.append(method.getName());
+            if (!PsiUtil.isOnAssignmentLeftHand(referenceExpression)) {
+                methodCallText.append("()");
+                replaceExpression(referenceExpression,
+                        methodCallText.toString());
+            } else {
+                final PsiAssignmentExpression assignmentExpression =
+                        PsiTreeUtil.getParentOfType(referenceExpression,
+                                PsiAssignmentExpression.class);
+                if (assignmentExpression == null) {
+                    return;
+                }
+                methodCallText.append('(');
+                final PsiExpression rhs = assignmentExpression.getRExpression();
+                if (rhs != null) {
+                    methodCallText.append(rhs.getText());
+                }
+                methodCallText.append(')');
+                replaceExpression(assignmentExpression,
+                        methodCallText.toString());
+            }
+        }
     }
 
     @Override
@@ -112,7 +205,7 @@ public class UseOfAnotherObjectsPrivateFieldInspection
             if(fieldNameElement == null){
                 return;
             }
-            registerError(fieldNameElement);
+            registerError(fieldNameElement, field, expression);
         }
     }
 }
