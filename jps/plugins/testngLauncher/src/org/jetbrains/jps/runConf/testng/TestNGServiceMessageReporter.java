e74db5317cc95b7570ef6dd7a4f5d93637e977e9
Pavel Sher <pavel.sher@jetbrains.com>
2011-06-24 18:54:47 +0200

    run configurations support merged

---
+++ jps/plugins/testngLauncher/src/org/jetbrains/jps/runConf/testng/TestNGServiceMessageReporter.java
@@ -0,0 +1,11 @@
+package org.jetbrains.jps.runConf.testng;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Pavel.Sher
+ * Date: 6/23/11
+ * Time: 12:19 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestNGServiceMessageReporter {
+}
