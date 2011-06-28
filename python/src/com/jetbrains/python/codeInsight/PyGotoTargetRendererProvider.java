283216c16d9a5e2c1d9b8bb8bafb3e796365c25f
Maxim Medvedev <maxim.medvedev@jetbrains.com>
2011-06-28 08:23:36 +0400

    'show implementations' for groovy implicit accessors

--- python/src/com/jetbrains/python/codeInsight/PyGotoTargetRendererProvider.java
+++ python/src/com/jetbrains/python/codeInsight/PyGotoTargetRendererProvider.java
@@ -1,5 +1,6 @@
 package com.jetbrains.python.codeInsight;
 
+import com.intellij.codeInsight.navigation.GotoTargetHandler;
 import com.intellij.codeInsight.navigation.GotoTargetRendererProvider;
 import com.intellij.ide.util.PsiElementListCellRenderer;
 import com.intellij.psi.PsiElement;
@@ -10,7 +11,7 @@ import com.jetbrains.python.psi.PyElement;
  * @author yole
  */
 public class PyGotoTargetRendererProvider implements GotoTargetRendererProvider {
-  public PsiElementListCellRenderer getRenderer(final PsiElement element) {
+  public PsiElementListCellRenderer getRenderer(final PsiElement element, GotoTargetHandler.GotoData gotoData) {
     if (element instanceof PyElement && element instanceof PsiNamedElement) return new PyElementListCellRenderer();
     return null;
   }
