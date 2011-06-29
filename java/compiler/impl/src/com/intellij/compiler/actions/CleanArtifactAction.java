26fa10c23fc15906346b20941ed74dde926326c9
nik <Nikolay.Chashnikov@jetbrains.com>
2011-06-29 12:02:55 +0400

    ask confirmation if 'clean artifact' action is invoked for artifact which output contains source roots

--- java/compiler/impl/src/com/intellij/compiler/actions/CleanArtifactAction.java
+++ java/compiler/impl/src/com/intellij/compiler/actions/CleanArtifactAction.java
@@ -18,14 +18,17 @@ package com.intellij.compiler.actions;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.packaging.artifacts.Artifact;
+import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author nik
@@ -42,13 +45,50 @@ public class CleanArtifactAction extends BuildArtifactActionBase {
 
   @Override
   protected void performAction(Project project, final List<Artifact> artifacts) {
+    Set<VirtualFile> parents = new HashSet<VirtualFile>();
+    final VirtualFile[] roots = ProjectRootManager.getInstance(project).getContentSourceRoots();
+    for (VirtualFile root : roots) {
+      VirtualFile parent = root;
+      while (parent != null && !parents.contains(parent)) {
+        parents.add(parent);
+        parent = parent.getParent();
+      }
+    }
+
+    Map<String, String> outputPathContainingSourceRoots = new HashMap<String, String>();
     final List<File> files = new ArrayList<File>();
     for (Artifact artifact : artifacts) {
       String outputPath = artifact.getOutputFilePath();
       if (outputPath != null) {
         files.add(new File(FileUtil.toSystemDependentName(outputPath)));
+        final VirtualFile outputFile = LocalFileSystem.getInstance().findFileByPath(outputPath);
+        if (parents.contains(outputFile)) {
+          outputPathContainingSourceRoots.put(artifact.getName(), outputPath);
+        }
       }
     }
+
+    if (!outputPathContainingSourceRoots.isEmpty()) {
+      final String message;
+      if (outputPathContainingSourceRoots.size() == 1 && outputPathContainingSourceRoots.values().size() == 1) {
+        final String name = ContainerUtil.getFirstItem(outputPathContainingSourceRoots.keySet());
+        final String output = outputPathContainingSourceRoots.get(name);
+        message = "The output directory '" + output + "' of '" + name + "' artifact contains source roots of the project. Do you want to continue and clear it?";
+      }
+      else {
+        StringBuilder info = new StringBuilder();
+        for (String name : outputPathContainingSourceRoots.keySet()) {
+          info.append(" '").append(name).append("' artifact ('").append(outputPathContainingSourceRoots.get(name)).append("')\n");
+        }
+        message = "The output directories of the following artifacts contains source roots:\n" +
+                  info + "Do you want to continue and clear these directories?";
+      }
+      final int answer = Messages.showYesNoDialog(project, message, "Clean Artifacts", null);
+      if (answer != 0) {
+        return;
+      }
+    }
+
     new Task.Backgroundable(project, "Cleaning artifacts...", true) {
       @Override
       public void run(@NotNull ProgressIndicator indicator) {
