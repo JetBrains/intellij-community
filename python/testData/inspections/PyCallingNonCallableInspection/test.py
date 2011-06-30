3b9db695a3669e2cf79d5d4590f0fe6e07bcecc6
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2011-06-30 22:24:13 +0400

    Fixed callable inspection for both direct and __class__ constructor calls

--- python/testData/inspections/PyCallingNonCallableInspection/test.py
+++ python/testData/inspections/PyCallingNonCallableInspection/test.py
@@ -17,3 +17,21 @@ def concealer():
 def test_module():
     import sys
     <warning descr="'sys' is not callable">sys()</warning>
+
+def test_class_as_callable():
+    class C1(object):
+        def __init__(self):
+            pass
+        def f(self):
+            pass
+    class C2(object):
+        def g(self):
+            pass
+    class C3:
+        pass
+    c1 = C1() #pass
+    c2 = C2() #pass
+    c3 = C3() #pass
+    c1.__class__().f() #pass
+    c2.__class__().g() #pass
+    c3.__class__() #pass
