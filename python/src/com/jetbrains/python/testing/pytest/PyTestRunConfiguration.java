6fa716eb8d21cf81803f4221bfc02ed394c2ed4c
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2011-06-30 19:23:36 +0400

    fixed PY-3836 With default python interpreter on Mac OS PyCharm in not able to find valid py.test and PY-4045 pytest does not work with virtualenv (when pytest is installed globally)

--- python/src/com/jetbrains/python/testing/pytest/PyTestRunConfiguration.java
+++ python/src/com/jetbrains/python/testing/pytest/PyTestRunConfiguration.java
@@ -10,11 +10,12 @@ import com.intellij.openapi.util.JDOMExternalizerUtil;
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.util.WriteExternalException;
 import com.jetbrains.python.run.AbstractPythonRunConfiguration;
+import com.jetbrains.python.sdk.PythonSdkFlavor;
 import org.jdom.Element;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.File;
+import java.io.*;
 
 /**
  * @author yole
@@ -104,7 +105,36 @@ public class PyTestRunConfiguration extends AbstractPythonRunConfiguration {
     if (runner.exists()) return runner.getPath();
     runner = new File(new File (new File(bin_dir.getParentFile(), "local"), "bin"), PY_TEST);
     if (runner.exists()) return runner.getPath();
+
+    return findRunnerInStdBin(PY_TEST, sdkHome);
+  }
+
+  private static String findRunnerInStdBin(String PY_TEST, final String sdkHome) {
+    File runner = new File(new File (new File("/usr", "local"), "bin"), PY_TEST);
+    if (!runner.exists()) runner = new File(new File ("/usr", "bin"), PY_TEST);
+    if (runner.exists() && checkVersion(runner, sdkHome))
+      return runner.getPath();
     return null;
+  }
+
+  private static boolean checkVersion(File runner, final String sdkHome) {
+    try {
+      BufferedReader br = new BufferedReader(new FileReader(runner));
+      String interpreterPath = br.readLine().substring(2);    // as it presented as #!/usr/local/bin/python2.6
+      String realPath = new File(interpreterPath).getCanonicalPath().toLowerCase();
+      int ind = realPath.indexOf("python");
+      if (ind != -1) {
+        String version = realPath.substring(ind + "python".length());
+        final PythonSdkFlavor flavor = PythonSdkFlavor.getFlavor(sdkHome);
+        String sdkVersion = flavor != null ? flavor.getVersionString(sdkHome) : null;
+        if (sdkVersion != null && sdkVersion.contains(version))
+          return true;
+      }
+    }
+    catch (IOException e) {
+      return false;
+    }
+    return false;
   }
 
   @Override
