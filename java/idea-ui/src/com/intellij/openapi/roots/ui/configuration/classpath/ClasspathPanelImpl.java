c8ffc2f64af3d3673c9ad5d220990843daed7571
Konstantin Bulenkov <kb@jetbrains.com>
2011-07-04 12:58:11 +0400

    copyright

--- java/idea-ui/src/com/intellij/openapi/roots/ui/configuration/classpath/ClasspathPanelImpl.java
+++ java/idea-ui/src/com/intellij/openapi/roots/ui/configuration/classpath/ClasspathPanelImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2005 Alexey Efimov
+ * Copyright 2000-2011 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
