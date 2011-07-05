2089bda93766970fb46ce3e00dc40cb42d09da44
Konstantin Bulenkov <kb@jetbrains.com>
2011-07-05 14:19:07 +0400

    IDEA-71428 Navigation bar scrambling

--- platform/lang-impl/src/com/intellij/ide/navigationToolbar/NavBarModel.java
+++ platform/lang-impl/src/com/intellij/ide/navigationToolbar/NavBarModel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2011 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -101,7 +101,7 @@ public class NavBarModel {
     }
 
     psiElement = normalize(psiElement);
-    if (myModel.contains(psiElement) && !myChanged) return;
+    if (!myModel.isEmpty() && myModel.get(myModel.size() - 1).equals(psiElement) && !myChanged) return;
 
     if (psiElement != null && psiElement.isValid()) {
       updateModel(psiElement);
@@ -198,6 +198,7 @@ public class NavBarModel {
           traverseToRoot(parent, roots, model);
         }
         foundByExtension = true;
+        break;
       }
     }
 
