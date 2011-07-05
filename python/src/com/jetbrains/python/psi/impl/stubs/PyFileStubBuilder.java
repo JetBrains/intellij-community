cbd92451a19e82714555c262acb661bd48dac2b1
Dmitry Jemerov <yole@jetbrains.com>
2011-07-05 14:28:55 +0200

    correct skipChildProcessing check when switching from Python stubs to AST (PY-4056)

--- python/src/com/jetbrains/python/psi/impl/stubs/PyFileStubBuilder.java
+++ python/src/com/jetbrains/python/psi/impl/stubs/PyFileStubBuilder.java
@@ -1,12 +1,15 @@
 package com.jetbrains.python.psi.impl.stubs;
 
+import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.stubs.DefaultStubBuilder;
 import com.intellij.psi.stubs.StubElement;
+import com.intellij.psi.tree.IElementType;
 import com.jetbrains.python.psi.PyFile;
 import com.jetbrains.python.psi.PyIfStatement;
 import com.jetbrains.python.psi.PyUtil;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * @author yole
@@ -23,9 +26,17 @@ public class PyFileStubBuilder extends DefaultStubBuilder {
 
   @Override
   protected boolean skipChildProcessingWhenBuildingStubs(PsiElement element, PsiElement child) {
-    if (element instanceof PyIfStatement) {
-      return PyUtil.isIfNameEqualsMain((PyIfStatement)element);
+    return element instanceof PyIfStatement && PyUtil.isIfNameEqualsMain((PyIfStatement)element);
+  }
+
+  @Override
+  public boolean skipChildProcessingWhenBuildingStubs(@Nullable ASTNode parent, IElementType childType) {
+    if (parent != null) {
+      final PsiElement psi = parent.getPsi();
+      if (psi != null) {
+        return psi instanceof PyIfStatement && PyUtil.isIfNameEqualsMain((PyIfStatement)psi);
+      }
     }
-    return false;
+    return super.skipChildProcessingWhenBuildingStubs(parent, childType);
   }
 }
