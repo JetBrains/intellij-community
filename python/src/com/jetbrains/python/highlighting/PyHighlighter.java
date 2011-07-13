1ed997c9b2b105aff73c7ec3c5196246cc350e66
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2011-07-13 14:51:12 +0400

    fixed stringliterallexer constructor

--- python/src/com/jetbrains/python/highlighting/PyHighlighter.java
+++ python/src/com/jetbrains/python/highlighting/PyHighlighter.java
@@ -31,19 +31,19 @@ public class PyHighlighter extends SyntaxHighlighterBase {
   public Lexer getHighlightingLexer() {
     LayeredLexer ret = new LayeredLexer(new PythonHighlightingLexer(myLanguageLevel));
     ret.registerSelfStoppingLayer(
-      new PyStringLiteralLexer(PyTokenTypes.SINGLE_QUOTED_STRING, myLanguageLevel.isPy3K()),
+      new PyStringLiteralLexer(PyTokenTypes.SINGLE_QUOTED_STRING),
       new IElementType[]{PyTokenTypes.SINGLE_QUOTED_STRING}, IElementType.EMPTY_ARRAY
     );
     ret.registerSelfStoppingLayer(
-      new PyStringLiteralLexer(PyTokenTypes.SINGLE_QUOTED_UNICODE, myLanguageLevel.isPy3K()),
+      new PyStringLiteralLexer(PyTokenTypes.SINGLE_QUOTED_UNICODE),
       new IElementType[]{PyTokenTypes.SINGLE_QUOTED_UNICODE}, IElementType.EMPTY_ARRAY
     );
     ret.registerSelfStoppingLayer(
-      new PyStringLiteralLexer(PyTokenTypes.TRIPLE_QUOTED_STRING, myLanguageLevel.isPy3K()),
+      new PyStringLiteralLexer(PyTokenTypes.TRIPLE_QUOTED_STRING),
       new IElementType[]{PyTokenTypes.TRIPLE_QUOTED_STRING}, IElementType.EMPTY_ARRAY
     );
     ret.registerSelfStoppingLayer(
-      new PyStringLiteralLexer(PyTokenTypes.TRIPLE_QUOTED_UNICODE, myLanguageLevel.isPy3K()),
+      new PyStringLiteralLexer(PyTokenTypes.TRIPLE_QUOTED_UNICODE),
       new IElementType[]{PyTokenTypes.TRIPLE_QUOTED_UNICODE}, IElementType.EMPTY_ARRAY
     );
 
--- python/testSrc/com/jetbrains/python/PyStringLiteralLexerTest.java
+++ python/testSrc/com/jetbrains/python/PyStringLiteralLexerTest.java
@@ -8,7 +8,7 @@ import com.jetbrains.python.lexer.PyStringLiteralLexer;
  */
 public class PyStringLiteralLexerTest extends PyLexerTestCase {
   public void testBackslashN() {  // PY-1313
-    doLexerTest("u\"\\N{LATIN SMALL LETTER B}\"", new PyStringLiteralLexer(PyElementTypes.STRING_LITERAL_EXPRESSION, false),
+    doLexerTest("u\"\\N{LATIN SMALL LETTER B}\"", new PyStringLiteralLexer(PyElementTypes.STRING_LITERAL_EXPRESSION),
                 "Py:STRING_LITERAL_EXPRESSION", "VALID_STRING_ESCAPE_TOKEN", "Py:STRING_LITERAL_EXPRESSION");    
   }
 }
