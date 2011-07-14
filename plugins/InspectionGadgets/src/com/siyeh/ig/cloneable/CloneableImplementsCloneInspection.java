d1b2d8a34e707bf66dc36fb7d11ce4a487a1c0ef
Bas Leijdekkers <basleijdekkers@gmail.com>
2011-07-14 14:30:17 +0200

    cleanup

--- plugins/InspectionGadgets/src/com/siyeh/ig/cloneable/CloneableImplementsCloneInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/cloneable/CloneableImplementsCloneInspection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2007 Dave Griffith, Bas Leijdekkers
+ * Copyright 2003-2011 Dave Griffith, Bas Leijdekkers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,34 +32,40 @@ public class CloneableImplementsCloneInspection extends BaseInspection {
     /** @noinspection PublicField*/
     public boolean m_ignoreCloneableDueToInheritance = false;
 
+    @Override
     @NotNull
     public String getID() {
         return "CloneableClassWithoutClone";
     }
 
+    @Override
     @NotNull
     public String getDisplayName() {
         return InspectionGadgetsBundle.message(
                 "cloneable.class.without.clone.display.name");
     }
 
+    @Override
     @NotNull
     public String buildErrorString(Object... infos) {
         return InspectionGadgetsBundle.message(
                 "cloneable.class.without.clone.problem.descriptor");
     }
 
+    @Override
     public JComponent createOptionsPanel() {
         return new SingleCheckboxOptionsPanel(InspectionGadgetsBundle.message(
                 "cloneable.class.without.clone.ignore.option"),
                 this, "m_ignoreCloneableDueToInheritance");
     }
 
+    @Override
     public BaseInspectionVisitor buildVisitor() {
-        return new CloneableDefinesCloneVisitor();
+        return new CloneableImplementsCloneVisitor();
     }
 
-    private class CloneableDefinesCloneVisitor extends BaseInspectionVisitor {
+    private class CloneableImplementsCloneVisitor
+            extends BaseInspectionVisitor {
 
         @Override public void visitClass(@NotNull PsiClass aClass) {
             // no call to super, so it doesn't drill down
