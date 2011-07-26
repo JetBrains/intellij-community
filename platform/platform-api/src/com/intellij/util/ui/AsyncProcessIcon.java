b4d17378af717cbbb462f60cfe5261097b96f41b
Kirill Kalishev <kirill.kalishev@jetbrains.com>
2011-07-26 20:27:20 +0400

    animation timer fix - removing duplicate firings on a chain of resume/suspend/resume/suspend/..

--- platform/platform-api/src/com/intellij/util/ui/AsyncProcessIcon.java
+++ platform/platform-api/src/com/intellij/util/ui/AsyncProcessIcon.java
@@ -21,13 +21,14 @@ import com.intellij.ui.LayeredIcon;
 import org.jetbrains.annotations.NonNls;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class AsyncProcessIcon extends AnimatedIcon {
   public static final int COUNT = 12;
-  public static final int CYCLE_LENGTH = 1300;
+  public static final int CYCLE_LENGTH = 800;
 
   private static final Icon[] SMALL_ICONS = findIcons("/process/step_", "/process/step_mask.png");
   private static final Icon SMALL_PASSIVE_ICON = IconLoader.getIcon("/process/step_passive.png");
@@ -89,49 +90,6 @@ public class AsyncProcessIcon extends AnimatedIcon {
     public Big(@NonNls final String name) {
       super(name, BIG_ICONS, BIG_PASSIVE_ICON);
     }
-  }
-
-  public static void main(String[] args) {
-    IconLoader.activate();
-
-
-    JFrame frame = new JFrame();
-    frame.getContentPane().setLayout(new BorderLayout());
-    JPanel content = new JPanel(new FlowLayout());
-
-
-    AsyncProcessIcon progress = new AsyncProcessIcon("Process");
-    content.add(progress);
-
-    JButton button = new JButton("press me");
-    content.add(button);
-    button.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        Thread sleeper = new Thread() {
-          public void run() {
-            try {
-              sleep(15000);
-            } catch (InterruptedException e1) {
-              e1.printStackTrace();
-            }
-          }
-        };
-
-        sleeper.start();
-        try {
-          sleeper.join();
-        } catch (InterruptedException e1) {
-          e1.printStackTrace();
-        }
-      }
-    });
-
-    progress.resume();
-
-
-    frame.getContentPane().add(content, BorderLayout.CENTER);
-    frame.setBounds(200, 200, 400, 400);
-    frame.show();
   }
 
   public boolean isDisposed() {
