c84c6e51510a6d9b572eb568c839ac903d44ba12
Maxim.Mossienko <maxim.mossienko@jetbrains.com>
2011-07-27 16:14:42 +0400

    ensure dirty status propagated

--- platform/util/src/com/intellij/util/io/PersistentEnumeratorBase.java
+++ platform/util/src/com/intellij/util/io/PersistentEnumeratorBase.java
@@ -55,6 +55,7 @@ abstract class PersistentEnumeratorBase<Data> implements Forceable, Closeable {
   private boolean myCorrupted = false;
   private final MyDataIS myKeyReadStream;
   private final Version myVersion;
+  private volatile boolean myDirtyStatusUpdateInProgress;
 
   public static class Version {
     private final int correctlyClosedMagic;
@@ -457,16 +458,20 @@ abstract class PersistentEnumeratorBase<Data> implements Forceable, Closeable {
 
   protected final void markDirty(boolean dirty) throws IOException {
     //assert Thread.holdsLock(this) || Thread.holdsLock(ourLock); // we hold one lock or another so can access myDirty
-    if (dirty && myDirty) return;
+    if (dirty && myDirty && !myDirtyStatusUpdateInProgress) return;
     synchronized (ourLock) {
       if (myDirty) {
         if (!dirty) {
+          myDirtyStatusUpdateInProgress = true;
           markClean();
+          myDirtyStatusUpdateInProgress = false;
         }
       }
       else {
         if (dirty) {
+          myDirtyStatusUpdateInProgress = true;
           myStorage.putInt(0, myVersion.dirtyMagic);
+          myDirtyStatusUpdateInProgress = false;
           myDirty = true;
         }
       }
