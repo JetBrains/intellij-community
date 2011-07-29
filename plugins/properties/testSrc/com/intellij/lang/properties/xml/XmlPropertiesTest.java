35da5d01e41d535e024b1208f85c7b6766372c4d
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2011-07-29 15:18:11 +0400

    IDEA-53318: Support for xml properties: dtd bundled

--- plugins/properties/testSrc/com/intellij/lang/properties/xml/XmlPropertiesTest.java
+++ plugins/properties/testSrc/com/intellij/lang/properties/xml/XmlPropertiesTest.java
@@ -5,8 +5,8 @@ import com.intellij.lang.properties.PropertiesReferenceManager;
 import com.intellij.lang.properties.PropertiesUtil;
 import com.intellij.lang.properties.psi.PropertiesFile;
 import com.intellij.openapi.application.PluginPathManager;
-import com.intellij.testFramework.LightPlatformCodeInsightTestCase;
 import com.intellij.testFramework.PlatformTestCase;
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
 
 import java.util.List;
 
@@ -14,11 +14,11 @@ import java.util.List;
  * @author Dmitry Avdeev
  *         Date: 7/26/11
  */
-public class XmlPropertiesTest extends LightPlatformCodeInsightTestCase {
+public class XmlPropertiesTest extends LightPlatformCodeInsightFixtureTestCase {
 
   public void testXmlProperties() throws Exception {
-    configureByFile("foo.xml");
-    List<PropertiesFile> files = PropertiesReferenceManager.getInstance(getProject()).findPropertiesFiles(getModule(), "foo");
+    myFixture.configureByFile("foo.xml");
+    List<PropertiesFile> files = PropertiesReferenceManager.getInstance(getProject()).findPropertiesFiles(myModule, "foo");
     assertEquals(1, files.size());
     PropertiesFile file = files.get(0);
     assertEquals(1, file.findPropertiesByKey("foo").size());
@@ -27,9 +27,18 @@ public class XmlPropertiesTest extends LightPlatformCodeInsightTestCase {
     assertEquals(1, properties.size());
   }
 
+  public void testHighlighting() throws Exception {
+    myFixture.testHighlighting("foo.xml");
+  }
+
   @Override
   protected String getTestDataPath() {
     return PluginPathManager.getPluginHomePath("properties") + "/testData/xml/";
+  }
+
+  @Override
+  protected boolean isWriteActionRequired() {
+    return false;
   }
 
   public XmlPropertiesTest() {
