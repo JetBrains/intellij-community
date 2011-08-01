45c0b08adab6bbbb1bc91a4c0a3c44f6c0b22b03
Evgeny Zakrevsky <evgeny.zakrevsky@jetbrains.com>
2011-08-01 20:18:12 +0400

    minor fixes in forms

--- java/java-impl/src/com/intellij/application/options/CodeStyleImportsPanel.java
+++ java/java-impl/src/com/intellij/application/options/CodeStyleImportsPanel.java
@@ -90,7 +90,7 @@ public class CodeStyleImportsPanel extends JPanel {
     final MultiLineLabel oneImportPerDirectiveLabel = new MultiLineLabel("<% page import=\"com.company.Boo\"%>\n" +
                                                                          "<% page import=\"com.company.Far\"%>");
     final JPanel labelPanel = new JPanel(new BorderLayout());
-    labelPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(20, 10, 0, 0), IdeBorderFactory.createTitledBorderBoldWithIndent(
+    labelPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(20, 10, 0, 0), IdeBorderFactory.createTitledBorderSimpleWithoutIndent(
       ApplicationBundle.message("title.preview"))));
 
     JPanel resultPanel = new JPanel(new BorderLayout());
