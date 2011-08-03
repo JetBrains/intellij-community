3321d8938cd4c30763e904cbcc98993d8192695e
Dennis Ushakov <dennis.ushakov@gmail.com>
2011-08-03 15:09:20 +0400

    RUBY-9065 move coverage related color settings to general/coverage tab

--- java/java-impl/src/com/intellij/openapi/options/colors/pages/JavaColorSettingsPage.java
+++ java/java-impl/src/com/intellij/openapi/options/colors/pages/JavaColorSettingsPage.java
@@ -28,14 +28,11 @@ import com.intellij.openapi.options.colors.AttributesDescriptor;
 import com.intellij.openapi.options.colors.ColorDescriptor;
 import com.intellij.openapi.options.colors.ColorSettingsPage;
 import com.intellij.pom.java.LanguageLevel;
-import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class JavaColorSettingsPage implements ColorSettingsPage, InspectionColorSettingsPage {
@@ -125,15 +122,7 @@ public class JavaColorSettingsPage implements ColorSettingsPage, InspectionColor
 
   @NotNull
   public AttributesDescriptor[] getAttributeDescriptors() {
-    List<AttributesDescriptor> descriptors = new ArrayList<AttributesDescriptor>();
-    ContainerUtil.addAll(descriptors, ourDescriptors);
-    descriptors.add(
-      new AttributesDescriptor(OptionsBundle.message("options.java.color.descriptor.full.coverage"), CodeInsightColors.LINE_FULL_COVERAGE));
-    descriptors.add(new AttributesDescriptor(OptionsBundle.message("options.java.color.descriptor.partial.coverage"),
-                                             CodeInsightColors.LINE_PARTIAL_COVERAGE));
-    descriptors.add(
-      new AttributesDescriptor(OptionsBundle.message("options.java.color.descriptor.none.coverage"), CodeInsightColors.LINE_NONE_COVERAGE));
-    return descriptors.toArray(new AttributesDescriptor[descriptors.size()]);
+    return ourDescriptors;
   }
 
   @NotNull
