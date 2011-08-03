55ab15c99a3dd0b7109e4d94d8b091fdc89bbbe1
Kirill Likhodedov <Kirill.Likhodedov@gmail.com>
2011-08-03 14:46:28 +0400

    Temporarly disable the assertion "don't run Git from AWT"

--- plugins/git4idea/src/git4idea/commands/GitHandler.java
+++ plugins/git4idea/src/git4idea/commands/GitHandler.java
@@ -619,7 +619,7 @@ public abstract class GitHandler {
   }
 
   public void runInCurrentThread(@Nullable Runnable postStartAction) {
-    LOG.assertTrue(!ApplicationManager.getApplication().isDispatchThread(), "Git process should never start in the dispatch thread.");
+    //LOG.assertTrue(!ApplicationManager.getApplication().isDispatchThread(), "Git process should never start in the dispatch thread.");
 
         final GitVcs vcs = GitVcs.getInstance(myProject);
     if (vcs == null) { return; }
