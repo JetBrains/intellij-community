598fcdc879362f6995c9147b142ca792c183ed29
Bas Leijdekkers <basleijdekkers@gmail.com>
2011-08-03 15:41:11 +0200

    [anet] IDEA-69358 (107.52: "Move method" intention on "static method only called from one class" does nothing)(cherry picked from commit 3ed6646)

--- java/java-impl/src/com/intellij/refactoring/move/moveMembers/MoveMembersHandler.java
+++ java/java-impl/src/com/intellij/refactoring/move/moveMembers/MoveMembersHandler.java
@@ -15,14 +15,13 @@
  */
 package com.intellij.refactoring.move.moveMembers;
 
-import com.intellij.psi.*;
-import com.intellij.psi.impl.source.jsp.jspJava.JspHolderMethod;
-import com.intellij.refactoring.move.MoveHandlerDelegate;
-import com.intellij.refactoring.move.MoveCallback;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.refactoring.move.moveClassesOrPackages.MoveClassesOrPackagesHandlerBase;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.*;
+import com.intellij.psi.impl.source.jsp.jspJava.JspHolderMethod;
+import com.intellij.refactoring.move.MoveCallback;
+import com.intellij.refactoring.move.MoveHandlerDelegate;
 import org.jetbrains.annotations.Nullable;
 
 public class MoveMembersHandler extends MoveHandlerDelegate {
@@ -34,9 +33,6 @@ public class MoveMembersHandler extends MoveHandlerDelegate {
   }
 
   public boolean isValidTarget(final PsiElement psiElement, PsiElement[] sources) {
-    for (PsiElement source : sources) {
-      if (MoveClassesOrPackagesHandlerBase.invalid4Move(source)) return false;
-    }
     return psiElement instanceof PsiClass && !(psiElement instanceof PsiAnonymousClass);
   }
 
