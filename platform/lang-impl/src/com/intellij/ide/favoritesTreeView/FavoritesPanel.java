dc680ed09cbbf7ba5a05f265a843a611eabfa786
Konstantin Bulenkov <kb@jetbrains.com>
2011-08-05 02:50:12 +0400

    DnD for Favorites

--- platform/lang-impl/src/com/intellij/ide/favoritesTreeView/FavoritesPanel.java
+++ platform/lang-impl/src/com/intellij/ide/favoritesTreeView/FavoritesPanel.java
@@ -17,12 +17,16 @@ package com.intellij.ide.favoritesTreeView;
 
 import com.intellij.ide.dnd.*;
 import com.intellij.ide.dnd.aware.DnDAwareTree;
+import com.intellij.ide.projectView.impl.TransferableWrapper;
 import com.intellij.ide.util.treeView.AbstractTreeBuilder;
 import com.intellij.ide.util.treeView.AbstractTreeNode;
 import com.intellij.ide.util.treeView.NodeDescriptor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Disposer;
+import com.intellij.psi.PsiElement;
+import com.intellij.ui.awt.RelativeRectangle;
 import com.intellij.util.Function;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
@@ -72,25 +76,37 @@ public class FavoritesPanel {
       .setBeanProvider(new Function<DnDActionInfo, DnDDragStartBean>() {
         @Override
         public DnDDragStartBean fun(DnDActionInfo dnDActionInfo) {
-          return new DnDDragStartBean(4);
+          return new DnDDragStartBean("");
         }
       })
       .setTargetChecker(new DnDTargetChecker() {
         @Override
         public boolean update(DnDEvent event) {
           final Point p = event.getPoint();
-          final TreePath path = myTree.getPathForLocation(p.x, p.y);
-          if (path != null && path.getPathCount() > 1) {
-            final Object o = path.getPath()[1];
-            if (o instanceof DefaultMutableTreeNode) {
-              final Object obj = ((DefaultMutableTreeNode)o).getUserObject();
-              if (obj instanceof FavoritesTreeNodeDescriptor) {
-                final AbstractTreeNode node = ((FavoritesTreeNodeDescriptor)obj).getElement();
-                if (node instanceof FavoritesListNode) {
-                  //todo
+
+          FavoritesListNode node = findFavoritesListNode(p);
+          if (node != null) {
+            TreePath pathToList = myTree.getPathForLocation(p.x, p.y);
+            while (pathToList != null) {
+              final Object pathObj = pathToList.getLastPathComponent();
+              if (pathObj instanceof DefaultMutableTreeNode) {
+                final Object userObject = ((DefaultMutableTreeNode)pathObj).getUserObject();
+                if (userObject instanceof FavoritesTreeNodeDescriptor) {
+                  if (((FavoritesTreeNodeDescriptor)userObject).getElement() == node) {
+                    break;
+                  }
                 }
               }
+              pathToList = pathToList.getParentPath();
             }
+            if (pathToList != null) {
+              Rectangle bounds = myTree.getPathBounds(pathToList);
+              if (bounds != null) {
+                event.setHighlighting(new RelativeRectangle(myTree, bounds), DnDEvent.DropTargetHighlightingType.RECTANGLE);
+              }
+            }
+            event.setDropPossible(true, null);
+            return true;
           }
           event.setDropPossible(false, null);
           return false;
@@ -99,10 +115,35 @@ public class FavoritesPanel {
       .setDropHandler(new DnDDropHandler() {
         @Override
         public void drop(DnDEvent event) {
-
+          final Object obj = event.getAttachedObject();
+          if (obj instanceof TransferableWrapper) {
+            final PsiElement[] elements = ((TransferableWrapper)obj).getPsiElements();
+            final FavoritesListNode node = findFavoritesListNode(event.getPoint());
+            if (elements != null && elements.length > 0 && node != null) {
+              FavoritesManager.getInstance(myProject).addRoots(node.getValue(), null, elements);
+            }
+          }
         }
       })
       .setDisposableParent(myProject)
       .install();
+  }
+
+  @Nullable
+  private FavoritesListNode findFavoritesListNode(Point point) {
+    final TreePath path = myTree.getPathForLocation(point.x, point.y);
+    if (path != null && path.getPathCount() > 1) {
+      final Object o = path.getPath()[1];
+      if (o instanceof DefaultMutableTreeNode) {
+        final Object obj = ((DefaultMutableTreeNode)o).getUserObject();
+        if (obj instanceof FavoritesTreeNodeDescriptor) {
+          final AbstractTreeNode node = ((FavoritesTreeNodeDescriptor)obj).getElement();
+          if (node instanceof FavoritesListNode) {
+            return (FavoritesListNode)node;
+          }
+        }
+      }
+    }
+    return null;
   }
 }
