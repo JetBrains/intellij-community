63ba56bea534954733c1688c519163b083558485
Dmitry Jemerov <yole@jetbrains.com>
2011-08-05 16:56:15 +0200

    more fine-grained usage types (IDEA-69152)

--- platform/usageView/src/com/intellij/usages/impl/rules/UsageType.java
+++ platform/usageView/src/com/intellij/usages/impl/rules/UsageType.java
@@ -31,7 +31,9 @@ public final class UsageType {
   public static final UsageType CLASS_CATCH_CLAUSE_PARAMETER_DECLARATION = new UsageType(UsageViewBundle.message("usage.type.catch.declaration"));
   public static final UsageType CLASS_METHOD_RETURN_TYPE = new UsageType(UsageViewBundle.message("usage.type.return"));
   public static final UsageType CLASS_NEW_OPERATOR = new UsageType(UsageViewBundle.message("usage.type.new"));
+  public static final UsageType CLASS_NEW_ARRAY = new UsageType(UsageViewBundle.message("usage.type.new.array"));
   public static final UsageType ANNOTATION = new UsageType(UsageViewBundle.message("usage.type.annotation"));
+  public static final UsageType TYPE_PARAMETER = new UsageType(UsageViewBundle.message("usage.type.type.parameter"));
 
   public static final UsageType READ = new UsageType(UsageViewBundle.message("usage.type.read"));
   public static final UsageType WRITE = new UsageType(UsageViewBundle.message("usage.type.write"));
