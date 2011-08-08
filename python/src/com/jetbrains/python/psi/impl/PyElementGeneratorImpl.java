5e153ee339809ee6a5be4b1ebde08eb61e5fb3c6
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2011-08-08 16:56:20 +0400

    Test fixed.

--- python/src/com/jetbrains/python/psi/impl/PyElementGeneratorImpl.java
+++ python/src/com/jetbrains/python/psi/impl/PyElementGeneratorImpl.java
@@ -10,6 +10,7 @@ import com.intellij.psi.impl.PsiFileFactoryImpl;
 import com.intellij.psi.tree.TokenSet;
 import com.intellij.testFramework.LightVirtualFile;
 import com.intellij.util.IncorrectOperationException;
+import com.jetbrains.django.util.DjangoStringUtil;
 import com.jetbrains.python.PyTokenTypes;
 import com.jetbrains.python.PythonFileType;
 import com.jetbrains.python.PythonLanguage;
@@ -51,7 +52,7 @@ public class PyElementGeneratorImpl extends PyElementGenerator {
   }
 
   public PyStringLiteralExpression createStringLiteralAlreadyEscaped(String str) {
-    final PsiFile dummyFile = createDummyFile(LanguageLevel.getDefault(), "a="+str);
+    final PsiFile dummyFile = createDummyFile(LanguageLevel.getDefault(), "a=" + str);
     final PyAssignmentStatement expressionStatement = (PyAssignmentStatement)dummyFile.getFirstChild();
     return (PyStringLiteralExpression)expressionStatement.getAssignedValue();
   }
@@ -162,7 +163,6 @@ public class PyElementGeneratorImpl extends PyElementGenerator {
           exprNode.addChild(add);
         }
       }
-
     }
     else {
       ASTNode lastArgNode = afterThis.getNode();
@@ -238,7 +238,7 @@ public class PyElementGeneratorImpl extends PyElementGenerator {
     return (T)ret;
   }
 
-  public PyExpressionStatement createDocstring(String content){
+  public PyExpressionStatement createDocstring(String content) {
     return createFromText(LanguageLevel.getDefault(),
                           PyExpressionStatement.class, content + "\n");
   }
