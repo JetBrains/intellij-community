8d956e321b838051bb42e8b2dd880985d781bdad
unknown <Nadya.Zabrodina@.Labs.IntelliJ.Net>
2011-08-08 11:58:23 +0400

    show formatting blocks CheckBox added

--- platform/lang-impl/src/com/intellij/internal/psiView/PsiViewerSettings.java
+++ platform/lang-impl/src/com/intellij/internal/psiView/PsiViewerSettings.java
@@ -36,6 +36,7 @@ public class PsiViewerSettings implements PersistentStateComponent<PsiViewerSett
   public int textDividerLocation = 250;
   public int treeDividerLocation = 400;
   public int blockRefDividerLocation = 600;
+  public boolean showBlocks = true;
 
   public static PsiViewerSettings getSettings() {
     return ServiceManager.getService(PsiViewerSettings.class);
