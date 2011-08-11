0ed9f997eec6edb429d2bc4769ec574670304eca
Evgeny Zakrevsky <evgeny.zakrevsky@jetbrains.com>
2011-08-11 15:32:07 +0400

    git update dialog fixed

--- platform/vcs-impl/src/com/intellij/openapi/vcs/update/UpdateOrStatusOptionsDialog.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/update/UpdateOrStatusOptionsDialog.java
@@ -43,7 +43,7 @@ public abstract class UpdateOrStatusOptionsDialog extends OptionsDialog {
       myMainPanel = new JPanel(new BorderLayout());
       final Configurable configurable = confs.keySet().iterator().next();
       addComponent(confs.get(configurable), configurable, BorderLayout.CENTER);
-      myMainPanel.add(Box.createHorizontalGlue());
+      myMainPanel.add(Box.createVerticalStrut(10), BorderLayout.SOUTH);
     }
     else {
       myMainPanel = new JTabbedPane();
