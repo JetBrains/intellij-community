0e58281373cdb54f60338fabe10203deaea7f410
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2011-08-12 15:29:20 +0400

    Fixed console prompt inconsistency (PY-3307).

--- python/src/com/jetbrains/python/console/PydevConsoleExecuteActionHandler.java
+++ python/src/com/jetbrains/python/console/PydevConsoleExecuteActionHandler.java
@@ -149,7 +149,7 @@ public class PydevConsoleExecuteActionHandler extends ConsoleExecuteActionHandle
           myInputBuffer = null;
           // Handle prompt
           if (interpreterResponse.need_input) {
-            if (!PyConsoleUtil.INPUT_PROMPT.equals(console.getPrompt())) {
+            if (!PyConsoleUtil.INPUT_PROMPT.equals(console.getPrompt()) && !PyConsoleUtil.HELP_PROMPT.equals(console.getPrompt())) {
               console.setPrompt(PyConsoleUtil.INPUT_PROMPT);
               PyConsoleUtil.scrollDown(currentEditor);
             }
@@ -205,7 +205,7 @@ public class PydevConsoleExecuteActionHandler extends ConsoleExecuteActionHandle
     }
   }
 
-  public String getPrevCommandRunningMessage() {
+  public static String getPrevCommandRunningMessage() {
     return "Previous command is still running. Please wait or press Ctrl+C to interrupt.";
   }
 
