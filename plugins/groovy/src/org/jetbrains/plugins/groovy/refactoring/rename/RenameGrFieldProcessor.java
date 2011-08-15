659254634a2c1c88dbc67ca0ed2d0c911d152cfe
Sergey Evdokimov <sergey.evdokimov@jetbrains.com>
2011-08-15 18:05:22 +0400

    IDEA-73242 (Renaming groovy property brakes java code.)

--- plugins/groovy/src/org/jetbrains/plugins/groovy/refactoring/rename/RenameGrFieldProcessor.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/refactoring/rename/RenameGrFieldProcessor.java
@@ -33,7 +33,6 @@ import com.intellij.util.containers.MultiMap;
 import com.intellij.util.containers.hash.HashMap;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.groovy.lang.GrReferenceAdjuster;
-import org.jetbrains.plugins.groovy.lang.psi.GroovyPsiElement;
 import org.jetbrains.plugins.groovy.lang.psi.GroovyPsiElementFactory;
 import org.jetbrains.plugins.groovy.lang.psi.api.GroovyResolveResult;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrField;
@@ -177,7 +176,15 @@ public class RenameGrFieldProcessor extends RenameJavaVariableProcessor {
   }
 
   private static void handleElementRename(String newName, PsiReference ref, String fieldName) {
-    final String refText = ref.getCanonicalText();
+    final String refText;
+
+    if (ref instanceof PsiQualifiedReference) {
+      refText = ((PsiQualifiedReference)ref).getReferenceName();
+    }
+    else {
+      refText = ref.getCanonicalText();
+    }
+
     String toRename;
     if (fieldName.equals(refText)) {
       toRename = newName;
