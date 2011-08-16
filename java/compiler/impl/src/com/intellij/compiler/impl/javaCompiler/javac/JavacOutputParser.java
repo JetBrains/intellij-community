ef1b903ab508674e47dc712b110737f1d8675d14
Eugene Zhuravlev <jeka@intellij.com>
2011-08-16 11:17:00 +0200

    better detection of javac's out-of-memory errors

--- java/compiler/impl/src/com/intellij/compiler/impl/javaCompiler/javac/JavacOutputParser.java
+++ java/compiler/impl/src/com/intellij/compiler/impl/javaCompiler/javac/JavacOutputParser.java
@@ -80,7 +80,7 @@ public class JavacOutputParser extends OutputParser {
 
     if (colonIndex1 >= 0){ // looks like found something like file path
       @NonNls String part1 = line.substring(0, colonIndex1).trim();
-      if(part1.equalsIgnoreCase("error")) { // jikes
+      if(part1.equalsIgnoreCase("error") /*jikes*/ || part1.equalsIgnoreCase("Caused by")) {
         addMessage(callback, CompilerMessageCategory.ERROR, line.substring(colonIndex1));
         return true;
       }
