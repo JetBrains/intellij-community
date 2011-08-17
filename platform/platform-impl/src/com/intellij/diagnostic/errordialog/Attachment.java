64e0309d3de24644dce6ee24fe7a8276bb1f3487
develar <develar@gmail.com>
2011-08-17 14:46:48 +0400

    add another convenient Attachment constructor

--- platform/platform-impl/src/com/intellij/diagnostic/errordialog/Attachment.java
+++ platform/platform-impl/src/com/intellij/diagnostic/errordialog/Attachment.java
@@ -1,5 +1,12 @@
 package com.intellij.diagnostic.errordialog;
 
+import com.intellij.openapi.fileEditor.impl.LoadTextUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.util.Base64Converter;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
 public class Attachment {
   private final String myPath;
   private String myContent;
@@ -8,6 +15,24 @@ public class Attachment {
   public Attachment(String path, String content) {
     myPath = path;
     myContent = content;
+  }
+
+  public Attachment(@NotNull VirtualFile file) {
+    this(file.getPresentableUrl(), loadText(file));
+  }
+
+  private static String loadText(VirtualFile file) {
+    if (file.getFileType().isBinary()) {
+      try {
+        return "Binary file, base64 encoded: " + Base64Converter.encode(file.contentsToByteArray());
+      }
+      catch (IOException e) {
+        return "Cannot load binary file content";
+      }
+    }
+    else {
+      return LoadTextUtil.loadText(file).toString();
+    }
   }
 
   public String getPath() {
