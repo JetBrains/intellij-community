7f6c10e7d5a390ede72ee3ec9fb47b9d6c30cdce
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2011-08-23 12:33:27 +0200

    Load  IdeaWin32 in tests; cleanup

--- platform/platform-impl/src/com/intellij/openapi/vfs/impl/win32/IdeaWin32.java
+++ platform/platform-impl/src/com/intellij/openapi/vfs/impl/win32/IdeaWin32.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2011 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,8 +19,7 @@ package com.intellij.openapi.vfs.impl.win32;
  * @author Dmitry Avdeev
  */
 public class IdeaWin32 {
+  public native FileInfo getInfo(String path);
 
-    public native FileInfo getInfo(String path);
-
-    public native FileInfo[] listChildren(String path); 
+  public native FileInfo[] listChildren(String path);
 }
--- platform/platform-impl/testSrc/com/intellij/openapi/vfs/local/SymLinkHandlingTest.java
+++ platform/platform-impl/testSrc/com/intellij/openapi/vfs/local/SymLinkHandlingTest.java
@@ -28,9 +28,23 @@ import java.io.File;
 import java.io.IOException;
 
 public class SymLinkHandlingTest extends LightPlatformTestCase {
+  private LocalFileSystem myFileSystem;
+
   @SuppressWarnings("JUnitTestCaseWithNonTrivialConstructors")
   public SymLinkHandlingTest() {
     PlatformTestCase.initPlatformLangPrefix();
+  }
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    myFileSystem = LocalFileSystem.getInstance();
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    myFileSystem = null;
+    super.tearDown();
   }
 
   public void testBadLinksAreIgnored() throws Exception {
@@ -39,11 +53,11 @@ public class SymLinkHandlingTest extends LightPlatformTestCase {
     final File missingFile = new File(FileUtil.getTempDirectory(), "missing_file");
     assertTrue(missingFile.getAbsolutePath(), !missingFile.exists() || missingFile.delete());
     final File missingLinkFile = createTempLink(missingFile.getAbsolutePath(), "missing_link");
-    final VirtualFile missingLinkVFile = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(missingLinkFile);
+    final VirtualFile missingLinkVFile = refreshAndFind(missingLinkFile);
     assertNull(missingLinkVFile);
 
     final File selfLinkFile = createTempLink("self_link", "self_link");
-    final VirtualFile selfLinkVFile = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(selfLinkFile);
+    final VirtualFile selfLinkVFile = refreshAndFind(selfLinkFile);
     assertNull(selfLinkVFile);
   }
 
@@ -52,27 +66,27 @@ public class SymLinkHandlingTest extends LightPlatformTestCase {
 
     final File targetFile = FileUtil.createTempFile("target", "");
     final File linkFile = createTempLink(targetFile.getAbsolutePath(), "link");
-    final VirtualFile linkVFile = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(linkFile);
+    final VirtualFile linkVFile = refreshAndFind(linkFile);
     assertTrue("link=" + linkFile + ", vLink=" + linkVFile, linkVFile != null && !linkVFile.isDirectory() && linkVFile.isSymLink());
 
     assertTrue(targetFile.getAbsolutePath(), targetFile.setWritable(true, false) && targetFile.canWrite());
-    linkVFile.refresh(false, false);
+    refresh();
     assertTrue(linkVFile.getPath(), linkVFile.isWritable());
     assertTrue(targetFile.getAbsolutePath(), targetFile.setWritable(false, false) && !targetFile.canWrite());
-    linkVFile.refresh(false, false);
+    refresh();
     assertFalse(linkVFile.getPath(), linkVFile.isWritable());
 
     final File targetDir = FileUtil.createTempDirectory("targetDir", "");
     final File linkDir = createTempLink(targetDir.getAbsolutePath(), "linkDir");
-    final VirtualFile linkVDir = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(linkDir);
+    final VirtualFile linkVDir = refreshAndFind(linkDir);
     assertTrue("link=" + linkDir + ", vLink=" + linkVDir, linkVDir != null && linkVDir.isDirectory() && linkVDir.isSymLink());
 
     if (!SystemInfo.isWindows) {
       assertTrue(targetDir.getAbsolutePath(), targetDir.setWritable(true, false) && targetDir.canWrite());
-      linkVDir.refresh(false, true);
+      refresh();
       assertTrue(linkVDir.getPath(), linkVDir.isWritable());
       assertTrue(targetDir.getAbsolutePath(), targetDir.setWritable(false, false) && !targetDir.canWrite());
-      linkVDir.refresh(false, true);
+      refresh();
       assertFalse(linkVDir.getPath(), linkVDir.isWritable());
     }
     else {
@@ -85,7 +99,7 @@ public class SymLinkHandlingTest extends LightPlatformTestCase {
 
     final File targetFile = FileUtil.createTempFile("target", "");
     final File linkFile = createTempLink(targetFile.getAbsolutePath(), "link");
-    final VirtualFile linkVFile = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(linkFile);
+    final VirtualFile linkVFile = refreshAndFind(linkFile);
     assertTrue("link=" + linkFile + ", vLink=" + linkVFile, linkVFile != null && !linkVFile.isDirectory() && linkVFile.isSymLink());
 
     AccessToken token = ApplicationManager.getApplication().acquireWriteActionLock(getClass());
@@ -103,7 +117,7 @@ public class SymLinkHandlingTest extends LightPlatformTestCase {
     final File childFile = new File(targetDir, "child.txt");
     assertTrue(childFile.getAbsolutePath(), childFile.exists() || childFile.createNewFile());
     final File linkDir = createTempLink(targetDir.getAbsolutePath(), "linkDir");
-    final VirtualFile linkVDir = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(linkDir);
+    final VirtualFile linkVDir = refreshAndFind(linkDir);
     assertTrue("link=" + linkDir + ", vLink=" + linkVDir,
                linkVDir != null && linkVDir.isDirectory() && linkVDir.isSymLink() && linkVDir.getChildren().length == 1);
 
@@ -142,5 +156,17 @@ public class SymLinkHandlingTest extends LightPlatformTestCase {
     final File targetFile = new File(target);
     assertEquals("target=" + target + ", link=" + linkFile, targetFile.exists(), linkFile.exists());
     return linkFile;
+  }
+
+  private VirtualFile refreshAndFind(final File ioFile) {
+    refresh();
+    return myFileSystem.findFileByPath(ioFile.getAbsolutePath());
+  }
+
+  private void refresh() {
+    final String tempPath = FileUtil.getTempDirectory();
+    final VirtualFile tempDir = myFileSystem.findFileByPath(tempPath);
+    assertNotNull(tempPath, tempDir);
+    tempDir.refresh(false, true);
   }
 }
