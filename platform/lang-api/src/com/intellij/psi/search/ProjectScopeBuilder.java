ca1018acd8e2f1d546a5a87aec12d6d2f666393e
Dmitry Jemerov <yole@jetbrains.com>
2011-08-23 17:50:08 +0200

    separate interface and implementation for ProjectScopeBuilder

--- platform/lang-api/src/com/intellij/psi/search/ProjectScopeBuilder.java
+++ platform/lang-api/src/com/intellij/psi/search/ProjectScopeBuilder.java
@@ -19,101 +19,17 @@
  */
 package com.intellij.psi.search;
 
-import com.intellij.injected.editor.VirtualFileWindow;
 import com.intellij.openapi.components.ServiceManager;
-import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.ProjectFileIndex;
-import com.intellij.openapi.roots.ProjectRootManager;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiBundle;
-import org.jetbrains.annotations.NotNull;
 
-public class ProjectScopeBuilder {
-  private Project myProject;
-
-  public ProjectScopeBuilder(Project project) {
-    myProject = project;
-  }
-
-  public GlobalSearchScope buildLibrariesScope() {
-    return new ProjectAndLibrariesScope(myProject) {
-      @Override
-      public boolean contains(VirtualFile file) {
-        return myProjectFileIndex.isInLibrarySource(file) || myProjectFileIndex.isInLibraryClasses(file);
-      }
-
-      @Override
-      public boolean isSearchInModuleContent(@NotNull Module aModule) {
-        return false;
-      }
-    };
-  }
-
+public abstract class ProjectScopeBuilder {
   public static ProjectScopeBuilder getInstance(Project project) {
     return ServiceManager.getService(project, ProjectScopeBuilder.class);
   }
 
-  public GlobalSearchScope buildAllScope() {
-    final ProjectRootManager projectRootManager = ProjectRootManager.getInstance(myProject);
-    return projectRootManager == null ? new EverythingGlobalScope(myProject) : new ProjectAndLibrariesScope(myProject);
-  }
+  public abstract GlobalSearchScope buildLibrariesScope();
 
-  public GlobalSearchScope buildProjectScope() {
-    final ProjectRootManager projectRootManager = ProjectRootManager.getInstance(myProject);
-    if (projectRootManager == null) {
-      return new EverythingGlobalScope(myProject) {
-        public boolean isSearchInLibraries() {
-          return false;
-        }
-      };
-    }
-    else {
-      return new GlobalSearchScope(myProject) {
-        private final ProjectFileIndex myFileIndex = projectRootManager.getFileIndex();
+  public abstract GlobalSearchScope buildAllScope();
 
-        public boolean contains(VirtualFile file) {
-          if (file instanceof VirtualFileWindow) return true;
-
-          if (myFileIndex.isInLibraryClasses(file) && !myFileIndex.isInSourceContent(file)) return false;
-
-          return myFileIndex.isInContent(file);
-        }
-
-        public int compare(VirtualFile file1, VirtualFile file2) {
-          return 0;
-        }
-
-        public boolean isSearchInModuleContent(@NotNull Module aModule) {
-          return true;
-        }
-
-        public boolean isSearchInLibraries() {
-          return false;
-        }
-
-        public String getDisplayName() {
-          return PsiBundle.message("psi.search.scope.project");
-        }
-
-        public String toString() {
-          return getDisplayName();
-        }
-
-        @Override
-        public GlobalSearchScope uniteWith(@NotNull GlobalSearchScope scope) {
-          if (scope == this || !scope.isSearchInLibraries() || !scope.isSearchOutsideRootModel()) return this;
-          return super.uniteWith(scope);
-        }
-
-        @NotNull
-        @Override
-        public GlobalSearchScope intersectWith(@NotNull GlobalSearchScope scope) {
-          if (scope == this) return this;
-          if (!scope.isSearchInLibraries()) return scope;
-          return super.intersectWith(scope);
-        }
-      };
-    }
-  }
+  public abstract GlobalSearchScope buildProjectScope();
 }
