be0cc7cf9fd0d674db05ec0445de6bfd4d2ed50a
Gregory.Shrago <gregory.shrago@jetbrains.com>
2011-08-26 21:50:24 +0400

    fix: Cyclic component initialization Exception

--- xml/relaxng/src/org/intellij/plugins/relaxNG/ApplicationLoader.java
+++ xml/relaxng/src/org/intellij/plugins/relaxNG/ApplicationLoader.java
@@ -110,7 +110,7 @@ public class ApplicationLoader implements ApplicationComponent, InspectionToolPr
     };
   }
 
-  public class ResourceProvider implements StandardResourceProvider {
+  public static class ResourceProvider implements StandardResourceProvider {
     public void registerResources(ResourceRegistrar registrar) {
       registrar.addStdResource(RNG_NAMESPACE, "/resources/relaxng.rng", getClass());
       registrar.addIgnoredResource("http://relaxng.org/ns/compatibility/annotations/1.0");
