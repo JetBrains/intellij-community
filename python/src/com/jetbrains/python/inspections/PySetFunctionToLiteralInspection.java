704d2ae8946bdd98b7cbd0eafeaa3378a20c139f
Dmitry Jemerov <yole@jetbrains.com>
2011-08-29 17:14:07 +0200

    optimize inspection (fast checks first)

--- python/src/com/jetbrains/python/inspections/PySetFunctionToLiteralInspection.java
+++ python/src/com/jetbrains/python/inspections/PySetFunctionToLiteralInspection.java
@@ -1,15 +1,14 @@
 package com.jetbrains.python.inspections;
 
 import com.intellij.codeInspection.ProblemsHolder;
-import com.intellij.openapi.roots.ProjectFileIndex;
-import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
-import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiReference;
 import com.jetbrains.python.PyBundle;
+import com.jetbrains.python.PyNames;
 import com.jetbrains.python.actions.ReplaceFunctionWithSetLiteralQuickFix;
 import com.jetbrains.python.psi.*;
+import com.jetbrains.python.psi.impl.PyBuiltinCache;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 
@@ -43,49 +42,40 @@ public class PySetFunctionToLiteralInspection extends PyInspection {
     public void visitPyCallExpression(final PyCallExpression node) {
       if (LanguageLevel.forElement(node).supportsSetLiterals()) {
         PyExpression callee = node.getCallee();
-        if (callee != null) {
-          if (isBuiltinSet(callee)) {
-            PyExpression[] arguments = node.getArguments();
-            if (arguments.length == 1) {
-              PyExpression argument= arguments[0];
-              String string = "";
-              PyElement[] elements = {};
-              if (argument instanceof PyStringLiteralExpression) {
-                string = ((PyStringLiteralExpression)argument).getStringValue();
-              }
-              if ((argument instanceof PySequenceExpression || (argument instanceof PyParenthesizedExpression &&
-                            ((PyParenthesizedExpression)argument).getContainedExpression() instanceof PyTupleExpression))) {
-
-                if (argument instanceof PySequenceExpression)
-                  elements = ((PySequenceExpression)argument).getElements();
-                if (argument instanceof PyParenthesizedExpression) {
-                  PyExpression tuple = ((PyParenthesizedExpression)argument).getContainedExpression();
-                  if (tuple instanceof PyTupleExpression)
-                    elements = ((PyTupleExpression)(tuple)).getElements();
-                }
-              }
-              if (elements.length != 0 || !string.isEmpty())
-                  registerProblem(node, "Function call can be replaced with set literal",
-                                                   new ReplaceFunctionWithSetLiteralQuickFix(elements, string));
+        if (node.isCalleeText(PyNames.SET) && isInBuiltins(callee)) {
+          PyExpression[] arguments = node.getArguments();
+          if (arguments.length == 1) {
+            PyExpression argument= arguments[0];
+            String string = "";
+            PyElement[] elements = {};
+            if (argument instanceof PyStringLiteralExpression) {
+              string = ((PyStringLiteralExpression)argument).getStringValue();
             }
+            if ((argument instanceof PySequenceExpression || (argument instanceof PyParenthesizedExpression &&
+                          ((PyParenthesizedExpression)argument).getContainedExpression() instanceof PyTupleExpression))) {
+
+              if (argument instanceof PySequenceExpression)
+                elements = ((PySequenceExpression)argument).getElements();
+              if (argument instanceof PyParenthesizedExpression) {
+                PyExpression tuple = ((PyParenthesizedExpression)argument).getContainedExpression();
+                if (tuple instanceof PyTupleExpression)
+                  elements = ((PyTupleExpression)(tuple)).getElements();
+              }
+            }
+            if (elements.length != 0 || !string.isEmpty())
+                registerProblem(node, "Function call can be replaced with set literal",
+                                                 new ReplaceFunctionWithSetLiteralQuickFix(elements, string));
           }
         }
       }
     }
 
-    private static boolean isBuiltinSet(PyExpression callee) {
-      ProjectFileIndex ind = ProjectRootManager.getInstance(callee.getProject()).getFileIndex();
+    private static boolean isInBuiltins(PyExpression callee) {
       PsiReference reference = callee.getReference();
       if (reference != null) {
         PsiElement resolved = reference.resolve();
-        if (resolved != null) {
-          PsiFile file = resolved.getContainingFile();
-          if (file != null && file.getVirtualFile() != null && ind.isInLibraryClasses(file.getVirtualFile())) {
-            if (callee.getText().equals("set")) {
-              return true;
-
-            }
-          }
+        if (resolved != null && PyBuiltinCache.getInstance(callee).hasInBuiltins(resolved)) {
+          return true;
         }
       }
       return false;
