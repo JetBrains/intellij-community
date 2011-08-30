b16a2ef1548cf4ce8b118dcb0d9ac5c94a54b811
Rustam.Vishnyakov <rustam.vishnyakov@gmail.com>
2011-08-30 16:37:04 +0400

    Separate storage for new project code style settings (for compatibility with versions before 11.x)

--- platform/lang-api/src/com/intellij/psi/codeStyle/LegacyCodeStyleSettingsManager.java
+++ platform/lang-api/src/com/intellij/psi/codeStyle/LegacyCodeStyleSettingsManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2011 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,20 +13,37 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.intellij.psi.codeStyle;
 
+import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
 import com.intellij.openapi.components.StorageScheme;
+import org.jdom.Element;
 
-
+/**
+ * Supports old code style settings (before version 11.x). The settings are returned as is without any
+ * changes.
+ * @author Rustam Vishnyakov
+ */
 @State(
-  name = "ProjectCodeStyleSettingsManager",
+  name = "CodeStyleSettingsManager",
   storages = {
     @Storage( file = "$PROJECT_FILE$")
-   ,@Storage( file = "$PROJECT_CONFIG_DIR$/codeStyleSettings.xml", scheme = StorageScheme.DIRECTORY_BASED)
+   ,@Storage( file = "$PROJECT_CONFIG_DIR$/projectCodeStyle.xml", scheme = StorageScheme.DIRECTORY_BASED)
     }
 )
-public class ProjectCodeStyleSettingsManager extends CodeStyleSettingsManager{
+public class LegacyCodeStyleSettingsManager implements PersistentStateComponent<Element> {
+  
+  private Element myState;
+  
+  @Override
+  public Element getState() {
+    return myState;
+  }
+
+  @Override
+  public void loadState(Element state) {
+    myState = state;
+  }
 }
