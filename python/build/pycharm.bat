06bc7c1311d9e39fbd4c45b9c34c3b5e08cd118a
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2011-08-30 14:52:29 +0200

    .bat scripts fixed and unified

--- python/build/pycharm.bat
+++ python/build/pycharm.bat
@@ -1,64 +1,71 @@
-@echo off
+@ECHO OFF
 
 ::----------------------------------------------------------------------
 :: PyCharm Startup Script
 ::----------------------------------------------------------------------
 
 :: ---------------------------------------------------------------------
-:: Before you run PyCharm specify the location of the
-:: JDK 1.5 installation directory which will be used for running IDEA
+:: Location of the JDK 1.6 installation directory
+:: which will be used for running the IDE.
 :: ---------------------------------------------------------------------
-IF "%IDEA_JDK%" == "" SET IDEA_JDK=%JDK_HOME%
-IF "%IDEA_JDK%" == "" goto error
+SET JDK=%PYCHARM_JDK%
+IF "%JDK%" == "" SET JDK=%JDK_HOME%
+IF "%JDK%" == "" GOTO error
+
+SET JAVA_EXE=%JDK%\jre\bin\java.exe
+IF NOT EXIST "%JAVA_EXE%" GOTO error
 
 :: ---------------------------------------------------------------------
-:: Before you run PyCharm specify the location of the
-:: directory where PyCharm is installed
+:: Location of the directory where the IDE is installed
 :: In most cases you do not need to change the settings below.
 :: ---------------------------------------------------------------------
-SET IDEA_HOME=..
+SET IDE_BIN_DIR=%~dp0
+SET IDE_HOME=%IDE_BIN_DIR%\..
 
-SET JAVA_EXE=%IDEA_JDK%\jre\bin\java.exe
-IF NOT EXIST "%JAVA_EXE%" goto error
+SET MAIN_CLASS_NAME=%PYCHARM_MAIN_CLASS_NAME%
+IF "%MAIN_CLASS_NAME%" == "" SET MAIN_CLASS_NAME=com.intellij.idea.Main
 
-IF "%IDEA_MAIN_CLASS_NAME%" == "" SET IDEA_MAIN_CLASS_NAME=com.intellij.idea.Main
-
-IF NOT "%IDEA_PROPERTIES%" == "" set IDEA_PROPERTIES_PROPERTY="-Didea.properties.file=%IDEA_PROPERTIES%"
+IF NOT "%PYCHARM_PROPERTIES%" == "" SET IDE_PROPERTIES_PROPERTY="-Didea.properties.file=%PYCHARM_PROPERTIES%"
 
 :: ---------------------------------------------------------------------
-:: You may specify your own JVM arguments in pycharm.exe.vmoptions file. Put one option per line there.
+:: You may specify your own JVM arguments in .vmoptions file.
+:: Put one option per line there.
 :: ---------------------------------------------------------------------
+SET VM_OPTIONS_FILE=%IDE_BIN_DIR%\pycharm.exe.vmoptions
 SET ACC=
-FOR /F "delims=" %%i in (%IDEA_HOME%\bin\pycharm.exe.vmoptions) DO call %IDEA_HOME%\bin\append.bat "%%i"
+FOR /F "delims=" %%i IN (%VM_OPTIONS_FILE%) DO CALL %IDE_BIN_DIR%\append.bat "%%i"
 
-set REQUIRED_IDEA_JVM_ARGS=-Xbootclasspath/a:%IDEA_HOME%/lib/boot.jar -Didea.platform.prefix=Python -Didea.paths.selector=@@system_selector@@ %IDEA_PROPERTIES_PROPERTY% %REQUIRED_IDEA_JVM_ARGS%
-SET JVM_ARGS=%ACC% %REQUIRED_IDEA_JVM_ARGS%
+SET REQUIRED_JVM_ARGS=-Xbootclasspath/a:%IDE_HOME%/lib/boot.jar -Didea.paths.selector=@@system_selector@@ %IDE_PROPERTIES_PROPERTY%
+SET SPECIAL_JVM_ARGS=-Didea.platform.prefix=Python
+SET JVM_ARGS=%ACC% %REQUIRED_JVM_ARGS% %SPECIAL_JVM_ARGS% %REQUIRED_PYCHARM_JVM_ARGS%
 
 SET OLD_PATH=%PATH%
-SET PATH=%IDEA_HOME%\bin;%PATH%
+SET PATH=%IDE_BIN_DIR%;%PATH%
 
-SET CLASS_PATH=%IDEA_HOME%\lib\bootstrap.jar
-SET CLASS_PATH=%CLASS_PATH%;%IDEA_HOME%\lib\util.jar
-SET CLASS_PATH=%CLASS_PATH%;%IDEA_HOME%\lib\jdom.jar
-SET CLASS_PATH=%CLASS_PATH%;%IDEA_HOME%\lib\log4j.jar
-SET CLASS_PATH=%CLASS_PATH%;%IDEA_HOME%\lib\extensions.jar
-SET CLASS_PATH=%CLASS_PATH%;%IDEA_HOME%\lib\trove4j.jar
-SET CLASS_PATH=%CLASS_PATH%;%IDEA_HOME%\lib\jna.jar
+SET CLASS_PATH=%IDE_HOME%\lib\bootstrap.jar
+SET CLASS_PATH=%CLASS_PATH%;%IDE_HOME%\lib\util.jar
+SET CLASS_PATH=%CLASS_PATH%;%IDE_HOME%\lib\jdom.jar
+SET CLASS_PATH=%CLASS_PATH%;%IDE_HOME%\lib\log4j.jar
+SET CLASS_PATH=%CLASS_PATH%;%IDE_HOME%\lib\extensions.jar
+SET CLASS_PATH=%CLASS_PATH%;%IDE_HOME%\lib\trove4j.jar
+SET CLASS_PATH=%CLASS_PATH%;%IDE_HOME%\lib\jna.jar
 
 :: ---------------------------------------------------------------------
-:: You may specify additional class paths in IDEA_CLASS_PATH variable.
+:: You may specify additional class paths in PYCHARM_CLASS_PATH variable.
 :: It is a good idea to specify paths to your plugins in this variable.
 :: ---------------------------------------------------------------------
-IF NOT "%IDEA_CLASS_PATH%" == "" SET CLASS_PATH=%CLASS_PATH%;%IDEA_CLASS_PATH%
+IF NOT "%PYCHARM_CLASS_PATH%" == "" SET CLASS_PATH=%CLASS_PATH%;%PYCHARM_CLASS_PATH%
 
-"%JAVA_EXE%" %JVM_ARGS% -cp "%CLASS_PATH%" %IDEA_MAIN_CLASS_NAME% %*
+"%JAVA_EXE%" %JVM_ARGS% -cp "%CLASS_PATH%" %MAIN_CLASS_NAME% %*
 
 SET PATH=%OLD_PATH%
-goto end
+GOTO end
+
 :error
-echo ---------------------------------------------------------------------
-echo ERROR: cannot start PyCharm.
-echo No JDK found to run PyCharm. Please validate either IDEA_JDK or JDK_HOME points to valid JDK installation
-echo ---------------------------------------------------------------------
-pause
+ECHO ---------------------------------------------------------------------
+ECHO ERROR: cannot start PyCharm.
+ECHO No JDK found. Please validate either PYCHARM_JDK or JDK_HOME points to valid JDK installation.
+ECHO ---------------------------------------------------------------------
+PAUSE
+
 :end
