127039672fd2821308848312b11b5bddcbcd024c
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2011-08-30 14:52:30 +0200

    .bat scripts fixed and unified

--- bin/win/inspect_diff.bat
+++ bin/win/inspect_diff.bat
@@ -1,53 +1,58 @@
-@echo off
+@ECHO OFF
 
 ::----------------------------------------------------------------------
 :: IntelliJ IDEA Startup Script
 ::----------------------------------------------------------------------
 
 :: ---------------------------------------------------------------------
-:: Before you run IntelliJ IDEA specify the location of the
-:: JDK 1.5 installation directory which will be used for running IDEA
+:: Location of the JDK 1.6 installation directory
+:: which will be used for running the IDE.
 :: ---------------------------------------------------------------------
+SET JDK=%IDEA_JDK%
+IF "%JDK%" == "" SET JDK=%JDK_HOME%
+IF "%JDK%" == "" GOTO error
 
-IF "%IDEA_JDK%" == "" SET IDEA_JDK=%JDK_HOME%
-IF "%IDEA_JDK%" == "" goto error
+SET JAVA_EXE=%JDK%\jre\bin\java.exe
+IF NOT EXIST "%JAVA_EXE%" GOTO error
 
 :: ---------------------------------------------------------------------
-:: Before you run IntelliJ IDEA specify the location of the
-:: directory where IntelliJ IDEA is installed
+:: Location of the directory where the IDE is installed
 :: In most cases you do not need to change the settings below.
 :: ---------------------------------------------------------------------
-SET IDEA_HOME=..
+SET IDE_BIN_DIR=%~dp0
+SET IDE_HOME=%IDE_BIN_DIR%\..
 
-SET JAVA_EXE=%IDEA_JDK%\jre\bin\java.exe
-IF NOT EXIST "%JAVA_EXE%" goto error
+SET MAIN_CLASS_NAME=%IDEA_INSPECT_DIFF_CLASS_NAME%
+IF "%MAIN_CLASS_NAME%" == "" SET MAIN_CLASS_NAME=com.intellij.codeInspection.InspectionDiff
 
-IF "%IDEA_INSPECT_DIFF_CLASS_NAME%" == "" SET IDEA_INSPECT_DIFF_CLASS_NAME=com.intellij.codeInspection.InspectionDiff
-
-IF NOT "%IDEA_PROPERTIES%" == "" set IDEA_PROPERTIES_PROPERTY=-Didea.properties.file=%IDEA_PROPERTIES%
+IF NOT "%IDEA_PROPERTIES%" == "" SET IDE_PROPERTIES_PROPERTY="-Didea.properties.file=%IDEA_PROPERTIES%"
 
 :: ---------------------------------------------------------------------
-:: You may specify your own JVM arguments in idea.exe.vmoptions file. Put one option per line there.
+:: You may specify your own JVM arguments in .vmoptions file.
+:: Put one option per line there.
 :: ---------------------------------------------------------------------
+SET VM_OPTIONS_FILE=%IDE_BIN_DIR%\idea.exe.vmoptions
 SET ACC=
-FOR /F "delims=" %%i in (%IDEA_HOME%\bin\idea.exe.vmoptions) DO call %IDEA_HOME%\bin\append.bat "%%i"
+FOR /F "delims=" %%i IN (%VM_OPTIONS_FILE%) DO CALL %IDE_BIN_DIR%\append.bat "%%i"
 
-set REQUIRED_IDEA_JVM_ARGS=-Xbootclasspath/a:%IDEA_HOME%/lib/boot.jar -Didea.paths.selector=@@system_selector@@ %IDEA_PROPERTIES_PROPERTY% %REQUIRED_IDEA_JVM_ARGS%
-SET JVM_ARGS=%ACC% %REQUIRED_IDEA_JVM_ARGS%
+SET REQUIRED_JVM_ARGS=-Xbootclasspath/a:%IDE_HOME%/lib/boot.jar -Didea.paths.selector=@@system_selector@@ %IDE_PROPERTIES_PROPERTY%
+SET SPECIAL_JVM_ARGS=
+SET JVM_ARGS=%ACC% %REQUIRED_JVM_ARGS% %SPECIAL_JVM_ARGS% %REQUIRED_IDEA_JVM_ARGS%
 
 SET OLD_PATH=%PATH%
-SET PATH=%IDEA_HOME%\bin;%PATH%
+SET PATH=%IDE_BIN_DIR%;%PATH%
 
-SET CLASS_PATH=%IDEA_HOME%\lib\bootstrap.jar
-SET CLASS_PATH=%CLASS_PATH%;%IDEA_HOME%\lib\idea.jar
-SET CLASS_PATH=%CLASS_PATH%;%IDEA_HOME%\lib\resources_en.jar
-SET CLASS_PATH=%CLASS_PATH%;%IDEA_HOME%\lib\openapi.jar
-SET CLASS_PATH=%CLASS_PATH%;%IDEA_HOME%\lib\util.jar
-SET CLASS_PATH=%CLASS_PATH%;%IDEA_HOME%\lib\jdom.jar
-SET CLASS_PATH=%CLASS_PATH%;%IDEA_HOME%\lib\log4j.jar
-SET CLASS_PATH=%CLASS_PATH%;%IDEA_HOME%\lib\extensions.jar
-SET CLASS_PATH=%CLASS_PATH%;%IDEA_HOME%\lib\trove4j.jar
-SET CLASS_PATH=%CLASS_PATH%;%IDEA_JDK%\lib\tools.jar
+SET CLASS_PATH=%IDE_HOME%\lib\bootstrap.jar
+SET CLASS_PATH=%CLASS_PATH%;%IDE_HOME%\lib\idea.jar
+SET CLASS_PATH=%CLASS_PATH%;%IDE_HOME%\lib\resources_en.jar
+SET CLASS_PATH=%CLASS_PATH%;%IDE_HOME%\lib\openapi.jar
+SET CLASS_PATH=%CLASS_PATH%;%IDE_HOME%\lib\util.jar
+SET CLASS_PATH=%CLASS_PATH%;%IDE_HOME%\lib\jdom.jar
+SET CLASS_PATH=%CLASS_PATH%;%IDE_HOME%\lib\log4j.jar
+SET CLASS_PATH=%CLASS_PATH%;%IDE_HOME%\lib\extensions.jar
+SET CLASS_PATH=%CLASS_PATH%;%IDE_HOME%\lib\trove4j.jar
+SET CLASS_PATH=%CLASS_PATH%;%IDE_HOME%\lib\jna.jar
+SET CLASS_PATH=%CLASS_PATH%;%JDK%\lib\tools.jar
 
 :: ---------------------------------------------------------------------
 :: You may specify additional class paths in IDEA_CLASS_PATH variable.
@@ -55,15 +60,16 @@ SET CLASS_PATH=%CLASS_PATH%;%IDEA_JDK%\lib\tools.jar
 :: ---------------------------------------------------------------------
 IF NOT "%IDEA_CLASS_PATH%" == "" SET CLASS_PATH=%CLASS_PATH%;%IDEA_CLASS_PATH%
 
-"%JAVA_EXE%" %JVM_ARGS% -cp "%CLASS_PATH%" %IDEA_INSPECT_DIFF_CLASS_NAME% %*
+"%JAVA_EXE%" %JVM_ARGS% -cp "%CLASS_PATH%" %MAIN_CLASS_NAME% %*
 
 SET PATH=%OLD_PATH%
-goto end
+GOTO end
+
 :error
-echo ---------------------------------------------------------------------
-echo ERROR: cannot start IntelliJ IDEA.
-echo No JDK found to run IDEA. Please validate either IDEA_JDK or JDK_HOME points to valid JDK installation
-echo ---------------------------------------------------------------------
-pause
+ECHO ---------------------------------------------------------------------
+ECHO ERROR: cannot start IntelliJ IDEA.
+ECHO No JDK found. Please validate either IDEA_JDK or JDK_HOME points to valid JDK installation.
+ECHO ---------------------------------------------------------------------
+PAUSE
 
 :end
