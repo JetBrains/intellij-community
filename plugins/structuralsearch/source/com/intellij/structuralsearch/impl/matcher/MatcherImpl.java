ca72779b78c2e8b875f8b9b07c3c0c76b3e9bf43
Dmitry Jemerov <yole@jetbrains.com>
2011-09-01 17:58:08 +0200

    get rid of identical method implementations in PsiLanguageInjectionHost interface, delegate clients directly to InjectedLanguageUtil

--- plugins/structuralsearch/source/com/intellij/structuralsearch/impl/matcher/MatcherImpl.java
+++ plugins/structuralsearch/source/com/intellij/structuralsearch/impl/matcher/MatcherImpl.java
@@ -24,6 +24,7 @@ import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileFilter;
 import com.intellij.psi.*;
+import com.intellij.psi.impl.source.tree.injected.InjectedLanguageUtil;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.search.LocalSearchScope;
 import com.intellij.psi.search.SearchScope;
@@ -676,7 +677,7 @@ public class MatcherImpl {
       match(el);
     }
     if (element instanceof PsiLanguageInjectionHost) {
-      ((PsiLanguageInjectionHost)element).processInjectedPsi(new PsiLanguageInjectionHost.InjectedPsiVisitor() {
+      InjectedLanguageUtil.enumerate(element, new PsiLanguageInjectionHost.InjectedPsiVisitor() {
         @Override
         public void visit(@NotNull PsiFile injectedPsi, @NotNull List<PsiLanguageInjectionHost.Shred> places) {
           match(injectedPsi);
