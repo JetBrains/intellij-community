6afae67d2f11b59bf5504169c34351611209648f
peter <peter@jetbrains.com>
2011-09-06 15:35:36 +0200

    groovy live templates granularity

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/GroovyLiveTemplatesTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/GroovyLiveTemplatesTest.java
@@ -7,12 +7,17 @@ package org.jetbrains.plugins.groovy.lang;
 import com.intellij.codeInsight.lookup.Lookup;
 import com.intellij.codeInsight.lookup.LookupManager;
 import com.intellij.codeInsight.lookup.impl.LookupImpl;
+import com.intellij.codeInsight.template.impl.TemplateImpl;
+import com.intellij.codeInsight.template.impl.TemplateManagerImpl;
+import com.intellij.codeInsight.template.impl.TemplateSettings;
 import com.intellij.codeInsight.template.impl.actions.ListTemplatesAction;
 import com.intellij.openapi.application.Result;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
 import org.jetbrains.plugins.groovy.util.TestUtils;
+
+import java.io.IOException;
 
 /**
  * @author peter
@@ -37,6 +42,41 @@ public class GroovyLiveTemplatesTest extends LightCodeInsightFixtureTestCase{
         ((LookupImpl)LookupManager.getActiveLookup(editor)).finishLookup(Lookup.NORMAL_SELECT_CHAR);
       }
     }.execute();
+  }
+
+  public void testGroovyStatementContext() throws Exception {
+    final TemplateImpl template = TemplateSettings.getInstance().getTemplate("inst", "other");
+    assertFalse(isApplicable("class Foo {{ if (a inst<caret>) }}", template));
+    assertTrue(isApplicable("class Foo {{ inst<caret> }}", template));
+    assertTrue(isApplicable("inst<caret>", template));
+    assertFalse(isApplicable("class Foo {{ return (inst<caret>) }}", template));
+  }
+
+  public void testGroovyExpressionContext() throws Exception {
+    final TemplateImpl template = TemplateSettings.getInstance().getTemplate("lst", "other");
+    assertFalse(isApplicable("class Foo {{ if (a toar<caret>) }}", template));
+    assertTrue(isApplicable("class Foo {{ toar<caret> }}", template));
+    assertTrue(isApplicable("xxx<caret>", template));
+    assertTrue(isApplicable("<caret>xxx", template));
+    assertTrue(isApplicable("class Foo {{ return (toar<caret>) }}", template));
+  }
+
+  public void testGroovyDeclarationContext() throws Exception {
+    final TemplateImpl template = TemplateSettings.getInstance().getTemplate("psvm", "other");
+    assertFalse(isApplicable("class Foo {{ <caret>xxx }}", template));
+    assertFalse(isApplicable("class Foo {{ toar<caret> }}", template));
+    assertFalse(isApplicable("class Foo {{ if (a toar<caret>) }}", template));
+    assertFalse(isApplicable("class Foo {{ return (toar<caret>) }}", template));
+    assertTrue(isApplicable("class Foo { xxx<caret> }", template));
+    assertTrue(isApplicable("class Foo { xxx<caret> }", template));
+    assertFalse(isApplicable("class Foo { int xxx<caret> }", template));
+    assertTrue(isApplicable("class Foo {}\nxxx<caret>", template));
+    assertTrue(isApplicable("xxx<caret>", template));
+  }
+
+  private boolean isApplicable(String text, TemplateImpl inst) throws IOException {
+    myFixture.configureByText("a.groovy", text);
+    return TemplateManagerImpl.isApplicable(myFixture.getFile(), myFixture.getEditor().getCaretModel().getOffset(), inst);
   }
 
 }
