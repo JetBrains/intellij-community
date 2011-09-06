e91dd9831569717e0a267cae7e263d2b7b3b0eb1
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2011-09-06 16:30:10 +0400

    fixed PY-4426 Mako Template: Changing integrated tool to Mako takes place only after PyCharm restart

--- python/src/com/jetbrains/python/configuration/PyIntegratedToolsConfigurable.java
+++ python/src/com/jetbrains/python/configuration/PyIntegratedToolsConfigurable.java
@@ -1,15 +1,27 @@
 package com.jetbrains.python.configuration;
 
+import com.google.common.collect.Lists;
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.ex.EditorEx;
+import com.intellij.openapi.editor.highlighter.EditorHighlighter;
+import com.intellij.openapi.editor.highlighter.EditorHighlighterFactory;
+import com.intellij.openapi.editor.impl.EditorFactoryImpl;
 import com.intellij.openapi.fileChooser.FileChooserDescriptor;
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.options.NonDefaultProjectConfigurable;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ContentIterator;
+import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
 import com.intellij.openapi.util.Comparing;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.CollectionComboBoxModel;
+import com.intellij.util.FileContentUtil;
+import com.jetbrains.django.lang.template.DjangoTemplateFileType;
 import com.jetbrains.mako.TemplatesConfigurationsModel;
 import com.jetbrains.mako.TemplatesService;
 import com.jetbrains.python.documentation.DocStringFormat;
@@ -18,6 +30,7 @@ import com.jetbrains.python.testing.PythonTestConfigurationsModel;
 import com.jetbrains.python.testing.TestRunnerService;
 import com.jetbrains.rest.ReSTService;
 import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.util.List;
@@ -89,7 +102,6 @@ public class PyIntegratedToolsConfigurable implements Configurable, NonDefaultPr
       return true;
     }
     if (myTemplateLanguage.getSelectedItem() != myTemplatesModel.getProjectConfiguration()) {
-      DaemonCodeAnalyzer.getInstance(myProject).restart();
       return true;
     }
     if (!Comparing.equal(myDocstringFormatComboBox.getSelectedItem(), myDocumentationSettings.myDocStringFormat)) {
@@ -103,13 +115,52 @@ public class PyIntegratedToolsConfigurable implements Configurable, NonDefaultPr
     return false;
   }
 
+  public static void reparseTemplateFiles(final @NotNull Project project) {
+    final List<VirtualFile> templatesToReparse = Lists.newArrayList();
+    ProjectRootManager.getInstance(project).getFileIndex().iterateContent(new ContentIterator() {
+      @Override
+      public boolean processFile(VirtualFile fileOrDir) {
+        if (!fileOrDir.isDirectory() && DjangoTemplateFileType.getPossibleExtensions().contains(fileOrDir.getExtension())) {
+          templatesToReparse.add(fileOrDir);
+          System.out.println(fileOrDir);
+        }
+        return true;
+      }
+    });
+    FileContentUtil.reparseFiles(project, templatesToReparse, true);
+
+    ApplicationManager.getApplication().runWriteAction(new Runnable() {
+      @Override
+      public void run() {
+
+        for (Editor editor : EditorFactoryImpl.getInstance().getAllEditors()) {
+          if (editor instanceof EditorEx) {
+            final VirtualFile vFile = ((EditorEx)editor).getVirtualFile();
+            if (vFile != null) {
+              final EditorHighlighter highlighter = EditorHighlighterFactory.getInstance().createEditorHighlighter(project, vFile);
+              ((EditorEx)editor).setHighlighter(highlighter);
+            }
+          }
+        }
+      }
+    });
+
+    DaemonCodeAnalyzer.getInstance(project).restart();
+  }
+
+
   @Override
   public void apply() throws ConfigurationException {
     myModel.apply();
+    boolean needReparse = false;
+    if (myTemplateLanguage.getSelectedItem() != myTemplatesModel.getProjectConfiguration())
+      needReparse = true;
     myTemplatesModel.apply();
     myDocumentationSettings.myDocStringFormat = (String) myDocstringFormatComboBox.getSelectedItem();
     ReSTService.getInstance(myProject).setWorkdir(myWorkDir.getText());
     ReSTService.getInstance(myProject).setTxtIsRst(txtIsRst.isSelected());
+    if (needReparse)
+      reparseTemplateFiles(myProject);
   }
 
   @Override
