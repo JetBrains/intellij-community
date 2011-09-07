dd4c42436139eb617e4420eda56c18d6ed6a3fbd
Dmitry Jemerov <yole@jetbrains.com>
2011-09-07 19:10:28 +0200

    SOE protection when resolving type references (EA-29174 - SOE: PyReturnTypeReference.isBuiltin)

--- python/src/com/jetbrains/python/psi/types/PyTypeReferenceImpl.java
+++ python/src/com/jetbrains/python/psi/types/PyTypeReferenceImpl.java
@@ -1,5 +1,6 @@
 package com.jetbrains.python.psi.types;
 
+import com.intellij.psi.PsiElement;
 import com.intellij.util.ArrayUtil;
 import com.intellij.util.ProcessingContext;
 import com.jetbrains.python.psi.AccessDirection;
@@ -7,9 +8,12 @@ import com.jetbrains.python.psi.PyExpression;
 import com.jetbrains.python.psi.resolve.PyResolveContext;
 import com.jetbrains.python.psi.resolve.RatedResolveResult;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author yole
@@ -26,4 +30,23 @@ public abstract class PyTypeReferenceImpl implements PyTypeReference {
   public Object[] getCompletionVariants(String completionPrefix, PyExpression location, ProcessingContext context) {
     return ArrayUtil.EMPTY_OBJECT_ARRAY;
   }
+
+  @Override
+  public PyType resolve(@Nullable PsiElement context, TypeEvalContext typeEvalContext) {
+    Set<PyTypeReferenceImpl> seen = new HashSet<PyTypeReferenceImpl>();
+    seen.add(this);
+    PyType resolved;
+    PyTypeReferenceImpl current = this;
+    while (true) {
+      resolved = current.resolveStep(null, typeEvalContext);
+      if (!(resolved instanceof PyTypeReferenceImpl)) break;
+      current = (PyTypeReferenceImpl) resolved;
+      if (seen.contains(current)) return null;
+      seen.add(current);
+    }
+    return resolved;
+  }
+  
+  @Nullable
+  protected abstract PyType resolveStep(@Nullable PsiElement context, TypeEvalContext typeEvalContext);
 }
