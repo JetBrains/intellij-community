66a4ee70b89b49fbc533ad020036722c65aa07cc
peter <peter@jetbrains.com>
2011-09-08 18:58:02 +0200

    apply known element heuristics only to jdk elements

--- platform/lang-impl/src/com/intellij/psi/util/proximity/SdkOrLibraryWeigher.java
+++ platform/lang-impl/src/com/intellij/psi/util/proximity/SdkOrLibraryWeigher.java
@@ -15,13 +15,14 @@
  */
 package com.intellij.psi.util.proximity;
 
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.JdkOrderEntry;
 import com.intellij.openapi.roots.OrderEntry;
 import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.ProximityLocation;
-import com.intellij.psi.util.PsiUtilBase;
+import com.intellij.psi.util.PsiUtilCore;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -32,12 +33,14 @@ import java.util.List;
 public class SdkOrLibraryWeigher extends ProximityWeigher {
 
   public Comparable weigh(@NotNull final PsiElement element, @NotNull final ProximityLocation location) {
-    if (location.getProject() == null){
-      return null;
-    }
-    final VirtualFile file = PsiUtilBase.getVirtualFile(element);
+    Project project = location.getProject();
+    return project == null ? null : isJdkElement(element, project);
+  }
+
+  static boolean isJdkElement(PsiElement element, final Project project) {
+    final VirtualFile file = PsiUtilCore.getVirtualFile(element);
     if (file != null) {
-      List<OrderEntry> orderEntries = ProjectRootManager.getInstance(location.getProject()).getFileIndex().getOrderEntriesForFile(file);
+      List<OrderEntry> orderEntries = ProjectRootManager.getInstance(project).getFileIndex().getOrderEntriesForFile(file);
       if (!orderEntries.isEmpty() && orderEntries.get(0) instanceof JdkOrderEntry) {
         return true;
       }
