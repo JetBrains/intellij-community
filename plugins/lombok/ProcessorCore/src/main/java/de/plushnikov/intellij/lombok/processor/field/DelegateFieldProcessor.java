4dc4a7333f795271761cac85534eae02f8b73f03
Michail Plushnikov <mplushnikov@gmail.com>
2011-09-12 11:11:21 +0000

    Addded basic support for @Delegate
    git-svn-id: http://lombok-intellij-plugin.googlecode.com/svn/trunk@6 9796b3e4-e716-bb5d-d578-ca2f8dc3330c
    
    GitOrigin-RevId: 54b5dd4046fe68120d67c4079517dab10ffa2d5a

--- plugins/lombok/ProcessorCore/src/main/java/de/plushnikov/intellij/lombok/processor/field/DelegateFieldProcessor.java
+++ plugins/lombok/ProcessorCore/src/main/java/de/plushnikov/intellij/lombok/processor/field/DelegateFieldProcessor.java
@@ -1,12 +1,33 @@
 package de.plushnikov.intellij.lombok.processor.field;
 
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.PsiAnnotation;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiClassType;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementFactory;
 import com.intellij.psi.PsiField;
+import com.intellij.psi.PsiManager;
 import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiModifier;
+import com.intellij.psi.PsiModifierList;
+import com.intellij.psi.PsiParameter;
+import com.intellij.psi.PsiParameterList;
+import com.intellij.psi.PsiSubstitutor;
+import com.intellij.psi.PsiType;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.util.StringBuilderSpinAllocator;
+import de.plushnikov.intellij.lombok.MethodUtils;
+import de.plushnikov.intellij.lombok.psi.MyLightMethod;
 import lombok.Delegate;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -24,9 +45,99 @@ public class DelegateFieldProcessor extends AbstractLombokFieldProcessor {
 
   public <Psi extends PsiElement> boolean process(@NotNull PsiField psiField, @NotNull PsiAnnotation psiAnnotation, @NotNull List<Psi> target) {
     boolean result = false;
-    // TODO implement me
+
+    final PsiClass psiClass = psiField.getContainingClass();
+    if (null == psiClass) {
+      return result;
+    }
+
+    final Project project = psiField.getProject();
+    final PsiManager manager = psiField.getContainingFile().getManager();
+
+    final PsiType psiType = psiField.getType();
+    if (psiType instanceof PsiClassType) {
+      final PsiClassType objectType = PsiType.getJavaLangObject(manager, GlobalSearchScope.allScope(project));
+      final PsiClass objectClass = objectType.resolve();
+
+      PsiClassType psiClassType = (PsiClassType) psiType;
+      PsiClassType.ClassResolveResult resolveResult = psiClassType.resolveGenerics();
+      PsiSubstitutor fieldClassSubstitutor = resolveResult.getSubstitutor();
+
+      PsiClass fieldClass = resolveResult.getElement();
+      if (null != fieldClass && null != objectClass) {
+        final Collection<PsiMethod> methodsToDelegate = collectAllMethods(fieldClass, objectClass);
+
+        final PsiElementFactory elementFactory = JavaPsiFacade.getInstance(project).getElementFactory();
+        for (PsiMethod psiMethod : methodsToDelegate) {
+          MyLightMethod myLightMethod = generateDelegateMethod(psiClass, manager, elementFactory, psiMethod, fieldClassSubstitutor);
+          target.add((Psi) myLightMethod);
+        }
+
+        result = !methodsToDelegate.isEmpty();
+      }
+    }
     return result;
   }
 
+  private Collection<PsiMethod> collectAllMethods(@NotNull PsiClass rootClass, @NotNull PsiClass objectClass) {
+    final PsiMethod[] objectClassMethods = objectClass.getMethods();
 
+    Collection<PsiMethod> result = new ArrayList<PsiMethod>();
+    // take all methods
+    Collections.addAll(result, rootClass.getAllMethods());
+
+    final Iterator<PsiMethod> methodIterator = result.iterator();
+    while (methodIterator.hasNext()) {
+      PsiMethod psiMethod = methodIterator.next();
+
+      boolean removeMethod = psiMethod.isConstructor();
+      if (!removeMethod) {
+        final PsiModifierList modifierList = psiMethod.getModifierList();
+        removeMethod = !modifierList.hasModifierProperty(PsiModifier.PUBLIC) || modifierList.hasModifierProperty(PsiModifier.STATIC);
+      }
+      if (!removeMethod) {
+        for (PsiMethod objMethod : objectClassMethods) {
+          removeMethod = MethodUtils.methodMatches(psiMethod, objMethod.getReturnTypeNoResolve(), objMethod.getName(), objMethod.getParameterList());
+          if (removeMethod) {
+            break;
+          }
+        }
+      }
+
+      if (removeMethod) {
+        methodIterator.remove();
+      }
+    }
+
+    return result;
+  }
+
+  @NotNull
+  private MyLightMethod generateDelegateMethod(@NotNull PsiClass psiClass, @NotNull PsiManager manager, @NotNull PsiElementFactory elementFactory, @NotNull PsiMethod psiMethod, @Nullable PsiSubstitutor psiSubstitutor) {
+    final StringBuilder builder = StringBuilderSpinAllocator.alloc();
+    try {
+      builder.append(PsiModifier.PUBLIC);
+      builder.append(' ');
+      final PsiType returnType = null == psiSubstitutor ? psiMethod.getReturnType() : psiSubstitutor.substitute(psiMethod.getReturnType());
+      builder.append(null == returnType ? "" : returnType.getCanonicalText());
+      builder.append(' ');
+      builder.append(psiMethod.getName());
+      builder.append('(');
+
+      PsiParameterList parameterList = psiMethod.getParameterList();
+      if (parameterList.getParametersCount() > 0) {
+        for (PsiParameter psiParameter : parameterList.getParameters()) {
+          final PsiType psiParameterType = null == psiSubstitutor ? psiParameter.getType() : psiSubstitutor.substitute(psiParameter.getType());
+          builder.append(psiParameterType.getCanonicalText()).append(' ').append(psiParameter.getName()).append(',');
+        }
+        builder.deleteCharAt(builder.length() - 1);
+      }
+      builder.append(')');
+      builder.append("{ }");
+
+      return new MyLightMethod(manager, elementFactory.createMethodFromText(builder.toString(), psiClass), psiClass);
+    } finally {
+      StringBuilderSpinAllocator.dispose(builder);
+    }
+  }
 }
