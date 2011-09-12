e8eff77645bd7a0e70046864f34c96d704a10d47
Dmitry Jemerov <yole@jetbrains.com>
2011-09-12 17:32:36 +0200

    simplify PyBreakContinueGotoProvider by not using ParentMatcher

--- python/src/com/jetbrains/python/codeInsight/PyBreakContinueGotoProvider.java
+++ python/src/com/jetbrains/python/codeInsight/PyBreakContinueGotoProvider.java
@@ -10,9 +10,6 @@ import com.intellij.psi.util.PsiTreeUtil;
 import com.jetbrains.python.PyTokenTypes;
 import com.jetbrains.python.PythonLanguage;
 import com.jetbrains.python.psi.*;
-import com.jetbrains.python.psi.patterns.ParentMatcher;
-
-import java.util.List;
 
 /**
  * Provides reaction on ctrl+click for {@code break} and {@code continue} statements.
@@ -23,19 +20,16 @@ public class PyBreakContinueGotoProvider extends GotoDeclarationHandlerBase {
 
   public PsiElement getGotoDeclarationTarget(PsiElement source) {
     if (source != null && source.getLanguage() instanceof PythonLanguage) {
-      ParentMatcher parent_matcher = new ParentMatcher(PyForStatement.class, PyWhileStatement.class);
-      parent_matcher.limitBy(PyFunction.class, PyClass.class);
-      List<? extends PsiElement> cycle_list = parent_matcher.search(source);
-      if (cycle_list != null && cycle_list.size() == 1) {
-        PsiElement cycle_element = cycle_list.get(0);
+      final PyLoopStatement loop = PsiTreeUtil.getParentOfType(source, PyLoopStatement.class, false, PyFunction.class, PyClass.class);
+      if (loop != null) {
         ASTNode node = source.getNode();
         if (node != null) {
           IElementType node_type = node.getElementType();
           if (node_type == PyTokenTypes.CONTINUE_KEYWORD) {
-            return cycle_element;
+            return loop;
           }
           if (node_type == PyTokenTypes.BREAK_KEYWORD) {
-            PsiElement outer_element = cycle_element;
+            PsiElement outer_element = loop;
             PsiElement after_cycle;
             while (true) {
               after_cycle = outer_element.getNextSibling();
@@ -51,7 +45,7 @@ public class PyBreakContinueGotoProvider extends GotoDeclarationHandlerBase {
               }
             }
             // cycle is the last statement in the flow of execution. its last element is our best bet.
-            return PsiTreeUtil.getDeepestLast(cycle_element);
+            return PsiTreeUtil.getDeepestLast(loop);
           }
         }
       }
