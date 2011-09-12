34c9bf2a557bbf992c37b0e78e3d01910442f961
Eugene Kudelevsky <Eugene.Kudelevsky@jetbrains.com>
2011-09-12 19:45:24 +0400

    IDEA-70427 android: add 'apk' to archive file type

--- platform/platform-impl/src/com/intellij/openapi/fileTypes/impl/PlatformFileTypeFactory.java
+++ platform/platform-impl/src/com/intellij/openapi/fileTypes/impl/PlatformFileTypeFactory.java
@@ -24,7 +24,7 @@ import org.jetbrains.annotations.NotNull;
  */
 public class PlatformFileTypeFactory extends FileTypeFactory {
   public void createFileTypes(final @NotNull FileTypeConsumer consumer) {
-    consumer.consume(new ArchiveFileType(), "zip;jar;war;ear;swc;egg");
+    consumer.consume(new ArchiveFileType(), "zip;jar;war;ear;swc;egg;apk");
     consumer.consume(PlainTextFileType.INSTANCE,
                      new ExtensionFileNameMatcher("txt"),
                      new ExtensionFileNameMatcher("sh"),
