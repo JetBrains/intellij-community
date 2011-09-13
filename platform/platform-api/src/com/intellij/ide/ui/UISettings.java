33142e0ced12d0a4eb7892a3863af7a005ffb736
Maxim Shafirov <max@jetbrains.com>
2011-09-13 21:34:00 +0400

    Single row for editor tabs by default, at least for AppCode

--- platform/platform-api/src/com/intellij/ide/ui/UISettings.java
+++ platform/platform-api/src/com/intellij/ide/ui/UISettings.java
@@ -69,7 +69,7 @@ public class UISettings implements PersistentStateComponent<UISettings>, Exporta
   public boolean SHOW_NAVIGATION_BAR = true;
   public boolean ALWAYS_SHOW_WINDOW_BUTTONS = false;
   public boolean CYCLE_SCROLLING = true;
-  public boolean SCROLL_TAB_LAYOUT_IN_EDITOR = false;
+  public boolean SCROLL_TAB_LAYOUT_IN_EDITOR = PlatformUtils.isCidr();
   public boolean SHOW_CLOSE_BUTTON = true;
   public int EDITOR_TAB_PLACEMENT = 1;
   public boolean HIDE_KNOWN_EXTENSION_IN_TABS = false;
