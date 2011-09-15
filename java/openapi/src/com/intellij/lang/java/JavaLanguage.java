36da339cbe421ef826f1a0e5a2ce779decae907d
Dmitry Jemerov <yole@jetbrains.com>
2011-09-15 15:23:43 +0200

    JavaLanguage to OpenAPI; register SyntaxHighlighterFactory for Java in XML

--- java/openapi/src/com/intellij/lang/java/JavaLanguage.java
+++ java/openapi/src/com/intellij/lang/java/JavaLanguage.java
@@ -15,15 +15,7 @@
  */
 package com.intellij.lang.java;
 
-import com.intellij.ide.highlighter.JavaFileHighlighter;
 import com.intellij.lang.Language;
-import com.intellij.openapi.fileTypes.SyntaxHighlighter;
-import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory;
-import com.intellij.openapi.module.LanguageLevelUtil;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.pom.java.LanguageLevel;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * @author max
@@ -34,13 +26,6 @@ public class JavaLanguage extends Language {
 
   public JavaLanguage() {
     super("JAVA", "text/java", "application/x-java", "text/x-java");
-    SyntaxHighlighterFactory.LANGUAGE_FACTORY.addExplicitExtension(this, new SyntaxHighlighterFactory() {
-      @NotNull
-      public SyntaxHighlighter getSyntaxHighlighter(final Project project, final VirtualFile virtualFile) {
-        return new JavaFileHighlighter(
-          virtualFile != null ? LanguageLevelUtil.getLanguageLevelForFile(virtualFile) : LanguageLevel.HIGHEST);
-      }
-    });
   }
 
   public String getDisplayName() {
