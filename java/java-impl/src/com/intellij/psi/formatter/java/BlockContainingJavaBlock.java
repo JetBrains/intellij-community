8096cc48aa4e8df4aeb90c5423bbe3f8b59bd25d
Dmitry Jemerov <yole@jetbrains.com>
2011-09-16 13:10:00 +0200

    rationalize static field access

--- java/java-impl/src/com/intellij/psi/formatter/java/BlockContainingJavaBlock.java
+++ java/java-impl/src/com/intellij/psi/formatter/java/BlockContainingJavaBlock.java
@@ -18,7 +18,6 @@ package com.intellij.psi.formatter.java;
 import com.intellij.formatting.*;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.JavaTokenType;
-import com.intellij.psi.codeStyle.CodeStyleSettings;
 import com.intellij.psi.codeStyle.CommonCodeStyleSettings;
 import com.intellij.psi.formatter.FormatterUtil;
 import com.intellij.psi.impl.source.tree.ElementType;
@@ -78,7 +77,7 @@ public class BlockContainingJavaBlock extends AbstractJavaBlock{
         //
         // We want to have the comment and method as distinct blocks then in order to correctly process indentation for inner method
         // elements. See IDEA-53778 for example of situation when it is significant.
-        if (prevChild != null && myNode.getElementType() == ElementType.METHOD
+        if (prevChild != null && myNode.getElementType() == JavaElementType.METHOD
             && ElementType.JAVA_COMMENT_BIT_SET.contains(prevChild.getElementType())
             && !ElementType.JAVA_COMMENT_BIT_SET.contains(child.getElementType()))
         {
@@ -101,23 +100,23 @@ public class BlockContainingJavaBlock extends AbstractJavaBlock{
 
   private static int calcNewState(final ASTNode child, final int state) {
     if (state == BEFORE_FIRST) {
-      if (child.getElementType() == ElementType.ELSE_KEYWORD) {
+      if (child.getElementType() == JavaTokenType.ELSE_KEYWORD) {
         return AFTER_ELSE;
       }
       if (StdTokenSets.COMMENT_BIT_SET.contains(child.getElementType())) {
         return BEFORE_FIRST;
       }
-      if (child.getElementType() == ElementType.CATCH_SECTION) {
+      if (child.getElementType() == JavaElementType.CATCH_SECTION) {
         return BEFORE_FIRST;
       }
     } else if (state == BEFORE_BLOCK){
-      if (child.getElementType() == ElementType.ELSE_KEYWORD) {
+      if (child.getElementType() == JavaTokenType.ELSE_KEYWORD) {
         return AFTER_ELSE;
       }
-      if (child.getElementType() == ElementType.BLOCK_STATEMENT) {
+      if (child.getElementType() == JavaElementType.BLOCK_STATEMENT) {
         return BEFORE_FIRST;
       }
-      if (child.getElementType() == ElementType.CODE_BLOCK) {
+      if (child.getElementType() == JavaElementType.CODE_BLOCK) {
         return BEFORE_FIRST;
       }
 
@@ -126,7 +125,7 @@ public class BlockContainingJavaBlock extends AbstractJavaBlock{
   }
 
   private Indent calcIndent(final ASTNode child, final int state) {
-    if (state == AFTER_ELSE && child.getElementType() == ElementType.IF_STATEMENT) {
+    if (state == AFTER_ELSE && child.getElementType() == JavaElementType.IF_STATEMENT) {
       if (mySettings.SPECIAL_ELSE_IF_TREATMENT) {
         return Indent.getNoneIndent();
       } else {
@@ -136,9 +135,9 @@ public class BlockContainingJavaBlock extends AbstractJavaBlock{
     if (isSimpleStatement(child)){
       return createNormalIndent(1);
     }
-    if (child.getElementType() == ElementType.ELSE_KEYWORD)
+    if (child.getElementType() == JavaTokenType.ELSE_KEYWORD)
       return Indent.getNoneIndent();
-    if (state == BEFORE_FIRST || child.getElementType() == ElementType.WHILE_KEYWORD) {
+    if (state == BEFORE_FIRST || child.getElementType() == JavaTokenType.WHILE_KEYWORD) {
       return Indent.getNoneIndent();
     }
     else {
@@ -162,31 +161,31 @@ public class BlockContainingJavaBlock extends AbstractJavaBlock{
         return getCodeBlockExternalIndent();
       }
     }
-    if (state == BEFORE_BLOCK && (isSimpleStatement(child) || child.getElementType() == ElementType.BLOCK_STATEMENT)){
+    if (state == BEFORE_BLOCK && (isSimpleStatement(child) || child.getElementType() == JavaElementType.BLOCK_STATEMENT)){
       return getCodeBlockInternalIndent(0);
     }
     if (state == BEFORE_FIRST) {
       return getCodeBlockExternalIndent();
     }
-    if (child.getElementType() == ElementType.ELSE_KEYWORD)
+    if (child.getElementType() == JavaTokenType.ELSE_KEYWORD)
       return getCodeBlockExternalIndent();
 
     return Indent.getContinuationIndent(myIndentSettings.USE_RELATIVE_INDENTS);
   }
 
   private static boolean isSimpleStatement(final ASTNode child) {
-    if (child.getElementType() == ElementType.BLOCK_STATEMENT) return false;
+    if (child.getElementType() == JavaElementType.BLOCK_STATEMENT) return false;
     if (!ElementType.STATEMENT_BIT_SET.contains(child.getElementType())) return false;
     return isStatement(child, child.getTreeParent());
   }
 
   private static boolean isPartOfCodeBlock(final ASTNode child) {
     if (child == null) return false;
-    if (child.getElementType() == ElementType.BLOCK_STATEMENT) return true;
-    if (child.getElementType() == ElementType.CODE_BLOCK) return true;
+    if (child.getElementType() == JavaElementType.BLOCK_STATEMENT) return true;
+    if (child.getElementType() == JavaElementType.CODE_BLOCK) return true;
 
     if (FormatterUtil.containsWhiteSpacesOnly(child)) return isPartOfCodeBlock(child.getTreeNext());
-    if (child.getElementType() == ElementType.END_OF_LINE_COMMENT) return isPartOfCodeBlock(child.getTreeNext());
+    if (child.getElementType() == JavaTokenType.END_OF_LINE_COMMENT) return isPartOfCodeBlock(child.getTreeNext());
     return child.getElementType() == JavaDocElementType.DOC_COMMENT;
   }
 
@@ -198,7 +197,7 @@ public class BlockContainingJavaBlock extends AbstractJavaBlock{
       return new ChildAttributes(Indent.getNoneIndent(), null);
     }
 
-    if (myNode.getElementType() == ElementType.FOR_STATEMENT && mySettings.ALIGN_MULTILINE_FOR && isInsideForParens(newChildIndex)) {
+    if (myNode.getElementType() == JavaElementType.FOR_STATEMENT && mySettings.ALIGN_MULTILINE_FOR && isInsideForParens(newChildIndex)) {
       Alignment prev = getUsedAlignment(newChildIndex);
       if (prev != null) {
         return new ChildAttributes(null, prev);
@@ -248,7 +247,7 @@ public class BlockContainingJavaBlock extends AbstractJavaBlock{
       if (i >= subBlocks.size()) return false;
       final Block block = subBlocks.get(i);
       if (block instanceof LeafBlock) {
-        if (((LeafBlock)block).getTreeNode().getElementType() == ElementType.RPARENTH) return false;
+        if (((LeafBlock)block).getTreeNode().getElementType() == JavaTokenType.RPARENTH) return false;
       }
     }
     return true;
