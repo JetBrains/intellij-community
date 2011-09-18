fe7b6b039e69cf38d07a2f542426406b8e1c4f95
Michail Plushnikov <mplushnikov@gmail.com>
2011-09-18 17:35:57 +0000

    added generation of canEquals method
    git-svn-id: http://lombok-intellij-plugin.googlecode.com/svn/trunk@34 9796b3e4-e716-bb5d-d578-ca2f8dc3330c
    
    GitOrigin-RevId: c940606e62bccfeb43a5c43330c169976d83d1f1

--- plugins/lombok/ProcessorCore/src/main/java/de/plushnikov/intellij/lombok/processor/clazz/EqualsAndHashCodeProcessor.java
+++ plugins/lombok/ProcessorCore/src/main/java/de/plushnikov/intellij/lombok/processor/clazz/EqualsAndHashCodeProcessor.java
@@ -4,12 +4,15 @@ import com.intellij.openapi.project.Project;
 import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.PsiAnnotation;
 import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiClassType;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementFactory;
 import com.intellij.psi.PsiField;
 import com.intellij.psi.PsiManager;
 import com.intellij.psi.PsiMethod;
 import com.intellij.psi.PsiModifier;
+import com.intellij.psi.PsiType;
+import com.intellij.psi.search.GlobalSearchScope;
 import de.plushnikov.intellij.lombok.UserMapKeys;
 import lombok.EqualsAndHashCode;
 import org.jetbrains.annotations.NotNull;
@@ -25,6 +28,7 @@ public class EqualsAndHashCodeProcessor extends AbstractLombokClassProcessor {
   private static final String CLASS_NAME = EqualsAndHashCode.class.getName();
   public static final String EQUALS_METHOD_NAME = "equals";
   public static final String HASH_CODE_METHOD_NAME = "hashCode";
+  public static final String CAN_EQUAL_METHOD_NAME = "canEqual";
 
   public EqualsAndHashCodeProcessor() {
     super(CLASS_NAME, PsiMethod.class);
@@ -32,28 +36,53 @@ public class EqualsAndHashCodeProcessor extends AbstractLombokClassProcessor {
 
   public <Psi extends PsiElement> void process(@NotNull PsiClass psiClass, @NotNull PsiMethod[] classMethods, @NotNull PsiAnnotation psiAnnotation, @NotNull List<Psi> target) {
     if (!hasMethodByName(classMethods, EQUALS_METHOD_NAME, HASH_CODE_METHOD_NAME)) {
-      Project project = psiClass.getProject();
-      PsiManager manager = psiClass.getContainingFile().getManager();
-      PsiElementFactory elementFactory = JavaPsiFacade.getInstance(project).getElementFactory();
+      boolean shouldGenerateCanEqual = shouldGenerateCanEqual(psiClass);
 
-      PsiMethod equalsMethod = createEqualsMethod(psiClass, elementFactory);
-      target.add((Psi) prepareMethod(manager, equalsMethod, psiClass, psiAnnotation));
+      if (!shouldGenerateCanEqual || !hasMethodByName(classMethods, CAN_EQUAL_METHOD_NAME)) {
+        Project project = psiClass.getProject();
+        PsiManager manager = psiClass.getContainingFile().getManager();
+        PsiElementFactory elementFactory = JavaPsiFacade.getInstance(project).getElementFactory();
 
-      PsiMethod hashcodeMethod = createHashCodeMethod(psiClass, elementFactory);
-      target.add((Psi) prepareMethod(manager, hashcodeMethod, psiClass, psiAnnotation));
+        PsiMethod equalsMethod = createEqualsMethod(psiClass, elementFactory);
+        target.add((Psi) prepareMethod(manager, equalsMethod, psiClass, psiAnnotation));
 
-      Collection<PsiField> equalsAndHashCodeFields = filterFieldsByModifiers(psiClass.getFields(), PsiModifier.STATIC, PsiModifier.TRANSIENT);
-      UserMapKeys.addReadUsageFor(equalsAndHashCodeFields);
+        PsiMethod hashcodeMethod = createHashCodeMethod(psiClass, elementFactory);
+        target.add((Psi) prepareMethod(manager, hashcodeMethod, psiClass, psiAnnotation));
+
+        if (shouldGenerateCanEqual) {
+          PsiMethod canEqualsMethod = createCanEqualMethod(psiClass, elementFactory);
+          target.add((Psi) prepareMethod(manager, canEqualsMethod, psiClass, psiAnnotation));
+        }
+
+        Collection<PsiField> equalsAndHashCodeFields = filterFieldsByModifiers(psiClass.getFields(), PsiModifier.STATIC, PsiModifier.TRANSIENT);
+        UserMapKeys.addReadUsageFor(equalsAndHashCodeFields);
+      }
     } else {
       //TODO create warning in code
       //Not generating methodName(): A method with that name already exists
     }
   }
 
+  private boolean shouldGenerateCanEqual(@NotNull PsiClass psiClass) {
+    boolean result = true;
+    final PsiClass superClass = psiClass.getSuperClass();
+    if (null == superClass && psiClass.hasModifierProperty(PsiModifier.FINAL)) {
+      result = false;
+    }
+    if (null != superClass && psiClass.hasModifierProperty(PsiModifier.FINAL)) {
+      final Project project = psiClass.getProject();
+      final PsiManager manager = psiClass.getContainingFile().getManager();
+      final PsiClassType javaLangObject = PsiType.getJavaLangObject(manager, GlobalSearchScope.projectScope(project));
+
+      result = !superClass.equals(javaLangObject.resolve());
+    }
+    return result;
+  }
+
   @NotNull
   private PsiMethod createEqualsMethod(@NotNull PsiClass psiClass, @NotNull PsiElementFactory elementFactory) {
     return elementFactory.createMethodFromText(
-        "@java.lang.Override public boolean " + EQUALS_METHOD_NAME + "(Object o) { return super.equals(o); }",
+        "@java.lang.Override public boolean " + EQUALS_METHOD_NAME + "(final java.lang.Object other) { return super.equals(other); }",
         psiClass);
   }
 
@@ -64,4 +93,10 @@ public class EqualsAndHashCodeProcessor extends AbstractLombokClassProcessor {
         psiClass);
   }
 
+  @NotNull
+  private PsiMethod createCanEqualMethod(@NotNull PsiClass psiClass, @NotNull PsiElementFactory elementFactory) {
+    return elementFactory.createMethodFromText(
+        "public boolean " + CAN_EQUAL_METHOD_NAME + "(final java.lang.Object other) { return other instanceof " + psiClass.getName() + "; }",
+        psiClass);
+  }
 }
