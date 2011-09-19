417e8cb897fa9f9f6aa05dca883a8fa2ae1fc58a
Kirill Likhodedov <Kirill.Likhodedov@gmail.com>
2011-09-19 14:51:22 +0400

    Git compare branches: fix swapped descriptions

--- plugins/git4idea/src/git4idea/ui/branch/GitCompareBranchesDialog.java
+++ plugins/git4idea/src/git4idea/ui/branch/GitCompareBranchesDialog.java
@@ -114,7 +114,7 @@ public class GitCompareBranchesDialog extends DialogWrapper {
     String firstBranch = forward ? currentBranch : myBranchName;
     String secondBranch = forward ? myBranchName : currentBranch;
     return String.format("<html>Commits that exist in <code><b>%s</b></code> but don't exist in <code><b>%s</b></code> (<code>git log %s..%s</code>):</html>",
-                         firstBranch, secondBranch, firstBranch, secondBranch);
+                         secondBranch, firstBranch, firstBranch, secondBranch);
   }
 
   /**
