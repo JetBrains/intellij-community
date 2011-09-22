1fd83a071620aa779b0be19e8f07d08e6499a36c
Kirill Likhodedov <Kirill.Likhodedov@gmail.com>
2011-09-22 15:45:35 +0400

    Git: Compare With Branch action rewritten
    1. Use GitRepository framework
    2. Disable for fresh repository (IDEA-74495) and when there are no branches to compare with.
    3. Don't show current branch in the popup.
    4. Query content in a Task.Backgroundable instead of invokeLater.

--- plugins/git4idea/src/git4idea/actions/GitCompareWithBranchAction.java
+++ plugins/git4idea/src/git4idea/actions/GitCompareWithBranchAction.java
@@ -19,15 +19,14 @@ import com.intellij.notification.NotificationType;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.actionSystem.Presentation;
-import com.intellij.openapi.application.Application;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.openapi.vcs.FilePath;
 import com.intellij.openapi.vcs.FilePathImpl;
-import com.intellij.openapi.vcs.ProjectLevelVcsManager;
 import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vcs.history.CurrentRevision;
 import com.intellij.openapi.vcs.history.VcsFileRevision;
@@ -35,22 +34,23 @@ import com.intellij.openapi.vcs.history.VcsHistoryUtil;
 import com.intellij.openapi.vcs.history.VcsRevisionNumber;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.components.JBList;
-import com.intellij.util.NotNullFunction;
-import com.intellij.vcsUtil.VcsUtil;
 import git4idea.GitBranch;
 import git4idea.GitFileRevision;
 import git4idea.GitRevisionNumber;
-import git4idea.GitVcs;
 import git4idea.history.GitHistoryUtils;
+import git4idea.repo.GitRepository;
+import git4idea.repo.GitRepositoryManager;
+import git4idea.ui.GitUIUtil;
 import org.jetbrains.annotations.NotNull;
 
-import javax.swing.*;
-import java.awt.font.TextAttribute;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
- * Compares selected file with itself in one of other branches.
+ * Compares selected file with itself in another branch.
  * @author Kirill Likhodedov
  */
 public class GitCompareWithBranchAction extends DumbAwareAction {
@@ -59,111 +59,60 @@ public class GitCompareWithBranchAction extends DumbAwareAction {
 
   @Override
   public void actionPerformed(final AnActionEvent event) {
-    // get basic information
-    final Project project = event.getData(PlatformDataKeys.PROJECT);
-    if (project == null || project.isDisposed()) {
-      notifyError(project, "Project is null. " + event.getPlace() + ", " + event.getDataContext(), null);
-      return;
-    }
-    final VirtualFile[] vFiles = event.getData(PlatformDataKeys.VIRTUAL_FILE_ARRAY);
-    if (vFiles == null || vFiles.length != 1 || vFiles[0] == null) {
-      notifyError(project, "Selected incorrect virtual files array: " + Arrays.toString(vFiles), null);
-      return;
-    }
-    final VirtualFile file = vFiles[0];
-    final VirtualFile vcsRoot = VcsUtil.getVcsRootFor(project, file);
-    if (vcsRoot == null) {
-      notifyError(project, "The file " + file + " is not under Git version control.", null);
-      return;
-    }
+    final Project project = event.getProject();
+    assert project != null;
 
-    // get branches information
-    final List<GitBranch> branches = new ArrayList<GitBranch>();
-    GitBranch curBranch = null;
-    try {
-      curBranch = GitBranch.list(project, vcsRoot, true, true, branches, null);
-    } catch (VcsException e) {
-      notifyError(project, "Couldn't get information about current branch", e);
-    }
-    if (curBranch == null) {
-      notifyError(project, "Current branch is null.", null);
-      return;
-    }
-    final String currentBranch = curBranch.getName();
+    final VirtualFile file = getAffectedFile(event);
+
+    GitRepository repository = GitRepositoryManager.getInstance(project).getRepositoryForFile(file);
+    assert repository != null;
+
+    final String head = repository.getCurrentRevision();
+    final List<String> branchNames = getBranchNamesExceptCurrent(repository);
 
     // prepare and invoke popup
-    final JBList list = new JBList(branches);
-    list.installCellRenderer(new NotNullFunction<GitBranch, JComponent>() { // display current branch in bold with asterisk
-      @NotNull public JComponent fun(GitBranch branch) {
-        if (branch.isActive()) {
-          JLabel label = new JLabel(branch.getName() + " *");
-          final Map<TextAttribute, Float> attributes = new HashMap<TextAttribute, Float>(1);
-          attributes.put(TextAttribute.WEIGHT, TextAttribute.WEIGHT_BOLD);
-          label.setFont(label.getFont().deriveFont(attributes));
-          return label;
-        }
-        return new JLabel(branch.getName());
-      }
-    });
+    final JBList list = new JBList(branchNames);
 
     JBPopupFactory.getInstance()
       .createListPopupBuilder(list)
       .setTitle("Select branch to compare")
-      .setItemChoosenCallback(
-        new Runnable() {
-          public void run() {
-            Application app = ApplicationManager.getApplication();
-            if (project.isDisposed() || app == null || !app.isActive() || app.isDisposed() || app.isDisposeInProgress()) { // safe check
-              return;
-            }
-            ApplicationManager.getApplication()
-              .invokeLater(new Runnable() { // don't block awt thread - getting revision content may take long
-
-                @Override
-                public void run() {
-                  try {
-                    showDiffWithBranch(project, file, currentBranch, list.getSelectedValue().toString());
-                  }
-                  catch (Exception e) {
-                    notifyError(project, "Couldn't compare file [" + file + "] with selected branch [" + list.getSelectedValue() + "]", e);
-                  }
-                }
-              });
-          }
-        })
+      .setItemChoosenCallback(new OnBranchChooseRunnable(project, file, head, list))
       .setAutoselectOnMouseMove(true)
       .createPopup()
       .showInBestPositionFor(event.getDataContext());
   }
 
-  private static void notifyError(Project project, String message, Throwable t) {
-    if (t != null) {
-      LOG.info(message, t);
-    } else {
-      LOG.info(message);
+  private static List<String> getBranchNamesExceptCurrent(GitRepository repository) {
+    List<GitBranch> localBranches = new ArrayList<GitBranch>(repository.getBranches().getLocalBranches());
+    Collections.sort(localBranches);
+    List<GitBranch> remoteBranches = new ArrayList<GitBranch>(repository.getBranches().getRemoteBranches());
+    Collections.sort(remoteBranches);
+    
+    if (repository.isOnBranch()) {
+      localBranches.remove(repository.getCurrentBranch());
     }
-    GitVcs.IMPORTANT_ERROR_NOTIFICATION.createNotification("Couldn't compare with branch", message, NotificationType.WARNING, null).notify(project);
+    
+    final List<String> branchNames = new ArrayList<String>();
+    for (GitBranch branch : localBranches) {
+      branchNames.add(branch.getName());
+    }
+    for (GitBranch branch : remoteBranches) {
+      branchNames.add(branch.getName());
+    }
+    return branchNames;
   }
 
-  private static void showDiffWithBranch(Project project, VirtualFile file, String currentBranch, String compareBranch) throws VcsException, IOException {
-    final FilePath filePath = new FilePathImpl(file);
-    final VcsRevisionNumber currentRevisionNumber = GitHistoryUtils.getCurrentRevision(project, filePath, currentBranch);
-    final VcsRevisionNumber compareRevisionNumber = GitHistoryUtils.getCurrentRevision(project, filePath, compareBranch);
-    if (compareRevisionNumber == null) {
-      GitVcs.IMPORTANT_ERROR_NOTIFICATION.createNotification("File doesn't exist in branch", "File " + file.getPresentableUrl() + " doesn't exist in branch [" + compareBranch + "]", NotificationType.INFORMATION, null).notify(project);
-      return;
-    }
-    final VcsFileRevision compareRevision = new GitFileRevision(project, filePath, (GitRevisionNumber)compareRevisionNumber);
-    final String currentTitle = currentRevisionNumber != null ? ((GitRevisionNumber)currentRevisionNumber).getShortRev() + " on " + currentBranch : "Local changes on " + currentBranch;
-    final String compareTitle = ((GitRevisionNumber)compareRevisionNumber).getShortRev() + " on " + compareBranch;
-    VcsHistoryUtil.showDiff(project, filePath, new CurrentRevision(file, currentRevisionNumber), compareRevision, currentTitle, compareTitle);
+  private static VirtualFile getAffectedFile(AnActionEvent event) {
+    final VirtualFile[] vFiles = event.getData(PlatformDataKeys.VIRTUAL_FILE_ARRAY);
+    assert vFiles != null && vFiles.length == 1 && vFiles[0] != null : "Illegal virtual files selected: " + Arrays.toString(vFiles);
+    return vFiles[0];
   }
 
   @Override
   public void update(AnActionEvent e) {
     super.update(e);
     Presentation presentation = e.getPresentation();
-    Project project = e.getData(PlatformDataKeys.PROJECT);
+    Project project = e.getProject();
     if (project == null) {
       presentation.setEnabled(false);
       presentation.setVisible(false);
@@ -171,17 +120,87 @@ public class GitCompareWithBranchAction extends DumbAwareAction {
     }
 
     VirtualFile[] vFiles = e.getData(PlatformDataKeys.VIRTUAL_FILE_ARRAY);
-    if (vFiles == null || vFiles.length == 0 || vFiles.length > 1) { // only 1 file for now
+    if (vFiles == null || vFiles.length != 1 || vFiles[0] == null || vFiles[0].isDirectory()) { // only 1 file for now, not for dirs
       presentation.setEnabled(false);
       presentation.setVisible(true);
       return;
     }
-    GitVcs vcs = GitVcs.getInstance(project);
-    boolean enabled = ProjectLevelVcsManager.getInstance(project).checkAllFilesAreUnder(vcs, vFiles);
-    enabled &= vFiles[0] != null && !vFiles[0].isDirectory(); // not for dirs for now
 
-    presentation.setEnabled(enabled);
-    presentation.setVisible(enabled);
+    GitRepository repository = GitRepositoryManager.getInstance(project).getRepositoryForFile(vFiles[0]);
+    if (repository == null || repository.isFresh() || noBranchesToCompare(repository)) {
+      presentation.setEnabled(false);
+      presentation.setVisible(true);
+    }
+  }
+
+  private static boolean noBranchesToCompare(@NotNull GitRepository repository) {
+    int locals = repository.getBranches().getLocalBranches().size();
+    boolean haveRemotes = !repository.getBranches().getRemoteBranches().isEmpty();
+    if (repository.isOnBranch()) {  // there are other branches to compare
+      return locals < 2 && !haveRemotes;
+    }
+    return locals == 0 && !haveRemotes; // there are at least 1 branch to compare
+  }
+
+  private static class OnBranchChooseRunnable implements Runnable {
+    private final Project myProject;
+    private final VirtualFile myFile;
+    private final String myHead;
+    private final JBList myList;
+
+    public OnBranchChooseRunnable(Project project, VirtualFile file, String head, JBList list) {
+      myProject = project;
+      myFile = file;
+      myHead = head;
+      myList = list;
+    }
+
+    @Override
+    public void run() {
+      new Task.Backgroundable(myProject, "Comparing...") {
+        @Override public void run(@NotNull ProgressIndicator indicator) {
+          String branchToCompare = myList.getSelectedValue().toString();
+          try {
+            showDiffWithBranch(myProject, myFile, myHead, branchToCompare);
+          }
+          catch (VcsException e) {
+            if (e.getMessage().contains("exists on disk, but not in")) {
+              fileDoesntExistInBranchError(myProject, myFile, branchToCompare);
+            } else {
+              GitUIUtil.notifyError(myProject, "Couldn't compare with branch",
+                                    "Couldn't compare file [" + myFile + "] with selected branch [" + myList.getSelectedValue() + "]",
+                                    false, e);
+            }
+          }
+        }
+      }.queue();
+    }
+
+    private static void showDiffWithBranch(@NotNull Project project, @NotNull VirtualFile file, @NotNull String head, @NotNull String branchToCompare) throws VcsException {
+      final FilePath filePath = new FilePathImpl(file);
+      final VcsRevisionNumber currentRevisionNumber = GitHistoryUtils.getCurrentRevision(project, filePath, head);
+      final VcsRevisionNumber compareRevisionNumber = GitHistoryUtils.getCurrentRevision(project, filePath, branchToCompare);
+      if (compareRevisionNumber == null) {
+        fileDoesntExistInBranchError(project, file, branchToCompare);
+        return;
+      }
+      final VcsFileRevision compareRevision = new GitFileRevision(project, filePath, (GitRevisionNumber)compareRevisionNumber);
+      final String currentTitle = currentRevisionNumber != null ? ((GitRevisionNumber)currentRevisionNumber).getShortRev() + " on " + head : "Local changes on " + head;
+      final String compareTitle = ((GitRevisionNumber)compareRevisionNumber).getShortRev() + " on " + branchToCompare;
+
+      try {
+        VcsHistoryUtil.showDiff(project, filePath, new CurrentRevision(file, currentRevisionNumber), compareRevision, currentTitle, compareTitle);
+      }
+      catch (IOException e) {
+        throw new VcsException(String.format("Couldn't show diff for file [%s], head [%s] and branch [%s]", file.getPresentableUrl(), head, branchToCompare), e);
+      }
+    }
+
+    private static void fileDoesntExistInBranchError(Project project, VirtualFile file, String branchToCompare) {
+      GitUIUtil.notifyMessage(project, "File doesn't exist in branch",
+                              String.format("File <code>%s</code> doesn't exist in branch <code>%s</code>", file.getPresentableUrl(), branchToCompare),
+                              NotificationType.WARNING, true, null);
+    }
   }
 
 }
