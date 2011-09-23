8fb263884aebfbd5e0c4dac3d8c2ea1e650d5f5d
peter <peter@jetbrains.com>
2011-09-23 19:59:34 +0200

    IDEA-68684 Extra parenthesis and semicolon in inserted by smart completion of array type initializer in some cases

---
+++ java/java-tests/testData/codeInsight/completion/smartType/ArrayInitializerBeforeVarargs.java
@@ -0,0 +1,5 @@
+class Super {
+  void foo(String[] params, int... indices) {
+      foo(new <caret>, 0);
+    }
+}
--- java/java-tests/testData/codeInsight/completion/smartType/NoCommaBeforeVarargs-out.java
+++ java/java-tests/testData/codeInsight/completion/smartType/NoCommaBeforeVarargs-out.java
@@ -2,7 +2,7 @@ class Foooo {
     int bar(int a, Object... varargs) {}
 
     int foo() {
-        bar(hashCode())<caret>;
+        bar(hashCode()<caret>);
     }
 
 }
