e2b072311fac37b9f7dd403cdadf51c4a2273d11
Michail Plushnikov <mplushnikov@gmail.com>
2011-09-23 19:19:49 +0000

    added support for suppressConstructorProperties and staticName properties on @Constructor
    git-svn-id: http://lombok-intellij-plugin.googlecode.com/svn/trunk@69 9796b3e4-e716-bb5d-d578-ca2f8dc3330c
    
    GitOrigin-RevId: 22c5d7749c622d4bd0d8936430a536d9d4e7717f

--- plugins/lombok/processor-core/src/main/java/de/plushnikov/intellij/lombok/processor/clazz/constructor/AbstractConstructorClassProcessor.java
+++ plugins/lombok/processor-core/src/main/java/de/plushnikov/intellij/lombok/processor/clazz/constructor/AbstractConstructorClassProcessor.java
@@ -1,5 +1,6 @@
 package de.plushnikov.intellij.lombok.processor.clazz.constructor;
 
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiAnnotation;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiField;
@@ -7,14 +8,17 @@ import com.intellij.psi.PsiMethod;
 import com.intellij.psi.PsiModifier;
 import com.intellij.psi.PsiModifierList;
 import com.intellij.util.StringBuilderSpinAllocator;
+import de.plushnikov.intellij.lombok.LombokConstants;
 import de.plushnikov.intellij.lombok.UserMapKeys;
 import de.plushnikov.intellij.lombok.problem.ProblemBuilder;
 import de.plushnikov.intellij.lombok.processor.LombokProcessorUtil;
 import de.plushnikov.intellij.lombok.processor.clazz.AbstractLombokClassProcessor;
+import de.plushnikov.intellij.lombok.util.PsiAnnotationUtil;
 import de.plushnikov.intellij.lombok.util.PsiMethodUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -38,7 +42,7 @@ public abstract class AbstractConstructorClassProcessor extends AbstractLombokCl
     if (!validateVisibility(psiAnnotation)) {
       result = false;
     }
-    //TODO implement me
+    //TODO add validation for construction name already exist
     return result;
   }
 
@@ -61,7 +65,7 @@ public abstract class AbstractConstructorClassProcessor extends AbstractLombokCl
     Collection<PsiField> allNotInitializedNotStaticFields = new ArrayList<PsiField>();
     for (PsiField psiField : psiClass.getFields()) {
       // skip fields named $
-      boolean addField = !psiField.getName().startsWith("$");
+      boolean addField = !psiField.getName().startsWith(LombokConstants.LOMBOK_INTERN_FIELD_MARKER);
 
       final PsiModifierList modifierList = psiField.getModifierList();
       if (null != modifierList) {
@@ -78,36 +82,103 @@ public abstract class AbstractConstructorClassProcessor extends AbstractLombokCl
     return allNotInitializedNotStaticFields;
   }
 
-
   @NotNull
   protected Collection<PsiMethod> createConstructorMethod(@NotNull PsiClass psiClass, @NotNull String methodVisibility, @NotNull PsiAnnotation psiAnnotation, @NotNull Collection<PsiField> params) {
     //TODO add check if constructor already exists
 
+    final String suppressConstructorProperties = PsiAnnotationUtil.getAnnotationValue(psiAnnotation, "suppressConstructorProperties");
+
+    final String staticName = PsiAnnotationUtil.getAnnotationValue(psiAnnotation, "staticName");
+    final boolean staticConstrRequired = !StringUtil.isEmptyOrSpaces(staticName);
+
+    final String constrVisibility = staticConstrRequired || psiClass.isEnum() ? PsiModifier.PRIVATE : methodVisibility;
+
+    PsiMethod constructor = createConstructor(psiClass, constrVisibility, Boolean.valueOf(suppressConstructorProperties), params, psiAnnotation);
+    if (staticConstrRequired) {
+      PsiMethod staticConstructor = createStaticConstructor(psiClass, staticName, params, psiAnnotation);
+      return Arrays.asList(constructor, staticConstructor);
+    }
+    return Collections.singletonList(constructor);
+  }
+
+  private PsiMethod createConstructor(@NotNull PsiClass psiClass, @NotNull String methodVisibility, boolean suppressConstructorProperties, @NotNull Collection<PsiField> params, @NotNull PsiAnnotation psiAnnotation) {
     final StringBuilder builder = StringBuilderSpinAllocator.alloc();
     try {
-      builder.append(psiClass.isEnum() ? PsiModifier.PRIVATE : methodVisibility);
-      if (builder.length() > 0) {
-        builder.append(' ');
-      }
-      //TODO append @java.beans.ConstructorProperties({"x"}) annotation
-      builder.append(psiClass.getName());
-      builder.append('(');
-      if (!params.isEmpty()) {
+      if (!suppressConstructorProperties && !params.isEmpty()) {
+        builder.append("@java.beans.ConstructorProperties( {");
         for (PsiField param : params) {
-          builder.append(param.getType().getCanonicalText()).append(' ').append(param.getName()).append(',');
+          builder.append('"').append(param.getName()).append('"').append(',');
         }
         builder.deleteCharAt(builder.length() - 1);
+        builder.append("} ) ");
       }
-      builder.append(')');
+
+      builder.append(methodVisibility);
+      if (StringUtil.isNotEmpty(methodVisibility)) {
+        builder.append(' ');
+      }
+
+      builder.append(psiClass.getName());
+      appendParamDeclaration(params, builder);
       builder.append("{ super();}");
 
       for (PsiField psiField : params) {
         UserMapKeys.addWriteUsageFor(psiField);
       }
 
-      return Collections.singletonList(PsiMethodUtil.createMethod(psiClass, builder.toString(), psiAnnotation));
+      return PsiMethodUtil.createMethod(psiClass, builder.toString(), psiAnnotation);
     } finally {
       StringBuilderSpinAllocator.dispose(builder);
     }
+  }
+
+  private PsiMethod createStaticConstructor(PsiClass psiClass, String staticName, Collection<PsiField> params, PsiAnnotation psiAnnotation) {
+    final StringBuilder builder = StringBuilderSpinAllocator.alloc();
+    try {
+      builder.append(PsiModifier.PUBLIC);
+      builder.append(' ');
+      builder.append(PsiModifier.STATIC);
+      builder.append(' ');
+
+      builder.append(psiClass.getName());
+      builder.append(' ');
+
+      builder.append(staticName);
+      appendParamDeclaration(params, builder);
+      builder.append("{ ").
+          append("return new ").append(psiClass.getName()).
+          append("(");
+      appendParamList(params, builder).
+          append(")").
+          append(";}");
+
+      return PsiMethodUtil.createMethod(psiClass, builder.toString(), psiAnnotation);
+    } finally {
+      StringBuilderSpinAllocator.dispose(builder);
+    }
+  }
+
+  private StringBuilder appendParamDeclaration(Collection<PsiField> params, StringBuilder builder) {
+    builder.append('(');
+    if (!params.isEmpty()) {
+      for (PsiField param : params) {
+        builder.append(param.getType().getCanonicalText()).append(' ').append(param.getName()).append(',');
+      }
+      builder.deleteCharAt(builder.length() - 1);
+    }
+    builder.append(')');
+    return builder;
+  }
+
+  private StringBuilder appendParamList(Collection<PsiField> params, StringBuilder builder) {
+    builder.append('(');
+    if (!params.isEmpty()) {
+      for (PsiField param : params) {
+        builder.append(param.getName()).append(',');
+      }
+      builder.deleteCharAt(builder.length() - 1);
+    }
+    builder.append(')');
+    return builder;
   }
 }
