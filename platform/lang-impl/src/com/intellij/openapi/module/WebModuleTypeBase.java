c6d43c2d3c61b3de342aa57bc43b9195902b0e2c
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2011-09-24 02:06:40 +0200

    Correct icon for web module

--- platform/lang-impl/src/com/intellij/openapi/module/WebModuleTypeBase.java
+++ platform/lang-impl/src/com/intellij/openapi/module/WebModuleTypeBase.java
@@ -1,9 +1,21 @@
+/*
+ * Copyright 2000-2011 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.intellij.openapi.module;
 
 import com.intellij.ide.util.projectWizard.ModuleBuilder;
-import com.intellij.openapi.application.ApplicationInfo;
-import com.intellij.openapi.application.ex.ApplicationInfoEx;
-import com.intellij.openapi.module.ModuleType;
 import com.intellij.openapi.project.ProjectBundle;
 import com.intellij.openapi.util.IconLoader;
 import org.jetbrains.annotations.NonNls;
@@ -15,6 +27,8 @@ import javax.swing.*;
  */
 public abstract class WebModuleTypeBase<T extends ModuleBuilder> extends ModuleType<T> {
   @NonNls public static final String WEB_MODULE = "WEB_MODULE";
+
+  private static final Icon WEB_MODULE_ICON = IconLoader.getIcon("/javaee/webModuleBig.png");
 
   public WebModuleTypeBase() {
     super(WEB_MODULE);
@@ -29,7 +43,7 @@ public abstract class WebModuleTypeBase<T extends ModuleBuilder> extends ModuleT
   }
 
   public Icon getBigIcon() {
-    return IconLoader.getIcon(((ApplicationInfoEx)ApplicationInfo.getInstance()).getSmallIconUrl());
+    return WEB_MODULE_ICON;
   }
 
   public Icon getNodeIcon(boolean isOpened) {
