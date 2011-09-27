cc1eb87b27460828e7df64761ba9ff5a80d2d101
Dmitry Jemerov <yole@jetbrains.com>
2011-09-27 20:36:29 +0200

    move stuff for selecting a template language from Mako to common templateLanguages package; separate configurable for template language

--- python/src/com/jetbrains/python/configuration/PyIntegratedToolsConfigurable.java
+++ python/src/com/jetbrains/python/configuration/PyIntegratedToolsConfigurable.java
@@ -22,8 +22,8 @@ import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.CollectionComboBoxModel;
 import com.intellij.util.FileContentUtil;
 import com.jetbrains.django.lang.template.DjangoTemplateFileType;
-import com.jetbrains.mako.TemplatesConfigurationsModel;
-import com.jetbrains.mako.TemplatesService;
+import com.jetbrains.python.templateLanguages.TemplatesConfigurationsModel;
+import com.jetbrains.python.templateLanguages.TemplatesService;
 import com.jetbrains.python.documentation.DocStringFormat;
 import com.jetbrains.python.documentation.PyDocumentationSettings;
 import com.jetbrains.python.testing.PythonTestConfigurationsModel;
@@ -43,12 +43,10 @@ public class PyIntegratedToolsConfigurable implements Configurable, NonDefaultPr
   private JComboBox myTestRunnerComboBox;
   private JComboBox myDocstringFormatComboBox;
   private PythonTestConfigurationsModel myModel;
-  private TemplatesConfigurationsModel myTemplatesModel;
   private Project myProject;
   private final PyDocumentationSettings myDocumentationSettings;
   private TextFieldWithBrowseButton myWorkDir;
   private JCheckBox txtIsRst;
-  private JComboBox myTemplateLanguage;
 
   public PyIntegratedToolsConfigurable(Project project) {
     myProject = project;
@@ -84,24 +82,17 @@ public class PyIntegratedToolsConfigurable implements Configurable, NonDefaultPr
     myModel = new PythonTestConfigurationsModel(configurations, TestRunnerService.getInstance(myProject).getProjectConfiguration(),
                                                 myProject);
 
-    List<String> templateConfigurations = TemplatesService.getInstance(myProject).getConfigurations();
-    myTemplatesModel = new TemplatesConfigurationsModel(templateConfigurations, TemplatesService.getInstance(myProject).getProjectConfiguration(),
-                                                myProject);
     updateConfigurations();
     return myMainPanel;
   }
 
   private void updateConfigurations() {
     myTestRunnerComboBox.setModel(myModel);
-    myTemplateLanguage.setModel(myTemplatesModel);
   }
 
   @Override
   public boolean isModified() {
     if (myTestRunnerComboBox.getSelectedItem() != myModel.getProjectConfiguration()) {
-      return true;
-    }
-    if (myTemplateLanguage.getSelectedItem() != myTemplatesModel.getProjectConfiguration()) {
       return true;
     }
     if (!Comparing.equal(myDocstringFormatComboBox.getSelectedItem(), myDocumentationSettings.myDocStringFormat)) {
@@ -115,60 +106,19 @@ public class PyIntegratedToolsConfigurable implements Configurable, NonDefaultPr
     return false;
   }
 
-  public static void reparseTemplateFiles(final @NotNull Project project) {
-    final List<VirtualFile> templatesToReparse = Lists.newArrayList();
-    ProjectRootManager.getInstance(project).getFileIndex().iterateContent(new ContentIterator() {
-      @Override
-      public boolean processFile(VirtualFile fileOrDir) {
-        if (!fileOrDir.isDirectory() && DjangoTemplateFileType.getPossibleExtensions().contains(fileOrDir.getExtension())) {
-          templatesToReparse.add(fileOrDir);
-        }
-        return true;
-      }
-    });
-    FileContentUtil.reparseFiles(project, templatesToReparse, true);
-
-    ApplicationManager.getApplication().runWriteAction(new Runnable() {
-      @Override
-      public void run() {
-
-        for (Editor editor : EditorFactoryImpl.getInstance().getAllEditors()) {
-          if (editor instanceof EditorEx) {
-            final VirtualFile vFile = ((EditorEx)editor).getVirtualFile();
-            if (vFile != null) {
-              final EditorHighlighter highlighter = EditorHighlighterFactory.getInstance().createEditorHighlighter(project, vFile);
-              ((EditorEx)editor).setHighlighter(highlighter);
-            }
-          }
-        }
-      }
-    });
-
-    DaemonCodeAnalyzer.getInstance(project).restart();
-  }
-
-
   @Override
   public void apply() throws ConfigurationException {
     myModel.apply();
-    boolean needReparse = false;
-    if (myTemplateLanguage.getSelectedItem() != myTemplatesModel.getProjectConfiguration())
-      needReparse = true;
-    myTemplatesModel.apply();
     myDocumentationSettings.myDocStringFormat = (String) myDocstringFormatComboBox.getSelectedItem();
     ReSTService.getInstance(myProject).setWorkdir(myWorkDir.getText());
     ReSTService.getInstance(myProject).setTxtIsRst(txtIsRst.isSelected());
-    if (needReparse)
-      reparseTemplateFiles(myProject);
   }
 
   @Override
   public void reset() {
     myTestRunnerComboBox.setSelectedItem(myModel.getProjectConfiguration());
-    myTemplateLanguage.setSelectedItem(myTemplatesModel.getProjectConfiguration());
     myTestRunnerComboBox.repaint();
     myModel.reset();
-    myTemplatesModel.reset();
     myDocstringFormatComboBox.setSelectedItem(myDocumentationSettings.myDocStringFormat);
     myWorkDir.setText(ReSTService.getInstance(myProject).getWorkdir());
     txtIsRst.setSelected(ReSTService.getInstance(myProject).txtIsRst());
