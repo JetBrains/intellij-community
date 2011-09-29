87e9b080755f3c2cb8c7ca6dc3aa6cb8980dda5c
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2011-09-29 11:37:22 +0400

    Python Coverage: report generation, covered line highlighting style toggle, handling of multi-line covered statements and whitespaces between covered statements.

--- python/helpers/coverage.py
+++ python/helpers/coverage.py
@@ -4,10 +4,13 @@ import os
 import sys
 
 coverage_file = os.getenv('PYCHARM_COVERAGE_FILE')
+run_cov = os.getenv('PYCHARM_RUN_COVERAGE')
 os.environ['COVERAGE_FILE'] = coverage_file
-with open(coverage_file + '.workdir.txt', mode='w') as a_file:
-    a_file.write(os.getcwd())
+if run_cov:
+    with open(coverage_file + '.workdir.txt', mode='w') as a_file:
+        a_file.write(os.getcwd())
 main()
-main(["xml", "-o", coverage_file + ".xml"])
+if run_cov:
+    main(["xml", "-o", coverage_file + ".xml"])
 
 
