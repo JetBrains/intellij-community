732bf97b16680802da21678a88516c638254a948
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2011-09-29 18:23:04 +0400

    Disable coverage for Django server configuration.

--- python/helpers/coverage.py
+++ python/helpers/coverage.py
@@ -15,7 +15,8 @@ else:
 
 coverage_file = os.getenv('PYCHARM_COVERAGE_FILE')
 run_cov = os.getenv('PYCHARM_RUN_COVERAGE')
-os.environ['COVERAGE_FILE'] = coverage_file
+if coverage_file:
+    os.environ['COVERAGE_FILE'] = coverage_file
 if run_cov:
     with open(coverage_file + '.workdir.txt', mode='w') as a_file:
         a_file.write(os.getcwd())
