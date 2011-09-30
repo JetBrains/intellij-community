296943941eea34fd4e8f313de6e841d172d4ff32
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2011-09-30 15:47:00 +0400

    fixed PY-4556 Quickfix to add missing "self." should also work for functions

--- python/src/com/jetbrains/python/inspections/PyUnresolvedReferencesInspection.java
+++ python/src/com/jetbrains/python/inspections/PyUnresolvedReferencesInspection.java
@@ -343,8 +343,7 @@ public class PyUnresolvedReferencesInspection extends PyInspection {
               }
             }
             for (PyFunction method : containedClass.getMethods()) {
-              Property property = method.getProperty();
-              if (property != null && method.getName().equals(refex.getText())) {
+              if (method.getName().equals(refex.getText())) {
                 actions.add(new UnresolvedReferenceAddSelfQuickFix(refex));
               }
             }
---
+++ python/testData/inspections/AddSelfFunction_after.py
@@ -0,0 +1,5 @@
+class A:
+  def get_a(self):
+      pass
+  def foo():
+    self.get_a()
