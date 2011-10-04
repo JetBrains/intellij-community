eb4138efef795bf98d00c99333a30d411498856f
nik <Nikolay.Chashnikov@jetbrains.com>
2011-10-04 14:09:52 +0400

    missing testData files added

---
+++ jps/testData/overlappingSourceRoots/src/inner/src/y/Class2.java
@@ -0,0 +1,11 @@
+package y;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nik
+ * Date: 10/4/11
+ * Time: 1:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Class2 {
+}
