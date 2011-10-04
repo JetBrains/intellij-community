cf8385bb4fcfaac7f2279eeb6a8aa7f3d18abfee
peter <peter@jetbrains.com>
2011-10-04 16:29:43 +0200

    unblock document (EA-29936)

--- java/java-impl/src/com/intellij/codeInsight/lookup/PsiTypeLookupItem.java
+++ java/java-impl/src/com/intellij/codeInsight/lookup/PsiTypeLookupItem.java
@@ -24,6 +24,7 @@ import com.intellij.openapi.editor.ScrollType;
 import com.intellij.openapi.util.ClassConditionKey;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.*;
+import com.intellij.psi.impl.source.PostprocessReformattingAspect;
 import com.intellij.psi.impl.source.PsiClassReferenceType;
 import com.intellij.psi.util.PsiUtil;
 import org.jetbrains.annotations.NonNls;
@@ -51,6 +52,7 @@ public class PsiTypeLookupItem extends LookupItem {
   public void handleInsert(InsertionContext context) {
     context.getDocument().insertString(context.getTailOffset(), calcGenerics());
     DefaultInsertHandler.addImportForItem(context, this);
+    PostprocessReformattingAspect.getInstance(context.getProject()).doPostponedFormatting();
 
     int tail = context.getTailOffset();
     String braces = StringUtil.repeat("[]", getBracketsCount());
