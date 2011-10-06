c5d26c232e96375d4dbb04057ad5dfb0c926e64c
Evgeny Zakrevsky <evgeny.zakrevsky@jetbrains.com>
2011-10-06 20:05:41 +0400

    1. OC-1864 Can't switch tabs. 2. Titles in Tabbed Panes now spotlighted

--- platform/platform-api/src/com/intellij/ui/components/JBTabbedPane.java
+++ platform/platform-api/src/com/intellij/ui/components/JBTabbedPane.java
@@ -16,6 +16,8 @@ import java.util.Arrays;
  * @author evgeny.zakrevsky
  */
 public class JBTabbedPane extends JTabbedPane implements HierarchyListener {
+  public static final String LABEL_FROM_TABBED_PANE = "JBTabbedPane.labelFromTabbedPane";
+  
   public JBTabbedPane() {
   }
 
@@ -33,30 +35,38 @@ public class JBTabbedPane extends JTabbedPane implements HierarchyListener {
     component.addHierarchyListener(this);
     UIUtil.setNotOpaqueRecursively(component);
     setInsets(component);
+    revalidate();
+    repaint();
   }
 
   @Override
   public void insertTab(String title, Icon icon, Component component, String tip, int index) {
     super.insertTab(title, icon, component, tip, index);
+
+    //set custom label for correct work spotlighting in settings
+    JLabel label = new JLabel(title);
+    label.setIcon(icon);
+    label.setBorder(new EmptyBorder(1,1,1,1));
+    setTabComponentAt(index, label);
+    label.putClientProperty(LABEL_FROM_TABBED_PANE, Boolean.TRUE);
+
     component.addHierarchyListener(this);
     UIUtil.setNotOpaqueRecursively(component);
     setInsets(component);
+
+    revalidate();
+    repaint();
   }
 
-  private void setInsets(Component component) {
+  private static void setInsets(Component component) {
     if (component instanceof JComponent) {
-      if (((JComponent)component).getBorder() == null) {
-        ((JComponent)component).setBorder(new EmptyBorder(UIUtil.PANEL_SMALL_INSETS));
-      }
-      else {
-        ((JComponent)component)
-          .setBorder(new CompoundBorder(new EmptyBorder(UIUtil.PANEL_SMALL_INSETS), ((JComponent)component).getBorder()));
-      }
+      UIUtil.addInsets((JComponent)component, UIUtil.PANEL_SMALL_INSETS);
     }
   }
 
   @Override
   public void hierarchyChanged(HierarchyEvent e) {
     UIUtil.setNotOpaqueRecursively(e.getComponent());
+    repaint();
   }
 }
--- platform/util/src/com/intellij/util/ui/UIUtil.java
+++ platform/util/src/com/intellij/util/ui/UIUtil.java
@@ -36,6 +36,8 @@ import org.jetbrains.annotations.TestOnly;
 import javax.swing.*;
 import javax.swing.Timer;
 import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import javax.swing.plaf.ComboBoxUI;
 import javax.swing.plaf.ProgressBarUI;
@@ -2475,5 +2477,13 @@ public class UIUtil {
     }
   }
 
+  public static void addInsets(JComponent component, Insets insets) {
+    if (component.getBorder() != null) {
+      component.setBorder(new CompoundBorder(new EmptyBorder(insets), component.getBorder()));
+    }
+    else {
+      component.setBorder(new EmptyBorder(insets));
+    }
+  }
 }
 
