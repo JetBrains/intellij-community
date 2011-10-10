a1d78e6ae47027b21326e1f47aa73db2dc69f94f
Dmitry Boulytchev <db@mobilis.(none)>
2011-10-11 00:35:55 +0400

    ChangeMembers --- improved.

--- jps/src/org/jetbrains/ether/Pair.java
+++ jps/src/org/jetbrains/ether/Pair.java
@@ -15,4 +15,24 @@ public class Pair<X, Y> {
         this.fst = fst;
         this.snd = snd;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Pair pair = (Pair) o;
+
+        if (!fst.equals(pair.fst)) return false;
+        if (!snd.equals(pair.snd)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = fst.hashCode();
+        result = 31 * result + snd.hashCode();
+        return result;
+    }
 }
