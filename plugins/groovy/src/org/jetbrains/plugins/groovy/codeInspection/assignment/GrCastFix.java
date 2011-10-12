ed3774fe919267737c5aa9bb1bb4982af4f87313
Maxim.Medvedev <maxim.medvedev@jetbrains.com>
2011-10-12 13:11:53 +0400

    IDEA-42905 offer to cast to desired (if convertible) type on unassignable types warning

---
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/codeInspection/assignment/GrCastFix.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2000-2011 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jetbrains.plugins.groovy.codeInspection.assignment;
+
+import com.intellij.codeInspection.LocalQuickFix;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiType;
+import com.intellij.util.IncorrectOperationException;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.codeInspection.GroovyFix;
+import org.jetbrains.plugins.groovy.lang.GrReferenceAdjuster;
+import org.jetbrains.plugins.groovy.lang.psi.GroovyPsiElementFactory;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrExpression;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrSafeCastExpression;
+import org.jetbrains.plugins.groovy.lang.psi.api.types.GrTypeElement;
+
+/**
+ * @author Maxim.Medvedev
+ */
+public class GrCastFix extends GroovyFix implements LocalQuickFix {
+  private GrExpression myExpression;
+  private PsiType myExpectedType;
+
+  public GrCastFix(GrExpression expression, PsiType expectedType) {
+    myExpression = expression;
+    myExpectedType = expectedType;
+  }
+
+  @Override
+  protected void doFix(Project project, ProblemDescriptor descriptor) throws IncorrectOperationException {
+    final GroovyPsiElementFactory factory = GroovyPsiElementFactory.getInstance(project);
+    final GrSafeCastExpression cast = (GrSafeCastExpression)factory.createExpressionFromText("foo as String");
+    final GrTypeElement typeElement = factory.createTypeElement(myExpectedType);
+    cast.getOperand().replace(myExpression);
+    cast.getCastTypeElement().replace(typeElement);
+
+    final GrExpression replaced = myExpression.replaceWithExpression(cast, true);
+    GrReferenceAdjuster.shortenReferences(replaced);
+  }
+
+  @NotNull
+  @Override
+  public String getName() {
+    return "Cast to " + myExpectedType.getPresentableText();
+  }
+}
