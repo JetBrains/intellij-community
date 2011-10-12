f714610da8857928c7e737865bc5a53e77bdeeec
nik <Nikolay.Chashnikov@jetbrains.com>
2011-10-12 13:47:24 +0400

    added action: import iml file from Project View as a module

---
+++ platform/lang-impl/src/com/intellij/ide/projectView/actions/ImportModuleFromImlFileAction.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2000-2011 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.ide.projectView.actions;
+
+import com.intellij.CommonBundle;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.application.AccessToken;
+import com.intellij.openapi.application.WriteAction;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.fileTypes.StdFileTypes;
+import com.intellij.openapi.module.ModifiableModuleModel;
+import com.intellij.openapi.module.ModuleManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.VirtualFile;
+
+/**
+ * @author nik
+ */
+public class ImportModuleFromImlFileAction extends AnAction {
+  private static final Logger LOG = Logger.getInstance("#com.intellij.ide.projectView.actions.ImportModuleFromImlFileAction");
+
+  @Override
+  public void actionPerformed(AnActionEvent e) {
+    final VirtualFile[] files = e.getData(PlatformDataKeys.VIRTUAL_FILE_ARRAY);
+    final Project project = getEventProject(e);
+    if (files == null || project == null) return;
+
+    try {
+      final ModifiableModuleModel model = ModuleManager.getInstance(project).getModifiableModel();
+      for (VirtualFile file : files) {
+        model.loadModule(file.getPath());
+      }
+
+      AccessToken token = WriteAction.start();
+      try {
+        model.commit();
+      }
+      finally {
+        token.finish();
+      }
+    }
+    catch (Exception ex) {
+      LOG.info(ex);
+      Messages.showErrorDialog(project, "Cannot import module: " + ex.getMessage(), CommonBundle.getErrorTitle());
+    }
+  }
+
+  @Override
+  public void update(AnActionEvent e) {
+    final boolean visible = isVisible(e);
+    final Presentation presentation = e.getPresentation();
+    presentation.setVisible(visible);
+    presentation.setEnabled(visible);
+  }
+
+  private static boolean isVisible(AnActionEvent e) {
+    final VirtualFile[] files = e.getData(PlatformDataKeys.VIRTUAL_FILE_ARRAY);
+    if (getEventProject(e) == null || files == null || files.length == 0) {
+      return false;
+    }
+    for (VirtualFile file : files) {
+      if (!file.getFileType().equals(StdFileTypes.IDEA_MODULE)) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
