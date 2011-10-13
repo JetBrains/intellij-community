f97f508c4e59b73a35c28dd294e296eb46eb8807
Eugene Zhuravlev <jeka@intellij.com>
2011-10-13 22:05:44 +0200

    temporary comment explicit deps to fix compilation

--- java/compiler/compile-server/src/org/jetbrains/jpsservice/Bootstrap.java
+++ java/compiler/compile-server/src/org/jetbrains/jpsservice/Bootstrap.java
@@ -15,20 +15,9 @@
  */
 package org.jetbrains.jpsservice;
 
-import com.intellij.ant.PseudoClassLoader;
-import com.intellij.compiler.notNullVerification.NotNullVerifyingInstrumenter;
 import com.intellij.openapi.application.PathManager;
-import com.intellij.openapi.util.io.FileUtil;
-import com.intellij.uiDesigner.compiler.AlienFormFileException;
-import com.intellij.uiDesigner.core.GridConstraints;
-import com.jgoodies.forms.layout.CellConstraints;
 import gnu.trove.TIntHash;
-import net.n3.nanoxml.IXMLBuilder;
-import org.codehaus.groovy.GroovyException;
-import org.jetbrains.jps.MacroExpander;
 import org.jetbrains.jps.server.Facade;
-import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.commons.EmptyVisitor;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -48,18 +37,18 @@ public class Bootstrap {
     cp.add(getResourcePath(com.google.protobuf.Message.class));
     cp.add(getResourcePath(org.jboss.netty.bootstrap.Bootstrap.class));
     cp.add(getResourcePath(TIntHash.class));  // trove
-    cp.add(getResourcePath(FileUtil.class));  // util module
-    cp.add(getResourcePath(ClassWriter.class));  // asm
-    cp.add(getResourcePath(EmptyVisitor.class));  // asm-commons
-    cp.add(getResourcePath(MacroExpander.class));  // jps-model
-    cp.add(getResourcePath(AlienFormFileException.class));  // forms-compiler
-    cp.add(getResourcePath(PseudoClassLoader.class));  // javac2
-    cp.add(getResourcePath(GroovyException.class));  // groovy
-    cp.add(getResourcePath(org.jdom.input.SAXBuilder.class));  // jdom
-    cp.add(getResourcePath(GridConstraints.class));  // forms-rt
-    cp.add(getResourcePath(CellConstraints.class));  // jgoodies-forms
-    cp.add(getResourcePath(NotNullVerifyingInstrumenter.class));  // not-null
-    cp.add(getResourcePath(IXMLBuilder.class));  // nano-xml
+    //cp.add(getResourcePath(FileUtil.class));  // util module
+    //cp.add(getResourcePath(ClassWriter.class));  // asm
+    //cp.add(getResourcePath(EmptyVisitor.class));  // asm-commons
+    //cp.add(getResourcePath(MacroExpander.class));  // jps-model
+    //cp.add(getResourcePath(AlienFormFileException.class));  // forms-compiler
+    //cp.add(getResourcePath(PseudoClassLoader.class));  // javac2
+    //cp.add(getResourcePath(GroovyException.class));  // groovy
+    //cp.add(getResourcePath(org.jdom.input.SAXBuilder.class));  // jdom
+    //cp.add(getResourcePath(GridConstraints.class));  // forms-rt
+    //cp.add(getResourcePath(CellConstraints.class));  // jgoodies-forms
+    //cp.add(getResourcePath(NotNullVerifyingInstrumenter.class));  // not-null
+    //cp.add(getResourcePath(IXMLBuilder.class));  // nano-xml
     final File jpsFacadeJar = getResourcePath(Facade.class);
     cp.add(jpsFacadeJar);
 
