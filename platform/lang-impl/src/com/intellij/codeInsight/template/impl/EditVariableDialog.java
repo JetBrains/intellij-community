da83cc3124f9a729d5bb45bb9c2af80696003966
Dmitry Jemerov <yole@jetbrains.com>
2011-10-13 18:23:44 +0200

    use new-style table reordering UI when editing live template variables (related to IDEA-25125)

--- platform/lang-impl/src/com/intellij/codeInsight/template/impl/EditVariableDialog.java
+++ platform/lang-impl/src/com/intellij/codeInsight/template/impl/EditVariableDialog.java
@@ -28,28 +28,20 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.help.HelpManager;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.ui.IdeBorderFactory;
-import com.intellij.ui.ScrollPaneFactory;
-import com.intellij.util.ui.Table;
+import com.intellij.ui.ToolbarDecorator;
+import com.intellij.ui.table.JBTable;
+import com.intellij.util.ui.EditableModel;
 
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableCellEditor;
 import javax.swing.table.TableModel;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 import java.util.List;
 
 class EditVariableDialog extends DialogWrapper {
   private ArrayList<Variable> myVariables = new ArrayList<Variable>();
-
-  private JButton myMoveUpButton;
-  private JButton myMoveDownButton;
 
   private JTable myTable;
   private Editor myEditor;
@@ -64,7 +56,6 @@ class EditVariableDialog extends DialogWrapper {
     init();
     setTitle(CodeInsightBundle.message("templates.dialog.edit.variables.title"));
     setOKButtonText(CommonBundle.getOkButtonText());
-    updateButtons();
   }
 
   protected Action[] createActions() {
@@ -89,46 +80,7 @@ class EditVariableDialog extends DialogWrapper {
       CodeInsightBundle.message("templates.dialog.edit.variables.border.title"), false, false, true));
     panel.setLayout(new BorderLayout());
     panel.add(createVariablesTable(), BorderLayout.CENTER);
-    panel.add(createTableButtonPanel(), BorderLayout.EAST);
     return panel;
-  }
-
-  private JPanel createTableButtonPanel() {
-    JPanel tableButtonsPanel = new JPanel();
-    tableButtonsPanel.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
-
-    tableButtonsPanel.setLayout(new GridBagLayout());
-    GridBagConstraints gbConstraints = new GridBagConstraints();
-    gbConstraints.gridwidth = GridBagConstraints.REMAINDER;
-    gbConstraints.fill = GridBagConstraints.HORIZONTAL;
-    gbConstraints.insets = new Insets(0, 0, 4, 0);
-    myMoveUpButton = new JButton(CodeInsightBundle.message("templates.dialog.edit.variables.action.move.up"));
-//    myMoveUpButton.setMargin(new Insets(2,0,0,2));
-    tableButtonsPanel.add(myMoveUpButton, gbConstraints);
-    myMoveDownButton = new JButton(CodeInsightBundle.message("templates.dialog.edit.variables.action.move.down"));
-//    myMoveDownButton.setMargin(new Insets(2,0,0,2));
-    tableButtonsPanel.add(myMoveDownButton, gbConstraints);
-
-    gbConstraints.weighty = 1;
-    tableButtonsPanel.add(new JPanel(), gbConstraints);
-
-    myMoveUpButton.addActionListener(
-      new ActionListener() {
-        public void actionPerformed(ActionEvent e) {
-          moveRowUp();
-        }
-      }
-    );
-
-    myMoveDownButton.addActionListener(
-      new ActionListener() {
-        public void actionPerformed(ActionEvent e) {
-          moveRowDown();
-        }
-      }
-    );
-
-    return tableButtonsPanel;
   }
 
   private JComponent createVariablesTable() {
@@ -140,71 +92,10 @@ class EditVariableDialog extends DialogWrapper {
     };
 
     // Create a model of the data.
-    TableModel dataModel = new AbstractTableModel() {
-      public int getColumnCount() {
-        return names.length;
-      }
-
-      public int getRowCount() {
-        return myVariables.size();
-      }
-
-      public Object getValueAt(int row, int col) {
-        Variable variable = myVariables.get(row);
-        if (col == 0) {
-          return variable.getName();
-        }
-        if (col == 1) {
-          return variable.getExpressionString();
-        }
-        if (col == 2) {
-          return variable.getDefaultValueString();
-        }
-        else {
-          return variable.isAlwaysStopAt() ? Boolean.FALSE : Boolean.TRUE;
-        }
-      }
-
-      public String getColumnName(int column) {
-        return names[column];
-      }
-
-      public Class getColumnClass(int c) {
-        if (c <= 2) {
-          return String.class;
-        }
-        else {
-          return Boolean.class;
-        }
-      }
-
-      public boolean isCellEditable(int row, int col) {
-        return true;
-      }
-
-      public void setValueAt(Object aValue, int row, int col) {
-        Variable variable = myVariables.get(row);
-        if (col == 0) {
-           String varName = (String) aValue;
-          Variable newVar = new Variable (varName, variable.getExpressionString(), variable.getDefaultValueString(),
-                          variable.isAlwaysStopAt());
-          myVariables.set(row, newVar);
-          updateTemplateTextByVarNameChange(variable, newVar);
-        }
-        else if (col == 1) {
-          variable.setExpressionString((String)aValue);
-        }
-        else if (col == 2) {
-          variable.setDefaultValueString((String)aValue);
-        }
-        else {
-          variable.setAlwaysStopAt(!((Boolean)aValue).booleanValue());
-        }
-      }
-    };
+    TableModel dataModel = new VariablesModel(names);
 
     // Create the table
-    myTable = new Table(dataModel);
+    myTable = new JBTable(dataModel);
     myTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
     myTable.setPreferredScrollableViewportSize(new Dimension(500, myTable.getRowHeight() * 8));
     myTable.getColumn(names[0]).setPreferredWidth(120);
@@ -251,74 +142,8 @@ class EditVariableDialog extends DialogWrapper {
     cellEditor.setClickCountToStart(1);
     myTable.setDefaultEditor(String.class, cellEditor);
 
-    myTable.getSelectionModel().addListSelectionListener(
-      new ListSelectionListener() {
-        public void valueChanged(ListSelectionEvent e) {
-          updateButtons();
-        }
-      }
-    );
-    return ScrollPaneFactory.createScrollPane(myTable);
-  }
-
-  private void updateButtons() {
-    int selected = myTable.getSelectedRow();
-    if (myMoveUpButton != null) {
-      myMoveUpButton.setEnabled(selected >= 1);
-    }
-    if (myMoveDownButton != null) {
-      myMoveDownButton.setEnabled(selected >= 0 && selected < myVariables.size() - 1);
-    }
-  }
-
-  private void moveRowUp() {
-    int selected = myTable.getSelectedRow();
-    if (selected < 1) {
-      return;
-    }
-    if (myTable.isEditing()) {
-      TableCellEditor editor = myTable.getCellEditor();
-      if (editor != null) {
-        editor.stopCellEditing();
-      }
-    }
-    moveElementUp(myVariables, selected);
-
-    AbstractTableModel model = (AbstractTableModel)myTable.getModel();
-    model.fireTableRowsUpdated(selected - 1, selected);
-    myTable.setRowSelectionInterval(selected - 1, selected - 1);
-  }
-
-  private static void moveElementUp(ArrayList<Variable> array, int offset) {
-    Variable element = array.get(offset);
-    Variable previousElement = array.get(offset - 1);
-    array.set(offset, previousElement);
-    array.set(offset - 1, element);
-  }
-
-  private void moveRowDown() {
-    int selected = myTable.getSelectedRow();
-    if (selected >= myVariables.size() - 1 || selected < 0) {
-      return;
-    }
-    if (myTable.isEditing()) {
-      TableCellEditor editor = myTable.getCellEditor();
-      if (editor != null) {
-        editor.stopCellEditing();
-      }
-    }
-    moveElementDown(myVariables, selected);
-
-    AbstractTableModel model = (AbstractTableModel)myTable.getModel();
-    model.fireTableRowsUpdated(selected, selected + 1);
-    myTable.setRowSelectionInterval(selected + 1, selected + 1);
-  }
-
-  private static void moveElementDown(ArrayList<Variable> array, int offset) {
-    Variable element = array.get(offset);
-    Variable nextElement = array.get(offset + 1);
-    array.set(offset, nextElement);
-    array.set(offset + 1, element);
+    final ToolbarDecorator decorator = ToolbarDecorator.createDecorator(myTable).disableAddAction().disableRemoveAction();
+    return decorator.createPanel();
   }
 
   protected void doOKAction() {
@@ -365,5 +190,89 @@ class EditVariableDialog extends DialogWrapper {
         }, null, null);
       }
     });
+  }
+
+  private class VariablesModel extends AbstractTableModel implements EditableModel {
+    private final String[] myNames;
+
+    public VariablesModel(String[] names) {
+      myNames = names;
+    }
+
+    public int getColumnCount() {
+      return myNames.length;
+    }
+
+    public int getRowCount() {
+      return myVariables.size();
+    }
+
+    public Object getValueAt(int row, int col) {
+      Variable variable = myVariables.get(row);
+      if (col == 0) {
+        return variable.getName();
+      }
+      if (col == 1) {
+        return variable.getExpressionString();
+      }
+      if (col == 2) {
+        return variable.getDefaultValueString();
+      }
+      else {
+        return variable.isAlwaysStopAt() ? Boolean.FALSE : Boolean.TRUE;
+      }
+    }
+
+    public String getColumnName(int column) {
+      return myNames[column];
+    }
+
+    public Class getColumnClass(int c) {
+      if (c <= 2) {
+        return String.class;
+      }
+      else {
+        return Boolean.class;
+      }
+    }
+
+    public boolean isCellEditable(int row, int col) {
+      return true;
+    }
+
+    public void setValueAt(Object aValue, int row, int col) {
+      Variable variable = myVariables.get(row);
+      if (col == 0) {
+         String varName = (String) aValue;
+        Variable newVar = new Variable (varName, variable.getExpressionString(), variable.getDefaultValueString(),
+                        variable.isAlwaysStopAt());
+        myVariables.set(row, newVar);
+        updateTemplateTextByVarNameChange(variable, newVar);
+      }
+      else if (col == 1) {
+        variable.setExpressionString((String)aValue);
+      }
+      else if (col == 2) {
+        variable.setDefaultValueString((String)aValue);
+      }
+      else {
+        variable.setAlwaysStopAt(!((Boolean)aValue).booleanValue());
+      }
+    }
+
+    @Override
+    public void addRow() {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void removeRow(int index) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void exchangeRows(int oldIndex, int newIndex) {
+      Collections.swap(myVariables, oldIndex, newIndex);
+    }
   }
 }
