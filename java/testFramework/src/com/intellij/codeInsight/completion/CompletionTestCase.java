fac740f9add12648ba487ccb11c9d5e7b2bfcc23
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2011-10-14 16:56:52 +0200

    Fix tests?

--- java/testFramework/src/com/intellij/codeInsight/completion/CompletionTestCase.java
+++ java/testFramework/src/com/intellij/codeInsight/completion/CompletionTestCase.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2011 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.intellij.codeInsight.completion;
 
 import com.intellij.codeInsight.daemon.DaemonAnalyzerTestCase;
@@ -31,8 +46,12 @@ public abstract class CompletionTestCase extends DaemonAnalyzerTestCase {
 
   @Override
   protected void tearDown() throws Exception {
-    LookupManager.getInstance(myProject).hideActiveLookup();
-    super.tearDown();
+    try {
+      LookupManager.getInstance(myProject).hideActiveLookup();
+    }
+    finally {
+      super.tearDown();
+    }
     myItems = null;
   }
 
