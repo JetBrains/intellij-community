723769d1355816602b73ce2f3b9e457e2d234194
Bas Leijdekkers <basleijdekkers@gmail.com>
2011-10-17 12:41:49 +0200

    fix equals()

--- plugins/cvs/cvs-core/src/com/intellij/cvsSupport2/config/ProxySettings.java
+++ plugins/cvs/cvs-core/src/com/intellij/cvsSupport2/config/ProxySettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2011 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,10 +41,10 @@ public class ProxySettings implements JDOMExternalizable, Cloneable {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
 
-    ProxySettings that = (ProxySettings)o;
+    final ProxySettings that = (ProxySettings)o;
     if (USE_PROXY != that.USE_PROXY) return false;
     if (!USE_PROXY) {
-      return false;
+      return true;
     }
     return PROXY_PORT == that.PROXY_PORT &&
            TYPE == that.TYPE &&
