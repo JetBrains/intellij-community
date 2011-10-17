4e5ba514e0a977122ccb85df47ddb86a71b439f0
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2011-10-17 23:25:49 +0200

    disabled unused import inspection in mako (fixed PY-4755 Mako: unused imports: imports are always unused in module-level blocks)

--- python/src/com/jetbrains/python/inspections/PyUnresolvedReferencesInspection.java
+++ python/src/com/jetbrains/python/inspections/PyUnresolvedReferencesInspection.java
@@ -107,7 +107,7 @@ public class PyUnresolvedReferencesInspection extends PyInspection {
 
     @Override
     public void visitPyTargetExpression(PyTargetExpression node) {
-      if (CythonLanguageDialect._isDisabledFor(node)) {
+      if (CythonLanguageDialect._isDisabledFor(node) || MakoLanguage._isDisabledFor(node)) {
         return;
       }
       checkSlots(node);
@@ -132,7 +132,7 @@ public class PyUnresolvedReferencesInspection extends PyInspection {
     @Override
     public void visitPyImportElement(PyImportElement node) {
       super.visitPyImportElement(node);
-      if (CythonLanguageDialect._isDisabledFor(node)) {
+      if (CythonLanguageDialect._isDisabledFor(node) || MakoLanguage._isDisabledFor(node)) {
         return;
       }
       final PyFromImportStatement fromImport = PsiTreeUtil.getParentOfType(node, PyFromImportStatement.class);
@@ -144,7 +144,7 @@ public class PyUnresolvedReferencesInspection extends PyInspection {
     @Override
     public void visitPyStarImportElement(PyStarImportElement node) {
       super.visitPyStarImportElement(node);
-      if (CythonLanguageDialect._isDisabledFor(node)) {
+      if (CythonLanguageDialect._isDisabledFor(node) || MakoLanguage._isDisabledFor(node)) {
         return;
       }
       myAllImports.add(node);
