5b2b338949c2a9f64a57e3b98ea05aba51178500
Evgeny Zakrevsky <evgeny.zakrevsky@jetbrains.com>
2011-10-17 21:32:14 +0400

    EA-30523 - NPE: PluginManagerConfigurable$1.run

--- platform/platform-impl/src/com/intellij/ide/plugins/PluginManagerConfigurable.java
+++ platform/platform-impl/src/com/intellij/ide/plugins/PluginManagerConfigurable.java
@@ -163,6 +163,7 @@ public class PluginManagerConfigurable extends BaseConfigurable implements Searc
   public Runnable enableSearch(final String option) {
     return new Runnable(){
       public void run() {
+        if (myPluginManagerMain == null) return;
         myPluginManagerMain.filter(option);
       }
     };
