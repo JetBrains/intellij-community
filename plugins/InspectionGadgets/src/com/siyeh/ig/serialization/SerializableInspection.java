5e0716470b67cac448c2c948ed36b4fbde62205a
Bas Leijdekkers <basleijdekkers@gmail.com>
2011-10-25 10:51:00 +0200

    better settings ui for serializable inspections

--- plugins/InspectionGadgets/src/com/siyeh/ig/serialization/SerializableInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/serialization/SerializableInspection.java
@@ -15,95 +15,86 @@
  */
 package com.siyeh.ig.serialization;
 
-import com.intellij.codeInspection.ui.ListTable;
-import com.intellij.codeInspection.ui.ListWrappingTableModel;
-import com.intellij.openapi.actionSystem.ActionToolbar;
 import com.intellij.openapi.util.InvalidDataException;
-import com.intellij.openapi.util.WriteExternalException;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.util.InheritanceUtil;
-import com.intellij.ui.ScrollPaneFactory;
 import com.intellij.util.ui.CheckBox;
 import com.siyeh.InspectionGadgetsBundle;
 import com.siyeh.ig.BaseInspection;
 import com.siyeh.ig.psiutils.SerializationUtils;
+import com.siyeh.ig.ui.ExternalizableStringSet;
 import com.siyeh.ig.ui.UiUtils;
 import org.jdom.Element;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public abstract class SerializableInspection extends BaseInspection {
 
+  private static final JComponent[] EMPTY_COMPONENT_ARRAY = {};
+
   @SuppressWarnings({"PublicField"})
   public boolean ignoreAnonymousInnerClasses = false;
-  @SuppressWarnings({"PublicField"})
-  public String superClassString = "java.awt.Component";
-  protected List<String> superClassList = new ArrayList();
 
-  protected SerializableInspection() {
-    parseString(superClassString, superClassList);
-  }
+  @Deprecated @SuppressWarnings({"PublicField"})
+  public String superClassString = "java.awt.Component";
+
+  @SuppressWarnings("PublicField")
+  public final ExternalizableStringSet superClasses = new ExternalizableStringSet();
 
   @Override
-  public JComponent createOptionsPanel() {
+  public final JComponent createOptionsPanel() {
     final JComponent panel = new JPanel(new GridBagLayout());
 
-    final ListTable table = new ListTable(new ListWrappingTableModel(
-      superClassList, InspectionGadgetsBundle.message(
-      "ignore.classes.in.hierarchy.column.name")));
-    final JScrollPane scrollPane =
-      ScrollPaneFactory.createScrollPane(table);
-    UiUtils.setScrollPaneSize(scrollPane, 7, 25);
-    final ActionToolbar toolbar =
-      UiUtils.createAddRemoveTreeClassChooserToolbar(table,
-                                                     InspectionGadgetsBundle.message(
-                                                       "choose.super.class.to.ignore"));
+    final JPanel chooserList = UiUtils.createTreeClassChooserList(
+      superClasses, InspectionGadgetsBundle.message("ignore.classes.in.hierarchy.column.name"),
+      InspectionGadgetsBundle.message("choose.super.class.to.ignore"));
+    UiUtils.setComponentSize(chooserList, 7, 25);
     final CheckBox checkBox = new CheckBox(InspectionGadgetsBundle.message(
-      "ignore.anonymous.inner.classes"), this,
-                                           "ignoreAnonymousInnerClasses");
+      "ignore.anonymous.inner.classes"), this, "ignoreAnonymousInnerClasses");
 
     final GridBagConstraints constraints = new GridBagConstraints();
     constraints.gridx = 0;
     constraints.gridy = 0;
     constraints.insets.left = 4;
     constraints.insets.right = 4;
-    constraints.fill = GridBagConstraints.HORIZONTAL;
-    panel.add(toolbar.getComponent(), constraints);
 
-    constraints.gridy = 1;
     constraints.weightx = 1.0;
     constraints.weighty = 1.0;
     constraints.fill = GridBagConstraints.BOTH;
-    panel.add(scrollPane, constraints);
+    panel.add(chooserList, constraints);
 
-    constraints.gridy = 2;
-    constraints.weighty = 0.0;
     constraints.fill = GridBagConstraints.HORIZONTAL;
-    panel.add(checkBox, constraints);
+    final JComponent[] additionalOptions = createAdditionalOptions();
+    for (JComponent additionalOption : additionalOptions) {
+      constraints.gridy++;
+      panel.add(additionalOption, constraints);
+    }
 
+    constraints.gridy++;
+    constraints.weighty = 0.0;
+    panel.add(checkBox, constraints);
     return panel;
   }
 
   @Override
   public void readSettings(Element node) throws InvalidDataException {
     super.readSettings(node);
-    parseString(superClassString, superClassList);
+    if (!superClassString.isEmpty()) {
+      parseString(superClassString, superClasses);
+      superClassString = "";
+    }
   }
 
-  @Override
-  public void writeSettings(Element node) throws WriteExternalException {
-    superClassString = formatString(superClassList);
-    super.writeSettings(node);
+  protected JComponent[] createAdditionalOptions() {
+    return EMPTY_COMPONENT_ARRAY;
   }
 
   protected boolean isIgnoredSubclass(PsiClass aClass) {
     if (SerializationUtils.isDirectlySerializable(aClass)) {
       return false;
     }
-    for (String superClassName : superClassList) {
+    for (String superClassName : superClasses) {
       if (InheritanceUtil.isInheritor(aClass, superClassName)) {
         return true;
       }
