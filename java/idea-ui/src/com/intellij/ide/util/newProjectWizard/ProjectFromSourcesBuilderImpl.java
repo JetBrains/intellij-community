cdb85c973d6b04cbf81283243daed68f1f0f30a8
nik <Nikolay.Chashnikov@jetbrains.com>
2011-10-25 11:38:45 +0400

    unused methods removed

--- java/idea-ui/src/com/intellij/ide/util/newProjectWizard/ProjectFromSourcesBuilderImpl.java
+++ java/idea-ui/src/com/intellij/ide/util/newProjectWizard/ProjectFromSourcesBuilderImpl.java
@@ -81,6 +81,7 @@ public class ProjectFromSourcesBuilderImpl extends ProjectBuilder implements Pro
     myRoots = roots;
   }
 
+  @NotNull
   @Override
   public Collection<DetectedProjectRoot> getProjectRoots(ProjectStructureDetector detector) {
     return myRoots.get(detector);
@@ -263,6 +264,7 @@ public class ProjectFromSourcesBuilderImpl extends ProjectBuilder implements Pro
 
   }
 
+  @NotNull
   @Override
   public ProjectDescriptor getProjectDescriptor(ProjectStructureDetector detector) {
     return myProjectDescriptors.get(detector);
