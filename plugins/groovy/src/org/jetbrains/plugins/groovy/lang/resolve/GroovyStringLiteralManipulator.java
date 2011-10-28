22d660d7dae8cf6c77ccc4dec3c2b4840433858b
Maxim.Medvedev <maxim.medvedev@jetbrains.com>
2011-10-28 10:55:57 +0400

    IDEA-74490 Groovy: "Inject language" intention should not be suggested for numeric and boolean literals

--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/resolve/GroovyStringLiteralManipulator.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/resolve/GroovyStringLiteralManipulator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2011 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -56,6 +56,7 @@ public class GroovyStringLiteralManipulator extends AbstractElementManipulator<G
 
   public TextRange getRangeInElement(final GrLiteral element) {
     final String text = element.getText();
+    if (!(element.getValue() instanceof String)) return super.getRangeInElement(element);
     return getLiteralRange(text);
   }
 
