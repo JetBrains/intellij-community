349bae9c3583dc9595cec4884e950360d334bfa2
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2011-11-03 17:34:09 +0100

    fixed PY-4832 Test runner can't attach to pinax django tests

--- python/helpers/pycharm/django_test_manage.py
+++ python/helpers/pycharm/django_test_manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from pycharm.fix_getpass import fixGetpass
 from pycharm import django_settings
-from django.core.management import execute_manager
+from django.core.management import execute_manager, setup_environ
 
 import os
 manage_file = os.getenv('PYCHARM_DJANGO_MANAGE_MODULE')
@@ -9,6 +9,9 @@ if not manage_file:
     manage_file = 'manage'
 
 if __name__ == "__main__":
+  setup_environ(django_settings)
+  from django.conf import settings
+  settings.DATABASES # just to initialize django lazy settings correctly
   try:
     __import__(manage_file)
   except ImportError:
