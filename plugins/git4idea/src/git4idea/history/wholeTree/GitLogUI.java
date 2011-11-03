961e9499894656b55ec58c23a15d5fef952c8d82
irengrig <Irina.Chernushina@jetbrains.com>
2011-11-03 18:49:46 +0400

    IDEA-76418 Git log: revision context menu: Branch action has only All option

--- plugins/git4idea/src/git4idea/history/wholeTree/GitLogUI.java
+++ plugins/git4idea/src/git4idea/history/wholeTree/GitLogUI.java
@@ -298,6 +298,7 @@ public class GitLogUI implements Disposable {
         }
 
         myBranchSelectorAction.setSymbolicRefs(myRecalculatedCommon);
+        myBranchSelectorAction.asTextAction();
       }
     };
   }
