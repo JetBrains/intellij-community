4293691e26d7934e093ef9d37001f736f77430a0
Maxim.Medvedev <maxim.medvedev@jetbrains.com>
2011-11-03 16:54:45 +0400

    IDEA-74355 Packages view drag-and-drop refactoring doesn't update package statements

---
+++ plugins/groovy/testdata/refactoring/move/moveFile/moveJavaGroovyText/after/pack2/A.groovy
@@ -0,0 +1,9 @@
+package pack2
+
+import pack1.C
+
+class A {
+  def a = new A()
+  def b = new B()
+  def c = new C()
+}
---
+++ plugins/groovy/testdata/refactoring/move/moveFile/moveJavaGroovyText/before/pack1/B.java
@@ -0,0 +1,7 @@
+package pack1;
+
+class B {
+  A a = new A();
+  B b = new B();
+  C c = new C();
+}
