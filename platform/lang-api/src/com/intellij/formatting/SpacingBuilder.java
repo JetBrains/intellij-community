7c2b53c58bf3eb76a9777a66c96aa35812d79f72
Dmitry Jemerov <yole@jetbrains.com>
2011-11-04 17:02:23 +0100

    rule-based API for spacing calculation in formatter; use it in Python formatter

---
+++ platform/lang-api/src/com/intellij/formatting/SpacingBuilder.java
@@ -0,0 +1,215 @@
+package com.intellij.formatting;
+
+import com.intellij.psi.codeStyle.CodeStyleSettings;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.tree.TokenSet;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author yole
+ */
+public class SpacingBuilder {
+  private static class SpacingRule {
+    private TokenSet myParentType;
+    private TokenSet myChild1Type;
+    private TokenSet myChild2Type;
+    private int myMinSpaces;
+    private int myMaxSpaces;
+    private int myMinLF;
+    private boolean myKeepLineBreaks;
+    private int myKeepBlankLines;
+
+    private SpacingRule(RuleCondition condition, int minSpaces, int maxSpaces, int minLF, boolean keepLineBreaks, int keepBlankLines) {
+      myParentType = condition.myParentType;
+      myChild1Type = condition.myChild1Type;
+      myChild2Type = condition.myChild2Type;
+      myMinSpaces = minSpaces;
+      myMaxSpaces = maxSpaces;
+      myMinLF = minLF;
+      myKeepLineBreaks = keepLineBreaks;
+      myKeepBlankLines = keepBlankLines;
+    }
+
+    public boolean matches(IElementType parentType, IElementType child1Type, IElementType child2Type) {
+      return ((myParentType == null || myParentType.contains(parentType)) &&
+              (myChild1Type == null || myChild1Type.contains(child1Type)) &&
+              (myChild2Type == null || myChild2Type.contains(child2Type)));
+    }
+
+    public Spacing createSpacing() {
+      return Spacing.createSpacing(myMinSpaces, myMaxSpaces, myMinLF, myKeepLineBreaks, myKeepBlankLines);
+    }
+  }
+  
+  private class RuleCondition {
+    private final TokenSet myParentType;
+    private final TokenSet myChild1Type;
+    private final TokenSet myChild2Type;
+
+    private RuleCondition(TokenSet parentType, TokenSet child1Type, TokenSet child2Type) {
+      myParentType = parentType;
+      myChild1Type = child1Type;
+      myChild2Type = child2Type;
+    }
+  }
+  
+  public class RuleBuilder {
+    RuleCondition[] myConditions;
+
+    private RuleBuilder(RuleCondition... conditions) {
+      myConditions = conditions;
+    }
+
+    public SpacingBuilder none() {
+      return spaces(0);
+    }
+    
+    public SpacingBuilder spaceIf(boolean option) {
+      return spaces(option ? 1 : 0);
+    }
+
+    public SpacingBuilder spaces(final int count) {
+      for (RuleCondition condition : myConditions) {
+        myRules.add(new SpacingRule(condition, count, count, 0,
+                                    myCodeStyleSettings.KEEP_LINE_BREAKS, myCodeStyleSettings.KEEP_BLANK_LINES_IN_CODE));
+      }
+      return SpacingBuilder.this;
+    }
+
+    public SpacingBuilder blankLines(int count) {
+      int blankLines = count + 1;
+      for (RuleCondition condition : myConditions) {
+        myRules.add(new SpacingRule(condition, 0, 0, blankLines,
+                                    myCodeStyleSettings.KEEP_LINE_BREAKS, myCodeStyleSettings.KEEP_BLANK_LINES_IN_DECLARATIONS));
+      }
+      return SpacingBuilder.this;
+    }
+
+    public SpacingBuilder spacing(int minSpaces, int maxSpaces, int minLF, boolean keepLineBreaks, int keepBlankLines) {
+      for (RuleCondition condition : myConditions) {
+        myRules.add(new SpacingRule(condition, minSpaces, maxSpaces,  minLF, keepLineBreaks, keepBlankLines));
+      }
+      return SpacingBuilder.this;
+    }
+  }
+  
+  private final CodeStyleSettings myCodeStyleSettings;
+  private final List<SpacingRule> myRules = new ArrayList<SpacingRule>();
+
+  public SpacingBuilder(CodeStyleSettings codeStyleSettings) {
+    myCodeStyleSettings = codeStyleSettings;
+  }
+
+  public RuleBuilder after(IElementType elementType) {
+    return new RuleBuilder(new RuleCondition(null, TokenSet.create(elementType), null));
+  }
+
+  public RuleBuilder afterInside(IElementType elementType, IElementType parentType) {
+    return new RuleBuilder(new RuleCondition(TokenSet.create(parentType), TokenSet.create(elementType), null));
+  }
+
+  public RuleBuilder afterInside(IElementType elementType, TokenSet parentType) {
+    return new RuleBuilder(new RuleCondition(parentType, TokenSet.create(elementType), null));
+  }
+
+  public RuleBuilder afterInside(TokenSet tokenSet, IElementType parentType) {
+    return new RuleBuilder(new RuleCondition(TokenSet.create(parentType), tokenSet, null));
+  }
+
+  public RuleBuilder before(IElementType elementType) {
+    return new RuleBuilder(new RuleCondition(null, null, TokenSet.create(elementType)));
+  }
+
+  public RuleBuilder beforeInside(TokenSet tokenSet, IElementType parentType) {
+    return new RuleBuilder(new RuleCondition(TokenSet.create(parentType), null, tokenSet));
+  }
+
+  public RuleBuilder beforeInside(IElementType elementType, IElementType parentType) {
+    return new RuleBuilder(new RuleCondition(TokenSet.create(parentType), null, TokenSet.create(elementType)));
+  }
+
+  public RuleBuilder between(IElementType left, IElementType right) {
+    return new RuleBuilder(new RuleCondition(null, TokenSet.create(left), TokenSet.create(right)));
+  }
+
+  public RuleBuilder between(IElementType left, TokenSet rightSet) {
+    return new RuleBuilder(new RuleCondition(null, TokenSet.create(left), rightSet));
+  }
+
+  public RuleBuilder between(TokenSet leftSet, IElementType right) {
+    return new RuleBuilder(new RuleCondition(null, leftSet, TokenSet.create(right)));
+  }
+
+  public RuleBuilder between(TokenSet leftType, TokenSet rightType) {
+    return new RuleBuilder(new RuleCondition(null, leftType, rightType));
+  }
+
+  public RuleBuilder betweenInside(IElementType leftType, IElementType rightType, IElementType parentType) {
+    return new RuleBuilder(new RuleCondition(TokenSet.create(parentType), TokenSet.create(leftType), TokenSet.create(rightType)));
+  }
+
+  public RuleBuilder betweenInside(TokenSet leftSet, TokenSet rightSet, IElementType parentType) {
+    return new RuleBuilder(new RuleCondition(TokenSet.create(parentType), leftSet, rightSet));
+  }
+
+  public RuleBuilder withinPair(IElementType pairFirst, IElementType pairSecond) {
+    RuleCondition before = new RuleCondition(null, TokenSet.create(pairFirst), null);
+    RuleCondition after = new RuleCondition(null, null, TokenSet.create(pairSecond));
+    return new RuleBuilder(before, after);
+  }
+
+  public RuleBuilder withinPairInside(IElementType pairFirst, IElementType pairSecond, IElementType parent) {
+    TokenSet parentSet = TokenSet.create(parent);
+    RuleCondition before = new RuleCondition(parentSet, TokenSet.create(pairFirst), null);
+    RuleCondition after = new RuleCondition(parentSet, null, TokenSet.create(pairSecond));
+    return new RuleBuilder(before, after);
+  }
+
+  public RuleBuilder around(IElementType elementType) {
+    return around(TokenSet.create(elementType));
+  }
+
+  public RuleBuilder around(TokenSet tokenSet) {
+    RuleCondition before = new RuleCondition(null, null, tokenSet);
+    RuleCondition after = new RuleCondition(null, tokenSet, null);
+    return new RuleBuilder(before, after);
+  }
+  
+  public RuleBuilder aroundInside(TokenSet tokenSet, TokenSet parent) {
+    RuleCondition before = new RuleCondition(parent, null, tokenSet);
+    RuleCondition after = new RuleCondition(parent, tokenSet, null);
+    return new RuleBuilder(before, after);
+  }
+
+  public RuleBuilder aroundInside(TokenSet tokenSet, IElementType parent) {
+    RuleCondition before = new RuleCondition(TokenSet.create(parent), null, tokenSet);
+    RuleCondition after = new RuleCondition(TokenSet.create(parent), tokenSet, null);
+    return new RuleBuilder(before, after);
+  }
+
+  public RuleBuilder aroundInside(IElementType token, IElementType parent) {
+    final TokenSet tokenSet = TokenSet.create(token);
+    RuleCondition before = new RuleCondition(TokenSet.create(parent), null, tokenSet);
+    RuleCondition after = new RuleCondition(TokenSet.create(parent), tokenSet, null);
+    return new RuleBuilder(before, after);
+  }
+
+  @Nullable
+  public Spacing getSpacing(Block parent, Block child1, Block child2) {
+    if (!(parent instanceof ASTBlock) || !(child1 instanceof ASTBlock) || !(child2 instanceof ASTBlock)) {
+      return null;
+    }
+    IElementType parentType = ((ASTBlock) parent).getNode().getElementType();
+    IElementType child1Type = ((ASTBlock) child1).getNode().getElementType();
+    IElementType child2Type = ((ASTBlock) child2).getNode().getElementType();
+    for (SpacingRule rule : myRules) {
+      if (rule.matches(parentType, child1Type, child2Type)) {
+        return rule.createSpacing();
+      }
+    }
+    return null;
+  }
+}
