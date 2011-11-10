7cd102c69a0182d4aa6f930fb7ea8421a5bad3a0
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2011-11-10 18:12:56 +0100

    fixed PY-4357 Tuple assignment balance is incorrect: inspection doesn't affect variables

--- python/testData/inspections/PyTupleAssignmentBalanceInspection3/test.py
+++ python/testData/inspections/PyTupleAssignmentBalanceInspection3/test.py
@@ -1,1 +1,5 @@
-a, b, c = (<warning descr="Too many values to unpack">1, 2, 3, 4</warning>)
+a, b, c = <warning descr="Too many values to unpack">(1, 2, 3, 4)</warning>
+
+#PY-4357
+c = 1, 2, 3
+a, b = <warning descr="Too many values to unpack">c</warning>
