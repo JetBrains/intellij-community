e13971c5a431dfcf00b40e677baf69fde3d87c77
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2011-11-13 14:58:34 +0100

    @Nullable

--- java/java-psi-impl/src/com/intellij/lang/java/parser/FileParser.java
+++ java/java-psi-impl/src/com/intellij/lang/java/parser/FileParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2010 JetBrains s.r.o.
+ * Copyright 2000-2011 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -105,6 +105,7 @@ public class FileParser {
     }
   }
 
+  @Nullable
   protected PsiBuilder.Marker parseInitial(PsiBuilder builder) {
     return myDeclarationParser.parse(builder, DeclarationParser.Context.FILE);
   }
