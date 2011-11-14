0e0672465f7c75582cffa1faaca4bf8684a8ce28
peter <peter@jetbrains.com>
2011-11-14 17:47:25 +0100

    [Hamlet & Brice @ hackergarten] split into 2 if's intention in groovy

---
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/intentions/SplitIfTest.groovy
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2000-2011 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jetbrains.plugins.groovy.intentions
+
+/**
+ * @author Brice Dutheil
+ * @author Hamlet D'Arcy
+ */
+class SplitIfTest extends GrIntentionTestCase {
+  public void test_that_two_binary_operand_are_split_into_2_if_statements() throws Exception {
+    doTextTest '''if(a <caret>&& b) {
+  c();
+}
+''',
+'Split into 2 if\'s',
+'''if (a) {
+    if (b) {
+        c();
+    }
+}
+'''
+  }
+
+
+public void test_that_two_binary_operand_are_not_split_when_if_statements_has_else_branch() throws Exception {
+    doAntiTest '''if(a <caret>&& b) {
+  c();
+} else {
+  d();
+}
+''',
+'Split into 2 if\'s'
+  }
+}
