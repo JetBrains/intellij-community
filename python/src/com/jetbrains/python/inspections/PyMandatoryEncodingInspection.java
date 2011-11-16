71675ccee3e7b5270da8effc88171907d903bb5e
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2011-11-16 14:28:15 +0100

    fixed PY-4735 "mandatory encoding declaration" inspection+quick fix on python files

---
+++ python/src/com/jetbrains/python/inspections/PyMandatoryEncodingInspection.java
@@ -0,0 +1,87 @@
+package com.jetbrains.python.inspections;
+
+import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiElementVisitor;
+import com.jetbrains.python.PyBundle;
+import com.jetbrains.python.PythonFileType;
+import com.jetbrains.python.actions.AddEncodingQuickFix;
+import com.jetbrains.python.psi.PyFile;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * User : catherine
+ */
+public class PyMandatoryEncodingInspection extends PyInspection {
+  @Nls
+  @NotNull
+  @Override
+  public String getDisplayName() {
+    return PyBundle.message("INSP.NAME.mandatory.encoding");
+  }
+
+  @Override
+  public boolean isEnabledByDefault() {
+    return true;
+  }
+
+  @NotNull
+  @Override
+  public PsiElementVisitor buildVisitor(@NotNull ProblemsHolder holder, boolean isOnTheFly) {
+    return new Visitor(holder);
+  }
+
+  private class Visitor extends PyInspectionVisitor {
+
+    public Visitor(final ProblemsHolder holder) {
+      super(holder);
+    }
+
+    @Override
+    public void visitPyFile(PyFile node) {
+      final String charsetString = PythonFileType.getCharsetFromEncodingDeclaration(node.getText());
+      if (charsetString == null) {
+        TextRange tr = new TextRange(0,0);
+        ProblemsHolder holder = getHolder();
+        if (holder != null)
+          holder.registerProblem(node, tr, "No encoding specified for file", new AddEncodingQuickFix(myDefaultEncoding,
+                                                                                                     myEncodingFormatIndex));
+      }
+    }
+  }
+
+  public String myDefaultEncoding = "utf-8";
+  public int myEncodingFormatIndex = 0;
+
+  @Override
+  public JComponent createOptionsPanel() {
+    final JComboBox defaultEncoding = new JComboBox(PyEncodingUtil.POSSIBLE_ENCODINGS);
+    defaultEncoding.setSelectedItem(myDefaultEncoding);
+
+    defaultEncoding.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        JComboBox cb = (JComboBox)e.getSource();
+        myDefaultEncoding = (String)cb.getSelectedItem();
+      }
+    });
+
+    final JComboBox encodingFormat = new JComboBox(PyEncodingUtil.ENCODING_FORMAT);
+
+    encodingFormat.setSelectedIndex(myEncodingFormatIndex);
+    encodingFormat.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        JComboBox cb = (JComboBox)e.getSource();
+        myEncodingFormatIndex = cb.getSelectedIndex();
+      }
+    });
+
+    return PyEncodingUtil.createEncodingOptionsPanel(defaultEncoding, encodingFormat);
+  }
+}
