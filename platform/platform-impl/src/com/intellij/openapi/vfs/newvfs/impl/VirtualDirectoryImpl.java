e88b5db50c426dd1b0e45282871d8d0f5ba51a3d
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2011-11-17 17:03:24 +0100

    Renamed env name.

--- platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/impl/VirtualDirectoryImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/impl/VirtualDirectoryImpl.java
@@ -182,7 +182,7 @@ public class VirtualDirectoryImpl extends VirtualFileSystemEntry {
 
   private static final boolean IS_UNDER_TEAMCITY = System.getProperty("bootstrap.testcases") != null;
 
-  private static final boolean SHOULD_PERFORM_ACCESS_CHECK = System.getenv("should.not.perform.access.check") == null;
+  private static final boolean SHOULD_PERFORM_ACCESS_CHECK = System.getenv("NO_FS_ROOTS_ACCESS_CHECK") == null;
 
   private static final boolean IS_UNIT_TESTS = ApplicationManager.getApplication().isUnitTestMode();
 
