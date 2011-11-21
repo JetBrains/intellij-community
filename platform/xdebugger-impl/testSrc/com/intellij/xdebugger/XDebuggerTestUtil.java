746af827c9b0794d4b8f684146f929166e4abcfd
andrey.zaytsev <andrey.zaytsev@jetbrains.com>
2011-11-21 17:58:01 +0300

    Debugger: tests

--- platform/xdebugger-impl/testSrc/com/intellij/xdebugger/XDebuggerTestUtil.java
+++ platform/xdebugger-impl/testSrc/com/intellij/xdebugger/XDebuggerTestUtil.java
@@ -32,6 +32,7 @@ import com.intellij.xdebugger.impl.breakpoints.XBreakpointUtil;
 import com.intellij.xdebugger.impl.breakpoints.XLineBreakpointImpl;
 import com.intellij.xdebugger.ui.DebuggerIcons;
 import junit.framework.Assert;
+import org.intellij.lang.annotations.Language;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -165,6 +166,7 @@ public class XDebuggerTestUtil {
     assertVariableValue(findVar(vars, name), name, value);
   }
 
+<<<<<<< HEAD
   public static void assertVariableValueMatches(Collection<XValue> vars,
                                                 @Nullable String name,
                                                 @Nullable String valuePattern) throws InterruptedException {
@@ -186,6 +188,38 @@ public class XDebuggerTestUtil {
     if (type != null) Assert.assertEquals(type, node.myType);
     if (valuePattern != null)
       Assert.assertTrue("Expected value" + valuePattern + " Actual value: " + node.myValue, node.myValue.matches(valuePattern));
+=======
+  public static void assertVariableValueMatches(Collection<XValue> vars, @Nullable String name, @Language("RegExp") String valuePattern)
+    throws InterruptedException {
+    assertVariableValueMatches(findVar(vars, name), name, valuePattern);
+  }
+
+  public static void assertVariableValueMatches(XValue var, @Nullable String name, @Language("RegExp") String valuePattern) throws InterruptedException {
+    assertVariableValueMatches(var, name, null, valuePattern);
+  }
+
+  public static void assertVariableValueMatches(XValue var, @Nullable String name, String type, @Language("RegExp") String valuePattern) throws InterruptedException {
+    XTestValueNode node = computePresentation(var);
+    if (name != null) {
+      Assert.assertEquals(name, node.myName);
+    }
+    if (type != null) {
+      Assert.assertEquals(type, node.myType);
+    }
+    if (valuePattern != null) {
+      Assert.assertTrue("Expected value" + valuePattern + " Actual value: " + node.myValue, node.myValue.matches(valuePattern));
+    }
+  }
+
+  public static void assertVariableTypeMatches(XValue var, String name, @Language("RegExp") String typePattern) throws InterruptedException {
+    XTestValueNode node = computePresentation(var);
+    if (name != null) {
+      Assert.assertEquals(name, node.myName);
+    }
+    if (typePattern != null) {
+      Assert.assertTrue("Expected type" + typePattern + " Actual type: " + node.myType, node.myType.matches(typePattern));
+    }
+>>>>>>> Debugger: tests
   }
 
   public static void assertVariableFullValue(XValue var, @Nullable String value) throws InterruptedException {
