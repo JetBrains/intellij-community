a0b40c7085be9e143f177f3309007c728469f2a3
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2011-11-22 14:55:42 +0400

    Fixed bug in handling union types in argument list inspection (PY-4968)

--- python/testData/inspections/PyArgumentListInspection/args.py
+++ python/testData/inspections/PyArgumentListInspection/args.py
@@ -1,3 +1,11 @@
 # PY-5057
 def test_iterable_not_sequence():
     print os.path.join(*xrange(10)) #pass
+
+
+# PY-4968
+def test_union_type():
+    def foo(*args):
+        pass
+    bar = (1,) if True else (1, 2)
+    foo(*bar) #pass
