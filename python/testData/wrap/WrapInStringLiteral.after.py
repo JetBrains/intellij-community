8b7334d370dc48187765c49289d3d51aa3b351e4
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2011-11-23 19:23:39 +0100

    fixed test data for string literal wrap

--- python/testData/wrap/WrapInStringLiteral.after.py
+++ python/testData/wrap/WrapInStringLiteral.after.py
@@ -3,5 +3,5 @@ class Test:
         pass
 
     def someFunction(self):
-       self.execQueries("SELECT field1, field2, field3 FROM %s WHERE 1=1 AND " \
+       self.execQueries("SELECT field1, field2, field3 FROM %s WHERE 1=1 AND "
                         "field")
