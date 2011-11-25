e7fe08a983e3483ee62e61b82f54daf068ed2c06
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2011-11-25 16:36:50 +0400

    Extensible Smart Step Into. (reviewed by: Jeka)

--- java/debugger/impl/src/com/intellij/debugger/actions/JavaSmartStepIntoHandler.java
+++ java/debugger/impl/src/com/intellij/debugger/actions/JavaSmartStepIntoHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2011 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,75 +15,33 @@
  */
 package com.intellij.debugger.actions;
 
-import com.intellij.debugger.DebuggerManagerEx;
 import com.intellij.debugger.SourcePosition;
-import com.intellij.debugger.engine.RequestHint;
-import com.intellij.debugger.engine.SuspendContextImpl;
-import com.intellij.debugger.impl.DebuggerContextImpl;
-import com.intellij.debugger.impl.DebuggerSession;
-import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.lang.java.JavaLanguage;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
-import com.intellij.openapi.fileEditor.FileEditor;
-import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.fileEditor.TextEditor;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.popup.JBPopupFactory;
-import com.intellij.openapi.ui.popup.ListPopup;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.*;
-import com.intellij.ui.awt.RelativePoint;
 import com.intellij.util.containers.OrderedSet;
 import com.intellij.util.text.CharArrayUtil;
-import com.intellij.xdebugger.impl.actions.DebuggerActionHandler;
-import com.intellij.xdebugger.impl.ui.DebuggerUIUtil;
 import gnu.trove.TObjectHashingStrategy;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.Collections;
 import java.util.List;
 
-public class SmartStepIntoActionHandler extends DebuggerActionHandler {
-  public void perform(@NotNull final Project project, final AnActionEvent event) {
-    final DebuggerContextImpl debuggerContext = (DebuggerManagerEx.getInstanceEx(project)).getContext();
-    doStep(project, debuggerContext.getSourcePosition(), debuggerContext.getDebuggerSession());
+/**
+ * User: Alexander Podkhalyuzin
+ * Date: 22.11.11
+ */
+public class JavaSmartStepIntoHandler extends JvmSmartStepIntoHandler {
+  @Override
+  public boolean isAvailable(final SourcePosition position) {
+    final PsiFile file = position.getFile();
+    return file.getLanguage().isKindOf(JavaLanguage.INSTANCE);
   }
 
-  
-  private static void doStep(final @NotNull Project project, final @Nullable SourcePosition position, final @NotNull DebuggerSession session) {
-    final VirtualFile file = position != null ? position.getFile().getVirtualFile() : null;
-    final FileEditor fileEditor = file != null? FileEditorManager.getInstance(project).getSelectedEditor(file) : null;
-    if (fileEditor instanceof TextEditor) {
-      final List<PsiMethod> methods = findReferencedMethods(position);
-      if (methods.size() > 0) {
-        if (methods.size() == 1) {
-          session.stepInto(true, createSmartStepFilter(methods.get(0)));
-        }
-        else {
-          final PsiMethodListPopupStep popupStep = new PsiMethodListPopupStep(methods, new PsiMethodListPopupStep.OnChooseRunnable() {
-            public void execute(PsiMethod chosenMethod) {
-              session.stepInto(true, createSmartStepFilter(chosenMethod));
-            }
-          });
-          final ListPopup popup = JBPopupFactory.getInstance().createListPopup(popupStep);
-          final RelativePoint point = DebuggerUIUtil.calcPopupLocation(((TextEditor)fileEditor).getEditor(), position.getLine());
-          popup.show(point);
-        }
-        return;
-      }
-    }
-    session.stepInto(true, null);
-  }
-
-  @Nullable
-  private static RequestHint.SmartStepFilter createSmartStepFilter(final PsiMethod method) {
-    return new RequestHint.SmartStepFilter(method);
-  }
-
-
-  private static List<PsiMethod> findReferencedMethods(final SourcePosition position) {
+  @Override
+  public List<PsiMethod> findReferencedMethods(final SourcePosition position) {
     final int line = position.getLine();
     if (line < 0) {
       return Collections.emptyList(); // the document has been changed
@@ -95,8 +53,9 @@ public class SmartStepIntoActionHandler extends DebuggerActionHandler {
       // the file is not physical
       return Collections.emptyList();
     }
-    
+
     final Document doc = FileDocumentManager.getInstance().getDocument(vFile);
+    if (doc == null) return Collections.emptyList();
     if (line >= doc.getLineCount()) {
       return Collections.emptyList(); // the document has been changed
     }
@@ -143,14 +102,5 @@ public class SmartStepIntoActionHandler extends DebuggerActionHandler {
       return methods;
     }
     return Collections.emptyList();
-  }
-
-  public boolean isEnabled(@NotNull final Project project, final AnActionEvent event) {
-    final DebuggerContextImpl context = (DebuggerManagerEx.getInstanceEx(project)).getContext();
-    DebuggerSession debuggerSession = context.getDebuggerSession();
-    final boolean isPaused = debuggerSession != null && debuggerSession.isPaused();
-    final SuspendContextImpl suspendContext = context.getSuspendContext();
-    final boolean hasCurrentThread = suspendContext != null && suspendContext.getThread() != null;
-    return isPaused && hasCurrentThread;
   }
 }
