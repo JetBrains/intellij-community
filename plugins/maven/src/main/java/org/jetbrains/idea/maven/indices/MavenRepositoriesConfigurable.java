fae22c6be8fb0991e29ae746ea268eb9a6a3e159
Gregory.Shrago <gregory.shrago@jetbrains.com>
2011-11-25 20:52:33 +0300

    [nik] gson & jaxb libraries updated IDEA-75076 IDEA-73020

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/indices/MavenRepositoriesConfigurable.java
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/indices/MavenRepositoriesConfigurable.java
@@ -168,10 +168,10 @@ public class MavenRepositoriesConfigurable extends BaseConfigurable implements S
         else {
           final StringBuilder sb = new StringBuilder();
           sb.append(infos.size()).append(infos.size() == 1 ? "repository" : " repositories").append(" found");
-          for (MavenRepositoryInfo info : infos) {
-            sb.append("\n  ");
-            sb.append(info.getId()).append(" (").append(info.getName()).append(")").append(": ").append(info.getUrl());
-          }
+          //for (MavenRepositoryInfo info : infos) {
+          //  sb.append("\n  ");
+          //  sb.append(info.getId()).append(" (").append(info.getName()).append(")").append(": ").append(info.getUrl());
+          //}
           Messages.showMessageDialog(sb.toString(), "Service Connection Successful", Messages.getInformationIcon());
         }
         return true;
