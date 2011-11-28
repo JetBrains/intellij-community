05b558d793d8d62b418c82af0fbfd688595429f1
Sascha Weinreuter <sascha.weinreuter@gmail.com>
2011-11-28 12:19:01 +0100

    IDEA-77665 XSLT debug: IOOBE at SegmentArray.getSegmentStart() on deleting file content while debugging

--- plugins/xslt-debugger/src/org/intellij/plugins/xsltDebugger/impl/XsltBreakpointHandler.java
+++ plugins/xslt-debugger/src/org/intellij/plugins/xsltDebugger/impl/XsltBreakpointHandler.java
@@ -164,7 +164,7 @@ public class XsltBreakpointHandler extends XBreakpointHandler<XLineBreakpoint<XB
 
     int offset = -1;
     final Document document = PsiDocumentManager.getInstance(project).getDocument(file);
-    if (document != null) {
+    if (document != null && document.getLineCount() > position.getLine()) {
       offset = document.getLineStartOffset(position.getLine());
     }
     if (offset < 0) {
