b13eca78295d3d2d46b5a64039524fd29ddaedf3
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2011-11-28 20:12:04 +0100

    fixed PY-5041 Closing pair quote not inserted if caret is before pair of quotes

--- python/src/com/jetbrains/python/editor/BaseQuoteHandler.java
+++ python/src/com/jetbrains/python/editor/BaseQuoteHandler.java
@@ -3,8 +3,8 @@ package com.jetbrains.python.editor;
 import com.intellij.codeInsight.editorActions.SimpleTokenSetQuoteHandler;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.highlighter.HighlighterIterator;
+import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.tree.TokenSet;
-import com.jetbrains.python.PyTokenTypes;
 
 import java.util.Arrays;
 
@@ -14,7 +14,7 @@ import java.util.Arrays;
 public class BaseQuoteHandler extends SimpleTokenSetQuoteHandler {
 
   private final char[] ourAutoClosingChars; // we add auto-close quotes before these
-  
+
   public BaseQuoteHandler(TokenSet tokenSet, char[] autoClosingChars) {
     super(tokenSet);
     ourAutoClosingChars = autoClosingChars;
@@ -29,15 +29,17 @@ public class BaseQuoteHandler extends SimpleTokenSetQuoteHandler {
       return false;
     }
     CharSequence text = document.getCharsSequence();
-    if (offset+1 >= text.length() || Arrays.binarySearch(ourAutoClosingChars, text.charAt(offset+1)) >= 0) {
+    if (offset + 1 >= text.length() || Arrays.binarySearch(ourAutoClosingChars, text.charAt(offset + 1)) >= 0) {
       char the_quote = text.charAt(offset);
       // if we're next to two same quotes, don't auto-close, the user may want a triple quote
       if (
         offset >= 2 &&
-        text.charAt(offset-1) == the_quote &&
-        text.charAt(offset-2) == the_quote &&
-        (offset < 3 || text.charAt(offset-3) != the_quote)
-      ) return false;
+        text.charAt(offset - 1) == the_quote &&
+        text.charAt(offset - 2) == the_quote &&
+        (offset < 3 || text.charAt(offset - 3) != the_quote)
+        ) {
+        return false;
+      }
       // handle string literal context
       if (super.isOpeningQuote(iterator, offset)) {
         return true;
@@ -68,6 +70,27 @@ public class BaseQuoteHandler extends SimpleTokenSetQuoteHandler {
   protected boolean isNonClosedLiteral(HighlighterIterator iterator, CharSequence chars) {
     if (getLiteralStartOffset(chars, iterator.getStart()) >= iterator.getEnd() - 1) return true;
     if (chars.charAt(iterator.getEnd() - 1) != '\"' && chars.charAt(iterator.getEnd() - 1) != '\'') return true;
+    return false;
+  }
+
+  @Override
+  public boolean isClosingQuote(HighlighterIterator iterator, int offset) {
+    final IElementType tokenType = iterator.getTokenType();
+    if (myLiteralTokenSet.contains(tokenType)) {
+      int start = iterator.getStart();
+      int end = iterator.getEnd();
+      if (end - start >= 1 && offset == end - 1) {
+        Document doc = iterator.getDocument();
+        CharSequence chars = doc.getCharsSequence();
+        if (chars.length() > offset + 1) {
+          Character ch = chars.charAt(offset + 1);
+          if (Arrays.binarySearch(ourAutoClosingChars, ch) < 0)
+            return false;
+        }
+        return true;
+      }
+    }
+
     return false;
   }
 }
