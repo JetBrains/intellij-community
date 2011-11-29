29ef28277f07ae5b94e80a49424963cb90e7f470
Maxim Shafirov <max@jetbrains.com>
2011-11-29 20:59:11 +0400

    [r=kirillk] pinch zoom gesture for the editor

--- platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/MouseGestureManager.java
+++ platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/MouseGestureManager.java
@@ -15,22 +15,25 @@
  */
 package com.intellij.openapi.actionSystem.impl;
 
+import com.apple.eawt.event.*;
+import com.intellij.ide.DataManager;
 import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.colors.EditorColorsManager;
+import com.intellij.openapi.editor.ex.EditorEx;
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.wm.IdeFrame;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -39,7 +42,9 @@ public class MouseGestureManager implements ApplicationComponent {
   private static final Logger LOG = Logger.getInstance("MouseGestureManager");
   private ActionManagerImpl myActionManager;
 
-  private Map<IdeFrame, Object> myListeners = new HashMap<IdeFrame, Object>();
+  private Map<IdeFrame, GestureListener> myListeners = new HashMap<IdeFrame, GestureListener>();
+  private boolean HAS_TRACKPAD = false;
+  
 
   public MouseGestureManager(ActionManagerImpl actionManager) {
     myActionManager = actionManager;
@@ -54,28 +59,70 @@ public class MouseGestureManager implements ApplicationComponent {
           remove(frame);
         }
 
-        Class<?> gestureListenerClass = Class.forName("com.apple.eawt.event.GestureListener");
-        Class<?> swipeListenerClass = Class.forName("com.apple.eawt.event.SwipeListener");
-        Object listener = Proxy.newProxyInstance(getClass().getClassLoader(), new Class[]{swipeListenerClass}, new InvocationHandler() {
+        GestureAdapter listener = new GestureAdapter() {
+          double magnification = 0;
           @Override
-          public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            if ("swipedRight".equals(method.getName())) {
-              processRightSwipe(frame);
-            } else if ("swipedLeft".equals(method.getName())) {
-              processLeftSwipe(frame);
-            }
-            return null;
+          public void gestureBegan(GesturePhaseEvent event) {
+            activateTrackpad();
+            magnification = 0;
           }
-        });
 
-        Class<?> utilsClass = Class.forName("com.apple.eawt.event.GestureUtilities");
-        Method addMethod = utilsClass.getDeclaredMethod("addGestureListenerTo", JComponent.class, gestureListenerClass);
-        addMethod.invoke(null, frame.getComponent(), listener);
+          @Override
+          public void gestureEnded(GesturePhaseEvent event) {
+            activateTrackpad();
+            if (magnification != 0) {
+              processMagnification(frame, magnification);
+              magnification = 0;
+            }
+          }
+
+          @Override
+          public void swipedLeft(SwipeEvent event) {
+            activateTrackpad();
+            processLeftSwipe(frame);
+          }
+
+          @Override
+          public void swipedRight(SwipeEvent event) {
+            activateTrackpad();
+            processRightSwipe(frame);
+          }
+
+          @Override
+          public void magnify(MagnificationEvent event) {
+            activateTrackpad();
+            magnification += event.getMagnification();
+          }
+        };
+
+        GestureUtilities.addGestureListenerTo(frame.getComponent(), listener);
 
         myListeners.put(frame, listener);
       }
-      catch (Exception e) {
+      catch (Throwable e) {
         LOG.debug(e);
+      }
+    }
+  }
+
+  private void activateTrackpad() {
+    HAS_TRACKPAD = true;
+  }
+  
+  public boolean hasTrackpad() {
+    return HAS_TRACKPAD;
+  }
+
+  private static void processMagnification(IdeFrame frame, double magnification) {
+    Point mouse = MouseInfo.getPointerInfo().getLocation();
+    SwingUtilities.convertPointFromScreen(mouse, frame.getComponent());
+    Component componentAt = SwingUtilities.getDeepestComponentAt(frame.getComponent(), mouse.x, mouse.y);
+    if (componentAt != null) {
+      Editor editor = PlatformDataKeys.EDITOR.getData(DataManager.getInstance().getDataContext(componentAt));
+      if (editor != null) {
+        double currentSize = editor.getColorsScheme().getEditorFontSize();
+        int defaultFontSize = EditorColorsManager.getInstance().getGlobalScheme().getEditorFontSize();
+        ((EditorEx)editor).setFontSize((int)(Math.max(currentSize + magnification * 3, defaultFontSize)));
       }
     }
   }
@@ -94,7 +141,7 @@ public class MouseGestureManager implements ApplicationComponent {
     myActionManager.tryToExecute(back, createMouseEventWrapper(frame), null, null, false);
   }
 
-  private MouseEvent createMouseEventWrapper(IdeFrame frame) {
+  private static MouseEvent createMouseEventWrapper(IdeFrame frame) {
     return new MouseEvent(frame.getComponent(), ActionEvent.ACTION_PERFORMED, System.currentTimeMillis(), 0, 0, 0, 0, false, 0);
   }
 
@@ -103,17 +150,14 @@ public class MouseGestureManager implements ApplicationComponent {
 
     if (SystemInfo.isMacOSSnowLeopard) {
       try {
-        Object listener = myListeners.get(frame);
+        GestureListener listener = myListeners.get(frame);
         JComponent cmp = frame.getComponent();
         myListeners.remove(frame);
         if (listener != null && cmp != null && cmp.isShowing()) {
-          Class<?> gestureListenerClass = Class.forName("com.apple.eawt.event.GestureListener");
-          Class<?> utilsClass = Class.forName("com.apple.eawt.event.GestureUtilities");
-          Method addMethod = utilsClass.getDeclaredMethod("removeGestureListenerFrom", JComponent.class, gestureListenerClass);
-          addMethod.invoke(null, cmp, listener);
+          GestureUtilities.removeGestureListenerFrom(cmp, listener);
         }
       }
-      catch (Exception e) {
+      catch (Throwable e) {
         LOG.debug(e);
       }
     }
