adfb963b7e2c214886ebbad0cb4809cecf61a920
Sascha Weinreuter <sascha.weinreuter@gmail.com>
2011-11-30 09:53:07 +0100

    IDEA-77666: XSLT debug: Step Out leaves the session in an incomplete state (Step Out not supported yet, does a normal step)(cherry picked from commit 11a3bd0)

--- plugins/xslt-debugger/src/org/intellij/plugins/xsltDebugger/impl/XsltDebugProcess.java
+++ plugins/xslt-debugger/src/org/intellij/plugins/xsltDebugger/impl/XsltDebugProcess.java
@@ -21,8 +21,8 @@ import org.intellij.plugins.xsltDebugger.VMPausedException;
 import org.intellij.plugins.xsltDebugger.XsltDebuggerSession;
 import org.intellij.plugins.xsltDebugger.rt.engine.Breakpoint;
 import org.intellij.plugins.xsltDebugger.rt.engine.BreakpointManager;
-import org.intellij.plugins.xsltDebugger.rt.engine.Debugger;
 import org.intellij.plugins.xsltDebugger.rt.engine.BreakpointManagerImpl;
+import org.intellij.plugins.xsltDebugger.rt.engine.Debugger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -129,6 +129,7 @@ public class XsltDebugProcess extends XDebugProcess implements Disposable {
 
   @Override
   public void startStepOut() {
+    myDebuggerSession.stepOver();
   }
 
   @Override
