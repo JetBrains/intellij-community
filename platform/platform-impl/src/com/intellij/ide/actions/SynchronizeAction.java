8ae412ff335cb5669e98ac82f46a935cd8c8f223
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2011-11-30 11:23:24 +0100

    Cleanup

--- platform/platform-impl/src/com/intellij/ide/actions/SynchronizeAction.java
+++ platform/platform-impl/src/com/intellij/ide/actions/SynchronizeAction.java
@@ -1,6 +1,5 @@
-
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2011 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,6 +27,7 @@ public class SynchronizeAction extends AnAction implements DumbAware {
     FileDocumentManager.getInstance().saveAllDocuments();
 
     SaveAndSyncHandler.refreshOpenFiles();
+
     VirtualFileManager.getInstance().refreshWithoutFileWatcher(true);
   }
 }
