510aee8823aa36f70fba9752f650d5820371690f
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2011-12-05 17:55:43 +0400

    Tree-based PyClassImpl instance may not have a parent element (EA-32381)

--- python/src/com/jetbrains/python/psi/impl/PyClassImpl.java
+++ python/src/com/jetbrains/python/psi/impl/PyClassImpl.java
@@ -629,7 +629,8 @@ public class PyClassImpl extends PyPresentableElementImpl<PyClassStub> implement
   @Nullable
   private Property scanProperties(@Nullable String name, @Nullable Processor<Property> filter, boolean inherited) {
     LanguageLevel level = LanguageLevel.getDefault();
-    final PsiFile file = getContainingFile();
+    // EA-32381: A tree-based instance may not have a parent element somehow, so getContainingFile() may be not appropriate
+    final PsiFile file = getParentByStub() != null ? getContainingFile() : null;
     if (file != null) {
       final VirtualFile vfile = file.getVirtualFile();
       if (vfile != null) {
