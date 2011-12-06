150634f562b5cb3905923813b1f0d585bbc1d0ea
Dmitry Jemerov <yole@jetbrains.com>
2011-12-06 16:01:59 +0100

    logging in PyProjectStructureDetector

--- python/pluginSrc/com/jetbrains/python/module/PyProjectStructureDetector.java
+++ python/pluginSrc/com/jetbrains/python/module/PyProjectStructureDetector.java
@@ -5,6 +5,7 @@ import com.intellij.ide.util.importProject.ProjectDescriptor;
 import com.intellij.ide.util.projectWizard.importSources.DetectedProjectRoot;
 import com.intellij.ide.util.projectWizard.importSources.ProjectFromSourcesBuilder;
 import com.intellij.ide.util.projectWizard.importSources.ProjectStructureDetector;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.extensions.Extensions;
 import com.intellij.openapi.util.io.FileUtil;
 import org.jetbrains.annotations.NotNull;
@@ -18,14 +19,19 @@ import java.util.List;
  * @author yole
  */
 public class PyProjectStructureDetector extends ProjectStructureDetector {
+  private static final Logger LOG = Logger.getInstance("#com.jetbrains.python.module.PyProjectStructureDetector"); 
+  
+  
   @NotNull
   @Override
   public DirectoryProcessingResult detectRoots(@NotNull File dir,
                                                @NotNull File[] children,
                                                @NotNull File base,
                                                @NotNull List<DetectedProjectRoot> result) {
+    LOG.info("Detecting roots under "  + dir);
     for (File child : children) {
       if (FileUtil.getExtension(child.getName()).equals("py")) {
+        LOG.info("Found Python file " + child.getPath());
         result.add(new DetectedProjectRoot(dir) {
           @NotNull
           @Override
