fbcd1cc24fc95960b7816f5a7c1c949e051a273e
Anton Makeev <Anton.Makeev@jetbrains.com>
2011-12-08 16:00:33 +0100

    Maven  import notifications improved (cherry picked from commit bee32f6fcfd74c04f2bc9b2db672bf06f7775db1)

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/utils/MavenSimpleProjectComponent.java
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/utils/MavenSimpleProjectComponent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2011 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,8 +19,8 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.AbstractProjectComponent;
 import com.intellij.openapi.project.Project;
 
-public abstract class SimpleProjectComponent extends AbstractProjectComponent {
-  protected SimpleProjectComponent(Project project) {
+public abstract class MavenSimpleProjectComponent extends AbstractProjectComponent {
+  protected MavenSimpleProjectComponent(Project project) {
     super(project);
   }
 
