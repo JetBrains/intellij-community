11d383c9964d5934f1f24be03106455f6700b136
Denis.Zhdanov <Denis.Zhdanov@jetbrains.com>
2011-12-09 18:54:11 +0400

    IDEA-74108 Editor always scrolled to the top when selected using 'Recent Files' action (Ctrl-e)
    EditorHistoryManager is notified by active editor change after it. The problem is that it's not possible to derive FileEditorProvider for the old editor when no tabs are shown. However, the provider is known during the event construction, so, it's just wrapped there.

--- platform/platform-api/src/com/intellij/openapi/fileEditor/FileEditorManagerEvent.java
+++ platform/platform-api/src/com/intellij/openapi/fileEditor/FileEditorManagerEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2011 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,44 +16,78 @@
 package com.intellij.openapi.fileEditor;
 
 import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.EventObject;
 
 public final class FileEditorManagerEvent extends EventObject {
-  private final VirtualFile myOldFile;
-  private final FileEditor myOldEditor;
-  private final VirtualFile myNewFile;
-  private final FileEditor myNewEditor;
 
-  public FileEditorManagerEvent(FileEditorManager source,
-                                VirtualFile oldFile,
-                                FileEditor oldEditor,
-                                VirtualFile newFile,
-                                FileEditor newEditor) {
+  private final VirtualFile        myOldFile;
+  private final FileEditor         myOldEditor;
+  private final VirtualFile        myNewFile;
+  private final FileEditor         myNewEditor;
+  private final FileEditorProvider myOldProvider;
+  private final FileEditorProvider myNewProvider;
+
+  public FileEditorManagerEvent(@NotNull FileEditorManager source,
+                                @Nullable VirtualFile oldFile,
+                                @Nullable FileEditor oldEditor,
+                                @Nullable VirtualFile newFile,
+                                @Nullable FileEditor newEditor)
+  {
+    this(source, oldFile, oldEditor, null, newFile, newEditor, null);
+  }
+  
+  public FileEditorManagerEvent(@NotNull FileEditorManager source,
+                                @Nullable VirtualFile oldFile,
+                                @Nullable FileEditor oldEditor,
+                                @Nullable FileEditorProvider oldProvider,
+                                @Nullable VirtualFile newFile,
+                                @Nullable FileEditor newEditor,
+                                @Nullable FileEditorProvider newProvider)
+  {
     super(source);
     myOldFile = oldFile;
     myOldEditor = oldEditor;
     myNewFile = newFile;
     myNewEditor = newEditor;
+    myOldProvider = oldProvider;
+    myNewProvider = newProvider;
   }
 
+  @NotNull
   public FileEditorManager getManager(){
     return (FileEditorManager)getSource();
   }
 
+  @Nullable
   public VirtualFile getOldFile() {
     return myOldFile;
   }
 
+  @Nullable
   public VirtualFile getNewFile() {
     return myNewFile;
   }
 
+  @Nullable
   public FileEditor getOldEditor() {
     return myOldEditor;
   }
 
+  @Nullable
   public FileEditor getNewEditor() {
     return myNewEditor;
+  }
+
+  @Nullable
+  public FileEditorProvider getOldProvider() {
+    return myOldProvider;
+  }
+
+  @Nullable
+  public FileEditorProvider getNewProvider() {
+    return myNewProvider;
   }
 }
