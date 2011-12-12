1499785cce67f01d75b4455b5e1ac322053f655b
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2011-12-12 16:31:40 +0400

    fixed PY-5264 Tuple assignment balance false positives for function calls

--- python/src/com/jetbrains/python/inspections/PyTupleAssignmentBalanceInspection.java
+++ python/src/com/jetbrains/python/inspections/PyTupleAssignmentBalanceInspection.java
@@ -6,6 +6,7 @@ import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElementVisitor;
 import com.jetbrains.python.PyBundle;
 import com.jetbrains.python.psi.*;
+import com.jetbrains.python.psi.types.PyReturnTypeReference;
 import com.jetbrains.python.psi.types.PyType;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
@@ -44,7 +45,7 @@ public class PyTupleAssignmentBalanceInspection extends PyInspection {
 
       if (assignedValue == null) return;
       PyType type = assignedValue.getType(myTypeEvalContext);
-      if (lhsExpression instanceof PyTupleExpression && type != null) {
+      if (lhsExpression instanceof PyTupleExpression && type != null && !(type instanceof PyReturnTypeReference)){
         int valuesLength = PyUtil.getElementsCount(assignedValue, myTypeEvalContext);
         PyExpression[] elements = ((PyTupleExpression) lhsExpression).getElements();
 
