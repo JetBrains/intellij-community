19f6df686567899ee58cd54470f65265e182c3d6
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2011-12-12 11:46:07 +0100

    fsnotifier: fix return type

--- native/fsNotifier/linux/inotify.c
+++ native/fsNotifier/linux/inotify.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2010 JetBrains s.r.o.
+ * Copyright 2000-2011 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -339,7 +339,7 @@ static bool process_inotify_event(struct inotify_event* event) {
 
 
 bool process_inotify_input() {
-  size_t len = read(inotify_fd, event_buf, EVENT_BUF_LEN);
+  ssize_t len = read(inotify_fd, event_buf, EVENT_BUF_LEN);
   if (len < 0) {
     userlog(LOG_ERR, "read: %s", strerror(errno));
     return false;
