5998c28caff93d2d2e75f60528286a489614bfcb
Eugene Kudelevsky <Eugene.Kudelevsky@jetbrains.com>
2011-12-14 18:03:34 +0400

    clean up; hide Android action group if there is no facet in project

--- plugins/android/src/org/jetbrains/android/actions/AndroidToolsActionGroup.java
+++ plugins/android/src/org/jetbrains/android/actions/AndroidToolsActionGroup.java
@@ -17,8 +17,8 @@ package org.jetbrains.android.actions;
 
 import com.intellij.facet.ProjectFacetManager;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DataKeys;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.android.facet.AndroidFacet;
 
@@ -28,7 +28,7 @@ import org.jetbrains.android.facet.AndroidFacet;
 public class AndroidToolsActionGroup extends DefaultActionGroup {
   @Override
   public void update(AnActionEvent e) {
-    final Project project = e.getData(DataKeys.PROJECT);
-    e.getPresentation().setEnabled(project != null && ProjectFacetManager.getInstance(project).getFacets(AndroidFacet.ID).size() > 0);
+    final Project project = e.getData(PlatformDataKeys.PROJECT);
+    e.getPresentation().setVisible(project != null && ProjectFacetManager.getInstance(project).getFacets(AndroidFacet.ID).size() > 0);
   }
 }
