6d6acc696250b2a24f4d8691d174836496eeff96
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2011-12-29 13:19:22 +0400

    Fixed parameter analaysis of Python binary operators

--- python/src/com/jetbrains/python/psi/types/PyTypeChecker.java
+++ python/src/com/jetbrains/python/psi/types/PyTypeChecker.java
@@ -336,7 +336,7 @@ public class PyTypeChecker {
           final PyExpression arg = isRight ? expr.getLeftExpression() : expr.getRightExpression();
           final PyExpression receiver = isRight ? expr.getRightExpression() : expr.getLeftExpression();
           final PyParameter[] parameters = function.getParameterList().getParameters();
-          if (parameters.length == 2) {
+          if (parameters.length >= 2) {
             final PyNamedParameter param = parameters[1].getAsNamed();
             if (arg != null && param != null) {
               final Map<PyExpression, PyNamedParameter> arguments = new LinkedHashMap<PyExpression, PyNamedParameter>();
