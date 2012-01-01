2841c3eb28e1c144ba3cd73b0ee737b788a9d8f7
peter <peter@jetbrains.com>
2012-01-01 18:25:10 +0100

    select word to honor camel-humps in java string literals, treat $ as word separator

--- platform/lang-impl/src/com/intellij/codeInsight/editorActions/SelectWordUtil.java
+++ platform/lang-impl/src/com/intellij/codeInsight/editorActions/SelectWordUtil.java
@@ -89,27 +89,11 @@ public class SelectWordUtil {
       int end = cursorOffset + 1;
       final int textLen = editorText.length();
 
-      while (start > 0 && Character.isJavaIdentifierPart(editorText.charAt(start - 1))) {
-        final char prevChar = editorText.charAt(start - 1);
-        final char curChar = editorText.charAt(start);
-        final char nextChar = start + 1 < textLen ? editorText.charAt(start + 1) : 0; // 0x00 is not lowercase.
-
-        if (Character.isLowerCase(prevChar) && Character.isUpperCase(curChar) || prevChar == '_' && curChar != '_' ||
-            Character.isUpperCase(prevChar) && Character.isUpperCase(curChar) && Character.isLowerCase(nextChar)) {
-          break;
-        }
+      while (start > 0 && Character.isJavaIdentifierPart(editorText.charAt(start - 1)) && !isWordBound(editorText, start)) {
         start--;
       }
 
-      while (end < textLen && Character.isJavaIdentifierPart(editorText.charAt(end))) {
-        final char prevChar = editorText.charAt(end - 1);
-        final char curChar = editorText.charAt(end);
-        final char nextChar = end + 1 < textLen ? editorText.charAt(end + 1) : 0; // 0x00 is not lowercase
-
-        if (Character.isLowerCase(prevChar) && Character.isUpperCase(curChar) || prevChar != '_' && curChar == '_' ||
-            Character.isUpperCase(prevChar) && Character.isUpperCase(curChar) && Character.isLowerCase(nextChar)) {
-          break;
-        }
+      while (end < textLen && Character.isJavaIdentifierPart(editorText.charAt(end)) && !isWordBound(editorText, end)) {
         end++;
       }
 
@@ -119,6 +103,19 @@ public class SelectWordUtil {
     }
 
     return null;
+  }
+
+  private static boolean isWordBound(CharSequence editorText, int offset) {
+    final char prevChar = editorText.charAt(offset - 1);
+    final char curChar = editorText.charAt(offset);
+    final char nextChar = offset + 1 < editorText.length() ? editorText.charAt(offset + 1) : 0; // 0x00 is not lowercase.
+
+    return Character.isLowerCase(prevChar) && Character.isUpperCase(curChar) ||
+        prevChar == '_' && curChar != '_' ||
+        prevChar != '_' && curChar == '_' ||
+        prevChar == '$' && Character.isLetterOrDigit(curChar) ||
+        Character.isLetterOrDigit(prevChar) && curChar == '$' ||
+        Character.isUpperCase(prevChar) && Character.isUpperCase(curChar) && Character.isLowerCase(nextChar);
   }
 
   @Nullable
