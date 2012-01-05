66293b50bd1acfcc361ba64d91da743250078836
Yann Cebron <yann.cebron@googlemail.com>
2012-01-05 20:57:50 +0100

    NutNull -> NotNull

--- platform/annotations/src/org/jetbrains/annotations/NotNull.java
+++ platform/annotations/src/org/jetbrains/annotations/NotNull.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2012 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,11 +19,11 @@ package org.jetbrains.annotations;
 import java.lang.annotation.*;
 
 /**
- *
- * An element annotated with NutNull claims <code>null</code> value is <em>forbidden</em>
+ * An element annotated with NotNull claims <code>null</code> value is <em>forbidden</em>
  * to return (for methods), pass to (parameters) and hold (local variables and fields).
  * Apart from documentation purposes this annotation is intended to be used by static analysis tools
  * to validate against probable runtime errors and element contract violations.
+ *
  * @author max
  */
 @Documented
