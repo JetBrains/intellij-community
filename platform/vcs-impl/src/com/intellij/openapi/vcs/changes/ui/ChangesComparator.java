6c55d2861f56a4d564112251aebb68f81fb6fe63
Bas Leijdekkers <basleijdekkers@gmail.com>
2012-01-05 21:45:37 +0100

    really fix IDEA-16964 (Show Diff Compare Next/Previous File order different from Commit Changes dialog order when invoked on file)

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/ChangesComparator.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/ChangesComparator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2011 JetBrains s.r.o.
+ * Copyright 2000-2012 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -50,7 +50,8 @@ public class ChangesComparator implements Comparator<Change> {
       if (parentPath2 != null && parentPath2.isUnder(parentPath1, true)) {
         return 1;
       }
+      return filePath1.getPath().compareToIgnoreCase(filePath2.getPath());
     }
-    return ChangesUtil.getFilePath(o1).getName().compareToIgnoreCase(ChangesUtil.getFilePath(o2).getName());
+    return filePath1.getName().compareToIgnoreCase(filePath2.getName());
   }
 }
