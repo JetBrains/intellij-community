d165a96e645f86e76f385184f2551e1eaa6ddab7
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2012-01-11 16:30:22 +0400

    Initial classes for Python package management

---
+++ python/src/com/jetbrains/python/packaging/PyPackage.java
@@ -0,0 +1,52 @@
+package com.jetbrains.python.packaging;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+/**
+ * @author vlan
+ */
+public class PyPackage {
+  private final String myName;
+  private final String myVersion;
+  private final String myLocation;
+  private final List<PyRequirement> myRequirements;
+
+  public PyPackage(@NotNull String name, @NotNull String version, @Nullable String location, @NotNull List<PyRequirement> requirements) {
+    myName = name;
+    myVersion = version;
+    myLocation = location;
+    myRequirements = requirements;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%s %s (%s)", getName(), getVersion(), getLocation());
+  }
+
+  @NotNull
+  public String getName() {
+    return myName;
+  }
+
+  @NotNull
+  public String getVersion() {
+    return myVersion;
+  }
+
+  @NotNull
+  public List<PyRequirement> getRequirements() {
+    return myRequirements;
+  }
+
+  @Nullable
+  public String getLocation() {
+    return myLocation;
+  }
+
+  public boolean isInstalled() {
+    return myLocation != null;
+  }
+}
