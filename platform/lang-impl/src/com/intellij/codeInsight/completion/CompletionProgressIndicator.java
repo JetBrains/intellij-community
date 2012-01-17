102698fd443c478eaf8539d36f62a67d296c8df2
peter <peter@jetbrains.com>
2012-01-17 17:57:18 +0100

    dump.threads.on.empty.lookup(cherry picked from commit 3725f48)

--- platform/lang-impl/src/com/intellij/codeInsight/completion/CompletionProgressIndicator.java
+++ platform/lang-impl/src/com/intellij/codeInsight/completion/CompletionProgressIndicator.java
@@ -26,6 +26,7 @@ import com.intellij.codeInsight.hint.HintManager;
 import com.intellij.codeInsight.lookup.*;
 import com.intellij.codeInsight.lookup.impl.LookupImpl;
 import com.intellij.codeInsight.template.impl.LiveTemplateLookupElement;
+import com.intellij.diagnostic.PerformanceWatcher;
 import com.intellij.featureStatistics.FeatureUsageTracker;
 import com.intellij.injected.editor.DocumentWindow;
 import com.intellij.injected.editor.EditorWindow;
@@ -47,6 +48,7 @@ import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.patterns.ElementPattern;
 import com.intellij.psi.PsiFile;
@@ -331,6 +333,10 @@ public class CompletionProgressIndicator extends ProgressIndicatorBase implement
     if (!myLookup.isShown() && shouldShowLookup()) {
       if (hideAutopopupIfMeaningless()) {
         return false;
+      }
+
+      if (Registry.is("dump.threads.on.empty.lookup") && myLookup.isCalculating() && myLookup.getItems().isEmpty()) {
+        PerformanceWatcher.getInstance().dumpThreads(true);
       }
 
       if (StringUtil.isEmpty(myLookup.getAdvertisementText()) && !isAutopopupCompletion()) {
