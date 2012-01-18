81bf0ebfcef685187536641d4dcdbb9df9b48fda
Bas Leijdekkers <basleijdekkers@gmail.com>
2012-01-18 17:38:16 +0100

    really fix IDEA-16964 (Show Diff Compare Next/Previous File order different from Commit Changes dialog order when invoked on file)(cherry picked from commit 6c55d28)

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/ChangesComparator.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/ChangesComparator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2010 JetBrains s.r.o.
+ * Copyright 2000-2012 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,22 +15,43 @@
  */
 package com.intellij.openapi.vcs.changes.ui;
 
+import com.intellij.openapi.vcs.FilePath;
 import com.intellij.openapi.vcs.changes.Change;
 import com.intellij.openapi.vcs.changes.ChangesUtil;
 
 import java.util.Comparator;
 
 public class ChangesComparator implements Comparator<Change> {
-  private static final ChangesComparator ourInstance = new ChangesComparator();
+  private static final ChangesComparator ourFlattenedInstance = new ChangesComparator(false);
+  private static final ChangesComparator ourTreeInstance = new ChangesComparator(true);
+  private final boolean myTreeCompare;
 
-  public static ChangesComparator getInstance() {
-    return ourInstance;
+  public static ChangesComparator getInstance(boolean flattened) {
+    if (flattened) {
+      return ourFlattenedInstance;
+    } else {
+      return ourTreeInstance;
+    }
   }
 
-  private ChangesComparator() {
+  private ChangesComparator(boolean treeCompare) {
+    myTreeCompare = treeCompare;
   }
 
   public int compare(final Change o1, final Change o2) {
-    return ChangesUtil.getFilePath(o1).getName().compareToIgnoreCase(ChangesUtil.getFilePath(o2).getName());
+    final FilePath filePath1 = ChangesUtil.getFilePath(o1);
+    final FilePath filePath2 = ChangesUtil.getFilePath(o2);
+    if (myTreeCompare) {
+      final FilePath parentPath1 = filePath1.getParentPath();
+      final FilePath parentPath2 = filePath2.getParentPath();
+      if (parentPath1 != null && parentPath1.isUnder(parentPath2, true)) {
+        return -1;
+      }
+      if (parentPath2 != null && parentPath2.isUnder(parentPath1, true)) {
+        return 1;
+      }
+      return filePath1.getPath().compareToIgnoreCase(filePath2.getPath());
+    }
+    return filePath1.getName().compareToIgnoreCase(filePath2.getName());
   }
 }
