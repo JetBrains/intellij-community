3247c40f47525f188d8ad6cd55dfbfda74c7c21c
Bas Leijdekkers <basleijdekkers@gmail.com>
2012-01-23 14:26:54 +0100

    Compare with the correct revision (=next revision in table) if only one revision is selected in file history (IDEA-78954)(cherry picked from commit 8676c8b)

--- platform/platform-api/src/com/intellij/ui/table/TableView.java
+++ platform/platform-api/src/com/intellij/ui/table/TableView.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2011 JetBrains s.r.o.
+ * Copyright 2000-2012 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -161,6 +161,10 @@ public class TableView<Item> extends BaseTableView implements ItemsProvider, Sel
 
   public List<Item> getItems() {
     return getListTableModel().getItems();
+  }
+
+  public Item getRow(int row) {
+    return getItems().get(convertRowIndexToModel(row));
   }
 
   public void setMinRowHeight(int i) {
