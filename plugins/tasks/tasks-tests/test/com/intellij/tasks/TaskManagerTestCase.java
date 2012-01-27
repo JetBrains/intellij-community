07300dfc8fc93e51922e076fed6807a5490e7592
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2012-01-27 14:05:12 +0400

    tests fixed

--- plugins/tasks/tasks-tests/test/com/intellij/tasks/TaskManagerTestCase.java
+++ plugins/tasks/tasks-tests/test/com/intellij/tasks/TaskManagerTestCase.java
@@ -1,16 +1,11 @@
 package com.intellij.tasks;
 
-import com.intellij.testFramework.PlatformTestCase;
 import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
 
 /**
  * @author Dmitry Avdeev
  */
 public abstract class TaskManagerTestCase extends LightCodeInsightFixtureTestCase {
-
-  protected TaskManagerTestCase() {
-    PlatformTestCase.setPlatformPrefix("PlatformLangXml");
-  }
 
   protected TaskManager myManager;
 
