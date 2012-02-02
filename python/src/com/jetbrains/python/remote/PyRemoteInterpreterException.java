8bcc6a30205f925ecdcd3f953f33bee7eaa91b8b
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2012-02-02 20:47:11 +0100

    RI: validation of paths.

--- python/src/com/jetbrains/python/remote/PyRemoteInterpreterException.java
+++ python/src/com/jetbrains/python/remote/PyRemoteInterpreterException.java
@@ -1,17 +1,37 @@
 package com.jetbrains.python.remote;
 
+import java.net.NoRouteToHostException;
+
 /**
  * @author traff
  */
 public class PyRemoteInterpreterException extends Exception {
-  public PyRemoteInterpreterException() {
-  }
-
-  public PyRemoteInterpreterException(String s) {
-    super(s);
-  }
+  private final boolean myNoRouteToHost;
+  private final boolean myAuthFailed;
 
   public PyRemoteInterpreterException(String s, Throwable throwable) {
     super(s, throwable);
+    myNoRouteToHost = throwable instanceof NoRouteToHostException;
+    myAuthFailed = false;
+  }
+
+  public boolean isNoRouteToHost() {
+    return myNoRouteToHost;
+  }
+
+  public boolean isAuthFailed() {
+    return myAuthFailed;
+  }
+
+  public String getMessage() {
+    if (myNoRouteToHost) {
+      return getCause().getMessage();
+    }
+    else if (myAuthFailed) {
+      return "Authentication failed";
+    }
+    else {
+      return super.getMessage();
+    }
   }
 }
