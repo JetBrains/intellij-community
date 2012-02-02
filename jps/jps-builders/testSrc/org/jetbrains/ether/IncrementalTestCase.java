9d0b4a01e82d526c80d6ba20a0905483e2d4927c
Eugene Zhuravlev <jeka@intellij.com>
2012-02-02 17:02:03 +0100

    add server-based make test root

--- jps/jps-builders/testSrc/org/jetbrains/ether/IncrementalTestCase.java
+++ jps/jps-builders/testSrc/org/jetbrains/ether/IncrementalTestCase.java
@@ -21,7 +21,6 @@ import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.util.io.FileUtil;
 import junit.framework.TestCase;
 import junitx.framework.FileAssert;
-import org.apache.log4j.FileAppender;
 import org.apache.log4j.Level;
 import org.apache.log4j.PropertyConfigurator;
 import org.jetbrains.annotations.NonNls;
