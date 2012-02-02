b5e1236eb9aec9a2d5aa624c217d9e35ae1146c5
Gregory.Shrago <gregory.shrago@jetbrains.com>
2012-02-03 02:12:24 +0300

    night fixes!

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/TestWindowManager.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/TestWindowManager.java
@@ -114,10 +114,6 @@ public final class TestWindowManager extends WindowManagerEx implements Applicat
       return null;
     }
 
-    @Override
-    public void setLogMessage(String text) {
-    }
-
     public boolean isVisible() {
       return false;
     }
