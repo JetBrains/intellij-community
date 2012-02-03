0fc1bbaf6d974e7c86754b46c14a4bd466a06c9f
Alexey Kudravtsev <cdr@intellij.com>
2012-02-03 14:28:41 +0400

    IDEA-77945

--- platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/SlowLineMarkersPass.java
+++ platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/SlowLineMarkersPass.java
@@ -84,7 +84,7 @@ public class SlowLineMarkersPass extends TextEditorHighlightingPass implements L
     UpdateHighlightersUtil.setLineMarkersToEditor(myProject, myDocument, myStartOffset, myEndOffset, myMarkers, getId());
 
     DaemonCodeAnalyzer daemonCodeAnalyzer = DaemonCodeAnalyzer.getInstance(myProject);
-    ((DaemonCodeAnalyzerImpl)daemonCodeAnalyzer).getFileStatusMap().markFileUpToDate(myFile.getProject(),myDocument, getId());
+    ((DaemonCodeAnalyzerImpl)daemonCodeAnalyzer).getFileStatusMap().markFileUpToDate(myDocument, getId());
   }
 }
 
