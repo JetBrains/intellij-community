00020401abd119e88400bf17184d482e4687b908
Maxim.Mossienko <maxim.mossienko@jetbrains.com>
2012-02-04 16:27:16 +0400

    use cheaper getAttributeValue just for ensuring no attribute present

--- xml/impl/src/com/intellij/codeInsight/daemon/impl/analysis/XmlHighlightVisitor.java
+++ xml/impl/src/com/intellij/codeInsight/daemon/impl/analysis/XmlHighlightVisitor.java
@@ -317,7 +317,7 @@ public class XmlHighlightVisitor extends XmlElementVisitor implements HighlightV
 
     if (requiredAttributes != null) {
       for (final String attrName : requiredAttributes) {
-        if (tag.getAttribute(attrName, "") == null &&
+        if (tag.getAttributeValue(attrName) == null &&
             !XmlExtension.getExtension(tag.getContainingFile()).isRequiredAttributeImplicitlyPresent(tag, attrName)) {
 
           final InsertRequiredAttributeFix insertRequiredAttributeIntention = new InsertRequiredAttributeFix(
