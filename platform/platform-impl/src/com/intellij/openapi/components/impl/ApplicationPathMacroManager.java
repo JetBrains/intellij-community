af0be0eae1092f6d720226c0c6b7f8af2f0035cc
peter <peter@jetbrains.com>
2012-02-07 15:23:55 +0100

    don't redefine application in PathMacroManagerTest

--- platform/platform-impl/src/com/intellij/openapi/components/impl/ApplicationPathMacroManager.java
+++ platform/platform-impl/src/com/intellij/openapi/components/impl/ApplicationPathMacroManager.java
@@ -19,4 +19,7 @@ package com.intellij.openapi.components.impl;
  * @author mike
  */
 public class ApplicationPathMacroManager extends BasePathMacroManager {
+  public ApplicationPathMacroManager() {
+    super(null);
+  }
 }
