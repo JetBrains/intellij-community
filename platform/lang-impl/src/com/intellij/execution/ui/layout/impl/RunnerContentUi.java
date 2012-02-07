18c667c60d49f0ea6560094d591b9b5d4337af05
Dennis Ushakov <dennis.ushakov@gmail.com>
2012-02-07 12:49:34 +0400

    IDEA-80821 Debug tool window: splitting Debugger tab into two tabs with two views in each results in two Debugger tabs wich is confusing

--- platform/lang-impl/src/com/intellij/execution/ui/layout/impl/RunnerContentUi.java
+++ platform/lang-impl/src/com/intellij/execution/ui/layout/impl/RunnerContentUi.java
@@ -811,9 +811,10 @@ public class RunnerContentUi implements ContentUI, Disposable, CellTransform.Fac
   private void updateTabsUI(final boolean validateNow) {
     boolean hasToolbarContent = rebuildToolbar();
 
+    Set<String> usedNames = new HashSet<String>();
     List<TabInfo> tabs = myTabs.getTabs();
     for (TabInfo each : tabs) {
-      hasToolbarContent |= updateTabUI(each);
+      hasToolbarContent |= updateTabUI(each, usedNames);
     }
     int tabsCount = tabs.size();
     for (RunnerContentUi child : myChildren) {
@@ -827,7 +828,7 @@ public class RunnerContentUi implements ContentUI, Disposable, CellTransform.Fac
     }
   }
 
-  private boolean updateTabUI(TabInfo tab) {
+  private boolean updateTabUI(TabInfo tab, Set<String> usedNames) {
     TabImpl t = getTabFor(tab);
     if (t == null) {
       return false;
@@ -842,7 +843,7 @@ public class RunnerContentUi implements ContentUI, Disposable, CellTransform.Fac
     String title = contents.size() > 1 ? t.getDisplayName() : null;
     if (title == null) {
       final String name = myLayoutSettings.getDefaultDisplayName(t.getDefaultIndex());
-      if (name != null && contents.size() > 1) {
+      if (name != null && contents.size() > 1 && !usedNames.contains(name)) {
         title = name;
       } else {
         title = StringUtil.join(contents, new NotNullFunction<Content, String>() {
@@ -854,6 +855,7 @@ public class RunnerContentUi implements ContentUI, Disposable, CellTransform.Fac
         },  " | ");
       }
     }
+    usedNames.add(title);
 
     boolean hidden = true;
     for (Content content : contents) {
