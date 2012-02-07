2e9e111d3850987c435c67ec4edc22d166c59d81
anna <Anna.Kozlova@jetbrains.com>
2012-02-07 21:18:44 +0100

    jarjar asm4.0 to be able to support jacoco and avoid conflicts with asm3.0 from main lib (IDEA-80709)

--- plugins/coverage/src/com/intellij/coverage/SourceLineCounterUtil.java
+++ plugins/coverage/src/com/intellij/coverage/SourceLineCounterUtil.java
@@ -3,7 +3,7 @@ package com.intellij.coverage;
 import com.intellij.rt.coverage.instrumentation.SourceLineCounter;
 import gnu.trove.TIntObjectHashMap;
 import gnu.trove.TIntProcedure;
-import org.objectweb.asm.ClassReader;
+import org.jetbrains.coverage.asm.ClassReader;
 
 import java.util.List;
 
@@ -13,47 +13,36 @@ import java.util.List;
  */
 public class SourceLineCounterUtil {
   public static boolean collectNonCoveredClassInfo(final PackageAnnotator.ClassCoverageInfo classCoverageInfo,
-                                                   final PackageAnnotator.PackageCoverageInfo packageCoverageInfo, 
-                                                   byte[] content, 
+                                                   final PackageAnnotator.PackageCoverageInfo packageCoverageInfo,
+                                                   byte[] content,
                                                    final boolean excludeLines) {
     if (content == null) return false;
     ClassReader reader = new ClassReader(content, 0, content.length);
-    
-    try {
-      SourceLineCounter counter = new SourceLineCounter(null, excludeLines);
-      reader.accept(counter, 0);
-      classCoverageInfo.totalLineCount += counter.getNSourceLines();
-      classCoverageInfo.totalMethodCount += counter.getNMethodsWithCode();
-      packageCoverageInfo.totalLineCount += counter.getNSourceLines();
-      packageCoverageInfo.totalMethodCount += counter.getNMethodsWithCode();
-      if (!counter.isInterface()) {
-        packageCoverageInfo.totalClassCount++;
-      }
-      return counter.getNMethodsWithCode() > 0;
+
+    SourceLineCounter counter = new SourceLineCounter(null, excludeLines);
+    reader.accept(counter, 0);
+    classCoverageInfo.totalLineCount += counter.getNSourceLines();
+    classCoverageInfo.totalMethodCount += counter.getNMethodsWithCode();
+    packageCoverageInfo.totalLineCount += counter.getNSourceLines();
+    packageCoverageInfo.totalMethodCount += counter.getNMethodsWithCode();
+    if (!counter.isInterface()) {
+      packageCoverageInfo.totalClassCount++;
     }
-    catch (IncompatibleClassChangeError e) {
-      //todo waiting for asm4 in idea lib 
-      return true;
-    }
+    return counter.getNMethodsWithCode() > 0;
   }
 
   public static void collectSrcLinesForUntouchedFiles(final List<Integer> uncoveredLines,
                                                       byte[] content, final boolean excludeLines) {
     final ClassReader reader = new ClassReader(content, 0, content.length);
-    try {
-      final SourceLineCounter collector = new SourceLineCounter(null, excludeLines);
-      reader.accept(collector, 0);
-      final TIntObjectHashMap lines = collector.getSourceLines();
-      lines.forEachKey(new TIntProcedure() {
-        public boolean execute(int line) {
-          line--;
-          uncoveredLines.add(line);
-          return true;
-        }
-      });
-    }
-    catch (IncompatibleClassChangeError e) {
-      //todo waiting for asm4 in idea lib
-    }
+    final SourceLineCounter collector = new SourceLineCounter(null, excludeLines);
+    reader.accept(collector, 0);
+    final TIntObjectHashMap lines = collector.getSourceLines();
+    lines.forEachKey(new TIntProcedure() {
+      public boolean execute(int line) {
+        line--;
+        uncoveredLines.add(line);
+        return true;
+      }
+    });
   }
 }
