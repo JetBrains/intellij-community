c098f1dec72ed3559cec7f9bfef0ab45e8839791
Eugene Kudelevsky <Eugene.Kudelevsky@jetbrains.com>
2012-02-09 19:58:53 +0400

    android-jps-plugin initial

---
+++ plugins/android/jps-plugin/src/org/jetbrains/jps/android/AndroidBuilderService.java
@@ -0,0 +1,20 @@
+package org.jetbrains.jps.android;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.jps.incremental.BuilderService;
+import org.jetbrains.jps.incremental.ModuleLevelBuilder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+
+/**
+ * @author Eugene.Kudelevsky
+ */
+public class AndroidBuilderService extends BuilderService {
+  @NotNull
+  @Override
+  public List<? extends ModuleLevelBuilder> createModuleLevelBuilders(ExecutorService executorService) {
+    return Arrays.asList(new AndroidSourceGeneratingBuilder());
+  }
+}
