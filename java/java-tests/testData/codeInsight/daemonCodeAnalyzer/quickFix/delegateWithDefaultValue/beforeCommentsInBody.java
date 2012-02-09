b13cd651dc5f53da2ecc90bfdc25282e828124e4
anna <Anna.Kozlova@jetbrains.com>
2012-02-09 12:49:39 +0100

    do not insert initial method comments in delegate with default param value (IDEA-81106)

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/delegateWithDefaultValue/beforeCommentsInBody.java
@@ -0,0 +1,9 @@
+// "Generate delegated method with default parameter value" "true"
+class Test {
+  int foo(int i<caret>i){
+    //comment1
+    System.out.println("");
+    //comment2
+    return 1;
+  }
+}
