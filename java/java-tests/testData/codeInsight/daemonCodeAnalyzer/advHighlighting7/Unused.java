a829ba256816f85547d25ac6f6d4396ecce2d01a
anna <Anna.Kozlova@jetbrains.com>
2012-02-14 11:55:24 +0100

    ignore parameter for foreach (IDEA-72671)

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/advHighlighting7/Unused.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/advHighlighting7/Unused.java
@@ -24,7 +24,7 @@ class UnusedDeclBug {
 class ForEachTest {
   public static void main(String[] args) {
     int count = 0;
-    for (String arg : args) {
+    for (String ignore : args) {
       count++;
     }
     System.out.println(count);
