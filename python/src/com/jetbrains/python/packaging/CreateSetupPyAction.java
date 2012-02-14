4e01a35764a33d34bd0cec1a28c1302f1d39cd52
Dmitry Jemerov <yole@jetbrains.com>
2012-02-14 18:11:44 +0100

    fill 'packages' keyword argument when creating setup.py

--- python/src/com/jetbrains/python/packaging/CreateSetupPyAction.java
+++ python/src/com/jetbrains/python/packaging/CreateSetupPyAction.java
@@ -12,13 +12,17 @@ import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiManager;
 import com.intellij.util.SystemProperties;
+import com.jetbrains.python.PyNames;
 import com.jetbrains.python.psi.PyUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
@@ -49,8 +53,35 @@ public class CreateSetupPyAction extends CreateFromTemplateAction {
       final PropertiesComponent properties = PropertiesComponent.getInstance();
       defaults.add("Author", properties.getOrInit(AUTHOR_PROPERTY, SystemProperties.getUserName()));
       defaults.add("Author_Email", properties.getOrInit(EMAIL_PROPERTY, ""));
+      defaults.addPredefined("PackageList", getPackageList(dataContext));
     }
     return defaults;
+  }
+
+  private static String getPackageList(DataContext dataContext) {
+    final Module module = LangDataKeys.MODULE.getData(dataContext);
+    if (module != null) {
+      final List<VirtualFile> sourceRoots = PyUtil.getSourceRoots(module);
+      List<String> packageNames = new ArrayList<String>();
+      for (VirtualFile sourceRoot : sourceRoots) {
+        collectPackageNames(module.getProject(), sourceRoot, packageNames, "");
+      }
+      return "['" + StringUtil.join(packageNames, "', '") + "']";
+    }
+    return "[]";
+  }
+
+  private static void collectPackageNames(Project project, VirtualFile root, List<String> names, String prefix) {
+    for (VirtualFile child : root.getChildren()) {
+      if (ProjectRootManager.getInstance(project).getFileIndex().isIgnored(child)) {
+        continue;
+      }
+      if (child.findChild(PyNames.INIT_DOT_PY) != null) {
+        final String name = prefix + child.getName();
+        names.add(name);
+        collectPackageNames(project, child, names, name + ".");
+      }
+    }
   }
 
   @Override
