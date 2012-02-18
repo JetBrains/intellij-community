a0e25e5202abe1ff58c5745efcdddebb706753d1
Bas Leijdekkers <basleijdekkers@gmail.com>
2012-02-18 16:10:24 +0100

    EA-33721 (NPE: CastConflictsWithInstanceofInspection$ReplaceFix.doFix)

--- plugins/InspectionGadgets/src/com/siyeh/ig/bugs/CastConflictsWithInstanceofInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/bugs/CastConflictsWithInstanceofInspection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2011 Dave Griffith, Bas Leijdekkers
+ * Copyright 2003-2012 Dave Griffith, Bas Leijdekkers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -183,6 +183,9 @@ public class CastConflictsWithInstanceofInspection extends BaseInspection {
       }
       final PsiInstanceOfExpression conflictingInstanceof =
         InstanceOfUtils.getConflictingInstanceof(castTypeElement.getType(), reference, element);
+      if (conflictingInstanceof == null) {
+        return;
+      }
       final PsiTypeElement instanceofTypeElement = conflictingInstanceof.getCheckType();
       if (instanceofTypeElement == null) {
         return;
