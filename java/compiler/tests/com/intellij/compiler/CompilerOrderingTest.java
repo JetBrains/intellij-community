c41d138b6f0f52d7092bd50010919b7c01dad962
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2012-02-21 15:32:36 +0400

    refactored Flex resource files handling

--- java/compiler/tests/com/intellij/compiler/CompilerOrderingTest.java
+++ java/compiler/tests/com/intellij/compiler/CompilerOrderingTest.java
@@ -47,7 +47,6 @@ public class CompilerOrderingTest extends IdeaTestCase {
     
     compareDescriptions(new String[] {
       CompilerBundle.message("resource.compiler.description"),
-      "Flex Resource Compiler",
       //"groovy compiler",
       "C2",
       "C4",
