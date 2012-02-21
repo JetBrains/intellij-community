1188b58704b565048d8b1e469d4cc16e901e279b
anna <Anna.Kozlova@jetbrains.com>
2012-02-21 12:15:39 +0100

    do not loose array dimension expressions when converting array types; move caret and selection only in case when no expressions were detected

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/changeNewOperatorType/beforeNonPrimitiveArrayDimensions.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/changeNewOperatorType/beforeNonPrimitiveArrayDimensions.java
@@ -14,12 +14,8 @@
  * limitations under the License.
  */
 
-// "Change 'new TTT[0][]' to 'new TTT[]'" "true"
-public class TTT {
-    void f() {
-        String s = new Object();
-        int[][] i = new Object();
-        TTT[] f = new TTT[0]<caret>;
+// "Change 'new X[42]' to 'new long[]'" "true"
 
-    }
-}
+ class X {
+ public long[] foo() { return <caret>new X[42]; }
+ }
