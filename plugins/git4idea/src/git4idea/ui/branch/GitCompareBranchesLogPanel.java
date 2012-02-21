9f2da31c076e9443b0b26cb40103aa4ac47e2572
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2012-02-21 20:26:18 +0400

    GitCompareBranchesLogPanel: make repository selector thinner, fix alignment

--- plugins/git4idea/src/git4idea/ui/branch/GitCompareBranchesLogPanel.java
+++ plugins/git4idea/src/git4idea/ui/branch/GitCompareBranchesLogPanel.java
@@ -53,7 +53,7 @@ class GitCompareBranchesLogPanel extends JPanel {
 
   GitCompareBranchesLogPanel(@NotNull Project project, @NotNull String branchName, @NotNull String currentBranchName,
                                     @NotNull GitCommitCompareInfo compareInfo, @NotNull GitRepository initialRepo) {
-    super(new BorderLayout());
+    super(new BorderLayout(UIUtil.DEFAULT_HGAP, UIUtil.DEFAULT_VGAP));
     myProject = project;
     myBranchName = branchName;
     myCurrentBranchName = currentBranchName;
@@ -103,10 +103,10 @@ class GitCompareBranchesLogPanel extends JPanel {
       }
     });
 
-    JPanel repoSelectorPanel = new JPanel(new BorderLayout(UIUtil.DEFAULT_VGAP, UIUtil.DEFAULT_HGAP));
+    JPanel repoSelectorPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
     JBLabel label = new JBLabel("Repository: ");
     label.setLabelFor(repoSelectorPanel);
-    repoSelectorPanel.add(label, BorderLayout.WEST);
+    repoSelectorPanel.add(label);
     repoSelectorPanel.add(repoSelector);
 
     if (myCompareInfo.getRepositories().size() < 2) {
