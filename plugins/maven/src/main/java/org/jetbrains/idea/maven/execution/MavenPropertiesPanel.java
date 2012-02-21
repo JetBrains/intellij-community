3ff5b7823309f8b40d0247eb13b43f5cac08ef39
Sergey Evdokimov <sergey.evdokimov@jetbrains.com>
2012-02-21 15:54:07 +0400

    IDEA-46477 (Need ability to feed custom properties into maven archetype when creating a new project)

---
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/execution/MavenPropertiesPanel.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2000-2012 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jetbrains.idea.maven.execution;
+
+import com.intellij.openapi.util.Pair;
+import com.intellij.ui.AddEditRemovePanel;
+
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
+/**
+* @author Sergey Evdokimov
+*/
+public class MavenPropertiesPanel extends AddEditRemovePanel<Pair<String, String>> {
+  private Map<String, String> myAvailableProperties;
+
+  public MavenPropertiesPanel(Map<String, String> availableProperties) {
+    super(new MyPropertiesTableModel(), new ArrayList<Pair<String, String>>(), null);
+    setPreferredSize(new Dimension(100, 100));
+    myAvailableProperties = availableProperties;
+  }
+
+  protected Pair<String, String> addItem() {
+    return doAddOrEdit(new Pair<String, String>("", ""));
+  }
+
+  protected boolean removeItem(Pair<String, String> o) {
+    return true;
+  }
+
+  protected Pair<String, String> editItem(Pair<String, String> o) {
+    return doAddOrEdit(o);
+  }
+
+  private Pair<String, String> doAddOrEdit(Pair<String, String> o) {
+    EditMavenPropertyDialog d = new EditMavenPropertyDialog(o, myAvailableProperties);
+    d.show();
+    if (!d.isOK()) return null;
+    return d.getValue();
+  }
+
+  public Map<String, String> getDataAsMap() {
+    Map<String, String> result = new LinkedHashMap<String, String>();
+    for (Pair<String, String> p : getData()) {
+      result.put(p.getFirst(), p.getSecond());
+    }
+    return result;
+  }
+
+  public void setDataFromMap(Map<String, String> map) {
+    List<Pair<String, String>> result = new ArrayList<Pair<String, String>>();
+    for (Map.Entry<String, String> e : map.entrySet()) {
+      result.add(new Pair<String, String>(e.getKey(), e.getValue()));
+    }
+    setData(result);
+  }
+
+  private static class MyPropertiesTableModel extends AddEditRemovePanel.TableModel<Pair<String, String>> {
+    public int getColumnCount() {
+      return 2;
+    }
+
+    public String getColumnName(int c) {
+      return c == 0 ? "Name" : "Value";
+    }
+
+    public Object getField(Pair<String, String> o, int c) {
+      return c == 0 ? o.getFirst() : o.getSecond();
+    }
+  }
+
+}
