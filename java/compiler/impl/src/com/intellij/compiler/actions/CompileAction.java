dff474222ffc96a6e8d45b80e6a48022da8094b9
Dmitry Jemerov <yole@jetbrains.com>
2012-02-22 19:28:06 +0100

    hide disabled "Compile" action in context menu (IDEA-81700)

--- java/compiler/impl/src/com/intellij/compiler/actions/CompileAction.java
+++ java/compiler/impl/src/com/intellij/compiler/actions/CompileAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2012 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -75,6 +75,7 @@ public class CompileAction extends CompileActionBase {
     final VirtualFile[] files = getCompilableFiles(project, PlatformDataKeys.VIRTUAL_FILE_ARRAY.getData(dataContext));
     if (module == null && files.length == 0) {
       presentation.setEnabled(false);
+      presentation.setVisible(!ActionPlaces.isPopupPlace(event.getPlace()));
       return;
     }
 
