29f686b50df085b2ee7e3b8f211735f27bf6daeb
Dmitry Jemerov <yole@jetbrains.com>
2012-02-23 18:36:04 +0100

    JBTabsTest renamed to JBTabsDemo and moved to platform-tests

--- platform/platform-tests/testSrc/com/intellij/ui/tabs/impl/JBTabsDemo.java
+++ platform/platform-tests/testSrc/com/intellij/ui/tabs/impl/JBTabsDemo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2012 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,7 +36,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
-public class JBTabsTest {
+public class JBTabsDemo {
   public static void main(String[] args) {
     System.out.println("JBTabs.main");
 
