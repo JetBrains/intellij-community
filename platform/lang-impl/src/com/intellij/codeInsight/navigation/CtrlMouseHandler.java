58d932f319796ff23112006574f0de40791da146
Dmitry Jemerov <yole@jetbrains.com>
2012-02-23 15:12:09 +0100

    EA-33929 - assert: EditorWindow.logicalPositionToXY

--- platform/lang-impl/src/com/intellij/codeInsight/navigation/CtrlMouseHandler.java
+++ platform/lang-impl/src/com/intellij/codeInsight/navigation/CtrlMouseHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2012 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -570,7 +570,7 @@ public class CtrlMouseHandler extends AbstractProjectComponent {
     }
 
     private void showHint(Info info) {
-      if (myDisposed) return;
+      if (myDisposed || myEditor.isDisposed()) return;
       Component internalComponent = myEditor.getContentComponent();
       if (myHighlighter != null) {
         if (!info.isSimilarTo(myHighlighter.getStoredInfo())) {
