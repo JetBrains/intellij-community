11e61f834abffe5f48d8f1e57f5f56bcf0690fbe
Dmitry Jemerov <yole@jetbrains.com>
2012-02-23 18:05:03 +0100

    parameter order fixed

--- platform/platform-api/src/com/intellij/ui/tabs/impl/singleRow/SingleRowLayoutStrategy.java
+++ platform/platform-api/src/com/intellij/ui/tabs/impl/singleRow/SingleRowLayoutStrategy.java
@@ -46,7 +46,7 @@ public abstract class SingleRowLayoutStrategy {
   protected abstract int getFixedFitLength(final SingleRowPassInfo data);
 
   public Rectangle getLayoutRect(final SingleRowPassInfo data, final int position, final int length) {
-    return getLayoutRec(getFixedPosition(data), position, length, getFixedFitLength(data));
+    return getLayoutRec(position, getFixedPosition(data), length, getFixedFitLength(data));
   }
 
   protected abstract Rectangle getLayoutRec(final int position, final int fixedPos, final int length, final int fixedFitLength);
