ed16e91facc8fac9dabb6776e65d7bb0ff15d61f
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2012-02-27 17:40:02 +0400

    Fixed tests for PY-5837

---
+++ python/testData/inspections/PyPackageRequirementsInspection/ImportsNotInRequirementsTxt/test1.py
@@ -0,0 +1,9 @@
+import pip
+import <warning descr="Package 'opster' is not listed in project requirements">opster</warning>
+from <warning descr="Package 'clevercss' is not listed in project requirements">clevercss</warning> import convert
+import <warning descr="Package 'django' is not listed in project requirements">django</warning>.conf
+import httplib
+import test2, <warning descr="Package 'test3' is not listed in project requirements">test3</warning>
+
+print('Hello, World!')
+
