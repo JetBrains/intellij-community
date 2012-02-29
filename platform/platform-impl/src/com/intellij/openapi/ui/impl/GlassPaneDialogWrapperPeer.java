766af0c35cd0c61a2646aff290b48eb8f8979f4e
Dmitry Jemerov <yole@jetbrains.com>
2012-02-29 18:05:30 +0100

    UOE

--- platform/platform-impl/src/com/intellij/openapi/ui/impl/GlassPaneDialogWrapperPeer.java
+++ platform/platform-impl/src/com/intellij/openapi/ui/impl/GlassPaneDialogWrapperPeer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2012 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -192,7 +192,7 @@ public class GlassPaneDialogWrapperPeer extends DialogWrapperPeer implements Foc
   }
 
   public Window getWindow() {
-    throw new UnsupportedOperationException("Not implemented in " + getClass().getCanonicalName());
+    return null;
   }
 
   public JRootPane getRootPane() {
