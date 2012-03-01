96884f79f1d528a3189e752f3d1cb2b7414abc70
Dmitry Jemerov <yole@jetbrains.com>
2012-03-01 17:58:24 +0100

    source roots containing __init__.py should still be displayed as source roots

--- python/src/com/jetbrains/python/PyDirectoryIconProvider.java
+++ python/src/com/jetbrains/python/PyDirectoryIconProvider.java
@@ -1,7 +1,9 @@
 package com.jetbrains.python;
 
 import com.intellij.ide.IconProvider;
+import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.util.Iconable;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
 import com.intellij.util.PlatformIcons;
@@ -18,7 +20,11 @@ public class PyDirectoryIconProvider extends IconProvider {
     if (element instanceof PsiDirectory) {
       final PsiDirectory directory = (PsiDirectory)element;
       if (directory.findFile(PyNames.INIT_DOT_PY) != null) {
-        return (flags & Iconable.ICON_FLAG_OPEN) != 0 ? PlatformIcons.PACKAGE_OPEN_ICON : PlatformIcons.PACKAGE_ICON;
+        final VirtualFile vFile = directory.getVirtualFile();
+        final VirtualFile root = ProjectRootManager.getInstance(directory.getProject()).getFileIndex().getSourceRootForFile(vFile);
+        if (root != vFile) {
+          return (flags & Iconable.ICON_FLAG_OPEN) != 0 ? PlatformIcons.PACKAGE_OPEN_ICON : PlatformIcons.PACKAGE_ICON;
+        }
       }
     }
     return null;
