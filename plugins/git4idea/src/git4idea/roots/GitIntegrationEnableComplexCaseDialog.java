841122326221964b62c1026597ea3f4b7012367d
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2012-03-01 15:08:08 +0400

    Better layout in GitIntegrationEnableComplexCaseDialog

--- plugins/git4idea/src/git4idea/roots/GitIntegrationEnableComplexCaseDialog.java
+++ plugins/git4idea/src/git4idea/roots/GitIntegrationEnableComplexCaseDialog.java
@@ -17,16 +17,20 @@ package git4idea.roots;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.components.JBLabel;
 import com.intellij.util.Function;
+import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.util.Collection;
+
+import static com.intellij.openapi.util.text.StringUtil.pluralize;
 
 /**
  * @author Kirill Likhodedov
@@ -47,37 +51,26 @@ class GitIntegrationEnableComplexCaseDialog extends DialogWrapper {
     super(project);
     myRoots = roots;
 
-    myGitInit = new JRadioButton("Create Git repository for the whole project, and register all VCS roots");
-    myJustAddRoots = new JRadioButton("Just register these Git repositories as VCS roots");
+    int rootsNum = myRoots.size();
+    myGitInit = new JRadioButton("Create Git repository for the whole project, and register " +
+                                 (rootsNum == 1 ? "both" : "all") + " VCS roots");
+    myJustAddRoots = new JRadioButton("Just register " + pluralize("this", rootsNum) + " Git " + pluralize("repository", rootsNum) +
+                                      " as VCS " + pluralize("root", rootsNum));
 
     setTitle("Enable Git Integration");
     init();
   }
 
   @Override
-  protected JComponent createNorthPanel() {
-    String intro;
-    String repos;
-    String problemText = ", but the whole project is not under Git.";
-    if (myRoots.size() == 1) {
-      String root = myRoots.iterator().next().getPresentableUrl();
-      intro = "We've detected that there is a Git repository " + root + " inside the project";
-      repos = "";
-    }
-    else {
-      intro = "We've detected that there are Git repositories inside the project";
-      repos = "\nRepositories: \n" + StringUtil.join(myRoots, new Function<VirtualFile, String>() {
-        @Override
-        public String fun(VirtualFile virtualFile) {
-          return virtualFile.getPresentableUrl();
-        }
-      }, "\n");
-    }
-    return new JBLabel(intro + problemText + repos);
+  protected JComponent createCenterPanel() {
+    JPanel mainPanel = new JPanel(new BorderLayout(UIUtil.DEFAULT_HGAP, UIUtil.DEFAULT_VGAP));
+    mainPanel.add(new JBLabel(Messages.getQuestionIcon()), BorderLayout.WEST);
+    mainPanel.add(createRootPanel());
+    return mainPanel;
   }
 
-  @Override
-  protected JComponent createCenterPanel() {
+  @NotNull
+  private JComponent createRootPanel() {
     myJustAddRoots.setMnemonic(KeyEvent.VK_R);
 
     myGitInit.setSelected(true);
@@ -87,10 +80,33 @@ class GitIntegrationEnableComplexCaseDialog extends DialogWrapper {
     group.add(myJustAddRoots);
     group.add(myGitInit);
 
-    JPanel rootPanel = new JPanel(new BorderLayout());
-    rootPanel.add(myGitInit);
-    rootPanel.add(myJustAddRoots);
+    JPanel rootPanel = new JPanel(new BorderLayout(UIUtil.DEFAULT_HGAP, UIUtil.DEFAULT_VGAP));
+    rootPanel.add(createQuestionPanel(), BorderLayout.NORTH);
+    rootPanel.add(myGitInit, BorderLayout.CENTER);
+    rootPanel.add(myJustAddRoots, BorderLayout.SOUTH);
     return rootPanel;
+  }
+
+  @NotNull
+  private JComponent createQuestionPanel() {
+    String problemText = "but the whole project is not under Git.";
+    String text;
+    if (myRoots.size() == 1) {
+      String root = myRoots.iterator().next().getPresentableUrl();
+      String intro = "We've detected that there is a Git repository <br/>" + root + " inside the project";
+      text = intro + ",<br/>" + problemText;
+    }
+    else {
+      String intro = "We've detected that there are Git repositories inside the project, ";
+      String repos = "<br/>Repositories: <br/>" + StringUtil.join(myRoots, new Function<VirtualFile, String>() {
+        @Override
+        public String fun(VirtualFile virtualFile) {
+          return virtualFile.getPresentableUrl();
+        }
+      }, "<br/>");
+      text = intro + problemText + repos;
+    }
+    return new JBLabel("<html>" + text + "</html>");
   }
 
   @NotNull
