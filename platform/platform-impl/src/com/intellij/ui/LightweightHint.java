8086e7b4d54103763f745cb85c2b74d7a47335b9
Dmitry Jemerov <yole@jetbrains.com>
2012-03-02 16:35:59 +0100

    fix typo in method name

--- platform/platform-impl/src/com/intellij/ui/LightweightHint.java
+++ platform/platform-impl/src/com/intellij/ui/LightweightHint.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2012 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -166,7 +166,7 @@ public class LightweightHint extends UserDataHolderBase implements Hint {
           .setFont(hintHint.getTextFont())
           .setCalloutShift(hintHint.getCalloutShift())
           .setPositionChangeShift(hintHint.getPositionChangeX(), hintHint.getPositionChangeY())
-          .setExlicitClose(hintHint.isExplicitClose());
+          .setExplicitClose(hintHint.isExplicitClose());
 
         myComponent.validate();
         myCurrentIdeTooltip = IdeTooltipManager.getInstance().show(tooltip, false);
