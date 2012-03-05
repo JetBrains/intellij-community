3f8ab894a1ccc8234d8a920a714b202117f96472
Maxim.Mossienko <maxim.mossienko@jetbrains.com>
2012-03-05 18:53:29 +0400

    extracted duplicates

--- xml/impl/src/com/intellij/xml/util/ColorSampleLookupValue.java
+++ xml/impl/src/com/intellij/xml/util/ColorSampleLookupValue.java
@@ -21,6 +21,7 @@ import com.intellij.codeInsight.lookup.LookupValueWithPriority;
 import com.intellij.codeInsight.lookup.LookupValueWithUIHint;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Iconable;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.xml.XmlBundle;
 import org.jetbrains.annotations.NonNls;
@@ -434,5 +435,29 @@ public class ColorSampleLookupValue implements LookupValueWithUIHint, DeferredUs
 
     String colorBox = "<div style=\"border: 1px solid #000000; width: 50px; height: 20px; background-color:" + code + "\"></div>";
     buf.append(XmlBundle.message("color.preview", colorBox)).append(BR);
+  }
+
+  public static Color getColor(String text) {
+    if (StringUtil.isEmptyOrSpaces(text)) {
+      return null;
+    }
+    String hexValue = text.charAt(0) == '#' ? text : getHexCodeForColorName(text.toLowerCase());
+    if (hexValue != null) {
+      String hexValue2 = hexValue.substring(1);
+      if (hexValue2.length() == 3) {
+        StringBuilder buf = new StringBuilder(6);
+        buf.append(hexValue2.charAt(0)).append(hexValue2.charAt(0));
+        buf.append(hexValue2.charAt(1)).append(hexValue2.charAt(1));
+        buf.append(hexValue2.charAt(2)).append(hexValue2.charAt(2));
+
+        hexValue2 = buf.toString();
+      }
+
+      try {
+        return Color.decode("0x" + hexValue2);
+      }
+      catch (NumberFormatException e) {}
+    }
+    return null;
   }
 }
