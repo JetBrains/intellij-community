271acba28c17d6863279de7b742d258343c818b6
peter <peter@jetbrains.com>
2012-03-05 16:22:56 +0400

    IDEA-79863 jUnit 3: class/smart completion of classnames in test-sources should prefer *TestCase classes

--- java/java-tests/testSrc/com/intellij/codeInsight/completion/HeavyNormalCompletionTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/completion/HeavyNormalCompletionTest.java
@@ -9,9 +9,12 @@ import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupManager;
 import com.intellij.codeInsight.lookup.impl.LookupImpl;
 import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.roots.ContentEntry;
 import com.intellij.openapi.roots.ModifiableRootModel;
 import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.roots.SourceFolder;
 import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.JavaPsiFacade;
 import com.intellij.testFramework.fixtures.JavaCodeInsightFixtureTestCase;
 
@@ -42,7 +45,30 @@ public class HeavyNormalCompletionTest extends JavaCodeInsightFixtureTestCase {
     assertTrue(JavaPsiFacade.getInstance(getProject()).findPackage("foo.bar").isValid());
     assertTrue(JavaPsiFacade.getInstance(getProject()).findPackage("foo.bar.goo").isValid());
   }
-  
+
+  public void testPreferTestCases() throws Throwable {
+    myFixture.configureByFile("/codeInsight/completion/normal/" + getTestName(false) + ".java");
+    new WriteCommandAction.Simple(getProject()) {
+      @Override
+      protected void run() throws Throwable {
+        final ModifiableRootModel model = ModuleRootManager.getInstance(myFixture.getModule()).getModifiableModel();
+        ContentEntry contentEntry = model.getContentEntries()[0];
+        SourceFolder sourceFolder = contentEntry.getSourceFolders()[0];
+        VirtualFile file = sourceFolder.getFile();
+        contentEntry.removeSourceFolder(sourceFolder);
+        contentEntry.addSourceFolder(file, true);
+        model.commit();
+      }
+    }.execute().throwException();
+
+    myFixture.addClass("package foo; public class SomeTestCase {}");
+    myFixture.addClass("package bar; public class SomeTestec {}");
+    myFixture.addClass("package goo; public class SomeAnchor {}");
+
+    myFixture.completeBasic();
+    myFixture.assertPreferredCompletionItems(0, "SomeTestCase", "SomeAnchor", "SomeTestec");
+  }
+
   public void testAllClassesWhenNothingIsFound() throws Throwable {
     myFixture.addClass("package foo.bar; public class AxBxCxDxEx {}");
 
