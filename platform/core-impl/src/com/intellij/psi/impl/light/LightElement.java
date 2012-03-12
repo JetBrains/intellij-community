a6259036480820609c0565652aa4489e545a33dc
anna <Anna.Kozlova@jetbrains.com>
2012-03-12 16:19:42 +0100

    logging to detect incorrect calls to light elements

--- platform/core-impl/src/com/intellij/psi/impl/light/LightElement.java
+++ platform/core-impl/src/com/intellij/psi/impl/light/LightElement.java
@@ -130,37 +130,37 @@ public abstract class LightElement extends PsiElementBase {
 
   @Override
   public void checkAdd(@NotNull PsiElement element) throws IncorrectOperationException {
-    throw new IncorrectOperationException();
+    throw new IncorrectOperationException(getClass().getName());
   }
 
   @Override
   public PsiElement add(@NotNull PsiElement element) throws IncorrectOperationException {
-    throw new IncorrectOperationException();
+    throw new IncorrectOperationException(getClass().getName());
   }
 
   @Override
   public PsiElement addBefore(@NotNull PsiElement element, PsiElement anchor) throws IncorrectOperationException {
-    throw new IncorrectOperationException();
+    throw new IncorrectOperationException(getClass().getName());
   }
 
   @Override
   public PsiElement addAfter(@NotNull PsiElement element, PsiElement anchor) throws IncorrectOperationException {
-    throw new IncorrectOperationException();
+    throw new IncorrectOperationException(getClass().getName());
   }
 
   @Override
   public void delete() throws IncorrectOperationException {
-    throw new IncorrectOperationException();
+    throw new IncorrectOperationException(getClass().getName());
   }
 
   @Override
   public void checkDelete() throws IncorrectOperationException {
-    throw new IncorrectOperationException();
+    throw new IncorrectOperationException(getClass().getName());
   }
 
   @Override
   public PsiElement replace(@NotNull PsiElement newElement) throws IncorrectOperationException {
-    throw new IncorrectOperationException();
+    throw new IncorrectOperationException(getClass().getName());
   }
 
   @Override
