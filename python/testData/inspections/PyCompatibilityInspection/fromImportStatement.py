0d8205ae95876cb71561c71ee45b024fed53a456
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2012-03-12 17:26:17 +0400

    fixed PY-5732 code compat false positive, python version does not have email.Utils problem in lower case fixed with patch

--- python/testData/inspections/PyCompatibilityInspection/fromImportStatement.py
+++ python/testData/inspections/PyCompatibilityInspection/fromImportStatement.py
@@ -4,3 +4,5 @@
 from <warning descr="Python version 3.0, 3.1 do not have module Bastion">Bastion</warning> import BastionClass
 
 <warning descr="Python version 2.4 doesn't support this syntax.">from . import exceptions</warning>
+
+from email.Utils import formatdate
