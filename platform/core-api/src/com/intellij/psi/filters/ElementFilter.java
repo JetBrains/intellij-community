a5da6ce73fd37629b0ff6cc932b883341b2234f2
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2012-03-14 13:57:48 +0400

    cleanup

--- platform/core-api/src/com/intellij/psi/filters/ElementFilter.java
+++ platform/core-api/src/com/intellij/psi/filters/ElementFilter.java
@@ -18,15 +18,16 @@ package com.intellij.psi.filters;
 
 import com.intellij.psi.PsiElement;
 import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.Nullable;
 
 public interface ElementFilter{
   /**
-   * Checks if element in given context is mathed by given filter.
+   * Checks if element in given context is matched by given filter.
    * @param element most often PsiElement
    * @param context context of the element (if any)
    * @return true if matched
    */
-  boolean isAcceptable(Object element, PsiElement context);
+  boolean isAcceptable(Object element, @Nullable PsiElement context);
 
   /**
    * Quick check if the filter acceptable for elements of given class at all.
