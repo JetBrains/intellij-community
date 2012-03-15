9cb4eaa7d6182415db6be2919442d9d8a6516085
Alexey Kudravtsev <cdr@intellij.com>
2012-03-15 17:04:25 +0400

    notnull

--- platform/platform-api/src/com/intellij/openapi/editor/event/EditorFactoryEvent.java
+++ platform/platform-api/src/com/intellij/openapi/editor/event/EditorFactoryEvent.java
@@ -17,21 +17,24 @@ package com.intellij.openapi.editor.event;
 
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.EditorFactory;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.EventObject;
 
 public class EditorFactoryEvent extends EventObject {
   private final Editor myEditor;
 
-  public EditorFactoryEvent(EditorFactory editorFactory, Editor editor) {
+  public EditorFactoryEvent(@NotNull EditorFactory editorFactory, @NotNull Editor editor) {
     super(editorFactory);
     myEditor = editor;
   }
 
+  @NotNull
   public EditorFactory getFactory(){
     return (EditorFactory) getSource();
   }
 
+  @NotNull
   public Editor getEditor() {
     return myEditor;
   }
