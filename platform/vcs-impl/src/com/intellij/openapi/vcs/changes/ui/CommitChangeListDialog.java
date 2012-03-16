e1e866d3f105bf400695ecf3471566d97f1f5f62
peter <peter@jetbrains.com>
2012-03-16 17:14:10 +0400

    correctly render 'Commit' menu option in case of multiple VCSes present

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/CommitChangeListDialog.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/CommitChangeListDialog.java
@@ -107,7 +107,7 @@ public class CommitChangeListDialog extends DialogWrapper implements CheckinProj
   private static final String DETAILS_SHOW_OPTION = "CommitChangeListDialog.DETAILS_SHOW_OPTION_";
   private JPanel myDetailsPanel;
   private final FileAndDocumentListenersForShortDiff myListenersForShortDiff;
-  private String myOkActionText;
+  private final String myOkActionText;
   private final ZipperUpdater myZipperUpdater;
   private final Runnable myRefreshDetails;
 
@@ -767,10 +767,10 @@ public class CommitChangeListDialog extends DialogWrapper implements CheckinProj
         name = checkinEnvironment.getCheckinOperationName();
       }
       else {
-        name = VcsBundle.message("commit.dialog.default.commit.operation.name");
+        name = VcsBundle.getString("commit.dialog.default.commit.operation.name");
       }
     }
-    return name != null ? name : VcsBundle.message("commit.dialog.default.commit.operation.name");
+    return name != null ? name : VcsBundle.getString("commit.dialog.default.commit.operation.name");
   }
 
   @Override
