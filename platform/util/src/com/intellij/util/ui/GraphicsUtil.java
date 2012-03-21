ea0ec4dc8a506de6dc65c88e6be1b544d4351fda
Konstantin Bulenkov <kb@jetbrains.com>
2012-03-21 16:12:43 +0100

    fix AA in editor (cherry picked from commit 190a4e5)

--- platform/util/src/com/intellij/util/ui/GraphicsUtil.java
+++ platform/util/src/com/intellij/util/ui/GraphicsUtil.java
@@ -25,21 +25,21 @@ import java.util.Map;
  */
 public class GraphicsUtil {
   public static void setupAntialiasing(@NotNull Graphics g2) {
-    setupAntialiasing(g2, true);
+    setupAntialiasing(g2, true, false);
   }
 
-  public static void setupAntialiasing(Graphics g2, boolean enable) {
+  public static void setupAntialiasing(Graphics g2, boolean enableAA, boolean ignoreSystemSettings) {
     if (g2 instanceof Graphics2D) {
       Graphics2D g = (Graphics2D)g2;
       Toolkit tk = Toolkit.getDefaultToolkit();
       //noinspection HardCodedStringLiteral
       Map map = (Map)tk.getDesktopProperty("awt.font.desktophints");
 
-      if (map != null) {
+      if (map != null && !ignoreSystemSettings) {
         g.addRenderingHints(map);
       } else {
         g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
-                           enable ? RenderingHints.VALUE_ANTIALIAS_ON : RenderingHints.VALUE_ANTIALIAS_OFF);
+                           enableAA ? RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HBGR : RenderingHints.VALUE_TEXT_ANTIALIAS_OFF);
       }
     }
   }
