7d0a82dc6b2f4df1701a35a75e2f110a5e7b719c
irengrig <Irina.Chernushina@jetbrains.com>
2012-03-26 15:27:25 +0400

    IDEA-83024 Show all affected files failed at SVN History window (cherry picked from commit 8054d5487bf8e278e764add33133c0faadb0aa6f) [review=kirill.l]

--- plugins/git4idea/src/git4idea/changes/GitCommittedChangeListProvider.java
+++ plugins/git4idea/src/git4idea/changes/GitCommittedChangeListProvider.java
@@ -266,6 +266,11 @@ public class GitCommittedChangeListProvider implements CommittedChangesProvider<
     return new Pair<CommittedChangeList, FilePath>(commit, ((GitFileRevision) history.get(history.size() - 1)).getPath());
   }
 
+  @Override
+  public RepositoryLocation getForNonLocal(VirtualFile file) {
+    return null;
+  }
+
   public int getFormatVersion() {
     return 0;
   }
