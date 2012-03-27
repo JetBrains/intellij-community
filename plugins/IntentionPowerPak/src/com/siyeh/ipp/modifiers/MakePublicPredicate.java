33a16efc5117f78d07835624eda039a4f15be04a
Bas Leijdekkers <basleijdekkers@gmail.com>
2012-03-27 20:26:21 +0200

    "Make Public" intention

---
+++ plugins/IntentionPowerPak/src/com/siyeh/ipp/modifiers/MakePublicPredicate.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2000-2012 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.siyeh.ipp.modifiers;
+
+import com.intellij.psi.*;
+import com.intellij.psi.javadoc.PsiDocComment;
+import com.siyeh.ipp.base.PsiElementPredicate;
+
+/**
+ * @author Bas Leijdekkers
+ */
+public class MakePublicPredicate implements PsiElementPredicate {
+
+  @Override
+  public boolean satisfiedBy(PsiElement element) {
+    final PsiElement parent = element.getParent();
+    if (!(parent instanceof PsiClass || parent instanceof PsiField || parent instanceof PsiMethod)) {
+      return false;
+    }
+    if (element instanceof PsiDocComment || element instanceof PsiCodeBlock) {
+      return false;
+    }
+    if (parent instanceof PsiClass) {
+      final PsiClass aClass = (PsiClass)parent;
+      final PsiElement brace = aClass.getLBrace();
+      if (brace != null && brace.getTextOffset() < element.getTextOffset()) {
+        return false;
+      }
+    }
+    final PsiModifierListOwner owner = (PsiModifierListOwner)parent;
+    final PsiModifierList modifierList = owner.getModifierList();
+    if (modifierList == null || modifierList.hasModifierProperty(PsiModifier.PUBLIC)) {
+      return false;
+    }
+    return true;
+  }
+}
