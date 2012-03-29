e3dd4dd5d761e090fd18e43dbf267343e9d03d3e
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2012-03-29 18:51:11 +0400

    IDEA-83670  CME
    Access myRepositories under read lock.
    Copy to a temp variable in updateAllRepositories not to lock during update.

--- plugins/git4idea/src/git4idea/repo/GitRepositoryManagerImpl.java
+++ plugins/git4idea/src/git4idea/repo/GitRepositoryManagerImpl.java
@@ -137,15 +137,32 @@ public class GitRepositoryManagerImpl extends AbstractProjectComponent implement
 
   @Override
   public void updateAllRepositories(GitRepository.TrackedTopic... topics) {
-    for (VirtualFile root : myRepositories.keySet()) {
+    Map<VirtualFile, GitRepository> repositories;
+    try {
+      REPO_LOCK.readLock().lock();
+      repositories = new HashMap<VirtualFile, GitRepository>(myRepositories);
+    }
+    finally {
+      REPO_LOCK.readLock().unlock();
+    }
+
+    for (VirtualFile root : repositories.keySet()) {
       updateRepository(root, topics);
     }
   }
 
   // note: we are not calling this method during the project startup - it is called anyway by the GitRootTracker
   private void updateRepositoriesCollection() {
-      Map<VirtualFile, GitRepository> repositories = new HashMap<VirtualFile, GitRepository>(myRepositories);
-      final VirtualFile[] roots = myVcsManager.getRootsUnderVcs(myVcs);
+    Map<VirtualFile, GitRepository> repositories;
+    try {
+      REPO_LOCK.readLock().lock();
+      repositories = new HashMap<VirtualFile, GitRepository>(myRepositories);
+    }
+    finally {
+      REPO_LOCK.readLock().unlock();
+    }
+
+    final VirtualFile[] roots = myVcsManager.getRootsUnderVcs(myVcs);
       // remove repositories that are not in the roots anymore
       for (Iterator<Map.Entry<VirtualFile, GitRepository>> iterator = repositories.entrySet().iterator(); iterator.hasNext(); ) {
         if (!ArrayUtil.contains(iterator.next().getValue().getRoot(), roots)) {
