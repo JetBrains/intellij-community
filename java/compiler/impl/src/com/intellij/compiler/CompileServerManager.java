d0f2335e340c428db99b3c493ec86992a3c4f2f4
Alexey Kudravtsev <cdr@intellij.com>
2012-03-30 17:25:13 +0400

    disabled automake

--- java/compiler/impl/src/com/intellij/compiler/CompileServerManager.java
+++ java/compiler/impl/src/com/intellij/compiler/CompileServerManager.java
@@ -286,6 +286,7 @@ public class CompileServerManager implements ApplicationComponent{
   }
 
   private void runAutoMake() {
+    if (ApplicationManager.getApplication().isUnitTestMode()) return;
     final Project[] openProjects = myProjectManager.getOpenProjects();
     if (openProjects.length > 0) {
       final List<RequestFuture> futures = new ArrayList<RequestFuture>();
