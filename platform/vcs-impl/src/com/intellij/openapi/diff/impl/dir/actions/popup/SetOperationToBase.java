732e0db1a38aa007a0e3eab568d3fefb79eb2084
Shaverdova Elena <eshaverdova@gmail.com>
2012-03-30 22:35:29 +0400

    proper update for context actions (cherry picked from commit 9fc638e)

--- platform/vcs-impl/src/com/intellij/openapi/diff/impl/dir/actions/popup/SetOperationToBase.java
+++ platform/vcs-impl/src/com/intellij/openapi/diff/impl/dir/actions/popup/SetOperationToBase.java
@@ -46,13 +46,21 @@ public abstract class SetOperationToBase extends AnAction {
   protected abstract DirDiffOperation getOperation();
 
   @Override
-  public void update(AnActionEvent e) {
+  public final void update(AnActionEvent e) {
     final DirDiffTableModel model = getModel(e);
     final JTable table = getTable(e);
-    e.getPresentation().setEnabled(table != null
-                                   && model != null
-                                   && !model.getSelectedElements().isEmpty());
+    if (table != null && model != null) {
+      for (DirDiffElement element : model.getSelectedElements()) {
+        if (isEnabledFor(element)) {
+          e.getPresentation().setEnabled(true);
+          return;
+        }
+      }
+    }
+    e.getPresentation().setEnabled(false);
   }
+
+  protected abstract boolean isEnabledFor(DirDiffElement element);
 
   @Nullable
   private static JTable getTable(AnActionEvent e) {
