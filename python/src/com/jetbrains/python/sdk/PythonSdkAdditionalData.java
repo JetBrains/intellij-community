d6518a34a114706a846409c8a82bb3ecb4f669dc
Dmitry Jemerov <yole@jetbrains.com>
2012-04-04 13:25:33 +0200

    store path of associated project for VirtualEnv

--- python/src/com/jetbrains/python/sdk/PythonSdkAdditionalData.java
+++ python/src/com/jetbrains/python/sdk/PythonSdkAdditionalData.java
@@ -2,9 +2,11 @@ package com.jetbrains.python.sdk;
 
 import com.google.common.collect.Sets;
 import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.projectRoots.SdkAdditionalData;
 import com.intellij.openapi.projectRoots.SdkModel;
+import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.jetbrains.django.util.VirtualFileUtil;
 import org.jdom.Element;
@@ -24,11 +26,13 @@ public class PythonSdkAdditionalData implements SdkAdditionalData {
   @NonNls private static final String PATH_ADDED_BY_USER = "PATH_ADDED_BY_USER";
   @NonNls private static final String PATHS_REMOVED_BY_USER_ROOT = "PATHS_REMOVED_BY_USER_ROOT";
   @NonNls private static final String PATH_REMOVED_BY_USER = "PATH_REMOVED_BY_USER";
+  @NonNls private static final String ASSOCIATED_PROJECT_PATH = "ASSOCIATED_PROJECT_PATH";
 
 
   private Set<VirtualFile> myAddedPaths = Sets.newHashSet();
   private Set<VirtualFile> myExcludedPaths = Sets.newHashSet();
   private final PythonSdkFlavor myFlavor;
+  private String myAssociatedProjectPath;
 
   public PythonSdkAdditionalData(@Nullable PythonSdkFlavor flavor) {
     myFlavor = flavor;
@@ -39,6 +43,7 @@ public class PythonSdkAdditionalData implements SdkAdditionalData {
       final PythonSdkAdditionalData copy = (PythonSdkAdditionalData)super.clone();
       copy.setAddedPaths(getAddedPaths());
       copy.setExcludedPaths(getExcludedPaths());
+      copy.setAssociatedProjectPath(getAssociatedProjectPath());
       return copy;
     }
     catch (CloneNotSupportedException e) {
@@ -62,6 +67,21 @@ public class PythonSdkAdditionalData implements SdkAdditionalData {
     myExcludedPaths = Sets.newHashSet(excludedPaths);
   }
 
+  public String getAssociatedProjectPath() {
+    return myAssociatedProjectPath;
+  }
+
+  public void setAssociatedProjectPath(@Nullable String associatedProjectPath) {
+    myAssociatedProjectPath = associatedProjectPath;
+  }
+
+  public void associateWithProject(Project project) {
+    final String path = project.getBasePath();
+    if (path != null) {
+      myAssociatedProjectPath = FileUtil.toSystemIndependentName(path);
+    }
+  }
+
   @Override
   public void checkValid(SdkModel sdkModel) throws ConfigurationException {
 
@@ -78,6 +98,9 @@ public class PythonSdkAdditionalData implements SdkAdditionalData {
       final Element child = new Element(PATHS_REMOVED_BY_USER_ROOT);
       child.setAttribute(PATH_REMOVED_BY_USER, removed.getPath());
       rootElement.addContent(child);
+    }
+    if (myAssociatedProjectPath != null) {
+      rootElement.setAttribute(ASSOCIATED_PROJECT_PATH, myAssociatedProjectPath);
     }
   }
 
@@ -98,6 +121,7 @@ public class PythonSdkAdditionalData implements SdkAdditionalData {
   protected static void load(@Nullable Element element, @NotNull PythonSdkAdditionalData data) {
     data.setAddedPaths(loadStringList(element, PATHS_ADDED_BY_USER_ROOT, PATH_ADDED_BY_USER));
     data.setExcludedPaths(loadStringList(element, PATHS_REMOVED_BY_USER_ROOT, PATH_REMOVED_BY_USER));
+    data.setAssociatedProjectPath(element.getAttributeValue(ASSOCIATED_PROJECT_PATH));
   }
 
   private static Set<VirtualFile> loadStringList(@Nullable Element element, @NotNull String rootName, @NotNull String attrName) {
