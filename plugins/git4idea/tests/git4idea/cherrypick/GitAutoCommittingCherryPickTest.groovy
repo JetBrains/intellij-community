d9535ae7e3acd654b2933bd599795b45d5639ec0
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2012-04-05 20:07:38 +0400

    join GitAutoCommittingCherryPickTest

--- plugins/git4idea/tests/git4idea/cherrypick/GitAutoCommittingCherryPickTest.groovy
+++ plugins/git4idea/tests/git4idea/cherrypick/GitAutoCommittingCherryPickTest.groovy
@@ -23,22 +23,107 @@ import com.intellij.notification.NotificationType
 import git4idea.test.MockGit
 
 import static git4idea.test.MockGit.OperationName.CHERRY_PICK
-import static git4idea.test.MockGit.OperationName.CHERRY_PICK
-import static git4idea.test.MockGit.OperationName.CHERRY_PICK
+import com.intellij.notification.Notification
 import com.intellij.openapi.vcs.changes.ui.CommitChangeListDialog
 import git4idea.tests.TestDialogHandler
 import com.intellij.openapi.ui.DialogWrapper
 
 /**
- *   // multiple commits with "commit at once" option
+ * Cherry-pick of one or multiple commits with "commit at once" option enabled.
+ *
  * @author Kirill Likhodedov
  */
-class GitMultipleAutoCommittingCherryPickTest extends GitCherryPickTest {
+class GitAutoCommittingCherryPickTest extends GitCherryPickTest {
 
   @Before
-  protected void setUp() {
+  void setUp() {
     super.setUp()
     myCherryPicker = new CherryPicker(myProject, myGit, myPlatformFacade, myRepositoryManager, true)
+  }
+
+  @Test
+  void "clean tree, no conflicts, then commit & notify, no new changelists"() {
+    GitCommit commit = commit()
+    invokeCherryPick(commit)
+
+    assertHeadCommit(commit)
+    assertOnlyDefaultChangelist()
+    assertNotificationShown("Successful cherry-pick", notificationContent(commit), NotificationType.INFORMATION)
+  }
+
+  @Test
+  void "dirty tree, conflicting with commit, then show error"() {
+    myGit.registerOperationExecutors(new MockGit.SimpleErrorOperationExecutor(CHERRY_PICK, LOCAL_CHANGES_OVERWRITTEN_BY_CHERRY_PICK))
+
+    invokeCherryPick(commit())
+
+    assertNotCherryPicked()
+    assertOnlyDefaultChangelist()
+    assertNotificationShown(new Notification(TEST_NOTIFICATION_GROUP, "Cherry-pick failed",
+                                             "Your local changes to some files would be overwritten by cherry-pick. View them",
+                                             NotificationType.ERROR))
+  }
+
+  @Test
+  void "conflict, merge dialog, not all merged, then new & active changelist"() {
+    prepareConflict()
+    myGit.registerOperationExecutors(new MockGit.SimpleErrorOperationExecutor(MockGit.OperationName.GET_UNMERGED_FILES,
+"""
+100644 d87b28d6fd6e97620603e64ce70fc2f24535ec28 1\ttest.txt
+100644 7b50450f5deb7cce3b5ce92ba866f1af6e58c3c6 2\ttest.txt
+100644 a784477cdd0437a84751c52f72b971503deb48cb 3\ttest.txt
+"""
+    ))
+
+    GitCommit commit = commit()
+    invokeCherryPick(commit)
+    assertMergeDialogShown()
+    assertChangeLists([DEFAULT, commit.getSubject()], commit.getSubject())
+  }
+
+  @Test
+  void "conflict, merge completed, then commit dialog"() {
+    prepareConflict()
+    GitCommit commit = commit()
+    invokeCherryPick(commit)
+    assertMergeDialogShown()
+    assertCommitDialogShown()
+  }
+
+  @Test
+  void "conflict, merge finished, commit succeeded, no new changelists"() {
+    prepareConflict()
+
+    myDialogManager.registerDialogHandler(CommitChangeListDialog, new TestDialogHandler<CommitChangeListDialog>() {
+      @Override
+      int handleDialog(CommitChangeListDialog dialog) {
+        return DialogWrapper.OK_EXIT_CODE
+      }
+    })
+
+    GitCommit commit = commit()
+    invokeCherryPick(commit)
+    assertMergeDialogShown()
+    assertCommitDialogShown()
+    assertOnlyDefaultChangelist()
+  }
+
+  @Test
+  void "conflict, merge ok, commit cancelled, then new & active changelist"() {
+    prepareConflict()
+
+    myDialogManager.registerDialogHandler(CommitChangeListDialog, new TestDialogHandler<CommitChangeListDialog>() {
+      @Override
+      int handleDialog(CommitChangeListDialog dialog) {
+        return DialogWrapper.CANCEL_EXIT_CODE
+      }
+    })
+
+    GitCommit commit = commit()
+    invokeCherryPick(commit)
+    assertMergeDialogShown()
+    assertCommitDialogShown()
+    assertChangeLists([DEFAULT, commit.getSubject()], commit.getSubject())
   }
 
   @Test
