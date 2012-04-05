9242d622133214140c70829b2ca1c6ecca3a91c8
anna <Anna.Kozlova@jetbrains.com>
2012-04-05 13:14:33 +0200

    good code is red: wildcard should check the bound if any in order to prove difference (IDEA-63291)

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/GenericsHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/GenericsHighlightingTest.java
@@ -130,6 +130,7 @@ public class GenericsHighlightingTest extends LightDaemonAnalyzerTestCase {
   public void testIDEA66311_16() throws Exception { doTest(false);}
   public void testIDEA76283() throws Exception {doTest(false);}
   public void testIDEA74899() throws Exception {doTest(false);}
+  public void testIDEA63291() throws Exception {doTest(false);}
 
   public void testJavaUtilCollections_NoVerify() throws Exception {
     PsiClass collectionsClass = getJavaFacade().findClass("java.util.Collections", GlobalSearchScope.moduleWithLibrariesScope(getModule()));
