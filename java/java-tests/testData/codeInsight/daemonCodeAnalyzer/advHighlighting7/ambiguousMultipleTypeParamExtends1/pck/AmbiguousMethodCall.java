8fe1604bd17b54044389182a21a6a075ec8be0a8
anna <Anna.Kozlova@jetbrains.com>
2012-04-08 14:05:51 +0200

    show warning on type parameter declaration if it lead to multiple methods with same erasure (java7 only)

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/advHighlighting7/ambiguousMultipleTypeParamExtends1/pck/AmbiguousMethodCall.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/advHighlighting7/ambiguousMultipleTypeParamExtends1/pck/AmbiguousMethodCall.java
@@ -9,7 +9,7 @@ interface IB{
     <T> void foo(A<T> x);
 }
 class C {
-    <T extends IA & IB> void bar(T x, A<String> y){
+    <<error descr="'foo(A<T>)' in 'pck.IB' clashes with 'foo(A<?>)' in 'pck.IA'; both methods have same erasure, yet neither overrides the other"></error>T extends IA & IB> void bar(T x, A<String> y){
         x.foo<error descr="Ambiguous method call: both 'IA.foo(A<?>)' and 'IB.foo(A<String>)' match">(y)</error>;
     }
 }
--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/advHighlighting7/ambiguousMultipleTypeParamExtends2/pck/AmbiguousMethodCall.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/advHighlighting7/ambiguousMultipleTypeParamExtends2/pck/AmbiguousMethodCall.java
@@ -9,7 +9,7 @@ interface IB{
     <T> void foo(A<? super A<T>> x);
 }
 class C {
-    <T extends IB & IA> void bar(T x, A<A<String>> y){
+    <<error descr="'foo(A<A<T>>)' in 'pck.IA' clashes with 'foo(A<? super A<T>>)' in 'pck.IB'; both methods have same erasure, yet neither overrides the other"></error>T extends IB & IA> void bar(T x, A<A<String>> y){
         x.foo(y);
     }
 }
