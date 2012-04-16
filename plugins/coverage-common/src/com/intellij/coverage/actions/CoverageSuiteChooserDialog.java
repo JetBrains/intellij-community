ad49aea6ecc0db1b1ee48a4006133a28806e4c2d
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2012-04-16 18:06:52 +0200

    Simplify file chooser API; fix EA-35601

--- plugins/coverage-common/src/com/intellij/coverage/actions/CoverageSuiteChooserDialog.java
+++ plugins/coverage-common/src/com/intellij/coverage/actions/CoverageSuiteChooserDialog.java
@@ -274,12 +274,12 @@ public class CoverageSuiteChooserDialog extends DialogWrapper {
     @Override
     public void actionPerformed(AnActionEvent e) {
       final VirtualFile file =
-        FileChooser.chooseFile(myProject, new FileChooserDescriptor(true, false, false, false, false, false) {
+        FileChooser.chooseFile(new FileChooserDescriptor(true, false, false, false, false, false) {
           @Override
           public boolean isFileSelectable(VirtualFile file) {
             return getCoverageRunner(file) != null;
           }
-        });
+        }, myProject, null);
       if (file != null) {
         final CoverageRunner coverageRunner = getCoverageRunner(file);
         LOG.assertTrue(coverageRunner != null);
