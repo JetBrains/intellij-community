92299d4ef933730832a8a250b9491b168339bf1e
Shaverdova Elena <eshaverdova@gmail.com>
2012-04-20 20:04:16 +0400

    Structure view

---
+++ src/com/jetbrains/typoscript/TypoScriptIcons.java
@@ -0,0 +1,15 @@
+package com.jetbrains.typoscript;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * @author lene
+ *         Date: 20.04.12
+ */
+public interface TypoScriptIcons {
+  Icon CONDITION_ICON = IconLoader.getIcon("/icons/condition_ts.png");
+  Icon PROPERTY_ICON = IconLoader.getIcon("/icons/property_ts.png");
+  Icon INCLUDE_ICON = IconLoader.getIcon("/icons/include_ts.png");
+}
---
+++ src/com/jetbrains/typoscript/lang/TypoScriptStructureViewFactory.java
@@ -0,0 +1,162 @@
+package com.jetbrains.typoscript.lang;
+
+import com.intellij.ide.structureView.*;
+import com.intellij.ide.util.treeView.smartTree.TreeElement;
+import com.intellij.lang.PsiStructureViewFactory;
+import com.intellij.navigation.ItemPresentation;
+import com.intellij.pom.Navigatable;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.util.PlatformIcons;
+import com.jetbrains.typoscript.TypoScriptIcons;
+import com.jetbrains.typoscript.lang.psi.*;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+/**
+ * @author lene
+ *         Date: 20.04.12
+ */
+public class TypoScriptStructureViewFactory implements PsiStructureViewFactory {
+  private static final Class[] SUITABLE_CLASSES =
+    new Class[]{Assignment.class, CodeBlock.class, ConditionElement.class, Copying.class, IncludeStatementElement.class,
+      MultilineValueAssignment.class, Unsetting.class, ValueModification.class};
+
+  @Override
+  public StructureViewBuilder getStructureViewBuilder(final PsiFile psiFile) {
+
+    return new TreeBasedStructureViewBuilder() {
+      @NotNull
+      public StructureViewModel createStructureViewModel() {
+        return new Model(psiFile);
+      }
+
+      @Override
+      public boolean isRootNodeShown() {
+        return false;
+      }
+    };
+  }
+
+  private static class Model extends StructureViewModelBase implements StructureViewModel.ElementInfoProvider {
+
+    public Model(@NotNull PsiFile psiFile) {
+      super(psiFile, new Element(psiFile));
+      withSuitableClasses(SUITABLE_CLASSES);
+    }
+
+    @Override
+    public boolean isAlwaysShowsPlus(StructureViewTreeElement element) {
+      return false;
+    }
+
+    @Override
+    public boolean isAlwaysLeaf(StructureViewTreeElement element) {
+      Object psi = element.getValue();
+      return !(psi instanceof TypoScriptFile || psi instanceof CodeBlock);
+    }
+  }
+
+  private static class Element implements StructureViewTreeElement, ItemPresentation {
+    private final PsiElement myElement;
+
+    public Element(PsiElement element) {
+      this.myElement = element;
+    }
+
+    @Override
+    public Object getValue() {
+      return myElement;
+    }
+
+    @Override
+    public void navigate(boolean requestFocus) {
+      ((Navigatable)myElement).navigate(requestFocus);
+    }
+
+    @Override
+    public boolean canNavigate() {
+      return ((Navigatable)myElement).canNavigate();
+    }
+
+    @Override
+    public boolean canNavigateToSource() {
+      return ((Navigatable)myElement).canNavigateToSource();
+    }
+
+    @Override
+    public ItemPresentation getPresentation() {
+      return this;
+    }
+
+    @Override
+    public String getPresentableText() {
+      if (myElement instanceof TypoScriptFile) {
+        return myElement.getContainingFile().getName();
+      }
+      else if (myElement instanceof IncludeStatementElement) {
+        return myElement.getText();
+      }
+      else if (myElement instanceof ConditionElement) {
+        return myElement.getText();
+      }
+      ObjectPath objectPath = PsiTreeUtil.getChildOfType(myElement, ObjectPath.class);
+      if (objectPath != null) {
+        return objectPath.getText();
+      }
+      throw new AssertionError(myElement.getClass().getName() + " " + myElement.getText());
+    }
+
+    @Override
+    public String getLocationString() {
+      return null;
+    }
+
+    @Override
+    public Icon getIcon(boolean open) {
+      if (myElement instanceof TypoScriptFile) {
+        return myElement.getIcon(0);
+      }
+      else if (myElement instanceof IncludeStatementElement) {
+        return TypoScriptIcons.INCLUDE_ICON;
+      }
+      else if (myElement instanceof ConditionElement) {
+        return TypoScriptIcons.CONDITION_ICON;
+      }
+      else if (myElement instanceof CodeBlock) {
+        return PlatformIcons.FOLDER_ICON;
+      }
+      return TypoScriptIcons.PROPERTY_ICON;
+    }
+
+    @Override
+    public TreeElement[] getChildren() {
+      if (myElement instanceof TypoScriptFile || myElement instanceof CodeBlock) {
+        final ArrayList<TreeElement> result = new ArrayList<TreeElement>();
+        for (PsiElement child : myElement.getChildren()) {
+          if (isSuitable(child)) {
+            result.add(new Element(child));
+          }
+        }
+        return result.toArray(new TreeElement[result.size()]);
+      }
+      return new TreeElement[0];
+    }
+  }
+
+
+  private static boolean isSuitable(PsiElement element) {
+    if (!(element instanceof TypoScriptCompositeElement)) {
+      return false;
+    }
+    for (Class aClass : SUITABLE_CLASSES) {
+      if (aClass.isInstance(element)) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
