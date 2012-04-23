4168e43a01e3cd669771b0cbffc6449cb1be554c
Andrey Vlasovskikh <andrey.vlasovskikh@gmail.com>
2012-04-23 17:37:52 +0400

    Fixed assignment balance inspection for sliced tuples (PY-6315)

--- python/src/com/jetbrains/python/psi/PySliceExpression.java
+++ python/src/com/jetbrains/python/psi/PySliceExpression.java
@@ -1,11 +1,13 @@
 package com.jetbrains.python.psi;
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
  * @author yole
  */
 public interface PySliceExpression extends PyExpression {
+  @NotNull
   PyExpression getOperand();
 
   @Nullable
