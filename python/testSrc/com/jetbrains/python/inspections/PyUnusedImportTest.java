1deb0c5c9fc02508a89dfd2a080ea935ab67e273
Andrey Vlasovskikh <andrey.vlasovskikh@gmail.com>
2012-04-26 17:21:13 +0400

    Refactored tests of unused imports

--- python/testSrc/com/jetbrains/python/inspections/PyUnusedImportTest.java
+++ python/testSrc/com/jetbrains/python/inspections/PyUnusedImportTest.java
@@ -1,34 +1,35 @@
 package com.jetbrains.python.inspections;
 
 import com.jetbrains.python.fixtures.PyTestCase;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author yole
  */
 public class PyUnusedImportTest extends PyTestCase {
-  public void testModuleAndSubmodule() {  // PY-3626
-    myFixture.copyDirectoryToProject("inspections/unusedImport/moduleAndSubmodule", "");
-    myFixture.enableInspections(PyUnresolvedReferencesInspection.class);
-    myFixture.testHighlighting(true, false, false, "py3626.py");
+  // PY-3626
+  public void testModuleAndSubmodule() {
+    doTest("py3626.py");
   }
 
-  public void testSubpackageInInitPy() {  // PY-3201
-    myFixture.copyDirectoryToProject("inspections/unusedImport/subpackageInInitPy", "");
-    myFixture.enableInspections(PyUnresolvedReferencesInspection.class);
-    myFixture.testHighlighting(true, false, false, "package1/__init__.py");
+  // PY-3201
+  public void testSubpackageInInitPy() {
+    doTest("package1/__init__.py");
   }
 
   // PY-5589
   public void testUnusedPackageAndSubmodule() {
-    myFixture.copyDirectoryToProject("inspections/unusedImport/unusedPackageAndSubmodule", "");
-    myFixture.enableInspections(PyUnresolvedReferencesInspection.class);
-    myFixture.testHighlighting(true, false, false, "test1.py");
+    doTest("test1.py");
   }
 
   // PY-5621
   public void testUnusedSubmodule() {
-    myFixture.copyDirectoryToProject("inspections/unusedImport/unusedSubmodule", "");
+    doTest("test1.py");
+  }
+
+  private void doTest(@NotNull String filename) {
+    myFixture.copyDirectoryToProject("inspections/unusedImport/" + getTestName(true), "");
     myFixture.enableInspections(PyUnresolvedReferencesInspection.class);
-    myFixture.testHighlighting(true, false, false, "test1.py");
+    myFixture.testHighlighting(true, false, false, filename);
   }
 }
