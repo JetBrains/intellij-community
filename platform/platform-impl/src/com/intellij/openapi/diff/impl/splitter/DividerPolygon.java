6406390f066c7f236bc1ba0bc2aadc6a3d70ecb3
Konstantin Bulenkov <kb@jetbrains.com>
2012-04-30 16:46:37 +0200

    fix tests

--- platform/platform-impl/src/com/intellij/openapi/diff/impl/splitter/DividerPolygon.java
+++ platform/platform-impl/src/com/intellij/openapi/diff/impl/splitter/DividerPolygon.java
@@ -27,7 +27,8 @@ import com.intellij.util.ui.UIUtil;
 import java.awt.*;
 import java.util.ArrayList;
 
-class DividerPolygon {
+public class DividerPolygon {
+  public static final int OFFSET = 3;
   private final Color myColor;
   private final int myStart1;
   private final int myStart2;
@@ -114,7 +115,6 @@ class DividerPolygon {
   }
 
   private static DividerPolygon createPolygon(Transformation[] transformations, Trapezium trapezium, Color color, FragmentSide left) {
-    final int offset = 3; //IDEA-85146
     Interval base1 = trapezium.getBase(left);
     Interval base2 = trapezium.getBase(left.otherSide());
     Transformation leftTransform = transformations[left.getIndex()];
@@ -123,7 +123,7 @@ class DividerPolygon {
     int end1 = leftTransform.transform(base1.getEnd());
     int start2 = rightTransform.transform(base2.getStart());
     int end2 = rightTransform.transform(base2.getEnd());
-    return new DividerPolygon(start1 - offset, start2 - offset, end1 - offset, end2 - offset, color);
+    return new DividerPolygon(start1 - OFFSET, start2 - OFFSET, end1 - OFFSET, end2 - OFFSET, color);
   }
 
   static Interval getVisibleInterval(Editor editor) {
