b8d7e6f27f965d36349510cf9688c205a513e7db
Dmitry Jemerov <yole@jetbrains.com>
2012-04-30 18:16:24 +0200

    try really hard to display nice and unique names in editor tabs (IDEA-76832)

--- platform/lang-impl/src/com/intellij/openapi/fileEditor/impl/UniqueNameEditorTabTitleProvider.java
+++ platform/lang-impl/src/com/intellij/openapi/fileEditor/impl/UniqueNameEditorTabTitleProvider.java
@@ -4,11 +4,6 @@ import com.intellij.ide.ui.UISettings;
 import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.search.FilenameIndex;
-import com.intellij.psi.search.ProjectScope;
-
-import java.io.File;
 
 /**
  * @author yole
@@ -19,14 +14,6 @@ public class UniqueNameEditorTabTitleProvider implements EditorTabTitleProvider 
     if (!UISettings.getInstance().SHOW_DIRECTORY_FOR_NON_UNIQUE_FILENAMES || DumbService.isDumb(project)) {
       return null;
     }
-    VirtualFile parent = file.getParent();
-    if (parent == null) {
-      return null;
-    }
-    final PsiFile[] filesWithSameName = FilenameIndex.getFilesByName(project, file.getName(), ProjectScope.getProjectScope(project));
-    if (filesWithSameName.length > 1) {
-      return parent.getName() + File.separator + file.getPresentableName();
-    }
-    return null;
+    return UniqueNameBuilder.getUniqueVirtualFilePath(project, file);
   }
 }
