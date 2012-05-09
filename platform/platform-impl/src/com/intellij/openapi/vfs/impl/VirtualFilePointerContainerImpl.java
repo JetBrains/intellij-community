a6acaf7ab629c2f575aa4c1c0e9cf6d427549c1c
Dmitry Jemerov <yole@jetbrains.com>
2012-05-09 18:45:44 +0200

    dead code

--- platform/platform-impl/src/com/intellij/openapi/vfs/impl/VirtualFilePointerContainerImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/vfs/impl/VirtualFilePointerContainerImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2012 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -257,11 +257,6 @@ public class VirtualFilePointerContainerImpl implements VirtualFilePointerContai
   @Override
   public int size() {
     return myList.size();
-  }
-
-  @Override
-  public Object get(int index) {
-    return myList.get(index);
   }
 
   public boolean equals(Object o) {
