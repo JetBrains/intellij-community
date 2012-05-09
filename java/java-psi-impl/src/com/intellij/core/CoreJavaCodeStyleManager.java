f3b1e7b9b1c21e16d191f993edcca2c296f5e7b7
Evgeny Pasynkov <Evgeny.Pasynkov@jetbrains.com>
2012-05-09 13:35:15 +0200

    +CoreJavaCodeStyleManager

---
+++ java/java-psi-impl/src/com/intellij/core/CoreJavaCodeStyleManager.java
@@ -0,0 +1,11 @@
+package com.intellij.core;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pasynkov
+ * Date: 09.05.12
+ * Time: 13:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class CoreJavaCodeStyleManager {
+}
