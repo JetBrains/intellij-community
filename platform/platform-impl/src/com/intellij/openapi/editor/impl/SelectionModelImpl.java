28ff5f0241f698e25eac9bf8f40b1cacf38f654f
Denis.Zhdanov <Denis.Zhdanov@jetbrains.com>
2012-05-11 09:43:16 +0400

    IDEA-85100 Deleting wrapped lines with Ctrl+Y doen't work properly with "soft wrapped" lines

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/SelectionModelImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/SelectionModelImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2012 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -721,7 +721,7 @@ public class SelectionModelImpl implements SelectionModel, PrioritizedDocumentLi
       return;
     }
 
-    Pair<LogicalPosition, LogicalPosition> lines = EditorUtil.calcCaretLinesRange(myEditor);
+    Pair<LogicalPosition, LogicalPosition> lines = EditorUtil.calcCaretLineRange(myEditor);
     LogicalPosition lineStart = lines.first;
     LogicalPosition nextLineStart = lines.second;
 
