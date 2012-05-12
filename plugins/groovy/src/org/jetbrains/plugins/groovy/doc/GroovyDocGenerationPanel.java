aa27329967970bc6e32019ba254c4b83bd762fb2
Maxim.Medvedev <maxim.medvedev@jetbrains.com>
2012-05-12 17:32:10 +0400

    fix minimum size for 'Generate GroovyDoc' dialog

--- plugins/groovy/src/org/jetbrains/plugins/groovy/doc/GroovyDocGenerationPanel.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/doc/GroovyDocGenerationPanel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2012 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -75,7 +75,7 @@ public final class GroovyDocGenerationPanel extends JPanel {
 
     actionToolbar.updateActionsImmediately();
 
-    myPanel.setMinimumSize(new Dimension(-1, 350));
+    myPanel.setMinimumSize(new Dimension(275, 350));
   }
 
   private ActionGroup getActionGroup() {
