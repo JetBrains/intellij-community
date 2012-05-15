e5b412d4c3b1787c02697cb478ff6e5c796d2b90
Maxim.Medvedev <maxim.medvedev@jetbrains.com>
2012-05-15 19:04:46 +0400

    'create groovy method from usage' position caret at the beginning of selection

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/createConstructorFromCall/afterAnon.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/createConstructorFromCall/afterAnon.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2000-2012 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // "Create Constructor" "true"
 class Test {
 
@@ -7,7 +23,7 @@ class Test {
 
     class Inner {
         public Inner(String a) {
-            <selection>//To change body of created methods use File | Settings | File Templates.</selection><caret>
+            <caret><selection>//To change body of created methods use File | Settings | File Templates.</selection>
         }
     }
 }
--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/genericCreateFromUsage/after4.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/genericCreateFromUsage/after4.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2000-2012 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // "Create Method 'foo'" "true"
 interface Comparable<T> {
 }
@@ -8,7 +24,7 @@ class R implements Comparable<R> {
 
 class A1<T> {
     public T foo() {
-        <selection>return null;  //To change body of created methods use File | Settings | File Templates.<caret></selection>
+        <caret><selection>return null;  //To change body of created methods use File | Settings | File Templates.</selection>
     }
 }
 
