a153662512feda75ad50df3def384a0f5ccb54b6
Dmitry Jemerov <yole@jetbrains.com>
2012-05-21 12:43:41 +0200

    exception protection (IDEA-86335)

--- platform/platform-api/src/com/intellij/openapi/ide/CopyPasteManager.java
+++ platform/platform-api/src/com/intellij/openapi/ide/CopyPasteManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2011 JetBrains s.r.o.
+ * Copyright 2000-2012 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,7 +39,7 @@ public abstract class CopyPasteManager {
 
   public abstract void removeContentChangedListener(ContentChangedListener listener);
 
-  public abstract boolean isDataFlavorAvailable(@NotNull DataFlavor flavor);
+  public abstract boolean isDataFlavorAvailable(@Nullable DataFlavor flavor);
 
   @Nullable
   public abstract Transferable getContents();
