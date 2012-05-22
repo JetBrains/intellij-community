rtyley/intellij-community-set-committer-to-author

IDEA-84488 control flow of try with some catches

plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/controlFlow/impl/ControlFlowBuilder.java
@@ -430,9 +430,8 @@ public class ControlFlowBuilder extends GroovyRecursiveElementVisitor {
 
       if (ControlFlowUtils.isIncOrDecOperand(refExpr) && !myAssertionsOnly) {
         final InstructionImpl i = new ReadWriteVariableInstruction(name, refExpr, myInstructionNumber++, READ);
-        addNode(i);
+        addNodeAndCheckPending(i);
         addNode(new ReadWriteVariableInstruction(name, refExpr, myInstructionNumber++, WRITE));
-        checkPending(i);
       }
       else {
         boolean isWrite = !myAssertionsOnly && PsiUtil.isLValue(refExpr);
@@ -818,24 +817,31 @@ public class ControlFlowBuilder extends GroovyRecursiveElementVisitor {
         }
       }
 
+      //save added postcalls into separate list because we don't want returnInstruction grabbed their pending edges
+      List<Pair<InstructionImpl, GroovyPsiElement>> pendingPostCalls = myPending;
+      myPending = new ArrayList<Pair<InstructionImpl, GroovyPsiElement>>();
+
       myHead = finallyInstruction;
       finallyClause.accept(this);
-      final ReturnInstruction returnInstruction = new ReturnInstruction(myInstructionNumber++);
+      final ReturnInstruction returnInstruction = new ReturnInstruction(finallyClause, myInstructionNumber++);
       for (AfterCallInstruction postCall : postCalls) {
         postCall.setReturnInstruction(returnInstruction);
         addEdge(returnInstruction, postCall);
       }
-      addNode(returnInstruction);
+      addNodeAndCheckPending(returnInstruction);
       interruptFlow();
       finishNode(finallyInstruction);
 
       assert oldPending != null;
-      oldPending.addAll(myPending);
+      oldPending.addAll(pendingPostCalls);
       myPending = oldPending;
     }
     else {
       if (tryEnd != null) {
         addPendingEdge(tryCatchStatement, tryEnd);
+      }
+      for (InstructionImpl catchEnd : catches) {
+        addPendingEdge(tryBlock, catchEnd);
       }
     }
   }

JetBrains/intellij-community

plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/controlFlow/impl/ControlFlowBuilder.java
@@ -804,24 +804,32 @@ public class ControlFlowBuilder extends GroovyRecursiveElementVisitor {
         }
       }
 
+      //save added postcalls into separate list because we don't want returnInstruction grabbed their pending edges
+      List<Pair<InstructionImpl, GroovyPsiElement>> pendingPostCalls = myPending;
+      myPending = new ArrayList<Pair<InstructionImpl, GroovyPsiElement>>();
+
       myHead = finallyInstruction;
       finallyClause.accept(this);
-      final ReturnInstruction returnInstruction = new ReturnInstruction(myInstructionNumber++);
+      final ReturnInstruction returnInstruction = new ReturnInstruction(finallyClause, myInstructionNumber++);
       for (AfterCallInstruction postCall : postCalls) {
         postCall.setReturnInstruction(returnInstruction);
         addEdge(returnInstruction, postCall);
       }
       addNode(returnInstruction);
+      checkPending(returnInstruction);
       interruptFlow();
       finishNode(finallyInstruction);
 
       assert oldPending != null;
-      oldPending.addAll(myPending);
+      oldPending.addAll(pendingPostCalls);
       myPending = oldPending;
     }
     else {
       if (tryEnd != null) {
         addPendingEdge(tryCatchStatement, tryEnd);
+      }
+      for (InstructionImpl catchEnd : catches) {
+        addPendingEdge(tryBlock, catchEnd);
       }
     }
   }