b5921927fc8c686b6fb0f15f8faa4b4300326d14
Anna Kozlova <anna.kozlova@jetbrains.com>
2012-05-23 13:35:00 +0400

    wrong redundant cast suggestion (IDEA-86095)

--- java/java-tests/testData/inspection/redundantCast/IDEADEV25675/src/A.java
+++ java/java-tests/testData/inspection/redundantCast/IDEADEV25675/src/A.java
@@ -6,4 +6,10 @@ public class Test {
     public void bar(String[] array) {
         ((Object[]) array)[0] = new Object();
     }
+
+    public Object obj;  
+    public static void setObjValueInArray(Test t, Object val) {
+      t.obj = new Object[1];
+      ((Object[])t.obj)[0] = val;
+    }
 }
