3c440e3dc4d259726c6eeca2ed81fe5c53ed7470
Eugene Kudelevsky <Eugene.Kudelevsky@jetbrains.com>
2012-05-25 18:43:05 +0400

    use copyDir instead of moveDirWithContent because tmp dir may be on other disk

--- plugins/android/src/org/jetbrains/android/compiler/AndroidAptCompiler.java
+++ plugins/android/src/org/jetbrains/android/compiler/AndroidAptCompiler.java
@@ -122,11 +122,9 @@ public class AndroidAptCompiler implements SourceGeneratingCompiler {
                 context.addMessage(CompilerMessageCategory.ERROR, "Cannot create directory " + parent.getPath(), null, -1, -1);
                 continue;
               }
-              if (!FileUtil.moveDirWithContent(tmpOutputDir, genRootDir)) {
-                final String message = "Cannot move content from " + tmpOutputDir.getPath() + " to " + genRootPath;
-                context.addMessage(CompilerMessageCategory.ERROR, message, null, -1, -1);
-                continue;
-              }
+              // we use copyDir instead of moveDirWithContent here, because tmp directory may be located on other disk and
+              // moveDirWithContent doesn't work for such case
+              FileUtil.copyDir(tmpOutputDir, genRootDir);
             }
             results.add(aptItem);
           }
