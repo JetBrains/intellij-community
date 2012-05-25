abc32a5df3726f00a53cae20c00ff53e3d7e4896
Shaverdova Elena <eshaverdova@gmail.com>
2012-05-25 23:17:13 +0400

    Fixture for movement of TypoScript to contrib repository

--- plugins/typoScript/testData/typoscript/lexer/copyingOperator.test.ts
+++
@@ -1,3 +0,0 @@
-myOtherObject < myObject
-20 <  pageObj.10 strange text
-30 <.10
--- plugins/typoScript/testData/typoscript/lexer/valueModificationOperator.test.ts
+++
@@ -1,7 +0,0 @@
-value1:=addToList(4,5)
-value2:=addToList(4,5
-value3:=( 4 and some text   )
-value4:
-value5:=add To My List ( 4,5)
-value6:=add To My List(4,5)  some IGNORED_TEXT
-value7: addToList( ,5)
