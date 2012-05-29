1a0e96bbdd1f18f33fb658ee66d710e9861dbf5f
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2012-05-29 12:03:44 +0400

    [git] Make GitUnstashDialog non-modal.
    So that the user could access the editor and the project while viewing stashed changes.

--- plugins/git4idea/src/git4idea/ui/GitUnstashDialog.java
+++ plugins/git4idea/src/git4idea/ui/GitUnstashDialog.java
@@ -124,6 +124,7 @@ public class GitUnstashDialog extends DialogWrapper {
    */
   public GitUnstashDialog(final Project project, final List<VirtualFile> roots, final VirtualFile defaultRoot) {
     super(project, true);
+    setModal(false);
     myProject = project;
     myVcs = GitVcs.getInstance(project);
     setTitle(GitBundle.getString("unstash.title"));
