c63295cda95b251d04a50fb05fe195e8556af6ec
Danila Ponomarenko <Danila.Ponomarenko@jetbrains.com>
2012-06-08 21:49:54 +0400

    understood and reverted

--- plugins/properties/src/com/intellij/lang/properties/editor/ResourceBundleUtil.java
+++ plugins/properties/src/com/intellij/lang/properties/editor/ResourceBundleUtil.java
@@ -15,7 +15,6 @@
  */
 package com.intellij.lang.properties.editor;
 
-import com.intellij.openapi.util.text.StringUtil;
 import gnu.trove.TIntHashSet;
 import org.jetbrains.annotations.NotNull;
 
@@ -28,8 +27,9 @@ import java.util.Properties;
  */
 public class ResourceBundleUtil {
 
-  private static final String ADDITIONAL_ESCAPE_SYMBOLS = "#!=:";
-
+  private static final TIntHashSet SYMBOLS_TO_ESCAPE = new TIntHashSet(new int[]{'#', '!', '=', ':'});
+  private static final char        ESCAPE_SYMBOL     = '\\';
+  
   private ResourceBundleUtil() {
   }
 
@@ -37,25 +37,47 @@ public class ResourceBundleUtil {
    * Allows to map given 'raw' property value text to the 'user-friendly' text to show at the resource bundle editor.
    * <p/>
    * <b>Note:</b> please refer to {@link Properties#store(Writer, String)} contract for the property value escape rules.
-   *
-   * @param text 'raw' property value text
-   * @return 'user-friendly' text to show at the resource bundle editor
+   * 
+   * @param text  'raw' property value text
+   * @return      'user-friendly' text to show at the resource bundle editor
    */
   @NotNull
   public static String fromPropertyValueToValueEditor(@NotNull String text) {
-    return StringUtil.unescapeStringCharacters(text);
+    StringBuilder buffer = new StringBuilder();
+    boolean escaped = false;
+    for (int i = 0; i < text.length(); i++) {
+      char c = text.charAt(i);
+      if (c == ESCAPE_SYMBOL && !escaped) {
+        escaped = true;
+        continue;
+      }
+      buffer.append(c);
+      escaped = false;
+    }
+    return buffer.toString();
   }
 
   /**
-   * Performs reverse operation to {@link #fromPropertyValueToValueEditor(String)}.
-   *
-   * @param text 'user-friendly' text shown to the user at the resource bundle editor
-   * @return 'raw' value to store at the *.properties file
+   * Perform reverse operation to {@link #fromPropertyValueToValueEditor(String)}.
+   * 
+   * @param text  'user-friendly' text shown to the user at the resource bundle editor
+   * @return      'raw' value to store at the *.properties file
    */
   @NotNull
   public static String fromValueEditorToPropertyValue(@NotNull String text) {
-    final StringBuilder buffer = new StringBuilder();
-    StringUtil.escapeStringCharacters(text.length(), text, ADDITIONAL_ESCAPE_SYMBOLS, true, buffer);
+    StringBuilder buffer = new StringBuilder();
+    for (int i = 0; i < text.length(); i++) {
+      char c = text.charAt(i);
+      
+      if ((i == 0 && (c == ' ' || c == '\t')) // Leading white space
+          || c == '\n'                        // Multi-line value
+          || c == ESCAPE_SYMBOL               // Escaped 'escape' symbol
+          || SYMBOLS_TO_ESCAPE.contains(c))   // Special symbol
+      {
+        buffer.append(ESCAPE_SYMBOL);
+      }
+      buffer.append(c);
+    }
     return buffer.toString();
   }
 }
