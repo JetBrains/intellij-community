e2937d0f0a62ca2ca4a24f826539c8ae8598f26a
Eugene Kudelevsky <Eugene.Kudelevsky@jetbrains.com>
2012-06-09 20:43:45 +0400

    IDEA-78338 pass cannonical adb path

--- plugins/android/src/org/jetbrains/android/sdk/AndroidSdkData.java
+++ plugins/android/src/org/jetbrains/android/sdk/AndroidSdkData.java
@@ -41,6 +41,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.awt.*;
 import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -316,9 +317,15 @@ public class AndroidSdkData {
   private String getAdbPath() {
     String path = getLocation() + File.separator + SdkConstants.OS_SDK_PLATFORM_TOOLS_FOLDER + SdkConstants.FN_ADB;
     if (!new File(path).exists()) {
-      return getLocation() + File.separator + AndroidCommonUtils.toolPath(SdkConstants.FN_ADB);
+      path = getLocation() + File.separator + AndroidCommonUtils.toolPath(SdkConstants.FN_ADB);
     }
-    return path;
+    try {
+      return new File(path).getCanonicalPath();
+    }
+    catch (IOException e) {
+      LOG.info(e);
+      return path;
+    }
   }
 
   public static void terminateDdmlib() {
