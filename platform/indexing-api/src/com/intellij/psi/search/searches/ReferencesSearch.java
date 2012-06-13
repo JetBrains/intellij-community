e002004e4a06f1c232de81520b6f1e4e910e6be1
Evgeny Pasynkov <Evgeny.Pasynkov@jetbrains.com>
2012-06-13 18:24:21 +0200

    extension points

--- platform/indexing-api/src/com/intellij/psi/search/searches/ReferencesSearch.java
+++ platform/indexing-api/src/com/intellij/psi/search/searches/ReferencesSearch.java
@@ -28,7 +28,7 @@ import org.jetbrains.annotations.Nullable;
  * @author max
  */
 public class ReferencesSearch extends ExtensibleQueryFactory<PsiReference, ReferencesSearch.SearchParameters> {
-  public static ExtensionPointName<ReferencesSearch> EP_NAME = ExtensionPointName.create("com.intellij.referencesSearch");
+  public static ExtensionPointName<QueryExecutor> EP_NAME = ExtensionPointName.create("com.intellij.referencesSearch");
   private static final ReferencesSearch INSTANCE = new ReferencesSearch();
 
   private ReferencesSearch() {
