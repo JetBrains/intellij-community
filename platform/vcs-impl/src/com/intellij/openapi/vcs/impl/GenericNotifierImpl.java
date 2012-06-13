b88bb2aec2965d6d0504f6c5086c697d67526a2c
peter <peter@jetbrains.com>
2012-06-13 21:27:09 +0200

    handle inability to login to p4 (IDEA-48151)

--- platform/vcs-impl/src/com/intellij/openapi/vcs/impl/GenericNotifierImpl.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/impl/GenericNotifierImpl.java
@@ -21,6 +21,7 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
+import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -94,17 +95,11 @@ public abstract class GenericNotifierImpl<T, Key> {
   }
 
   private void expireNotification(final MyNotification<T> notification) {
-    final Application application = ApplicationManager.getApplication();
-    final Runnable runnable = new Runnable() {
+    UIUtil.invokeLaterIfNeeded(new Runnable() {
       public void run() {
         notification.expire();
       }
-    };
-    if (application.isDispatchThread()) {
-      runnable.run();
-    } else {
-      application.invokeLater(runnable, ModalityState.NON_MODAL, myProject.getDisposed());
-    }
+    });
   }
 
   public boolean ensureNotify(final T obj) {
