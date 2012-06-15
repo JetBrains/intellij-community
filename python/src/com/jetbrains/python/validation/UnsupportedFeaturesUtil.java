aa5de6afb7b76a1123a9c9a40cc7da37fcd6bdd6
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2012-06-15 18:53:28 +0400

    added updater for versions.xml fixed PY-6704 Update compatibility inspection for Python 3.3

--- python/src/com/jetbrains/python/validation/UnsupportedFeaturesUtil.java
+++ python/src/com/jetbrains/python/validation/UnsupportedFeaturesUtil.java
@@ -40,6 +40,7 @@ public class UnsupportedFeaturesUtil {
     ALL_LANGUAGE_LEVELS.add(LanguageLevel.PYTHON30.toString());
     ALL_LANGUAGE_LEVELS.add(LanguageLevel.PYTHON31.toString());
     ALL_LANGUAGE_LEVELS.add(LanguageLevel.PYTHON32.toString());
+    ALL_LANGUAGE_LEVELS.add(LanguageLevel.PYTHON33.toString());
   }
 
   private static void fillMaps() throws IOException {
