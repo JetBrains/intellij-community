5f522f2ac0705376c5201b578fbc8115cbf110dc
Bas Leijdekkers <basleijdekkers@gmail.com>
2012-06-22 10:04:46 +0200

    Make "Merge nested 'try' statements" intention applicable in more cases

--- plugins/IntentionPowerPak/testSrc/com/siyeh/ipp/exceptions/MergeNestedTryStatementsIntentionTest.java
+++ plugins/IntentionPowerPak/testSrc/com/siyeh/ipp/exceptions/MergeNestedTryStatementsIntentionTest.java
@@ -1,23 +1,3 @@
-/*
- * Copyright 2000-2012 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
- * (c) 2012 Desert Island BV
- * created: 16 06 2012
- */
 package com.siyeh.ipp.exceptions;
 
 import com.siyeh.IntentionPowerPackBundle;
@@ -29,6 +9,7 @@ import com.siyeh.ipp.IPPTestCase;
 public class MergeNestedTryStatementsIntentionTest extends IPPTestCase {
 
   public void testSimple() { doTest(); }
+  public void testWithoutAndWithResources() { doTest(); }
 
   @Override
   protected String getIntentionName() {
