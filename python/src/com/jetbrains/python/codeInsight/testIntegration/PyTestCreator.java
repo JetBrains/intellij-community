2a2fe5ea312aad335671947b59864e10c672460b
Dmitry Jemerov <yole@jetbrains.com>
2012-06-29 20:07:58 +0200

    remove incorrect extension registration (IDEA-87117)

--- python/src/com/jetbrains/python/codeInsight/testIntegration/PyTestCreator.java
+++ python/src/com/jetbrains/python/codeInsight/testIntegration/PyTestCreator.java
@@ -1,12 +1,22 @@
 package com.jetbrains.python.codeInsight.testIntegration;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.ex.IdeDocumentHistory;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Computable;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.codeStyle.CodeStyleManager;
+import com.intellij.psi.impl.source.PostprocessReformattingAspect;
 import com.intellij.testIntegration.TestCreator;
 import com.intellij.util.IncorrectOperationException;
+import com.jetbrains.python.PythonFileType;
+import com.jetbrains.python.codeInsight.imports.AddImportHelper;
+import com.jetbrains.python.psi.*;
+
+import java.util.List;
 
 /**
  * User: catherine
@@ -34,5 +44,50 @@ public class PyTestCreator implements TestCreator {
     catch (IncorrectOperationException e) {
       LOG.warn(e);
     }
+  }
+
+  public static PsiFile generateTest(final Project project, final CreateTestDialog dialog) {
+    return PostprocessReformattingAspect.getInstance(project).postponeFormattingInside(new Computable<PsiFile>() {
+      public PsiFile compute() {
+        return ApplicationManager.getApplication().runWriteAction(new Computable<PsiFile>() {
+          public PsiFile compute() {
+            try {
+              IdeDocumentHistory.getInstance(project).includeCurrentPlaceAsChangePlace();
+
+              String fileName = dialog.getFileName();
+              if (!fileName.endsWith(".py"))
+                fileName = fileName + "." + PythonFileType.INSTANCE.getDefaultExtension();
+
+              StringBuilder fileText = new StringBuilder();
+              fileText.append("class ").append(dialog.getClassName()).append("(TestCase):\n  ");
+              List<String> methods = dialog.getMethods();
+              if (methods.size() == 0)
+                fileText.append("pass\n");
+
+              for (String method : methods) {
+                fileText.append("def ").append(method).append("(self):\n    self.fail()\n\n  ");
+              }
+
+              PsiFile psiFile = PyUtil.getOrCreateFile(
+                dialog.getTargetDir() + "/" + fileName, project);
+              AddImportHelper.addImportFrom(psiFile, null, "unittest", "TestCase", null, AddImportHelper.ImportPriority.BUILTIN);
+
+              PyElement createdClass = PyElementGenerator.getInstance(project).createFromText(
+                LanguageLevel.forElement(psiFile), PyClass.class,
+                                                           fileText.toString());
+              createdClass = (PyElement)psiFile.addAfter(createdClass, psiFile.getLastChild());
+
+              CodeStyleManager.getInstance(project).reformat(psiFile);
+              createdClass.navigate(false);
+              return psiFile;
+            }
+            catch (IncorrectOperationException e) {
+              LOG.warn(e);
+              return null;
+            }
+          }
+        });
+      }
+    });
   }
 }
