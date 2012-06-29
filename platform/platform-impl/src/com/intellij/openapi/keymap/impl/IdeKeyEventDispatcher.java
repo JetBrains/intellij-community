05142b03ffe7d04ffeb8dff3962ac8c67107fc9f
Dmitry Jemerov <yole@jetbrains.com>
2012-06-29 19:47:12 +0200

    setting to force ctrl-alt to be processed as alt-gr (IDEA-87400)

--- platform/platform-impl/src/com/intellij/openapi/keymap/impl/IdeKeyEventDispatcher.java
+++ platform/platform-impl/src/com/intellij/openapi/keymap/impl/IdeKeyEventDispatcher.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2012 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -375,6 +375,9 @@ public final class IdeKeyEventDispatcher implements Disposable {
 
     // http://www.jetbrains.net/jira/browse/IDEADEV-12372
     if (myLeftCtrlPressed && myRightAltPressed && focusOwner != null && e.getModifiers() == (InputEvent.CTRL_MASK | InputEvent.ALT_MASK)) {
+      if (Registry.is("actionSystem.force.alt.gr")) {
+        return false;
+      }
       final InputContext inputContext = focusOwner.getInputContext();
       if (inputContext != null) {
         Locale locale = inputContext.getLocale();
