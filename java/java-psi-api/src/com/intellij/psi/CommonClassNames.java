68c017caef18576d41ca661ede994fa54d187876
Maxim.Medvedev <maxim.medvedev@jetbrains.com>
2012-07-02 14:41:57 +0400

    IDEA-87991 Static type checking false negative ignoring automatic type conversion

--- java/java-psi-api/src/com/intellij/psi/CommonClassNames.java
+++ java/java-psi-api/src/com/intellij/psi/CommonClassNames.java
@@ -87,4 +87,5 @@ public interface CommonClassNames {
   @NonNls String JAVA_LANG_INVOKE_MH_POLYMORPHIC = "java.lang.invoke.MethodHandle.PolymorphicSignature";
   String TARGET_ANNOTATION_FQ_NAME = "java.lang.annotation.Target";
   @NonNls String JAVA_LANG_RUNNABLE = "java.lang.Runnable";
+  @NonNls String JAVA_IO_FILE = "java.io.File";
 }
