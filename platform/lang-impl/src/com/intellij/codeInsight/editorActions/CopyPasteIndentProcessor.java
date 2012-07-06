16d8c49fce01bf64c6a3cd1b2670999427e2ccf3
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2012-07-06 15:33:36 +0400

    fixed PY-6887 Paste changes code logic when making top-level function inner

--- platform/lang-impl/src/com/intellij/codeInsight/editorActions/CopyPasteIndentProcessor.java
+++ platform/lang-impl/src/com/intellij/codeInsight/editorActions/CopyPasteIndentProcessor.java
@@ -65,7 +65,7 @@ public class CopyPasteIndentProcessor implements CopyPastePostProcessor<IndentTr
     }
     int firstNonSpaceChar = CharArrayUtil.shiftForward(document.getCharsSequence(), startOffsets[0], " \t");
     int firstLineLeadingSpaces = (firstNonSpaceChar <= document.getLineEndOffset(selStartLine)) ? firstNonSpaceChar - startOffsets[0] : 0;
-    return new IndentTransferableData(minIndent, maxIndent, firstLineLeadingSpaces);
+    return new IndentTransferableData(maxIndent - minIndent, maxIndent, firstLineLeadingSpaces);
   }
 
   private static boolean acceptFileType(FileType fileType) {
@@ -152,7 +152,7 @@ public class CopyPasteIndentProcessor implements CopyPastePostProcessor<IndentTr
         }
 
         final List<String> strings = StringUtil.split(pastedText, "\n");
-        if (caretColumn > value.getIndent() && !strings.isEmpty() &&
+        if (caretColumn >= value.getIndent() && !strings.isEmpty() &&
             StringUtil.isEmptyOrSpaces(strings.get(strings.size()-1))) endLine -=1;
 
         for (int i = startLine+1; i <= endLine; i++) {
