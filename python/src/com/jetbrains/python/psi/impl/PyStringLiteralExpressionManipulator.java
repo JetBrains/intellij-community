bf0be0278978efd78f19c7a9900b63216f860e04
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2012-07-17 14:47:45 +0200

    #PY-6979 Fixed: Fixed rename of template parameter

--- python/src/com/jetbrains/python/psi/impl/PyStringLiteralExpressionManipulator.java
+++ python/src/com/jetbrains/python/psi/impl/PyStringLiteralExpressionManipulator.java
@@ -1,6 +1,7 @@
 package com.jetbrains.python.psi.impl;
 
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.AbstractElementManipulator;
 import com.jetbrains.python.PythonStringUtil;
@@ -13,10 +14,14 @@ public class PyStringLiteralExpressionManipulator extends AbstractElementManipul
   private static final Logger LOG = Logger.getInstance(PyStringLiteralExpressionManipulator.class);
 
   public PyStringLiteralExpressionImpl handleContentChange(PyStringLiteralExpressionImpl element, TextRange range, String newContent) {
-    String newName = range.replace(element.getText(), newContent);
-    if (!PythonStringUtil.isQuoted(newName)) {
-      LOG.error("Should be quoted: " + newName);
+    Pair<String, String> quotes = PythonStringUtil.getQuotes(range.substring(element.getText()));
+
+    if (quotes != null) {
+      range = TextRange.create(range.getStartOffset() + quotes.first.length(), range.getEndOffset() - quotes.second.length());
     }
+
+    String newName = range.replace(element.getText(), newContent);
+
     return (PyStringLiteralExpressionImpl)element
       .replace(PyElementGenerator.getInstance(element.getProject()).createStringLiteralAlreadyEscaped(newName));
   }
