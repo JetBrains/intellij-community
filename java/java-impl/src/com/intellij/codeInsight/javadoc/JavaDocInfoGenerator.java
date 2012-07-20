1491f18c7b044bb7f161f306e61aaebaf5b0c58c
Denis.Zhdanov <Denis.Zhdanov@jetbrains.com>
2012-07-20 13:20:40 +0400

    EA-37552 - NPE: JavaDocInfoGenerator.isEmptyDescription

--- java/java-impl/src/com/intellij/codeInsight/javadoc/JavaDocInfoGenerator.java
+++ java/java-impl/src/com/intellij/codeInsight/javadoc/JavaDocInfoGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2011 JetBrains s.r.o.
+ * Copyright 2000-2012 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -855,6 +855,9 @@ public class JavaDocInfoGenerator {
   }
 
   private static boolean isEmptyDescription(PsiDocComment comment) {
+    if (comment == null) {
+      return true;
+    }
     PsiElement[] descriptionElements = comment.getDescriptionElements();
 
     for (PsiElement description : descriptionElements) {
