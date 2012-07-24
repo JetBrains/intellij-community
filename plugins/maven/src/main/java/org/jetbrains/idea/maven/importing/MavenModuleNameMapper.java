97ef06692d7fd6193b380b74eec5026f40dd3fe8
Sergey Evdokimov <sergey.evdokimov@jetbrains.com>
2012-07-24 14:15:57 +0400

    Add additional diagnostic

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/importing/MavenModuleNameMapper.java
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/importing/MavenModuleNameMapper.java
@@ -102,10 +102,7 @@ public class MavenModuleNameMapper {
       mavenProjectToModuleName.put(each.project, each.getResultName());
     }
 
-    assert new HashSet<String>(mavenProjectToModuleName.values()).size() == mavenProjectToModuleName.size() : mavenProjectToModuleName
-                                                                                                                .values() +
-                                                                                                              "\n||\n" +
-                                                                                                              mavenProjectToModuleName;
+    assert new HashSet<String>(mavenProjectToModuleName.values()).size() == mavenProjectToModuleName.size() : mavenProjectToModuleName;
   }
 
   public static class NameItem implements Comparable<NameItem> {
