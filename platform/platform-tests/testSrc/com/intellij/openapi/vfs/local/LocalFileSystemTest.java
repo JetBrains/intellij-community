7ce3d4f4f491811f0b772e40a228c4c5b103fbbb
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2012-07-26 16:31:39 +0200

    Test cleanup

--- platform/platform-tests/testSrc/com/intellij/openapi/vfs/local/LocalFileSystemTest.java
+++ platform/platform-tests/testSrc/com/intellij/openapi/vfs/local/LocalFileSystemTest.java
@@ -16,7 +16,6 @@
 package com.intellij.openapi.vfs.local;
 
 import com.intellij.ide.GeneralSettings;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.util.io.FileAttributes;
 import com.intellij.openapi.util.io.FileUtil;
@@ -33,216 +32,122 @@ import com.intellij.openapi.vfs.newvfs.impl.VirtualDirectoryImpl;
 import com.intellij.openapi.vfs.newvfs.persistent.PersistentFS;
 import com.intellij.testFramework.PlatformLangTestCase;
 import com.intellij.testFramework.PlatformTestUtil;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.nio.charset.Charset;
 import java.util.Arrays;
 
 public class LocalFileSystemTest extends PlatformLangTestCase {
-  public static void setContentOnDisk(File file, byte[] bom, String content, Charset charset) throws IOException {
-    FileOutputStream stream = new FileOutputStream(file);
-    if (bom != null) {
-      stream.write(bom);
+  public void testChildrenAccessedButNotCached() throws Exception {
+    File dir = createTempDirectory(false);
+    ManagingFS managingFS = ManagingFS.getInstance();
+
+    VirtualFile vFile = LocalFileSystem.getInstance().refreshAndFindFileByPath(dir.getPath().replace(File.separatorChar, '/'));
+    assertNotNull(vFile);
+    assertFalse(managingFS.areChildrenLoaded(vFile));
+    assertFalse(managingFS.wereChildrenAccessed(vFile));
+
+    File child = new File(dir, "child");
+    boolean created = child.createNewFile();
+    assertTrue(created);
+
+    File subdir = new File(dir, "subdir");
+    boolean subdirCreated = subdir.mkdir();
+    assertTrue(subdirCreated);
+
+    File subChild = new File(subdir, "subdir");
+    boolean subChildCreated = subChild.createNewFile();
+    assertTrue(subChildCreated);
+
+    VirtualFile childVFile = LocalFileSystem.getInstance().refreshAndFindFileByPath(child.getPath().replace(File.separatorChar, '/'));
+    assertNotNull(childVFile);
+    assertFalse(managingFS.areChildrenLoaded(vFile));
+    assertTrue(managingFS.wereChildrenAccessed(vFile));
+
+    VirtualFile subdirVFile = LocalFileSystem.getInstance().refreshAndFindFileByPath(subdir.getPath().replace(File.separatorChar, '/'));
+    assertNotNull(subdirVFile);
+    assertFalse(managingFS.areChildrenLoaded(subdirVFile));
+    assertFalse(managingFS.wereChildrenAccessed(subdirVFile));
+    assertFalse(managingFS.areChildrenLoaded(vFile));
+    assertTrue(managingFS.wereChildrenAccessed(vFile));
+
+    vFile.getChildren();
+    assertTrue(managingFS.areChildrenLoaded(vFile));
+    assertTrue(managingFS.wereChildrenAccessed(vFile));
+    assertFalse(managingFS.areChildrenLoaded(subdirVFile));
+    assertFalse(managingFS.wereChildrenAccessed(subdirVFile));
+
+    VirtualFile subChildVFile = LocalFileSystem.getInstance().refreshAndFindFileByPath(subChild.getPath().replace(File.separatorChar, '/'));
+    assertNotNull(subChildVFile);
+    assertTrue(managingFS.areChildrenLoaded(vFile));
+    assertTrue(managingFS.wereChildrenAccessed(vFile));
+    assertFalse(managingFS.areChildrenLoaded(subdirVFile));
+    assertTrue(managingFS.wereChildrenAccessed(subdirVFile));
+  }
+
+  public void testRefreshAndFindFile() throws Exception {
+    File dir = createTempDirectory();
+
+    VirtualFile vFile = LocalFileSystem.getInstance().refreshAndFindFileByPath(dir.getPath().replace(File.separatorChar, '/'));
+    assertNotNull(vFile);
+    vFile.getChildren();
+
+    for (int i = 0; i < 100; i++) {
+      File subdir = new File(dir, "a" + i);
+      assertTrue(subdir.mkdir());
     }
-    OutputStreamWriter writer = new OutputStreamWriter(stream, charset);
-    try {
-      writer.write(content);
-    }
-    finally {
-      writer.close();
-    }
+
+    File subdir = new File(dir, "aaa");
+    assertTrue(subdir.mkdir());
+
+    VirtualFile file = LocalFileSystem.getInstance().refreshAndFindFileByPath(subdir.getPath().replace(File.separatorChar, '/'));
+    assertNotNull(file);
   }
 
-  public static VirtualFile createTempFile(@NonNls String ext, @Nullable byte[] bom, @NonNls String content, Charset charset) throws IOException {
-    File temp = FileUtil.createTempFile("copy", "." + ext);
-    setContentOnDisk(temp, bom, content, charset);
+  public void testCopyFile() throws Exception {
+    File fromDir = createTempDirectory();
+    File toDir = createTempDirectory();
 
-    myFilesToDelete.add(temp);
-    final VirtualFile file = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(temp);
-    assert file != null : temp;
-    return file;
+    VirtualFile fromVDir = LocalFileSystem.getInstance().findFileByPath(fromDir.getPath().replace(File.separatorChar, '/'));
+    VirtualFile toVDir = LocalFileSystem.getInstance().findFileByPath(toDir.getPath().replace(File.separatorChar, '/'));
+    assertNotNull(fromVDir);
+    assertNotNull(toVDir);
+    final VirtualFile fileToCopy = fromVDir.createChildData(this, "temp_file");
+    final byte[] byteContent = {0, 1, 2, 3};
+    fileToCopy.setBinaryContent(byteContent);
+    final String newName = "new_temp_file";
+    final VirtualFile copy = fileToCopy.copy(this, toVDir, newName);
+    assertEquals(newName, copy.getName());
+    assertTrue(Arrays.equals(byteContent, copy.contentsToByteArray()));
   }
 
-  public void testChildrenAccessedButNotCached() throws Exception{
-    ApplicationManager.getApplication().runWriteAction(
-      new Runnable() {
-        @Override
-        public void run() {
-          try{
-            File dir = createTempDirectory(false);
-            final ManagingFS managingFS = ManagingFS.getInstance();
+  public void testCopyDir() throws Exception {
+    File fromDir = createTempDirectory();
+    File toDir = createTempDirectory();
 
-            VirtualFile vFile = LocalFileSystem.getInstance().refreshAndFindFileByPath(dir.getPath().replace(File.separatorChar, '/'));
-            assertNotNull(vFile);
-            assertFalse(managingFS.areChildrenLoaded(vFile));
-            assertFalse(managingFS.wereChildrenAccessed(vFile));
-
-            final File child = new File(dir, "child");
-            final boolean created = child.createNewFile();
-            assertTrue(created);
-
-            final File subdir = new File(dir, "subdir");
-            final boolean subdirCreated = subdir.mkdir();
-            assertTrue(subdirCreated);
-
-            final File subChild = new File(subdir, "subdir");
-            final boolean subChildCreated = subChild.createNewFile();
-            assertTrue(subChildCreated);
-
-            final VirtualFile childVFile = LocalFileSystem.getInstance().refreshAndFindFileByPath(child.getPath().replace(File.separatorChar, '/'));
-            assertNotNull(childVFile);
-            assertFalse(managingFS.areChildrenLoaded(vFile));
-            assertTrue(managingFS.wereChildrenAccessed(vFile));
-
-            final VirtualFile subdirVFile = LocalFileSystem.getInstance().refreshAndFindFileByPath(subdir.getPath().replace(File.separatorChar, '/'));
-            assertNotNull(subdirVFile);
-            assertFalse(managingFS.areChildrenLoaded(subdirVFile));
-            assertFalse(managingFS.wereChildrenAccessed(subdirVFile));
-
-            assertFalse(managingFS.areChildrenLoaded(vFile));
-            assertTrue(managingFS.wereChildrenAccessed(vFile));
-            
-            
-            vFile.getChildren();
-            assertTrue(managingFS.areChildrenLoaded(vFile));
-            assertTrue(managingFS.wereChildrenAccessed(vFile));
-            assertFalse(managingFS.areChildrenLoaded(subdirVFile));
-            assertFalse(managingFS.wereChildrenAccessed(subdirVFile));
-            
-            final VirtualFile subChildVFile = LocalFileSystem.getInstance().refreshAndFindFileByPath(subChild.getPath().replace(File.separatorChar, '/'));
-            assertNotNull(subChildVFile);
-            assertTrue(managingFS.areChildrenLoaded(vFile));
-            assertTrue(managingFS.wereChildrenAccessed(vFile));
-            assertFalse(managingFS.areChildrenLoaded(subdirVFile));
-            assertTrue(managingFS.wereChildrenAccessed(subdirVFile));
-          }
-          catch(IOException e){
-            LOG.error(e);
-          }
-        }
-      }
-    );
-  }
-  
-  public void testRefreshAndFindFile() throws Exception{
-    ApplicationManager.getApplication().runWriteAction(
-      new Runnable() {
-        @Override
-        public void run() {
-          try{
-            File dir = createTempDirectory();
-
-            VirtualFile vFile = LocalFileSystem.getInstance().refreshAndFindFileByPath(dir.getPath().replace(File.separatorChar, '/'));
-            assertNotNull(vFile);
-            vFile.getChildren();
-
-            for(int i = 0; i < 100; i++){
-              File subdir = new File(dir, "a" + i);
-              assertTrue(subdir.mkdir());
-            }
-
-            File subdir = new File(dir, "aaa");
-            assertTrue(subdir.mkdir());
-
-            VirtualFile file = LocalFileSystem.getInstance().refreshAndFindFileByPath(subdir.getPath().replace(File.separatorChar, '/'));
-            assertNotNull(file);
-          }
-          catch(IOException e){
-            LOG.error(e);
-          }
-        }
-      }
-    );
+    VirtualFile fromVDir = LocalFileSystem.getInstance().findFileByPath(fromDir.getPath().replace(File.separatorChar, '/'));
+    VirtualFile toVDir = LocalFileSystem.getInstance().findFileByPath(toDir.getPath().replace(File.separatorChar, '/'));
+    assertNotNull(fromVDir);
+    assertNotNull(toVDir);
+    final VirtualFile dirToCopy = fromVDir.createChildDirectory(this, "dir");
+    final VirtualFile file = dirToCopy.createChildData(this, "temp_file");
+    file.setBinaryContent(new byte[]{0, 1, 2, 3});
+    final String newName = "dir";
+    final VirtualFile dirCopy = dirToCopy.copy(this, toVDir, newName);
+    assertEquals(newName, dirCopy.getName());
+    PlatformTestUtil.assertDirectoriesEqual(toVDir, fromVDir, null);
   }
 
-  public void testCopyFile() throws Exception{
-    ApplicationManager.getApplication().runWriteAction(
-      new Runnable() {
-        @Override
-        public void run() {
-          try {
-            File fromDir = createTempDirectory();
-            File toDir = createTempDirectory();
-
-            VirtualFile fromVDir = LocalFileSystem.getInstance().findFileByPath(fromDir.getPath().replace(File.separatorChar, '/'));
-            VirtualFile toVDir = LocalFileSystem.getInstance().findFileByPath(toDir.getPath().replace(File.separatorChar, '/'));
-            assertNotNull(fromVDir);
-            assertNotNull(toVDir);
-            final VirtualFile fileToCopy = fromVDir.createChildData(this, "temp_file");
-            final byte[] byteContent = {0, 1, 2, 3};
-            fileToCopy.setBinaryContent(byteContent);
-            final String newName = "new_temp_file";
-            final VirtualFile copy = fileToCopy.copy(this, toVDir, newName);
-            assertEquals(newName, copy.getName());
-            assertTrue(Arrays.equals(byteContent, copy.contentsToByteArray()));
-          }
-          catch (Exception e) {
-            LOG.error(e);
-          }
-        }
-      }
-    );
-  }
-
-  public void testCopyDir() throws Exception{
-    ApplicationManager.getApplication().runWriteAction(
-      new Runnable() {
-        @Override
-        public void run() {
-          try{
-            File fromDir = createTempDirectory();
-            File toDir = createTempDirectory();
-
-            VirtualFile fromVDir = LocalFileSystem.getInstance().findFileByPath(fromDir.getPath().replace(File.separatorChar, '/'));
-            VirtualFile toVDir = LocalFileSystem.getInstance().findFileByPath(toDir.getPath().replace(File.separatorChar, '/'));
-            assertNotNull(fromVDir);
-            assertNotNull(toVDir);
-            final VirtualFile dirToCopy = fromVDir.createChildDirectory(this, "dir");
-            final VirtualFile file = dirToCopy.createChildData(this, "temp_file");
-            file.setBinaryContent(new byte[]{0, 1, 2, 3});
-            final String newName = "dir";
-            final VirtualFile dirCopy = dirToCopy.copy(this, toVDir, newName);
-            assertEquals(newName, dirCopy.getName());
-            PlatformTestUtil.assertDirectoriesEqual(toVDir, fromVDir, null);
-          }
-          catch(Exception e){
-            LOG.error(e);
-          }
-        }
-      }
-    );
-  }
-
-  public void testGermanLetters() throws Exception{
+  public void testGermanLetters() throws Exception {
     final File dirFile = createTempDirectory();
-
     final String name = "te\u00dft123123123.txt";
     final File childFile = new File(dirFile, name);
     assert childFile.createNewFile() || childFile.exists() : childFile;
 
-    ApplicationManager.getApplication().runWriteAction(
-      new Runnable() {
-        @Override
-        public void run() {
-          try{
-            final VirtualFile dir = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(dirFile);
-            assertNotNull(dir);
+    final VirtualFile dir = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(dirFile);
+    assertNotNull(dir);
 
-            final VirtualFile child = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(childFile);
-            assertNotNull(child);
-
-          }
-          catch(Exception e){
-            LOG.error(e);
-          }
-        }
-      }
-    );
+    final VirtualFile child = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(childFile);
+    assertNotNull(child);
 
     assertTrue(childFile.delete());
   }
