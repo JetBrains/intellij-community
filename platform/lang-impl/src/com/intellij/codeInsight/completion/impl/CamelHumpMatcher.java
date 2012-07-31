8e3a36dffbeb77759366efc8812fd9816c2cd890
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2012-07-31 18:36:37 +0400

    RubyMine 4.5.1. Try to fix tests =)

--- platform/lang-impl/src/com/intellij/codeInsight/completion/impl/CamelHumpMatcher.java
+++ platform/lang-impl/src/com/intellij/codeInsight/completion/impl/CamelHumpMatcher.java
@@ -1,22 +1,25 @@
-
 package com.intellij.codeInsight.completion.impl;
 
 import com.intellij.codeInsight.CodeInsightSettings;
 import com.intellij.codeInsight.completion.PrefixMatcher;
 import com.intellij.codeInsight.lookup.LookupElement;
-import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.Disposable;
+import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.codeStyle.MinusculeMatcher;
 import com.intellij.psi.codeStyle.NameUtil;
-import com.intellij.util.text.Matcher;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.TestOnly;
 
 /**
  * @author peter
-*/
+ */
 public class CamelHumpMatcher extends PrefixMatcher {
-  private final Matcher myMatcher;
+  private final MinusculeMatcher myMatcher;
   private final boolean myCaseSensitive;
+  private static boolean ourForceStartMatching;
+
 
   public CamelHumpMatcher(@NotNull final String prefix) {
     this(prefix, true);
@@ -55,29 +58,26 @@ public class CamelHumpMatcher extends PrefixMatcher {
     return new CamelHumpMatcher(prefix, myCaseSensitive);
   }
 
-  private Matcher createMatcher(final boolean caseSensitive) {
+  private MinusculeMatcher createMatcher(final boolean caseSensitive) {
     String prefix = applyMiddleMatching(myPrefix);
 
     if (!caseSensitive) {
-      return NameUtil.buildCompletionMatcher(prefix, 0, true, true);
+      return NameUtil.buildMatcher(prefix, NameUtil.MatchingCaseSensitivity.NONE);
     }
 
     switch (CodeInsightSettings.getInstance().COMPLETION_CASE_SENSITIVE) {
       case CodeInsightSettings.NONE:
-        return NameUtil.buildCompletionMatcher(prefix, 0, true, true);
+        return NameUtil.buildMatcher(prefix, NameUtil.MatchingCaseSensitivity.NONE);
       case CodeInsightSettings.FIRST_LETTER:
-        int exactPrefixLen = prefix.startsWith("*") ? 0 : 1;
-        return NameUtil.buildCompletionMatcher(prefix, exactPrefixLen, true, true);
-      case CodeInsightSettings.ALL:
-        return NameUtil.buildCompletionMatcher(prefix, 1, false, false);
+        return NameUtil.buildMatcher(prefix, NameUtil.MatchingCaseSensitivity.FIRST_LETTER);
       default:
-        return NameUtil.buildCompletionMatcher(prefix, 0, true, false);
+        return NameUtil.buildMatcher(prefix, NameUtil.MatchingCaseSensitivity.ALL);
     }
   }
 
   public static String applyMiddleMatching(String prefix) {
-    if (Registry.is("ide.completion.middle.matching") && !prefix.isEmpty() && !ApplicationManager.getApplication().isUnitTestMode()) {
-      return " " + StringUtil.replace(prefix, ".", ". ").trim();
+    if (Registry.is("ide.completion.middle.matching") && !prefix.isEmpty() && !ourForceStartMatching) {
+      return "*" + StringUtil.replace(prefix, ".", ". ").trim();
     }
     return prefix;
   }
@@ -87,4 +87,21 @@ public class CamelHumpMatcher extends PrefixMatcher {
     return myPrefix;
   }
 
+  /**
+   * In an ideal world, all tests would use the same settings as production, i.e. middle matching.
+   * If you see a usage of this method which can be easily removed (i.e. it's easy to make a test pass without it
+   * by modifying test expectations slightly), please do it
+   */
+  @TestOnly
+  @Deprecated
+  public static void forceStartMatching(Disposable parent) {
+    ourForceStartMatching = true;
+    Disposer.register(parent, new Disposable() {
+      @Override
+      public void dispose() {
+        //noinspection AssignmentToStaticFieldFromInstanceMethod
+        ourForceStartMatching = false;
+      }
+    });
+  }
 }
