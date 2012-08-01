fc4208a1b9822fa0cf994bbd7ada4aad3b69cf8f
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2012-08-01 22:30:47 +0400

    xml attribute value has presentation with proper container text

--- platform/lang-impl/src/com/intellij/psi/presentation/java/SymbolPresentationUtil.java
+++ platform/lang-impl/src/com/intellij/psi/presentation/java/SymbolPresentationUtil.java
@@ -17,10 +17,17 @@ package com.intellij.psi.presentation.java;
 
 import com.intellij.navigation.ItemPresentation;
 import com.intellij.navigation.NavigationItem;
+import com.intellij.openapi.roots.ProjectFileIndex;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.vfs.VfsUtilCore;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiNamedElement;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
 
 public class SymbolPresentationUtil {
   private SymbolPresentationUtil() {
@@ -44,9 +51,28 @@ public class SymbolPresentationUtil {
       final ItemPresentation presentation = ((NavigationItem)element).getPresentation();
       if (presentation != null){
         return presentation.getLocationString();
+      } else {
+        PsiFile file = element.getContainingFile();
+        if (file != null) {
+          VirtualFile virtualFile = file.getVirtualFile();
+          if (virtualFile != null) return virtualFile.getPath();
+        }
       }
     }
 
     return null;
+  }
+
+  public static String getFilePathPresentation(PsiFile psiFile) {
+    ProjectFileIndex index = ProjectRootManager.getInstance(psiFile.getProject()).getFileIndex();
+    VirtualFile file = psiFile.getOriginalFile().getVirtualFile();
+    VirtualFile rootForFile = file != null ? index.getContentRootForFile(file):null;
+
+    if (rootForFile != null) {
+      String relativePath = VfsUtilCore.getRelativePath(file, rootForFile, File.separatorChar);
+      if (relativePath != null) return relativePath;
+    }
+
+    return psiFile.getName();
   }
 }
