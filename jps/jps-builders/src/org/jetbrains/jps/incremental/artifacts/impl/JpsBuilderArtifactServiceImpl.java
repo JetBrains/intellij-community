735e80358b09460b234d4cf8517d48348bd51ba3
nik <Nikolay.Chashnikov@jetbrains.com>
2012-08-02 12:47:27 +0400

    tests fixed

--- jps/jps-builders/src/org/jetbrains/jps/incremental/artifacts/impl/JpsBuilderArtifactServiceImpl.java
+++ jps/jps-builders/src/org/jetbrains/jps/incremental/artifacts/impl/JpsBuilderArtifactServiceImpl.java
@@ -6,7 +6,7 @@ import org.jetbrains.jps.incremental.artifacts.JpsSyntheticArtifactProvider;
 import org.jetbrains.jps.model.JpsModel;
 import org.jetbrains.jps.model.artifact.JpsArtifact;
 import org.jetbrains.jps.model.artifact.JpsArtifactService;
-import org.jetbrains.jps.model.artifact.impl.JpsArtifactRole;
+import org.jetbrains.jps.model.impl.JpsElementChildRoleBase;
 import org.jetbrains.jps.model.impl.JpsElementCollectionImpl;
 import org.jetbrains.jps.model.impl.JpsElementCollectionRole;
 import org.jetbrains.jps.service.JpsServiceManager;
@@ -19,7 +19,7 @@ import java.util.List;
  * @author nik
  */
 public class JpsBuilderArtifactServiceImpl extends JpsBuilderArtifactService {
-  private static final JpsElementCollectionRole<JpsArtifact> SYNTHETIC_ARTIFACTS = JpsElementCollectionRole.create(JpsArtifactRole.INSTANCE);
+  private static final JpsElementCollectionRole<JpsArtifact> SYNTHETIC_ARTIFACTS = JpsElementCollectionRole.create(JpsElementChildRoleBase.<JpsArtifact>create("synthetic artifact"));
 
   @Override
   public Collection<JpsArtifact> getArtifacts(JpsModel model, boolean includeSynthetic) {
