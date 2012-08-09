2286674d2e60b7c800fe2b33bb473cdb6792df87
Bas Leijdekkers <basleijdekkers@gmail.com>
2012-08-09 14:48:25 +0200

    IDEA-89893 (inconsistent "Redundant call to 'String.format()'" when using static method import)

--- plugins/InspectionGadgets/test/com/siyeh/igtest/performance/redundant_string_format_call/RedundantStringFormatCall.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/performance/redundant_string_format_call/RedundantStringFormatCall.java
@@ -1,6 +1,6 @@
 package com.siyeh.igtest.performance.redundant_string_format_call;
 
-
+import static java.lang.String.format;
 
 
 
@@ -9,7 +9,7 @@ public class RedundantStringFormatCall {
     public static final String A = String.format("%n");
   String b = String.format("no parameters");
   String c = String.format("asdf%n" +
-                           "asdf%n")
+                           "asdf%n");
           String d = String.format("asdf" + "asdf" + "asdf%n");
-
+    String e = format("test");
 }
