c844ba49f3dc38808c39c8ce40f655964fe22020
nik <Nikolay.Chashnikov@jetbrains.com>
2012-08-09 13:01:20 +0400

    new project model: extracted common code from element types

--- jps/model-api/src/org/jetbrains/jps/model/library/JpsLibraryType.java
+++ jps/model-api/src/org/jetbrains/jps/model/library/JpsLibraryType.java
@@ -1,15 +1,10 @@
 package org.jetbrains.jps.model.library;
 
 import org.jetbrains.jps.model.JpsElement;
-import org.jetbrains.jps.model.JpsElementChildRole;
+import org.jetbrains.jps.model.JpsElementType;
 
 /**
  * @author nik
  */
-public abstract class JpsLibraryType<P extends JpsElement> {
-  private final JpsElementChildRole<P> myPropertiesRole = new JpsElementChildRole<P>();
-
-  public final JpsElementChildRole<P> getPropertiesRole() {
-    return myPropertiesRole;
-  }
+public abstract class JpsLibraryType<P extends JpsElement> extends JpsElementType<P> {
 }
