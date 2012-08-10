3998c886e596a761ba73b2e1693912cf0b582223
peter <peter@jetbrains.com>
2012-08-10 16:46:39 +0200

    make python completion weigher work only in python contexts

--- python/src/com/jetbrains/python/codeInsight/completion/PythonCompletionWeigher.java
+++ python/src/com/jetbrains/python/codeInsight/completion/PythonCompletionWeigher.java
@@ -4,6 +4,8 @@ import com.intellij.codeInsight.completion.CompletionLocation;
 import com.intellij.codeInsight.completion.CompletionWeigher;
 import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementPresentation;
+import com.intellij.psi.util.PsiUtilBase;
+import com.jetbrains.python.PythonLanguage;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
@@ -18,6 +20,10 @@ public class PythonCompletionWeigher extends CompletionWeigher {
 
   @Override
   public Comparable weigh(@NotNull final LookupElement element, @NotNull final CompletionLocation location) {
+    if (!PsiUtilBase.findLanguageFromElement(location.getCompletionParameters().getPosition()).isKindOf(PythonLanguage.getInstance())) {
+      return 0;
+    }
+
     final String name = element.getLookupString();
     final LookupElementPresentation presentation = LookupElementPresentation.renderElement(element);
     // move dict keys to the top
