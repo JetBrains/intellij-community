f85d2b33222e601a5028699da2ab191fb9c0ccdd
Dmitry Jemerov <yole@jetbrains.com>
2012-08-10 15:15:38 +0200

    StringUtils.isBlank -> StringUtil.isEmptyOrSpaces

--- plugins/hg4idea/src/org/zmlx/hg4idea/ui/HgPushDialog.java
+++ plugins/hg4idea/src/org/zmlx/hg4idea/ui/HgPushDialog.java
@@ -16,6 +16,7 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
@@ -139,7 +140,7 @@ public class HgPushDialog extends DialogWrapper {
 
   private boolean validateOptions() {
     return StringUtils.isNotBlank(repositoryTxt.getText())
-      && !(revisionCbx.isSelected() && StringUtils.isBlank(revisionTxt.getText()))
+      && !(revisionCbx.isSelected() && StringUtil.isEmptyOrSpaces(revisionTxt.getText()))
       && !(branchCheckBox.isSelected() && (branchComboBox.getSelectedItem() == null));
   }
 
