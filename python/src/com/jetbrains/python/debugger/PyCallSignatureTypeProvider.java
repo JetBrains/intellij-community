a35bac71b0591746f0a02567e028aed8d3206e99
Andrey Vlasovskikh <andrey.vlasovskikh@gmail.com>
2012-08-14 15:36:21 +0400

    Removed unnecessary check that forced switch from stubs to AST

--- python/src/com/jetbrains/python/debugger/PyCallSignatureTypeProvider.java
+++ python/src/com/jetbrains/python/debugger/PyCallSignatureTypeProvider.java
@@ -2,16 +2,17 @@ package com.jetbrains.python.debugger;
 
 import com.jetbrains.python.psi.PyFunction;
 import com.jetbrains.python.psi.PyNamedParameter;
-import com.jetbrains.python.psi.PyParameterList;
 import com.jetbrains.python.psi.impl.PyWeakTypeFactory;
-import com.jetbrains.python.psi.types.*;
+import com.jetbrains.python.psi.types.PyType;
+import com.jetbrains.python.psi.types.PyTypeParser;
+import com.jetbrains.python.psi.types.PyTypeProviderBase;
+import com.jetbrains.python.psi.types.TypeEvalContext;
 
 /**
  * @author traff
  */
 public class PyCallSignatureTypeProvider extends PyTypeProviderBase {
   public PyType getParameterType(final PyNamedParameter param, final PyFunction func, TypeEvalContext context) {
-    if (!(param.getParent() instanceof PyParameterList)) return null;
     String type = ((PySignatureCacheManagerImpl)PySignatureCacheManager.getInstance(param.getProject())).findParameterType(param);
     if (type != null) {
       PyType typeByName = PyTypeParser.getTypeByName(param, type);
