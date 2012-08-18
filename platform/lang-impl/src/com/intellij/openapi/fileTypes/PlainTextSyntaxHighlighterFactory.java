192e474caafa6a2e135668172ff4b1a97d195c32
Maxim.Mossienko <maxim.mossienko@jetbrains.com>
2012-08-18 19:37:18 +0400

    [Peter] avoid tokens with size 1 (IDEA-89672)

--- platform/lang-impl/src/com/intellij/openapi/fileTypes/PlainTextSyntaxHighlighterFactory.java
+++ platform/lang-impl/src/com/intellij/openapi/fileTypes/PlainTextSyntaxHighlighterFactory.java
@@ -20,14 +20,17 @@
 package com.intellij.openapi.fileTypes;
 
 import com.intellij.ide.highlighter.custom.AbstractCustomLexer;
-import com.intellij.ide.highlighter.custom.CustomHighlighterColors;
-import com.intellij.ide.highlighter.custom.tokens.*;
+import com.intellij.ide.highlighter.custom.tokens.BraceTokenParser;
+import com.intellij.ide.highlighter.custom.tokens.TokenParser;
+import com.intellij.ide.highlighter.custom.tokens.WhitespaceParser;
 import com.intellij.lexer.Lexer;
+import com.intellij.lexer.MergingLexerAdapter;
 import com.intellij.openapi.editor.colors.TextAttributesKey;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.CustomHighlighterTokenType;
 import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.tree.TokenSet;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -50,7 +53,7 @@ public class PlainTextSyntaxHighlighterFactory extends SyntaxHighlighterFactory 
         tokenParsers.addAll(BraceTokenParser.getBrackets());
         tokenParsers.addAll(BraceTokenParser.getAngleBrackets());
 
-        return new AbstractCustomLexer(tokenParsers);
+        return new MergingLexerAdapter(new AbstractCustomLexer(tokenParsers), TokenSet.create(CustomHighlighterTokenType.CHARACTER));
       }
 
       @NotNull
