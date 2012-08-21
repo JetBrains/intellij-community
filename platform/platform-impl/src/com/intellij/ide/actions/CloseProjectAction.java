eff35de2d5fc8c9cf46e17d482cd189f23d83bbe
Dmitry Jemerov <yole@jetbrains.com>
2012-08-21 11:50:53 +0200

    don't rename action to "close projects in current window" if we have only one project

--- platform/platform-impl/src/com/intellij/ide/actions/CloseProjectAction.java
+++ platform/platform-impl/src/com/intellij/ide/actions/CloseProjectAction.java
@@ -21,6 +21,7 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.projectImport.ProjectAttachProcessor;
@@ -38,8 +39,11 @@ public class CloseProjectAction extends AnAction implements DumbAware {
     Presentation presentation = event.getPresentation();
     Project project = event.getData(PlatformDataKeys.PROJECT);
     presentation.setEnabled(project != null);
-    if (ProjectAttachProcessor.canAttachToProject()) {
+    if (ProjectAttachProcessor.canAttachToProject() && project != null && ModuleManager.getInstance(project).getModules().length > 1) {
       presentation.setText("Close Projects in Current Window");
+    }
+    else {
+      presentation.setText(getTemplatePresentation().getText());
     }
   }
 }
