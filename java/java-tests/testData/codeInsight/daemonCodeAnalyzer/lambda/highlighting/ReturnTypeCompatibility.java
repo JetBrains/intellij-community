a5c50473a3b575b3267ebebbe35aac00cf11eda5
Anna Kozlova <anna.kozlova@jetbrains.com>
2012-08-21 17:04:40 +0400

    lambda: skip some cyclic inference cases

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/lambda/highlighting/ReturnTypeCompatibility.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/lambda/highlighting/ReturnTypeCompatibility.java
@@ -57,7 +57,7 @@ class ReturnTypeCompatibility {
     }
 
     public static void main(String[] args) {
-        <error descr="Cannot resolve method 'call(<lambda expression>)'">call</error>(i-> {return i;});
+        call(<error descr="Cyclic inference">i-> {return i;}</error>);
     }
 }
 
