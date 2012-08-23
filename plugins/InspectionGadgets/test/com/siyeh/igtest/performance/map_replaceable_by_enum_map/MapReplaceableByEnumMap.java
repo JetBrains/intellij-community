3b1e52c93369d3e17a53bdab7233e988bc21ff4b
Bas Leijdekkers <basleijdekkers@gmail.com>
2012-08-23 16:56:26 +0200

    IDEA-90475 ("Map replaceable with EnumMap" inspection suggests replacement when TreeMap constructor takes a Comparator)

--- plugins/InspectionGadgets/test/com/siyeh/igtest/performance/map_replaceable_by_enum_map/MapReplaceableByEnumMap.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/performance/map_replaceable_by_enum_map/MapReplaceableByEnumMap.java
@@ -1,7 +1,7 @@
 package com.siyeh.igtest.performance.map_replaceable_by_enum_map;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Collections; import java.util.HashMap;
+import java.util.Map; import java.util.TreeMap;
 
 public class MapReplaceableByEnumMap {
 
@@ -13,5 +13,9 @@ public class MapReplaceableByEnumMap {
         foo, bar, baz;
         Map<MyEnum, Object> enums = new HashMap();
         // enum map here throws exception at runtime -> don't suggest it
+    }
+
+    void foo() {
+        final Map<MyEnum, Object> map = new TreeMap(Collections.reverseOrder());
     }
 }
