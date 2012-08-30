dd550e9feb2da3acc536afc659964940f27ba62d
Eugene Zhuravlev <jeka@intellij.com>
2012-08-30 20:27:34 +0200

    external make: when clearing output directories, clear the directory itself, not its content

--- jps/jps-builders/src/org/jetbrains/jps/incremental/IncProjectBuilder.java
+++ jps/jps-builders/src/org/jetbrains/jps/incremental/IncProjectBuilder.java
@@ -365,16 +365,10 @@ public class IncProjectBuilder {
         }
       }
       if (okToDelete) {
-        // do not delete output root itself to avoid lots of unnecessary "roots_changed" events in IDEA
-        final File[] children = outputRoot.listFiles();
-        if (children != null) {
-          filesToDelete.addAll(Arrays.asList(children));
-        }
+        filesToDelete.add(outputRoot);
       }
       else {
-        context.processMessage(new CompilerMessage(BUILD_NAME, BuildMessage.Kind.WARNING, "Output path " +
-                                                                                                   outputRoot.getPath() +
-                                                                                                   " intersects with a source root. The output cannot be cleaned."));
+        context.processMessage(new CompilerMessage(BUILD_NAME, BuildMessage.Kind.WARNING, "Output path " + outputRoot.getPath() + " intersects with a source root. The output cannot be cleaned."));
         // clean only those files we are aware of
         for (Pair<String, Boolean> info : entry.getValue()) {
           clearOutputFiles(context, info.first, info.second);
@@ -383,11 +377,7 @@ public class IncProjectBuilder {
     }
 
     for (File annotationOutput : annotationOutputs) {
-      // do not delete output root itself to avoid lots of unnecessary "roots_changed" events in IDEA
-      final File[] children = annotationOutput.listFiles();
-      if (children != null) {
-        filesToDelete.addAll(Arrays.asList(children));
-      }
+      filesToDelete.add(annotationOutput);
     }
 
     context.processMessage(new ProgressMessage("Cleaning output directories..."));
