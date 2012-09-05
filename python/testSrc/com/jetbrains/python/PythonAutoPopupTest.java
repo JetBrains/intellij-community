2de09ade8e41b12afe85d8ddaa95f2b981f71af2
peter <peter@jetbrains.com>
2012-09-05 16:29:17 +0200

    IDEA-90765 When a completion popup is visible in Python code, typing . does not popup next completion

--- python/testSrc/com/jetbrains/python/PythonAutoPopupTest.java
+++ python/testSrc/com/jetbrains/python/PythonAutoPopupTest.java
@@ -1,5 +1,6 @@
 package com.jetbrains.python;
 
+import com.intellij.codeInsight.lookup.impl.LookupImpl;
 import com.intellij.testFramework.fixtures.CompletionAutoPopupTester;
 import com.jetbrains.python.fixtures.PyTestCase;
 
@@ -31,6 +32,23 @@ public class PythonAutoPopupTest extends PyTestCase {
     myFixture.configureByText("a.py", FOO_CLASS + "Foo()<caret>");
     myTester.typeWithPauses(".");
     assertNotNull(myTester.getLookup());
+  }
+
+  public void testAutoPopupAfterDotDuringAnotherAutoPopup() {
+    myFixture.configureByText("a.py", FOO_CLASS +
+                                      "foo = Foo()\n" +
+                                      "foo2 = Foo()\n" +
+                                      "<caret>");
+    myTester.typeWithPauses("foo");
+    LookupImpl lookup1 = myTester.getLookup();
+    assertNotNull(lookup1);
+    assertFalse(lookup1.isFocused());
+
+    myTester.typeWithPauses(".");
+    LookupImpl lookup2 = myTester.getLookup();
+    assertNotSame(lookup1, lookup2);
+    assertNotNull(lookup2);
+    assertFalse(lookup2.isFocused());
   }
 
 }
