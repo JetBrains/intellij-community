6b38e25f63680bbf04ff6abbb69c26c0b29d0cb9
Maxim Shafirov <max@jetbrains.com>
2012-09-06 20:01:23 +0400

    dead icons

--- plugins/maven/src/main/java/icons/MavenIcons.java
+++ plugins/maven/src/main/java/icons/MavenIcons.java
@@ -13,32 +13,24 @@ public class MavenIcons {
     return IconLoader.getIcon(path, MavenIcons.class);
   }
 
-  public static final Icon AttachPlugin = load("/images/attachPlugin.png"); // 16x16
   public static final Icon ChildrenProjects = load("/images/childrenProjects.png"); // 12x12
-  public static final Icon DetachPlugin = load("/images/detachPlugin.png"); // 16x16
   public static final Icon Download = load("/images/download.png"); // 16x16
+  
+  @SuppressWarnings("unused")
   public static final Icon Error = load("/images/error.png"); // 16x16
   public static final Icon FilterPhases = load("/images/filterPhases.png"); // 16x16
   public static final Icon MavenLogo = load("/images/mavenLogo.png"); // 16x16
   public static final Icon MavenPlugin = load("/images/mavenPlugin.png"); // 16x16
   public static final Icon MavenProject = load("/images/mavenProject.png"); // 16x16
-  public static final Icon MavenSettings = load("/images/mavenSettings.png"); // 16x16
   public static final Icon ModulesClosed = load("/images/modulesClosed.png"); // 16x16
-  public static final Icon ModulesOpen = load("/images/modulesOpen.png"); // 16x16
   public static final Icon OfflineMode = load("/images/offlineMode.png"); // 16x16
   public static final Icon OverridenDependency = load("/images/overridenDependency.png"); // 10x14
   public static final Icon OverridingDependency = load("/images/overridingDependency.png"); // 10x14
   public static final Icon ParentProject = load("/images/parentProject.png"); // 12x12
   public static final Icon Phase = load("/images/phase.png"); // 16x16
   public static final Icon PhasesClosed = load("/images/phasesClosed.png"); // 16x16
-  public static final Icon PhasesOpen = load("/images/phasesOpen.png"); // 16x16
   public static final Icon PluginGoal = load("/images/pluginGoal.png"); // 16x16
   public static final Icon ProfilesClosed = load("/images/profilesClosed.png"); // 16x16
-  public static final Icon ProfilesOpen = load("/images/profilesOpen.png"); // 16x16
-  public static final Icon Repository = load("/images/repository.png"); // 16x16
   public static final Icon ToolWindowMaven = load("/images/toolWindowMaven.png"); // 13x13
-  public static final Icon UpdateDependencies = load("/images/updateDependencies.png"); // 16x16
   public static final Icon UpdateFolders = load("/images/updateFolders.png"); // 16x16
-  public static final Icon UpdateIndices = load("/images/updateIndices.png"); // 16x16
-  public static final Icon Warning = load("/images/warning.png"); // 16x16
 }
