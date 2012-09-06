ea18fe790c4e40d87b75a68e11c363a2ac3b502f
peter <peter@jetbrains.com>
2012-09-06 17:11:39 +0200

    IDEA-89780 "Next Bookmark" doesn't work in files from excluded directories

--- platform/lang-impl/src/com/intellij/ide/bookmarks/actions/PreviousBookmarkAction.java
+++ platform/lang-impl/src/com/intellij/ide/bookmarks/actions/PreviousBookmarkAction.java
@@ -16,13 +16,8 @@
 
 package com.intellij.ide.bookmarks.actions;
 
-import com.intellij.ide.bookmarks.Bookmark;
-import com.intellij.ide.bookmarks.BookmarkManager;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.project.Project;
-
 public class PreviousBookmarkAction extends GotoBookmarkActionBase {
-  protected Bookmark getBookmarkToGo(Project project, Editor editor) {
-    return BookmarkManager.getInstance(project).getPreviousBookmark(editor, true);
+  public PreviousBookmarkAction() {
+    super(false);
   }
 }
