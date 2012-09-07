447bc37818e1d928356141740e392dfe4511846d
Anna Kozlova <anna.kozlova@jetbrains.com>
2012-09-07 22:28:41 +0400

    lambda: remove redundant code block inspection accept void compatible lambda & nested blocks (IDEA-91184)

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/lambdaCodeBlock2Expr/beforeVoid.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/lambdaCodeBlock2Expr/beforeVoid.java
@@ -1,6 +1,6 @@
 // "Replace with one line expression" "true"
 class Test {
   {
-    Comparable<String> c = (o) -> {r<caret>eturn 0;};
+    Runnable c = () -> <caret>{System.out.println();};
   }
 }
