ed0c65ef1be4b9dd3a8e5aa0a92e3917cca58de7
Konstantin Bulenkov <kb@jetbrains.com>
2012-09-11 01:57:30 +0400

    better painting

--- platform/platform-impl/src/com/intellij/ide/ui/laf/borders/DarculaCheckBoxUI.java
+++ platform/platform-impl/src/com/intellij/ide/ui/laf/borders/DarculaCheckBoxUI.java
@@ -32,6 +32,7 @@ import java.awt.*;
  * @author Konstantin Bulenkov
  */
 public class DarculaCheckBoxUI extends MetalCheckBoxUI {
+  @SuppressWarnings("MethodOverridesStaticMethodOfSuperclass")
   public static ComponentUI createUI(JComponent c) {
     return new DarculaCheckBoxUI();
   }
@@ -40,13 +41,12 @@ public class DarculaCheckBoxUI extends MetalCheckBoxUI {
   public synchronized void paint(Graphics g2d, JComponent c) {
     Graphics2D g = (Graphics2D)g2d;
     JCheckBox b = (JCheckBox) c;
-    ButtonModel model = b.getModel();
+    final ButtonModel model = b.getModel();
+    final Dimension size = c.getSize();
+    final Font font = c.getFont();
 
-    Dimension size = c.getSize();
-
-    Font f = c.getFont();
-    g.setFont(f);
-    FontMetrics fm = SwingUtilities2.getFontMetrics(c, g, f);
+    g.setFont(font);
+    FontMetrics fm = SwingUtilities2.getFontMetrics(c, g, font);
 
     Rectangle viewRect = new Rectangle(size);
     Rectangle iconRect = new Rectangle();
@@ -58,31 +58,30 @@ public class DarculaCheckBoxUI extends MetalCheckBoxUI {
     viewRect.width -= (i.right + viewRect.x);
     viewRect.height -= (i.bottom + viewRect.y);
 
-    Icon altIcon = b.getIcon();
+    String text = SwingUtilities.layoutCompoundLabel(c, fm, b.getText(), getDefaultIcon(),
+                                                     b.getVerticalAlignment(), b.getHorizontalAlignment(),
+                                                     b.getVerticalTextPosition(), b.getHorizontalTextPosition(),
+                                                     viewRect, iconRect, textRect, b.getIconTextGap());
 
-    String text = SwingUtilities.layoutCompoundLabel(
-      c, fm, b.getText(), altIcon != null ? altIcon : getDefaultIcon(),
-      b.getVerticalAlignment(), b.getHorizontalAlignment(),
-      b.getVerticalTextPosition(), b.getHorizontalTextPosition(),
-      viewRect, iconRect, textRect, b.getIconTextGap());
-
-    // fill background
+    //background
     if(c.isOpaque()) {
       g.setColor(b.getBackground());
       g.fillRect(0,0, size.width, size.height);
     }
 
-    //paint icon bg
-    int width = iconRect.width;
-    int height = iconRect.height;
-    final GradientPaint paint = new GradientPaint(iconRect.x + width / 2, iconRect.y, b.getBackground().brighter(),
-                                                  iconRect.x + width / 2, iconRect.y + height, b.getBackground());
+    final int x = iconRect.x + 2;
+    final int y = iconRect.y + 2;
+    final int w = iconRect.width - 4;
+    final int h = iconRect.height - 4;
+    final int u = w / 16;
+
+    g.translate(x, y);
+    final GradientPaint paint = new GradientPaint(w / 2, 0, b.getBackground().brighter(),
+                                                  w / 2, h, b.getBackground());
     g.setPaint(paint);
-    g2d.fillRect(iconRect.x + 1, iconRect.y + 1, width - 2, height - 2);
+    g.fillRect(u, u, w - 2 * u, h - 2 * u);
 
-
-
-
+    //setup AA for lines
     final GraphicsConfig config = new GraphicsConfig(g);
     g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
     g.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_DEFAULT);
@@ -90,57 +89,46 @@ public class DarculaCheckBoxUI extends MetalCheckBoxUI {
     final boolean armed = b.getModel().isArmed();
 
     if (c.hasFocus()) {
-      g.setPaint(new GradientPaint(width / 2, 1, armed ? Gray._40: Gray._60, width / 2, height, armed ? Gray._25 : Gray._45));
-      g.fillRoundRect(iconRect.x, iconRect.y, width-2, height-2, 4, 4);
+      g.setPaint(new GradientPaint(w/2, 1, armed ? Gray._40: Gray._60, w/2, h, armed ? Gray._25 : Gray._45));
+      g.fillRoundRect(0, 0, w - 2, h - 2, 4, 4);
 
-      MacUIUtil.paintFocusRing(g, new Color(96, 175, 255), new Rectangle(iconRect.x+1, iconRect.y + 1, iconRect.width-2,iconRect.height-2));
+      MacUIUtil.paintFocusRing(g, new Color(96, 175, 255), new Rectangle(1, 1, w - 2, h - 2));
     } else {
-      g.setPaint(new GradientPaint(width / 2, 1, Gray._80, width / 2, height, Gray._65));
-      g.fillRoundRect(iconRect.x, iconRect.y, width-2, height-2, 4, 4);
+      g.setPaint(new GradientPaint(w / 2, 1, Gray._80, w / 2, h, Gray._65));
+      g.fillRoundRect(0, 0, w - 2, h - 2, 4, 4);
 
-      g.setPaint(new GradientPaint(width / 2, 1, ColorUtil.toAlpha(Gray._120, 90), width / 2, height, ColorUtil.toAlpha(Gray._105, 90)));
-      g.drawRoundRect(iconRect.x, iconRect.y + 1, iconRect.width, iconRect.height - 1, 4, 4);
-
+      g.setPaint(new GradientPaint(w / 2, 1, ColorUtil.toAlpha(Gray._120, 90), w / 2, h, ColorUtil.toAlpha(Gray._105, 90)));
+      g.drawRoundRect(0, 1, w, h - 1, 4, 4);
 
       g.setPaint(Gray._30);
-      g.drawRoundRect(iconRect.x, iconRect.y, width, height-1, 4, 4);
-
+      g.drawRoundRect(0, 0, w, h - 1, 4, 4);
     }
 
     if (b.getModel().isSelected()) {
       g.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
-      g.setStroke(new BasicStroke(2.0f, BasicStroke.CAP_ROUND,BasicStroke.JOIN_ROUND));
+      g.setStroke(new BasicStroke(u*2.0f, BasicStroke.CAP_ROUND,BasicStroke.JOIN_ROUND));
       g.setPaint(Gray._30);
-      g.drawLine(iconRect.x + 4, iconRect.y + 8, iconRect.x + 9, iconRect.y + 14);
-      g.drawLine(iconRect.x + 9, iconRect.y + 14, iconRect.x + iconRect.width, iconRect.y+2);
+      g.drawLine(4*u, 8*u, 8*u, 12*u);
+      g.drawLine(8*u, 12*u, w, 2*u);
       g.setPaint(Gray._200);
-      g.drawLine(iconRect.x + 4, iconRect.y + 6, iconRect.x + 9, iconRect.y + 12);
-      g.drawLine(iconRect.x + 9, iconRect.y + 12, iconRect.x + iconRect.width, iconRect.y);
+      g.drawLine(4*u, 6*u, 8*u, 10*u);
+      g.drawLine(8*u, 10*u, w, 0);
     }
+    g.translate(-x, -y);
     config.restore();
-    //icon.paintIcon(c, g, iconRect.x, iconRect.y);
 
-    // Draw the Text
+    //text
     if(text != null) {
-      View v = (View) c.getClientProperty(BasicHTML.propertyKey);
-      if (v != null) {
-        v.paint(g, textRect);
+      View view = (View) c.getClientProperty(BasicHTML.propertyKey);
+      if (view != null) {
+        view.paint(g, textRect);
       } else {
-        int mnemIndex = b.getDisplayedMnemonicIndex();
-        if(model.isEnabled()) {
-          // *** paint the text normally
-          g.setColor(b.getForeground());
-        } else {
-          // *** paint the text disabled
-          g.setColor(getDisabledTextColor());
-        }
-        SwingUtilities2.drawStringUnderlineCharAt(c,g,text,
-                                                  mnemIndex, textRect.x, textRect.y + fm.getAscent());
+        g.setColor(model.isEnabled() ? b.getForeground() : getDisabledTextColor());
+        SwingUtilities2.drawStringUnderlineCharAt(c, g, text,
+                                                  b.getDisplayedMnemonicIndex(),
+                                                  textRect.x,
+                                                  textRect.y + fm.getAscent());
       }
     }
-  }
-
-  public Icon getSelectedIcon() {
-    return UIManager.getIcon(getPropertyPrefix() + "selectedIcon");
   }
 }
