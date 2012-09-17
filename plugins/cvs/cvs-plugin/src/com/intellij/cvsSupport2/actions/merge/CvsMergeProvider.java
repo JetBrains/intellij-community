09228bd87b2d709142c87ed7a7815fa497069df3
Bas Leijdekkers <basleijdekkers@gmail.com>
2012-09-17 16:58:12 +0200

    IDEA-88523 (zip file conflict -> idea destroys zip file no matter which option is chosen)

--- plugins/cvs/cvs-plugin/src/com/intellij/cvsSupport2/actions/merge/CvsMergeProvider.java
+++ plugins/cvs/cvs-plugin/src/com/intellij/cvsSupport2/actions/merge/CvsMergeProvider.java
@@ -16,14 +16,18 @@
 package com.intellij.cvsSupport2.actions.merge;
 
 import com.intellij.cvsSupport2.CvsUtil;
+import com.intellij.cvsSupport2.application.CvsEntriesManager;
 import com.intellij.cvsSupport2.util.CvsVfsUtil;
+import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vcs.merge.MergeData;
 import com.intellij.openapi.vcs.merge.MergeProvider;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
+import org.netbeans.lib.cvsclient.admin.Entry;
 
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
@@ -31,41 +35,46 @@ import java.io.IOException;
  * @author lesya
  */
 public class CvsMergeProvider implements MergeProvider {
+
+  @Override
   @NotNull
-  public MergeData loadRevisions(final VirtualFile file) throws VcsException {
-    return parseConflictsInFile(file).createData();
+  public MergeData loadRevisions(VirtualFile file) throws VcsException {
+    try {
+      final Entry entry = CvsEntriesManager.getInstance().getEntryFor(file);
+      final File cvsFile = CvsVfsUtil.getFileFor(file);
+      if (entry != null && entry.isResultOfMerge() && entry.isBinary()) {
+        final String originalRevision = CvsUtil.getOriginalRevisionForFile(file);
+        final MergeData mergeData = new MergeData();
+        mergeData.CURRENT = CvsUtil.getStoredContentForFile(file, originalRevision);
+        mergeData.LAST = FileUtil.loadFileBytes(cvsFile);
+        return mergeData;
+      }
+      final BufferedInputStream input = new BufferedInputStream(new FileInputStream(cvsFile));
+      try {
+        final CvsConflictsParser parser = CvsConflictsParser.createOn(input);
+        final MergeData mergeData = new MergeData();
+        mergeData.ORIGINAL = parser.getCenterVersion().getBytes();
+        mergeData.CURRENT = parser.getLeftVersion().getBytes();
+        mergeData.LAST = parser.getRightVersion().getBytes();
+        return mergeData;
+      }
+      finally {
+        input.close();
+      }
+    }
+    catch (IOException e) {
+      throw new VcsException(e);
+    }
   }
 
+  @Override
   public void conflictResolvedForFile(VirtualFile file) {
     CvsUtil.resolveConflict(file);
+    CvsEntriesManager.getInstance().clearCachedEntriesFor(file.getParent());
   }
 
-  public boolean isBinary(final VirtualFile file) {
+  @Override
+  public boolean isBinary(VirtualFile file) {
     return false;
-  }
-
-  @NotNull private static MergeDataProvider parseConflictsInFile(final VirtualFile file) {
-    return new MergeDataProvider() {
-      @NotNull
-      public MergeData createData() throws VcsException {
-        try {
-          BufferedInputStream input = new BufferedInputStream(new FileInputStream(CvsVfsUtil.getFileFor(file)));
-          try {
-            final CvsConflictsParser parser = CvsConflictsParser.createOn(input);
-            final MergeData mergeData = new MergeData();
-            mergeData.ORIGINAL = parser.getCenterVersion().getBytes();
-            mergeData.CURRENT = parser.getLeftVersion().getBytes();
-            mergeData.LAST = parser.getRightVersion().getBytes();
-            return mergeData;
-          }
-          finally {
-            input.close();
-          }
-        }
-        catch (IOException e) {
-          throw new VcsException(e);
-        }
-      }
-    };
   }
 }
