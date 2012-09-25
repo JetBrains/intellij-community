a114350111d3a05080ff0ccb7c0259b29fc0b1d6
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2012-09-25 20:37:44 +0400

    IDEA-66715 Move GitHub settings page to the other VCSs
    * Use vcsConfigurableProvider extension point.
    * Also move additional configurable providers below the common VCS sub-configurables, before actual VCSs.

--- plugins/github/src/org/jetbrains/plugins/github/ui/GitHubSettingsConfigurable.java
+++ plugins/github/src/org/jetbrains/plugins/github/ui/GitHubSettingsConfigurable.java
@@ -1,10 +1,14 @@
 package org.jetbrains.plugins.github.ui;
 
+import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.options.SearchableConfigurable;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vcs.VcsConfigurableProvider;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.github.GithubSettings;
 
 import javax.swing.*;
@@ -12,7 +16,7 @@ import javax.swing.*;
 /**
  * @author oleg
  */
-public class GitHubSettingsConfigurable implements SearchableConfigurable {
+public class GitHubSettingsConfigurable implements SearchableConfigurable, VcsConfigurableProvider {
   private static final String DEFAULT_PASSWORD_TEXT = "************";
   private GithubSettingsPanel mySettingsPane;
   private final GithubSettings mySettings;
@@ -78,5 +82,11 @@ public class GitHubSettingsConfigurable implements SearchableConfigurable {
 
   public Runnable enableSearch(String option) {
     return null;
+  }
+
+  @Nullable
+  @Override
+  public Configurable getConfigurable(Project project) {
+    return this;
   }
 }
