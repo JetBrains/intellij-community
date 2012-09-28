f52a54ed1690621cd228cf0cf4494d9d460430ea
peter <peter@jetbrains.com>
2012-09-28 13:39:14 +0200

    IDEA-90876 File menu Close Project Mnemonic "J" moved to Project Settings that already has keyboard shortcut (cherry-picked from 05e5807)

--- platform/platform-impl/src/com/intellij/ide/actions/CloseProjectAction.java
+++ platform/platform-impl/src/com/intellij/ide/actions/CloseProjectAction.java
@@ -22,8 +22,8 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.module.ModuleManager;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
 import com.intellij.projectImport.ProjectAttachProcessor;
 
 public class CloseProjectAction extends AnAction implements DumbAware {
@@ -40,10 +40,10 @@ public class CloseProjectAction extends AnAction implements DumbAware {
     Project project = event.getData(PlatformDataKeys.PROJECT);
     presentation.setEnabled(project != null);
     if (ProjectAttachProcessor.canAttachToProject() && project != null && ModuleManager.getInstance(project).getModules().length > 1) {
-      presentation.setText("Close Projects in Current Window");
+      presentation.setText("Close Proj_ects in Current Window");
     }
     else {
-      presentation.setText(getTemplatePresentation().getText());
+      presentation.setText("Close Pro_ject");
     }
   }
 }
