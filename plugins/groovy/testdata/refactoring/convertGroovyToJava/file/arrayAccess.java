d3ccce68d7ce577c657b72237343b64bbea7f0f3
Max Medvedev <maxim.medvedev@jetbrains.com>
2012-09-28 20:50:54 +0300

    fix testdata

--- plugins/groovy/testdata/refactoring/convertGroovyToJava/file/arrayAccess.java
+++ plugins/groovy/testdata/refactoring/convertGroovyToJava/file/arrayAccess.java
@@ -18,7 +18,7 @@ print(putAt0(map, "1", "6"));
 print(putAt0(map, 2, "7"));
 org.codehaus.groovy.runtime.DefaultGroovyMethods.putAt(map, "6", 1);
 print(map.get("1"));
-print(org.codehaus.groovy.runtime.DefaultGroovyMethods.getAt(map, 2));
+print(map.get(2));
 Foo foo = new Foo();
 foo.putAt("a", 2, 4);
 print(putAt1(foo, "a", 2, 4));
