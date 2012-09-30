98016065dfe2fe948de81cbab28bf94612db70cd
Konstantin Bulenkov <kb@jetbrains.com>
2012-09-29 22:35:46 -0700

    IDEA-77650 Compare directory dialog doesn't store last path

--- platform/platform-impl/src/com/intellij/openapi/diff/actions/CompareDirectoriesAction.java
+++ platform/platform-impl/src/com/intellij/openapi/diff/actions/CompareDirectoriesAction.java
@@ -18,6 +18,7 @@ package com.intellij.openapi.diff.actions;
 import com.intellij.featureStatistics.FeatureUsageTracker;
 import com.intellij.ide.diff.DirDiffSettings;
 import com.intellij.ide.diff.VirtualFileDiffElement;
+import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
@@ -25,12 +26,16 @@ import com.intellij.openapi.diff.DirDiffManager;
 import com.intellij.openapi.fileChooser.FileChooser;
 import com.intellij.openapi.fileChooser.FileChooserDescriptor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 
 /**
  * @author Konstantin Bulenkov
  */
 public class CompareDirectoriesAction extends AnAction {
+
+  public static final String LAST_USED_KEY = "dir.diff.last.used.directory";
+
   @Override
   public void actionPerformed(AnActionEvent e) {
     final Project project = getEventProject(e);
@@ -46,8 +51,17 @@ public class CompareDirectoriesAction extends AnAction {
       else if (files.length == 1 && files[0].isDirectory()) {
         src = new VirtualFileDiffElement(files[0]);
         final FileChooserDescriptor descriptor = new FileChooserDescriptor(false, true, false, false, false, false);
-        final VirtualFile[] result = FileChooser.chooseFiles(descriptor, project, project.getBaseDir());
+        final String path = PropertiesComponent.getInstance(project).getValue(LAST_USED_KEY);
+        VirtualFile toSelect = null;
+        if (path != null) {
+          toSelect = LocalFileSystem.getInstance().findFileByPath(path);
+        }
+        if (toSelect == null) {
+          toSelect = project.getBaseDir();
+        }
+        final VirtualFile[] result = FileChooser.chooseFiles(descriptor, project, toSelect);
         if (result.length == 1 && result[0] != null && result[0].isDirectory()) {
+          PropertiesComponent.getInstance(project).setValue(LAST_USED_KEY, result[0].getPath());
           trg = new VirtualFileDiffElement(result[0]);
         }
       }
