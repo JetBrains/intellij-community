744e3dcdea44f0a80777e7af0472093855c4734f
Konstantin Bulenkov <kb@jetbrains.com>
2012-10-08 02:34:26 +0200

    show icons and borders. Protect against key typing on color cells

--- platform/lang-impl/src/com/intellij/ui/debugger/ShowUIDefaultsAction.java
+++ platform/lang-impl/src/com/intellij/ui/debugger/ShowUIDefaultsAction.java
@@ -19,11 +19,14 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.ui.Cell;
 import com.intellij.ui.ColorPicker;
 import com.intellij.ui.ColorUtil;
 import com.intellij.ui.TableSpeedSearch;
 import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.table.JBTable;
+import com.intellij.util.PairFunction;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import javax.swing.border.Border;
@@ -31,6 +34,7 @@ import javax.swing.plaf.ColorUIResource;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.MouseEvent;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Enumeration;
@@ -76,7 +80,7 @@ public class ShowUIDefaultsAction extends AnAction {
         }) {
           @Override
           public boolean editCellAt(int row, int column, EventObject e) {
-            if (isCellEditable(row, column)) {
+            if (isCellEditable(row, column) && e instanceof MouseEvent) {
               final Object color = getValueAt(row, column);
               final Color newColor = ColorPicker.showDialog(this, "Choose Color", (Color)color, true, null);
               if (newColor != null) {
@@ -107,16 +111,20 @@ public class ShowUIDefaultsAction extends AnAction {
               panel.setBackground(c);
               return panel;
             } else if (value instanceof Icon) {
-              //final Icon icon = (Icon)value;
-              //if (icon.getIconHeight() <= 20) {
-              //  //label.setIcon(icon);
-              //}
-              //label.setText(String.format("(%dx%d) %s)",icon.getIconWidth(), icon.getIconHeight(), label.getText()));
+              try {
+                final Icon icon = (Icon)value;
+                if (icon.getIconHeight() <= 20) {
+                  label.setIcon(icon);
+                }
+                label.setText(String.format("(%dx%d) %s)",icon.getIconWidth(), icon.getIconHeight(), label.getText()));
+              }
+              catch (Throwable e1) {//
+              }
               return panel;
             } else if (value instanceof Border) {
               try {
                 final Insets i = ((Border)value).getBorderInsets(null);
-                label.setText(String.format("border[%d, %d, %d, %d] %s", i.top, i.left, i.bottom, i.right, label.getText()));
+                label.setText(String.format("[%d, %d, %d, %d] %s", i.top, i.left, i.bottom, i.right, label.getText()));
                 return panel;
               } catch (Exception ignore) {}
             }
@@ -124,7 +132,13 @@ public class ShowUIDefaultsAction extends AnAction {
           }
         });
         final JBScrollPane pane = new JBScrollPane(table);
-        new TableSpeedSearch(table);
+        new TableSpeedSearch(table, new PairFunction<Object, Cell, String>() {
+          @Nullable
+          @Override
+          public String fun(Object o, Cell cell) {
+            return cell.column == 1 ? null : String.valueOf(o);
+          }
+        });
         table.setShowGrid(false);
         final JPanel panel = new JPanel(new BorderLayout());
         panel.add(pane, BorderLayout.CENTER);
