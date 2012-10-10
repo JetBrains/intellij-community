8a3e220697a8a55e6dc6011e94d4d9ddc79589f1
Alexey Kudravtsev <cdr@intellij.com>
2012-10-10 19:49:39 +0400

    notnull

--- platform/util/src/com/intellij/util/text/Matcher.java
+++ platform/util/src/com/intellij/util/text/Matcher.java
@@ -1,11 +1,12 @@
 package com.intellij.util.text;
 
 import com.intellij.psi.codeStyle.NameUtil;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Konstantin Bulenkov
  */
 @SuppressWarnings("ClassNameSameAsAncestorName")
 public interface Matcher extends NameUtil.Matcher {
-  boolean matches(String name);
+  boolean matches(@NotNull String name);
 }
