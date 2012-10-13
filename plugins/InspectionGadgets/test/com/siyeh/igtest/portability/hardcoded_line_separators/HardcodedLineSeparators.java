724998524c5d611a1cd28c7af12f6339099515ca
Bas Leijdekkers <basleijdekkers@gmail.com>
2012-10-13 10:42:16 +0200

    improve "hardcoded line separator" inspection

---
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/portability/hardcoded_line_separators/HardcodedLineSeparators.java
@@ -0,0 +1,9 @@
+package com.siyeh.igtest.portability.hardcoded_line_separators;
+
+public class HardcodedLineSeparators {
+  final String newlineString = "\n";
+  final String returnString = "\r";
+
+  final char newLineChar = '\12';
+  final char returnChar = '\15';
+}
