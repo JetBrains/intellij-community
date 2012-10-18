71328e44bda8e543f8698522f48e06b392b246e0
Gregory.Shrago <gregory.shrago@jetbrains.com>
2012-10-18 23:16:09 +0400

    IDEA-92339 Database Console  Copy as HTML improvement

--- platform/platform-impl/src/com/intellij/util/ui/TextTransferrable.java
+++ platform/platform-impl/src/com/intellij/util/ui/TextTransferrable.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.intellij.openapi.vcs.history;
+package com.intellij.util.ui;
 
 import com.intellij.openapi.diagnostic.Logger;
 
