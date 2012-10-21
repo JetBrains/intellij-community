1619c74c9f344064f2f01eb946f7b73b65bbc36e
Max Medvedev <maxim.medvedev@jetbrains.com>
2012-10-21 19:14:59 +0400

    IDEA-93257 Groovy: good code is red when annotation attribute is named "def"

---
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/parser/AnnotationsParsingTest.groovy
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2000-2012 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jetbrains.plugins.groovy.lang.parser
+
+/**
+ * @author peter
+ */
+public class AnnotationsParsingTest extends GroovyParsingTestCase {
+  final String basePath = super.basePath + "annotations"
+
+  public void testAnn1() { doTest() }
+  public void testAnn2() { doTest() }
+  public void testAnn3() { doTest() }
+  public void testAnn4() { doTest() }
+  public void testAnn5() { doTest() }
+  public void testAnn6() { doTest() }
+  public void testAnn7() { doTest() }
+  public void testClassLiteral() { doTest() }
+  public void testImportAnn() { doTest() }
+  public void testPackageAnn() { doTest() }
+  public void testDefAttribute() {doTest()}
+}
