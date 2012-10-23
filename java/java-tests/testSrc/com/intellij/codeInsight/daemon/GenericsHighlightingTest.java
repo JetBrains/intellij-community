7c50a4b1b484427d9429462f60c2a9658ba9a651
anna <Anna.Kozlova@jetbrains.com>
2012-10-23 19:11:09 +0200

    testdata for IDEA-57484

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/GenericsHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/GenericsHighlightingTest.java
@@ -162,6 +162,7 @@ public class GenericsHighlightingTest extends LightDaemonAnalyzerTestCase {
   public void testIDEA57509() throws Exception { doTest(false); }
   public void testIDEA57410() throws Exception { doTest(false); }
   public void testIDEA57411() throws Exception { doTest(false); }
+  public void testIDEA57484() throws Exception { doTest(false); }
   public void testInconvertibleTypes() throws Exception { doTest(false); }
   public void testIncompatibleReturnType() throws Exception { doTest(false); }
   public void testContinueInferenceAfterFirstRawResult() throws Exception { doTest(false); }
