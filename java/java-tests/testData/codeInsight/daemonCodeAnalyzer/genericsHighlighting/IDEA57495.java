26378d12121acbc3deb03b61ec8782b5ff648d37
anna <Anna.Kozlova@jetbrains.com>
2012-10-23 19:18:55 +0200

    testdata for IDEA-57495

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/genericsHighlighting/IDEA57495.java
@@ -0,0 +1,9 @@
+import java.util.*;
+interface C<T> extends List<List<T>>{}
+abstract class A {
+    abstract <T> T baz(List<? super List<? super T>> a);
+
+    void bar(C<?> x){
+        baz<error descr="'baz(java.util.List<? super java.util.List<? super java.lang.Object>>)' in 'A' cannot be applied to '(C<capture<?>>)'">(x)</error>;
+    }
+}
--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/GenericsHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/GenericsHighlightingTest.java
@@ -167,6 +167,7 @@ public class GenericsHighlightingTest extends LightDaemonAnalyzerTestCase {
   public void testIDEA57486() throws Exception { doTest(false); }
   public void testIDEA57492() throws Exception { doTest(false); }
   public void testIDEA57493() throws Exception { doTest(false); }
+  public void testIDEA57495() throws Exception { doTest(false); }
   public void testInconvertibleTypes() throws Exception { doTest(false); }
   public void testIncompatibleReturnType() throws Exception { doTest(false); }
   public void testContinueInferenceAfterFirstRawResult() throws Exception { doTest(false); }
