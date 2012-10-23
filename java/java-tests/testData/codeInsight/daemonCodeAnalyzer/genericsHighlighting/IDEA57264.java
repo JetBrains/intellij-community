d0449739cffa44a64d717a0e438809569722732b
anna <Anna.Kozlova@jetbrains.com>
2012-10-23 20:04:51 +0200

    SOE (IDEA-57264)

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/genericsHighlighting/IDEA57264.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/genericsHighlighting/IDEA57264.java
@@ -2,7 +2,7 @@ class A1<T> {}
 class B1<T extends A1<? super A1<? super T>>>{
     {
         T a = null;
-        A1<? super T> b = a;
+        <error descr="Incompatible types. Found: 'T', required: 'A1<? super T>'">A1<? super T> b = a;</error>
     }
 }
 
