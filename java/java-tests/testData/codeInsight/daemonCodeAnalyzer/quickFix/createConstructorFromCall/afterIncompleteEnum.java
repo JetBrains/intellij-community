7b4b48764d0dc8b97ebd32d01dae206f03f1407f
anna <Anna.Kozlova@jetbrains.com>
2012-10-24 15:36:38 +0200

    skip ignoring whitespaces when choosing the anchor (IDEA-93459)

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/createConstructorFromCall/afterIncompleteEnum.java
@@ -0,0 +1,8 @@
+// "Create Constructor" "true"
+public enum ReportType {
+    AGING("Aging");
+
+    ReportType(String aging) {
+        <selection>//To change body of created methods use File | Settings | File Templates.</selection>
+    }
+}
