6d455c56d31e509cb48a10b21034867c1eba179f
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2012-10-24 15:05:00 +0400

    GitPusher.NO_TARGET_BRANCH: deprecate, fix fake remote

--- plugins/git4idea/src/git4idea/push/GitPusher.java
+++ plugins/git4idea/src/git4idea/push/GitPusher.java
@@ -62,7 +62,8 @@ public final class GitPusher {
    */
   static final int RECENT_COMMITS_NUMBER = 5;
   
-  static final GitRemoteBranch NO_TARGET_BRANCH = new GitStandardRemoteBranch(null, "", GitBranch.DUMMY_HASH);
+  @Deprecated
+  static final GitRemoteBranch NO_TARGET_BRANCH = new GitStandardRemoteBranch(GitRemote.DOT, "", GitBranch.DUMMY_HASH);
 
   private static final Logger LOG = Logger.getInstance(GitPusher.class);
   private static final String INDICATOR_TEXT = "Pushing";
