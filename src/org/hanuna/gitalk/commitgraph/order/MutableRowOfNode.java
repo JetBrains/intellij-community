5e065c428d5da5c6a4972b888384c2c6aa9f71d6
erokhins <smevok@yandex.ru>
2012-10-28 20:18:19 +0400

    add orderNode Calculator

---
+++ src/org/hanuna/gitalk/commitgraph/order/MutableRowOfNode.java
@@ -0,0 +1,112 @@
+package org.hanuna.gitalk.commitgraph.order;
+
+import org.hanuna.gitalk.commitgraph.Node;
+import org.hanuna.gitalk.commitmodel.Commit;
+import org.hanuna.gitalk.common.ReadOnlyIterator;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author erokhins
+ */
+public class MutableRowOfNode implements RowOfNode {
+    @NotNull
+    public static MutableRowOfNode create(RowOfNode row) {
+        List<Node> nodes = new LinkedList<Node>();
+        for (Node node : row) {
+            assert node != null : "null Node in RowOfNode";
+            nodes.add(node);
+        }
+        return new MutableRowOfNode(nodes, row.getRowIndex(), row.getLastColorIndex());
+    }
+
+    @NotNull
+    public static MutableRowOfNode getEmpty(int rowIndex, int lastColorIndex) {
+        List<Node> nodes = new LinkedList<Node>();
+        return new MutableRowOfNode(nodes, rowIndex, lastColorIndex);
+    }
+
+    private final List<Node> nodes;
+    private int rowIndex;
+    private int lastColorIndex;
+
+    private MutableRowOfNode(List<Node> nodes, int rowIndex, int lastColorIndex) {
+        this.nodes = nodes;
+        this.rowIndex = rowIndex;
+        this.lastColorIndex = lastColorIndex;
+    }
+
+    @NotNull
+    public Node removeNode(int index) {
+        return nodes.remove(index);
+    }
+
+    @Nullable
+    public Node removeNode(@NotNull Commit commit) {
+        int index = getIndexOfCommit(commit);
+        if (index == -1) {
+            return null;
+        } else {
+            return nodes.remove(index);
+        }
+    }
+
+    public void addNode(int  index, @NotNull Node node) {
+        nodes.add(index, node);
+    }
+
+    public void addNode(@NotNull Node node) {
+        nodes.add(node);
+    }
+
+    public void setLastColorIndex(int colorIndex) {
+        this.lastColorIndex = colorIndex;
+    }
+
+    public void setRowIndex(int newRowIndex) {
+        this.rowIndex = newRowIndex;
+    }
+
+
+    @Override
+    public int getRowIndex() {
+        return rowIndex;
+    }
+
+    @Override
+    public int getLastColorIndex() {
+        return lastColorIndex;
+    }
+
+    @Override
+    public int getIndexOfCommit(@NotNull Commit commit) {
+        for (int i = 0; i < nodes.size(); i++) {
+            Node node = nodes.get(i);
+            if (node.getCommit().equals(commit)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    @Override
+    public int size() {
+        return nodes.size();
+    }
+
+    @NotNull
+    @Override
+    public Node get(int index) {
+        return nodes.get(index);
+    }
+
+    @Override
+    public Iterator<Node> iterator() {
+        return new ReadOnlyIterator<Node>(nodes.iterator());
+    }
+}
---
+++ src/org/hanuna/gitalk/commitgraph/order/RowOfNodeCalculator.java
@@ -0,0 +1,76 @@
+package org.hanuna.gitalk.commitgraph.order;
+
+import org.hanuna.gitalk.commitgraph.Node;
+import org.hanuna.gitalk.commitmodel.Commit;
+import org.hanuna.gitalk.commitmodel.CommitsModel;
+import org.hanuna.gitalk.common.ReadOnlyList;
+import org.hanuna.gitalk.common.calculatemodel.calculator.AbstractCalculator;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author erokhins
+ */
+public class RowOfNodeCalculator extends AbstractCalculator<MutableRowOfNode, RowOfNode> {
+    private final CommitsModel commitsModel;
+
+    public RowOfNodeCalculator(CommitsModel commitsModel) {
+        this.commitsModel = commitsModel;
+    }
+
+    @NotNull
+    @Override
+    public RowOfNode getFirst() {
+        assert commitsModel.size() > 0 : "empty CommitsModel";
+        Node firstNode = new Node(commitsModel.get(0), 0);
+        MutableRowOfNode firstRow = MutableRowOfNode.getEmpty(0, 0);
+        firstRow.addNode(0, firstNode);
+        return firstRow;
+    }
+
+    @NotNull
+    @Override
+    protected MutableRowOfNode createMutable(RowOfNode prev) {
+        return MutableRowOfNode.create(prev);
+    }
+
+    @Override
+    protected int size() {
+        return commitsModel.size();
+    }
+
+    @NotNull
+    protected MutableRowOfNode oneStep(MutableRowOfNode row) {
+        Commit prevCommit = commitsModel.get(row.getRowIndex());
+        int prevCommitIndex = row.getIndexOfCommit(prevCommit);
+        assert prevCommitIndex != -1 : "bad prev row";
+        Node prevNode = row.removeNode(prevCommitIndex);
+        ReadOnlyList<Commit> parents = prevCommit.getParents();
+
+        int countNewNode = 0;
+        for (int i = 0; i < parents.size(); i++) {
+            int colorIndex;
+            if (i == 0) {
+                colorIndex = prevNode.getColorIndex();
+            } else {
+                colorIndex = row.getLastColorIndex() + i;
+            }
+            Commit parent = parents.get(i);
+            if (row.getIndexOfCommit(parent) == -1) {
+                Node newNode = new Node(parent, colorIndex);
+                row.addNode(prevCommitIndex + countNewNode, newNode);
+                countNewNode++;
+            }
+        }
+        if (parents.size() > 0) {
+            row.setLastColorIndex(row.getLastColorIndex() + parents.size() - 1);
+        }
+        Commit thisCommit = commitsModel.get(row.getRowIndex() + 1);
+        if (thisCommit.getChildren().size() == 0) {
+            row.addNode(new Node(thisCommit, row.getLastColorIndex() + 1));
+            row.setLastColorIndex(row.getLastColorIndex() + 1);
+        }
+
+        row.setRowIndex(row.getRowIndex() + 1);
+        return row;
+    }
+}
