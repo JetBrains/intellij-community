cb0fd56ab8362d525abdb4cbd7796fdac68656ba
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2012-10-30 13:00:00 +0100

    Fix test data

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/advHighlighting/NamesHighlighting.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/advHighlighting/NamesHighlighting.java
@@ -1,44 +1,46 @@
 import <symbolName descr="null" type="CLASS_NAME">java.io</symbolName>.*; // highlight on demand import as class name
 
 class <symbolName descr="null" type="CLASS_NAME">a</symbolName> {
- void <symbolName descr="null" type="METHOD_DECLARATION">method</symbolName>() {
-   <symbolName descr="null" type="METHOD_CALL">method</symbolName>();
+  void <symbolName descr="null" type="METHOD_DECLARATION">method</symbolName>() {
+    <symbolName descr="null" type="METHOD_CALL">method</symbolName>();
 
-   new <symbolName descr="null" type="CONSTRUCTOR_CALL">Exception</symbolName>();
-   new <symbolName descr="null" type="CONSTRUCTOR_CALL">java.lang.Exception</symbolName>();
- }
- <symbolName descr="null" type="CONSTRUCTOR_DECLARATION">a</symbolName>() {
-   new <symbolName descr="null" type="CONSTRUCTOR_CALL">a</symbolName>();
- }
+    new <symbolName descr="null" type="CONSTRUCTOR_CALL">Exception</symbolName>();
+    new <symbolName descr="null" type="CONSTRUCTOR_CALL">java.lang.Exception</symbolName>();
+  }
+
+  <symbolName descr="null" type="CONSTRUCTOR_DECLARATION">a</symbolName>() {
+    new <symbolName descr="null" type="CONSTRUCTOR_CALL">a</symbolName>();
+  }
  
- /**
-   * @see <symbolName descr="null" type="INTERFACE_NAME">itf</symbolName>#<symbolName descr="null" type="METHOD_CALL">method</symbolName>(double)
-  */
+  /**
+   * @see <symbolName descr="null" type="INTERFACE_NAME">itf</symbolName>#<symbolName descr="null" type="ABSTRACT_METHOD">method</symbolName>(double)
+   */
   static void <symbolName descr="null" type="METHOD_DECLARATION">f</symbolName>() {
-   <symbolName descr="null" type="CLASS_NAME">Integer</symbolName>.<symbolName descr="null" type="STATIC_METHOD">parseInt</symbolName>("");
-   <symbolName descr="null" type="CLASS_NAME">java.lang.Integer</symbolName>.<symbolName descr="null" type="STATIC_METHOD">parseInt</symbolName>("");
-   <symbolName descr="null" type="STATIC_METHOD">f</symbolName>();
- }
+    <symbolName descr="null" type="CLASS_NAME">Integer</symbolName>.<symbolName descr="null" type="STATIC_METHOD">parseInt</symbolName>("");
+    <symbolName descr="null" type="CLASS_NAME">java.lang.Integer</symbolName>.<symbolName descr="null" type="STATIC_METHOD">parseInt</symbolName>("");
+    <symbolName descr="null" type="STATIC_METHOD">f</symbolName>();
+  }
 
- interface <symbolName descr="null" type="INTERFACE_NAME">itf</symbolName>{
-   int <symbolName descr="null" type="STATIC_FIELD">CONST</symbolName> = 0;
-   /** .
+  interface <symbolName descr="null" type="INTERFACE_NAME">itf</symbolName> {
+    int <symbolName descr="null" type="STATIC_FINAL_FIELD">CONST</symbolName> = 0;
+
+    /**
      * @param <symbolName descr="null" type="PARAMETER">d</symbolName> Important param
      */
-   void <symbolName descr="null" type="METHOD_DECLARATION">method</symbolName>(double <symbolName descr="null" type="PARAMETER">d</symbolName>);
- }
- void <symbolName descr="null" type="METHOD_DECLARATION">ff</symbolName>(<symbolName descr="null" type="INTERFACE_NAME">Runnable</symbolName> <symbolName descr="null" type="PARAMETER">r</symbolName>) {
-   <symbolName descr="null" type="METHOD_CALL">ff</symbolName>(
-    new <symbolName descr="null" type="ANONYMOUS_CLASS_NAME">java.lang.Runnable</symbolName>()
-       {
+    void <symbolName descr="null" type="METHOD_DECLARATION">method</symbolName>(double <symbolName descr="null" type="PARAMETER">d</symbolName>);
+  }
+
+  void <symbolName descr="null" type="METHOD_DECLARATION">ff</symbolName>(<symbolName descr="null" type="INTERFACE_NAME">Runnable</symbolName> <symbolName descr="null" type="PARAMETER">r</symbolName>) {
+     <symbolName descr="null" type="METHOD_CALL">ff</symbolName>(
+       new <symbolName descr="null" type="ANONYMOUS_CLASS_NAME">java.lang.Runnable</symbolName>() {
          public void <symbolName descr="null" type="METHOD_DECLARATION">run</symbolName>() {}
          int <symbolName descr="null" type="INSTANCE_FIELD">instance</symbolName> = 0;
        }
-   );
+     );
 
-   int <symbolName descr="null" type="LOCAL_VARIABLE">i</symbolName> = <symbolName descr="null" type="CLASS_NAME">java.lang.Integer</symbolName>.<symbolName descr="null" type="STATIC_FIELD">MIN_VALUE</symbolName>;
-   int <symbolName descr="null" type="LOCAL_VARIABLE">j</symbolName> = <symbolName descr="null" type="INTERFACE_NAME">itf</symbolName>.<symbolName descr="null" type="STATIC_FIELD">CONST</symbolName>;
- }
+    int <symbolName descr="null" type="LOCAL_VARIABLE">i</symbolName> = <symbolName descr="null" type="CLASS_NAME">java.lang.Integer</symbolName>.<symbolName descr="null" type="STATIC_FINAL_FIELD">MIN_VALUE</symbolName>;
+    int <symbolName descr="null" type="LOCAL_VARIABLE">j</symbolName> = <symbolName descr="null" type="INTERFACE_NAME">itf</symbolName>.<symbolName descr="null" type="STATIC_FINAL_FIELD">CONST</symbolName>;
+  }
 }
 
 class <symbolName descr="null" type="CLASS_NAME">NoCtrClass</symbolName> {
@@ -46,11 +48,12 @@ class <symbolName descr="null" type="CLASS_NAME">NoCtrClass</symbolName> {
     // default constructor call looks like class
     new <symbolName descr="null" type="CLASS_NAME">NoCtrClass</symbolName>();
   }
-  void <symbolName descr="null" type="METHOD_DECLARATION">ff</symbolName>(int <symbolName descr="null" type="REASSIGNED_PARAMETER">param</symbolName>) {
-    int <symbolName descr="null" type="REASSIGNED_LOCAL_VARIABLE">i</symbolName> = 1;
-    <symbolName descr="null" type="REASSIGNED_LOCAL_VARIABLE">i</symbolName> ++;
 
-    <symbolName descr="null" type="REASSIGNED_PARAMETER">param</symbolName> = 0;
+  void <symbolName descr="null" type="METHOD_DECLARATION">ff</symbolName>(int <symbolName descr="null" type="REASSIGNED_PARAMETER">param</symbolName>) {
+     int <symbolName descr="null" type="REASSIGNED_LOCAL_VARIABLE">i</symbolName> = 1;
+     <symbolName descr="null" type="REASSIGNED_LOCAL_VARIABLE">i</symbolName> ++;
+
+     <symbolName descr="null" type="REASSIGNED_PARAMETER">param</symbolName> = 0;
   }
 }
 
