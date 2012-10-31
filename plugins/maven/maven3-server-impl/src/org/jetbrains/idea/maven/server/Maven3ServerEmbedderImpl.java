38d7ce236092d5b4f2bdca28ee6a3f4ba2787e4e
Sergey Evdokimov <sergey.evdokimov@jetbrains.com>
2012-10-31 16:14:31 +0400

    Revert bad commit

--- plugins/maven/maven3-server-impl/src/org/jetbrains/idea/maven/server/Maven3ServerEmbedderImpl.java
+++ plugins/maven/maven3-server-impl/src/org/jetbrains/idea/maven/server/Maven3ServerEmbedderImpl.java
@@ -338,7 +338,7 @@ public class Maven3ServerEmbedderImpl extends MavenRemoteObject implements Maven
 
           project.setArtifacts(result.getArtifacts());
           // end copied from DefaultMavenProjectBuilder.buildWithDependencies
-          ref.set(new MavenExecutionResult(project, result.getExceptions()));
+          ref.set(new MavenExecutionResult(project, new ArrayList<Exception>()));
         }
         catch (Exception e) {
           ref.set(handleException(e));
