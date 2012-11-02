632d6afdc0293dc7e1b689c6d36d19c14cedcd43
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2012-11-02 19:52:51 +0400

    IDEA-53175 Case rename problems: set core.ignorecase on clone.
    While Git init and clone set core.ignorecase to correct value, JGit doesn't do it.
    => do it manually after successful clone.

--- plugins/git4idea/src/git4idea/jgit/GitHttpAdapter.java
+++ plugins/git4idea/src/git4idea/jgit/GitHttpAdapter.java
@@ -22,6 +22,7 @@ import com.intellij.ide.passwordSafe.impl.PasswordSafeImpl;
 import com.intellij.ide.passwordSafe.impl.PasswordSafeProvider;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.SystemInfo;
 import com.intellij.util.AuthData;
 import git4idea.GitBranch;
 import git4idea.GitUtil;
@@ -39,8 +40,10 @@ import org.eclipse.jgit.api.errors.JGitInternalException;
 import org.eclipse.jgit.errors.NoRemoteRepositoryException;
 import org.eclipse.jgit.errors.NotSupportedException;
 import org.eclipse.jgit.errors.TransportException;
+import org.eclipse.jgit.lib.ConfigConstants;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.StoredConfig;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.eclipse.jgit.transport.RefSpec;
 import org.jetbrains.annotations.NotNull;
@@ -67,6 +70,7 @@ public final class GitHttpAdapter {
   private static final Logger LOG = Logger.getInstance(GitHttpAdapter.class);
 
   private static final Pattern HTTP_URL_WITH_USERNAME_AND_PASSWORD = Pattern.compile("http(s?)://([^\\s^@:]+):([^\\s^@:]+)@.*");
+  private static final String IGNORECASE_SETTING = "ignorecase";
 
   public static boolean shouldUseJGit(@NotNull String url) {
     if (!url.startsWith("http")) {
@@ -234,8 +238,13 @@ public final class GitHttpAdapter {
     GitFetchResult.Type resultType;
     try {
       final GitHttpCredentialsProvider provider = new GitHttpCredentialsProvider(project, url);
-      GeneralResult result = callWithAuthRetry(new GitHttpRemoteCommand.Clone(directory,  provider, url), project);
+      GitHttpRemoteCommand.Clone command = new GitHttpRemoteCommand.Clone(directory, provider, url);
+      GeneralResult result = callWithAuthRetry(command, project);
       resultType = convertToFetchResultType(result);
+      if (resultType.equals(GitFetchResult.Type.SUCCESS)) {
+        updateCoreIgnoreCaseSetting(command.getGit());
+      }
+      return new GitFetchResult(resultType);
     }
     catch (InvalidRemoteException e) {
       LOG.info("Exception while cloning " + url + " to " + directory, e);
@@ -249,7 +258,24 @@ public final class GitHttpAdapter {
       LOG.info("Exception while cloning " + url + " to " + directory, e);
       return GitFetchResult.error(e);
     }
-    return new GitFetchResult(resultType);
+  }
+
+  private static void updateCoreIgnoreCaseSetting(@Nullable Git git) {
+    if (SystemInfo.isFileSystemCaseSensitive) {
+      return;
+    }
+    if (git == null) {
+      LOG.info("jgit.Git is null, the command should have failed. Not updating the settings.");
+      return;
+    }
+    StoredConfig config = git.getRepository().getConfig();
+    config.setString(ConfigConstants.CONFIG_CORE_SECTION, null, IGNORECASE_SETTING, Boolean.TRUE.toString());
+    try {
+      config.save();
+    }
+    catch (IOException e) {
+      LOG.info("Couldn't save config for " + git.getRepository().getDirectory().getPath(), e);
+    }
   }
 
   @NotNull
