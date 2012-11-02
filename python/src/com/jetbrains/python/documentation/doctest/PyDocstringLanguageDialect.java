469504786fcbd573e553388bbb1826a7675e3cf7
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2012-11-02 14:42:29 +0400

    Edit injection language fragment action is not available for Python Docstring language

--- python/src/com/jetbrains/python/documentation/doctest/PyDocstringLanguageDialect.java
+++ python/src/com/jetbrains/python/documentation/doctest/PyDocstringLanguageDialect.java
@@ -1,5 +1,6 @@
 package com.jetbrains.python.documentation.doctest;
 
+import com.intellij.codeInsight.intention.impl.QuickEditAction;
 import com.intellij.lang.InjectableLanguage;
 import com.intellij.lang.Language;
 import com.jetbrains.python.PythonLanguage;
@@ -14,5 +15,6 @@ public class PyDocstringLanguageDialect extends Language implements InjectableLa
 
   protected PyDocstringLanguageDialect() {
     super(PythonLanguage.getInstance(), "PyDocstring");
+    putUserData(QuickEditAction.EDIT_ACTION_AVAILABLE, false);
   }
 }
