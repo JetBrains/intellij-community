4d84e2e1f9a2e91ee7c64fc9514b4ba137f43302
peter <peter@jetbrains.com>
2012-11-05 15:18:09 +0100

    highlight usages tests -> community

---
+++ java/java-tests/testSrc/com/intellij/codeInsight/highlighting/HighlightUsagesHandlerTest.groovy
@@ -0,0 +1,159 @@
+package com.intellij.codeInsight.highlighting;
+
+import com.intellij.JavaTestUtil;
+import com.intellij.codeInspection.LocalInspectionTool;
+import com.intellij.codeInspection.sillyAssignment.SillyAssignmentInspection;
+import com.intellij.openapi.editor.markup.RangeHighlighter;
+import com.intellij.testFramework.LightCodeInsightTestCase;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author cdr
+ */
+public class HighlightUsagesHandlerTest extends LightCodeInsightTestCase {
+  private static void ctrlShiftF7() {
+    HighlightUsagesHandler.invoke(getProject(), getEditor(), getFile());
+  }
+
+  public void testSimpleThrows() throws Exception {
+    configureFile();
+    ctrlShiftF7();
+    assertRangeText("throws", "Exception");
+    checkUnselect();
+  }
+  public void testThrowsExpression() throws Exception {
+    configureFile();
+    ctrlShiftF7();
+    assertRangeText("throws", "(Exception)detail");
+    checkUnselect();
+  }
+  public void testThrowsReference() throws Exception {
+    configureFile();
+    ctrlShiftF7();
+    assertRangeText("throws", "detail");
+    checkUnselect();
+  }
+
+  private static void checkUnselect() {
+    ctrlShiftF7();
+    assertRangeText();
+  }
+
+  public void testUnselectUsage() throws Exception {
+    configureFile();
+    ctrlShiftF7();
+    assertRangeText("foo", "foo", "foo");
+    checkUnselect();
+  }
+
+  public void testHighlightOverridden() throws Exception {
+    configureFile();
+    ctrlShiftF7();
+    assertRangeText("extends", "foo");
+    checkUnselect();
+  }
+  public void testHighlightOverriddenImplements() throws Exception {
+    configureFile();
+    ctrlShiftF7();
+    assertRangeText("implements", "foo");
+    checkUnselect();
+  }
+  public void testHighlightOverriddenNothing() throws Exception {
+    configureFile();
+    ctrlShiftF7();
+    assertRangeText();
+    checkUnselect();
+  }
+  public void testHighlightOverriddenMultiple() throws Exception {
+    configureFile();
+    ctrlShiftF7();
+    assertRangeText("implements", "foo", "other");
+    checkUnselect();
+  }
+
+  public void testIDEADEV28822() throws Exception {
+    configureFromFileText("Foo.java",
+                          """public class Foo {public String foo(String s) {
+    while (s.length() > 0) {
+      if (s.length() < 0) {
+        s = "";
+        continue;
+      }
+      else {
+      }
+    }
+    re<caret>turn s;
+  }
+}""");
+    ctrlShiftF7();
+    assertRangeText("return s;");
+  }
+            
+  public void testReturnsInTryFinally() throws Exception {
+    // See IDEADEV-14028
+    configureFromFileText("Foo.java",
+                          """public class Foo {
+  int foo(boolean b) {
+    try {
+      if (b) return 1;
+    }
+    finally {
+      if (b) return 2;
+    }
+    r<caret>eturn 3;
+  }
+}""");
+
+    ctrlShiftF7();
+    assertRangeText("return 1;", "return 2;", "return 3;");
+  }
+
+  public void testReturnsInLambda() throws Exception {
+    // See IDEADEV-14028
+    configureFromFileText("Foo.java",
+                          """public class Foo {
+  {
+    Runnable r = () -> {
+           if (true) return;
+           retur<caret>n;
+    }
+  }
+}""");
+
+    ctrlShiftF7();
+    assertRangeText("return;", "return;");
+  }
+
+  @Override
+  protected LocalInspectionTool[] configureLocalInspectionTools() {
+    return [new SillyAssignmentInspection()]
+  }
+
+  public void testSuppressedWarningsHighlights() throws Exception {
+    configureFromFileText("Foo.java", """public class Foo {
+        @SuppressWarnings({"Sil<caret>lyAssignment"})
+        void foo() {
+            int i = 0;
+            i = i;
+        }
+    }""");
+    ctrlShiftF7();
+    assertRangeText("i = i");
+  }
+
+  private static void assertRangeText(@NonNls String... texts) {
+    RangeHighlighter[] highlighters = getEditor().getMarkupModel().getAllHighlighters();
+    assertSameElements(highlighters.collect { myFile.text.substring(it.startOffset, it.endOffset) }, texts)
+  }
+  
+  private void configureFile() throws Exception {
+    configureByFile("/codeInsight/highlightUsagesHandler/" + getTestName(false) + ".java");
+  }
+
+  @NotNull
+  @Override
+  protected String getTestDataPath() {
+    return JavaTestUtil.getJavaTestDataPath();
+  }
+}
