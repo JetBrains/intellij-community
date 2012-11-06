f3d1e78817edd3ea812eb7b37adba2c7ae0abb24
Konstantin Bulenkov <kb@jetbrains.com>
2012-11-06 11:12:38 +0100

    IDEA-94258 (Comboboxes are too high)

--- platform/platform-impl/src/com/intellij/ide/ui/laf/darcula/ui/DarculaComboBoxUI.java
+++ platform/platform-impl/src/com/intellij/ide/ui/laf/darcula/ui/DarculaComboBoxUI.java
@@ -17,7 +17,7 @@ package com.intellij.ide.ui.laf.darcula.ui;
 
 import com.intellij.ide.ui.laf.darcula.DarculaUIUtil;
 import com.intellij.openapi.ui.GraphicsConfig;
-import com.intellij.ui.ColorUtil;
+import com.intellij.ui.Gray;
 import com.intellij.util.ui.JBInsets;
 import com.intellij.util.ui.UIUtil;
 
@@ -34,7 +34,7 @@ import java.awt.geom.Path2D;
  */
 public class DarculaComboBoxUI extends BasicComboBoxUI implements Border {
   private final JComboBox myComboBox;
-  private JBInsets myComboBoxInsets = new JBInsets(4, 7, 4, 3);
+  private JBInsets myComboBoxInsets = new JBInsets(4, 7, 4, 5);
 
   public DarculaComboBoxUI(JComboBox comboBox) {
     myComboBox = comboBox;
@@ -67,13 +67,15 @@ protected JButton createArrowButton() {
         final int xU = w / 4;
         final int yU = h / 4;
         final Path2D.Double path = new Path2D.Double();
+        g.translate(1,1);
         path.moveTo(xU + 1, yU + 2);
         path.lineTo(3 * xU + 1, yU + 2);
         path.lineTo(2 * xU + 1, 3 * yU);
         path.lineTo(xU + 1, yU + 2);
         path.closePath();
         g.fill(path);
-        g.setColor(ColorUtil.fromHex("939393"));
+        g.translate(-1,-1);
+        g.setColor(getBorderColor());
         g.drawLine(0, -1, 0, h);
         config.restore();
       }
@@ -104,29 +106,34 @@ protected JButton createArrowButton() {
       g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
       g.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_NORMALIZE);
       g.setColor(editor.getBackground());
-      g.fillRoundRect(x+1,y+1,width-2,height-2, 5, 5);
+      g.fillRoundRect(x+1,y+2,width-2,height-4, 5, 5);
       g.setColor(arrowButton.getBackground());
-      g.fillRoundRect(xxx, y+1, width - xxx , height-2, 5, 5);
+      g.fillRoundRect(xxx, y+2, width - xxx , height-4, 5, 5);
       g.setColor(editor.getBackground());
-      g.fillRect(xxx, y + 1, 5, height - 2);
+      g.fillRect(xxx, y + 2, 5, height - 4);
       config.restore();
     }
 
-    g.setColor(ColorUtil.fromHex("939393"));
+    final Color borderColor = getBorderColor();//ColorUtil.shift(UIUtil.getBorderColor(), 4);
+    g.setColor(borderColor);
     int off = hasFocus ? 1 : 0;
-    g.drawLine(xxx + 5, y+1 + off, xxx + 5, height - 1 - 2*off);
+    g.drawLine(xxx + 5, y+2 + off, xxx + 5, height - 2 - 2*off);
 
     if (hasFocus || (editor != null && editor.hasFocus())) {
       DarculaUIUtil.paintFocusRing(g, 2, 2, width - 4, height - 4);
     }
     else {
-      g.setColor(ColorUtil.fromHex("939393"));
+      g.setColor(borderColor);
       final GraphicsConfig config = new GraphicsConfig(g);
       g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
       g.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_NORMALIZE);
-      g.drawRoundRect(1, 1, width - 2, height - 2, 5, 5);
+      g.drawRoundRect(1, 2, width - 2, height - 4, 5, 5);
       config.restore();
     }
+  }
+
+  private Gray getBorderColor() {
+    return Gray._100;
   }
 
   @Override
