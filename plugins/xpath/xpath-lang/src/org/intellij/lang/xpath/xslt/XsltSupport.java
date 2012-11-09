abbcd4ccac9bf930589eb10411964da0e3f3ad95
anna <Anna.Kozlova@jetbrains.com>
2012-11-09 21:42:21 +0100

    fix tests

--- plugins/xpath/xpath-lang/src/org/intellij/lang/xpath/xslt/XsltSupport.java
+++ plugins/xpath/xpath-lang/src/org/intellij/lang/xpath/xslt/XsltSupport.java
@@ -115,7 +115,7 @@ public class XsltSupport {
         if (isXsltAttribute(attribute)) {
             final String tagName = attribute.getParent().getLocalName();
             final String s = XPATH_ATTR_MAP.get(name);
-            if (s != null && s.length() > 0 && !tagName.equals(s)) {
+            if ((s == null || s.length() > 0) && !tagName.equals(s)) {
                 if (!isAttributeValueTemplate(attribute, true)) {
                     return false;
                 }
