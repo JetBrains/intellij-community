c58640ebbf456bc08d2cfd6d6eeff8ceb35cfdc8
irengrig <Irina.Chernushina@jetbrains.com>
2012-11-09 20:18:54 +0400

    IDEA-94664 Subversion: local changes inside external directory is not shown if command line acceleration is used

--- plugins/svn4idea/src/org/jetbrains/idea/svn/commandLine/SvnCommandLineStatusClient.java
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/commandLine/SvnCommandLineStatusClient.java
@@ -35,7 +35,9 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.Collection;
 
 /**
@@ -112,9 +114,10 @@ public class SvnCommandLineStatusClient implements SvnStatusClientI {
     if (includeIgnored) {
       command.addParameters("--no-ignore");
     }
-    if (! collectParentExternals) {
+    // no way in interface to ignore externals
+    /*if (! collectParentExternals) {
       command.addParameters("--ignore-externals");
-    }
+    }*/
 
     //--changelist (--cl) ARG
     changelistsToCommand(changeLists, command);
