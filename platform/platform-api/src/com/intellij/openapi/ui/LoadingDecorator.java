5f8038426933ef08cf912e29c9989241017ca36f
Dmitry Jemerov <yole@jetbrains.com>
2012-11-09 17:19:32 +0100

    removing test code out of production code

--- platform/platform-api/src/com/intellij/openapi/ui/LoadingDecorator.java
+++ platform/platform-api/src/com/intellij/openapi/ui/LoadingDecorator.java
@@ -227,38 +227,6 @@ public class LoadingDecorator {
     }
   }
 
-  public static void main(String[] args) {
-    IconLoader.activate();
-
-    final JFrame frame = new JFrame();
-    frame.getContentPane().setLayout(new BorderLayout());
-
-    final JPanel content = new JPanel(new BorderLayout());
-
-    final LoadingDecorator loadingTree = new LoadingDecorator(new JComboBox(), Disposer.newDisposable(), -1);
-
-    content.add(loadingTree.getComponent(), BorderLayout.CENTER);
-
-    final JCheckBox loadingCheckBox = new JCheckBox("Loading");
-    loadingCheckBox.addActionListener(new ActionListener() {
-      public void actionPerformed(final ActionEvent e) {
-        if (loadingTree.isLoading()) {
-          loadingTree.stopLoading();
-        } else {
-          loadingTree.startLoading(false);
-        }
-      }
-    });
-
-    content.add(loadingCheckBox, BorderLayout.SOUTH);
-
-
-    frame.getContentPane().add(content, BorderLayout.CENTER);
-
-    frame.setBounds(300, 300, 300, 300);
-    frame.show();
-  }
-
   private static class MyLayeredPane extends JLayeredPane {
     @Override
     public void doLayout() {
