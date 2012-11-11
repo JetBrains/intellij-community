ec75c0ec1823b084ea8e4fd1f14397096676264b
Max Medvedev <maxim.medvedev@jetbrains.com>
2012-11-11 16:22:25 +0400

    IDEA-94709 Groovy: create member from ref is not available in static methods

--- plugins/groovy/src/org/jetbrains/plugins/groovy/annotator/intentions/QuickfixUtil.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/annotator/intentions/QuickfixUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2012 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -53,7 +53,12 @@ import java.util.*;
 public class QuickfixUtil {
   @Nullable
   public static PsiClass findTargetClass(GrReferenceExpression refExpr, boolean compileStatic) {
+    if (refExpr.getQualifier() == null) {
+      return PsiUtil.getContextClass(refExpr);
+    }
+
     PsiType type = GrReferenceResolveUtil.getQualifierType(refExpr);
+
     if (type == null && compileStatic) {
       return GroovyPsiManager.getInstance(refExpr.getProject()).findClassWithCache(CommonClassNames.JAVA_LANG_OBJECT, refExpr.getResolveScope());
     }
