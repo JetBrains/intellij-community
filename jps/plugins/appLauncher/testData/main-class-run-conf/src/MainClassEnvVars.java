df4197265bb0eb8932f2c21dd011a10b8772a4ce
nik <Nikolay.Chashnikov@jetbrains.com>
2012-11-12 11:19:43 +0400

    jps; unused modules removed

--- jps/plugins/appLauncher/testData/main-class-run-conf/src/MainClassEnvVars.java
+++
@@ -1,8 +0,0 @@
-import java.lang.System;
-
-public class MainClassEnvVars {
-  public static void main(String[] args) {
-    System.out.println(System.getenv("ENV1"));
-    System.out.println(System.getenv("ENV2"));
-  }
-}
