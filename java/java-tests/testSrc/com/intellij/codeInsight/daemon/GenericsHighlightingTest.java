7cee6ae2b4d6c82247ecefec6a788228dd856dff
anna <Anna.Kozlova@jetbrains.com>
2012-11-13 20:11:32 +0100

    temp decline raw assignments (IDEA-92022)

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/GenericsHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/GenericsHighlightingTest.java
@@ -193,6 +193,7 @@ public class GenericsHighlightingTest extends LightDaemonAnalyzerTestCase {
   public void _testIDEA94011() throws Exception { doTest(false); }
   public void testDifferentTypeParamsInOverloadedMethods() throws Exception { doTest(true); }
   public void testIDEA91626() throws Exception { doTest(true); }
+  public void testIDEA92022() throws Exception { doTest(false); }
 
   public void testJavaUtilCollections_NoVerify() throws Exception {
     PsiClass collectionsClass = getJavaFacade().findClass("java.util.Collections", GlobalSearchScope.moduleWithLibrariesScope(getModule()));
