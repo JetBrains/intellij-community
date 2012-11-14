fb90b29b0174e1c6d21c57bb34775a95249dcb39
irengrig <Irina.Chernushina@jetbrains.com>
2012-11-14 16:24:33 +0400

    IDEA-87472 CCE at com.intellij.ide.favoritesTreeView.FavoritesPanel$1.drop

--- platform/lang-impl/src/com/intellij/ide/favoritesTreeView/actions/SendToFavoritesAction.java
+++ platform/lang-impl/src/com/intellij/ide/favoritesTreeView/actions/SendToFavoritesAction.java
@@ -21,13 +21,11 @@ import com.intellij.ide.favoritesTreeView.FavoritesManager;
 import com.intellij.ide.favoritesTreeView.FavoritesTreeNodeDescriptor;
 import com.intellij.ide.favoritesTreeView.FavoritesTreeViewPanel;
 import com.intellij.ide.util.treeView.AbstractTreeNode;
-import com.intellij.ide.util.treeView.NodeDescriptor;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.text.StringUtil;
 
 import java.util.Collections;
 
@@ -48,28 +46,14 @@ public class SendToFavoritesAction extends AnAction{
     final FavoritesManager favoritesManager = FavoritesManager.getInstance(project);
 
     FavoritesTreeNodeDescriptor[] roots = FavoritesTreeViewPanel.CONTEXT_FAVORITES_ROOTS_DATA_KEY.getData(dataContext);
-
-    String listName = null;
     if (roots == null) return;
+
     for (FavoritesTreeNodeDescriptor root : roots) {
-      final AbstractTreeNode element = root.getElement();
-      if (element instanceof FavoritesListNode) {
-        listName = null; break;
-      }
-      final NodeDescriptor parent = root.getParentDescriptor();
-      if (!(parent instanceof FavoritesTreeNodeDescriptor)) {
-        listName = null; break;
-      }
-      String name = ((FavoritesListNode)parent.getElement()).getName();
-      if (listName == null) {
-        listName = name;
-      }
-      if (!StringUtil.equals(listName, name)) {
-        listName = null; break;
+      FavoritesTreeNodeDescriptor listNode = root.getFavoritesRoot();
+      if (listNode != null && listNode.getElement() instanceof FavoritesListNode) {
+        doSend(favoritesManager, new FavoritesTreeNodeDescriptor[]{root}, listNode.getElement().getName());
       }
     }
-
-    doSend(favoritesManager, roots, listName);
   }
 
   public void doSend(final FavoritesManager favoritesManager, final FavoritesTreeNodeDescriptor[] roots, final String listName) {
@@ -92,13 +76,13 @@ public class SendToFavoritesAction extends AnAction{
       e.getPresentation().setEnabled(false);
       return;
     }
-    //FavoritesTreeNodeDescriptor[] roots = FavoritesTreeViewPanel.CONTEXT_FAVORITES_ROOTS_DATA_KEY.getData(dataContext);
-    //if (roots == null || roots.length == 0) {
-    //  e.getPresentation().setEnabled(false);
-    //  return;
-    //}
+    FavoritesTreeNodeDescriptor[] roots = FavoritesTreeViewPanel.CONTEXT_FAVORITES_ROOTS_DATA_KEY.getData(dataContext);
+    if (roots == null || roots.length == 0) {
+      e.getPresentation().setEnabled(false);
+      return;
+    }
 
-    //String listName = FavoritesTreeViewPanel.FAVORITES_LIST_NAME_DATA_KEY.getData(dataContext);
+    ///String listName = FavoritesTreeViewPanel.FAVORITES_LIST_NAME_DATA_KEY.getData(dataContext);
     e.getPresentation().setEnabled(true);
   }
 }
