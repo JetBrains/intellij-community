7fe3c56a397986587209ab0adff51016623b08a7
Eugene Zhuravlev <jeka@intellij.com>
2012-11-20 00:24:19 +0100

    form's bound source discovery optimization

--- jps/jps-builders/testSrc/org/jetbrains/ether/UiDesignerTest.java
+++ jps/jps-builders/testSrc/org/jetbrains/ether/UiDesignerTest.java
@@ -1,11 +1,26 @@
 package org.jetbrains.ether;
 
+import org.jetbrains.jps.builders.BuildResult;
+import org.jetbrains.jps.incremental.java.JavaBuilder;
+
 /**
  * @author nik
  */
 public class UiDesignerTest extends IncrementalTestCase {
   public UiDesignerTest() throws Exception {
     super("uiDesigner");
+  }
+
+  @Override
+  public BuildResult doTest() {
+    final boolean wasEnabled = JavaBuilder.isFormsInstrumentationEnabled();
+    try {
+      JavaBuilder.setFormsInstrumentationEnabled(true);
+      return super.doTest();
+    }
+    finally {
+      JavaBuilder.setFormsInstrumentationEnabled(wasEnabled);
+    }
   }
 
   public void testSimple() throws Exception {
