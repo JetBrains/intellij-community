b4e7ef0ae3878536e39e00504040a88f010268df
nik <Nikolay.Chashnikov@jetbrains.com>
2012-11-21 17:32:22 +0400

    optimization [approved by Jeka]

--- jps/jps-builders/src/org/jetbrains/jps/incremental/FSOperations.java
+++ jps/jps-builders/src/org/jetbrains/jps/incremental/FSOperations.java
@@ -176,6 +176,8 @@ public class FSOperations {
   }
 
   public static void pruneEmptyDirs(CompileContext context, @Nullable final Set<File> dirsToDelete) {
+    if (dirsToDelete == null || dirsToDelete.isEmpty()) return;
+
     Set<File> doNotDelete = ALL_OUTPUTS_KEY.get(context);
     if (doNotDelete == null) {
       doNotDelete = new THashSet<File>(FileUtil.FILE_HASHING_STRATEGY);
