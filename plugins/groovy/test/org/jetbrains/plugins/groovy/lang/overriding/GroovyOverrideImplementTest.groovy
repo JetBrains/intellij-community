a747e257396afe7c569be90379547ab22d911428
Max Medvedev <maxim.medvedev@jetbrains.com>
2012-11-22 18:30:53 +0400

    IDEA-95550 Groovy: add throws clause to generated methods

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/overriding/GroovyOverrideImplementTest.groovy
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/overriding/GroovyOverrideImplementTest.groovy
@@ -1,19 +1,32 @@
-package org.jetbrains.plugins.groovy.lang.overriding;
-
-
+/*
+ * Copyright 2000-2012 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jetbrains.plugins.groovy.lang.overriding
 import com.intellij.codeInsight.generation.OverrideImplementUtil
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.psi.JavaPsiFacade
 import com.intellij.psi.PsiClassOwner
 import com.intellij.psi.PsiMethod
 import com.intellij.psi.impl.source.PostprocessReformattingAspect
-import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase
+import com.intellij.psi.search.GlobalSearchScope
+import org.jetbrains.plugins.groovy.LightGroovyTestCase
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.GrTypeDefinition
-
 /**
  * @author peter
  */
-public class GroovyOverrideImplementTest extends LightCodeInsightFixtureTestCase {
+public class GroovyOverrideImplementTest extends LightGroovyTestCase {
 
   public void testInEmptyBraces() throws Exception {
     myFixture.configureByText "a.groovy", """
@@ -86,6 +99,34 @@ class Test<T> extends Base<T> {
 """
   }
 
+  void testTrhowsList() {
+    myFixture.configureByText('a.groovy', '''\
+class X implements I {
+    <caret>
+}
+
+interface I {
+    void foo() throws RuntimeException
+}
+''')
+
+    generateImplementation(findMethod('I', 'foo'))
+
+    myFixture.checkResult('''\
+class X implements I {
+
+    @Override
+    void foo() throws RuntimeException {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
+
+interface I {
+    void foo() throws RuntimeException
+}
+''')
+  }
+
   public void _testImplementIntention() {
     myFixture.configureByText('a.groovy', '''
 class Base<E> {
@@ -104,19 +145,20 @@ class Test extends Base<String> {
   }
 
   private def generateImplementation(PsiMethod method) {
-    ApplicationManager.application.runWriteAction(new Runnable() {
-      @Override
-      void run() {
-        GrTypeDefinition clazz = ((PsiClassOwner) myFixture.file).classes[0]
-        OverrideImplementUtil.overrideOrImplement(clazz, method);
-        PostprocessReformattingAspect.getInstance(myFixture.project).doPostponedFormatting()
-      }
-    });
+    ApplicationManager.application.runWriteAction {
+      GrTypeDefinition clazz = (myFixture.file as PsiClassOwner).classes[0] as GrTypeDefinition
+      OverrideImplementUtil.overrideOrImplement(clazz, method);
+      PostprocessReformattingAspect.getInstance(myFixture.project).doPostponedFormatting()
+    }
     myFixture.editor.selectionModel.removeSelection()
   }
 
-  def findMethod(String className, String methodName) {
-    return JavaPsiFacade.getInstance(project).findClass(className).findMethodsByName(methodName, false)[0]
+  PsiMethod findMethod(String className, String methodName) {
+    return JavaPsiFacade.getInstance(project).findClass(className, GlobalSearchScope.allScope(project)).findMethodsByName(methodName, false)[0]
   }
 
+  @Override
+  protected String getBasePath() {
+    return null
+  }
 }
