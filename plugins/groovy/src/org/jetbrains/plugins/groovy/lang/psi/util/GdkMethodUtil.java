720a96873b92de40bfbe21d0bb6c465d09f75817
Max Medvedev <maxim.medvedev@jetbrains.com>
2012-11-26 12:27:36 +0400

    Groovy: cache metaclass mixin results, performance rev = Peter

--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/util/GdkMethodUtil.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/util/GdkMethodUtil.java
@@ -250,10 +250,10 @@ public class GdkMethodUtil {
         GrMethodCall call = (GrMethodCall)statement;
 
         Pair<PsiClassType, GrReferenceExpression> original = getTypeToMixIn(call);
-        if (original == null) return null;
+        if (original == null) return Result.create(null, PsiModificationTracker.MODIFICATION_COUNT);
 
         PsiClass mix = getTypeToMix(call);
-        if (mix == null) return null;
+        if (mix == null) return Result.create(null, PsiModificationTracker.MODIFICATION_COUNT);
 
         return Result.create(new Trinity<PsiClassType, GrReferenceExpression, PsiClass>(original.first, original.second, mix),
                              PsiModificationTracker.MODIFICATION_COUNT);
