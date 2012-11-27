d34d77bd6e65effa21e91ce3aab076d752c189ff
Nadya.Zabrodina <Nadya.Zabrodina@jetbrains.com>
2012-11-27 16:07:07 +0400

    IDEA-65964 Push/Pull servers in Mercurial
    *HgRememberedInputs - PersistentStateComponent to remember user inputs across Mercurial. HgPushDialog and HgPullDialog  to remember visited repository urls  and offer dropdown list.
    *Move  setRoots() method call after init(), cause  when EditorComboBox init, its selected item became a default index item (0), so default path for current repository replace to another value.

--- plugins/hg4idea/src/org/zmlx/hg4idea/ui/HgPullDialog.java
+++ plugins/hg4idea/src/org/zmlx/hg4idea/ui/HgPullDialog.java
@@ -13,16 +13,19 @@
 package org.zmlx.hg4idea.ui;
 
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.editor.event.DocumentAdapter;
+import com.intellij.openapi.editor.event.DocumentEvent;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.ui.EditorComboBox;
+import com.intellij.util.ArrayUtil;
 import com.intellij.util.ui.UIUtil;
+import org.zmlx.hg4idea.HgRememberedInputs;
 import org.zmlx.hg4idea.command.HgShowConfigCommand;
 
 import javax.swing.*;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Collection;
@@ -31,8 +34,8 @@ public class HgPullDialog extends DialogWrapper {
 
   private final Project project;
   private HgRepositorySelectorComponent hgRepositorySelector;
-  private JTextField sourceTxt;
   private JPanel mainPanel;
+  private EditorComboBox myRepositoryURL;
 
   public HgPullDialog(Project project) {
     super(project, false);
@@ -43,31 +46,36 @@ public class HgPullDialog extends DialogWrapper {
         onChangeRepository();
       }
     });
-    DocumentListener documentListener = new DocumentListener() {
-      public void insertUpdate(DocumentEvent e) {
-        onChangePullSource();
-      }
 
-      public void removeUpdate(DocumentEvent e) {
-        onChangePullSource();
-      }
-
-      public void changedUpdate(DocumentEvent e) {
-        onChangePullSource();
-      }
-    };
-    sourceTxt.getDocument().addDocumentListener(documentListener);
     setTitle("Pull");
     setOKButtonText("Pull");
     init();
   }
+
+  public void createUIComponents() {
+    myRepositoryURL = new EditorComboBox("");
+    final HgRememberedInputs rememberedInputs = HgRememberedInputs.getInstance(project);
+    myRepositoryURL.setHistory(ArrayUtil.toObjectArray(rememberedInputs.getRepositoryUrls(), String.class));
+    myRepositoryURL.addDocumentListener(new DocumentAdapter() {
+      @Override
+      public void documentChanged(DocumentEvent e) {
+        onChangePullSource();
+      }
+    });
+  }
+
+  public void rememberSettings() {
+    final HgRememberedInputs rememberedInputs = HgRememberedInputs.getInstance(project);
+    rememberedInputs.addRepositoryUrl(getSource());
+  }
+
 
   public VirtualFile getRepository() {
     return hgRepositorySelector.getRepository();
   }
 
   public String getSource() {
-    return sourceTxt.getText();
+    return myRepositoryURL.getText();
   }
 
   public void setRoots(Collection<VirtualFile> repos) {
@@ -94,7 +102,7 @@ public class HgPullDialog extends DialogWrapper {
         UIUtil.invokeAndWaitIfNeeded(new Runnable() {
           @Override
           public void run() {
-            sourceTxt.setText(defaultPath);
+            myRepositoryURL.setText(defaultPath);
           }
         });
 
@@ -104,7 +112,7 @@ public class HgPullDialog extends DialogWrapper {
   }
 
   private void onChangePullSource() {
-    setOKActionEnabled(!StringUtil.isEmptyOrSpaces(sourceTxt.getText()));
+    setOKActionEnabled(!StringUtil.isEmptyOrSpaces(myRepositoryURL.getText()));
   }
 
   @Override
