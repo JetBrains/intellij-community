f42a3e80e0427511152a9735dc9900fdcc3a7f90
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2012-11-28 14:10:09 +0400

    fixed PY-8104 Change Signature: refactoring is invoked for method not at the caret when caret is inside other method

--- python/src/com/jetbrains/python/refactoring/changeSignature/PyChangeSignatureHandler.java
+++ python/src/com/jetbrains/python/refactoring/changeSignature/PyChangeSignatureHandler.java
@@ -9,7 +9,6 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiReference;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.refactoring.RefactoringBundle;
 import com.intellij.refactoring.changeSignature.ChangeSignatureHandler;
@@ -37,22 +36,12 @@ public class PyChangeSignatureHandler implements ChangeSignatureHandler {
   @Nullable
   @Override
   public PsiElement findTargetMember(PsiElement element) {
-    final PyReferenceExpression referenceExpression = PsiTreeUtil.getParentOfType(element, PyReferenceExpression.class);
-    if (referenceExpression != null) {
-      final PsiReference reference = referenceExpression.getReference();
-      final PsiElement resolved = reference.resolve();
-      if (resolved instanceof PyFunction) return resolved;
+    final PyCallExpression callExpression = PsiTreeUtil.getParentOfType(element, PyCallExpression.class);
+    if (callExpression != null) {
+      return callExpression.resolveCalleeFunction(PyResolveContext.defaultContext());
     }
     final PyFunction function = PsiTreeUtil.getParentOfType(element, PyFunction.class);
-    if (function != null)
-      return function;
-    else {
-      final PyCallExpression callExpression = PsiTreeUtil.getParentOfType(element, PyCallExpression.class);
-      if (callExpression != null) {
-        return callExpression.resolveCalleeFunction(PyResolveContext.defaultContext());
-      }
-    }
-    return null;
+    return function;
   }
 
   @Override
