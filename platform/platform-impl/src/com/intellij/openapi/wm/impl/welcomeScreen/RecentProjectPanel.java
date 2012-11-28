91d397027e457d1953cae3b45fbc35e8600942c4
Maxim Shafirov <max@jetbrains.com>
2012-11-28 09:51:53 +0100

    Hand cursor for the recents list making sure it's clear the item is to be executed on single click

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/RecentProjectPanel.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/RecentProjectPanel.java
@@ -112,6 +112,13 @@ public class RecentProjectPanel extends JPanel {
           Point point = e.getPoint();
           int index = myList.locationToIndex(point);
           myList.setSelectedIndex(index);
+
+          if (myList.getCellBounds(index, index).contains(point)) {
+            myList.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+          }
+          else {
+            myList.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+          }
         }
         else {
           myIsEngaged = true;
