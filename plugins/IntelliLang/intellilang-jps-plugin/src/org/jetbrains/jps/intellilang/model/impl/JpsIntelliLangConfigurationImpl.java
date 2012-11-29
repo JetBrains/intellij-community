52cb602e4c0133836d09d453b5b4f5a42884751d
Eugene Zhuravlev <jeka@intellij.com>
2012-11-29 16:10:02 +0100

    intellilang pattern validation for external build

---
+++ plugins/IntelliLang/intellilang-jps-plugin/src/org/jetbrains/jps/intellilang/model/impl/JpsIntelliLangConfigurationImpl.java
@@ -0,0 +1,50 @@
+package org.jetbrains.jps.intellilang.model.impl;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.jps.intellilang.instrumentation.InstrumentationType;
+import org.jetbrains.jps.intellilang.model.JpsIntelliLangConfiguration;
+import org.jetbrains.jps.model.JpsElementChildRole;
+import org.jetbrains.jps.model.ex.JpsElementBase;
+import org.jetbrains.jps.model.ex.JpsElementChildRoleBase;
+
+/**
+ * @author Eugene Zhuravlev
+ *         Date: 11/29/12
+ */
+public class JpsIntelliLangConfigurationImpl extends JpsElementBase<JpsIntelliLangConfigurationImpl> implements
+                                                                                             JpsIntelliLangConfiguration {
+  public static final JpsElementChildRole<JpsIntelliLangConfiguration> ROLE = JpsElementChildRoleBase.create("IntelliLang");
+
+  private String myPatternAnnotationClassName = "org.intellij.lang.annotations.Pattern";
+  private InstrumentationType myInstrumentationType = InstrumentationType.ASSERT;
+
+  @NotNull
+  @Override
+  public JpsIntelliLangConfigurationImpl createCopy() {
+    return new JpsIntelliLangConfigurationImpl();
+  }
+
+  @Override
+  public void applyChanges(@NotNull JpsIntelliLangConfigurationImpl modified) {
+    myPatternAnnotationClassName = modified.myPatternAnnotationClassName;
+    myInstrumentationType = modified.myInstrumentationType;
+  }
+
+  @Override
+  public String getPatternAnnotationClass() {
+    return myPatternAnnotationClassName;
+  }
+
+  @Override
+  public InstrumentationType getInstrumentationType() {
+    return myInstrumentationType;
+  }
+
+  public void setPatternAnnotationClassName(String patternAnnotationClassName) {
+    myPatternAnnotationClassName = patternAnnotationClassName;
+  }
+
+  public void setInstrumentationType(InstrumentationType instrumentationType) {
+    myInstrumentationType = instrumentationType;
+  }
+}
---
+++ plugins/IntelliLang/intellilang-jps-plugin/src/org/jetbrains/jps/intellilang/model/impl/JpsIntelliLangConfigurationSerializer.java
@@ -0,0 +1,56 @@
+package org.jetbrains.jps.intellilang.model.impl;
+
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.util.JDOMExternalizerUtil;
+import org.jdom.Element;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.jps.intellilang.instrumentation.InstrumentationType;
+import org.jetbrains.jps.intellilang.model.JpsIntelliLangExtensionService;
+import org.jetbrains.jps.model.JpsGlobal;
+import org.jetbrains.jps.model.serialization.JpsGlobalExtensionSerializer;
+
+/**
+ * @author Eugene Zhuravlev
+ *         Date: 11/29/12
+ */
+public class JpsIntelliLangConfigurationSerializer extends JpsGlobalExtensionSerializer {
+  private static final Logger LOG = Logger.getInstance("#org.jetbrains.jps.intellilang.model.impl.JpsIntelliLangConfigurationSerializer");
+  private static final String INSTRUMENTATION_TYPE_NAME = "INSTRUMENTATION";
+  private static final String PATTERN_ANNOTATION_NAME = "PATTERN_ANNOTATION";
+
+  public JpsIntelliLangConfigurationSerializer() {
+    super("IntelliLang.xml", "LanguageInjectionConfiguration");
+  }
+
+  @Override
+  public void loadExtension(@NotNull JpsGlobal global, @NotNull Element componentTag) {
+    final JpsIntelliLangConfigurationImpl configuration = new JpsIntelliLangConfigurationImpl();
+
+    final String annotationName = JDOMExternalizerUtil.readField(componentTag, PATTERN_ANNOTATION_NAME);
+    if (annotationName != null) {
+      configuration.setPatternAnnotationClassName(annotationName);
+    }
+
+    final String instrumentationType = JDOMExternalizerUtil.readField(componentTag, INSTRUMENTATION_TYPE_NAME);
+    if (instrumentationType != null) {
+      try {
+        final InstrumentationType type = InstrumentationType.valueOf(instrumentationType);
+        configuration.setInstrumentationType(type);
+      }
+      catch (IllegalArgumentException ignored) {
+        LOG.info(ignored);
+      }
+    }
+
+    JpsIntelliLangExtensionService.getInstance().setConfiguration(global, configuration);
+  }
+
+  @Override
+  public void loadExtensionWithDefaultSettings(@NotNull JpsGlobal global) {
+    JpsIntelliLangExtensionService.getInstance().setConfiguration(global, new JpsIntelliLangConfigurationImpl());
+  }
+
+  @Override
+  public void saveExtension(@NotNull JpsGlobal global, @NotNull Element componentTag) {
+  }
+}
