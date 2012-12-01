00194ca2d70bcc8a609dec2c298289d2438abae1
Gregory.Shrago <gregory.shrago@jetbrains.com>
2012-12-01 16:56:21 +0400

    IDEA-73925 injection in properties file

---
+++ plugins/properties/src/com/intellij/lang/properties/PropertyManipulator.java
@@ -0,0 +1,29 @@
+package com.intellij.lang.properties;
+
+import com.intellij.lang.ASTNode;
+import com.intellij.lang.properties.psi.impl.PropertyImpl;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.AbstractElementManipulator;
+import com.intellij.util.IncorrectOperationException;
+
+/**
+ * @author gregsh
+ */
+public class PropertyManipulator extends AbstractElementManipulator<PropertyImpl> {
+  @Override
+  public PropertyImpl handleContentChange(PropertyImpl element, TextRange range, String newContent) throws IncorrectOperationException {
+    TextRange valueRange = getRangeInElement(element);
+    final String oldText = element.getText();
+    String newText = oldText.substring(0, range.getStartOffset()) + newContent + oldText.substring(range.getEndOffset());
+    element.setValue(newText.substring(valueRange.getStartOffset()));
+    return element;
+  }
+
+  @Override
+  public TextRange getRangeInElement(PropertyImpl element) {
+    ASTNode valueNode = element.getValueNode();
+    if (valueNode == null) return TextRange.create(element.getTextLength(), 0);
+    TextRange range = valueNode.getTextRange();
+    return TextRange.from(range.getStartOffset() - element.getTextRange().getStartOffset(), range.getLength());
+  }
+}
