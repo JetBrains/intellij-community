deccad0a6e63036d0fabe572fd155759be17e778
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2012-12-03 17:07:43 +0400

    updated test data

--- python/testData/inspections/PyCompatibilityInspection/stringLiteralExpression.py
+++ python/testData/inspections/PyCompatibilityInspection/stringLiteralExpression.py
@@ -1,1 +1,1 @@
-a = <warning descr="Python version 3.0, 3.1 do not support a leading 'u' or 'U'.">u"String"</warning>
+a = <warning descr="Python version 3.0, 3.1, 3.2 do not support a leading 'u' or 'U'.">u"String"</warning>
