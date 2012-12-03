b5d235eaae1e1717439bf5207f186a4ba1896f9e
Konstantin Bulenkov <kb@jetbrains.com>
2012-12-03 01:53:49 +0100

    some fixes for major darcula combobox issues (no actions assigned, gaps between editor and border, etc) (cherry picked from commit bc23f142c03c7aefd376f1bb676dc99df16ddf7b)

--- platform/platform-impl/src/com/intellij/ui/ComboboxEditorTextField.java
+++ platform/platform-impl/src/com/intellij/ui/ComboboxEditorTextField.java
@@ -63,11 +63,14 @@ public class ComboboxEditorTextField extends EditorTextField {
   public ComboboxEditorTextField(Document document, Project project, FileType fileType, boolean isViewer) {
     super(document, project, fileType, isViewer);
     setOneLineMode(true);
+    if (UIUtil.isUnderDarcula()) { //todo[kb] make for all LaFs and color schemes ?
+      setBackground(UIUtil.getTextFieldBackground());
+    }
   }
 
   @Override
   protected boolean shouldHaveBorder() {
-    return UIManager.getBorder("ComboBox.border") == null;
+    return UIManager.getBorder("ComboBox.border") == null && !UIUtil.isUnderDarcula();
   }
 
   @Override
