141fff89465607f4fc128d970be088550b364697
erokhins <smevok@yandex.ru>
2012-12-03 14:28:35 +0400

    add test for CompressedList

---
+++ common/test/org/hanuna/gitalk/common/generatemodel/IntegerCompressedListTest.java
@@ -0,0 +1,99 @@
+package org.hanuna.gitalk.common.generatemodel;
+
+import org.hanuna.gitalk.common.generatemodel.generator.Generator;
+import org.jetbrains.annotations.NotNull;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+/**
+ * @author erokhins
+ */
+public class IntegerCompressedListTest {
+    private final int size = 200;
+    private final IntegerGenerator generator = new IntegerGenerator(size);
+    private final Integer first = 0;
+
+    private final Replace[] simple = {
+            new Replace(1, 5, 1),
+            new Replace(1, 5, 10),
+            new Replace(10, 15, 97)
+    };
+
+    private final Replace[] marginal = {
+            new Replace(size - 2, size - 1, 0),
+            new Replace(size - 2, size - 1, 100),
+            new Replace(0, 1, 0),
+            new Replace(0, 1, 100),
+            new Replace(10, 15, 97)
+    };
+
+    @Test
+    public void simpleNoCompressedList() {
+        runTests(new NoCompressedList<Integer>(generator, first, size), simple);
+    }
+
+    @Test
+    public void simpleRuntimeGenerateCompressedList() {
+        runTests(new RuntimeGenerateCompressedList<Integer>(generator, first, size), simple);
+    }
+
+
+    @Test
+    public void marginalNoCompressedList() {
+        runTests(new NoCompressedList<Integer>(generator, first, size), marginal);
+    }
+
+    @Test
+    public void marginalRuntimeGenerateCompressedList() {
+        runTests(new RuntimeGenerateCompressedList<Integer>(generator, first, size), marginal);
+    }
+
+
+    public void runTests(CompressedList<Integer> list, Replace[] replaces) {
+        RunCompressedListTest<Integer> runner = new RunCompressedListTest<Integer>(list, generator, first);
+        runner.assertList();
+
+        for (Replace replace : replaces) {
+            generator.replace(replace);
+            runner.runReplace(replace);
+        }
+    }
+
+
+    private class IntegerGenerator implements Generator<Integer> {
+        private final List<Integer> list;
+        private int replaceCount = 0;
+
+        public IntegerGenerator(int size) {
+            this.list = new ArrayList<Integer>(size);
+            for (int i = 0; i < size; i++) {
+                list.add(i);
+            }
+        }
+
+        @NotNull
+        @Override
+        public Integer generate(@NotNull Integer prev, int steps) {
+            int prevIndex = list.indexOf(prev);
+            assert prevIndex != -1;
+            if (prevIndex + steps >= list.size()) {
+                throw new NoSuchElementException();
+            }
+            return list.get(prevIndex + steps);
+        }
+
+        public void replace(Replace replace) {
+            replaceCount++;
+            int shift = replaceCount * 1000;
+            list.subList(replace.from() + 1, replace.to()).clear();
+            List<Integer> newLists = new ArrayList<Integer>(replace.addElementsCount());
+            for (int i = 0; i < replace.addElementsCount(); i++) {
+                newLists.add(shift + i);
+            }
+            list.addAll(replace.from() + 1, newLists);
+        }
+    }
+}
---
+++ common/test/org/hanuna/gitalk/common/generatemodel/RunCompressedListTest.java
@@ -0,0 +1,58 @@
+package org.hanuna.gitalk.common.generatemodel;
+
+import org.hanuna.gitalk.common.generatemodel.generator.Generator;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * @author erokhins
+ */
+public class RunCompressedListTest<T> {
+    private final CompressedList<T> compressedList;
+    private final Generator<T> generator;
+    private final T first;
+
+    public RunCompressedListTest(CompressedList<T> compressedList, Generator<T> generator, T first) {
+        this.compressedList = compressedList;
+        this.generator = generator;
+        this.first = first;
+    }
+
+    private String CompressedListStr() {
+            StringBuilder s = new StringBuilder();
+        for (int i = 0; i < compressedList.getList().size(); i++) {
+            T listElement = compressedList.getList().get(i);
+            s.append(listElement).append(" ");
+        }
+        return s.toString();
+    }
+
+    private String ActualListStr() {
+        StringBuilder s = new StringBuilder();
+        T t = first;
+        s.append(t).append(" ");
+        for (int i = 1; i < compressedList.getList().size(); i++) {
+            t = generator.generate(t, 1);
+            s.append(t).append(" ");
+        }
+        return s.toString();
+    }
+
+    public void assertList() {
+        assertList("");
+    }
+    public void assertList(String message) {
+        assertEquals(message, ActualListStr(), CompressedListStr());
+    }
+
+    public void runReplace(Replace replace) {
+        compressedList.recalculate(replace);
+        assertList(replaceToStr(replace));
+    }
+
+
+    public String replaceToStr(Replace replace) {
+        return "Replace: from: " + replace.from() + ", to: " + replace.to() + ", addElementsCounts: " + replace.addElementsCount();
+    }
+
+}
