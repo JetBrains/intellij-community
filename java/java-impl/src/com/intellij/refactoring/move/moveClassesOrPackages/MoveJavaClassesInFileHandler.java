d62ef4a4adc0257f106461ae5e8396c2eb4b86b9
Serega Vasiliev <sergey.vasiliev@jetbrains.com>
2012-12-04 16:25:07 +0100

    move drools class (IDEA-95505) (cherry-picked from 54dfa87)

---
+++ java/java-impl/src/com/intellij/refactoring/move/moveClassesOrPackages/MoveJavaClassesInFileHandler.java
@@ -0,0 +1,30 @@
+package com.intellij.refactoring.move.moveClassesOrPackages;
+
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiClassOwner;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.impl.light.LightClass;
+import com.intellij.util.ArrayUtil;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+
+public class MoveJavaClassesInFileHandler extends MoveAllClassesInFileHandler {
+
+  @Override
+  public void processMoveAllClassesInFile(@NotNull Map<PsiClass, Boolean> allClasses, PsiClass psiClass, PsiElement... elementsToMove) {
+    if (psiClass instanceof LightClass) return;
+    final PsiClassOwner containingFile = (PsiClassOwner)psiClass.getContainingFile();
+    final PsiClass[] classes = containingFile.getClasses();
+    boolean all = true;
+    for (PsiClass aClass : classes) {
+      if (ArrayUtil.find(elementsToMove, aClass) == -1) {
+        all = false;
+        break;
+      }
+    }
+    for (PsiClass aClass : classes) {
+      allClasses.put(aClass, all);
+    }
+  }
+}
