36ef3cb22fb19d384cd175c3fe8e03941c11c74b
irengrig <Irina.Chernushina@jetbrains.com>
2012-12-06 16:47:06 +0400

    IDEA-96501 Exception at opening project

--- platform/vcs-impl/src/com/intellij/openapi/vcs/FilePathImpl.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/FilePathImpl.java
@@ -46,12 +46,12 @@ public class FilePathImpl implements FilePath {
   private boolean myIsDirectory;
   private final boolean myLocal;
 
-  private FilePathImpl(VirtualFile virtualParent,
+  private FilePathImpl(@NotNull VirtualFile virtualParent,
                        @NotNull String name,
                        final boolean isDirectory,
                        VirtualFile child,
                        final boolean forDeleted) {
-    this(virtualParent == null ? new File(name) : new File(new File(virtualParent.getPath()), name), isDirectory, true);
+    this(new File(virtualParent.getPath(), name), isDirectory, true);
     myVirtualParent = virtualParent;
 
     if (!forDeleted) {
@@ -88,6 +88,7 @@ public class FilePathImpl implements FilePath {
   }
   private FilePathImpl(@NotNull File file, final boolean isDirectory, boolean local) {
     myFile = file;
+    assert myFile.isAbsolute();
     myName = file.getName();
     myIsDirectory = isDirectory;
     myLocal = local;
