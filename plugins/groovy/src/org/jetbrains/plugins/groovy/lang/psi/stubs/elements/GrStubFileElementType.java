11122cae8067b99218ec8dbb6242eed62fb4e6d9
Max Medvedev <maxim.medvedev@jetbrains.com>
2012-12-07 09:16:01 +0400

    fix NPE in Groovy stubs

--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/stubs/elements/GrStubFileElementType.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/stubs/elements/GrStubFileElementType.java
@@ -55,7 +55,7 @@ public class GrStubFileElementType extends IStubFileElementType<GrFileStub> {
 
   @Override
   public int getStubVersion() {
-    return super.getStubVersion() + 15;
+    return super.getStubVersion() + 16;
   }
 
   public String getExternalId() {
