9b9be3f623730ec0da449da0f4fa380ef74281ef
anna <Anna.Kozlova@jetbrains.com>
2012-12-10 12:50:22 +0100

    highlight this inside interface inner classes (IDEA-97032)

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/advHighlighting/ThisInInterface.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/advHighlighting/ThisInInterface.java
@@ -5,4 +5,20 @@ class C implements A {
 interface A {
     A a = new C(<error descr="Cannot find symbol variable this">this</error>);
     A a1 = new C(<error descr="Cannot find symbol variable this">this</error>){};
+
+    class B  {
+        A foo() {
+            return <error descr="Cannot find symbol variable this">A.this</error>;
+        }
+
+        B foo1() {
+            return B.this;
+        }
+
+        class D {
+            B f() {
+                return B.this;
+            }
+        }
+    }
 }
