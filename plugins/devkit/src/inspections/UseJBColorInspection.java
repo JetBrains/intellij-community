bbf0b8758f018b706b4aa1f8ced99825a2a82dfe
Konstantin Bulenkov <kb@jetbrains.com>
2012-12-11 12:33:37 +0100

    update inspection for constants

--- plugins/devkit/src/inspections/UseJBColorInspection.java
+++ plugins/devkit/src/inspections/UseJBColorInspection.java
@@ -26,6 +26,10 @@ import com.intellij.ui.JBColor;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.idea.devkit.inspections.quickfix.ConvertToJBColorConstantQuickFix;
+import org.jetbrains.idea.devkit.inspections.quickfix.ConvertToJBColorQuickFix;
+
+import java.awt.*;
 
 /**
  * @author Konstantin Bulenkov
@@ -40,6 +44,34 @@ public class UseJBColorInspection extends DevKitInspectionBase {
         final ProblemDescriptor descriptor = checkNewExpression(expression, holder.getManager(), isOnTheFly);
         if (descriptor != null) {
           holder.registerProblem(descriptor);
+        }
+        super.visitNewExpression(expression);
+      }
+
+      @Override
+      public void visitReferenceExpression(PsiReferenceExpression expression) {
+        super.visitReferenceExpression(expression);
+        final PsiElement colorField = expression.resolve();
+        if (colorField != null && colorField instanceof PsiField && ((PsiField)colorField).hasModifierProperty(PsiModifier.STATIC)) {
+          final PsiClass colorClass = ((PsiField)colorField).getContainingClass();
+          if (colorClass != null && Color.class.getName().equals(colorClass.getQualifiedName())) {
+            String text = expression.getText();
+            if (text.contains(".")) {
+              text = text.substring(text.lastIndexOf('.'));
+            }
+            if (text.startsWith(".")) {
+              text = text.substring(1);
+            }
+            if (text.equalsIgnoreCase("lightGray")) {
+              text = "LIGHT_GRAY";
+            } else if (text.equalsIgnoreCase("darkGray")) {
+              text = "DARK_GRAY";
+            }
+            final ProblemDescriptor descriptor = holder.getManager()
+              .createProblemDescriptor(expression, "Change to JBColor." + text.toUpperCase(), new ConvertToJBColorConstantQuickFix(text.toUpperCase()),
+                                       ProblemHighlightType.GENERIC_ERROR_OR_WARNING, isOnTheFly);
+            holder.registerProblem(descriptor);
+          }
         }
       }
     };
--- plugins/devkit/src/inspections/quickfix/ConvertToJBColorQuickFix.java
+++ plugins/devkit/src/inspections/quickfix/ConvertToJBColorQuickFix.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jetbrains.idea.devkit.inspections;
+package org.jetbrains.idea.devkit.inspections.quickfix;
 
 import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.ProblemDescriptor;
