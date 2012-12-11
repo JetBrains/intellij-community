66314ee25f6ec1d2835f4f2650a8c54b66a1f69a
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2012-12-11 17:11:48 +0100

    Minor optimization and cleanup

--- jps/model-impl/src/org/jetbrains/jps/model/java/impl/JavaSdkUtil.java
+++ jps/model-impl/src/org/jetbrains/jps/model/java/impl/JavaSdkUtil.java
@@ -1,12 +1,27 @@
+/*
+ * Copyright 2000-2012 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jetbrains.jps.model.java.impl;
 
 import com.intellij.openapi.util.SystemInfo;
+import com.intellij.openapi.util.io.FileSystemUtil;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.util.containers.ContainerUtil;
 
 import java.io.File;
 import java.io.FileFilter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -14,7 +29,7 @@ import java.util.Set;
  * @author nik
  */
 public class JavaSdkUtil {
-  public static List<File> getJdkClassesRoots(File file, boolean isJre) {
+  public static List<File> getJdkClassesRoots(File home, boolean isJre) {
     FileFilter jarFileFilter = new FileFilter() {
       @Override
       @SuppressWarnings({"HardCodedStringLiteral"})
@@ -24,60 +39,59 @@ public class JavaSdkUtil {
     };
 
     File[] jarDirs;
-    if (SystemInfo.isMac && !file.getName().startsWith("mockJDK")) {
-      final File openJdkRtJar = new File(new File(new File(file, "jre"), "lib"), "rt.jar");
+    if (SystemInfo.isMac && !home.getName().startsWith("mockJDK")) {
+      File openJdkRtJar = new File(home, "jre/lib/rt.jar");
       if (openJdkRtJar.exists() && !openJdkRtJar.isDirectory()) {
-        // OpenJDK
-        File libFile = new File(file, "lib");
-        File classesFile = openJdkRtJar.getParentFile();
-        File libExtFile = new File(openJdkRtJar.getParentFile(), "ext");
-        File libEndorsedFile = new File(libFile, "endorsed");
-        jarDirs = new File[]{libEndorsedFile, libFile, classesFile, libExtFile};
+        File libDir = new File(home, "lib");
+        File classesDir = openJdkRtJar.getParentFile();
+        File libExtDir = new File(openJdkRtJar.getParentFile(), "ext");
+        File libEndorsedDir = new File(libDir, "endorsed");
+        jarDirs = new File[]{libEndorsedDir, libDir, classesDir, libExtDir};
       }
       else {
-        File libFile = new File(file, "lib");
-        File classesFile = new File(file, "../Classes");
-        File libExtFile = new File(libFile, "ext");
-        File libEndorsedFile = new File(libFile, "endorsed");
-        jarDirs = new File[]{libEndorsedFile, libFile, classesFile, libExtFile};
+        File libDir = new File(home, "lib");
+        File classesDir = new File(home, "../Classes");
+        File libExtDir = new File(libDir, "ext");
+        File libEndorsedDir = new File(libDir, "endorsed");
+        jarDirs = new File[]{libEndorsedDir, libDir, classesDir, libExtDir};
       }
     }
     else {
-      File jreLibFile = isJre ? new File(file, "lib") : new File(new File(file, "jre"), "lib");
-      File jreLibExtFile = new File(jreLibFile, "ext");
-      File jreLibEndorsedFile = new File(jreLibFile, "endorsed");
-      jarDirs = new File[]{jreLibEndorsedFile, jreLibFile, jreLibExtFile};
+      File libDir = isJre ? new File(home, "lib") : new File(home, "jre/lib");
+      File libExtDir = new File(libDir, "ext");
+      File libEndorsedDir = new File(libDir, "endorsed");
+      jarDirs = new File[]{libEndorsedDir, libDir, libExtDir};
     }
 
-    Set<File> filter = new LinkedHashSet<File>();
-    List<File> rootFiles = new ArrayList<File>();
+    Set<String> pathFilter = ContainerUtil.newTroveSet(FileUtil.PATH_HASHING_STRATEGY);
+    List<File> rootFiles = ContainerUtil.newArrayList();
     for (File jarDir : jarDirs) {
       if (jarDir != null && jarDir.isDirectory()) {
         File[] jarFiles = jarDir.listFiles(jarFileFilter);
         for (File jarFile : jarFiles) {
-          final String jarFileName = jarFile.getName();
-          if (jarFileName.equals("alt-rt.jar") || jarFileName.equals("alt-string.jar")) continue;
-          try {
-            // File.getCanonicalFile() allows us to filter out duplicate (symbolically linked) jar files,
-            // commonly found in osx JDK distributions
-            if (filter.add(jarFile.getCanonicalFile())) rootFiles.add(jarFile);
+          String jarFileName = jarFile.getName();
+          if (jarFileName.equals("alt-rt.jar") || jarFileName.equals("alt-string.jar")) {
+            continue;  // filter out alternative implementations
           }
-          catch (IOException e) {
-            // Symbolic links may fail to resolve. Just skip those jars as we won't be able to find virtual file in this case anyway.
+          String canonicalPath = FileSystemUtil.resolveSymLink(jarFile);
+          if (canonicalPath == null || !pathFilter.add(canonicalPath)) {
+            continue;  // filter out duplicate (symbolically linked) .jar files commonly found in OS X JDK distributions
           }
+          rootFiles.add(jarFile);
         }
       }
     }
 
-    File classesZip = new File(new File(file, "lib"), "classes.zip");
+    File classesZip = new File(home, "lib/classes.zip");
     if (classesZip.isFile()) {
       rootFiles.add(classesZip);
     }
 
-    File classesDir = new File(file, "classes");
+    File classesDir = new File(home, "classes");
     if (rootFiles.isEmpty() && classesDir.isDirectory()) {
       rootFiles.add(classesDir);
     }
+
     return rootFiles;
   }
 }
