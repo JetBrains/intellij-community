304c754c113019edbb054a06d90b4223801fb113
erokhins <smevok@yandex.ru>
2012-12-11 23:06:11 +0400

    add existedCommitRefs

--- refs/src/org/hanuna/gitalk/refs/RefsModel.java
+++ refs/src/org/hanuna/gitalk/refs/RefsModel.java
@@ -1,5 +1,6 @@
 package org.hanuna.gitalk.refs;
 
+import org.hanuna.gitalk.commitmodel.Commit;
 import org.hanuna.gitalk.commitmodel.Hash;
 import org.hanuna.gitalk.common.ReadOnlyList;
 import org.jetbrains.annotations.NotNull;
@@ -14,6 +15,27 @@ import java.util.Set;
  * @author erokhins
  */
 public class RefsModel {
+    public static RefsModel existedCommitRefs(List<Ref> allRefs, List<Commit> commits) {
+        Set<Hash> refCommits = new HashSet<Hash>();
+        for (Ref ref : allRefs) {
+            refCommits.add(ref.getCommitHash());
+        }
+        Set<Hash> existedCommitsRefs = new HashSet<Hash>();
+        for (Commit commit : commits) {
+            if (refCommits.contains(commit.hash())) {
+                existedCommitsRefs.add(commit.hash());
+            }
+        }
+
+        List<Ref> existedRef = new ArrayList<Ref>();
+        for (Ref ref : allRefs) {
+            if (existedCommitsRefs.contains(ref.getCommitHash())) {
+                existedRef.add(ref);
+            }
+        }
+        return new RefsModel(existedRef);
+    }
+
     private final List<Ref> allRefs;
     private final Set<Hash> trackedHash = new HashSet<Hash>();
     private final List<Ref> localBranches = new ArrayList<Ref>();
@@ -33,7 +55,7 @@ public class RefsModel {
     }
 
     @Nullable
-    public Ref refInToCommit(@NotNull Hash hash) {
+    public Ref refToCommit(@NotNull Hash hash) {
         if (trackedHash.contains(hash)) {
             for (Ref ref : allRefs) {
                 if (ref.getCommitHash().equals(hash)) {
