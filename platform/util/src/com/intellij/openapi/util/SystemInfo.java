1f21d0b8726af89dd1ec302b1b9b135a6cab3c69
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2012-12-12 17:42:48 +0100

    OS version comparison fixed

--- platform/util/src/com/intellij/openapi/util/SystemInfo.java
+++ platform/util/src/com/intellij/openapi/util/SystemInfo.java
@@ -41,7 +41,7 @@ public class SystemInfo extends SystemInfoRt {
   public static final boolean isUnix = SystemInfoRt.isUnix;
 
   public static boolean isOsVersionAtLeast(@NotNull String version) {
-    return StringUtil.compareVersionNumbers(version, OS_VERSION) >= 0;
+    return StringUtil.compareVersionNumbers(OS_VERSION, version) >= 0;
   }
 
   // version numbers from http://msdn.microsoft.com/en-us/library/windows/desktop/ms724832.aspx
