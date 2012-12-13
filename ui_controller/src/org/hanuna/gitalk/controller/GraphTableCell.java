b9b99f64d1b11af28b34835d7b7d8de3397281e5
erokhins <smevok@yandex.ru>
2012-12-13 10:38:14 +0400

    add method GraphTableCell.getRefsToThisCommit()

--- ui_controller/src/org/hanuna/gitalk/controller/GraphTableCell.java
+++ ui_controller/src/org/hanuna/gitalk/controller/GraphTableCell.java
@@ -33,4 +33,8 @@ public class GraphTableCell {
     public String getText() {
         return text;
     }
+
+    public ReadOnlyList<Ref> getRefsToThisCommit() {
+        return refsToThisCommit;
+    }
 }
