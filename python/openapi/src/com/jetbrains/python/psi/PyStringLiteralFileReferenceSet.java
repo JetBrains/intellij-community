41d2352cf8aba5c26243dafa35d9d6fb324cfc0e
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2012-12-14 16:50:39 +0100

    Fixed broken highlighting for file path references ended by \\ (PY-7434).

--- python/openapi/src/com/jetbrains/python/psi/PyStringLiteralFileReferenceSet.java
+++ python/openapi/src/com/jetbrains/python/psi/PyStringLiteralFileReferenceSet.java
@@ -69,9 +69,7 @@ public class PyStringLiteralFileReferenceSet extends RootFileReferenceSet {
     @Override
     public boolean process(int startOffset, int endOffset, String value) {
       if ("\\".equals(value) || "/".equals(value)) {
-        if (myStartOffset != -1) {
-          addReference(startOffset);
-        }
+        addReference(startOffset);
       }
       else {
         if (myStartOffset == -1) {
@@ -84,14 +82,17 @@ public class PyStringLiteralFileReferenceSet extends RootFileReferenceSet {
     }
 
     private void addReference(int startOffset) {
-      final FileReference ref = myFileReferenceSet.createFileReference(
-        new TextRange(myStartOffset, startOffset),
-        myIndex++,
-        myItem.toString());
-      myReferenceList.add(ref);
-      myStartOffset = -1;
-      myItem.setLength(0);
+      if (myStartOffset != -1) {
+        final FileReference ref = myFileReferenceSet.createFileReference(
+          new TextRange(myStartOffset, startOffset),
+          myIndex++,
+          myItem.toString());
+        myReferenceList.add(ref);
+        myStartOffset = -1;
+        myItem.setLength(0);
+      }
     }
+
 
     public void finish() {
       addReference(myEndOffset);
