5efcefd49b203dafacbc3e103f5af0bdd908a035
Konstantin Bulenkov <kb@jetbrains.com>
2012-12-14 13:53:43 +0100

    set Darcula LnF if user sets Darcula color scheme

--- platform/lang-impl/src/com/intellij/application/options/colors/ColorAndFontOptions.java
+++ platform/lang-impl/src/com/intellij/application/options/colors/ColorAndFontOptions.java
@@ -21,6 +21,10 @@ import com.intellij.application.options.editor.EditorOptionsProvider;
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer;
 import com.intellij.codeInsight.daemon.impl.DaemonCodeAnalyzerImpl;
 import com.intellij.execution.impl.ConsoleViewUtil;
+import com.intellij.ide.ui.LafManager;
+import com.intellij.ide.ui.laf.darcula.DarculaInstaller;
+import com.intellij.ide.ui.laf.darcula.DarculaLaf;
+import com.intellij.ide.ui.laf.darcula.DarculaLookAndFeelInfo;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ApplicationBundle;
 import com.intellij.openapi.diagnostic.Logger;
@@ -45,6 +49,7 @@ import com.intellij.openapi.options.SearchableConfigurable;
 import com.intellij.openapi.options.colors.*;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.Pair;
@@ -59,6 +64,7 @@ import com.intellij.psi.search.scope.packageSet.PackageSet;
 import com.intellij.util.ArrayUtil;
 import com.intellij.util.containers.HashMap;
 import com.intellij.util.diff.FilesTooBigForDiffException;
+import com.intellij.util.ui.UIUtil;
 import gnu.trove.THashSet;
 import gnu.trove.TObjectHashingStrategy;
 import org.jetbrains.annotations.Nls;
@@ -188,7 +194,7 @@ public class ColorAndFontOptions extends SearchableConfigurable.Parent.Abstract 
     newScheme.setIsNew();
 
     mySchemes.put(name, newScheme);
-    selectScheme(newScheme.getName());    
+    selectScheme(newScheme.getName());
     resetSchemesCombo(null);
   }
 
@@ -236,7 +242,16 @@ public class ColorAndFontOptions extends SearchableConfigurable.Parent.Abstract 
 
         EditorColorsScheme originalScheme = mySelectedScheme.getOriginalScheme();
         myColorsManager.setGlobalScheme(originalScheme);
-
+        if (originalScheme != null && DarculaLaf.NAME.equals(originalScheme.getName()) && !UIUtil.isUnderDarcula()) {
+          int ok = Messages.showYesNoDialog(
+            "Darcula color scheme has been set for editors. Would you like to set Darcula as default Look and Feel?",
+            "Darcula Look and Feel",
+            Messages.getQuestionIcon());
+          if (ok == Messages.OK) {
+            LafManager.getInstance().setCurrentLookAndFeel(new DarculaLookAndFeelInfo());
+            DarculaInstaller.install();
+          }
+        }
         applyChangesToEditors();
 
         reset();
@@ -299,7 +314,7 @@ public class ColorAndFontOptions extends SearchableConfigurable.Parent.Abstract 
 
   @Override
   public Configurable[] buildConfigurables() {
-    myDisposeCompleted = false;    
+    myDisposeCompleted = false;
     initAll();
 
     List<ColorAndFontPanelFactory> panelFactories = createPanelFactories();
@@ -376,7 +391,7 @@ public class ColorAndFontOptions extends SearchableConfigurable.Parent.Abstract 
       }
     });
     result.addAll(extensions);
-    
+
     result.add(new DiffColorsPageFactory());
     result.add(new FileStatusColorsPageFactory());
     result.add(new ScopeColorsPageFactory());
@@ -524,7 +539,7 @@ public class ColorAndFontOptions extends SearchableConfigurable.Parent.Abstract 
   private static void initDiffDescriptors(ArrayList<EditorSchemeAttributeDescriptor> descriptions, MyColorScheme scheme) {
     DiffOptionsPanel.addSchemeDescriptions(descriptions, scheme);
   }
-                               
+
   private static void initFileStatusDescriptors(ArrayList<EditorSchemeAttributeDescriptor> descriptions, MyColorScheme scheme) {
 
     FileStatus[] statuses = FileStatusFactory.getInstance().getAllFileStatuses();
@@ -625,7 +640,7 @@ public class ColorAndFontOptions extends SearchableConfigurable.Parent.Abstract 
     if (isSchemeListModified() || isSomeSchemeModified()) {
       myRevertChangesCompleted = false;
     }
-    
+
     if (!myRevertChangesCompleted) {
       ensureSchemesPanel();
 
@@ -728,7 +743,7 @@ public class ColorAndFontOptions extends SearchableConfigurable.Parent.Abstract 
       myRevertChangesCompleted = false;
 
       myApplyCompleted = false;
-      myRootSchemesPanel = null;      
+      myRootSchemesPanel = null;
     }
   }
 
