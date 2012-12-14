fef0eeaf13e03753bd8c8d22e9497b0d35d43fee
Max Medvedev <maxim.medvedev@jetbrains.com>
2012-12-14 16:23:35 +0400

    IDEA-97548 NPE while change signature of groovy constructor

--- platform/lang-impl/src/com/intellij/refactoring/changeSignature/ChangeSignatureDialogBase.java
+++ platform/lang-impl/src/com/intellij/refactoring/changeSignature/ChangeSignatureDialogBase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2011 JetBrains s.r.o.
+ * Copyright 2000-2012 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -100,6 +100,8 @@ public abstract class ChangeSignatureDialogBase<
   private final Alarm myUpdateSignatureAlarm = new Alarm(Alarm.ThreadToUse.SWING_THREAD);
 
   protected VisibilityPanelBase<Visibility> myVisibilityPanel;
+
+  @Nullable
   protected PsiCodeFragment myReturnTypeCodeFragment;
   private DelegationPanel myDelegationPanel;
   protected AnActionButton myPropagateParamChangesButton;
