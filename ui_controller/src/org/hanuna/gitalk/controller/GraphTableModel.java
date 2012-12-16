301986fc7d735f2309fc68a1d64dfe7b5c7e7cd4
erokhins <smevok@yandex.ru>
2012-12-16 22:37:20 +0400

    separate UI_Controller & GraphTableModel

---
+++ ui_controller/src/org/hanuna/gitalk/controller/GraphTableModel.java
@@ -0,0 +1,111 @@
+package org.hanuna.gitalk.controller;
+
+import org.hanuna.gitalk.commitmodel.Commit;
+import org.hanuna.gitalk.commitmodel.CommitData;
+import org.hanuna.gitalk.common.ReadOnlyList;
+import org.hanuna.gitalk.graph.Graph;
+import org.hanuna.gitalk.graph.graph_elements.GraphElement;
+import org.hanuna.gitalk.graph.graph_elements.Node;
+import org.hanuna.gitalk.printmodel.PrintCellModel;
+import org.hanuna.gitalk.printmodel.SpecialCell;
+import org.hanuna.gitalk.refs.Ref;
+import org.hanuna.gitalk.refs.RefsModel;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.table.AbstractTableModel;
+
+/**
+* @author erokhins
+*/
+class GraphTableModel extends AbstractTableModel {
+    private final String[] columnNames = {"Subject", "Author", "Date"};
+    private final Graph graph;
+    private final RefsModel refsModel;
+    private final PrintCellModel printCellModel;
+
+    public GraphTableModel(Graph graph, RefsModel refsModel, PrintCellModel printCellModel) {
+        this.graph = graph;
+        this.refsModel = refsModel;
+        this.printCellModel = printCellModel;
+    }
+
+
+    @Nullable
+    private Commit getCommitInRow(int rowIndex) {
+        ReadOnlyList<SpecialCell> cells = printCellModel.getPrintCellRow(rowIndex).getSpecialCell();
+        for (SpecialCell cell : cells) {
+            if (cell.getType() == SpecialCell.Type.COMMIT_NODE) {
+                GraphElement element = cell.getGraphElement();
+                Node node =  element.getNode();
+                assert node != null;
+                return node.getCommit();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int getRowCount() {
+        return graph.getNodeRows().size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 3;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Commit commit = getCommitInRow(rowIndex);
+        CommitData data;
+        if (commit == null) {
+            data = null;
+        } else {
+            data = commit.getData();
+            assert data != null;
+        }
+        switch (columnIndex) {
+            case 0:
+                String message = "";
+                ReadOnlyList<Ref> refs = ReadOnlyList.emptyList();
+                if (data != null) {
+                    message = data.getMessage();
+                    refs = refsModel.refsToCommit(commit.hash());
+                }
+                return new GraphTableCell(printCellModel.getPrintCellRow(rowIndex), message, refs);
+            case 1:
+                if (data == null) {
+                    return "";
+                } else {
+                    return data.getAuthor();
+                }
+            case 2:
+                if (data == null) {
+                    return "";
+                } else {
+                    return DateConverter.getStringOfDate(data.getTimeStamp());
+                }
+            default:
+                throw new IllegalArgumentException("columnIndex > 2");
+        }
+    }
+
+    @Override
+    public Class<?> getColumnClass(int column) {
+        switch (column) {
+            case 0:
+                return GraphTableCell.class;
+            case 1:
+                return String.class;
+            case 2:
+                return String.class;
+            default:
+                throw new IllegalArgumentException("column > 2");
+        }
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return columnNames[column];
+    }
+}
--- ui_controller/src/org/hanuna/gitalk/controller/UI_Controller.java
+++ ui_controller/src/org/hanuna/gitalk/controller/UI_Controller.java
@@ -1,24 +1,17 @@
 package org.hanuna.gitalk.controller;
 
-import org.hanuna.gitalk.commitmodel.Commit;
-import org.hanuna.gitalk.commitmodel.CommitData;
-import org.hanuna.gitalk.common.ReadOnlyList;
 import org.hanuna.gitalk.common.compressedlist.Replace;
 import org.hanuna.gitalk.graph.Graph;
 import org.hanuna.gitalk.graph.GraphFragment;
 import org.hanuna.gitalk.graph.graph_elements.GraphElement;
-import org.hanuna.gitalk.graph.graph_elements.Node;
 import org.hanuna.gitalk.graph.mutable_graph.graph_fragment_controller.GraphFragmentController;
 import org.hanuna.gitalk.printmodel.PrintCell;
 import org.hanuna.gitalk.printmodel.PrintCellModel;
 import org.hanuna.gitalk.printmodel.SelectController;
-import org.hanuna.gitalk.printmodel.SpecialCell;
-import org.hanuna.gitalk.refs.Ref;
 import org.hanuna.gitalk.refs.RefsModel;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableModel;
 
 import static org.hanuna.gitalk.controller.EventsController.ControllerListener;
@@ -27,40 +20,26 @@ import static org.hanuna.gitalk.controller.EventsController.ControllerListener;
  * @author erokhins
  */
 public class UI_Controller {
-    private final Graph graph;
     private final SelectController selectController;
     private final PrintCellModel printCellModel;
     private final GraphFragmentController fragmentController;
-    private final RefsModel refsModel;
     private final EventsController events = new EventsController();
+    private final GraphTableModel graphTableModel;
 
     private GraphElement prevGraphElement = null;
 
     public UI_Controller(Graph graph, RefsModel refsModel) {
-        this.graph = graph;
         this.fragmentController = graph.getFragmentController();
-        this.refsModel = refsModel;
         this.printCellModel = new PrintCellModel(graph);
         this.selectController = printCellModel.getSelectController();
+
+        this.graphTableModel = new GraphTableModel(graph, refsModel, printCellModel);
     }
 
-    public TableModel getTableModel() {
-        return new GitAlkTableModel();
+    public TableModel getGraphTableModel() {
+        return graphTableModel;
     }
 
-    @Nullable
-    private Commit getCommitInRow(int rowIndex) {
-        ReadOnlyList<SpecialCell> cells = printCellModel.getPrintCellRow(rowIndex).getSpecialCell();
-        for (SpecialCell cell : cells) {
-            if (cell.getType() == SpecialCell.Type.COMMIT_NODE) {
-                GraphElement element = cell.getGraphElement();
-                Node node =  element.getNode();
-                assert node != null;
-                return node.getCommit();
-            }
-        }
-        return null;
-    }
 
     public PrintCell getGraphPrintCell(int rowIndex) {
         return printCellModel.getPrintCellRow(rowIndex);
@@ -106,72 +85,4 @@ public class UI_Controller {
         events.runJumpToRow(replace.from());
     }
 
-    private class GitAlkTableModel extends AbstractTableModel {
-        private final String[] columnNames = {"Subject", "Author", "Date"};
-
-        @Override
-        public int getRowCount() {
-            return graph.getNodeRows().size();
-        }
-
-        @Override
-        public int getColumnCount() {
-            return 3;
-        }
-
-        @Override
-        public Object getValueAt(int rowIndex, int columnIndex) {
-            Commit commit = getCommitInRow(rowIndex);
-            CommitData data;
-            if (commit == null) {
-                data = null;
-            } else {
-                data = commit.getData();
-                assert data != null;
-            }
-            switch (columnIndex) {
-                case 0:
-                    String message = "";
-                    ReadOnlyList<Ref> refs = ReadOnlyList.emptyList();
-                    if (data != null) {
-                        message = data.getMessage();
-                        refs = refsModel.refsToCommit(commit.hash());
-                    }
-                    return new GraphTableCell(printCellModel.getPrintCellRow(rowIndex), message, refs);
-                case 1:
-                    if (data == null) {
-                        return "";
-                    } else {
-                        return data.getAuthor();
-                    }
-                case 2:
-                    if (data == null) {
-                        return "";
-                    } else {
-                        return DateConverter.getStringOfDate(data.getTimeStamp());
-                    }
-                default:
-                    throw new IllegalArgumentException("columnIndex > 2");
-            }
-        }
-
-        @Override
-        public Class<?> getColumnClass(int column) {
-            switch (column) {
-                case 0:
-                    return GraphTableCell.class;
-                case 1:
-                    return String.class;
-                case 2:
-                    return String.class;
-                default:
-                    throw new IllegalArgumentException("column > 2");
-            }
-        }
-
-        @Override
-        public String getColumnName(int column) {
-            return columnNames[column];
-        }
-    }
 }
