cddd46b18d1f87485e1de3eab53bfd60f9662392
erokhins <smevok@yandex.ru>
2012-12-19 21:54:36 +0400

    create first jar file

---
+++ swing_ui/src/org/hanuna/gitalk/MyTest.java
@@ -0,0 +1,20 @@
+package org.hanuna.gitalk;
+
+import org.hanuna.gitalk.controller.Controller;
+import org.hanuna.gitalk.ui_controller.UI_Controller;
+import org.hanuna.gitalk.swing_ui.GitAlkUI;
+
+import java.io.IOException;
+
+/**
+ * @author erokhins
+ */
+public class MyTest {
+    public static void main(String[] args) throws IOException {
+        Controller controller = new Controller();
+        controller.prepare();
+        UI_Controller UIController = new UI_Controller(controller);
+        GitAlkUI ui = new GitAlkUI(UIController);
+        ui.showUi();
+    }
+}
