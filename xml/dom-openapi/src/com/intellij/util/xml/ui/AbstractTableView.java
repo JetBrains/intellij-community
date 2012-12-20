dccf212f0fef474874db94ee6f4d1b611906813c
peter <peter@jetbrains.com>
2012-12-20 16:51:59 +0100

    fix inability to add the first dom element to the collection control, and duplicate "Edit deployment descriptor" actions (IDEA-97589)
    (cherry-picked from db2c916)

--- xml/dom-openapi/src/com/intellij/util/xml/ui/AbstractTableView.java
+++ xml/dom-openapi/src/com/intellij/util/xml/ui/AbstractTableView.java
@@ -128,7 +128,7 @@ public abstract class AbstractTableView<T> extends JPanel implements TypeSafeDat
     final ActionManager actionManager = ActionManager.getInstance();
     final ToolbarPosition position = getToolbarPosition();
     final ActionToolbar myActionToolbar = actionManager.createActionToolbar(ActionPlaces.PROJECT_VIEW_TOOLBAR, actionGroup, position == ToolbarPosition.TOP || position == ToolbarPosition.BOTTOM);
-    myActionToolbar.setTargetComponent(myTable);
+    myActionToolbar.setTargetComponent(myInnerPanel);
     final JComponent toolbarComponent = myActionToolbar.getComponent();
     final MatteBorder matteBorder = BorderFactory.createMatteBorder(0, 0, position == ToolbarPosition.TOP ? 1 : 0, 0, JBColor.DARK_GRAY);
     toolbarComponent.setBorder(BorderFactory.createCompoundBorder(matteBorder, toolbarComponent.getBorder()));
