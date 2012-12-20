27c2364d0c9cea7d6b8a269f7dc9924af69911ce
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2012-12-20 12:33:03 +0100

    EA-42210 (canonicalize paths before comparing)

--- platform/vcs-impl/src/com/intellij/openapi/vcs/impl/projectlevelman/FileWatchRequestModifier.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/impl/projectlevelman/FileWatchRequestModifier.java
@@ -17,16 +17,21 @@ package com.intellij.openapi.vcs.impl.projectlevelman;
 
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vcs.VcsDirectoryMapping;
 import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.util.containers.ContainerUtil;
 
 import java.util.*;
+
+import static com.intellij.util.containers.ContainerUtil.newHashMap;
+import static com.intellij.util.containers.ContainerUtil.newLinkedList;
 
 /**
 * @author irengrig
 */
 public class FileWatchRequestModifier implements Runnable {
-  private static final Logger LOG = Logger.getInstance("#com.intellij.openapi.vcs.impl.projectlevelman.FileWatchRequestModifier");
+  private static final Logger LOG = Logger.getInstance(FileWatchRequestModifier.class);
 
   private final Project myProject;
   private final NewMappings myNewMappings;
@@ -37,28 +42,28 @@ public class FileWatchRequestModifier implements Runnable {
     myLfs = localFileSystem;
     myProject = project;
     myNewMappings = newMappings;
-    myDirectoryMappingWatches = new HashMap<VcsDirectoryMapping, LocalFileSystem.WatchRequest>();
+    myDirectoryMappingWatches = newHashMap();
   }
 
   @Override
   public void run() {
-    if ((! myProject.isInitialized()) || myProject.isDisposed()) return;
+    if (!myProject.isInitialized() || myProject.isDisposed()) return;
     final List<VcsDirectoryMapping> copy = myNewMappings.getDirectoryMappings();
 
-    final List<VcsDirectoryMapping> added = new LinkedList<VcsDirectoryMapping>(copy);
+    final List<VcsDirectoryMapping> added = newLinkedList(copy);
     added.removeAll(myDirectoryMappingWatches.keySet());
 
-    final List<VcsDirectoryMapping> deleted = new LinkedList<VcsDirectoryMapping>(myDirectoryMappingWatches.keySet());
+    final List<VcsDirectoryMapping> deleted = newLinkedList(myDirectoryMappingWatches.keySet());
     deleted.removeAll(copy);
 
-    final Map<String, VcsDirectoryMapping> toAdd = new HashMap<String, VcsDirectoryMapping>();
+    final Map<String, VcsDirectoryMapping> toAdd = ContainerUtil.newTroveMap(FileUtil.PATH_HASHING_STRATEGY);
     for (VcsDirectoryMapping mapping : added) {
       if (!mapping.isDefaultMapping()) {
-        toAdd.put(mapping.getDirectory(), mapping);
+        toAdd.put(FileUtil.toCanonicalPath(mapping.getDirectory()), mapping);
       }
     }
 
-    final Collection<LocalFileSystem.WatchRequest> toRemove = new ArrayList<LocalFileSystem.WatchRequest>();
+    final Collection<LocalFileSystem.WatchRequest> toRemove = newLinkedList();
     for (VcsDirectoryMapping mapping : deleted) {
       if (mapping.isDefaultMapping()) continue;
       final LocalFileSystem.WatchRequest removed = myDirectoryMappingWatches.remove(mapping);
