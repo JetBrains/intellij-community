822b4f0e1a9f3dc271e3ce07edda7d6f07942dd2
erokhins <smevok@yandex.ru>
2012-12-26 20:16:44 +0400

    add ErrorFrame

---
+++ swing_ui/src/org/hanuna/gitalk/swing_ui/ErrorFrame.java
@@ -0,0 +1,31 @@
+package org.hanuna.gitalk.swing_ui;
+
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author erokhins
+ */
+public class ErrorFrame extends JFrame {
+
+    public ErrorFrame(@NotNull String message) throws HeadlessException {
+        super("fatal error");
+        prepare(message);
+        setVisible(true);
+    }
+
+    private void prepare(@NotNull String message) {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        JLabel label = new JLabel(message);
+        setContentPane(label);
+        pack();
+        setModalExclusionType(Dialog.ModalExclusionType.NO_EXCLUDE);
+        setMinimumSize(new Dimension(200, 50));
+        pack();
+
+        UI_Utilities.setCenterLocation(this);
+    }
+
+}
