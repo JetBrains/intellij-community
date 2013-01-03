cbc61c0f1334decba1a47f087d645eecc087efe5
Konstantin Bulenkov <kb@jetbrains.com>
2013-01-03 12:28:05 +0100

    Finder like sorting + IDEA-80435 Project file sorting

--- platform/platform-api/src/com/intellij/ide/util/treeView/AlphaComparator.java
+++ platform/platform-api/src/com/intellij/ide/util/treeView/AlphaComparator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
  */
 package com.intellij.ide.util.treeView;
 
+import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.text.StringUtil;
 
 import java.util.Comparator;
@@ -37,6 +38,30 @@ public class AlphaComparator implements Comparator<NodeDescriptor>{
     if (s1 == null) return s2 == null ? 0 : -1;
     if (s2 == null) return +1;
 
-    return StringUtil.naturalCompare(s1, s2);
+    //for super natural comparison (IDEA-80435)
+    Pair<String,String> normalized = normalize(s1, s2);
+    return StringUtil.naturalCompare(normalized.first, normalized.second);
+  }
+
+  private static Pair<String, String> normalize(String s1, String s2) {
+    final int minLen = Math.min(s1.length(), s2.length());
+    final StringBuilder sb1 = new StringBuilder(s1);
+    final StringBuilder sb2 = new StringBuilder(s2);
+    for (int i = 0; i < minLen; i++) {
+      final char ch1 = s1.charAt(i);
+      final char ch2 = sb2.charAt(i);
+      if (ch1 == ch2 && ch1 == '-') {
+        sb1.setCharAt(i, '_');
+        sb2.setCharAt(i, '_');
+      } else if (ch1 == '-' && ch2 != '_') {
+        sb1.setCharAt(i, '_');
+      } else if (ch2 == '-' && ch1 != '_') {
+        sb2.setCharAt(i, '_');
+      }
+    }
+
+    s1 = sb1.toString();
+    s2 = sb2.toString();
+    return Pair.create(s1, s2);
   }
 }
