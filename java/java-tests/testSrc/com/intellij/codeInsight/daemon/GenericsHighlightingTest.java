d1893766c49e309b07fa65f71a4803b707b9b4cf
anna <Anna.Kozlova@jetbrains.com>
2013-01-07 19:01:29 +0100

    type params: error for jdk < 1.7 (IDEA-67861)

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/GenericsHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/GenericsHighlightingTest.java
@@ -195,6 +195,7 @@ public class GenericsHighlightingTest extends LightDaemonAnalyzerTestCase {
   public void testIDEA98421() { doTest5(false); }
   public void testErasureTypeParameterBound() { doTest5(false); }
   public void testThisAsAccessObject() { doTest5(false); }
+  public void testIDEA67861() { doTest7Incompatibility(false); }
 
   public void testJavaUtilCollections_NoVerify() throws Exception {
     PsiClass collectionsClass = getJavaFacade().findClass("java.util.Collections", GlobalSearchScope.moduleWithLibrariesScope(getModule()));
