5055466ac68bc34a8da4bf4c863ca51cf61912b4
anna <Anna.Kozlova@jetbrains.com>
2013-01-07 19:08:21 +0100

    testdata for IDEA-67597

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/GenericsHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/GenericsHighlightingTest.java
@@ -196,6 +196,7 @@ public class GenericsHighlightingTest extends LightDaemonAnalyzerTestCase {
   public void testErasureTypeParameterBound() { doTest5(false); }
   public void testThisAsAccessObject() { doTest5(false); }
   public void testIDEA67861() { doTest7Incompatibility(false); }
+  public void testIDEA67597() { doTest7Incompatibility(false); }
 
   public void testJavaUtilCollections_NoVerify() throws Exception {
     PsiClass collectionsClass = getJavaFacade().findClass("java.util.Collections", GlobalSearchScope.moduleWithLibrariesScope(getModule()));
