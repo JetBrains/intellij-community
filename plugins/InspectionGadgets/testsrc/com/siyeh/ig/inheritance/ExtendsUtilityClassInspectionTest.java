cdbf46f4a15f68946f5b47c07966e839aaa94bf2
Bas Leijdekkers <basleijdekkers@gmail.com>
2013-01-10 15:15:43 +0100

    IDEA-98682 (Class extends utility class does not ignore on empty child classes)

---
+++ plugins/InspectionGadgets/testsrc/com/siyeh/ig/inheritance/ExtendsUtilityClassInspectionTest.java
@@ -0,0 +1,12 @@
+package com.siyeh.ig.inheritance;
+
+import com.siyeh.ig.IGInspectionTestCase;
+
+public class ExtendsUtilityClassInspectionTest extends IGInspectionTestCase {
+
+  public void test() throws Exception {
+    final ExtendsUtilityClassInspection tool = new ExtendsUtilityClassInspection();
+    tool.ignoreUtilityClasses = true;
+    doTest("com/siyeh/igtest/inheritance/extends_utility_class", tool);
+  }
+}
