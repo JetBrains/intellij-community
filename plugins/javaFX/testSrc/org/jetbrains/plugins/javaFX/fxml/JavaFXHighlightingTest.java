9a92ffa6ae65b067d29bd8861288ff813c52aee0
anna <Anna.Kozlova@jetbrains.com>
2013-01-11 12:48:48 +0100

    remove jfxrt.jar from mock

--- plugins/javaFX/testSrc/org/jetbrains/plugins/javaFX/fxml/JavaFXHighlightingTest.java
+++ plugins/javaFX/testSrc/org/jetbrains/plugins/javaFX/fxml/JavaFXHighlightingTest.java
@@ -1,20 +1,27 @@
 package org.jetbrains.plugins.javaFX.fxml;
 
-import com.intellij.codeInsight.daemon.LightDaemonAnalyzerTestCase;
+import com.intellij.codeInsight.daemon.DaemonAnalyzerTestCase;
 import com.intellij.openapi.application.PluginPathManager;
+import com.intellij.testFramework.PsiTestUtil;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * User: anna
  * Date: 1/10/13
  */
-public class JavaFXHighlightingTest extends LightDaemonAnalyzerTestCase {
+public class JavaFXHighlightingTest extends DaemonAnalyzerTestCase {
+  @Override
+  protected void setUpModule() {
+    super.setUpModule();
+    PsiTestUtil.addLibrary(getModule(), "javafx", PluginPathManager.getPluginHomePath("javaFX") + "/testData", "jfxrt.jar");
+  }
+
   public void testLoginForm() throws Exception {
     doTest();
   }
 
-  private void doTest() {
-    doTest(getTestName(true) + ".fxml", false, false);
+  private void doTest() throws Exception {
+    doTest(false, false, getTestName(true) + ".fxml");
   }
 
   @NotNull
