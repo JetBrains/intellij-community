225296c67fbf0f8acdbcf934bc9859374bf98336
anna <Anna.Kozlova@jetbrains.com>
2013-01-15 11:39:40 +0100

    treat Object[] (IDEA-99061)

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/GenericsHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/GenericsHighlightingTest.java
@@ -201,6 +201,7 @@ public class GenericsHighlightingTest extends LightDaemonAnalyzerTestCase {
   public void testIDEA67597() { doTest5(false); }
   public void testIDEA57539() { doTest5(false); }
   public void testIDEA67570() { doTest5(false); }
+  public void testIDEA99061() { doTest5(false); }
 
   public void testJavaUtilCollections_NoVerify() throws Exception {
     PsiClass collectionsClass = getJavaFacade().findClass("java.util.Collections", GlobalSearchScope.moduleWithLibrariesScope(getModule()));
