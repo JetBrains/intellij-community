7e3d95b8e56012a8166e2814124104e2c53e8f77
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2013-01-15 15:22:05 +0100

    Excessive logging fixed

--- platform/platform-impl/src/com/intellij/idea/StartupUtil.java
+++ platform/platform-impl/src/com/intellij/idea/StartupUtil.java
@@ -221,7 +221,7 @@ public class StartupUtil {
         log.info("Using \"FocusKiller\" library to prevent focus stealing.");
       }
       catch (Throwable t) {
-        log.warn("\"FocusKiller\" library not found or there were problems loading it.", t);
+        log.info("\"FocusKiller\" library not found or there were problems loading it.", t);
       }
     }
   }
