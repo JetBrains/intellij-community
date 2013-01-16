9aee1026cc7f922dcf3013e7d664a340cee15efe
Max Medvedev <maxim.medvedev@jetbrains.com>
2013-01-16 16:20:54 +0400

    fix completion testdata

--- plugins/groovy/test/org/jetbrains/plugins/groovy/completion/GroovyCompletionTest.groovy
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/completion/GroovyCompletionTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -102,7 +102,7 @@ public class GroovyCompletionTest extends GroovyCompletionTestBase {
   }
 
   public void testNamedParametersForConstructorCall() {
-    doVariantableTest("hahaha", "hohoho", "hashCode");
+    doVariantableTest("hahaha", "hohoho");
   }
 
   public void testUnfinishedMethodTypeParameter() {
