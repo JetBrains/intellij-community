fb9e20be8f71fa8c0e6c4d1a73a2a8408b05be6a
Bas Leijdekkers <basleijdekkers@gmail.com>
2013-01-17 17:42:45 +0100

    close streams after resources are loaded load resources with utf-8 encoding instead of platform remove unneeded FileUtil class

--- plugins/generate-tostring/src/org/jetbrains/generate/tostring/template/TemplateResourceLocator.java
+++ plugins/generate-tostring/src/org/jetbrains/generate/tostring/template/TemplateResourceLocator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2001-2007 the original author or authors.
+ * Copyright 2001-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,53 +15,54 @@
  */
 package org.jetbrains.generate.tostring.template;
 
+import com.intellij.openapi.util.io.FileUtil;
 import org.jetbrains.generate.tostring.exception.TemplateResourceException;
-import org.jetbrains.generate.tostring.util.FileUtil;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
- * Resource locator for default method body templates and user specific.
- * <p/>
- * Will scan the 'tostring-plugin' folder of the IDEA plugins folder for additional templates.
+ * Resource locator for default method body templates.
  */
 public class TemplateResourceLocator {
-    /** Foldername for additional velocity body templates. The folder should be a subfolder in the IDEA/<i>plugins</i> folder. */
-    public static final String FOLDER_NAME = "tostring-plugin";
 
-    /** Filename for autosaving active template. */
-    public static final String AUTOSAVE_ACTIVE_TEMPLATE_FILE_NAME = "__autosave_active";
+  private static final String DEFAULT_CONCAT = "/org/jetbrains/generate/tostring/template/DefaultConcatMember.vm";
+  private static final String DEFAULT_CONCAT_SUPER = "/org/jetbrains/generate/tostring/template/DefaultConcatMemberSuper.vm";
+  private static final String DEFAULT_BUFFER = "/org/jetbrains/generate/tostring/template/DefaultBuffer.vm";
+  private static final String DEFAULT_BUILDER = "/org/jetbrains/generate/tostring/template/DefaultBuilder.vm";
+  private static final String DEFAULT_TOSTRINGBUILDER = "/org/jetbrains/generate/tostring/template/DefaultToStringBuilder.vm";
 
-    private static final String DEFAULT_CONCAT = "/org/jetbrains/generate/tostring/template/DefaultConcatMember.vm";
-    private static final String DEFAULT_CONCAT_SUPER = "/org/jetbrains/generate/tostring/template/DefaultConcatMemberSuper.vm";
-    private static final String DEFAULT_BUFFER = "/org/jetbrains/generate/tostring/template/DefaultBuffer.vm";
-    private static final String DEFAULT_BUILDER = "/org/jetbrains/generate/tostring/template/DefaultBuilder.vm";
-    private static final String DEFAULT_TOSTRINGBUILDER = "/org/jetbrains/generate/tostring/template/DefaultToStringBuilder.vm";
+  private TemplateResourceLocator() {}
 
-    /**
-     * Only static methods.
-     */
-    private TemplateResourceLocator() {
+  /**
+   * Get the default templates.
+   */
+  public static TemplateResource[] getDefaultTemplates() {
+    try {
+      return new TemplateResource[]{
+        new TemplateResource("String concat (+)", readFile(DEFAULT_CONCAT), true),
+        new TemplateResource("String concat (+) and super.toString()", readFile(DEFAULT_CONCAT_SUPER), true),
+        new TemplateResource("StringBuffer", readFile(DEFAULT_BUFFER), true),
+        new TemplateResource("StringBuilder (JDK 1.5)", readFile(DEFAULT_BUILDER), true),
+        new TemplateResource("ToStringBuilder (Apache Commons)", readFile(DEFAULT_TOSTRINGBUILDER), true),
+      };
     }
-
-
-    /**
-     * Get the default templates.
-     */
-    public static TemplateResource[] getDefaultTemplates() {
-        try {
-            TemplateResource tr1 = new TemplateResource("String concat (+)", FileUtil.readFile(DEFAULT_CONCAT), true);
-            TemplateResource tr2 = new TemplateResource("String concat (+) and super.toString()", FileUtil.readFile(DEFAULT_CONCAT_SUPER), true);
-            TemplateResource tr3 = new TemplateResource("StringBuffer", FileUtil.readFile(DEFAULT_BUFFER), true);
-            TemplateResource tr4 = new TemplateResource("StringBuilder (JDK 1.5)", FileUtil.readFile(DEFAULT_BUILDER), true);
-            TemplateResource tr5 = new TemplateResource("ToStringBuilder (Apache Commons)", FileUtil.readFile(DEFAULT_TOSTRINGBUILDER), true);
-
-            return new TemplateResource[]{tr1, tr2, tr3, tr4, tr5};
-
-        } catch (IOException e) {
-            throw new TemplateResourceException("Error loading default templates", e);
-        }
+    catch (IOException e) {
+      throw new TemplateResourceException("Error loading default templates", e);
     }
+  }
 
-
+  /**
+   * Reads the content of the resource and return it as a String.
+   * <p/>Uses the class loader that loaded this class to find the resource in its classpath.
+   *
+   * @param resource the resource name. Will lookup using the classpath.
+   * @return the content if the resource
+   * @throws IOException error reading the file.
+   */
+  private static String readFile(String resource) throws IOException {
+    BufferedInputStream in = new BufferedInputStream(TemplateResourceLocator.class.getResourceAsStream(resource));
+    return FileUtil.loadTextAndClose(new InputStreamReader(in, "UTF-8"));
+  }
 }
