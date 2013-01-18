e1c2d408f0d4be1138d71bff8137c7e24c3f19e6
Bas Leijdekkers <basleijdekkers@gmail.com>
2013-01-18 12:06:36 +0100

    Check if variable is used in the declaration statement itself in case of multiple declarations in one statement (IDEA-99235)

--- plugins/InspectionGadgets/test/com/siyeh/igtest/dataflow/unnecessary_local_vars/C.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/dataflow/unnecessary_local_vars/C.java
@@ -25,4 +25,62 @@ class C {
     long ll = l;
     return (int) ll;
   }
+
+  public int foo() {
+    int a = 2;
+    int b = a;
+    return b;
+  }
+
+  public int bar() {
+    int b = 3;
+    return b;
+  }
+
+  public int bar2() throws Exception{
+    final Exception b = new Exception();
+    throw b;
+  }
+
+  public int baz() {
+    int a;
+    int b = 3;
+    a = b;
+    return a;
+  }
+
+  public int bazoom() {
+    final int i = foo();
+    bar();
+    final int value = i;
+    System.out.println(value);
+    return 3;
+  }
+
+  double time() {
+    double time = 0.0, dt = time - 1.0;
+    System.out.println(time);
+    return dt;
+  }
+
+  double time2() {
+    double time = 0.0, dt = time - 1.0;
+    return time;
+  }
+
+  void time3() {
+    double time =  0.0, dt = time - 1.0;
+    double time2 = time;
+    time2 += 1;
+  }
+
+  void through() throws Exception {
+    Exception e2 = instance(), e3 = new RuntimeException(e2);
+    throw e2;
+  }
+
+  Exception instance() {
+    return null;
+  }
+
 }
