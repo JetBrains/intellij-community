e025c74ea944b8e6e5957047da37b16ec7a2e42e
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2013-01-21 13:19:32 +0400

    improved reflow for ReStructredText

--- python/rest/src/com/jetbrains/rest/actions/RestFillParagraphHandler.java
+++ python/rest/src/com/jetbrains/rest/actions/RestFillParagraphHandler.java
@@ -4,7 +4,9 @@ import com.intellij.codeInsight.editorActions.fillParagraph.ParagraphFillHandler
 import com.intellij.psi.PsiComment;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiWhiteSpace;
 import com.jetbrains.rest.RestFile;
+import com.jetbrains.rest.RestTokenTypes;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -26,5 +28,11 @@ public class RestFillParagraphHandler extends ParagraphFillHandler {
   @Override
   protected boolean isBunchOfElement(PsiElement element) {
     return true;
+  }
+
+  @Override
+  protected boolean atWhitespaceToken(@Nullable final PsiElement element) {
+    return element instanceof PsiWhiteSpace ||
+           element != null && element.getNode().getElementType() == RestTokenTypes.WHITESPACE;
   }
 }
