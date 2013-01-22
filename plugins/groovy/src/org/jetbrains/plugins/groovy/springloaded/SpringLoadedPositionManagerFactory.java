b0c47a44c7c25f6870c124cf667709521d4dab8f
Sergey Evdokimov <sergey.evdokimov@jetbrains.com>
2013-01-22 19:17:51 +0400

    Support spring loaded for remote process.

--- plugins/groovy/src/org/jetbrains/plugins/groovy/springloaded/SpringLoadedPositionManagerFactory.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/springloaded/SpringLoadedPositionManagerFactory.java
@@ -3,6 +3,9 @@ package org.jetbrains.plugins.groovy.springloaded;
 import com.intellij.debugger.PositionManager;
 import com.intellij.debugger.PositionManagerFactory;
 import com.intellij.debugger.engine.DebugProcess;
+import com.intellij.openapi.application.AccessToken;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.psi.JavaPsiFacade;
 
 /**
  * Factory for position manager to debug classes reloaded by com.springsource.springloaded
@@ -12,7 +15,19 @@ public class SpringLoadedPositionManagerFactory extends PositionManagerFactory {
 
   @Override
   public PositionManager createPositionManager(final DebugProcess process) {
+    AccessToken accessToken = ApplicationManager.getApplication().acquireReadActionLock();
     try {
+      JavaPsiFacade facade = JavaPsiFacade.getInstance(process.getProject());
+      if (facade.findPackage("com.springsource.loaded") != null || facade.findPackage("org.springsource.loaded") != null) {
+        return new SpringLoadedPositionManager(process);
+      }
+    }
+    finally {
+      accessToken.finish();
+    }
+
+    try {
+      // Check spring loaded for remote process
       if (process.getVirtualMachineProxy().classesByName("com.springsource.loaded.agent.SpringLoadedAgent").size() > 0
           || process.getVirtualMachineProxy().classesByName("org.springsource.loaded.agent.SpringLoadedAgent").size() > 0) {
         return new SpringLoadedPositionManager(process);
