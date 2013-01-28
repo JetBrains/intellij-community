bc6a964e4242b0e516b090e4b1882dd10207421a
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2013-01-28 15:50:05 +0400

    logging for OOME from byte buffers

--- platform/util/src/com/intellij/util/io/PagedFileStorage.java
+++ platform/util/src/com/intellij/util/io/PagedFileStorage.java
@@ -610,16 +610,14 @@ public class PagedFileStorage implements Forceable {
             mySizeLimit -= owner.myPageSize;
           }
           long newSize = mySize - owner.myPageSize;
-          if (newSize >= 0) {
-            ensureSize(newSize);
-            continue; // next try
-          }
-          else {
+          if (newSize < 0) {
+            LOG.info("Mapping failed due to OOME. Current buffers: " + mySegments);
             throw new MappingFailedException(
               "Cannot recover from OOME in memory mapping: -Xmx=" + Runtime.getRuntime().maxMemory() / MB + "MB " +
               "new size limit: " + mySizeLimit / MB + "MB " +
               "trying to allocate " + wrapper.myLength + " block", e);
           }
+          ensureSize(newSize); // next try
         }
       }
     }
