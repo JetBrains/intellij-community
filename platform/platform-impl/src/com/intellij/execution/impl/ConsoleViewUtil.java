49c9ca98da8bf59abbd865c527e47d5c75154b0f
peter <peter@jetbrains.com>
2013-01-29 11:20:22 +0100

    setup console editor in a read action (EA-43236 - assert: EditorImpl.visualToLogicalPosition)

--- platform/platform-impl/src/com/intellij/execution/impl/ConsoleViewUtil.java
+++ platform/platform-impl/src/com/intellij/execution/impl/ConsoleViewUtil.java
@@ -16,6 +16,7 @@
 package com.intellij.execution.impl;
 
 import com.intellij.execution.ui.ConsoleViewContentType;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.EditorFactory;
 import com.intellij.openapi.editor.EditorSettings;
@@ -47,24 +48,28 @@ public class ConsoleViewUtil {
     return editor;
   }
 
-  public static void setupConsoleEditor(EditorEx editor, boolean foldingOutlineShown, boolean lineMarkerAreaShown) {
-    editor.setSoftWrapAppliancePlace(SoftWrapAppliancePlaces.CONSOLE);
+  public static void setupConsoleEditor(final EditorEx editor, final boolean foldingOutlineShown, final boolean lineMarkerAreaShown) {
+    ApplicationManager.getApplication().runReadAction(new Runnable() {
+      public void run() {
+        editor.setSoftWrapAppliancePlace(SoftWrapAppliancePlaces.CONSOLE);
 
-    final EditorSettings editorSettings = editor.getSettings();
-    editorSettings.setLineMarkerAreaShown(lineMarkerAreaShown);
-    editorSettings.setIndentGuidesShown(false);
-    editorSettings.setLineNumbersShown(false);
-    editorSettings.setFoldingOutlineShown(foldingOutlineShown);
-    editorSettings.setAdditionalPageAtBottom(false);
-    editorSettings.setAdditionalColumnsCount(0);
-    editorSettings.setAdditionalLinesCount(0);
+        final EditorSettings editorSettings = editor.getSettings();
+        editorSettings.setLineMarkerAreaShown(lineMarkerAreaShown);
+        editorSettings.setIndentGuidesShown(false);
+        editorSettings.setLineNumbersShown(false);
+        editorSettings.setFoldingOutlineShown(foldingOutlineShown);
+        editorSettings.setAdditionalPageAtBottom(false);
+        editorSettings.setAdditionalColumnsCount(0);
+        editorSettings.setAdditionalLinesCount(0);
 
-    editor.putUserData(EDITOR_IS_CONSOLE_VIEW, true);
+        editor.putUserData(EDITOR_IS_CONSOLE_VIEW, true);
 
-    final DelegateColorScheme scheme = updateConsoleColorScheme(editor.getColorsScheme());
-    editor.setColorsScheme(scheme);
-    scheme.setColor(EditorColors.CARET_ROW_COLOR, null);
-    scheme.setColor(EditorColors.RIGHT_MARGIN_COLOR, null);
+        final DelegateColorScheme scheme = updateConsoleColorScheme(editor.getColorsScheme());
+        editor.setColorsScheme(scheme);
+        scheme.setColor(EditorColors.CARET_ROW_COLOR, null);
+        scheme.setColor(EditorColors.RIGHT_MARGIN_COLOR, null);
+      }
+    });
   }
 
   public static DelegateColorScheme updateConsoleColorScheme(EditorColorsScheme scheme) {
