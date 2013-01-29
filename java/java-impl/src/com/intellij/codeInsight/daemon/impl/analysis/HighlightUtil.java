d2435840b2ce84a8396278d182416f96b528a36b
Anna Kozlova <anna.kozlova@jetbrains.com>
2013-01-29 14:42:06 +0400

    allow refs to constants from inner classes in explicit constructor invocation: http://docs.oracle.com/javase/specs/jls/se7/html/jls-8.html#jls-8.8.7.1 (IDEA-99980)

--- java/java-impl/src/com/intellij/codeInsight/daemon/impl/analysis/HighlightUtil.java
+++ java/java-impl/src/com/intellij/codeInsight/daemon/impl/analysis/HighlightUtil.java
@@ -1934,6 +1934,14 @@ public class HighlightUtil extends HighlightUtilBase {
 
         return highlightInfo;
       }
+
+      if (element instanceof PsiReferenceExpression) {
+        final PsiElement resolve = ((PsiReferenceExpression)element).resolve();
+        if (resolve instanceof PsiField && ((PsiField)resolve).hasModifierProperty(PsiModifier.STATIC)) {
+          return null;
+        }
+      }
+
       element = element.getParent();
       if (element instanceof PsiClass && InheritanceUtil.isInheritorOrSelf((PsiClass)element, referencedClass, true)) return null;
     }
