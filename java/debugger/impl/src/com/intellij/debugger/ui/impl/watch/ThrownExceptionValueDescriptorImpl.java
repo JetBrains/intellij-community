dd71643f8edf146337a375b23e08da6f286c352d
Eugene Zhuravlev <jeka@intellij.com>
2013-01-29 21:52:32 +0100

    IDEA-100057 Show exception object in debugger Variables when stopped on exception breakpoint

---
+++ java/debugger/impl/src/com/intellij/debugger/ui/impl/watch/ThrownExceptionValueDescriptorImpl.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2000-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.debugger.ui.impl.watch;
+
+import com.intellij.debugger.DebuggerContext;
+import com.intellij.debugger.engine.evaluation.EvaluateException;
+import com.intellij.debugger.engine.evaluation.EvaluationContextImpl;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiExpression;
+import com.sun.jdi.ObjectReference;
+import com.sun.jdi.Type;
+import com.sun.jdi.Value;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * User: lex
+ * Date: Oct 8, 2003
+ * Time: 5:08:07 PM
+ */
+public class ThrownExceptionValueDescriptorImpl extends ValueDescriptorImpl{
+  @NotNull
+  private final ObjectReference myExceptionObj;
+
+  public ThrownExceptionValueDescriptorImpl(Project project, @NotNull ObjectReference exceptionObj) {
+    super(project);
+    myExceptionObj = exceptionObj;
+  }
+
+  public Value calcValue(EvaluationContextImpl evaluationContext) throws EvaluateException {
+    return myExceptionObj;
+  }
+
+  public String getName() {
+    return "Exception";
+  }
+
+  @NotNull
+  @Override
+  public Type getType() {
+    return myExceptionObj.referenceType();
+  }
+
+  public String calcValueName() {
+    return getName();
+  }
+
+  public PsiExpression getDescriptorEvaluation(DebuggerContext context) throws EvaluateException {
+    throw new EvaluateException("Evaluation not supported for thrown exception object");
+  }
+
+  public boolean canSetValue() {
+    return false;
+  }
+}
