294686a79e449b2f4034eb34f500adc8bd0b6492
erokhins <smevok@yandex.ru>
2013-01-30 12:12:42 +0400

    write code for EdgeController & ElementVisibilityController

--- graph/src/org/hanuna/gitalk/graph/new_mutable/EdgeController.java
+++ graph/src/org/hanuna/gitalk/graph/new_mutable/EdgeController.java
@@ -1,5 +1,7 @@
 package org.hanuna.gitalk.graph.new_mutable;
 
+import org.hanuna.gitalk.common.HashInvertibleMap;
+import org.hanuna.gitalk.common.InvertibleMap;
 import org.hanuna.gitalk.graph.elements.Edge;
 import org.hanuna.gitalk.graph.elements.Node;
 import org.jetbrains.annotations.NotNull;
@@ -10,32 +12,44 @@ import java.util.Set;
  * @author erokhins
  */
 public class EdgeController {
+    private final InvertibleMap<Edge, Node> upNodes = new HashInvertibleMap<Edge, Node>();
+    private final InvertibleMap<Edge, Node> downNodes = new HashInvertibleMap<Edge, Node>();
 
     public void addEdge(@NotNull Node upNode, @NotNull Node downNode, @NotNull Edge edge) {
-
+        upNodes.put(edge, upNode);
+        downNodes.put(edge, downNode);
     }
 
-    public void removeEdge(@NotNull Node upNode, @NotNull Node downNode, @NotNull Edge edge) {
-
+    public void removeEdge(@NotNull Edge edge) {
+        upNodes.remove(edge);
+        downNodes.remove(edge);
     }
 
     @NotNull
     public Node getUpNode(@NotNull Edge edge) {
-        return null;
+        Node node = upNodes.get(edge);
+        if (node == null) {
+            throw new IllegalStateException("request up node of edge, but edge doesn't add");
+        }
+        return node;
     }
 
     @NotNull
     public Node getDownNode(@NotNull Edge edge) {
-        return null;
+        Node node = downNodes.get(edge);
+        if (node == null) {
+            throw new IllegalStateException("request down node of edge, but edge doesn't add");
+        }
+        return node;
     }
 
     @NotNull
     public Set<Edge> getUpEdges(@NotNull Node node) {
-        return null;
+        return downNodes.getKeys(node);
     }
 
     @NotNull
     public Set<Edge> getDownEdges(@NotNull Node node) {
-        return null;
+        return upNodes.getKeys(node);
     }
 }
--- graph/src/org/hanuna/gitalk/graph/new_mutable/ElementVisibilityController.java
+++ graph/src/org/hanuna/gitalk/graph/new_mutable/ElementVisibilityController.java
@@ -3,12 +3,27 @@ package org.hanuna.gitalk.graph.new_mutable;
 import org.hanuna.gitalk.graph.elements.GraphElement;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author erokhins
  */
 public class ElementVisibilityController {
+    private final Set<GraphElement> hiddenElements = new HashSet<GraphElement>();
+
 
     public boolean isVisible(@NotNull GraphElement graphElement) {
-        return true;
+        return hiddenElements.contains(graphElement);
+    }
+
+    public void hide(@NotNull Collection<GraphElement> graphElements) {
+        hiddenElements.addAll(graphElements);
+    }
+
+
+    public void show(@NotNull Collection<GraphElement> graphElements) {
+        hiddenElements.removeAll(graphElements);
     }
 }
