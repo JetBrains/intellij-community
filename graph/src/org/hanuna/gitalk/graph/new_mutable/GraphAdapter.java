09988bced106a892f54e53d225d2b629eec54ab2
erokhins <smevok@yandex.ru>
2013-01-31 01:54:59 +0400

    add GraphAdapter(NewGraph -> Graph) & test for NewGraph

---
+++ graph/src/org/hanuna/gitalk/graph/new_mutable/GraphAdapter.java
@@ -0,0 +1,81 @@
+package org.hanuna.gitalk.graph.new_mutable;
+
+import org.hanuna.gitalk.common.Executor;
+import org.hanuna.gitalk.common.compressedlist.Replace;
+import org.hanuna.gitalk.graph.Graph;
+import org.hanuna.gitalk.graph.GraphFragmentController;
+import org.hanuna.gitalk.graph.NewGraph;
+import org.hanuna.gitalk.graph.elements.GraphElement;
+import org.hanuna.gitalk.graph.elements.GraphFragment;
+import org.hanuna.gitalk.graph.elements.NodeRow;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.AbstractList;
+import java.util.List;
+
+/**
+ * @author erokhins
+ */
+public class GraphAdapter implements Graph {
+    private final NewGraph graph;
+    private final List<NodeRow> nodeRows;
+    private final GraphFragmentController graphFragmentController = new GraphFragmentController() {
+        @Override
+        public GraphFragment relateFragment(@NotNull GraphElement graphElement) {
+            return null;
+        }
+
+        @NotNull
+        @Override
+        public Replace setVisible(@NotNull GraphFragment fragment, boolean visible) {
+            return Replace.ID_REPLACE;
+        }
+
+        @Override
+        public boolean isVisible(@NotNull GraphFragment fragment) {
+            return false;
+        }
+    };
+
+    public GraphAdapter(final NewGraph graph) {
+        this.graph = graph;
+        nodeRows = new AbstractList<NodeRow>() {
+            @Override
+            public NodeRow get(int index) {
+                return graph.getNodeRow(index);
+            }
+
+            @Override
+            public int size() {
+                return graph.size();
+            }
+        };
+    }
+
+    @NotNull
+    @Override
+    public List<NodeRow> getNodeRows() {
+        return nodeRows;
+    }
+
+    @NotNull
+    @Override
+    public GraphFragmentController getFragmentController() {
+        return graphFragmentController;
+    }
+
+    @Override
+    public void addUpdateListener(@NotNull final GraphUpdateListener updateListener) {
+        graph.addUpdateListener(new Executor<Replace>() {
+            @Override
+            public void execute(Replace key) {
+                updateListener.doReplace(key);
+            }
+        });
+    }
+
+    @Override
+    public void removeAllListeners() {
+        graph.removeAllListeners();
+    }
+}
