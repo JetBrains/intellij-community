f2fcc50eea1bf541c0f94b4c7c590e8965144636
Dmitry Jemerov <yole@jetbrains.com>
2013-01-31 19:14:44 +0100

    statistics for Python interpreter versions and used packages

---
+++ python/src/com/jetbrains/python/statistics/PyPackageUsagesCollector.java
@@ -0,0 +1,59 @@
+package com.jetbrains.python.statistics;
+
+import com.intellij.internal.statistic.AbstractApplicationUsagesCollector;
+import com.intellij.internal.statistic.CollectUsagesException;
+import com.intellij.internal.statistic.beans.GroupDescriptor;
+import com.intellij.internal.statistic.beans.UsageDescriptor;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.projectRoots.Sdk;
+import com.jetbrains.python.packaging.PyPIPackageUtil;
+import com.jetbrains.python.packaging.PyPackageManagerImpl;
+import com.jetbrains.python.packaging.PyRequirement;
+import com.jetbrains.python.sdk.PythonSdkType;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * @author yole
+ */
+public class PyPackageUsagesCollector extends AbstractApplicationUsagesCollector {
+  private static final String GROUP_ID = "py-packages";
+
+  @NotNull
+  @Override
+  public Set<UsageDescriptor> getProjectUsages(@NotNull Project project) throws CollectUsagesException {
+    Set<UsageDescriptor> result = new HashSet<UsageDescriptor>();
+    for(Module m: ModuleManager.getInstance(project).getModules()) {
+      Sdk pythonSdk = PythonSdkType.findPythonSdk(m);
+      if (pythonSdk != null) {
+        List<PyRequirement> requirements = PyPackageManagerImpl.getRequirements(m);
+        if (requirements != null) {
+          Collection<String> packages;
+          try {
+            packages = new HashSet<String>(PyPIPackageUtil.INSTANCE.getPackageNames());
+          }
+          catch (IOException e) {
+            continue;
+          }
+          for (PyRequirement requirement : requirements) {
+            String name = requirement.getName();
+            if (packages.contains(name)) {
+              result.add(new UsageDescriptor(name, 1));
+            }
+          }
+        }
+      }
+    }
+    return result;
+  }
+
+  @NotNull
+  @Override
+  public GroupDescriptor getGroupId() {
+    return GroupDescriptor.create(GROUP_ID);
+  }
+}
