95513d4051b8bad54ea26789cc328c1f0d3094db
erokhins <smevok@yandex.ru>
2013-01-31 22:56:16 +0400

    add branch show/hide capability

--- graph/src/org/hanuna/gitalk/graph/new_mutable/ElementVisibilityController.java
+++ graph/src/org/hanuna/gitalk/graph/new_mutable/ElementVisibilityController.java
@@ -1,27 +1,54 @@
 package org.hanuna.gitalk.graph.new_mutable;
 
+import org.hanuna.gitalk.commitmodel.Commit;
+import org.hanuna.gitalk.graph.elements.Edge;
 import org.hanuna.gitalk.graph.elements.GraphElement;
+import org.hanuna.gitalk.graph.elements.Node;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author erokhins
  */
 public class ElementVisibilityController {
-    private final Set<GraphElement> hiddenElements = new HashSet<GraphElement>();
+    private final Set<Node> hiddenNodes = new HashSet<Node>();
+    private final Collection<Commit> visibleCommits = new HashSet<Commit>();
+    private boolean showAllBranchesFlag = true;
 
     public boolean isVisible(@NotNull GraphElement graphElement) {
-        return !hiddenElements.contains(graphElement);
+        Node node = graphElement.getNode();
+        if (node != null) {
+            return isVisibleNode(node);
+        } else {
+            Edge edge = graphElement.getEdge();
+            assert edge != null;
+            return isVisible(edge.getUpNode()) && isVisible(edge.getDownNode());
+        }
     }
 
-    public void hide(@NotNull Collection<GraphElement> graphElements) {
-        hiddenElements.addAll(graphElements);
+    private boolean isVisibleNode(@NotNull Node node) {
+        return !hiddenNodes.contains(node) && (showAllBranchesFlag || visibleCommits.contains(node.getCommit()));
     }
 
-    public void show(@NotNull Collection<GraphElement> graphElements) {
-        hiddenElements.removeAll(graphElements);
+
+    public void hide(@NotNull Collection<Node> graphElements) {
+        hiddenNodes.addAll(graphElements);
     }
+
+    public void show(@NotNull Collection<Node> graphElements) {
+        hiddenNodes.removeAll(graphElements);
+    }
+
+    public void setShowAllBranchesFlag(boolean showAllBranchesFlag) {
+        this.showAllBranchesFlag = showAllBranchesFlag;
+    }
+
+    public void setVisibleCommits(@NotNull List<Commit> visibleCommits) {
+        this.visibleCommits.clear();
+        this.visibleCommits.addAll(visibleCommits);
+        showAllBranchesFlag = false;
+    }
+
+
 }
