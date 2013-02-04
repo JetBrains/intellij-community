ac4e428e4f0a277b884773cb42ea24a17a220134
Denis.Zhdanov <Denis.Zhdanov@jetbrains.com>
2013-02-04 09:05:14 +0400

    IDEA-99600 Exception: Unexpected caret move request

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/CaretModelImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/CaretModelImpl.java
@@ -622,7 +622,7 @@ public class CaretModelImpl implements CaretModel, PrioritizedDocumentListener, 
 
   @Override
   public boolean isUpToDate() {
-    return !myIsInUpdate;
+    return !myIsInUpdate && !myReportCaretMoves;
   }
 
   @Override
