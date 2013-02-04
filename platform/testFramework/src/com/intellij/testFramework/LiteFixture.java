e2722a263742d14caae98fe5b4875c937c5f28ff
Dmitry Jemerov <yole@jetbrains.com>
2013-02-04 21:11:16 +0100

    allow running aop-common tests in module classpath

---
+++ platform/testFramework/src/com/intellij/testFramework/LiteFixture.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2000-2007 JetBrains s.r.o. All Rights Reserved.
+ */
+package com.intellij.testFramework;
+
+import com.intellij.psi.IdentitySmartPointer;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.impl.source.resolve.FileContextUtil;
+
+/**
+ * @author peter
+ */
+public abstract class LiteFixture extends PlatformLiteFixture {
+  public static void setContext(final PsiFile psiFile, final PsiElement context) {
+    if (context != null) {
+      psiFile.putUserData(FileContextUtil.INJECTED_IN_ELEMENT, new IdentitySmartPointer<PsiElement>(context, psiFile));
+    }
+  }
+}
---
+++ platform/testFramework/src/com/intellij/testFramework/MockProblemDescriptor.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2000-2007 JetBrains s.r.o. All Rights Reserved.
+ */
+package com.intellij.testFramework;
+
+import com.intellij.codeInspection.LocalQuickFix;
+import com.intellij.codeInspection.ProblemHighlightType;
+import com.intellij.codeInspection.ex.ProblemDescriptorImpl;
+import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author peter
+*/
+public class MockProblemDescriptor extends ProblemDescriptorImpl {
+  public MockProblemDescriptor(@NotNull final PsiElement psiElement, final String descriptionTemplate,
+                               final ProblemHighlightType highlightType,
+                               final LocalQuickFix... fixes) {
+    super(psiElement, psiElement, descriptionTemplate, fixes, highlightType, false, null, true);
+  }
+
+  @Override
+  protected void assertPhysical(final PsiElement startElement) {
+  }
+}
