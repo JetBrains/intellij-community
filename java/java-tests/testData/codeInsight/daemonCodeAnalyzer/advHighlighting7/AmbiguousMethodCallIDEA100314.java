c4be7cc14a44df76f9fe74644c2d1cdd9dd9fd0c
Anna Kozlova <anna.kozlova@jetbrains.com>
2013-02-04 11:22:13 +0400

    ambiguous method call: prefer non raw types (IDEA-100314)

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/advHighlighting7/AmbiguousMethodCallIDEA100314.java
@@ -0,0 +1,13 @@
+class Intellij {
+
+  public static <T> T getProperty(final String str, final Class<T> cls) {
+    return null;
+  }
+
+  public static <T> T getProperty(final String str, final T defaultValue) {
+    return null;
+  }
+
+  String[] ids = Intellij.getProperty(" ", String[].class);
+
+}
