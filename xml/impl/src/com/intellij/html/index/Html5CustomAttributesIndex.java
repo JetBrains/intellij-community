3f35fcaf7fd21aa3d411e11384503d9a39cbdcd6
Yann Cébron <yann.cebron@jetbrains.com>
2013-02-04 11:30:57 +0100

    cleanup

--- xml/impl/src/com/intellij/html/index/Html5CustomAttributesIndex.java
+++ xml/impl/src/com/intellij/html/index/Html5CustomAttributesIndex.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2010 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,7 @@ import com.intellij.lang.xhtml.XHTMLLanguage;
 import com.intellij.lexer.HtmlHighlightingLexer;
 import com.intellij.lexer.Lexer;
 import com.intellij.lexer.XHtmlHighlightingLexer;
+import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.fileTypes.LanguageFileType;
 import com.intellij.openapi.fileTypes.StdFileTypes;
 import com.intellij.openapi.vfs.LocalFileSystem;
@@ -78,8 +79,12 @@ public class Html5CustomAttributesIndex extends ScalarIndexExtension<String> {
   private final FileBasedIndex.InputFilter myInputFilter = new FileBasedIndex.InputFilter() {
     @Override
     public boolean acceptInput(final VirtualFile file) {
-      return (file.getFileSystem() == LocalFileSystem.getInstance() || file.getFileSystem() instanceof TempFileSystem) &&
-             file.getFileType() == StdFileTypes.HTML || file.getFileType() == StdFileTypes.XHTML;
+      if (file.getFileSystem() != LocalFileSystem.getInstance() && !(file.getFileSystem() instanceof TempFileSystem)) {
+        return false;
+      }
+
+      final FileType fileType = file.getFileType();
+      return fileType == StdFileTypes.HTML || fileType == StdFileTypes.XHTML;
     }
   };
 
