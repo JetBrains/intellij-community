615ebf1bc16e729292c545dc1bdd0376565ace97
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2013-02-07 19:37:55 +0400

    Fixed unbound local for a variable after 'with' statement after 'raise' (PY-7966)

--- python/src/com/jetbrains/python/codeInsight/controlflow/PyControlFlowBuilder.java
+++ python/src/com/jetbrains/python/codeInsight/controlflow/PyControlFlowBuilder.java
@@ -709,7 +709,8 @@ public class PyControlFlowBuilder extends PyRecursiveElementVisitor {
     myBuilder.processPending(new ControlFlowBuilder.PendingProcessor() {
       public void process(final PsiElement pendingScope, final Instruction instruction) {
         final PsiElement element = instruction.getElement();
-        if (PsiTreeUtil.getParentOfType(element, PyRaiseStatement.class) != null) {
+        if (element != null && PsiTreeUtil.isAncestor(node, element, true) &&
+            PsiTreeUtil.getParentOfType(element, PyRaiseStatement.class) != null) {
           myBuilder.addPendingEdge(node, instruction);
         }
         else {
