5b17e0aeb19219adda08998a9555d99dbfca5ad5
Dmitry Jemerov <yole@jetbrains.com>
2013-02-07 20:31:04 +0100

    rename test class appropriately

--- java/java-tests/testSrc/com/intellij/psi/CoreJavaFileManagerTest.java
+++ java/java-tests/testSrc/com/intellij/psi/CoreJavaFileManagerTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.intellij.psi;
 
 import com.intellij.core.CoreJavaFileManager;
@@ -8,7 +23,7 @@ import com.intellij.testFramework.PsiTestCase;
 import com.intellij.testFramework.PsiTestUtil;
 
 
-public class NotNullInnerClass extends PsiTestCase {
+public class CoreJavaFileManagerTest extends PsiTestCase {
 
   public void testNotNullInnerClass() throws Exception {
     VirtualFile root = PsiTestUtil.createTestProjectStructure(myProject, myModule, myFilesToDelete);
