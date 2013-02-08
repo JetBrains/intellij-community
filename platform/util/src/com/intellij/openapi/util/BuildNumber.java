85bbc002c280063d5117aae5787dd4032639aa94
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2013-02-08 10:25:05 +0100

    Typo

--- platform/util/src/com/intellij/openapi/util/BuildNumber.java
+++ platform/util/src/com/intellij/openapi/util/BuildNumber.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -103,7 +103,7 @@ public class BuildNumber implements Comparable<BuildNumber> {
         code = code.substring(baselineVersionSeparator + 1);
       }
       catch (NumberFormatException e) {
-        throw new RuntimeException("Unparseable version number: " + version + "; plugin name: " + name);
+        throw new RuntimeException("Invalid version number: " + version + "; plugin name: " + name);
       }
 
       buildNumber = parseBuildNumber(version, code, name);
@@ -130,7 +130,7 @@ public class BuildNumber implements Comparable<BuildNumber> {
       return Integer.parseInt(code);
     }
     catch (NumberFormatException e) {
-      throw new RuntimeException("Unparseable version number: " + version +"; plugin name: " + name);
+      throw new RuntimeException("Invalid version number: " + version +"; plugin name: " + name);
     }
   }
 
