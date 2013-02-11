173057f13fe539e6f90023b09f61fd38caf08a64
Bas Leijdekkers <basleijdekkers@gmail.com>
2013-02-11 17:24:52 +0100

    do not report native methods as 'may be static'

--- plugins/InspectionGadgets/testsrc/com/siyeh/ig/performance/MethodMayBeStaticInspectionTest.java
+++ plugins/InspectionGadgets/testsrc/com/siyeh/ig/performance/MethodMayBeStaticInspectionTest.java
@@ -5,7 +5,8 @@ import com.siyeh.ig.IGInspectionTestCase;
 public class MethodMayBeStaticInspectionTest extends IGInspectionTestCase {
 
   public void test() throws Exception {
-    doTest("com/siyeh/igtest/performance/method_may_be_static",
-           new MethodMayBeStaticInspection());
+    final MethodMayBeStaticInspection tool = new MethodMayBeStaticInspection();
+    tool.m_ignoreEmptyMethods = false;
+    doTest("com/siyeh/igtest/performance/method_may_be_static", tool);
   }
 }
