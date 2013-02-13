a7a6302dc3e0ad2871cac646da07a7cb2900196d
Vassiliy Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2013-02-13 19:55:46 +0400

    IDEA-86724 Choose color dialog input fields are too narrow

--- platform/platform-impl/src/com/intellij/ui/ColorPicker.java
+++ platform/platform-impl/src/com/intellij/ui/ColorPicker.java
@@ -73,7 +73,14 @@ public class ColorPicker extends JPanel implements ColorListener, DocumentListen
   private final JLabel myR_after = new JLabel("");
   private final JLabel myG_after = new JLabel("");
   private final JLabel myB_after = new JLabel("");
-  private final JComboBox myFormat = new JComboBox(new String[]{"RGB", "HSB"});
+  private final JComboBox myFormat = new JComboBox(new String[]{"RGB", "HSB"}) {
+    @Override
+    public Dimension getPreferredSize() {
+      Dimension size = super.getPreferredSize();
+      size.width+=10;
+      return size;
+    }
+  };
 
   public ColorPicker(@NotNull Disposable parent, @Nullable Color color, boolean enableOpacity) {
     this(parent, color, true, enableOpacity, new ColorPickerListener[0]);
@@ -152,7 +159,7 @@ public class ColorPicker extends JPanel implements ColorListener, DocumentListen
   private JTextField createColorField(boolean hex) {
     final NumberDocument doc = new NumberDocument(hex);
     final int lafFix = UIUtil.isUnderWindowsClassicLookAndFeel() || UIUtil.isUnderWindowsLookAndFeel() || UIUtil.isUnderDarcula() ? 1 : 0;
-    final JTextField field = new JTextField(doc, "", (hex ? 5:2) + lafFix);
+    final JTextField field = new JTextField(doc, "", (hex ? 6 : 3) + lafFix);
     field.setSize(50, -1);
     doc.setSource(field);
     field.getDocument().addDocumentListener(this);
