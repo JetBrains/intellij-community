ffeaa8c21f7379fadd0c421b3a98f4d5ea3d77e8
anna <Anna.Kozlova@jetbrains.com>
2013-02-14 12:48:11 +0100

    javafx: open .fxml in SceneBuilder

---
+++ plugins/javaFX/src/org/jetbrains/plugins/javaFX/JavaFxSettings.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2000-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jetbrains.plugins.javaFX;
+
+import com.intellij.openapi.components.*;
+import com.intellij.util.xmlb.XmlSerializerUtil;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * User: anna
+ * Date: 2/14/13
+ */
+@State(
+  name="JavaFxSettings",
+  storages = {
+    @Storage(
+      file = StoragePathMacros.APP_CONFIG + "/other.xml"
+    )}
+)
+public class JavaFxSettings implements PersistentStateComponent<JavaFxSettings> {
+  public String myPathToSceneBuilder = null;
+  
+  public static JavaFxSettings getInstance() {
+    return ServiceManager.getService(JavaFxSettings.class);
+  }
+
+  public JavaFxSettings getState() {
+    return this;
+  }
+
+  public void loadState(JavaFxSettings object) {
+    XmlSerializerUtil.copyBean(object, this);
+  }
+
+  @Nullable
+  public String getPathToSceneBuilder() {
+    return myPathToSceneBuilder;
+  }
+
+  public void setPathToSceneBuilder(String pathToSceneBuilder) {
+    myPathToSceneBuilder = pathToSceneBuilder;
+  }
+}
