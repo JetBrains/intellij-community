98692ea2a6afb2aab713d5d9435aff6debc86b5b
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2013-02-15 10:50:55 +0100

    Meaningful logging

--- platform/core-impl/src/com/intellij/openapi/vfs/impl/jar/JarHandlerBase.java
+++ platform/core-impl/src/com/intellij/openapi/vfs/impl/jar/JarHandlerBase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -198,7 +198,7 @@ public class JarHandlerBase {
       };
     }
     catch (IOException e) {
-      LOG.warn(e.getMessage() + ": " + originalFile.getPath());
+      LOG.warn(e.getMessage() + ": " + originalFile.getPath(), e);
       return null;
     }
   }
