71fd4a7ba92553bc6b22a8438697300eb635a399
Nadya.Zabrodina <Nadya.Zabrodina@jetbrains.com>
2013-02-15 17:48:56 +0400

    Refactor HG action dialogs. Load  hg content (branches) before dialog appears if needed. Execute hg actions as backgroundable tasks.
    *Change using structure HgGlobalCommandBuilder to  execute method, which is overridden by inheritors.
    *Fixed IDEA-101120,  IDEA-100982 (Mercurial updateTo and merge dialogs are not wide enough by default) - now we load branches for all hg repositories before dialog constructor called.
    *Fixed IDEA-100985 Mercurial Merge was executed in AWT thread, now we call it in backgroundable task. (and other actions too).
    *Store all branches for all repositories in Merge and Switch (UpdateTo)dialog.

--- plugins/hg4idea/src/org/zmlx/hg4idea/action/HgSwitchWorkingDirectoryAction.java
+++ plugins/hg4idea/src/org/zmlx/hg4idea/action/HgSwitchWorkingDirectoryAction.java
@@ -14,62 +14,70 @@ package org.zmlx.hg4idea.action;
 
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
 import org.zmlx.hg4idea.HgVcs;
+import org.zmlx.hg4idea.HgVcsMessages;
+import org.zmlx.hg4idea.command.HgTagBranch;
 import org.zmlx.hg4idea.command.HgUpdateCommand;
 import org.zmlx.hg4idea.execution.HgCommandResult;
 import org.zmlx.hg4idea.ui.HgSwitchDialog;
 import org.zmlx.hg4idea.util.HgErrorUtil;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 public class HgSwitchWorkingDirectoryAction extends HgAbstractGlobalAction {
 
-  protected HgGlobalCommandBuilder getHgGlobalCommandBuilder(final Project project) {
-    return new HgGlobalCommandBuilder() {
-      public HgGlobalCommand build(Collection<VirtualFile> repos) {
-        HgSwitchDialog dialog = new HgSwitchDialog(project);
-        dialog.setRoots(repos);
-        dialog.show();
-        if (dialog.isOK()) {
-          return buildCommand(dialog, project);
-        }
-        return null;
-      }
-    };
+  protected void execute(final Project project, final Collection<VirtualFile> repos) {
+    loadBranchesInBackgroundableAndExecuteAction(project, repos);
   }
 
-  private static HgGlobalCommand buildCommand(final HgSwitchDialog dialog, final Project project) {
-    return new HgGlobalCommand() {
-      public VirtualFile getRepo() {
-        return dialog.getRepository();
-      }
+  @Override
+  protected void showDialogAndExecute(final Project project,
+                                      Collection<VirtualFile> repos,
+                                      Map<VirtualFile, List<HgTagBranch>> branchesForRepos) {
+    final HgSwitchDialog dialog = new HgSwitchDialog(project);
+    dialog.setRoots(repos, branchesForRepos);
+    dialog.show();
+    if (dialog.isOK()) {
+      new Task.Backgroundable(project, HgVcsMessages.message("action.hg4idea.switch.description")) {
 
-      public void execute() {
-        final HgUpdateCommand command = new HgUpdateCommand(project, dialog.getRepository());
-        command.setClean(dialog.isRemoveLocalChanges());
-        if (dialog.isRevisionSelected()) {
-          command.setRevision(dialog.getRevision());
+        @Override
+        public void run(@NotNull ProgressIndicator indicator) {
+          updateTo(dialog, project);
+          markDirtyAndHandleErrors(project, dialog.getRepository());
         }
-        if (dialog.isBranchSelected()) {
-          command.setBranch(dialog.getBranch().getName());
+      }.queue();
+    }
+  }
+
+  public void updateTo(HgSwitchDialog dialog, final Project project) {
+    final HgUpdateCommand command = new HgUpdateCommand(project, dialog.getRepository());
+    command.setClean(dialog.isRemoveLocalChanges());
+    if (dialog.isRevisionSelected()) {
+      command.setRevision(dialog.getRevision());
+    }
+    if (dialog.isBranchSelected()) {
+      command.setBranch(dialog.getBranch().getName());
+    }
+    if (dialog.isTagSelected()) {
+      command.setRevision(dialog.getTag().getName());
+    }
+    FileDocumentManager.getInstance().saveAllDocuments();
+    ApplicationManager.getApplication().executeOnPooledThread(new Runnable() {
+      @Override
+      public void run() {
+        HgCommandResult result = command.execute();
+        if (HgErrorUtil.hasErrorsInCommandExecution(result)) {
+          new HgCommandResultNotifier(project).notifyError(result, "", "Update failed");
         }
-        if (dialog.isTagSelected()) {
-          command.setRevision(dialog.getTag().getName());
-        }
-        FileDocumentManager.getInstance().saveAllDocuments();
-        ApplicationManager.getApplication().executeOnPooledThread(new Runnable() {
-          @Override
-          public void run() {
-            HgCommandResult result = command.execute();
-            if (HgErrorUtil.hasErrorsInCommandExecution(result)) {
-              new HgCommandResultNotifier(project).notifyError(result, "", "Update failed");
-            }
-            project.getMessageBus().syncPublisher(HgVcs.BRANCH_TOPIC).update(project, null);
-          }
-        });
+        project.getMessageBus().syncPublisher(HgVcs.BRANCH_TOPIC).update(project, null);
       }
-    };
+    });
   }
 }
