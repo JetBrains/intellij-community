3ff80d756211a5a36796e6e66fdb9311693a7bfa
peter <peter@jetbrains.com>
2013-02-16 10:04:31 +0100

    fix css completion tests

--- platform/util/src/com/intellij/psi/codeStyle/MinusculeMatcher.java
+++ platform/util/src/com/intellij/psi/codeStyle/MinusculeMatcher.java
@@ -119,7 +119,7 @@ public class MinusculeMatcher implements Matcher {
 
     int integral = 0; // matching-character-count * hump-index; favors longer fragments matching earlier words
     int humpIndex = 0;
-    int nextHumpStart = NameUtil.nextWord(name, 0);
+    int nextHumpStart = name.isEmpty() ? 0: NameUtil.nextWord(name, 0);
     for (TextRange range : iterable) {
       if (first == null) {
         first = range;
