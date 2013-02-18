104b9814cc46fb2745193f5a4e381bc8b55db798
Nadya.Zabrodina <Nadya.Zabrodina@jetbrains.com>
2013-02-18 17:26:19 +0400

    Extract loadBranchesInBackgroundableAndExecuteAction method to a separate util class.
    *Add consumer to argument list to handle success.
    (cherry picked from commit 694d24be6eef7a9e59bd37bdd1f20a0851f1c11d)

--- plugins/hg4idea/src/org/zmlx/hg4idea/action/HgAbstractGlobalAction.java
+++ plugins/hg4idea/src/org/zmlx/hg4idea/action/HgAbstractGlobalAction.java
@@ -14,22 +14,13 @@ package org.zmlx.hg4idea.action;
 
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.Consumer;
-import com.intellij.util.containers.HashMap;
-import org.jetbrains.annotations.NotNull;
-import org.zmlx.hg4idea.command.HgTagBranch;
-import org.zmlx.hg4idea.command.HgTagBranchCommand;
 import org.zmlx.hg4idea.util.HgUtil;
 
 import javax.swing.*;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
-import java.util.List;
-import java.util.Map;
 
 abstract class HgAbstractGlobalAction extends AnAction {
   protected HgAbstractGlobalAction(Icon icon) {
@@ -82,30 +73,4 @@ abstract class HgAbstractGlobalAction extends AnAction {
     }
   }
 
-  protected void loadBranchesInBackgroundableAndExecuteAction(final Project project, final Collection<VirtualFile> repos) {
-    final Map<VirtualFile, List<HgTagBranch>> branchesForRepos = new HashMap<VirtualFile, List<HgTagBranch>>();
-    new Task.Backgroundable(project, "Update branches...") {
-      @Override
-      public void run(@NotNull ProgressIndicator indicator) {
-        for (final VirtualFile repo : repos) {
-          new HgTagBranchCommand(project, repo).listBranches(new Consumer<List<HgTagBranch>>() {
-            @Override
-            public void consume(final List<HgTagBranch> branches) {
-              branchesForRepos.put(repo, branches);
-            }
-          });
-        }
-      }
-
-      @Override
-      public void onSuccess() {
-        showDialogAndExecute(project, repos, branchesForRepos);
-      }
-    }.queue();
-  }
-
-  protected void showDialogAndExecute(Project project,
-                                      Collection<VirtualFile> repos,
-                                      Map<VirtualFile, List<HgTagBranch>> loadedBranches) {
-  }
 }
