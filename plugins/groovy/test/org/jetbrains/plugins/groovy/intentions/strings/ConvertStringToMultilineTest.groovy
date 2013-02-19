1df87fd3206bdeab1bd9e6922e962433dbc15cd1
Max Medvedev <maxim.medvedev@jetbrains.com>
2013-02-19 15:17:01 +0400

    IDEA-94214 Convert to multiline String should work on concatenation

--- plugins/groovy/test/org/jetbrains/plugins/groovy/intentions/strings/ConvertStringToMultilineTest.groovy
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/intentions/strings/ConvertStringToMultilineTest.groovy
@@ -15,67 +15,82 @@
  */
 package org.jetbrains.plugins.groovy.intentions.strings
 
-import com.intellij.openapi.application.WriteAction
-import org.jetbrains.annotations.Nullable
-import org.jetbrains.plugins.groovy.LightGroovyTestCase
+import com.intellij.codeInsight.intention.IntentionAction
+import org.jetbrains.plugins.groovy.GroovyFileType
+import org.jetbrains.plugins.groovy.intentions.GrIntentionTestCase
 import org.jetbrains.plugins.groovy.intentions.conversions.strings.ConvertStringToMultilineIntention
+import org.jetbrains.plugins.groovy.util.TestUtils
 
 /**
  * @author Max Medvedev
  */
-public class ConvertStringToMultilineTest extends LightGroovyTestCase {
-  final String basePath = ''
+public class ConvertStringToMultilineTest extends GrIntentionTestCase {
+  ConvertStringToMultilineTest() {
+    super(ConvertStringToMultilineIntention.hint)
+  }
+
+  final String basePath = TestUtils.testDataPath + "intentions/convertToMultiline/"
 
   void testPlainString() {
-    doTest("print 'ab<caret>c'", "print '''abc'''")
+    doTextTest("print 'ab<caret>c'", "print '''abc'''")
   }
 
   void testGString() {
-    doTest('print "ab<caret>c"', 'print """abc"""')
+    doTextTest('print "ab<caret>c"', 'print """abc"""')
   }
 
   void testPlainString2() {
-    doTest("print 'a\\nb<caret>c'", "print '''a\nbc'''")
+    doTextTest("print 'a\\nb<caret>c'", "print '''a\nbc'''")
   }
 
   void testGString2() {
-    doTest('print "a\\nb<caret>c"', 'print """a\nbc"""')
+    doTextTest('print "a\\nb<caret>c"', 'print """a\nbc"""')
   }
 
   void testGString3() {
-    doTest('print "a\\nb${a<caret>}c"', 'print """a\nb${a}c"""')
+    doTextTest('print "a\\nb${a<caret>}c"', 'print """a\nb${a}c"""')
   }
 
   void testAlreadyMultiline() {
-    doTest('print """a<caret>bc"""', null)
+    doAntiTest('print """a<caret>bc"""')
   }
 
   void testGString4() {
-    doTest('''\
+    doTextTest('''\
 print "ab<caret>c\\$ $x"
 ''', '''\
 print """abc\\$ $x"""
 ''')
   }
 
-  private doTest(String before, @Nullable String after) {
-    myFixture.with {
-      configureByText('_.groovy', before)
-      def intentions = filterAvailableIntentions(ConvertStringToMultilineIntention.hint)
-      if (!after) {
-        assertEmpty(intentions)
-        return
-      }
+  void doSelectionTest(String before, String after) {
+    myFixture.configureByText(GroovyFileType.GROOVY_FILE_TYPE, before)
 
-      assertOneElement(intentions)
-      def accessToken = WriteAction.start()
-      try {
-        intentions[0].invoke(project, editor, file)
-      }
-      finally {
-        accessToken.finish()
-      }
-      checkResult(after)
-    }
+    assertTrue myFixture.editor.selectionModel.hasSelection()
+    final IntentionAction intention = new ConvertStringToMultilineIntention()
+    assertTrue intention.isAvailable(myFixture.project, myFixture.editor, myFixture.file)
+    intention.invoke(myFixture.project, myFixture.editor, myFixture.file)
+
+    myFixture.checkResult(after)
+  }
+
+  void testSimpleConcatenation() {
+    doSelectionTest("""
+print <selection>'foo\\n' +
+      'bar'</selection>
+""", """
+print <selection>'''foo
+bar'''</selection>
+""")
+  }
+
+  void testGStringConcatenation() {
+    doSelectionTest('''
+print <selection>"foo${x}\\n" +
+      "bar"</selection>
+''', '''
+print <selection>"""foo${x}
+bar"""</selection>
+''')
   }
 }
