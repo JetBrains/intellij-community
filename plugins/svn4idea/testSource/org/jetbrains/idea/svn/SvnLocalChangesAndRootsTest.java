c5fef2b32e764e9e7264cdd2f56ff15d127c5bc9
irengrig <Irina.Chernushina@jetbrains.com>
2013-02-19 13:15:08 +0400

    Subversion: SvnLocalChangesAndRootsTest * 2 (now unused) - use new lock-surviving methods

--- plugins/svn4idea/testSource/org/jetbrains/idea/svn/SvnLocalChangesAndRootsTest.java
+++ plugins/svn4idea/testSource/org/jetbrains/idea/svn/SvnLocalChangesAndRootsTest.java
@@ -64,9 +64,9 @@ public class SvnLocalChangesAndRootsTest extends Svn17TestCase {
       myFile.createNewFile();
       sleep100();
 
-      verify(runSvn("import", "-m", "test", myDir.getAbsolutePath(), myRepoUrl + "/" + name));
+      runInAndVerifyIgnoreOutput("import", "-m", "test", myDir.getAbsolutePath(), myRepoUrl + "/" + name);
       FileUtil.delete(myDir);
-      verify(runSvn("co", myRepoUrl + "/" + name, myDir.getAbsolutePath()));
+      runInAndVerifyIgnoreOutput("co", myRepoUrl + "/" + name, myDir.getAbsolutePath());
 
       myUnversioned = new File(myDir, "unversioned.txt");
       myFile.createNewFile();
