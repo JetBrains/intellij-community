7979f1396a6e190f7f2dd6b702e923093b4c2989
Max Medvedev <maxim.medvedev@jetbrains.com>
2013-02-19 18:44:13 +0400

    IDEA-100072 Groovy: Code Completion for "default" keyword is missing. Don't show 'throws' in annotation methods

--- plugins/groovy/test/org/jetbrains/plugins/groovy/completion/KeywordCompletionTest.groovy
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/completion/KeywordCompletionTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -99,6 +99,7 @@ public class KeywordCompletionTest extends CompletionTestBase {
   void testCompletionInTupleVar(){ doTest() }
   void testAnnotationArg()       { doTest() }
   void testDefaultAnnotationArg(){ doTest() }
+  void testDefaultInAnnotation() { doTest() }
 
   String basePath = TestUtils.testDataPath + 'groovy/oldCompletion/keyword'
 
