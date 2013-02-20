0983dff0fce6fb01963da02d26f0a3511a3d0c39
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2013-02-20 16:12:42 +0400

    fixed PY-7883 Doctest: decrease severity for errors in doctest to warnings

---
+++ python/src/com/jetbrains/python/documentation/doctest/PyDocstringErrorFilter.java
@@ -0,0 +1,20 @@
+package com.jetbrains.python.documentation.doctest;
+
+import com.intellij.codeInsight.highlighting.HighlightErrorFilter;
+import com.intellij.lang.injection.InjectedLanguageManager;
+import com.intellij.psi.PsiErrorElement;
+import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * User : ktisha
+ *
+ * Do not highlight syntax errors in doctests
+ */
+public class PyDocstringErrorFilter extends HighlightErrorFilter {
+
+  public boolean shouldHighlightErrorElement(@NotNull final PsiErrorElement element) {
+    final PsiFile file = element.getContainingFile();
+    return !(InjectedLanguageManager.getInstance(element.getProject()).isInjectedFragment(file));
+  }
+}
