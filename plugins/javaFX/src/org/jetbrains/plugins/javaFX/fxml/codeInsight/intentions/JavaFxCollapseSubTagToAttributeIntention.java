70a2761955f8fa03e538de61c3592f77045eb1f7
anna <Anna.Kozlova@jetbrains.com>
2013-02-22 12:59:17 +0100

    javafx: collapse property tags to attributes (IDEA-100231)

---
+++ plugins/javaFX/src/org/jetbrains/plugins/javaFX/fxml/codeInsight/intentions/JavaFxCollapseSubTagToAttributeIntention.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2000-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jetbrains.plugins.javaFX.fxml.codeInsight.intentions;
+
+import com.intellij.codeInsight.CodeInsightUtilBase;
+import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.XmlElementFactory;
+import com.intellij.psi.xml.*;
+import com.intellij.util.IncorrectOperationException;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.javaFX.fxml.descriptors.JavaFxClassBackedElementDescriptor;
+import org.jetbrains.plugins.javaFX.fxml.descriptors.JavaFxPropertyElementDescriptor;
+
+/**
+ * User: anna
+ * Date: 2/22/13
+ */
+public class JavaFxCollapseSubTagToAttributeIntention extends PsiElementBaseIntentionAction{
+  @Override
+  public void invoke(@NotNull Project project, Editor editor, @NotNull PsiElement element) throws IncorrectOperationException {
+    if (!CodeInsightUtilBase.preparePsiElementsForWrite(element)) return;
+    final XmlTag tag = (XmlTag)element.getParent();
+    final XmlAttribute attribute = XmlElementFactory.getInstance(project).createXmlAttribute(tag.getName(), tag.getValue().getText().trim());
+    final XmlTag parentTag = tag.getParentTag();
+    parentTag.add(attribute);
+    tag.delete();
+  }
+
+  @Override
+  public boolean isAvailable(@NotNull Project project, Editor editor, @NotNull PsiElement element) {
+    if (element instanceof XmlToken && ((XmlToken)element).getTokenType() == XmlTokenType.XML_NAME && element.getParent() instanceof XmlTag) {
+      final XmlTag tag = (XmlTag)element.getParent();
+      if (tag.getSubTags().length == 0) {
+        final XmlTag parentTag = tag.getParentTag();
+        if (parentTag != null &&
+            tag.getDescriptor() instanceof JavaFxPropertyElementDescriptor &&
+            parentTag.getDescriptor() instanceof JavaFxClassBackedElementDescriptor) {
+  
+          setText("Collapse tag '" + tag.getName() + "' to attribute");
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  @NotNull
+  @Override
+  public String getFamilyName() {
+    return "Collapse tag to attribute";
+  }
+}
