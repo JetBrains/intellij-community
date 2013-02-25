edaaa853753148279cfb9a0eed704e896b9480c0
Konstantin Bulenkov <kb@jetbrains.com>
2013-02-25 15:17:51 +0100

    IDEA-101815 Splitter divider is white on Windows

--- platform/platform-impl/src/com/intellij/ide/ui/laf/LafManagerImpl.java
+++ platform/platform-impl/src/com/intellij/ide/ui/laf/LafManagerImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -344,7 +344,6 @@ public final class LafManagerImpl extends LafManager implements ApplicationCompo
     }
     else if (DarculaLookAndFeelInfo.CLASS_NAME.equals(lookAndFeelInfo.getClassName())) {
       DarculaLaf laf = new DarculaLaf();
-      MetalLookAndFeel.setCurrentTheme(new IdeaDarkMetalTheme());
       try {
         UIManager.setLookAndFeel(laf);
       }
