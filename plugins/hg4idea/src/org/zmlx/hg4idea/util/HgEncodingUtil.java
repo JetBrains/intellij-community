7c58cd4b768f8d57ceb95f29e94a43b4ec773faf
Mitsuhiro Koga <shiena.jp@gmail.com>
2013-03-10 13:46:48 +0900

    IDEA-98030 Encoding problem in comments in hg4idea plugin in IDEA
    The default encoding for Windows may be changed depending on the locale.
    For example, Japanese is cp932.
    And, if the HGENCODING is set to overrides the system default.

--- plugins/hg4idea/src/org/zmlx/hg4idea/util/HgEncodingUtil.java
+++ plugins/hg4idea/src/org/zmlx/hg4idea/util/HgEncodingUtil.java
@@ -9,32 +9,38 @@ import java.nio.charset.Charset;
  * @author Kirill Likhodedov
  */
 public class HgEncodingUtil {
-  
-  private static final String WINDOWS_DEFAULT_CHARSET = "cp1251";
-  private static final String UNIX_DEFAULT_CHARSET = "UTF-8";
+
+  private static final Charset DEFAULT_CHARSET;
+
+  static {
+    // next in line is HGENCODING in environment
+    String enc = System.getenv("HGENCODING");
+
+    // next is platform encoding as available in JDK
+    Charset cs = SystemInfo.isWindows ? Charset.defaultCharset() : Charset.forName("UTF-8");
+    try {
+      if (enc != null && enc.length() > 0 && Charset.isSupported(enc)) {
+        cs = Charset.forName(enc);
+      }
+    } catch (Exception e) {
+      cs = SystemInfo.isWindows ? Charset.defaultCharset() : Charset.forName("UTF-8");
+    } finally {
+      DEFAULT_CHARSET = cs;
+    }
+  }
 
   private HgEncodingUtil() {
   }
 
   /**
-   * Returns the default charset for Mercurial.
-   * It is cp1251 for Windows, and UTF-8 for Unix-like systems.
-   * The {@code HGENCODING} environment variable is not considered, because being set for IDEA it is inherited by the hg process
-   * spawned by IDEA.
-   * @return cp1251 for windows / UTF-8 for Unix-like systems.
+   * The default encoding which is used by the current environment.
+   * <b>Note</b>: Python's encoding isn't 1-1 with Charset.name() so do not store
+   * {@link java.nio.charset.Charset}.
+   *
+   * @return a valid encoding name, never null.
    */
   @NotNull
   public static Charset getDefaultCharset() {
-    return SystemInfo.isWindows ? getCharsetForNameOrDefault(WINDOWS_DEFAULT_CHARSET) : getCharsetForNameOrDefault(UNIX_DEFAULT_CHARSET);
-  }
-
-  @NotNull
-  private static Charset getCharsetForNameOrDefault(@NotNull String name) {
-    try {
-      return Charset.forName(name);
-    }
-    catch (Exception e) {
-      return Charset.defaultCharset();
-    }
+    return DEFAULT_CHARSET;
   }
 }
