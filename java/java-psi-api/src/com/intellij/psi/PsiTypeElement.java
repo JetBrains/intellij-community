30747cbf99b769d286a6c0eddee1dc8d5bfad78d
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2013-03-13 12:23:51 +0100

    Deprecation

--- java/java-psi-api/src/com/intellij/psi/PsiTypeElement.java
+++ java/java-psi-api/src/com/intellij/psi/PsiTypeElement.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -53,6 +53,7 @@ public interface PsiTypeElement extends PsiElement,PsiAnnotationOwner {
   @Nullable
   PsiJavaCodeReferenceElement getInnermostComponentReferenceElement();
 
+  /** @deprecated equals to getType() (to remove in IDEA 13) */
   PsiAnnotationOwner getOwner(PsiAnnotation annotation);
 
   PsiType getTypeNoResolve(@NotNull PsiElement context);
