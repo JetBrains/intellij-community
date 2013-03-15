e9519eaa4baf6a4991fdbad76c1f3e5768aa0e92
erokhins <smevok@yandex.ru>
2013-03-15 10:09:02 +0400

    (update UI)/2

--- swing_ui/src/org/hanuna/gitalk/swing_ui/frame/MainFrame.java
+++ swing_ui/src/org/hanuna/gitalk/swing_ui/frame/MainFrame.java
@@ -4,8 +4,6 @@ import org.hanuna.gitalk.swing_ui.UI_Utilities;
 import org.hanuna.gitalk.ui.UI_Controller;
 
 import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -18,9 +16,10 @@ public class MainFrame extends JFrame {
     private final UI_GraphTable graphTable;
     private final UI_RefTable refTable;
 
-    private final JTabbedPane tabs = new JTabbedPane(JTabbedPane.TOP);
-    private final JPanel leftGraphPanel = new JPanel();
+    private final JPanel topPanel = new JPanel();
+    private final JPanel tablePanel = new JPanel();
     private final JPanel mainPanel = new JPanel();
+
 
     public MainFrame(final UI_Controller ui_controller) {
         this.ui_controller = ui_controller;
@@ -34,66 +33,63 @@ public class MainFrame extends JFrame {
     }
 
 
-    private void packTabs() {
-        packLeftGraphPanel();
+    private void packTables() {
         JScrollPane graphScroll = new JScrollPane(graphTable);
-        JPanel graphPanel = new JPanel();
-        graphPanel.setLayout(new BoxLayout(graphPanel, BoxLayout.X_AXIS));
-        graphPanel.add(leftGraphPanel);
-        graphPanel.add(graphScroll);
-        tabs.add("graph", graphPanel);
+        tablePanel.setLayout(new BoxLayout(tablePanel, BoxLayout.X_AXIS));
+        tablePanel.add(graphScroll);
 
         JScrollPane branchScroll = new JScrollPane(refTable);
-        tabs.add("branches", branchScroll);
-
-        tabs.addChangeListener(new ChangeListener() {
+        refTable.addMouseListener(new MouseAdapter() {
             @Override
-            public void stateChanged(ChangeEvent e) {
-                if (tabs.getSelectedIndex() == 0) {
-                    ui_controller.updateVisibleBranches();
-                }
+            public void mouseClicked(MouseEvent e) {
+                ui_controller.updateVisibleBranches();
             }
         });
+        tablePanel.add(branchScroll);
     }
 
-    private void packLeftGraphPanel() {
-        leftGraphPanel.setLayout(new BoxLayout(leftGraphPanel, BoxLayout.PAGE_AXIS));
-        leftGraphPanel.setMaximumSize(new Dimension(10, 10000));
+    private void packTopGraphPanel() {
+        topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.LINE_AXIS));
+        topPanel.setMaximumSize(new Dimension(10000, 10));
 
-        JButton hideButton = new JButton("H");
+        JButton hideButton = new JButton("Hide");
         hideButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 ui_controller.hideAll();
             }
         });
-        leftGraphPanel.add(hideButton);
+        topPanel.add(hideButton);
 
-        JButton showButton = new JButton("S");
+        JButton showButton = new JButton("Show");
         showButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 ui_controller.showAll();
             }
         });
-        leftGraphPanel.add(showButton);
+        topPanel.add(showButton);
 
 
-        final JCheckBox visibleLongEdges = new JCheckBox("", false);
+        final JCheckBox visibleLongEdges = new JCheckBox("Show full patch", false);
         visibleLongEdges.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 ui_controller.setLongEdgeVisibility(visibleLongEdges.isSelected());
             }
         });
-        leftGraphPanel.add(visibleLongEdges);
+        topPanel.add(visibleLongEdges);
 
-        leftGraphPanel.add(Box.createVerticalGlue());
+        topPanel.add(Box.createHorizontalGlue());
     }
 
     private void packMainPanel() {
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
-        mainPanel.add(tabs);
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+
+        packTopGraphPanel();
+        mainPanel.add(topPanel);
+        mainPanel.add(tablePanel);
+
 
         setContentPane(mainPanel);
         pack();
@@ -103,7 +99,7 @@ public class MainFrame extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("GitAlk");
 
-        packTabs();
+        packTables();
         packMainPanel();
 
         Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
