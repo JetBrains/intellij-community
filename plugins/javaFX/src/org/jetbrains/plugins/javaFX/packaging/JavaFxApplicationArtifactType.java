ff1d5404181e3ca33abfbd586691a3a99c88bb1b
anna <Anna.Kozlova@jetbrains.com>
2013-03-15 17:29:12 +0100

    javafx artifact creation template: include module output (IDEA-103198)

--- plugins/javaFX/src/org/jetbrains/plugins/javaFX/packaging/JavaFxApplicationArtifactType.java
+++ plugins/javaFX/src/org/jetbrains/plugins/javaFX/packaging/JavaFxApplicationArtifactType.java
@@ -16,20 +16,30 @@
 package org.jetbrains.plugins.javaFX.packaging;
 
 import com.intellij.icons.AllIcons;
+import com.intellij.openapi.module.JavaModuleType;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleType;
+import com.intellij.openapi.roots.ui.configuration.ChooseModulesDialog;
+import com.intellij.packaging.artifacts.ArtifactTemplate;
 import com.intellij.packaging.artifacts.ArtifactType;
-import com.intellij.packaging.elements.CompositePackagingElement;
-import com.intellij.packaging.elements.PackagingElementFactory;
-import com.intellij.packaging.elements.PackagingElementOutputKind;
+import com.intellij.packaging.elements.*;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * User: anna
  * Date: 3/12/13
  */
 public class JavaFxApplicationArtifactType extends ArtifactType {
+  public static JavaFxApplicationArtifactType getInstance() {
+    return EP_NAME.findExtension(JavaFxApplicationArtifactType.class);
+  }
+  
   protected JavaFxApplicationArtifactType() {
     super("javafx", "JavaFx Application");
   }
@@ -51,8 +61,61 @@ public class JavaFxApplicationArtifactType extends ArtifactType {
   public CompositePackagingElement<?> createRootElement(@NotNull String artifactName) {
     return PackagingElementFactory.getInstance().createArchive(artifactName + ".jar");
   }
-  
-  public static JavaFxApplicationArtifactType getInstance() {
-    return EP_NAME.findExtension(JavaFxApplicationArtifactType.class);
+
+  @NotNull
+  @Override
+  public List<? extends ArtifactTemplate> getNewArtifactTemplates(@NotNull PackagingElementResolvingContext context) {
+    final List<Module> modules = new ArrayList<Module>();
+    for (Module module : context.getModulesProvider().getModules()) {
+      if (ModuleType.get(module) instanceof JavaModuleType) {
+        modules.add(module);
+      }
+    }
+    if (modules.isEmpty()) {
+      return Collections.emptyList();
+    }
+    return Collections.singletonList(new JavaFxArtifactTemplate(modules));
+  }
+
+  private class JavaFxArtifactTemplate extends ArtifactTemplate {
+    private final List<Module> myModules;
+
+    public JavaFxArtifactTemplate(List<Module> modules) {
+      myModules = modules;
+    }
+
+    @Override
+    public String getPresentableName() {
+      if (myModules.size() == 1) {
+        return "From module '" + myModules.get(0).getName() + "'";
+      }
+      return "From modules ...";
+    }
+
+    @Override
+    public NewArtifactConfiguration createArtifact() {
+      Module module = null;
+      if (myModules.size() == 1) {
+        module = myModules.get(0);
+      } else {
+        final ChooseModulesDialog dialog = new ChooseModulesDialog(myModules.get(0).getProject(), myModules,
+                                                                   "Select Module",
+                                                                   "Selected module output would to be included in the artifact");
+        dialog.setSingleSelectionMode();
+        dialog.show();
+        if (dialog.isOK()) {
+          final List<Module> elements = dialog.getChosenElements();
+          if (elements.isEmpty()) {
+            return null;
+          }
+          module = elements.get(0);
+        }
+      }
+      if (module == null) return null;
+      final CompositePackagingElement<?> rootElement = JavaFxApplicationArtifactType.this.createRootElement(module.getName());
+      final PackagingElement<?> moduleOutputElement = PackagingElementFactory.getInstance().createModuleOutput(module);
+      rootElement.addFirstChild(moduleOutputElement);
+      return new NewArtifactConfiguration(rootElement, module.getName(), JavaFxApplicationArtifactType.this);
+    }
   }
 }
