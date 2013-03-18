d123ff6bd2ce845b96103e6b5502b90ae345dc4f
anna <Anna.Kozlova@jetbrains.com>
2013-03-18 10:36:53 +0100

    javafx: allow include tags inside define (IDEA-103324) (cherry picked from commit b7ed509)

--- plugins/javaFX/javaFX-CE/testSrc/org/jetbrains/plugins/javaFX/fxml/JavaFXHighlightingTest.java
+++ plugins/javaFX/javaFX-CE/testSrc/org/jetbrains/plugins/javaFX/fxml/JavaFXHighlightingTest.java
@@ -209,6 +209,16 @@ public class JavaFXHighlightingTest extends DaemonAnalyzerTestCase {
     doDoTest(false, false);
   }
 
+  public void testWrongBindingType() throws Exception {
+    configureByFiles(null, getTestName(true) + ".fxml", getTestName(false) + ".java");
+    doDoTest(false, false);
+  }
+
+  public void testAllowIncludeTagInsideDefine() throws Exception {
+    configureByFiles(null, getTestName(true) + ".fxml", "btn.fxml");
+    doDoTest(false, false);
+  }
+
   public void testValueOfAcceptance() throws Exception {
     doTest();
   }
