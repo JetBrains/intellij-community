bfb7b50b18dfbe7e53954429b055cf3ba026a69d
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2013-03-27 13:50:39 +0400

    [hg] Fix compilation: has occasionally committed local changes :(

--- plugins/hg4idea/src/org/zmlx/hg4idea/ui/HgRepositorySelectorComponent.java
+++ plugins/hg4idea/src/org/zmlx/hg4idea/ui/HgRepositorySelectorComponent.java
@@ -64,22 +64,9 @@ public class HgRepositorySelectorComponent {
       return repo.getPresentableUrl();
     }
 
-
     @Override
-    public boolean equals(Object o) {
-      if (this == o) return true;
-      if (o == null || getClass() != o.getClass()) return false;
-
-      RepositoryDisplay display = (RepositoryDisplay)o;
-
-      if (repo != null ? !repo.equals(display.repo) : display.repo != null) return false;
-
-      return true;
-    }
-
-    @Override
-    public int hashCode() {
-      return repo != null ? repo.hashCode() : 0;
+    public boolean equals(Object obj) {
+      return obj instanceof RepositoryDisplay && this.repo.equals(((RepositoryDisplay)obj).repo);
     }
 
     @Override
