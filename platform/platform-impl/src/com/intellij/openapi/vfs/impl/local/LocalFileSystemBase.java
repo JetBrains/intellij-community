85189b3356ee392eea68c01b2e740d36a8c37fd5
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2013-03-29 16:05:08 +0400

    IDEA-103651 File is getting empty

--- platform/platform-impl/src/com/intellij/openapi/vfs/impl/local/LocalFileSystemBase.java
+++ platform/platform-impl/src/com/intellij/openapi/vfs/impl/local/LocalFileSystemBase.java
@@ -443,7 +443,9 @@ public abstract class LocalFileSystemBase extends LocalFileSystem {
   public byte[] contentsToByteArray(@NotNull final VirtualFile file) throws IOException {
     final FileInputStream stream = new FileInputStream(convertToIOFileAndCheck(file));
     try {
-      final int length = (int)file.getLength();
+      long l = file.getLength();
+      if (l > Integer.MAX_VALUE) throw new IOException("File is too large: " + l + ", " + file);
+      final int length = (int)l;
       if (length < 0) throw new IOException("Invalid file length: " + length + ", " + file);
       return FileUtil.loadBytes(stream, length);
     }
