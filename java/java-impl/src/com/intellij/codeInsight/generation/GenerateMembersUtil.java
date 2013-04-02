d7169811ce56ab0836ffe9ca37f7833567ef93cc
Max Medvedev <maxim.medvedev@jetbrains.com>
2013-04-02 13:33:10 +0400

    [rev=R. Shevchenko] IDEA-104162 Groovy: create constructor matching super should add corresponding imports (cherry picked from commit 3fe122e)

--- java/java-impl/src/com/intellij/codeInsight/generation/GenerateMembersUtil.java
+++ java/java-impl/src/com/intellij/codeInsight/generation/GenerateMembersUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -477,7 +477,7 @@ public class GenerateMembersUtil {
       else {
         JVMElementFactory factory = JVMElementFactories.requireFactory(targetParam.getLanguage(), targetParam.getProject());
         for (PsiAnnotation annotation : sourceModifierList.getAnnotations()) {
-          targetModifierList.add(factory.createAnnotationFromText(annotation.getText(), targetParam));
+          targetModifierList.add(factory.createAnnotationFromText(annotation.getText(), sourceParam));
         }
         for (@PsiModifier.ModifierConstant String m : PsiModifier.MODIFIERS) {
           targetModifierList.setModifierProperty(m, sourceParam.hasModifierProperty(m));
