2afab1786fbe7832fb48af1b44986b9236b1c990
Max Medvedev <maxim.medvedev@jetbrains.com>
2013-04-02 15:08:51 +0400

    testdata

--- plugins/groovy/testdata/refactoring/convertGroovyToJava/file/mutualCasts.java
+++ plugins/groovy/testdata/refactoring/convertGroovyToJava/file/mutualCasts.java
@@ -1,17 +1,17 @@
 public class X {
-  public void foo(java.util.List l) {
-    if (l instanceof java.lang.String){
-      if (l instanceof MyList){
-        org.codehaus.groovy.runtime.DefaultGroovyMethods.print(this, l);
-        java.lang.String v = ((MyList)l).getValue();
-      }
+public void foo(java.util.List l) {
+if (l instanceof java.lang.String){
+if (l instanceof MyList){
+org.codehaus.groovy.runtime.DefaultGroovyMethods.print(this, l);
+java.lang.String v = ((MyList)l).getValue();
+}
 
-    }
+}
 
-  }
+}
 
 }
 public class MyList {
-  public java.lang.String getValue() {return "";}
+public java.lang.String getValue() {return "";}
 
 }
