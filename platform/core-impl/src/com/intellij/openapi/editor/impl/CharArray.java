0590bfdb098eaea08e42d446b5858fd975d23266
Alexey Kudravtsev <cdr@intellij.com>
2013-04-04 12:48:17 +0400

    (test) performance: make IndentsPass call getCharsSequence less frequently

--- platform/core-impl/src/com/intellij/openapi/editor/impl/CharArray.java
+++ platform/core-impl/src/com/intellij/openapi/editor/impl/CharArray.java
@@ -16,7 +16,6 @@
 package com.intellij.openapi.editor.impl;
 
 import com.intellij.diagnostic.Dumpable;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.event.DocumentEvent;
 import com.intellij.openapi.util.text.StringUtil;
@@ -42,7 +41,6 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 abstract class CharArray implements CharSequenceBackedByArray, Dumpable {
   private static final Logger LOG = Logger.getInstance("#" + CharArray.class.getName());
-  private static final boolean CHECK = ApplicationManager.getApplication().isUnitTestMode();
 
   @SuppressWarnings("UseOfArchaicSystemPropertyAccessors")
   private static final boolean DISABLE_DEFERRED_PROCESSING = Boolean.getBoolean("idea.document.deny.deferred.changes");
@@ -186,7 +184,6 @@ abstract class CharArray implements CharSequenceBackedByArray, Dumpable {
   }
 
   private void assertConsistency() {
-    if (!CHECK) return;
     if (isDeferredChangeMode()) {
       assert myOriginalSequence == null;
     }
@@ -206,7 +203,8 @@ abstract class CharArray implements CharSequenceBackedByArray, Dumpable {
     if (myArray != null) {
       assert myCount <= myArray.length;
       seqFromCharArray = new CharArrayCharSequence(myArray, myStart, myCount);
-    } else {
+    }
+    else {
       seqFromCharArray = null;
     }
 
