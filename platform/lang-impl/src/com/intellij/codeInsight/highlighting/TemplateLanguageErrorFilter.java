3290fadbd3f28c521f5e2b1e83f110563a0bcb30
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2013-04-06 19:41:40 +0400

    Injected language error filter [CR-IU-88]

--- platform/lang-impl/src/com/intellij/codeInsight/highlighting/TemplateLanguageErrorFilter.java
+++ platform/lang-impl/src/com/intellij/codeInsight/highlighting/TemplateLanguageErrorFilter.java
@@ -16,11 +16,12 @@
 package com.intellij.codeInsight.highlighting;
 
 import com.intellij.lang.Language;
-import com.intellij.psi.FileViewProvider;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiErrorElement;
+import com.intellij.lang.injection.InjectedLanguageManager;
+import com.intellij.openapi.util.Key;
+import com.intellij.psi.*;
 import com.intellij.psi.tree.TokenSet;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
 
@@ -34,6 +35,8 @@ public abstract class TemplateLanguageErrorFilter extends HighlightErrorFilter {
   private final Class myTemplateFileViewProviderClass;
 
   private final Set<Language> knownLanguageSet;
+  
+  private final static Key<Class> TEMPLATE_VIEW_PROVIDER_CLASS_KEY = Key.create("TEMPLATE_VIEW_PROVIDER_CLASS");
 
   protected TemplateLanguageErrorFilter(
     final @NotNull TokenSet templateExpressionStartTokens,
@@ -63,20 +66,46 @@ public abstract class TemplateLanguageErrorFilter extends HighlightErrorFilter {
   }
 
   @Override
-  public boolean shouldHighlightErrorElement(@NotNull PsiErrorElement element) {
-    final FileViewProvider viewProvider = element.getContainingFile().getViewProvider();
-    if (!(viewProvider.getClass() == myTemplateFileViewProviderClass)) {
-      return true;
-    }
+  public boolean shouldHighlightErrorElement(@NotNull PsiErrorElement element) {        
     if (isKnownSubLanguage(element.getParent().getLanguage())) {
-      final PsiElement next = viewProvider.findElementAt(element.getTextOffset() + 1, viewProvider.getBaseLanguage());
-      if (next != null && myTemplateExpressionStartTokens.contains(next.getNode().getElementType())) {
-        return false;
+      //
+      // Immediately discard filters with non-matching template class if already known
+      //
+      Class templateClass = element.getUserData(TEMPLATE_VIEW_PROVIDER_CLASS_KEY);
+      if (templateClass != null && (templateClass != myTemplateFileViewProviderClass)) return true;
+      
+      PsiFile psiFile = element.getContainingFile();
+      int offset = element.getTextOffset();
+      InjectedLanguageManager injectedLanguageManager = InjectedLanguageManager.getInstance(element.getProject());
+      if (injectedLanguageManager.isInjectedFragment(psiFile)) {
+        PsiElement host = injectedLanguageManager.getInjectionHost(element);
+        if (host != null) {
+          psiFile = host.getContainingFile();
+          offset = injectedLanguageManager.injectedToHost(element, offset);
+        }
       }
+      final FileViewProvider viewProvider = psiFile.getViewProvider();
+      element.putUserData(TEMPLATE_VIEW_PROVIDER_CLASS_KEY, viewProvider.getClass());
+      if (!(viewProvider.getClass() == myTemplateFileViewProviderClass)) {
+        return true;
+      }
+      //
+      // An error can occur at template element or before it. Check both.
+      //
+      if (shouldIgnoreErrorAt(viewProvider, offset) || shouldIgnoreErrorAt(viewProvider, offset + 1)) return false;
     }
     return true;
   }
-
+  
+  private boolean shouldIgnoreErrorAt(@NotNull FileViewProvider viewProvider, int offset) {
+    PsiElement element = viewProvider.findElementAt(offset, viewProvider.getBaseLanguage());
+    if (element instanceof PsiWhiteSpace) element = element.getNextSibling();
+    if (element != null && myTemplateExpressionStartTokens.contains(element.getNode().getElementType())) {
+      return true;
+    }
+    return false;
+  }
+  
   protected boolean isKnownSubLanguage(final @NotNull Language language) {
     for (Language knownLanguage : knownLanguageSet) {
       if (language.is(knownLanguage)) {
