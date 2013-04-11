de7c575d52994372b94d8551156e8bd6542fe22b
Alexander Lobas <Alexander.Lobas@jetbrains.com>
2013-04-11 21:37:49 +0400

    Fix designer window location [fix-33053]

--- plugins/ui-designer-core/src/com/intellij/designer/DesignerToolWindowManager.java
+++ plugins/ui-designer-core/src/com/intellij/designer/DesignerToolWindowManager.java
@@ -156,7 +156,7 @@ public final class DesignerToolWindowManager extends AbstractToolWindowManager {
     });
 
     DesignerCustomizations customization = getCustomizations();
-    ToolWindowAnchor anchor = customization != null ? customization.getPaletteAnchor() : ToolWindowAnchor.LEFT;
+    ToolWindowAnchor anchor = customization != null ? customization.getStructureAnchor() : ToolWindowAnchor.LEFT;
 
     myToolWindow = ToolWindowManager.getInstance(myProject).registerToolWindow(DesignerBundle.message("designer.toolwindow.name"),
                                                                                false, anchor, myProject, true);
