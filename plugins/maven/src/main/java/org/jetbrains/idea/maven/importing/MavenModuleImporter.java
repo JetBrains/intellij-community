a139049bd837ed26ac776f1d0af048781bf91af2
Sergey Evdokimov <sergey.evdokimov@jetbrains.com>
2013-04-15 14:44:17 +0400

    Revert check artifact.getScope() == 'system' first +review CR-IC-699

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/importing/MavenModuleImporter.java
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/importing/MavenModuleImporter.java
@@ -151,25 +151,23 @@ public class MavenModuleImporter {
 
       DependencyScope scope = selectScope(artifact.getScope());
 
+      MavenProject depProject = myMavenTree.findProject(artifact.getMavenId());
+
+      if (depProject != null && !myMavenTree.isIgnored(depProject)) {
+        if (depProject == myMavenProject) continue;
+        boolean isTestJar = MavenConstants.TYPE_TEST_JAR.equals(artifact.getType()) || "tests".equals(artifact.getClassifier());
+        myRootModelAdapter.addModuleDependency(myMavenProjectToModuleName.get(depProject), scope, isTestJar);
+
+        Element buildHelperCfg = depProject.getPluginGoalConfiguration("org.codehaus.mojo", "build-helper-maven-plugin", "attach-artifact");
+        if (buildHelperCfg != null) {
+          addAttachArtifactDependency(buildHelperCfg, scope, depProject, artifact);
+        }
+      }
       if ("system".equals(artifact.getScope())) {
         myRootModelAdapter.addSystemDependency(artifact, scope);
       }
       else {
-        MavenProject depProject = myMavenTree.findProject(artifact.getMavenId());
-
-        if (depProject != null && !myMavenTree.isIgnored(depProject)) {
-          if (depProject == myMavenProject) continue;
-          boolean isTestJar = MavenConstants.TYPE_TEST_JAR.equals(artifact.getType()) || "tests".equals(artifact.getClassifier());
-          myRootModelAdapter.addModuleDependency(myMavenProjectToModuleName.get(depProject), scope, isTestJar);
-
-          Element buildHelperCfg = depProject.getPluginGoalConfiguration("org.codehaus.mojo", "build-helper-maven-plugin", "attach-artifact");
-          if (buildHelperCfg != null) {
-            addAttachArtifactDependency(buildHelperCfg, scope, depProject, artifact);
-          }
-        }
-        else {
-          myRootModelAdapter.addLibraryDependency(artifact, scope, myModifiableModelsProvider, myMavenProject);
-        }
+        myRootModelAdapter.addLibraryDependency(artifact, scope, myModifiableModelsProvider, myMavenProject);
       }
     }
 
