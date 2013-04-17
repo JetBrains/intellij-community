adeb8c15a554a3a5bb654b06e6f30a306c6e20fe
anna <Anna.Kozlova@jetbrains.com>
2013-04-17 15:55:43 +0200

    extract method: duplicates processing fixed (IDEA-105562) (cherry picked from commit ceb9354f15afd5fa30d4b320bc13d8a2fcabca35)

--- java/java-tests/testSrc/com/intellij/refactoring/ExtractMethodTest.java
+++ java/java-tests/testSrc/com/intellij/refactoring/ExtractMethodTest.java
@@ -579,6 +579,10 @@ public class ExtractMethodTest extends LightCodeInsightTestCase {
     doDuplicatesTest();
   }
 
+  public void testArrayReturnType() throws Exception {
+    doDuplicatesTest();
+  }
+
   private void doTestDisabledParam() throws PrepareFailedException {
     final CodeStyleSettings settings = CodeStyleSettingsManager.getSettings(getProject());
     settings.ELSE_ON_NEW_LINE = true;
