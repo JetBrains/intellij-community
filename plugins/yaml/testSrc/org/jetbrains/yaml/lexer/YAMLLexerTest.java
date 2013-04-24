2c90af3c8b469c6f2ff305f4468011fe2ecb55a6
Oleg Sukhodolsky <oleg.sukhodolsky@jetbrains.com>
2013-04-24 12:12:32 +0400

    Refactoring: moving yaml lexer tests' date to files

--- plugins/yaml/testSrc/org/jetbrains/yaml/lexer/YAMLLexerTest.java
+++ plugins/yaml/testSrc/org/jetbrains/yaml/lexer/YAMLLexerTest.java
@@ -15,199 +15,98 @@ public class YAMLLexerTest extends LexerTestCase {
   }
 
   public void test2docs() {
-    doTest("# Ranking of 1998 home runs\n" +
-           "---\n" +
-           "- Mark McGwire\n" +
-           "- Sammy Sosa\n" +
-           "- Ken Griffey\n" +
-           "\n" +
-           "# Team ranking\n" +
-           "---\n" +
-           "- Chicago Cubs\n" +
-           "- St Louis Cardinals");
+    doFileTest("yml");
   }
 
   public void testColorspage(){
-    doTest("# Read about fixtures at http://ar.rubyonrails.org/classes/Fixtures.html\n" +
-           "static_sidebar:\n" +
-           "  id: \"foo\"\n" +
-           "  name: 'side_bar'\n" +
-           "  staged_position: 1\n" +
-           "  blog_id: 1\n" +
-           "  config: |+\n" +
-           "    --- !map:HashWithIndifferentAccess\n" +
-           "      title: Static Sidebar\n" +
-           "      body: The body of a static sidebar\n" +
-           "  type: StaticSidebar\n" +
-           "  type: > some_type_here");
-
+    doFileTest("yml");
   }
 
   public void testDocuments(){
-    doTest("---\n" +
-           "RubyMine Develoers:\n" +
-           "    name: yole\n" +
-           "    name: romeo\n" +
-           "    name: den\n" +
-           "    name: oleg\n" +
-           "---\n" +
-           "RubyMine Versions:\n" +
-           "    - 1.0\n" +
-           "    - 1.1\n" +
-           "    - 1.1.1\n" +
-           "    - 1.5 EAP\n" +
-           "    - 1.5");
+    doFileTest("yml");
   }
 
   public void testIndentation(){
-    doTest("name: Mark McGwire\n" +
-           "accomplishment: >\n" +
-           "  Mark set a major league\n" +
-           "  home run record in 1998.\n" +
-           "stats: |\n" +
-           "  65 Home Runs\n" +
-           "  0.278 Batting Average");
+    doFileTest("yml");
   }
 
-
   public void testMap_between_seq(){
-    doTest("? - Detroit Tigers\n" +
-           "  - Chicago cubs\n" +
-           ":\n" +
-           "  - 2001-07-23\n" +
-           "\n" +
-           "? [ New York Yankees,\n" +
-           "    Atlanta Braves ]\n" +
-           ": [ 2001-07-02, 2001-08-12,\n" +
-           "    2001-08-14 ]");
+    doFileTest("yml");
   }
 
   public void testMap_map(){
-    doTest("Mark McGwire: {hr: 65, avg: 0.278}\n" +
-           "Sammy Sosa: {\n" +
-           "    hr: 63,\n" +
-           "    avg: 0.288\n" +
-           "  }");
+    doFileTest("yml");
   }
 
   public void testQuoted_scalars(){
-    doTest("unicode: \"Sosa did fine.\\u263A\"\n" +
-           "control: \"\\b1998\\t1999\\t2000\\n\"\n" +
-           "hex esc: \"\\x0d\\x0a is \\r\\n\"\n" +
-           "\n" +
-           "single: '\"Howdy!\" he cried.'\n" +
-           "quoted: ' # not a ''comment''.'\n" +
-           "tie-fighter: '|\\-*-/|'\n" +
-           "foo: ['a', 'b']\n" +
-           "key: 'value\\'\n" +
-           "key2: 'value'");
+    doFileTest("yml");
   }
 
   public void testSample_log(){
-    doTest("Date: 2001-11-23\n" +
-           "Stack:\n" +
-           "  - file: TopClass.py\n" +
-           "    line: 23\n" +
-           "    code: |\n" +
-           "      x = MoreObject(\"345\\n\")\n" +
-           "  - file: MoreClass.py\n" +
-           "    line: 58\n" +
-           "    code: |-\n" +
-           "      foo = bar");
+    doFileTest("yml");
   }
 
   public void testSeq_seq(){
-    doTest("- [name        , hr, avg  ]\n" +
-           "- [Mark McGwire, 65, 0.278]\n" +
-           "- [Sammy Sosa  , 63, 0.288]");
+    doFileTest("yml");
   }
 
   public void testSequence_mappings(){
-    doTest("-\n" +
-           "  name: Mark McGwire\n" +
-           "  hr:   65\n" +
-           "  avg:  0.278\n" +
-           "-\n" +
-           "  name: Sammy Sosa\n" +
-           "  hr:   63\n" +
-           "  avg:  0.288");
+    doFileTest("yml");
   }
 
   public void testWrong_string_highlighting(){
-    doTest("status:\n" +
-           "    draft: \"Brouillon\"  \n" +
-           "    reviewed: \"Revise\"\n" +
-           "    hidden: \"Cache\"\n" +
-           "    published: \"Public\"");
+    doFileTest("yml");
   }
 
   public void testValue_injection(){
-    doTest("key:\n" + "    one: 1 text\n" + "    other: {{count}} text");
+    doFileTest("yml");
   }
 
   public void testValue_injection_2(){
-    doTest("key:\n" + "    one: 1 text\n" + "    other: some {{count}} text");
+    doFileTest("yml");
   }
 
   public void testComma(){
-    doTest("key: Hello, how are you?");
+    doFileTest("yml");
   }
 
   public void testIndex(){
-    doTest("indexes:\n" +
-           "\n" +
-           "- kind: MessageIndex\n" +
-           "  properties:\n" +
-           "  - name: group_name\n" +
-           "  - name: created_at\n" +
-           "    direction: desc");
+    doFileTest("yml");
   }
 
   public void testKeydots(){
-    doTest("my.testevent.pre:\n" +
-           "  type:     sfEvent");
-
+    doFileTest("yml");
   }
 
   public void testColons74100(){
-    doTest("server1: http://localhost:9876\n" +
-           "server2:\n" +
-           "  http://localhost:8765\n" +
-           "server3: test");
-
+    doFileTest("yml");
   }
 
   public void testOnlyyamlkey(){
-    doTest("foo:");
+    doFileTest("yml");
   }
 
   public void testKey_parens(){
-    doTest("comment(c):");
+    doFileTest("yml");
   }
 
   public void testKey_trailing_space(){
-    doTest("foo   :");
+    doFileTest("yml");
   }
 
   public void testComments(){
-    doTest("en:\n" +
-           "  aaa: \"AAA\"\n" +
-           "#  bbb:\n" +
-           "#    ccc: \"CCC\"\n" +
-           "installation:\n" +
-           "  name: [] # todo: \"Not specified\"\n" +
-           "en #comment: TODO");
+    doFileTest("yml");
   }
 
   public void testNon_comment() {
-    doTest("hello: hello#my_non_comment");
+    doFileTest("yml");
   }
 
   public void testNon_comment2() {
-    doTest("foo#non_comment: text");
+    doFileTest("yml");
   }
 
   public void testKey_with_brackets() {
-    doTest("foo[in_brackets]: text");
+    doFileTest("yml");
   }
 }
