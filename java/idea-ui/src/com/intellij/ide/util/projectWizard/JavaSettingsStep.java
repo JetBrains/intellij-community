21a063e67661c6170c557be9d85ec837fea6924c
Denis.Zhdanov <Denis.Zhdanov@jetbrains.com>
2013-04-25 18:50:47 +0400

    IDEA-106148 Allow user to set a default value for "source root path" to override current default of "src" in New Project dialog

--- java/idea-ui/src/com/intellij/ide/util/projectWizard/JavaSettingsStep.java
+++ java/idea-ui/src/com/intellij/ide/util/projectWizard/JavaSettingsStep.java
@@ -17,6 +17,7 @@ package com.intellij.ide.util.projectWizard;
 
 import com.intellij.ide.IdeBundle;
 import com.intellij.ide.util.BrowseFilesListener;
+import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.projectRoots.SdkTypeId;
 import com.intellij.openapi.ui.ComponentWithBrowseButton;
@@ -27,6 +28,7 @@ import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.components.JBCheckBox;
+import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.TestOnly;
 
@@ -42,13 +44,17 @@ import java.util.Collections;
  */
 public class JavaSettingsStep extends SdkSettingsStep {
 
-  private final ModuleBuilder myModuleBuilder;
-  private JBCheckBox myCreateSourceRoot;
-  private TextFieldWithBrowseButton mySourcePath;
-  private JPanel myPanel;
+  @NotNull @NonNls private static final String MODULE_SOURCE_ROOT_KEY = "java.module.default.source.root";
+  @NotNull @NonNls private static final String DEFAULT_MODULE_SOURCE_ROOT_PATH = "src";
+
+  private final ModuleBuilder             myModuleBuilder;
+  private       JBCheckBox                myCreateSourceRoot;
+  private       TextFieldWithBrowseButton mySourcePath;
+  private       JPanel                    myPanel;
 
   public JavaSettingsStep(SettingsStep settingsStep, ModuleBuilder moduleBuilder, @NotNull Condition<SdkTypeId> sdkFilter) {
     super(settingsStep, moduleBuilder, sdkFilter);
+    mySourcePath.setText(PropertiesComponent.getInstance().getValue(MODULE_SOURCE_ROOT_KEY, DEFAULT_MODULE_SOURCE_ROOT_PATH));
     myModuleBuilder = moduleBuilder;
 
     if (moduleBuilder instanceof JavaModuleBuilder) {
@@ -61,7 +67,8 @@ public class JavaSettingsStep extends SdkSettingsStep {
     ComponentWithBrowseButton.BrowseFolderActionListener<JTextField> listener =
       new ComponentWithBrowseButton.BrowseFolderActionListener<JTextField>(
         IdeBundle.message("prompt.select.source.directory"), null, mySourcePath, project, BrowseFilesListener.SINGLE_DIRECTORY_DESCRIPTOR,
-        TextComponentAccessor.TEXT_FIELD_WHOLE_TEXT) {
+        TextComponentAccessor.TEXT_FIELD_WHOLE_TEXT)
+      {
         @Override
         protected void onFileChoosen(VirtualFile chosenFile) {
           String contentEntryPath = myModuleBuilder.getContentEntryPath();
@@ -94,6 +101,7 @@ public class JavaSettingsStep extends SdkSettingsStep {
         String contentEntryPath = myModuleBuilder.getContentEntryPath();
         if (contentEntryPath != null) {
           final String dirName = mySourcePath.getText().trim().replace(File.separatorChar, '/');
+          PropertiesComponent.getInstance().setValue(MODULE_SOURCE_ROOT_KEY, dirName);
           String text = dirName.length() > 0? contentEntryPath + "/" + dirName : contentEntryPath;
           ((JavaModuleBuilder)myModuleBuilder).setSourcePaths(Collections.singletonList(Pair.create(text, "")));
         }
