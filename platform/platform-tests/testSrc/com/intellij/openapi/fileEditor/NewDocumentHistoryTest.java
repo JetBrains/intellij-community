99a2216bad3ecda97eabfa5b1e624c79a2c10085
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2013-04-30 15:55:49 +0400

    tests fixed

--- platform/platform-tests/testSrc/com/intellij/openapi/fileEditor/NewDocumentHistoryTest.java
+++ platform/platform-tests/testSrc/com/intellij/openapi/fileEditor/NewDocumentHistoryTest.java
@@ -1,26 +1,21 @@
 package com.intellij.openapi.fileEditor;
 
 import com.intellij.codeInsight.navigation.NavigationUtil;
-import com.intellij.openapi.components.impl.ComponentManagerImpl;
 import com.intellij.openapi.fileEditor.ex.IdeDocumentHistory;
-import com.intellij.openapi.fileEditor.impl.EditorHistoryManager;
-import com.intellij.openapi.fileEditor.impl.FileEditorManagerImpl;
-import com.intellij.openapi.fileEditor.impl.IdeDocumentHistoryImpl;
-import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.testFramework.PlatformLangTestCase;
+import com.intellij.psi.PsiManager;
+import com.intellij.testFramework.PlatformTestCase;
 import com.intellij.testFramework.PlatformTestUtil;
-import com.intellij.ui.docking.DockManager;
-
-import java.io.File;
 
 /**
  * @author Dmitry Avdeev
  *         Date: 4/29/13
  */
-public class NewDocumentHistoryTest extends PlatformLangTestCase {
+public class NewDocumentHistoryTest extends HeavyFileEditorManagerTestCase {
 
-  private FileEditorManagerImpl myManager;
+  public NewDocumentHistoryTest() {
+    PlatformTestCase.initPlatformLangPrefix();
+  }
 
   public void testBackNavigationBetweenEditors() throws Exception {
     PlatformTestUtil.registerExtension(FileEditorProvider.EP_FILE_EDITOR_PROVIDER, new FileEditorManagerTest.MyFileEditorProvider(), getTestRootDisposable());
@@ -42,22 +37,9 @@ public class NewDocumentHistoryTest extends PlatformLangTestCase {
     myManager.openFile(file1, true);
     VirtualFile file2 = getFile("/src/2.txt");
     myManager.openFile(file2, true);
-    NavigationUtil.activateFileWithPsiElement(getPsiManager().findFile(file1));
+    NavigationUtil.activateFileWithPsiElement(PsiManager.getInstance(getProject()).findFile(file1));
     VirtualFile[] files = myManager.getSelectedFiles();
     assertEquals(1, files.length);
     assertEquals("1.txt", files[0].getName());
-  }
-
-  protected VirtualFile getFile(String path) {
-    return LocalFileSystem.getInstance().refreshAndFindFileByPath(
-      PlatformTestUtil.getCommunityPath().replace(File.separatorChar, '/') + "/platform/platform-tests/testData/fileEditorManager" + path);
-  }
-
-  public void setUp() throws Exception {
-    super.setUp();
-    myManager = new FileEditorManagerImpl(getProject(), DockManager.getInstance(getProject()));
-    ((ComponentManagerImpl)getProject()).registerComponentInstance(FileEditorManager.class, myManager);
-    ((IdeDocumentHistoryImpl)IdeDocumentHistory.getInstance(getProject())).projectOpened();
-    EditorHistoryManager.getInstance(getProject()).projectOpened();
   }
 }
