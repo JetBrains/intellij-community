9db22de85c2f59f3fffbba4abd5e6b73218a0444
anna <Anna.Kozlova@jetbrains.com>
2013-05-02 14:35:36 +0200

    restore after merge

--- java/java-psi-api/src/com/intellij/psi/util/TypeConversionUtil.java
+++ java/java-psi-api/src/com/intellij/psi/util/TypeConversionUtil.java
@@ -896,6 +896,9 @@ public class TypeConversionUtil {
     PsiSubstitutor leftSubstitutor = leftResult.getSubstitutor();
 
     if (!leftClass.getManager().areElementsEquivalent(leftClass, rightClass)) {
+      if (!allowUncheckedConversion && PsiUtil.isRawSubstitutor(leftClass, leftSubstitutor) && !rightClass.hasTypeParameters() && !(rightClass instanceof PsiTypeParameter)) {
+        return false;
+      }
       rightSubstitutor = getSuperClassSubstitutor(leftClass, rightClass, rightSubstitutor);
       rightClass = leftClass;
     }
