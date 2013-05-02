abfb8f08d9a0d7d15fb16588b24e5728ed621a92
anna <Anna.Kozlova@jetbrains.com>
2013-05-02 12:46:32 +0200

    testdata for IDEA-57482

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/GenericsHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/GenericsHighlightingTest.java
@@ -212,6 +212,7 @@ public class GenericsHighlightingTest extends LightDaemonAnalyzerTestCase {
   public void testIDEA67677(){ doTest5(false); }
   public void testIDEA67798(){ doTest5(false); }
   public void testIDEA57534(){ doTest5(false); }
+  public void testIDEA57482(){ doTest5(false); }
 
   public void testWildcardsOnRawTypes() { doTest5(false); }
   public void testDisableWithinBoundsCheckForSuperWildcards() {
