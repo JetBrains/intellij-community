ae2a45724a86fd72265b9c35ff1fd5aee5ca1c08
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2013-05-02 15:53:09 +0200

    Testdata supplemented with new cases

--- java/java-tests/testData/psi/cls/mirror/src/pkg/Booleans.java
+++ java/java-tests/testData/psi/cls/mirror/src/pkg/Booleans.java
@@ -1,9 +1,13 @@
 package pkg;
 
 class Booleans  {
-  public static final boolean RELEASE = true;
-  public static final boolean DEBUG = false;
-  public static final boolean PROFILE = false;
-  public static final boolean LOGV = false;
-  public static final boolean LOGD = true;
+  public static final boolean TRUE = true;
+  public static final boolean FALSE = false;
+
+  @BooleanAnno(true) public static boolean TRUE() { return TRUE; }
+  @BooleanAnno(false) public static boolean FALSE() { return FALSE; }
+}
+
+@interface BooleanAnno {
+  boolean value();
 }
