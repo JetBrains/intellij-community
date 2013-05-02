6a77a1aadde3b5e1d0db0fba717c7c6951c439e7
Max Medvedev <maxim.medvedev@jetbrains.com>
2013-05-02 18:55:59 +0400

    IDEA-106598 Groovy: good code is red. Missing return statement inspection in with closure that ends with an assert

--- plugins/groovy/src/org/jetbrains/plugins/groovy/codeInspection/noReturnMethod/MissingReturnInspection.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/codeInspection/noReturnMethod/MissingReturnInspection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -137,7 +137,7 @@ public class MissingReturnInspection extends GroovySuppressableInspectionTool {
           }
         }
         else if (instruction instanceof ThrowingInstruction) {
-          sometimesHaveReturn.set(true);
+          //skip throwing instructions
         }
         else if (element instanceof GrAssertStatement) {
           sometimesHaveReturn.set(true);
