a2525d9de20eac9c303c368057bce282dd0a2bc7
anna <Anna.Kozlova@jetbrains.com>
2013-05-03 11:56:16 +0200

    testdata for IDEA-67998

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/GenericsHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/GenericsHighlightingTest.java
@@ -220,6 +220,7 @@ public class GenericsHighlightingTest extends LightDaemonAnalyzerTestCase {
   public void testIDEA57272(){ doTest5(false); }
   public void testIDEA57285(){ doTest5(false); }
   public void testIDEA65066(){ doTest5(false); }
+  public void testIDEA67998(){ doTest5(false); }
   public void testIDEA67571(){ doTest(LanguageLevel.JDK_1_7, JavaSdkVersion.JDK_1_7, false); }
 
   public void testWildcardsOnRawTypes() { doTest5(false); }
