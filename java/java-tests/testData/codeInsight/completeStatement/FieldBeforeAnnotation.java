a232670766a4a70e39705ee861678adf2b25b52b
peter <peter@jetbrains.com>
2013-05-07 16:19:33 +0200

    tests for IDEA-105010 Action "Complete current statement" doesn't work on fields if next field is annotated

---
+++ java/java-tests/testData/codeInsight/completeStatement/FieldBeforeAnnotation.java
@@ -0,0 +1,5 @@
+public class Test {
+    private String s
+
+    @Deprecated private String foo;
+}
---
+++ java/java-tests/testData/codeInsight/completeStatement/MethodBeforeAnnotation_after.java
@@ -0,0 +1,7 @@
+public class Test {
+    private String s() {
+        <caret>
+    }
+
+    @Deprecated private String foo;
+}
