46f5e570a0ffff0dce1350d6e77a70aea99689ff
anna <Anna.Kozlova@jetbrains.com>
2013-05-07 10:27:02 +0200

    testdata adjusted

--- plugins/javaFX/testData/quickfix/CreateControllerMethod_after.java
+++ plugins/javaFX/testData/quickfix/CreateControllerMethod_after.java
@@ -2,6 +2,6 @@ import javafx.event.ActionEvent;
 
 public class CreateControllerMethod {
     public void bar(ActionEvent actionEvent) {
-        //To change body of created methods use File | Settings | File Templates.
+
     }
 }
