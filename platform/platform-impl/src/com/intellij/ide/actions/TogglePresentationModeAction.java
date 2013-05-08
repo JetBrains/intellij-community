57f9bf9bf8d5a93f842982fe2cecce211f06d5dc
Konstantin Bulenkov <kb@jetbrains.com>
2013-05-08 18:00:54 +0200

    save full screen state and reinit editor properly

--- platform/platform-impl/src/com/intellij/ide/actions/TogglePresentationModeAction.java
+++ platform/platform-impl/src/com/intellij/ide/actions/TogglePresentationModeAction.java
@@ -1,8 +1,25 @@
+/*
+ * Copyright 2000-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.intellij.ide.actions;
 
 import com.intellij.ide.ui.UISettings;
+import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.editor.ex.util.EditorUtil;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.impl.IdeFrameImpl;
@@ -34,8 +51,17 @@ public class TogglePresentationModeAction extends AnAction implements DumbAware 
     if (project != null) {
       Window frame = IdeFrameImpl.getActiveFrame();
       if (frame instanceof IdeFrameImpl) {
-        ((IdeFrameImpl)frame).toggleFullScreen(true);
+        final PropertiesComponent propertiesComponent = PropertiesComponent.getInstance(project);
+        if (settings.PRESENTATION_MODE) {
+          propertiesComponent.setValue("full.screen.before.presentation.mode", String.valueOf(((IdeFrameImpl)frame).isInFullScreen()));
+          ((IdeFrameImpl)frame).toggleFullScreen(true);
+        } else {
+          final String value = propertiesComponent.getValue("full.screen.before.presentation.mode");
+          ((IdeFrameImpl)frame).toggleFullScreen("true".equalsIgnoreCase(value));
+        }
       }
     }
+
+    EditorUtil.reinitSettings();
   }
 }
