b0ec35852a6b1d2be1f38c27bd6bc1683c4d3e31
Vassiliy Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2013-05-08 13:09:49 +0400

    Rollback

--- platform/platform-api/src/com/intellij/ui/DoubleClickListener.java
+++ platform/platform-api/src/com/intellij/ui/DoubleClickListener.java
@@ -19,40 +19,13 @@
  */
 package com.intellij.ui;
 
-import javax.swing.*;
-import javax.swing.plaf.basic.BasicTreeUI;
-import javax.swing.tree.TreePath;
 import java.awt.event.MouseEvent;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 public abstract class DoubleClickListener extends ClickListener {
   @Override
   public final boolean onClick(MouseEvent event, int clickCount) {
     if (clickCount == 2 && event.getButton() == MouseEvent.BUTTON1) {
-      return isExpandIconClick(event) ? false : onDoubleClick(event);
-    }
-    return false;
-  }
-
-  private static boolean isExpandIconClick(MouseEvent event) {
-    if (event.getComponent() instanceof JTree) {
-      JTree tree = (JTree)event.getComponent();
-      TreePath treePath = tree.getClosestPathForLocation(event.getX(), event.getY());
-      if (treePath == null || !(tree.getUI() instanceof BasicTreeUI)) return false;
-      BasicTreeUI ui = (BasicTreeUI)tree.getUI();
-      try {
-        Method method = BasicTreeUI.class.getDeclaredMethod("isLocationInExpandControl", TreePath.class, Integer.TYPE, Integer.TYPE);
-        method.setAccessible(true);
-        Object result = method.invoke(ui, treePath, event.getX(), event.getY());
-        if (result instanceof Boolean && result.equals(Boolean.TRUE)) return true;
-      }
-      catch (NoSuchMethodException e) {
-      }
-      catch (InvocationTargetException e) {
-      }
-      catch (IllegalAccessException e) {
-      }
+      return onDoubleClick(event);
     }
     return false;
   }
