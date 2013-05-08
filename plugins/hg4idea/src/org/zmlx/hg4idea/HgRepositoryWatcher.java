cd7ceff90224f2775c09f2d114375ec4b2ce4ab6
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2013-05-08 17:02:00 +0200

    IDEA-106835 (bulk remove watch roots)

--- plugins/hg4idea/src/org/zmlx/hg4idea/HgRepositoryWatcher.java
+++ plugins/hg4idea/src/org/zmlx/hg4idea/HgRepositoryWatcher.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -65,9 +65,7 @@ public class HgRepositoryWatcher extends AbstractProjectComponent implements Bul
 
   @Override
   public void disposeComponent() {
-    for (LocalFileSystem.WatchRequest request : myWatchRequests) {
-      LocalFileSystem.getInstance().removeWatchedRoot(request);
-    }
+    LocalFileSystem.getInstance().removeWatchedRoots(myWatchRequests);
     myWatchRequests.clear();
     myRoots.clear();
   }
