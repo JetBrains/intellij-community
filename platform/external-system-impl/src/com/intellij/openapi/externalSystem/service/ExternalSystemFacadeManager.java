a64ed37bcae11bf47d09e2c78cbf3885b399520b
Denis.Zhdanov <Denis.Zhdanov@jetbrains.com>
2013-05-11 16:26:51 +0400

    IDEA-104500 Gradle: Allow to reuse common logic for other external systems
    Add *.jar file with external system-specific project resolver class to the classpath

--- platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/ExternalSystemFacadeManager.java
+++ platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/ExternalSystemFacadeManager.java
@@ -176,6 +176,7 @@ public class ExternalSystemFacadeManager {
         if (externalSystemId != null) {
           ExternalSystemManager<?, ?, ?, ?, ?> manager = ExternalSystemApiUtil.getManager(externalSystemId);
           if (manager != null) {
+            params.getClassPath().add(PathUtil.getJarPathForClass(manager.getProjectResolverClass().getClass()));
             params.getProgramParametersList().add(manager.getProjectResolverClass().getName());
             params.getProgramParametersList().add(manager.getBuildManagerClass().getName());
             manager.enhanceParameters(params);
