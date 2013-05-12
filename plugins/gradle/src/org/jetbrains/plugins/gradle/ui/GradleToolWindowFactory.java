0072f5f3f4f74e7784d6f6415107c3581cec5f34
Denis.Zhdanov <Denis.Zhdanov@jetbrains.com>
2013-05-12 23:03:08 +0400

    IDEA-104500 Gradle: Allow to reuse common logic for other external systems
    Start rewriting tasks to the generic external system api

--- plugins/gradle/src/org/jetbrains/plugins/gradle/ui/GradleToolWindowFactory.java
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/ui/GradleToolWindowFactory.java
@@ -1,6 +1,6 @@
 package org.jetbrains.plugins.gradle.ui;
 
-import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil;
+import com.intellij.openapi.externalSystem.service.task.ExternalSystemTasksPanel;
 import com.intellij.openapi.externalSystem.util.ExternalSystemBundle;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
@@ -8,7 +8,6 @@ import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.ContentManager;
 import com.intellij.ui.content.impl.ContentImpl;
-import org.jetbrains.plugins.gradle.tasks.GradleTasksPanel;
 import org.jetbrains.plugins.gradle.util.GradleBundle;
 
 public class GradleToolWindowFactory implements ToolWindowFactory, DumbAware {
@@ -18,7 +17,7 @@ public class GradleToolWindowFactory implements ToolWindowFactory, DumbAware {
     toolWindow.setTitle(GradleBundle.message("gradle.name"));
     ContentManager contentManager = toolWindow.getContentManager();
     String tasksTitle = ExternalSystemBundle.message("tool.window.title.tasks");
-    ContentImpl tasksContent = new ContentImpl(new GradleTasksPanel(project), tasksTitle, true);
+    ContentImpl tasksContent = new ContentImpl(new ExternalSystemTasksPanel(), tasksTitle, true);
     contentManager.addContent(tasksContent);
   }
 }
