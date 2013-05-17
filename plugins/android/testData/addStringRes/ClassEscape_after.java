304185136849f097bf150736bc14b6dfec72f0ad
Dmitry Jemerov <yole@jetbrains.com>
2013-05-17 16:21:35 +0200

    remove old version of Android plugin (current code lives at https://android.googlesource.com/platform/tools/adt/idea/)

--- plugins/android/testData/addStringRes/ClassEscape_after.java
+++
@@ -1,9 +0,0 @@
-package p1.p2;
-
-import android.content.Context;
-
-public class ClassEscape {
-  public static void f(Context context) {
-    String s = context.getString(R.string.hello);
-  }
-}
--- plugins/android/testData/dom/layout/MyFragmentActivity.java
+++
@@ -1,6 +0,0 @@
-package p1.p2;
-
-public class MyFragmentActivity extends android.app.Activity {
-  public static class MyFragment extends android.app.Fragment {
-  }
-}
