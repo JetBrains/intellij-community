b4864fb92055a5289ba7a5ee381b4bdb083e44a6
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2013-05-22 20:32:19 +0200

    API simplification (config path already exists in all these case)

--- platform/platform-impl/src/com/intellij/openapi/components/impl/stores/FileBasedStorage.java
+++ platform/platform-impl/src/com/intellij/openapi/components/impl/stores/FileBasedStorage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -71,7 +71,7 @@ public class FileBasedStorage extends XmlElementStorage {
 
     if (!myConfigDirectoryRefreshed && (app.isUnitTestMode() || app.isDispatchThread())) {
       try {
-        syncRefreshPathRecursively(PathManager.getConfigPath(true), "componentVersions");
+        syncRefreshPathRecursively(PathManager.getConfigPath(), "componentVersions");
       }
       finally {
         //noinspection AssignmentToStaticFieldFromInstanceMethod
