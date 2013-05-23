874d4f7000f16338a89a01a79e0bfa6a7516cc45
Andrey Breslav <andrey.breslav@jetbrains.com>
2013-05-23 21:08:45 +0400

    Action framework

--- plugins/git4idea/GitAlk/swing_ui/src/org/hanuna/gitalk/swing_ui/GitLogIcons.java
+++ plugins/git4idea/GitAlk/swing_ui/src/org/hanuna/gitalk/swing_ui/GitLogIcons.java
@@ -5,7 +5,9 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.Icon;
 
 public class GitLogIcons {
-  public static final Icon CHERRY_PICK = IconLoader.getIcon("/cherry-pick-32.png"); // 32x32
-  public static final Icon REBASE = IconLoader.getIcon("/bricks-multicolor-32.png"); // 32x32
-  public static final Icon REBASE_INTERACTIVE = IconLoader.getIcon("/bricks-user-32.png"); // 32x32
+  public static final Icon CHERRY_PICK = IconLoader.getIcon("/cherry-pick-32.png");
+  public static final Icon MOVE = IconLoader.getIcon("/move-32.png");
+  public static final Icon REBASE = IconLoader.getIcon("/bricks-multicolor-32.png");
+  public static final Icon REBASE_INTERACTIVE = IconLoader.getIcon("/bricks-user-32.png");
+  public static final Icon FORBIDDEN = IconLoader.getIcon("/forbidden-32.png");
 }
--- plugins/git4idea/GitAlk/swing_ui/src/org/hanuna/gitalk/swing_ui/Swing_UI.java
+++ plugins/git4idea/GitAlk/swing_ui/src/org/hanuna/gitalk/swing_ui/Swing_UI.java
@@ -6,9 +6,7 @@ import org.hanuna.gitalk.graph.elements.Node;
 import org.hanuna.gitalk.swing_ui.frame.ErrorModalDialog;
 import org.hanuna.gitalk.swing_ui.frame.MainFrame;
 import org.hanuna.gitalk.swing_ui.frame.ProgressFrame;
-import org.hanuna.gitalk.ui.ControllerListener;
-import org.hanuna.gitalk.ui.DragDropListener;
-import org.hanuna.gitalk.ui.UI_Controller;
+import org.hanuna.gitalk.ui.*;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
@@ -67,16 +65,18 @@ public class Swing_UI {
 
   private class DragDropConditions {
 
-    public boolean sameBranch(Node commit, Node commitBeingDragged) {
-      return false; // TODO
+    public boolean sameBranch(Node commit, List<Node> commitsBeingDragged) {
+      return true; // TODO
     }
   }
 
-  private enum DragDropActions {
-
+  interface Action {
+    void hint(Node commit, MouseEvent e, List<Node> commitsBeingDragged);
+    void perform(Node commit, MouseEvent e, List<Node> commitsBeingDragged);
   }
 
   private class SwingDragDropListener extends DragDropListener {
+
     private final JLabel hintLabel = new JLabel("") {
       @Override
       protected void paintComponent(Graphics g) {
@@ -101,41 +101,155 @@ public class Swing_UI {
     };
     private final LightweightHint dragDropHint = new LightweightHint(hintLabel);
 
+    private final Action NONE = new Action() {
+      @Override
+      public void hint(Node commit, MouseEvent e, List<Node> commitsBeingDragged) {
+      }
+
+      @Override
+      public void perform(Node commit, MouseEvent e, List<Node> commitsBeingDragged) {
+      }
+    };
+
+    private final Action MOVE = new Action() {
+
+      @Override
+      public void hint(Node commit, MouseEvent e, List<Node> commitsBeingDragged) {
+        showHint(e, GitLogIcons.MOVE, "Move " + renderCommits(commitsBeingDragged));
+      }
+
+      @Override
+      public void perform(Node commit, MouseEvent e, List<Node> commitsBeingDragged) {
+
+      }
+    };
+
+    private final Action PICK = new Action() {
+
+      @Override
+      public void hint(Node commit, MouseEvent e, List<Node> commitsBeingDragged) {
+        showHint(e, GitLogIcons.CHERRY_PICK, "Cherry-pick " + renderCommits(commitsBeingDragged));
+      }
+
+      @Override
+      public void perform(Node commit, MouseEvent e, List<Node> commitsBeingDragged) {
+
+      }
+    };
+
+    private final Action REBASE = new Action() {
+
+      @Override
+      public void hint(Node commit, MouseEvent e, List<Node> commitsBeingDragged) {
+        showHint(e, GitLogIcons.REBASE, "Rebase " + renderCommits(commitsBeingDragged));
+      }
+
+      @Override
+      public void perform(Node commit, MouseEvent e, List<Node> commitsBeingDragged) {
+
+      }
+    };
+
+    private final Action FORBIDDEN = new Action() {
+
+      @Override
+      public void hint(Node commit, MouseEvent e, List<Node> commitsBeingDragged) {
+        showHint(e, GitLogIcons.FORBIDDEN, "No action possible");
+      }
+
+      @Override
+      public void perform(Node commit, MouseEvent e, List<Node> commitsBeingDragged) {
+        // Do nothing
+      }
+    };
+
+    private CharSequence renderCommits(List<Node> commitsBeingDragged) {
+      if (commitsBeingDragged.size() == 1) {
+        Node node = commitsBeingDragged.get(0);
+        String message = ui_controller.getDataPack().getCommitDataGetter().getCommitData(node).getMessage();
+        return "\"" + message + "\"";
+      }
+
+      return commitsBeingDragged.size() + " commits";
+    }
+
+    private final ActionHandler actionHandler = new ActionHandler();
+
+    private class ActionHandler extends Handler implements Action {
+      private Action currentAction = NONE;
+
+      @Override
+      public void above(int rowIndex, Node commit, MouseEvent e, List<Node> commitsBeingDragged) {
+        if (myConditions.sameBranch(commit, commitsBeingDragged)) {
+          currentAction = MOVE;
+        }
+        else {
+          currentAction = FORBIDDEN;
+        }
+      }
+
+      @Override
+      public void below(int rowIndex, Node commit, MouseEvent e, List<Node> commitsBeingDragged) {
+        if (myConditions.sameBranch(commit, commitsBeingDragged)) {
+          currentAction = MOVE;
+        }
+        else {
+          currentAction = FORBIDDEN;
+        }
+      }
+
+      @Override
+      public void over(int rowIndex, Node commit, MouseEvent e, List<Node> commitsBeingDragged) {
+        // cherry pick (different branches)
+        // rebase (different branches + label on top)
+        // nothing otherwise
+        currentAction = REBASE;
+      }
+
+      @Override
+      public void overNode(int rowIndex, Node commit, MouseEvent e, List<Node> commitsBeingDragged) {
+        // fixup (same branch)
+        // nothing otherwise
+        currentAction = FORBIDDEN;
+      }
+
+      @Override
+      public void hint(Node commit, MouseEvent e, List<Node> commitsBeingDragged) {
+        currentAction.hint(commit, e, commitsBeingDragged);
+      }
+
+      @Override
+      public void perform(Node commit, MouseEvent e, List<Node> commitsBeingDragged) {
+        currentAction.perform(commit, e, commitsBeingDragged);
+        dragDropHint.hide();
+      }
+    }
+
     @Override
     public Handler drag() {
       return new Handler() {
         @Override
         public void above(int rowIndex, Node commit, MouseEvent e, List<Node> commitsBeingDragged) {
-          if (myConditions.sameBranch(commit, commitsBeingDragged)) {
-            showHint(e, GitLogIcons.CHERRY_PICK, "Above " + commit);
-          }
-          else {
-
-          }
-          // reorder - same branch
-          // nothing otherwise
+          actionHandler.above(rowIndex, commit, e, commitsBeingDragged);
+          actionHandler.hint(commit, e, commitsBeingDragged);
         }
 
         @Override
         public void below(int rowIndex, Node commit, MouseEvent e, List<Node> commitsBeingDragged) {
-          // reorder - same branch
-          // nothing otherwise
-          showHint(e, GitLogIcons.REBASE, "Below " + commit);
+          actionHandler.below(rowIndex, commit, e, commitsBeingDragged);
+          actionHandler.hint(commit, e, commitsBeingDragged);
         }
 
         @Override
         public void over(int rowIndex, Node commit, MouseEvent e, List<Node> commitsBeingDragged) {
-          // cherry pick (different branches)
-          // rebase (different branches + label on top)
-          // nothing otherwise
-          showHint(e, GitLogIcons.REBASE_INTERACTIVE, "Over " + commit);
+          actionHandler.over(rowIndex, commit, e, commitsBeingDragged);
+          actionHandler.hint(commit, e, commitsBeingDragged);
         }
 
         @Override
         public void overNode(int rowIndex, Node commit, MouseEvent e, List<Node> commitsBeingDragged) {
-          // fixup (same branch)
-          // nothing otherwise
-          showForbidden(e);
+          actionHandler.overNode(rowIndex, commit, e, commitsBeingDragged);
+          actionHandler.hint(commit, e, commitsBeingDragged);
         }
       };
     }
@@ -156,22 +270,26 @@ public class Swing_UI {
       return new Handler() {
         @Override
         public void above(int rowIndex, Node commit, MouseEvent e, List<Node> commitsBeingDragged) {
-          dragDropHint.hide();
+          actionHandler.above(rowIndex, commit, e, commitsBeingDragged);
+          actionHandler.perform(commit, e, commitsBeingDragged);
         }
 
         @Override
         public void below(int rowIndex, Node commit, MouseEvent e, List<Node> commitsBeingDragged) {
-          dragDropHint.hide();
+          actionHandler.below(rowIndex, commit, e, commitsBeingDragged);
+          actionHandler.perform(commit, e, commitsBeingDragged);
         }
 
         @Override
         public void over(int rowIndex, Node commit, MouseEvent e, List<Node> commitsBeingDragged) {
-          dragDropHint.hide();
+          actionHandler.over(rowIndex, commit, e, commitsBeingDragged);
+          actionHandler.perform(commit, e, commitsBeingDragged);
         }
 
         @Override
         public void overNode(int rowIndex, Node commit, MouseEvent e, List<Node> commitsBeingDragged) {
-          dragDropHint.hide();
+          actionHandler.overNode(rowIndex, commit, e, commitsBeingDragged);
+          actionHandler.perform(commit, e, commitsBeingDragged);
         }
       };
     }
