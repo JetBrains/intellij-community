4dc925981ca3d5480f10ed1250d673419bb37a12
Dmitry Jemerov <yole@jetbrains.com>
2013-05-23 22:27:33 +0200

    register new stuff in XmlCoreEnvironment

--- xml/xml-psi-impl/src/com/intellij/xml/XmlCoreEnvironment.java
+++ xml/xml-psi-impl/src/com/intellij/xml/XmlCoreEnvironment.java
@@ -4,6 +4,11 @@ import com.intellij.core.CoreApplicationEnvironment;
 import com.intellij.core.CoreProjectEnvironment;
 import com.intellij.ide.highlighter.DTDFileType;
 import com.intellij.ide.highlighter.XmlFileType;
+import com.intellij.lang.LanguageParserDefinitions;
+import com.intellij.lang.xml.XMLLanguage;
+import com.intellij.lang.xml.XMLParserDefinition;
+import com.intellij.openapi.extensions.Extensions;
+import com.intellij.psi.xml.XmlChildRole;
 
 /**
  * @author yole
@@ -15,5 +20,9 @@ public class XmlCoreEnvironment {
     appEnvironment.registerFileType(DTDFileType.INSTANCE, "dtd;ent;mod;elt");
 
     appEnvironment.registerFileType(XmlFileType.INSTANCE, "xml;xsd;tld;xsl;jnlp;wsdl;jhm;ant;xul;xslt;rng;fxml");
+
+    appEnvironment.addExplicitExtension(LanguageParserDefinitions.INSTANCE, XMLLanguage.INSTANCE, new XMLParserDefinition());
+
+    CoreApplicationEnvironment.registerExtensionPoint(Extensions.getRootArea(), XmlChildRole.EP_NAME, XmlChildRole.StartTagEndTokenProvider.class);
   }
 }
