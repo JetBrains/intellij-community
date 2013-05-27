0e86feb3b8ee111f296d7b39cede10d2601c5ccb
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2013-05-27 17:40:42 +0200

    avoid override deprecated createRunnerSettings/getRunnerSettingsEditor

--- plugins/devkit/src/run/PluginRunConfiguration.java
+++ plugins/devkit/src/run/PluginRunConfiguration.java
@@ -21,7 +21,6 @@ import com.intellij.execution.Executor;
 import com.intellij.execution.configurations.*;
 import com.intellij.execution.filters.TextConsoleBuilderFactory;
 import com.intellij.execution.runners.ExecutionEnvironment;
-import com.intellij.execution.runners.ProgramRunner;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
@@ -63,18 +62,12 @@ public class PluginRunConfiguration extends RunConfigurationBase implements Modu
     super(project, factory, name);
   }
 
+  @Override
   public SettingsEditor<? extends RunConfiguration> getConfigurationEditor() {
     return new PluginRunConfigurationEditor(this);
   }
 
-  public JDOMExternalizable createRunnerSettings(ConfigurationInfoProvider provider) {
-    return null;
-  }
-
-  public SettingsEditor<JDOMExternalizable> getRunnerSettingsEditor(ProgramRunner runner) {
-    return null;
-  }
-
+  @Override
   public RunProfileState getState(@NotNull final Executor executor, @NotNull final ExecutionEnvironment env) throws ExecutionException {
     if (getModule() == null){
       throw new ExecutionException(DevKitBundle.message("run.configuration.no.module.specified"));
@@ -107,6 +100,7 @@ public class PluginRunConfiguration extends RunConfigurationBase implements Modu
     IdeaLicenseHelper.copyIDEALicense(sandboxHome, ideaJdk);
 
     final JavaCommandLineState state = new JavaCommandLineState(env) {
+      @Override
       protected JavaParameters createJavaParameters() throws ExecutionException {
 
         final JavaParameters params = new JavaParameters();
@@ -223,11 +217,13 @@ public class PluginRunConfiguration extends RunConfigurationBase implements Modu
     }
   }
 
+  @Override
   public void checkConfiguration() throws RuntimeConfigurationException {
     if (getModule() == null) {
       throw new RuntimeConfigurationException(DevKitBundle.message("run.configuration.no.module.specified"));
     }
     String moduleName = ApplicationManager.getApplication().runReadAction(new Computable<String>() {
+      @Override
       public String compute() {
         return getModule().getName();
       }
@@ -246,12 +242,14 @@ public class PluginRunConfiguration extends RunConfigurationBase implements Modu
   }
 
 
+  @Override
   @NotNull
   public Module[] getModules() {
     final Module module = getModule();
     return module != null ? new Module[]{module} : Module.EMPTY_ARRAY;
   }
 
+  @Override
   public void readExternal(Element element) throws InvalidDataException {
     Element module = element.getChild(MODULE);
     if (module != null) {
@@ -267,9 +265,11 @@ public class PluginRunConfiguration extends RunConfigurationBase implements Modu
     super.readExternal(element);
   }
 
+  @Override
   public void writeExternal(Element element) throws WriteExternalException {
     Element moduleElement = new Element(MODULE);
     moduleElement.setAttribute(NAME, ApplicationManager.getApplication().runReadAction(new Computable<String>() {
+      @Override
       public String compute() {
         final Module module = getModule();
         return module != null ? module.getName()
