dc6c2439f1f043157ecd6b26f5382c90f5f92862
Alexey Kudravtsev <cdr@intellij.com>
2013-05-28 18:21:15 +0400

    moved to analysis

--- plugins/InspectionGadgets/src/com/siyeh/ig/threading/AccessToNonThreadSafeStaticFieldFromInstanceInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/threading/AccessToNonThreadSafeStaticFieldFromInstanceInspection.java
@@ -16,65 +16,17 @@
 package com.siyeh.ig.threading;
 
 import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.psi.*;
-import com.intellij.psi.util.PsiTreeUtil;
 import com.siyeh.InspectionGadgetsBundle;
-import com.siyeh.ig.BaseInspection;
-import com.siyeh.ig.BaseInspectionVisitor;
-import com.siyeh.ig.ui.ExternalizableStringSet;
 import com.siyeh.ig.ui.UiUtils;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import javax.swing.JComponent;
+import javax.swing.*;
 import java.util.List;
 
 public class AccessToNonThreadSafeStaticFieldFromInstanceInspection
-  extends BaseInspection {
-
-  @NonNls
-  @SuppressWarnings({"PublicField"})
-  public String nonThreadSafeTypes = "";
-  @SuppressWarnings("PublicField")
-  public final ExternalizableStringSet nonThreadSafeClasses =
-    new ExternalizableStringSet(
-      "java.text.SimpleDateFormat",
-      "java.util.Calendar");
+  extends AccessToNonThreadSafeStaticFieldFromInstanceInspectionBase {
 
   public AccessToNonThreadSafeStaticFieldFromInstanceInspection() {
-    if (nonThreadSafeTypes.length() != 0) {
-      nonThreadSafeClasses.clear();
-      final List<String> strings =
-        StringUtil.split(nonThreadSafeTypes, ",");
-      for (String string : strings) {
-        nonThreadSafeClasses.add(string);
-      }
-      nonThreadSafeTypes = "";
-    }
-  }
-
-  @NotNull
-  @Override
-  public String getID() {
-    return "AccessToNonThreadSafeStaticField";
-  }
-
-  @Override
-  @Nls
-  @NotNull
-  public String getDisplayName() {
-    return InspectionGadgetsBundle.message(
-      "access.to.non.thread.safe.static.field.from.instance.display.name");
-  }
-
-  @Override
-  @NotNull
-  protected String buildErrorString(Object... infos) {
-    return InspectionGadgetsBundle.message(
-      "access.to.non.thread.safe.static.field.from.instance.field.problem.descriptor",
-      infos[0]);
   }
 
   @Override
@@ -85,61 +37,5 @@ public class AccessToNonThreadSafeStaticFieldFromInstanceInspection
                                                 "access.to.non.thread.safe.static.field.from.instance.option.title"),
                                               InspectionGadgetsBundle.message(
                                                 "access.to.non.thread.safe.static.field.from.instance.class.chooser.title"));
-  }
-
-  @Override
-  public BaseInspectionVisitor buildVisitor() {
-    return new AccessToNonThreadSafeStaticFieldFromInstanceVisitor();
-  }
-
-  class AccessToNonThreadSafeStaticFieldFromInstanceVisitor
-    extends BaseInspectionVisitor {
-
-    @Override
-    public void visitReferenceExpression(
-      PsiReferenceExpression expression) {
-      super.visitReferenceExpression(expression);
-      final PsiModifierListOwner parent =
-        PsiTreeUtil.getParentOfType(expression,
-                                    PsiField.class, PsiMethod.class,
-                                    PsiClassInitializer.class);
-      if (parent == null) {
-        return;
-      }
-      if (parent instanceof PsiMethod ||
-          parent instanceof PsiClassInitializer) {
-        if (parent.hasModifierProperty(PsiModifier.SYNCHRONIZED)) {
-          return;
-        }
-        final PsiSynchronizedStatement synchronizedStatement =
-          PsiTreeUtil.getParentOfType(expression,
-                                      PsiSynchronizedStatement.class);
-        if (synchronizedStatement != null) {
-          return;
-        }
-      }
-      final PsiExpression qualifier = expression.getQualifierExpression();
-      if (qualifier != null) {
-        return;
-      }
-      final PsiType type = expression.getType();
-      if (!(type instanceof PsiClassType)) {
-        return;
-      }
-      final PsiClassType classType = (PsiClassType)type;
-      final String className = classType.rawType().getCanonicalText();
-      if (!nonThreadSafeClasses.contains(className)) {
-        return;
-      }
-      final PsiElement target = expression.resolve();
-      if (!(target instanceof PsiField)) {
-        return;
-      }
-      final PsiField field = (PsiField)target;
-      if (!field.hasModifierProperty(PsiModifier.STATIC)) {
-        return;
-      }
-      registerError(expression, className);
-    }
   }
 }
--- plugins/InspectionGadgets/src/com/siyeh/ig/threading/AccessToStaticFieldLockedOnInstanceInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/threading/AccessToStaticFieldLockedOnInstanceInspection.java
@@ -15,120 +15,16 @@
  */
 package com.siyeh.ig.threading;
 
-import com.intellij.psi.*;
-import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.util.containers.OrderedSet;
-import com.siyeh.InspectionGadgetsBundle;
-import com.siyeh.ig.BaseInspection;
-import com.siyeh.ig.BaseInspectionVisitor;
-import com.siyeh.ig.psiutils.ExpressionUtils;
 import com.siyeh.ig.ui.UiUtils;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 
-public class AccessToStaticFieldLockedOnInstanceInspection extends BaseInspection {
-
-  @SuppressWarnings("PublicField") public OrderedSet<String> ignoredClasses = new OrderedSet();
-
-  @Override
-  @NotNull
-  public String getDisplayName() {
-    return InspectionGadgetsBundle.message("access.to.static.field.locked.on.instance.display.name");
-  }
-
-  @Override
-  @NotNull
-  protected String buildErrorString(Object... infos) {
-    return InspectionGadgetsBundle.message("access.to.static.field.locked.on.instance.problem.descriptor");
-  }
+public class AccessToStaticFieldLockedOnInstanceInspection extends AccessToStaticFieldLockedOnInstanceInspectionBase {
 
   @Nullable
   @Override
   public JComponent createOptionsPanel() {
     return UiUtils.createTreeClassChooserList(ignoredClasses, "Ignored Classes", "Choose class to ignore");
-  }
-
-  @Override
-  public BaseInspectionVisitor buildVisitor() {
-    return new AccessToStaticFieldLockedOnInstanceVisitor();
-  }
-
-  private class AccessToStaticFieldLockedOnInstanceVisitor extends BaseInspectionVisitor {
-
-    @Override
-    public void visitReferenceExpression(@NotNull PsiReferenceExpression expression) {
-      super.visitReferenceExpression(expression);
-      boolean isLockedOnInstance = false;
-      boolean isLockedOnClass = false;
-      final PsiMethod containingMethod = PsiTreeUtil.getParentOfType(expression, PsiMethod.class);
-      if (containingMethod != null && containingMethod.hasModifierProperty(PsiModifier.SYNCHRONIZED)) {
-        if (containingMethod.hasModifierProperty(PsiModifier.STATIC)) {
-          isLockedOnClass = true;
-        }
-        else {
-          isLockedOnInstance = true;
-        }
-      }
-      final PsiClass expressionClass = PsiTreeUtil.getParentOfType(expression, PsiClass.class);
-      if (expressionClass == null) {
-        return;
-      }
-      PsiElement elementToCheck = expression;
-      while (true) {
-        final PsiSynchronizedStatement synchronizedStatement = PsiTreeUtil.getParentOfType(elementToCheck, PsiSynchronizedStatement.class);
-        if (synchronizedStatement == null || !PsiTreeUtil.isAncestor(expressionClass, synchronizedStatement, true)) {
-          break;
-        }
-        final PsiExpression lockExpression = synchronizedStatement.getLockExpression();
-        if (lockExpression instanceof PsiReferenceExpression) {
-          final PsiReferenceExpression reference = (PsiReferenceExpression)lockExpression;
-          final PsiElement target = reference.resolve();
-          if (target instanceof PsiField) {
-            final PsiField lockField = (PsiField)target;
-            if (lockField.hasModifierProperty(PsiModifier.STATIC)) {
-              isLockedOnClass = true;
-            }
-            else {
-              isLockedOnInstance = true;
-            }
-          }
-        }
-        else if (lockExpression instanceof PsiThisExpression) {
-          isLockedOnInstance = true;
-        }
-        else if (lockExpression instanceof PsiClassObjectAccessExpression) {
-          isLockedOnClass = true;
-        }
-        elementToCheck = synchronizedStatement;
-      }
-      if (!isLockedOnInstance || isLockedOnClass) {
-        return;
-      }
-      final PsiElement target = expression.resolve();
-      if (!(target instanceof PsiField)) {
-        return;
-      }
-      final PsiField lockedField = (PsiField)target;
-      if (!lockedField.hasModifierProperty(PsiModifier.STATIC) || ExpressionUtils.isConstant(lockedField)) {
-        return;
-      }
-      final PsiClass containingClass = lockedField.getContainingClass();
-      if (!PsiTreeUtil.isAncestor(containingClass, expression, false)) {
-        return;
-      }
-      if (!ignoredClasses.isEmpty()) {
-        final PsiType type = lockedField.getType();
-        if (type instanceof PsiClassType) {
-          final PsiClassType classType = (PsiClassType)type;
-          final PsiClass aClass = classType.resolve();
-          if (aClass != null && ignoredClasses.contains(aClass.getQualifiedName())) {
-            return;
-          }
-        }
-      }
-      registerError(expression);
-    }
   }
 }
