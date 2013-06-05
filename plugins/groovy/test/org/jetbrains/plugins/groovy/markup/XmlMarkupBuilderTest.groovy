5f33791cde15ebfd80dfc799f67a34fae67dcfe4
Max Medvedev <maxim.medvedev@jetbrains.com>
2013-06-05 18:37:32 +0400

    IDEA-108318 "Method call is ambiguous" when using groovy builders

--- plugins/groovy/test/org/jetbrains/plugins/groovy/markup/XmlMarkupBuilderTest.groovy
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/markup/XmlMarkupBuilderTest.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jetbrains.plugins.groovy.markup
 
 import com.intellij.psi.impl.compiled.ClsMethodImpl
@@ -19,7 +34,7 @@ class XmlMarkupBuilderTest extends LightGroovyTestCase {
     myFixture.enableInspections(GroovyAssignabilityCheckInspection, GrUnresolvedAccessInspection)
 
 
-    myFixture.addFileToProject("A.groovy", """
+    myFixture.configureByText("A.groovy", """
 class A {
     void testSomething() {
         <warning>x</warning>()
@@ -30,6 +45,8 @@ class A {
                 x()
                 country('Australia')
                 record(type:'speed', 'Production Pickup Truck with speed of 271kph')
+                map(a:'a', b:'b')
+                emptyTag()
             }
 
             foo<warning>("a", "b")</warning>
@@ -38,7 +55,7 @@ class A {
 }
 """)
 
-    myFixture.testHighlighting(true, false, true, "A.groovy")
+    myFixture.testHighlighting(true, false, true)
   }
 
   public void testResolveToMethod1() {
