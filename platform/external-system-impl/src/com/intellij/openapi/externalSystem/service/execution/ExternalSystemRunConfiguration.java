cd5116daf040c880ead994f74e77e9317d638a28
Denis.Zhdanov <Denis.Zhdanov@jetbrains.com>
2013-06-07 12:55:23 +0400

    IDEA-108602 Gradle: Integrate 'debug task' to the execution system
    External tasks are executed by external system and ide remote java debugger is attached to external system process. Current drawback is that editor is not updated accordingly (break point row highlight) when a break point is hit during external task debugging

--- platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/execution/ExternalSystemRunConfiguration.java
+++ platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/execution/ExternalSystemRunConfiguration.java
@@ -12,6 +12,7 @@ import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.execution.runners.ProgramRunner;
 import com.intellij.execution.ui.ConsoleView;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.externalSystem.model.ProjectSystemId;
 import com.intellij.openapi.externalSystem.model.execution.ExternalSystemTaskExecutionSettings;
 import com.intellij.openapi.externalSystem.model.serialization.ExternalTaskPojo;
@@ -26,12 +27,15 @@ import com.intellij.openapi.util.InvalidDataException;
 import com.intellij.openapi.util.JDOMExternalizable;
 import com.intellij.openapi.util.WriteExternalException;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.wm.ToolWindowId;
 import com.intellij.util.containers.ContainerUtilRt;
+import com.intellij.util.net.NetUtils;
 import com.intellij.util.xmlb.XmlSerializer;
 import org.jdom.Element;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.IOException;
 import java.io.OutputStream;
 import java.util.List;
 
@@ -40,7 +44,9 @@ import java.util.List;
  * @since 23.05.13 18:30
  */
 public class ExternalSystemRunConfiguration extends RunConfigurationBase implements LocatableConfiguration {
-  
+
+  private static final Logger LOG = Logger.getInstance("#" + ExternalSystemRunConfiguration.class.getName());
+
   private ExternalSystemTaskExecutionSettings mySettings = new ExternalSystemTaskExecutionSettings();
 
   public ExternalSystemRunConfiguration(@NotNull ProjectSystemId externalSystemId,
@@ -61,7 +67,7 @@ public class ExternalSystemRunConfiguration extends RunConfigurationBase impleme
   private String getGeneratedName() {
     return AbstractExternalSystemTaskConfigurationType.generateName(getProject(), mySettings);
   }
-  
+
   @Override
   public String suggestedName() {
     return getGeneratedName();
@@ -116,73 +122,114 @@ public class ExternalSystemRunConfiguration extends RunConfigurationBase impleme
   @Nullable
   @Override
   public RunProfileState getState(@NotNull Executor executor, @NotNull ExecutionEnvironment env) throws ExecutionException {
-    return new RunnableState() {
-      @Nullable
-      @Override
-      public ExecutionResult execute(Executor executor, @NotNull ProgramRunner runner) throws ExecutionException {
-        ConsoleView console = new TextConsoleBuilderImpl(getProject()).getConsole();
-        final MyProcessHandler processHandler = new MyProcessHandler();
-        console.attachToProcess(processHandler);
-        final List<ExternalTaskPojo> tasks = ContainerUtilRt.newArrayList();
-        for (String taskName : mySettings.getTaskNames()) {
-          tasks.add(new ExternalTaskPojo(taskName, mySettings.getExternalProjectPath(), null));
-        }
-        if (tasks.isEmpty()) {
-          throw new ExecutionException(ExternalSystemBundle.message("run.error.undefined.task"));
-        }
-        final ExternalSystemExecuteTaskTask task = new ExternalSystemExecuteTaskTask(mySettings.getExternalSystemId(),
-                                                                                     getProject(),
-                                                                                     tasks,
-                                                                                     mySettings.getVmOptions());
-        ApplicationManager.getApplication().executeOnPooledThread(new Runnable() {
-          @Override
-          public void run() {
-            final String greeting;
-            if (mySettings.getTaskNames().size() > 1) {
-              greeting = ExternalSystemBundle.message("run.text.starting.multiple.task", StringUtil.join(mySettings.getTaskNames(), " "));
-            }
-            else {
-              greeting = ExternalSystemBundle.message("run.text.starting.single.task", StringUtil.join(mySettings.getTaskNames(), " "));
-            }
-            processHandler.notifyTextAvailable(greeting, ProcessOutputTypes.SYSTEM);
-            task.execute(new ExternalSystemTaskNotificationListenerAdapter() {
-
-              private boolean myResetGreeting = true;
-
-              @Override
-              public void onTaskOutput(@NotNull ExternalSystemTaskId id, @NotNull String text, boolean stdOut) {
-                if (myResetGreeting) {
-                  processHandler.notifyTextAvailable("\r", ProcessOutputTypes.SYSTEM);
-                  myResetGreeting = false;
-                }
-                processHandler.notifyTextAvailable(text, stdOut ? ProcessOutputTypes.STDOUT : ProcessOutputTypes.STDERR);
-              }
-
-              @Override
-              public void onEnd(@NotNull ExternalSystemTaskId id) {
-                processHandler.notifyProcessTerminated(0);
-              }
-            });
-          }
-        });
-        return new DefaultExecutionResult(console, processHandler);
-      }
-
-      @Override
-      public RunnerSettings getRunnerSettings() {
-        return null;
-      }
-
-      @Override
-      public ConfigurationPerRunnerSettings getConfigurationSettings() {
-        return null;
-      }
-    };
+    return new MyRunnableState(mySettings, getProject(), ToolWindowId.DEBUG.equals(executor.getId()));
   }
 
   @Override
   public void checkConfiguration() throws RuntimeConfigurationException {
     // TODO den implement
+  }
+  
+  public static class MyRunnableState implements RunnableState {
+    
+    @NotNull private final ExternalSystemTaskExecutionSettings mySettings;
+    @NotNull private final Project myProject;
+    
+    private final int myDebugPort;
+
+    public MyRunnableState(@NotNull ExternalSystemTaskExecutionSettings settings, @NotNull Project project, boolean debug) {
+      mySettings = settings;
+      myProject = project;
+      int port;
+      if (debug) {
+        try {
+          port = NetUtils.findAvailableSocketPort();
+        }
+        catch (IOException e) {
+          LOG.warn("Unexpected I/O exception occurred on attempt to find a free port to use for external system task debugging", e);
+          port = 0;
+        }
+      }
+      else {
+        port = 0;
+      }
+      myDebugPort = port;
+    }
+
+    public int getDebugPort() {
+      return myDebugPort;
+    }
+
+    @Nullable
+    @Override
+    public ExecutionResult execute(Executor executor, @NotNull ProgramRunner runner) throws ExecutionException {
+      ConsoleView console = new TextConsoleBuilderImpl(myProject).getConsole();
+      final MyProcessHandler processHandler = new MyProcessHandler();
+      console.attachToProcess(processHandler);
+      final List<ExternalTaskPojo> tasks = ContainerUtilRt.newArrayList();
+      for (String taskName : mySettings.getTaskNames()) {
+        tasks.add(new ExternalTaskPojo(taskName, mySettings.getExternalProjectPath(), null));
+      }
+      if (tasks.isEmpty()) {
+        throw new ExecutionException(ExternalSystemBundle.message("run.error.undefined.task"));
+      }
+      String vmOptions;
+      if (myDebugPort > 0) {
+        String debuggerSetup = "-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=" + myDebugPort;
+        String regular = mySettings.getVmOptions();
+        vmOptions = regular == null ? debuggerSetup : regular + " " + debuggerSetup;
+      }
+      else {
+        vmOptions = mySettings.getVmOptions();
+      }
+      
+      final ExternalSystemExecuteTaskTask task = new ExternalSystemExecuteTaskTask(mySettings.getExternalSystemId(),
+                                                                                   myProject,
+                                                                                   tasks,
+                                                                                   vmOptions);
+      ApplicationManager.getApplication().executeOnPooledThread(new Runnable() {
+        @Override
+        public void run() {
+          final String greeting;
+          if (mySettings.getTaskNames().size() > 1) {
+            greeting = ExternalSystemBundle.message("run.text.starting.multiple.task", StringUtil.join(mySettings.getTaskNames(), " "));
+          }
+          else {
+            greeting = ExternalSystemBundle.message("run.text.starting.single.task", StringUtil.join(mySettings.getTaskNames(), " "));
+          }
+          processHandler.notifyTextAvailable(greeting, ProcessOutputTypes.SYSTEM);
+          task.execute(new ExternalSystemTaskNotificationListenerAdapter() {
+
+            private boolean myResetGreeting = true;
+
+            @Override
+            public void onTaskOutput(@NotNull ExternalSystemTaskId id, @NotNull String text, boolean stdOut) {
+              if (myResetGreeting) {
+                processHandler.notifyTextAvailable("\r", ProcessOutputTypes.SYSTEM);
+                myResetGreeting = false;
+              }
+              processHandler.notifyTextAvailable(text, stdOut ? ProcessOutputTypes.STDOUT : ProcessOutputTypes.STDERR);
+            }
+
+            @Override
+            public void onEnd(@NotNull ExternalSystemTaskId id) {
+              processHandler.notifyProcessTerminated(0);
+            }
+          });
+        }
+      });
+      return new DefaultExecutionResult(console, processHandler);
+    }
+
+    @Override
+    public RunnerSettings getRunnerSettings() {
+      return null;
+    }
+
+    @Override
+    public ConfigurationPerRunnerSettings getConfigurationSettings() {
+      return null;
+    }
   }
   
   private static class MyProcessHandler extends ProcessHandler {
