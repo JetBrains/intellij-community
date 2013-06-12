bcd3ba69d41c5ea5bddd269a15f1e4f1c1c97b9b
Kirill Likhodedov <Kirill.Likhodedov@gmail.com>
2013-06-12 19:56:49 +0400

    cleanup

--- platform/dvcs/vcs-log/vcs-log-ui/src/org/hanuna/gitalk/ui/render/GraphCommitCellRender.java
+++ platform/dvcs/vcs-log/vcs-log-ui/src/org/hanuna/gitalk/ui/render/GraphCommitCellRender.java
@@ -1,5 +1,7 @@
 package org.hanuna.gitalk.ui.render;
 
+import com.intellij.ui.JBColor;
+import com.intellij.util.ui.UIUtil;
 import org.hanuna.gitalk.printmodel.SpecialPrintElement;
 import org.hanuna.gitalk.ui.render.painters.GraphCellPainter;
 import org.hanuna.gitalk.ui.render.painters.RefPainter;
@@ -21,8 +23,10 @@ import static org.hanuna.gitalk.ui.render.PrintParameters.WIDTH_NODE;
  * @author erokhins
  */
 public class GraphCommitCellRender implements TableCellRenderer {
+
   public static final Color MARKED_BACKGROUND = new Color(0xB6, 0xE4, 0xFF);
   public static final Color APPLIED_BACKGROUND = new Color(0x92, 0xF5, 0x8F);
+
   private final GraphCellPainter graphPainter;
   private final RefPainter refPainter = new RefPainter();
   private ExtDefaultCellRender cellRender = new ExtDefaultCellRender();
@@ -31,12 +35,8 @@ public class GraphCommitCellRender implements TableCellRenderer {
     this.graphPainter = graphPainter;
   }
 
-  private GraphCommitCell getAssertGraphCommitCell(Object value) {
-    return (GraphCommitCell)value;
-  }
-
   protected int getLeftPadding(JTable table, @Nullable Object value) {
-    GraphCommitCell cell = getAssertGraphCommitCell(value);
+    GraphCommitCell cell = (GraphCommitCell)value;
 
     if (cell == null) {
       return 0;
@@ -51,8 +51,8 @@ public class GraphCommitCellRender implements TableCellRenderer {
     return refPadding + graphPadding;
   }
 
-  protected String getCellText(JTable table, @Nullable Object value) {
-    GraphCommitCell cell = getAssertGraphCommitCell(value);
+  protected String getCellText(@Nullable Object value) {
+    GraphCommitCell cell = (GraphCommitCell)value;
     if (cell == null) {
       return "!!! No cell for value: " + value;
     }
@@ -61,13 +61,13 @@ public class GraphCommitCellRender implements TableCellRenderer {
     }
   }
 
-  protected void additionPaint(Graphics g, JTable table, @Nullable Object value) {
-    GraphCommitCell cell = getAssertGraphCommitCell(value);
+  protected void additionPaint(Graphics g, @Nullable Object value) {
+    GraphCommitCell cell = (GraphCommitCell)value;
     if (cell == null) {
       return;
     }
 
-    BufferedImage image = new BufferedImage(1000, HEIGHT_CELL, BufferedImage.TYPE_INT_ARGB);
+    BufferedImage image = UIUtil.createImage(1000, HEIGHT_CELL, BufferedImage.TYPE_INT_ARGB);
     Graphics2D g2 = image.createGraphics();
     g2.setBackground(new Color(0, 0, 0, 0));
 
@@ -80,8 +80,8 @@ public class GraphCommitCellRender implements TableCellRenderer {
     g.drawImage(image, 0, 0, null);
   }
 
-  protected boolean isMarked(JTable table, @Nullable Object value) {
-    GraphCommitCell cell = getAssertGraphCommitCell(value);
+  protected boolean isMarked(@Nullable Object value) {
+    GraphCommitCell cell = (GraphCommitCell)value;
     if (cell == null) {
       return false;
     }
@@ -93,8 +93,8 @@ public class GraphCommitCellRender implements TableCellRenderer {
     return false;
   }
 
-  protected GraphCommitCell.Kind getKind(JTable table, @Nullable Object value) {
-    GraphCommitCell cell = getAssertGraphCommitCell(value);
+  protected GraphCommitCell.Kind getKind(@Nullable Object value) {
+    GraphCommitCell cell = (GraphCommitCell)value;
     if (cell == null) {
       return GraphCommitCell.Kind.NORMAL;
     }
@@ -108,24 +108,22 @@ public class GraphCommitCellRender implements TableCellRenderer {
   }
 
   public class ExtDefaultCellRender extends DefaultTableCellRenderer {
-    private JTable table;
     private Object value;
 
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-      this.table = table;
       this.value = value;
-      super.getTableCellRendererComponent(table, getCellText(table, value), isSelected, hasFocus, row, column);
-      if (isMarked(table, value) && !isSelected) {
+      super.getTableCellRendererComponent(table, getCellText(value), isSelected, hasFocus, row, column);
+      if (isMarked(value) && !isSelected) {
         setBackground(MARKED_BACKGROUND);
       }
       else {
-        setBackground(isSelected ? table.getSelectionBackground() : Color.WHITE);
+        setBackground(isSelected ? table.getSelectionBackground() : JBColor.WHITE);
       }
       Border paddingBorder = BorderFactory.createEmptyBorder(0, getLeftPadding(table, value), 0, 0);
       this.setBorder(BorderFactory.createCompoundBorder(this.getBorder(), paddingBorder));
 
-      GraphCommitCell.Kind kind = getKind(table, value);
-      Color textColor = isSelected ? table.getSelectionForeground() : Color.BLACK;
+      GraphCommitCell.Kind kind = getKind(value);
+      Color textColor = isSelected ? table.getSelectionForeground() : JBColor.BLACK;
       switch (kind) {
         case APPLIED:
           setBackground(APPLIED_BACKGROUND);
@@ -139,11 +137,11 @@ public class GraphCommitCellRender implements TableCellRenderer {
           break;
         case FIXUP:
           setFont(getFont().deriveFont(Font.BOLD));
-          setForeground(Color.GRAY);
+          setForeground(JBColor.GRAY);
           break;
         case REWORD:
           setFont(getFont().deriveFont(Font.BOLD));
-          setForeground(Color.blue);
+          setForeground(JBColor.BLUE);
           break;
       }
 
@@ -154,7 +152,7 @@ public class GraphCommitCellRender implements TableCellRenderer {
     @Override
     public void paint(Graphics g) {
       super.paint(g);
-      additionPaint(g, table, value);
+      additionPaint(g, value);
     }
   }
 }
