23d454f501b65541ee3225ed0488d51e56c79c96
Anna Kozlova <anna.kozlova@jetbrains.com>
2013-06-12 11:43:10 +0400

    fix assertion

--- java/java-impl/src/com/intellij/refactoring/rename/RenameJavaMethodProcessor.java
+++ java/java-impl/src/com/intellij/refactoring/rename/RenameJavaMethodProcessor.java
@@ -210,7 +210,7 @@ public class RenameJavaMethodProcessor extends RenameJavaMemberProcessor {
           if (element instanceof PsiReferenceExpression) {
             if (((PsiReferenceExpression)element).resolve() == methodToRename) {
               final PsiElement parent = element.getParent();
-              LOG.assertTrue(parent instanceof PsiMethodReferenceExpression, parent.getText());
+              LOG.assertTrue(parent instanceof PsiMethodCallExpression, parent.getText());
               final PsiMethodCallExpression copy = (PsiMethodCallExpression)JavaPsiFacade.getElementFactory(element.getProject())
                 .createExpressionFromText(parent.getText(), element);
               final PsiReferenceExpression expression = (PsiReferenceExpression)processRef(copy.getMethodExpression(), newName);
