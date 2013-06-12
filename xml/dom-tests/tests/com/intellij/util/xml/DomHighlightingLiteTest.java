8ca876c25e0ad3624108bdbb1036128d1037f31c
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2013-06-12 16:21:38 +0400

    platform: direct dependencies instead of service polling; tests moved

--- xml/dom-tests/tests/com/intellij/util/xml/DomHighlightingLiteTest.java
+++ xml/dom-tests/tests/com/intellij/util/xml/DomHighlightingLiteTest.java
@@ -1,5 +1,17 @@
 /*
- * Copyright (c) 2000-2006 JetBrains s.r.o. All Rights Reserved.
+ * Copyright 2000-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.intellij.util.xml;
 
@@ -22,6 +34,7 @@ import com.intellij.psi.xml.XmlTag;
 import com.intellij.testFramework.MockSchemesManagerFactory;
 import com.intellij.util.xml.highlighting.*;
 import com.intellij.util.xml.impl.DefaultDomAnnotator;
+import com.intellij.util.xml.impl.DomTestCase;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
