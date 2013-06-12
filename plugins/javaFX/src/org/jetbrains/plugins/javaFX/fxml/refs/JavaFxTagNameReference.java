6589ea498dc4e30d5c6fd996dcb040330a6018a0
Dmitry Jemerov <yole@jetbrains.com>
2013-06-12 10:50:40 +0200

    javafx tag name completion converted to completion contributor

--- plugins/javaFX/src/org/jetbrains/plugins/javaFX/fxml/refs/JavaFxTagNameReference.java
+++ plugins/javaFX/src/org/jetbrains/plugins/javaFX/fxml/refs/JavaFxTagNameReference.java
@@ -4,7 +4,6 @@ import com.intellij.codeInsight.completion.InsertionContext;
 import com.intellij.codeInsight.completion.XmlTagInsertHandler;
 import com.intellij.codeInsight.daemon.QuickFixActionRegistrar;
 import com.intellij.codeInsight.lookup.LookupElement;
-import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.codeInsight.quickfix.UnresolvedReferenceQuickFixProvider;
 import com.intellij.lang.ASTNode;
 import com.intellij.openapi.diagnostic.Logger;
@@ -13,18 +12,12 @@ import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.impl.source.xml.TagNameReference;
-import com.intellij.psi.impl.source.xml.TagNameVariantCollector;
-import com.intellij.psi.xml.*;
-import com.intellij.util.Function;
+import com.intellij.psi.xml.XmlFile;
+import com.intellij.psi.xml.XmlTag;
 import com.intellij.util.IncorrectOperationException;
-import com.intellij.xml.XmlElementDescriptor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.javaFX.fxml.JavaFxPsiUtil;
 import org.jetbrains.plugins.javaFX.fxml.descriptors.JavaFxClassBackedElementDescriptor;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * User: anna
@@ -74,24 +67,10 @@ public class JavaFxTagNameReference extends TagNameReference{
   @NotNull
   @Override
   public LookupElement[] getVariants() {
-    final PsiElement element = getElement();
     if(!myStartTagFlag){
       return super.getVariants();
     }
-    final XmlTag xmlTag = (XmlTag)element;
-
-    final List<XmlElementDescriptor>
-      variants = TagNameVariantCollector
-      .<XmlElementDescriptor>getTagNameVariants(xmlTag, Arrays.asList(xmlTag.knownNamespaces()), new ArrayList<String>(), Function.ID);
-    final List<LookupElement> elements = new ArrayList<LookupElement>(variants.size());
-    for (XmlElementDescriptor descriptor : variants) {
-      final String descriptorName = descriptor.getName(element);
-      if (descriptorName != null) {
-        LookupElementBuilder lookupElement = LookupElementBuilder.create(descriptor, descriptorName);
-        elements.add(lookupElement.withInsertHandler(JavaFxTagInsertHandler.INSTANCE));
-      }
-    }
-    return elements.toArray(new LookupElement[elements.size()]);
+    return LookupElement.EMPTY_ARRAY;
   }
 
   public static class JavaFxUnresolvedTagRefsProvider extends UnresolvedReferenceQuickFixProvider<JavaFxTagNameReference> {
@@ -112,22 +91,6 @@ public class JavaFxTagNameReference extends TagNameReference{
     @Override
     public Class<JavaFxTagNameReference> getReferenceClass() {
       return JavaFxTagNameReference.class;
-    }
-  }
-
-  private static class JavaFxTagInsertHandler extends XmlTagInsertHandler {
-    public static final JavaFxTagInsertHandler INSTANCE = new JavaFxTagInsertHandler();
-
-    @Override
-    public void handleInsert(InsertionContext context, LookupElement item) {
-      super.handleInsert(context, item);
-      final Object object = item.getObject();
-      if (object instanceof JavaFxClassBackedElementDescriptor) {
-        final XmlFile xmlFile = (XmlFile)context.getFile();
-        final String shortName = ((JavaFxClassBackedElementDescriptor)object).getName();
-        context.commitDocument();
-        JavaFxPsiUtil.insertImportWhenNeeded(xmlFile, shortName, ((JavaFxClassBackedElementDescriptor)object).getQualifiedName());
-      }
     }
   }
 }
