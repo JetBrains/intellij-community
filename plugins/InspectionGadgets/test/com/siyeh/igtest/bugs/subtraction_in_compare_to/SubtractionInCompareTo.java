7316d63ae80534e7125b78e345c5757dd3826a99
Bas Leijdekkers <basleijdekkers@gmail.com>
2013-06-16 15:14:41 +0200

    IDEA-108605 (Inspection in compareTo() or compare() might cause integer overflow)

--- plugins/InspectionGadgets/test/com/siyeh/igtest/bugs/subtraction_in_compare_to/SubtractionInCompareTo.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/bugs/subtraction_in_compare_to/SubtractionInCompareTo.java
@@ -1,6 +1,6 @@
 package com.siyeh.igtest.bugs.subtraction_in_compare_to;
 
-public class SubtractionInCompareTo
+public class SubtractionInCompareTo implements java.util.Comparator<SubtractionInCompareTo>
 {
     private int m_foo = 3;
 
@@ -8,5 +8,11 @@ public class SubtractionInCompareTo
     {
         final int temp = m_foo + ((SubtractionInCompareTo)foo).m_foo;
         return m_foo - ((SubtractionInCompareTo)foo).m_foo;
+    }
+
+
+    @Override
+    public int compare(SubtractionInCompareTo o1, SubtractionInCompareTo o2) {
+        return o1.m_foo - o2.m_foo;
     }
 }
