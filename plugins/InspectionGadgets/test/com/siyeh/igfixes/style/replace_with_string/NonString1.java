739af14ffc38574f0edd856b1e70072776e0c485
Bas Leijdekkers <basleijdekkers@gmail.com>
2013-06-18 16:17:13 +0200

    Inspection warns and fixes more cases IDEA-106632 (Invalid "StringBuilder can be replaced with String" inspection warning) IDEA-106803 (IDEA does wrong auto-replacement StringBuilder to String)

--- plugins/InspectionGadgets/test/com/siyeh/igfixes/style/replace_with_string/NonString1.java
+++ plugins/InspectionGadgets/test/com/siyeh/igfixes/style/replace_with_string/NonString1.java
@@ -3,6 +3,6 @@ package com.siyeh.igfixes.style.replace_with_string;
 class NonString1 {
 
   String foo(CharSequence text) {
-    return new <caret>StringBuilder(text).toString();
+    return new <caret>StringBuilder(text).toString(); // no toString() because of NPEs
   }
 }
