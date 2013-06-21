d77f4e3a0918c3e63a618aba6600133755e36044
Konstantin Bulenkov <kb@jetbrains.com>
2013-06-21 10:53:21 +0200

    workaround for IDEA-109227 (cherry picked from commit 935a6cb)

--- bin/scripts/unix/idea.sh
+++ bin/scripts/unix/idea.sh
@@ -159,7 +159,7 @@ if [ "$IS_EAP" = "true" ]; then
   OS_NAME=`echo $OS_TYPE | "$TR" '[:upper:]' '[:lower:]'`
   AGENT_LIB="yjpagent-$OS_NAME$BITS"
   if [ -r "$IDE_BIN_HOME/lib$AGENT_LIB.so" ]; then
-    AGENT="-agentlib:$AGENT_LIB=disablej2ee,disablealloc,sessionname=@@system_selector@@"
+    AGENT="-agentlib:$AGENT_LIB=disablej2ee,disablealloc,delay=10000,sessionname=@@system_selector@@"
   fi
 fi
 
