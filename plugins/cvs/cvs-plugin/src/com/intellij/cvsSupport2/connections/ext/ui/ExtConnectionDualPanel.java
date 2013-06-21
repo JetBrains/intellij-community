22a0224d67cab1170367fddcdaefe2db257a191d
Bas Leijdekkers <basleijdekkers@gmail.com>
2013-06-21 12:00:44 +0200

    enable proxy settings for ext root using internal ssh implementation

--- plugins/cvs/cvs-plugin/src/com/intellij/cvsSupport2/connections/ext/ui/ExtConnectionDualPanel.java
+++ plugins/cvs/cvs-plugin/src/com/intellij/cvsSupport2/connections/ext/ui/ExtConnectionDualPanel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2011 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,23 +19,27 @@ import com.intellij.CvsBundle;
 import com.intellij.cvsSupport2.config.ExtConfiguration;
 import com.intellij.cvsSupport2.config.SshSettings;
 import com.intellij.cvsSupport2.connections.ssh.ui.SshConnectionSettingsPanel;
+import com.intellij.cvsSupport2.ui.CvsRootChangeListener;
 import com.intellij.openapi.project.Project;
+import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NonNls;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Collection;
 
 public class ExtConnectionDualPanel {
 
   private final ExtConnectionSettingsPanel myExtSettingsPanel;
   private final SshConnectionSettingsPanel mySshSettingsPanel;
 
+  private final Collection<CvsRootChangeListener> myCvsRootChangeListeners = ContainerUtil.createLockFreeCopyOnWriteList();
+
   private final JPanel myPanel = new JPanel(new BorderLayout());
   private final JPanel myDualPanel = new JPanel(new CardLayout());
-  private final JCheckBox myUseInternalImplementationCheckBox =
-    new JCheckBox(CvsBundle.message("checkbox.text.use.internal.ssh.implementation"));
+  private final JCheckBox myUseInternalImplementationCheckBox = new JCheckBox(CvsBundle.message("checkbox.text.use.internal.ssh.implementation"));
   @NonNls private static final String EXT = "EXT";
   @NonNls private static final String SSH = "SSH";
 
@@ -52,12 +56,23 @@ public class ExtConnectionDualPanel {
     myUseInternalImplementationCheckBox.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         updatePage();
+        notifyListeners();
       }
     });
   }
 
+  public void addCvsRootChangeListener(CvsRootChangeListener l) {
+    myCvsRootChangeListeners.add(l);
+  }
+
+  private void notifyListeners() {
+    for (CvsRootChangeListener cvsRootChangeListener : myCvsRootChangeListeners) {
+      cvsRootChangeListener.onCvsRootChanged();
+    }
+  }
+
   private void updatePage() {
-    final CardLayout cardLayout = ((CardLayout)myDualPanel.getLayout());
+    final CardLayout cardLayout = (CardLayout)myDualPanel.getLayout();
     if (myUseInternalImplementationCheckBox.isSelected()){
       cardLayout.show(myDualPanel, SSH);
     } else {
@@ -90,5 +105,9 @@ public class ExtConnectionDualPanel {
     myExtSettingsPanel.saveTo(extConfiguration);
     mySshSettingsPanel.saveTo(sshSettings);
     extConfiguration.USE_INTERNAL_SSH_IMPLEMENTATION = myUseInternalImplementationCheckBox.isSelected();
+  }
+
+  public boolean isUseInternalSshImplementation() {
+    return myUseInternalImplementationCheckBox.isSelected();
   }
 }
