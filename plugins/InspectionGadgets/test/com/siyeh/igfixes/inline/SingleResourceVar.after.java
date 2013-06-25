da5ea78c1b8ae8e248413e46b3c199d5b9484447
Bas Leijdekkers <basleijdekkers@gmail.com>
2013-06-25 13:06:47 +0200

    fix test

--- plugins/InspectionGadgets/test/com/siyeh/igfixes/inline/SingleResourceVar.after.java
+++ plugins/InspectionGadgets/test/com/siyeh/igfixes/inline/SingleResourceVar.after.java
@@ -1,8 +1,9 @@
 class C {
     void m() throws Exception {
-        AutoCloseable r1 = null;
-        try {
+        try (AutoCloseable r1 = null) {
+          try {
             System.out.println(r1 + ", " + r1);
+          }
         }
     }
 }
