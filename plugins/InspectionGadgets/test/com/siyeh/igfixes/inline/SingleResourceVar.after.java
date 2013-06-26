65c6404d12b577a3a1ecb69e17d774028a5eec41
Anna Kozlova <anna.kozlova@jetbrains.com>
2013-06-26 11:05:16 +0400

    testdata fixed

--- plugins/InspectionGadgets/test/com/siyeh/igfixes/inline/SingleResourceVar.after.java
+++ plugins/InspectionGadgets/test/com/siyeh/igfixes/inline/SingleResourceVar.after.java
@@ -1,9 +1,7 @@
 class C {
     void m() throws Exception {
         try (AutoCloseable r1 = null) {
-          try {
             System.out.println(r1 + ", " + r1);
-          }
         }
     }
 }
