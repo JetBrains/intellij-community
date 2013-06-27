e686f8dba2155bd085b137f7182fa8564be94afe
Denis.Zhdanov <Denis.Zhdanov@jetbrains.com>
2013-06-27 18:48:18 +0400

    IDEA-109722 Gradle tool window: when a project node is selected, "Detach" button become disabled as soon as mouse pointer enters Recent Tasks area

--- platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/task/ui/ExternalSystemTasksPanel.java
+++ platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/task/ui/ExternalSystemTasksPanel.java
@@ -85,7 +85,10 @@ public class ExternalSystemTasksPanel extends SimpleToolWindowPanel implements D
     myRecentTasksList = new ExternalSystemRecentTasksList(recentTasksModel, externalSystemId, project) {
       @Override
       protected void processMouseEvent(MouseEvent e) {
-        mySelectedTaskProvider = myRecentTasksList;
+        if (e.getClickCount() > 0) {
+          mySelectedTaskProvider = myRecentTasksList;
+          myAllTasksTree.getSelectionModel().clearSelection();
+        }
         super.processMouseEvent(e);
       }
     };
@@ -94,7 +97,10 @@ public class ExternalSystemTasksPanel extends SimpleToolWindowPanel implements D
     myAllTasksTree = new ExternalSystemTasksTree(myAllTasksModel, settings.getExpandStates()) {
       @Override
       protected void processMouseEvent(MouseEvent e) {
-        mySelectedTaskProvider = myAllTasksTree;
+        if (e.getClickCount() > 0) {
+          mySelectedTaskProvider = myAllTasksTree;
+          myRecentTasksList.getSelectionModel().clearSelection();
+        }
         super.processMouseEvent(e);
       }
     };
