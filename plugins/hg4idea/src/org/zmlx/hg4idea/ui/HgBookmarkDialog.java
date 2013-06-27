9faa03642a503db52cadb19ac0a39826ebf0db07
Nadya Zabrodina <Nadya.Zabrodina@jetbrains.com>
2013-06-27 16:54:37 +0400

    Hg NewBookmark dialog changed without using ui designer form.

--- plugins/hg4idea/src/org/zmlx/hg4idea/ui/HgBookmarkDialog.java
+++ plugins/hg4idea/src/org/zmlx/hg4idea/ui/HgBookmarkDialog.java
@@ -4,22 +4,31 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.ui.ValidationInfo;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.ui.components.JBCheckBox;
+import com.intellij.ui.components.JBLabel;
+import com.intellij.ui.components.JBTextField;
+import com.intellij.util.ui.GridBag;
+import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.awt.*;
+
+import static com.intellij.util.ui.UIUtil.DEFAULT_HGAP;
+import static com.intellij.util.ui.UIUtil.DEFAULT_VGAP;
 
 /**
  * @author Nadya Zabrodina
  */
 public class HgBookmarkDialog extends DialogWrapper {
-  @NotNull private JPanel myContentPanel;
-  @NotNull private JTextField myBookmarkName;
-  @NotNull private JCheckBox myActiveCheckbox;
+  @NotNull private JBTextField myBookmarkName;
+  @NotNull private JBCheckBox myActiveCheckbox;
 
   public HgBookmarkDialog(@Nullable Project project) {
     super(project, false);
     setTitle("Create Bookmark");
+    setResizable(false);
     init();
   }
 
@@ -41,9 +50,30 @@ public class HgBookmarkDialog extends DialogWrapper {
     return HgBookmarkDialog.class.getName();
   }
 
+  @Override
   @NotNull
   protected JComponent createCenterPanel() {
-    return myContentPanel;
+
+    JPanel contentPanel = new JPanel(new GridBagLayout());
+    GridBag g = new GridBag()
+      .setDefaultInsets(new Insets(0, 0, DEFAULT_VGAP, DEFAULT_HGAP))
+      .setDefaultAnchor(GridBagConstraints.LINE_START)
+      .setDefaultFill(GridBagConstraints.HORIZONTAL);
+
+    JLabel icon = new JLabel(UIUtil.getQuestionIcon(), SwingConstants.LEFT);
+    myBookmarkName = new JBTextField(13);
+
+
+    JBLabel bookmarkLabel = new JBLabel("Bookmark name:");
+    bookmarkLabel.setLabelFor(myBookmarkName);
+
+    myActiveCheckbox = new JBCheckBox("Inactive", false);
+
+    contentPanel.add(icon, g.nextLine().next().coverColumn(3).pady(DEFAULT_HGAP));
+    contentPanel.add(bookmarkLabel, g.next().fillCellNone().insets(new Insets(0, 6, DEFAULT_VGAP, DEFAULT_HGAP)));
+    contentPanel.add(myBookmarkName, g.next().coverLine().setDefaultWeightX(1));
+    contentPanel.add(myActiveCheckbox, g.nextLine().next().next().coverLine(2));
+    return contentPanel;
   }
 
   public boolean isActive() {
