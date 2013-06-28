47cd4d259c3b4cbbaa5a4f04c50e4bad5258b9bc
Anna Kozlova <anna.kozlova@jetbrains.com>
2013-06-28 19:00:38 +0400

    fix tests (cherry picked from commit c7c226153308894c27c86f5ad60d3730bd19c78d)

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/genericsHighlighting/IDEA67672.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/genericsHighlighting/IDEA67672.java
@@ -3,5 +3,5 @@ import java.util.List;
 
 interface A
 {
-    <<error descr="'addAll(Collection<? extends E>)' in 'java.util.Collection' clashes with 'addAll(Collection<? extends E>)' in 'java.util.List'; both methods have same erasure, yet neither overrides the other"></error>T extends List<?> & Collection<? extends Cloneable>> void foo(T x);
+    <<error descr="'add(E)' in 'java.util.List' clashes with 'add(E)' in 'java.util.Collection'; both methods have same erasure, yet neither overrides the other"></error>T extends List<?> & Collection<? extends Cloneable>> void foo(T x);
 }
