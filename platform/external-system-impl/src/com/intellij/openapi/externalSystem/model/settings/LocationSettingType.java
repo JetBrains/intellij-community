2c259f8c7e1ca48020b26d18849f60d7d61937f8
Denis.Zhdanov <Denis.Zhdanov@jetbrains.com>
2013-06-28 13:02:11 +0400

    IDEA-108171 unable to open Settings/Gradle page in Idea 13

--- platform/external-system-impl/src/com/intellij/openapi/externalSystem/model/settings/LocationSettingType.java
+++ platform/external-system-impl/src/com/intellij/openapi/externalSystem/model/settings/LocationSettingType.java
@@ -7,6 +7,7 @@ import org.jetbrains.annotations.PropertyKey;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Arrays;
 
 /**
  * Enumerates possible types of 'gradle home' location setting.
@@ -17,7 +18,7 @@ import java.awt.*;
 public enum LocationSettingType {
 
   /** User hasn't defined gradle location but the IDE discovered it automatically. */
-  DEDUCED("setting.type.location.deduced", "TextField.inactiveForeground"),
+  DEDUCED("setting.type.location.deduced", "TextField.inactiveForeground", "nimbusDisabledText"),
 
   /** User hasn't defined gradle location and the IDE was unable to discover it automatically. */
   UNKNOWN("setting.type.location.unknown"),
@@ -26,19 +27,28 @@ public enum LocationSettingType {
   EXPLICIT_INCORRECT("setting.type.location.explicit.correct"),
 
   EXPLICIT_CORRECT("setting.type.location.explicit.incorrect");
-
+  
   @NotNull private final String myDescriptionKey;
-  @NotNull private final String myColorKey;
+  @NotNull private final Color myColor;
 
   LocationSettingType(@NotNull String descriptionKey) {
     this(descriptionKey, "TextField.foreground");
   }
 
   LocationSettingType(@NotNull @PropertyKey(resourceBundle = ExternalSystemBundle.PATH_TO_BUNDLE) String descriptionKey,
-                      @NotNull String colorKey)
+                      @NotNull String ... colorKeys)
   {
     myDescriptionKey = descriptionKey;
-    myColorKey = colorKey;
+    Color c = null;
+    for (String key : colorKeys) {
+      c = UIManager.getColor(key);
+      if (c != null) {
+        break;
+      }
+    }
+    
+    assert c != null : "Can't find color for keys " + Arrays.toString(colorKeys);
+    myColor = c;
   }
 
   /**
@@ -50,6 +60,6 @@ public enum LocationSettingType {
 
   @NotNull
   public Color getColor() {
-    return UIManager.getColor(myColorKey);
+    return myColor;
   }
 }
