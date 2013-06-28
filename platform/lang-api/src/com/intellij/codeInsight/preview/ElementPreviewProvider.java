69701d43af6da3a7dcd9940a007b676e43cc88f7
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2013-06-28 13:26:51 +0200

    ImageOrColorPreviewManager now more low-level — high-level hint management moved to new EP com.intellij.elementPreviewProvider implementation ElementPreviewHintProvider
    Now we can implement http://youtrack.jetbrains.com/issue/WEB-1799 (preview not only as hint, but any what you want and can imagine)
    
    LveEdit impl of this new EP is initial.

---
+++ platform/lang-api/src/com/intellij/codeInsight/preview/ElementPreviewProvider.java
@@ -0,0 +1,20 @@
+package com.intellij.codeInsight.preview;
+
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.extensions.ExtensionPointName;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.awt.*;
+
+public interface ElementPreviewProvider {
+  ExtensionPointName<ElementPreviewProvider> EP_NAME = ExtensionPointName.create("com.intellij.elementPreviewProvider");
+
+  boolean isSupportedFile(@NotNull PsiFile psiFile);
+
+  void show(@NotNull PsiElement element, @NotNull Editor editor, @NotNull Point point);
+
+  void hide(@Nullable("if disposed") PsiElement element);
+}
