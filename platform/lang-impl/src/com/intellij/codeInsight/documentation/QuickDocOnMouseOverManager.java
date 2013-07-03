32adea6c54a15efa870c09980e85e5cc775009b7
Denis.Zhdanov <Denis.Zhdanov@jetbrains.com>
2013-07-03 15:19:20 +0400

    IDEA-109748 Quick doc on mouse hover pops up when not hovering over anything

--- platform/lang-impl/src/com/intellij/codeInsight/documentation/QuickDocOnMouseOverManager.java
+++ platform/lang-impl/src/com/intellij/codeInsight/documentation/QuickDocOnMouseOverManager.java
@@ -22,6 +22,7 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.EditorFactory;
+import com.intellij.openapi.editor.VisualPosition;
 import com.intellij.openapi.editor.event.*;
 import com.intellij.openapi.editor.ex.EditorSettingsExternalizable;
 import com.intellij.openapi.project.Project;
@@ -194,8 +195,14 @@ public class QuickDocOnMouseOverManager {
       closeQuickDocIfPossible();
       return;
     }
+
+    VisualPosition visualPosition = editor.xyToVisualPosition(e.getMouseEvent().getPoint());
+    if (editor.getSoftWrapModel().isInsideOrBeforeSoftWrap(visualPosition)) {
+      closeQuickDocIfPossible();
+      return;
+    }
     
-    int mouseOffset = editor.logicalPositionToOffset(editor.xyToLogicalPosition(e.getMouseEvent().getPoint()));
+    int mouseOffset = editor.logicalPositionToOffset(editor.visualToLogicalPosition(visualPosition));
     PsiElement elementUnderMouse = psiFile.findElementAt(mouseOffset);
     if (elementUnderMouse == null || elementUnderMouse instanceof PsiWhiteSpace) {
       closeQuickDocIfPossible();
