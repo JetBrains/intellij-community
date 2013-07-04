b634c14907f33a899b18b56f61872292142b7a9a
peter <peter@jetbrains.com>
2013-07-04 17:30:27 +0200

    ExportSettingsAction cleanup

--- platform/platform-impl/src/com/intellij/ide/actions/ExportSettingsAction.java
+++ platform/platform-impl/src/com/intellij/ide/actions/ExportSettingsAction.java
@@ -29,7 +29,6 @@ import com.intellij.openapi.application.PathManager;
 import com.intellij.openapi.components.ExportableApplicationComponent;
 import com.intellij.openapi.components.ExportableComponent;
 import com.intellij.openapi.components.ServiceBean;
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
@@ -45,7 +44,6 @@ import java.util.*;
 import java.util.jar.JarOutputStream;
 
 public class ExportSettingsAction extends AnAction implements DumbAware {
-  private static final Logger LOG = Logger.getInstance("#com.intellij.ide.actions.ExportSettingsAction");
 
   public void actionPerformed(AnActionEvent e) {
     Project project = getEventProject(e);
