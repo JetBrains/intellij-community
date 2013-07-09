9eee7ce45fdfc2829af139a855711e9b0de13138
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2013-07-09 17:48:49 +0400

    fixed PY-10186 "Method could be static" should ignore methods annotated with @abstractmethod

--- python/src/com/jetbrains/python/inspections/PyMethodMayBeStaticInspection.java
+++ python/src/com/jetbrains/python/inspections/PyMethodMayBeStaticInspection.java
@@ -57,7 +57,8 @@ public class PyMethodMayBeStaticInspection extends PyInspection {
       if (decoratorList != null) {
         for (PyDecorator decorator : decoratorList.getDecorators()) {
           final String decoratorName = decorator.getName();
-          if (PyNames.STATICMETHOD.equals(decoratorName) || PyNames.CLASSMETHOD.equals(decoratorName)) {
+          if (PyNames.STATICMETHOD.equals(decoratorName) || PyNames.CLASSMETHOD.equals(decoratorName) ||
+              PyNames.ABSTRACTMETHOD.equals(decoratorName)) {
             return;
           }
           final Property property = containingClass.findPropertyByCallable(node);
