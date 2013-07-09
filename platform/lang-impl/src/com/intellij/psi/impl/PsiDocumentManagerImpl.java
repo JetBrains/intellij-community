6bd002dab0dac52669ed8822a0905bcdce9840c0
Alexey Kudravtsev <cdr@intellij.com>
2013-07-09 13:32:32 +0400

    EA-47473 - assert: FileManagerImpl.getCachedPsiFile

--- platform/lang-impl/src/com/intellij/psi/impl/PsiDocumentManagerImpl.java
+++ platform/lang-impl/src/com/intellij/psi/impl/PsiDocumentManagerImpl.java
@@ -64,7 +64,7 @@ public class PsiDocumentManagerImpl extends PsiDocumentManagerBase implements Se
         PsiFile psiFile = ApplicationManager.getApplication().runReadAction(new Computable<PsiFile>() {
           @Override
           public PsiFile compute() {
-            return getCachedPsiFile(virtualFile);
+            return myProject.isDisposed() ? null : getCachedPsiFile(virtualFile);
           }
         });
         fireDocumentCreated(document, psiFile);
