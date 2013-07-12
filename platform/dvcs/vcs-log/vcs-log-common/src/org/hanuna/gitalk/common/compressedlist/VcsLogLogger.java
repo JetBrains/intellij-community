fb619bfc188590534926c74742713bbcad664a9d
Kirill Likhodedov <Kirill.Likhodedov@gmail.com>
2013-07-12 18:03:57 +0400

    Introduce a common logger category for the vcs log.

---
+++ platform/dvcs/vcs-log/vcs-log-common/src/org/hanuna/gitalk/common/compressedlist/VcsLogLogger.java
@@ -0,0 +1,12 @@
+package org.hanuna.gitalk.common.compressedlist;
+
+import com.intellij.openapi.diagnostic.Logger;
+
+/**
+ * @author Kirill Likhodedov
+ */
+public class VcsLogLogger {
+
+  public static final Logger LOG = Logger.getInstance("Vcs.Log");
+
+}
