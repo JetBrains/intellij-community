10d89d644c9206dcff48a09c8c79dfe4bd48b4b9
Max Medvedev <maxim.medvedev@jetbrains.com>
2013-07-14 13:08:49 +0400

    remove obsolete test

--- plugins/groovy/test/org/jetbrains/plugins/groovy/compiler/GppCompilerTest.groovy
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/compiler/GppCompilerTest.groovy
@@ -1,29 +1,28 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.jetbrains.plugins.groovy.compiler
 
 import com.intellij.compiler.CompilerConfiguration
-import com.intellij.compiler.CompilerConfigurationImpl;
+import com.intellij.compiler.CompilerConfigurationImpl
 import com.intellij.compiler.server.BuildManager
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.testFramework.PsiTestUtil
 import org.jetbrains.plugins.groovy.util.TestUtils
-
 /**
  * @author peter
  */
@@ -44,35 +43,6 @@ public abstract class GppCompilerTest extends GroovyCompilerTestCase {
     conf.removeResourceFilePatterns()
     oldPatterns.each { conf.addResourceFilePattern(it) }
     super.tearDown()
-  }
-
-  public void testTraitStubs() throws Throwable {
-    myFixture.addFileToProject("A.groovy", """
-@Trait
-abstract class SomeTrait {
-  abstract def some()
-  def concrete() {}
-}
-
-@Trait
-abstract class AnotherTrait extends SomeTrait {
-  abstract def another()
-}
-
-class Goo implements SomeTrait {
-  def some() {}
-}
-class Bar implements AnotherTrait {
-  def some() {}
-  def another() {}
-}
-""");
-    myFixture.addClass("""
-class Foo implements SomeTrait {
-  public Object some() { return null; }
-  public Object concrete() { return null; }
-}""")
-    assertEmpty(make());
   }
 
   public void testRecompileDependentGroovyClasses() throws Exception {
