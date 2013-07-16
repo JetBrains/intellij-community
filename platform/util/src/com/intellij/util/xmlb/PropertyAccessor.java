b12f51e98757a07b9fcfbeb33ab5824301d2df94
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2013-07-16 11:00:20 +0200

    Pointless nullity check

--- platform/util/src/com/intellij/util/xmlb/PropertyAccessor.java
+++ platform/util/src/com/intellij/util/xmlb/PropertyAccessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2011 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -84,15 +84,8 @@ class PropertyAccessor implements Accessor {
 
   private Annotation[] calcAnnotations() {
     List<Annotation> result = new ArrayList<Annotation>();
-
-    if (myReadMethod != null) {
-      ContainerUtil.addAll(result, myReadMethod.getAnnotations());
-    }
-
-    if (myWriteMethod != null) {
-      ContainerUtil.addAll(result, myWriteMethod.getAnnotations());
-    }
-
+    ContainerUtil.addAll(result, myReadMethod.getAnnotations());
+    ContainerUtil.addAll(result, myWriteMethod.getAnnotations());
     return result.toArray(new Annotation[result.size()]);
   }
 
