03ee8a5d08fb53bf5169a60ed9dc9a61c9bdf6c3
peter <peter@jetbrains.com>
2013-07-19 14:06:50 +0200

    correct equals/hashCode for delegating scopes

--- platform/core-api/src/com/intellij/psi/search/GlobalSearchScope.java
+++ platform/core-api/src/com/intellij/psi/search/GlobalSearchScope.java
@@ -518,6 +518,7 @@ public abstract class GlobalSearchScope extends SearchScope implements ProjectAw
     public boolean equals(Object o) {
       if (this == o) return true;
       if (!(o instanceof FileTypeRestrictionScope)) return false;
+      if (!super.equals(o)) return false;
 
       FileTypeRestrictionScope that = (FileTypeRestrictionScope)o;
 
