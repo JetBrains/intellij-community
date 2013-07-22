9a5af3db19997696903021764b65a2f746ddb1be
Erokhin Stanislav <erokhins239@gmail.com>
2013-07-22 17:48:46 +0400

    add check what all new commits were loaded

--- platform/dvcs/vcs-log/vcs-log-data/src/org/hanuna/gitalk/data/VcsLogJoiner.java
+++ platform/dvcs/vcs-log/vcs-log-data/src/org/hanuna/gitalk/data/VcsLogJoiner.java
@@ -16,13 +16,17 @@
 package org.hanuna.gitalk.data;
 
 import com.intellij.openapi.util.Computable;
+import com.intellij.util.containers.HashSet;
 import com.intellij.vcs.log.CommitParents;
+import com.intellij.vcs.log.Hash;
 import com.intellij.vcs.log.Ref;
 import com.intellij.vcs.log.VcsCommitDetails;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Attaches the block of latest commits, which was read from the VCS, to the existing log structure.
@@ -43,8 +47,48 @@ public class VcsLogJoiner {
   public List<? extends CommitParents> addCommits(@NotNull List<CommitParents> savedLog,
                                                   @NotNull List<? extends CommitParents> firstBlock, @NotNull Collection<Ref> refs,
                                                   @NotNull Computable<List<CommitParents>> wholeLogGetter) {
+    int unsafeBlockSize = getFirstSafeIndex(savedLog, firstBlock, refs);
+    if (unsafeBlockSize == -1) { // firstBlock not enough
+      //TODO
+    }
+
     // TODO
     return firstBlock;
+  }
+
+  /**
+   *
+   * @param savedLog       currently available part of the log.
+   * @param firstBlock     the first n commits read from the VCS.
+   * @param refs           all references (branches) of the repository.
+   * @return -1 if not enough commits in firstBlock
+   */
+  private static int getFirstSafeIndex(@NotNull List<CommitParents> savedLog,
+                                       @NotNull List<? extends CommitParents> firstBlock,
+                                       @NotNull Collection<Ref> refs) {
+    Set<Hash> allUnresolvedLinkedHashes = new HashSet<Hash>();
+    for (Ref ref: refs) {
+      allUnresolvedLinkedHashes.add(ref.getCommitHash());
+    }
+    for (CommitParents commit : firstBlock) {
+      allUnresolvedLinkedHashes.addAll(commit.getParents());
+    }
+    for (CommitParents commit : firstBlock) {
+      allUnresolvedLinkedHashes.remove(commit.getHash());
+    }
+    return getLastSearchIndex(savedLog, allUnresolvedLinkedHashes);
+  }
+
+  private static int getLastSearchIndex(@NotNull List<CommitParents> commits, @NotNull Set<Hash> searchHashes) {
+    int lastIndex = 0;
+    for (CommitParents commit : commits) {
+      if (searchHashes.size() == 0) {
+        return lastIndex;
+      }
+      searchHashes.remove(commit.getHash());
+      lastIndex++;
+    }
+    return -1;
   }
 
 }
