7a247df7ec6e41c97b8fc1511f6f1c4f0d296135
Aleksey Pivovarov <Aleksey.Pivovarov@jetbrains.com>
2013-07-25 13:28:00 +0400

    Github: fix access scopes

--- plugins/github/src/org/jetbrains/plugins/github/api/GithubApiUtil.java
+++ plugins/github/src/org/jetbrains/plugins/github/api/GithubApiUtil.java
@@ -48,7 +48,7 @@ public class GithubApiUtil {
   private static final int CONNECTION_TIMEOUT = 5000;
   private static final Logger LOG = GithubUtil.LOG;
 
-  @NotNull public static final Gson gson = initGson();
+  @NotNull private static final Gson gson = initGson();
 
   private static Gson initGson() {
     GsonBuilder builder = new GsonBuilder();
@@ -235,7 +235,7 @@ public class GithubApiUtil {
    * Github API
    */
 
-  public static <Raw extends DataConstructor<Result>, Result> Result createDataFromRaw(@NotNull Raw rawObject,
+  static <Raw extends DataConstructor<Result>, Result> Result createDataFromRaw(@NotNull Raw rawObject,
                                                                                        @NotNull Class<Result> result) throws JsonException {
     try {
       return rawObject.create();
@@ -245,7 +245,7 @@ public class GithubApiUtil {
     }
   }
 
-  public static <Raw extends DataConstructorDetailed<Result>, Result> Result createDataFromRaw(@NotNull Raw rawObject,
+  static <Raw extends DataConstructorDetailed<Result>, Result> Result createDataFromRaw(@NotNull Raw rawObject,
                                                                                                @NotNull Class<Result> result)
     throws JsonException {
     try {
