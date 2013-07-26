fd743b1582f8ce19bfa1f31bfe0b75a42b9fa49f
Aleksey Pivovarov <Aleksey.Pivovarov@jetbrains.com>
2013-07-26 13:42:53 +0400

    Github: move provider class from ui package

--- plugins/github/src/org/jetbrains/plugins/github/GithubAnnotationGutterActionProvider.java
+++ plugins/github/src/org/jetbrains/plugins/github/GithubAnnotationGutterActionProvider.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jetbrains.plugins.github.ui;
+package org.jetbrains.plugins.github;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.vcs.annotate.AnnotationGutterActionProvider;
