4e2f11d6338fb4cc91f77a1ec14ac6a60ecb7922
Dmitry Jemerov <yole@jetbrains.com>
2013-07-29 17:22:15 +0200

    pull up most commonly used APIs from RunManagerEx to RunManager; remove some unnecessary usages of RunManagerEx

--- python/src/com/jetbrains/python/run/PyRunConfigurationFactoryImpl.java
+++ python/src/com/jetbrains/python/run/PyRunConfigurationFactoryImpl.java
@@ -1,6 +1,6 @@
 package com.jetbrains.python.run;
 
-import com.intellij.execution.RunManagerEx;
+import com.intellij.execution.RunManager;
 import com.intellij.execution.RunnerAndConfigurationSettings;
 import com.intellij.execution.configurations.ConfigurationFactory;
 import com.intellij.execution.configurations.ModuleBasedConfiguration;
@@ -24,16 +24,16 @@ public class PyRunConfigurationFactoryImpl extends PyRunConfigurationFactory {
   @Override
   public RunnerAndConfigurationSettings createRunConfiguration(Module module, ConfigurationFactory factory) {
     final Project project = module.getProject();
-    final RunManagerEx runManagerEx = RunManagerEx.getInstanceEx(project);
+    final RunManager runManager = RunManager.getInstance(project);
     final RunnerAndConfigurationSettings settings = createConfigurationSettings(factory, module);
-    runManagerEx.addConfiguration(settings, false);
-    runManagerEx.setSelectedConfiguration(settings);
+    runManager.addConfiguration(settings, false);
+    runManager.setSelectedConfiguration(settings);
     return settings;
   }
 
   private static RunnerAndConfigurationSettings createConfigurationSettings(ConfigurationFactory factory, @NotNull final Module module) {
     final RunnerAndConfigurationSettings settings =
-      RunManagerEx.getInstanceEx(module.getProject()).createConfiguration(module.getName(), factory);
+      RunManager.getInstance(module.getProject()).createRunConfiguration(module.getName(), factory);
     ModuleBasedConfiguration configuration = (ModuleBasedConfiguration) settings.getConfiguration();
     configuration.setModule(module);
     return settings;
