e55eb3527baf46ff3287af555ea464a0289f4721
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2013-07-31 19:17:57 +0200

    decoder continue WI-17365 internal web server should serve PHP files

--- platform/platform-impl/src/org/jetbrains/io/PortUnificationServerHandler.java
+++ platform/platform-impl/src/org/jetbrains/io/PortUnificationServerHandler.java
@@ -79,15 +79,14 @@ final class PortUnificationServerHandler extends SimpleChannelUpstreamHandler {
   }
 
   @Override
-  public void messageReceived(
-    ChannelHandlerContext context, MessageEvent e) throws Exception {
+  public void messageReceived(ChannelHandlerContext context, MessageEvent e) throws Exception {
     Object m = e.getMessage();
     if (!(m instanceof ChannelBuffer)) {
       context.sendUpstream(e);
       return;
     }
 
-    ChannelBuffer input = (ChannelBuffer) m;
+    ChannelBuffer input = (ChannelBuffer)m;
     if (!input.readable()) {
       return;
     }
