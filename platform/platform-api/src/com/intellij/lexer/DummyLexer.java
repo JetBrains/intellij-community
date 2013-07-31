d50fd3b74410d1991088fbd9646fbe21b63e911e
Fedor Korotkov <fedor.korotkov@gmail.com>
2013-07-31 16:36:42 +0400

    Introduced DummyLexer

---
+++ platform/platform-api/src/com/intellij/lexer/DummyLexer.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2000-2009 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.lexer;
+
+import com.intellij.psi.tree.IElementType;
+
+public class DummyLexer extends LexerBase {
+  private CharSequence myBuffer;
+  private int myStartOffset;
+  private int myEndOffset;
+  private final IElementType myTokenType;
+
+  public DummyLexer(IElementType type) {
+    myTokenType = type;
+  }
+
+  public void start(final CharSequence buffer, final int startOffset, final int endOffset, final int initialState) {
+    myBuffer = buffer;
+    myStartOffset = startOffset;
+    myEndOffset = endOffset;
+  }
+
+  public CharSequence getBufferSequence() {
+    return myBuffer;
+  }
+
+  public int getState() {
+    return 0;
+  }
+
+  public IElementType getTokenType() {
+    return (myStartOffset < myEndOffset ? myTokenType : null);
+  }
+
+  public int getTokenStart() {
+    return myStartOffset;
+  }
+
+  public int getTokenEnd() {
+    return myEndOffset;
+  }
+
+  public void advance() {
+    myStartOffset = myEndOffset;
+  }
+
+  public LexerPosition getCurrentPosition() {
+    return new LexerPositionImpl(0, getState());
+  }
+
+  public void restore(LexerPosition position) {
+  }
+
+  public int getBufferEnd() {
+    return myEndOffset;
+  }
+}
