00df0608a88b5459008aef49c496a7d8bfe9e661
Denis.Zhdanov <Denis.Zhdanov@jetbrains.com>
2013-08-09 19:16:47 +0400

    IDEA-111831 External system: Make it possible to communicate with external system at the ide process

--- platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/ExternalSystemFacadeManager.java
+++ platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/ExternalSystemFacadeManager.java
@@ -1,53 +1,23 @@
 package com.intellij.openapi.externalSystem.service;
 
-import com.intellij.CommonBundle;
-import com.intellij.execution.DefaultExecutionResult;
-import com.intellij.execution.ExecutionException;
-import com.intellij.execution.ExecutionResult;
-import com.intellij.execution.Executor;
-import com.intellij.execution.configurations.CommandLineState;
-import com.intellij.execution.configurations.GeneralCommandLine;
-import com.intellij.execution.configurations.RunProfileState;
-import com.intellij.execution.configurations.SimpleJavaParameters;
-import com.intellij.execution.process.OSProcessHandler;
-import com.intellij.execution.process.ProcessHandler;
-import com.intellij.execution.process.ProcessTerminatedListener;
-import com.intellij.execution.rmi.RemoteProcessSupport;
-import com.intellij.execution.runners.ProgramRunner;
-import com.intellij.ide.actions.OpenProjectFileChooserDescriptor;
 import com.intellij.openapi.Disposable;
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.application.PathManager;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.externalSystem.ExternalSystemManager;
 import com.intellij.openapi.externalSystem.model.ProjectSystemId;
 import com.intellij.openapi.externalSystem.model.settings.ExternalSystemExecutionSettings;
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskId;
-import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskNotificationListener;
 import com.intellij.openapi.externalSystem.service.notification.ExternalSystemProgressNotificationManager;
-import com.intellij.openapi.externalSystem.service.remote.ExternalSystemProgressNotificationManagerImpl;
 import com.intellij.openapi.externalSystem.service.remote.RemoteExternalSystemProgressNotificationManager;
 import com.intellij.openapi.externalSystem.service.remote.wrapper.ExternalSystemFacadeWrapper;
 import com.intellij.openapi.externalSystem.settings.ExternalSystemSettingsManager;
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil;
+import com.intellij.openapi.externalSystem.util.ExternalSystemConstants;
 import com.intellij.openapi.externalSystem.util.IntegrationKey;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectBundle;
 import com.intellij.openapi.project.ProjectManager;
-import com.intellij.openapi.projectRoots.JavaSdkType;
-import com.intellij.openapi.projectRoots.JdkUtil;
-import com.intellij.openapi.projectRoots.Sdk;
-import com.intellij.openapi.projectRoots.SimpleJavaSdkType;
-import com.intellij.openapi.roots.DependencyScope;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.Pair;
-import com.intellij.openapi.util.ShutDownTracker;
-import com.intellij.psi.PsiBundle;
-import com.intellij.util.Alarm;
+import com.intellij.openapi.util.registry.Registry;
 import com.intellij.util.Consumer;
-import com.intellij.util.PathUtil;
-import com.intellij.util.SystemProperties;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.containers.ContainerUtilRt;
 import org.jetbrains.annotations.NotNull;
@@ -57,14 +27,11 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.nio.charset.Charset;
 import java.rmi.RemoteException;
-import java.rmi.server.UnicastRemoteObject;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -79,10 +46,6 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class ExternalSystemFacadeManager {
 
-  private static final Logger LOG = Logger.getInstance("#" + ExternalSystemFacadeManager.class.getName());
-
-  private static final String MAIN_CLASS_NAME = ExternalSystemFacadeImpl.class.getName();
-
   private static final int REMOTE_FAIL_RECOVERY_ATTEMPTS_NUMBER = 3;
 
   private final ConcurrentMap<IntegrationKey, RemoteExternalSystemFacade> myFacadeWrappers = ContainerUtil.newConcurrentMap();
@@ -90,43 +53,30 @@ public class ExternalSystemFacadeManager {
   private final Map<IntegrationKey, Pair<RemoteExternalSystemFacade, ExternalSystemExecutionSettings>> myRemoteFacades
     = ContainerUtil.newConcurrentMap();
 
-  @NotNull private final Lock myLock = new ReentrantLock();
+  @NotNull private final Lock          myLock                   = new ReentrantLock();
+  @NotNull private final AtomicBoolean myInProcessCommunication = new AtomicBoolean();
 
-  private final AtomicReference<RemoteExternalSystemProgressNotificationManager> myExportedNotificationManager
-    = new AtomicReference<RemoteExternalSystemProgressNotificationManager>();
+  @NotNull private final AtomicReference<ExternalSystemCommunicationManager> myCommunicationManager =
+    new AtomicReference<ExternalSystemCommunicationManager>();
 
-  @NotNull private final ExternalSystemProgressNotificationManagerImpl                    myProgressManager;
-  @NotNull private final ExternalSystemSettingsManager                                    mySettingsManager;
-  @NotNull private final RemoteProcessSupport<Object, RemoteExternalSystemFacade, String> mySupport;
 
-  @NotNull private final ThreadLocal<ProjectSystemId> myTargetExternalSystemId = new ThreadLocal<ProjectSystemId>();
+  @NotNull private final ExternalSystemSettingsManager                   mySettingsManager;
+  @NotNull private final RemoteExternalSystemProgressNotificationManager myProgressManager;
+  @NotNull private final RemoteExternalSystemCommunicationManager        myRemoteCommunicationManager;
+  @NotNull private final InProcessExternalSystemCommunicationManager     myInProcessCommunicationManager;
 
   public ExternalSystemFacadeManager(@NotNull ExternalSystemSettingsManager settingsManager,
-                                     @NotNull ExternalSystemProgressNotificationManager notificationManager)
+                                     @NotNull ExternalSystemProgressNotificationManager notificationManager,
+                                     @NotNull RemoteExternalSystemCommunicationManager remoteCommunicationManager,
+                                     @NotNull InProcessExternalSystemCommunicationManager inProcessCommunicationManager)
   {
     mySettingsManager = settingsManager;
-    myProgressManager = (ExternalSystemProgressNotificationManagerImpl)notificationManager;
-    mySupport = new RemoteProcessSupport<Object, RemoteExternalSystemFacade, String>(RemoteExternalSystemFacade.class) {
-      @Override
-      protected void fireModificationCountChanged() {
-      }
-
-      @Override
-      protected String getName(Object o) {
-        return RemoteExternalSystemFacade.class.getName();
-      }
-
-      @Override
-      protected RunProfileState getRunProfileState(Object o, String configuration, Executor executor) throws ExecutionException {
-        return createRunProfileState();
-      }
-    };
-
-    ShutDownTracker.getInstance().registerShutdownTask(new Runnable() {
-      public void run() {
-        shutdown(false);
-      }
-    });
+    myProgressManager = (RemoteExternalSystemProgressNotificationManager)notificationManager;
+    myRemoteCommunicationManager = remoteCommunicationManager;
+    myInProcessCommunicationManager = inProcessCommunicationManager;
+    boolean inProcessCommunication = Registry.is(ExternalSystemConstants.USE_IN_PROCESS_COMMUNICATION_REGISTRY_KEY, false);
+    myInProcessCommunication.set(inProcessCommunication);
+    myCommunicationManager.set(inProcessCommunication ? myInProcessCommunicationManager : myRemoteCommunicationManager);
   }
 
   @NotNull
@@ -138,93 +88,6 @@ public class ExternalSystemFacadeManager {
       }
     }
     return projectManager.getDefaultProject();
-  }
-
-  private RunProfileState createRunProfileState() {
-    return new CommandLineState(null) {
-      private SimpleJavaParameters createJavaParameters() throws ExecutionException {
-
-        final SimpleJavaParameters params = new SimpleJavaParameters();
-        params.setJdk(new SimpleJavaSdkType().createJdk("tmp", SystemProperties.getJavaHome()));
-
-        params.setWorkingDirectory(PathManager.getBinPath());
-        final List<String> classPath = ContainerUtilRt.newArrayList();
-
-        // IDE jars.
-        classPath.addAll(PathManager.getUtilClassPath());
-        ContainerUtil.addIfNotNull(PathUtil.getJarPathForClass(ProjectBundle.class), classPath);
-        ExternalSystemApiUtil.addBundle(params.getClassPath(), "messages.ProjectBundle", ProjectBundle.class);
-        ContainerUtil.addIfNotNull(PathUtil.getJarPathForClass(PsiBundle.class), classPath);
-        ContainerUtil.addIfNotNull(PathUtil.getJarPathForClass(Alarm.class), classPath);
-        ContainerUtil.addIfNotNull(PathUtil.getJarPathForClass(DependencyScope.class), classPath);
-        ContainerUtil.addIfNotNull(PathUtil.getJarPathForClass(ExtensionPointName.class), classPath);
-        ContainerUtil.addIfNotNull(PathUtil.getJarPathForClass(OpenProjectFileChooserDescriptor.class), classPath);
-        ContainerUtil.addIfNotNull(PathUtil.getJarPathForClass(ExternalSystemTaskNotificationListener.class), classPath);
-
-        // External system module jars
-        ContainerUtil.addIfNotNull(PathUtil.getJarPathForClass(getClass()), classPath);
-        ExternalSystemApiUtil.addBundle(params.getClassPath(), "messages.CommonBundle", CommonBundle.class);
-        params.getClassPath().addAll(classPath);
-
-        params.setMainClass(MAIN_CLASS_NAME);
-        params.getVMParametersList().addParametersString("-Djava.awt.headless=true");
-
-        // It may take a while for gradle api to resolve external dependencies. Default RMI timeout
-        // is 15 seconds (http://download.oracle.com/javase/6/docs/technotes/guides/rmi/sunrmiproperties.html#connectionTimeout),
-        // we don't want to get EOFException because of that.
-        params.getVMParametersList().addParametersString(
-          "-Dsun.rmi.transport.connectionTimeout=" + String.valueOf(TimeUnit.HOURS.toMillis(1))
-        );
-//        params.getVMParametersList().addParametersString("-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5009");
-
-        ProjectSystemId externalSystemId = myTargetExternalSystemId.get();
-        if (externalSystemId != null) {
-          ExternalSystemManager<?, ?, ?, ?, ?> manager = ExternalSystemApiUtil.getManager(externalSystemId);
-          if (manager != null) {
-            params.getClassPath().add(PathUtil.getJarPathForClass(manager.getProjectResolverClass().getClass()));
-            params.getProgramParametersList().add(manager.getProjectResolverClass().getName());
-            params.getProgramParametersList().add(manager.getTaskManagerClass().getName());
-            manager.enhanceParameters(params);
-          }
-        }
-
-        return params;
-      }
-
-      @Override
-      @NotNull
-      public ExecutionResult execute(@NotNull Executor executor, @NotNull ProgramRunner runner) throws ExecutionException {
-        ProcessHandler processHandler = startProcess();
-        return new DefaultExecutionResult(null, processHandler, AnAction.EMPTY_ARRAY);
-      }
-
-      @NotNull
-      protected OSProcessHandler startProcess() throws ExecutionException {
-        SimpleJavaParameters params = createJavaParameters();
-        Sdk sdk = params.getJdk();
-        if (sdk == null) {
-          throw new ExecutionException("No sdk is defined. Params: " + params);
-        } 
-
-        final GeneralCommandLine commandLine = JdkUtil.setupJVMCommandLine(
-          ((JavaSdkType)sdk.getSdkType()).getVMExecutablePath(sdk),
-          params,
-          false
-        );
-        final OSProcessHandler processHandler = new OSProcessHandler(commandLine.createProcess(), commandLine.getCommandLineString()) {
-          @Override
-          public Charset getCharset() {
-            return commandLine.getCharset();
-          }
-        };
-        ProcessTerminatedListener.attach(processHandler);
-        return processHandler;
-      }
-    };
-  }
-
-  public synchronized void shutdown(boolean wait) {
-    mySupport.stopAll(wait);
   }
 
   public void onProjectRename(@NotNull String oldName, @NotNull String newName) {
@@ -254,9 +117,9 @@ public class ExternalSystemFacadeManager {
       }
     }
   }
-  
+
   /**
-   * @return              gradle api facade to use
+   * @return gradle api facade to use
    * @throws Exception    in case of inability to return the facade
    */
   @NotNull
@@ -271,7 +134,8 @@ public class ExternalSystemFacadeManager {
     final RemoteExternalSystemFacade facade = myFacadeWrappers.get(key);
     if (facade == null) {
       final RemoteExternalSystemFacade newFacade = (RemoteExternalSystemFacade)Proxy.newProxyInstance(
-        ExternalSystemFacadeManager.class.getClassLoader(), new Class[]{RemoteExternalSystemFacade.class, Consumer.class}, new MyHandler(key)
+        ExternalSystemFacadeManager.class.getClassLoader(), new Class[]{RemoteExternalSystemFacade.class, Consumer.class},
+        new MyHandler(key)
       );
       myFacadeWrappers.putIfAbsent(key, newFacade);
     }
@@ -299,6 +163,12 @@ public class ExternalSystemFacadeManager {
   @SuppressWarnings("ConstantConditions")
   @NotNull
   private RemoteExternalSystemFacade doGetFacade(@NotNull IntegrationKey key, @NotNull Project project) throws Exception {
+    boolean currentInProcess = Registry.is(ExternalSystemConstants.USE_IN_PROCESS_COMMUNICATION_REGISTRY_KEY, false);
+    if (myInProcessCommunication.getAndSet(currentInProcess) != currentInProcess) {
+      myCommunicationManager.get().clear();
+      myCommunicationManager.set(currentInProcess ? myInProcessCommunicationManager : myRemoteCommunicationManager);
+    }
+    
     ExternalSystemManager manager = ExternalSystemApiUtil.getManager(key.getExternalSystemId());
     if (project.isDisposed() || manager == null) {
       return RemoteExternalSystemFacade.NULL_OBJECT;
@@ -315,7 +185,7 @@ public class ExternalSystemFacadeManager {
         return pair.first;
       }
       if (pair != null) {
-        mySupport.stopAll(true);
+        myCommunicationManager.get().clear();
         myFacadeWrappers.clear();
         myRemoteFacades.clear();
       }
@@ -329,16 +199,14 @@ public class ExternalSystemFacadeManager {
   @SuppressWarnings("unchecked")
   @NotNull
   private RemoteExternalSystemFacade doCreateFacade(@NotNull IntegrationKey key, @NotNull Project project) throws Exception {
-    myTargetExternalSystemId.set(key.getExternalSystemId());
-    final RemoteExternalSystemFacade facade = mySupport.acquire(this, project.getName());
-    myTargetExternalSystemId.set(null);
+    final RemoteExternalSystemFacade facade = myCommunicationManager.get().acquire(project.getName(), key.getExternalSystemId());
     if (facade == null) {
-      throw new IllegalStateException("Can't obtain facade to working with gradle api at the remote process. Project: " + project);
+      throw new IllegalStateException("Can't obtain facade to working with external api at the remote process. Project: " + project);
     }
     Disposer.register(project, new Disposable() {
       @Override
       public void dispose() {
-        mySupport.stopAll(true);
+        myCommunicationManager.get().clear();
         myFacadeWrappers.clear();
         myRemoteFacades.clear();
       }
@@ -349,22 +217,6 @@ public class ExternalSystemFacadeManager {
     Pair<RemoteExternalSystemFacade, ExternalSystemExecutionSettings> newPair = Pair.create(result, settings);
     myRemoteFacades.put(key, newPair);
     result.applySettings(newPair.second);
-    RemoteExternalSystemProgressNotificationManager exported = myExportedNotificationManager.get();
-    if (exported == null) {
-      try {
-        exported = (RemoteExternalSystemProgressNotificationManager)UnicastRemoteObject.exportObject(myProgressManager, 0);
-        myExportedNotificationManager.set(exported);
-      }
-      catch (RemoteException e) {
-        exported = myExportedNotificationManager.get();
-      }
-    }
-    if (exported == null) {
-      LOG.warn("Can't export progress manager");
-    }
-    else {
-      result.applyProgressManager(exported);
-    }
     return result;
   }
 
@@ -373,10 +225,10 @@ public class ExternalSystemFacadeManager {
                           @NotNull IntegrationKey key,
                           @NotNull Pair<RemoteExternalSystemFacade, ExternalSystemExecutionSettings> pair)
   {
-    // Check if remote process is alive.
+     if (!myCommunicationManager.get().isAlive(pair.first)) {
+      return false;
+    }
     try {
-      pair.first.getResolver();
-
       ExternalSystemExecutionSettings currentSettings
         = mySettingsManager.getExecutionSettings(project, key.getExternalProjectConfigPath(), key.getExternalSystemId());
       if (!currentSettings.equals(pair.second)) {
