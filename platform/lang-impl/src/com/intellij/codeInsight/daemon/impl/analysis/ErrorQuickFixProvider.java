df6391aed20d872ac6dabb74eb43cf7276048baf
Alexey Kudravtsev <cdr@intellij.com>
2013-08-13 17:50:46 +0400

    notnull

--- platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/analysis/ErrorQuickFixProvider.java
+++ platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/analysis/ErrorQuickFixProvider.java
@@ -19,12 +19,13 @@ package com.intellij.codeInsight.daemon.impl.analysis;
 import com.intellij.codeInsight.daemon.impl.HighlightInfo;
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.psi.PsiErrorElement;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author yole
  */
 public interface ErrorQuickFixProvider {
   ExtensionPointName<ErrorQuickFixProvider> EP_NAME = ExtensionPointName.create("com.intellij.errorQuickFixProvider");
-  
-  void registerErrorQuickFix(PsiErrorElement errorElement, HighlightInfo highlightInfo);
+
+  void registerErrorQuickFix(@NotNull PsiErrorElement errorElement, @NotNull HighlightInfo highlightInfo);
 }
