9c069903cf80ada2f5085e6c8661eed8fef8327c
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2013-08-13 16:52:11 +0200

    EA-48341 << java.lang.NullPointerException          	at com.intellij.ide.XmlRpcServerImpl.getHandler(XmlRpcServerImpl.java:123)

--- platform/platform-impl/src/com/intellij/ide/XmlRpcServerImpl.java
+++ platform/platform-impl/src/com/intellij/ide/XmlRpcServerImpl.java
@@ -17,6 +17,7 @@ package com.intellij.ide;
 
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.extensions.Extensions;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.CharsetToolkit;
 import gnu.trove.THashMap;
 import org.apache.xmlrpc.*;
@@ -92,6 +93,12 @@ public class XmlRpcServerImpl implements XmlRpcServer {
       ChannelBufferInputStream in = new ChannelBufferInputStream(request.getContent());
       try {
         XmlRpcServerRequest xmlRpcServerRequest = new XmlRpcRequestProcessor().decodeRequest(in);
+
+        if (StringUtil.isEmpty(xmlRpcServerRequest.getMethodName())) {
+          LOG.warn("method name empty");
+          return false;
+        }
+
         Object response = invokeHandler(getHandler(xmlRpcServerRequest.getMethodName(), handlers == null ? handlerMapping : handlers), xmlRpcServerRequest);
         result = ChannelBuffers.copiedBuffer(new XmlRpcResponseProcessor().encodeResponse(response, CharsetToolkit.UTF8));
       }
