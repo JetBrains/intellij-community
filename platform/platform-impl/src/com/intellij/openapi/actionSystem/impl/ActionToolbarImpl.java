3d1dabd5b0c7af20590a957c0e0296eff3da8557
Konstantin Bulenkov <kb@jetbrains.com>
2013-08-13 17:48:03 +0200

    rollback removeAll

--- platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/ActionToolbarImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/ActionToolbarImpl.java
@@ -212,7 +212,6 @@ public class ActionToolbarImpl extends JPanel implements ActionToolbar {
   @Override
   public void removeNotify() {
     super.removeNotify();
-    this.removeAll();
     ourToolbars.remove(this);
     myActionManager.removeTimerListener(myWeakTimerListener);
     myActionManager.removeTransparentTimerListener(myWeakTimerListener);
