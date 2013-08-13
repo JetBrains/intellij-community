8e7b654d75b7293e2c6f4986a5195ab082917674
nik <Nikolay.Chashnikov@jetbrains.com>
2013-08-13 18:38:01 +0400

    remote servers: show deployment log in 'Application Servers' view

--- platform/remote-servers/api/src/com/intellij/remoteServer/runtime/ServerConnection.java
+++ platform/remote-servers/api/src/com/intellij/remoteServer/runtime/ServerConnection.java
@@ -1,9 +1,12 @@
 package com.intellij.remoteServer.runtime;
 
+import com.intellij.openapi.ui.ComponentContainer;
 import com.intellij.remoteServer.configuration.RemoteServer;
 import com.intellij.remoteServer.configuration.deployment.DeploymentConfiguration;
 import com.intellij.remoteServer.runtime.deployment.DeploymentTask;
+import com.intellij.util.ParameterizedRunnable;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 
@@ -24,10 +27,13 @@ public interface ServerConnection<D extends DeploymentConfiguration> {
   void connect(@NotNull Runnable onFinished);
 
 
-  void deploy(@NotNull DeploymentTask<D> task);
+  void deploy(@NotNull DeploymentTask<D> task, @NotNull ParameterizedRunnable<String> onDeploymentStarted);
 
   void computeDeployments(@NotNull Runnable onFinished);
 
   @NotNull
   Collection<Deployment> getDeployments();
+
+  @Nullable
+  ComponentContainer getLogConsole(@NotNull Deployment deployment);
 }
--- plugins/google-app-engine/source/com/intellij/appengine/cloud/AppEngineCloudType.java
+++ plugins/google-app-engine/source/com/intellij/appengine/cloud/AppEngineCloudType.java
@@ -183,7 +183,7 @@ public class AppEngineCloudType extends ServerType<AppEngineServerConfiguration>
       Artifact artifact = ((ArtifactDeploymentSource)task.getSource()).getArtifact();
       if (artifact == null) return;
 
-      AppEngineUploader uploader = AppEngineUploader.createUploader(task.getProject(), artifact, myConfiguration, callback);
+      AppEngineUploader uploader = AppEngineUploader.createUploader(task.getProject(), artifact, myConfiguration, callback, task.getLoggingHandler());
       if (uploader != null) {
         uploader.startUploading();
       }
