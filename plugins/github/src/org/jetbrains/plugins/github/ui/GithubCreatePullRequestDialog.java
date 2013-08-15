b62e8cd96ecd3353641e612ea17aa4d8c9784756
Aleksey Pivovarov <Aleksey.Pivovarov@jetbrains.com>
2013-08-15 15:36:23 +0400

    Github: show diff for new PullRequest
    * lookup configured GitHub remotes

--- plugins/github/src/org/jetbrains/plugins/github/ui/GithubCreatePullRequestDialog.java
+++ plugins/github/src/org/jetbrains/plugins/github/ui/GithubCreatePullRequestDialog.java
@@ -19,13 +19,14 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.ui.ValidationInfo;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.util.Consumer;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.TestOnly;
 import org.jetbrains.plugins.github.GithubSettings;
 
 import javax.swing.*;
-import java.util.List;
+import java.util.Collection;
 import java.util.regex.Pattern;
 
 /**
@@ -35,9 +36,12 @@ public class GithubCreatePullRequestDialog extends DialogWrapper {
   private final GithubCreatePullRequestPanel myGithubCreatePullRequestPanel;
   private static final Pattern GITHUB_REPO_PATTERN = Pattern.compile("[a-zA-Z0-9_.-]+:[a-zA-Z0-9_.-]+");
 
-  public GithubCreatePullRequestDialog(@NotNull final Project project, @NotNull List<String> branches, @Nullable String suggestedBranch) {
+  public GithubCreatePullRequestDialog(@NotNull final Project project,
+                                       @NotNull Collection<String> branches,
+                                       @Nullable String suggestedBranch,
+                                       @NotNull Consumer<String> showDiff) {
     super(project, true);
-    myGithubCreatePullRequestPanel = new GithubCreatePullRequestPanel();
+    myGithubCreatePullRequestPanel = new GithubCreatePullRequestPanel(showDiff);
 
     myGithubCreatePullRequestPanel.setBranches(branches);
 
