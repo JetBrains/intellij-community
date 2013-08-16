5780e7e7dd8fdfb176ef7366638d7de5e6753686
Denis.Zhdanov <Denis.Zhdanov@jetbrains.com>
2013-08-16 12:35:51 +0400

    IDEA-50450 Gradle code insight
    Basic infrastructure for gradle script contributors

--- plugins/gradle/src/org/jetbrains/plugins/gradle/service/resolve/GradleScriptContributor.java
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/service/resolve/GradleScriptContributor.java
@@ -19,16 +19,15 @@ import com.intellij.psi.*;
 import com.intellij.psi.scope.PsiScopeProcessor;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.containers.ContainerUtilRt;
-import org.jetbrains.annotations.NonNls;
+import com.intellij.util.containers.Stack;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.gradle.util.GradleConstants;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrExpression;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrMethodCall;
-import org.jetbrains.plugins.groovy.lang.psi.impl.GroovyPsiManager;
 import org.jetbrains.plugins.groovy.lang.psi.impl.synthetic.GroovyScriptClass;
 import org.jetbrains.plugins.groovy.lang.resolve.NonCodeMembersContributor;
 
-import java.util.Map;
+import java.util.List;
 
 /**
  * @author Denis Zhdanov
@@ -36,21 +35,13 @@ import java.util.Map;
  */
 public class GradleScriptContributor extends NonCodeMembersContributor {
 
-  // TODO den add doc
-  @NotNull private static final Map<String, String> CONTRIBUTORS = ContainerUtilRt.newHashMap();
-  static {
-    CONTRIBUTORS.put("repositories", "org.gradle.api.artifacts.dsl.RepositoryHandler");
-    CONTRIBUTORS.put("dependencies", "org.gradle.api.artifacts.dsl.DependencyHandler");
-  }
-
-  @NotNull @NonNls private static final String ROOT_CONTRIBUTOR_CLASS_NAME = "org.gradle.api.Project";
-
   @Override
   public void processDynamicElements(@NotNull PsiType qualifierType,
                                      PsiClass aClass,
                                      PsiScopeProcessor processor,
                                      PsiElement place,
-                                     ResolveState state) {
+                                     ResolveState state)
+  {
     if (place == null) {
       return;
     }
@@ -64,29 +55,23 @@ public class GradleScriptContributor extends NonCodeMembersContributor {
       return;
     }
 
-    GroovyPsiManager psiManager = GroovyPsiManager.getInstance(aClass.getProject());
-    String contributorClassName = ROOT_CONTRIBUTOR_CLASS_NAME;
+    List<String> methodInfo = ContainerUtilRt.newArrayList();
     for (GrMethodCall current = PsiTreeUtil.getParentOfType(place, GrMethodCall.class);
          current != null;
          current = PsiTreeUtil.getParentOfType(current, GrMethodCall.class))
     {
       GrExpression expression = current.getInvokedExpression();
-      if (expression != null) {
-        String text = expression.getText();
-        if (text == null || text.equals(place.getText())) {
-          continue;
-        }
-        String s = CONTRIBUTORS.get(text);
-        if (s != null) {
-          contributorClassName = s;
-          break;
-        }
+      if (expression == null) {
+        continue;
+      }
+      String text = expression.getText();
+      if (text != null) {
+        methodInfo.add(text);
       }
     }
 
-    PsiClass contributorClass = psiManager.findClassWithCache(contributorClassName, place.getResolveScope());
-    if (contributorClass != null) {
-      contributorClass.processDeclarations(processor, state, null, place);
+    for (GradleMethodContextContributor contributor : GradleMethodContextContributor.EP_NAME.getExtensions()) {
+      contributor.process(methodInfo, processor, state, place);
     }
   }
 }
