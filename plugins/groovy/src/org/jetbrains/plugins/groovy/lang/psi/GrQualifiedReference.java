a712f87da8946d5fb30bb90745d0cf404f51c779
Max Medvedev <maxim.medvedev@jetbrains.com>
2013-08-17 12:58:15 +0400

    pull up 'isQualified()' method

--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/GrQualifiedReference.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/GrQualifiedReference.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2011 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@
 package org.jetbrains.plugins.groovy.lang.psi;
 
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiQualifiedReference;
 import com.intellij.psi.PsiQualifiedReferenceElement;
 import org.jetbrains.annotations.Nullable;
 
@@ -31,4 +30,6 @@ public interface GrQualifiedReference<Qualifier extends PsiElement> extends PsiQ
 
   @Nullable
   PsiElement getReferenceNameElement();
+
+  boolean isQualified();
 }
--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/impl/statements/expressions/GrThisSuperReferenceExpressionBase.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/impl/statements/expressions/GrThisSuperReferenceExpressionBase.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jetbrains.plugins.groovy.lang.psi.impl.statements.expressions;
 
 import com.intellij.lang.ASTNode;
@@ -45,6 +60,11 @@ public abstract class GrThisSuperReferenceExpressionBase extends GrExpressionImp
   @Override
   public PsiElement getReferenceNameElement() {
     return getLastChild();
+  }
+
+  @Override
+  public boolean isQualified() {
+    return getQualifier() != null;
   }
 
   @Override
