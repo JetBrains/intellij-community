df94be3c39b8c486e3cfcc5abbbf753d73fe2482
Aleksey Pivovarov <Aleksey.Pivovarov@jetbrains.com>
2013-08-19 13:58:47 +0400

    Github: fix parameters order

--- plugins/github/src/org/jetbrains/plugins/github/ui/GithubSettingsPanel.java
+++ plugins/github/src/org/jetbrains/plugins/github/ui/GithubSettingsPanel.java
@@ -84,18 +84,18 @@ public class GithubSettingsPanel {
         try {
           GithubUser user = GithubUtil.checkAuthData(getAuthData());
           if (GithubAuthData.AuthType.TOKEN.equals(getAuthType())) {
-            GithubNotifications.showInfoDialog(myPane, "Connection successful for user " + user.getLogin(), "Success");
+            GithubNotifications.showInfoDialog(myPane, "Success", "Connection successful for user " + user.getLogin());
           }
           else {
-            GithubNotifications.showInfoDialog(myPane, "Connection successful", "Success");
+            GithubNotifications.showInfoDialog(myPane, "Success", "Connection successful");
           }
         }
         catch (GithubAuthenticationException ex) {
-          GithubNotifications.showErrorDialog(myPane, "Can't login using given credentials: " + ex.getMessage(), "Login Failure");
+          GithubNotifications.showErrorDialog(myPane, "Login Failure", "Can't login using given credentials: " + ex.getMessage());
         }
         catch (IOException ex) {
           LOG.info(ex);
-          GithubNotifications.showErrorDialog(myPane, "Can't login: " + GithubUtil.getErrorTextFromException(ex), "Login Failure");
+          GithubNotifications.showErrorDialog(myPane, "Login Failure", "Can't login: " + GithubUtil.getErrorTextFromException(ex));
         }
       }
     });
