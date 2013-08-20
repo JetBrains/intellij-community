ce7bf8df79f79ee67c317c556a10387a50bbdb6e
Konstantin Bulenkov <kb@jetbrains.com>
2013-08-20 15:23:02 +0200

    Got It messages

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/status/ToolWindowsWidget.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/status/ToolWindowsWidget.java
@@ -30,21 +30,20 @@ import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.wm.*;
 import com.intellij.openapi.wm.impl.IdeFrameImpl;
 import com.intellij.openapi.wm.impl.ToolWindowImpl;
+import com.intellij.ui.GotItMessage;
 import com.intellij.ui.IdeBorderFactory;
-import com.intellij.ui.JBColor;
+import com.intellij.ui.UIBundle;
 import com.intellij.ui.awt.RelativePoint;
 import com.intellij.ui.components.JBLabel;
 import com.intellij.ui.components.JBList;
 import com.intellij.util.Alarm;
 import com.intellij.util.ui.BaseButtonBehavior;
-import com.intellij.util.ui.GraphicsUtil;
 import com.intellij.util.ui.TimedDeadzone;
 import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
@@ -189,22 +188,15 @@ class ToolWindowsWidget extends JLabel implements CustomStatusBarWidget, StatusB
       alarm.addRequest(new Runnable() {
         @Override
         public void run() {
-          final ToolWindowWidgetInfoPanel panel = new ToolWindowWidgetInfoPanel();
-          panel.myButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-          final Balloon balloon = JBPopupFactory.getInstance().createBalloonBuilder(panel.myRoot)
-            .setFillColor(UIUtil.getListBackground())
-            .setHideOnClickOutside(false)
+          GotItMessage.createMessage(UIBundle.message("tool.window.quick.access.title"), UIBundle.message(
+            "tool.window.quick.access.message"))
             .setDisposable(ToolWindowsWidget.this)
-            .createBalloon();
-          panel.myButton.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-              balloon.hide();
-              PropertiesComponent.getInstance().setValue(key, String.valueOf(true));
-            }
-          });
-
-          balloon.show(new RelativePoint(ToolWindowsWidget.this, new Point(10, 0)), Balloon.Position.above);
+            .setCallback(new Runnable() {
+              @Override
+              public void run() {
+                PropertiesComponent.getInstance().setValue(key, String.valueOf(true));
+              }
+            }).show(new RelativePoint(ToolWindowsWidget.this, new Point(10, 0)), Balloon.Position.above);
           Disposer.dispose(alarm);
         }
       }, 5000);
@@ -299,28 +291,5 @@ class ToolWindowsWidget extends JLabel implements CustomStatusBarWidget, StatusB
     Disposer.dispose(this);
     KeyboardFocusManager.getCurrentKeyboardFocusManager().removePropertyChangeListener("focusOwner", this);
     myStatusBar = null;
-  }
-
-  public static class ToolWindowWidgetInfoPanel {
-    JPanel myButton;
-    JPanel myRoot;
-
-    private void createUIComponents() {
-      myButton = new JPanel(new BorderLayout()) {
-        {
-          enableEvents(AWTEvent.MOUSE_EVENT_MASK);
-        }
-        @Override
-        protected void paintComponent(Graphics g) {
-          super.paintComponent(g);
-          GraphicsUtil.setupAAPainting(g);
-          ((Graphics2D)g).setPaint(new GradientPaint(0, 0, new JBColor(new Color(77, 143, 253), new Color(52, 74, 100)), 0, getHeight(),
-                                                     new JBColor(new Color(71, 135, 237), new Color(38, 53, 73))));
-          g.fillRoundRect(0,0,getWidth()-1, getHeight()-1, 5,5);
-          g.setColor(new JBColor(new Color(48, 121, 237), new Color(87, 93, 101)));
-          g.drawRoundRect(0,0,getWidth()-1, getHeight()-1, 5,5);
-        }
-      };
-    }
   }
 }
