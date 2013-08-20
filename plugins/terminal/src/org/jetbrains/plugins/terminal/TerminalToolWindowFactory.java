6b6fef738150850aacb2ce28648fc5caf5b6ec0e
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2013-08-20 18:08:08 +0200

    Work while indexing (PY-10618).

--- plugins/terminal/src/org/jetbrains/plugins/terminal/TerminalToolWindowFactory.java
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/TerminalToolWindowFactory.java
@@ -1,5 +1,6 @@
 package org.jetbrains.plugins.terminal;
 
+import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
@@ -7,7 +8,7 @@ import com.intellij.openapi.wm.ToolWindowFactory;
 /**
  * @author traff
  */
-public class TerminalToolWindowFactory implements ToolWindowFactory {
+public class TerminalToolWindowFactory implements ToolWindowFactory, DumbAware {
   @Override
   public void createToolWindowContent(Project project, ToolWindow toolWindow) {
     TerminalView terminalView = TerminalView.getInstance();
