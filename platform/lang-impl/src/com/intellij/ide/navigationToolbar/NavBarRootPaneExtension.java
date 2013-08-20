5672d96aa369adf90f482c05c0f18c7212a8eaf9
Konstantin Bulenkov <kb@jetbrains.com>
2013-08-20 10:41:07 +0200

    NPE fix

--- platform/lang-impl/src/com/intellij/ide/navigationToolbar/NavBarRootPaneExtension.java
+++ platform/lang-impl/src/com/intellij/ide/navigationToolbar/NavBarRootPaneExtension.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -210,6 +210,9 @@ public class NavBarRootPaneExtension extends IdeRootPaneNorthExtension {
         myScrollPane = null;
         if (myNavigationBar != null && !Disposer.isDisposed(myNavigationBar)) {
           Disposer.dispose(myNavigationBar);
+        }
+        if (myProject == null) {
+          return;
         }
         myNavigationBar = new NavBarPanel(myProject, true);
         myWrapperPanel.putClientProperty("NavBarPanel", myNavigationBar);
