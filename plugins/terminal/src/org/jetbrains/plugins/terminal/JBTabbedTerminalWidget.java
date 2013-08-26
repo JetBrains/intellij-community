a7ebcaf637f6caae5950971478e8a8ea29d99042
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2013-08-27 00:39:06 +0200

    Open session action moved to constructor.

--- plugins/terminal/src/org/jetbrains/plugins/terminal/JBTabbedTerminalWidget.java
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/JBTabbedTerminalWidget.java
@@ -1,23 +1,23 @@
 package org.jetbrains.plugins.terminal;
 
+import com.google.common.base.Predicate;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.DumbAwareAction;
-import com.jediterm.terminal.ui.JediTermWidget;
-import com.jediterm.terminal.ui.SystemSettingsProvider;
-import com.jediterm.terminal.ui.TabbedTerminalWidget;
-import com.jediterm.terminal.ui.TerminalAction;
+import com.jediterm.terminal.ui.*;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import java.awt.event.KeyEvent;
 import java.util.List;
 
 /**
  * @author traff
  */
 public class JBTabbedTerminalWidget extends TabbedTerminalWidget {
-  public JBTabbedTerminalWidget(@NotNull SystemSettingsProvider settingsProvider) {
-    super(settingsProvider);
+
+  public JBTabbedTerminalWidget(@NotNull SystemSettingsProvider settingsProvider, @NotNull Predicate<TerminalWidget> createNewSessionAction) {
+    super(settingsProvider, createNewSessionAction);
 
     convertActions(this, getActions());
   }
@@ -27,7 +27,7 @@ public class JBTabbedTerminalWidget extends TabbedTerminalWidget {
       AnAction a = new DumbAwareAction() {
         @Override
         public void actionPerformed(AnActionEvent e) {
-          action.perform();
+          action.perform(e.getInputEvent() instanceof KeyEvent? (KeyEvent)e.getInputEvent() : null);
         }
       };
       a.registerCustomShortcutSet(action.getKeyCode(), action.getModifiers(), component);
