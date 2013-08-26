828e8485f4e076aeb2a1b535595785f84b6602d2
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2013-08-26 10:03:14 +0400

    fixed PY-10642 Run Configuration: NPE at com.jetbrains.python.testing.PythonTestConfigurationProducer.isConfigurationFromContext

--- python/src/com/jetbrains/python/testing/PythonTestConfigurationProducer.java
+++ python/src/com/jetbrains/python/testing/PythonTestConfigurationProducer.java
@@ -40,7 +40,8 @@ abstract public class PythonTestConfigurationProducer extends RunConfigurationPr
     if (location == null || !isAvailable(location)) return false;
     final PsiElement element = location.getPsiElement();
     final PsiFileSystemItem file = element.getContainingFile();
-    final VirtualFile virtualFile = file.getVirtualFile();
+    final VirtualFile virtualFile = element instanceof PsiDirectory ? ((PsiDirectory)element).getVirtualFile()
+                                                                    : file.getVirtualFile();
     if (virtualFile == null) return false;
     final PyFunction pyFunction = PsiTreeUtil.getParentOfType(element, PyFunction.class, false);
     final PyClass pyClass = PsiTreeUtil.getParentOfType(element, PyClass.class);
