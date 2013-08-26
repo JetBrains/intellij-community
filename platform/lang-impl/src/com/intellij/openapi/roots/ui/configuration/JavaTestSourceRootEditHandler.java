e60a55bdfbe1e0d5d7dd7008bbfd39baf4c2851d
nik <Nikolay.Chashnikov@jetbrains.com>
2013-08-26 15:53:53 +0400

    source roots editors refactored: root type specific UI moved to extension

---
+++ platform/lang-impl/src/com/intellij/openapi/roots/ui/configuration/JavaTestSourceRootEditHandler.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2000-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.openapi.roots.ui.configuration;
+
+import com.intellij.icons.AllIcons;
+import com.intellij.openapi.actionSystem.CustomShortcutSet;
+import com.intellij.openapi.project.ProjectBundle;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.jps.model.java.JavaSourceRootType;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+/**
+ * @author nik
+ */
+public class JavaTestSourceRootEditHandler extends JavaSourceRootEditHandlerBase {
+  public JavaTestSourceRootEditHandler() {
+    super(JavaSourceRootType.TEST_SOURCE);
+  }
+
+
+  @NotNull
+  @Override
+  public String getRootTypeName() {
+    return ProjectBundle.message("module.toggle.test.sources.action");
+  }
+
+  @NotNull
+  @Override
+  public String getRootsGroupTitle() {
+    return ProjectBundle.message("module.paths.test.sources.group");
+  }
+
+  @NotNull
+  @Override
+  public Icon getRootIcon() {
+    return AllIcons.Modules.TestRoot;
+  }
+
+  @Override
+  public CustomShortcutSet getMarkRootShortcutSet() {
+    return new CustomShortcutSet(KeyStroke.getKeyStroke(KeyEvent.VK_T, InputEvent.ALT_MASK));
+  }
+
+  @NotNull
+  @Override
+  public Color getRootsGroupColor() {
+    return ContentRootPanel.TESTS_COLOR;
+  }
+
+  @NotNull
+  @Override
+  public String getUnmarkRootActionName() {
+    return ProjectBundle.message("module.paths.unmark.tests.tooltip");
+  }
+}
