0f0f39eae6ab231b1fd7e564c7970bda21bcea72
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2013-08-27 17:02:19 +0200

    cleanup, Overrides

--- platform/util/src/com/intellij/util/PatternUtil.java
+++ platform/util/src/com/intellij/util/PatternUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -73,7 +73,8 @@ public class PatternUtil {
 //    String pattern = mask.replaceAll("\\.", "\\.").replaceAll("\\*", ".*").replaceAll("\\?", ".");
     try {
       return Pattern.compile(convertToRegex(mask));
-    } catch (PatternSyntaxException e) {
+    }
+    catch (PatternSyntaxException e) {
       LOG.error(mask, e);
       return Pattern.compile("");
     }
