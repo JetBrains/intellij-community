a6ce6f9934f49fa5d1ec470e7465ac498736ba15
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2013-08-29 20:12:38 +0400

    fixed  PY-8823 Enforce PEP 8 naming conventions
    Conflicts:
    
    	python/src/META-INF/python-plugin-common.xml

--- python/src/com/jetbrains/python/inspections/PyShadowingBuiltinsInspection.java
+++ python/src/com/jetbrains/python/inspections/PyShadowingBuiltinsInspection.java
@@ -1,24 +1,10 @@
 package com.jetbrains.python.inspections;
 
 import com.intellij.codeInspection.LocalInspectionToolSession;
-import com.intellij.codeInspection.LocalQuickFix;
-import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.codeInspection.ProblemsHolder;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.fileEditor.OpenFileDescriptor;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.PsiNameIdentifierOwner;
-import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.refactoring.rename.PsiElementRenameHandler;
-import com.intellij.refactoring.rename.RenameProcessor;
-import com.intellij.refactoring.rename.RenamePsiElementProcessor;
-import com.intellij.refactoring.rename.inplace.VariableInplaceRenamer;
-import com.jetbrains.python.codeInsight.dataflow.scope.ScopeUtil;
 import com.jetbrains.python.psi.*;
 import com.jetbrains.python.psi.impl.PyBuiltinCache;
 import org.jetbrains.annotations.NotNull;
@@ -78,53 +64,6 @@ public class PyShadowingBuiltinsInspection extends PyInspection {
           final PsiElement identifier = element.getNameIdentifier();
           registerProblem(identifier != null ? identifier : element, "Shadows a built-in with the same name",
                           new PyRenameElementQuickFix());
-        }
-      }
-    }
-
-    private static class PyRenameElementQuickFix implements LocalQuickFix {
-      @NotNull
-      @Override
-      public String getName() {
-        return "Rename element";
-      }
-
-      @NotNull
-      @Override
-      public String getFamilyName() {
-        return "Rename element";
-      }
-
-      @Override
-      public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
-        final PsiElement element = descriptor.getPsiElement();
-        final PsiNameIdentifierOwner nameOwner = element instanceof PsiNameIdentifierOwner ?
-                                                 (PsiNameIdentifierOwner)element :
-                                                 PsiTreeUtil.getParentOfType(element, PsiNameIdentifierOwner.class, true);
-        if (nameOwner != null) {
-          final VirtualFile virtualFile = nameOwner.getContainingFile().getVirtualFile();
-          if (virtualFile != null) {
-            final Editor editor = FileEditorManager.getInstance(project).openTextEditor(new OpenFileDescriptor(project, virtualFile), true);
-            if (ApplicationManager.getApplication().isUnitTestMode()) {
-              renameInUnitTestMode(project, nameOwner, editor);
-            }
-            else {
-              if (nameOwner instanceof PyNamedParameter || nameOwner instanceof PyTargetExpression) {
-                new VariableInplaceRenamer(nameOwner, editor).performInplaceRename();
-              }
-              else {
-                PsiElementRenameHandler.invoke(nameOwner, project, ScopeUtil.getScopeOwner(nameOwner), editor);
-              }
-            }
-          }
-        }
-      }
-
-      private static void renameInUnitTestMode(@NotNull Project project, @NotNull PsiNameIdentifierOwner nameOwner,
-                                               @Nullable Editor editor) {
-        final PsiElement substitution = RenamePsiElementProcessor.forElement(nameOwner).substituteElementToRename(nameOwner, editor);
-        if (substitution != null) {
-          new RenameProcessor(project, substitution, "a", false, false).run();
         }
       }
     }
