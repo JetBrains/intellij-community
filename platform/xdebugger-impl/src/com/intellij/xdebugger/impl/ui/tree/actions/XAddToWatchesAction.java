c7c8f30d763d2f1a806e8ec95508f2dd3476a13f
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2013-08-30 14:22:39 +0200

    cleanup

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/ui/tree/actions/XAddToWatchesAction.java
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/ui/tree/actions/XAddToWatchesAction.java
@@ -15,21 +15,23 @@
  */
 package com.intellij.xdebugger.impl.ui.tree.actions;
 
-import com.intellij.xdebugger.impl.ui.tree.nodes.XValueNodeImpl;
-import com.intellij.xdebugger.impl.ui.XDebugSessionTab;
-import com.intellij.xdebugger.impl.XDebugSessionImpl;
-import com.intellij.xdebugger.XDebugSession;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.xdebugger.XDebugSession;
+import com.intellij.xdebugger.impl.XDebugSessionImpl;
+import com.intellij.xdebugger.impl.ui.XDebugSessionTab;
+import com.intellij.xdebugger.impl.ui.tree.nodes.XValueNodeImpl;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * @author nik
  */
 public class XAddToWatchesAction extends XDebuggerTreeActionBase {
+  @Override
   protected boolean isEnabled(final XValueNodeImpl node) {
     return super.isEnabled(node) && node.getValueContainer().getEvaluationExpression() != null;
   }
 
+  @Override
   protected void perform(final XValueNodeImpl node, @NotNull final String nodeName, final AnActionEvent e) {
     XDebugSession session = node.getTree().getSession();
     XDebugSessionTab sessionTab = ((XDebugSessionImpl)session).getSessionTab();
