0c00570428a1edae336a82873c74e43c6507f85a
nik <Nikolay.Chashnikov@jetbrains.com>
2013-08-30 13:41:10 +0400

    unused code removed

--- plugins/coverage/src/com/intellij/coverage/CoberturaCoverageRunner.java
+++ plugins/coverage/src/com/intellij/coverage/CoberturaCoverageRunner.java
@@ -5,25 +5,14 @@
 package com.intellij.coverage;
 
 import com.intellij.coverage.info.CoberturaLoaderUtil;
-import com.intellij.execution.ExecutionException;
-import com.intellij.execution.configurations.JavaParameters;
 import com.intellij.execution.configurations.SimpleJavaParameters;
-import com.intellij.execution.process.DefaultJavaProcessHandler;
-import com.intellij.execution.process.ProcessAdapter;
-import com.intellij.execution.process.ProcessEvent;
-import com.intellij.ide.BrowserUtil;
 import com.intellij.openapi.application.PathManager;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.util.SystemInfo;
-import com.intellij.openapi.util.io.FileUtil;
-import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.rt.coverage.data.ProjectData;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import javax.swing.*;
 import java.io.File;
 
 public class CoberturaCoverageRunner extends JavaCoverageRunner {
@@ -56,44 +45,6 @@ public class CoberturaCoverageRunner extends JavaCoverageRunner {
     javaParameters.getVMParametersList().add(argument.toString());
     javaParameters.getVMParametersList().defineProperty("net.sourceforge.cobertura.datafile", sessionDataFilePath);
     javaParameters.getClassPath().add(PathManager.getLibPath() + File.separator + "cobertura.jar");
-  }
-
-  protected void generateJavaReport(@NotNull final Project project,
-                                    boolean trackTestFolders,
-                                    @NotNull final String binaryFilePath,
-                                    final String outputDirectory,
-                                    final boolean openInBrowser) {
-    try {
-      final JavaParameters javaParameters = new JavaParameters();
-      javaParameters.setMainClass("net.sourceforge.cobertura.reporting.Main");
-      javaParameters.getProgramParametersList().add("--destination", outputDirectory);
-      javaParameters.getProgramParametersList().add("--datafile", binaryFilePath);
-      javaParameters.getClassPath().add(PathManager.getLibPath() + File.separator + "cobertura.jar");
-      javaParameters.getClassPath().add(PathManager.getLibPath() + File.separator + "log4j.jar");
-      final ProjectRootManager projectRootManager = ProjectRootManager.getInstance(project);
-      final VirtualFile[] sourceRoots = projectRootManager.getContentSourceRoots();
-      for (VirtualFile sourceRoot : sourceRoots) {
-        javaParameters.getProgramParametersList().add(FileUtil.toSystemDependentName(sourceRoot.getPath()));
-      }
-      javaParameters.setJdk(projectRootManager.getProjectSdk());
-      final DefaultJavaProcessHandler processHandler = new DefaultJavaProcessHandler(javaParameters);
-      processHandler.startNotify();
-      processHandler.addProcessListener(new ProcessAdapter() {
-        public void processTerminated(final ProcessEvent event) {
-          processHandler.removeProcessListener(this);
-          SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-              if (openInBrowser) {
-                BrowserUtil.browse(new File(outputDirectory, "/index.html"));
-              }
-            }
-          });
-        }
-      });
-    }
-    catch (ExecutionException e1) {
-      //should not happen
-    }
   }
 
   public String getPresentableName() {
