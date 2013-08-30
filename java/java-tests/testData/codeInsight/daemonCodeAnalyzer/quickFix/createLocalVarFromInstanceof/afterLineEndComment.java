7c56a37bc40e19478194800e557eb3be9b800918
Anna Kozlova <anna.kozlova@jetbrains.com>
2013-08-30 13:38:21 +0400

    create var from instanceOf check: do not insert before previous line end-line comment

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/createLocalVarFromInstanceof/afterLineEndComment.java
@@ -0,0 +1,10 @@
+// "Insert '(String)o' declaration" "true"
+class C {
+    void f(Object o, Object f) {
+        if (o instanceof String) {//todo comment
+
+            String s = (String) o;
+            
+        }
+    }
+}
