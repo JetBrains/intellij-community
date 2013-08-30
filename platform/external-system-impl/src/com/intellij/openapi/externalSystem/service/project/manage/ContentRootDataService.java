b56c3aab7be34af0446c09221fc5bd7990558831
nik <Nikolay.Chashnikov@jetbrains.com>
2013-08-30 13:36:54 +0400

    unused code removed

--- platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/project/manage/ContentRootDataService.java
+++ platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/project/manage/ContentRootDataService.java
@@ -7,7 +7,6 @@ import com.intellij.openapi.externalSystem.model.ProjectKeys;
 import com.intellij.openapi.externalSystem.model.project.ContentRootData;
 import com.intellij.openapi.externalSystem.model.project.ExternalSystemSourceType;
 import com.intellij.openapi.externalSystem.model.project.ModuleData;
-import com.intellij.openapi.externalSystem.service.project.ModuleAwareContentRoot;
 import com.intellij.openapi.externalSystem.service.project.ProjectStructureHelper;
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil;
 import com.intellij.openapi.externalSystem.util.ExternalSystemConstants;
@@ -34,7 +33,7 @@ import java.util.Set;
  * @since 2/7/12 3:20 PM
  */
 @Order(ExternalSystemConstants.BUILTIN_SERVICE_ORDER)
-public class ContentRootDataService implements ProjectDataService<ContentRootData, ModuleAwareContentRoot> {
+public class ContentRootDataService implements ProjectDataService<ContentRootData, ContentEntry> {
 
   private static final Logger LOG = Logger.getInstance("#" + ContentRootDataService.class.getName());
 
@@ -139,10 +138,7 @@ public class ContentRootDataService implements ProjectDataService<ContentRootDat
   @NotNull
   private static ContentEntry findOrCreateContentRoot(@NotNull ModifiableRootModel model, @NotNull String path) {
     ContentEntry[] entries = model.getContentEntries();
-    if (entries == null) {
-      return model.addContentEntry(toVfsUrl(path));
-    }
-    
+
     for (ContentEntry entry : entries) {
       VirtualFile file = entry.getFile();
       if (file == null) {
@@ -172,11 +168,6 @@ public class ContentRootDataService implements ProjectDataService<ContentRootDat
 
   private static void createExcludedRootIfAbsent(@NotNull ContentEntry entry, @NotNull String path, @NotNull String moduleName) {
     ExcludeFolder[] folders = entry.getExcludeFolders();
-    if (folders == null) {
-      LOG.info(String.format("Importing excluded root '%s' for content root '%s' of module '%s'", path, entry.getUrl(), moduleName));
-      entry.addExcludeFolder(toVfsUrl(path));
-      return;
-    }
     for (ExcludeFolder folder : folders) {
       VirtualFile file = folder.getFile();
       if (file == null) {
@@ -206,37 +197,7 @@ public class ContentRootDataService implements ProjectDataService<ContentRootDat
   }
 
   @Override
-  public void removeData(@NotNull Collection<? extends ModuleAwareContentRoot> toRemove, @NotNull Project project, boolean synchronous) {
-    Map<Module, Collection<ModuleAwareContentRoot>> byModule = ContainerUtilRt.newHashMap();
-    for (ModuleAwareContentRoot root : toRemove) {
-      Collection<ModuleAwareContentRoot> roots = byModule.get(root.getModule());
-      if (roots == null) {
-        byModule.put(root.getModule(), roots = ContainerUtilRt.newArrayList());
-      }
-      roots.add(root);
-    }
-    for (Map.Entry<Module, Collection<ModuleAwareContentRoot>> entry : byModule.entrySet()) {
-      doRemoveData(entry.getValue(), synchronous);
-    }
-  }
-
-  private static void doRemoveData(@NotNull final Collection<ModuleAwareContentRoot> contentRoots, boolean synchronous) {
-    ExternalSystemApiUtil.executeProjectChangeAction(synchronous, new Runnable() {
-      @Override
-      public void run() {
-        for (ModuleAwareContentRoot contentRoot : contentRoots) {
-          final ModuleRootManager moduleRootManager = ModuleRootManager.getInstance(contentRoot.getModule());
-          ModifiableRootModel model = moduleRootManager.getModifiableModel();
-          try {
-            model.removeContentEntry(contentRoot);
-            LOG.info(String.format("Removing content root '%s' from module %s", contentRoot.getUrl(), contentRoot.getModule().getName()));
-          }
-          finally {
-            model.commit();
-          }
-        }
-      }
-    });
+  public void removeData(@NotNull Collection<? extends ContentEntry> toRemove, @NotNull Project project, boolean synchronous) {
   }
 
   private static String toVfsUrl(@NotNull String path) {
