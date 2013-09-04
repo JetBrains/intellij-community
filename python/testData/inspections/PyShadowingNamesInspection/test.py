682d2ad537074f7bdb2f5cda4051dc966835729a
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2013-09-04 18:27:32 +0400

    Changed priority of shadowed names inspection to weak warning

--- python/testData/inspections/PyShadowingNamesInspection/test.py
+++ python/testData/inspections/PyShadowingNamesInspection/test.py
@@ -1,37 +1,37 @@
 def test_import_builtin_names():
     import float
     from foo import float
-    from bar import baz as <warning descr="Shadows built-in name 'float'">float</warning>
+    from bar import baz as <weak_warning descr="Shadows built-in name 'float'">float</weak_warning>
 
 
 def test_builtin_function_parameters():
-    def test1(x, _, <warning descr="Shadows built-in name 'len'">len</warning>, <warning descr="Shadows built-in name 'file'">file</warning>=None):
+    def test1(x, _, <weak_warning descr="Shadows built-in name 'len'">len</weak_warning>, <weak_warning descr="Shadows built-in name 'file'">file</weak_warning>=None):
         pass
 
 
 def test_builtin_function_name():
-    def <warning descr="Shadows built-in name 'list'">list</warning>():
+    def <weak_warning descr="Shadows built-in name 'list'">list</weak_warning>():
         pass
 
 
 def test_builtin_assignment_targets():
     foo = 2
-    <warning descr="Shadows built-in name 'list'">list</warning> = []
-    for <warning descr="Shadows built-in name 'int'">int</warning> in range(10):
+    <weak_warning descr="Shadows built-in name 'list'">list</weak_warning> = []
+    for <weak_warning descr="Shadows built-in name 'int'">int</weak_warning> in range(10):
         print(int)
-    <warning descr="Shadows built-in name 'range'">range</warning> = []
-    <warning descr="Shadows built-in name 'list'">list</warning>, _ = (1, 2)
-    return [int for <warning descr="Shadows built-in name 'int'">int</warning> in range(10)]
+    <weak_warning descr="Shadows built-in name 'range'">range</weak_warning> = []
+    <weak_warning descr="Shadows built-in name 'list'">list</weak_warning>, _ = (1, 2)
+    return [int for <weak_warning descr="Shadows built-in name 'int'">int</weak_warning> in range(10)]
 
 
 def test_builtin_class_name():
-    class <warning descr="Shadows built-in name 'list'">list</warning>(object):
+    class <weak_warning descr="Shadows built-in name 'list'">list</weak_warning>(object):
         pass
 
 
 def test_builtin_method_name():
     class C:
-        def <warning descr="Shadows built-in name 'list'">list</warning>(self):
+        def <weak_warning descr="Shadows built-in name 'list'">list</weak_warning>(self):
             pass
 
 
@@ -47,18 +47,18 @@ def test_builtin_qualified_name():
 # PY-10164
 def test_builtin_class_attribute():
     class C:
-        <warning descr="Shadows built-in name 'id'">id</warning> = 1
+        <weak_warning descr="Shadows built-in name 'id'">id</weak_warning> = 1
 
 
 def test_outer_function():
     foo = 1
     def bar():
-        <warning descr="Shadows name 'foo' from outer scope">foo</warning>, <warning descr="Shadows name 'bar' from outer scope">bar</warning> = 1, 2
-    def baz(<warning descr="Shadows name 'foo' from outer scope">foo</warning>, <warning descr="Shadows name 'bar' from outer scope">bar</warning>, <warning descr="Shadows name 'baz' from outer scope">baz</warning>):
+        <weak_warning descr="Shadows name 'foo' from outer scope">foo</weak_warning>, <weak_warning descr="Shadows name 'bar' from outer scope">bar</weak_warning> = 1, 2
+    def baz(<weak_warning descr="Shadows name 'foo' from outer scope">foo</weak_warning>, <weak_warning descr="Shadows name 'bar' from outer scope">bar</weak_warning>, <weak_warning descr="Shadows name 'baz' from outer scope">baz</weak_warning>):
         pass
     def nested():
-        def <warning descr="Shadows name 'baz' from outer scope">baz</warning>(<warning descr="Shadows name 'foo' from outer scope">foo</warning>):
-            <warning descr="Shadows name 'bar' from outer scope">bar</warning> = 1
+        def <weak_warning descr="Shadows name 'baz' from outer scope">baz</weak_warning>(<weak_warning descr="Shadows name 'foo' from outer scope">foo</weak_warning>):
+            <weak_warning descr="Shadows name 'bar' from outer scope">bar</weak_warning> = 1
 
 
 def test_outer_class():
@@ -68,16 +68,16 @@ def test_outer_class():
     class C(object):
         def foo(self):
             def foo():
-                def <warning descr="Shadows name 'bar' from outer scope">bar</warning>():
-                    class <warning descr="Shadows name 'C' from outer scope">C</warning>:
-                        def <warning descr="Shadows name 'baz' from outer scope">baz</warning>(self):
+                def <weak_warning descr="Shadows name 'bar' from outer scope">bar</weak_warning>():
+                    class <weak_warning descr="Shadows name 'C' from outer scope">C</weak_warning>:
+                        def <weak_warning descr="Shadows name 'baz' from outer scope">baz</weak_warning>(self):
                             pass
             def bar():
                 pass
 
-            <warning descr="Shadows name 'baz' from outer scope">baz</warning> = 2
+            <weak_warning descr="Shadows name 'baz' from outer scope">baz</weak_warning> = 2
 
-        def <warning descr="Shadows name 'spam' from outer scope">spam</warning>(self):
+        def <weak_warning descr="Shadows name 'spam' from outer scope">spam</weak_warning>(self):
             pass
 
-        <warning descr="Shadows name 'quux' from outer scope">quux</warning> = 1
+        <weak_warning descr="Shadows name 'quux' from outer scope">quux</weak_warning> = 1
