517c614461d76fa9419e39043195fa11dbd35e12
Konstantin Bulenkov <kb@jetbrains.com>
2013-09-06 01:58:21 +0200

    remove refs to Project asap

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/EditorComposite.java
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/EditorComposite.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -437,6 +437,7 @@ public abstract class EditorComposite implements Disposable {
         Disposer.dispose(editor);
       }
     }
+    myFocusWatcher.deinstall(myFocusWatcher.getTopComponent());
   }
 
   private static class TopBottomPanel extends JPanel {
