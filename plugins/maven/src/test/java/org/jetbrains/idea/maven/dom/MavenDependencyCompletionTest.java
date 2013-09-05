06eb88db740b50342b74f116ab7d2d16aa09f405
Sergey Evdokimov <sergey.evdokimov@jetbrains.com>
2013-09-05 22:03:01 +0400

    Add test for inserting maven dependency by smart completion

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/dom/MavenDependencyCompletionTest.java
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/dom/MavenDependencyCompletionTest.java
@@ -1,5 +1,8 @@
 package org.jetbrains.idea.maven.dom;
 
+import com.intellij.codeInsight.completion.CompletionType;
+import com.intellij.codeInsight.lookup.LookupElement;
+
 import java.io.IOException;
 
 /**
@@ -14,11 +17,37 @@ public class MavenDependencyCompletionTest extends MavenDomWithIndicesTestCase {
 
                      "<dependencies>" +
                      "  <dependency>" +
-                     "    jun<caret>" +
+                     "    <caret>" +
                      "  </dependency>" +
                      "</dependencies>");
 
     assertCompletionVariantsInclude(myProjectPom, "junit:junit");
+  }
+
+  public void testInsertDependency() throws IOException {
+    createProjectPom("<groupId>test</groupId>\n" +
+                     "<artifactId>project</artifactId>\n" +
+                     "<version>1</version>\n" +
+
+                     "<dependencies>\n" +
+                     "  <dependency>ju<caret></dependency>\n" +
+                     "</dependencies>\n");
+
+    configTest(myProjectPom);
+    myFixture.complete(CompletionType.SMART);
+    assertContain(myFixture.getLookupElementStrings(), "4.0", "3.8.2");
+
+    myFixture.checkResult(createPomXml("<groupId>test</groupId>\n" +
+                                       "<artifactId>project</artifactId>\n" +
+                                       "<version>1</version>\n" +
+
+                                       "<dependencies>\n" +
+                                       "    <dependency>\n" +
+                                       "        <groupId>junit</groupId>\n" +
+                                       "        <artifactId>junit</artifactId>\n" +
+                                       "        <version><caret></version>\n" +
+                                       "    </dependency>\n" +
+                                       "</dependencies>\n"));
   }
 
 }
