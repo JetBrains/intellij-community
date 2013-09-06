3807e66ea8c9fe702ab4bdfb4c8b00266e60791d
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2013-09-06 17:20:29 +0400

    Split shadowing inspection into built-in and names from outer scopes shadowing
    The reason is to have compatible shared inspections profile files for
    various versions of the IDE.
    
    Conflicts:
    
    	python/community/src/com/jetbrains/python/inspections/PyShadowingNamesInspection.java

--- python/testData/inspections/PyShadowingNamesInspection/test.py
+++ python/testData/inspections/PyShadowingNamesInspection/test.py
@@ -1,58 +1,6 @@
 global_foo = 1
 
 
-def test_import_builtin_names():
-    import float
-    from foo import float
-    from bar import baz as <weak_warning descr="Shadows built-in name 'float'">float</weak_warning>
-
-
-def test_builtin_function_parameters():
-    def test1(x, _, <weak_warning descr="Shadows built-in name 'len'">len</weak_warning>, <weak_warning descr="Shadows built-in name 'file'">file</weak_warning>=None):
-        pass
-
-
-def test_builtin_function_name():
-    def <weak_warning descr="Shadows built-in name 'list'">list</weak_warning>():
-        pass
-
-
-def test_builtin_assignment_targets():
-    foo = 2
-    <weak_warning descr="Shadows built-in name 'list'">list</weak_warning> = []
-    for <weak_warning descr="Shadows built-in name 'int'">int</weak_warning> in range(10):
-        print(int)
-    <weak_warning descr="Shadows built-in name 'range'">range</weak_warning> = []
-    <weak_warning descr="Shadows built-in name 'list'">list</weak_warning>, _ = (1, 2)
-    return [int for <weak_warning descr="Shadows built-in name 'int'">int</weak_warning> in range(10)]
-
-
-def test_builtin_class_name():
-    class <weak_warning descr="Shadows built-in name 'list'">list</weak_warning>(object):
-        pass
-
-
-def test_builtin_method_name():
-    class C:
-        def list(self):
-            pass
-
-
-# PY-8646
-def test_builtin_qualified_name():
-    test1.range = float()
-
-    class C:
-        def foo(self):
-            self.list = []
-
-
-# PY-10164
-def test_builtin_class_attribute():
-    class C:
-        id = 1
-
-
 def test_outer_function():
     foo = 1
     def bar():
@@ -91,7 +39,7 @@ def test_outer_global():
     global_foo = 2
 
 
-def test_comprehensions():
+def test_outer_comprehensions():
     print(x for x in range(10))
     print([y for y in range(10)])
     def f(x, <weak_warning descr="Shadows name 'y' from outer scope">y</weak_warning>):
