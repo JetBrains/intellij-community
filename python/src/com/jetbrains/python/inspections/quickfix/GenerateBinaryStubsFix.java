d618c9fc96457fbadd8297443aab64b7444022bf
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2013-09-06 16:05:05 +0400

    fixed PY-6932 Support for Gtk+ 3 dynamic modules

--- python/src/com/jetbrains/python/inspections/quickfix/GenerateBinaryStubsFix.java
+++ python/src/com/jetbrains/python/inspections/quickfix/GenerateBinaryStubsFix.java
@@ -1,11 +1,16 @@
 package com.jetbrains.python.inspections.quickfix;
 
+import com.google.common.collect.Lists;
 import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.execution.process.ProcessOutput;
 import com.intellij.notification.Notification;
 import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.ModuleUtil;
+import com.intellij.openapi.module.ModuleUtilCore;
+import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
+import com.intellij.openapi.progress.impl.BackgroundableProcessIndicator;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.vfs.LocalFileSystem;
@@ -13,16 +18,23 @@ import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiReference;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.Consumer;
+import com.jetbrains.python.PythonHelpersLocator;
 import com.jetbrains.python.psi.*;
+import com.jetbrains.python.psi.impl.PyQualifiedName;
 import com.jetbrains.python.psi.impl.references.PyImportReference;
-import com.jetbrains.python.sdk.*;
+import com.jetbrains.python.sdk.InvalidSdkException;
+import com.jetbrains.python.sdk.PySdkUtil;
+import com.jetbrains.python.sdk.PythonSdkType;
 import com.jetbrains.python.sdk.flavors.IronPythonSdkFlavor;
 import com.jetbrains.python.sdk.flavors.PythonSdkFlavor;
+import com.jetbrains.python.sdk.skeletons.PySkeletonGenerator;
 import com.jetbrains.python.sdk.skeletons.PySkeletonRefresher;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -44,7 +56,17 @@ public class GenerateBinaryStubsFix implements LocalQuickFix {
     while (element.getParent() instanceof PyReferenceExpression) {
       element = element.getParent();
     }
-    return element.getText();
+    final String elementText = element.getText();
+
+    final PyFromImportStatement importStatementBase = PsiTreeUtil.getParentOfType(element, PyFromImportStatement.class);
+    if (importStatementBase != null) {
+      final PyQualifiedName qName = importStatementBase.getImportSourceQName();
+      if (qName != null) {
+        return qName.append(elementText).toString();
+      }
+    }
+
+    return elementText;
   }
 
   @NotNull
@@ -58,14 +80,23 @@ public class GenerateBinaryStubsFix implements LocalQuickFix {
   }
 
   public void applyFix(@NotNull final Project project, @NotNull final ProblemDescriptor descriptor) {
-    ProgressManager.getInstance().runProcessWithProgressSynchronously(new Runnable() {
-      public void run() {
-        ProgressManager.getInstance().getProgressIndicator().setIndeterminate(true);
+    final Task.Backgroundable backgroundable = new Task.Backgroundable(project, "Generating skeletons for binary module", false) {
+
+      @Override
+      public void run(@NotNull ProgressIndicator indicator) {
+        indicator.setIndeterminate(true);
         List<String> assemblyRefs = collectAssemblyReferences(descriptor.getPsiElement().getContainingFile());
 
         try {
           final PySkeletonRefresher refresher = new PySkeletonRefresher(project, null, mySdk, null, null);
-          refresher.generateSkeleton(myQualifiedName, "", assemblyRefs, Consumer.EMPTY_CONSUMER);
+
+          if (needBinaryList(myQualifiedName)) {
+            if (!generateSkeletonsForList(refresher, indicator)) return;
+          }
+          else {
+            //noinspection unchecked
+            refresher.generateSkeleton(myQualifiedName, "", assemblyRefs, Consumer.EMPTY_CONSUMER);
+          }
           final VirtualFile skeletonDir;
           skeletonDir = LocalFileSystem.getInstance().findFileByPath(refresher.getSkeletonsPath());
           if (skeletonDir != null) {
@@ -77,7 +108,43 @@ public class GenerateBinaryStubsFix implements LocalQuickFix {
           notification.notify(project);
         }
       }
-    }, "Generating skeletons for binary module", false, project);
+    };
+    ProgressManager.getInstance().runProcessWithProgressAsynchronously(backgroundable, new BackgroundableProcessIndicator(backgroundable));
+  }
+
+  private boolean generateSkeletonsForList(@NotNull final PySkeletonRefresher refresher, ProgressIndicator indicator) throws InvalidSdkException {
+    final PySkeletonGenerator generator = new PySkeletonGenerator(refresher.getSkeletonsPath());
+    indicator.setIndeterminate(false);
+    final String homePath = mySdk.getHomePath();
+    if (homePath == null) return false;
+    final ProcessOutput runResult = PySdkUtil.getProcessOutput(
+      new File(homePath).getParent(),
+      new String[]{
+        homePath,
+        PythonHelpersLocator.getHelperPath("extra_syspath.py"), myQualifiedName},
+      PythonSdkType.getVirtualEnvAdditionalEnv(homePath), 5000
+    );
+    if (runResult.getExitCode() == 0 && !runResult.isTimeout()) {
+      final String extraPath = runResult.getStdout();
+      final PySkeletonGenerator.ListBinariesResult binaries = generator.listBinaries(mySdk, extraPath);
+      final List<String> names = Lists.newArrayList(binaries.modules.keySet());
+      Collections.sort(names);
+      final int size = names.size();
+      for (int i = 0; i != size; ++i) {
+        final String name = names.get(i);
+        indicator.setFraction((double)i / size);
+        if (needBinaryList(name)) {
+          indicator.setText2(name);
+          //noinspection unchecked
+          refresher.generateSkeleton(name, "", new ArrayList<String>(), Consumer.EMPTY_CONSUMER);
+        }
+      }
+    }
+    return true;
+  }
+
+  private static boolean needBinaryList(@NotNull final String qualifiedName) {
+    return qualifiedName.startsWith("gi.repository");
   }
 
   private List<String> collectAssemblyReferences(PsiFile file) {
@@ -100,7 +167,7 @@ public class GenerateBinaryStubsFix implements LocalQuickFix {
     return result;
   }
 
-  public static boolean isApplicable(PsiReference ref) {
+  public static boolean isApplicable(@NotNull final PsiReference ref) {
     if (!(ref instanceof PyImportReference)) {
       return false;
     }
@@ -112,12 +179,22 @@ public class GenerateBinaryStubsFix implements LocalQuickFix {
     if (flavor instanceof IronPythonSdkFlavor) {
       return getReferenceText(ref).matches("[A-Z][A-Za-z0-9]+(\\.[A-Z][A-Za-z0-9]+)*");
     }
+    return isGtk(ref);
+  }
+
+  private static boolean isGtk(@NotNull final PsiReference ref) {
+    final PyFromImportStatement importStatementBase = PsiTreeUtil.getParentOfType(ref.getElement(), PyFromImportStatement.class);
+    if (importStatementBase != null) {
+      final PyQualifiedName qName = importStatementBase.getImportSourceQName();
+      if (qName != null && qName.matches("gi", "repository"))
+        return true;
+    }
     return false;
   }
 
   @Nullable
-  private static Sdk getPythonSdk(PsiReference ref) {
-    final Module module = ModuleUtil.findModuleForPsiElement(ref.getElement());
+  private static Sdk getPythonSdk(@NotNull final PsiReference ref) {
+    final Module module = ModuleUtilCore.findModuleForPsiElement(ref.getElement());
     return module == null ? null : PythonSdkType.findPythonSdk(module);
   }
 }
