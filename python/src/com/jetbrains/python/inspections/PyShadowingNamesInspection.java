de8cc97b6709f386677d3df78e68db74c08b0631
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2013-09-06 17:20:29 +0400

    Split shadowing inspection into built-in and names from outer scopes shadowing
    The reason is to have compatible shared inspections profile files for
    various versions of the IDE.

--- python/src/com/jetbrains/python/inspections/PyShadowingNamesInspection.java
+++ python/src/com/jetbrains/python/inspections/PyShadowingNamesInspection.java
@@ -1,32 +1,21 @@
 package com.jetbrains.python.inspections;
 
-import com.google.common.collect.ImmutableSet;
-import com.intellij.codeInsight.intention.LowPriorityAction;
-import com.intellij.codeInspection.*;
-import com.intellij.codeInspection.ui.ListEditForm;
-import com.intellij.openapi.project.Project;
-import com.intellij.profile.codeInspection.InspectionProjectProfileManager;
+import com.intellij.codeInspection.LocalInspectionToolSession;
+import com.intellij.codeInspection.ProblemHighlightType;
+import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.PsiNameIdentifierOwner;
 import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.util.Consumer;
 import com.jetbrains.python.codeInsight.controlflow.ControlFlowCache;
 import com.jetbrains.python.codeInsight.controlflow.ScopeOwner;
 import com.jetbrains.python.codeInsight.dataflow.scope.Scope;
 import com.jetbrains.python.codeInsight.dataflow.scope.ScopeUtil;
 import com.jetbrains.python.psi.*;
-import com.jetbrains.python.psi.impl.PyBuiltinCache;
 import com.jetbrains.python.psi.resolve.PyResolveUtil;
 import com.jetbrains.python.psi.resolve.ResolveProcessor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import javax.swing.*;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
 
 /**
  * Warns about shadowing names defined in outer scopes.
@@ -34,19 +23,10 @@ import java.util.Set;
  * @author vlan
  */
 public class PyShadowingNamesInspection extends PyInspection {
-  // Persistent settings
-  public List<String> ignoredNames = new ArrayList<String>();
-
   @NotNull
   @Override
   public String getDisplayName() {
-    return "Shadowing names";
-  }
-
-  @Override
-  public JComponent createOptionsPanel() {
-    final ListEditForm form = new ListEditForm("Ignore built-ins", ignoredNames);
-    return form.getContentPanel();
+    return "Shadowing names from outer scopes";
   }
 
   @NotNull
@@ -54,15 +34,12 @@ public class PyShadowingNamesInspection extends PyInspection {
   public PsiElementVisitor buildVisitor(@NotNull ProblemsHolder holder,
                                         boolean isOnTheFly,
                                         @NotNull LocalInspectionToolSession session) {
-    return new Visitor(holder, session, ignoredNames);
+    return new Visitor(holder, session);
   }
 
   private static class Visitor extends PyInspectionVisitor {
-    private final Set<String> myIgnoredNames;
-
-    public Visitor(@Nullable ProblemsHolder holder, @NotNull LocalInspectionToolSession session, @NotNull Collection<String> ignoredNames) {
+    public Visitor(@Nullable ProblemsHolder holder, @NotNull LocalInspectionToolSession session) {
       super(holder, session);
-      myIgnoredNames = ImmutableSet.copyOf(ignoredNames);
     }
 
     @Override
@@ -92,99 +69,35 @@ public class PyShadowingNamesInspection extends PyInspection {
 
     private void processElement(@NotNull PsiNameIdentifierOwner element) {
       final ScopeOwner owner = ScopeUtil.getScopeOwner(element);
-      // Class-level names are rarely accessed inside at the class level, usually they are accessed as attributes
       if (owner instanceof PyClass) {
         return;
       }
       final String name = element.getName();
-      if (name != null && !myIgnoredNames.contains(name)) {
+      if (name != null) {
         final PsiElement identifier = element.getNameIdentifier();
         final PsiElement problemElement = identifier != null ? identifier : element;
-        final PyBuiltinCache builtinCache = PyBuiltinCache.getInstance(element);
-        final PsiElement builtin = builtinCache.getByName(name);
-        if (builtin != null) {
-          processBuiltin(element, name, problemElement, builtin);
+        if ("_".equals(name)) {
+          return;
         }
-        else {
-          processOuterScope(element, name, problemElement);
-        }
-      }
-    }
-
-    private void processBuiltin(@NotNull PsiNameIdentifierOwner element,
-                                @NotNull String name,
-                                @NotNull PsiElement problemElement,
-                                @NotNull PsiElement builtin) {
-      if (!PyUtil.inSameFile(builtin, element)) {
-        registerProblem(problemElement, String.format("Shadows built-in name '%s'", name),
-                        ProblemHighlightType.WEAK_WARNING, null, new PyRenameElementQuickFix(),
-                        new PyIgnoreBuiltinQuickFix(name));
-      }
-    }
-
-    private void processOuterScope(@NotNull PsiNameIdentifierOwner element, @NotNull String name, @NotNull PsiElement problemElement) {
-      if ("_".equals(name)) {
-        return;
-      }
-      final ScopeOwner owner = ScopeUtil.getScopeOwner(element);
-      if (owner != null) {
-        final ScopeOwner nextOwner = ScopeUtil.getScopeOwner(owner);
-        if (nextOwner != null) {
-          final ResolveProcessor processor = new ResolveProcessor(name);
-          PyResolveUtil.scopeCrawlUp(processor, nextOwner, null, name, null);
-          final PsiElement resolved = processor.getResult();
-          if (resolved != null) {
-            final PyComprehensionElement comprehension = PsiTreeUtil.getParentOfType(resolved, PyComprehensionElement.class);
-            if (comprehension != null && PyUtil.isOwnScopeComprehension(comprehension)) {
-              return;
-            }
-            final Scope scope = ControlFlowCache.getScope(owner);
-            if (scope.isGlobal(name) || scope.isNonlocal(name)) {
-              return;
-            }
-            registerProblem(problemElement, String.format("Shadows name '%s' from outer scope", name),
-                            ProblemHighlightType.WEAK_WARNING, null, new PyRenameElementQuickFix());
-          }
-        }
-      }
-    }
-
-    private static class PyIgnoreBuiltinQuickFix implements LocalQuickFix, LowPriorityAction {
-      @NotNull private final String myName;
-
-      private PyIgnoreBuiltinQuickFix(@NotNull String name) {
-        myName = name;
-      }
-
-      @NotNull
-      @Override
-      public String getName() {
-        return getFamilyName() + " \"" + myName + "\"";
-      }
-
-      @NotNull
-      @Override
-      public String getFamilyName() {
-        return "Ignore shadowed built-in name";
-      }
-
-      @Override
-      public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
-        final PsiElement element = descriptor.getPsiElement();
-        if (element != null) {
-          final InspectionProfile profile = InspectionProjectProfileManager.getInstance(project).getInspectionProfile();
-          profile.modifyProfile(new Consumer<ModifiableModel>() {
-            @Override
-            public void consume(ModifiableModel model) {
-              final String toolName = PyShadowingNamesInspection.class.getSimpleName();
-              final PyShadowingNamesInspection inspection = (PyShadowingNamesInspection)model.getUnwrappedTool(toolName, element);
-              if (inspection != null) {
-                if (!inspection.ignoredNames.contains(myName)) {
-                  inspection.ignoredNames.add(myName);
-                }
+        if (owner != null) {
+          final ScopeOwner nextOwner = ScopeUtil.getScopeOwner(owner);
+          if (nextOwner != null) {
+            final ResolveProcessor processor = new ResolveProcessor(name);
+            PyResolveUtil.scopeCrawlUp(processor, nextOwner, null, name, null);
+            final PsiElement resolved = processor.getResult();
+            if (resolved != null) {
+              final PyComprehensionElement comprehension = PsiTreeUtil.getParentOfType(resolved, PyComprehensionElement.class);
+              if (comprehension != null && PyUtil.isOwnScopeComprehension(comprehension)) {
+                return;
               }
+              final Scope scope = ControlFlowCache.getScope(owner);
+              if (scope.isGlobal(name) || scope.isNonlocal(name)) {
+                return;
+              }
+              registerProblem(problemElement, String.format("Shadows name '%s' from outer scope", name),
+                              ProblemHighlightType.WEAK_WARNING, null, new PyRenameElementQuickFix());
             }
-          });
+          }
         }
       }
     }
