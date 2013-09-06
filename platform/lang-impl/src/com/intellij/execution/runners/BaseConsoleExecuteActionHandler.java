599c49bba4717e05b5043c015b3b1330235ba90d
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2013-09-06 13:35:44 +0200

    init WEB-1171 javascript live console

---
+++ platform/lang-impl/src/com/intellij/execution/runners/BaseConsoleExecuteActionHandler.java
@@ -0,0 +1,59 @@
+package com.intellij.execution.runners;
+
+import com.intellij.execution.console.LanguageConsoleImpl;
+import com.intellij.execution.process.ConsoleHistoryModel;
+import com.intellij.openapi.command.impl.UndoManagerImpl;
+import com.intellij.openapi.command.undo.DocumentReferenceManager;
+import com.intellij.openapi.command.undo.UndoManager;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.util.TextRange;
+import org.jetbrains.annotations.NotNull;
+
+public abstract class BaseConsoleExecuteActionHandler {
+  private boolean myAddCurrentToHistory = true;
+  private final ConsoleHistoryModel myConsoleHistoryModel;
+  private final boolean myPreserveMarkup;
+
+  public BaseConsoleExecuteActionHandler(boolean preserveMarkup) {
+    myConsoleHistoryModel = new ConsoleHistoryModel();
+    myPreserveMarkup = preserveMarkup;
+  }
+
+  public ConsoleHistoryModel getConsoleHistoryModel() {
+    return myConsoleHistoryModel;
+  }
+
+  public void setAddCurrentToHistory(boolean addCurrentToHistory) {
+    myAddCurrentToHistory = addCurrentToHistory;
+  }
+
+  public void runExecuteAction(LanguageConsoleImpl languageConsole) {
+    // process input and add to history
+    Document document = languageConsole.getCurrentEditor().getDocument();
+    String text = document.getText();
+    TextRange range = new TextRange(0, document.getTextLength());
+
+    languageConsole.getCurrentEditor().getSelectionModel().setSelection(range.getStartOffset(), range.getEndOffset());
+
+    if (myAddCurrentToHistory) {
+      languageConsole.addCurrentToHistory(range, false, myPreserveMarkup);
+    }
+
+    languageConsole.setInputText("");
+
+    UndoManager manager = languageConsole.getProject() == null ? UndoManager.getGlobalInstance() : UndoManager.getInstance(languageConsole.getProject());
+    ((UndoManagerImpl)manager).invalidateActionsFor(DocumentReferenceManager.getInstance().create(document));
+
+    myConsoleHistoryModel.addToHistory(text);
+    execute(text);
+  }
+
+  protected abstract void execute(@NotNull String text);
+
+  public void finishExecution() {
+  }
+
+  public String getEmptyExecuteAction() {
+    return ConsoleExecuteAction.CONSOLE_EXECUTE_ACTION_ID;
+  }
+}
