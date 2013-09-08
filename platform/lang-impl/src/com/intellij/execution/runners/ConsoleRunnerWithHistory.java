09c720663b437277515e4ebba64974fb30043bd7
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2013-09-08 18:44:10 +0200

    Implement get variable for console variables view.

--- platform/lang-impl/src/com/intellij/execution/runners/ConsoleRunnerWithHistory.java
+++ platform/lang-impl/src/com/intellij/execution/runners/ConsoleRunnerWithHistory.java
@@ -1,10 +1,10 @@
 package com.intellij.execution.runners;
 
-import com.intellij.execution.console.LanguageConsoleViewImpl;
+import com.intellij.execution.console.LanguageConsoleView;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
-public abstract class ConsoleRunnerWithHistory<T extends LanguageConsoleViewImpl> {
+public abstract class ConsoleRunnerWithHistory<T extends LanguageConsoleView> {
   protected final Project myProject;
 
   public ConsoleRunnerWithHistory(@NotNull Project project) {
