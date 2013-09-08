8941cf8fb5a92c0b0b6545ad7d404bad221b9537
Bas Leijdekkers <basleijdekkers@gmail.com>
2013-09-08 12:29:27 +0200

    IDEA-113298 (replace assertTrue with assertFalse removes message)

---
+++ plugins/IntentionPowerPak/testSrc/com/siyeh/ipp/junit/FlipAssertLiteralIntentionTest.java
@@ -0,0 +1,36 @@
+/**
+ * (c) 2013 Desert Island BV
+ * created: 08 09 2013
+ */
+package com.siyeh.ipp.junit;
+
+import com.siyeh.IntentionPowerPackBundle;
+import com.siyeh.ipp.IPPTestCase;
+
+/**
+ * @see FlipAssertLiteralIntention
+ * @author Bas Leijdekkers
+ */
+public class FlipAssertLiteralIntentionTest extends IPPTestCase {
+
+  public void testMessage() { doTest(); }
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    myFixture.addClass("package org.junit;" +
+                       "class Assert {" +
+                       "  public static void assertTrue(java.lang.String message, boolean condition) {}" +
+                       "}");
+  }
+
+  @Override
+  protected String getRelativePath() {
+    return "junit/flip_assert_literal";
+  }
+
+  @Override
+  protected String getIntentionName() {
+    return IntentionPowerPackBundle.message("flip.assert.literal.intention.name", "assertTrue", "assertFalse");
+  }
+}
