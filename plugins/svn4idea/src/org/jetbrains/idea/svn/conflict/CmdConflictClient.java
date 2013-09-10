2a375aa9faafa09f1fc630efa70dde0c7fce47bd
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2013-09-10 19:26:18 +0400

    IDEA-94942 Add ability to specify depth for "resolve conflict" command (cherry picked from commit 8abd924)

--- plugins/svn4idea/src/org/jetbrains/idea/svn/conflict/CmdConflictClient.java
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/conflict/CmdConflictClient.java
@@ -2,6 +2,7 @@ package org.jetbrains.idea.svn.conflict;
 
 import com.intellij.openapi.vcs.VcsException;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.idea.svn.api.BaseSvnClient;
 import org.jetbrains.idea.svn.commandLine.CommandUtil;
 import org.jetbrains.idea.svn.commandLine.SvnCommandName;
@@ -19,11 +20,15 @@ public class CmdConflictClient extends BaseSvnClient implements ConflictClient {
   // TODO: Add possibility to resolve content, property and tree conflicts separately.
   // TODO: Or rewrite logic to have one "Resolve conflicts" action instead of separate actions for each conflict type.
   @Override
-  public void resolve(@NotNull File path, boolean resolveProperty, boolean resolveContent, boolean resolveTree) throws VcsException {
+  public void resolve(@NotNull File path,
+                      @Nullable SVNDepth depth,
+                      boolean resolveProperty,
+                      boolean resolveContent,
+                      boolean resolveTree) throws VcsException {
     List<String> parameters = new ArrayList<String>();
 
     CommandUtil.put(parameters, path);
-    CommandUtil.put(parameters, SVNDepth.EMPTY);
+    CommandUtil.put(parameters, depth);
     parameters.add("--accept");
     parameters.add("working");
 
