ab23b281ac848482763171942e0827c55acf91a4
Max Medvedev <maxim.medvedev@jetbrains.com>
2013-09-11 17:00:55 +0400

    don't generate java doc for synthetic elements

--- plugins/groovy/src/org/intellij/plugins/intelliLang/inject/groovy/GroovyLanguageInjectionSupport.java
+++ plugins/groovy/src/org/intellij/plugins/intelliLang/inject/groovy/GroovyLanguageInjectionSupport.java
@@ -22,7 +22,14 @@ import com.intellij.openapi.util.Pair;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.light.LightElement;
 import com.intellij.psi.tree.IElementType;
+import com.intellij.util.NullableFunction;
+import com.intellij.util.containers.ContainerUtil;
+import org.intellij.plugins.intelliLang.Configuration;
 import org.intellij.plugins.intelliLang.inject.AbstractLanguageInjectionSupport;
+import org.intellij.plugins.intelliLang.inject.InjectorUtils;
+import org.intellij.plugins.intelliLang.inject.LanguageInjectionSupport;
+import org.intellij.plugins.intelliLang.inject.config.BaseInjection;
+import org.intellij.plugins.intelliLang.inject.java.ConcatenationInjector;
 import org.intellij.plugins.intelliLang.inject.java.JavaLanguageInjectionSupport;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
@@ -39,11 +46,16 @@ import org.jetbrains.plugins.groovy.lang.psi.api.statements.blocks.GrOpenBlock;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.branch.GrReturnStatement;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.*;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.literals.GrLiteral;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.literals.GrString;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.literals.GrStringContent;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.members.GrMethod;
 import org.jetbrains.plugins.groovy.lang.psi.impl.signatures.GrClosureSignatureUtil;
 import org.jetbrains.plugins.groovy.lang.psi.impl.statements.expressions.literals.GrLiteralImpl;
 import org.jetbrains.plugins.groovy.lang.psi.patterns.GroovyPatterns;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -82,6 +94,59 @@ public class GroovyLanguageInjectionSupport extends AbstractLanguageInjectionSup
 
 
     return doInject(language.getID(), psiElement, psiElement);
+  }
+
+  @Override
+  public boolean removeInjectionInPlace(final PsiLanguageInjectionHost psiElement) {
+    if (!isStringLiteral(psiElement)) return false;
+    final HashMap<BaseInjection, Pair<PsiMethod, Integer>> injectionsMap = ContainerUtil.newHashMap();
+    final ArrayList<PsiElement> annotations = new ArrayList<PsiElement>();
+    final GrLiteral host = (GrLiteral)psiElement;
+    final Project project = host.getProject();
+    final Configuration configuration = Configuration.getProjectInstance(project);
+    collectInjections(host, configuration, this, injectionsMap, annotations);
+
+    if (injectionsMap.isEmpty() && annotations.isEmpty()) return false;
+    final ArrayList<BaseInjection> originalInjections = new ArrayList<BaseInjection>(injectionsMap.keySet());
+    final List<BaseInjection>
+      newInjections = ContainerUtil.mapNotNull(originalInjections, new NullableFunction<BaseInjection, BaseInjection>() {
+      public BaseInjection fun(final BaseInjection injection) {
+        final Pair<PsiMethod, Integer> pair = injectionsMap.get(injection);
+        final String placeText = JavaLanguageInjectionSupport.getPatternStringForJavaPlace(pair.first, pair.second);
+        final BaseInjection newInjection = injection.copy();
+        newInjection.setPlaceEnabled(placeText, false);
+        return InjectorUtils.canBeRemoved(newInjection) ? null : newInjection;
+      }
+    });
+    configuration.replaceInjectionsWithUndo(project, newInjections, originalInjections, annotations);
+    return true;
+  }
+
+  private static void collectInjections(GrLiteral host,
+                                        Configuration configuration,
+                                        LanguageInjectionSupport support,
+                                        final HashMap<BaseInjection, Pair<PsiMethod, Integer>> injectionsMap,
+                                        final ArrayList<PsiElement> annotations) {
+    new ConcatenationInjector.InjectionProcessor(configuration, support, host) {
+
+      @Override
+      protected boolean processCommentInjectionInner(PsiVariable owner, PsiElement comment, BaseInjection injection) {
+        ContainerUtil.addAll(annotations, comment);
+        return true;
+      }
+
+      @Override
+      protected boolean processAnnotationInjectionInner(PsiModifierListOwner owner, PsiAnnotation[] annos) {
+        ContainerUtil.addAll(annotations, annos);
+        return true;
+      }
+
+      @Override
+      protected boolean processXmlInjections(BaseInjection injection, PsiModifierListOwner owner, PsiMethod method, int paramIndex) {
+        injectionsMap.put(injection, Pair.create(method, paramIndex));
+        return true;
+      }
+    }.processInjections();
   }
 
   private static boolean doInject(String languageId, PsiElement psiElement, PsiLanguageInjectionHost host) {
@@ -152,7 +217,10 @@ public class GroovyLanguageInjectionSupport extends AbstractLanguageInjectionSup
   }
 
   private static boolean isStringLiteral(PsiLanguageInjectionHost element) {
-    if (element instanceof GrLiteral) {
+    if (element instanceof GrStringContent) {
+      return true;
+    }
+    else if (element instanceof GrLiteral) {
       final IElementType type = GrLiteralImpl.getLiteralType((GrLiteral)element);
       return TokenSets.STRING_LITERALS.contains(type);
     }
@@ -165,6 +233,7 @@ public class GroovyLanguageInjectionSupport extends AbstractLanguageInjectionSup
     PsiElement parent = target.getParent();
     for (; parent != null; target = parent, parent = target.getParent()) {
       if (parent instanceof GrBinaryExpression) continue;
+      if (parent instanceof GrString) continue;
       if (parent instanceof GrParenthesizedExpression) continue;
       if (parent instanceof GrConditionalExpression && ((GrConditionalExpression)parent).getCondition() != target) continue;
       if (parent instanceof GrAnnotationArrayInitializer) continue;
