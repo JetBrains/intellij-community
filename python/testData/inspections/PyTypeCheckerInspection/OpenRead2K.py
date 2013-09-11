4469bcdcc21d3c6cc908501febe5f5aeb5c6e032
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2013-09-11 17:27:54 +0400

    More examples for open() type tests

--- python/testData/inspections/PyTypeCheckerInspection/OpenRead2K.py
+++ python/testData/inspections/PyTypeCheckerInspection/OpenRead2K.py
@@ -4,4 +4,10 @@ def f(s):
     """
     pass
 
+def g(s):
+    """
+    :type s: unicode
+    """
+
 f(open('foo').read()) # pass
+g(<warning descr="Expected type 'unicode', got 'str' instead">open('foo').read()</warning>)
