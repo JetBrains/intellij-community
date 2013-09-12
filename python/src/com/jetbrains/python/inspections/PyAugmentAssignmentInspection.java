2e5cb2c71ff5c0040a8bc6fc546f6f91fa1d576d
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2013-09-12 16:02:28 +0400

    fixed PY-6511 Augmented assignment quickfix treats & and | operators as non-commutative

--- python/src/com/jetbrains/python/inspections/PyAugmentAssignmentInspection.java
+++ python/src/com/jetbrains/python/inspections/PyAugmentAssignmentInspection.java
@@ -70,7 +70,7 @@ public class PyAugmentAssignmentInspection extends PyInspection {
                                                     PyTokenTypes.FLOORDIV, PyTokenTypes.DIV, PyTokenTypes.PERC, PyTokenTypes.AND,
                                                     PyTokenTypes.OR, PyTokenTypes.XOR, PyTokenTypes.LTLT, PyTokenTypes.GTGT,
                                                     PyTokenTypes.EXP);
-        final TokenSet commutativeOperations = TokenSet.create(PyTokenTypes.PLUS, PyTokenTypes.MULT);
+        final TokenSet commutativeOperations = TokenSet.create(PyTokenTypes.PLUS, PyTokenTypes.MULT, PyTokenTypes.OR, PyTokenTypes.AND);
         if ((operations.contains(op) && !changedParts) || (changedParts && commutativeOperations.contains(op))) {
           if (leftExpression.getText().equals(targetText) && (leftExpression instanceof PyReferenceExpression || leftExpression instanceof PySubscriptionExpression)) {
             final PyType type = myTypeEvalContext.getType(rightExpression);
