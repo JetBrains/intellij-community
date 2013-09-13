066d097a70e7e766c6113037e39f4c35f231fc65
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2013-09-13 16:08:52 +0400

    WEB-9250 Add "Start browser"/"with JavaScript debugger" options to Node.js run/debug configuration (cherry picked from commit 269a0de) http://crucible.labs.intellij.net/cru/CR-WS-132

--- xml/impl/src/com/intellij/ide/browsers/StartBrowserPanel.java
+++ xml/impl/src/com/intellij/ide/browsers/StartBrowserPanel.java
@@ -1,5 +1,7 @@
 package com.intellij.ide.browsers;
 
+import com.intellij.util.ui.UIUtil;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,17 +15,13 @@ public class StartBrowserPanel extends JPanel {
   private JTextField myStartupPage;
   private BrowserSelector myBrowserSelector;
 
-  @SuppressWarnings("UnusedDeclaration")
-  private JPanel ignored;
+  private JPanel myRoot;
 
   public StartBrowserPanel() {
     myStartBrowserCheckBox.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
-        boolean selected = myStartBrowserCheckBox.isSelected();
-        myStartupPage.setEnabled(selected);
-        myBrowserComboBox.setEnabled(selected);
-        myStartJavaScriptDebuggerCheckBox.setEnabled(selected);
+        setPanelEnabled(myStartBrowserCheckBox.isSelected());
       }
     });
 
@@ -36,11 +34,12 @@ public class StartBrowserPanel extends JPanel {
 
   public void setSelected(boolean value) {
     myStartBrowserCheckBox.setSelected(value);
-    if (!value) {
-      myStartupPage.setEnabled(false);
-      myBrowserComboBox.setEnabled(false);
-      myStartJavaScriptDebuggerCheckBox.setEnabled(false);
-    }
+    setPanelEnabled(value);
+  }
+
+  private void setPanelEnabled(boolean enabled) {
+    UIUtil.setEnabled(myRoot, enabled, true);
+    myStartBrowserCheckBox.setEnabled(true);
   }
 
   public JCheckBox getStartJavaScriptDebuggerCheckBox() {
@@ -53,6 +52,10 @@ public class StartBrowserPanel extends JPanel {
 
   public JTextField getStartupPageField() {
     return myStartupPage;
+  }
+
+  public JPanel getRoot() {
+    return myRoot;
   }
 
   private void createUIComponents() {
