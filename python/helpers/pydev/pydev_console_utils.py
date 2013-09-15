8cbaec66492036dc9b4410142f01f12e705f77de
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2013-09-15 21:12:27 +0200

    Set value: fix import

--- python/helpers/pydev/pydev_console_utils.py
+++ python/helpers/pydev/pydev_console_utils.py
@@ -14,6 +14,11 @@ except:
     import queue as _queue
 
 try:
+    from pydevd_exec import Exec
+except:
+    from pydevd_exec2 import Exec
+
+try:
     if USE_LIB_COPY:
         import _pydev_thread as thread
     else:
@@ -371,7 +376,4 @@ class BaseInterpreterInterface:
         return xml
 
     def changeVariable(self, attr, value):
-        try:
-            Exec('%s=%s' % (attr, value), self.getNamespace(), self.getNamespace())
-        except:
-            pass
+        Exec('%s=%s' % (attr, value), self.getNamespace(), self.getNamespace())
