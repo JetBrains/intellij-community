92370285e48616a20ff1b7a6fee6c4320dd0e48a
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2013-09-16 12:19:21 +0400

    fixed PY-10798 False positive on "must implement all abstract methods" inspection

--- python/src/com/jetbrains/python/inspections/PyAbstractClassInspection.java
+++ python/src/com/jetbrains/python/inspections/PyAbstractClassInspection.java
@@ -50,7 +50,7 @@ public class PyAbstractClassInspection extends PyInspection {
       final Collection<PyFunction> functions = PyOverrideImplementUtil.getAllSuperFunctions(node);
       for (PyFunction method : functions) {
         final PyDecoratorList list = method.getDecoratorList();
-        if (list != null) {
+        if (list != null && node.findMethodByName(method.getName(), false) == null) {
           if (list.findDecorator(PyNames.ABSTRACTMETHOD) != null || list.findDecorator(PyNames.ABSTRACTPROPERTY) != null) {
             toBeImplemented.add(method);
           }
@@ -58,7 +58,7 @@ public class PyAbstractClassInspection extends PyInspection {
       }
       final ASTNode nameNode = node.getNameNode();
       if (!toBeImplemented.isEmpty() && nameNode != null) {
-        registerProblem(nameNode.getPsi(), PyBundle.message("INSP.NAME.abstract.class.$0.must.implement", node),
+        registerProblem(nameNode.getPsi(), PyBundle.message("INSP.NAME.abstract.class.$0.must.implement", node.getName()),
                         ProblemHighlightType.INFO, null, new PyImplementMethodsQuickFix(node, toBeImplemented));
       }
     }
