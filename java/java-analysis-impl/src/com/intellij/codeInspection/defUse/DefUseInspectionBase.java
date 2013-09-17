bdd7827921c1d70cd818851f43a0c5d1dba7d90a
Bas Leijdekkers <basleijdekkers@gmail.com>
2013-09-17 22:54:05 +0200

    IDEA-98275 (Inspection "Unused assignment" does not work with the += operator)

--- java/java-analysis-impl/src/com/intellij/codeInspection/defUse/DefUseInspectionBase.java
+++ java/java-analysis-impl/src/com/intellij/codeInspection/defUse/DefUseInspectionBase.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.intellij.codeInspection.defUse;
 
 import com.intellij.codeInsight.daemon.GroupNames;
@@ -82,8 +97,7 @@ public class DefUseInspectionBase extends BaseJavaBatchLocalInspectionTool {
             }
           }
         }
-        else if (context instanceof PsiAssignmentExpression &&
-                 ((PsiAssignmentExpression)context).getOperationTokenType() == JavaTokenType.EQ) {
+        else if (context instanceof PsiAssignmentExpression) {
           final PsiAssignmentExpression assignment = (PsiAssignmentExpression)context;
           holder.registerProblem(assignment.getLExpression(),
                                  InspectionsBundle.message("inspection.unused.assignment.problem.descriptor3",
