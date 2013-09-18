4ce0f0a737965cd1d010411428bc64d239de3ea5
Dmitry Jemerov <yole@jetbrains.com>
2013-09-18 19:36:28 +0200

    remove hard dependency of python-rest on django

--- python/python-rest/src/com/jetbrains/rest/RestEditorHighlighter.java
+++ python/python-rest/src/com/jetbrains/rest/RestEditorHighlighter.java
@@ -4,11 +4,12 @@ import com.intellij.openapi.editor.colors.EditorColors;
 import com.intellij.openapi.editor.colors.EditorColorsScheme;
 import com.intellij.openapi.editor.ex.util.LayerDescriptor;
 import com.intellij.openapi.editor.ex.util.LayeredLexerEditorHighlighter;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.fileTypes.FileTypeManager;
 import com.intellij.openapi.fileTypes.StdFileTypes;
 import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.jetbrains.django.lang.template.DjangoTemplateFileType;
 import com.jetbrains.python.PythonFileType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -26,9 +27,12 @@ public class RestEditorHighlighter extends LayeredLexerEditorHighlighter {
     registerLayer(RestTokenTypes.PYTHON_LINE, new LayerDescriptor(
       SyntaxHighlighterFactory.getSyntaxHighlighter(PythonFileType.INSTANCE, project, file), "", EditorColors.INJECTED_LANGUAGE_FRAGMENT));
 
-    registerLayer(RestTokenTypes.DJANGO_LINE, new LayerDescriptor(
-      SyntaxHighlighterFactory.getSyntaxHighlighter(DjangoTemplateFileType.INSTANCE, project, file), "", EditorColors.INJECTED_LANGUAGE_FRAGMENT));
-
+    FileType djangoTemplateFileType = FileTypeManager.getInstance().findFileTypeByName("DjangoTemplate");
+    if (djangoTemplateFileType != null) {
+      registerLayer(RestTokenTypes.DJANGO_LINE, new LayerDescriptor(
+        SyntaxHighlighterFactory.getSyntaxHighlighter(djangoTemplateFileType, project, file), "",
+        EditorColors.INJECTED_LANGUAGE_FRAGMENT));
+    }
 
     registerLayer(RestTokenTypes.JAVASCRIPT_LINE, new LayerDescriptor(
       SyntaxHighlighterFactory.getSyntaxHighlighter(StdFileTypes.JS, project, file), "", EditorColors.INJECTED_LANGUAGE_FRAGMENT));
