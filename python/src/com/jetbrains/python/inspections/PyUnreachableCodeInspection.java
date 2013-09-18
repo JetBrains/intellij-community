3afada53aa6c9a85b42e234e885b6684391e048e
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2013-09-18 16:26:43 +0400

    Fixed unresolved reference inspection for unreachable code (PY-10006)

--- python/src/com/jetbrains/python/inspections/PyUnreachableCodeInspection.java
+++ python/src/com/jetbrains/python/inspections/PyUnreachableCodeInspection.java
@@ -5,12 +5,14 @@ import com.intellij.codeInsight.controlflow.ControlFlowUtil;
 import com.intellij.codeInsight.controlflow.Instruction;
 import com.intellij.codeInspection.LocalInspectionToolSession;
 import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.openapi.util.Ref;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.util.Function;
 import com.jetbrains.python.PyBundle;
 import com.jetbrains.python.codeInsight.controlflow.ControlFlowCache;
 import com.jetbrains.python.codeInsight.controlflow.ScopeOwner;
+import com.jetbrains.python.codeInsight.dataflow.scope.ScopeUtil;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 
@@ -62,5 +64,29 @@ public class PyUnreachableCodeInspection extends PyInspection {
         }
       }
     }
+  }
+
+  public static boolean isUnreachable(@NotNull PsiElement element) {
+    final ScopeOwner owner = ScopeUtil.getScopeOwner(element);
+    if (owner != null) {
+      final ControlFlow flow = ControlFlowCache.getControlFlow(owner);
+      final Instruction[] instructions = flow.getInstructions();
+      final int start = ControlFlowUtil.findInstructionNumberByElement(instructions, element);
+      if (start >= 0) {
+        final Ref<Boolean> resultRef = Ref.create(false);
+        ControlFlowUtil.iteratePrev(start, instructions, new Function<Instruction, ControlFlowUtil.Operation>() {
+          @Override
+          public ControlFlowUtil.Operation fun(Instruction instruction) {
+            if (instruction.allPred().isEmpty() && instruction.num() != 0) {
+              resultRef.set(true);
+              return ControlFlowUtil.Operation.BREAK;
+            }
+            return ControlFlowUtil.Operation.NEXT;
+          }
+        });
+        return resultRef.get();
+      }
+    }
+    return false;
   }
 }
