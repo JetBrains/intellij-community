43dbb6f2678035d081095e0a320c91463d6aa8e7
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2013-09-19 02:56:56 +0200

    Terminal options.

---
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/TerminalSettingsPanel.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2000-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jetbrains.plugins.terminal;
+
+import com.intellij.openapi.Disposable;
+import com.intellij.openapi.util.Comparing;
+import com.intellij.ui.components.JBCheckBox;
+
+import javax.swing.*;
+
+/**
+ * @author traff
+ */
+public class TerminalSettingsPanel {
+  private JPanel myWholePanel;
+  private JTextField myShellTextField;
+  private JBCheckBox mySoundBellCheckBox;
+  private JBCheckBox myCloseSessionCheckBox;
+  private JBCheckBox myMouseReportCheckBox;
+  private JTextField myTabNameTextField;
+  private JBCheckBox myPasteOnMiddleButtonCheckBox;
+  private JBCheckBox myCopyOnSelectionCheckBox;
+  private TerminalOptionsProvider myOptionsProvider;
+
+  public JComponent createPanel(TerminalOptionsProvider provider) {
+    myOptionsProvider = provider;
+
+    return myWholePanel;
+  }
+
+  public boolean isModified() {
+    return !Comparing.equal(myShellTextField.getText(), myOptionsProvider.getShellPath())
+           || !Comparing.equal(myTabNameTextField.getText(), myOptionsProvider.getTabName())
+           || (myCloseSessionCheckBox.isSelected() != myOptionsProvider.closeSessionOnLogout())
+           || (myMouseReportCheckBox.isSelected() != myOptionsProvider.enableMouseReporting())
+           || (mySoundBellCheckBox.isSelected() != myOptionsProvider.audibleBell())
+           || (myCopyOnSelectionCheckBox.isSelected() != myOptionsProvider.copyOnSelection())
+           || (myPasteOnMiddleButtonCheckBox.isSelected() != myOptionsProvider.pasteOnMiddleMouseButton())
+      ;
+  }
+
+  public void apply() {
+    myOptionsProvider.setShellPath(myShellTextField.getText());
+    myOptionsProvider.setTabName(myTabNameTextField.getText());
+    myOptionsProvider.setCloseSessionOnLogout(myCloseSessionCheckBox.isSelected());
+    myOptionsProvider.setReportMouse(myMouseReportCheckBox.isSelected());
+    myOptionsProvider.setSoundBell(mySoundBellCheckBox.isSelected());
+    myOptionsProvider.setCopyOnSelection(myCopyOnSelectionCheckBox.isSelected());
+    myOptionsProvider.setPasteOnMiddleMouseButton(myPasteOnMiddleButtonCheckBox.isSelected());
+  }
+
+  public void reset() {
+    myShellTextField.setText(myOptionsProvider.getShellPath());
+    myTabNameTextField.setText(myOptionsProvider.getTabName());
+    myCloseSessionCheckBox.setSelected(myOptionsProvider.closeSessionOnLogout());
+    myMouseReportCheckBox.setSelected(myOptionsProvider.enableMouseReporting());
+    mySoundBellCheckBox.setSelected(myOptionsProvider.audibleBell());
+    myCopyOnSelectionCheckBox.setSelected(myOptionsProvider.copyOnSelection());
+    myPasteOnMiddleButtonCheckBox.setSelected(myOptionsProvider.pasteOnMiddleMouseButton());
+  }
+}
