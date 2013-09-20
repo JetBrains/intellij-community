8807223bc3ba644e18f21f1ae707d1cd12dac2cb
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2013-09-20 16:28:41 +0200

    Bight console colors added to settings.

--- platform/platform-api/src/com/intellij/execution/process/ConsoleHighlighter.java
+++ platform/platform-api/src/com/intellij/execution/process/ConsoleHighlighter.java
@@ -15,52 +15,64 @@
  */
 package com.intellij.execution.process;
 
-import com.intellij.openapi.editor.HighlighterColors;
+import com.intellij.execution.ui.ConsoleViewContentType;
 import com.intellij.openapi.editor.colors.TextAttributesKey;
-import com.intellij.openapi.editor.markup.TextAttributes;
-import com.intellij.ui.JBColor;
-import com.intellij.util.ui.PlatformColors;
 import org.jetbrains.annotations.NonNls;
-
-import java.awt.*;
 
 /**
  * @author oleg
  */
 public class ConsoleHighlighter {
+  @NonNls static final String BLACK_ID = "CONSOLE_BLACK_OUTPUT";
   @NonNls static final String RED_ID = "CONSOLE_RED_OUTPUT";
   @NonNls static final String GREEN_ID = "CONSOLE_GREEN_OUTPUT";
   @NonNls static final String YELLOW_ID = "CONSOLE_YELLOW_OUTPUT";
   @NonNls static final String BLUE_ID = "CONSOLE_BLUE_OUTPUT";
   @NonNls static final String MAGENTA_ID = "CONSOLE_MAGENTA_OUTPUT";
   @NonNls static final String CYAN_ID = "CONSOLE_CYAN_OUTPUT";
-  @NonNls static final String GRAY_ID = "CONSOLE_GRAY_OUTPUT";
+  @NonNls static final String GRAY_ID = "CONSOLE_GRAY_OUTPUT"; //ISO white
 
-  public static final TextAttributes RED_DEFAULT_ATTRS = HighlighterColors.TEXT.getDefaultAttributes().clone();
-  public static final TextAttributes GREEN_DEFAULT_ATTRS = HighlighterColors.TEXT.getDefaultAttributes().clone();
-  public static final TextAttributes YELLOW_DEFAULT_ATTRS = HighlighterColors.TEXT.getDefaultAttributes().clone();
-  public static final TextAttributes BLUE_DEFAULT_ATTRS = HighlighterColors.TEXT.getDefaultAttributes().clone();
-  public static final TextAttributes MAGENTA_DEFAULT_ATTRS = HighlighterColors.TEXT.getDefaultAttributes().clone();
-  public static final TextAttributes CYAN_DEFAULT_ATTRS = HighlighterColors.TEXT.getDefaultAttributes().clone();
-  public static final TextAttributes GRAY_DEFAULT_ATTRS = HighlighterColors.TEXT.getDefaultAttributes().clone();
 
-  static {
-    RED_DEFAULT_ATTRS.setForegroundColor(JBColor.RED);
-    GREEN_DEFAULT_ATTRS.setForegroundColor(new Color(0, 128, 0));
-    YELLOW_DEFAULT_ATTRS.setForegroundColor(new Color(255, 204, 0));
-    BLUE_DEFAULT_ATTRS.setForegroundColor(PlatformColors.BLUE);
-    MAGENTA_DEFAULT_ATTRS.setForegroundColor(JBColor.MAGENTA);
-    CYAN_DEFAULT_ATTRS.setForegroundColor(JBColor.CYAN.darker());
-    GRAY_DEFAULT_ATTRS.setForegroundColor(JBColor.GRAY.darker());
-  }
+  @NonNls static final String DARKGRAY_ID = "CONSOLE_DARKGRAY_OUTPUT";
+  @NonNls static final String RED_BRIGHT_ID = "CONSOLE_RED_BRIGHT_OUTPUT";
+  @NonNls static final String GREEN_BRIGHT_ID = "CONSOLE_GREEN_BRIGHT_OUTPUT";
+  @NonNls static final String YELLOW_BRIGHT_ID = "CONSOLE_YELLOW_BRIGHT_OUTPUT";
+  @NonNls static final String BLUE_BRIGHT_ID = "CONSOLE_BLUE_BRIGHT_OUTPUT";
+  @NonNls static final String MAGENTA_BRIGHT_ID = "CONSOLE_MAGENTA_BRIGHT_OUTPUT";
+  @NonNls static final String CYAN_BRIGHT_ID = "CONSOLE_CYAN_BRIGHT_OUTPUT";
+  @NonNls static final String WHITE_ID = "CONSOLE_WHITE_OUTPUT"; //ISO bright white
 
-  public static final TextAttributesKey RED = TextAttributesKey.createTextAttributesKey(RED_ID, RED_DEFAULT_ATTRS);
-  public static final TextAttributesKey GREEN = TextAttributesKey.createTextAttributesKey(GREEN_ID, GREEN_DEFAULT_ATTRS);
-  public static final TextAttributesKey YELLOW = TextAttributesKey.createTextAttributesKey(YELLOW_ID, YELLOW_DEFAULT_ATTRS);
-  public static final TextAttributesKey BLUE = TextAttributesKey.createTextAttributesKey(BLUE_ID, BLUE_DEFAULT_ATTRS);
-  public static final TextAttributesKey MAGENTA = TextAttributesKey.createTextAttributesKey(MAGENTA_ID, MAGENTA_DEFAULT_ATTRS);
-  public static final TextAttributesKey CYAN = TextAttributesKey.createTextAttributesKey(CYAN_ID, CYAN_DEFAULT_ATTRS);
-  public static final TextAttributesKey GRAY = TextAttributesKey.createTextAttributesKey(GRAY_ID, GRAY_DEFAULT_ATTRS);
+  public static final TextAttributesKey BLACK =
+    TextAttributesKey.createTextAttributesKey(BLACK_ID, ConsoleViewContentType.NORMAL_OUTPUT_KEY);
+  public static final TextAttributesKey RED = TextAttributesKey.createTextAttributesKey(RED_ID, ConsoleViewContentType.NORMAL_OUTPUT_KEY);
+  public static final TextAttributesKey GREEN =
+    TextAttributesKey.createTextAttributesKey(GREEN_ID, ConsoleViewContentType.NORMAL_OUTPUT_KEY);
+  public static final TextAttributesKey YELLOW =
+    TextAttributesKey.createTextAttributesKey(YELLOW_ID, ConsoleViewContentType.NORMAL_OUTPUT_KEY);
+  public static final TextAttributesKey BLUE = TextAttributesKey.createTextAttributesKey(BLUE_ID, ConsoleViewContentType.NORMAL_OUTPUT_KEY);
+  public static final TextAttributesKey MAGENTA =
+    TextAttributesKey.createTextAttributesKey(MAGENTA_ID, ConsoleViewContentType.NORMAL_OUTPUT_KEY);
+  public static final TextAttributesKey CYAN = TextAttributesKey.createTextAttributesKey(CYAN_ID, ConsoleViewContentType.NORMAL_OUTPUT_KEY);
+  public static final TextAttributesKey GRAY = TextAttributesKey.createTextAttributesKey(GRAY_ID, ConsoleViewContentType.NORMAL_OUTPUT_KEY);
+
+
+  public static final TextAttributesKey DARKGRAY =
+    TextAttributesKey.createTextAttributesKey(DARKGRAY_ID, ConsoleViewContentType.NORMAL_OUTPUT_KEY);
+  public static final TextAttributesKey RED_BRIGHT =
+    TextAttributesKey.createTextAttributesKey(RED_BRIGHT_ID, ConsoleViewContentType.NORMAL_OUTPUT_KEY);
+  public static final TextAttributesKey GREEN_BRIGHT =
+    TextAttributesKey.createTextAttributesKey(GREEN_BRIGHT_ID, ConsoleViewContentType.NORMAL_OUTPUT_KEY);
+  public static final TextAttributesKey YELLOW_BRIGHT =
+    TextAttributesKey.createTextAttributesKey(YELLOW_BRIGHT_ID, ConsoleViewContentType.NORMAL_OUTPUT_KEY);
+  public static final TextAttributesKey BLUE_BRIGHT =
+    TextAttributesKey.createTextAttributesKey(BLUE_BRIGHT_ID, ConsoleViewContentType.NORMAL_OUTPUT_KEY);
+  public static final TextAttributesKey MAGENTA_BRIGHT =
+    TextAttributesKey.createTextAttributesKey(MAGENTA_BRIGHT_ID, ConsoleViewContentType.NORMAL_OUTPUT_KEY);
+  public static final TextAttributesKey CYAN_BRIGHT =
+    TextAttributesKey.createTextAttributesKey(CYAN_BRIGHT_ID, ConsoleViewContentType.NORMAL_OUTPUT_KEY);
+  public static final TextAttributesKey WHITE =
+    TextAttributesKey.createTextAttributesKey(WHITE_ID, ConsoleViewContentType.NORMAL_OUTPUT_KEY);
+
 
   private ConsoleHighlighter() {
   }
