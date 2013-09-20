eb22a991a463291dc2b8c1deca61f631da78c006
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2013-09-20 15:52:53 +0200

    fix todo duplication when several pattern match -> prefer finding match with last pattern (in settings list), thus default TODO pattern is matched last

--- platform/indexing-impl/src/com/intellij/psi/impl/cache/impl/BaseFilterLexer.java
+++ platform/indexing-impl/src/com/intellij/psi/impl/cache/impl/BaseFilterLexer.java
@@ -22,6 +22,7 @@ import com.intellij.psi.impl.cache.impl.id.IdTableBuilding;
 import com.intellij.psi.search.IndexPattern;
 import com.intellij.psi.search.UsageSearchContext;
 import com.intellij.util.text.CharArrayUtil;
+import gnu.trove.TIntArrayList;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.regex.Matcher;
@@ -32,7 +33,7 @@ public abstract class BaseFilterLexer extends DelegateLexer implements IdTableBu
 
   private int myTodoScannedBound = 0;
   private int myOccurenceMask;
-  private TodoScanningData[] myTodoScanningData;
+  private TodoScanningState myTodoScanningState;
   private CharSequence myCachedBufferSequence;
   private char[] myCachedArraySequence;
   
@@ -49,49 +50,56 @@ public abstract class BaseFilterLexer extends DelegateLexer implements IdTableBu
     if (start >= end) return; // this prevents scanning of the same comment twice
 
     CharSequence input = myCachedBufferSequence.subSequence(start, end);
-    myTodoScanningData = advanceTodoItemsCount(input, myOccurrenceConsumer, myTodoScanningData);
+    myTodoScanningState = advanceTodoItemsCount(input, myOccurrenceConsumer, myTodoScanningState);
 
     myTodoScannedBound = end;
   }
 
-  public static class TodoScanningData {
-    final IndexPattern pattern;
-    final Matcher matcher;
+  public static class TodoScanningState {
+    final IndexPattern[] myPatterns;
+    final Matcher[] myMatchers;
+    TIntArrayList myOccurences;
 
-    public TodoScanningData(IndexPattern pattern, Matcher matcher) {
-      this.matcher = matcher;
-      this.pattern = pattern;
+    public TodoScanningState(IndexPattern[] patterns, Matcher[] matchers) {
+      myPatterns = patterns;
+      myMatchers = matchers;
+      myOccurences = new TIntArrayList(1);
     }
   }
 
-  public static TodoScanningData[] advanceTodoItemsCount(final CharSequence input, final OccurrenceConsumer consumer, TodoScanningData[] todoScanningData) {
-    if (todoScanningData == null) {
+  public static TodoScanningState advanceTodoItemsCount(final CharSequence input, final OccurrenceConsumer consumer, TodoScanningState todoScanningState) {
+    if (todoScanningState == null) {
       IndexPattern[] patterns = IndexPatternUtil.getIndexPatterns();
-      todoScanningData = new TodoScanningData[patterns.length];
+
+      Matcher[] matchers = new Matcher[patterns.length];
+      todoScanningState = new TodoScanningState(patterns, matchers);
 
       for (int i = 0; i < patterns.length; ++i) {
-        IndexPattern indexPattern = patterns[i];
-        Pattern pattern = indexPattern.getPattern();
+        Pattern pattern = patterns[i].getPattern();
 
         if (pattern != null) {
-          todoScanningData [i] = new TodoScanningData(indexPattern, pattern.matcher(""));
+          matchers[i] = pattern.matcher("");
         }
       }
+    } else {
+      todoScanningState.myOccurences.resetQuick();
     }
 
-    for (TodoScanningData data:todoScanningData) {
-      if (data == null) continue;
-      Matcher matcher = data.matcher;
+    for (int i = todoScanningState.myMatchers.length - 1; i >= 0; --i) {
+      Matcher matcher = todoScanningState.myMatchers[i];
+      if (matcher == null) continue;
       matcher.reset(input);
 
       while (matcher.find()) {
-        if (matcher.start() != matcher.end()) {
-          consumer.incTodoOccurrence(data.pattern);
+        int start = matcher.start();
+        if (start != matcher.end() && todoScanningState.myOccurences.indexOf(start) == -1) {
+          consumer.incTodoOccurrence(todoScanningState.myPatterns[i]);
+          todoScanningState.myOccurences.add(start);
         }
       }
     }
 
-    return todoScanningData;
+    return todoScanningState;
   }
 
   @Override
