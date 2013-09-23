c7c44317ecbab3a05caa1820dbe15ac17a95d454
peter <peter@jetbrains.com>
2013-09-23 20:03:19 +0200

    fix SliceBackwardTest

--- java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/ValuableDataFlowRunner.java
+++ java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/ValuableDataFlowRunner.java
@@ -95,5 +95,26 @@ public class ValuableDataFlowRunner extends DataFlowRunner {
       return myValue;
     }
 
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (!(o instanceof ValuableDfaVariableState)) return false;
+      if (!super.equals(o)) return false;
+
+      ValuableDfaVariableState state = (ValuableDfaVariableState)o;
+
+      if (myExpression != null ? !myExpression.equals(state.myExpression) : state.myExpression != null) return false;
+      if (myValue != null ? !myValue.equals(state.myValue) : state.myValue != null) return false;
+
+      return true;
+    }
+
+    @Override
+    public int hashCode() {
+      int result = super.hashCode();
+      result = 31 * result + (myValue != null ? myValue.hashCode() : 0);
+      result = 31 * result + (myExpression != null ? myExpression.hashCode() : 0);
+      return result;
+    }
   }
 }
