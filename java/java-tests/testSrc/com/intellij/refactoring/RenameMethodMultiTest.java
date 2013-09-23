1efd60435a307120418e389c70bd943900ba539a
Konstantin Bulenkov <kb@jetbrains.com>
2013-09-23 10:56:11 +0200

    update test data

--- java/java-tests/testSrc/com/intellij/refactoring/RenameMethodMultiTest.java
+++ java/java-tests/testSrc/com/intellij/refactoring/RenameMethodMultiTest.java
@@ -1,5 +1,21 @@
+/*
+ * Copyright 2000-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.intellij.refactoring;
 
+import com.intellij.JavaTestUtil;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.JavaPsiFacade;
@@ -7,7 +23,6 @@ import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiMethod;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.refactoring.rename.RenameProcessor;
-import com.intellij.JavaTestUtil;
 import org.junit.Assert;
 
 /**
@@ -51,7 +66,7 @@ public class RenameMethodMultiTest extends MultiFileTestCase {
     catch (BaseRefactoringProcessor.ConflictsInTestsException e) {
       Assert.assertEquals("Renaming method will override final \"method <b><code>A.finalMethod()</code></b>\"\n" +
                           "Method finalMethod() will override \n" +
-                          "a method of the base class <b><code>p.A</code></b>.", e.getMessage());
+                          "a method of the base class <b><code>p.A</code></b>", e.getMessage());
       return;
     }
     fail("Conflicts were not found");
