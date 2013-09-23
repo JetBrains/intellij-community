082417c107bce131f1faea2fdaace5a7849f4269
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2013-09-23 19:44:24 +0400

    fixed PY-9448 Add ability to directly create pdf's from sphinx documentation

--- python/python-rest/src/com/jetbrains/rest/run/sphinx/SphinxTasksModel.java
+++ python/python-rest/src/com/jetbrains/rest/run/sphinx/SphinxTasksModel.java
@@ -9,32 +9,36 @@ import java.util.List;
  * User : catherine
  */
 public class SphinxTasksModel extends CollectionComboBoxModel {
+  private static List<String> targets = Lists.newArrayList();
+  static {
+    targets.add("changes");
+    targets.add("coverage");
+    targets.add("devhelp");
+    targets.add("dirhtml");
+    targets.add("doctest");
+    targets.add("epub");
+    targets.add("gettext");
+    targets.add("html");
+    targets.add("htmlhelp");
+    targets.add("json");
+    targets.add("latex");
+    targets.add("latexpdf");
+    targets.add("linkcheck");
+    targets.add("man");
+    targets.add("pickle");
+    targets.add("qthelp");
+    targets.add("singlehtml");
+    targets.add("text");
+    targets.add("texinfo");
+    targets.add("web");
+    targets.add("websupport");
+  }
+
   public SphinxTasksModel() {
     super(getTasks(), "html");
   }
 
   private static List<String> getTasks() {
-    List<String> result = Lists.newArrayList();
-    result.add("html");
-    result.add("dirhtml");
-    result.add("singlehtml");
-    result.add("pickle");
-    result.add("json");
-    result.add("web");
-    result.add("htmlhelp");
-    result.add("devhelp");
-    result.add("qthelp");
-    result.add("epub");
-    result.add("latex");
-    result.add("text");
-    result.add("man");
-    result.add("texinfo");
-    result.add("changes");
-    result.add("linkcheck");
-    result.add("websupport");
-    result.add("gettext");
-    result.add("doctest");
-    result.add("coverage");
-    return result;
+    return targets;
   }
 }
