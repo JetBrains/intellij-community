19e1a9797936ae8b2b62dd91e02db688c00c4100
Alexey Kudravtsev <cdr@intellij.com>
2013-09-24 14:45:16 +0400

    made public

--- platform/editor-ui-ex/src/com/intellij/ide/todo/TodoConfiguration.java
+++ platform/editor-ui-ex/src/com/intellij/ide/todo/TodoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -52,9 +52,9 @@ public class TodoConfiguration implements NamedComponent, JDOMExternalizable {
   private final MessageBus myMessageBus;
 
   /**
-   * Invoked by reflection
+   * public for upsource
    */
-  TodoConfiguration(@NotNull MessageBus messageBus) {
+  public TodoConfiguration(@NotNull MessageBus messageBus) {
     myMessageBus = messageBus;
     resetToDefaultTodoPatterns();
   }
