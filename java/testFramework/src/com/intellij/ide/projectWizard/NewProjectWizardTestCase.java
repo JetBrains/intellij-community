27c6645ff01d2615dcc5be268cfb33c78312e974
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2013-09-25 17:30:07 +0400

    compilation fix

--- java/testFramework/src/com/intellij/ide/projectWizard/NewProjectWizardTestCase.java
+++ java/testFramework/src/com/intellij/ide/projectWizard/NewProjectWizardTestCase.java
@@ -1,8 +1,8 @@
 package com.intellij.ide.projectWizard;
 
 import com.intellij.ide.util.newProjectWizard.AbstractProjectWizard;
-import com.intellij.ide.wizard.Step;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ui.configuration.ModulesProvider;
 
 import java.io.File;
 
@@ -10,9 +10,9 @@ import java.io.File;
  * @author Dmitry Avdeev
  *         Date: 23.09.13
  */
-public class NewProjectWizardTestCase extends ProjectWizardTestCase {
+public abstract class NewProjectWizardTestCase extends ProjectWizardTestCase {
   @Override
-  protected AbstractProjectWizard<? extends Step> createWizard(Project project, File directory) {
-    return new NewProjectWizard(project, directory.getPath());
+  protected AbstractProjectWizard createWizard(Project project, File directory) {
+    return new NewProjectWizard(project, ModulesProvider.EMPTY_MODULES_PROVIDER, directory.getPath());
   }
 }
