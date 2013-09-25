66926021e9a41a5b184b9cac8498c87760ce0086
anna <Anna.Kozlova@jetbrains.com>
2013-09-25 14:29:52 +0200

    cleanup current file from highlighting markup

--- plugins/IdeaTestAssistant/src/com/intellij/internal/DumpCleanHighlightingTestdataAction.java
+++ plugins/IdeaTestAssistant/src/com/intellij/internal/DumpCleanHighlightingTestdataAction.java
@@ -15,10 +15,7 @@
  */
 package com.intellij.internal;
 
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.fileChooser.FileChooser;
@@ -31,6 +28,7 @@ import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiFile;
 import com.intellij.testFramework.ExpectedHighlightingData;
 
 import java.io.File;
@@ -47,6 +45,18 @@ public class DumpCleanHighlightingTestdataAction extends AnAction implements Dum
   public void actionPerformed(final AnActionEvent event) {
     final DataContext dataContext = event.getDataContext();
     final Project project = CommonDataKeys.PROJECT.getData(dataContext);
+    final PsiFile psiFile = CommonDataKeys.PSI_FILE.getData(dataContext);
+    if (psiFile != null) {
+      final VirtualFile virtualFile = psiFile.getVirtualFile();
+      if (virtualFile != null) {
+        final Document document = FileDocumentManager.getInstance().getDocument(virtualFile);
+        if (document != null) {
+          final ExpectedHighlightingData data = new ExpectedHighlightingData(document, true, true);
+          data.init();
+        }
+        return;
+      }
+    }
     final FileChooserDescriptor descriptor = FileChooserDescriptorFactory.createSingleFolderDescriptor();
     descriptor.setTitle("Choose Directory");
     descriptor.setDescription("Directory containing highlighting test data");
