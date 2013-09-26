0319948d5052a139f87e6b6c1ae25496265d0232
Alexey Kudravtsev <cdr@intellij.com>
2013-09-26 14:14:58 +0400

    made final

--- platform/core-api/src/com/intellij/concurrency/SameThreadExecutorWithTrampoline.java
+++ platform/core-api/src/com/intellij/concurrency/SameThreadExecutorWithTrampoline.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@ import java.util.concurrent.Executor;
  */
 public class SameThreadExecutorWithTrampoline implements Executor {
 
-  private ThreadLocal<Queue<Runnable>> myExecutionTrampoline = new ThreadLocal<Queue<Runnable>>();
+  private final ThreadLocal<Queue<Runnable>> myExecutionTrampoline = new ThreadLocal<Queue<Runnable>>();
 
   public SameThreadExecutorWithTrampoline() {}
 
