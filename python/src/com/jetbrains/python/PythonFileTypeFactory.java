3e3c5809cf1e2fe9d1fe2e1a978b63e48c61146e
Dmitry Jemerov <yole@jetbrains.com>
2013-09-27 11:52:22 +0200

    move PythonFileTypeFactory to correct place

--- python/src/com/jetbrains/python/PythonFileTypeFactory.java
+++ python/src/com/jetbrains/python/PythonFileTypeFactory.java
@@ -3,22 +3,14 @@ package com.jetbrains.python;
 import com.intellij.ide.highlighter.XmlFileType;
 import com.intellij.openapi.fileTypes.FileTypeConsumer;
 import com.intellij.openapi.fileTypes.FileTypeFactory;
-import com.intellij.ultimate.PluginVerifier;
-import com.intellij.ultimate.UltimateVerifier;
 import com.jetbrains.pyqt.QtUIFileType;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Plugin version of file type factory - uses UltimateVerifier.
- *
  * @author yole
  */
 public class PythonFileTypeFactory extends FileTypeFactory {
-  public PythonFileTypeFactory(UltimateVerifier verifier) {
-    PluginVerifier.verifyUltimatePlugin(verifier);
-  }
-
   public void createFileTypes(@NonNls @NotNull final FileTypeConsumer consumer) {
     consumer.consume(PythonFileType.INSTANCE, "py;pyw;");
     consumer.consume(QtUIFileType.INSTANCE, "ui");
