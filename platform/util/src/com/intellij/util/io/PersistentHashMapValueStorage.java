2383e90605b99ab17c3042fa839f62f689ab379e
Alexey Kudravtsev <cdr@intellij.com>
2013-09-27 14:41:49 +0400

    assertion for EA-50103 - AIOOBE: PersistentHashMapValueStorage.readBytes

--- platform/util/src/com/intellij/util/io/PersistentHashMapValueStorage.java
+++ platform/util/src/com/intellij/util/io/PersistentHashMapValueStorage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -306,6 +306,9 @@ public class PersistentHashMapValueStorage {
         myBufferStreamWrapper.init(myBuffer, 0, len);
 
         final int chunkSize = DataInputOutputUtil.readINT(myBufferDataStreamWrapper);
+        if (chunkSize < 0) {
+          throw new IOException("Value storage corrupted: negative chunk size: "+chunkSize);
+        }
         final long prevChunkAddress = readPrevChunkAddress(chunk);
         final int headerOffset = len - myBufferStreamWrapper.available();
 
