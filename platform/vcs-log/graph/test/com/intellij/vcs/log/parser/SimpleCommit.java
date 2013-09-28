ba4cf8d1b2d34d7bcf9c45ceb1358f121bec5c25
Kirill Likhodedov <Kirill.Likhodedov@gmail.com>
2013-09-28 18:23:31 +0400

    [log] create SimpleHash & SimpleCommit for tests to run w/o platform
    We could make graph tests to be UsefulTests instead,
    but it seems to be harder, and will slow down the tests.

---
+++ platform/vcs-log/graph/test/com/intellij/vcs/log/parser/SimpleCommit.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2000-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.vcs.log.parser;
+
+import com.intellij.vcs.log.Hash;
+import com.intellij.vcs.log.TimedVcsCommit;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class SimpleCommit implements TimedVcsCommit {
+
+  private final Hash myHash;
+  private final List<Hash> myParents;
+  private final long myTime;
+
+  public SimpleCommit(Hash commitHash, List<Hash> parentHashes, long time) {
+    myHash = commitHash;
+    myParents = parentHashes;
+    myTime = time;
+  }
+
+  @Override
+  public long getAuthorTime() {
+    return myTime;
+  }
+
+  @NotNull
+  @Override
+  public Hash getHash() {
+    return myHash;
+  }
+
+  @NotNull
+  @Override
+  public List<Hash> getParents() {
+    return myParents;
+  }
+}
