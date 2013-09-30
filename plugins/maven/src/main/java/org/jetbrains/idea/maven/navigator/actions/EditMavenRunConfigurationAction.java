1517564e22653eab6e652d8eed88dc510fc81a40
Sergey Evdokimov <sergey.evdokimov@jetbrains.com>
2013-09-30 20:50:16 +0400

    Add "Edit Run Configuration" action to Maven Tool Window.

---
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/navigator/actions/EditMavenRunConfigurationAction.java
@@ -0,0 +1,28 @@
+package org.jetbrains.idea.maven.navigator.actions;
+
+import com.intellij.execution.RunManager;
+import com.intellij.execution.RunnerAndConfigurationSettings;
+import com.intellij.execution.impl.EditConfigurationsDialog;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.idea.maven.utils.MavenDataKeys;
+
+/**
+ * @author Sergey Evdokimov
+ */
+public class EditMavenRunConfigurationAction extends AnAction {
+  @Override
+  public void actionPerformed(AnActionEvent e) {
+    Project project = PlatformDataKeys.PROJECT.getData(e.getDataContext());
+    RunnerAndConfigurationSettings settings = MavenDataKeys.RUN_CONFIGURATION.getData(e.getDataContext());
+
+    if (settings == null || project == null) return;
+
+    RunManager.getInstance(project).setSelectedConfiguration(settings);
+
+    EditConfigurationsDialog dialog = new EditConfigurationsDialog(project);
+    dialog.show();
+  }
+}
