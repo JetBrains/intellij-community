8fcffbb2c85970a53a2544a2194280164594fb5b
Dmitry Semeniouta <Dmitry.Semeniouta@jetbrains.com>
2013-09-30 21:28:23 +0400

    CodeInsightTestFixtureImpl: reset caret position and selection on 'configure...' (cherry picked from commit a2f6e65)

--- platform/testFramework/src/com/intellij/testFramework/fixtures/impl/CodeInsightTestFixtureImpl.java
+++ platform/testFramework/src/com/intellij/testFramework/fixtures/impl/CodeInsightTestFixtureImpl.java
@@ -1346,10 +1346,9 @@ public class CodeInsightTestFixtureImpl extends BaseFixture implements CodeInsig
         assert myEditor != null : "Editor couldn't be created for file: " +
                                   copy.getPath() +
                                   ", use copyFileToProject(..) method for this file instead of configureByFile(..)";
-        if (loader.caretMarker != null) {
-          int offset = loader.caretMarker.getStartOffset();
-          myEditor.getCaretModel().moveToOffset(offset);
-        }
+        int offset = loader.caretMarker != null ? loader.caretMarker.getStartOffset() : 0;
+        myEditor.getCaretModel().moveToOffset(offset);
+
         if (loader.selStartMarker != null && loader.selEndMarker != null) {
           int start = loader.selStartMarker.getStartOffset();
           int end = loader.selEndMarker.getStartOffset();
@@ -1359,6 +1358,9 @@ public class CodeInsightTestFixtureImpl extends BaseFixture implements CodeInsig
           else {
             myEditor.getSelectionModel().setSelection(start, end);
           }
+        }
+        else {
+          myEditor.getSelectionModel().removeSelection();
         }
 
         Module module = getModule();
