ee5c78f1bd10380deb06296c187ec33486707a78
Konstantin Bulenkov <kb@jetbrains.com>
2013-09-30 22:59:33 +0200

    fix table header (better font + force AA)

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorHeaderComponent.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorHeaderComponent.java
@@ -32,6 +32,14 @@ public class EditorHeaderComponent extends JPanel {
   }
 
   @Override
+  public void paint(Graphics g) {
+    final Graphics2D g2 = (Graphics2D)g;
+    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+    g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+    super.paint(g);
+  }
+
+  @Override
   protected void paintComponent(Graphics g) {
     super.paintComponent(g);
     paintGradient(g, this);
