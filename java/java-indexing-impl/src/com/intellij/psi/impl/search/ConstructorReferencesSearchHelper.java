5f5ca205970d6dc9cd02eb1e6903b0433dad4ad3
Alexey Kudravtsev <cdr@intellij.com>
2013-10-01 19:30:00 +0400

    cleanup, tiny optimisation

--- java/java-indexing-impl/src/com/intellij/psi/impl/search/ConstructorReferencesSearchHelper.java
+++ java/java-indexing-impl/src/com/intellij/psi/impl/search/ConstructorReferencesSearchHelper.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.intellij.psi.impl.search;
 
 import com.intellij.openapi.application.ApplicationManager;
@@ -38,6 +53,7 @@ public class ConstructorReferencesSearchHelper {
     final boolean[] isEnum = new boolean[1];
     final boolean[] isUnder18 = new boolean[1];
     ApplicationManager.getApplication().runReadAction(new Runnable() {
+      @Override
       public void run() {
         constructorCanBeCalledImplicitly[0] = constructor.getParameterList().getParametersCount() == 0;
         isEnum[0] = containingClass.isEnum();
@@ -83,6 +99,7 @@ public class ConstructorReferencesSearchHelper {
 
     // search usages like "this(..)"
     if (!ApplicationManager.getApplication().runReadAction(new Computable<Boolean>() {
+      @Override
       public Boolean compute() {
         return processSuperOrThis(containingClass, constructor, constructorCanBeCalledImplicitly[0], searchScope, isStrictSignatureSearch,
                                   PsiKeyword.THIS, processor);
@@ -111,6 +128,7 @@ public class ConstructorReferencesSearchHelper {
                                                @NotNull final PsiMethod constructor,
                                                @NotNull final PsiClass aClass) {
     return ApplicationManager.getApplication().runReadAction(new Computable<Boolean>() {
+      @Override
       public Boolean compute() {
         for (PsiField field : aClass.getFields()) {
           if (field instanceof PsiEnumConstant) {
@@ -161,7 +179,7 @@ public class ConstructorReferencesSearchHelper {
     }
     for (PsiMethod method : constructors) {
       PsiCodeBlock body = method.getBody();
-      if (body == null) {
+      if (body == null || method == constructor) {
         continue;
       }
       PsiStatement[] statements = body.getStatements();
@@ -202,6 +220,7 @@ public class ConstructorReferencesSearchHelper {
                                                  @NotNull final PsiClass containingClass) {
     if (containingClass instanceof PsiAnonymousClass) return true;
     boolean same = ApplicationManager.getApplication().runReadAction(new Computable<Boolean>() {
+      @Override
       public Boolean compute() {
         return myManager.areElementsEquivalent(constructor.getContainingClass(), containingClass.getSuperClass());
       }
