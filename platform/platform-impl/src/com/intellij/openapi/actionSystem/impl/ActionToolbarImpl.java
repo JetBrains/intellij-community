8619f20a70079c363893a4b3304dc2a4dc0e21a6
Konstantin Bulenkov <kb@jetbrains.com>
2013-10-01 12:07:22 +0200

    IDEA-114287 Search control in toolbar disappears when second project is open

--- platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/ActionToolbarImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/ActionToolbarImpl.java
@@ -303,9 +303,13 @@ public class ActionToolbarImpl extends JPanel implements ActionToolbar {
     if (Registry.is("search.everywhere.enabled") && (ActionPlaces.MAIN_TOOLBAR.equals(myPlace) || ActionPlaces.NAVIGATION_BAR.equals(myPlace))) {
       final AnAction searchEverywhereAction = ActionManager.getInstance().getAction("SearchEverywhere");
       if (searchEverywhereAction != null) {
-        final JComponent searchEverywhere = getCustomComponent(searchEverywhereAction);
-        searchEverywhere.putClientProperty("SEARCH_EVERYWHERE", Boolean.TRUE);
-        add(searchEverywhere);
+        try {
+          final JComponent searchEverywhere = getCustomComponent(searchEverywhereAction.getClass().newInstance());
+          searchEverywhere.putClientProperty("SEARCH_EVERYWHERE", Boolean.TRUE);
+          add(searchEverywhere);
+        }
+        catch (InstantiationException ignore) {}
+        catch (IllegalAccessException ignore) {}
       }
     }
   }
