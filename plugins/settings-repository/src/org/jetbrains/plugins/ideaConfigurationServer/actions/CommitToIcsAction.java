59171018adafd107442843411ab9767ee61792ae
develar <develar@gmail.com>
2013-10-02 16:44:18 +0200

    CR-IC-2522 BeforeCommitDialogHandler

--- plugins/settings-repository/src/org/jetbrains/plugins/ideaConfigurationServer/actions/CommitToIcsAction.java
+++ plugins/settings-repository/src/org/jetbrains/plugins/ideaConfigurationServer/actions/CommitToIcsAction.java
@@ -4,14 +4,18 @@ import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.components.StorageScheme;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ex.ProjectEx;
+import com.intellij.openapi.vcs.CheckinProjectPanel;
 import com.intellij.openapi.vcs.FilePath;
 import com.intellij.openapi.vcs.FileStatus;
 import com.intellij.openapi.vcs.actions.CommonCheckinFilesAction;
 import com.intellij.openapi.vcs.actions.VcsContext;
 import com.intellij.openapi.vcs.changes.Change;
 import com.intellij.openapi.vcs.changes.ChangeListManager;
+import com.intellij.openapi.vcs.changes.CommitContext;
 import com.intellij.openapi.vcs.changes.CommitExecutor;
-import com.intellij.openapi.vcs.checkin.BeforeCommitDialogHandler;
+import com.intellij.openapi.vcs.checkin.BeforeCheckinDialogHandler;
+import com.intellij.openapi.vcs.checkin.CheckinHandler;
+import com.intellij.openapi.vcs.checkin.CheckinHandlerFactory;
 import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.Consumer;
@@ -27,16 +31,32 @@ import java.util.Collections;
 import java.util.List;
 
 class CommitToIcsAction extends CommonCheckinFilesAction {
-  static class IcsBeforeCommitDialogHandler extends BeforeCommitDialogHandler {
-    @Override
-    public boolean beforeCommitDialogShownCallback(@NotNull Project project, @NotNull List<Change> changes, @NotNull Iterable<CommitExecutor> executors, boolean showVcsCommit) {
-      ProjectChangeCollectConsumer collectConsumer = new ProjectChangeCollectConsumer(project);
-      collectProjectChanges(changes, collectConsumer);
+  static class IcsBeforeCommitDialogHandler extends CheckinHandlerFactory {
+    private static final BeforeCheckinDialogHandler BEFORE_CHECKIN_DIALOG_HANDLER = new BeforeCheckinDialogHandler() {
+      @Override
+      public boolean beforeCommitDialogShown(@NotNull Project project,
+                                             @NotNull List<Change> changes,
+                                             @NotNull Iterable<CommitExecutor> executors,
+                                             boolean showVcsCommit) {
+        ProjectChangeCollectConsumer collectConsumer = new ProjectChangeCollectConsumer(project);
+        collectProjectChanges(changes, collectConsumer);
 
-      if (collectConsumer.hasResult()) {
-        new CommitToIcsDialog(project, collectConsumer.getResult()).show();
+        if (collectConsumer.hasResult()) {
+          new CommitToIcsDialog(project, collectConsumer.getResult()).show();
+        }
+        return true;
       }
-      return true;
+    };
+
+    @NotNull
+    @Override
+    public CheckinHandler createHandler(CheckinProjectPanel panel, CommitContext commitContext) {
+      return CheckinHandler.DUMMY;
+    }
+
+    @Override
+    public BeforeCheckinDialogHandler createSystemReadyHandler(Project project) {
+      return BEFORE_CHECKIN_DIALOG_HANDLER;
     }
   }
 
