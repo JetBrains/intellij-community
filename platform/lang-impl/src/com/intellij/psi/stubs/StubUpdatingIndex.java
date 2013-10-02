256bec1f97d95edc18643f0d2587c83c5f1e2f64
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2013-10-02 17:25:52 +0200

    advance stub versions as we fixed problem with before / after events in FileBasedIndexImpl

--- platform/lang-impl/src/com/intellij/psi/stubs/StubUpdatingIndex.java
+++ platform/lang-impl/src/com/intellij/psi/stubs/StubUpdatingIndex.java
@@ -50,7 +50,7 @@ public class StubUpdatingIndex extends CustomImplementationFileBasedIndexExtensi
 
   public static final ID<Integer, SerializedStubTree> INDEX_ID = ID.create("Stubs");
 
-  private static final int VERSION = 25;
+  private static final int VERSION = 26;
 
   private static final DataExternalizer<SerializedStubTree> KEY_EXTERNALIZER = new DataExternalizer<SerializedStubTree>() {
     @Override
