3625b6feb2c3231c52a029a6864ed77436debad5
Konstantin Bulenkov <kb@jetbrains.com>
2013-10-02 11:50:31 +0200

    IDEA-114294 BackSpace key doesn't work in GoTo Class/File/Action

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/navigator/actions/RemoveMavenRunConfigurationAction.java
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/navigator/actions/RemoveMavenRunConfigurationAction.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jetbrains.idea.maven.navigator.actions;
 
 import com.intellij.execution.RunManager;
@@ -6,7 +21,6 @@ import com.intellij.execution.RunnerAndConfigurationSettings;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.idea.maven.utils.MavenDataKeys;
 
@@ -21,13 +35,23 @@ public class RemoveMavenRunConfigurationAction extends AnAction {
     Project project = CommonDataKeys.PROJECT.getData(e.getDataContext());
     RunnerAndConfigurationSettings settings = MavenDataKeys.RUN_CONFIGURATION.getData(e.getDataContext());
 
-    if (settings == null || project == null) return;
+    assert settings != null && project != null;
 
-    int res = JOptionPane
-      .showConfirmDialog(JOptionPane.getRootFrame(), "Delete \"" + settings.getName() + "\"?", "Confirmation", JOptionPane.YES_NO_OPTION);
+    int res = JOptionPane.showConfirmDialog(JOptionPane.getRootFrame(),
+                                            "Delete \"" + settings.getName() + "\"?",
+                                            "Confirmation", JOptionPane.YES_NO_OPTION);
 
     if (res == JOptionPane.YES_OPTION) {
       ((RunManagerEx)RunManager.getInstance(project)).removeConfiguration(settings);
     }
+  }
+
+  @Override
+  public void update(AnActionEvent e) {
+    Project project = CommonDataKeys.PROJECT.getData(e.getDataContext());
+    RunnerAndConfigurationSettings settings = MavenDataKeys.RUN_CONFIGURATION.getData(e.getDataContext());
+
+    boolean enabled = settings != null && project != null;
+    e.getPresentation().setEnabledAndVisible(enabled);
   }
 }
