ef1d96d2584b8263cfd932ead0d79667e2cf5418
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2013-10-03 18:41:36 +0400

    Remove all debug-specific code from annotator

--- plugins/tasks/tasks-core/src/com/intellij/tasks/youtrack/lang/YouTrackHighlightingAnnotator.java
+++ plugins/tasks/tasks-core/src/com/intellij/tasks/youtrack/lang/YouTrackHighlightingAnnotator.java
@@ -5,15 +5,11 @@ import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.ExternalAnnotator;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.markup.EffectType;
-import com.intellij.openapi.editor.markup.TextAttributes;
 import com.intellij.psi.PsiFile;
 import com.intellij.tasks.youtrack.YouTrackIntellisense;
-import com.intellij.ui.JBColor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.awt.*;
 import java.util.Collections;
 import java.util.List;
 
@@ -26,16 +22,9 @@ import static com.intellij.tasks.youtrack.lang.YouTrackHighlightingAnnotator.Que
 public class YouTrackHighlightingAnnotator extends ExternalAnnotator<QueryInfo, List<HighlightRange>> {
   private static Logger LOG = Logger.getInstance(YouTrackHighlightingAnnotator.class);
 
-  private static final boolean DEBUG = false;
-  private static final TextAttributes DEBUG_ATTRIBUTES =
-    new TextAttributes(JBColor.RED, JBColor.WHITE, JBColor.GREEN, EffectType.STRIKEOUT, Font.BOLD);
-
   @Nullable
   @Override
   public QueryInfo collectInformation(@NotNull PsiFile file, @NotNull Editor editor, boolean hasErrors) {
-    if (DEBUG) {
-      return new QueryInfo(0, "empty", null);
-    }
     final YouTrackIntellisense intellisense = file.getUserData(YouTrackIntellisense.INTELLISENSE_KEY);
     if (intellisense == null || !intellisense.getRepository().isConfigured()) {
       return null;
@@ -49,12 +38,11 @@ public class YouTrackHighlightingAnnotator extends ExternalAnnotator<QueryInfo, 
   @Nullable
   @Override
   public List<HighlightRange> doAnnotate(QueryInfo collectedInfo) {
-    //noinspection ConstantConditions
-    if (collectedInfo == null || DEBUG) {
+    if (collectedInfo == null) {
       return Collections.emptyList();
     }
-    String query = collectedInfo.myText;
-    int offset = collectedInfo.myCaretOffset;
+    final String query = collectedInfo.myText;
+    final int offset = collectedInfo.myCaretOffset;
     try {
       return collectedInfo.myIntellisense.fetchHighlighting(query, offset);
     }
@@ -65,11 +53,6 @@ public class YouTrackHighlightingAnnotator extends ExternalAnnotator<QueryInfo, 
 
   @Override
   public void apply(@NotNull PsiFile file, List<HighlightRange> ranges, @NotNull AnnotationHolder holder) {
-    if (DEBUG) {
-      Annotation annotation = holder.createErrorAnnotation(file.getFirstChild(), "Debug message");
-      annotation.setEnforcedTextAttributes(DEBUG_ATTRIBUTES);
-      return;
-    }
     for (HighlightRange range : ranges) {
       if (range.getStyleClass().equals("error")) {
         holder.createErrorAnnotation(range.getTextRange(), null);
