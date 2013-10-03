05a5e6eb4a3a6dd1bff662319d778ce8a1cb29bc
Konstantin Bulenkov <kb@jetbrains.com>
2013-10-03 18:34:48 +0200

    realistic isIdeaProject() impl

--- platform/platform-api/src/com/intellij/util/PlatformUtils.java
+++ platform/platform-api/src/com/intellij/util/PlatformUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -80,10 +80,9 @@ public class PlatformUtils extends PlatformUtilsCore {
   }
 
   public static boolean isIdeaProject(@Nullable Project project) {
-    final VirtualFile baseDir;
-    return project != null
-           && ("IDEA".equals(project.getName()) || "community".equals(project.getName()))
-           && (baseDir = project.getBaseDir()) != null
-           && baseDir.findFileByRelativePath("plugins") != null;
+    if (project == null) return false;
+    final VirtualFile baseDir = project.getBaseDir();
+    if (baseDir == null) return false;
+    return baseDir.findChild("idea.iml") != null || baseDir.findChild("community-main.iml") != null;
   }
 }
