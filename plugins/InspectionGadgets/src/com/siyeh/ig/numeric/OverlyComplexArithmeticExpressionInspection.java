03219cee1fb69039b59a2ef82c1b52b0b9ab3ec7
Alexey Kudravtsev <cdr@intellij.com>
2013-10-10 14:55:42 +0400

    moved to analysis

--- plugins/InspectionGadgets/src/com/siyeh/ig/numeric/OverlyComplexArithmeticExpressionInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/numeric/OverlyComplexArithmeticExpressionInspection.java
@@ -15,33 +15,16 @@
  */
 package com.siyeh.ig.numeric;
 
-import com.intellij.psi.*;
-import com.intellij.psi.tree.IElementType;
+import com.intellij.codeInspection.ui.SingleIntegerFieldOptionsPanel;
+import com.intellij.psi.JavaTokenType;
 import com.siyeh.InspectionGadgetsBundle;
-import com.siyeh.ig.BaseInspection;
-import com.siyeh.ig.BaseInspectionVisitor;
 import com.siyeh.ig.InspectionGadgetsFix;
 import com.siyeh.ig.fixes.ExtractMethodFix;
-import com.siyeh.ig.psiutils.TypeUtils;
-import com.intellij.codeInspection.ui.SingleIntegerFieldOptionsPanel;
-import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
-import java.util.HashSet;
-import java.util.Set;
 
 public class OverlyComplexArithmeticExpressionInspection
-  extends BaseInspection {
-
-  private static final int TERM_LIMIT = 6;
-
-  /**
-   * @noinspection PublicField
-   */
-  public int m_limit = TERM_LIMIT;  //this is public for the DefaultJDOMExternalizer thingy
-
-  private static final Set<IElementType> arithmeticTokens =
-    new HashSet<IElementType>(5);
+  extends OverlyComplexArithmeticExpressionInspectionBase {
 
   static {
     arithmeticTokens.add(JavaTokenType.PLUS);
@@ -49,20 +32,6 @@ public class OverlyComplexArithmeticExpressionInspection
     arithmeticTokens.add(JavaTokenType.ASTERISK);
     arithmeticTokens.add(JavaTokenType.DIV);
     arithmeticTokens.add(JavaTokenType.PERC);
-  }
-
-  @Override
-  @NotNull
-  public String getDisplayName() {
-    return InspectionGadgetsBundle.message(
-      "overly.complex.arithmetic.expression.display.name");
-  }
-
-  @Override
-  @NotNull
-  protected String buildErrorString(Object... infos) {
-    return InspectionGadgetsBundle.message(
-      "overly.complex.arithmetic.expression.problem.descriptor");
   }
 
   @Override
@@ -76,113 +45,5 @@ public class OverlyComplexArithmeticExpressionInspection
   @Override
   protected InspectionGadgetsFix buildFix(Object... infos) {
     return new ExtractMethodFix();
-  }
-
-  @Override
-  protected boolean buildQuickFixesOnlyForOnTheFlyErrors() {
-    return true;
-  }
-
-  @Override
-  public BaseInspectionVisitor buildVisitor() {
-    return new OverlyComplexArithmeticExpressionVisitor();
-  }
-
-  private class OverlyComplexArithmeticExpressionVisitor
-    extends BaseInspectionVisitor {
-
-    @Override
-    public void visitBinaryExpression(
-      @NotNull PsiBinaryExpression expression) {
-      super.visitBinaryExpression(expression);
-      checkExpression(expression);
-    }
-
-    @Override
-    public void visitPrefixExpression(
-      @NotNull PsiPrefixExpression expression) {
-      super.visitPrefixExpression(expression);
-      checkExpression(expression);
-    }
-
-    @Override
-    public void visitParenthesizedExpression(
-      PsiParenthesizedExpression expression) {
-      super.visitParenthesizedExpression(expression);
-      checkExpression(expression);
-    }
-
-    private void checkExpression(PsiExpression expression) {
-      if (isParentArithmetic(expression)) {
-        return;
-      }
-      if (!isArithmetic(expression)) {
-        return;
-      }
-      final int numTerms = countTerms(expression);
-      if (numTerms <= m_limit) {
-        return;
-      }
-      registerError(expression);
-    }
-
-    private int countTerms(PsiExpression expression) {
-      if (!isArithmetic(expression)) {
-        return 1;
-      }
-      if (expression instanceof PsiBinaryExpression) {
-        final PsiBinaryExpression binaryExpression =
-          (PsiBinaryExpression)expression;
-        final PsiExpression lhs = binaryExpression.getLOperand();
-        final PsiExpression rhs = binaryExpression.getROperand();
-        return countTerms(lhs) + countTerms(rhs);
-      }
-      else if (expression instanceof PsiPrefixExpression) {
-        final PsiPrefixExpression prefixExpression =
-          (PsiPrefixExpression)expression;
-        final PsiExpression operand = prefixExpression.getOperand();
-        return countTerms(operand);
-      }
-      else if (expression instanceof PsiParenthesizedExpression) {
-        final PsiParenthesizedExpression parenthesizedExpression =
-          (PsiParenthesizedExpression)expression;
-        final PsiExpression contents = parenthesizedExpression.getExpression();
-        return countTerms(contents);
-      }
-      return 1;
-    }
-
-    private boolean isParentArithmetic(PsiExpression expression) {
-      final PsiElement parent = expression.getParent();
-      if (!(parent instanceof PsiExpression)) {
-        return false;
-      }
-      return isArithmetic((PsiExpression)parent);
-    }
-
-    private boolean isArithmetic(PsiExpression expression) {
-      if (expression instanceof PsiBinaryExpression) {
-        final PsiType type = expression.getType();
-        if (TypeUtils.isJavaLangString(type)) {
-          return false; //ignore string concatenations
-        }
-        final PsiBinaryExpression binaryExpression =
-          (PsiBinaryExpression)expression;
-        return arithmeticTokens.contains(binaryExpression.getOperationTokenType());
-      }
-      else if (expression instanceof PsiPrefixExpression) {
-        final PsiPrefixExpression prefixExpression =
-          (PsiPrefixExpression)expression;
-        return arithmeticTokens.contains(prefixExpression.getOperationTokenType());
-      }
-      else if (expression instanceof PsiParenthesizedExpression) {
-        final PsiParenthesizedExpression parenthesizedExpression =
-          (PsiParenthesizedExpression)expression;
-        final PsiExpression contents =
-          parenthesizedExpression.getExpression();
-        return isArithmetic(contents);
-      }
-      return false;
-    }
   }
 }
