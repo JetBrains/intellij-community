6bf16de05788c9e5904957482ce8d7c1d53540dc
Alexey Kudravtsev <cdr@intellij.com>
2013-10-10 16:36:57 +0400

    moved to analysis

--- plugins/InspectionGadgets/src/com/siyeh/ig/junit/BeforeClassOrAfterClassIsPublicStaticVoidNoArgInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/junit/BeforeClassOrAfterClassIsPublicStaticVoidNoArgInspection.java
@@ -17,46 +17,19 @@ package com.siyeh.ig.junit;
 
 import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.*;
-import com.intellij.psi.util.PsiFormatUtil;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiModifier;
+import com.intellij.psi.PsiModifierList;
+import com.intellij.psi.PsiType;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.refactoring.changeSignature.ChangeSignatureProcessor;
 import com.intellij.refactoring.changeSignature.ParameterInfoImpl;
 import com.intellij.util.IncorrectOperationException;
-import com.siyeh.InspectionGadgetsBundle;
-import com.siyeh.ig.BaseInspection;
-import com.siyeh.ig.BaseInspectionVisitor;
 import com.siyeh.ig.InspectionGadgetsFix;
-import com.siyeh.ig.psiutils.TestUtils;
 import org.jetbrains.annotations.NotNull;
 
 public class BeforeClassOrAfterClassIsPublicStaticVoidNoArgInspection
-  extends BaseInspection {
-
-  @Override
-  @NotNull
-  public String getID() {
-    return "BeforeOrAfterWithIncorrectSignature";
-  }
-
-  @Override
-  @NotNull
-  public String getDisplayName() {
-    return InspectionGadgetsBundle.message(
-      "before.class.or.after.class.is.public.static.void.no.arg.display.name");
-  }
-
-  @Override
-  @NotNull
-  protected String buildErrorString(Object... infos) {
-    return InspectionGadgetsBundle.message(
-      "before.class.or.after.class.is.public.static.void.no.arg.problem.descriptor");
-  }
-
-  @Override
-  public BaseInspectionVisitor buildVisitor() {
-    return new BeforeClassOrAfterClassIsPublicStaticVoidNoArgVisitor();
-  }
+  extends BeforeClassOrAfterClassIsPublicStaticVoidNoArgInspectionBase {
 
   @Override
   protected InspectionGadgetsFix buildFix(Object... infos) {
@@ -64,43 +37,6 @@ public class BeforeClassOrAfterClassIsPublicStaticVoidNoArgInspection
     final Object name = infos[0];
     if (!(name instanceof String)) return null;
     return new MakePublicStaticVoidFix((String)name);
-  }
-
-  private static class BeforeClassOrAfterClassIsPublicStaticVoidNoArgVisitor
-    extends BaseInspectionVisitor {
-
-    @Override
-    public void visitMethod(@NotNull PsiMethod method) {
-      //note: no call to super;
-      if (!TestUtils.isJUnit4BeforeClassOrAfterClassMethod(method)) {
-        return;
-      }
-      final PsiType returnType = method.getReturnType();
-      if (returnType == null) {
-        return;
-      }
-      final PsiClass targetClass = method.getContainingClass();
-      if (targetClass == null) {
-        return;
-      }
-
-      final PsiParameterList parameterList = method.getParameterList();
-      if (parameterList.getParametersCount() != 0 ||
-          !returnType.equals(PsiType.VOID) ||
-          !method.hasModifierProperty(PsiModifier.PUBLIC) ||
-          !method.hasModifierProperty(PsiModifier.STATIC)) {
-        registerMethodError(method, "Change signature of \'" +
-                                    PsiFormatUtil.formatMethod(method, PsiSubstitutor.EMPTY,
-                                                               PsiFormatUtil.SHOW_NAME |
-                                                               PsiFormatUtil.SHOW_MODIFIERS |
-                                                               PsiFormatUtil.SHOW_PARAMETERS |
-                                                               PsiFormatUtil.SHOW_TYPE,
-                                                               PsiFormatUtil.SHOW_TYPE) +
-                                    "\' to \'public static void " +
-                                    method.getName() +
-                                    "()\'");
-      }
-    }
   }
 
   private static class MakePublicStaticVoidFix extends InspectionGadgetsFix {
