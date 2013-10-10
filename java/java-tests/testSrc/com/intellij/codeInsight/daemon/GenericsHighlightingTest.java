efb328123b9d799c778bd9153f451e1c65bdb2c9
anna <Anna.Kozlova@jetbrains.com>
2013-10-10 20:29:35 +0200

    testdata for IDEA-67591

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/GenericsHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/GenericsHighlightingTest.java
@@ -319,6 +319,9 @@ public class GenericsHighlightingTest extends LightDaemonAnalyzerTestCase {
   public void testIDEA67518() throws Exception { doTest5(false); }
   public void testIDEA57252() throws Exception { doTest5(false); }
   public void testIDEA57274() throws Exception { doTest(LanguageLevel.JDK_1_7, JavaSdkVersion.JDK_1_7, false); }
+  public void testIDEA67591() throws Exception {
+    doTest5(false);
+  }
 
   public void testJavaUtilCollections_NoVerify() throws Exception {
     PsiClass collectionsClass = getJavaFacade().findClass("java.util.Collections", GlobalSearchScope.moduleWithLibrariesScope(getModule()));
