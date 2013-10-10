1ea667f01029393332e7a3ee5cbca0b0df2c39d6
Alexey Kudravtsev <cdr@intellij.com>
2013-10-10 17:24:59 +0400

    moved to analysis

--- plugins/InspectionGadgets/src/com/siyeh/ig/errorhandling/BadExceptionCaughtInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/errorhandling/BadExceptionCaughtInspection.java
@@ -18,65 +18,15 @@ package com.siyeh.ig.errorhandling;
 import com.intellij.codeInspection.ui.ListTable;
 import com.intellij.codeInspection.ui.ListWrappingTableModel;
 import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.psi.PsiCatchSection;
-import com.intellij.psi.PsiParameter;
-import com.intellij.psi.PsiType;
-import com.intellij.psi.PsiTypeElement;
-import com.intellij.psi.util.PsiTreeUtil;
 import com.siyeh.InspectionGadgetsBundle;
-import com.siyeh.ig.BaseInspection;
-import com.siyeh.ig.BaseInspectionVisitor;
-import com.siyeh.ig.ui.ExternalizableStringSet;
 import com.siyeh.ig.ui.UiUtils;
-import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.util.List;
 
-public class BadExceptionCaughtInspection extends BaseInspection {
-
-  /**
-   * @noinspection PublicField
-   */
-  public String exceptionsString = "";
-
-  /**
-   * @noinspection PublicField
-   */
-  public final ExternalizableStringSet exceptions =
-    new ExternalizableStringSet(
-      "java.lang.NullPointerException",
-      "java.lang.IllegalMonitorStateException",
-      "java.lang.ArrayIndexOutOfBoundsException"
-    );
+public class BadExceptionCaughtInspection extends BadExceptionCaughtInspectionBase {
 
   public BadExceptionCaughtInspection() {
-    if (exceptionsString.length() != 0) {
-      exceptions.clear();
-      final List<String> strings = StringUtil.split(exceptionsString, ",");
-      for (String string : strings) {
-        exceptions.add(string);
-      }
-      exceptionsString = "";
-    }
-  }
-
-  @Override
-  @NotNull
-  public String getID() {
-    return "ProhibitedExceptionCaught";
-  }
-
-  @Override
-  @NotNull
-  public String getDisplayName() {
-    return InspectionGadgetsBundle.message("bad.exception.caught.display.name");
-  }
-
-  @Override
-  @NotNull
-  public String buildErrorString(Object... infos) {
-    return InspectionGadgetsBundle.message("bad.exception.caught.problem.descriptor");
   }
 
   @Override
@@ -85,42 +35,5 @@ public class BadExceptionCaughtInspection extends BaseInspection {
       new ListTable(new ListWrappingTableModel(exceptions, InspectionGadgetsBundle.message("exception.class.column.name")));
     return UiUtils.createAddRemoveTreeClassChooserPanel(table, InspectionGadgetsBundle.message("choose.exception.class"),
                                                         "java.lang.Throwable");
-  }
-
-  @Override
-  public BaseInspectionVisitor buildVisitor() {
-    return new BadExceptionCaughtVisitor();
-  }
-
-  private class BadExceptionCaughtVisitor extends BaseInspectionVisitor {
-
-    @Override
-    public void visitCatchSection(PsiCatchSection section) {
-      super.visitCatchSection(section);
-      final PsiParameter parameter = section.getParameter();
-      if (parameter == null) {
-        return;
-      }
-      final PsiTypeElement typeElement = parameter.getTypeElement();
-      if (typeElement == null) {
-        return;
-      }
-      final PsiTypeElement[] childTypeElements = PsiTreeUtil.getChildrenOfType(typeElement, PsiTypeElement.class);
-      if (childTypeElements != null) {
-        for (PsiTypeElement childTypeElement : childTypeElements) {
-          checkTypeElement(childTypeElement);
-        }
-      }
-      else {
-        checkTypeElement(typeElement);
-      }
-    }
-
-    private void checkTypeElement(PsiTypeElement typeElement) {
-      final PsiType type = typeElement.getType();
-      if (exceptions.contains(type.getCanonicalText())) {
-        registerError(typeElement);
-      }
-    }
   }
 }
