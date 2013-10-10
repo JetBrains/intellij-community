cba23f49a75f6dccb5935c2e31ea45ef13d9e128
Alexey Kudravtsev <cdr@intellij.com>
2013-10-10 15:11:20 +0400

    moved to analysis

--- plugins/InspectionGadgets/src/com/siyeh/ig/naming/MethodNamesDifferOnlyByCaseInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/naming/MethodNamesDifferOnlyByCaseInspection.java
@@ -16,41 +16,13 @@
 package com.siyeh.ig.naming;
 
 import com.intellij.codeInspection.ui.SingleCheckboxOptionsPanel;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiIdentifier;
-import com.intellij.psi.PsiMethod;
 import com.siyeh.InspectionGadgetsBundle;
-import com.siyeh.ig.BaseInspection;
-import com.siyeh.ig.BaseInspectionVisitor;
 import com.siyeh.ig.InspectionGadgetsFix;
 import com.siyeh.ig.fixes.RenameFix;
-import com.siyeh.ig.psiutils.MethodUtils;
-import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 
-public class MethodNamesDifferOnlyByCaseInspection extends BaseInspection {
-
-  @SuppressWarnings("PublicField")
-  public boolean ignoreIfMethodIsOverride = true;
-
-  @Override
-  @NotNull
-  public String getID() {
-    return "MethodNamesDifferingOnlyByCase";
-  }
-
-  @Override
-  @NotNull
-  public String getDisplayName() {
-    return InspectionGadgetsBundle.message("method.names.differ.only.by.case.display.name");
-  }
-
-  @Override
-  @NotNull
-  public String buildErrorString(Object... infos) {
-    return InspectionGadgetsBundle.message("method.names.differ.only.by.case.problem.descriptor", infos[0]);
-  }
+public class MethodNamesDifferOnlyByCaseInspection extends MethodNamesDifferOnlyByCaseInspectionBase {
 
   @Override
   public JComponent createOptionsPanel() {
@@ -59,46 +31,7 @@ public class MethodNamesDifferOnlyByCaseInspection extends BaseInspection {
   }
 
   @Override
-  public BaseInspectionVisitor buildVisitor() {
-    return new MethodNamesDifferOnlyByCaseVisitor();
-  }
-
-  @Override
-  protected boolean buildQuickFixesOnlyForOnTheFlyErrors() {
-    return true;
-  }
-
-  @Override
   protected InspectionGadgetsFix buildFix(Object... infos) {
     return new RenameFix();
-  }
-
-  private class MethodNamesDifferOnlyByCaseVisitor extends BaseInspectionVisitor {
-
-    @Override
-    public void visitMethod(@NotNull PsiMethod method) {
-      if (method.isConstructor()) {
-        return;
-      }
-      final PsiIdentifier nameIdentifier = method.getNameIdentifier();
-      if (nameIdentifier == null) {
-        return;
-      }
-      final String methodName = method.getName();
-      if (ignoreIfMethodIsOverride && MethodUtils.hasSuper(method)) {
-        return;
-      }
-      final PsiClass aClass = method.getContainingClass();
-      if (aClass == null) {
-        return;
-      }
-      final PsiMethod[] methods = aClass.getAllMethods();
-      for (PsiMethod testMethod : methods) {
-        final String testMethodName = testMethod.getName();
-        if (!methodName.equals(testMethodName) && methodName.equalsIgnoreCase(testMethodName)) {
-          registerError(nameIdentifier, testMethodName);
-        }
-      }
-    }
   }
 }
