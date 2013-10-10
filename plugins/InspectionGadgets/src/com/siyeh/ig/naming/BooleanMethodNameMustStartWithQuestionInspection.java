03cd3735e4dc8dfa18dc8d9a1e18a33465073418
Alexey Kudravtsev <cdr@intellij.com>
2013-10-10 15:26:54 +0400

    moved to analysis

--- plugins/InspectionGadgets/src/com/siyeh/ig/naming/BooleanMethodNameMustStartWithQuestionInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/naming/BooleanMethodNameMustStartWithQuestionInspection.java
@@ -17,74 +17,18 @@ package com.siyeh.ig.naming;
 
 import com.intellij.codeInspection.ui.ListTable;
 import com.intellij.codeInspection.ui.ListWrappingTableModel;
-import com.intellij.openapi.util.InvalidDataException;
-import com.intellij.openapi.util.WriteExternalException;
-import com.intellij.psi.CommonClassNames;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiMethod;
-import com.intellij.psi.PsiType;
 import com.intellij.util.ui.CheckBox;
 import com.siyeh.InspectionGadgetsBundle;
-import com.siyeh.ig.BaseInspection;
-import com.siyeh.ig.BaseInspectionVisitor;
 import com.siyeh.ig.InspectionGadgetsFix;
 import com.siyeh.ig.fixes.RenameFix;
-import com.siyeh.ig.psiutils.LibraryUtil;
-import com.siyeh.ig.psiutils.MethodUtils;
 import com.siyeh.ig.ui.UiUtils;
-import org.jdom.Element;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
 
-public class BooleanMethodNameMustStartWithQuestionInspection extends BaseInspection {
-
-  @SuppressWarnings({"PublicField"})
-  public boolean ignoreBooleanMethods = false;
-
-  @SuppressWarnings({"PublicField"})
-  public boolean ignoreInAnnotationInterface = true;
-
-  @SuppressWarnings({"PublicField"})
-  public boolean onlyWarnOnBaseMethods = true;
-
-  /**
-   * @noinspection PublicField
-   */
-  @NonNls public String questionString = "is,can,has,should,could,will,shall,check,contains,equals,add,put,remove,startsWith,endsWith";
-
-  List<String> questionList = new ArrayList(32);
+public class BooleanMethodNameMustStartWithQuestionInspection extends BooleanMethodNameMustStartWithQuestionInspectionBase {
 
   public BooleanMethodNameMustStartWithQuestionInspection() {
-    parseString(questionString, questionList);
-  }
-
-  @Override
-  @NotNull
-  public String getDisplayName() {
-    return InspectionGadgetsBundle.message("boolean.method.name.must.start.with.question.display.name");
-  }
-
-  @Override
-  @NotNull
-  public String buildErrorString(Object... infos) {
-    return InspectionGadgetsBundle.message("boolean.method.name.must.start.with.question.problem.descriptor");
-  }
-
-  @Override
-  public void readSettings(@NotNull Element element) throws InvalidDataException {
-    super.readSettings(element);
-    parseString(questionString, questionList);
-  }
-
-  @Override
-  public void writeSettings(@NotNull Element element) throws WriteExternalException {
-    questionString = formatString(questionList);
-    super.writeSettings(element);
   }
 
   @Override
@@ -123,52 +67,5 @@ public class BooleanMethodNameMustStartWithQuestionInspection extends BaseInspec
   @Override
   protected InspectionGadgetsFix buildFix(Object... infos) {
     return new RenameFix();
-  }
-
-  @Override
-  protected boolean buildQuickFixesOnlyForOnTheFlyErrors() {
-    return true;
-  }
-
-  @Override
-  public BaseInspectionVisitor buildVisitor() {
-    return new BooleanMethodNameMustStartWithQuestionVisitor();
-  }
-
-  private class BooleanMethodNameMustStartWithQuestionVisitor extends BaseInspectionVisitor {
-
-    @Override
-    public void visitMethod(@NotNull PsiMethod method) {
-      final PsiType returnType = method.getReturnType();
-      if (returnType == null) {
-        return;
-      }
-      else if (!returnType.equals(PsiType.BOOLEAN)) {
-        if (ignoreBooleanMethods || !returnType.equalsToText(CommonClassNames.JAVA_LANG_BOOLEAN)) {
-          return;
-        }
-      }
-      if (ignoreInAnnotationInterface) {
-        final PsiClass containingClass = method.getContainingClass();
-        if (containingClass != null && containingClass.isAnnotationType()) {
-          return;
-        }
-      }
-      final String name = method.getName();
-      for (String question : questionList) {
-        if (name.startsWith(question)) {
-          return;
-        }
-      }
-      if (onlyWarnOnBaseMethods) {
-        if (MethodUtils.hasSuper(method)) {
-          return;
-        }
-      }
-      else if (LibraryUtil.isOverrideOfLibraryMethod(method)) {
-        return;
-      }
-      registerMethodError(method);
-    }
   }
 }
