f06ad8a2e893083c9fac3aa4c1cf3ba49b7a84f4
Alexey Kudravtsev <cdr@intellij.com>
2013-10-10 14:54:28 +0400

    moved to analysis

--- plugins/InspectionGadgets/src/com/siyeh/ig/performance/ToArrayCallWithZeroLengthArrayArgumentInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/performance/ToArrayCallWithZeroLengthArrayArgumentInspection.java
@@ -18,44 +18,16 @@ package com.siyeh.ig.performance;
 import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
-import com.intellij.psi.util.InheritanceUtil;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.IncorrectOperationException;
 import com.siyeh.InspectionGadgetsBundle;
-import com.siyeh.ig.BaseInspection;
-import com.siyeh.ig.BaseInspectionVisitor;
 import com.siyeh.ig.InspectionGadgetsFix;
-import com.siyeh.ig.psiutils.CollectionUtils;
-import com.siyeh.ig.psiutils.ExpressionUtils;
 import com.siyeh.ig.psiutils.HighlightUtils;
-import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class ToArrayCallWithZeroLengthArrayArgumentInspection extends BaseInspection {
-
-  @Override
-  @Nls
-  @NotNull
-  public String getDisplayName() {
-    return InspectionGadgetsBundle.message(
-      "to.array.call.with.zero.length.array.argument.display.name");
-  }
-
-  @Override
-  @NotNull
-  protected String buildErrorString(Object... infos) {
-    final PsiExpression argument = (PsiExpression)infos[1];
-    return InspectionGadgetsBundle.message(
-      "to.array.call.with.zero.length.array.argument.problem.descriptor",
-      argument.getText());
-  }
-
-  @Override
-  public boolean isEnabledByDefault() {
-    return true;
-  }
+public class ToArrayCallWithZeroLengthArrayArgumentInspection extends ToArrayCallWithZeroLengthArrayArgumentInspectionBase {
 
   @Override
   @Nullable
@@ -148,60 +120,6 @@ public class ToArrayCallWithZeroLengthArrayArgumentInspection extends BaseInspec
       final PsiMethodCallExpression sizeExpression = (PsiMethodCallExpression)argument.getArrayDimensions()[0];
       final PsiReferenceExpression ref2 = (PsiReferenceExpression)sizeExpression.getMethodExpression().getQualifierExpression();
       HighlightUtils.showRenameTemplate(context, variable, ref1, ref2);
-    }
-  }
-
-  @Override
-  public BaseInspectionVisitor buildVisitor() {
-    return new ToArrayCallWithZeroLengthArrayArgument();
-  }
-
-  private static class ToArrayCallWithZeroLengthArrayArgument extends BaseInspectionVisitor {
-
-    @Override
-    public void visitMethodCallExpression(PsiMethodCallExpression expression) {
-      super.visitMethodCallExpression(expression);
-      final PsiReferenceExpression methodExpression = expression.getMethodExpression();
-      @NonNls final String methodName = methodExpression.getReferenceName();
-      if (!"toArray".equals(methodName)) {
-        return;
-      }
-      final PsiExpressionList argumentList = expression.getArgumentList();
-      final PsiExpression[] arguments = argumentList.getExpressions();
-      if (arguments.length != 1) {
-        return;
-      }
-      final PsiExpression argument = arguments[0];
-      final PsiType type = argument.getType();
-      if (!(type instanceof PsiArrayType)) {
-        return;
-      }
-      if (type.getArrayDimensions() != 1) {
-        return;
-      }
-      if (argument instanceof PsiReferenceExpression) {
-        final PsiReferenceExpression referenceExpression = (PsiReferenceExpression)argument;
-        final PsiElement element = referenceExpression.resolve();
-        if (!(element instanceof PsiField)) {
-          return;
-        }
-        final PsiField field = (PsiField)element;
-        if (!CollectionUtils.isConstantEmptyArray(field)) {
-          return;
-        }
-      }
-      else if (!ExpressionUtils.isZeroLengthArrayConstruction(argument)) {
-        return;
-      }
-      final PsiMethod method = expression.resolveMethod();
-      if (method == null) {
-        return;
-      }
-      final PsiClass containingClass = method.getContainingClass();
-      if (!InheritanceUtil.isInheritor(containingClass, CommonClassNames.JAVA_UTIL_COLLECTION)) {
-        return;
-      }
-      registerMethodCallError(expression, expression, argument);
     }
   }
 }
