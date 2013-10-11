359ddf81dfd1a4e24fa0da6d91a86f7f5c9b7bba
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2013-10-11 12:08:39 +0200

    IDEA-114647 (fsnotifier size check updated)

--- platform/platform-impl/src/com/intellij/openapi/vfs/impl/local/FileWatcher.java
+++ platform/platform-impl/src/com/intellij/openapi/vfs/impl/local/FileWatcher.java
@@ -219,7 +219,7 @@ public class FileWatcher {
   }
 
   private static boolean isUpToDate(File executable) {
-    long length = SystemInfo.isWindows ? 70216 :
+    long length = SystemInfo.isWindows ? 71208 :
                   SystemInfo.isMac ? 13924 :
                   SystemInfo.isLinux ? SystemInfo.isAMD64 ? 29155 : 22791 :
                   -1;
