4710a086323fc8ce6eee2a38f211d3b5fcf544dc
anna <Anna.Kozlova@jetbrains.com>
2013-10-11 16:56:08 +0200

    show editor notification for text mapped files about known plugins from the repository

--- platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/pluginsAdvertisement/PluginsAdvertiser.java
+++ platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/pluginsAdvertisement/PluginsAdvertiser.java
@@ -24,8 +24,10 @@ import com.intellij.notification.*;
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationInfo;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.PathManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.extensions.PluginId;
+import com.intellij.openapi.fileTypes.FileTypeFactory;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.progress.Task;
@@ -33,28 +35,36 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
 import com.intellij.openapi.updateSettings.impl.PluginDownloader;
 import com.intellij.openapi.updateSettings.impl.UpdateSettings;
+import com.intellij.openapi.util.JDOMUtil;
+import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.util.Function;
 import com.intellij.util.net.HttpConfigurable;
+import com.intellij.util.xmlb.XmlSerializer;
+import com.intellij.util.xmlb.XmlSerializerUtil;
+import com.intellij.util.xmlb.annotations.*;
+import com.intellij.util.xmlb.annotations.AbstractCollection;
+import org.jdom.Document;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import javax.swing.event.HyperlinkEvent;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.*;
 import java.net.HttpURLConnection;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class PluginsAdvertiser implements StartupActivity {
   private static final Logger LOG = Logger.getInstance("#" + PluginsAdvertiser.class.getName());
+  private static final String FEATURE_IMPLEMENTATIONS_URL = "http://plugins.jetbrains.com/feature/getImplementations?";
+  private static final String CASHED_EXTENSIONS = "extensions.xml";
 
   public static List<PluginId> retrieve(UnknownFeature unknownFeature) {
     final String featureType = unknownFeature.getFeatureType();
     final String implementationName = unknownFeature.getImplementationName();
     final String buildNumber = ApplicationInfo.getInstance().getBuild().asString();
-    final String pluginRepositoryUrl = "http://plugins.jetbrains.com/feature/getImplementations?" +
+    final String pluginRepositoryUrl = FEATURE_IMPLEMENTATIONS_URL +
                                        "featureType=" + featureType +
                                        "&implementationName=" + implementationName.replaceAll("#", "%23") +
                                        "&build=" + buildNumber;
@@ -84,12 +94,70 @@ public class PluginsAdvertiser implements StartupActivity {
     }
   }
 
+  private static Map<String, Set<PluginId>> loadSupportedExtensions() {
+    final String pluginRepositoryUrl = FEATURE_IMPLEMENTATIONS_URL + "featureType=" + FileTypeFactory.FILE_TYPE_FACTORY_EP.getName();
+    try {
+      HttpURLConnection connection = HttpConfigurable.getInstance().openHttpConnection(pluginRepositoryUrl);
+      connection.connect();
+      final InputStreamReader streamReader = new InputStreamReader(connection.getInputStream());
+      try {
+        final JsonReader jsonReader = new JsonReader(streamReader);
+        jsonReader.setLenient(true);
+        final JsonElement jsonRootElement = new JsonParser().parse(jsonReader);
+        final Map<String, Set<PluginId>> result = new HashMap<String, Set<PluginId>>();
+        for (JsonElement jsonElement : jsonRootElement.getAsJsonArray()) {
+          final JsonObject jsonObject = jsonElement.getAsJsonObject();
+          final JsonElement ext = jsonObject.get("implementationName");
+          final String extension = StringUtil.unquoteString(ext.toString());
+          Set<PluginId> pluginIds = result.get(extension);
+          if (pluginIds == null) {
+            pluginIds = new HashSet<PluginId>();
+            result.put(extension, pluginIds);
+          }
+          pluginIds.add(PluginId.getId(StringUtil.unquoteString(jsonObject.get("pluginId").toString())));
+        }
+        saveExtensions(result);
+        return result;
+      }
+      finally {
+        streamReader.close();
+      }
+    }
+    catch (IOException e) {
+      LOG.info(e);
+      return null;
+    }
+  }
+
+  public static KnownExtensions loadExtensions() {
+    try {
+      File file = new File(PathManager.getPluginsPath(), CASHED_EXTENSIONS);
+      if (file.isFile()) {
+        final Document document = JDOMUtil.loadDocument(file);
+        return XmlSerializer.deserialize(document, KnownExtensions.class);
+      }
+    }
+    catch (Exception ignore) {
+    }
+    return null;
+  }
+
+  private static void saveExtensions(Map<String, Set<PluginId>> extensions) throws IOException {
+    File plugins = new File(PathManager.getPluginsPath(), CASHED_EXTENSIONS);
+    if (!plugins.isFile()) {
+      FileUtil.ensureCanCreateFile(plugins);
+    }
+    plugins.deleteOnExit();
+    JDOMUtil.writeDocument(new Document(XmlSerializer.serialize(new KnownExtensions(extensions))), plugins, "\n");
+  }
+  
   @Override
   public void runActivity(@NotNull final Project project) {
     if (!UpdateSettings.getInstance().CHECK_NEEDED) return;
     final UnknownFeaturesCollector collectorSuggester = UnknownFeaturesCollector.getInstance(project);
     final Set<UnknownFeature> unknownFeatures = collectorSuggester.getUnknownFeatures();
-    if (unknownFeatures.isEmpty()) return;
+    final KnownExtensions extensions = loadExtensions();
+    if (extensions != null && unknownFeatures.isEmpty()) return;
     final Runnable runnable = new Runnable() {
       public void run() {
         final Application application = ApplicationManager.getApplication();
@@ -100,6 +168,9 @@ public class PluginsAdvertiser implements StartupActivity {
 
           @Override
           public void run(@NotNull ProgressIndicator indicator) {
+            if (extensions == null) {
+              loadSupportedExtensions();
+            }
             int idx = 0;
             final Set<PluginId> ids = new HashSet<PluginId>();
             for (UnknownFeature feature : unknownFeatures) {
@@ -162,6 +233,43 @@ public class PluginsAdvertiser implements StartupActivity {
     };
 
     SwingUtilities.invokeLater(runnable);
+  }
+
+  @Tag("exts")
+  public static class KnownExtensions {
+    @MapAnnotation(surroundWithTag = false, surroundKeyWithTag = false, surroundValueWithTag = false)
+    public Map<String, PluginSet> myExtensions = new HashMap<String, PluginSet>();
+
+    public KnownExtensions() {
+    }
+
+    public KnownExtensions(Map<String, Set<PluginId>> extensions) {
+      for (String ext : extensions.keySet()) {
+        myExtensions.put(ext, new PluginSet(extensions.get(ext)));
+      }
+    }
+
+    public Set<String> find(String extension) {
+      final PluginSet pluginSet = myExtensions.get(extension);
+      if (pluginSet != null) {
+        return pluginSet.myPlugins;
+      }
+      return null;
+    }
+  }
+
+  @Tag("plugins")
+  public static class PluginSet {
+    public Set<String> myPlugins = new HashSet<String>();
+
+    public PluginSet() {
+    }
+
+    public PluginSet(Set<PluginId> plugins) {
+      for (PluginId plugin : plugins) {
+        myPlugins.add(plugin.toString());
+      }
+    }
   }
 }
 
