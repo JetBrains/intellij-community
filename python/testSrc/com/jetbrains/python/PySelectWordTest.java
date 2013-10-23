fde3cf42ffbf64f5db67249e5536ba70c52434ab
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2013-10-23 19:23:39 +0400

    Reimplement select word tests for CSS, CoffeeScript, Ruby, Python, Sass, Scss and Less (cherry-picked from ee6a21f)

--- python/testSrc/com/jetbrains/python/PySelectWordTest.java
+++ python/testSrc/com/jetbrains/python/PySelectWordTest.java
@@ -15,12 +15,8 @@
  */
 package com.jetbrains.python;
 
-import com.intellij.codeInsight.editorActions.SelectWordHandler;
-import com.intellij.ide.DataManager;
+import com.intellij.testFramework.fixtures.CodeInsightTestUtil;
 import com.jetbrains.python.fixtures.PyTestCase;
-import org.jetbrains.annotations.NonNls;
-
-import java.io.File;
 
 /**
  * @author yole
@@ -51,27 +47,6 @@ public class PySelectWordTest extends PyTestCase {
   }
 
   private void doTest() {
-    @NonNls final String path = "selectWord/" + getTestName(true);
-    myFixture.copyDirectoryToProject(path, path);
-    myFixture.configureByFile(path + "/before.py");
-    int i = 1;
-    while (true) {
-      @NonNls String resultPath = path + "/after" + i + ".py";
-      if (new File(getTestDataPath() + "/" + resultPath).exists()) {
-        performAction();
-        //System.out.println("comparing with "+resultPath);
-        myFixture.checkResultByFile(resultPath, false);
-        i++;
-      }
-      else {
-        break;
-      }
-    }
-    assertTrue(i > 1);
-  }
-
-  private void performAction() {
-    SelectWordHandler action = new SelectWordHandler(null);
-    action.execute(myFixture.getEditor(), DataManager.getInstance().getDataContext());
+    CodeInsightTestUtil.doWordSelectionTestOnDirectory(myFixture, "selectWord/" + getTestName(true), "py");
   }
 }
