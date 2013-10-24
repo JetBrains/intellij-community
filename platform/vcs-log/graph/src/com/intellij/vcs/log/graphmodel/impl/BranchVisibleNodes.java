876f751361eacfdea849df211676b561624218b5
Kirill Likhodedov <Kirill.Likhodedov@gmail.com>
2013-10-24 16:10:45 +0400

    [log] cleanup

--- platform/vcs-log/graph/src/com/intellij/vcs/log/graphmodel/impl/BranchVisibleNodes.java
+++ platform/vcs-log/graph/src/com/intellij/vcs/log/graphmodel/impl/BranchVisibleNodes.java
@@ -20,16 +20,18 @@ import java.util.Set;
  * @author erokhins
  */
 public class BranchVisibleNodes {
-  private final MutableGraph graph;
-  private Set<Node> visibleNodes = Collections.emptySet();
 
-  public BranchVisibleNodes(MutableGraph graph) {
-    this.graph = graph;
+  @NotNull private final MutableGraph myGraph;
+  @NotNull private Set<Node> myVisibleNodes = Collections.emptySet();
+
+  public BranchVisibleNodes(@NotNull MutableGraph graph) {
+    myGraph = graph;
   }
 
+  @NotNull
   public Set<Node> generateVisibleBranchesNodes(@NotNull Function<Node, Boolean> isStartedNode) {
     Set<Node> visibleNodes = new HashSet<Node>();
-    for (MutableNodeRow row : graph.getAllRows()) {
+    for (MutableNodeRow row : myGraph.getAllRows()) {
       for (MutableNode node : row.getInnerNodeList()) {
         if (isStartedNode.fun(node)) {
           visibleNodes.add(node);
@@ -45,20 +47,20 @@ public class BranchVisibleNodes {
   }
 
   public void setVisibleNodes(@NotNull Set<Node> visibleNodes) {
-    this.visibleNodes = visibleNodes;
+    myVisibleNodes = visibleNodes;
   }
 
   public Set<Node> getVisibleNodes() {
-    return Collections.unmodifiableSet(visibleNodes);
+    return Collections.unmodifiableSet(myVisibleNodes);
   }
 
   public boolean isVisibleNode(@NotNull Node node) {
-    return visibleNodes.contains(node);
+    return myVisibleNodes.contains(node);
   }
 
   @Nullable
   public Node getNodeIfVisible(@NotNull final Hash hash) {
-    return ContainerUtil.find(visibleNodes, new Condition<Node>() {
+    return ContainerUtil.find(myVisibleNodes, new Condition<Node>() {
       @Override
       public boolean value(Node node) {
         return node.getCommitHash().equals(hash);
