f56bde39a87e01501997da02e544b49f75eea9d5
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2013-10-29 16:01:27 +0400

    'os.error' is now defined in Python skeletons

--- python/src/com/jetbrains/python/codeInsight/stdlib/PyStdlibModuleMembersProvider.java
+++ python/src/com/jetbrains/python/codeInsight/stdlib/PyStdlibModuleMembersProvider.java
@@ -17,10 +17,9 @@ package com.jetbrains.python.codeInsight.stdlib;
 
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.QualifiedName;
 import com.jetbrains.python.codeInsight.PyDynamicMember;
 import com.jetbrains.python.psi.PyFile;
-import com.jetbrains.python.psi.impl.PyBuiltinCache;
-import com.intellij.psi.util.QualifiedName;
 import com.jetbrains.python.psi.resolve.ResolveImportUtil;
 import com.jetbrains.python.psi.types.PyModuleMembersProvider;
 
@@ -38,15 +37,10 @@ public class PyStdlibModuleMembersProvider extends PyModuleMembersProvider {
     if (qName.equals("os")) {
       final List<PyDynamicMember> results = new ArrayList<PyDynamicMember>();
       PsiElement path = null;
-      PsiElement osError = null;
       if (module != null) {
-        final PyBuiltinCache builtinCache = PyBuiltinCache.getInstance(module);
-        osError = builtinCache.getByName("OSError");
-
         final String pathModuleName = SystemInfo.isWindows ? "ntpath" : "posixpath";
         path = ResolveImportUtil.resolveModuleInRoots(QualifiedName.fromDottedString(pathModuleName), module);
       }
-      results.add(new PyDynamicMember("error", osError));
       results.add(new PyDynamicMember("path", path));
       return results;
     }
