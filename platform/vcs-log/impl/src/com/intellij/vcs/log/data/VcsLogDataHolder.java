ac28ad01adc8404135d67258bfa3da6c022bc1ab
Kirill Likhodedov <Kirill.Likhodedov@gmail.com>
2013-10-30 14:09:36 +0400

    [log] Disable the assertion until properly fixed.

--- platform/vcs-log/impl/src/com/intellij/vcs/log/data/VcsLogDataHolder.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/data/VcsLogDataHolder.java
@@ -720,9 +720,12 @@ public class VcsLogDataHolder implements Disposable {
         // log the error only once for the getTopCommitDetails request
         if (!errorDetailsAttached.get()) {
           errorDetailsAttached.set(true);
-          LOG.error(errorMessage,
-                    new Attachment("details_cache.txt", myTopCommitsDetailsCache.toString()),
-                    new Attachment("top_commits.txt", topCommits.toString()));
+          // temporary disable the error message until the bug is properly fixed.
+          // the bug is: we store 1000 commit details per root, but iterate through the roots_num * 1000 latest commits
+          // therefore this error shouldn't happen only in the case when commits of all repositories are evently distributed in time.
+//          LOG.error(errorMessage,
+//                    new Attachment("details_cache.txt", myTopCommitsDetailsCache.toString()),
+//                    new Attachment("top_commits.txt", topCommits.toString()));
         }
         return null;
       }
