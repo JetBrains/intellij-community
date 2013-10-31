1913a066754a207ab6347a473567f359bf54e196
Bas Leijdekkers <basleijdekkers@gmail.com>
2013-10-31 13:30:33 +0100

    IDEA-115604 (Intention "Negate '?:'")

--- plugins/IntentionPowerPak/testSrc/com/siyeh/ipp/IPPTestCase.java
+++ plugins/IntentionPowerPak/testSrc/com/siyeh/ipp/IPPTestCase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2011 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,7 @@ import com.intellij.openapi.application.PluginPathManager;
 import com.intellij.testFramework.fixtures.CodeInsightTestUtil;
 import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
 import com.intellij.util.SmartList;
+import org.jetbrains.annotations.NonNls;
 
 import java.util.List;
 
@@ -30,6 +31,7 @@ public abstract class IPPTestCase extends LightCodeInsightFixtureTestCase {
     return PluginPathManager.getPluginHomePathRelative("IntentionPowerPak") + "/test/com/siyeh/ipp/" + getRelativePath();
   }
 
+  @NonNls
   protected abstract String getRelativePath();
 
   protected void doTest() {
