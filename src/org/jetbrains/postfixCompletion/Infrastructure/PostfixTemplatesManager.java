1c54ba91c4d13863299911fba8a7977cb4c12989
Alexander.Shvedov <shvedov89@gmail.com>
2013-11-01 22:34:06 +0400

    Bits of 'x > 0.if'-like expressions support

--- src/org/jetbrains/postfixCompletion/Infrastructure/PostfixTemplatesManager.java
+++ src/org/jetbrains/postfixCompletion/Infrastructure/PostfixTemplatesManager.java
@@ -2,50 +2,88 @@ package org.jetbrains.postfixCompletion.Infrastructure;
 
 import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.psi.*;
+import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class PostfixTemplatesManager implements ApplicationComponent {
   @NotNull private final PostfixTemplateProvider[] myTemplateProviders;
 
-  public PostfixTemplatesManager(@NotNull PostfixTemplateProvider[] providers) {
+  public PostfixTemplatesManager(@NotNull final PostfixTemplateProvider[] providers) {
     myTemplateProviders = providers;
   }
 
-
-
-  public boolean getAvailableActions(@NotNull PsiElement positionElement) {
-
-    // simple case: someExpression.postfix
+  public boolean getAvailableActions(@NotNull final PsiElement positionElement) {
     if (positionElement instanceof PsiIdentifier) {
       final PsiReferenceExpression referenceExpression =
-        match(positionElement.getParent(), PsiReferenceExpression.class);
+        PsiTreeUtil.getParentOfType(positionElement, PsiReferenceExpression.class);
       if (referenceExpression != null) {
-        final PsiExpression qualifierExpression = referenceExpression.getQualifierExpression();
-        if (qualifierExpression != null) {
-          final PsiType type = qualifierExpression.getType();
-          final PsiPrimitiveType primitiveType = match(type, PsiPrimitiveType.class);
-          if (primitiveType != null) {
-            if (primitiveType == PsiType.BOOLEAN) {
 
+        // simple case: someExpression.postfix
+        final PsiExpression qualifier = referenceExpression.getQualifierExpression();
+        if (qualifier != null) {
+          doOtherWork(referenceExpression, qualifier);
+          return true;
+        }
 
-                return true;
+        // hard case: x > 0.if (two expression statements)
+        if (referenceExpression.getFirstChild() instanceof PsiReferenceParameterList) {
+          final PsiExpressionStatement statement =
+            PsiTreeUtil.getParentOfType(referenceExpression, PsiExpressionStatement.class);
+          if (statement != null) {
+            final PsiExpressionStatement prevStatement =
+              PsiTreeUtil.getPrevSiblingOfType(statement, PsiExpressionStatement.class);
+            if (prevStatement != null){
+              // look for 'non-terminated expression-statement' error
+              final PsiElement lastErrorChild = prevStatement.getLastChild();
+              if (lastErrorChild instanceof PsiErrorElement) {
+                PsiExpression expression = prevStatement.getExpression();
+                if (prevStatement.getFirstChild() == expression &&
+                    lastErrorChild.getPrevSibling() == expression) {
 
+                  PsiLiteralExpression brokenLiteral = null;
+                  do {
+                    // look for double literal broken by dot at end
+                    if (expression instanceof PsiLiteralExpression) {
+                      final PsiJavaToken token = PsiTreeUtil.getChildOfType(expression, PsiJavaToken.class);
+                      if (token != null
+                          && token.getTokenType() == JavaTokenType.DOUBLE_LITERAL
+                          && token.getText().endsWith(".")) {
+                        brokenLiteral = (PsiLiteralExpression) expression;
+                        break;
+                      }
+                    }
+
+                    // skip current expression and look its last inner expression
+                    final PsiElement last = expression.getLastChild();
+                    if (last instanceof PsiExpression) expression = (PsiExpression) last;
+                    else expression = PsiTreeUtil.getPrevSiblingOfType(last, PsiExpression.class);
+                  } while (expression != null);
+
+                  if (brokenLiteral != null) {
+                    return true;
+                  }
+                }
+              }
             }
           }
-
-          //return true;
         }
       }
     }
 
 
+    //if (positionElement instanceof )
 
-    for (PostfixTemplateProvider templateProvider : myTemplateProviders) {
+
+    return true;
+  }
+
+  private void doOtherWork(PsiReferenceExpression reference, PsiExpression expression) {
+
+    for (final PostfixTemplateProvider templateProvider : myTemplateProviders) {
 
     }
 
-    return true;
   }
 
   @Nullable
