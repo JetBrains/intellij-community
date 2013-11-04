d7a70253549ba0fef723746519ce1fb35941f444
Bas Leijdekkers <basleijdekkers@gmail.com>
2013-11-04 19:59:49 +0100

    move test to correct location

--- plugins/InspectionGadgets/testsrc/com/siyeh/ig/maturity/ThrowablePrintStackTraceInspectionTest.java
+++ plugins/InspectionGadgets/testsrc/com/siyeh/ig/maturity/ThrowablePrintStackTraceInspectionTest.java
@@ -13,10 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.siyeh.ig;
+package com.siyeh.ig.maturity;
 
 import com.intellij.codeInspection.InspectionProfileEntry;
-import com.siyeh.ig.maturity.ThrowablePrintStackTraceInspection;
+import com.siyeh.ig.LightInspectionTestCase;
 
 /**
  * @author Bas Leijdekkers
