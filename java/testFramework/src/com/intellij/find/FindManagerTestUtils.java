202e2ae6468f1b9928ec387d23dcc0c209dedea1
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2013-11-07 13:09:33 +0100

    Cleanup (test)
    - Groovy tests moved to corresponding module
    - assertions cleaned
    - parasitic exceptions suppressed

--- java/testFramework/src/com/intellij/find/FindManagerTestUtils.java
+++ java/testFramework/src/com/intellij/find/FindManagerTestUtils.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.intellij.find;
 
 import com.intellij.testFramework.LightVirtualFile;
@@ -8,10 +23,7 @@ public class FindManagerTestUtils {
     runFindInCommentsAndLiterals(findManager, findModel, text, "java");
   }
 
-  static void runFindInCommentsAndLiterals(FindManager findManager,
-                                           FindModel findModel,
-                                           String text,
-                                           String ext) {
+  public static void runFindInCommentsAndLiterals(FindManager findManager, FindModel findModel, String text, String ext) {
     findModel.setInStringLiteralsOnly(true);
     findModel.setInCommentsOnly(false);
     runFindForwardAndBackward(findManager, findModel, text, ext);
@@ -21,7 +33,7 @@ public class FindManagerTestUtils {
     runFindForwardAndBackward(findManager, findModel, text, ext);
   }
 
-  static void runFindForwardAndBackward(FindManager findManager, FindModel findModel, String text) {
+  public static void runFindForwardAndBackward(FindManager findManager, FindModel findModel, String text) {
     runFindForwardAndBackward(findManager, findModel, text, "java");
   }
 
