29ac8730a6bc6326a3d0930c7006902efe9cbf5e
Gregory.Shrago <gregory.shrago@jetbrains.com>
2013-11-08 02:09:47 +0400

    IDEA-97390 Java Debugger: View Text action shows white on white [2]

--- platform/platform-impl/src/com/intellij/ui/EditorTextField.java
+++ platform/platform-impl/src/com/intellij/ui/EditorTextField.java
@@ -413,26 +413,14 @@ public class EditorTextField extends NonOpaquePanel implements DocumentListener,
     // set mode in editor
     editor.setOneLineMode(isOneLineMode);
 
-    final EditorColorsManager mgr = EditorColorsManager.getInstance();
-    final EditorColorsScheme defaultScheme = UIUtil.isUnderDarcula()
-                                             ? mgr.getScheme(mgr.getGlobalScheme().getName())
-                                             : mgr.getScheme(EditorColorsManager.DEFAULT_SCHEME_NAME);
-    final EditorColorsScheme customGlobalScheme = isOneLineMode ? defaultScheme : null;
+    EditorColorsManager colorsManager = EditorColorsManager.getInstance();
+    final EditorColorsScheme defaultScheme =
+      UIUtil.isUnderDarcula() ? colorsManager.getGlobalScheme() : colorsManager.getScheme(EditorColorsManager.DEFAULT_SCHEME_NAME);
+    EditorColorsScheme customGlobalScheme = isOneLineMode? defaultScheme : null;
 
-    // Probably we need change scheme only for color schemas with white BG, but on the other hand
-    // FindUsages dialog always uses FindUsages color scheme based on a default one and should be also fixed
-    //
-    //final EditorColorsScheme customGlobalScheme;
-    //final EditorColorsScheme currentScheme = EditorColorsManager.getInstance().getGlobalScheme();
-    //if (currentScheme.getDefaultBackground() == Color.WHITE) {
-    //  customGlobalScheme = currentScheme;
-    //} else {
-    //  final EditorColorsScheme defaultScheme = EditorColorsManager.getInstance().getScheme(EditorColorsManager.DEFAULT_SCHEME_NAME);
-    //  customGlobalScheme = isOneLineMode ? defaultScheme : null;
-    //}
     editor.setColorsScheme(editor.createBoundColorSchemeDelegate(customGlobalScheme));
 
-    final EditorColorsScheme colorsScheme = editor.getColorsScheme();
+    EditorColorsScheme colorsScheme = editor.getColorsScheme();
     colorsScheme.setColor(EditorColors.CARET_ROW_COLOR, null);
     editor.setColorsScheme(new DelegateColorScheme(colorsScheme) {
       @Override
@@ -449,7 +437,7 @@ public class EditorTextField extends NonOpaquePanel implements DocumentListener,
     // color scheme settings:
     setupEditorFont(editor);
     updateBorder(editor);
-    editor.setBackgroundColor(getBackgroundColor(!myIsViewer, colorsScheme));
+    editor.setBackgroundColor(getBackgroundColor(isEnabled(), colorsScheme));
   }
 
 
