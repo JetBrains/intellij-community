b7c6e4c4654275851f2c16c9386e6bd9db1d32a9
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2013-11-08 14:55:33 +0100

    overrides

--- plugins/copyright/src/com/maddyhome/idea/copyright/ui/CopyrightProfilesPanel.java
+++ plugins/copyright/src/com/maddyhome/idea/copyright/ui/CopyrightProfilesPanel.java
@@ -33,7 +33,7 @@ import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.openapi.ui.popup.PopupStep;
 import com.intellij.openapi.ui.popup.util.BaseListPopupStep;
-import com.intellij.openapi.vfs.VfsUtil;
+import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.IconUtil;
 import com.intellij.util.PlatformIcons;
@@ -48,6 +48,7 @@ import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import javax.swing.tree.TreePath;
+import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -80,6 +81,7 @@ public class CopyrightProfilesPanel extends MasterDetailsComponent implements Se
     return "Copyright.UI";
   }
 
+  @Override
   protected void processRemovedItems() {
     Map<String, CopyrightProfile> profiles = getAllProfiles();
     final List<CopyrightProfile> deleted = new ArrayList<CopyrightProfile>();
@@ -93,16 +95,19 @@ public class CopyrightProfilesPanel extends MasterDetailsComponent implements Se
     }
   }
 
+  @Override
   protected boolean wasObjectStored(Object o) {
     return myManager.getCopyrights().contains((CopyrightProfile)o);
   }
 
+  @Override
   @Nls
   public String getDisplayName() {
     return "Copyright Profiles";
   }
 
-  @Nullable
+  @Override
+  @NotNull
   @NonNls
   public String getHelpTopic() {
     return "copyright.profiles";
@@ -114,6 +119,7 @@ public class CopyrightProfilesPanel extends MasterDetailsComponent implements Se
     }
   }
 
+  @Override
   public void apply() throws ConfigurationException {
     final Set<String> profiles = new HashSet<String>();
     for (int i = 0; i < myRoot.getChildCount(); i++) {
@@ -151,6 +157,7 @@ public class CopyrightProfilesPanel extends MasterDetailsComponent implements Se
     myInitialized.set(false);
   }
 
+  @Override
   @Nullable
   protected ArrayList<AnAction> createActions(boolean fromPopup) {
     ArrayList<AnAction> result = new ArrayList<AnAction>();
@@ -159,6 +166,7 @@ public class CopyrightProfilesPanel extends MasterDetailsComponent implements Se
         registerCustomShortcutSet(CommonShortcuts.INSERT, myTree);
       }
 
+      @Override
       public void actionPerformed(AnActionEvent event) {
         final String name = askForProfileName("Create Copyright Profile", "");
         if (name == null) return;
@@ -169,9 +177,10 @@ public class CopyrightProfilesPanel extends MasterDetailsComponent implements Se
     result.add(new MyDeleteAction());
     result.add(new AnAction("Copy", "Copy", PlatformIcons.COPY_ICON) {
       {
-        registerCustomShortcutSet(new CustomShortcutSet(KeyStroke.getKeyStroke(KeyEvent.VK_D, KeyEvent.CTRL_MASK)), myTree);
+        registerCustomShortcutSet(new CustomShortcutSet(KeyStroke.getKeyStroke(KeyEvent.VK_D, InputEvent.CTRL_MASK)), myTree);
       }
 
+      @Override
       public void actionPerformed(AnActionEvent event) {
         final String profileName = askForProfileName("Copy Copyright Profile", "");
         if (profileName == null) return;
@@ -181,12 +190,14 @@ public class CopyrightProfilesPanel extends MasterDetailsComponent implements Se
         addProfileNode(clone);
       }
 
+      @Override
       public void update(AnActionEvent event) {
         super.update(event);
         event.getPresentation().setEnabled(getSelectedObject() != null);
       }
     });
     result.add(new AnAction("Import", "Import", PlatformIcons.IMPORT_ICON) {
+      @Override
       public void actionPerformed(AnActionEvent event) {
         final OpenProjectFileChooserDescriptor descriptor = new OpenProjectFileChooserDescriptor(true) {
           @Override
@@ -207,7 +218,7 @@ public class CopyrightProfilesPanel extends MasterDetailsComponent implements Se
         final VirtualFile file = FileChooser.chooseFile(descriptor, myProject, null);
         if (file == null) return;
 
-        final List<CopyrightProfile> copyrightProfiles = ExternalOptionHelper.loadOptions(VfsUtil.virtualToIoFile(file));
+        final List<CopyrightProfile> copyrightProfiles = ExternalOptionHelper.loadOptions(VfsUtilCore.virtualToIoFile(file));
         if (copyrightProfiles == null) return;
         if (!copyrightProfiles.isEmpty()) {
           if (copyrightProfiles.size() == 1) {
@@ -219,6 +230,7 @@ public class CopyrightProfilesPanel extends MasterDetailsComponent implements Se
                 @Override
                 public PopupStep onChosen(final CopyrightProfile selectedValue, boolean finalChoice) {
                   return doFinalStep(new Runnable() {
+                    @Override
                     public void run() {
                       importProfile(selectedValue);
                     }
@@ -253,10 +265,12 @@ public class CopyrightProfilesPanel extends MasterDetailsComponent implements Se
   @Nullable
   private String askForProfileName(String title, String initialName) {
     return Messages.showInputDialog("New copyright profile name:", title, Messages.getQuestionIcon(), initialName, new InputValidator() {
+      @Override
       public boolean checkInput(String s) {
         return !getAllProfiles().containsKey(s) && s.length() > 0;
       }
 
+      @Override
       public boolean canClose(String s) {
         return checkInput(s);
       }
@@ -289,6 +303,7 @@ public class CopyrightProfilesPanel extends MasterDetailsComponent implements Se
     myInitialized.set(true);
   }
 
+  @Override
   public void reset() {
     reloadTree();
     super.reset();
@@ -301,21 +316,25 @@ public class CopyrightProfilesPanel extends MasterDetailsComponent implements Se
 
   public void addItemsChangeListener(final Runnable runnable) {
     addItemsChangeListener(new ItemsChangeListener() {
+      @Override
       public void itemChanged(@Nullable Object deletedItem) {
         SwingUtilities.invokeLater(runnable);
       }
 
+      @Override
       public void itemsExternallyChanged() {
         SwingUtilities.invokeLater(runnable);
       }
     });
   }
 
+  @Override
   @NotNull
   public String getId() {
     return getHelpTopic();
   }
 
+  @Override
   public Runnable enableSearch(String option) {
     return null;
   }
