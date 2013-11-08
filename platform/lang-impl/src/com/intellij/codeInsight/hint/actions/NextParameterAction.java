773f5f0bb9d71cf24a160a27bb954e0ecc137e16
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2013-11-08 16:26:59 +0400

    IDEA-102454 Parameter Info: TAB/Shift-TAB and editing does not work in language injected fragment

--- platform/lang-impl/src/com/intellij/codeInsight/hint/actions/NextParameterAction.java
+++ platform/lang-impl/src/com/intellij/codeInsight/hint/actions/NextParameterAction.java
@@ -17,12 +17,11 @@
 package com.intellij.codeInsight.hint.actions;
 
 import com.intellij.codeInsight.hint.PrevNextParameterHandler;
-import com.intellij.openapi.editor.actionSystem.EditorAction;
 
 /**
  * @author ven
  */
-public class NextParameterAction extends EditorAction {
+public class NextParameterAction extends InjectionAwareEditorAction {
   public NextParameterAction() {
     super(new PrevNextParameterHandler(true));
   }
