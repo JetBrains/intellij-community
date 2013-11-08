7ea7425612c6e7507f9ef84a6359fdc9f12aa022
Alexander.Shvedov <shvedov89@gmail.com>
2013-11-09 03:26:41 +0400

    Decompose StatementPostfixLookupElement.java

---
+++ src/org/jetbrains/postfixCompletion/LookupItems/PostfixLookupElement.java
@@ -0,0 +1,79 @@
+package org.jetbrains.postfixCompletion.LookupItems;
+
+import com.intellij.codeInsight.completion.*;
+import com.intellij.codeInsight.lookup.*;
+import com.intellij.openapi.application.*;
+import com.intellij.openapi.editor.*;
+import com.intellij.openapi.util.*;
+import com.intellij.psi.*;
+import org.jetbrains.annotations.*;
+import org.jetbrains.postfixCompletion.Infrastructure.*;
+
+import java.util.*;
+
+public abstract class PostfixLookupElement<TPsiElement> extends LookupElement {
+  @NotNull protected final Class<? extends PsiExpression> myExpressionType;
+  @NotNull protected final TextRange myExpressionRange;
+  @NotNull protected final String myLookupString;
+
+  public PostfixLookupElement(@NotNull String lookupString, @NotNull PrefixExpressionContext context) {
+    myExpressionType = context.expression.getClass();
+    myLookupString = lookupString;
+    myExpressionRange = context.expressionRange;
+  }
+
+  @NotNull @Override public final String getLookupString() {
+    return myLookupString;
+  }
+
+  @Override public final boolean isWorthShowingInAutoPopup() {
+    return true; // thx IDEA folks for implementing this!
+  }
+
+  @Override public final Set<String> getAllLookupStrings() {
+    HashSet<String> set = new HashSet<String>();
+
+    // this hack prevents completion list from closing
+    // when whole template name is typed
+    set.add(myLookupString);
+    set.add(myLookupString + " ");
+
+    return set;
+  }
+
+  @Override public void handleInsert(@NotNull InsertionContext context) {
+    Document document = context.getDocument();
+    int startOffset = context.getStartOffset();
+    PsiDocumentManager documentManager = PsiDocumentManager.getInstance(context.getProject());
+
+    // note: use 'postfix' string, to break expression like '0.postfix'
+    document.replaceString(startOffset, context.getTailOffset(), "postfix");
+    context.commitDocument();
+
+    PsiFile file = context.getFile();
+    PsiElement psiElement = file.findElementAt(startOffset);
+    if (psiElement == null) return; // shit happens?
+
+    PostfixTemplatesManager manager =
+      ApplicationManager.getApplication().getComponent(PostfixTemplatesManager.class);
+    PostfixTemplateAcceptanceContext acceptanceContext = manager.isAvailable(psiElement, true);
+    if (acceptanceContext == null) return; // yes, shit happens
+
+    for (PrefixExpressionContext expression : acceptanceContext.expressions) {
+      if (myExpressionType.isInstance(expression.expression) &&
+          expression.expressionRange.equals(myExpressionRange)) {
+
+        TPsiElement newStatement = handlePostfixInsert(context, expression);
+        documentManager.doPostponedOperationsAndUnblockDocument(document);
+        postProcess(context, newStatement);
+        break;
+      }
+    }
+  }
+
+  @NotNull protected abstract TPsiElement handlePostfixInsert(
+    @NotNull InsertionContext context, @NotNull PrefixExpressionContext expressionContext);
+
+  protected abstract void postProcess(
+    @NotNull InsertionContext context, @NotNull TPsiElement element);
+}
--- src/org/jetbrains/postfixCompletion/LookupItems/StatementPostfixLookupElement.java
+++ src/org/jetbrains/postfixCompletion/LookupItems/StatementPostfixLookupElement.java
@@ -1,101 +1,46 @@
 package org.jetbrains.postfixCompletion.LookupItems;
 
 import com.intellij.codeInsight.completion.*;
-import com.intellij.codeInsight.lookup.*;
-import com.intellij.openapi.application.*;
-import com.intellij.openapi.editor.*;
-import com.intellij.openapi.util.*;
+import com.intellij.openapi.project.*;
 import com.intellij.psi.*;
 import org.jetbrains.annotations.*;
 import org.jetbrains.postfixCompletion.Infrastructure.*;
 
-import java.util.*;
-
 public abstract class StatementPostfixLookupElement<TStatement extends PsiStatement>
-  extends LookupElement {
-
-  @NotNull private final Class<? extends PsiExpression> myExpressionType;
-  @NotNull private final TextRange myExpressionRange;
-  @NotNull private final String myLookupString;
+  extends PostfixLookupElement<TStatement> {
 
   public StatementPostfixLookupElement(
     @NotNull String lookupString, @NotNull PrefixExpressionContext context) {
-    myLookupString = lookupString;
-    myExpressionType = context.expression.getClass();
-    myExpressionRange = context.expressionRange;
+    super(lookupString, context);
   }
 
-  @NotNull @Override public final String getLookupString() {
-    return myLookupString;
-  }
+  @Override @NotNull protected TStatement handlePostfixInsert(
+    @NotNull InsertionContext context, @NotNull PrefixExpressionContext expressionContext) {
+    // get facade and factory while all elements are physical and valid
+    Project project = expressionContext.expression.getProject();
+    JavaPsiFacade psiFacade = JavaPsiFacade.getInstance(project);
+    PsiElementFactory psiElementFactory = psiFacade.getElementFactory();
 
-  @Override public final boolean isWorthShowingInAutoPopup() {
-    return true; // thx IDEA folks for implementing this!
-  }
+    // fix up expression before template expansion
+    PrefixExpressionContext fixedContext = expressionContext.fixUp();
 
-  @Override public final Set<String> getAllLookupStrings() {
-    HashSet<String> set = new HashSet<String>();
+    // get target statement to replace
+    PsiStatement targetStatement = fixedContext.getContainingStatement();
+    assert targetStatement != null : "targetStatement != null";
 
-    // this hack prevents completion list from closing
-    // when whole template name is typed
-    set.add(myLookupString);
-    set.add(myLookupString + " ");
+    PsiExpression exprCopy = (PsiExpression) fixedContext.expression.copy();
+    TStatement newStatement = createNewStatement(
+      psiElementFactory, exprCopy, fixedContext.expression);
 
-    return set;
-  }
-
-  @Override public void handleInsert(@NotNull InsertionContext context) {
-    Document document = context.getDocument();
-    int startOffset = context.getStartOffset();
-    PsiDocumentManager documentManager = PsiDocumentManager.getInstance(context.getProject());
-
-    // note: use 'postfix' string, to break expression like '0.postfix'
-    document.replaceString(startOffset, context.getTailOffset(), "postfix");
-    context.commitDocument();
-
-    PsiFile file = context.getFile();
-    PsiElement psiElement = file.findElementAt(startOffset);
-    if (psiElement == null) return; // shit happens?
-
-    PostfixTemplatesManager manager =
-      ApplicationManager.getApplication().getComponent(PostfixTemplatesManager.class);
-    PostfixTemplateAcceptanceContext acceptanceContext = manager.isAvailable(psiElement, true);
-    if (acceptanceContext == null) return; // yes, shit happens
-
-    for (PrefixExpressionContext expression : acceptanceContext.expressions) {
-      PsiExpression expr = expression.expression;
-      if (myExpressionType.isInstance(expr) &&
-          expression.expressionRange.equals(myExpressionRange)) {
-
-        // get facade and factory while all elements are physical and valid
-        JavaPsiFacade psiFacade = JavaPsiFacade.getInstance(expr.getProject());
-        PsiElementFactory psiElementFactory = psiFacade.getElementFactory();
-
-        // fix up expression before template expansion
-        PrefixExpressionContext fixedContext = expression.fixUp();
-
-        // get target statement to replace
-        PsiStatement targetStatement = fixedContext.getContainingStatement();
-        assert targetStatement != null : "targetStatement != null";
-
-        PsiExpression exprCopy = (PsiExpression) fixedContext.expression.copy();
-        TStatement newStatement = createNewStatement(psiElementFactory, exprCopy, file);
-
-        //noinspection unchecked
-        TStatement statement = (TStatement) targetStatement.replace(newStatement);
-
-        documentManager.doPostponedOperationsAndUnblockDocument(document);
-
-        postProcess(context, statement);
-        break;
-      }
-    }
+    //noinspection unchecked
+    return (TStatement) targetStatement.replace(newStatement);
   }
 
   @NotNull protected abstract TStatement createNewStatement(
-    @NotNull PsiElementFactory factory, @NotNull PsiExpression expression, @NotNull PsiFile context);
+    @NotNull PsiElementFactory factory, @NotNull PsiExpression expression, @NotNull PsiElement context);
 
-  protected void postProcess(@NotNull InsertionContext context, @NotNull TStatement statement) {
+  @Override protected void postProcess(
+    @NotNull InsertionContext context, @NotNull TStatement statement) {
     int offset = statement.getTextRange().getEndOffset();
     context.getEditor().getCaretModel().moveToOffset(offset);
   }
