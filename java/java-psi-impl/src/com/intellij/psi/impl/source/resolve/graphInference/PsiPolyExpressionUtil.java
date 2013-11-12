8a5dfada595c21de4943c96b49aed3c9c164a6d0
anna <Anna.Kozlova@jetbrains.com>
2013-11-12 20:14:09 +0100

    new inference: accept methods in lambda return statements as standalone when they do not depend on lambda params

--- java/java-psi-impl/src/com/intellij/psi/impl/source/resolve/graphInference/PsiPolyExpressionUtil.java
+++ java/java-psi-impl/src/com/intellij/psi/impl/source/resolve/graphInference/PsiPolyExpressionUtil.java
@@ -59,9 +59,7 @@ public class PsiPolyExpressionUtil {
         }
       }
     } else if (expression instanceof PsiMethodCallExpression) {
-      final PsiLambdaExpression lambdaExpression = PsiTreeUtil.getParentOfType(expression, PsiLambdaExpression.class);
-      final boolean isLambdaReturnStmt = lambdaExpression != null && LambdaUtil.getReturnExpressions(lambdaExpression).contains(expression);
-      return isMethodCallPolyExpression(expression, isLambdaReturnStmt ? null : ((PsiMethodCallExpression)expression).resolveMethod());
+      return isMethodCallPolyExpression(expression, dependsOnLambdaParams(expression) ? null : ((PsiMethodCallExpression)expression).resolveMethod());
     }
     else if (expression instanceof PsiConditionalExpression) {
       final ConditionalKind conditionalKind = isBooleanOrNumeric(expression);
@@ -70,6 +68,33 @@ public class PsiPolyExpressionUtil {
       }
     }
     return false;
+  }
+
+  private static boolean dependsOnLambdaParams(PsiExpression expression) {
+    final PsiLambdaExpression lambdaExpression = PsiTreeUtil.getParentOfType(expression, PsiLambdaExpression.class);
+    boolean isLambdaReturnStmt = lambdaExpression != null && LambdaUtil.getReturnExpressions(lambdaExpression).contains(expression);
+    if (isLambdaReturnStmt) {
+      final boolean [] dependsOnParams = new boolean[] {false};
+      expression.accept(new JavaRecursiveElementWalkingVisitor() {
+        @Override
+        public void visitElement(PsiElement element) {
+          if (dependsOnParams[0]) return;
+          super.visitElement(element);
+        }
+
+        @Override
+        public void visitReferenceExpression(PsiReferenceExpression expression) {
+          if (dependsOnParams[0]) return;
+          super.visitReferenceExpression(expression);
+          final PsiElement resolve = expression.resolve();
+          if (resolve instanceof PsiParameter && ((PsiParameter)resolve).getDeclarationScope() == lambdaExpression) {
+            dependsOnParams[0] = true;
+          }
+        }
+      });
+      isLambdaReturnStmt = dependsOnParams[0];
+    }
+    return isLambdaReturnStmt;
   }
 
   public static boolean isMethodCallPolyExpression(PsiExpression expression, final PsiMethod method) {
