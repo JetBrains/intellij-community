1baa4fa4e7024d8ef7ca47cfc64e3efcb5ce8505
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2013-11-13 21:53:30 +0400

    IDEA-113730 Reverted CmdContentClient implementation back - as export could not be used to get contents of scheduled for deletion file

--- plugins/svn4idea/src/org/jetbrains/idea/svn/content/CmdContentClient.java
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/content/CmdContentClient.java
@@ -1,19 +1,17 @@
 package org.jetbrains.idea.svn.content;
 
-import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vcs.impl.ContentRevisionCache;
+import com.intellij.openapi.vfs.CharsetToolkit;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.idea.svn.api.BaseSvnClient;
 import org.jetbrains.idea.svn.commandLine.*;
-import org.tmatesoft.svn.core.SVNDepth;
-import org.tmatesoft.svn.core.internal.util.SVNPathUtil;
 import org.tmatesoft.svn.core.wc.SVNRevision;
 import org.tmatesoft.svn.core.wc2.SvnTarget;
 
-import java.io.File;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Konstantin Kolosovsky.
@@ -24,37 +22,20 @@ public class CmdContentClient extends BaseSvnClient implements ContentClient {
   public byte[] getContent(@NotNull SvnTarget target, @Nullable SVNRevision revision, @Nullable SVNRevision pegRevision)
     throws VcsException, FileTooBigRuntimeException {
     // TODO: rewrite this to provide output as Stream
+    // TODO: rewrite without conversion from String to byte[]
     // TODO: Also implement max size constraint like in SvnKitContentClient
-    // TODO: As alternative implementation we could indicate in Command that it requires binary output and create special
-    // TODO: ProcessHandler that will read process input stream as binary
+    // TODO: Could not use export to get content of scheduled for deletion file - use cat command, but write special binary handler
 
-    SvnTarget from = resolveTarget(target, pegRevision);
-    File temp = null;
-    byte[] bytes;
-    try {
-      temp = FileUtil.createTempDirectory("svn_content", null);
-      myFactory.createExportClient().export(from, temp, revision, SVNDepth.EMPTY, null, false, false, null);
-      bytes = FileUtil.loadFileBytes(new File(temp, SVNPathUtil.tail(getPath(from))));
-    } catch (IOException e) {
-      throw new SvnBindException(e);
-    } finally {
-      if (temp != null) {
-        FileUtil.delete(temp);
-      }
-    }
+    List<String> parameters = new ArrayList<String>();
+    CommandUtil.put(parameters, target.getPathOrUrlString(), pegRevision);
+    CommandUtil.put(parameters, revision);
+
+    CommandExecutor command = CommandUtil.execute(myVcs, target, SvnCommandName.cat, parameters, null);
+
+    byte[] bytes = CharsetToolkit.getUtf8Bytes(command.getOutput());
 
     ContentRevisionCache.checkContentsSize(target.getPathOrUrlString(), bytes.length);
 
     return bytes;
-  }
-
-  @NotNull
-  private static String getPath(@NotNull SvnTarget target) {
-    return (target.isFile() ? target.getFile().getAbsolutePath() : target.getURL().toDecodedString()).replace("\\", "/");
-  }
-
-  @NotNull
-  private static SvnTarget resolveTarget(@NotNull SvnTarget target, @Nullable SVNRevision pegRevision) throws SvnBindException {
-    return target.isFile() ? SvnTarget.fromFile(target.getFile(), pegRevision) : SvnTarget.fromURL(target.getURL(), pegRevision);
   }
 }
