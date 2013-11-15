JetBrains/intellij-community

commit 6aa542e980b6a06ef37c3fe5631a0f38a1dc54e6

[vcs] Fix Add to VCS for Git
* Remove GitAdd action since it duplicates the functionality of
  ScheduleForAdditionAction & CheckinEnvironment#scheduleForAddition.
* Let ScheduleForAdditionAction be enabled if user selects a directory,
  and the VCS doesn't support directory versioning. That would let to
  add all files in a directory recursively for Git and similar VCSs.
* Since directories may now be presented in the data passed from
  ScheduleForAdditionAction to the ChangeListManager,
  don't add everything that is found there: check for unversioned
  files.
* Add a test on 'git add'.

This fixes IDEA-104070 & IDEA-76670, because all files are now
handled individually, and hidden or ignored files simply don't count.
This also fixes IDEA-98689, because the common action is used now,
and its shortcut can be reassigned.

platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ChangeListManagerImpl.java
@@ -1136,7 +1136,9 @@ public class ChangeListManagerImpl extends ChangeListManagerEx implements Projec
             final Processor<VirtualFile> addProcessor = new Processor<VirtualFile>() {
               @Override
               public boolean process(VirtualFile file) {
-                descendant.add(file);
+                if (getStatus(file) == FileStatus.UNKNOWN) {
+                  descendant.add(file);
+                }
                 return true;
               }
             };

platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/ScheduleForAdditionAction.java
@@ -28,8 +28,10 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vcs.AbstractVcs;
 import com.intellij.openapi.vcs.FileStatus;
 import com.intellij.openapi.vcs.FileStatusManager;
+import com.intellij.openapi.vcs.ProjectLevelVcsManager;
 import com.intellij.openapi.vcs.changes.ChangeListManagerImpl;
 import com.intellij.openapi.vcs.changes.ui.ChangesListView;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -85,7 +87,9 @@ public class ScheduleForAdditionAction extends AnAction implements DumbAware {
       return unversionedFiles;
     }
     for (VirtualFile file : files) {
-      if (file != null && FileStatusManager.getInstance(project).getStatus(file) == FileStatus.UNKNOWN) {
+      AbstractVcs vcs = ProjectLevelVcsManager.getInstance(project).getVcsFor(file);
+      if (vcs != null && !vcs.areDirectoriesVersionedItems() && file.isDirectory() ||
+          FileStatusManager.getInstance(project).getStatus(file) == FileStatus.UNKNOWN) {
         unversionedFiles.add(file);
       }
     }

plugins/git4idea/src/META-INF/plugin.xml
@@ -19,8 +19,8 @@
     </action>
 
     <group id="GitFileActions">
-      <action id="Git.Add" class="git4idea.actions.GitAdd" text="Add" icon="AllIcons.General.Add" use-shortcut-of="ChangesView.AddUnversioned" />
       <reference ref="CheckinFiles"/>
+      <reference ref="ChangesView.AddUnversioned"/>
       <separator/>
       <reference ref="Annotate"/>
       <reference ref="Show.Current.Revision"/>

plugins/git4idea/src/git4idea/actions/GitAdd.java
@@ -1,93 +1,0 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package git4idea.actions;
-
-import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.FileStatus;
-import com.intellij.openapi.vcs.FileStatusManager;
-import com.intellij.openapi.vcs.ProjectLevelVcsManager;
-import com.intellij.openapi.vcs.VcsException;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.Consumer;
-import git4idea.GitUtil;
-import git4idea.GitVcs;
-import git4idea.util.GitFileUtils;
-import git4idea.i18n.GitBundle;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-
-/**
- * Git "add" action
- */
-public class GitAdd extends BasicAction {
-
-  @Override
-  public boolean perform(@NotNull final Project project,
-                         final GitVcs vcs,
-                         @NotNull final List<VcsException> exceptions,
-                         @NotNull final VirtualFile[] affectedFiles) {
-    saveAll();
-    if (!ProjectLevelVcsManager.getInstance(project).checkAllFilesAreUnder(GitVcs.getInstance(project), affectedFiles)) return false;
-    return toBackground(project, vcs, affectedFiles, exceptions, new Consumer<ProgressIndicator>() {
-      public void consume(ProgressIndicator indicator) {
-        try {
-          addFiles(project, affectedFiles, indicator);
-        }
-        catch (VcsException e) {
-          exceptions.add(e);
-        }
-      }
-    });
-
-  }
-
-  /**
-   * Add the specified files to the project.
-   *
-   * @param project The project to add files to
-   * @param files   The files to add  @throws VcsException If an error occurs
-   * @param pi      progress indicator
-   */
-  public static void addFiles(@NotNull final Project project, @NotNull final VirtualFile[] files, ProgressIndicator pi)
-    throws VcsException {
-    final Map<VirtualFile, List<VirtualFile>> roots = GitUtil.sortFilesByGitRoot(Arrays.asList(files));
-    for (Map.Entry<VirtualFile, List<VirtualFile>> entry : roots.entrySet()) {
-      pi.setText(entry.getKey().getPresentableUrl());
-      GitFileUtils.addFiles(project, entry.getKey(), entry.getValue());
-    }
-  }
-
-  @Override
-  @NotNull
-  protected String getActionName() {
-    return GitBundle.getString("add.action.name");
-  }
-
-  @Override
-  protected boolean isEnabled(@NotNull Project project, @NotNull GitVcs vcs, @NotNull VirtualFile... vFiles) {
-    for (VirtualFile file : vFiles) {
-      FileStatus fileStatus = FileStatusManager.getInstance(project).getStatus(file);
-      if (file.isDirectory() || (fileStatus != FileStatus.NOT_CHANGED && fileStatus != FileStatus.DELETED && fileStatus != FileStatus.ADDED)) {
-        return true;
-      }
-    }
-    return false;
-  }
-}

plugins/git4idea/test-features/git4idea/git-add.feature
@@ -1,0 +1,35 @@
+Feature: Git Add
+  As a happy Git integration user,
+  I want to be able to add files to the VCS.
+
+  Scenario: Add one simple file
+    Given unversioned file unv.txt
+    When I add unv.txt to VCS
+    Then unv.txt should become ADDED
+
+  Scenario: Add a directory
+    Given unversioned file dir/unv.txt
+    When I add dir to VCS
+    Then dir/unv.txt should become ADDED
+
+  @nestedroot
+  Scenario: Add file from different roots
+    Given unversioned file unv.txt
+    And unversioned file community/com.txt
+    When I add unv.txt, community/com.txt to VCS
+    Then unv.txt should become ADDED
+    And community/com.txt should become ADDED
+
+  @nestedroot
+  Scenario: Add a file from subroot by calling "add" on the upper root
+    Given unversioned file community/com.txt
+    When I add the project dir to VCS
+    Then community/com.txt should become ADDED
+
+  @nestedroot
+  Scenario: Add a file from nested root and the directory above the nested root
+    Given unversioned file community/com.txt
+    And unversioned file community/unv.txt
+    When I add the project dir, community/com.txt to VCS
+    Then community/com.txt should become ADDED
+    And community/unv.txt should become ADDED

plugins/git4idea/test-stepdefs/git4idea/GitAddSteps.java
@@ -1,0 +1,80 @@
+/*
+ * Copyright 2000-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package git4idea;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.testFramework.VfsTestUtil;
+import cucumber.annotation.en.Given;
+import cucumber.annotation.en.Then;
+import cucumber.annotation.en.When;
+import git4idea.repo.GitRepository;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static git4idea.GitCucumberWorld.*;
+import static git4idea.test.GitExecutor.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Kirill Likhodedov
+ */
+public class GitAddSteps {
+
+  @Given("^unversioned file (.*)$")
+  public void unversioned_file(String filePath) throws Throwable {
+    cd(myRepository);
+    VfsTestUtil.createFile(myProjectDir, filePath);
+  }
+
+  @When("^I add (.*) to VCS$")
+  public void I_add_file_to_VCS(List<String> files) throws Throwable {
+    List<VirtualFile> vFiles = Lists.transform(files, new Function<String, VirtualFile>() {
+      @Override
+      public VirtualFile apply(@Nullable String file) {
+        assertNotNull(file);
+        if ("the project dir".equals(file)) {
+          return myRepository.getRoot();
+        }
+        return myRepository.getRoot().findFileByRelativePath(file);
+      }
+    });
+    final CountDownLatch latch = new CountDownLatch(1);
+    //executeOnPooledThread(new Runnable() {
+    //  public void run() {
+    //    myChangeListManager.ensureUpToDate(false);
+    //    latch.countDown();
+    //  }
+    //});
+    latch.await(5, TimeUnit.SECONDS);
+    myChangeListManager.addUnversionedFiles(myChangeListManager.addChangeList("dummy", null), vFiles);
+  }
+
+
+  @Then("^(.*) should become ADDED$")
+  public void file_should_become_ADDED(String filePath) throws Throwable {
+    VirtualFile vf = myProjectDir.findFileByRelativePath(filePath);
+    assertNotNull(vf);
+    GitRepository repo = myPlatformFacade.getRepositoryManager(myProject).getRepositoryForFile(vf);
+    String status = git(repo, "status --porcelain " + vf.getPath());
+    assertTrue("File status is not added: " + status, 'A' == status.charAt(0));
+  }
+}

plugins/git4idea/test-stepdefs/git4idea/GitCucumberWorld.java
@@ -10,6 +10,7 @@ import com.intellij.openapi.vcs.AbstractVcs;
 import com.intellij.openapi.vcs.AbstractVcsHelper;
 import com.intellij.openapi.vcs.ProjectLevelVcsManager;
 import com.intellij.openapi.vcs.changes.ChangeListManager;
+import com.intellij.openapi.vcs.changes.ChangeListManagerImpl;
 import com.intellij.openapi.vcs.changes.VcsDirtyScopeManager;
 import com.intellij.openapi.vcs.impl.ProjectLevelVcsManagerImpl;
 import com.intellij.openapi.vfs.LocalFileSystem;
@@ -49,6 +50,7 @@ import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static com.intellij.dvcs.test.Executor.cd;
+import static com.intellij.dvcs.test.Executor.mkdir;
 import static junit.framework.Assert.assertNotNull;
 import static org.junit.Assume.assumeTrue;
 
@@ -62,13 +64,14 @@ public class GitCucumberWorld {
 
   public static String myTestRoot;
   public static String myProjectRoot;
+  public static VirtualFile myProjectDir;
   public static Project myProject;
 
   public static GitPlatformFacade myPlatformFacade;
   public static Git myGit;
   public static GitRepository myRepository;
   public static GitVcsSettings mySettings;
-  public static ChangeListManager myChangeListManager;
+  public static ChangeListManagerImpl myChangeListManager;
   public static GitVcs myVcs;
 
   public static MockVcsHelper myVcsHelper;
@@ -104,6 +107,7 @@ public class GitCucumberWorld {
     ((ProjectComponent)VcsDirtyScopeManager.getInstance(myProject)).projectOpened();
 
     myProjectRoot = myProject.getBasePath();
+    myProjectDir = myProject.getBaseDir();
     myTestRoot = myProjectRoot;
 
     myPlatformFacade = ServiceManager.getService(myProject, GitPlatformFacade.class);
@@ -114,7 +118,7 @@ public class GitCucumberWorld {
     // dynamic overriding is used instead of making it in plugin.xml,
     // because MockVcsHelper is not ready to be a full featured implementation for all tests.
     myVcsHelper = overrideService(myProject, AbstractVcsHelper.class, MockVcsHelper.class);
-    myChangeListManager = myPlatformFacade.getChangeListManager(myProject);
+    myChangeListManager = (ChangeListManagerImpl)myPlatformFacade.getChangeListManager(myProject);
     myNotificator = (TestNotificator)ServiceManager.getService(myProject, Notificator.class);
     myVcs = GitVcs.getInstance(myProject);
 
@@ -153,6 +157,14 @@ public class GitCucumberWorld {
     // default port will be occupied by main idea instance => define the custom default to avoid searching of free port
     System.setProperty(WebServerManagerImpl.PROPERTY_RPC_PORT, "64463");
     myHttpAuthService = (GitHttpAuthTestService)ServiceManager.getService(GitHttpAuthService.class);
+  }
+
+  @Before("@nestedroot")
+  @Order(2)
+  public void setUpStandardMultipleRootsConfig() {
+    cd(myProjectRoot);
+    String community = mkdir("community");
+    createRepo(community);
   }
 
   @After("@remote")

commit 645e5c2d02195f9801c892c24be24f8bffda2bfd

[git] IDEA-109511 Quick-fix of "Git Add" for tracked files.
The problem was introduced in 6aa542e: the common
ScheduleForAdditionAction doesn't know that there are VCSs which can
ADD not-unversioned files.

Correct fix would be to introduce some API which plugins could use to
specify if the file of given status can be added or not.
But the timing doesn't allow to do it now.

Therefore partly return to a separate GitAdd action, but this time
extend ScheduleForAdditionAction to follow the common process to add
files.
Let specify a function determining if the file status is suitable for
calling "add to vcs" on it.

This fix keeps IDEA-76670 & IDEA-104070 fixed, but reopens IDEA-98689.

platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ChangeListManagerImpl.java
@@ -1115,7 +1115,22 @@ public class ChangeListManagerImpl extends ChangeListManagerEx implements Projec
     myChangesViewManager.scheduleRefresh();
   }
 
+  @Override
   public void addUnversionedFiles(final LocalChangeList list, @NotNull final List<VirtualFile> files) {
+    addUnversionedFiles(list, files, new BooleanFunction<FileStatus>() {
+      @Override
+      public boolean fun(FileStatus status) {
+        return status == FileStatus.UNKNOWN;
+      }
+    });
+  }
+
+  // TODO this is for quick-fix for GitAdd problem. To be removed after proper fix
+  // (which should introduce something like VcsAddRemoveEnvironment)
+  @Deprecated
+  public void addUnversionedFiles(final LocalChangeList list, @NotNull final List<VirtualFile> files,
+                                  final BooleanFunction<FileStatus> statusChecker) {
+
     final List<VcsException> exceptions = new ArrayList<VcsException>();
     final Set<VirtualFile> allProcessedFiles = new HashSet<VirtualFile>();
     ChangesUtil.processVirtualFilesByVcs(myProject, files, new ChangesUtil.PerVcsProcessor<VirtualFile>() {
@@ -1127,7 +1142,7 @@ public class ChangeListManagerImpl extends ChangeListManagerEx implements Projec
             final Processor<VirtualFile> addProcessor = new Processor<VirtualFile>() {
               @Override
               public boolean process(VirtualFile file) {
-                if (getStatus(file) == FileStatus.UNKNOWN) {
+                if (statusChecker.fun(getStatus(file))) {
                   descendant.add(file);
                 }
                 return true;

platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/ScheduleForAdditionAction.java
@@ -26,7 +26,6 @@ import com.intellij.openapi.actionSystem.ActionPlaces;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vcs.AbstractVcs;
@@ -36,6 +35,7 @@ import com.intellij.openapi.vcs.ProjectLevelVcsManager;
 import com.intellij.openapi.vcs.changes.ChangeListManagerImpl;
 import com.intellij.openapi.vcs.changes.ui.ChangesListView;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.util.BooleanFunction;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -60,10 +60,15 @@ public class ScheduleForAdditionAction extends AnAction implements DumbAware {
       return;
     }
     final ChangeListManagerImpl changeListManager = ChangeListManagerImpl.getInstanceImpl(e.getData(CommonDataKeys.PROJECT));
-    changeListManager.addUnversionedFiles(changeListManager.getDefaultChangeList(), unversionedFiles);
+    changeListManager.addUnversionedFiles(changeListManager.getDefaultChangeList(), unversionedFiles, new BooleanFunction<FileStatus>() {
+      @Override
+      public boolean fun(FileStatus status) {
+        return isStatusForAddition(status);
+      }
+    });
   }
 
-  private static boolean thereAreUnversionedFiles(AnActionEvent e) {
+  private boolean thereAreUnversionedFiles(AnActionEvent e) {
     List<VirtualFile> unversionedFiles = getFromChangesView(e);
     if (unversionedFiles != null && !unversionedFiles.isEmpty()) {
       return true;
@@ -84,7 +89,7 @@ public class ScheduleForAdditionAction extends AnAction implements DumbAware {
   }
 
   @NotNull
-  private static List<VirtualFile> getUnversionedFiles(final AnActionEvent e) {
+  private List<VirtualFile> getUnversionedFiles(final AnActionEvent e) {
     List<VirtualFile> unversionedFiles = getFromChangesView(e);
     if (unversionedFiles != null && !unversionedFiles.isEmpty()) {
       return unversionedFiles;
@@ -106,11 +111,15 @@ public class ScheduleForAdditionAction extends AnAction implements DumbAware {
     return unversionedFiles;
   }
 
-  private static boolean isFileUnversioned(@NotNull VirtualFile file,
-                                           @NotNull ProjectLevelVcsManager vcsManager, @NotNull FileStatusManager fileStatusManager) {
+  private boolean isFileUnversioned(@NotNull VirtualFile file,
+                                    @NotNull ProjectLevelVcsManager vcsManager, @NotNull FileStatusManager fileStatusManager) {
     AbstractVcs vcs = vcsManager.getVcsFor(file);
     return vcs != null && !vcs.areDirectoriesVersionedItems() && file.isDirectory() ||
-           fileStatusManager.getStatus(file) == FileStatus.UNKNOWN;
+           isStatusForAddition(fileStatusManager.getStatus(file));
+  }
+
+  protected boolean isStatusForAddition(FileStatus status) {
+    return status == FileStatus.UNKNOWN;
   }
 
   @Nullable

plugins/git4idea/src/META-INF/plugin.xml
@@ -20,7 +20,7 @@
 
     <group id="GitFileActions">
       <reference ref="CheckinFiles"/>
-      <reference ref="ChangesView.AddUnversioned"/>
+      <action id="Git.Add" class="git4idea.actions.GitAdd" text="Add" icon="AllIcons.General.Add" use-shortcut-of="ChangesView.AddUnversioned" />
       <separator/>
       <reference ref="Annotate"/>
       <reference ref="Show.Current.Revision"/>

plugins/git4idea/src/git4idea/actions/GitAdd.java
@@ -1,0 +1,28 @@
+/*
+ * Copyright 2000-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package git4idea.actions;
+
+import com.intellij.openapi.vcs.FileStatus;
+import com.intellij.openapi.vcs.changes.actions.ScheduleForAdditionAction;
+
+public class GitAdd extends ScheduleForAdditionAction {
+
+  @Override
+  protected boolean isStatusForAddition(FileStatus status) {
+    return status == FileStatus.UNKNOWN || status == FileStatus.MODIFIED || status == FileStatus.MERGED_WITH_CONFLICTS;
+  }
+
+}