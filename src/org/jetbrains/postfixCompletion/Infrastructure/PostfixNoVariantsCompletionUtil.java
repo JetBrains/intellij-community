bcfc005400e490443a8c62f160db4b7489bed42e
Alexander.Shvedov <shvedov89@gmail.com>
2013-11-18 02:51:37 +0400

    Relocate copy&paste...

--- src/org/jetbrains/postfixCompletion/Infrastructure/PostfixNoVariantsCompletionUtil.java
+++ src/org/jetbrains/postfixCompletion/Infrastructure/PostfixNoVariantsCompletionUtil.java
@@ -21,23 +21,16 @@ import java.util.LinkedHashSet;
 // todo: fix 'scn.nn' prefix matching
 
 public abstract class PostfixNoVariantsCompletionUtil {
-  public static void addCompletions(
-    @NotNull CompletionParameters parameters, @NotNull CompletionResultSet resultSet,
-    @NotNull PostfixExecutionContext executionContext) {
-
-
-
-  }
-
-  public static List<LookupElement> addCompletions2(
+  @NotNull private static List<LookupElement> addCompletions(
     @NotNull CompletionParameters parameters, @NotNull PostfixExecutionContext executionContext,
     final PsiReferenceExpression mockExpression) {
+
+
 
     Application application = ApplicationManager.getApplication();
     PostfixTemplatesManager manager = application.getComponent(PostfixTemplatesManager.class);
 
     PsiElement positionElement = parameters.getPosition();
-
     PsiElement reference = positionElement.getParent();
 
     PostfixTemplateContext acceptanceContext = new PostfixTemplateContext(
@@ -85,7 +78,63 @@ public abstract class PostfixNoVariantsCompletionUtil {
     return Collections.emptyList();
   }
 
-  @NotNull public static Set<LookupElement> suggestQualifierItems(
+  public static void suggestChainedCalls(
+    @NotNull CompletionParameters parameters, @NotNull CompletionResultSet result) {
+
+    PsiElement position = parameters.getPosition(), parent = position.getParent();
+    if ((!(parent instanceof PsiJavaCodeReferenceElement))) return;
+
+    PsiElement qualifier = ((PsiJavaCodeReferenceElement) parent).getQualifier();
+    if (!(qualifier instanceof PsiJavaCodeReferenceElement)) return;
+
+    PsiJavaCodeReferenceElement qualifierReference = (PsiJavaCodeReferenceElement) qualifier;
+    if (qualifierReference.isQualified()) return;
+
+    PsiElement target = qualifierReference.resolve();
+    if (target != null && !(target instanceof PsiPackage)) return;
+
+    PsiFile file = position.getContainingFile();
+    if (file instanceof PsiJavaCodeReferenceCodeFragment) return;
+
+    // "prefix."
+    String fullPrefix = parent.getText().substring(
+      0, parameters.getOffset() - parent.getTextRange().getStartOffset());
+
+    CompletionResultSet qualifiedCollector = result.withPrefixMatcher(fullPrefix);
+
+
+    for (LookupElement base : PostfixNoVariantsCompletionUtil.suggestQualifierItems(parameters, qualifierReference)) {
+
+      PsiType type = JavaCompletionUtil.getLookupElementType(base);
+      if (type != null && !PsiType.VOID.equals(type)) {
+        PsiReferenceExpression ref = ReferenceExpressionCompletionContributor.createMockReference(position, type, base);
+        if (ref != null) {
+          // todo:
+
+          List<LookupElement> elements = addCompletions(
+            parameters, new PostfixExecutionContext(false, "postfix"), ref);
+
+
+
+          for (LookupElement postfixElement : elements) {
+
+            JavaChainLookupElement chainedPostfix =
+              new JavaChainLookupElement(base, postfixElement) {
+                @Override public PsiType getType() {
+                  return null;
+                }
+              };
+
+            qualifiedCollector.addElement(chainedPostfix);
+          }
+
+          // TODO: create chain here
+        }
+      }
+    }
+  }
+
+  @NotNull private static Set<LookupElement> suggestQualifierItems(
     @NotNull CompletionParameters parameters, @NotNull PsiJavaCodeReferenceElement qualifier) {
 
     String referenceName = qualifier.getReferenceName();
--- src/org/jetbrains/postfixCompletion/PostfixCompletionContributor.java
+++ src/org/jetbrains/postfixCompletion/PostfixCompletionContributor.java
@@ -2,7 +2,6 @@ package org.jetbrains.postfixCompletion;
 
 import com.intellij.codeInsight.completion.*;
 import com.intellij.codeInsight.lookup.*;
-import com.intellij.psi.*;
 import org.jetbrains.annotations.*;
 import org.jetbrains.postfixCompletion.Infrastructure.*;
 
@@ -42,70 +41,13 @@ public final class PostfixCompletionContributor extends CompletionContributor {
       result.addElement(element);
     }
 
-    suggestChainedCalls(parameters, result);
-  }
-
-
-  private static void suggestChainedCalls(
-      @NotNull CompletionParameters parameters, @NotNull CompletionResultSet result) {
-
-    PsiElement position = parameters.getPosition(), parent = position.getParent();
-    if ((!(parent instanceof PsiJavaCodeReferenceElement))) return;
-
-    PsiElement qualifier = ((PsiJavaCodeReferenceElement) parent).getQualifier();
-    if (!(qualifier instanceof PsiJavaCodeReferenceElement)) return;
-
-    PsiJavaCodeReferenceElement qualifierReference = (PsiJavaCodeReferenceElement) qualifier;
-    if (qualifierReference.isQualified()) return;
-
-    PsiElement target = qualifierReference.resolve();
-    if (target != null && !(target instanceof PsiPackage)) return;
-
-    PsiFile file = position.getContainingFile();
-    if (file instanceof PsiJavaCodeReferenceCodeFragment) return;
-
-    // "prefix."
-    String fullPrefix = parent.getText().substring(
-      0, parameters.getOffset() - parent.getTextRange().getStartOffset());
-
-    CompletionResultSet qualifiedCollector = result.withPrefixMatcher(fullPrefix);
-
-
-    for (LookupElement base : PostfixNoVariantsCompletionUtil.suggestQualifierItems(parameters, qualifierReference)) {
-
-      PsiType type = JavaCompletionUtil.getLookupElementType(base);
-      if (type != null && !PsiType.VOID.equals(type)) {
-        PsiReferenceExpression ref = ReferenceExpressionCompletionContributor.createMockReference(position, type, base);
-        if (ref != null) {
-          // todo:
-
-          List<LookupElement> elements = PostfixNoVariantsCompletionUtil.addCompletions2(
-            parameters, new PostfixExecutionContext(false, "postfix"), ref);
-
-
-
-          for (LookupElement postfixElement : elements) {
-
-            JavaChainLookupElement chainedPostfix =
-              new JavaChainLookupElement(base, postfixElement) {
-                @Override public PsiType getType() {
-                  return null;
-                }
-              };
-
-            qualifiedCollector.addElement(chainedPostfix);
-
-            //PrefixMatcher prefixMatcher = completionResult.getPrefixMatcher();
-            //if (!prefixMatcher.prefixMatches(chainedPostfix)) continue;
-            //CompletionResult postfixResult = completionResult.withLookupElement(chainedPostfix);
-            //result.passResult(postfixResult);
-          }
-
-          // TODO: create chain here
-        }
-      }
+    if (results.isEmpty()) {
+      PostfixNoVariantsCompletionUtil.suggestChainedCalls(parameters, result);
     }
   }
+
+
+
 
 
 }
