62cbb8c70afdaa151dc7327bbc2b064cf0c1d823
Yann Cébron <yann.cebron@jetbrains.com>
2013-11-19 16:28:25 +0100

    FinderRecursivePanel*Test: split

--- platform/platform-tests/testSrc/com/intellij/ui/FinderRecursivePanelSelectionUpdateTest.java
+++ platform/platform-tests/testSrc/com/intellij/ui/FinderRecursivePanelSelectionUpdateTest.java
@@ -27,70 +27,9 @@ import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
 
-public class FinderRecursivePanelTest extends PlatformTestCase {
+public class FinderRecursivePanelSelectionUpdateTest extends PlatformTestCase {
 
-  public void testListModelMerge1() {
-    assertMerge(new String[]{"a", "b", "c", "d"}, 0, 0, "a");
-  }
-
-  public void testListModelMerge2() {
-    assertMerge(new String[0], "a", "b", "c", "d");
-  }
-
-  public void testListModelMerge3() {
-    assertMerge(new String[]{"a", "b", "c", "d"}, 0, -1 /* nothing */);
-  }
-
-  public void testListModelMerge4() {
-    assertMerge(new String[]{"a", "b", "c", "d"}, 2, 2, "a", "b", "c", "d");
-  }
-
-  public void _testListModelMerge5() {
-    assertMerge(new String[]{"a", "b", "c", "d"}, "d", "c", "b", "a");
-  }
-
-  public void testListModelMerge6() {
-    assertMerge(new String[]{"a", "b", "c", "d"}, 2, 1, "b", "c");
-  }
-
-  public void testListModelMerge7() {
-    assertMerge(new String[]{"a", "b", "c", "d"}, 1, 0, "b", "c", "d", "e");
-  }
-
-  public void _testListModelMerge8() {
-    assertMerge(new String[]{"a", "a", "b", "b"}, "b", "a", "b", "a");
-  }
-
-  public void testListModelMerge9() {
-    assertMerge(new String[]{"a", "b"}, 1, 3, "e", "d", "a", "b");
-  }
-
-  public void testListModelMerge10() {
-    assertMerge(new String[]{"a", "b"}, 0, -1, "c", "d");
-  }
-
-  private static void assertMerge(String[] initialItems,
-                                  int initialSelectionIdx,
-                                  int selectionIndexAfterMerge,
-                                  String... itemsToMerge) {
-    CollectionListModel<String> model = new CollectionListModel<String>();
-    model.add(Arrays.asList(initialItems));
-    JBList list = new JBList(model);
-    list.setSelectedIndex(initialSelectionIdx);
-
-    FinderRecursivePanel.mergeListItems(model, Arrays.asList(itemsToMerge));
-    assertEquals(itemsToMerge.length, model.getSize());
-    for (int i = 0; i < itemsToMerge.length; i++) {
-      assertEquals("idx:" + i + " " + toString(model.getItems(), ","), itemsToMerge[i], model.getElementAt(i));
-    }
-    assertEquals(toString(model.getItems(), ","), selectionIndexAfterMerge, list.getSelectedIndex());
-  }
-
-  private static void assertMerge(String[] items, String... newItems) {
-    assertMerge(items, -1, -1, newItems);
-  }
-
-  @Bombed(year = 2013, month = Calendar.DECEMBER, day = 01, user = "Yann Cebron")
+  @Bombed(year = 2013, month = Calendar.DECEMBER, day = 1, user = "Yann Cebron")
   public void testUpdate() throws InterruptedException {
     StringFinderRecursivePanel panel_0 = new StringFinderRecursivePanel(getProject()) {
       @NotNull
