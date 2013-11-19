5d4caa2ca7624e6b8ab1eb8fdbd4d82836558e6e
Alexander.Shvedov <shvedov89@gmail.com>
2013-11-20 01:53:11 +0400

    [Tab]-expansion, refactor

--- src/org/jetbrains/postfixCompletion/ExpandPostfixEditorActionHandler.java
+++ src/org/jetbrains/postfixCompletion/ExpandPostfixEditorActionHandler.java
@@ -13,129 +13,112 @@ import com.intellij.psi.util.*;
 import org.jetbrains.annotations.*;
 import org.jetbrains.postfixCompletion.Infrastructure.*;
 
-import java.util.*;
-
 public final class ExpandPostfixEditorActionHandler extends EditorActionHandler {
-  @NotNull private final EditorActionHandler myUnderlying;
+  @NotNull private final EditorActionHandler myUnderlyingHandler;
   @NotNull private final PostfixTemplatesManager myTemplatesManager;
 
   public ExpandPostfixEditorActionHandler(
-      @NotNull EditorActionHandler underlying, @NotNull PostfixTemplatesManager templatesManager) {
+      @NotNull EditorActionHandler underlyingHandler, @NotNull PostfixTemplatesManager templatesManager) {
     myTemplatesManager = templatesManager;
-    myUnderlying = underlying;
+    myUnderlyingHandler = underlyingHandler;
   }
 
-  @Override public boolean isEnabled(Editor editor, DataContext dataContext) {
+  @Override public boolean isEnabled(@NotNull Editor editor, @NotNull DataContext dataContext) {
+    if (findPostfixTemplate(editor, false) != null)
+      return true;
 
-
-    if (findFoo(editor, false) != null) return true;
-
-    return myUnderlying.isEnabled(editor, dataContext);
+    return myUnderlyingHandler.isEnabled(editor, dataContext);
   }
 
-  @Nullable
-  protected LookupElement findFoo(Editor editor, boolean reparseIfAvailable) {
+  @Override public void execute(@NotNull Editor editor, @NotNull DataContext dataContext) {
+    final LookupElement postfixElement = findPostfixTemplate(editor, true);
+    if (postfixElement == null) {
+      myUnderlyingHandler.execute(editor, dataContext);
+      return;
+    }
+
+    Project project = editor.getProject();
+    assert (project != null) : "project != null";
+
+    Document document = editor.getDocument();
+    PsiDocumentManager.getInstance(project).commitDocument(document);
+
+    PsiFile psiFile = PsiUtilBase.getPsiFileInEditor(editor, project);
+    assert (psiFile != null) : "psiFile != null";
+
+    OffsetMap offsetMap = new OffsetMap(document);
+    int startOffset = editor.getCaretModel().getOffset() - postfixElement.getLookupString().length();
+    offsetMap.addOffset(CompletionInitializationContext.START_OFFSET, startOffset);
+
+    final InsertionContext insertionContext = new InsertionContext(
+      offsetMap, '\t', new LookupElement[]{postfixElement}, psiFile, editor, false);
+
+    Application application = ApplicationManager.getApplication();
+    application.runWriteAction(new Runnable() {
+      @Override public void run() { postfixElement.handleInsert(insertionContext); }
+    });
+
+    Runnable laterRunnable = insertionContext.getLaterRunnable();
+    if (laterRunnable != null) {
+      if (application.isUnitTestMode()) {
+        laterRunnable.run();
+      } else {
+        application.invokeLater(laterRunnable);
+      }
+    }
+  }
+
+  @Nullable protected LookupElement findPostfixTemplate(@NotNull Editor editor, boolean leaveReparseOnSuccess) {
     Project project = editor.getProject();
     if (project == null) return null;
 
     final Document document = editor.getDocument();
     final int offset = editor.getCaretModel().getOffset();
+    final String dummyIdentifier = CompletionUtilCore.DUMMY_IDENTIFIER_TRIMMED;
 
     boolean reparseBack = true;
+    Application application = ApplicationManager.getApplication();
     try {
-      ApplicationManager.getApplication().runWriteAction(new Runnable() {
-        @Override public void run() {
-          document.insertString(offset, CompletionUtilCore.DUMMY_IDENTIFIER_TRIMMED);
-        }
+      // ugly physical reparse with dummy identifier
+      application.runWriteAction(new Runnable() {
+        @Override public void run() { document.insertString(offset, dummyIdentifier); }
       });
 
-      PsiDocumentManager.getInstance(project).commitAllDocuments();
+      PsiDocumentManager.getInstance(project).commitDocument(document);
 
+      PsiFile psiFile = PsiUtilBase.getPsiFileInEditor(editor, project);
+      if (psiFile == null) return null;
 
-      PsiFile file = PsiUtilBase.getPsiFileInEditor(editor, project);
-      if (file == null) return null;
+      PsiElement position = psiFile.findElementAt(offset);
+      if (position == null) return null;
 
+      // fine, now let's build contexts and look for postfix templates
+      PostfixExecutionContext executionContext =
+        new PostfixExecutionContext(true, dummyIdentifier, (psiFile instanceof PsiCodeFragment));
 
-      PsiElement elementAt = file.findElementAt(offset);
-      if (elementAt == null) return null;
-
-      if (elementAt instanceof PsiWhiteSpace && offset > 0) {
-        // todo: handle other situations?
-        elementAt = file.findElementAt(offset - 1);
-      }
-
-      if (elementAt == null) return null;
-
-      PostfixExecutionContext executionContext = new PostfixExecutionContext(true, "postfix", false /* ? */);
-      PostfixTemplateContext templateContext = myTemplatesManager.isAvailable(elementAt, executionContext);
+      PostfixTemplateContext templateContext = myTemplatesManager.isAvailable(position, executionContext);
       if (templateContext == null) return null;
 
-      List<LookupElement> elements = myTemplatesManager.collectTemplates(templateContext);
-
-
-      for (LookupElement element : elements) {
-
+      for (LookupElement element : myTemplatesManager.collectTemplates(templateContext)) {
         String lookupString = element.getLookupString();
-        if (offset > lookupString.length()) {
-          String prefix = document.getText(new TextRange(offset - lookupString.length(), offset));
-          if (prefix.equals(lookupString)) {
-            if (reparseIfAvailable) {
-              reparseBack = false;
-            }
+        if (offset <= lookupString.length()) continue;
 
-            return element;
-          }
+        TextRange prefixRange = new TextRange(offset - lookupString.length(), offset);
+        if (lookupString.equals(document.getText(prefixRange))) {
+          if (leaveReparseOnSuccess) reparseBack = false;
+          return element;
         }
       }
-
-      return null;
-
     } finally {
-      if (reparseBack)
-      {
-        ApplicationManager.getApplication().runWriteAction(new Runnable() {
+      if (reparseBack) {
+        application.runWriteAction(new Runnable() {
           @Override public void run() {
-            document.deleteString(offset, offset + CompletionUtilCore.DUMMY_IDENTIFIER_TRIMMED.length());
+            document.deleteString(offset, offset + dummyIdentifier.length());
           }
         });
-
-
       }
     }
 
-
-  }
-
-  @Override public void execute(Editor editor, DataContext dataContext) {
-    final LookupElement lookupElement = findFoo(editor, true);
-    if (lookupElement != null) {
-      PsiFile psiFile = PsiUtilBase.getPsiFileInEditor(editor, editor.getProject());
-
-      OffsetMap offsetMap = new OffsetMap(editor.getDocument());
-      int offset = editor.getCaretModel().getOffset() - lookupElement.getLookupString().length();
-      offsetMap.addOffset(CompletionInitializationContext.START_OFFSET, offset);
-
-      final InsertionContext context = new InsertionContext(
-        offsetMap, '\t',
-        new LookupElement[]{lookupElement}, psiFile,
-        editor, false);
-
-
-      ApplicationManager.getApplication().runWriteAction(new Runnable() {
-        @Override public void run() {
-          lookupElement.handleInsert(context);
-        }
-      });
-
-      /*public InsertionContext(
-      com.intellij.codeInsight.completion.OffsetMap offsetMap,
-       char completionChar, com.intellij.codeInsight.lookup.LookupElement[] elements,
-        @org.jetbrains.annotations.NotNull com.intellij.psi.PsiFile file, @org.jetbrains.annotations.NotNull
-       com.intellij.openapi.editor.Editor editor, boolean addCompletionChar)*/
-
-      return;
-    }
-
-    myUnderlying.execute(editor, dataContext);
+    return null;
   }
 }
