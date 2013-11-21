693eddf5d8347e35e71f918bc5002cd98cd524a4
anna <Anna.Kozlova@jetbrains.com>
2013-11-21 13:03:05 +0100

    new inference: testdata (cherry picked from commit 6f0f160f905ae275a2eeaca58343a176df1a42c8)

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewMethodRefHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewMethodRefHighlightingTest.java
@@ -112,6 +112,10 @@ public class NewMethodRefHighlightingTest extends LightDaemonAnalyzerTestCase {
     doTest();
   }
 
+  public void testAfterBounds() throws Exception {
+    doTest();
+  }
+
   private void doTest() {
     doTest(false);
   }
