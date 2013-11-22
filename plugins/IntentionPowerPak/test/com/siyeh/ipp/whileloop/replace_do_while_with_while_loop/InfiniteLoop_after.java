a1f28153f33d2c75d8129c2cf3c2696f113c58e7
Bas Leijdekkers <basleijdekkers@gmail.com>
2013-11-22 20:39:08 +0100

    temporary tests fix

--- plugins/IntentionPowerPak/test/com/siyeh/ipp/whileloop/replace_do_while_with_while_loop/InfiniteLoop_after.java
+++ plugins/IntentionPowerPak/test/com/siyeh/ipp/whileloop/replace_do_while_with_while_loop/InfiniteLoop_after.java
@@ -2,7 +2,7 @@ package com.siyeh.ipp.whileloop.replace_do_while_with_while_loop;
 
 class InfiniteLoop {
   void m() {
-      while ((true)) {
+      while (true) {
           int i = 10;
           System.out.println(i);
       }
