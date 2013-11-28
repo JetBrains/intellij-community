9bbe8c24034f60bcd2491c5dbb3e85e50b53957f
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2013-11-28 17:10:12 +0400

    Add test cases for parsing and completion in JSON plugin

---
+++ plugins/json-tests/test/com/intellij/json/ParsingTest.java
@@ -0,0 +1,39 @@
+package com.intellij.json;
+
+import com.intellij.testFramework.ParsingTestCase;
+import com.intellij.testFramework.PlatformTestUtil;
+import com.jetbrains.json.JsonParserDefinition;
+
+/**
+ * @author Mikhail Golubev
+ */
+public class ParsingTest extends ParsingTestCase {
+  public ParsingTest() {
+    super("psi", "json", new JsonParserDefinition());
+  }
+
+  public void testKeywords() throws Exception {
+    doTest();
+  }
+
+  public void testNestedArrayLiterals() throws Exception {
+    doTest();
+  }
+
+  public void testNestedObjectLiterals() throws Exception {
+    doTest();
+  }
+
+  public void testTopLevelStringLiteral() throws Exception {
+    doTest();
+  }
+
+  private void doTest() {
+    doTest(true);
+  }
+
+  @Override
+  protected String getTestDataPath() {
+    return PlatformTestUtil.getCommunityPath() + "/plugins/json-tests/testData";
+  }
+}
