b5675816ba8ed2ff66e8defd3ce231696e45372e
Konstantin Bulenkov <kb@jetbrains.com>
2013-11-29 15:22:24 +0100

    IDEA-117258 IntelliJ theme: selected tabs in Debugger and in Run <application server> views get black color
    (cherry picked from commit 5c1098b0b075d1a391a2929de1048502260952bc)

--- platform/platform-api/src/com/intellij/ui/tabs/JBTabsPresentation.java
+++ platform/platform-api/src/com/intellij/ui/tabs/JBTabsPresentation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,6 +31,8 @@ public interface JBTabsPresentation {
   JBTabsPresentation setTabSidePaintBorder(int size);
 
   JBTabsPresentation setPaintFocus(boolean paintFocus);
+
+  JBTabsPresentation setAlwaysPaintSelectedTab(final boolean paintSelected);
 
   JBTabsPresentation setStealthTabMode(boolean stealthTabMode);
 
