c48a500695934d2a25d5499767543ae0abad4228
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2013-11-30 16:48:36 +0400

    cleanup

--- src/org/jetbrains/postfixCompletion/templates/PostfixTemplate.java
+++ src/org/jetbrains/postfixCompletion/templates/PostfixTemplate.java
@@ -6,8 +6,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.postfixCompletion.infrastructure.PostfixTemplateContext;
 
-import java.util.List;
-
 public abstract class PostfixTemplate {
   @NotNull
   public static final ExtensionPointName<PostfixTemplate> EP_NAME =
