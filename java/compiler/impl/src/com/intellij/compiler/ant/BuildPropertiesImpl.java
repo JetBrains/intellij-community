c8891678328b1123623900d1fa3ecc6e16ead736
Eugene Zhuravlev <jeka@intellij.com>
2013-12-02 18:16:59 +0100

    get max heap value for javac from the new location

--- java/compiler/impl/src/com/intellij/compiler/ant/BuildPropertiesImpl.java
+++ java/compiler/impl/src/com/intellij/compiler/ant/BuildPropertiesImpl.java
@@ -15,6 +15,7 @@
  */
 package com.intellij.compiler.ant;
 
+import com.intellij.compiler.CompilerWorkspaceConfiguration;
 import com.intellij.compiler.ant.taskdefs.*;
 import com.intellij.compiler.impl.javaCompiler.javac.JavacConfiguration;
 import com.intellij.openapi.compiler.CompilerBundle;
@@ -57,7 +58,8 @@ public class BuildPropertiesImpl extends BuildProperties {
     add(new Property(PROPERTY_COMPILER_GENERATE_NO_WARNINGS, javacSettings.GENERATE_NO_WARNINGS ? "on" : "off"));
     add(new Property(PROPERTY_COMPILER_ADDITIONAL_ARGS, javacSettings.ADDITIONAL_OPTIONS_STRING));
     //noinspection HardCodedStringLiteral
-    add(new Property(PROPERTY_COMPILER_MAX_MEMORY, Integer.toString(javacSettings.MAXIMUM_HEAP_SIZE) + "m"));
+    final int heapSize = CompilerWorkspaceConfiguration.getInstance(project).getProcessHeapSize(javacSettings.MAXIMUM_HEAP_SIZE);
+    add(new Property(PROPERTY_COMPILER_MAX_MEMORY, Integer.toString(heapSize) + "m"));
 
     add(new IgnoredFiles());
 
