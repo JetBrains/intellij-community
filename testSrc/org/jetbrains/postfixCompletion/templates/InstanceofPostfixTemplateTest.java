681278aa3a782811665a6fa693986042a2a8a043
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2013-12-03 17:57:49 +0400

    Add test for instanceof template

--- testSrc/org/jetbrains/postfixCompletion/templates/InstanceofPostfixTemplateTest.java
+++ testSrc/org/jetbrains/postfixCompletion/templates/InstanceofPostfixTemplateTest.java
@@ -1,11 +1,28 @@
 package org.jetbrains.postfixCompletion.templates;
 
-/**
- * @author ignatov
- */
+import com.intellij.codeInsight.template.impl.TemplateManagerImpl;
+import com.intellij.codeInsight.template.impl.TemplateState;
+
 public class InstanceofPostfixTemplateTest extends PostfixTemplateTestCase {
   @Override
   protected String getTestDataPath() { return "testData/templates/instanceof"; }
 
   public void testSingleExpression() { doTest(); }
+
+  public void testSingleExpressionTemplate() {
+    TemplateManagerImpl.setTemplateTesting(getProject(), getTestRootDisposable());
+    
+    myFixture.configureByFile(getTestName(true) + ".java");
+    myFixture.type('\t');
+
+    TemplateState templateState = TemplateManagerImpl.getTemplateState(myFixture.getEditor());
+    assertNotNull(templateState);
+    assertFalse(templateState.isFinished());
+
+    myFixture.type("Integer");
+    templateState.nextTab();
+    assertTrue(templateState.isFinished());
+
+    myFixture.checkResultByFile(getTestName(true) + "_after.java", true);
+  }
 }
