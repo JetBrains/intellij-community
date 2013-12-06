1cd17aa1a7cef22ce95e627ac47e2938f426cd2e
Anton Makeev <Anton.Makeev@jetbrains.com>
2013-12-06 10:28:16 +0100

    support downloadUrl for custom plugin repositories (IDEA-112778) (cherry picked from commit 32fa8b7da1696ea3ac76490e1370b4b27a4cf5f9)

--- platform/platform-impl/src/com/intellij/ide/plugins/RepositoryContentHandler.java
+++ platform/platform-impl/src/com/intellij/ide/plugins/RepositoryContentHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -47,6 +47,7 @@ class RepositoryContentHandler extends DefaultHandler {
   @NonNls public static final String CHNAGE_NOTES = "change-notes";
   @NonNls private static final String DEPENDS = "depends";
   @NonNls private static final String DOWNLOADS = "downloads";
+  @NonNls public static final String DOWNLOAD_URL = "downloadUrl";
   @NonNls private static final String SIZE = "size";
   @NonNls private static final String RATING = "rating";
 
@@ -115,6 +116,9 @@ class RepositoryContentHandler extends DefaultHandler {
     }
     else if (qName.equals(RATING)) {
       currentPlugin.setRating(currentValue);
+    }
+    else if (qName.equals(DOWNLOAD_URL)) {
+      currentPlugin.setDownloadUrl(currentValue);
     }
     currentValue = "";
   }
