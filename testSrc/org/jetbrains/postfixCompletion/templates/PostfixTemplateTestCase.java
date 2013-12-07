d86bd2caacb370d0c06f1acbfacfd972c4cc1ea5
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2013-12-08 02:45:53 +0400

    Add stub of Boolean class to environment

--- testSrc/org/jetbrains/postfixCompletion/templates/PostfixTemplateTestCase.java
+++ testSrc/org/jetbrains/postfixCompletion/templates/PostfixTemplateTestCase.java
@@ -1,11 +1,21 @@
 package org.jetbrains.postfixCompletion.templates;
 
-import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
+import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
 
-abstract public class PostfixTemplateTestCase extends LightPlatformCodeInsightFixtureTestCase {
+abstract public class PostfixTemplateTestCase extends LightCodeInsightFixtureTestCase {
   protected void doTest() {
     myFixture.configureByFile(getTestName(true) + ".java");
     myFixture.type('\t');
     myFixture.checkResultByFile(getTestName(true) + "_after.java", true);
+  }
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+
+    myFixture.addClass("package java.lang;\n" +
+                       "public final class Boolean implements java.io.Serializable,\n" +
+                       "                                      Comparable<Boolean>\n" +
+                       "{}");
   }
 }
