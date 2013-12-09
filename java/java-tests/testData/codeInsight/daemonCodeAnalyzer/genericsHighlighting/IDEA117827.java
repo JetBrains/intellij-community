cd6570629f1b590e4a830a3ebe8f8111fb39d7d9
Anna Kozlova <anna.kozlova@jetbrains.com>
2013-12-09 15:12:48 +0400

    IDEA-117827 Invalid "ambiguous method call" error

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/genericsHighlighting/IDEA117827.java
@@ -0,0 +1,16 @@
+
+import java.util.Collection;
+import java.util.List;
+
+public class Testsss {
+
+  public <TA, CA extends Iterable<TA>> void that(Iterable<TA> target) {}
+
+  public <T, C extends Collection<T>> void that(Collection<T> target) {}
+
+  void foo(ImmutableList<String> l) {
+    that( l);
+  }
+
+  interface ImmutableList<T> extends List<T> {}
+}
