e4df1349c20ffcab95722cd21eb66e4e8a96dcd3
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2013-12-12 15:38:53 +0400

    Add "Show certificate dialog" action to "Internal Actions" group

---
+++ plugins/tasks/tasks-core/src/com/intellij/tasks/actions/ShowCertificateInfoAction.java
@@ -0,0 +1,43 @@
+package com.intellij.tasks.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.util.io.StreamUtil;
+import com.intellij.util.net.UntrustedCertificateWarningDialog;
+
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.cert.X509Certificate;
+
+/**
+ * @author Mikhail Golubev
+ */
+public class ShowCertificateInfoAction extends AnAction {
+  private static final Logger LOG = Logger.getInstance(ShowCertificateInfoAction.class);
+
+  public ShowCertificateInfoAction() {
+    super("Show certificate information dialog");
+  }
+
+  @Override
+  public void actionPerformed(final AnActionEvent e) {
+    try {
+      InputStream stream = ShowCertificateInfoAction.class.getResourceAsStream("keystore");
+      try {
+        final KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        keyStore.load(stream, "changeit".toCharArray());
+        UntrustedCertificateWarningDialog dialog =
+          new UntrustedCertificateWarningDialog((X509Certificate)keyStore.getCertificate("mykey"), "", "");
+        LOG.debug("Accepted: " + dialog.showAndGet());
+      }
+      finally {
+        StreamUtil.closeStream(stream);
+      }
+    }
+    catch (Exception logged) {
+      LOG.error(logged);
+    }
+  }
+}
+
