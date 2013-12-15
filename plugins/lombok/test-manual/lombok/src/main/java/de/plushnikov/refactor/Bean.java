b0274296c1cf5cb9d5ca475c1d3f67dfdce5ceda
Michail Plushnikov <michail@plushnikov.de>
2013-12-15 20:40:37 +0100

    Simple Bean class
    GitOrigin-RevId: 9bb4e2f916490bed4e54f417dac6d759b2bb36a9

---
+++ plugins/lombok/test-manual/lombok/src/main/java/de/plushnikov/refactor/Bean.java
@@ -0,0 +1,73 @@
+package de.plushnikov.refactor;
+
+import java.io.InputStream;
+import java.util.Date;
+
+public class Bean {
+    private int a;
+    private float b;
+    private double c;
+    private String d;
+    private Integer e;
+    private InputStream f;
+    private Date g;
+
+    public Bean() {
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public float getB() {
+        return b;
+    }
+
+    public void setB(float b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    public String getD() {
+        return d;
+    }
+
+    public void setD(String d) {
+        this.d = d;
+    }
+
+    public Integer getE() {
+        return e;
+    }
+
+    public void setE(Integer e) {
+        this.e = e;
+    }
+
+    public InputStream getF() {
+        return f;
+    }
+
+    public void setF(InputStream f) {
+        this.f = f;
+    }
+
+    public Date getG() {
+        return g;
+    }
+
+    public void setG(Date g) {
+        this.g = g;
+    }
+}
