84c3770e5d61e86bd9dbdb99898d140db3c8bfd0
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2013-12-17 02:44:10 +0400

    Reformat tests

--- testSrc/org/jetbrains/postfixCompletion/templates/ForAscendingPostfixTemplateTest.java
+++ testSrc/org/jetbrains/postfixCompletion/templates/ForAscendingPostfixTemplateTest.java
@@ -1,33 +1,13 @@
 package org.jetbrains.postfixCompletion.templates;
 
 public class ForAscendingPostfixTemplateTest extends PostfixTemplateTestCase {
-  public void testIntArray() {
-    doTest();
-  }
-
-  public void testIntNumber() {
-    doTest();
-  }
-
-  public void testByteNumber() {
-    doTest();
-  }
-
-  public void testBoxedByteNumber() {
-    doTest();
-  }
-
-  public void testCollection() {
-    doTest();
-  }
-
-  public void testBoxedIntegerArray() {
-    doTest();
-  }
-
-  public void testBoxedLongArray() {
-    doTest();
-  }
+  public void testIntArray() { doTest(); }
+  public void testIntNumber() { doTest(); }
+  public void testByteNumber() { doTest(); }
+  public void testBoxedByteNumber() { doTest(); }
+  public void testCollection() { doTest(); }
+  public void testBoxedIntegerArray() { doTest(); }
+  public void testBoxedLongArray() { doTest(); }
 
   @Override
   protected String getTestDataPath() {
