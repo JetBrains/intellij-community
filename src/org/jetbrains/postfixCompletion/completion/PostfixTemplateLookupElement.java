3fb8600fa12c4b3d534834107f798253091c775e
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2013-12-17 03:11:04 +0400

    Remove redundant dots and parenthesis from lookup elements Remove unsupported template from readme

--- src/org/jetbrains/postfixCompletion/completion/PostfixTemplateLookupElement.java
+++ src/org/jetbrains/postfixCompletion/completion/PostfixTemplateLookupElement.java
@@ -1,10 +1,12 @@
 package org.jetbrains.postfixCompletion.completion;
 
 import com.intellij.codeInsight.completion.InsertionContext;
+import com.intellij.codeInsight.lookup.LookupElementPresentation;
 import com.intellij.codeInsight.template.CustomTemplateCallback;
 import com.intellij.codeInsight.template.impl.LiveTemplateLookupElement;
 import com.intellij.codeInsight.template.impl.TemplateImpl;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.util.SystemInfo;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.postfixCompletion.templates.PostfixLiveTemplate;
@@ -17,13 +19,19 @@ class PostfixTemplateLookupElement extends LiveTemplateLookupElement {
   private final PostfixTemplate myTemplate;
 
   public PostfixTemplateLookupElement(@NotNull PostfixTemplate template, char shortcut) {
-    super(createStubTemplate(template, shortcut), template.getKey(), true, true);
+    super(createStubTemplate(template, shortcut), template.getPresentableName(), true, true);
     myTemplate = template;
   }
 
   @NotNull
   public PostfixTemplate getPostfixTemplate() {
     return myTemplate;
+  }
+
+  @Override
+  public void renderElement(LookupElementPresentation presentation) {
+    super.renderElement(presentation);
+    presentation.setTailText(" " + arrow() + " " + myTemplate.getExample());
   }
 
   @Override
@@ -47,8 +55,11 @@ class PostfixTemplateLookupElement extends LiveTemplateLookupElement {
 
   private static TemplateImpl createStubTemplate(@NotNull PostfixTemplate postfixTemplate, char shortcut) {
     TemplateImpl template = new TemplateImpl(postfixTemplate.getKey(), "postfixTemplate");
-    template.setDescription(postfixTemplate.getExample());
     template.setShortcutChar(shortcut);
     return template;
+  }
+
+  private static String arrow() {
+    return SystemInfo.isMac ? "→" :"->";
   }
 }
