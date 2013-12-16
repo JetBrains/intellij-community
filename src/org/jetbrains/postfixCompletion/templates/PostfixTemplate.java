857aa951348e0da37f1f1804fabd334c4d956ac2
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2013-12-17 02:54:18 +0400

    MEGA CLEAN UP!!!11

--- src/org/jetbrains/postfixCompletion/templates/PostfixTemplate.java
+++ src/org/jetbrains/postfixCompletion/templates/PostfixTemplate.java
@@ -1,35 +1,29 @@
 package org.jetbrains.postfixCompletion.templates;
 
-import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.extensions.ExtensionPointName;
-import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiExpression;
 import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.postfixCompletion.infrastructure.PostfixTemplateContext;
-import org.jetbrains.postfixCompletion.infrastructure.TemplateInfo;
 import org.jetbrains.postfixCompletion.settings.PostfixCompletionSettings;
 
 public abstract class PostfixTemplate {
-  @Nullable private final String myPresentableName;
-  @Nullable private final String myKey;
-  @Nullable private final String myDescription;
-  @Nullable private final String myExample;
+  @NotNull private final String myPresentableName;
+  @NotNull private final String myKey;
+  @NotNull private final String myDescription;
+  @NotNull private final String myExample;
 
-  @Deprecated
-  protected PostfixTemplate() {
-    this(null, null, null, null);
+  @NotNull
+  public static final ExtensionPointName<PostfixTemplate> EP_NAME = ExtensionPointName.create("org.jetbrains.postfixCompletion.postfixTemplate");
+
+  protected PostfixTemplate(@NotNull String name, @NotNull String description, @NotNull String example) {
+    this(name, "." + name, description, example);
   }
 
-  protected PostfixTemplate(@Nullable String name, @Nullable String description, @Nullable String example) {
-    this(name, null, description, example);
-  }
-  
-  protected PostfixTemplate(@Nullable String name, @Nullable String key, @Nullable String description, @Nullable String example) {
+  protected PostfixTemplate(@NotNull String name, @NotNull String key, @NotNull String description, @NotNull String example) {
     myPresentableName = name;
     myKey = key;
     myDescription = description;
@@ -37,46 +31,23 @@ public abstract class PostfixTemplate {
   }
 
   @NotNull
-  public static final ExtensionPointName<PostfixTemplate> EP_NAME =
-    ExtensionPointName.create("org.jetbrains.postfixCompletion.postfixTemplate");
-
-  @Nullable
-  public LookupElement createLookupElement(@NotNull PostfixTemplateContext context) {
-    throw new UnsupportedOperationException();
-  }
-  
-  //NEW API
-  
-  @NotNull
   public final String getKey() {
-    return StringUtil.notNullize(myKey, "." + getPresentableName());
+    return myKey;
   }
 
   @NotNull
   public String getPresentableName() {
-    if (myPresentableName != null) {
-      return myPresentableName;
-    }
-    TemplateInfo annotation = getClass().getAnnotation(TemplateInfo.class);
-    return annotation.templateName();
+    return myPresentableName;
   }
 
   @NotNull
   public String getDescription() {
-    if (myDescription != null) {
-      return myDescription;
-    }
-    TemplateInfo annotation = getClass().getAnnotation(TemplateInfo.class);
-    return annotation.description();
+    return myDescription;
   }
 
   @NotNull
   public String getExample() {
-    if (myExample != null) {
-      return myExample;
-    }
-    TemplateInfo annotation = getClass().getAnnotation(TemplateInfo.class);
-    return annotation.example();
+    return myExample;
   }
 
   public boolean isEnabled() {
@@ -89,10 +60,7 @@ public abstract class PostfixTemplate {
     return PsiTreeUtil.getTopmostParentOfType(context, PsiExpression.class);
   }
 
-  public boolean isApplicable(@NotNull PsiElement context, @NotNull Document copyDocument, int newOffset) {
-    //todo: implement it in each template, make method abstract
-    return false;
-  }
+  public abstract boolean isApplicable(@NotNull PsiElement context, @NotNull Document copyDocument, int newOffset);
 
   public abstract void expand(@NotNull PsiElement context, @NotNull Editor editor);
 }
