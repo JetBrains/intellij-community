b318047b8cac2518fea8aca86d88cacfe3f64bd2
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2013-12-18 12:52:43 +0100

    2x more compact mapping storage for 7bit ascii paths [r=Eugene.Zhuravlev] (cherry picked from commit 2f10822)

--- jps/jps-builders/src/org/jetbrains/jps/incremental/storage/OneToManyPathsMapping.java
+++ jps/jps-builders/src/org/jetbrains/jps/incremental/storage/OneToManyPathsMapping.java
@@ -82,9 +82,10 @@ public class OneToManyPathsMapping extends AbstractStateStorage<String, Collecti
   }
 
   private static class PathCollectionExternalizer implements DataExternalizer<Collection<String>> {
+    private final byte[] myBuffer = IOUtil.allocReadWriteUTFBuffer();
     public void save(DataOutput out, Collection<String> value) throws IOException {
       for (String str : value) {
-        IOUtil.writeString(str, out);
+        IOUtil.writeUTFFast(myBuffer, out, str);
       }
     }
 
@@ -92,7 +93,7 @@ public class OneToManyPathsMapping extends AbstractStateStorage<String, Collecti
       final Set<String> result = new THashSet<String>(FileUtil.PATH_HASHING_STRATEGY);
       final DataInputStream stream = (DataInputStream)in;
       while (stream.available() > 0) {
-        final String str = IOUtil.readString(stream);
+        final String str = IOUtil.readUTFFast(myBuffer, stream);
         result.add(str);
       }
       return result;
