2f10822e839ed5a25d3a90d6b6489c9f98b6f276
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2013-12-18 12:49:42 +0100

    2x more compact mapping storage for 7bit ascii paths [r=Eugene.Zhuravlev]

--- jps/jps-builders/src/org/jetbrains/jps/incremental/storage/BuildDataManager.java
+++ jps/jps-builders/src/org/jetbrains/jps/incremental/storage/BuildDataManager.java
@@ -41,7 +41,7 @@ import java.util.concurrent.ConcurrentMap;
  *         Date: 10/7/11
  */
 public class BuildDataManager implements StorageOwner {
-  private static final int VERSION = 21;
+  private static final int VERSION = 22;
   private static final Logger LOG = Logger.getInstance("#org.jetbrains.jps.incremental.storage.BuildDataManager");
   private static final String SRC_TO_FORM_STORAGE = "src-form";
   private static final String MAPPINGS_STORAGE = "mappings";
