d5de3de366605723c9e71b61fa75023bb3caeb1c
Gregory.Shrago <gregory.shrago@jetbrains.com>
2013-12-24 16:56:46 +0400

    cleanup

--- platform/testFramework/src/com/intellij/testFramework/UsefulTestCase.java
+++ platform/testFramework/src/com/intellij/testFramework/UsefulTestCase.java
@@ -58,6 +58,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.awt.*;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -681,10 +682,14 @@ public abstract class UsefulTestCase extends TestCase {
     String fileText;
     try {
       if (OVERWRITE_TESTDATA) {
-        FileUtil.writeToFile(new File(filePath), actualText);
+        VfsTestUtil.overwriteTestData(filePath, actualText);
         System.out.println("File " + filePath + " created.");
       }
       fileText = FileUtil.loadFile(new File(filePath));
+    }
+    catch (FileNotFoundException e) {
+      VfsTestUtil.overwriteTestData(filePath, actualText);
+      throw new AssertionFailedError("No output text found. File " + filePath + " created.");
     }
     catch (IOException e) {
       throw new RuntimeException(e);
