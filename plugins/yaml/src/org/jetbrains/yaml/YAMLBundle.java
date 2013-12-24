0c2fd975bdefea706a1f5a6ac3afc6bd34840de5
Alexey Kudravtsev <cdr@intellij.com>
2013-12-24 19:26:05 +0400

    notnull

--- plugins/yaml/src/org/jetbrains/yaml/YAMLBundle.java
+++ plugins/yaml/src/org/jetbrains/yaml/YAMLBundle.java
@@ -3,6 +3,7 @@ package org.jetbrains.yaml;
 import com.intellij.CommonBundle;
 import com.intellij.reference.SoftReference;
 import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.PropertyKey;
 
 import java.lang.ref.Reference;
@@ -12,22 +13,22 @@ import java.util.ResourceBundle;
  * @author oleg
  */
 public class YAMLBundle {
-  private static Reference<ResourceBundle> ourBundle;
 
+  public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, @NotNull Object... params) {
+    return CommonBundle.message(getBundle(), key, params);
+  }
+
+  private static Reference<ResourceBundle> ourBundle;
   @NonNls
   private static final String BUNDLE = "messages.YAMLBundle";
 
   private YAMLBundle() {
   }
 
-  public static String message(@PropertyKey(resourceBundle = BUNDLE)String key, Object... params) {
-    return CommonBundle.message(getBundle(), key, params);
-  }
-
   /*
-   * This method added for jruby access
-   */
-  public static String message(@PropertyKey(resourceBundle = BUNDLE)String key) {
+     * This method added for jruby access
+     */
+  public static String message(@PropertyKey(resourceBundle = BUNDLE) String key) {
     return CommonBundle.message(getBundle(), key);
   }
 
