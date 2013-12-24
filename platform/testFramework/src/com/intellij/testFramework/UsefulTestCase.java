4548db3b7c52381c3a106ff7c910ed8218c116ff
Gregory.Shrago <gregory.shrago@jetbrains.com>
2013-12-24 20:38:06 +0400

    test fix: explicit encoding is required

--- platform/testFramework/src/com/intellij/testFramework/UsefulTestCase.java
+++ platform/testFramework/src/com/intellij/testFramework/UsefulTestCase.java
@@ -31,6 +31,7 @@ import com.intellij.openapi.util.*;
 import com.intellij.openapi.util.io.FileSystemUtil;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vfs.CharsetToolkit;
 import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileVisitor;
@@ -685,7 +686,7 @@ public abstract class UsefulTestCase extends TestCase {
         VfsTestUtil.overwriteTestData(filePath, actualText);
         System.out.println("File " + filePath + " created.");
       }
-      fileText = FileUtil.loadFile(new File(filePath));
+      fileText = FileUtil.loadFile(new File(filePath), CharsetToolkit.UTF8);
     }
     catch (FileNotFoundException e) {
       VfsTestUtil.overwriteTestData(filePath, actualText);
