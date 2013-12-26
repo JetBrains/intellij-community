f3ea46ebcc7a6e6637607be7f7f5cb56194ba412
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2013-12-26 19:23:29 +0400

    updated test data for python3.4

--- python/psi-api/src/com/jetbrains/python/psi/LanguageLevel.java
+++ python/psi-api/src/com/jetbrains/python/psi/LanguageLevel.java
@@ -108,7 +108,10 @@ public enum LanguageLevel {
       if (pythonVersion.startsWith("3.2")) {
         return PYTHON32;
       }
-      return PYTHON33;
+      if (pythonVersion.startsWith("3.3")) {
+        return PYTHON33;
+      }
+      return PYTHON34;
     }
     return PYTHON24;
   }
--- python/testData/inspections/PyCompatibilityInspection/fromImportStatement.py
+++ python/testData/inspections/PyCompatibilityInspection/fromImportStatement.py
@@ -1,7 +1,7 @@
 <warning descr="Python version 2.4 doesn't support this syntax.">from . import smth</warning>
 <warning descr="Python version 2.4 doesn't support this syntax.">from .module import name1, name2</warning>
 
-from <warning descr="Python version 3.0, 3.1, 3.2, 3.3 do not have module Bastion">Bastion</warning> import BastionClass
+from <warning descr="Python version 3.0, 3.1, 3.2, 3.3, 3.4 do not have module Bastion">Bastion</warning> import BastionClass
 
 <warning descr="Python version 2.4 doesn't support this syntax.">from . import exceptions</warning>
 
