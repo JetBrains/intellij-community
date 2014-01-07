354936fb5f776295d8ee265b404d8e3ca15becc0
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-01-07 12:05:15 +0100

    normalize tests

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/quickFix/ChangeParameterClassTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/quickFix/ChangeParameterClassTest.java
@@ -6,9 +6,12 @@ import com.intellij.pom.java.LanguageLevel;
 
 
 public class ChangeParameterClassTest extends LightQuickFix15TestCase {
+  @Override
+  protected LanguageLevel getLanguageLevel() {
+    return LanguageLevel.JDK_1_5;
+  }
 
   public void test() throws Exception {
-    setLanguageLevel(LanguageLevel.JDK_1_5);
     doAllTests();
   }
 
