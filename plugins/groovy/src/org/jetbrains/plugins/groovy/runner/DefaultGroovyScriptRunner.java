d1d61a1c996637f6cf4216a865fcb0a261e6756a
Max Medvedev <maxim.medvedev@jetbrains.com>
2014-01-11 19:04:55 +0400

    [^Peter]IDEA-117982 correct classpath for groovy scripts

--- plugins/groovy/src/org/jetbrains/plugins/groovy/runner/DefaultGroovyScriptRunner.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/runner/DefaultGroovyScriptRunner.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -118,6 +118,8 @@ public class DefaultGroovyScriptRunner extends GroovyScriptRunner {
 
     params.getProgramParametersList().add("--main");
     params.getProgramParametersList().add(mainClass);
+
+    addClasspathFromRootModel(module, tests, params, true);
 
     if (params.getVMParametersList().getPropertyValue(GroovycOSProcessHandler.GRAPE_ROOT) == null) {
       String sysRoot = System.getProperty(GroovycOSProcessHandler.GRAPE_ROOT);
