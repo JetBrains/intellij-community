7ab28bba3189ffb5026d2ce12527ee58be69947d
Alexey Kudravtsev <cdr@intellij.com>
2014-01-17 17:53:33 +0400

    notnull

--- platform/lang-impl/src/com/intellij/codeInsight/hint/ImplementationViewComponent.java
+++ platform/lang-impl/src/com/intellij/codeInsight/hint/ImplementationViewComponent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -87,7 +87,7 @@ public class ImplementationViewComponent extends JPanel {
   private final ActionToolbar myToolbar;
   private JLabel myLabel;
 
-  public void setHint(final JBPopup hint, final String title) {
+  public void setHint(final JBPopup hint, @NotNull String title) {
     myHint = hint;
     myTitle = title;
   }
