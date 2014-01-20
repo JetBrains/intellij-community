db42545dd3c0a3d19a23239652e1938dc10cc496
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2014-01-21 00:45:02 +0400

    PY-6955 Unused import not greyed out if also failing (test refactored)

--- python/testSrc/com/jetbrains/python/fixtures/PyInspectionTestCase.java
+++ python/testSrc/com/jetbrains/python/fixtures/PyInspectionTestCase.java
@@ -24,9 +24,23 @@ public abstract class PyInspectionTestCase extends PyTestCase {
    * Launches test. To be called by test author
    */
   protected void doTest() {
-    myFixture.configureByFile("inspections/" + getInspectionClass().getSimpleName() + "/" + getTestName(true) + ".py");
+    myFixture.configureByFile(getTestDirectory(true) + ".py");
+    configureInspection();
+  }
+
+  protected void doMultiFileTest(@NotNull String filename) {
+    myFixture.copyDirectoryToProject(getTestDirectory(false), "");
+    myFixture.configureFromTempProjectFile(filename);
+    configureInspection();
+  }
+
+  private void configureInspection() {
     myFixture.enableInspections(getInspectionClass());
     myFixture.checkHighlighting(isWarning(), isInfo(), isWeakWarning());
+  }
+
+  private String getTestDirectory(boolean lowercaseFirstLetter) {
+    return "inspections/" + getInspectionClass().getSimpleName() + "/" + getTestName(lowercaseFirstLetter);
   }
 
 
