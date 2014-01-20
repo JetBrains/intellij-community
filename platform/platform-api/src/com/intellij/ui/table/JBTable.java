20c7faac2d6c6504b8abeb29dcf76ab719975801
Konstantin Bulenkov <kb@jetbrains.com>
2014-01-20 18:48:42 +0100

    support table decoration on IntelliJ laf (cherry picked from commit 04425c9)

--- platform/platform-api/src/com/intellij/ui/table/JBTable.java
+++ platform/platform-api/src/com/intellij/ui/table/JBTable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -449,6 +449,7 @@ public class JBTable extends JTable implements ComponentWithEmptyText, Component
     return UIUtil.isUnderAlloyLookAndFeel()
            || UIUtil.isUnderNativeMacLookAndFeel()
            || UIUtil.isUnderDarcula()
+           || UIUtil.isUnderIntelliJLaF()
            || UIUtil.isUnderNimbusLookAndFeel()
            || UIUtil.isUnderWindowsLookAndFeel();
   }
