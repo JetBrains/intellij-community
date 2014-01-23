5459b34a5d6414e62b178fbf44cd408868607aa9
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2014-01-23 17:27:48 +0400

    default test runner is unittest

--- python/src/com/jetbrains/python/testing/TestRunnerService.java
+++ python/src/com/jetbrains/python/testing/TestRunnerService.java
@@ -34,7 +34,7 @@ import java.util.List;
 )
 public class TestRunnerService implements PersistentStateComponent<TestRunnerService> {
   private List<String> myConfigurations = new ArrayList<String>();
-  public String PROJECT_TEST_RUNNER = "";
+  public String PROJECT_TEST_RUNNER = PythonTestConfigurationsModel.PYTHONS_UNITTEST_NAME;
 
   public TestRunnerService() {
     myConfigurations.add(PythonTestConfigurationsModel.PYTHONS_UNITTEST_NAME);
