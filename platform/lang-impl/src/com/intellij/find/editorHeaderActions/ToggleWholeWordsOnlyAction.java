f29934b80e126df0db4da5939e8b388ed028c1b0
Denis Fokin <Denis.Fokin@jetbrains.com>
2014-01-23 15:08:14 +0400

    IDEA-111432 Mac OS X: Shortcut Alt-W in search dialog not working (the mnemonic should be consistent with find in path dialog) (cherry picked from commit 6889a43)

--- platform/lang-impl/src/com/intellij/find/editorHeaderActions/ToggleWholeWordsOnlyAction.java
+++ platform/lang-impl/src/com/intellij/find/editorHeaderActions/ToggleWholeWordsOnlyAction.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2014 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.intellij.find.editorHeaderActions;
 
 import com.intellij.find.EditorSearchComponent;
@@ -5,7 +20,7 @@ import com.intellij.find.FindSettings;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 
 public class ToggleWholeWordsOnlyAction extends EditorHeaderToggleAction {
-  private static final String WHOLE_WORDS_ONLY = "&Words";
+  private static final String WHOLE_WORDS_ONLY = "Wo&rds";
 
   public ToggleWholeWordsOnlyAction(EditorSearchComponent editorSearchComponent) {
     super(editorSearchComponent, WHOLE_WORDS_ONLY);
