eef5c145af3c779ede4364e6f06838ea2bb0430b
Max Medvedev <maxim.medvedev@jetbrains.com>
2014-01-24 16:54:54 +0400

    IDEA-119328 parse code reference elements without name element

--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/parser/parsing/statements/imports/ImportReference.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/parser/parsing/statements/imports/ImportReference.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,7 +36,9 @@ public class ImportReference implements GroovyElementTypes {
       return false;
     }
 
-    ReferenceElement.parseForImport(builder);
+    if (ReferenceElement.parseForImport(builder) == ReferenceElement.ReferenceElementResult.FAIL) {
+      return false;
+    }
 
     if (ParserUtils.getToken(builder, mDOT)) {
       ParserUtils.getToken(builder, mNLS);
