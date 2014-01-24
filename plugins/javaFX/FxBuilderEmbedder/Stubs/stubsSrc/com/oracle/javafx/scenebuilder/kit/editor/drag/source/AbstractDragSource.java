e4c83548150717a9a97308ec00b7b59a517e089d
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-01-24 18:38:27 +0400

    replace SBKit stubs with jar (it's under GPL license and we do not redistribute it)

--- plugins/javaFX/FxBuilderEmbedder/Stubs/stubsSrc/com/oracle/javafx/scenebuilder/kit/editor/drag/source/AbstractDragSource.java
+++
@@ -1,7 +0,0 @@
-package com.oracle.javafx.scenebuilder.kit.editor.drag.source;
-
-import javafx.scene.input.DataFormat;
-
-public class AbstractDragSource {
-    public static final DataFormat INTERNAL_DATA_FORMAT = new DataFormat();
-}
