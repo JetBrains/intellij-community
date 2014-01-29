6e66b227d64d7917335077163401b3204b1809b4
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2014-01-29 20:58:08 +0400

    Don't run code analysis for injections with interpolation in string formatting (PY-11962)

--- python/IntelliLang-python/src/com/jetbrains/python/intelliLang/PyTemporaryInjector.java
+++ python/IntelliLang-python/src/com/jetbrains/python/intelliLang/PyTemporaryInjector.java
@@ -5,6 +5,8 @@ import com.intellij.lang.injection.MultiHostRegistrar;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiLanguageInjectionHost;
+import com.intellij.psi.impl.source.tree.injected.InjectedLanguageUtil;
+import com.jetbrains.python.codeInsight.PyInjectionUtil;
 import com.jetbrains.python.codeInsight.PyInjectorBase;
 import org.intellij.plugins.intelliLang.inject.InjectedLanguage;
 import org.intellij.plugins.intelliLang.inject.InjectorUtils;
@@ -18,9 +20,13 @@ import org.jetbrains.annotations.Nullable;
 public class PyTemporaryInjector extends PyInjectorBase {
   @Override
   public void getLanguagesToInject(@NotNull MultiHostRegistrar registrar, @NotNull PsiElement context) {
-    if (registerInjection(registrar, context)) {
+    final PyInjectionUtil.InjectionResult result = registerInjection(registrar, context);
+    if (result.isInjected()) {
       final TemporaryPlacesRegistry registry = TemporaryPlacesRegistry.getInstance(context.getProject());
       InjectorUtils.registerSupport(registry.getLanguageInjectionSupport(), false, registrar);
+      if (!result.isStrict()) {
+        InjectorUtils.putInjectedFileUserData(registrar, InjectedLanguageUtil.FRANKENSTEIN_INJECTION, Boolean.TRUE);
+      }
     }
   }
 
