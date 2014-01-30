2db0f1ec0807b2744e7fd0022d8db86aa83f6269
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2014-01-30 18:29:19 +0400

    custom steps are back

--- java/idea-ui/src/com/intellij/ide/projectWizard/ProjectTypeStep.java
+++ java/idea-ui/src/com/intellij/ide/projectWizard/ProjectTypeStep.java
@@ -180,8 +180,14 @@ public class ProjectTypeStep extends ModuleWizardStep implements Disposable {
       }
     });
 
-    for (ProjectTemplate category : myTemplatesMap.values()) {
-      myWizard.getSequence().addStepsForBuilder(myBuilders.get(category), context, modulesProvider);
+    for (TemplatesGroup templatesGroup : myTemplatesMap.keySet()) {
+      ModuleBuilder builder = templatesGroup.getModuleBuilder();
+      if (builder != null) {
+        myWizard.getSequence().addStepsForBuilder(builder, context, modulesProvider);
+      }
+      for (ProjectTemplate template : myTemplatesMap.get(templatesGroup)) {
+        myWizard.getSequence().addStepsForBuilder(myBuilders.get(template), context, modulesProvider);
+      }
     }
 
     final String groupId = PropertiesComponent.getInstance().getValue(PROJECT_WIZARD_GROUP);
