2070864b8063bfcecbfad3862b7df89bd62a5922
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-01-31 13:17:08 +0400

    test to choose overloaded method to detect target type

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/GraphInferenceHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/GraphInferenceHighlightingTest.java
@@ -116,6 +116,10 @@ public class GraphInferenceHighlightingTest extends LightDaemonAnalyzerTestCase 
     doTest();
   }
 
+  public void testTargetTypeByOverloadedMethod() throws Exception {
+    doTest();
+  }
+
   private void doTest() throws Exception {
     doTest(false);
   }
