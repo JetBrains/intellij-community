3529094fd2ec4935b0f5db339ad34963239fa878
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2014-02-04 14:15:41 +0400

    Get rid of grandparent for alignment.

--- python/src/com/jetbrains/python/formatter/PyBlock.java
+++ python/src/com/jetbrains/python/formatter/PyBlock.java
@@ -142,22 +142,29 @@ public class PyBlock implements ASTBlock {
     Indent childIndent = Indent.getNoneIndent();
     Alignment childAlignment = null;
 
-    //First we are going to set-up possible alignment to parents
-    PyBlock p = !breaksAlignment(parentType) ? myParent : null;
-    while (p != null) {
-      ASTNode pNode = p.getNode();
-      if (ourListElementTypes.contains(pNode.getElementType())) {
-        if (needListAlignment(child) && !isEmptyList(_node.getPsi()) && !isInControlStatement()) {
+    if (parentType == PyElementTypes.BINARY_EXPRESSION && !isInControlStatement()) {
+      //Setup alignments for binary expression
+      childAlignment = getAlignmentForChildren();
 
+      PyBlock p = myParent; //Check grandparents
+      while (p != null) {
+        ASTNode pNode = p.getNode();
+        if (ourListElementTypes.contains(pNode.getElementType())) {
+          if (needListAlignment(child) && !isEmptyList(_node.getPsi())) {
+
+            childAlignment = p.getChildAlignment();
+            break;
+          }
+        }
+        else if (pNode == PyElementTypes.BINARY_EXPRESSION) {
           childAlignment = p.getChildAlignment();
+        }
+        if (!breaksAlignment(pNode.getElementType())) {
+          p = p.myParent;
+        }
+        else {
           break;
         }
-      }
-      if (!breaksAlignment(pNode.getElementType())) {
-        p = p.myParent;
-      }
-      else {
-        break;
       }
     }
 
@@ -192,14 +199,6 @@ public class PyBlock implements ASTBlock {
         PyParenthesizedExpression parens = PsiTreeUtil.getParentOfType(_node.getPsi(), PyParenthesizedExpression.class, true,
                                                                        PyStatementPart.class);
         childIndent = parens != null ? Indent.getNormalIndent() : Indent.getContinuationIndent();
-      }
-      else {
-        if (grandparentType == PyElementTypes.BINARY_EXPRESSION && myParent != null) {
-          childAlignment = myParent.getAlignmentForChildren();
-        }
-        else {
-          childAlignment = getAlignmentForChildren();
-        }
       }
     }
 
