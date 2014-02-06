f761cebead50e54d65dcad52c7ae1bae4da420d3
Bas Leijdekkers <basleijdekkers@gmail.com>
2014-02-06 14:03:18 +0100

    better warning message & more tests

--- plugins/InspectionGadgets/testsrc/com/siyeh/ig/performance/CollectionsMustHaveInitialCapacityInspectionTest.java
+++ plugins/InspectionGadgets/testsrc/com/siyeh/ig/performance/CollectionsMustHaveInitialCapacityInspectionTest.java
@@ -31,11 +31,55 @@ public class CollectionsMustHaveInitialCapacityInspectionTest extends LightInspe
   protected String[] getEnvironmentClasses() {
     return new String[] {
       "package java.util.concurrent;" +
-      "public class ConcurrentHashMap {}"
+      "public class ConcurrentHashMap {}",
+      "package java.util;" +
+      "public class WeakHashMap {" +
+      "  public WeakHashMap() {}" +
+      "  public WeakHashMap(int c) {}" +
+      "}",
+      "package java.util;" +
+      "public class HashSet {" +
+      "  public HashSet() {}" +
+      "  public HashSet(int c) {}" +
+      "}",
+      "package java.util;" +
+      "public class BitSet {" +
+      "  public BitSet() {}" +
+      "  public BitSet(int c) {}" +
+      "}",
+      "package java.util;" +
+      "public class Vector {" +
+      "  public Vector() {}" +
+      "  public Vector(int c) {}" +
+      "}"
     };
   }
 
   public void testSimple() {
-    doStatementTest("new /*'java.util.concurrent.ConcurrentHashMap' without initial capacity*/java.util.concurrent.ConcurrentHashMap/**/();");
+    doStatementTest("new /*'new java.util.concurrent.ConcurrentHashMap()' without initial capacity*/java.util.concurrent.ConcurrentHashMap/**/();");
+  }
+
+  public void testMore() {
+    doTest("import java.util.*;" +
+           "class X {" +
+           "  void m() {" +
+           "    new /*'new HashMap<String, String>()' without initial capacity*/HashMap<String, String>/**/();" +
+           "    new HashMap<String, String>(3);" +
+           "    new /*'new HashMap()' without initial capacity*/HashMap/**/();" +
+           "    new HashMap(3);" +
+           "    new /*'new WeakHashMap()' without initial capacity*/WeakHashMap/**/();" +
+           "    new WeakHashMap(3);" +
+           "    new /*'new HashSet()' without initial capacity*/HashSet/**/();" +
+           "    new HashSet(3);" +
+           "    new /*'new Hashtable()' without initial capacity*/Hashtable/**/();" +
+           "    new Hashtable(3);" +
+           "    new /*'new BitSet()' without initial capacity*/BitSet/**/();" +
+           "    new BitSet(3);" +
+           "    new /*'new Vector()' without initial capacity*/Vector/**/();" +
+           "    new Vector(3);" +
+           "    new /*'new ArrayList()' without initial capacity*/ArrayList/**/();" +
+           "    new ArrayList(3);" +
+           "  }" +
+           "}");
   }
 }
