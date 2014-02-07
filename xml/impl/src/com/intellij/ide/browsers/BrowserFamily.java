aecf185bba87196c910af428fb421da45b04a1d2
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-02-07 11:54:12 +0100

    prepare to move UrlOpener to platform-api

--- xml/impl/src/com/intellij/ide/browsers/BrowserFamily.java
+++ xml/impl/src/com/intellij/ide/browsers/BrowserFamily.java
@@ -1,32 +1,32 @@
 package com.intellij.ide.browsers;
 
 import com.intellij.icons.AllIcons;
+import com.intellij.ide.IdeBundle;
 import com.intellij.ide.browsers.chrome.ChromeSettings;
 import com.intellij.ide.browsers.firefox.FirefoxSettings;
 import com.intellij.openapi.util.Iconable;
 import com.intellij.openapi.util.SystemInfo;
-import com.intellij.xml.XmlBundle;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 
 public enum BrowserFamily implements Iconable {
-  CHROME(XmlBundle.message("browsers.chrome"), "chrome", "google-chrome", "Google Chrome", AllIcons.Xml.Browsers.Chrome16) {
+  CHROME(IdeBundle.message("browsers.chrome"), "chrome", "google-chrome", "Google Chrome", AllIcons.Xml.Browsers.Chrome16) {
     @Override
     public BrowserSpecificSettings createBrowserSpecificSettings() {
       return new ChromeSettings();
     }
   },
-  EXPLORER(XmlBundle.message("browsers.explorer"), "iexplore", null, null, AllIcons.Xml.Browsers.Explorer16),
-  FIREFOX(XmlBundle.message("browsers.firefox"), "firefox", "firefox", "Firefox", AllIcons.Xml.Browsers.Firefox16) {
+  EXPLORER(IdeBundle.message("browsers.explorer"), "iexplore", null, null, AllIcons.Xml.Browsers.Explorer16),
+  FIREFOX(IdeBundle.message("browsers.firefox"), "firefox", "firefox", "Firefox", AllIcons.Xml.Browsers.Firefox16) {
     @Override
     public BrowserSpecificSettings createBrowserSpecificSettings() {
       return new FirefoxSettings();
     }
   },
-  OPERA(XmlBundle.message("browsers.opera"), "opera", "opera", "Opera", AllIcons.Xml.Browsers.Opera16),
-  SAFARI(XmlBundle.message("browsers.safari"), "safari", null, "Safari", AllIcons.Xml.Browsers.Safari16);
+  OPERA(IdeBundle.message("browsers.opera"), "opera", "opera", "Opera", AllIcons.Xml.Browsers.Opera16),
+  SAFARI(IdeBundle.message("browsers.safari"), "safari", null, "Safari", AllIcons.Xml.Browsers.Safari16);
 
   private final String myName;
   private final String myWindowsPath;
