5358bd270eb5a963c11167ca6365523d7a8d72b2
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-02-07 19:09:29 +0100

    new inference: testdata for IDEA-120376

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewMethodRefHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewMethodRefHighlightingTest.java
@@ -149,6 +149,10 @@ public class NewMethodRefHighlightingTest extends LightDaemonAnalyzerTestCase {
     doTest();
   }
 
+  public void testIDEA120376() throws Exception {
+    doTest();
+  }
+
   private void doTest() {
     doTest(false);
   }
