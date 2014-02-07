539ccf88b13ff54340c2f037240b08a31e381b9c
Alexey Kudravtsev <cdr@intellij.com>
2014-02-07 13:10:28 +0400

    don't rely on SmartPOinterManager to load document

--- java/java-tests/testSrc/com/intellij/psi/StubAstSwitchTest.groovy
+++ java/java-tests/testSrc/com/intellij/psi/StubAstSwitchTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -89,7 +89,7 @@ class StubAstSwitchTest extends LightCodeInsightFixtureTestCase {
     def oldClass = JavaPsiFacade.getInstance(project).findClass("A", GlobalSearchScope.allScope(project))
     def pointer = SmartPointerManager.getInstance(project).createSmartPsiElementPointer(oldClass)
     
-    def document = FileDocumentManager.instance.getCachedDocument(file.virtualFile)
+    def document = FileDocumentManager.instance.getDocument(file.virtualFile)
     assert document
     assert file == PsiDocumentManager.getInstance(project).getCachedPsiFile(document)
     assert document == PsiDocumentManager.getInstance(project).getCachedDocument(file)
