58c879183e127882b4117ab724dff210ad30205f
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2014-02-10 17:08:14 +0400

    Gradle: war artifact/facet creation fix.
    related issues:
    http://youtrack.jetbrains.com/issue/IDEA-119187
    http://youtrack.jetbrains.com/issue/IDEA-119818
    http://youtrack.jetbrains.com/issue/IDEA-120407

--- plugins/gradle/src/org/jetbrains/plugins/gradle/model/data/WebConfigurationModelData.java
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/model/data/WebConfigurationModelData.java
@@ -20,13 +20,8 @@ import com.intellij.openapi.externalSystem.model.ProjectKeys;
 import com.intellij.openapi.externalSystem.model.ProjectSystemId;
 import com.intellij.openapi.externalSystem.model.project.AbstractExternalEntityData;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.io.File;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * @author Vladislav.Soroka
@@ -36,7 +31,8 @@ public class WebConfigurationModelData extends AbstractExternalEntityData {
   private static final long serialVersionUID = 1L;
 
   @NotNull
-  public static final Key<WebConfigurationModelData> KEY = Key.create(WebConfigurationModelData.class, ProjectKeys.LIBRARY_DEPENDENCY.getProcessingWeight() + 1);
+  public static final Key<WebConfigurationModelData> KEY =
+    Key.create(WebConfigurationModelData.class, ProjectKeys.LIBRARY_DEPENDENCY.getProcessingWeight() + 1);
 
   @NotNull
   private final List<War> myWars;
@@ -49,120 +45,6 @@ public class WebConfigurationModelData extends AbstractExternalEntityData {
   @NotNull
   public List<War> getWars() {
     return myWars;
-  }
-
-  public static class War {
-
-    @NotNull
-    private final String myName;
-
-    @NotNull
-    private final String myWebAppDirName;
-    @NotNull
-    private final File myWebAppDir;
-    @Nullable
-    private File myWebXml;
-    @NotNull
-    private Map<String, Set<String>> myWebRoots;
-    @NotNull
-    private Set<File> myClasspath;
-    @Nullable
-    private String myManifestContent;
-
-
-    public War(@NotNull String name, @NotNull String webAppDirName, @NotNull File webAppDir) {
-      myName = name;
-      myWebAppDirName = webAppDirName;
-      myWebAppDir = webAppDir;
-      myWebRoots = Collections.emptyMap();
-      myClasspath = Collections.emptySet();
-    }
-
-    @NotNull
-    public String getName() {
-      return myName;
-    }
-
-    @NotNull
-    public String getWebAppDirName() {
-      return myWebAppDirName;
-    }
-
-    @NotNull
-    public File getWebAppDir() {
-      return myWebAppDir;
-    }
-
-    public void setWebXml(@Nullable File webXml) {
-      myWebXml = webXml;
-    }
-
-    @Nullable
-    public File getWebXml() {
-      return myWebXml;
-    }
-
-    public void setWebRoots(@Nullable Map<String, Set<String>> webRoots) {
-      myWebRoots = webRoots == null ? Collections.<String, Set<String>>emptyMap() : webRoots;
-    }
-
-    @NotNull
-    public Map<String, Set<String>> getWebRoots() {
-      return myWebRoots;
-    }
-
-    public void setClasspath(@Nullable Set<File> classpath) {
-      myClasspath = classpath == null ? Collections.<File>emptySet() : classpath;
-    }
-
-    @NotNull
-    public Set<File> getClasspath() {
-      return myClasspath;
-    }
-
-    public void setManifestContent(@Nullable String manifestContent) {
-      myManifestContent = manifestContent;
-    }
-
-    @Nullable
-    public String getManifestContent() {
-      return myManifestContent;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) return true;
-      if (o == null || getClass() != o.getClass()) return false;
-      if (!super.equals(o)) return false;
-
-      War that = (War)o;
-
-      if (!myName.equals(that.myName)) return false;
-      if (!myWebAppDirName.equals(that.myWebAppDirName)) return false;
-      if (!myWebRoots.equals(that.myWebRoots)) return false;
-
-      return true;
-    }
-
-    @Override
-    public int hashCode() {
-      int result = super.hashCode();
-      result = 31 * result + myName.hashCode();
-      result = 31 * result + myWebAppDirName.hashCode();
-      result = 31 * result + myWebRoots.hashCode();
-      return result;
-    }
-
-    @Override
-    public String toString() {
-      return "War{" +
-             "myName='" + myName + '\'' +
-             ", myWebAppDirName='" + myWebAppDirName + '\'' +
-             ", myWebAppDir=" + myWebAppDir +
-             ", myWebXml=" + myWebXml +
-             ", myWebRoots=" + myWebRoots +
-             '}';
-    }
   }
 
   @Override
