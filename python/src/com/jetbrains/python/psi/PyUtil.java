7f3af36085c5a3f57c0cad49effbbf60ceb578d4
Matthew L Daniel <mdaniel@gmail.com>
2014-02-11 00:51:15 -0800

    Allow slots to be a reference expression
    This is a fix for ``8a038fa37``

--- python/src/com/jetbrains/python/psi/PyUtil.java
+++ python/src/com/jetbrains/python/psi/PyUtil.java
@@ -52,7 +52,6 @@ import com.intellij.util.IncorrectOperationException;
 import com.intellij.util.PlatformIcons;
 import com.intellij.util.SmartList;
 import com.intellij.util.containers.ContainerUtil;
-import com.intellij.util.containers.HashSet;
 import com.jetbrains.NotNullPredicate;
 import com.jetbrains.python.PyBundle;
 import com.jetbrains.python.PyNames;
@@ -959,6 +958,17 @@ public class PyUtil {
   public static List<String> strListValue(PyExpression value) {
     while (value instanceof PyParenthesizedExpression) {
       value = ((PyParenthesizedExpression)value).getContainedExpression();
+    }
+    if (value instanceof PyReferenceExpression) {
+      PyReferenceExpression refExpr = (PyReferenceExpression)value;
+      PsiElement deref = refExpr.getReference().resolve();
+      if (deref instanceof PyTargetExpression) {
+        PyTargetExpression te = (PyTargetExpression)deref;
+        PyExpression assignedValue = te.findAssignedValue();
+        if (assignedValue instanceof PySequenceExpression) {
+          value = assignedValue;
+        }
+      }
     }
     if (value instanceof PySequenceExpression) {
       final PyExpression[] elements = ((PySequenceExpression)value).getElements();
