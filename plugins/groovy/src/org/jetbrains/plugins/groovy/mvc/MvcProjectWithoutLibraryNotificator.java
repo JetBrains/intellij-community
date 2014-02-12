eb2683da0420acec4eb575c5b33d5ae1e8be7439
peter <peter@jetbrains.com>
2014-02-12 20:07:36 +0100

    make MvcProjectWithoutLibraryNotificator read action cancellable

--- plugins/groovy/src/org/jetbrains/plugins/groovy/mvc/MvcProjectWithoutLibraryNotificator.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/mvc/MvcProjectWithoutLibraryNotificator.java
@@ -18,17 +18,16 @@ package org.jetbrains.plugins.groovy.mvc;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationListener;
 import com.intellij.notification.NotificationType;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.util.ProgressIndicatorUtils;
+import com.intellij.openapi.progress.util.ReadTask;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.CommonClassNames;
-import com.intellij.psi.JavaPsiFacade;
-import com.intellij.psi.search.GlobalSearchScope;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -40,40 +39,39 @@ import javax.swing.event.HyperlinkEvent;
 public class MvcProjectWithoutLibraryNotificator implements StartupActivity, DumbAware {
   @Override
   public void runActivity(@NotNull final Project project) {
-    ApplicationManager.getApplication().executeOnPooledThread(new Runnable() {
+    ProgressIndicatorUtils.scheduleWithWriteActionPriority(new ReadTask() {
       @Override
-      public void run() {
-        ApplicationManager.getApplication().runReadAction(new Runnable() {
-          @Override
-          public void run() {
-            if (project.isDisposed() || JavaPsiFacade.getInstance(project).findClass(CommonClassNames.JAVA_LANG_OBJECT, GlobalSearchScope.allScope(project)) == null) {
-              return; // If indexes is corrupted JavaPsiFacade.findClass() can't find classes during StartupActivity (may be it's a bug).
-              // So we can't determine whether exists Grails library or not.
-            }
+      public void computeInReadAction(@NotNull ProgressIndicator indicator) {
+        if (project.isDisposed()) {
+          return; 
+        }
 
-            Pair<Module, MvcFramework> pair = findModuleWithoutLibrary(project);
+        Pair<Module, MvcFramework> pair = findModuleWithoutLibrary(project);
 
-            if (pair != null) {
-              final MvcFramework framework = pair.second;
-              final Module module = pair.first;
+        if (pair != null) {
+          final MvcFramework framework = pair.second;
+          final Module module = pair.first;
 
-              new Notification(framework.getFrameworkName() + ".Configure",
-                               framework.getFrameworkName() + " SDK not found.",
-                               "<html><body>Module '" +
-                               module.getName() +
-                               "' has no " +
-                               framework.getFrameworkName() +
-                               " SDK. <a href='create'>Configure SDK</a></body></html>", NotificationType.INFORMATION,
-                               new NotificationListener.Adapter() {
-                                 @Override
-                                 protected void hyperlinkActivated(@NotNull Notification notification, @NotNull HyperlinkEvent e) {
-                                   MvcConfigureNotification.configure(framework, module);
-                                 }
+          String name = framework.getFrameworkName();
+          String content = "<html><body>Module '" + module.getName() + "' has no " + name + " SDK. <a href='create'>Configure SDK</a></body></html>";
+          new Notification(name + ".Configure",
+                           name + " SDK not found",
+                           content, NotificationType.INFORMATION,
+                           new NotificationListener.Adapter() {
+                             @Override
+                             protected void hyperlinkActivated(@NotNull Notification notification, @NotNull HyperlinkEvent e) {
+                               if (!module.isDisposed()) {
+                                 MvcConfigureNotification.configure(framework, module);
                                }
-              ).notify(project);
-            }
-          }
-        });
+                             }
+                           }
+          ).notify(project);
+        }
+      }
+
+      @Override
+      public void onCanceled(@NotNull ProgressIndicator indicator) {
+        ProgressIndicatorUtils.scheduleWithWriteActionPriority(this);
       }
     });
   }
