c76e36729b9be3224b4d2f3acd0b2ac37a6b9b14
Bas Leijdekkers <basleijdekkers@gmail.com>
2014-02-13 14:05:31 +0100

    IDEA-120623 (Add "org.mockito.InOrder" to the list of known classes for "JUnit test method without any assertions")

--- plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/junit/TestMethodWithoutAssertionInspectionBase.java
+++ plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/junit/TestMethodWithoutAssertionInspectionBase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,6 +45,7 @@ public class TestMethodWithoutAssertionInspectionBase extends BaseInspection {
     "org.junit.Assert,assert.*|fail.*," +
     "junit.framework.Assert,assert.*|fail.*," +
     "org.mockito.Mockito,verify.*," +
+    "org.mockito.InOrder,verify," +
     "org.junit.rules.ExpectedException,expect.*," +
     "org.hamcrest.MatcherAssert,assertThat";
   @SuppressWarnings({"PublicField"})
