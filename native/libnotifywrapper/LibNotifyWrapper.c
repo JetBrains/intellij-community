ad6e6ee9513b9ae234f843d5d76abf88b2a0e305
Tuomas Tynkkynen <dezgeg@gmail.com>
2014-02-18 22:36:02 +0200

    libnotifywrapper: Fix crashes when showing notification
    Function name of "notify_uninit" was typoed to "notify_init" in the
    dlsym call, which would cause a segfault on at least Ubuntu 12.04.
    
    Also, each notification would cause libnotify.so to be dlopened
    and dlclosed again. This caused segfaults as libnotify uses GObject,
    which can't be reloaded after being unloaded
    (https://bugzilla.gnome.org/show_bug.cgi?id=705535).

--- native/libnotifywrapper/LibNotifyWrapper.c
+++ native/libnotifywrapper/LibNotifyWrapper.c
@@ -1,6 +1,22 @@
 #include "com_intellij_ui_LibNotifyWrapper.h"
 #include <dlfcn.h>
 
+static void *libnotify_handle;
+static gboolean (*notify_init_func)(const char *app_name);
+static void (*notify_uninit_func)();
+static NotifyNotification* (*notify_notification_new_func) (const char *summary,
+                                                            const char *body,
+                                                            const char *icon);
+static gboolean (*notify_notification_show_func) (NotifyNotification *notification,
+                                                  GError **error);
+
+JNIEXPORT void JNI_OnUnload(JavaVM *vm, void *reserved) {
+  if (libnotify_handle != NULL) {
+    (*notify_uninit_func)();
+    dlclose(libnotify_handle);
+  }
+}
+
 /*
  * Class:     com_intellij_ui_LibNotifyWrapper
  * Method:    showNotification
@@ -9,44 +25,35 @@
 JNIEXPORT void JNICALL Java_com_intellij_ui_LibNotifyWrapper_showNotification
   (JNIEnv * jEnv, jclass c, jstring jTitle, jstring jDescription, jstring jLogoPath) {
 
-  void *handle;
-  gboolean (*notify_init_func)(const char *app_name);
-  void (*notify_uninit_func)();
-  NotifyNotification* (*notify_notification_new_func) (const char *summary,
-                                                         const char *body,
-                                                         const char *icon);
-  gboolean (*notify_notification_show_func) (NotifyNotification *notification,
-                                                         GError **error);
-  char *error;
+  if (libnotify_handle == NULL) {
+    char *error;
 
-  dlerror();
+    dlerror();
 
-  handle = dlopen("libnotify.so", RTLD_LAZY);
+    libnotify_handle = dlopen("libnotify.so", RTLD_LAZY);
 
-  if ((error = dlerror()) != NULL)  
-  {
-    (*jEnv)->ThrowNew(jEnv, (*jEnv)->FindClass(jEnv, "java/lang/UnsatisfiedLinkError"), error);
-    return;
+    if ((error = dlerror()) != NULL)
+    {
+      (*jEnv)->ThrowNew(jEnv, (*jEnv)->FindClass(jEnv, "java/lang/UnsatisfiedLinkError"), error);
+      return;
+    }
+
+    dlerror();
+
+    *(void **) (&notify_init_func) = dlsym(libnotify_handle, "notify_init");
+    *(void **) (&notify_uninit_func) = dlsym(libnotify_handle, "notify_uninit");
+    *(void **) (&notify_notification_new_func) = dlsym(libnotify_handle, "notify_notification_new");
+    *(void **) (&notify_notification_show_func) = dlsym(libnotify_handle, "notify_notification_show");
+
+    (*notify_init_func) ("JetBrains");
   }
-
-  dlerror();
- 
- *(void **) (&notify_init_func) = dlsym(handle, "notify_init");
- *(void **) (&notify_uninit_func) = dlsym(handle, "notify_init");
- *(void **) (&notify_notification_new_func) = dlsym(handle, "notify_notification_new");
- *(void **) (&notify_notification_show_func) = dlsym(handle, "notify_notification_show");
-
-  (*notify_init_func) ("JetBrains");
 
   const char *title = (*jEnv)->GetStringUTFChars(jEnv, jTitle, 0);
   const char *description = (*jEnv)->GetStringUTFChars(jEnv, jDescription, 0);
   const char *logoPath = (*jEnv)->GetStringUTFChars(jEnv, jLogoPath, 0);
   NotifyNotification * notification = (*notify_notification_new_func) (title, description, logoPath);
   (*notify_notification_show_func) (notification, NULL);
-  (*notify_uninit_func)();
   (*jEnv)->ReleaseStringUTFChars(jEnv, jTitle, title);
   (*jEnv)->ReleaseStringUTFChars(jEnv, jDescription, description);
   (*jEnv)->ReleaseStringUTFChars(jEnv, jLogoPath, logoPath);
-
-  dlclose(handle);
 }
