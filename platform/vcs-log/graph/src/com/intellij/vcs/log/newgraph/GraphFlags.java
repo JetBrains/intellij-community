413a6a961a7e5333bbe60d5a277d492dfeab5f70
Erokhin Stanislav <erokhins239@gmail.com>
2014-02-18 21:10:14 +0400

    [log] Add GraphFlags

---
+++ platform/vcs-log/graph/src/com/intellij/vcs/log/newgraph/GraphFlags.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2000-2014 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intellij.vcs.log.newgraph;
+
+import com.intellij.vcs.log.newgraph.utils.Flags;
+
+public class GraphFlags {
+  private final int myNodesCount;
+  private final byte[] myFlags;
+
+  public GraphFlags(int nodesCount) {
+    myNodesCount = nodesCount;
+    myFlags = new byte[nodesCount];
+  }
+
+  private Flags getFlags(int offset) {
+    final int mask = 1 << offset;
+    return new Flags() {
+      @Override
+      public int size() {
+        return myNodesCount;
+      }
+
+      @Override
+      public boolean get(int index) {
+        return (myFlags[index] & mask) != 0;
+      }
+
+      @Override
+      public void set(int index, boolean value) {
+        if (value) {
+          myFlags[index] |= mask;
+        } else {
+          myFlags[index] &= ~mask;
+        }
+      }
+    };
+  }
+
+  public Flags getHideNodesFlags() {
+    return getFlags(1);
+  }
+
+  // From node k exist only one edge to k + 1
+  public Flags getSimpleNodeFlags() {
+    return getFlags(0);
+  }
+}
