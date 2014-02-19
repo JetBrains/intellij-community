5f12f65a04b68a5477161ffa711c46d7435ab493
Alexey Kudravtsev <cdr@intellij.com>
2014-02-19 18:18:04 +0400

    moved to editor

--- platform/editor-ui-api/src/com/intellij/ui/PlaceHolder.java
+++ platform/editor-ui-api/src/com/intellij/ui/PlaceHolder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2011 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,9 +15,11 @@
  */
 package com.intellij.ui;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * @author Konstantin Bulenkov
  */
 public interface PlaceHolder<Place> extends PlaceProvider<Place> {
-  void setPlace(Place place);
+  void setPlace(@NotNull Place place);
 }
