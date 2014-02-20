a5f7dd7ff70f0326c839482f4ba7c00fb132625e
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-02-20 14:35:20 +0100

    accidentally committed — revert

--- platform/editor-ui-ex/src/com/intellij/openapi/editor/impl/RangeHighlighterImpl.java
+++ platform/editor-ui-ex/src/com/intellij/openapi/editor/impl/RangeHighlighterImpl.java
@@ -221,7 +221,7 @@ class RangeHighlighterImpl extends RangeMarkerImpl implements RangeHighlighterEx
 
   @Override
   public int getAffectedAreaEndOffset() {
-    return getEndOffset();
+    return getData().getAffectedAreaEndOffset();
   }
 
   @Override
