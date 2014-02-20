f2ed5a62ca075a2636f548ef294585a85e061146
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2014-02-20 14:14:02 +0100

    Cleanup (platform tests moved to platform)

--- platform/testFramework/src/com/intellij/testFramework/fixtures/LightPlatformCodeInsightFixtureTestCase.java
+++ platform/testFramework/src/com/intellij/testFramework/fixtures/LightPlatformCodeInsightFixtureTestCase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,7 +40,8 @@ import java.io.File;
 public abstract class LightPlatformCodeInsightFixtureTestCase extends UsefulTestCase {
   @SuppressWarnings("JUnitTestCaseWithNonTrivialConstructors")
   public LightPlatformCodeInsightFixtureTestCase() {
-    PlatformTestCase.autodetectPlatformPrefix();
+    //PlatformTestCase.autodetectPlatformPrefix();
+    PlatformTestCase.initPlatformLangPrefix();
   }
 
   protected CodeInsightTestFixture myFixture;
