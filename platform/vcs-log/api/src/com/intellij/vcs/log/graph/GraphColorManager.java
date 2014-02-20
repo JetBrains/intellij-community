a00646d58e48dc656d8c9d74bbdffd45eb229ac4
Kirill Likhodedov <Kirill.Likhodedov@gmail.com>
2014-02-20 16:55:21 +0400

    GraphColorManager: more methods + javadocs

--- platform/vcs-log/api/src/com/intellij/vcs/log/graph/GraphColorManager.java
+++ platform/vcs-log/api/src/com/intellij/vcs/log/graph/GraphColorManager.java
@@ -20,8 +20,25 @@ import org.jetbrains.annotations.NotNull;
 
 public interface GraphColorManager {
 
+  /**
+   * Returns the color which should be used to draw the given branch.
+   * @param headCommit branch head commit index.
+   */
   @NotNull
   JBColor getColorOfBranch(int headCommit);
+
+  /**
+   * Returns the color which should be used to draw a not-main fragment of a branch
+   * (e.g. there was master, then I've checked out a feature, made some commits and merged back to master - these my commits form such
+   * a fragment which is the same branch in terms of VCS, but is a separate branch in terms of graph,
+   * and therefore we may want to color it separately.
+   *
+   * @param headCommit commit of the branch head which the fragment belongs to.
+   * @param magicIndex some magic index identifying the fragment (we don't know which one - it is some Graph internal thing).
+   * @return the color which should be used to draw this fragment.
+   */
+  @NotNull
+  JBColor getColorOfFragment(int headCommit, int magicIndex);
 
   /**
    * Returns 1, 0 or -1 if branch identified by commit {@code head1} is "more powerful", "equally powerful" or "less powerful"
