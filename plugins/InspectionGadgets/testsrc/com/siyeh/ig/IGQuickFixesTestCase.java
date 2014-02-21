6cc57f84849f73e337b50943bef4c0390b248a44
Bas Leijdekkers <basleijdekkers@gmail.com>
2014-02-21 20:13:56 +0100

    EA-54183 (CCE: BooleanConstructorInspection$BooleanConstructorFix.doFix)

--- plugins/InspectionGadgets/testsrc/com/siyeh/ig/IGQuickFixesTestCase.java
+++ plugins/InspectionGadgets/testsrc/com/siyeh/ig/IGQuickFixesTestCase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,10 +16,14 @@
 package com.siyeh.ig;
 
 import com.intellij.codeInsight.intention.IntentionAction;
+import com.intellij.ide.highlighter.JavaFileType;
 import com.intellij.openapi.application.PluginPathManager;
 import com.intellij.pom.java.LanguageLevel;
 import com.intellij.testFramework.builders.JavaModuleFixtureBuilder;
 import com.intellij.testFramework.fixtures.JavaCodeInsightFixtureTestCase;
+import org.intellij.lang.annotations.Language;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author anna
@@ -29,6 +33,20 @@ import com.intellij.testFramework.fixtures.JavaCodeInsightFixtureTestCase;
 public abstract class IGQuickFixesTestCase extends JavaCodeInsightFixtureTestCase {
   protected String myDefaultHint = null;
   protected String myRelativePath = null;
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+
+    final BaseInspection inspection = getInspection();
+    if (inspection != null) {
+      myFixture.enableInspections(inspection);
+    }
+  }
+
+  protected BaseInspection getInspection() {
+    return null;
+  }
 
   @Override
   protected void tuneFixture(final JavaModuleFixtureBuilder builder) throws Exception {
@@ -57,6 +75,29 @@ public abstract class IGQuickFixesTestCase extends JavaCodeInsightFixtureTestCas
     assertNotNull(action);
     myFixture.launchAction(action);
     myFixture.checkResultByFile(getRelativePath() + "/" + testName + ".after.java");
+  }
+
+  protected void doExpressionTest(
+    String hint,
+    @Language(value = "JAVA", prefix = "class X {{System.out.print(", suffix = ");}}") @NotNull @NonNls String before,
+    @Language(value = "JAVA", prefix = "class X {{System.out.print(", suffix = ");}}") @NotNull @NonNls String after) {
+    doTest(hint, "class X {{System.out.print(" + before + ");}}", "class X {{System.out.print(" + after + ");}}");
+  }
+
+  protected void doMemberTest(
+    String hint,
+    @Language(value = "JAVA", prefix = "class X {", suffix = "}") @NotNull @NonNls String before,
+    @Language(value = "JAVA", prefix = "class X {", suffix = "}") @NotNull @NonNls String after) {
+    doTest(hint, "class X {" + before + "}", "class X {" + after + "}");
+  }
+
+  protected void doTest(String hint, @Language("JAVA") @NotNull @NonNls String before, @Language("JAVA") @NotNull @NonNls String after) {
+    before = before.replace("/**/", "<caret>");
+    myFixture.configureByText(JavaFileType.INSTANCE, before);
+    final IntentionAction intention = myFixture.findSingleIntention(hint);
+    assertNotNull(intention);
+    myFixture.launchAction(intention);
+    myFixture.checkResult(after);
   }
 
   protected String getRelativePath() {
