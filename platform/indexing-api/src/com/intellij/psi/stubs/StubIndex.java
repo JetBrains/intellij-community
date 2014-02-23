e52b01a70454e1b785d6a84e75a4e1cf919d3439
peter <peter@jetbrains.com>
2014-02-23 21:02:39 +0100

    an API to avoid CCEs when doing StubIndex.process

--- platform/indexing-api/src/com/intellij/psi/stubs/StubIndex.java
+++ platform/indexing-api/src/com/intellij/psi/stubs/StubIndex.java
@@ -64,10 +64,20 @@ public abstract class StubIndex {
     return get(indexKey, key, project, scope);
   }
 
-  public abstract <Key, Psi extends PsiElement> boolean process(@NotNull StubIndexKey<Key, Psi> indexKey,
+  @Deprecated
+  public <Key, Psi extends PsiElement> boolean process(@NotNull StubIndexKey<Key, Psi> indexKey,
                                                                 @NotNull Key key,
                                                                 @NotNull Project project,
                                                                 GlobalSearchScope scope,
+                                                                @NotNull Processor<? super Psi> processor) {
+    return processElements(indexKey, key, project, scope, (Class<Psi>)PsiElement.class, processor);
+  }
+
+  public abstract <Key, Psi extends PsiElement> boolean processElements(@NotNull StubIndexKey<Key, Psi> indexKey,
+                                                                @NotNull Key key,
+                                                                @NotNull Project project,
+                                                                GlobalSearchScope scope,
+                                                                Class<Psi> requiredClass,
                                                                 @NotNull Processor<? super Psi> processor);
 
   public <Key, Psi extends PsiElement> boolean process(@NotNull StubIndexKey<Key, Psi> indexKey,
@@ -75,6 +85,16 @@ public abstract class StubIndex {
                                                                 @NotNull Project project,
                                                                 GlobalSearchScope scope,
                                                                 IdFilter idFilter,
+                                                                @NotNull Processor<? super Psi> processor) {
+    return process(indexKey, key, project, scope, processor);
+  }
+
+  public <Key, Psi extends PsiElement> boolean processElements(@NotNull StubIndexKey<Key, Psi> indexKey,
+                                                                @NotNull Key key,
+                                                                @NotNull Project project,
+                                                                GlobalSearchScope scope,
+                                                                IdFilter idFilter,
+                                                                Class<Psi> requiredClass,
                                                                 @NotNull Processor<? super Psi> processor) {
     return process(indexKey, key, project, scope, processor);
   }
