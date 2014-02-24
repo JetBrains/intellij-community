e422a99a16a9cb660a20626e98f38da3d3124044
Kirill Likhodedov <Kirill.Likhodedov@gmail.com>
2014-02-24 16:40:01 +0400

    [vcs] Fix already disposed exception
    It was thrown from UpdatingChangeListBuilder.isExcluded because
    checkIsDisposed just checked the updater's stopped-status,
    but not the disposal state.

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ChangeListManagerImpl.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ChangeListManagerImpl.java
@@ -579,8 +579,14 @@ public class ChangeListManagerImpl extends ChangeListManagerEx implements Projec
   private void iterateScopes(DataHolder dataHolder, List<VcsDirtyScope> scopes, boolean wasEverythingDirty) {
     final ChangeListManagerGate gate = dataHolder.getChangeListWorker().createSelfGate();
     // do actual requests about file statuses
+    Getter<Boolean> disposedGetter = new Getter<Boolean>() {
+      @Override
+      public Boolean get() {
+        return myProject.isDisposed() || myUpdater.getIsStoppedGetter().get();
+      }
+    };
     final UpdatingChangeListBuilder builder = new UpdatingChangeListBuilder(dataHolder.getChangeListWorker(),
-      dataHolder.getComposite(), myUpdater.getIsStoppedGetter(), myIgnoredIdeaLevel, gate);
+      dataHolder.getComposite(), disposedGetter, myIgnoredIdeaLevel, gate);
 
     for (final VcsDirtyScope scope : scopes) {
       myUpdateChangesProgressIndicator.checkCanceled();
