e796dc6465610133f7fa85bfeb375da34d6f5198
Konstantin Bulenkov <kb@jetbrains.com>
2014-02-25 03:12:10 +0100

    make myPressPointScreen accessible in processDragFinish

--- platform/platform-api/src/com/intellij/ui/MouseDragHelper.java
+++ platform/platform-api/src/com/intellij/ui/MouseDragHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,7 +37,7 @@ public abstract class MouseDragHelper implements MouseListener, MouseMotionListe
 
   private final JComponent myDragComponent;
 
-  private Point myPressPointScreen;
+  protected Point myPressPointScreen;
   private Point myPressPointComponent;
 
   private boolean myDraggingNow;
@@ -131,7 +131,6 @@ public abstract class MouseDragHelper implements MouseListener, MouseMotionListe
       return;
     }
     boolean wasDragging = myDraggingNow;
-    myPressPointScreen = null;
     myDraggingNow = false;
     myDragJustStarted = false;
 
@@ -144,6 +143,7 @@ public abstract class MouseDragHelper implements MouseListener, MouseMotionListe
         }
       }
       finally {
+        myPressPointScreen = null;
         resetDragState();
         e.consume();
         if (myDetachPostponed) {
