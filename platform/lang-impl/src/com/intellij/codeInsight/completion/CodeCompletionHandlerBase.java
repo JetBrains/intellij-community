c074bd25dcc051734847e6df06c950ebd33d7507
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2014-02-25 12:12:45 +0400

    1-to-1 mapping between Caret instances for injected and main editors (IDEA-80056)
    allows to compare Caret instances using == in generic code

--- platform/lang-impl/src/com/intellij/codeInsight/completion/CodeCompletionHandlerBase.java
+++ platform/lang-impl/src/com/intellij/codeInsight/completion/CodeCompletionHandlerBase.java
@@ -628,7 +628,7 @@ public class CodeCompletionHandlerBase {
         @Override
         public void perform(Caret caret) {
           CompletionAssertions.WatchingInsertionContext currentContext = insertItem(indicator, item, completionChar, items, update, editor, caret.getOffset(), caret.getOffset() + idEndOffsetDelta);
-          if (caret.getVisualPosition().equals(editor.getCaretModel().getPrimaryCaret().getVisualPosition())) {
+          if (caret == editor.getCaretModel().getPrimaryCaret()) {
             contextRef.set(currentContext);
           }
         }
