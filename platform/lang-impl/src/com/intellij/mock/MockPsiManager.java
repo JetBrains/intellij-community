8248670085eba04064d0fba0672addef9038c17e
Alexey Kudravtsev <cdr@intellij.com>
2014-02-25 13:30:04 +0400

    notnull

--- platform/lang-impl/src/com/intellij/mock/MockPsiManager.java
+++ platform/lang-impl/src/com/intellij/mock/MockPsiManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -84,7 +84,7 @@ public class MockPsiManager extends PsiManagerEx {
   }
 
   @Override
-  public void addPsiTreeChangeListener(@NotNull PsiTreeChangeListener listener, Disposable parentDisposable) {
+  public void addPsiTreeChangeListener(@NotNull PsiTreeChangeListener listener, @NotNull Disposable parentDisposable) {
   }
 
   @Override
