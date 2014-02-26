7788a2f14cf1a2b9240bb4e040797ee8a8e3c7f2
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2014-02-26 17:46:32 +0400

    PY-12173 Extract Superclass: warn about conflicting names

--- python/src/com/jetbrains/python/refactoring/classes/extractSuperclass/PyExtractSuperclassPresenterImpl.java
+++ python/src/com/jetbrains/python/refactoring/classes/extractSuperclass/PyExtractSuperclassPresenterImpl.java
@@ -6,13 +6,17 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
 import com.intellij.refactoring.RefactoringBundle;
 import com.intellij.refactoring.classMembers.MemberInfoModel;
 import com.jetbrains.python.PyBundle;
 import com.jetbrains.python.PythonLanguage;
 import com.jetbrains.python.psi.PyClass;
 import com.jetbrains.python.psi.PyElement;
+import com.jetbrains.python.psi.PyFile;
 import com.jetbrains.python.psi.PyUtil;
 import com.jetbrains.python.refactoring.classes.PyMemberInfoStorage;
 import com.jetbrains.python.refactoring.classes.membersManager.PyMemberInfo;
@@ -47,8 +51,9 @@ class PyExtractSuperclassPresenterImpl extends MembersBasedPresenterNoPreviewImp
       throw new BadDataException(PyBundle.message("refactoring.extract.super.name.0.must.be.ident", myView.getSuperClassName()));
     }
     boolean rootFound = false;
+    final File moduleFile = new File(myView.getModuleFile());
     try {
-      final String targetDir = FileUtil.toSystemIndependentName(new File(myView.getModuleFile()).getCanonicalPath());
+      final String targetDir = FileUtil.toSystemIndependentName(moduleFile.getCanonicalPath());
       for (final VirtualFile file : ProjectRootManager.getInstance(project).getContentRoots()) {
         if (StringUtil.startsWithIgnoreCase(targetDir, file.getPath())) {
           rootFound = true;
@@ -61,6 +66,19 @@ class PyExtractSuperclassPresenterImpl extends MembersBasedPresenterNoPreviewImp
     if (!rootFound) {
       throw new BadDataException(PyBundle.message("refactoring.extract.super.target.path.outside.roots"));
     }
+
+    // TODO: Cover with test. It can't be done for now, because testFixture reports root path incorrectly
+    // PY-12173
+    myView.getModuleFile();
+    final VirtualFile moduleVirtualFile = LocalFileSystem.getInstance().findFileByIoFile(moduleFile);
+    if (moduleVirtualFile != null) {
+      final PsiFile psiFile = PsiManager.getInstance(project).findFile(moduleVirtualFile);
+      if (psiFile instanceof PyFile) {
+        if (((PyFile)psiFile).findTopLevelClass(myView.getSuperClassName()) != null) {
+          throw new BadDataException(PyBundle.message("refactoring.extract.super.target.class.already.exists", myView.getSuperClassName()));
+        }
+      }
+    }
   }
 
   @Override
@@ -71,9 +89,9 @@ class PyExtractSuperclassPresenterImpl extends MembersBasedPresenterNoPreviewImp
       PyUtil.filterOutObject(myStorage.getClassMemberInfos(myClassUnderRefactoring));
     myView.configure(
       new PyExtractSuperclassInitializationInfo(myModel, pyMemberInfos, defaultFilePath,
-                                                roots));
+                                                roots)
+    );
     myView.initAndShow();
-
   }
 
   @NotNull
