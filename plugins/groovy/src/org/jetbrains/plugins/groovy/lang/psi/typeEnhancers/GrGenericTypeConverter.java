18f2aee5ba7df93e8468ec0ac561d121baabfb03
Max Medvedev <maxim.medvedev@jetbrains.com>
2014-02-26 17:56:51 +0400

    optimize resolve. In some cases resolve() works faster than resolveGenerics (for example in LiteralClassType) and does not need to infer type parameters which can are needed only after some checks

--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/typeEnhancers/GrGenericTypeConverter.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/psi/typeEnhancers/GrGenericTypeConverter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2014 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jetbrains.plugins.groovy.lang.psi.typeEnhancers;
 
 import com.intellij.psi.*;
@@ -25,18 +40,17 @@ public class GrGenericTypeConverter extends GrTypeConverter {
       return null;
     }
 
-
-    PsiClassType.ClassResolveResult lresult = ((PsiClassType)ltype).resolveGenerics();
-    PsiClassType.ClassResolveResult rresult = ((PsiClassType)rtype).resolveGenerics();
-
-    PsiClass lclass = lresult.getElement();
-    PsiClass rclass = rresult.getElement();
+    PsiClass lclass = ((PsiClassType)ltype).resolve();
+    PsiClass rclass = ((PsiClassType)rtype).resolve();
 
     if (lclass == null || rclass == null) return null;
 
     if (lclass.getTypeParameters().length == 0) return null;
 
     if (!InheritanceUtil.isInheritorOrSelf(rclass, lclass, true)) return null;
+
+    PsiClassType.ClassResolveResult lresult = ((PsiClassType)ltype).resolveGenerics();
+    PsiClassType.ClassResolveResult rresult = ((PsiClassType)rtype).resolveGenerics();
 
     if (typeParametersAgree(lclass, rclass, lresult.getSubstitutor(), rresult.getSubstitutor(), context)) return Boolean.TRUE;
 
