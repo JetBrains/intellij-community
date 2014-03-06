8f8ef28b5f0f61ec848b1c5480f94e274ed127f4
Vassiliy <vassiliy.kudryashov@jetbrains.com>
2014-03-06 23:08:07 +0400

    IDEA-114457 Undocked 'project' tool window doesn't always automatically hide when I double-click to open a file Animation fix (don't freeze EDT, use Animator), request focus fix.

--- platform/platform-api/src/com/intellij/util/ui/FadeInFadeOut.java
+++ platform/platform-api/src/com/intellij/util/ui/FadeInFadeOut.java
@@ -27,6 +27,7 @@ import java.awt.image.BufferedImage;
 
 public class FadeInFadeOut extends JComponent {
   public static final Integer LAYER = new Integer(JLayeredPane.POPUP_LAYER + 1);
+  public static final int TOTAL_FRAMES = 10;
 
   @NotNull
   private final Component myComponent;
@@ -40,13 +41,14 @@ public class FadeInFadeOut extends JComponent {
   private final boolean myFadeIn;
 
   private boolean myTriggered = false;
-  private final int myTimeToComplete;
   private double myRatio = 0;
+  private final Animator myAnimator;
+  private Runnable myOnDone = null;
+
 
   public FadeInFadeOut(@NotNull Component component, int timeToComplete, boolean fadeIn, @NotNull Component icon) {
     setFocusable(false);
     myComponent = component;
-    myTimeToComplete = timeToComplete;
     myFadeIn = fadeIn;
     myRatio = myFadeIn ? 0 : 1;
     myIcon = icon;
@@ -55,6 +57,23 @@ public class FadeInFadeOut extends JComponent {
     Graphics2D graphics = myComponentImage.createGraphics();
     myComponent.paint(graphics);
     graphics.dispose();
+    myAnimator = new Animator("FadeInFadeOut", TOTAL_FRAMES, timeToComplete, false) {
+      @Override
+      public void paintNow(int frame, int totalFrames, int cycle) {
+        double linearProgress = Math.max(0, Math.min(1, (double)frame / totalFrames));
+        if (!myFadeIn) linearProgress = 1 - linearProgress;
+        myRatio = (1 - Math.cos(Math.PI * linearProgress)) / 2;
+        paintImmediately(0, 0, getWidth(), getHeight());
+      }
+
+      @Override
+      protected void paintCycleEnd() {
+        super.paintCycleEnd();
+        if (myOnDone != null) {
+          myOnDone.run();
+        }
+      }
+    };
   }
 
   @Override
@@ -66,25 +85,15 @@ public class FadeInFadeOut extends JComponent {
     }
   }
 
-  public void doAnimation() {
+  public void doAnimation(final Runnable onDone) {
+    myOnDone = onDone;
     if (myTriggered || !isShowing()) return;
 
     myTriggered = true;
-    long startTime = System.currentTimeMillis();
-    while (true) {
-      paintImmediately(0, 0, getWidth(), getHeight());
-      final long timeSpent = System.currentTimeMillis() - startTime;
-      if (timeSpent >= myTimeToComplete) {
-        return;
-      }
-      double linearProgress = Math.max(0, Math.min(1, (double)timeSpent / myTimeToComplete));
-      if (!myFadeIn) linearProgress = 1 - linearProgress;
-      myRatio = (1 - Math.cos(Math.PI * linearProgress)) / 2;
-    }
+    myAnimator.resume();
   }
 
   public final void paint(final Graphics g_) {
-    long l = System.currentTimeMillis();
     int x = (int)(myIconBounds.x * (1 - myRatio) + myComponentBounds.x * myRatio);
     int y = (int)(myIconBounds.y * (1 - myRatio) + myComponentBounds.y * myRatio);
     int width = (int)(myIconBounds.width * (1 - myRatio) + myComponentBounds.width * myRatio);
