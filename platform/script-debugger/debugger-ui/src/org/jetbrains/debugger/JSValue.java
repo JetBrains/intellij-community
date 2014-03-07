636252f821f6ffa8e27246dbff1e6bcaf65fb657
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-03-07 12:00:11 +0100

    continue decouple xdebugger ex ui — XValue impl
    (cherry picked from commit e4dab993db37dff4c97250ee6a872888c573258e)

---
+++ platform/script-debugger/debugger-ui/src/org/jetbrains/debugger/JSValue.java
@@ -0,0 +1,33 @@
+package org.jetbrains.debugger;
+
+import com.intellij.icons.AllIcons;
+import com.intellij.xdebugger.frame.XNamedValue;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public abstract class JSValue extends XNamedValue {
+  protected JSValue(@NotNull String name) {
+    super(name);
+  }
+
+  public abstract ValueType getValueType();
+
+  public boolean isDomPropertiesValue() {
+    return false;
+  }
+
+  @Nullable
+  protected Icon getIcon() {
+    ValueType type = getValueType();
+    switch (type) {
+      case FUNCTION:
+        return AllIcons.Nodes.Function;
+      case ARRAY:
+        return AllIcons.Debugger.Db_array;
+      default:
+        return type.isObjectType() ? AllIcons.Debugger.Value : AllIcons.Debugger.Db_primitive;
+    }
+  }
+}
