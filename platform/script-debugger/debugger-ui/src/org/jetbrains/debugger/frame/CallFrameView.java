d55b117483fe1c1a2976ee69cec34811248fb848
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-03-08 12:32:32 +0100

    rename DeclarativeScopeBase to DeclarativeScope
    (cherry picked from commit 6c56fcdce04a3368020bad968efe277c5e1db6aa)

--- platform/script-debugger/debugger-ui/src/org/jetbrains/debugger/frame/CallFrameView.java
+++ platform/script-debugger/debugger-ui/src/org/jetbrains/debugger/frame/CallFrameView.java
@@ -11,7 +11,7 @@ import org.jetbrains.debugger.*;
 
 import java.util.List;
 
-public class CallFrameView extends StackFrameImplBase implements VariableContext {
+public final class CallFrameView extends StackFrameImplBase implements VariableContext {
   private final DebugProcessEx debugProcess;
   private final CallFrame callFrame;
 
