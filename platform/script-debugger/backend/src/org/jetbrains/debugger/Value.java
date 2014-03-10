efbc4d77242c94dbf06c2ca406e436eb07044562
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-03-10 17:07:53 +0100

    cleanup
    (cherry picked from commit fdc3ccc4f8faf90224401d929e27a992de5341e0)

--- platform/script-debugger/backend/src/org/jetbrains/debugger/Value.java
+++ platform/script-debugger/backend/src/org/jetbrains/debugger/Value.java
@@ -1,17 +1,10 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 package org.jetbrains.debugger;
 
 import com.intellij.openapi.util.ActionCallback;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * An object that represents a browser JavaScript VM variable value (compound or atomic).
- * <p>In some backends values (currently only strings) are loaded with a size limit.
- * The value part that exceeds the limit is truncated. The full value
- * can be loaded by {@link #reloadHeavyValue} method.
+ * An object that represents a VM variable value (compound or atomic).
  */
 public interface Value extends EvaluateContextAdditionalParameter {
   @NotNull
