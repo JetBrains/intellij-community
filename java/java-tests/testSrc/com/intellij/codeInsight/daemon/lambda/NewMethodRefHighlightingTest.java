77017d7790b78c5bb20c591e2588c1eccd0f96d7
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-03-10 14:18:03 +0100

    new inference: reject varargs as method reference target

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewMethodRefHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewMethodRefHighlightingTest.java
@@ -189,6 +189,10 @@ public class NewMethodRefHighlightingTest extends LightDaemonAnalyzerTestCase {
     doTest();
   }
 
+  public void testAmbiguityVarargs() throws Exception {
+    doTest();
+  }
+
   private void doTest() {
     doTest(false);
   }
