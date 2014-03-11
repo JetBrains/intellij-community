0b772f8f72bee8f829176bf7c00a40d84e516718
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2014-03-12 03:16:33 +0400

    Don't format shebang only in first line of the script. Don't format comments that start with : and #.

--- python/src/com/jetbrains/python/formatter/PyPreFormatProcessor.java
+++ python/src/com/jetbrains/python/formatter/PyPreFormatProcessor.java
@@ -84,8 +84,12 @@ public class PyPreFormatProcessor implements PreFormatProcessor {
       String text = element.getText();
       int commentStart = text.indexOf('#');
       if (commentStart != -1 && (commentStart + 1) < text.length()) {
-        if (text.charAt(commentStart+1) == '!') {
+        char charAfterDash = text.charAt(commentStart + 1);
+        if (charAfterDash == '!' && element.getTextRange().getStartOffset() == 0) {
           return; //shebang
+        }
+        if (charAfterDash == '#' || charAfterDash == ':') {
+          return;
         }
         String commentText = StringUtil.trimLeading(text.substring(commentStart + 1));
 
