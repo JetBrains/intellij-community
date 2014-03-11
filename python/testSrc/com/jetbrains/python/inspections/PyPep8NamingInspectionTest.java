dcd744451399ac6757df14bc53144d26c9d48e25
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2014-03-11 13:34:35 +0400

    fixed PY-11981 "PEP 8 naming convention violation" inspection treats class attributes as function variables
    (cherry picked from commit b6b5a685e9a4c57fe2d630448cde9dadb3dd96c3)

--- python/testSrc/com/jetbrains/python/inspections/PyPep8NamingInspectionTest.java
+++ python/testSrc/com/jetbrains/python/inspections/PyPep8NamingInspectionTest.java
@@ -58,6 +58,10 @@ public class PyPep8NamingInspectionTest extends PyTestCase {
     doTest();
   }
 
+  public void testClassAttributes() {
+    doTest();
+  }
+
   public void testTest() {
     doTest();
   }
