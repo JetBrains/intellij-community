3666c1e43a82e5f0741801690e21703fb4cb0810
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-03-11 12:33:16 +0100

    cleanup
    (cherry picked from commit 4cfa1f7b1aa615ca6c324bcf5e3fe3d8c3689bfa)

--- platform/script-debugger/backend/src/org/jetbrains/debugger/ExceptionData.java
+++ platform/script-debugger/backend/src/org/jetbrains/debugger/ExceptionData.java
@@ -1,7 +1,3 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 package org.jetbrains.debugger;
 
 import com.intellij.util.ThreeState;
--- platform/script-debugger/backend/src/org/jetbrains/debugger/MapBackedEvaluateContextAdditionalParametersProvider.java
+++
@@ -1,4 +0,0 @@
-package org.jetbrains.debugger;
-
-public class MapBackedEvaluateContextAdditionalParametersProvider {
-}
