0fca26f75db5295f0a0d6a7d11d5907a9b6709d2
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-03-11 23:52:10 +0100

    nashorn indexed array support
    (cherry picked from commit d6a80f5563c42f838b97f89bdc08a787a65511e2)

--- platform/script-debugger/backend/src/org/jetbrains/debugger/ArrayValue.java
+++ platform/script-debugger/backend/src/org/jetbrains/debugger/ArrayValue.java
@@ -6,8 +6,17 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 
 public interface ArrayValue extends Value {
+  /**
+   * Be aware - it is not equals to java array length.
+   * In case of sparse array {@code
+   * var sparseArray = [3, 4];
+   * sparseArray[45] = 34;
+   * sparseArray[40999995] = "foo";
+   * }
+   * length will be equal to 40999995.
+   */
   int getLength();
 
   @NotNull
-  AsyncResult<List<Value>> getValues();
+  AsyncResult<List<Variable>> getVariables();
 }
