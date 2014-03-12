dae303ef70de3c83bab317b892843222ff4face3
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-03-12 13:05:32 +0100

    EvaluateContextBase

---
+++ platform/script-debugger/backend/src/org/jetbrains/debugger/EvaluateContextBase.java
@@ -0,0 +1,13 @@
+package org.jetbrains.debugger;
+
+import com.intellij.openapi.util.AsyncResult;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.debugger.values.Value;
+
+public abstract class EvaluateContextBase implements EvaluateContext {
+  @NotNull
+  @Override
+  public AsyncResult<Value> evaluate(@NotNull String expression) {
+    return evaluate(expression, null);
+  }
+}
