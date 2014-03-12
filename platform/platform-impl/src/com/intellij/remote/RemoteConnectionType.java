7e09953434c9f931a33b921a40fe69edbcd1e038
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2014-03-12 21:22:15 +0100

    Add javadoc for RemoteConnectionType.
    (cherry picked from commit a33ddd0d4d3730740ad4fd08ef0570d772155375)

--- platform/platform-impl/src/com/intellij/remote/RemoteConnectionType.java
+++ platform/platform-impl/src/com/intellij/remote/RemoteConnectionType.java
@@ -1,10 +1,31 @@
 package com.intellij.remote;
 
 /**
+ * This class denotes the type of the source to obtain remote credentials. 
+ * 
  * @author traff
  */
 public enum RemoteConnectionType {
-  DEFAULT_SDK, DEPLOYMENT_SERVER, REMOTE_SDK, CURRENT_VAGRANT, NONE;
+  /**
+   * Currently selected SDK (e.g. <Project default>) 
+   */
+  DEFAULT_SDK,
+  /**
+   * Web deployment server  
+   */
+  DEPLOYMENT_SERVER,
+  /**
+   * Remote SDK
+   */
+  REMOTE_SDK,
+  /**
+   * Current project vagrant
+   */
+  CURRENT_VAGRANT,
+  /**
+   * No source is predefined - it would be asked on request 
+   */
+  NONE;
 
   public static RemoteConnectionType findByName(String name) {
     try {
