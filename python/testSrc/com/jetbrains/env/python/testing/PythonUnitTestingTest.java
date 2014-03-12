7935816a66f29450cf87431b997c044f33d4b2e2
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2014-03-12 19:31:58 +0400

    moved unittest tests to env tests

--- python/testSrc/com/jetbrains/env/python/testing/PythonUnitTestingTest.java
+++ python/testSrc/com/jetbrains/env/python/testing/PythonUnitTestingTest.java
@@ -9,7 +9,7 @@ import junit.framework.Assert;
  */
 public class PythonUnitTestingTest extends PyEnvTestCase{
   public void testUTRunner() {
-    runPythonTest(new PyUnitTestTask("/testRunner/env", "test1.py") {
+    runPythonTest(new PyUnitTestTask("/testRunner/env/unit", "test1.py") {
 
       @Override
       public void after() {
@@ -21,13 +21,57 @@ public class PythonUnitTestingTest extends PyEnvTestCase{
   }
 
   public void testUTRunner2() {
-    runPythonTest(new PyUnitTestTask("/testRunner/env", "test2.py") {
+    runPythonTest(new PyUnitTestTask("/testRunner/env/unit", "test2.py") {
 
       @Override
       public void after() {
         assertEquals(3, allTestsCount());
         assertEquals(1, passedTestsCount());
         assertEquals(2, failedTestsCount());
+      }
+    });
+  }
+
+  public void testClass() {
+    runPythonTest(new PyUnitTestTask("/testRunner/env/unit", "test_file.py::GoodTest") {
+
+      @Override
+      public void after() {
+        assertEquals(1, allTestsCount());
+        assertEquals(1, passedTestsCount());
+      }
+    });
+  }
+
+  public void testMethod() {
+    runPythonTest(new PyUnitTestTask("/testRunner/env/unit", "test_file.py::GoodTest::test_passes") {
+
+      @Override
+      public void after() {
+        assertEquals(1, allTestsCount());
+        assertEquals(1, passedTestsCount());
+      }
+    });
+  }
+
+  public void testFolder() {
+    runPythonTest(new PyUnitTestTask("/testRunner/env/unit", "") {
+
+      @Override
+      public void after() {
+        assertEquals(9, allTestsCount());
+        assertEquals(6, passedTestsCount());
+      }
+    });
+  }
+
+  public void testDependent() {
+    runPythonTest(new PyUnitTestTask("/testRunner/env/unit", "dependentTests/test_my_class.py") {
+
+      @Override
+      public void after() {
+        assertEquals(1, allTestsCount());
+        assertEquals(1, passedTestsCount());
       }
     });
   }
