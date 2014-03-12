3c5eb934c85bbc2410ef5c034713d45e984d1571
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2014-03-12 19:04:04 +0400

    replace tuple with list quick fix for tuple item assignment inspection
    (cherry picked from commit db3f6c79b5075ceda71e30b9daf4611931922f69)

---
+++ python/testData/quickFixes/PyReplaceTupleWithListQuickFixTest/empty.py
@@ -0,0 +1,2 @@
+a = ()
+<warning descr="Tuples don't support item assignment">a[0] <caret>= 3</warning>
---
+++ python/testData/quickFixes/PyReplaceTupleWithListQuickFixTest/parenthesized.py
@@ -0,0 +1,2 @@
+a = ("11",)
+<warning descr="Tuples don't support item assignment">a[0]<caret> = 3</warning>
