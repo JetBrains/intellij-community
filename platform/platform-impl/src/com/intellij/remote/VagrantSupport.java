04c14189b74c31e2f4274782a3f90dfd8260449f
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2014-03-12 21:27:03 +0100

    Cleanup.
    (cherry picked from commit 85a331b0fb5babcd96f4b686a5db21db5352b270)

--- platform/platform-impl/src/com/intellij/remote/VagrantSupport.java
+++ platform/platform-impl/src/com/intellij/remote/VagrantSupport.java
@@ -29,12 +29,10 @@ import java.util.Collection;
  * @author traff
  */
 public abstract class VagrantSupport {
+  @Nullable
   public static VagrantSupport getInstance() {
     return ServiceManager.getService(VagrantSupport.class);
   }
-  
-  @Nullable
-  public abstract Pair<String, String> getVagrantInstanceParameters(@NotNull Project project);
 
   @Nullable
   public abstract Pair<String, RemoteCredentials> getVagrantSettings(Project project);
