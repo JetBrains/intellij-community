92f6762b79a292baf1053574272695ab776127ee
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2014-03-12 18:02:36 +0400

    IDEA-121995 Code selection is reseted on last line with space if Allow placement of caret inside tabs is enabled (cherry picked from commit 1f0a076)

--- platform/platform-tests/testSrc/com/intellij/openapi/editor/EditorMultiCaretUndoRedoTest.java
+++ platform/platform-tests/testSrc/com/intellij/openapi/editor/EditorMultiCaretUndoRedoTest.java
@@ -26,7 +26,6 @@ import com.intellij.openapi.editor.impl.AbstractEditorTest;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.fileEditor.TextEditor;
 import com.intellij.openapi.fileEditor.impl.text.TextEditorProvider;
-import com.intellij.testFramework.EditorTestUtil;
 import com.intellij.testFramework.TestFileType;
 import org.jetbrains.annotations.NotNull;
 
@@ -134,7 +133,7 @@ public class EditorMultiCaretUndoRedoTest extends AbstractEditorTest {
 
   private void init(String text) throws IOException {
     init(text, TestFileType.TEXT);
-    EditorTestUtil.setEditorVisibleSize(myEditor, 1000, 1000);
+    setEditorVisibleSize(1000, 1000);
     getUndoManager().setEditorProvider(new CurrentEditorProvider() {
       @Override
       public FileEditor getCurrentEditor() {
