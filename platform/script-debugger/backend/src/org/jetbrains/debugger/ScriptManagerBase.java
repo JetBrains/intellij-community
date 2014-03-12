a3bb37c94f91256dcd77d33211b07a958299b312
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-03-12 13:39:12 +0100

    remove dependency on JavaScriptDebugger
    (cherry picked from commit d5597c825a5a251117efb4e33c0c4f3d580ca6ff)

--- platform/script-debugger/backend/src/org/jetbrains/debugger/ScriptManagerBase.java
+++ platform/script-debugger/backend/src/org/jetbrains/debugger/ScriptManagerBase.java
@@ -5,6 +5,7 @@ import com.intellij.openapi.util.AsyncResult;
 import com.intellij.openapi.util.AsyncValueLoaderManager;
 import com.intellij.util.CommonProcessors;
 import com.intellij.util.Processor;
+import com.intellij.util.Url;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -50,5 +51,9 @@ public abstract class ScriptManagerBase<SCRIPT extends ScriptBase> implements Sc
   public Script forEachScript(@NotNull CommonProcessors.FindProcessor<Script> scriptProcessor) {
     forEachScript(((Processor<Script>)scriptProcessor));
     return scriptProcessor.getFoundValue();
+  }
+
+  public static boolean isSpecial(@NotNull Url url) {
+    return !url.isInLocalFileSystem() && (url.getScheme() == null || url.getScheme().equals(ScriptManager.VM_SCHEME) || url.getAuthority() == null);
   }
 }
