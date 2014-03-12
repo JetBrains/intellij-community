c883ac359ac933a082259efe4a963632680f3f71
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2014-03-12 17:57:28 +0400

    added rename argument quick fix for the Incorrect call arguments inspection
    (cherry picked from commit 54936dfbc6e00e7d351ed401fbcb2ed0e5188faa)

---
+++ python/src/com/jetbrains/python/inspections/quickfix/PyRenameArgumentQuickFix.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2000-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jetbrains.python.inspections.quickfix;
+
+import com.intellij.codeInsight.template.TemplateBuilderImpl;
+import com.intellij.codeInspection.LocalQuickFix;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.fileEditor.OpenFileDescriptor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiNamedElement;
+import com.jetbrains.python.PyBundle;
+import org.jetbrains.annotations.NotNull;
+
+public class PyRenameArgumentQuickFix implements LocalQuickFix {
+  @NotNull
+  @Override
+  public String getName() {
+    return PyBundle.message("QFIX.NAME.rename.argument");
+  }
+
+  @NotNull
+  @Override
+  public String getFamilyName() {
+    return getName();
+  }
+
+  @Override
+  public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
+    final PsiElement element = descriptor.getPsiElement();
+    if (!(element instanceof PsiNamedElement)) return;
+    final VirtualFile virtualFile = element.getContainingFile().getVirtualFile();
+    if (virtualFile != null) {
+      final Editor editor = FileEditorManager.getInstance(project).openTextEditor(new OpenFileDescriptor(project, virtualFile), true);
+      final TemplateBuilderImpl builder = new TemplateBuilderImpl(element);
+      final String name = ((PsiNamedElement)element).getName();
+      assert name != null;
+      assert editor != null;
+      builder.replaceElement(element, TextRange.create(0, name.length()), name);
+      builder.run(editor, false);
+    }
+  }
+
+}
