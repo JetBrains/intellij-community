573f1bc7fda29ef2ce03a5a65c4055def104c005
Kirill Likhodedov <Kirill.Likhodedov@gmail.com>
2014-03-13 13:17:45 +0400

    [log] IDEA-117753 trim spaces in log filters
    (cherry picked from commit 86eda0804bfc46ef091b471b4e97ad339b5a678e)

--- platform/vcs-log/impl/src/com/intellij/vcs/log/ui/filter/MultilinePopupBuilder.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/ui/filter/MultilinePopupBuilder.java
@@ -34,6 +34,7 @@ import com.intellij.ui.EditorCustomization;
 import com.intellij.ui.EditorTextField;
 import com.intellij.ui.EditorTextFieldProvider;
 import com.intellij.ui.SoftWrapsEditorCustomization;
+import com.intellij.util.Function;
 import com.intellij.util.TextFieldCompletionProviderDumbAware;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
@@ -94,7 +95,12 @@ class MultilinePopupBuilder {
 
   @NotNull
   Collection<String> getSelectedValues() {
-    return ContainerUtil.toCollection(StringUtil.tokenize(myTextField.getText().trim(), new String(SEPARATORS)));
+    return ContainerUtil.map(StringUtil.tokenize(myTextField.getText(), new String(SEPARATORS)), new Function<String, String>() {
+      @Override
+      public String fun(String value) {
+        return value.trim();
+      }
+    });
   }
 
   private static class MyCompletionProvider extends TextFieldCompletionProviderDumbAware {
