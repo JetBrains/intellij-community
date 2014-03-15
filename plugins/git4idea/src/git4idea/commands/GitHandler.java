2025e78b0ad86d6d54ca156baaf27a18b657939c
Kirill Likhodedov <Kirill.Likhodedov@gmail.com>
2014-03-15 17:38:11 +0400

    [git] log suppressed output by separate logger
    This allows to enable git4idea debug log and avoid massive output
    from git log etc.
    If the whole output is needed, it can be enabled by separate logger
    with the "output" prefix.
    Prefix is used instead of suffix, because loggers are dot-recursive,
    i.e. enabling git4idea.GitHandler would enable git4idea.GitHandler.out.

--- plugins/git4idea/src/git4idea/commands/GitHandler.java
+++ plugins/git4idea/src/git4idea/commands/GitHandler.java
@@ -54,6 +54,10 @@ import java.util.concurrent.LinkedBlockingQueue;
  * A handler for git commands
  */
 public abstract class GitHandler {
+
+  protected static final Logger LOG = Logger.getInstance(GitHandler.class);
+  protected static final Logger OUTPUT_LOG = Logger.getInstance("#output." + GitHandler.class.getName());
+
   protected final Project myProject;
   protected final GitCommand myCommand;
 
@@ -61,7 +65,6 @@ public abstract class GitHandler {
   private final List<VcsException> myErrors = Collections.synchronizedList(new ArrayList<VcsException>());
   private final List<String> myLastOutput = Collections.synchronizedList(new ArrayList<String>());
   private final int LAST_OUTPUT_SIZE = 5;
-  protected static final Logger LOG = Logger.getInstance(GitHandler.class.getName());
   final GeneralCommandLine myCommandLine;
   @SuppressWarnings({"FieldAccessedSynchronizedAndUnsynchronized"})
   Process myProcess;
