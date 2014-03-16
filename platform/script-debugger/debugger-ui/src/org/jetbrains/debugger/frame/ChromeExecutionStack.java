08f232e321de8b3b26837ec4fae8c3c48d0b8128
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-03-16 11:00:53 +0100

    ChromeExecutionStack is generic impl

---
+++ platform/script-debugger/debugger-ui/src/org/jetbrains/debugger/frame/ChromeExecutionStack.java
@@ -0,0 +1,57 @@
+package org.jetbrains.debugger.frame;
+
+import com.intellij.xdebugger.frame.XExecutionStack;
+import com.intellij.xdebugger.frame.XStackFrame;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.debugger.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+final class ChromeExecutionStack extends XExecutionStack {
+  private final CallFrameView topFrame;
+  private final DebuggerViewSupport debugProcess;
+
+  public ChromeExecutionStack(@NotNull DebuggerViewSupport debugProcess, @NotNull SuspendContext suspendContext, @Nullable Script script) {
+    super("");
+
+    this.debugProcess = debugProcess;
+    topFrame = suspendContext.getTopFrame() == null ? null : new CallFrameView(suspendContext.getTopFrame(), debugProcess, script);
+  }
+
+  @Override
+  @Nullable
+  public CallFrameView getTopFrame() {
+    return topFrame;
+  }
+
+  @Override
+  public void computeStackFrames(final int firstFrameIndex, final XStackFrameContainer container) {
+    SuspendContext suspendContext = debugProcess.getVm().getSuspendContextManager().getContext();
+    assert suspendContext != null;
+    suspendContext.getCallFrames().doWhenDone(new ContextDependentAsyncResultConsumer<CallFrame[]>(suspendContext) {
+      @Override
+      protected void consume(CallFrame[] frames, @NotNull Vm vm) {
+        int count = frames.length - firstFrameIndex;
+        List<XStackFrame> result;
+        if (count < 1) {
+          result = Collections.emptyList();
+        }
+        else {
+          result = new ArrayList<XStackFrame>(count);
+          for (int i = firstFrameIndex; i < frames.length; i++) {
+            CallFrame frame = frames[i];
+            // if script is null, it is native function (Object.forEach for example), so, skip it
+            Script script = vm.getScriptManager().getScript(frame);
+            if (script != null) {
+              result.add(new CallFrameView(frame, debugProcess, script));
+            }
+          }
+        }
+        container.addStackFrames(result, true);
+      }
+    });
+  }
+}
