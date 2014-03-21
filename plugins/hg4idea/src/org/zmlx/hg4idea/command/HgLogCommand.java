d23e073deb4e8658f450516189666172dd9aceb3
Nadya Zabrodina <Nadya.Zabrodina@jetbrains.com>
2014-03-21 19:42:34 +0400

    log problem for empty commit message fixed (cherry picked from commit 8ab972c)

--- plugins/hg4idea/src/org/zmlx/hg4idea/command/HgLogCommand.java
+++ plugins/hg4idea/src/org/zmlx/hg4idea/command/HgLogCommand.java
@@ -125,11 +125,12 @@ public class HgLogCommand {
     String[] changeSets = output.split(HgChangesetUtil.CHANGESET_SEPARATOR);
     for (String line : changeSets) {
       try {
-        String[] attributes = line.split(HgChangesetUtil.ITEM_SEPARATOR);
+        // we need to get all attributes, include empty trailing strings, so use non-positive limit as second argument
+        String[] attributes = line.split(HgChangesetUtil.ITEM_SEPARATOR, -1);
         // At least in the case of the long template, it's OK that we don't have everything...for example, if there were no
         //  deleted or copied files, then we won't get any attributes for them...
         int numAttributes = attributes.length;
-        if (!includeFiles && (numAttributes != expectedItemCount)) {
+        if (!includeFiles && (numAttributes < expectedItemCount)) { //there are may be empty string as last split attributes
           LOG.debug("Wrong format. Skipping line " + line);
           continue;
         }
