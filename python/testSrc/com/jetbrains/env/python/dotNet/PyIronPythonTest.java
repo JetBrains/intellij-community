a9f6d2fd5f37281f967c040166e7ea06e8bf4af1
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2014-03-27 02:11:28 +0400

    PY-12497 .NET stubs are generated incorrectly when name is imported instead of module

--- python/testSrc/com/jetbrains/env/python/dotNet/PyIronPythonTest.java
+++ python/testSrc/com/jetbrains/env/python/dotNet/PyIronPythonTest.java
@@ -1,9 +1,12 @@
 package com.jetbrains.env.python.dotNet;
 
 import com.jetbrains.env.python.debug.PyEnvTestCase;
+import com.jetbrains.python.psi.PyFile;
+import org.junit.Assert;
 
 /**
  * IronPython.NET specific tests
+ *
  * @author Ilya.Kazakevich
  */
 public class PyIronPythonTest extends PyEnvTestCase {
@@ -21,6 +24,113 @@ public class PyIronPythonTest extends PyEnvTestCase {
    * Tests skeleton generation
    */
   public void testSkeletons() throws Exception {
-    runPythonTest(new SkeletonTestTask());
+    runPythonTest(new SkeletonTestTask(
+      "dotNet/expected.skeleton.java.py",
+      "com.just.like.java",
+      "testSkeleton.py",
+      null
+    ));
+  }
+
+  /**
+   * Tests skeleton generation with "from" statements
+   */
+  public void testClassFromModule() throws Exception {
+    runPythonTest(new SkeletonTestTask(
+      "dotNet/expected.skeleton.java.py",
+      "com.just.like.java",
+      "import_class_from_module.py",
+      null
+    ));
+  }
+
+  /**
+   * Tests skeleton generation when imported as alias
+   */
+  public void testClassFromModuleAlias() throws Exception {
+    runPythonTest(new SkeletonTestTask(
+      "dotNet/expected.skeleton.java.py",
+      "com.just.like.java",
+      "import_class_from_module_alias.py",
+      null
+    ));
+  }
+
+  /**
+   * Tests skeleton generation when module is imported
+   */
+  public void testModuleFromPackage() throws Exception {
+    runPythonTest(new SkeletonTestTask(
+      "dotNet/expected.skeleton.java.py",
+      "com.just.like.java",
+      "import_module_from_package.py",
+      null
+    ));
+  }
+
+  /**
+   * Tests skeleton generation when several classes are imported
+   */
+  public void testSeveralClasses() throws Exception {
+    runPythonTest(new SkeletonTestTask(
+      "dotNet/expected.skeleton.java.py",
+      "com.just.like.java",
+      "import_several_classes_from_module.py",
+      "com.just.like.java.LikeJavaClass"
+    ));
+  }
+
+  /**
+   * Tests skeletons for built-in classes. We can't compare files (CLR class may be changed from version to version),
+   * but we are sure there should be class System.Web.AspNetHostingPermissionLevel which is part of public API
+   */
+  public void testImportBuiltInSystem() throws Exception {
+    final SkeletonTestTask task = new SkeletonTestTask(
+      null,
+      "System.Web",
+      "import_system.py",
+      null
+    );
+    runPythonTest(task);
+    final PyFile skeleton = task.getGeneratedSkeleton();
+    Assert.assertNotNull("System.Web does not contain class AspNetHostingPermissionLevel. Error generating stub? It has classes  " +
+                         skeleton.getTopLevelClasses(),
+                         skeleton.findTopLevelClass("AspNetHostingPermissionLevel"));
+  }
+
+  /**
+   * Test importing of inner classes
+   */
+  public void testImportInnerClass() throws Exception {
+    runPythonTest(new SkeletonTestTask(
+      "dotNet/expected.skeleton.Deep.py",
+      "SingleNameSpace.Some.Deep",
+      "inner_class.py",
+      null
+    ));
+  }
+
+  /**
+   * Test importing of the whole namespace
+   */
+  public void testWholeNameSpace() throws Exception {
+    runPythonTest(new SkeletonTestTask(
+      "dotNet/expected.skeleton.SingleNameSpace.py",
+      "SingleNameSpace",
+      "whole_namespace.py",
+      null
+    ));
+  }
+
+  /**
+   * Test importing of single class
+   */
+  public void testSingleClass() throws Exception {
+    runPythonTest(new SkeletonTestTask(
+      "dotNet/expected.skeleton.SingleNameSpace.py",
+      "SingleNameSpace",
+      "single_class.py",
+      null
+    ));
   }
 }
--- python/testSrc/com/jetbrains/env/python/dotNet/SkeletonTestTask.java
+++ python/testSrc/com/jetbrains/env/python/dotNet/SkeletonTestTask.java
@@ -8,9 +8,13 @@ import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.util.ui.UIUtil;
 import com.jetbrains.env.python.debug.PyExecutionFixtureTestTask;
+import com.jetbrains.python.PyBundle;
+import com.jetbrains.python.PyNames;
 import com.jetbrains.python.inspections.PyUnresolvedReferencesInspection;
+import com.jetbrains.python.psi.PyFile;
 import com.jetbrains.python.sdk.PythonSdkType;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.junit.Assert;
 
 import java.io.File;
@@ -33,37 +37,80 @@ class SkeletonTestTask extends PyExecutionFixtureTestTask {
    * Number of seconds we wait for skeleton generation external process (should be enough)
    */
   private static final int SECONDS_TO_WAIT_FOR_SKELETON_GENERATION = 20;
+
+  @Nullable
+  private final String myExpectedSkeletonFile;
+  @NotNull
+  private final String myModuleNameToBeGenerated;
+  @NotNull
+  private final String mySourceFileToRunGenerationOn;
+  @NotNull
+  private final String myUseQuickFixWithThisModuleOnly;
+  private PyFile myGeneratedSkeleton;
+
   /**
-   * We check that generated skeleton is exactly as this file
+   * @param expectedSkeletonFile          if you want test to compare generated result with some file, provide its name.
+   *                                      Pass null if you do not want to compare result with anything (you may do it yourself with {@link #getGeneratedSkeleton()})
+   * @param moduleNameToBeGenerated       name of module you think we should generate in dotted notation (like "System.Web" or "com.myModule").
+   *                                      System will wait for skeleton file for this module to be generated
+   * @param sourceFileToRunGenerationOn   Source file where we should run "generate stubs" on. Be sure to place "caret" on appropriate place!
+   * @param useQuickFixWithThisModuleOnly If there are several quick fixes in code, you may run fix only on this module.
+   *                                      Pass null if you are sure there would be only one quickfix
    */
-  private static final String DOT_NET_EXPECTED_SKELETON_PY = "dotNet/expected.skeleton.py";
-  private static final String SKELETON_FILE_TO_TEST = "testSkeleton.py";
+  SkeletonTestTask(@Nullable final String expectedSkeletonFile,
+                   @NotNull final String moduleNameToBeGenerated,
+                   @NotNull final String sourceFileToRunGenerationOn,
+                   @Nullable final String useQuickFixWithThisModuleOnly) {
+    myExpectedSkeletonFile = expectedSkeletonFile;
+    myModuleNameToBeGenerated = moduleNameToBeGenerated.replace('.', '/');
+    mySourceFileToRunGenerationOn = sourceFileToRunGenerationOn;
+    myUseQuickFixWithThisModuleOnly = useQuickFixWithThisModuleOnly != null ? useQuickFixWithThisModuleOnly : "";
+  }
+
 
   @Override
   public void runTestOn(@NotNull final String sdkHome) throws IOException {
     final Sdk sdk = getSdk(sdkHome);
     ModuleRootModificationUtil.setModuleSdk(myFixture.getModule(), sdk);
     final File skeletonsPath = new File(PythonSdkType.getSkeletonsPath(PathManager.getSystemPath(), sdkHome));
-    final File skeleton = new File(skeletonsPath, "com/just/like/java.py"); // File with module skeleton
+    File skeletonFileOrDirectory = new File(skeletonsPath, myModuleNameToBeGenerated); // File with module skeleton
 
-    myFixture.copyFileToProject("dotNet/testSkeleton.py", SKELETON_FILE_TO_TEST); // File that uses CLR library
+    // Module may be stored in "moduleName.py" or "moduleName/__init__.py"
+    if (skeletonFileOrDirectory.isDirectory()) {
+      skeletonFileOrDirectory = new File(skeletonFileOrDirectory, PyNames.INIT_DOT_PY);
+    }
+    else {
+      skeletonFileOrDirectory = new File(skeletonFileOrDirectory.getAbsolutePath() + PyNames.DOT_PY);
+    }
+
+    final File skeletonFile = skeletonFileOrDirectory;
+
+    if (skeletonFile.exists()) { // To make sure we do not reuse it
+      skeletonFile.delete();
+    }
+
+    myFixture.copyFileToProject("dotNet/" + mySourceFileToRunGenerationOn, mySourceFileToRunGenerationOn); // File that uses CLR library
     myFixture.copyFileToProject("dotNet/PythonLibs.dll", "PythonLibs.dll"); // Library itself
-    myFixture.configureByFile(SKELETON_FILE_TO_TEST);
+    myFixture.copyFileToProject("dotNet/SingleNameSpace.dll", "SingleNameSpace.dll"); // Another library
+    myFixture.configureByFile(mySourceFileToRunGenerationOn);
     myFixture.enableInspections(PyUnresolvedReferencesInspection.class); // This inspection should suggest us to generate stubs
-
 
 
     UIUtil.invokeAndWaitIfNeeded(new Runnable() {
       @Override
       public void run() {
         PsiDocumentManager.getInstance(myFixture.getProject()).commitAllDocuments();
-        myFixture.findSingleIntention("Generate").invoke(myFixture.getProject(), myFixture.getEditor(), myFixture.getFile());
-        waitForSkeleton(skeleton);
+        final String intentionName = PyBundle.message("sdk.gen.stubs.for.binary.modules", myUseQuickFixWithThisModuleOnly);
+        myFixture.findSingleIntention(intentionName).invoke(myFixture.getProject(), myFixture.getEditor(), myFixture.getFile());
+        waitForSkeleton(skeletonFile);
       }
     });
 
-    FileUtil.copy(skeleton, new File(myFixture.getTempDirPath(), skeleton.getName()));
-    myFixture.checkResultByFile(skeleton.getName(), DOT_NET_EXPECTED_SKELETON_PY, false);
+    FileUtil.copy(skeletonFile, new File(myFixture.getTempDirPath(), skeletonFile.getName()));
+    if (myExpectedSkeletonFile != null) {
+      myFixture.checkResultByFile(skeletonFile.getName(), myExpectedSkeletonFile, false);
+    }
+    myGeneratedSkeleton = (PyFile)myFixture.configureByFile(skeletonFile.getName());
   }
 
 
@@ -75,6 +122,7 @@ class SkeletonTestTask extends PyExecutionFixtureTestTask {
   /**
    * Waits {@link #SECONDS_TO_WAIT_FOR_SKELETON_GENERATION} seconds for file to be created.
    * Fails test after that.
+   *
    * @param skeletonToWait file to wait
    */
   private static void waitForSkeleton(@NotNull final File skeletonToWait) {
@@ -94,5 +142,13 @@ class SkeletonTestTask extends PyExecutionFixtureTestTask {
     final String message =
       String.format("After %s seconds of waiting, skeleton %s still not ready", SECONDS_TO_WAIT_FOR_SKELETON_GENERATION, skeletonToWait);
     Assert.assertTrue(message, skeletonToWait.exists());
+  }
+
+  /**
+   * @return File for generated skeleton. Call it after {@link #runTestOn(String)} only!
+   */
+  @NotNull
+  PyFile getGeneratedSkeleton() {
+    return myGeneratedSkeleton;
   }
 }
