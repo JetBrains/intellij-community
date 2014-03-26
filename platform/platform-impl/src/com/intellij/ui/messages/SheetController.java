71a9f36f5cf117286d2261b93b2f1e93f0077356
Denis Fokin <Denis.Fokin@jetbrains.com>
2014-03-26 14:28:51 +0400

    Initial value of "do not ask" option has not been honored

--- platform/platform-impl/src/com/intellij/ui/messages/SheetController.java
+++ platform/platform-impl/src/com/intellij/ui/messages/SheetController.java
@@ -83,6 +83,7 @@ public class SheetController {
     }
 
     myDoNotAskOption = doNotAskOption;
+    myDoNotAskResult = !doNotAskOption.isToBeShown();
     mySheetMessage = sheetMessage;
     buttons = new JButton[buttonTitles.length];
 
