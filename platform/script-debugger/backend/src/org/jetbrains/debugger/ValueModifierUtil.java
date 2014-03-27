f0780746a4074936e3a8d1b0f66a121e4042b4ec
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-03-27 16:20:29 +0100

    cleanup — remove getGlobalEvaluateContext
    (cherry picked from commit aafa722178b63ce8f392c77ee7bcb197791daeb6)

--- platform/script-debugger/backend/src/org/jetbrains/debugger/ValueModifierUtil.java
+++ platform/script-debugger/backend/src/org/jetbrains/debugger/ValueModifierUtil.java
@@ -5,12 +5,12 @@ import com.intellij.util.Consumer;
 import org.jetbrains.debugger.values.Value;
 
 public final class ValueModifierUtil {
-  public static ActionCallback setValue(final Variable variable, String newValue, EvaluateContext evaluateContext, final ValueModifier modifier) {
+  public static ActionCallback setValue(final Variable variable, String newValue, final EvaluateContext evaluateContext, final ValueModifier modifier) {
     final ActionCallback callback = new ActionCallback();
     evaluateContext.evaluate(newValue).doWhenDone(new Consumer<Value>() {
       @Override
       public void consume(Value value) {
-        modifier.setValue(variable, value).notify(callback);
+        modifier.setValue(variable, value, evaluateContext).notify(callback);
       }
     }).notifyWhenRejected(callback);
     return callback;
