0b70f88c0b1b4ab7cd27c7703d29acbc5b70bae3
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2014-03-27 16:01:49 +0100

    IDEA-122677 (do not show "fatal error" dialog if the app is already loaded)

--- platform/platform-impl/src/com/intellij/ide/plugins/PluginManager.java
+++ platform/platform-impl/src/com/intellij/ide/plugins/PluginManager.java
@@ -99,13 +99,10 @@ public class PluginManager extends PluginManagerCore {
   }
 
   public static void processException(Throwable t) {
-    @SuppressWarnings("ThrowableResultOfMethodCallIgnored") StartupAbortedException se = findCause(t);
+    if (!IdeaApplication.isLoaded()) {
+      @SuppressWarnings("ThrowableResultOfMethodCallIgnored") StartupAbortedException se = findCause(t);
+      if (se == null) se = new StartupAbortedException(t);
 
-    if (se == null && !IdeaApplication.isLoaded()) {
-      se = new StartupAbortedException(t);
-    }
-
-    if (se != null) {
       if (se.logError()) {
         try {
           if (Logger.isInitialized() && !(t instanceof ProcessCanceledException)) {
@@ -119,8 +116,7 @@ public class PluginManager extends PluginManagerCore {
 
       System.exit(se.exitCode());
     }
-
-    if (!(t instanceof ProcessCanceledException)) {
+    else if (!(t instanceof ProcessCanceledException)) {
       getLogger().error(t);
     }
   }
