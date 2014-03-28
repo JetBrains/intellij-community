7a39b2b6cb14cb1098f6dba3441d0183e1ca7441
Kirill Likhodedov <Kirill.Likhodedov@gmail.com>
2014-03-28 14:29:52 +0400

    [log] Extend VcsShortCommitDetailsImpl from TimedVcsCommitImpl
    To reuse correct equals & hashcode implementations.
    Add warnings to javadocs.

--- platform/vcs-log/api/src/com/intellij/vcs/log/VcsShortCommitDetails.java
+++ platform/vcs-log/api/src/com/intellij/vcs/log/VcsShortCommitDetails.java
@@ -6,14 +6,15 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 
 /**
- * <p>Returns the basic level of commit meta-data: author, time, subject.</p>
- *
- * <p>These details will be displayed in the log table.</p>
- *
- * <p>An instance of this object can be obtained via
- *    {@link VcsLogObjectsFactory#createShortDetails(Hash, List, long, VirtualFile, String, String, String)
- *    VcsLogObjectsFactory#createShortDetails}
- * </p>
+ * Returns the basic level of commit meta-data: author, time, subject. These details will be displayed in the log table.
+ * <p/>
+ * An instance of this object can be obtained via
+ * {@link VcsLogObjectsFactory#createShortDetails(Hash, List, long, VirtualFile, String, String, String)
+ * VcsLogObjectsFactory#createShortDetails}.
+ * <p/>
+ * It is not recommended to create a custom implementation of this interface, but if you need it, <b>make sure to implement {@code equals()}
+ * and {@code hashcode()} so that they consider only the Hash</b>, i.e. two VcsShortCommitDetails are equal if and only if they have equal
+ * hash codes. The VCS Log framework heavily relies on this fact.
  *
  * @see VcsCommitMetadata
  * @see VcsFullCommitDetails
