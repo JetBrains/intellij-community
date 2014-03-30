be93e049dc4b26b635a91e8ec1cf737fb26e72d2
Michail Plushnikov <michail@plushnikov.de>
2014-03-30 19:48:01 +0200

    updated ide settings
    GitOrigin-RevId: 36a4c3321d2fe3d03d0be315402e7b2e765349d1

---
+++ plugins/lombok/test-manual/lombok/src/main/java/de/plushnikov/builder/BuilderComplex.java
@@ -0,0 +1,15 @@
+package de.plushnikov.builder;
+
+import java.util.List;
+import lombok.experimental.Builder;
+
+public class BuilderComplex {
+	@Builder(buildMethodName = "execute")
+	private static <T extends Number> void testVoidWithGenerics(T number, int arg2, String arg3, BuilderComplex selfRef) {}
+
+    public static void main(String[] args) {
+
+//        VoidBuilder<Integer> builder = (VoidBuilder<Integer>) BuilderComplex.<Integer>builder();
+//        builder.number(12).arg2(32).arg3("sss").execute();
+    }
+}
