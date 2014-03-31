880b479f0e6e9be19f257f4bbe17399cd3a877dd
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2014-03-31 12:04:36 +0200

    Huge performance hit when adding new property at the end of the large .properties file (IDEA-101698)

--- plugins/properties/properties-psi-impl/src/com/intellij/lang/properties/parsing/PropertiesElementTypes.java
+++ plugins/properties/properties-psi-impl/src/com/intellij/lang/properties/parsing/PropertiesElementTypes.java
@@ -15,11 +15,20 @@
  */
 package com.intellij.lang.properties.parsing;
 
+import com.intellij.lang.ASTNode;
+import com.intellij.lang.LanguageParserDefinitions;
+import com.intellij.lang.LighterASTNode;
+import com.intellij.lang.ParserDefinition;
+import com.intellij.lang.PsiBuilder;
+import com.intellij.lang.PsiBuilderFactory;
 import com.intellij.lang.properties.PropertiesLanguage;
+import com.intellij.lang.properties.PropertiesFileType;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.stubs.IStubElementType;
-import com.intellij.psi.tree.IFileElementType;
-import com.intellij.psi.tree.IStubFileElementType;
+import com.intellij.psi.tree.ILightStubFileElementType;
 import com.intellij.psi.tree.TokenSet;
+import com.intellij.util.diff.FlyweightCapableTreeStructure;
 
 /**
  * @author max
@@ -27,7 +36,20 @@ import com.intellij.psi.tree.TokenSet;
 public interface PropertiesElementTypes {
   PropertiesLanguage LANG = PropertiesLanguage.INSTANCE;
 
-  IFileElementType FILE = new IStubFileElementType(LANG);
+  ILightStubFileElementType FILE = new ILightStubFileElementType(LANG) {
+    public FlyweightCapableTreeStructure<LighterASTNode> parseContentsLight(ASTNode chameleon) {
+      PsiElement psi = chameleon.getPsi();
+      assert (psi != null) : ("Bad chameleon: " + chameleon);
+
+      Project project = psi.getProject();
+      PsiBuilderFactory factory = PsiBuilderFactory.getInstance();
+      PsiBuilder builder = factory.createBuilder(project, chameleon);
+      ParserDefinition parserDefinition = LanguageParserDefinitions.INSTANCE.forLanguage(getLanguage());
+      assert (parserDefinition != null) : this;
+      PropertiesParser parser = new PropertiesParser();
+      return parser.parseLight(this, builder);
+    }
+  };
   IStubElementType PROPERTY = new PropertyStubElementType();
 
   IStubElementType PROPERTIES_LIST = new PropertyListStubElementType();
