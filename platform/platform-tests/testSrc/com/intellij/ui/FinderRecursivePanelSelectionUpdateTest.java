33ded497253650ebfc3654ab667699aba0aecf79
Yann Cébron <yann.cebron@jetbrains.com>
2014-03-31 10:50:37 +0200

    advance bomb

--- platform/platform-tests/testSrc/com/intellij/ui/FinderRecursivePanelSelectionUpdateTest.java
+++ platform/platform-tests/testSrc/com/intellij/ui/FinderRecursivePanelSelectionUpdateTest.java
@@ -31,7 +31,7 @@ import java.util.List;
 @SkipInHeadlessEnvironment
 public class FinderRecursivePanelSelectionUpdateTest extends PlatformTestCase {
 
-  @Bombed(year = 2014, month = Calendar.APRIL, day = 1, user = "Yann Cebron")
+  @Bombed(year = 2015, month = Calendar.APRIL, day = 1, user = "Yann Cebron")
   public void testUpdate() throws InterruptedException {
     StringFinderRecursivePanel panel_0 = new StringFinderRecursivePanel(getProject()) {
       @NotNull
