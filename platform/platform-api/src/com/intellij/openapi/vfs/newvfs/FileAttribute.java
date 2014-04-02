e75d93b7ecb120853e7f3b210b2e24b88b7aea86
Alexey Kudravtsev <cdr@intellij.com>
2014-04-02 13:21:54 +0400

    notnull, cleanup

--- platform/platform-api/src/com/intellij/openapi/vfs/newvfs/FileAttribute.java
+++ platform/platform-api/src/com/intellij/openapi/vfs/newvfs/FileAttribute.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -55,7 +55,7 @@ public class FileAttribute {
   }
 
   @Nullable
-  public DataInputStream readAttribute(VirtualFile file) {
+  public DataInputStream readAttribute(@NotNull VirtualFile file) {
     DataInputStream stream = ManagingFS.getInstance().readAttribute(file, this);
     if (stream != null) {
       try {
@@ -72,7 +72,8 @@ public class FileAttribute {
     return stream;
   }
 
-  public DataOutputStream writeAttribute(VirtualFile file) {
+  @NotNull
+  public DataOutputStream writeAttribute(@NotNull VirtualFile file) {
     final DataOutputStream stream = ManagingFS.getInstance().writeAttribute(file, this);
     try {
       DataInputOutputUtil.writeINT(stream, myVersion);
