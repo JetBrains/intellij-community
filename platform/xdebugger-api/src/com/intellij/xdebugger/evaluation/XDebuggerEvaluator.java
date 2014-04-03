4ad08526d8b1aaf72d04ae08bf6bf85c964147a7
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-04-03 15:14:23 +0200

    show source text as value name, part of "WEB-6069 CoffeeScript Debugger doesn't show value of the variables" (cherry picked from commit a587c8b)

--- platform/xdebugger-api/src/com/intellij/xdebugger/evaluation/XDebuggerEvaluator.java
+++ platform/xdebugger-api/src/com/intellij/xdebugger/evaluation/XDebuggerEvaluator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -108,6 +108,8 @@ public abstract class XDebuggerEvaluator {
   }
 
   /**
+   * @deprecated Use {@link #getExpressionInfoAtOffset(com.intellij.openapi.project.Project, com.intellij.openapi.editor.Document, int, boolean)}
+   *
    * Return text range of expression which can be evaluated.
    *
    * @param project            project
@@ -118,6 +120,7 @@ public abstract class XDebuggerEvaluator {
    * @return pair of text range of expression (to highlight as link) and actual expression to evaluate (optional, could be null)
    */
   @Nullable
+  @Deprecated
   public Pair<TextRange, String> getExpressionAtOffset(@NotNull Project project, @NotNull Document document, int offset, boolean sideEffectsAllowed) {
     TextRange range = getExpressionRangeAtOffset(project, document, offset, sideEffectsAllowed);
     if (range == null) {
@@ -126,6 +129,21 @@ public abstract class XDebuggerEvaluator {
     else {
       return Pair.create(range, null);
     }
+  }
+
+  /**
+   * @param project            project
+   * @param document           document
+   * @param offset             offset
+   * @param sideEffectsAllowed if this parameter is false, the expression should not have any side effects when evaluated
+   *                           (such expressions are evaluated in quick popups)
+   * @return {@link com.intellij.xdebugger.evaluation.ExpressionInfo} of expression which can be evaluated
+   */
+  @Nullable
+  public ExpressionInfo getExpressionInfoAtOffset(@NotNull Project project, @NotNull Document document, int offset, boolean sideEffectsAllowed) {
+    @SuppressWarnings("deprecation")
+    Pair<TextRange, String> result = getExpressionAtOffset(project, document, offset, sideEffectsAllowed);
+    return result == null ? null : new ExpressionInfo(result.first, result.second);
   }
 
   /**
