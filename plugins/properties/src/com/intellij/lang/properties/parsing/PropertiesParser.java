645e2ca7a2a36365659319380d24f463ae23ebef
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2014-04-03 12:39:55 +0200

    properties-IC-129.713-gw01

--- plugins/properties/src/com/intellij/lang/properties/parsing/PropertiesParser.java
+++ plugins/properties/src/com/intellij/lang/properties/parsing/PropertiesParser.java
@@ -16,17 +16,76 @@
 package com.intellij.lang.properties.parsing;
 
 import com.intellij.lang.ASTNode;
+import com.intellij.lang.LighterASTNode;
+import com.intellij.lang.LighterASTTokenNode;
 import com.intellij.lang.PsiBuilder;
 import com.intellij.lang.PsiParser;
+import com.intellij.lang.impl.PsiBuilderImpl;
+import com.intellij.openapi.util.Ref;
+import com.intellij.psi.impl.source.tree.LightTreeUtil;
 import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.xml.XmlElementType;
+import com.intellij.psi.xml.XmlTag;
+import com.intellij.psi.xml.XmlTokenType;
+import com.intellij.util.ThreeState;
+import com.intellij.util.TripleFunction;
+import com.intellij.util.diff.FlyweightCapableTreeStructure;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 /**
  * @author max
  */
 public class PropertiesParser implements PsiParser {
+  private static final TripleFunction<ASTNode,LighterASTNode,FlyweightCapableTreeStructure<LighterASTNode>,ThreeState>
+          MATCH_BY_KEY = new TripleFunction<ASTNode, LighterASTNode, FlyweightCapableTreeStructure<LighterASTNode>, ThreeState>() {
+    @Override
+    public ThreeState fun(ASTNode oldNode,
+                          LighterASTNode newNode,
+                          FlyweightCapableTreeStructure<LighterASTNode> structure) {
+
+      if (oldNode.getElementType() == PropertiesElementTypes.PROPERTY) {
+        ASTNode oldName = oldNode.findChildByType(PropertiesTokenTypes.KEY_CHARACTERS);
+        if (oldName != null) {
+          String oldNameStr = oldName.getText();
+          CharSequence newNameStr = findKeyCharacters(newNode, structure);
+
+          if (oldNameStr != null && !oldNameStr.equals(newNameStr)) {
+            return ThreeState.NO;
+          }
+        }
+      }
+      return ThreeState.UNSURE;
+    }
+  };
+
+  private static CharSequence findKeyCharacters(LighterASTNode newNode, FlyweightCapableTreeStructure<LighterASTNode> structure) {
+    Ref<LighterASTNode[]> childrenRef = Ref.create(null);
+    structure.getChildren(newNode, childrenRef);
+    LighterASTNode[] children = childrenRef.get();
+    for (int i = 0; i < children.length; ++i) {
+      if (children[i].getTokenType() == PropertiesTokenTypes.KEY_CHARACTERS)
+        return ((LighterASTTokenNode) children[i]).getText();
+    }
+    return null;
+  }
+
+
   @NotNull
   public ASTNode parse(IElementType root, PsiBuilder builder) {
+    doParse(root, builder);
+    return builder.getTreeBuilt();
+  }
+
+  @NotNull
+  public FlyweightCapableTreeStructure<LighterASTNode> parseLight(IElementType root, PsiBuilder builder) {
+    doParse(root, builder);
+    return builder.getLightTree();
+  }
+
+  public void doParse(IElementType root, PsiBuilder builder) {
+    builder.putUserDataUnprotected(PsiBuilderImpl.CUSTOM_COMPARATOR, MATCH_BY_KEY);
     final PsiBuilder.Marker rootMarker = builder.mark();
     final PsiBuilder.Marker propertiesList = builder.mark();
     while (!builder.eof()) {
@@ -34,6 +93,5 @@ public class PropertiesParser implements PsiParser {
     }
     propertiesList.done(PropertiesElementTypes.PROPERTIES_LIST);
     rootMarker.done(root);
-    return builder.getTreeBuilt();
   }
 }
