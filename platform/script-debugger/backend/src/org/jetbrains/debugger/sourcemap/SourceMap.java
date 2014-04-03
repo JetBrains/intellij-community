bc234fe75fce8f80e6d63fc1a856a201812a5871
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-04-03 13:33:51 +0200

    WEB-6069 CoffeeScript Debugger doesn't show value of the variables

--- platform/script-debugger/backend/src/org/jetbrains/debugger/sourcemap/SourceMap.java
+++ platform/script-debugger/backend/src/org/jetbrains/debugger/sourcemap/SourceMap.java
@@ -2,6 +2,7 @@ package org.jetbrains.debugger.sourcemap;
 
 import com.intellij.openapi.util.NullableLazyValue;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.util.Processor;
 import com.intellij.util.Url;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -9,8 +10,8 @@ import org.jetbrains.annotations.Nullable;
 import java.util.List;
 
 public class SourceMap {
-  private final List<MappingEntry> mappings;
-  final MappingEntry[][] sourceIndexToMappings;
+  private final MappingList mappings;
+  final MappingList[] sourceIndexToMappings;
 
   private final String outFile;
 
@@ -18,8 +19,8 @@ public class SourceMap {
 
   // sources - is not originally specified, but canonicalized/normalized
   public SourceMap(@Nullable String outFile,
-                   @NotNull List<MappingEntry> mappings,
-                   @NotNull MappingEntry[][] sourceIndexToMappings,
+                   @NotNull MappingList mappings,
+                   @NotNull MappingList[] sourceIndexToMappings,
                    @NotNull SourceResolver sourceResolver) {
     this.outFile = outFile;
     this.mappings = mappings;
@@ -41,70 +42,32 @@ public class SourceMap {
     return sourceResolver.canonicalizedSources;
   }
 
-  public List<MappingEntry> getMappings() {
+  public MappingList getMappings() {
     return mappings;
   }
 
-  @NotNull
-  public MappingEntry findEntry(int generatedLine, int generatedColumn) {
-    // todo honor Google Chrome bug related to paused location
-    return mappings.get(findEntryIndex(generatedLine, generatedColumn));
-  }
-
-  public int findEntryIndex(int generatedLine, int generatedColumn) {
-    int first = 0;
-    int count = mappings.size();
-    while (count > 1) {
-      int step = count >> 1;
-      int middle = first + step;
-      MappingEntry mapping = mappings.get(middle);
-      if (generatedLine < mapping.getGeneratedLine() ||
-          (generatedLine == mapping.getGeneratedLine() && generatedColumn < mapping.getGeneratedColumn())) {
-        count = step;
-      }
-      else {
-        first = middle;
-        count -= step;
-      }
-    }
-    return first;
-  }
-
-  @Nullable
-  public MappingEntry findFirstEntryInLine(@NotNull List<Url> sourceUrls, int sourceLine, @Nullable VirtualFile sourceFile, @Nullable NullableLazyValue<SourceResolver.Resolver> resolver) {
-    MappingEntry[] entries = sourceResolver.findEntries(sourceUrls, this, sourceFile);
-    if (entries == null) {
+  public boolean processMappingsInLine(@NotNull List<Url> sourceUrls,
+                                       int sourceLine,
+                                       @NotNull Processor<MappingEntry> mappingProcessor,
+                                       @Nullable VirtualFile sourceFile,
+                                       @Nullable NullableLazyValue<SourceResolver.Resolver> resolver) {
+    MappingList mappings = sourceResolver.findMappings(sourceUrls, this, sourceFile);
+    if (mappings == null) {
       if (resolver != null) {
         SourceResolver.Resolver resolverValue = resolver.getValue();
         if (resolverValue != null) {
-          entries = sourceResolver.findEntries(this, resolverValue);
+          mappings = sourceResolver.findMappings(this, resolverValue);
         }
       }
-      if (entries == null) {
-        return null;
+      if (mappings == null) {
+        return false;
       }
     }
+    return mappings.processMappingsInLine(sourceLine, mappingProcessor);
+  }
 
-    int low = 0;
-    int high = entries.length - 1;
-    while (low <= high) {
-      int middle = (low + high) >>> 1;
-      MappingEntry mapping = entries[middle];
-      if (sourceLine == mapping.getSourceLine()) {
-        // find first
-        int firstEntryIndex = middle;
-        while (firstEntryIndex > 0 && entries[firstEntryIndex - 1].getSourceLine() == mapping.getSourceLine()) {
-          firstEntryIndex--;
-        }
-        return entries[firstEntryIndex];
-      }
-      else if (sourceLine > mapping.getSourceLine()) {
-        low = middle + 1;
-      }
-      else {
-        high = middle - 1;
-      }
-    }
-    return null;
+  @NotNull
+  public MappingList getMappingsOrderedBySource(int source) {
+    return sourceIndexToMappings[source];
   }
 }
