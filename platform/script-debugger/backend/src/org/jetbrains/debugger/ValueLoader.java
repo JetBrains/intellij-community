4cab25e18332c1101f1b2e459fecf15a1af71f63
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-04-04 14:09:51 +0200

    nullability (cherry picked from commit ad81d5c)

--- platform/script-debugger/backend/src/org/jetbrains/debugger/ValueLoader.java
+++ platform/script-debugger/backend/src/org/jetbrains/debugger/ValueLoader.java
@@ -2,6 +2,7 @@ package org.jetbrains.debugger;
 
 import com.intellij.openapi.util.AsyncResult;
 import com.intellij.openapi.util.AsyncValueLoaderManager;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -10,18 +11,18 @@ public abstract class ValueLoader<OBJECT_VALUE extends ObjectValueBase> {
 
   protected final AsyncValueLoaderManager<ObjectValueBase, ObjectPropertyData> propertiesLoader = new AsyncValueLoaderManager<ObjectValueBase, ObjectPropertyData>(ObjectValueBase.PROPERTY_DATA_UPDATER) {
     @Override
-    public boolean checkFreshness(ObjectValueBase host, ObjectPropertyData data) {
+    public boolean checkFreshness(@NotNull ObjectValueBase host, @NotNull ObjectPropertyData data) {
       return cacheStampRef.get() == data.getCacheState();
     }
 
     @Override
-    public void load(ObjectValueBase host, AsyncResult<ObjectPropertyData> result) {
+    public void load(@NotNull ObjectValueBase host, @NotNull AsyncResult<ObjectPropertyData> result) {
       //noinspection unchecked
       loadProperties(((OBJECT_VALUE)host), result);
     }
   };
 
-  protected abstract void loadProperties(OBJECT_VALUE value, AsyncResult<ObjectPropertyData> result);
+  protected abstract void loadProperties(@NotNull OBJECT_VALUE value, @NotNull AsyncResult<ObjectPropertyData> result);
 
   public AsyncValueLoaderManager<ObjectValueBase, ObjectPropertyData> getPropertiesLoader() {
     return propertiesLoader;
