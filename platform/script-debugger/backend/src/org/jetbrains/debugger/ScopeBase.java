46e292142fbbc5c15a65a8d646721429ccec7115
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-04-04 14:09:48 +0200

    nullability (cherry picked from commit 63f68f2)

--- platform/script-debugger/backend/src/org/jetbrains/debugger/ScopeBase.java
+++ platform/script-debugger/backend/src/org/jetbrains/debugger/ScopeBase.java
@@ -1,12 +1,13 @@
 package org.jetbrains.debugger;
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public abstract class ScopeBase implements Scope {
   private final Type type;
   private final String className;
 
-  protected ScopeBase(Type type, String className) {
+  protected ScopeBase(@NotNull Type type, @Nullable String className) {
     this.type = type;
     this.className = className;
   }
@@ -17,6 +18,7 @@ public abstract class ScopeBase implements Scope {
     return className;
   }
 
+  @NotNull
   @Override
   public Type getType() {
     return type;
