a0b16a0a9c9cf65bd155a4e76a882f2f32edff66
Denis Fokin <Denis.Fokin@jetbrains.com>
2014-04-08 18:56:19 +0400

    IDEA-123466 Sheet dialogs doesn't transfer focus back then closed

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/ToolWindowManagerImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/ToolWindowManagerImpl.java
@@ -2072,12 +2072,12 @@ public final class ToolWindowManagerImpl extends ToolWindowManagerEx implements 
   private final class EditorComponentFocusWatcher extends FocusWatcher {
     @Override
     protected void focusedComponentChanged(final Component component, final AWTEvent cause) {
-      if (myWindowManager.getCommandProcessor().getCommandCount() > 0 || component == null) {
-        return;
-      }
-
       final KeyboardFocusManager mgr = KeyboardFocusManager.getCurrentKeyboardFocusManager();
       final Component owner = mgr.getFocusOwner();
+
+      if (myWindowManager.getCommandProcessor().getCommandCount() > 0 || component == null || component== owner) {
+        return;
+      }
 
       IdeFocusManager.getInstance(myProject).doWhenFocusSettlesDown(new ExpirableRunnable.ForProject(myProject) {
         @Override
