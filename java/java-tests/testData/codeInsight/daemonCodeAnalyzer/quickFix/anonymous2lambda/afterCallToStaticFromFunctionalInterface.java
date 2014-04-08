c8bbd668503564f7f3714f140ab93c8635b75703
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-04-08 13:25:01 +0200

    anonym -> lambda, method ref: do not suggest replacement when default method is called out of functional interface context

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/anonymous2lambda/afterCallToStaticFromFunctionalInterface.java
@@ -0,0 +1,13 @@
+// "Replace with lambda" "true"
+class Test {
+  interface InOut {
+    void run() throws IOException;
+    static void foo(){}
+  }
+
+  InOut bind() {
+    return () -> {
+      InOut.foo();
+    };
+  }
+}
