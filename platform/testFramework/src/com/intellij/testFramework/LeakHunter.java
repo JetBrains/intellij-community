53d6460204a6a801234141c6353ea0b051e558f4
Yann Cébron <yann.cebron@jetbrains.com>
2014-04-08 22:28:02 +0200

    cleanup imports

--- platform/testFramework/src/com/intellij/testFramework/LeakHunter.java
+++ platform/testFramework/src/com/intellij/testFramework/LeakHunter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2011 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,8 +16,6 @@
 package com.intellij.testFramework;
 
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.extensions.ExtensionPoint;
 import com.intellij.openapi.extensions.Extensions;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.impl.ProjectImpl;
