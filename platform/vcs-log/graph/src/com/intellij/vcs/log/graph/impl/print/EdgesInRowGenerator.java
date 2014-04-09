3784b459b0b928935a9387919dfcec97c46da874
Erokhin Stanislav <erokhins239@gmail.com>
2014-04-09 14:03:28 +0400

    [log] Remake EdgesInRowGenerator.

--- platform/vcs-log/graph/src/com/intellij/vcs/log/graph/impl/print/EdgesInRowGenerator.java
+++ platform/vcs-log/graph/src/com/intellij/vcs/log/graph/impl/print/EdgesInRowGenerator.java
@@ -13,25 +13,167 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.intellij.vcs.log.graph.impl.print;
 
+import com.intellij.util.SmartList;
+import com.intellij.util.containers.SLRUMap;
 import com.intellij.vcs.log.graph.api.PrintedLinearGraph;
 import com.intellij.vcs.log.graph.api.elements.GraphEdge;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class EdgesInRowGenerator {
-  @NotNull
-  private final PrintedLinearGraph myPrintedLinearGraph;
+  private static final int CACHE_SIZE = 10;
+  private static final int BLOCK_SIZE = 10;
 
-  public EdgesInRowGenerator(@NotNull PrintedLinearGraph printedLinearGraph) {
-    myPrintedLinearGraph = printedLinearGraph;
+  private final int WALK_SIZE;
+
+  @NotNull
+  private final PrintedLinearGraph myGraph;
+
+  @NotNull
+  private final SLRUMap<Integer, GraphEdges> cacheNU = new SLRUMap<Integer, GraphEdges>(CACHE_SIZE, CACHE_SIZE * 2);
+  private final SLRUMap<Integer, GraphEdges> cacheND = new SLRUMap<Integer, GraphEdges>(CACHE_SIZE, CACHE_SIZE * 2);
+
+  public EdgesInRowGenerator(@NotNull PrintedLinearGraph graph) {
+    this(graph, 1000);
+  }
+
+  public EdgesInRowGenerator(@NotNull PrintedLinearGraph graph, int walk_size) {
+    myGraph = graph;
+    WALK_SIZE = walk_size;
   }
 
   @NotNull
   public Set<GraphEdge> getEdgesInRow(int rowIndex) {
-    return null;
+    GraphEdges neighborU = getNeighborU(rowIndex);
+    while (neighborU.myRow < rowIndex) {
+      neighborU = oneDownStep(neighborU);
+    }
+
+    GraphEdges neighborD = getNeighborD(rowIndex);
+    while (neighborU.myRow > rowIndex) {
+      neighborU = oneUpStep(neighborU);
+    }
+
+    Set<GraphEdge> result = neighborU.myEdges;
+    result.addAll(neighborD.myEdges);
+    return result;
+  }
+
+  public void invalidate() {
+    cacheNU.clear();
+    cacheND.clear();
+  }
+
+  @NotNull
+  private GraphEdges getNeighborU(int rowIndex) {
+    int upNeighborIndex = getUpNeighborIndex(rowIndex);
+    GraphEdges graphEdges = cacheNU.get(upNeighborIndex);
+    if (graphEdges == null) {
+      graphEdges = getUCorrectEdges(upNeighborIndex);
+      cacheNU.put(upNeighborIndex, graphEdges);
+    }
+    return graphEdges;
+  }
+
+  @NotNull
+  private GraphEdges getNeighborD(int rowIndex) {
+    int downNeighborIndex = getUpNeighborIndex(rowIndex) + 1;
+
+    if (downNeighborIndex >= myGraph.nodesCount()) {
+      return new GraphEdges(myGraph.nodesCount() - 1);
+    }
+
+    GraphEdges graphEdges = cacheND.get(downNeighborIndex);
+    if (graphEdges == null) {
+      graphEdges = getDCorrectEdges(downNeighborIndex);
+      cacheNU.put(downNeighborIndex, graphEdges);
+    }
+    return graphEdges;
+  }
+
+  private static int getUpNeighborIndex(int rowIndex) {
+    return (rowIndex / BLOCK_SIZE) * BLOCK_SIZE;
+  }
+
+  @NotNull
+  private GraphEdges getUCorrectEdges(int rowIndex) {
+    int startCalculateIndex = Math.max(rowIndex - WALK_SIZE, 0);
+    GraphEdges graphEdges = new GraphEdges(startCalculateIndex);
+
+    for (int i = startCalculateIndex; i < rowIndex; i++) {
+      graphEdges = oneDownStep(graphEdges);
+    }
+    return graphEdges;
+  }
+
+  @NotNull
+  private GraphEdges getDCorrectEdges(int rowIndex) {
+    int endCalculateIndex = Math.min(rowIndex + WALK_SIZE, myGraph.nodesCount() - 1);
+    GraphEdges graphEdges = new GraphEdges(endCalculateIndex);
+
+    for (int i = endCalculateIndex; i > rowIndex; i--) {
+      graphEdges = oneUpStep(graphEdges);
+    }
+    return graphEdges;
+  }
+
+  @NotNull
+  private GraphEdges oneDownStep(@NotNull GraphEdges graphEdges) {
+    Set<GraphEdge> edgesInCurrentRow = graphEdges.myEdges;
+    int currentRow = graphEdges.myRow;
+
+    edgesInCurrentRow.removeAll(createUpEdges(currentRow + 1));
+    edgesInCurrentRow.addAll(createDownEdges(currentRow));
+
+    return new GraphEdges(edgesInCurrentRow, currentRow + 1);
+  }
+
+  @NotNull
+  private GraphEdges oneUpStep(@NotNull GraphEdges graphEdges) {
+    Set<GraphEdge> edgesInCurrentRow = graphEdges.myEdges;
+    int currentRow = graphEdges.myRow;
+
+    edgesInCurrentRow.removeAll(createDownEdges(currentRow - 1));
+    edgesInCurrentRow.addAll(createUpEdges(currentRow));
+    return new GraphEdges(edgesInCurrentRow, currentRow - 1);
+  }
+
+  public List<GraphEdge> createUpEdges(int nodeIndex) {
+    List<GraphEdge> result = new SmartList<GraphEdge>();
+    for (int upNode : myGraph.getUpNodes(nodeIndex)) {
+      GraphEdge.Type type =  myGraph.getEdgeType(upNode, nodeIndex);
+      result.add(new GraphEdge(upNode, nodeIndex, type));
+    }
+    return result;
+  }
+
+  public List<GraphEdge> createDownEdges(int nodeIndex) {
+    List<GraphEdge> result = new SmartList<GraphEdge>();
+    for (int downNode : myGraph.getDownNodes(nodeIndex)) {
+      GraphEdge.Type type =  myGraph.getEdgeType(nodeIndex, downNode);
+      result.add(new GraphEdge(nodeIndex, downNode, type));
+    }
+    return result;
+  }
+
+  private static class GraphEdges {
+    // this must be mutably set
+    @NotNull
+    private final Set<GraphEdge> myEdges;
+    private final int myRow;
+
+    private GraphEdges(int row) {
+      this(new HashSet<GraphEdge>(), row);
+    }
+
+    private GraphEdges(@NotNull Set<GraphEdge> edges, int row) {
+      myEdges = edges;
+      myRow = row;
+    }
   }
 }
