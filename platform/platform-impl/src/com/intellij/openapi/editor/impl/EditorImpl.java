6e8e8dc83d51c92062761af49297a53fcc2002e1
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2014-04-09 11:54:10 +0400

    EA-55239, EA-55618 - IllegalArgumentException
    (cherry picked from commit 5bc3f1d545e5f29820b3215d842cad0486d65c47)

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorImpl.java
@@ -4230,7 +4230,7 @@ public final class EditorImpl extends UserDataHolderBase implements EditorEx, Hi
       else {
         if (isColumnMode() || e.isAltDown()) {
           if (myCaretModel.supportsMultipleCarets()) {
-            if (myCurrentDragIsSubstantial || !newLogicalCaret.equals(myLastMousePressedLocation)) {
+            if (myLastMousePressedLocation != null && (myCurrentDragIsSubstantial || !newLogicalCaret.equals(myLastMousePressedLocation))) {
               selectionModel.setBlockSelection(myLastMousePressedLocation, newLogicalCaret);
               myCurrentDragIsSubstantial = true;
             }
@@ -4276,7 +4276,7 @@ public final class EditorImpl extends UserDataHolderBase implements EditorEx, Hi
           }
 
           if (!myMousePressedInsideSelection) {
-            if (myCaretModel.supportsMultipleCarets()) {
+            if (myCaretModel.supportsMultipleCarets() && myLastMousePressedLocation != null) {
               oldSelectionStart = logicalPositionToOffset(myLastMousePressedLocation);
               oldVisLeadSelectionStart = logicalToVisualPosition(myLastMousePressedLocation);
             }
@@ -4770,7 +4770,7 @@ public final class EditorImpl extends UserDataHolderBase implements EditorEx, Hi
                 return;
               }
 
-              if (myMultiSelectionInProgress) {
+              if (myMultiSelectionInProgress && myLastMousePressedLocation != null) {
                 myTargetMultiSelectionPosition = pos;
                 LogicalPosition newLogicalPosition = visualToLogicalPosition(pos);
                 getScrollingModel().scrollTo(newLogicalPosition, ScrollType.RELATIVE);
