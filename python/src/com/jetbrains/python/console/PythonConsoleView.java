24f029b98d9cdc2ac74c376571131551aa468047
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2014-04-10 02:54:22 +0200

    Dispose correctly console view.
    (cherry picked from commit 0a57685f505af1cf46e2072502421d50206c2387)

--- python/src/com/jetbrains/python/console/PythonConsoleView.java
+++ python/src/com/jetbrains/python/console/PythonConsoleView.java
@@ -76,7 +76,7 @@ public class PythonConsoleView extends JPanel implements LanguageConsoleView, Ob
   private boolean myHyperlink;
 
   private final LanguageConsoleViewImpl myLanguageConsoleView;
-  
+
   private Disposable mySplitDisposable;
 
   public PythonConsoleView(final Project project, final String title, final Sdk sdk) {
@@ -92,6 +92,7 @@ public class PythonConsoleView extends JPanel implements LanguageConsoleView, Ob
     languageConsole.initComponents();
 
     myLanguageConsoleView = new LanguageConsoleViewImpl(languageConsole);
+    Disposer.register(this, myLanguageConsoleView);
 
     add(myLanguageConsoleView.getComponent(), BorderLayout.CENTER);
 
@@ -372,7 +373,7 @@ public class PythonConsoleView extends JPanel implements LanguageConsoleView, Ob
 
   @Override
   public void dispose() {
-    myLanguageConsoleView.dispose();
+    Disposer.dispose(this);
   }
 
   @Override
@@ -414,7 +415,7 @@ public class PythonConsoleView extends JPanel implements LanguageConsoleView, Ob
     p.add(myLanguageConsoleView.getComponent(), JSplitPane.LEFT);
     mySplitDisposable = componentDisposable;
     p.add(component, JSplitPane.RIGHT);
-    p.setDividerLocation((int)getSize().getWidth()*2/3);
+    p.setDividerLocation((int)getSize().getWidth() * 2 / 3);
     add(p, BorderLayout.CENTER);
 
     validate();
