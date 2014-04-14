12e7a78b77258e27624c99429b25435a1f8d05c9
Michael Golubev <michael.golubev@jetbrains.com>
2014-04-14 17:29:39 +0200

    IDEA-123825 - Git Clouds detect Git remotes - OpenShift and generic

--- plugins/git4idea/remote-servers-git/src/com/intellij/remoteServer/util/importProject/CloudGitProjectStructureDetector.java
+++ plugins/git4idea/remote-servers-git/src/com/intellij/remoteServer/util/importProject/CloudGitProjectStructureDetector.java
@@ -28,9 +28,6 @@ import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.remoteServer.ServerType;
-import com.intellij.remoteServer.util.CloudAccountSelectionEditor;
-import com.intellij.remoteServer.util.CloudDeploymentNameConfiguration;
 import com.intellij.remoteServer.util.CloudGitDeploymentDetector;
 import git4idea.GitPlatformFacade;
 import git4idea.GitUtil;
@@ -41,25 +38,14 @@ import org.jetbrains.annotations.NotNull;
 import javax.swing.*;
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
  * @author michael.golubev
  */
-public abstract class CloudGitProjectStructureDetectorBase<DC extends CloudDeploymentNameConfiguration> extends ProjectStructureDetector {
+public class CloudGitProjectStructureDetector extends ProjectStructureDetector {
 
   private final JavaProjectStructureDetector myJavaDetector = new JavaProjectStructureDetector();
-
-  private final String myId;
-  private final String myJavaSourceRootTypeName;
-  private final CloudGitDeploymentDetector myDeploymentDetector;
-
-  protected CloudGitProjectStructureDetectorBase(ServerType serverType, CloudGitDeploymentDetector deploymentDetector) {
-    myId = serverType.getPresentableName();
-    myJavaSourceRootTypeName = "Java/" + myId;
-    myDeploymentDetector = deploymentDetector;
-  }
 
   @NotNull
   @Override
@@ -71,14 +57,14 @@ public abstract class CloudGitProjectStructureDetectorBase<DC extends CloudDeplo
 
     for (DetectedProjectRoot projectRoot : result) {
       if ((projectRoot instanceof CloudGitProjectRoot) && FileUtil.isAncestor(projectRoot.getDirectory(), dir, true)) {
-        return detectJavaRoots(dir, children, base, result);
+        return detectJavaRoots(((CloudGitProjectRoot)projectRoot).getJavaSourceRootTypeName(), dir, children, base, result);
       }
     }
 
     return DirectoryProcessingResult.PROCESS_CHILDREN;
   }
 
-  private void detectApplicationRoot(@NotNull File dir, @NotNull List<DetectedProjectRoot> result) {
+  private static void detectApplicationRoot(@NotNull File dir, @NotNull List<DetectedProjectRoot> result) {
     VirtualFile repositoryRoot = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(dir);
     if (repositoryRoot == null) {
       return;
@@ -94,15 +80,16 @@ public abstract class CloudGitProjectStructureDetectorBase<DC extends CloudDeplo
       = GitRepositoryImpl.getLightInstance(repositoryRoot, project, ServiceManager.getService(project, GitPlatformFacade.class), project);
     repository.update();
 
-    List<String> applicationNames = myDeploymentDetector.collectApplicationNames(repository);
-    if (applicationNames.isEmpty()) {
-      return;
+    for (CloudGitDeploymentDetector deploymentDetector : CloudGitDeploymentDetector.EP_NAME.getExtensions()) {
+      String applicationName = deploymentDetector.getFirstApplicationName(repository);
+      if (applicationName != null) {
+        result.add(new CloudGitProjectRoot(deploymentDetector, dir, repositoryRoot, applicationName));
+      }
     }
-
-    result.add(new CloudGitProjectRoot(myId, myJavaSourceRootTypeName, dir, repositoryRoot, applicationNames.get(0)));
   }
 
-  private DirectoryProcessingResult detectJavaRoots(@NotNull File dir,
+  private DirectoryProcessingResult detectJavaRoots(String javaSourceRootTypeName,
+                                                    @NotNull File dir,
                                                     @NotNull File[] children,
                                                     @NotNull File base,
                                                     @NotNull List<DetectedProjectRoot> result) {
@@ -110,25 +97,20 @@ public abstract class CloudGitProjectStructureDetectorBase<DC extends CloudDeplo
     DirectoryProcessingResult processingResult = myJavaDetector.detectRoots(dir, children, base, detectedJavaRoots);
     for (DetectedProjectRoot detectedJavaRoot : detectedJavaRoots) {
       if (detectedJavaRoot instanceof JavaModuleSourceRoot) {
-        result.add(new CloudGitJavaSourceRoot(myJavaSourceRootTypeName, (JavaModuleSourceRoot)detectedJavaRoot));
+        result.add(new CloudGitJavaSourceRoot(javaSourceRootTypeName, (JavaModuleSourceRoot)detectedJavaRoot));
       }
     }
     return processingResult;
   }
 
   @Override
-  public String getDetectorId() {
-    return myId;
-  }
-
-  @Override
   public List<ModuleWizardStep> createWizardSteps(ProjectFromSourcesBuilder builder,
                                                   ProjectDescriptor projectDescriptor,
                                                   Icon stepIcon) {
-    return Collections.<ModuleWizardStep>singletonList(new CloudGitChooseAccountStep<DC>(this, builder, projectDescriptor));
+    List<ModuleWizardStep> result = new ArrayList<ModuleWizardStep>();
+    for (CloudGitDeploymentDetector deploymentDetector : CloudGitDeploymentDetector.EP_NAME.getExtensions()) {
+      result.add(new CloudGitChooseAccountStepImpl(deploymentDetector, this, builder, projectDescriptor));
+    }
+    return result;
   }
-
-  public abstract CloudAccountSelectionEditor<?, DC, ?> createAccountSelectionEditor();
-
-  public abstract DC createDeploymentConfiguration();
 }
