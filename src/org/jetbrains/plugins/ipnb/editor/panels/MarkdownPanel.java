1911527010b60eca51eb0c94b68d7df3e21b7ba8
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2014-04-15 18:23:17 -0400

    initial formulas rendering

--- src/org/jetbrains/plugins/ipnb/editor/panels/MarkdownPanel.java
+++ src/org/jetbrains/plugins/ipnb/editor/panels/MarkdownPanel.java
@@ -15,23 +15,36 @@
  */
 package org.jetbrains.plugins.ipnb.editor.panels;
 
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.ui.JBColor;
 import com.intellij.ui.components.JBLabel;
-import org.jetbrains.annotations.NotNull;
+import net.sourceforge.jeuclid.swing.JMathComponent;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.ipnb.IpnbUtils;
+import org.jetbrains.plugins.ipnb.editor.IpnbEditorUtil;
 import org.jetbrains.plugins.ipnb.format.cells.MarkdownCell;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import uk.ac.ed.ph.snuggletex.SnuggleEngine;
+import uk.ac.ed.ph.snuggletex.SnuggleInput;
+import uk.ac.ed.ph.snuggletex.SnuggleSession;
+import uk.ac.ed.ph.snuggletex.XMLStringOutputOptions;
+import uk.ac.ed.ph.snuggletex.internal.util.XMLUtilities;
+import uk.ac.ed.ph.snuggletex.utilities.SimpleStylesheetCache;
+import uk.ac.ed.ph.snuggletex.utilities.StylesheetManager;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.IOException;
 
 /**
  * @author traff
  */
 public class MarkdownPanel extends JPanel {
+  private static final Logger LOG = Logger.getInstance(MarkdownPanel.class);
   private boolean myEditing = false;
   private Project myProject;
 
@@ -39,8 +52,7 @@ public class MarkdownPanel extends JPanel {
     super(new BorderLayout());
     myProject = project;
     final String text = StringUtil.join(cell.getSource(), "\n");
-    final String html = IpnbUtils.markdown2Html(text);
-    add(createPanel(html), BorderLayout.CENTER);
+    initPanel(text);
 
 
     addMouseListener(new MouseAdapter() {
@@ -54,11 +66,50 @@ public class MarkdownPanel extends JPanel {
     });
   }
 
-  private JLabel createPanel(@NotNull final String text) {
-    JLabel label = new JBLabel(text);
-    label.setBackground(JBColor.WHITE);  // TODO: use background colour from settings
-    label.setOpaque(true);
-    return label;
+  private void initPanel(@Nullable  String text) {
+    if (text == null) return;
+    setLayout(new GridBagLayout());
+    GridBagConstraints c = new GridBagConstraints();
+    c.fill = GridBagConstraints.HORIZONTAL;
+    c.gridy = 0;
+    c.gridx = 0;
+
+    final SnuggleEngine engine = new SnuggleEngine(new SimpleStylesheetCache());
+    final SnuggleSession session = engine.createSession();
+
+    final SnuggleInput input = new SnuggleInput(text);
+    try {
+      session.parseInput(input);
+    }
+    catch (IOException e) {
+      LOG.error("Couldn't parse formula " + text);
+      return;
+    }
+    final NodeList nodes = session.buildDOMSubtree();
+    for (int i = 0; i != nodes.getLength(); ++i) {
+      final Node node = nodes.item(i);
+      final StylesheetManager stylesheetManager = session.getStylesheetManager();
+      final XMLStringOutputOptions xmlStringOutputOptions = engine.getDefaultXMLStringOutputOptions();
+      final String str = XMLUtilities.serializeNodeChildren(stylesheetManager, node, xmlStringOutputOptions);
+
+      if (str.startsWith("<math")) {
+        final JMathComponent comp = new JMathComponent();
+        comp.setBackground(IpnbEditorUtil.getBackground());
+        comp.setOpaque(true);
+        comp.setContent("<html>" + str + "</html>");
+        add(comp, c);
+      }
+      else {
+        final String html = IpnbUtils.markdown2Html(str);
+        final JBLabel comp = new JBLabel(html);
+        comp.setBackground(IpnbEditorUtil.getBackground());
+        comp.setOpaque(true);
+        add(comp, c);
+      }
+      c.gridy = c.gridy + 1;
+    }
+    setBackground(IpnbEditorUtil.getBackground());
+    setOpaque(true);
   }
 
   public boolean isEditing() {
