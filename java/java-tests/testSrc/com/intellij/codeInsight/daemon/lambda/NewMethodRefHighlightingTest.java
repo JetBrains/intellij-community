57e886f7a4841a50ed74be84d696b805ec1ffd69
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-04-22 16:28:00 +0200

    testdata: check that method ref on static interface method is accepted

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewMethodRefHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewMethodRefHighlightingTest.java
@@ -237,6 +237,10 @@ public class NewMethodRefHighlightingTest extends LightDaemonAnalyzerTestCase {
     doTest();
   }
 
+  public void testRefOnStaticInterfaceMethod() throws Exception {
+    doTest();
+  }
+
   private void doTest() {
     doTest(false);
   }
