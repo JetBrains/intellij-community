1cbffd0a02ee11bb36585cfd69848638dfc32f07
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-04-23 20:10:19 +0200

    IDEA-124271 (cherry picked from commit b8fb550)

--- java/java-analysis-impl/src/com/intellij/codeInsight/daemon/impl/analysis/JavaGenericsUtil.java
+++ java/java-analysis-impl/src/com/intellij/codeInsight/daemon/impl/analysis/JavaGenericsUtil.java
@@ -281,7 +281,7 @@ public class JavaGenericsUtil {
       if (typeParameter == null) return null;
       PsiClass owner = (PsiClass)typeParameter.getOwner();
       if (owner == null) return null;
-      PsiSubstitutor superClassSubstitutor = TypeConversionUtil.getClassSubstitutor(owner, aClass, PsiSubstitutor.EMPTY);
+      PsiSubstitutor superClassSubstitutor = TypeConversionUtil.getClassSubstitutor(owner, aClass, substitutor);
       if (superClassSubstitutor == null) return null;
       PsiType itemType = superClassSubstitutor.substitute(typeParameter);
       itemType = substitutor.substitute(itemType);
