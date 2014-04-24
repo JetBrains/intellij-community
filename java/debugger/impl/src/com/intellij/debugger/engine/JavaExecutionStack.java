24fb430feaf1b07b6b387e3e348296462bf6d68a
Egor.Ushakov <egor.ushakov@jetbrains.com>
2014-04-24 20:22:22 +0400

    java-xdebugger: correctly calculate frames

--- java/debugger/impl/src/com/intellij/debugger/engine/JavaExecutionStack.java
+++ java/debugger/impl/src/com/intellij/debugger/engine/JavaExecutionStack.java
@@ -20,8 +20,10 @@ import com.intellij.debugger.engine.events.DebuggerCommandImpl;
 import com.intellij.debugger.jdi.StackFrameProxyImpl;
 import com.intellij.debugger.jdi.ThreadReferenceProxyImpl;
 import com.intellij.debugger.ui.impl.watch.MethodsTracker;
+import com.intellij.debugger.ui.impl.watch.ThreadDescriptorImpl;
 import com.intellij.icons.AllIcons;
 import com.intellij.xdebugger.frame.XExecutionStack;
+import com.sun.jdi.ThreadReference;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -66,18 +68,35 @@ public class JavaExecutionStack extends XExecutionStack {
   }
 
   @Override
-  public void computeStackFrames(int firstFrameIndex, final XStackFrameContainer container) {
+  public void computeStackFrames(final int firstFrameIndex, final XStackFrameContainer container) {
     myDebugProcess.getManagerThread().schedule(new DebuggerCommandImpl() {
       @Override
       protected void action() throws Exception {
         List<JavaStackFrame> frames = new ArrayList<JavaStackFrame>();
-        boolean top = true;
-        for (StackFrameProxyImpl frameProxy : myThreadProxy.frames()) {
-          if (top) {
-            top = false;
-            continue;
+        if (!myThreadProxy.isCollected() && myDebugProcess.getSuspendManager().isSuspended(myThreadProxy)) {
+          int status = myThreadProxy.status();
+          if (!(status == ThreadReference.THREAD_STATUS_UNKNOWN) &&
+              !(status == ThreadReference.THREAD_STATUS_NOT_STARTED) &&
+              !(status == ThreadReference.THREAD_STATUS_ZOMBIE)) {
+            try {
+              int framesToSkip = firstFrameIndex;
+              for (StackFrameProxyImpl stackFrame : myThreadProxy.frames()) {
+                if (framesToSkip > 0) {
+                  framesToSkip--;
+                  continue;
+                }
+                //Method method = stackFrame.location().method();
+                //ToDo :check whether is synthetic if (shouldDisplay(method)) {
+                //myChildren.add(myNodeManager.createNode(myNodeManager.getStackFrameDescriptor(threadDescriptor, stackFrame),
+                //                                        getDebuggerContext().createEvaluationContext()));
+                frames.add(new JavaStackFrame(stackFrame, myDebugProcess, myTracker));
+              }
+            }
+            catch (EvaluateException e) {
+              container.errorOccurred(e.getMessage());
+              return;
+            }
           }
-          frames.add(new JavaStackFrame(frameProxy, myDebugProcess, myTracker));
         }
         container.addStackFrames(frames, true);
       }
