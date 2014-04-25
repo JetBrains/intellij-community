f7ee188394e32abbbe914338bb26b0086839945d
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-04-25 18:35:28 +0200

    create method from method ref: fis for type element qualifier (IDEA-124485)
    (cherry picked from commit a4f2f0e73bff04c2e5cfae106cdf946cf6aaf0be)

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/createMethodFromMethodRef/afterTypeElementQualifier.java
@@ -0,0 +1,11 @@
+// "Create Method 'fooBar'" "true"
+class FooBar {
+  {
+    Runnable r = Container<String>::fooBar;
+  }
+}
+class Container<T>{
+    public static void fooBar() {
+        
+    }
+}
