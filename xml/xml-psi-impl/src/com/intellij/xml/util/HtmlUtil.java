77470ec74adb706049cb2e7cfc5ec0b11de5a3a6
Dennis Ushakov <dennis.ushakov@gmail.com>
2014-04-28 12:34:42 +0400

    fix test (cherry picked from commit 403d4f1) r=zolotov

--- xml/xml-psi-impl/src/com/intellij/xml/util/HtmlUtil.java
+++ xml/xml-psi-impl/src/com/intellij/xml/util/HtmlUtil.java
@@ -420,7 +420,7 @@ public class HtmlUtil {
   }
 
   public static boolean isHtmlTag(@NotNull XmlTag tag) {
-    if (tag.getLanguage() == XHTMLLanguage.INSTANCE) return false;
+    if (tag.getLanguage() != HTMLLanguage.INSTANCE) return false;
 
     XmlDocument doc = PsiTreeUtil.getParentOfType(tag, XmlDocument.class);
 
