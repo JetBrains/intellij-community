6c1e59f307cf307c70b957825564d0d94877b7a4
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2014-04-29 12:58:05 +0400

    removed duplicated test see DjangoTestRunnerTest

--- python/testSrc/com/jetbrains/env/django/DjangoPathTest.java
+++ python/testSrc/com/jetbrains/env/django/DjangoPathTest.java
@@ -5,10 +5,7 @@ import com.intellij.execution.configurations.ConfigurationFactory;
 import com.intellij.openapi.util.io.FileUtil;
 import com.jetbrains.django.run.DjangoServerRunConfiguration;
 import com.jetbrains.django.run.DjangoServerRunConfigurationType;
-import com.jetbrains.django.testRunner.DjangoTestsConfigurationType;
-import com.jetbrains.django.testRunner.DjangoTestsRunConfiguration;
 import com.jetbrains.env.python.debug.PyEnvTestCase;
-import com.jetbrains.python.packaging.*;
 import com.jetbrains.python.run.AbstractPythonRunConfiguration;
 import junit.framework.Assert;
 import org.jetbrains.annotations.Nullable;
@@ -24,7 +21,7 @@ public class DjangoPathTest extends PyEnvTestCase {
   /**
    * This string is printed after printing sys.path in djangoPath/settings.py
    */
-  public static final String THE_END_OF_PROCESS = "Process finished with exit code 0";
+  public static final String THE_END_OF_SYS_PATH = "The end of sys.path";
 
   public void testRunserverPath() throws IOException {
     runPythonTest(new DjangoPathTestTask() {
@@ -43,7 +40,7 @@ public class DjangoPathTest extends PyEnvTestCase {
 
 
       public void testing() throws Exception {
-        waitForOutput(THE_END_OF_PROCESS);
+        waitForOutput(THE_END_OF_SYS_PATH);
 
         doTest(output(), getTestDataPath());
       }
@@ -53,14 +50,15 @@ public class DjangoPathTest extends PyEnvTestCase {
   private static void doTest(String output, String testDataPath) {
     final String[] splittedOutput = output.split("\\n");
     final ArrayList<String> outputList = Lists.newArrayList();
-
-    boolean add = false;
     for (String s : splittedOutput) {
-      if ("sys.path end".equals(s)) add = false;
-      if (add) outputList.add(norm(s));
-      if ("sys.path start".equals(s)) add = true;
+      if (s.equals(THE_END_OF_SYS_PATH)) {
+        break;
+      }
+      outputList.add(norm(s));
     }
+
     testDataPath = norm(testDataPath);
+
 
     Assert.assertEquals(testDataPath, outputList.get(1));
 
@@ -70,38 +68,6 @@ public class DjangoPathTest extends PyEnvTestCase {
 
   private static String norm(String testDataPath) {
     return FileUtil.toSystemIndependentName(testDataPath);
-  }
-
-  public void testTestPath() throws IOException {
-    runPythonTest(new DjangoPathTestTask() {
-
-      @Override
-      public ConfigurationFactory getFactory() {
-        return DjangoTestsConfigurationType.getInstance().getConfigurationFactories()[0];
-      }
-
-      @Override
-      protected void configure(AbstractPythonRunConfiguration config) {
-        String target = "mysite.SimpleTest";
-        try {
-          final PyPackage django = ((PyPackageManagerImpl)PyPackageManager.getInstance(config.getSdk())).findPackage("django");
-          if (django != null) {
-            if (django.matches(PyRequirement.fromStringGuaranteed("django>=1.6a"))) {
-              target = "mysite.tests.SimpleTest";
-            }
-          }
-        }
-        catch (PyExternalProcessException ignored) {
-        }
-
-        ((DjangoTestsRunConfiguration)config).setTarget(target);
-      }
-
-      public void testing() throws Exception {
-        waitForOutput(THE_END_OF_PROCESS);
-        doTest(output(), getTestDataPath());
-      }
-    });
   }
 
   public void testManagePath() throws IOException {
@@ -114,12 +80,12 @@ public class DjangoPathTest extends PyEnvTestCase {
       }
 
       public void testing() throws Exception {
-        waitForOutput(THE_END_OF_PROCESS);
+        waitForOutput(THE_END_OF_SYS_PATH);
 
         final String[] splittedOutput = output().split("\\n");
         final ArrayList<String> outputList = Lists.newArrayList();
         for (String s : splittedOutput) {
-          if (s.equals(THE_END_OF_PROCESS)) {
+          if (s.equals(THE_END_OF_SYS_PATH)) {
             break;
           }
           outputList.add(s);
