JetBrains/intellij-community

commit f3eda435f9b80d9dcece36eb7213d5a71332ee99

fixed PY-12506 Updating skeletons is run twice
setupSdkPaths is called from PythonSdkDetailsStep (see callBack) so there is no need to call it second time

python/src/com/jetbrains/python/sdk/PythonSdkType.java
@@ -288,7 +288,6 @@ public class PythonSdkType extends SdkType {
         public void consume(@Nullable Sdk sdk) {
           if (sdk != null) {
             sdk.putUserData(SDK_CREATOR_COMPONENT_KEY, new WeakReference<Component>(parentComponent));
-            sdkCreatedCallback.consume(sdk);
           }
         }
       });

commit 21de2af88fbc1b9a23aa81a9f40b52a92530343f

fixed PY-12848 Adding local Python SDK does not work
Regression produced in f3eda43

python/src/com/jetbrains/python/sdk/PythonSdkType.java
@@ -288,6 +288,7 @@ public class PythonSdkType extends SdkType {
         public void consume(@Nullable Sdk sdk) {
           if (sdk != null) {
             sdk.putUserData(SDK_CREATOR_COMPONENT_KEY, new WeakReference<Component>(parentComponent));
+            sdkCreatedCallback.consume(sdk);
           }
         }
       });
@@ -498,6 +499,11 @@ public class PythonSdkType extends SdkType {
       project = CommonDataKeys.PROJECT.getData(DataManager.getInstance().getDataContext());
     }
     setupSdkPaths(sdk, project, ownerComponent);
+  }
+
+  @Override
+  public boolean setupSdkPaths(Sdk sdk, SdkModel sdkModel) {
+    return true;  // run setupSdkPaths only once (from PythonSdkDetailsStep). Skip this from showCustomCreateUI
   }
 
   public static void setupSdkPaths(Sdk sdk, @Nullable Project project, @Nullable Component ownerComponent) {