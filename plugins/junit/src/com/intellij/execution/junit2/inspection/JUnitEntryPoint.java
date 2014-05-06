5a44bef4bc58adf7d68bf26890169ec9a9e1f77d
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-05-06 13:30:33 +0400

    ignore @org.mockito.Spy (IDEA-124802)
    (cherry picked from commit 459d870ccf09cfd3d3e1d4c648d923648a8bfcdb)

--- plugins/junit/src/com/intellij/execution/junit2/inspection/JUnitEntryPoint.java
+++ plugins/junit/src/com/intellij/execution/junit2/inspection/JUnitEntryPoint.java
@@ -98,6 +98,11 @@ public class JUnitEntryPoint extends EntryPoint {
 
   @Override
   public String[] getIgnoreAnnotations() {
-    return new String[]{"org.junit.Rule", "org.mockito.Mock", "org.mockito.InjectMocks", "org.junit.ClassRule", "org.junit.experimental.theories.DataPoint"};
+    return new String[]{"org.junit.Rule",
+                        "org.mockito.Mock",
+                        "org.mockito.Spy",
+                        "org.mockito.InjectMocks",
+                        "org.junit.ClassRule",
+                        "org.junit.experimental.theories.DataPoint"};
   }
 }
