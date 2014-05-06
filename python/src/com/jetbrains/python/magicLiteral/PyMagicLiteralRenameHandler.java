1fda81df3dfc77d866aac69e037d0e7a8b9ef347
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2014-05-06 18:44:19 +0200

    Temp remove.

--- python/src/com/jetbrains/python/magicLiteral/PyMagicLiteralRenameHandler.java
+++ python/src/com/jetbrains/python/magicLiteral/PyMagicLiteralRenameHandler.java
@@ -35,23 +35,7 @@ import org.jetbrains.annotations.Nullable;
 public class PyMagicLiteralRenameHandler implements RenameHandler {
   @Override
   public boolean isAvailableOnDataContext(DataContext dataContext) {
-    final Editor editor = CommonDataKeys.EDITOR.getData(dataContext);
-    if (editor == null) {
-      return false;
-    }
-
-    final PsiFile file = CommonDataKeys.PSI_FILE.getData(dataContext);
-    if (file == null) {
-      return false;
-    }
-
-    final PsiElement element = getElement(file, editor);
-
-    if (element == null || !PyMagicLiteralTools.isMagicLiteral(element)) {
-      return false;
-    }
-
-    return true;
+    return false;
   }
 
   @Nullable
@@ -70,11 +54,6 @@ public class PyMagicLiteralRenameHandler implements RenameHandler {
 
   @Override
   public void invoke(@NotNull Project project, Editor editor, PsiFile file, DataContext dataContext) {
-    PsiElement element = getElement(file, editor);
-    if (element == null) {
-      return;
-    }
-    RenameDialog.showRenameDialog(dataContext, new RenameDialog(project, element, null, editor));
   }
 
   @Override
