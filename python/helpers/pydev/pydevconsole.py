5455834321272e41aedc4aa0fdc822cb7e47eeab
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2014-05-07 23:00:42 +0200

    IPython should be enabled by default.

--- python/helpers/pydev/pydevconsole.py
+++ python/helpers/pydev/pydevconsole.py
@@ -179,7 +179,7 @@ def process_exec_queue(interpreter):
 if 'IPYTHONENABLE' in os.environ:
     IPYTHON = bool(os.environ['IPYTHONENABLE'])
 else:
-    IPYTHON = False
+    IPYTHON = True
 
 try:
     try:
