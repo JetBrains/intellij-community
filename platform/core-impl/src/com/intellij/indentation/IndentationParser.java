3c41d20812cf20a72689207d7a68fa7e0727a1e0
Andrey Vokin <andrey.vokin@jetbrains.com>
2014-05-07 19:38:02 +0400

    Embedding Ruby code into Slim.

--- platform/core-impl/src/com/intellij/indentation/IndentationParser.java
+++ platform/core-impl/src/com/intellij/indentation/IndentationParser.java
@@ -23,6 +23,9 @@ import com.intellij.util.containers.Stack;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author oleg
  */
@@ -35,6 +38,21 @@ public abstract class IndentationParser implements PsiParser {
   private final IElementType myBlockElementType;
   @NotNull
   private final IElementType myDocumentType;
+  private final List<IElementType> myContainerTypes;
+
+  public IndentationParser(
+    @NotNull IElementType documentType,
+    @NotNull final IElementType blockElementType,
+    @NotNull final IElementType eolTokenType,
+    @NotNull final IElementType indentTokenType,
+    final List<IElementType> containerTypes)
+  {
+    myDocumentType = documentType;
+    myBlockElementType = blockElementType;
+    myEolTokenType = eolTokenType;
+    myIndentTokenType = indentTokenType;
+    myContainerTypes = containerTypes;
+  }
 
   public IndentationParser(
     @NotNull IElementType documentType,
@@ -42,10 +60,7 @@ public abstract class IndentationParser implements PsiParser {
     @NotNull final IElementType eolTokenType,
     @NotNull final IElementType indentTokenType)
   {
-    myDocumentType = documentType;
-    myBlockElementType = blockElementType;
-    myEolTokenType = eolTokenType;
-    myIndentTokenType = indentTokenType;
+    this(documentType, blockElementType, eolTokenType, indentTokenType, null);
   }
 
   @Override
@@ -53,6 +68,13 @@ public abstract class IndentationParser implements PsiParser {
   public final ASTNode parse(final IElementType root, final PsiBuilder builder) {
     final PsiBuilder.Marker fileMarker = builder.mark();
     final PsiBuilder.Marker documentMarker = builder.mark();
+    final ArrayList<PsiBuilder.Marker> containerMarkers = new ArrayList<PsiBuilder.Marker>();
+    if (myContainerTypes != null) {
+      for (IElementType ignored : myContainerTypes) {
+        final PsiBuilder.Marker containerMarker = builder.mark();
+        containerMarkers.add(containerMarker);
+      }
+    }
 
     while (builder.getTokenType() == myEolTokenType) {
       advanceLexer(builder);
@@ -122,6 +144,13 @@ public abstract class IndentationParser implements PsiParser {
     while (!stack.isEmpty()){
       final BlockInfo blockInfo = stack.pop();
       closeBlock(builder, blockInfo.getMarker(), blockInfo.getStartTokenType());
+    }
+
+    if (myContainerTypes != null) {
+      for (int i = containerMarkers.size() - 1; i >= 0; i--) {
+        final PsiBuilder.Marker marker = containerMarkers.get(i);
+        marker.done(myContainerTypes.get(i));
+      }
     }
 
     documentMarker.done(myDocumentType);
