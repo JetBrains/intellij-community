0204ba233aaa3e4f468c71b968dc2fe49f09036d
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-05-12 17:52:35 +0400

    access to super members fixed (IDEA-124985)

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/AdvHighlighting8Test.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/AdvHighlighting8Test.java
@@ -15,39 +15,12 @@
  */
 package com.intellij.codeInsight.daemon.lambda;
 
-import com.intellij.analysis.PackagesScopesProvider;
-import com.intellij.application.options.colors.ScopeAttributesUtil;
 import com.intellij.codeInsight.daemon.DaemonAnalyzerTestCase;
-import com.intellij.codeInsight.daemon.impl.HighlightInfo;
-import com.intellij.openapi.application.ex.PathManagerEx;
-import com.intellij.openapi.editor.colors.EditorColorsManager;
-import com.intellij.openapi.editor.colors.EditorColorsScheme;
-import com.intellij.openapi.editor.colors.TextAttributesKey;
-import com.intellij.openapi.editor.markup.EffectType;
-import com.intellij.openapi.editor.markup.TextAttributes;
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.roots.LanguageLevelProjectExtension;
-import com.intellij.openapi.roots.ModuleRootModificationUtil;
-import com.intellij.openapi.vfs.LocalFileSystem;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.packageDependencies.DependencyValidationManager;
 import com.intellij.pom.java.LanguageLevel;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiClassType;
-import com.intellij.psi.PsiField;
-import com.intellij.psi.PsiJavaFile;
-import com.intellij.psi.search.scope.packageSet.NamedScope;
-import com.intellij.psi.search.scope.packageSet.NamedScopeManager;
-import com.intellij.psi.search.scope.packageSet.NamedScopesHolder;
-import com.intellij.psi.search.scope.packageSet.PatternPackageSet;
 import com.intellij.testFramework.IdeaTestUtil;
 import org.jetbrains.annotations.NonNls;
-
-import java.awt.*;
-import java.io.File;
-import java.util.Collection;
 
 /**
  * This class intended for "heavy-loaded" tests only, e.g. those need to setup separate project directory structure to run.
