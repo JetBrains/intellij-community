2c935f8a986a934f571e575b753becbc2d869f6f
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2014-05-16 18:35:39 +0400

    [git tests] Fix expected-actual order

--- plugins/git4idea/tests/git4idea/history/GitHistoryUtilsTest.java
+++ plugins/git4idea/tests/git4idea/history/GitHistoryUtilsTest.java
@@ -355,7 +355,7 @@ public class GitHistoryUtilsTest extends GitSingleRepoTest {
   @Test
   public void testHistory() throws Exception {
     List<VcsFileRevision> revisions = GitHistoryUtils.history(myProject, toFilePath(bfile));
-    assertEquals(revisions.size(), myRevisions.size());
+    assertEquals("Incorrect number of commits in history", myRevisions.size(), revisions.size());
     for (int i = 0; i < revisions.size(); i++) {
       assertEqualRevisions((GitFileRevision) revisions.get(i), myRevisions.get(i));
     }
