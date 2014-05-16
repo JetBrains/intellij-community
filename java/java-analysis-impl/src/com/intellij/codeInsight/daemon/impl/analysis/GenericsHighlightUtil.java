8d2b706f599aacb48eeee3bcf0a73d218943cdbe
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-05-16 19:47:18 +0400

    change method return type on call site (IDEA-125166)

--- java/java-analysis-impl/src/com/intellij/codeInsight/daemon/impl/analysis/GenericsHighlightUtil.java
+++ java/java-analysis-impl/src/com/intellij/codeInsight/daemon/impl/analysis/GenericsHighlightUtil.java
@@ -688,7 +688,7 @@ public class GenericsHighlightUtil {
     final PsiType parameterType = parameter.getType();
     HighlightInfo highlightInfo = HighlightUtil.checkAssignability(parameterType, itemType, null, new TextRange(start, end), 0);
     if (highlightInfo != null) {
-      HighlightUtil.registerChangeVariableTypeFixes(parameter, itemType, highlightInfo);
+      HighlightUtil.registerChangeVariableTypeFixes(parameter, itemType, expression, highlightInfo);
     }
     return highlightInfo;
   }
@@ -1297,7 +1297,7 @@ public class GenericsHighlightUtil {
         }
         PsiSubstitutor substitutor = factory.createSubstitutor(map);
         PsiType suggestedType = factory.createType(aClass, substitutor);
-        HighlightUtil.registerChangeVariableTypeFixes(variable, suggestedType, highlightInfo);
+        HighlightUtil.registerChangeVariableTypeFixes(variable, suggestedType, variable.getInitializer(), highlightInfo);
       }
     }
   }
