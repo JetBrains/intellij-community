47f5b9d81fab0c2df87c1eac3ebace6fc3a7c099
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2014-05-16 12:53:38 +0400

    Revert "[vcs-log-graph] use Couple"
    This reverts commit 00c700fb0cc7489b41c6e8b355be16cc35fd69e0.
    
    To avoid merge conflicts in a long running feature branch.

--- platform/vcs-log/graph/src/com/intellij/vcs/log/newgraph/gpaph/fragments/FragmentGenerator.java
+++ platform/vcs-log/graph/src/com/intellij/vcs/log/newgraph/gpaph/fragments/FragmentGenerator.java
@@ -16,7 +16,7 @@
 
 package com.intellij.vcs.log.newgraph.gpaph.fragments;
 
-import com.intellij.openapi.util.Couple;
+import com.intellij.openapi.util.Pair;
 import com.intellij.util.Function;
 import com.intellij.vcs.log.newgraph.SomeGraph;
 import com.intellij.vcs.log.newgraph.gpaph.Edge;
@@ -96,19 +96,19 @@ public class FragmentGenerator {
 
   @Nullable
   public GraphFragment getDownFragment(int upperVisibleNodeIndex) {
-    Couple<Integer> fragment = getFragment(myMutableGraph.getIndexInPermanentGraph(upperVisibleNodeIndex),
-                                           downNodesFun,
-                                           upNodesFun,
-                                           thisNodeCantBeInMiddle);
+    Pair<Integer, Integer> fragment = getFragment(myMutableGraph.getIndexInPermanentGraph(upperVisibleNodeIndex),
+                                                  downNodesFun,
+                                                  upNodesFun,
+                                                  thisNodeCantBeInMiddle);
     return fragment == null ? null : new GraphFragment(myMutableGraph.toVisibleIndex(fragment.first), myMutableGraph.toVisibleIndex(fragment.second));
   }
 
   @Nullable
   public GraphFragment getUpFragment(int lowerNodeIndex) {
-    Couple<Integer> fragment = getFragment(myMutableGraph.getIndexInPermanentGraph(lowerNodeIndex),
-                                           upNodesFun,
-                                           downNodesFun,
-                                           thisNodeCantBeInMiddle);
+    Pair<Integer, Integer> fragment = getFragment(myMutableGraph.getIndexInPermanentGraph(lowerNodeIndex),
+                                                  upNodesFun,
+                                                  downNodesFun,
+                                                  thisNodeCantBeInMiddle);
     return fragment == null ? null : new GraphFragment(myMutableGraph.toVisibleIndex(fragment.second), myMutableGraph.toVisibleIndex(fragment.first));
   }
 
@@ -159,10 +159,10 @@ public class FragmentGenerator {
   }
 
   @Nullable
-  private static Couple<Integer> getFragment(int startNode,
-                                             Function<Integer, List<Integer>> getNextNodes,
-                                             Function<Integer, List<Integer>> getPrevNodes,
-                                             Function<Integer, Boolean> thisNodeCantBeInMiddle) {
+  private static Pair<Integer, Integer> getFragment(int startNode,
+                                                    Function<Integer, List<Integer>> getNextNodes,
+                                                    Function<Integer, List<Integer>> getPrevNodes,
+                                                    Function<Integer, Boolean> thisNodeCantBeInMiddle) {
     Set<Integer> blackNodes = new HashSet<Integer>();
     blackNodes.add(startNode);
 
@@ -197,7 +197,7 @@ public class FragmentGenerator {
     }
 
     if (endNode != -1)
-      return Couple.newOne(startNode, endNode);
+      return Pair.create(startNode, endNode);
     else
       return null;
   }
