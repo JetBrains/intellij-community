d3469b45a1410008dd944b74afa276a46ac7acd2
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2014-05-23 20:38:35 +0400

    Cleanup
    (cherry picked from commit e23c4c09ac8151ec31e949ff16b7e563bfb49fe8)

--- python/src/com/jetbrains/python/validation/ReturnAnnotator.java
+++ python/src/com/jetbrains/python/validation/ReturnAnnotator.java
@@ -19,14 +19,13 @@ import com.intellij.psi.util.PsiTreeUtil;
 import com.jetbrains.python.psi.*;
 
 /**
- * Highlights incorrect return statements: 'return' and 'yield' outside functions, returning values from generators;
+ * Highlights incorrect return statements: 'return' and 'yield' outside functions, returning values from generators.
  */
 public class ReturnAnnotator extends PyAnnotator {
   public void visitPyReturnStatement(final PyReturnStatement node) {
     PyFunction function = PsiTreeUtil.getParentOfType(node, PyFunction.class, false, PyClass.class);
     if (function == null) {
       getHolder().createErrorAnnotation(node, "'return' outside of function");
-      return;
     }
   }
 
@@ -34,10 +33,5 @@ public class ReturnAnnotator extends PyAnnotator {
     if (PsiTreeUtil.getParentOfType(node, PyFunction.class, false, PyClass.class) == null) {
       getHolder().createErrorAnnotation(node, "'yield' outside of function");
     }
-    /* this is now allowed in python 2.5
-    if (node.getContainingElement(PyTryFinallyStatement.class) != null) {
-      getHolder().createErrorAnnotation(node, "'yield' not allowed in a 'try' block with a 'finally' clause");
-    }
-    */
   }
 }
