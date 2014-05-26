53fd9542bcfe7c8707e582bdb01b7b70768e0f85
Egor.Ushakov <egor.ushakov@jetbrains.com>
2014-05-26 16:11:14 +0400

    IDEA-125542 new debugger: 'rerun' failed tests are lost

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/XDebugSessionImpl.java
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/XDebugSessionImpl.java
@@ -110,6 +110,8 @@ public class XDebugSessionImpl implements XDebugSession {
   private boolean myPauseActionSupported;
   private boolean myShowTabOnSuspend;
   private final List<AnAction> myRestartActions = new SmartList<AnAction>();
+  private final List<AnAction> myExtraStopActions = new SmartList<AnAction>();
+  private final List<AnAction> myExtraActions = new SmartList<AnAction>();
   private ConsoleView myConsoleView;
   private final Icon myIcon;
 
@@ -177,6 +179,22 @@ public class XDebugSessionImpl implements XDebugSession {
 
   public void addRestartActions(AnAction... restartActions) {
     Collections.addAll(myRestartActions, restartActions);
+  }
+
+  public List<AnAction> getExtraActions() {
+    return myExtraActions;
+  }
+
+  public void addExtraActions(AnAction... extraActions) {
+    Collections.addAll(myExtraActions, extraActions);
+  }
+
+  public List<AnAction> getExtraStopActions() {
+    return myExtraStopActions;
+  }
+
+  public void addExtraStopActions(AnAction... extraStopActions) {
+    Collections.addAll(myExtraStopActions, extraStopActions);
   }
 
   @Override
