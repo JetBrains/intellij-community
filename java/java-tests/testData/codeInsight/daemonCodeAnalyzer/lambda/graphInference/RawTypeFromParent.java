fffde12ba8368cc604d3c6d40946c40b9bcc9a35
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-05-30 21:43:31 +0400

    new inference: mark erased for type params/raw types compatibility (IDEA-122932)

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/lambda/graphInference/RawTypeFromParent.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/lambda/graphInference/RawTypeFromParent.java
@@ -5,6 +5,11 @@ class Foo<T> {
 
   public void test(Foo parent) {
     List<Foo> elements = getElements(parent);
+
+
+    for (<error descr="Incompatible types. Found: 'java.lang.Object', required: 'Foo'">Foo foo : getElements(parent)</error>) {
+      System.out.println(foo);
+    }
   }
 
   public static <E extends Foo<E>> List<E> getElements(E parent) {
