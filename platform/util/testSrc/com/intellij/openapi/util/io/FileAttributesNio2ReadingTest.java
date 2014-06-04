11adc57208073416eeeae9d0af3d844ee9314deb
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2014-06-04 15:40:57 +0400

    Cleanup (warning; unneeded reflection)

--- platform/util/testSrc/com/intellij/openapi/util/io/FileAttributesNio2ReadingTest.java
+++ platform/util/testSrc/com/intellij/openapi/util/io/FileAttributesNio2ReadingTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,36 +19,22 @@ import com.intellij.openapi.util.SystemInfo;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
-import java.lang.reflect.Field;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assume.assumeTrue;
 
 public class FileAttributesNio2ReadingTest extends FileAttributesReadingTest {
-  private static final String FORCE_USE_NIO_2_KEY;
-  static {
-    try {
-      Field field = FileSystemUtil.class.getDeclaredField("FORCE_USE_NIO2_KEY");
-      field.setAccessible(true);
-      FORCE_USE_NIO_2_KEY = (String)field.get(null);
-    }
-    catch (Exception e) {
-      throw new AssertionError("Please keep constants in sync: " + e.getMessage());
-    }
-  }
-
   @BeforeClass
   public static void setUpClass() throws Exception {
     assumeTrue(SystemInfo.isJavaVersionAtLeast("1.7"));
 
-    System.setProperty(FORCE_USE_NIO_2_KEY, "true");
+    System.setProperty(FileSystemUtil.FORCE_USE_NIO2_KEY, "true");
     FileSystemUtil.resetMediator();
     assertEquals("Nio2", FileSystemUtil.getMediatorName());
   }
 
   @AfterClass
   public static void tearDownClass() throws Exception {
-    System.setProperty(FORCE_USE_NIO_2_KEY, "");
+    System.clearProperty(FileSystemUtil.FORCE_USE_NIO2_KEY);
     FileSystemUtil.resetMediator();
   }
 }
