350760c993dee773aa52c8db38fb409884ba3d16
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-06-05 15:55:30 +0400

    rename: detect "field would hide local" conflict (IDEA-125934)

---
+++ java/java-impl/src/com/intellij/refactoring/rename/FieldHidesLocalUsageInfo.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2000-2014 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Created by IntelliJ IDEA.
+ * User: dsl
+ * Date: 05.06.2002
+ * Time: 12:43:27
+ * To change template for new class use 
+ * Code Style | Class Templates options (Tools | IDE Options).
+ */
+package com.intellij.refactoring.rename;
+
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiMethod;
+import com.intellij.refactoring.RefactoringBundle;
+import com.intellij.refactoring.util.CommonRefactoringUtil;
+import com.intellij.refactoring.util.RefactoringUIUtil;
+import com.intellij.usageView.UsageViewUtil;
+
+public class FieldHidesLocalUsageInfo extends UnresolvableCollisionUsageInfo {
+  public FieldHidesLocalUsageInfo(PsiElement element, PsiElement referencedElement) {
+    super(element, referencedElement);
+  }
+
+  public String getDescription() {
+    String descr = RefactoringBundle.message("local.will.be.hidden.renamed",
+                                             RefactoringUIUtil.getDescription(getElement(), true));
+    return CommonRefactoringUtil.capitalize(descr);
+  }
+}
