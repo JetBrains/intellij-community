f06f5a629e60660821eaaa72a86ec3e85faa2e24
suman-ganta <suman.ganta@gmail.com>
2014-06-05 22:37:18 -0700

    Handle '@' symbol in ssh username
    http://youtrack.jetbrains.com/issue/IDEA-123890
    Git pull doesn't work if git username has 'at' symbol

--- plugins/git4idea/rt/src/org/jetbrains/git4idea/ssh/SSHMain.java
+++ plugins/git4idea/rt/src/org/jetbrains/git4idea/ssh/SSHMain.java
@@ -471,7 +471,7 @@ public class SSHMain implements GitExternalApp {
     }
     String host = args[i++];
     String user;
-    int atIndex = host.indexOf('@');
+    int atIndex = host.lastIndexOf('@');
     if (atIndex == -1) {
       user = null;
     }
