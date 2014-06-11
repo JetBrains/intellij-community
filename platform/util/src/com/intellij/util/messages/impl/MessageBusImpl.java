54783affda53cae9b1b831afe9e111bf1472ddda
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2014-06-11 22:31:55 +0400

    EA-52085 (diagnostic)

--- platform/util/src/com/intellij/util/messages/impl/MessageBusImpl.java
+++ platform/util/src/com/intellij/util/messages/impl/MessageBusImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -76,7 +76,6 @@ public class MessageBusImpl implements MessageBus {
   private MessageBusImpl myParentBus;
 
   //is used for debugging purposes
-  @SuppressWarnings({"UnusedDeclaration", "FieldCanBeLocal"})
   private final Object myOwner;
   private boolean myDisposed;
 
@@ -222,7 +221,7 @@ public class MessageBusImpl implements MessageBus {
   }
 
   private void checkNotDisposed() {
-    LOG.assertTrue(!myDisposed, "Already disposed");
+    LOG.assertTrue(!myDisposed, "Already disposed: " + myOwner);
   }
 
   private void calcSubscribers(Topic topic, List<MessageBusConnectionImpl> result) {
