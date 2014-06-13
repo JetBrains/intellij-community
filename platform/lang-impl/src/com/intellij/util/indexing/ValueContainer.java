326614c48a1d290a7baa7b496bed5967d13c059f
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2014-06-13 18:17:34 +0200

    we don't need to implement Cloneable

--- platform/lang-impl/src/com/intellij/util/indexing/ValueContainer.java
+++ platform/lang-impl/src/com/intellij/util/indexing/ValueContainer.java
@@ -29,7 +29,7 @@ import java.util.List;
  *         Date: Dec 14, 2007
  */
 public abstract class ValueContainer<Value> {
-  public interface IntIterator extends Cloneable {
+  public interface IntIterator {
     boolean hasNext();
 
     int next();
