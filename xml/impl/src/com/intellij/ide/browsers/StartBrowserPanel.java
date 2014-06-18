a07ff41d8764a59c69d309f2efff594efea25c0b
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2014-06-18 14:28:43 +0400

    meteor client debug

--- xml/impl/src/com/intellij/ide/browsers/StartBrowserPanel.java
+++ xml/impl/src/com/intellij/ide/browsers/StartBrowserPanel.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2014 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.intellij.ide.browsers;
 
 import com.intellij.ide.DataManager;
@@ -139,6 +154,13 @@ public class StartBrowserPanel {
     browserSettings.setStartJavaScriptDebugger(myStartJavaScriptDebuggerCheckBox.isSelected());
     browserSettings.setUrl(getUrl());
     return browserSettings;
+  }
+
+  public void setFromSettings(StartBrowserSettings settings) {
+    setSelected(settings.isSelected());
+    setUrl(settings.getUrl());
+    myStartJavaScriptDebuggerCheckBox.setSelected(settings.isStartJavaScriptDebugger());
+    myBrowserSelector.setSelected(settings.getBrowser());
   }
 
   public static void setupUrlField(@NotNull TextFieldWithBrowseButton field, @NotNull final Project project) {
