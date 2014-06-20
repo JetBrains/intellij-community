3f567e99d48d5a0705a470dca084c894e9165dc4
Alexey Kudravtsev <cdr@intellij.com>
2014-06-20 15:26:03 +0400

    notnull

--- platform/core-api/src/com/intellij/openapi/progress/Task.java
+++ platform/core-api/src/com/intellij/openapi/progress/Task.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2010 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -189,6 +189,7 @@ public abstract class Task implements TaskInfo, Progressive {
       return false;
     }
 
+    @NotNull
     public DumbModeAction getDumbModeAction() {
       return DumbModeAction.NOTHING;
     }
