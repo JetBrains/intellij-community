596d24d5056d96f23d1c8784c7af1a95903da36f
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-06-23 20:06:41 +0200

    method refs: reject constructor refs with 'receiver' syntax (IDEA-126484)

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewMethodRefHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewMethodRefHighlightingTest.java
@@ -261,6 +261,10 @@ public class NewMethodRefHighlightingTest extends LightDaemonAnalyzerTestCase {
     doTest();
   }
 
+  public void testRejectReceiverTypesForConstructorRefs() {
+    doTest();
+  }
+
   private void doTest() {
     doTest(false);
   }
