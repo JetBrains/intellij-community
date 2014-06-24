4e200abdb4ce9b0994294d9910d8a16dc23f22d0
medvector <medeja-vector@yandex.ru>
2014-06-24 14:32:21 +0400

    changes

--- EDIDE/src/ru/compscicenter/edide/StudyDirectoryProjectGenerator.java
+++ EDIDE/src/ru/compscicenter/edide/StudyDirectoryProjectGenerator.java
@@ -13,11 +13,11 @@ import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.platform.DirectoryProjectGenerator;
-import com.intellij.platform.templates.github.GeneratorException;
 import com.intellij.platform.templates.github.ZipUtil;
 import com.intellij.util.PathUtil;
 import org.jetbrains.annotations.Nls;
@@ -25,29 +25,41 @@ import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import ru.compscicenter.edide.model.Course;
-import ru.compscicenter.edide.model.Lesson;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * User: lia
  */
 class StudyDirectoryProjectGenerator implements DirectoryProjectGenerator {
   private static final Logger LOG = Logger.getInstance(StudyDirectoryProjectGenerator.class.getName());
-  private static File myBaseCouseFile;
-  private File myDefaultCoursesBaseDir;
-  private static Map<String, File> myCourseFiles =  new HashMap<String, File>();
+  public static final String REPO_URL = "https://github.com/medvector/initial-python-course/archive/master.zip";
+  public static final String USER_NAME = "medvector";
+  public static final String REPOSITORY_NAME = "initial-python-course";
+  private static File myBaseCourseFile;
+  private static File myDefaultCoursesBaseDir;
+  private static Map<String, File> myDefaultCourseFiles =  new HashMap<String, File>();
+  private Map<String, File> myDefaultCourses;
 
   @Nls
   @NotNull
   @Override
   public String getName() {
     return "Study project";
+  }
+
+
+
+
+  public StudyDirectoryProjectGenerator() {
+    myDefaultCoursesBaseDir = new File(PathManager.getLibPath() + "/courses");
+  }
+
+  public static File getMyDefaultCoursesBaseDir() {
+    return myDefaultCoursesBaseDir;
   }
 
   public static File getResourcesRoot() {
@@ -60,12 +72,12 @@ class StudyDirectoryProjectGenerator implements DirectoryProjectGenerator {
     return new File(jarPath);
   }
 
-  public static String getMyBaseCouseFile() {
-    return myBaseCouseFile.getName();
+  public static String getMyBaseCourseFile() {
+    return myBaseCourseFile.getName();
   }
 
-  public static void setMyBaseCouseFile(String mymyBaseCouseFile) {
-    myBaseCouseFile =myCourseFiles.get(mymyBaseCouseFile);
+  public static void setMyBaseCourseFile(String mymyBaseCouseFile) {
+    myBaseCourseFile = myDefaultCourseFiles.get(mymyBaseCouseFile);
   }
 
   @Nullable
@@ -87,7 +99,7 @@ class StudyDirectoryProjectGenerator implements DirectoryProjectGenerator {
 
   public static ArrayList<String> getCourseFiles() {
     ArrayList<String> fileName = new ArrayList<String>();
-    for (String key : myCourseFiles.keySet()) {
+    for (String key : myDefaultCourseFiles.keySet()) {
      fileName.add(key);
     }
     return fileName;
@@ -96,98 +108,128 @@ class StudyDirectoryProjectGenerator implements DirectoryProjectGenerator {
   @Override
   public void generateProject(@NotNull final Project project, @NotNull final VirtualFile baseDir,
                               @Nullable Object settings, @NotNull Module module) {
-  //getting available courses from github
+    if (!myDefaultCoursesBaseDir.exists()) {
+      downloadCoursesFromGithub();
+    }
+    myDefaultCourses = getDefaultCourses();
+    ////select course window
+    StudyNewCourseDialog dlg = new StudyNewCourseDialog(project, myDefaultCourses.keySet());
+    dlg.show();
+    //InputStream file = null;
+    //try {
+    //  file = new FileInputStream(myBaseCourseFile);
+    //}
+    //catch (FileNotFoundException e) {
+    //  e.printStackTrace();
+    //}
+    //Reader reader = new InputStreamReader(file);
+    //Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
+    //Course course = gson.fromJson(reader, Course.class);
+    //System.out.println();
+    //
+    //ApplicationManager.getApplication().invokeLater(
+    //  new Runnable() {
+    //    @Override
+    //    public void run() {
+    //      ApplicationManager.getApplication().runWriteAction(new Runnable() {
+    //        @Override
+    //        public void run() {
+    //          try {
+    //            //StudyPlugin.createTaskManager(project.getName());
+    //            //TaskManager taskManager = TaskManager.getInstance(project);
+    //            TaskManager taskManager = TaskManager.getInstance(project);
+    //            int tasksNumber = taskManager.getTasksNum();
+    //            for (int task = 0; task < tasksNumber; task++) {
+    //              VirtualFile taskDirectory = baseDir.createChildDirectory(this, "task" + (task + 1));
+    //              for (int file = 0; file < taskManager.getTaskFileNum(task); file++) {
+    //                final String curFileName = taskManager.getFileName(task, file);
+    //                createFile(curFileName, taskDirectory);
+    //              }
+    //              final VirtualFile ideaDir = baseDir.findChild(".idea");
+    //              if (ideaDir != null) {
+    //                createFile(taskManager.getTest(task), ideaDir);
+    //              }
+    //              else {
+    //                LOG.error("Could not find .idea directory");
+    //              }
+    //            }
+    //          }
+    //          catch (IOException e) {
+    //            Log.print("Problems with creating files");
+    //            Log.print(e.toString());
+    //            Log.flush();
+    //          }
+    //          LocalFileSystem.getInstance().refresh(false);
+    //        }
+    //      });
+    //    }
+    //  }
+    //);
+  }
+
+  private static boolean downloadCoursesFromGithub() {
     File outputFile = new File(PathManager.getLibPath() + "/courses.zip");
+      try {
+        GithubDownloadUtil.downloadAtomically(null, REPO_URL,
+                                              outputFile, USER_NAME, REPOSITORY_NAME);
+        if (outputFile.exists()) {
+          ZipUtil.unzip(null, myDefaultCoursesBaseDir, outputFile, null, null, true);
+          if (myDefaultCoursesBaseDir.exists()) {
+            return true;
+          }
+        }
+        return false;
+      }
+      catch (IOException e) {
+        e.printStackTrace();
+        return false;
+      }
+  }
+
+  private  static Map<String, File> getDefaultCourses() {
+    Map<String, File> defaultCourses = new HashMap<String, File>();
+    if (!myDefaultCoursesBaseDir.exists()) {
+      return defaultCourses;
+    }
     try {
-      //GithubDownloadUtil.downloadContentToFileWithProgressSynchronously(project, "https://github.com/medvector/initial-python-course/archive/master.zip",
-      //                                                                  "downloading courses", outputFile, "medvector", "initial-python-course" , true );
-      GithubDownloadUtil.downloadAtomically(null, "https://github.com/medvector/initial-python-course/archive/master.zip",
-                                                                         outputFile, "medvector", "initial-python-course");
-      myDefaultCoursesBaseDir = new File(PathManager.getLibPath() + "/courses");
-      //ZipUtil.unzipWithProgressSynchronously(project, "unzipping", outputFile, myDefaultCoursesBaseDir, true);
-      ZipUtil.unzip(null, myDefaultCoursesBaseDir, outputFile, null, null, true);
       File[] files = myDefaultCoursesBaseDir.listFiles();
-      for (File f:files) {
-        if (f.isDirectory()) {
-          File[] filesInCourse = f.listFiles();
-          for (File courseFile:filesInCourse) {
-            if (courseFile.getName().equals("course.json")) {
-              String name = getCourseName(courseFile);
-              int i = 1;
-              if (name!= null) {
-                File item = myCourseFiles.get(name);
-                while(item!= null && !FileUtil.filesEqual(item, courseFile)) {
-                  name = name + "1";
-                  i++;
+      if (files != null) {
+        for (File f:files) {
+          if (f.isDirectory()) {
+            File[] filesInCourse = f.listFiles();
+            if (filesInCourse != null) {
+              for (File courseFile:filesInCourse) {
+                if (courseFile.getName().equals("course.json")) {
+                  String name = getCourseName(courseFile);
+                  int i = 2;
+                  if (name!= null) {
+                    File item = defaultCourses.get(name);
+                    String tmp = name;
+                    while(item!= null && !FileUtil.filesEqual(item, courseFile)) {
+                      if (i>2)  {
+                        name = name.substring(0, name.length() - 3);
+                      }
+                      name = tmp + Integer.toString(i);
+                      i++;
+                      item = defaultCourses.get(name);
+                    }
+                    defaultCourses.put(name, courseFile);
+                  }
                 }
-                myBaseCouseFile = courseFile;
-                myCourseFiles.put(name, courseFile);
               }
             }
           }
         }
       }
-      files.toString();
-    }
-    catch (IOException e) {
-      e.printStackTrace();
-    }
-    //select course window
-      StudyNewCourseDialog dlg = new StudyNewCourseDialog(project);
-      dlg.show();
-    InputStream file = null;
-    try {
-      file = new FileInputStream(myBaseCouseFile);
-    }
-    catch (FileNotFoundException e) {
-      e.printStackTrace();
-    }
-    Reader reader = new InputStreamReader(file);
-    Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
-    Course course = gson.fromJson(reader, Course.class);
-    System.out.println();
 
-    ApplicationManager.getApplication().invokeLater(
-      new Runnable() {
-        @Override
-        public void run() {
-          ApplicationManager.getApplication().runWriteAction(new Runnable() {
-            @Override
-            public void run() {
-              try {
-                //StudyPlugin.createTaskManager(project.getName());
-                //TaskManager taskManager = TaskManager.getInstance(project);
-                TaskManager taskManager = TaskManager.getInstance(project);
-                int tasksNumber = taskManager.getTasksNum();
-                for (int task = 0; task < tasksNumber; task++) {
-                  VirtualFile taskDirectory = baseDir.createChildDirectory(this, "task" + (task + 1));
-                  for (int file = 0; file < taskManager.getTaskFileNum(task); file++) {
-                    final String curFileName = taskManager.getFileName(task, file);
-                    createFile(curFileName, taskDirectory);
-                  }
-                  final VirtualFile ideaDir = baseDir.findChild(".idea");
-                  if (ideaDir != null) {
-                    createFile(taskManager.getTest(task), ideaDir);
-                  }
-                  else {
-                    LOG.error("Could not find .idea directory");
-                  }
-                }
-              }
-              catch (IOException e) {
-                Log.print("Problems with creating files");
-                Log.print(e.toString());
-                Log.flush();
-              }
-              LocalFileSystem.getInstance().refresh(false);
-            }
-          });
-        }
-      }
-    );
+    }
+    catch (NullPointerException e) {
+      LOG.error("default course folder doesn't exist");
+    }
+    return defaultCourses;
   }
 
-  private String getCourseName(File file) {
+  private static String getCourseName(File file) {
     InputStream metaIS = null;
     String name = null;
     try {
@@ -208,5 +250,13 @@ class StudyDirectoryProjectGenerator implements DirectoryProjectGenerator {
   @Override
   public ValidationResult validate(@NotNull String s) {
     return ValidationResult.OK;
+  }
+
+  public static Set<String> updateDefaultCourseList() {
+    downloadCoursesFromGithub();
+    if (myDefaultCoursesBaseDir.exists()) {
+      return getDefaultCourses().keySet();
+    }
+    return new HashSet<String>();
   }
 }
--- EDIDE/src/ru/compscicenter/edide/StudyNewCourseDialog.java
+++ EDIDE/src/ru/compscicenter/edide/StudyNewCourseDialog.java
@@ -1,77 +1,72 @@
 package ru.compscicenter.edide;
 
-import com.intellij.dvcs.ui.DvcsBundle;
-import com.intellij.openapi.fileChooser.*;
+import com.intellij.openapi.fileChooser.FileChooserDescriptor;
+import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.*;
-import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.event.*;
-import java.util.ArrayList;
+import java.util.Set;
 
 public class StudyNewCourseDialog extends DialogWrapper {
-    private JPanel contentPane;
-    private JComboBox comboBox1;
-  private JLabel locationLabel;
-  private TextFieldWithBrowseButton button;
-
-    public StudyNewCourseDialog(final Project project) {
-        super(project, true);
-        setTitle("Select the Course");
-        init();
-        ArrayList<String> availableCouseNames = StudyDirectoryProjectGenerator.getCourseFiles();
-        for (String courseName: availableCouseNames) {
-          comboBox1.addItem(courseName);
-        }
-      button =  new TextFieldWithBrowseButton();
-      button.getButton().setFocusable(true);
-      button.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-          final JFileChooser fc = new JFileChooser();
-          int returnVal = fc.showOpenDialog(button);
-        }
-      });
-      FileChooserDescriptor fcd = FileChooserDescriptorFactory.createSingleFolderDescriptor();
-      fcd.setShowFileSystemRoots(true);
-      fcd.setTitle(DvcsBundle.getString("clone.destination.directory.title"));
-      fcd.setDescription(DvcsBundle.getString("clone.destination.directory.description"));
-      fcd.setHideIgnored(false);
-      button.addActionListener(
-        new ComponentWithBrowseButton.BrowseFolderActionListener<JTextField>(fcd.getTitle(), fcd.getDescription(), button,
-                                                                             project, fcd, TextComponentAccessor.TEXT_FIELD_SELECTED_TEXT) {
-          @Override
-          protected VirtualFile getInitialFile() {
-            // suggest project base directory only if nothing is typed in the component.
-            String text = getComponentText();
-            if (text.length() == 0) {
-              VirtualFile file = project.getBaseDir();
-              if (file != null) {
-                return file;
-              }
-            }
-            return super.getInitialFile();
-          }
-        }
-      );
-        comboBox1.addActionListener(new ActionListener() {
-          @Override
-          public void actionPerformed(ActionEvent e) {
-            JComboBox cb = (JComboBox)e.getSource();
-            String testString = (String)cb.getSelectedItem();
-            StudyDirectoryProjectGenerator.setMyBaseCouseFile(testString);
-          }
-        });
+  private JPanel nyContentPane;
+  private JButton myRefreshButton;
+  private JComboBox myDefaultCoursesComboBox;
+  private TextFieldWithBrowseButton myCourseLocationField;
+  private JLabel myDefaultLabel;
+  private JLabel myLocationLabel;
+  private JLabel myErrorLabel;
 
 
+  public StudyNewCourseDialog(final Project project, Set<String> availableDefaultCourses) {
+    super(project, true);
+    setTitle("Select the Course");
+    init();
+    myErrorLabel.setVisible(false);
+    if (availableDefaultCourses.size() == 0) {
+        myErrorLabel.setVisible(true);
+    }
+    for (String courseName: availableDefaultCourses) {
+      myDefaultCoursesComboBox.addItem(courseName);
     }
 
-    @Nullable
-    @Override
-    protected JComponent createCenterPanel() {
-        return contentPane;
-    }
+    final FileChooserDescriptor fileChooser = FileChooserDescriptorFactory.createSingleLocalFileDescriptor();
+    myCourseLocationField.addBrowseFolderListener("Select course file", null, project, fileChooser);
+
+
+
+    myDefaultCoursesComboBox.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        JComboBox cb = (JComboBox)e.getSource();
+        String testString = (String)cb.getSelectedItem();
+        StudyDirectoryProjectGenerator.setMyBaseCourseFile(testString);
+      }
+    });
+    myRefreshButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+       myDefaultCoursesComboBox.removeAllItems();
+       Set<String> newCourses = StudyDirectoryProjectGenerator.updateDefaultCourseList();
+       if (newCourses.size() == 0) {
+         myErrorLabel.setVisible(true);
+         return;
+       }
+        myErrorLabel.setVisible(false);
+        for (String course:newCourses) {
+         myDefaultCoursesComboBox.addItem(course);
+       }
+      }
+    });
+    myRefreshButton.setVisible(true);
+  }
+
+  @Nullable
+  @Override
+  protected JComponent createCenterPanel() {
+    return nyContentPane;
+  }
 
 }
