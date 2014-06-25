8a59c79c97be9834cd328b72fc1c6ffb70bc94ab
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2014-06-25 16:12:48 +0400

    Gradle: fix compatibility with Gradle 2.0 (cherry picked from commit 84cd616)

--- plugins/gradle/tooling-extension-impl/testSources/org/jetbrains/plugins/gradle/tooling/builder/AbstractModelBuilderTest.java
+++ plugins/gradle/tooling-extension-impl/testSources/org/jetbrains/plugins/gradle/tooling/builder/AbstractModelBuilderTest.java
@@ -59,6 +59,7 @@ public abstract class AbstractModelBuilderTest {
   public static final String GRADLE_v1_10 = "1.10";
   public static final String GRADLE_v1_11 = "1.11";
   public static final String GRADLE_v1_12 = "1.12";
+  public static final String GRADLE_v2_0 = "2.0-rc-2";
 
   public static final Pattern TEST_METHOD_NAME_PATTERN = Pattern.compile("(.*)\\[(\\d*)\\]");
   private static final int DEFAULT_DAEMON_MAX_IDLE_TIME = 5;
@@ -79,10 +80,11 @@ public abstract class AbstractModelBuilderTest {
   @Parameterized.Parameters
   public static Collection<Object[]> data() {
     Object[][] data = {
-      {AbstractModelBuilderTest.GRADLE_v1_9},
-      {AbstractModelBuilderTest.GRADLE_v1_10},
-      {AbstractModelBuilderTest.GRADLE_v1_11},
-      {AbstractModelBuilderTest.GRADLE_v1_12}
+      {GRADLE_v1_9},
+      {GRADLE_v1_10},
+      {GRADLE_v1_11},
+      {GRADLE_v1_12},
+      {GRADLE_v2_0}
     };
     return Arrays.asList(data);
   }
