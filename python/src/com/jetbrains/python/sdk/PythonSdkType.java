85ec7394a886b7f2529645d9bb0b92af4aaebaef
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2014-07-04 15:35:33 +0200

    Don't offer to launch vagrant on skeleton building errors with non-vagrant based remote interpreter.

--- python/src/com/jetbrains/python/sdk/PythonSdkType.java
+++ python/src/com/jetbrains/python/sdk/PythonSdkType.java
@@ -56,9 +56,7 @@ import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.reference.SoftReference;
-import com.intellij.remote.RemoteSdkCredentials;
-import com.intellij.remote.RemoteSdkCredentialsHolder;
-import com.intellij.remote.VagrantNotStartedException;
+import com.intellij.remote.*;
 import com.intellij.util.ArrayUtil;
 import com.intellij.util.Consumer;
 import com.intellij.util.NullableConsumer;
@@ -71,6 +69,7 @@ import com.jetbrains.python.facet.PythonFacetSettings;
 import com.jetbrains.python.psi.LanguageLevel;
 import com.jetbrains.python.psi.impl.PyBuiltinCache;
 import com.jetbrains.python.psi.search.PyProjectScopeBuilder;
+import com.jetbrains.python.remote.PyRemoteSdkAdditionalDataBase;
 import com.jetbrains.python.remote.PythonRemoteInterpreterManager;
 import com.jetbrains.python.sdk.flavors.CPythonSdkFlavor;
 import com.jetbrains.python.sdk.flavors.PythonSdkFlavor;
@@ -236,6 +235,15 @@ public class PythonSdkType extends SdkType {
 
   public static boolean isRemote(@Nullable Sdk sdk) {
     return PySdkUtil.isRemote(sdk);
+  }
+
+  public static boolean isVagrant(@Nullable Sdk sdk) {
+    if (sdk != null && sdk.getSdkAdditionalData() instanceof PyRemoteSdkAdditionalDataBase) {
+      PyRemoteSdkAdditionalDataBase data = (PyRemoteSdkAdditionalDataBase) sdk.getSdkAdditionalData();
+
+      return data.getRemoteConnectionType() == CredentialsType.VAGRANT;
+    }
+    return false;
   }
 
   public static boolean isRemote(@Nullable String sdkPath) {
@@ -555,7 +563,7 @@ public class PythonSdkType extends SdkType {
               }
               catch (InvalidSdkException e) {
                 // If the SDK is invalid, the user should worry about the SDK itself, not about skeletons generation errors
-                if (isRemote(sdk)) {
+                if (isVagrant(sdk)) {
                   notifyRemoteSdkSkeletonsFail(e, new Runnable() {
                     @Override
                     public void run() {
