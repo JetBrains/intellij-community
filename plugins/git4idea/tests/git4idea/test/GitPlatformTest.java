797a61cc2cfb032d2c3df18802a35ba591b10429
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2014-07-19 19:31:26 +0400

    [git tests] shorten test names
    * test name is used in application and project folders
    * Introduce one more shortenText-utility method to let specify
      arbitrary ellipsis symbol (to avoid "..." in directory names not to
      confuse any FS or Git).

--- plugins/git4idea/tests/git4idea/test/GitPlatformTest.java
+++ plugins/git4idea/tests/git4idea/test/GitPlatformTest.java
@@ -19,6 +19,7 @@ import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vcs.*;
 import com.intellij.openapi.vcs.changes.ChangeListManager;
 import com.intellij.openapi.vcs.changes.VcsDirtyScopeManager;
@@ -112,9 +113,9 @@ public abstract class GitPlatformTest extends UsefulTestCase {
   @NotNull
   public String getTestName(boolean lowercaseFirstLetter) {
     String name = super.getTestName(lowercaseFirstLetter);
-    name = name.trim().replace(' ', '_');
-    if (name.length() > 50) {
-      name = name.substring(0, 50);
+    name = StringUtil.shortenTextWithEllipsis(name.trim().replace(" ", "_"), 12, 6, "_");
+    if (name.startsWith("_")) {
+      name = name.substring(1);
     }
     return name;
   }
