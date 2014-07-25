efd48202da12d38310329300c660d39b00355cac
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-07-25 16:45:11 +0200

    process test method templates without "test" prefix EA-58090 - assert: JavaParserUtil.parseFragment

--- java/java-impl/src/com/intellij/testIntegration/createTest/MissedTestsDialog.java
+++ java/java-impl/src/com/intellij/testIntegration/createTest/MissedTestsDialog.java
@@ -20,19 +20,15 @@ import com.intellij.codeInsight.template.Template;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.psi.JavaPsiFacade;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiMember;
-import com.intellij.psi.PsiMethod;
-import com.intellij.refactoring.ui.MemberSelectionPanel;
+import com.intellij.psi.*;
 import com.intellij.refactoring.ui.MemberSelectionTable;
 import com.intellij.refactoring.util.classMembers.MemberInfo;
 import com.intellij.testIntegration.TestFramework;
 import com.intellij.testIntegration.TestIntegrationUtils;
 import com.intellij.ui.ScrollPaneFactory;
-import com.intellij.ui.SeparatorFactory;
 import com.intellij.ui.components.JBCheckBox;
 import com.intellij.util.Function;
+import com.intellij.util.IncorrectOperationException;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.Nullable;
 
@@ -40,8 +36,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.*;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class MissedTestsDialog extends DialogWrapper {
   private final PsiClass mySourceClass;
@@ -85,9 +83,7 @@ public class MissedTestsDialog extends DialogWrapper {
 
   private void disableMethodsWithTests(List<MemberInfo> info) {
     final Set<String> existingNames = new HashSet<String>();
-    final Template template = TestIntegrationUtils
-      .createTestMethodTemplate(TestIntegrationUtils.MethodKind.TEST, myDescriptor, myTestClass, null, true, existingNames);
-    final String prefix = JavaPsiFacade.getElementFactory(myTestClass.getProject()).createMethodFromText(template.getTemplateText(), myTestClass).getName();
+    final String prefix = getTestPrefix(existingNames);
 
     existingNames.addAll(ContainerUtil.map(myTestClass.getMethods(), new Function<PsiMethod, String>() {
       @Override
@@ -100,6 +96,16 @@ public class MissedTestsDialog extends DialogWrapper {
     for (MemberInfo memberInfo : info) {
       final PsiMember member = memberInfo.getMember();
       memberInfo.setChecked(!(member instanceof PsiMethod && existingNames.contains(member.getName())));
+    }
+  }
+
+  private String getTestPrefix(Set<String> existingNames) {
+    final Template template = TestIntegrationUtils.createTestMethodTemplate(TestIntegrationUtils.MethodKind.TEST, myDescriptor, myTestClass, null, true, existingNames);
+    try {
+      return JavaPsiFacade.getElementFactory(myTestClass.getProject()).createMethodFromText(template.getTemplateText(), myTestClass).getName();
+    }
+    catch (IncorrectOperationException e) {
+      return "";
     }
   }
 
