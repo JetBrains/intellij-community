c680704f622d184a0b20487e74b689a2d368742e
Konstantin Bulenkov <kb@jetbrains.com>
2014-07-29 13:27:18 +0200

    better solution for datacontext assertions from editors actions

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/NonProjectFileWritingAccessProvider.java
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/NonProjectFileWritingAccessProvider.java
@@ -16,7 +16,6 @@
 package com.intellij.openapi.fileEditor.impl;
 
 import com.intellij.ProjectTopics;
-import com.intellij.ide.IdeEventQueue;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.StorageScheme;
 import com.intellij.openapi.components.impl.stores.IProjectStore;
@@ -114,9 +113,7 @@ public class NonProjectFileWritingAccessProvider extends WritingAccessProvider {
 
     if (deniedFiles.isEmpty()) return Collections.emptyList();
 
-    final int savedEventCount = IdeEventQueue.getInstance().getEventCount();
     UnlockOption unlockOption = askToUnlock(deniedFiles);
-    IdeEventQueue.getInstance().setEventCount(savedEventCount);
 
     if (unlockOption == null) return deniedFiles;
 
