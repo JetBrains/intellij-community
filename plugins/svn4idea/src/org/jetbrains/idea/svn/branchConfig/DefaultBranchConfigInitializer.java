12d0104ca60caf5142278f5294f580713ecb5de4
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2014-08-07 00:31:27 +0400

    svn: Refactoring - DefaultConfigLoader logic moved to DefaultBranchConfigInitializer, removed DefaultConfigLoader

--- plugins/svn4idea/src/org/jetbrains/idea/svn/branchConfig/DefaultBranchConfigInitializer.java
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/branchConfig/DefaultBranchConfigInitializer.java
@@ -17,25 +17,52 @@ package org.jetbrains.idea.svn.branchConfig;
 
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.idea.svn.SvnVcs;
+import org.jetbrains.idea.svn.api.Depth;
+import org.jetbrains.idea.svn.browse.DirectoryEntry;
+import org.jetbrains.idea.svn.browse.DirectoryEntryConsumer;
+import org.jetbrains.idea.svn.info.Info;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNURL;
+import org.tmatesoft.svn.core.internal.util.SVNPathUtil;
+import org.tmatesoft.svn.core.wc.SVNRevision;
+import org.tmatesoft.svn.core.wc2.SvnTarget;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
 * @author Konstantin Kolosovsky.
 */
-class DefaultBranchConfigInitializer implements Runnable {
+public class DefaultBranchConfigInitializer implements Runnable {
+
+  private static final Logger LOG = Logger.getInstance(DefaultBranchConfigInitializer.class);
+
+  @NonNls private static final String DEFAULT_TRUNK_NAME = "trunk";
+  @NonNls private static final String DEFAULT_BRANCHES_NAME = "branches";
+  @NonNls private static final String DEFAULT_TAGS_NAME = "tags";
+
   private final Project myProject;
   private final NewRootBunch myBunch;
   private final VirtualFile myRoot;
 
-  DefaultBranchConfigInitializer(final Project project, final NewRootBunch bunch, final VirtualFile root) {
+  public DefaultBranchConfigInitializer(final Project project, final NewRootBunch bunch, final VirtualFile root) {
     myProject = project;
     myRoot = root;
     myBunch = bunch;
   }
 
   public void run() {
-    final SvnBranchConfigurationNew result = DefaultConfigLoader.loadDefaultConfiguration(myProject, myRoot);
+    final SvnBranchConfigurationNew result = loadDefaultConfiguration();
     if (result != null) {
       final Application application = ApplicationManager.getApplication();
       for (String url : result.getBranchUrls()) {
@@ -44,5 +71,67 @@ class DefaultBranchConfigInitializer implements Runnable {
       }
       myBunch.updateForRoot(myRoot, new InfoStorage<SvnBranchConfigurationNew>(result, InfoReliability.defaultValues), null);
     }
+  }
+
+  @Nullable
+  public SvnBranchConfigurationNew loadDefaultConfiguration() {
+    try {
+      final SvnVcs vcs = SvnVcs.getInstance(myProject);
+
+      File rootFile = new File(myRoot.getPath());
+      final Info info = vcs.getInfo(rootFile);
+      if (info == null || info.getURL() == null) {
+        LOG.info("Directory is not a working copy: " + myRoot.getPresentableUrl());
+        return null;
+      }
+      SVNURL baseUrl = info.getURL();
+
+      final SvnBranchConfigurationNew result = new SvnBranchConfigurationNew();
+      result.setTrunkUrl(baseUrl.toString());
+      while (true) {
+        final String s = SVNPathUtil.tail(baseUrl.getPath());
+        if (s.equalsIgnoreCase(DEFAULT_TRUNK_NAME) || s.equalsIgnoreCase(DEFAULT_BRANCHES_NAME) || s.equalsIgnoreCase(DEFAULT_TAGS_NAME)) {
+          SVNURL rootPath = baseUrl.removePathTail();
+          SvnTarget target = SvnTarget.fromURL(rootPath);
+
+          vcs.getFactory(target).createBrowseClient().list(target, SVNRevision.HEAD, Depth.IMMEDIATES, createHandler(result, rootPath));
+          break;
+        }
+        if (SVNPathUtil.removeTail(baseUrl.getPath()).length() == 0) {
+          break;
+        }
+        baseUrl = baseUrl.removePathTail();
+      }
+      return result;
+    }
+    catch (SVNException e) {
+      LOG.info(e);
+      return null;
+    }
+    catch (VcsException e) {
+      LOG.info(e);
+      return null;
+    }
+  }
+
+  @NotNull
+  private static DirectoryEntryConsumer createHandler(final SvnBranchConfigurationNew result, final SVNURL rootPath) {
+    return new DirectoryEntryConsumer() {
+
+      @Override
+      public void consume(final DirectoryEntry entry) throws SVNException {
+        if (entry.isDirectory()) {
+          SVNURL childUrl = rootPath.appendPath(entry.getName(), false);
+
+          if (StringUtil.endsWithIgnoreCase(entry.getName(), DEFAULT_TRUNK_NAME)) {
+            result.setTrunkUrl(childUrl.toString());
+          }
+          else {
+            result.addBranches(childUrl.toString(),
+                               new InfoStorage<List<SvnBranchItem>>(new ArrayList<SvnBranchItem>(0), InfoReliability.defaultValues));
+          }
+        }
+      }
+    };
   }
 }
