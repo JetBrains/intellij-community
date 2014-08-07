f85bf0e65e51a8c03232ba0ca0f8f6fad26b2c39
Konstantin Bulenkov <kb@jetbrains.com>
2014-08-07 17:21:08 +0200

    remove etched border

--- platform/platform-api/src/com/intellij/openapi/ui/PanelWithText.java
+++ platform/platform-api/src/com/intellij/openapi/ui/PanelWithText.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,7 +36,7 @@ public class PanelWithText extends JPanel {
 
   public PanelWithText(String text) {
     super(new GridBagLayout());
-    setBorder(BorderFactory.createEtchedBorder());
+    //setBorder(BorderFactory.createEtchedBorder());
     myLabel.setText(XmlStringUtil.wrapInHtml(text));
     add(myLabel, new GridBagConstraints(0, 0, 1, 1, 1, 1, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, new Insets(8,8,8,8), 0, 0));
   }
