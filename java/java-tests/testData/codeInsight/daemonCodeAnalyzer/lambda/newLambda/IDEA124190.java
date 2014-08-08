7c6d93dfb49247381b4e5b86da896d5b1c01ba76
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-08-08 19:48:17 +0400

    method reference: take type args into account

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/lambda/newLambda/IDEA124190.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/lambda/newLambda/IDEA124190.java
@@ -5,6 +5,7 @@ class Test {
 
   void foo(final Stream<String> stream){
     stream.collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+    stream.collect(ArrayList<String>::new, ArrayList::add, ArrayList::addAll);
     stream.collect(ArrayList::new, Collection::add, Collection::addAll);
   }
 
