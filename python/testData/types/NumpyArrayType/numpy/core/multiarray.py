18a30146bc145512b125a9be1307a7fa92ebd969
Alexander Marchuk <Alexander.Marchuk@jetbrains.com>
2014-08-13 13:55:36 +0400

    update test for PY-13653

--- python/testData/types/NumpyArrayType/numpy/core/multiarray.py
+++ python/testData/types/NumpyArrayType/numpy/core/multiarray.py
@@ -1,3 +1,95 @@
+def array(p_object, dtype=None, copy=True, order=None, subok=False, ndmin=0): # real signature unknown; restored from __doc__
+    """
+    array(object, dtype=None, copy=True, order=None, subok=False, ndmin=0)
+
+        Create an array.
+
+        Parameters
+        ----------
+        object : array_like
+            An array, any object exposing the array interface, an
+            object whose __array__ method returns an array, or any
+            (nested) sequence.
+        dtype : data-type, optional
+            The desired data-type for the array.  If not given, then
+            the type will be determined as the minimum type required
+            to hold the objects in the sequence.  This argument can only
+            be used to 'upcast' the array.  For downcasting, use the
+            .astype(t) method.
+        copy : bool, optional
+            If true (default), then the object is copied.  Otherwise, a copy
+            will only be made if __array__ returns a copy, if obj is a
+            nested sequence, or if a copy is needed to satisfy any of the other
+            requirements (`dtype`, `order`, etc.).
+        order : {'C', 'F', 'A'}, optional
+            Specify the order of the array.  If order is 'C' (default), then the
+            array will be in C-contiguous order (last-index varies the
+            fastest).  If order is 'F', then the returned array
+            will be in Fortran-contiguous order (first-index varies the
+            fastest).  If order is 'A', then the returned array may
+            be in any order (either C-, Fortran-contiguous, or even
+            discontiguous).
+        subok : bool, optional
+            If True, then sub-classes will be passed-through, otherwise
+            the returned array will be forced to be a base-class array (default).
+        ndmin : int, optional
+            Specifies the minimum number of dimensions that the resulting
+            array should have.  Ones will be pre-pended to the shape as
+            needed to meet this requirement.
+
+        Returns
+        -------
+        out : ndarray
+            An array object satisfying the specified requirements.
+
+        See Also
+        --------
+        empty, empty_like, zeros, zeros_like, ones, ones_like, fill
+
+        Examples
+        --------
+        >>> np.array([1, 2, 3])
+        array([1, 2, 3])
+
+        Upcasting:
+
+        >>> np.array([1, 2, 3.0])
+        array([ 1.,  2.,  3.])
+
+        More than one dimension:
+
+        >>> np.array([[1, 2], [3, 4]])
+        array([[1, 2],
+               [3, 4]])
+
+        Minimum dimensions 2:
+
+        >>> np.array([1, 2, 3], ndmin=2)
+        array([[1, 2, 3]])
+
+        Type provided:
+
+        >>> np.array([1, 2, 3], dtype=complex)
+        array([ 1.+0.j,  2.+0.j,  3.+0.j])
+
+        Data-type consisting of more than one element:
+
+        >>> x = np.array([(1,2),(3,4)],dtype=[('a','<i4'),('b','<i4')])
+        >>> x['a']
+        array([1, 3])
+
+        Creating an array from sub-classes:
+
+        >>> np.array(np.mat('1 2; 3 4'))
+        array([[1, 2],
+               [3, 4]])
+
+        >>> np.array(np.mat('1 2; 3 4'), subok=True)
+        matrix([[1, 2],
+                [3, 4]])
+    """
+    pass
+
 class ndarray(object):
     """
     ndarray(shape, dtype=float, buffer=None, offset=0,
@@ -118,12 +210,3 @@ class ndarray(object):
         array([2, 3])
     """
     pass
-
-
-    def __mul__(self, y):
-        """ x.__mul__(y) <==> x*y """
-        pass
-
-    def __rmul__(self, y):
-        """ x.__rmul__(y) <==> x*y """
-        pass
