bc8eb9de86fdecf37bba501054c2a2be3c749ee1
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2014-08-16 14:35:15 +0400

    updated empty panel

--- python/edu/learn-python/src/com/jetbrains/python/edu/StudyInstructionPainter.java
+++ python/edu/learn-python/src/com/jetbrains/python/edu/StudyInstructionPainter.java
@@ -30,19 +30,17 @@ public class StudyInstructionPainter extends EditorEmptyTextPainter {
     g.setFont(UIUtil.getLabelFont().deriveFont(isDarkBackground ? 24f : 20f));
 
     UIUtil.TextPainter painter = new UIUtil.TextPainter().withLineSpacing(1.5f);
-    painter.withShadow(true, new JBColor(Gray._200.withAlpha(100), Gray._0.withAlpha(255)));
 
     painter.appendLine("PyCharm Educational Edition").underlined(new JBColor(Gray._150, Gray._180));
-    painter.appendLine("Learn Python programming in your favorite IDE").smaller();
-    painter.appendLine("Navigate between tasks with Ctrl + Shift + < and Ctrl + Shift + >").smaller().withBullet();
     painter.appendLine("Navigate to the next task window with Ctrl + Enter").smaller().withBullet();
     painter.appendLine("Navigate between task windows with Ctrl + < and Ctrl + >").smaller().withBullet();
-    painter.draw(g, new PairFunction<Integer, Integer, Couple<Integer>>() {
-      @Override
-      public Couple<Integer> fun(Integer width, Integer height) {
-        Dimension s = splitters.getSize();
-        return Couple.of((s.width - width) / 2, (s.height - height) / 2);
-      }
-    });
+    painter.appendLine("To see your progress open the 'Course Description' panel").smaller().withBullet();
+                       painter.draw(g, new PairFunction<Integer, Integer, Couple<Integer>>() {
+                         @Override
+                         public Couple<Integer> fun(Integer width, Integer height) {
+                           Dimension s = splitters.getSize();
+                           return Couple.of((s.width - width) / 2, (s.height - height) / 2);
+                         }
+                       });
   }
 }
