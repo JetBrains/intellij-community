90d68d86394a9295a3c3ea7e0c032812f5e63245
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2014-08-21 14:13:21 +0400

    tests: test allowed to run on module classpath

--- platform/platform-tests/testSrc/com/intellij/openapi/keymap/impl/ModifierKeyDoubleClickHandlerTest.java
+++ platform/platform-tests/testSrc/com/intellij/openapi/keymap/impl/ModifierKeyDoubleClickHandlerTest.java
@@ -22,13 +22,13 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.KeyboardShortcut;
 import com.intellij.openapi.keymap.KeymapManager;
 import com.intellij.openapi.util.Clock;
-import com.intellij.testFramework.LightPlatformTestCase;
+import com.intellij.testFramework.LightPlatformLangTestCase;
 
 import javax.swing.*;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 
-public class ModifierKeyDoubleClickHandlerTest extends LightPlatformTestCase {
+public class ModifierKeyDoubleClickHandlerTest extends LightPlatformLangTestCase {
   private static final String MY_SHIFT_SHIFT_ACTION = "ModifierKeyDoubleClickHandlerTest.action1";
   private static final String MY_SHIFT_KEY_ACTION = "ModifierKeyDoubleClickHandlerTest.action2";
   private static final String MY_SHIFT_SHIFT_KEY_ACTION = "ModifierKeyDoubleClickHandlerTest.action3";
