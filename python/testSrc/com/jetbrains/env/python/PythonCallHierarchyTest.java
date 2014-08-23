54ad0ba635ef83dd23bbccdac52709501bb4a585
novokrest <novokrest013@gmail.com>
2014-08-24 02:22:35 +0400

    remove unnecessary components from PythonCallHierarchyTest

--- python/testSrc/com/jetbrains/env/python/PythonCallHierarchyTest.java
+++ python/testSrc/com/jetbrains/env/python/PythonCallHierarchyTest.java
@@ -15,52 +15,14 @@
  */
 package com.jetbrains.env.python;
 
-import com.intellij.ide.hierarchy.HierarchyBrowserBaseEx;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.JDOMUtil;
-import com.intellij.openapi.vfs.LocalFileSystem;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiManager;
-import com.intellij.util.ui.UIUtil;
 import com.jetbrains.env.PyEnvTestCase;
-import com.jetbrains.env.python.debug.PyDebuggerTask;
 import com.jetbrains.env.python.hierarchy.PyCallHierarchyTask;
-import com.jetbrains.python.PythonTestUtil;
-import com.jetbrains.python.hierarchy.HierarchyTreeStructureViewer;
-import com.jetbrains.python.hierarchy.call.PyCalleeFunctionTreeStructure;
-import com.jetbrains.python.hierarchy.call.PyCallerFunctionTreeStructure;
-import com.jetbrains.python.psi.PyFunction;
-import com.jetbrains.python.psi.PyRecursiveElementVisitor;
-import org.jdom.Document;
-
-import java.io.File;
 
 
 public class PythonCallHierarchyTest extends PyEnvTestCase {
 
-  private static final String TARGET_FUNCTION_NAME = "target_func";
-  private static final String CALLER_VERIFICATION_SUFFIX = "_caller_verification.xml";
-  private static final String CALLEE_VERIFICATION_SUFFIX = "_callee_verification.xml";
-
   private String getBasePath() {
     return "/hierarchy/call/Dynamic/" + getTestName(false);
-  }
-
-  private static String getTestDataPath() {
-    return PythonTestUtil.getTestDataPath();
-  }
-
-  private String getVerificationFilePath(final String suffix) {
-    return getTestDataPath() + "/" + getBasePath() + "/" + getTestName(false) + suffix;
-  }
-
-  private String getVerificationCallerFilePath() {
-    return getVerificationFilePath(CALLER_VERIFICATION_SUFFIX);
-  }
-
-  private String getVerificationCalleeFilePath() {
-    return getVerificationFilePath(CALLEE_VERIFICATION_SUFFIX);
   }
 
   public void testSimple() throws Exception {
