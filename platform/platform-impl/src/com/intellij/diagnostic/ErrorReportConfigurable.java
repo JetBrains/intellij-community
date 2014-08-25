104fe76911476b47f861ca38547a677713df9132
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-08-25 13:44:39 +0200

    use our util Base64 platform-api doesn't export commons codec dependency

--- platform/platform-impl/src/com/intellij/diagnostic/ErrorReportConfigurable.java
+++ platform/platform-impl/src/com/intellij/diagnostic/ErrorReportConfigurable.java
@@ -21,7 +21,8 @@ import com.intellij.openapi.util.DefaultJDOMExternalizer;
 import com.intellij.openapi.util.InvalidDataException;
 import com.intellij.openapi.util.JDOMExternalizable;
 import com.intellij.openapi.util.WriteExternalException;
-import org.apache.commons.codec.binary.Base64;
+import com.intellij.openapi.vfs.CharsetToolkit;
+import com.intellij.util.Base64;
 import org.jdom.Element;
 import org.jetbrains.annotations.NotNull;
 
@@ -39,15 +40,17 @@ public class ErrorReportConfigurable implements JDOMExternalizable, NamedCompone
   @Override
   public void readExternal(Element element) throws InvalidDataException {
     DefaultJDOMExternalizer.readExternal(this, element);
-    if (! KEEP_ITN_PASSWORD)
+    if (!KEEP_ITN_PASSWORD) {
       ITN_PASSWORD_CRYPT = "";
+    }
   }
 
   @Override
   public void writeExternal(Element element) throws WriteExternalException {
     String itnPassword = ITN_PASSWORD_CRYPT;
-    if (! KEEP_ITN_PASSWORD)
+    if (!KEEP_ITN_PASSWORD) {
       ITN_PASSWORD_CRYPT = "";
+    }
     DefaultJDOMExternalizer.writeExternal(this, element);
 
     ITN_PASSWORD_CRYPT = itnPassword;
@@ -59,11 +62,11 @@ public class ErrorReportConfigurable implements JDOMExternalizable, NamedCompone
     return "ErrorReportConfigurable";
   }
 
-  public String getPlainItnPassword () {
-    return new String(new Base64().decode(getInstance().ITN_PASSWORD_CRYPT.getBytes()));
+  public String getPlainItnPassword() {
+    return new String(Base64.decode(getInstance().ITN_PASSWORD_CRYPT), CharsetToolkit.UTF8_CHARSET);
   }
 
-  public void setPlainItnPassword (String password) {
-    ITN_PASSWORD_CRYPT = new String(new Base64().encode(password.getBytes()));
+  public void setPlainItnPassword(String password) {
+    ITN_PASSWORD_CRYPT = Base64.encode(password.getBytes(CharsetToolkit.UTF8_CHARSET));
   }
 }
