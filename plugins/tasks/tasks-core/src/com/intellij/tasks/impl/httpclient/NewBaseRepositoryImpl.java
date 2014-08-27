c0e9be4692dd8f9e4fa3b55913aded7b5fc9e3c9
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-08-27 08:09:04 +0200

    cleanup

--- plugins/tasks/tasks-core/src/com/intellij/tasks/impl/httpclient/NewBaseRepositoryImpl.java
+++ plugins/tasks/tasks-core/src/com/intellij/tasks/impl/httpclient/NewBaseRepositoryImpl.java
@@ -34,7 +34,6 @@ import java.io.IOException;
  * @author Mikhail Golubev
  */
 public abstract class NewBaseRepositoryImpl extends BaseRepository {
-  private static final Logger LOG = Logger.getInstance(NewBaseRepositoryImpl.class);
   private static final AuthScope BASIC_AUTH_SCOPE =
     new AuthScope(AuthScope.ANY_HOST, AuthScope.ANY_PORT, AuthScope.ANY_REALM, AuthSchemes.BASIC);
   // Provides preemptive authentication in HttpClient 4.x
