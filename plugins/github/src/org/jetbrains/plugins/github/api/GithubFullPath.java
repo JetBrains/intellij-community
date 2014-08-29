e75d6b67e9fae9435d8725a366052ec7acf8a6f0
Aleksey Pivovarov <Aleksey.Pivovarov@jetbrains.com>
2014-08-29 18:17:39 +0400

    github: fix typo

--- plugins/github/src/org/jetbrains/plugins/github/api/GithubFullPath.java
+++ plugins/github/src/org/jetbrains/plugins/github/api/GithubFullPath.java
@@ -58,8 +58,8 @@ public class GithubFullPath {
 
     GithubFullPath that = (GithubFullPath)o;
 
-    if (!StringUtil.endsWithIgnoreCase(myRepositoryName, that.myRepositoryName)) return false;
-    if (!StringUtil.endsWithIgnoreCase(myUserName, that.myUserName)) return false;
+    if (!StringUtil.equalsIgnoreCase(myRepositoryName, that.myRepositoryName)) return false;
+    if (!StringUtil.equalsIgnoreCase(myUserName, that.myUserName)) return false;
 
     return true;
   }
