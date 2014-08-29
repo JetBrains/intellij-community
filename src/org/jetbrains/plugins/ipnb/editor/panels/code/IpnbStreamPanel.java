cb36627eab501481675c91d9597b03e18028258b
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2014-08-29 13:04:30 +0400

    stream panel is basically the same as output panel

--- src/org/jetbrains/plugins/ipnb/editor/panels/code/IpnbStreamPanel.java
+++ src/org/jetbrains/plugins/ipnb/editor/panels/code/IpnbStreamPanel.java
@@ -3,17 +3,8 @@ package org.jetbrains.plugins.ipnb.editor.panels.code;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.ipnb.format.cells.output.IpnbStreamOutputCell;
 
-import javax.swing.*;
-
 public class IpnbStreamPanel extends IpnbCodeOutputPanel<IpnbStreamOutputCell> {
   public IpnbStreamPanel(@NotNull final IpnbStreamOutputCell cell) {
     super(cell);
-  }
-
-  @Override
-  protected JComponent createViewPanel() {
-    JTextArea textArea = new JTextArea(myCell.getSourceAsString());
-    textArea.setEditable(false);
-    return textArea;
   }
 }
