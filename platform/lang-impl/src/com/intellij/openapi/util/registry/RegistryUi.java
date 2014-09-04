d1e4e88bca9d507faad9438f5cd4c07971d249a3
Konstantin Bulenkov <kb@jetbrains.com>
2014-09-04 13:29:49 +0400

    fix AIOOB while editing colors

--- platform/lang-impl/src/com/intellij/openapi/util/registry/RegistryUi.java
+++ platform/lang-impl/src/com/intellij/openapi/util/registry/RegistryUi.java
@@ -471,7 +471,7 @@ public class RegistryUi implements Disposable {
     public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
       myValue = ((MyTableModel)table.getModel()).getRegistryValue(row);
       if (myValue.asColor(null) != null) {
-        final Color color = ColorChooser.chooseColor(table, "Choose color", ((RegistryValue)value).asColor(Color.WHITE));
+        final Color color = ColorChooser.chooseColor(table, "Choose color", myValue.asColor(Color.WHITE));
         if (color != null) {
           myValue.setValue(color.getRed() + "," + color.getGreen() + "," + color.getBlue());
         }
