eff86b77f818c7c4a42820e3eaf5be5e839f1ad7
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2014-09-04 18:56:39 +0400

    Extracted common abstract base class

--- python/src/com/jetbrains/python/hierarchy/call/PyCalleeFunctionTreeStructure.java
+++ python/src/com/jetbrains/python/hierarchy/call/PyCalleeFunctionTreeStructure.java
@@ -15,78 +15,33 @@
  */
 package com.jetbrains.python.hierarchy.call;
 
-import com.intellij.ide.hierarchy.HierarchyNodeDescriptor;
-import com.intellij.ide.hierarchy.HierarchyTreeStructure;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
-import com.intellij.util.ArrayUtil;
-import com.intellij.util.containers.hash.HashMap;
-import com.jetbrains.python.hierarchy.PyHierarchyNodeDescriptor;
-import com.jetbrains.python.psi.PyClass;
 import com.jetbrains.python.psi.PyElement;
-import com.jetbrains.python.psi.PyFile;
-import com.jetbrains.python.psi.PyFunction;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author novokrest
  */
-public class PyCalleeFunctionTreeStructure extends HierarchyTreeStructure {
-  private final String myScopeType;
-
+public class PyCalleeFunctionTreeStructure extends PyCallHierarchyTreeStructureBase {
   public PyCalleeFunctionTreeStructure(Project project, PsiElement element, String currentScopeType) {
-    super(project, new PyHierarchyNodeDescriptor(null, element, true));
-    myScopeType = currentScopeType;
+    super(project, element, currentScopeType);
   }
 
   @NotNull
   @Override
-  protected Object[] buildChildren(@NotNull HierarchyNodeDescriptor descriptor) {
-    final List<PyHierarchyNodeDescriptor> descriptors = new ArrayList<PyHierarchyNodeDescriptor>();
-    if (descriptor instanceof PyHierarchyNodeDescriptor) {
-      final PyHierarchyNodeDescriptor pyDescriptor = (PyHierarchyNodeDescriptor)descriptor;
-      final PsiElement element = pyDescriptor.getPsiElement();
-      final boolean isCallable = element instanceof PyFunction || element instanceof PyClass || element instanceof PyFile;
-      HierarchyNodeDescriptor nodeDescriptor = getBaseDescriptor();
-      if (!(element instanceof PyElement) || !isCallable || nodeDescriptor == null) {
-        return ArrayUtil.EMPTY_OBJECT_ARRAY;
-      }
-
-      final List<PsiElement> callees = new ArrayList<PsiElement>();
-
-      PyCallDataManager[] functionManagers = {
-        // TODO: Add dynamic call data manager
-        PyStaticCallDataManager.getInstance(myProject),
-      };
-
-      for (PyCallDataManager functionManager : functionManagers) {
-        callees.addAll(functionManager.getCallees((PyElement)element));
-      }
-
-      final Map<PsiElement, PyHierarchyNodeDescriptor> calleeToDescriptorMap = new HashMap<PsiElement, PyHierarchyNodeDescriptor>();
-      PsiElement baseClass = element instanceof PyFunction ? ((PyFunction)element).getContainingClass() : null;
-
-      for (PsiElement callee : callees) {
-        if (isInScope(baseClass, callee, myScopeType)) {
-          PyHierarchyNodeDescriptor calleeDescriptor = calleeToDescriptorMap.get(callee);
-          if (calleeDescriptor == null) {
-            calleeDescriptor = new PyHierarchyNodeDescriptor(descriptor, callee, false);
-            calleeToDescriptorMap.put(callee, calleeDescriptor);
-            descriptors.add(calleeDescriptor);
-          }
-        }
-      }
-
+  protected List<PsiElement> getChildren(@NotNull PyElement element) {
+    final List<PsiElement> callees = new ArrayList<PsiElement>();
+    final PyCallDataManager[] functionManagers = {
+      // TODO: Add dynamic call data manager
+      PyStaticCallDataManager.getInstance(myProject),
+    };
+    for (PyCallDataManager functionManager : functionManagers) {
+      callees.addAll(functionManager.getCallees(element));
     }
-    return ArrayUtil.toObjectArray(descriptors);
-  }
-
-  @Override
-  public boolean isAlwaysShowPlus() {
-    return true;
+    return callees;
   }
 }
--- python/src/com/jetbrains/python/hierarchy/call/PyCallerFunctionTreeStructure.java
+++ python/src/com/jetbrains/python/hierarchy/call/PyCallerFunctionTreeStructure.java
@@ -15,77 +15,33 @@
  */
 package com.jetbrains.python.hierarchy.call;
 
-import com.intellij.ide.hierarchy.HierarchyNodeDescriptor;
-import com.intellij.ide.hierarchy.HierarchyTreeStructure;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
-import com.intellij.util.ArrayUtil;
-import com.jetbrains.python.hierarchy.PyHierarchyNodeDescriptor;
-import com.jetbrains.python.psi.PyClass;
 import com.jetbrains.python.psi.PyElement;
-import com.jetbrains.python.psi.PyFile;
-import com.jetbrains.python.psi.PyFunction;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 /**
  * @author novokrest
  */
-public class PyCallerFunctionTreeStructure extends HierarchyTreeStructure {
-  private final String myScopeType;
-
+public class PyCallerFunctionTreeStructure extends PyCallHierarchyTreeStructureBase {
   public PyCallerFunctionTreeStructure(Project project, PsiElement element, String currentScopeType) {
-    super(project, new PyHierarchyNodeDescriptor(null, element, true));
-    myScopeType = currentScopeType;
+    super(project, element, currentScopeType);
   }
 
   @NotNull
   @Override
-  protected Object[] buildChildren(@NotNull HierarchyNodeDescriptor descriptor) {
-    final List<PyHierarchyNodeDescriptor> descriptors = new ArrayList<PyHierarchyNodeDescriptor>();
-    if (descriptor instanceof PyHierarchyNodeDescriptor) {
-      final PyHierarchyNodeDescriptor pyDescriptor = (PyHierarchyNodeDescriptor)descriptor;
-      final PsiElement element = pyDescriptor.getPsiElement();
-      final boolean isCallable = element instanceof PyFunction || element instanceof PyClass || element instanceof PyFile;
-      HierarchyNodeDescriptor nodeDescriptor = getBaseDescriptor();
-      if (!(element instanceof PyElement) || !isCallable || nodeDescriptor == null) {
-        return ArrayUtil.EMPTY_OBJECT_ARRAY;
-      }
-
-      final List<PsiElement> callers = new ArrayList<PsiElement>();
-
-      PyCallDataManager[] functionManagers = {
-        // TODO: Add dynamic call data manager
-        PyStaticCallDataManager.getInstance(myProject),
-      };
-
-      for (PyCallDataManager functionManager : functionManagers) {
-        callers.addAll(functionManager.getCallers((PyElement)element));
-      }
-
-      final HashMap<PsiElement, PyHierarchyNodeDescriptor> callerToDescriptorMap = new HashMap<PsiElement, PyHierarchyNodeDescriptor>();
-      PsiElement baseClass = element instanceof PyFunction ? ((PyFunction)element).getContainingClass() : null;
-
-      for (PsiElement caller : callers) {
-        if (isInScope(baseClass, caller, myScopeType)) {
-          PyHierarchyNodeDescriptor callerDescriptor = callerToDescriptorMap.get(caller);
-          if (callerDescriptor == null) {
-            callerDescriptor = new PyHierarchyNodeDescriptor(descriptor, caller, false);
-            callerToDescriptorMap.put(caller, callerDescriptor);
-            descriptors.add(callerDescriptor);
-          }
-        }
-      }
-
+  protected List<PsiElement> getChildren(@NotNull PyElement element) {
+    final List<PsiElement> callers = new ArrayList<PsiElement>();
+    final PyCallDataManager[] functionManagers = {
+      // TODO: Add dynamic call data manager
+      PyStaticCallDataManager.getInstance(myProject),
+    };
+    for (PyCallDataManager functionManager : functionManagers) {
+      callers.addAll(functionManager.getCallers(element));
     }
-    return ArrayUtil.toObjectArray(descriptors);
-  }
-
-  @Override
-  public boolean isAlwaysShowPlus() {
-    return true;
+    return callers;
   }
 }
