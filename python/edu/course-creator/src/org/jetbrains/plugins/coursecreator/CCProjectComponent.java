d785412c8e1ec0880a8febb178d82840e472ce15
Liana Bakradze <liana.bakradze@jetbrains.com>
2014-09-05 13:06:27 +0400

    EDU-147 preview mode for task files

--- python/edu/course-creator/src/org/jetbrains/plugins/coursecreator/CCProjectComponent.java
+++ python/edu/course-creator/src/org/jetbrains/plugins/coursecreator/CCProjectComponent.java
@@ -1,12 +1,13 @@
 package org.jetbrains.plugins.coursecreator;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.EditorFactory;
 import com.intellij.openapi.editor.event.EditorFactoryEvent;
 import com.intellij.openapi.editor.impl.EditorFactoryImpl;
-import com.intellij.openapi.fileEditor.FileEditor;
-import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.fileEditor.*;
 import com.intellij.openapi.fileEditor.impl.text.PsiAwareTextEditorImpl;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
@@ -15,7 +16,10 @@ import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.coursecreator.format.Course;
 
+import java.io.IOException;
+
 public class CCProjectComponent implements ProjectComponent {
+  private static final Logger LOG = Logger.getInstance(CCProjectComponent.class.getName());
   private final Project myProject;
 
   public CCProjectComponent(Project project) {
@@ -39,13 +43,43 @@ public class CCProjectComponent implements ProjectComponent {
       public void run() {
         Course course = CCProjectService.getInstance(myProject).getCourse();
         if (course != null) {
-          EditorFactory.getInstance().addEditorFactoryListener(new CCEditorFactoryListener(), myProject);
+          myProject.getMessageBus().connect(myProject).subscribe(
+            FileEditorManagerListener.FILE_EDITOR_MANAGER, new FileEditorManagerAdapter() {
+              @Override
+              public void selectionChanged(@NotNull FileEditorManagerEvent event) {
+                final VirtualFile oldFile = event.getOldFile();
+                if (oldFile == null) {
+                  return;
+                }
+                if (CCProjectService.getInstance(myProject).isTaskFile(oldFile)) {
+                  FileEditorManager.getInstance(myProject).closeFile(oldFile);
+                    ApplicationManager.getApplication().runWriteAction(new Runnable() {
+                      @Override
+                      public void run() {
+                        try {
+                          oldFile.delete(myProject);
+                        }
+                        catch (IOException e) {
+                          LOG.error(e);
+                        }
+                      }
+                    });
+                }
+              }
+            });
+
+          final CCEditorFactoryListener editorFactoryListener = new CCEditorFactoryListener();
+          EditorFactory.getInstance().addEditorFactoryListener(editorFactoryListener, myProject);
           VirtualFile[] files = FileEditorManager.getInstance(myProject).getOpenFiles();
           for (VirtualFile file : files) {
+            if (CCProjectService.getInstance(myProject).isTaskFile(file)) {
+              FileEditorManager.getInstance(myProject).closeFile(file);
+              continue;
+            }
             FileEditor fileEditor = FileEditorManager.getInstance(myProject).getSelectedEditor(file);
             if (fileEditor instanceof PsiAwareTextEditorImpl) {
               Editor editor = ((PsiAwareTextEditorImpl)fileEditor).getEditor();
-              new CCEditorFactoryListener().editorCreated(new EditorFactoryEvent(new EditorFactoryImpl(ProjectManager.getInstance()), editor ));
+              editorFactoryListener.editorCreated(new EditorFactoryEvent(new EditorFactoryImpl(ProjectManager.getInstance()), editor));
             }
           }
         }
