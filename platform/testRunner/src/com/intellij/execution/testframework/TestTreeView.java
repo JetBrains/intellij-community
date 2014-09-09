be895ed80c39f40040c70e8b7e7e24cb29434200
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-09-09 13:39:35 +0400

    navigate through test failures without closing diff view (IDEA-119967)

--- platform/testRunner/src/com/intellij/execution/testframework/TestTreeView.java
+++ platform/testRunner/src/com/intellij/execution/testframework/TestTreeView.java
@@ -46,6 +46,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class TestTreeView extends Tree implements DataProvider, CopyProvider {
+  public static final DataKey<TestFrameworkRunningModel> MODEL_DATA_KEY = DataKey.create("testFrameworkModel.dataId");
+
   private TestFrameworkRunningModel myModel;
 
   protected abstract TreeCellRenderer getRenderer(TestConsoleProperties properties);
@@ -127,7 +129,11 @@ public abstract class TestTreeView extends Tree implements DataProvider, CopyPro
         return locations.isEmpty() ? null : locations.toArray(new Location[locations.size()]);
       }
     }
-    
+
+    if (MODEL_DATA_KEY.is(dataId)) {
+      return myModel;
+    }
+
     final TreePath selectionPath = getSelectionPath();
     if (selectionPath == null) return null;
     final AbstractTestProxy testProxy = getSelectedTest(selectionPath);
