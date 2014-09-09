d750b63c8de817613774ca5528bf34028a417031
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2014-09-09 14:11:07 +0400

    Resolve "values" and "valueOf" in enum class object
    Import enum class's static scope into its class object's member scope
    
    GitOrigin-RevId: c4038857f8f3edd5d672c5ee5b6c7cc9534e6aad

--- plugins/kotlin/idea/tests/testData/checker/ClassObjectInEnum.kt
+++ plugins/kotlin/idea/tests/testData/checker/ClassObjectInEnum.kt
@@ -3,7 +3,14 @@ enum class E {
 
     class object {
         fun foo(): E = ENTRY
+        fun bar(): Array<E> = values()
+        fun baz(): E = valueOf("ENTRY")
+        val valuez = values()
     }
+
+    fun oof(): E = ENTRY
+    fun rab(): Array<E> = values()
+    fun zab(): E = valueOf("ENTRY")
 }
 
 fun foo() = E.ENTRY
