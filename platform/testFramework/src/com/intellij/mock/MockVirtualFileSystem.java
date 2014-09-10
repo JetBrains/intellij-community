fb82f788095b858607e2f83a7a1001e2284a8efb
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-09-10 18:15:16 +0200

    notnull, use thashmap, simlpify — add getRoot

--- platform/testFramework/src/com/intellij/mock/MockVirtualFileSystem.java
+++ platform/testFramework/src/com/intellij/mock/MockVirtualFileSystem.java
@@ -20,12 +20,14 @@ import com.intellij.openapi.vfs.DeprecatedVirtualFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileSystem;
 import com.intellij.testFramework.LightVirtualFile;
+import gnu.trove.THashMap;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.HashMap;
+import java.util.Map;
 
 public class MockVirtualFileSystem extends DeprecatedVirtualFileSystem {
   private final MyVirtualFile myRoot = new MyVirtualFile("", null);
@@ -42,6 +44,11 @@ public class MockVirtualFileSystem extends DeprecatedVirtualFileSystem {
       file = file.getOrCreate(component);
     }
     return file;
+  }
+
+  @NotNull
+  public VirtualFile getRoot() {
+    return myRoot;
   }
 
   @Override
@@ -93,11 +100,12 @@ public class MockVirtualFileSystem extends DeprecatedVirtualFileSystem {
   }
 
   public class MyVirtualFile extends LightVirtualFile {
-    private final HashMap<String, MyVirtualFile> myChildren = new HashMap<String, MyVirtualFile>();
+    private final Map<String, MyVirtualFile> myChildren = new THashMap<String, MyVirtualFile>();
     private final MyVirtualFile myParent;
 
-    public MyVirtualFile(String name, MyVirtualFile parent) {
+    public MyVirtualFile(@NotNull String name, @Nullable MyVirtualFile parent) {
       super(name);
+
       myParent = parent;
     }
 
@@ -108,7 +116,7 @@ public class MockVirtualFileSystem extends DeprecatedVirtualFileSystem {
     }
 
     @NotNull
-    public MyVirtualFile getOrCreate(String name) {
+    public MyVirtualFile getOrCreate(@NotNull String name) {
       MyVirtualFile file = myChildren.get(name);
       if (file == null) {
         file = new MyVirtualFile(name, this);
