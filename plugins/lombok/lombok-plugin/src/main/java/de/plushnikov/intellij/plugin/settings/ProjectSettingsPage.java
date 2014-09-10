6fb4de576a145a221147623b20c615e496e255fa
Michail Plushnikov <michail@plushnikov.de>
2014-09-10 10:24:22 +0200

    updated master to be Intellij 14 compatible
    GitOrigin-RevId: 673783ab95ae0c622fb9c8d1a192f57c3163fb76

--- plugins/lombok/lombok-plugin/src/main/java/de/plushnikov/intellij/plugin/settings/ProjectSettingsPage.java
+++ plugins/lombok/lombok-plugin/src/main/java/de/plushnikov/intellij/plugin/settings/ProjectSettingsPage.java
@@ -1,8 +1,5 @@
 package de.plushnikov.intellij.plugin.settings;
 
-import com.intellij.compiler.CompilerConfiguration;
-import com.intellij.compiler.CompilerConfigurationImpl;
-import com.intellij.compiler.CompilerWorkspaceConfiguration;
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.options.SearchableConfigurable;
@@ -12,34 +9,16 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-/**
- *
- */
 public class ProjectSettingsPage implements SearchableConfigurable, Configurable.NoScroll {
-  public static final String JAVAC_COMPILER_ID = "Javac";
 
   private JPanel myPanel;
   private JCheckBox myEnableLombokInProject;
-  private JLabel myAnnotationConfigurationInfo1Label;
-  private JLabel myAnnotationConfigurationInfo2Label;
-  private JLabel myAnnotationConfigurationInfo3Label;
-  private JLabel myAnnotationConfigurationOkLabel;
-  private JButton checkButton;
 
   private Project myProject;
 
   public ProjectSettingsPage(Project project) {
     myProject = project;
-
-    checkButton.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        updateAnnotationConfigurationInfo();
-      }
-    });
   }
 
   @Nls
@@ -61,27 +40,7 @@ public class ProjectSettingsPage implements SearchableConfigurable, Configurable
   @Override
   public JComponent createComponent() {
     myEnableLombokInProject.setSelected(ProjectSettings.isEnabledInProject(myProject));
-    updateAnnotationConfigurationInfo();
-
     return myPanel;
-  }
-
-  private void updateAnnotationConfigurationInfo() {
-    boolean annotationProcessingPossible = isLombokAnnotationProcessingPossible();
-
-    myAnnotationConfigurationOkLabel.setVisible(annotationProcessingPossible);
-    myAnnotationConfigurationInfo1Label.setVisible(!annotationProcessingPossible );
-    myAnnotationConfigurationInfo2Label.setVisible(!annotationProcessingPossible );
-    myAnnotationConfigurationInfo3Label.setVisible(!annotationProcessingPossible );
-  }
-
-  private boolean isLombokAnnotationProcessingPossible() {
-    final CompilerConfiguration compilerConfiguration = CompilerConfiguration.getInstance(myProject);
-    boolean javacCompiler = JAVAC_COMPILER_ID.equals(((CompilerConfigurationImpl) compilerConfiguration).getDefaultCompiler().getId());
-    boolean annotationProcessorsEnabled = compilerConfiguration.isAnnotationProcessorsEnabled();
-    boolean externBuild = CompilerWorkspaceConfiguration.getInstance(myProject).useOutOfProcessBuild();
-
-    return (externBuild && annotationProcessorsEnabled) || (!externBuild && !annotationProcessorsEnabled && javacCompiler);
   }
 
   @Override
