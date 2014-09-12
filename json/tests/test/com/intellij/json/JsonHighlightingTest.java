029138296f4d2e06ea873d44be98c75682851b7d
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2014-09-12 21:34:00 +0400

    Move highlighting tests from JavaScript to JSON
    Semantic highlighting of property keys is not supported for now however.

--- json/tests/test/com/intellij/json/JsonHighlightingTest.java
+++ json/tests/test/com/intellij/json/JsonHighlightingTest.java
@@ -1,24 +1,57 @@
 package com.intellij.json;
 
+import com.intellij.json.codeinsight.JsonStandardComplianceInspection;
+
 /**
  * @author Mikhail Golubev
  */
 public class JsonHighlightingTest extends JsonTestCase {
 
   private void doTest() {
-    final String TEST_PATH = "/highlighting/";
-    myFixture.testHighlighting(true, true, false, TEST_PATH + getTestName(false) + ".json");
+    doTestHighlighting(true, true, true);
+  }
+
+  private long doTestHighlighting(boolean checkInfo, boolean checkWeakWarning, boolean checkWarning) {
+    return myFixture.testHighlighting(checkWarning, checkInfo, checkWeakWarning, "/highlighting/" + getTestName(false) + ".json");
+  }
+
+  private void enableStandardComplianceInspection(boolean checkComments) {
+    final JsonStandardComplianceInspection inspection = new JsonStandardComplianceInspection();
+    inspection.myWarnAboutComments = checkComments;
+    myFixture.enableInspections(inspection);
   }
 
   public void testStringLiterals() {
     doTest();
   }
 
-  public void testCompliance() {
+  public void testComplianceProblemsTopLevelValue() {
+    enableStandardComplianceInspection(true);
     doTest();
   }
 
-  public void testComplianceTopLevelValues() {
+  public void testComplianceProblems() {
+    enableStandardComplianceInspection(true);
     doTest();
   }
+
+  // Moved from JavaScript
+
+  public void testJSON_with_comment() throws Exception {
+    enableStandardComplianceInspection(false);
+    doTest();
+  }
+
+  public void testJSON() throws Exception {
+    enableStandardComplianceInspection(true);
+    doTest();
+  }
+
+  // TODO: JSON semantic highlighting?
+  /*
+  public void testSemanticHighlighting() throws Exception {
+    doTest();
+  }
+  */
+
 }
