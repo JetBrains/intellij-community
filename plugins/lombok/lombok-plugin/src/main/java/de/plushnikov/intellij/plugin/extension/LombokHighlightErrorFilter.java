0257ab0012ae4330c66af1c0ed9306589544aecb
siosio <siosio12@gmail.com>
2014-09-17 08:47:17 +0900

    fixed #1 lombok-intellij-plugin-1 Problem of Lazy Getter
    GitOrigin-RevId: dda15ef19b19cb50f12c22586f9d778d6e36fa4c

--- plugins/lombok/lombok-plugin/src/main/java/de/plushnikov/intellij/plugin/extension/LombokHighlightErrorFilter.java
+++ plugins/lombok/lombok-plugin/src/main/java/de/plushnikov/intellij/plugin/extension/LombokHighlightErrorFilter.java
@@ -5,12 +5,24 @@ import com.intellij.codeInsight.daemon.impl.HighlightInfoFilter;
 import com.intellij.codeInsight.daemon.impl.HighlightInfoType;
 import com.intellij.lang.annotation.HighlightSeverity;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.PsiAnnotation;
+import com.intellij.psi.PsiAnnotationParameterList;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiField;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiIdentifier;
 import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiReferenceExpression;
+import com.intellij.psi.impl.source.tree.java.PsiAnnotationImpl;
 import com.intellij.psi.util.PsiTreeUtil;
 import de.plushnikov.intellij.plugin.handler.SneakyTrowsExceptionHandler;
+import de.plushnikov.intellij.plugin.util.PsiAnnotationUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LombokHighlightErrorFilter implements HighlightInfoFilter {
 
@@ -36,7 +48,39 @@ public class LombokHighlightErrorFilter implements HighlightInfoFilter {
           }
         }
       }
+
+      if (uninitializedField(description) && isLazyGetter(highlightInfo, file)) {
+        return false;
+      }
     }
     return true;
+  }
+
+  private boolean isLazyGetter(HighlightInfo highlightInfo, PsiFile file) {
+    PsiElement element = file.findElementAt(highlightInfo.getStartOffset());
+    if (!(element instanceof PsiIdentifier)) {
+      return false;
+    }
+    PsiField field = PsiTreeUtil.getParentOfType(element, PsiField.class);
+    if (field == null) {
+      return false;
+    }
+    Collection<PsiAnnotation> psiAnnotations = PsiTreeUtil.findChildrenOfType(field, PsiAnnotation.class);
+    for (PsiAnnotation psiAnnotation : psiAnnotations) {
+      if ("lombok.LazyGetter".equals(psiAnnotation.getQualifiedName())) {
+        return true;
+      } else if ("lombok.Getter".equals(psiAnnotation.getQualifiedName())) {
+        Boolean lazyObj = PsiAnnotationUtil.getAnnotationValue(psiAnnotation, "lazy", Boolean.class);
+        return null != lazyObj && lazyObj;
+      }
+    }
+    return false;
+  }
+
+  private static final Pattern UNINITIALIZED_MESSAGE = Pattern.compile("Variable '.+' might not have been initialized");
+
+  private boolean uninitializedField(String description) {
+    Matcher matcher = UNINITIALIZED_MESSAGE.matcher(description);
+    return matcher.matches();
   }
 }
