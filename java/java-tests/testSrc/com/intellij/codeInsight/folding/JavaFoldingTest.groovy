eeb5346457d917886ac7e3284fecf39e7979abcf
peter <peter@jetbrains.com>
2014-09-16 15:25:30 +0200

    extract JavaFoldingTest."test don't inline array methods"

--- java/java-tests/testSrc/com/intellij/codeInsight/folding/JavaFoldingTest.groovy
+++ java/java-tests/testSrc/com/intellij/codeInsight/folding/JavaFoldingTest.groovy
@@ -578,15 +578,21 @@ class Test {
    return 0;
  }
 
+}"""
+    PsiClass fooClass = JavaPsiFacade.getInstance(project).findClass('Foo', GlobalSearchScope.allScope(project))
+    def regions = myFixture.editor.foldingModel.allFoldRegions.sort { it.startOffset }
+    assert regions.size() == 3
+    checkAccessorFolding(regions[0], regions[1], fooClass.methods[0])
+  }
+
+  public void "test don't inline array methods"() {
+    configure """class Foo {
  int arrayMethod(int param)[] {
    return new int[];
  }
 
 }"""
-    PsiClass fooClass = JavaPsiFacade.getInstance(project).findClass('Foo', GlobalSearchScope.allScope(project))
-    def regions = myFixture.editor.foldingModel.allFoldRegions.sort { it.startOffset }
-    assert regions.size() == 4
-    checkAccessorFolding(regions[0], regions[1], fooClass.methods[0])
+    assert myFixture.editor.foldingModel.allFoldRegions.size() == 1
   }
 
   public void "test don't inline very long one-line methods"() {
