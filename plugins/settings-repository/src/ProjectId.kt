6b4625e46b68b6e1916163bc8a5531ba0097362c
develar <develar@gmail.com>
2014-09-16 15:29:45 +0200

    convert to Kotlin

--- plugins/settings-repository/src/ProjectId.kt
+++ plugins/settings-repository/src/ProjectId.kt
@@ -1,12 +1,5 @@
 package org.jetbrains.plugins.settingsRepository;
 
-import com.intellij.openapi.components.PersistentStateComponent;
-import com.intellij.openapi.components.State;
-import com.intellij.openapi.components.Storage;
-import com.intellij.openapi.components.StoragePathMacros;
-import com.intellij.util.xmlb.XmlSerializerUtil;
-import org.jetbrains.annotations.Nullable;
-
 @State(name = "IcsProjectId", storages = {@Storage(file = StoragePathMacros.WORKSPACE_FILE)})
 public class ProjectId implements PersistentStateComponent<ProjectId> {
   public String uid;
