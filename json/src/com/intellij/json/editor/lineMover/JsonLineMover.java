c22d5c0e97d2ebddd2141c0a0285616ade5c191b
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2014-09-17 14:57:07 +0400

    Adjacent line commentaries are moved together

--- json/src/com/intellij/json/editor/lineMover/JsonLineMover.java
+++ json/src/com/intellij/json/editor/lineMover/JsonLineMover.java
@@ -1,13 +1,16 @@
 package com.intellij.json.editor.lineMover;
 
 import com.intellij.codeInsight.editorActions.moveUpDown.LineMover;
+import com.intellij.codeInsight.editorActions.moveUpDown.LineRange;
 import com.intellij.json.JsonElementTypes;
 import com.intellij.json.psi.*;
+import com.intellij.lang.ASTNode;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiComment;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
@@ -31,28 +34,65 @@ public class JsonLineMover extends LineMover {
       return false;
     }
 
-    final Pair<PsiElement, PsiElement> movedElementRange = getElementRange(editor, file, info.toMove);
+    Pair<PsiElement, PsiElement> movedElementRange = getElementRange(editor, file, info.toMove);
+    if (!isValidElementRange(movedElementRange)) {
+      return false;
+    }
+
+    // Tweak range to move if it's necessary
+    movedElementRange = expandCommentsInRange(movedElementRange);
+    info.toMove = new LineRange(movedElementRange.getFirst(), movedElementRange.getSecond());
+
+    // Adjust destination range to prevent illegal offsets
+    final int lineCount = editor.getDocument().getLineCount();
+    if (down) {
+      info.toMove2 = new LineRange(info.toMove.endLine, Math.min(info.toMove.endLine + 1, lineCount));
+    }
+    else {
+      info.toMove2 = new LineRange(Math.max(info.toMove.startLine - 1, 0), info.toMove.startLine);
+    }
+
+    if (movedElementRange.getFirst() instanceof PsiComment && movedElementRange.getSecond() instanceof PsiComment) {
+      return true;
+    }
+
+    // Check whether additional comma is needed
     final Pair<PsiElement, PsiElement> destElementRange = getElementRange(editor, file, info.toMove2);
-
-    if (!isValidElementRange(movedElementRange) || !isValidElementRange(destElementRange)) {
-      return false;
-    }
-
-    if (movedElementRange.getFirst().getParent() != destElementRange.getSecond().getParent()) {
-      return false;
-    }
-
-    final PsiElement commonParent = movedElementRange.getFirst().getParent();
-    final PsiElement lowerRightElement = down ? destElementRange.getSecond() : movedElementRange.getSecond();
-    // Destination rightmost element is not closing brace or bracket
-    if (lowerRightElement instanceof JsonElement) {
-      if (commonParent instanceof JsonArray && notFollowedByNextElementOrComma(lowerRightElement, JsonValue.class) ||
-          commonParent instanceof JsonObject && notFollowedByNextElementOrComma(lowerRightElement, JsonProperty.class)) {
-        myShouldAddComma = true;
+    if (isValidElementRange(destElementRange) && movedElementRange.getFirst().getParent() == destElementRange.getSecond().getParent()) {
+      final PsiElement commonParent = movedElementRange.getFirst().getParent();
+      final PsiElement lowerRightElement = down ? destElementRange.getSecond() : movedElementRange.getSecond();
+      // Destination rightmost element is not closing brace or bracket
+      if (lowerRightElement instanceof JsonElement) {
+        if (commonParent instanceof JsonArray && notFollowedByNextElementOrComma(lowerRightElement, JsonValue.class) ||
+            commonParent instanceof JsonObject && notFollowedByNextElementOrComma(lowerRightElement, JsonProperty.class)) {
+          myShouldAddComma = true;
+        }
       }
     }
-
     return true;
+  }
+
+  @NotNull
+  private Pair<PsiElement, PsiElement> expandCommentsInRange(@NotNull Pair<PsiElement, PsiElement> range) {
+    final PsiElement firstElement = expandComment(range.first, true);
+    final PsiElement lastElement = expandComment(range.second, false);
+    return Pair.create(firstElement, lastElement);
+  }
+
+  @NotNull
+  private PsiElement expandComment(@NotNull PsiElement first, boolean before) {
+    ASTNode node = first.getNode();
+    if (node.getElementType() != JsonElementTypes.LINE_COMMENT) {
+      return first;
+    }
+    ASTNode sibling;
+    do {
+      sibling = node;
+      node = before ? node.getTreePrev() : node.getTreeNext();
+    }
+    while (node != null && (node.getElementType() == JsonElementTypes.LINE_COMMENT ||
+                            sibling.getElementType() == JsonElementTypes.LINE_COMMENT));
+    return (before ? sibling.getTreeNext() : sibling.getTreePrev()).getPsi();
   }
 
   @Override
@@ -74,12 +114,12 @@ public class JsonLineMover extends LineMover {
     }
   }
 
-  private static boolean notFollowedByNextElementOrComma(@NotNull PsiElement anchor, @NotNull Class<? extends PsiElement> nextElementType) {
+  private boolean notFollowedByNextElementOrComma(@NotNull PsiElement anchor, @NotNull Class<? extends PsiElement> nextElementType) {
     return PsiTreeUtil.getNextSiblingOfType(anchor, nextElementType) == null &&
            TreeUtil.findSibling(anchor.getNode(), JsonElementTypes.COMMA) == null;
   }
 
-  private static boolean isValidElementRange(@Nullable Pair<PsiElement, PsiElement> elementRange) {
+  private boolean isValidElementRange(@Nullable Pair<PsiElement, PsiElement> elementRange) {
     if (elementRange == null) {
       return false;
     }
