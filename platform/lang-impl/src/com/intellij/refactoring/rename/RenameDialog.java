6eb91496604494b15c63fdd4eafc66bf1b093257
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-09-19 15:50:22 +0400

    reverted

--- platform/lang-impl/src/com/intellij/refactoring/rename/RenameDialog.java
+++ platform/lang-impl/src/com/intellij/refactoring/rename/RenameDialog.java
@@ -326,12 +326,12 @@ public class RenameDialog extends RefactoringDialog {
   @Override
   protected void canRun() throws ConfigurationException {
     if (Comparing.strEqual(getNewName(), myOldName)) throw new ConfigurationException(null);
-    final Function<String, String> inputValidator = RenameInputValidatorRegistry.getInputErrorValidator(myPsiElement);
-    if (inputValidator != null) {
-      throw new ConfigurationException(inputValidator.fun(getNewName()));
-    }
     if (!areButtonsValid()) {
       throw new ConfigurationException("\'" + getNewName() + "\' is not a valid identifier");
+    }
+    final Function<String, String> inputValidator = RenameInputValidatorRegistry.getInputErrorValidator(myPsiElement);
+    if (inputValidator != null) {
+      setErrorText(inputValidator.fun(getNewName()));
     }
   }
 
