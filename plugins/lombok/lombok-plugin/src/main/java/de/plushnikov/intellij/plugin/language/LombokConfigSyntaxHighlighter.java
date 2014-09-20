e2074a406df3a2327d19b1d0b10aee091390144c
Michail Plushnikov <michail@plushnikov.de>
2014-09-20 23:26:40 +0200

    Initial Editor support for lombok.config files: registered basic lombok.config color highlighter
    GitOrigin-RevId: 065da9ce02fdf4f0d850256b971f51e5343ad83e

---
+++ plugins/lombok/lombok-plugin/src/main/java/de/plushnikov/intellij/plugin/language/LombokConfigSyntaxHighlighter.java
@@ -0,0 +1,57 @@
+package de.plushnikov.intellij.plugin.language;
+
+import com.intellij.lexer.FlexAdapter;
+import com.intellij.lexer.Lexer;
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.editor.markup.TextAttributes;
+import com.intellij.openapi.fileTypes.SyntaxHighlighterBase;
+import com.intellij.psi.TokenType;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.ui.JBColor;
+import de.plushnikov.intellij.plugin.language.psi.LombokConfigTypes;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+import java.io.Reader;
+
+public class LombokConfigSyntaxHighlighter extends SyntaxHighlighterBase {
+  public static final TextAttributesKey SEPARATOR = TextAttributesKey.createTextAttributesKey("SIMPLE_SEPARATOR", DefaultLanguageHighlighterColors.OPERATION_SIGN);
+  public static final TextAttributesKey KEY = TextAttributesKey.createTextAttributesKey("SIMPLE_KEY", DefaultLanguageHighlighterColors.KEYWORD);
+  public static final TextAttributesKey VALUE = TextAttributesKey.createTextAttributesKey("SIMPLE_VALUE", DefaultLanguageHighlighterColors.STRING);
+  public static final TextAttributesKey COMMENT = TextAttributesKey.createTextAttributesKey("SIMPLE_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
+
+  static final TextAttributesKey BAD_CHARACTER = TextAttributesKey.createTextAttributesKey("SIMPLE_BAD_CHARACTER",
+      new TextAttributes(JBColor.RED, null, null, null, Font.BOLD));
+
+  private static final TextAttributesKey[] BAD_CHAR_KEYS = new TextAttributesKey[]{BAD_CHARACTER};
+  private static final TextAttributesKey[] SEPARATOR_KEYS = new TextAttributesKey[]{SEPARATOR};
+  private static final TextAttributesKey[] KEY_KEYS = new TextAttributesKey[]{KEY};
+  private static final TextAttributesKey[] VALUE_KEYS = new TextAttributesKey[]{VALUE};
+  private static final TextAttributesKey[] COMMENT_KEYS = new TextAttributesKey[]{COMMENT};
+  private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
+
+  @NotNull
+  @Override
+  public Lexer getHighlightingLexer() {
+    return new FlexAdapter(new LombokConfigLexer((Reader) null));
+  }
+
+  @NotNull
+  @Override
+  public TextAttributesKey[] getTokenHighlights(IElementType tokenType) {
+    if (tokenType.equals(LombokConfigTypes.SEPARATOR)) {
+      return SEPARATOR_KEYS;
+    } else if (tokenType.equals(LombokConfigTypes.KEY)) {
+      return KEY_KEYS;
+    } else if (tokenType.equals(LombokConfigTypes.VALUE)) {
+      return VALUE_KEYS;
+    } else if (tokenType.equals(LombokConfigTypes.COMMENT)) {
+      return COMMENT_KEYS;
+    } else if (tokenType.equals(TokenType.BAD_CHARACTER)) {
+      return BAD_CHAR_KEYS;
+    } else {
+      return EMPTY_KEYS;
+    }
+  }
+}
