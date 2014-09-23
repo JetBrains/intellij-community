47e56377811e366b2e6ed78640d3d33fa6ae1686
nik <Nikolay.Chashnikov@jetbrains.com>
2014-09-23 14:28:13 +0400

    Google App Engine deployment: don't show error if user pressed 'cancel' in 'Master Password' dialog

--- plugins/google-app-engine/source/com/intellij/appengine/facet/AppEngineAccountDialog.java
+++ plugins/google-app-engine/source/com/intellij/appengine/facet/AppEngineAccountDialog.java
@@ -64,7 +64,7 @@ public class AppEngineAccountDialog {
       PasswordSafe.getInstance().storePassword(project, AppEngineAccountDialog.class, getPasswordKey(email), password);
     }
     catch (PasswordSafeException e) {
-      LOG.error(e);
+      LOG.info(e);
     }
   }
 
@@ -82,7 +82,7 @@ public class AppEngineAccountDialog {
       return PasswordSafe.getInstance().getPassword(project, AppEngineAccountDialog.class, getPasswordKey(email));
     }
     catch (PasswordSafeException e) {
-      LOG.error(e);
+      LOG.info(e);
       return null;
     }
   }
