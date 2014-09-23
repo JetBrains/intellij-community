c933b9e59ecd2a70c778302fcab069fb8d430588
Konstantin Bulenkov <kb@jetbrains.com>
2014-09-23 15:12:04 +0200

    check that an OptionTopHitProvider is enabled while searching in SE

--- platform/lang-impl/src/com/intellij/ide/actions/SearchEverywhereAction.java
+++ platform/lang-impl/src/com/intellij/ide/actions/SearchEverywhereAction.java
@@ -1833,6 +1833,9 @@ public class SearchEverywhereAction extends AnAction implements CustomComponentA
 
         for (SearchTopHitProvider provider : SearchTopHitProvider.EP_NAME.getExtensions()) {
           check();
+          if (provider instanceof OptionsTopHitProvider && !((OptionsTopHitProvider)provider).isEnabled(project)) {
+            continue;
+          }
           provider.consumeTopHits(pattern, consumer, project);
         }
       }
