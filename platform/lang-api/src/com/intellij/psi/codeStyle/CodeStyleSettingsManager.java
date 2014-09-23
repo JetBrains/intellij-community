ff6ebd1f7fc86997329430c34f60b6cf1924cf94
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2014-09-23 12:26:20 +0400

    File indent options provider API: a comment to CodeStyleSettingsManager.updateIndentOptions() method [CR-IC-6333]

--- platform/lang-api/src/com/intellij/psi/codeStyle/CodeStyleSettingsManager.java
+++ platform/lang-api/src/com/intellij/psi/codeStyle/CodeStyleSettingsManager.java
@@ -126,6 +126,13 @@ public class CodeStyleSettingsManager implements PersistentStateComponent<Elemen
     return myIsLoaded;
   }
 
+  /**
+   * Updates document's indent options from indent options providers.
+   * <p><b>Note:</b> Calling this method directly when there is an editor associated with the document may cause the editor work
+   * incorrectly. To keep consistency with the editor call <code>EditorEx.reinitSettings()</code> instead.
+   * @param project  The project of the document.
+   * @param document The document to update indent options for.
+   */
   public static void updateDocumentIndentOptions(@NotNull Project project, @NotNull Document document) {
     if (!project.isDisposed()) {
       PsiDocumentManager documentManager = PsiDocumentManager.getInstance(project);
