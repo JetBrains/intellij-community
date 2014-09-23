f6991e77fafbf04d6c4e437da95cbabee333b266
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2014-09-24 01:50:15 +0400

    svn: Implemented ability to check and trust server certificate (for https protocol) without utilizing SVNKit authentication lifecycle - request is issued to the server and special TrustManager performs necessary certificate checks

--- plugins/svn4idea/src/org/jetbrains/idea/svn/commandLine/CertificateCallbackCase.java
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/commandLine/CertificateCallbackCase.java
@@ -15,6 +15,7 @@
  */
 package org.jetbrains.idea.svn.commandLine;
 
+import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.util.text.StringUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.idea.svn.auth.AuthenticationService;
@@ -29,6 +30,7 @@ public class CertificateCallbackCase extends AuthCallbackCase {
   private static final String CERTIFICATE_ERROR = "Error validating server certificate for";
   private static final String UNTRUSTED_SERVER_CERTIFICATE = "Server SSL certificate untrusted";
   private static final String CERTIFICATE_VERIFICATION_FAILED = "certificate verification failed";
+  private static final String CERTIFICATE_VERIFICATION_FAILED_ISSUER_NOT_TRUSTED = "certificate verification failed: issuer is not trusted";
 
   private boolean accepted;
 
@@ -38,11 +40,22 @@ public class CertificateCallbackCase extends AuthCallbackCase {
 
   @Override
   public boolean canHandle(String error) {
+    boolean useSvnKit = Registry.is("svn.use.svnkit.for.https.server.certificate.check");
+
     return error.startsWith(CERTIFICATE_ERROR) ||
            // https one-way protocol untrusted server certificate
            error.contains(UNTRUSTED_SERVER_CERTIFICATE) ||
-           // for instance, certificate issued for a different hostname, issuer is not trusted - for both 1.7 and 1.8
-           error.contains(CERTIFICATE_VERIFICATION_FAILED);
+           // any certificate verification failure - for instance, "certificate issued for a different hostname" and/or
+           // "issuer is not trusted" - for both 1.7 and 1.8.
+           // SVNKit-based implementation persists credentials (by utilizing SVNKit api) emulating situation as if credentials were cached
+           // by Subversion. That is why we could process both untrusted and invalid certificate errors.
+           useSvnKit && isCertificateVerificationFailed(error) ||
+           // valid but untrusted certificates - "issuer is not trusted" error - for both 1.7 and 1.8.
+           // Implementation not based on SVNKit does not persist credentials to emulate situation as if credentials were cached by
+           // Subversion. And in "--non-interactive" mode we could only make Subversion accept untrusted, but not invalid certificate.
+           // So we explicitly check that verification failure is only "issuer is not trusted". If certificate has some other failures,
+           // command will end with error.
+           !useSvnKit && isValidButUntrustedCertificate(error);
   }
 
   @Override
@@ -84,6 +97,14 @@ public class CertificateCallbackCase extends AuthCallbackCase {
       // force --non-interactive as it is required by --trust-server-cert, but --non-interactive is not default mode for 1.7 or earlier
       command.put("--non-interactive");
     }
+  }
+
+  public static boolean isValidButUntrustedCertificate(@NotNull String error) {
+    return error.contains(CERTIFICATE_VERIFICATION_FAILED_ISSUER_NOT_TRUSTED);
+  }
+
+  public static boolean isCertificateVerificationFailed(@NotNull String error) {
+    return error.contains(CERTIFICATE_VERIFICATION_FAILED);
   }
 
   private SVNURL getServerUrl(String realm) {
