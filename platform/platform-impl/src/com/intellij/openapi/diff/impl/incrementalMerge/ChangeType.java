9816adaa13aec740921d5b38efce1aa7de5be34e
Aleksey Pivovarov <AMPivovarov@gmail.com>
2014-09-25 20:02:14 +0400

    diff: revert 6a5ba163 4bdf2fb4
    This is not a very good approach.

--- platform/platform-impl/src/com/intellij/openapi/diff/impl/incrementalMerge/ChangeType.java
+++ platform/platform-impl/src/com/intellij/openapi/diff/impl/incrementalMerge/ChangeType.java
@@ -136,7 +136,6 @@ public class ChangeType {
         Graphics2D g2 = (Graphics2D)g;
         Color color = myDiffType.getPolygonColor(markup.getEditor());
         if (color != null) {
-          x2 = g.getClipBounds().width;
           if (type.isApplied()) {
             UIUtil.drawBoldDottedLine(g2, x1, x2, y, null, color, false);
           }
