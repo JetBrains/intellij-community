95368aabfff4cc98851f87dc8918cb9b087c9305
develar <develar@gmail.com>
2014-09-26 08:57:48 +0200

    build commit message using status

--- plugins/settings-repository/src/git/commit.kt
+++ plugins/settings-repository/src/git/commit.kt
@@ -9,27 +9,28 @@ import org.jetbrains.settingsRepository.LOG
 import org.jetbrains.settingsRepository.PROJECTS_DIR_NAME
 import org.jetbrains.jgit.dirCache.edit
 import org.jetbrains.jgit.dirCache.AddFile
+import com.intellij.util.PathUtilRt
 
 fun commit(manager: GitRepositoryManager, indicator: ProgressIndicator) {
   indicator.checkCanceled()
 
-  val index = manager.repository.computeIndexDiff()
-  val changed = index.diff(JGitProgressMonitor(indicator), ProgressMonitor.UNKNOWN, ProgressMonitor.UNKNOWN, "Commit")
+  val diff = manager.repository.computeIndexDiff()
+  val changed = diff.diff(JGitProgressMonitor(indicator), ProgressMonitor.UNKNOWN, ProgressMonitor.UNKNOWN, "Commit")
 
   if (LOG.isDebugEnabled()) {
     LOG.debug("Commit")
-    LOG.debug(indexDiffToString(index))
+    LOG.debug(indexDiffToString(diff))
   }
 
   // don't worry about untracked/modified only in the FS files
-  if (!changed || (index.getAdded().isEmpty() && index.getChanged().isEmpty() && index.getRemoved().isEmpty())) {
-    if (index.getModified().isEmpty()) {
+  if (!changed || (diff.getAdded().isEmpty() && diff.getChanged().isEmpty() && diff.getRemoved().isEmpty())) {
+    if (diff.getModified().isEmpty()) {
       LOG.debug("Skip scheduled commit, nothing to commit")
       return
     }
 
     var edits: MutableList<PathEdit>? = null
-    for (path in index.getModified()) {
+    for (path in diff.getModified()) {
       if (!path.startsWith(PROJECTS_DIR_NAME)) {
         if (edits == null) {
           edits = SmartList()
@@ -44,7 +45,13 @@ fun commit(manager: GitRepositoryManager, indicator: ProgressIndicator) {
 
   indicator.checkCanceled()
 
-  manager.commit("")
+  val builder = StringBuilder()
+  // we use Github (edit via web UI) terms here
+  addCompactList("Update", diff.getChanged(), builder)
+  addCompactList("Create", diff.getAdded(), builder)
+  addCompactList("Delete", diff.getRemoved(), builder)
+
+  manager.commit(builder.toString())
 }
 
 private fun indexDiffToString(diff: IndexDiff): String {
@@ -52,7 +59,7 @@ private fun indexDiffToString(diff: IndexDiff): String {
   builder.append("To commit:")
   addList("Added", diff.getAdded(), builder)
   addList("Changed", diff.getChanged(), builder)
-  addList("Removed", diff.getRemoved(), builder)
+  addList("Deleted", diff.getRemoved(), builder)
   addList("Modified on disk relative to the index", diff.getModified(), builder)
   addList("Untracked files", diff.getUntracked(), builder)
   addList("Untracked folders", diff.getUntrackedFolders(), builder)
@@ -60,12 +67,26 @@ private fun indexDiffToString(diff: IndexDiff): String {
   return builder.toString()
 }
 
-private fun addList(name: String, list: Collection<String>, builder: StringBuilder) {
+private fun addCompactList(name: String, list: Collection<String>, builder: StringBuilder) {
+  addList(name, list, builder, true)
+}
+
+private fun addList(name: String, list: Collection<String>, builder: StringBuilder, compact: Boolean = false) {
   if (list.isEmpty()) {
     return
   }
 
-  builder.append('\t').append(name).append(": ")
+  if (compact) {
+    if (builder.length() != 0) {
+      builder.append(' ')
+    }
+    builder.append(name)
+  }
+  else {
+    builder.append('\t').append(name).append(':')
+  }
+  builder.append(' ')
+
   var isNotFirst = false
   for (path in list) {
     if (isNotFirst) {
@@ -74,6 +95,6 @@ private fun addList(name: String, list: Collection<String>, builder: StringBuild
     else {
       isNotFirst = true
     }
-    builder.append(path)
+    builder.append(if (compact) PathUtilRt.getFileName(path) else path)
   }
 }
