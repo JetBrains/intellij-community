be518189936f9aa586460a9f031ea2b85fd7cf5d
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2014-09-26 17:23:48 +0400

    Switch on the new Settings view for Internal mode.

--- platform/platform-api/src/com/intellij/openapi/ui/MasterDetailsComponent.java
+++ platform/platform-api/src/com/intellij/openapi/ui/MasterDetailsComponent.java
@@ -17,6 +17,7 @@ package com.intellij.openapi.ui;
 
 import com.intellij.CommonBundle;
 import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.options.ConfigurationException;
@@ -140,7 +141,7 @@ public abstract class MasterDetailsComponent implements Configurable, DetailsCom
   }
 
   private boolean isNewProjectSettings() {
-    if (Registry.is("ide.new.settings.view")) {
+    if (ApplicationManager.getApplication().isInternal() && Registry.is("ide.new.settings.view")) {
       return true;
     }
     if (!Registry.is("ide.new.project.settings")) {
