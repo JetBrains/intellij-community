075fb378d2ff3f78381c0f25f699fe96a5ef9f2b
Bas Leijdekkers <basleijdekkers@gmail.com>
2014-09-30 16:02:49 +0200

    make IG test light

--- plugins/InspectionGadgets/testsrc/com/siyeh/ig/style/UnclearBinaryExpressionInspectionTest.java
+++ plugins/InspectionGadgets/testsrc/com/siyeh/ig/style/UnclearBinaryExpressionInspectionTest.java
@@ -1,11 +1,16 @@
 package com.siyeh.ig.style;
 
-import com.siyeh.ig.IGInspectionTestCase;
+import com.intellij.codeInspection.InspectionProfileEntry;
+import com.siyeh.ig.LightInspectionTestCase;
+import org.jetbrains.annotations.Nullable;
 
-public class UnclearBinaryExpressionInspectionTest extends IGInspectionTestCase {
+public class UnclearBinaryExpressionInspectionTest extends LightInspectionTestCase {
 
-  public void test() throws Exception {
-    doTest("com/siyeh/igtest/style/unclear_binary_expression",
-           new UnclearBinaryExpressionInspection());
+  public void testUnclearBinaryExpression() { doTest(); }
+
+  @Nullable
+  @Override
+  protected InspectionProfileEntry getInspection() {
+    return new UnclearBinaryExpressionInspection();
   }
 }
