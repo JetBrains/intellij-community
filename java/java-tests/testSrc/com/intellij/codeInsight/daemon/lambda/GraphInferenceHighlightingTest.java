e6483ac10c5636af57259dfc1ff8931c90a6c851
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-09-30 18:58:00 +0200

    testdata for IDEA-130549

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/GraphInferenceHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/GraphInferenceHighlightingTest.java
@@ -243,6 +243,10 @@ public class GraphInferenceHighlightingTest extends LightDaemonAnalyzerTestCase 
     doTest();
   }
 
+  public void testIDEA130549() throws Exception {
+    doTest();
+  }
+
   private void doTest() throws Exception {
     doTest(false);
   }
