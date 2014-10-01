fab6863b9ad3d9cfdad0590f4f83d600ce202755
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2014-10-01 17:16:06 +0400

    use JFXPanel to render html

--- src/org/jetbrains/plugins/ipnb/editor/panels/code/IpnbHtmlPanel.java
+++ src/org/jetbrains/plugins/ipnb/editor/panels/code/IpnbHtmlPanel.java
@@ -1,8 +1,11 @@
 package org.jetbrains.plugins.ipnb.editor.panels.code;
 
-import com.intellij.ui.components.JBLabel;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.web.WebView;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.plugins.ipnb.editor.IpnbEditorUtil;
 import org.jetbrains.plugins.ipnb.format.cells.output.IpnbHtmlOutputCell;
 
 import javax.swing.*;
@@ -14,15 +17,28 @@ public class IpnbHtmlPanel extends IpnbCodeOutputPanel<IpnbHtmlOutputCell> {
 
   @Override
   protected JComponent createViewPanel() {
+    final JFXPanel javafxPanel = new JFXPanel();
     final StringBuilder text = new StringBuilder("<html>");
     for (String html : myCell.getHtmls()) {
       html = html.replace("\"", "'");
       text.append(html);
     }
     text.append("</html>");
-    final JBLabel label = new JBLabel(text.toString());
-    label.setBackground(IpnbEditorUtil.getBackground());
-    label.setOpaque(true);
-    return label;
+    Platform.runLater(new Runnable() {
+      @Override
+      public void run() {
+
+        BorderPane borderPane = new BorderPane();
+        WebView webComponent = new WebView();
+
+        webComponent.getEngine().loadContent(text.toString());
+
+        borderPane.setCenter(webComponent);
+        Scene scene = new Scene(borderPane, 450, 450);
+        javafxPanel.setScene(scene);
+      }
+    });
+
+    return javafxPanel;
   }
 }
