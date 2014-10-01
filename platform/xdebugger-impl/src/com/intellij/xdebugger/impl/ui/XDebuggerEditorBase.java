bb192fc91682cad36902a54cab658bfa21992081
Egor.Ushakov <egor.ushakov@jetbrains.com>
2014-10-01 15:20:34 +0400

    IDEA-122773 Evaluate expression -> code fragment mode -> history empty

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/ui/XDebuggerEditorBase.java
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/ui/XDebuggerEditorBase.java
@@ -60,7 +60,7 @@ public abstract class XDebuggerEditorBase {
   @NotNull private final EvaluationMode myMode;
   @Nullable private final String myHistoryId;
   @Nullable private XSourcePosition mySourcePosition;
-  private int myHistoryIndex;
+  private int myHistoryIndex = -1;
 
   private final JLabel myChooseFactory = new JLabel();
   private WeakReference<ListPopup> myPopup;
@@ -209,7 +209,7 @@ public abstract class XDebuggerEditorBase {
   private void saveTextInHistory(final XExpression text) {
     if (myHistoryId != null) {
       boolean update = XDebuggerHistoryManager.getInstance(myProject).addRecentExpression(myHistoryId, text);
-      myHistoryIndex = 0;
+      myHistoryIndex = -1; //meaning not from the history list
       if (update) {
         onHistoryChanged();
       }
