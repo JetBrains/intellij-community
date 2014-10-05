90c809ee75db5a9f2b1bea4079eacf275759d895
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-10-05 08:16:55 +0200

    js debug auto expressions — support sub references (var.push -> don't allow function push -> try to use var -> show var)
    https://github.com/tastejs/todomvc/tree/master/architecture-examples/angularjs-perf
    
    breakpoint todoCtrl.js

--- platform/script-debugger/debugger-ui/src/org/jetbrains/debugger/VariableView.java
+++ platform/script-debugger/debugger-ui/src/org/jetbrains/debugger/VariableView.java
@@ -398,7 +398,7 @@ public final class VariableView extends XNamedValue implements VariableContext {
     });
   }
 
-  private void computeArrayRanges(@NotNull List<? extends Variable> properties, @NotNull XCompositeNode node) {
+  private void computeArrayRanges(@NotNull List<Variable> properties, @NotNull XCompositeNode node) {
     final List<Variable> variables = Variables.filterAndSort(properties, this, false);
     int count = variables.size();
     int bucketSize = XCompositeNode.MAX_CHILDREN_TO_SHOW;
