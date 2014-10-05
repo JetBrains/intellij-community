b74e1cfb86e4fd25f011e7daad64ec1baa07c0cd
Egor Ushakov <egor.ushakov@jetbrains.com>
2014-10-05 13:25:55 +0400

    IDEA-123114 Debugger: render awt.Image and swing.Icon in Variables/Watches/Evaluate views

---
+++ java/java-runtime/src/com/intellij/rt/debugger/ImageSerializer.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2000-2014 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.rt.debugger;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by Egor on 04.10.2014.
+ */
+public class ImageSerializer {
+  public static byte[] imageToBytes(Image image) throws IOException {
+    BufferedImage bi = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_RGB);
+    Graphics2D g = bi.createGraphics();
+    g.drawImage(image, 0, 0, null);
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    ImageIO.write(bi, "png", baos);
+    return baos.toByteArray();
+  }
+
+  public static byte[] iconToBytes(Icon icon) throws IOException {
+    return imageToBytes(toImage(icon));
+  }
+
+  // copied from IconUtils
+  private static Image toImage(Icon icon) {
+    if (icon instanceof ImageIcon) {
+      return ((ImageIcon)icon).getImage();
+    }
+    else {
+      final int w = icon.getIconWidth();
+      final int h = icon.getIconHeight();
+      final BufferedImage image = GraphicsEnvironment.getLocalGraphicsEnvironment()
+        .getDefaultScreenDevice().getDefaultConfiguration().createCompatibleImage(w, h, Transparency.TRANSLUCENT);
+      final Graphics2D g = image.createGraphics();
+      icon.paintIcon(null, g, 0, 0);
+      g.dispose();
+      return image;
+    }
+  }
+
+}
