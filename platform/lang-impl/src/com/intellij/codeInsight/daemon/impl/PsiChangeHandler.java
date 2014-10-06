4e2badc13c1c4178524c7fbdda8e967e25162175
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-10-06 08:02:33 +0200

    fix Already disposed: Project (Disposed)

--- platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/PsiChangeHandler.java
+++ platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/PsiChangeHandler.java
@@ -100,7 +100,7 @@ public class PsiChangeHandler extends PsiTreeChangeAdapter implements Disposable
   }
 
   private void updateChangesForDocument(@NotNull final Document document) {
-    if (DaemonListeners.isUnderIgnoredAction(null)) return;
+    if (DaemonListeners.isUnderIgnoredAction(null) || myProject.isDisposed()) return;
     List<Pair<PsiElement, Boolean>> toUpdate = changedElements.get(document);
     if (toUpdate == null) {
       // The document has been changed, but psi hasn't
