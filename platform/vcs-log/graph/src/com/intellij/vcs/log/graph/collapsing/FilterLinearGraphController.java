864dbac052209cb494002cd2489cc3e48834f920
Stanislav Erokhin <erokhins239@gmail.com>
2014-10-08 21:23:05 +0400

    [log] Draft of FilterLinearGraphController.

---
+++ platform/vcs-log/graph/src/com/intellij/vcs/log/graph/collapsing/FilterLinearGraphController.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2000-2014 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.vcs.log.graph.collapsing;
+
+import com.intellij.vcs.log.graph.api.LinearGraph;
+import com.intellij.vcs.log.graph.api.permanent.PermanentGraphInfo;
+import com.intellij.vcs.log.graph.api.printer.PrintElementWithGraphElement;
+import com.intellij.vcs.log.graph.impl.facade.CascadeLinearGraphController;
+import com.intellij.vcs.log.graph.impl.facade.GraphChanges;
+import com.intellij.vcs.log.graph.utils.UnsignedBitSet;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class FilterLinearGraphController extends CascadeLinearGraphController {
+  @NotNull
+  private CollapsedGraph myCollapsedGraph;
+  @NotNull
+  private final UnsignedBitSet myFilterNodeId;
+  private final boolean myAllowCollapseFilterNodes;
+
+  protected FilterLinearGraphController(@NotNull CascadeLinearGraphController delegateLinearGraphController,
+                                        @NotNull PermanentGraphInfo permanentGraphInfo,
+                                        @NotNull UnsignedBitSet filterNodeId,
+                                        boolean allowCollapseFilterNodes) {
+    super(delegateLinearGraphController, permanentGraphInfo);
+    myFilterNodeId = filterNodeId;
+    myAllowCollapseFilterNodes = allowCollapseFilterNodes;
+    myCollapsedGraph = CollapsedGraph.newInstance(getDelegateLinearGraphController().getCompiledGraph(), filterNodeId);
+  }
+
+  @NotNull
+  @Override
+  protected LinearGraphAnswer performDelegateUpdate(@NotNull LinearGraphAnswer delegateAnswer) {
+    if (delegateAnswer.getGraphChanges() != null) {
+      myCollapsedGraph = CollapsedGraph.updateInstance(myCollapsedGraph, getDelegateLinearGraphController().getCompiledGraph());
+      for (GraphChanges.Node<Integer> changedNode : delegateAnswer.getGraphChanges().getChangedNodes()) {
+        if (!changedNode.removed()) {
+          int nodeId = changedNode.getNodeId();
+          myCollapsedGraph.setNodeVisibility(nodeId, true);
+        }
+      }
+    }
+    return delegateAnswer;
+  }
+
+  @Nullable
+  @Override
+  protected LinearGraphAnswer performAction(@NotNull LinearGraphAction action) {
+    return CollapsedActionManager.performAction(this, action);
+  }
+
+  @NotNull
+  @Override
+  public LinearGraph getCompiledGraph() {
+    return myCollapsedGraph.getCompiledGraph();
+  }
+
+  @Override
+  protected boolean elementIsSelected(@NotNull PrintElementWithGraphElement printElement) {
+    return false;
+  }
+
+  @NotNull
+  protected CollapsedGraph getCollapsedGraph() {
+    return myCollapsedGraph;
+  }
+
+  @NotNull
+  protected UnsignedBitSet getFilterNodeId() {
+    return myFilterNodeId;
+  }
+
+  protected boolean isAllowCollapseFilterNodes() {
+    return myAllowCollapseFilterNodes;
+  }
+}
