e42797a6770f4d45bbd8b709d225ca9960ce515c
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2014-10-09 16:16:25 +0200

    IDEA-131036 (early catch for null values)

--- platform/platform-api/src/com/intellij/execution/configurations/GeneralCommandLine.java
+++ platform/platform-api/src/com/intellij/execution/configurations/GeneralCommandLine.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -370,6 +370,17 @@ public class GeneralCommandLine implements UserDataHolder {
   }
 
   private static class MyTHashMap extends THashMap<String, String> {
+    @Override
+    public String put(String key, String value) {
+      if (key == null || value == null) {
+        LOG.error(new Exception("Nulls are not allowed"));
+        return null;
+      }
+      else {
+        return super.put(key, value);
+      }
+    }
+
     @Override
     public void putAll(Map<? extends String, ? extends String> map) {
       if (map != null) {
