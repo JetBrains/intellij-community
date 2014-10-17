92697add955a27fddaf344c4a5011dfe19a5dff4
Bas Leijdekkers <basleijdekkers@gmail.com>
2014-10-17 12:33:11 +0200

    EA-61611 (IAE: StringUtil.unpluralize)

--- plugins/IntentionPowerPak/testSrc/com/siyeh/ipp/forloop/ReplaceForEachLoopWithIndexedForLoopIntentionTest.java
+++ plugins/IntentionPowerPak/testSrc/com/siyeh/ipp/forloop/ReplaceForEachLoopWithIndexedForLoopIntentionTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,6 +23,7 @@ public class ReplaceForEachLoopWithIndexedForLoopIntentionTest extends IPPTestCa
   public void testNormalForeachLoop() { doTest(); }
   public void testThisExpr() { doTest(); }
   public void testNewArray() { doTest(); }
+  public void testNoNPE() { doTest(); }
 
   @Override
   protected String getIntentionName() {
