c72295cc2738659910d5233cde58a2fe26fb111c
Liana Bakradze <liana.bakradze@jetbrains.com>
2014-10-21 12:06:53 +0400

    EDU-226 Rename Playground to Sandbox

--- python/edu/learn-python/gen/icons/StudyIcons.java
+++ python/edu/learn-python/gen/icons/StudyIcons.java
@@ -16,9 +16,9 @@ public class StudyIcons {
   public static final Icon EducationalProjectType = load("/icons/com/jetbrains/python/edu/EducationalProjectType.png"); // 32x32
   public static final Icon Lesson = load("/icons/com/jetbrains/python/edu/Lesson.png"); // 16x16
   public static final Icon LessonCompl = load("/icons/com/jetbrains/python/edu/LessonCompl.png"); // 16x16
-  public static final Icon Playground = load("/icons/com/jetbrains/python/edu/Playground.png"); // 16x16
   public static final Icon Prev = load("/icons/com/jetbrains/python/edu/prev.png"); // 16x16
   public static final Icon Resolve = load("/icons/com/jetbrains/python/edu/resolve.png"); // 16x16
+  public static final Icon Sandbox = load("/icons/com/jetbrains/python/edu/Sandbox.png"); // 16x16
   public static final Icon ShowHint = load("/icons/com/jetbrains/python/edu/showHint.png"); // 24x24
   public static final Icon Task = load("/icons/com/jetbrains/python/edu/Task.png"); // 16x16
   public static final Icon TaskCompl = load("/icons/com/jetbrains/python/edu/TaskCompl.png"); // 16x16
