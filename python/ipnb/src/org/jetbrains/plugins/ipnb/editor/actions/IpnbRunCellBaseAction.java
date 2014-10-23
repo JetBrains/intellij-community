5d1a55df95ad25c7c22580585cf6508ef7f3c0ca
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2014-10-23 14:35:32 +0400

    added run in-place action with shortcut

---
+++ python/ipnb/src/org/jetbrains/plugins/ipnb/editor/actions/IpnbRunCellBaseAction.java
@@ -0,0 +1,28 @@
+package org.jetbrains.plugins.ipnb.editor.actions;
+
+import com.intellij.icons.AllIcons;
+import com.intellij.openapi.actionSystem.AnAction;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.ipnb.editor.panels.IpnbEditablePanel;
+import org.jetbrains.plugins.ipnb.editor.panels.IpnbFilePanel;
+
+public abstract class IpnbRunCellBaseAction extends AnAction {
+  public IpnbRunCellBaseAction() {
+    super(AllIcons.General.Run);
+  }
+
+  public void runCell(@NotNull final IpnbFilePanel ipnbFilePanel, boolean selectNext) {
+    final IpnbEditablePanel cell = ipnbFilePanel.getSelectedCell();
+    cell.runCell();
+    if (selectNext) {
+      final int index = ipnbFilePanel.getSelectedIndex();
+      if (ipnbFilePanel.getIpnbPanels().size()-1 == index) {
+        ipnbFilePanel.createAndAddCell();
+      }
+      ipnbFilePanel.selectNext(cell);
+    }
+    ipnbFilePanel.revalidate();
+    ipnbFilePanel.repaint();
+    ipnbFilePanel.requestFocus();
+  }
+}
