fa1542efb768cc5eaa588f68c4e0cdbdcb6e0b72
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-10-23 11:23:14 +0200

    simplify protocol reader generator

--- platform/script-debugger/protocol/protocol-reader/src/org/jetbrains/protocolReader/ReaderGenerator.java
+++ platform/script-debugger/protocol/protocol-reader/src/org/jetbrains/protocolReader/ReaderGenerator.java
@@ -4,55 +4,43 @@
 
 package org.jetbrains.protocolReader;
 
+import gnu.trove.THashMap;
+import org.jetbrains.annotations.Nullable;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileSystems;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ReaderGenerator {
   protected static void mainImpl(String[] args, GenerateConfiguration configuration) throws IOException {
     FileUpdater fileUpdater = new FileUpdater(FileSystems.getDefault().getPath(parseArgs(args).outputDirectory(),
-                                                                               configuration.getPackageName().replace('.',
-                                                                                                                      File.separatorChar),
-                                                                               configuration.getClassName() + ".java"));
+                                                                               configuration.packageName.replace('.',
+                                                                                                                 File.separatorChar),
+                                                                               configuration.className + ".java"));
     generate(configuration, fileUpdater.builder);
     fileUpdater.update();
   }
 
-  protected static class GenerateConfiguration {
+  protected static class GenerateConfiguration<ROOT> {
     private final String packageName;
     private final String className;
-    private final DynamicReader<?> parser;
-    private final Collection<GeneratedCodeMap> basePackagesMap;
+    final Collection<Map<Class<?>, String>> basePackagesMap;
 
-    public GenerateConfiguration(String packageName, String className, DynamicReader<?> parser) {
-      this(packageName, className, parser, Collections.<GeneratedCodeMap>emptyList());
+    final LinkedHashMap<Class<?>, TypeHandler<?>> typeToTypeHandler;
+    final ReaderRoot<ROOT> root;
+
+    public GenerateConfiguration(String packageName, String className, Class<ROOT> readerRootClass, Class<?>[] protocolInterfaces) {
+      this(packageName, className, readerRootClass, protocolInterfaces, null);
     }
 
-    public GenerateConfiguration(String packageName, String className, DynamicReader<?> parser, Collection<GeneratedCodeMap> basePackagesMap) {
+    public GenerateConfiguration(String packageName, String className, Class<ROOT> readerRootClass, Class<?>[] protocolInterfaces, @Nullable Map<Class<?>, String> basePackagesMap) {
       this.packageName = packageName;
       this.className = className;
-      this.parser = parser;
-      this.basePackagesMap = basePackagesMap;
-    }
+      this.basePackagesMap = basePackagesMap == null ? Collections.emptyList() : Collections.singletonList(basePackagesMap);
 
-    public String getPackageName() {
-      return packageName;
-    }
-
-    public String getClassName() {
-      return className;
-    }
-
-    public DynamicReader<?> getParser() {
-      return parser;
-    }
-
-    public Collection<GeneratedCodeMap> getBasePackagesMap() {
-      return basePackagesMap;
+      typeToTypeHandler = new InterfaceReader(protocolInterfaces).go();
+      root = new ReaderRoot<>(readerRootClass, typeToTypeHandler);
     }
   }
 
@@ -122,13 +110,57 @@ public class ReaderGenerator {
     }
   }
 
-  protected static GeneratedCodeMap buildParserMap(GenerateConfiguration configuration) {
-    return generate(configuration, new StringBuilder());
+  protected static Map<Class<?>, String> buildParserMap(GenerateConfiguration<?> configuration) {
+    FileScope fileScope = generate(configuration, new StringBuilder());
+
+    Map<Class<?>, String> typeToImplClassName = new THashMap<>();
+    for (TypeHandler<?> typeHandler : configuration.typeToTypeHandler.values()) {
+      typeToImplClassName.put(typeHandler.getTypeClass(), configuration.packageName + "." + configuration.className + "." + fileScope.getTypeImplShortName(typeHandler));
+    }
+    return typeToImplClassName;
   }
 
-  private static GeneratedCodeMap generate(GenerateConfiguration configuration, StringBuilder stringBuilder) {
-    return configuration.getParser().generateReader(stringBuilder,
-                                                    configuration.getPackageName(), configuration.getClassName(),
-                                                    configuration.getBasePackagesMap());
+  private static FileScope generate(GenerateConfiguration<?> configuration, StringBuilder stringBuilder) {
+    GlobalScope globalScope = new GlobalScope(configuration.typeToTypeHandler.values(), configuration.basePackagesMap);
+    FileScope fileScope = globalScope.newFileScope(stringBuilder);
+    TextOutput out = fileScope.getOutput();
+    out.append("// Generated source");
+    out.newLine().append("package ").append(configuration.packageName).append(';');
+    out.newLine().newLine().append("import org.jetbrains.jsonProtocol.*;");
+    out.newLine().newLine().append("import static org.jetbrains.jsonProtocol.JsonReaders.*;");
+    out.newLine().newLine().append("public final class ").append(configuration.className).space();
+    out.append(configuration.root.getType().isInterface() ? "implements" : "extends").space().append(configuration.root.getType().getCanonicalName()).openBlock(false);
+
+    ClassScope rootClassScope = fileScope.newClassScope();
+    configuration.root.writeStaticMethodJava(rootClassScope);
+
+    for (TypeHandler<?> typeHandler : configuration.typeToTypeHandler.values()) {
+      out.newLine();
+      typeHandler.writeStaticClassJava(rootClassScope);
+      out.newLine();
+    }
+
+    boolean isFirst = true;
+    for (TypeHandler<?> typeHandler : globalScope.getTypeFactories()) {
+      if (isFirst) {
+        isFirst = false;
+      }
+      else {
+        out.newLine();
+      }
+
+      String originName = typeHandler.getTypeClass().getCanonicalName();
+      out.newLine().append("private static final class ").append(globalScope.getTypeImplShortName(typeHandler)).append(Util.TYPE_FACTORY_NAME_POSTFIX).append(" extends ObjectFactory<");
+      out.append(originName).append('>').openBlock();
+      out.append("@Override").newLine().append("public ").append(originName).append(" read(").append(Util.JSON_READER_PARAMETER_DEF);
+      out.append(')').openBlock();
+      out.append("return ");
+      typeHandler.writeInstantiateCode(rootClassScope, out);
+      out.append('(').append(Util.READER_NAME).append(", null);").closeBlock();
+      out.closeBlock();
+    }
+
+    out.closeBlock();
+    return fileScope;
   }
 }
--- platform/script-debugger/protocol/schema-reader-generator/src/org/jetbrains/jsonProtocol/SchemaReaderGenerator.java
+++ platform/script-debugger/protocol/schema-reader-generator/src/org/jetbrains/jsonProtocol/SchemaReaderGenerator.java
@@ -1,14 +1,11 @@
 package org.jetbrains.jsonProtocol;
 
-import org.jetbrains.protocolReader.DynamicReader;
 import org.jetbrains.protocolReader.ReaderGenerator;
 
 import java.io.IOException;
 
 public class SchemaReaderGenerator extends ReaderGenerator {
   public static void main(String[] args) throws IOException {
-    mainImpl(args, new GenerateConfiguration("org.jetbrains.jsonProtocol", "ProtocolSchemaReaderImpl",
-                                             new DynamicReader<>(ProtocolSchemaReader.class,
-                                                                 ProtocolMetaModel.class.getDeclaredClasses())));
+    mainImpl(args, new GenerateConfiguration<>("org.jetbrains.jsonProtocol", "ProtocolSchemaReaderImpl", ProtocolSchemaReader.class, ProtocolMetaModel.class.getDeclaredClasses()));
   }
 }
