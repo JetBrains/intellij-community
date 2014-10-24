d933e318dd8772c6b009767717954b77775ac831
Konstantin Bulenkov <kb@jetbrains.com>
2014-10-24 17:53:22 +0200

    TemplateProjectStructureAction shows Project structure dialog with gaps

--- java/idea-ui/src/com/intellij/ide/actions/TemplateProjectStructureAction.java
+++ java/idea-ui/src/com/intellij/ide/actions/TemplateProjectStructureAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,19 +15,5 @@
  */
 package com.intellij.ide.actions;
 
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.options.ShowSettingsUtil;
-import com.intellij.openapi.options.newEditor.OptionsEditorDialog;
-import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ex.ProjectManagerEx;
-import com.intellij.openapi.roots.ui.configuration.ProjectStructureConfigurable;
-
-public class TemplateProjectStructureAction extends AnAction implements DumbAware {
-  @Override
-  public void actionPerformed(final AnActionEvent e) {
-    Project defaultProject = ProjectManagerEx.getInstanceEx().getDefaultProject();
-    ShowSettingsUtil.getInstance().editConfigurable(defaultProject, OptionsEditorDialog.DIMENSION_KEY, ProjectStructureConfigurable.getInstance(defaultProject));
-  }
+public class TemplateProjectStructureAction extends ShowStructureSettingsAction {
 }
