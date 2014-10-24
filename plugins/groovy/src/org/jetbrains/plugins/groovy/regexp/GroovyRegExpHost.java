478790592021e30ccf37056175a87b88e173b735
Bas Leijdekkers <basleijdekkers@gmail.com>
2014-10-24 11:46:49 +0200

    IDEA-91028 (Language injection for java.util.regex.Pattern does not know about special hex syntax, good code is red)

--- plugins/groovy/src/org/jetbrains/plugins/groovy/regexp/GroovyRegExpHost.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/regexp/GroovyRegExpHost.java
@@ -17,7 +17,9 @@ package org.jetbrains.plugins.groovy.regexp;
 
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtilCore;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.impl.JavaRegExpHost;
+import org.intellij.lang.regexp.psi.RegExpChar;
 import org.intellij.lang.regexp.psi.RegExpGroup;
 import org.jetbrains.plugins.groovy.config.GroovyConfigUtils;
 
@@ -29,12 +31,23 @@ public class GroovyRegExpHost extends JavaRegExpHost {
   @Override
   public boolean supportsNamedGroupSyntax(RegExpGroup group) {
     if (group.isNamedGroup()) {
-      final Module module = ModuleUtilCore.findModuleForPsiElement(group);
-      if (module != null) {
-        final String version = GroovyConfigUtils.getInstance().getSDKVersion(module);
-        return version != null && version.compareTo(GroovyConfigUtils.GROOVY2_0) >= 0;
-      }
+      final String version = getGroovyVersion(group);
+      return version != null && version.compareTo(GroovyConfigUtils.GROOVY2_0) >= 0;
     }
     return false;
+  }
+
+  @Override
+  public boolean supportsExtendedHexCharacter(RegExpChar regExpChar) {
+    final String version = getGroovyVersion(regExpChar);
+    return version != null && version.compareTo(GroovyConfigUtils.GROOVY2_0) >= 0;
+  }
+
+  private static String getGroovyVersion(PsiElement element) {
+    final Module module = ModuleUtilCore.findModuleForPsiElement(element);
+    if (module == null) {
+      return null;
+    }
+    return GroovyConfigUtils.getInstance().getSDKVersion(module);
   }
 }
