fd9ead508c5842a65ac57374ad8323dbf5afb505
Michael Golubev <michael.golubev@jetbrains.com>
2014-10-27 17:32:49 +0100

    IDEA-131963 - Redesign application servers view actions

--- platform/remote-servers/impl/src/com/intellij/remoteServer/impl/runtime/ui/tree/actions/UndeployAction.java
+++ platform/remote-servers/impl/src/com/intellij/remoteServer/impl/runtime/ui/tree/actions/UndeployAction.java
@@ -1,30 +1,29 @@
 package com.intellij.remoteServer.impl.runtime.ui.tree.actions;
 
 import com.intellij.icons.AllIcons;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.remoteServer.impl.runtime.ui.ServersToolWindowContent;
 import com.intellij.remoteServer.impl.runtime.ui.tree.DeploymentNode;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.List;
 
 /**
  * @author michael.golubev
  */
-public class UndeployAction extends DeploymentActionBase {
+public class UndeployAction extends ServersTreeAction<DeploymentNode> {
+
   public UndeployAction() {
     super("Undeploy", "Undeploy the selected item", AllIcons.Nodes.Undeploy);
   }
 
   @Override
-  protected boolean isApplicable(@NotNull DeploymentNode node) {
+  protected Class<DeploymentNode> getTargetNodeClass() {
+    return DeploymentNode.class;
+  }
+
+  @Override
+  protected boolean isEnabled4(DeploymentNode node) {
     return node.isUndeployActionEnabled();
   }
 
   @Override
-  protected void perform(@NotNull List<DeploymentNode> nodes, ServersToolWindowContent content, AnActionEvent e) {
-    for (DeploymentNode node : nodes) {
-      node.undeploy();
-    }
+  protected void doActionPerformed(DeploymentNode node) {
+    node.undeploy();
   }
 }
