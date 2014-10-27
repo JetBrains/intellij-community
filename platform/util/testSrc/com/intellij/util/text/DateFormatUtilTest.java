aca2e0c94d60413c95ad5271fdb001cee88aa258
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2014-10-27 19:30:23 +0100

    Test fix

--- platform/util/testSrc/com/intellij/util/text/DateFormatUtilTest.java
+++ platform/util/testSrc/com/intellij/util/text/DateFormatUtilTest.java
@@ -49,7 +49,7 @@ public class DateFormatUtilTest {
 
   @Test
   public void testTime() throws Exception {
-    Clock.setTime(2004, Calendar.DECEMBER, 10, 17, 10, 15);
+    Clock.setTime(1980, Calendar.DECEMBER, 10, 17, 10, 15);
 
     if (SystemInfo.isMac) {
       assertEquals("17:10", DateFormatUtil.formatTime(Clock.getTime()));
@@ -60,7 +60,7 @@ public class DateFormatUtilTest {
       assertEquals("17:10:15", printTimeForLocale("de_DE.UTF-8", Clock.getTime()));
     }
     else if (SystemInfo.isWinVistaOrNewer) {
-      long time = new Date().getTime();
+      long time = Clock.getTime();
       assertEquals(printWindowsTime(time), DateFormatUtil.formatTimeWithSeconds(time));
     }
     else {
