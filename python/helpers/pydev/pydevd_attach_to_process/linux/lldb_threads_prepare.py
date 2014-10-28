3c8313f95fa9eeba6831a1d71bf14082dcb10556
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2014-10-28 23:58:48 +0100

    Execute settrace only for process threads in lldb (PY-14247).

---
+++ python/helpers/pydev/pydevd_attach_to_process/linux/lldb_threads_prepare.py
@@ -0,0 +1,17 @@
+# This file is meant to be run inside lldb as a command before
+# attaching the debugger to mark process threads as suspended
+# to distinguish them from debugger threads later
+
+def __lldb_init_module(debugger, internal_dict):
+    import lldb
+
+    try:
+        target = debugger.GetSelectedTarget()
+        if target:
+            process = target.GetProcess()
+            if process:
+                for thread in process:
+                    internal_dict['_thread_%d' % thread.GetThreadID()] = True
+                    # thread.Suspend()
+    except:
+        import traceback;traceback.print_exc()
