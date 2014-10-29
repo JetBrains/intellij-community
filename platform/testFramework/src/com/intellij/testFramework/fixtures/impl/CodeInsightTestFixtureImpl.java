a584c7e6f66a4da8f02895ae05bbfe1909b6926a
Anton Makeev <Anton.Makeev@jetbrains.com>
2014-10-29 13:53:42 +0100

    Testing framework now allows checking only specified warnings and highlightings (bug fixed) (c280b5094ed1f9ee4b0801e61d5cb796bb5a5399)

--- platform/testFramework/src/com/intellij/testFramework/fixtures/impl/CodeInsightTestFixtureImpl.java
+++ platform/testFramework/src/com/intellij/testFramework/fixtures/impl/CodeInsightTestFixtureImpl.java
@@ -407,7 +407,7 @@ public class CodeInsightTestFixtureImpl extends BaseFixture implements CodeInsig
   @Override
   public long checkHighlighting(final boolean checkWarnings, final boolean checkInfos, final boolean checkWeakWarnings, boolean ignoreExtraHighlighting) {
     try {
-      return collectAndCheckHighlighting(checkWarnings, checkInfos, checkWeakWarnings);
+      return collectAndCheckHighlighting(checkWarnings, checkInfos, checkWeakWarnings, ignoreExtraHighlighting);
     }
     catch (Exception e) {
       throw new RuntimeException(e);
@@ -1501,8 +1501,7 @@ public class CodeInsightTestFixtureImpl extends BaseFixture implements CodeInsig
   private long collectAndCheckHighlighting(boolean checkWarnings, boolean checkInfos, boolean checkWeakWarnings, 
                                            boolean ignoreExtraHighlighting) throws Exception {
     ExpectedHighlightingData data = new ExpectedHighlightingData(myEditor.getDocument(), 
-                                                                 checkWarnings, checkWeakWarnings, ignoreExtraHighlighting, 
-                                                                 checkInfos, getHostFile());
+                                                                 checkWarnings, checkWeakWarnings, checkInfos, ignoreExtraHighlighting, getHostFile());
     data.init();
     return collectAndCheckHighlighting(data);
   }
