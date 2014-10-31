6f64152699289506a7138f5d9686cb424cd39cc6
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-10-31 13:14:13 +0100

    cleanup

--- java/debugger/impl/src/com/intellij/debugger/impl/GenericDebuggerParametersRunnerConfigurable.java
+++ java/debugger/impl/src/com/intellij/debugger/impl/GenericDebuggerParametersRunnerConfigurable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -132,7 +132,7 @@ public class GenericDebuggerParametersRunnerConfigurable extends SettingsEditor<
   }
 
   private int getTransport() {
-    if(myIsLocal) {
+    if (myIsLocal) {
       return DebuggerSettings.getInstance().DEBUGGER_TRANSPORT;
     }
     else {
