49d9433a9d22bf9d209e51a5132b8bd39e4b979b
Bas Leijdekkers <basleijdekkers@gmail.com>
2014-11-07 11:16:11 +0100

    IDEA-132470 (Wrong code inspections on signature polymorphic method calls)

--- plugins/InspectionGadgets/test/com/siyeh/igtest/bugs/var_arg/PrimitiveArrayArgumentToVariableArgMethod.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/bugs/var_arg/PrimitiveArrayArgumentToVariableArgMethod.java
@@ -1,11 +1,15 @@
 package com.siyeh.igtest.bugs.var_arg;
 
+import java.lang.invoke.MethodHandle;
+
 public class PrimitiveArrayArgumentToVariableArgMethod
 {
-    public static void main(String[] arg)
+    public static void main(String[] arg) throws Throwable
     {
         methodVarArgObject(<warning descr="Confusing primitive array argument to var-arg method">new byte[3]</warning>);
         methodVarArgByteArray(new byte[3]);
+        MethodHandle meh = null;
+        meh.invokeExact(new int[] { });
     }
 
     private static void methodVarArgObject(Object... bytes)
