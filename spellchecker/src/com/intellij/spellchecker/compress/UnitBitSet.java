aa858d9e52574921acd7ddd369729b2e8ee421c6
Alexey Kudravtsev <cdr@intellij.com>
2014-11-10 14:18:07 +0300

    assertion for non-empty dictionary

--- spellchecker/src/com/intellij/spellchecker/compress/UnitBitSet.java
+++ spellchecker/src/com/intellij/spellchecker/compress/UnitBitSet.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2014 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.intellij.spellchecker.compress;
 
 import org.jetbrains.annotations.NotNull;
@@ -46,7 +61,7 @@ public class UnitBitSet {
   @NotNull
   public byte[] pack() {
     int meaningfulBits = 32 - Integer.numberOfLeadingZeros(alpha.getLastIndexUsed());
-    assert meaningfulBits <= 8;
+    assert meaningfulBits <= 8 && meaningfulBits >= 1 : meaningfulBits + ": "+alpha.getLastIndexUsed();
     byte[] result = new byte[(b.length * meaningfulBits + 7) / 8];
 
     int byteNumber = 0;
