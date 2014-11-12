c93310b39808a3c4d41b66e1b1f8c00541849298
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2014-11-12 20:44:53 +0300

    Don't resolve unqualified types in docstrings using class index (PY-12864)
    It may result in false positives where our index-based guess is
    incorrect.

--- python/testData/inspections/PyTypeCheckerInspection/Generator.py
+++ python/testData/inspections/PyTypeCheckerInspection/Generator.py
@@ -9,22 +9,22 @@ def test():
         return xs
     def f_2(xs):
         """
-        :type xs: Sequence of int
+        :type xs: collections.Sequence of int
         """
         return xs
     def f_3(xs):
         """
-        :type xs: Container of int
+        :type xs: collections.Container of int
         """
         return xs
     def f_4(xs):
         """
-        :type xs: Iterator of int
+        :type xs: collections.Iterator of int
         """
         return xs
     def f_5(xs):
         """
-        :type xs: Iterable of int
+        :type xs: collections.Iterable of int
         """
         return xs
     def f_6(xs):
@@ -34,22 +34,22 @@ def test():
         return xs
     def f_7(xs):
         """
-        :type xs: Sequence
+        :type xs: collections.Sequence
         """
         return xs
     def f_8(xs):
         """
-        :type xs: Container
+        :type xs: collections.Container
         """
         return xs
     def f_9(xs):
         """
-        :type xs: Iterator
+        :type xs: collections.Iterator
         """
         return xs
     def f_10(xs):
         """
-        :type xs: Iterable
+        :type xs: collections.Iterable
         """
         return xs
     def f_11(xs):
@@ -59,22 +59,22 @@ def test():
         return xs
     def f_12(xs):
         """
-        :type xs: Sequence of string
+        :type xs: collections.Sequence of string
         """
         return xs
     def f_13(xs):
         """
-        :type xs: Container of string
+        :type xs: collections.Container of string
         """
         return xs
     def f_14(xs):
         """
-        :type xs: Iterator of string
+        :type xs: collections.Iterator of string
         """
         return xs
     def f_15(xs):
         """
-        :type xs: Iterable of string
+        :type xs: collections.Iterable of string
         """
         return xs
     return [
