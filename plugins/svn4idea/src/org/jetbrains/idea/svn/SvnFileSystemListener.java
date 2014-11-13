deddecae3c223e9ad0ff545560df17a0a0a11bc0
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2014-11-14 01:46:48 +0300

    svn: Refactored SvnFileSystemListenerWrapper - code simplified

--- plugins/svn4idea/src/org/jetbrains/idea/svn/SvnFileSystemListener.java
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/SvnFileSystemListener.java
@@ -61,7 +61,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
-public class SvnFileSystemListener extends CommandAdapter implements LocalFileOperationsHandler {
+public class SvnFileSystemListener implements LocalFileOperationsHandler {
   private static final Logger LOG = Logger.getInstance("#org.jetbrains.idea.svn.SvnFileSystemListener");
   private final LocalFileSystem myLfs;
 
