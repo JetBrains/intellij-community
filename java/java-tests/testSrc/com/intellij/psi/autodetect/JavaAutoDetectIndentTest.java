97ef4a141225bed12050c75326551fa6ac1aa3d4
Yaroslav Lepenkin <yaroslav.lepenkin@jetbrains.com>
2014-11-14 10:17:29 +0200

    [AutoDetect] while detecting indent, skip line comments (IDEA-132279). Also was extracted base class for indent detection tests and created java specific one.

---
+++ java/java-tests/testSrc/com/intellij/psi/autodetect/JavaAutoDetectIndentTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2000-2014 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.psi.autodetect;
+
+import com.intellij.JavaTestUtil;
+import org.jetbrains.annotations.NotNull;
+
+public class JavaAutoDetectIndentTest extends AbstractIndentAutoDetectionTest {
+
+  @NotNull
+  @Override
+  protected String getFileNameWithExtension() {
+    return getTestName(true) + ".java";
+  }
+
+  @NotNull
+  @Override
+  protected String getTestDataPath() {
+    return JavaTestUtil.getJavaTestDataPath() +
+           "/psi/autodetect/";
+  }
+
+  public void testNotIndentedComment() {
+    doTestIndentSize(3);
+  }
+}
