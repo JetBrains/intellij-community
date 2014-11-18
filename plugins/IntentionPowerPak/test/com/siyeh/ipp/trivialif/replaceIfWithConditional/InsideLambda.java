0bcb0e937f3b5c476634a5a36466e8134e95f5af
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-11-18 18:30:48 +0100

    check functional interface when inside lambda

---
+++ plugins/IntentionPowerPak/test/com/siyeh/ipp/trivialif/replaceIfWithConditional/InsideLambda.java
@@ -0,0 +1,16 @@
+class Test {
+  interface I {
+    int m();
+  }
+  
+  String foo() {
+    I i = () -> {
+      <caret>if (true) {
+        return 1;
+      } else {
+        return new Integer(2);
+      }
+    };
+    return null;
+  }
+}
