ee7245146438b7a2270c994f33a029f23c20e123
Eugene Zhuravlev <jeka@intellij.com>
2014-11-21 00:20:48 +0300

    external build: do not log paths when cloning FilesDelta (cherry picked from commit 4095277)

--- jps/jps-builders/src/org/jetbrains/jps/incremental/fs/FilesDelta.java
+++ jps/jps-builders/src/org/jetbrains/jps/incremental/fs/FilesDelta.java
@@ -148,16 +148,13 @@ final class FilesDelta {
   }
 
   private boolean _addToRecompiled(BuildRootDescriptor root, File file) {
+    if (Utils.IS_TEST_MODE) {
+      LOG.info("Marking dirty: " + file.getPath());
+    }
     return _addToRecompiled(root, Collections.singleton(file));
   }
 
   private boolean _addToRecompiled(BuildRootDescriptor root, Collection<File> filesToAdd) {
-    if (Utils.IS_TEST_MODE) {
-      for (File file : filesToAdd) {
-        LOG.info("Marking dirty: " + file.getPath());
-      }
-    }
-
     Set<File> files;
     synchronized (myFilesToRecompile) {
       files = myFilesToRecompile.get(root);
