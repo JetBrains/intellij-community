72e156435219e3428f206326942b0c4d4bb9b8e9
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2014-11-21 14:50:26 +0300

    Add several methods to convert JSON literals to Java values
    * JsonStringLiteral#getValue returns escaped content of the string
    with quotes stripped
    * JsonBooleanLiteral#getValue returns true/false
    * JsonNumberLiteral#getValue returns corresponding floating
    point number
    
    Also I removed some redundant accessors from API of elements.

--- json/gen/com/intellij/json/psi/JsonBooleanLiteral.java
+++ json/gen/com/intellij/json/psi/JsonBooleanLiteral.java
@@ -7,4 +7,6 @@ import com.intellij.psi.PsiElement;
 
 public interface JsonBooleanLiteral extends JsonLiteral {
 
+  boolean getValue();
+
 }
--- json/gen/com/intellij/json/psi/JsonNumberLiteral.java
+++ json/gen/com/intellij/json/psi/JsonNumberLiteral.java
@@ -7,7 +7,6 @@ import com.intellij.psi.PsiElement;
 
 public interface JsonNumberLiteral extends JsonLiteral {
 
-  @NotNull
-  PsiElement getNumber();
+  double getValue();
 
 }
