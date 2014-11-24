031ab39ef52a81ac849ee8bf8622c61ade83c09e
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2014-11-24 14:16:39 +0300

    PY-9778 Do not warn about unused inner functions with unknown decorators
    I added helper class PyKnownDecoratorUtil that contains list of
    well-known decorators from Python's standard library and redefined
    several existing methods from PyUtil in terms of this class.

--- python/testData/inspections/PyUnusedLocalFunctionInspection/src/test.py
+++ python/testData/inspections/PyUnusedLocalFunctionInspection/src/test.py
@@ -21,3 +21,25 @@ def foo():
     def baz(): #pass
         bar()
     baz()
+
+
+# PY-9778
+def unused_inner_function_with_known_decorator():
+    @staticmethod
+    def func():  # fail
+        yield
+
+
+def unused_inner_function_with_unknown_decorator():
+    def register(f):
+        return f
+
+    @register
+    @contextmanager
+    def func(): # pass
+        pass
+
+def unused_inner_function_with_incomplete_decorator():
+    @
+    def func(): # pass
+        pass
