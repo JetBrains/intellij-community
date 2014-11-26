b7dfaa66c87fbee7df33d6965f5d69a129c129cc
peter <peter@jetbrains.com>
2014-11-26 20:43:52 +0100

    ignore inferred @NotNull on overrideable methods (IDEA-132831, IDEA-131608) (cherry picked from commit 6877796)

--- java/java-tests/testSrc/com/intellij/codeInspection/DataFlowInspectionTest.java
+++ java/java-tests/testSrc/com/intellij/codeInspection/DataFlowInspectionTest.java
@@ -267,6 +267,7 @@ public class DataFlowInspectionTest extends LightCodeInsightFixtureTestCase {
   public void testSameComparisonTwice() { doTest(); }
   public void testRootThrowableCause() { doTest(); }
 
+  public void testOverridingInferredNotNullMethod() { doTest(); }
   public void testUseInferredContracts() { doTest(); }
   public void testContractWithNoArgs() { doTest(); }
   public void testContractInferenceBewareOverriding() { doTest(); }
