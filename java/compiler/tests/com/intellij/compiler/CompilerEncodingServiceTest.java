74607b9f19d5c41a364aac69bc2487b701da91ed
Alexey Kudravtsev <cdr@intellij.com>
2014-11-27 14:20:47 +0300

    IDEA-131468 Settings / File Encoding / Project Encoding = System default: new files are created with IDE encoding; use EncodingProjectManager instead of EncodingManager wherever possible

--- java/compiler/tests/com/intellij/compiler/CompilerEncodingServiceTest.java
+++ java/compiler/tests/com/intellij/compiler/CompilerEncodingServiceTest.java
@@ -1,13 +1,17 @@
 package com.intellij.compiler;
 
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.vfs.encoding.EncodingManager;
 import com.intellij.openapi.vfs.encoding.EncodingProjectManager;
 import com.intellij.testFramework.PsiTestCase;
 import com.intellij.testFramework.PsiTestUtil;
+import gnu.trove.THashSet;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Set;
 
 /**
  * @author nik
@@ -16,49 +20,63 @@ public class CompilerEncodingServiceTest extends PsiTestCase {
   private static final Charset WINDOWS_1251 = Charset.forName("windows-1251");
   private static final Charset WINDOWS_1252 = Charset.forName("windows-1252");
 
+  private Collection<Charset> projectDefaultPlus(@NotNull Charset... charsets) {
+    Set<Charset> result = new THashSet<Charset>();
+    result.add(getProjectDefault());
+    result.addAll(Arrays.asList(charsets));
+    return result;
+  }
+
   public void testDefaultEncoding() {
-    assertSameElements(getService().getAllModuleEncodings(myModule), getIdeDefault());
-    assertEquals(getIdeDefault(), getService().getPreferredModuleEncoding(myModule));
+    assertSameElements(getService().getAllModuleEncodings(myModule), getProjectDefault());
+    assertEquals(getProjectDefault(), getService().getPreferredModuleEncoding(myModule));
   }
 
   public void testJavaFileEncoding() {
-    assertSameElements(getService().getAllModuleEncodings(myModule), getIdeDefault());
+    assertSameElements(getService().getAllModuleEncodings(myModule), getProjectDefault());
     final VirtualFile file = createFile("A.java");
-    assertSameElements(getService().getAllModuleEncodings(myModule), getIdeDefault());
+    assertEquals(getProjectDefault(), file.getCharset());
+    assertSameElements(getService().getAllModuleEncodings(myModule), getProjectDefault());
     EncodingProjectManager.getInstance(myProject).setEncoding(file, WINDOWS_1251);
 
-    assertSameElements(getService().getAllModuleEncodings(myModule), WINDOWS_1251);
+    assertSameElements(getService().getAllModuleEncodings(myModule), projectDefaultPlus(WINDOWS_1251));
   }
 
   public void testNonJavaFileEncoding() {
     final VirtualFile file = createFile("A.properties");
+    assertEquals(getProjectDefault(), file.getCharset());
     EncodingProjectManager.getInstance(myProject).setEncoding(file, WINDOWS_1251);
 
-    assertSameElements(getService().getAllModuleEncodings(myModule), getIdeDefault());
+    assertSameElements(getService().getAllModuleEncodings(myModule), getProjectDefault());
   }
 
   public void testTwoJavaFilesWithDifferentEncodings() {
     final VirtualFile fileA = createFile("A.java");
     final VirtualFile fileB = createFile("B.java");
+    assertEquals(getProjectDefault(), fileA.getCharset());
+    assertEquals(getProjectDefault(), fileB.getCharset());
     EncodingProjectManager.getInstance(myProject).setEncoding(fileA, WINDOWS_1251);
     EncodingProjectManager.getInstance(myProject).setEncoding(fileB, WINDOWS_1252);
 
-    assertSameElements(getService().getAllModuleEncodings(myModule), WINDOWS_1251, WINDOWS_1252);
+    assertSameElements(getService().getAllModuleEncodings(myModule), projectDefaultPlus(WINDOWS_1251, WINDOWS_1252));
   }
 
   public void testJavaAndNonJavaFilesWithDifferentEncodings() {
     final VirtualFile fileA = createFile("A.java");
     final VirtualFile fileB = createFile("B.properties");
+    assertEquals(getProjectDefault(), fileA.getCharset());
+    assertEquals(getProjectDefault(), fileB.getCharset());
     EncodingProjectManager.getInstance(myProject).setEncoding(fileA, WINDOWS_1251);
     EncodingProjectManager.getInstance(myProject).setEncoding(fileB, WINDOWS_1252);
 
-    assertSameElements(getService().getAllModuleEncodings(myModule), WINDOWS_1251);
+    assertSameElements(getService().getAllModuleEncodings(myModule), projectDefaultPlus(WINDOWS_1251));
   }
 
   public void testSourceRootEncodingDominatesOnFileEncoding() {
     final VirtualFile file = createFile("A.java");
+    assertEquals(getProjectDefault(), file.getCharset());
     EncodingProjectManager.getInstance(myProject).setEncoding(file, WINDOWS_1251);
-    assertSameElements(getService().getAllModuleEncodings(myModule), WINDOWS_1251);
+    assertSameElements(getService().getAllModuleEncodings(myModule), projectDefaultPlus(WINDOWS_1251));
     assertEquals(WINDOWS_1251, getService().getPreferredModuleEncoding(myModule));
 
     EncodingProjectManager.getInstance(myProject).setEncoding(file.getParent(), WINDOWS_1252);
@@ -87,9 +105,9 @@ public class CompilerEncodingServiceTest extends PsiTestCase {
     }
   }
 
-
-  private static Charset getIdeDefault() {
-    return EncodingManager.getInstance().getDefaultCharset();
+  @NotNull
+  private Charset getProjectDefault() {
+    return EncodingProjectManager.getInstance(getProject()).getDefaultCharset();
   }
 
   private CompilerEncodingService getService() {
