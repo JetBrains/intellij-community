81a1dd2b8c8ac254ee3e1af957e9a9e43f569fef
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2014-11-28 17:59:22 +0300

    Test fix

--- python/testData/dotNet/expected.skeleton.Deep.py
+++ python/testData/dotNet/expected.skeleton.Deep.py
@@ -1,7 +1,7 @@
 # encoding: utf-8
 # module SingleNameSpace.Some.Deep calls itself Deep
 # from SingleNameSpace, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-# by generator 1.136
+# by generatorXXX
 # no doc
 # no imports
 
