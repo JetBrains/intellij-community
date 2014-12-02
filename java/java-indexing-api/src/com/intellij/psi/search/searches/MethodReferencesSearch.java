7abf547b318923debdc2d71f24ec28b195d0a2da
peter <peter@jetbrains.com>
2014-12-02 16:00:41 +0100

    deprecate SearchParameters.getScope (cherry picked from commit 302967b) [r=Maxim.Mossienko]

--- java/java-indexing-api/src/com/intellij/psi/search/searches/MethodReferencesSearch.java
+++ java/java-indexing-api/src/com/intellij/psi/search/searches/MethodReferencesSearch.java
@@ -65,9 +65,22 @@ public class MethodReferencesSearch extends ExtensibleQueryFactory<PsiReference,
       return myOptimizer;
     }
 
+    /**
+     * @return the user-visible search scope, most often "Project Files" or "Project and Libraries".
+     * Searchers most likely need to use {@link #getEffectiveSearchScope()}.
+     */
+    public SearchScope getScopeDeterminedByUser() {
+      return myScope;
+    }
+    
+    
+    /**
+     * @return Same as {@link #getScopeDeterminedByUser()}. Searchers most likely need to use {@link #getEffectiveSearchScope()}.
+     */
+    @Deprecated
     @NotNull
     public SearchScope getScope() {
-      return myScope;
+      return getScopeDeterminedByUser();
     }
 
     @NotNull
