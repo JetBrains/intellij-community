b358647f4c4e4802a7f684225476890a7dfdb3ef
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-12-02 19:08:06 +0100

    testdata fixed (too wide fonts on linux splitted components into multi rows!)
    (cherry picked from commit 2fa3bdf88f023445409060e0fa7efeec55675939)

--- java/java-tests/testData/codeInsight/parameterInfo/IgnoreVarargs.java
+++ java/java-tests/testData/codeInsight/parameterInfo/IgnoreVarargs.java
@@ -3,7 +3,7 @@ class Test {
     final String method = getParentOfType(String.class, CharSe<caret>quence.class);
   }
 
-  public static <T extends CharSequence> T getParentOfType(Class<T> aClass, Class<? extends CharSequence>... stopAt) {
+  public static <T extends CharSequence> T getParentOfType(Class<T> a, Class<? extends CharSequence>... stopAt) {
     return null;
   }
 }
