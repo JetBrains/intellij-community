b8d736cded9f8c8b0c7bd6430521d7bf6c034373
Nadya Zabrodina <Nadya.Zabrodina@jetbrains.com>
2014-12-04 17:05:39 +0300

    tasks-test  git dependency reverted

--- plugins/tasks/tasks-tests/test/com/intellij/tasks/vcs/GitTaskBranchesTest.java
+++ plugins/tasks/tasks-tests/test/com/intellij/tasks/vcs/GitTaskBranchesTest.java
@@ -13,14 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package git4idea.branch;
+package com.intellij.tasks.vcs;
 
 import com.intellij.dvcs.repo.Repository;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.tasks.vcs.TaskBranchesTest;
+import git4idea.branch.GitBranchesCollection;
 import git4idea.config.GitVcsSettings;
 import git4idea.repo.GitRepository;
 import git4idea.test.GitExecutor;
