9cc71f8c0de054ad06a73439f701a75b687b3c7d
Bas Leijdekkers <basleijdekkers@gmail.com>
2014-12-04 12:22:53 +0100

    make IG test light

--- plugins/InspectionGadgets/test/com/siyeh/igtest/assignment/assignment_to_superclass_field/AssignmentToSuperclassField.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/assignment/assignment_to_superclass_field/AssignmentToSuperclassField.java
@@ -17,9 +17,9 @@ class B extends AssignmentToSuperclassField {
   int z;
 
   B() {
-    i += 3;
-    this.j = 4;
-    super.k++;
+    <warning descr="Assignment to field 'i' defined in superclass 'AssignmentToSuperclassField'">i</warning> += 3;
+    <warning descr="Assignment to field 'j' defined in superclass 'AssignmentToSuperclassField'">this.j</warning> = 4;
+    <warning descr="Assignment to field 'k' defined in superclass 'AssignmentToSuperclassField'">super.k</warning>++;
     z = 100;
   }
 }
