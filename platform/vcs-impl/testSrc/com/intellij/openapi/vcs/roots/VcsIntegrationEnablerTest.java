0e22cfc998b8112a70d1f7b14648c57709a46cb4
Nadya Zabrodina <Nadya.Zabrodina@jetbrains.com>
2014-12-05 14:31:01 +0300

    compile fixed

--- platform/vcs-impl/testSrc/com/intellij/openapi/vcs/roots/VcsIntegrationEnablerTest.java
+++ platform/vcs-impl/testSrc/com/intellij/openapi/vcs/roots/VcsIntegrationEnablerTest.java
@@ -20,6 +20,7 @@ import com.intellij.notification.NotificationType;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vcs.*;
 import com.intellij.openapi.vcs.changes.committed.MockAbstractVcs;
+import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.testFramework.PlatformTestCase;
 import com.intellij.util.Function;
