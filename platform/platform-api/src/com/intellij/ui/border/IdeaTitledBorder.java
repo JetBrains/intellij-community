2954b382c3ab2d329f9b01b4bea49952139c2e04
Konstantin Bulenkov <kb@jetbrains.com>
2014-12-08 15:00:12 +0100

    fix title border to support HiDPI

--- platform/platform-api/src/com/intellij/ui/border/IdeaTitledBorder.java
+++ platform/platform-api/src/com/intellij/ui/border/IdeaTitledBorder.java
@@ -1,7 +1,23 @@
+/*
+ * Copyright 2000-2014 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.intellij.ui.border;
 
 import com.intellij.ui.TitledSeparator;
 import com.intellij.util.ui.DialogUtil;
+import com.intellij.util.ui.JBInsets;
 import com.intellij.util.ui.UIUtil;
 
 import javax.swing.*;
@@ -23,8 +39,8 @@ public class IdeaTitledBorder extends TitledBorder {
     titledSeparator.setText(title);
     DialogUtil.registerMnemonic(titledSeparator.getLabel(), null);
 
-    outsideInsets = new Insets(insets.top, insets.left, insets.bottom, insets.right);
-    insideInsets = new Insets(TitledSeparator.BOTTOM_INSET, indent, 0, 0);
+    outsideInsets = JBInsets.create(insets);
+    insideInsets = new JBInsets(TitledSeparator.BOTTOM_INSET, indent, 0, 0);
   }
 
   @Override
