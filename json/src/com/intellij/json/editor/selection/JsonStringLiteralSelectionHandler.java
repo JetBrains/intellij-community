636b82648744fee18b21abc16f6a4dfdaeceb66e
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2014-12-09 16:27:26 +0300

    EA-61009 Fix assertion and do not process injected JSON strings (cherry picked from commit e13e16b)

--- json/src/com/intellij/json/editor/selection/JsonStringLiteralSelectionHandler.java
+++ json/src/com/intellij/json/editor/selection/JsonStringLiteralSelectionHandler.java
@@ -2,8 +2,8 @@ package com.intellij.json.editor.selection;
 
 import com.intellij.codeInsight.editorActions.ExtendWordSelectionHandlerBase;
 import com.intellij.codeInsight.editorActions.SelectWordUtil;
-import com.intellij.json.JsonParserDefinition;
 import com.intellij.json.psi.JsonStringLiteral;
+import com.intellij.lang.injection.InjectedLanguageManager;
 import com.intellij.lexer.StringLiteralLexer;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.util.TextRange;
@@ -22,17 +22,20 @@ import static com.intellij.json.JsonElementTypes.SINGLE_QUOTED_STRING;
 public class JsonStringLiteralSelectionHandler extends ExtendWordSelectionHandlerBase {
   @Override
   public boolean canSelect(PsiElement e) {
-    return JsonParserDefinition.STRING_LITERALS.contains(e.getNode().getElementType());
+    if (!(e.getParent() instanceof JsonStringLiteral)) {
+      return false;
+    }
+    return !InjectedLanguageManager.getInstance(e.getProject()).isInjectedFragment(e.getContainingFile());
   }
 
   @Override
   public List<TextRange> select(PsiElement e, CharSequence editorText, int cursorOffset, Editor editor) {
-    final PsiElement parent = e.getParent();
-    assert parent instanceof JsonStringLiteral;
     final IElementType type = e.getNode().getElementType();
     final StringLiteralLexer lexer = new StringLiteralLexer(type == SINGLE_QUOTED_STRING ? '\'' : '"', type, false, "/", false, false);
     final List<TextRange> result = new ArrayList<TextRange>();
     SelectWordUtil.addWordHonoringEscapeSequences(editorText, e.getTextRange(), cursorOffset, lexer, result);
+
+    final PsiElement parent = e.getParent();
     result.add(ElementManipulators.getValueTextRange(parent).shiftRight(parent.getTextOffset()));
     return result;
   }
