5a66c2a5eae37c7d10d38521e43b2b580157bcc3
Bas Leijdekkers <basleijdekkers@gmail.com>
2014-12-11 17:08:23 +0100

    IDEA-134153 (Iteration over 'keySet()' may be replaced with 'entrySet()' iteration inspection misses concrete Map implementations)

--- plugins/InspectionGadgets/test/com/siyeh/igtest/performance/key_set_iteration_may_use_entry_set/KeySetIterationMayUseEntrySet.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/performance/key_set_iteration_may_use_entry_set/KeySetIterationMayUseEntrySet.java
@@ -1,5 +1,6 @@
 package com.siyeh.igtest.performance.key_set_iteration_may_use_entry_set;
 
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -9,6 +10,17 @@ public class KeySetIterationMayUseEntrySet {
     for (String k : <warning descr="Iteration over 'm.keySet()' may be replaced with 'entrySet()' iteration">m.keySet()</warning>) {
       System.out.println(m.get(k));
     }
+  }
+
+  void bar() {
+    HashMap<String, String> map = get();
+    for (String a : <warning descr="Iteration over 'map.keySet()' may be replaced with 'entrySet()' iteration">map.keySet()</warning>) {
+      System.out.println(map.get(a));
+    }
+  }
+
+  HashMap<String, String> get() {
+    return null;
   }
 
   class MyClass {
