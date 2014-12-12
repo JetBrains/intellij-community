24a24495c6dc9866c3a8369ee416abde6ea65553
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-12-12 18:43:59 +0100

    HttpRequests — ability to disable host verification (ssl) cleanup — use HttpRequests (cherry picked from commit b770eca)

--- platform/platform-impl/src/com/intellij/util/io/HttpRequests.java
+++ platform/platform-impl/src/com/intellij/util/io/HttpRequests.java
@@ -17,12 +17,17 @@ package com.intellij.util.io;
 
 import com.intellij.ide.IdeBundle;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.util.SystemInfo;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.util.SystemProperties;
 import com.intellij.util.net.HttpConfigurable;
+import com.intellij.util.net.ssl.CertificateManager;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.io.Responses;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -42,8 +47,9 @@ import java.util.zip.GZIPInputStream;
  * }</pre>
  */
 public final class HttpRequests {
-  private static final boolean ourWrapClassLoader =
-    SystemInfo.isJavaVersionAtLeast("1.7") && !SystemProperties.getBooleanProperty("idea.parallel.class.loader", false);
+  private static final Logger LOG = Logger.getInstance(HttpRequests.class);
+
+  private static final boolean ourParallelLoader = SystemProperties.getBooleanProperty("idea.parallel.class.loader", true);
 
   public interface Request {
     @NotNull URLConnection getConnection() throws IOException;
@@ -60,7 +66,8 @@ public final class HttpRequests {
     private int myTimeout = HttpConfigurable.READ_TIMEOUT;
     private int myRedirectLimit = HttpConfigurable.REDIRECT_LIMIT;
     private boolean myGzip = true;
-    private boolean myForceHttps = false;
+    private boolean myForceHttps;
+    private boolean myDisableHostVerification;
 
     private RequestBuilder(@NotNull String url) {
       myUrl = url;
@@ -96,13 +103,19 @@ public final class HttpRequests {
       return this;
     }
 
+    @NotNull
+    public RequestBuilder disableHostVerification() {
+      myDisableHostVerification = true;
+      return this;
+    }
+
     public <T> T connect(@NotNull RequestProcessor<T> processor) throws IOException {
       // todo[r.sh] drop condition in IDEA 15
-      if (ourWrapClassLoader) {
-        return wrapAndProcess(this, processor);
+      if (ourParallelLoader) {
+        return process(this, processor);
       }
       else {
-        return process(this, processor);
+        return wrapAndProcess(this, processor);
       }
     }
   }
@@ -188,6 +201,30 @@ public final class HttpRequests {
 
       connection.setConnectTimeout(builder.myConnectTimeout);
       connection.setReadTimeout(builder.myTimeout);
+
+      String userAgent = Responses.getServerHeaderValue();
+      if (userAgent != null) {
+        connection.setRequestProperty("User-Agent", userAgent);
+      }
+
+      if (connection instanceof HttpsURLConnection) {
+        try {
+          HttpsURLConnection httpsConnection = (HttpsURLConnection)connection;
+          if (builder.myDisableHostVerification) {
+            httpsConnection.setHostnameVerifier(new HostnameVerifier() {
+              @Override
+              public boolean verify(String hostname, SSLSession session) {
+                return true;
+              }
+            });
+          }
+          httpsConnection.setSSLSocketFactory(CertificateManager.getInstance().getSslContext().getSocketFactory());
+        }
+        catch (Exception e) {
+          LOG.warn(e);
+        }
+      }
+
       if (builder.myGzip) {
         connection.setRequestProperty("Accept-Encoding", "gzip");
       }
