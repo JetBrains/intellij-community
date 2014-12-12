cd6366ceeb037696f49183c70f67826395bc4651
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-12-12 20:57:02 +0100

    method references: site substitution for method references without receiver (IDEA-134261)
    (cherry picked from commit 482de672ef350daf162759d4daf46c94fb9a06f6)

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewMethodRefHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewMethodRefHighlightingTest.java
@@ -336,6 +336,10 @@ public class NewMethodRefHighlightingTest extends LightDaemonAnalyzerTestCase {
     doTest(true);
   }
 
+  public void testSiteSubstitutionOfNonReceiverReference() throws Exception {
+    doTest();
+  }
+
   private void doTest() {
     doTest(false);
   }
