984255e539e2ddff79746170d74323ada631f405
nik <Nikolay.Chashnikov@jetbrains.com>
2014-12-16 21:47:16 +0300

    SegmentArrayWithData: fixed contradictory comparison in range check

--- platform/core-impl/src/com/intellij/openapi/editor/ex/util/SegmentArrayWithData.java
+++ platform/core-impl/src/com/intellij/openapi/editor/ex/util/SegmentArrayWithData.java
@@ -31,7 +31,7 @@ public class SegmentArrayWithData extends SegmentArray {
   }
 
   public void setElementAt(int i, int startOffset, int endOffset, int data) {
-    if (data < 0 && data > Short.MAX_VALUE) throw new IndexOutOfBoundsException("data out of short range" + data);
+    if (data < 0 || data > Short.MAX_VALUE) throw new IndexOutOfBoundsException("data out of short range" + data);
     setElementAt(i, startOffset, endOffset);
     myData = reallocateArray(myData, i+1);
     myData[i] = (short)data;
