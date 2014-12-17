1b4ed5ca91e3fc66e9d8d7007825a9d1ef109d82
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-12-17 15:50:18 +0100

    new overload resolution: check only functional arguments in isFunctionalTypeMoreSpecific (IDEA-134328)
    (cherry picked from commit a026cc2dd1bca630317412b6f65edb3c75e426f0)

--- java/java-psi-impl/src/com/intellij/psi/impl/source/resolve/graphInference/InferenceSession.java
+++ java/java-psi-impl/src/com/intellij/psi/impl/source/resolve/graphInference/InferenceSession.java
@@ -1202,7 +1202,7 @@ public class InferenceSession {
         continue;
       }
       if (LambdaUtil.isFunctionalType(sType) && LambdaUtil.isFunctionalType(tType) && !relates(sType, tType)) {
-        if (!isFunctionalTypeMoreSpecific(sType, tType, session, args)) {
+        if (!isFunctionalTypeMoreSpecific(sType, tType, session, args[i])) {
           return false;
         }
       } else {
--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/OverloadResolutionTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/OverloadResolutionTest.java
@@ -55,6 +55,10 @@ public class OverloadResolutionTest extends LightDaemonAnalyzerTestCase {
     doTest();
   }
 
+  public void testIgnoreNonFunctionalArgumentsWhenCheckIfFunctionalMoreSpecific() throws Exception {
+    doTest();
+  }
+
   private void doTest() {
     doTest(true);
   }
