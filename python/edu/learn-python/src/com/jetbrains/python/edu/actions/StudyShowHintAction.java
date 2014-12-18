48e8d0ef19f5453a68d340a9ea0d47bcfb981a27
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2014-12-18 14:52:21 +0300

    proofreading EDU (part 2)

--- python/edu/learn-python/src/com/jetbrains/python/edu/actions/StudyShowHintAction.java
+++ python/edu/learn-python/src/com/jetbrains/python/edu/actions/StudyShowHintAction.java
@@ -28,7 +28,7 @@ import java.io.File;
 public class StudyShowHintAction extends DumbAwareAction {
   public static final String ACTION_ID = "ShowHintAction";
   public static final String SHORTCUT = "ctrl pressed 7";
-  public static final String OUTSIDE_TASK_WINDOW_MESSAGE = "Put caret to answer placeholder to get hint";
+  public static final String OUTSIDE_TASK_WINDOW_MESSAGE = "Put the caret in the answer placeholder to get hint";
   public static final String HINT_NOT_AVAILABLE = "There is no hint for this answer placeholder";
 
   public StudyShowHintAction() {
