9075cc3a558938cd1149f2cc834f170bd19646c5
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-12-18 13:40:53 +0100

    surround with try/catch: ignore nested functional expressions from unchecked analyzes
    (cherry picked from commit 6e761e661fdcd78d2774027f95297418cb630f6c)

---
+++ java/java-tests/testData/codeInsight/generation/surroundWith/java/SurroundWithTryCatchFunctionalExpression_after.java
@@ -0,0 +1,18 @@
+class Test {
+
+  public static void main(String[] args){
+      try {
+          I i = ExceptionTest::foo;
+      } catch (Exception e) {
+          e.printStackTrace();
+      }
+  }
+
+  class Ex extends Exception {}
+
+  static void foo() throws Ex {}
+
+  interface I {
+    void f();
+  }
+}
