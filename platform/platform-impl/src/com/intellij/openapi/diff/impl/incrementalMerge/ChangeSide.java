8206d46992fde993133b228eec55ad0196b08b36
Alexey Kudravtsev <cdr@intellij.com>
2014-12-19 20:28:06 +0300

    cleanup

--- platform/platform-impl/src/com/intellij/openapi/diff/impl/incrementalMerge/ChangeSide.java
+++ platform/platform-impl/src/com/intellij/openapi/diff/impl/incrementalMerge/ChangeSide.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +21,7 @@ import org.jetbrains.annotations.NotNull;
 /**
 * @author Kirill Likhodedov
 */
-public abstract class ChangeSide {
+abstract class ChangeSide {
 
   public int getStart() {
     return getRange().getStartOffset();
@@ -43,6 +43,7 @@ public abstract class ChangeSide {
   @NotNull
   public abstract DiffRangeMarker getRange();
 
+  @NotNull
   public abstract ChangeHighlighterHolder getHighlighterHolder();
 
   public boolean contains(int offset) {
