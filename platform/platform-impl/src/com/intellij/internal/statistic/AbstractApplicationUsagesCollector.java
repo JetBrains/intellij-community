80c8b76f90956d74360ecfba5f8e12997aaf3bb5
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-12-19 14:06:53 +0100

    cleanup

--- platform/platform-impl/src/com/intellij/internal/statistic/AbstractApplicationUsagesCollector.java
+++ platform/platform-impl/src/com/intellij/internal/statistic/AbstractApplicationUsagesCollector.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.util.Function;
 import com.intellij.util.containers.ContainerUtil;
-import com.intellij.util.containers.hash.HashMap;
+import gnu.trove.THashMap;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -30,64 +30,60 @@ import java.util.Map;
 import java.util.Set;
 
 public abstract class AbstractApplicationUsagesCollector extends UsagesCollector {
-    private static final Logger LOG = Logger.getInstance("#com.intellij.internal.statistic.AbstractApplicationUsagesCollector");
+  private static final Logger LOG = Logger.getInstance(AbstractApplicationUsagesCollector.class);
 
-    public void persistProjectUsages(@NotNull Project project) {
-      try {
-        final Set<UsageDescriptor> projectUsages = getProjectUsages(project);
-        persistProjectUsages(project, projectUsages);
+  public void persistProjectUsages(@NotNull Project project) {
+    try {
+      persistProjectUsages(project, getProjectUsages(project));
+    }
+    catch (CollectUsagesException e) {
+      LOG.info(e);
+    }
+  }
+
+  public void persistProjectUsages(@NotNull Project project, @NotNull Set<UsageDescriptor> usages) {
+    persistProjectUsages(project, usages, ApplicationStatisticsPersistenceComponent.getInstance());
+  }
+
+  public void persistProjectUsages(@NotNull Project project,
+                                   @NotNull Set<UsageDescriptor> usages,
+                                   @NotNull ApplicationStatisticsPersistence persistence) {
+    persistence.persistUsages(getGroupId(), project, usages);
+  }
+
+  @NotNull
+  public Set<UsageDescriptor> getApplicationUsages() {
+    return getApplicationUsages(ApplicationStatisticsPersistenceComponent.getInstance());
+  }
+
+  @NotNull
+  public Set<UsageDescriptor> getApplicationUsages(@NotNull ApplicationStatisticsPersistence persistence) {
+    Map<String, Integer> result = new THashMap<String, Integer>();
+    for (Set<UsageDescriptor> usageDescriptors : persistence.getApplicationData(getGroupId()).values()) {
+      for (UsageDescriptor usageDescriptor : usageDescriptors) {
+        final String key = usageDescriptor.getKey();
+        final Integer count = result.get(key);
+        result.put(key, count == null ? 1 : count.intValue() + 1);
       }
-      catch (CollectUsagesException e) {
-        LOG.info(e);
+    }
+
+    return ContainerUtil.map2Set(result.entrySet(), new Function<Map.Entry<String, Integer>, UsageDescriptor>() {
+      @Override
+      public UsageDescriptor fun(Map.Entry<String, Integer> entry) {
+        return new UsageDescriptor(entry.getKey(), entry.getValue());
       }
+    });
+  }
+
+  @Override
+  @NotNull
+  public Set<UsageDescriptor> getUsages(@Nullable Project project) throws CollectUsagesException {
+    if (project != null) {
+      persistProjectUsages(project, getProjectUsages(project));
     }
+    return getApplicationUsages();
+  }
 
-    public void persistProjectUsages(@NotNull Project project, @NotNull Set<UsageDescriptor> usages) {
-        persistProjectUsages(project, usages, ApplicationStatisticsPersistenceComponent.getInstance());
-    }
-
-    public void persistProjectUsages(@NotNull Project project,
-                                     @NotNull Set<UsageDescriptor> usages,
-                                     @NotNull ApplicationStatisticsPersistence persistence) {
-        persistence.persistUsages(getGroupId(), project, usages);
-    }
-
-    @NotNull
-    public Set<UsageDescriptor> getApplicationUsages() {
-        return getApplicationUsages(ApplicationStatisticsPersistenceComponent.getInstance());
-    }
-
-    @NotNull
-    public Set<UsageDescriptor> getApplicationUsages(@NotNull final ApplicationStatisticsPersistence persistence) {
-        final Map<String, Integer> result = new HashMap<String, Integer>();
-
-        for (Set<UsageDescriptor> usageDescriptors : persistence.getApplicationData(getGroupId()).values()) {
-            for (UsageDescriptor usageDescriptor : usageDescriptors) {
-                final String key = usageDescriptor.getKey();
-                final Integer count = result.get(key);
-                result.put(key, count == null ? 1 : count.intValue() + 1);
-            }
-        }
-
-        return ContainerUtil.map2Set(result.entrySet(), new Function<Map.Entry<String, Integer>, UsageDescriptor>() {
-            @Override
-            public UsageDescriptor fun(Map.Entry<String, Integer> entry) {
-                return new UsageDescriptor(entry.getKey(), entry.getValue());
-            }
-        });
-    }
-
-    @Override
-    @NotNull
-    public Set<UsageDescriptor> getUsages(@Nullable Project project) throws CollectUsagesException {
-        if (project != null) {
-          final Set<UsageDescriptor> projectUsages = getProjectUsages(project);
-          persistProjectUsages(project, projectUsages);
-        }
-
-        return getApplicationUsages();
-    }
-
-    @NotNull
-    public abstract Set<UsageDescriptor> getProjectUsages(@NotNull Project project) throws CollectUsagesException;
+  @NotNull
+  public abstract Set<UsageDescriptor> getProjectUsages(@NotNull Project project) throws CollectUsagesException;
 }
