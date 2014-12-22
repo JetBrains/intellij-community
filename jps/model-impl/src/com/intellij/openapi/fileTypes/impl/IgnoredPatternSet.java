31d51ff6d9afd0ba83faeac6f92b9ab7dda2268a
Alexey Kudravtsev <cdr@intellij.com>
2014-12-22 18:49:13 +0300

    println removed

--- jps/model-impl/src/com/intellij/openapi/fileTypes/impl/IgnoredPatternSet.java
+++ jps/model-impl/src/com/intellij/openapi/fileTypes/impl/IgnoredPatternSet.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2011 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -65,7 +65,7 @@ public class IgnoredPatternSet {
     return fileName.endsWith(FileUtil.ASYNC_DELETE_EXTENSION);
   }
 
-  void clearPatterns() {
+  private void clearPatterns() {
     myMasks.clear();
     myIgnorePatterns.removeAllAssociations(Boolean.TRUE);
   }
