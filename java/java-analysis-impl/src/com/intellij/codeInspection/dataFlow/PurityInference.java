52a2de3b566de1dca0d13aad327e86fc4088f871
peter <peter@jetbrains.com>
2014-12-22 13:44:38 +0100

    don't infer annotations in anonymous classes unless they'll be useful

--- java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/PurityInference.java
+++ java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/PurityInference.java
@@ -34,7 +34,10 @@ import java.util.List;
 public class PurityInference {
 
   public static boolean inferPurity(@NotNull final PsiMethod method) {
-    if (ContractInference.isLibraryCode(method) || method.getReturnType() == PsiType.VOID || method.getBody() == null || method.isConstructor()) {
+    if (!InferenceFromSourceUtil.shouldInferFromSource(method) ||
+        method.getReturnType() == PsiType.VOID ||
+        method.getBody() == null ||
+        method.isConstructor()) {
       return false;
     }
 
