520b7d4fea04600caa7a51d1cd9789398f1d0777
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2014-12-23 13:03:03 +0100

    platform: restart build process on Java compiler settings modification
    (support request #48593)

--- java/compiler/impl/src/com/intellij/compiler/options/JavaCompilersTab.java
+++ java/compiler/impl/src/com/intellij/compiler/options/JavaCompilersTab.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -137,12 +137,14 @@ public class JavaCompilersTab implements SearchableConfigurable, Configurable.No
   public void apply() throws ConfigurationException {
     boolean modified = false;
     try {
+      modified = isModified();
       for (Configurable configurable : myConfigurables) {
         if (configurable.isModified()) {
           modified = true;
           configurable.apply();
         }
       }
+
       myCompilerConfiguration.setDefaultCompiler(mySelectedCompiler);
       myCompilerConfiguration.setProjectBytecodeTarget(myTargetLevelComponent.getProjectBytecodeTarget());
       myCompilerConfiguration.setModulesBytecodeTargetMap(myTargetLevelComponent.getModulesBytecodeTargetMap());
