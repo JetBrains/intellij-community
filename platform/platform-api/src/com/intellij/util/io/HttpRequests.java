9acd5f3bf27f21805756704a1d8a2aa54977e6b3
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-12-24 12:20:29 +0100

    IDEA-CR-1100 move RequestBuilder to top level

--- platform/platform-api/src/com/intellij/util/io/HttpRequests.java
+++ platform/platform-api/src/com/intellij/util/io/HttpRequests.java
@@ -16,26 +16,20 @@
 package com.intellij.util.io;
 
 import com.intellij.ide.IdeBundle;
-import com.intellij.openapi.application.Application;
-import com.intellij.openapi.application.ApplicationInfo;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.util.io.BufferExposingByteArrayOutputStream;
 import com.intellij.openapi.util.io.FileUtilRt;
 import com.intellij.openapi.util.io.StreamUtil;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.CharsetToolkit;
 import com.intellij.util.ArrayUtil;
-import com.intellij.util.SystemProperties;
 import com.intellij.util.net.HTTPMethod;
 import com.intellij.util.net.HttpConfigurable;
 import com.intellij.util.net.NetUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import java.io.*;
 import java.net.HttpURLConnection;
@@ -56,8 +50,8 @@ import java.util.zip.GZIPInputStream;
  * }</pre>
  */
 public final class HttpRequests {
-  private static final boolean ourWrapClassLoader =
-    SystemInfo.isJavaVersionAtLeast("1.7") && !SystemProperties.getBooleanProperty("idea.parallel.class.loader", true);
+  private HttpRequests() {
+  }
 
   public interface Request {
     @NotNull
@@ -84,7 +78,16 @@ public final class HttpRequests {
     T process(@NotNull Request request) throws IOException;
   }
 
-  private HttpRequests() {
+  @NotNull
+  public static RequestBuilder request(@NotNull String url) {
+    return new RequestBuilder(url);
+  }
+
+  @NotNull
+  public static RequestBuilder head(@NotNull String url) {
+    RequestBuilder builder = request(url);
+    builder.myMethod = HTTPMethod.HEAD;
+    return builder;
   }
 
   @NotNull
@@ -98,151 +101,7 @@ public final class HttpRequests {
     return errorMessage;
   }
 
-  public final static class RequestBuilder {
-    private final String myUrl;
-    private int myConnectTimeout = HttpConfigurable.CONNECTION_TIMEOUT;
-    private int myTimeout = HttpConfigurable.READ_TIMEOUT;
-    private int myRedirectLimit = HttpConfigurable.REDIRECT_LIMIT;
-    private boolean myGzip = true;
-    private boolean myForceHttps;
-    private HostnameVerifier myHostnameVerifier;
-    private String myUserAgent;
-    private String myAccept;
-
-    private HTTPMethod myMethod;
-
-    private RequestBuilder(@NotNull String url) {
-      myUrl = url;
-    }
-
-    @NotNull
-    public RequestBuilder connectTimeout(int value) {
-      myConnectTimeout = value;
-      return this;
-    }
-
-    @NotNull
-    public RequestBuilder readTimeout(int value) {
-      myTimeout = value;
-      return this;
-    }
-
-    @NotNull
-    public RequestBuilder redirectLimit(int redirectLimit) {
-      myRedirectLimit = redirectLimit;
-      return this;
-    }
-
-    @NotNull
-    public RequestBuilder gzip(boolean value) {
-      myGzip = value;
-      return this;
-    }
-
-    @NotNull
-    public RequestBuilder forceHttps(boolean forceHttps) {
-      myForceHttps = forceHttps;
-      return this;
-    }
-
-    @NotNull
-    public RequestBuilder hostNameVerifier(@Nullable HostnameVerifier hostnameVerifier) {
-      myHostnameVerifier = hostnameVerifier;
-      return this;
-    }
-
-    @NotNull
-    public RequestBuilder userAgent(@Nullable String userAgent) {
-      myUserAgent = userAgent;
-      return this;
-    }
-
-    @NotNull
-    public RequestBuilder userAgent() {
-      Application app = ApplicationManager.getApplication();
-      if (app != null && !app.isDisposed()) {
-        return userAgent(ApplicationInfo.getInstance().getVersionName());
-      }
-      else {
-        return userAgent("IntelliJ");
-      }
-    }
-
-    @NotNull
-    public RequestBuilder accept(@Nullable String mimeType) {
-      myAccept = mimeType;
-      return this;
-    }
-
-    public <T> T connect(@NotNull RequestProcessor<T> processor) throws IOException {
-      // todo[r.sh] drop condition in IDEA 15
-      if (ourWrapClassLoader) {
-        return wrapAndProcess(this, processor);
-      }
-      else {
-        return process(this, processor);
-      }
-    }
-
-    public <T> T connect(@NotNull RequestProcessor<T> processor, T errorValue, @Nullable Logger logger) {
-      try {
-        return connect(processor);
-      }
-      catch (Throwable e) {
-        if (logger != null) {
-          logger.warn(e);
-        }
-        return errorValue;
-      }
-    }
-
-    public void saveToFile(@NotNull final File file, @Nullable final ProgressIndicator indicator) throws IOException {
-      connect(new HttpRequests.RequestProcessor<Void>() {
-        @Override
-        public Void process(@NotNull HttpRequests.Request request) throws IOException {
-          request.saveToFile(file, indicator);
-          return null;
-        }
-      });
-    }
-
-    @NotNull
-    public byte[] readBytes(@Nullable final ProgressIndicator indicator) throws IOException {
-      return connect(new HttpRequests.RequestProcessor<byte[]>() {
-        @Override
-        public byte[] process(@NotNull HttpRequests.Request request) throws IOException {
-          return request.readBytes(indicator);
-        }
-      });
-    }
-
-    @NotNull
-    public String readString(@Nullable final ProgressIndicator indicator) throws IOException {
-      return connect(new HttpRequests.RequestProcessor<String>() {
-        @Override
-        public String process(@NotNull HttpRequests.Request request) throws IOException {
-          int contentLength = request.getConnection().getContentLength();
-          BufferExposingByteArrayOutputStream out = new BufferExposingByteArrayOutputStream(contentLength > 0 ? contentLength : 16 * 1024);
-          NetUtils.copyStreamContent(indicator, request.getInputStream(), out, contentLength);
-          return new String(out.getInternalBuffer(), 0, out.size(), getCharset(request));
-        }
-      });
-    }
-  }
-
-  @NotNull
-  public static RequestBuilder request(@NotNull String url) {
-    return new RequestBuilder(url);
-  }
-
-  @NotNull
-  public static RequestBuilder head(@NotNull String url) {
-    RequestBuilder builder = request(url);
-    builder.myMethod = HTTPMethod.HEAD;
-    return builder;
-  }
-
-  private static <T> T wrapAndProcess(RequestBuilder builder, RequestProcessor<T> processor) throws IOException {
+  static <T> T wrapAndProcess(RequestBuilder builder, RequestProcessor<T> processor) throws IOException {
     // hack-around for class loader lock in sun.net.www.protocol.http.NegotiateAuthentication (IDEA-131621)
     ClassLoader oldClassLoader = Thread.currentThread().getContextClassLoader();
     Thread.currentThread().setContextClassLoader(new URLClassLoader(new URL[0], oldClassLoader));
@@ -255,7 +114,7 @@ public final class HttpRequests {
   }
 
   @NotNull
-  private static Charset getCharset(@NotNull Request request) throws IOException {
+  static Charset getCharset(@NotNull Request request) throws IOException {
     String contentEncoding = request.getConnection().getContentEncoding();
     if (contentEncoding != null) {
       try {
@@ -267,7 +126,7 @@ public final class HttpRequests {
     return CharsetToolkit.UTF8_CHARSET;
   }
 
-  private static <T> T process(final RequestBuilder builder, RequestProcessor<T> processor) throws IOException {
+  static <T> T process(final RequestBuilder builder, RequestProcessor<T> processor) throws IOException {
     class RequestImpl implements Request {
       private URLConnection myConnection;
       private InputStream myInputStream;
