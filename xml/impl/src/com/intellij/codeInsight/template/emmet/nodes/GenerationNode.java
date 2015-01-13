cbf3e72618720491614d6c4a4cd3f92b4436bdcb
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2015-01-13 14:07:25 +0300

    Emmet: use language instead of file type while creating file in order to avoid language substitutors

--- xml/impl/src/com/intellij/codeInsight/template/emmet/nodes/GenerationNode.java
+++ xml/impl/src/com/intellij/codeInsight/template/emmet/nodes/GenerationNode.java
@@ -28,8 +28,8 @@ import com.intellij.codeInsight.template.emmet.generators.XmlZenCodingGeneratorI
 import com.intellij.codeInsight.template.emmet.generators.ZenCodingGenerator;
 import com.intellij.codeInsight.template.emmet.tokens.TemplateToken;
 import com.intellij.codeInsight.template.impl.TemplateImpl;
-import com.intellij.ide.highlighter.HtmlFileType;
 import com.intellij.injected.editor.DocumentWindowImpl;
+import com.intellij.lang.html.HTMLLanguage;
 import com.intellij.lang.xml.XMLLanguage;
 import com.intellij.openapi.command.undo.UndoConstants;
 import com.intellij.openapi.editor.Document;
@@ -282,7 +282,7 @@ public class GenerationNode extends UserDataHolderBase {
 
     final XmlFile xmlFile = token.getFile();
     PsiFileFactory fileFactory = PsiFileFactory.getInstance(xmlFile.getProject());
-    XmlFile dummyFile = (XmlFile)fileFactory.createFileFromText("dummy.html", HtmlFileType.INSTANCE, xmlFile.getText());
+    XmlFile dummyFile = (XmlFile)fileFactory.createFileFromText(HTMLLanguage.INSTANCE, xmlFile.getText());
     final XmlTag tag = dummyFile.getRootTag();
     if (tag != null) {
       
@@ -308,8 +308,7 @@ public class GenerationNode extends UserDataHolderBase {
       }
       XmlTag tag1 = hasChildren ? expandEmptyTagIfNecessary(tag) : tag;
       setAttributeValues(tag1, attributes, callback);
-      XmlFile physicalFile = (XmlFile)fileFactory.createFileFromText("dummy.html", HtmlFileType.INSTANCE, tag1.getContainingFile().getText(),
-                                                                     LocalTimeCounter.currentTime(), true);
+      XmlFile physicalFile = (XmlFile)fileFactory.createFileFromText(HTMLLanguage.INSTANCE, tag1.getContainingFile().getText());
       VirtualFile vFile = physicalFile.getVirtualFile();
       if (vFile != null) {
         vFile.putUserData(UndoConstants.DONT_RECORD_UNDO, Boolean.TRUE);
