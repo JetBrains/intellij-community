0d8f676acaca8c1f88c237cdd152b418b440e9f5
Dmitry Jemerov <yole@jetbrains.com>
2015-01-13 14:24:56 +0100

    optimize Kotlin coverage performance by avoiding calls to getContainingFile() on AbstractLightClass instances

--- plugins/coverage/src/com/intellij/coverage/PackageAnnotator.java
+++ plugins/coverage/src/com/intellij/coverage/PackageAnnotator.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.intellij.coverage;
 
 import com.intellij.openapi.application.ApplicationManager;
@@ -300,7 +315,7 @@ public class PackageAnnotator {
                 JavaPsiFacade.getInstance(myManager.getProject()).findClass(toplevelClassSrcFQName, GlobalSearchScope.moduleScope(module));
               if (aClass == null || !aClass.isValid()) return Boolean.FALSE;
               psiClassRef.set(aClass);
-              containingFileRef.set(aClass.getContainingFile().getVirtualFile());
+              containingFileRef.set(aClass.getNavigationElement().getContainingFile().getVirtualFile());
               if (containingFileRef.isNull()) {
                 LOG.info("No virtual file found for: " + aClass);
                 return null;
