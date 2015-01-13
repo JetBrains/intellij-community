c616cf0f0eb487d5627e3413c1ce267113961c5d
Aleksey Pivovarov <AMPivovarov@gmail.com>
2015-01-13 20:39:09 +0300

    IDEA-66963 IDEA-84000 vcs: annotate from annotate
    allow to annotate both: previous and selected revisions

--- platform/vcs-impl/src/com/intellij/openapi/vcs/actions/AnnotateRevisionAction.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/actions/AnnotateRevisionAction.java
@@ -1,6 +1,5 @@
 package com.intellij.openapi.vcs.actions;
 
-import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.vcs.AbstractVcs;
@@ -10,44 +9,30 @@ import com.intellij.openapi.vcs.annotate.FileAnnotation;
 import com.intellij.openapi.vcs.annotate.LineNumberListener;
 import com.intellij.openapi.vcs.history.VcsFileRevision;
 import com.intellij.openapi.vcs.history.VcsFileRevisionEx;
-import com.intellij.openapi.vcs.history.VcsRevisionNumber;
 import com.intellij.openapi.vcs.vfs.VcsFileSystem;
 import com.intellij.openapi.vcs.vfs.VcsVirtualFile;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.containers.HashMap;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
+import javax.swing.*;
 import java.util.List;
-import java.util.Map;
 
-class AnnotateRevisionAction extends AnnotateRevisionActionBase implements DumbAware, LineNumberListener {
+abstract class AnnotateRevisionAction extends AnnotateRevisionActionBase implements DumbAware, LineNumberListener {
   @NotNull private final FileAnnotation myAnnotation;
   @NotNull private final AbstractVcs myVcs;
 
-  @NotNull private final List<VcsFileRevision> myRevisions;
-
   private int currentLine;
 
-  public AnnotateRevisionAction(@NotNull FileAnnotation annotation, @NotNull AbstractVcs vcs) {
-    super("Annotate revision", "Annotate selected revision in new tab", AllIcons.Actions.Annotate);
+  public AnnotateRevisionAction(@Nullable String text, @Nullable String description, @Nullable Icon icon,
+                                @NotNull FileAnnotation annotation, @NotNull AbstractVcs vcs) {
+    super(text, description, icon);
     myAnnotation = annotation;
     myVcs = vcs;
-
-    Map<VcsRevisionNumber, VcsFileRevision> map = new HashMap<VcsRevisionNumber, VcsFileRevision>();
-    List<VcsFileRevision> revisions = myAnnotation.getRevisions();
-    if (revisions != null) {
-      for (VcsFileRevision revision : revisions) {
-        map.put(revision.getRevisionNumber(), revision);
-      }
-    }
-
-    myRevisions = new ArrayList<VcsFileRevision>(myAnnotation.getLineCount());
-    for (int i = 0; i < myAnnotation.getLineCount(); i++) {
-      myRevisions.add(map.get(myAnnotation.getLineRevisionNumber(i)));
-    }
   }
+
+  @NotNull
+  protected abstract List<VcsFileRevision> getRevisions();
 
   @Nullable
   protected AbstractVcs getVcs(@NotNull AnActionEvent e) {
@@ -68,8 +53,10 @@ class AnnotateRevisionAction extends AnnotateRevisionActionBase implements DumbA
   @Nullable
   @Override
   protected VcsFileRevision getFileRevision(@NotNull AnActionEvent e) {
-    if (currentLine < 0 || currentLine >= myRevisions.size()) return null;
-    return myRevisions.get(currentLine);
+    List<VcsFileRevision> revisions = getRevisions();
+
+    if (currentLine < 0 || currentLine >= revisions.size()) return null;
+    return revisions.get(currentLine);
   }
 
   @Override
