508efc107e5f1bbab8ff74845139adf3b5d9e8e7
Aleksey Pivovarov <AMPivovarov@gmail.com>
2015-01-13 21:13:43 +0300

    diff: svn - implement for specific Changes

---
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/difftool/SvnPhantomChangeDiffRequestProvider.java
@@ -0,0 +1,94 @@
+package org.jetbrains.idea.svn.difftool;
+
+import com.intellij.openapi.progress.ProcessCanceledException;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.UserDataHolder;
+import com.intellij.openapi.util.UserDataHolderBase;
+import com.intellij.openapi.util.diff.api.FrameDiffTool;
+import com.intellij.openapi.util.diff.chains.DiffRequestPresentableException;
+import com.intellij.openapi.util.diff.requests.DiffRequest;
+import com.intellij.openapi.vcs.changes.Change;
+import com.intellij.openapi.vcs.changes.actions.diff.ChangeDiffRequestPresentable;
+import com.intellij.openapi.vcs.changes.actions.diff.ChangeDiffRequestProvider;
+import com.intellij.vcsUtil.UIVcsUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public class SvnPhantomChangeDiffRequestProvider implements ChangeDiffRequestProvider {
+  @Override
+  public boolean canCreate(@NotNull Project project, @NotNull Change change) {
+    return change.isPhantom();
+  }
+
+  @NotNull
+  @Override
+  public DiffRequest process(@NotNull ChangeDiffRequestPresentable presentable,
+                             @NotNull UserDataHolder context,
+                             @NotNull ProgressIndicator indicator) throws DiffRequestPresentableException, ProcessCanceledException {
+    indicator.checkCanceled();
+    return new SvnPhantomDiffRequest(presentable.getChange());
+  }
+
+  public static class SvnPhantomDiffRequest extends UserDataHolderBase implements DiffRequest {
+    @NotNull private final Change myChange;
+
+    public SvnPhantomDiffRequest(@NotNull Change change) {
+      myChange = change;
+    }
+
+    @Nullable
+    @Override
+    public String getTitle() {
+      return ChangeDiffRequestPresentable.getRequestTitle(myChange.getBeforeRevision(), myChange.getAfterRevision());
+    }
+
+    @Override
+    public void onAssigned(boolean isAssigned) {
+    }
+  }
+
+  public static class SvnPhantomDiffTool implements FrameDiffTool {
+    @NotNull
+    @Override
+    public String getName() {
+      return "SVN Phantom Changes Viewer";
+    }
+
+    @Override
+    public boolean canShow(@NotNull DiffContext context, @NotNull DiffRequest request) {
+      return request instanceof SvnPhantomDiffRequest;
+    }
+
+    @NotNull
+    @Override
+    public DiffViewer createComponent(@NotNull DiffContext context, @NotNull DiffRequest request) {
+      return new DiffViewer() {
+        @NotNull
+        @Override
+        public JComponent getComponent() {
+          return UIVcsUtil.infoPanel("Technical record",
+                                     "This change is recorded because its target file was deleted,\nand some parent directory was copied (or moved) into the new place.");
+        }
+
+        @Nullable
+        @Override
+        public JComponent getPreferredFocusedComponent() {
+          return null;
+        }
+
+        @NotNull
+        @Override
+        public ToolbarComponents init() {
+          return new ToolbarComponents();
+        }
+
+        @Override
+        public void dispose() {
+        }
+      };
+    }
+  }
+}
---
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/difftool/SvnTreeConflictDiffRequestProvider.java
@@ -0,0 +1,123 @@
+package org.jetbrains.idea.svn.difftool;
+
+import com.intellij.openapi.progress.BackgroundTaskQueue;
+import com.intellij.openapi.progress.ProcessCanceledException;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.UserDataHolder;
+import com.intellij.openapi.util.UserDataHolderBase;
+import com.intellij.openapi.util.diff.api.FrameDiffTool;
+import com.intellij.openapi.util.diff.chains.DiffRequestPresentableException;
+import com.intellij.openapi.util.diff.impl.ModifiablePanel;
+import com.intellij.openapi.util.diff.requests.DiffRequest;
+import com.intellij.openapi.vcs.changes.Change;
+import com.intellij.openapi.vcs.changes.actions.diff.ChangeDiffRequestPresentable;
+import com.intellij.openapi.vcs.changes.actions.diff.ChangeDiffRequestProvider;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.idea.svn.ConflictedSvnChange;
+import org.jetbrains.idea.svn.treeConflict.TreeConflictRefreshablePanel;
+
+import javax.swing.*;
+
+public class SvnTreeConflictDiffRequestProvider implements ChangeDiffRequestProvider {
+  @Override
+  public boolean canCreate(@NotNull Project project, @NotNull Change change) {
+    return change instanceof ConflictedSvnChange && ((ConflictedSvnChange)change).getConflictState().isTree();
+  }
+
+  @NotNull
+  @Override
+  public DiffRequest process(@NotNull ChangeDiffRequestPresentable presentable,
+                             @NotNull UserDataHolder context,
+                             @NotNull ProgressIndicator indicator) throws DiffRequestPresentableException, ProcessCanceledException {
+    return new SvnTreeConflictDiffRequest(((ConflictedSvnChange)presentable.getChange()));
+  }
+
+  public static class SvnTreeConflictDiffRequest extends UserDataHolderBase implements DiffRequest {
+    @NotNull private final ConflictedSvnChange myChange;
+
+    public SvnTreeConflictDiffRequest(@NotNull ConflictedSvnChange change) {
+      myChange = change;
+    }
+
+    @NotNull
+    public ConflictedSvnChange getChange() {
+      return myChange;
+    }
+
+    @Nullable
+    @Override
+    public String getTitle() {
+      return ChangeDiffRequestPresentable.getRequestTitle(myChange.getBeforeRevision(), myChange.getAfterRevision());
+    }
+
+    @Override
+    public void onAssigned(boolean isAssigned) {
+    }
+  }
+
+  public static class SvnTreeConflictDiffTool implements FrameDiffTool {
+    @NotNull
+    @Override
+    public String getName() {
+      return "SVN Phantom Changes Viewer";
+    }
+
+    @Override
+    public boolean canShow(@NotNull DiffContext context, @NotNull DiffRequest request) {
+      return request instanceof SvnTreeConflictDiffRequest;
+    }
+
+    @NotNull
+    @Override
+    public DiffViewer createComponent(@NotNull DiffContext context, @NotNull DiffRequest request) {
+      return new SvnTreeConflictDiffViewer(context, (SvnTreeConflictDiffRequest)request);
+    }
+  }
+
+  private static class SvnTreeConflictDiffViewer implements FrameDiffTool.DiffViewer {
+    @NotNull private final FrameDiffTool.DiffContext myContext;
+    @NotNull private final SvnTreeConflictDiffRequest myRequest;
+    @NotNull private final ModifiablePanel myPanel = new ModifiablePanel();
+
+    @NotNull private final BackgroundTaskQueue myQueue;
+    @NotNull private final TreeConflictRefreshablePanel myDelegate;
+
+    public SvnTreeConflictDiffViewer(@NotNull FrameDiffTool.DiffContext context, @NotNull SvnTreeConflictDiffRequest request) {
+      myContext = context;
+      myRequest = request;
+
+      myQueue = new BackgroundTaskQueue(myContext.getProject(), "Loading change details");
+
+      // We don't need to listen on File/Document, because panel always will be the same for a single change (@see myDelegate.isStillValid())
+      // And if Change will change - we'll create new DiffRequest and DiffViewer
+      myDelegate =
+        new TreeConflictRefreshablePanel(myContext.getProject(), "Loading tree conflict details", myQueue, myRequest.getChange());
+      myPanel.setContent(myDelegate.getPanel());
+    }
+
+    @NotNull
+    @Override
+    public JComponent getComponent() {
+      return myPanel;
+    }
+
+    @Nullable
+    @Override
+    public JComponent getPreferredFocusedComponent() {
+      return myPanel;
+    }
+
+    @NotNull
+    @Override
+    public FrameDiffTool.ToolbarComponents init() {
+      return new FrameDiffTool.ToolbarComponents();
+    }
+
+    @Override
+    public void dispose() {
+      myQueue.clear();
+    }
+  }
+}
