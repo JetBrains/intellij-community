f6072a5836b0229ee693ec0ccbcde708ac3533e1
peter <peter@jetbrains.com>
2015-01-14 18:58:41 +0100

    diagnostics and a possible fix for PathMacroManagerTest

--- platform/platform-tests/testSrc/com/intellij/openapi/components/impl/PathMacroManagerTest.java
+++ platform/platform-tests/testSrc/com/intellij/openapi/components/impl/PathMacroManagerTest.java
@@ -52,7 +52,7 @@ import static org.junit.Assert.assertEquals;
 @RunWith(JMock.class)
 public class PathMacroManagerTest {
   private static final String APP_HOME = FileUtil.toSystemIndependentName(PathManager.getHomePath());
-  private static final String USER_HOME = FileUtil.toSystemIndependentName(StringUtil.trimEnd(SystemProperties.getUserHome(), "/"));
+  private static final String USER_HOME = StringUtil.trimEnd(FileUtil.toSystemIndependentName(SystemProperties.getUserHome()), "/");
 
   private Module myModule;
   private ProjectEx myProject;
@@ -213,7 +213,7 @@ public class PathMacroManagerTest {
       String[] split = s.split(" -> ");
       String path = split[0];
       String replaced = split[1];
-      assertEquals(replaced, map.substitute(path, true));
+      assertEquals("For " + path, replaced, map.substitute(path, true));
     }
   }
 
