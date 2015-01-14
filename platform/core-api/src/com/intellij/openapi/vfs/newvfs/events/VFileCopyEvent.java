6533f90f55f794fd504990307e53709cda22552b
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2015-01-14 14:20:25 +0100

    Cleanup (formatting)

--- platform/core-api/src/com/intellij/openapi/vfs/newvfs/events/VFileCopyEvent.java
+++ platform/core-api/src/com/intellij/openapi/vfs/newvfs/events/VFileCopyEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,10 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-/*
- * @author max
- */
 package com.intellij.openapi.vfs.newvfs.events;
 
 import com.intellij.openapi.vfs.VirtualFile;
@@ -24,6 +20,9 @@ import com.intellij.openapi.vfs.VirtualFileSystem;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * @author max
+ */
 public class VFileCopyEvent extends VFileEvent {
   private final VirtualFile myFile;
   private final VirtualFile myNewParent;
