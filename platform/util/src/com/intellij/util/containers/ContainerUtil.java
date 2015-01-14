8344c098997c7eb41c9663d5f55b21e40ad5e0bb
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2015-01-14 16:18:40 +0100

    Cleanup (parameterless method to avoid implicit array creation)

--- platform/util/src/com/intellij/util/containers/ContainerUtil.java
+++ platform/util/src/com/intellij/util/containers/ContainerUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -212,6 +212,12 @@ public class ContainerUtil extends ContainerUtilRt {
         return size;
       }
     };
+  }
+
+  @NotNull
+  @Contract(pure=true)
+  public static <T> List<T> newSmartList() {
+    return new SmartList<T>();
   }
 
   @NotNull
