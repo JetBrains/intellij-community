a431cda328fa793a77a77c107fcb72fd3c503597
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2015-01-15 14:06:32 +0300

    [log] Add hover action to CollapsedActionManager.

--- platform/vcs-log/graph/src/com/intellij/vcs/log/graph/collapsing/CollapsedLinearGraphController.java
+++ platform/vcs-log/graph/src/com/intellij/vcs/log/graph/collapsing/CollapsedLinearGraphController.java
@@ -15,23 +15,54 @@
  */
 package com.intellij.vcs.log.graph.collapsing;
 
+import com.intellij.openapi.util.Condition;
+import com.intellij.openapi.util.Pair;
+import com.intellij.util.containers.ContainerUtil;
 import com.intellij.vcs.log.graph.api.LinearGraph;
+import com.intellij.vcs.log.graph.api.elements.GraphEdge;
+import com.intellij.vcs.log.graph.api.elements.GraphElement;
+import com.intellij.vcs.log.graph.api.elements.GraphNode;
 import com.intellij.vcs.log.graph.api.permanent.PermanentGraphInfo;
 import com.intellij.vcs.log.graph.api.printer.PrintElementWithGraphElement;
 import com.intellij.vcs.log.graph.impl.facade.CascadeLinearGraphController;
 import com.intellij.vcs.log.graph.impl.facade.GraphChanges;
-import com.intellij.vcs.log.graph.utils.UnsignedBitSet;
+import com.intellij.vcs.log.graph.impl.visible.LinearFragmentGenerator;
+import com.intellij.vcs.log.graph.utils.LinearGraphUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Collections;
+import java.util.Set;
 
 public class CollapsedLinearGraphController extends CascadeLinearGraphController {
   @NotNull
   private CollapsedGraph myCollapsedGraph;
+  @NotNull
+  private Set<GraphElement> mySelectedElements = Collections.emptySet();
+  @NotNull
+  private Set<Integer> mySelectedNodes = Collections.emptySet();
+  @NotNull
+  private LinearFragmentGenerator myLinearFragmentGenerator;
+  @NotNull
+  private FragmentGenerator myFragmentGenerator;
 
   protected CollapsedLinearGraphController(@NotNull CascadeLinearGraphController delegateLinearGraphController,
-                                           @NotNull PermanentGraphInfo permanentGraphInfo) {
+                                           @NotNull final PermanentGraphInfo permanentGraphInfo) {
     super(delegateLinearGraphController, permanentGraphInfo);
     myCollapsedGraph = CollapsedGraph.newInstance(getDelegateLinearGraphController().getCompiledGraph(), null);
+    myLinearFragmentGenerator = new LinearFragmentGenerator(myCollapsedGraph.getCompiledGraph(), new Condition<Integer>() {
+      @Override
+      public boolean value(Integer nodeIndex) {
+        int nodeId = myCollapsedGraph.getCompiledGraph().getGraphNode(nodeIndex).getNodeId();
+        return permanentGraphInfo.getNotCollapsedNodes().value(nodeId);
+      }
+    });
+    myFragmentGenerator = new FragmentGenerator(LinearGraphUtils.asLiteLinearGraph(myCollapsedGraph.getCompiledGraph()), new Condition<Integer>() {
+      @Override
+      public boolean value(Integer integer) {
+        return false;
+      }
+    });
   }
 
   @NotNull
@@ -62,7 +93,19 @@ public class CollapsedLinearGraphController extends CascadeLinearGraphController
   }
 
   @Override
-  protected boolean elementIsSelected(@NotNull PrintElementWithGraphElement printElement) {
+  protected boolean elementIsSelected(@NotNull PrintElementWithGraphElement printElement) { // todo fix it index -> nodeId
+    GraphElement graphElement = printElement.getGraphElement();
+    if (mySelectedElements.contains(graphElement)) return true;
+
+    if (graphElement instanceof GraphNode) {
+      return mySelectedNodes.contains(((GraphNode)graphElement).getNodeIndex());
+    }
+
+    if (graphElement instanceof GraphEdge) {
+      Pair<Integer, Integer> normalEdge = LinearGraphUtils.asNormalEdge((GraphEdge)graphElement);
+      return normalEdge != null && mySelectedNodes.contains(normalEdge.first) && mySelectedNodes.contains(normalEdge.second);
+    }
+
     return false;
   }
 
@@ -71,4 +114,23 @@ public class CollapsedLinearGraphController extends CascadeLinearGraphController
     return myCollapsedGraph;
   }
 
+  public void setSelectedElements(@NotNull Set<GraphElement> selectedElements) {
+    mySelectedElements = selectedElements;
+    mySelectedNodes = Collections.emptySet();
+  }
+
+  public void setSelectedNodes(@NotNull Set<Integer> selectedNodes) {
+    mySelectedElements = Collections.emptySet();
+    mySelectedNodes = selectedNodes;
+  }
+
+  @NotNull
+  protected LinearFragmentGenerator getLinearFragmentGenerator() {
+    return myLinearFragmentGenerator;
+  }
+
+  @NotNull
+  public FragmentGenerator getFragmentGenerator() {
+    return myFragmentGenerator;
+  }
 }
