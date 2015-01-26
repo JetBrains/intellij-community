84cdc8a88f1cd371374cef65abe8f5d26623a2be
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2015-01-26 19:15:52 +0300

    fixed StudyExecutor extension point
    It was renamed earlier

--- python/educational/interactive-learning/src/com/jetbrains/edu/learning/StudyExecutor.java
+++ python/educational/interactive-learning/src/com/jetbrains/edu/learning/StudyExecutor.java
@@ -27,7 +27,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public interface StudyExecutor {
-  LanguageExtension<StudyExecutor> INSTANCE = new LanguageExtension<StudyExecutor>("Edu.StudyUtils");
+  LanguageExtension<StudyExecutor> INSTANCE = new LanguageExtension<StudyExecutor>("Edu.StudyExecutor");
 
   @Nullable
   Sdk findSdk(@NotNull final Project project);
