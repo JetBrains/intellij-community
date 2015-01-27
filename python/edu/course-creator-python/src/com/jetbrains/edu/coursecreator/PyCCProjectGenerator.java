4b1906547b44dc9c269e75d9d4db019bcad55e92
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2015-01-27 17:27:16 +0300

    merge conflict (renamed method createLesson)

--- python/edu/course-creator-python/src/com/jetbrains/edu/coursecreator/PyCCProjectGenerator.java
+++ python/edu/course-creator-python/src/com/jetbrains/edu/coursecreator/PyCCProjectGenerator.java
@@ -78,7 +78,7 @@ public class PyCCProjectGenerator extends PythonProjectGenerator implements Dire
         catch (Exception ignored) {
         }
         DirectoryUtil.createSubdirectories("hints", projectDir, "\\/");
-        final PsiDirectory lessonDir = CCCreateLesson.createLesson(projectDir, 1, null, null, course);
+        final PsiDirectory lessonDir = CCCreateLesson.createLessonDir(projectDir, 1, null, null, course);
         CCCreateTask.createTask(null, project, lessonDir, false);
       }
     }.execute();
