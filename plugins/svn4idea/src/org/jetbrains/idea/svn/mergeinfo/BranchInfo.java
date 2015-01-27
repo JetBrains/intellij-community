JetBrains/intellij-community

commit 5929bd8387b56502113d934e120b38a7ee490d6c

IDEA-22730 (Improve support for common merging/integration use cases)

plugins/svn4idea/src/org/jetbrains/idea/svn/mergeinfo/BranchInfo.java
@@ -17,6 +17,7 @@ package org.jetbrains.idea.svn.mergeinfo;
 
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.Comparing;
+import com.intellij.util.containers.MultiMap;
 import org.jetbrains.idea.svn.SvnVcs;
 import org.jetbrains.idea.svn.history.SvnChangeList;
 import org.tmatesoft.svn.core.*;
@@ -50,6 +51,7 @@ public class BranchInfo {
   private final SvnVcs myVcs;
 
   private SvnMergeInfoCache.CopyRevison myCopyRevison;
+  private final MultiMap<Long, String> myPartlyMerged;
 
   public BranchInfo(final SvnVcs vcs, final String repositoryRoot, final String branchUrl, final String trunkUrl,
                      final String trunkCorrected, final SVNWCClient client) {
@@ -61,6 +63,7 @@ public class BranchInfo {
     myClient = client;
 
     myPathMergedMap = new HashMap<String, Set<Long>>();
+    myPartlyMerged = new MultiMap<Long, String>();
     myNonInheritablePathMergedMap = new HashMap<String, Set<Long>>();
 
     myAlreadyCalculatedMap = new HashMap<Long, SvnMergeInfoCache.MergeCheckResult>();
@@ -127,42 +130,40 @@ public class BranchInfo {
     }
     final String subPathUnderBranch = SVNPathUtil.getRelativePath(myBranchUrl, info.getURL().toString());
 
-    final Set<SvnMergeInfoCache.MergeCheckResult> result = new HashSet<SvnMergeInfoCache.MergeCheckResult>();
-    result.addAll(checkPaths(list.getNumber(), list.getAddedPaths(), branchPath, subPathUnderBranch));
-    if (result.contains(SvnMergeInfoCache.MergeCheckResult.NOT_EXISTS)) {
+    final MultiMap<SvnMergeInfoCache.MergeCheckResult, String> result = new MultiMap<SvnMergeInfoCache.MergeCheckResult, String>();
+    checkPaths(list.getNumber(), list.getAddedPaths(), branchPath, subPathUnderBranch, result);
+    if (result.containsKey(SvnMergeInfoCache.MergeCheckResult.NOT_EXISTS)) {
       return SvnMergeInfoCache.MergeCheckResult.NOT_EXISTS;
     }
-    result.addAll(checkPaths(list.getNumber(), list.getDeletedPaths(), branchPath, subPathUnderBranch));
-    if (result.contains(SvnMergeInfoCache.MergeCheckResult.NOT_EXISTS)) {
+    checkPaths(list.getNumber(), list.getDeletedPaths(), branchPath, subPathUnderBranch, result);
+    if (result.containsKey(SvnMergeInfoCache.MergeCheckResult.NOT_EXISTS)) {
       return SvnMergeInfoCache.MergeCheckResult.NOT_EXISTS;
     }
-    result.addAll(checkPaths(list.getNumber(), list.getChangedPaths(), branchPath, subPathUnderBranch));
+    checkPaths(list.getNumber(), list.getChangedPaths(), branchPath, subPathUnderBranch, result);
 
-    if (result.contains(SvnMergeInfoCache.MergeCheckResult.NOT_EXISTS)) {
+    if (result.containsKey(SvnMergeInfoCache.MergeCheckResult.NOT_EXISTS)) {
       return SvnMergeInfoCache.MergeCheckResult.NOT_EXISTS;
-    } else if (result.contains(SvnMergeInfoCache.MergeCheckResult.NOT_MERGED)) {
+    } else if (result.containsKey(SvnMergeInfoCache.MergeCheckResult.NOT_MERGED)) {
+      myPartlyMerged.put(list.getNumber(), result.get(SvnMergeInfoCache.MergeCheckResult.NOT_MERGED));
       return SvnMergeInfoCache.MergeCheckResult.NOT_MERGED;
     }
     return SvnMergeInfoCache.MergeCheckResult.MERGED;
   }
 
-  private List<SvnMergeInfoCache.MergeCheckResult> checkPaths(final long number, final Collection<String> paths,
-                                                              final String branchPath, final String subPathUnderBranch) {
-    final List<SvnMergeInfoCache.MergeCheckResult> result = new ArrayList<SvnMergeInfoCache.MergeCheckResult>();
+  private void checkPaths(final long number, final Collection<String> paths, final String branchPath, final String subPathUnderBranch,
+                          final MultiMap<SvnMergeInfoCache.MergeCheckResult, String> result) {
     final String myTrunkPathCorrespondingToLocalBranchPath = SVNPathUtil.append(myTrunkCorrected, subPathUnderBranch);
     for (String path : paths) {
       final String absoluteInTrunkPath = SVNPathUtil.append(myRepositoryRoot, path);
       if (! absoluteInTrunkPath.startsWith(myTrunkPathCorrespondingToLocalBranchPath)) {
-        result.add(SvnMergeInfoCache.MergeCheckResult.NOT_EXISTS);
-        return result;
+        result.putValue(SvnMergeInfoCache.MergeCheckResult.NOT_EXISTS, path);
       }
       final String relativeToTrunkPath = absoluteInTrunkPath.substring(myTrunkPathCorrespondingToLocalBranchPath.length());
       final String localPathInBranch = new File(branchPath, relativeToTrunkPath).getAbsolutePath();
       
       final SvnMergeInfoCache.MergeCheckResult pathResult = checkPathGoingUp(number, -1, branchPath, localPathInBranch, path, true);
-      result.add(pathResult);
+      result.putValue(pathResult, path);
     }
-    return result;
   }
 
   private SvnMergeInfoCache.MergeCheckResult goUp(final long revisionAsked, final long targetRevision, final String branchRootPath,
@@ -364,5 +365,10 @@ public class BranchInfo {
 
   public boolean isMixedRevisionsFound() {
     return myMixedRevisionsFound;
+  }
+
+  // if nothing, maybe all not merged or merged: here only partly not merged
+  public Collection<String> getNotMergedPaths(final long number) {
+    return myPartlyMerged.get(number);
   }
 }

platform/vcs-api/src/com/intellij/openapi/vcs/CollectionSplitter.java
@@ -1,0 +1,29 @@
+package com.intellij.openapi.vcs;
+
+import java.util.*;
+
+public class CollectionSplitter<T> {
+  private final int myBunchSize;
+
+  public CollectionSplitter(int bunchSize) {
+    myBunchSize = bunchSize;
+  }
+
+  public List<List<T>> split(final Collection<T> in) {
+    if (in.size() <= myBunchSize) return Collections.<List<T>>singletonList(new ArrayList<T>(in));
+
+    final List<List<T>> result = new LinkedList<List<T>>();
+    List<T> piece = new LinkedList<T>();
+    for (T path : in) {
+      if (myBunchSize == piece.size()) {
+        result.add(piece);
+        piece = new LinkedList<T>();
+      }
+      piece.add(path);
+    }
+    if (! piece.isEmpty()) {
+      result.add(piece);
+    }
+    return result;
+  }
+}

platform/vcs-api/src/com/intellij/openapi/vcs/update/FileGroup.java
@@ -51,6 +51,7 @@ public class FileGroup implements JDOMExternalizable {
 
   @NonNls public static final String MODIFIED_ID = "MODIFIED";
   @NonNls public static final String MERGED_WITH_CONFLICT_ID = "MERGED_WITH_CONFLICTS";
+  @NonNls public static final String MERGED_WITH_TREE_CONFLICT = "MERGED_WITH_TREE_CONFLICT";
   @NonNls public static final String MERGED_WITH_PROPERTY_CONFLICT_ID = "MERGED_WITH_PROPERTY_CONFLICT";
   @NonNls public static final String MERGED_ID = "MERGED";
   @NonNls public static final String UNKNOWN_ID = "UNKNOWN";

platform/vcs-api/src/com/intellij/openapi/vcs/update/UpdatedFiles.java
@@ -93,6 +93,8 @@ public class UpdatedFiles implements JDOMExternalizable {
     result.registerGroup(new FileGroup(VcsBundle.message("update.group.name.skipped"), VcsBundle.message("status.group.name.skipped"), false, FileGroup.SKIPPED_ID, false));
 
     result.registerGroup(new FileGroup(VcsBundle.message("update.group.name.merged.with.conflicts"), VcsBundle.message("status.group.name.will.be.merged.with.conflicts"), false, FileGroup.MERGED_WITH_CONFLICT_ID, false));
+    result.registerGroup(new FileGroup(VcsBundle.message("update.group.name.merged.with.tree.conflicts"),
+                                       VcsBundle.message("update.group.name.merged.with.tree.conflicts"), false, FileGroup.MERGED_WITH_TREE_CONFLICT, false));
     //result.registerGroup(new FileGroup(VcsBundle.message("update.group.name.merged.with.property.conflicts"), VcsBundle.message("status.group.name.will.be.merged.with.property.conflicts"), false, FileGroup.MERGED_WITH_PROPERTY_CONFLICT_ID, false));
     result.registerGroup(new FileGroup(VcsBundle.message("update.group.name.merged"), VcsBundle.message("status.group.name.will.be.merged"), false, FileGroup.MERGED_ID, false));
     result.registerGroup(new FileGroup(VcsBundle.message("update.group.name.not.in.repository"), VcsBundle.message("status.group.name.not.in.repository"), true, FileGroup.UNKNOWN_ID, false));

platform/vcs-impl/src/com/intellij/openapi/vcs/changes/committed/CommittedChangeListRenderer.java
@@ -1,0 +1,173 @@
+/*
+ * Copyright 2000-2010 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.openapi.vcs.changes.committed;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.vcs.AbstractVcs;
+import com.intellij.openapi.vcs.CachingCommittedChangesProvider;
+import com.intellij.openapi.vcs.VcsBundle;
+import com.intellij.openapi.vcs.changes.issueLinks.IssueLinkRenderer;
+import com.intellij.openapi.vcs.versionBrowser.CommittedChangeList;
+import com.intellij.ui.ColoredTreeCellRenderer;
+import com.intellij.ui.SimpleTextAttributes;
+
+import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import java.awt.*;
+import java.text.DateFormat;
+import java.util.Date;
+
+public class CommittedChangeListRenderer extends ColoredTreeCellRenderer {
+  private final static DateFormat myDateFormat = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
+  private static final SimpleTextAttributes LINK_ATTRIBUTES = new SimpleTextAttributes(SimpleTextAttributes.STYLE_UNDERLINE, Color.blue);
+  private final IssueLinkRenderer myRenderer;
+  private final java.util.List<CommittedChangeListDecorator> myDecorators;
+  private final Project myProject;
+
+  public CommittedChangeListRenderer(final Project project, final java.util.List<CommittedChangeListDecorator> decorators) {
+    myProject = project;
+    myRenderer = new IssueLinkRenderer(project, this);
+    myDecorators = decorators;
+  }
+
+  public static String getDateOfChangeList(final Date date) {
+    return myDateFormat.format(date);
+  }
+
+  public static Pair<String, Boolean> getDescriptionOfChangeList(final String text) {
+    String description = text;
+    int pos = description.indexOf("\n");
+    if (pos >= 0) {
+      description = description.substring(0, pos).trim();
+      return new Pair<String, Boolean>(description, Boolean.TRUE);
+    }
+    return new Pair<String, Boolean>(description, Boolean.FALSE);
+  }
+
+  public static String truncateDescription(final String initDescription, final FontMetrics fontMetrics, int maxWidth) {
+    String description = initDescription;
+    int descWidth = fontMetrics.stringWidth(description);
+    while(description.length() > 0 && (descWidth > maxWidth)) {
+      description = trimLastWord(description);
+      descWidth = fontMetrics.stringWidth(description + " ");
+    }
+    return description;
+  }
+
+  public void customizeCellRenderer(JTree tree, Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
+    customize(tree, value, selected, expanded, leaf, row, hasFocus);
+  }
+
+  public void customize(JComponent tree, Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
+    DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
+    if (node.getUserObject() instanceof CommittedChangeList) {
+      CommittedChangeList changeList = (CommittedChangeList) node.getUserObject();
+
+      renderChangeList(tree, changeList);
+    }
+    else if (node.getUserObject() != null) {
+      append(node.getUserObject().toString(), SimpleTextAttributes.REGULAR_BOLD_ATTRIBUTES);
+    }
+  }
+
+  public void renderChangeList(JComponent tree, CommittedChangeList changeList) {
+    final Container parent = tree.getParent();
+    int parentWidth = parent == null ? 100 : parent.getWidth() - 44;
+    String date = ", " + getDateOfChangeList(changeList.getCommitDate());
+    final FontMetrics fontMetrics = tree.getFontMetrics(tree.getFont());
+    final FontMetrics boldMetrics = tree.getFontMetrics(tree.getFont().deriveFont(Font.BOLD));
+    int size = fontMetrics.stringWidth(date);
+    size += boldMetrics.stringWidth(changeList.getCommitterName());
+
+    final Pair<String, Boolean> descriptionInfo = getDescriptionOfChangeList(changeList.getName().trim());
+    boolean truncated = descriptionInfo.getSecond().booleanValue();
+    String description = descriptionInfo.getFirst();
+
+    for (CommittedChangeListDecorator decorator : myDecorators) {
+      final Icon icon = decorator.decorate(changeList);
+      if (icon != null) {
+        setIcon(icon);
+      }
+    }
+
+    int descMaxWidth = parentWidth - size - 8;
+    boolean partial = (changeList instanceof ReceivedChangeList) && ((ReceivedChangeList)changeList).isPartial();
+    if (partial) {
+      final String partialMarker = VcsBundle.message("committed.changes.partial.list") + " ";
+      append(partialMarker, SimpleTextAttributes.REGULAR_BOLD_ATTRIBUTES);
+      descMaxWidth -= boldMetrics.stringWidth(partialMarker);
+    }
+
+    int descWidth = fontMetrics.stringWidth(description);
+
+    int numberWidth = 0;
+    final AbstractVcs vcs = changeList.getVcs();
+    if (vcs != null) {
+      final CachingCommittedChangesProvider provider = vcs.getCachingCommittedChangesProvider();
+      if (provider != null && provider.getChangelistTitle() != null) {
+        String number = "#" + changeList.getNumber() + "  ";
+        numberWidth = fontMetrics.stringWidth(number);
+        descWidth += numberWidth;
+        append(number, SimpleTextAttributes.GRAY_ATTRIBUTES);
+      }
+    }
+
+    if (description.length() == 0 && !truncated) {
+      append(VcsBundle.message("committed.changes.empty.comment"), SimpleTextAttributes.GRAYED_ATTRIBUTES);
+      appendAlign(parentWidth - size);
+    }
+    else if (descMaxWidth < 0) {
+      myRenderer.appendTextWithLinks(description);
+    }
+    else if (descWidth < descMaxWidth && !truncated) {
+      myRenderer.appendTextWithLinks(description);
+      appendAlign(parentWidth - size);
+    }
+    else {
+      final String moreMarker = VcsBundle.message("changes.browser.details.marker");
+      int moreWidth = fontMetrics.stringWidth(moreMarker);
+      description = truncateDescription(description, fontMetrics, (descMaxWidth - moreWidth - numberWidth));
+      myRenderer.appendTextWithLinks(description);
+      // we don't have place for changelist number in this case
+      append(" ", SimpleTextAttributes.REGULAR_ATTRIBUTES);
+      append(moreMarker, LINK_ATTRIBUTES, new CommittedChangesTreeBrowser.MoreLauncher(myProject, changeList));
+      appendAlign(parentWidth - size);
+    }
+
+    append(changeList.getCommitterName(), SimpleTextAttributes.REGULAR_BOLD_ATTRIBUTES);
+    append(date, SimpleTextAttributes.REGULAR_ATTRIBUTES);
+  }
+
+  private void appendDescriptionAndNumber(final String description, final String number) {
+    myRenderer.appendTextWithLinks(description);
+    if (number != null) {
+      append(number, SimpleTextAttributes.GRAY_ATTRIBUTES);
+    }
+  }
+
+  private static String trimLastWord(final String description) {
+    int pos = description.trim().lastIndexOf(' ');
+    if (pos >= 0) {
+      return description.substring(0, pos).trim();
+    }
+    return description.substring(0, description.length()-1);
+  }
+
+  public Dimension getPreferredSize() {
+    return new Dimension(2000, super.getPreferredSize().height);
+  }
+}

platform/vcs-impl/src/com/intellij/openapi/vcs/changes/committed/CommittedChangesTreeBrowser.java
@@ -19,25 +19,22 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Splitter;
 import com.intellij.openapi.ui.SplitterProportionsData;
 import com.intellij.openapi.util.Comparing;
-import com.intellij.openapi.util.Pair;
-import com.intellij.openapi.vcs.AbstractVcs;
-import com.intellij.openapi.vcs.CachingCommittedChangesProvider;
-import com.intellij.openapi.vcs.VcsBundle;
 import com.intellij.openapi.vcs.VcsDataKeys;
 import com.intellij.openapi.vcs.changes.Change;
 import com.intellij.openapi.vcs.changes.ChangesUtil;
 import com.intellij.openapi.vcs.changes.ContentRevision;
-import com.intellij.openapi.vcs.changes.issueLinks.IssueLinkRenderer;
 import com.intellij.openapi.vcs.changes.issueLinks.TreeLinkMouseListener;
 import com.intellij.openapi.vcs.versionBrowser.CommittedChangeList;
 import com.intellij.pom.Navigatable;
-import com.intellij.ui.*;
+import com.intellij.ui.IdeBorderFactory;
+import com.intellij.ui.PopupHandler;
+import com.intellij.ui.SimpleTextAttributes;
+import com.intellij.ui.TreeCopyProvider;
 import com.intellij.ui.treeStructure.Tree;
 import com.intellij.ui.treeStructure.actions.CollapseAllAction;
 import com.intellij.ui.treeStructure.actions.ExpandAllAction;
 import com.intellij.util.messages.MessageBusConnection;
 import com.intellij.util.messages.Topic;
-import com.intellij.util.ui.UIUtil;
 import com.intellij.util.ui.tree.TreeUtil;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
@@ -55,7 +52,6 @@ import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreePath;
 import java.awt.*;
 import java.awt.event.ActionListener;
-import java.text.DateFormat;
 import java.util.*;
 import java.util.List;
 
@@ -450,144 +446,11 @@ public class CommittedChangesTreeBrowser extends JPanel implements TypeSafeDataP
     myProject.getMessageBus().syncPublisher(ITEMS_RELOADED).itemsReloaded();
   }
 
-  public static class CommittedChangeListRenderer extends ColoredTreeCellRenderer {
-    private final static DateFormat myDateFormat = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
-    private static final SimpleTextAttributes LINK_ATTRIBUTES = new SimpleTextAttributes(SimpleTextAttributes.STYLE_UNDERLINE, Color.blue);
-    private final IssueLinkRenderer myRenderer;
-    private final List<CommittedChangeListDecorator> myDecorators;
-    private final Project myProject;
-
-    public CommittedChangeListRenderer(final Project project, final List<CommittedChangeListDecorator> decorators) {
-      myProject = project;
-      myRenderer = new IssueLinkRenderer(project, this);
-      myDecorators = decorators;
-    }
-
-    public static String getDateOfChangeList(final Date date) {
-      return myDateFormat.format(date);
-    }
-
-    public static Pair<String, Boolean> getDescriptionOfChangeList(final String text) {
-      String description = text;
-      int pos = description.indexOf("\n");
-      if (pos >= 0) {
-        description = description.substring(0, pos).trim();
-        return new Pair<String, Boolean>(description, Boolean.TRUE);
-      }
-      return new Pair<String, Boolean>(description, Boolean.FALSE);
-    }
-
-    public static String truncateDescription(final String initDescription, final FontMetrics fontMetrics, int maxWidth) {
-      String description = initDescription;
-      int descWidth = fontMetrics.stringWidth(description);
-      while(description.length() > 0 && (descWidth > maxWidth)) {
-        description = trimLastWord(description);
-        descWidth = fontMetrics.stringWidth(description + " ");
-      }
-      return description;
-    }
-
-    public void customizeCellRenderer(JTree tree, Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
-      DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
-      if (node.getUserObject() instanceof CommittedChangeList) {
-        CommittedChangeList changeList = (CommittedChangeList) node.getUserObject();
-
-        final Container parent = tree.getParent();
-        int parentWidth = parent == null ? 100 : parent.getWidth() - 44;
-        String date = ", " + getDateOfChangeList(changeList.getCommitDate());
-        final FontMetrics fontMetrics = tree.getFontMetrics(tree.getFont());
-        final FontMetrics boldMetrics = tree.getFontMetrics(tree.getFont().deriveFont(Font.BOLD));
-        int size = fontMetrics.stringWidth(date);
-        size += boldMetrics.stringWidth(changeList.getCommitterName());
-
-        final Pair<String, Boolean> descriptionInfo = getDescriptionOfChangeList(changeList.getName().trim());
-        boolean truncated = descriptionInfo.getSecond().booleanValue();
-        String description = descriptionInfo.getFirst();
-
-        for (CommittedChangeListDecorator decorator : myDecorators) {
-          final Icon icon = decorator.decorate(changeList);
-          if (icon != null) {
-            setIcon(icon);
-          }
-        }
-
-        int descMaxWidth = parentWidth - size - 8;
-        boolean partial = (changeList instanceof ReceivedChangeList) && ((ReceivedChangeList)changeList).isPartial();
-        if (partial) {
-          final String partialMarker = VcsBundle.message("committed.changes.partial.list") + " ";
-          append(partialMarker, SimpleTextAttributes.REGULAR_BOLD_ATTRIBUTES);
-          descMaxWidth -= boldMetrics.stringWidth(partialMarker);
-        }
-
-        int descWidth = fontMetrics.stringWidth(description);
-
-        int numberWidth = 0;
-        final AbstractVcs vcs = changeList.getVcs();
-        if (vcs != null) {
-          final CachingCommittedChangesProvider provider = vcs.getCachingCommittedChangesProvider();
-          if (provider != null && provider.getChangelistTitle() != null) {
-            String number = "#" + changeList.getNumber() + "  ";
-            numberWidth = fontMetrics.stringWidth(number);
-            descWidth += numberWidth;
-            append(number, SimpleTextAttributes.GRAY_ATTRIBUTES);
-          }
-        }
-
-        if (description.length() == 0 && !truncated) {
-          append(VcsBundle.message("committed.changes.empty.comment"), SimpleTextAttributes.GRAYED_ATTRIBUTES);
-          appendAlign(parentWidth - size);
-        }
-        else if (descMaxWidth < 0) {
-          myRenderer.appendTextWithLinks(description);
-        }
-        else if (descWidth < descMaxWidth && !truncated) {
-          myRenderer.appendTextWithLinks(description);
-          appendAlign(parentWidth - size);
-        }
-        else {
-          final String moreMarker = VcsBundle.message("changes.browser.details.marker");
-          int moreWidth = fontMetrics.stringWidth(moreMarker);
-          description = truncateDescription(description, fontMetrics, (descMaxWidth - moreWidth - numberWidth));
-          myRenderer.appendTextWithLinks(description);
-          // we don't have place for changelist number in this case
-          append(" ", SimpleTextAttributes.REGULAR_ATTRIBUTES);
-          append(moreMarker, LINK_ATTRIBUTES, new MoreLauncher(myProject, changeList));
-          appendAlign(parentWidth - size);
-        }
-
-        append(changeList.getCommitterName(), SimpleTextAttributes.REGULAR_BOLD_ATTRIBUTES);
-        append(date, SimpleTextAttributes.REGULAR_ATTRIBUTES);
-      }
-      else if (node.getUserObject() != null) {
-        append(node.getUserObject().toString(), SimpleTextAttributes.REGULAR_BOLD_ATTRIBUTES);
-      }
-    }
-
-    private void appendDescriptionAndNumber(final String description, final String number) {
-      myRenderer.appendTextWithLinks(description);
-      if (number != null) {
-        append(number, SimpleTextAttributes.GRAY_ATTRIBUTES);
-      }
-    }
-
-    private static String trimLastWord(final String description) {
-      int pos = description.trim().lastIndexOf(' ');
-      if (pos >= 0) {
-        return description.substring(0, pos).trim();
-      }
-      return description.substring(0, description.length()-1);
-    }
-
-    public Dimension getPreferredSize() {
-      return new Dimension(2000, super.getPreferredSize().height);
-    }
-  }
-
-  private static class MoreLauncher implements Runnable {
+  public static class MoreLauncher implements Runnable {
     private final Project myProject;
     private final CommittedChangeList myList;
 
-    private MoreLauncher(final Project project, final CommittedChangeList list) {
+    MoreLauncher(final Project project, final CommittedChangeList list) {
       myProject = project;
       myList = list;
     }

platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/ChangesBrowser.java
@@ -25,7 +25,6 @@ import com.intellij.openapi.vcs.VcsDataKeys;
 import com.intellij.openapi.vcs.changes.*;
 import com.intellij.openapi.vcs.changes.actions.ShowDiffAction;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.ui.SeparatorFactory;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
@@ -57,6 +56,10 @@ public class ChangesBrowser extends JPanel implements TypeSafeDataProvider {
   public void setChangesToDisplay(final List<Change> changes) {
     myChangesToDisplay = changes;
     myViewer.setChangesToDisplay(changes);
+  }
+
+  public void setDecorator(final ChangeNodeDecorator decorator) {
+    myViewer.setChangeDecorator(decorator);
   }
 
   public ChangesBrowser(final Project project, List<? extends ChangeList> changeLists, final List<Change> changes,

commit 06a2f95f3bc24e310c7ffd24c3fe46244e3904f7

svn: Do not check other change list paths if NOT_EXISTS status (that means path does not belong to corresponding branch) is detected for one of them while checking if change list is merged/not merged to given branch (and what paths are not merged)
Such behavior was broken in 5929bd8387b56502113d934e120b38a7ee490d6c and partially changed back in 104d8c7d90e43a282a78cbaaa8e1df1657015caa

plugins/svn4idea/src/org/jetbrains/idea/svn/mergeinfo/BranchInfo.java
@@ -127,22 +127,13 @@ public class BranchInfo {
     if (info == null || info.getURL() == null || !SVNPathUtil.isAncestor(myBranch.getUrl(), info.getURL().toString())) {
       return SvnMergeInfoCache.MergeCheckResult.NOT_MERGED;
     }
+
     final String subPathUnderBranch = SVNPathUtil.getRelativePath(myBranch.getUrl(), info.getURL().toString());
+    MultiMap<SvnMergeInfoCache.MergeCheckResult, String> result = checkPaths(list, branchPath, subPathUnderBranch);
 
-    MultiMap<SvnMergeInfoCache.MergeCheckResult, String> result = MultiMap.create();
-    checkPaths(list.getNumber(), list.getAddedPaths(), branchPath, subPathUnderBranch, result);
     if (result.containsKey(SvnMergeInfoCache.MergeCheckResult.NOT_EXISTS)) {
       return SvnMergeInfoCache.MergeCheckResult.NOT_EXISTS;
     }
-    checkPaths(list.getNumber(), list.getDeletedPaths(), branchPath, subPathUnderBranch, result);
-    if (result.containsKey(SvnMergeInfoCache.MergeCheckResult.NOT_EXISTS)) {
-      return SvnMergeInfoCache.MergeCheckResult.NOT_EXISTS;
-    }
-    checkPaths(list.getNumber(), list.getChangedPaths(), branchPath, subPathUnderBranch, result);
-    if (result.containsKey(SvnMergeInfoCache.MergeCheckResult.NOT_EXISTS)) {
-      return SvnMergeInfoCache.MergeCheckResult.NOT_EXISTS;
-    }
-
     if (result.containsKey(SvnMergeInfoCache.MergeCheckResult.NOT_MERGED)) {
       myPartlyMerged.put(list.getNumber(), result.get(SvnMergeInfoCache.MergeCheckResult.NOT_MERGED));
       return SvnMergeInfoCache.MergeCheckResult.NOT_MERGED;
@@ -150,11 +141,14 @@ public class BranchInfo {
     return SvnMergeInfoCache.MergeCheckResult.MERGED;
   }
 
-  private void checkPaths(final long number, @NotNull Collection<String> paths, @NotNull String branchPath, final String subPathUnderBranch,
-                          @NotNull MultiMap<SvnMergeInfoCache.MergeCheckResult, String> result) {
+  @NotNull
+  private MultiMap<SvnMergeInfoCache.MergeCheckResult, String> checkPaths(@NotNull SvnChangeList list,
+                                                                          @NotNull String branchPath,
+                                                                          final String subPathUnderBranch) {
+    MultiMap<SvnMergeInfoCache.MergeCheckResult, String> result = MultiMap.create();
     String myTrunkPathCorrespondingToLocalBranchPath = SVNPathUtil.append(myInfo.getCurrentBranch().getUrl(), subPathUnderBranch);
 
-    for (String path : paths) {
+    for (String path : ContainerUtil.concat(list.getAddedPaths(), list.getDeletedPaths(), list.getChangedPaths())) {
       String absoluteInTrunkPath = SVNPathUtil.append(myInfo.getRepoUrl(), path);
       SvnMergeInfoCache.MergeCheckResult mergeCheckResult;
 
@@ -166,7 +160,7 @@ public class BranchInfo {
         String localPathInBranch = new File(branchPath, relativeToTrunkPath).getAbsolutePath();
 
         try {
-          mergeCheckResult = checkPathGoingUp(number, -1, branchPath, localPathInBranch, path, true);
+          mergeCheckResult = checkPathGoingUp(list.getNumber(), -1, branchPath, localPathInBranch, path, true);
         }
         catch (VcsException e) {
           LOG.info(e);
@@ -179,7 +173,15 @@ public class BranchInfo {
       }
 
       result.putValue(mergeCheckResult, path);
+
+      // Do not check other paths if NOT_EXISTS result detected as in this case resulting status for whole change list will also be
+      // NOT_EXISTS. And currently we're only interested in not merged paths for change lists with NOT_MERGED status.
+      if (SvnMergeInfoCache.MergeCheckResult.NOT_EXISTS.equals(mergeCheckResult)) {
+        break;
+      }
     }
+
+    return result;
   }
 
   @NotNull