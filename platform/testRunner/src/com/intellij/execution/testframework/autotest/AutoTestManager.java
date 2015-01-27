b646e6022f45f5d3f5dc4c48e2a8c242d172095b
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2015-01-27 17:24:37 +0300

    auto test manager: rerun on process termination if changes were made during test run

--- platform/testRunner/src/com/intellij/execution/testframework/autotest/AutoTestManager.java
+++ platform/testRunner/src/com/intellij/execution/testframework/autotest/AutoTestManager.java
@@ -16,13 +16,18 @@
 package com.intellij.execution.testframework.autotest;
 
 import com.intellij.execution.DelayedDocumentWatcher;
+import com.intellij.execution.process.ProcessAdapter;
+import com.intellij.execution.process.ProcessEvent;
 import com.intellij.execution.process.ProcessHandler;
+import com.intellij.execution.process.ProcessListener;
 import com.intellij.execution.runners.ExecutionUtil;
 import com.intellij.execution.ui.RunContentDescriptor;
 import com.intellij.ide.DataManager;
 import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
@@ -32,6 +37,8 @@ import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.content.Content;
 import com.intellij.util.Consumer;
 import com.intellij.util.containers.WeakHashMap;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.util.Collections;
@@ -43,6 +50,7 @@ import java.util.Set;
 public class AutoTestManager {
   static final Key<Boolean> AUTOTESTABLE = Key.create("auto.test.manager.supported");
   public static final String AUTO_TEST_MANAGER_DELAY = "auto.test.manager.delay";
+  private static final Key<ProcessListener> ON_TERMINATION_RESTARTER_KEY = Key.create("auto.test.manager.on.termination.restarter");
 
   private final Project myProject;
 
@@ -63,11 +71,11 @@ public class AutoTestManager {
   }
 
   private DelayedDocumentWatcher createWatcher() {
-    return new DelayedDocumentWatcher(myProject, myDelay, new Consumer<Set<VirtualFile>>() {
+    return new DelayedDocumentWatcher(myProject, myDelay, new Consumer<Integer>() {
       @Override
-      public void consume(Set<VirtualFile> files) {
+      public void consume(Integer modificationStamp) {
         for (Content content : myEnabledDescriptors) {
-          runAutoTest(content);
+          runAutoTest(content, modificationStamp, myDocumentWatcher);
         }
       }
     }, new Condition<VirtualFile>() {
@@ -79,16 +87,27 @@ public class AutoTestManager {
     });
   }
 
-  public void setAutoTestEnabled(RunContentDescriptor descriptor, boolean enabled) {
+  public void setAutoTestEnabled(@NotNull RunContentDescriptor descriptor, boolean enabled) {
     Content content = descriptor.getAttachedContent();
     if (enabled) {
       myEnabledDescriptors.add(content);
       myDocumentWatcher.activate();
     }
     else {
+      clearRestarterListener(descriptor.getProcessHandler());
       myEnabledDescriptors.remove(content);
       if (myEnabledDescriptors.isEmpty()) {
         myDocumentWatcher.deactivate();
+      }
+    }
+  }
+
+  private static void clearRestarterListener(@Nullable ProcessHandler processHandler) {
+    if (processHandler != null) {
+      ProcessListener restarterListener = ON_TERMINATION_RESTARTER_KEY.get(processHandler, null);
+      if (restarterListener != null) {
+        processHandler.removeProcessListener(restarterListener);
+        ON_TERMINATION_RESTARTER_KEY.set(processHandler, null);
       }
     }
   }
@@ -97,7 +116,7 @@ public class AutoTestManager {
     return myEnabledDescriptors.contains(descriptor.getAttachedContent());
   }
 
-  private static void runAutoTest(Content content) {
+  private static void runAutoTest(@NotNull Content content, int modificationStamp, @NotNull DelayedDocumentWatcher documentWatcher) {
     JComponent component = content.getComponent();
     if (component != null) {
       DataContext dataContext = DataManager.getInstance().getDataContext(component);
@@ -105,14 +124,46 @@ public class AutoTestManager {
       if (descriptor != null) {
         ProcessHandler processHandler = descriptor.getProcessHandler();
         if (processHandler != null && !processHandler.isProcessTerminated()) {
-          return;
+          scheduleRestartOnTermination(descriptor, content, processHandler, modificationStamp, documentWatcher);
         }
-
-        descriptor.setActivateToolWindowWhenAdded(false);
-        descriptor.setReuseToolWindowActivation(true);
-        ExecutionUtil.restart(content);
+        else {
+          restart(descriptor, content);
+        }
       }
     }
+  }
+
+  private static void scheduleRestartOnTermination(@NotNull final RunContentDescriptor descriptor,
+                                                   @NotNull final Content content,
+                                                   @NotNull final ProcessHandler processHandler,
+                                                   final int modificationStamp,
+                                                   @NotNull final DelayedDocumentWatcher documentWatcher) {
+    ProcessListener restarterListener = ON_TERMINATION_RESTARTER_KEY.get(processHandler);
+    if (restarterListener != null) {
+      return;
+    }
+    restarterListener = new ProcessAdapter() {
+      @Override
+      public void processTerminated(ProcessEvent event) {
+        clearRestarterListener(processHandler);
+        ApplicationManager.getApplication().invokeLater(new Runnable() {
+          @Override
+          public void run() {
+            if (documentWatcher.isUpToDate(modificationStamp)) {
+              restart(descriptor, content);
+            }
+          }
+        }, ModalityState.any());
+      }
+    };
+    ON_TERMINATION_RESTARTER_KEY.set(processHandler, restarterListener);
+    processHandler.addProcessListener(restarterListener);
+  }
+
+  private static void restart(@NotNull RunContentDescriptor descriptor, @NotNull Content content) {
+    descriptor.setActivateToolWindowWhenAdded(false);
+    descriptor.setReuseToolWindowActivation(true);
+    ExecutionUtil.restart(content);
   }
 
   int getDelay() {
