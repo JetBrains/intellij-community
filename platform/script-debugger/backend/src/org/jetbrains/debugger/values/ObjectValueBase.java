51b3dbc8c5b71cc741a7e454363afdbf76c4b67e
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2015-01-29 12:57:14 +0100

    continue WEB-14879 Firefox debugger: infinite 'Collecting data' for some variables

--- platform/script-debugger/backend/src/org/jetbrains/debugger/values/ObjectValueBase.java
+++ platform/script-debugger/backend/src/org/jetbrains/debugger/values/ObjectValueBase.java
@@ -50,44 +50,49 @@ public abstract class ObjectValueBase<VALUE_LOADER extends ValueManager> extends
         @NotNull
         @Override
         public Promise<List<Variable>> fun(List<Variable> variables) {
-          final List<Variable> properties = new SmartList<Variable>();
-          int getterCount = 0;
-          for (Variable property : variables) {
-            if (!property.isReadable() || !names.contains(property.getName())) {
-              continue;
-            }
-
-            if (!properties.isEmpty()) {
-              Collections.sort(properties, new Comparator<Variable>() {
-                @Override
-                public int compare(@NotNull Variable o1, @NotNull Variable o2) {
-                  return names.indexOf(o1.getName()) - names.indexOf(o2.getName());
-                }
-              });
-            }
-
-            properties.add(property);
-            if (property.getValue() == null) {
-              getterCount++;
-            }
-          }
-
-          if (getterCount == 0) {
-            return Promise.resolve(properties);
-          }
-          else {
-            List<Promise<?>> promises = new SmartList<Promise<?>>();
-            for (Variable variable : properties) {
-              if (variable.getValue() == null) {
-                ValueModifier valueModifier = variable.getValueModifier();
-                assert valueModifier != null;
-                promises.add(valueModifier.evaluateGet(variable, evaluateContext));
-              }
-            }
-            return Promise.all(promises, properties);
-          }
+          return getSpecifiedProperties(variables, names, evaluateContext);
         }
       });
+  }
+
+  @NotNull
+  protected static Promise<List<Variable>> getSpecifiedProperties(@NotNull List<Variable> variables, @NotNull final List<String> names, @NotNull EvaluateContext evaluateContext) {
+    final List<Variable> properties = new SmartList<Variable>();
+    int getterCount = 0;
+    for (Variable property : variables) {
+      if (!property.isReadable() || !names.contains(property.getName())) {
+        continue;
+      }
+
+      if (!properties.isEmpty()) {
+        Collections.sort(properties, new Comparator<Variable>() {
+          @Override
+          public int compare(@NotNull Variable o1, @NotNull Variable o2) {
+            return names.indexOf(o1.getName()) - names.indexOf(o2.getName());
+          }
+        });
+      }
+
+      properties.add(property);
+      if (property.getValue() == null) {
+        getterCount++;
+      }
+    }
+
+    if (getterCount == 0) {
+      return Promise.resolve(properties);
+    }
+    else {
+      List<Promise<?>> promises = new SmartList<Promise<?>>();
+      for (Variable variable : properties) {
+        if (variable.getValue() == null) {
+          ValueModifier valueModifier = variable.getValueModifier();
+          assert valueModifier != null;
+          promises.add(valueModifier.evaluateGet(variable, evaluateContext));
+        }
+      }
+      return Promise.all(promises, properties);
+    }
   }
 
   @Nullable
