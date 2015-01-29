61ba86da0e9f434ead789b6f191a35e11f585e5a
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2015-01-29 14:34:21 +0300

    svn: Refactored OneRecursiveShotMergeInfoWorker - several renames

--- plugins/svn4idea/src/org/jetbrains/idea/svn/mergeinfo/OneRecursiveShotMergeInfoWorker.java
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/mergeinfo/OneRecursiveShotMergeInfoWorker.java
@@ -44,21 +44,21 @@ public class OneRecursiveShotMergeInfoWorker {
   @NotNull private final MergeContext myMergeContext;
 
   // subpath [file] (local) to (subpathURL - merged FROM - to ranges list)
-  @NotNull private final AreaMap<String, Map<String, SVNMergeRangeList>> myDataMap;
+  @NotNull private final AreaMap<String, Map<String, SVNMergeRangeList>> myMergeInfoMap;
   @NotNull private final Object myLock;
-  @NotNull private final String myFromUrlRelative;
+  @NotNull private final String myRepositoryRelativeSourcePath;
 
   public OneRecursiveShotMergeInfoWorker(@NotNull MergeContext mergeContext) {
     myMergeContext = mergeContext;
     myLock = new Object();
     // TODO: Rewrite without AreaMap usage
-    myDataMap = AreaMap.create(new PairProcessor<String, String>() {
+    myMergeInfoMap = AreaMap.create(new PairProcessor<String, String>() {
       public boolean process(String parentUrl, String childUrl) {
         if (".".equals(parentUrl)) return true;
         return SVNPathUtil.isAncestor(SvnUtil.ensureStartSlash(parentUrl), SvnUtil.ensureStartSlash(childUrl));
       }
     });
-    myFromUrlRelative =
+    myRepositoryRelativeSourcePath =
       SvnUtil.ensureStartSlash(SVNPathUtil.getRelativePath(myMergeContext.getWcInfo().getRepositoryRoot(), myMergeContext.getSourceUrl()));
   }
 
@@ -85,7 +85,7 @@ public class OneRecursiveShotMergeInfoWorker {
         }
 
         synchronized (myLock) {
-          myDataMap.put(toKey(workingCopyRelativePath), mergeInfo);
+          myMergeInfoMap.put(toKey(workingCopyRelativePath), mergeInfo);
         }
       }
 
@@ -98,19 +98,20 @@ public class OneRecursiveShotMergeInfoWorker {
   }
 
   @NotNull
-  public SvnMergeInfoCache.MergeCheckResult isMerged(@NotNull String relativeToRepoURLPath, long revisionNumber) {
-    String relativeToWc = SVNPathUtil.getRelativePath(myFromUrlRelative, SvnUtil.ensureStartSlash(relativeToRepoURLPath));
+  public SvnMergeInfoCache.MergeCheckResult isMerged(@NotNull String repositoryRelativePath, long revisionNumber) {
+    String sourceRelativePath =
+      SVNPathUtil.getRelativePath(myRepositoryRelativeSourcePath, SvnUtil.ensureStartSlash(repositoryRelativePath));
     SvnMergeInfoCache.MergeCheckResult result;
 
-    if (relativeToWc == null) {
+    if (sourceRelativePath == null) {
       // TODO: SVNPathUtil.getRelativePath() is @NotNull - probably we need to check also isEmpty() here?
       result = SvnMergeInfoCache.MergeCheckResult.NOT_EXISTS;
     }
     else {
-      InfoProcessor processor = new InfoProcessor(relativeToWc, myFromUrlRelative, revisionNumber);
+      InfoProcessor processor = new InfoProcessor(sourceRelativePath, myRepositoryRelativeSourcePath, revisionNumber);
 
       synchronized (myLock) {
-        myDataMap.getSimiliar(toKey(relativeToWc), processor);
+        myMergeInfoMap.getSimiliar(toKey(sourceRelativePath), processor);
       }
 
       result = SvnMergeInfoCache.MergeCheckResult.getInstance(processor.isMerged());
@@ -121,46 +122,46 @@ public class OneRecursiveShotMergeInfoWorker {
 
   private static class InfoProcessor implements PairProcessor<String, Map<String, SVNMergeRangeList>> {
 
-    @NotNull private final String myWcLevelRelativeSourceUrl;
-    private boolean myMerged;
-    @NotNull private final String myFilePathAsked;
-    private final long myRevisionAsked;
+    @NotNull private final String myRepositoryRelativeSourcePath;
+    private boolean myIsMerged;
+    @NotNull private final String mySourceRelativePath;
+    private final long myRevisionNumber;
 
-    public InfoProcessor(@NotNull String filePathAsked, @NotNull String wcLevelRelativeSourceUrl, long revisionAsked) {
-      myFilePathAsked = filePathAsked;
-      myRevisionAsked = revisionAsked;
-      myWcLevelRelativeSourceUrl = SvnUtil.ensureStartSlash(wcLevelRelativeSourceUrl);
+    public InfoProcessor(@NotNull String sourceRelativePath, @NotNull String repositoryRelativeSourcePath, long revisionNumber) {
+      mySourceRelativePath = sourceRelativePath;
+      myRevisionNumber = revisionNumber;
+      myRepositoryRelativeSourcePath = SvnUtil.ensureStartSlash(repositoryRelativeSourcePath);
     }
 
     public boolean isMerged() {
-      return myMerged;
+      return myIsMerged;
     }
 
     // TODO: Try to unify with BranchInfo.processMergeinfoProperty()
-    public boolean process(@NotNull String relativeFileSubpath, @NotNull Map<String, SVNMergeRangeList> map) {
+    public boolean process(@NotNull String workingCopyRelativePath, @NotNull Map<String, SVNMergeRangeList> mergedPathsMap) {
       boolean processed = false;
-      final boolean self = relativeFileSubpath.equals(myFilePathAsked);
+      final boolean isCurrentPath = workingCopyRelativePath.equals(mySourceRelativePath);
 
-      if (map.isEmpty()) {
-        myMerged = false;
+      if (mergedPathsMap.isEmpty()) {
+        myIsMerged = false;
         processed = true;
       }
       else {
-        String mergedPathAffectingSourcePath = ContainerUtil.find(map.keySet(), new Condition<String>() {
+        String mergedPathAffectingSourcePath = ContainerUtil.find(mergedPathsMap.keySet(), new Condition<String>() {
           @Override
           public boolean value(String path) {
-            return SVNPathUtil.isAncestor(myWcLevelRelativeSourceUrl, SvnUtil.ensureStartSlash(path));
+            return SVNPathUtil.isAncestor(myRepositoryRelativeSourcePath, SvnUtil.ensureStartSlash(path));
           }
         });
 
         if (mergedPathAffectingSourcePath != null) {
-          SVNMergeRangeList mergeRangeList = map.get(mergedPathAffectingSourcePath);
+          SVNMergeRangeList mergeRangeList = mergedPathsMap.get(mergedPathAffectingSourcePath);
 
           processed = true;
-          myMerged = ContainerUtil.or(mergeRangeList.getRanges(), new Condition<SVNMergeRange>() {
+          myIsMerged = ContainerUtil.or(mergeRangeList.getRanges(), new Condition<SVNMergeRange>() {
             @Override
             public boolean value(@NotNull SVNMergeRange range) {
-              return BranchInfo.isInRange(range, myRevisionAsked) && (range.isInheritable() || self);
+              return BranchInfo.isInRange(range, myRevisionNumber) && (range.isInheritable() || isCurrentPath);
             }
           });
         }
