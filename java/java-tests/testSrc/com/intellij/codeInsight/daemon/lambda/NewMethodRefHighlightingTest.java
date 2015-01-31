a6d8e976fa203a1f57248ca4c33709201ef12cbf
Anna Kozlova <anna.kozlova@jetbrains.com>
2015-01-31 14:17:12 +0300

    testdata for ClassType::<TypeArguments>new method references: ensure compilation error if ClassType is raw but type arguments are given

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewMethodRefHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewMethodRefHighlightingTest.java
@@ -354,6 +354,10 @@ public class NewMethodRefHighlightingTest extends LightDaemonAnalyzerTestCase {
     doTest();
   }
 
+  public void testNewParameterizedReferenceOnRawType() throws Exception {
+    doTest();
+  }
+
   private void doTest() {
     doTest(false);
   }
