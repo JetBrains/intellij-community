ae5bed42728c0481a18981667436f28c17072c74
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2015-02-03 13:52:11 +0300

    IDEA-130840 'keymap' configurable  is modified after open Preferences (cherry picked from commit effc915) http://crucible.labs.intellij.net/cru/CR-IC-7199

--- platform/platform-impl/src/com/intellij/openapi/keymap/impl/ui/KeymapPanel.java
+++ platform/platform-impl/src/com/intellij/openapi/keymap/impl/ui/KeymapPanel.java
@@ -49,6 +49,7 @@ import com.intellij.openapi.ui.popup.JBPopup;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.openapi.ui.popup.ListPopup;
 import com.intellij.openapi.util.Comparing;
+import com.intellij.openapi.util.Condition;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.util.registry.Registry;
@@ -61,6 +62,7 @@ import com.intellij.ui.ListCellRendererWrapper;
 import com.intellij.ui.awt.RelativePoint;
 import com.intellij.util.Alarm;
 import com.intellij.util.IJSwingUtilities;
+import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.containers.HashMap;
 import com.intellij.util.ui.FormBuilder;
 import com.intellij.util.ui.UIUtil;
@@ -851,6 +853,7 @@ public class KeymapPanel extends JPanel implements SearchableConfigurable, Confi
     }
 
     myKeymapList.setSelectedItem(mySelectedKeymap);
+    myActionsTree.reset(mySelectedKeymap, getCurrentQuickListIds());
   }
 
   public void apply() throws ConfigurationException{
@@ -895,7 +898,12 @@ public class KeymapPanel extends JPanel implements SearchableConfigurable, Confi
     if (!Comparing.equal(mySelectedKeymap, keymapManager.getActiveKeymap())) {
       return true;
     }
-    Keymap[] managerKeymaps = keymapManager.getAllKeymaps();
+    Keymap[] managerKeymaps = ContainerUtil.filter(keymapManager.getAllKeymaps(), new Condition<Keymap>() {
+      @Override
+      public boolean value(Keymap keymap) {
+        return !SystemInfo.isMac || !KeymapManager.DEFAULT_IDEA_KEYMAP.equals(keymap.getName());
+      }
+    }).toArray(new Keymap[]{});
     Keymap[] panelKeymaps = new Keymap[myKeymapListModel.getSize()];
     for(int i = 0; i < myKeymapListModel.getSize(); i++){
       panelKeymaps[i] = (Keymap)myKeymapListModel.getElementAt(i);
