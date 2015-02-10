3318c68aadf60c079f04d25888e0ef778e7e73d6
Anna Kozlova <anna.kozlova@jetbrains.com>
2015-02-10 16:07:14 +0100

    test data for not accessible through captured wildcard

--- java/java-tests/testData/codeInsight/multipleJdks/java7/p/DeclaredTypeOfVariableImplementsUnknownType.java
+++ java/java-tests/testData/codeInsight/multipleJdks/java7/p/DeclaredTypeOfVariableImplementsUnknownType.java
@@ -3,4 +3,8 @@ abstract class B {
   void f(A a) {
     <error descr="Cannot access java.util.stream.Stream">a</error>.foo();
   }
+
+  void f(java.util.List<? extends A> a) {
+    <error descr="Cannot access java.util.stream.Stream">a.get(0)</error>.foo();
+  }
 }
