761432795cfe8b969879f35c184729a7a1bacbc7
Aleksey Pivovarov <AMPivovarov@gmail.com>
2015-02-12 20:21:09 +0300

    diff: remove duplication

--- platform/diff-impl/src/com/intellij/diff/tools/fragmented/OnesideDiffViewer.java
+++ platform/diff-impl/src/com/intellij/diff/tools/fragmented/OnesideDiffViewer.java
@@ -42,6 +42,7 @@ import com.intellij.diff.util.DiffUserDataKeysEx;
 import com.intellij.diff.util.DiffUserDataKeysEx.ScrollToPolicy;
 import com.intellij.diff.util.DiffUtil;
 import com.intellij.diff.util.DiffUtil.DocumentData;
+import com.intellij.diff.util.DiffUtil.EditorsVisiblePositions;
 import com.intellij.diff.util.Side;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.actionSystem.AnAction;
@@ -59,7 +60,6 @@ import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Computable;
-import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.UserDataHolder;
 import com.intellij.ui.LightweightHint;
@@ -70,7 +70,6 @@ import gnu.trove.TIntFunction;
 import org.jetbrains.annotations.*;
 
 import javax.swing.*;
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
@@ -978,13 +977,13 @@ public class OnesideDiffViewer extends TextDiffViewerBase {
     protected boolean myShouldScroll = true;
 
     @Nullable private ScrollToPolicy myScrollToChange;
-    @Nullable private EditorPosition myEditorPosition;
+    @Nullable private EditorsVisiblePositions myEditorPosition;
     @Nullable private LogicalPosition[] myCaretPosition;
     @Nullable private DiffNavigationContext myNavigationContext;
 
     public void processContext() {
       myScrollToChange = myRequest.getUserData(DiffUserDataKeysEx.SCROLL_TO_CHANGE);
-      myEditorPosition = myRequest.getUserData(EditorPosition.KEY);
+      myEditorPosition = myRequest.getUserData(EditorsVisiblePositions.KEY);
       myCaretPosition = myRequest.getUserData(DiffUserDataKeysEx.EDITORS_CARET_POSITION);
       myNavigationContext = myRequest.getUserData(DiffUserDataKeysEx.NAVIGATION_CONTEXT);
     }
@@ -996,10 +995,10 @@ public class OnesideDiffViewer extends TextDiffViewerBase {
       carets[0] = getPosition(pair.first[0], position.column);
       carets[1] = getPosition(pair.first[1], position.column);
 
-      EditorPosition editorsPosition = new EditorPosition(carets, DiffUtil.getScrollingPoint(myEditor));
+      EditorsVisiblePositions editorsPosition = new EditorsVisiblePositions(position, DiffUtil.getScrollingPosition(myEditor));
 
       myRequest.putUserData(DiffUserDataKeysEx.SCROLL_TO_CHANGE, null);
-      myRequest.putUserData(EditorPosition.KEY, editorsPosition);
+      myRequest.putUserData(EditorsVisiblePositions.KEY, editorsPosition);
       myRequest.putUserData(DiffUserDataKeysEx.EDITORS_CARET_POSITION, carets);
       myRequest.putUserData(DiffUserDataKeysEx.NAVIGATION_CONTEXT, null);
     }
@@ -1018,8 +1017,8 @@ public class OnesideDiffViewer extends TextDiffViewerBase {
 
         myEditor.getCaretModel().moveToLogicalPosition(position);
 
-        if (myEditorPosition != null && myEditorPosition.isSame(myCaretPosition)) {
-          DiffUtil.scrollToPoint(myEditor, myEditorPosition.myPoint);
+        if (myEditorPosition != null && myEditorPosition.isSame(position)) {
+          DiffUtil.scrollToPoint(myEditor, myEditorPosition.myPoints[0]);
         }
         else {
           myEditor.getScrollingModel().scrollToCaret(ScrollType.CENTER);
@@ -1082,27 +1081,6 @@ public class OnesideDiffViewer extends TextDiffViewerBase {
       if (line == -1) return false;
 
       return doScrollToLine(Side.RIGHT, new LogicalPosition(line, 0));
-    }
-  }
-
-  private static class EditorPosition {
-    public static final Key<EditorPosition> KEY = Key.create("Diff.OnesideEditorPosition");
-
-    @NotNull public final LogicalPosition[] myCaretPosition;
-    @NotNull public final Point myPoint;
-
-    public EditorPosition(@NotNull LogicalPosition[] caretPosition, @NotNull Point point) {
-      myCaretPosition = caretPosition;
-      myPoint = point;
-    }
-
-    public boolean isSame(@Nullable LogicalPosition[] caretPosition) {
-      // TODO: allow small fluctuations ?
-      if (caretPosition == null) return true;
-      if (caretPosition.length != 2) return false;
-      if (!caretPosition[0].equals(myCaretPosition[0])) return false;
-      if (!caretPosition[1].equals(myCaretPosition[1])) return false;
-      return true;
     }
   }
 
--- platform/diff-impl/src/com/intellij/diff/util/DiffUtil.java
+++ platform/diff-impl/src/com/intellij/diff/util/DiffUtil.java
@@ -175,6 +175,10 @@ public class DiffUtil {
     editor.reinitSettings();
   }
 
+  //
+  // Scrolling
+  //
+
   public static void scrollEditor(@Nullable final Editor editor, int line) {
     scrollEditor(editor, line, 0);
   }
@@ -210,10 +214,60 @@ public class DiffUtil {
   }
 
   @NotNull
-  public static Point getScrollingPoint(@Nullable Editor editor) {
+  public static Point getScrollingPosition(@Nullable Editor editor) {
     if (editor == null) return new Point(0, 0);
     ScrollingModel model = editor.getScrollingModel();
     return new Point(model.getHorizontalScrollOffset(), model.getVerticalScrollOffset());
+  }
+
+  @NotNull
+  public static LogicalPosition getCaretPosition(@Nullable Editor editor) {
+    return editor != null ? editor.getCaretModel().getLogicalPosition() : new LogicalPosition(0, 0);
+  }
+
+  @NotNull
+  public static Point[] getScrollingPositions(@NotNull List<? extends Editor> editors) {
+    Point[] carets = new Point[editors.size()];
+    for (int i = 0; i < editors.size(); i++) {
+      carets[i] = getScrollingPosition(editors.get(i));
+    }
+    return carets;
+  }
+
+  @NotNull
+  public static LogicalPosition[] getCaretPositions(@NotNull List<? extends Editor> editors) {
+    LogicalPosition[] carets = new LogicalPosition[editors.size()];
+    for (int i = 0; i < editors.size(); i++) {
+      carets[i] = getCaretPosition(editors.get(i));
+    }
+    return carets;
+  }
+
+  public static class EditorsVisiblePositions {
+    public static final Key<EditorsVisiblePositions> KEY = Key.create("Diff.EditorsVisiblePositions");
+
+    @NotNull public final LogicalPosition[] myCaretPosition;
+    @NotNull public final Point[] myPoints;
+
+    public EditorsVisiblePositions(@NotNull LogicalPosition caretPosition, @NotNull Point points) {
+      myCaretPosition = new LogicalPosition[]{caretPosition};
+      myPoints = new Point[]{points};
+    }
+
+    public EditorsVisiblePositions(@NotNull LogicalPosition[] caretPosition, @NotNull Point[] points) {
+      myCaretPosition = caretPosition;
+      myPoints = points;
+    }
+
+    public boolean isSame(@Nullable LogicalPosition... caretPosition) {
+      // TODO: allow small fluctuations ?
+      if (caretPosition == null) return true;
+      if (myCaretPosition.length != caretPosition.length) return false;
+      for (int i = 0; i < caretPosition.length; i++) {
+        if (!caretPosition[i].equals(myCaretPosition[i])) return false;
+      }
+      return true;
+    }
   }
 
   //
