85390d9838790f1c2c61ba932c80282afb21ed0e
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2015-02-16 16:56:36 +0300

    IDEA-136597 Android-Gradle: new project is created with no Project SDK specified

--- platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/execution/ExternalSystemJdkUtil.java
+++ platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/execution/ExternalSystemJdkUtil.java
@@ -25,6 +25,7 @@ import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.text.StringUtil;
 import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
@@ -133,5 +134,17 @@ public class ExternalSystemJdkUtil {
     }
 
     return null;
+  }
+
+  public static boolean checkForJdk(@NotNull Project project, @Nullable String jdkName) {
+    try {
+      final Sdk sdk = getJdk(project, jdkName);
+      if (sdk != null && sdk.getHomePath() != null && JdkUtil.checkForJdk(new File(sdk.getHomePath()))) {
+        return true;
+      }
+    }
+    catch (ExternalSystemJdkException ignore) {
+    }
+    return false;
   }
 }
