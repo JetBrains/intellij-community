49d8da62236eb6b5ddf6fb76957078116d0f4deb
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2015-02-17 16:46:35 +0300

    Fixed SOE in PyClass.isOverriddenMRO() check (PY-15002)
    In Python 2 it is possible to create an endless metaclass chain by
    defining '__metaclass__' at the top-level to be one of the classes
    defined in the module. We assume that these circular metaclasses don't
    override `mro()` in order to prevent infinite recursion in this check.
    (cherry picked from commit 332709f)

--- python/src/com/jetbrains/python/psi/impl/PyClassImpl.java
+++ python/src/com/jetbrains/python/psi/impl/PyClassImpl.java
@@ -1343,6 +1343,9 @@ public class PyClassImpl extends PyBaseElementImpl<PyClassStub> implements PyCla
       final PyType metaClassType = cls.getMetaClassType(context);
       if (metaClassType instanceof PyClassType) {
         final PyClass metaClass = ((PyClassType)metaClassType).getPyClass();
+        if (cls == metaClass) {
+          return false;
+        }
         final PyFunction mroMethod = metaClass.findMethodByName(PyNames.MRO, true);
         if (mroMethod != null) {
           final PyClass mroClass = mroMethod.getContainingClass();
