8d9b9364a2939854b4946db337ce8c5c2eafadd4
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2015-02-17 16:45:05 +0300

    Groovy method override autocompletion: reformat & refactor & fixed for traits & tests

--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/completion/GrMethodOverrideCompletionProvider.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/completion/GrMethodOverrideCompletionProvider.java
@@ -16,8 +16,6 @@
 package org.jetbrains.plugins.groovy.lang.completion;
 
 import com.intellij.codeInsight.completion.*;
-import com.intellij.codeInsight.generation.OverrideImplementExploreUtil;
-import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.icons.AllIcons;
 import com.intellij.psi.*;
@@ -30,24 +28,20 @@ import com.intellij.util.ProcessingContext;
 import com.intellij.util.VisibilityUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.groovy.lang.completion.handlers.GroovyMethodOverrideHandler;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.GrTypeDefinition;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.GrTypeDefinitionBody;
+import org.jetbrains.plugins.groovy.overrideImplement.GroovyOverrideImplementExploreUtil;
 
-import javax.swing.*;
 import java.util.Collection;
 
 import static com.intellij.patterns.PlatformPatterns.psiElement;
-
-/**
- * Created by Arasthel on 29/01/15.
- */
 
 class GrMethodOverrideCompletionProvider extends CompletionProvider<CompletionParameters> {
 
   @Override
   protected void addCompletions(@NotNull CompletionParameters parameters, ProcessingContext context, @NotNull CompletionResultSet result) {
-    PsiElement position = parameters.getPosition();
-
-    PsiClass currentClass = PsiTreeUtil.getParentOfType(position, PsiClass.class);
+    final PsiElement position = parameters.getPosition();
+    final GrTypeDefinition currentClass = PsiTreeUtil.getParentOfType(position, GrTypeDefinition.class);
 
     if (currentClass != null) {
       addSuperMethods(currentClass, result, false);
@@ -59,43 +53,31 @@ class GrMethodOverrideCompletionProvider extends CompletionProvider<CompletionPa
     contributor.extend(CompletionType.BASIC, psiElement().withParent(GrTypeDefinitionBody.class), new GrMethodOverrideCompletionProvider());
   }
 
-  private void addSuperMethods(final PsiClass psiClass, CompletionResultSet completionResultSet, boolean implement) {
-    Collection<CandidateInfo> candidates = OverrideImplementExploreUtil.getMethodsToOverrideImplement(psiClass, implement);
-    for(CandidateInfo candidateInfo : candidates) {
-      final PsiMethod method = (PsiMethod) candidateInfo.getElement();
-      PsiSubstitutor substitutor = candidateInfo.getSubstitutor();
+  private static void addSuperMethods(final GrTypeDefinition psiClass, CompletionResultSet completionResultSet, boolean toImplement) {
+    final Collection<CandidateInfo> candidates = GroovyOverrideImplementExploreUtil.getMethodsToOverrideImplement(psiClass, toImplement);
+    for (CandidateInfo candidateInfo : candidates) {
+      final PsiMethod method = (PsiMethod)candidateInfo.getElement();
+      if (method.isConstructor()) continue;
 
       RowIcon icon = new RowIcon(2);
       icon.setIcon(method.getIcon(0), 0);
-      icon.setIcon(implement ? AllIcons.Gutter.ImplementingMethod : AllIcons.Gutter.OverridingMethod, 1);
+      icon.setIcon(toImplement ? AllIcons.Gutter.ImplementingMethod : AllIcons.Gutter.OverridingMethod, 1);
 
-      if(!method.isConstructor()) {
-        completionResultSet.addElement(createLookUpElement(method, substitutor, psiClass, icon, new GroovyMethodOverrideHandler(psiClass)));
-      }
+      PsiSubstitutor substitutor = candidateInfo.getSubstitutor();
+      String parameters = PsiFormatUtil.formatMethod(method, substitutor, PsiFormatUtilBase.SHOW_PARAMETERS, PsiFormatUtilBase.SHOW_NAME);
+      String visibility = VisibilityUtil.getVisibilityModifier(method.getModifierList());
+      String modifiers = (visibility == PsiModifier.PACKAGE_LOCAL ? "" : visibility + " ");
+      PsiType type = substitutor.substitute(method.getReturnType());
+      String parentClassName = psiClass == null ? "" : psiClass.getName();
+      String signature = modifiers + (type == null ? "" : type.getPresentableText() + " ") + method.getName();
+
+      LookupElementBuilder lookupElement = LookupElementBuilder.create(method, signature)
+        .appendTailText(parameters, false)
+        .appendTailText("{...}", true)
+        .withTypeText(parentClassName)
+        .withIcon(icon)
+        .withInsertHandler(new GroovyMethodOverrideHandler(psiClass));
+      completionResultSet.addElement(lookupElement);
     }
-  }
-
-  private LookupElementBuilder createLookUpElement(final PsiMethod method, PsiSubstitutor substitutor, PsiClass currentClass, Icon icon,
-                                                   InsertHandler<LookupElement> insertHandler) {
-    String parameters = PsiFormatUtil.formatMethod(method, substitutor, PsiFormatUtilBase.SHOW_PARAMETERS, PsiFormatUtilBase.SHOW_NAME);
-
-    String visibility = VisibilityUtil.getVisibilityModifier(method.getModifierList());
-    String modifiers = (visibility == PsiModifier.PACKAGE_LOCAL ? "" : visibility + " ");
-
-    PsiType type = substitutor.substitute(method.getReturnType());
-
-    String parentClassName = currentClass == null ? "" : currentClass.getName();
-
-    String signature = modifiers + (type == null ? "" : type.getPresentableText() + " ") + method.getName();
-
-    LookupElementBuilder elementBuilder = LookupElementBuilder.create(method, signature)
-      .withLookupString(signature)
-      .appendTailText(parameters, false)
-      .appendTailText("{...}", true)
-      .withTypeText(parentClassName)
-      .withIcon(icon)
-      .withInsertHandler(insertHandler);
-
-    return elementBuilder;
   }
 }
