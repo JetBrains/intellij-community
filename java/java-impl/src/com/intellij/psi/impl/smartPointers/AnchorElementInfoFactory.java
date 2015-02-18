b0a23e5fafa5eb7f16d893b3408a0c6b487873ef
peter <peter@jetbrains.com>
2015-02-18 17:36:20 +0100

    AnchorElementInfoFactory: bring back xml handling via a (hopefully) temporary hack

--- java/java-impl/src/com/intellij/psi/impl/smartPointers/AnchorElementInfoFactory.java
+++ java/java-impl/src/com/intellij/psi/impl/smartPointers/AnchorElementInfoFactory.java
@@ -22,6 +22,7 @@ import com.intellij.psi.stubs.IStubElementType;
 import com.intellij.psi.stubs.StubTree;
 import com.intellij.psi.util.PsiUtilCore;
 import com.intellij.psi.xml.XmlTag;
+import com.intellij.psi.xml.XmlToken;
 import com.intellij.xml.util.XmlTagUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -54,6 +55,14 @@ public class AnchorElementInfoFactory implements SmartPointerElementInfoFactory 
 
   @Nullable
   static PsiElement getAnchor(PsiElement element) {
+    //a hack
+    if (element instanceof XmlTag) {
+      XmlToken tagNameElement = XmlTagUtil.getStartTagNameElement((XmlTag)element);
+      if (tagNameElement != null && tagNameElement.isPhysical()) {
+        return tagNameElement;
+      }
+    }
+    
     if (!element.getLanguage().isKindOf(JavaLanguage.INSTANCE)) {
       return null;
     }
@@ -73,9 +82,6 @@ public class AnchorElementInfoFactory implements SmartPointerElementInfoFactory 
     }
     else if (element instanceof PsiVariable) {
       anchor = ((PsiVariable)element).getNameIdentifier();
-    }
-    else if (element instanceof XmlTag) {
-      anchor = XmlTagUtil.getStartTagNameElement((XmlTag)element);
     }
     if (anchor != null && (!anchor.isPhysical() /*|| anchor.getTextRange()==null*/)) return null;
     return anchor;
