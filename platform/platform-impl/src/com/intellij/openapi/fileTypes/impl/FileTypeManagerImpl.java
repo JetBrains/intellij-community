58e443a403a61a97c876d3bf0f339ef982981946
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2015-02-19 21:06:14 +0100

    don't load default file types if DBE/CIDR

--- platform/platform-impl/src/com/intellij/openapi/fileTypes/impl/FileTypeManagerImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/fileTypes/impl/FileTypeManagerImpl.java
@@ -277,6 +277,11 @@ public class FileTypeManagerImpl extends FileTypeManagerEx implements Persistent
       registerFileTypeWithoutNotification(pair.fileType, pair.matchers, true);
     }
 
+    if (PlatformUtils.isDatabaseIDE() || PlatformUtils.isCidr()) {
+      // build scripts are correct, but it is required to run from sources
+      return;
+    }
+
     try {
       URL defaultFileTypesUrl = FileTypeManagerImpl.class.getResource("/defaultFileTypes.xml");
       if (defaultFileTypesUrl != null) {
