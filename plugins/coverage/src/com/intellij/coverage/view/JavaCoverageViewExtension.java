a5b8bdf0b92c051248d22b1695961f686d68588a
Dmitry Jemerov <yole@jetbrains.com>
2015-02-20 15:00:44 +0100

    API for customizing the coverage view tree structure

--- plugins/coverage/src/com/intellij/coverage/view/JavaCoverageViewExtension.java
+++ plugins/coverage/src/com/intellij/coverage/view/JavaCoverageViewExtension.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.intellij.coverage.view;
 
 import com.intellij.coverage.*;
@@ -207,6 +222,17 @@ public class JavaCoverageViewExtension extends CoverageViewExtension {
     List<AbstractTreeNode> children = new ArrayList<AbstractTreeNode>();
     if (node instanceof CoverageListNode) {
       final Object val = node.getValue();
+
+      for (JavaCoverageEngineExtension extension : JavaCoverageEngineExtension.EP_NAME.getExtensions()) {
+        List<AbstractTreeNode> childNodes = extension.getCoverageViewChildrenNodes(val);
+        if (childNodes != null) {
+          for (AbstractTreeNode childNode : childNodes) {
+            childNode.setParent(node);
+          }
+          return childNodes;
+        }
+      }
+
       if (val instanceof PsiClass) return Collections.emptyList();
 
       //append package classes
