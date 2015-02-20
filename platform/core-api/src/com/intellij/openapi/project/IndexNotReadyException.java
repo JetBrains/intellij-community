f8d95121db9376111d830a567490341804b79d21
peter <peter@jetbrains.com>
2015-02-20 17:37:49 +0100

    expand INRE javadoc to include new coping strategies

--- platform/core-api/src/com/intellij/openapi/project/IndexNotReadyException.java
+++ platform/core-api/src/com/intellij/openapi/project/IndexNotReadyException.java
@@ -15,13 +15,32 @@
  */
 package com.intellij.openapi.project;
 
+import com.intellij.openapi.util.Computable;
+
 /**
- * Thrown on accessing indices in dumb mode. Possible fixes:
- * <li> if {@link com.intellij.openapi.actionSystem.AnAction#actionPerformed(com.intellij.openapi.actionSystem.AnActionEvent)} is in stack trace,
+ * Thrown on accessing indices when they're not ready, in so-called dumb mode. Possible fixes:
+ * <ul>
+ * <li> If {@link com.intellij.openapi.actionSystem.AnAction#actionPerformed(com.intellij.openapi.actionSystem.AnActionEvent)} is in stack trace,
  * consider making the action not implement {@link DumbAware}.
- * <li> if this access is performed from some invokeLater activity, consider replacing it with
- * {@link DumbService#smartInvokeLater(Runnable)}
- * <li> otherwise, add {@link DumbService#isDumb()} checks where necessary
+ *
+ * <li> A {@link DumbAware} action, having got this exception, may just notify the user that the requested activity is not possible while
+ * indexing is in progress. It can be done via a dialog (see {@link com.intellij.openapi.ui.Messages}) or a status bar balloon
+ * (see {@link DumbService#showDumbModeNotification(String)}, {@link com.intellij.openapi.actionSystem.ex.ActionUtil#showDumbModeWarning(com.intellij.openapi.actionSystem.AnActionEvent...)}).
+ *
+ * <li> If index access is performed from some non-urgent invokeLater activity, consider replacing it with
+ * {@link DumbService#smartInvokeLater(Runnable)}. Note that this 'later' can be very late, several minutes may pass. So if that code
+ * involves user interaction, {@link DumbService#smartInvokeLater(Runnable)} should probably not be used to avoid dialogs popping out of the blue.
+ *
+ * <li> If it's a non-urgent background process (e.g. compilation, usage search), consider replacing topmost read-action with
+ * {@link DumbService#runReadActionInSmartMode(Computable)}.
+ *
+ * <li> If the exception comes from within Java's findClass call, and the IDE is currently performing a user-initiated action or a
+ * task when skipping findClass would lead to very negative consequences (e.g. not stopping at a breakpoint), then it might be possible
+ * to avoid index query by using alternative resolve (and findClass) strategy, which is significantly slower and might return null. To do this,
+ * use {@link DumbService#setAlternativeResolveEnabled(boolean)}.
+ *
+ * <li> It's preferable to avoid the exception entirely by adding {@link DumbService#isDumb()} checks where necessary.
+ * </ul>
  *
  * @author peter
  * @see DumbService
