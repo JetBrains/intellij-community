a2f1d6951c910ab08dda388c3da4b8c1d73da7c8
Egor.Ushakov <egor.ushakov@jetbrains.com>
2015-02-20 16:07:37 +0300

    toString implementations for easier debug

--- java/debugger/impl/src/com/intellij/debugger/engine/evaluation/expression/LocalVariableEvaluator.java
+++ java/debugger/impl/src/com/intellij/debugger/engine/evaluation/expression/LocalVariableEvaluator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -155,5 +155,10 @@ class LocalVariableEvaluator implements Evaluator {
       };
     }
     return modifier;
+  }
+
+  @Override
+  public String toString() {
+    return myLocalVariableName;
   }
 }
