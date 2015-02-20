90fcf392cca36a5361656b8645c33ca8cbb9c80d
Bas Leijdekkers <basleijdekkers@gmail.com>
2015-02-20 11:15:00 +0100

    EA-65194 (IAE: ContainerUtil.collectSet)

--- platform/structuralsearch/source/com/intellij/structuralsearch/plugin/ui/EditVarConstraintsDialog.java
+++ platform/structuralsearch/source/com/intellij/structuralsearch/plugin/ui/EditVarConstraintsDialog.java
@@ -48,8 +48,8 @@ import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Collection;
 import java.util.List;
-import java.util.Set;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
@@ -226,10 +226,10 @@ class EditVarConstraintsDialog extends DialogWrapper {
 
     customScriptCode.getButton().addActionListener(new ActionListener() {
       public void actionPerformed(@NotNull final ActionEvent e) {
-        Set<String> strings = ContainerUtil.collectSet(model.getConfig().getMatchOptions().getVariableConstraintNames());
-        strings.remove(current.getName());
-        strings.remove(CompiledPattern.ALL_CLASS_UNMATCHED_CONTENT_VAR_ARTIFICIAL_NAME);
-        EditScriptDialog dialog = new EditScriptDialog(project, customScriptCode.getChildComponent().getText(), strings);
+        final List<String> variableNames = ContainerUtil.newArrayList(model.getConfig().getMatchOptions().getVariableConstraintNames());
+        variableNames.remove(current.getName());
+        variableNames.remove(CompiledPattern.ALL_CLASS_UNMATCHED_CONTENT_VAR_ARTIFICIAL_NAME);
+        final EditScriptDialog dialog = new EditScriptDialog(project, customScriptCode.getChildComponent().getText(), variableNames);
         dialog.show();
         if (dialog.getExitCode() == OK_EXIT_CODE) {
           customScriptCode.getChildComponent().setText(dialog.getScriptText());
@@ -593,7 +593,7 @@ class EditVarConstraintsDialog extends DialogWrapper {
     private final Editor editor;
     private final String title;
 
-    public EditScriptDialog(Project project, String text, Set<String> names) {
+    public EditScriptDialog(Project project, String text, Collection<String> names) {
       super(project, true);
       setTitle(SSRBundle.message("edit.groovy.script.constraint.title"));
       editor = createEditor(project, text, "1.groovy");
--- platform/structuralsearch/source/com/intellij/structuralsearch/plugin/ui/SearchDialog.java
+++ platform/structuralsearch/source/com/intellij/structuralsearch/plugin/ui/SearchDialog.java
@@ -436,11 +436,9 @@ public class SearchDialog extends DialogWrapper {
     );
 
     model.getConfig().getMatchOptions().clearVariableConstraints();
-    if (matchOptions.hasVariableConstraints()) {
-      for (Iterator<String> i = matchOptions.getVariableConstraintNames(); i.hasNext(); ) {
-        final MatchVariableConstraint constraint = (MatchVariableConstraint)matchOptions.getVariableConstraint(i.next()).clone();
-        model.getConfig().getMatchOptions().addVariableConstraint(constraint);
-      }
+    for (String name : matchOptions.getVariableConstraintNames()) {
+      final MatchVariableConstraint constraint = (MatchVariableConstraint)matchOptions.getVariableConstraint(name).clone();
+      model.getConfig().getMatchOptions().addVariableConstraint(constraint);
     }
 
     MatchOptions options = configuration.getMatchOptions();
