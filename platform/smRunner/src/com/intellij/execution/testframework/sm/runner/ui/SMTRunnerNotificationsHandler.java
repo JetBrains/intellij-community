3c97e2381f0dcbe9ea598daaa1c80fd9e6b9f39b
Oleg Sukhodolsky <oleg.sukhodolsky@jetbrains.com>
2015-02-23 09:06:22 +0300

    temporary reverting fix for  IDEA-136763 Need to provide at least one implementation for the ext point to avoid IAE

--- platform/smRunner/src/com/intellij/execution/testframework/sm/runner/ui/SMTRunnerNotificationsHandler.java
+++ platform/smRunner/src/com/intellij/execution/testframework/sm/runner/ui/SMTRunnerNotificationsHandler.java
@@ -99,7 +99,8 @@ public class SMTRunnerNotificationsHandler extends SMTRunnerEventsAdapter {
         type = null;
     }
 
-    TestStatusListener.notifySuiteFinished(testsRoot, myConsoleProperties.getProject());
+    // todo: need to provide default implementation before notifying test status listeners to avoid IAE
+    //TestStatusListener.notifySuiteFinished(testsRoot, myConsoleProperties.getProject());
 
     if (msg != null) {
       notify(msg, type, testsRoot);
