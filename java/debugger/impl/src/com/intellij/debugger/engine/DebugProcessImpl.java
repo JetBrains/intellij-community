812692e8b02d4500dde3d6db09dd838bb57d0e7a
Egor.Ushakov <egor.ushakov@jetbrains.com>
2015-02-24 17:14:52 +0300

    extra logging for EA-63606

--- java/debugger/impl/src/com/intellij/debugger/engine/DebugProcessImpl.java
+++ java/debugger/impl/src/com/intellij/debugger/engine/DebugProcessImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -1098,7 +1098,7 @@ public abstract class DebugProcessImpl extends UserDataHolderBase implements Deb
           throw (RuntimeException)exception[0];
         }
         else {
-          LOG.assertTrue(false);
+          LOG.error("Unexpected exception " + exception[0]);
         }
       }
 
