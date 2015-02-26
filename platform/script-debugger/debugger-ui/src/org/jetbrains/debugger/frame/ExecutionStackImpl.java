217df5bd3360ef02e17ea03c6890176442c7f90b
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2015-02-26 10:12:22 +0100

    don't create new call frame view if it is first frame — otherwise FRAME_CHANGED event will be fired and variables tree will be calculated twice maybe platform should be changed, will be investigated later, in any case our impl should work correctly

--- platform/script-debugger/debugger-ui/src/org/jetbrains/debugger/frame/ExecutionStackImpl.java
+++ platform/script-debugger/debugger-ui/src/org/jetbrains/debugger/frame/ExecutionStackImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jetbrains.debugger.frame;
 
 import com.intellij.xdebugger.frame.XExecutionStack;
@@ -53,6 +68,11 @@ class ExecutionStackImpl extends XExecutionStack {
         else {
           result = new ArrayList<XStackFrame>(count);
           for (int i = firstFrameIndex; i < frames.length; i++) {
+            if (i == 0) {
+              result.add(topCallFrameView);
+              continue;
+            }
+
             CallFrame frame = frames[i];
             // if script is null, it is native function (Object.forEach for example), so, skip it
             Script script = vm.getScriptManager().getScript(frame);
