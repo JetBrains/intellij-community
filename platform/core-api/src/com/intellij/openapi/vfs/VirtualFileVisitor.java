c1c8459a4e12e6feda1afaee6029bc04221fb291
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2015-02-27 14:13:43 +0100

    Cleanup (warnings)

--- platform/core-api/src/com/intellij/openapi/vfs/VirtualFileVisitor.java
+++ platform/core-api/src/com/intellij/openapi/vfs/VirtualFileVisitor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -140,11 +140,11 @@ public abstract class VirtualFileVisitor<T> {
   public void afterChildrenVisited(@NotNull VirtualFile file) { }
 
   /**
-   * By default, visitor uses ({@linkplain com.intellij.openapi.vfs.VirtualFile#getChildren()}) to iterate over file's children.
+   * By default, visitor uses ({@linkplain VirtualFile#getChildren()}) to iterate over file's children.
    * You can override this method to implement another mechanism.
    *
    * @param file a virtual file to get children from.
-   * @return children iterable, or null to use {@linkplain com.intellij.openapi.vfs.VirtualFile#getChildren()}.
+   * @return children iterable, or null to use {@linkplain VirtualFile#getChildren()}.
    */
   @Nullable
   public Iterable<VirtualFile> getChildrenIterable(@NotNull VirtualFile file) {
