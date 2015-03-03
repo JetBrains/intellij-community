d0973635df84fe4d82bb2b6119b10d83632ce71f
Alexey Kudravtsev <cdr@intellij.com>
2015-03-03 20:04:28 +0300

    toString()

--- platform/extensions/src/com/intellij/openapi/extensions/impl/ExtensionsAreaImpl.java
+++ platform/extensions/src/com/intellij/openapi/extensions/impl/ExtensionsAreaImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -413,4 +413,8 @@ public class ExtensionsAreaImpl implements ExtensionsArea {
     }
   }
 
+  @Override
+  public String toString() {
+    return (myAreaClass == null ? "Root" : myAreaClass)+" Area";
+  }
 }
