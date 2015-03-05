3d757fa0c4483fe3cf20d215ccfcaaedb9ff912e
Konstantin Bulenkov <kb@jetbrains.com>
2015-03-05 13:23:52 +0400

    + add getter for project (cherry picked from commit 0553786)

--- platform/dvcs-impl/src/com/intellij/dvcs/push/PushController.java
+++ platform/dvcs-impl/src/com/intellij/dvcs/push/PushController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -497,6 +497,11 @@ public class PushController implements Disposable {
   @Override
   public void dispose() {
     myExecutorService.shutdownNow();
+  }
+
+  @NotNull
+  public Project getProject() {
+    return myProject;
   }
 
   private void addMoreCommits(RepositoryNode repositoryNode) {
