4282e5419e99fa4993166cd8d8080b88af57c99d
Alexey Kudravtsev <cdr@intellij.com>
2015-03-06 11:39:46 +0300

    remove unused parameters, signatures made more generics-friendly

--- platform/core-api/src/com/intellij/concurrency/Job.java
+++ platform/core-api/src/com/intellij/concurrency/Job.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,6 +30,7 @@ import java.util.concurrent.TimeoutException;
 
 public interface Job<T> {
   // the lower the priority the more important the task is
+  @Deprecated
   int DEFAULT_PRIORITY = 100;
 
   String getTitle();
