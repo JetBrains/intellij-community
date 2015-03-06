9ef637774bb909a223c44a3f46b68a2108d6edcf
Konstantin Bulenkov <kb@jetbrains.com>
2015-03-06 18:04:42 +0100

    use panel bg for empty space in editor tabs for darcula
    (cherry picked from commit 62169ab29961f1704f99fa711158c3b074625eb0)

--- platform/platform-api/src/com/intellij/ui/tabs/impl/DarculaEditorTabsPainter.java
+++ platform/platform-api/src/com/intellij/ui/tabs/impl/DarculaEditorTabsPainter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,11 +33,5 @@ class DarculaEditorTabsPainter extends DefaultEditorTabsPainter {
   @Override
   protected Color getInactiveMaskColor() {
     return ColorUtil.withAlpha(new Color(0x262626), .5);
-  }
-
-
-  @Override
-  public Color getEmptySpaceColor() {
-    return new Color(0x353536);
   }
 }
