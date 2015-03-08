c99475447bb180a783285aef0e923901a5ae55cd
Alexey Gopachenko <neuro159@gmail.com>
2015-03-08 10:01:32 +0100

    group compile messages in TC, cleanup

--- jps/standalone-builder/src/org/jetbrains/jps/gant/JpsGantProjectBuilder.java
+++ jps/standalone-builder/src/org/jetbrains/jps/gant/JpsGantProjectBuilder.java
@@ -234,6 +234,7 @@ public class JpsGantProjectBuilder {
 
   private void runBuild(final Set<String> modulesSet, final boolean allModules, boolean includeTests) {
     if (!myDryRun) {
+      info("##teamcity[blockOpened name='compile']")
       final AntMessageHandler messageHandler = new AntMessageHandler();
       //noinspection AssignmentToStaticFieldFromInstanceMethod
       AntLoggerFactory.ourMessageHandler = new AntMessageHandler();
@@ -281,6 +282,7 @@ public class JpsGantProjectBuilder {
         myBuildInfoPrinter.printStatisticsMessage(this, "Compilation time, ms", String.valueOf(System.currentTimeMillis() - compilationStart));
         myStatisticsReported = true;
       }
+      info("##teamcity[blockClosed name='compile']")
     }
     else {
       info("Building skipped as we're running dry");
