f98061b83147b69eed33fa86c4817bcbe67e26f4
Anna Kozlova <anna.kozlova@jetbrains.com>
2015-03-09 15:18:15 +0100

    completion: completion of constructor ref should insert new instead of class name (IDEA-137350)

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/Normal8CompletionTest.groovy
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/Normal8CompletionTest.groovy
@@ -74,4 +74,40 @@ class Test {
     assert items.find { LookupElementPresentation.renderElement(it).itemText == 'x -> {}' }
   }
 
+
+  public void "test constructor ref"() {
+    myFixture.configureByText "a.java", """
+interface Foo9 {
+  Bar test(int p);
+}
+
+class Bar {
+  public Bar(int p) {}
+}
+
+class Test88 {
+  {
+    Foo9 f = Bar::<caret>;
+  }
+}
+"""
+    myFixture.completeBasic()
+    myFixture.type('\n')
+
+    myFixture.checkResult """
+interface Foo9 {
+  Bar test(int p);
+}
+
+class Bar {
+  public Bar(int p) {}
+}
+
+class Test88 {
+  {
+    Foo9 f = Bar::new;
+  }
+}
+"""
+  }
 }
