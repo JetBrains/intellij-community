32e003e5073d316b591b317a320743dacbc2651c
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2015-03-16 01:51:12 +0300

    [vcs-log] better text and description for IntelliSort

--- platform/vcs-log/impl/src/com/intellij/vcs/log/ui/actions/IntelliSortChooserToggleAction.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/ui/actions/IntelliSortChooserToggleAction.java
@@ -27,10 +27,11 @@ import icons.VcsLogIcons;
 import org.jetbrains.annotations.NotNull;
 
 public class IntelliSortChooserToggleAction extends ToggleAction implements DumbAware {
+  @NotNull private static final String DEFAULT_TEXT = "IntelliSort";
   @NotNull private static final String DEFAULT_DESCRIPTION = "Turn IntelliSort On/Off";
 
   public IntelliSortChooserToggleAction() {
-    super("IntelliSort", DEFAULT_DESCRIPTION, IconUtil.flip(VcsLogIcons.Branch, false));
+    super(DEFAULT_TEXT, DEFAULT_DESCRIPTION, IconUtil.flip(VcsLogIcons.Branch, false));
   }
 
   @Override
@@ -54,9 +55,15 @@ public class IntelliSortChooserToggleAction extends ToggleAction implements Dumb
     e.getPresentation().setEnabled(BekSorter.isBekEnabled() && logUI != null);
 
     if (logUI != null) {
-      e.getPresentation().setDescription("Turn IntelliSort " + (logUI.getBekType() == PermanentGraph.SortType.Normal ? "On" : "Off" + ""));
+      boolean off = logUI.getBekType() == PermanentGraph.SortType.Normal;
+      e.getPresentation().setText("IntelliSort is " + (off ? "Off" : "On"));
+      e.getPresentation().setDescription("Turn IntelliSort " + (off ? "on" : "off") + " (" +
+                                         (off
+                                          ? PermanentGraph.SortType.Bek.getDescription().toLowerCase()
+                                          : PermanentGraph.SortType.Normal.getDescription().toLowerCase()) + ").");
     }
     else {
+      e.getPresentation().setText(DEFAULT_TEXT);
       e.getPresentation().setDescription(DEFAULT_DESCRIPTION);
     }
   }
