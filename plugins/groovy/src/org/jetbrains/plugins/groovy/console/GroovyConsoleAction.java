dae4d6a1833ad1a47fcf3e77c668db32020a2c3a
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2015-03-16 16:13:23 +0300

    IDEA-136429 GroovyShellCodeFragment used for Groovy Console

--- plugins/groovy/src/org/jetbrains/plugins/groovy/console/GroovyConsoleAction.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/console/GroovyConsoleAction.java
@@ -15,21 +15,19 @@
  */
 package org.jetbrains.plugins.groovy.console;
 
-import com.intellij.execution.console.LanguageConsoleImpl;
 import com.intellij.execution.console.LanguageConsoleView;
+import com.intellij.openapi.editor.ex.EditorEx;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiFile;
+import com.intellij.openapi.util.TextRange;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.plugins.groovy.GroovyLanguage;
-import org.jetbrains.plugins.groovy.debugger.fragments.GroovyCodeFragment;
 
 /**
  * Created by Max Medvedev on 9/20/13
  */
 public class GroovyConsoleAction extends GroovyShellActionBase implements DumbAware {
+
   @Override
   protected GroovyShellRunner getRunner(Module module) {
     return GroovyShellRunner.getAppropriateRunner(module);
@@ -42,12 +40,22 @@ public class GroovyConsoleAction extends GroovyShellActionBase implements DumbAw
 
   @Override
   protected LanguageConsoleView createConsole(Project project, String title) {
-    return new LanguageConsoleImpl(project, title, GroovyLanguage.INSTANCE) {
+    return new GroovyConsoleImpl(project, title) {
+
+      @Override
+      protected boolean isShell() {
+        return false;
+      }
+
       @NotNull
       @Override
-      protected PsiFile createFile(@NotNull Project project,
-                                   @NotNull VirtualFile virtualFile) {
-        return new GroovyCodeFragment(getProject(), virtualFile);
+      protected String addToHistoryInner(@NotNull TextRange textRange,
+                                         @NotNull EditorEx editor,
+                                         boolean erase,
+                                         boolean preserveMarkup) {
+        final String result = super.addToHistoryInner(textRange, editor, erase, preserveMarkup);
+        processCode();
+        return result;
       }
     };
   }
