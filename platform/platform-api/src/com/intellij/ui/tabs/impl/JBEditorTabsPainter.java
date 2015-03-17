12d66f81bd4057216e59056bc18ac4efd3b7512a
Konstantin Bulenkov <kb@jetbrains.com>
2015-03-17 04:14:46 +0100

    Tabs: Aqua dependent bg for tabs empty space
    (cherry picked from commit a0ceb106cbf8e73d311c475abf3c9ef329017f62)

--- platform/platform-api/src/com/intellij/ui/tabs/impl/JBEditorTabsPainter.java
+++ platform/platform-api/src/com/intellij/ui/tabs/impl/JBEditorTabsPainter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -92,6 +92,6 @@ abstract class JBEditorTabsPainter {
   public abstract Color getBackgroundColor();
 
   public Color getEmptySpaceColor() {
-    return UIUtil.getPanelBackground();
+    return UIUtil.isUnderAquaLookAndFeel() ? Gray.xC8 : UIUtil.getPanelBackground();
   }
 }
