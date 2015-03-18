fc3e17dadb0dc40d11f1a36ff5e0aa0636f45397
Konstantin Bulenkov <kb@jetbrains.com>
2015-03-18 13:48:30 +0100

    set default value for "Hide tabs if there is no space" to true
    (cherry picked from commit 8896d3199fcbd7bce5b166e4b19cdc8d1c3d224e)

--- platform/editor-ui-api/src/com/intellij/ide/ui/UISettings.java
+++ platform/editor-ui-api/src/com/intellij/ide/ui/UISettings.java
@@ -86,7 +86,7 @@ public class UISettings extends SimpleModificationTracker implements PersistentS
   public boolean ALWAYS_SHOW_WINDOW_BUTTONS = false;
   public boolean CYCLE_SCROLLING = true;
   public boolean SCROLL_TAB_LAYOUT_IN_EDITOR = true;
-  public boolean HIDE_TABS_IF_NEED = false;
+  public boolean HIDE_TABS_IF_NEED = true;
   public boolean SHOW_CLOSE_BUTTON = true;
   public int EDITOR_TAB_PLACEMENT = 1;
   public boolean HIDE_KNOWN_EXTENSION_IN_TABS = false;
