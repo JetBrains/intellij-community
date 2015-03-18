4ba0890182be516fb572fc09853139a49f39bcdc
Anna Kozlova <anna.kozlova@jetbrains.com>
2015-03-18 12:37:12 +0100

    testdata for IDEA-137795

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/Java8RegressionTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/Java8RegressionTest.java
@@ -55,6 +55,10 @@ public class Java8RegressionTest extends LightDaemonAnalyzerTestCase {
     doTest();
   }
 
+  public void testIDEA137795() throws Exception {
+    doTest();
+  }
+
   private void doTest() {
     doTest(false);
   }
