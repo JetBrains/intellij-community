1d782c595c78888fe39613c3ea39e33bce1d6598
Anna Kozlova <anna.kozlova@jetbrains.com>
2015-03-20 19:02:26 +0100

    lambda isValueCompatible: ensure that normal completion is impossible for infinite loops (IDEA-137848)
    (cherry picked from commit 1f506406711581657a84d8edc6195721891b6f4d)

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewLambdaHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewLambdaHighlightingTest.java
@@ -185,6 +185,10 @@ public class NewLambdaHighlightingTest extends LightDaemonAnalyzerTestCase {
     doTest();
   }
 
+  public void testInfiniteLoopAndValueCompatibility() throws Exception {
+    doTest();
+  }
+
   private void doTest() {
     doTest(false);
   }
