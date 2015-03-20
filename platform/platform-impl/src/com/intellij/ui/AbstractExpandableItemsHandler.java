5a4ad831bd2fcb5ad3f306ff9919dab898ad77b2
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2015-03-20 16:28:37 +0300

    memory leak

--- platform/platform-impl/src/com/intellij/ui/AbstractExpandableItemsHandler.java
+++ platform/platform-impl/src/com/intellij/ui/AbstractExpandableItemsHandler.java
@@ -219,6 +219,7 @@ public abstract class AbstractExpandableItemsHandler<KeyType, ComponentType exte
         @Override
         public void hierarchyChanged(HierarchyEvent e) {
           hideHint();
+          myPopup.dispose();
         }
       }
     );
