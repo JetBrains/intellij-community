5c9b57ea2fd16a48ee864d04e260add275703df1
Anna Kozlova <anna.kozlova@jetbrains.com>
2015-03-23 19:55:40 +0100

    assert that refactoring is not started under write action; check validity (EA-66799 - PIEAE: StubBasedPsiElementBase.getContainingFile)

--- platform/lang-impl/src/com/intellij/refactoring/BaseRefactoringProcessor.java
+++ platform/lang-impl/src/com/intellij/refactoring/BaseRefactoringProcessor.java
@@ -543,6 +543,7 @@ public abstract class BaseRefactoringProcessor implements Runnable {
       return;
     }
     if (ApplicationManager.getApplication().isWriteAccessAllowed()) {
+      LOG.info(new Exception());
       DumbService.getInstance(myProject).smartInvokeLater(new Runnable() {
         @Override
         public void run() {
