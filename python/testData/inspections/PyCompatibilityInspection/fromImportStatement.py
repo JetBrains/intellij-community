4b72a6ab91ae39980fd3ddb7ba698488e7946ab0
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2015-03-25 21:16:40 +0300

    fixed test data according new Python language level (3.5)

--- python/testData/inspections/PyCompatibilityInspection/fromImportStatement.py
+++ python/testData/inspections/PyCompatibilityInspection/fromImportStatement.py
@@ -1,8 +1,10 @@
-<warning descr="Python version 2.4 doesn't support this syntax.">from . import smth</warning>
-<warning descr="Python version 2.4 doesn't support this syntax.">from .module import name1, name2</warning>
+<warning descr="Python version 2.4 doesn't support this syntax.">
+</warning>
+<warning descr="Python version 2.4 doesn't support this syntax.">
+</warning>
 
-from <warning descr="Python version 3.0, 3.1, 3.2, 3.3, 3.4 do not have module Bastion">Bastion</warning> import BastionClass
+from <warning descr="Python version 3.0, 3.1, 3.2, 3.3, 3.4, 3.5 do not have module Bastion">Bastion</warning>
 
-<warning descr="Python version 2.4 doesn't support this syntax.">from . import exceptions</warning>
+<warning descr="Python version 2.4 doesn't support this syntax.">
+</warning>
 
-from email.Utils import formatdate
