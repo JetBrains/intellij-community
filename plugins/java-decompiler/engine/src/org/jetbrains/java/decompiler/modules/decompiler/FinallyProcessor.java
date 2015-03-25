636ab469faf9374a42c867f356dfdce0c5c764d3
Egor.Ushakov <egor.ushakov@jetbrains.com>
2015-03-25 13:54:25 +0300

    decompiler: create less thrown away objects
    (cherry picked from commit 072cb471dbe586ee0fb1e8ebc2763e29c70de1d8)

--- plugins/java-decompiler/engine/src/org/jetbrains/java/decompiler/modules/decompiler/FinallyProcessor.java
+++ plugins/java-decompiler/engine/src/org/jetbrains/java/decompiler/modules/decompiler/FinallyProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -1038,7 +1038,7 @@ public class FinallyProcessor {
 
       // new empty block
       BasicBlock emptyblock = new BasicBlock(++graph.last_id);
-      emptyblock.setSeq(new SimpleInstructionSequence());
+
       graph.getBlocks().addWithKey(emptyblock, emptyblock.id);
 
       // add to ranges if necessary
