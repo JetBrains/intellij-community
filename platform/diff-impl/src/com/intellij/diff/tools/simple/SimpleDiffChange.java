aa1daef0a808730d0719f25d485c2f8de1f91dfa
Aleksey Pivovarov <AMPivovarov@gmail.com>
2015-03-25 17:17:58 +0300

    IDEA-137553 diff: do not show chevrone append actions for inserted/removed fragments
    they have no sense (as 'append' will either do nothing or do the same as 'replace') and are rather confusing
    (cherry picked from commit 524884b)

--- platform/diff-impl/src/com/intellij/diff/tools/simple/SimpleDiffChange.java
+++ platform/diff-impl/src/com/intellij/diff/tools/simple/SimpleDiffChange.java
@@ -312,12 +312,14 @@ public class SimpleDiffChange {
 
       boolean isEditable = DiffUtil.isEditable(mySide.select(myEditor1, myEditor2));
       boolean isOtherEditable = DiffUtil.isEditable(mySide.other().select(myEditor1, myEditor2));
+      boolean isAppendable = myFragment.getStartLine1() != myFragment.getEndLine1() &&
+                             myFragment.getStartLine2() != myFragment.getEndLine2();
 
       if (myCtrlPressed && myShiftPressed) return null;
       if ((myShiftPressed || !isOtherEditable) && isEditable) {
         return createRevertRenderer(mySide);
       }
-      if (myCtrlPressed) {
+      if (myCtrlPressed && isAppendable) {
         return createAppendRenderer(mySide);
       }
       return createApplyRenderer(mySide);
