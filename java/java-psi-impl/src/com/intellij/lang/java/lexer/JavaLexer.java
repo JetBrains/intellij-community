8f852af44530a7613ce4f5a7fd6489c7dc5687c5
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2015-03-25 18:52:23 +0100

    EA-66864 (diagnostic)

--- java/java-psi-impl/src/com/intellij/lang/java/lexer/JavaLexer.java
+++ java/java-psi-impl/src/com/intellij/lang/java/lexer/JavaLexer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -306,8 +306,9 @@ public class JavaLexer extends LexerBase {
       myTokenType = myFlexLexer.advance();
       myTokenEndOffset = myFlexLexer.getTokenEnd();
     }
-    catch (IOException e) {
-      // Can't be
+    catch (IOException e) { /* impossible */ }
+    catch (Error e) {
+      throw new IllegalArgumentException("Non-parsable text `" + myBuffer + "`");
     }
   }
 
