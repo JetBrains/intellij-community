7b0ab3acbe2f9f13b45ad71e13a50cdae8e2a2f9
Alexey Kudravtsev <cdr@intellij.com>
2015-03-25 20:09:17 +0300

    notnull

--- platform/platform-impl/src/com/intellij/openapi/project/ex/ProjectManagerEx.java
+++ platform/platform-impl/src/com/intellij/openapi/project/ex/ProjectManagerEx.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2011 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -57,6 +57,7 @@ public abstract class ProjectManagerEx extends ProjectManager {
   public abstract void openTestProject(@NotNull Project project);
 
   @TestOnly
+  @NotNull
   // returns remaining open test projects
   public abstract Collection<Project> closeTestProject(@NotNull Project project);
 
