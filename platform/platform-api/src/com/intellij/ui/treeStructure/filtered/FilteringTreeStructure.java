72ead1ffcf54d1bf659d2628a623dedf4ad89824
Alexander Lobas <Alexander.Lobas@jetbrains.com>
2015-03-26 18:43:44 +0300

    OC-11100 Support 'Show inherited members' in Swift file structure (cherry picked from commit 3e216d7)

--- platform/platform-api/src/com/intellij/ui/treeStructure/filtered/FilteringTreeStructure.java
+++ platform/platform-api/src/com/intellij/ui/treeStructure/filtered/FilteringTreeStructure.java
@@ -81,6 +81,9 @@ public class FilteringTreeStructure extends AbstractTreeStructure {
       for (FilteringNode n : nodes) {
         addToCache(n, false);
       }
+      if (nodes.isEmpty()) {
+        myLeaves.add(node);
+      }
     }
   }
 
