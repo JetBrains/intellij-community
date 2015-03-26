e403140178b9386f892deb88fd2d8f89518b9899
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2015-03-26 11:40:13 +0100

    Fixed test data after introducing Python 3.5 in compatibility inspection (cherry picked from commit 6b3fca9)

--- python/testData/inspections/PyCompatibilityInspection/fromImportStatement.py
+++ python/testData/inspections/PyCompatibilityInspection/fromImportStatement.py
@@ -1,8 +1,8 @@
 <warning descr="Python version 2.4 doesn't support this syntax.">from . import smth</warning>
 <warning descr="Python version 2.4 doesn't support this syntax.">from .module import name1, name2</warning>
 
-from <warning descr="Python version 3.0, 3.1, 3.2, 3.3, 3.4 do not have module Bastion">Bastion</warning> import BastionClass
+from <warning descr="Python version 3.0, 3.1, 3.2, 3.3, 3.4, 3.5 do not have module Bastion">Bastion</warning> import BastionClass
 
 <warning descr="Python version 2.4 doesn't support this syntax.">from . import exceptions</warning>
 
-from email.Utils import formatdate
+from <warning descr="Python version 2.5, 2.6, 2.7, 3.0, 3.1, 3.2, 3.3, 3.4, 3.5 do not have module email.Utils">email.Utils</warning> import formatdate
