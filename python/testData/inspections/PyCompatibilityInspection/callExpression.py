6b3fca98671af241b1d636c9fa6b28476cd4dd94
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2015-03-26 13:40:13 +0300

    Fixed test data after introducing Python 3.5 in compatibility inspection

--- python/testData/inspections/PyCompatibilityInspection/callExpression.py
+++ python/testData/inspections/PyCompatibilityInspection/callExpression.py
@@ -2,7 +2,7 @@ class A(B):
     def __init__(self):
         <warning descr="Python version 2.4, 2.5, 2.6, 2.7 do not support this syntax. super() should have arguments in Python 2">super()</warning>
 
-<warning descr="Python version 3.1, 3.2, 3.3, 3.4 do not have method cmp">cmp()</warning>
+<warning descr="Python version 3.0, 3.1, 3.2, 3.3, 3.4, 3.5 do not have method cmp">cmp()</warning>
 <warning descr="Python version 3.0, 3.1, 3.2, 3.3, 3.4, 3.5 do not have method reduce">reduce()</warning>
 <warning descr="Python version 2.4 does not have method all">all()</warning>
 
