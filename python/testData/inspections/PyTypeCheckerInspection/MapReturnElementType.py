2b55d209ed4c2baa1e8426646c9433e23981e537
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2015-03-30 20:41:08 +0300

    Renamed 't1 | t2 | ...' to 'Union[t1, t2, ...]' according to PEP 484 (PY-15206)
    (cherry picked from commit 9a03e524415b77599b18788f1f2dd24c6797fc38)

--- python/testData/inspections/PyTypeCheckerInspection/MapReturnElementType.py
+++ python/testData/inspections/PyTypeCheckerInspection/MapReturnElementType.py
@@ -2,4 +2,4 @@ def test():
     xs = map(lambda x: x + 1, [1, 2, 3])
     print('foo' + xs[0])  # Can be a str since map returns list[V] | str | unicode
     ys = map(tuple, iter([1, 2, 3]))
-    print(1 + <warning descr="Expected type 'Number', got 'tuple | str | unicode' instead">ys[0]</warning>, 'bar' + ys[1])
+    print(1 + <warning descr="Expected type 'Number', got 'Union[tuple, str, unicode]' instead">ys[0]</warning>, 'bar' + ys[1])
