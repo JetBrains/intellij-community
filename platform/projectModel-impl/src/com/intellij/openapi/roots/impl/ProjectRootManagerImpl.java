b52014da87020344df1a1d36f3fb18613d8adec1
Alexey Kudravtsev <cdr@intellij.com>
2015-03-31 20:32:37 +0300

    do not place null in the array

--- platform/projectModel-impl/src/com/intellij/openapi/roots/impl/ProjectRootManagerImpl.java
+++ platform/projectModel-impl/src/com/intellij/openapi/roots/impl/ProjectRootManagerImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -57,7 +57,7 @@ public class ProjectRootManagerImpl extends ProjectRootManagerEx implements Proj
   private static final Logger LOG = Logger.getInstance("#com.intellij.openapi.projectRoots.impl.ProjectRootManagerImpl");
 
   @NonNls public static final String PROJECT_JDK_NAME_ATTR = "project-jdk-name";
-  @NonNls public static final String PROJECT_JDK_TYPE_ATTR = "project-jdk-type";
+  @NonNls private static final String PROJECT_JDK_TYPE_ATTR = "project-jdk-type";
 
   protected final Project myProject;
 
@@ -70,13 +70,13 @@ public class ProjectRootManagerImpl extends ProjectRootManagerEx implements Proj
 
   private final OrderRootsCache myRootsCache;
 
-  protected boolean myStartupActivityPerformed = false;
+  protected boolean myStartupActivityPerformed;
 
   private final RootProviderChangeListener myRootProviderChangeListener = new RootProviderChangeListener();
 
   protected class BatchSession {
-    private int myBatchLevel = 0;
-    private boolean myChanged = false;
+    private int myBatchLevel;
+    private boolean myChanged;
 
     private final boolean myFileTypes;
 
@@ -206,7 +206,7 @@ public class ProjectRootManagerImpl extends ProjectRootManagerEx implements Proj
       final VirtualFile[] files = ModuleRootManager.getInstance(module).getContentRoots();
       ContainerUtil.addAll(result, files);
     }
-    result.add(myProject.getBaseDir());
+    ContainerUtil.addIfNotNull(myProject.getBaseDir(), result);
     return VfsUtilCore.toVirtualFileArray(result);
   }
 
@@ -310,9 +310,9 @@ public class ProjectRootManagerImpl extends ProjectRootManagerEx implements Proj
     }
   }
 
-  private boolean myMergedCallStarted = false;
-  private boolean myMergedCallHasRootChange = false;
-  private int myRootsChangesDepth = 0;
+  private boolean myMergedCallStarted;
+  private boolean myMergedCallHasRootChange;
+  private int myRootsChangesDepth;
 
   @Override
   public void mergeRootsChangesDuring(@NotNull Runnable runnable) {
@@ -371,7 +371,7 @@ public class ProjectRootManagerImpl extends ProjectRootManagerEx implements Proj
     return fileTypes ? myFileTypesChanged : myRootsChanged;
   }
 
-  protected boolean isFiringEvent = false;
+  protected boolean isFiringEvent;
 
   private boolean fireBeforeRootsChanged(boolean fileTypes) {
     ApplicationManager.getApplication().assertWriteAccessAllowed();
@@ -484,7 +484,7 @@ public class ProjectRootManagerImpl extends ProjectRootManagerEx implements Proj
     synchronized (myLibraryTableListenersLock) {
       LibraryTableMultiListener multiListener = myLibraryTableMultiListeners.get(libraryTable);
       if (multiListener == null) {
-        multiListener = new LibraryTableMultiListener(libraryTable);
+        multiListener = new LibraryTableMultiListener();
         libraryTable.addListener(multiListener);
         myLibraryTableMultiListeners.put(libraryTable, multiListener);
       }
@@ -511,12 +511,7 @@ public class ProjectRootManagerImpl extends ProjectRootManagerEx implements Proj
 
   private class LibraryTableMultiListener implements LibraryTable.Listener {
     private final Set<LibraryTable.Listener> myListeners = new LinkedHashSet<LibraryTable.Listener>();
-    private final LibraryTable myLibraryTable;
     private LibraryTable.Listener[] myListenersArray;
-
-    private LibraryTableMultiListener(LibraryTable libraryTable) {
-      myLibraryTable = libraryTable;
-    }
 
     private synchronized void addListener(LibraryTable.Listener listener) {
       myListeners.add(listener);
@@ -593,7 +588,7 @@ public class ProjectRootManagerImpl extends ProjectRootManagerEx implements Proj
 
   private class JdkTableMultiListener implements ProjectJdkTable.Listener {
     private final Set<ProjectJdkTable.Listener> myListeners = new LinkedHashSet<ProjectJdkTable.Listener>();
-    private MessageBusConnection listenerConnection;
+    private final MessageBusConnection listenerConnection;
     private ProjectJdkTable.Listener[] myListenersArray;
 
     private JdkTableMultiListener(Project project) {
