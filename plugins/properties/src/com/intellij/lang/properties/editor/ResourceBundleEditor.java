c3f294867f7699d5dd32ad1355976b5043d896fc
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2015-04-02 19:51:47 +0300

    IDEA-86919 navigate to the next incomplete property in resource bundle

--- plugins/properties/src/com/intellij/lang/properties/editor/ResourceBundleEditor.java
+++ plugins/properties/src/com/intellij/lang/properties/editor/ResourceBundleEditor.java
@@ -27,9 +27,11 @@ import com.intellij.ide.structureView.newStructureView.StructureViewComponent;
 import com.intellij.ide.util.treeView.AbstractTreeNode;
 import com.intellij.ide.util.treeView.AbstractTreeUi;
 import com.intellij.ide.util.treeView.smartTree.CachingChildrenTreeNode;
+import com.intellij.ide.util.treeView.smartTree.Sorter;
 import com.intellij.ide.util.treeView.smartTree.TreeElement;
 import com.intellij.lang.properties.IProperty;
 import com.intellij.lang.properties.PropertiesImplUtil;
+import com.intellij.lang.properties.PropertiesUtil;
 import com.intellij.lang.properties.ResourceBundle;
 import com.intellij.lang.properties.psi.PropertiesFile;
 import com.intellij.lang.properties.psi.PropertiesResourceBundleUtil;
@@ -680,6 +682,38 @@ public class ResourceBundleEditor extends UserDataHolderBase implements FileEdit
   public ResourceBundleEditorViewElement getSelectedElementIfOnlyOne() {
     final Collection<ResourceBundleEditorViewElement> selectedElements = getSelectedElements();
     return selectedElements.size() == 1 ? ContainerUtil.getFirstItem(selectedElements) : null;
+  }
+
+  public void selectNextIncompleteProperty() {
+    if (getSelectedNodes().size() != 1) {
+      return;
+    }
+    final IProperty selectedProperty = getSelectedProperty();
+    if (selectedProperty == null) {
+      return;
+    }
+
+    final ResourceBundleFileStructureViewElement root =
+      (ResourceBundleFileStructureViewElement)myStructureViewComponent.getTreeModel().getRoot();
+    final Map<String, IProperty> propertiesMap =
+      ResourceBundleFileStructureViewElement.getPropertiesMap(myResourceBundle, root.isShowOnlyIncomplete());
+    final boolean isAlphaSorted = myStructureViewComponent.isActionActive(Sorter.ALPHA_SORTER_ID);
+    final List<String> keysOrder = new ArrayList<String>(propertiesMap.keySet());
+    if (isAlphaSorted) {
+      Collections.sort(keysOrder);
+    }
+
+    final String currentKey = selectedProperty.getKey();
+    final int idx = keysOrder.indexOf(currentKey);
+    LOG.assertTrue(idx != -1);
+    for (int i = 1; i < keysOrder.size(); i++) {
+      int trimmedIndex = (i + idx) % keysOrder.size();
+      final String key = keysOrder.get(trimmedIndex);
+      if (!PropertiesUtil.isPropertyComplete(myResourceBundle, key)) {
+        selectProperty(key);
+        return;
+      }
+    }
   }
 
   @Override
