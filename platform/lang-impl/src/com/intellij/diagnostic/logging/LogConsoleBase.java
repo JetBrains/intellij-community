f22ab2b1671a965d0169b61e6126336df27860e4
Eugene Kudelevsky <Eugene.Kudelevsky@jetbrains.com>
2015-04-03 17:40:51 +0300

    IDEA-138665 Add ability to format log messages in logcat, requires change to LogConsoleBase

--- platform/lang-impl/src/com/intellij/diagnostic/logging/LogConsoleBase.java
+++ platform/lang-impl/src/com/intellij/diagnostic/logging/LogConsoleBase.java
@@ -81,6 +81,7 @@ public abstract class LogConsoleBase extends AdditionalTabComponent implements L
   private ActionGroup myActions;
   private final boolean myBuildInActions;
   private LogFilterModel myModel;
+  private final LogFormatter myFormatter;
 
   private final List<LogConsoleListener> myListeners = ContainerUtil.createLockFreeCopyOnWriteList();
   private final List<? extends LogFilter> myFilters;
@@ -106,11 +107,17 @@ public abstract class LogConsoleBase extends AdditionalTabComponent implements L
   }
 
   public LogConsoleBase(@NotNull Project project, @Nullable Reader reader, String title, final boolean buildInActions, LogFilterModel model,
-                        @NotNull GlobalSearchScope scope) {
+                        @NotNull GlobalSearchScope scope){
+    this(project, reader, title, buildInActions, model, scope, new DefaultLogFormatter());
+  }
+
+  public LogConsoleBase(@NotNull Project project, @Nullable Reader reader, String title, final boolean buildInActions, LogFilterModel model,
+                        @NotNull GlobalSearchScope scope, LogFormatter formatter) {
     super(new BorderLayout());
     myProject = project;
     myTitle = title;
     myModel = model;
+    myFormatter = formatter;
     myFilters = myModel.getLogFilters();
     myReaderThread = new ReaderThread(reader);
     myBuildInActions = buildInActions;
@@ -311,7 +318,8 @@ public abstract class LogConsoleBase extends AdditionalTabComponent implements L
       final List<LogFragment> fragments = myContentPreprocessor.parseLogLine(text + "\n");
       myOriginalDocument = getOriginalDocument();
       for (LogFragment fragment : fragments) {
-        myProcessHandler.notifyTextAvailable(fragment.getText(), fragment.getOutputType());
+        String formattedMessage = myFormatter.formatMessage(fragment.getText());
+        myProcessHandler.notifyTextAvailable(formattedMessage, fragment.getOutputType());
         if (myOriginalDocument != null) {
           myOriginalDocument.append(fragment.getText());
         }
@@ -324,9 +332,11 @@ public abstract class LogConsoleBase extends AdditionalTabComponent implements L
         if (key != null) {
           final String messagePrefix = processingResult.getMessagePrefix();
           if (messagePrefix != null) {
-            myProcessHandler.notifyTextAvailable(messagePrefix, key);
+            String formattedPrefix = myFormatter.formatPrefix(messagePrefix);
+            myProcessHandler.notifyTextAvailable(formattedPrefix, key);
           }
-          myProcessHandler.notifyTextAvailable(text + "\n", key);
+          String formattedMessage = myFormatter.formatMessage(text);
+          myProcessHandler.notifyTextAvailable(formattedMessage + "\n", key);
         }
       }
       myOriginalDocument = getOriginalDocument();
@@ -457,7 +467,8 @@ public abstract class LogConsoleBase extends AdditionalTabComponent implements L
       for (LogFragment fragment : fragments) {
         ConsoleViewContentType consoleViewType = ConsoleViewContentType.getConsoleViewType(fragment.getOutputType());
         if (consoleViewType != null) {
-          console.print(fragment.getText(), consoleViewType);
+          String formattedText = myFormatter.formatMessage(fragment.getText());
+          console.print(formattedText, consoleViewType);
         }
       }
       return line.length() + 1;
@@ -471,9 +482,11 @@ public abstract class LogConsoleBase extends AdditionalTabComponent implements L
           if (type != null) {
             final String messagePrefix = processingResult.getMessagePrefix();
             if (messagePrefix != null) {
-              console.print(messagePrefix, type);
+              String formattedPrefix = myFormatter.formatPrefix(messagePrefix);
+              console.print(formattedPrefix, type);
             }
-            console.print(line + "\n", type);
+            String formattedMessage = myFormatter.formatMessage(line);
+            console.print(formattedMessage + "\n", type);
             return (messagePrefix != null ? messagePrefix.length() : 0) + line.length() + 1;
           }
         }
