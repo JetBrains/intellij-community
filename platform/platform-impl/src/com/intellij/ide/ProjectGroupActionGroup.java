5cac85049a8982755804b53bf7ba666830d3a9dd
Konstantin Bulenkov <kb@jetbrains.com>
2015-04-08 14:17:59 +0200

    wrap group of actions into AnAction

---
+++ platform/platform-impl/src/com/intellij/ide/ProjectGroupActionGroup.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.ide;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.openapi.project.DumbAware;
+
+import java.util.List;
+
+/**
+ * @author Konstantin Bulenkov
+ */
+public class ProjectGroupActionGroup extends DefaultActionGroup implements DumbAware {
+  private final ProjectGroup myGroup;
+
+  public ProjectGroupActionGroup(ProjectGroup group, List<AnAction> children) {
+    super(group.getName(), children);
+    myGroup = group;
+  }
+
+  @Override
+  public boolean isPopup() {
+    return !myGroup.isExpanded();
+  }
+
+  public ProjectGroup getGroup() {
+    return myGroup;
+  }
+}
