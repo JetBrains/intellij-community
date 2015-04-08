8246b65a9d039bed4802b3b31cfd852ccac5b969
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2015-04-08 21:29:21 +0300

    Initial support for argparse (django 1.8) for  PY-11855 (manage.py).
    (cherry picked from commit 9df9071511f123f2bda1b99c052d5d8a6e815b27)

--- python/helpers/pycharm/django_manage_commands_provider/provider.py
+++ python/helpers/pycharm/django_manage_commands_provider/provider.py
@@ -1,25 +1,23 @@
 # coding=utf-8
 """
 This is an entry point of this helper.
-It fetches data from Django manage commands via _optparse module and report is via _xml module.
+It fetches data from Django manage commands delegating calles to _parser package report it via _xml module.
 See _xml module and readme.txt for more info.
 
 Module can be called directly, but be sure env var DJANGO_SETTINGS_MODULE is set to something like "mysite.settings"
 """
 
-from distutils.version import LooseVersion
 import django
-import _optparse
+
+from _parser import parser
 import _xml
+
 
 __author__ = 'Ilya.Kazakevich'
 
-# TODO: Support Django 1.8 as well, it uses argparse, not optparse
-version = LooseVersion(django.get_version())
-assert version < LooseVersion('1.8a'), "Only Django <1.8 is supported now"
 # Some django versions require setup
 if hasattr(django, 'setup'):
     django.setup()
 dumper = _xml.XmlDumper()
-_optparse.report_data(dumper)
+parser.report_data(dumper)
 print(dumper.xml)
