1a87b74a17a5c0bd9b7adb9ff54f32e8869f7dd5
Bas Leijdekkers <basleijdekkers@gmail.com>
2015-04-10 13:21:49 +0200

    IDEA-139005 (Missing @Override annotation inspection should be enabled by default)

--- plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/inheritance/MissingOverrideAnnotationInspection.java
+++ plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/inheritance/MissingOverrideAnnotationInspection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -73,6 +73,11 @@ public class MissingOverrideAnnotationInspection extends BaseInspection {
       "ignore.methods.in.anonymous.classes"),
                       "ignoreAnonymousClassMethods");
     return panel;
+  }
+
+  @Override
+  public boolean isEnabledByDefault() {
+    return true;
   }
 
   @Override
