63281cf98ebaf5e117de5548679f7e6fbacb0d55
Bas Leijdekkers <basleijdekkers@gmail.com>
2015-04-10 13:30:28 +0200

    make IG test light

--- plugins/InspectionGadgets/test/com/siyeh/igtest/style/unnecessary_constant_array_creation_expression/UnnecessaryConstantArrayCreationExpression.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/style/unnecessary_constant_array_creation_expression/UnnecessaryConstantArrayCreationExpression.java
@@ -1,6 +1,6 @@
 package com.siyeh.igtest.style.unnecessary_constant_array_creation_expression;
 
 public class UnnecessaryConstantArrayCreationExpression {
-    private Class[] classes1 = new Class[]{};
+    private Class[] classes1 = <warning descr="'new Class[]{}' can be replaced with array initializer expression">new Class[]{}</warning>;
     private Class<String>[] classes2 = new Class[]{};
 }
