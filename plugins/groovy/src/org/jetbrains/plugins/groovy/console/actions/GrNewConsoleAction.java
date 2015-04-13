e835f35c4471e4890c9fc97bfcc499c2c1bf5a90
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2015-04-13 16:15:54 +0300

    [groovy] provide ability to run console with module from data context

--- plugins/groovy/src/org/jetbrains/plugins/groovy/console/actions/GrNewConsoleAction.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/console/actions/GrNewConsoleAction.java
@@ -19,10 +19,13 @@ import com.intellij.execution.console.ConsoleHistoryController;
 import com.intellij.ide.scratch.ScratchFileService;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleUtilCore;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.groovy.console.GroovyConsole;
 import org.jetbrains.plugins.groovy.console.GroovyConsoleRootType;
 import org.jetbrains.plugins.groovy.util.ModuleChooserUtil;
@@ -35,27 +38,37 @@ public class GrNewConsoleAction extends AnAction {
 
   @Override
   public void update(AnActionEvent e) {
-    final Project project = e.getProject();
-    e.getPresentation().setEnabledAndVisible(
-      project != null && !ModuleChooserUtil.getGroovyCompatibleModules(project, APPLICABLE_MODULE).isEmpty()
-    );
+    e.getPresentation().setEnabledAndVisible(getModule(e) != null);
   }
 
   @Override
   public void actionPerformed(AnActionEvent e) {
     final Project project = e.getProject();
-    if (project == null) return;
-    final List<Module> modules = ModuleChooserUtil.getGroovyCompatibleModules(project, APPLICABLE_MODULE);
-    if (!modules.isEmpty()) {
-      final Module module = modules.get(0);
-      final VirtualFile contentFile = ConsoleHistoryController.getContentFile(
-        GroovyConsoleRootType.getInstance(),
-        "groovy-console",
-        ScratchFileService.Option.create_new_always
-      );
-      assert contentFile != null;
-      GroovyConsole.createConsole(project, contentFile, module);
-      FileEditorManager.getInstance(project).openFile(contentFile, true);
+    final Module module = getModule(e);
+    if (project == null || module == null) return;
+
+    final VirtualFile contentFile = ConsoleHistoryController.getContentFile(
+      GroovyConsoleRootType.getInstance(),
+      "groovy-console",
+      ScratchFileService.Option.create_new_always
+    );
+    assert contentFile != null;
+    GroovyConsole.createConsole(project, contentFile, module);
+    FileEditorManager.getInstance(project).openFile(contentFile, true);
+  }
+
+  @Nullable
+  protected Module getModule(AnActionEvent e) {
+    final Project project = e.getProject();
+    if (project == null) return null;
+    final VirtualFile file = CommonDataKeys.VIRTUAL_FILE.getData(e.getDataContext());
+    
+    if (file != null) {
+      final Module moduleForFile = ModuleUtilCore.findModuleForFile(file, project);
+      if (moduleForFile != null) return moduleForFile;
     }
+
+    final List<Module> modules = ModuleChooserUtil.getGroovyCompatibleModules(project, APPLICABLE_MODULE);
+    return modules.isEmpty() ? null : modules.get(0);
   }
 }
