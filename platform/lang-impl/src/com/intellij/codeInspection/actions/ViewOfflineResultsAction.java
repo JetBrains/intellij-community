f3e0bee5d0bd426dfe1f425aecfdd5345b2cb4b4
Alexey Kudravtsev <cdr@intellij.com>
2015-04-17 18:02:45 +0300

    notnull

--- platform/lang-impl/src/com/intellij/codeInspection/actions/ViewOfflineResultsAction.java
+++ platform/lang-impl/src/com/intellij/codeInspection/actions/ViewOfflineResultsAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -214,7 +214,7 @@ public class ViewOfflineResultsAction extends AnAction implements DumbAware {
   @NotNull
   public static InspectionResultsView showOfflineView(@NotNull Project project,
                                                       @NotNull Map<String, Map<String, Set<OfflineProblemDescriptor>>> resMap,
-                                                      final InspectionProfile inspectionProfile,
+                                                      @NotNull InspectionProfile inspectionProfile,
                                                       final String title) {
     final AnalysisScope scope = new AnalysisScope(project);
     final InspectionManagerEx managerEx = (InspectionManagerEx)InspectionManager.getInstance(project);
