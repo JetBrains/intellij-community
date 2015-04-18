a94eb9a920b94e57cff3aa2f41eea3796804372a
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2015-04-18 13:50:28 +0200

    project: compilation fix

--- json/src/com/intellij/json/JsonScriptContentProvider.java
+++ json/src/com/intellij/json/JsonScriptContentProvider.java
@@ -13,23 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.intellij.json;/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package com.intellij.json;
 
-import com.intellij.javascript.JSScriptContentProvider;
 import com.intellij.lang.HtmlScriptContentProvider;
 import com.intellij.lexer.Lexer;
 import com.intellij.psi.tree.IElementType;
@@ -57,7 +42,7 @@ public final class JsonScriptContentProvider implements HtmlScriptContentProvide
     /**
      * We do not have highlighting JSON lexer for embedded environment for now, so we reuse JScript
      */
-    return new JSScriptContentProvider().getHighlightingLexer();
+    return null; //new JSScriptContentProvider().getHighlightingLexer();
   }
 
   private static final class JsonEmbedded extends ILazyParseableElementType {
