a3c084b553fb442fb17575d26f6e96c0572a9966
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2015-04-21 10:30:09 +0200

    fix mime-types

--- java/java-psi-api/src/com/intellij/lang/java/JavaLanguage.java
+++ java/java-psi-api/src/com/intellij/lang/java/JavaLanguage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,7 +26,7 @@ public class JavaLanguage extends Language {
   public static final JavaLanguage INSTANCE = new JavaLanguage();
 
   private JavaLanguage() {
-    super("JAVA", "text/java", "application/x-java", "text/x-java");
+    super("JAVA", "text/x-java-source", "text/java", "application/x-java", "text/x-java");
   }
 
   @NotNull
