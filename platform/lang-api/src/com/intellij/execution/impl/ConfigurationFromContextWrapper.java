c2724af029b718b8e6fc59017319dadc3f877778
Dmitry Jemerov <yole@jetbrains.com>
2015-04-23 14:22:16 +0200

    integrate https://android-review.googlesource.com/#/c/145848/: add API allowing one RunConfigurationProducer to replace another one (cherry picked from commit 6a3ed85)

--- platform/lang-api/src/com/intellij/execution/impl/ConfigurationFromContextWrapper.java
+++ platform/lang-api/src/com/intellij/execution/impl/ConfigurationFromContextWrapper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -60,5 +60,11 @@ public class ConfigurationFromContextWrapper extends ConfigurationFromContext {
   public boolean isPreferredTo(ConfigurationFromContext other) {
     return other instanceof ConfigurationFromContextWrapper &&
            myProducer.compareTo(((ConfigurationFromContextWrapper) other).myProducer) < 0;
+  }
+
+  @Override
+  public boolean shouldReplace(ConfigurationFromContext other) {
+    return other instanceof ConfigurationFromContextWrapper &&
+           myProducer.compareTo(((ConfigurationFromContextWrapper) other).myProducer) > 0;
   }
 }
