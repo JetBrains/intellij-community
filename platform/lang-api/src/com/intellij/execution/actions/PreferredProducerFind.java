9cd676566a2a61fddb83d6249f597a3a7796b67d
Dmitry Jemerov <yole@jetbrains.com>
2015-04-23 14:20:49 +0200

    integrate https://android-review.googlesource.com/#/c/145881/ : add API for ignoring certain RunConfigurationProducers in a project

--- platform/lang-api/src/com/intellij/execution/actions/PreferredProducerFind.java
+++ platform/lang-api/src/com/intellij/execution/actions/PreferredProducerFind.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -102,7 +102,7 @@ class PreferredProducerFind {
       configurationsFromContext.add(new ConfigurationFromContextWrapper(producer));
     }
 
-    for (RunConfigurationProducer producer : Extensions.getExtensions(RunConfigurationProducer.EP_NAME)) {
+    for (RunConfigurationProducer producer : RunConfigurationProducer.getProducers(context.getProject())) {
       ConfigurationFromContext fromContext = producer.findOrCreateConfigurationFromContext(context);
       if (fromContext != null) {
         configurationsFromContext.add(fromContext);
