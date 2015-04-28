954f04de38a6a570fab425122b8c45f95dce7102
Alexey Kudravtsev <cdr@intellij.com>
2015-04-28 14:46:08 +0300

    made fields final

--- java/java-psi-impl/src/com/intellij/psi/impl/java/stubs/PsiJavaFileStub.java
+++ java/java-psi-impl/src/com/intellij/psi/impl/java/stubs/PsiJavaFileStub.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,5 +26,6 @@ public interface PsiJavaFileStub extends PsiClassHolderFileStub<PsiJavaFile> {
   String getPackageName();
   boolean isCompiled();
   StubPsiFactory getPsiFactory();
+  @Deprecated
   void setPsiFactory(StubPsiFactory factory);
 }
--- java/java-psi-impl/src/com/intellij/psi/impl/java/stubs/impl/PsiJavaFileStubImpl.java
+++ java/java-psi-impl/src/com/intellij/psi/impl/java/stubs/impl/PsiJavaFileStubImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,21 +21,22 @@ import com.intellij.psi.PsiJavaFile;
 import com.intellij.psi.impl.java.stubs.*;
 import com.intellij.psi.stubs.PsiFileStubImpl;
 import com.intellij.psi.tree.IStubFileElementType;
+import com.intellij.util.IncorrectOperationException;
 import com.intellij.util.io.StringRef;
 
 /*
  * @author max
  */
 public class PsiJavaFileStubImpl extends PsiFileStubImpl<PsiJavaFile> implements PsiJavaFileStub {
-  private StringRef myPackageName;
+  private static final ClsStubPsiFactory CLS_FACTORY = new ClsStubPsiFactory();
+  private static final SourceStubPsiFactory SOURCE_FACTORY = new SourceStubPsiFactory();
+  private final StringRef myPackageName;
   private final boolean myCompiled;
-  private StubPsiFactory myFactory;
 
   public PsiJavaFileStubImpl(final PsiJavaFile file, final StringRef packageName, final boolean compiled) {
     super(file);
     myPackageName = packageName;
     myCompiled = compiled;
-    myFactory = compiled ? new ClsStubPsiFactory() : new SourceStubPsiFactory();
   }
 
   public PsiJavaFileStubImpl(final String packageName, final boolean compiled) {
@@ -54,16 +55,18 @@ public class PsiJavaFileStubImpl extends PsiFileStubImpl<PsiJavaFile> implements
 
   @Override
   public StubPsiFactory getPsiFactory() {
-    return myFactory;
+    return myCompiled ? CLS_FACTORY : SOURCE_FACTORY;
   }
 
   @Override
   public void setPsiFactory(StubPsiFactory factory) {
-    myFactory = factory;
+    throw new IncorrectOperationException();
   }
 
+  @Deprecated // use constructor
   public void setPackageName(final String packageName) {
-    myPackageName = StringRef.fromString(packageName);
+    //myPackageName = StringRef.fromString(packageName);
+    throw new IncorrectOperationException();
   }
 
   @Override
@@ -71,7 +74,7 @@ public class PsiJavaFileStubImpl extends PsiFileStubImpl<PsiJavaFile> implements
     return JavaStubElementTypes.JAVA_FILE;
   }
 
-  @SuppressWarnings({"HardCodedStringLiteral"})
+  @SuppressWarnings("HardCodedStringLiteral")
   public String toString() {
     return "PsiJavaFileStub [" + myPackageName + "]";
   }
