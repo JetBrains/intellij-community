f871f0e3e3f455d9b8b2d4eedc83453ab0e7aea6
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2015-04-28 17:12:36 +0300

    [vcs-log] fix IllegalArgumentException (invoke later after dispose) and try to prevent another (cherry picked from commit 15afd0f)

--- platform/vcs-log/impl/src/com/intellij/vcs/log/impl/VcsLogManager.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/impl/VcsLogManager.java
@@ -84,7 +84,7 @@ public class VcsLogManager implements Disposable {
           UIUtil.invokeLaterIfNeeded(new Runnable() {
             @Override
             public void run() {
-              if (!Disposer.isDisposed(myUi)) {
+              if (myUi != null && !Disposer.isDisposed(myUi)) {
                 myUi.setVisiblePack(pack);
               }
             }
@@ -106,7 +106,7 @@ public class VcsLogManager implements Disposable {
         }
       };
     }
-    refreshLogOnVcsEvents(logProviders);
+    refreshLogOnVcsEvents(logProviders, myLogRefresher);
     logDataHolder.initialize();
 
     // todo fix selection
@@ -122,14 +122,14 @@ public class VcsLogManager implements Disposable {
     return myUi.getMainFrame().getMainComponent();
   }
 
-  private void refreshLogOnVcsEvents(@NotNull Map<VirtualFile, VcsLogProvider> logProviders) {
+  private static void refreshLogOnVcsEvents(@NotNull Map<VirtualFile, VcsLogProvider> logProviders, @NotNull VcsLogRefresher refresher) {
     MultiMap<VcsLogProvider, VirtualFile> providers2roots = MultiMap.create();
     for (Map.Entry<VirtualFile, VcsLogProvider> entry : logProviders.entrySet()) {
       providers2roots.putValue(entry.getValue(), entry.getKey());
     }
 
     for (Map.Entry<VcsLogProvider, Collection<VirtualFile>> entry : providers2roots.entrySet()) {
-      entry.getKey().subscribeToRootRefreshEvents(entry.getValue(), myLogRefresher);
+      entry.getKey().subscribeToRootRefreshEvents(entry.getValue(), refresher);
     }
   }
 
