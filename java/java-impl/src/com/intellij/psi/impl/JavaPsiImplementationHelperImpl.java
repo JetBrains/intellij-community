4f3f0e27ebd237a09bf31d4cec38453718520423
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2015-05-04 19:51:32 +0200

    EA-50553 (file validity check added)

--- java/java-impl/src/com/intellij/psi/impl/JavaPsiImplementationHelperImpl.java
+++ java/java-impl/src/com/intellij/psi/impl/JavaPsiImplementationHelperImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -115,7 +115,7 @@ public class JavaPsiImplementationHelperImpl extends JavaPsiImplementationHelper
       if (!(orderEntry instanceof LibraryOrSdkOrderEntry)) continue;
       for (VirtualFile root : orderEntry.getFiles(OrderRootType.SOURCES)) {
         VirtualFile source = root.findFileByRelativePath(relativePath);
-        if (source != null) {
+        if (source != null && source.isValid()) {
           PsiFile psiSource = clsFile.getManager().findFile(source);
           if (psiSource instanceof PsiClassOwner) {
             return psiSource;
