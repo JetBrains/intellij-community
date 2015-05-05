db02da1bdd51dbf6e1bb0e6e5fc13b5ff5cd5cdd
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2015-05-05 14:16:43 +0300

    fixed resolve to the keyword argument introduced in 8b9e7981df930b037924d1df8aa72f1a52a9cd82
    needed for PY-15366 No completion for matplotlib figure
    
    (cherry picked from commit c882d5d5edcdc18f9b98af0c2a50ec9ffdbcb91a)

--- python/testSrc/com/jetbrains/python/PyMultiFileResolveTest.java
+++ python/testSrc/com/jetbrains/python/PyMultiFileResolveTest.java
@@ -15,7 +15,9 @@
  */
 package com.jetbrains.python;
 
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.*;
+import com.intellij.psi.impl.source.PsiFileImpl;
 import com.jetbrains.python.fixtures.PyMultiFileResolveTestCase;
 import com.jetbrains.python.fixtures.PyTestCase;
 import com.jetbrains.python.psi.*;
@@ -382,5 +384,22 @@ public class PyMultiFileResolveTest extends PyMultiFileResolveTestCase {
         assertResolvesTo(PyFile.class, "m1.py");
       }
     });
+  }
+
+  public void testKeywordArgument() {
+    final PsiFile file = prepareFile();
+    final PsiManager psiManager = myFixture.getPsiManager();
+    final VirtualFile dir = myFixture.findFileInTempDir("a.py");
+    final PsiFile psiFile = psiManager.findFile(dir);
+    //noinspection ConstantConditions   we need to unstub a.py here
+    ((PsiFileImpl)psiFile).calcTreeElement();
+    final PsiElement element;
+    try {
+      element = doResolve(file);
+    }
+    catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+    assertResolveResult(element, PyClass.class, "A");
   }
 }
