58a549f5aae08167d39c7e97c87488e223371375
irengrig <Irina.Chernushina@jetbrains.com>
2015-05-08 14:05:51 +0200

    tree table: correctly define if line is focused; do not always (have setting) paint selection background for darcula/aqua for tree table as if it has focus

--- platform/util/src/com/intellij/util/ui/tree/WideSelectionTreeUI.java
+++ platform/util/src/com/intellij/util/ui/tree/WideSelectionTreeUI.java
@@ -38,6 +38,7 @@ import java.awt.event.MouseListener;
 */
 public class WideSelectionTreeUI extends BasicTreeUI {
   public static final String TREE_TABLE_TREE_KEY = "TreeTableTree";
+  public static final String NO_SELECTION_PAINTING_DARK = "NO_SELECTION_PAINTING_DARK";
 
   @NonNls public static final String SOURCE_LIST_CLIENT_PROPERTY = "mac.ui.source.list";
   @NonNls public static final String STRIPED_CLIENT_PROPERTY = "mac.ui.striped";
@@ -361,7 +362,8 @@ public class WideSelectionTreeUI extends BasicTreeUI {
         }
       }
       else {
-        if (selected && (UIUtil.isUnderAquaBasedLookAndFeel() || UIUtil.isUnderDarcula() || UIUtil.isUnderIntelliJLaF())) {
+        if (selected && (UIUtil.isUnderAquaBasedLookAndFeel() || UIUtil.isUnderDarcula() || UIUtil.isUnderIntelliJLaF())
+            && ! Boolean.TRUE.equals(tree.getClientProperty(NO_SELECTION_PAINTING_DARK))) {
           Color bg = UIUtil.getTreeSelectionBackground(tree.hasFocus() || Boolean.TRUE.equals(tree.getClientProperty(TREE_TABLE_TREE_KEY)));
 
           if (myWideSelectionCondition.value(row)) {
