63959b7cde92df455f6c41628b183b51c0b5e70c
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2015-05-13 20:29:02 +0300

    IDEA-63980 Make idea.cycle.buffer.size configurable in IDEA GUI / IDEA settings revert

--- platform/lang-impl/src/com/intellij/execution/impl/ConsoleBuffer.java
+++ platform/lang-impl/src/com/intellij/execution/impl/ConsoleBuffer.java
@@ -120,15 +120,11 @@ public class ConsoleBuffer {
   }
 
   public static boolean useCycleBuffer() {
-    if (!RunConfigurable.MyConsoleBufferSlider.isBufferCycleEnabled()) return false;
     final String useCycleBufferProperty = System.getProperty("idea.cycle.buffer.size");
     return useCycleBufferProperty == null || !"disabled".equalsIgnoreCase(useCycleBufferProperty);
   }
 
   public static int getCycleBufferSize() {
-    if (!RunConfigurable.MyConsoleBufferSlider.isBufferCycleDefault()) {
-      return RunConfigurable.MyConsoleBufferSlider.getBufferSize();
-    }
     String cycleBufferSizeProperty = System.getProperty("idea.cycle.buffer.size");
     if (cycleBufferSizeProperty == null) return 1024 * 1024;
     try {
