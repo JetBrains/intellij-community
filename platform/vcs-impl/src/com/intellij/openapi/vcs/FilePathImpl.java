c54f63a9af860250e81c85fae4274e67e0cc1dc6
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2015-05-14 18:48:45 +0300

    [vcs] remove unused constructor

--- platform/vcs-impl/src/com/intellij/openapi/vcs/FilePathImpl.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/FilePathImpl.java
@@ -280,10 +280,6 @@ public class FilePathImpl implements FilePath {
     return myVirtualFile != null ? myVirtualFile.getFileType() : FileTypeManager.getInstance().getFileTypeByFileName(myFile.getName());
   }
 
-  public static FilePathImpl create(VirtualFile file) {
-    return create(VfsUtilCore.virtualToIoFile(file), file.isDirectory());
-  }
-
   public static FilePathImpl create(File selectedFile) {
     return create(selectedFile, false);
   }
