2edb4ff8873bf3d1d316ec104a865c48a31e6781
Alexey Kudravtsev <cdr@intellij.com>
2015-05-14 21:11:25 +0300

    notnull

--- platform/core-impl/src/com/intellij/openapi/editor/impl/PersistentRangeMarker.java
+++ platform/core-impl/src/com/intellij/openapi/editor/impl/PersistentRangeMarker.java
@@ -20,6 +20,7 @@ import com.intellij.openapi.editor.event.DocumentEvent;
 import com.intellij.openapi.editor.ex.DocumentEx;
 import com.intellij.openapi.editor.impl.event.DocumentEventImpl;
 import com.intellij.util.diff.FilesTooBigForDiffException;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * This class is an extension to range marker that tries to restore its range even in situations when target text referenced by it
@@ -98,7 +99,7 @@ class PersistentRangeMarker extends RangeMarkerImpl {
   }
 
   @Override
-  protected void changedUpdateImpl(DocumentEvent e) {
+  protected void changedUpdateImpl(@NotNull DocumentEvent e) {
     DocumentEventImpl event = (DocumentEventImpl)e;
     final boolean shouldTranslateViaDiff = PersistentRangeMarkerUtil.shouldTranslateViaDiff(event, this);
     boolean wasTranslated = shouldTranslateViaDiff;
