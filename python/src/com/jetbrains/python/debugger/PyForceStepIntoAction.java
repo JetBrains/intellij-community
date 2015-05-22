4e1274d3cc5f3e5da9f9d2a6afb8d0ba65b999bf
Elizaveta Shashkova <Elizaveta.Shashkova@jetbrains.com>
2015-05-22 15:40:53 +0300

    Step Into My Code (PY-15500): include in python plugin (cherry picked from commit 687110d)

--- python/src/com/jetbrains/python/debugger/PyForceStepIntoAction.java
+++ python/src/com/jetbrains/python/debugger/PyForceStepIntoAction.java
@@ -18,26 +18,44 @@ package com.jetbrains.python.debugger;
 import com.intellij.execution.RunManager;
 import com.intellij.execution.RunnerAndConfigurationSettings;
 import com.intellij.execution.configurations.RunConfiguration;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.project.Project;
+import com.intellij.xdebugger.XDebugSession;
+import com.intellij.xdebugger.impl.DebuggerSupport;
+import com.intellij.xdebugger.impl.actions.DebuggerActionHandler;
 import com.intellij.xdebugger.impl.actions.ForceStepIntoAction;
+import com.intellij.xdebugger.impl.actions.XDebuggerSuspendedActionHandler;
 import com.jetbrains.python.run.AbstractPythonRunConfiguration;
+import org.jetbrains.annotations.NotNull;
 
 public class PyForceStepIntoAction extends ForceStepIntoAction {
-  @Override
-  protected boolean isEnabled(AnActionEvent e) {
-    Project project = e.getData(CommonDataKeys.PROJECT);
-    if (project != null) {
-      RunnerAndConfigurationSettings settings = RunManager.getInstance(project).getSelectedConfiguration();
-      if (settings != null) {
-        RunConfiguration runConfiguration = settings.getConfiguration();
-        if (runConfiguration instanceof AbstractPythonRunConfiguration) {
-          return false;
-        }
-      }
-    }
+  private final XDebuggerSuspendedActionHandler myPyForceStepIntoHandler;
 
-    return super.isEnabled(e);
+  public PyForceStepIntoAction() {
+    myPyForceStepIntoHandler = new XDebuggerSuspendedActionHandler() {
+      @Override
+      protected void perform(@NotNull final XDebugSession session, final DataContext dataContext) {
+        session.forceStepInto();
+      }
+
+      @Override
+      public boolean isEnabled(@NotNull XDebugSession session, final DataContext dataContext) {
+        Project project = CommonDataKeys.PROJECT.getData(dataContext);
+        RunnerAndConfigurationSettings settings = RunManager.getInstance(project).getSelectedConfiguration();
+        if (settings != null) {
+          RunConfiguration runConfiguration = settings.getConfiguration();
+          if (runConfiguration instanceof AbstractPythonRunConfiguration) {
+            return false;
+          }
+        }
+        return super.isEnabled(session, dataContext);
+      }
+    };
+  }
+
+  @NotNull
+  @Override
+  protected DebuggerActionHandler getHandler(@NotNull DebuggerSupport debuggerSupport) {
+    return myPyForceStepIntoHandler;
   }
 }
