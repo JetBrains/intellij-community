29a23b05d6ebd3e001cb8b76141e7340d4053a0c
Andrey Vlasovskikh <andrey.vlasovskikh@gmail.com>
2015-05-22 16:16:13 +0200

    Use the highest language level among the open projects as the guess (PY-15898)
    If the user has several projects (modules) open in the same window,
    we used to select the language level of the first project which was
    confusing.

--- python/src/com/jetbrains/python/psi/PyUtil.java
+++ python/src/com/jetbrains/python/psi/PyUtil.java
@@ -832,11 +832,18 @@ public class PyUtil {
   public static LanguageLevel guessLanguageLevel(@NotNull Project project) {
     final ModuleManager moduleManager = ModuleManager.getInstance(project);
     if (moduleManager != null) {
+      LanguageLevel maxLevel = null;
       for (Module projectModule : moduleManager.getModules()) {
         final Sdk sdk = PythonSdkType.findPythonSdk(projectModule);
         if (sdk != null) {
-          return PythonSdkType.getLanguageLevelForSdk(sdk);
+          final LanguageLevel level = PythonSdkType.getLanguageLevelForSdk(sdk);
+          if (maxLevel == null || maxLevel.isOlderThan(level)) {
+            maxLevel = level;
+          }
         }
+      }
+      if (maxLevel != null) {
+        return maxLevel;
       }
     }
     return LanguageLevel.getDefault();
