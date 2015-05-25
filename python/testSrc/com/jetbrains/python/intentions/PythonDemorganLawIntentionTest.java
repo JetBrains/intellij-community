f7078d5a88b883864e89e12d1cdc31463e068520
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2015-05-25 18:56:28 +0300

    Cleanup in PyDemorganLawIntention and its tests
    * Added missing final modifiers
    * Reformat file
    * Element generator is aware of language level
    * Moved intention name to the bundle and properly capitalized it
    * PythonDemorganLawIntentionTest extends convenient PyIntentionTestCase
    and follows its conventions for test data location and naming

--- python/testSrc/com/jetbrains/python/intentions/PythonDemorganLawIntentionTest.java
+++ python/testSrc/com/jetbrains/python/intentions/PythonDemorganLawIntentionTest.java
@@ -20,36 +20,22 @@
  */
 package com.jetbrains.python.intentions;
 
-import com.intellij.codeInsight.intention.IntentionAction;
-import com.jetbrains.python.PythonTestUtil;
-import com.jetbrains.python.fixtures.PyTestCase;
+import com.jetbrains.python.PyBundle;
 
-public class PythonDemorganLawIntentionTest extends PyTestCase {
-  public void test1() throws Exception {
-    doTest();
+public class PythonDemorganLawIntentionTest extends PyIntentionTestCase {
+  public void testOr() {
+    doIntentionTest(PyBundle.message("INTN.demorgan.law"));
   }
 
-  public void test2() throws Exception {
-    doTest();
+  public void testNotOr() {
+    doIntentionTest(PyBundle.message("INTN.demorgan.law"));
   }
 
-  public void test3() throws Exception {
-    doTest();
+  public void testOrNot() {
+    doIntentionTest(PyBundle.message("INTN.demorgan.law"));
   }
 
-  public void test4() throws Exception {
-    doTest();
-  }
-
-  private void doTest() throws Exception {
-    myFixture.configureByFile("before" + getTestName(false) + ".py");
-    final IntentionAction action = myFixture.findSingleIntention("DeMorgan Law");
-    myFixture.launchAction(action);
-    myFixture.checkResultByFile("after" + getTestName(false) + ".py");
-
-  }
-  @Override
-  protected String getTestDataPath() {
-    return PythonTestUtil.getTestDataPath() + "/intentions/demorgan";
+  public void testComplex() {
+    doIntentionTest(PyBundle.message("INTN.demorgan.law"));
   }
 }
