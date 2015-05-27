959704f97ddb57022b28e23302cfbfed7d64266d
Alexey Kudravtsev <cdr@intellij.com>
2015-05-27 16:52:00 +0300

    cleanup, weaken visibility

--- platform/core-impl/src/com/intellij/psi/impl/smartPointers/SmartPointerEx.java
+++ platform/core-impl/src/com/intellij/psi/impl/smartPointers/SmartPointerEx.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.SmartPsiElementPointer;
 import org.jetbrains.annotations.Nullable;
 
-public interface SmartPointerEx<E extends PsiElement> extends SmartPsiElementPointer<E> {
+interface SmartPointerEx<E extends PsiElement> extends SmartPsiElementPointer<E> {
   void fastenBelt(int offset, @Nullable RangeMarker[] cachedRangeMarkers);
 
   void unfastenBelt(int offset);
