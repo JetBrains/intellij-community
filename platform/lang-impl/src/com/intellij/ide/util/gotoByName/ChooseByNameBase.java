9427a33db19b16b2714d93d47a70d5e7e4dbf212
Konstantin Bulenkov <kb@jetbrains.com>
2015-05-27 18:31:40 +0200

    return search input in data provder

--- platform/lang-impl/src/com/intellij/ide/util/gotoByName/ChooseByNameBase.java
+++ platform/lang-impl/src/com/intellij/ide/util/gotoByName/ChooseByNameBase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -309,6 +309,9 @@ public abstract class ChooseByNameBase {
       }
       else if (PlatformDataKeys.DOMINANT_HINT_AREA_RECTANGLE.is(dataId)) {
         return getBounds();
+      }
+      else if (PlatformDataKeys.SEARCH_INPUT_TEXT.is(dataId)) {
+        return myTextField == null ? null : myTextField.getText();
       }
       return null;
     }
