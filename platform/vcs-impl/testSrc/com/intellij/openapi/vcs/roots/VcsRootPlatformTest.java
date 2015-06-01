27f6601342c723b4048665c67f099142681b6da2
Yann Cébron <yann.cebron@jetbrains.com>
2015-06-01 21:45:02 +0200

    VcsRootPlatformTest.tearDown: more safe

--- platform/vcs-impl/testSrc/com/intellij/openapi/vcs/roots/VcsRootPlatformTest.java
+++ platform/vcs-impl/testSrc/com/intellij/openapi/vcs/roots/VcsRootPlatformTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -119,13 +119,17 @@ public abstract class VcsRootPlatformTest extends UsefulTestCase {
 
   @Override
   protected void tearDown() throws Exception {
-    getExtensionPoint().unregisterExtension(myExtension);
-    myVcsManager.unregisterVcs(myVcs);
-    for (File file : myFilesToDelete) {
-      delete(file);
+    try {
+      getExtensionPoint().unregisterExtension(myExtension);
+      myVcsManager.unregisterVcs(myVcs);
+      for (File file : myFilesToDelete) {
+        delete(file);
+      }
+      myProjectFixture.tearDown();
     }
-    myProjectFixture.tearDown();
-    super.tearDown();
+    finally {
+      super.tearDown();
+    }
   }
 
   private static void delete(File file) {
