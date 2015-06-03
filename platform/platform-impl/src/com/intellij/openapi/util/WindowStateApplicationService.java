9a789a7d287dc0bdcf9fcc509a3d98373f048402
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2015-06-03 17:06:31 +0300

    IDEA-140307 WindowStateService for application and projects

---
+++ platform/platform-impl/src/com/intellij/openapi/util/WindowStateApplicationService.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.openapi.util;
+
+import com.intellij.openapi.components.RoamingType;
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.components.StoragePathMacros;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+
+/**
+ * @author Sergey.Malenkov
+ */
+@State(
+  name = "WindowStateApplicationService",
+  storages = @Storage(file = StoragePathMacros.APP_CONFIG + "/window.state.xml", roamingType = RoamingType.DISABLED)
+)
+final class WindowStateApplicationService extends WindowStateService.Service {
+  @Override
+  Point getDefaultLocationOn(GraphicsDevice screen, @NotNull String key) {
+    //  backward compatibility when this service is used instead of DimensionService
+    return DimensionService.getInstance().getLocationOn(screen, key);
+  }
+
+  @Override
+  Dimension getDefaultSizeOn(GraphicsDevice screen, @NotNull String key) {
+    //  backward compatibility when this service is used instead of DimensionService
+    return DimensionService.getInstance().getSizeOn(screen, key);
+  }
+
+  @Override
+  Rectangle getDefaultBoundsOn(GraphicsDevice screen, @NotNull String key) {
+    Point location = getDefaultLocationOn(screen, key);
+    if (location == null) {
+      return null;
+    }
+    Dimension size = getDefaultSizeOn(screen, key);
+    if (size == null) {
+      return null;
+    }
+    return new Rectangle(location, size);
+  }
+
+  @Override
+  Integer getDefaultExtendedStateOn(GraphicsDevice screen, @NotNull String key) {
+    //  backward compatibility when this service is used instead of DimensionService
+    int value = DimensionService.getInstance().getExtendedState(key);
+    return value != -1 ? value : null;
+  }
+}
---
+++ platform/platform-impl/src/com/intellij/openapi/util/WindowStateProjectService.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.openapi.util;
+
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.components.StoragePathMacros;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.impl.ProjectFrameBounds;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+
+/**
+ * @author Sergey.Malenkov
+ */
+@State(
+  name = "WindowStateProjectService",
+  storages = {@Storage(file = StoragePathMacros.WORKSPACE_FILE)}
+)
+final class WindowStateProjectService extends WindowStateService.Service {
+  private final Project myProject;
+
+  WindowStateProjectService(Project project) {
+    myProject = project;
+  }
+
+  @Override
+  Point getDefaultLocationOn(GraphicsDevice screen, @NotNull String key) {
+    Rectangle bounds = getDefaultBoundsOn(screen, key);
+    return bounds == null ? null : bounds.getLocation();
+  }
+
+  @Override
+  Dimension getDefaultSizeOn(GraphicsDevice screen, @NotNull String key) {
+    Rectangle bounds = getDefaultBoundsOn(screen, key);
+    return bounds == null ? null : bounds.getSize();
+  }
+
+  @Override
+  Rectangle getDefaultBoundsOn(GraphicsDevice screen, @NotNull String key) {
+    //  backward compatibility when this service is used instead of ProjectFrameBounds
+    return !key.equals("ProjectFrameBounds") ? null : ProjectFrameBounds.getInstance(myProject).getBounds();
+  }
+}
