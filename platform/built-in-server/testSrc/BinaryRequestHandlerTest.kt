51136a10e6a9e08a8d346bb87f71e1846f4a4235
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2015-06-08 19:06:46 +0200

    fix kt compilation

--- platform/built-in-server/testSrc/BinaryRequestHandlerTest.kt
+++ platform/built-in-server/testSrc/BinaryRequestHandlerTest.kt
@@ -1,26 +1,27 @@
 package org.jetbrains.ide
 
-import io.netty.channel.ChannelInitializer
-import io.netty.channel.Channel
-import org.jetbrains.io.Decoder
-import io.netty.channel.ChannelHandlerContext
-import io.netty.buffer.ByteBuf
 import com.intellij.util.Consumer
-import java.util.UUID
-import io.netty.channel.ChannelHandler
-import io.netty.util.CharsetUtil
-import org.jetbrains.io.ChannelExceptionHandler
-import org.jetbrains.io.NettyUtil
+import com.intellij.util.concurrency.Semaphore
 import com.intellij.util.net.NetUtils
+import io.netty.buffer.ByteBuf
 import io.netty.buffer.Unpooled
+import io.netty.channel.Channel
+import io.netty.channel.ChannelHandler
+import io.netty.channel.ChannelHandlerContext
+import io.netty.channel.ChannelInitializer
+import io.netty.util.CharsetUtil
 import junit.framework.TestCase
-import org.junit.rules.RuleChain
-import org.junit.Rule
-import org.junit.Test
-import org.jetbrains.io.MessageDecoder
 import org.jetbrains.concurrency.AsyncPromise
 import org.jetbrains.concurrency.Promise
-import com.intellij.util.concurrency.Semaphore
+import org.jetbrains.io.ChannelExceptionHandler
+import org.jetbrains.io.Decoder
+import org.jetbrains.io.MessageDecoder
+import org.jetbrains.io.NettyUtil
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.RuleChain
+import java.util.UUID
+import java.util.concurrent.TimeUnit
 
 // we don't handle String in efficient way - because we want to test readContent/readChars also
 public class BinaryRequestHandlerTest {
@@ -42,7 +43,7 @@ public class BinaryRequestHandlerTest {
         channel.pipeline().addLast(object : Decoder() {
           override fun messageReceived(context: ChannelHandlerContext, input: ByteBuf) {
             val requiredLength = 4 + text.length()
-            val response = readContent(input, context, requiredLength) {(buffer, context, isCumulateBuffer) -> buffer.toString(buffer.readerIndex(), requiredLength, CharsetUtil.UTF_8) }
+            val response = readContent(input, context, requiredLength) { buffer, context, isCumulateBuffer -> buffer.toString(buffer.readerIndex(), requiredLength, CharsetUtil.UTF_8) }
             if (response != null) {
               result.setResult(response)
             }
@@ -62,7 +63,7 @@ public class BinaryRequestHandlerTest {
     val message = Unpooled.copiedBuffer(text, CharsetUtil.UTF_8)
     buffer.writeShort(message.readableBytes())
     channel.write(buffer)
-    channel.writeAndFlush(message).syncUninterruptibly()
+    channel.writeAndFlush(message).await(5, TimeUnit.SECONDS)
 
     try {
       result.rejected(object : Consumer<Throwable> {
@@ -89,7 +90,7 @@ public class BinaryRequestHandlerTest {
   }
 
   class MyBinaryRequestHandler : BinaryRequestHandler() {
-    class object {
+    companion object {
       val ID = UUID.fromString("E5068DD6-1DB7-437C-A3FC-3CA53B6E1AC9")
     }
 
@@ -105,7 +106,7 @@ public class BinaryRequestHandlerTest {
       private var state = State.HEADER
 
       private enum class State {
-        HEADER
+        HEADER,
         CONTENT
       }
 
@@ -113,20 +114,13 @@ public class BinaryRequestHandlerTest {
         while (true) {
           when (state) {
             State.HEADER -> {
-              val buffer = getBufferIfSufficient(input, 2, context)
-              if (buffer == null) {
-                return
-              }
-
+              val buffer = getBufferIfSufficient(input, 2, context) ?: return
               contentLength = buffer.readUnsignedShort()
               state = State.CONTENT
             }
 
             State.CONTENT -> {
-              val messageText = readChars(input)
-              if (messageText == null) {
-                return
-              }
+              val messageText = readChars(input) ?: return
 
               state = State.HEADER
               context.writeAndFlush(Unpooled.copiedBuffer("got-" + messageText, CharsetUtil.UTF_8))
