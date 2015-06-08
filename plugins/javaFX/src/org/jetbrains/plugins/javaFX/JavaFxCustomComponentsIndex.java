6f71df4d66ba3b0a4ef0f83aed2293d7d6c70671
Yann Cébron <yann.cebron@jetbrains.com>
2015-06-08 14:38:59 +0200

    FxmlDataExternalizer: optimize persistence

--- plugins/javaFX/src/org/jetbrains/plugins/javaFX/JavaFxCustomComponentsIndex.java
+++ plugins/javaFX/src/org/jetbrains/plugins/javaFX/JavaFxCustomComponentsIndex.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -112,7 +112,7 @@ public class JavaFxCustomComponentsIndex extends FileBasedIndexExtension<String,
 
   @Override
   public int getVersion() {
-    return 0;
+    return 1;
   }
 
   public static <T> List<T> findCustomFxml(final Project project,
