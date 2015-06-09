f4886379ad43aa548a918f25100d213288c152a8
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2015-06-09 13:17:40 +0300

    Add inspection that warns about duplicate keys in JSON objects
    (cherry picked from commit 5145241793259002c2100f6804e1d6adfaf681b1)

---
+++ json/src/com/intellij/json/codeinsight/JsonDuplicatePropertyKeysInspection.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.json.codeinsight;
+
+import com.intellij.codeInspection.LocalInspectionTool;
+import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.json.JsonBundle;
+import com.intellij.json.psi.JsonElementVisitor;
+import com.intellij.json.psi.JsonObject;
+import com.intellij.json.psi.JsonProperty;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.util.containers.MultiMap;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * @author Mikhail Golubev
+ */
+public class JsonDuplicatePropertyKeysInspection extends LocalInspectionTool {
+  @Nls
+  @NotNull
+  @Override
+  public String getDisplayName() {
+    return JsonBundle.message("inspection.duplicate.keys.name");
+  }
+
+  @NotNull
+  @Override
+  public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
+    return new JsonElementVisitor() {
+      @Override
+      public void visitObject(@NotNull JsonObject o) {
+        final MultiMap<String, PsiElement> keys = new MultiMap<String, PsiElement>();
+        for (JsonProperty property : o.getPropertyList()) {
+          keys.putValue(property.getName(), property.getNameElement());
+        }
+        for (Map.Entry<String, Collection<PsiElement>> entry : keys.entrySet()) {
+          final Collection<PsiElement> sameNamedKeys = entry.getValue();
+          if (sameNamedKeys.size() > 1) {
+            for (PsiElement element : sameNamedKeys) {
+              holder.registerProblem(element, JsonBundle.message("inspection.duplicate.keys.msg.duplicate.keys", entry.getKey()));
+            }
+          }
+        }
+      }
+    };
+  }
+}
