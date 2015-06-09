c6654d95137976986e59774a0639eb7d80bf671d
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2015-06-09 20:52:40 +0300

    Option conflicts should not prevent manage task from running (PY-16092)
    * any error leads to command skip and log now
    
    (cherry picked from commit c1975e421dfdd8ff4a2ee59cf2818427b910ecfc)

--- python/helpers/pycharm/django_manage_commands_provider/_parser/parser.py
+++ python/helpers/pycharm/django_manage_commands_provider/_parser/parser.py
@@ -8,6 +8,7 @@ from django.core.management import ManagementUtility, get_commands, BaseCommand
 
 from django_manage_commands_provider._parser import _optparse, _argparse
 from _jb_utils import VersionAgnosticUtils
+import sys
 
 
 __author__ = 'Ilya.Kazakevich'
@@ -24,8 +25,9 @@ def report_data(dumper):
     for command_name in get_commands().keys():
         try:
             command = utility.fetch_command(command_name)
-        except Exception:
-            continue  # TODO: Log somehow. Probably print to output?
+        except Exception as e:
+            sys.stderr.write("Error fetching command {0}: {1}\n".format(command_name, e))
+            continue
 
         assert isinstance(command, BaseCommand)
 
@@ -34,9 +36,16 @@ def report_data(dumper):
             use_argparse = command.use_argparse
         except AttributeError:
             pass
+
+        try:
+            parser = command.create_parser("", command_name)
+        except Exception as e:
+            sys.stderr.write("Error parsing command {0}: {1}\n".format(command_name, e))
+            continue
+
         dumper.start_command(command_name=command_name,
                              command_help_text=VersionAgnosticUtils().to_unicode(command.usage("")).replace("%prog", command_name))
         module_to_use = _argparse if use_argparse else _optparse # Choose appropriate module: argparse, optparse
-        module_to_use.process_command(dumper, command, command.create_parser("", command_name))
+        module_to_use.process_command(dumper, command, parser)
         dumper.close_command()
 
