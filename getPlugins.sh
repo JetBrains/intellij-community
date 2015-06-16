a3819197ca5d84087f36b276cbcde6ecf6d36717
badlogic <badlogicgames@gmail.com>
2015-06-16 15:46:14 +0200

    More build improvements

--- getPlugins.sh
+++ getPlugins.sh
@@ -1,3 +1,16 @@
 git clone git://git.jetbrains.org/idea/android.git android
+cd android
+git pull
+cd ..
 git clone git://git.jetbrains.org/idea/adt-tools-base.git android/tools-base
+cd android/tools-base
+git pull
+cd ../..
 git clone https://github.com/robovm/robovm-studio-branding robovm/robovm-studio-branding
+cd robovm/robovm-studio-branding
+git pull
+cd ../..
+git clone https://github.com/robovm/robovm-idea robovm/robovm-idea
+cd robovm/robovm-idea
+git pull
+cd ../..
