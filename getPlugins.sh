666effe313ea561f1e459886d9fbe07eb3efeef8
badlogic <badlogicgames@gmail.com>
2015-06-16 10:26:10 +0200

    Pulling in branding project

--- getPlugins.sh
+++ getPlugins.sh
@@ -1,2 +1,3 @@
 git clone git://git.jetbrains.org/idea/android.git android
 git clone git://git.jetbrains.org/idea/adt-tools-base.git android/tools-base
+git clone https://github.com/robovm/robovm-studio-branding
