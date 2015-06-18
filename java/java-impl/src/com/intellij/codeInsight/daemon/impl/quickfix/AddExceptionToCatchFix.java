5d6aacba18492302fa9b42bc3ccd5baad597265c
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2015-06-18 16:17:11 +0300

    EA-69132 (array bounds check added)

--- java/java-impl/src/com/intellij/codeInsight/daemon/impl/quickfix/AddExceptionToCatchFix.java
+++ java/java-impl/src/com/intellij/codeInsight/daemon/impl/quickfix/AddExceptionToCatchFix.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -56,6 +56,8 @@ public class AddExceptionToCatchFix extends BaseIntentionAction {
 
     PsiTryStatement tryStatement = (PsiTryStatement)element.getParent();
     List<PsiClassType> unhandledExceptions = new ArrayList<PsiClassType>(ExceptionUtil.collectUnhandledExceptions(element, null));
+    if (unhandledExceptions.isEmpty()) return;
+
     ExceptionUtil.sortExceptionsByHierarchy(unhandledExceptions);
 
     IdeDocumentHistory.getInstance(project).includeCurrentPlaceAsChangePlace();
