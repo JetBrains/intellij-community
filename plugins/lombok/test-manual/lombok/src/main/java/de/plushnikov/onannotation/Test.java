520204b16ef35f507e3a1b21da0b6b67a76648f8
Michail Plushnikov <michail@plushnikov.de>
2015-06-30 23:15:53 +0200

    Test onX
    GitOrigin-RevId: 3e6b5a05a1b4bd6b8b6a2a8c65e9e8684615ac71

--- plugins/lombok/test-manual/lombok/src/main/java/de/plushnikov/onannotation/Test.java
+++ plugins/lombok/test-manual/lombok/src/main/java/de/plushnikov/onannotation/Test.java
@@ -4,15 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-//import javax.inject.Inject;
-//import javax.persistence.Column;
-//import javax.persistence.Id;
-//import javax.validation.constraints.Max;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.validation.constraints.Max;
 
-//@AllArgsConstructor(onConstructor=@_(@Deprecated))
+@AllArgsConstructor(onConstructor=@__(@Deprecated))
 public class Test {
-//    @Getter(onMethod=@_({@Id, @Column(name="unique-id")}))
-//    @Setter(onParam=@_(@Max(10000)))
+    @Getter(onMethod=@_({@Id, @Column(name="unique-id")}))
+    @Setter(onParam=@_(@Max(10000)))
     private long unid;
 
+    public static void main(String[] args) {
+        Test test = new Test(1L);
+        System.out.println(test);
+    }
 }
