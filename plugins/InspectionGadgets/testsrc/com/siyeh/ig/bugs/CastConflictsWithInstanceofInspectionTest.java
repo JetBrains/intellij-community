ffded1998d8bdc140bcea56b7ee581a38fee40f3
Bas Leijdekkers <basleijdekkers@gmail.com>
2015-07-02 17:22:13 +0200

    make IG test light

--- plugins/InspectionGadgets/testsrc/com/siyeh/ig/bugs/CastConflictsWithInstanceofInspectionTest.java
+++ plugins/InspectionGadgets/testsrc/com/siyeh/ig/bugs/CastConflictsWithInstanceofInspectionTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,39 +15,43 @@
  */
 package com.siyeh.ig.bugs;
 
-import com.siyeh.ig.IGInspectionTestCase;
+import com.intellij.codeInspection.InspectionProfileEntry;
+import com.siyeh.ig.LightInspectionTestCase;
+import org.jetbrains.annotations.Nullable;
 
-public class CastConflictsWithInstanceofInspectionTest extends IGInspectionTestCase {
+public class CastConflictsWithInstanceofInspectionTest extends LightInspectionTestCase {
 
-  public void testElseElse() throws Exception {
+  public void testElseElse() {
     doTest();
   }
 
-  public void testSimple() throws Exception {
+  public void testSimple() {
     doTest();
   }
 
-  public void testElseElseOrOr() throws Exception {
+  public void testElseElseOrOr() {
     doTest();
   }
 
-  public void testAndAnd() throws Exception {
+  public void testAndAnd() {
     doTest();
   }
 
-  public void testPolyadic() throws Exception {
+  public void testPolyadic() {
     doTest();
   }
   
-  public void testNotOr() throws Exception {
+  public void testNotOr() {
     doTest();
   }
 
-  public void testOrInstanceofOrInstanceof() throws Exception {
+  public void testOrInstanceofOrInstanceof() {
     doTest();
   }
 
-  private void doTest() throws Exception {
-    doTest("com/siyeh/igtest/bugs/castConflictingInstanceof/" + getTestName(true), new CastConflictsWithInstanceofInspection());
+  @Nullable
+  @Override
+  protected InspectionProfileEntry getInspection() {
+    return new CastConflictsWithInstanceofInspection();
   }
 }
