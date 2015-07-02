a233749cce823d941e2fed160fad28cb8a72a85a
Alexey Kudravtsev <cdr@intellij.com>
2015-07-02 14:48:21 +0300

    optimisation

--- platform/core-api/src/com/intellij/psi/stubs/PsiFileStubImpl.java
+++ platform/core-api/src/com/intellij/psi/stubs/PsiFileStubImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -70,7 +70,8 @@ public class PsiFileStubImpl<T extends PsiFile> extends StubBase<T> implements P
   @NotNull
   @Override
   public PsiFileStub[] getStubRoots() {
-    return myStubRoots != null ? myStubRoots : new PsiFileStub[]{this};
+    PsiFileStub[] roots = myStubRoots;
+    return roots == null ? new PsiFileStub[]{this} : roots;
   }
 
   public void setStubRoots(@NotNull PsiFileStub[] roots) {
