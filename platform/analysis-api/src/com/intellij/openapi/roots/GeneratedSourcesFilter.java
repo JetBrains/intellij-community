56c9e5c2bc3f73095303882cb2bca5e9ad858afe
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2015-07-03 10:12:21 +0200

    Support navigation to the original element for generated source code

--- platform/analysis-api/src/com/intellij/openapi/roots/GeneratedSourcesFilter.java
+++ platform/analysis-api/src/com/intellij/openapi/roots/GeneratedSourcesFilter.java
@@ -17,8 +17,16 @@ package com.intellij.openapi.roots;
 
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.GeneratedSourcesFilterEx.LocationInFile;
+import com.intellij.openapi.util.Segment;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiElement;
+import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author nik
@@ -26,10 +34,7 @@ import org.jetbrains.annotations.NotNull;
 public abstract class GeneratedSourcesFilter {
   public static final ExtensionPointName<GeneratedSourcesFilter> EP_NAME = ExtensionPointName.create("com.intellij.generatedSourcesFilter");
 
-  public abstract boolean isGeneratedSource(@NotNull VirtualFile file, @NotNull Project project);
-
-  public static boolean isGeneratedSourceByAnyFilter(@NotNull VirtualFile file,
-                                                     @NotNull Project project) {
+  public static boolean isGeneratedSourceByAnyFilter(@NotNull VirtualFile file, @NotNull Project project) {
     for (GeneratedSourcesFilter filter : EP_NAME.getExtensions()) {
       if (filter.isGeneratedSource(file, project)) {
         return true;
@@ -37,4 +42,67 @@ public abstract class GeneratedSourcesFilter {
     }
     return false;
   }
+
+  @NotNull
+  public static List<? extends PsiElement> getAllOriginalElements(@NotNull PsiElement element) {
+    List<PsiElement> result = null;
+    for (GeneratedSourcesFilter filter : EP_NAME.getExtensions()) {
+      if (filter instanceof GeneratedSourcesFilterEx) {
+        result = addAll(((GeneratedSourcesFilterEx)filter).getOriginalElements(element), result);
+      }
+    }
+    return orEmpty(result);
+  }
+
+  @NotNull
+  public static List<? extends PsiElement> getAllGeneratedElements(@NotNull PsiElement element) {
+    List<PsiElement> result = null;
+    for (GeneratedSourcesFilter filter : EP_NAME.getExtensions()) {
+      if (filter instanceof GeneratedSourcesFilterEx) {
+        result = addAll(((GeneratedSourcesFilterEx)filter).getGeneratedElements(element), result);
+      }
+    }
+    return orEmpty(result);
+  }
+
+  @NotNull
+  public static List<? extends LocationInFile> getAllOriginalLocations(@NotNull Project project,
+                                                                       @NotNull VirtualFile file,
+                                                                       @Nullable Segment segment) {
+    List<GeneratedSourcesFilterEx.LocationInFile> result = null;
+    for (GeneratedSourcesFilter filter : EP_NAME.getExtensions()) {
+      if (filter instanceof GeneratedSourcesFilterEx) {
+        result = addAll(((GeneratedSourcesFilterEx)filter).getOriginalLocations(project, file, segment), result);
+      }
+    }
+    return orEmpty(result);
+  }
+
+  @NotNull
+  public static List<? extends LocationInFile> getAllGeneratedLocations(@NotNull Project project,
+                                                                        @NotNull VirtualFile file,
+                                                                        @Nullable Segment segment) {
+    List<GeneratedSourcesFilterEx.LocationInFile> result = null;
+    for (GeneratedSourcesFilter filter : EP_NAME.getExtensions()) {
+      if (filter instanceof GeneratedSourcesFilterEx) {
+        result = addAll(((GeneratedSourcesFilterEx)filter).getGeneratedLocations(project, file, segment), result);
+      }
+    }
+    return orEmpty(result);
+  }
+
+  private static <T> List<T> addAll(List<? extends T> elements, List<T> result) {
+    if (result == null) {
+      return ContainerUtil.newArrayList(elements);
+    }
+    result.addAll(elements);
+    return result;
+  }
+
+  private static <T> List<T> orEmpty(List<T> elements) {
+    if (elements != null) return elements;
+    return Collections.emptyList();
+  }
+
+  public abstract boolean isGeneratedSource(@NotNull VirtualFile file, @NotNull Project project);
 }
