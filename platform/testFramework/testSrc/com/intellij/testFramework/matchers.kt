e5e2b196d7e5a7c9e442c80917290cb006c976c5
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2015-07-03 10:07:15 +0200

    IDEA-CR-3419 move kt file according to code style

--- platform/testFramework/testSrc/com/intellij/testFramework/matchers.kt
+++ platform/testFramework/testSrc/com/intellij/testFramework/matchers.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jetbrains.testFramework
+package com.intellij.testFramework
 
 import org.hamcrest.Description
 import org.hamcrest.Factory
