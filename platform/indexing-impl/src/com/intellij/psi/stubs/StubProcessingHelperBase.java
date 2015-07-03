ba275cc6c589b5d9beffc7aa95b90d571e696fd3
Alexey Kudravtsev <cdr@intellij.com>
2015-07-03 19:59:44 +0300

    cleanup

--- platform/indexing-impl/src/com/intellij/psi/stubs/StubProcessingHelperBase.java
+++ platform/indexing-impl/src/com/intellij/psi/stubs/StubProcessingHelperBase.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.intellij.psi.stubs;
 
 import com.intellij.lang.ASTNode;
@@ -74,10 +89,8 @@ public abstract class StubProcessingHelperBase {
           if (stubbedRoot.second == stubBindingRoot) continue;
           if (stubbedRoot.second instanceof PsiFileImpl) {
             final StubTree secondaryStubTree = ((PsiFileImpl)stubbedRoot.second).calcStubTree();
-            if (secondaryStubTree != null) {
-              final PsiFileStub root = secondaryStubTree.getRoot();
-              roots.add(root);
-            }
+            final PsiFileStub root = secondaryStubTree.getRoot();
+            roots.add(root);
           }
         }
         final PsiFileStub[] rootsArray = roots.toArray(new PsiFileStub[roots.size()]);
