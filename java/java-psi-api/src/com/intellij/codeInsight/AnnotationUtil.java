fd89b66d2712b1ea93054cb7457f1b7c95846da1
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2015-07-06 19:15:09 +0300

    [java] added method for getting declared string value from annotation

--- java/java-psi-api/src/com/intellij/codeInsight/AnnotationUtil.java
+++ java/java-psi-api/src/com/intellij/codeInsight/AnnotationUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -523,6 +523,13 @@ public class AnnotationUtil {
     PsiAnnotationMemberValue attrValue = anno.findAttributeValue(attributeName);
     Object constValue = JavaPsiFacade.getInstance(anno.getProject()).getConstantEvaluationHelper().computeConstantExpression(attrValue);
     return constValue instanceof Boolean ? (Boolean)constValue : null;
+  }
+
+  @Nullable
+  public static String getDeclaredStringAttributeValue(@NotNull PsiAnnotation anno, @Nullable final String attributeName) {
+    PsiAnnotationMemberValue attrValue = anno.findDeclaredAttributeValue(attributeName);
+    Object constValue = JavaPsiFacade.getInstance(anno.getProject()).getConstantEvaluationHelper().computeConstantExpression(attrValue);
+    return constValue instanceof String ? (String)constValue : null;
   }
 
   @Nullable
