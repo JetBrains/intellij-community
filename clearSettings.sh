603d9b6641d5fae68abe2fe0fad602123821f1c5
badlogic <badlogicgames@gmail.com>
2015-07-06 14:25:31 +0200

    Fixed clear settings script

--- clearSettings.sh
+++ clearSettings.sh
@@ -1,4 +1,4 @@
 #/bin/sh
-rm -r ~/Library/Application\ Support/RoboVMStudio1.0/
-rm -r ~/Library/Caches/RoboVMStudio1.0/
-rm -r ~/Library/Preferences/RoboVMStudio1.0/
+rm -r ~/Library/Application\ Support/RoboVMStudio*/
+rm -r ~/Library/Caches/RoboVMStudio*/
+rm -r ~/Library/Preferences/RoboVMStudio*/
