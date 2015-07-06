653cba82eb116bc71d82d4b7bb2d83b7151fef08
badlogic <badlogicgames@gmail.com>
2015-07-06 15:34:15 +0200

    Fixed up getPlugins, clones the IDEA plugin everytime

--- getPlugins.sh
+++ getPlugins.sh
@@ -6,10 +6,12 @@ git clone git://git.jetbrains.org/idea/adt-tools-base.git android/tools-base
 cd android/tools-base
 git pull
 cd ../..
+rm -rf robovm/robovm-idea
 git clone https://github.com/robovm/robovm-idea robovm/robovm-idea
 cd robovm/robovm-idea
 git checkout master
 git pull
 : ${ROBOVM_IDEA_PLUGIN_VERSION="master"}
+git checkout -- .
 git checkout $ROBOVM_IDEA_PLUGIN_VERSION
 cd ../..
