6ca270bcc7cff3885561d382b099d3d98530af59
Bas Leijdekkers <basleijdekkers@gmail.com>
2015-07-06 14:25:35 +0200

    make IG test light

--- plugins/InspectionGadgets/test/com/siyeh/igtest/dataflow/constant_value_variable_use/ConstantValueVariableUse.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/dataflow/constant_value_variable_use/ConstantValueVariableUse.java
@@ -6,9 +6,9 @@ public class ConstantValueVariableUse {
     public static void test(double number) {
         if (number == -0.0) { // is true for -0.0
             f(number);
-        }
+        }8
         if (number == 1.0) {
-            f(number);
+            f(<warning descr="Value of 'number' is known to be constant">number</warning>);
         }
     }
 
