b3808433605f0429f5356b1edbd3e30871423562
Dennis Ushakov <dennis.ushakov@gmail.com>
2015-07-08 17:19:42 +0300

    escape symbols > 127 with defined entities WEB-519

--- xml/impl/src/com/intellij/xml/actions/EscapeEntitiesAction.java
+++ xml/impl/src/com/intellij/xml/actions/EscapeEntitiesAction.java
@@ -20,42 +20,69 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.ModificationTracker;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.search.PsiElementProcessor;
+import com.intellij.psi.util.CachedValueProvider;
+import com.intellij.psi.util.ParameterizedCachedValueProvider;
+import com.intellij.psi.xml.XmlEntityDecl;
 import com.intellij.psi.xml.XmlFile;
 import com.intellij.psi.xml.XmlTokenType;
+import com.intellij.util.ParameterizedCachedValueImpl;
+import com.intellij.xml.Html5SchemaProvider;
+import com.intellij.xml.util.XmlUtil;
+import io.netty.util.collection.IntObjectHashMap;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * @author Dennis.Ushakov
  */
 public class EscapeEntitiesAction extends SimpleCodeInsightAction {
+  private static ParameterizedCachedValueImpl<IntObjectHashMap<String>, PsiFile> ESCAPES = new ParameterizedCachedValueImpl<IntObjectHashMap<String>, PsiFile>(
+    new ParameterizedCachedValueProvider<IntObjectHashMap<String>, PsiFile>() {
+      @Nullable
+      @Override
+      public CachedValueProvider.Result<IntObjectHashMap<String>> compute(PsiFile param) {
+        final XmlFile file = XmlUtil.findXmlFile(param, Html5SchemaProvider.getCharsDtdLocation());
+        assert file != null;
+        final IntObjectHashMap<String> result = new IntObjectHashMap<String>();
+        XmlUtil.processXmlElements(file, new PsiElementProcessor() {
+          @Override
+          public boolean execute(@NotNull PsiElement element) {
+            if (element instanceof XmlEntityDecl) {
+              final String value = ((XmlEntityDecl)element).getValueElement().getValue();
+              final Integer key = Integer.valueOf(value.substring(2, value.length() - 1));
+              if (!result.containsKey(key)) {
+                result.put(key, ((XmlEntityDecl)element).getName());
+              }
+            }
+            return true;
+          }
+        }, true);
+        return new CachedValueProvider.Result<IntObjectHashMap<String>>(result, ModificationTracker.NEVER_CHANGED);
+      }
+    }) {
+    @Override
+    public boolean isFromMyProject(Project project) {
+      return true;
+    }
+  };
+
   private static String escape(XmlFile file, String text, int start) {
     final StringBuilder result = new StringBuilder();
     for (int i = 0; i < text.length(); i++) {
       char c = text.charAt(i);
       final PsiElement element = file.findElementAt(start + i);
       if (element != null && element.getNode().getElementType() == XmlTokenType.XML_DATA_CHARACTERS) {
-        if (c == '<') {
-          result.append("&lt;");
-          continue;
-        }
-        if (c == '>') {
-          result.append("&gt;");
-          continue;
-        }
-        if (c == '&') {
-          result.append("&amp;");
-          continue;
-        }
-        if (c == '"') {
-          result.append("&quot;");
-          continue;
-        }
-        if (c == '\'') {
-          result.append("&#39;");
-          continue;
+        if (c == '<' || c == '>' || c == '&' || c == '"' || c == '\'' || c > 0xff) {
+          final String escape = ESCAPES.getValue(file).get(c);
+          if (escape != null) {
+            result.append("&").append(escape).append(";");
+            continue;
+          }
         }
       }
       result.append(c);
