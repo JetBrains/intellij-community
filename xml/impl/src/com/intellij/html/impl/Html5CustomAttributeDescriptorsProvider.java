46a3939942189c1c4835ebcb2efdeec587c72f5f
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2015-07-09 12:54:10 +0200

    Unrecognized HTML5 data attributes in Tapestry (IDEA-124505)

--- xml/impl/src/com/intellij/html/impl/Html5CustomAttributeDescriptorsProvider.java
+++ xml/impl/src/com/intellij/html/impl/Html5CustomAttributeDescriptorsProvider.java
@@ -91,7 +91,7 @@ public class Html5CustomAttributeDescriptorsProvider implements XmlAttributeDesc
 
   @Override
   public XmlAttributeDescriptor getAttributeDescriptor(String attributeName, XmlTag context) {
-    if (context != null && HtmlUtil.isHtml5Context(context) && HtmlUtil.isCustomHtml5Attribute(attributeName)) {
+    if (context != null && HtmlUtil.isCustomHtml5Attribute(attributeName) && HtmlUtil.tagHasHtml5Schema(context)) {
       return new AnyXmlAttributeDescriptor(attributeName);
     }
     return null;
