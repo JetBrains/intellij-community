4a39298d84693a0b4e8caa404271bbb1e01e1825
Anton Makeev <Anton.Makeev@jetbrains.com>
2015-07-11 11:10:16 +0200

    Test Framework now allows running unconventionally named tests (TestSuite, TestFoo, FooTests etc) + reporting class loading problems.

--- platform/testFramework/src/com/intellij/ClassFinder.java
+++ platform/testFramework/src/com/intellij/ClassFinder.java
@@ -25,12 +25,14 @@
 package com.intellij;
 
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.codeStyle.NameUtil;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
@@ -38,8 +40,10 @@ import java.util.List;
 public class ClassFinder {
   private final List<String> classNameList = new ArrayList<String>();
   private final int startPackageName;
+  private final boolean includeUnconventionallyNamedTests;
 
-  public ClassFinder(final File classPathRoot, final String packageRoot) throws IOException {
+  public ClassFinder(final File classPathRoot, final String packageRoot, boolean includeUnconventionallyNamedTests) throws IOException {
+    this.includeUnconventionallyNamedTests = includeUnconventionallyNamedTests;
     startPackageName = classPathRoot.getAbsolutePath().length() + 1;
     String directoryOffset = packageRoot.replace('.', File.separatorChar);
     findAndStoreTestClasses(new File(classPathRoot, directoryOffset));
@@ -48,8 +52,32 @@ public class ClassFinder {
   @Nullable
   private String computeClassName(final File file) {
     String absPath = file.getAbsolutePath();
-    if (absPath.endsWith("Test.class")) {
-      return StringUtil.trimEnd(absPath.substring(startPackageName), ".class").replace(File.separatorChar, '.');
+    if (!includeUnconventionallyNamedTests) {
+      if (absPath.endsWith("Test.class")) {
+        return StringUtil.trimEnd(absPath.substring(startPackageName), ".class").replace(File.separatorChar, '.');
+      }
+    }
+    else {
+      String className = file.getName();
+      if (className.endsWith(".class")) {
+        int dollar = className.lastIndexOf("$");
+        if (dollar != -1) {
+          className = className.substring(dollar + 1);
+          // most likely something like RecursionManagerTest$_testMayCache_closure5 or other anonymous class
+          // may cause https://issues.apache.org/jira/browse/GROOVY-5351
+          if (!Character.isUpperCase(className.charAt(0))) return null;
+        }
+  
+        // A test may be named Test*, *Test, *Tests*, *TestCase, *TestSuite, *Suite, etc
+        List<String> words = Arrays.asList(NameUtil.nameToWords(className));
+        
+        if (words.contains("Test") || words.contains("Tests") || words.contains("Suite")) {
+          String fqn = StringUtil.trimEnd(absPath.substring(startPackageName), ".class").replace(File.separatorChar, '.');
+          if (!Arrays.asList("com.intellij.tests.BootstrapTests", "com.intellij.AllTests").contains(fqn)) {
+            return fqn;
+          }
+        }
+      }
     }
     return null;
   }
