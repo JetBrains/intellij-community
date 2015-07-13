283758f15e1756d5fe0d8a26fbb22cdc0f3cd4f0
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2015-07-13 18:40:05 +0300

    Don't expose any imported names in *.pyi except for the ones defined with "as" (PY-16402)

--- python/src/com/jetbrains/python/pyi/PyiModuleMembersProvider.java
+++ python/src/com/jetbrains/python/pyi/PyiModuleMembersProvider.java
@@ -17,17 +17,13 @@ package com.jetbrains.python.pyi;
 
 import com.intellij.psi.PsiElement;
 import com.jetbrains.python.codeInsight.PyCustomMember;
-import com.jetbrains.python.psi.NameDefiner;
-import com.jetbrains.python.psi.PyElement;
-import com.jetbrains.python.psi.PyFile;
+import com.jetbrains.python.psi.*;
 import com.jetbrains.python.psi.resolve.PointInImport;
 import com.jetbrains.python.psi.types.PyModuleMembersProvider;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author vlan
@@ -37,8 +33,12 @@ public class PyiModuleMembersProvider extends PyModuleMembersProvider {
   @Override
   public PsiElement resolveMember(PyFile module, String name) {
     final PsiElement pythonStub = PyiUtil.getPythonStub(module);
-    if (pythonStub instanceof NameDefiner) {
-      return ((NameDefiner)pythonStub).getElementNamed(name);
+    if (pythonStub instanceof PyFile) {
+      final PyFile stubFile = (PyFile)pythonStub;
+      final PsiElement member = stubFile.getElementNamed(name);
+      if (member != null && isExportedName(stubFile, name)) {
+        return member;
+      }
     }
     return null;
   }
@@ -46,11 +46,12 @@ public class PyiModuleMembersProvider extends PyModuleMembersProvider {
   @Override
   public Collection<PyCustomMember> getMembers(PyFile module, PointInImport point) {
     final PsiElement pythonStub = PyiUtil.getPythonStub(module);
-    if (pythonStub instanceof NameDefiner) {
+    if (pythonStub instanceof PyFile) {
+      final PyFile stubFile = (PyFile)pythonStub;
       final List<PyCustomMember> results = new ArrayList<PyCustomMember>();
-      for (PyElement element : ((NameDefiner)pythonStub).iterateNames()) {
+      for (PyElement element : stubFile.iterateNames()) {
         final String name = element.getName();
-        if (name != null) {
+        if (name != null && isExportedName(stubFile, name)) {
           results.add(new PyCustomMember(name, element));
         }
       }
@@ -62,5 +63,33 @@ public class PyiModuleMembersProvider extends PyModuleMembersProvider {
   @Override
   protected Collection<PyCustomMember> getMembersByQName(PyFile module, String qName) {
     return null;
+  }
+
+  private static boolean isExportedName(@NotNull PyFile file, @NotNull String name) {
+    final PyImportElement importElement = findImportElementByName(file, name);
+    return importElement == null || isExportedImportElement(importElement);
+  }
+
+  private static boolean isExportedImportElement(@NotNull PyImportElement element) {
+    return element.getAsNameElement() != null;
+  }
+
+  @Nullable
+  private static PyImportElement findImportElementByName(@NotNull PyFile file, @NotNull String name) {
+    for (PyImportElement element : getImportElements(file)) {
+      if (name.equals(element.getVisibleName())) {
+        return element;
+      }
+    }
+    return null;
+  }
+
+  private static List<PyImportElement> getImportElements(@NotNull PyFile file) {
+    final List<PyImportElement> result = new ArrayList<PyImportElement>();
+    result.addAll(file.getImportTargets());
+    for (PyFromImportStatement statement : file.getFromImports()) {
+      result.addAll(Arrays.asList(statement.getImportElements()));
+    }
+    return result;
   }
 }
