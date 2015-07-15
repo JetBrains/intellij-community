36086849d700531cf2e6e96b3d0e849be0d3dadc
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2015-07-15 17:09:33 +0200

    IDEA-141026 Global libraries are no longer exported via File | Export Settings (cherry picked from commit b2dec6a)

--- platform/lang-impl/src/com/intellij/openapi/roots/impl/libraries/ExportableApplicationLibraryTable.java
+++ platform/lang-impl/src/com/intellij/openapi/roots/impl/libraries/ExportableApplicationLibraryTable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@ import com.intellij.openapi.components.StoragePathMacros;
  */
 @State(
   name = "libraryTable",
-  storages = @Storage(file = StoragePathMacros.APP_CONFIG + "/applicationLibraries.xml", roamingType = RoamingType.DISABLED)
+  storages = @Storage(file = StoragePathMacros.APP_CONFIG + "/applicationLibraries.xml", roamingType = RoamingType.PER_PLATFORM)
 )
 public class ExportableApplicationLibraryTable extends ApplicationLibraryTable {
 }
