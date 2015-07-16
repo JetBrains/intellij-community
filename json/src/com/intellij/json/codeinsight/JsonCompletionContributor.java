0ee891e4d2363bb8913ed67cd8b79d6fefd2ed08
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2015-07-16 15:44:00 +0300

    Do not show keywords as completion variants inside JSON string literals
    Also I removed auxiliary debug messages from JsonCompletionContributor.

--- json/src/com/intellij/json/codeinsight/JsonCompletionContributor.java
+++ json/src/com/intellij/json/codeinsight/JsonCompletionContributor.java
@@ -19,10 +19,9 @@ import com.intellij.codeInsight.completion.*;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.json.psi.JsonArray;
 import com.intellij.json.psi.JsonProperty;
-import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.json.psi.JsonStringLiteral;
 import com.intellij.patterns.PsiElementPattern;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.impl.DebugUtil;
 import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.NotNull;
 
@@ -32,24 +31,19 @@ import static com.intellij.patterns.PlatformPatterns.psiElement;
  * @author Mikhail Golubev
  */
 public class JsonCompletionContributor extends CompletionContributor {
-  private static final Logger LOG = Logger.getInstance(JsonCompletionContributor.class);
 
   private static final PsiElementPattern.Capture<PsiElement> AFTER_COLON_IN_PROPERTY = psiElement()
-    .afterLeaf(":").withSuperParent(2, JsonProperty.class);
+    .afterLeaf(":").withSuperParent(2, JsonProperty.class)
+    .andNot(psiElement().withParent(JsonStringLiteral.class));
+  
   private static final PsiElementPattern.Capture<PsiElement> AFTER_COMMA_OR_BRACKET_IN_ARRAY = psiElement()
-    .afterLeaf("[", ",").withSuperParent(2, JsonArray.class);
-  @Override
-  public void fillCompletionVariants(@NotNull CompletionParameters parameters, @NotNull CompletionResultSet result) {
-    LOG.debug(DebugUtil.psiToString(parameters.getPosition().getContainingFile(), true));
-    super.fillCompletionVariants(parameters, result);
-  }
-
-
+    .afterLeaf("[", ",").withSuperParent(2, JsonArray.class)
+    .andNot(psiElement().withParent(JsonStringLiteral.class));
+  
   public JsonCompletionContributor() {
     extend(CompletionType.BASIC, AFTER_COLON_IN_PROPERTY, MyKeywordsCompletionProvider.INSTANCE);
     extend(CompletionType.BASIC, AFTER_COMMA_OR_BRACKET_IN_ARRAY, MyKeywordsCompletionProvider.INSTANCE);
   }
-
 
   private static class MyKeywordsCompletionProvider extends CompletionProvider<CompletionParameters> {
     private static final MyKeywordsCompletionProvider INSTANCE = new MyKeywordsCompletionProvider();
