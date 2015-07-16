e4dfd48ded8edc6a7a79cdb9cf819bbc77318c2f
Dmitry Lomov <dslomov@chromium.org>
2015-07-16 15:10:44 +0200

    Fixed getManualWatchRoots() + moved classes around.

--- platform/platform-api/src/com/intellij/openapi/vfs/local/FileWatcherNotificationSink.java
+++ platform/platform-api/src/com/intellij/openapi/vfs/local/FileWatcherNotificationSink.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.intellij.openapi.vfs.impl.local;
+package com.intellij.openapi.vfs.local;
 
 import com.intellij.notification.NotificationListener;
 import org.jetbrains.annotations.TestOnly;
