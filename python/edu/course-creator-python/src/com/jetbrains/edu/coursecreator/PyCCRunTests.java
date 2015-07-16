0d7e88c387be77ef6420a97cde045b5ea184f15c
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2015-07-16 17:36:15 +0300

    reverted run tests in file for course creator plugin

---
+++ python/edu/course-creator-python/src/com/jetbrains/edu/coursecreator/PyCCRunTests.java
@@ -0,0 +1,15 @@
+package com.jetbrains.edu.coursecreator;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.jetbrains.edu.coursecreator.actions.PyCCRunTestsAction;
+import com.jetbrains.python.edu.PyExecuteFileExtensionPoint;
+import org.jetbrains.annotations.NotNull;
+
+public class PyCCRunTests implements PyExecuteFileExtensionPoint {
+
+  @NotNull
+  public AnAction getRunAction() {
+    return new PyCCRunTestsAction();
+  }
+
+}
