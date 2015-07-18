9a75bb153921e207350f938136109d355285928c
Anton Makeev <Anton.Makeev@jetbrains.com>
2015-07-18 12:30:00 +0200

    Test Framework refactored to allow fully customized project setup - reapplied with fix

--- platform/testFramework/src/com/intellij/testFramework/LightProjectDescriptor.java
+++ platform/testFramework/src/com/intellij/testFramework/LightProjectDescriptor.java
@@ -15,39 +15,144 @@
  */
 package com.intellij.testFramework;
 
+import com.intellij.openapi.Disposable;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.module.EmptyModuleType;
 import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.module.ModuleType;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.roots.ContentEntry;
+import com.intellij.openapi.roots.ContentIterator;
 import com.intellij.openapi.roots.ModifiableRootModel;
+import com.intellij.openapi.util.Computable;
+import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.vfs.VfsUtilCore;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileManager;
+import com.intellij.openapi.vfs.VirtualFileVisitor;
+import com.intellij.openapi.vfs.ex.temp.TempFileSystem;
+import com.intellij.util.Consumer;
+import com.intellij.util.indexing.FileBasedIndex;
+import com.intellij.util.indexing.IndexableFileSet;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-/**
- * @author yole
- */
-public interface LightProjectDescriptor {
-  @NotNull
-  ModuleType getModuleType();
-  Sdk getSdk();
-  void configureModule(@NotNull Module module, @NotNull ModifiableRootModel model, @NotNull ContentEntry contentEntry);
+import java.io.IOException;
 
-  class Empty implements LightProjectDescriptor {
-    @NotNull
-    @Override
-    public ModuleType getModuleType() {
-      return EmptyModuleType.getInstance();
-    }
+import static com.intellij.openapi.roots.ModuleRootModificationUtil.updateModel;
 
-    @Override
-    public Sdk getSdk() {
-      return null;
-    }
 
-    @Override
-    public void configureModule(@NotNull Module module, @NotNull ModifiableRootModel model, @NotNull ContentEntry contentEntry) {
+public class LightProjectDescriptor {
+  public static final LightProjectDescriptor EMPTY_PROJECT_DESCRIPTOR = new LightProjectDescriptor();
+
+  public void setUpProject(@NotNull Project project, @NotNull SetupHandler handler) throws Exception {
+    Module module = createMainModule(project);
+    handler.moduleCreated(module);
+    VirtualFile sourceRoot = createSourcesRoot(module);
+    if (sourceRoot != null) {
+      handler.sourceRootCreated(sourceRoot);
+      createContentEntry(module, sourceRoot);
     }
   }
 
-  LightProjectDescriptor EMPTY_PROJECT_DESCRIPTOR = new Empty();
+  @NotNull
+  public Module createMainModule(@NotNull final Project project) {
+    return ApplicationManager.getApplication().runWriteAction(new Computable<Module>() {
+      @Override
+      public Module compute() {
+        return ModuleManager.getInstance(project).newModule("light_idea_test_case.iml", getModuleType().getId());
+      }
+    });
+  }
+
+  @NotNull
+  public ModuleType getModuleType() {
+    return EmptyModuleType.getInstance();
+  }
+
+  @Nullable
+  public VirtualFile createSourcesRoot(@NotNull final Module module) {
+    VirtualFile dummyRoot = VirtualFileManager.getInstance().findFileByUrl("temp:///");
+    assert dummyRoot != null;
+    dummyRoot.refresh(false, false);
+
+    final VirtualFile srcRoot;
+    try {
+      srcRoot = dummyRoot.createChildDirectory(this, "src");
+      cleanSourceRoot(srcRoot);
+    }
+    catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+    final IndexableFileSet indexableFileSet = new IndexableFileSet() {
+      @Override
+      public boolean isInSet(@NotNull final VirtualFile file) {
+        return file.getFileSystem() == srcRoot.getFileSystem() &&
+               module.getProject().isOpen();
+      }
+
+      @Override
+      public void iterateIndexableFilesIn(@NotNull final VirtualFile file, @NotNull final ContentIterator iterator) {
+        VfsUtilCore.visitChildrenRecursively(file, new VirtualFileVisitor() {
+          @Override
+          public boolean visitFile(@NotNull VirtualFile file) {
+            iterator.processFile(file);
+            return true;
+          }
+        });
+      }
+    };
+    FileBasedIndex.getInstance().registerIndexableSet(indexableFileSet, null);
+    Disposer.register(module.getProject(), new Disposable() {
+      @Override
+      public void dispose() {
+        FileBasedIndex.getInstance().removeIndexableSet(indexableFileSet);
+      }
+    });
+
+    return srcRoot;
+  }
+
+  protected void createContentEntry(@NotNull final Module module, @NotNull final VirtualFile srcRoot) {
+    updateModel(module, new Consumer<ModifiableRootModel>() {
+      @Override
+      public void consume(ModifiableRootModel model) {
+        Sdk sdk = getSdk();
+        if (sdk != null) {
+          model.setSdk(sdk);
+        }
+
+        ContentEntry contentEntry = model.addContentEntry(srcRoot);
+        contentEntry.addSourceFolder(srcRoot, false);
+
+        configureModule(module, model, contentEntry);
+      }
+    });
+  }
+
+  @Nullable
+  public Sdk getSdk() {
+    return null;
+  }
+
+  private void cleanSourceRoot(@NotNull VirtualFile contentRoot) throws IOException {
+    TempFileSystem tempFs = (TempFileSystem)contentRoot.getFileSystem();
+    for (VirtualFile child : contentRoot.getChildren()) {
+      if (!tempFs.exists(child)) {
+        tempFs.createChildFile(this, contentRoot, child.getName());
+      }
+      child.delete(this);
+    }
+  }
+
+  protected void configureModule(@NotNull Module module, @NotNull ModifiableRootModel model, @NotNull ContentEntry contentEntry) {
+  }
+  
+  public interface SetupHandler {
+    void moduleCreated(@NotNull Module module);
+    void sourceRootCreated(@NotNull VirtualFile sourceRoot); 
+  } 
 }
