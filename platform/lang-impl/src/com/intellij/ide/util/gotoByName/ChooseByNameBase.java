ea07addeee4012faef640ea6ee194e95750e621f
peter <peter@jetbrains.com>
2015-07-20 19:33:53 +0200

    run choose by name bg calculation in smart mode (EA-65609 - INRE: FileBasedIndexImpl.handleDumbMode)

--- platform/lang-impl/src/com/intellij/ide/util/gotoByName/ChooseByNameBase.java
+++ platform/lang-impl/src/com/intellij/ide/util/gotoByName/ChooseByNameBase.java
@@ -49,6 +49,7 @@ import com.intellij.openapi.progress.util.ProgressIndicatorBase;
 import com.intellij.openapi.progress.util.ProgressIndicatorUtils;
 import com.intellij.openapi.progress.util.ReadTask;
 import com.intellij.openapi.progress.util.TooManyUsagesStatus;
+import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.popup.*;
 import com.intellij.openapi.util.*;
@@ -1494,7 +1495,7 @@ public abstract class ChooseByNameBase {
     return panel;
   }
 
-  private class CalcElementsThread implements ReadTask {
+  private class CalcElementsThread extends ReadTask {
     private final String myPattern;
     private final boolean myCheckboxState;
     private final Consumer<Set<?>> myCallback;
@@ -1519,6 +1520,16 @@ public abstract class ChooseByNameBase {
       myCalcElementsThread = this;
       showCard(SEARCHING_CARD, 200);
       ProgressIndicatorUtils.scheduleWithWriteActionPriority(myProgress, this);
+    }
+
+    @Override
+    public void runBackgroundProcess(@NotNull final ProgressIndicator indicator) {
+      DumbService.getInstance(myProject).runReadActionInSmartMode(new Runnable() {
+        @Override
+        public void run() {
+          computeInReadAction(indicator);
+        }
+      });
     }
 
     @Override
