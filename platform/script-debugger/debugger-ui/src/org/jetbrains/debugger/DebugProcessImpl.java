58becc74a0cb0cbd7b3d6674cfad9da58c05f4a4
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2015-07-27 15:42:48 +0200

    convert to kotlin

--- platform/script-debugger/debugger-ui/src/org/jetbrains/debugger/DebugProcessImpl.java
+++ platform/script-debugger/debugger-ui/src/org/jetbrains/debugger/DebugProcessImpl.java
@@ -13,291 +13,226 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jetbrains.debugger;
+package org.jetbrains.debugger
 
-import com.intellij.execution.ExecutionResult;
-import com.intellij.execution.process.ProcessHandler;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.Url;
-import com.intellij.util.containers.ContainerUtil;
-import com.intellij.util.io.socketConnection.ConnectionStatus;
-import com.intellij.util.io.socketConnection.SocketConnectionListener;
-import com.intellij.xdebugger.DefaultDebugProcessHandler;
-import com.intellij.xdebugger.XDebugProcess;
-import com.intellij.xdebugger.XDebugSession;
-import com.intellij.xdebugger.XExpression;
-import com.intellij.xdebugger.breakpoints.XBreakpoint;
-import com.intellij.xdebugger.breakpoints.XBreakpointHandler;
-import com.intellij.xdebugger.evaluation.XDebuggerEditorsProvider;
-import com.intellij.xdebugger.frame.XSuspendContext;
-import com.intellij.xdebugger.stepping.XSmartStepIntoHandler;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.debugger.connection.VmConnection;
-import org.jetbrains.debugger.frame.SuspendContextImpl;
+import com.intellij.execution.ExecutionResult
+import com.intellij.execution.process.ProcessHandler
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.util.Url
+import com.intellij.util.containers.ContainerUtil
+import com.intellij.util.io.socketConnection.ConnectionStatus
+import com.intellij.util.io.socketConnection.SocketConnectionListener
+import com.intellij.xdebugger.DefaultDebugProcessHandler
+import com.intellij.xdebugger.XDebugProcess
+import com.intellij.xdebugger.XDebugSession
+import com.intellij.xdebugger.breakpoints.XBreakpoint
+import com.intellij.xdebugger.breakpoints.XBreakpointHandler
+import com.intellij.xdebugger.evaluation.XDebuggerEditorsProvider
+import com.intellij.xdebugger.frame.XSuspendContext
+import com.intellij.xdebugger.stepping.XSmartStepIntoHandler
+import org.jetbrains.debugger.connection.VmConnection
+import org.jetbrains.debugger.frame.SuspendContextImpl
+import java.util.concurrent.ConcurrentMap
+import java.util.concurrent.atomic.AtomicBoolean
+import javax.swing.event.HyperlinkListener
+import kotlin.properties.Delegates
 
-import javax.swing.event.HyperlinkListener;
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.atomic.AtomicBoolean;
+public abstract class DebugProcessImpl<C : VmConnection<*>>(session: XDebugSession,
+                                                             public val connection: C,
+                                                             private val editorsProvider: XDebuggerEditorsProvider,
+                                                             private val smartStepIntoHandler: XSmartStepIntoHandler<*>?,
+                                                             protected val executionResult: ExecutionResult?) : XDebugProcess(session) {
+  protected val repeatStepInto: AtomicBoolean = AtomicBoolean()
+  volatile protected var lastStep: StepAction? = null
+  volatile protected var lastCallFrame: CallFrame? = null
+  volatile protected var isForceStep: Boolean = false
+  volatile protected var disableDoNotStepIntoLibraries: Boolean = false
 
-public abstract class DebugProcessImpl<C extends VmConnection> extends XDebugProcess {
-  protected final AtomicBoolean repeatStepInto = new AtomicBoolean();
-  protected volatile StepAction lastStep;
-  protected volatile CallFrame lastCallFrame;
-  protected volatile boolean isForceStep;
-  protected volatile boolean disableDoNotStepIntoLibraries;
+  protected val urlToFileCache: ConcurrentMap<Url, VirtualFile> = ContainerUtil.newConcurrentMap<Url, VirtualFile>()
 
-  protected final ConcurrentMap<Url, VirtualFile> urlToFileCache = ContainerUtil.newConcurrentMap();
+  private var processBreakpointConditionsAtIdeSide: Boolean = false
 
-  protected final C connection;
+  private val _breakpointHandlers: Array<XBreakpointHandler<*>> by Delegates.lazy { createBreakpointHandlers() }
 
-  private boolean processBreakpointConditionsAtIdeSide;
-
-  private final XDebuggerEditorsProvider editorsProvider;
-  private final XSmartStepIntoHandler<?> smartStepIntoHandler;
-  protected XBreakpointHandler<?>[] breakpointHandlers;
-
-  protected final ExecutionResult executionResult;
-
-  protected DebugProcessImpl(@NotNull XDebugSession session, @NotNull C connection,
-                             @NotNull XDebuggerEditorsProvider editorsProvider,
-                             @Nullable XSmartStepIntoHandler<?> smartStepIntoHandler,
-                             @Nullable ExecutionResult executionResult) {
-    super(session);
-
-    this.executionResult = executionResult;
-    this.connection = connection;
-    this.editorsProvider = editorsProvider;
-    this.smartStepIntoHandler = smartStepIntoHandler;
-
-    connection.addListener(new SocketConnectionListener() {
-      @Override
-      public void statusChanged(@NotNull ConnectionStatus status) {
-        if (status == ConnectionStatus.DISCONNECTED || status == ConnectionStatus.DETACHED) {
-          if (status == ConnectionStatus.DETACHED) {
+  init {
+    connection.addListener(object : SocketConnectionListener {
+      override fun statusChanged(status: ConnectionStatus) {
+        if (status === ConnectionStatus.DISCONNECTED || status === ConnectionStatus.DETACHED) {
+          if (status === ConnectionStatus.DETACHED) {
             if (getRealProcessHandler() != null) {
               // here must we must use effective process handler
-              getProcessHandler().detachProcess();
+              getProcessHandler().detachProcess()
             }
           }
-          getSession().stop();
+          getSession().stop()
         }
         else {
-          getSession().rebuildViews();
+          getSession().rebuildViews()
         }
       }
-    });
+    })
   }
 
-  @Nullable
-  protected final ProcessHandler getRealProcessHandler() {
-    return executionResult == null ? null : executionResult.getProcessHandler();
+  protected fun getRealProcessHandler(): ProcessHandler? = executionResult?.getProcessHandler()
+
+  override final fun getSmartStepIntoHandler() = smartStepIntoHandler
+
+  override final fun getBreakpointHandlers() = _breakpointHandlers
+
+  override fun getEditorsProvider() = editorsProvider
+
+  public fun setProcessBreakpointConditionsAtIdeSide(processBreakpointConditionsAtIdeSide: Boolean) {
+    this.processBreakpointConditionsAtIdeSide = processBreakpointConditionsAtIdeSide
   }
 
-  @NotNull
-  public final C getConnection() {
-    return connection;
-  }
+  public fun getVm(): Vm? = connection.getVm()
 
-  @Override
-  @Nullable
-  public final XSmartStepIntoHandler<?> getSmartStepIntoHandler() {
-    return smartStepIntoHandler;
-  }
+  protected abstract fun createBreakpointHandlers(): Array<XBreakpointHandler<*>>
 
-  @NotNull
-  @Override
-  public final XBreakpointHandler<?>[] getBreakpointHandlers() {
-    return breakpointHandlers;
-  }
-
-  @Override
-  @NotNull
-  public final XDebuggerEditorsProvider getEditorsProvider() {
-    return editorsProvider;
-  }
-
-  public void setProcessBreakpointConditionsAtIdeSide(boolean processBreakpointConditionsAtIdeSide) {
-    this.processBreakpointConditionsAtIdeSide = processBreakpointConditionsAtIdeSide;
-  }
-
-  public final Vm getVm() {
-    return connection.getVm();
-  }
-
-  private void updateLastCallFrame() {
-    Vm vm = getVm();
+  private fun updateLastCallFrame() {
+    val vm = getVm()
     if (vm != null) {
-      SuspendContext context = vm.getSuspendContextManager().getContext();
+      val context = vm.getSuspendContextManager().getContext()
       if (context != null) {
-        lastCallFrame = context.getTopFrame();
-        return;
+        lastCallFrame = context.getTopFrame()
+        return
       }
     }
 
-    lastCallFrame = null;
+    lastCallFrame = null
   }
 
-  @Override
-  public boolean checkCanPerformCommands() {
-    return getVm() != null;
+  override fun checkCanPerformCommands() = getVm() != null
+
+  override fun isValuesCustomSorted() = true
+
+  override fun startStepOver() {
+    updateLastCallFrame()
+    continueVm(StepAction.OVER)
   }
 
-  @Override
-  public boolean isValuesCustomSorted() {
-    return true;
+  override fun startForceStepInto() {
+    isForceStep = true
+    startStepInto()
   }
 
-  @Override
-  public void startStepOver() {
-    updateLastCallFrame();
-    continueVm(StepAction.OVER);
+  override fun startStepInto() {
+    updateLastCallFrame()
+    continueVm(StepAction.IN)
   }
 
-  @Override
-  public void startForceStepInto() {
-    isForceStep = true;
-    startStepInto();
-  }
-
-  @Override
-  public void startStepInto() {
-    updateLastCallFrame();
-    continueVm(StepAction.IN);
-  }
-
-  @Override
-  public void startStepOut() {
+  override fun startStepOut() {
     if (isVmStepOutCorrect()) {
-      lastCallFrame = null;
+      lastCallFrame = null
     }
     else {
-      updateLastCallFrame();
+      updateLastCallFrame()
     }
-    continueVm(StepAction.OUT);
+    continueVm(StepAction.OUT)
   }
 
   // some VM (firefox for example) doesn't implement step out correctly, so, we need to fix it
-  protected boolean isVmStepOutCorrect() {
-    return true;
-  }
+  protected open fun isVmStepOutCorrect(): Boolean = true
 
-  protected void continueVm(@NotNull StepAction stepAction) {
-    SuspendContextManager suspendContextManager = getVm().getSuspendContextManager();
-    if (stepAction == StepAction.CONTINUE) {
+  protected fun continueVm(stepAction: StepAction) {
+    val suspendContextManager = getVm()!!.getSuspendContextManager()
+    if (stepAction === StepAction.CONTINUE) {
       if (suspendContextManager.getContext() == null) {
         // on resumed we ask session to resume, and session then call our "resume", but we have already resumed, so, we don't need to send "continue" message
-        return;
+        return
       }
 
-      lastStep = null;
-      lastCallFrame = null;
-      urlToFileCache.clear();
-      disableDoNotStepIntoLibraries = false;
+      lastStep = null
+      lastCallFrame = null
+      urlToFileCache.clear()
+      disableDoNotStepIntoLibraries = false
     }
     else {
-      lastStep = stepAction;
+      lastStep = stepAction
     }
-    suspendContextManager.continueVm(stepAction, 1);
+    suspendContextManager.continueVm(stepAction, 1)
   }
 
-  protected final void setOverlay() {
-    getVm().getSuspendContextManager().setOverlayMessage("Paused in debugger");
+  protected fun setOverlay() {
+    getVm()!!.getSuspendContextManager().setOverlayMessage("Paused in debugger")
   }
 
-  protected final void processBreakpoint(@NotNull final SuspendContext suspendContext,
-                                         @NotNull final XBreakpoint<?> breakpoint,
-                                         @NotNull final SuspendContextImpl xSuspendContext) {
-    XExpression conditionExpression = breakpoint.getConditionExpression();
-    String condition = conditionExpression == null ? null : conditionExpression.getExpression();
+  protected fun processBreakpoint(suspendContext: SuspendContext, breakpoint: XBreakpoint<*>, xSuspendContext: SuspendContextImpl) {
+    val conditionExpression = breakpoint.getConditionExpression()
+    val condition = conditionExpression?.getExpression()
     if (!processBreakpointConditionsAtIdeSide || condition == null) {
-      processBreakpointLogExpressionAndSuspend(breakpoint, xSuspendContext, suspendContext);
+      processBreakpointLogExpressionAndSuspend(breakpoint, xSuspendContext, suspendContext)
     }
     else {
-      xSuspendContext.evaluateExpression(condition).done(new ContextDependentAsyncResultConsumer<String>(suspendContext) {
-        @Override
-        public void consume(String evaluationResult, @NotNull Vm vm) {
-          if ("false".equals(evaluationResult)) {
-            resume();
+      xSuspendContext.evaluateExpression(condition).done(object : ContextDependentAsyncResultConsumer<String>(suspendContext) {
+        override fun consume(evaluationResult: String, vm: Vm) {
+          if ("false" == evaluationResult) {
+            resume()
           }
           else {
-            processBreakpointLogExpressionAndSuspend(breakpoint, xSuspendContext, suspendContext);
+            processBreakpointLogExpressionAndSuspend(breakpoint, xSuspendContext, suspendContext)
           }
         }
-      }).rejected(new ContextDependentAsyncResultConsumer<Throwable>(suspendContext) {
-        @Override
-        public void consume(Throwable failure, @NotNull Vm vm) {
-          processBreakpointLogExpressionAndSuspend(breakpoint, xSuspendContext, suspendContext);
+      }).rejected(object : ContextDependentAsyncResultConsumer<Throwable>(suspendContext) {
+        override fun consume(failure: Throwable, vm: Vm) {
+          processBreakpointLogExpressionAndSuspend(breakpoint, xSuspendContext, suspendContext)
         }
-      });
+      })
     }
   }
 
-  private void processBreakpointLogExpressionAndSuspend(@NotNull final XBreakpoint<?> breakpoint,
-                                                        @NotNull final SuspendContextImpl xSuspendContext,
-                                                        @NotNull SuspendContext suspendContext) {
-    XExpression logExpressionObject = breakpoint.getLogExpressionObject();
-    final String logExpression = logExpressionObject == null ? null : logExpressionObject.getExpression();
+  private fun processBreakpointLogExpressionAndSuspend(breakpoint: XBreakpoint<*>, xSuspendContext: SuspendContextImpl, suspendContext: SuspendContext) {
+    val logExpressionObject = breakpoint.getLogExpressionObject()
+    val logExpression = logExpressionObject?.getExpression()
 
     if (logExpression == null) {
-      breakpointReached(breakpoint, null, xSuspendContext);
+      breakpointReached(breakpoint, null, xSuspendContext)
     }
     else {
-      xSuspendContext.evaluateExpression(logExpression).done(new ContextDependentAsyncResultConsumer<String>(suspendContext) {
-        @Override
-        public void consume(String logResult, @NotNull Vm vm) {
-          breakpointReached(breakpoint, logResult, xSuspendContext);
+      xSuspendContext.evaluateExpression(logExpression).done(object : ContextDependentAsyncResultConsumer<String>(suspendContext) {
+        override fun consume(logResult: String, vm: Vm) {
+          breakpointReached(breakpoint, logResult, xSuspendContext)
         }
-      }).rejected(new ContextDependentAsyncResultConsumer<Throwable>(suspendContext) {
-        @Override
-        public void consume(Throwable logResult, @NotNull Vm vm) {
-          breakpointReached(breakpoint, "Failed to evaluate expression: " + logExpression, xSuspendContext);
+      }).rejected(object : ContextDependentAsyncResultConsumer<Throwable>(suspendContext) {
+        override fun consume(logResult: Throwable, vm: Vm) {
+          breakpointReached(breakpoint, "Failed to evaluate expression: " + logExpression, xSuspendContext)
         }
-      });
+      })
     }
   }
 
-  private void breakpointReached(@NotNull XBreakpoint<?> breakpoint,
-                                 @Nullable String evaluatedLogExpression,
-                                 @NotNull XSuspendContext suspendContext) {
+  private fun breakpointReached(breakpoint: XBreakpoint<*>, evaluatedLogExpression: String?, suspendContext: XSuspendContext) {
     if (getSession().breakpointReached(breakpoint, evaluatedLogExpression, suspendContext)) {
-      setOverlay();
+      setOverlay()
     }
     else {
-      resume();
+      resume()
     }
   }
 
-  @Override
-  public final void startPausing() {
-    connection.getVm().getSuspendContextManager().suspend().rejected(new RejectErrorReporter(getSession(), "Cannot pause"));
+  override fun startPausing() {
+    connection.getVm().getSuspendContextManager().suspend().rejected(RejectErrorReporter(getSession(), "Cannot pause"))
   }
 
-  @Override
-  public final String getCurrentStateMessage() {
-    return connection.getState().getMessage();
+  override fun getCurrentStateMessage(): String {
+    return connection.getState().getMessage()
   }
 
-  @Nullable
-  @Override
-  public final HyperlinkListener getCurrentStateHyperlinkListener() {
-    return getConnection().getState().getMessageLinkListener();
+  override fun getCurrentStateHyperlinkListener(): HyperlinkListener? {
+    return connection.getState().getMessageLinkListener()
   }
 
-  @Override
-  @NotNull
-  protected ProcessHandler doGetProcessHandler() {
-    return executionResult == null ? new SilentDestroyDebugProcessHandler() : executionResult.getProcessHandler();
+  override fun doGetProcessHandler(): ProcessHandler {
+    return if (executionResult == null) SilentDestroyDebugProcessHandler() else executionResult.getProcessHandler()
   }
 
-  private static final class SilentDestroyDebugProcessHandler extends DefaultDebugProcessHandler {
-    @Override
-    public boolean isSilentlyDestroyOnClose() {
-      return true;
+  private class SilentDestroyDebugProcessHandler : DefaultDebugProcessHandler() {
+    override fun isSilentlyDestroyOnClose(): Boolean {
+      return true
     }
   }
 
-  public void saveResolvedFile(@NotNull Url url, @NotNull VirtualFile file) {
-    urlToFileCache.putIfAbsent(url, file);
+  public fun saveResolvedFile(url: Url, file: VirtualFile) {
+    urlToFileCache.putIfAbsent(url, file)
   }
 }
