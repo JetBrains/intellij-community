36aa051f9096065a782ac879a2bd049d3fac8912
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2015-07-29 09:25:35 +0200

    cleanup

--- platform/configuration-store-impl/testSrc/StateStorageManageTest.kt
+++ platform/configuration-store-impl/testSrc/StateStorageManageTest.kt
@@ -28,7 +28,7 @@ import org.junit.Rule
 import org.junit.Test
 import kotlin.properties.Delegates
 
-class StateStorageManagerImplTest {
+class StateStorageManageTest {
   private val fixtureManager = FixtureRule()
   public Rule fun getFixtureManager(): FixtureRule = fixtureManager
 
