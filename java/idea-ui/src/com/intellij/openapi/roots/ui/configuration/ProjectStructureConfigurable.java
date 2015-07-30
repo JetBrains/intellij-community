2f0789b4d54a64866ee6f8a1fb781221282e7457
peter <peter@jetbrains.com>
2015-07-30 16:13:15 +0200

    ProjectStructureConfigurable contains global SDK settings, so dumb mode can start in any project

--- java/idea-ui/src/com/intellij/openapi/roots/ui/configuration/ProjectStructureConfigurable.java
+++ java/idea-ui/src/com/intellij/openapi/roots/ui/configuration/ProjectStructureConfigurable.java
@@ -336,7 +336,7 @@ public class ProjectStructureConfigurable extends BaseConfigurable implements Se
       }
     }
     final Ref<ConfigurationException> exceptionRef = Ref.create();
-    DumbService.getInstance(myProject).allowStartingDumbModeInside(DumbModePermission.MAY_START_BACKGROUND, new Runnable() {
+    DumbService.allowStartingDumbModeInside(DumbModePermission.MAY_START_BACKGROUND, ProjectManager.getInstance().getOpenProjects(), new Runnable() {
       @Override
       public void run() {
         try {
