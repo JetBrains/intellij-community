cb1fcce02ea54d8d880cfb910531045b30baa2fd
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2015-08-03 17:09:08 +0200

    bombed failing tests

--- platform/platform-tests/testSrc/com/intellij/lang/PsiBuilderQuickTest.java
+++ platform/platform-tests/testSrc/com/intellij/lang/PsiBuilderQuickTest.java
@@ -15,6 +15,7 @@
  */
 package com.intellij.lang;
 
+import com.intellij.idea.Bombed;
 import com.intellij.lang.impl.PsiBuilderImpl;
 import com.intellij.lexer.Lexer;
 import com.intellij.lexer.LexerBase;
@@ -34,6 +35,7 @@ import com.intellij.util.diff.ShallowNodeComparator;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Calendar;
 import java.util.List;
 
 public class PsiBuilderQuickTest extends LightPlatformTestCase {
@@ -483,6 +485,7 @@ public class PsiBuilderQuickTest extends LightPlatformTestCase {
            "      PsiWhiteSpace(' ')\n");
   }
 
+  @Bombed(month = Calendar.AUGUST, day = 15)
   public void testEmptyCollapsedNode() {
     doTest("a<<b",
            new Parser() {
