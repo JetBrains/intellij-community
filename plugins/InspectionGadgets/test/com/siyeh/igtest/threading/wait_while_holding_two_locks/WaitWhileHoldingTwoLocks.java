d6f3d395cf2e29b59693eac324b8810035e3e154
Bas Leijdekkers <basleijdekkers@gmail.com>
2015-08-04 23:03:56 +0200

    IG: test for "'wait()' while holding two locks" inspection

--- plugins/InspectionGadgets/test/com/siyeh/igtest/threading/wait_while_holding_two_locks/WaitWhileHoldingTwoLocks.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/threading/wait_while_holding_two_locks/WaitWhileHoldingTwoLocks.java
@@ -1,6 +1,6 @@
 package com.siyeh.igtest.threading;
 
-public class WaitWhileHoldingTwoLocksInspection
+public class WaitWhileHoldingTwoLocks
 {
     private final Object lock = new Object();
     private final Object lock2 = new Object();
@@ -8,14 +8,14 @@ public class WaitWhileHoldingTwoLocksInspection
     public  void foo() throws InterruptedException {
         synchronized (lock2) {
             synchronized (lock) {
-                lock.wait();
+                lock.<warning descr="Call to 'wait()' is made while holding two locks">wait</warning>();
             }
         }
 
     }
     public  synchronized void bar() throws InterruptedException {
         synchronized (lock) {
-            lock.wait();
+            lock.<warning descr="Call to 'wait()' is made while holding two locks">wait</warning>();
         }
     }
 
