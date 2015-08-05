125a384fec1f5adc350efb9facac4d57606c5549
badlogic <badlogicgames@gmail.com>
2015-08-05 20:46:19 +0200

    Fixed up plugins getter script to use a specific version of the android plugin and tool base

--- getPlugins.sh
+++ getPlugins.sh
@@ -1,10 +1,15 @@
+ANDROID_PLUGIN_COMMIT=46f7904a9d8ce097136a942151724ffd51bfba12
+ANDROID_TOOLS_BASE_COMMIT=4a46ed4b0524fc6389962de7fa3d79f69c6193a6
+rm -rf android
 git clone git://git.jetbrains.org/idea/android.git android
 cd android
 git pull
+git checkout $ANDROID_PLUGIN_COMMIT
 cd ..
 git clone git://git.jetbrains.org/idea/adt-tools-base.git android/tools-base
 cd android/tools-base
 git pull
+git checkout $ANDROID_TOOLS_BASE_COMMIT
 cd ../..
 rm -rf robovm/robovm-idea
 git clone https://github.com/robovm/robovm-idea robovm/robovm-idea
