3828b8c3df0c02c15c1aecc58d62a7be450194fa
Alexey Kudravtsev <cdr@intellij.com>
2015-08-18 14:15:45 +0300

    optimization: dequadratisation (Prefer to resolve first, traverse up later to avoid performance drop in deep hierarchies)

--- plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/assignment/AssignmentToSuperclassFieldInspection.java
+++ plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/assignment/AssignmentToSuperclassFieldInspection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -77,10 +77,6 @@ public class AssignmentToSuperclassFieldInspection extends  BaseInspection {
       if (!(expression instanceof PsiReferenceExpression)) {
         return;
       }
-      final PsiMethod method = PsiTreeUtil.getParentOfType(expression, PsiMethod.class, true, PsiClass.class, PsiLambdaExpression.class);
-      if (method == null || !method.isConstructor()) {
-        return;
-      }
       final PsiReferenceExpression referenceExpression = (PsiReferenceExpression)expression;
       final PsiExpression qualifierExpression = referenceExpression.getQualifierExpression();
       if (qualifierExpression != null &&
@@ -94,6 +90,10 @@ public class AssignmentToSuperclassFieldInspection extends  BaseInspection {
       final PsiField field = (PsiField)target;
       final PsiClass fieldClass = field.getContainingClass();
       if (fieldClass == null) {
+        return;
+      }
+      final PsiMethod method = PsiTreeUtil.getParentOfType(expression, PsiMethod.class, true, PsiClass.class, PsiLambdaExpression.class);
+      if (method == null || !method.isConstructor()) {
         return;
       }
       final PsiClass assignmentClass = method.getContainingClass();
