6cac068ea3d4de85978abb9a26d6e1a8fd1c96e1
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2015-08-18 14:33:09 +0200

    StateMap — it is not allowed to call load twice, so, simplify — load as a constructor

--- platform/configuration-store-impl/testSrc/ModuleStoreTest.kt
+++ platform/configuration-store-impl/testSrc/ModuleStoreTest.kt
@@ -9,15 +9,13 @@ import com.intellij.openapi.project.ex.ProjectEx
 import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.roots.ModuleRootModificationUtil
 import com.intellij.openapi.roots.impl.storage.ClasspathStorage
-import com.intellij.openapi.util.io.parentSystemIndependentPath
-import com.intellij.openapi.util.io.systemIndependentPath
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.testFramework.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.ClassRule
 import org.junit.Rule
 import org.junit.Test
-import java.io.File
+import java.nio.file.Path
 
 @RunsInEdt class ModuleStoreTest {
   companion object {
@@ -36,7 +34,7 @@ import java.io.File
 
     private fun VirtualFile.loadModule() = runWriteAction { ModuleManager.getInstance(projectRule.project).loadModule(getPath()) }
 
-    private fun File.createModule() = runWriteAction { ModuleManager.getInstance(projectRule.project).newModule(systemIndependentPath, ModuleTypeId.JAVA_MODULE) }
+    private fun Path.createModule() = runWriteAction { ModuleManager.getInstance(projectRule.project).newModule(systemIndependentPath, ModuleTypeId.JAVA_MODULE) }
   }
 
   private val tempDirManager = TemporaryDirectory()
@@ -45,7 +43,7 @@ import java.io.File
 
   public Rule fun getChain(): RuleChain = ruleChain
 
-  public @Test fun `set option`() {
+  @Test fun `set option`() {
     val moduleFile = runWriteAction {
       VfsTestUtil.createFile(tempDirManager.newVirtualDirectory("module"), "test.iml", "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
         "<module type=\"JAVA_MODULE\" foo=\"bar\" version=\"4\" />")
@@ -68,14 +66,14 @@ import java.io.File
     }
   }
 
-  public @Test fun `must be empty if classpath storage`() {
+  @Test fun `must be empty if classpath storage`() {
     // we must not use VFS here, file must not be created
-    val moduleFile = File(tempDirManager.newDirectory("module"), "test.iml")
+    val moduleFile = tempDirManager.newPath("module").resolve("test.iml")
     projectRule.project.runInStoreLoadMode {
       moduleFile.createModule().useAndDispose {
         ModuleRootModificationUtil.addContentRoot(this, moduleFile.parentSystemIndependentPath)
         saveStore()
-        assertThat(moduleFile).isFile()
+        assertThat(moduleFile).isRegularFile()
         assertThat(moduleFile.readText()).startsWith("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<module type=\"JAVA_MODULE\" version=\"4\">")
 
         ClasspathStorage.setStorageType(ModuleRootManager.getInstance(this), "eclipse")
