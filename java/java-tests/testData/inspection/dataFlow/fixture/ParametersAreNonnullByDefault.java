0bae70149a34e8e21c2f32c8967c1f4c8e7c8106
peter <peter@jetbrains.com>
2015-08-18 19:49:14 +0200

    IDEA-143937 Bogus warning by 'Constant conditions & exceptions' inspection within lambda expressions, due to JSR 305 @TypeQualifierDefault annotation

--- java/java-tests/testData/inspection/dataFlow/fixture/ParametersAreNonnullByDefault.java
+++ java/java-tests/testData/inspection/dataFlow/fixture/ParametersAreNonnullByDefault.java
@@ -15,6 +15,17 @@ class Some {
 }
 
 @javax.annotation.ParametersAreNonnullByDefault
+class CheckLoop {
+  void foo(Iterable<String> it) {
+    for (String s : it) {
+      if (s == null) { // it's not ElementType.PARAMETER, no warning here
+        System.out.println();
+      }
+    }
+  }
+}
+
+@javax.annotation.ParametersAreNonnullByDefault
 class NotNullClass {
   static void foo(String s) {}
   
