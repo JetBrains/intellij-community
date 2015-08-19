f7d14e930702ae9aa42d279c4666029e1486c395
peter <peter@jetbrains.com>
2015-08-19 20:45:28 +0200

    global inspections: intern RefEntity names to save memory

--- platform/analysis-impl/src/com/intellij/codeInspection/reference/RefEntityImpl.java
+++ platform/analysis-impl/src/com/intellij/codeInspection/reference/RefEntityImpl.java
@@ -45,7 +45,7 @@ abstract class RefEntityImpl implements RefEntity {
 
   RefEntityImpl(@NotNull String name, @NotNull RefManager manager) {
     myManager = (RefManagerImpl)manager;
-    myName = name;
+    myName = myManager.internName(name);
   }
 
   @NotNull
