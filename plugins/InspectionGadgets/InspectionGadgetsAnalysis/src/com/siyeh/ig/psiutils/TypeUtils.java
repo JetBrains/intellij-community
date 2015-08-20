c9081d2c7b5abea9b07b52de215d18d7ef254ef0
Bas Leijdekkers <basleijdekkers@gmail.com>
2015-08-20 15:29:02 +0200

    IG: consider boxing in "Unary plus" inspection

--- plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/psiutils/TypeUtils.java
+++ plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/psiutils/TypeUtils.java
@@ -213,4 +213,28 @@ public class TypeUtils {
     final PsiClass aClass = classType.resolve();
     return aClass != null && aClass instanceof PsiTypeParameter;
   }
+
+  /**
+   * JLS 5.6.1 Unary Numeric Promotion
+   */
+  public static PsiType unaryNumericPromotion(PsiType type) {
+    if (type == null) {
+      return null;
+    }
+    if (type.equalsToText("java.lang.Byte") || type.equalsToText("java.lang.Short") ||
+        type.equalsToText("java.lang.Character") || type.equalsToText("java.lang.Integer") ||
+        type.equals(PsiType.BYTE) || type.equals(PsiType.SHORT) || type.equals(PsiType.CHAR)) {
+      return PsiType.INT;
+    }
+    else if (type.equalsToText("java.lang.Long")) {
+      return PsiType.LONG;
+    }
+    else if (type.equalsToText("java.lang.Float")) {
+      return PsiType.FLOAT;
+    }
+    else if (type.equalsToText("java.lang.Double")) {
+      return PsiType.DOUBLE;
+    }
+    return type;
+  }
 }
