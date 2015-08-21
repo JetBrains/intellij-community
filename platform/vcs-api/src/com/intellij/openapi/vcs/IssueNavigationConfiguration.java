2a6f4dfeb8541fa0b790d35512affdff8e5a5067
Alexander Zolotov <alexander.zolotov@jetbrains.com>
2015-08-21 20:00:46 +0300

    String -> CharSequence

--- platform/vcs-api/src/com/intellij/openapi/vcs/IssueNavigationConfiguration.java
+++ platform/vcs-api/src/com/intellij/openapi/vcs/IssueNavigationConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -92,7 +92,7 @@ public class IssueNavigationConfiguration implements PersistentStateComponent<Is
     }
   }
 
-  public List<LinkMatch> findIssueLinks(String text) {
+  public List<LinkMatch> findIssueLinks(CharSequence text) {
     final List<LinkMatch> result = new ArrayList<LinkMatch>();
     for(IssueNavigationLink link: myLinks) {
       Pattern issuePattern = link.getIssuePattern();
