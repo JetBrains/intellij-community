2f0239522121bd54b4c02af0c2e54bb84282d136
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2015-08-26 16:42:39 +0300

    fixed post task with no tests

--- python/educational/src/com/jetbrains/edu/courseFormat/Task.java
+++ python/educational/src/com/jetbrains/edu/courseFormat/Task.java
@@ -140,35 +140,35 @@ public class Task implements StudyItem {
     return null;
   }
 
-  @Nullable
+  @NotNull
   public String getTaskText(@NotNull final Project project) {
     if (!StringUtil.isEmptyOrSpaces(text)) return text;
     final VirtualFile taskDir = getTaskDir(project);
     if (taskDir != null) {
       final VirtualFile file = taskDir.findChild(EduNames.TASK_HTML);
-      if (file == null) return null;
+      if (file == null) return "";
       final Document document = FileDocumentManager.getInstance().getDocument(file);
       if (document != null) {
         return document.getImmutableCharSequence().toString();
       }
     }
 
-    return null;
+    return "";
   }
 
-  @Nullable
+  @NotNull
   public String getTestsText(@NotNull final Project project) {
     final VirtualFile taskDir = getTaskDir(project);
     if (taskDir != null) {
       final VirtualFile file = taskDir.findChild(EduNames.TESTS_FILE);
-      if (file == null) return null;
+      if (file == null) return "";
       final Document document = FileDocumentManager.getInstance().getDocument(file);
       if (document != null) {
         return document.getImmutableCharSequence().toString();
       }
     }
 
-    return null;
+    return "";
   }
 
   @Override
