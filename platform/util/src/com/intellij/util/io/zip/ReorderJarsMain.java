1766b2b375be60266ada3bbb669c2683e0b3624e
Egor.Ushakov <egor.ushakov@jetbrains.com>
2015-08-26 12:24:03 +0300

    delete temp files in ReorderJars no matter what

--- platform/util/src/com/intellij/util/io/zip/ReorderJarsMain.java
+++ platform/util/src/com/intellij/util/io/zip/ReorderJarsMain.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -68,7 +68,7 @@ public class ReorderJarsMain {
           }
         });
 
-        final File tempJarFile = FileUtil.createTempFile("__reorder__", "__reorder__");
+        final File tempJarFile = FileUtil.createTempFile("__reorder__", "__reorder__", true);
         final JBZipFile file = new JBZipFile(tempJarFile);
 
         final JBZipEntry sizeEntry = file.getOrCreateEntry(JarMemoryLoader.SIZE_ENTRY);
