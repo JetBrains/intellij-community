c6b5947f11f8ebd443254781e3499885db21e84c
Egor.Ushakov <egor.ushakov@jetbrains.com>
2015-09-01 16:26:49 +0300

    EA-70970 - IAE: ServiceManager.getService

--- platform/lang-impl/src/com/intellij/codeInsight/lookup/LookupManager.java
+++ platform/lang-impl/src/com/intellij/codeInsight/lookup/LookupManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,7 +28,7 @@ import org.jetbrains.annotations.Nullable;
 import java.beans.PropertyChangeListener;
 
 public abstract class LookupManager {
-  public static LookupManager getInstance(Project project){
+  public static LookupManager getInstance(@NotNull Project project){
     return ServiceManager.getService(project, LookupManager.class);
   }
 
