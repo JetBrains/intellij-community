10d5db3598532ebdc42d056b4c7e57c7560ef4de
Bas Leijdekkers <basleijdekkers@gmail.com>
2015-09-02 14:50:47 +0200

    IDEA-144665 (Incorrect 'tearDown() is not called from finally block' inspection)

--- plugins/InspectionGadgets/test/com/siyeh/igtest/junit/super_tear_down_in_finally/SuperTearDownInFinally.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/junit/super_tear_down_in_finally/SuperTearDownInFinally.java
@@ -1,17 +1,19 @@
-class SuperTearDownInFinally extends junit.framework.TestCase {
+import junit.framework.TestCase;
+
+class SuperTearDownInFinally extends TestCase {
 
   public void tearDown() throws Exception {
     super.<warning descr="'tearDown()' not called from 'finally' block">tearDown</warning>();
     System.out.println();
   }
 }
-class NoProblem extends junit.framework.TestCase {
+class NoProblem extends TestCase {
 
   public void tearDown() throws Exception {
     super.tearDown();
   }
 }
-class CalledInFinally extends junit.framework.TestCase {
+class CalledInFinally extends TestCase {
 
   public void tearDown() throws Exception {
     try {
@@ -20,4 +22,16 @@ class CalledInFinally extends junit.framework.TestCase {
       super.tearDown();
     }
   }
+}
+class SomeTest extends TestCase {
+  @Override
+  protected void setUp() throws Exception {
+    try {
+      super.setUp();
+    }
+    catch (Throwable t) {
+      super.tearDown(); // yellow code
+    }
+  }
+  public void test_something() {}
 }
