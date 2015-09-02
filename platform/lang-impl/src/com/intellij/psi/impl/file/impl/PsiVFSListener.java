8d5dd11cdba9a214dc541442ce71b9364d3d48e2
peter <peter@jetbrains.com>
2015-09-02 08:47:44 +0200

    avoid PsiVFSListener nesting (EA-56525 - assert: PsiManagerImpl.fireEvent)

--- platform/lang-impl/src/com/intellij/psi/impl/file/impl/PsiVFSListener.java
+++ platform/lang-impl/src/com/intellij/psi/impl/file/impl/PsiVFSListener.java
@@ -29,10 +29,10 @@ import com.intellij.openapi.fileTypes.FileTypeListener;
 import com.intellij.openapi.fileTypes.FileTypeManager;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.roots.*;
 import com.intellij.openapi.roots.impl.PushedFilePropertiesUpdater;
 import com.intellij.openapi.startup.StartupManager;
-import com.intellij.openapi.util.io.FileUtilRt;
 import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.vfs.*;
 import com.intellij.openapi.vfs.impl.BulkVirtualFileListenerAdapter;
@@ -60,6 +60,30 @@ public class PsiVFSListener extends VirtualFileAdapter {
   private final Project myProject;
   private boolean myReportedUnloadedPsiChange;
 
+  static {
+    ApplicationManager.getApplication().getMessageBus().connect().subscribe(VirtualFileManager.VFS_CHANGES, new BulkFileListener() {
+      @Override
+      public void before(@NotNull List<? extends VFileEvent> events) {
+        for (Project project : ProjectManager.getInstance().getOpenProjects()) {
+          PsiVFSListener listener = project.getComponent(PsiVFSListener.class);
+          assert listener != null;
+          new BulkVirtualFileListenerAdapter(listener).before(events);
+        }
+      }
+
+      @Override
+      public void after(@NotNull List<? extends VFileEvent> events) {
+        for (Project project : ProjectManager.getInstance().getOpenProjects()) {
+          PsiVFSListener listener = project.getComponent(PsiVFSListener.class);
+          assert listener != null;
+          listener.myReportedUnloadedPsiChange = false;
+          new BulkVirtualFileListenerAdapter(listener).after(events);
+          listener.myReportedUnloadedPsiChange = false;
+        }
+      }
+    });
+  }
+
   public PsiVFSListener(Project project) {
     myProject = project;
     myFileTypeManager = FileTypeManager.getInstance();
@@ -72,20 +96,6 @@ public class PsiVFSListener extends VirtualFileAdapter {
     StartupManager.getInstance(project).registerPreStartupActivity(new Runnable() {
       @Override
       public void run() {
-        final BulkVirtualFileListenerAdapter adapter = new BulkVirtualFileListenerAdapter(PsiVFSListener.this);
-        myConnection.subscribe(VirtualFileManager.VFS_CHANGES, new BulkFileListener() {
-          @Override
-          public void before(@NotNull List<? extends VFileEvent> events) {
-            adapter.before(events);
-          }
-
-          @Override
-          public void after(@NotNull List<? extends VFileEvent> events) {
-            myReportedUnloadedPsiChange = false;
-            adapter.after(events);
-            myReportedUnloadedPsiChange = false;
-          }
-        });
         myConnection.subscribe(ProjectTopics.PROJECT_ROOTS, new MyModuleRootListener());
         myConnection.subscribe(FileTypeManager.TOPIC, new FileTypeListener.Adapter() {
           @Override
@@ -593,11 +603,6 @@ public class PsiVFSListener extends VirtualFileAdapter {
     return PsiFileFactory.getInstance(myManager.getProject()).createFileFromText(name, fileTypeByFileName,
                                                                                  document != null ? document.getCharsSequence() : "", vFile.getModificationStamp(),
                                                                                  true, false);
-  }
-
-  // When file is renamed so that extension changes then language dialect might change and thus psiFile should be invalidated
-  private static boolean languageDialectChanged(final PsiFile newPsiFile, String oldFileName) {
-    return newPsiFile != null && !FileUtilRt.extensionEquals(oldFileName, FileUtilRt.getExtension(newPsiFile.getName()));
   }
 
   private class MyModuleRootListener implements ModuleRootListener {
