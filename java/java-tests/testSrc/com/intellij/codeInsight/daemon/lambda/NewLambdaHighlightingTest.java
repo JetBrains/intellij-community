eb638c67d7c6633a17f6c47b3702c4437a13d9c3
Anna Kozlova <anna.kozlova@jetbrains.com>
2015-09-03 16:51:35 +0300

    testdata for IDEA-133920

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewLambdaHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewLambdaHighlightingTest.java
@@ -220,6 +220,10 @@ public class NewLambdaHighlightingTest extends LightDaemonAnalyzerTestCase {
     doTest();
   }
 
+  public void testIDEA133920() throws Exception {
+    doTest();
+  }
+
   private void doTest() {
     doTest(false);
   }
