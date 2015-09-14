8236b297b3ac0319037674ba5d17ef4abf0afa72
Bas Leijdekkers <basleijdekkers@gmail.com>
2015-09-14 19:44:27 +0200

    EA-73040 (AIOOBE: EqualsCalledOnEnumConstantInspection$EqualsCalledOnEnumValueVisitor.visitMethodCallExpression)

--- plugins/InspectionGadgets/test/com/siyeh/igtest/style/equals_called_on_enum_constant/EqualsCalled.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/style/equals_called_on_enum_constant/EqualsCalled.java
@@ -9,6 +9,7 @@ public class EqualsCalled {
     void one() {
         E.A.<warning descr="'equals()' called on Enum value">equals</warning>(E.C);
         E.B.<warning descr="'equals()' called on Enum value">equals</warning>(new Object());
+        E.C.<warning descr="'equals()' called on Enum value">equals</warning><error descr="'equals(java.lang.Object)' in 'java.lang.Enum' cannot be applied to '()'">()</error>;
         final Object A = new Object();
         A.equals(1);
     }
