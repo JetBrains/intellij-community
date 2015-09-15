a98bbc575719edeaaa697f51f328beddd1f711c6
Bas Leijdekkers <basleijdekkers@gmail.com>
2015-09-15 19:34:48 +0200

    make IG test light

--- plugins/InspectionGadgets/testsrc/com/siyeh/ig/naming/ConstantNamingConventionInspectionTest.java
+++ plugins/InspectionGadgets/testsrc/com/siyeh/ig/naming/ConstantNamingConventionInspectionTest.java
@@ -1,10 +1,18 @@
 package com.siyeh.ig.naming;
 
-import com.siyeh.ig.IGInspectionTestCase;
+import com.intellij.codeInspection.InspectionProfileEntry;
+import com.siyeh.ig.LightInspectionTestCase;
+import org.jetbrains.annotations.Nullable;
 
-public class ConstantNamingConventionInspectionTest extends IGInspectionTestCase {
+public class ConstantNamingConventionInspectionTest extends LightInspectionTestCase {
 
-  public void test() throws Exception {
-    doTest("com/siyeh/igtest/naming/constant_naming_convention", new ConstantNamingConventionInspection());
+  public void testConstantNamingConvention() {
+    doTest();
+  }
+
+  @Nullable
+  @Override
+  protected InspectionProfileEntry getInspection() {
+    return new ConstantNamingConventionInspection();
   }
 }
