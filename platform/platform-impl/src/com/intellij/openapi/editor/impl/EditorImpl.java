3cda1e30ee3ebbaf8b02b37469e576a1f447bfb6
Pavel Fatin <pavel.fatin@jetbrains.com>
2015-09-16 19:22:18 +0200

    Zero-latency typing: skip Paste with existing selection

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorImpl.java
@@ -999,7 +999,8 @@ public final class EditorImpl extends UserDataHolderBase implements EditorEx, Hi
     AnActionListener.Adapter actionListener = new AnActionListener.Adapter() {
       @Override
       public void beforeActionPerformed(AnAction action, DataContext dataContext, AnActionEvent event) {
-        if (isZeroLatencyTypingEnabled() && IMMEDIATE_EDITING_ACTIONS.contains(action.getClass())) {
+        if (isZeroLatencyTypingEnabled() && IMMEDIATE_EDITING_ACTIONS.contains(action.getClass()) &&
+            !(action.getClass() == PasteAction.class && mySelectionModel.hasSelection())) {
           myImmediateEditingInProgress = true;
         }
       }
