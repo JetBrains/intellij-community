cb81afa810f493746f0198f59e38243f42e4bb77
Alexander Koshevoy <Alexander.Koshevoy@jetbrains.com>
2015-09-16 12:16:55 +0300

    PY-15476 Remove PythonSdkType.getDockerSkeletonsPath() method that is identical to PythonSdkType.getSkeletonsPath()

--- python/src/com/jetbrains/python/sdk/PythonSdkType.java
+++ python/src/com/jetbrains/python/sdk/PythonSdkType.java
@@ -754,11 +754,6 @@ public class PythonSdkType extends SdkType {
     return getSkeletonsRootPath(basePath) + sep + FileUtil.toSystemIndependentName(sdkHome).hashCode() + sep;
   }
 
-  public static String getDockerSkeletonsPath(String basePath, String sdkHome) {
-    String sep = File.separator;
-    return getSkeletonsRootPath(basePath) + sep + FileUtil.toSystemIndependentName(sdkHome).hashCode() + sep;
-  }
-
   public static String getSkeletonsRootPath(String basePath) {
     return basePath + File.separator + SKELETON_DIR_NAME;
   }
