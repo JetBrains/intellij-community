6c70777806c5c11855b6cef5e396eded0a53f20a
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2015-09-21 16:08:43 +0300

    Fix IOOBE in PlainDocString parser (EA-73320)

--- python/src/com/jetbrains/python/documentation/docstrings/PlainDocString.java
+++ python/src/com/jetbrains/python/documentation/docstrings/PlainDocString.java
@@ -37,7 +37,7 @@ public class PlainDocString extends DocStringLineParser implements StructuredDoc
     if (!isEmpty(0) && isEmptyOrDoesNotExist(1)) {
       mySummary = getLine(0).trim().toString();
       final int next = consumeEmptyLines(1);
-      if (next != 1) {
+      if (next != 1 && next < getLineCount()) {
         final String remaining = getLine(next).union(getLine(getLineCount() - 1)).toString();
         myDescription = PyIndentUtil.removeCommonIndent(remaining, false);
       }
