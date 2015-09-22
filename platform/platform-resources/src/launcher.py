062c987e76bca0d2c5d9f3e4519449256da025bb
Vladimir.Orlov <Vladimir.Orlov@jetbrains.com>
2015-09-22 17:50:37 +0300

    IDEA-145376 MacOS: "[Errno 2] No such file or directory" error on attempt to open folder with command-line launcher

--- platform/platform-resources/src/launcher.py
+++ platform/platform-resources/src/launcher.py
@@ -82,7 +82,7 @@ try:
     port = int(f.read())
 except Exception:
     type, value, traceback = sys.exc_info()
-    print(value)
+    print('No IDE instance has been found. It will be started new one.')
     port = -1
 
 if port == -1:
