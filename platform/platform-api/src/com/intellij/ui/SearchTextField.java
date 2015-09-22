51f1f032a555946d6438f73ae43b486cae5de8d2
Konstantin Bulenkov <kb@jetbrains.com>
2015-09-22 22:49:38 +0200

    SearchTextField: set proper UI classes in case if IntelliJ laf is available on Mac

--- platform/platform-api/src/com/intellij/ui/SearchTextField.java
+++ platform/platform-api/src/com/intellij/ui/SearchTextField.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -91,11 +91,13 @@ public class SearchTextField extends JPanel {
       public void setUI(TextUI ui) {
         if (SystemInfo.isMac) {
           try {
-            Class<?> uiClass = Class.forName("com.intellij.ide.ui.laf.darcula.ui.DarculaTextFieldUI");
+            Class<?> uiClass = UIUtil.isUnderIntelliJLaF() ? Class.forName("com.intellij.ide.ui.laf.intellij.MacIntelliJTextFieldUI")
+                                                           : Class.forName("com.intellij.ide.ui.laf.darcula.ui.DarculaTextFieldUI");
             Method method = ReflectionUtil.getMethod(uiClass, "createUI", JComponent.class);
             if (method != null) {
               super.setUI((TextUI)method.invoke(uiClass, this));
-              Class<?> borderClass = Class.forName("com.intellij.ide.ui.laf.darcula.ui.DarculaTextBorder");
+              Class<?> borderClass = UIUtil.isUnderIntelliJLaF() ? Class.forName("com.intellij.ide.ui.laf.intellij.MacIntelliJTextBorder")
+                                                                 : Class.forName("com.intellij.ide.ui.laf.darcula.ui.DarculaTextBorder");
               setBorder((Border)ReflectionUtil.newInstance(borderClass));
               setOpaque(false);
             }
