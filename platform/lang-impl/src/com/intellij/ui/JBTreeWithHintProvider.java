3b054a1d89cb6c6b702fdf8bda0aa2ce289c3128
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2015-09-22 14:36:51 +0300

    cleanup

--- platform/lang-impl/src/com/intellij/ui/JBTreeWithHintProvider.java
+++ platform/lang-impl/src/com/intellij/ui/JBTreeWithHintProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,7 +30,7 @@ import javax.swing.tree.TreeNode;
 /**
  * @author Konstantin Bulenkov
  * @deprecated
- * @see com.intellij.ui.popup.HintUpdateSupply
+ * @see HintUpdateSupply
  */
 public class JBTreeWithHintProvider extends DnDAwareTree {
   {
