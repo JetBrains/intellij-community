1230a1efe6010ad2c264b184f518d6d7c31b0d43
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2015-09-24 18:42:56 +0300

    [git] simplify and fix NPE: use utility method returning boolean instead of overriding onCancel

--- plugins/git4idea/src/git4idea/ui/GitUnstashDialog.java
+++ plugins/git4idea/src/git4idea/ui/GitUnstashDialog.java
@@ -361,21 +361,17 @@ public class GitUnstashDialog extends DialogWrapper {
     AccessToken token = DvcsUtil.workingTreeChangeStarted(myProject);
     try {
       final Ref<GitCommandResult> result = Ref.create();
-      final Ref<Boolean> cancelled = Ref.create();
-      ProgressManager.getInstance().run(new Task.Modal(h.project(), GitBundle.getString("unstash.unstashing"), true) {
+      final ProgressManager progressManager = ProgressManager.getInstance();
+      boolean completed = progressManager.runProcessWithProgressSynchronously(new Runnable() {
         @Override
-        public void run(@NotNull final ProgressIndicator indicator) {
-          indicator.setIndeterminate(true);
-          h.addLineListener(new GitHandlerUtil.GitLineHandlerListenerProgress(indicator, h, "stash", false));
+        public void run() {
+          h.addLineListener(new GitHandlerUtil.GitLineHandlerListenerProgress(progressManager.getProgressIndicator(), h, "stash", false));
           Git git = ServiceManager.getService(Git.class);
           result.set(git.runCommand(new Computable.PredefinedValueComputable<GitLineHandler>(h)));
         }
-        @Override
-        public void onCancel() {
-          cancelled.set(true);
-        }
-      });
-      if (cancelled.get()) return;
+      }, GitBundle.getString("unstash.unstashing"), true, myProject);
+
+      if (!completed) return;
 
       ServiceManager.getService(myProject, GitPlatformFacade.class).hardRefresh(root);
       GitCommandResult res = result.get();
