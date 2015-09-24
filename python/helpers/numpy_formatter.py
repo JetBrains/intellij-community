955263d15247b87631859452944a7dbebaab9a98
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2015-09-24 17:35:49 +0300

    Dump full stacktrace in idea.log for errors during docstring rendering

--- python/helpers/numpy_formatter.py
+++ python/helpers/numpy_formatter.py
@@ -1,19 +1,14 @@
 import sys
-
-from sphinxcontrib.napoleon.docstring import NumpyDocstring
+import textwrap
 
 import rest_formatter
+from sphinxcontrib.napoleon.docstring import NumpyDocstring
 
 
 def main(text=None):
-  try:
     src = sys.stdin.read() if text is None else text
-    import textwrap
     rest_formatter.main(str(NumpyDocstring(textwrap.dedent(src))))
-  except:
-    exc_type, exc_value, exc_traceback = sys.exc_info()
-    sys.stderr.write("Error calculating docstring: " + str(exc_value))
 
 
 if __name__ == '__main__':
-  main()
+    main()
--- python/helpers/rest_formatter.py
+++ python/helpers/rest_formatter.py
@@ -210,34 +210,28 @@ def parse_docstring(docstring, errors, **options):
 
 
 def main(text=None):
-    try:
-        src = sys.stdin.read() if text is None else text
+    src = sys.stdin.read() if text is None else text
 
-        errors = []
+    errors = []
 
-        class EmptyLinker(DocstringLinker):
-            def translate_indexterm(self, indexterm):
-                return ""
+    class EmptyLinker(DocstringLinker):
+        def translate_indexterm(self, indexterm):
+            return ""
 
-            def translate_identifier_xref(self, identifier, label=None):
-                return identifier
+        def translate_identifier_xref(self, identifier, label=None):
+            return identifier
 
-        docstring = parse_docstring(src, errors)
-        html = docstring.to_html(EmptyLinker())
+    docstring = parse_docstring(src, errors)
+    html = docstring.to_html(EmptyLinker())
 
-        if errors and not html:
-            sys.stderr.write("Error parsing docstring:\n")
-            for error in errors:
-                sys.stderr.write(str(error) + "\n")
-            sys.exit(1)
-
-        sys.stdout.write(html)
-        sys.stdout.flush()
-    except:
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        sys.stderr.write("Error calculating docstring: " + str(exc_value))
+    if errors and not html:
+        sys.stderr.write("Error parsing docstring:\n")
+        for error in errors:
+            sys.stderr.write(str(error) + "\n")
         sys.exit(1)
 
+    sys.stdout.write(html)
+    sys.stdout.flush()
 
 if __name__ == '__main__':
     main()
