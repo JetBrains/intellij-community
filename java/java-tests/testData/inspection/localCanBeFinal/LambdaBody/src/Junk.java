b29779ed87c5b6754493a764b2515742cc6538ad
Anna Kozlova <anna.kozlova@jetbrains.com>
2015-09-25 12:39:13 +0300

    local can be final: don't ignore problems found in lambda body (IDEA-145500)

--- java/java-tests/testData/inspection/localCanBeFinal/LambdaBody/src/Junk.java
+++ java/java-tests/testData/inspection/localCanBeFinal/LambdaBody/src/Junk.java
@@ -1,3 +1,13 @@
+import java.util.HashMap;
+
+class Issue {
+  public static void filterMap() {
+    new HashMap<String, String>().forEach((s1, s2) -> {
+      String cc = s1 + s2;
+      System.err.println(cc);
+    });
+  }
+}
 public final class Junk {
   public void sillyMethod() {
     Runnable r = () -> {
