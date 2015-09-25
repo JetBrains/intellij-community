2623ebbd58b39aed468f31f0f439b5ab7287df86
Bas Leijdekkers <basleijdekkers@gmail.com>
2015-09-25 16:09:33 +0200

    IG: cleanup

--- plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/controlflow/SimplifiableConditionalExpressionInspection.java
+++ plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/controlflow/SimplifiableConditionalExpressionInspection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2009 Dave Griffith, Bas Leijdekkers
+ * Copyright 2003-2015 Dave Griffith, Bas Leijdekkers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,6 @@ import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiConditionalExpression;
 import com.intellij.psi.PsiExpression;
 import com.intellij.psi.PsiType;
-import com.intellij.util.IncorrectOperationException;
 import com.siyeh.InspectionGadgetsBundle;
 import com.siyeh.ig.BaseInspection;
 import com.siyeh.ig.BaseInspectionVisitor;
@@ -31,14 +30,12 @@ import com.siyeh.ig.psiutils.ParenthesesUtils;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
-public class SimplifiableConditionalExpressionInspection
-  extends BaseInspection {
+public class SimplifiableConditionalExpressionInspection extends BaseInspection {
 
   @Override
   @NotNull
   public String getDisplayName() {
-    return InspectionGadgetsBundle.message(
-      "simplifiable.conditional.expression.display.name");
+    return InspectionGadgetsBundle.message("simplifiable.conditional.expression.display.name");
   }
 
   @Override
@@ -49,11 +46,9 @@ public class SimplifiableConditionalExpressionInspection
   @Override
   @NotNull
   public String buildErrorString(Object... infos) {
-    final PsiConditionalExpression expression =
-      (PsiConditionalExpression)infos[0];
-    return InspectionGadgetsBundle.message(
-      "simplifiable.conditional.expression.problem.descriptor",
-      calculateReplacementExpression(expression));
+    final PsiConditionalExpression expression = (PsiConditionalExpression)infos[0];
+    return InspectionGadgetsBundle.message("simplifiable.conditional.expression.problem.descriptor",
+                                           calculateReplacementExpression(expression));
   }
 
   @Override
@@ -61,14 +56,12 @@ public class SimplifiableConditionalExpressionInspection
     return new SimplifiableConditionalFix();
   }
 
-  private static class SimplifiableConditionalFix
-    extends InspectionGadgetsFix {
+  private static class SimplifiableConditionalFix extends InspectionGadgetsFix {
 
     @Override
     @NotNull
     public String getName() {
-      return InspectionGadgetsBundle.message(
-        "constant.conditional.expression.simplify.quickfix");
+      return InspectionGadgetsBundle.message("constant.conditional.expression.simplify.quickfix");
     }
 
     @NotNull
@@ -78,12 +71,9 @@ public class SimplifiableConditionalExpressionInspection
     }
 
     @Override
-    public void doFix(Project project, ProblemDescriptor descriptor)
-      throws IncorrectOperationException {
-      final PsiConditionalExpression expression =
-        (PsiConditionalExpression)descriptor.getPsiElement();
-      final String newExpression =
-        calculateReplacementExpression(expression);
+    public void doFix(Project project, ProblemDescriptor descriptor) {
+      final PsiConditionalExpression expression = (PsiConditionalExpression)descriptor.getPsiElement();
+      final String newExpression = calculateReplacementExpression(expression);
       PsiReplacementUtil.replaceExpression(expression, newExpression);
     }
   }
@@ -94,84 +84,41 @@ public class SimplifiableConditionalExpressionInspection
   }
 
   @NonNls
-  static String calculateReplacementExpression(
-    PsiConditionalExpression expression) {
+  static String calculateReplacementExpression(PsiConditionalExpression expression) {
     final PsiExpression thenExpression = expression.getThenExpression();
     final PsiExpression elseExpression = expression.getElseExpression();
     final PsiExpression condition = expression.getCondition();
     assert thenExpression != null;
     assert elseExpression != null;
-
-    final String elseText = elseExpression.getText();
-    String conditionText = condition.getText();
     if (BoolUtils.isTrue(thenExpression)) {
-      @NonNls final String elseExpressionText;
-      if (ParenthesesUtils.getPrecedence(elseExpression) >
-          ParenthesesUtils.OR_PRECEDENCE) {
-        elseExpressionText = '(' + elseText + ')';
-      }
-      else {
-        elseExpressionText = elseText;
-      }
-      if (ParenthesesUtils.getPrecedence(condition) > ParenthesesUtils.OR_PRECEDENCE) {
-        conditionText = "(" + conditionText + ")";
-      }
-      return conditionText + " || " + elseExpressionText;
+      final String elseExpressionText = ParenthesesUtils.getText(elseExpression, ParenthesesUtils.OR_PRECEDENCE);
+      return ParenthesesUtils.getText(condition, ParenthesesUtils.OR_PRECEDENCE) + " || " + elseExpressionText;
     }
     else if (BoolUtils.isFalse(thenExpression)) {
-      @NonNls final String elseExpressionText;
-      if (ParenthesesUtils.getPrecedence(elseExpression) >
-          ParenthesesUtils.AND_PRECEDENCE) {
-        elseExpressionText = '(' + elseText + ')';
-      }
-      else {
-        elseExpressionText = elseText;
-      }
-      return BoolUtils.getNegatedExpressionText(condition) + " && " +
-             elseExpressionText;
+      @NonNls final String elseExpressionText = ParenthesesUtils.getText(elseExpression, ParenthesesUtils.AND_PRECEDENCE);
+      return BoolUtils.getNegatedExpressionText(condition, ParenthesesUtils.AND_PRECEDENCE) + " && " + elseExpressionText;
     }
-    final String thenText = thenExpression.getText();
     if (BoolUtils.isFalse(elseExpression)) {
-      @NonNls final String thenExpressionText;
-      if (ParenthesesUtils.getPrecedence(thenExpression) >
-          ParenthesesUtils.AND_PRECEDENCE) {
-        thenExpressionText = '(' + thenText + ')';
-      }
-      else {
-        thenExpressionText = thenText;
-      }
-      if (ParenthesesUtils.getPrecedence(condition) > ParenthesesUtils.AND_PRECEDENCE) {
-        conditionText = "(" + conditionText + ")";
-      }
-      return conditionText + " && " + thenExpressionText;
+      @NonNls final String thenExpressionText = ParenthesesUtils.getText(thenExpression, ParenthesesUtils.AND_PRECEDENCE);
+      return ParenthesesUtils.getText(condition, ParenthesesUtils.AND_PRECEDENCE) + " && " + thenExpressionText;
     }
     else {
-      @NonNls final String thenExpressionText;
-      if (ParenthesesUtils.getPrecedence(thenExpression) >
-          ParenthesesUtils.OR_PRECEDENCE) {
-        thenExpressionText = '(' + thenText + ')';
-      }
-      else {
-        thenExpressionText = thenText;
-      }
-      return BoolUtils.getNegatedExpressionText(condition) + " || " +
-             thenExpressionText;
+      @NonNls final String thenExpressionText = ParenthesesUtils.getText(thenExpression, ParenthesesUtils.OR_PRECEDENCE);
+      return BoolUtils.getNegatedExpressionText(condition, ParenthesesUtils.OR_PRECEDENCE) + " || " + thenExpressionText;
     }
   }
 
-  private static class SimplifiableConditionalExpressionVisitor
-    extends BaseInspectionVisitor {
+  private static class SimplifiableConditionalExpressionVisitor extends BaseInspectionVisitor {
 
     @Override
-    public void visitConditionalExpression(
-      PsiConditionalExpression expression) {
+    public void visitConditionalExpression(PsiConditionalExpression expression) {
       super.visitConditionalExpression(expression);
       final PsiExpression thenExpression = expression.getThenExpression();
       if (thenExpression == null) {
         return;
       }
       final PsiType thenType = thenExpression.getType();
-      if (thenType != PsiType.BOOLEAN) {
+      if (!PsiType.BOOLEAN.equals(thenType)) {
         return;
       }
       final PsiExpression elseExpression = expression.getElseExpression();
@@ -179,13 +126,11 @@ public class SimplifiableConditionalExpressionInspection
         return;
       }
       final PsiType elseType = elseExpression.getType();
-      if (elseType != PsiType.BOOLEAN) {
+      if (!PsiType.BOOLEAN.equals(elseType)) {
         return;
       }
-      final boolean thenConstant = BoolUtils.isFalse(thenExpression) ||
-                                   BoolUtils.isTrue(thenExpression);
-      final boolean elseConstant = BoolUtils.isFalse(elseExpression) ||
-                                   BoolUtils.isTrue(elseExpression);
+      final boolean thenConstant = BoolUtils.isFalse(thenExpression) || BoolUtils.isTrue(thenExpression);
+      final boolean elseConstant = BoolUtils.isFalse(elseExpression) || BoolUtils.isTrue(elseExpression);
       if (thenConstant == elseConstant) {
         return;
       }
