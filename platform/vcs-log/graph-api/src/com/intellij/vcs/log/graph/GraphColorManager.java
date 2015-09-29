03430454b7356ac967b6d9b025fd6c15c33fab84
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2015-09-29 17:49:20 +0300

    [vcs-log] move GraphColorManager to vcs-log-graph-api

--- platform/vcs-log/graph-api/src/com/intellij/vcs/log/graph/GraphColorManager.java
+++ platform/vcs-log/graph-api/src/com/intellij/vcs/log/graph/GraphColorManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,8 +14,6 @@
  * limitations under the License.
  */
 package com.intellij.vcs.log.graph;
-
-import com.intellij.vcs.log.VcsLogRefManager;
 
 public interface GraphColorManager<CommitId> {
 
