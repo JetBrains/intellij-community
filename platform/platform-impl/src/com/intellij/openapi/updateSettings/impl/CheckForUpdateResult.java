5f0dfe6c7f06c0f77f897bedc2af9463ab70945d
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2015-09-30 17:52:25 +0200

    Cleanup (formatting; dead code)

--- platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/CheckForUpdateResult.java
+++ platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/CheckForUpdateResult.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,10 +45,6 @@ public class CheckForUpdateResult {
     myAllChannelIds = Collections.emptyList();
     myState = state;
     myError = e;
-  }
-
-  public CheckForUpdateResult(@NotNull UpdateStrategy.State state) {
-    this(state, null);
   }
 
   @Nullable
