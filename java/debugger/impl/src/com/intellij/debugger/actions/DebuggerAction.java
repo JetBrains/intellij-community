b90871efc3ccb748a6a7f5ffe907576a5a9013d6
Egor.Ushakov <egor.ushakov@jetbrains.com>
2015-09-30 13:22:04 +0300

    Nullability

--- java/debugger/impl/src/com/intellij/debugger/actions/DebuggerAction.java
+++ java/debugger/impl/src/com/intellij/debugger/actions/DebuggerAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -97,6 +97,7 @@ public abstract class DebuggerAction extends AnAction {
     return nodes.toArray(new DebuggerTreeNodeImpl[nodes.size()]);
   }
 
+  @NotNull
   public static DebuggerContextImpl getDebuggerContext(DataContext dataContext) {
     DebuggerTreePanel panel = getPanel(dataContext);
     if(panel != null) {
