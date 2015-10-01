36a3029585fc8c7c8e36ef623223657bc46a1c29
Egor.Ushakov <egor.ushakov@jetbrains.com>
2015-10-01 15:30:52 +0300

    remove alpha in tooltips fill color

--- platform/platform-api/src/com/intellij/openapi/ui/MessageType.java
+++ platform/platform-api/src/com/intellij/openapi/ui/MessageType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,13 +26,13 @@ import java.awt.*;
 public class MessageType {
 
   public static final MessageType ERROR = new MessageType(AllIcons.General.NotificationError,
-                                                          new JBColor(new Color(255, 204, 204, 230), new Color(112, 71, 69)),
+                                                          new JBColor(new Color(255, 204, 204), new Color(112, 71, 69)),
                                                           new JBColor(new Color(0xAC0013), new Color(0xEF5F65)));
   public static final MessageType INFO = new MessageType(AllIcons.General.NotificationInfo,
-                                                         new JBColor(new Color(186, 238, 186, 230), new Color(73, 117, 73)),
+                                                         new JBColor(new Color(186, 238, 186), new Color(73, 117, 73)),
                                                          new JBColor(new Color(0x000000), new Color(0xbbbbbb)));
   public static final MessageType WARNING = new MessageType(AllIcons.General.NotificationWarning,
-                                                            new JBColor(new Color(249, 247, 142, 230), new Color(90, 82, 33)),
+                                                            new JBColor(new Color(249, 247, 142), new Color(90, 82, 33)),
                                                             new JBColor(new Color(164, 145, 82), new Color(0xBBB529)));
 
   private final Icon myDefaultIcon;
