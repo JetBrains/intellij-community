809b331a0defdd410c644a3e26c8be6565cb42ea
Anna Kozlova <anna.kozlova@jetbrains.com>
2015-10-01 21:18:00 +0200

    overload resolution: provide type parameter bounds with site substitutor to correctly compare types during most specific check (IDEA-144463)
    (cherry picked from commit 197110ff95f1b3528778c0fd5567fd92c69c3b92)

--- java/java-psi-impl/src/com/intellij/psi/impl/source/resolve/graphInference/constraints/StrictSubtypingConstraint.java
+++ java/java-psi-impl/src/com/intellij/psi/impl/source/resolve/graphInference/constraints/StrictSubtypingConstraint.java
@@ -164,6 +164,6 @@ public class StrictSubtypingConstraint implements ConstraintFormula {
 
   @Override
   public String toString() {
-    return myS.getPresentableText() + " < " + myT.getPresentableText();
+    return myT.getPresentableText() + " < " + myS.getPresentableText();
   }
 }
