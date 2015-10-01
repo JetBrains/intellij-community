52a43235ddc22c6583b29a899409421bef5fef89
Gauthier POGAM--LE MONTAGNER <gauthier.pogam-lemontagner@clever-cloud.com>
2015-10-01 14:26:04 +0200

    Remove uname based check

--- native/fsNotifier/linux/make.sh
+++ native/fsNotifier/linux/make.sh
@@ -2,7 +2,7 @@
 CC_FLAGS="-O3 -Wall -std=c99 -D_BSD_SOURCE -D_DEFAULT_SOURCE -D_XOPEN_SOURCE=500"
 ARCH=`uname -m`
 
-if [ "$ARCH" = "i686" -o -f "/usr/include/gnu/stubs-32.h" ] ; then
+if [ -f "/usr/include/gnu/stubs-32.h" ] ; then
   echo "compiling 32-bit version"
   clang -m32 $CC_FLAGS -o fsnotifier main.c inotify.c util.c
   if [ $? -eq 0 ] ; then
@@ -10,7 +10,7 @@ if [ "$ARCH" = "i686" -o -f "/usr/include/gnu/stubs-32.h" ] ; then
   fi
 fi
 
-if [ "$ARCH" = "x86_64" -o -f "/usr/include/gnu/stubs-64.h" ] ; then
+if [ -f "/usr/include/gnu/stubs-64.h" ] ; then
   echo "compiling 64-bit version"
   clang -m64 $CC_FLAGS -o fsnotifier64 main.c inotify.c util.c
   if [ $? -eq 0 ] ; then
