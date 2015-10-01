2471dcf0b2687b0cca87b503d31449bd70f13cbc
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2015-10-01 16:19:58 +0200

    Cleanup (fsnotifier compile script)

--- native/fsNotifier/linux/make.sh
+++ native/fsNotifier/linux/make.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
+
 CC_FLAGS="-O3 -Wall -std=c99 -D_DEFAULT_SOURCE"
 
 if [ -f "/usr/include/gnu/stubs-32.h" ] ; then
   echo "compiling 32-bit version"
-  clang -m32 $CC_FLAGS -o fsnotifier main.c inotify.c util.c
-  if [ $? -eq 0 ] ; then
-    chmod 755 fsnotifier
-  fi
+  clang -m32 ${CC_FLAGS} -o fsnotifier main.c inotify.c util.c && chmod 755 fsnotifier
 fi
 
 if [ -f "/usr/include/gnu/stubs-64.h" ] ; then
   echo "compiling 64-bit version"
-  clang -m64 $CC_FLAGS -o fsnotifier64 main.c inotify.c util.c
-  if [ $? -eq 0 ] ; then
-    chmod 755 fsnotifier64
-  fi
+  clang -m64 ${CC_FLAGS} -o fsnotifier64 main.c inotify.c util.c && chmod 755 fsnotifier64
 fi
