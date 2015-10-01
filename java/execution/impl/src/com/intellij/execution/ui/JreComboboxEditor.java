5fd7c832682f1e0bafd6c93b0e94ad59a410d41d
nik <Nikolay.Chashnikov@jetbrains.com>
2015-10-01 13:21:40 +0300

    corrected file chooser behaviour for 'JRE' path in run configurations

--- java/execution/impl/src/com/intellij/execution/ui/JreComboboxEditor.java
+++ java/execution/impl/src/com/intellij/execution/ui/JreComboboxEditor.java
@@ -15,10 +15,12 @@
  */
 package com.intellij.execution.ui;
 
+import com.intellij.openapi.ui.TextComponentAccessor;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.ui.SortedComboBoxModel;
 import com.intellij.ui.components.JBTextField;
 import com.intellij.util.ui.StatusText;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import javax.swing.plaf.basic.BasicComboBoxEditor;
@@ -27,6 +29,7 @@ import javax.swing.plaf.basic.BasicComboBoxEditor;
  * @author nik
  */
 class JreComboboxEditor extends BasicComboBoxEditor {
+  public static final TextComponentAccessor<JComboBox> TEXT_COMPONENT_ACCESSOR = new JreComboBoxTextComponentAccessor();
   private final SortedComboBoxModel<JrePathEditor.JreComboBoxItem> myComboBoxModel;
 
   public JreComboboxEditor(SortedComboBoxModel<JrePathEditor.JreComboBoxItem> comboBoxModel) {
@@ -60,5 +63,18 @@ class JreComboboxEditor extends BasicComboBoxEditor {
 
   public JBTextField getEditorComponent() {
     return (JBTextField)super.getEditorComponent();
+  }
+
+  private static class JreComboBoxTextComponentAccessor implements TextComponentAccessor<JComboBox> {
+    @Override
+    public String getText(JComboBox component) {
+      Object item = component.getEditor().getItem();
+      return item != null ? ((JrePathEditor.JreComboBoxItem)item).getPresentableText() : "";
+    }
+
+    @Override
+    public void setText(JComboBox component, @NotNull String text) {
+      component.getEditor().setItem(new JrePathEditor.CustomJreItem(FileUtil.toSystemIndependentName(text)));
+    }
   }
 }
