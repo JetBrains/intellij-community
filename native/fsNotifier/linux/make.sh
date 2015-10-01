af7eea961926e6c9f3930e1af4dec9268a4faefc
Gauthier POGAM--LE MONTAGNER <gauthier.pogam-lemontagner@clever-cloud.com>
2015-10-01 14:23:27 +0200

    Improve cross-compilation

--- native/fsNotifier/linux/make.sh
+++ native/fsNotifier/linux/make.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
-CC_FLAGS="-O3 -Wall -std=c99 -D_BSD_SOURCE -D_XOPEN_SOURCE=500"
-echo "compiling 32-bit version"
-clang -m32 $CC_FLAGS -o fsnotifier main.c inotify.c util.c
-if [ $? -eq 0 ] ; then
+CC_FLAGS="-O3 -Wall -std=c99 -D_BSD_SOURCE -D_DEFAULT_SOURCE -D_XOPEN_SOURCE=500"
+ARCH=`uname -m`
+
+if [ "$ARCH" = "i686" -o -f "/usr/include/gnu/stubs-32.h" ] ; then
+  echo "compiling 32-bit version"
+  clang -m32 $CC_FLAGS -o fsnotifier main.c inotify.c util.c
+  if [ $? -eq 0 ] ; then
+    chmod 755 fsnotifier
+  fi
+fi
+
+if [ "$ARCH" = "x86_64" -o -f "/usr/include/gnu/stubs-64.h" ] ; then
   echo "compiling 64-bit version"
   clang -m64 $CC_FLAGS -o fsnotifier64 main.c inotify.c util.c
   if [ $? -eq 0 ] ; then
-    chmod 755 fsnotifier fsnotifier64
+    chmod 755 fsnotifier64
   fi
 fi
