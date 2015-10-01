86d321b29aca7ae9a47337e51cfce7bab2afbda7
Konstantin Bulenkov <kb@jetbrains.com>
2015-10-02 01:47:12 +0200

    Mac Messages: make do not ask checkbox transparent
    (cherry picked from commit 9a6609205076e2ae5704cf98b159d8093175753d)

--- platform/platform-impl/src/com/intellij/ui/messages/SheetController.java
+++ platform/platform-impl/src/com/intellij/ui/messages/SheetController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -441,7 +441,7 @@ public class SheetController {
   private void layoutDoNotAskCheckbox(JPanel sheetPanel) {
     doNotAskCheckBox.setText(myDoNotAskOption.getDoNotShowMessage());
     doNotAskCheckBox.setSelected(!myDoNotAskOption.isToBeShown());
-
+    doNotAskCheckBox.setOpaque(false);
     doNotAskCheckBox.addItemListener(new ItemListener() {
       @Override
       public void itemStateChanged(@NotNull ItemEvent e) {
