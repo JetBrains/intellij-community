c2d21df9268d3ac658a6054e5a6f18d85fe973c9
Anna Kozlova <anna.kozlova@jetbrains.com>
2015-10-02 21:30:07 +0200

    testdata for IDEA-136708
    (cherry picked from commit 010dd7c48db14d305dfcefcac54ac7bc198bfb11)

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewMethodRefHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewMethodRefHighlightingTest.java
@@ -438,6 +438,10 @@ public class NewMethodRefHighlightingTest extends LightDaemonAnalyzerTestCase {
     doTest();
   }
 
+  public void testIDEA136708() throws Exception {
+    doTest();
+  }
+
   private void doTest() {
     doTest(false);
   }
