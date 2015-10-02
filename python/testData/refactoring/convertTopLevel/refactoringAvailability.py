62cdf312fe905d2b06f818e0ef6653d21f9522c7
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2015-10-02 19:06:53 +0300

    PY-6637 Don't show refactoring for all sorts of special methods
    (cherry picked from commit 7b6777862960cecaf2d20ef03e64d7fdfe0e9239)

--- python/testData/refactoring/convertTopLevel/refactoringAvailability.py
+++ python/testData/refactoring/convertTopLevel/refactoringAvailability.py
@@ -1,16 +1,33 @@
-def fu<caret>nc():
+def func():
     def local():
         pass
-    
+
 
 class C:
     def method(self):
         pass
-    
+
     @staticmethod
     def static_method(x):
         pass
-    
+
     @classmethod
     def class_method(self):
         pass
+
+    @property
+    def field(self):
+        return self._x
+
+    def __magic__(self):
+        pass
+
+
+class Base:
+    def overridden_method(self):
+        pass
+
+
+class Subclass(Base):
+    def overridden_method(self):
+        super(Subclass, self).overridden_method()
