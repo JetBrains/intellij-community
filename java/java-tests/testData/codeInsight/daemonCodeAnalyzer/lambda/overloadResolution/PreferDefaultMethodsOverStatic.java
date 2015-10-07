c0006247046a1c9516459b042b45c866ee3dcaaf
Anna Kozlova <anna.kozlova@jetbrains.com>
2015-10-07 18:58:49 +0200

    overload resolution: treat default methods as abstract; don't include static interface methods in conflict resolution when target expression type does not correspond to the containing interface (IDEA-146055)

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/lambda/overloadResolution/PreferDefaultMethodsOverStatic.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/lambda/overloadResolution/PreferDefaultMethodsOverStatic.java
@@ -13,6 +13,33 @@ class Test {
   public static void main(String[] args) {
     new IJ(). f();
     new JI(). f();
+
+    IJ.<error descr="Non-static method 'f()' cannot be referenced from a static context">f</error>();
+    JI.<error descr="Non-static method 'f()' cannot be referenced from a static context">f</error>();
+
     J.f();
   }
+}
+
+class Test2 {
+  interface I {
+    static void f(String <warning descr="Parameter 's' is never used">s</warning>) {}
+  }
+
+  interface J<T> {
+    default void f(T <warning descr="Parameter 't' is never used">t</warning>) {}
+
+    //another pair
+    default void j(T <warning descr="Parameter 't' is never used">t</warning>) {}
+    static  void j(String <warning descr="Parameter 's' is never used">s</warning>) {};
+  }
+
+  static class IJ implements I, J<String> {}
+
+  public static void main(IJ s, J<String> j) {
+    s.f("");
+
+    <error descr="Static method may be invoked on containing interface class only">j.j("");</error>
+  }
+
 }
