0f79d59da247ecfe7a509446dd892f64f8e811f7
Alexey Kudravtsev <cdr@intellij.com>
2015-10-09 15:20:49 +0300

    compilation fixed

--- platform/platform-api/src/com/intellij/ui/tabs/impl/TabLabel.java
+++ platform/platform-api/src/com/intellij/ui/tabs/impl/TabLabel.java
@@ -32,6 +32,7 @@ import com.intellij.ui.tabs.impl.table.TableLayout;
 import com.intellij.util.PairConsumer;
 import com.intellij.util.ui.Centerizer;
 import com.intellij.util.ui.UIUtil;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
