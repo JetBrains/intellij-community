8208db6950a304bb0027865bb7112e4edd5f485e
irengrig <Irina.Chernushina@jetbrains.com>
2015-10-09 12:19:56 +0200

    startup tasks: fix bug when same-named run configurations with different types were not remembered (cherry picked from commit 7d8e394)

--- platform/lang-impl/src/com/intellij/execution/startup/ProjectStartupTaskManager.java
+++ platform/lang-impl/src/com/intellij/execution/startup/ProjectStartupTaskManager.java
@@ -18,6 +18,7 @@ package com.intellij.execution.startup;
 import com.intellij.execution.RunManager;
 import com.intellij.execution.RunManagerEx;
 import com.intellij.execution.RunnerAndConfigurationSettings;
+import com.intellij.execution.impl.RunManagerImpl;
 import com.intellij.notification.NotificationGroup;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.Project;
@@ -84,8 +85,8 @@ public class ProjectStartupTaskManager {
     final List<RunnerAndConfigurationSettings> result = new ArrayList<RunnerAndConfigurationSettings>();
     final List<ProjectStartupConfigurationBase.ConfigurationDescriptor> list = configuration.getList();
     for (ProjectStartupConfigurationBase.ConfigurationDescriptor descriptor : list) {
-      final RunnerAndConfigurationSettings settings = myRunManager.findConfigurationByName(descriptor.getName());
-      if (settings != null && settings.getUniqueID().equals(descriptor.getId())) {
+      final RunnerAndConfigurationSettings settings = ((RunManagerImpl) myRunManager).getConfigurationById(descriptor.getId());
+      if (settings != null && settings.getName().equals(descriptor.getName())) {
         result.add(settings);
       } else {
         NOTIFICATION_GROUP.createNotification(PREFIX + " Run Configuration '" + descriptor.getName() + "' not found, removed from list.",
