d818956cacfceea0c9d081892bfd30417c28996a
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2015-10-09 20:45:18 +0300

    allows to show other windows above this popup

--- platform/platform-impl/src/com/intellij/ide/RecentProjectsManagerBase.java
+++ platform/platform-impl/src/com/intellij/ide/RecentProjectsManagerBase.java
@@ -413,6 +413,7 @@ public abstract class RecentProjectsManagerBase extends RecentProjectsManager im
             .setTitle("Recent Projects")
             .setFocusable(true)
             .setRequestFocus(true)
+            .setMayBeParent(true)
             .setMovable(true)
             .createPopup();
           Disposer.register(popup, disposable);
