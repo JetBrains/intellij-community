56e0f1b54de5a2308d6da66b6d50756ae90c0ede
Anna Kozlova <anna.kozlova@jetbrains.com>
2015-10-12 13:46:51 +0200

    ig: extract common code for nested statements inspections (IDEA-146258)
    (cherry picked from commit 68f1c89a1dab1503bb5424fb176b2eb54edfa357)

--- plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/controlflow/NestedSwitchStatementInspection.java
+++ plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/controlflow/NestedSwitchStatementInspection.java
@@ -15,13 +15,11 @@
  */
 package com.siyeh.ig.controlflow;
 
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiMethod;
 import com.intellij.psi.PsiSwitchStatement;
-import com.intellij.psi.util.PsiTreeUtil;
 import com.siyeh.InspectionGadgetsBundle;
 import com.siyeh.ig.BaseInspection;
 import com.siyeh.ig.BaseInspectionVisitor;
+import com.siyeh.ig.threading.NestedSynchronizedStatementInspection;
 import org.jetbrains.annotations.NotNull;
 
 public class NestedSwitchStatementInspection extends BaseInspection {
@@ -45,30 +43,14 @@ public class NestedSwitchStatementInspection extends BaseInspection {
     return new NestedSwitchStatementVisitor();
   }
 
-  private static class NestedSwitchStatementVisitor
-    extends BaseInspectionVisitor {
+  private static class NestedSwitchStatementVisitor extends BaseInspectionVisitor {
 
     @Override
-    public void visitSwitchStatement(
-      @NotNull PsiSwitchStatement statement) {
+    public void visitSwitchStatement(@NotNull PsiSwitchStatement statement) {
       super.visitSwitchStatement(statement);
-      final PsiElement containingSwitchStatement =
-        PsiTreeUtil.getParentOfType(statement,
-                                    PsiSwitchStatement.class);
-      if (containingSwitchStatement == null) {
-        return;
+      if (NestedSynchronizedStatementInspection.isNestedStatement(statement, PsiSwitchStatement.class)) {
+        registerStatementError(statement);
       }
-      final PsiMethod containingMethod =
-        PsiTreeUtil.getParentOfType(statement, PsiMethod.class);
-      final PsiMethod containingContainingMethod =
-        PsiTreeUtil.getParentOfType(containingSwitchStatement,
-                                    PsiMethod.class);
-      if (containingMethod == null ||
-          containingContainingMethod == null ||
-          !containingMethod.equals(containingContainingMethod)) {
-        return;
-      }
-      registerStatementError(statement);
     }
   }
 }
