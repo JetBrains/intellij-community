e7bb0a1f177742dc27561da44de3a3dab099df2e
Alexey Kudravtsev <cdr@intellij.com>
2015-10-12 14:35:34 +0300

    clear temp dirs after test

--- platform/platform-tests/testSrc/com/intellij/util/io/PersistenceStressTest.java
+++ platform/platform-tests/testSrc/com/intellij/util/io/PersistenceStressTest.java
@@ -15,7 +15,6 @@
  */
 package com.intellij.util.io;
 
-import com.intellij.ide.caches.FileContent;
 import com.intellij.openapi.progress.EmptyProgressIndicator;
 import com.intellij.openapi.project.CacheUpdateRunner;
 import com.intellij.openapi.util.Condition;
@@ -25,7 +24,6 @@ import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
-import com.intellij.util.Consumer;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.indexing.FileBasedIndex;
 import com.intellij.util.indexing.FileBasedIndexImpl;
@@ -39,23 +37,21 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Dmitry Avdeev
  */
 public class PersistenceStressTest extends LightPlatformCodeInsightFixtureTestCase {
-
-  public static final Condition<Future<Boolean>> CONDITION = new Condition<Future<Boolean>>() {
-    @Override
-    public boolean value(Future<Boolean> future) {
-      return !future.isDone();
-    }
-  };
+  private static final Condition<Future<Boolean>> STILL_RUNNING = future -> !future.isDone();
   private ExecutorService myThreadPool;
-  private final List<PersistentHashMap<String, Record>> myMaps = new ArrayList<PersistentHashMap<String, Record>>();
-  private final List<String> myKeys = new ArrayList<String>();
+  private final List<PersistentHashMap<String, Record>> myMaps = new ArrayList<>();
+  private final List<String> myKeys = new ArrayList<>();
   private PersistentStringEnumerator myEnumerator;
+  private File myTempDirectory;
 
   public static class Record {
     public final int magnitude;
@@ -67,26 +63,56 @@ public class PersistenceStressTest extends LightPlatformCodeInsightFixtureTestCa
     }
   }
 
-  public void testReadWrite() throws Exception {
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    Random random = new Random();
+    for (int i = 0; i < 100; i++) {
+      byte[] bytes = new byte[1000];
+      random.nextBytes(bytes);
+      String key = new String(bytes, CharsetToolkit.UTF8_CHARSET);
+      myKeys.add(key);
+    }
+    myTempDirectory = FileUtil.createTempDirectory("map", "");
 
-    List<Future<Boolean>> futures = new ArrayList<Future<Boolean>>();
+    for (int i = 0; i < 10; i++) {
+      PersistentHashMap<String, Record> map = createMap(FileUtil.createTempFile(myTempDirectory, "persistent", "map" + i));
+      myMaps.add(map);
+    }
+    PagedFileStorage.StorageLockContext storageLockContext = new PagedFileStorage.StorageLockContext(false);
+    myEnumerator = new PersistentStringEnumerator(FileUtil.createTempFile(myTempDirectory, "persistent", "enum"), storageLockContext);
+
+    myThreadPool = Executors.newFixedThreadPool(myMaps.size() + 2);
+  }
+
+  @Override
+  public void tearDown() throws Exception {
+    super.tearDown();
+    for (PersistentHashMap<String, Record> map : myMaps) {
+      map.close();
+    }
+    myEnumerator.close();
+    myThreadPool.shutdown();
+    assertTrue(myThreadPool.awaitTermination(100, TimeUnit.SECONDS));
+    FileUtil.delete(myTempDirectory);
+  }
+
+  public void testReadWrite() throws Exception {
+    List<Future<Boolean>> futures = new ArrayList<>();
     for (PersistentHashMap<String, Record> map : myMaps) {
       Future<Boolean> submit = submit(map);
       futures.add(submit);
     }
-    myThreadPool.submit(new Runnable() {
-      @Override
-      public void run() {
-        try {
-          while (ContainerUtil.find(futures, CONDITION) != null) {
-            Thread.sleep(100);
-            for (PersistentHashMap<String, Record> map : myMaps) {
-              map.dropMemoryCaches();
-            }
+    myThreadPool.submit((Runnable)() -> {
+      try {
+        while (ContainerUtil.find(futures, STILL_RUNNING) != null) {
+          Thread.sleep(100);
+          for (PersistentHashMap<String, Record> map : myMaps) {
+            map.dropMemoryCaches();
           }
         }
-        catch (InterruptedException ignore) {
-        }
+      }
+      catch (InterruptedException ignore) {
       }
     });
 
@@ -100,14 +126,10 @@ public class PersistenceStressTest extends LightPlatformCodeInsightFixtureTestCa
     }
 
     FileBasedIndexImpl index = (FileBasedIndexImpl)FileBasedIndex.getInstance();
-    while (ContainerUtil.find(futures, CONDITION) != null) {
+    while (ContainerUtil.find(futures, STILL_RUNNING) != null) {
       Thread.sleep(100);
-      CacheUpdateRunner.processFiles(new EmptyProgressIndicator(), true, files, getProject(), new Consumer<FileContent>() {
-        @Override
-        public void consume(FileContent content) {
-          index.indexFileContent(getProject(), content);
-        }
-      });
+      CacheUpdateRunner.processFiles(new EmptyProgressIndicator(), true, files, getProject(),
+                                     content -> index.indexFileContent(getProject(), content));
     }
     for (Future<Boolean> future : futures) {
       assertTrue(future.get());
@@ -115,22 +137,19 @@ public class PersistenceStressTest extends LightPlatformCodeInsightFixtureTestCa
   }
 
   @NotNull
-  protected Future<Boolean> submit(final PersistentHashMap<String, Record> map) {
-    return myThreadPool.submit(new Callable<Boolean>() {
-      @Override
-      public Boolean call() throws Exception {
-        try {
-          return doTask(map);
-        }
-        catch (IOException e) {
-          e.printStackTrace();
-          return false;
-        }
+  private Future<Boolean> submit(final PersistentHashMap<String, Record> map) {
+    return myThreadPool.submit(() -> {
+      try {
+        return doTask(map);
+      }
+      catch (IOException e) {
+        e.printStackTrace();
+        return false;
       }
     });
   }
 
-  protected boolean doTask(PersistentHashMap<String, Record> map) throws IOException {
+  private boolean doTask(PersistentHashMap<String, Record> map) throws IOException {
     Random random = new Random();
     try {
       for (int i = 0; i < 100000; i++) {
@@ -160,40 +179,9 @@ public class PersistenceStressTest extends LightPlatformCodeInsightFixtureTestCa
     }
   }
 
-  @Override
-  protected void setUp() throws Exception {
-    super.setUp();
-    Random random = new Random();
-    for (int i = 0; i < 1000; i++) {
-      byte[] bytes = new byte[1000];
-      random.nextBytes(bytes);
-      String key = new String(bytes, CharsetToolkit.UTF8_CHARSET);
-      myKeys.add(key);
-    }
-    for (int i = 0; i < 5; i++) {
-      PersistentHashMap<String, Record> map = createMap(FileUtil.createTempFile("persistent", "map" + i));
-      myMaps.add(map);
-    }
-    PagedFileStorage.StorageLockContext storageLockContext = new PagedFileStorage.StorageLockContext(false);
-    myEnumerator = new PersistentStringEnumerator(FileUtil.createTempFile("persistent", "enum"), storageLockContext);
-
-    myThreadPool = Executors.newFixedThreadPool(myMaps.size() + 2);
-  }
-
-  @Override
-  public void tearDown() throws Exception {
-    super.tearDown();
-    for (PersistentHashMap<String, Record> map : myMaps) {
-      map.close();
-    }
-    myEnumerator.close();
-    myThreadPool.shutdown();
-    assertTrue(myThreadPool.awaitTermination(100, TimeUnit.SECONDS));
-  }
-
   @NotNull
   private static PersistentHashMap<String, Record> createMap(File file) throws IOException {
-    return new PersistentHashMap<String, Record>(file, new EnumeratorStringDescriptor(), new DataExternalizer<Record>() {
+    return new PersistentHashMap<>(file, new EnumeratorStringDescriptor(), new DataExternalizer<Record>() {
       @Override
       public void save(@NotNull DataOutput out, Record value) throws IOException {
         out.writeInt(value.magnitude);
