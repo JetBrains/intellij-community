b355126ededa9a94ad19ca4de13063f52ca6de8b
Konstantin Bulenkov <kb@jetbrains.com>
2015-10-13 17:25:57 +0200

    IDEA-145975 Exception appears when first  run IDEA (UI Themes page)

--- platform/editor-ui-api/src/com/intellij/ide/ui/AntialiasingType.java
+++ platform/editor-ui-api/src/com/intellij/ide/ui/AntialiasingType.java
@@ -15,6 +15,7 @@
  */
 package com.intellij.ide.ui;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.util.ui.UIUtil;
 import sun.swing.SwingUtilities2;
 
@@ -36,9 +37,11 @@ public enum AntialiasingType {
   }
 
   public static Object getAAHintForSwingComponent() {
-    UISettings uiSettings = UISettings.getInstance();
+    if (ApplicationManager.getApplication() == null) {
+      return getAAEnabledTextInfo();
+    }
 
-    if (uiSettings == null) return getAAEnabledTextInfo();
+    UISettings uiSettings = UISettings.getInstance();
 
     switch (uiSettings.IDE_AA_TYPE) {
       case SUBPIXEL:
