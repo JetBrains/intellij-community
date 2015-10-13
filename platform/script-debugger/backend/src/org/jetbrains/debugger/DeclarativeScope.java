8860ca45eb2ebf7bdda64b9a57684edafedc40cc
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2015-10-13 17:27:58 +0200

    Convert to kotlin (cherry picked from commit c7bb3c7)

--- platform/script-debugger/backend/src/org/jetbrains/debugger/DeclarativeScope.java
+++ platform/script-debugger/backend/src/org/jetbrains/debugger/DeclarativeScope.java
@@ -1,38 +1,39 @@
-package org.jetbrains.debugger;
+/*
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jetbrains.debugger
 
-import com.intellij.util.Consumer;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.concurrency.Promise;
-import org.jetbrains.debugger.values.ObjectValue;
-import org.jetbrains.debugger.values.ValueManager;
+import com.intellij.util.Consumer
+import org.jetbrains.concurrency.Promise
+import org.jetbrains.debugger.values.ObjectValue
+import org.jetbrains.debugger.values.ValueManager
 
-import java.util.List;
+abstract class DeclarativeScope<VALUE_MANAGER : ValueManager<out Vm>>(type: Scope.Type, description: String? = null) : ScopeBase(type, description) {
+  protected abstract val childrenManager: VariablesHost<VALUE_MANAGER>
 
-public abstract class DeclarativeScope<VALUE_MANAGER extends ValueManager> extends ScopeBase {
-  protected VariablesHost<VALUE_MANAGER> childrenManager;
-
-  protected DeclarativeScope(@NotNull Type type, @Nullable String description) {
-    super(type, description);
-  }
-
-  @NotNull
-  protected final Promise<List<Variable>> loadScopeObjectProperties(@NotNull ObjectValue value) {
-    if (childrenManager.valueManager.isObsolete()) {
-      return ValueManager.reject();
+  protected fun loadScopeObjectProperties(value: ObjectValue): Promise<List<Variable>> {
+    if (childrenManager.valueManager.isObsolete) {
+      return ValueManager.reject()
     }
 
-    return value.getProperties().done(new Consumer<List<Variable>>() {
-      @Override
-      public void consume(List<Variable> variables) {
-        childrenManager.updateCacheStamp();
+    return value.properties.done(object : Consumer<List<Variable>> {
+      override fun consume(variables: List<Variable>) {
+        childrenManager.updateCacheStamp()
       }
-    });
+    })
   }
 
-  @NotNull
-  @Override
-  public final VariablesHost getVariablesHost() {
-    return childrenManager;
-  }
+  override fun getVariablesHost() = childrenManager
 }
