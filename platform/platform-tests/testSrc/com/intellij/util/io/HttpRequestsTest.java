d57ad1c9e20bc289d951dbe0cfe4e007056c8f28
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2015-10-13 12:37:18 +0200

    [tests] HttpRequest test migrated to local connections

--- platform/platform-tests/testSrc/com/intellij/util/io/HttpRequestsTest.java
+++ platform/platform-tests/testSrc/com/intellij/util/io/HttpRequestsTest.java
@@ -16,40 +16,40 @@
 package com.intellij.util.io;
 
 import com.intellij.ide.IdeBundle;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.util.TimeoutUtil;
+import com.sun.net.httpserver.HttpServer;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.net.Inet4Address;
-import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.SocketTimeoutException;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.junit.Assert.*;
-import static org.junit.Assume.assumeThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class HttpRequestsTest  {
-  private final HttpRequests.RequestProcessor<Void> myProcessor = new HttpRequests.RequestProcessor<Void>() {
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    @Override
-    public Void process(@NotNull HttpRequests.Request request) throws IOException {
-      request.getInputStream().read();
-      return null;
-    }
-  };
+  private static final String LOCALHOST = "127.0.0.1";
+
+  private HttpServer myServer;
 
   @Before
-  public void setUp() throws Exception {
-    InetAddress addr = InetAddress.getByName("openjdk.java.net");
-    assumeThat(addr, instanceOf(Inet4Address.class));
+  public void setUp() throws IOException {
+    myServer = HttpServer.create();
+    myServer.bind(new InetSocketAddress(LOCALHOST, 0), 1);
+    myServer.start();
+  }
+
+  @After
+  public void tearDown() {
+    myServer.stop(0);
   }
 
   @Test
-  public void testLimit() {
+  public void testRedirectLimit() {
     try {
-      HttpRequests.request("").redirectLimit(0).connect(myProcessor);
+      HttpRequests.request("").redirectLimit(0).readString(null);
       fail();
     }
     catch (IOException e) {
@@ -58,19 +58,29 @@ public class HttpRequestsTest  {
   }
 
   @Test(timeout = 5000, expected = SocketTimeoutException.class)
-  public void testConnectTimeout() throws IOException {
-    HttpRequests.request("http://openjdk.java.net").connectTimeout(1).connect(myProcessor);
-    fail();
-  }
-
-  @Test(timeout = 5000, expected = SocketTimeoutException.class)
   public void testReadTimeout() throws IOException {
-    HttpRequests.request("http://openjdk.java.net").readTimeout(1).connect(myProcessor);
+    myServer.createContext("/", ex -> {
+      TimeoutUtil.sleep(1000);
+      ex.sendResponseHeaders(200, 0);
+      ex.close();
+    });
+
+    String url = "http://" + LOCALHOST + ":" + myServer.getAddress().getPort();
+    HttpRequests.request(url).readTimeout(50).readString(null);
     fail();
   }
 
   @Test(timeout = 5000)
-  public void testReadString() throws IOException {
-    assertThat(HttpRequests.request("http://openjdk.java.net").readString(null), containsString("Download"));
+  public void testDataRead() throws IOException {
+    myServer.createContext("/", ex -> {
+      ex.getResponseHeaders().add("Content-Type", "text/plain");
+      ex.sendResponseHeaders(200, 0);
+      ex.getResponseBody().write("hello".getBytes("US-ASCII"));
+      ex.close();
+    });
+
+    String url = "http://" + LOCALHOST + ":" + myServer.getAddress().getPort();
+    String content = HttpRequests.request(url).readString(null);
+    assertEquals("hello", content);
   }
 }
