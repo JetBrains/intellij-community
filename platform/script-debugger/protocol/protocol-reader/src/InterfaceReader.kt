c87694fe467e56db000fed523833c1059f41e906
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2015-10-13 16:32:48 +0200

    WEB-15645 Debug: Grunt: com.google.gson.JsonParseException on Step Into

--- platform/script-debugger/protocol/protocol-reader/src/InterfaceReader.kt
+++ platform/script-debugger/protocol/protocol-reader/src/InterfaceReader.kt
@@ -10,11 +10,10 @@ import java.lang.reflect.Method
 import java.lang.reflect.ParameterizedType
 import java.lang.reflect.Type
 import java.lang.reflect.WildcardType
-import java.util.ArrayList
-import java.util.LinkedHashMap
+import java.util.*
 
-fun InterfaceReader(protocolInterfaces: Array<Class<*>>): InterfaceReader {
-  val map = LinkedHashMap<Class<*>, TypeWriter<*>>(protocolInterfaces.size())
+fun InterfaceReader(protocolInterfaces: List<Class<*>>): InterfaceReader {
+  val map = LinkedHashMap<Class<*>, TypeWriter<*>?>(protocolInterfaces.size())
   for (typeClass in protocolInterfaces) {
     map.put(typeClass, null)
   }
@@ -56,23 +55,19 @@ val VOID_PARSER: ValueReader = object : ValueReader() {
   }
 }
 
-fun createHandler(typeToTypeHandler: LinkedHashMap<Class<*>, TypeWriter<*>>, aClass: Class<*>): TypeWriter<*> {
+fun createHandler(typeToTypeHandler: LinkedHashMap<Class<*>, TypeWriter<*>?>, aClass: Class<*>): TypeWriter<*> {
   val reader = InterfaceReader(typeToTypeHandler)
   reader.processed.addAll(typeToTypeHandler.keySet())
   reader.go(arrayOf(aClass))
-  return typeToTypeHandler.get(aClass)
+  return typeToTypeHandler.get(aClass)!!
 }
 
-class InterfaceReader(val typeToTypeHandler: LinkedHashMap<Class<*>, TypeWriter<*>>) {
+class InterfaceReader(val typeToTypeHandler: LinkedHashMap<Class<*>, TypeWriter<*>?>) {
   val processed = THashSet<Class<*>>()
   private val refs = ArrayList<TypeRef<*>>()
   val subtypeCasters = ArrayList<SubtypeCaster>()
 
-  fun go(): LinkedHashMap<Class<*>, TypeWriter<*>> {
-    return go(typeToTypeHandler.keySet().toTypedArray())
-  }
-
-  fun go(classes: Array<Class<*>>): LinkedHashMap<Class<*>, TypeWriter<*>> {
+  fun go(classes: Array<Class<*>> = typeToTypeHandler.keySet().toTypedArray()): LinkedHashMap<Class<*>, TypeWriter<*>?> {
     for (typeClass in classes) {
       createIfNotExists(typeClass)
     }
@@ -103,7 +98,7 @@ class InterfaceReader(val typeToTypeHandler: LinkedHashMap<Class<*>, TypeWriter<
   }
 
   private fun createIfNotExists(typeClass: Class<*>) {
-    if (typeClass == javaClass<Map<Any, Any>>() || typeClass == javaClass<List<Any>>() || !typeClass.isInterface()) {
+    if (typeClass == Map::class.java || typeClass == List::class.java || !typeClass.isInterface) {
       return
     }
 
@@ -113,17 +108,17 @@ class InterfaceReader(val typeToTypeHandler: LinkedHashMap<Class<*>, TypeWriter<
 
     typeToTypeHandler.put(typeClass, null)
 
-    for (aClass in typeClass.getDeclaredClasses()) {
+    for (aClass in typeClass.declaredClasses) {
       createIfNotExists(aClass)
     }
 
-    if (!typeClass.isInterface()) {
-      throw JsonProtocolModelParseException("Json model type should be interface: " + typeClass.getName())
+    if (!typeClass.isInterface) {
+      throw JsonProtocolModelParseException("Json model type should be interface: " + typeClass.name)
     }
 
     val fields = FieldProcessor(this, typeClass)
     for (method in fields.methodHandlerMap.keySet()) {
-      val returnType = method.getReturnType()
+      val returnType = method.returnType
       if (returnType != typeClass) {
         createIfNotExists(returnType)
       }
@@ -154,53 +149,53 @@ class InterfaceReader(val typeToTypeHandler: LinkedHashMap<Class<*>, TypeWriter<
       else if (type == java.lang.Float.TYPE) {
         return FLOAT_PARSER
       }
-      else if (type == javaClass<Number>() || type == java.lang.Double.TYPE) {
+      else if (type == Number::class.java || type == java.lang.Double.TYPE) {
         return NUMBER_PARSER
       }
       else if (type == Void.TYPE) {
         return VOID_PARSER
       }
-      else if (type == javaClass<String>()) {
+      else if (type == String::class.java) {
         if (method != null) {
-          val jsonField = method.getAnnotation<JsonField>(javaClass<JsonField>())
+          val jsonField = method.getAnnotation<JsonField>(JsonField::class.java)
           if (jsonField != null && jsonField.allowAnyPrimitiveValue) {
             return RAW_STRING_PARSER
           }
-          else if ((jsonField != null && jsonField.optional) || method.getAnnotation<JsonOptionalField>(javaClass<JsonOptionalField>()) != null) {
+          else if ((jsonField != null && jsonField.optional) || method.getAnnotation<JsonOptionalField>(JsonOptionalField::class.java) != null) {
             return NULLABLE_STRING_PARSER
           }
         }
         return STRING_PARSER
       }
-      else if (type == javaClass<Any>()) {
+      else if (type == Any::class.java) {
         return RAW_STRING_OR_MAP_PARSER
       }
-      else if (type == javaClass<JsonReaderEx>()) {
+      else if (type == JsonReaderEx::class.java) {
         return JSON_PARSER
       }
-      else if (type == javaClass<StringIntPair>()) {
+      else if (type == StringIntPair::class.java) {
         return STRING_INT_PAIR_PARSER
       }
-      else if (type.isArray()) {
-        return ArrayReader(getFieldTypeParser(type.getComponentType(), false, null), false)
+      else if (type.isArray) {
+        return ArrayReader(getFieldTypeParser(type.componentType, false, null), false)
       }
-      else if (type.isEnum()) {
+      else if (type.isEnum) {
         return EnumReader(type as Class<Enum<*>>)
       }
       val ref = getTypeRef(type)
       if (ref != null) {
-        return ObjectValueReader(ref, isSubtyping, method?.getAnnotation<JsonField>(javaClass<JsonField>())?.primitiveValue)
+        return ObjectValueReader(ref, isSubtyping, method?.getAnnotation<JsonField>(JsonField::class.java)?.primitiveValue)
       }
-      throw UnsupportedOperationException("Method return type " + type + " (simple class) not supported")
+      throw UnsupportedOperationException("Method return type $type (simple class) not supported")
     }
     else if (type is ParameterizedType) {
-      val isList = type.getRawType() == javaClass<List<Any>>()
-      if (isList || type.getRawType() == javaClass<Map<Any, Any>>()) {
-        var argumentType = type.getActualTypeArguments()[if (isList) 0 else 1]
+      val isList = type.rawType == List::class.java
+      if (isList || type.rawType == Map::class.java) {
+        var argumentType = type.actualTypeArguments[if (isList) 0 else 1]
         if (argumentType is WildcardType) {
           val wildcard = argumentType
-          if (wildcard.getLowerBounds().size() == 0 && wildcard.getUpperBounds().size() == 1) {
-            argumentType = wildcard.getUpperBounds()[0]
+          if (wildcard.lowerBounds.size() == 0 && wildcard.upperBounds.size() == 1) {
+            argumentType = wildcard.upperBounds[0]
           }
         }
         val componentParser = getFieldTypeParser(argumentType, false, method)
@@ -223,23 +218,23 @@ class InterfaceReader(val typeToTypeHandler: LinkedHashMap<Class<*>, TypeWriter<
 
   private fun getSuperclassRef(typeClass: Class<*>): TypeRef<*>? {
     var result: TypeRef<*>? = null
-    for (interfaceGeneric in typeClass.getGenericInterfaces()) {
+    for (interfaceGeneric in typeClass.genericInterfaces) {
       if (interfaceGeneric !is ParameterizedType) {
         continue
       }
-      if (interfaceGeneric.getRawType() != javaClass<JsonSubtype<Any>>()) {
+      if (interfaceGeneric.rawType != JsonSubtype::class.java) {
         continue
       }
-      val param = interfaceGeneric.getActualTypeArguments()[0]
+      val param = interfaceGeneric.actualTypeArguments[0]
       if (param !is Class<*>) {
         throw JsonProtocolModelParseException("Unexpected type of superclass " + param)
       }
       if (result != null) {
-        throw JsonProtocolModelParseException("Already has superclass " + result.typeClass.getName())
+        throw JsonProtocolModelParseException("Already has superclass " + result.typeClass.name)
       }
       result = getTypeRef(param)
       if (result == null) {
-        throw JsonProtocolModelParseException("Unknown base class " + param.getName())
+        throw JsonProtocolModelParseException("Unknown base class " + param.name)
       }
     }
     return result
