5a264a0055e337ff43e647e44b6283d003ef41d1
Konstantin Bulenkov <kb@jetbrains.com>
2015-10-15 03:39:16 +0200

    Make UIUtil.CONTRAST_BORDER_COLOR a function. Redefine colors for Mac laf
    (cherry picked from commit 996b25444af6fcf62b7e62afbd944c963e25464c)

--- platform/util/src/com/intellij/util/ui/UIUtil.java
+++ platform/util/src/com/intellij/util/ui/UIUtil.java
@@ -230,7 +230,17 @@ public class UIUtil {
   private static final Color INACTIVE_HEADER_COLOR = Gray._128;
   private static final Color BORDER_COLOR = Color.LIGHT_GRAY;
 
-  public static final Color CONTRAST_BORDER_COLOR = new JBColor(0x9b9b9b, 0x282828);
+  public static final Color CONTRAST_BORDER_COLOR = new JBColor(new NotNullProducer<Color>() {
+    final Color color = new JBColor(0x9b9b9b, 0x282828);
+    @NotNull
+    @Override
+    public Color produce() {
+      if (SystemInfo.isMac && isUnderIntelliJLaF()) {
+        return Gray.xC9;
+      }
+      return color;
+    }
+  });
   public static final Color SIDE_PANEL_BACKGROUND = new JBColor(new Color(0xE6EBF0), new Color(0x3E434C));
 
   public static final Color AQUA_SEPARATOR_FOREGROUND_COLOR = new JBColor(Gray._190, Gray.x51);
