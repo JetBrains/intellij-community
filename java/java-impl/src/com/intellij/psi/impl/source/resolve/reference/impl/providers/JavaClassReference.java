e6508ec2bcbd517c72abcaed5d8e2ba65bd650c3
Alexey Kudravtsev <cdr@intellij.com>
2015-10-20 14:26:14 +0300

    assertion for EA-70897 - SOE: CachingReference.resolve

--- java/java-impl/src/com/intellij/psi/impl/source/resolve/reference/impl/providers/JavaClassReference.java
+++ java/java-impl/src/com/intellij/psi/impl/source/resolve/reference/impl/providers/JavaClassReference.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -59,7 +59,10 @@ import com.intellij.util.text.CharArrayUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author peter
@@ -86,6 +89,7 @@ public class JavaClassReference extends GenericReference implements PsiJavaRefer
   @Nullable
   public PsiElement getContext() {
     final PsiReference contextRef = getContextReference();
+    assert contextRef != this : getCanonicalText();
     return contextRef != null ? contextRef.resolve() : null;
   }
 
