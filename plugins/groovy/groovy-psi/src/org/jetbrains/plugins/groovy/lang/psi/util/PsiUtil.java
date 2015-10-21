3b23d405650da7236075d3fecded2d440b98a16d
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2015-10-21 14:44:37 +0300

    [groovy] @NotNull & fix warnings & remove unnecessary casts

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/util/PsiUtil.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/util/PsiUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -1078,7 +1078,7 @@ public class PsiUtil {
     return ((GrListOrMap)firstArg).getNamedArguments();
   }
 
-  public static boolean isExpressionStatement(@NotNull PsiElement expr) {
+  public static boolean isExpressionStatement(@Nullable PsiElement expr) {
     if (!(expr instanceof GrStatement)) return false;
 
     final PsiElement parent = expr.getParent();
