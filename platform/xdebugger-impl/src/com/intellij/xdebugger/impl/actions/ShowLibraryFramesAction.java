bc7884b8a335c62935508f3809e784235cb1eeb8
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2015-10-23 12:25:33 +0200

    IDEA-CR-6066 add isLibraryFrameFilterSupported — ability to declare capability

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/actions/ShowLibraryFramesAction.java
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/actions/ShowLibraryFramesAction.java
@@ -19,6 +19,7 @@ import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.actionSystem.ToggleAction;
+import com.intellij.xdebugger.XDebugSession;
 import com.intellij.xdebugger.impl.XDebuggerUtilImpl;
 import com.intellij.xdebugger.impl.settings.XDebuggerSettingManagerImpl;
 import org.jetbrains.annotations.NotNull;
@@ -27,6 +28,10 @@ import org.jetbrains.annotations.NotNull;
  * @author egor
  */
 final class ShowLibraryFramesAction extends ToggleAction {
+  // we should remember initial answer "isLibraryFrameFilterSupported" because on stop no debugger process, but UI is still shown
+  // - we should avoid "jumping" (visible (start) - invisible (stop) - visible (start again))
+  private static final String IS_LIBRARY_FRAME_FILTER_SUPPORTED = "isLibraryFrameFilterSupported";
+
   private volatile boolean myShouldShow;
   private static final String ourTextWhenShowIsOn = "Hide Frames from Libraries";
   private static final String ourTextWhenShowIsOff = "Show All Frames";
@@ -39,9 +44,30 @@ final class ShowLibraryFramesAction extends ToggleAction {
   @Override
   public void update(@NotNull final AnActionEvent e) {
     super.update(e);
-    final Presentation presentation = e.getPresentation();
-    final boolean shouldShow = !Boolean.TRUE.equals(presentation.getClientProperty(SELECTED_PROPERTY));
-    presentation.setText(shouldShow ? ourTextWhenShowIsOn : ourTextWhenShowIsOff);
+
+    Presentation presentation = e.getPresentation();
+
+    Object isSupported = presentation.getClientProperty(IS_LIBRARY_FRAME_FILTER_SUPPORTED);
+    XDebugSession session = e.getData(XDebugSession.DATA_KEY);
+    if (isSupported == null) {
+      if (session == null) {
+        // if session is null and isSupported is null - just return, it means that action created initially not in the xdebugger tab
+        presentation.setVisible(false);
+        return;
+      }
+
+      isSupported = session.getDebugProcess().isLibraryFrameFilterSupported();
+      presentation.putClientProperty(IS_LIBRARY_FRAME_FILTER_SUPPORTED, isSupported);
+    }
+
+    if (Boolean.TRUE.equals(isSupported)) {
+      presentation.setVisible(true);
+      final boolean shouldShow = !Boolean.TRUE.equals(presentation.getClientProperty(SELECTED_PROPERTY));
+      presentation.setText(shouldShow ? ourTextWhenShowIsOn : ourTextWhenShowIsOff);
+    }
+    else {
+      presentation.setVisible(false);
+    }
   }
 
   @Override
