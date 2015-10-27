8f326caf4122823233cc10a7c49b3d47966fc29e
Egor.Ushakov <egor.ushakov@jetbrains.com>
2015-10-27 19:06:40 +0300

    EA-65128 - AIOOBE: DefaultMutableTreeNode.getChildAt

--- platform/script-debugger/debugger-ui/src/org/jetbrains/debugger/DebuggerSupportUtils.java
+++ platform/script-debugger/debugger-ui/src/org/jetbrains/debugger/DebuggerSupportUtils.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jetbrains.debugger;
 
 import com.intellij.openapi.vfs.VirtualFile;
@@ -13,7 +28,7 @@ public final class DebuggerSupportUtils {
       PsiElement navigationElement = element.getNavigationElement();
       VirtualFile file = navigationElement.getContainingFile().getVirtualFile();
       if (file != null) {
-        return XDebuggerUtil.getInstance().createPositionByOffset(file, navigationElement.getTextOffset());
+        return XDebuggerUtil.getInstance().createPositionByElement(navigationElement);
       }
     }
     return null;
