53c9a826d7f473fd998be7950470dc30ba7c3f8a
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2015-10-28 14:53:18 +0100

    convert ScriptBase, Script to kotlin (cherry picked from commit d4973a3724cf543c3c6122759118392d367e8983)

--- platform/script-debugger/backend/src/org/jetbrains/debugger/ScriptBase.java
+++ platform/script-debugger/backend/src/org/jetbrains/debugger/ScriptBase.java
@@ -1,78 +1,19 @@
-package org.jetbrains.debugger;
+package org.jetbrains.debugger
 
-import com.intellij.openapi.util.UserDataHolderBase;
-import com.intellij.util.Url;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.concurrency.Promise;
-import org.jetbrains.debugger.sourcemap.SourceMap;
+import com.intellij.openapi.util.UserDataHolderBase
+import com.intellij.util.Url
+import org.jetbrains.concurrency.Promise
+import org.jetbrains.debugger.sourcemap.SourceMap
 
-public abstract class ScriptBase extends UserDataHolderBase implements Script {
+abstract class ScriptBase(override val type: Script.Type,
+                          override val url: Url,
+                          override val line: Int,
+                          override val column: Int,
+                          override val endLine: Int) : UserDataHolderBase(), Script {
   @SuppressWarnings("UnusedDeclaration")
-  private volatile Promise<String> source;
+  private @Volatile var source: Promise<String>? = null
 
-  private final Url url;
-  protected final int line;
-  protected final int column;
-  protected final int endLine;
+  override var sourceMap: SourceMap? = null
 
-  protected final Type type;
-
-  private SourceMap sourceMap;
-
-  protected ScriptBase(@NotNull Type type, @NotNull Url url, int line, int column, int endLine) {
-    this.type = type;
-    this.url = url;
-    this.line = line;
-    this.column = column;
-    this.endLine = endLine;
-  }
-
-  @NotNull
-  @Override
-  public Url getUrl() {
-    return url;
-  }
-
-  @Nullable
-  @Override
-  public String getFunctionName() {
-    return null;
-  }
-
-  @Override
-  @Nullable
-  public SourceMap getSourceMap() {
-    return sourceMap;
-  }
-
-  @Override
-  public void setSourceMap(@Nullable SourceMap sourceMap) {
-    this.sourceMap = sourceMap;
-  }
-
-  @Override
-  public Type getType() {
-    return type;
-  }
-
-  @Override
-  public int getLine() {
-    return line;
-  }
-
-  @Override
-  public int getColumn() {
-    return column;
-  }
-
-  @Override
-  public int getEndLine() {
-    return endLine;
-  }
-
-  @Override
-  public String toString() {
-    return "[url=" + getUrl() + ", lineRange=[" + getLine() + ';' + getEndLine() + "]]";
-  }
+  override fun toString() = "[url=$url, lineRange=[$line;$endLine]]"
 }
