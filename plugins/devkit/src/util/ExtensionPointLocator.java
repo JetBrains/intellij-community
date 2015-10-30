ec5250b3602efa7bc3d3cd1e9a70c2ad3e59868b
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2015-10-30 19:01:20 +0100

    revert 2df19bff1c72adaaa6fa280e9852eefb16d2aed4 (kotlin cannot compile, may be due to package prefix)

--- plugins/devkit/src/util/ExtensionPointLocator.java
+++ plugins/devkit/src/util/ExtensionPointLocator.java
@@ -27,7 +27,6 @@ import com.intellij.util.SmartList;
 import com.intellij.util.xml.DomElement;
 import com.intellij.util.xml.DomService;
 import com.intellij.util.xml.DomUtil;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.idea.devkit.dom.ExtensionPoint;
 import org.jetbrains.idea.devkit.dom.IdeaPlugin;
@@ -71,7 +70,9 @@ public class ExtensionPointLocator {
 
   private static void findExtensionPointCandidates(PsiClass psiClass, final List<ExtensionPointCandidate> list) {
     String name = psiClass.getQualifiedName();
-    if (name == null) return;
+    if (name == null) {
+      return;
+    }
 
     final Project project = psiClass.getProject();
     final Collection<VirtualFile> candidates = DomService.getInstance().getDomFileCandidates(IdeaPlugin.class, project, GlobalSearchScope.allScope(project));
@@ -82,23 +83,6 @@ public class ExtensionPointLocator {
         PsiElement element = file.findElementAt(startOffset);
         processExtensionPointCandidate(element, list);
         return true;
-      }
-    }, scope);
-  }
-  
-  public static boolean isImplementedEp(@NotNull PsiClass psiClass, @NotNull final ExtensionPointCandidate candidate) {
-    String name = psiClass.getQualifiedName();
-    if (name == null) return false;
-
-    Project project = psiClass.getProject();
-    Collection<VirtualFile> candidates = DomService.getInstance().getDomFileCandidates(IdeaPlugin.class, project, GlobalSearchScope.allScope(project));
-    GlobalSearchScope scope = GlobalSearchScope.filesScope(project, candidates);
-    
-    return !PsiSearchHelper.SERVICE.getInstance(project).processUsagesInNonJavaFiles(name, new PsiNonJavaFileReferenceProcessor() {
-      @Override
-      public boolean process(PsiFile file, int startOffset, int endOffset) {
-        XmlTag tag = PsiTreeUtil.getParentOfType(file.findElementAt(startOffset), XmlTag.class);
-        return tag == null || !candidate.epName.endsWith(tag.getName());
       }
     }, scope);
   }
