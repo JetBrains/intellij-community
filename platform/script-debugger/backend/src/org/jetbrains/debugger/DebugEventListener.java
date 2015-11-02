8be6c5418e2ba11bd5103db75ae37e9ec0ee1b28
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2015-11-02 14:52:52 +0100

    continue WEB-16829 "too soon" typescript breakpoints are not hit

--- platform/script-debugger/backend/src/org/jetbrains/debugger/DebugEventListener.java
+++ platform/script-debugger/backend/src/org/jetbrains/debugger/DebugEventListener.java
@@ -1,6 +1,20 @@
+/*
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jetbrains.debugger;
 
-import com.intellij.util.Url;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -30,8 +44,6 @@ public interface DebugEventListener extends EventListener {
    * Reports that a new script has been loaded.
    */
   void scriptAdded(@NotNull Script script, @Nullable String sourceMapUrl);
-
-  void sourceMapFound(@NotNull Script script, @Nullable Url sourceMapUrl, @NotNull String sourceMapData);
 
   /**
    * Reports that the script has been collected and is no longer used in VM.
