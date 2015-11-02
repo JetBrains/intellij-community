acf75458e1bffb12d1288dc30a68939d17ef5844
Alexander Zolotov <alexander.zolotov@jetbrains.com>
2015-11-02 15:10:27 +0300

    DevKit: evaluate language names from bundle messages

--- plugins/devkit/testData/codeInsight/MyLanguage.java
+++ plugins/devkit/testData/codeInsight/MyLanguage.java
@@ -1,5 +1,6 @@
 public class MyLanguage extends com.intellij.lang.Language {
   public static final MyLanguage ANONYMOUS_LANUAGE = new MyLanguage("MyAnonymousLanguageID") {}; 
+  public static final MyLanguage ANONYMOUS_LANUAGE_WITH_NAME_FROM_PROPERTIES = new MyLanguage(MyBundle.message("language.name")) {}; 
 
   public MyLanguage() {
     super("MyLanguageID");
