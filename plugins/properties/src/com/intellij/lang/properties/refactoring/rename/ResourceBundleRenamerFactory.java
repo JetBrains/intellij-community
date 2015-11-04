1383d3c1125c130a5fedb3702d99527a984b8ab2
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2015-11-04 13:14:10 +0300

    ResourceBundle: renamer improved according to (IDEA-147065)

--- plugins/properties/src/com/intellij/lang/properties/refactoring/rename/ResourceBundleRenamerFactory.java
+++ plugins/properties/src/com/intellij/lang/properties/refactoring/rename/ResourceBundleRenamerFactory.java
@@ -17,12 +17,14 @@ package com.intellij.lang.properties.refactoring.rename;
 
 import com.intellij.lang.properties.PropertiesBundle;
 import com.intellij.lang.properties.PropertiesImplUtil;
+import com.intellij.lang.properties.ResourceBundle;
 import com.intellij.lang.properties.psi.PropertiesFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.refactoring.rename.naming.AutomaticRenamer;
 import com.intellij.refactoring.rename.naming.AutomaticRenamerFactory;
 import com.intellij.usageView.UsageInfo;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
@@ -32,11 +34,16 @@ import java.util.Collection;
  */
 public class ResourceBundleRenamerFactory implements AutomaticRenamerFactory {
   @Override
-  public boolean isApplicable(final PsiElement element) {
+  public boolean isApplicable(@NotNull final PsiElement element) {
     if (!(element instanceof PsiFile)) {
       return false;
     }
-    return PropertiesImplUtil.isPropertiesFile((PsiFile)element);
+    final PropertiesFile file = PropertiesImplUtil.getPropertiesFile(element);
+    if (file == null) {
+      return false;
+    }
+    final ResourceBundle resourceBundle = file.getResourceBundle();
+    return resourceBundle.getBaseDirectory() != null && resourceBundle.getPropertiesFiles().size() != 1;
   }
 
 
@@ -55,6 +62,7 @@ public class ResourceBundleRenamerFactory implements AutomaticRenamerFactory {
   public void setEnabled(final boolean enabled) {
   }
 
+  @NotNull
   @Override
   public AutomaticRenamer createRenamer(final PsiElement element, final String newName, final Collection<UsageInfo> usages) {
     final PropertiesFile propertiesFile = PropertiesImplUtil.getPropertiesFile((PsiFile)element);
