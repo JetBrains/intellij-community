3825996f724d63c54fba1823a950f1c4e2d3f777
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2015-11-05 17:17:21 +0100

    if we set breakpoint in a file, we should stop in it - ignore sourcemap (cherry picked from commit 2dcec71)

--- platform/script-debugger/debugger-ui/src/org/jetbrains/debugger/frame/SuspendContextImpl.kt
+++ platform/script-debugger/debugger-ui/src/org/jetbrains/debugger/frame/SuspendContextImpl.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jetbrains.debugger.frame
 
 import com.intellij.xdebugger.frame.XSuspendContext
@@ -5,14 +20,11 @@ import org.jetbrains.concurrency.Promise
 import org.jetbrains.concurrency.rejectedPromise
 import org.jetbrains.concurrency.resolvedPromise
 import org.jetbrains.concurrency.thenAsync
-import org.jetbrains.debugger.DebuggerViewSupport
-import org.jetbrains.debugger.EvaluateContext
-import org.jetbrains.debugger.Script
-import org.jetbrains.debugger.SuspendContext
+import org.jetbrains.debugger.*
 import org.jetbrains.debugger.values.StringValue
 
-open class SuspendContextImpl(private val suspendContext: SuspendContext, debugProcess: DebuggerViewSupport, topFrameScript: Script?) : XSuspendContext() {
-  private val executionStack = ExecutionStackImpl(suspendContext, debugProcess, topFrameScript)
+open class SuspendContextImpl(suspendContext: SuspendContext, debugProcess: DebuggerViewSupport, topFrameScript: Script?, topFrameSourceInfo: SourceInfo? = null) : XSuspendContext() {
+  private val executionStack = ExecutionStackImpl(suspendContext, debugProcess, topFrameScript, topFrameSourceInfo)
 
   override fun getActiveExecutionStack() = executionStack
 
