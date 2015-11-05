a65d10d074683c0fb9b31b0a69276d5afa7e4c69
Bas Leijdekkers <basleijdekkers@gmail.com>
2015-11-05 21:40:21 +0100

    IDEA-135101 (False positive of "Empty class") (cherry picked from commit 07bc394)

--- plugins/InspectionGadgets/test/com/siyeh/igtest/classlayout/empty_class/EmptyClass.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/classlayout/empty_class/EmptyClass.java
@@ -1,5 +1,7 @@
 package com.siyeh.igtest.classlayout.emptyclass;
 
+import java.io.Serializable;
+
 public class EmptyClass {
     {
       final java.util.ArrayList<String> stringList = new java.util.ArrayList<String>() {};
@@ -9,3 +11,12 @@ public class EmptyClass {
 class MyList extends java.util.ArrayList<String> {}
 class MyException extends java.lang.Exception {}
 abstract class <warning descr="Class 'ReportMe' is empty">ReportMe</warning> implements java.util.List {}
+abstract class <warning descr="Class 'Empty1' is empty">Empty1</warning> extends <error descr="No interface expected here">Serializable</error> {}
+
+interface Interface {
+  default void method() {
+    new Interface() { };
+  }
+}
+class EmptyClassTest implements Interface {
+}
