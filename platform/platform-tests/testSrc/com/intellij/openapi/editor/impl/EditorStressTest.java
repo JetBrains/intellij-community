8de9b0fa459444ce3818473d6921aaba4b26cfb2
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2015-11-05 21:05:08 +0300

    extract EditorStressTest

---
+++ platform/platform-tests/testSrc/com/intellij/openapi/editor/impl/EditorStressTest.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.openapi.editor.impl;
+
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.FoldRegion;
+import com.intellij.openapi.editor.FoldingModel;
+import com.intellij.openapi.editor.ex.DocumentEx;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class EditorStressTest extends AbstractEditorTest {
+  private static final int ITERATIONS = 10000;
+  private static final Long SEED_OVERRIDE = null; // set non-null value to run with a specific seed
+
+  private static final List<? extends Action> ourActions = Arrays.asList(new AddText("a"),
+                                                                         new AddText("\n"),
+                                                                         new AddText("\t"),
+                                                                         new RemoveCharacter(),
+                                                                         new MoveCharacter(),
+                                                                         new AddFoldRegion(),
+                                                                         new RemoveFoldRegion(),
+                                                                         new CollapseFoldRegion(),
+                                                                         new ExpandFoldRegion());
+
+  private final Random myRandom = new Random() {{
+    //noinspection ConstantConditions
+    setSeed(mySeed = (SEED_OVERRIDE == null ? nextLong() : SEED_OVERRIDE));
+  }};
+  private long mySeed;
+
+  public void testRandomActions() {
+    int i = 0;
+    try {
+      initText("");
+      configureSoftWraps(10);
+      for (i = 1; i <= ITERATIONS; i++) {
+        doRandomAction();
+        checkConsistency(myEditor);
+      }
+    }
+    catch (Throwable t) {
+      throw new RuntimeException("Failed when run with seed=" + mySeed + " in iteration " + i, t);
+    }
+  }
+
+  private void doRandomAction() {
+    ourActions.get(myRandom.nextInt(ourActions.size())).perform(myEditor, myRandom);
+  }
+
+  protected void checkConsistency(Editor editor) {
+  }
+
+  interface Action {
+    void perform(Editor editor, Random random);
+  }
+
+  private static class AddText implements Action {
+    private final String myText;
+
+    AddText(String text) {
+      myText = text;
+    }
+
+    @Override
+    public void perform(Editor editor, Random random) {
+      Document document = editor.getDocument();
+      int offset = random.nextInt(document.getTextLength() + 1);
+      document.insertString(offset, myText);
+    }
+  }
+
+  private static class RemoveCharacter implements Action {
+    @Override
+    public void perform(Editor editor, Random random) {
+      Document document = editor.getDocument();
+      int textLength = document.getTextLength();
+      if (textLength <= 0) return;
+      int offset = random.nextInt(textLength);
+      document.deleteString(offset, offset + 1);
+    }
+  }
+
+  private static class MoveCharacter implements Action {
+    @Override
+    public void perform(Editor editor, Random random) {
+      Document document = editor.getDocument();
+      int textLength = document.getTextLength();
+      if (textLength <= 0) return;
+      int offset = random.nextInt(textLength);
+      int targetOffset = random.nextInt(textLength + 1);
+      if (targetOffset < offset || targetOffset > offset + 1) {
+        ((DocumentEx)document).moveText(offset, offset + 1, targetOffset);
+      }
+    }
+  }
+
+  private static class AddFoldRegion implements Action {
+    @Override
+    public void perform(final Editor editor, Random random) {
+      Document document = editor.getDocument();
+      int textLength = document.getTextLength();
+      if (textLength <= 0) return;
+      final int startOffset = random.nextInt(textLength + 1);
+      final int endOffset = random.nextInt(textLength + 1);
+      if (startOffset == endOffset) return;
+      final FoldingModel foldingModel = editor.getFoldingModel();
+      foldingModel.runBatchFoldingOperation(() -> foldingModel.addFoldRegion(Math.min(startOffset, endOffset),
+                                                                             Math.max(startOffset, endOffset),
+                                                                             "."));
+    }
+  }
+
+  private static class RemoveFoldRegion implements Action {
+    @Override
+    public void perform(final Editor editor, Random random) {
+      final FoldingModel foldingModel = editor.getFoldingModel();
+      FoldRegion[] foldRegions = foldingModel.getAllFoldRegions();
+      if (foldRegions.length == 0) return;
+      final FoldRegion region = foldRegions[random.nextInt(foldRegions.length)];
+      foldingModel.runBatchFoldingOperation(() -> foldingModel.removeFoldRegion(region));
+    }
+  }
+
+  private static class CollapseFoldRegion implements Action {
+    @Override
+    public void perform(final Editor editor, Random random) {
+      final FoldingModel foldingModel = editor.getFoldingModel();
+      FoldRegion[] foldRegions = foldingModel.getAllFoldRegions();
+      if (foldRegions.length == 0) return;
+      final FoldRegion region = foldRegions[random.nextInt(foldRegions.length)];
+      foldingModel.runBatchFoldingOperation(() -> region.setExpanded(false));
+    }
+  }
+
+  private static class ExpandFoldRegion implements Action {
+    @Override
+    public void perform(final Editor editor, Random random) {
+      final FoldingModel foldingModel = editor.getFoldingModel();
+      FoldRegion[] foldRegions = foldingModel.getAllFoldRegions();
+      if (foldRegions.length == 0) return;
+      final FoldRegion region = foldRegions[random.nextInt(foldRegions.length)];
+      foldingModel.runBatchFoldingOperation(() -> region.setExpanded(true));
+    }
+  }
+}
--- platform/platform-tests/testSrc/com/intellij/openapi/editor/impl/softwrap/mapping/SoftWrapStressTest.java
+++ platform/platform-tests/testSrc/com/intellij/openapi/editor/impl/softwrap/mapping/SoftWrapStressTest.java
@@ -16,77 +16,51 @@
 package com.intellij.openapi.editor.impl.softwrap.mapping;
 
 import com.intellij.openapi.editor.*;
-import com.intellij.openapi.editor.ex.DocumentEx;
-import com.intellij.openapi.editor.impl.AbstractEditorTest;
+import com.intellij.openapi.editor.impl.EditorStressTest;
 import com.intellij.openapi.editor.impl.SoftWrapModelImpl;
 import com.intellij.openapi.util.registry.Registry;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
 
-public class SoftWrapStressTest extends AbstractEditorTest {
-  private static final int ITERATIONS = 10000;
-  private static final Long SEED_OVERRIDE = null; // set non-null value to run with a specific seed
-  
-  private static final List<? extends Action> ourActions = Arrays.asList(new AddText("a"),
-                                                                         new AddText("\n"),
-                                                                         new AddText("\t"),
-                                                                         new RemoveCharacter(),
-                                                                         new MoveCharacter(),
-                                                                         new AddFoldRegion(),
-                                                                         new RemoveFoldRegion(),
-                                                                         new CollapseFoldRegion(),
-                                                                         new ExpandFoldRegion());
-  
-  private final Random myRandom = new Random() {{
-    //noinspection ConstantConditions
-    setSeed(mySeed = (SEED_OVERRIDE == null ? nextLong() : SEED_OVERRIDE)); 
-  }};
-  private long mySeed;
-  
-  public void testSoftWrapModelInternalCachesStayConsistentAfterDocumentAndFoldingChanges() {
+public class SoftWrapStressTest extends EditorStressTest {
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
     Registry.get("editor.new.rendering").setValue(false);
-    int i = 0;
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
     try {
-      initText("");
-      configureSoftWraps(10);
-      for (i = 1; i <= ITERATIONS; i++) {
-        doRandomAction();
-        checkConsistency();
-      }
-    }
-    catch (Throwable t) {
-      throw new RuntimeException("Failed when run with seed=" + mySeed + " in iteration " + i, t);
-    }
-    finally {
       Registry.get("editor.new.rendering").setValue(true);
     }
+    finally {
+      super.tearDown();
+    }
   }
-  
-  private void doRandomAction() {
-    ourActions.get(myRandom.nextInt(ourActions.size())).perform(myEditor, myRandom);
-  }
-  
-  private static void checkConsistency() {
-    Set<Integer> softWrapOffsets = checkSoftWraps();
-    List<CacheEntry> cache = ((SoftWrapModelImpl)myEditor.getSoftWrapModel()).getDataMapper().getCache();
+
+  @Override
+  protected void checkConsistency(Editor editor) {
+    Set<Integer> softWrapOffsets = checkSoftWraps(editor);
+    List<CacheEntry> cache = ((SoftWrapModelImpl)editor.getSoftWrapModel()).getDataMapper().getCache();
     CacheEntry prevEntry = null;
     for (CacheEntry entry : cache) {
       boolean currentLineStartsWithSoftWrap = checkRelationBetweenNeighbourEntries(entry, prevEntry, softWrapOffsets);
       checkConsistencyWithinEntry(entry, currentLineStartsWithSoftWrap);
-      checkConsistencyWithFoldingsAndTabs(entry);
+      checkConsistencyWithFoldingsAndTabs(editor, entry);
       prevEntry = entry;
     }
     assertTrue(softWrapOffsets.isEmpty());
   }
 
-  private static void checkConsistencyWithFoldingsAndTabs(CacheEntry entry) {
+  private static void checkConsistencyWithFoldingsAndTabs(Editor editor, CacheEntry entry) {
     Map<Integer, FoldRegion> actualFoldRegions = new HashMap<>();
     List<Integer> actualTabPositions = new ArrayList<>();
     for (int i = entry.startOffset; i < entry.endOffset; i++) {
-      FoldRegion region = myEditor.getFoldingModel().getCollapsedRegionAtOffset(i);
+      FoldRegion region = editor.getFoldingModel().getCollapsedRegionAtOffset(i);
       if (region == null) {
-        if (myEditor.getDocument().getCharsSequence().charAt(i) == '\t') {
+        if (editor.getDocument().getCharsSequence().charAt(i) == '\t') {
           actualTabPositions.add(i);
         }
       }
@@ -182,8 +156,8 @@ public class SoftWrapStressTest extends AbstractEditorTest {
   }
 
   @NotNull
-  private static Set<Integer> checkSoftWraps() {
-    List<? extends SoftWrap> softWraps = myEditor.getSoftWrapModel().getSoftWrapsForRange(0, myEditor.getDocument().getTextLength());
+  private static Set<Integer> checkSoftWraps(Editor editor) {
+    List<? extends SoftWrap> softWraps = editor.getSoftWrapModel().getSoftWrapsForRange(0, editor.getDocument().getTextLength());
     Set<Integer> softWrapOffsets = new HashSet<>();
     int lastOffset = -1;
     for (SoftWrap softWrap : softWraps) {
@@ -193,98 +167,5 @@ public class SoftWrapStressTest extends AbstractEditorTest {
       softWrapOffsets.add(lastOffset);
     }
     return softWrapOffsets;
-  }
-
-  interface Action {
-    void perform(Editor editor, Random random);
-  }
-  
-  private static class AddText implements Action {
-    private final String myText;
-
-    AddText(String text) {
-      myText = text;
-    }
-    
-    @Override
-    public void perform(Editor editor, Random random) {
-      Document document = editor.getDocument();
-      int offset = random.nextInt(document.getTextLength() + 1);
-      document.insertString(offset, myText);
-    }
-  }
-  
-  private static class RemoveCharacter implements Action {
-    @Override
-    public void perform(Editor editor, Random random) {
-      Document document = editor.getDocument();
-      int textLength = document.getTextLength();
-      if (textLength <= 0) return;
-      int offset = random.nextInt(textLength);
-      document.deleteString(offset, offset + 1);
-    }
-  }
-  
-  private static class MoveCharacter implements Action {
-    @Override
-    public void perform(Editor editor, Random random) {
-      Document document = editor.getDocument();
-      int textLength = document.getTextLength();
-      if (textLength <= 0) return;
-      int offset = random.nextInt(textLength);
-      int targetOffset = random.nextInt(textLength + 1);
-      if (targetOffset < offset || targetOffset > offset + 1) {
-        ((DocumentEx)document).moveText(offset, offset + 1, targetOffset);
-      }
-    }
-  }
-  
-  private static class AddFoldRegion implements Action {
-    @Override
-    public void perform(final Editor editor, Random random) {
-      Document document = editor.getDocument();
-      int textLength = document.getTextLength();
-      if (textLength <= 0) return;
-      final int startOffset = random.nextInt(textLength + 1);
-      final int endOffset = random.nextInt(textLength + 1);
-      if (startOffset == endOffset) return;
-      final FoldingModel foldingModel = editor.getFoldingModel();
-      foldingModel.runBatchFoldingOperation(() -> foldingModel.addFoldRegion(Math.min(startOffset, endOffset), 
-                                                                             Math.max(startOffset, endOffset), 
-                                                                             "."));
-    }
-  }
-  
-  private static class RemoveFoldRegion implements Action {
-    @Override
-    public void perform(final Editor editor, Random random) {
-      final FoldingModel foldingModel = editor.getFoldingModel();
-      FoldRegion[] foldRegions = foldingModel.getAllFoldRegions();
-      if (foldRegions.length == 0) return;
-      final FoldRegion region = foldRegions[random.nextInt(foldRegions.length)];
-      foldingModel.runBatchFoldingOperation(() -> foldingModel.removeFoldRegion(region));
-    }
-  }
-  
-  private static class CollapseFoldRegion implements Action {
-    @Override
-    public void perform(final Editor editor, Random random) {
-      final FoldingModel foldingModel = editor.getFoldingModel();
-      FoldRegion[] foldRegions = foldingModel.getAllFoldRegions();
-      if (foldRegions.length == 0) return;
-      final FoldRegion region = foldRegions[random.nextInt(foldRegions.length)];
-      foldingModel.runBatchFoldingOperation(() -> region.setExpanded(false));
-    }
-  }
-  
-  private static class ExpandFoldRegion implements Action {
-    @Override
-    public void perform(final Editor editor, Random random) {
-      final FoldingModel foldingModel = editor.getFoldingModel();
-      FoldRegion[] foldRegions = foldingModel.getAllFoldRegions();
-      if (foldRegions.length == 0) return;
-      final FoldRegion region = foldRegions[random.nextInt(foldRegions.length)];
-      foldingModel.runBatchFoldingOperation(() -> region.setExpanded(true));
-    }
   }
 }
