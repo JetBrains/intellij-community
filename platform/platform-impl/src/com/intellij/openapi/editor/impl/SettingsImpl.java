a7b28ab90179bf2e403801ee1cca2a28ff4f8cc9
Yaroslav Lepenkin <yaroslav.lepenkin@jetbrains.com>
2015-11-05 22:12:52 +0300

    Do not run indent detector in any way in viewer mode

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/SettingsImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/SettingsImpl.java
@@ -32,6 +32,7 @@ import com.intellij.openapi.editor.ex.DocumentEx;
 import com.intellij.openapi.editor.ex.EditorEx;
 import com.intellij.openapi.editor.ex.EditorSettingsExternalizable;
 import com.intellij.openapi.editor.impl.softwrap.SoftWrapAppliancePlaces;
+import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.registry.Registry;
 import com.intellij.psi.PsiDocumentManager;
@@ -355,7 +356,12 @@ public class SettingsImpl implements EditorSettings {
     }
     else  {
       PsiFile file = getPsiFile(project);
-      tabSize = CodeStyleSettingsManager.getSettings(project).getIndentOptionsByFile(file).TAB_SIZE;
+      if (myEditor != null && myEditor.isViewer()) {
+        FileType fileType = file != null ? file.getFileType() : null;
+        tabSize = CodeStyleSettingsManager.getSettings(project).getIndentOptions(fileType).TAB_SIZE; 
+      } else {
+        tabSize = CodeStyleSettingsManager.getSettings(project).getIndentOptionsByFile(file).TAB_SIZE;
+      }
     }
     myCachedTabSize = Integer.valueOf(tabSize);
     return tabSize;
