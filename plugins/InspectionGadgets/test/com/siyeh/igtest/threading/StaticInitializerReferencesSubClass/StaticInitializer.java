6fdc2d284ea8ed1a36ad84db48f687f5b400625b
peter <peter@jetbrains.com>
2015-11-06 12:22:21 +0100

    StaticInitializerReferencesSubClassInspection: don't report private subclasses that can't be initialized by another thread

--- plugins/InspectionGadgets/test/com/siyeh/igtest/threading/StaticInitializerReferencesSubClass/StaticInitializer.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/threading/StaticInitializerReferencesSubClass/StaticInitializer.java
@@ -4,16 +4,30 @@ class Super {
   static final Sub C3;
   static final Sub C4 = SubFactory.<warning descr="Referencing subclass Sub from superclass Super initializer might lead to class loading deadlock">create</warning>();
   static final String C5 = SubFactory.<warning descr="Referencing subclass Sub from superclass Super initializer might lead to class loading deadlock">create</warning>().toString();
+  static Super USED_PRIVATE = new <warning descr="Referencing subclass MyUsedSub from superclass Super initializer might lead to class loading deadlock">MyUsedSub</warning>();
 
   static Object OK_INSIDE_ANONYMOUS = new Object() {{ Sub s = new Sub(); }};
   static Object OK_UNRELATED = "abc";
   static Super OK_SAME = new Super();
   static Super OK_SAME_ANONYMOUS = new Super(){};
   static Sub[] OK_ARRAY = new Sub[3];
+  static Super OK_PRIVATE = new MySub();
   static java.util.List<Sub> OK_GENERICS = new java.util.ArrayList<Sub>();
 
   static {
     C3 = new <warning descr="Referencing subclass Sub from superclass Super initializer might lead to class loading deadlock">Sub</warning>();
+  }
+
+  static void foo() {
+    MyUsedSub.foo();
+  }
+
+  private static class MySub extends Super { }
+  private static class MyUsedSub extends Super {
+    static void foo() {
+      System.out.println("hello");
+    }
+
   }
 }
 
