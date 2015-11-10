238f57659cde58bbdf76605556badacffc27560d
Anna Kozlova <anna.kozlova@jetbrains.com>
2015-11-10 13:48:20 +0100

    inferred type check for raw constraints (IDEA-147529)
    (cherry picked from commit c29f92472e326c4d01e0f1fb01e2c5ed80d38b31)

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewMethodRefHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewMethodRefHighlightingTest.java
@@ -450,6 +450,10 @@ public class NewMethodRefHighlightingTest extends LightDaemonAnalyzerTestCase {
     doTest();
   }
 
+  public void testRawInferredTypeCheck() throws Exception {
+    doTest();
+  }
+
   private void doTest() {
     doTest(false);
   }
