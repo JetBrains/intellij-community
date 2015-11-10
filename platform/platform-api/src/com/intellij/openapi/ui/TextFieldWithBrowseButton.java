JetBrains/intellij-community

commit f219b19ddfc02ab7f0fb8de52da9727a1d243dda

diff: fix memory leak in dirdiff
installFileCompletion() with null Disposable will register itself on DialogWrapper from focused component (or on application-wide Disposable "ui").
We can show dirdiff in FrameWrapper/ToolWindow now, so we should provide right Disposable.

platform/platform-api/src/com/intellij/openapi/ui/TextFieldWithBrowseButton.java
@@ -41,15 +41,23 @@ public class TextFieldWithBrowseButton extends ComponentWithBrowseButton<JTextFi
   }
 
   public TextFieldWithBrowseButton(JTextField field, @Nullable ActionListener browseActionListener) {
+    this(field, browseActionListener, null);
+  }
+
+  public TextFieldWithBrowseButton(JTextField field, @Nullable ActionListener browseActionListener, @Nullable Disposable parent) {
     super(field, browseActionListener);
     if (!(field instanceof JBTextField)) {
       UIUtil.addUndoRedoActions(field);
     }
-    installPathCompletion(FileChooserDescriptorFactory.createSingleLocalFileDescriptor());
+    installPathCompletion(FileChooserDescriptorFactory.createSingleLocalFileDescriptor(), parent);
   }
 
   public TextFieldWithBrowseButton(ActionListener browseActionListener) {
-    this(new JBTextField(10/* to prevent field to be infinitely resized in grid-box layouts */), browseActionListener);
+    this(browseActionListener, null);
+  }
+
+  public TextFieldWithBrowseButton(ActionListener browseActionListener, Disposable parent) {
+    this(new JBTextField(10/* to prevent field to be infinitely resized in grid-box layouts */), browseActionListener, parent);
   }
 
   public void addBrowseFolderListener(@Nullable String title, @Nullable String description, @Nullable Project project, FileChooserDescriptor fileChooserDescriptor) {

platform/diff-impl/src/com/intellij/openapi/diff/impl/dir/DirDiffPanel.form
@@ -55,7 +55,7 @@
                             <constraints border-constraint="West"/>
                             <properties/>
                           </component>
-                          <component id="ee75f" class="com.intellij.openapi.ui.TextFieldWithBrowseButton" binding="mySourceDirField">
+                          <component id="ee75f" class="com.intellij.openapi.ui.TextFieldWithBrowseButton" binding="mySourceDirField" custom-create="true">
                             <constraints border-constraint="Center"/>
                             <properties/>
                           </component>
@@ -72,7 +72,7 @@
                             <constraints border-constraint="West"/>
                             <properties/>
                           </component>
-                          <component id="cc2ae" class="com.intellij.openapi.ui.TextFieldWithBrowseButton" binding="myTargetDirField">
+                          <component id="cc2ae" class="com.intellij.openapi.ui.TextFieldWithBrowseButton" binding="myTargetDirField" custom-create="true">
                             <constraints border-constraint="Center"/>
                             <properties/>
                           </component>

platform/diff-impl/src/com/intellij/openapi/diff/impl/dir/DirDiffPanel.java
@@ -511,6 +511,9 @@ public class DirDiffPanel implements Disposable, DataProvider {
   }
 
   private void createUIComponents() {
+    mySourceDirField = new TextFieldWithBrowseButton(null, this);
+    myTargetDirField = new TextFieldWithBrowseButton(null, this);
+
     final AtomicBoolean callUpdate = new AtomicBoolean(true);
     myRootPanel = new JPanel(new BorderLayout()) {
       @Override

commit ad0299362366deb76c0bb6df4480f6fcfb5ecd93

disable completion in NoPathCompletion
was broken by f219b19

platform/platform-api/src/com/intellij/openapi/ui/TextFieldWithBrowseButton.java
@@ -128,7 +128,7 @@ public class TextFieldWithBrowseButton extends ComponentWithBrowseButton<JTextFi
     }
 
     @Override
-    protected void installPathCompletion(final FileChooserDescriptor fileChooserDescriptor) {
+    protected void installPathCompletion(FileChooserDescriptor fileChooserDescriptor, @Nullable Disposable parent) {
     }
   }
 }