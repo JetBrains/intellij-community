9bb691136b31d98204c47eef372ac44ffb5c3ca6
Yaroslav Lepenkin <yaroslav.lepenkin@jetbrains.com>
2015-11-11 15:21:04 +0300

    tracking all stuff through AnActionListener

--- src/com/stats/completion/StatsCompletionLoader.kt
+++ src/com/stats/completion/StatsCompletionLoader.kt
@@ -1,49 +1,119 @@
 package com.stats.completion
 
+import com.intellij.codeInsight.lookup.LookupAdapter
+import com.intellij.codeInsight.lookup.LookupEvent
 import com.intellij.codeInsight.lookup.LookupManager
 import com.intellij.codeInsight.lookup.impl.LookupImpl
-import com.intellij.codeInsight.lookup.impl.actions.ChooseItemAction
 import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.actionSystem.ex.AnActionListener
-import com.intellij.openapi.components.ApplicationComponent
+import com.intellij.openapi.components.AbstractProjectComponent
+import com.intellij.openapi.project.Project
+import java.beans.PropertyChangeListener
 
 
-class CompletionPopupActionsTracker : AnActionListener {
+class CompletionTrackerInitializer(project: Project): AbstractProjectComponent(project) {
+    private val lookupPopupActionTracker = CompletionPopupActionsTracker()
+    
+    private val lookupTrackerInitializer = PropertyChangeListener {
+        val lookup = it.newValue
+        if (lookup is LookupImpl) {
+            lookup.addLookupListener(TrackingLookupListener(lookupPopupActionTracker))
+        }
+    }
+
+    override fun initComponent() {
+        ActionManager.getInstance().addAnActionListener(lookupPopupActionTracker)
+    }
+
+    override fun disposeComponent() {
+        ActionManager.getInstance().removeAnActionListener(lookupPopupActionTracker)
+    }
+
+    override fun projectOpened() {
+        val manager = LookupManager.getInstance(myProject)
+        manager.addPropertyChangeListener(lookupTrackerInitializer)
+    }
+    
+    override fun projectClosed() {
+        val manager = LookupManager.getInstance(myProject)
+        manager.removePropertyChangeListener(lookupTrackerInitializer)
+    }
+    
+}
+
+
+interface CompletionPopupListener {
+    fun downPressed()
+    fun upPressed()
+    fun backSpacePressed()
+    fun typed(c: Char)
+    
+    class Adapter: CompletionPopupListener {
+        override fun downPressed() = Unit
+        override fun upPressed() = Unit
+        override fun backSpacePressed() = Unit
+        override fun typed(c: Char) = Unit
+    }
+}
+
+
+class TrackingLookupListener(private val completionTracker: CompletionPopupActionsTracker): CompletionPopupListener, LookupAdapter() {
+    private var completionPopupShown = false
+    
+    override fun lookupCanceled(event: LookupEvent) {
+        println("Cancelled")
+    }
+
+    override fun currentItemChanged(event: LookupEvent) {
+        if (!completionPopupShown) {
+            completionTracker.popupListener = this
+            completionPopupShown
+        }
+    }
+
+    override fun itemSelected(event: LookupEvent) {
+        println("Item selected")
+    }
+    
+    override fun downPressed() {
+        println("Down pressed")
+    }
+
+    override fun upPressed() {
+        println("Up pressed")
+    }
+
+    override fun backSpacePressed() {
+        println("Backspace pressed")
+    }
+
+    override fun typed(c: Char) {
+        println("Typed $c")
+    }
+    
+}
+
+class CompletionPopupActionsTracker : AnActionListener.Adapter() {
     
     private val down = ActionManager.getInstance().getAction(IdeActions.ACTION_EDITOR_MOVE_CARET_DOWN)
     private val up = ActionManager.getInstance().getAction(IdeActions.ACTION_EDITOR_MOVE_CARET_UP)
     private val backspace = ActionManager.getInstance().getAction(IdeActions.ACTION_EDITOR_BACKSPACE)
+    
+    var popupListener: CompletionPopupListener = CompletionPopupListener.Adapter()
     
     private fun obtainLookup(dataContext: DataContext) = LookupManager.getActiveLookup(CommonDataKeys.EDITOR.getData(dataContext)) as LookupImpl?
 
     override fun beforeActionPerformed(action: AnAction, dataContext: DataContext, event: AnActionEvent) {
         val lookup = obtainLookup(dataContext) ?: return
         when (action) {
-            down -> println("Down")
-            up -> println("Up")
-            backspace -> println("Backspace")
-            is ChooseItemAction -> println("Item chosen") 
+            down -> popupListener.downPressed()
+            up -> popupListener.upPressed()
+            backspace -> popupListener.backSpacePressed()
         }
     }
 
     override fun beforeEditorTyping(c: Char, dataContext: DataContext) {
         val lookup = obtainLookup(dataContext) ?: return
-        println("Typed $c")
-    }
-
-    override fun afterActionPerformed(action: AnAction, dataContext: DataContext, event: AnActionEvent) {
-        val lookup = obtainLookup(dataContext) ?: return
-    }
-}
-
-class CompletionStatsRetriever: ApplicationComponent.Adapter() {
-    private val trackingListener = CompletionPopupActionsTracker()
-
-    override fun initComponent() {
-        ActionManager.getInstance().addAnActionListener(trackingListener)
-    }
-    
-    override fun disposeComponent() {
-        ActionManager.getInstance().removeAnActionListener(trackingListener)
+        popupListener.typed(c)
     }
 }
