159bc61d953bdf907fea97622cad31cb71710ba8
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2015-11-13 17:16:32 +0100

    convert EvaluateContextBase, EvaluateContext to kotlin (cherry picked from commit aed86068231b70b10e06936ae9659de980982ddb)

--- platform/script-debugger/backend/src/org/jetbrains/debugger/EvaluateContextBase.java
+++ platform/script-debugger/backend/src/org/jetbrains/debugger/EvaluateContextBase.java
@@ -1,54 +1,13 @@
-package org.jetbrains.debugger;
+package org.jetbrains.debugger
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.concurrency.Promise;
-import org.jetbrains.debugger.values.ValueManager;
+import org.jetbrains.concurrency.Promise
+import org.jetbrains.debugger.values.ValueManager
 
-import java.util.Map;
+abstract class EvaluateContextBase<VALUE_MANAGER : ValueManager<out Vm>>(val valueManager: VALUE_MANAGER) : EvaluateContext {
+  override fun withValueManager(objectGroup: String) = this
 
-public abstract class EvaluateContextBase<VALUE_MANAGER extends ValueManager> implements EvaluateContext {
-  protected final VALUE_MANAGER valueManager;
-
-  protected EvaluateContextBase(@NotNull VALUE_MANAGER valueManager) {
-    this.valueManager = valueManager;
+  override fun releaseObjects() {
   }
 
-  @NotNull
-  @Override
-  public Promise<EvaluateResult> evaluate(@NotNull String expression) {
-    return evaluate(expression, null, false);
-  }
-
-  @NotNull
-  @Override
-  public Promise<EvaluateResult> evaluate(@NotNull String expression, boolean enableBreak) {
-    return evaluate(expression, null, true);
-  }
-
-  @NotNull
-  @Override
-  public EvaluateContext withValueManager(@NotNull String objectGroup) {
-    return this;
-  }
-
-  @Override
-  public void releaseObjects() {
-  }
-
-  @NotNull
-  @Override
-  public abstract Promise<EvaluateResult> evaluate(@NotNull String expression, @Nullable Map<String, Object> additionalContext, boolean enableBreak);
-
-  @NotNull
-  public final VALUE_MANAGER getValueManager() {
-    return valueManager;
-  }
-
-  @NotNull
-  @Override
-  public Promise<?> refreshOnDone(@NotNull Promise<?> promise) {
-    //noinspection unchecked
-    return promise.then(valueManager.getClearCachesTask());
-  }
+  override fun refreshOnDone(promise: Promise<*>): Promise<*> = promise.then(valueManager.clearCachesTask)
 }
