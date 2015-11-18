88de87a4d670196f094573022f86695b53b6ec69
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2015-11-18 18:32:06 +0100

    Cleanup (formatting; warnings)

--- platform/platform-impl/src/com/intellij/ide/customize/CustomizeMacKeyboardLayoutStep.java
+++ platform/platform-impl/src/com/intellij/ide/customize/CustomizeMacKeyboardLayoutStep.java
@@ -25,56 +25,48 @@ import javax.swing.*;
 import java.awt.*;
 
 public class CustomizeMacKeyboardLayoutStep extends AbstractCustomizeWizardStep {
-  private final JPanel myMacPanel;
-  private final JPanel myDefaultPanel;
-
-  public CustomizeMacKeyboardLayoutStep() {//&#8997; alt
+  public CustomizeMacKeyboardLayoutStep() {
     setLayout(new GridLayout(1, 2, GAP, GAP));
-    final JRadioButton macRadioButton =
-      new JRadioButton("I've never used " + ApplicationNamesInfo.getInstance().getProductName());
+
+    String style = "<style type=\"text/css\">" +
+                   "body {margin-left:" + GAP + "px; border:none;padding:0px;}" +
+                   "table {margin:0px; cell-padding:0px; border:none;}" +
+                   "</style>";
+
+    JRadioButton macRadioButton = new JRadioButton("I've never used " + ApplicationNamesInfo.getInstance().getProductName());
     macRadioButton.setOpaque(false);
-    myMacPanel = createBigButtonPanel(new VerticalFlowLayout(), macRadioButton, new Runnable() {
+    JPanel macPanel = createBigButtonPanel(new VerticalFlowLayout(), macRadioButton, new Runnable() {
       @Override
       public void run() {
         WelcomeWizardUtil.setWizardKeymap(KeymapManager.MAC_OS_X_10_5_PLUS_KEYMAP);
       }
     });
-    String style = "<style type=\"text/css\">" +
-    "body {margin-left:"+ GAP +"px; border:none;padding:0px;}"+
-    "table {margin:0px; cell-padding:0px; border:none;}"+
-    "</style>";
+    macPanel.add(macRadioButton);
+    macPanel.add(new JLabel("<html><head>" + style + "</head><body><h3>" + KeymapManager.MAC_OS_X_10_5_PLUS_KEYMAP + " keymap</h3>" +
+                            "Adapted for OS X<br><br><table><tr><td align=\"left\" colspan=\"2\">EXAMPLES</td></tr>" +
+                            "<tr><td style=\"text-align:right;\">&#8984;N</td><td style=\"text-align:left;\">Generate</td></tr>" +
+                            "<tr><td style=\"text-align:right;\">&#8984;O</td><td style=\"text-align:left;\">Go to class</td></tr>" +
+                            "<tr><td style=\"text-align:right;\">&#8984;&#9003;</td><td style=\"text-align:left;\">Delete line</td></tr>" +
+                            "</table></body></html>"));
+    add(macPanel);
 
-    myMacPanel.add(macRadioButton);
-    myMacPanel.add(
-      new JLabel("<html><head>"+style+"</head><body><h3>" + KeymapManager.MAC_OS_X_10_5_PLUS_KEYMAP + " keymap</h3>" +
-                 "Adapted for Mac<br><br><table><tr><td align=\"left\" colspan=\"2\">EXAMPLES</td></tr>" +
-                 "<tr><td style=\"text-align:right;\">&#8984;N</td><td style=\"text-align:left;\">Generate</td></tr>" +
-                 "<tr><td style=\"text-align:right;\">&#8984;O</td><td style=\"text-align:left;\">Go to class</td></tr>" +
-                 "<tr><td style=\"text-align:right;\">&#8984;&#9003;</td><td style=\"text-align:left;\">Delete line</td></tr>" +
-                 "</table></body></html>"
-      ));
-
-    add(myMacPanel);
-    final JRadioButton defaultRadioButton =
-      new JRadioButton("I used " + ApplicationNamesInfo.getInstance().getProductName() + " before");
+    JRadioButton defaultRadioButton = new JRadioButton("I used " + ApplicationNamesInfo.getInstance().getProductName() + " before");
     defaultRadioButton.setOpaque(false);
-    myDefaultPanel = createBigButtonPanel(new VerticalFlowLayout(),defaultRadioButton, new Runnable() {
+    JPanel defaultPanel = createBigButtonPanel(new VerticalFlowLayout(), defaultRadioButton, new Runnable() {
       @Override
       public void run() {
         WelcomeWizardUtil.setWizardKeymap(KeymapManager.MAC_OS_X_KEYMAP);
       }
-    } );
-    myDefaultPanel.add(defaultRadioButton);
-    myDefaultPanel.add(new JLabel("<html><head>" + style + "</head><body><h3>" + KeymapManager.MAC_OS_X_KEYMAP + " keymap</h3>" +
-                                  "Default for all platforms<br><br><table><tr><td align=\"left\" colspan=\"2\">EXAMPLES</td></tr>" +
-                                  "<tr><td style=\"text-align:right;\">^N</td><td style=\"text-align:left;\">Generate</td></tr>" +
-                                  "<tr><td style=\"text-align:right;\">&#8984;N</td><td style=\"text-align:left;\">Go to class</td></tr>" +
-                                  "<tr><td style=\"text-align:right;\">&#8984;Y</td><td style=\"text-align:left;\">Delete line</td></tr>" +
-                                  "</table></body></html>"
-    ));
+    });
+    defaultPanel.add(defaultRadioButton);
+    defaultPanel.add(new JLabel("<html><head>" + style + "</head><body><h3>" + KeymapManager.MAC_OS_X_KEYMAP + " keymap</h3>" +
+                                "Default for all platforms<br><br><table><tr><td align=\"left\" colspan=\"2\">EXAMPLES</td></tr>" +
+                                "<tr><td style=\"text-align:right;\">^N</td><td style=\"text-align:left;\">Generate</td></tr>" +
+                                "<tr><td style=\"text-align:right;\">&#8984;N</td><td style=\"text-align:left;\">Go to class</td></tr>" +
+                                "<tr><td style=\"text-align:right;\">&#8984;Y</td><td style=\"text-align:left;\">Delete line</td></tr>" +
+                                "</table></body></html>"));
+    add(defaultPanel);
 
-    add(myMacPanel);
-    add(myDefaultPanel);
     ButtonGroup group = new ButtonGroup();
     group.add(macRadioButton);
     group.add(defaultRadioButton);
