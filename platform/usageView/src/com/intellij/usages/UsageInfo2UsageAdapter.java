3a89b61d9d5b13eec910ff259f678adee7650617
Alexey Kudravtsev <cdr@intellij.com>
2015-12-02 15:27:51 +0300

    make sure ChunkExtractor won't be called for binary files

--- platform/usageView/src/com/intellij/usages/UsageInfo2UsageAdapter.java
+++ platform/usageView/src/com/intellij/usages/UsageInfo2UsageAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -520,13 +520,12 @@ public class UsageInfo2UsageAdapter implements UsageInModule,
       PsiFile file = getPsiFile();
 
       if (file != null) {
-        ChunkExtractor extractor = ChunkExtractor.getExtractor(file);
         Segment segment = getFirstSegment();
 
         if (segment != null) {
           Document document = PsiDocumentManager.getInstance(getProject()).getDocument(file);
-
           if (document != null) {
+            ChunkExtractor extractor = ChunkExtractor.getExtractor(file);
             SmartList<TextChunk> chunks = new SmartList<TextChunk>();
             extractor.createTextChunks(
               this,
