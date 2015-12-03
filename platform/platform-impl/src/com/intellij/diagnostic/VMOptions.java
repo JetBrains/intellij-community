61206faf9cbb42d3e3cc6e9dfe8144af7daca66a
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2015-12-03 11:30:06 +0100

    Cleanup (missing import)

--- platform/platform-impl/src/com/intellij/diagnostic/VMOptions.java
+++ platform/platform-impl/src/com/intellij/diagnostic/VMOptions.java
@@ -22,6 +22,7 @@ import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.util.SystemProperties;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.TestOnly;
 
