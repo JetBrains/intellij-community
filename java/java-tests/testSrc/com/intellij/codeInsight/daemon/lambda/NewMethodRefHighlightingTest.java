36ea2f9041743d100dec70bea7438e43aece58ed
Anna Kozlova <anna.kozlova@jetbrains.com>
2015-12-03 13:22:53 +0100

    testdata for IDEA-148841

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewMethodRefHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewMethodRefHighlightingTest.java
@@ -466,6 +466,10 @@ public class NewMethodRefHighlightingTest extends LightDaemonAnalyzerTestCase {
     doTest();
   }
 
+  public void testIDEA148841() throws Exception {
+    doTest();
+  }
+
   private void doTest() {
     doTest(false);
   }
