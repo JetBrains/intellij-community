8a908f16f35fc2ed98fbc9a4498e8bfec78633a3
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2015-12-03 20:28:30 +0300

    avoid flickering of editor search field background on text typing (regression after commit ef82524)

--- platform/lang-impl/src/com/intellij/find/EditorSearchSession.java
+++ platform/lang-impl/src/com/intellij/find/EditorSearchSession.java
@@ -160,7 +160,7 @@ public class EditorSearchSession implements SearchSession,
           myFindModel.setWholeWordsOnly(false);
         }
         updateUIWithFindModel();
-        clearSearchResults();
+        mySearchResults.clear();
         updateResults(true);
         FindUtil.updateFindInFileModel(getProject(), myFindModel);
       }
@@ -236,6 +236,7 @@ public class EditorSearchSession implements SearchSession,
 
   @Override
   public void searchResultsUpdated(SearchResults sr) {
+    if (sr.getFindModel() == null) return;
     if (myComponent.getSearchTextComponent().getText().isEmpty()) {
       updateUIWithEmptyResults();
     } else {
