92cfa8c5749a353a9f48be1531af46a046d6e6d2
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2015-12-03 10:04:28 +0300

    java inspections:  inspection for identical branches for conditional expressions  extended to highlight and fix branches which differ "by one element" (IDEA-147900)

--- plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/controlflow/ConditionalExpressionWithIdenticalBranchesInspection.java
+++ plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/controlflow/ConditionalExpressionWithIdenticalBranchesInspection.java
@@ -17,18 +17,29 @@ package com.siyeh.ig.controlflow;
 
 import com.intellij.codeInspection.CleanupLocalInspectionTool;
 import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.codeInspection.ui.SingleCheckboxOptionsPanel;
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiConditionalExpression;
-import com.intellij.psi.PsiExpression;
+import com.intellij.psi.*;
 import com.siyeh.InspectionGadgetsBundle;
 import com.siyeh.ig.BaseInspection;
 import com.siyeh.ig.BaseInspectionVisitor;
 import com.siyeh.ig.InspectionGadgetsFix;
 import com.siyeh.ig.PsiReplacementUtil;
 import com.siyeh.ig.psiutils.EquivalenceChecker;
+import com.siyeh.ig.psiutils.ParenthesesUtils;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
 
 public class ConditionalExpressionWithIdenticalBranchesInspection extends BaseInspection implements CleanupLocalInspectionTool {
+  public boolean myReportOnlyExactlyIdentical;
+
+  @Nullable
+  @Override
+  public JComponent createOptionsPanel() {
+    return new SingleCheckboxOptionsPanel("Report only exactly identical branches", this, "myReportOnlyExactlyIdentical");
+  }
 
   @Override
   @NotNull
@@ -44,31 +55,61 @@ public class ConditionalExpressionWithIdenticalBranchesInspection extends BaseIn
 
   @Override
   public InspectionGadgetsFix buildFix(Object... infos) {
-    return new CollapseConditional();
+    return new CollapseConditional((PsiConditionalExpression)infos[0]);
   }
 
   private static class CollapseConditional extends InspectionGadgetsFix {
+    private final SmartPsiElementPointer<PsiConditionalExpression> myConditionalExpression;
+
+    public CollapseConditional(PsiConditionalExpression expression) {
+      myConditionalExpression = SmartPointerManager.getInstance(expression.getProject()).createSmartPsiElementPointer(expression);
+    }
 
     @Override
     @NotNull
     public String getName() {
-      return InspectionGadgetsBundle.message("conditional.expression.with.identical.branches.collapse.quickfix");
+      return InspectionGadgetsBundle.message(getEquivalenceDecision().getExactlyMatches() ?
+                                             "conditional.expression.with.identical.branches.collapse.quickfix" :
+                                             "conditional.expression.with.identical.branches.collapse.quickfix.family") ;
     }
 
     @Override
     @NotNull
     public String getFamilyName() {
-      return getName();
+      return InspectionGadgetsBundle.message("conditional.expression.with.identical.branches.collapse.quickfix.family");
+    }
+
+    public PsiConditionalExpression getConditionalExpression() {
+      return myConditionalExpression.getElement();
+    }
+
+    private EquivalenceChecker.Decision getEquivalenceDecision() {
+      return EquivalenceChecker.expressionsAreEquivalentDecision(getConditionalExpression().getThenExpression(), getConditionalExpression().getElseExpression());
     }
 
     @Override
     public void doFix(Project project, ProblemDescriptor descriptor) {
-      final PsiConditionalExpression expression = (PsiConditionalExpression)descriptor.getPsiElement();
-      final PsiExpression thenExpression = expression.getThenExpression();
+      final EquivalenceChecker.Decision decision = getEquivalenceDecision();
+      final PsiConditionalExpression conditionalExpression = getConditionalExpression();
+      final PsiExpression thenExpression = conditionalExpression.getThenExpression();
       assert thenExpression != null;
-      final String bodyText = thenExpression.getText();
-      PsiReplacementUtil.replaceExpression(expression, bodyText);
-    }
+      if (decision.getExactlyMatches()) {
+        final PsiConditionalExpression expression = (PsiConditionalExpression)descriptor.getPsiElement();
+        final String bodyText = thenExpression.getText();
+        PsiReplacementUtil.replaceExpression(expression, bodyText);
+      } else if (!decision.isExactUnMatches()) {
+        final PsiElement leftDiff = decision.getLeftDiff();
+        final PsiElement rightDiff = decision.getRightDiff();
+
+        final String expression = "(" + conditionalExpression.getCondition().getText() + " ? " + leftDiff.getText() + " : " + rightDiff.getText() + ")";
+        final PsiExpression newConditionalExpression =
+          JavaPsiFacade.getElementFactory(project).createExpressionFromText(expression, conditionalExpression);
+
+        final PsiElement replacedConditionalExpression = leftDiff.replace(newConditionalExpression);
+        ParenthesesUtils.removeParentheses((PsiExpression)replacedConditionalExpression, false);
+        conditionalExpression.replace(thenExpression);
+      }
+     }
   }
 
   @Override
@@ -76,15 +117,16 @@ public class ConditionalExpressionWithIdenticalBranchesInspection extends BaseIn
     return new ConditionalExpressionWithIdenticalBranchesVisitor();
   }
 
-  private static class ConditionalExpressionWithIdenticalBranchesVisitor extends BaseInspectionVisitor {
+  private class ConditionalExpressionWithIdenticalBranchesVisitor extends BaseInspectionVisitor {
 
     @Override
     public void visitConditionalExpression(PsiConditionalExpression expression) {
       super.visitConditionalExpression(expression);
       final PsiExpression thenExpression = expression.getThenExpression();
       final PsiExpression elseExpression = expression.getElseExpression();
-      if (thenExpression != null && EquivalenceChecker.expressionsAreEquivalent(thenExpression, elseExpression)) {
-        registerError(expression);
+      final EquivalenceChecker.Decision decision = EquivalenceChecker.expressionsAreEquivalentDecision(thenExpression, elseExpression);
+      if (thenExpression != null && (myReportOnlyExactlyIdentical ? decision.getExactlyMatches() : !decision.isExactUnMatches())) {
+        registerError(expression, expression);
       }
     }
   }
