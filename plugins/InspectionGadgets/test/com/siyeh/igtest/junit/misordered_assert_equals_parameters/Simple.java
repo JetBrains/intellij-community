23cf91342860423f50512b4116c5585c2a39e59c
Anton Makeev <Anton.Makeev@jetbrains.com>
2015-12-11 17:20:34 +0100

    Revert: IDEA-141144 (Misordered assertEquals doesn't work for new instances of JDK classes) (2981bf1fe2eddb107d400a8d73a5207ae85e87c9)

--- plugins/InspectionGadgets/test/com/siyeh/igtest/junit/misordered_assert_equals_parameters/Simple.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/junit/misordered_assert_equals_parameters/Simple.java
@@ -20,19 +20,12 @@ class Simple {
       Assert.<warning descr="Arguments to 'assertEquals()' in wrong order">assertEquals</warning>(timeUnit, TimeUnit.HOURS);
       Assert.<warning descr="Arguments to 'assertEquals()' in wrong order">assertEquals</warning>(test, map);
 
-      Assert.<warning descr="Arguments to 'assertEquals()' in wrong order">assertEquals</warning>("message", test, null);
-      Assert.<warning descr="Arguments to 'assertEquals()' in wrong order">assertEquals</warning>(new X().m(), new Integer(10));
+      Assert.<warning descr="Arguments to 'assertEquals()' in wrong order">assertEquals</warning><error descr="'assertEquals(java.lang.String, long, long)' in 'org.junit.Assert' cannot be applied to '(java.lang.String, java.lang.Object, null)'">("message", new Object(), null)</error>;
       return null;
     }
   }
 
   enum TimeUnit {
     HOURS
-  }
-
-  class X {
-    Integer m() {
-      return null;
-    }
   }
 }
