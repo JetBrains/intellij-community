35cf666b7c0334da6a8f438d91693248d67b39f5
Anton Makeev <Anton.Makeev@jetbrains.com>
2015-12-13 00:05:11 +0100

    OSProcessManager.killProcess

--- platform/platform-impl/src/com/intellij/execution/process/impl/OSProcessManagerImpl.java
+++ platform/platform-impl/src/com/intellij/execution/process/impl/OSProcessManagerImpl.java
@@ -57,6 +57,21 @@ public class OSProcessManagerImpl extends OSProcessManager {
     return false;
   }
 
+  public static void killProcess(@NotNull Process process) {
+    if (SystemInfo.isWindows) {
+      try {
+        WinProcess winProcess = createWinProcess(process);
+        winProcess.kill();
+      }
+      catch (Throwable e) {
+        LOG.info("Cannot kill process tree", e);
+      }
+    }
+    else if (SystemInfo.isUnix) {
+      UnixProcessManager.sendSignal(UnixProcessManager.getProcessPid(process), UnixProcessManager.SIGKILL);
+    }
+  }
+
   @SuppressWarnings("deprecation")
   @NotNull
   private static WinProcess createWinProcess(@NotNull Process process) {
