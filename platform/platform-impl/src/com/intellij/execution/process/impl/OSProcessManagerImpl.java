c20a414e02cf472bcec791c9657e03061e3c7979
Anton Makeev <Anton.Makeev@jetbrains.com>
2015-12-13 19:55:12 +0100

    OSProcessManagerImpl.getProcessID

--- platform/platform-impl/src/com/intellij/execution/process/impl/OSProcessManagerImpl.java
+++ platform/platform-impl/src/com/intellij/execution/process/impl/OSProcessManagerImpl.java
@@ -64,12 +64,28 @@ public class OSProcessManagerImpl extends OSProcessManager {
         winProcess.kill();
       }
       catch (Throwable e) {
-        LOG.info("Cannot kill process tree", e);
+        LOG.info("Cannot kill process", e);
       }
     }
     else if (SystemInfo.isUnix) {
       UnixProcessManager.sendSignal(UnixProcessManager.getProcessPid(process), UnixProcessManager.SIGKILL);
     }
+  }
+  
+  public static int getProcessID(@NotNull Process process) {
+    if (SystemInfo.isWindows) {
+      try {
+        return createWinProcess(process).getPid();
+      }
+      catch (Throwable e) {
+        LOG.info("Cannot get process id", e);
+        return -1;
+      }
+    }
+    else if (SystemInfo.isUnix) {
+      return UnixProcessManager.getProcessPid(process);
+    }
+    throw new IllegalStateException("Unknown OS: "  + SystemInfo.OS_NAME);
   }
 
   @SuppressWarnings("deprecation")
