ea06bf5b96e896b1d837f85ecd395792a9bede1d
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2015-12-14 16:06:08 +0100

    EDT watermark to contain product name, os, java version (cherry picked from commit c4fefb6)

--- platform/core-impl/src/com/intellij/openapi/application/impl/ApplicationInfoImpl.java
+++ platform/core-impl/src/com/intellij/openapi/application/impl/ApplicationInfoImpl.java
@@ -22,13 +22,15 @@ import com.intellij.openapi.application.ex.ApplicationInfoEx;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.BuildNumber;
 import com.intellij.openapi.util.JDOMUtil;
+import com.intellij.openapi.util.SystemInfo;
+import com.intellij.openapi.util.SystemInfoRt;
 import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.ui.JBColor;
 import com.intellij.util.ArrayUtil;
 import com.intellij.util.Function;
-import com.intellij.util.ImageLoader;
 import com.intellij.util.PlatformUtils;
+import com.intellij.util.SystemProperties;
 import com.intellij.util.containers.ContainerUtil;
 import org.jdom.Document;
 import org.jdom.Element;
@@ -629,7 +631,13 @@ public class ApplicationInfoImpl extends ApplicationInfoEx {
     }
 
     Thread currentThread = Thread.currentThread();
-    currentThread.setName(currentThread.getName() + " " + myMajorVersion + "." + myMinorVersion + "#" + myBuildNumber + ", eap:" + myEAP);
+    currentThread.setName(
+      currentThread.getName() + " " +
+      ApplicationNamesInfo.getInstance().getProductName() + " " +
+      myMajorVersion + "." + myMinorVersion + "#" + myBuildNumber +
+      " " + ApplicationNamesInfo.getInstance().getProductName() +
+      ", eap:" + myEAP + ", os:" + SystemInfoRt.OS_NAME + " " + SystemInfoRt.OS_VERSION +
+      ", java-version:" + SystemProperties.getJavaVendor() + " " + SystemInfo.JAVA_RUNTIME_VERSION);
 
     Element logoElement = parentNode.getChild(ELEMENT_LOGO);
     if (logoElement != null) {
