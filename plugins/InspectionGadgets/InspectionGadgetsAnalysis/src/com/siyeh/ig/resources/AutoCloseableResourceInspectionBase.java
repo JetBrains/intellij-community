e05addf17a44c1712d20c27b2e94734b6051cfc2
Bas Leijdekkers <basleijdekkers@gmail.com>
2015-12-14 20:04:56 +0100

    IDEA-130318 (Incorrect "connection used without try-with-resource" highlight)

--- plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/resources/AutoCloseableResourceInspectionBase.java
+++ plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/resources/AutoCloseableResourceInspectionBase.java
@@ -119,7 +119,8 @@ public class AutoCloseableResourceInspectionBase extends BaseInspection {
       }
       if (MethodCallUtils.isCallToMethod(expression, "java.util.Formatter", null, "format", null) ||
           MethodCallUtils.isCallToMethod(expression, "java.io.Writer", null, "append", null) ||
-          MethodCallUtils.isCallToMethod(expression, "com.google.common.base.Preconditions", null, "checkNotNull", null)) {
+          MethodCallUtils.isCallToMethod(expression, "com.google.common.base.Preconditions", null, "checkNotNull", null) ||
+          MethodCallUtils.isCallToMethod(expression, "org.hibernate.Session", null, "close")) {
         return;
       }
       if (!isNotSafelyClosedResource(expression)) {
