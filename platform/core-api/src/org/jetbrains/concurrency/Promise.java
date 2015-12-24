a376b83628d30074716273e342ddbf4c45b13101
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2015-12-24 16:00:49 +0100

    koltin beta 5093 compatibility — thenAsync
    (cherry picked from commit 1400c20a080d0b3ccaa807a1dd10d8008014712b)

--- platform/core-api/src/org/jetbrains/concurrency/Promise.java
+++ platform/core-api/src/org/jetbrains/concurrency/Promise.java
@@ -152,7 +152,7 @@ public abstract class Promise<T> {
   public abstract <SUB_RESULT> Promise<SUB_RESULT> then(@NotNull Function<T, SUB_RESULT> done);
 
   @NotNull
-  public abstract <SUB_RESULT> Promise<SUB_RESULT> then(@NotNull AsyncFunction<T, SUB_RESULT> done);
+  public abstract <SUB_RESULT> Promise<SUB_RESULT> thenAsync(@NotNull AsyncFunction<T, SUB_RESULT> done);
 
   @NotNull
   public abstract State getState();
