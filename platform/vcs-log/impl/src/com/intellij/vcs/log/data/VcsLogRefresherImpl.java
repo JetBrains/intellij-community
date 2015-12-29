e37d8123be27ec4b7b6c78d72dcc4be6d3363e50
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2015-12-29 22:13:23 +0300

    [vcs-log] fix myDataPack field value in refresher

--- platform/vcs-log/impl/src/com/intellij/vcs/log/data/VcsLogRefresherImpl.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/data/VcsLogRefresherImpl.java
@@ -110,9 +110,9 @@ public class VcsLogRefresherImpl implements VcsLogRefresher {
       Map<VirtualFile, Set<VcsRef>> refs = data.getRefs();
       List<GraphCommit<Integer>> compoundList = multiRepoJoin(commits);
       compoundList = compoundList.subList(0, Math.min(myRecentCommitCount, compoundList.size()));
-      DataPack dataPack = DataPack.build(compoundList, refs, myProviders, myHashMap, false);
+      myDataPack = DataPack.build(compoundList, refs, myProviders, myHashMap, false);
       mySingleTaskController.request(RefreshRequest.RELOAD_ALL); // build/rebuild the full log in background
-      return dataPack;
+      return myDataPack;
     }
     catch (VcsException e) {
       myExceptionHandler.consume(e);
