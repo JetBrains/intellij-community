ca61a35933c3925a7fe952e4f3885b824e5a7e94
Alexey Kudravtsev <cdr@intellij.com>
2015-12-30 14:19:44 +0300

    execute popState() always to avoid "stack is empty" exceptions like EA-73546 - assert: AbstractProgressIndicatorBase.popState

--- java/compiler/openapi/src/com/intellij/openapi/compiler/DummyCompileContext.java
+++ java/compiler/openapi/src/com/intellij/openapi/compiler/DummyCompileContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -61,6 +61,7 @@ public class DummyCompileContext implements CompileContext {
     return 0;
   }
 
+  @NotNull
   public ProgressIndicator getProgressIndicator() {
     return null;
   }
