3386c28d5551bcca6d7509e68a2421cad86400b2
Bas Leijdekkers <basleijdekkers@gmail.com>
2015-12-31 12:36:17 +0100

    IG: improved "Confusing argument to varargs method" inspection - added quickfixes - clarified name and problem descriptor - added warning when passed array subtype

--- plugins/InspectionGadgets/test/com/siyeh/igtest/bugs/null_argument_to_variable_arg_method/NullArgumentToVariableArgMethod.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/bugs/null_argument_to_variable_arg_method/NullArgumentToVariableArgMethod.java
@@ -1,9 +1,10 @@
 package com.siyeh.igtest.bugs.null_argument_to_variable_arg_method;
 
 public class NullArgumentToVariableArgMethod {
-    public void foo()
+    public void foo(String[] ss)
     {
-        String.format("%s", <warning descr="Confusing 'null' argument to var-arg method">null</warning>);
+        String.format("%s", <warning descr="Confusing argument 'null', unclear if a varargs or non-varargs call is desired">null</warning>);
         String.format("%d", 1);
+        String.format("%s", <warning descr="Confusing argument 'ss', unclear if a varargs or non-varargs call is desired">ss</warning>);
     }
 }
