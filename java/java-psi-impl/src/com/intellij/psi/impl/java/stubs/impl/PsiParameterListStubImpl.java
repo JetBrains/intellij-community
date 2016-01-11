a080a53cf9f802144500bb24e68426d1fa1b6ff3
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2016-01-11 14:26:13 +0100

    Cleanup (formatting)

--- java/java-psi-impl/src/com/intellij/psi/impl/java/stubs/impl/PsiParameterListStubImpl.java
+++ java/java-psi-impl/src/com/intellij/psi/impl/java/stubs/impl/PsiParameterListStubImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,10 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-/*
- * @author max
- */
 package com.intellij.psi.impl.java.stubs.impl;
 
 import com.intellij.psi.PsiParameterList;
@@ -25,6 +21,9 @@ import com.intellij.psi.impl.java.stubs.PsiParameterListStub;
 import com.intellij.psi.stubs.StubBase;
 import com.intellij.psi.stubs.StubElement;
 
+/**
+ * @author max
+ */
 public class PsiParameterListStubImpl extends StubBase<PsiParameterList> implements PsiParameterListStub {
   public PsiParameterListStubImpl(final StubElement parent) {
     super(parent, JavaStubElementTypes.PARAMETER_LIST);
