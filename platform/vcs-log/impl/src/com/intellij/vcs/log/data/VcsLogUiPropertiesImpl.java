6775ce5c836c61e714fa8e162cba11a9ee090ebe
Julia Beliaeva <julia.beliaeva@jetbrains.com>
2016-01-12 14:31:18 +0300

    [vcs-log] multiple-log properties

--- platform/vcs-log/impl/src/com/intellij/vcs/log/data/VcsLogUiPropertiesImpl.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/data/VcsLogUiPropertiesImpl.java
@@ -16,9 +16,6 @@
 package com.intellij.vcs.log.data;
 
 import com.intellij.openapi.components.PersistentStateComponent;
-import com.intellij.openapi.components.State;
-import com.intellij.openapi.components.Storage;
-import com.intellij.openapi.components.StoragePathMacros;
 import com.intellij.util.Function;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.vcs.log.VcsLogSettings;
@@ -32,11 +29,8 @@ import java.util.*;
  * Differs from {@link VcsLogSettings} in the fact, that these settings have no representation in the UI settings,
  * and have insignificant effect to the logic of the log, they are just gracefully remember what user prefers to see in the UI.
  */
-@State(name = "Vcs.Log.UiProperties", storages = {@Storage(StoragePathMacros.WORKSPACE_FILE)})
-public class VcsLogUiPropertiesImpl implements PersistentStateComponent<VcsLogUiPropertiesImpl.State>, VcsLogUiProperties {
+public abstract class VcsLogUiPropertiesImpl implements PersistentStateComponent<VcsLogUiPropertiesImpl.State>, VcsLogUiProperties {
   private static final int RECENTLY_FILTERED_VALUES_LIMIT = 10;
-  private final VcsLogSettings mySettings;
-  private State myState = new State();
 
   public static class State {
     public boolean SHOW_DETAILS = true;
@@ -49,24 +43,9 @@ public class VcsLogUiPropertiesImpl implements PersistentStateComponent<VcsLogUi
     public Map<String, Boolean> HIGHLIGHTERS = ContainerUtil.newTreeMap();
   }
 
-  public VcsLogUiPropertiesImpl(@NotNull VcsLogSettings settings) {
-    mySettings = settings;
-  }
-
-  @Nullable
+  @NotNull
   @Override
-  public State getState() {
-    return myState;
-  }
-
-  @Override
-  public void loadState(State state) {
-    myState = state;
-    if (mySettings.isShowBranchesPanel()) {
-      myState.SHOW_BRANCHES_PANEL = true;
-      mySettings.setShowBranchesPanel(false);
-    }
-  }
+  public abstract State getState();
 
   /**
    * Returns true if the details pane (which shows commit meta-data, such as the full commit message, commit date, all references, etc.)
@@ -74,22 +53,22 @@ public class VcsLogUiPropertiesImpl implements PersistentStateComponent<VcsLogUi
    */
   @Override
   public boolean isShowDetails() {
-    return myState.SHOW_DETAILS;
+    return getState().SHOW_DETAILS;
   }
 
   @Override
   public void setShowDetails(boolean showDetails) {
-    myState.SHOW_DETAILS = showDetails;
+    getState().SHOW_DETAILS = showDetails;
   }
 
   @Override
   public void addRecentlyFilteredUserGroup(@NotNull List<String> usersInGroup) {
-    addRecentGroup(usersInGroup, myState.RECENTLY_FILTERED_USER_GROUPS);
+    addRecentGroup(usersInGroup, getState().RECENTLY_FILTERED_USER_GROUPS);
   }
 
   @Override
   public void addRecentlyFilteredBranchGroup(@NotNull List<String> valuesInGroup) {
-    addRecentGroup(valuesInGroup, myState.RECENTLY_FILTERED_BRANCH_GROUPS);
+    addRecentGroup(valuesInGroup, getState().RECENTLY_FILTERED_BRANCH_GROUPS);
   }
 
   private static void addRecentGroup(@NotNull List<String> valuesInGroup, @NotNull Deque<UserGroup> stateField) {
@@ -107,13 +86,13 @@ public class VcsLogUiPropertiesImpl implements PersistentStateComponent<VcsLogUi
   @Override
   @NotNull
   public List<List<String>> getRecentlyFilteredUserGroups() {
-    return getRecentGroup(myState.RECENTLY_FILTERED_USER_GROUPS);
+    return getRecentGroup(getState().RECENTLY_FILTERED_USER_GROUPS);
   }
 
   @Override
   @NotNull
   public List<List<String>> getRecentlyFilteredBranchGroups() {
-    return getRecentGroup(myState.RECENTLY_FILTERED_BRANCH_GROUPS);
+    return getRecentGroup(getState().RECENTLY_FILTERED_BRANCH_GROUPS);
   }
 
   @NotNull
@@ -128,53 +107,53 @@ public class VcsLogUiPropertiesImpl implements PersistentStateComponent<VcsLogUi
 
   @Override
   public boolean areLongEdgesVisible() {
-    return myState.LONG_EDGES_VISIBLE;
+    return getState().LONG_EDGES_VISIBLE;
   }
 
   @Override
   public void setLongEdgesVisibility(boolean visible) {
-    myState.LONG_EDGES_VISIBLE = visible;
+    getState().LONG_EDGES_VISIBLE = visible;
   }
 
   @Override
   public int getBekSortType() {
-    return myState.BEK_SORT_TYPE;
+    return getState().BEK_SORT_TYPE;
   }
 
   @Override
   public void setBek(int bekSortType) {
-    myState.BEK_SORT_TYPE = bekSortType;
+    getState().BEK_SORT_TYPE = bekSortType;
   }
 
   @Override
   public boolean isShowRootNames() {
-    return myState.SHOW_ROOT_NAMES;
+    return getState().SHOW_ROOT_NAMES;
   }
 
   @Override
   public void setShowRootNames(boolean isShowRootNames) {
-    myState.SHOW_ROOT_NAMES = isShowRootNames;
+    getState().SHOW_ROOT_NAMES = isShowRootNames;
   }
 
   @Override
   public boolean isHighlighterEnabled(@NotNull String id) {
-    Boolean result = myState.HIGHLIGHTERS.get(id);
+    Boolean result = getState().HIGHLIGHTERS.get(id);
     return result != null ? result : true; // new highlighters get enabled by default
   }
 
   @Override
   public void enableHighlighter(@NotNull String id, boolean value) {
-    myState.HIGHLIGHTERS.put(id, value);
+    getState().HIGHLIGHTERS.put(id, value);
   }
 
   @Override
   public boolean isShowBranchesPanel() {
-    return myState.SHOW_BRANCHES_PANEL;
+    return getState().SHOW_BRANCHES_PANEL;
   }
 
   @Override
   public void setShowBranchesPanel(boolean show) {
-    myState.SHOW_BRANCHES_PANEL = show;
+    getState().SHOW_BRANCHES_PANEL = show;
   }
 
   public static class UserGroup {
