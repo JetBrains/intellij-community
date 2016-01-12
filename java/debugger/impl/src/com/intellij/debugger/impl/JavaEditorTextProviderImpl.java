ec2e976f7bdfc0f01842340bb40ff7df9fb31c4e
Egor.Ushakov <egor.ushakov@jetbrains.com>
2016-01-12 16:13:58 +0300

    do not quick evaluate lambda expressions

--- java/debugger/impl/src/com/intellij/debugger/impl/JavaEditorTextProviderImpl.java
+++ java/debugger/impl/src/com/intellij/debugger/impl/JavaEditorTextProviderImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -124,7 +124,9 @@ public class JavaEditorTextProviderImpl implements EditorTextProvider {
         }
       }
     }
-    else if (parent instanceof PsiExpression && !(parent instanceof PsiNewExpression)) {
+    else if (parent instanceof PsiExpression &&
+             !(parent instanceof PsiNewExpression) &&
+             !(parent instanceof PsiLambdaExpression)) {
       if (allowMethodCalls || !DebuggerUtils.hasSideEffects(parent)) {
         expression = parent;
       }
