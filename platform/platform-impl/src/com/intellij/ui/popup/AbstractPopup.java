b079c088deaa10f1f94a9691c5e838b570c41089
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2016-01-13 17:37:03 +0300

    IDEA-110029 documentation window disappears immediately

--- platform/platform-impl/src/com/intellij/ui/popup/AbstractPopup.java
+++ platform/platform-impl/src/com/intellij/ui/popup/AbstractPopup.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -555,7 +555,7 @@ public class AbstractPopup implements JBPopup {
     if (preferredBounds.y - adjustedBounds.y <= 0) {
       return preferredLocation;
     }
-    int adjustedY = preferredBounds.y - editor.getLineHeight() * 3 / 2 - preferredSize.height;
+    int adjustedY = preferredBounds.y - editor.getLineHeight() - preferredSize.height;
     if (adjustedY < 0) {
       return preferredLocation;
     }
