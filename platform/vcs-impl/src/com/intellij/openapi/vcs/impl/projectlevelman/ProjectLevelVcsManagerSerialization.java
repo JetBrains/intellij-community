5a6b03b9828ffa4c260a162b8173d14f03ea86bc
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2016-01-18 16:38:28 +0100

    cleanup

--- platform/vcs-impl/src/com/intellij/openapi/vcs/impl/projectlevelman/ProjectLevelVcsManagerSerialization.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/impl/projectlevelman/ProjectLevelVcsManagerSerialization.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,6 @@ import com.intellij.openapi.vcs.VcsShowOptionsSettingImpl;
 import org.jdom.Element;
 import org.jetbrains.annotations.NonNls;
 
-import java.util.List;
 import java.util.Map;
 
 public class ProjectLevelVcsManagerSerialization {
@@ -32,13 +31,6 @@ public class ProjectLevelVcsManagerSerialization {
   @NonNls private static final String VALUE_ATTTIBUTE = "value";
   @NonNls private static final String ID_ATTRIBUTE = "id";
 
-  @NonNls private static final String ELEMENT_MAPPING = "mapping";
-  @NonNls private static final String ATTRIBUTE_DIRECTORY = "directory";
-  @NonNls private static final String ATTRIBUTE_VCS = "vcs";
-  @NonNls private static final String ATTRIBUTE_DEFAULT_PROJECT = "defaultProject";
-  @NonNls private static final String ELEMENT_ROOT_SETTINGS = "rootSettings";
-  @NonNls private static final String ATTRIBUTE_CLASS = "class";
-
   // read-only can be kept here
   private final Map<String, VcsShowConfirmationOption.Value> myReadValue;
 
@@ -46,7 +38,7 @@ public class ProjectLevelVcsManagerSerialization {
     myReadValue = new com.intellij.util.containers.HashMap<String, VcsShowConfirmationOption.Value>();
   }
 
-  private VcsShowOptionsSettingImpl getOrCreateOption(final Map<String, VcsShowOptionsSettingImpl> options, final String actionName) {
+  private static VcsShowOptionsSettingImpl getOrCreateOption(final Map<String, VcsShowOptionsSettingImpl> options, final String actionName) {
     if (! options.containsKey(actionName)) {
       options.put(actionName, new VcsShowOptionsSettingImpl(actionName));
     }
@@ -55,37 +47,26 @@ public class ProjectLevelVcsManagerSerialization {
 
   public void readExternalUtil(final Element element, final OptionsAndConfirmations optionsAndConfirmations) throws InvalidDataException {
     final Map<String, VcsShowOptionsSettingImpl> options = optionsAndConfirmations.getOptions();
-    List subElements = element.getChildren(OPTIONS_SETTING);
-    for (Object o : subElements) {
-      if (o instanceof Element) {
-        final Element subElement = ((Element)o);
-        final String id = subElement.getAttributeValue(ID_ATTRIBUTE);
-        final String value = subElement.getAttributeValue(VALUE_ATTTIBUTE);
-        if (id != null && value != null) {
-          try {
-            final boolean booleanValue = Boolean.valueOf(value).booleanValue();
-            getOrCreateOption(options, id).setValue(booleanValue);
-          }
-          catch (Exception e) {
-            //ignore
-          }
+    for (Element subElement : element.getChildren(OPTIONS_SETTING)) {
+      final String id = subElement.getAttributeValue(ID_ATTRIBUTE);
+      final String value = subElement.getAttributeValue(VALUE_ATTTIBUTE);
+      if (id != null && value != null) {
+        try {
+          getOrCreateOption(options, id).setValue(Boolean.parseBoolean(value));
+        }
+        catch (Exception ignored) {
         }
       }
     }
     myReadValue.clear();
-    subElements = element.getChildren(CONFIRMATIONS_SETTING);
-    for (Object o : subElements) {
-      if (o instanceof Element) {
-        final Element subElement = ((Element)o);
-        final String id = subElement.getAttributeValue(ID_ATTRIBUTE);
-        final String value = subElement.getAttributeValue(VALUE_ATTTIBUTE);
-        if (id != null && value != null) {
-          try {
-            myReadValue.put(id, VcsShowConfirmationOption.Value.fromString(value));
-          }
-          catch (Exception e) {
-            //ignore
-          }
+    for (Element subElement : element.getChildren(CONFIRMATIONS_SETTING)) {
+      final String id = subElement.getAttributeValue(ID_ATTRIBUTE);
+      final String value = subElement.getAttributeValue(VALUE_ATTTIBUTE);
+      if (id != null && value != null) {
+        try {
+          myReadValue.put(id, VcsShowConfirmationOption.Value.fromString(value));
+        }
+        catch (Exception ignored) {
         }
       }
     }
