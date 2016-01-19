e7047ffafe977e5d3938288accbe83dbc4174015
Alexey Kudravtsev <cdr@intellij.com>
2016-01-19 19:10:33 +0300

    changed scheduleAtFixedRate() to scheduleWithFixedDelay() because the former is bad for hibernation

--- java/compiler/impl/src/com/intellij/compiler/server/BuildManager.java
+++ java/compiler/impl/src/com/intellij/compiler/server/BuildManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -342,7 +342,7 @@ public class BuildManager implements Disposable {
       }
     });
 
-    JobScheduler.getScheduler().scheduleAtFixedRate(new Runnable() {
+    JobScheduler.getScheduler().scheduleWithFixedDelay(new Runnable() {
       @Override
       public void run() {
         runCommand(myGCTask);
