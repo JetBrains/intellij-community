a953884dd0a0c1cac4dad9afde190ad6950476da
Egor.Ushakov <egor.ushakov@jetbrains.com>
2016-01-19 15:52:57 +0300

    no need for assert

--- java/debugger/impl/src/com/intellij/debugger/engine/SuspendContextImpl.java
+++ java/debugger/impl/src/com/intellij/debugger/engine/SuspendContextImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -126,11 +126,6 @@ public abstract class SuspendContextImpl extends XSuspendContext implements Susp
   @Override
   @NotNull
   public DebugProcessImpl getDebugProcess() {
-    assertNotResumed();
-    return myDebugProcess;
-  }
-
-  public DebugProcessImpl getDebugProcessNoAssert() {
     return myDebugProcess;
   }
 
