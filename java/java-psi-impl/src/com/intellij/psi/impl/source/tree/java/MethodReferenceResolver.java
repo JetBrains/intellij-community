0d1cca1cd13dd287479b044118e806acf521f2ad
Anna Kozlova <anna.kozlova@jetbrains.com>
2016-01-19 14:40:26 +0100

    inference: force incorporation after reduce

--- java/java-psi-impl/src/com/intellij/psi/impl/source/tree/java/MethodReferenceResolver.java
+++ java/java-psi-impl/src/com/intellij/psi/impl/source/tree/java/MethodReferenceResolver.java
@@ -121,7 +121,7 @@ public class MethodReferenceResolver implements ResolveCache.PolyVariantContextR
                     return psiSubstitutor;
                   }
 
-                  if (!session.repeatInferencePhases(false)) {
+                  if (!session.repeatInferencePhases(true)) {
                     return substitutor;
                   }
 
