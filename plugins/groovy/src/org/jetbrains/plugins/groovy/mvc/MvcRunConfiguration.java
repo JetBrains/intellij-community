b40856e00b81e898dbcbe749c99f3b4ffca5a307
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2016-01-19 18:55:58 +0300

    [mvc] MvcCommand: set vm options outside, include environment variables

--- plugins/groovy/src/org/jetbrains/plugins/groovy/mvc/MvcRunConfiguration.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/mvc/MvcRunConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -304,7 +304,7 @@ public abstract class MvcRunConfiguration extends ModuleBasedConfiguration<RunCo
     }
 
     protected JavaParameters createJavaParametersMVC() throws ExecutionException {
-      MvcCommand cmd = MvcCommand.parse(myCmdLine, vmParams);
+      MvcCommand cmd = MvcCommand.parse(myCmdLine).setVmOptions(vmParams);
 
       final JavaParameters params = myFramework.createJavaParameters(myModule, false, myForTests, depsClasspath, cmd);
 
