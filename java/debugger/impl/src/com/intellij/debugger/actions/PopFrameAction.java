2ca263e1acfd7d929ea19593c02e22e6e5391fae
Egor.Ushakov <egor.ushakov@jetbrains.com>
2016-01-19 18:30:52 +0300

    more correct thread isAtBreakpoint

--- java/debugger/impl/src/com/intellij/debugger/actions/PopFrameAction.java
+++ java/debugger/impl/src/com/intellij/debugger/actions/PopFrameAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -313,7 +313,7 @@ public class PopFrameAction extends DebuggerAction {
     boolean enable = false;
 
     StackFrameProxyImpl proxy = getStackFrameProxy(e);
-    if (proxy != null && !proxy.isBottom() && isAtBreakpoint(e)) {
+    if (proxy != null && !proxy.isBottom() /*&& isAtBreakpoint(e)*/) {
       enable = proxy.getVirtualMachine().canPopFrames();
     }
 
