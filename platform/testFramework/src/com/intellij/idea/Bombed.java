0a089cfe1fa17cb8d8403bda943a9ae07efc7d5f
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2016-01-20 10:34:11 +0100

    [test framework] mandatory @Bombed.user attribute

--- platform/testFramework/src/com/intellij/idea/Bombed.java
+++ platform/testFramework/src/com/intellij/idea/Bombed.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,6 +29,6 @@ public @interface Bombed {
   @JdkConstants.CalendarMonth int month();
   int day();
   int time() default 0;
-  String user() default "unknown, or, rather, Max";
+  String user();
   String description() default "";
 }
