9e82314944aa5331dd19f5d79ea4fa11e6807728
Dmitry Jemerov <yole@jetbrains.com>
2016-01-22 17:01:17 +0100

    handle IndexNotReadyException thrown by PsiClassOwner.getClasses() (IDEA-150618) (cherry picked from commit 31c403f)

--- java/java-impl/src/com/intellij/ide/projectView/impl/ClassesTreeStructureProvider.java
+++ java/java-impl/src/com/intellij/ide/projectView/impl/ClassesTreeStructureProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -75,7 +75,12 @@ public class ClassesTreeStructureProvider implements SelectableTreeStructureProv
           PsiClass[] classes = ApplicationManager.getApplication().runReadAction(new Computable<PsiClass[]>() {
             @Override
             public PsiClass[] compute() {
-              return classOwner.getClasses();
+              try {
+                return classOwner.getClasses();
+              }
+              catch (IndexOutOfBoundsException e) {
+                return PsiClass.EMPTY_ARRAY;
+              }
             }
           });
           if (classes.length == 1 && !(classes[0] instanceof SyntheticElement) &&
