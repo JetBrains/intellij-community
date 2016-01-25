3099a4b7844847e3f0753259a9d2b387da76b0f0
Egor.Ushakov <egor.ushakov@jetbrains.com>
2016-01-25 13:33:18 +0300

    fixed evaluation in groovy

--- java/debugger/impl/src/com/intellij/debugger/impl/DebuggerUtilsEx.java
+++ java/debugger/impl/src/com/intellij/debugger/impl/DebuggerUtilsEx.java
@@ -463,7 +463,12 @@ public abstract class DebuggerUtilsEx extends DebuggerUtils {
   public static CodeFragmentFactory getCodeFragmentFactory(@Nullable PsiElement context, @Nullable FileType fileType) {
     DefaultCodeFragmentFactory defaultFactory = DefaultCodeFragmentFactory.getInstance();
     if (fileType == null) {
-      return defaultFactory;
+      if (context == null) {
+        return defaultFactory;
+      }
+      else {
+        fileType = context.getContainingFile().getFileType();
+      }
     }
     for (CodeFragmentFactory factory : ApplicationManager.getApplication().getExtensions(CodeFragmentFactory.EXTENSION_POINT_NAME)) {
       if (factory != defaultFactory && factory.getFileType().equals(fileType) && factory.isContextAccepted(context)) {
