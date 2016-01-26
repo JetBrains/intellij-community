1c70bcad32fccbcbfe935d1c10a0cfd0cb15b5a8
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2016-01-26 18:56:01 +0300

    [platform] fixes NPE (EA-78417)

--- platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/UpdateChecker.kt
+++ platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/UpdateChecker.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -628,12 +628,14 @@ object UpdateChecker {
 
   @JvmStatic
   fun checkForUpdate(event: IdeaLoggingEvent) {
-    if (!ourHasFailedPlugins && UpdateSettings.getInstance().isCheckNeeded) {
-      val throwable = event.throwable
-      val pluginDescriptor = PluginManager.getPlugin(IdeErrorsDialog.findPluginId(throwable))
-      if (pluginDescriptor != null && !pluginDescriptor.isBundled) {
-        ourHasFailedPlugins = true
-        updateAndShowResult()
+    if (!ourHasFailedPlugins) {
+      val settings = UpdateSettings.getInstance()
+      if (settings != null && settings.isCheckNeeded) {
+        val pluginDescriptor = PluginManager.getPlugin(IdeErrorsDialog.findPluginId(event.throwable))
+        if (pluginDescriptor != null && !pluginDescriptor.isBundled) {
+          ourHasFailedPlugins = true
+          updateAndShowResult()
+        }
       }
     }
   }
