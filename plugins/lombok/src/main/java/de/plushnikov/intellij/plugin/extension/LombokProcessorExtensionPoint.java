a2d751d5a4e6381908da3bc3354814a7449de72f
Michail Plushnikov <michail@plushnikov.de>
2016-01-31 14:45:08 +0100

    changed back id of plugin because it can not be changed for existing plugins...
    GitOrigin-RevId: 314a28196af81a7a524ac7f18e853c36a48091bf

--- plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/extension/LombokProcessorExtensionPoint.java
+++ plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/extension/LombokProcessorExtensionPoint.java
@@ -7,5 +7,5 @@ import de.plushnikov.intellij.plugin.processor.Processor;
  * Date: 21.07.13 Time: 12:54
  */
 public class LombokProcessorExtensionPoint {
-  public static final ExtensionPointName<Processor> EP_NAME = ExtensionPointName.create("de.plushnikov.lombok.processor");
+  public static final ExtensionPointName<Processor> EP_NAME = ExtensionPointName.create("Lombook Plugin.processor");
 }
