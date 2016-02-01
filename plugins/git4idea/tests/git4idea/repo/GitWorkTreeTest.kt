c0f8150025ab080757b2b5352880401d3a6a502c
Max Medvedev <maxim.medvedev@jetbrains.com>
2016-02-01 10:48:42 +0300

    fix compilation

--- plugins/git4idea/tests/git4idea/repo/GitWorkTreeTest.kt
+++ plugins/git4idea/tests/git4idea/repo/GitWorkTreeTest.kt
@@ -59,7 +59,7 @@ class GitWorkTreeTest : GitPlatformTest() {
 
     val branches = myRepo.branches
     val expectedBranches = listOf("master", "feature", "project") // 'project' is created automatically by `git worktree add`
-    assertSameElements(branches.localBranches.map {it.name}, expectedBranches)
+    assertSameElements("", branches.localBranches.map {it.name}, expectedBranches)
     assertBranchHash(masterHead, branches, "master")
     assertBranchHash(featureHead, branches, "feature")
   }
@@ -80,7 +80,7 @@ class GitWorkTreeTest : GitPlatformTest() {
     myRepo.update()
 
     val branches = myRepo.branches
-    assertSameElements(branches.remoteBranches.map {it.nameForLocalOperations}, listOf("origin/master", "origin/feature"))
+    assertSameElements("", branches.remoteBranches.map {it.nameForLocalOperations}, listOf("origin/master", "origin/feature"))
     assertBranchHash(masterHead, branches, "origin/master")
     assertBranchHash(featureHead, branches, "origin/feature")
   }
