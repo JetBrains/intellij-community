6fb36472456e216be22650214d95269e75d18fa8
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2016-02-01 15:54:33 +0300

    IDEA-150943 Strange code folding in a method with lambda

--- platform/platform-impl/src/com/intellij/openapi/editor/ex/FoldingModelEx.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/ex/FoldingModelEx.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -64,4 +64,8 @@ public interface FoldingModelEx extends FoldingModel {
   
   @NotNull
   List<FoldRegion> getGroupedRegions(FoldingGroup group);
+  
+  boolean documentHasBeenChangedExternally();
+  
+  void clearExternalChangeFlag();
 }
