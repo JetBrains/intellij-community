6ae3d3c98c2e21b69b7921c8046b085cb875e04a
andrey zaytsev <andrey.zaytsev@jetbrains.com>
2016-02-02 21:15:24 +0300

    visible range opt for on-air

--- platform/analysis-impl/src/com/intellij/codeInsight/daemon/impl/VisibleHighlightingPassFactory.java
+++ platform/analysis-impl/src/com/intellij/codeInsight/daemon/impl/VisibleHighlightingPassFactory.java
@@ -22,14 +22,11 @@ package com.intellij.codeInsight.daemon.impl;
 import com.intellij.codeHighlighting.Pass;
 import com.intellij.openapi.components.AbstractProjectComponent;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.LogicalPosition;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.ProperTextRange;
 import com.intellij.openapi.util.TextRange;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.awt.*;
 
 public abstract class VisibleHighlightingPassFactory extends AbstractProjectComponent {
   public VisibleHighlightingPassFactory(Project project) {
@@ -38,15 +35,7 @@ public abstract class VisibleHighlightingPassFactory extends AbstractProjectComp
 
   @NotNull
   public static ProperTextRange calculateVisibleRange(@NotNull Editor editor) {
-    Rectangle rect = editor.getScrollingModel().getVisibleArea();
-    LogicalPosition startPosition = editor.xyToLogicalPosition(new Point(rect.x, rect.y));
-
-    int visibleStart = editor.logicalPositionToOffset(startPosition);
-    LogicalPosition endPosition = editor.xyToLogicalPosition(new Point(rect.x + rect.width, rect.y + rect.height));
-
-    int visibleEnd = editor.logicalPositionToOffset(new LogicalPosition(endPosition.line + 1, 0));
-
-    return new ProperTextRange(visibleStart, Math.max(visibleEnd, visibleStart));
+    return VisibleRangeCalculator.SERVICE.getInstance().getVisibleTextRange(editor);
   }
 
   @Nullable
---
+++ platform/analysis-impl/src/com/intellij/codeInsight/daemon/impl/VisibleRangeCalculatorImpl.java
@@ -0,0 +1,24 @@
+package com.intellij.codeInsight.daemon.impl;
+
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.LogicalPosition;
+import com.intellij.openapi.util.ProperTextRange;
+import com.sun.istack.internal.NotNull;
+
+import java.awt.*;
+
+/**
+ * Created by jetzajac on 02/02/16.
+ */
+public class VisibleRangeCalculatorImpl implements VisibleRangeCalculator {
+  @Override
+  @NotNull
+  public ProperTextRange getVisibleTextRange(@NotNull Editor editor) {
+    Rectangle rect = editor.getScrollingModel().getVisibleArea();
+    LogicalPosition startPosition = editor.xyToLogicalPosition(new Point(rect.x, rect.y));
+    int visibleStart = editor.logicalPositionToOffset(startPosition);
+    LogicalPosition endPosition = editor.xyToLogicalPosition(new Point(rect.x + rect.width, rect.y + rect.height));
+    int visibleEnd = editor.logicalPositionToOffset(new LogicalPosition(endPosition.line + 1, 0));
+    return new ProperTextRange(visibleStart, Math.max(visibleEnd, visibleStart));
+  }
+}
