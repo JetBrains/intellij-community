f36107123b1431d0370ffda8391f1c9de88ffc28
nik <Nikolay.Chashnikov@jetbrains.com>
2016-02-02 21:45:29 +0300

    new project wizard tests fixed

--- java/idea-ui/src/com/intellij/ide/projectWizard/ProjectTypeStep.java
+++ java/idea-ui/src/com/intellij/ide/projectWizard/ProjectTypeStep.java
@@ -520,7 +520,7 @@ public class ProjectTypeStep extends ModuleWizardStep implements SettingsStep, D
 
   public void onWizardFinished() throws CommitStepException {
     if (isFrameworksMode()) {
-      boolean ok = myFrameworksPanel.downloadLibraries(myWizard.getContentPanel());
+      boolean ok = myFrameworksPanel.downloadLibraries(myWizard.getContentComponent());
       if (!ok) {
         throw new CommitStepException(null);
       }
