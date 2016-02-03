ff21fd0ea5b087bdb6e09d0253d10ab1373c8d11
Anna Kozlova <anna.kozlova@jetbrains.com>
2016-02-03 16:08:27 +0300

    calculate transitive inference variable's dependencies (IDEA-151170)

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/GraphInferenceHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/GraphInferenceHighlightingTest.java
@@ -371,6 +371,10 @@ public class GraphInferenceHighlightingTest extends LightDaemonAnalyzerTestCase 
     doTest();
   }
 
+  public void testTransitiveInferenceVariableDependencies() throws Exception {
+    doTest();
+  }
+
   private void doTest() throws Exception {
     doTest(false);
   }
