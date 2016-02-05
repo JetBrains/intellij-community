45f4c640e5c379d907b93f0d74009a2d421bb255
Egor.Ushakov <egor.ushakov@jetbrains.com>
2016-02-05 13:13:34 +0300

    fixed incorrect breakpoint inside inner lambdas

--- java/debugger/impl/src/com/intellij/debugger/engine/PositionManagerImpl.java
+++ java/debugger/impl/src/com/intellij/debugger/engine/PositionManagerImpl.java
@@ -261,7 +261,7 @@ public class PositionManagerImpl implements PositionManager, MultiRequestPositio
           PsiFile file = original.getFile();
           int line = original.getLine();
           if (LambdaMethodFilter.isLambdaName(myExpectedMethodName) && myLambdaOrdinal > -1) {
-            List<PsiLambdaExpression> lambdas = DebuggerUtilsEx.collectLambdas(original, false);
+            List<PsiLambdaExpression> lambdas = DebuggerUtilsEx.collectLambdas(original, true);
 
             Document document = PsiDocumentManager.getInstance(file.getProject()).getDocument(file);
             if (document == null || line >= document.getLineCount()) {
