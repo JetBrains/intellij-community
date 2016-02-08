d69cfb17589d6c3477e988889dbc367e3e10100e
Alexander Koshevoy <Alexander.Koshevoy@jetbrains.com>
2016-02-08 14:51:14 +0300

    Test fails of PythonSdkTypeTest after merging python_docker branch fixed

--- python/testSrc/com/jetbrains/python/sdk/PythonSdkTypeTest.java
+++ python/testSrc/com/jetbrains/python/sdk/PythonSdkTypeTest.java
@@ -3,18 +3,22 @@ package com.jetbrains.python.sdk;
 import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.projectRoots.SdkAdditionalData;
 import com.intellij.remote.VagrantBasedCredentialsHolder;
+import com.intellij.remote.ext.CredentialsCase;
+import com.intellij.remote.ext.CredentialsLanguageContribution;
+import com.intellij.testFramework.PlatformLiteFixture;
 import com.jetbrains.python.remote.PyRemoteSdkAdditionalDataBase;
-import junit.framework.TestCase;
 import org.junit.Assert;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 
 /**
  * @author Alexander Koshevoy
  */
-public class PythonSdkTypeTest extends TestCase {
+public class PythonSdkTypeTest extends PlatformLiteFixture {
   @Mock
   private Sdk sdk;
   @Mock
@@ -23,11 +27,14 @@ public class PythonSdkTypeTest extends TestCase {
   private SdkAdditionalData sdkAdditionalData;
   @Mock
   private VagrantBasedCredentialsHolder vagrantBasedCredentialsHolder;
+  @Mock
+  private CredentialsLanguageContribution credentialsLanguageContribution;
 
   @Override
   public void setUp() throws Exception {
     super.setUp();
     MockitoAnnotations.initMocks(this);
+    registerExtension(CredentialsLanguageContribution.EP_NAME, credentialsLanguageContribution);
   }
 
   public void testLocalSdk() throws Exception {
@@ -55,13 +62,9 @@ public class PythonSdkTypeTest extends TestCase {
   }
 
   private static void mockSwitchOnConnectionType(PyRemoteSdkAdditionalDataBase data, final VagrantBasedCredentialsHolder credentials) {
-    // TODO: fix
-    //Mockito.doAnswer(new Answer() {
-    //  @Override
-    //  public Void answer(InvocationOnMock invocation) {
-    //    ((RemoteSdkConnectionAcceptor)invocation.getArguments()[0]).vagrant(credentials);
-    //    return null;
-    //  }
-    //}).when(data).switchOnConnectionType(any(RemoteSdkConnectionAcceptor.class));
+    Mockito.doAnswer(invocation -> {
+      ((CredentialsCase.Vagrant)invocation.getArguments()[0]).process(credentials);
+      return null;
+    }).when(data).switchOnConnectionType(any(CredentialsCase.Vagrant.class));
   }
 }
