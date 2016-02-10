475d29486884890d5e4d0a830003a07713914d39
Bas Leijdekkers <basleijdekkers@gmail.com>
2016-02-10 12:19:58 +0100

    make IG test light

--- plugins/InspectionGadgets/test/com/siyeh/igtest/logging/log_statement_guarded_by_log_condition/LogStatementGuardedByLogCondition.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/logging/log_statement_guarded_by_log_condition/LogStatementGuardedByLogCondition.java
@@ -17,7 +17,7 @@ public class LogStatementGuardedByLogCondition {
     void unguarded(Object object) {
         if (true) {
             if (true) {
-                LOG.debug("log log log " + object);
+                LOG.<warning descr="'debug()' logging calls not guarded by log condition">debug</warning>("log log log " + object);
             }
         }
     }
@@ -26,14 +26,14 @@ public class LogStatementGuardedByLogCondition {
         if (LOG.isTrace()) {
             if (true) {
                 if (true) {
-                    LOG.debug("really expensive logging" + object);
+                    LOG.<warning descr="'debug()' logging calls not guarded by log condition">debug</warning>("really expensive logging" + object);
                 }
             }
         }
     }
 
     void alternativeDebugMethodSignature(int i) {
-        LOG.debug(i, "asdfasdf");
+        LOG.<warning descr="'debug()' logging calls not guarded by log condition">debug</warning>(i, "asdfasdf");
     }
 
   static class Logger {
