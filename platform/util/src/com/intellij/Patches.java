5b2bd241de53d8ae095ba9d5211f2e20693c1072
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2016-02-11 17:24:14 +0100

    Cleanup (getting rid of useless warnings)

--- platform/util/src/com/intellij/Patches.java
+++ platform/util/src/com/intellij/Patches.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -110,13 +110,13 @@ public class Patches {
    * Marker field to find all usages of the reflective access to JDK 7-specific methods
    * which need to be changed when migrated to JDK 7
    */
-  public static final boolean USE_REFLECTION_TO_ACCESS_JDK7 = true;
+  public static final boolean USE_REFLECTION_TO_ACCESS_JDK7 = Boolean.valueOf(true);
 
   /**
    * Marker field to find all usages of the reflective access to JDK 7-specific methods
    * which need to be changed when migrated to JDK 8
    */
-  public static final boolean USE_REFLECTION_TO_ACCESS_JDK8 = true;
+  public static final boolean USE_REFLECTION_TO_ACCESS_JDK8 = Boolean.valueOf(true);
 
   /**
    * AtomicIntegerFieldUpdater does not work when SecurityManager is installed
