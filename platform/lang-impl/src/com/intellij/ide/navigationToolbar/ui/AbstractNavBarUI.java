5d849241c8645c70260a23c6d09f2442d6553428
Egor.Ushakov <egor.ushakov@jetbrains.com>
2016-02-11 17:47:12 +0300

    fixed run toolbar vertical alignment (was leaning to the bottom)

--- platform/lang-impl/src/com/intellij/ide/navigationToolbar/ui/AbstractNavBarUI.java
+++ platform/lang-impl/src/com/intellij/ide/navigationToolbar/ui/AbstractNavBarUI.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -290,11 +290,11 @@ public abstract class AbstractNavBarUI implements NavBarUI {
 
   @Override
   public Insets getWrapperPanelInsets(Insets insets) {
-    final JBInsets result = JBUI.insets(insets);
-    if (shouldPaintWrapperPanel()) {
-      result.top += JBUI.scale(1);
-    }
-    return result;
+    //final JBInsets result = JBUI.insets(insets);
+    //if (shouldPaintWrapperPanel()) {
+    //  result.top += JBUI.scale(1);
+    //}
+    return insets;
   }
 
   private static boolean shouldPaintWrapperPanel() {
