09079ea6ae21f923cc5cd05f8ec8d70ed101a9a3
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2016-02-12 17:12:11 +0300

    inspection toolwindow: disposed editor call fixed

--- platform/usageView/src/com/intellij/usages/impl/UsagePreviewPanel.java
+++ platform/usageView/src/com/intellij/usages/impl/UsagePreviewPanel.java
@@ -122,7 +122,7 @@ public class UsagePreviewPanel extends UsageContextPanelBase {
       public void run() {
         if (project.isDisposed()) return;
         //noinspection ConstantConditions
-        if (editor1 == editor) {
+        if (editor1 == editor && !editor.isDisposed()) {
           doHighlight(infos, editor, project);
         }
       }
