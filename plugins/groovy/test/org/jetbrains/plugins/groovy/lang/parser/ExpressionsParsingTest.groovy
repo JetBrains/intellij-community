553f612634eff74b1dd06ecfff1d8b7ee9653092
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2016-02-15 22:38:03 +0300

    [groovy] parser: do not rollback marker and leave parsed label as is if no colon ahead (IDEA-151354)

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/parser/ExpressionsParsingTest.groovy
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/parser/ExpressionsParsingTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -835,4 +835,6 @@ Groovy script
   PsiElement(new line)('\\n')
 ''')
   }
+
+  void testspecial$mapHang() { doTest() }
 }
