ecdd7a309699a69a62fe326b61859188b928bfde
Anna Kozlova <anna.kozlova@jetbrains.com>
2016-02-17 16:26:28 +0100

    inference: check glb with captured wildcard: if not assignable check if it is an interface
    (cherry picked from commit 0ac147ad950068b0f82417854d6d8d2687b7d2c2)

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/GraphInferenceHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/GraphInferenceHighlightingTest.java
@@ -387,6 +387,10 @@ public class GraphInferenceHighlightingTest extends LightDaemonAnalyzerTestCase 
     doTest();
   }
 
+  public void testGlbValidityWithCapturedWildcards() throws Exception {
+    doTest();
+  }
+
   private void doTest() throws Exception {
     doTest(false);
   }
