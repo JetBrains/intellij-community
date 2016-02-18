59e2a1aeec78fe7a57273ba4d18b4995fc8967e3
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2016-02-18 12:35:40 +0300

    IDEA-150906 Preliminary implementation of MacScrollBarUI

--- platform/platform-api/src/com/intellij/ui/components/JBScrollPane.java
+++ platform/platform-api/src/com/intellij/ui/components/JBScrollPane.java
@@ -64,6 +64,12 @@ public class JBScrollPane extends JScrollPane {
   @Deprecated
   public static final RegionPainter<Float> THUMB_DARK_PAINTER = new ThumbPainter(.35f, .25f, Gray.x80, Gray.x94);
 
+  @Deprecated
+  public static final RegionPainter<Float> MAC_THUMB_PAINTER = new RoundThumbPainter(2, .5f, .4f, Gray.x99);
+
+  @Deprecated
+  public static final RegionPainter<Float> MAC_THUMB_DARK_PAINTER = new RoundThumbPainter(2, .15f, .25f, Gray.x80);
+
   private int myViewportBorderWidth = -1;
   private boolean myHasOverlayScrollbars;
   private volatile boolean myBackgroundRequested; // avoid cyclic references
@@ -315,7 +321,9 @@ public class JBScrollPane extends JScrollPane {
       if (ui instanceof AbstractScrollBarUI) return;
       setUI(!SystemInfo.isMac && Registry.is("ide.scroll.new.layout")
             ? new DefaultScrollBarUI()
-            : ButtonlessScrollBarUI.createNormal());
+            : SystemInfo.isMac && Registry.is("mac.scroll.new.ui")
+              ? new MacScrollBarUI()
+              : ButtonlessScrollBarUI.createNormal());
     }
 
     @Override
@@ -801,7 +809,6 @@ public class JBScrollPane extends JScrollPane {
     }
 
     void paint(Graphics2D g, int x, int y, int width, int height) {
-      g.setColor(myFillColor);
       g.fillRect(x, y, width, height);
     }
 
@@ -814,10 +821,33 @@ public class JBScrollPane extends JScrollPane {
           g.setComposite(alpha < 1
                          ? newComposite(alpha)
                          : AlphaComposite.SrcOver);
+          g.setColor(myFillColor);
           paint(g, x, y, width, height);
           g.setComposite(old);
         }
       }
+    }
+  }
+
+  private static class RoundThumbPainter extends AlphaPainter {
+    private final int myBorder;
+
+    private RoundThumbPainter(int border, float base, float delta, Color fill) {
+      super(base, delta, fill);
+      myBorder = border;
+    }
+
+    @Override
+    void paint(Graphics2D g, int x, int y, int width, int height) {
+      Object old = g.getRenderingHint(RenderingHints.KEY_ANTIALIASING);
+      g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+      width -= myBorder + myBorder;
+      height -= myBorder + myBorder;
+
+      int arc = Math.min(width, height);
+      g.fillRoundRect(x + myBorder, y + myBorder, width, height, arc, arc);
+      g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, old);
     }
   }
 
