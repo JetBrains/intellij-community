ae2fd3287b1815b8d5262250ecfd5c8dbc58eb1e
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2016-02-19 15:24:36 +0300

    [dbe] move empty text painter a bit higher
    (cherry picked from commit 57a51c6a08b4fc63445dc748bc69a21aecaf6524)

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/EditorEmptyTextPainter.java
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/EditorEmptyTextPainter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -53,10 +53,14 @@ public class EditorEmptyTextPainter {
       public Couple<Integer> fun(Integer width, Integer height) {
         Dimension s = splitters.getSize();
         int w = (s.width - width) / 2;
-        int h = s.height * 3 / 8; // fix vertical position @ golden ratio
+        int h = (int)(s.height * heightRatio());
         return Couple.of(w, h);
       }
     });
+  }
+
+  protected double heightRatio() {
+    return 0.375; // fix vertical position @ golden ratio 
   }
 
   protected void advertiseActions(@NotNull JComponent splitters, @NotNull UIUtil.TextPainter painter) {
