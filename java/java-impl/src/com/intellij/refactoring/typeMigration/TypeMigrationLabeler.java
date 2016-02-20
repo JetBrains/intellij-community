549a86bcbcea9539c70aa7cb91d6bf72d71e657c
Dmitry Batkovich <batya239@gmail.com>
2016-02-21 00:19:06 +0300

    guava type migration: consider any PsiClassType (not only PsiClassReferenceType) while collecting type parameters from migration type

--- java/java-impl/src/com/intellij/refactoring/typeMigration/TypeMigrationLabeler.java
+++ java/java-impl/src/com/intellij/refactoring/typeMigration/TypeMigrationLabeler.java
@@ -540,7 +540,7 @@ public class TypeMigrationLabeler {
 
     if (!userDefinedType) {
       final Set<PsiTypeParameter> collector;
-      if (type instanceof PsiClassReferenceType) {
+      if (type instanceof PsiClassType) {
         collector = type.accept(new PsiExtendedTypeVisitor<Set<PsiTypeParameter>>() {
           private final Set<PsiTypeParameter> myResult = new HashSet<PsiTypeParameter>();
 
