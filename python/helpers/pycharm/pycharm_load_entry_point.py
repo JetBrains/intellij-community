63274ab8e8173163fa1b83cee64b01dc7599e2fa
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2016-02-25 16:02:24 +0300

    fixed indentation in load_entry_point (cherry picked from commit 57c227a)

--- python/helpers/pycharm/pycharm_load_entry_point.py
+++ python/helpers/pycharm/pycharm_load_entry_point.py
@@ -5,8 +5,8 @@ if __name__ == '__main__':
   dist = os.environ.get("PYCHARM_EP_DIST")
   name = os.environ.get("PYCHARM_EP_NAME")
   if dist == "ipython" and name == "ipython":
-      from IPython import start_ipython
-      f = start_ipython
-    else:
-      f = load_entry_point(dist, "console_scripts", name)
+    from IPython import start_ipython
+    f = start_ipython
+  else:
+    f = load_entry_point(dist, "console_scripts", name)
   sys.exit(f())
