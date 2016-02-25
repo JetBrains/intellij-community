f1ab9a9a91df365d8f8684d455b468ac1a291a78
Anna Kozlova <anna.kozlova@jetbrains.com>
2016-02-25 16:12:25 +0100

    bundle SceneBuilderKit

--- plugins/javaFX/src/org/jetbrains/plugins/javaFX/sceneBuilder/SceneBuilderKitWrapper.java
+++ plugins/javaFX/src/org/jetbrains/plugins/javaFX/sceneBuilder/SceneBuilderKitWrapper.java
@@ -1,14 +1,8 @@
 package org.jetbrains.plugins.javaFX.sceneBuilder;
 
-import com.oracle.javafx.scenebuilder.kit.editor.drag.source.AbstractDragSource;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.input.DataFormat;
 
-import java.lang.reflect.Field;
 import java.net.URL;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
 
 /**
  * @author Alexander Lobas
