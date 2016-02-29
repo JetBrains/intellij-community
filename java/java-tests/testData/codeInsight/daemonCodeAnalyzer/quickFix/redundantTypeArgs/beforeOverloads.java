0e606219bacdbaa78198d67c045d7c997d741a35
Anna Kozlova <anna.kozlova@jetbrains.com>
2016-02-29 19:34:38 +0100

    testdata for IDEA-152332

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/redundantTypeArgs/beforeOverloads.java
@@ -0,0 +1,15 @@
+// "Remove explicit type arguments" "false"
+class Test {
+
+  public void valueOfPasses() {
+    assertEquals("a string", String.valueOf(this.<Obj<caret>ect>someProperty()));
+  }
+
+  public static void assertEquals(Object expected, Object actual) {}
+
+  public <T> T someProperty() {
+    //noinspection unchecked
+    return (T) "a string";
+  }
+
+}
