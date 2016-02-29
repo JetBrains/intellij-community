3637c3a87bb76417d900cd58babf7e6b1c91438d
Bas Leijdekkers <basleijdekkers@gmail.com>
2016-02-29 21:00:14 +0100

    IG: fix test (cherry picked from commit ca6dc29)

--- plugins/InspectionGadgets/testsrc/com/siyeh/ig/bugs/OptionalGetWithoutIsPresentInspectionTest.java
+++ plugins/InspectionGadgets/testsrc/com/siyeh/ig/bugs/OptionalGetWithoutIsPresentInspectionTest.java
@@ -89,6 +89,16 @@ public class OptionalGetWithoutIsPresentInspectionTest extends LightInspectionTe
       "  public double getAsDouble() {" +
       "    return 0.0;" +
       "  }" +
+      "}",
+
+      "package org.junit;" +
+      "public class Assert {" +
+      "  public static void assertTrue(boolean b) {}" +
+      "}",
+
+      "package org.testng;" +
+      "public class Assert {" +
+      "  public static void assertTrue(boolean b) {}" +
       "}"
     };
   }
