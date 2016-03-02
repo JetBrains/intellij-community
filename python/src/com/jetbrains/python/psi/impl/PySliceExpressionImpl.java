b33ab7a46943d9fd3667bee00047169f8820c77e
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2016-03-02 15:25:37 +0300

    PY-18560 Fixed: wrong inferred type for result of __getitem__ by square brackets with slice
    Fix calculating type of slice expression: keep tuples and collections as is and resolve __getitem__ method for other class types

--- python/src/com/jetbrains/python/psi/impl/PySliceExpressionImpl.java
+++ python/src/com/jetbrains/python/psi/impl/PySliceExpressionImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,16 +16,20 @@
 package com.jetbrains.python.psi.impl;
 
 import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.jetbrains.python.PyNames;
 import com.jetbrains.python.PythonDialectsTokenSetProvider;
-import com.jetbrains.python.psi.PyExpression;
-import com.jetbrains.python.psi.PySliceExpression;
-import com.jetbrains.python.psi.PySliceItem;
-import com.jetbrains.python.psi.types.PyTupleType;
-import com.jetbrains.python.psi.types.PyType;
-import com.jetbrains.python.psi.types.TypeEvalContext;
+import com.jetbrains.python.psi.*;
+import com.jetbrains.python.psi.resolve.PyResolveContext;
+import com.jetbrains.python.psi.resolve.RatedResolveResult;
+import com.jetbrains.python.psi.types.*;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author yole
@@ -39,11 +43,38 @@ public class PySliceExpressionImpl extends PyElementImpl implements PySliceExpre
   @Override
   public PyType getType(@NotNull TypeEvalContext context, @NotNull TypeEvalContext.Key key) {
     final PyType type = context.getType(getOperand());
+
     // TODO: Currently we don't evaluate the static range of the slice, so we have to return a generic tuple type without elements
     if (type instanceof PyTupleType) {
       return PyBuiltinCache.getInstance(this).getTupleType();
     }
-    return type;
+
+    if (type instanceof PyCollectionType) {
+      return type;
+    }
+
+    if (type instanceof PyClassType) {
+      final List<? extends RatedResolveResult> resolveResults = type.resolveMember(
+        PyNames.GETITEM,
+        null,
+        AccessDirection.READ,
+        PyResolveContext.noImplicits().withTypeEvalContext(context)
+      );
+
+      if (resolveResults != null) {
+        final List<PyType> types = new ArrayList<>();
+
+        for (RatedResolveResult resolveResult : resolveResults) {
+          types.addAll(
+            getPossibleReturnTypes(resolveResult.getElement(), context)
+          );
+        }
+
+        return PyUnionType.union(types);
+      }
+    }
+
+    return null;
   }
 
   @NotNull
@@ -56,5 +87,33 @@ public class PySliceExpressionImpl extends PyElementImpl implements PySliceExpre
   @Override
   public PySliceItem getSliceItem() {
     return PsiTreeUtil.getChildOfType(this, PySliceItem.class);
+  }
+
+  @NotNull
+  private static List<PyType> getPossibleReturnTypes(@Nullable PsiElement element, @NotNull TypeEvalContext context) {
+    final List<PyType> result = new ArrayList<PyType>();
+
+    if (element instanceof PyTypedElement) {
+      final PyType elementType = context.getType((PyTypedElement)element);
+
+      result.addAll(getPossibleReturnTypes(elementType, context));
+
+      if (elementType instanceof PyUnionType) {
+        for (PyType type : ((PyUnionType)elementType).getMembers()) {
+          result.addAll(getPossibleReturnTypes(type, context));
+        }
+      }
+    }
+
+    return result;
+  }
+
+  @NotNull
+  private static List<PyType> getPossibleReturnTypes(@Nullable PyType type, @NotNull TypeEvalContext context) {
+    if (type instanceof PyCallableType) {
+      return Collections.singletonList(((PyCallableType)type).getReturnType(context));
+    }
+
+    return Collections.emptyList();
   }
 }
