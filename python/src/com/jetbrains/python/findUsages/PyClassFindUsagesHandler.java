c47da586b7a446c33b4ff053901a3f4f5775349b
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2016-03-03 17:50:17 +0100

    Cleanup (typo)

--- python/src/com/jetbrains/python/findUsages/PyClassFindUsagesHandler.java
+++ python/src/com/jetbrains/python/findUsages/PyClassFindUsagesHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -47,7 +47,7 @@ public class PyClassFindUsagesHandler extends FindUsagesHandler {
   }
 
   @Override
-  protected boolean isSearchForTextOccurencesAvailable(@NotNull PsiElement psiElement, boolean isSingleFile) {
+  protected boolean isSearchForTextOccurrencesAvailable(@NotNull PsiElement psiElement, boolean isSingleFile) {
     return true;
   }
 
