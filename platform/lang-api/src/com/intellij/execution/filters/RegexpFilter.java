6f9a8278207ebc9f87c67e63248a3dd30774928a
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2016-03-05 12:32:01 +0300

    speedup gradle console filter, fix IDEA-150077
    – cheap check if the string starts with / or {Alpha}:\
    – check if the project has any gradle module
    – wrap pattern matching with the bombed char sequence

--- platform/lang-api/src/com/intellij/execution/filters/RegexpFilter.java
+++ platform/lang-api/src/com/intellij/execution/filters/RegexpFilter.java
@@ -20,6 +20,7 @@ import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
@@ -139,7 +140,7 @@ public class RegexpFilter implements Filter {
 
   @Override
   public Result applyFilter(String line, int entireLength) {
-    Matcher matcher = myPattern.matcher(StringUtil.newBombedCharSequence(line, 1000));
+    Matcher matcher = myPattern.matcher(oneSecondBomb(line));
     if (!matcher.find()) {
       return null;
     }
@@ -175,6 +176,11 @@ public class RegexpFilter implements Filter {
     final int highlightEndOffset = highlightStartOffset + filePath.length();
     final HyperlinkInfo info = createOpenFileHyperlink(filePath, line1, column);
     return new Result(highlightStartOffset, highlightEndOffset, info);
+  }
+
+  @NotNull
+  protected static CharSequence oneSecondBomb(@NotNull String line) {
+    return StringUtil.newBombedCharSequence(line, 1000);
   }
 
   @Nullable
