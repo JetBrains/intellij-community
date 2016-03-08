20565cd7f969c6dcaabf529d7c1530591fa73ddd
peter <peter@jetbrains.com>
2016-03-08 20:38:31 +0100

    invoke saveAll under a transaction

--- java/compiler/impl/src/com/intellij/compiler/server/BuildManager.java
+++ java/compiler/impl/src/com/intellij/compiler/server/BuildManager.java
@@ -36,10 +36,7 @@ import com.intellij.ide.PowerSaveMode;
 import com.intellij.ide.file.BatchFileChangeListener;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.application.Application;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.application.ModalityState;
-import com.intellij.openapi.application.PathManager;
+import com.intellij.openapi.application.*;
 import com.intellij.openapi.compiler.CompilationStatusListener;
 import com.intellij.openapi.compiler.CompileContext;
 import com.intellij.openapi.compiler.CompilerPaths;
@@ -77,7 +74,6 @@ import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
 import com.intellij.openapi.vfs.newvfs.impl.FileNameCache;
 import com.intellij.openapi.wm.IdeFrame;
 import com.intellij.util.*;
-import com.intellij.util.concurrency.Semaphore;
 import com.intellij.util.concurrency.SequentialTaskExecutor;
 import com.intellij.util.containers.IntArrayList;
 import com.intellij.util.io.storage.HeavyProcessLatch;
@@ -105,7 +101,6 @@ import org.jetbrains.jps.cmdline.ClasspathBootstrap;
 import org.jetbrains.jps.incremental.Utils;
 import org.jetbrains.jps.model.serialization.JpsGlobalLoader;
 
-import javax.swing.*;
 import javax.tools.*;
 import java.awt.*;
 import java.io.File;
@@ -186,25 +181,11 @@ public class BuildManager implements Disposable {
       return Registry.intValue("compiler.document.save.trigger.delay");
     }
 
-    private final Semaphore mySemaphore = new Semaphore();
-    private final Runnable mySaveDocsRunnable = new Runnable() {
-      @Override
-      public void run() {
-        try {
-          ((FileDocumentManagerImpl)FileDocumentManager.getInstance()).saveAllDocuments(false);
-        }
-        finally {
-          mySemaphore.up();
-        }
-      }
-    };
-
     @Override
     public void runTask() {
       if (shouldSaveDocuments()) {
-        mySemaphore.down();
-        ApplicationManager.getApplication().invokeLater(mySaveDocsRunnable, ModalityState.NON_MODAL);
-        mySemaphore.waitFor();
+        TransactionGuard.getInstance().submitTransactionAndWait(TransactionKind.ANY_CHANGE, () ->
+          ((FileDocumentManagerImpl)FileDocumentManager.getInstance()).saveAllDocuments(false));
       }
     }
 
@@ -772,12 +753,7 @@ public class BuildManager implements Disposable {
                       // ensure project model is saved on disk, so that automake sees the latest model state.
                       // For ordinary make all project, app settings and unsaved docs are always saved before build starts.
                       try {
-                        SwingUtilities.invokeAndWait(new Runnable() {
-                          @Override
-                          public void run() {
-                            project.save();
-                          }
-                        });
+                        TransactionGuard.getInstance().submitTransactionAndWait(TransactionKind.ANY_CHANGE, project::save);
                       }
                       catch (Throwable e) {
                         LOG.info(e);
