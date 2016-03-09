77a63a0eeade238f4e15441e51c65a2c15136ccc
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2016-03-09 13:28:11 +0100

    [java] @see tag ref verification (IDEA-97635)

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/JavadocHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/JavadocHighlightingTest.java
@@ -67,6 +67,7 @@ public class JavadocHighlightingTest extends LightDaemonAnalyzerTestCase {
   public void testSee5() { doTest(); }
   public void testLinkToItself() { doTest(); }
   public void testSeeConstants() { doTest(); }
+  public void testSeeNonRefs() { doTest(); }
   public void testReturn0() { doTest(); }
   public void testException0() { doTest(); }
   public void testException1() { doTest(); }
