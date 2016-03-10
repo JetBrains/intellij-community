eaffa2c5d7535fb8f4d684659d463cc147b1dec5
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2016-03-10 14:08:47 +0300

    use proper thread to show url dialog

--- python/ipnb/src/org/jetbrains/plugins/ipnb/configuration/IpnbConnectionManager.java
+++ python/ipnb/src/org/jetbrains/plugins/ipnb/configuration/IpnbConnectionManager.java
@@ -124,27 +124,32 @@ public final class IpnbConnectionManager implements ProjectComponent {
 
   @Nullable
   public static String showDialogUrl(@NotNull final String initialUrl) {
-    final String url = Messages.showInputDialog("Jupyter Notebook URL:", "Start Jupyter Notebook", null, initialUrl,
-                                                new InputValidator() {
-                                                  @Override
-                                                  public boolean checkInput(String inputString) {
-                                                    try {
-                                                      final URI uri = new URI(inputString);
-                                                      if (uri.getPort() == -1 || StringUtil.isEmptyOrSpaces(uri.getHost())) {
-                                                        return false;
-                                                      }
-                                                    }
-                                                    catch (URISyntaxException e) {
-                                                      return false;
-                                                    }
-                                                    return !inputString.isEmpty();
-                                                  }
+    final String url = UIUtil.invokeAndWaitIfNeeded(new Computable<String>() {
+      @Override
+      public String compute() {
+        return Messages.showInputDialog("Jupyter Notebook URL:", "Start Jupyter Notebook", null, initialUrl,
+                                 new InputValidator() {
+                                   @Override
+                                   public boolean checkInput(String inputString) {
+                                     try {
+                                       final URI uri = new URI(inputString);
+                                       if (uri.getPort() == -1 || StringUtil.isEmptyOrSpaces(uri.getHost())) {
+                                         return false;
+                                       }
+                                     }
+                                     catch (URISyntaxException e) {
+                                       return false;
+                                     }
+                                     return !inputString.isEmpty();
+                                   }
 
-                                                  @Override
-                                                  public boolean canClose(String inputString) {
-                                                    return true;
-                                                  }
-                                                });
+                                   @Override
+                                   public boolean canClose(String inputString) {
+                                     return true;
+                                   }
+                                 });
+      }
+    });
     return url == null ? null : StringUtil.trimEnd(url, "/");
   }
 
