f50c6e4d10dfd5a3b2947204eb4786e11a869350
Christopher Carpenter <intermediatelanguage@gmail.com>
2016-03-10 12:14:40 -0500

    Append annotations before comments about the class/method/field that it is being applied to. For example, the annotations should go before the message saying that the field is synthetic or that it was renamed.

--- plugins/java-decompiler/engine/src/org/jetbrains/java/decompiler/main/ClassWriter.java
+++ plugins/java-decompiler/engine/src/org/jetbrains/java/decompiler/main/ClassWriter.java
@@ -299,6 +299,8 @@ public class ClassWriter {
       appendDeprecation(buffer, indent);
     }
 
+    appendAnnotations(buffer, cl, indent);
+
     if (interceptor != null) {
       String oldName = interceptor.getOldName(cl.qualifiedName);
       appendRenameComment(buffer, oldName, MType.CLASS, indent);
@@ -307,8 +309,6 @@ public class ClassWriter {
     if (isSynthetic) {
       appendComment(buffer, "synthetic class", indent);
     }
-
-    appendAnnotations(buffer, cl, indent);
 
     buffer.appendIndent(indent);
 
@@ -395,6 +395,8 @@ public class ClassWriter {
       appendDeprecation(buffer, indent);
     }
 
+    appendAnnotations(buffer, fd, indent);
+
     if (interceptor != null) {
       String oldName = interceptor.getOldName(cl.qualifiedName + " " + fd.getName() + " " + fd.getDescriptor());
       appendRenameComment(buffer, oldName, MType.FIELD, indent);
@@ -403,8 +405,6 @@ public class ClassWriter {
     if (fd.isSynthetic()) {
       appendComment(buffer, "synthetic field", indent);
     }
-
-    appendAnnotations(buffer, fd, indent);
 
     buffer.appendIndent(indent);
 
@@ -611,6 +611,8 @@ public class ClassWriter {
         appendDeprecation(buffer, indent);
       }
 
+      appendAnnotations(buffer, mt, indent);
+
       if (interceptor != null) {
         String oldName = interceptor.getOldName(cl.qualifiedName + " " + mt.getName() + " " + mt.getDescriptor());
         appendRenameComment(buffer, oldName, MType.METHOD, indent);
@@ -624,8 +626,6 @@ public class ClassWriter {
       if (isBridge) {
         appendComment(buffer, "bridge method", indent);
       }
-
-      appendAnnotations(buffer, mt, indent);
 
       buffer.appendIndent(indent);
 
