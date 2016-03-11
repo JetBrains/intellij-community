516019f2bf78e2f9ebdf3b205f786aa72ac428a9
Natalia Ukhorskaya <natalia.ukhorskaya@jetbrains.com>
2016-03-11 15:59:29 +0300

    Refactoring: rename android-compiler-plugin to android-extensions-compiler
    GitOrigin-RevId: 9e4b2d90e565381466e10b9984cca86df0f90e2c

--- plugins/kotlin/compiler-plugins/android-compiler-plugin/testData/codegen/android/FakeActivity.kt
+++
@@ -1,8 +0,0 @@
-package android.app
-
-import android.view.View
-import android.content.Context
-
-open class Activity: Context {
-    open fun findViewById(id: Int): View? = null
-}
--- plugins/kotlin/compiler-plugins/android-compiler-plugin/testData/codegen/android/FakeView.kt
+++
@@ -1,7 +0,0 @@
-package android.view
-
-import android.content.Context
-
-open class View(ctx: Context) {
-    open fun findViewById(id: Int): View? = null
-}
