e1df0defde9b7ad18d6bf937d3281dc2ac0a5236
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2016-03-15 22:00:37 +0300

    [groovy] tests: make setUp/tearDown public

--- plugins/groovy/test/org/jetbrains/plugins/groovy/LightGroovyTestCase.groovy
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/LightGroovyTestCase.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,6 +32,16 @@ public abstract class LightGroovyTestCase extends LightCodeInsightFixtureTestCas
 
   JavaCodeInsightTestFixture getFixture() {
     myFixture
+  }
+
+  @Override
+  void setUp() throws Exception {
+    super.setUp()
+  }
+
+  @Override
+  void tearDown() throws Exception {
+    super.tearDown()
   }
 
   @Override
