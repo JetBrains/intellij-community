654df4a206289f6c7df2b71de43c4ae2c3ef6e29
Bas Leijdekkers <basleijdekkers@gmail.com>
2016-03-17 17:29:25 +0100

    IG: remove quick fix (IDEA-153124)

--- plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/bugs/StringConcatenationInFormatCallInspection.java
+++ plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/bugs/StringConcatenationInFormatCallInspection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2012 Bas Leijdekkers
+ * Copyright 2010-2016 Bas Leijdekkers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,15 +15,10 @@
  */
 package com.siyeh.ig.bugs;
 
-import com.intellij.codeInspection.ProblemDescriptor;
-import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
-import com.intellij.util.IncorrectOperationException;
 import com.siyeh.InspectionGadgetsBundle;
 import com.siyeh.ig.BaseInspection;
 import com.siyeh.ig.BaseInspectionVisitor;
-import com.siyeh.ig.InspectionGadgetsFix;
-import com.siyeh.ig.PsiReplacementUtil;
 import com.siyeh.ig.psiutils.ExpressionUtils;
 import com.siyeh.ig.psiutils.FormatUtils;
 import org.jetbrains.annotations.Nls;
@@ -42,67 +37,6 @@ public class StringConcatenationInFormatCallInspection extends BaseInspection {
   @Override
   protected String buildErrorString(Object... infos) {
     return InspectionGadgetsBundle.message("string.concatenation.in.format.call.problem.descriptor");
-  }
-
-  @Override
-  protected InspectionGadgetsFix buildFix(Object... infos) {
-    return new StringConcatenationInFormatCallFix(((Boolean)infos[0]).booleanValue());
-  }
-
-  private static class StringConcatenationInFormatCallFix extends InspectionGadgetsFix {
-
-
-    private final boolean myPlural;
-
-    public StringConcatenationInFormatCallFix(boolean plural) {
-      myPlural = plural;
-    }
-
-    @Override
-    @NotNull
-    public String getName() {
-      if (myPlural) {
-        return InspectionGadgetsBundle.message("string.concatenation.in.format.call.plural.quickfix");
-      }
-      else {
-        return InspectionGadgetsBundle.message("string.concatenation.in.format.call.quickfix");
-      }
-    }
-
-    @NotNull
-    @Override
-    public String getFamilyName() {
-      return InspectionGadgetsBundle.message("string.concatenation.in.format.call.plural.quickfix"); 
-    }
-
-    @Override
-    protected void doFix(Project project, ProblemDescriptor descriptor) throws IncorrectOperationException {
-      final PsiElement element = descriptor.getPsiElement().getParent().getParent();
-      if (!(element instanceof PsiMethodCallExpression)) {
-        return;
-      }
-      final PsiMethodCallExpression methodCallExpression = (PsiMethodCallExpression)element;
-      final PsiExpressionList argumentList = methodCallExpression.getArgumentList();
-      final PsiExpression formatArgument = FormatUtils.getFormatArgument(argumentList);
-      if (!(formatArgument instanceof PsiPolyadicExpression)) {
-        return;
-      }
-      final PsiPolyadicExpression polyadicExpression = (PsiPolyadicExpression)formatArgument;
-      final StringBuilder newExpression = new StringBuilder();
-      final PsiExpression[] operands = polyadicExpression.getOperands();
-      for (PsiExpression operand : operands) {
-        if (operand instanceof PsiReferenceExpression) {
-          argumentList.add(operand);
-          continue;
-        }
-        final PsiJavaToken token = polyadicExpression.getTokenBeforeOperand(operand);
-        if (token != null) {
-          newExpression.append(token.getText());
-        }
-        newExpression.append(operand.getText());
-      }
-      PsiReplacementUtil.replaceExpression(polyadicExpression, newExpression.toString());
-    }
   }
 
   @Override
@@ -141,7 +75,7 @@ public class StringConcatenationInFormatCallInspection extends BaseInspection {
       if (count == 0) {
         return;
       }
-      registerMethodCallError(expression, Boolean.valueOf(count > 1));
+      registerMethodCallError(expression);
     }
   }
 }
