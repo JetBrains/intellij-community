ccbe6148e89a30c6591230b8b65cff41fc6509a8
Yaroslav Lepenkin <yaroslav.lepenkin@jetbrains.com>
2016-03-18 17:42:36 +0300

    CompletionState now can perform some validation

--- src/com/intellij/stats/completion/validator/ConsecutiveEventsValidator.kt
+++ src/com/intellij/stats/completion/validator/ConsecutiveEventsValidator.kt
@@ -1,38 +1,73 @@
 package com.intellij.stats.completion.validator
 
-import com.intellij.stats.completion.events.*
+
+//class CompletionStartedValidator(private val initialEvent: CompletionStartedEvent) : LogEventVisitor(), TransitionValidator {
+//    
+//    override var completionState = CompletionState(initialEvent)
+//    
+//    override fun visit(event: TypeEvent) {
+//        
+//    }
+//
+//    override fun visit(event: DownPressedEvent) {
+//        val newPosition = initialEvent.currentPosition + 1
+//        isValid = event.currentPosition == newPosition
+//        updateCompletionState(event)
+//    }
+//
+//    override fun visit(event: UpPressedEvent) {
+//        val totalItems = initialEvent.completionListLength
+//        val newPosition = (initialEvent.currentPosition - 1 + totalItems) % totalItems   
+//        isValid = event.currentPosition == newPosition
+//        updateCompletionState(event)
+//    }
+//
+//    override fun visit(event: BackspaceEvent) {
+//        isValid = event.completionListIds.isNotEmpty()
+//        updateCompletionState(event)
+//    }
+//
+//    override fun visit(event: CompletionCancelledEvent) {
+//        isValid = true
+//        completionState.isFinished = isValid
+//    }
+//
+//    override fun visit(event: ExplicitSelectEvent) {
+//        isValid = event.currentPosition == initialEvent.currentPosition
+//        completionState.isFinished = isValid
+//    }
+//    
+//}
 
 
+//class TypedEventValidator(private val initialEvent: TypeEvent) : LogEventVisitor(), TransitionValidator {
+//
+//    override var isValid = false
+//
+//    override fun visit(event: TypeEvent) {
+//        isValid = true
+//    }
+//
+//    override fun visit(event: DownPressedEvent) {
+//        val initialPosition = initialEvent.currentPosition
+//        isValid = initialPosition == event.currentPosition
+//    }
+//
+//    override fun visit(event: UpPressedEvent) {
+//
+//    }
+//
+//    override fun visit(event: BackspaceEvent) {
+//
+//    }
+//
+//    override fun visit(event: CompletionCancelledEvent) {
+//
+//    }
+//
+//    override fun visit(event: ExplicitSelectEvent) {
+//
+//    }
+//
+//}
 
-class CompletionStartedValidator(private val initialEvent: CompletionStartedEvent) : LogEventVisitor(), TransitionValidator {
-
-    override var isValid: Boolean = false
-
-    override fun visit(event: TypeEvent) {
-        isValid = true
-    }
-
-    override fun visit(event: DownPressedEvent) {
-        val newPosition = initialEvent.currentPosition + 1
-        isValid = event.currentPosition == newPosition
-    }
-
-    override fun visit(event: UpPressedEvent) {
-        val totalItems = initialEvent.completionListLength
-        val newPosition = (initialEvent.currentPosition - 1 + totalItems) % totalItems   
-        isValid = event.currentPosition == newPosition
-    }
-
-    override fun visit(event: BackspaceEvent) {
-        isValid = true
-    }
-
-    override fun visit(event: CompletionCancelledEvent) {
-        isValid = true
-    }
-
-    override fun visit(event: ExplicitSelectEvent) {
-        isValid = event.currentPosition == initialEvent.currentPosition 
-    }
-    
-}
--- src/com/intellij/stats/completion/validator/EventStreamValidator.kt
+++ src/com/intellij/stats/completion/validator/EventStreamValidator.kt
@@ -1,20 +1,53 @@
 package com.intellij.stats.completion.validator
 
-import com.intellij.stats.completion.events.LogEvent
-import com.intellij.stats.completion.events.LogEventSerializer
+import com.intellij.stats.completion.events.*
 
-class EventStreamValidator {
-
-    private var previousEvent: LogEvent? = null
-    
-    fun isValidTransfer(s: String): Boolean {
-        val event: LogEvent = LogEventSerializer.fromString(s) ?: return false
-        return true    
-    }
-        
-}
 
 interface TransitionValidator {
-    var isValid: Boolean
+    var completionState: CompletionState
 }
+
+class CompletionState(event: CompletionStartedEvent) {
+    var currentPosition    = event.currentPosition
+    var completionList     = event.completionListIds
+    val allCompletionItems = event.newCompletionListItems.toMutableList()
+
+    var isFinished = false
+    var isValid    = true
+
+    private fun updateState(nextEvent: LookupStateLogData) {
+        currentPosition = nextEvent.currentPosition
+        allCompletionItems.addAll(nextEvent.newCompletionListItems)
+        if (nextEvent.completionListIds.isNotEmpty()) {
+            completionList = nextEvent.completionListIds
+        }
+    }
+    
+    fun feed(event: DownPressedEvent) {
+        val beforeDownPressedPosition = currentPosition
+        updateState(event)
+        isValid = (beforeDownPressedPosition + 1) % completionList.size == currentPosition
+    }
+
+    fun feed(event: UpPressedEvent) {
+        val beforeUpPressedPosition = currentPosition
+        updateState(event)
+        isValid = (completionList.size + beforeUpPressedPosition - 1) % completionList.size == currentPosition
+    }
+
+    fun feed(event: TypeEvent) {
+        val listBefore = completionList 
+        updateState(event)
+        isValid = listBefore.containsAll(completionList)
+    }
+
+    fun feed(event: ItemSelectedByTypingEvent) {
+        val id = event.selectedId
+        isValid = allCompletionItems.find { it.id == id } != null && completionList.find { it == id } != null
+        isFinished = true
+    }
+    
+}
+
+
 
