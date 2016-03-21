8f93e905610bfbcd2b939cf5b54de7d8eb3f0f2b
Yaroslav Lepenkin <yaroslav.lepenkin@jetbrains.com>
2016-03-21 21:00:44 +0300

    Added failing test because of containAll and Int's

--- test/com/intellij/stats/completion/validator/StreamValidatorTest.kt
+++ test/com/intellij/stats/completion/validator/StreamValidatorTest.kt
@@ -1,7 +1,7 @@
 package com.intellij.stats.completion.validator
 
+import com.intellij.stats.completion.events.LogEvent
 import com.intellij.stats.completion.events.LogEventSerializer
-import com.intellij.stats.completion.events.LookupStateLogData
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import java.io.ByteArrayInputStream
@@ -47,8 +47,30 @@ class StreamValidatorTest {
         )
         validate(list, expectedOut = "", expectedErr = list.join())
     }
-    
-    private fun validate(list: List<LookupStateLogData>, expectedOut: String, expectedErr: String) {
+
+    @Test
+    fun selected_by_typing() {
+        var list = listOf(
+                LogEventFixtures.completion_started_3_items_shown,
+                LogEventFixtures.type_event_current_pos_0_left_1_3,
+                LogEventFixtures.type_event_current_pos_0_left_1,
+                LogEventFixtures.selected_by_typing_0
+        )
+        validate(list, expectedOut = "", expectedErr = list.join())
+    }
+
+    @Test
+    fun selected_by_typing_error() {
+        var list = listOf(
+                LogEventFixtures.completion_started_3_items_shown,
+                LogEventFixtures.type_event_current_pos_0_left_1_3,
+                LogEventFixtures.down_event_new_pos_1,
+                LogEventFixtures.explicit_select_position_1
+        )
+        validate(list, expectedOut = list.join(), expectedErr = "")
+    }
+
+    private fun validate(list: List<LogEvent>, expectedOut: String, expectedErr: String) {
         val output = ByteArrayOutputStream()
         val err = ByteArrayOutputStream()
 
@@ -61,7 +83,7 @@ class StreamValidatorTest {
 
 }
 
-private fun List<LookupStateLogData>.join(): String {
+private fun List<LogEvent>.join(): String {
     return this.map {
         LogEventSerializer.toString(it)
     }.joinToString("\n")
