e46000eb4f277881b34903675643d7746b4c0864
Anna.Kozlova <anna.kozlova@jetbrains.com>
2016-03-21 12:07:25 +0100

    testdata for IDEA-153284
    (cherry picked from commit ab7f16e4b9e9767e7808ff31d15954737f15fb0a)

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewLambdaHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewLambdaHighlightingTest.java
@@ -284,6 +284,10 @@ public class NewLambdaHighlightingTest extends LightDaemonAnalyzerTestCase {
     doTest();
   }
 
+  public void testIDEA153284() throws Exception {
+    doTest();
+  }
+
   private void doTest() {
     doTest(false);
   }
