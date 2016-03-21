3a700a9bc90ec22517e481e57b92a4c48dc41ab4
Anna.Kozlova <anna.kozlova@jetbrains.com>
2016-03-21 11:11:42 +0100

    process override equivalent methods when check for functional interface target (IDEA-153324)

--- java/java-psi-api/src/com/intellij/psi/util/PsiSuperMethodUtil.java
+++ java/java-psi-api/src/com/intellij/psi/util/PsiSuperMethodUtil.java
@@ -17,10 +17,13 @@ package com.intellij.psi.util;
 
 import com.intellij.psi.*;
 import com.intellij.util.containers.HashSet;
+import gnu.trove.THashMap;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class PsiSuperMethodUtil {
@@ -79,5 +82,63 @@ public class PsiSuperMethodUtil {
     }
 
     return false;
+  }
+
+  @NotNull
+  public static PsiSubstitutor obtainFinalSubstitutor(@NotNull PsiClass superClass,
+                                                      @NotNull PsiSubstitutor superSubstitutor,
+                                                      @NotNull PsiSubstitutor derivedSubstitutor,
+                                                      boolean inRawContext) {
+    if (inRawContext) {
+      Set<PsiTypeParameter> typeParams = superSubstitutor.getSubstitutionMap().keySet();
+      PsiElementFactory factory = JavaPsiFacade.getElementFactory(superClass.getProject());
+      superSubstitutor = factory.createRawSubstitutor(derivedSubstitutor, typeParams.toArray(new PsiTypeParameter[typeParams.size()]));
+    }
+    Map<PsiTypeParameter, PsiType> map = null;
+    for (PsiTypeParameter typeParameter : PsiUtil.typeParametersIterable(superClass)) {
+      PsiType type = superSubstitutor.substitute(typeParameter);
+      final PsiType t = derivedSubstitutor.substitute(type);
+      if (map == null) {
+        map = new THashMap<PsiTypeParameter, PsiType>();
+      }
+      map.put(typeParameter, t);
+    }
+
+    return map == null ? PsiSubstitutor.EMPTY : JavaPsiFacade.getInstance(superClass.getProject()).getElementFactory().createSubstitutor(map);
+  }
+
+  @NotNull
+  public static Map<MethodSignature, Set<PsiMethod>> collectOverrideEquivalents(@NotNull PsiClass aClass) {
+    final Map<MethodSignature, Set<PsiMethod>> overrideEquivalent =
+      new THashMap<MethodSignature, Set<PsiMethod>>(MethodSignatureUtil.METHOD_PARAMETERS_ERASURE_EQUALITY);
+    PsiClass[] supers = aClass.getSupers();
+    for (int i = 0; i < supers.length; i++) {
+      PsiClass superClass = supers[i];
+      boolean subType = false;
+      for (int j = 0; j < supers.length; j++) {
+        if (j == i) continue;
+        subType |= supers[j].isInheritor(supers[i], true);
+      }
+      if (subType) continue;
+      final PsiSubstitutor superClassSubstitutor = TypeConversionUtil.getSuperClassSubstitutor(superClass, aClass, PsiSubstitutor.EMPTY);
+      for (HierarchicalMethodSignature hms : superClass.getVisibleSignatures()) {
+        final PsiMethod method = hms.getMethod();
+        if (MethodSignatureUtil.findMethodBySignature(aClass, method.getSignature(superClassSubstitutor), false) != null) continue;
+        final PsiClass containingClass = method.getContainingClass();
+        if (containingClass == null) continue;
+        final PsiSubstitutor containingClassSubstitutor = TypeConversionUtil.getClassSubstitutor(containingClass, aClass, PsiSubstitutor.EMPTY);
+        if (containingClassSubstitutor == null) continue;
+        final PsiSubstitutor finalSubstitutor =
+          obtainFinalSubstitutor(containingClass, containingClassSubstitutor, hms.getSubstitutor(), false);
+        final MethodSignatureBackedByPsiMethod signature = MethodSignatureBackedByPsiMethod.create(method, finalSubstitutor, false);
+        Set<PsiMethod> methods = overrideEquivalent.get(signature);
+        if (methods == null) {
+          methods = new LinkedHashSet<PsiMethod>();
+          overrideEquivalent.put(signature, methods);
+        }
+        methods.add(method);
+      }
+    }
+    return overrideEquivalent;
   }
 }
