f2e3277190c544df6604f9832292ce5f1619306a
Yaroslav Lepenkin <yaroslav.lepenkin@jetbrains.com>
2016-03-22 12:59:25 +0300

    Logging refactoring: start

--- plugins/stats-collector/src/com/intellij/stats/completion/CompletionLogger.kt
+++ plugins/stats-collector/src/com/intellij/stats/completion/CompletionLogger.kt
@@ -1,5 +1,6 @@
 package com.intellij.stats.completion
 
+import com.intellij.codeInsight.lookup.impl.LookupImpl
 import com.intellij.openapi.components.ServiceManager
 
 
@@ -17,20 +18,18 @@ abstract class CompletionLoggerProvider {
 
 abstract class CompletionLogger {
 
-    abstract fun completionStarted(completionList: List<LookupStringWithRelevance>, 
-                                   isExperimentPerformed: Boolean,
-                                   experimentVersion: Int)
+    abstract fun completionStarted(lookup: LookupImpl, isExperimentPerformed: Boolean, experimentVersion: Int)
     
-    abstract fun afterCharTyped(c: Char, completionList: List<LookupStringWithRelevance>)
+    abstract fun afterCharTyped(c: Char, lookup: LookupImpl)
     
-    abstract fun afterBackspacePressed(pos: Int, itemName: String, completionList: List<LookupStringWithRelevance>)
+    abstract fun afterBackspacePressed(lookup: LookupImpl)
 
-    abstract fun downPressed(pos: Int, itemName: String, completionList: List<LookupStringWithRelevance>)
-    abstract fun upPressed(pos: Int, itemName: String, completionList: List<LookupStringWithRelevance>)
+    abstract fun downPressed(lookup: LookupImpl)
+    abstract fun upPressed(lookup: LookupImpl)
 
-    abstract fun itemSelectedCompletionFinished(pos: Int, itemName: String, completionList: List<LookupStringWithRelevance>)
+    abstract fun itemSelectedCompletionFinished(lookup: LookupImpl)
     abstract fun completionCancelled()
-    abstract fun itemSelectedByTyping(itemName: String)
+    abstract fun itemSelectedByTyping(lookup: LookupImpl)
 
     abstract fun customMessage(message: String)
 
--- plugins/stats-collector/src/com/intellij/stats/completion/CompletionLoggerImpl.kt
+++ plugins/stats-collector/src/com/intellij/stats/completion/CompletionLoggerImpl.kt
@@ -1,5 +1,6 @@
 package com.intellij.stats.completion
 
+import com.intellij.codeInsight.lookup.impl.LookupImpl
 import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.updateSettings.impl.UpdateChecker
 import java.util.*
@@ -29,192 +30,40 @@ class CompletionFileLogger(private val installationUID: String,
                            private val completionUID: String,
                            private val logFileManager: LogFileManager) : CompletionLogger() {
 
-    private val itemsToId: MutableMap<String, Int> = hashMapOf()
 
-    override fun completionStarted(completionList: List<LookupStringWithRelevance>,
-                                   isExperimentPerformed: Boolean,
-                                   experimentVersion: Int) 
-    {
-        val builder = logBuilder(Action.COMPLETION_STARTED)
-        builder.addPair("EXPERIMENT_PERFORMED", isExperimentPerformed)
-        builder.addPair("EXPERIMENT_VERSION", experimentVersion)
-        builder.addPair("COMP_LIST_LEN", completionList.size)
-        builder.addText(firstCompletionListText(completionList))
-        log(builder)
+    override fun completionStarted(lookup: LookupImpl, isExperimentPerformed: Boolean, experimentVersion: Int) {
     }
+
 
     override fun customMessage(message: String) {
-        val builder = logBuilder(Action.CUSTOM)
-        builder.addText(message)
-        log(builder)
     }
 
-    override fun afterCharTyped(c: Char, completionList: List<LookupStringWithRelevance>) {
-        val builder = logBuilder(Action.TYPE)
-        builder.addPair("COMP_LIST_LEN", completionList.size)
-        builder.addText(createIdListAddUntrackedItems(completionList))
-        log(builder)
+
+    override fun afterCharTyped(c: Char, lookup: LookupImpl) {
     }
 
-    override fun downPressed(pos: Int, itemName: String, completionList: List<LookupStringWithRelevance>) {
-        val builder = logBuilder(Action.DOWN)
-        builder.addPair("POS", pos)
 
-        val id = getItemId(itemName)
-        var idsList = ""
-        if (id == null) {
-            idsList = createIdListAddUntrackedItems(completionList)
-        }
-
-        builder.addPair("ID", getItemId(itemName)!!)
-
-        if (idsList.isNotEmpty()) {
-            builder.addText(idsList)
-        }
-        
-        log(builder)
+    override fun downPressed(lookup: LookupImpl) {
     }
 
-    override fun upPressed(pos: Int, itemName: String, completionList: List<LookupStringWithRelevance>) {
-        val builder = logBuilder(Action.UP)
-        builder.addPair("POS", pos)
 
-        val id = getItemId(itemName)
-        var idsList = ""
-        if (id == null) {
-            idsList = createIdListAddUntrackedItems(completionList)
-        }
-
-        builder.addPair("ID", getItemId(itemName)!!)
-
-        if (idsList.isNotEmpty()) {
-            builder.addText(idsList)
-        }
-        
-        builder.addPair("ID", getItemId(itemName)!!)
-        log(builder)
+    override fun upPressed(lookup: LookupImpl) {
     }
+
 
     override fun completionCancelled() {
-        val builder = logBuilder(Action.COMPLETION_CANCELED)
-        log(builder)
     }
 
-    override fun itemSelectedByTyping(itemName: String) {
-        val builder = logBuilder(Action.TYPED_SELECT)
-        
-        //todo remove: for debugging purposes
-        if (getItemId(itemName) == null) {
-            builder.addPair("ID", "-777")
-            builder.addPair("ITEM", itemName)
-            
-            val allValues = itemsToId.map {
-                "${it.key}=${it.value}"
-            }.joinToString(",", "[", "]")
-            
-            builder.addText(allValues)
-            log(builder)
-            return
-        }
 
-        val id = getItemId(itemName)!!
-        builder.addPair("ID", id)
-        log(builder)
+    override fun itemSelectedByTyping(lookup: LookupImpl) {
     }
 
-    override fun itemSelectedCompletionFinished(pos: Int, itemName: String, completionList: List<LookupStringWithRelevance>) {
-        val builder = logBuilder(Action.EXPLICIT_SELECT)
-        builder.addPair("POS", pos)
-        
-        var id = getItemId(itemName)
-        var ids = ""
-        if (id == null) {
-            ids = createIdListAddUntrackedItems(completionList)
-            id = getItemId(itemName)
-        }
-        
-        builder.addPair("ID", id!!)
 
-        if (ids.isNotEmpty()) {
-            builder.addText(ids)
-        }
-        
-        log(builder)
+    override fun itemSelectedCompletionFinished(lookup: LookupImpl) {
     }
 
-    override fun afterBackspacePressed(pos: Int, itemName: String, completionList: List<LookupStringWithRelevance>) {
-        val builder = logBuilder(Action.BACKSPACE)
-        builder.addPair("POS", pos)
-        builder.addPair("COMP_LIST_LEN", completionList.size)
-        val ids = createIdListAddUntrackedItems(completionList)
-        val id = getItemId(itemName)
-        if (id != null) {
-            builder.addPair("ID", id)
-        } else {
-            builder.addPair("ID", "NULL($itemName)")
-        }
-        builder.addText(ids)
-        log(builder)
-    }
 
-    private fun firstCompletionListText(items: List<LookupStringWithRelevance>): String {
-        val builder = StringBuilder()
-        with(builder, {
-            append("[")
-            var first = true
-            items.forEach {
-                if (!first) {
-                    append(", ")
-                }
-                first = false
-                val id = addNewItem(it.item)
-                append("{ID=$id, ${it.toData()}}")
-            }
-            append("]")
-        })
-        return builder.toString()
-    }
-    
-    private fun log(logLineBuilder: LogLineBuilder) {
-        val msg = logLineBuilder.text()
-        logFileManager.println(msg)
-    }
-
-    private fun getItemId(itemName: String): Int? {
-        return itemsToId[itemName]
-    }
-    
-    private fun addNewItem(itemName: String): Int {
-        val size = itemsToId.size
-        itemsToId[itemName] = size
-        return size 
-    }
-
-    private fun createIdListAddUntrackedItems(items: List<LookupStringWithRelevance>): String {
-        val idsList = StringBuilder()
-        with (idsList) {
-            append("[")
-            var first = true
-            items.forEach {
-                if (!first) {
-                    append(", ")
-                }
-                first = false
-                
-                var relevance = ""
-                if (getItemId(it.item) == null) {
-                    addNewItem(it.item)
-                    relevance = " ${it.toData()}"
-                }
-                append("ID=${getItemId(it.item)!!}$relevance")
-            }
-            append(']')
-        }
-        return idsList.toString()
-    }
-
-    private fun logBuilder(action: Action): LogLineBuilder {
-        return LogLineBuilder(installationUID, completionUID, action)
+    override fun afterBackspacePressed(lookup: LookupImpl) {
     }
 
 }
