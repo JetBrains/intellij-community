55c03fb6f1d78802174dabc249f7525361a37918
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2016-03-23 18:50:14 +0100

    [platform] ignores malformed repository files (EA-80904)

--- platform/platform-impl/src/com/intellij/ide/plugins/RepositoryHelper.java
+++ platform/platform-impl/src/com/intellij/ide/plugins/RepositoryHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -264,10 +264,7 @@ public class RepositoryHelper {
       parser.parse(new InputSource(reader), handler);
       return handler.getPluginsList();
     }
-    catch (ParserConfigurationException e) {
-      throw new IOException(e);
-    }
-    catch (SAXException e) {
+    catch (ParserConfigurationException | SAXException | RuntimeException e) {
       throw new IOException(e);
     }
     finally {
