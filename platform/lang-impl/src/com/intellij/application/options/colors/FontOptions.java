37fee6e51b31df250c9262853faa7fcf3d4c4e47
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2016-03-24 17:00:39 +0300

    IDEA-153364 Warning 'Cannot modify read-only scheme' when no one setting is changed

--- platform/lang-impl/src/com/intellij/application/options/colors/FontOptions.java
+++ platform/lang-impl/src/com/intellij/application/options/colors/FontOptions.java
@@ -129,15 +129,11 @@ public class FontOptions extends JPanel implements OptionsPanel{
       mySecondaryCombo.setEnabled(myUseSecondaryFontCheckbox.isSelected());
       syncFontFamilies();
     });
-    ItemListener itemListener = e -> {
-      if (e.getStateChange() == ItemEvent.SELECTED) {
-        syncFontFamilies();
-      }
-    };
+    ItemListener itemListener = this::syncFontFamilies;
     myPrimaryCombo.addItemListener(itemListener);
     mySecondaryCombo.addItemListener(itemListener);
 
-    ActionListener actionListener = e -> syncFontFamilies();
+    ActionListener actionListener = this::syncFontFamilies;
     myPrimaryCombo.addActionListener(actionListener);
     mySecondaryCombo.addActionListener(actionListener);
 
@@ -213,6 +209,22 @@ public class FontOptions extends JPanel implements OptionsPanel{
        return Math.min(EditorFontsConstants.getMaxEditorLineSpacing(), Math.max(EditorFontsConstants.getMinEditorLineSpacing(), Float.parseFloat(myLineSpacingField.getText())));
     } catch (NumberFormatException e){
       return EditorFontsConstants.getDefaultEditorLineSpacing();
+    }
+  }
+
+  /**
+   * Processes an event from {@code FontComboBox}
+   * if it is enabled and its item is selected.
+   *
+   * @param event the event to process
+   */
+  private void syncFontFamilies(AWTEvent event) {
+    Object source = event.getSource();
+    if (source instanceof FontComboBox) {
+      FontComboBox combo = (FontComboBox)source;
+      if (combo.isEnabled() && combo.getSelectedItem() != null) {
+        syncFontFamilies();
+      }
     }
   }
 
