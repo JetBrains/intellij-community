55c028dfc8f809565ab2e332e3929163d5d18bbe
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2016-03-29 11:30:03 +0300

    Extract separate test function for #PY-18725 and #PY-18954 (cherry picked from commit b8268be)

--- python/testData/inspections/PyStringFormatInspection/DictionaryArgument.py
+++ python/testData/inspections/PyStringFormatInspection/DictionaryArgument.py
@@ -8,12 +8,3 @@ my_dict['tmp'] = 'classes %(css_class)s' % my_dict
 
 my_dict['tmp'] = 'classes %(claz)s' % <warning descr="Key 'claz' has no following argument">my_dict</warning>
 
-
-"%s" % {"a": 1}
-
-f = "fst"
-s = "snd"
-"first is %(fst)s, second is %(snd)s" % {f: 1, s: 2}
-
-snd = "snd"
-"%(f)s %(snd)s" % {"f": 2, snd: 2}
