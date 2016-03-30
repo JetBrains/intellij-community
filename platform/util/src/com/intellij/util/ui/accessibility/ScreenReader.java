2e4b627462111f25daae5f3eaa38b367a4c4ddd3
Anton Tarasov <anton.tarasov@jetbrains.com>
2016-03-30 14:40:52 +0300

    move UIUtil.isA11YEnabled(..) to ScreenReader.isEnabled(..)

--- platform/util/src/com/intellij/util/ui/accessibility/ScreenReader.java
+++ platform/util/src/com/intellij/util/ui/accessibility/ScreenReader.java
@@ -16,15 +16,21 @@
 package com.intellij.util.ui.accessibility;
 
 import com.intellij.util.SystemProperties;
+import org.jetbrains.annotations.NotNull;
 
 import javax.accessibility.AccessibleContext;
 import java.awt.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
 
 /**
  * Expose system-wide screen reader status for accessibility features.
  */
 public class ScreenReader {
-  public static String SYSTEM_PROPERTY_KEY = "screenreader";
+  public static final String SYSTEM_PROPERTY_KEY = "screenreader";
+  public static final String ATK_WRAPPER = "org.GNOME.Accessibility.AtkWrapper";
+  public static final String ACCESS_BRIDGE = "com.sun.java.accessibility.AccessBridge";
 
   /**
    * Components that need to customize their behavior in the presence of a external screen reader
@@ -48,6 +54,49 @@ public class ScreenReader {
         AccessibleContext frameContext = ((AccessibleContextAccessor)frame).getCurrentAccessibleContext();
         if (frameContext != null)
           return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Checks whether a particular a11y technology is enabled, in order of priority:
+   * 1) via the sys. property: "javax.accessibility.assistive_technologies"
+   * 2) in the prop. file: <user home>/.accessibility.properties
+   * 3) in the prop. file: <jre>/lib/accessibility.properties
+   *
+   * @see {@link #ACCESS_BRIDGE}
+   * @see {@link #ATK_WRAPPER}
+   * @param a11yClassName the full class name representing the a11y technology
+   * @return true if enabled, otherwise false
+   */
+  public static boolean isEnabled(String a11yClassName) {
+    String[] paths = new String[] {System.getProperty("user.home") + File.separator + ".accessibility.properties",
+      System.getProperty("java.home") + File.separator + "lib" + File.separator + "accessibility.properties"};
+    Properties properties = new Properties();
+    for (String path : paths) {
+      try {
+        FileInputStream in = new FileInputStream(new File(path));
+        try {
+          properties.load(in);
+        } finally {
+          if (in != null) in.close();
+        }
+      }
+      catch (Exception ignore) {
+        continue;
+      }
+      if (!properties.isEmpty()) break;
+    }
+    if (!properties.isEmpty()) {
+      // First, check the system property
+      String classNames = System.getProperty("javax.accessibility.assistive_technologies");
+      if (classNames == null) {
+        // If the system property is not set, Toolkit will try to use the properties file.
+        classNames = properties.getProperty("assistive_technologies", null);
+      }
+      if (classNames != null && classNames.contains(a11yClassName)) {
+        return true;
       }
     }
     return false;
