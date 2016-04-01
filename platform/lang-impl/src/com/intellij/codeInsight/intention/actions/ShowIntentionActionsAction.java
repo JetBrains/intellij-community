bca1ab39a861de543ceb2bd3ef2e9b86392416f8
peter <peter@jetbrains.com>
2016-04-01 14:14:21 +0200

    don't wrap automatic QuestionAction invocation into a command with meaningless name

--- platform/lang-impl/src/com/intellij/codeInsight/intention/actions/ShowIntentionActionsAction.java
+++ platform/lang-impl/src/com/intellij/codeInsight/intention/actions/ShowIntentionActionsAction.java
@@ -20,6 +20,12 @@ import com.intellij.codeInsight.CodeInsightActionHandler;
 import com.intellij.codeInsight.actions.BaseCodeInsightAction;
 import com.intellij.codeInsight.hint.HintManagerImpl;
 import com.intellij.codeInsight.intention.impl.ShowIntentionActionsHandler;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiUtilBase;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -28,6 +34,21 @@ import org.jetbrains.annotations.NotNull;
 public class ShowIntentionActionsAction extends BaseCodeInsightAction implements HintManagerImpl.ActionToIgnore {
   public ShowIntentionActionsAction() {
     setEnabledInModalContext(true);
+  }
+
+  @Override
+  public void actionPerformed(AnActionEvent e) {
+    Project project = e.getProject();
+    if (project == null) return;
+
+    Editor editor = getEditor(e.getDataContext(), project, false);
+    if (editor == null) return;
+
+    final PsiFile psiFile = PsiUtilBase.getPsiFileInEditor(editor, project);
+    if (psiFile == null) return;
+
+    if (!ApplicationManager.getApplication().isUnitTestMode() && !editor.getContentComponent().isShowing()) return;
+    getHandler().invoke(project, editor, psiFile);
   }
 
   @Override
