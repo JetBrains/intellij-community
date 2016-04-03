a91056297176651dc8619f1440905dd2ab5a7f9b
Michail Plushnikov <michail@plushnikov.de>
2016-04-03 16:23:55 +0200

    added change notes Configuration keys #53
    GitOrigin-RevId: f08946aa03a4b7f1d9e841c5330ddce536b96112

--- plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/lombokconfig/ConfigKey.java
+++ plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/lombokconfig/ConfigKey.java
@@ -1,9 +1,5 @@
 package de.plushnikov.intellij.plugin.lombokconfig;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashSet;
-
 public enum ConfigKey {
 
   CONFIG_STOP_BUBBLING("config.stopBubbling", "false"),
@@ -26,7 +22,46 @@ public enum ConfigKey {
   SINGULAR_USE_GUAVA("lombok.singular.useGuava", "false"),
   SINGULAR_AUTO("lombok.singular.auto", "true"),
 
-  NONNULL_EXCEPTIONTYPE("lombok.nonNull.exceptionType", "java.lang.NullPointerException");
+  FIELDDEFAULTS_FINAL("lombok.fieldDefaults.defaultFinal", "false"),
+  FIELDDEFAULTS_PRIVATE("lombok.fieldDefaults.defaultPrivate", "false"),
+
+  NONNULL_EXCEPTIONTYPE("lombok.nonNull.exceptionType", "java.lang.NullPointerException"),
+
+  ADD_GENERATED_ANNOTATION("lombok.addGeneratedAnnotation", "true"),
+  ADD_SUPPRESS_FB_WARNINGS("lombok.extern.findbugs.addSuppressFBWarnings", "false"),
+
+  ACCESSORS_FLAG_USAGE("lombok.accessors.flagUsage", ""),
+  ALLARGSCONSTRUCTOR_FLAG_USAGE("lombok.allArgsConstructor.flagUsage", ""),
+  ANYCONSTRUCTOR_FLAG_USAGE("lombok.anyConstructor.flagUsage", ""),
+  BUILDER_FLAG_USAGE("lombok.builder.flagUsage", ""),
+  CLEANUP_FLAG_USAGE("lombok.cleanup.flagUsage", ""),
+  DATA_FLAG_USAGE("lombok.data.flagUsage", ""),
+  DELEGATE_FLAG_USAGE("lombok.delegate.flagUsage", ""),
+  EQUALSANDHASHCODE_FLAG_USAGE("lombok.equalsAndHashCode.flagUsage", ""),
+  EXPERIMENTAL_FLAG_USAGE("lombok.experimental.flagUsage", ""),
+  EXTENSIONMETHOD_FLAG_USAGE("lombok.extensionMethod.flagUsage", ""),
+  FIELDDEFAULTS_FLAG_USAGE("lombok.fieldDefaults.flagUsage", ""),
+  GETTER_FLAG_USAGE("lombok.getter.flagUsage", ""),
+  GETTER_LAZY_FLAG_USAGE("lombok.getter.lazy.flagUsage", ""),
+  LOG_APACHECOMMONS_FLAG_USAGE("lombok.log.apacheCommons.flagUsage", ""),
+  LOG_FLAG_USAGE("lombok.log.flagUsage", ""),
+  LOG_JAVAUTILLOGGING_FLAG_USAGE("lombok.log.javaUtilLogging.flagUsage", ""),
+  LOG_LOG4J_FLAG_USAGE("lombok.log.log4j.flagUsage", ""),
+  LOG_LOG4J2_FLAG_USAGE("lombok.log.log4j2.flagUsage", ""),
+  LOG_SLF4J_FLAG_USAGE("lombok.log.slf4j.flagUsage", ""),
+  LOG_XSLF4J_FLAG_USAGE("lombok.log.xslf4j.flagUsage", ""),
+  NOARGSCONSTRUCTOR_FLAG_USAGE("lombok.noArgsConstructor.flagUsage", ""),
+  NONNULL_FLAG_USAGE("lombok.nonNull.flagUsage", ""),
+  REQUIREDARGSCONSTRUCTOR_FLAG_USAGE("lombok.requiredArgsConstructor.flagUsage", ""),
+  SETTER_FLAG_USAGE("lombok.setter.flagUsage", ""),
+  SNEAKYTHROWS_FLAG_USAGE("lombok.sneakyThrows.flagUsage", ""),
+  SYNCHRONIZED_FLAG_USAGE("lombok.synchronized.flagUsage", ""),
+  TOSTRING_FLAG_USAGE("lombok.toString.flagUsage", ""),
+  VAL_FLAG_USAGE("lombok.val.flagUsage", ""),
+  VALUE_FLAG_USAGE("lombok.value.flagUsage", ""),
+  WITHER_FLAG_USAGE("lombok.wither.flagUsage", ""),
+  UTILITYCLASS_FLAG_USAGE("lombok.utilityClass.flagUsage", ""),
+  HELPER_FLAG_USAGE("lombok.helper.flagUsage", "");
 
   private final String configKey;
   private final String configDefaultValue;
@@ -43,19 +78,6 @@ public enum ConfigKey {
   public String getConfigDefaultValue() {
     return configDefaultValue;
   }
-
-  //TODO ADD ALL KEYS
-  final Collection<String> flagUsageOptions = new HashSet<String>(Arrays.asList(
-      "lombok.accessors.flagUsage", "lombok.allArgsConstructor.flagUsage", "lombok.anyConstructor.flagUsage",
-      "lombok.builder.flagUsage", "lombok.cleanup.flagUsage", "lombok.data.flagUsage", "lombok.delegate.flagUsage",
-      "lombok.equalsAndHashCode.flagUsage", "lombok.experimental.flagUsage", "lombok.extensionMethod.flagUsage",
-      "lombok.fieldDefaults.flagUsage", "lombok.getter.flagUsage", "lombok.getter.lazy.flagUsage",
-      "lombok.log.apacheCommons.flagUsage", "lombok.log.flagUsage", "lombok.log.javaUtilLogging.flagUsage",
-      "lombok.log.log4j.flagUsage", "lombok.log.log4j2.flagUsage", "lombok.log.slf4j.flagUsage",
-      "lombok.log.xslf4j.flagUsage", "lombok.noArgsConstructor.flagUsage", "lombok.nonNull.flagUsage",
-      "lombok.requiredArgsConstructor.flagUsage", "lombok.setter.flagUsage", "lombok.sneakyThrows.flagUsage",
-      "lombok.synchronized.flagUsage", "lombok.toString.flagUsage", "lombok.val.flagUsage", "lombok.value.flagUsage",
-      "lombok.wither.flagUsage"));
 
   public static ConfigKey fromConfigStringKey(String configStringKey) {
     for (ConfigKey keys : ConfigKey.values()) {
