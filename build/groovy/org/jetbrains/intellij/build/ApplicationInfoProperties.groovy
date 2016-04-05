5e81fde2d8b6a83df77eeca664ddd334002bb73b
Vladimir.Orlov <Vladimir.Orlov@jetbrains.com>
2016-04-05 17:42:48 +0300

    removed obsoleted install-over from ApplicationInfo.xml files and build scripts.

--- build/groovy/org/jetbrains/intellij/build/ApplicationInfoProperties.groovy
+++ build/groovy/org/jetbrains/intellij/build/ApplicationInfoProperties.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,21 +24,11 @@ class ApplicationInfoProperties {
   final String majorVersion
   final String minorVersion
   final boolean isEAP
-  final InstallOverProperties installOver
 
   ApplicationInfoProperties(String appInfoXmlPath) {
     def root = new XmlParser().parse(new File(appInfoXmlPath))
     majorVersion = root.version.first().@major
     minorVersion = root.version.first().@minor
     isEAP = Boolean.parseBoolean(root.version.first().@eap)
-    def installOverTag = root."install-over".first()
-    installOver = new InstallOverProperties(minBuild: installOverTag.@minbuild, maxBuild: installOverTag.@maxbuild, version: installOverTag.@version)
   }
-}
-
-@Immutable
-class InstallOverProperties {
-  final String minBuild
-  final String maxBuild
-  final String version
 }
