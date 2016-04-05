710c89230d4f7106d182f36195b72aa60ed804ce
Bas Leijdekkers <basleijdekkers@gmail.com>
2016-04-05 17:04:50 +0200

    consistently spell fallthrough (IDEA-153854)

--- plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/controlflow/FallthruInSwitchStatementInspection.java
+++ plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/controlflow/FallthruInSwitchStatementInspection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2013 Dave Griffith, Bas Leijdekkers
+ * Copyright 2003-2016 Dave Griffith, Bas Leijdekkers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -86,7 +86,7 @@ public class FallthruInSwitchStatementInspection extends BaseInspection {
 
   private static class FallthroughInSwitchStatementVisitor extends BaseInspectionVisitor {
 
-    private static final Pattern commentPattern = Pattern.compile("(?i)falls?\\s*thro?u");
+    private static final Pattern commentPattern = Pattern.compile("(?i)falls?\\s*-?thro?u");
 
     @Override
     public void visitSwitchStatement(@NotNull PsiSwitchStatement switchStatement) {
