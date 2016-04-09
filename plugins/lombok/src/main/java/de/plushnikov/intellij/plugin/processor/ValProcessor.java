75701de13ae8ba746d2660e442aa8713b251fe71
Alexej Kubarev <alexej.kubarev@accedo.tv>
2016-04-09 14:27:06 +0200

    Code style change to remove * imports. Checking for modifier name prior to other checks on val
    GitOrigin-RevId: b3c623d74e5e442a0acd547cc6d324607fe4382d

--- plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/processor/ValProcessor.java
+++ plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/processor/ValProcessor.java
@@ -7,7 +7,25 @@ import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.util.RecursionManager;
-import com.intellij.psi.*;
+import com.intellij.psi.PsiAnnotation;
+import com.intellij.psi.PsiArrayInitializerExpression;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiDeclarationStatement;
+import com.intellij.psi.PsiDiamondType;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiExpression;
+import com.intellij.psi.PsiForStatement;
+import com.intellij.psi.PsiForeachStatement;
+import com.intellij.psi.PsiJavaCodeReferenceElement;
+import com.intellij.psi.PsiLambdaExpression;
+import com.intellij.psi.PsiLocalVariable;
+import com.intellij.psi.PsiModifier;
+import com.intellij.psi.PsiModifierList;
+import com.intellij.psi.PsiNewExpression;
+import com.intellij.psi.PsiParameter;
+import com.intellij.psi.PsiReferenceParameterList;
+import com.intellij.psi.PsiType;
+import com.intellij.psi.PsiTypeElement;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.util.TypeConversionUtil;
 
@@ -23,142 +41,161 @@ import lombok.val;
 
 public class ValProcessor extends AbstractProcessor {
 
-  private static final String LOMBOK_VAL_FQN = "lombok.val";
-  private static final String LOMBOK_VAL_SHORT_NAME = "val";
+    private static final String LOMBOK_VAL_FQN = "lombok.val";
+    private static final String LOMBOK_VAL_SHORT_NAME = "val";
 
-  public ValProcessor() {
-    super(PsiElement.class, val.class);
-  }
-
-  public boolean isEnabled(@NotNull Project project) {
-    return isEnabled(PropertiesComponent.getInstance(project));
-  }
-
-  @Override
-  public boolean isEnabled(@NotNull PropertiesComponent propertiesComponent) {
-    return ProjectSettings.isEnabled(propertiesComponent, ProjectSettings.IS_VAL_ENABLED);
-  }
-
-  @NotNull
-  @Override
-  public Collection<PsiAnnotation> collectProcessedAnnotations(@NotNull PsiClass psiClass) {
-    return Collections.emptyList();
-  }
-
-  @NotNull
-  @Override
-  public Collection<LombokProblem> verifyAnnotation(@NotNull PsiAnnotation psiAnnotation) {
-    return Collections.emptyList();
-  }
-
-  public void verifyVariable(@NotNull final PsiLocalVariable psiLocalVariable, @NotNull final ProblemsHolder holder) {
-    if (isSameName(psiLocalVariable.getTypeElement().getText())) {
-      final PsiExpression initializer = psiLocalVariable.getInitializer();
-      if (initializer == null) {
-        holder.registerProblem(psiLocalVariable, "'val' on a local variable requires an initializer expression", ProblemHighlightType.ERROR);
-      } else if (initializer instanceof PsiArrayInitializerExpression) {
-        holder.registerProblem(psiLocalVariable, "'val' is not compatible with array initializer expressions. Use the full form (new int[] { ... } instead of just { ... })", ProblemHighlightType.ERROR);
-      } else if (initializer instanceof PsiLambdaExpression) {
-        holder.registerProblem(psiLocalVariable, "'val' is not allowed with lambda expressions.", ProblemHighlightType.ERROR);
-      } else {
-        final PsiElement typeParentParent = psiLocalVariable.getParent();
-        if (typeParentParent instanceof PsiDeclarationStatement && typeParentParent.getParent() instanceof PsiForStatement) {
-          holder.registerProblem(psiLocalVariable, "'val' is not allowed in old-style for loops", ProblemHighlightType.ERROR);
-        }
-      }
-    }
-  }
-
-  public void verifyParameter(@NotNull final PsiParameter psiParameter, @NotNull final ProblemsHolder holder) {
-    final PsiTypeElement typeElement = psiParameter.getTypeElement();
-    if (null != typeElement && isSameName(typeElement.getText())) {
-      if (!(psiParameter.getDeclarationScope() instanceof PsiForeachStatement)) {
-        holder.registerProblem(psiParameter, "'val' works only on local variables and on foreach loops", ProblemHighlightType.ERROR);
-      }
-    }
-  }
-
-  public static boolean isVal(@NotNull PsiLocalVariable psiLocalVariable) {
-    return psiLocalVariable.getInitializer() != null && isSameName(psiLocalVariable.getTypeElement().getText());
-  }
-
-  private boolean isVal(@NotNull PsiParameter psiParameter) {
-    return psiParameter.getParent() instanceof PsiForeachStatement && isSameName(psiParameter.getTypeElement().getText());
-  }
-
-  private static boolean isSameName(String className) {
-    return LOMBOK_VAL_SHORT_NAME.equals(className) || LOMBOK_VAL_FQN.equals(className);
-  }
-
-  public Boolean hasModifierProperty(@NotNull PsiModifierList modifierList, @NotNull String name) {
-    final PsiElement parent = modifierList.getParent();
-
-    //Only apply final modifier to val local variables
-    if (parent instanceof PsiLocalVariable && isVal((PsiLocalVariable) parent)) {
-      return PsiModifier.FINAL.equals(name) ? Boolean.TRUE : null;
+    public ValProcessor() {
+        super(PsiElement.class, val.class);
     }
 
-    return null;
-  }
+    public boolean isEnabled(@NotNull Project project) {
 
-  @Nullable
-  public PsiType inferType(PsiTypeElement typeElement) {
-    PsiType psiType = null;
-
-    final PsiElement parent = typeElement.getParent();
-    if ((parent instanceof PsiLocalVariable && isVal((PsiLocalVariable) parent)) ||
-        (parent instanceof PsiParameter && isVal((PsiParameter) parent))) {
-
-      if (parent instanceof PsiLocalVariable) {
-        psiType = processLocalVariableInitializer(((PsiLocalVariable) parent).getInitializer());
-      } else {
-        psiType = processParameterDeclaration(((PsiParameter) parent).getDeclarationScope());
-      }
-
-      if (null == psiType) {
-        psiType = PsiType.getJavaLangObject(typeElement.getManager(), GlobalSearchScope.allScope(typeElement.getProject()));
-      }
+        return isEnabled(PropertiesComponent.getInstance(project));
     }
-    return psiType;
-  }
 
-  private PsiType processLocalVariableInitializer(final PsiExpression psiExpression) {
-    PsiType result = null;
-    if (null != psiExpression && !(psiExpression instanceof PsiArrayInitializerExpression)) {
+    @Override
+    public boolean isEnabled(@NotNull PropertiesComponent propertiesComponent) {
 
-      result = RecursionManager.doPreventingRecursion(psiExpression, true, new Computable<PsiType>() {
-        @Override
-        public PsiType compute() {
-          return psiExpression.getType();
-        }
-      });
+        return ProjectSettings.isEnabled(propertiesComponent, ProjectSettings.IS_VAL_ENABLED);
+    }
 
-      if (psiExpression instanceof PsiNewExpression) {
-        final PsiJavaCodeReferenceElement reference = ((PsiNewExpression) psiExpression).getClassOrAnonymousClassReference();
-        if (reference != null) {
-          final PsiReferenceParameterList parameterList = reference.getParameterList();
-          if (parameterList != null) {
-            final PsiTypeElement[] elements = parameterList.getTypeParameterElements();
-            if (elements.length == 1 && elements[0].getType() instanceof PsiDiamondType) {
-              result = TypeConversionUtil.erasure(result);
+    @NotNull
+    @Override
+    public Collection<PsiAnnotation> collectProcessedAnnotations(@NotNull PsiClass psiClass) {
+
+        return Collections.emptyList();
+    }
+
+    @NotNull
+    @Override
+    public Collection<LombokProblem> verifyAnnotation(@NotNull PsiAnnotation psiAnnotation) {
+
+        return Collections.emptyList();
+    }
+
+    public void verifyVariable(@NotNull final PsiLocalVariable psiLocalVariable, @NotNull final ProblemsHolder holder) {
+
+        if (isSameName(psiLocalVariable.getTypeElement()
+                                       .getText())) {
+            final PsiExpression initializer = psiLocalVariable.getInitializer();
+            if (initializer == null) {
+                holder.registerProblem(psiLocalVariable, "'val' on a local variable requires an initializer expression", ProblemHighlightType.ERROR);
+            } else if (initializer instanceof PsiArrayInitializerExpression) {
+                holder.registerProblem(psiLocalVariable,
+                                       "'val' is not compatible with array initializer expressions. Use the full form (new int[] { ... } instead of just { ... })",
+                                       ProblemHighlightType.ERROR);
+            } else if (initializer instanceof PsiLambdaExpression) {
+                holder.registerProblem(psiLocalVariable, "'val' is not allowed with lambda expressions.", ProblemHighlightType.ERROR);
+            } else {
+                final PsiElement typeParentParent = psiLocalVariable.getParent();
+                if (typeParentParent instanceof PsiDeclarationStatement && typeParentParent.getParent() instanceof PsiForStatement) {
+                    holder.registerProblem(psiLocalVariable, "'val' is not allowed in old-style for loops", ProblemHighlightType.ERROR);
+                }
             }
-          }
         }
-      }
     }
 
-    return result;
-  }
+    public void verifyParameter(@NotNull final PsiParameter psiParameter, @NotNull final ProblemsHolder holder) {
 
-  private PsiType processParameterDeclaration(PsiElement parentDeclarationScope) {
-    PsiType result = null;
-    if (parentDeclarationScope instanceof PsiForeachStatement) {
-      final PsiForeachStatement foreachStatement = (PsiForeachStatement) parentDeclarationScope;
-      final PsiExpression iteratedValue = foreachStatement.getIteratedValue();
-      if (iteratedValue != null) {
-        result = JavaGenericsUtil.getCollectionItemType(iteratedValue);
-      }
+        final PsiTypeElement typeElement = psiParameter.getTypeElement();
+        if (null != typeElement && isSameName(typeElement.getText())) {
+            if (!(psiParameter.getDeclarationScope() instanceof PsiForeachStatement)) {
+                holder.registerProblem(psiParameter, "'val' works only on local variables and on foreach loops", ProblemHighlightType.ERROR);
+            }
+        }
     }
-    return result;
-  }
+
+    public static boolean isVal(@NotNull PsiLocalVariable psiLocalVariable) {
+
+        return psiLocalVariable.getInitializer() != null && isSameName(psiLocalVariable.getTypeElement()
+                                                                                       .getText());
+    }
+
+    private boolean isVal(@NotNull PsiParameter psiParameter) {
+
+        return psiParameter.getParent() instanceof PsiForeachStatement && isSameName(psiParameter.getTypeElement()
+                                                                                                 .getText());
+    }
+
+    private static boolean isSameName(String className) {
+
+        return LOMBOK_VAL_SHORT_NAME.equals(className) || LOMBOK_VAL_FQN.equals(className);
+    }
+
+    public Boolean hasModifierProperty(@NotNull PsiModifierList modifierList, @NotNull String name) {
+
+        final PsiElement parent = modifierList.getParent();
+
+        // Only apply final modifier to val local variables
+        if (PsiModifier.FINAL.equals(name) && parent instanceof PsiLocalVariable && isVal((PsiLocalVariable) parent)) {
+            return Boolean.TRUE;
+        }
+
+        return null;
+    }
+
+    @Nullable
+    public PsiType inferType(PsiTypeElement typeElement) {
+
+        PsiType psiType = null;
+
+        final PsiElement parent = typeElement.getParent();
+        if ((parent instanceof PsiLocalVariable && isVal((PsiLocalVariable) parent)) || (parent instanceof PsiParameter && isVal((PsiParameter) parent))) {
+
+            if (parent instanceof PsiLocalVariable) {
+                psiType = processLocalVariableInitializer(((PsiLocalVariable) parent).getInitializer());
+            } else {
+                psiType = processParameterDeclaration(((PsiParameter) parent).getDeclarationScope());
+            }
+
+            if (null == psiType) {
+                psiType = PsiType.getJavaLangObject(typeElement.getManager(), GlobalSearchScope.allScope(typeElement.getProject()));
+            }
+        }
+        return psiType;
+    }
+
+    private PsiType processLocalVariableInitializer(final PsiExpression psiExpression) {
+
+        PsiType result = null;
+        if (null != psiExpression && !(psiExpression instanceof PsiArrayInitializerExpression)) {
+
+            result = RecursionManager.doPreventingRecursion(psiExpression, true, new Computable<PsiType>() {
+
+                @Override
+                public PsiType compute() {
+
+                    return psiExpression.getType();
+                }
+            });
+
+            if (psiExpression instanceof PsiNewExpression) {
+                final PsiJavaCodeReferenceElement reference = ((PsiNewExpression) psiExpression).getClassOrAnonymousClassReference();
+                if (reference != null) {
+                    final PsiReferenceParameterList parameterList = reference.getParameterList();
+                    if (parameterList != null) {
+                        final PsiTypeElement[] elements = parameterList.getTypeParameterElements();
+                        if (elements.length == 1 && elements[0].getType() instanceof PsiDiamondType) {
+                            result = TypeConversionUtil.erasure(result);
+                        }
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private PsiType processParameterDeclaration(PsiElement parentDeclarationScope) {
+
+        PsiType result = null;
+        if (parentDeclarationScope instanceof PsiForeachStatement) {
+            final PsiForeachStatement foreachStatement = (PsiForeachStatement) parentDeclarationScope;
+            final PsiExpression iteratedValue = foreachStatement.getIteratedValue();
+            if (iteratedValue != null) {
+                result = JavaGenericsUtil.getCollectionItemType(iteratedValue);
+            }
+        }
+        return result;
+    }
 }
--- plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/provider/LombokAugmentProvider.java
+++ plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/provider/LombokAugmentProvider.java
@@ -3,7 +3,13 @@ package de.plushnikov.intellij.plugin.provider;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.*;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiField;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiModifierList;
+import com.intellij.psi.PsiType;
+import com.intellij.psi.PsiTypeElement;
 import com.intellij.psi.augment.PsiAugmentProvider;
 import com.intellij.psi.impl.source.PsiExtensibleClass;
 import com.intellij.psi.util.CachedValueProvider;
@@ -28,112 +34,121 @@ import java.util.List;
  * @author Plushnikov Michail
  */
 public class LombokAugmentProvider extends PsiAugmentProvider {
-  private static final Logger log = Logger.getInstance(LombokAugmentProvider.class.getName());
 
-  private ValProcessor valProcessor;
+    private static final Logger log = Logger.getInstance(LombokAugmentProvider.class.getName());
 
-  public LombokAugmentProvider() {
-    log.debug("LombokAugmentProvider created");
-    valProcessor = new ValProcessor();
-  }
+    private ValProcessor valProcessor;
 
-  @Nullable
-  //@Override //May vause issues with older versions of IDEA SDK that are currently supported
-  protected Boolean hasModifierProperty(@NotNull PsiModifierList modifierList, @NotNull String name) {
-
-    if (DumbService.isDumb(modifierList.getProject()) || !valProcessor.isEnabled(modifierList.getProject())) {
-      return null;
+    public LombokAugmentProvider() {
+        log.debug("LombokAugmentProvider created");
+        valProcessor = new ValProcessor();
     }
 
-    return valProcessor.hasModifierProperty(modifierList, name);
-  }
+    @Nullable
+    // @Override //May vause issues with older versions of IDEA SDK that are currently supported
+    protected Boolean hasModifierProperty(@NotNull PsiModifierList modifierList, @NotNull String name) {
 
-  @Nullable
-  @Override
-  protected PsiType inferType(@NotNull PsiTypeElement typeElement) {
-    if (DumbService.isDumb(typeElement.getProject()) || !valProcessor.isEnabled(typeElement.getProject())) {
-      return null;
-    }
-    return valProcessor.inferType(typeElement);
-  }
+        if (DumbService.isDumb(modifierList.getProject()) || !valProcessor.isEnabled(modifierList.getProject())) {
+            return null;
+        }
 
-  @NotNull
-  @Override
-  public <Psi extends PsiElement> List<Psi> getAugments(@NotNull PsiElement element, @NotNull final Class<Psi> type) {
-    final List<Psi> emptyResult = Collections.emptyList();
-    // skip processing during index rebuild
-    final Project project = element.getProject();
-    if (DumbService.isDumb(project)) {
-      return emptyResult;
-    }
-    // Expecting that we are only augmenting an PsiClass
-    // Don't filter !isPhysical elements or code auto completion will not work
-    if (!(element instanceof PsiExtensibleClass) || !element.isValid()) {
-      return emptyResult;
-    }
-    // Skip processing of Annotations and Interfaces
-    if (((PsiClass) element).isAnnotationType() || ((PsiClass) element).isInterface()) {
-      return emptyResult;
-    }
-
-    // skip processing if plugin is disabled
-    if (!ProjectSettings.isLombokEnabledInProject(project)) {
-      return emptyResult;
-    }
-
-    final PsiClass psiClass = (PsiClass) element;
-
-    if (type == PsiField.class) {
-      return CachedValuesManager.getCachedValue(element, new FieldLombokCachedValueProvider<Psi>(type, psiClass));
-    } else if (type == PsiMethod.class) {
-      return CachedValuesManager.getCachedValue(element, new MethodLombokCachedValueProvider<Psi>(type, psiClass));
-    } else if (type == PsiClass.class) {
-      return CachedValuesManager.getCachedValue(element, new ClassLombokCachedValueProvider<Psi>(type, psiClass));
-    } else {
-      return emptyResult;
-    }
-  }
-
-  private static class FieldLombokCachedValueProvider<Psi extends PsiElement> extends LombokCachedValueProvider<Psi> {
-    FieldLombokCachedValueProvider(Class<Psi> type, PsiClass psiClass) {
-      super(type, psiClass);
-    }
-  }
-
-  private static class MethodLombokCachedValueProvider<Psi extends PsiElement> extends LombokCachedValueProvider<Psi> {
-    MethodLombokCachedValueProvider(Class<Psi> type, PsiClass psiClass) {
-      super(type, psiClass);
-    }
-  }
-
-  private static class ClassLombokCachedValueProvider<Psi extends PsiElement> extends LombokCachedValueProvider<Psi> {
-    ClassLombokCachedValueProvider(Class<Psi> type, PsiClass psiClass) {
-      super(type, psiClass);
-    }
-  }
-
-  private static class LombokCachedValueProvider<Psi extends PsiElement> implements CachedValueProvider<List<Psi>> {
-    private final Class<Psi> type;
-    private final PsiClass psiClass;
-
-    LombokCachedValueProvider(Class<Psi> type, PsiClass psiClass) {
-      this.type = type;
-      this.psiClass = psiClass;
+        return valProcessor.hasModifierProperty(modifierList, name);
     }
 
     @Nullable
     @Override
-    public Result<List<Psi>> compute() {
-      if (log.isDebugEnabled()) {
-        log.debug(String.format("Process call for type: %s class: %s", type, psiClass.getQualifiedName()));
-      }
+    protected PsiType inferType(@NotNull PsiTypeElement typeElement) {
 
-      final List<Psi> result = new ArrayList<Psi>();
-      final Collection<Processor> lombokProcessors = LombokProcessorProvider.getInstance(psiClass.getProject()).getLombokProcessors(type);
-      for (Processor processor : lombokProcessors) {
-        result.addAll((Collection<Psi>) processor.process(psiClass));
-      }
-      return new Result<List<Psi>>(result, PsiModificationTracker.JAVA_STRUCTURE_MODIFICATION_COUNT);
+        if (DumbService.isDumb(typeElement.getProject()) || !valProcessor.isEnabled(typeElement.getProject())) {
+            return null;
+        }
+        return valProcessor.inferType(typeElement);
     }
-  }
+
+    @NotNull
+    @Override
+    public <Psi extends PsiElement> List<Psi> getAugments(@NotNull PsiElement element, @NotNull final Class<Psi> type) {
+
+        final List<Psi> emptyResult = Collections.emptyList();
+        // skip processing during index rebuild
+        final Project project = element.getProject();
+        if (DumbService.isDumb(project)) {
+            return emptyResult;
+        }
+        // Expecting that we are only augmenting an PsiClass
+        // Don't filter !isPhysical elements or code auto completion will not work
+        if (!(element instanceof PsiExtensibleClass) || !element.isValid()) {
+            return emptyResult;
+        }
+        // Skip processing of Annotations and Interfaces
+        if (((PsiClass) element).isAnnotationType() || ((PsiClass) element).isInterface()) {
+            return emptyResult;
+        }
+
+        // skip processing if plugin is disabled
+        if (!ProjectSettings.isLombokEnabledInProject(project)) {
+            return emptyResult;
+        }
+
+        final PsiClass psiClass = (PsiClass) element;
+
+        if (type == PsiField.class) {
+            return CachedValuesManager.getCachedValue(element, new FieldLombokCachedValueProvider<Psi>(type, psiClass));
+        } else if (type == PsiMethod.class) {
+            return CachedValuesManager.getCachedValue(element, new MethodLombokCachedValueProvider<Psi>(type, psiClass));
+        } else if (type == PsiClass.class) {
+            return CachedValuesManager.getCachedValue(element, new ClassLombokCachedValueProvider<Psi>(type, psiClass));
+        } else {
+            return emptyResult;
+        }
+    }
+
+    private static class FieldLombokCachedValueProvider<Psi extends PsiElement> extends LombokCachedValueProvider<Psi> {
+
+        FieldLombokCachedValueProvider(Class<Psi> type, PsiClass psiClass) {
+            super(type, psiClass);
+        }
+    }
+
+    private static class MethodLombokCachedValueProvider<Psi extends PsiElement> extends LombokCachedValueProvider<Psi> {
+
+        MethodLombokCachedValueProvider(Class<Psi> type, PsiClass psiClass) {
+            super(type, psiClass);
+        }
+    }
+
+    private static class ClassLombokCachedValueProvider<Psi extends PsiElement> extends LombokCachedValueProvider<Psi> {
+
+        ClassLombokCachedValueProvider(Class<Psi> type, PsiClass psiClass) {
+            super(type, psiClass);
+        }
+    }
+
+    private static class LombokCachedValueProvider<Psi extends PsiElement> implements CachedValueProvider<List<Psi>> {
+
+        private final Class<Psi> type;
+        private final PsiClass psiClass;
+
+        LombokCachedValueProvider(Class<Psi> type, PsiClass psiClass) {
+            this.type = type;
+            this.psiClass = psiClass;
+        }
+
+        @Nullable
+        @Override
+        public Result<List<Psi>> compute() {
+
+            if (log.isDebugEnabled()) {
+                log.debug(String.format("Process call for type: %s class: %s", type, psiClass.getQualifiedName()));
+            }
+
+            final List<Psi> result = new ArrayList<Psi>();
+            final Collection<Processor> lombokProcessors = LombokProcessorProvider.getInstance(psiClass.getProject())
+                                                                                  .getLombokProcessors(type);
+            for (Processor processor : lombokProcessors) {
+                result.addAll((Collection<Psi>) processor.process(psiClass));
+            }
+            return new Result<List<Psi>>(result, PsiModificationTracker.JAVA_STRUCTURE_MODIFICATION_COUNT);
+        }
+    }
 }
