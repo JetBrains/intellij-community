1fad6680e9456fb73c19927ff71765659b1dd4d0
Alexej Kubarev <alexej.kubarev@accedo.tv>
2016-04-10 20:49:20 +0200

    Handling of FieldDefaults with tests
    GitOrigin-RevId: 71f227e783653953d81f960de9882e5045f48115

---
+++ plugins/lombok/src/test/java/de/plushnikov/intellij/plugin/processor/modifier/FieldDefaultsModifierTest.java
@@ -0,0 +1,126 @@
+package de.plushnikov.intellij.plugin.processor.modifier;
+
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiField;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiModifier;
+import com.intellij.psi.PsiModifierList;
+import com.intellij.psi.augment.PsiAugmentProvider;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.testFramework.PlatformTestUtil;
+import de.plushnikov.ApiVersionAwareLightCodeInsightFixureTestCase;
+import de.plushnikov.RequiredApiVersion;
+import de.plushnikov.intellij.plugin.provider.LombokAugmentProvider;
+import lombok.NonNull;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author Alexej Kubarev
+ */
+@RequiredApiVersion("146.1154") // Modifier augmentation has been added in build 146.1154
+public class FieldDefaultsModifierTest extends ApiVersionAwareLightCodeInsightFixureTestCase {
+
+  @Override
+  protected String getTestDataPath() {
+    return "testData/augment/modifier";
+  }
+
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    PlatformTestUtil.registerExtension(PsiAugmentProvider.EP_NAME, new LombokAugmentProvider(), myTestRootDisposable);
+    myFixture.addClass("package lombok;\npublic enum AccessLevel { PUBLIC, MODULE, PROTECTED, PACKAGE, PRIVATE, NONE; }");
+    myFixture.addClass("package lombok.experimental;\npublic @interface PackagePrivate { }");
+    myFixture.addClass("package lombok.experimental;\npublic @interface NonFinal { }");
+    myFixture.addClass("package lombok.experimental;\n" +
+                        "public @interface FieldDefaults {\n" +
+                          "AccessLevel level() default AccessLevel.NONE;\n" +
+                          "boolean makeFinal() default false;\n" +
+                        "}");
+  }
+
+  //<editor-fold desc="Handling of makeFinal and @NonFinal">
+
+  public void testFieldDefaultsFinal() {
+
+    PsiModifierList modifierList = getFieldModifierListAtCaret();
+
+    assertTrue("@FieldDefaults(makeFinal = true) should make all @NonFinal fields final", modifierList.hasModifierProperty(PsiModifier.FINAL));
+    assertTrue("@FieldDefaults(makeFinal = true) should keep fields package local", modifierList.hasModifierProperty(PsiModifier.PACKAGE_LOCAL));
+  }
+
+  public void testFieldDefaultsFinalFalse() {
+
+    PsiModifierList modifierList = getFieldModifierListAtCaret();
+
+    assertFalse("@FieldDefaults(makeFinal = false) should make all @NonFinal fields final", modifierList.hasModifierProperty(PsiModifier.FINAL));
+    assertTrue("@FieldDefaults(makeFinal = false) should keep fields package local", modifierList.hasModifierProperty(PsiModifier.PACKAGE_LOCAL));
+  }
+
+  public void testFieldDefaultsWithNonFinal() {
+
+    PsiModifierList modifierList = getFieldModifierListAtCaret();
+
+    assertFalse("@FieldDefaults(makeFinal = true) should not make @NonFinal fields final", modifierList.hasModifierProperty(PsiModifier.FINAL));
+  }
+
+  //</editor-fold>
+
+  //<editor-fold desc="Handling of visibility modifiers">
+
+  public void testFieldDefaultsPublic() {
+
+    PsiModifierList modifierList = getFieldModifierListAtCaret();
+
+    assertTrue("@FieldDefaults(level = AccessLevel.PUBLIC) should make non-@PackagePrivate fields public", modifierList.hasModifierProperty(PsiModifier.PUBLIC));
+  }
+
+  public void testFieldDefaultsProtected() {
+
+    PsiModifierList modifierList = getFieldModifierListAtCaret();
+
+    assertTrue("@FieldDefaults(level = AccessLevel.PROTECTED) should non-@PackagePrivate make fields protected", modifierList.hasModifierProperty(PsiModifier.PROTECTED));
+  }
+
+  public void testFieldDefaultsPrivate() {
+
+    PsiModifierList modifierList = getFieldModifierListAtCaret();
+
+    assertTrue("@FieldDefaults(level = AccessLevel.PRIVATE) should make non-@PackagePrivate fields private", modifierList.hasModifierProperty(PsiModifier.PRIVATE));
+  }
+
+  public void testFieldDefaultsPublicWithPackagePrivate() {
+
+    PsiModifierList modifierList = getFieldModifierListAtCaret();
+
+    assertFalse("@FieldDefaults(level = AccessLevel.PUBLIC) should not make @PackagePrivate fields public", modifierList.hasModifierProperty(PsiModifier.PUBLIC));
+    assertTrue("@FieldDefaults should keep @PackagePrivate fields package-private", modifierList.hasModifierProperty(PsiModifier.PACKAGE_LOCAL));
+  }
+
+  public void testFieldDefaultsWithExplicitModifier() {
+
+    PsiModifierList modifierList = getFieldModifierListAtCaret();
+
+    assertFalse("@FieldDefaults should not touch fields with explicit modifier", modifierList.hasModifierProperty(PsiModifier.PUBLIC));
+    assertTrue("@FieldDefaults should keep explicit modifier intact", modifierList.hasModifierProperty(PsiModifier.PROTECTED));
+  }
+
+  //</editor-fold>
+
+  //<editor-fold desc="Internal support methods">
+
+  @NotNull
+  private PsiModifierList getFieldModifierListAtCaret() {
+    PsiFile file = myFixture.configureByFile(getTestName(false) + ".java");
+    PsiField field = PsiTreeUtil.getParentOfType(file.findElementAt(myFixture.getCaretOffset()), PsiField.class);
+
+    assertNotNull(field);
+
+    PsiModifierList modifierList = field.getModifierList();
+    assertNotNull(modifierList);
+
+    return modifierList;
+  }
+
+  //</editor-fold>
+}
