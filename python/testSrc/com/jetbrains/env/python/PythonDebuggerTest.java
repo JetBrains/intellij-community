fe7c47d68869ac1e5f2f8b3a6a789a1f131ebeca
fitermay <fiterman@gmail.com>
2016-04-13 15:28:26 -0400

    --test for short path to long windows pathname normalization in pydev debugger

--- python/testSrc/com/jetbrains/env/python/PythonDebuggerTest.java
+++ python/testSrc/com/jetbrains/env/python/PythonDebuggerTest.java
@@ -4,6 +4,8 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.intellij.openapi.util.SystemInfo;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.testFramework.UsefulTestCase;
 import com.intellij.testFramework.fixtures.IdeaProjectTestFixture;
 import com.intellij.xdebugger.XDebuggerTestUtil;
@@ -23,6 +25,7 @@ import com.jetbrains.python.sdk.flavors.PythonSdkFlavor;
 import com.jetbrains.python.sdkTools.SdkCreationType;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -581,6 +584,43 @@ public class PythonDebuggerTest extends PyEnvTestCase {
 
     });
   }
+
+  public void testWinLongName() throws Exception {
+    if (!SystemInfo.isWindows)
+    {
+      return; // Only needs to run on windows
+    }
+    runPythonTest(new PyDebuggerTask("/debug", "long_n~1.py") {
+      @Override
+      public void before() throws Exception {
+
+        String scriptPath = getScriptPath();
+        String longPath = FileUtil
+          .toSystemDependentName((new File(scriptPath).getCanonicalPath()));
+        LocalFileSystem.getInstance().refreshAndFindFileByPath(longPath);
+        toggleBreakpoint(longPath, 2);
+
+
+      }
+
+      @Override
+      public void testing() throws Exception {
+        waitForPause();
+        eval("x").hasValue("10");
+        resume();
+
+      }
+
+      @NotNull
+      @Override
+      public Set<String> getTags() {
+        return ImmutableSet.of("-jython"); //TODO: fix that for Jython if anybody needs it
+      }
+
+
+    });
+  }
+
 
 
   public void testStepOverConditionalBreakpoint() throws Exception {
