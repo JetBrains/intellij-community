65db81cf29c02a2d097276de6e06d8339ab90f2c
Anna.Kozlova <anna.kozlova@jetbrains.com>
2016-04-13 20:15:43 +0200

    inference: ensure foreign variables do not participate in inference process, e.g. variables from method reference overload resolution (IDEA-154622)
    (cherry picked from commit d780e31bdff882ad2a0fbced9f7382dcb6d28f2e)

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewMethodRefHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/NewMethodRefHighlightingTest.java
@@ -510,6 +510,10 @@ public class NewMethodRefHighlightingTest extends LightDaemonAnalyzerTestCase {
     doTest();
   }
 
+  public void testIgnoreForeignVariables() throws Exception {
+    doTest();
+  }
+
   private void doTest() {
     doTest(false);
   }
