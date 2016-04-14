6974f887d8aa4d3d1c253c754b93b18b1532cfcd
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2016-04-14 17:14:06 +0300

    Move basic vcs test classes from testSrc to source root of vcs-tests module
    To make them accessible for external plugins.
    
    Add a dependency on vcs-tests where needed,
    move some tests from vcs-impl to vcs-tests to avoid circular dependency.

--- platform/vcs-tests/testSrc/com/intellij/openapi/vcs/roots/VcsIntegrationEnablerTest.java
+++ platform/vcs-tests/testSrc/com/intellij/openapi/vcs/roots/VcsIntegrationEnablerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,7 +22,6 @@ import com.intellij.openapi.vcs.*;
 import com.intellij.openapi.vcs.changes.committed.MockAbstractVcs;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.testFramework.PlatformTestCase;
 import com.intellij.util.Function;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.vcsUtil.VcsUtil;
