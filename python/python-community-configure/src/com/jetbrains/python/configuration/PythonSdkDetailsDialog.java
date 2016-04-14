3515a4edcc4052e89fec50e35c1be4d8e37153a9
fitermay <fiterman@gmail.com>
2016-04-15 01:46:42 +0300

    --fix NPE in PythonSdkDetailsDialog.java (cherry picked from commit 3c68a65)

--- python/python-community-configure/src/com/jetbrains/python/configuration/PythonSdkDetailsDialog.java
+++ python/python-community-configure/src/com/jetbrains/python/configuration/PythonSdkDetailsDialog.java
@@ -226,7 +226,9 @@ public class PythonSdkDetailsDialog extends DialogWrapper {
       mySdkSettingsWereModified.run();
     }
     for (SdkModificator modificator : myModifiedModificators) {
-      modificator.commitChanges();
+      if (modificator.isWritable()) {
+        modificator.commitChanges();
+      }
     }
     myModificators.clear();
     myModifiedModificators.clear();
@@ -431,7 +433,13 @@ public class PythonSdkDetailsDialog extends DialogWrapper {
   }
 
   private void reloadSdk(@NotNull Sdk currentSdk) {
-    PythonSdkUpdater.update(currentSdk, myModificators.get(currentSdk), myProject, null);
+    /* PythonSdkUpdater.update invalidates the modificator so we need to create a new
+      one for further changes
+     */
+    if (PythonSdkUpdater.update(currentSdk, myModificators.get(currentSdk), myProject, null)){
+      myModifiedModificators.remove(myModificators.get(currentSdk));
+      myModificators.put(currentSdk, currentSdk.getSdkModificator());
+    }
   }
 
   private class ToggleVirtualEnvFilterButton extends ToggleActionButton implements DumbAware {
