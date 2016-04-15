406fc5ad149ec4fe85cd320ed2934f9171819ecc
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2016-04-16 01:04:45 +0300

    fix chameleon syntax highlighting pass, process all file, not a visible part

--- platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/ChameleonSyntaxHighlightingPass.java
+++ platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/ChameleonSyntaxHighlightingPass.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -79,7 +79,7 @@ class ChameleonSyntaxHighlightingPass extends TextEditorHighlightingPass {
 
     myEditor = editor;
 
-    TextRange range = VisibleHighlightingPassFactory.calculateVisibleRange(myEditor);
+    TextRange range = file.getTextRange();
     myStartOffset = range.getStartOffset();
     myEndOffset = range.getEndOffset();
 
