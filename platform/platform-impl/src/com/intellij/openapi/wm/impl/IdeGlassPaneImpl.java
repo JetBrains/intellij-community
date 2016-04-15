bd91a7ce786a9b3be4809a9d21b68b39dd6e0631
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2016-04-15 21:31:56 +0300

    reuse utility method to convert mouse events

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/IdeGlassPaneImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/IdeGlassPaneImpl.java
@@ -166,7 +166,7 @@ public class IdeGlassPaneImpl extends JPanel implements IdeGlassPaneEx, IdeEvent
           final boolean overSelection = ((DnDAware)target).isOverSelection(targetPoint);
           if (overSelection) {
             final MouseListener[] listeners = target.getListeners(MouseListener.class);
-            final MouseEvent mouseEvent = convertEvent(me, target);
+            final MouseEvent mouseEvent = MouseEventAdapter.convert(me, target);
             switch (me.getID()) {
               case MouseEvent.MOUSE_PRESSED:
                 boolean consumed = false;
@@ -282,7 +282,7 @@ public class IdeGlassPaneImpl extends JPanel implements IdeGlassPaneEx, IdeEvent
     try {
       if (UIUtil.getWindow(this) != UIUtil.getWindow(e.getComponent())) return false;
 
-      final MouseEvent event = convertEvent(e, eventRootPane);
+      final MouseEvent event = MouseEventAdapter.convert(e, eventRootPane);
 
       if (!IdeGlassPaneUtil.canBePreprocessed(e)) {
         return false;
@@ -394,11 +394,6 @@ public class IdeGlassPaneImpl extends JPanel implements IdeGlassPaneEx, IdeEvent
     else {
       myListener2Cursor.put(requestor, cursor);
     }
-  }
-
-  private static MouseEvent convertEvent(final MouseEvent e, final Component target) {
-    final Point point = SwingUtilities.convertPoint(e.getComponent(), e.getPoint(), target);
-    return MouseEventAdapter.convert(e, target, point.x, point.y);
   }
 
   private static void fireMouseEvent(final MouseListener listener, final MouseEvent event) {
