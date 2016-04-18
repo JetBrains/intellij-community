14ae763e217044a1ad892a0cd3abd1d8ddc722f8
Konstantin Bulenkov <kb@jetbrains.com>
2016-04-18 15:30:56 +0200

    +hasPath(String) (cherry picked from commit 9d4e78e)

--- platform/platform-api/src/com/intellij/ide/RecentProjectsManager.java
+++ platform/platform-api/src/com/intellij/ide/RecentProjectsManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -52,4 +52,8 @@ public abstract class RecentProjectsManager {
   public List<ProjectGroup> getGroups() {return Collections.emptyList();}
   public void addGroup(ProjectGroup group) {}
   public void removeGroup(ProjectGroup group) {}
+
+  public boolean hasPath(String path) {
+    return false;
+  }
 }
