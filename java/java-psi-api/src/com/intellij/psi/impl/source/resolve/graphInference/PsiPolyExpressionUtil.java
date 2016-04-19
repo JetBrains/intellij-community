c09caae28e84c9b4be861809e7daa113034c5370
Anna.Kozlova <anna.kozlova@jetbrains.com>
2016-04-19 14:57:26 +0200

    treat conditional expressions with new expressions with diamonds inside as poly EA-76546 - assert: InferenceSessionContainer.treeWalkUp

--- java/java-psi-api/src/com/intellij/psi/impl/source/resolve/graphInference/PsiPolyExpressionUtil.java
+++ java/java-psi-api/src/com/intellij/psi/impl/source/resolve/graphInference/PsiPolyExpressionUtil.java
@@ -169,9 +169,13 @@ public class PsiPolyExpressionUtil {
     }
     if (expr == null) return null;
     PsiType type = null;
-    if (expr instanceof PsiNewExpression || hasStandaloneForm(expr)) {
+    //A class instance creation expression (§15.9) for a class that is convertible to a numeric type.
+    //As numeric classes do not have type parameters, at this point expressions with diamonds could be ignored
+    if (expr instanceof PsiNewExpression && !PsiDiamondType.hasDiamond((PsiNewExpression)expr) ||
+        hasStandaloneForm(expr)) {
       type = expr.getType();
-    } else if (expr instanceof PsiMethodCallExpression) {
+    }
+    else if (expr instanceof PsiMethodCallExpression) {
       final PsiMethod method = ((PsiMethodCallExpression)expr).resolveMethod();
       if (method != null) {
         type = method.getReturnType();
