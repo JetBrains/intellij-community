306d16f2b43e5b22129f50a9684d2655d887de0e
nik <Nikolay.Chashnikov@jetbrains.com>
2016-04-21 17:35:59 +0300

    BuildNumber: deprecate 'fallback' method and use 'currentVersion' instead; 'fallback' name is obscure and it's better to use fallback version (999.SNAPSHOT) only if we cannot read more actual build number from build.txt, otherwise some plugins may be disabled as incompatible

--- platform/core-impl/src/com/intellij/ide/plugins/PluginManagerCore.java
+++ platform/core-impl/src/com/intellij/ide/plugins/PluginManagerCore.java
@@ -94,7 +94,7 @@ public class PluginManagerCore {
       if (ourBuildNumber == null) {
         ourBuildNumber = BUILD_NUMBER == null ? null : BuildNumber.fromString(BUILD_NUMBER);
         if (ourBuildNumber == null) {
-          ourBuildNumber = BuildNumber.fromString("SNAPSHOT");
+          ourBuildNumber = BuildNumber.currentVersion();
         }
       }
       return ourBuildNumber;
