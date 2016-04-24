228bbde6a64451ea45f4d4d6face3f86633a3879
fitermay <fiterman@gmail.com>
2016-04-24 18:29:47 -0400

    Support pandas dataframe in array viewer during debugging (PY-14330)
    (cherry picked from commit 669292aa3fb3678411d5b28ce4756ca102de98f0)
    (cherry picked from commit bc8f2ee3639390165406f89854577bdb782c6334)

--- python/src/com/jetbrains/python/debugger/containerview/PyViewNumericContainerAction.java
+++ python/src/com/jetbrains/python/debugger/containerview/PyViewNumericContainerAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,33 +13,53 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.jetbrains.python.debugger.array;
+package com.jetbrains.python.debugger.containerview;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.xdebugger.impl.ui.tree.XDebuggerTree;
 import com.intellij.xdebugger.impl.ui.tree.actions.XDebuggerTreeActionBase;
 import com.intellij.xdebugger.impl.ui.tree.nodes.XValueNodeImpl;
 import com.jetbrains.python.debugger.PyDebugValue;
+import com.jetbrains.python.debugger.array.NumpyArrayTable;
+import com.jetbrains.python.debugger.dataframe.DataFrameTable;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import javax.swing.*;
 import javax.swing.tree.TreePath;
 
 /**
  * @author amarch
  */
 
-public class PyViewArrayAction extends XDebuggerTreeActionBase {
+public class PyViewNumericContainerAction extends XDebuggerTreeActionBase {
 
   @Override
   protected void perform(XValueNodeImpl node, @NotNull String nodeName, AnActionEvent e) {
     Project p = e.getProject();
     if (p != null && node != null && node.getValueContainer() instanceof PyDebugValue && node.isComputed()) {
-      final ViewArrayDialog dialog = new ViewArrayDialog(p, (PyDebugValue)(node.getValueContainer()));
+      PyDebugValue debugValue = (PyDebugValue)node.getValueContainer();
+      String nodeType = debugValue.getType();
+      final ViewNumericContainerDialog dialog;
+      if ("ndarray".equals(nodeType)) {
+        dialog = new ViewNumericContainerDialog(p, (dialogWrapper) -> {
+          NumpyArrayTable arrayTable = new NumpyArrayTable(p, dialogWrapper, debugValue);
+          arrayTable.init();
+          return arrayTable.getComponent().getMainPanel();
+        });
+      }
+      else if (("DataFrame".equals(nodeType))) {
+        dialog = new ViewNumericContainerDialog(p, (dialogWrapper) -> {
+          DataFrameTable dataFrameTable = new DataFrameTable(p, dialogWrapper, debugValue);
+          dataFrameTable.init();
+          return dataFrameTable.getComponent().getMainPanel();
+        });
+      }
+      else {
+        throw new IllegalStateException("Cannot render node type: " + nodeType);
+      }
+
       dialog.show();
     }
   }
@@ -65,54 +85,21 @@ public class PyViewArrayAction extends XDebuggerTreeActionBase {
 
         String nodeType = debugValue.getType();
         if ("ndarray".equals(nodeType)) {
+          e.getPresentation().setText("View as Array");
           e.getPresentation().setVisible(true);
-          return;
+        }
+        else if (("DataFrame".equals(nodeType))) {
+          e.getPresentation().setText("View as DataFrame");
+          e.getPresentation().setVisible(true);
+        }
+        else {
+          e.getPresentation().setVisible(false);
         }
       }
-    }
-    e.getPresentation().setVisible(false);
-  }
-
-  protected static class ViewArrayDialog extends DialogWrapper {
-    private Project myProject;
-    private NumpyArrayTable myNumpyArrayTable;
-
-    private ViewArrayDialog(@NotNull Project project, PyDebugValue debugValue) {
-      super(project, false);
-      setModal(false);
-      setCancelButtonText("Close");
-      setCrossClosesWindow(true);
-
-      myProject = project;
-      myNumpyArrayTable = new NumpyArrayTable(myProject, this, debugValue);
-
-      myNumpyArrayTable.init();
-      init();
-    }
-
-    public void setError(String text) {
-      //todo: think about this usage
-      setErrorText(text);
-    }
-
-    @Override
-    @NotNull
-    protected Action[] createActions() {
-      return new Action[]{getCancelAction()};
-    }
-
-    @Override
-    protected String getDimensionServiceKey() {
-      return "#com.jetbrains.python.actions.view.array.PyViewArrayAction";
-    }
-
-    @Override
-    protected JComponent createCenterPanel() {
-      return myNumpyArrayTable.getComponent().getMainPanel();
-    }
-
-    public ArrayTableForm getComponent() {
-      return myNumpyArrayTable.getComponent();
+      else
+      {
+        e.getPresentation().setVisible(false);
+      }
     }
   }
 }
