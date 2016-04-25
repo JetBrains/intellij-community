201fa142c6073e251f244444d64ad3c27203df67
Renaud Paquay <rpaquay@google.com>
2016-04-25 11:44:53 -0700

    a11y: Make ScreenReader.isActive a configurable setting
    * ScreenReader.isActive is now controlled a via a user-configurable
      setting in the "Appearance & Behavior > System Settings" settings
      page.
    
    * We need a intermediate IDE component to keep the user-setting in
      sync with ScreenReader.isActive to keep layering intact.
    
    Change-Id: I3cc992d27b61af0b66294b32704cea236a86e6eb

--- platform/util/src/com/intellij/util/ui/accessibility/ScreenReader.java
+++ platform/util/src/com/intellij/util/ui/accessibility/ScreenReader.java
@@ -15,74 +15,26 @@
  */
 package com.intellij.util.ui.accessibility;
 
-import com.intellij.openapi.util.SystemInfo;
-import com.intellij.util.SystemProperties;
-
-import javax.accessibility.AccessibleContext;
-import java.awt.*;
-
 /**
  * Expose system-wide screen reader status for accessibility features.
  */
 public class ScreenReader {
-  public static String SYSTEM_PROPERTY_KEY = "screenreader";
-  /**
-   * <p>Cache the <b>positive</b> state of having an active screen reader.</p>
-   *
-   * <p><b>Note:</b>
-   * Screen readers always attach to applications after some delay, due to the
-   * way Java Access Bridge events work. The delay varies between screen readers, as
-   * it depends on how the initial handshake is implemented by the screen reader.</p>
-   *
-   * <p>This means caching the negative case would require heuristics to deal
-   * with the non-determinism introduced by this delay. In other words, if we don't
-   * detect an active screen reader at some point in time, how long can we remember
-   * that fact?</p>
-   *
-   * <p>However, caching the positive case is safe, as we can assume that once we detect
-   * an active screen reader, we can remember that fact until the application is
-   * terminated -- the way {@link javax.accessibility.Accessible#getAccessibleContext}
-   * is implemented follows the same assumption.</p>
-   *
-   * <p>Also, it turns out caching the positive case helps alleviate the latency
-   * issue in the case where the application keeps closing and opening top level
-   * windows, as each new window has a {@code null} {@link AccessibleContext} until
-   * the screen reader attaches to the window. By remembering the "positive" state,
-   * we ensure screen reader latency only affects the very first window opened by
-   * the application.</p>
-   */
-  private static boolean myActiveCache = false;
+  private static boolean myActive = false;
 
   /**
-   * Components that need to customize their behavior in the presence of a external screen reader
-   * application should call this method  to determine the presence of such screen reader.
-   *
-   * For example, this can be used to determine if components should to be focusable via keyboard
-   * as opposed to accessible only via the mouse pointer.
-   *
-   * @return <code>true</code> if a screen reader is currently active, or has been active
-   * since the start of the application.
+   * Components that need to customize their behavior in the presence of an external screen reader
+   * application should call this method. For example, this can be used to determine if components
+   * should to be focusable via keyboard -- as opposed to accessible only via the mouse pointer.
    */
   public static boolean isActive() {
-    if (myActiveCache)
-      return true;
+    return myActive;
+  }
 
-    // Return system property value if it is set
-    if (SystemProperties.has(SYSTEM_PROPERTY_KEY)) {
-      return SystemProperties.is(SYSTEM_PROPERTY_KEY);
-    }
-
-    // Auto-detect if system property not set
-    // Note: Only on Windows until Mac/Linux are fully supported.
-    if (SystemInfo.isWindows) {
-      for (Frame frame : Frame.getFrames()) {
-        if (frame instanceof AccessibleContextAccessor) {
-          AccessibleContext frameContext = ((AccessibleContextAccessor)frame).getCurrentAccessibleContext();
-          if (frameContext != null)
-            return true;
-        }
-      }
-    }
-    return false;
+  /**
+   * This method should be called by a central configuration mechanism to enable screen reader
+   * support for the application.
+   */
+  public static void setActive(boolean active) {
+    myActive = active;
   }
 }
