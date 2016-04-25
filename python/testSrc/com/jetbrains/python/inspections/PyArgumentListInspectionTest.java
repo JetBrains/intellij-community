0929934a048a06568809ee198417e60dc2aecc67
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2016-04-25 15:32:42 +0300

    PY-19130 Fixed: Type inference: decorators behaving oddly
    If class is decorated, PyArgumentListInspection doesn't check its constructor arguments

--- python/testSrc/com/jetbrains/python/inspections/PyArgumentListInspectionTest.java
+++ python/testSrc/com/jetbrains/python/inspections/PyArgumentListInspectionTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,6 +43,16 @@ public class PyArgumentListInspectionTest extends PyTestCase {
     finally {
       PythonLanguageLevelPusher.setForcedLanguageLevel(myFixture.getProject(), null);
     }
+  }
+
+  // PY-19130
+  public void testClassDecoratedThroughDecorator() {
+    doTest();
+  }
+
+  // PY-19130
+  public void testClassDecoratedThroughCall() {
+    doTest();
   }
   
   public void testTupleVsLiteralList() {
