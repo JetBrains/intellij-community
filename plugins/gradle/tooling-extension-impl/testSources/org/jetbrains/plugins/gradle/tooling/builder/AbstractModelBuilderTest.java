2c3ab52c8c43e381bc3b586df3a143d279d1a04c
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2016-04-26 19:55:24 +0300

    gradle: vmOptions adjusted for gradle daemon in tests

--- plugins/gradle/tooling-extension-impl/testSources/org/jetbrains/plugins/gradle/tooling/builder/AbstractModelBuilderTest.java
+++ plugins/gradle/tooling-extension-impl/testSources/org/jetbrains/plugins/gradle/tooling/builder/AbstractModelBuilderTest.java
@@ -155,7 +155,7 @@ public abstract class AbstractModelBuilderTest {
       assertNotNull(initScript);
       String jdkHome = IdeaTestUtil.requireRealJdkHome();
       buildActionExecutor.setJavaHome(new File(jdkHome));
-      buildActionExecutor.setJvmArguments("-Xmx64m", "-XX:MaxPermSize=64m");
+      buildActionExecutor.setJvmArguments("-Xmx128m", "-XX:MaxPermSize=64m");
       buildActionExecutor.withArguments("--info", "--recompile-scripts", GradleConstants.INIT_SCRIPT_CMD_OPTION, initScript.getAbsolutePath());
       allModels = buildActionExecutor.run();
       assertNotNull(allModels);
