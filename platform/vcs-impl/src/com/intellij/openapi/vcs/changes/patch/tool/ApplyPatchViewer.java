14f8e50af23e6fba7b4e1186a935267adb5bdff6
Aleksey Pivovarov <AMPivovarov@gmail.com>
2016-04-28 15:44:06 +0300

    Revert "patch: apply somehow - make double click on splitter reset to default proportions"
    This reverts commit aa69c1db045b27a23602b0c2a8e1f1ddd45d3895.

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/patch/tool/ApplyPatchViewer.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/patch/tool/ApplyPatchViewer.java
@@ -98,7 +98,8 @@ class ApplyPatchViewer implements Disposable {
     patchPanel.add(myViewer.getPatchTitle(), BorderLayout.NORTH);
 
 
-    myPatchSplitter = new DiffSplitter(2f / 3);
+    myPatchSplitter = new DiffSplitter();
+    myPatchSplitter.setProportion(2f / 3);
     myPatchSplitter.setFirstComponent(myViewer.getComponent());
     myPatchSplitter.setSecondComponent(patchPanel);
     myPatchSplitter.setHonorComponentsMinimumSize(false);
