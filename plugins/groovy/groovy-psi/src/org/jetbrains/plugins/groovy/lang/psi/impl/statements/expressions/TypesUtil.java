76fa66b7ec5208b8f555cf14b2d04acaaa226c2c
Serega Vasiliev <sergey.vasiliev@jetbrains.com>
2016-04-29 14:53:36 +0200

    spring bean type processing (IDEA-155326)

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/statements/expressions/TypesUtil.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/statements/expressions/TypesUtil.java
@@ -612,15 +612,15 @@ public class TypesUtil {
   }
 
   @NotNull
-  public static PsiType getLeastUpperBound(PsiClass[] classes, PsiManager manager) {
+  public static PsiType getLeastUpperBound(PsiType[] classes, PsiManager manager) {
     PsiElementFactory factory = JavaPsiFacade.getElementFactory(manager.getProject());
 
     if (classes.length == 0) return factory.createTypeByFQClassName(CommonClassNames.JAVA_LANG_OBJECT);
 
-    PsiType type = factory.createType(classes[0]);
+    PsiType type = classes[0];
 
     for (int i = 1; i < classes.length; i++) {
-      PsiType t = getLeastUpperBound(type, factory.createType(classes[i]), manager);
+      PsiType t = getLeastUpperBound(type, classes[i], manager);
       if (t != null) {
         type = t;
       }
