bf648a827169a4d77fa8ac4bcd1902ffb907453d
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2016-05-02 17:49:03 +0300

    Properly show and hide test results tool window

--- python/educational-core/student/src/com/jetbrains/edu/learning/checker/StudyCheckUtils.java
+++ python/educational-core/student/src/com/jetbrains/edu/learning/checker/StudyCheckUtils.java
@@ -203,12 +203,15 @@ public class StudyCheckUtils {
       window = toolWindowManager.getToolWindow("Test Results");
       new StudyTestResultsToolWindowFactory().createToolWindowContent(project, window);
     }
+
     final Content[] contents = window.getContentManager().getContents();
     for (Content content : contents) {
       final JComponent component = content.getComponent();
       if (component instanceof StudyTestResultsToolWindow) {
         ((StudyTestResultsToolWindow)component).setText(message);
-        component.setVisible(true);
+        window.setAvailable(true, () -> {});
+        window.show(() -> {});
+        return;
       }
     }
   }
--- python/educational-core/student/src/com/jetbrains/edu/learning/ui/StudyTestResultsToolWindowFactory.kt
+++ python/educational-core/student/src/com/jetbrains/edu/learning/ui/StudyTestResultsToolWindowFactory.kt
@@ -1,6 +1,10 @@
 package com.jetbrains.edu.learning.ui
 
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.fileEditor.FileEditorManagerEvent
+import com.intellij.openapi.fileEditor.FileEditorManagerListener
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.wm.ToolWindow
 
 
@@ -8,10 +12,31 @@ class StudyTestResultsToolWindowFactory: StudyToolWindowFactory() {
   override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
     val testResultsToolWindow = StudyTestResultsToolWindow()
     testResultsToolWindow.init()
+
+    toolWindow.isToHideOnEmptyContent = true
     
     val contentManager = toolWindow.contentManager
     val content = contentManager.factory.createContent(testResultsToolWindow, null, false)
     contentManager.addContent(content)
+
+    project.messageBus.connect().subscribe(FileEditorManagerListener.FILE_EDITOR_MANAGER, getFileEditorManagerListener(toolWindow))
+  }
+
+  fun getFileEditorManagerListener(toolWindow: ToolWindow): FileEditorManagerListener {
+
+    return object : FileEditorManagerListener {
+
+      override fun fileOpened(source: FileEditorManager, file: VirtualFile) {
+      }
+
+      override fun fileClosed(source: FileEditorManager, file: VirtualFile) {
+        toolWindow.setAvailable(false, {})
+      }
+
+      override fun selectionChanged(event: FileEditorManagerEvent) {
+        toolWindow.setAvailable(false, {})
+      }
+    }
   }
 }
 
