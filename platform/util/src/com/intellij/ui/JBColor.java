e08e784a889805f7458930df97e46869bc33c9a9
Konstantin Bulenkov <kb@jetbrains.com>
2016-05-03 16:39:04 +0200

    introduce JBColor.isBright() for better use with lambdas

--- platform/util/src/com/intellij/ui/JBColor.java
+++ platform/util/src/com/intellij/ui/JBColor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -56,6 +56,10 @@ public class JBColor extends Color {
 
   public static void setDark(boolean dark) {
     DARK = dark;
+  }
+
+  public static boolean isBright() {
+    return !DARK;
   }
 
   Color getDarkVariant() {
