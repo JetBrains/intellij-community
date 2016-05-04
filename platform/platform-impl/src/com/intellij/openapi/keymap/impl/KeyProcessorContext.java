JetBrains/intellij-community

commit 9b6176d312318d6c9fa071656b488f26ff53611c

project leak

platform/platform-impl/src/com/intellij/openapi/keymap/impl/KeyProcessorContext.java
@@ -88,4 +88,12 @@ public class KeyProcessorContext {
   public KeyEvent getInputEvent() {
     return myInputEvent;
   }
+
+  public void clear() {
+    myInputEvent = null;
+    myActions.clear();
+    myFocusOwner = null;
+    myDataContext = null;
+    myFoundComponent = null;
+  }
 }

platform/platform-impl/src/com/intellij/openapi/keymap/impl/IdeKeyEventDispatcher.java
@@ -183,18 +183,28 @@ public final class IdeKeyEventDispatcher implements Disposable {
     myContext.setModalContext(isModalContext);
     myContext.setInputEvent(e);
 
-    if (getState() == KeyState.STATE_INIT) {
-      return inInitState();
-    } else if (getState() == KeyState.STATE_PROCESSED) {
-      return inProcessedState();
-    } else if (getState() == KeyState.STATE_WAIT_FOR_SECOND_KEYSTROKE) {
-      return inWaitForSecondStrokeState();
-    } else if (getState() == KeyState.STATE_SECOND_STROKE_IN_PROGRESS) {
-      return inSecondStrokeInProgressState();
-    } else if (getState() == KeyState.STATE_KEY_GESTURE_PROCESSOR) {
-      return myKeyGestureProcessor.process();
-    } else {
-      throw new IllegalStateException("state = " + getState());
+    try {
+      if (getState() == KeyState.STATE_INIT) {
+        return inInitState();
+      }
+      else if (getState() == KeyState.STATE_PROCESSED) {
+        return inProcessedState();
+      }
+      else if (getState() == KeyState.STATE_WAIT_FOR_SECOND_KEYSTROKE) {
+        return inWaitForSecondStrokeState();
+      }
+      else if (getState() == KeyState.STATE_SECOND_STROKE_IN_PROGRESS) {
+        return inSecondStrokeInProgressState();
+      }
+      else if (getState() == KeyState.STATE_KEY_GESTURE_PROCESSOR) {
+        return myKeyGestureProcessor.process();
+      }
+      else {
+        throw new IllegalStateException("state = " + getState());
+      }
+    }
+    finally {
+      myContext.clear();
     }
   }
 

commit 6fe346e6ece9a2acfcfc52af770b057953151745

IDEA-154620 ui: fix handling of shortcuts with second stroke for local actions
We need `myFoundComponent` to survive till the next action event, because is is used to find local actions (registered with `registerWithCustomShortcutSet`) in `inSecondStrokeInProgressState`.
It is a WeakReference, so this will not cause memory leaks.

It was broken in 9b6176d

platform/platform-impl/src/com/intellij/openapi/keymap/impl/KeyProcessorContext.java
@@ -98,6 +98,5 @@ public class KeyProcessorContext {
     myActions.clear();
     myFocusOwner = null;
     myDataContext = null;
-    myFoundComponent = null;
   }
 }