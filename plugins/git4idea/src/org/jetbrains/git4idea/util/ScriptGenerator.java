5a2a4650514355424f1c1c4ff61f0be360dc9213
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2016-05-06 17:09:30 +0200

    Cleanup (readability)

--- plugins/git4idea/src/org/jetbrains/git4idea/util/ScriptGenerator.java
+++ plugins/git4idea/src/org/jetbrains/git4idea/util/ScriptGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,16 +37,7 @@ public class ScriptGenerator {
   /**
    * The extension of the ssh script name
    */
-  @NonNls public static final String SCRIPT_EXT;
-
-  static {
-    if (SystemInfo.isWindows) {
-      SCRIPT_EXT = ".bat";
-    }
-    else {
-      SCRIPT_EXT = ".sh";
-    }
-  }
+  public static final String SCRIPT_EXT = SystemInfo.isWindows ? ".bat" : ".sh";
 
   /**
    * The script prefix
