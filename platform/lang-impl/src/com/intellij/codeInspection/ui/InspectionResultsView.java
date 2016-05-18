3f625e1ebca1ec8633aa5a78f5bef50ad9e786a2
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2016-05-18 12:10:47 +0300

    inspection toolwindow: IDEA-156157 Odd vertical line (and indent) in preview of inspection results

--- platform/lang-impl/src/com/intellij/codeInspection/ui/InspectionResultsView.java
+++ platform/lang-impl/src/com/intellij/codeInspection/ui/InspectionResultsView.java
@@ -562,7 +562,7 @@ public class InspectionResultsView extends JPanel implements Disposable, Occuren
       if (problemCount == 0) {
         myPreviewEditor.getScrollingModel().scrollTo(myPreviewEditor.offsetToLogicalPosition(selectedElement.getTextOffset()), ScrollType.CENTER_UP);
       }
-      myPreviewEditor.getSettings().setFoldingOutlineShown(problemCount != 1);
+      myPreviewEditor.getSettings().setFoldingOutlineShown(problemCount > 1);
       myPreviewEditor.getComponent().setBorder(IdeBorderFactory.createEmptyBorder());
       return Pair.create(myPreviewEditor.getComponent(), myPreviewEditor);
     }
