a0a58a2cbfe2ca43adc55ef524218152145e9274
Bas Leijdekkers <basleijdekkers@gmail.com>
2016-05-20 23:42:54 +0200

    regex: parse property inside class definition [\p{X}] correctly

--- RegExpSupport/src/org/intellij/lang/regexp/RegExpParser.java
+++ RegExpSupport/src/org/intellij/lang/regexp/RegExpParser.java
@@ -437,8 +437,8 @@ public class RegExpParser implements PsiParser {
       marker.done(RegExpElementTypes.PY_COND_REF);
     }
     else if (type == RegExpTT.PROPERTY) {
+      marker.drop();
       parseProperty(builder);
-      marker.done(RegExpElementTypes.PROPERTY);
     }
     else if (RegExpTT.SIMPLE_CLASSES.contains(type)) {
       builder.advanceLexer();
@@ -467,24 +467,26 @@ public class RegExpParser implements PsiParser {
   }
 
   private static void parseProperty(PsiBuilder builder) {
-    checkMatches(builder, RegExpTT.PROPERTY, "'\\p' expected");
-
+    final PsiBuilder.Marker marker = builder.mark();
+    builder.advanceLexer();
     if (builder.getTokenType() == RegExpTT.CATEGORY_SHORT_HAND) {
       builder.advanceLexer();
-      return;
-    }
-    checkMatches(builder, RegExpTT.LBRACE, "Character category expected");
-    if (builder.getTokenType() == RegExpTT.NAME) {
-      builder.advanceLexer();
-    }
-    else if (builder.getTokenType() == RegExpTT.RBRACE) {
-      builder.error("Empty character family");
     }
     else {
-      builder.error("Character family name expected");
-      builder.advanceLexer();
+      checkMatches(builder, RegExpTT.LBRACE, "Character category expected");
+      if (builder.getTokenType() == RegExpTT.NAME) {
+        builder.advanceLexer();
+      }
+      else if (builder.getTokenType() == RegExpTT.RBRACE) {
+        builder.error("Empty character family");
+      }
+      else {
+        builder.error("Character family name expected");
+        builder.advanceLexer();
+      }
+      checkMatches(builder, RegExpTT.RBRACE, "Unclosed character family");
     }
-    checkMatches(builder, RegExpTT.RBRACE, "Unclosed character family");
+    marker.done(RegExpElementTypes.PROPERTY);
   }
 
   private static void patternExpected(PsiBuilder builder) {
