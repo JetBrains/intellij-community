c026cef7ad61db10e621c9b2bd9523b117130422
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2016-05-20 14:37:59 +0300

    IDEA-155554 ImportMavenRepositoriesTask should be asynchronous

--- plugins/gradle/src/org/jetbrains/plugins/gradle/integrations/maven/GradleMavenProjectImportNotificationListener.java
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/integrations/maven/GradleMavenProjectImportNotificationListener.java
@@ -18,7 +18,7 @@ package org.jetbrains.plugins.gradle.integrations.maven;
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskId;
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskNotificationListenerAdapter;
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskType;
-import com.intellij.openapi.project.DumbService;
+import com.intellij.openapi.progress.util.ProgressIndicatorUtils;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.gradle.util.GradleConstants;
@@ -37,7 +37,7 @@ public class GradleMavenProjectImportNotificationListener extends ExternalSystem
         && id.getType() == ExternalSystemTaskType.RESOLVE_PROJECT) {
       final Project project = id.findProject();
       if (project == null) return;
-      DumbService.getInstance(project).smartInvokeLater(new ImportMavenRepositoriesTask(project));
+      ProgressIndicatorUtils.scheduleWithWriteActionPriority(new ImportMavenRepositoriesTask(project));
     }
   }
 }
