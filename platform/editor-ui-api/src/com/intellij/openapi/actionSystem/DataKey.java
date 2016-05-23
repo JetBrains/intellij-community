c13bc3e0c2935aa4eef31d1330310dc1745cb33a
Konstantin Bulenkov <kb@jetbrains.com>
2016-05-23 19:42:10 +0200

    deprecate key.getData(storage) approach

--- platform/editor-ui-api/src/com/intellij/openapi/actionSystem/DataKey.java
+++ platform/editor-ui-api/src/com/intellij/openapi/actionSystem/DataKey.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -69,6 +69,9 @@ public class DataKey<T> {
     return myName.equals(dataId);
   }
 
+  /**
+   * @deprecated use {@link DataContext#getData(DataKey)}
+   */
   @Nullable
   public T getData(@NotNull DataContext dataContext) {
     //noinspection unchecked
