850929cbb91ffc96e413431a058d531599ea8fc0
Yaroslav Lepenkin <yaroslav.lepenkin@jetbrains.com>
2016-05-27 17:54:40 +0300

    Make tests pass, do not use getPluginDir function in tests

--- plugins/stats-collector/test/com/intellij/stats/completion/StatisticSenderTest.kt
+++ plugins/stats-collector/test/com/intellij/stats/completion/StatisticSenderTest.kt
@@ -1,82 +1,39 @@
 package com.intellij.stats.completion
 
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.testFramework.LightPlatformTestCase
-import com.intellij.testFramework.UsefulTestCase
-import org.mockito.ArgumentCaptor
-import org.mockito.Captor
+import org.assertj.core.api.Assertions.assertThat
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.mock
-import org.mockito.MockitoAnnotations
-import org.picocontainer.MutablePicoContainer
 import java.io.File
 
+class TestFilePathProvider: UniqueFilesProvider("chunk", File("."))
 
 class StatisticsSenderTest: LightPlatformTestCase() {
-    lateinit var secondFile: File
     lateinit var firstFile: File
+    lateinit var secondFile: File
+    
     lateinit var urlProvider: UrlProvider
-    lateinit var pathProvider: FilePathProvider
     lateinit var requestService: RequestService
-    lateinit var pico: MutablePicoContainer
-    lateinit var oldFilePathProvider: FilePathProvider
-    
-    var lastSendData: Map<String, String>? = null
-    var lastSendDataUrl: String? = null
-    
-    val text = """
-1 ACTION
-2 ACTION
-3 ACTION
-"""
+    lateinit var filePathProvider: FilePathProvider
 
-    @Captor
-    private lateinit var captor: ArgumentCaptor<Map<String, String>>
-    
-    init {
-        MockitoAnnotations.initMocks(this)
-    }
-    
     override fun setUp() {
         super.setUp()
-
-        firstFile = File("chunk_0")
-        secondFile = File("chunk_1")
-
+        
+        firstFile = File("first_file")
+        secondFile = File("second_file")
+        
         urlProvider = mock(UrlProvider::class.java)
-        `when`(urlProvider.statsServerPostUrl).thenReturn("http://localhost:8080/upload")
-
-        pathProvider = mock(FilePathProvider::class.java)
-        `when`(pathProvider.getDataFiles()).thenAnswer { listOf(firstFile, secondFile) }
-
         requestService = mock(RequestService::class.java)
-        `when`(requestService.post("x", File("x"))).thenReturn(ResponseData(200))
-        
-        pico = ApplicationManager.getApplication().picoContainer as MutablePicoContainer
-        oldFilePathProvider = pico.getComponentInstance(FilePathProvider::class.java.name) as FilePathProvider
-        pico.replaceComponent(FilePathProvider::class.java, pathProvider)
-        
-        lastSendData = null
-        lastSendDataUrl = null
+        filePathProvider = mock(FilePathProvider::class.java)
     }
 
-    override fun tearDown() {
-        super.tearDown()
-        if (secondFile.exists()) {
-            secondFile.delete()
-        }
-        if (firstFile.exists()) {
-            firstFile.delete()
-        }
-    }
-
-
-    fun `test data is sent and removed`() {
-        val sender = StatisticSender(urlProvider, requestService)
-        sender.sendStatsData("uuid-secret-xxx")
+    fun `test data is sent and file are removed`() {
+        `when`(filePathProvider.getDataFiles()).thenReturn(listOf(firstFile, secondFile))
+        val sender = StatisticSender(urlProvider, requestService, filePathProvider)
+        sender.sendStatsData("uuid-secret")
         
-        UsefulTestCase.assertTrue(!secondFile.exists())
-        UsefulTestCase.assertTrue(!firstFile.exists())
+        assertThat(firstFile.exists()).isEqualTo(false)
+        assertThat(secondFile.exists()).isEqualTo(false)
     }
     
     
