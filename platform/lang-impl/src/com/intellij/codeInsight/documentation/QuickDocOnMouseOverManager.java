4ff7881d6150a6c95e8a8524e9555a9a27589abf
Dennis Ushakov <dennis.ushakov@gmail.com>
2016-05-27 17:06:06 +0300

    check whether quick doc is enabled, not only whether app is active #WEB-20554 fixed

--- platform/lang-impl/src/com/intellij/codeInsight/documentation/QuickDocOnMouseOverManager.java
+++ platform/lang-impl/src/com/intellij/codeInsight/documentation/QuickDocOnMouseOverManager.java
@@ -155,7 +155,7 @@ public class QuickDocOnMouseOverManager {
   }
   
   private void processMouseMove(@NotNull EditorMouseEvent e) {
-    if (!myApplicationActive || e.getArea() != EditorMouseEventArea.EDITING_AREA) {
+    if (!myApplicationActive || !myEnabled || e.getArea() != EditorMouseEventArea.EDITING_AREA) {
       // Skip if the mouse is not at the editing area.
       closeQuickDocIfPossible();
       return;
