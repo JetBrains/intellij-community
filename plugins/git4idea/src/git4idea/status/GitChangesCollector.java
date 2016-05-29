d060c2f5c27947a7b647d977fbf2c4dbf7a2b69f
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2016-05-29 20:18:40 +0300

    simplify to lambda, and don't wrap into new File()

--- plugins/git4idea/src/git4idea/status/GitChangesCollector.java
+++ plugins/git4idea/src/git4idea/status/GitChangesCollector.java
@@ -26,10 +26,10 @@ import com.intellij.openapi.vcs.changes.ChangeListManager;
 import com.intellij.openapi.vcs.changes.ContentRevision;
 import com.intellij.openapi.vcs.changes.VcsDirtyScope;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.util.containers.ContainerUtil;
 import com.intellij.vcsUtil.VcsUtil;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.File;
 import java.util.*;
 
 /**
@@ -108,12 +108,7 @@ abstract class GitChangesCollector {
 
     removeCommonParents(allPaths);
 
-    final List<FilePath> paths = new ArrayList<FilePath>(allPaths.size());
-    for (String p : allPaths) {
-      final File file = new File(p);
-      paths.add(VcsUtil.getFilePath(file));
-    }
-    return paths;
+    return ContainerUtil.map(allPaths, VcsUtil::getFilePath);
   }
 
   protected void addToPaths(FilePath pathToAdd, List<String> paths) {
