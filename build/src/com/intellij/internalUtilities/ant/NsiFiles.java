62c4cdd25a1ae6d0ce0d884326e529d0907bb836
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2016-05-31 14:37:05 +0300

    use FIle.separator in NsiFiles, fix idea_win.nsh on Linux

--- build/src/com/intellij/internalUtilities/ant/NsiFiles.java
+++ build/src/com/intellij/internalUtilities/ant/NsiFiles.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.intellij.internalUtilities.ant;
 
 import org.apache.tools.ant.BuildException;
@@ -159,13 +174,13 @@ public class NsiFiles extends MatchingTask {
   private void processFileSet(final FileSet fileSet) throws IOException {
     final DirectoryScanner scanner = fileSet.getDirectoryScanner(getProject());
     final String[] files = scanner.getIncludedFiles();
-    String base = fileSet.getDir(getProject()).getCanonicalPath() + "\\";
+    String base = fileSet.getDir(getProject()).getCanonicalPath() + File.separator;
     for (String file : files) {
       String lastDir = "";
       getDirFileList(lastDir);
       int idx = -1;
       do {
-        idx = file.indexOf('\\', idx + 1);
+        idx = file.indexOf(File.separator, idx + 1);
         if (idx == -1) break;
         lastDir = file.substring(0, idx);
         getDirFileList(lastDir);
