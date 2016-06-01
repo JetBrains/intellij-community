11bc063de0d46c724fa233ed11534aa9ddaaadfe
Konstantin Bulenkov <kb@jetbrains.com>
2016-06-01 17:50:26 +0200

    make isTableCellRenderer() protected

--- platform/platform-impl/src/com/intellij/ide/ui/laf/darcula/ui/DarculaComboBoxUI.java
+++ platform/platform-impl/src/com/intellij/ide/ui/laf/darcula/ui/DarculaComboBoxUI.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -213,7 +213,7 @@ public class DarculaComboBoxUI extends BasicComboBoxUI implements Border {
     paintCurrentValue(g, r, hasFocus);
   }
 
-  private static boolean isTableCellEditor(JComponent c) {
+  protected static boolean isTableCellEditor(JComponent c) {
     return Boolean.TRUE.equals(c.getClientProperty("JComboBox.isTableCellEditor")) || c.getParent() instanceof JTable;
   }
 
