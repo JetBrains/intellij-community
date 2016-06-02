4ed2ed2db27e055ce5e3b397d8d38e0a369bd2c6
peter <peter@jetbrains.com>
2016-06-02 08:23:56 +0200

    simplify and speed up JavaAllOverridingMethodsSearcher by reusing overriding methods search checks whether a class has methods with a given name

--- java/java-indexing-impl/src/com/intellij/psi/impl/search/JavaOverridingMethodsSearcher.java
+++ java/java-indexing-impl/src/com/intellij/psi/impl/search/JavaOverridingMethodsSearcher.java
@@ -137,10 +137,10 @@ public class JavaOverridingMethodsSearcher implements QueryExecutor<PsiMethod, O
   }
 
   @Nullable
-  private static PsiMethod findOverridingMethod(@NotNull Project project,
-                                                @NotNull PsiClass inheritor,
-                                                @NotNull PsiMethod method,
-                                                @NotNull PsiClass methodContainingClass) {
+  static PsiMethod findOverridingMethod(@NotNull Project project,
+                                        @NotNull PsiClass inheritor,
+                                        @NotNull PsiMethod method,
+                                        @NotNull PsiClass methodContainingClass) {
     String name = method.getName();
     if (inheritor.findMethodsByName(name, false).length > 0) {
       PsiMethod found = MethodSignatureUtil.findMethodBySuperSignature(inheritor, getSuperSignature(inheritor, methodContainingClass, method), false);
