4df0981c5a4d73ced290b39d8dcf8c83c3422795
Vasily Romanikhin <vasily.romanikhin@jetbrains.com>
2016-06-03 15:46:39 +0300

    Revert "remove InjectionBackgroundSuppressor"
    This reverts commit 58809e05d41d5ca2f26b492c18c85bed04b22245.
    discussed with Sergey Ignatov

--- platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/InjectedGeneralHighlightingPass.java
+++ platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/InjectedGeneralHighlightingPass.java
@@ -224,7 +224,7 @@ public class InjectedGeneralHighlightingPass extends GeneralHighlightingPass imp
       if (textRange.isEmpty()) continue;
       String desc = injectedPsi.getLanguage().getDisplayName() + ": " + injectedPsi.getText();
       HighlightInfo.Builder builder = HighlightInfo.newHighlightInfo(HighlightInfoType.INJECTED_LANGUAGE_BACKGROUND).range(textRange);
-      if (injectedAttributes != null) {
+      if (injectedAttributes != null && InjectedLanguageUtil.isHighlightInjectionBackground(host)) {
         builder.textAttributes(injectedAttributes);
       }
       builder.unescapedToolTip(desc);
@@ -245,7 +245,8 @@ public class InjectedGeneralHighlightingPass extends GeneralHighlightingPass imp
     highlightInjectedSyntax(injectedPsi, holder);
     for (int i = injectedStart; i < holder.size(); i++) {
       HighlightInfo info = holder.get(i);
-      final TextRange fixedTextRange = getFixedTextRange(documentWindow, info.startOffset);
+      final int startOffset = info.startOffset;
+      final TextRange fixedTextRange = getFixedTextRange(documentWindow, startOffset);
       if (fixedTextRange == null) {
         info.setFromInjection(true);
         outInfos.add(info);
