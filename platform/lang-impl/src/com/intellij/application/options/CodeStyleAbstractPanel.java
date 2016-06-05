8eae946da5102910af8f4006e9733a39c0d04581
Konstantin Bulenkov <kb@jetbrains.com>
2016-06-06 00:18:00 +0200

    remove double checking of (offsetToScroll < 0)

--- platform/lang-impl/src/com/intellij/application/options/CodeStyleAbstractPanel.java
+++ platform/lang-impl/src/com/intellij/application/options/CodeStyleAbstractPanel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -492,13 +492,11 @@ public abstract class CodeStyleAbstractPanel implements Disposable {
                                  || isWithinBounds(myEditor.offsetToVisualPosition(range.getEndOffset()), visualStart, visualEnd);
           scrollToChange = !rangeVisible;
           if (offsetToScroll < 0) {
-            if (offsetToScroll < 0) {
-              if (text.charAt(range.getStartOffset()) != '\n') {
-                offsetToScroll = range.getStartOffset();
-              }
-              else if (range.getEndOffset() > 0 && text.charAt(range.getEndOffset() - 1) != '\n') {
-                offsetToScroll = range.getEndOffset() - 1;
-              }
+            if (text.charAt(range.getStartOffset()) != '\n') {
+              offsetToScroll = range.getStartOffset();
+            }
+            else if (range.getEndOffset() > 0 && text.charAt(range.getEndOffset() - 1) != '\n') {
+              offsetToScroll = range.getEndOffset() - 1;
             }
           }
         }
