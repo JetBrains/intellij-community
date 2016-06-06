cb071c57a64b4cfc99508530821f666b8c220cea
Anna Kozlova <anna.kozlova@jetbrains.com>
2016-06-06 20:09:16 +0300

    reject imports from default package (IDEA-138822; IDEA-154024)

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/genericsHighlighting/InnerClassRef.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/genericsHighlighting/InnerClassRef.java
@@ -1,19 +1,10 @@
 import java.util.*;
-import Node.Details;
-
-class Node<E> {
-    public class Details {
-        public E data;
-    }
-    public Details addNode(Node<E> child) {
-        return new Details();
-    }
-}
+import java.util.Map.Entry;
 
 class Test {
   public static void main(String[] args) {
-     Map<String, Details> m = null;
-     Map<String, Node.Details> sorted1;
+     Map<String, Entry> m = null;
+     Map<String, Map.Entry> sorted1;
      sorted1 = m;
   }
 }
