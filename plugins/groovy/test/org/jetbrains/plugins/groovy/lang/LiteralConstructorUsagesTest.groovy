8e1e1fe0e12855631c543a711528797448bc022b
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2016-06-08 22:45:14 +0300

    [groovy] tests: implement getBasePath() (cherry picked from commit da9bca9)

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/LiteralConstructorUsagesTest.groovy
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/LiteralConstructorUsagesTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,10 +23,6 @@ import org.jetbrains.plugins.groovy.LightGroovyTestCase
  * @author peter
  */
 class LiteralConstructorUsagesTest extends LightGroovyTestCase {
-  @Override
-  protected String getBasePath() {
-    null
-  }
 
   public void testList_AsCast() throws Exception {
     def foo = myFixture.addClass("""class Foo {
