4d81d4e2c42cb86ca6b1a3ff397b373706dc5d7f
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2016-06-09 23:59:30 +0200

    do not log unneeded stuff during test run

--- platform/lang-impl/src/com/intellij/util/indexing/UnindexedFilesUpdater.java
+++ platform/lang-impl/src/com/intellij/util/indexing/UnindexedFilesUpdater.java
@@ -64,7 +64,9 @@ public class UnindexedFilesUpdater extends DumbModeTask {
   private void updateUnindexedFiles(ProgressIndicator indicator) {
     PerformanceWatcher.Snapshot snapshot = PerformanceWatcher.takeSnapshot();
     PushedFilePropertiesUpdater.getInstance(myProject).pushAllPropertiesNow();
-    snapshot.logResponsivenessSinceCreation("Pushing properties");
+    boolean trackResponsiveness = !ApplicationManager.getApplication().isUnitTestMode();
+
+    if (trackResponsiveness) snapshot.logResponsivenessSinceCreation("Pushing properties");
 
     indicator.setIndeterminate(true);
     indicator.setText(IdeBundle.message("progress.indexing.scanning"));
@@ -76,7 +78,7 @@ public class UnindexedFilesUpdater extends DumbModeTask {
 
     myIndex.filesUpdateEnumerationFinished();
 
-    snapshot.logResponsivenessSinceCreation("Indexable file iteration");
+    if (trackResponsiveness) snapshot.logResponsivenessSinceCreation("Indexable file iteration");
 
     List<VirtualFile> files = finder.getFiles();
 
@@ -91,14 +93,14 @@ public class UnindexedFilesUpdater extends DumbModeTask {
 
     snapshot = PerformanceWatcher.takeSnapshot();
 
-    LOG.info("Unindexed files update started: " + files.size() + " files to update");
+    if (trackResponsiveness) LOG.info("Unindexed files update started: " + files.size() + " files to update");
 
     indicator.setIndeterminate(false);
     indicator.setText(IdeBundle.message("progress.indexing.updating"));
 
     indexFiles(indicator, files);
 
-    snapshot.logResponsivenessSinceCreation("Unindexed files update");
+    if (trackResponsiveness) snapshot.logResponsivenessSinceCreation("Unindexed files update");
   }
 
   private void indexFiles(ProgressIndicator indicator, List<VirtualFile> files) {
