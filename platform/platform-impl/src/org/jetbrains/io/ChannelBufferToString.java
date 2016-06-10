dad432fdf76a40bb7386372cb1aa376b128a4785
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2016-06-10 17:46:05 +0200

    convert ChannelBufferToString to kotlin (cherry picked from commit 9464076)

--- platform/platform-impl/src/org/jetbrains/io/ChannelBufferToString.java
+++ platform/platform-impl/src/org/jetbrains/io/ChannelBufferToString.java
@@ -13,39 +13,50 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jetbrains.io;
+package org.jetbrains.io
 
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufUtil;
-import io.netty.buffer.ByteBufUtilEx;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import io.netty.buffer.ByteBuf
+import io.netty.buffer.ByteBufUtil
+import io.netty.util.CharsetUtil
+import java.nio.ByteBuffer
+import java.nio.CharBuffer
+import java.nio.charset.CharacterCodingException
+import java.nio.charset.CharsetDecoder
 
-import java.io.IOException;
-import java.nio.CharBuffer;
-
-public final class ChannelBufferToString {
-  @NotNull
-  public static CharSequence readChars(@NotNull ByteBuf buffer) throws IOException {
-    return new CharSequenceBackedByChars(readIntoCharBuffer(buffer, buffer.readableBytes(), null));
+fun ByteBuf.readIntoCharBuffer(byteCount: Int = readableBytes(), charBuffer: CharBuffer) {
+  val decoder = CharsetUtil.decoder(CharsetUtil.UTF_8)
+  if (nioBufferCount() == 1) {
+    decodeString(decoder, internalNioBuffer(readerIndex(), byteCount), charBuffer)
   }
-
-  @SuppressWarnings("unused")
-  @NotNull
-  public static CharSequence readChars(@NotNull ByteBuf buffer, int byteCount) throws IOException {
-    return new CharSequenceBackedByChars(readIntoCharBuffer(buffer, byteCount, null));
-  }
-
-  @NotNull
-  public static CharBuffer readIntoCharBuffer(@NotNull ByteBuf buffer, int byteCount, @Nullable CharBuffer charBuffer) throws IOException {
-    if (charBuffer == null) {
-      charBuffer = CharBuffer.allocate(byteCount);
+  else {
+    val buffer = alloc().heapBuffer(byteCount)
+    try {
+      buffer.writeBytes(this, readerIndex(), byteCount)
+      decodeString(decoder, buffer.internalNioBuffer(0, byteCount), charBuffer)
     }
-    ByteBufUtilEx.readUtf8(buffer, byteCount, charBuffer);
-    return charBuffer;
+    finally {
+      buffer.release()
+    }
+  }
+}
+
+private fun decodeString(decoder: CharsetDecoder, src: ByteBuffer, dst: CharBuffer) {
+  try {
+    var cr = decoder.decode(src, dst, true)
+    if (!cr.isUnderflow) {
+      cr.throwException()
+    }
+    cr = decoder.flush(dst)
+    if (!cr.isUnderflow) {
+      cr.throwException()
+    }
+  }
+  catch (x: CharacterCodingException) {
+    throw IllegalStateException(x)
   }
 
-  public static void writeIntAsAscii(int value, @NotNull ByteBuf buffer) {
-    ByteBufUtil.writeAscii(buffer, new StringBuilder().append(value));
-  }
+}
+
+fun writeIntAsAscii(value: Int, buffer: ByteBuf) {
+  ByteBufUtil.writeAscii(buffer, StringBuilder().append(value))
 }
