4d13852756e17302c138946b01c9b58da0c3230c
Michail Plushnikov <michail@plushnikov.de>
2016-06-11 16:28:43 +0200

    updated test data classes
    GitOrigin-RevId: adba98520ba926635a32a409e9d8d17913b7be8d

--- plugins/lombok/testData/after/builder/BuilderExampleCustomized.java
+++ plugins/lombok/testData/after/builder/BuilderExampleCustomized.java
@@ -1,38 +1,6 @@
+@lombok.experimental.Builder(builderClassName = "HelloWorldBuilder", buildMethodName = "execute",
+    builderMethodName = "helloWorld", fluent = false, chain = false)
 public class BuilderExample {
   private String name;
   private int age;
-
-  BuilderExample(String name, int age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  public static HelloWorldBuilder helloWorld() {
-    return new HelloWorldBuilder();
-  }
-
-  public static class HelloWorldBuilder {
-    private String name;
-    private int age;
-
-    HelloWorldBuilder() {
-    }
-
-    public void setName(String name) {
-      this.name = name;
-    }
-
-    public void setAge(int age) {
-      this.age = age;
-    }
-
-    public BuilderExample execute() {
-      return new BuilderExample(name, age);
-    }
-
-    @java.lang.Override
-    public String toString() {
-      return "BuilderExample.HelloWorldBuilder(name = " + this.name + ", age = " + this.age + ")";
-    }
-  }
 }
