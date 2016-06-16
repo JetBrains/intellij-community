78f77588947e754b98c158bfd9d8a9bed76a5d1e
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2016-06-16 10:29:20 +0200

    cleanup

--- java/testFramework/src/com/intellij/compiler/CompilerTestUtil.java
+++ java/testFramework/src/com/intellij/compiler/CompilerTestUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -48,12 +48,6 @@ public class CompilerTestUtil {
   public static void setupJavacForTests(Project project) {
     CompilerConfigurationImpl compilerConfiguration = (CompilerConfigurationImpl)CompilerConfiguration.getInstance(project);
     compilerConfiguration.setDefaultCompiler(compilerConfiguration.getJavacCompiler());
-  }
-
-  /**
-   * @deprecated not needed anymore
-   */
-  public static void scanSourceRootsToRecompile(Project project) {
   }
 
   @TestOnly
