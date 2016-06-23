87145fa71c474864d1925af9af087e229629ea6a
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2016-06-23 20:29:47 +0300

    [vcs-log] get rid of VcsRootsRegistry
    Since hash map stores data in a file for a specific set of roots, we do not need root indexes to be consistent in a project, only in a specific set of roots. This means we do not need to store roots on disk at all. This solves various issues with having another PE.

--- platform/vcs-log/impl/src/com/intellij/vcs/log/util/PersistentUtil.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/util/PersistentUtil.java
@@ -64,12 +64,4 @@ public class PersistentUtil {
 
     return IOUtil.openCleanOrResetBroken(() -> new PersistentEnumerator<>(storageFile, keyDescriptor, Page.PAGE_SIZE), storageFile);
   }
-
-  @NotNull
-  public static PersistentStringEnumerator createPersistentStringEnumerator(@NotNull String logKind, @NotNull String logId, int version)
-    throws IOException {
-    final File storageFile = getStorageFile(logId, logKind, version);
-
-    return IOUtil.openCleanOrResetBroken(() -> new PersistentStringEnumerator(storageFile), storageFile);
-  }
 }
