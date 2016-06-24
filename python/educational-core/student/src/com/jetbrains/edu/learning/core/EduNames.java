c6fb7615e2ea9bb9ea2845e9d2ee69cf864d6dbb
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2016-06-24 19:46:21 +0300

    EDU-277 Implement separate feature usage statistics for PyCharm Educational Edition

--- python/educational-core/student/src/com/jetbrains/edu/learning/core/EduNames.java
+++ python/educational-core/student/src/com/jetbrains/edu/learning/core/EduNames.java
@@ -40,9 +40,11 @@ public class EduNames {
 
   public static final String SANDBOX_DIR = "Sandbox";
   public static final String COURSE_META_FILE = "course.json";
-  public static final String STUDY = "Study";
   public static final String PYCHARM_ADDITIONAL = "PyCharm additional materials";
   public static final String PYCHARM = "PyCharm";
+
+  public static final String STUDY = "Study";
+  public static final String ADAPTIVE = "Adaptive";
 
   public static final String PLACEHOLDER = "Answer Placeholder";
   public static final String SRC = "src";
