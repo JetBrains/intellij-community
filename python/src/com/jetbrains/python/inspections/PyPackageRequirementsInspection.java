3ed431f8ca45e6ed5bddc272cffe69919288b5d2
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2016-06-24 22:17:15 +0300

    PY-19925 Ask user to install setuptools when SDK does NOT have management
    I fixed accidentally inverted condition in PyInstallRequirementsFix.
    
    (cherry picked from commit 0925aeac29e2d8ec556b968ed017009d4d8b5900)
    (cherry picked from commit 55a09c9cb6011afc5f2da58dda4e70d26aeeb560)

--- python/src/com/jetbrains/python/inspections/PyPackageRequirementsInspection.java
+++ python/src/com/jetbrains/python/inspections/PyPackageRequirementsInspection.java
@@ -289,7 +289,7 @@ public class PyPackageRequirementsInspection extends PyInspection {
       if (packages == null) {
         return;
       }
-      if (PyPackageUtil.hasManagement(packages)) {
+      if (!PyPackageUtil.hasManagement(packages)) {
         final int result = Messages.showYesNoDialog(project,
                                                     "Python packaging tools are required for installing packages. Do you want to " +
                                                     "install 'pip' and 'setuptools' for your interpreter?",
