243eeb0364cb2e493b2ce0607665e5c6efb3fcbd
Bas Leijdekkers <basleijdekkers@gmail.com>
2016-06-25 15:16:28 +0200

    make IG test light

--- plugins/InspectionGadgets/test/com/siyeh/igtest/bugs/math_random_cast_to_int/MathRandomCastToInt.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/bugs/math_random_cast_to_int/MathRandomCastToInt.java
@@ -3,7 +3,7 @@ package com.siyeh.igtest.bugs.math_random_cast_to_int;
 public class MathRandomCastToInt {
 
   void foo() {
-    int runs = (int) Math.random() * 1000000 * 2;
-    long random = (long) Math.random() * 10;
+    int runs = (int) <warning descr="'Math.random()' cast to 'int' is always rounded down to '0'">Math.random()</warning> * 1000000 * 2;
+    long random = (long) <warning descr="'Math.random()' cast to 'int' is always rounded down to '0'">Math.random()</warning> * 10;
   }
 }
