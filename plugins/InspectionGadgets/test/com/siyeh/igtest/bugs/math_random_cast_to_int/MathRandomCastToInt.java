b5eee7a5a2e76208bc8feaad36070f4a8b7eb195
Bas Leijdekkers <basleijdekkers@gmail.com>
2016-06-25 15:52:40 +0200

    IDEA-157857 (Inspection: Math.random()' cast to 'int' - improvement suggestions)

--- plugins/InspectionGadgets/test/com/siyeh/igtest/bugs/math_random_cast_to_int/MathRandomCastToInt.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/bugs/math_random_cast_to_int/MathRandomCastToInt.java
@@ -4,6 +4,7 @@ public class MathRandomCastToInt {
 
   void foo() {
     int runs = (int) <warning descr="'Math.random()' cast to 'int' is always rounded down to '0'">Math.random()</warning> * 1000000 * 2;
-    long random = (long) <warning descr="'Math.random()' cast to 'int' is always rounded down to '0'">Math.random()</warning> * 10;
+    long random = (long) <warning descr="'Math.random()' cast to 'long' is always rounded down to '0'">Math.random()</warning> * 10;
+    long r = (byte) -+-+-+<warning descr="'Math.random()' cast to 'byte' is always rounded down to '0'">Math.random()</warning>*8L;
   }
 }
