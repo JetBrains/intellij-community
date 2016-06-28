e1d0b2b1f656158858a2eea71ff58f021829b379
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2016-06-28 18:49:06 +0300

    Cleanup (formatting; typos)

--- java/java-tests/testSrc/com/intellij/codeInspection/DefUseTest.java
+++ java/java-tests/testSrc/com/intellij/codeInspection/DefUseTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.intellij.codeInspection;
 
 import com.intellij.JavaTestUtil;
@@ -10,35 +25,18 @@ public class DefUseTest extends InspectionTestCase {
     return JavaTestUtil.getJavaTestDataPath() + "/inspection";
   }
 
-  private void doTest() throws Exception {
+  public void testSCR5144() { doTest(); }
+  public void testSCR6843() { doTest(); }
+  public void testUnusedVariable() { doTest(); }
+  public void testArrayIndexUsages() { doTest(); }
+  public void testSCR28019() { doTest(); }
+  public void testSCR40364() { doTest(); }
+  public void testArrayLength() { doTest(); }
+  public void testUsedInArrayInitializer() { doTest(); }
+  public void testHang() { doTest(); }
+  public void testOperatorAssignment() { doTest(); }
+
+  private void doTest() {
     doTest("defUse/" + getTestName(false), new DefUseInspection());
   }
-
-
-  public void testSCR5144() throws Exception {
-    doTest();
-  }
-
-  public void testSCR6843() throws Exception {
-    doTest();
-  }
-
-  public void testunusedVariable() throws Exception {
-    doTest();
-  }
-
-  public void testarrayIndexUsages() throws Exception {
-    doTest();
-  }
-
-  // TODO:
-  public void testSCR28019() throws Exception {
-    doTest();
-  }
-
-  public void testSCR40364() throws Exception { doTest(); }
-  public void testArrayLength() throws Exception { doTest(); }
-  public void testUsedInArrayInitializer() throws Exception { doTest(); }
-  public void testHang() throws Exception { doTest(); }
-  public void testOperatorAssignment() throws Exception { doTest(); }
 }
