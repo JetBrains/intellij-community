90dc26535d5c8e06c9c6862c387231197a07cdb3
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2016-06-30 00:44:18 +0300

    PY-19773 Remember already suggested importable elements using their canonical names
    The problem was caused by the fact that we ignore imported element
    "request" in "flask/__init__.py" at first and mark this module as
    already checked (using the name "flask") and then refuse to suggest
    "flask.globals.requests" because its canonical qualified name is
    "flask.request".
    
    I've also added a test on importing functions of os.path module since
    it's a well-known case where several different symbols should be imported
    using the same name (provided by PyStdlibCanonicalPathProvider).
    My first attempt to fix the problem by saving actual import candidates
    instead of their canonical qualified names has broken exactly this
    scenario.
    (cherry picked from commit 1c3b81b)
    (cherry picked from commit 10d6e98)

---
+++ python/testSrc/com/jetbrains/python/quickFixes/AddImportQuickFixTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2000-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jetbrains.python.quickFixes;
+
+import com.intellij.codeInsight.intention.IntentionAction;
+import com.intellij.codeInspection.ex.QuickFixWrapper;
+import com.intellij.util.containers.ContainerUtil;
+import com.jetbrains.python.PyQuickFixTestCase;
+import com.jetbrains.python.codeInsight.imports.AutoImportQuickFix;
+import com.jetbrains.python.codeInsight.imports.ImportCandidateHolder;
+import com.jetbrains.python.inspections.unresolvedReference.PyUnresolvedReferencesInspection;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * @author Mikhail Golubev
+ */
+public class AddImportQuickFixTest extends PyQuickFixTestCase {
+
+  // PY-19773
+  public void testReexportedName() throws Exception {
+    doMultiFileAutoImportTest("Import 'flask.request'");
+  }
+
+  public void testOsPathFunctions() throws Exception {
+    doMultiFileAutoImportTest("Import", fix -> {
+      final List<ImportCandidateHolder> candidates = fix.getCandidates();
+      final List<String> names = ContainerUtil.map(candidates, c -> c.getPresentableText("join"));
+      assertSameElements(names, "os.path.join(path, *paths)");
+    });
+  }
+
+  private void doMultiFileAutoImportTest(@NotNull String hintPrefix) {
+    doMultiFileAutoImportTest(hintPrefix, null);
+  }
+
+  private void doMultiFileAutoImportTest(@NotNull String hintPrefix, @Nullable Consumer<AutoImportQuickFix> checkQuickfix) {
+    myFixture.copyDirectoryToProject(getTestName(true), "");
+    myFixture.enableInspections(PyUnresolvedReferencesInspection.class);
+    final String entryPoint = "main";
+    myFixture.configureByFile(entryPoint + ".py");
+    myFixture.checkHighlighting(true, false, false);
+    final List<IntentionAction> intentions = myFixture.filterAvailableIntentions(hintPrefix);
+    final IntentionAction intention = ContainerUtil.find(intentions, action -> {
+      return action instanceof QuickFixWrapper && ((QuickFixWrapper)action).getFix() instanceof AutoImportQuickFix;
+    });
+    assertNotNull("Auto import quick fix starting with '" + hintPrefix + "' wasn't found", intention);
+    final AutoImportQuickFix quickfix = (AutoImportQuickFix)((QuickFixWrapper)intention).getFix();
+    if (checkQuickfix != null) {
+      checkQuickfix.accept(quickfix);
+    }
+    myFixture.launchAction(intention);
+    myFixture.checkResultByFile(getTestName(true) + "/" + entryPoint + "_after.py", true);
+  }
+}
