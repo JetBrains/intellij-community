3cac7ca4cc108d3658f3bf723775f966f4d8353c
Bas Leijdekkers <basleijdekkers@gmail.com>
2016-06-29 14:08:13 +0200

    IG: check if resolve result is valid (IDEA-157888)

--- plugins/InspectionGadgets/test/com/siyeh/igtest/style/unnecessary_super_qualifier/UnnecessarySuperQualifier.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/style/unnecessary_super_qualifier/UnnecessarySuperQualifier.java
@@ -34,4 +34,50 @@ public class UnnecessarySuperQualifier {
             super.prot = prot;
         }
     }
+
+    public static abstract class Parent {
+
+        private final int f = 0;
+
+        private void a() {}
+
+        static class Child extends Parent {
+
+            @Override
+            public String toString() {
+                super.a();
+                return String.valueOf(super.f);
+            }
+        }
+    }
+    private class OtherChild extends Parent {
+
+        public String toString() {
+            super.a();
+            return String.valueOf(super.f);
+        }
+    }
+
+    public static abstract class Uncle {
+
+        final int f = 0;
+
+        void a() {}
+
+        static class Nephew extends Uncle {
+
+            @Override
+            public String toString() {
+                <warning descr="Qualifier 'super' is unnecessary in this context">super</warning>.a();
+                return String.valueOf(<warning descr="Qualifier 'super' is unnecessary in this context">super</warning>.f);
+            }
+        }
+    }
+    private class Niece extends Uncle {
+
+        public String toString() {
+            <warning descr="Qualifier 'super' is unnecessary in this context">super</warning>.a();
+            return String.valueOf(<warning descr="Qualifier 'super' is unnecessary in this context">super</warning>.f);
+        }
+    }
 }
