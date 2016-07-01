6f990b5fd50b39a7070366ab876d241cd9e4d257
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2016-07-01 19:53:58 +0300

    [java] file type check (EA-84623)

--- java/java-impl/src/com/intellij/codeInsight/intention/impl/AddOverrideAnnotationAction.java
+++ java/java-impl/src/com/intellij/codeInsight/intention/impl/AddOverrideAnnotationAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@ package com.intellij.codeInsight.intention.impl;
 import com.intellij.codeInsight.CodeInsightBundle;
 import com.intellij.codeInsight.intention.AddAnnotationFix;
 import com.intellij.codeInsight.intention.IntentionAction;
+import com.intellij.lang.java.JavaLanguage;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
@@ -46,6 +47,7 @@ public class AddOverrideAnnotationAction implements IntentionAction {
 
   @Override
   public boolean isAvailable(@NotNull Project project, Editor editor, PsiFile file) {
+    if (!file.getLanguage().isKindOf(JavaLanguage.INSTANCE)) return false;
     if (!PsiUtil.isLanguageLevel5OrHigher(file)) return false;
     if (!file.getManager().isInProject(file)) return false;
     PsiMethod method = findMethod(file, editor.getCaretModel().getOffset());
