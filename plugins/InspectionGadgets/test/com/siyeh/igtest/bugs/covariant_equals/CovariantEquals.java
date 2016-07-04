2d97a336b99fa43fd0daa7430f20292148f66200
Bas Leijdekkers <basleijdekkers@gmail.com>
2016-07-04 21:00:31 +0200

    IG: don't warn on covariant equals() when there is a super method

--- plugins/InspectionGadgets/test/com/siyeh/igtest/bugs/covariant_equals/CovariantEquals.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/bugs/covariant_equals/CovariantEquals.java
@@ -1,3 +1,31 @@
 interface I {
   boolean equals(I i);
 }
+class A {
+
+  public boolean <warning descr="'equals()' should take 'Object' as its argument">equals</warning>(A a) {
+    return false;
+  }
+}
+class B extends A {
+
+  @Override
+  public boolean equals(A a) {
+    return super.equals(a);
+  }
+}
+class C {
+  public boolean equals(C c) {
+    return false;
+  }
+
+  public boolean equals(Object o) {
+    return true;
+  }
+}
+class D implements I {
+  @Override
+  public boolean equals(I i) {
+    return false;
+  }
+}
