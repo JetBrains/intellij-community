d7153f459a2e2a4ae31c422a87d74a7b1a97d08c
Konstantin Bulenkov <kb@jetbrains.com>
2016-07-05 23:22:07 +0200

    cleanup new AnAction[0] -> AnAction.EMPTY_ARRAY

--- platform/lang-api/src/com/intellij/execution/configurations/CommandLineState.java
+++ platform/lang-api/src/com/intellij/execution/configurations/CommandLineState.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -108,7 +108,7 @@ public abstract class CommandLineState implements RunProfileState {
 
   protected AnAction[] createActions(final ConsoleView console, final ProcessHandler processHandler, Executor executor) {
     if (console == null || !console.canPause() || (executor != null && !DefaultRunExecutor.EXECUTOR_ID.equals(executor.getId()))) {
-      return new AnAction[0];
+      return AnAction.EMPTY_ARRAY;
     }
     return new AnAction[]{new PauseOutputAction(console, processHandler)};
   }
