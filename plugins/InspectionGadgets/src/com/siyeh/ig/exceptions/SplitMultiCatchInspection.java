a06c028a5b5ee256d9389db6ba5b428a9ec918eb
Pavel Dolgov <pavel.dolgov@jetbrains.com>
2016-07-05 16:55:31 +0300

    Java inspection: In batch mode avoid indicating the problem twice with "Split Multi-Catch into Separate Catch Blocks" inspection (IDEA-157727)

--- plugins/InspectionGadgets/src/com/siyeh/ig/exceptions/SplitMultiCatchInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/exceptions/SplitMultiCatchInspection.java
@@ -112,7 +112,7 @@ public class SplitMultiCatchInspection extends BaseInspection {
     @Override
     public void visitKeyword(PsiKeyword keyword) {
       super.visitKeyword(keyword);
-      if (isAcceptable(keyword)) {
+      if (isOnTheFly() && isAcceptable(keyword)) {
         registerError(keyword);
       }
     }
