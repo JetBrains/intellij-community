36f323a886a096ca41ce42c52dfb380afcd63b74
Yaroslav Lepenkin <yaroslav.lepenkin@jetbrains.com>
2016-07-07 17:33:01 +0300

    [freeze detector] moved to separate class

---
+++ platform/platform-api/src/com/intellij/openapi/editor/actionSystem/FreezeLogger.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2000-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.openapi.editor.actionSystem;
+
+import com.intellij.diagnostic.ThreadDumper;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.util.Alarm;
+
+public class FreezeLogger {
+  
+  private static final Logger LOG = Logger.getInstance(FreezeLogger.class);
+  private static final Alarm ALARM = new Alarm(Alarm.ThreadToUse.POOLED_THREAD, ApplicationManager.getApplication());
+  private static final int MAX_ALLOWED_TIME = 500;
+  
+  public static void runUnderPerformanceMonitor(Runnable action) {
+    ALARM.cancelAllRequests();
+    ALARM.addRequest(FreezeLogger::dumpThreads, MAX_ALLOWED_TIME);
+    
+    try {
+      action.run();
+    }
+    finally {
+      ALARM.cancelAllRequests();
+    }
+  }
+  
+  private static void dumpThreads() {
+    String dumps = ThreadDumper.dumpThreadsToString();
+    String msg = "Typing freeze report, thread dumps attached. EDT stacktrace:\n" 
+                 + ThreadDumper.dumpEdtStackTrace() 
+                 + "\n\n\n";
+    
+    LOG.error(msg, dumps);
+  }
+  
+}
