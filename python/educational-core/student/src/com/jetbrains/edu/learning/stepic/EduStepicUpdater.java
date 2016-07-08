2682dac79dc6e7a3967930aa9bca81865fe6b45f
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2016-07-08 16:54:13 +0300

    add notification about new course availability (cherry picked from commit 125987b)

--- python/educational-core/student/src/com/jetbrains/edu/learning/stepic/EduStepicUpdater.java
+++ python/educational-core/student/src/com/jetbrains/edu/learning/stepic/EduStepicUpdater.java
@@ -1,10 +1,13 @@
 package com.jetbrains.edu.learning.stepic;
 
 import com.intellij.ide.AppLifecycleListener;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.util.ActionCallback;
 import com.intellij.openapi.util.Ref;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.util.Alarm;
 import com.intellij.util.text.DateFormatUtil;
 import com.jetbrains.edu.learning.courseGeneration.StudyProjectGenerator;
@@ -45,8 +48,26 @@ public class EduStepicUpdater {
     ActionCallback callback = new ActionCallback();
     ApplicationManager.getApplication().executeOnPooledThread(() -> {
       final List<CourseInfo> courses = EduStepicConnector.getCourses();
+      final List<CourseInfo> cachedCourses = StudyProjectGenerator.getCoursesFromCache();
       StudyProjectGenerator.flushCache(courses);
       StepicUpdateSettings.getInstance().setLastTimeChecked(System.currentTimeMillis());
+
+      courses.removeAll(cachedCourses);
+      if (!courses.isEmpty()) {
+        final String message;
+        final String title;
+        if (courses.size() == 1) {
+          message = courses.get(0).getName();
+          title = "New course available";
+        }
+        else {
+          title = "New courses available";
+          message = StringUtil.join(courses, CourseInfo::getName, ", ");
+        }
+        final Notification notification =
+          new Notification("New.course", title, message, NotificationType.INFORMATION);
+        notification.notify(null);
+      }
     });
     return callback;
   }
