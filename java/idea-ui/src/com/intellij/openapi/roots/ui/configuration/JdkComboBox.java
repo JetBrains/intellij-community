5bf97584abfbe7a4f568948d3aecfe736fd32b89
Andrey Vokin <andrey.vokin@jetbrains.com>
2016-07-11 16:22:25 +0200

    RUBY-18217 Show only Ruby 2.2+ for Rails API applications in Ruby Plugin. After review changes

--- java/idea-ui/src/com/intellij/openapi/roots/ui/configuration/JdkComboBox.java
+++ java/idea-ui/src/com/intellij/openapi/roots/ui/configuration/JdkComboBox.java
@@ -66,14 +66,13 @@ public class JdkComboBox extends ComboBoxWithWidePopup {
 
   public JdkComboBox(@NotNull final ProjectSdksModel jdkModel,
                      @Nullable Condition<SdkTypeId> filter) {
-    this(jdkModel, getSdkFilter(filter), filter, false);
+    this(jdkModel, getSdkFilter(filter), filter);
   }
 
   public JdkComboBox(@NotNull final ProjectSdksModel jdkModel,
                      @Nullable Condition<Sdk> filter,
-                     @Nullable Condition<SdkTypeId> creationFilter,
-                     boolean addSuggestedItems) {
-    super(new JdkComboBoxModel(jdkModel, filter, addSuggestedItems));
+                     @Nullable Condition<SdkTypeId> creationFilter) {
+    super(new JdkComboBoxModel(jdkModel, filter));
     myFilter = filter;
     myCreationFilter = creationFilter;
     setRenderer(new ProjectJdkListRenderer() {
@@ -289,27 +288,12 @@ public class JdkComboBox extends ComboBoxWithWidePopup {
   }
 
   private static class JdkComboBoxModel extends DefaultComboBoxModel {
-    public JdkComboBoxModel(final ProjectSdksModel jdksModel, @Nullable Condition<Sdk> sdkFilter, boolean addSuggested) {
+    public JdkComboBoxModel(final ProjectSdksModel jdksModel, @Nullable Condition<Sdk> sdkFilter) {
       Sdk[] jdks = jdksModel.getSdks();
       Arrays.sort(jdks, (s1, s2) -> s1.getName().compareToIgnoreCase(s2.getName()));
       for (Sdk jdk : jdks) {
         if (sdkFilter == null || sdkFilter.value(jdk)) {
           addElement(new JdkComboBoxItem(jdk));
-        }
-      }
-      if (addSuggested) {
-        addSuggestedItems(sdkFilter, jdks);
-      }
-    }
-
-    protected void addSuggestedItems(@Nullable Condition<Sdk> sdkFilter, Sdk[] jdks) {
-      SdkType[] types = SdkType.getAllTypes();
-      for (SdkType type : types) {
-        if (sdkFilter == null || ContainerUtil.find(jdks, sdkFilter) == null) {
-          String homePath = type.suggestHomePath();
-          if (homePath != null && type.isValidSdkHome(homePath)) {
-            addElement(new SuggestedJdkItem(type, homePath));
-          }
         }
       }
     }
