e1d4d1acc4cc1545a6d7f3508d7a413a6aa6a0f7
Bas Leijdekkers <basleijdekkers@gmail.com>
2016-07-21 00:01:43 +0200

    test profile for merged throwable not thrown inspections

--- java/java-tests/testSrc/com/intellij/codeInspection/ex/InspectionProfileTest.java
+++ java/java-tests/testSrc/com/intellij/codeInspection/ex/InspectionProfileTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -357,6 +357,18 @@ public class InspectionProfileTest extends LightIdeaTestCase {
                          "    <option name=\"ADD_SERVLET_TO_ENTRIES\" value=\"true\" />\n" +
                          "    <option name=\"ADD_NONJAVA_TO_ENTRIES\" value=\"true\" />\n" +
                          "  </inspection_tool>\n" +
+                         "</profile>");
+  }
+
+  public void testMergedThrowableNotThrownInspections() throws Exception {
+    checkMergedNoChanges("<profile version=\"1.0\">\n" +
+                         "  <option name=\"myName\" value=\"" + PROFILE + "\" />\n" +
+                         "  <inspection_tool class=\"ThrowableInstanceNeverThrown\" enabled=\"false\" level=\"WARNING\" enabled_by_default=\"false\" />\n" +
+                         "  <inspection_tool class=\"ThrowableResultOfMethodCallIgnored\" enabled=\"false\" level=\"WARNING\" enabled_by_default=\"false\" />\n" +
+                         "</profile>");
+    checkMergedNoChanges("<profile version=\"1.0\">\n" +
+                         "  <option name=\"myName\" value=\"" + PROFILE + "\" />\n" +
+                         "  <inspection_tool class=\"ThrowableNotThrown\" enabled=\"false\" level=\"WARNING\" enabled_by_default=\"false\" />\n" +
                          "</profile>");
   }
 
