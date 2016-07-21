58613086c5ed7caf388a4948751703bb99ccfcb0
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2016-07-21 17:17:17 +0200

    cleanup

--- platform/platform-api/src/com/intellij/ide/passwordSafe/PasswordSafe.java
+++ platform/platform-api/src/com/intellij/ide/passwordSafe/PasswordSafe.java
@@ -16,21 +16,10 @@
 package com.intellij.ide.passwordSafe;
 
 import com.intellij.openapi.components.ServiceManager;
+import org.jetbrains.annotations.NotNull;
 
-/**
- * The password safe component. It allows storing, removing, and retrieving the passwords.
- * Note that on the first access to the password safe functions, the dialog asking for the
- * master password might have to be shown. So the access should be either done from UI thread,
- * or it should be possible to invoke {@link java.awt.EventQueue#invokeAndWait(Runnable)}
- * method when password access methods are invoked.
- *
- * @see com.intellij.ide.passwordSafe.ui.PasswordSafePromptDialog
- */
 public abstract class PasswordSafe implements PasswordStorage {
-
-  /**
-   * @return the instance of password safe service
-   */
+  @NotNull
   public static PasswordSafe getInstance() {
     return ServiceManager.getService(PasswordSafe.class);
   }
--- platform/platform-tests/testSrc/com/intellij/ide/passwordSafe/impl/providers/masterKey/windows/WindowsCryptUtilTest.java
+++ platform/platform-tests/testSrc/com/intellij/ide/passwordSafe/impl/providers/masterKey/windows/WindowsCryptUtilTest.java
@@ -1,7 +1,21 @@
+/*
+ * Copyright 2000-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.intellij.ide.passwordSafe.impl.providers.masterKey.windows;
 
 import com.intellij.openapi.util.SystemInfo;
-import com.intellij.ide.passwordSafe.MasterPasswordUnavailableException;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -12,7 +26,7 @@ import java.security.SecureRandom;
  */
 public class WindowsCryptUtilTest {
   @Test
-  public void testProtect() throws MasterPasswordUnavailableException {
+  public void testProtect() {
     if(SystemInfo.isWindows) {
       SecureRandom t = new SecureRandom();
       byte[] data = new byte[256];
