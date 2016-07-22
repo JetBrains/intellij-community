5d0cb4f64dac3e3464c2b6ed1ba2db8e84c5a2a8
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2016-07-22 22:11:09 +0300

    PY-19491 Fixed: *args, **kwargs should not trigger PyUnusedLocal inspection our should be different inspection
    Don't mark *args and **kwargs params in overridden __init__ as possibly unused while collecting write instructions

--- python/testSrc/com/jetbrains/python/PythonInspectionsTest.java
+++ python/testSrc/com/jetbrains/python/PythonInspectionsTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -112,6 +112,10 @@ public class PythonInspectionsTest extends PyTestCase {
   public void testPyUnusedLocalCoroutine() {
     myFixture.copyDirectoryToProject("inspections/" + getTestName(false), "");
     doHighlightingTest(PyUnusedLocalInspection.class, LanguageLevel.PYTHON34);
+  }
+
+  public void testPyUnusedParameterInspection() {
+    doHighlightingTest(PyUnusedLocalInspection.class);
   }
 
   // PY-20805
