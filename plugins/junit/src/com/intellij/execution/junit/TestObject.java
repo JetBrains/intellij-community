b5f5f3efb08e7a35a4ec0b77caea428b57f34480
Anna.Kozlova <anna.kozlova@jetbrains.com>
2016-07-22 12:58:15 +0200

    testing: provide unknown stubs for unregistered configuration types

--- plugins/junit/src/com/intellij/execution/junit/TestObject.java
+++ plugins/junit/src/com/intellij/execution/junit/TestObject.java
@@ -93,7 +93,7 @@ public abstract class TestObject extends JavaTestFrameworkRunnableState<JUnitCon
     if (JUnitConfiguration.BY_SOURCE_CHANGES.equals(id)) {
       return new TestsByChanges(configuration, environment);
     }
-    LOG.error(MESSAGE + id);
+    LOG.info(MESSAGE + id);
     return null;
   }
 
