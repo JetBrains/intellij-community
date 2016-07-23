49bd7d88ebaf20eee6e702265c3e25221c918603
Konstantin Bulenkov <kb@jetbrains.com>
2016-07-23 23:15:32 +0200

    show Quick Action popup under selection

--- platform/platform-api/src/com/intellij/ui/switcher/QuickActionManager.java
+++ platform/platform-api/src/com/intellij/ui/switcher/QuickActionManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -95,7 +95,7 @@ public class QuickActionManager implements ProjectComponent {
 
           JBPopupFactory.getInstance()
             .createActionGroupPopup(null, group, context, JBPopupFactory.ActionSelectionAid.ALPHA_NUMBERING, true,
-                                    () -> myActiveProvider = null, -1).showInFocusCenter();
+                                    () -> myActiveProvider = null, -1).showInBestPositionFor(context);
         }
       }
     });
