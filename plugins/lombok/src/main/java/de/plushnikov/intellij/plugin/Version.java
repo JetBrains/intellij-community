8471ee45dfb98de89613ca2d34a6f6184e6ad498
Michail Plushnikov <michail@plushnikov.de>
2016-07-23 15:36:50 +0200

    updated to next version updated gradle-plugin and lombok versions
    GitOrigin-RevId: 2331b13c72a46ca69d677e74073f61b162632aa3

--- plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/Version.java
+++ plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/Version.java
@@ -5,7 +5,7 @@ public interface Version {
   /**
    * Current plugin version.
    */
-  String PLUGIN_VERSION = "0.12";
+  String PLUGIN_VERSION = "0.13";
   /**
    * Current version of lombok plugin
    */
