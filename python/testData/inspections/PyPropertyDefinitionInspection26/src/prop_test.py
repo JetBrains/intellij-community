3ba67a51a1f85023440f55506620e7fefcfa56dc
Semyon Proshev <semyon.proshev@jetbrains.com>
2016-07-25 18:01:01 +0300

    PY-19701 Fixed: False positive "setter should not return a value" with local function definition
    In PyPropertyDefinitionInspection use control flow to check if there is raise, return or yield expression in function
    (cherry picked from commit 2955a4f)

--- python/testData/inspections/PyPropertyDefinitionInspection26/src/prop_test.py
+++ python/testData/inspections/PyPropertyDefinitionInspection26/src/prop_test.py
@@ -68,3 +68,233 @@ class A(object):
   @abstractproperty
   def abstract_property(self):
       pass
+
+
+# PY-19701
+class Test(object):
+    def __init__(self):
+        self._myprop = None
+
+    def get_myprop(self):
+        return self._myprop
+
+    def set_myprop(self, val):
+        def inner_func(n):
+            return n
+        self._myprop = inner_func(val)
+
+    myprop = property(get_myprop, set_myprop)  # pass
+
+
+# all flows have exit point
+class Test(object):
+    def __init__(self):
+        self._myprop = None
+
+    def get_myprop(self):
+        if a > b:
+            return self._myprop
+        elif a < b:
+            raise self._myprop
+        else:
+            yield self._myprop
+
+    myprop = property(get_myprop)  # pass
+
+
+# some flows have not exit point
+class Test(object):
+    def __init__(self):
+        self._myprop = None
+
+    def get_myprop(self):
+        if a > b:
+            return self._myprop
+        elif a < b:
+            raise self._myprop
+
+    myprop = property(get_myprop)  # pass
+
+
+# some flows have not exit point
+class Test(object):
+    def __init__(self):
+        self._myprop = None
+
+    def get_myprop(self):
+        if a > b:
+            return self._myprop
+
+    myprop = property(get_myprop)  # pass
+
+
+# non-empty for
+class Test(object):
+    def __init__(self):
+        self._myprop = None
+
+    def get_myprop(self):
+        for i in range(5):
+            yield i
+
+    myprop = property(get_myprop)  # pass
+
+
+# empty for
+class Test(object):
+    def __init__(self):
+        self._myprop = None
+
+    def get_myprop(self):
+        for i in []:
+            yield i
+
+    myprop = property(get_myprop)  # shouldn't pass with better analysis, pass at the moment
+
+
+# non-empty while
+class Test(object):
+    def __init__(self):
+        self._myprop = None
+
+    def get_myprop(self):
+        i = 0
+        while i < 5:
+            yield i
+            i += 1
+
+    myprop = property(get_myprop)  # pass
+
+
+# empty while
+class Test(object):
+    def __init__(self):
+        self._myprop = None
+
+    def get_myprop(self):
+        while False:
+            yield i
+
+    myprop = property(get_myprop)  # shouldn't pass with better analysis, pass at the moment
+
+
+# non-empty while with two conditions
+class Test(object):
+    def __init__(self):
+        self._myprop = None
+
+    def get_myprop(self):
+        i = 0
+        j = 0
+        while i < 5 and j == 0:
+            yield i
+            i += 1
+
+    myprop = property(get_myprop)  # pass
+
+
+# empty while with two conditions
+class Test(object):
+    def __init__(self):
+        self._myprop = None
+
+    def get_myprop(self):
+        i = 0
+        j = 0
+        while i > 5 and j == 0:
+            yield i
+
+    myprop = property(get_myprop)  # shouldn't pass with better analysis, pass at the moment
+
+
+# setter has exit point
+class Test(object):
+    def __init__(self):
+        self._myprop = None
+
+    def get_myprop(self):
+        return self._myprop
+
+    def set_myprop(self, val):
+        self._myprop = val
+        return 10
+
+    myprop = property(get_myprop, set_myprop)  # shouldn't pass
+
+
+# setter has exit point
+class Test(object):
+    def __init__(self):
+        self._myprop = None
+
+    def get_myprop(self):
+        return self._myprop
+
+    def set_myprop(self, val):
+        self._myprop = val
+        yield 10
+
+    myprop = property(get_myprop, set_myprop)  # shouldn't pass
+
+
+# setter has raise statement
+class Test(object):
+    def __init__(self):
+        self._myprop = None
+
+    def get_myprop(self):
+        return self._myprop
+
+    def set_myprop(self, val):
+        self._myprop = val
+        raise NotImplementedError()
+
+    myprop = property(get_myprop, set_myprop)  # pass
+
+
+# setter has exit point in some flow
+class Test(object):
+    def __init__(self):
+        self._myprop = None
+
+    def get_myprop(self):
+        return self._myprop
+
+    def set_myprop(self, val):
+        self._myprop = val
+        if a > b:
+            return 10
+
+    myprop = property(get_myprop, set_myprop)  # shouldn't pass
+
+
+# setter has exit point in some flow
+class Test(object):
+    def __init__(self):
+        self._myprop = None
+
+    def get_myprop(self):
+        return self._myprop
+
+    def set_myprop(self, val):
+        self._myprop = val
+        if a > b:
+            yield 10
+
+    myprop = property(get_myprop, set_myprop)  # shouldn't pass
+
+
+# setter has raise statement in some flow
+class Test(object):
+    def __init__(self):
+        self._myprop = None
+
+    def get_myprop(self):
+        return self._myprop
+
+    def set_myprop(self, val):
+        self._myprop = val
+        if a > b:
+            raise NotImplementedError()
+
+    myprop = property(get_myprop, set_myprop)  # pass
