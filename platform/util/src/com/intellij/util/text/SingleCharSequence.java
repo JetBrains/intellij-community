194e8dfe986d84f338f5d2eb669ad8201608dd5c
Alexey Kudravtsev <cdr@intellij.com>
2016-07-26 15:28:23 +0300

    make SingleCharSequence available to util

---
+++ platform/util/src/com/intellij/util/text/SingleCharSequence.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2000-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.util.text;
+
+public class SingleCharSequence implements CharSequence {
+  private final char myCh;
+
+  public SingleCharSequence(char ch) {
+    myCh = ch;
+  }
+
+  @Override
+  public int length() {
+    return 1;
+  }
+
+  @Override
+  public char charAt(int index) {
+    if (index != 0) {
+      throw new IndexOutOfBoundsException("Index out of bounds: " + index);
+    }
+    return myCh;
+  }
+
+  @Override
+  public CharSequence subSequence(int start, int end) {
+    throw new RuntimeException("Method subSequence not implemented");
+  }
+}
