38b411a565e9c7b52588ea373f8de9976c703d51
Lada Gagina <lada.gagina@jetbrains.com>
2016-07-26 19:25:01 +0300

    PY-16055 Fix test for function return type checking (Python 2.7)
    Import 'typing' module for type hints in Python 2.7 and rewrite test so now it has more cases.

--- python/testData/inspections/PyTypeCheckerInspection/FunctionReturnType.py
+++ python/testData/inspections/PyTypeCheckerInspection/FunctionReturnType.py
@@ -1,7 +1,17 @@
-def a():
+from typing import List
+
+def a(x):
+    # type: (List[int]) -> List[str]
+    return <warning descr="Expected type 'List[str]', got 'List[List[int]]' instead">[x]</warning>
+
+def b(x):
+    # type: (int) -> List[str]
+    return <warning descr="Expected type 'List[str]', got 'List[int]' instead">[1,2]</warning>
+
+def c():
     # type: () -> int
     return <warning descr="Expected type 'int', got 'str' instead">'abc'</warning>
 
-def b(x):
-    # type: (int) -> str
-    return str(x)
+def d(x):
+    # type: (x: int) -> List[str]
+    return [str(x)]
