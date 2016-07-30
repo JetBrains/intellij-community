79b865d6df10f8029b6064e265658a376ded1b57
Elizaveta Shashkova <Elizaveta.Shashkova@jetbrains.com>
2016-07-30 19:32:40 +0300

    Debugger stopping in all threads after resuming from a breakpoint (PY-20158)
    Sometimes it isn't possible to suspend thread, which is sleeping or waiting on lock. This thread is trying to suspend, but it's not suspended yet. When user is sending "Resume" command, we don't resume this thread, because it isn't suspended. After that, when this thread stops waiting, it doesn't know anything about Resume command and it's trying to suspend.
    So, we need to send Run command to all threads (suspended and non-suspended) and we should do it without using internal command.

---
+++ python/testData/debug/test_two_threads_resume.py
@@ -0,0 +1,19 @@
+from threading import Thread, RLock
+from time import sleep
+lock = RLock()
+
+
+def foo():
+    while True:
+        lock.acquire()
+        x = 12
+        sleep(0.01)
+        lock.release()
+    print("finished foo()", x)
+
+
+threads = [Thread(target=foo, name="Thread1"),
+           Thread(target=foo, name="Thread2")]
+
+for thread in threads:
+    thread.start()
