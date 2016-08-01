71c918250286497ae76a3cb22c928dbe5f851992
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2016-08-01 20:14:45 +0300

    Command-line formatter startup scripts

--- bin/scripts/win/format.bat
+++ bin/scripts/win/format.bat
@@ -4,7 +4,5 @@
 :: @@product_full@@ formatting script.
 ::----------------------------------------------------------------------
 
-SET DEFAULT_PROJECT_PATH=%CD%
-
 SET IDE_BIN_DIR=%~dp0
 CALL "%IDE_BIN_DIR%\@@script_name@@" format %*
