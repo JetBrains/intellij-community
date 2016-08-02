9b301bb9435f32aec31bd2eca757261d7fee2b65
Semyon Proshev <semyon.proshev@jetbrains.com>
2016-08-02 21:13:36 +0300

    Fix Py3UnresolvedReferencesInspectionTest.testAsyncInitMethod and PythonHighlightingTest.testAsyncBuiltinMethods (cherry picked from commit aacd5e6)

--- python/testData/highlighting/asyncBuiltinMethods.py
+++ python/testData/highlighting/asyncBuiltinMethods.py
@@ -1,8 +1,8 @@
 class A:
-    <error descr="method \"__init__\" cannot be async">async</error> def __init__(self):
+    <error descr="function \"__init__\" cannot be async">async</error> def __init__(self):
         pass
 
-    <error descr="method \"__contains__\" cannot be async">async</error> def __contains__(self, value):
+    <error descr="function \"__contains__\" cannot be async">async</error> def __contains__(self, value):
         pass
 
     async def __aiter__(self):
