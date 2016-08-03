9503fc37af56793ee2705fee65c6ecdb00aef0f9
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2016-08-03 17:31:06 +0300

    FIXED IDEA-159285 Editor doesn't auto-indent within switch-case correctly

--- platform/lang-impl/src/com/intellij/psi/impl/source/codeStyle/lineIndent/JavaLikeLangLineIndentProvider.java
+++ platform/lang-impl/src/com/intellij/psi/impl/source/codeStyle/lineIndent/JavaLikeLangLineIndentProvider.java
@@ -172,6 +172,12 @@ public abstract class JavaLikeLangLineIndentProvider extends FormatterBasedLineI
     }
     while (!position.isAtEnd()) {
       if (currLanguage == Language.ANY || currLanguage == null) currLanguage = position.getLanguage();
+      if (position.isAt(Colon)) {
+        SemanticEditorPosition afterColon = getPosition(position.getEditor(), position.getStartOffset()).after().afterOptional(Whitespace);
+        if (position.isAfterOnSameLine(SwitchCase, SwitchDefault)) {
+          return afterColon.getStartOffset();
+        }
+      }
       if (position.isAtAnyOf(Semicolon, BlockOpeningBrace, BlockClosingBrace, BlockComment, DocBlockEnd, LineComment, LeftParenthesis) ||
           (position.getLanguage() != Language.ANY) && !position.isAtLanguage(currLanguage)) {
         SemanticEditorPosition statementStart = getPosition(position.getEditor(), position.getStartOffset());
