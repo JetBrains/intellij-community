1dc85a9e41838a70b5b8355a6ed3580ac10aa467
nik <Nikolay.Chashnikov@jetbrains.com>
2016-08-05 13:42:01 +0300

    project structure: pass selected files to 'delete' action to make sure that IDEA won't try to delete the whole project (IDEA-159125)

--- platform/lang-impl/src/com/intellij/openapi/roots/ui/configuration/ContentEntryTreeEditor.java
+++ platform/lang-impl/src/com/intellij/openapi/roots/ui/configuration/ContentEntryTreeEditor.java
@@ -21,10 +21,7 @@ import com.intellij.ide.util.treeView.AbstractTreeBuilder;
 import com.intellij.ide.util.treeView.AbstractTreeStructure;
 import com.intellij.ide.util.treeView.NodeDescriptor;
 import com.intellij.idea.ActionsBundle;
-import com.intellij.openapi.actionSystem.CustomShortcutSet;
-import com.intellij.openapi.actionSystem.DataProvider;
-import com.intellij.openapi.actionSystem.DefaultActionGroup;
-import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.actionSystem.ex.CustomComponentAction;
 import com.intellij.openapi.fileChooser.FileChooserDescriptor;
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
@@ -279,6 +276,10 @@ public class ContentEntryTreeEditor {
     @Override
     @Nullable
     public Object getData(@NonNls final String dataId) {
+      if (CommonDataKeys.VIRTUAL_FILE_ARRAY.is(dataId)) {
+        return myFileSystemTree.getSelectedFiles();
+      }
+
       if (FileSystemTree.DATA_KEY.is(dataId)) {
         return myFileSystemTree;
       }
