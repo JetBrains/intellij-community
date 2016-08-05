eb3237a184c5be2516441b88a64b2f8364498b8d
Yaroslav Lepenkin <yaroslav.lepenkin@jetbrains.com>
2016-08-05 17:42:30 +0300

    added test for StatusInfoProvider

--- plugins/stats-collector/test/com/intellij/stats/completion/experiment/StatusInfoProviderTest.kt
+++ plugins/stats-collector/test/com/intellij/stats/completion/experiment/StatusInfoProviderTest.kt
@@ -1,36 +1,65 @@
 package com.intellij.stats.completion.experiment
 
-import com.intellij.stats.completion.SimpleRequestService
+import com.intellij.stats.completion.RequestService
+import com.intellij.stats.completion.ResponseData
 import com.intellij.testFramework.LightIdeaTestCase
+import org.assertj.core.api.Assertions.assertThat
+import org.mockito.Matchers
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
 
 class StatusInfoProviderTest : LightIdeaTestCase() {
 
+
+    fun newResponse(status: String, salt: String, version: String, url: String) = """
+{
+  "status" : "$status",
+  "salt" : "$salt",
+  "experimentVersion" : $version,
+  "url": "$url"  
+}
+"""
+    
     fun `test experiment info is fetched`() {
-        
-//        val requestSender = mock(RequestService::class.java)
-//        `when`(requestSender.get(Matchers.anyString()))
-//                .thenReturn(ResponseData(200, "{ performExperiment : true, experimentVersion: 10 }"))
+        val response = newResponse("ok", "sdfs", "2", "http://test.jetstat-resty.aws.intellij.net/uploadstats")
+        val infoProvider = getProvider(response)
 
-//        val urlProvider = mock(UrlProvider::class.java)
-//        `when`(urlProvider.experimentDataUrl).thenReturn("http://qq.da")
+        infoProvider.updateExperimentData()
 
-//        val helper = StatusInfoProvider(requestSender, urlProvider)
-//        helper.unsetLastUpdate()
+        assertThat(infoProvider.getDataServerUrl()).isEqualTo("http://test.jetstat-resty.aws.intellij.net/uploadstats")
+        assertThat(infoProvider.isServerOk()).isEqualTo(true)
+        assertThat(infoProvider.getExperimentVersion()).isEqualTo(2)
+    }
+
+    fun `test server is not ok`() {
+        val response = newResponse("maintance", "sdfs", "2", "http://xxx.xxx")
+        val infoProvider = getProvider(response)
+
+        infoProvider.updateExperimentData()
+
+        assertThat(infoProvider.isServerOk()).isEqualTo(false)
+        assertThat(infoProvider.getExperimentVersion()).isEqualTo(2)
+        assertThat(infoProvider.getDataServerUrl()).isEqualTo("http://xxx.xxx")
+    }
+
+    fun `test round to Int`() {
+        var response = newResponse("maintance", "sdfs", "2.9", "http://xxx.xxx")
+        var infoProvider = getProvider(response)
+        infoProvider.updateExperimentData()
+        assertThat(infoProvider.getExperimentVersion()).isEqualTo(2)
         
-//        val performExperiment = helper.isPerformExperiment()
-//        val version = helper.getExperimentVersion()
-        
-//        UsefulTestCase.assertEquals(performExperiment, true)
-//        UsefulTestCase.assertEquals(version, 10)
+        response = newResponse("maintance", "sdfs", "2.1", "http://xxx.xxx")
+        infoProvider = getProvider(response)
+        infoProvider.updateExperimentData()
+        assertThat(infoProvider.getExperimentVersion()).isEqualTo(2)
+    }
 
 
-        val info = StatusInfoProvider(SimpleRequestService())
-        info.updateExperimentData()
-
-        println()
-        
-        
-        
+    private fun getProvider(response: String): StatusInfoProvider {
+        val requestSender = mock(RequestService::class.java)
+        `when`(requestSender.get(Matchers.anyString()))
+                .thenReturn(ResponseData(200, response))
+        return StatusInfoProvider(requestSender)
     }
 
 }
