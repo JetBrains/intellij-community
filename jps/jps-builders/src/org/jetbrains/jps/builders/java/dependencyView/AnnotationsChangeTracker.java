ab37a7c8e5bc1ba90574c0934e9ad6cc23b12a65
Eugene Zhuravlev <jeka@intellij.com>
2016-08-08 18:55:35 +0200

    javadocs

--- jps/jps-builders/src/org/jetbrains/jps/builders/java/dependencyView/AnnotationsChangeTracker.java
+++ jps/jps-builders/src/org/jetbrains/jps/builders/java/dependencyView/AnnotationsChangeTracker.java
@@ -22,19 +22,61 @@ import java.util.EnumSet;
 import java.util.Set;
 
 /**
+ * This class defines an extension, allowing to customise dependency analysis in cases when JPS detects, that
+ * annotations for certain program elements are changed. Currently annotations on classes, fields and methods are tracked.
+ * For methods, their parameter annotations are tracked too.
+ * Every time changes in annotation set on mentioned program elements are detected, all registered extensions are queried, and
+ * all requested dependencies are marked for recompilation.
+ *
+ * To register custom AnnotationsChangeTracker, a JPS plugin must:
+ * - define a subclass of this class implementing desired callback methods
+ * - register this implementation via standard java Service Provider Interface mechanism:
+ *   create a file 'META-INF/services/org.jetbrains.jps.builders.java.dependencyView.AnnotationsChangeTracker'
+ *   containing fully qualified name of extension implementation, e.g. "org.plugin-name.MyAnnotationsChangeTrackerImpl"
+ *
  * @author Eugene Zhuravlev
  *         Date: 25-Jul-16
  * @noinspection UnusedParameters
  */
 public abstract class AnnotationsChangeTracker {
 
+  /**
+   * Enumeration defining all possile places that extension requests to recompile
+   */
   public enum Recompile {
-    USAGES, SUBCLASSES;
+    /**
+     * If present in the returned result set, the usages of the annotated program element (class, field, method) will be affected.
+     * it means that files where this program element is references, will be marked for recompilation
+     */
+    USAGES,
+
+    /**
+     * If present in the returned result set, the subclasses of the annotated class will be affected.
+     * If returned for an annotated field/method, the subclasses of the class containing this field/method will be affected.
+     */
+    SUBCLASSES;
   }
 
+  /**
+   * Utility object specifying extension's return result, when all possible dependencies on the item being examined, should be recompiled
+   * See {@link Recompile}
+   */
   public static final Set<Recompile> RECOMPILE_ALL = Collections.unmodifiableSet(EnumSet.allOf(Recompile.class));
+
+  /**
+   * Utility object specifying extension's return result, indicating that no additional files should be recompiled.
+   * See {@link Recompile}
+   */
   public static final Set<Recompile> RECOMPILE_NONE = Collections.unmodifiableSet(EnumSet.noneOf(Recompile.class));
 
+  /**
+   * Invoked when changes in annotation list or parameter annotations for some method are detected
+   * @param context
+   * @param method the method in question
+   * @param annotationsDiff differences descriptor for annotations on the method
+   * @param paramAnnotationsDiff differences descriptor on method parameters annotations
+   * @return a set of specifiers, determining what places in the program should be recompiled, see {@link Recompile}
+   */
   @NotNull
   public Set<Recompile> methodAnnotationsChanged(
     DependencyContext context, MethodRepr method,
@@ -44,11 +86,25 @@ public abstract class AnnotationsChangeTracker {
     return RECOMPILE_NONE;
   }
 
+  /**
+   * Invoked when changes in annotation list for some field are detected
+   * @param context
+   * @param field the field in question
+   * @param annotationsDiff differences descriptor for annotations on the field
+   * @return a set of specifiers, determining what places in the program should be recompiled, see {@link Recompile}
+   */
   @NotNull
   public Set<Recompile> fieldAnnotationsChanged(DependencyContext context, FieldRepr field, Difference.Specifier<TypeRepr.ClassType, Difference> annotationsDiff) {
     return RECOMPILE_NONE;
   }
 
+  /**
+   * Invoked when changes in annotation list for some class are detected
+   * @param context
+   * @param aClass the class in question
+   * @param annotationsDiff differences descriptor for the class annotations
+   * @return a set of specifiers, determining what places in the program should be recompiled, see {@link Recompile}
+   */
   @NotNull
   public Set<Recompile> classAnnotationsChanged(DependencyContext context, ClassRepr aClass, Difference.Specifier<TypeRepr.ClassType, Difference> annotationsDiff) {
     return RECOMPILE_NONE;
