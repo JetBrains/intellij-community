4a6544d655fde4805f12e75351aa661422201f11
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2016-08-09 21:33:58 +0300

    [groovy] unnecessary 'def' modifier inspection (IDEA-141044)

---
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/codeInspection/style/GrUnnecessaryDefModifierInspection.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2000-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jetbrains.plugins.groovy.codeInspection.style;
+
+import com.intellij.codeInspection.ProblemHighlightType;
+import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.plugins.groovy.codeInspection.GroovySuppressableInspectionTool;
+import org.jetbrains.plugins.groovy.codeInspection.bugs.GrModifierFix;
+import org.jetbrains.plugins.groovy.lang.lexer.GroovyTokenTypes;
+import org.jetbrains.plugins.groovy.lang.psi.GroovyElementVisitor;
+import org.jetbrains.plugins.groovy.lang.psi.GroovyPsiElementVisitor;
+import org.jetbrains.plugins.groovy.lang.psi.api.auxiliary.modifiers.GrModifier;
+import org.jetbrains.plugins.groovy.lang.psi.api.auxiliary.modifiers.GrModifierList;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrVariable;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrVariableDeclaration;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.params.GrParameter;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.members.GrMethod;
+
+public class GrUnnecessaryDefModifierInspection extends GroovySuppressableInspectionTool {
+
+  private static final GrModifierFix FIX = new GrModifierFix(
+    "Remove 'def'", GrModifier.DEF, false, GrModifierFix.MODIFIER_LIST_CHILD
+  );
+
+  @NotNull
+  @Override
+  public PsiElementVisitor buildVisitor(@NotNull ProblemsHolder holder, boolean isOnTheFly) {
+    return new GroovyPsiElementVisitor(new GroovyElementVisitor() {
+      @Override
+      public void visitModifierList(GrModifierList modifierList) {
+        PsiElement parent = modifierList.getParent();
+        if (!(parent instanceof GrMethod) && !(parent instanceof GrParameter) && !(parent instanceof GrVariableDeclaration)) return;
+        if (!modifierList.hasExplicitModifier(GrModifier.DEF)) return;
+
+        PsiElement modifier = findDefModifierElement(modifierList);
+        if (modifier == null) return;
+
+        if (parent instanceof GrMethod && ((GrMethod)parent).getReturnTypeElementGroovy() != null ||
+            parent instanceof GrVariable && ((GrVariable)parent).getTypeElementGroovy() != null ||
+            parent instanceof GrVariableDeclaration && ((GrVariableDeclaration)parent).getTypeElementGroovy() != null) {
+          holder.registerProblem(modifier, "'def' is not necessary", ProblemHighlightType.GENERIC_ERROR_OR_WARNING, FIX);
+        }
+      }
+    });
+  }
+
+  @Nullable
+  private static PsiElement findDefModifierElement(@NotNull GrModifierList modifierList) {
+    PsiElement def = null;
+    for (PsiElement element : modifierList.getModifiers()) {
+      if (element.getNode().getElementType().equals(GroovyTokenTypes.kDEF)) {
+        def = element;
+      }
+    }
+    return def;
+  }
+}
---
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/codeInspection/style/GrUnnecessaryDefModifierInspectionTest.groovy
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2000-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jetbrains.plugins.groovy.codeInspection.style
+
+import com.intellij.testFramework.LightProjectDescriptor
+import groovy.transform.CompileStatic
+import org.jetbrains.plugins.groovy.GroovyLightProjectDescriptor
+import org.jetbrains.plugins.groovy.LightGroovyTestCase
+
+@CompileStatic
+public class GrUnnecessaryDefModifierInspectionTest extends LightGroovyTestCase {
+
+  LightProjectDescriptor projectDescriptor = GroovyLightProjectDescriptor.GROOVY_LATEST
+
+  void 'test highlighting and fix'() {
+    fixture.with {
+      enableInspections GrUnnecessaryDefModifierInspection
+      configureByText '_.groovy', '''\
+def foo(<weak_warning descr="'def' is not necessary">def</weak_warning> Object a) {}
+<weak_warning descr="'def' is not necessary">d<caret>ef</weak_warning> boolean baz(def a) {}
+<weak_warning descr="'def' is not necessary">def</weak_warning> Object bar
+def baf
+def (int a, b) = [1, 2]
+'''
+      checkHighlighting()
+      launchAction findSingleIntention("Fix all 'Unnecessary 'def''")
+      checkResult '''\
+def foo(Object a) {}
+
+boolean baz(def a) {}
+
+Object bar
+def baf
+def (int a, b) = [1, 2]
+'''
+    }
+  }
+}
