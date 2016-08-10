2096465a4cffecd6b0ec14dff66d278203ddcd1f
Alexey Kudravtsev <cdr@intellij.com>
2016-08-10 11:49:50 +0300

    remove highly non-obvious not necessary anymore code

--- java/jsp-openapi/src/com/intellij/psi/jsp/JavaJspRecursiveElementVisitor.java
+++ java/jsp-openapi/src/com/intellij/psi/jsp/JavaJspRecursiveElementVisitor.java
@@ -15,9 +15,8 @@
  */
 package com.intellij.psi.jsp;
 
-import com.intellij.psi.PsiReferenceExpression;
-import com.intellij.psi.PsiBinaryExpression;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiReferenceExpression;
 import com.intellij.util.containers.Stack;
 
 /**
@@ -27,31 +26,12 @@ public abstract class JavaJspRecursiveElementVisitor extends JavaJspElementVisit
   // This stack thing is intended to prevent exponential child traversing due to visitReferenceExpression calls both visitRefElement
   // and visitExpression.
   private final Stack<PsiReferenceExpression> myRefExprsInVisit = new Stack<>();
-  private final Stack<PsiBinaryExpression> myBinaryExpressions = new Stack<>();
 
   @Override
   public void visitElement(PsiElement element) {
     if (!myRefExprsInVisit.isEmpty() && myRefExprsInVisit.peek() == element) {
       myRefExprsInVisit.pop();
       myRefExprsInVisit.push(null);
-    }
-    else if (element instanceof PsiBinaryExpression) {
-      //implement smart traversing to avoid stack overflow
-      if (!myBinaryExpressions.isEmpty() && myBinaryExpressions.peek() == element) {
-        return;
-      }
-      PsiElement child = element.getFirstChild();
-      while (child != null) {
-        if (child instanceof PsiBinaryExpression) {
-          myBinaryExpressions.push((PsiBinaryExpression)child);
-        }
-        child.accept(this);
-        child = child.getNextSibling();
-        if (child == null) {
-          child = myBinaryExpressions.isEmpty() ? null : myBinaryExpressions.pop();
-          if (child != null) child = child.getFirstChild();
-        }
-      }
     }
     else {
       element.acceptChildren(this);
