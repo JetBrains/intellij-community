8cf829b5a9a058f34f5727532329b201a5fa105f
Bas Leijdekkers <basleijdekkers@gmail.com>
2016-08-11 18:44:38 +0200

    regexp: fix test

--- RegExpSupport/test/org/intellij/lang/regexp/RegExpCompletionTest.java
+++ RegExpSupport/test/org/intellij/lang/regexp/RegExpCompletionTest.java
@@ -60,7 +60,7 @@ public class RegExpCompletionTest extends CodeInsightFixtureTestCase {
     public void testBackSlashVariants() throws Throwable {
         List<String> nameList =
           new ArrayList<>(Arrays.asList("d", "D", "s", "S", "w", "W", "b", "B", "A", "G", "Z", "z", "Q", "E",
-                                        "t", "n", "r", "f", "a", "e", "h", "H", "v", "V", "R", "X"));
+                                        "t", "n", "r", "f", "a", "e", "h", "H", "v", "V", "R", "X", "b{g}"));
         for (String[] stringArray : DefaultRegExpPropertiesProvider.getInstance().getAllKnownProperties()) {
             nameList.add("p{" + stringArray[0] + "}");
         }
