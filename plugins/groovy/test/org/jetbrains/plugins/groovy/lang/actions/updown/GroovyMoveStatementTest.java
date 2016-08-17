7ce07666ede766bef5c0b4a8989a7a82a47efcd9
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2016-08-17 21:15:12 +0300

    [groovy] move up/down: fix moving fields with groovy docs (IDEA-159925) (IDEA-CR-13095) (cherry picked from commit 0c8e117)

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/actions/updown/GroovyMoveStatementTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/actions/updown/GroovyMoveStatementTest.java
@@ -1,5 +1,6 @@
 /*
- * Copyright 2000-2008 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -65,6 +66,7 @@ public class GroovyMoveStatementTest extends GroovyEditorActionTestBase {
   public void testMoveGroovydocWithMethod() throws Throwable { bothTest(); }
   public void testLeaveGroovydocWithMethod() throws Throwable { bothTest(); }
   public void testMoveMethodWithGroovydoc() throws Throwable { bothTest(); }
+  public void testMoveGroovyDocWithField() throws Throwable { bothTest(); }
   
   public void testMoveSecondFieldUp() throws Throwable { bothTest(); }
   public void testMoveFirstFieldDown() throws Throwable { bothTest(); }
