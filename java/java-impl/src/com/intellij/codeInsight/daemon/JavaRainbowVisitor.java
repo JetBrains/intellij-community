bffa818bd6069d46d9cd9700cb85773e811dbffb
Alexey Kudravtsev <cdr@intellij.com>
2016-08-17 19:46:27 +0300

    performance: avoid excessive Pair allocations

--- java/java-impl/src/com/intellij/codeInsight/daemon/JavaRainbowVisitor.java
+++ java/java-impl/src/com/intellij/codeInsight/daemon/JavaRainbowVisitor.java
@@ -18,7 +18,6 @@ package com.intellij.codeInsight.daemon;
 import com.intellij.codeInsight.daemon.impl.HighlightInfo;
 import com.intellij.codeInsight.daemon.impl.HighlightVisitor;
 import com.intellij.ide.highlighter.JavaHighlightingColors;
-import com.intellij.openapi.util.Pair;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.source.javadoc.PsiDocParamRef;
 import com.intellij.psi.javadoc.PsiDocTagValue;
@@ -40,29 +39,30 @@ public class JavaRainbowVisitor extends RainbowVisitor {
         || element instanceof PsiDocParamRef) {
       PsiElement context = PsiTreeUtil.findFirstParent(element, p -> p instanceof PsiMethod);
       if (context != null) {
-        HighlightInfo attrs = getRainbowSymbolKey(
-          context,
-          element instanceof PsiReferenceExpression
-          ? Pair.create(element, ((PsiReferenceExpression)element).resolve())
-          : element instanceof PsiDocParamRef
-            ? Pair.create(element, element.getReference() == null ? null : element.getReference().resolve())
-            : Pair.create(((PsiVariable)element).getNameIdentifier(), element));
+        PsiElement rainbowElement = element instanceof PsiReferenceExpression || element instanceof PsiDocParamRef
+                            ? element : ((PsiVariable)element).getNameIdentifier();
+        PsiElement resolved = element instanceof PsiReferenceExpression
+                             ? ((PsiReferenceExpression)element).resolve()
+                             : element instanceof PsiDocParamRef
+                               ? element.getReference() == null ? null : element.getReference().resolve()
+                               : element;
+        HighlightInfo attrs = getRainbowSymbolKey(context, rainbowElement, resolved);
         addInfo(attrs);
       }
     }
   }
 
   @Nullable
-  private HighlightInfo getRainbowSymbolKey(@NotNull PsiElement context, @NotNull Pair<PsiElement, PsiElement> rainbow) {
-    if (rainbow.first == null || rainbow.second == null) {
+  private HighlightInfo getRainbowSymbolKey(@NotNull PsiElement context, PsiElement rainbowElement, PsiElement resolved) {
+    if (rainbowElement == null || resolved == null) {
       return null;
     }
-    if (rainbow.second instanceof PsiLocalVariable || rainbow.second instanceof PsiParameter) {
-      String name = ((PsiVariable)rainbow.second).getName();
+    if (resolved instanceof PsiLocalVariable || resolved instanceof PsiParameter) {
+      String name = ((PsiVariable)resolved).getName();
       if (name != null) {
-        return getInfo(context, rainbow.first, name, rainbow.second instanceof PsiLocalVariable
+        return getInfo(context, rainbowElement, name, resolved instanceof PsiLocalVariable
                                                      ? JavaHighlightingColors.LOCAL_VARIABLE_ATTRIBUTES
-                                                     : rainbow.first instanceof PsiDocTagValue
+                                                     : rainbowElement instanceof PsiDocTagValue
                                                        ? JavaHighlightingColors.DOC_COMMENT_TAG_VALUE
                                                        : JavaHighlightingColors.PARAMETER_ATTRIBUTES);
       }
