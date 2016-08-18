5ebd640aa6f4cd59474a5dcbe933ecd261bcdc8f
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2016-08-18 13:01:41 +0300

    remove unusable logging

--- jps/jps-builders/src/org/jetbrains/jps/classFilesIndex/indexer/api/ClassFilesIndicesBuilder.java
+++ jps/jps-builders/src/org/jetbrains/jps/classFilesIndex/indexer/api/ClassFilesIndicesBuilder.java
@@ -33,8 +33,6 @@ import org.jetbrains.org.objectweb.asm.ClassWriter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author Dmitry Batkovich
@@ -45,8 +43,6 @@ public class ClassFilesIndicesBuilder extends BaseInstrumentingBuilder {
   private static final String PROGRESS_MESSAGE = "Indexing class-files...";
   public static final String PROPERTY_NAME = "intellij.compiler.output.index";
 
-  private final AtomicLong myMs = new AtomicLong(0);
-  private final AtomicInteger myFilesCount = new AtomicInteger(0);
   private final Collection<ClassFilesIndexWriter> myIndexWriters = new ArrayList<ClassFilesIndexWriter>();
 
   @Override
@@ -91,13 +87,10 @@ public class ClassFilesIndicesBuilder extends BaseInstrumentingBuilder {
     if (!isEnabled()) {
       return;
     }
-    final long ms = System.currentTimeMillis();
     for (final ClassFilesIndexWriter index : myIndexWriters) {
       index.close(context);
     }
     myIndexWriters.clear();
-    myMs.addAndGet(System.currentTimeMillis() - ms);
-    LOG.info("class files indexing finished for " + myFilesCount.get() + " files in " + myMs.get() + "ms");
   }
 
   @Nullable
@@ -107,7 +100,6 @@ public class ClassFilesIndicesBuilder extends BaseInstrumentingBuilder {
                                      final ClassReader reader,
                                      final ClassWriter writer,
                                      final InstrumentationClassFinder finder) {
-    final long ms = System.currentTimeMillis();
     String className = compiled.getClassName();
     if (className == null) {
       LOG.debug("class name is empty for " + compiled.getOutputFile().getAbsolutePath());
@@ -118,8 +110,6 @@ public class ClassFilesIndicesBuilder extends BaseInstrumentingBuilder {
         index.update(className, reader);
       }
     }
-    myMs.addAndGet(System.currentTimeMillis() - ms);
-    myFilesCount.incrementAndGet();
     return null;
   }
 
