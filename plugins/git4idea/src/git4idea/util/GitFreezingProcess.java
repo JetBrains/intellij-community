9753434ca0c48777ed5d0a6d3edeeeebecc092be
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2016-08-18 23:00:09 +0300

    IDEA-159580 fix ChangeListManager.freeze() method which hangs sometimes.
    Semaphore should be turned down() before up(),
    otherwise it may waitFor().
    
    Using standard Semaphore instead of SemaphoreContinuationContext
    since the architecture of the latter doesn't differ down() and waitFor().
    
    In addition to that, make the waitFor() method cancellable.
    
    This fix is for branch 162.
    The deprecated API is not used externally and will be removed in master.

--- plugins/git4idea/src/git4idea/util/GitFreezingProcess.java
+++ plugins/git4idea/src/git4idea/util/GitFreezingProcess.java
@@ -23,7 +23,7 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ex.ProjectManagerEx;
 import com.intellij.openapi.vcs.changes.ChangeListManager;
 import com.intellij.openapi.vcs.changes.ChangeListManagerEx;
-import com.intellij.util.continuation.SemaphoreContinuationContext;
+import com.intellij.openapi.vcs.changes.ChangeListManagerImpl;
 import org.jetbrains.annotations.NotNull;
 
 import static com.intellij.openapi.application.ModalityState.defaultModalityState;
@@ -94,8 +94,7 @@ public class GitFreezingProcess {
   }
 
   private void freeze() {
-    myChangeListManager.freeze(new SemaphoreContinuationContext(),
-                               "Local changes are not available until Git " + myOperationTitle + " is finished.");
+    ((ChangeListManagerImpl)myChangeListManager).freeze("Local changes are not available until Git " + myOperationTitle + " is finished.");
   }
 
   private void unfreeze() {
