4f8cb9b5e85101263bdcc637f5d080333740bb1b
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2016-08-26 12:30:12 +0300

    FIXED IDEA-159856 Colors & Fonts > Import button is present in IDEA-based IDEs and does nothing

--- platform/lang-impl/src/com/intellij/application/options/colors/SchemesPanel.java
+++ platform/lang-impl/src/com/intellij/application/options/colors/SchemesPanel.java
@@ -132,6 +132,7 @@ public class SchemesPanel extends JPanel implements SkipSelfSearchComponent {
         chooseAndImport();
       }
     });
+    myImportButton.setVisible(isImportAvailable());
     panel.add(myImportButton,
               new GridBagConstraints(gridx++, 0, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.NONE, new JBInsets(0, 0, 5, 5), 0,
                                      0));
@@ -247,5 +248,8 @@ public class SchemesPanel extends JPanel implements SkipSelfSearchComponent {
       }
     }
   }
-  
+
+  private static boolean isImportAvailable() {
+    return !SchemeImporterEP.getExtensions(EditorColorsScheme.class).isEmpty();
+  }
 }
