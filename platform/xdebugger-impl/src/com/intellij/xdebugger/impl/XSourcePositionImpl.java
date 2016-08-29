75a55df00440969f0bda8fa61d8fca1a18358a7e
Egor.Ushakov <egor.ushakov@jetbrains.com>
2016-08-29 11:56:46 +0300

    use api method

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/XSourcePositionImpl.java
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/XSourcePositionImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -83,6 +83,9 @@ public class XSourcePositionImpl implements XSourcePosition {
     }
   }
 
+  /**
+   * do not call this method from plugins, use {@link XDebuggerUtil#createPositionByElement(PsiElement)} instead
+   */
   @Nullable
   public static XSourcePositionImpl createByElement(@Nullable PsiElement element) {
     if (element == null) return null;
