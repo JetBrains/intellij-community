0dca7cc516a0a1af0b1ab2f1675ad01081fb4e6d
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2016-09-01 17:13:07 +0200

    Fix build take 2.

--- build/groovy/org/jetbrains/intellij/build/CommunityRepositoryModules.groovy
+++ build/groovy/org/jetbrains/intellij/build/CommunityRepositoryModules.groovy
@@ -149,9 +149,7 @@ class CommunityRepositoryModules {
       withOptionalModule("tasks-java")
       doNotCreateSeparateJarForLocalizableResources()
     },
-    plugin("terminal") {
-      withResource("lib/jediterm-sh.in", "lib")
-    },
+    plugin("terminal"),
     plugin("editorconfig"),
     plugin("coverage"),
     plugin("yaml"),
