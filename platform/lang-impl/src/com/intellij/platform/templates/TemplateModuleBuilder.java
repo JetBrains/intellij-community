dd6274a3daccf743c94f55ce1cdaee2bb2313292
Shaverdova Elena <Elena.Shaverdova@jetbrains.com>
2016-09-01 20:23:42 +0200

    Fix exception on new project creation from template

--- platform/lang-impl/src/com/intellij/platform/templates/TemplateModuleBuilder.java
+++ platform/lang-impl/src/com/intellij/platform/templates/TemplateModuleBuilder.java
@@ -429,7 +429,7 @@ public class TemplateModuleBuilder extends ModuleBuilder {
           }
         }
       }));
-    }, ModalityState.any());
+    }, ModalityState.defaultModalityState());
     return projectRef.get();
   }
 
