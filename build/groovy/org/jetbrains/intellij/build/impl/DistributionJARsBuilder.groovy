3f38d6a2105b16a27280be28280bb9ae7af907ec
nik <Nikolay.Chashnikov@jetbrains.com>
2016-09-02 15:18:05 +0300

    build scripts: don't specify too restrictive since-build for non-bundled plugins - 2

--- build/groovy/org/jetbrains/intellij/build/impl/DistributionJARsBuilder.groovy
+++ build/groovy/org/jetbrains/intellij/build/impl/DistributionJARsBuilder.groovy
@@ -355,7 +355,7 @@ class DistributionJARsBuilder {
                       replace: "<idea-version since-build=\"${sinceBuild}\" until-build=\"${untilBuild}\"")
     buildContext.ant.replaceregexp(file: pluginXmlPath,
                                    match: "<idea-version\\s*since-build=\"\\d+\\.\\d+\"",
-                                   replace: "<idea-version since-build=\"${buildNumber}\"")
+                                   replace: "<idea-version since-build=\"${sinceBuild}\"")
     buildContext.ant.replaceregexp(file: pluginXmlPath,
                       match: "<change-notes>\\s*<\\!\\[CDATA\\[\\s*Plugin version: \\\$\\{version\\}",
                       replace: "<change-notes>\n<![CDATA[\nPlugin version: ${buildNumber}")
