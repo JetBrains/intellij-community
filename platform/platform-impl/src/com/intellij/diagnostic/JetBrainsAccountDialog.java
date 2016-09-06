1dd25fcc57c7aa4fbafaec2651dc90e3825fdad8
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2016-09-06 16:28:07 +0200

    convert ITNReporter, JetBrainsAccountDialog to kotlin

--- platform/platform-impl/src/com/intellij/diagnostic/JetBrainsAccountDialog.java
+++ platform/platform-impl/src/com/intellij/diagnostic/JetBrainsAccountDialog.java
@@ -13,99 +13,91 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.intellij.diagnostic;
+package com.intellij.diagnostic
 
-import com.intellij.credentialStore.CredentialAttributes;
-import com.intellij.credentialStore.Credentials;
-import com.intellij.ide.BrowserUtil;
-import com.intellij.ide.passwordSafe.PasswordSafe;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.DialogWrapper;
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.ui.ClickListener;
-import com.intellij.util.net.HttpConfigurable;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.credentialStore.CredentialAttributes
+import com.intellij.credentialStore.Credentials
+import com.intellij.ide.BrowserUtil
+import com.intellij.ide.passwordSafe.PasswordSafe
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.ui.ClickListener
+import com.intellij.util.net.HttpConfigurable
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.MouseEvent;
+import javax.swing.*
+import java.awt.*
+import java.awt.event.MouseEvent
 
-public class JetBrainsAccountDialog extends DialogWrapper {
-  private JTextField myItnLoginTextField;
-  private JPasswordField myPasswordText;
-  private JCheckBox myRememberCheckBox;
+class JetBrainsAccountDialog : DialogWrapper {
+  private val myItnLoginTextField: JTextField? = null
+  private val myPasswordText: JPasswordField? = null
+  private val myRememberCheckBox: JCheckBox? = null
 
-  public JetBrainsAccountDialog(Component parent) throws HeadlessException {
-    super(parent, false);
-    init();
+  @Throws(HeadlessException::class)
+  constructor(parent: Component) : super(parent, false) {
+    init()
   }
 
-  public JetBrainsAccountDialog(Project project) throws HeadlessException {
-    super(project, false);
-    init();
+  @Throws(HeadlessException::class)
+  constructor(project: Project) : super(project, false) {
+    init()
   }
 
-  protected JPanel myMainPanel;
-  protected JLabel mySendingSettingsLabel;
-  private JLabel myCreateAccountLabel;
+  var myMainPanel: JPanel? = null
+  var mySendingSettingsLabel: JLabel? = null
+  private val myCreateAccountLabel: JLabel? = null
 
-  @Override
-  protected String getDimensionServiceKey() {
-    return "#com.intellij.diagnostic.AbstractSendErrorDialog";
+  override fun getDimensionServiceKey(): String? {
+    return "#com.intellij.diagnostic.AbstractSendErrorDialog"
   }
 
-  @Override
-  public JComponent getPreferredFocusedComponent() {
-    return myItnLoginTextField;
+  override fun getPreferredFocusedComponent(): JComponent? {
+    return myItnLoginTextField
   }
 
-  @Override
-  protected void init() {
-    setTitle(ReportMessages.ERROR_REPORT);
-    getContentPane().add(myMainPanel);
+  override fun init() {
+    title = ReportMessages.ERROR_REPORT
+    contentPane.add(myMainPanel)
 
-    new ClickListener() {
-      @Override
-      public boolean onClick(@NotNull MouseEvent e, int clickCount) {
-        HttpConfigurable.editConfigurable(myMainPanel);
-        return true;
+    object : ClickListener() {
+      override fun onClick(e: MouseEvent, clickCount: Int): Boolean {
+        HttpConfigurable.editConfigurable(myMainPanel)
+        return true
       }
-    }.installOn(mySendingSettingsLabel);
+    }.installOn(mySendingSettingsLabel!!)
 
-    mySendingSettingsLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+    mySendingSettingsLabel!!.cursor = Cursor(Cursor.HAND_CURSOR)
 
-    Credentials credentials = ErrorReportConfigurable.getCredentials();
-    String userName = credentials == null ? null : credentials.getUserName();
-    myItnLoginTextField.setText(userName);
-    String password = credentials == null ? null : credentials.getPasswordAsString();
-    myPasswordText.setText(password);
+    val credentials = ErrorReportConfigurable.getCredentials()
+    val userName = credentials?.userName
+    myItnLoginTextField!!.text = userName
+    val password = credentials?.getPasswordAsString()
+    myPasswordText!!.text = password
     // if no user name - never stored and so, defaults to remember. if user name set, but no password, so, previously was stored without password
-    myRememberCheckBox.setSelected(StringUtil.isEmpty(userName) || !StringUtil.isEmpty(password));
+    myRememberCheckBox!!.isSelected = StringUtil.isEmpty(userName) || !StringUtil.isEmpty(password)
 
-    new ClickListener() {
-      @Override
-      public boolean onClick(@NotNull MouseEvent e, int clickCount) {
-        BrowserUtil.browse("http://account.jetbrains.com");
-        return true;
+    object : ClickListener() {
+      override fun onClick(e: MouseEvent, clickCount: Int): Boolean {
+        BrowserUtil.browse("http://account.jetbrains.com")
+        return true
       }
-    }.installOn(myCreateAccountLabel);
-    myCreateAccountLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+    }.installOn(myCreateAccountLabel!!)
+    myCreateAccountLabel.cursor = Cursor(Cursor.HAND_CURSOR)
 
-    super.init();
+    super.init()
   }
 
-  @Override
-  protected void doOKAction() {
-    String userName = myItnLoginTextField.getText();
+  override fun doOKAction() {
+    val userName = myItnLoginTextField!!.text
     if (!StringUtil.isEmpty(userName)) {
-      PasswordSafe.getInstance().set(new CredentialAttributes(ErrorReportConfigurable.SERVICE_NAME, userName),
-                                     new Credentials(userName, myRememberCheckBox.isSelected() ? myPasswordText.getPassword() : null));
+      PasswordSafe.getInstance().set(CredentialAttributes(ErrorReportConfigurable.SERVICE_NAME, userName),
+          Credentials(userName, if (myRememberCheckBox!!.isSelected) myPasswordText!!.password else null))
     }
-    super.doOKAction();
+    super.doOKAction()
   }
 
-  @Override
-  protected JComponent createCenterPanel() {
-    return myMainPanel;
+  override fun createCenterPanel(): JComponent? {
+    return myMainPanel
   }
 }
