747fc289533dfe6e06a51dd3c8d389efbebeec5b
Vitaliy Bibaev <vitaliy.bibaev@jetbrains.com>
2016-09-06 18:56:01 +0300

    Fix bug when "Show New Instances" action is enabled, but tracker is not ready

--- action/ShowNewInstancesAction.java
+++ action/ShowNewInstancesAction.java
@@ -1,10 +1,8 @@
 package org.jetbrains.debugger.memory.action;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.project.Project;
 import com.intellij.xdebugger.XDebugSession;
 import com.sun.jdi.ReferenceType;
-import org.jetbrains.debugger.memory.component.InstancesTracker;
 import org.jetbrains.debugger.memory.utils.InstancesProvider;
 import org.jetbrains.debugger.memory.view.ClassesTable;
 import org.jetbrains.debugger.memory.view.InstancesWindow;
@@ -12,10 +10,10 @@ import org.jetbrains.debugger.memory.view.InstancesWindow;
 public class ShowNewInstancesAction extends ClassesActionBase {
   @Override
   protected boolean isEnabled(AnActionEvent e) {
-    Project project = e.getProject();
+    XDebugSession session = getDebugSession(e);
     ReferenceType selectedClass = getSelectedClass(e);
-    return super.isEnabled(e) && selectedClass != null &&
-        project != null && InstancesTracker.getInstance(project).isTracked(selectedClass.name());
+    InstancesProvider provider = e.getData(ClassesTable.NEW_INSTANCES_PROVIDER_KEY);
+    return super.isEnabled(e) && session != null && selectedClass != null && provider != null;
   }
 
   @Override
