b498f2276e1bda6a8424e7ee7d446031348df022
Renaud Paquay <rpaquay@google.com>
2016-09-07 11:43:49 -0700

    Use less intrusive method to send focus event to screen readers
    Instead of sending a FocusEvent to the EDT queue to reach the
    AccessBridge and screen readers, we send the FocusEvent directly
    to the AccessBridge event handler.
    
    This is more direct and avoids side effects of having an extra event
    sent (and potentially intercepted) to the EDT queue.
    
    BUG=https://code.google.com/p/android/issues/detail?id=217960
    
    Change-Id: I5070167dac364957e20b11ecca4a0e93f8c6b2ae

--- platform/util/src/com/intellij/util/ui/accessibility/AccessBridgeUtil.java
+++ platform/util/src/com/intellij/util/ui/accessibility/AccessBridgeUtil.java
@@ -15,14 +15,27 @@
  */
 package com.intellij.util.ui.accessibility;
 
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.util.ReflectionUtil;
 import com.intellij.util.ui.EdtInvocationManager;
 import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class AccessBridgeUtil {
+  private static final Logger LOG = Logger.getInstance("#" + AccessBridgeUtil.class.getName());
+  private static final String ACCESS_BRIDGE_CLASS_NAME = "com.sun.java.accessibility.AccessBridge";
+  private static InstanceMethodEntry ourFocusGainedMethod;
+
 
   /**
    * Returns {@code true} if the current thread is the access bridge worker thread.
@@ -45,6 +58,101 @@ public class AccessBridgeUtil {
     assert isWorkerThread();
 
     return UIUtil.invokeAndWaitIfNeeded(computable);
+  }
+
+  /*
+   * Dispatch a "focus gained" event to screen readers via the access bridge (if it is
+   * enabled).
+   */
+  public static void sendFocusGainedEvent(@Nullable Component focusOwner) {
+    if (focusOwner == null) {
+      return;
+    }
+
+    // Note: It is ok to cache the method + instance obtained from
+    // the access bridge because the access bridge is unloaded only
+    // when the JVM shuts down.
+    if (ourFocusGainedMethod == null) {
+      ourFocusGainedMethod = createFocusGainedMethod();
+    }
+
+    try {
+      FocusEvent event = new FocusEvent(focusOwner, FocusEvent.FOCUS_GAINED, false, null);
+      ourFocusGainedMethod.invoke(event);
+    }
+    catch (Throwable e) {
+      LOG.warn("Unexpected error invoking focusGained on the AccessBridge class", e);
+    }
+  }
+
+  private static class InstanceMethodEntry {
+    public static final InstanceMethodEntry Empty = new InstanceMethodEntry(null, null);
+
+    private final Object myInstance;
+    private final Method myMethod;
+
+    public InstanceMethodEntry(@Nullable Object instance, @Nullable Method method) {
+      myInstance = instance;
+      myMethod = method;
+    }
+
+    public void invoke(@NotNull FocusEvent event) throws InvocationTargetException, IllegalAccessException {
+      if (myMethod == null) {
+        return;
+      }
+      myMethod.invoke(myInstance, event);
+    }
+  }
+
+  @NotNull
+  private static InstanceMethodEntry createFocusGainedMethod() {
+    Class accessBridgeClass = findAccessBridgeClass();
+    if (accessBridgeClass == null) {
+      return InstanceMethodEntry.Empty;
+    }
+
+    try {
+      // Look for the "AccessBridge.theAccessBridge.eventHandler.focusGained(event)" method.
+      Field theAccessBridgeField = ReflectionUtil.findField(accessBridgeClass, null, "theAccessBridge");
+      Object theAccessBridge = theAccessBridgeField.get(null);
+      if (theAccessBridge == null) {
+        return InstanceMethodEntry.Empty;
+      }
+      Field eventHandlerField = ReflectionUtil.findField(accessBridgeClass, null, "eventHandler");
+      Object eventHandler = eventHandlerField.get(theAccessBridge);
+      Method method = ReflectionUtil.getMethod(eventHandler.getClass(), "focusGained", FocusEvent.class);
+      return new InstanceMethodEntry(eventHandler, method);
+    }
+    catch (Throwable e) {
+      LOG.warn("Unexpected error looking for the AccessBridge class", e);
+      return InstanceMethodEntry.Empty;
+    }
+  }
+
+  /**
+   * Find the {@link Class} object corresponding to the {@link com.sun.java.accessibility.AccessBridge} class,
+   * or {@code null} if the class his not currently loaded.
+   */
+  @Nullable
+  private static Class findAccessBridgeClass() {
+    // We go through the parent chain of ClassLoaders until we find the right loader, because
+    // the AccessBridge is loaded via the "Java Extensions" class loader by default.
+    //
+    // Also, we need to invoke (via reflection) the "findLoadedClass" method on each ClassLoader instance
+    // to avoid accidentally loading the AccessBridge class if it is not enabled.
+    Method findLoadedClass = ReflectionUtil.getDeclaredMethod(ClassLoader.class, "findLoadedClass", String.class);
+    for (ClassLoader cl = ClassLoader.getSystemClassLoader(); cl != null; cl = cl.getParent()) {
+      try {
+        Class cls = (Class)findLoadedClass.invoke(cl, ACCESS_BRIDGE_CLASS_NAME);
+        if (cls != null) {
+          return cls;
+        }
+      }
+      catch (Throwable e) {
+        LOG.warn("Unexpected error looking for the AccessBridge class", e);
+      }
+    }
+    return null;
   }
 
   private static class Status {
@@ -71,7 +179,7 @@ public class AccessBridgeUtil {
 
       StackTraceElement[] stack = Thread.currentThread().getStackTrace();
       for (StackTraceElement e : stack) {
-        if (StringUtil.equals(e.getClassName(), "com.sun.java.accessibility.AccessBridge") &&
+        if (StringUtil.equals(e.getClassName(), ACCESS_BRIDGE_CLASS_NAME) &&
             StringUtil.equals(e.getMethodName(), "runDLL")) {
           return true;
         }
