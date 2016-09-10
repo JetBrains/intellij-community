c6e73314004dab27360bd23a000690b03e465094
peter <peter@jetbrains.com>
2016-09-10 08:56:10 +0200

    tolerate non-directory source roots when determining java file language level (IDEA-160928)

--- java/java-impl/src/com/intellij/psi/impl/JavaPsiImplementationHelperImpl.java
+++ java/java-impl/src/com/intellij/psi/impl/JavaPsiImplementationHelperImpl.java
@@ -159,7 +159,7 @@ public class JavaPsiImplementationHelperImpl extends JavaPsiImplementationHelper
     final ProjectFileIndex index = ProjectRootManager.getInstance(myProject).getFileIndex();
     final VirtualFile sourceRoot = index.getSourceRootForFile(virtualFile);
     final VirtualFile folder = virtualFile.getParent();
-    if (sourceRoot != null && folder != null) {
+    if (sourceRoot != null && sourceRoot.isDirectory() && folder != null) {
       String relativePath = VfsUtilCore.getRelativePath(folder, sourceRoot, '/');
       if (relativePath == null) {
         LOG.error("Null relative path: folder=" + folder + "; root=" + sourceRoot);
