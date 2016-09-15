7df86e7749616aa4d91e77333870aa82dd302778
Konstantin Bulenkov <kb@jetbrains.com>
2016-09-15 18:33:09 +0200

    IDEA-161043 Rename "Version Control" toolwindow to "VCS"

--- platform/vcs-impl/src/com/intellij/openapi/vcs/history/FileHistorySessionPartner.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/history/FileHistorySessionPartner.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -133,7 +133,7 @@ public class FileHistorySessionPartner implements VcsAppendableHistorySessionPar
   @NotNull
   private static ToolWindow getToolWindow(@NotNull Project project) {
     ToolWindow toolWindow = ToolWindowManager.getInstance(project).getToolWindow(ToolWindowId.VCS);
-    assert toolWindow != null : "Version Control ToolWindow should be available at this point.";
+    assert toolWindow != null : "VCS ToolWindow should be available at this point.";
     return toolWindow;
   }
 
