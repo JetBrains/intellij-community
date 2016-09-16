40e1b7f91dfec82ee2e0ad103a7b4b7f15cf495c
Pavel Dolgov <pavel.dolgov@jetbrains.com>
2016-09-16 17:23:14 +0300

    Java inspection: Preserve comments from replaced and removed statements in EqualsReplaceableByObjectsCallInspection (IDEA-161076)

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/returnSeparatedFromComputation/afterLabeledFor4.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/returnSeparatedFromComputation/afterLabeledFor4.java
@@ -3,15 +3,15 @@ class T {
     int f(int[][] a) {
         int n = -1;
         myLabel:
-          for (int i = 0; i < a.length; i++) {
-              if (a[i].length == 0) {
-                  return -i - 1;
-              }
-              for(int j = 0; j < a[i].length; j++) {
-                  n = j;
-                  if (a[i][j] == 0) return n;
-              }
-          }
-          return n;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i].length == 0) {
+                return -i - 1;
+            }
+            for(int j = 0; j < a[i].length; j++) {
+                n = j;
+                if (a[i][j] == 0) return n;
+            }
+        }
+        return n;
     }
 }
