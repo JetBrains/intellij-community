9068a812080bfad4f68720d27a1ae198ced756ce
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2016-09-20 16:35:14 +0300

    adjust presentation of inline parameter hints

--- java/java-impl/src/com/intellij/codeInsight/daemon/impl/ParameterHintsPresentationManager.java
+++ java/java-impl/src/com/intellij/codeInsight/daemon/impl/ParameterHintsPresentationManager.java
@@ -28,7 +28,6 @@ import com.intellij.openapi.editor.markup.TextAttributes;
 import com.intellij.openapi.ui.GraphicsConfig;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.Key;
-import com.intellij.ui.ColorUtil;
 import com.intellij.util.Alarm;
 import com.intellij.util.ui.GraphicsUtil;
 import org.jetbrains.annotations.NotNull;
@@ -172,11 +171,8 @@ public class ParameterHintsPresentationManager implements Disposable {
           Color backgroundColor = attributes.getBackgroundColor();
           if (backgroundColor != null) {
             GraphicsConfig config = GraphicsUtil.setupAAPainting(g);
-            int shadeRectHeight = Math.min(4, r.height - 3);
-            g.setColor(ColorUtil.darker(backgroundColor, 1));
-            g.fillRoundRect(r.x + 2, r.y + r.height - shadeRectHeight - 1, r.width - 4, shadeRectHeight, 4, 4);
             g.setColor(backgroundColor);
-            g.fillRoundRect(r.x + 2, r.y + 1, r.width - 4, r.height - 3, 4, 4);
+            g.fillRoundRect(r.x + 2, r.y + 2, r.width - 4, r.height - 4, 8, 8);
             config.restore();
           }
           Color foregroundColor = attributes.getForegroundColor();
@@ -185,7 +181,7 @@ public class ParameterHintsPresentationManager implements Disposable {
             g.setFont(getFont(editor));
             FontMetrics metrics = g.getFontMetrics();
             Shape savedClip = g.getClip();
-            g.clipRect(r.x + 3, r.y + 1, r.width - 6, r.height - 3);
+            g.clipRect(r.x + 3, r.y + 2, r.width - 6, r.height - 4);
             int editorAscent = editor instanceof EditorImpl ? ((EditorImpl)editor).getAscent() : 0;
             g.drawString(myText, r.x + 7, r.y + Math.max(editorAscent, (r.height + metrics.getAscent() - metrics.getDescent()) / 2) - 1);
             g.setClip(savedClip);
