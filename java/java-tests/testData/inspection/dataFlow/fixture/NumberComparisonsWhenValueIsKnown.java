e3e321d056cbca2b23b761bf2d39721ec5d1e345
peter <peter@jetbrains.com>
2016-09-20 16:09:10 +0200

    issue dfa warnings when comparing non-final variables with constant values (IDEA-157871)

--- java/java-tests/testData/inspection/dataFlow/fixture/NumberComparisonsWhenValueIsKnown.java
+++ java/java-tests/testData/inspection/dataFlow/fixture/NumberComparisonsWhenValueIsKnown.java
@@ -18,4 +18,30 @@ class Test {
     String str1 = <warning descr="Condition 'time <= 0' is always 'false'">time <= 0</warning> ? "" : "";
     String str2 = <warning descr="Condition 'time >= 100' is always 'false'">time >= 100</warning> ? "" : "";
   }
+
+  void forLoop() {
+    int target = 3;
+    for (int current = 0; <warning descr="Condition 'current < target' is always 'true'">current < target</warning>; ) {
+      current = 0;
+    }
+  }
+
+  void doLoop() {
+    int current;
+    int target = 3;
+    do
+    {
+      current = 0;
+    } while (<warning descr="Condition 'current < target' is always 'true'">current < target</warning>);
+  }
+
+  void whileLoop() {
+    int current = 0;
+    int target = 3;
+    while (<warning descr="Condition 'current < target' is always 'true'">current < target</warning>)
+    {
+      current = 0;
+    }
+  }
+
 }
