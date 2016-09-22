44c1a1bdff18d5ac5a0890d29e0894d45991eebb
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2016-09-22 22:19:50 +0200

    revert previous attempts to fix builds

--- build/groovy/org/jetbrains/intellij/build/IdeaCommunityBuilder.groovy
+++ build/groovy/org/jetbrains/intellij/build/IdeaCommunityBuilder.groovy
@@ -70,7 +70,7 @@ class IdeaCommunityBuilder {
       }
       buildContext.notifyArtifactBuilt(intellijCoreZip)
     }
-    if (buildJps && false) {
+    if (buildJps) {
       buildContext.messages.block("Build standalone JPS") {
         String jpsArtifactDir = "$buildContext.paths.artifacts/jps"
         layouts.layoutJps(buildContext.paths.communityHome, jpsArtifactDir, buildContext.fullBuildNumber, {})
