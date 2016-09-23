d85f19a6e601cd66f4f8e9fe3ac9659d15e7819b
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2016-09-23 10:53:22 +0200

    convert QuickListsManager to kotlin

--- platform/platform-impl/src/com/intellij/openapi/actionSystem/ex/QuickListsManager.java
+++ platform/platform-impl/src/com/intellij/openapi/actionSystem/ex/QuickListsManager.java
@@ -13,160 +13,120 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.intellij.openapi.actionSystem.ex;
+package com.intellij.openapi.actionSystem.ex
 
-import com.intellij.ide.IdeBundle;
-import com.intellij.ide.actions.QuickSwitchSchemeAction;
-import com.intellij.openapi.actionSystem.*;
-import com.intellij.openapi.actionSystem.impl.BundledQuickListsProvider;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.components.ExportableApplicationComponent;
-import com.intellij.openapi.options.NonLazySchemeProcessor;
-import com.intellij.openapi.options.SchemeManager;
-import com.intellij.openapi.options.SchemeManagerFactory;
-import com.intellij.openapi.project.Project;
-import gnu.trove.THashSet;
-import org.jdom.Element;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.configurationStore.LazySchemeProcessor
+import com.intellij.configurationStore.SchemeDataHolder
+import com.intellij.ide.IdeBundle
+import com.intellij.ide.actions.QuickSwitchSchemeAction
+import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.actionSystem.ActionPlaces
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.actionSystem.DefaultActionGroup
+import com.intellij.openapi.actionSystem.impl.BundledQuickListsProvider
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.components.ExportableApplicationComponent
+import com.intellij.openapi.options.SchemeManager
+import com.intellij.openapi.options.SchemeManagerFactory
+import com.intellij.openapi.project.Project
+import gnu.trove.THashSet
+import java.util.function.Function
 
-import java.io.File;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
+class QuickListsManager(private val myActionManager: ActionManager, schemeManagerFactory: SchemeManagerFactory) : ExportableApplicationComponent {
+  private val mySchemeManager: SchemeManager<QuickList>
 
-public class QuickListsManager implements ExportableApplicationComponent {
-  private static final String LIST_TAG = "list";
-
-  private final ActionManager myActionManager;
-  private final SchemeManager<QuickList> mySchemeManager;
-
-  public QuickListsManager(@NotNull ActionManager actionManager, @NotNull SchemeManagerFactory schemeManagerFactory) {
-    myActionManager = actionManager;
-    mySchemeManager = schemeManagerFactory.create("quicklists", new NonLazySchemeProcessor<QuickList, QuickList>() {
-      @NotNull
-      @Override
-      public QuickList readScheme(@NotNull Element element, boolean duringLoad) {
-        return createItem(element);
-      }
-
-      @NotNull
-      @Override
-      public Element writeScheme(@NotNull QuickList scheme) {
-        Element element = new Element(LIST_TAG);
-        scheme.writeExternal(element);
-        return element;
-      }
-    });
+  init {
+    mySchemeManager = schemeManagerFactory.create("quicklists",
+        object : LazySchemeProcessor<QuickList, QuickList>() {
+          override fun createScheme(dataHolder: SchemeDataHolder<QuickList>,
+                                    name: String,
+                                    attributeProvider: Function<String, String?>) = createItem(dataHolder)
+        })
   }
 
-  @NotNull
-  public static QuickListsManager getInstance() {
-    return ApplicationManager.getApplication().getComponent(QuickListsManager.class);
+  companion object {
+    @JvmStatic
+    val instance: QuickListsManager
+      get() = ApplicationManager.getApplication().getComponent(QuickListsManager::class.java)
+
+    private fun createItem(dataHolder: SchemeDataHolder<QuickList>): QuickList {
+      val item = QuickList()
+      item.readExternal(dataHolder.read())
+      return item
+    }
   }
 
-  @Override
-  @NotNull
-  public File[] getExportFiles() {
-    return new File[]{mySchemeManager.getRootDirectory()};
-  }
+  override fun getExportFiles() = arrayOf(mySchemeManager.rootDirectory)
 
-  @NotNull
-  @Override
-  public String getPresentableName() {
-    return IdeBundle.message("quick.lists.presentable.name");
-  }
+  override fun getPresentableName() = IdeBundle.message("quick.lists.presentable.name")!!
 
-  @NotNull
-  private static QuickList createItem(@NotNull Element element) {
-    QuickList item = new QuickList();
-    item.readExternal(element);
-    return item;
-  }
+  override fun getComponentName() = "QuickListsManager"
 
-  @Override
-  @NotNull
-  public String getComponentName() {
-    return "QuickListsManager";
-  }
-
-  @Override
-  public void initComponent() {
-    for (BundledQuickListsProvider provider : BundledQuickListsProvider.EP_NAME.getExtensions()) {
-      for (final String path : provider.getBundledListsRelativePaths()) {
-        mySchemeManager.loadBundledScheme(path, provider, QuickListsManager::createItem);
+  override fun initComponent() {
+    for (provider in BundledQuickListsProvider.EP_NAME.extensions) {
+      for (path in provider.bundledListsRelativePaths) {
+        mySchemeManager.loadBundledScheme(path, provider, { createItem(it) })
       }
     }
-    mySchemeManager.loadSchemes();
-    registerActions();
+    mySchemeManager.loadSchemes()
+    registerActions()
   }
 
-  @Override
-  public void disposeComponent() {
+  override fun disposeComponent() {
   }
 
-  @NotNull
-  public SchemeManager<QuickList> getSchemeManager() {
-    return mySchemeManager;
-  }
+  val schemeManager: SchemeManager<QuickList>
+    get() = mySchemeManager
 
-  @NotNull
-  public QuickList[] getAllQuickLists() {
-    Collection<QuickList> lists = mySchemeManager.getAllSchemes();
-    return lists.toArray(new QuickList[lists.size()]);
-  }
+  val allQuickLists: Array<QuickList>
+    get() {
+      return mySchemeManager.allSchemes.toTypedArray()
+    }
 
-  private void registerActions() {
+  private fun registerActions() {
     // to prevent exception if 2 or more targets have the same name
-    Set<String> registeredIds = new THashSet<>();
-    for (QuickList list : mySchemeManager.getAllSchemes()) {
-      String actionId = list.getActionId();
+    val registeredIds = THashSet<String>()
+    for (scheme in mySchemeManager.allSchemes) {
+      val actionId = scheme.actionId
       if (registeredIds.add(actionId)) {
-        myActionManager.registerAction(actionId, new InvokeQuickListAction(list));
+        myActionManager.registerAction(actionId, InvokeQuickListAction(scheme))
       }
     }
   }
 
-  private void unregisterActions() {
-    for (String oldId : myActionManager.getActionIds(QuickList.QUICK_LIST_PREFIX)) {
-      myActionManager.unregisterAction(oldId);
+  private fun unregisterActions() {
+    for (oldId in myActionManager.getActionIds(QuickList.QUICK_LIST_PREFIX)) {
+      myActionManager.unregisterAction(oldId)
     }
   }
 
-  public void setQuickLists(@NotNull List<QuickList> quickLists) {
-    unregisterActions();
-    mySchemeManager.setSchemes(quickLists);
-    registerActions();
+  // used by external plugin
+  fun setQuickLists(quickLists: List<QuickList>) {
+    unregisterActions()
+    mySchemeManager.setSchemes(quickLists)
+    registerActions()
+  }
+}
+
+private class InvokeQuickListAction(private val myQuickList: QuickList) : QuickSwitchSchemeAction() {
+  init {
+    myActionPlace = ActionPlaces.ACTION_PLACE_QUICK_LIST_POPUP_ACTION
+    templatePresentation.description = myQuickList.description
+    templatePresentation.setText(myQuickList.name, false)
   }
 
-  private static class InvokeQuickListAction extends QuickSwitchSchemeAction {
-    private final QuickList myQuickList;
-
-    public InvokeQuickListAction(@NotNull QuickList quickList) {
-      myQuickList = quickList;
-      myActionPlace = ActionPlaces.ACTION_PLACE_QUICK_LIST_POPUP_ACTION;
-      getTemplatePresentation().setDescription(myQuickList.getDescription());
-      getTemplatePresentation().setText(myQuickList.getName(), false);
-    }
-
-    @Override
-    protected void fillActions(Project project, @NotNull DefaultActionGroup group, @NotNull DataContext dataContext) {
-      ActionManager actionManager = ActionManager.getInstance();
-      for (String actionId : myQuickList.getActionIds()) {
-        if (QuickList.SEPARATOR_ID.equals(actionId)) {
-          group.addSeparator();
-        }
-        else {
-          AnAction action = actionManager.getAction(actionId);
-          if (action != null) {
-            group.add(action);
-          }
+  override fun fillActions(project: Project, group: DefaultActionGroup, dataContext: DataContext) {
+    val actionManager = ActionManager.getInstance()
+    for (actionId in myQuickList.actionIds) {
+      if (QuickList.SEPARATOR_ID == actionId) {
+        group.addSeparator()
+      }
+      else {
+        val action = actionManager.getAction(actionId)
+        if (action != null) {
+          group.add(action)
         }
       }
-    }
-
-    @Override
-    protected boolean isEnabled() {
-      return true;
     }
   }
 }
