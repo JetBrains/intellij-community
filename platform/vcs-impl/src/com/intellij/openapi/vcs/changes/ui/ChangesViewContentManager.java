JetBrains/intellij-community

commit 8f6589d311c8138eaff9e4589fc422de62a3d603

IDEA-161662 VersionControl toolwindow should not be available for project with no VCS enabled

platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/ChangesViewContentManager.java
@@ -24,8 +24,8 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.Key;
-import com.intellij.openapi.vcs.AbstractVcs;
 import com.intellij.openapi.vcs.ProjectLevelVcsManager;
+import com.intellij.openapi.vcs.VcsDirectoryMapping;
 import com.intellij.openapi.vcs.VcsListener;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowId;
@@ -65,6 +65,7 @@ public class ChangesViewContentManager extends AbstractProjectComponent implemen
     super(project);
     myVcsManager = vcsManager;
     myVcsChangeAlarm = new Alarm(Alarm.ThreadToUse.SWING_THREAD, project);
+    myProject.getMessageBus().connect().subscribe(ProjectLevelVcsManager.VCS_CONFIGURATION_CHANGED, myVcsListener);
   }
 
   public void setUp(ToolWindow toolWindow) {
@@ -73,13 +74,9 @@ public class ChangesViewContentManager extends AbstractProjectComponent implemen
     myContentManagerListener = new MyContentManagerListener();
     contentManager.addContentManagerListener(myContentManagerListener);
 
-    myVcsManager.addVcsListener(myVcsListener);
-
     Disposer.register(myProject, new Disposable(){
       public void dispose() {
         contentManager.removeContentManagerListener(myContentManagerListener);
-
-        myVcsManager.removeVcsListener(myVcsListener);
       }
     });
 
@@ -146,11 +143,15 @@ public class ChangesViewContentManager extends AbstractProjectComponent implemen
   }
 
   private void updateToolWindowAvailability() {
-    final AbstractVcs[] abstractVcses = myVcsManager.getAllActiveVcss();
     ToolWindow toolWindow = ToolWindowManager.getInstance(myProject).getToolWindow(TOOLWINDOW_ID);
-    toolWindow.setAvailable(abstractVcses.length > 0, null);
+    toolWindow.setAvailable(isAvailable(), null);
   }
-  
+
+  public boolean isAvailable() {
+    final List<VcsDirectoryMapping> mappings = myVcsManager.getDirectoryMappings();
+    return !mappings.isEmpty() && !mappings.get(0).isDefaultMapping() && mappings.get(0).getVcs() != null;
+  }
+
   public void projectClosed() {
     myVcsChangeAlarm.cancelAllRequests();
   }
@@ -216,7 +217,9 @@ public class ChangesViewContentManager extends AbstractProjectComponent implemen
         public void run() {
           if (myProject.isDisposed()) return;
           updateToolWindowAvailability();
-          updateExtensionTabs();
+          if (myContentManager != null) {
+            updateExtensionTabs();
+          }
         }
       }, 100, ModalityState.NON_MODAL);
     }

platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/ChangesViewContentI.java
@@ -24,6 +24,7 @@ import org.jetbrains.annotations.Nullable;
  */
 public interface ChangesViewContentI {
   void setUp(ToolWindow toolWindow);
+  boolean isAvailable();
   void addContent(Content content);
   void removeContent(final Content content);
   void setSelectedContent(final Content content);

platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/ChangesViewToolWindowFactory.java
@@ -17,6 +17,7 @@ package com.intellij.openapi.vcs.changes.ui;
 
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Condition;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import org.jetbrains.annotations.NotNull;
@@ -24,9 +25,14 @@ import org.jetbrains.annotations.NotNull;
 /**
  * @author Dmitry Avdeev
  */
-public class ChangesViewToolWindowFactory implements ToolWindowFactory, DumbAware {
+public class ChangesViewToolWindowFactory implements ToolWindowFactory, DumbAware, Condition<Project> {
   @Override
   public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
     ChangesViewContentManager.getInstance(project).setUp(toolWindow);
+  }
+
+  @Override
+  public boolean value(Project project) {
+    return ChangesViewContentManager.getInstance(project).isAvailable();
   }
 }

platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/DummyChangesViewContentManager.java
@@ -28,6 +28,11 @@ public class DummyChangesViewContentManager implements ChangesViewContentI {
   }
 
   @Override
+  public boolean isAvailable() {
+    return false;
+  }
+
+  @Override
   public void addContent(final Content content) {
   }
 

commit 9a56d351ac0ce0f0dde5a250ccd81fb546bff5ac

Make VCS toolwindow available for projects with <Project> mapping
The issue was introduced in 8f6589d: default mapping is a valid
mapping and shouldn't be checked here.

This fixes IDEA-161752.

Previously the availability check used ProjectLevelVcsManager.getAllActiveVcss(),
but this check became less powerful in 00adb8e: it stopped initializing
active VCSs if they weren't initialized yet.
This functionality might return back, but it will be done separately.

platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/ChangesViewContentManager.java
@@ -24,6 +24,7 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.Key;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vcs.ProjectLevelVcsManager;
 import com.intellij.openapi.vcs.VcsDirectoryMapping;
 import com.intellij.openapi.vcs.VcsListener;
@@ -151,7 +152,7 @@ public class ChangesViewContentManager extends AbstractProjectComponent implemen
 
   public boolean isAvailable() {
     final List<VcsDirectoryMapping> mappings = myVcsManager.getDirectoryMappings();
-    return !mappings.isEmpty() && !mappings.get(0).isDefaultMapping() && mappings.get(0).getVcs() != null;
+    return !mappings.isEmpty() && !StringUtil.isEmpty(mappings.get(0).getVcs());
   }
 
   public void projectClosed() {