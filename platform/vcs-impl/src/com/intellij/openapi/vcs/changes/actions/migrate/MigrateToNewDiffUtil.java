970bfa3a2832d518f66f6d321f4505477c88630b
Aleksey Pivovarov <AMPivovarov@gmail.com>
2016-09-26 19:23:18 +0300

    diff: remove unused code

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/migrate/MigrateToNewDiffUtil.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/migrate/MigrateToNewDiffUtil.java
@@ -1,6 +1,5 @@
 package com.intellij.openapi.vcs.changes.actions.migrate;
 
-import com.intellij.diff.actions.impl.GoToChangePopupBuilder;
 import com.intellij.diff.chains.DiffRequestChain;
 import com.intellij.diff.chains.DiffRequestProducer;
 import com.intellij.diff.chains.DiffRequestProducerException;
@@ -14,7 +13,6 @@ import com.intellij.diff.requests.ErrorDiffRequest;
 import com.intellij.diff.requests.SimpleDiffRequest;
 import com.intellij.diff.util.DiffUserDataKeysEx;
 import com.intellij.diff.util.LineCol;
-import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.diff.DiffNavigationContext;
 import com.intellij.openapi.diff.DiffTool;
@@ -22,23 +20,9 @@ import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.util.UserDataHolder;
-import com.intellij.openapi.util.UserDataHolderBase;
-import com.intellij.openapi.vcs.VcsDataKeys;
-import com.intellij.openapi.vcs.VcsException;
-import com.intellij.openapi.vcs.changes.Change;
-import com.intellij.openapi.vcs.changes.ChangeRequestChain;
-import com.intellij.openapi.vcs.changes.actions.ChangeDiffRequestPresentable;
-import com.intellij.openapi.vcs.changes.actions.DiffRequestPresentable;
-import com.intellij.openapi.vcs.changes.actions.DiffRequestPresentableProxy;
-import com.intellij.openapi.vcs.changes.actions.diff.ChangeGoToChangePopupAction;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.pom.Navigatable;
-import com.intellij.util.Consumer;
-import com.intellij.util.Function;
-import com.intellij.util.containers.ContainerUtil;
-import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -56,14 +40,8 @@ public class MigrateToNewDiffUtil {
 
   @NotNull
   public static DiffRequestChain convertRequestChain(@NotNull com.intellij.openapi.diff.DiffRequest oldRequest) {
-    ChangeRequestChain oldChain = (ChangeRequestChain)oldRequest.getGenericData().get(VcsDataKeys.DIFF_REQUEST_CHAIN.getName());
-    if (oldChain == null || oldChain.getAllRequests().size() < 2) {
-      DiffRequest request = convertRequest(oldRequest);
-      return new SimpleDiffRequestChain(request);
-    }
-    else {
-      return new ChangeRequestChainWrapper(oldChain);
-    }
+    DiffRequest request = convertRequest(oldRequest);
+    return new SimpleDiffRequestChain(request);
   }
 
   @NotNull
@@ -125,133 +103,6 @@ public class MigrateToNewDiffUtil {
           oldContent.onAssigned(isAssigned);
         }
       };
-    }
-  }
-
-  private static class ChangeRequestChainWrapper extends UserDataHolderBase implements DiffRequestChain, GoToChangePopupBuilder.Chain {
-    @NotNull private final ChangeRequestChain myChain;
-    @NotNull private final List<MyProducerWrapper> myRequests;
-
-    private int myIndex;
-
-    public ChangeRequestChainWrapper(@NotNull ChangeRequestChain chain) {
-      myChain = chain;
-      myRequests = ContainerUtil.map(myChain.getAllRequests(),
-                                     new Function<DiffRequestPresentable, MyProducerWrapper>() {
-                                       @Override
-                                       public MyProducerWrapper fun(DiffRequestPresentable presentable) {
-                                         return new MyProducerWrapper(myChain, presentable);
-                                       }
-                                     });
-
-      myIndex = chain.getAllRequests().indexOf(chain.getCurrentRequest());
-    }
-
-    @NotNull
-    @Override
-    public List<? extends MyProducerWrapper> getRequests() {
-      return myRequests;
-    }
-
-    @Override
-    public int getIndex() {
-      return myIndex;
-    }
-
-    @Override
-    public void setIndex(int index) {
-      assert index >= 0 && index < myRequests.size();
-      myIndex = index;
-    }
-
-    @Nullable
-    private static Change getChange(@NotNull DiffRequestPresentable presentable) {
-      if (presentable instanceof DiffRequestPresentableProxy) {
-        try {
-          presentable = ((DiffRequestPresentableProxy)presentable).init();
-        }
-        catch (VcsException e) {
-          LOG.info(e);
-          return null;
-        }
-      }
-      if (presentable instanceof ChangeDiffRequestPresentable) {
-        return ((ChangeDiffRequestPresentable)presentable).getChange();
-      }
-      return null;
-    }
-
-    @NotNull
-    @Override
-    public AnAction createGoToChangeAction(@NotNull Consumer<Integer> onSelected) {
-      return new ChangeGoToChangePopupAction<ChangeRequestChainWrapper>(this, onSelected) {
-        @NotNull
-        @Override
-        protected List<Change> getChanges() {
-          return ContainerUtil.mapNotNull(myChain.getRequests(), new Function<MyProducerWrapper, Change>() {
-            @Override
-            @Nullable
-            public Change fun(MyProducerWrapper wrapper) {
-              return getChange(wrapper.getPresentable());
-            }
-          });
-        }
-
-        @Nullable
-        @Override
-        protected Change getCurrentSelection() {
-          return getChange(myChain.getRequests().get(myIndex).getPresentable());
-        }
-
-        @Override
-        protected int findSelectedStep(@Nullable Change change) {
-          if (change == null) return -1;
-          for (int i = 0; i < myRequests.size(); i++) {
-            Change c = getChange(myRequests.get(i).getPresentable());
-            if (c != null && change.equals(c)) {
-              return i;
-            }
-          }
-          return -1;
-        }
-      };
-    }
-  }
-
-  private static class MyProducerWrapper implements DiffRequestProducer {
-    @NotNull private final DiffRequestPresentable myPresentable;
-    @NotNull private final ChangeRequestChain myChain;
-
-    public MyProducerWrapper(@NotNull ChangeRequestChain chain,
-                             @NotNull DiffRequestPresentable presentable) {
-      myPresentable = presentable;
-      myChain = chain;
-    }
-
-    @NotNull
-    @Override
-    public String getName() {
-      return myPresentable.getPathPresentation();
-    }
-
-    @NotNull
-    public DiffRequestPresentable getPresentable() {
-      return myPresentable;
-    }
-
-    @NotNull
-    @Override
-    public DiffRequest process(@NotNull UserDataHolder context, @NotNull ProgressIndicator indicator)
-      throws DiffRequestProducerException, ProcessCanceledException {
-      com.intellij.openapi.diff.DiffRequest oldRequest =
-        UIUtil.invokeAndWaitIfNeeded(new Computable<com.intellij.openapi.diff.DiffRequest>() {
-          @Override
-          public com.intellij.openapi.diff.DiffRequest compute() {
-            return myChain.moveTo(myPresentable);
-          }
-        });
-      if (oldRequest == null) return new ErrorDiffRequest(this, "Can't build old-style request");
-      return convertRequest(oldRequest);
     }
   }
 
