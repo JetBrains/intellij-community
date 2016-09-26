c420ec722c8092f2eebf5a375256c0f205408e83
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2016-09-26 17:56:15 +0700

    DummyEntryPointsTool: suppress InspectionDescriptionNotFoundInspection

--- java/java-analysis-impl/src/com/intellij/codeInspection/deadCode/DummyEntryPointsTool.java
+++ java/java-analysis-impl/src/com/intellij/codeInspection/deadCode/DummyEntryPointsTool.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -59,6 +59,7 @@ public class DummyEntryPointsTool extends UnusedDeclarationInspectionBase {
   @Override
   @NotNull
   public String getShortName() {
+    //noinspection InspectionDescriptionNotFoundInspection
     return "";
   }
 }
