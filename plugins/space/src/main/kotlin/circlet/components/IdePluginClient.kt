c79601ceaff8a71d5b56cd05ee5c2b5276641903
Sergey Shkredov <sergey.shkredov@jetbrains.com>
2016-10-03 18:14:45 +0300

    store refresh token in IDEA passwords
    GitOrigin-RevId: ea2c2a9c3efd774f01c0abafbd2891be4e11bcea

--- plugins/space/src/main/kotlin/circlet/components/IdePluginClient.kt
+++ plugins/space/src/main/kotlin/circlet/components/IdePluginClient.kt
@@ -2,27 +2,33 @@ package circlet.components
 
 import circlet.client.*
 import circlet.client.auth.*
+import circlet.ring.*
 import circlet.utils.*
+import com.intellij.ide.passwordSafe.*
 import com.intellij.notification.*
 import com.intellij.openapi.components.*
 import com.intellij.openapi.progress.*
 import com.intellij.openapi.project.*
 import com.intellij.util.net.ssl.*
+import json.*
 import klogging.*
 import lifetime.*
 import nl.komponents.kovenant.*
 import org.apache.http.client.utils.*
+import org.jetbrains.hub.oauth2.client.*
 import rx.subjects.*
+import rx.subjects.Promise
 import java.util.*
+import java.util.Base64
 
 private val log = KLoggers.logger("plugin/IdePluginClient.kt")
 
 class IdePluginClient(project : Project) : AbstractProjectComponent(project) {
     val DISPLAY_ID = "circlet.components.IdePluginClient"
-
-    enum class ConnectionStates { Connecting, Connected }
-
+    val PASS_ID = "circlet.components.IdePluginClient.Pass"
     private val serverConfig = ServerConfig()
+    private val passwords = PasswordSafe.getInstance()
+
     var connectionState : IdePluginConnectionState? = null
 
     fun connect() {
@@ -52,19 +58,8 @@ class IdePluginClient(project : Project) : AbstractProjectComponent(project) {
 
             RingAuth(CertificateManager.getInstance().sslContext, CertificateManager.HOSTNAME_VERIFIER, serverConfig, connectedStateLifetimeDef.lifetime).apply {
                 connectionState?.message = "Connecting to hub"
-                hubInfo() thenAwait {
-                    log.info("got hub info $it")
-                    connectionState?.message = "Getting auth code"
-                    authCode(it)
-                } thenAwait {
-                    log.info("got auth code")
-                    connectionState?.message = "Getting refresh token"
-                    refreshToken(it)
-                } thenAwait {
-                    log.info("got refresh token")
-                    connectionState?.message = "Getting access token"
-                    authToken(it)
-                } then {
+
+                refreshTokenStored() then {
                     val me = user(it)
 
                     connectionState?.message = "Connecting to circlet server"
@@ -90,7 +85,7 @@ class IdePluginClient(project : Project) : AbstractProjectComponent(project) {
                     state.session = session
 
                 } then {
-                    // succcessful connection.
+                    // successful connection.
                     val notification = Notification(DISPLAY_ID, "Circlet", "Connected to server", NotificationType.INFORMATION)
                     Notifications.Bus.notify(notification, myProject)
 
@@ -102,7 +97,7 @@ class IdePluginClient(project : Project) : AbstractProjectComponent(project) {
                             Notifications.Bus.notify(notification, myProject)
                         }
                         is ExpectedException -> {
-                            val notification = Notification(DISPLAY_ID, "Circlet connection failed", th.message ?: "", NotificationType.ERROR)
+                            val notification = Notification(DISPLAY_ID, "Circlet", "Connection failed. ${th.message ?: ""}", NotificationType.ERROR)
                             Notifications.Bus.notify(notification, myProject)
                         }
                         else -> {
@@ -112,22 +107,58 @@ class IdePluginClient(project : Project) : AbstractProjectComponent(project) {
                 }
             }
         }
+    }
 
+    private data class TokenDataStored(val token : String, val expires : Long, val serverUri : String, val clientId : String, val scope : List<String>) {}
+
+    private fun RingAuth.refreshTokenStored(): Promise<AccessTokenInfo> {
+        val refreshCode = passwords.getPassword(myProject, javaClass, PASS_ID) ?: ""
+        if (!refreshCode.isNullOrEmpty())
+        {
+            val stored = parseJson<TokenDataStored>(refreshCode)
+            return Promise<AccessTokenInfo> { resolved, rejected ->
+                resolved(AccessTokenInfo(object : AccessTokenSource() {
+                    override fun loadToken() =
+                        AccessToken(stored.token, Calendar.getInstance().apply { setTimeInMillis(1) }, stored.scope)
+                }, HubInfo(stored.serverUri, stored.clientId)))
+            }
+        } else {
+            return hubInfo() thenAwait {
+                log.info("got hub info $it")
+                connectionState?.message = "Getting auth code"
+                authCode(it)
+            } thenAwait {
+                log.info("got auth code")
+                connectionState?.message = "Getting refresh token"
+                refreshToken(it)
+            } thenAwait {
+                log.info("got refresh token")
+                connectionState?.message = "Getting access token"
+                authToken(it)
+            } then {
+                // last: store to cache...
+                val accessToken = it.accessTokenSource.accessToken
+                val toStore = printJson(TokenDataStored(accessToken.accessToken, accessToken.expiresAt.timeInMillis, it.hubInfo.serverUri, it.hubInfo.clientId, accessToken.scope))
+                passwords.storePassword(myProject, javaClass, PASS_ID, toStore)
+                it
+            }
+        }
     }
 
     fun disconnect(){
-        // may disconned in both connected and connecting states.
+        // may disconnect in both connected and connecting states.
 
-        val state = connectionState
-        if (state == null)
-            return
+        val state = connectionState ?: return
 
         connectionState = null
 
-        // succcessful connection.
+        // successful connection.
         val notification = Notification(DISPLAY_ID, "Circlet", "Disconnected from server", NotificationType.INFORMATION)
         Notifications.Bus.notify(notification, myProject)
 
         state.Close()
     }
+
+    enum class ConnectionStates { Connecting, Connected }
+
 }
