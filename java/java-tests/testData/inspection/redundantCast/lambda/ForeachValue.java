1cf1300ab1142224ecc1b6f6e51e6f003bc1382f
Anna.Kozlova <anna.kozlova@jetbrains.com>
2016-10-04 17:40:26 +0200

    redundant cast: convert tests to light

--- java/java-tests/testData/inspection/redundantCast/lambda/ForeachValue.java
+++ java/java-tests/testData/inspection/redundantCast/lambda/ForeachValue.java
@@ -3,7 +3,7 @@ import java.util.Iterator;
 
 class IterableMain {
   public static void main(final String... args) {
-    for (final String s :  (Iterable<String>) XIterator::new) {}
+    for (final String s :  (Iterable<String>) <error descr="'XIterator' is abstract; cannot be instantiated">XIterator::new</error>) {}
   }
 
   public static interface XIterator extends Iterator<String> {}
