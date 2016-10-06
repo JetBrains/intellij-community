34daff132b55de876ec22c897e783ea2ae4151b3
Alexander Koshevoy <alexander.koshevoy@jetbrains.com>
2016-10-06 16:15:12 +0300

    PY-17447 PyRemoteDebugSessionCreator extension point with PyDockerDebugSessionCreator implementation that uses new Python debugger connection strategy introduced
    (cherry picked from commit dec3641)

---
+++ python/src/com/jetbrains/python/debugger/PyRemoteDebugSessionCreator.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2000-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jetbrains.python.debugger;
+
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.openapi.extensions.ExtensionPointName;
+import com.intellij.remote.RemoteSdkAdditionalData;
+import com.intellij.xdebugger.XDebugSession;
+import com.jetbrains.python.run.PythonCommandLineState;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * @author Alexander Koshevoy
+ */
+public abstract class PyRemoteDebugSessionCreator {
+  public static final ExtensionPointName<PyRemoteDebugSessionCreator> EP_NAME
+    = ExtensionPointName.create("Pythonid.pyRemoteDebugSessionCreator");
+
+  protected abstract boolean appliesTo(@NotNull RemoteSdkAdditionalData remoteSdkAdditionalData);
+
+  @NotNull
+  public abstract XDebugSession createSession(@NotNull PyDebugRunner runner,
+                                              @NotNull PythonCommandLineState state,
+                                              @NotNull ExecutionEnvironment environment)
+    throws ExecutionException;
+
+  @Contract("null -> null")
+  @Nullable
+  public static PyRemoteDebugSessionCreator findExtension(@Nullable RemoteSdkAdditionalData remoteSdkAdditionalData) {
+    if (remoteSdkAdditionalData == null) {
+      return null;
+    }
+    for (PyRemoteDebugSessionCreator sessionCreator : EP_NAME.getExtensions()) {
+      if (sessionCreator.appliesTo(remoteSdkAdditionalData)) {
+        return sessionCreator;
+      }
+    }
+    return null;
+  }
+}
