ae1e09f0630b30e3012805d8f91673812828cdd0
Anna.Kozlova <anna.kozlova@jetbrains.com>
2016-10-07 16:22:13 +0200

    junit refactoring: extract common repeat so available for junit5/3 as well as 4; make test discovery work with junit 5

--- plugins/junit_rt/src/com/intellij/rt/execution/junit/IdeaTestRunner.java
+++ plugins/junit_rt/src/com/intellij/rt/execution/junit/IdeaTestRunner.java
@@ -24,12 +24,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 public interface IdeaTestRunner {
+  void createListeners(ArrayList listeners);
 
-  int startRunnerWithArgs(String[] args, ArrayList listeners, String name, int count, boolean sendTree);
+  /**
+   * @return -2 internal failure
+   *         -1 there were failed tests
+   *          0 all tests were successful
+   */
+  int startRunnerWithArgs(String[] args, String name, int count, boolean sendTree);
 
   Object getTestToStart(String[] args, String name);
   List getChildTests(Object description);
   String getStartDescription(Object child);
 
   String getTestClassName(Object child);
+
+  class Repeater {
+    public static int startRunnerWithArgs(IdeaTestRunner testRunner,
+                                          String[] args,
+                                          ArrayList listeners,
+                                          String name,
+                                          int count,
+                                          boolean sendTree) {
+      testRunner.createListeners(listeners);
+      if (count == 1) {
+        return testRunner.startRunnerWithArgs(args, name, count, sendTree);
+      }
+      else {
+        if (count > 0) {
+          boolean success = true;
+          int i = 0;
+          while (i++ < count) {
+            final int result = testRunner.startRunnerWithArgs(args, name, count, sendTree);
+            if (result == -2) {
+              return result;
+            }
+            success &= result == 0;
+            sendTree = false;
+          }
+
+          return success ? 0 : -1;
+        }
+        else {
+          boolean success = true;
+          while (true) {
+            int result = testRunner.startRunnerWithArgs(args, name, count, sendTree);
+            if (result == -2) {
+              return -1;
+            }
+            success &= result == 0;
+            if (count == -2 && !success) {
+              return -1;
+            }
+          }
+        }
+      }
+    }
+  }
 }
