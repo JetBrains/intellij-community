cf47b17b497bc8bf56f815c46dd8ce0180784761
Pavel Dolgov <pavel.dolgov@jetbrains.com>
2016-10-07 17:35:07 +0300

    Javafx: Migration to JDK 9 (IDEA-157536)

---
+++ plugins/javaFX/testData/migration/ImportClasses.java
@@ -0,0 +1,7 @@
+import com.sun.javafx.css.CompoundSelector;
+import com.sun.javafx.scene.control.skin.AccordionSkin;
+
+class ImportClasses {
+  CompoundSelector compoundSelector;
+  AccordionSkin accordionSkin;
+}
