a2de22657a04df6e9c2475f71ff8d5b2880426e9
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2016-10-07 11:36:59 +0300

    merging android studio 2.2

--- plugins/gradle/src/org/jetbrains/plugins/gradle/service/project/DistributionFactoryExt.java
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/service/project/DistributionFactoryExt.java
@@ -152,7 +152,7 @@ public class DistributionFactoryExt extends DistributionFactory {
       return String.format("Gradle distribution '%s'", wrapperConfiguration.getDistribution());
     }
 
-    public ClassPath getToolingImplementationClasspath(final ProgressLoggerFactory progressLoggerFactory, final File userHomeDir, BuildCancellationToken cancellationToken) {
+    public ClassPath getToolingImplementationClasspath(org.gradle.internal.logging.progress.ProgressLoggerFactory progressLoggerFactory, final File userHomeDir, BuildCancellationToken cancellationToken) {
       if (installedDistribution == null) {
         Callable<File> installDistroTask = () -> {
           File installDir;
