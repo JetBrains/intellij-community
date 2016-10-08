22f3831f5b5fc0fd342d9ef876953ba69e4756b3
Yaroslav Lepenkin <yaroslav.lepenkin@jetbrains.com>
2016-10-08 18:00:56 +0300

    [Parameter Hints Manager] use blacklist

--- java/java-impl/src/com/intellij/codeInsight/daemon/impl/ParameterHintsPassFactory.java
+++ java/java-impl/src/com/intellij/codeInsight/daemon/impl/ParameterHintsPassFactory.java
@@ -19,6 +19,8 @@ import com.intellij.codeHighlighting.EditorBoundHighlightingPass;
 import com.intellij.codeHighlighting.TextEditorHighlightingPass;
 import com.intellij.codeHighlighting.TextEditorHighlightingPassFactory;
 import com.intellij.codeHighlighting.TextEditorHighlightingPassRegistrar;
+import com.intellij.codeInsight.hints.filtering.Matcher;
+import com.intellij.codeInsight.hints.filtering.MatcherConstructor;
 import com.intellij.openapi.components.AbstractProjectComponent;
 import com.intellij.openapi.editor.Caret;
 import com.intellij.openapi.editor.Editor;
@@ -34,10 +36,8 @@ import gnu.trove.TIntObjectHashMap;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ParameterHintsPassFactory extends AbstractProjectComponent implements TextEditorHighlightingPassFactory {
   private static final Key<Boolean> REPEATED_PASS = Key.create("RepeatedParameterHintsPass");
@@ -66,24 +66,28 @@ public class ParameterHintsPassFactory extends AbstractProjectComponent implemen
       myAnnotations.clear();
       if (!isEnabled() || !(myFile instanceof PsiJavaFile)) return;
       PsiJavaFile file = (PsiJavaFile) myFile;
-      
-      SyntaxTraverser.psiTraverser(file).forEach(element -> process(element));
+
+      List<Matcher> matchers = ParameterNameHintsSettings
+        .getInstance()
+        .getBlacklist()
+        .stream()
+        .map((item) -> MatcherConstructor.INSTANCE.createMatcher(item))
+        .collect(Collectors.toList());
+
+      SyntaxTraverser.psiTraverser(file).forEach(element -> process(element, matchers));
     }
 
     private static boolean isEnabled() {
       return EditorSettingsExternalizable.getInstance().isShowParameterNameHints();
     }
 
-    private void process(PsiElement child) {
+    private void process(PsiElement child, List<Matcher> matchers) {
       if (child instanceof PsiCallExpression) {
-        inlineLiteralArgumentsNames((PsiCallExpression)child);
-      }
-    }
-    
-    private void inlineLiteralArgumentsNames(@NotNull PsiCallExpression expression) {
-      ParameterNameHintsManager manager = new ParameterNameHintsManager(expression);
-      for (InlayInfo info : manager.getDescriptors()) {
-        myAnnotations.put(info.getOffset(), info.getText());
+        PsiCallExpression callExpression = (PsiCallExpression)child;
+        ParameterNameHintsManager manager = new ParameterNameHintsManager(callExpression, matchers);
+        for (InlayInfo info : manager.getDescriptors()) {
+          myAnnotations.put(info.getOffset(), info.getText());
+        }
       }
     }
 
