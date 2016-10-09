dbdb249ebe23d642ce17164df5b5b943c397a3e2
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2016-10-09 20:42:51 +0300

    Disallow tracking for arrays

--- action/tracking/TrackInstanceCreationAction.java
+++ action/tracking/TrackInstanceCreationAction.java
@@ -1,12 +1,41 @@
 package org.jetbrains.debugger.memory.action.tracking;
 
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.Project;
+import com.sun.jdi.ArrayType;
+import com.sun.jdi.ReferenceType;
+import org.jetbrains.debugger.memory.action.ClassesActionBase;
+import org.jetbrains.debugger.memory.component.InstancesTracker;
 import org.jetbrains.debugger.memory.tracking.TrackingType;
 
-public class TrackInstanceCreationAction extends InstancesTrackingActionBase {
-  @NotNull
+public class TrackInstanceCreationAction extends ClassesActionBase {
   @Override
-  protected TrackingType getType() {
-    return TrackingType.CREATION;
+  protected boolean isEnabled(AnActionEvent e) {
+    Project project = e.getProject();
+    if (!super.isEnabled(e) || project == null) {
+      return false;
+    }
+
+    InstancesTracker tracker = InstancesTracker.getInstance(project);
+    ReferenceType selectedClass = getSelectedClass(e);
+    if (selectedClass != null) {
+      TrackingType currentType = tracker.getTrackingType(selectedClass.name());
+      if (TrackingType.CREATION == currentType) {
+        return false;
+      }
+    }
+
+    return selectedClass != null && !(selectedClass instanceof ArrayType);
+  }
+
+  @Override
+  protected void perform(AnActionEvent e) {
+    Project project = e.getProject();
+    ReferenceType selectedClass = getSelectedClass(e);
+
+    if (selectedClass != null && project != null) {
+      InstancesTracker tracker = InstancesTracker.getInstance(project);
+      tracker.add(selectedClass, TrackingType.CREATION);
+    }
   }
 }
