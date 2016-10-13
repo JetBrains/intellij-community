be696b63c89603c23fb88f4fb8b972d7b2a7b0df
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2016-10-13 13:37:42 +0300

    Fix property which customizes diff memory allocation
    idea.max.vcs.loaded.size.kb is used to restrict memory allocations
    when calculating a diff, to avoid possible OOM when comparing large
    files.
    
    However, previously it was not really possible to relax the restriction,
    since diff took the minimum of idea.max.vcs.loaded.size.kb and
    idea.max.intellisense.filesize (which is used as a default).
    
    The fix is to use idea.max.vcs.loaded.size.kb is it is set, without
    looking into idea.max.intellisense.filesize.
    
    This fixes IDEA-114799

--- platform/vcs-api/src/com/intellij/vcsUtil/VcsUtil.java
+++ platform/vcs-api/src/com/intellij/vcsUtil/VcsUtil.java
@@ -64,9 +64,9 @@ public class VcsUtil {
 
   private static int computeLoadedFileSize() {
     int result = (int)PersistentFSConstants.FILE_LENGTH_TO_CACHE_THRESHOLD;
-    final String userLimitKb = System.getProperty(MAX_VCS_LOADED_SIZE_KB);
+    String userLimitKb = System.getProperty(MAX_VCS_LOADED_SIZE_KB);
     try {
-      return userLimitKb != null ? Math.min(Integer.parseInt(userLimitKb) * 1024, result) : result;
+      return userLimitKb != null ? Integer.parseInt(userLimitKb) * 1024 : result;
     }
     catch (NumberFormatException ignored) {
       return result;
