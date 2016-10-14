00a7f4f373666c36d91c80adc0f49ec8c9940698
Egor.Ushakov <egor.ushakov@jetbrains.com>
2016-10-14 18:29:00 +0300

    decompiler: a test for the pull request #394
    (cherry picked from commit b0b141b)

--- plugins/java-decompiler/engine/test/org/jetbrains/java/decompiler/SingleClassesTest.java
+++ plugins/java-decompiler/engine/test/org/jetbrains/java/decompiler/SingleClassesTest.java
@@ -89,6 +89,7 @@ public class SingleClassesTest {
   @Test public void testExtendingSubclass() { doTest("pkg/TestExtendingSubclass"); }
   @Test public void testSyntheticAccess() { doTest("pkg/TestSyntheticAccess"); }
   @Test public void testIllegalVarName() { doTest("pkg/TestIllegalVarName"); }
+  @Test public void testAsserts() { doTest("pkg/TestAsserts"); }
 
   private void doTest(String testFile, String... companionFiles) {
     ConsoleDecompiler decompiler = fixture.getDecompiler();
