92a5c62b3000764522a1aec383aba2602038dc9b
liana.bakradze <liana.bakradze@jetbrains.com>
2016-10-17 21:05:51 +0300

    project view refactoring in educational plugins
    (cherry picked from commit 1d80760)

--- python/educational-core/student/src/com/jetbrains/edu/learning/projectView/StudyTreeStructureProvider.java
+++ python/educational-core/student/src/com/jetbrains/edu/learning/projectView/StudyTreeStructureProvider.java
@@ -2,15 +2,13 @@ package com.jetbrains.edu.learning.projectView;
 
 import com.intellij.ide.projectView.TreeStructureProvider;
 import com.intellij.ide.projectView.ViewSettings;
-import com.intellij.ide.projectView.impl.nodes.PsiFileNode;
+import com.intellij.ide.projectView.impl.nodes.ProjectViewProjectNode;
+import com.intellij.ide.projectView.impl.nodes.PsiDirectoryNode;
 import com.intellij.ide.util.treeView.AbstractTreeNode;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDirectory;
-import com.jetbrains.edu.learning.core.EduNames;
 import com.jetbrains.edu.learning.courseFormat.Course;
-import com.jetbrains.edu.learning.courseFormat.TaskFile;
 import com.jetbrains.edu.learning.StudyTaskManager;
 import com.jetbrains.edu.learning.StudyUtils;
 import org.jetbrains.annotations.NotNull;
@@ -25,78 +23,37 @@ public class StudyTreeStructureProvider implements TreeStructureProvider, DumbAw
   public Collection<AbstractTreeNode> modify(@NotNull AbstractTreeNode parent,
                                              @NotNull Collection<AbstractTreeNode> children,
                                              ViewSettings settings) {
-    if (!needModify(parent)) {
+    Project project = parent.getProject();
+    if (project == null || !shouldModify(project)) {
       return children;
     }
-    Collection<AbstractTreeNode> nodes = new ArrayList<>();
+    Course course = StudyTaskManager.getInstance(project).getCourse();
+    if (course == null) {
+      return children;
+    }
+    Collection<AbstractTreeNode> modifiedNodes = new ArrayList<>();
     for (AbstractTreeNode node : children) {
-      final Project project = node.getProject();
-      if (project != null) {
-        if (node.getValue() instanceof PsiDirectory) {
-          final PsiDirectory nodeValue = (PsiDirectory)node.getValue();
-          final String name = nodeValue.getName();
-          if (!name.contains(EduNames.USER_TESTS) && !name.startsWith(".") && !"lib".equals(name)) {
-            AbstractTreeNode newNode = createStudyDirectoryNode(settings, project, nodeValue);
-            nodes.add(newNode);
-          }
-        }
-        else {
-          if (parent instanceof StudyDirectoryNode && node instanceof PsiFileNode) {
-            final PsiFileNode psiFileNode = (PsiFileNode)node;
-            final VirtualFile virtualFile = psiFileNode.getVirtualFile();
-            if (virtualFile == null) {
-              return nodes;
-            }
-            final TaskFile taskFile = StudyUtils.getTaskFile(project, virtualFile);
-            if (taskFile != null) {
-              nodes.add(node);
-              continue;
-            }
-            final String parentName = parent.getName();
-            if (parentName != null) {
-              if (parentName.equals(EduNames.SANDBOX_DIR)) {
-                nodes.add(node);
-              }
-              if (parentName.startsWith(EduNames.TASK)) {
-                addNonInvisibleFiles(nodes, node, project, virtualFile);
-              }
-            }
-          }
+      if (parent instanceof ProjectViewProjectNode && node instanceof PsiDirectoryNode) {
+        modifiedNodes.add(createCourseNode(project, node, settings, course));
+        continue;
+      }
+      if (parent instanceof StudyDirectoryNode) {
+        AbstractTreeNode modifiedNode = ((StudyDirectoryNode)parent).modifyChildNode(node);
+        if (modifiedNode != null) {
+          modifiedNodes.add(modifiedNode);
         }
       }
     }
-    return nodes;
+    return modifiedNodes;
   }
 
   @NotNull
-  protected AbstractTreeNode createStudyDirectoryNode(ViewSettings settings, Project project, PsiDirectory nodeValue) {
-    return new StudyDirectoryNode(project, nodeValue, settings);
+  protected CourseDirectoryNode createCourseNode(Project project, AbstractTreeNode node, ViewSettings settings, Course course) {
+    return new CourseDirectoryNode(project, ((PsiDirectory)node.getValue()), settings, course);
   }
 
-  private static void addNonInvisibleFiles(@NotNull final Collection<AbstractTreeNode> nodes,
-                                           @NotNull final AbstractTreeNode node,
-                                           @NotNull final Project project,
-                                           @NotNull final VirtualFile virtualFile) {
-    if (!StudyTaskManager.getInstance(project).isInvisibleFile(virtualFile.getPath())) {
-      String fileName = virtualFile.getName();
-      if (!fileName.contains(EduNames.WINDOW_POSTFIX) && !fileName.contains(EduNames.WINDOWS_POSTFIX)
-          && !StudyUtils.isTestsFile(project, fileName) && !StudyUtils.isTaskDescriptionFile(fileName)) {
-        nodes.add(node);
-      }
-    }
-  }
-
-  protected boolean needModify(@NotNull final AbstractTreeNode parent) {
-    final Project project = parent.getProject();
-    if (project == null) {
-      return false;
-    }
-    final StudyTaskManager studyTaskManager = StudyTaskManager.getInstance(project);
-    Course course = studyTaskManager.getCourse();
-    if (course == null) {
-      return false;
-    }
-    return EduNames.STUDY.equals(course.getCourseMode());
+  protected boolean shouldModify(@NotNull final Project project) {
+    return StudyUtils.isStudentProject(project);
   }
 
   @Nullable
