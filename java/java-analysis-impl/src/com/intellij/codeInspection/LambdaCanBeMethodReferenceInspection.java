3dd085b6cc761ea9f806b58776e228d00684a7ef
Tagir Valeev <tagir.valeev@jetbrains.com>
2016-10-18 11:53:15 +0700

    IDEA-162574 False positive 'Lambda can be replaced with method reference'
    (cherry picked from commit ee1bd69; IDEA-CR-14640)

--- java/java-analysis-impl/src/com/intellij/codeInspection/LambdaCanBeMethodReferenceInspection.java
+++ java/java-analysis-impl/src/com/intellij/codeInspection/LambdaCanBeMethodReferenceInspection.java
@@ -297,8 +297,9 @@ public class LambdaCanBeMethodReferenceInspection extends BaseJavaBatchLocalInsp
           return null;
         }
         PsiType type = typeElement.getType();
-        if (type instanceof PsiPrimitiveType)
-          return null;
+        if (type instanceof PsiPrimitiveType) return null;
+        type = type.getDeepComponentType();
+        if (type instanceof PsiClassType && (((PsiClassType)type).resolve() instanceof PsiTypeParameter)) return null;
         return expression;
       }
     }
