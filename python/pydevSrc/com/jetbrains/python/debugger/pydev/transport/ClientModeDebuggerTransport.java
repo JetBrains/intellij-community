364db9f86dbabb0ac69ed0d9d1d52e650f2695da
Alexander Koshevoy <Alexander.Koshevoy@jetbrains.com>
2016-10-18 16:33:14 +0300

    PY-17447 ClientModeDebuggerTransport logic explanation added

--- python/pydevSrc/com/jetbrains/python/debugger/pydev/transport/ClientModeDebuggerTransport.java
+++ python/pydevSrc/com/jetbrains/python/debugger/pydev/transport/ClientModeDebuggerTransport.java
@@ -4,6 +4,8 @@ import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.vfs.CharsetToolkit;
 import com.jetbrains.python.debugger.IPyDebugProcess;
 import com.jetbrains.python.debugger.PyDebuggerException;
+import com.jetbrains.python.debugger.pydev.AbstractCommand;
+import com.jetbrains.python.debugger.pydev.ClientModeMultiProcessDebugger;
 import com.jetbrains.python.debugger.pydev.ProtocolFrame;
 import com.jetbrains.python.debugger.pydev.RemoteDebugger;
 import org.jetbrains.annotations.NotNull;
@@ -22,7 +24,29 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
+ * {@link DebuggerTransport} implementation that expects a debugging script to behave as a server. The main process of the debugging script
+ * and all of the Python processes forked and created within it receives incoming connections on the <b>same port</b> using
+ * {@code SO_REUSEPORT} server socket option on Linux (available since 3.9 core version), Mac OS, BSD platforms and {@code SO_REUSEADDR}
+ * server socket option on Windows platforms (see {@code start_server(port)} method in <i>pydevd_comm.py</i>).
+ * <p>
+ * Each Python process within the debugging script requires <b>single connection</b> from the IDE. When a new Python process is created the
+ * originator process sends {@link AbstractCommand#PROCESS_CREATED} message to the IDE. The new process binds server socket to the same
+ * address and port as the originator process and starts listening for an incoming connection. The IDE tries to establish a new
+ * connection with the script.
+ * <p>
+ * At the last point the following problem could arise. When several processes are created almost simultaneously and they become
+ * bound to the single port the IDE could establish the connection to some of the processes twice or more times. The first connection is
+ * accepted by the Python process but the others are not. Other connections would stay in <i>completed connection queue</i> until a timeout
+ * for a response for the {@link AbstractCommand#RUN} arouse. To solve this problem {@link ClientModeDebuggerTransport} has several
+ * states. The transport is created with {@link State#INIT}. After the connection is established the transport object is transferred to
+ * {@link State#CONNECTED}. After the first message is received from the debugging script the transport is transferred to
+ * {@link State#APPROVED}. After the transport is transferred to {@link State#CONNECTED} a task is scheduled in
+ * {@link ClientModeDebuggerTransport#CHECK_CONNECTION_APPROVED_DELAY} to check if the state has been changed to {@link State#APPROVED}. If
+ * the state had been changed then the current connection is kept and the normal communication with debugging script is performed. If the
+ * state has not been changed for this period of time the transport tries to reconnect and schedules the check again.
+ *
  * @author Alexander Koshevoy
+ * @see ClientModeMultiProcessDebugger
  */
 public class ClientModeDebuggerTransport extends BaseDebuggerTransport {
   private static final Logger LOG = Logger.getInstance(ClientModeDebuggerTransport.class);
@@ -220,11 +244,13 @@ public class ClientModeDebuggerTransport extends BaseDebuggerTransport {
      */
     INIT,
     /**
-     * Connection to the debugger script established
+     * Socket connection to the debugger host:port address established and no messages has been received from the debugging script yet.
+     * The connection might be ephemeral at this point (see {@link ClientModeDebuggerTransport}).
      */
     CONNECTED,
     /**
-     * Established connection to the debugger script has been approved
+     * Socket connection to the debugger host:port address established and at least one message has been received from the debugging script.
+     * This state means that a script is on the other end had accepted the connection.
      */
     APPROVED,
     /**
