0ab5789d337b2b73f17e9ed0292868f63121569f
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2016-10-18 17:16:32 +0300

    Remove unused method
    (cherry picked from commit a984c5512f1a87a8f51708c72a4c2cb0002551a3)

--- plugins/git4idea/src/git4idea/actions/GitRepositoryAction.java
+++ plugins/git4idea/src/git4idea/actions/GitRepositoryAction.java
@@ -15,7 +15,6 @@
  */
 package git4idea.actions;
 
-import com.intellij.dvcs.repo.Repository;
 import com.intellij.openapi.actionSystem.ActionPlaces;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
@@ -31,7 +30,6 @@ import git4idea.GitVcs;
 import git4idea.branch.GitBranchUtil;
 import git4idea.i18n.GitBundle;
 import git4idea.repo.GitRepository;
-import git4idea.repo.GitRepositoryManager;
 import org.jetbrains.annotations.CalledInAwt;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -94,26 +92,6 @@ public abstract class GitRepositoryAction extends DumbAwareAction {
   @Deprecated
   protected boolean executeFinalTasksSynchronously() {
     return true;
-  }
-
-  @CalledInAwt
-  protected static boolean isRebasing(AnActionEvent e) {
-    final Project project = e.getData(CommonDataKeys.PROJECT);
-    if (project != null) {
-      final VirtualFile[] files = e.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY);
-      if (files != null) {
-        for (VirtualFile file : files) {
-          GitRepositoryManager manager = GitUtil.getRepositoryManager(project);
-          if (isRebasing(manager.getRepositoryForFile(file))) return true;
-        }
-      }
-      if (isRebasing(GitBranchUtil.getCurrentRepository(project))) return true;
-    }
-    return false;
-  }
-
-  private static boolean isRebasing(@Nullable GitRepository repository) {
-    return repository != null && repository.getState() == Repository.State.REBASING;
   }
 
   /**
