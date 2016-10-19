74975bfc34ff13a6e4df9f2d7965cc41b9fd695c
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2016-10-19 16:04:04 +0300

    project tasks: build methods javadoc added

--- java/compiler/openapi/src/com/intellij/task/ProjectTaskManager.java
+++ java/compiler/openapi/src/com/intellij/task/ProjectTaskManager.java
@@ -47,20 +47,47 @@ public abstract class ProjectTaskManager {
                            @NotNull ProjectTask projectTask,
                            @Nullable ProjectTaskNotification callback);
 
+  /**
+   * Build all modules with modified files and all modules with files that depend on them all over the project.
+   *
+   * @param callback a notification callback, or null if no notifications needed
+   */
   public abstract void buildAllModules(@Nullable ProjectTaskNotification callback);
 
+  /**
+   * Build all modules with modified files and all modules with files that depend on them all over the project.
+   */
   public void buildAllModules() {
     buildAllModules(null);
   }
 
+  /**
+   * Rebuild the whole project modules from scratch.
+   *
+   * @param callback a notification callback, or null if no notifications needed
+   */
   public abstract void rebuildAllModules(@Nullable ProjectTaskNotification callback);
 
+  /**
+   * Rebuild the whole project modules from scratch.
+   */
   public void rebuildAllModules() {
     rebuildAllModules(null);
   }
 
+  /**
+   * Build modules and all modules these modules depend on recursively.
+   *
+   * @param modules  modules to build
+   * @param callback a notification callback, or null if no notifications needed
+   */
   public abstract void build(@NotNull Module[] modules, @Nullable ProjectTaskNotification callback);
 
+  /**
+   * Build modules and all modules these modules depend on recursively.
+   *
+   * @param modules modules to build
+   */
   public void build(@NotNull Module... modules) {
     build(modules, null);
   }
@@ -71,8 +98,19 @@ public abstract class ProjectTaskManager {
     rebuild(modules, null);
   }
 
+  /**
+   * Compile a set of files.
+   *
+   * @param files    a list of files to compile. If a VirtualFile is a directory, all containing files should be processed.
+   * @param callback a notification callback, or null if no notifications needed.
+   */
   public abstract void compile(@NotNull VirtualFile[] files, @Nullable ProjectTaskNotification callback);
 
+  /**
+   * Compile a set of files.
+   *
+   * @param files a list of files to compile. If a VirtualFile is a directory, all containing files should be processed.
+   */
   public void compile(@NotNull VirtualFile... files) {
     compile(files, null);
   }
