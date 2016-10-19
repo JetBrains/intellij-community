18c3011b32200f17716f8ade7fa5c6536b4eab67
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2016-10-19 12:32:01 +0300

    stepic -> stepik
    Stupid stepik doesn't want to make our life easier
    EDU-766

--- python/educational-core/student/src/com/jetbrains/edu/learning/stepic/EduStepicNames.java
+++ python/educational-core/student/src/com/jetbrains/edu/learning/stepic/EduStepicNames.java
@@ -1,7 +1,7 @@
 package com.jetbrains.edu.learning.stepic;
 
 public class EduStepicNames {
-  public static final String STEPIC_URL = "https://stepic.org";
+  public static final String STEPIC_URL = "https://stepik.org";
   public static final String STEPIC_API_URL = STEPIC_URL + "/api";
   public static final String RECOMMENDATIONS_URL = "/recommendations";
   public static final String CONTENT_TYPE_APPL_JSON = "application/json";
