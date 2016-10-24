b9d8992475ee2b02520766be86d39e71800686e1
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2016-10-24 20:01:22 +0300

    Ipnb Fix according to a new api after merge
    (cherry picked from commit ae5ba7d)

--- python/ipnb/src/org/jetbrains/plugins/ipnb/editor/panels/IpnbFilePanel.java
+++ python/ipnb/src/org/jetbrains/plugins/ipnb/editor/panels/IpnbFilePanel.java
@@ -261,7 +261,7 @@ public class IpnbFilePanel extends JPanel implements Scrollable, DataProvider, D
     if (currentCellPanel == null) return;
 
     if (below) {
-      selectNext(currentCellPanel);
+      selectNext(currentCellPanel, false);
     }
     else {
       selectPrev(currentCellPanel);
@@ -279,7 +279,7 @@ public class IpnbFilePanel extends JPanel implements Scrollable, DataProvider, D
 
     currentCellPanel.repaint();
     deleteCell(currentCellPanel);
-    saveToFile();
+    saveToFile(false);
   }
 
   private static ArrayList<String> mergeCellsSource(@NotNull List<String> currentCellSource,
@@ -333,7 +333,7 @@ public class IpnbFilePanel extends JPanel implements Scrollable, DataProvider, D
     panel = createPanel(cell, newCellSource);
     addCell(panel, true);
 
-    saveToFile();
+    saveToFile(false);
   }
 
   @NotNull
@@ -382,7 +382,7 @@ public class IpnbFilePanel extends JPanel implements Scrollable, DataProvider, D
 
     int indexToSelect = index < myIpnbPanels.size() ? index : index-1;
     final IpnbEditablePanel panel = myIpnbPanels.get(indexToSelect);
-    setSelectedCell(panel);
+    setSelectedCell(panel, false);
   }
 
   public void saveToFile(boolean synchronize) {
