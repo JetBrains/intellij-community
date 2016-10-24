2015561947f08b5f4b59e14d31cec2e6a55d1890
Anna.Kozlova <anna.kozlova@jetbrains.com>
2016-10-24 11:36:22 +0200

    setup multiselection for configuration context based on locations (IDEA-163025)

--- platform/lang-api/src/com/intellij/execution/actions/ConfigurationContext.java
+++ platform/lang-api/src/com/intellij/execution/actions/ConfigurationContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -92,6 +92,8 @@ public class ConfigurationContext {
     final Location<PsiElement> location = (Location<PsiElement>)Location.DATA_KEY.getData(dataContext);
     if (location != null) {
       myLocation = location;
+      Location<?>[] locations = Location.DATA_KEYS.getData(dataContext);
+      myMultipleSelection = locations != null && locations.length > 1;
       return;
     }
     final Project project = CommonDataKeys.PROJECT.getData(dataContext);
