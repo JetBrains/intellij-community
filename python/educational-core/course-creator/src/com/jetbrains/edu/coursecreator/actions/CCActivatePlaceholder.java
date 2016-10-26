2c46b4102bb46381ab55efb787218fd176d44f62
Liana.Bakradze <liana.bakradze@jetbrains.com>
2016-10-26 14:58:40 +0300

    activate/deactivate placeholders

---
+++ python/educational-core/course-creator/src/com/jetbrains/edu/coursecreator/actions/CCActivatePlaceholder.java
@@ -0,0 +1,48 @@
+package com.jetbrains.edu.coursecreator.actions;
+
+import com.intellij.openapi.util.TextRange;
+import com.jetbrains.edu.learning.courseFormat.AnswerPlaceholder;
+import com.jetbrains.edu.learning.courseFormat.AnswerPlaceholderSubtaskInfo;
+import com.jetbrains.edu.learning.courseFormat.TaskFile;
+
+public class CCActivatePlaceholder extends CCSubtaskPlaceholderAction {
+
+  public static final String TITLE = "Activate Answer Placeholder";
+
+  protected CCActivatePlaceholder() {
+    super(TITLE, TITLE);
+  }
+
+  @Override
+  protected AnswerPlaceholderSubtaskInfo getInfo(CCState state,
+                                                 int subtaskIndex,
+                                                 AnswerPlaceholder existingPlaceholder) {
+    int visibleLength = existingPlaceholder.getVisibleLength(subtaskIndex);
+    int placeholderOffset = existingPlaceholder.getOffset();
+    String possibleAnswer = state.getEditor().getDocument().getText(TextRange.create(placeholderOffset, placeholderOffset + visibleLength));
+    AnswerPlaceholderSubtaskInfo info = new AnswerPlaceholderSubtaskInfo();
+    info.setPossibleAnswer(possibleAnswer);
+    return info;
+  }
+
+  @Override
+  protected String getTitle() {
+    return TITLE;
+  }
+
+  @Override
+  protected void redoAction(AnswerPlaceholder existingPlaceholder, int subtaskIndex, AnswerPlaceholderSubtaskInfo info) {
+    existingPlaceholder.getSubtaskInfos().put(subtaskIndex, info);
+  }
+
+  @Override
+  protected void undoAction(AnswerPlaceholder existingPlaceholder, int subtaskIndex, AnswerPlaceholderSubtaskInfo info) {
+    existingPlaceholder.getSubtaskInfos().remove(subtaskIndex);
+  }
+
+  @Override
+  protected boolean isAvailable(TaskFile taskFile, int offset) {
+    AnswerPlaceholder existingPlaceholder = taskFile.getAnswerPlaceholder(offset, taskFile.getAnswerPlaceholders());
+    return existingPlaceholder != null && !existingPlaceholder.isActive();
+  }
+}
---
+++ python/educational-core/course-creator/src/com/jetbrains/edu/coursecreator/actions/CCSubtaskPlaceholderAction.java
@@ -0,0 +1,77 @@
+package com.jetbrains.edu.coursecreator.actions;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.command.undo.BasicUndoableAction;
+import com.intellij.openapi.command.undo.UnexpectedUndoException;
+import com.intellij.openapi.editor.Editor;
+import com.jetbrains.edu.learning.StudyUtils;
+import com.jetbrains.edu.learning.core.EduUtils;
+import com.jetbrains.edu.learning.courseFormat.AnswerPlaceholder;
+import com.jetbrains.edu.learning.courseFormat.AnswerPlaceholderSubtaskInfo;
+import com.jetbrains.edu.learning.courseFormat.TaskFile;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public abstract class CCSubtaskPlaceholderAction extends CCAnswerPlaceholderAction {
+  protected CCSubtaskPlaceholderAction(@Nullable String text, @Nullable String description) {
+    super(text, description);
+  }
+
+  @Override
+  protected void performAnswerPlaceholderAction(@NotNull CCState state) {
+    Editor editor = state.getEditor();
+    final int offset = editor.getCaretModel().getOffset();
+    TaskFile taskFile = state.getTaskFile();
+    int subtaskIndex = state.getTaskFile().getTask().getActiveSubtaskIndex();
+    AnswerPlaceholder existingPlaceholder = taskFile.getAnswerPlaceholder(offset, taskFile.getAnswerPlaceholders());
+    if (existingPlaceholder == null) {
+      return;
+    }
+    AnswerPlaceholderSubtaskInfo info = getInfo(state, subtaskIndex, existingPlaceholder);
+    if (info == null) {
+      return;
+    }
+    EduUtils.runUndoableAction(state.getProject(), getTitle(), new BasicUndoableAction(state.getEditor().getDocument()) {
+      @Override
+      public void undo() throws UnexpectedUndoException {
+        undoAction(existingPlaceholder, subtaskIndex, info);
+        StudyUtils.drawAllWindows(editor, taskFile);
+      }
+
+      @Override
+      public void redo() throws UnexpectedUndoException {
+        redoAction(existingPlaceholder, subtaskIndex, info);
+        StudyUtils.drawAllWindows(editor, taskFile);
+      }
+    });
+  }
+
+  protected abstract AnswerPlaceholderSubtaskInfo getInfo(CCState state, int subtaskIndex, AnswerPlaceholder existingPlaceholder);
+
+  protected abstract String getTitle();
+
+  protected abstract void redoAction(AnswerPlaceholder existingPlaceholder, int subtaskIndex, AnswerPlaceholderSubtaskInfo info);
+
+  protected abstract void undoAction(AnswerPlaceholder existingPlaceholder, int subtaskIndex, AnswerPlaceholderSubtaskInfo info);
+
+  @Override
+  public void update(AnActionEvent e) {
+    Presentation presentation = e.getPresentation();
+    presentation.setEnabledAndVisible(false);
+    CCState state = getState(e);
+    if (state == null) {
+      return;
+    }
+    TaskFile taskFile = state.getTaskFile();
+    if (!taskFile.getTask().hasSubtasks()) {
+      return;
+    }
+    int offset = state.getEditor().getCaretModel().getOffset();
+    if (isAvailable(taskFile, offset)) {
+      presentation.setEnabledAndVisible(true);
+    }
+  }
+
+  protected abstract boolean isAvailable(TaskFile taskFile, int offset);
+}
