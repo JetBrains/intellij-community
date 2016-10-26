3897001f18fc41ace2ab2a0269ab9b5e34becc52
Anna.Kozlova <anna.kozlova@jetbrains.com>
2016-10-26 17:21:30 +0200

    simplify static checks

--- java/execution/impl/src/com/intellij/execution/junit/JUnitUtil.java
+++ java/execution/impl/src/com/intellij/execution/junit/JUnitUtil.java
@@ -122,9 +122,8 @@ public class JUnitUtil {
       }
     }
     if (psiMethod.getParameterList().getParametersCount() > 0) return false;
-    if (psiMethod.hasModifierProperty(PsiModifier.STATIC) && SUITE_METHOD_NAME.equals(psiMethod.getName())) return false;
-    if (!psiMethod.getName().startsWith("test")) return false;
     if (psiMethod.hasModifierProperty(PsiModifier.STATIC)) return false;
+    if (!psiMethod.getName().startsWith("test")) return false;
     PsiClass testCaseClass = getTestCaseClassOrNull(location);
     return testCaseClass != null && psiMethod.getContainingClass().isInheritor(testCaseClass, true) && PsiType.VOID.equals(psiMethod.getReturnType());
   }
