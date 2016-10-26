e370b1a954c34229d8e96e11cf628dfd9fa7b051
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2016-10-26 20:04:21 +0300

    IDEA-162905 "Select Target -> Project View" doesn't work

--- platform/lang-impl/src/com/intellij/ide/projectView/impl/ProjectViewPane.java
+++ platform/lang-impl/src/com/intellij/ide/projectView/impl/ProjectViewPane.java
@@ -194,7 +194,7 @@ public class ProjectViewPane extends AbstractProjectViewPSIPane {
 
     @Override
     public boolean isToBuildChildrenInBackground(Object element) {
-      return true;
+      return !(element instanceof AbstractTreeNode) || !((AbstractTreeNode)element).isAlwaysLeaf();
     }
   }
 
