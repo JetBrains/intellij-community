ebf0eec15e2c37fed81540c8ba31815539e54046
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2016-11-01 21:23:46 +0300

    Switch to standard PyResolveImportUtil to find Python stubs for Python files
    I've increased the priority of Python stubs so that resolve works to a
    stub file in the SDK even though there are corresponding Python files
    in the project. Let's see if it is a good idea.

--- python/src/com/jetbrains/python/pyi/PyiUtil.java
+++ python/src/com/jetbrains/python/pyi/PyiUtil.java
@@ -15,19 +15,9 @@
  */
 package com.jetbrains.python.pyi;
 
-import com.google.common.collect.ImmutableList;
-import com.intellij.openapi.extensions.Extensions;
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.projectRoots.Sdk;
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiManager;
 import com.intellij.psi.util.QualifiedName;
-import com.intellij.util.containers.ContainerUtil;
 import com.jetbrains.python.codeInsight.controlflow.ScopeOwner;
 import com.jetbrains.python.codeInsight.dataflow.scope.ScopeUtil;
 import com.jetbrains.python.psi.*;
@@ -35,11 +25,9 @@ import com.jetbrains.python.psi.resolve.*;
 import com.jetbrains.python.psi.types.PyClassLikeType;
 import com.jetbrains.python.psi.types.PyType;
 import com.jetbrains.python.psi.types.TypeEvalContext;
-import com.jetbrains.python.sdk.PythonSdkType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -62,103 +50,15 @@ public class PyiUtil {
 
   @Nullable
   private static PyiFile getPythonStubFile(@NotNull PyFile file) {
-    final PyiFile result = findPythonStubNextToFile(file);
-    if (result != null) {
-      return result;
-    }
-    return findPythonStubInRoots(file);
-  }
-
-  @Nullable
-  private static PyiFile findPythonStubInRoots(@NotNull PyFile file) {
-    final QualifiedName qName = findImportableName(file);
-    final Sdk sdk = PythonSdkType.getSdk(file);
-    if (qName != null && sdk != null) {
-      final List<String> stubQNameComponents = ContainerUtil.newArrayList("python-stubs");
-      stubQNameComponents.addAll(qName.getComponents());
-      final QualifiedName stubQName = QualifiedName.fromComponents(stubQNameComponents);
-      final Project project = file.getProject();
-      final PythonSdkPathCache cache = PythonSdkPathCache.getInstance(project, sdk);
-      final List<PsiElement> cachedResults = cache.get(stubQName);
-      if (cachedResults != null) {
-        return getFirstPyiFile(cachedResults);
-      }
-      final ArrayList<PsiElement> results = new ArrayList<>();
-      final PsiManager psiManager = PsiManager.getInstance(project);
-      final String nameAsPath = StringUtil.join(qName.getComponents(), "/");
-      final List<String> paths = ImmutableList.of(
-        nameAsPath + "/__init__.pyi",
-        nameAsPath + ".pyi");
-      final RootVisitor rootVisitor = new RootVisitor() {
-        @Override
-        public boolean visitRoot(VirtualFile root, @Nullable Module module, @Nullable Sdk sdk, boolean isModuleSource) {
-          if (root.isValid()) {
-            for (String path : paths) {
-              final VirtualFile pyiVirtualFile = root.findFileByRelativePath(path);
-              if (pyiVirtualFile != null) {
-                final PsiFile pyiFile = psiManager.findFile(pyiVirtualFile);
-                if (pyiFile instanceof PyiFile) {
-                  results.add(pyiFile);
-                  return false;
-                }
-              }
-            }
-          }
-          return true;
-        }
-      };
-      RootVisitorHost.visitRoots(file, rootVisitor);
-      RootVisitorHost.visitSdkRoots(sdk, rootVisitor);
-      cache.put(stubQName, results);
-      return getFirstPyiFile(results);
-    }
-    return null;
-  }
-
-  @Nullable
-  private static PyiFile getFirstPyiFile(List<PsiElement> elements) {
-    if (elements.isEmpty()) {
+    final QualifiedName name = QualifiedNameFinder.findCanonicalImportPath(file, file);
+    if (name == null) {
       return null;
     }
-    final PsiElement result = elements.get(0);
-    if (result instanceof PyiFile) {
-      return (PyiFile)result;
-    }
-    return null;
-  }
-
-  @Nullable
-  private static PyiFile findPythonStubNextToFile(@NotNull PyFile file) {
-    final PsiDirectory dir = file.getContainingDirectory();
-    final VirtualFile virtualFile = file.getVirtualFile();
-    if (dir != null && virtualFile != null) {
-      final String fileName = virtualFile.getNameWithoutExtension();
-      final String pythonStubFileName = fileName + "." + PyiFileType.INSTANCE.getDefaultExtension();
-      final PsiFile pythonStubFile = dir.findFile(pythonStubFileName);
-      if (pythonStubFile instanceof PyiFile) {
-        return (PyiFile)pythonStubFile;
-      }
-    }
-    return null;
-  }
-
-  @Nullable
-  private static QualifiedName findImportableName(@NotNull PyFile file) {
-    final VirtualFile moduleVirtualFile = file.getVirtualFile();
-    if (moduleVirtualFile != null) {
-      String moduleName = QualifiedNameFinder.findShortestImportableName(file, moduleVirtualFile);
-      if (moduleName != null) {
-        final QualifiedName qName = QualifiedName.fromDottedString(moduleName);
-        for (PyCanonicalPathProvider provider : Extensions.getExtensions(PyCanonicalPathProvider.EP_NAME)) {
-          final QualifiedName restored = provider.getCanonicalPath(qName, null);
-          if (restored != null) {
-            return restored;
-          }
-        }
-        return qName;
-      }
-    }
-    return null;
+    final PyQualifiedNameResolveContext context = PyResolveImportUtil.fromFoothold(file);
+    return PyUtil.as(PyResolveImportUtil.resolveQualifiedName(name, context)
+      .stream()
+      .findFirst()
+      .orElse(null), PyiFile.class);
   }
 
   @Nullable
