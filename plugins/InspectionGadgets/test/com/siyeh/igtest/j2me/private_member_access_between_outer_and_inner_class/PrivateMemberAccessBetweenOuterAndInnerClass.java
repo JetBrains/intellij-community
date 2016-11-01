2fb544975c11988901233ddd4fd26d687446f2d9
Bas Leijdekkers <basleijdekkers@gmail.com>
2016-11-01 12:22:38 +0100

    Report access of private members between sibling inner classes in "Synthetic method call" inspection (IDEA-163379)

--- plugins/InspectionGadgets/test/com/siyeh/igtest/j2me/private_member_access_between_outer_and_inner_class/PrivateMemberAccessBetweenOuterAndInnerClass.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/j2me/private_member_access_between_outer_and_inner_class/PrivateMemberAccessBetweenOuterAndInnerClass.java
@@ -9,11 +9,52 @@ public class PrivateMemberAccessBetweenOuterAndInnerClass {
         System.out.println(caption);
         Object btn = new Object() {
             public void foo() {
-                System.out.println(<warning descr="Access to private member of class 'PrivateMemberAccessBetweenOuterAndInnerClass'">caption</warning>);
+                System.out.println(<warning descr="Access to private member of class 'PrivateMemberAccessBetweenOuterAndInnerClass' requires synthetic method call">caption</warning>);
                 System.out.println(N);
             }
         };
     }
 
     private static class Inner{}
+}
+class X {
+    void test() {
+        Private ref = new Private("access");
+        System.out.println(ref.<warning descr="Access to private member of class 'Private' requires synthetic method call">field</warning>);
+        PrivateAccessor.printPrivate(ref);
+        new PrivateAccessor().print(ref);
+    }
+
+    private static class Private {
+        private final String field;
+        Private(String value) {
+            this.field = value;
+        }
+    }
+
+    private static class PrivateAccessor {
+        PrivateAccessor() {
+            // prevent synthetic constructor
+        }
+        void print(Private ref) {
+            System.out.println(ref.<warning descr="Access to private member of class 'Private' requires synthetic method call">field</warning>);
+        }
+        static void printPrivate(Private ref) {
+            System.out.println(ref.<warning descr="Access to private member of class 'Private' requires synthetic method call">field</warning>);
+        }
+    }
+}
+class Y {
+    void m() {
+        String caption = new PrivateMemberAccessBetweenOuterAndInnerClass().<error descr="'caption' has private access in 'com.siyeh.igtest.j2me.private_member_access_between_outer_and_inner_class.PrivateMemberAccessBetweenOuterAndInnerClass'">caption</error>;
+        //noinspection PrivateMemberAccessBetweenOuterAndInnerClass
+        System.out.println(X.s);
+
+        //noinspection SyntheticMethodCall
+        System.out.println(X.s);
+    }
+
+    static class X {
+        private static String s = "";
+    }
 }
