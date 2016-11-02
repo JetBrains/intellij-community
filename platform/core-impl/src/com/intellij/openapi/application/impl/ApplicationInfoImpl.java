720174b07ab9c4ee7796bbd91e16c877c391b6a4
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2016-11-02 11:20:40 +0100

    cleanup
    (cherry picked from commit 476a7f3ce89868d4334331d96c539cf4733ec9df)

--- platform/core-impl/src/com/intellij/openapi/application/impl/ApplicationInfoImpl.java
+++ platform/core-impl/src/com/intellij/openapi/application/impl/ApplicationInfoImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -869,12 +869,11 @@ public class ApplicationInfoImpl extends ApplicationInfoEx {
     }
 
     myPluginChooserPages = new ArrayList<PluginChooserPage>();
-    final List children = parentNode.getChildren(PLUGINS_PAGE_ELEMENT_NAME);
-    for(Object child: children) {
-      myPluginChooserPages.add(new PluginChooserPageImpl((Element) child));
+    for (Element child : parentNode.getChildren(PLUGINS_PAGE_ELEMENT_NAME)) {
+      myPluginChooserPages.add(new PluginChooserPageImpl(child));
     }
 
-    List<Element> essentialPluginsElements = JDOMUtil.getChildren(parentNode, ESSENTIAL_PLUGIN);
+    List<Element> essentialPluginsElements = parentNode.getChildren(ESSENTIAL_PLUGIN);
     Collection<String> essentialPluginsIds = ContainerUtil.mapNotNull(essentialPluginsElements, new Function<Element, String>() {
       @Override
       public String fun(Element element) {
