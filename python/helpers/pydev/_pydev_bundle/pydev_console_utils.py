7e70c470ac772725b866a56580a0733381478110
Elizaveta Shashkova <Elizaveta.Shashkova@jetbrains.com>
2016-11-02 19:11:45 +0300

    Error 'Inappropriate ioctl for device' in debugger with Django (PY-21096)
    Do not override original sys.stdin.isatty(), because it can't be set to any constant value. It should be False for local interpreters and True for remote interpreters.

--- python/helpers/pydev/_pydev_bundle/pydev_console_utils.py
+++ python/helpers/pydev/_pydev_bundle/pydev_console_utils.py
@@ -10,14 +10,6 @@ from _pydevd_bundle.pydevd_constants import IS_JYTHON
 from _pydevd_bundle.pydevd_utils import to_string
 
 
-USE_IPYTHON = False
-
-
-def set_result_ipython_value(ipython):
-    global USE_IPYTHON
-    USE_IPYTHON = ipython
-
-
 # =======================================================================================================================
 # Null
 # =======================================================================================================================
@@ -83,10 +75,6 @@ class BaseStdIn:
 
     def write(self, *args, **kwargs):
         pass  # not available StdIn (but it can be expected to be in the stream interface)
-
-    def isatty(self):
-        # return True for IPython console only
-        return USE_IPYTHON
 
     def flush(self, *args, **kwargs):
         pass  # not available StdIn (but it can be expected to be in the stream interface)
