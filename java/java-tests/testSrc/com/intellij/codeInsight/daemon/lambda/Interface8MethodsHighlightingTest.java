0fca4a0f17ebc9100f7fe17abae1e915de4d107c
Anna.Kozlova <anna.kozlova@jetbrains.com>
2016-11-02 12:37:41 +0100

    unrelated defaults when one interface overrides 2 unrelated (IDEA-163469)
    (cherry picked from commit bf259fed793b46c26b153a707663c86462aa1fd5)

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/Interface8MethodsHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/Interface8MethodsHighlightingTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -64,6 +64,10 @@ public class Interface8MethodsHighlightingTest extends LightCodeInsightFixtureTe
   }
 
   public void testUnrelatedDefaultsWithTypeParameter() throws Exception {
+    doTest(false, false);
+  }
+
+  public void testUnrelatedDefaultsWhenOneInterfaceOverrides2Unrelated() throws Exception {
     doTest(false, false);
   }
 
