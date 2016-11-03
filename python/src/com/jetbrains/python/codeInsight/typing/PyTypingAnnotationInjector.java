09944f3725ebf7b84b223dca5be64ff544604df2
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2016-11-03 15:56:23 +0300

    Moved PyTyping* classes into *.codeInsight.typing package

--- python/src/com/jetbrains/python/codeInsight/typing/PyTypingAnnotationInjector.java
+++ python/src/com/jetbrains/python/codeInsight/typing/PyTypingAnnotationInjector.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.jetbrains.python.codeInsight;
+package com.jetbrains.python.codeInsight.typing;
 
 import com.intellij.codeInsight.completion.CompletionUtil;
 import com.intellij.lang.Language;
@@ -23,6 +23,8 @@ import com.intellij.psi.PsiComment;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiLanguageInjectionHost;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.jetbrains.python.codeInsight.PyInjectionUtil;
+import com.jetbrains.python.codeInsight.PyInjectorBase;
 import com.jetbrains.python.codeInsight.functionTypeComments.PyFunctionTypeAnnotationDialect;
 import com.jetbrains.python.documentation.doctest.PyDocstringLanguageDialect;
 import com.jetbrains.python.psi.PyAnnotation;
