cf5531e2e60ed8abb2eeb34f230c7e97345ff0c7
peter <peter@jetbrains.com>
2016-11-03 14:23:31 +0100

    assert that MemberChooser is shown in a transaction (IDEA-CR-15125)

--- platform/lang-impl/src/com/intellij/ide/util/MemberChooser.java
+++ platform/lang-impl/src/com/intellij/ide/util/MemberChooser.java
@@ -22,6 +22,7 @@ import com.intellij.ide.IdeBundle;
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.TransactionGuard;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.keymap.KeymapManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
@@ -55,6 +56,7 @@ import java.util.*;
 import java.util.List;
 
 public class MemberChooser<T extends ClassMember> extends DialogWrapper implements TypeSafeDataProvider {
+  private static final Logger LOG = Logger.getInstance("#com.intellij.ide.util.MemberChooser");
   protected Tree myTree;
   private DefaultTreeModel myTreeModel;
   protected JComponent[] myOptionControls;
@@ -685,7 +687,8 @@ public class MemberChooser<T extends ClassMember> extends DialogWrapper implemen
 
   @Override
   public void show() {
-    TransactionGuard.getInstance().submitTransactionAndWait(super::show);
+    LOG.assertTrue(TransactionGuard.getInstance().getContextTransaction() != null, "Member Chooser should be shown in a transaction, see AnAction#startInTransaction");
+    super.show();
   }
 
   @Override
