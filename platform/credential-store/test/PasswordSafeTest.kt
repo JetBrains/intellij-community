2faa26850b089413f8832d7f9a530c1d0d399c77
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2016-11-04 17:14:19 +0100

    fix test `credentials with empty username - KeePass`
    (cherry picked from commit b8a41dc)

--- platform/credential-store/test/PasswordSafeTest.kt
+++ platform/credential-store/test/PasswordSafeTest.kt
@@ -1,5 +1,6 @@
 package com.intellij.credentialStore
 
+import com.intellij.ide.passwordSafe.PasswordSafe
 import com.intellij.ide.passwordSafe.impl.PasswordSafeImpl
 import com.intellij.testFramework.ApplicationRule
 import com.intellij.testFramework.Assertions.assertThat
@@ -77,6 +78,85 @@ class PasswordSafeTest {
       val saved = ps.get(attributes)!!
       assertThat(saved.userName).isNullOrEmpty()
       assertThat(saved.password).isEqualTo("password")
+    }
+    finally {
+      ps.set(attributes, null)
+    }
+  }
+
+  @Test
+  fun `overwrite credentials - KeePass`() {
+    val settings = PasswordSafeSettings()
+    settings.providerType = ProviderType.KEEPASS
+    val ps = PasswordSafeImpl(settings, KeePassCredentialStore(baseDirectory = tempDirManager.newPath()))
+
+    val booleans = booleanArrayOf(true, false)
+    for (old in booleans) {
+      for (new in booleans) {
+        for (rewriteNull in booleans) {
+          checkCredentialsRewritten(ps, old, new, rewriteNull)
+        }
+      }
+    }
+  }
+
+  fun checkCredentialsRewritten(passwordSafe: PasswordSafe,
+                                isMemoryOnlyOld: Boolean,
+                                isMemoryOnlyNew: Boolean,
+                                rewriteWithNull: Boolean) {
+    val id = "test checkCredentialsRewritten $isMemoryOnlyOld $isMemoryOnlyNew $rewriteWithNull"
+    val attributesMemoryOnly = CredentialAttributes(id, null, null, true)
+    val attributesSaved = CredentialAttributes(id, null, null, false)
+
+    val description = "Parameters isMemoryOnlyOld=$isMemoryOnlyOld isMemoryOnlyNew=$isMemoryOnlyNew rewriteWithNull=$rewriteWithNull"
+    val oldPassword = "oldPassword"
+    val newPassword = if (rewriteWithNull) null else "newPassword"
+    val oldCredentials = Credentials("oldName", oldPassword)
+    val newCredentials = Credentials("newName", newPassword)
+
+    try {
+      passwordSafe.set(if (isMemoryOnlyOld) attributesMemoryOnly else attributesSaved, oldCredentials)
+      passwordSafe.set(if (isMemoryOnlyNew) attributesMemoryOnly else attributesSaved, newCredentials)
+
+      checkCredentials(description, attributesMemoryOnly, newCredentials, passwordSafe, rewriteWithNull)
+      checkCredentials(description, attributesSaved, newCredentials, passwordSafe, rewriteWithNull)
+    }
+    finally {
+      passwordSafe.set(attributesMemoryOnly, null)
+      passwordSafe.set(attributesSaved, null)
+    }
+  }
+
+  private fun checkCredentials(description: String,
+                               attributes: CredentialAttributes,
+                               newCredentials: Credentials,
+                               passwordSafe: PasswordSafe,
+                               rewriteWithNull: Boolean) {
+    val resultMemoryOnly = passwordSafe.get(attributes)!!
+    assertThat(resultMemoryOnly.userName).`as`(description).isEqualTo(newCredentials.userName)
+    val assertThat = assertThat(resultMemoryOnly.password).`as`(description)
+    if (rewriteWithNull) {
+      assertThat.isNullOrEmpty()
+    }
+    else {
+      assertThat.isEqualTo(newCredentials.password)
+    }
+  }
+
+  @Test
+  fun `credentials with empty username - KeePass`() {
+    val settings = PasswordSafeSettings()
+    settings.providerType = ProviderType.KEEPASS
+    val ps = PasswordSafeImpl(settings, KeePassCredentialStore(baseDirectory = tempDirManager.newPath()))
+
+    val id = "test PasswordSafeTest.credentials with empty username"
+    val attributes = CredentialAttributes(id, isPasswordMemoryOnly = true)
+    try {
+      val credentials = Credentials(null, "passphrase")
+      ps.set(attributes, credentials)
+      val saved = ps.get(attributes)!!
+      assertThat(saved.password).isEqualTo(credentials.password)
+      assertThat(saved.userName).isNullOrEmpty()
     }
     finally {
       ps.set(attributes, null)
