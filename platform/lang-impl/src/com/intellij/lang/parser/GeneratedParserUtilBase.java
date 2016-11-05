d860fbb3f6c3571ca833f0d4fade962975feaa5b
Gregory.Shrago <gregory.shrago@jetbrains.com>
2016-11-05 19:45:14 +0300

    GK: merge "token sequences with token texts: completion"
    (cherry picked from commit d6172f1836d2bdcaf463f86018582c0dc4fdec2b)

--- platform/lang-impl/src/com/intellij/lang/parser/GeneratedParserUtilBase.java
+++ platform/lang-impl/src/com/intellij/lang/parser/GeneratedParserUtilBase.java
@@ -839,7 +839,7 @@ public class GeneratedParserUtilBase {
 
     @Override
     public String fun(Object o) {
-      return o.toString();
+      return convertItem(o);
     }
 
     public void addItem(@NotNull PsiBuilder builder, @NotNull String text) {
