c048cb8794b94602f6fce5297f9b23ebbe8de135
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2016-11-08 23:22:33 +0300

    svn: Fixed "TreeConflictRefreshablePanel" disposing

--- plugins/svn4idea/src/org/jetbrains/idea/svn/difftool/SvnTreeConflictDiffRequestProvider.java
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/difftool/SvnTreeConflictDiffRequestProvider.java
@@ -1,14 +1,15 @@
 package org.jetbrains.idea.svn.difftool;
 
-import com.intellij.openapi.progress.BackgroundTaskQueue;
-import com.intellij.openapi.progress.ProcessCanceledException;
-import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.UserDataHolder;
 import com.intellij.diff.DiffContext;
 import com.intellij.diff.FrameDiffTool;
 import com.intellij.diff.chains.DiffRequestProducerException;
 import com.intellij.diff.requests.DiffRequest;
+import com.intellij.openapi.progress.BackgroundTaskQueue;
+import com.intellij.openapi.progress.ProcessCanceledException;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.util.UserDataHolder;
 import com.intellij.openapi.vcs.changes.Change;
 import com.intellij.openapi.vcs.changes.actions.diff.ChangeDiffRequestProducer;
 import com.intellij.openapi.vcs.changes.actions.diff.ChangeDiffRequestProvider;
@@ -131,6 +132,7 @@ public class SvnTreeConflictDiffRequestProvider implements ChangeDiffRequestProv
     @Override
     public void dispose() {
       myQueue.clear();
+      Disposer.dispose(myDelegate);
     }
   }
 }
