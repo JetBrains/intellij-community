240c0b2d33d8cf1ea94e47c681e98e294cbfbe74
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2016-11-09 14:41:22 +0100

    convert IndexedVariablesConsumer, FunctionValue, ValueType, PrimitiveValue to kotlin

--- platform/script-debugger/backend/src/org/jetbrains/debugger/values/FunctionValue.java
+++ platform/script-debugger/backend/src/org/jetbrains/debugger/values/FunctionValue.java
@@ -1,35 +1,30 @@
-package org.jetbrains.debugger.values;
+package org.jetbrains.debugger.values
 
-import com.intellij.util.ThreeState;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.concurrency.Promise;
-import org.jetbrains.debugger.Scope;
+import com.intellij.util.ThreeState
+import org.jetbrains.concurrency.Promise
+import org.jetbrains.debugger.Scope
 
-public interface FunctionValue extends ObjectValue {
+interface FunctionValue : ObjectValue {
   /**
-   * You must invoke {@link #resolve} to use any function value methods
+   * You must invoke [.resolve] to use any function value methods
    */
-  @NotNull
-  Promise<FunctionValue> resolve();
+  fun resolve(): Promise<FunctionValue>
 
   /**
    * Returns position of opening parenthesis of function arguments. Position is absolute
    * within resource (not relative to script start position).
-   *
+
    * @return position or null if position is not available
    */
-  int getOpenParenLine();
+  val openParenLine: Int
 
-  int getOpenParenColumn();
+  val openParenColumn: Int
 
-  @Nullable
-  Scope[] getScopes();
+  val scopes: Array<Scope>?
 
   /**
    * Method could be called (it is normal and expected) for unresolved function.
-   * It must return quickly. Return {@link com.intellij.util.ThreeState#UNSURE} otherwise.
+   * It must return quickly. Return [com.intellij.util.ThreeState.UNSURE] otherwise.
    */
-  @NotNull
-  ThreeState hasScopes();
+  fun hasScopes() = ThreeState.UNSURE
 }
