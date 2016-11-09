bf71c158425aecfb8630639cca6954a6b97e7cde
irengrig <irina.chernushina@jetbrains.com>
2016-11-09 15:21:12 +0100

    WEB-23995 JSON schema: "type is not allowed" for valid schema and instance
    (cherry picked from commit 47204fa)

--- json/tests/test/com/jetbrains/jsonSchema/impl/JsonBySchemaCompletionTest.java
+++ json/tests/test/com/jetbrains/jsonSchema/impl/JsonBySchemaCompletionTest.java
@@ -10,7 +10,6 @@ import org.junit.Assert;
 
 import java.io.StringReader;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -97,6 +96,59 @@ public class JsonBySchemaCompletionTest extends CompletionTestCase {
     final String schema = "{\"allOf\": [{\"type\": \"object\", \"properties\": {\"first\": {}}}," +
                           " {\"properties\": {\"second\": {\"enum\": [33,44]}}}]}";
     testImpl(schema, "{\"second\": <caret>}", "33", "44");
+  }
+
+  public void testValueCompletion() throws Exception {
+    final String schema = "{\n" +
+                          "  \"properties\": {\n" +
+                          "    \"top\": {\n" +
+                          "      \"enum\": [\"test\", \"me\"]\n" +
+                          "    }\n" +
+                          "  }\n" +
+                          "}";
+    testImpl(schema, "{\"top\": <caret>}", "\"me\"", "\"test\"");
+  }
+
+  public void testTopLevelArrayPropNameCompletion() throws Exception {
+    final String schema = parcelShopSchema();
+    testImpl(schema, "[{<caret>}]", "\"address\"");
+    testImpl(schema, "[{\"address\": {<caret>}}]", "\"fax\"", "\"houseNumber\"");
+    testImpl(schema, "[{\"address\": {\"houseNumber\": <caret>}}]", "1", "2");
+  }
+
+  @NotNull
+  private static String parcelShopSchema() {
+    return "{\n" +
+                          "  \"$schema\": \"http://json-schema.org/draft-04/schema#\",\n" +
+                          "\n" +
+                          "  \"title\": \"parcelshop search response schema\",\n" +
+                          "\n" +
+                          "  \"definitions\": {\n" +
+                          "    \"address\": {\n" +
+                          "      \"type\": \"object\",\n" +
+                          "      \"properties\": {\n" +
+                          "        \"houseNumber\": { \"type\": \"integer\", \"enum\": [1,2]},\n" +
+                          "        \"fax\": { \"$ref\": \"#/definitions/phone\" }\n" +
+                          "      }\n" +
+                          "    },\n" +
+                          "    \"phone\": {\n" +
+                          "      \"type\": \"object\",\n" +
+                          "      \"properties\": {\n" +
+                          "        \"countryPrefix\": { \"type\": \"string\" },\n" +
+                          "        \"number\": { \"type\": \"string\" }\n" +
+                          "      }\n" +
+                          "    }\n" +
+                          "  },\n" +
+                          "\n" +
+                          "  \"type\": \"array\",\n" +
+                          "\n" +
+                          "  \"items\": {\n" +
+                          "    \"type\": \"object\",\n" +
+                          "    \"properties\": {\n" +
+                          "      \"address\": { \"$ref\": \"#/definitions/address\" }\n" +
+                          "    }\n" +
+                          "  }\n" +
+                          "}";
   }
 
   private void testImpl(@NotNull final String schema, final @NotNull String text,
