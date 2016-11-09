a77aed6035c742bb5a4979a58930ac4d58201623
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2016-11-09 19:29:20 +0300

    PY-20769 Fixed: Support PEP 519: File system path protocol
    Update skeletons to accept PathLike objects in pathlib classes

--- python/testData/inspections/PyTypeCheckerInspection/PathLikePassedToStdlibFunctions/a.py
+++ python/testData/inspections/PyTypeCheckerInspection/PathLikePassedToStdlibFunctions/a.py
@@ -1,5 +1,6 @@
 import os
 import os.path
+from pathlib import Path, PurePath
 
 
 # os.PathLike
@@ -14,6 +15,9 @@ open(a)
 os.fspath(a)
 os.fsencode(a)
 os.fsdecode(a)
+
+Path(a)
+PurePath(a)
 
 os.path.abspath(a)
 
@@ -30,4 +34,22 @@ os.fspath(b)  # TODO fail
 os.fsencode(b)  # TODO fail
 os.fsdecode(b)  # TODO fail
 
+Path(<warning descr="Expected type 'Union[str, bytes, PathLike]', got 'B' instead">b</warning>)
+PurePath(<warning descr="Expected type 'Union[str, bytes, PathLike]', got 'B' instead">b</warning>)
+
 os.path.abspath(<warning descr="Expected type 'Union[bytes, str, PathLike]', got 'B' instead">b</warning>)
+
+
+# pathlib.PurePath
+p = Path(".")
+
+open(p)
+
+os.fspath(p)
+os.fsencode(p)
+os.fsdecode(p)
+
+Path(p)
+PurePath(p)
+
+os.path.abspath(p)
