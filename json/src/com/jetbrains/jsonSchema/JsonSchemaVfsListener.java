45edf75f248fc19a90f2ef0b1281d081d6b003a3
irengrig <irina.chernushina@jetbrains.com>
2016-11-11 17:30:12 +0100

    Json schema: improve highlighting speed (invalidate all json files corresponding to the schema on schema change). fixes WEB-21418 JSON Schema: if file under the schema is placed in the split Editor tab update comes later than should be key: "json.schema.fast.annotation.update" (turned on by default)
    (cherry picked from commit 95cf95b)

--- json/src/com/jetbrains/jsonSchema/JsonSchemaVfsListener.java
+++ json/src/com/jetbrains/jsonSchema/JsonSchemaVfsListener.java
@@ -15,11 +15,27 @@
  */
 package com.jetbrains.jsonSchema;
 
+import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer;
+import com.intellij.json.JsonFileType;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.util.ZipperUpdater;
+import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.vfs.*;
 import com.intellij.openapi.vfs.impl.BulkVirtualFileListenerAdapter;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.search.FileTypeIndex;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.util.Alarm;
+import com.intellij.util.containers.ContainerUtil;
 import com.jetbrains.jsonSchema.impl.JsonSchemaServiceImpl;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Irina.Chernushina on 3/30/2016.
@@ -29,6 +45,32 @@ public class JsonSchemaVfsListener extends BulkVirtualFileListenerAdapter {
     super(new VirtualFileAdapter() {
       @NotNull private final JsonSchemaServiceImpl myService = service;
       private JsonSchemaMappingsProjectConfiguration myMappingsProjectConfiguration = JsonSchemaMappingsProjectConfiguration.getInstance(project);
+      private final ZipperUpdater myUpdater = new ZipperUpdater(200, Alarm.ThreadToUse.POOLED_THREAD, project);
+      private final Set<VirtualFile> myDirtySchemas = ContainerUtil.newConcurrentSet();
+      private final Runnable myRunnable = () -> {
+        final Set<VirtualFile> scope = new HashSet<>(myDirtySchemas);
+        myDirtySchemas.removeAll(scope);
+        if (scope.isEmpty()) return;
+
+        final DaemonCodeAnalyzer analyzer = DaemonCodeAnalyzer.getInstance(project);
+        ApplicationManager.getApplication().runReadAction(() -> {
+          FileTypeIndex.processFiles(JsonFileType.INSTANCE, jsonFile -> {
+            final Collection<Pair<VirtualFile, String>> collection = myService.getSchemaFilesByFile(jsonFile);
+            if (collection != null && !collection.isEmpty()) {
+              for (Pair<VirtualFile, String> pair : collection) {
+                if (scope.contains(pair.getFirst())) {
+                  final PsiFile psiFile = PsiManager.getInstance(project).findFile(jsonFile);
+                  if (psiFile != null) {
+                    analyzer.restart(psiFile);
+                  }
+                  return true;
+                }
+              }
+            }
+            return true;
+          }, GlobalSearchScope.projectScope(project));
+        });
+      };
 
       @Override
       public void contentsChanged(@NotNull VirtualFileEvent event) {
@@ -60,9 +102,13 @@ public class JsonSchemaVfsListener extends BulkVirtualFileListenerAdapter {
         onFileChange(event.getFile());
       }
 
-      private void onFileChange(@NotNull final VirtualFile file) {
-        if (myMappingsProjectConfiguration.isRegisteredSchemaFile(file)) {
-          myService.dropProviderFromCache(file);
+      private void onFileChange(@NotNull final VirtualFile schemaFile) {
+        if (myMappingsProjectConfiguration.isRegisteredSchemaFile(schemaFile)) {
+          myService.dropProviderFromCache(schemaFile);
+          if (Registry.is("json.schema.fast.annotation.update")) {
+            myDirtySchemas.add(schemaFile);
+            myUpdater.queue(myRunnable);
+          }
         }
       }
     });
