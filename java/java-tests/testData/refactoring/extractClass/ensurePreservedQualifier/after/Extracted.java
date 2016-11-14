034a7db3e3260ea5e5f17d6945726d37b22dcaa0
Anna.Kozlova <anna.kozlova@jetbrains.com>
2016-11-14 18:09:25 +0100

    extract delegate: ensure class fqns are not lost (IDEA-163265)

---
+++ java/java-tests/testData/refactoring/extractClass/ensurePreservedQualifier/after/Extracted.java
@@ -0,0 +1,11 @@
+import java.util.Collections;
+import java.util.List;
+
+public class Extracted {
+    public Extracted() {
+    }
+
+    List<Test> foo() {
+        return Collections.emptyList();
+    }
+}
