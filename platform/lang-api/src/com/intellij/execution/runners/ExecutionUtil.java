94e011e47c6f088930f77dd438c2b94d921bf03e
Egor.Ushakov <egor.ushakov@jetbrains.com>
2016-11-16 18:39:56 +0300

    do not scale twice

--- platform/lang-api/src/com/intellij/execution/runners/ExecutionUtil.java
+++ platform/lang-api/src/com/intellij/execution/runners/ExecutionUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -203,7 +203,7 @@ public class ExecutionUtil {
           GraphicsUtil.setupAAPainting(g2d);
           g2d.setColor(Color.GREEN);
           Ellipse2D.Double shape =
-            new Ellipse2D.Double(x + getIconWidth() - JBUI.scale(iSize), y + getIconHeight() - iSize, iSize, iSize);
+            new Ellipse2D.Double(x + getIconWidth() - iSize, y + getIconHeight() - iSize, iSize, iSize);
           g2d.fill(shape);
           g2d.setColor(ColorUtil.withAlpha(Color.BLACK, .40));
           g2d.draw(shape);
