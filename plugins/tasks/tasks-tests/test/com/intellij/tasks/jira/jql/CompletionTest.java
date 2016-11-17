a7bb3c83a96a270ebd6dee5b85fd74c69e4dc57f
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2016-11-17 17:14:50 +0300

    Simplify completion tests for JQL

--- plugins/tasks/tasks-tests/test/com/intellij/tasks/jira/jql/CompletionTest.java
+++ plugins/tasks/tasks-tests/test/com/intellij/tasks/jira/jql/CompletionTest.java
@@ -21,6 +21,7 @@ import com.intellij.tasks.jira.jql.codeinsight.JqlStandardFunction;
 import com.intellij.testFramework.fixtures.CodeInsightFixtureTestCase;
 import com.intellij.util.ArrayUtil;
 import com.intellij.util.containers.ContainerUtil;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -28,6 +29,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 
 import static com.intellij.tasks.jira.jql.codeinsight.JqlStandardField.ALL_FIELD_NAMES;
@@ -48,22 +50,22 @@ public class CompletionTest extends CodeInsightFixtureTestCase {
     return true;
   }
 
-  private void checkCompletionVariants(List<String> initial, String... others) throws IOException {
+  private void checkCompletionVariants(@NotNull List<String> variants) throws IOException {
     final Path path = Paths.get(getTestDataPath(), getTestName(false) + ".jql");
     final BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8);
     // ".jql" extension is not registered globally anymore
     myFixture.configureByText(JqlFileType.INSTANCE, StreamUtil.readTextFrom(reader));
     myFixture.completeBasic();
     final List<String> lookups = myFixture.getLookupElementStrings();
-    assertSameElements(lookups, ArrayUtil.toStringArray(ContainerUtil.append(initial, others)));
+    assertSameElements(lookups, ArrayUtil.toStringArray(variants));
   }
 
   private void checkCompletionVariants(String... variants) throws IOException {
-    checkCompletionVariants(ContainerUtil.<String>emptyList(), variants);
+    checkCompletionVariants(Arrays.asList(variants));
   }
 
   public void testBeginningOfLine() throws Exception {
-    checkCompletionVariants(ALL_FIELD_NAMES, "not");
+    checkCompletionVariants(ContainerUtil.append(ALL_FIELD_NAMES, "not"));
   }
 
   public void testAfterClause() throws Exception {
@@ -121,7 +123,7 @@ public class CompletionTest extends CodeInsightFixtureTestCase {
   }
 
   public void testAfterLeftParenthesisInSubClause() throws Exception {
-    checkCompletionVariants(ALL_FIELD_NAMES, "not");
+    checkCompletionVariants(ContainerUtil.append(ALL_FIELD_NAMES, "not"));
   }
 
   public void testAfterSubClause() throws Exception {
@@ -134,7 +136,7 @@ public class CompletionTest extends CodeInsightFixtureTestCase {
   }
 
   public void testAfterNotKeywordInNotClause() throws Exception {
-    checkCompletionVariants(ALL_FIELD_NAMES, "not");
+    checkCompletionVariants(ContainerUtil.append(ALL_FIELD_NAMES, "not"));
   }
 
   public void testAfterOrderKeyword() throws Exception {
@@ -142,7 +144,7 @@ public class CompletionTest extends CodeInsightFixtureTestCase {
   }
 
   public void testAfterWasKeyword() throws Exception {
-    checkCompletionVariants(ALL_FUNCTION_NAMES, "not", "in");
+    checkCompletionVariants(ContainerUtil.append(ALL_FUNCTION_NAMES, "not", "in"));
   }
 
   public void testInList() throws Exception {
