becb6c7387a3c7595d6de7e8e5a470615d006d4c
Anna.Kozlova <anna.kozlova@jetbrains.com>
2016-11-21 17:27:56 +0100

    inference: mark call unchecked required when proper types are compared (IDEA-164243)

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/GraphInferenceHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/GraphInferenceHighlightingTest.java
@@ -491,6 +491,10 @@ public class GraphInferenceHighlightingTest extends LightDaemonAnalyzerTestCase 
     doTest();
   }
 
+  public void testUncheckedConversionDuringProperTypeExpressionConstraintResolution() throws Exception {
+    doTest();
+  }
+
   private void doTest() throws Exception {
     doTest(false);
   }
