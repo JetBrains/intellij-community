e9b94ec11a1ff680a3f61d030a1e238bebaecb8e
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2016-11-21 18:39:26 +0300

    Remove unused stepic names, add "/" to stepic urls where needed
    (cherry picked from commit c83cde1)

--- python/educational-core/student/src/com/jetbrains/edu/learning/stepic/EduStepicNames.java
+++ python/educational-core/student/src/com/jetbrains/edu/learning/stepic/EduStepicNames.java
@@ -11,16 +11,13 @@ public class EduStepicNames {
   public static final String RECOMMENDATION_REACTIONS_URL = "/recommendation-reactions";
   public static final String ATTEMPTS = "/attempts";
   public static final String SUBMISSIONS = "/submissions";
-  public static final String ASSIGNMENT = "/assignments";
-  public static final String VIEWS_URL = "/views";
   public static final String UNITS = "/units";
   public static final String DEFAULT_TASKFILE_NAME = "code.py";
-  public static final String USERS = "/users";
-  public static final String LOGIN = "/accounts/login/";
+  public static final String USERS = "/users/";
   public static final String STEP_SOURCES = "/step-sources/";
   public static final String CURRENT_USER = "/stepics/1";
   public static final String COURSES = "/courses";
-  public static final String STEPS = "/steps";
+  public static final String STEPS = "/steps/";
   public static final String SECTIONS = "/sections/";
   public static final String ENROLLMENTS = "/enrollments";
   public static final String STEPIC_REGISTRATION_LINK = "https://stepic.org/registration?next=%2Fexplore%2Fcourses";
