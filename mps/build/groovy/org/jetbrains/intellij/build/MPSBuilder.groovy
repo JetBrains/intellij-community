72825c77d7c830f30ed0b8b4f64cb79678f8721f
Victor.Matchenko <Victor.Matchenko@jetbrains.com>
2016-11-23 12:52:52 +0100

    mps-platform project: initial comit
    Add project with MPS platform, which can build platform by this steps:
    * clone IDEA community in subfolder
    * checkout exact version by git branch/commit/tag
    * apply MPS patches over IDEA sources
    * run groovy scripts to download Kotlin
    * run groovy scripts to build platform
    
    GitOrigin-RevId: 6d960252284b319c74eb10f26fa70a6ce665d602

---
+++ mps/build/groovy/org/jetbrains/intellij/build/MPSBuilder.groovy
@@ -0,0 +1,26 @@
+package org.jetbrains.intellij.build
+
+import org.codehaus.gant.GantBinding
+
+/**
+ * @author nik
+ */
+class MPSBuilder {
+    private final GantBinding binding
+    private final String home
+
+    MPSBuilder(String home, GantBinding binding) {
+        this.home = home
+        this.binding = binding
+    }
+
+    def build() {
+        def buildContext = BuildContext.createContext(
+                binding.ant, binding.projectBuilder, binding.project, binding.global,
+                "$home/community", home, new MPSProperties(home)
+        )
+        buildContext.getOptions().targetOS = ""
+        def buildTasks = BuildTasks.create(buildContext)
+        buildTasks.compileModulesAndBuildDistributions()
+    }
+}
---
+++ mps/build/groovy/org/jetbrains/intellij/build/MPSProperties.groovy
@@ -0,0 +1,136 @@
+package org.jetbrains.intellij.build
+
+import org.jetbrains.intellij.build.impl.PlatformLayout
+
+import java.util.function.Consumer
+
+/**
+ * @author nik
+ */
+class MPSProperties extends ProductProperties {
+    MPSProperties(String home) {
+        baseFileName = "mps"
+        productCode = "MPS"
+        platformPrefix = "Idea"
+        applicationInfoModule = "community-resources"
+//        brandingResourcePaths = ["$home/branding/MPS"]
+        toolsJarRequired = true
+
+        productLayout.mainJarName = "platform.jar"
+        productLayout.mainModule = "community-main"
+        productLayout.platformApiModules = CommunityRepositoryModules.PLATFORM_API_MODULES
+        productLayout.platformImplementationModules = CommunityRepositoryModules.PLATFORM_IMPLEMENTATION_MODULES +
+                ["util",
+                 "util-rt",
+                 "annotations",
+                 "extensions",
+                 "platform-main",
+                 "testFramework",
+                 "tests_bootstrap",
+                 "execution-openapi",
+                 "execution-impl",
+                 "bootstrap",
+                 "resources",
+//                 "community-resources",
+                 "platform-resources",
+                 "forms_rt",
+                 "forms-compiler",
+                 "resources-en",
+                 "platform-resources-en",
+                 "colorSchemes",
+                 "javac2",
+                 "instrumentation-util",
+                 "external-system-impl"]
+
+        productLayout.bundledPluginModules = [
+                "git4idea", "svn4idea", "cvs-plugin",
+                "terminal"
+                /*, "properties", "ant"*/
+        ]
+
+        productLayout.additionalPlatformJars.put("forms_rt.jar", "forms-compiler")
+        productLayout.additionalPlatformJars.putAll("resources.jar", ["resources", "resources-en"])
+        productLayout.additionalPlatformJars.
+                putAll("javac2.jar", ["javac2", "forms-compiler", "forms_rt", "instrumentation-util", "instrumentation-util-8"])
+
+        productLayout.platformLayoutCustomizer = { PlatformLayout layout ->
+            layout.customize {
+                withModule("java-runtime", "idea_rt.jar", false)
+                withProjectLibrary("Eclipse")
+                withProjectLibrary("jgoodies-common")
+                withProjectLibrary("jgoodies-looks")
+                withProjectLibrary("commons-net")
+                withProjectLibrary("hamcrest")
+                withProjectLibrary("JUnit4")
+                withProjectLibrary("http-client-3.1")
+                withProjectLibrary("pty4j") // for terminal plugin
+                withProjectLibrary("purejavacomm") // for terminal plugin
+                withoutProjectLibrary("Ant")
+                withoutProjectLibrary("Gradle")
+                withoutProjectLibrary("com.twelvemonkeys.imageio:imageio-tiff:3.2.1")
+                excludeFromModule("resources", "META-INF/IdeaPlugin.xml")
+                excludeFromModule("resources", "componentSets")
+                excludeFromModule("resources", "ProductivityFeaturesRegistry.xml")
+//                excludeFromModule("community-resources", "idea")
+//                excludeFromModule("community-resources", "lafs")
+//                excludeFromModule("community-resources", "lafs")
+                excludeFromModule("platform-resources", "META-INF/LangExtensions.xml")
+                excludeFromModule("platform-resources", "META-INF/PlatformLangPlugin.xml")
+                excludeFromModule("platform-resources", "META-INF/PlatformLangXmlPlugin.xml")
+                excludeFromModule("platform-resources", "META-INF/XmlPlugin.xml")
+                excludeFromModule("platform-resources", "META-INF/XmlActions.xml")
+                excludeFromModule("platform-resources", "idea/PlatformActions.xml")
+                excludeFromModule("platform-resources-en", "messages/FeatureStatisticsBundle.properties")
+            }
+        } as Consumer<PlatformLayout>
+
+        buildSourcesArchive = true
+    }
+
+    @Override
+    void copyAdditionalFiles(final BuildContext context, final String targetDirectory) {
+        super.copyAdditionalFiles(context, targetDirectory)
+        context.ant.copy(todir: "$targetDirectory/lib/ant") {
+            fileset(dir: "$context.paths.communityHome/lib/ant")
+        }
+
+        // for terminal plugin
+        context.ant.copy(todir: "$targetDirectory/lib/libpty/") {
+            fileset(dir: "$context.paths.communityHome/lib/libpty/")
+        }
+    }
+
+    @Override
+    String baseArtifactName(ApplicationInfoProperties applicationInfo, String buildNumber) {
+        "platform"
+    }
+
+    @Override
+    String systemSelector(ApplicationInfoProperties applicationInfo) {
+        "MPS${applicationInfo.majorVersion}0"
+    }
+
+    @Override
+    WindowsDistributionCustomizer createWindowsCustomizer(String projectHome) {
+        return new WindowsDistributionCustomizer() {
+            {
+            }
+        }
+    }
+
+    @Override
+    LinuxDistributionCustomizer createLinuxCustomizer(String projectHome) {
+        return new LinuxDistributionCustomizer() {
+            {
+            }
+        }
+    }
+
+    @Override
+    MacDistributionCustomizer createMacCustomizer(String projectHome) {
+        return new MacDistributionCustomizer() {
+            {
+            }
+        }
+    }
+}
