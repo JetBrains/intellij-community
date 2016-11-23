44490ea1f002294d591cfbcfbf891f2a92c4763e
Anton Makeev <anton.makeev@jetbrains.com>
2016-11-23 22:05:20 +0100

    CPP-8004 Set CLion's project name from CMake's ${PROJECT_NAME}: reset names cache on the welcome screen
    (cherry picked from commit 86d9bf6)

--- platform/platform-impl/src/com/intellij/openapi/project/impl/ProjectImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/project/impl/ProjectImpl.java
@@ -15,6 +15,7 @@
  */
 package com.intellij.openapi.project.impl;
 
+import com.intellij.ide.RecentProjectsManager;
 import com.intellij.ide.plugins.IdeaPluginDescriptor;
 import com.intellij.ide.startup.StartupManagerEx;
 import com.intellij.openapi.application.ApplicationManager;
@@ -133,6 +134,8 @@ public class ProjectImpl extends PlatformComponentManagerImpl implements Project
   public void setProjectName(@NotNull String projectName) {
     if (!projectName.equals(myName)) {
       myName = projectName;
+      RecentProjectsManager.getInstance().clearNameCache();
+      
       StartupManager.getInstance(this).runWhenProjectIsInitialized((DumbAwareRunnable)() -> {
         if (isDisposed()) return;
 
