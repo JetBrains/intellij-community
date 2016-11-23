e390bd0c32ec55872426fbbef92d1cf02b38f190
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2016-11-23 13:04:41 +0100

    Autoinserted value in Search Everywhere should be selected (IDEA-146791) Search Everywhere is not automatically displaying selected text (IDEA-157100)

--- platform/lang-impl/src/com/intellij/ide/actions/SearchEverywhereAction.java
+++ platform/lang-impl/src/com/intellij/ide/actions/SearchEverywhereAction.java
@@ -373,13 +373,14 @@ public class SearchEverywhereAction extends AnAction implements CustomComponentA
           return;
         }
         String text = "";
-        //if (myEditor != null) {
-        //  text = myEditor.getSelectionModel().getSelectedText();
-        //  text = text == null ? "" : text.trim();
-        //}
+        if (myEditor != null) {
+          text = myEditor.getSelectionModel().getSelectedText();
+          text = text == null ? "" : text.trim();
+        }
 
         search.setText(text);
         search.getTextEditor().setForeground(UIUtil.getLabelForeground());
+        search.selectText();
         //titleIndex = new TitleIndexes();
         editor.setColumns(SEARCH_FIELD_COLUMNS);
         myFocusComponent = e.getOppositeComponent();
