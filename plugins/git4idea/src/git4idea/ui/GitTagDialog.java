JetBrains/intellij-community

commit bedf7d3c106756895fdc74fde4cf46d843d79530

Remove unused logic of populating exceptions list
It used to show errors from the list later, but that does't
happen anymore.
Show error manually when needed.

In addition to that move tagging process to the modal task
instead of freezing the UI.

plugins/git4idea/src/git4idea/ui/GitTagDialog.java
@@ -20,7 +20,6 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.io.FileUtil;
-import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vcs.VcsNotifier;
 import com.intellij.openapi.vfs.CharsetToolkit;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -156,10 +155,8 @@ public class GitTagDialog extends DialogWrapper {
 
   /**
    * Perform tagging according to selected options
-   *
-   * @param exceptions the list where exceptions are collected
    */
-  public void runAction(final List<VcsException> exceptions) {
+  public void runAction() {
     final String message = myMessageTextArea.getText();
     final boolean hasMessage = message.trim().length() != 0;
     final File messageFile;
@@ -200,20 +197,18 @@ public class GitTagDialog extends DialogWrapper {
       if (object.length() != 0) {
         h.addParameters(object);
       }
-      try {
-        GitHandlerUtil.doSynchronously(h, GitBundle.getString("tagging.title"), h.printableCommandLine());
-        VcsNotifier.getInstance(myProject).notifySuccess(myTagNameTextField.getText(),
-                                                         "Created tag " + myTagNameTextField.getText() + " successfully.");
-        GitRepository repository = GitUtil.getRepositoryManager(myProject).getRepositoryForRoot(getGitRoot());
-        if (repository != null) {
-          repository.getRepositoryFiles().refresh(true);
-        }
-        else {
-          LOG.error("No repository registered for root: " + getGitRoot());
-        }
+      GitHandlerUtil.doSynchronously(h, GitBundle.getString("tagging.title"), h.printableCommandLine());
+      VcsNotifier.getInstance(myProject).notifySuccess(myTagNameTextField.getText(),
+                                                       "Created tag " + myTagNameTextField.getText() + " successfully.");
+      GitRepository repository = GitUtil.getRepositoryManager(myProject).getRepositoryForRoot(getGitRoot());
+      if (repository != null) {
+        repository.getRepositoryFiles().refresh(true);
       }
-      finally {
-        exceptions.addAll(h.errors());
+      else {
+        LOG.error("No repository registered for root: " + getGitRoot());
+      }
+      if(!h.errors().isEmpty()) {
+        GitUIUtil.notifyImportantError(myProject, "Error rebasing", GitUIUtil.stringifyErrors(h.errors()));
       }
     }
     finally {

plugins/git4idea/src/git4idea/actions/GitFetch.java
@@ -18,7 +18,6 @@ package git4idea.actions;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vfs.VirtualFile;
 import git4idea.GitUtil;
 import git4idea.GitVcs;
@@ -38,8 +37,7 @@ public class GitFetch extends GitRepositoryAction {
 
   protected void perform(@NotNull final Project project,
                          @NotNull final List<VirtualFile> gitRoots,
-                         @NotNull final VirtualFile defaultRoot,
-                         final List<VcsException> exceptions) throws VcsException {
+                         @NotNull final VirtualFile defaultRoot) {
     GitVcs.runInBackground(new Task.Backgroundable(project, "Fetching...", true) {
       @Override
       public void run(@NotNull ProgressIndicator indicator) {

plugins/git4idea/src/git4idea/actions/GitMergeAction.java
@@ -64,8 +64,7 @@ abstract class GitMergeAction extends GitRepositoryAction {
 
   protected void perform(@NotNull final Project project,
                          @NotNull final List<VirtualFile> gitRoots,
-                         @NotNull final VirtualFile defaultRoot,
-                         final List<VcsException> exceptions) throws VcsException {
+                         @NotNull final VirtualFile defaultRoot) {
     final DialogState dialogState = displayDialog(project, gitRoots, defaultRoot);
     if (dialogState == null) {
       return;

plugins/git4idea/src/git4idea/actions/GitRepositoryAction.java
@@ -34,7 +34,6 @@ import org.jetbrains.annotations.CalledInAwt;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static com.intellij.util.ObjectUtils.notNull;
@@ -54,13 +53,7 @@ public abstract class GitRepositoryAction extends DumbAwareAction {
 
     final VirtualFile defaultRoot = getDefaultRoot(project, roots, e.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY));
 
-    List<VcsException> exceptions = new ArrayList<>();
-    try {
-      perform(project, roots, defaultRoot, exceptions);
-    }
-    catch (VcsException ex) {
-      exceptions.add(ex);
-    }
+    perform(project, roots, defaultRoot);
   }
 
   @NotNull
@@ -128,13 +121,11 @@ public abstract class GitRepositoryAction extends DumbAwareAction {
    * @param project       a context project
    * @param gitRoots      a git roots that affect the current project (sorted by {@link VirtualFile#getPresentableUrl()})
    * @param defaultRoot   a guessed default root (based on the currently selected file list)
-   * @param exceptions    a list of exceptions from running git
    * @throws VcsException if there is a problem with running git (this exception is considered to be added to the end of the exception list)
    */
   protected abstract void perform(@NotNull Project project,
                                   @NotNull List<VirtualFile> gitRoots,
-                                  @NotNull VirtualFile defaultRoot,
-                                  List<VcsException> exceptions) throws VcsException;
+                                  @NotNull VirtualFile defaultRoot);
 
   @Override
   public void update(final AnActionEvent e) {

plugins/git4idea/src/git4idea/actions/GitResetHead.java
@@ -18,7 +18,6 @@ package git4idea.actions;
 import com.intellij.dvcs.DvcsUtil;
 import com.intellij.openapi.application.AccessToken;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import git4idea.GitUtil;
@@ -48,8 +47,7 @@ public class GitResetHead extends GitRepositoryAction {
    */
   protected void perform(@NotNull Project project,
                          @NotNull List<VirtualFile> gitRoots,
-                         @NotNull VirtualFile defaultRoot,
-                         List<VcsException> exceptions) throws VcsException {
+                         @NotNull VirtualFile defaultRoot) {
     GitResetDialog d = new GitResetDialog(project, gitRoots, defaultRoot);
     if (!d.showAndGet()) {
       return;
@@ -65,6 +63,8 @@ public class GitResetHead extends GitRepositoryAction {
     GitRepositoryManager manager = GitUtil.getRepositoryManager(project);
     manager.updateRepository(d.getGitRoot());
     VfsUtil.markDirtyAndRefresh(true, true, false, d.getGitRoot());
-    showErrors(project, getActionName(), exceptions);
+    if(!h.errors().isEmpty()) {
+      showErrors(project, getActionName(), h.errors());
+    }
   }
 }

plugins/git4idea/src/git4idea/actions/GitStash.java
@@ -18,7 +18,6 @@ package git4idea.actions;
 import com.intellij.dvcs.DvcsUtil;
 import com.intellij.openapi.application.AccessToken;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vcs.changes.ChangeListManager;
 import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -40,8 +39,7 @@ public class GitStash extends GitRepositoryAction {
    */
   protected void perform(@NotNull final Project project,
                          @NotNull final List<VirtualFile> gitRoots,
-                         @NotNull final VirtualFile defaultRoot,
-                         final List<VcsException> exceptions) throws VcsException {
+                         @NotNull final VirtualFile defaultRoot) {
     final ChangeListManager changeListManager = ChangeListManager.getInstance(project);
     if (changeListManager.isFreezedWithNotification("Can not stash changes now")) return;
     GitStashDialog d = new GitStashDialog(project, gitRoots, defaultRoot);
@@ -58,7 +56,9 @@ public class GitStash extends GitRepositoryAction {
       DvcsUtil.workingTreeChangeFinished(project, token);
     }
     VfsUtil.markDirtyAndRefresh(false, true, false, root);
-    showErrors(project, getActionName(), exceptions);
+    if(!h.errors().isEmpty()) {
+      showErrors(project, getActionName(), h.errors());
+    }
   }
 
   /**

plugins/git4idea/src/git4idea/actions/GitTag.java
@@ -15,8 +15,9 @@
  */
 package git4idea.actions;
 
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vfs.VirtualFile;
 import git4idea.i18n.GitBundle;
 import git4idea.ui.GitTagDialog;
@@ -42,13 +43,15 @@ public class GitTag extends GitRepositoryAction {
    */
   protected void perform(@NotNull final Project project,
                          @NotNull final List<VirtualFile> gitRoots,
-                         @NotNull final VirtualFile defaultRoot,
-                         final List<VcsException> exceptions) throws VcsException {
+                         @NotNull final VirtualFile defaultRoot) {
     GitTagDialog d = new GitTagDialog(project, gitRoots, defaultRoot);
-    if (!d.showAndGet()) {
-      return;
+    if (d.showAndGet()) {
+      new Task.Modal(project, "Tagging...", true) {
+        @Override
+        public void run(@NotNull ProgressIndicator indicator) {
+          d.runAction();
+        }
+      }.queue();
     }
-    d.runAction(exceptions);
-    showErrors(project, getActionName(), exceptions);
   }
 }

plugins/git4idea/src/git4idea/actions/GitUnstash.java
@@ -16,7 +16,6 @@
 package git4idea.actions;
 
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vcs.changes.ChangeListManager;
 import com.intellij.openapi.vfs.VirtualFile;
 import git4idea.i18n.GitBundle;
@@ -43,8 +42,7 @@ public class GitUnstash extends GitRepositoryAction {
    */
   protected void perform(@NotNull final Project project,
                          @NotNull final List<VirtualFile> gitRoots,
-                         @NotNull final VirtualFile defaultRoot,
-                         final List<VcsException> exceptions) throws VcsException {
+                         @NotNull final VirtualFile defaultRoot) {
     final ChangeListManager changeListManager = ChangeListManager.getInstance(project);
     if (changeListManager.isFreezedWithNotification("Can not unstash changes now")) return;
     GitUnstashDialog.showUnstashDialog(project, gitRoots, defaultRoot);

commit 365501a32003ba73433904b66d66471e8abdaf0c

Fix tag creation  IDEA-164369
It was broken in bedf7d3c when runAction was wrapped into a bg task
and thus moved to a pooled thread, but doSynchronously was kept intact
and was still starting a modal task inside.

To fix it, don't start the modal task, and use new procedure of
executing handlers and collecting results.

plugins/git4idea/src/git4idea/i18n/GitBundle.properties
@@ -387,7 +387,6 @@ tag.nonvcs.error.title=Tag Result
 tag.title=Tag
 tag.validate.tooltip=Click this button to the validate commit to be tagged.
 tag.validate=&Validate
-tagging.title=Tagging files...
 task.queue.title=Git Operations
 unindexed.files.changlelist.name=Unindexed Files
 unstash.action.name=UnStash

plugins/git4idea/src/git4idea/ui/GitTagDialog.java
@@ -15,6 +15,7 @@
  */
 package git4idea.ui;
 
+import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
@@ -25,14 +26,13 @@ import com.intellij.openapi.vfs.CharsetToolkit;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.DocumentAdapter;
 import git4idea.GitUtil;
-import git4idea.commands.GitCommand;
-import git4idea.commands.GitHandlerUtil;
-import git4idea.commands.GitSimpleHandler;
+import git4idea.commands.*;
 import git4idea.i18n.GitBundle;
 import git4idea.repo.GitRepository;
 import git4idea.util.GitUIUtil;
 import git4idea.util.StringScanner;
 import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
@@ -90,6 +90,9 @@ public class GitTagDialog extends DialogWrapper {
    * The current project
    */
   private final Project myProject;
+  @NotNull private final Git myGit;
+  @NotNull private final VcsNotifier myNotifier;
+
   /**
    * Existing tags for the project
    */
@@ -119,6 +122,9 @@ public class GitTagDialog extends DialogWrapper {
     setTitle(GitBundle.getString("tag.title"));
     setOKButtonText(GitBundle.getString("tag.button"));
     myProject = project;
+    myNotifier = VcsNotifier.getInstance(myProject);
+    myGit = ServiceManager.getService(Git.class);
+
     GitUIUtil.setupRootChooser(myProject, roots, defaultRoot, myGitRootComboBox, myCurrentBranch);
     myGitRootComboBox.addActionListener(new ActionListener() {
       public void actionPerformed(final ActionEvent e) {
@@ -182,7 +188,7 @@ public class GitTagDialog extends DialogWrapper {
       messageFile = null;
     }
     try {
-      GitSimpleHandler h = new GitSimpleHandler(myProject, getGitRoot(), GitCommand.TAG);
+      GitLineHandler h = new GitLineHandler(myProject, getGitRoot(), GitCommand.TAG);
       if (hasMessage) {
         h.addParameters("-a");
       }
@@ -197,18 +203,22 @@ public class GitTagDialog extends DialogWrapper {
       if (object.length() != 0) {
         h.addParameters(object);
       }
-      GitHandlerUtil.doSynchronously(h, GitBundle.getString("tagging.title"), h.printableCommandLine());
-      VcsNotifier.getInstance(myProject).notifySuccess(myTagNameTextField.getText(),
-                                                       "Created tag " + myTagNameTextField.getText() + " successfully.");
+
+      GitCommandResult result = myGit.runCommand(h);
+      if (result.success()) {
+        myNotifier.notifySuccess(myTagNameTextField.getText(),
+                                                         "Created tag " + myTagNameTextField.getText() + " successfully.");
+      }
+      else {
+        myNotifier.notifyError("Couldn't Create Tag", result.getErrorOutputAsHtmlString());
+      }
+
       GitRepository repository = GitUtil.getRepositoryManager(myProject).getRepositoryForRoot(getGitRoot());
       if (repository != null) {
         repository.getRepositoryFiles().refresh(true);
       }
       else {
         LOG.error("No repository registered for root: " + getGitRoot());
-      }
-      if(!h.errors().isEmpty()) {
-        GitUIUtil.notifyImportantError(myProject, "Error rebasing", GitUIUtil.stringifyErrors(h.errors()));
       }
     }
     finally {