b1bc86f649d89d7be27ca32367d7269fe5a6eb51
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2016-11-24 13:00:21 +0300

    WEB-24182 Debugger: no console output when using Chromium v. < 54 for debugging

--- platform/script-debugger/protocol/protocol-model-generator/src/Generator.kt
+++ platform/script-debugger/protocol/protocol-model-generator/src/Generator.kt
@@ -232,7 +232,8 @@ val READER_INTERFACE_NAME = "ProtocolResponseReader"
 
   // todo DOMDebugger
 private val INCLUDED_DOMAINS = arrayOf("CSS", "Debugger", "DOM", "Inspector", "Log", "Network", "Page", "Runtime", "ServiceWorker",
-                                       "Tracing", "Worker")
+                                       "Tracing", "Worker",
+                                       "Console")
 
 fun generateMethodNameSubstitute(originalName: String, out: TextOutput): String {
   if (originalName != "this") {
