aa8982ea47a5752a78dfa99c09c5bac91eac2d4a
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2016-11-24 15:14:08 +0300

    [grails] extract springloaded check into separate method

--- plugins/groovy/src/org/jetbrains/plugins/groovy/springloaded/SpringLoadedPositionManagerFactory.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/springloaded/SpringLoadedPositionManagerFactory.java
@@ -18,6 +18,7 @@ package org.jetbrains.plugins.groovy.springloaded;
 import com.intellij.debugger.PositionManager;
 import com.intellij.debugger.PositionManagerFactory;
 import com.intellij.debugger.engine.DebugProcess;
+import com.intellij.debugger.engine.jdi.VirtualMachineProxy;
 import com.intellij.openapi.application.AccessToken;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.psi.JavaPsiFacade;
@@ -25,34 +26,28 @@ import org.jetbrains.annotations.NotNull;
 
 /**
  * Factory for position manager to debug classes reloaded by com.springsource.springloaded
+ *
  * @author Sergey Evdokimov
  */
 public class SpringLoadedPositionManagerFactory extends PositionManagerFactory {
 
   @Override
   public PositionManager createPositionManager(@NotNull final DebugProcess process) {
-    AccessToken accessToken = ApplicationManager.getApplication().acquireReadActionLock();
-    try {
+    return usesSpringLoaded(process) ? new SpringLoadedPositionManager(process) : null;
+  }
+
+  private static boolean usesSpringLoaded(@NotNull final DebugProcess process) {
+    try (AccessToken ignored = ApplicationManager.getApplication().acquireReadActionLock()) {
       JavaPsiFacade facade = JavaPsiFacade.getInstance(process.getProject());
-      if (facade.findPackage("com.springsource.loaded") != null || facade.findPackage("org.springsource.loaded") != null) {
-        return new SpringLoadedPositionManager(process);
+      if (facade.findPackage("com.springsource.loaded") != null ||
+          facade.findPackage("org.springsource.loaded") != null) {
+        return true;
       }
     }
-    finally {
-      accessToken.finish();
-    }
 
-    try {
-      // Check spring loaded for remote process
-      if (!process.getVirtualMachineProxy().classesByName("com.springsource.loaded.agent.SpringLoadedAgent").isEmpty()
-          || !process.getVirtualMachineProxy().classesByName("org.springsource.loaded.agent.SpringLoadedAgent").isEmpty()) {
-        return new SpringLoadedPositionManager(process);
-      }
-    }
-    catch (Exception ignored) {
-      // Some problem with virtual machine.
-    }
-
-    return null;
+    // Check spring loaded for remote process
+    VirtualMachineProxy proxy = process.getVirtualMachineProxy();
+    return !proxy.classesByName("com.springsource.loaded.agent.SpringLoadedAgent").isEmpty() ||
+           !proxy.classesByName("org.springsource.loaded.agent.SpringLoadedAgent").isEmpty();
   }
 }
