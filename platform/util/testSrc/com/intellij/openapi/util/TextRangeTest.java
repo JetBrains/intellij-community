acb13c14587082674b9fd22801da31888c9f2e2e
Anna.Kozlova <anna.kozlova@jetbrains.com>
2016-11-25 19:06:48 +0100

    convert to junit 4 to use expected exceptions (problem found by Error Prone)

--- platform/util/testSrc/com/intellij/openapi/util/TextRangeTest.java
+++ platform/util/testSrc/com/intellij/openapi/util/TextRangeTest.java
@@ -17,71 +17,71 @@
 package com.intellij.openapi.util;
 
 import com.intellij.util.text.TextRangeUtil;
-import junit.framework.TestCase;
+import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
+import static org.junit.Assert.*;
+
 /**
  * @author dyoma
  */
-public class TextRangeTest extends TestCase {
-  public void testSubstring() {
+public class TextRangeTest {
+  @Test
+  public void substring() {
     assertEquals("abc", new TextRange(0, 3).substring("abc"));
     assertEquals("abc", new TextRange(0, 3).substring("abcd"));
     assertEquals("bc", new TextRange(1, 3).substring("abcd"));
     assertEquals("", new TextRange(2, 2).substring("abcd"));
   }
 
-  public void testCutOut() {
+  @Test(expected = AssertionError.class)
+  public void cutOut() {
     assertEquals(new TextRange(1, 5), new TextRange(1, 5).cutOut(new TextRange(0, 4)));
     assertEquals(new TextRange(2, 5), new TextRange(1, 5).cutOut(new TextRange(1, 4)));
     assertEquals(new TextRange(1, 4), new TextRange(1, 5).cutOut(new TextRange(0, 3)));
     assertEquals(new TextRange(3, 3), new TextRange(1, 5).cutOut(new TextRange(2, 2)));
-    boolean assertionErrorThrown = false;
-    try {
-      new TextRange(1, 5).cutOut(new TextRange(1, 10));
-    }
-    catch (AssertionError e) {
-      assertionErrorThrown = true;
-    }
-    assertTrue("AssertionError must be thrown", assertionErrorThrown);
+    new TextRange(1, 5).cutOut(new TextRange(1, 10));
   }
 
-  public void testShiftRight() {
+  @Test
+  public void shiftRight() {
     TextRange range = new TextRange(1, 2);
     assertEquals(new TextRange(2, 3), range.shiftRight(1));
     assertEquals(new TextRange(0, 1), range.shiftRight(-1));
     assertSame(range, range.shiftRight(0));
   }
 
-  public void testReplace() {
+  @Test
+  public void replace() {
     TextRange range = new TextRange(1, 3);
     assertEquals("0a345", range.replace("012345", "a"));
     assertEquals("0345", range.replace("012345", ""));
     assertEquals("0abcdef345", range.replace("012345", "abcdef"));
   }
-  
-  public void testExcludedRanges() {
-    List<TextRange> excludedRanges = 
+
+  @Test
+  public void excludedRanges() {
+    List<TextRange> excludedRanges =
       Arrays.asList(
-        new TextRange(95,110),
-        new TextRange(15,40),
-        new TextRange(5,20),
-        new TextRange(105,120),
-        new TextRange(70,90),
-        new TextRange(50,57),
-        new TextRange(56,65),
-        new TextRange(50,60)
+        new TextRange(95, 110),
+        new TextRange(15, 40),
+        new TextRange(5, 20),
+        new TextRange(105, 120),
+        new TextRange(70, 90),
+        new TextRange(50, 57),
+        new TextRange(56, 65),
+        new TextRange(50, 60)
       );
-    List<TextRange> expectedRanges = 
+    List<TextRange> expectedRanges =
       Arrays.asList(
-        new TextRange(40,50),
-        new TextRange(65,70),
-        new TextRange(90,95)
+        new TextRange(40, 50),
+        new TextRange(65, 70),
+        new TextRange(90, 95)
       );
-    Iterable<TextRange> result = TextRangeUtil.excludeRanges(new TextRange(30,100), excludedRanges);
+    Iterable<TextRange> result = TextRangeUtil.excludeRanges(new TextRange(30, 100), excludedRanges);
     Iterator<TextRange> resultIterator = result.iterator();
     for (TextRange expectedRange : expectedRanges) {
       assertTrue("Less elements than expected", resultIterator.hasNext());
