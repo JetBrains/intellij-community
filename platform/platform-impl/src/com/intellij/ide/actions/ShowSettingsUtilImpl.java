55c839cb96e328d37f69e81b74e8e9181e0a7891
Dana Dahlstrom <dahlstrom@google.com>
2016-11-25 19:22:05 +0100

    Fix mismatching varargs/array params in overrides

--- platform/platform-impl/src/com/intellij/ide/actions/ShowSettingsUtilImpl.java
+++ platform/platform-impl/src/com/intellij/ide/actions/ShowSettingsUtilImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +21,8 @@ import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.options.ConfigurableGroup;
 import com.intellij.openapi.options.ShowSettingsUtil;
 import com.intellij.openapi.options.TabbedConfigurable;
-import com.intellij.openapi.options.ex.*;
+import com.intellij.openapi.options.ex.ConfigurableExtensionPointUtil;
+import com.intellij.openapi.options.ex.ConfigurableVisitor;
 import com.intellij.openapi.options.newEditor.SettingsDialog;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
@@ -86,7 +87,7 @@ public class ShowSettingsUtilImpl extends ShowSettingsUtil {
   }
 
   @Override
-  public void showSettingsDialog(@NotNull Project project, @NotNull ConfigurableGroup[] group) {
+  public void showSettingsDialog(@NotNull Project project, @NotNull ConfigurableGroup... group) {
     try {
       getDialog(project, group, null).show();
     }
