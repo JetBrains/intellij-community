abe7643b323d7c3bccd9cb39db9e3dc0e8e952df
peter <peter@jetbrains.com>
2016-11-27 12:32:51 +0100

    revert JavaNullMethodArgumentIndex and JavaFunctionalExpressionIndex
    repeated findElementAt calls don't reuse previously parsed chameleons

--- java/java-indexing-impl/src/com/intellij/psi/impl/search/JavaNullMethodArgumentIndex.java
+++ java/java-indexing-impl/src/com/intellij/psi/impl/search/JavaNullMethodArgumentIndex.java
@@ -20,21 +20,22 @@ import com.intellij.lang.LighterAST;
 import com.intellij.lang.LighterASTNode;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.JavaTokenType;
 import com.intellij.psi.PsiKeyword;
+import com.intellij.psi.impl.cache.RecordUtil;
 import com.intellij.psi.impl.java.stubs.JavaStubElementTypes;
-import com.intellij.psi.impl.source.JavaLightTreeUtil;
 import com.intellij.psi.impl.source.tree.ElementType;
+import com.intellij.psi.impl.source.tree.JavaElementType;
 import com.intellij.psi.impl.source.tree.LightTreeUtil;
+import com.intellij.psi.impl.source.tree.RecursiveLighterASTNodeWalkingVisitor;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.tree.TokenSet;
-import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.containers.IntArrayList;
 import com.intellij.util.indexing.*;
 import com.intellij.util.io.DataInputOutputUtil;
 import com.intellij.util.io.EnumeratorStringDescriptor;
 import com.intellij.util.io.KeyDescriptor;
-import com.intellij.util.text.StringSearcher;
 import gnu.trove.THashMap;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -42,15 +43,14 @@ import org.jetbrains.annotations.Nullable;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
-import java.util.*;
-
-import static com.intellij.psi.impl.source.tree.JavaElementType.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 public class JavaNullMethodArgumentIndex extends ScalarIndexExtension<JavaNullMethodArgumentIndex.MethodCallData> implements PsiDependentIndex {
   private static final Logger LOG = Logger.getInstance(JavaNullMethodArgumentIndex.class);
 
   public static final ID<MethodCallData, Void> INDEX_ID = ID.create("java.null.method.argument");
-  private static final TokenSet CALL_TYPES = TokenSet.create(METHOD_CALL_EXPRESSION, NEW_EXPRESSION, ANONYMOUS_CLASS);
   private boolean myOfflineMode = ApplicationManager.getApplication().isCommandLine() &&
                                   !ApplicationManager.getApplication().isUnitTestMode();
 
@@ -64,86 +64,89 @@ public class JavaNullMethodArgumentIndex extends ScalarIndexExtension<JavaNullMe
   @Override
   public DataIndexer<MethodCallData, Void, FileContent> getIndexer() {
     return inputData -> {
+      final CharSequence contentAsText = inputData.getContentAsText();
       if (!JavaStubElementTypes.JAVA_FILE.shouldBuildStubFor(inputData.getFile())) {
         return Collections.emptyMap();
       }
       if (myOfflineMode) {
         return Collections.emptyMap();
       }
-
-      int[] nullOffsets = new StringSearcher(PsiKeyword.NULL, true, true).findAllOccurrences(inputData.getContentAsText());
-      if (nullOffsets.length == 0) return Collections.emptyMap();
-
-      LighterAST lighterAst = ((FileContentImpl)inputData).getLighterASTForPsiDependentIndex();
-      Set<LighterASTNode> calls = findCallsWithNulls(lighterAst, nullOffsets);
-      if (calls.isEmpty()) return Collections.emptyMap();
+      if (!StringUtil.contains(contentAsText, PsiKeyword.NULL)) {
+        return Collections.emptyMap();
+      }
 
       Map<MethodCallData, Void> result = new THashMap<>();
-      for (LighterASTNode element : calls) {
-        final IntArrayList indices = getNullParameterIndices(lighterAst, element);
-        if (indices != null) {
-          final String name = getMethodName(lighterAst, element, element.getTokenType());
-          if (name != null) {
-            for (int i = 0; i < indices.size(); i++) {
-              result.put(new MethodCallData(name, indices.get(i)), null);
+      final LighterAST lighterAst = ((FileContentImpl)inputData).getLighterASTForPsiDependentIndex();
+
+      new RecursiveLighterASTNodeWalkingVisitor(lighterAst) {
+        @Override
+        public void visitNode(@NotNull LighterASTNode element) {
+          if (element.getTokenType() == JavaElementType.METHOD_CALL_EXPRESSION ||
+              element.getTokenType() == JavaElementType.NEW_EXPRESSION ||
+              element.getTokenType() == JavaElementType.ANONYMOUS_CLASS) {
+            final IntArrayList indices = getNullParameterIndices(element);
+            if (indices != null) {
+              final String name = getMethodName(element, element.getTokenType());
+              if (name != null) {
+                for (int i = 0; i < indices.size(); i++) {
+                  final int nullParameterIndex = indices.get(i);
+                  result.put(new MethodCallData(name, nullParameterIndex), null);
+                }
+              }
             }
           }
+          super.visitNode(element);
         }
-      }
+
+        @Nullable
+        private IntArrayList getNullParameterIndices(@NotNull LighterASTNode methodCall) {
+          final LighterASTNode node = LightTreeUtil.firstChildOfType(lighterAst, methodCall, JavaElementType.EXPRESSION_LIST);
+          if (node == null) return null;
+          final List<LighterASTNode> parameters = LightTreeUtil.getChildrenOfType(lighterAst, node, ElementType.EXPRESSION_BIT_SET);
+          IntArrayList indices = null;
+          for (int idx = 0; idx < parameters.size(); idx++) {
+            LighterASTNode parameter = parameters.get(idx);
+            if (parameter.getTokenType() == JavaElementType.LITERAL_EXPRESSION) {
+              final LighterASTNode literalTextNode = LightTreeUtil.firstChildOfType(lighterAst, parameter, TokenSet.ANY);
+              if (literalTextNode != null && StringUtil.equals(RecordUtil.intern(lighterAst.getCharTable(), literalTextNode), PsiKeyword.NULL)) {
+                if (indices == null) {
+                  indices = new IntArrayList(1);
+                }
+                indices.add(idx);
+              }
+            }
+          }
+          return indices;
+        }
+
+        @Nullable
+        private String getMethodName(@NotNull LighterASTNode call, IElementType elementType) {
+          if (elementType == JavaElementType.NEW_EXPRESSION || elementType == JavaElementType.ANONYMOUS_CLASS) {
+            final List<LighterASTNode> refs = LightTreeUtil.getChildrenOfType(lighterAst, call, JavaElementType.JAVA_CODE_REFERENCE);
+            if (refs.isEmpty()) return null;
+            final LighterASTNode lastRef = refs.get(refs.size() - 1);
+            return getLastIdentifierText(lastRef);
+          } else {
+            LOG.assertTrue(elementType == JavaElementType.METHOD_CALL_EXPRESSION);
+            final LighterASTNode methodReference = lighterAst.getChildren(call).get(0);
+            if (methodReference.getTokenType() == JavaElementType.REFERENCE_EXPRESSION) {
+              return getLastIdentifierText(methodReference);
+            }
+          }
+          return null;
+        }
+
+        @Nullable
+        private String getLastIdentifierText(LighterASTNode lastRef) {
+          final List<LighterASTNode> identifiers = LightTreeUtil.getChildrenOfType(lighterAst, lastRef, JavaTokenType.IDENTIFIER);
+          if (identifiers.isEmpty()) return null;
+          final LighterASTNode methodNameIdentifier = identifiers.get(identifiers.size() - 1);
+          return RecordUtil.intern(lighterAst.getCharTable(), methodNameIdentifier);
+        }
+      }.visitNode(lighterAst.getRoot());
+
       return result;
     };
-  }
-
-  @NotNull
-  private static Set<LighterASTNode> findCallsWithNulls(LighterAST lighterAst, int[] nullOffsets) {
-    Set<LighterASTNode> calls = new HashSet<>();
-    for (int offset : nullOffsets) {
-      LighterASTNode leaf = LightTreeUtil.findLeafElementAt(lighterAst, offset);
-      LighterASTNode literal = leaf == null ? null : lighterAst.getParent(leaf);
-      if (isNullLiteral(lighterAst, literal)) {
-        LighterASTNode exprList = lighterAst.getParent(literal);
-        if (exprList != null && exprList.getTokenType() == EXPRESSION_LIST) {
-          ContainerUtil.addIfNotNull(calls, LightTreeUtil.getParentOfType(lighterAst, exprList, CALL_TYPES, ElementType.MEMBER_BIT_SET));
-        }
-      }
-    }
-    return calls;
-  }
-
-  @Nullable
-  private static IntArrayList getNullParameterIndices(LighterAST lighterAst, @NotNull LighterASTNode methodCall) {
-    final LighterASTNode node = LightTreeUtil.firstChildOfType(lighterAst, methodCall, EXPRESSION_LIST);
-    if (node == null) return null;
-    final List<LighterASTNode> parameters = JavaLightTreeUtil.getExpressionChildren(lighterAst, node);
-    IntArrayList indices = new IntArrayList(1);
-    for (int idx = 0; idx < parameters.size(); idx++) {
-      if (isNullLiteral(lighterAst, parameters.get(idx))) {
-        indices.add(idx);
-      }
-    }
-    return indices;
-  }
-
-  private static boolean isNullLiteral(LighterAST lighterAst, @Nullable LighterASTNode expr) {
-    return expr != null && expr.getTokenType() == LITERAL_EXPRESSION &&
-           lighterAst.getChildren(expr).get(0).getTokenType() == JavaTokenType.NULL_KEYWORD;
-  }
-
-  @Nullable
-  private static String getMethodName(LighterAST lighterAst, @NotNull LighterASTNode call, IElementType elementType) {
-    if (elementType == NEW_EXPRESSION || elementType == ANONYMOUS_CLASS) {
-      final List<LighterASTNode> refs = LightTreeUtil.getChildrenOfType(lighterAst, call, JAVA_CODE_REFERENCE);
-      if (refs.isEmpty()) return null;
-      final LighterASTNode lastRef = refs.get(refs.size() - 1);
-      return JavaLightTreeUtil.getNameIdentifierText(lighterAst, lastRef);
-    }
-
-    LOG.assertTrue(elementType == METHOD_CALL_EXPRESSION);
-    final LighterASTNode methodReference = lighterAst.getChildren(call).get(0);
-    if (methodReference.getTokenType() == REFERENCE_EXPRESSION) {
-      return JavaLightTreeUtil.getNameIdentifierText(lighterAst, methodReference);
-    }
-    return null;
   }
 
   @NotNull
