ded172b89a2f3363834ecc5f3306dcf0eb7e173e
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2016-11-28 13:41:57 +0300

    git: use actual local changes for diff after cherry-pick
    Working with Changes taken from the cherry-picked commit leads to
    incorrect diff: it is shown for the "old" changes, which means that:
    - content at the right is read-only
    - if there were local changes in the file, the old diff is still shown
      instead of the combined change.
    
    Therefore after cherry-picking we should update the CLM and find local
    changes which correspond to the changes from the original commit.
    
    The method for this already exists in GitUncommitAction
    => move it and reuse.
    
    IDEA-163941
    
    (cherry picked from commit b370b6f6a1c9b038c1c8640b63cfedac8aa18dc3)

--- plugins/git4idea/src/git4idea/GitUtil.java
+++ plugins/git4idea/src/git4idea/GitUtil.java
@@ -45,6 +45,7 @@ import com.intellij.util.Consumer;
 import com.intellij.util.Function;
 import com.intellij.util.ObjectUtils;
 import com.intellij.util.containers.ContainerUtil;
+import com.intellij.util.containers.OpenTHashSet;
 import com.intellij.util.ui.UIUtil;
 import com.intellij.vcsUtil.VcsFileUtil;
 import com.intellij.vcsUtil.VcsUtil;
@@ -1082,5 +1083,20 @@ public class GitUtil {
   @Nullable
   public static String getRelativePath(@NotNull String root, @NotNull ContentRevision after) {
     return FileUtil.getRelativePath(root, after.getFile().getPath(), File.separatorChar);
+  }
+
+  /**
+   * <p>Finds the local changes which are "the same" as the given changes.</p>
+   * <p>The purpose of this method is to get actual local changes after some other changes were applied to the working tree
+   * (e.g. if they were cherry-picked from a commit). Working with the original non-local changes is limited, in particular,
+   * the difference between content revisions may be not the same as the local change.</p>
+   * <p>"The same" here means the changes made in the same files. It is possible that there was a change made in file A in the original
+   * commit, but there are no local changes made in file A. Such situations are ignored.</p>
+   */
+  @NotNull
+  public static Collection<Change> findCorrespondentLocalChanges(@NotNull ChangeListManager changeListManager,
+                                                                 @NotNull Collection<Change> originalChanges) {
+    OpenTHashSet<Change> allChanges = new OpenTHashSet<>(changeListManager.getAllChanges());
+    return ContainerUtil.mapNotNull(originalChanges, allChanges::get);
   }
 }
