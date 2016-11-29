44a762a37836c66155384dbe4131f1075c876580
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2016-11-29 20:47:19 +0300

    gradle: resolve properties of "rootProject"

--- plugins/gradle/src/org/jetbrains/plugins/gradle/service/resolve/GradleNonCodeMembersContributor.kt
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/service/resolve/GradleNonCodeMembersContributor.kt
@@ -15,7 +15,9 @@
  */
 package org.jetbrains.plugins.gradle.service.resolve
 
+import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
 import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.util.Ref
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
@@ -25,7 +27,9 @@ import com.intellij.psi.scope.PsiScopeProcessor
 import org.jetbrains.plugins.gradle.service.resolve.GradleCommonClassNames.GRADLE_API_PROJECT
 import org.jetbrains.plugins.gradle.service.resolve.GradleExtensionsContributor.Companion.getDocumentation
 import org.jetbrains.plugins.gradle.settings.GradleExtensionsSettings
+import org.jetbrains.plugins.gradle.settings.GradleExtensionsSettings.GradleExtensionsData
 import org.jetbrains.plugins.groovy.dsl.holders.NonCodeMembersHolder
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrReferenceExpression
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.path.GrMethodCallExpression
 import org.jetbrains.plugins.groovy.lang.psi.impl.synthetic.GrLightVariable
 import org.jetbrains.plugins.groovy.lang.resolve.NonCodeMembersContributor
@@ -48,31 +52,50 @@ class GradleNonCodeMembersContributor : NonCodeMembersContributor() {
     processDeclarations(aClass, processor, state, place)
 
     if (qualifierType.equalsToText(GRADLE_API_PROJECT)) {
+      val propCandidate = place.references.singleOrNull()?.canonicalText ?: return
+      val extensionsData: GradleExtensionsData?
       val methodCall = place.children.singleOrNull()
       if (methodCall is GrMethodCallExpression) {
         val projectPath = methodCall.argumentList.expressionArguments.singleOrNull()?.reference?.canonicalText ?: return
-        val propCandidate = place.references.singleOrNull()?.canonicalText ?: return
-        val module = ModuleManager.getInstance(place.project).findModuleByName(projectPath.trimStart(':')) ?: return
-        val extensionsData = GradleExtensionsSettings.getInstance(place.project).getExtensionsFor(module) ?: return
-
-        val processVariable: (GradleExtensionsSettings.GradleProp) -> Boolean = {
-          val docRef = Ref.create<String>()
-          val variable = object : GrLightVariable(place.manager, propCandidate, it.typeFqn, place) {
-            override fun getNavigationElement(): PsiElement {
-              val navigationElement = super.getNavigationElement()
-              navigationElement.putUserData(NonCodeMembersHolder.DOCUMENTATION, docRef.get())
-              return navigationElement
-            }
-          }
-          val doc = getDocumentation(it, variable)
-          docRef.set(doc)
-          place.putUserData(NonCodeMembersHolder.DOCUMENTATION, doc)
-          processor.execute(variable, state)
+        if (projectPath == ":") {
+          val file = containingFile?.originalFile?.virtualFile ?: return
+          val module = ProjectFileIndex.SERVICE.getInstance(place.project).getModuleForFile(file)
+          val rootProjectPath = ExternalSystemApiUtil.getExternalRootProjectPath(module)
+          extensionsData = GradleExtensionsSettings.getInstance(place.project).getExtensionsFor(rootProjectPath, rootProjectPath) ?: return
         }
-
-        extensionsData.tasks.firstOrNull { it.name == propCandidate }?.let(processVariable)
-        extensionsData.findProperty(propCandidate)?.let(processVariable)
+        else {
+          val module = ModuleManager.getInstance(place.project).findModuleByName(projectPath.trimStart(':')) ?: return
+          extensionsData = GradleExtensionsSettings.getInstance(place.project).getExtensionsFor(module) ?: return
+        }
       }
+      else if (methodCall is GrReferenceExpression) {
+        if (place.children[0].text == "rootProject") {
+          val file = containingFile?.originalFile?.virtualFile ?: return
+          val module = ProjectFileIndex.SERVICE.getInstance(place.project).getModuleForFile(file)
+          val rootProjectPath = ExternalSystemApiUtil.getExternalRootProjectPath(module)
+          extensionsData = GradleExtensionsSettings.getInstance(place.project).getExtensionsFor(rootProjectPath, rootProjectPath) ?: return
+        }
+        else return
+      }
+      else return
+
+      val processVariable: (GradleExtensionsSettings.GradleProp) -> Boolean = {
+        val docRef = Ref.create<String>()
+        val variable = object : GrLightVariable(place.manager, propCandidate, it.typeFqn, place) {
+          override fun getNavigationElement(): PsiElement {
+            val navigationElement = super.getNavigationElement()
+            navigationElement.putUserData(NonCodeMembersHolder.DOCUMENTATION, docRef.get())
+            return navigationElement
+          }
+        }
+        val doc = getDocumentation(it, variable)
+        docRef.set(doc)
+        place.putUserData(NonCodeMembersHolder.DOCUMENTATION, doc)
+        processor.execute(variable, state)
+      }
+
+      extensionsData.tasks.firstOrNull { it.name == propCandidate }?.let(processVariable)
+      extensionsData.findProperty(propCandidate)?.let(processVariable)
     }
   }
 }
