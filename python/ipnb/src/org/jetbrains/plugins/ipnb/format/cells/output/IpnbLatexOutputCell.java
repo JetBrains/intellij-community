ad5994564a78d1b05b1acc04cf379e4cf3f034e3
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2016-11-29 17:43:18 +0300

    PY-21447 Jupyter Notebook fails to properly handle IPython.display Markdown
    (cherry picked from commit d9806e5)

--- python/ipnb/src/org/jetbrains/plugins/ipnb/format/cells/output/IpnbLatexOutputCell.java
+++ python/ipnb/src/org/jetbrains/plugins/ipnb/format/cells/output/IpnbLatexOutputCell.java
@@ -8,17 +8,21 @@ import java.util.Map;
 
 public class IpnbLatexOutputCell extends IpnbOutputCell {
   @NotNull private final List<String> myLatex;
+  private final boolean isMarkdown;
 
-  public IpnbLatexOutputCell(@NotNull final List<String> latex,
-                             Integer promptNumber,
-                             @NotNull final List<String> text,
-                             @Nullable Map<String, Object> metadata) {
+  public IpnbLatexOutputCell(@NotNull final List<String> latex, final boolean markdown, Integer promptNumber,
+                             @NotNull final List<String> text, @Nullable Map<String, Object> metadata) {
     super(text, promptNumber, metadata);
     myLatex = latex;
+    isMarkdown = markdown;
   }
 
   @NotNull
   public List<String> getLatex() {
     return myLatex;
+  }
+
+  public boolean isMarkdown() {
+    return isMarkdown;
   }
 }
