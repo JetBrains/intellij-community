b4128968828d263f83d48482143eb2e7ddc21849
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2016-11-30 22:12:01 +0300

    Rename JBSpinner to JBIntSpinner, PortField now extends JBIntSpinner

--- platform/platform-api/src/com/intellij/ui/PortField.java
+++ platform/platform-api/src/com/intellij/ui/PortField.java
@@ -15,12 +15,9 @@
  */
 package com.intellij.ui;
 
-import org.jetbrains.annotations.NotNull;
-
 import javax.swing.*;
-import java.awt.event.*;
 
-public class PortField extends JSpinner {
+public class PortField extends JBIntSpinner {
   public PortField() {
     this(0);
   }
@@ -34,111 +31,10 @@ public class PortField extends JSpinner {
   }
 
   public PortField(int value, int min) {
-    setModel(new SpinnerNumberModel(value, min, 65535, 1));
-    final NumberEditor editor = new NumberEditor(this, "#");
-    setEditor(editor);
-    final MyListener listener = new MyListener();
-    addMouseWheelListener(listener);
-    final JFormattedTextField field = editor.getTextField();
-    field.addFocusListener(listener);
-    field.addMouseListener(listener);
-  }
-
-  public void setEditable(boolean value) {
-    ((NumberEditor)getEditor()).getTextField().setEditable(value);
-  }
-
-  public void setNumber(int value) {
-    setValue(value);
-  }
-
-  public int getNumber() {
-    return ((SpinnerNumberModel)getModel()).getNumber().intValue();
+    super(value, min, 65535);
   }
 
   public boolean isSpecified() {
     return getNumber() != 0;
-  }
-
-  private static class MyListener extends MouseAdapter implements FocusListener {
-
-    private boolean select = true;
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-      select = false;
-    }
-
-    @Override
-    public void focusGained(FocusEvent e) {
-      if (!select) {
-        select = true;
-        return;
-      }
-      //noinspection SSBasedInspection
-      SwingUtilities.invokeLater(() -> {
-        final Object source = e.getSource();
-        if (source instanceof JFormattedTextField) {
-          final JFormattedTextField textField = (JFormattedTextField)source;
-          textField.selectAll();
-        }
-      });
-    }
-
-
-    @Override
-    public void focusLost(FocusEvent e) {}
-
-    @Override
-    public void mouseWheelMoved(MouseWheelEvent e) {
-      int units = e.getUnitsToScroll();
-      if (units == 0) return;
-      final Object source = e.getSource();
-      if (source instanceof JSpinner) {
-        final JSpinner spinner = (JSpinner)source;
-        final SpinnerModel model = spinner.getModel();
-        if (model instanceof SpinnerNumberModel) {
-          final SpinnerNumberModel numberModel = (SpinnerNumberModel)model;
-          final Number value = (Number)numberModel.getValue();
-          final Number stepSize = numberModel.getStepSize();
-          final Comparable minimum = numberModel.getMinimum();
-          final Comparable maximum = numberModel.getMaximum();
-          final Number newValue = calculateNewValue(value, stepSize, minimum, maximum, units);
-          if (newValue != null) {
-            numberModel.setValue(newValue);
-          }
-        }
-      }
-    }
-
-    private static Number calculateNewValue(@NotNull Number value, @NotNull Number stepSize,
-                                            Comparable minimum, Comparable maximum, int steps) {
-      final Number newValue;
-      if ((value instanceof Float) || (value instanceof Double)) {
-        final double v = value.doubleValue() + (stepSize.doubleValue() * (double)steps);
-        newValue = value instanceof Double ? new Double(v) : new Float(v);
-      }
-      else {
-        final long v = value.longValue() + (stepSize.longValue() * (long)steps);
-
-        if (value instanceof Long) {
-          newValue = Long.valueOf(v);
-        }
-        else if (value instanceof Integer) {
-          newValue = Integer.valueOf((int)v);
-        }
-        else if (value instanceof Short) {
-          newValue = Short.valueOf((short)v);
-        }
-        else {
-          newValue = Byte.valueOf((byte)v);
-        }
-      }
-
-      if ((maximum != null) && (maximum.compareTo(newValue) < 0) || (minimum != null) && (minimum.compareTo(newValue) > 0)) {
-        return null;
-      }
-      return newValue;
-    }
   }
 }
