676e9060a8714d908bb888b60da8cc8db24e5493
Aleksey Pivovarov <AMPivovarov@gmail.com>
2016-12-01 17:14:25 +0300

    IDEA-164814 merge: fix grammar

--- platform/diff-impl/tests/com/intellij/diff/util/DiffUtilTest.kt
+++ platform/diff-impl/tests/com/intellij/diff/util/DiffUtilTest.kt
@@ -31,6 +31,7 @@
 package com.intellij.diff.util
 
 import com.intellij.diff.DiffTestCase
+import com.intellij.openapi.diff.DiffBundle
 import com.intellij.util.containers.ContainerUtil
 
 class DiffUtilTest : DiffTestCase() {
@@ -57,5 +58,24 @@ class DiffUtilTest : DiffTestCase() {
     doTest(8, 7, 6, 5, 4, 3, 2, 1) { v1, v2 -> v2 - v1 }
 
     doTest(1, 3, 5, 7, 8, 6, 4, 2) { v1, v2 -> v2 - v1 }
+  }
+
+  fun `test merge conflict partially resolved confirmation message`() {
+    fun doTest(changes: Int, conflicts: Int, expected: String) {
+      val actual = DiffBundle.message("merge.dialog.apply.partially.resolved.changes.confirmation.message", changes, conflicts)
+      assertTrue(actual.startsWith(expected), actual)
+    }
+
+    doTest(1, 0, "There is one change left")
+    doTest(0, 1, "There is one conflict left")
+    doTest(1, 1, "There is one change and one conflict left")
+
+    doTest(2, 0, "There are 2 changes left")
+    doTest(0, 2, "There are 2 conflicts left")
+    doTest(2, 2, "There are 2 changes and 2 conflicts left")
+
+    doTest(1, 2, "There is one change and 2 conflicts left")
+    doTest(2, 1, "There are 2 changes and one conflict left")
+    doTest(2, 3, "There are 2 changes and 3 conflicts left")
   }
 }
