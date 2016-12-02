369af45824f2c92bbf0397feb6cec0ea77facece
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2016-12-02 15:21:48 +0300

    Get DataContext from recent mouse event when it's possible, otherwise fallback to getFocusedComponent(). New registry flag 'actionSystem.getContextByRecentMouseEvent' is OFF by default.

--- platform/platform-impl/src/com/intellij/ide/impl/DataManagerImpl.java
+++ platform/platform-impl/src/com/intellij/ide/impl/DataManagerImpl.java
@@ -38,6 +38,7 @@ import com.intellij.openapi.wm.impl.FloatingDecorator;
 import com.intellij.reference.SoftReference;
 import com.intellij.util.KeyedLazyInstanceEP;
 import com.intellij.util.containers.WeakValueHashMap;
+import com.intellij.util.ui.SwingHelper;
 import gnu.trove.THashSet;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
@@ -208,7 +209,11 @@ public class DataManagerImpl extends DataManager {
   @Override
   @NotNull
   public DataContext getDataContext() {
-    return getDataContext(getFocusedComponent());
+    Component component = null;
+    if (Registry.is("actionSystem.getContextByRecentMouseEvent")) {
+      component = SwingHelper.getComponentFromRecentMouseEvent();
+    }
+    return getDataContext(component != null ? component : getFocusedComponent());
   }
 
   @Override
