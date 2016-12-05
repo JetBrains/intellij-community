05f04009aca3d7fe097f8420539428fd7cc56735
Nadya Zabrodina <Nadya.Zabrodina@jetbrains.com>
2016-12-05 19:23:29 +0300

    [patch]: IDEA-164889 do not process clipboard if not needed
    * we can't get content size from clipboard, so it's better to avoid
    heavy content loading if possible;

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/patch/PatchClipboardTracker.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/patch/PatchClipboardTracker.java
@@ -22,6 +22,7 @@ import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.openapi.diff.impl.patch.PatchReader;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vcs.VcsApplicationSettings;
+import com.intellij.openapi.wm.IdeFrame;
 import com.intellij.util.messages.MessageBusConnection;
 import org.jetbrains.annotations.NotNull;
 
@@ -44,13 +45,25 @@ public class PatchClipboardTracker extends ApplicationComponent.Adapter {
 
   private static class PatchClipboardListener extends ClipboardAnalyzeListener {
     @Override
+    public void applicationActivated(IdeFrame ideFrame) {
+      // we can't get clipboard details especially content size, so we should avoid clipboard processing when it's possible;
+      if (!VcsApplicationSettings.getInstance().DETECT_PATCH_ON_THE_FLY) return;
+      super.applicationActivated(ideFrame);
+    }
+
+    @Override
+    public void applicationDeactivated(IdeFrame ideFrame) {
+      if (!VcsApplicationSettings.getInstance().DETECT_PATCH_ON_THE_FLY) return;
+      super.applicationDeactivated(ideFrame);
+    }
+
+    @Override
     protected void handle(@NotNull Project project, @NotNull String value) {
       new ApplyPatchFromClipboardAction.MyApplyPatchFromClipboardDialog(project, value).show();
     }
 
     @Override
     public boolean canHandle(@NotNull String value) {
-      if (!VcsApplicationSettings.getInstance().DETECT_PATCH_ON_THE_FLY) return false;
       return PatchReader.isPatchContent(value);
     }
   }
