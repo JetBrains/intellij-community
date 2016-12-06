a483a00274d2a2815ca93a79eb9a540cd091d8ae
Anna.Kozlova <anna.kozlova@jetbrains.com>
2016-12-06 13:11:10 +0100

    fix testdata

--- plugins/InspectionGadgets/test/com/siyeh/igfixes/style/methodRefs2lambda/CollapseToExpressionLambdaWhenCast.after.java
+++ plugins/InspectionGadgets/test/com/siyeh/igfixes/style/methodRefs2lambda/CollapseToExpressionLambdaWhenCast.after.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -7,7 +22,7 @@ import java.util.stream.Collectors;
 class Collectors2 {
   static <T> Map<T, Integer> combine(Collection<Map<? extends T, Integer>> pMaps) {
     return pMaps.stream()
-      .map((java.util.function.Function<Map<? extends T, Integer>, java.util.Set<? extends Map.Entry<? extends T, Integer>>>) (integerMap) -> integerMap.entrySet())
+      .map((java.util.function.Function<Map<? extends T, Integer>, java.util.Set<? extends Map.Entry<? extends T, Integer>>>) integerMap -> integerMap.entrySet())
       .flatMap(Collection::stream)
       .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
                                 (pEntry1, pEntry2) -> pEntry1 + pEntry2,
--- plugins/InspectionGadgets/test/com/siyeh/igfixes/style/methodRefs2lambda/SuperRefs.after.java
+++ plugins/InspectionGadgets/test/com/siyeh/igfixes/style/methodRefs2lambda/SuperRefs.after.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 public class MyTest {
 
     interface I {
@@ -10,7 +25,7 @@ public class MyTest {
 
     static class B extends A {
         void m(int i1) {
-            I i = (i2) -> super.m(i2);
+            I i = i2 -> super.m(i2);
         }
     }
 }
