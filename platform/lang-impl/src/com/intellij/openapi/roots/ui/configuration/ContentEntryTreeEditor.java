21b226d7da078b5672646c36cb6f9fba76ba1fe1
Alexander Zolotov <alexander.zolotov@jetbrains.com>
2016-12-06 19:20:10 +0300

    Prettify content entries configurables
    - allow to create content entries editor with borders and enable border in
    configurables
    - remove redundant borders around splitter
    - move delete icons to 3px right
    - replace Insets with JBUI.insets
    
    (cherry picked from commit a39d42f)
    IDEA-CR-16622

--- platform/lang-impl/src/com/intellij/openapi/roots/ui/configuration/ContentEntryTreeEditor.java
+++ platform/lang-impl/src/com/intellij/openapi/roots/ui/configuration/ContentEntryTreeEditor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -89,7 +89,7 @@ public class ContentEntryTreeEditor {
     new TreeSpeedSearch(myTree);
 
     myTreePanel = new MyPanel(new BorderLayout());
-    final JScrollPane scrollPane = ScrollPaneFactory.createScrollPane(myTree);
+    final JScrollPane scrollPane = ScrollPaneFactory.createScrollPane(myTree, true);
     myTreePanel.add(scrollPane, BorderLayout.CENTER);
 
     myTreePanel.setVisible(false);
