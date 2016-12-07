8cab448be912ce60c89fa8a53faeda6934486607
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2016-12-07 18:00:21 +0300

    index todos in JSDoc comments in embedded scripts

--- platform/core-api/src/com/intellij/lang/ParserDefinition.java
+++ platform/core-api/src/com/intellij/lang/ParserDefinition.java
@@ -72,6 +72,7 @@ public interface ParserDefinition {
    * Returns the set of token types which are treated as comments by the PSI builder.
    * Tokens of those types are automatically skipped by PsiBuilder. Also, To Do patterns
    * are searched in the text of tokens of those types.
+   * This token set shouldn't contain types of non-leaf comment inner elements.
    *
    * @return the set of comment token types.
    */
