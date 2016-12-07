962c2a358a3a8d6723aa9d16d27cffd4a17e15cd
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2016-12-08 01:27:45 +0300

    PY-1409: Junk removed

--- platform/smRunner/src/com/intellij/execution/testframework/sm/runner/OutputLineSplitter.java
+++ platform/smRunner/src/com/intellij/execution/testframework/sm/runner/OutputLineSplitter.java
@@ -23,7 +23,6 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 
 public abstract class OutputLineSplitter {
