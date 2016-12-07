4290a37a08df4ce2e8a2320b183dd847e168a3cb
Anna.Kozlova <anna.kozlova@jetbrains.com>
2016-12-07 12:21:37 +0100

    new inference: check intersection type of upper bound to detect substitution (IDEA-165011)

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/GraphInferenceHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/lambda/GraphInferenceHighlightingTest.java
@@ -495,6 +495,10 @@ public class GraphInferenceHighlightingTest extends LightDaemonAnalyzerTestCase 
     doTest();
   }
 
+  public void testRecursiveTypeWithCapture() throws Exception {
+    doTest();
+  }
+
   private void doTest() throws Exception {
     doTest(false);
   }
