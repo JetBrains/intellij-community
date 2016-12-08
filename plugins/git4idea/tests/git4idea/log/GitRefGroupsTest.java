70b09027414d8de358d1fe26fe937ab5d2d3b8c0
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2016-12-08 19:30:28 +0300

    [git] fix ref groups test

--- plugins/git4idea/tests/git4idea/log/GitRefGroupsTest.java
+++ plugins/git4idea/tests/git4idea/log/GitRefGroupsTest.java
@@ -31,7 +31,8 @@ import java.util.List;
 public class GitRefGroupsTest extends GitRefManagerTest {
   public void test_single_tracked_branch() throws IOException {
     check(given("HEAD", "master", "origin/master"), ContainerUtil.list("HEAD"), Pair.create("Local", ContainerUtil.list("master")),
-          Pair.create("Tracked", ContainerUtil.list("origin/master")));
+          Pair.create("Tracked", ContainerUtil.list("origin/master")),
+          Pair.create("origin/...", ContainerUtil.list("origin/master")));
   }
 
   public void test_single_local_branch() throws IOException {
@@ -41,7 +42,7 @@ public class GitRefGroupsTest extends GitRefManagerTest {
   public void test_local_tracked_and_remote_branch() throws IOException {
     check(given("HEAD", "master", "origin/master", "origin/remote_branch", "local_branch"), ContainerUtil.list("HEAD"),
           Pair.create("Local", ContainerUtil.list("master", "local_branch")), Pair.create("Tracked", ContainerUtil.list("origin/master")),
-          Pair.create("origin/...", ContainerUtil.list("origin/remote_branch")));
+          Pair.create("origin/...", ContainerUtil.list("origin/master", "origin/remote_branch")));
   }
 
   private void check(@NotNull Collection<VcsRef> actual,
