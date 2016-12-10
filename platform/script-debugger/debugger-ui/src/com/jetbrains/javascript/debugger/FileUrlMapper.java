81be191f440eb79adae9fe824c9c1a9f84df104b
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2016-12-10 20:25:07 +0100

    convert FileUrlMapper to kotlin

--- platform/script-debugger/debugger-ui/src/com/jetbrains/javascript/debugger/FileUrlMapper.java
+++ platform/script-debugger/debugger-ui/src/com/jetbrains/javascript/debugger/FileUrlMapper.java
@@ -1,57 +1,40 @@
-package com.jetbrains.javascript.debugger;
+package com.jetbrains.javascript.debugger
 
-import com.intellij.openapi.extensions.ExtensionPointName;
-import com.intellij.openapi.fileEditor.OpenFileDescriptor;
-import com.intellij.openapi.fileTypes.FileType;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.pom.Navigatable;
-import com.intellij.util.Url;
-import com.intellij.util.containers.ObjectIntHashMap;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.extensions.ExtensionPointName
+import com.intellij.openapi.fileEditor.OpenFileDescriptor
+import com.intellij.openapi.fileTypes.FileType
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.pom.Navigatable
+import com.intellij.util.Url
+import com.intellij.util.containers.ObjectIntHashMap
 
-import java.util.Collections;
-import java.util.List;
-
-public abstract class FileUrlMapper {
-  public static final ExtensionPointName<FileUrlMapper> EP_NAME = ExtensionPointName.create("com.jetbrains.fileUrlMapper");
-
-  @NotNull
-  public List<Url> getUrls(@NotNull VirtualFile file, @NotNull Project project, @Nullable String currentAuthority) {
-    return Collections.emptyList();
+abstract class FileUrlMapper {
+  companion object {
+    val EP_NAME = ExtensionPointName.create<FileUrlMapper>("com.jetbrains.fileUrlMapper")
   }
+
+  open fun getUrls(file: VirtualFile, project: Project, currentAuthority: String?): List<Url> = emptyList()
 
   /**
    * Optional to implement, useful if default navigation position to source file is not equals to 0:0 (java file for example)
    */
-  @Nullable
-  public Navigatable getNavigatable(@NotNull Url url, @NotNull Project project, @Nullable Url requestor) {
-    VirtualFile file = getFile(url, project, requestor);
-    return file == null ? null : new OpenFileDescriptor(project, file);
-  }
+  open fun getNavigatable(url: Url, project: Project, requestor: Url?): Navigatable? = getFile(url, project, requestor)?.let { OpenFileDescriptor(project, it) }
 
-  @Nullable
-  public abstract VirtualFile getFile(@NotNull Url url, @NotNull Project project, @Nullable Url requestor);
+  abstract fun getFile(url: Url, project: Project, requestor: Url?): VirtualFile?
 
   /**
    * Optional to implement, sometimes you cannot build URL, but can match.
    * Lifetime: resolve session lifetime. Could be called multiple times: n <= total sourcemap count
    */
-  @Nullable
-  public SourceResolver createSourceResolver(@NotNull VirtualFile file, @NotNull Project project) {
-    return null;
-  }
+  open fun createSourceResolver(file: VirtualFile, project: Project): FileUrlMapperSourceResolver? = null
 
-  @Nullable
-  public FileType getFileType(@NotNull Url url) {
-    return null;
-  }
+  open fun getFileType(url: Url): FileType? = null
+}
 
-  public static abstract class SourceResolver {
-    /**
-     * Return -1 if no match
-     */
-    public abstract int resolve(@NotNull ObjectIntHashMap<Url> map, @NotNull Project project);
-  }
+interface FileUrlMapperSourceResolver {
+  /**
+   * Return -1 if no match
+   */
+  fun resolve(map: ObjectIntHashMap<Url>, project: Project): Int
 }
