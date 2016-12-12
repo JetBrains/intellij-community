97b235937e7fa567355c720bf4b183249f29842f
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2016-12-12 17:14:41 +0100

    [jps] excludes module-info classes from instrumentation

--- jps/jps-builders/src/org/jetbrains/jps/incremental/instrumentation/NotNullInstrumentingBuilder.java
+++ jps/jps-builders/src/org/jetbrains/jps/incremental/instrumentation/NotNullInstrumentingBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -69,7 +69,7 @@ public class NotNullInstrumentingBuilder extends BaseInstrumentingBuilder{
 
   @Override
   protected boolean canInstrument(CompiledClass compiledClass, int classFileVersion) {
-    return classFileVersion >= Opcodes.V1_5;
+    return classFileVersion >= Opcodes.V1_5 && !"module-info".equals(compiledClass.getClassName());
   }
 
   // todo: probably instrument other NotNull-like annotations defined in project settings?
--- plugins/IntelliLang/intellilang-jps-plugin/src/org/jetbrains/jps/intellilang/instrumentation/PatternValidatorBuilder.java
+++ plugins/IntelliLang/intellilang-jps-plugin/src/org/jetbrains/jps/intellilang/instrumentation/PatternValidatorBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -53,7 +53,7 @@ public class PatternValidatorBuilder extends BaseInstrumentingBuilder{
 
   @Override
   protected boolean canInstrument(CompiledClass compiledClass, int classFileVersion) {
-    return true;
+    return !"module-info".equals(compiledClass.getClassName());
   }
 
   @Nullable
