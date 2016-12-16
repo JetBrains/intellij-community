20818e37affd8e56529458fe8e1d3a60211c14e4
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2016-12-16 17:45:14 +0300

    javac ast indices: reduce "--add-exports" options in BuildManager

--- java/compiler/impl/src/com/intellij/compiler/server/BuildManager.java
+++ java/compiler/impl/src/com/intellij/compiler/server/BuildManager.java
@@ -1168,12 +1168,6 @@ public class BuildManager implements Disposable {
 
     //TODO[Dmitry Batkovich] should be replaced with the proper solution
     if (sdkVersion != null && sdkVersion.isAtLeast(JavaSdkVersion.JDK_1_9)) {
-      cmdLine.addParameters("--add-exports", "jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED");
-      cmdLine.addParameters("--add-exports", "jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED");
-      cmdLine.addParameters("--add-exports", "jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED");
-      cmdLine.addParameters("--add-exports", "jdk.compiler/com.sun.tools.javac.model=ALL-UNNAMED");
-      cmdLine.addParameters("--add-exports", "jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED");
-      cmdLine.addParameters("--add-exports", "jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED");
       cmdLine.addParameters("--add-exports", "jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED");
     }
 
