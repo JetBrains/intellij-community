a66acc109b51ea5ca9a64b7f852a48425dafcca1
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2016-12-19 18:12:49 +0300

    javac ast indices: cleanup

--- jps/jps-builders/src/org/jetbrains/jps/javac/ast/JavacReferenceCollector.java
+++ jps/jps-builders/src/org/jetbrains/jps/javac/ast/JavacReferenceCollector.java
@@ -24,7 +24,7 @@ import javax.tools.*;
  * Code here should not depend on any javac private API located in tools.jar if no JavacFileReferencesRegistrar-s will be run.
  * A workaround to allow run standalone jps with improperly configured classloader without NoClassDefFoundError (e.g: IDEA-162877)
  */
-public class JavacReferencesCollector {
+public class JavacReferenceCollector {
   public static void installOn(JavaCompiler.CompilationTask task,
                                boolean divideImportRefs,
                                Consumer<JavacFileData> fileDataConsumer) {
