4c6c0c8bfe7eeed9c2e22a9bd2e1ab097689b67f
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2016-12-21 23:20:51 +0300

    javac ast indices: improve a test

--- java/java-tests/testSrc/com/intellij/compiler/CompilerReferencesFindUsagesTest.java
+++ java/java-tests/testSrc/com/intellij/compiler/CompilerReferencesFindUsagesTest.java
@@ -173,7 +173,7 @@ public class CompilerReferencesFindUsagesTest extends DaemonAnalyzerTestCase {
     try {
       configureByFiles(getName(), Arrays.stream(testFiles).map(f -> getName() + "/" + f).toArray(String[]::new));
       excludesConfigurationPatcher.consume(excludesConfiguration);
-      //assertSize(expectedUsagesCount, FunctionalExpressionSearch.search(myJavaFacade.findClass(CommonClassNames.JAVA_LANG_RUNNABLE)).findAll());
+      assertSize(expectedUsagesCount, FunctionalExpressionSearch.search(myJavaFacade.findClass(CommonClassNames.JAVA_LANG_RUNNABLE)).findAll());
       myCompilerTester.rebuild();
       assertSize(expectedUsagesCount, FunctionalExpressionSearch.search(myJavaFacade.findClass(CommonClassNames.JAVA_LANG_RUNNABLE)).findAll());
     } finally {
