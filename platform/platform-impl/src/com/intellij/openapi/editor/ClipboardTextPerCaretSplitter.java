85c54f00578adb90f53dd6fc700222f9df52a636
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2016-12-21 12:39:58 +0300

    IDEA-165306 Multiple caret Copy and Paste gives inconsistent results

--- platform/platform-impl/src/com/intellij/openapi/editor/ClipboardTextPerCaretSplitter.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/ClipboardTextPerCaretSplitter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,14 +33,14 @@ public class ClipboardTextPerCaretSplitter {
     }
     List<String> result = new ArrayList<>(caretCount);
     int sourceCaretCount = caretData == null ? -1 : caretData.startOffsets.length;
-    String[] lines = sourceCaretCount == 1 || sourceCaretCount == caretCount ? null : input.split("\n", -1);
+    String[] lines = sourceCaretCount < 1 ? null : input.split("\n", -1);
     for (int i = 0; i < caretCount; i++) {
       if (sourceCaretCount == 1) {
         result.add(input);
       }
-      else if (sourceCaretCount == caretCount) {
+      else if (sourceCaretCount > 1) {
         //noinspection ConstantConditions
-        result.add(new String(input.substring(caretData.startOffsets[i], caretData.endOffsets[i])));
+        result.add(i < sourceCaretCount ? new String(input.substring(caretData.startOffsets[i], caretData.endOffsets[i])) : "");
       }
       else if (lines.length == 0) {
         result.add("");
