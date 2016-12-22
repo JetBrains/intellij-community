927ad76eb49c9d984efbbc790b24297e2c8a2530
Bas Leijdekkers <basleijdekkers@gmail.com>
2016-12-22 17:09:59 +0100

    fix testdata

--- plugins/InspectionGadgets/test/com/siyeh/igtest/bugs/malformed_regex/MalformedRegex.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/bugs/malformed_regex/MalformedRegex.java
@@ -1,7 +1,7 @@
 import java.util.regex.Pattern;
 
 class MalformedRegex {
-  Pattern BACKSLASH_PATTERN = Pattern.compile("<error descr="Illegal/unsupported escape sequence">\</error>\" , Pattern.LITERAL);
+  Pattern BACKSLASH_PATTERN = Pattern.compile("<error descr="Illegal/unsupported escape sequence">\\</error>" , Pattern.LITERAL);
 
   private static final String ASTERISK = "<error descr="Dangling metacharacter">*</error>";
 
