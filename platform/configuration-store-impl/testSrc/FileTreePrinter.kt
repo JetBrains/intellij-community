8af5aa96c211b471d30f625a22d6ae1e101a2f1c
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2016-12-28 14:53:34 +0100

    do not save default proxy.settings.pwd

--- platform/configuration-store-impl/testSrc/FileTreePrinter.kt
+++ platform/configuration-store-impl/testSrc/FileTreePrinter.kt
@@ -3,27 +3,21 @@ package com.intellij.configurationStore
 import com.intellij.util.io.directoryStreamIfExists
 import com.intellij.util.io.isDirectory
 import java.nio.file.Path
+import java.util.*
 
-fun printDirectoryTree(folder: Path): String {
-  if (!folder.isDirectory()) {
-    throw IllegalArgumentException("folder is not a Directory")
-  }
-  val indent = 0
+fun printDirectoryTree(dir: Path): String {
   val sb = StringBuilder()
-  printDirectoryTree(folder, indent, sb)
+  printDirectoryTree(dir, 0, sb)
   return sb.toString()
 }
 
 private fun printDirectoryTree(dir: Path, indent: Int, sb: StringBuilder) {
-  if (!dir.isDirectory()) {
-    throw IllegalArgumentException("folder is not a Directory")
-  }
   getIndentString(indent, sb)
   sb.append("\u251c\u2500\u2500")
   sb.append(dir.fileName.toString())
   sb.append("/")
   sb.append("\n")
-  for (file in dir.directoryStreamIfExists { it.map { it } }!!) {
+  for (file in sortedFileList(dir)) {
     if (file.isDirectory()) {
       printDirectoryTree(file, indent + 1, sb)
     }
@@ -31,6 +25,15 @@ private fun printDirectoryTree(dir: Path, indent: Int, sb: StringBuilder) {
       printFile(file, indent + 1, sb)
     }
   }
+}
+
+private fun sortedFileList(dir: Path): List<Path> {
+  return dir.directoryStreamIfExists {
+    val list = ArrayList<Path>()
+    it.mapTo(list) { it }
+    list.sort()
+    list
+  } ?: emptyList()
 }
 
 private fun printFile(file: Path, indent: Int, sb: StringBuilder) {
