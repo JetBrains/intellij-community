a53b8f8800734b42ef7def887ec32e1e01ca59e5
Elizaveta Shashkova <elizaveta.shashkova@jetbrains.com>
2016-12-28 20:21:50 +0300

    Fix cython setup script
    (cherry picked from commit 08e19a9)

--- python/helpers/pydev/_pydevd_frame_eval/setup.py
+++ python/helpers/pydev/_pydevd_frame_eval/setup.py
@@ -1,10 +1,19 @@
-from distutils.core import setup
-from Cython.Build import cythonize
+import os
 import sys
+from distutils.core import setup
+from setuptools import Extension
+from Cython.Build import cythonize
 
 if sys.version_info < (3, 6):
     raise Exception('PEP 523 API is available on Python 3.6 and higher')
 
-setup(name='pydevd_frame_evaluator',
-      ext_modules=cythonize("pydevd_frame_evaluator.pyx"),
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+target_name = 'pydevd_frame_evaluator'
+
+ext_modules = [
+    Extension(target_name,  # location of the resulting .so
+              ["{}.pyx".format(target_name)], )]
+
+setup(name=target_name,
+      ext_modules=cythonize(ext_modules),
       requires=['Cython'])
