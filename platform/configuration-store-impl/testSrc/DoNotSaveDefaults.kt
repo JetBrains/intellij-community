01d3b6f6105b9713124c14c0024f6341eb44898b
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2016-12-29 15:59:51 +0100

    do not save defaults — "tomee.extensions.xml", "jboss.extensions.xml", "glassfish.extensions.xml" as known violation

--- platform/configuration-store-impl/testSrc/DoNotSaveDefaults.kt
+++ platform/configuration-store-impl/testSrc/DoNotSaveDefaults.kt
@@ -58,7 +58,8 @@ class DoNotSaveDefaultsTest {
     println(directory)
     val directoryTree = printDirectoryTree(dirPath, setOf(
       "path.macros.xml" /* todo EP to register (provide) macro dynamically */,
-      "stubIndex.xml" /* low-level non-roamable stuff */
+      "stubIndex.xml" /* low-level non-roamable stuff */,
+      "tomee.extensions.xml", "jboss.extensions.xml", "glassfish.extensions.xml" /* javaee non-roamable stuff, it will be better to fix it */
     ))
     println(directoryTree)
     assertThat(directoryTree).toMatchSnapshot(testData.resolve("DoNotSaveDefaults.snap.txt"))
