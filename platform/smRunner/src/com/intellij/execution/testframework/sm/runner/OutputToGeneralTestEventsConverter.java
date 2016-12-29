57f292dd7cf23f2956c4ffe98d1b6cb0b0c2787b
Anna.Kozlova <anna.kozlova@jetbrains.com>
2016-12-29 13:59:33 +0100

    optimize output processing when console's cycle buffer is activated; react on changing of configured size

--- platform/smRunner/src/com/intellij/execution/testframework/sm/runner/OutputToGeneralTestEventsConverter.java
+++ platform/smRunner/src/com/intellij/execution/testframework/sm/runner/OutputToGeneralTestEventsConverter.java
@@ -42,7 +42,7 @@ import static com.intellij.execution.testframework.sm.runner.GeneralToSMTRunnerE
  */
 public class OutputToGeneralTestEventsConverter implements ProcessOutputConsumer {
   private static final Logger LOG = Logger.getInstance(OutputToGeneralTestEventsConverter.class.getName());
-  private final int CYCLE_BUFFER_SIZE = ConsoleBuffer.getCycleBufferSize();
+  private static final boolean USE_CYCLE_BUFFER = ConsoleBuffer.useCycleBuffer();
 
   private final MyServiceMessageVisitor myServiceMessageVisitor;
   private final String myTestFrameworkName;
@@ -97,9 +97,10 @@ public class OutputToGeneralTestEventsConverter implements ProcessOutputConsumer
   }
 
   protected void processConsistentText(String text, final Key outputType, boolean tcLikeFakeOutput) {
-    if (text.length() > CYCLE_BUFFER_SIZE) {
-      final StringBuilder builder = new StringBuilder(CYCLE_BUFFER_SIZE);
-      builder.append(text, 0, CYCLE_BUFFER_SIZE - 105);
+    final int cycleBufferSize = ConsoleBuffer.getCycleBufferSize();
+    if (USE_CYCLE_BUFFER && text.length() > cycleBufferSize) {
+      final StringBuilder builder = new StringBuilder(cycleBufferSize);
+      builder.append(text, 0, cycleBufferSize - 105);
       builder.append("<...>");
       builder.append(text, text.length() - 100, text.length());
       text = builder.toString();
