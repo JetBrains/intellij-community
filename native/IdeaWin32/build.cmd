404f46245a6817533b8d73f44f5849a903289ab1
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2017-01-04 13:18:47 +0100

    [platform] migrates IdeaWin32 build to CMake (build script)

--- native/IdeaWin32/build.cmd
+++ native/IdeaWin32/build.cmd
@@ -7,8 +7,7 @@ SET CMAKE=%CMAKE_PATH%\bin\cmake
 IF EXIST "%BUILD_DIR%" RMDIR /S /Q "%BUILD_DIR%"
 MKDIR "%BUILD_DIR%" & CD "%BUILD_DIR%"
 
-:: todo "%CMAKE%" -G "Visual Studio 12 2013" -T v120_xp -A "%PLATFORM%" ..
-"%CMAKE%" -G "Visual Studio 14 2015" -A "%PLATFORM%" ..
+"%CMAKE%" -G "Visual Studio 12 2013" -T v120_xp -A "%PLATFORM%" ..
 IF ERRORLEVEL 1 EXIT 1
 
 "%CMAKE%" --build . --config Release
