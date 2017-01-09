eb74eeb82816a021e6b7c01f9e53226e95f1f604
Alexey Kudravtsev <cdr@intellij.com>
2017-01-09 18:55:14 +0300

    test for ANSI coloring performance + fixes

--- platform/platform-tests/testSrc/com/intellij/execution/process/AnsiEscapeDecoderTest.java
+++ platform/platform-tests/testSrc/com/intellij/execution/process/AnsiEscapeDecoderTest.java
@@ -2,14 +2,22 @@ package com.intellij.execution.process;
 
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.util.io.BufferExposingByteArrayInputStream;
+import com.intellij.openapi.util.io.BufferExposingByteArrayOutputStream;
 import com.intellij.testFramework.PlatformTestCase;
+import com.intellij.testFramework.PlatformTestUtil;
+import com.intellij.util.Consumer;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 import org.junit.Assert;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class AnsiEscapeDecoderTest extends PlatformTestCase {
 
@@ -79,16 +87,81 @@ public class AnsiEscapeDecoderTest extends PlatformTestCase {
   private static AnsiEscapeDecoder.ColoredChunksAcceptor createExpectedAcceptor(@NotNull final Pair<String, Key>... expected) {
     return new AnsiEscapeDecoder.ColoredChunksAcceptor() {
       @Override
-      public void coloredChunksAvailable(List<Pair<String, Key>> chunks) {
+      public void coloredChunksAvailable(@NotNull List<Pair<String, Key>> chunks) {
         List<Pair<String, String>> expectedWithKeyName = toListWithKeyName(Arrays.asList(expected));
         List<Pair<String, String>> actualWithKeyName = toListWithKeyName(chunks);
         Assert.assertEquals(expectedWithKeyName, actualWithKeyName);
       }
 
       @Override
-      public void coloredTextAvailable(String text, Key attributes) {
+      public void coloredTextAvailable(@NotNull String text, @NotNull Key attributes) {
         throw new RuntimeException(); // shouldn't be called
       }
     };
+  }
+
+  public void testPerformance() throws IOException {
+    byte[] buffer = new byte[100000];
+    BufferExposingByteArrayOutputStream outputStream = new BufferExposingByteArrayOutputStream(buffer);
+    BufferExposingByteArrayInputStream inputStream = new BufferExposingByteArrayInputStream(buffer);
+    AtomicBoolean finished = new AtomicBoolean();
+    Process testProcess = new Process() {
+      @Override
+      public OutputStream getOutputStream() {
+        return outputStream;
+      }
+
+      @Override
+      public InputStream getInputStream() {
+        return inputStream;
+      }
+
+      @Override
+      public InputStream getErrorStream() {
+        return inputStream;
+      }
+
+      @Override
+      public int waitFor() {
+        while (!finished.get());
+        return 0;
+      }
+
+      @Override
+      public int exitValue() {
+        return 0;
+      }
+
+      @Override
+      public void destroy() {
+        finished.set(true);
+      }
+    };
+
+    withProcessHandlerFrom(testProcess, handler -> {
+      PlatformTestUtil.startPerformanceTest("ansi color", 15000, ()->{
+        for (int i=0; i<2_000_000;i++) {
+          handler.notifyTextAvailable(i+"Chrome 35.0.1916 (Linux): Executed 0 of 1\u001B[32m SUCCESS\u001B[39m (0 secs / 0 secs)\n", ProcessOutputTypes.STDOUT);
+          handler.notifyTextAvailable(i+"Plain\u001B[32mGreen\u001B[39mNormal\u001B[1A\u001B[2K\u001B[31mRed\u001B[39m\n", ProcessOutputTypes.SYSTEM);
+        }
+      }).assertTiming();
+    });
+  }
+
+  public static void withProcessHandlerFrom(@NotNull Process testProcess, @NotNull Consumer<ProcessHandler> consumer) {
+    KillableColoredProcessHandler handler = new KillableColoredProcessHandler(testProcess, "testProcess");
+    handler.setShouldDestroyProcessRecursively(false);
+    handler.startNotify();
+    handler.notifyTextAvailable("Running stuff...\n", ProcessOutputTypes.STDOUT);
+
+    try {
+      consumer.consume(handler);
+    }
+    finally {
+      handler.doDestroyProcess();
+      handler.notifyProcessTerminated(0);
+      handler.waitFor();
+    }
+
   }
 }
