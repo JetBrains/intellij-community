66bf61455ac377c0178bf41c8e62155e4d4780e0
Gregory.Shrago <gregory.shrago@jetbrains.com>
2017-01-10 14:39:54 +0300

    @nullable

--- platform/util/src/com/intellij/util/ui/UIUtil.java
+++ platform/util/src/com/intellij/util/ui/UIUtil.java
@@ -3782,8 +3782,10 @@ public class UIUtil {
    * @return the first window ancestor of the component; or {@code null}
    *         if the component is not a window and is not contained inside a window
    */
-  public static Window getWindow(Component component) {
-    return component instanceof Window ? (Window)component : SwingUtilities.getWindowAncestor(component);
+  @Nullable
+  public static Window getWindow(@Nullable Component component) {
+    return component == null ? null :
+           component instanceof Window ? (Window)component : SwingUtilities.getWindowAncestor(component);
   }
 
   /**
@@ -3792,10 +3794,9 @@ public class UIUtil {
    *
    * @param window the window to activate
    */
-  public static void toFront(Window window) {
+  public static void toFront(@Nullable Window window) {
     if (window instanceof Frame) {
-      Frame frame = (Frame)window;
-      frame.setState(Frame.NORMAL);
+      ((Frame)window).setState(Frame.NORMAL);
     }
     if (window != null) {
       window.toFront();
