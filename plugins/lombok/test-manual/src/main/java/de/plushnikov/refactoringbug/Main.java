e9583206a99696a32baa639ceddbea35ee8537d3
Michail Plushnikov <mplushnikov@gmail.com>
2017-01-10 20:07:19 +0100

    updated files
    GitOrigin-RevId: b9b238c8da1576ec43df5e02d61fc6fb1d8df763

--- plugins/lombok/test-manual/src/main/java/de/plushnikov/refactoringbug/Main.java
+++ plugins/lombok/test-manual/src/main/java/de/plushnikov/refactoringbug/Main.java
@@ -3,6 +3,7 @@ package de.plushnikov.refactoringbug;
 public class Main {
   public static void main(String[] args) {
     PlayerWrapper wrapper = new PlayerWrapper();
-    wrapper.getMyPLAYER(); // works
+    Object myPLAYER = wrapper.getMyPLAYER();// works
+    System.out.println(myPLAYER);
   }
 }
