3738e54feefface2170e3994f4ce2e55c2ce164e
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2017-01-10 09:42:03 +0600

    StreamApiMigration: add parentheses if necessary when .stream() can be omitted (fix for EA-93812).

--- java/java-impl/src/com/intellij/codeInspection/streamMigration/TerminalBlock.java
+++ java/java-impl/src/com/intellij/codeInspection/streamMigration/TerminalBlock.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -447,7 +447,7 @@ class TerminalBlock {
 
   String generate(boolean noStreamForEmpty) {
     if(noStreamForEmpty && myOperations.length == 1 && myOperations[0] instanceof CollectionStream) {
-      return myOperations[0].getExpression().getText();
+      return ParenthesesUtils.getText(myOperations[0].getExpression(), ParenthesesUtils.POSTFIX_PRECEDENCE);
     }
     return StreamEx.of(myOperations).map(Operation::createReplacement).joining();
   }
