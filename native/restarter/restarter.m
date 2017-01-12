17c330afa72070a7b26d1ea619ab33e06cba2a99
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2017-01-12 20:49:16 +0100

    [platform] updates macOS restarter
    - minor optimization
    - build script instead of a monstrous XCode project
    - system requirements lifted

--- native/restarter/restarter.m
+++ native/restarter/restarter.m
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,12 +19,9 @@
 int main(int argc, const char *argv[]) {
     if (argc < 2) return EXIT_FAILURE;
 
-    unsigned int interval = 500; // check every 0.5 second
-    unsigned int slept = 0;
+    useconds_t interval = 500 * 1000;  // check every 0.5 second
     while (getppid() != 1) {
-        usleep(interval * 1000);
-
-        slept += interval;
+        usleep(interval);
     }
 
     if (argc > 2) {
