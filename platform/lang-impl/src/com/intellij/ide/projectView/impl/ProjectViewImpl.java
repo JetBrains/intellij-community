3bb0ad0f9a07b41911679a9981e1786e92968f11
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2017-01-13 22:02:18 +0300

    EA-64495 - assert: ProjectViewImpl.changeViewCB Add project info to assertion message

--- platform/lang-impl/src/com/intellij/ide/projectView/impl/ProjectViewImpl.java
+++ platform/lang-impl/src/com/intellij/ide/projectView/impl/ProjectViewImpl.java
@@ -849,7 +849,7 @@ public class ProjectViewImpl extends ProjectView implements PersistentStateCompo
   @Override
   public ActionCallback changeViewCB(@NotNull String viewId, String subId) {
     AbstractProjectViewPane pane = getProjectViewPaneById(viewId);
-    LOG.assertTrue(pane != null, "Project view pane not found: " + viewId + "; subId:" + subId);
+    LOG.assertTrue(pane != null, "Project view pane not found: " + viewId + "; subId:" + subId + "; project: " + myProject);
     if (!viewId.equals(getCurrentViewId())
         || subId != null && !subId.equals(pane.getSubId())) {
       for (Content content : getContentManager().getContents()) {
