2548a800e1c1acc5da1ac056336e8707ca1409aa
Konstantin Aleev <konstantin.aleev@jetbrains.com>
2017-01-17 13:09:44 +0300

    IDEA-155345 Add run/stop/edit and grouping actions for runtime dashboard tool window

--- platform/remote-servers/impl/src/com/intellij/remoteServer/impl/runtime/ui/tree/actions/ServersTreeAction.java
+++ platform/remote-servers/impl/src/com/intellij/remoteServer/impl/runtime/ui/tree/actions/ServersTreeAction.java
@@ -2,11 +2,15 @@ package com.intellij.remoteServer.impl.runtime.ui.tree.actions;
 
 import com.intellij.execution.dashboard.DashboardTreeAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.remoteServer.impl.runtime.ui.ServersToolWindowContent;
 import com.intellij.remoteServer.impl.runtime.ui.tree.ServersTreeNode;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.util.List;
 
 public abstract class ServersTreeAction<T extends ServersTreeNode> extends DashboardTreeAction<T, ServersToolWindowContent>
   implements DumbAware {
@@ -17,5 +21,35 @@ public abstract class ServersTreeAction<T extends ServersTreeNode> extends Dashb
   @Override
   protected final ServersToolWindowContent getTreeContent(AnActionEvent e) {
     return e.getData(ServersToolWindowContent.KEY);
+  }
+
+  @Override
+  protected boolean isVisible4(T node) {
+    return super.isVisible4(node);
+  }
+
+  @Override
+  protected boolean isEnabled4(T node) {
+    return super.isEnabled4(node);
+  }
+
+  @Override
+  protected void updatePresentation(@NotNull Presentation presentation, @Nullable T node) {
+    super.updatePresentation(presentation, node);
+  }
+
+  @Override
+  protected void doActionPerformed(@NotNull ServersToolWindowContent content, AnActionEvent e, List<T> nodes) {
+    super.doActionPerformed(content, e, nodes);
+  }
+
+  @Override
+  protected void doActionPerformed(@NotNull ServersToolWindowContent content, AnActionEvent e, T node) {
+    super.doActionPerformed(content, e, node);
+  }
+
+  @Override
+  protected void doActionPerformed(T node) {
+    super.doActionPerformed(node);
   }
 }
