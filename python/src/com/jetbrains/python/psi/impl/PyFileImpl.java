dd6436af5191f7743af35a13c03ca55542ce9530
Dmitry Panchenko <dmitry.panchenko.work@gmail.com>
2017-01-19 22:57:20 +0300

    PY-21915 Fix PyDeprecationInspection for DeprecationWarning subclasses

--- python/src/com/jetbrains/python/psi/impl/PyFileImpl.java
+++ python/src/com/jetbrains/python/psi/impl/PyFileImpl.java
@@ -634,13 +634,20 @@ public class PyFileImpl extends PsiFileBase implements PyFile, PyExpression {
     return enabled;
   }
 
+  @Nullable
   @Override
-  public String getDeprecationMessage() {
+  public String getDeprecationMessage(TypeEvalContext typeEvalContext) {
     final StubElement stub = getStub();
     if (stub instanceof PyFileStub) {
       return ((PyFileStub)stub).getDeprecationMessage();
     }
-    return extractDeprecationMessage();
+    return extractDeprecationMessage(typeEvalContext);
+  }
+
+  @Nullable
+  @Override
+  public String getDeprecationMessage() {
+    return getDeprecationMessage(null);
   }
 
   @Override
@@ -666,8 +673,12 @@ public class PyFileImpl extends PsiFileBase implements PyFile, PyExpression {
   }
 
   public String extractDeprecationMessage() {
+    return extractDeprecationMessage(null);
+  }
+
+  public String extractDeprecationMessage(TypeEvalContext typeEvalContext) {
     if (canHaveDeprecationMessage(getText())) {
-      return PyFunctionImpl.extractDeprecationMessage(getStatements());
+      return PyFunctionImpl.extractDeprecationMessage(getStatements(), typeEvalContext);
     }
     else {
       return null;
