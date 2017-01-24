6d7144fa101324296f373ecd582ce3907c75d1cf
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2017-01-24 22:37:13 +0300

    [groovy] update 'Cannot cast' inspection - move all cast logic into single type converter - fix tests - do not rely on TypesUtil.isNumeric()

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/statements/expressions/GrReferenceExpressionImpl.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/statements/expressions/GrReferenceExpressionImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,7 +43,6 @@ import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrField;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrVariable;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrExpression;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrReferenceExpression;
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.params.GrParameter;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.members.GrAccessorMethod;
 import org.jetbrains.plugins.groovy.lang.psi.api.toplevel.imports.GrImportStatement;
 import org.jetbrains.plugins.groovy.lang.psi.api.types.GrTypeArgumentList;
@@ -386,7 +385,7 @@ public class GrReferenceExpressionImpl extends GrReferenceElementImpl<GrExpressi
         if (resolvedF instanceof GrField) {
           type = ((GrField)resolvedF).getType();
         }
-        else if (resolvedF instanceof GrVariable && !(resolvedF instanceof GrParameter)) {
+        else if (resolvedF instanceof GrVariable) {
           type = ((GrVariable)resolvedF).getDeclaredType();
         }
         else if (resolvedF instanceof GrAccessorMethod) {
