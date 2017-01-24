2350aee64bbdae02220cb010c7bcacbece272bc5
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2017-01-24 20:30:21 +0300

    Typeshed uses 'int' instead of 'Number' in most cases
    (cherry picked from commit 9990a36)

--- python/testData/inspections/PyTypeCheckerInspection/DictLiterals.py
+++ python/testData/inspections/PyTypeCheckerInspection/DictLiterals.py
@@ -1,7 +1,7 @@
 def test():
     xs = {'foo': 1, 'bar': 2}
     for v in xs.values():
-        print(v + <warning descr="Expected type 'Number', got 'None' instead">None</warning>)
+        print(v + <warning descr="Expected type 'int', got 'None' instead">None</warning>)
     for k in xs.keys():
         print(k + <warning descr="Expected type 'Union[str, unicode]', got 'None' instead">None</warning>)
     for k in xs:
