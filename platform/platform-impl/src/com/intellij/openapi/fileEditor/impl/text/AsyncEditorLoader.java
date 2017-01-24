0ea6156d6e3fcbb8c443b38f0a0b64b5aaab03ac
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2017-01-24 12:17:41 +0300

    use new approach for retrying read action in AsyncEditorLoader (following IDEA-CR-17103)

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/text/AsyncEditorLoader.java
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/text/AsyncEditorLoader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -106,7 +106,7 @@ public class AsyncEditorLoader {
             });
             return continuation;
           }
-          TimeUnit.MILLISECONDS.sleep(RETRY_TIME_MS);
+          ProgressIndicatorUtils.yieldToPendingWriteActions();
         }
       }
       finally {
