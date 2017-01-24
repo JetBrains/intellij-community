6ed9496dff307868319292623abb653eada10cc8
Elizaveta Shashkova <elizaveta.shashkova@jetbrains.com>
2017-01-24 21:07:55 +0300

    Some code cleanup and fixing performance problems for often functions calls
    (cherry picked from commit fd84a11)

--- python/helpers/pydev/_pydevd_frame_eval/pydevd_frame_evaluator.pyx
+++ python/helpers/pydev/_pydevd_frame_eval/pydevd_frame_evaluator.pyx
@@ -7,16 +7,22 @@ from _pydevd_frame_eval.pydevd_frame_tracing import pydev_trace_code_wrapper, up
 from _pydevd_frame_eval.pydevd_modify_bytecode import insert_code
 from pydevd_file_utils import get_abs_path_real_path_and_base_from_frame, NORM_PATHS_AND_BASE_CONTAINER
 
+AVOID_RECURSION = [
+    'pydevd_additional_thread_info_regular.py',
+    'threading.py',
+    '_weakrefset.py'
+]
 
-def get_breakpoints_for_file(filename, main_debugger):
-    breakpoints_for_file = main_debugger.breakpoints.get(filename)
-    return breakpoints_for_file
+get_file_type = DONT_TRACE.get
 
-cdef PyObject* get_bytecode_while_frame_eval(PyFrameObject *frame, int exc):
-    filepath = str(<object> frame.f_code.co_filename)
+cdef PyObject* get_bytecode_while_frame_eval(PyFrameObject *frame_obj, int exc):
+    frame = <object> frame_obj
+    filepath = frame.f_code.co_filename
     skip_file = False
     breakpoints = None
-    for file in DONT_TRACE.keys():
+
+    for file in AVOID_RECURSION:
+        # we can't call any other function without this check, because we can get stack overflow
         if filepath.endswith(file):
             skip_file = True
             break
@@ -30,44 +36,51 @@ cdef PyObject* get_bytecode_while_frame_eval(PyFrameObject *frame, int exc):
         except:
             additional_info = t.additional_info = PyDBAdditionalThreadInfo()
 
-        if not additional_info.is_tracing:
-            additional_info.is_tracing = True
-            try:
-                abs_path_real_path_and_base = NORM_PATHS_AND_BASE_CONTAINER[<object> frame.f_code.co_filename]
-            except:
-                abs_path_real_path_and_base = get_abs_path_real_path_and_base_from_frame(<object> frame)
+        if additional_info.is_tracing or getattr(t, 'pydev_do_not_trace', None):
+            return _PyEval_EvalFrameDefault(frame_obj, exc)
 
-            filepath = abs_path_real_path_and_base[1]
-            main_debugger = get_global_debugger()
-            breakpoints = get_breakpoints_for_file(filepath, main_debugger)
-            if breakpoints:
-                was_break = False
-                breakpoints_to_update = []
-                code_object = <object> frame.f_code
-                for offset, line in dis.findlinestarts(code_object):
-                    if line in breakpoints:
-                        breakpoint = breakpoints[line]
-                        if code_object not in breakpoint.code_objects:
-                            # This check is needed for generator functions, because after each yield the new frame is created
-                            # but the former code object is used
-                            breakpoints_to_update.append(breakpoint)
-                            new_code = insert_code(<object> frame.f_code, pydev_trace_code_wrapper.__code__, line)
-                            Py_INCREF(new_code)
-                            frame.f_code = <PyCodeObject *> new_code
-                            was_break = True
-                if was_break:
-                    update_globals_dict(<object> frame.f_globals)
-                    for bp in breakpoints_to_update:
-                        bp.code_objects.append(<object> frame.f_code)
-            else:
-                if main_debugger.has_plugin_line_breaks:
-                    can_not_skip = main_debugger.plugin.can_not_skip(main_debugger, None, <object>frame)
-                    if can_not_skip:
-                        main_debugger.SetTrace(main_debugger.trace_dispatch)
-                        main_debugger.set_trace_for_frame_and_parents(<object>frame)
+        additional_info.is_tracing = True
+        try:
+            abs_path_real_path_and_base = NORM_PATHS_AND_BASE_CONTAINER[frame.f_code.co_filename]
+        except:
+            abs_path_real_path_and_base = get_abs_path_real_path_and_base_from_frame(frame)
 
+        main_debugger = get_global_debugger()
+        file_type = get_file_type(abs_path_real_path_and_base[-1])  #we don't want to debug anything related to pydevd
+        if file_type is not None:
             additional_info.is_tracing = False
-    return _PyEval_EvalFrameDefault(frame, exc)
+            return _PyEval_EvalFrameDefault(frame_obj, exc)
+
+        main_debugger = get_global_debugger()
+        breakpoints = main_debugger.breakpoints.get(abs_path_real_path_and_base[1])
+        if breakpoints:
+            was_break = False
+            breakpoints_to_update = []
+            code_object = frame.f_code
+            for offset, line in dis.findlinestarts(code_object):
+                if line in breakpoints:
+                    breakpoint = breakpoints[line]
+                    if code_object not in breakpoint.code_objects:
+                        # This check is needed for generator functions, because after each yield the new frame is created
+                        # but the former code object is used
+                        breakpoints_to_update.append(breakpoint)
+                        new_code = insert_code(frame.f_code, pydev_trace_code_wrapper.__code__, line)
+                        Py_INCREF(new_code)
+                        frame_obj.f_code = <PyCodeObject *> new_code
+                        was_break = True
+            if was_break:
+                update_globals_dict(frame.f_globals)
+                for bp in breakpoints_to_update:
+                    bp.code_objects.append(frame.f_code)
+        else:
+            if main_debugger.has_plugin_line_breaks:
+                can_not_skip = main_debugger.plugin.can_not_skip(main_debugger, None, frame)
+                if can_not_skip:
+                    main_debugger.SetTrace(main_debugger.trace_dispatch)
+                    main_debugger.set_trace_for_frame_and_parents(frame)
+
+        additional_info.is_tracing = False
+    return _PyEval_EvalFrameDefault(frame_obj, exc)
 
 def frame_eval_func():
     cdef PyThreadState *state = PyThreadState_Get()
