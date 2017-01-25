62f8d4ce0b72802eeec4fe1dfb99e80e53f91bef
kskrygan <kirillskrygan@gmail.com>
2017-01-25 20:47:31 +0100

    Changes due to review

--- platform/platform-impl/src/com/intellij/openapi/options/newEditor/SettingsDialogFactory.java
+++ platform/platform-impl/src/com/intellij/openapi/options/newEditor/SettingsDialogFactory.java
@@ -19,6 +19,7 @@ import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.options.ConfigurableGroup;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.DialogWrapper;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
@@ -28,15 +29,15 @@ public class SettingsDialogFactory {
     return ServiceManager.getService(SettingsDialogFactory.class);
   }
 
-  public SettingsDialog create(Project project, String key, @NotNull Configurable configurable, boolean showApplyButton, boolean showResetButton){
+  public DialogWrapper create(Project project, String key, @NotNull Configurable configurable, boolean showApplyButton, boolean showResetButton){
     return new SettingsDialog(project, key, configurable, showApplyButton, showResetButton);
   }
 
-  public SettingsDialog create(@NotNull Component parent, String key, @NotNull Configurable configurable, boolean showApplyButton, boolean showResetButton){
+  public DialogWrapper create(@NotNull Component parent, String key, @NotNull Configurable configurable, boolean showApplyButton, boolean showResetButton){
     return new SettingsDialog(parent, key, configurable, showApplyButton, showResetButton);
   }
 
-  public SettingsDialog create(@NotNull Project project, @NotNull ConfigurableGroup[] groups, Configurable configurable, String filter){
+  public DialogWrapper create(@NotNull Project project, @NotNull ConfigurableGroup[] groups, Configurable configurable, String filter){
     return new SettingsDialog(project, groups, configurable, filter);
   }
 }
