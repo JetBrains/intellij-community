bf4e98784ba9a42c9c9a512f2342d67eee17ea4c
Alexey Kudravtsev <cdr@intellij.com>
2017-01-27 17:41:57 +0300

    renamed to match field

--- platform/core-impl/src/com/intellij/psi/impl/PsiDocumentManagerBase.java
+++ platform/core-impl/src/com/intellij/psi/impl/PsiDocumentManagerBase.java
@@ -64,7 +64,7 @@ import java.util.concurrent.ConcurrentMap;
 public abstract class PsiDocumentManagerBase extends PsiDocumentManager implements DocumentListener, ProjectComponent {
   static final Logger LOG = Logger.getInstance("#com.intellij.psi.impl.PsiDocumentManagerImpl");
   private static final Key<Document> HARD_REF_TO_DOCUMENT = Key.create("HARD_REFERENCE_TO_DOCUMENT");
-  private final Key<PsiFile> HARD_REF_TO_PSI = Key.create("HARD_REFERENCE_TO_PSI"); // has to be different for each project to avoid mixups
+  private final Key<PsiFile> HARD_REF_TO_PSI = Key.create("HARD_REF_TO_PSI"); // has to be different for each project to avoid mixups
   private static final Key<List<Runnable>> ACTION_AFTER_COMMIT = Key.create("ACTION_AFTER_COMMIT");
 
   protected final Project myProject;
