1ebeac31f1b475357b7e551fb56b3425f52fbcd5
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2017-01-27 23:54:51 +0300

    Use types.FunctionType from typeshed to resolve function attributes

--- python/psi-api/src/com/jetbrains/python/PyNames.java
+++ python/psi-api/src/com/jetbrains/python/PyNames.java
@@ -91,8 +91,9 @@ public class PyNames {
   public static final String FALSE = "False";
 
   public static final String FAKE_OLD_BASE = "___Classobj";
-  public static final String FAKE_FUNCTION = "__function";
-  public static final String FAKE_METHOD = "__method";
+
+  public static final String TYPES_FUNCTION_TYPE = "types.FunctionType";
+  public static final String TYPES_METHOD_TYPE = "types.UnboundMethodType";
 
   public static final String FUTURE_MODULE = "__future__";
   public static final String UNICODE_LITERALS = "unicode_literals";
