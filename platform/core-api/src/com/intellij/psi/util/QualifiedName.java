83145bf8b11dae9c7602e4657ffd04349bf2d1ec
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2017-01-27 23:54:13 +0300

    Don't allow dots in components of QualifiedNames
    Dots are separators between components, not a part of components
    themselves.
    
    (cherry picked from commit 341bc86)

--- platform/core-api/src/com/intellij/psi/util/QualifiedName.java
+++ platform/core-api/src/com/intellij/psi/util/QualifiedName.java
@@ -39,6 +39,9 @@ public class QualifiedName implements Comparable<QualifiedName> {
   }
 
   public static QualifiedName fromComponents(Collection<String> components) {
+    for (String component : components) {
+      assertNoDots(component);
+    }
     QualifiedName qName = new QualifiedName(components.size());
     qName.myComponents.addAll(components);
     return qName;
@@ -46,6 +49,9 @@ public class QualifiedName implements Comparable<QualifiedName> {
 
   @NotNull
   public static QualifiedName fromComponents(String... components) {
+    for (String component : components) {
+      assertNoDots(component);
+    }
     QualifiedName result = new QualifiedName(components.length);
     Collections.addAll(result.myComponents, components);
     return result;
@@ -209,5 +215,11 @@ public class QualifiedName implements Comparable<QualifiedName> {
   @Override
   public int compareTo(@NotNull QualifiedName other) {
     return toString().compareTo(other.toString());
+  }
+
+  private static void assertNoDots(@NotNull String component) {
+    if (component.contains(".")) {
+      throw new IllegalArgumentException("Components of QualifiedName cannot contain dots inside them");
+    }
   }
 }
