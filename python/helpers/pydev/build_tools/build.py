e49a20b565f58257330cf9fca4b0d063784d36a6
Elizaveta Shashkova <Elizaveta.Shashkova@jetbrains.com>
2017-01-29 18:04:51 +0300

    Merge scripts for building cython speedups and frame evaluation speedups

--- python/helpers/pydev/build_tools/build.py
+++ python/helpers/pydev/build_tools/build.py
@@ -142,6 +142,8 @@ def build():
     for arg in sys.argv:
         if arg.startswith('--target-pyd-name='):
             additional_args.append(arg)
+        if arg.startswith('--target-pyd-frame-eval='):
+            additional_args.append(arg)
             break
     else:
         additional_args.append('--force-cython')  # Build always forces cython!
