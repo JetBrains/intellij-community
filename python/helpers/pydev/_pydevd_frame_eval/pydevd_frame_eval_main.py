9735f3aa62385e88be6f2a2f5e1b0dce92f88ec5
Elizaveta Shashkova <elizaveta.shashkova@jetbrains.com>
2017-01-29 18:23:22 +0300

    Add cython wrapper for correct importing frame evaluation extensions on Windows
    (cherry picked from commit 7d81082)

--- python/helpers/pydev/_pydevd_frame_eval/pydevd_frame_eval_main.py
+++ python/helpers/pydev/_pydevd_frame_eval/pydevd_frame_eval_main.py
@@ -16,10 +16,10 @@ if use_frame_eval == 'NO':
 else:
     if IS_PY36_OR_OLDER:
         try:
-            from _pydevd_frame_eval.pydevd_frame_evaluator import frame_eval_func, stop_frame_eval
+            from _pydevd_frame_eval.pydevd_frame_eval_cython_wrapper import frame_eval_func, stop_frame_eval
         except ImportError:
             from _pydev_bundle.pydev_monkey import log_error_once
 
-            dirname = os.path.dirname(__file__)
-            log_error_once("warning: Debugger speedups for Python 3.6 not found. Run '\"%s\" \"%s\" build_ext --inplace' to build." % (
-                sys.executable, os.path.join(dirname, 'setup.py')))
+            dirname = os.path.dirname(os.path.dirname(__file__))
+            log_error_once("warning: Debugger speedups using cython not found. Run '\"%s\" \"%s\" build_ext --inplace' to build." % (
+                sys.executable, os.path.join(dirname, 'setup_cython.py')))
