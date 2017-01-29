15d8b345b6344fd81bbb8f982df6ad1f082c9c15
Elizaveta Shashkova <Elizaveta.Shashkova@jetbrains.com>
2017-01-29 16:13:27 +0300

    Update scripts for building windows binaries for python 3.6

--- python/helpers/pydev/build_tools/build_binaries_windows.py
+++ python/helpers/pydev/build_tools/build_binaries_windows.py
@@ -67,10 +67,12 @@ python_installations = [
     r'%s\py27_32\python.exe' % miniconda32_envs,
     r'%s\py34_32\python.exe' % miniconda32_envs,
     r'%s\py35_32\python.exe' % miniconda32_envs,
+    r'%s\py36_32\python.exe' % miniconda32_envs,
 
     r'%s\py27_64\python.exe' % miniconda64_envs,
     r'%s\py34_64\python.exe' % miniconda64_envs,
     r'%s\py35_64\python.exe' % miniconda64_envs,
+    r'%s\py36_64\python.exe' % miniconda64_envs,
 ]
 
 root_dir = os.path.dirname(os.path.dirname(__file__))
