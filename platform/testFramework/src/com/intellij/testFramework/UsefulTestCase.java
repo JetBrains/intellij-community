c2553bed71aa0851446636656e024b837507e3b3
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2017-02-01 19:13:35 +0100

    [test framework] drops invalid contract annotation (IDEA-CR-17781)

--- platform/testFramework/src/com/intellij/testFramework/UsefulTestCase.java
+++ platform/testFramework/src/com/intellij/testFramework/UsefulTestCase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -663,7 +663,6 @@ public abstract class UsefulTestCase extends TestCase {
   }
 
   @SafeVarargs
-  @Contract("null, _ -> fail")
   public static <T> void assertOneOf(T value, @NotNull T... values) {
     boolean found = false;
     for (T v : values) {
