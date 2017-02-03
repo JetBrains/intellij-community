7bd590e5ff782bd8c49bbda343ecfc3184a86748
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2017-02-03 15:49:15 +0100

    IDEA-166321 if path is a regular file — it is valid project for reopen

--- platform/platform-impl/src/com/intellij/ide/RecentProjectsManagerBase.java
+++ platform/platform-impl/src/com/intellij/ide/RecentProjectsManagerBase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -614,7 +614,8 @@ public abstract class RecentProjectsManagerBase extends RecentProjectsManager im
       try {
         myBatchOpening = true;
         for (String openPath : openPaths) {
-          if (ProjectKt.isValidProjectPath(openPath)) {
+          // https://youtrack.jetbrains.com/issue/IDEA-166321
+          if (ProjectKt.isValidProjectPath(openPath, true)) {
             doOpenProject(openPath, null, forceNewFrame);
           }
         }
