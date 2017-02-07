dffe6aca5a8eb8590760b6f2f16d3ee8223a38f5
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2017-02-07 19:53:13 +0300

    [vcs-log] try to fix a flaky test
    In the test it is checked that the first log read reads only first few commits (in order to show them quickly). But also readFirstBlock sends RELOAD_ALL request, which is processed immediately, since we call readFirstBlock outside of SingleTaskController. It seems that sometimes we are able to reload in background and set a new (full) data pack before returning from readFirstBlock method. Because of this full data pack is returned. It would not be a problem in production (it is good to return all the data immediately) but since we want to do a proper test here, we block full log loading temporarily.

--- platform/vcs-log/impl/test/com/intellij/vcs/log/data/VcsLogRefresherTest.java
+++ platform/vcs-log/impl/test/com/intellij/vcs/log/data/VcsLogRefresherTest.java
@@ -95,7 +95,9 @@ public class VcsLogRefresherTest extends VcsPlatformTest {
   }
 
   public void test_initialize_shows_short_history() throws InterruptedException, ExecutionException, TimeoutException {
+    myLogProvider.blockFullLog();
     DataPack result = myLoader.readFirstBlock();
+    myLogProvider.unblockFullLog();
     assertNotNull(result);
     assertDataPack(log(myCommits.subList(0, 2)), result.getPermanentGraph().getAllCommits());
     waitForBackgroundTasksToComplete();
