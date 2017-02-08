160b2b4d30dcaa826113e8a4f313b28b2305603b
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2017-02-08 18:37:38 +0300

    [vcs-log] fix NPE when copying table information

--- platform/vcs-log/impl/src/com/intellij/vcs/log/ui/table/VcsLogGraphTable.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/ui/table/VcsLogGraphTable.java
@@ -338,7 +338,7 @@ public class VcsLogGraphTable extends TableWithProgress implements DataProvider,
     int[] selectedRows = getSelectedRows();
     for (int i = 0; i < Math.min(VcsLogUtil.MAX_SELECTED_COMMITS, selectedRows.length); i++) {
       int row = selectedRows[i];
-      for (int j = GraphTableModel.ROOT_COLUMN + 1; j < getModel().getRowCount(); j++) {
+      for (int j = GraphTableModel.ROOT_COLUMN + 1; j < getModel().getColumnCount(); j++) {
         sb.append(getModel().getValueAt(row, j).toString());
         if (j < getModel().getRowCount() - 1) sb.append(" ");
       }
