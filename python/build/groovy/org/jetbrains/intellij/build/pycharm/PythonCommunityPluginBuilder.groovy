773da219eecf26a65c074befee2b2b37599b55a4
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2017-02-09 19:22:24 +0300

    Use newer build API for Python Community plugin

---
+++ python/build/groovy/org/jetbrains/intellij/build/pycharm/PythonCommunityPluginBuilder.groovy
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2000-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jetbrains.intellij.build.pycharm
+
+import org.codehaus.gant.GantBinding
+import org.jetbrains.intellij.build.BuildContext
+import org.jetbrains.intellij.build.BuildOptions
+import org.jetbrains.intellij.build.BuildTasks
+import org.jetbrains.intellij.build.ProprietaryBuildTools
+
+/**
+ * @author vlan
+ */
+class PythonCommunityPluginBuilder {
+  private final GantBinding binding
+  private final String home
+
+  PythonCommunityPluginBuilder(String home, GantBinding binding) {
+    this.home = home
+    this.binding = binding
+  }
+
+  def build() {
+    def pluginBuildNumber = System.getProperty("build.number", "SNAPSHOT")
+    def options = new BuildOptions(targetOS: BuildOptions.OS_NONE, buildNumber: pluginBuildNumber)
+    def buildContext = BuildContext.createContext(binding.ant, binding.projectBuilder, binding.project, binding.global,
+                                                  home, home, new PythonCommunityPluginProperties(pluginBuildNumber),
+                                                  ProprietaryBuildTools.DUMMY, options)
+    def buildTasks = BuildTasks.create(buildContext)
+    buildTasks.buildDistributions()
+  }
+}
---
+++ python/build/groovy/org/jetbrains/intellij/build/pycharm/PythonPluginPropertiesBase.groovy
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2000-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jetbrains.intellij.build.pycharm
+
+import org.jetbrains.intellij.build.ApplicationInfoProperties
+import org.jetbrains.intellij.build.LinuxDistributionCustomizer
+import org.jetbrains.intellij.build.MacDistributionCustomizer
+import org.jetbrains.intellij.build.WindowsDistributionCustomizer
+import org.jetbrains.intellij.build.impl.PluginLayout
+
+/**
+ * @author vlan
+ */
+abstract class PythonPluginPropertiesBase extends PyCharmPropertiesBase {
+  List<String> communityModules = [
+    "IntelliLang-python",
+    "ipnb",
+    "python-openapi",
+    "python-community-plugin-core",
+    "python-community-plugin-java",
+    "python-psi-api",
+    "python-pydev",
+    "python-community",
+  ]
+
+  String pythonCommunityPluginModule = "python-community-plugin-resources"
+
+  PythonPluginPropertiesBase() {
+    super()
+  }
+
+  PluginLayout pythonCommunityPluginLayout(String pluginVersion) {
+    pythonPlugin(pythonCommunityPluginModule, "python-ce", "python-community-plugin-build-patches",
+                 communityModules, pluginVersion)
+  }
+
+  static PluginLayout pythonPlugin(String mainModuleName, String name, String buildPatchesModule, List<String> modules,
+                                   String pluginVersion, @DelegatesTo(PluginLayout.PluginLayoutSpec) Closure body = {}) {
+    return PluginLayout.plugin(mainModuleName) {
+      directoryName = name
+      mainJarName = "${name}.jar"
+      version = pluginVersion
+      modules.each { module ->
+        withModule(module, mainJarName, false)
+        excludeFromModule(module, "**/plugin.xml")
+        excludeFromModule(module, "**/python-plugin-dependencies.xml")
+      }
+      withModule(buildPatchesModule, mainJarName, false)
+      withResourceFromModule("python-helpers", "", "helpers")
+      doNotCreateSeparateJarForLocalizableResources()
+      body.delegate = delegate
+      body()
+    }
+  }
+
+  @Override
+  String getBaseArtifactName(ApplicationInfoProperties applicationInfo, String buildNumber) {
+    return null
+  }
+
+  @Override
+  WindowsDistributionCustomizer createWindowsCustomizer(String projectHome) {
+    return null
+  }
+
+  @Override
+  LinuxDistributionCustomizer createLinuxCustomizer(String projectHome) {
+    return null
+  }
+
+  @Override
+  MacDistributionCustomizer createMacCustomizer(String projectHome) {
+    return null
+  }
+}
