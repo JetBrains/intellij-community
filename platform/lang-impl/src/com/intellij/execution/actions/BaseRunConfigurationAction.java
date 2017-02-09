417535e82cbef18046a857823096d9d58e41b345
Alexander Zolotov <alexander.zolotov@jetbrains.com>
2017-02-09 15:07:49 +0300

    Show configuration name instead of configurationType name while showing popup with a configuration to run
    if several run configuration producers produced two different configuration
    of the same type, then user should be able to distinguish them. It's
    impossible by using configuration type name

--- platform/lang-impl/src/com/intellij/execution/actions/BaseRunConfigurationAction.java
+++ platform/lang-impl/src/com/intellij/execution/actions/BaseRunConfigurationAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -140,7 +140,7 @@ public abstract class BaseRunConfigurationAction extends ActionGroup {
             @Override
             @NotNull
             public String getTextFor(final ConfigurationFromContext producer) {
-              return producer.getConfigurationType().getDisplayName();
+              return StringUtil.notNullize(producer.getConfiguration().getName(), producer.getConfigurationType().getDisplayName());
             }
 
             @Override
