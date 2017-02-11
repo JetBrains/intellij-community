JetBrains/intellij-community

commit 905fdebb979bba991ed086685acc3fffc3484aca

git: rebase: make rebase dialog reuse the existing common rebase process
This fixes: IDEA-81093, IDEA-55479, IDEA-55672, IDEA-76778, IDEA-55672

plugins/git4idea/src/git4idea/rebase/GitRebaseDialog.java
@@ -26,14 +26,14 @@ import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.DocumentAdapter;
 import git4idea.*;
 import git4idea.branch.GitBranchUtil;
-import git4idea.commands.GitCommand;
-import git4idea.commands.GitLineHandler;
+import git4idea.branch.GitRebaseParams;
 import git4idea.config.GitConfigUtil;
 import git4idea.config.GitRebaseSettings;
 import git4idea.i18n.GitBundle;
 import git4idea.merge.GitMergeUtil;
 import git4idea.repo.GitRemote;
 import git4idea.repo.GitRepository;
+import git4idea.repo.GitRepositoryManager;
 import git4idea.ui.GitReferenceValidator;
 import git4idea.util.GitUIUtil;
 import org.jetbrains.annotations.NotNull;
@@ -46,12 +46,17 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.intellij.openapi.util.text.StringUtil.isEmptyOrSpaces;
+import static com.intellij.util.ObjectUtils.assertNotNull;
+
 /**
  * The dialog that allows initiating git rebase activity
  */
 public class GitRebaseDialog extends DialogWrapper {
 
   private static final Logger LOG = Logger.getInstance(GitRebaseDialog.class);
+
+  @NotNull private final GitRepositoryManager myRepositoryManager;
 
   /**
    * Git root selector
@@ -151,6 +156,7 @@ public class GitRebaseDialog extends DialogWrapper {
     init();
     myProject = project;
     mySettings = ServiceManager.getService(myProject, GitRebaseSettings.class);
+    myRepositoryManager = GitUtil.getRepositoryManager(myProject);
     final Runnable validateRunnable = new Runnable() {
       public void run() {
         validateFields();
@@ -208,39 +214,6 @@ public class GitRebaseDialog extends DialogWrapper {
       LOG.debug(e);
       return false;
     }
-  }
-
-  public GitLineHandler handler() {
-    GitLineHandler h = new GitLineHandler(myProject, gitRoot(), GitCommand.REBASE);
-    h.setStdoutSuppressed(false);
-    if (myInteractiveCheckBox.isSelected() && myInteractiveCheckBox.isEnabled()) {
-      h.addParameters("-i");
-    }
-    h.addParameters("-v");
-    if (!myDoNotUseMergeCheckBox.isSelected()) {
-      if (myMergeStrategyComboBox.getSelectedItem().equals(GitMergeUtil.DEFAULT_STRATEGY)) {
-        h.addParameters("-m");
-      }
-      else {
-        h.addParameters("-s", myMergeStrategyComboBox.getSelectedItem().toString());
-      }
-    }
-    if (myPreserveMergesCheckBox.isSelected()) {
-      h.addParameters("-p");
-    }
-    String from = GitUIUtil.getTextField(myFromComboBox).getText();
-    String onto = GitUIUtil.getTextField(myOntoComboBox).getText();
-    if (from.length() == 0) {
-      h.addParameters(onto);
-    }
-    else {
-      h.addParameters("--onto", onto, from);
-    }
-    final String selectedBranch = (String)myBranchComboBox.getSelectedItem();
-    if (myCurrentBranch != null && !myCurrentBranch.getName().equals(selectedBranch)) {
-      h.addParameters(selectedBranch);
-    }
-    return h;
   }
 
   @Override
@@ -457,6 +430,31 @@ public class GitRebaseDialog extends DialogWrapper {
     return (VirtualFile)myGitRootComboBox.getSelectedItem();
   }
 
+  @NotNull
+  public GitRepository getSelectedRepository() {
+    return assertNotNull(myRepositoryManager.getRepositoryForRoot(gitRoot()));
+  }
+
+  @NotNull
+  public GitRebaseParams getSelectedParams() {
+    String selectedBranch = (String)myBranchComboBox.getSelectedItem();
+    String branch = myCurrentBranch != null && !myCurrentBranch.getName().equals(selectedBranch) ? selectedBranch : null;
+
+    String from = GitUIUtil.getTextField(myFromComboBox).getText();
+    String onto = GitUIUtil.getTextField(myOntoComboBox).getText();
+    String upstream;
+    String newBase;
+    if (isEmptyOrSpaces(from)) {
+      upstream = onto;
+      newBase = null;
+    }
+    else {
+      upstream = from;
+      newBase = onto;
+    }
+
+    return new GitRebaseParams(branch, newBase, upstream, myInteractiveCheckBox.isSelected(), myPreserveMergesCheckBox.isSelected());
+  }
 
   /**
    * {@inheritDoc}

platform/dvcs-impl/src/com/intellij/dvcs/DvcsUtil.java
@@ -444,7 +444,7 @@ public class DvcsUtil {
   }
 
   @Nullable
-  private static VirtualFile guessVcsRoot(@NotNull Project project, @Nullable VirtualFile file) {
+  public static VirtualFile guessVcsRoot(@NotNull Project project, @Nullable VirtualFile file) {
     VirtualFile root = null;
     if (file != null) {
       root = ProjectLevelVcsManager.getInstance(project).getVcsRootFor(file);

plugins/git4idea/src/git4idea/actions/GitAbstractRebaseAction.java
@@ -44,7 +44,7 @@ abstract class GitAbstractRebaseAction extends DumbAwareAction {
     super.update(e);
     Project project = e.getProject();
     if (project == null || !hasGitRepositories(project)) {
-      e.getPresentation().setVisible(false);
+      e.getPresentation().setEnabledAndVisible(false);
     }
     else {
       e.getPresentation().setEnabledAndVisible(hasRebaseInProgress(project));

plugins/git4idea/src/git4idea/actions/GitRebase.java
@@ -15,38 +15,58 @@
  */
 package git4idea.actions;
 
+import com.intellij.dvcs.DvcsUtil;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
+import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import git4idea.commands.GitLineHandler;
-import git4idea.i18n.GitBundle;
+import com.intellij.util.containers.ContainerUtil;
 import git4idea.rebase.GitRebaseDialog;
+import git4idea.rebase.GitRebaseUtils;
+import git4idea.repo.GitRepository;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Git rebase action
- */
-public class GitRebase extends GitRebaseActionBase {
+import static com.intellij.dvcs.DvcsUtil.sortRepositories;
+import static git4idea.GitUtil.*;
+import static git4idea.rebase.GitRebaseUtils.getRebasingRepositories;
+import static java.util.Collections.singletonList;
 
-  /**
-   * {@inheritDoc}
-   */
-  @NotNull
-  protected String getActionName() {
-    return GitBundle.getString("rebase.action.name");
+public class GitRebase extends DumbAwareAction {
+
+  @Override
+  public void update(@NotNull AnActionEvent e) {
+    super.update(e);
+    Project project = e.getProject();
+    if (project == null || !hasGitRepositories(project)) {
+      e.getPresentation().setEnabledAndVisible(false);
+    }
+    else {
+      e.getPresentation().setVisible(true);
+      e.getPresentation().setEnabled(getRebasingRepositories(project).size() < getRepositories(project).size());
+    }
   }
 
-  /**
-   * {@inheritDoc}
-   */
-  @Nullable
-  protected GitLineHandler createHandler(Project project, List<VirtualFile> gitRoots, VirtualFile defaultRoot) {
-    GitRebaseDialog dialog = new GitRebaseDialog(project, gitRoots, defaultRoot);
-    if (!dialog.showAndGet()) {
-      return null;
+  @Override
+  public void actionPerformed(@NotNull AnActionEvent e) {
+    final Project project = e.getRequiredData(CommonDataKeys.PROJECT);
+    ArrayList<GitRepository> repositories = ContainerUtil.newArrayList(getRepositories(project));
+    repositories.removeAll(getRebasingRepositories(project));
+    List<VirtualFile> roots = ContainerUtil.newArrayList(getRootsFromRepositories(sortRepositories(repositories)));
+    VirtualFile defaultRoot = DvcsUtil.guessVcsRoot(project, e.getData(CommonDataKeys.VIRTUAL_FILE));
+    final GitRebaseDialog dialog = new GitRebaseDialog(project, roots, defaultRoot);
+    if (dialog.showAndGet()) {
+      ProgressManager.getInstance().run(new Task.Backgroundable(project, "Rebasing...") {
+        public void run(@NotNull ProgressIndicator indicator) {
+          GitRebaseUtils.rebase(project, singletonList(dialog.getSelectedRepository()), dialog.getSelectedParams(), indicator);
+        }
+      });
     }
-    return dialog.handler();
   }
 }

plugins/git4idea/src/git4idea/actions/GitRebaseActionBase.java
@@ -1,154 +1,0 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package git4idea.actions;
-
-import com.intellij.dvcs.DvcsUtil;
-import com.intellij.openapi.application.AccessToken;
-import com.intellij.openapi.components.ServiceManager;
-import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.progress.Task;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.VcsException;
-import com.intellij.openapi.vcs.VcsNotifier;
-import com.intellij.openapi.vfs.VfsUtil;
-import com.intellij.openapi.vfs.VirtualFile;
-import git4idea.GitUtil;
-import git4idea.commands.Git;
-import git4idea.commands.GitCommandResult;
-import git4idea.commands.GitLineHandler;
-import git4idea.i18n.GitBundle;
-import git4idea.rebase.GitInteractiveRebaseEditorHandler;
-import git4idea.rebase.GitRebaseEditorService;
-import git4idea.rebase.GitRebaseLineListener;
-import git4idea.repo.GitRepositoryManager;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
-import java.util.Set;
-
-/**
- * The base class for rebase actions that use editor
- */
-public abstract class GitRebaseActionBase extends GitRepositoryAction {
-  /**
-   * {@inheritDoc}
-   */
-  protected void perform(@NotNull final Project project,
-                         @NotNull final List<VirtualFile> gitRoots,
-                         @NotNull final VirtualFile defaultRoot,
-                         final Set<VirtualFile> affectedRoots,
-                         final List<VcsException> exceptions) throws VcsException {
-    final GitLineHandler h = createHandler(project, gitRoots, defaultRoot);
-    if (h == null) {
-      return;
-    }
-    final VirtualFile root = h.workingDirectoryFile();
-    GitRebaseEditorService service = GitRebaseEditorService.getInstance();
-    final GitInteractiveRebaseEditorHandler editor = new GitInteractiveRebaseEditorHandler(service, project, root, h);
-    final GitRebaseLineListener resultListener = new GitRebaseLineListener();
-    h.addLineListener(resultListener);
-    configureEditor(editor);
-    affectedRoots.add(root);
-
-    service.configureHandler(h, editor.getHandlerNo());
-    new Task.Backgroundable(project, GitBundle.getString("rebasing.title"), true) {
-      @Override
-      public void run(@NotNull ProgressIndicator indicator) {
-        AccessToken token = DvcsUtil.workingTreeChangeStarted(project);
-        try {
-          GitCommandResult result = ServiceManager.getService(Git.class).runCommand(h);
-          editor.close();
-          GitRepositoryManager manager = GitUtil.getRepositoryManager(project);
-          manager.updateRepository(root);
-          VfsUtil.markDirtyAndRefresh(false, true, false, root);
-          notifyAboutResult(result, resultListener, editor.wasNoopSituationDetected(), exceptions, project);
-        }
-        finally {
-          DvcsUtil.workingTreeChangeFinished(project, token);
-        }
-      }
-    }.queue();
-  }
-
-  private static void notifyAboutResult(@NotNull GitCommandResult commandResult,
-                                        @NotNull GitRebaseLineListener resultListener,
-                                        boolean noopSituation,
-                                        @NotNull List<VcsException> exceptions,
-                                        @NotNull Project project) {
-    final GitRebaseLineListener.Result result = resultListener.getResult();
-    String messageId;
-    String message = null;
-    boolean isError = true;
-    switch (result.status) {
-      case CONFLICT:
-        messageId = "rebase.result.conflict";
-        break;
-      case ERROR:
-        messageId = "rebase.result.error";
-        message = commandResult.getErrorOutputAsHtmlString();
-        break;
-      case CANCELLED:
-        // we do not need to show a message if editing was cancelled.
-        exceptions.clear();
-        return;
-      case EDIT:
-        isError = false;
-        messageId = "rebase.result.amend";
-        break;
-      case FINISHED:
-        isError = false;
-        messageId = "rebase.result.success";
-        if (noopSituation) {
-          message = "Current branch was reset to the base branch";
-        }
-        break;
-      default:
-        throw new IllegalStateException("Unsupported rebase result: " + result.status);
-    }
-
-    String title = GitBundle.message(messageId + ".title");
-    if (message == null) {
-      message = GitBundle.message(messageId, result.current, result.total);
-    }
-
-    if (isError) {
-      VcsNotifier.getInstance(project).notifyError(title, message);
-    }
-    else {
-      VcsNotifier.getInstance(project).notifySuccess(title, message);
-    }
-  }
-
-  /**
-   * This method could be overridden to supply additional information to the editor.
-   *
-   * @param editor the editor to configure
-   */
-  protected void configureEditor(GitInteractiveRebaseEditorHandler editor) {
-  }
-
-  /**
-   * Create line handler that represents a git operation
-   *
-   * @param project     the context project
-   * @param gitRoots    the git roots
-   * @param defaultRoot the default root
-   * @return the line handler or null
-   */
-  @Nullable
-  protected abstract GitLineHandler createHandler(Project project, List<VirtualFile> gitRoots, VirtualFile defaultRoot);
-}

plugins/git4idea/src/git4idea/branch/GitRebaseParams.java
@@ -18,31 +18,71 @@ package git4idea.branch;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
+import static com.intellij.openapi.util.text.StringUtil.nullize;
+import static java.util.Arrays.asList;
+
 public class GitRebaseParams {
 
-  @NotNull private final String myNewBase;
+  @Nullable private final String myBranch;
+  @Nullable private final String myNewBase;
+  @NotNull private final String myUpstream;
+  private final boolean myInteractive;
+  private final boolean myPreserveMerges;
 
-  public GitRebaseParams(@NotNull String newBase) {
-    myNewBase = newBase;
+  public GitRebaseParams(@NotNull String upstream) {
+    this(null, null, upstream, false, false);
+  }
+  public GitRebaseParams(@Nullable String branch,
+                         @Nullable String newBase,
+                         @NotNull String upstream,
+                         boolean interactive,
+                         boolean preserveMerges) {
+    myBranch = nullize(branch, true);
+    myNewBase = nullize(newBase, true);
+    myUpstream = upstream;
+    myInteractive = interactive;
+    myPreserveMerges = preserveMerges;
   }
 
   @NotNull
   public List<String> asCommandLineArguments() {
     List<String> args = ContainerUtil.newArrayList();
-    args.add(myNewBase);
+    if (myInteractive) {
+      args.add("--interactive");
+    }
+    if (myPreserveMerges) {
+      args.add("--preserve-merges");
+    }
+    if (myNewBase != null) {
+      args.addAll(asList("--onto", myNewBase));
+    }
+    args.add(myUpstream);
+    if (myBranch != null) {
+      args.add(myBranch);
+    }
     return args;
   }
 
-  @NotNull
+  @Nullable
   public String getNewBase() {
     return myNewBase;
+  }
+
+  @NotNull
+  public String getUpstream() {
+    return myUpstream;
   }
 
   @Override
   public String toString() {
     return StringUtil.join(asCommandLineArguments(), " ");
+  }
+
+  public boolean isInteractive() {
+    return myInteractive;
   }
 }

plugins/git4idea/src/git4idea/commands/GitImpl.java
@@ -15,6 +15,7 @@
  */
 package git4idea.commands;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Computable;
@@ -567,10 +568,12 @@ public class GitImpl implements Git {
   public GitCommandResult rebase(@NotNull GitRepository repository,
                                  @NotNull GitRebaseParams parameters,
                                  @NotNull GitLineHandlerListener... listeners) {
-    GitLineHandler handler = new GitLineHandler(repository.getProject(), repository.getRoot(), GitCommand.REBASE);
+    Project project = repository.getProject();
+    VirtualFile root = repository.getRoot();
+    GitLineHandler handler = new GitLineHandler(project, root, GitCommand.REBASE);
     handler.addParameters(parameters.asCommandLineArguments());
     addListeners(handler, listeners);
-    return run(handler);
+    return parameters.isInteractive() ? runWithEditor(project, root, handler, true) : run(handler);
   }
 
   @NotNull
@@ -595,9 +598,9 @@ public class GitImpl implements Git {
   }
 
   @NotNull
-  private static GitCommandResult rebaseResume(@NotNull GitRepository repository,
-                                               @NotNull GitRebaseResumeMode rebaseMode,
-                                               @NotNull GitLineHandlerListener[] listeners) {
+  private GitCommandResult rebaseResume(@NotNull GitRepository repository,
+                                        @NotNull GitRebaseResumeMode rebaseMode,
+                                        @NotNull GitLineHandlerListener[] listeners) {
     Project project = repository.getProject();
     VirtualFile root = repository.getRoot();
     GitLineHandler handler = new GitLineHandler(project, root, GitCommand.REBASE);
@@ -607,10 +610,10 @@ public class GitImpl implements Git {
   }
 
   @NotNull
-  private static GitCommandResult runWithEditor(@NotNull Project project,
-                                                @NotNull VirtualFile root,
-                                                @NotNull GitLineHandler handler,
-                                                boolean commitListAware) {
+  private GitCommandResult runWithEditor(@NotNull Project project,
+                                         @NotNull VirtualFile root,
+                                         @NotNull GitLineHandler handler,
+                                         boolean commitListAware) {
     GitInteractiveRebaseEditorHandler editor = configureEditor(project, root, handler, commitListAware);
     try {
       return run(handler);
@@ -620,11 +623,12 @@ public class GitImpl implements Git {
     }
   }
 
+  @VisibleForTesting
   @NotNull
-  private static GitInteractiveRebaseEditorHandler configureEditor(@NotNull Project project,
-                                                                   @NotNull VirtualFile root,
-                                                                   @NotNull GitLineHandler handler,
-                                                                   boolean commitListAware) {
+  protected GitInteractiveRebaseEditorHandler configureEditor(@NotNull Project project,
+                                                              @NotNull VirtualFile root,
+                                                              @NotNull GitLineHandler handler,
+                                                              boolean commitListAware) {
     GitRebaseEditorService service = GitRebaseEditorService.getInstance();
     GitInteractiveRebaseEditorHandler editor = new GitInteractiveRebaseEditorHandler(service, project, root, handler);
     if (!commitListAware) {

plugins/git4idea/src/git4idea/rebase/GitRebaseProblemDetector.java
@@ -42,10 +42,12 @@ public class GitRebaseProblemDetector extends GitLineHandlerAdapter {
     "you have unstaged changes",
     "your index contains uncommitted changes"
   };
+  private static final String STOPPED_FOR_EDITING = "You can amend the commit now";
 
   private volatile boolean myMergeConflict;
   private volatile boolean myNoChangeError;
   private volatile boolean myDirtyTree;
+  private volatile boolean myStoppedForEditing;
 
   public boolean isNoChangeError() {
     return myNoChangeError;
@@ -57,6 +59,10 @@ public class GitRebaseProblemDetector extends GitLineHandlerAdapter {
 
   public boolean isDirtyTree() {
     return myDirtyTree;
+  }
+
+  public boolean hasStoppedForEditing() {
+    return myStoppedForEditing;
   }
 
   @Override
@@ -78,6 +84,10 @@ public class GitRebaseProblemDetector extends GitLineHandlerAdapter {
         myDirtyTree = true;
         return;
       }
+    }
+
+    if (StringUtil.containsIgnoreCase(line, STOPPED_FOR_EDITING)) {
+      myStoppedForEditing = true;
     }
   }
 }

commit 0853871297e621847d1cb048eb214fe5202ab56e

Remove merge strategies selector from Git rebase dialog
Merge strategy seems to be a very advanced option, that it rarely used
and just clutters the UI of the rebase dialog.

Moreover, it was actually broken back in 905fdeb and nobody cared,
so it seems to be actually never used.

plugins/git4idea/src/git4idea/rebase/GitRebaseDialog.form
@@ -1,9 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <form xmlns="http://www.intellij.com/uidesigner/form/" version="1" bind-to-class="git4idea.rebase.GitRebaseDialog">
-  <grid id="27dc6" binding="myPanel" layout-manager="GridLayoutManager" row-count="8" column-count="3" same-size-horizontally="false" same-size-vertically="false" hgap="-1" vgap="-1">
+  <grid id="27dc6" binding="myPanel" layout-manager="GridLayoutManager" row-count="7" column-count="3" same-size-horizontally="false" same-size-vertically="false" hgap="-1" vgap="-1">
     <margin top="0" left="0" bottom="0" right="0"/>
     <constraints>
-      <xy x="20" y="20" width="583" height="255"/>
+      <xy x="20" y="20" width="583" height="263"/>
     </constraints>
     <properties/>
     <border type="none"/>
@@ -19,7 +19,7 @@
       </component>
       <vspacer id="67a13">
         <constraints>
-          <grid row="7" column="0" row-span="1" col-span="1" vsize-policy="6" hsize-policy="1" anchor="0" fill="2" indent="0" use-parent-layout="false"/>
+          <grid row="6" column="0" row-span="1" col-span="1" vsize-policy="6" hsize-policy="1" anchor="0" fill="2" indent="0" use-parent-layout="false"/>
         </constraints>
       </vspacer>
       <component id="bebca" class="com.intellij.openapi.ui.ComboBox" binding="myGitRootComboBox" default-binding="true">
@@ -74,15 +74,6 @@
           <toolTipText resource-bundle="git4idea/i18n/GitBundle" key="rebase.valdate.onto.tooltip"/>
         </properties>
       </component>
-      <component id="cb44a" class="javax.swing.JLabel">
-        <constraints>
-          <grid row="6" column="0" row-span="1" col-span="1" vsize-policy="0" hsize-policy="0" anchor="9" fill="0" indent="0" use-parent-layout="false"/>
-        </constraints>
-        <properties>
-          <labelFor value="b0f9a"/>
-          <text resource-bundle="git4idea/i18n/GitBundle" key="rebase.merge.strategy"/>
-        </properties>
-      </component>
       <component id="4aaae" class="javax.swing.JLabel">
         <constraints>
           <grid row="4" column="0" row-span="1" col-span="1" vsize-policy="0" hsize-policy="0" anchor="8" fill="0" indent="0" use-parent-layout="false"/>
@@ -110,39 +101,6 @@
           <toolTipText resource-bundle="git4idea/i18n/GitBundle" key="rebase.validate.from.tooltip"/>
         </properties>
       </component>
-      <grid id="f939d" layout-manager="GridLayoutManager" row-count="2" column-count="2" same-size-horizontally="false" same-size-vertically="false" hgap="-1" vgap="-1">
-        <margin top="0" left="0" bottom="0" right="0"/>
-        <constraints>
-          <grid row="6" column="1" row-span="1" col-span="2" vsize-policy="3" hsize-policy="3" anchor="0" fill="3" indent="0" use-parent-layout="false"/>
-        </constraints>
-        <properties/>
-        <border type="none"/>
-        <children>
-          <component id="b0f9a" class="com.intellij.openapi.ui.ComboBox" binding="myMergeStrategyComboBox" default-binding="true">
-            <constraints>
-              <grid row="0" column="0" row-span="1" col-span="1" vsize-policy="0" hsize-policy="2" anchor="8" fill="0" indent="0" use-parent-layout="false"/>
-            </constraints>
-            <properties>
-              <toolTipText resource-bundle="git4idea/i18n/GitBundle" key="rebase.merge.strategy.tooltip"/>
-            </properties>
-          </component>
-          <hspacer id="1ba1f">
-            <constraints>
-              <grid row="0" column="1" row-span="1" col-span="1" vsize-policy="1" hsize-policy="6" anchor="0" fill="1" indent="0" use-parent-layout="false"/>
-            </constraints>
-          </hspacer>
-          <component id="3a627" class="javax.swing.JCheckBox" binding="myDoNotUseMergeCheckBox" default-binding="true">
-            <constraints>
-              <grid row="1" column="0" row-span="1" col-span="1" vsize-policy="0" hsize-policy="1" anchor="8" fill="0" indent="0" use-parent-layout="false"/>
-            </constraints>
-            <properties>
-              <selected value="false"/>
-              <text resource-bundle="git4idea/i18n/GitBundle" key="rebase.no.merge"/>
-              <toolTipText resource-bundle="git4idea/i18n/GitBundle" key="rebase.no.merge.tooltip"/>
-            </properties>
-          </component>
-        </children>
-      </grid>
       <grid id="63e88" layout-manager="GridLayoutManager" row-count="1" column-count="3" same-size-horizontally="false" same-size-vertically="false" hgap="-1" vgap="-1">
         <margin top="0" left="0" bottom="0" right="0"/>
         <constraints>

plugins/git4idea/src/git4idea/rebase/GitRebaseDialog.java
@@ -30,7 +30,6 @@ import git4idea.branch.GitRebaseParams;
 import git4idea.config.GitConfigUtil;
 import git4idea.config.GitRebaseSettings;
 import git4idea.i18n.GitBundle;
-import git4idea.merge.GitMergeUtil;
 import git4idea.repo.GitRemote;
 import git4idea.repo.GitRepository;
 import git4idea.repo.GitRepositoryManager;
@@ -87,17 +86,9 @@ public class GitRebaseDialog extends DialogWrapper {
    */
   private JCheckBox myShowTagsCheckBox;
   /**
-   * Merge strategy drop down
-   */
-  private ComboBox myMergeStrategyComboBox;
-  /**
    * If selected, rebase is interactive
    */
   protected JCheckBox myInteractiveCheckBox;
-  /**
-   * No merges are performed if selected.
-   */
-  private JCheckBox myDoNotUseMergeCheckBox;
   /**
    * The root panel of the dialog
    */
@@ -174,7 +165,6 @@ public class GitRebaseDialog extends DialogWrapper {
     });
 
     setupBranches();
-    setupStrategy();
 
     myInteractiveCheckBox.setSelected(mySettings.isInteractive());
     myPreserveMergesCheckBox.setSelected(mySettings.isPreserveMerges());
@@ -236,22 +226,6 @@ public class GitRebaseDialog extends DialogWrapper {
       mySettings.setOnto(onto);
     }
   }
-
-  /**
-   * Setup strategy
-   */
-  private void setupStrategy() {
-    for (String s : GitMergeUtil.getMergeStrategies(1)) {
-      myMergeStrategyComboBox.addItem(s);
-    }
-    myMergeStrategyComboBox.setSelectedItem(GitMergeUtil.DEFAULT_STRATEGY);
-    myDoNotUseMergeCheckBox.addActionListener(new ActionListener() {
-      public void actionPerformed(final ActionEvent e) {
-        myMergeStrategyComboBox.setEnabled(!myDoNotUseMergeCheckBox.isSelected());
-      }
-    });
-  }
-
 
   /**
    * Validate fields
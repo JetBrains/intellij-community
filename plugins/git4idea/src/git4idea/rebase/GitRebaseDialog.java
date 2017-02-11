0853871297e621847d1cb048eb214fe5202ab56e
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2017-02-11 10:22:43 +0000

    Remove merge strategies selector from Git rebase dialog
    Merge strategy seems to be a very advanced option, that it rarely used
    and just clutters the UI of the rebase dialog.
    
    Moreover, it was actually broken back in 905fdeb and nobody cared,
    so it seems to be actually never used.

--- plugins/git4idea/src/git4idea/rebase/GitRebaseDialog.java
+++ plugins/git4idea/src/git4idea/rebase/GitRebaseDialog.java
@@ -30,7 +30,6 @@ import git4idea.branch.GitRebaseParams;
 import git4idea.config.GitConfigUtil;
 import git4idea.config.GitRebaseSettings;
 import git4idea.i18n.GitBundle;
-import git4idea.merge.GitMergeUtil;
 import git4idea.repo.GitRemote;
 import git4idea.repo.GitRepository;
 import git4idea.repo.GitRepositoryManager;
@@ -87,17 +86,9 @@ public class GitRebaseDialog extends DialogWrapper {
    */
   private JCheckBox myShowTagsCheckBox;
   /**
-   * Merge strategy drop down
-   */
-  private ComboBox myMergeStrategyComboBox;
-  /**
    * If selected, rebase is interactive
    */
   protected JCheckBox myInteractiveCheckBox;
-  /**
-   * No merges are performed if selected.
-   */
-  private JCheckBox myDoNotUseMergeCheckBox;
   /**
    * The root panel of the dialog
    */
@@ -174,7 +165,6 @@ public class GitRebaseDialog extends DialogWrapper {
     });
 
     setupBranches();
-    setupStrategy();
 
     myInteractiveCheckBox.setSelected(mySettings.isInteractive());
     myPreserveMergesCheckBox.setSelected(mySettings.isPreserveMerges());
@@ -236,22 +226,6 @@ public class GitRebaseDialog extends DialogWrapper {
       mySettings.setOnto(onto);
     }
   }
-
-  /**
-   * Setup strategy
-   */
-  private void setupStrategy() {
-    for (String s : GitMergeUtil.getMergeStrategies(1)) {
-      myMergeStrategyComboBox.addItem(s);
-    }
-    myMergeStrategyComboBox.setSelectedItem(GitMergeUtil.DEFAULT_STRATEGY);
-    myDoNotUseMergeCheckBox.addActionListener(new ActionListener() {
-      public void actionPerformed(final ActionEvent e) {
-        myMergeStrategyComboBox.setEnabled(!myDoNotUseMergeCheckBox.isSelected());
-      }
-    });
-  }
-
 
   /**
    * Validate fields
