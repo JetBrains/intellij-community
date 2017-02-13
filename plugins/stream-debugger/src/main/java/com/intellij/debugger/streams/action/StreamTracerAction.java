e117b78018e6389ecf4be9e1dbefa4d42ed0bb9f
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-02-13 19:46:59 +0300

    Add idea plugin without any features

---
+++ plugins/stream-debugger/src/main/java/com/intellij/debugger/streams/action/StreamTracerAction.java
@@ -0,0 +1,14 @@
+package com.intellij.debugger.streams.action;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * @author Vitaliy.Bibaev
+ */
+public class StreamTracerAction extends AnAction {
+  @Override
+  public void actionPerformed(AnActionEvent e) {
+
+  }
+}
