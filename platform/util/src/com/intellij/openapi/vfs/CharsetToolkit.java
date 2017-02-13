ceb8b87cc4f537b8d4222b74290a59b6d51ad0a8
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2017-02-13 12:19:52 +0100

    [vfs] skips BOM detection on non-supported streams
    ... instead of failing with assertion

--- platform/util/src/com/intellij/openapi/vfs/CharsetToolkit.java
+++ platform/util/src/com/intellij/openapi/vfs/CharsetToolkit.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -128,7 +128,10 @@ public class CharsetToolkit {
 
   @NotNull
   public static InputStream inputStreamSkippingBOM(@NotNull InputStream stream) throws IOException {
-    assert stream.markSupported() :stream;
+    if (!stream.markSupported()) {
+      return stream;
+    }
+
     stream.mark(4);
     boolean mustReset = true;
     try {
