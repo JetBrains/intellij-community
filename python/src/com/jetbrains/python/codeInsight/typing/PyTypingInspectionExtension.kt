6f2e8b74857fab4b2604e7efd2693919cc63acff
Semyon Proshev <semyon.proshev@jetbrains.com>
2017-02-14 20:55:42 +0300

    Update PyTypingInspectionExtension to not ignore `__getitem__` for definition of builtin types.
    PyUnresolvedReferencesInspectionTest.testBuiltinListGetItem() fixed.

--- python/src/com/jetbrains/python/codeInsight/typing/PyTypingInspectionExtension.kt
+++ python/src/com/jetbrains/python/codeInsight/typing/PyTypingInspectionExtension.kt
@@ -17,20 +17,30 @@ package com.jetbrains.python.codeInsight.typing
 
 import com.jetbrains.python.PyNames
 import com.jetbrains.python.inspections.PyInspectionExtension
+import com.jetbrains.python.psi.impl.PyBuiltinCache
 import com.jetbrains.python.psi.types.PyClassLikeType
+import com.jetbrains.python.psi.types.PyClassType
 import com.jetbrains.python.psi.types.PyType
 import com.jetbrains.python.psi.types.TypeEvalContext
 
 class PyTypingInspectionExtension : PyInspectionExtension() {
 
   override fun ignoreUnresolvedMember(type: PyType, name: String, context: TypeEvalContext): Boolean {
-    return name == PyNames.GETITEM && type is PyClassLikeType && type.isDefinition && isGenericItselfOrDescendant(type, context)
+    return name == PyNames.GETITEM &&
+           type is PyClassLikeType &&
+           type.isDefinition &&
+           !isBuiltin(type) &&
+           isGenericItselfOrDescendant(type, context)
   }
 
   private fun isGenericItselfOrDescendant(type: PyClassLikeType,
                                           context: TypeEvalContext): Boolean {
     return type.classQName == PyTypingTypeProvider.GENERIC ||
            type.getSuperClassTypes(context).any { it.classQName == PyTypingTypeProvider.GENERIC }
+  }
+
+  private fun isBuiltin(type: PyClassLikeType): Boolean {
+    return if (type is PyClassType) PyBuiltinCache.getInstance(type.pyClass).isBuiltin(type.pyClass) else false
   }
 }
 
