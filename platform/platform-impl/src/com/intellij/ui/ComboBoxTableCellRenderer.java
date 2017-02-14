a78644886b7b8c1ff7592c1b826e47bb43ce7d96
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2017-02-14 14:11:42 +0000

    Use ComboBox instead of deprecated JComboBox

--- platform/platform-impl/src/com/intellij/ui/ComboBoxTableCellRenderer.java
+++ platform/platform-impl/src/com/intellij/ui/ComboBoxTableCellRenderer.java
@@ -17,13 +17,15 @@ package com.intellij.ui;
 
 import com.intellij.openapi.diagnostic.LogUtil;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.ui.ComboBox;
 import com.intellij.util.ListWithSelection;
+import com.intellij.util.ui.JBUI;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableCellRenderer;
 import java.awt.*;
-import java.util.Arrays;
+import java.util.Collections;
 
 public class ComboBoxTableCellRenderer extends JPanel implements TableCellRenderer {
   public static final TableCellRenderer INSTANCE = new ComboBoxTableCellRenderer();
@@ -35,7 +37,7 @@ public class ComboBoxTableCellRenderer extends JPanel implements TableCellRender
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
       if (isSelected) {
-        value = new ListWithSelection<Object>(Arrays.asList(value));
+        value = new ListWithSelection<>(Collections.singletonList(value));
         return INSTANCE.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
       }
       return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
@@ -44,12 +46,12 @@ public class ComboBoxTableCellRenderer extends JPanel implements TableCellRender
 
   private static final Logger LOG = Logger.getInstance("#com.intellij.util.ui.ComboBoxTableCellRenderer");
 
-  private final JComboBox myCombo = new JComboBox();
+  private final JComboBox myCombo = new ComboBox();
 
   private ComboBoxTableCellRenderer() {
     super(new GridBagLayout());
     add(myCombo,
-        new GridBagConstraints(0, 0, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+        new GridBagConstraints(0, 0, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, JBUI.emptyInsets(), 0, 0));
   }
 
   @Override
