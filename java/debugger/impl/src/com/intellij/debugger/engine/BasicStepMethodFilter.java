bb41893aa71fecef5f18767d2d8d166c831bddca
Egor.Ushakov <egor.ushakov@jetbrains.com>
2017-02-15 16:19:52 +0300

    IDEA-143277 Debugger: Smart Step Into can't find a line with lambda, while Step Into can - fix for generic interfaces

--- java/debugger/impl/src/com/intellij/debugger/engine/BasicStepMethodFilter.java
+++ java/debugger/impl/src/com/intellij/debugger/engine/BasicStepMethodFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -77,6 +77,9 @@ public class BasicStepMethodFilter implements NamedMethodFilter {
             PsiMethod interfaceMethod = LambdaUtil.getFunctionalInterfaceMethod(type);
             if (type != null && interfaceMethod != null && myTargetMethodName.equals(interfaceMethod.getName())) {
               try {
+                if (type instanceof PsiClassType) {
+                  type = ((PsiClassType)type).rawType();
+                }
                 return type.getCanonicalText().equals(myDeclaringClassName.getName(process).replace('$', '.'));
               }
               catch (EvaluateException e) {
