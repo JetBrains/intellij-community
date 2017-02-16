70df6dfc0851b967377ca59a364c9266c458ece3
peter <peter@jetbrains.com>
2017-02-16 14:58:46 +0100

    introduce RuntimeExceptionWithAttachments

--- platform/core-impl/src/com/intellij/extapi/psi/StubBasedPsiElementBase.java
+++ platform/core-impl/src/com/intellij/extapi/psi/StubBasedPsiElementBase.java
@@ -20,7 +20,7 @@ import com.intellij.lang.ASTNode;
 import com.intellij.lang.Language;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Attachment;
-import com.intellij.openapi.diagnostic.ExceptionWithAttachments;
+import com.intellij.openapi.diagnostic.RuntimeExceptionWithAttachments;
 import com.intellij.openapi.progress.ProgressIndicatorProvider;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectCoreUtil;
@@ -172,23 +172,7 @@ public class StubBasedPsiElementBase<T extends StubElement> extends ASTDelegateP
       attachments.add(new Attachment("creationTraces.txt", creationTraces));
     }
 
-    throw new FailedToBindStubToAstException(message, attachments.toArray(Attachment.EMPTY_ARRAY));
-  }
-
-  private static class FailedToBindStubToAstException extends RuntimeException implements ExceptionWithAttachments {
-
-    private final Attachment[] myAttachments;
-
-    public FailedToBindStubToAstException(String message, Attachment[] attachments) {
-      super(message);
-      myAttachments = attachments;
-    }
-
-    @NotNull
-    @Override
-    public Attachment[] getAttachments() {
-      return myAttachments;
-    }
+    throw new RuntimeExceptionWithAttachments(message, attachments.toArray(Attachment.EMPTY_ARRAY));
   }
 
   @NotNull
