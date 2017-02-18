36368ff874af499aaef2be02b80250921e93693e
Tor Norbye <tnorbye@google.com>
2017-02-17 20:45:40 -0800

    Tweak build script
    There's a mismatch between the artifacts being built
    and the script attempting to unzip/rezip them to fix
    hyperlinks. One place refers to the raw build numbers,
    the other includes the branch name as a prefix. This
    just uses wildcards as a hotfix to get the builds green
    again.
    
    Change-Id: I9d0acfd13aeb40499b754de2e598e0b25605bdca

--- build_studio.sh
+++ build_studio.sh
@@ -78,9 +78,9 @@ $ANT "-Dout=$OUT" "-Dbuild=$BNUM" "-Denable.ui.tests=$UI_TESTS" -Dbundle.gradle.
 
 # Temp: figure out how to preserve symlinks
 cd "$OUT/artifacts"
-unzip -q "android-studio-$BNUM.mac.zip"
+unzip -q "android-studio-*.mac.zip"
 cd "Android Studio.app/Contents/jre/jdk/Contents/MacOS" && ln -fs ../Home/jre/lib/jli/libjli.dylib && cd ../../../../../../
-zip --symlinks -r "android-studio-$BNUM.mac.zip" "Android Studio.app/Contents/jre/jdk/Contents/MacOS"
+zip --symlinks -r "android-studio-*.mac.zip" "Android Studio.app/Contents/jre/jdk/Contents/MacOS"
 cd ../../
 
 echo "## Copying android-studio distribution files"
