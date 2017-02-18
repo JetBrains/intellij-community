149a39bb257d6a5639d66bf54dab4875f67585a8
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2017-02-18 11:47:10 +0300

    Actions popup instead of menu-like action list (IDEA-165087, comment #3)

--- platform/lang-impl/src/com/intellij/application/options/schemes/AbstractSchemesPanel.java
+++ platform/lang-impl/src/com/intellij/application/options/schemes/AbstractSchemesPanel.java
@@ -19,14 +19,16 @@ import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.application.ApplicationBundle;
 import com.intellij.openapi.options.Scheme;
-import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.ui.MessageType;
 import com.intellij.openapi.ui.popup.Balloon;
 import com.intellij.openapi.ui.popup.BalloonBuilder;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.openapi.ui.popup.ListPopup;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.ui.JBColor;
+import com.intellij.ui.awt.RelativePoint;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -59,7 +61,7 @@ public abstract class AbstractSchemesPanel<T extends Scheme, InfoComponent exten
     controlsPanel.add(new JLabel(getSchemeTypeName() + ":"));
     controlsPanel.add(Box.createRigidArea(new Dimension(10, 0)));
     myActions = createSchemeActions();
-    mySchemesCombo = new EditableSchemesCombo<T>(this);
+    mySchemesCombo = new EditableSchemesCombo<>(this);
     controlsPanel.add(mySchemesCombo.getComponent());
     myToolbar = createToolbar();
     controlsPanel.add(myToolbar);
@@ -73,33 +75,14 @@ public abstract class AbstractSchemesPanel<T extends Scheme, InfoComponent exten
     add(Box.createVerticalGlue());
     add(Box.createRigidArea(new Dimension(0, 10)));
   }
+  
   private JComponent createToolbar() {
     DefaultActionGroup toolbarActionGroup = new DefaultActionGroup();
-    toolbarActionGroup.add(new TopActionGroup());
     ActionToolbar toolbar = ActionManager.getInstance().createActionToolbar(ActionPlaces.TOOLBAR, toolbarActionGroup, true);
     JComponent toolbarComponent = toolbar.getComponent();
+    toolbarActionGroup.add(new ShowSchemesActionsListAction(myActions.getActions(), toolbarComponent));
     toolbarComponent.setMaximumSize(new Dimension(toolbarComponent.getPreferredSize().width, Short.MAX_VALUE));
     return toolbarComponent;
-  }
-
-
-  private class TopActionGroup extends ActionGroup implements DumbAware {
-    public TopActionGroup() {
-      super("", true);
-    }
-
-    @NotNull
-    @Override
-    public AnAction[] getChildren(@Nullable AnActionEvent e) {
-      Collection<AnAction> actions = myActions.getActions();
-      return actions.toArray(new AnAction[actions.size()]);
-    }
-
-    @Override
-    public void update(AnActionEvent e) {
-      Presentation p = e.getPresentation();
-      p.setIcon(AllIcons.General.GearPlain);
-    }
   }
 
   public final JComponent getToolbar() {
@@ -185,15 +168,28 @@ public abstract class AbstractSchemesPanel<T extends Scheme, InfoComponent exten
     balloon.showInCenterOf(myToolbar);
     Disposer.register(ProjectManager.getInstance().getDefaultProject(), balloon);
   }
-  
-  @SuppressWarnings("UseJBColor")
-  private static Color messageTypeToColor(@NotNull MessageType messageType) {
-    if (messageType == MessageType.INFO) {
-      return Color.GRAY;
+
+  private static class ShowSchemesActionsListAction extends DumbAwareAction {
+
+    private final ActionGroup myActionGroup;
+    private final Component myParentComponent;
+
+    ShowSchemesActionsListAction(Collection<AnAction> actions, Component component) {
+      myParentComponent = component;
+      myActionGroup = new DefaultActionGroup(actions.toArray(new AnAction[actions.size()]));
     }
-    else if (messageType == MessageType.ERROR) {
-      return Color.RED;
+
+    @Override
+    public void update(AnActionEvent e) {
+      Presentation p = e.getPresentation();
+      p.setIcon(AllIcons.General.Gear);
     }
-    return JBColor.BLACK;
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+      final ListPopup actionGroupPopup =
+        JBPopupFactory.getInstance().createActionGroupPopup(null, myActionGroup, e.getDataContext(), true, null, Integer.MAX_VALUE);
+      actionGroupPopup.show(new RelativePoint(myParentComponent, new Point(2, myParentComponent.getHeight() - 1)));
+    }
   }
 }
