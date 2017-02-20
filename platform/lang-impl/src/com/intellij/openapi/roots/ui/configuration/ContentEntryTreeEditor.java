2c86a18a54c6b7014d16967700841ecf53468c7b
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2017-02-20 11:14:47 +0100

    [platform] avoids 'null' file chooser root (EA-97093)

--- platform/lang-impl/src/com/intellij/openapi/roots/ui/configuration/ContentEntryTreeEditor.java
+++ platform/lang-impl/src/com/intellij/openapi/roots/ui/configuration/ContentEntryTreeEditor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -148,9 +148,11 @@ public class ContentEntryTreeEditor {
     final ContentEntry entry = contentEntryEditor.getContentEntry();
     assert entry != null : contentEntryEditor;
     final VirtualFile file = entry.getFile();
-    myDescriptor.setRoots(file);
-    if (file == null) {
-      final String path = VfsUtilCore.urlToPath(entry.getUrl());
+    if (file != null) {
+      myDescriptor.setRoots(file);
+    }
+    else {
+      String path = VfsUtilCore.urlToPath(entry.getUrl());
       myDescriptor.setTitle(FileUtil.toSystemDependentName(path));
     }
 
