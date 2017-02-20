c56db11e99da47edbc98a0d79f577ebd8ab7ae56
irengrig <irina.chernushina@jetbrains.com>
2017-02-20 20:55:52 +0100

    webpack config support: also recognize variables called webPack and think that an object literal in initializer (even in constructs like merge(inherited, {})) is config
    (cherry picked from commit 01ce3c3)

--- json/src/com/jetbrains/jsonSchema/impl/JsonValueAdapter.java
+++ json/src/com/jetbrains/jsonSchema/impl/JsonValueAdapter.java
@@ -34,4 +34,6 @@ public interface JsonValueAdapter {
 
   @Nullable JsonObjectValueAdapter getAsObject();
   @Nullable JsonArrayValueAdapter getAsArray();
+
+  default boolean shouldCheckIntegralRequirements() {return true;}
 }
