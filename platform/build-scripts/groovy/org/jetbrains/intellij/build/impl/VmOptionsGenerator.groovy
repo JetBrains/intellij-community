6bdac08ffbd0ea516342317300a498aaff28e42d
Raluca Sauciuc <ralucas@google.com>
2017-02-19 18:56:49 -0800

    Build scripts: adjust JVM args in generated vmoptions files
    Change-Id: Ie7351d928bd938e2555a70331b2f8467e3ea0851

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/VmOptionsGenerator.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/VmOptionsGenerator.groovy
@@ -13,22 +13,29 @@ class VmOptionsGenerator {
   static final List<String> COMMON_VM_OPTIONS =
     [
       '-XX:+UseConcMarkSweepGC', '-XX:SoftRefLRUPolicyMSPerMB=50',
+/* Android Studio: removed by Change Ie7351d92
       '-ea',
+Android Studio: removed by Change Ie7351d92 */
       '-XX:CICompilerCount=2',
       '-Dsun.io.useCanonPrefixCache=false', '-Djava.net.preferIPv4Stack=true',
       '-Djdk.http.auth.tunneling.disabledSchemes=""',
+/* Android Studio: removed by Change Ie7351d92
       '-XX:+HeapDumpOnOutOfMemoryError',
       '-XX:-OmitStackTraceInFastThrow',
+Android Studio: removed by Change Ie7351d92 */
       '-Djdk.attach.allowAttachSelf=true',
       '-Dkotlinx.coroutines.debug=off',
       '-Djdk.module.illegalAccess.silent=true',
+      '-Djna.nosys=true',  // Android Studio: added by Change Ie7351d92
+      '-Djna.boot.library.path=',  // Android Studio: added by Change Ie7351d92
     ]
 
   static List<String> computeVmOptions(JvmArchitecture arch, boolean isEAP, ProductProperties productProperties) {
     List<String> commonVmOptions
     if (isEAP) {
       //must be consistent with com.intellij.openapi.application.ConfigImportHelper.updateVMOptions
-      commonVmOptions = COMMON_VM_OPTIONS + ["-XX:MaxJavaStackTraceDepth=10000"]
+      // Android Studio: modified by Change Ie7351d92
+      commonVmOptions = COMMON_VM_OPTIONS + ["-XX:MaxJavaStackTraceDepth=10000 -XX:+HeapDumpOnOutOfMemoryError -XX:-OmitStackTraceInFastThrow -ea"]
     }
     else {
       commonVmOptions = COMMON_VM_OPTIONS
@@ -39,8 +46,9 @@ class VmOptionsGenerator {
   private static List<String> vmMemoryOptions(JvmArchitecture arch, ProductProperties productProperties) {
     switch (arch) {
       // NOTE: when changing, please review usages of ProductProperties.getCustomJvmMemoryOptionsX64 and synchronize if necessary  
-      case JvmArchitecture.x32: return ['-server', '-Xms128m', '-Xmx512m', '-XX:ReservedCodeCacheSize=240m']
-      case JvmArchitecture.x64: return productProperties.customJvmMemoryOptionsX64?.split(' ')?.toList() ?: ['-Xms128m', '-Xmx750m', '-XX:ReservedCodeCacheSize=240m']
+      // Android Studio: modified by Change Ie7351d92
+      case JvmArchitecture.x32: return ['-server', '-Xms256m', '-Xmx768m', '-XX:ReservedCodeCacheSize=240m']
+      case JvmArchitecture.x64: return productProperties.customJvmMemoryOptionsX64?.split(' ')?.toList() ?: ['-Xms256m', '-Xmx1280m', '-XX:ReservedCodeCacheSize=240m']
     }
     throw new AssertionError(arch)
   }
