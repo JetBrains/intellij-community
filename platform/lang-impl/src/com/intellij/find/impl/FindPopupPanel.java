d0e37e70b643ffd61c9d971f79b1a9556a42545f
Vassiliy.Kudryashov <vassiliy.kudryashov@jetbrains.com>
2017-02-21 16:00:25 +0100

    IDEA-144857 Find in Path layout
    Prevent regression (IDEA-146150 Page up/down in preview mode of find in path)
    
    (cherry picked from commit 078a33d)

--- platform/lang-impl/src/com/intellij/find/impl/FindPopupPanel.java
+++ platform/lang-impl/src/com/intellij/find/impl/FindPopupPanel.java
@@ -106,6 +106,8 @@ public class FindPopupPanel extends JBPanel implements FindUI {
 
   private static final KeyStroke MOVE_CARET_DOWN = KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0);
   private static final KeyStroke MOVE_CARET_UP = KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0);
+  private static final KeyStroke MOVE_CARET_PAGE_DOWN = KeyStroke.getKeyStroke(KeyEvent.VK_PAGE_DOWN, 0);
+  private static final KeyStroke MOVE_CARET_PAGE_UP = KeyStroke.getKeyStroke(KeyEvent.VK_PAGE_UP, 0);
   private static final KeyStroke NEW_LINE = KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0);
 
   private static final KeyStroke MOVE_CARET_DOWN_ALTERNATIVE = KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, InputEvent.ALT_DOWN_MASK);
@@ -523,7 +525,7 @@ public class FindPopupPanel extends JBPanel implements FindUI {
     KeymapUtil.reassignAction(mySearchComponent, MOVE_CARET_DOWN, MOVE_CARET_DOWN_ALTERNATIVE, WHEN_IN_FOCUSED_WINDOW);
     KeymapUtil.reassignAction(mySearchComponent, MOVE_CARET_UP, MOVE_CARET_UP_ALTERNATIVE, WHEN_IN_FOCUSED_WINDOW);
     KeymapUtil.reassignAction(mySearchComponent, NEW_LINE, NEW_LINE_ALTERNATIVE, WHEN_IN_FOCUSED_WINDOW);
-    UIUtil.redirectKeystrokes(myDisposable, mySearchComponent, myResultsPreviewTable, MOVE_CARET_UP, MOVE_CARET_DOWN, NEW_LINE);
+    UIUtil.redirectKeystrokes(myDisposable, mySearchComponent, myResultsPreviewTable, MOVE_CARET_UP, MOVE_CARET_DOWN, NEW_LINE, MOVE_CARET_PAGE_UP, MOVE_CARET_PAGE_DOWN);
 
 
     myUsagePreviewPanel = new UsagePreviewPanel(myProject, new UsageViewPresentation(), PREVIEW_IS_EDITABLE) {
