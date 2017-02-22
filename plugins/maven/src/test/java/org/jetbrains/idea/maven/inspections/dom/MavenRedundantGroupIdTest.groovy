096b90bd410d1c10ca51e75230aef272ef44593b
Vladislav.Soroka <vladislav.soroka@jetbrains.com>
2017-02-22 14:23:02 +0300

    add maven surefire plugin and maven dom inspection tests to the MAVEN_INTEGRATION_TESTS group
    (cherry picked from commit ca5ffa2)

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/inspections/dom/MavenRedundantGroupIdTest.groovy
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/inspections/dom/MavenRedundantGroupIdTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jetbrains.idea.maven.inspections
+package org.jetbrains.idea.maven.inspections.dom
 
 import com.intellij.codeInsight.intention.IntentionAction
 import com.intellij.psi.impl.source.PostprocessReformattingAspect
