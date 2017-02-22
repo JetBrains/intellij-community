47970281f9cdf10f8429cfb88c7fffc3c9c27f2a
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2017-02-22 18:05:01 +0300

    Fix test data for PyArgumentListInspectionTest.testDictFromKeys.

--- python/testData/inspections/PyArgumentListInspection/dictFromKeys.py
+++ python/testData/inspections/PyArgumentListInspection/dictFromKeys.py
@@ -1,2 +1,2 @@
-print(dict.fromkeys(<warning descr="Parameter 'seq' unfilled">)</warning>)
+print(dict.fromkeys(<warning descr="Parameter(s) unfilledPossible callees:dict.fromkeys(seq: Sequence[TypeVar('_T')])dict.fromkeys(seq: Sequence[TypeVar('_T')], value: TypeVar('_S'))">)</warning>)
 print(dict.fromkeys(['foo', 'bar']))
