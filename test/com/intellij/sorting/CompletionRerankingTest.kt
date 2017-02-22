41dd420c76199e66fd1b076042ed1110519c6166
Yaroslav Lepenkin <yaroslav.lepenkin@jetbrains.com>
2017-02-22 10:46:03 +0300

    replace completion contributor with fake one

--- test/com/intellij/sorting/CompletionRerankingTest.kt
+++ test/com/intellij/sorting/CompletionRerankingTest.kt
@@ -8,6 +8,8 @@ import com.intellij.codeInsight.lookup.LookupArranger
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.impl.LookupImpl
 import com.intellij.ide.highlighter.JavaFileType
+import com.intellij.lang.Language
+import com.intellij.openapi.extensions.ExtensionPoint
 import com.intellij.openapi.extensions.Extensions
 import com.intellij.openapi.util.Pair
 import com.intellij.stats.completion.sorting.MLCompletionContributor
@@ -38,16 +40,28 @@ class Test {
 class CompletionRerankingTest : LightFixtureCompletionTestCase() {
 
     private var pointIndex = 0
+    private lateinit var realContributor: CompletionContributorEP
     
     override fun setUp() {
         super.setUp()
 
         val point = getExtensionPoint<CompletionContributorEP>("com.intellij.completion.contributor")
         val extensions = point.extensions
+
+        pointIndex = extensions.indexOfFirst { 
+            it.implementationClass == MLCompletionContributor::class.java.canonicalName 
+        }
+        realContributor = extensions[pointIndex]
         
-        val originalClass = MLCompletionContributor::class.java.canonicalName
-        pointIndex = extensions.indexOfFirst { it.implementationClass == originalClass }
-        extensions[pointIndex].implementationClass = TestCompletionContributor::class.java.canonicalName
+        extensions[pointIndex] = CompletionContributorEP().apply { 
+            implementationClass = TestCompletionContributor::class.java.canonicalName
+            language = "any"
+        }
+        
+        val field = point::class.java.getDeclaredField("myExtensionsCache").apply { 
+            isAccessible = true
+        }
+        field.set(point, extensions)
 
         myFixture.addClass(testInterface)
         myFixture.configureByText(JavaFileType.INSTANCE, text)
@@ -55,10 +69,14 @@ class CompletionRerankingTest : LightFixtureCompletionTestCase() {
 
     override fun tearDown() {
         val point = getExtensionPoint<CompletionContributorEP>("com.intellij.completion.contributor")
-        val contributorEP = point.extensions[pointIndex]
-        contributorEP.implementationClass = MLCompletionContributor::class.java.canonicalName
-        //todo this will fail, since implementation will be cached
-        //todo reuse reflection and set extension directly into myExtensionsCache
+        
+        val extensions = point.extensions
+        extensions[pointIndex] = realContributor
+
+        val field = point::class.java.getDeclaredField("myExtensionsCache").apply { 
+            isAccessible = true
+        }
+        field.set(point, extensions)
         
         super.tearDown()
     }
