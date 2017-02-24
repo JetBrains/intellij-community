JetBrains/intellij-community

commit be3de925ac3100713203c129c2e4a230fcd7554d

Automatically add a single Git/hg root
If we detected that there is a single unregistered root in the project,
which wasn't reported previously, and there are no other roots,
add this root automatically.

If user doesn't want VCS integration, he can easily remove the root
from the Version Control preferences, and it won't be reported or
auto-added anymore.

Add a registry option for a while (on by default)
in case something goes wrong with this behavior.

This fixes IDEA-118475

platform/vcs-impl/src/com/intellij/openapi/vcs/roots/VcsRootProblemNotifier.java
@@ -19,10 +19,12 @@ import com.intellij.idea.ActionsBundle;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationListener;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.options.ShowSettingsUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectFileIndex;
 import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vcs.*;
 import com.intellij.openapi.vcs.changes.ChangeListManager;
@@ -38,16 +40,19 @@ import org.jetbrains.annotations.Nullable;
 
 import javax.swing.event.HyperlinkEvent;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
 import static com.intellij.openapi.util.text.StringUtil.pluralize;
+import static com.intellij.util.ObjectUtils.notNull;
+import static com.intellij.util.containers.ContainerUtil.getFirstItem;
 
 /**
  * Searches for Vcs roots problems via {@link VcsRootErrorsFinder} and notifies about them.
  */
 public class VcsRootProblemNotifier {
-
+  private static final Logger LOG = Logger.getInstance(VcsRootProblemNotifier.class);
   public static final Function<VcsRootError, String> PATH_FROM_ROOT_ERROR = VcsRootError::getMapping;
 
   @NotNull private final Project myProject;
@@ -85,6 +90,19 @@ public class VcsRootProblemNotifier {
 
     Collection<VcsRootError> importantUnregisteredRoots = getImportantUnregisteredMappings(errors);
     Collection<VcsRootError> invalidRoots = getInvalidRoots(errors);
+
+    if (importantUnregisteredRoots.size() == 1) {
+      VcsRootError singleUnregRoot = notNull(getFirstItem(importantUnregisteredRoots));
+      String mappingPath = singleUnregRoot.getMapping();
+      if (!myVcsManager.hasAnyMappings()
+          && !myReportedUnregisteredRoots.contains(mappingPath)
+          && Registry.is("vcs.auto.add.single.root")) {
+        VcsDirectoryMapping mapping = new VcsDirectoryMapping(mappingPath, singleUnregRoot.getVcsKey().getName());
+        myVcsManager.setDirectoryMappings(Collections.singletonList(mapping));
+        LOG.info("Added " + mapping.getVcs() + " root " + mapping + " as the only auto-detected root.");
+        return;
+      }
+    }
 
     List<String> unregRootPaths = ContainerUtil.map(importantUnregisteredRoots, PATH_FROM_ROOT_ERROR);
     if (invalidRoots.isEmpty() && (importantUnregisteredRoots.isEmpty() || myReportedUnregisteredRoots.containsAll(unregRootPaths))) {

platform/util/resources/misc/registry.properties
@@ -316,6 +316,8 @@ vcs.log.labels.left.aligned.description=Align labels in Log table to the left
 
 vcs.executable.validator.timeout.sec=60
 vcs.unversioned.files.in.commit=true
+vcs.auto.add.single.root=true
+vcs.auto.add.single.root.description=Automatically register Git/Hg root if it was auto-detected as the only root in the project
 
 psi.incremental.reparse.depth.limit=1000
 psi.deferIconLoading=true

commit e736a37c05f8fe84a9f1fc4e7940003315ba5fc2

Allow to remove Git root mapping
The bug was introduced in be3de925 where Git roots became auto-added
in simple cases.

The IGNORED_UNREGISTERED_ROOTS setting was read only once - on the
service initialization, i.e. on startup. The fix is to take it into
account every time.

On the other hand, there is no need to pre-populate
myReportedUnregisteredRoots with ignored unregistered roots, since
it is a different conception: reported roots are not explicitly ignored
by the user, they are stored to be reported once per session.

The reason why this bug was not noticed by the author of the original
change is unclear, but probably the root was reported before testing
the fix and became remembered in the field.

Fixes IDEA-168690.

platform/vcs-api/src/com/intellij/openapi/vcs/VcsConfiguration.java
@@ -304,4 +304,8 @@ public final class VcsConfiguration implements PersistentStateComponent<VcsConfi
     unregisteredRoots.removeAll(roots);
     IGNORED_UNREGISTERED_ROOTS = unregisteredRoots;
   }
+
+  public boolean isIgnoredUnregisteredRoot(@NotNull String root) {
+    return IGNORED_UNREGISTERED_ROOTS.contains(root);
+  }
 }

platform/vcs-impl/src/com/intellij/openapi/vcs/roots/VcsRootProblemNotifier.java
@@ -61,6 +61,7 @@ public class VcsRootProblemNotifier {
   @NotNull private final ChangeListManager myChangeListManager;
   @NotNull private final ProjectFileIndex myProjectFileIndex;
 
+  // unregistered roots reported during this session but not explicitly ignored
   @NotNull private final Set<String> myReportedUnregisteredRoots;
 
   @Nullable private Notification myNotification;
@@ -76,7 +77,7 @@ public class VcsRootProblemNotifier {
     myChangeListManager = ChangeListManager.getInstance(project);
     myProjectFileIndex = ProjectFileIndex.SERVICE.getInstance(myProject);
     myVcsManager = ProjectLevelVcsManager.getInstance(project);
-    myReportedUnregisteredRoots = new HashSet<>(mySettings.IGNORED_UNREGISTERED_ROOTS);
+    myReportedUnregisteredRoots = new HashSet<>();
   }
 
   public void rescanAndNotifyIfNeeded() {
@@ -132,7 +133,7 @@ public class VcsRootProblemNotifier {
            FileUtil.isAncestor(mapping, projectDir, false);
   }
 
-  private boolean isIgnoredOrExcluded(@NotNull String mapping) {
+  private boolean isIgnoredOrExcludedPath(@NotNull String mapping) {
     VirtualFile file = LocalFileSystem.getInstance().findFileByPath(mapping);
     return file != null && (myChangeListManager.isIgnoredFile(file) || myProjectFileIndex.isExcluded(file));
   }
@@ -217,7 +218,10 @@ public class VcsRootProblemNotifier {
   private List<VcsRootError> getImportantUnregisteredMappings(@NotNull Collection<VcsRootError> errors) {
     return ContainerUtil.filter(errors, error -> {
       String mapping = error.getMapping();
-      return error.getType() == VcsRootError.Type.UNREGISTERED_ROOT && isUnderOrAboveProjectDir(mapping) && !isIgnoredOrExcluded(mapping);
+      return error.getType() == VcsRootError.Type.UNREGISTERED_ROOT &&
+             isUnderOrAboveProjectDir(mapping) &&
+             !isIgnoredOrExcludedPath(mapping) &&
+             !mySettings.isIgnoredUnregisteredRoot(mapping);
     });
   }
 