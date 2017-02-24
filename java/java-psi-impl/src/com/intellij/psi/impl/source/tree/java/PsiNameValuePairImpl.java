dc37bfc059e39f60ccf232ef5f0fd9f613e76bd2
peter <peter@jetbrains.com>
2017-02-24 15:49:11 +0100

    [^roma] remove PsiNameValuePair.Detachable, make getDetachedValue a default method

--- java/java-psi-impl/src/com/intellij/psi/impl/source/tree/java/PsiNameValuePairImpl.java
+++ java/java-psi-impl/src/com/intellij/psi/impl/source/tree/java/PsiNameValuePairImpl.java
@@ -42,7 +42,7 @@ import java.lang.ref.Reference;
  * @author Dmitry Avdeev
  *         Date: 7/27/12
  */
-public class PsiNameValuePairImpl extends JavaStubPsiElement<PsiNameValuePairStub> implements PsiNameValuePair.Detachable {
+public class PsiNameValuePairImpl extends JavaStubPsiElement<PsiNameValuePairStub> implements PsiNameValuePair {
 
   public PsiNameValuePairImpl(@NotNull PsiNameValuePairStub stub) {
     super(stub, JavaStubElementTypes.NAME_VALUE_PAIR);
@@ -103,7 +103,7 @@ public class PsiNameValuePairImpl extends JavaStubPsiElement<PsiNameValuePairStu
   @Override
   @Nullable
   public PsiAnnotationMemberValue getDetachedValue() {
-    PsiNameValuePairStub stub = getGreenStub();
+    PsiNameValuePairStub stub = getStub();
     if (stub != null) {
       String text = stub.getValue();
       PsiAnnotationMemberValue result = SoftReference.dereference(myDetachedValue);
