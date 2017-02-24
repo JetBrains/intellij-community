6dc51313dd12743caecdb651cf79709080313d9f
peter <peter@jetbrains.com>
2017-02-24 12:42:12 +0100

    [^develar] cache live template context types to avoid allocating them like crazy during save
    (cherry picked from commit b69f3cb)

--- platform/lang-impl/src/com/intellij/codeInsight/template/impl/TemplateManagerImpl.java
+++ platform/lang-impl/src/com/intellij/codeInsight/template/impl/TemplateManagerImpl.java
@@ -51,6 +51,7 @@ import org.jetbrains.annotations.TestOnly;
 import java.util.*;
 
 public class TemplateManagerImpl extends TemplateManager implements Disposable {
+  private static final TemplateContextType[] ourContextTypes = Extensions.getExtensions(TemplateContextType.EP_NAME);
   private final Project myProject;
   private boolean myTemplateTesting;
 
@@ -517,7 +518,7 @@ public class TemplateManagerImpl extends TemplateManager implements Disposable {
   }
 
   public static TemplateContextType[] getAllContextTypes() {
-    return Extensions.getExtensions(TemplateContextType.EP_NAME);
+    return ourContextTypes;
   }
 
   @Override
