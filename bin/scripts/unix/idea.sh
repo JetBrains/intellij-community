68b368f305668b6bd5ecad42c51e02335743466d
Vladimir.Orlov <vladimir.orlov@jetbrains.com>
2017-02-27 09:02:31 +0300

    JRE bundled folder structure changed: jre/jre/.. -> jre/.. IDEA-166909 Compiling 1.5 with JDK9 problem IDEA-CR-18311 (cherry picked from commit 16e2a4a) (cherry picked from commit 797018d)

--- bin/scripts/unix/idea.sh
+++ bin/scripts/unix/idea.sh
@@ -68,7 +68,7 @@ elif [ -s "$HOME/.@@system_selector@@/config/@@vm_options@@.jdk" ]; then
   if [ ! -d "$JDK" ]; then
     JDK="$IDE_HOME/$JDK"
   fi
-elif [ "$OS_TYPE" = "Linux" ] && [ -x "$IDE_HOME/jre/jre/bin/java" ] && "$IDE_HOME/jre/jre/bin/java" -version > /dev/null 2>&1 ; then
+elif [ "$OS_TYPE" = "Linux" ] && [ -x "$IDE_HOME/jre/bin/java" ] && "$IDE_HOME/jre/bin/java" -version > /dev/null 2>&1 ; then
   JDK="$IDE_HOME/jre"
 elif [ -n "$JDK_HOME" -a -x "$JDK_HOME/bin/java" ]; then
   JDK="$JDK_HOME"
