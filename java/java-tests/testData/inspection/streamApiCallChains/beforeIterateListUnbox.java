45b11c4b10fb87aafc0f423d0db2bbe08d3bc06b
Tagir Valeev <tagir.valeev@jetbrains.com>
2017-02-28 15:20:02 +0700

    IDEA-168586 Unify 'Replace with' inspection text messages
    (cherry picked from commit 1975e87; IDEA-CR-18678)

--- java/java-tests/testData/inspection/streamApiCallChains/beforeIterateListUnbox.java
+++ java/java-tests/testData/inspection/streamApiCallChains/beforeIterateListUnbox.java
@@ -1,4 +1,4 @@
-// "Replace IntStream.range().mapToLong() with collection.stream()" "true"
+// "Replace IntStream.range().mapToLong() with list.stream()" "true"
 
 import java.util.ArrayList;
 import java.util.List;
