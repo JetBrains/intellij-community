33dffdf5c63f6c3d5e683e437b00c4fa6dd35d5f
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2017-02-28 18:33:57 +0000

    Sort roots in the "unregistered roots detected" notification
    (cherry picked from commit e8c8a93610846d6e579af5c547d68d7248ed9aff)

--- platform/vcs-impl/src/com/intellij/openapi/vcs/roots/VcsRootProblemNotifier.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/roots/VcsRootProblemNotifier.java
@@ -39,15 +39,16 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.event.HyperlinkEvent;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
+import static com.intellij.openapi.util.io.FileUtil.toSystemDependentName;
 import static com.intellij.openapi.util.text.StringUtil.pluralize;
+import static com.intellij.openapi.vcs.VcsDirectoryMapping.PROJECT_CONSTANT;
 import static com.intellij.project.ProjectKt.guessProjectDir;
 import static com.intellij.util.ObjectUtils.notNull;
 import static com.intellij.util.containers.ContainerUtil.getFirstItem;
+import static com.intellij.util.containers.ContainerUtil.sorted;
+import static java.util.Collections.singletonList;
 
 /**
  * Searches for Vcs roots problems via {@link VcsRootErrorsFinder} and notifies about them.
@@ -66,6 +67,11 @@ public class VcsRootProblemNotifier {
 
   @Nullable private Notification myNotification;
   @NotNull private final Object NOTIFICATION_LOCK = new Object();
+
+  @NotNull private static final Function<VcsRootError, String> ROOT_TO_PRESENTABLE = rootError -> {
+    if (rootError.getMapping().equals(PROJECT_CONSTANT)) return StringUtil.escapeXml(rootError.getMapping());
+    return toSystemDependentName(rootError.getMapping());
+  };
 
   public static VcsRootProblemNotifier getInstance(@NotNull Project project) {
     return new VcsRootProblemNotifier(project);
@@ -101,7 +107,7 @@ public class VcsRootProblemNotifier {
           FileUtil.isAncestor(projectDir.getPath(), mappingPath, false) &&
           Registry.is("vcs.auto.add.single.root")) {
         VcsDirectoryMapping mapping = new VcsDirectoryMapping(mappingPath, singleUnregRoot.getVcsKey().getName());
-        myVcsManager.setDirectoryMappings(Collections.singletonList(mapping));
+        myVcsManager.setDirectoryMappings(singletonList(mapping));
         LOG.info("Added " + mapping.getVcs() + " root " + mapping + " as the only auto-detected root.");
         return;
       }
@@ -128,7 +134,7 @@ public class VcsRootProblemNotifier {
 
   private boolean isUnderOrAboveProjectDir(@NotNull String mapping) {
     String projectDir = ObjectUtils.assertNotNull(myProject.getBasePath());
-    return mapping.equals(VcsDirectoryMapping.PROJECT_CONSTANT) ||
+    return mapping.equals(PROJECT_CONSTANT) ||
            FileUtil.isAncestor(projectDir, mapping, false) ||
            FileUtil.isAncestor(mapping, projectDir, false);
   }
@@ -156,24 +162,17 @@ public class VcsRootProblemNotifier {
   @NotNull
   private static String makeDescription(@NotNull Collection<VcsRootError> unregisteredRoots,
                                         @NotNull Collection<VcsRootError> invalidRoots) {
-    Function<VcsRootError, String> rootToDisplayableString = rootError -> {
-      if (rootError.getMapping().equals(VcsDirectoryMapping.PROJECT_CONSTANT)) {
-        return StringUtil.escapeXml(rootError.getMapping());
-      }
-      return FileUtil.toSystemDependentName(rootError.getMapping());
-    };
-
     StringBuilder description = new StringBuilder();
     if (!invalidRoots.isEmpty()) {
       if (invalidRoots.size() == 1) {
         VcsRootError rootError = invalidRoots.iterator().next();
         String vcsName = rootError.getVcsKey().getName();
         description.append(String.format("The directory %s is registered as a %s root, but no %s repositories were found there.",
-                                         rootToDisplayableString.fun(rootError), vcsName, vcsName));
+                                         ROOT_TO_PRESENTABLE.fun(rootError), vcsName, vcsName));
       }
       else {
         description.append("The following directories are registered as VCS roots, but they are not: <br/>" +
-                           StringUtil.join(invalidRoots, rootToDisplayableString, "<br/>"));
+                           joinRootsForPresentation(invalidRoots));
       }
       description.append("<br/>");
     }
@@ -182,11 +181,11 @@ public class VcsRootProblemNotifier {
       if (unregisteredRoots.size() == 1) {
         VcsRootError unregisteredRoot = unregisteredRoots.iterator().next();
         description.append(String.format("The directory %s is under %s, but is not registered in the Settings.",
-                                         rootToDisplayableString.fun(unregisteredRoot), unregisteredRoot.getVcsKey().getName()));
+                                         ROOT_TO_PRESENTABLE.fun(unregisteredRoot), unregisteredRoot.getVcsKey().getName()));
       }
       else {
         description.append("The following directories are roots of VCS repositories, but they are not registered in the Settings: <br/>" +
-                           StringUtil.join(unregisteredRoots, rootToDisplayableString, "<br/>"));
+                           joinRootsForPresentation(unregisteredRoots));
       }
       description.append("<br/>");
     }
@@ -197,6 +196,16 @@ public class VcsRootProblemNotifier {
     description.append(add + configure + ignore);
 
     return description.toString();
+  }
+
+  @NotNull
+  private static String joinRootsForPresentation(@NotNull Collection<VcsRootError> errors) {
+
+    return StringUtil.join(sorted(errors, (root1, root2) -> {
+      if (root1.getMapping().equals(PROJECT_CONSTANT)) return -1;
+      if (root2.getMapping().equals(PROJECT_CONSTANT)) return 1;
+      return root1.getMapping().compareTo(root2.getMapping());
+    }), ROOT_TO_PRESENTABLE, "<br/>");
   }
 
   @NotNull
