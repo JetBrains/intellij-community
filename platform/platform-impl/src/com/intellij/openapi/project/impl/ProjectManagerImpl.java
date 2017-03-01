24f057ba1c90df17705020a8a0e53327a445914b
Alexey Kudravtsev <cdr@intellij.com>
2017-03-01 20:35:53 +0300

    even more wrong fix for "IncorrectOperationException: UnsafeWeakList is not RandomAccess" rewritten

--- platform/platform-impl/src/com/intellij/openapi/project/impl/ProjectManagerImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/project/impl/ProjectManagerImpl.java
@@ -221,7 +221,7 @@ public class ProjectManagerImpl extends ProjectManagerEx implements Disposable {
     if (getLeakedProjects().count() >= MAX_LEAKY_PROJECTS) {
       List<Project> copy = getLeakedProjects().collect(Collectors.toCollection(UnsafeWeakList::new));
       myProjects.clear();
-      if (new ArrayList<>(copy).size() >= MAX_LEAKY_PROJECTS) {
+      if (ContainerUtil.collect(copy.iterator()).size() >= MAX_LEAKY_PROJECTS) {
         throw new TooManyProjectLeakedException(copy);
       }
     }
