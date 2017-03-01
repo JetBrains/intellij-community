157b31fdb33cada7a890b97c3b0ec40dfbb1eacf
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2017-03-01 12:54:11 +0700

    Java9CollectionFactoryInspectionTest#HashMap10: expected code fixed

--- java/java-tests/testData/inspection/java9CollectionFactory/afterHashMap10.java
+++ java/java-tests/testData/inspection/java9CollectionFactory/afterHashMap10.java
@@ -4,6 +4,14 @@ import java.util.*;
 public class Test {
   public void test() {
     Map<String, String> myMap;
-      myMap = Map.<String, String>of("a", "1", "b", "1", "c", "1", "d", "1", "e", "1", "f", "1", "g", "1", "h", "1", "i", "1", "j", "1");
+
+      myMap = Map.<String, String>of("a", "1", "b", "1", "c", "1",
+
+              // D follows
+              "d", "1", "e", /* this is also 1*/ "1", "f", "1", "g", "1", // G is important!
+              "h", "1", "i", "1",
+
+    /* Finally J */
+    /* why not putting comment inside the call expression? */"j", "1");
   }
 }
