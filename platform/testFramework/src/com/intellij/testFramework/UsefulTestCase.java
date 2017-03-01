034f185463facb523ab568c91b41cb8aee7d7f65
Eldar Abusalimov <eldar.abusalimov@jetbrains.com>
2017-03-01 14:01:00 +0300

    testFramework: (testing) Fix type parameters in assertThrows()
    Follows-up: IDEA-CR-18741 testFramework: (testing) Add UsefulTestCase.assertThrows()

--- platform/testFramework/src/com/intellij/testFramework/UsefulTestCase.java
+++ platform/testFramework/src/com/intellij/testFramework/UsefulTestCase.java
@@ -877,7 +877,7 @@ public abstract class UsefulTestCase extends TestCase {
    * @param exceptionClass   Expected exception type
    * @param runnable         Block annotated with some exception type
    */
-  protected <T extends Throwable> void assertThrows(@NotNull Class<? extends T> exceptionClass,
+  protected <T extends Throwable> void assertThrows(@NotNull Class<? extends Throwable> exceptionClass,
                                                     @NotNull ThrowableRunnable<T> runnable) throws T {
     assertThrows(exceptionClass, null, runnable);
   }
@@ -891,7 +891,7 @@ public abstract class UsefulTestCase extends TestCase {
    * @param runnable         Block annotated with some exception type
    */
   @SuppressWarnings({"unchecked", "SameParameterValue"})
-  protected <T extends Throwable> void assertThrows(@NotNull Class<?> exceptionClass, @Nullable String expectedErrorMsg,
+  protected <T extends Throwable> void assertThrows(@NotNull Class<? extends Throwable> exceptionClass, @Nullable String expectedErrorMsg,
                                                     @NotNull ThrowableRunnable<T> runnable) throws T {
     assertExceptionOccurred(true, new AbstractExceptionCase() {
       @Override
