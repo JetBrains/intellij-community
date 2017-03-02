613e8be0d4fd11999af850f96f090f9f4dd78984
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2017-03-02 22:58:28 +0000

    Don't read branch.lock files as branches
    Since Git 2.10 (92b1551b1d407065f961ffd1d972481063a0edcc) such
    lock files can be created to synchronize ref updates.
    
    We used to read them as branches and later fail either because no hash
    was available, or because the files were removed.
    
    (cherry picked from commit 0668f251cff83ddf0dade28686b408a6f6e7b13a)

--- plugins/git4idea/src/git4idea/repo/GitRepositoryReader.java
+++ plugins/git4idea/src/git4idea/repo/GitRepositoryReader.java
@@ -31,6 +31,7 @@ import com.intellij.vcs.log.Hash;
 import com.intellij.vcs.log.impl.HashImpl;
 import git4idea.*;
 import git4idea.branch.GitBranchUtil;
+import git4idea.validators.GitRefNameValidator;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -272,7 +273,7 @@ class GitRepositoryReader {
           if (relativePath != null) {
             String branchName = prefix + FileUtil.toSystemIndependentName(relativePath);
             String hash = loadHashFromBranchFile(file);
-            if (hash != null) {
+            if (hash != null && GitRefNameValidator.getInstance().checkInput(branchName)) {
               result.put(branchName, hash);
             }
           }
