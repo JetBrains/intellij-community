6479e586fb68a0cf3963508cca138017003fe8f6
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-03-07 17:36:30 +0300

    Add equals/hashCode into TraceElement implementation

--- plugins/stream-debugger/src/main/java/com/intellij/debugger/streams/trace/smart/TraceElementImpl.java
+++ plugins/stream-debugger/src/main/java/com/intellij/debugger/streams/trace/smart/TraceElementImpl.java
@@ -3,6 +3,8 @@ package com.intellij.debugger.streams.trace.smart;
 import com.sun.jdi.Value;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 /**
  * @author Vitaliy.Bibaev
  */
@@ -13,6 +15,22 @@ public class TraceElementImpl implements TraceElement {
   public TraceElementImpl(int time, @NotNull Value value) {
     myTime = time;
     myValue = value;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof TraceElement) {
+      final int time = ((TraceElement)obj).getTime();
+      final Value value = ((TraceElement)obj).getValue();
+      return time == myTime && myValue.equals(value);
+    }
+
+    return false;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(myTime, myValue);
   }
 
   @Override
