d83ec825c5749e73a1f6c2972c683efc9cccc119
Sergey Karashevich <sergey.karashevich@jetbrains.com>
2017-03-09 20:27:36 +0100

    [gui-test] extract and separate all java-specific configuration part; added JavaGuiTestConfigurator to specify java limitations

--- platform/testGuiFramework/src/com/intellij/testGuiFramework/idea/IdeGuiTest.java
+++ platform/testGuiFramework/src/com/intellij/testGuiFramework/idea/IdeGuiTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.intellij.testGuiFramework.framework;
+package com.intellij.testGuiFramework.idea;
 
 import com.intellij.openapi.projectRoots.JavaSdkVersion;
 
