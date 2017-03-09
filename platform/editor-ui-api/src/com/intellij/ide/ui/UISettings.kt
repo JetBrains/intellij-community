934b9d06684401d0df0fd75702c5ad2eae378cfb
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2017-03-09 17:43:08 +0100

    IDEA-169363 Internal error on stating RubyMine EAP

--- platform/editor-ui-api/src/com/intellij/ide/ui/UISettings.kt
+++ platform/editor-ui-api/src/com/intellij/ide/ui/UISettings.kt
@@ -303,7 +303,7 @@ class UISettings : BaseState(), PersistentStateComponent<UISettings> {
     val shadowInstance: UISettings
       get() {
         val app = ApplicationManager.getApplication()
-        return (if (app == null) null else instance) ?: UISettings().withDefFont()
+        return (if (app == null) null else instanceOrNull) ?: UISettings().withDefFont()
       }
 
     private val systemFontFaceAndSize: Pair<String, Int>
