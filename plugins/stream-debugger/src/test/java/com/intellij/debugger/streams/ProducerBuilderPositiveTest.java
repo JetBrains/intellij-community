8bff06a31f4c4c00a05663df58fb5cf425867966
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-03-14 15:57:45 +0300

    Add tests to stream producers

---
+++ plugins/stream-debugger/src/test/java/com/intellij/debugger/streams/ProducerBuilderPositiveTest.java
@@ -0,0 +1,44 @@
+package com.intellij.debugger.streams;
+
+import com.intellij.debugger.streams.wrapper.StreamChain;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author Vitaliy.Bibaev
+ */
+public class ProducerBuilderPositiveTest extends StreamChainBuilderPositiveTestBase {
+  public void testCollectionStream() throws Exception {
+    doTest();
+  }
+
+  public void testCustomSource() throws Exception {
+    doTest();
+  }
+
+  public void testIntStreamRange() throws Exception {
+    doTest();
+  }
+
+  public void testIntStreamRangeClosed() throws Exception {
+    doTest();
+  }
+
+  public void testIterate() throws Exception {
+    doTest();
+  }
+
+  public void testConcat() throws Exception {
+    doTest();
+  }
+
+  @Override
+  protected void checkResultChain(StreamChain chain) {
+    assertNotNull(chain.getProducerCall());
+  }
+
+  @NotNull
+  @Override
+  protected String getDirectoryName() {
+    return "producer";
+  }
+}
