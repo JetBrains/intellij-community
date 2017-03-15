02f98436fb8291780ffe1f6c1e80cf49f439d04d
Yaroslav Lepenkin <yaroslav.lepenkin@jetbrains.com>
2017-03-15 18:20:25 +0300

    experimental sorting

--- src/com/intellij/plugin/Actions.kt
+++ src/com/intellij/plugin/Actions.kt
@@ -1,5 +1,7 @@
 package com.intellij.plugin
 
+import com.intellij.codeInsight.completion.CodeCompletionHandlerBase
+import com.intellij.codeInsight.completion.CompletionType
 import com.intellij.codeInsight.lookup.LookupManager
 import com.intellij.codeInsight.lookup.impl.LookupImpl
 import com.intellij.notification.Notification
@@ -30,15 +32,16 @@ class ToggleMlSorting: AnAction() {
         val editor = CommonDataKeys.EDITOR.getData(e.dataContext)
         val project = CommonDataKeys.PROJECT.getData(e.dataContext)
 
-        val content = if (before) "Reranking disabled" else "Reranking enabled"
-        val collector = "Completion Stats Collector"
-        val notification = Notification(collector, collector, content, NotificationType.INFORMATION)
-        
-        notification.notify(project)
-        
-        
-        val lookup = LookupManager.getActiveLookup(editor) as? LookupImpl ?: return
-        lookup.resort(false)
+
+        val lookup = LookupManager.getActiveLookup(editor)
+        if (lookup != null) {
+            CodeCompletionHandlerBase(CompletionType.BASIC).invokeCompletion(project!!, editor!!, 10, false, false)
+        } else {
+            val content = if (before) "Reranking disabled" else "Reranking enabled"
+            val collector = "Completion Stats Collector"
+            val notification = Notification(collector, collector, content, NotificationType.INFORMATION)
+            notification.notify(project)
+        }
     }
     
 }
