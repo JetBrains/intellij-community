2a88fad12d4828401b31f6bd63800090637202fa
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-03-15 19:51:41 +0300

    Store type of objects in stream in PeekCall

--- plugins/stream-debugger/src/main/java/com/intellij/debugger/streams/trace/smart/handler/PeekCall.java
+++ plugins/stream-debugger/src/main/java/com/intellij/debugger/streams/trace/smart/handler/PeekCall.java
@@ -1,17 +1,20 @@
 package com.intellij.debugger.streams.trace.smart.handler;
 
-import com.intellij.debugger.streams.wrapper.StreamCall;
+import com.intellij.debugger.streams.trace.smart.handler.type.GenericType;
+import com.intellij.debugger.streams.wrapper.IntermediateStreamCall;
 import com.intellij.debugger.streams.wrapper.StreamCallType;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Vitaliy.Bibaev
  */
-public class PeekCall implements StreamCall {
+public class PeekCall implements IntermediateStreamCall {
   private final String myLambda;
+  private final GenericType myElementType;
 
-  public PeekCall(@NotNull String lambda) {
+  public PeekCall(@NotNull String lambda, @NotNull GenericType elementType) {
     myLambda = lambda;
+    myElementType = elementType;
   }
 
   @NotNull
@@ -30,5 +33,17 @@ public class PeekCall implements StreamCall {
   @Override
   public StreamCallType getType() {
     return StreamCallType.INTERMEDIATE;
+  }
+
+  @NotNull
+  @Override
+  public GenericType getTypeAfter() {
+    return myElementType;
+  }
+
+  @NotNull
+  @Override
+  public GenericType getTypeBefore() {
+    return myElementType;
   }
 }
