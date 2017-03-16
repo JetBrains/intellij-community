48bf90ef5cdf43d04ee83ccea8b0fd6f0b37be27
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-03-16 19:06:31 +0300

    Remove redundant action handler, perform an action in TraceStreamAction

--- plugins/stream-debugger/src/main/java/com/intellij/debugger/streams/action/TraceStreamAction.java
+++ plugins/stream-debugger/src/main/java/com/intellij/debugger/streams/action/TraceStreamAction.java
@@ -1,32 +1,77 @@
 package com.intellij.debugger.streams.action;
 
+import com.intellij.debugger.engine.evaluation.EvaluationContextImpl;
+import com.intellij.debugger.streams.psi.DebuggerPositionResolver;
+import com.intellij.debugger.streams.psi.impl.DebuggerPositionResolverImpl;
+import com.intellij.debugger.streams.resolve.ResolvedTrace;
+import com.intellij.debugger.streams.trace.ResolvedTracingResult;
+import com.intellij.debugger.streams.trace.TracingCallback;
+import com.intellij.debugger.streams.trace.TracingResult;
+import com.intellij.debugger.streams.trace.smart.MapToArrayTracerImpl;
+import com.intellij.debugger.streams.ui.EvaluationAwareTraceWindow;
+import com.intellij.debugger.streams.wrapper.StreamChain;
+import com.intellij.debugger.streams.wrapper.impl.StreamChainBuilder;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
+import com.intellij.xdebugger.XDebugSession;
+import com.intellij.xdebugger.XDebuggerManager;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 /**
  * @author Vitaliy.Bibaev
  */
 public class TraceStreamAction extends AnAction {
-  private static class Holder {
-    private static final JvmStreamDebuggerActionHandler HANDLER = new JvmStreamDebuggerActionHandler();
+  private static final Logger LOG = Logger.getInstance(TraceStreamAction.class);
+
+  private final DebuggerPositionResolver myPositionResolver = new DebuggerPositionResolverImpl();
+
+  @Override
+  public void update(@NotNull AnActionEvent e) {
+    final XDebugSession session = getCurrentSession(e);
+    final PsiElement element = session == null ? null : myPositionResolver.getNearestElementToBreakpoint(session);
+    e.getPresentation().setEnabled(element != null && StreamChainBuilder.checkStreamExists(element));
   }
 
   @Override
-  public void update(AnActionEvent e) {
-    final Project project = e.getProject();
-    if (project != null) {
-      e.getPresentation().setEnabled(Holder.HANDLER.isEnabled(project));
-      return;
+  public void actionPerformed(@NotNull AnActionEvent e) {
+    final XDebugSession session = getCurrentSession(e);
+    final PsiElement element = session == null ? null : myPositionResolver.getNearestElementToBreakpoint(session);
+    final StreamChain chain = element == null ? null : StreamChainBuilder.tryBuildChain(element);
+
+    if (chain != null) {
+      final EvaluationAwareTraceWindow window = new EvaluationAwareTraceWindow(session.getProject(), chain);
+      ApplicationManager.getApplication().invokeLater(window::show);
+      new MapToArrayTracerImpl(session).trace(chain, new TracingCallback() {
+        @Override
+        public void evaluated(@NotNull TracingResult result, @NotNull EvaluationContextImpl context) {
+          final ResolvedTracingResult resolvedTrace = result.resolve();
+          final List<ResolvedTrace> calls = resolvedTrace.getResolvedCalls();
+          ApplicationManager.getApplication()
+            .invokeLater(() -> window.setTrace(calls, result.getResult(), context));
+        }
+
+        @Override
+        public void failed(@NotNull String traceExpression, @NotNull String reason) {
+          LOG.warn(reason + System.lineSeparator() + "expression:" + System.lineSeparator() + traceExpression);
+          ApplicationManager.getApplication().invokeLater(() -> window.setFailMessage(reason));
+        }
+      });
     }
-    e.getPresentation().setEnabled(false);
+    else {
+      LOG.warn("stream chain is not built");
+    }
   }
 
-  @Override
-  public void actionPerformed(AnActionEvent e) {
+  @Nullable
+  private XDebugSession getCurrentSession(@NotNull AnActionEvent e) {
     final Project project = e.getProject();
-    if (project != null && Holder.HANDLER.isEnabled(project)) {
-      Holder.HANDLER.perform(project);
-    }
+    return project == null ? null : XDebuggerManager.getInstance(project).getCurrentSession();
   }
 }
