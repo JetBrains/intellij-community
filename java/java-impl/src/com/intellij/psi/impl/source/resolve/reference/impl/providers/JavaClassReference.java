75d9b8aa69ed55bf1c89447b507a274397efe1c4
Anna.Kozlova <anna.kozlova@jetbrains.com>
2017-03-17 11:53:49 +0100

    restore optimization but accept fields/members (IDEA-CR-19430)

--- java/java-impl/src/com/intellij/psi/impl/source/resolve/reference/impl/providers/JavaClassReference.java
+++ java/java-impl/src/com/intellij/psi/impl/source/resolve/reference/impl/providers/JavaClassReference.java
@@ -165,6 +165,11 @@ public class JavaClassReference extends GenericReference implements PsiJavaRefer
   }
 
   @Override
+  public boolean isReferenceTo(PsiElement element) {
+    return (element instanceof PsiMember || element instanceof PsiPackage) && super.isReferenceTo(element);
+  }
+
+  @Override
   public TextRange getRangeInElement() {
     return myRange;
   }
