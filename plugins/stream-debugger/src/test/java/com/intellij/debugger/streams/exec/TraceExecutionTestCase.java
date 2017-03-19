433705c0de1e97618ad694f8f0e1e4afb1d9b55c
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-03-19 11:32:46 +0300

    Small fixes in test case with debugger

--- plugins/stream-debugger/src/test/java/com/intellij/debugger/streams/exec/TraceExecutionTestCase.java
+++ plugins/stream-debugger/src/test/java/com/intellij/debugger/streams/exec/TraceExecutionTestCase.java
@@ -3,7 +3,6 @@ package com.intellij.debugger.streams.exec;
 import com.intellij.debugger.DebuggerTestCase;
 import com.intellij.debugger.engine.evaluation.EvaluationContextImpl;
 import com.intellij.debugger.impl.OutputChecker;
-import com.intellij.debugger.streams.JdkManager;
 import com.intellij.debugger.streams.psi.DebuggerPositionResolver;
 import com.intellij.debugger.streams.psi.impl.DebuggerPositionResolverImpl;
 import com.intellij.debugger.streams.resolve.ResolvedTrace;
@@ -15,9 +14,7 @@ import com.intellij.debugger.streams.wrapper.impl.StreamChainBuilder;
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.process.ProcessOutputTypes;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.util.Computable;
-import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess;
 import com.intellij.psi.PsiElement;
 import com.intellij.xdebugger.XDebugSession;
 import com.intellij.xdebugger.XDebugSessionListener;
@@ -32,6 +29,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Function;
 
 /**
@@ -46,9 +44,12 @@ public class TraceExecutionTestCase extends DebuggerTestCase {
     doTest(false);
   }
 
+  public void testMap() throws InterruptedException, ExecutionException, InvocationTargetException {
+    doTest(false);
+  }
+
   @Override
   protected void setUp() throws Exception {
-    VfsRootAccess.allowRootAccess(getTestRootDisposable(), JdkManager.JDK18_PATH);
     super.setUp();
   }
 
@@ -62,11 +63,6 @@ public class TraceExecutionTestCase extends DebuggerTestCase {
     return new File("testData/" + getRelativeTestPath()).getAbsolutePath();
   }
 
-  @Override
-  protected Sdk getTestProjectJdk() {
-    return JdkManager.getMockJdk18();
-  }
-
   protected void doTest(boolean isResultNull) throws InterruptedException, ExecutionException, InvocationTargetException {
     final String name = getTestName(false);
 
@@ -74,10 +70,15 @@ public class TraceExecutionTestCase extends DebuggerTestCase {
     final XDebugSession session = getDebuggerSession().getXDebugSession();
     assertNotNull(session);
 
+    final AtomicBoolean completed = new AtomicBoolean(false);
     session.addSessionListener(new XDebugSessionListener() {
       @Override
       public void sessionPaused() {
-        session.removeSessionListener(this);
+        if (completed.getAndSet(true)) {
+          resume();
+          return;
+        }
+
         printContext(getDebugProcess().getDebuggerContext());
         final StreamChain chain = ApplicationManager.getApplication().runReadAction((Computable<StreamChain>)() -> {
           final PsiElement elementAtBreakpoint = myPositionResolver.getNearestElementToBreakpoint(session);
@@ -106,6 +107,10 @@ public class TraceExecutionTestCase extends DebuggerTestCase {
                             @Nullable TracingResult result,
                             @Nullable String evaluationError) {
         handleResults(chain, result, evaluationError, isResultNull);
+        resume();
+      }
+
+      private void resume() {
         ApplicationManager.getApplication().invokeLater(session::resume);
       }
     }, getTestRootDisposable());
@@ -115,7 +120,6 @@ public class TraceExecutionTestCase extends DebuggerTestCase {
                                @Nullable TracingResult result,
                                @Nullable String evaluationError,
                                boolean resultMustBeNull) {
-
     assertNotNull(chain);
     assertNull(evaluationError);
     assertNotNull(result);
@@ -139,7 +143,6 @@ public class TraceExecutionTestCase extends DebuggerTestCase {
     }
     else {
       assertNotNull(result);
-      println(result.type().name(), ProcessOutputTypes.SYSTEM);
     }
   }
 
@@ -178,13 +181,13 @@ public class TraceExecutionTestCase extends DebuggerTestCase {
   }
 
   @NotNull
-  private String evalTimesRepresentation(@NotNull String before, int elementTime, @NotNull String after) {
-    before = before.isEmpty() ? "nothing" : before;
-    after = after.isEmpty() ? "nothing" : after;
+  private static String evalTimesRepresentation(@NotNull String before, int elementTime, @NotNull String after) {
+    before = replaceIfEmpty(before);
+    after = replaceIfEmpty(after);
     return String.format("\t %s -> %d -> %s", before, elementTime, after);
   }
 
-  private void checkTracesIsCorrectInBothDirections(@NotNull List<ResolvedTrace> resolvedTraces) {
+  private static void checkTracesIsCorrectInBothDirections(@NotNull List<ResolvedTrace> resolvedTraces) {
     for (int i = 1, size = resolvedTraces.size(); i < size; i++) {
       final ResolvedTrace previous = resolvedTraces.get(i - 1);
       final ResolvedTrace current = resolvedTraces.get(i);
@@ -192,7 +195,7 @@ public class TraceExecutionTestCase extends DebuggerTestCase {
     }
   }
 
-  private void checkNeighborTraces(@NotNull ResolvedTrace left, @NotNull ResolvedTrace right) {
+  private static void checkNeighborTraces(@NotNull ResolvedTrace left, @NotNull ResolvedTrace right) {
     final Set<TraceElement> leftValues = new HashSet<>(left.getValues());
     final Set<TraceElement> rightValues = new HashSet<>(right.getValues());
 
@@ -200,10 +203,10 @@ public class TraceExecutionTestCase extends DebuggerTestCase {
     checkThatMappingsIsCorrect(rightValues, leftValues, right::getPreviousValues, left::getNextValues);
   }
 
-  private void checkThatMappingsIsCorrect(@NotNull Set<TraceElement> prev,
-                                          @NotNull Set<TraceElement> next,
-                                          @NotNull Function<TraceElement, List<TraceElement>> toNext,
-                                          @NotNull Function<TraceElement, List<TraceElement>> toPrev) {
+  private static void checkThatMappingsIsCorrect(@NotNull Set<TraceElement> prev,
+                                                 @NotNull Set<TraceElement> next,
+                                                 @NotNull Function<TraceElement, List<TraceElement>> toNext,
+                                                 @NotNull Function<TraceElement, List<TraceElement>> toPrev) {
     for (final TraceElement leftElement : prev) {
       final List<TraceElement> mapToRight = toNext.apply(leftElement);
       for (final TraceElement rightElement : mapToRight) {
@@ -214,8 +217,13 @@ public class TraceExecutionTestCase extends DebuggerTestCase {
   }
 
   @NotNull
-  private String valuesOrderToString(@NotNull Map<Integer, TraceElement> values) {
-    return StreamEx.of(values.keySet()).sorted().joining(",");
+  private static String valuesOrderToString(@NotNull Map<Integer, TraceElement> values) {
+    return replaceIfEmpty(StreamEx.of(values.keySet()).sorted().joining(","));
+  }
+
+  @NotNull
+  private static String replaceIfEmpty(@NotNull String str) {
+    return str.isEmpty() ? "nothing" : str;
   }
 
   @NotNull
