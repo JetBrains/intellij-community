69a51251e69f9f223bb6baa005e5b4cb8c48d71e
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2017-03-20 12:50:08 +0100

    extract StandaloneDocument to avoid code duplication

--- platform/core-impl/src/com/intellij/openapi/editor/impl/FrozenDocument.java
+++ platform/core-impl/src/com/intellij/openapi/editor/impl/FrozenDocument.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,8 +33,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.beans.PropertyChangeListener;
-import java.util.Collections;
-import java.util.List;
 
 /**
  * @author peter
@@ -148,12 +146,6 @@ public class FrozenDocument implements DocumentEx {
   @Override
   public void setInBulkUpdate(boolean value) {
     throw new UnsupportedOperationException();
-  }
-
-  @NotNull
-  @Override
-  public List<RangeMarker> getGuardedBlocks() {
-    return Collections.emptyList();
   }
 
   @Override
