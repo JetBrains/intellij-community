cb4c37e581b38cd2f1094fecd8a649e344feb00c
Andrey Vokin <andrey.vokin@jetbrains.com>
2017-03-20 17:30:48 +0100

    Ruby. Slim. Supported colon ":" as tag delimiter

--- platform/core-impl/src/com/intellij/indentation/IndentationParser.java
+++ platform/core-impl/src/com/intellij/indentation/IndentationParser.java
@@ -113,6 +113,10 @@ public abstract class IndentationParser implements PsiParser {
             // sometimes we do not have EOL between indents
             eolSeen = true;
           }
+          if (isCustomTagDelimiter(type)) {
+            builder.advanceLexer();
+            eolSeen = true;
+          }
           if (eolSeen) {
             if (startLineMarker != null) {
               startLineMarker.rollbackTo();
@@ -163,6 +167,10 @@ public abstract class IndentationParser implements PsiParser {
 
     fileMarker.done(root);
     return builder.getTreeBuilt();
+  }
+
+  protected boolean isCustomTagDelimiter(IElementType type) {
+    return false;
   }
 
   protected void closeBlock(final @NotNull PsiBuilder builder,
