9d178f61823753095af0dc9ba28c0804284d595c
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2017-03-21 12:34:28 +0100

    IDEA-162522 Password is not saved

--- platform/platform-impl/src/com/intellij/credentialStore/credentialPromt.kt
+++ platform/platform-impl/src/com/intellij/credentialStore/credentialPromt.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -73,7 +73,7 @@ fun askPassword(project: Project?,
 
     if (dialog(dialogTitle, project = project, panel = panel, focusedComponent = passwordField, errorText = error).showAndGet()) {
       val credentials = Credentials(attributes.userName, passwordField.password.nullize())
-      store.set(attributes, credentials, store.isMemoryOnly || rememberCheckBox!!.isSelected)
+      store.set(attributes, credentials, store.isMemoryOnly || !rememberCheckBox!!.isSelected)
       credentials.getPasswordAsString()
     }
     else {
