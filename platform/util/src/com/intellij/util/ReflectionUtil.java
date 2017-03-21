b655a0493638e22ded32992e4523e62035c97941
andrey zaytsev <andrey.zaytsev@jetbrains.com>
2017-03-21 16:28:15 +0300

    fix merge artifacts

--- platform/util/src/com/intellij/util/ReflectionUtil.java
+++ platform/util/src/com/intellij/util/ReflectionUtil.java
@@ -28,10 +28,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.*;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class ReflectionUtil {
   private static final Logger LOG = Logger.getInstance("#com.intellij.util.ReflectionUtil");
@@ -620,6 +617,31 @@ public class ReflectionUtil {
 
   public static boolean isAssignable(@NotNull Class<?> ancestor, @NotNull Class<?> descendant) {
     return ancestor == descendant || ancestor.isAssignableFrom(descendant);
+  }
+
+  public static List<Class<?>> getAllInterfaces(final Class<?> cls) {
+    if (cls == null) {
+      return null;
+    }
+
+    final HashSet<Class<?>> interfacesFound = new HashSet<Class<?>>();
+    getAllInterfaces(cls, interfacesFound);
+
+    return new ArrayList<Class<?>>(interfacesFound);
+  }
+
+  private static void getAllInterfaces(Class<?> clazz, final HashSet<Class<?>> interfacesFound) {
+    while (clazz != null) {
+      final Class<?>[] interfaces = clazz.getInterfaces();
+
+      for (final Class<?> i : interfaces) {
+        if (interfacesFound.add(i)) {
+          getAllInterfaces(i, interfacesFound);
+        }
+      }
+
+      clazz = clazz.getSuperclass();
+    }
   }
 
   @NotNull
