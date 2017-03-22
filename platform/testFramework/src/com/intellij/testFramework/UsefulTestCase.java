e32a894da28aed3cd5ce273161ea59b403ecdead
Anton Makeev <Anton.Makeev@jetbrains.com>
2017-03-22 11:42:26 +0100

    removed inheritance from UsefulTestCase where not needed

--- platform/testFramework/src/com/intellij/testFramework/UsefulTestCase.java
+++ platform/testFramework/src/com/intellij/testFramework/UsefulTestCase.java
@@ -877,8 +877,8 @@ public abstract class UsefulTestCase extends TestCase {
    * @param exceptionClass   Expected exception type
    * @param runnable         Block annotated with some exception type
    */
-  protected <T extends Throwable> void assertThrows(@NotNull Class<? extends Throwable> exceptionClass,
-                                                    @NotNull ThrowableRunnable<T> runnable) throws T {
+  public static <T extends Throwable> void assertThrows(@NotNull Class<? extends Throwable> exceptionClass,
+                                                           @NotNull ThrowableRunnable<T> runnable) throws T {
     assertThrows(exceptionClass, null, runnable);
   }
 
@@ -891,8 +891,9 @@ public abstract class UsefulTestCase extends TestCase {
    * @param runnable         Block annotated with some exception type
    */
   @SuppressWarnings({"unchecked", "SameParameterValue"})
-  protected <T extends Throwable> void assertThrows(@NotNull Class<? extends Throwable> exceptionClass, @Nullable String expectedErrorMsg,
-                                                    @NotNull ThrowableRunnable<T> runnable) throws T {
+  public static <T extends Throwable> void assertThrows(@NotNull Class<? extends Throwable> exceptionClass,
+                                                        @Nullable String expectedErrorMsg,
+                                                        @NotNull ThrowableRunnable<T> runnable) throws T {
     assertExceptionOccurred(true, new AbstractExceptionCase() {
       @Override
       public Class<Throwable> getExpectedExceptionClass() {
