836c4981785efe49ed29999eb59c3a177efae7e3
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2017-03-22 16:34:15 +0700

    IDEA-168201 Intention to extract a set from "foo".equals(xyz) || "bar".equals(xyz)

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/extractSetFromComparison/afterParentheses.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+// "Extract Set from comparison chain" "true"
+public class Test {
+  public static final String BAR = "bar";
+    private static final Set<String> PROPERTIES = new HashSet<>(Arrays.asList("foo", BAR, "baz"));
+
+    void testOr(int i, String property) {
+    int PROPERTIES;
+
+    if(i > 0 && Test.PROPERTIES.contains(property)) {
+      System.out.println("foobarbaz");
+    }
+  }
+}
