839d9b7218453bb68fcca94e17373c7cb2cdc094
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2017-03-22 15:42:36 +0300

    PY-22767 Fixed: Zip with 5+ arguments raises "unexpected arguments" warning
    Add `zip` stubs for 5 arguments and fallback for 6+ arguments.

--- python/helpers/typeshed/stdlib/3/builtins.pyi
+++ python/helpers/typeshed/stdlib/3/builtins.pyi
@@ -24,6 +24,7 @@ _T1 = TypeVar('_T1')
 _T2 = TypeVar('_T2')
 _T3 = TypeVar('_T3')
 _T4 = TypeVar('_T4')
+_T5 = TypeVar('_T5')
 _TT = TypeVar('_TT', bound='type')
 
 class staticmethod: pass   # Special, only valid as a decorator.
@@ -842,7 +843,15 @@ def zip(iter1: Iterable[_T1], iter2: Iterable[_T2],
 @overload
 def zip(iter1: Iterable[_T1], iter2: Iterable[_T2], iter3: Iterable[_T3],
         iter4: Iterable[_T4]) -> Iterator[Tuple[_T1, _T2,
-                                               _T3, _T4]]: ...  # TODO more than four iterables
+                                               _T3, _T4]]: ...
+@overload
+def zip(iter1: Iterable[_T1], iter2: Iterable[_T2], iter3: Iterable[_T3],
+        iter4: Iterable[_T4], iter5: Iterable[_T5]) -> Iterator[Tuple[_T1, _T2,
+                                                                      _T3, _T4, _T5]]: ...
+@overload
+def zip(iter1: Iterable[Any], iter2: Iterable[Any], iter3: Iterable[Any],
+        iter4: Iterable[Any], iter5: Iterable[Any], iter6: Iterable[Any],
+        *iterables: Iterable[Any]) -> Iterator[Tuple[Any, ...]]: ...
 def __import__(name: str, globals: Dict[str, Any] = ..., locals: Dict[str, Any] = ...,
                fromlist: List[str] = ..., level: int = -1) -> Any: ...
 
--- python/testData/typeshed/stdlib/3/builtins_test.py
+++ python/testData/typeshed/stdlib/3/builtins_test.py
@@ -1,20 +1,3 @@
-def test_map():
-    assert map(str, [1, 2, 3]) + ['foo'] == ['1', '2', '3', 'foo']
-    assert map(lambda x: x.upper(), 'foo').pop() == 'O'
-
-
-def test_filter():
-    assert filter(lambda x: x % 2 == 0, [1, 2, 3]) + [4, 5, 6] == [2, 4, 5, 6]
-    assert filter(lambda x: x != 'f', 'foo') + 'bar' == 'oobar'
-
-
-def test_open(tmpdir):
-    path = tmpdir.join('foo')
-    path.write('test')
-    with open(str(path), 'r') as fd:
-        assert '\n'.join(fd.xreadlines()) == 'test'
-
-
 def test_zip():
     assert(list(zip([1])) == [(1,)])
     assert(list(zip([1], [2])) == [(1, 2,)])
