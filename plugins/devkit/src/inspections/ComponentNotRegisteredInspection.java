7c52ef8b42ed23a630a98305ad53dad7359489cb
Yann Cébron <yann.cebron@jetbrains.com>
2017-03-22 17:27:16 +0100

    IDEA-169555 Devkit: make all inspections work on non-plugin module type

--- plugins/devkit/src/inspections/ComponentNotRegisteredInspection.java
+++ plugins/devkit/src/inspections/ComponentNotRegisteredInspection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -160,6 +160,7 @@ public class ComponentNotRegisteredInspection extends DevKitInspectionBase {
     PsiFile psiFile = psiClass.getContainingFile();
     LOG.assertTrue(psiFile != null);
     Module module = ModuleUtilCore.findModuleForFile(psiFile.getVirtualFile(), project);
-    return PluginModuleType.isPluginModuleOrDependency(module);
+    return PluginModuleType.isPluginModuleOrDependency(module) ||
+           module != null && org.jetbrains.idea.devkit.util.PsiUtil.isPluginModule(module);
   }
 }
