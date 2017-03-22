b608abddda62f3883fe5d474e4326cad582fb894
Anna.Kozlova <anna.kozlova@jetbrains.com>
2017-03-22 18:04:03 +0100

    give up with move-rename usage info: fix for java only

--- platform/lang-api/src/com/intellij/refactoring/util/MoveRenameUsageInfo.java
+++ platform/lang-api/src/com/intellij/refactoring/util/MoveRenameUsageInfo.java
@@ -87,26 +87,6 @@ public class MoveRenameUsageInfo extends UsageInfo{
   }
 
   @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    if (!super.equals(o)) return false;
-
-    MoveRenameUsageInfo info = (MoveRenameUsageInfo)o;
-
-    if (myReferencedElement != null ? !myReferencedElement.equals(info.myReferencedElement) : info.myReferencedElement != null) return false;
-
-    return true;
-  }
-
-  @Override
-  public int hashCode() {
-    int result = super.hashCode();
-    result = 31 * result + (myReferencedElement != null ? myReferencedElement.hashCode() : 0);
-    return result;
-  }
-
-  @Override
   @Nullable
   public PsiReference getReference() {
     if (myReference != null) {
