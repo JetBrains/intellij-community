626dbc226a00c9fb30d76ef253a460b765779775
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2017-03-22 11:25:56 +0300

    EA-90091 - assert: CoreCommandProcessor.executeCommand

--- platform/platform-api/src/com/intellij/util/ui/EditorAdapter.java
+++ platform/platform-api/src/com/intellij/util/ui/EditorAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -67,6 +67,7 @@ public class EditorAdapter {
       lines = new ArrayList<>(myLines);
       myLines.clear();
     }
+    if (myEditor.isDisposed() || myProject != null && myProject.isDisposed()) return;
     ApplicationManager.getApplication().runWriteAction(writingCommand(lines));
   }
 
