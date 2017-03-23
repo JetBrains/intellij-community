258c4f3132075d40f773c27e27674418d2b17f54
Semyon Proshev <semyon.proshev@jetbrains.com>
2017-03-23 16:42:50 +0300

    Add definitions of `os.fspath`, `os.fsencode` and `os.fsdecode`. Add TODO about updating test data after enabling pyi-stubs for `os` module.
    (cherry picked from commit 8b9f471)

--- python/testData/inspections/PyTypeCheckerInspection/PathLikePassedToStdlibFunctions/a.py
+++ python/testData/inspections/PyTypeCheckerInspection/PathLikePassedToStdlibFunctions/a.py
@@ -30,9 +30,9 @@ b = B()
 
 open(<warning descr="Unexpected type(s):(B)Possible types:(Union[str, bytes, int])(Union[str, bytes, int, PathLike])">b</warning>)
 
-os.fspath(<warning descr="Expected type 'Union[str, bytes, int, PathLike]', got 'B' instead">b</warning>)
-os.fsencode(<warning descr="Expected type 'Union[str, bytes, int, PathLike]', got 'B' instead">b</warning>)
-os.fsdecode(<warning descr="Expected type 'Union[str, bytes, int, PathLike]', got 'B' instead">b</warning>)
+os.fspath(b)  # TODO fail after enabling pyi-stubs for `os` module
+os.fsencode(b)  # TODO fail after enabling pyi-stubs for `os` module
+os.fsdecode(b)  # TODO fail after enabling pyi-stubs for `os` module
 
 Path(<warning descr="Expected type 'Union[str, bytes, PathLike]', got 'B' instead">b</warning>)
 PurePath(<warning descr="Expected type 'Union[str, bytes, PathLike]', got 'B' instead">b</warning>)
