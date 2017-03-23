dbec83902c8f12e1dab9bd27b92a9c29d75e4428
Elizaveta Shashkova <elizaveta.shashkova@jetbrains.com>
2017-03-23 19:15:10 +0300

    Add a version field for checking versions number in cython extensions (PY-23249)
    (cherry picked from commit a0df8a7)
    
    (cherry picked from commit a441b80)

--- python/helpers/pydev/_pydevd_bundle/pydevd_trace_dispatch.py
+++ python/helpers/pydev/_pydevd_bundle/pydevd_trace_dispatch.py
@@ -5,6 +5,28 @@ import os
 use_cython = os.getenv('PYDEVD_USE_CYTHON', None)
 import sys
 
+
+def delete_old_compiled_extensions():
+    pydev_dir = os.path.dirname(os.path.dirname(__file__))
+    _pydevd_bundle_dir = os.path.dirname(__file__)
+    _pydevd_frame_eval_dir = os.path.join(pydev_dir, '_pydevd_frame_eval')
+    try:
+        import shutil
+        for file in os.listdir(_pydevd_bundle_dir):
+            if file.startswith("pydevd") and file.endswith(".so"):
+                os.remove(os.path.join(_pydevd_bundle_dir, file))
+        for file in os.listdir(_pydevd_frame_eval_dir):
+            if file.startswith("pydevd") and file.endswith(".so"):
+                os.remove(os.path.join(_pydevd_frame_eval_dir, file))
+        build_dir = os.path.join(pydev_dir, "build")
+        if os.path.exists(build_dir):
+            shutil.rmtree(os.path.join(pydev_dir, "build"))
+    except OSError:
+        from _pydev_bundle.pydev_monkey import log_error_once
+        log_error_once("warning: failed to delete old cython speedups. Please delete all *.so files from the directories "
+                       "\"%s\" and \"%s\"" % (_pydevd_bundle_dir, _pydevd_frame_eval_dir))
+
+
 if use_cython == 'YES':
     # We must import the cython version if forcing cython
     from _pydevd_bundle.pydevd_cython_wrapper import trace_dispatch as _trace_dispatch, global_cache_skips, global_cache_frame_skips
@@ -21,6 +43,14 @@ elif use_cython is None:
         from _pydevd_bundle.pydevd_cython_wrapper import trace_dispatch as _trace_dispatch, global_cache_skips, global_cache_frame_skips
         def trace_dispatch(py_db, frame, event, arg):
             return _trace_dispatch(py_db, frame, event, arg)
+
+        # This version number is always available
+        from _pydevd_bundle.pydevd_additional_thread_info_regular import version as regular_version
+        # This version number from the already compiled cython extension
+        from _pydevd_bundle.pydevd_cython_wrapper import version as cython_version
+        if cython_version != regular_version:
+            delete_old_compiled_extensions()
+            raise ImportError()
 
     except ImportError:
         from _pydevd_bundle.pydevd_additional_thread_info_regular import PyDBAdditionalThreadInfo  # @UnusedImport
