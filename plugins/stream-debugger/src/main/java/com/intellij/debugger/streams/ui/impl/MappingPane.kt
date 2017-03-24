9496f03f84f700112bfac799ba34f75aa4a65a82
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-03-24 15:48:49 +0300

    Add component which drawing a mapping between before and after values

--- plugins/stream-debugger/src/main/java/com/intellij/debugger/streams/ui/impl/MappingPane.kt
+++ plugins/stream-debugger/src/main/java/com/intellij/debugger/streams/ui/impl/MappingPane.kt
@@ -1,27 +1,39 @@
 package com.intellij.debugger.streams.ui.impl
 
+import com.intellij.debugger.streams.ui.LinkedValuesMapping
 import com.intellij.debugger.streams.ui.ValueWithPosition
 import com.intellij.ui.JBColor
+import com.intellij.ui.components.JBLabel
+import java.awt.BorderLayout
 import java.awt.Graphics
 import javax.swing.JPanel
 
 /**
  * @author Vitaliy.Bibaev
  */
-class MappingPane(private val beforeValues: List<ValueWithPosition>) : JPanel() {
+class MappingPane(private val beforeValues: List<ValueWithPosition>,
+                  private val mapping: LinkedValuesMapping) : JPanel(BorderLayout()) {
+  init {
+    add(JBLabel("map"), BorderLayout.NORTH)
+    add(MyDrawPane(), BorderLayout.CENTER)
+  }
 
-  override fun paintComponent(g: Graphics?) {
-    if (g == null) {
-      return
-    }
+  private inner class MyDrawPane : JPanel() {
+    override fun paintComponent(g: Graphics?) {
+      if (g == null) {
+        return
+      }
 
-    val x1 = x
-    val x2 = x + width
-    for (value in beforeValues) {
-      val position: Int = value.position
-      for (nextValue in value.nextValues.filter { it.isVisible }) {
-        g.color = JBColor.WHITE
-        g.drawLine(x1, position, x2, nextValue.position)
+      val x1 = x
+      val x2 = x + width
+      for (value in beforeValues) {
+        if (!value.isVisible) continue
+        val position: Int = value.position
+        val linkedValues = mapping.getLinkedValues(value) ?: continue
+        for (nextValue in linkedValues.filter { it.isVisible }) {
+          g.color = JBColor.BLACK
+          g.drawLine(x1, position, x2, nextValue.position)
+        }
       }
     }
   }
