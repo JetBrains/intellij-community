d707615cb597b55a9ff0317d383c488c1bf80b12
Alexey Kudravtsev <cdr@intellij.com>
2017-03-27 13:48:54 +0300

    made disposal more explicit to fix EA-94900 - assert: DocumentImpl.doRemoveDocumentListener

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/text/TextEditorComponent.java
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/text/TextEditorComponent.java
@@ -15,6 +15,7 @@
  */
 package com.intellij.openapi.fileEditor.impl.text;
 
+import com.intellij.openapi.Disposable;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.DataProvider;
 import com.intellij.openapi.actionSystem.IdeActions;
@@ -38,6 +39,7 @@ import com.intellij.openapi.fileTypes.FileTypeManager;
 import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Comparing;
+import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileAdapter;
 import com.intellij.openapi.vfs.VirtualFileEvent;
@@ -58,7 +60,7 @@ import java.awt.*;
  * @author Anton Katilin
  * @author Vladimir Kondratyev
  */
-class TextEditorComponent extends JBLoadingPanel implements DataProvider {
+class TextEditorComponent extends JBLoadingPanel implements DataProvider, Disposable {
   private static final Logger LOG = Logger.getInstance("#com.intellij.openapi.fileEditor.impl.text.TextEditorComponent");
 
   private final Project myProject;
@@ -69,8 +71,6 @@ class TextEditorComponent extends JBLoadingPanel implements DataProvider {
    */
   private final Document myDocument;
 
-  private final MyDocumentListener myDocumentListener;
-  private final MyVirtualFileListener myVirtualFileListener;
   @NotNull private final Editor myEditor;
 
   /**
@@ -81,7 +81,6 @@ class TextEditorComponent extends JBLoadingPanel implements DataProvider {
    * Whether the editor is valid or not
    */
   private boolean myValid;
-  private final MessageBusConnection myConnection;
 
   TextEditorComponent(@NotNull final Project project, @NotNull final VirtualFile file, @NotNull final TextEditorImpl textEditor) {
     super(new BorderLayout(), textEditor);
@@ -92,8 +91,7 @@ class TextEditorComponent extends JBLoadingPanel implements DataProvider {
 
     myDocument = FileDocumentManager.getInstance().getDocument(myFile);
     LOG.assertTrue(myDocument!=null);
-    myDocumentListener = new MyDocumentListener();
-    myDocument.addDocumentListener(myDocumentListener);
+    myDocument.addDocumentListener(new MyDocumentListener(), this);
 
     myEditor = createEditor();
     add(myEditor.getComponent(), BorderLayout.CENTER);
@@ -101,9 +99,10 @@ class TextEditorComponent extends JBLoadingPanel implements DataProvider {
     myValid = isEditorValidImpl();
     LOG.assertTrue(myValid);
 
-    myVirtualFileListener = new MyVirtualFileListener();
+    MyVirtualFileListener myVirtualFileListener = new MyVirtualFileListener();
     myFile.getFileSystem().addVirtualFileListener(myVirtualFileListener);
-    myConnection = project.getMessageBus().connect();
+    Disposer.register(this, ()-> myFile.getFileSystem().removeVirtualFileListener(myVirtualFileListener));
+    MessageBusConnection myConnection = project.getMessageBus().connect(this);
     myConnection.subscribe(FileTypeManager.TOPIC, new MyFileTypeListener());
     myConnection.subscribe(DumbService.DUMB_MODE, new DumbService.DumbModeListener() {
       @Override
@@ -124,15 +123,13 @@ class TextEditorComponent extends JBLoadingPanel implements DataProvider {
    * editors, unregisters listeners. The behaviour of the splitter after disposing is
    * unpredictable.
    */
-  void dispose(){
-    myDocument.removeDocumentListener(myDocumentListener);
+  @Override
+  public void dispose(){
     if (!myProject.isDefault()) { // There's no EditorHistoryManager for default project (which is used in diff command-line application)
       EditorHistoryManager.getInstance(myProject).updateHistoryEntry(myFile, false);
     }
     disposeEditor();
-    myConnection.disconnect();
 
-    myFile.getFileSystem().removeVirtualFileListener(myVirtualFileListener);
     myDisposed = true;
     //myFocusWatcher.deinstall(this);
     //removePropertyChangeListener(mySplitterPropertyChangeListener);
