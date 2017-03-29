d865a333dedd4000b2ab884c65106a4e48c46168
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2017-03-29 21:26:38 +0200

    [tests] disables hanging test (IDEA-CR-19850)

--- platform/platform-tests/testSrc/com/intellij/execution/PtyCommandLineTest.java
+++ platform/platform-tests/testSrc/com/intellij/execution/PtyCommandLineTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,11 +17,15 @@ package com.intellij.execution;
 
 import com.intellij.execution.configurations.GeneralCommandLine;
 import com.intellij.execution.configurations.PtyCommandLine;
-import com.intellij.openapi.util.SystemInfo;
 
-import static org.junit.Assume.assumeFalse;
+import static org.junit.Assume.assumeTrue;
 
 public class PtyCommandLineTest extends GeneralCommandLineTest {
+  @Override
+  public void redirectInput() {
+    assumeTrue(false);
+  }
+
   @Override
   protected GeneralCommandLine createCommandLine(String... command) {
     PtyCommandLine cmd = new PtyCommandLine();
