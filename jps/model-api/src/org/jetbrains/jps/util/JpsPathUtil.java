0fab77d57eb9d58435db775ab8afcf6d587bce72
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2017-03-30 13:31:52 +0200

    [jps] adds JRT root filtering to enumerator (IDEA-170307)

--- jps/model-api/src/org/jetbrains/jps/util/JpsPathUtil.java
+++ jps/model-api/src/org/jetbrains/jps/util/JpsPathUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -96,5 +96,9 @@ public class JpsPathUtil {
       return "file://" + path;
     }
     return "jar://" + path + "!/";
+  }
+
+  public static boolean isJrtUrl(@NotNull String url) {
+    return url.startsWith("jrt://");
   }
 }
