ec6b4de6e72f5de5afaff098ae02eca57ec21797
Yaroslav Lepenkin <yaroslav.lepenkin@jetbrains.com>
2017-04-03 11:47:13 +0300

    corrected info message

--- plugins/stats-collector/src/com/intellij/plugin/Startup.kt
+++ plugins/stats-collector/src/com/intellij/plugin/Startup.kt
@@ -12,15 +12,16 @@ class NotificationManager : StartupActivity {
         private val PLUGIN_NAME = "Completion Stats Collector"
         private val MESSAGE_TEXT = 
                 "Data about your code completion usage will be anonymously reported. " +
-                "Note, the plugin doesn’t send any of your personal data or code. " + 
-                "All we send is just numbers calculated on your completion usage patterns"
+                "No personal data or code will be sent."
         
         private val MESSAGE_SHOWN_KEY = "completion.stats.allow.message.shown"
     }
 
     override fun runActivity(project: Project) {
         val isMessageShown = PropertiesComponent.getInstance().getBoolean(MESSAGE_SHOWN_KEY, false)
-        if (!isMessageShown) notify(project)
+        if (!isMessageShown) {
+            notify(project)
+        }
     }
     
     private fun notify(project: Project) {
