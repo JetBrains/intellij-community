4bda5b83c512170e1372cbd070447863a342a8e2
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2017-04-03 10:13:37 +0200

    [updater] prunes empty directories
    Allows moving and renaming optional components. Without the fix, an empty directory tree would be created when an optional component is missing from an installation being patched.

--- updater/src/com/intellij/updater/Utils.java
+++ updater/src/com/intellij/updater/Utils.java
@@ -129,6 +129,14 @@ public class Utils {
     Files.createSymbolicLink(path, Paths.get(target));
   }
 
+  public static boolean isEmptyDirectory(File file) {
+    if (file.isDirectory()) {
+      String[] children = file.list();
+      return children != null && children.length == 0;
+    }
+    return false;
+  }
+
   public static void copy(File from, File to) throws IOException {
     if (!from.exists()) throw new IOException("Source does not exist: " + from);
 
