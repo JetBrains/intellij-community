0086ec4a70558464542154777c477c9f38af511d
Kiryl Chetyrbak <kirylchetyrbak@gmail.com>
2017-04-04 13:10:04 -0400

    Moved extension comparing to beginning

--- python/src/com/jetbrains/python/sdk/PreferredSdkComparator.java
+++ python/src/com/jetbrains/python/sdk/PreferredSdkComparator.java
@@ -32,6 +32,14 @@ public class PreferredSdkComparator implements Comparator<Sdk> {
 
   @Override
   public int compare(Sdk o1, Sdk o2) {
+    for (PySdkWeightProvider provider : Extensions.getExtensions(PySdkWeightProvider.EP_NAME)) {
+      int weight1 = provider.getWeight(o1);
+      int weight2 = provider.getWeight(o2);
+      if(weight1 != weight2) {
+        return weight2 - weight1;
+      }
+    }
+
     final PythonSdkFlavor flavor1 = PythonSdkFlavor.getFlavor(o1);
     final PythonSdkFlavor flavor2 = PythonSdkFlavor.getFlavor(o2);
     int remote1Weight = PySdkUtil.isRemote(o1) ? 0 : 1;
@@ -50,18 +58,11 @@ public class PreferredSdkComparator implements Comparator<Sdk> {
     if (venv1weight != venv2weight) {
       return venv2weight - venv1weight;
     }
+
     int flavor1weight = flavor1 instanceof CPythonSdkFlavor ? 1 : 0;
     int flavor2weight = flavor2 instanceof CPythonSdkFlavor ? 1 : 0;
     if (flavor1weight != flavor2weight) {
       return flavor2weight - flavor1weight;
-    }
-
-    for (PySdkWeightProvider provider : Extensions.getExtensions(PySdkWeightProvider.EP_NAME)) {
-      int weight1 = provider.getWeight(o1);
-      int weight2 = provider.getWeight(o2);
-      if(weight1 != weight2) {
-        return weight2 - weight1;
-      }
     }
 
     return -Comparing.compare(o1.getVersionString(), o2.getVersionString());
