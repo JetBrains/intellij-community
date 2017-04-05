c941b6ab815de5fe4c01bd4418244d799f664b7d
Petrov Alexander <alexander.petrov@stepik.org>
2017-04-05 17:42:54 +0700

    Added StepikRestService

---
+++ python/educational-core/src/com/jetbrains/edu/learning/builtInServer/StepikRestService.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2000-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jetbrains.edu.learning.builtInServer;
+
+import com.intellij.openapi.diagnostic.Logger;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.QueryStringDecoder;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.ide.RestService;
+
+import java.io.IOException;
+
+/**
+ * @author meanmail
+ */
+public class StepikRestService extends RestService {
+  private static final Logger LOG = Logger.getInstance(StepikRestService.class.getName());
+  private static final String SERVICE_NAME = "stepik";
+
+  @NotNull
+  @Override
+  protected String getServiceName() {
+    return SERVICE_NAME;
+  }
+
+  @Override
+  protected boolean isMethodSupported(@NotNull HttpMethod method) {
+    return method.equals(HttpMethod.GET);
+  }
+
+  @Nullable
+  @Override
+  public String execute(@NotNull QueryStringDecoder urlDecoder, @NotNull FullHttpRequest request, @NotNull ChannelHandlerContext context)
+    throws IOException {
+    LOG.info("Request: " + urlDecoder.path());
+    RestService.sendOk(request, context);
+    return null;
+  }
+}
