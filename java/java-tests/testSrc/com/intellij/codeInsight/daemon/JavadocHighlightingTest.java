72a7f60f5868f56abd24d7733a3a0929130261ae
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2017-04-07 18:53:56 +0200

    [java] recognizes @moduleGraph tag

--- java/java-tests/testSrc/com/intellij/codeInsight/daemon/JavadocHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/codeInsight/daemon/JavadocHighlightingTest.java
@@ -107,6 +107,7 @@ public class JavadocHighlightingTest extends LightDaemonAnalyzerTestCase {
   public void testIgnoreDuplicateThrows() { myInspection.setIgnoreDuplicatedThrows(true); doTest(); }
   public void testIgnoreAccessors() { myInspection.setIgnoreSimpleAccessors(true); doTest(); }
   public void testAuthoredMethod() { doTest(); }
+  public void testModuleTags() { setLanguageLevel(LanguageLevel.JDK_1_9); doTest("module-info.java", true, false); }
 
   public void testIssueLinksInJavaDoc() {
     IssueNavigationConfiguration navigationConfiguration = IssueNavigationConfiguration.getInstance(getProject());
