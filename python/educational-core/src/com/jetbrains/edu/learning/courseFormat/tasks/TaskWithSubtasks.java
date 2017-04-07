fd54035b21120454863e2d36b67b949be54057e3
Liana.Bakradze <liana.bakradze@jetbrains.com>
2017-04-07 19:22:42 +0300

    extract TaskWithSubtasksChecker (EDU-882)

--- python/educational-core/src/com/jetbrains/edu/learning/courseFormat/tasks/TaskWithSubtasks.java
+++ python/educational-core/src/com/jetbrains/edu/learning/courseFormat/tasks/TaskWithSubtasks.java
@@ -2,23 +2,13 @@ package com.jetbrains.edu.learning.courseFormat.tasks;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.MessageType;
-import com.intellij.openapi.util.TextRange;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.jetbrains.edu.learning.StudySubtaskUtils;
-import com.jetbrains.edu.learning.checker.StudyCheckUtils;
 import com.jetbrains.edu.learning.checker.StudyTaskChecker;
-import com.jetbrains.edu.learning.core.EduNames;
+import com.jetbrains.edu.learning.checker.TaskWithSubtasksChecker;
 import com.jetbrains.edu.learning.courseFormat.AnswerPlaceholder;
 import com.jetbrains.edu.learning.courseFormat.StudyStatus;
 import com.jetbrains.edu.learning.courseFormat.TaskFile;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Map;
 
 public class TaskWithSubtasks extends Task {
   private int myActiveSubtaskIndex = 0;
@@ -74,52 +64,6 @@ public class TaskWithSubtasks extends Task {
 
   @Override
   public StudyTaskChecker getChecker(@NotNull Project project) {
-    return new StudyTaskChecker<TaskWithSubtasks>(this, project) {
-      @Override
-      public void onTaskSolved(@NotNull String message) {
-        boolean hasMoreSubtasks = myTask.activeSubtaskNotLast();
-        final int activeSubtaskIndex = myTask.getActiveSubtaskIndex();
-        int visibleSubtaskIndex = activeSubtaskIndex + 1;
-        ApplicationManager.getApplication().invokeLater(() -> {
-          int subtaskSize = myTask.getLastSubtaskIndex() + 1;
-          String resultMessage = !hasMoreSubtasks ? message : "Subtask " + visibleSubtaskIndex + "/" + subtaskSize + " solved";
-          StudyCheckUtils.showTestResultPopUp(resultMessage, MessageType.INFO.getPopupBackground(), myProject);
-          if (hasMoreSubtasks) {
-            int nextSubtaskIndex = activeSubtaskIndex + 1;
-            StudySubtaskUtils.switchStep(myProject, myTask, nextSubtaskIndex);
-            rememberAnswers(nextSubtaskIndex, myTask);
-          }
-        });
-      }
-
-      private void rememberAnswers(int nextSubtaskIndex, @NotNull TaskWithSubtasks task) {
-        VirtualFile taskDir = task.getTaskDir(myProject);
-        if (taskDir == null) {
-          return;
-        }
-        VirtualFile srcDir = taskDir.findChild(EduNames.SRC);
-        if (srcDir != null) {
-          taskDir = srcDir;
-        }
-        for (Map.Entry<String, TaskFile> entry : task.getTaskFiles().entrySet()) {
-          TaskFile taskFile = entry.getValue();
-          VirtualFile virtualFile = taskDir.findFileByRelativePath(entry.getKey());
-          if (virtualFile == null) {
-            continue;
-          }
-          Document document = FileDocumentManager.getInstance().getDocument(virtualFile);
-          if (document == null) {
-            continue;
-          }
-          for (AnswerPlaceholder placeholder : taskFile.getActivePlaceholders()) {
-            if (placeholder.getSubtaskInfos().containsKey(nextSubtaskIndex - 1)) {
-              int offset = placeholder.getOffset();
-              String answer = document.getText(TextRange.create(offset, offset + placeholder.getRealLength()));
-              placeholder.getSubtaskInfos().get(nextSubtaskIndex - 1).setAnswer(answer);
-            }
-          }
-        }
-      }
-    };
+    return new TaskWithSubtasksChecker(this, project);
   }
 }
