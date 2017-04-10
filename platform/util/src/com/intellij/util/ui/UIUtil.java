343642204e9f29fd34c59aec8cddc54d1aa9b8eb
Anton Tarasov <anton.tarasov@jetbrains.com>
2017-04-10 14:40:17 +0300

    IDEA-170099 [followup] let Swing text components dynamically scale HTML text on user scale factor change

--- platform/util/src/com/intellij/util/ui/UIUtil.java
+++ platform/util/src/com/intellij/util/ui/UIUtil.java
@@ -2528,7 +2528,7 @@ public class UIUtil {
 
     public JBHtmlEditorKit(boolean noGapsBetweenParagraphs) {
       style.addStyleSheet(isUnderDarcula() ? (StyleSheet)UIManager.getDefaults().get("StyledEditorKit.JBDefaultStyle") : DEFAULT_HTML_KIT_CSS);
-      style.addRule("code { font-size: 90%; }"); // small by Swing's default, make it b/w small and medium
+      style.addRule("code { font-size: 100%; }"); // small by Swing's default
       style.addRule("small { font-size: small; }"); // x-small by Swing's default
       if (noGapsBetweenParagraphs) style.addRule("p { margin-top: 0; }");
     }
