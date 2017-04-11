423fe4e07dee0592e9784c2aed637a5453ce1d0e
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2017-04-11 22:08:39 +0300

    [groovy] deprecate callExpressionTypeCalculator and expressionTypeCalculator APIs

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/typing/MethodCallTypeCalculator.kt
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/typing/MethodCallTypeCalculator.kt
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+@file:Suppress("DEPRECATION")
+
 package org.jetbrains.plugins.groovy.lang.typing
 
 import com.intellij.psi.PsiType
