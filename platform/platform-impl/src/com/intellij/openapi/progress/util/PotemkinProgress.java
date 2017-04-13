1d77df03bc5f1ebf5ff12f04d37da63213895f9f
Alexey Utkin <alexey.utkin@jetbrains.com>
2017-04-13 16:18:38 +0300

    CPP-9359 Freeze while calculating run configuration for Google Test #3 (+rename, +goto, test-macro-in-context fast probe)

--- platform/platform-impl/src/com/intellij/openapi/progress/util/PotemkinProgress.java
+++ platform/platform-impl/src/com/intellij/openapi/progress/util/PotemkinProgress.java
@@ -127,13 +127,17 @@ public class PotemkinProgress extends ProgressWindow {
   }
 
   @Nullable
-  private JRootPane considerShowingDialog(long now) {
-    if (now - myLastUiUpdate > DEFAULT_PROGRESS_DIALOG_POSTPONE_TIME_MILLIS) {
+  protected JRootPane considerShowingDialog(long now) {
+    if (isReadyShowing(now)) {
       getDialog().myRepaintRunnable.run();
       showDialog();
       return getDialog().getPanel().getRootPane();
     }
     return null;
+  }
+
+  protected boolean isReadyShowing(long now) {
+    return now - myLastUiUpdate > myDelayInMillis;
   }
 
   private boolean timeToPaint(long now) {
