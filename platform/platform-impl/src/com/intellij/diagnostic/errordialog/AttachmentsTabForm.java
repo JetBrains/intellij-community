ea3d0a0d0e7089cfcb565c5e378ad5fb03d8534f
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2017-04-14 19:03:00 +0200

    - limit growth of attachment panel due to large attachment - show attachment source as details on right - cosmetics

--- platform/platform-impl/src/com/intellij/diagnostic/errordialog/AttachmentsTabForm.java
+++ platform/platform-impl/src/com/intellij/diagnostic/errordialog/AttachmentsTabForm.java
@@ -14,6 +14,7 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -78,11 +79,11 @@ public class AttachmentsTabForm {
         if (e.getValueIsAdjusting()) {
           return;
         }
-        LabeledTextComponent.setText(myFileTextArea.getTextComponent(), null, true);
+        LabeledTextComponent.setText(myFileTextArea.getTextComponent(), null, false);
         Attachment attachment = myTable.getSelectedObject();
         if (attachment != null) {
           try {
-            LabeledTextComponent.setText(myFileTextArea.getTextComponent(), attachment.getDisplayText(), true);
+            LabeledTextComponent.setText(myFileTextArea.getTextComponent(), attachment.getDisplayText(), false);
           }
           catch (Throwable th) {
             LOG.warn(th);
@@ -107,6 +108,7 @@ public class AttachmentsTabForm {
         myTable.repaint();
       }
     }, KeyStroke.getKeyStroke(KeyEvent.VK_SPACE, 0), JComponent.WHEN_FOCUSED);
+    myContentPane.setPreferredSize(new Dimension(200, 210)); // limit growth due to large attachment
   }
 
   public JComponent getPreferredFocusedComponent() {
