63350850357329f870e2645b19cbaa01012b67f3
Egor.Ushakov <egor.ushakov@jetbrains.com>
2017-04-17 16:29:50 +0300

    cleanup use default methods

--- java/debugger/impl/src/com/intellij/debugger/impl/AlternativeJreClassFinder.java
+++ java/debugger/impl/src/com/intellij/debugger/impl/AlternativeJreClassFinder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,7 +41,7 @@ import java.util.List;
 public class AlternativeJreClassFinder extends NonClasspathClassFinder {
   public AlternativeJreClassFinder(Project project, DebuggerManagerEx manager) {
     super(project);
-    manager.addDebuggerManagerListener(new DebuggerManagerAdapter() {
+    manager.addDebuggerManagerListener(new DebuggerManagerListener() {
       @Override
       public void sessionCreated(DebuggerSession session) {
         clearCache();
