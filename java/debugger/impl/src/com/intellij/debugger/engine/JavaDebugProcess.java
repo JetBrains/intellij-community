8b2fa83ec39baeae4a499fa63ec77d4ca4a1026b
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-04-17 15:31:15 +0300

    Minor: remove unused field in MemoryViewDebugProcessData

--- java/debugger/impl/src/com/intellij/debugger/engine/JavaDebugProcess.java
+++ java/debugger/impl/src/com/intellij/debugger/engine/JavaDebugProcess.java
@@ -372,7 +372,7 @@ public class JavaDebugProcess extends XDebugProcess {
         memoryViewContent.setCloseable(false);
         memoryViewContent.setShouldDisposeContent(true);
 
-        final MemoryViewDebugProcessData data = new MemoryViewDebugProcessData(classesFilteredView);
+        final MemoryViewDebugProcessData data = new MemoryViewDebugProcessData();
         process.putUserData(MemoryViewDebugProcessData.KEY, data);
         session.addSessionListener(new XDebugSessionListener() {
           @Override
