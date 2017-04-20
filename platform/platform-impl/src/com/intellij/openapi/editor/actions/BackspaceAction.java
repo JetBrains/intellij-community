7c3a0031e3bf7d18246480d0d8509a811715c7e0
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2017-04-20 16:05:22 +0300

    IDEA-170997 Multi-caret: A character is deleted despite empty selection

--- platform/platform-impl/src/com/intellij/openapi/editor/actions/BackspaceAction.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/actions/BackspaceAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -58,11 +58,6 @@ public class BackspaceAction extends TextComponentEditorAction {
   }
 
   private static void doBackSpaceAtCaret(@NotNull Editor editor) {
-    if(editor.getSelectionModel().hasSelection()) {
-      EditorModificationUtil.deleteSelectedText(editor);
-      return;
-    }
-
     VisualPosition caretPosition = editor.getCaretModel().getVisualPosition();
     if (caretPosition.column > 0 &&
         editor.getInlayModel().hasInlineElementAt(new VisualPosition(caretPosition.line, caretPosition.column - 1))) {
