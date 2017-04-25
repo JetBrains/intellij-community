3f39c42725fa2834b246c75594b836522e39b796
Liana.Bakradze <liana.bakradze@jetbrains.com>
2017-04-25 17:17:21 +0300

    add location to Advanced Settings (EDU-926)

--- python/educational-core/src/com/jetbrains/edu/learning/newproject/EduStartCourseAction.java
+++ python/educational-core/src/com/jetbrains/edu/learning/newproject/EduStartCourseAction.java
@@ -1,18 +1,14 @@
 package com.jetbrains.edu.learning.newproject;
 
-import com.intellij.ide.impl.ProjectUtil;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.ui.DialogBuilder;
 import com.intellij.openapi.ui.DialogWrapper;
-import com.intellij.openapi.util.io.FileUtil;
 import com.jetbrains.edu.learning.EduPluginConfigurator;
 import com.jetbrains.edu.learning.courseFormat.Course;
 import com.jetbrains.edu.learning.newproject.ui.EduCoursesPanel;
 import com.jetbrains.edu.learning.newproject.ui.EduCreateNewProjectDialog;
 import icons.EducationalCoreIcons;
-
-import java.io.File;
 
 public class EduStartCourseAction extends AnAction {
   public EduStartCourseAction() {
@@ -28,7 +24,7 @@ public class EduStartCourseAction extends AnAction {
     dialogBuilder.setOkOperation(() -> {
       dialogBuilder.getDialogWrapper().close(DialogWrapper.OK_EXIT_CODE);
       Course course = panel.getSelectedCourse();
-      String location = FileUtil.findSequentNonexistentFile(new File(ProjectUtil.getBaseDir()), FileUtil.sanitizeFileName(course.getName()), "").getAbsolutePath();
+      String location = panel.getLocationString();
       EduCreateNewProjectDialog.createProject(EduPluginConfigurator.INSTANCE.forLanguage(course.getLanguageById()).getEduCourseProjectGenerator(), course, location);
     });
     dialogBuilder.show();
--- python/educational-core/src/com/jetbrains/edu/learning/newproject/ui/EduCoursesPanel.java
+++ python/educational-core/src/com/jetbrains/edu/learning/newproject/ui/EduCoursesPanel.java
@@ -1,8 +1,13 @@
 package com.jetbrains.edu.learning.newproject.ui;
 
+import com.intellij.ide.impl.ProjectUtil;
+import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
+import com.intellij.openapi.ui.LabeledComponent;
 import com.intellij.openapi.ui.OnePixelDivider;
+import com.intellij.openapi.ui.TextFieldWithBrowseButton;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.platform.DirectoryProjectGenerator;
 import com.intellij.ui.*;
-import com.intellij.ui.components.JBLabel;
 import com.intellij.ui.components.JBList;
 import com.intellij.ui.components.JBScrollPane;
 import com.intellij.util.ui.JBUI;
@@ -11,12 +16,15 @@ import com.jetbrains.edu.learning.EduPluginConfigurator;
 import com.jetbrains.edu.learning.courseFormat.Course;
 import com.jetbrains.edu.learning.courseGeneration.StudyProjectGenerator;
 import com.jetbrains.edu.learning.newproject.EduCourseProjectGenerator;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import java.awt.*;
+import java.io.File;
 import java.util.List;
 
 public class EduCoursesPanel extends JPanel {
@@ -28,6 +36,7 @@ public class EduCoursesPanel extends JPanel {
   private JPanel myAdvancedSettingsPlaceholder;
   private JPanel myAdvancedSettings;
   private JBList<Course> myCoursesList;
+  private LabeledComponent<TextFieldWithBrowseButton> myLocationField;
 
   public EduCoursesPanel() {
     setLayout(new BorderLayout());
@@ -49,21 +58,21 @@ public class EduCoursesPanel extends JPanel {
       @Override
       public void customize(JList list, Course value, int index, boolean selected, boolean hasFocus) {
         setText(value.getName());
-        EduCourseProjectGenerator projectGenerator =
-          EduPluginConfigurator.INSTANCE.forLanguage(value.getLanguageById()).getEduCourseProjectGenerator();
-        if (projectGenerator != null) {
-          setIcon(projectGenerator.getDirectoryProjectGenerator().getLogo());
+        DirectoryProjectGenerator generator = getGenerator(value);
+        if (generator != null) {
+          setIcon(generator.getLogo());
         }
       }
     });
-    JBLabel label = new JBLabel();
+    myLocationField = createLocationComponent();
+    UIUtil.setBackgroundRecursively(myLocationField, UIUtil.getTextFieldBackground());
     myCoursesList.addListSelectionListener(new ListSelectionListener() {
       @Override
       public void valueChanged(ListSelectionEvent e) {
         Course selectedCourse = myCoursesList.getSelectedValue();
         myDescriptionTextArea.setText(selectedCourse.getDescription());
-        label.setText(selectedCourse.getName());
         myAdvancedSettingsPlaceholder.setVisible(true);
+        myLocationField.getComponent().setText(nameToLocation(selectedCourse.getName()));
       }
     });
     JScrollPane installedScrollPane = ScrollPaneFactory.createScrollPane(myCoursesList, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
@@ -77,11 +86,35 @@ public class EduCoursesPanel extends JPanel {
     myAdvancedSettings.setBorder(IdeBorderFactory.createEmptyBorder(0, IdeBorderFactory.TITLED_BORDER_INDENT, 5, 0));
     UIUtil.setBackgroundRecursively(myAdvancedSettingsPlaceholder, UIUtil.getTextFieldBackground());
     myAdvancedSettings.setLayout(new BorderLayout());
-    myAdvancedSettings.add(label);
+    myAdvancedSettings.add(myLocationField, BorderLayout.CENTER);
     myAdvancedSettingsPlaceholder.setVisible(false);
   }
 
   public Course getSelectedCourse() {
     return myCoursesList.getSelectedValue();
+  }
+
+  @Nullable
+  private static DirectoryProjectGenerator getGenerator(@NotNull Course course) {
+    EduCourseProjectGenerator projectGenerator =
+      EduPluginConfigurator.INSTANCE.forLanguage(course.getLanguageById()).getEduCourseProjectGenerator();
+    return projectGenerator == null ? null : projectGenerator.getDirectoryProjectGenerator();
+  }
+
+  private static LabeledComponent<TextFieldWithBrowseButton> createLocationComponent() {
+    TextFieldWithBrowseButton field = new TextFieldWithBrowseButton();
+    field.addBrowseFolderListener("Select Course Location", "Select course location", null,
+                                  FileChooserDescriptorFactory.createSingleFolderDescriptor());
+    return LabeledComponent.create(field, "Location", BorderLayout.WEST);
+  }
+
+  @NotNull
+  private static String nameToLocation(@NotNull String courseName) {
+    String name = FileUtil.sanitizeFileName(courseName);
+    return FileUtil.findSequentNonexistentFile(new File(ProjectUtil.getBaseDir()), name, "").getAbsolutePath();
+  }
+
+  public String getLocationString() {
+    return myLocationField.getComponent().getText();
   }
 }
