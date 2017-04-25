JetBrains/intellij-community

commit c776e564701f96c0ba126902daca5fdea8a8acc4

made quickfix no longer visible when applied to fix IDEA-170081 Inspection "Collapse 'if' statement" generates wrong code when quickly fixing the code

platform/lang-impl/src/com/intellij/codeInsight/intention/impl/IntentionActionWithTextCaching.java
@@ -18,18 +18,26 @@ package com.intellij.codeInsight.intention.impl;
 
 import com.intellij.codeInsight.daemon.impl.HighlightInfo;
 import com.intellij.codeInsight.intention.IntentionAction;
+import com.intellij.codeInsight.intention.IntentionActionDelegate;
 import com.intellij.openapi.actionSystem.ShortcutProvider;
 import com.intellij.openapi.actionSystem.ShortcutSet;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.PossiblyDumbAware;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Comparing;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.util.IncorrectOperationException;
+import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiConsumer;
 
 /**
 * @author cdr
@@ -45,20 +53,20 @@ public class IntentionActionWithTextCaching implements Comparable<IntentionActio
   private final Icon myIcon;
 
   IntentionActionWithTextCaching(@NotNull IntentionAction action){
-    this(action, action.getText(), null);
+    this(action, action.getText(), null, null);
   }
 
-  IntentionActionWithTextCaching(@NotNull HighlightInfo.IntentionActionDescriptor action){
-    this(action.getAction(), action.getDisplayName(), action.getIcon());
+  IntentionActionWithTextCaching(@NotNull HighlightInfo.IntentionActionDescriptor descriptor, @Nullable BiConsumer<IntentionActionWithTextCaching,IntentionAction> markInvoked){
+    this(descriptor.getAction(), descriptor.getDisplayName(), descriptor.getIcon(), markInvoked);
   }
 
-  private IntentionActionWithTextCaching(@NotNull IntentionAction action, String displayName, @Nullable Icon icon) {
+  private IntentionActionWithTextCaching(@NotNull IntentionAction action, String displayName, @Nullable Icon icon, @Nullable BiConsumer<IntentionActionWithTextCaching, IntentionAction> markInvoked) {
     myIcon = icon;
     myText = action.getText();
     // needed for checking errors in user written actions
     //noinspection ConstantConditions
     LOG.assertTrue(myText != null, "action "+action.getClass()+" text returned null");
-    myAction = action;
+    myAction = new MyIntentionAction(action, markInvoked);
     myDisplayName = displayName;
   }
 
@@ -132,5 +140,69 @@ public class IntentionActionWithTextCaching implements Comparable<IntentionActio
   @Override
   public ShortcutSet getShortcut() {
     return myAction instanceof ShortcutProvider ? ((ShortcutProvider)myAction).getShortcut() : null;
+  }
+
+  // IntentionAction which wraps the original action and then marks it as executed to hide it from the popup to avoid invoking it twice accidentally
+  private class MyIntentionAction implements IntentionAction,IntentionActionDelegate {
+    private final IntentionAction myAction;
+    private final BiConsumer<IntentionActionWithTextCaching, IntentionAction> myMarkInvoked;
+
+    MyIntentionAction(IntentionAction action, BiConsumer<IntentionActionWithTextCaching, IntentionAction> markInvoked) {
+      myAction = action;
+      myMarkInvoked = markInvoked;
+    }
+
+    @Nls
+    @NotNull
+    @Override
+    public String getText() {
+      return myAction.getText();
+    }
+
+    @Override
+    public String toString() {
+      return getDelegate().getClass()+": "+getDelegate();
+    }
+
+    @Nls
+    @NotNull
+    @Override
+    public String getFamilyName() {
+      return myAction.getFamilyName();
+    }
+
+    @Override
+    public boolean isAvailable(@NotNull Project project, Editor editor, PsiFile file) {
+      return myAction.isAvailable(project, editor, file);
+    }
+
+    @Override
+    public void invoke(@NotNull Project project, Editor editor, PsiFile file) throws IncorrectOperationException {
+      try {
+        myAction.invoke(project, editor, file);
+      }
+      finally {
+        if (myMarkInvoked != null) {
+          myMarkInvoked.accept(IntentionActionWithTextCaching.this, myAction);
+        }
+      }
+    }
+
+    @Override
+    public boolean startInWriteAction() {
+      return myAction.startInWriteAction();
+    }
+
+    @NotNull
+    @Override
+    public IntentionAction getDelegate() {
+      return myAction;
+    }
+
+    @Nullable
+    @Override
+    public PsiElement getElementToMakeWritable(@NotNull PsiFile currentFile) {
+      return myAction.getElementToMakeWritable(currentFile);
+    }
   }
 }

java/java-impl/src/com/intellij/codeInsight/ExternalAnnotationsLineMarkerProvider.java
@@ -20,10 +20,10 @@ import com.intellij.codeInsight.daemon.GutterIconNavigationHandler;
 import com.intellij.codeInsight.daemon.LineMarkerInfo;
 import com.intellij.codeInsight.daemon.LineMarkerProviderDescriptor;
 import com.intellij.codeInsight.intention.IntentionAction;
+import com.intellij.codeInsight.intention.IntentionActionDelegate;
 import com.intellij.codeInsight.intention.IntentionManager;
 import com.intellij.codeInsight.intention.impl.AddAnnotationIntention;
 import com.intellij.codeInsight.intention.impl.DeannotateIntentionAction;
-import com.intellij.codeInsight.intention.impl.config.IntentionActionWrapper;
 import com.intellij.codeInsight.javadoc.JavaDocInfoGenerator;
 import com.intellij.codeInspection.dataFlow.EditContractIntention;
 import com.intellij.icons.AllIcons;
@@ -212,7 +212,7 @@ public class ExternalAnnotationsLineMarkerProvider extends LineMarkerProviderDes
              action instanceof EditContractIntention ||
              action instanceof ToggleSourceInferredAnnotations ||
              action instanceof MakeInferredAnnotationExplicit ||
-             action instanceof IntentionActionWrapper && shouldShowInGutterPopup(((IntentionActionWrapper)action).getDelegate());
+             action instanceof IntentionActionDelegate && shouldShowInGutterPopup(((IntentionActionDelegate)action).getDelegate());
     }
   }
 }

java/java-tests/testSrc/com/intellij/codeInsight/daemon/impl/DaemonRespondToChangesTest.java
@@ -19,7 +19,6 @@ import com.intellij.codeHighlighting.*;
 import com.intellij.codeInsight.EditorInfo;
 import com.intellij.codeInsight.completion.CompletionContributor;
 import com.intellij.codeInsight.daemon.*;
-import com.intellij.codeInsight.daemon.impl.quickfix.DeleteCatchFix;
 import com.intellij.codeInsight.daemon.quickFix.LightQuickFixTestCase;
 import com.intellij.codeInsight.folding.CodeFoldingManager;
 import com.intellij.codeInsight.hint.EditorHintListener;
@@ -27,9 +26,7 @@ import com.intellij.codeInsight.intention.AbstractIntentionAction;
 import com.intellij.codeInsight.intention.IntentionAction;
 import com.intellij.codeInsight.intention.IntentionManager;
 import com.intellij.codeInsight.intention.impl.IntentionHintComponent;
-import com.intellij.codeInspection.InspectionProfile;
-import com.intellij.codeInspection.LocalInspectionTool;
-import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.codeInspection.*;
 import com.intellij.codeInspection.accessStaticViaInstance.AccessStaticViaInstance;
 import com.intellij.codeInspection.deadCode.UnusedDeclarationInspection;
 import com.intellij.codeInspection.deadCode.UnusedDeclarationInspectionBase;
@@ -977,8 +974,7 @@ public class DaemonRespondToChangesTest extends DaemonAnalyzerTestCase {
       new GotoNextErrorHandler(true).invoke(getProject(), getEditor(), getFile());
 
       List<IntentionAction> fixes = LightQuickFixTestCase.getAvailableActions(getEditor(), getFile());
-      IntentionAction fix = assertContainsOneOf(fixes, DeleteCatchFix.class);
-      assertEquals("Delete catch for 'java.io.IOException'", fix.getText());
+      IntentionAction fix = assertContainsOneOf(fixes, "Delete catch for 'java.io.IOException'");
 
       final IntentionAction finalFix = fix;
       WriteCommandAction.runWriteCommandAction(getProject(), () -> finalFix.invoke(getProject(), getEditor(), getFile()));
@@ -988,8 +984,7 @@ public class DaemonRespondToChangesTest extends DaemonAnalyzerTestCase {
 
       new GotoNextErrorHandler(true).invoke(getProject(), getEditor(), getFile());
       fixes = LightQuickFixTestCase.getAvailableActions(getEditor(), getFile());
-      fix = assertContainsOneOf(fixes, DeleteCatchFix.class);
-      assertEquals("Delete catch for 'java.io.IOException'", fix.getText());
+      fix = assertContainsOneOf(fixes, "Delete catch for 'java.io.IOException'");
 
       final IntentionAction finalFix1 = fix;
       WriteCommandAction.runWriteCommandAction(getProject(), () -> finalFix1.invoke(getProject(), getEditor(), getFile()));
@@ -999,8 +994,7 @@ public class DaemonRespondToChangesTest extends DaemonAnalyzerTestCase {
 
       new GotoNextErrorHandler(true).invoke(getProject(), getEditor(), getFile());
       fixes = LightQuickFixTestCase.getAvailableActions(getEditor(), getFile());
-      fix = assertContainsOneOf(fixes, DeleteCatchFix.class);
-      assertEquals("Delete catch for 'java.io.IOException'", fix.getText());
+      fix = assertContainsOneOf(fixes, "Delete catch for 'java.io.IOException'");
 
       final IntentionAction finalFix2 = fix;
       WriteCommandAction.runWriteCommandAction(getProject(), () -> finalFix2.invoke(getProject(), getEditor(), getFile()));
@@ -1013,19 +1007,19 @@ public class DaemonRespondToChangesTest extends DaemonAnalyzerTestCase {
     }
   }
 
-  private static <T> T assertContainsOneOf(@NotNull Collection<T> collection, @NotNull Class<?> aClass) {
-    T result = null;
-    for (T t : collection) {
-      if (aClass.isInstance(t)) {
+  private static IntentionAction assertContainsOneOf(@NotNull Collection<IntentionAction> collection, @NotNull String text) {
+    IntentionAction result = null;
+    for (IntentionAction action : collection) {
+      if (text.equals(action.getText())) {
         if (result != null) {
-          fail("multiple " + aClass.getName() + " objects present in collection " + collection);
+          fail("multiple " + " objects present in collection " + collection);
         }
         else {
-          result = t;
+          result = action;
         }
       }
     }
-    assertNotNull(aClass.getName() + " object not found in collection " + collection, result);
+    assertNotNull(" object not found in collection " + collection, result);
     return result;
   }
 
@@ -1330,8 +1324,7 @@ public class DaemonRespondToChangesTest extends DaemonAnalyzerTestCase {
     CodeInsightTestFixtureImpl.invokeIntention(descriptor.getAction(), getFile(), getEditor(), "");
 
     highlightErrors();
-    actions = ShowIntentionsPass.getAvailableFixes(getEditor(), getFile(), -1);
-    assertEmpty(actions);
+    assertEmpty(ShowIntentionsPass.getAvailableFixes(getEditor(), getFile(), -1));
   }
 
 
@@ -2605,6 +2598,91 @@ public class DaemonRespondToChangesTest extends DaemonAnalyzerTestCase {
             }
           }
         });
+      }
+    }
+  }
+
+  public void testDumbQuickFixIsNoLongerVisibleAfterApplied() {
+    MyInspection tool = new MyInspection();
+    enableInspectionTool(tool);
+    disposeOnTearDown(() -> disableInspectionTool(tool.getShortName()));
+
+    @Language("JAVA")
+    String text = "class X { void f() { if (this == null) {} else return; } }";
+    configureByText(JavaFileType.INSTANCE, text);
+    WriteCommandAction.runWriteCommandAction(getProject(), () -> myEditor.getDocument().setText(text));
+    getEditor().getCaretModel().moveToOffset(getFile().getText().indexOf("if (") + 1);
+    assertEmpty(doHighlighting(HighlightSeverity.ERROR));
+    List<IntentionAction> fixes = findStupidFixes();
+    IntentionAction fix = assertOneElement(fixes);
+    fix.invoke(getProject(), getEditor(), getFile());
+
+    fixes = findStupidFixes();
+    assertEmpty(fixes);
+
+    assertEmpty(doHighlighting(HighlightSeverity.ERROR));
+    fixes = findStupidFixes();
+    assertEmpty(fixes);
+  }
+
+  private List<IntentionAction> findStupidFixes() {
+    return CodeInsightTestFixtureImpl.getAvailableIntentions(getEditor(), getFile())
+      .stream().filter(f->f.getFamilyName().equals(new MyInspection.StupidQuickFixWhichDoesntCheckItsOwnApplicability().getFamilyName()))
+      .collect(Collectors.toList());
+  }
+
+  private static class MyInspection extends LocalInspectionTool {
+    @Nls
+    @NotNull
+    @Override
+    public String getGroupDisplayName() {
+      return "danuna";
+    }
+
+    @Nls
+    @NotNull
+    @Override
+    public String getDisplayName() {
+      return getGroupDisplayName();
+    }
+
+    @NotNull
+    @Override
+    public String getShortName() {
+      return getGroupDisplayName();
+    }
+
+    @NotNull
+    @Override
+    public PsiElementVisitor buildVisitor(@NotNull ProblemsHolder holder, boolean isOnTheFly) {
+      return new JavaElementVisitor() {
+        @Override
+        public void visitIfStatement(PsiIfStatement statement) {
+          if (statement.getElseBranch() != null) {
+            PsiKeyword keyw = (PsiKeyword)statement.getChildren()[0];
+            holder.registerProblem(keyw, "dododo", new StupidQuickFixWhichDoesntCheckItsOwnApplicability());
+          }
+        }
+      };
+    }
+    private static class StupidQuickFixWhichDoesntCheckItsOwnApplicability implements LocalQuickFix {
+      @Nls
+      @NotNull
+      @Override
+      public String getName() {
+        return "danu";
+      }
+
+      @Nls
+      @NotNull
+      @Override
+      public String getFamilyName() {
+        return getName();
+      }
+
+      @Override
+      public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
+        WriteCommandAction.runWriteCommandAction(project, () -> ((PsiIfStatement)descriptor.getPsiElement().getParent()).getElseBranch().delete());
       }
     }
   }

java/java-tests/testSrc/com/intellij/codeInsight/daemon/quickFix/AddMethodQualifierTest.java
@@ -18,13 +18,16 @@ package com.intellij.codeInsight.daemon.quickFix;
 import com.intellij.JavaTestUtil;
 import com.intellij.codeInsight.daemon.impl.quickfix.AddMethodQualifierFix;
 import com.intellij.codeInsight.intention.IntentionAction;
+import com.intellij.codeInsight.intention.IntentionActionDelegate;
 import com.intellij.psi.PsiNamedElement;
 import com.intellij.testFramework.fixtures.JavaCodeInsightFixtureTestCase;
 import com.intellij.util.Function;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.*;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * @author Dmitry Batkovich
@@ -93,9 +96,10 @@ public class AddMethodQualifierTest extends JavaCodeInsightFixtureTestCase {
   private AddMethodQualifierFix getQuickFix() {
     final List<IntentionAction> availableIntentions = myFixture.getAvailableIntentions();
     AddMethodQualifierFix addMethodQualifierFix = null;
-    for (final IntentionAction availableIntention : availableIntentions) {
-      if (availableIntention instanceof AddMethodQualifierFix) {
-        addMethodQualifierFix = (AddMethodQualifierFix)availableIntention;
+    for (IntentionAction action : availableIntentions) {
+      if (action instanceof IntentionActionDelegate) action = ((IntentionActionDelegate)action).getDelegate();
+      if (action instanceof AddMethodQualifierFix) {
+        addMethodQualifierFix = (AddMethodQualifierFix)action;
         break;
       }
     }

java/java-tests/testSrc/com/intellij/codeInsight/daemon/quickFix/EmptyIntentionInspectionQuickFixTest.java
@@ -2,6 +2,7 @@ package com.intellij.codeInsight.daemon.quickFix;
 
 import com.intellij.codeInsight.intention.EmptyIntentionAction;
 import com.intellij.codeInsight.intention.IntentionAction;
+import com.intellij.codeInsight.intention.IntentionActionDelegate;
 import com.intellij.codeInspection.LocalInspectionTool;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.codeInspection.defUse.DefUseInspection;
@@ -69,6 +70,7 @@ public class EmptyIntentionInspectionQuickFixTest extends LightQuickFixTestCase 
     List<IntentionAction> emptyActions = getAvailableActions();
     for (int i = emptyActions.size()-1; i>=0; i--) {
       IntentionAction action = emptyActions.get(i);
+      if (action instanceof IntentionActionDelegate) action = ((IntentionActionDelegate)action).getDelegate();
       if (!(action instanceof EmptyIntentionAction)) emptyActions.remove(i);
     }
     assertEquals(emptyActions.toString(), 1, emptyActions.size());
@@ -79,17 +81,20 @@ public class EmptyIntentionInspectionQuickFixTest extends LightQuickFixTestCase 
     List<IntentionAction> emptyActions = getAvailableActions();
     int i = 0;
     for(;i < emptyActions.size(); i++) {
-      final IntentionAction intentionAction = emptyActions.get(i);
-      if ("Make 'i' not final".equals(intentionAction.getText())) {
+      IntentionAction action = emptyActions.get(i);
+      if (action instanceof IntentionActionDelegate) action = ((IntentionActionDelegate)action).getDelegate();
+      if ("Make 'i' not final".equals(action.getText())) {
         break;
       }
-      if (intentionAction instanceof EmptyIntentionAction) {
+      if (action instanceof EmptyIntentionAction) {
         fail("Low priority action prior to quick fix");
       }
     }
     assertTrue(i < emptyActions.size());
     for (; i < emptyActions.size(); i++) {
-      if (emptyActions.get(i) instanceof EditInspectionToolsSettingsAction) {
+      IntentionAction action = emptyActions.get(i);
+      if (action instanceof IntentionActionDelegate) action = ((IntentionActionDelegate)action).getDelegate();
+      if (action instanceof EditInspectionToolsSettingsAction) {
         return;
       }
     }

java/java-tests/testSrc/com/intellij/codeInsight/intention/AddImportActionTest.groovy
@@ -152,7 +152,7 @@ public class Foo {
     Ma<caret>p l = new HashMap<>();
 }
 '''
-    ImportClassFix intention = myFixture.findSingleIntention("Import class") as ImportClassFix
+    ImportClassFix intention = myFixture.findSingleIntention("Import class").delegate as ImportClassFix
     assert intention.classesToImport.collect { it.qualifiedName } == ['java.util.Map']
   }
 

java/testFramework/src/com/intellij/codeInsight/daemon/quickFix/ActionHint.java
@@ -16,6 +16,7 @@
 package com.intellij.codeInsight.daemon.quickFix;
 
 import com.intellij.codeInsight.intention.IntentionAction;
+import com.intellij.codeInsight.intention.IntentionActionDelegate;
 import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ex.QuickFixWrapper;
 import com.intellij.lang.Commenter;
@@ -88,7 +89,9 @@ public class ActionHint {
         fail("Action with text '" + myExpectedText + "' not found\nAvailable actions: " +
              actions.stream().map(IntentionAction::getText).collect(Collectors.joining(", ", "[", "]\n")) +
              infoSupplier.get());
-      } else if(myHighlightType != null) {
+      }
+      else if(myHighlightType != null) {
+        if (result instanceof IntentionActionDelegate) result = ((IntentionActionDelegate)result).getDelegate();
         if(!(result instanceof QuickFixWrapper)) {
           fail("Action with text '" + myExpectedText + "' is not a LocalQuickFix, but " + result.getClass().getName() +
                "\nExpected LocalQuickFix with ProblemHighlightType=" + myHighlightType + "\n" +
@@ -144,8 +147,7 @@ public class ActionHint {
     String state = matcher.group(2);
     if(state.equals("true") || state.equals("false")) {
       return new ActionHint(text, Boolean.parseBoolean(state), null);
-    } else {
-      return new ActionHint(text, true, ProblemHighlightType.valueOf(state));
     }
+    return new ActionHint(text, true, ProblemHighlightType.valueOf(state));
   }
 }

platform/analysis-api/src/com/intellij/codeInsight/intention/IntentionActionDelegate.java
@@ -1,0 +1,23 @@
+/*
+ * Copyright 2000-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.codeInsight.intention;
+
+import org.jetbrains.annotations.NotNull;
+
+public interface IntentionActionDelegate {
+  @NotNull
+  IntentionAction getDelegate();
+}

commit da17449807f14f8d7aaaf29d75c467b03ae1b342

Fix displaying shortcuts for intention actions
Was broken in c776e564701f96c0ba126902daca5fdea8a8acc4

platform/lang-impl/src/com/intellij/codeInsight/intention/impl/IntentionActionWithTextCaching.java
@@ -143,7 +143,7 @@ public class IntentionActionWithTextCaching implements Comparable<IntentionActio
   }
 
   // IntentionAction which wraps the original action and then marks it as executed to hide it from the popup to avoid invoking it twice accidentally
-  private class MyIntentionAction implements IntentionAction, IntentionActionDelegate, Comparable<MyIntentionAction> {
+  private class MyIntentionAction implements IntentionAction, IntentionActionDelegate, Comparable<MyIntentionAction>, ShortcutProvider {
     private final IntentionAction myAction;
     private final BiConsumer<IntentionActionWithTextCaching, IntentionAction> myMarkInvoked;
 
@@ -203,6 +203,12 @@ public class IntentionActionWithTextCaching implements Comparable<IntentionActio
     @Override
     public PsiElement getElementToMakeWritable(@NotNull PsiFile currentFile) {
       return myAction.getElementToMakeWritable(currentFile);
+    }
+
+    @Nullable
+    @Override
+    public ShortcutSet getShortcut() {
+      return myAction instanceof ShortcutProvider ? ((ShortcutProvider)myAction).getShortcut() : null;
     }
 
     @Override