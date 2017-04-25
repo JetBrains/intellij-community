8ec2e44101eea22df7d8fea2aa9755c26b0f0f4a
Anna.Kozlova <anna.kozlova@jetbrains.com>
2017-04-25 14:19:05 +0200

    SOE

--- java/java-psi-impl/src/com/intellij/psi/impl/source/resolve/graphInference/InferenceVariable.java
+++ java/java-psi-impl/src/com/intellij/psi/impl/source/resolve/graphInference/InferenceVariable.java
@@ -90,8 +90,7 @@ public class InferenceVariable extends LightTypeParameter {
   }
 
   public boolean addBound(PsiType classType, InferenceBound inferenceBound, @Nullable InferenceIncorporationPhase incorporationPhase) {
-    if (inferenceBound == InferenceBound.EQ &&
-        PsiUtil.resolveClassInClassTypeOnly(classType) == this) {
+    if (PsiUtil.resolveClassInClassTypeOnly(classType) == this) {
       return false;
     }
     List<PsiType> bounds = myBounds.get(inferenceBound);
