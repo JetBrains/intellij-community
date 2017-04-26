82521d0d469bd41ee8b9d1a58e8dc2bfe81b7155
Denis Fokin <Denis.Fokin@jetbrains.com>
2017-04-26 19:24:21 +0300

    Revert: IDEA-164154 Mac OS: Alt+I mnemonic in Commit dialog triggers the action, but also inserts ˆ to commit message

--- platform/platform-api/src/com/intellij/openapi/ui/DialogWrapper.java
+++ platform/platform-api/src/com/intellij/openapi/ui/DialogWrapper.java
@@ -759,7 +759,7 @@ public abstract class DialogWrapper {
   @NotNull
   private JButton createJOptionsButton(@NotNull OptionAction action) {
     JBOptionButton optionButton = new JBOptionButton(action, action.getOptions());
-    optionButton.setOkToProcessDefaultMnemonics(true);
+    optionButton.setOkToProcessDefaultMnemonics(false);
     optionButton.setOptionTooltipText(
       "Press " + KeymapUtil.getKeystrokeText(SHOW_OPTION_KEYSTROKE) + " to expand or use a mnemonic of a contained action");
 
@@ -775,7 +775,7 @@ public abstract class DialogWrapper {
               final JBOptionButton buttonToActivate = eachInfo.getButton();
               buttonToActivate.showPopup(eachInfo.getAction(), true);
             }
-          }.registerCustomShortcutSet(MnemonicHelper.createShortcut(mnemonic), optionButton, myDisposable);
+          }.registerCustomShortcutSet(MnemonicHelper.createShortcut(mnemonic), rootPane, myDisposable);
         }
       }
     }
