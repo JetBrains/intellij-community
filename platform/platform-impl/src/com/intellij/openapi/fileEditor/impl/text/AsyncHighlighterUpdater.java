b8cc02e3c2cbfa5e895707063a3c382f2cd17a6d
peter <peter@jetbrains.com>
2017-04-26 14:59:10 +0200

    [^batrmi] cancel previous async editor highlighter update when new one is scheduled
    plus don't pile up too many Swing events at once when rescheduling (IDEA-171496)

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/text/AsyncHighlighterUpdater.java
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/text/AsyncHighlighterUpdater.java
@@ -32,7 +32,7 @@ import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.TestOnly;
 
-import java.util.Set;
+import java.util.Map;
 import java.util.concurrent.*;
 
 /**
@@ -40,7 +40,7 @@ import java.util.concurrent.*;
  */
 public class AsyncHighlighterUpdater extends ReadTask {
   private static final ExecutorService ourExecutor = AppExecutorUtil.createBoundedApplicationPoolExecutor("AsyncEditorLoader pool", 2);
-  private static final Set<Future<?>> ourHighlighterFutures = ContainerUtil.newConcurrentSet();
+  private static final Map<Editor, Future<?>> ourHighlighterFutures = ContainerUtil.newConcurrentMap();
   private final Project myProject;
   private final Editor myEditor;
   private final VirtualFile myFile;
@@ -72,15 +72,18 @@ public class AsyncHighlighterUpdater extends ReadTask {
     AsyncHighlighterUpdater task = new AsyncHighlighterUpdater(project, editor, file);
     if (task.isEverythingValid()) {
       CompletableFuture<?> future = ProgressIndicatorUtils.scheduleWithWriteActionPriority(ourExecutor, task);
-      ourHighlighterFutures.add(future);
-      future.whenComplete((a, b) -> ourHighlighterFutures.remove(future));
+      Future<?> prev = ourHighlighterFutures.put(editor, future);
+      if (prev != null) {
+        prev.cancel(false);
+      }
+      future.whenComplete((a, b) -> ourHighlighterFutures.remove(editor, future));
     }
   }
 
   @TestOnly
   public static void completeAsyncTasks() {
     assert !ApplicationManager.getApplication().isWriteAccessAllowed();
-    ApplicationManager.getApplication().invokeAndWait(() -> ourHighlighterFutures.forEach(AsyncHighlighterUpdater::waitForFuture));
+    ApplicationManager.getApplication().invokeAndWait(() -> ourHighlighterFutures.values().forEach(AsyncHighlighterUpdater::waitForFuture));
     UIUtil.dispatchAllInvocationEvents();
   }
 
