77a5d567be639293325b454166945b088c40951f
Nicolay Mitropolsky <nicolay.mitropolsky@jetbrains.com>
2017-04-26 13:02:26 +0300

    `JvmCommonCodeModifications` EP as registry for language-abstracted modifications for creating language-independent quickfixes + `ImplicitSubclassInspection` uses it

---
+++ java/java-impl/src/com/intellij/codeInsight/intention/impl/JavaCommonCodeModifications.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2000-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.codeInsight.intention.impl
+
+import com.intellij.codeInsight.daemon.impl.quickfix.ModifierFix
+import com.intellij.codeInsight.intention.IntentionAction
+import org.jetbrains.uast.UDeclaration
+import com.intellij.codeInsight.intention.JvmCommonCodeModifications
+
+
+class JavaCommonCodeModifications : JvmCommonCodeModifications() {
+
+  override fun createChangeModifierAction(declaration: UDeclaration, modifier: String, shouldPresent: Boolean): IntentionAction {
+    return ModifierFix(declaration.modifierList, modifier, shouldPresent, false)
+  }
+
+}
