acdad510c8755082df37239579d05eec20519b21
nik <Nikolay.Chashnikov@jetbrains.com>
2017-04-27 15:36:15 +0300

    project structure dialog: show warning if user tries to manually edit configuration of a library imported from an external model (IDEA-171948)

--- java/idea-ui/src/com/intellij/openapi/roots/ui/configuration/ModuleEditor.java
+++ java/idea-ui/src/com/intellij/openapi/roots/ui/configuration/ModuleEditor.java
@@ -16,7 +16,6 @@
 package com.intellij.openapi.roots.ui.configuration;
 
 import com.intellij.facet.impl.ProjectFacetsConfigurator;
-import com.intellij.icons.AllIcons;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.actionSystem.DataProvider;
 import com.intellij.openapi.actionSystem.LangDataKeys;
@@ -41,8 +40,6 @@ import com.intellij.ui.navigation.History;
 import com.intellij.ui.navigation.Place;
 import com.intellij.util.EventDispatcher;
 import com.intellij.util.containers.ContainerUtil;
-import com.intellij.util.ui.JBUI;
-import com.intellij.util.ui.UIUtil;
 import gnu.trove.THashSet;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
@@ -70,7 +67,7 @@ public abstract class ModuleEditor implements Place.Navigator, Disposable {
 
   private final Project myProject;
   private JPanel myGenericSettingsPanel;
-  private JLabel myModificationOfImportedModelWarningLabel;
+  private ModificationOfImportedModelWarningComponent myModificationOfImportedModelWarningComponent;
   private ModifiableRootModel myModifiableRootModel; // important: in order to correctly update OrderEntries UI use corresponding proxy for the model
 
   private final ModulesProvider myModulesProvider;
@@ -269,8 +266,8 @@ public abstract class ModuleEditor implements Place.Navigator, Disposable {
 
     final JComponent component = createCenterPanel();
     myGenericSettingsPanel.add(component, BorderLayout.CENTER);
-    myModificationOfImportedModelWarningLabel = new JLabel();
-    myGenericSettingsPanel.add(myModificationOfImportedModelWarningLabel, BorderLayout.SOUTH);
+    myModificationOfImportedModelWarningComponent = new ModificationOfImportedModelWarningComponent();
+    myGenericSettingsPanel.add(myModificationOfImportedModelWarningComponent.getLabel(), BorderLayout.SOUTH);
     updateImportedModelWarning();
     myEditorsInitialized = true;
     return myGenericSettingsPanel;
@@ -306,13 +303,10 @@ public abstract class ModuleEditor implements Place.Navigator, Disposable {
 
     ProjectModelExternalSource externalSource = ModuleRootManager.getInstance(myModule).getExternalSource();
     if (externalSource != null && isModified()) {
-      myModificationOfImportedModelWarningLabel.setVisible(true);
-      myModificationOfImportedModelWarningLabel.setBorder(JBUI.Borders.empty(5, 5));
-      myModificationOfImportedModelWarningLabel.setIcon(AllIcons.General.Warning);
-      myModificationOfImportedModelWarningLabel.setText(UIUtil.toHtml("Module '" + myModule.getName() + "' is imported from " + externalSource.getDisplayName() + ". Any changes made in its configuration may be lost after reimporting."));
+      myModificationOfImportedModelWarningComponent.showWarning("Module '" + myModule.getName() + "'", externalSource);
     }
     else {
-      myModificationOfImportedModelWarningLabel.setVisible(false);
+      myModificationOfImportedModelWarningComponent.hideWarning();
     }
   }
 
