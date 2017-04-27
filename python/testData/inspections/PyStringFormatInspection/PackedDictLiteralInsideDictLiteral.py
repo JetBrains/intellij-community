ccb8ee5e8d0928248721fe3504e83513c7cf1a2a
Valentina Kiryushkina <valentina.kiryushkina@jetbrains.com>
2017-04-27 14:40:47 +0300

    Fix test warning message

--- python/testData/inspections/PyStringFormatInspection/PackedDictLiteralInsideDictLiteral.py
+++ python/testData/inspections/PyStringFormatInspection/PackedDictLiteralInsideDictLiteral.py
@@ -1,2 +1,2 @@
 "{b}".format(**{"a": 1, **{"b":2}})
-<warning descr="Key 'c' has no following argument">"{c}"</warning>.format(**{"a": 1, **{"b":2}})
+<warning descr="Key 'c' has no corresponding argument">"{c}"</warning>.format(**{"a": 1, **{"b":2}})
