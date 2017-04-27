f79e804cb098beddae787d18aa12a43ff4e5336e
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-04-27 17:14:09 +0300

    Show popup at the end of the chain

--- plugins/stream-debugger/src/main/java/com/intellij/debugger/streams/ui/impl/ElementChooserImpl.java
+++ plugins/stream-debugger/src/main/java/com/intellij/debugger/streams/ui/impl/ElementChooserImpl.java
@@ -15,16 +15,21 @@
  */
 package com.intellij.debugger.streams.ui.impl;
 
-import com.intellij.debugger.streams.ui.ElementChooser;
 import com.intellij.debugger.streams.ui.ChooserOption;
+import com.intellij.debugger.streams.ui.ElementChooser;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.RangeMarker;
 import com.intellij.openapi.editor.colors.EditorColors;
 import com.intellij.openapi.editor.colors.EditorColorsManager;
-import com.intellij.openapi.editor.markup.*;
+import com.intellij.openapi.editor.markup.HighlighterLayer;
+import com.intellij.openapi.editor.markup.HighlighterTargetArea;
+import com.intellij.openapi.editor.markup.RangeHighlighter;
+import com.intellij.openapi.editor.markup.TextAttributes;
+import com.intellij.openapi.ui.popup.JBPopup;
 import com.intellij.openapi.ui.popup.JBPopupAdapter;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.openapi.ui.popup.LightweightWindowEvent;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.ui.components.JBList;
 import org.jetbrains.annotations.NotNull;
 
@@ -55,8 +60,10 @@ public class ElementChooserImpl<T extends ChooserOption> implements ElementChoos
   @Override
   public void show(@NotNull List<T> options, @NotNull CallBack<T> callBack) {
     final DefaultListModel<T> model = new DefaultListModel<>();
+    int maxOffset = -1;
     for (final T option : options) {
       model.addElement(option);
+      maxOffset = Math.max(maxOffset, option.rangeStream().mapToInt(TextRange::getEndOffset).max().orElse(-1));
     }
 
     final JBList<T> list = new JBList<>(model);
@@ -81,7 +88,7 @@ public class ElementChooserImpl<T extends ChooserOption> implements ElementChoos
       });
     });
 
-    JBPopupFactory.getInstance().createListPopupBuilder(list)
+    final JBPopup popup = JBPopupFactory.getInstance().createListPopupBuilder(list)
       .setTitle("Multiple chains found")
       .setMovable(true)
       .setResizable(false)
@@ -93,7 +100,13 @@ public class ElementChooserImpl<T extends ChooserOption> implements ElementChoos
           dropHighlighters();
         }
       })
-      .createPopup().showInBestPositionFor(myEditor);
+      .createPopup();
+
+    if (maxOffset != -1) {
+      myEditor.getCaretModel().moveToOffset(maxOffset);
+    }
+
+    popup.showInBestPositionFor(myEditor);
   }
 
   private void dropHighlighters() {
