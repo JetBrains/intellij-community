b35ac83f23c7a46ca7838cae1f9c4d15530d5984
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2017-04-27 13:11:56 +0300

    don't run parameter hints pass on each caret move
    correct refresh after 'Toggle hints' action

--- platform/lang-impl/src/com/intellij/codeInsight/hints/PopupActions.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/hints/PopupActions.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -316,6 +316,7 @@ private fun hasEditorParameterHintAtOffset(editor: Editor, file: PsiFile): Boole
 
 
 private fun refreshAllOpenEditors() {
+  ParameterHintsPassFactory.forceHintsUpdateOnNextPass();
   ProjectManager.getInstance().openProjects.forEach {
     val psiManager = PsiManager.getInstance(it)
     val daemonCodeAnalyzer = DaemonCodeAnalyzer.getInstance(it)
