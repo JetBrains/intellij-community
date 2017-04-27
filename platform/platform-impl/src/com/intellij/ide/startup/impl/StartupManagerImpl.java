78da0503b2f363e52581512dc9ff556b7472f08d
peter <peter@jetbrains.com>
2017-04-27 13:45:12 +0200

    Revert: allow runWhenProjectIsInitialized to run in available modality (IDEA-171478)
    it appears to be controversial, let's test it more in master

--- platform/platform-impl/src/com/intellij/ide/startup/impl/StartupManagerImpl.java
+++ platform/platform-impl/src/com/intellij/ide/startup/impl/StartupManagerImpl.java
@@ -374,7 +374,7 @@ public class StartupManagerImpl extends StartupManagerEx {
 
       action.run();
     };
-    GuiUtils.invokeLaterIfNeeded(runnable, ModalityState.defaultModalityState());
+    GuiUtils.invokeLaterIfNeeded(runnable, ModalityState.NON_MODAL);
   }
 
   @TestOnly
