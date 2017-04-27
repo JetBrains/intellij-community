JetBrains/intellij-community

commit 7122ad8fa57d34ac826496e4440d8912a4fb6b5d

don't run parameter hints pass on each caret move

platform/lang-impl/src/com/intellij/codeInsight/hints/ParameterHintsPassFactory.java
@@ -27,11 +27,13 @@ import com.intellij.codeInsight.hints.settings.ParameterNameHintsSettings;
 import com.intellij.lang.Language;
 import com.intellij.openapi.components.AbstractProjectComponent;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.EditorFactory;
 import com.intellij.openapi.editor.Inlay;
 import com.intellij.openapi.editor.ex.EditorSettingsExternalizable;
 import com.intellij.openapi.editor.ex.util.CaretVisualPositionKeeper;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Key;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.SyntaxTraverser;
@@ -45,6 +47,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class ParameterHintsPassFactory extends AbstractProjectComponent implements TextEditorHighlightingPassFactory {
+  private static final Key<Long> PSI_MODIFICATION_STAMP = Key.create("psi.modification.stamp");
 
   public ParameterHintsPassFactory(Project project, TextEditorHighlightingPassRegistrar registrar) {
     super(project);
@@ -55,7 +58,14 @@ public class ParameterHintsPassFactory extends AbstractProjectComponent implemen
   @Override
   public TextEditorHighlightingPass createHighlightingPass(@NotNull PsiFile file, @NotNull Editor editor) {
     if (editor.isOneLineMode()) return null;
+    long currentStamp = getCurrentModificationStamp(file);
+    Long savedStamp = editor.getUserData(PSI_MODIFICATION_STAMP);
+    if (savedStamp != null && savedStamp == currentStamp) return null;
     return new ParameterHintsPass(file, editor);
+  }
+
+  private static long getCurrentModificationStamp(@NotNull PsiFile file) {
+    return file.getManager().getModificationTracker().getModificationCount();
   }
 
   public static List<Matcher> getBlackListMatchers(Language language) {
@@ -71,6 +81,12 @@ public class ParameterHintsPassFactory extends AbstractProjectComponent implemen
       .map((item) -> MatcherConstructor.INSTANCE.createMatcher(item))
       .filter((e) -> e != null)
       .collect(Collectors.toList());
+  }
+
+  public static void forceHintsUpdateOnNextPass() {
+    for (Editor editor : EditorFactory.getInstance().getAllEditors()) {
+      editor.putUserData(PSI_MODIFICATION_STAMP, null);
+    }
   }
 
   private static class ParameterHintsPass extends EditorBoundHighlightingPass {
@@ -144,6 +160,7 @@ public class ParameterHintsPassFactory extends AbstractProjectComponent implemen
       ParameterHintsUpdater updater = new ParameterHintsUpdater(myEditor, hints, myHints, myShowOnlyIfExistedBeforeHints);
       updater.update();
       keeper.restoreOriginalLocation(false);
+      myEditor.putUserData(PSI_MODIFICATION_STAMP, getCurrentModificationStamp(myFile));
     }
 
     @NotNull

platform/lang-impl/src/com/intellij/application/options/editor/EditorAppearanceConfigurable.java
@@ -18,6 +18,7 @@ package com.intellij.application.options.editor;
 
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzerSettings;
 import com.intellij.codeInsight.hints.InlayParameterHintsExtension;
+import com.intellij.codeInsight.hints.ParameterHintsPassFactory;
 import com.intellij.codeInsight.hints.settings.ParameterNameHintsConfigurable;
 import com.intellij.ide.ui.LafManager;
 import com.intellij.ide.ui.UISettings;
@@ -99,6 +100,7 @@ public class EditorAppearanceConfigurable extends CompositeConfigurable<UnnamedC
   private void applyNameHintsSettings() {
     EditorSettingsExternalizable settings = EditorSettingsExternalizable.getInstance();
     settings.setShowParameterNameHints(myShowParameterNameHints.isSelected());
+    ParameterHintsPassFactory.forceHintsUpdateOnNextPass();
   }
 
   private void updateWhitespaceCheckboxesState() {

platform/lang-impl/src/com/intellij/codeInsight/hints/settings/ParameterNameHintsConfigurable.java
@@ -16,10 +16,7 @@
 package com.intellij.codeInsight.hints.settings;
 
 import com.intellij.codeInsight.CodeInsightBundle;
-import com.intellij.codeInsight.hints.HintUtilsKt;
-import com.intellij.codeInsight.hints.InlayParameterHintsExtension;
-import com.intellij.codeInsight.hints.InlayParameterHintsProvider;
-import com.intellij.codeInsight.hints.Option;
+import com.intellij.codeInsight.hints.*;
 import com.intellij.lang.Language;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
@@ -138,6 +135,7 @@ public class ParameterNameHintsConfigurable extends DialogWrapper {
     });
     
     myOptions.forEach((option, checkBox) -> option.set(checkBox.isSelected()));
+    ParameterHintsPassFactory.forceHintsUpdateOnNextPass();
   }
 
   private static void storeBlackListDiff(@NotNull Language language, @NotNull String text) {

commit 9b0a544ac153f809867e0d94ef80320bf691f4ae

restore hiding of completion hints after caret movement out of parameter list (broken after commit 7122ad8)

platform/lang-impl/src/com/intellij/codeInsight/hint/ParameterInfoController.java
@@ -17,6 +17,7 @@
 package com.intellij.codeInsight.hint;
 
 import com.intellij.codeInsight.daemon.impl.ParameterHintsPresentationManager;
+import com.intellij.codeInsight.hints.ParameterHintsPassFactory;
 import com.intellij.codeInsight.lookup.Lookup;
 import com.intellij.codeInsight.lookup.LookupManager;
 import com.intellij.ide.IdeTooltip;
@@ -292,6 +293,7 @@ public class ParameterInfoController implements Disposable {
         }
       }
       if (removeHints) {
+        ParameterHintsPassFactory.forceHintsUpdateOnNextPass(myEditor);
         Disposer.dispose(this);
         return;
       }

platform/lang-impl/src/com/intellij/codeInsight/hints/ParameterHintsPassFactory.java
@@ -85,8 +85,12 @@ public class ParameterHintsPassFactory extends AbstractProjectComponent implemen
 
   public static void forceHintsUpdateOnNextPass() {
     for (Editor editor : EditorFactory.getInstance().getAllEditors()) {
-      editor.putUserData(PSI_MODIFICATION_STAMP, null);
+      forceHintsUpdateOnNextPass(editor);
     }
+  }
+
+  public static void forceHintsUpdateOnNextPass(@NotNull Editor editor) {
+    editor.putUserData(PSI_MODIFICATION_STAMP, null);
   }
 
   private static class ParameterHintsPass extends EditorBoundHighlightingPass {