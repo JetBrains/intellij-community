ddc56bcaf5b9092060bbf0e524d7268daf296386
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2017-04-28 11:58:01 +0200

    rename to LiveTemplateSchemeTest

--- platform/configuration-store-impl/testSrc/LiveTemplateSchemeTest.kt
+++ platform/configuration-store-impl/testSrc/LiveTemplateSchemeTest.kt
@@ -10,7 +10,7 @@ import org.junit.ClassRule
 import org.junit.Rule
 import org.junit.Test
 
-class TemplateSchemeTest {
+class LiveTemplateSchemeTest {
   companion object {
     @JvmField
     @ClassRule
