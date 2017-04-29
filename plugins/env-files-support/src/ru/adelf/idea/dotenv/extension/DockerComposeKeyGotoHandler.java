bd0800010e27cea899f92935a8285ad5b06c46ba
Adel <adel.faiz@gmail.com>
2017-04-29 19:24:49 +0300

    Find usages for docker-compose.yml environment variables
    GitOrigin-RevId: 905fcce40f6201de7ce0ee3a2a4065965c4998ff

---
+++ plugins/env-files-support/src/ru/adelf/idea/dotenv/extension/DockerComposeKeyGotoHandler.java
@@ -0,0 +1,53 @@
+package ru.adelf.idea.dotenv.extension;
+
+import com.intellij.codeInsight.navigation.actions.GotoDeclarationHandler;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.yaml.psi.YAMLKeyValue;
+import org.jetbrains.yaml.psi.YAMLScalar;
+import org.jetbrains.yaml.psi.YAMLSequenceItem;
+import ru.adelf.idea.dotenv.api.EnvironmentVariablesApi;
+import ru.adelf.idea.dotenv.psi.DotEnvProperty;
+import ru.adelf.idea.dotenv.util.EnvironmentVariablesUtil;
+
+public class DockerComposeKeyGotoHandler implements GotoDeclarationHandler {
+
+    @Nullable
+    @Override
+    public PsiElement[] getGotoDeclarationTargets(@Nullable PsiElement psiElement, int i, Editor editor) {
+
+        if(psiElement == null || psiElement.getParent() == null || psiElement.getParent().getParent() == null || psiElement.getParent().getParent().getParent() == null) {
+            return new PsiElement[0];
+        }
+
+        psiElement = psiElement.getParent();
+
+        if(!(psiElement instanceof YAMLScalar)) {
+            return new PsiElement[0];
+        }
+
+        if(!(psiElement.getParent() instanceof YAMLSequenceItem)) {
+            return new PsiElement[0];
+        }
+
+        PsiElement yamlKeyValue = psiElement.getParent().getParent().getParent();
+
+        if(!(yamlKeyValue instanceof YAMLKeyValue)) {
+            return new PsiElement[0];
+        }
+
+        if(!"environment".equals(((YAMLKeyValue) yamlKeyValue).getKeyText())) {
+            return new PsiElement[0];
+        }
+
+        return EnvironmentVariablesApi.getKeyUsages(psiElement.getProject(), EnvironmentVariablesUtil.getKeyFromString(((YAMLScalar) psiElement).getTextValue()));
+    }
+
+    @Nullable
+    @Override
+    public String getActionText(DataContext dataContext) {
+        return null;
+    }
+}
