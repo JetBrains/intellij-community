06fc3b6b917d20e12005f1ac92efa74bc1ce623d
liana.bakradze <liana.bakradze@jetbrains.com>
2017-04-30 17:41:14 +0300

    initial design of course information panel (EDU-926)

--- python/educational-core/src/com/jetbrains/edu/learning/newproject/ui/EduCoursesPanel.java
+++ python/educational-core/src/com/jetbrains/edu/learning/newproject/ui/EduCoursesPanel.java
@@ -8,6 +8,7 @@ import com.intellij.openapi.ui.TextFieldWithBrowseButton;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.platform.DirectoryProjectGenerator;
 import com.intellij.ui.*;
+import com.intellij.ui.components.JBLabel;
 import com.intellij.ui.components.JBList;
 import com.intellij.ui.components.JBScrollPane;
 import com.intellij.util.ui.JBUI;
@@ -31,13 +32,15 @@ import java.util.List;
 public class EduCoursesPanel extends JPanel {
   private static final JBColor LIST_COLOR = new JBColor(Gray.xFF, Gray.x39);
   private JPanel myMainPanel;
-  private JEditorPane myDescriptionTextArea;
   private JPanel myCourseListPanel;
-  private JPanel myInfoPanel;
-  private JBScrollPane myDescriptionScrollPane;
+  private JPanel myCoursePanel;
   private JPanel myAdvancedSettingsPlaceholder;
   private JPanel myAdvancedSettings;
   private FilterComponent mySearchField;
+  private JEditorPane myDescriptionTextArea;
+  private JBLabel myCourseNameLabel;
+  private JPanel myTagsPanel;
+  private JBScrollPane myInfoScroll;
   private JBList<Course> myCoursesList;
   private LabeledComponent<TextFieldWithBrowseButton> myLocationField;
   private List<Course> myCourses;
@@ -51,7 +54,13 @@ public class EduCoursesPanel extends JPanel {
 
   private void initUI() {
     GuiUtils.replaceJSplitPaneWithIDEASplitter(myMainPanel, true);
+    myCourseNameLabel.setBorder(IdeBorderFactory.createEmptyBorder(0, 10, -10, 10));
+    myTagsPanel.setBorder(IdeBorderFactory.createEmptyBorder(0, 10, 0, 10));
+    myDescriptionTextArea.setBorder(IdeBorderFactory.createEmptyBorder(20, 10, 10, 10));
+    myDescriptionTextArea.setEditorKit(UIUtil.getHTMLEditorKit());
     myDescriptionTextArea.setEditable(false);
+    myDescriptionTextArea.setPreferredSize(JBUI.size(myCoursePanel.getPreferredSize()));
+    myInfoScroll.setBorder(null);
     myCoursesList = new JBList<>();
     myCourses = new StudyProjectGenerator().getCoursesUnderProgress(true, "Getting Available Courses", null);
     updateModel(myCourses);
@@ -74,25 +83,8 @@ public class EduCoursesPanel extends JPanel {
         if (selectedCourse == null) {
           return;
         }
-        myDescriptionTextArea.setText(selectedCourse.getDescription());
-        myAdvancedSettingsPlaceholder.setVisible(true);
-        myLocationField.getComponent().setText(nameToLocation(selectedCourse.getName()));
-        EduPluginConfigurator configurator = EduPluginConfigurator.INSTANCE.forLanguage(selectedCourse.getLanguageById());
-        if (configurator == null) {
-          return;
-        }
-        EduCourseProjectGenerator generator = configurator.getEduCourseProjectGenerator();
-        if (generator == null) {
-          return;
-        }
-        LabeledComponent<JComponent> component = generator.getLanguageSettingsComponent();
-        if (component == null) {
-          return;
-        }
-        myAdvancedSettings.removeAll();
-        myAdvancedSettings.add(myLocationField, BorderLayout.NORTH);
-        myAdvancedSettings.add(component, BorderLayout.SOUTH);
-        UIUtil.mergeComponentsWithAnchor(myLocationField, component);
+        updateCourseInfoPanel(selectedCourse);
+        updateAdvancedSettings(selectedCourse);
       }
     });
     JScrollPane installedScrollPane = ScrollPaneFactory.createScrollPane(myCoursesList, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
@@ -100,14 +92,64 @@ public class EduCoursesPanel extends JPanel {
     installedScrollPane.setBorder(null);
     myCourseListPanel.add(installedScrollPane, BorderLayout.CENTER);
     Border border = JBUI.Borders.customLine(OnePixelDivider.BACKGROUND, 1, 0, 1, 1);
-    myInfoPanel.setBorder(border);
+    myCoursePanel.setBorder(border);
     myCourseListPanel.setBorder(JBUI.Borders.customLine(OnePixelDivider.BACKGROUND, 1, 1, 1, 1));
     HideableDecorator decorator = new HideableDecorator(myAdvancedSettingsPlaceholder, "Advanced Settings", false);
     decorator.setContentComponent(myAdvancedSettings);
     myAdvancedSettings.setBorder(IdeBorderFactory.createEmptyBorder(0, IdeBorderFactory.TITLED_BORDER_INDENT, 5, 0));
     UIUtil.setBackgroundRecursively(myCoursesList, LIST_COLOR);
-    UIUtil.setBackgroundRecursively(myDescriptionScrollPane, UIUtil.getPanelBackground());
+    myDescriptionTextArea.setBackground(UIUtil.getPanelBackground());
     myAdvancedSettingsPlaceholder.setVisible(false);
+  }
+
+  private void updateAdvancedSettings(Course selectedCourse) {
+    myAdvancedSettingsPlaceholder.setVisible(true);
+    myLocationField.getComponent().setText(nameToLocation(selectedCourse.getName()));
+    EduPluginConfigurator configurator = EduPluginConfigurator.INSTANCE.forLanguage(selectedCourse.getLanguageById());
+    if (configurator == null) {
+      return;
+    }
+    EduCourseProjectGenerator generator = configurator.getEduCourseProjectGenerator();
+    if (generator == null) {
+      return;
+    }
+    LabeledComponent<JComponent> component = generator.getLanguageSettingsComponent();
+    if (component == null) {
+      return;
+    }
+    myAdvancedSettings.removeAll();
+    myAdvancedSettings.add(myLocationField, BorderLayout.NORTH);
+    myAdvancedSettings.add(component, BorderLayout.SOUTH);
+    UIUtil.mergeComponentsWithAnchor(myLocationField, component);
+    myAdvancedSettings.revalidate();
+    myAdvancedSettings.repaint();
+  }
+
+  private void updateCourseInfoPanel(Course selectedCourse) {
+    String courseName = selectedCourse.getName();
+    String description = selectedCourse.getDescription();
+    myCourseNameLabel.setText(UIUtil.toHtml("<h1>" + courseName + "</h1>"));
+    myTagsPanel.removeAll();
+    addTags(myTagsPanel, selectedCourse);
+    myTagsPanel.revalidate();
+    myTagsPanel.repaint();
+    myDescriptionTextArea.setText(UIUtil.toHtml(description.replace("\n", "<br>")));
+  }
+
+  private static void addTags(JPanel tagsPanel, @NotNull Course course) {
+    tagsPanel.add(createTagLabel(course.getLanguageById().getDisplayName()));
+    if (course.isAdaptive()) {
+      tagsPanel.add(createTagLabel("Adaptive"));
+    }
+  }
+
+  private static JLabel createTagLabel(String tagText) {
+    Border emptyBorder = IdeBorderFactory.createEmptyBorder(3, 5, 3, 5);
+    JBLabel label = new JBLabel(tagText);
+    label.setOpaque(true);
+    label.setBorder(emptyBorder);
+    label.setBackground(JBColor.LIGHT_GRAY);
+    return label;
   }
 
   private void updateModel(List<Course> courses) {
