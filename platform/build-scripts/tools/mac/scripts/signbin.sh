5ed2b17d7725cf79cc423f192b91444db4801cb4
Vladimir.Orlov <vladimir.orlov@jetbrains.com>
2017-05-02 15:52:58 +0300

    IDEA-CR-20747
    (cherry picked from commit c78d3e5)

--- platform/build-scripts/tools/mac/scripts/signbin.sh
+++ platform/build-scripts/tools/mac/scripts/signbin.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#immediately exit script with an error if a command fails
+set -euo pipefail
+
 FILENAME=$1
 USERNAME=$2
 PASSWORD=$3
@@ -11,22 +14,22 @@ security unlock-keychain -p ${PASSWORD} /Users/${USERNAME}/Library/Keychains/log
 
 attemp=1
 limit=3
+set +e
 while [ $attemp -le $limit ]
 do
   echo "signing (attemp $attemp) ${FILEPATH}"
   codesign -v --deep --force -s "${CODESIGN_STRING}" ${FILEPATH}
-  echo "signing done"
-  codesign -v ${FILEPATH} -vvvvv
-  echo "check sign done"
   if [ "$?" != "0" ]; then
     let "attemp += 1"
     if [ $attemp -eq $limit ]; then
-      #immediately exit script with an error if a command fails
-      set -euo pipefail
+      set -e
     fi
     echo "wait for 30 sec and try to sign again"
     sleep 30;
   else
     let "attemp += $limit"
-  fi
+    echo "signing done"
+    codesign -v ${FILEPATH} -vvvvv
+    echo "check sign done"
+    fi
 done
