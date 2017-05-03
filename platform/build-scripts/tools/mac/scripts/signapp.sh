e23e777f255b8f06b975acea65c2e32e45769750
Vladimir.Orlov <Vladimir.Orlov@jetbrains.com>
2017-05-03 11:49:46 +0300

    IDEA-171989 Signing of IDEA Mac OS application sometimes fail with "A timestamp was expected but was not found" error
    IDEA-CR-20747

--- platform/build-scripts/tools/mac/scripts/signapp.sh
+++ platform/build-scripts/tools/mac/scripts/signapp.sh
@@ -66,12 +66,27 @@ set -euo pipefail
 # Make sure *.p12 is imported into local KeyChain
 security unlock-keychain -p ${PASSWORD} /Users/${USERNAME}/Library/Keychains/login.keychain
 
-echo "signing ${EXPLODED}/$BUILD_NAME"
-codesign -v --deep --force -s "${CODESIGN_STRING}" ${EXPLODED}/"$BUILD_NAME"
-echo "signing is done"
-echo "check sign"
-codesign -v ${EXPLODED}/"$BUILD_NAME" -vvvvv
-echo "check sign done"
+attemp=1
+limit=3
+set +e
+while [ $attemp -le $limit ]
+do
+  echo "signing (attemp $attemp) ${EXPLODED}/$BUILD_NAME"
+  codesign -v --deep --force -s "${CODESIGN_STRING}" ${EXPLODED}/"$BUILD_NAME"
+  if [ "$?" != "0" ]; then
+    let "attemp += 1"
+    if [ $attemp -eq $limit ]; then
+      set -e
+    fi
+    echo "wait for 30 sec and try to sign again"
+    sleep 30;
+  else
+    echo "signing done"
+    codesign -v ${EXPLODED}/"$BUILD_NAME" -vvvvv
+    echo "check sign done"
+    let "attemp += $limit"
+  fi
+done
 
 echo "Zipping ${BUILD_NAME} to $1.sit..."
 cd ${EXPLODED}
