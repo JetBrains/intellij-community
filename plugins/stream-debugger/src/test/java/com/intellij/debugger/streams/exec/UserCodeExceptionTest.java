57cdc84f9627179997aa4f6de0b9a6b3a2d9b45c
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-05-03 19:38:35 +0300

    Add tests to check that exception found

--- plugins/stream-debugger/src/test/java/com/intellij/debugger/streams/exec/UserCodeExceptionTest.java
+++ plugins/stream-debugger/src/test/java/com/intellij/debugger/streams/exec/UserCodeExceptionTest.java
@@ -15,6 +15,7 @@
  */
 package com.intellij.debugger.streams.exec;
 
+import com.intellij.debugger.streams.exec.TraceExecutionTestCase;
 import com.intellij.debugger.streams.trace.TracingResult;
 import com.intellij.debugger.streams.wrapper.StreamChain;
 import org.jetbrains.annotations.Nullable;
@@ -23,20 +24,26 @@ import org.jetbrains.annotations.Nullable;
  * @author Vitaliy.Bibaev
  */
 public class UserCodeExceptionTest extends TraceExecutionTestCase {
-  public void testExceptionWhenEvaluating() {
+  public void testExceptionInProducerCall() {
     doTest(false);
   }
 
-  public void testExceptionInPrimitiveStream() {
+  public void testExceptionInTerminatorCall() {
     doTest(false);
   }
 
-  public void testExceptionAsStreamResult() {
+  public void testExceptionInIntermediateCall() {
+    doTest(false);
+  }
+
+  public void testExceptionInStreamWithPrimitiveResult() {
     doTest(false);
   }
 
   @Override
   protected void handleSuccess(@Nullable StreamChain chain, @Nullable TracingResult result, boolean resultMustBeNull) {
+    assertNotNull(result);
     super.handleSuccess(chain, result, resultMustBeNull);
+    assertTrue(result.isException());
   }
 }
