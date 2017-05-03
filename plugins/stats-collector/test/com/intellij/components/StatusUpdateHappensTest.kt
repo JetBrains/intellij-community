58e63c761a625e96d8f69e75bc80ed0a6169baf0
Yaroslav Lepenkin <yaroslav.lepenkin@jetbrains.com>
2017-05-03 11:32:57 +0300

    added test on turning on perform experiment

--- plugins/stats-collector/test/com/intellij/components/StatusUpdateHappensTest.kt
+++ plugins/stats-collector/test/com/intellij/components/StatusUpdateHappensTest.kt
@@ -1,5 +1,7 @@
 package com.intellij.components
 
+import com.intellij.mocks.TestRequestService
+import com.intellij.sorting.WebServiceMock
 import com.intellij.stats.completion.FilePathProvider
 import com.intellij.stats.completion.RequestService
 import com.intellij.stats.completion.SenderComponent
@@ -10,6 +12,7 @@ import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.never
 import com.nhaarman.mockito_kotlin.times
 import com.nhaarman.mockito_kotlin.verify
+import org.assertj.core.api.Assertions.assertThat
 
 
 class StatusUpdateHappensTest: LightPlatformTestCase() {
@@ -26,6 +29,18 @@ class StatusUpdateHappensTest: LightPlatformTestCase() {
         verify(statusHelper, never()).isPerformExperiment()
 
         senderComponent.disposeComponent()
+    }
+
+    fun `test model turning on performExperiment`() {
+        val status = WebServiceStatus.getInstance()
+
+        TestRequestService.mock = WebServiceMock.mockRequestService(performExperiment = false)
+        status.updateStatus()
+        assertThat(status.isPerformExperiment()).isFalse()
+
+        TestRequestService.mock = WebServiceMock.mockRequestService(performExperiment = true)
+        status.updateStatus()
+        assertThat(status.isPerformExperiment()).isTrue()
     }
 
 }
