5166755978e99566a09f34dab2ccf9a07ff3a4a6
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2017-05-03 15:13:36 +0300

    [groovy] cleanup

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/api/statements/expressions/path/GrIndexProperty.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/api/statements/expressions/path/GrIndexProperty.java
@@ -35,8 +35,4 @@ public interface GrIndexProperty extends GrExpression {
 
   @Nullable
   GroovyPolyVariantReference getRValueReference();
-
-  @NotNull
-  @Override
-  GroovyPolyVariantReference[] getReferences();
 }
