7a5cc771418cb2f50014ea03916729e5e7c79f38
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2017-05-04 10:02:50 +0200

    convert ProjectUtil to kotlin

--- platform/lang-api/src/com/intellij/openapi/project/ProjectUtil.java
+++ platform/lang-api/src/com/intellij/openapi/project/ProjectUtil.java
@@ -13,115 +13,93 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.intellij.openapi.project;
+package com.intellij.openapi.project
 
-import com.intellij.ide.DataManager;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.actionSystem.DataContext;
-import com.intellij.openapi.fileEditor.UniqueVFilePathBuilder;
-import com.intellij.openapi.fileTypes.FileType;
-import com.intellij.openapi.fileTypes.FileTypeManager;
-import com.intellij.openapi.roots.ProjectRootManager;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.vfs.VirtualFilePathWrapper;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.ide.DataManager
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.fileEditor.UniqueVFilePathBuilder
+import com.intellij.openapi.fileTypes.FileType
+import com.intellij.openapi.fileTypes.FileTypeManager
+import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFilePathWrapper
+import javax.swing.JComponent
 
-import javax.swing.*;
-
-/**
- * @author max
- */
-public class ProjectUtil {
-  private ProjectUtil() {
-  }
-
-  @NotNull
-  public static String calcRelativeToProjectPath(@NotNull final VirtualFile file,
-                                                 @Nullable final Project project,
-                                                 final boolean includeFilePath) {
-    return calcRelativeToProjectPath(file, project, includeFilePath, false, false);
-  }
-
-  @NotNull
-  public static String calcRelativeToProjectPath(@NotNull final VirtualFile file,
-                                                 @Nullable final Project project,
-                                                 final boolean includeFilePath,
-                                                 final boolean includeUniqueFilePath,
-                                                 final boolean keepModuleAlwaysOnTheLeft) {
-    if (file instanceof VirtualFilePathWrapper && ((VirtualFilePathWrapper)file).enforcePresentableName()) {
-      return includeFilePath ? ((VirtualFilePathWrapper)file).getPresentablePath() : file.getName();
+object ProjectUtil {
+  @JvmOverloads
+  @JvmStatic
+  fun calcRelativeToProjectPath(file: VirtualFile, project: Project?, includeFilePath: Boolean, includeUniqueFilePath: Boolean = false, keepModuleAlwaysOnTheLeft: Boolean = false): String {
+    if (file is VirtualFilePathWrapper && file.enforcePresentableName()) {
+      return if (includeFilePath) file.presentablePath else file.name
     }
-    String url;
+    val url: String
     if (includeFilePath) {
-      url = file.getPresentableUrl();
+      url = file.presentableUrl
     }
     else if (includeUniqueFilePath) {
-      url = UniqueVFilePathBuilder.getInstance().getUniqueVirtualFilePath(project, file);
+      url = UniqueVFilePathBuilder.getInstance().getUniqueVirtualFilePath(project, file)
     }
     else {
-      url = file.getName();
+      url = file.name
     }
     if (project == null) {
-      return url;
+      return url
     }
-    return ProjectUtilCore.displayUrlRelativeToProject(file, url, project, includeFilePath, keepModuleAlwaysOnTheLeft);
+    return ProjectUtilCore.displayUrlRelativeToProject(file, url, project, includeFilePath, keepModuleAlwaysOnTheLeft)
   }
 
-  public static String calcRelativeToProjectPath(final VirtualFile file, final Project project) {
-    return calcRelativeToProjectPath(file, project, true);
+  @JvmStatic
+  fun calcRelativeToProjectPath(file: VirtualFile, project: Project): String {
+    return calcRelativeToProjectPath(file, project, true)
   }
 
-  @Nullable
-  public static Project guessProjectForFile(VirtualFile file) {
-    return ProjectLocator.getInstance().guessProjectForFile(file);
-  }
-
-  @Nullable
-  public static Project guessProjectForContentFile(@NotNull VirtualFile file) {
-    return guessProjectForContentFile(file, file.getFileType());
+  @JvmStatic
+  fun guessProjectForFile(file: VirtualFile): Project? {
+    return ProjectLocator.getInstance().guessProjectForFile(file)
   }
 
   /***
    * guessProjectForFile works incorrectly - even if file is config (idea config file) first opened project will be returned
    */
-  @Nullable
-  public static Project guessProjectForContentFile(@NotNull VirtualFile file, @NotNull FileType fileType) {
+  @JvmOverloads
+  @JvmStatic
+  fun guessProjectForContentFile(file: VirtualFile, fileType: FileType = file.fileType): Project? {
     if (ProjectCoreUtil.isProjectOrWorkspaceFile(file, fileType)) {
-      return null;
+      return null
     }
 
-    for (Project project : ProjectManager.getInstance().getOpenProjects()) {
-      if (!project.isDefault() && project.isInitialized() && !project.isDisposed() && ProjectRootManager.getInstance(project).getFileIndex().isInContent(file)) {
-        return project;
+    for (project in ProjectManager.getInstance().openProjects) {
+      if (!project.isDefault && project.isInitialized && !project.isDisposed && ProjectRootManager.getInstance(project).fileIndex.isInContent(file)) {
+        return project
       }
     }
 
-    return null;
+    return null
   }
 
-  public static boolean isProjectOrWorkspaceFile(@NotNull VirtualFile file) {
+  @JvmStatic
+  fun isProjectOrWorkspaceFile(file: VirtualFile): Boolean {
     // do not use file.getFileType() to avoid autodetection by content loading for arbitrary files
-    return ProjectCoreUtil.isProjectOrWorkspaceFile(file, FileTypeManager.getInstance().getFileTypeByFileName(file.getName()));
+    return ProjectCoreUtil.isProjectOrWorkspaceFile(file, FileTypeManager.getInstance().getFileTypeByFileName(file.name))
   }
 
-  @NotNull
-  public static Project guessCurrentProject(@Nullable JComponent component) {
-    Project project = null;
+  @JvmStatic
+  fun guessCurrentProject(component: JComponent?): Project {
+    var project: Project? = null
     if (component != null) {
-      project = CommonDataKeys.PROJECT.getData(DataManager.getInstance().getDataContext(component));
+      project = CommonDataKeys.PROJECT.getData(DataManager.getInstance().getDataContext(component))
     }
     if (project == null) {
-      Project[] openProjects = ProjectManager.getInstance().getOpenProjects();
-      if (openProjects.length > 0) project = openProjects[0];
+      val openProjects = ProjectManager.getInstance().openProjects
+      if (openProjects.size > 0) project = openProjects[0]
       if (project == null) {
-        DataContext dataContext = DataManager.getInstance().getDataContext();
-        project = CommonDataKeys.PROJECT.getData(dataContext);
+        val dataContext = DataManager.getInstance().dataContext
+        project = CommonDataKeys.PROJECT.getData(dataContext)
       }
       if (project == null) {
-        project = ProjectManager.getInstance().getDefaultProject();
+        project = ProjectManager.getInstance().defaultProject
       }
     }
-    return project;
+    return project
   }
 }
