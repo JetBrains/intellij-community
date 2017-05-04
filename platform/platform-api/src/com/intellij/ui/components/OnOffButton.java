0dd02656eb31fe0502067285927e0acca39341b2
Kirill Kirichenko <kirill.kirichenko@jetbrains.com>
2017-05-04 12:18:01 +0300

    IDEA-171869 OnOfButtonUI creation fixed

--- platform/platform-api/src/com/intellij/ui/components/OnOffButton.java
+++ platform/platform-api/src/com/intellij/ui/components/OnOffButton.java
@@ -15,14 +15,23 @@
  */
 package com.intellij.ui.components;
 
+import com.intellij.ui.Gray;
+import com.intellij.ui.JBColor;
+import com.intellij.util.ui.JBUI;
+import com.intellij.util.ui.UIUtil;
+
 import javax.swing.*;
+import javax.swing.plaf.ComponentUI;
+import javax.swing.plaf.basic.BasicToggleButtonUI;
+import java.awt.*;
+import java.util.Locale;
 
 /**
  * @author Konstantin Bulenkov
  */
 public class OnOffButton extends JToggleButton {
-  private final static String myOnText = "ON";
-  private final static String myOffText = "OFF";
+  private String myOnText = "ON";
+  private String myOffText = "OFF";
 
   public OnOffButton() {
     setBorder(null);
@@ -33,19 +42,108 @@ public class OnOffButton extends JToggleButton {
     return myOnText;
   }
 
-  //public void setOnText(String onText) {
-  //  myOnText = onText;
-  //}
+  @SuppressWarnings("unused")
+  public void setOnText(String onText) {
+    myOnText = onText;
+  }
 
   public String getOffText() {
     return myOffText;
   }
 
-  //public void setOffText(String offText) {
-  //  myOffText = offText;
-  //}
+  @SuppressWarnings("unused")
+  public void setOffText(String offText) {
+    myOffText = offText;
+  }
 
   @Override public String getUIClassID() {
     return "OnOffButtonUI";
+  }
+
+  @Override public void updateUI() {
+    setUI(UIManager.get(getUIClassID()) == null ?
+          DefaultOnOffButtonUI.createUI(this) :
+          UIManager.getUI(this));
+  }
+
+  private static class DefaultOnOffButtonUI extends BasicToggleButtonUI {
+    @SuppressWarnings({"MethodOverridesStaticMethodOfSuperclass", "UnusedDeclaration"})
+    public static ComponentUI createUI(JComponent c) {
+      c.setAlignmentY(0.5f);
+      return new DefaultOnOffButtonUI();
+    }
+
+    @Override
+    public Dimension getPreferredSize(JComponent c) {
+      int vGap = JBUI.scale(4);
+
+      OnOffButton button = (OnOffButton)c;
+      String text = button.getOffText().length() > button.getOnText().length() ? button.getOffText() : button.getOnText();
+      text = text.toUpperCase(Locale.getDefault());
+      FontMetrics fm = c.getFontMetrics(c.getFont());
+      int w = fm.stringWidth(text);
+      int h = fm.getHeight();
+      h += 2 * vGap;
+      w += 3 * h / 2;
+      return new Dimension(w, h);
+    }
+
+    @Override
+    public void paint(Graphics gr, JComponent c) {
+      if (!(c instanceof OnOffButton)) return;
+
+      int toggleArc = JBUI.scale(3);
+      int buttonArc = JBUI.scale(5);
+      int vGap = JBUI.scale(4);
+      int hGap = JBUI.scale(3);
+      int border = 1;
+
+      OnOffButton button = (OnOffButton)c;
+      Dimension size = button.getSize();
+      int w = size.width - 2 * vGap;
+      int h = size.height - 2 * hGap;
+      if (h % 2 == 1) {
+        h--;
+      }
+      Graphics2D g = ((Graphics2D)gr);
+      g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+      int xOff = (button.getWidth() - w) / 2;
+      int yOff = (button.getHeight() - h) / 2;
+      g.translate(xOff, yOff);
+      if (button.isSelected()) {
+        g.setColor(new JBColor(new Color(74, 146, 73), new Color(77, 105, 76)));
+        g.fillRoundRect(0, 0, w, h, buttonArc, buttonArc);
+        g.setColor(new JBColor(Gray._192, Gray._80));
+        g.drawRoundRect(0, 0, w, h, buttonArc, buttonArc);
+        g.setColor(new JBColor(Gray._200, Gray._100));
+        g.fillRoundRect(w - h, border, h, h - border, toggleArc, toggleArc);
+        g.setColor(UIUtil.getListForeground(true));
+        g.drawString(button.getOnText(), h / 2, h - vGap);
+      }
+      else {
+        g.setColor(UIUtil.getPanelBackground());
+        g.fillRoundRect(0, 0, w, h, buttonArc, buttonArc);
+        g.setColor(new JBColor(Gray._192, Gray._100));
+        g.drawRoundRect(0, 0, w, h, buttonArc, buttonArc);
+        g.setColor(UIUtil.getLabelDisabledForeground());
+        g.drawString(button.getOffText(), h + vGap, h - vGap);
+        g.setColor(JBColor.border());
+        g.setPaint(new GradientPaint(h, 0, new JBColor(Gray._158, Gray._100), 0, h, new JBColor(Gray._210, Gray._100)));
+        g.fillRoundRect(0, 0, h, h, toggleArc, toggleArc);
+        // g.setColor(UIUtil.getBorderColor());
+        // g.drawOval(0, 0, h, h);
+      }
+      g.translate(-xOff, -yOff);
+    }
+
+    @Override
+    public Dimension getMinimumSize(JComponent c) {
+      return getPreferredSize(c);
+    }
+
+    @Override
+    public Dimension getMaximumSize(JComponent c) {
+      return getPreferredSize(c);
+    }
   }
 }
