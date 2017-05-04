72e18a48b4b771f0c24de032cea70653bd440220
Vladimir.Orlov <Vladimir.Orlov@jetbrains.com>
2017-05-04 13:44:46 +0300

    IDEA-171989 Signing of IDEA Mac OS application sometimes fail with "A timestamp was expected but was not found" error
    IDEA-CR-20747

--- platform/build-scripts/tools/mac/scripts/signapp.sh
+++ platform/build-scripts/tools/mac/scripts/signapp.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+#immediately exit script with an error if a command fails
+set -euo pipefail
+
 export COPY_EXTENDED_ATTRIBUTES_DISABLE=true
 export COPYFILE_DISABLE=true
 EXPLODED=$2.exploded
@@ -43,6 +47,9 @@ HELP_DIR=${EXPLODED}/"$BUILD_NAME"/Contents/Resources/"$HELP_FILE"/Contents/Reso
 echo "Building help indices for $HELP_DIR"
 hiutil -Cagvf "$HELP_DIR/search.helpindex" "$HELP_DIR"
 
+#enable nullglob option to ensure that 'for' cycles don't iterate if nothing matches to the file pattern
+shopt -s nullglob
+
 for f in ${EXPLODED}/"$BUILD_NAME"/Contents/bin/*.jnilib ; do
   if [ -f "$f" ]; then
     b="$(basename "$f" .jnilib)"
@@ -56,16 +63,32 @@ for f in ${EXPLODED}/"$BUILD_NAME"/Contents/*.txt ; do
     mv "$f" ${EXPLODED}/"$BUILD_NAME"/Contents/Resources
   fi
 done
+shopt -u nullglob
 
 # Make sure *.p12 is imported into local KeyChain
 security unlock-keychain -p ${PASSWORD} /Users/${USERNAME}/Library/Keychains/login.keychain
 
-echo "signing ${EXPLODED}/$BUILD_NAME"
-codesign -v --deep --force -s "${CODESIGN_STRING}" ${EXPLODED}/"$BUILD_NAME"
-echo "signing is done"
-echo "check sign"
-codesign -v ${EXPLODED}/"$BUILD_NAME" -vvvvv
-echo "check sign done"
+attemp=1
+limit=3
+set +e
+while [ $attemp -le $limit ]
+do
+  echo "signing (attemp $attemp) ${EXPLODED}/$BUILD_NAME"
+  codesign -v --deep --force -s "${CODESIGN_STRING}" ${EXPLODED}/"$BUILD_NAME"
+  if [ "$?" != "0" ]; then
+    let "attemp += 1"
+    if [ $attemp -eq $limit ]; then
+      set -e
+    fi
+    echo "wait for 30 sec and try to sign again"
+    sleep 30;
+  else
+    echo "signing done"
+    codesign -v ${EXPLODED}/"$BUILD_NAME" -vvvvv
+    echo "check sign done"
+    let "attemp += $limit"
+  fi
+done
 
 echo "Zipping ${BUILD_NAME} to $1.sit..."
 cd ${EXPLODED}
--- platform/build-scripts/tools/mac/scripts/signbin.sh
+++ platform/build-scripts/tools/mac/scripts/signbin.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+#immediately exit script with an error if a command fails
+set -euo pipefail
+
 FILENAME=$1
 USERNAME=$2
 PASSWORD=$3
@@ -8,9 +12,24 @@ FILEPATH=$(dirname $0)/$FILENAME
 # Make sure *.p12 is imported into local KeyChain
 security unlock-keychain -p ${PASSWORD} /Users/${USERNAME}/Library/Keychains/login.keychain
 
-echo "signing ${FILEPATH}"
-codesign -v --deep --force -s "${CODESIGN_STRING}" ${FILEPATH}
-echo "signing is done"
-echo "check sign"
-codesign -v ${FILEPATH} -vvvvv
-echo "check sign done"
+attemp=1
+limit=3
+set +e
+while [ $attemp -le $limit ]
+do
+  echo "signing (attemp $attemp) ${FILEPATH}"
+  codesign -v --deep --force -s "${CODESIGN_STRING}" ${FILEPATH}
+  if [ "$?" != "0" ]; then
+    let "attemp += 1"
+    if [ $attemp -eq $limit ]; then
+      set -e
+    fi
+    echo "wait for 30 sec and try to sign again"
+    sleep 30;
+  else
+    let "attemp += $limit"
+    echo "signing done"
+    codesign -v ${FILEPATH} -vvvvv
+    echo "check sign done"
+    fi
+done
