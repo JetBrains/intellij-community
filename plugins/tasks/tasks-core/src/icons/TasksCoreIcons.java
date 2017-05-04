8c180b831fcf46a3c85c5b9efb8e7344ccd4ffb6
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2017-05-04 15:17:17 +0300

    extracting task platform modules: fixed TasksCoreIcons

--- plugins/tasks/tasks-core/src/icons/TasksCoreIcons.java
+++ plugins/tasks/tasks-core/src/icons/TasksCoreIcons.java
@@ -13,32 +13,32 @@ public class TasksCoreIcons {
     return IconLoader.getIcon(path, TasksCoreIcons.class);
   }
 
-  public static final Icon Asana = load("/com/intellij/tasks/icons/asana.png"); // 16x16
-  public static final Icon Assembla = load("/com/intellij/tasks/icons/assembla.png"); // 16x16
-  public static final Icon AutoMode = load("/com/intellij/tasks/icons/autoMode.png"); // 16x16
-  public static final Icon Bugzilla = load("/com/intellij/tasks/icons/bugzilla.png"); // 16x16
-  public static final Icon Clock = load("/com/intellij/tasks/icons/clock.png"); // 13x13
-  public static final Icon Fogbugz = load("/com/intellij/tasks/icons/fogbugz.png"); // 16x16
-  public static final Icon Github = load("/com/intellij/tasks/icons/github.png"); // 16x16
-  public static final Icon Gitlab = load("/com/intellij/tasks/icons/gitlab.png"); // 16x16
-  public static final Icon Jira = load("/com/intellij/tasks/icons/jira.png"); // 16x16
-  public static final Icon Lighthouse = load("/com/intellij/tasks/icons/lighthouse.png"); // 16x16
-  public static final Icon Mantis = load("/com/intellij/tasks/icons/mantis.png"); // 16x16
+  public static final Icon Asana = load("/icons/asana.png"); // 16x16
+  public static final Icon Assembla = load("/icons/assembla.png"); // 16x16
+  public static final Icon AutoMode = load("/icons/autoMode.png"); // 16x16
+  public static final Icon Bug = load("/icons/bug.png"); // 16x16
+  public static final Icon Bugzilla = load("/icons/bugzilla.png"); // 16x16
+  public static final Icon Clock = load("/icons/clock.png"); // 13x13
+  public static final Icon Feature = load("/icons/feature.png"); // 16x16
+  public static final Icon Fogbugz = load("/icons/fogbugz.png"); // 16x16
+  public static final Icon Github = load("/icons/github.png"); // 16x16
+  public static final Icon Gitlab = load("/icons/gitlab.png"); // 16x16
+  public static final Icon Jira = load("/icons/jira.png"); // 16x16
+  public static final Icon Lighthouse = load("/icons/lighthouse.png"); // 16x16
+  public static final Icon Mantis = load("/icons/mantis.png"); // 16x16
   
   public static class Pivotal {
-    public static final Icon Bug = load("/com/intellij/tasks/icons/pivotal/bug.png"); // 16x16
-    public static final Icon Chore = load("/com/intellij/tasks/icons/pivotal/chore.png"); // 16x16
-    public static final Icon Feature = load("/com/intellij/tasks/icons/pivotal/feature.png"); // 16x16
-    public static final Icon Release = load("/com/intellij/tasks/icons/pivotal/release.png"); // 16x16
+    public static final Icon Chore = load("/icons/pivotal/chore.png"); // 16x16
+    public static final Icon Release = load("/icons/pivotal/release.png"); // 16x16
 
   }
-  public static final Icon Pivotal = load("/com/intellij/tasks/icons/pivotal.png"); // 16x16
-  public static final Icon Redmine = load("/com/intellij/tasks/icons/redmine.png"); // 16x16
-  public static final Icon SavedContext = load("/com/intellij/tasks/icons/savedContext.png"); // 16x16
-  public static final Icon Sprintly = load("/com/intellij/tasks/icons/sprintly.png"); // 16x16
-  public static final Icon StartTimer = load("/com/intellij/tasks/icons/startTimer.png"); // 16x16
-  public static final Icon StopTimer = load("/com/intellij/tasks/icons/stopTimer.png"); // 16x16
-  public static final Icon Trac = load("/com/intellij/tasks/icons/trac.png"); // 16x16
-  public static final Icon Trello = load("/com/intellij/tasks/icons/trello.png"); // 16x16
-  public static final Icon Youtrack = load("/com/intellij/tasks/icons/youtrack.png"); // 16x16
+  public static final Icon Pivotal = load("/icons/pivotal.png"); // 16x16
+  public static final Icon Redmine = load("/icons/redmine.png"); // 16x16
+  public static final Icon SavedContext = load("/icons/savedContext.png"); // 16x16
+  public static final Icon Sprintly = load("/icons/sprintly.png"); // 16x16
+  public static final Icon StartTimer = load("/icons/startTimer.png"); // 16x16
+  public static final Icon StopTimer = load("/icons/stopTimer.png"); // 16x16
+  public static final Icon Trac = load("/icons/trac.png"); // 16x16
+  public static final Icon Trello = load("/icons/trello.png"); // 16x16
+  public static final Icon Youtrack = load("/icons/youtrack.png"); // 16x16
 }
