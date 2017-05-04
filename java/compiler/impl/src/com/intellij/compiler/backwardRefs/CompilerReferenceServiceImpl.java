519892efe900244871338e204c0ef973d919463e
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2017-05-04 13:38:59 +0300

    javac ast indices: do not catch control flow exceptions

--- java/compiler/impl/src/com/intellij/compiler/backwardRefs/CompilerReferenceServiceImpl.java
+++ java/compiler/impl/src/com/intellij/compiler/backwardRefs/CompilerReferenceServiceImpl.java
@@ -343,8 +343,8 @@ public class CompilerReferenceServiceImpl extends CompilerReferenceServiceEx imp
   @Override
   public int getInheritorCount(@NotNull LightRef.LightClassHierarchyElementDef baseClass) throws ReferenceIndexUnavailableException {
     myReadDataLock.lock();
+    if (myReader == null) throw new ReferenceIndexUnavailableException();
     try {
-      if (myReader == null) throw new ReferenceIndexUnavailableException();
       LightRef.NamedLightRef[] hierarchy = myReader.getHierarchy(baseClass, false, true, -1);
       return hierarchy == null ? -1 : hierarchy.length;
     } catch (Exception e) {
