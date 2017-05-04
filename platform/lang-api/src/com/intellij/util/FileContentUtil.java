69dc95c029341c344c56a2d66495a8b28cf03c55
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2017-05-04 11:49:03 +0200

    restore setFileText (still used)

--- platform/lang-api/src/com/intellij/util/FileContentUtil.java
+++ platform/lang-api/src/com/intellij/util/FileContentUtil.java
@@ -15,12 +15,21 @@
  */
 package com.intellij.util;
 
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.project.ProjectUtil;
+import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedHashSet;
@@ -29,6 +38,30 @@ import java.util.LinkedHashSet;
  * @author peter
  */
 public class FileContentUtil extends FileContentUtilCore {
+
+  /**
+   * @deprecated to be removed after IDEA 15. Use {@link VfsUtil#saveText(VirtualFile, String)} instead.
+   */
+  @Deprecated
+  public static void setFileText(@Nullable Project project, final VirtualFile virtualFile, final String text) throws IOException {
+    if (project == null) {
+      project = ProjectUtil.guessProjectForFile(virtualFile);
+    }
+    if (project != null) {
+      final PsiFile psiFile = PsiManager.getInstance(project).findFile(virtualFile);
+      final PsiDocumentManager psiDocumentManager = PsiDocumentManager.getInstance(project);
+      final Document document = psiFile == null? null : psiDocumentManager.getDocument(psiFile);
+      if (document != null) {
+        document.setText(text != null ? text : "");
+        psiDocumentManager.commitDocument(document);
+        FileDocumentManager.getInstance().saveDocument(document);
+        return;
+      }
+    }
+    VfsUtil.saveText(virtualFile, text != null ? text : "");
+    virtualFile.refresh(false, false);
+  }
+
   public static void reparseFiles(@NotNull final Project project, @NotNull final Collection<VirtualFile> files, final boolean includeOpenFiles) {
     LinkedHashSet<VirtualFile> fileSet = new LinkedHashSet<>(files);
     if (includeOpenFiles) {
