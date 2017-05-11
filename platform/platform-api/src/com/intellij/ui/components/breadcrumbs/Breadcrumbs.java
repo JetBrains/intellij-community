0ca434d8d2f3fb97dbad9b60a7fae935644107c1
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2017-05-11 18:55:59 +0300

    IDEA-169924 fix some typos

--- platform/platform-api/src/com/intellij/ui/components/breadcrumbs/Breadcrumbs.java
+++ platform/platform-api/src/com/intellij/ui/components/breadcrumbs/Breadcrumbs.java
@@ -471,16 +471,16 @@ public class Breadcrumbs extends JComponent {
       else if (isHovered(crumb)) {
         paintMarker(g, bounds.x, bounds.y, bounds.width, bounds.height, crumb, scale);
       }
-      if (font != null) {
+      if (font != null && foreground != null) {
         g.setFont(font);
-        FontMetrics fm = g.getFontMetrics(font);
+        FontMetrics fm = getFontMetrics(font);
         if (fm != null) {
           Rectangle iconR = new Rectangle();
           Rectangle textR = new Rectangle();
           Rectangle viewR = getBounds(scale * LEFT_RIGHT, scale * TOP_BOTTOM);
           String text = layout(fm, iconR, textR, viewR);
           if (!StringUtil.isEmpty(text)) {
-            g.setColor(foreground != null ? foreground : getForeground());
+            g.setColor(foreground);
             g.drawString(text, textR.x, textR.y += fm.getAscent());
             if (effectColor != null && effectType != null) {
               EffectPainter painter = getEffectPainter(effectType);
