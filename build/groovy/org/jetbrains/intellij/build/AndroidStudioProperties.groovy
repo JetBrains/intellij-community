dd930408a9b6367de7b1a270e7a028975369f3de
estebandlc <estebandlc@google.com>
2017-05-12 12:12:52 -0700

    Adds flags module to android.jar
    Test: Manual
    Change-Id: Icd2ccc3bc24afd875f5956a7b450566f99323bc7

--- build/groovy/org/jetbrains/intellij/build/AndroidStudioProperties.groovy
+++ build/groovy/org/jetbrains/intellij/build/AndroidStudioProperties.groovy
@@ -114,6 +114,7 @@ class AndroidStudioProperties extends BaseIdeaProperties {
       withModule("android", "android.jar", false)
       withModule("android-plugin", "android.jar")
       withModule("observable", "android.jar")
+      withModule("flags", "android.jar")
       withModule("designer", "android.jar")
       withModule("sdk-updates", "android.jar")
       withModule("wizard", "android.jar")
