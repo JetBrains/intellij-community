7cf95215e62cf59b8c1ad6bf9038ae61829ac4e0
alexey.afanasiev <Alexey.Afanasiev@jetbrains.com>
2017-05-15 16:47:06 +0300

    EA-101045 - IAE: SyntheticGroovyBlock.

--- plugins/groovy/src/org/jetbrains/plugins/groovy/formatter/blocks/GroovyBlockGenerator.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/formatter/blocks/GroovyBlockGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -223,8 +223,9 @@ public class GroovyBlockGenerator {
           subBlocks.add(new GroovyBlock(childNode, getIndent(childNode), getChildWrap(childNode), myContext));
         }
       }
-
-      subBlocks.add(createSwitchBodyBlock(bodyBlocks));
+      if (!bodyBlocks.isEmpty()) {
+        subBlocks.add(createSwitchBodyBlock(bodyBlocks));
+      }
       return subBlocks;
     }
 
