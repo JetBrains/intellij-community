df88347cbdd007f6bc1138678526a6290f03afee
likaiguo <likaiguo.happy@gmail.com>
2017-05-16 17:09:33 +0800

    pydevconsole unicode error ignore

--- python/helpers/pydev/pydevconsole.py
+++ python/helpers/pydev/pydevconsole.py
@@ -467,7 +467,10 @@ def console_exec(thread_id, frame_id, expression, dbg):
     frame = pydevd_vars.find_frame(thread_id, frame_id)
 
     is_multiline = expression.count('@LINE@') > 1
-    expression = str(expression.replace('@LINE@', '\n'))
+    try:
+        expression = str(expression.replace('@LINE@', '\n'))
+    except UnicodeEncodeError:
+        pass
 
     #Not using frame.f_globals because of https://sourceforge.net/tracker2/?func=detail&aid=2541355&group_id=85796&atid=577329
     #(Names not resolved in generator expression in method)
