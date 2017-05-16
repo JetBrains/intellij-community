b7c81f0c0ef8eb6242fa4e85a2664f4bf7f6d9e3
Konstantin Bulenkov <kb@jetbrains.com>
2017-05-16 23:28:52 +0300

    set ids for Platform toolbars
    Signed-off-by: Konstantin Bulenkov <kb@jetbrains.com>

--- platform/platform-api/src/com/intellij/ui/tabs/impl/JBTabsImpl.java
+++ platform/platform-api/src/com/intellij/ui/tabs/impl/JBTabsImpl.java
@@ -1439,7 +1439,7 @@ public class JBTabsImpl extends JComponent
       if (group != null) {
         final String place = info.getPlace();
         ActionToolbar toolbar =
-          myTabs.myActionManager.createActionToolbar(place != null ? place : ActionPlaces.UNKNOWN, group, myTabs.myHorizontalSide);
+          myTabs.myActionManager.createActionToolbar(place != null ? place : "JBTabs", group, myTabs.myHorizontalSide);
         toolbar.setTargetComponent(info.getActionsContextComponent());
         final JComponent actionToolbar = toolbar.getComponent();
         add(actionToolbar, BorderLayout.CENTER);
