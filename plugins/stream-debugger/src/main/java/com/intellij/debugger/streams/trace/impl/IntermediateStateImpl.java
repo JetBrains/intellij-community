d2085d75ed8e1a0315e4dae17a6c932e4cc4d844
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-05-16 17:36:09 +0300

    Add classes and interfaces for storing of intermediate states

---
+++ plugins/stream-debugger/src/main/java/com/intellij/debugger/streams/trace/impl/IntermediateStateImpl.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2000-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.debugger.streams.trace.impl;
+
+import com.intellij.debugger.streams.trace.NextAwareState;
+import com.intellij.debugger.streams.trace.PrevAwareState;
+import com.intellij.debugger.streams.trace.TraceElement;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Vitaliy.Bibaev
+ */
+public class IntermediateStateImpl extends StateBase implements NextAwareState, PrevAwareState {
+  @NotNull private final Map<TraceElement, List<TraceElement>> myToPrev;
+  @NotNull private final Map<TraceElement, List<TraceElement>> myToNext;
+
+  public IntermediateStateImpl(@NotNull List<TraceElement> elements,
+                               @NotNull Map<TraceElement, List<TraceElement>> toPrevMapping,
+                               @NotNull Map<TraceElement, List<TraceElement>> toNextMapping) {
+    super(elements);
+    myToPrev = toPrevMapping;
+    myToNext = toNextMapping;
+  }
+
+  @NotNull
+  @Override
+  public List<TraceElement> getPrevValues(@NotNull TraceElement value) {
+    return myToPrev.getOrDefault(value, Collections.emptyList());
+  }
+
+  @NotNull
+  @Override
+  public List<TraceElement> getNextValues(@NotNull TraceElement value) {
+    return myToNext.getOrDefault(value, Collections.emptyList());
+  }
+}
