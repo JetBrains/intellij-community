8e6699eb44ba6580e3323b8353a366d4a5d7a57b
Bas Leijdekkers <basleijdekkers@gmail.com>
2017-05-18 10:33:38 +0200

    make IG test light

--- plugins/InspectionGadgets/test/com/siyeh/igtest/threading/synchronized_on_literal_object/SynchronizedOnLiteralObject.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/threading/synchronized_on_literal_object/SynchronizedOnLiteralObject.java
@@ -6,9 +6,9 @@ class SynchronizedOnLiteralObject {
   private Character c = 'a';
 
   void foo() {
-    synchronized (myInteger) {}
-    synchronized ("asdf") {}
-    synchronized ((Boolean) true) {}
-    synchronized (c) {}
+    synchronized (<warning descr="Synchronization on Integer 'myInteger' which is initialized by a literal">myInteger</warning>) {}
+    synchronized (<warning descr="Synchronization on String literal '\"asdf\"'">"asdf"</warning>) {}
+    synchronized (<warning descr="Synchronization on Boolean literal '(Boolean) true'">(Boolean) true</warning>) {}
+    synchronized (<warning descr="Synchronization on Character 'c' which is initialized by a literal">c</warning>) {}
   }
 }
