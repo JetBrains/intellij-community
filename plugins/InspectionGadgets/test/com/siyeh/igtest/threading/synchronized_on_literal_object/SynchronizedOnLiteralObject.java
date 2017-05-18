ccd6c7744f418b7dd4089704b069b0aac0237760
Bas Leijdekkers <basleijdekkers@gmail.com>
2017-05-18 11:14:59 +0200

    IG: don't warn on synchronization on AtomicInteger

--- plugins/InspectionGadgets/test/com/siyeh/igtest/threading/synchronized_on_literal_object/SynchronizedOnLiteralObject.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/threading/synchronized_on_literal_object/SynchronizedOnLiteralObject.java
@@ -1,5 +1,7 @@
 package com.siyeh.igtest.threading.synchronized_on_literal_object;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 class SynchronizedOnLiteralObject {
 
   private Integer myInteger = (Integer)1;
@@ -10,5 +12,13 @@ class SynchronizedOnLiteralObject {
     synchronized (<warning descr="Synchronization on String literal '\"asdf\"'">"asdf"</warning>) {}
     synchronized (<warning descr="Synchronization on Boolean literal '(Boolean) true'">(Boolean) true</warning>) {}
     synchronized (<warning descr="Synchronization on Character 'c' which is initialized by a literal">c</warning>) {}
+  }
+
+  private AtomicInteger count = new AtomicInteger(1);
+
+  void up() {
+    synchronized (count) {
+      System.out.println();
+    }
   }
 }
