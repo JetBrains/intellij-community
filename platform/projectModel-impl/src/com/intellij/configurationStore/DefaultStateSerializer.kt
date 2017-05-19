92c863615de21d160d1f2222464bfb6f40ed6ac6
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2017-05-19 18:10:26 +0200

    use new kotlin-friendly xml deserializer

--- platform/projectModel-impl/src/com/intellij/configurationStore/DefaultStateSerializer.kt
+++ platform/projectModel-impl/src/com/intellij/configurationStore/DefaultStateSerializer.kt
@@ -19,35 +19,28 @@ import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.util.JDOMExternalizable
 import com.intellij.openapi.util.JDOMUtil
 import com.intellij.util.ReflectionUtil
-import com.intellij.util.xmlb.XmlSerializer
 import org.jdom.Element
 
 private val LOG = Logger.getInstance("#com.intellij.openapi.components.impl.stores.DefaultStateSerializer")
 
 fun <T> deserializeState(stateElement: Element?, stateClass: Class<T>, mergeInto: T?): T? {
-  @Suppress("DEPRECATION")
-  if (stateElement == null) {
-    return mergeInto
-  }
-  else if (stateClass == Element::class.java) {
-    @Suppress("UNCHECKED_CAST")
-    return stateElement as T?
-  }
-  else if (JDOMExternalizable::class.java.isAssignableFrom(stateClass)) {
-    if (mergeInto != null) {
-      val elementText = JDOMUtil.writeElement(stateElement)
-      LOG.error("State is ${stateClass.name}, merge into is ${mergeInto.toString()}, state element text is $elementText")
-    }
+  @Suppress("DEPRECATION", "UNCHECKED_CAST")
+  return when {
+    stateElement == null -> mergeInto
+    stateClass == Element::class.java -> stateElement as T?
+    JDOMExternalizable::class.java.isAssignableFrom(stateClass) -> {
+      if (mergeInto != null) {
+        LOG.error("State is ${stateClass.name}, merge into is ${mergeInto.toString()}, state element text is ${JDOMUtil.writeElement(stateElement)}")
+      }
 
-    val t = ReflectionUtil.newInstance(stateClass)
-    (t as JDOMExternalizable).readExternal(stateElement)
-    return t
-  }
-  else if (mergeInto == null) {
-    return XmlSerializer.deserialize(stateElement, stateClass)
-  }
-  else {
-    XmlSerializer.deserializeInto(mergeInto, stateElement)
-    return mergeInto
+      val t = ReflectionUtil.newInstance(stateClass)
+      (t as JDOMExternalizable).readExternal(stateElement)
+      t
+    }
+    mergeInto == null -> stateElement.deserialize(stateClass)
+    else -> {
+      stateElement.deserializeInto(mergeInto)
+      mergeInto
+    }
   }
 }
