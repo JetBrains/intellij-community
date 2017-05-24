1db8b83dfff4427515b5032655ca6e5696494879
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2017-05-24 14:12:51 +0700

    IDEA-173177 Inspection: Comparing 'compareTo()` or 'Comparator.compare()' result with 1 / -1

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/comparatorResultComparison/beforeComparableVar.java
@@ -0,0 +1,17 @@
+// "Fix all 'Comparison of compare method result with specific constant' problems in file" "true"
+import java.util.*;
+
+class Test {
+  String test(Comparable<?> c1, Comparable<?> c2) {
+    int result = c1.compareTo(c2);
+    if(0 == result) {
+      return "equal";
+    } else if(-1 =<caret>= result) {
+      return "less";
+    } else if(1 == result) {
+      return "greater";
+    } else {
+      return "impossible";
+    }
+  }
+}
