ea4c21384de642ac110d3060dafe616bedadc13a
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2017-05-25 12:42:43 +0700

    Test01#checkTrueFail marked as pure

--- java/java-tests/testSrc/com/intellij/java/codeInspection/bytecodeAnalysis/data/Test01.java
+++ java/java-tests/testSrc/com/intellij/java/codeInspection/bytecodeAnalysis/data/Test01.java
@@ -201,7 +201,7 @@ public class Test01 {
     return !x;
   }
 
-  @ExpectContract(value = "_,true->true;null,_->true")
+  @ExpectContract(value = "_,true->true;null,_->true", pure=true)
   boolean checkTrueFail(Object a, boolean b) {
     if (a == null) return true;
     if (b) throw new RuntimeException();
