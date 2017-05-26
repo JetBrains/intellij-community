JetBrains/intellij-community

commit 485dc5269122ff989471d37b7f3826cb5f34bb49

IDEA-131690 Settings repository plugin: when I merged settings from PyCharm java-related setting was deleted

platform/lang-api/src/com/intellij/psi/codeStyle/CodeStyleSettings.java
@@ -15,6 +15,8 @@
  */
 package com.intellij.psi.codeStyle;
 
+import com.intellij.configurationStore.UnknownElementCollector;
+import com.intellij.configurationStore.UnknownElementWriter;
 import com.intellij.lang.Language;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Document;
@@ -56,6 +58,8 @@ public class CodeStyleSettings extends CommonCodeStyleSettings implements Clonea
 
   @NonNls private static final String FILETYPE = "fileType";
   private CommonCodeStyleSettingsManager myCommonSettingsManager = new CommonCodeStyleSettingsManager(this);
+
+  private UnknownElementWriter myUnknownElementWriter = UnknownElementWriter.EMPTY;
 
   public CodeStyleSettings() {
     this(true);
@@ -490,11 +494,14 @@ public class CodeStyleSettings extends CommonCodeStyleSettings implements Clonea
         IMPORT_LAYOUT_TABLE.addEntry(PackageEntry.ALL_OTHER_STATIC_IMPORTS_ENTRY);
       }
     }
+
+    UnknownElementCollector unknownElementCollector = new UnknownElementCollector();
     for (CustomCodeStyleSettings settings : getCustomSettingsValues()) {
+      unknownElementCollector.addKnownName(settings.getTagName());
       settings.readExternal(element);
-      settings.importLegacySettings();
     }
 
+    unknownElementCollector.addKnownName(ADDITIONAL_INDENT_OPTIONS);
     List<Element> list = element.getChildren(ADDITIONAL_INDENT_OPTIONS);
     if (list != null) {
       for (Element additionalIndentElement : list) {
@@ -512,7 +519,10 @@ public class CodeStyleSettings extends CommonCodeStyleSettings implements Clonea
       }
     }
 
+    unknownElementCollector.addKnownName(CommonCodeStyleSettingsManager.COMMON_SETTINGS_TAG);
     myCommonSettingsManager.readExternal(element);
+
+    myUnknownElementWriter = unknownElementCollector.createWriter(element);
 
     if (USE_SAME_INDENTS) {
       IGNORE_SAME_INDENTS_FOR_LANGUAGES = true;
@@ -523,16 +533,14 @@ public class CodeStyleSettings extends CommonCodeStyleSettings implements Clonea
   public void writeExternal(Element element) throws WriteExternalException {
     final CodeStyleSettings parentSettings = new CodeStyleSettings();
     DefaultJDOMExternalizer.writeExternal(this, element, new DifferenceFilter<CodeStyleSettings>(this, parentSettings));
-    List<CustomCodeStyleSettings> customSettings = new ArrayList<CustomCodeStyleSettings>(getCustomSettingsValues());
-    customSettings.sort((o1, o2) -> o1.getTagName().compareTo(o2.getTagName()));
 
-    for (final CustomCodeStyleSettings settings : customSettings) {
-      final CustomCodeStyleSettings parentCustomSettings = parentSettings.getCustomSettings(settings.getClass());
+    myUnknownElementWriter.write(element, getCustomSettingsValues(), CustomCodeStyleSettings::getTagName, settings -> {
+      CustomCodeStyleSettings parentCustomSettings = parentSettings.getCustomSettings(settings.getClass());
       if (parentCustomSettings == null) {
         throw new WriteExternalException("Custom settings are null for " + settings.getClass());
       }
       settings.writeExternal(element, parentCustomSettings);
-    }
+    });
 
     final FileType[] fileTypes = myAdditionalIndentOptions.keySet().toArray(new FileType[myAdditionalIndentOptions.keySet().size()]);
     Arrays.sort(fileTypes, (o1, o2) -> o1.getDefaultExtension().compareTo(o2.getDefaultExtension()));

platform/lang-api/src/com/intellij/psi/codeStyle/CustomCodeStyleSettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,7 +45,11 @@ public abstract class CustomCodeStyleSettings implements Cloneable {
   }
 
   public void readExternal(Element parentElement) throws InvalidDataException {
-    DefaultJDOMExternalizer.readExternal(this, parentElement.getChild(myTagName));
+    Element child = parentElement.getChild(myTagName);
+    if (child != null) {
+      DefaultJDOMExternalizer.readExternal(this, child);
+      importLegacySettings();
+    }
   }
 
   public void writeExternal(Element parentElement, @NotNull final CustomCodeStyleSettings parentSettings) throws WriteExternalException {
@@ -69,7 +73,6 @@ public abstract class CustomCodeStyleSettings implements Cloneable {
   /**
    * For compatibility with old code style settings stored in CodeStyleSettings.
    */
-  public void importLegacySettings() {
+  protected void importLegacySettings() {
   }
-
 }

xml/impl/src/com/intellij/psi/formatter/xml/XmlCodeStyleSettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -54,7 +54,7 @@ public class XmlCodeStyleSettings extends CustomCodeStyleSettings {
   }
 
   @Override
-  public void importLegacySettings() {
+  protected void importLegacySettings() {
     if (!XML_LEGACY_SETTINGS_IMPORTED) {
       CodeStyleSettings container = getContainer();
       CodeStyleSettings defaults = new CodeStyleSettings();

java/java-impl/src/com/intellij/psi/codeStyle/JavaCodeStyleSettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -48,11 +48,7 @@ public class JavaCodeStyleSettings extends CustomCodeStyleSettings {
   }
   
   @Override
-  public void importLegacySettings() {
-    importLegacyUseFqClassNamesInJavadocSetting();
-  }
-
-  private void importLegacyUseFqClassNamesInJavadocSetting() {
+  protected void importLegacySettings() {
     CodeStyleSettings settings = getContainer();
     boolean isDefaultValue = settings.USE_FQ_CLASS_NAMES_IN_JAVADOC;
     if (!isDefaultValue) {

platform/configuration-store-impl/testSrc/CodeStyleTest.kt
@@ -1,0 +1,45 @@
+package com.intellij.configurationStore
+
+import com.intellij.openapi.util.JDOMUtil
+import com.intellij.psi.codeStyle.CodeStyleSettings
+import com.intellij.testFramework.ProjectRule
+import com.intellij.util.loadElement
+import org.assertj.core.api.Assertions.assertThat
+import org.jdom.Element
+import org.junit.ClassRule
+import org.junit.Test
+
+class CodeStyleTest {
+  companion object {
+    @JvmField
+    @ClassRule
+    val projectRule = ProjectRule()
+  }
+
+  @Test fun `do not remove unknown`() {
+    val settings = CodeStyleSettings()
+    val loaded = """
+    <code_scheme name="testSchemeName">
+      <UnknownDoNotRemoveMe>
+        <option name="ALIGN_OBJECT_PROPERTIES" value="2" />
+      </UnknownDoNotRemoveMe>
+      <codeStyleSettings language="CoffeeScript">
+        <option name="LINE_COMMENT_AT_FIRST_COLUMN" value="false" />
+      </codeStyleSettings>
+      <codeStyleSettings language="Gherkin">
+        <indentOptions>
+          <option name="USE_TAB_CHARACTER" value="true" />
+        </indentOptions>
+      </codeStyleSettings>
+      <codeStyleSettings language="SQL">
+        <option name="KEEP_LINE_BREAKS" value="false" />
+        <option name="KEEP_BLANK_LINES_IN_CODE" value="10" />
+      </codeStyleSettings>
+    </code_scheme>""".trimIndent()
+    settings.readExternal(loadElement(loaded))
+
+    val serialized = Element("code_scheme").setAttribute("name", "testSchemeName")
+    settings.writeExternal(serialized)
+    assertThat(JDOMUtil.writeElement(serialized)).isEqualTo(loaded)
+  }
+}

platform/lang-api/lang-api.iml
@@ -16,5 +16,6 @@
     <orderEntry type="library" scope="TEST" name="JUnit4" level="project" />
     <orderEntry type="library" scope="TEST" name="Mocks" level="project" />
     <orderEntry type="module" module-name="analysis-api" exported="" />
+    <orderEntry type="library" name="KotlinJavaRuntime" level="project" />
   </component>
 </module>

platform/lang-api/src/com/intellij/configurationStore/UnknownElementManager.kt
@@ -1,0 +1,85 @@
+/*
+ * Copyright 2000-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.configurationStore
+
+import gnu.trove.THashMap
+import gnu.trove.THashSet
+import org.jdom.Element
+import java.util.function.Consumer
+import java.util.function.Function
+
+// Empty unknown tags supported to simplify client write code (with and without unknown elements)
+class UnknownElementWriter internal constructor(private val unknownElements: Map<String, Element> = emptyMap()) {
+  companion object {
+    @JvmField
+    val EMPTY = UnknownElementWriter()
+  }
+
+  fun <T> write(outElement: Element, items: Collection<T>, itemToTagName: Function<T, String>, writer: Consumer<T>) {
+    val knownNameToWriter = THashMap<String, T>(items.size)
+    for (item in items) {
+      knownNameToWriter.put(itemToTagName.apply(item), item)
+    }
+    write(outElement, knownNameToWriter, writer)
+  }
+
+  fun <T> write(outElement: Element, knownNameToWriter: Map<String, T>, writer: Consumer<T>) {
+    val names: Set<String>
+    if (unknownElements.isEmpty()) {
+      names = knownNameToWriter.keys
+    }
+    else {
+      names = THashSet<String>(unknownElements.keys)
+      names.addAll(knownNameToWriter.keys)
+    }
+
+    val sortedNames = names.toTypedArray()
+    sortedNames.sort()
+    for (name in sortedNames) {
+      val known = knownNameToWriter.get(name)
+      if (known == null) {
+        outElement.addContent(unknownElements.get(name)!!.clone())
+      }
+      else {
+        writer.accept(known)
+      }
+    }
+  }
+}
+
+class UnknownElementCollector {
+  private val knownTagNames = THashSet<String>()
+
+  fun addKnownName(name: String) {
+    knownTagNames.add(name)
+  }
+
+  fun createWriter(element: Element): UnknownElementWriter? {
+    var unknownElements: MutableMap<String, Element>? = null
+    val iterator = element.children.iterator()
+    for (child in iterator) {
+      if (child.name != "option" && !knownTagNames.contains(child.name)) {
+        if (unknownElements == null) {
+          unknownElements = THashMap()
+        }
+        unknownElements.put(child.name, child)
+        iterator.remove()
+      }
+    }
+
+    return unknownElements?.let { UnknownElementWriter(it) } ?: UnknownElementWriter.EMPTY
+  }
+}

platform/lang-api/src/com/intellij/psi/codeStyle/CommonCodeStyleSettingsManager.java
@@ -45,7 +45,7 @@ public class CommonCodeStyleSettingsManager {
 
   @NotNull private final CodeStyleSettings myParentSettings;
 
-  @NonNls private static final String COMMON_SETTINGS_TAG = "codeStyleSettings";
+  @NonNls static final String COMMON_SETTINGS_TAG = "codeStyleSettings";
   private static final String LANGUAGE_ATTR = "language";
 
   CommonCodeStyleSettingsManager(@NotNull CodeStyleSettings parentSettings) {

commit 7110677d087d24a4c67d937e94d69e4f32697ad5

seems like 'legacy settings' import was broken after 485dc5269122ff989471d37b7f3826cb5f34bb49 for cases of importing from CodeStyleSettings to language-specific New settings element will not be present (since it's new), so method will never be called. As such, seems safe to get rid of legacy XML settings

platform/lang-api/src/com/intellij/psi/codeStyle/CodeStyleSettings.java
@@ -417,65 +417,6 @@ public class CodeStyleSettings extends CommonCodeStyleSettings implements Clonea
 
 // endregion
 
-// region Legacy(!) XML formatting options
-
-  /**
-   * @deprecated Use XmlCodeStyleSettings.
-   */
-  public boolean XML_KEEP_WHITESPACES;
-  /**
-   * @deprecated Use XmlCodeStyleSettings.
-   */
-  public int XML_ATTRIBUTE_WRAP = WRAP_AS_NEEDED;
-  /**
-   * @deprecated Use XmlCodeStyleSettings.
-   */
-  public int XML_TEXT_WRAP = WRAP_AS_NEEDED;
-  /**
-   * @deprecated Use XmlCodeStyleSettings.
-   */
-  public boolean XML_KEEP_LINE_BREAKS = true;
-  /**
-   * @deprecated Use XmlCodeStyleSettings.
-   */
-  public boolean XML_KEEP_LINE_BREAKS_IN_TEXT = true;
-  /**
-   * @deprecated Use XmlCodeStyleSettings.
-   */
-  public int XML_KEEP_BLANK_LINES = 2;
-
-  /**
-   * @deprecated Use XmlCodeStyleSettings.
-   */
-  public boolean XML_ALIGN_ATTRIBUTES = true;
-  /**
-   * @deprecated Use XmlCodeStyleSettings.
-   */
-  public boolean XML_ALIGN_TEXT;
-  /**
-   * @deprecated Use XmlCodeStyleSettings.
-   */
-  public boolean XML_SPACE_AROUND_EQUALITY_IN_ATTRIBUTE;
-  /**
-   * @deprecated Use XmlCodeStyleSettings.
-   */
-  public boolean XML_SPACE_AFTER_TAG_NAME;
-  /**
-   * @deprecated Use XmlCodeStyleSettings.
-   */
-  public boolean XML_SPACE_INSIDE_EMPTY_TAG;
-  /**
-   * @deprecated Use XmlCodeStyleSettings.
-   */
-  public boolean XML_KEEP_WHITE_SPACES_INSIDE_CDATA;
-  /**
-   * @deprecated Use XmlCodeStyleSettings.
-   */
-  public int XML_WHITE_SPACE_AROUND_CDATA;
-
-// endregion
-
-
 // region HTML formatting options (legacy)
 
   /**

platform/lang-api/src/com/intellij/psi/codeStyle/CustomCodeStyleSettings.java
@@ -60,8 +60,8 @@ public abstract class CustomCodeStyleSettings implements Cloneable {
     Element child = parentElement.getChild(myTagName);
     if (child != null) {
       DefaultJDOMExternalizer.readExternal(this, child);
-      importLegacySettings();
     }
+    importLegacySettings();
   }
 
   public void writeExternal(Element parentElement, @NotNull final CustomCodeStyleSettings parentSettings) throws WriteExternalException {

xml/impl/src/com/intellij/psi/formatter/xml/XmlCodeStyleSettings.java
@@ -53,40 +53,4 @@ public class XmlCodeStyleSettings extends CustomCodeStyleSettings {
     super("XML", container);
   }
 
-  @Override
-  protected void importLegacySettings() {
-    if (!XML_LEGACY_SETTINGS_IMPORTED) {
-      CodeStyleSettings container = getContainer();
-      CodeStyleSettings defaults = new CodeStyleSettings();
-      XML_KEEP_WHITESPACES = container.XML_KEEP_WHITESPACES;
-      XML_ATTRIBUTE_WRAP = container.XML_ATTRIBUTE_WRAP;
-      XML_TEXT_WRAP = container.XML_TEXT_WRAP;
-      XML_KEEP_LINE_BREAKS = container.XML_KEEP_LINE_BREAKS;
-      XML_KEEP_LINE_BREAKS_IN_TEXT = container.XML_KEEP_LINE_BREAKS_IN_TEXT;
-      XML_KEEP_BLANK_LINES = container.XML_KEEP_BLANK_LINES;
-      XML_ALIGN_ATTRIBUTES = container.XML_ALIGN_ATTRIBUTES;
-      XML_ALIGN_TEXT = container.XML_ALIGN_TEXT;
-      XML_SPACE_AROUND_EQUALITY_IN_ATTRIBUTE = container.XML_SPACE_AROUND_EQUALITY_IN_ATTRIBUTE;
-      XML_SPACE_AFTER_TAG_NAME = container.XML_SPACE_AFTER_TAG_NAME;
-      XML_SPACE_INSIDE_EMPTY_TAG = container.XML_SPACE_INSIDE_EMPTY_TAG;
-      XML_KEEP_WHITE_SPACES_INSIDE_CDATA = container.XML_KEEP_WHITE_SPACES_INSIDE_CDATA;
-      XML_WHITE_SPACE_AROUND_CDATA = container.XML_WHITE_SPACE_AROUND_CDATA;
-
-      container.XML_KEEP_WHITESPACES = defaults.XML_KEEP_WHITESPACES ;
-      container.XML_ATTRIBUTE_WRAP = defaults.XML_ATTRIBUTE_WRAP;
-      container.XML_TEXT_WRAP = defaults.XML_TEXT_WRAP;
-      container.XML_KEEP_LINE_BREAKS = defaults.XML_KEEP_LINE_BREAKS;
-      container.XML_KEEP_LINE_BREAKS_IN_TEXT = defaults.XML_KEEP_LINE_BREAKS_IN_TEXT;
-      container.XML_KEEP_BLANK_LINES = defaults.XML_KEEP_BLANK_LINES;
-      container.XML_ALIGN_ATTRIBUTES = defaults.XML_ALIGN_ATTRIBUTES;
-      container.XML_ALIGN_TEXT = defaults.XML_ALIGN_TEXT;
-      container.XML_SPACE_AROUND_EQUALITY_IN_ATTRIBUTE = defaults.XML_SPACE_AROUND_EQUALITY_IN_ATTRIBUTE;
-      container.XML_SPACE_AFTER_TAG_NAME = defaults.XML_SPACE_AFTER_TAG_NAME;
-      container.XML_SPACE_INSIDE_EMPTY_TAG = defaults.XML_SPACE_INSIDE_EMPTY_TAG;
-      container.XML_KEEP_WHITE_SPACES_INSIDE_CDATA = defaults.XML_KEEP_WHITE_SPACES_INSIDE_CDATA;
-      container.XML_WHITE_SPACE_AROUND_CDATA = defaults.XML_WHITE_SPACE_AROUND_CDATA;
-
-      XML_LEGACY_SETTINGS_IMPORTED = true;
-    }
-  }
 }