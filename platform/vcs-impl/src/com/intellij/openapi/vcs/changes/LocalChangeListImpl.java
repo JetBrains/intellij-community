JetBrains/intellij-community

commit 61365c7e90042787ca09e2d1b47be93aa184fb1d

vcs: cleanup - move field copying into copy constructor

platform/vcs-impl/src/com/intellij/openapi/vcs/changes/LocalChangeListImpl.java
@@ -49,6 +49,23 @@ public class LocalChangeListImpl extends LocalChangeList {
     myId = origin.getId();
     myProject = origin.myProject;
     setName(origin.myName);
+
+    myComment = origin.myComment;
+    myIsDefault = origin.myIsDefault;
+    myIsReadOnly = origin.myIsReadOnly;
+    myData = origin.myData;
+
+    if (myChanges != null) {
+      myChanges = ContainerUtil.newHashSet(origin.myChanges);
+    }
+
+    if (myChangesBeforeUpdate != null) {
+      myChangesBeforeUpdate = new OpenTHashSet((Collection<Change>)origin.myChangesBeforeUpdate);
+    }
+
+    if (myReadChangesCache != null) {
+      myReadChangesCache = ContainerUtil.newHashSet(origin.myReadChangesCache);
+    }
   }
 
   @NotNull
@@ -246,25 +263,7 @@ public class LocalChangeListImpl extends LocalChangeList {
   }
 
   public LocalChangeList copy() {
-    final LocalChangeListImpl copy = new LocalChangeListImpl(this);
-    copy.myComment = myComment;
-    copy.myIsDefault = myIsDefault;
-    copy.myIsReadOnly = myIsReadOnly;
-    copy.myData = myData;
-
-    if (myChanges != null) {
-      copy.myChanges = ContainerUtil.newHashSet(myChanges);
-    }
-
-    if (myChangesBeforeUpdate != null) {
-      copy.myChangesBeforeUpdate = new OpenTHashSet<>((Collection<Change>)myChangesBeforeUpdate);
-    }
-
-    if (myReadChangesCache != null) {
-      copy.myReadChangesCache = ContainerUtil.newHashSet(myReadChangesCache);
-    }
-
-    return copy;
+    return new LocalChangeListImpl(this);
   }
 
   public void setId(String id) {

commit 940b794133f000b3cfe75611ea0dde83099e3147

vcs: remove pointless check
typo introduced in 61365c7e90042787ca09e2d1b47be93aa184fb1d
follow-up: 16d73a518f0b2ce67c5193b1e245290246edeaab

platform/vcs-impl/src/com/intellij/openapi/vcs/changes/LocalChangeListImpl.java
@@ -53,10 +53,7 @@ public class LocalChangeListImpl extends LocalChangeList {
     myData = origin.myData;
 
     myChanges = ContainerUtil.newHashSet(origin.myChanges);
-
-    if (myReadChangesCache != null) {
-      myReadChangesCache = origin.myReadChangesCache;
-    }
+    myReadChangesCache = origin.myReadChangesCache;
   }
 
   @NotNull