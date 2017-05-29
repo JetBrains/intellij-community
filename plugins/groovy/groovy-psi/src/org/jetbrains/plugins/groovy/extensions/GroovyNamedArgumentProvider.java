423195eb47a41008371bc971f9c16cfb02159a6b
alexey.afanasiev <Alexey.Afanasiev@jetbrains.com>
2017-05-29 12:15:27 +0300

    IDEA-170804 Idea does't support @Newify annotation

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/extensions/GroovyNamedArgumentProvider.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/extensions/GroovyNamedArgumentProvider.java
@@ -109,10 +109,10 @@ public abstract class GroovyNamedArgumentProvider {
 
         if (element instanceof PsiMethod) {
           PsiMethod method = (PsiMethod)element;
-          method = PsiUtil.handleMirrorMethod(method);
+          PsiMethod mirror = PsiUtil.handleMirrorMethod(method);
           PsiParameter[] parameters = method.getParameterList().getParameters();
 
-          if (!method.isConstructor() && !(parameters.length > 0 && canBeMap(parameters[0]))) continue;
+          if (!mirror.isConstructor() && !(parameters.length > 0 && canBeMap(parameters[0]))) continue;
 
           mapExpected = true;
 
