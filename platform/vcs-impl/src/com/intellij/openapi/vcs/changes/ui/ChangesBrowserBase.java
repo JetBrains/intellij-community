6567d4ea2d716443dea5f4a7ec946de06df3e23e
Aleksey Pivovarov <AMPivovarov@gmail.com>
2017-05-30 18:16:56 +0300

    vcs: consume "Show Diff" shortcut in log if there are no changes
    * avoid spurious invocation of other actions (ex: "Compare Files")

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/ChangesBrowserBase.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/ChangesBrowserBase.java
@@ -48,6 +48,7 @@ import javax.swing.border.Border;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreePath;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.util.*;
 import java.util.List;
@@ -369,7 +370,7 @@ public abstract class ChangesBrowserBase<T> extends JPanel implements TypeSafeDa
   protected void buildToolBar(final DefaultActionGroup toolBarGroup) {
     myDiffAction = new DumbAwareAction() {
       public void update(AnActionEvent e) {
-        e.getPresentation().setEnabled(canShowDiff());
+        e.getPresentation().setEnabled(canShowDiff() || e.getInputEvent() instanceof KeyEvent);
       }
 
       public void actionPerformed(AnActionEvent e) {
