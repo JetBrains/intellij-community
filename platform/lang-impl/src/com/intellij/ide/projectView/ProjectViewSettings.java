40f14e07f9edfbaee0473abe6e24b25f59c402ad
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2017-05-30 22:48:01 +0300

    WEB-17977 Feature: Configure Patterns for Automatic Nesting of Transpiled Files

--- platform/lang-impl/src/com/intellij/ide/projectView/ProjectViewSettings.java
+++ platform/lang-impl/src/com/intellij/ide/projectView/ProjectViewSettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,4 +20,6 @@ package com.intellij.ide.projectView;
  */
 public interface ProjectViewSettings extends ViewSettings {
   boolean isShowExcludedFiles();
+
+  default boolean isUseFileNestingRules() {return true;}
 }
---
+++ platform/lang-impl/src/com/intellij/ide/projectView/impl/ProjectViewFileNestingService.java
@@ -0,0 +1,163 @@
+/*
+ * Copyright 2000-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.ide.projectView.impl;
+
+import com.intellij.ide.projectView.ProjectViewNestingRulesProvider;
+import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.extensions.ExtensionPointName;
+import com.intellij.util.containers.SortedList;
+import com.intellij.util.xmlb.annotations.AbstractCollection;
+import com.intellij.util.xmlb.annotations.Attribute;
+import com.intellij.util.xmlb.annotations.Tag;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Holds data used by {@link NestingTreeStructureProvider} and {@link FileNestingInProjectViewDialog}.
+ */
+@State(
+  name = "ProjectViewFileNesting",
+  storages = @Storage("ui.lnf.xml")
+)
+public class ProjectViewFileNestingService implements PersistentStateComponent<ProjectViewFileNestingService.MyState> {
+  private static final Logger LOG = Logger.getInstance(ProjectViewFileNestingService.class);
+
+  private static final ExtensionPointName<ProjectViewNestingRulesProvider> EP_NAME =
+    ExtensionPointName.create("com.intellij.projectViewNestingRulesProvider");
+
+  public static final NestingRule[] DEFAULT_NESTING_RULES = loadDefaultNestingRules();
+
+  private MyState myState = new MyState();
+
+  @NotNull
+  public static ProjectViewFileNestingService getInstance() {
+    return ServiceManager.getService(ProjectViewFileNestingService.class);
+  }
+
+  @NotNull
+  private static NestingRule[] loadDefaultNestingRules() {
+    final List<NestingRule> result = new SortedList<>(Comparator.comparing(o -> o.getParentFileSuffix()));
+
+    final ProjectViewNestingRulesProvider.Consumer consumer = new ProjectViewNestingRulesProvider.Consumer() {
+      @Override
+      public void addNestingRule(@NotNull final String parentFileSuffix, @NotNull final String childFileSuffix) {
+        LOG.assertTrue(!parentFileSuffix.isEmpty() && !childFileSuffix.isEmpty(), "file suffix must not be empty");
+        LOG.assertTrue(!parentFileSuffix.equals(childFileSuffix), "parent and child suffixes must be different: " + parentFileSuffix);
+        result.add(new NestingRule(parentFileSuffix, childFileSuffix));
+      }
+    };
+
+    for (ProjectViewNestingRulesProvider provider : EP_NAME.getExtensions()) {
+      provider.addFileNestingRules(consumer);
+    }
+
+    return result.toArray(new NestingRule[0]);
+  }
+
+  @Override
+  public MyState getState() {
+    return myState;
+  }
+
+  @Override
+  public void loadState(@NotNull final MyState state) {
+    myState = state;
+  }
+
+  /**
+   * This list of rules is used for serialization and for UI.
+   * See also {@link NestingTreeStructureProvider}, it adjusts this list of rules to match its needs
+   */
+  @NotNull
+  public List<NestingRule> getRules() {
+    return myState.myRules;
+  }
+
+  public void setRules(@NotNull final List<NestingRule> rules) {
+    myState.myRules.clear();
+    myState.myRules.addAll(rules);
+  }
+
+  public static class MyState {
+    @Tag("nesting-rules")
+    @AbstractCollection(surroundWithTag = false)
+    public List<NestingRule> myRules = new SortedList<>(Comparator.comparing(o -> o.getParentFileSuffix()));
+
+    public MyState() {
+      myRules.addAll(Arrays.asList(DEFAULT_NESTING_RULES));
+    }
+  }
+
+  public static class NestingRule {
+    @NotNull private String myParentFileSuffix;
+
+    @NotNull private String myChildFileSuffix;
+
+    @SuppressWarnings("unused") // used by serializer
+    public NestingRule() {
+      this("", "");
+    }
+
+    public NestingRule(@NotNull String parentFileSuffix, @NotNull String childFileSuffix) {
+      myParentFileSuffix = parentFileSuffix;
+      myChildFileSuffix = childFileSuffix;
+    }
+
+    @NotNull
+    @Attribute("parent-file-suffix")
+    public String getParentFileSuffix() {
+      return myParentFileSuffix;
+    }
+
+    public void setParentFileSuffix(@NotNull final String parentFileSuffix) {
+      myParentFileSuffix = parentFileSuffix;
+    }
+
+    @NotNull
+    @Attribute("child-file-suffix")
+    public String getChildFileSuffix() {
+      return myChildFileSuffix;
+    }
+
+    public void setChildFileSuffix(@NotNull final String childFileSuffix) {
+      myChildFileSuffix = childFileSuffix;
+    }
+
+    @Override
+    public String toString() {
+      return myParentFileSuffix + "->" + myChildFileSuffix;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      return o instanceof NestingRule &&
+             myParentFileSuffix.equals(((NestingRule)o).myParentFileSuffix) &&
+             myChildFileSuffix.equals(((NestingRule)o).myChildFileSuffix);
+    }
+
+    @Override
+    public int hashCode() {
+      return myParentFileSuffix.hashCode() + 239 * myChildFileSuffix.hashCode();
+    }
+  }
+}
