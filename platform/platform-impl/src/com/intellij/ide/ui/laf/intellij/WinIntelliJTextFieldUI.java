14e5a933d9f03c8ef49b64a1cb2ab611dc4d6afc
Kirill Kirichenko <kirill.kirichenko@jetbrains.com>
2017-05-31 17:47:25 +0300

    IDEA-171869 Win10 LaF plugins table border and spinner editor fixes

--- platform/platform-impl/src/com/intellij/ide/ui/laf/intellij/WinIntelliJTextFieldUI.java
+++ platform/platform-impl/src/com/intellij/ide/ui/laf/intellij/WinIntelliJTextFieldUI.java
@@ -59,7 +59,8 @@ public class WinIntelliJTextFieldUI extends DarculaTextFieldUI {
   @Override
   protected void paintBackground(Graphics g) {
     JTextComponent c = getComponent();
-    if (UIUtil.getParentOfType(JComboBox.class, c) != null) return;
+    if (UIUtil.getParentOfType(JComboBox.class, c) != null ||
+        UIUtil.getParentOfType(JSpinner.class, c) != null) return;
 
     Graphics2D g2 = (Graphics2D)g.create();
     try {
