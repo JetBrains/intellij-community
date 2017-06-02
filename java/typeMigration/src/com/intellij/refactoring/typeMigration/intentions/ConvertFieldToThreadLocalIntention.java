e1ae657059a0d8f4574dff79d9cb8167a2464e9b
Anna Kozlova <anna.kozlova@jetbrains.com>
2017-06-02 16:13:35 +0300

    ensure write action not taken for clear readonly status

--- java/typeMigration/src/com/intellij/refactoring/typeMigration/intentions/ConvertFieldToThreadLocalIntention.java
+++ java/typeMigration/src/com/intellij/refactoring/typeMigration/intentions/ConvertFieldToThreadLocalIntention.java
@@ -4,6 +4,7 @@ import com.intellij.codeInsight.FileModificationService;
 import com.intellij.codeInsight.intention.LowPriorityAction;
 import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction;
 import com.intellij.lang.java.JavaLanguage;
+import com.intellij.openapi.application.WriteAction;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
@@ -73,88 +74,91 @@ public class ConvertFieldToThreadLocalIntention extends PsiElementBaseIntentionA
       elements.add(reference.getElement());
     }
     if (!FileModificationService.getInstance().preparePsiElementsForWrite(elements)) return;
-    psiField.normalizeDeclaration();
+    WriteAction.run(() -> {
+      psiField.normalizeDeclaration();
 
-    final JavaPsiFacade psiFacade = JavaPsiFacade.getInstance(project);
-    final PsiElementFactory factory = JavaPsiFacade.getElementFactory(project);
-    final PsiType fromType = psiField.getType();
+      final JavaPsiFacade psiFacade = JavaPsiFacade.getInstance(project);
+      final PsiElementFactory factory = JavaPsiFacade.getElementFactory(project);
+      final PsiType fromType = psiField.getType();
 
-    final PsiClass threadLocalClass = psiFacade.findClass(ThreadLocal.class.getName(), GlobalSearchScope.allScope(project));
-    if (threadLocalClass == null) {//show warning
-      return;
-    }
-    final Map<PsiTypeParameter, PsiType> substitutor = ContainerUtil.newHashMap();
-    final PsiTypeParameter[] typeParameters = threadLocalClass.getTypeParameters();
-    if (typeParameters.length == 1) {
-      PsiType type = fromType;
-      if (fromType instanceof PsiPrimitiveType) type = ((PsiPrimitiveType)fromType).getBoxedType(element);
-      substitutor.put(typeParameters[0], type);
-    }
-    final PsiClassType toType = factory.createType(threadLocalClass, factory.createSubstitutor(substitutor));
+      final PsiClass threadLocalClass = psiFacade.findClass(ThreadLocal.class.getName(), GlobalSearchScope.allScope(project));
+      if (threadLocalClass == null) {//show warning
+        return;
+      }
+      final Map<PsiTypeParameter, PsiType> substitutor = ContainerUtil.newHashMap();
+      final PsiTypeParameter[] typeParameters = threadLocalClass.getTypeParameters();
+      if (typeParameters.length == 1) {
+        PsiType type = fromType;
+        if (fromType instanceof PsiPrimitiveType) type = ((PsiPrimitiveType)fromType).getBoxedType(element);
+        substitutor.put(typeParameters[0], type);
+      }
+      final PsiClassType toType = factory.createType(threadLocalClass, factory.createSubstitutor(substitutor));
 
-    try {
-      final TypeMigrationRules rules = new TypeMigrationRules();
-      rules.setBoundScope(GlobalSearchScope.fileScope(element.getContainingFile()));
-      final TypeMigrationLabeler labeler = new TypeMigrationLabeler(rules, toType);
-      labeler.getMigratedUsages(false, psiField);
-      for (PsiReference reference : refs) {
-        PsiElement psiElement = reference.getElement();
-        if (psiElement instanceof PsiExpression) {
-          final PsiElement parent = psiElement.getParent();
-          if (parent instanceof PsiExpression && !(parent instanceof PsiReferenceExpression || parent instanceof PsiPolyadicExpression)) {
-            psiElement = parent;
+      try {
+        final TypeMigrationRules rules = new TypeMigrationRules();
+        rules.setBoundScope(GlobalSearchScope.fileScope(element.getContainingFile()));
+        final TypeMigrationLabeler labeler = new TypeMigrationLabeler(rules, toType);
+        labeler.getMigratedUsages(false, psiField);
+        for (PsiReference reference : refs) {
+          PsiElement psiElement = reference.getElement();
+          if (psiElement instanceof PsiExpression) {
+            final PsiElement parent = psiElement.getParent();
+            if (parent instanceof PsiExpression && !(parent instanceof PsiReferenceExpression || parent instanceof PsiPolyadicExpression)) {
+              psiElement = parent;
+            }
+            final TypeConversionDescriptor conversion =
+              ThreadLocalConversionRule.findDirectConversion(psiElement, toType, fromType, labeler);
+            if (conversion != null) {
+              TypeMigrationReplacementUtil.replaceExpression((PsiExpression)psiElement, project, conversion, new TypeEvaluator(null, null));
+            }
           }
-          final TypeConversionDescriptor conversion = ThreadLocalConversionRule.findDirectConversion(psiElement, toType, fromType, labeler);
-          if (conversion != null) {
-            TypeMigrationReplacementUtil.replaceExpression((PsiExpression)psiElement, project, conversion, new TypeEvaluator(null, null));
+        }
+
+        PsiExpression initializer = psiField.getInitializer();
+        if (initializer == null) {
+          final PsiType type = psiField.getType();
+          String initializerText = null;
+          if (PsiType.BOOLEAN.equals(type)) {
+            initializerText = "false";
+          }
+          else if (type instanceof PsiPrimitiveType) {
+            initializerText = "0";
+          }
+          if (initializerText != null) {
+            psiField.setInitializer(factory.createExpressionFromText(initializerText, psiField));
+            initializer = psiField.getInitializer();
           }
         }
-      }
+        if (initializer != null) {
+          if (initializer instanceof PsiArrayInitializerExpression) {
+            PsiExpression normalizedExpr =
+              RefactoringUtil.createNewExpressionFromArrayInitializer((PsiArrayInitializerExpression)initializer, psiField.getType());
+            initializer = (PsiExpression)initializer.replace(normalizedExpr);
+          }
+          final TypeConversionDescriptor conversion = ThreadLocalConversionRule.wrapWithNewExpression(toType, fromType, initializer);
+          TypeMigrationReplacementUtil.replaceExpression(initializer, project, conversion, new TypeEvaluator(null, null));
+          CodeStyleManager.getInstance(project).reformat(psiField);
+        }
+        else if (!assertNotNull(psiField.getModifierList()).hasModifierProperty(PsiModifier.FINAL)) {
+          final String text = "new " + PsiDiamondTypeUtil.getCollapsedType(toType, psiField) + "()";
+          final PsiExpression newInitializer = factory.createExpressionFromText(text, psiField);
+          psiField.setInitializer(newInitializer);
+        }
 
-      PsiExpression initializer = psiField.getInitializer();
-      if (initializer == null) {
-        final PsiType type = psiField.getType();
-        String initializerText = null;
-        if (PsiType.BOOLEAN.equals(type)) {
-          initializerText = "false";
-        }
-        else if (type instanceof PsiPrimitiveType) {
-          initializerText = "0";
-        }
-        if (initializerText != null) {
-          psiField.setInitializer(factory.createExpressionFromText(initializerText, psiField));
-          initializer = psiField.getInitializer();
-        }
-      }
-      if (initializer != null) {
-        if (initializer instanceof PsiArrayInitializerExpression) {
-          PsiExpression normalizedExpr =
-            RefactoringUtil.createNewExpressionFromArrayInitializer((PsiArrayInitializerExpression)initializer, psiField.getType());
-          initializer = (PsiExpression)initializer.replace(normalizedExpr);
-        }
-        final TypeConversionDescriptor conversion = ThreadLocalConversionRule.wrapWithNewExpression(toType, fromType, initializer);
-        TypeMigrationReplacementUtil.replaceExpression(initializer, project, conversion, new TypeEvaluator(null, null));
-        CodeStyleManager.getInstance(project).reformat(psiField);
-      }
-      else if (!assertNotNull(psiField.getModifierList()).hasModifierProperty(PsiModifier.FINAL)) {
-        final String text = "new " + PsiDiamondTypeUtil.getCollapsedType(toType, psiField) + "()";
-        final PsiExpression newInitializer = factory.createExpressionFromText(text, psiField);
-        psiField.setInitializer(newInitializer);
-      }
+        assertNotNull(psiField.getTypeElement()).replace(factory.createTypeElement(toType));
 
-      assertNotNull(psiField.getTypeElement()).replace(factory.createTypeElement(toType));
-
-      final PsiModifierList modifierList = assertNotNull(psiField.getModifierList());
-      modifierList.setModifierProperty(PsiModifier.FINAL, true);
-      modifierList.setModifierProperty(PsiModifier.VOLATILE, false);
-    }
-    catch (IncorrectOperationException e) {
-      LOG.error(e);
-    }
+        final PsiModifierList modifierList = assertNotNull(psiField.getModifierList());
+        modifierList.setModifierProperty(PsiModifier.FINAL, true);
+        modifierList.setModifierProperty(PsiModifier.VOLATILE, false);
+      }
+      catch (IncorrectOperationException e) {
+        LOG.error(e);
+      }
+    });
   }
 
   @Override
   public boolean startInWriteAction() {
-    return true;
+    return false;
   }
 }
