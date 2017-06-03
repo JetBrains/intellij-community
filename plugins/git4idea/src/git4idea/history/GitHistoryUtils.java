3250c8f17d1135e67d0e30eb699c082ba14a1fdb
Julia Beliaeva <julia.beliaeva@jetbrains.com>
2017-06-03 19:40:54 +0300

    [git] extract GitLogOutputSplitter class from GitHistoryUtils
    (cherry picked from commit d89f84c)

--- plugins/git4idea/src/git4idea/history/GitHistoryUtils.java
+++ plugins/git4idea/src/git4idea/history/GitHistoryUtils.java
@@ -15,7 +15,6 @@
  */
 package git4idea.history;
 
-import com.intellij.execution.process.ProcessOutputTypes;
 import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.diagnostic.Logger;
@@ -53,7 +52,6 @@ import git4idea.commands.*;
 import git4idea.config.GitVersion;
 import git4idea.config.GitVersionSpecialty;
 import git4idea.history.browser.SHAHash;
-import git4idea.i18n.GitBundle;
 import git4idea.log.GitLogProvider;
 import git4idea.log.GitRefManager;
 import org.jetbrains.annotations.NotNull;
@@ -525,7 +523,7 @@ public class GitHistoryUtils {
     handler.addParameters(parameters);
     handler.endOptions();
 
-    MyGitLineHandlerListener handlerListener = new MyGitLineHandlerListener(handler, output -> {
+    GitLogOutputSplitter handlerListener = new GitLogOutputSplitter(handler, output -> {
       List<GitLogRecord> records = parser.parse(output);
       for (GitLogRecord record : records) {
         if (record == null) continue;
@@ -850,7 +848,7 @@ public class GitHistoryUtils {
     StopWatch sw = StopWatch.start("loading details in [" + root.getName() + "]");
 
     Ref<Throwable> parseError = new Ref<>();
-    MyGitLineHandlerListener handlerListener = new MyGitLineHandlerListener(handler, output -> {
+    GitLogOutputSplitter handlerListener = new GitLogOutputSplitter(handler, output -> {
       try {
         GitLogRecord record = parser.parseOneRecord(output);
         if (record != null) {
@@ -1044,109 +1042,6 @@ public class GitHistoryUtils {
         return false;
       }
       return true;
-    }
-  }
-
-  private static class MyGitLineHandlerListener implements GitLineHandlerListener {
-    @NotNull private final GitLineHandler myHandler;
-    @NotNull private final Consumer<StringBuilder> myRecordConsumer;
-
-    @NotNull private final StringBuilder myOutput = new StringBuilder();
-    @NotNull private final StringBuilder myErrors = new StringBuilder();
-    @Nullable private VcsException myException = null;
-
-    private boolean myIsInsideBody = true;
-
-    public MyGitLineHandlerListener(@NotNull GitLineHandler handler,
-                                    @NotNull Consumer<StringBuilder> recordConsumer) {
-      myHandler = handler;
-      myRecordConsumer = recordConsumer;
-
-      myHandler.addLineListener(this);
-    }
-
-    @Override
-    public void onLineAvailable(String line, Key outputType) {
-      if (outputType == ProcessOutputTypes.STDERR) {
-        myErrors.append(line).append("\n");
-      }
-      else if (outputType == ProcessOutputTypes.STDOUT) {
-        try {
-          processOutputLine(line);
-        }
-        catch (Exception e) {
-          myException = new VcsException(e);
-        }
-      }
-    }
-
-    private void processOutputLine(@NotNull String line) {
-      // format of the record is <RECORD_START><BODY><RECORD_END><CHANGES>
-      // then next record goes
-      // (rather inconveniently, after RECORD_END there is a list of modified files)
-      // so here I'm trying to find text between two RECORD_START symbols
-      // that simultaneously contains a RECORD_END
-      // this helps to deal with commits like a929478f6720ac15d949117188cd6798b4a9c286 in linux repo that have RECORD_START symbols in the message
-      // wont help with RECORD_END symbols in the message however (have not seen those yet)
-
-      if (myIsInsideBody) {
-        // find body
-        int bodyEnd = line.indexOf(GitLogParser.RECORD_END);
-        if (bodyEnd >= 0) {
-          myIsInsideBody = false;
-          myOutput.append(line.substring(0, bodyEnd + GitLogParser.RECORD_END.length()));
-          processOutputLine(line.substring(bodyEnd + GitLogParser.RECORD_END.length()));
-        }
-        else {
-          myOutput.append(line).append("\n");
-        }
-      }
-      else {
-        int nextRecordStart = line.indexOf(GitLogParser.RECORD_START);
-        if (nextRecordStart >= 0) {
-          myOutput.append(line.substring(0, nextRecordStart));
-          myRecordConsumer.consume(myOutput);
-          myOutput.setLength(0);
-          myIsInsideBody = true;
-          processOutputLine(line.substring(nextRecordStart));
-        }
-        else {
-          myOutput.append(line).append("\n");
-        }
-      }
-    }
-
-    @Override
-    public void processTerminated(int exitCode) {
-      if (exitCode != 0) {
-        String errorMessage = myErrors.toString();
-        if (errorMessage.isEmpty()) {
-          errorMessage = GitBundle.message("git.error.exit", exitCode);
-        }
-        myException = new VcsException(errorMessage + "\nCommand line: [" + myHandler.printableCommandLine() + "]");
-      }
-      else {
-        try {
-          myRecordConsumer.consume(myOutput);
-        }
-        catch (Exception e) {
-          myException = new VcsException(e);
-        }
-      }
-    }
-
-    @Override
-    public void startFailed(Throwable exception) {
-      myException = new VcsException(exception);
-    }
-
-    public void reportErrors() throws VcsException {
-      if (myException != null) {
-        if (myException.getCause() instanceof ProcessCanceledException) {
-          throw (ProcessCanceledException)myException.getCause();
-        }
-        throw myException;
-      }
     }
   }
 }
