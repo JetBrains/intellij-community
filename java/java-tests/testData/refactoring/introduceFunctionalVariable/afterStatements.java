e81b5b73a73a626b6e5d7a553273d9a828593365
Anna Kozlova <anna.kozlova@jetbrains.com>
2017-06-07 16:57:32 +0300

    introduce functional: combine extract method & introduce
    IDEA-174011; IDEA-174021
    
    (cherry picked from commit d65ff6ad81d278c1df65af00a3d77475ad72ebaf)

--- java/java-tests/testData/refactoring/introduceFunctionalVariable/afterStatements.java
+++ java/java-tests/testData/refactoring/introduceFunctionalVariable/afterStatements.java
@@ -1,16 +1,16 @@
 import java.util.function.Consumer;
 
 class Test {
-  void foo(String s) {
+    void foo(String s) {
     if (true) {
-        Consumer<String> stringConsumer = new Consumer<String>() {
+        Consumer<String> consumer = new Consumer<String>() {
             public void accept(String s) {
                 System.out.println("Hello, world " + s);
                 System.out.println();
             }
         };
-        stringConsumer.accept(s);
-
+        consumer.accept(s);
+      
         System.out.println();
     }
   }
