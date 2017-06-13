a88c59bd151f6c6112e8a6f9ac4718e197b29727
Artem Khvastunov <Artem.Khvastunov@jetbrains.com>
2017-06-13 13:13:21 +0200

    add icons generated by icons.gant

--- plugins/tasks/tasks-core/src/icons/TasksCoreIcons.java
+++ plugins/tasks/tasks-core/src/icons/TasksCoreIcons.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package icons;
 
 import com.intellij.openapi.util.IconLoader;
@@ -28,7 +43,9 @@ public class TasksCoreIcons {
   public static final Icon Mantis = load("/icons/mantis.png"); // 16x16
   
   public static class Pivotal {
+    public static final Icon Bug = load("/icons/pivotal/bug.png"); // 16x16
     public static final Icon Chore = load("/icons/pivotal/chore.png"); // 16x16
+    public static final Icon Feature = load("/icons/pivotal/feature.png"); // 16x16
     public static final Icon Release = load("/icons/pivotal/release.png"); // 16x16
 
   }
