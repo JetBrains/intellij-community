52d732c3c348a0ec5560b8e281f875defe7a4633
Semyon Proshev <semyon.proshev@jetbrains.com>
2017-06-16 16:12:49 +0300

    Update PyCompatibilityInspection to correctly process `long` in Python 2.
    The reason of update is that `long` is described as `long = int` in pyi-stubs.
    
    (cherry picked from commit c4c6294)
    
    (cherry picked from commit 947580c)

--- python/testSrc/com/jetbrains/python/inspections/PyCompatibilityInspectionTest.java
+++ python/testSrc/com/jetbrains/python/inspections/PyCompatibilityInspectionTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -226,6 +226,10 @@ public class PyCompatibilityInspectionTest extends PyTestCase {
 
   // PY-23355
   public void testNoWarningAboutStarredExpressionsInFunctionTypeComments() {
+    doTest();
+  }
+
+  public void testBuiltinLong() {
     doTest();
   }
 
