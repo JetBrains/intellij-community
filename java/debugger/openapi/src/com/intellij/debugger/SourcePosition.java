080b7c4f760f6eb5211047e1b82d8621a3280ab9
Egor.Ushakov <egor.ushakov@jetbrains.com>
2017-06-16 17:39:25 +0300

    EA-102311 - PIEAE: PsiUtilCore.ensureValid - fix for exception

--- java/debugger/openapi/src/com/intellij/debugger/SourcePosition.java
+++ java/debugger/openapi/src/com/intellij/debugger/SourcePosition.java
@@ -55,7 +55,7 @@ public abstract class SourcePosition implements Navigatable{
 
   private abstract static class SourcePositionCache extends SourcePosition {
     @NotNull private final PsiFile myFile;
-    @NotNull private final SmartPsiElementPointer<PsiFile> myFilePointer;
+    @Nullable private final SmartPsiElementPointer<PsiFile> myFilePointer;
     private long myModificationStamp = -1L;
 
     private WeakReference<PsiElement> myPsiElementRef;
@@ -64,14 +64,15 @@ public abstract class SourcePosition implements Navigatable{
 
     public SourcePositionCache(@NotNull PsiFile file) {
       myFile = file;
-      myFilePointer = SmartPointerManager.getInstance(file.getProject()).createSmartPsiElementPointer(file);
+      myFilePointer = ReadAction.compute(
+        () -> file.isValid() ? SmartPointerManager.getInstance(file.getProject()).createSmartPsiElementPointer(file) : null);
       updateData();
     }
 
     @Override
     @NotNull
     public PsiFile getFile() {
-      PsiFile file = myFilePointer.getElement();
+      PsiFile file = myFilePointer != null ? myFilePointer.getElement() : null;
       return file != null ? file : myFile; // in case of full invalidation, rollback to the original psiFile
     }
 
