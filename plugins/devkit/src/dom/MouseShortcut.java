d743a3ce03a331544717273ee069b33cb90964e6
Yann Cébron <yann.cebron@jetbrains.com>
2017-06-19 12:04:12 +0200

    [devkit] Keyboard/MouseShortcut: fix DOM for boolean attributes

--- plugins/devkit/src/dom/MouseShortcut.java
+++ plugins/devkit/src/dom/MouseShortcut.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -53,7 +53,7 @@ public interface MouseShortcut extends DomElement {
          * @return the value of the should current shortcut be removed or not.
          */
         @NotNull
-        GenericAttributeValue<String> getRemove();
+        GenericAttributeValue<Boolean> getRemove();
 
         /**
          * Returns the value of the should all previous shortcuts be removed by that one or not.
@@ -61,5 +61,5 @@ public interface MouseShortcut extends DomElement {
          * @return the value of the should all previous shortcuts be removed by that one or not.
          */
         @NotNull
-        GenericAttributeValue<String> getReplaceAll();
+        GenericAttributeValue<Boolean> getReplaceAll();
 }
