d66f186a987c5f4505028957ca5998d923e7e206
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-06-20 18:43:00 +0300

    Fix test data for filtering operations

--- plugins/stream-debugger/testData/streamex/src/filtering/DropWhile.java
+++ plugins/stream-debugger/testData/streamex/src/filtering/DropWhile.java
@@ -5,7 +5,7 @@ import one.util.streamex.StreamEx;
 public class DropWhile {
   public static void main(String[] args) {
     // Breakpoint!
-    final long count = StreamEx.of(1, 2, 3, 4).dropWhile(x -> x < 4).count();
+    final long count = StreamEx.of(1, 2, 3, 2).dropWhile(x -> x < 3).count();
     System.out.println(count);
   }
 }
--- plugins/stream-debugger/testData/streamex/src/filtering/RemoveKeys.java
+++ plugins/stream-debugger/testData/streamex/src/filtering/RemoveKeys.java
@@ -5,7 +5,7 @@ import one.util.streamex.EntryStream;
 public class RemoveKeys {
   public static void main(String[] args) {
     // Breakpoint!
-    final long count = EntryStream.of(1, 1, 2, 4, 3, 9).removeKeys(x -> x < 7).count();
+    final long count = EntryStream.of(1, 1, 2, 4, 3, 9).removeKeys(x -> x < 3).count();
     System.out.println(count);
   }
 }
