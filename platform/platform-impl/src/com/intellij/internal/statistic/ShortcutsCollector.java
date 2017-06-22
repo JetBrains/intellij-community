22c86a0652c282db5c45db2363c34dafdbfe7b25
Konstantin Bulenkov <kb@jetbrains.com>
2017-06-22 16:22:07 +0200

    use locale independent names for keys in statistic

--- platform/platform-impl/src/com/intellij/internal/statistic/ShortcutsCollector.java
+++ platform/platform-impl/src/com/intellij/internal/statistic/ShortcutsCollector.java
@@ -20,7 +20,9 @@ import com.intellij.internal.statistic.beans.UsageDescriptor;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.KeyboardShortcut;
 import com.intellij.openapi.components.*;
+import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.SystemInfo;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.xmlb.annotations.MapAnnotation;
 import com.intellij.util.xmlb.annotations.Tag;
@@ -29,9 +31,16 @@ import org.jetbrains.annotations.NotNull;
 import javax.swing.*;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.awt.event.InputEvent.ALT_GRAPH_MASK;
+import static java.awt.event.InputEvent.ALT_MASK;
+import static java.awt.event.InputEvent.BUTTON1_MASK;
+import static java.awt.event.InputEvent.CTRL_MASK;
+import static java.awt.event.InputEvent.META_MASK;
+import static java.awt.event.InputEvent.SHIFT_MASK;
+import static java.awt.event.KeyEvent.*;
 
 /**
  * @author Konstantin Bulenkov
@@ -46,6 +55,7 @@ public class ShortcutsCollector implements PersistentStateComponent<ShortcutsCol
     @MapAnnotation(surroundWithTag = false, keyAttributeName = "shortcut", valueAttributeName = "count")
     public final Map<String, Integer> myValues = new HashMap<>();
   }
+
   private MyState myState = new MyState();
 
   @NotNull
@@ -59,7 +69,7 @@ public class ShortcutsCollector implements PersistentStateComponent<ShortcutsCol
 
   public static void record(AnActionEvent event) {
     _record(event, false);
-   }
+  }
 
   public static void recordDoubleShortcut(AnActionEvent event) {
     _record(event, true);
@@ -73,7 +83,7 @@ public class ShortcutsCollector implements PersistentStateComponent<ShortcutsCol
       //Stat server doesn't support UFT-8
       String key = getShortcutText(shortcut);
       if (isDoubleShortcut) {
-        key = SystemInfo.isMac ? key + key : key + "+" + key;
+        key = key + "+" + key;
       }
       incValue(key);
     }
@@ -83,13 +93,13 @@ public class ShortcutsCollector implements PersistentStateComponent<ShortcutsCol
     String results = "";
     int modifiers = shortcut.getFirstKeyStroke().getModifiers();
     if (modifiers > 0) {
-      final String keyModifiersText = KeyEvent.getKeyModifiersText(modifiers);
+      final String keyModifiersText = getLocaleUnawareKeyModifiersText(modifiers);
       if (!keyModifiersText.isEmpty()) {
         results = keyModifiersText + "+";
       }
     }
 
-    results += KeyEvent.getKeyText(shortcut.getFirstKeyStroke().getKeyCode());
+    results += getLocaleUnawareKeyText(shortcut.getFirstKeyStroke().getKeyCode());
     return results;
   }
 
@@ -120,7 +130,6 @@ public class ShortcutsCollector implements PersistentStateComponent<ShortcutsCol
     @NotNull
     public Set<UsageDescriptor> getUsages() {
       MyState state = getInstance().getState();
-      assert state != null;
       return ContainerUtil.map2Set(state.myValues.entrySet(), e -> new UsageDescriptor(e.getKey(), e.getValue()));
     }
 
@@ -128,5 +137,237 @@ public class ShortcutsCollector implements PersistentStateComponent<ShortcutsCol
     public GroupDescriptor getGroupId() {
       return GROUP;
     }
+  }
+
+  private static String getLocaleUnawareKeyText(int keyCode) {
+    if (keyCode >= VK_0 && keyCode <= VK_9 ||
+        keyCode >= VK_A && keyCode <= VK_Z) {
+      return String.valueOf((char)keyCode);
+    }
+
+    String knownKeyCode = ourKeyCodes.get(keyCode);
+    if (knownKeyCode != null) return knownKeyCode;
+
+    if (keyCode >= VK_NUMPAD0 && keyCode <= VK_NUMPAD9) {
+      char c = (char)(keyCode - VK_NUMPAD0 + '0');
+      return "NumPad-" + c;
+    }
+
+    if ((keyCode & 0x01000000) != 0) {
+      return String.valueOf((char)(keyCode ^ 0x01000000));
+    }
+
+    return "Unknown keyCode: 0x" + Integer.toString(keyCode, 16);
+  }
+
+  private static final List<Pair<Integer, String>> ourModifiers = new ArrayList<>(6);
+  static {
+    ourModifiers.add(Pair.create(META_MASK, "Meta"));
+    ourModifiers.add(Pair.create(CTRL_MASK, "Ctrl"));
+    ourModifiers.add(Pair.create(ALT_MASK, "Alt"));
+    ourModifiers.add(Pair.create(SHIFT_MASK, "Shift"));
+    ourModifiers.add(Pair.create(ALT_GRAPH_MASK, "Alt Graph"));
+    ourModifiers.add(Pair.create(BUTTON1_MASK, "Button1"));
+  }
+
+  private static String getLocaleUnawareKeyModifiersText(int modifiers) {
+    List<String> pressed = ourModifiers.stream()
+      .filter(p -> (p.first & modifiers) != 0)
+      .map(p -> p.second).collect(Collectors.toList());
+    return StringUtil.join(pressed, "+");
+  }
+
+  private static final Map<Integer, String> ourKeyCodes = new HashMap<>();
+
+  static {
+    ourKeyCodes.put(VK_ESZETT, "\u00DF");
+    ourKeyCodes.put(VK_A_WITH_GRAVE, "\u00e0");
+    ourKeyCodes.put(VK_A_WITH_ACUTE, "\u00e1");
+    ourKeyCodes.put(VK_A_WITH_CIRCUMFLEX, "\u00e2");
+    ourKeyCodes.put(VK_A_WITH_TILDE, "\u00e3");
+    ourKeyCodes.put(VK_A_WITH_DIAERESIS, "\u00e4");
+    ourKeyCodes.put(VK_A_WITH_RING_ABOVE, "\u00e5");
+    ourKeyCodes.put(VK_AE, "\u00e6");
+    ourKeyCodes.put(VK_C_WITH_CEDILLA, "\u00e7");
+    ourKeyCodes.put(VK_E_WITH_GRAVE, "\u00e8");
+    ourKeyCodes.put(VK_E_WITH_ACUTE, "\u00e9");
+    ourKeyCodes.put(VK_E_WITH_CIRCUMFLEX, "\u00ea");
+    ourKeyCodes.put(VK_E_WITH_DIAERESIS, "\u00eb");
+    ourKeyCodes.put(VK_I_WITH_GRAVE, "\u00ec");
+    ourKeyCodes.put(VK_I_WITH_ACUTE, "\u00ed");
+    ourKeyCodes.put(VK_I_WITH_CIRCUMFLEX, "\u00ee");
+    ourKeyCodes.put(VK_I_WITH_DIAERESIS, "\u00ef");
+    ourKeyCodes.put(VK_ETH, "\u00f0");
+    ourKeyCodes.put(VK_N_WITH_TILDE, "\u00f1");
+    ourKeyCodes.put(VK_O_WITH_GRAVE, "\u00f2");
+    ourKeyCodes.put(VK_O_WITH_ACUTE, "\u00f3");
+    ourKeyCodes.put(VK_O_WITH_CIRCUMFLEX, "\u00f4");
+    ourKeyCodes.put(VK_O_WITH_TILDE, "\u00f5");
+    ourKeyCodes.put(VK_O_WITH_DIAERESIS, "\u00f6");
+    ourKeyCodes.put(VK_DIVISION_SIGN, "\u00f7");
+    ourKeyCodes.put(VK_O_WITH_SLASH, "\u00f8");
+    ourKeyCodes.put(VK_U_WITH_GRAVE, "\u00f9");
+    ourKeyCodes.put(VK_U_WITH_ACUTE, "\u00fa");
+    ourKeyCodes.put(VK_U_WITH_CIRCUMFLEX, "\u00fb");
+    ourKeyCodes.put(VK_U_WITH_DIAERESIS, "\u00fc");
+    ourKeyCodes.put(VK_Y_WITH_ACUTE, "\u00fd");
+    ourKeyCodes.put(VK_THORN, "\u00fe");
+    ourKeyCodes.put(VK_Y_WITH_DIAERESIS, "\u00ff");
+
+    ourKeyCodes.put(VK_ENTER, "Enter");
+    ourKeyCodes.put(VK_BACK_SPACE, "Backspace");
+    ourKeyCodes.put(VK_TAB, "Tab");
+    ourKeyCodes.put(VK_CANCEL, "Cancel");
+    ourKeyCodes.put(VK_CLEAR, "Clear");
+    ourKeyCodes.put(VK_COMPOSE, "Compose");
+    ourKeyCodes.put(VK_PAUSE, "Pause");
+    ourKeyCodes.put(VK_CAPS_LOCK, "Caps Lock");
+    ourKeyCodes.put(VK_ESCAPE, "Escape");
+    ourKeyCodes.put(VK_SPACE, "Space");
+    ourKeyCodes.put(VK_PAGE_UP, "Page Up");
+    ourKeyCodes.put(VK_PAGE_DOWN, "Page Down");
+    ourKeyCodes.put(VK_END, "End");
+    ourKeyCodes.put(VK_HOME, "Home");
+    ourKeyCodes.put(VK_LEFT, "Left");
+    ourKeyCodes.put(VK_UP, "Up");
+    ourKeyCodes.put(VK_RIGHT, "Right");
+    ourKeyCodes.put(VK_DOWN, "Down");
+    ourKeyCodes.put(VK_BEGIN, "Begin");
+
+    // modifiers
+    ourKeyCodes.put(VK_SHIFT, "Shift");
+    ourKeyCodes.put(VK_CONTROL, "Control");
+    ourKeyCodes.put(VK_ALT, "Alt");
+    ourKeyCodes.put(VK_META, "Meta");
+    ourKeyCodes.put(VK_ALT_GRAPH, "Alt Graph");
+
+    // punctuation
+    ourKeyCodes.put(VK_COMMA, "Comma");
+    ourKeyCodes.put(VK_PERIOD, "Period");
+    ourKeyCodes.put(VK_SLASH, "Slash");
+    ourKeyCodes.put(VK_SEMICOLON, "Semicolon");
+    ourKeyCodes.put(VK_EQUALS, "Equals");
+    ourKeyCodes.put(VK_OPEN_BRACKET, "Open Bracket");
+    ourKeyCodes.put(VK_BACK_SLASH, "Back Slash");
+    ourKeyCodes.put(VK_CLOSE_BRACKET, "Close Bracket");
+
+    // numpad numeric keys handled below
+    ourKeyCodes.put(VK_MULTIPLY, "NumPad *");
+    ourKeyCodes.put(VK_ADD, "NumPad +");
+    ourKeyCodes.put(VK_SEPARATOR, "NumPad ,");
+    ourKeyCodes.put(VK_SUBTRACT, "NumPad -");
+    ourKeyCodes.put(VK_DECIMAL, "NumPad .");
+    ourKeyCodes.put(VK_DIVIDE, "NumPad /");
+    ourKeyCodes.put(VK_DELETE, "Delete");
+    ourKeyCodes.put(VK_NUM_LOCK, "Num Lock");
+    ourKeyCodes.put(VK_SCROLL_LOCK, "Scroll Lock");
+
+    ourKeyCodes.put(VK_WINDOWS, "Windows");
+    ourKeyCodes.put(VK_CONTEXT_MENU, "Context Menu");
+
+    ourKeyCodes.put(VK_F1, "F1");
+    ourKeyCodes.put(VK_F2, "F2");
+    ourKeyCodes.put(VK_F3, "F3");
+    ourKeyCodes.put(VK_F4, "F4");
+    ourKeyCodes.put(VK_F5, "F5");
+    ourKeyCodes.put(VK_F6, "F6");
+    ourKeyCodes.put(VK_F7, "F7");
+    ourKeyCodes.put(VK_F8, "F8");
+    ourKeyCodes.put(VK_F9, "F9");
+    ourKeyCodes.put(VK_F10, "F10");
+    ourKeyCodes.put(VK_F11, "F11");
+    ourKeyCodes.put(VK_F12, "F12");
+    ourKeyCodes.put(VK_F13, "F13");
+    ourKeyCodes.put(VK_F14, "F14");
+    ourKeyCodes.put(VK_F15, "F15");
+    ourKeyCodes.put(VK_F16, "F16");
+    ourKeyCodes.put(VK_F17, "F17");
+    ourKeyCodes.put(VK_F18, "F18");
+    ourKeyCodes.put(VK_F19, "F19");
+    ourKeyCodes.put(VK_F20, "F20");
+    ourKeyCodes.put(VK_F21, "F21");
+    ourKeyCodes.put(VK_F22, "F22");
+    ourKeyCodes.put(VK_F23, "F23");
+    ourKeyCodes.put(VK_F24, "F24");
+
+    ourKeyCodes.put(VK_PRINTSCREEN, "Print Screen");
+    ourKeyCodes.put(VK_INSERT, "Insert");
+    ourKeyCodes.put(VK_HELP, "Help");
+    ourKeyCodes.put(VK_BACK_QUOTE, "Back Quote");
+    ourKeyCodes.put(VK_QUOTE, "Quote");
+
+    ourKeyCodes.put(VK_KP_UP, "Up");
+    ourKeyCodes.put(VK_KP_DOWN, "Down");
+    ourKeyCodes.put(VK_KP_LEFT, "Left");
+    ourKeyCodes.put(VK_KP_RIGHT, "Right");
+
+    ourKeyCodes.put(VK_DEAD_GRAVE, "Dead Grave");
+    ourKeyCodes.put(VK_DEAD_ACUTE, "Dead Acute");
+    ourKeyCodes.put(VK_DEAD_CIRCUMFLEX, "Dead Circumflex");
+    ourKeyCodes.put(VK_DEAD_TILDE, "Dead Tilde");
+    ourKeyCodes.put(VK_DEAD_MACRON, "Dead Macron");
+    ourKeyCodes.put(VK_DEAD_BREVE, "Dead Breve");
+    ourKeyCodes.put(VK_DEAD_ABOVEDOT, "Dead Above Dot");
+    ourKeyCodes.put(VK_DEAD_DIAERESIS, "Dead Diaeresis");
+    ourKeyCodes.put(VK_DEAD_ABOVERING, "Dead Above Ring");
+    ourKeyCodes.put(VK_DEAD_DOUBLEACUTE, "Dead Double Acute");
+    ourKeyCodes.put(VK_DEAD_CARON, "Dead Caron");
+    ourKeyCodes.put(VK_DEAD_CEDILLA, "Dead Cedilla");
+    ourKeyCodes.put(VK_DEAD_OGONEK, "Dead Ogonek");
+    ourKeyCodes.put(VK_DEAD_IOTA, "Dead Iota");
+    ourKeyCodes.put(VK_DEAD_VOICED_SOUND, "Dead Voiced Sound");
+    ourKeyCodes.put(VK_DEAD_SEMIVOICED_SOUND, "Dead Semivoiced Sound");
+
+    ourKeyCodes.put(VK_AMPERSAND, "Ampersand");
+    ourKeyCodes.put(VK_ASTERISK, "Asterisk");
+    ourKeyCodes.put(VK_QUOTEDBL, "Double Quote");
+    ourKeyCodes.put(VK_LESS, "Less");
+    ourKeyCodes.put(VK_GREATER, "Greater");
+    ourKeyCodes.put(VK_BRACELEFT, "Left Brace");
+    ourKeyCodes.put(VK_BRACERIGHT, "Right Brace");
+    ourKeyCodes.put(VK_AT, "At");
+    ourKeyCodes.put(VK_COLON, "Colon");
+    ourKeyCodes.put(VK_CIRCUMFLEX, "Circumflex");
+    ourKeyCodes.put(VK_DOLLAR, "Dollar");
+    ourKeyCodes.put(VK_EURO_SIGN, "Euro");
+    ourKeyCodes.put(VK_EXCLAMATION_MARK, "Exclamation Mark");
+    ourKeyCodes.put(VK_INVERTED_EXCLAMATION_MARK, "Inverted Exclamation Mark");
+    ourKeyCodes.put(VK_LEFT_PARENTHESIS, "Left Parenthesis");
+    ourKeyCodes.put(VK_NUMBER_SIGN, "Number Sign");
+    ourKeyCodes.put(VK_MINUS, "Minus");
+    ourKeyCodes.put(VK_PLUS, "Plus");
+    ourKeyCodes.put(VK_RIGHT_PARENTHESIS, "Right Parenthesis");
+    ourKeyCodes.put(VK_UNDERSCORE, "Underscore");
+
+    ourKeyCodes.put(VK_FINAL, "Final");
+    ourKeyCodes.put(VK_CONVERT, "Convert");
+    ourKeyCodes.put(VK_NONCONVERT, "No Convert");
+    ourKeyCodes.put(VK_ACCEPT, "Accept");
+    ourKeyCodes.put(VK_MODECHANGE, "Mode Change");
+    ourKeyCodes.put(VK_KANA, "Kana");
+    ourKeyCodes.put(VK_KANJI, "Kanji");
+    ourKeyCodes.put(VK_ALPHANUMERIC, "Alphanumeric");
+    ourKeyCodes.put(VK_KATAKANA, "Katakana");
+    ourKeyCodes.put(VK_HIRAGANA, "Hiragana");
+    ourKeyCodes.put(VK_FULL_WIDTH, "Full-Width");
+    ourKeyCodes.put(VK_HALF_WIDTH, "Half-Width");
+    ourKeyCodes.put(VK_ROMAN_CHARACTERS, "Roman Characters");
+    ourKeyCodes.put(VK_ALL_CANDIDATES, "All Candidates");
+    ourKeyCodes.put(VK_PREVIOUS_CANDIDATE, "Previous Candidate");
+    ourKeyCodes.put(VK_CODE_INPUT, "Code Input");
+    ourKeyCodes.put(VK_JAPANESE_KATAKANA, "Japanese Katakana");
+    ourKeyCodes.put(VK_JAPANESE_HIRAGANA, "Japanese Hiragana");
+    ourKeyCodes.put(VK_JAPANESE_ROMAN, "Japanese Roman");
+    ourKeyCodes.put(VK_KANA_LOCK, "Kana Lock");
+    ourKeyCodes.put(VK_INPUT_METHOD_ON_OFF, "Input Method On/Off");
+
+    ourKeyCodes.put(VK_AGAIN, "Again");
+    ourKeyCodes.put(VK_UNDO, "Undo");
+    ourKeyCodes.put(VK_COPY, "Copy");
+    ourKeyCodes.put(VK_PASTE, "Paste");
+    ourKeyCodes.put(VK_CUT, "Cut");
+    ourKeyCodes.put(VK_FIND, "Find");
+    ourKeyCodes.put(VK_PROPS, "Props");
+    ourKeyCodes.put(VK_STOP, "Stop");
   }
 }
