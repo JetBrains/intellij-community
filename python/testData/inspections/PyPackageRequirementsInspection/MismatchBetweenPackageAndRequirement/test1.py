d574c0ad9a2cf51fee80034cbc50ff8f7c6caf74
Semyon Proshev <semyon.proshev@jetbrains.com>
2017-06-22 19:21:40 +0300

    PY-11963 Fixed: Package requirements inspection warning for Python packages with names that don't match PyPI packages
    Update PyPackageRequirementsInspection to honor PyPIPackageUtil.PACKAGES_TOPLEVEL and use this data as possible package name
    
    (cherry picked from commit 7088d9d)

---
+++ python/testData/inspections/PyPackageRequirementsInspection/MismatchBetweenPackageAndRequirement/test1.py
@@ -0,0 +1,16 @@
+<warning descr="Package requirements 'factory-boy', 'python-magic', 'attrs', 'python-jenkins', 'python-dateutil', 'PyYAML', 'beautifulsoup4', 'scikit-learn', 'future', 'django-pipeline', 'django_jsonfield', 'django-simple-captcha', 'Pillow', 'pyzmq', 'robotframework', 'opencv-python' are not satisfied">import factory
+import magic
+import attr
+import jenkins
+import dateutil
+import yaml
+import bs4
+import sklearn
+import past
+import pipeline
+import jsonfield
+import captcha
+import PIL
+import zmq
+import robot
+import cv2</warning>
