cba2ff917beaf3712206f36c7ceaf4759b1c8315
Anna Kozlova <anna.kozlova@jetbrains.com>
2017-06-22 12:09:25 +0300

    take into account container class type parameters
    when non-generic inner class is located inside generic outer, inference variables could be hidden in parent parameters (IDEA-174762; IDEA-174776)

--- java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/lambda/GenericsHighlighting8Test.java
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/lambda/GenericsHighlighting8Test.java
@@ -1013,6 +1013,8 @@ public class GenericsHighlighting8Test extends LightDaemonAnalyzerTestCase {
     doTest();
   }
 
+  public void testNonGenericInnerOfGenericOuter() { doTest(); }
+
   public void testTypeParameterBoundsWithSubstitutionWhenMethodHierarchyIsChecked() throws Exception {
     doTest();
   }
