886ae4ae2372b2dde0057764ab0a0968aff134b9
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2017-06-23 11:47:53 +0200

    UAST value analysis: more accurate merging of call results
    Same calls with different arguments are merged to calls
    with undetermined arguments. Better equals / hashCode.
    
    Otherwise we can waste a lot of time in attempt to merge
    calls of the same function with changing arguments
    
    Related to IDEA-174594
    
    (cherry picked from commit 3d12aba)

--- uast/uast-common/src/org/jetbrains/uast/values/UCallResultValue.kt
+++ uast/uast-common/src/org/jetbrains/uast/values/UCallResultValue.kt
@@ -21,9 +21,27 @@ import org.jetbrains.uast.UResolvable
 // Value of something resolvable (e.g. call or property access)
 // that we cannot or do not want to evaluate
 class UCallResultValue(val resolvable: UResolvable, val arguments: List<UValue>) : UValueBase(), UDependency {
-    override fun equals(other: Any?) = other is UCallResultValue && resolvable == other.resolvable && arguments == other.arguments
 
-    override fun hashCode() = resolvable.hashCode()
+    private val argumentsHashCode = arguments.hashCode()
+
+    override fun merge(other: UValue): UValue = when (other) {
+        this -> this
+        is UCallResultValue -> {
+            if (resolvable == other.resolvable) {
+                UCallResultValue(resolvable, arguments.map { UUndeterminedValue })
+            }
+            else {
+                UPhiValue.create(this, other)
+            }
+        }
+        else -> UPhiValue.create(this, other)
+    }
+
+    override fun equals(other: Any?) =
+        other is UCallResultValue && resolvable == other.resolvable &&
+        argumentsHashCode == other.argumentsHashCode && arguments == other.arguments
+
+    override fun hashCode() = resolvable.hashCode() * 19 + argumentsHashCode
 
     override fun toString(): String {
         return "external ${(resolvable as? UElement)?.asRenderString() ?: "???"}(${arguments.joinToString()})"
