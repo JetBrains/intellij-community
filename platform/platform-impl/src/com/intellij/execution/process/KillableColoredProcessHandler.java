24d3247b98355139649fcba74adc7a360cb40dd8
vadim.lomshakov <vadim.lomshakov@jetbrains.com>
2017-06-24 00:52:43 +0700

    [rider]: patched runnerw.exe mediator to allow create process with new visible console (#RIDER-4160)

--- platform/platform-impl/src/com/intellij/execution/process/KillableColoredProcessHandler.java
+++ platform/platform-impl/src/com/intellij/execution/process/KillableColoredProcessHandler.java
@@ -35,7 +35,7 @@ public class KillableColoredProcessHandler extends ColoredProcessHandler impleme
    * Starts a process with a {@link RunnerMediator mediator} when {@code withMediator} is set to {@code true} and the platform is Windows.
    */
   public KillableColoredProcessHandler(@NotNull GeneralCommandLine commandLine, boolean withMediator) throws ExecutionException {
-    super(mediate(commandLine, withMediator));
+    super(mediate(commandLine, withMediator, false));
     setShouldKillProcessSoftly(true);
   }
 
