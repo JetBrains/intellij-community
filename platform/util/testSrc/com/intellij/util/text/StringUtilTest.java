51417d8f6d69f1f3bf0d5b3c2c40ab4bc4e96e15
Gregory.Shrago <gregory.shrago@jetbrains.com>
2017-06-23 16:08:49 +0300

    unpluralize: databases -> database
    (cherry picked from commit 524ed40af94a69c5b997c68d4ccbcad40a6d6064)

--- platform/util/testSrc/com/intellij/util/text/StringUtilTest.java
+++ platform/util/testSrc/com/intellij/util/text/StringUtilTest.java
@@ -130,6 +130,8 @@ public class StringUtilTest {
     assertEquals("process", StringUtil.unpluralize("process"));
     assertEquals("PROPERTY", StringUtil.unpluralize("PROPERTIES"));
     assertEquals("THIS", StringUtil.unpluralize("THESE"));
+    assertEquals("database", StringUtil.unpluralize("databases"));
+    assertEquals("basis", StringUtil.unpluralize("bases"));
   }
 
   @Test
@@ -152,6 +154,9 @@ public class StringUtilTest {
     assertEquals("schemas", StringUtil.pluralize("schema")); // anglicized version
     assertEquals("PROPERTIES", StringUtil.pluralize("PROPERTY"));
     assertEquals("THESE", StringUtil.pluralize("THIS"));
+    assertEquals("databases", StringUtil.pluralize("database"));
+    assertEquals("bases", StringUtil.pluralize("base"));
+    assertEquals("bases", StringUtil.pluralize("basis"));
   }
 
   @Test
