a22d49408eae8d84ee612e39ec2bb132c86eaf57
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2017-06-23 14:02:34 +0200

    [gui-test] Refactor and greenify ContextChecker

--- platform/testGuiFramework/src/com/intellij/testGuiFramework/recorder/ContextChecker.kt
+++ platform/testGuiFramework/src/com/intellij/testGuiFramework/recorder/ContextChecker.kt
@@ -16,10 +16,6 @@
 
 package com.intellij.testGuiFramework.recorder
 
-import com.intellij.openapi.wm.impl.IdeFrameImpl
-import com.intellij.openapi.wm.impl.ToolWindowImpl
-import com.intellij.openapi.wm.impl.ToolWindowManagerImpl
-import com.intellij.openapi.wm.impl.WindowManagerImpl
 import com.intellij.testGuiFramework.generators.*
 import java.awt.Component
 import java.awt.Point
@@ -38,139 +34,106 @@ object ContextChecker {
   private val writer: (String) -> Unit = { code -> ScriptGenerator.addToScript(code) }
   private val contextTree: ContextTree = ContextTree(writer)
 
-  private fun getGlobalApplicableContext(component: Component, me: MouseEvent, cp: Point): Context? {
-    val applicableContextGenerator = globalContextGenerators.filter { generator -> generator.accept(component) }.sortedByDescending(
-      ContextCodeGenerator<*>::priority).firstOrNull() ?: return null
-    return applicableContextGenerator.buildContext(component, me, cp)
+  fun getContextDepth(): Int = contextTree.getSize()
+  fun clearContext() = contextTree.clear()
+
+  fun checkContext(component: Component, me: MouseEvent, cp: Point) {
+    val globalContext = getGlobalApplicableContext(component, me, cp)
+    val localContextList: List<Context> = getLocalApplicableContext(component, me, cp)
+    contextTree.checkAliveContexts(me)
+    if (globalContext != null) contextTree.addContext(globalContext)
+    if (!localContextList.isNullOrEmpty()) contextTree.addContexts(localContextList)
   }
+
+  private fun List<*>?.isNullOrEmpty(): Boolean = this == null || this.isEmpty()
+
+  private fun getGlobalApplicableContext(component: Component, me: MouseEvent, cp: Point): Context? =
+    globalContextGenerators.filter { generator -> generator.accept(component) }.sortedByDescending(
+      ContextCodeGenerator<*>::priority).firstOrNull()?.buildContext(component, me, cp)
+
 
   private fun getLocalApplicableContext(component: Component, me: MouseEvent, cp: Point): List<Context> =
     localContextGenerators.filter { generator -> generator.accept(component) }
       .sortedBy(ContextCodeGenerator<*>::priority)
       .map { applicableGenerator -> applicableGenerator.buildContext(component, me, cp) }
+}
 
-  private fun Component.containsLocationOnScreen(locationOnScreen: Point): Boolean {
-    val rectangle = this.bounds
-    rectangle.location = this.locationOnScreen
-    return rectangle.contains(locationOnScreen)
+private class ContextTree(val writeFun: (String) -> Unit) {
+  private val myContextsTree: ArrayList<Context> = ArrayList()
+  private var lastContext: Context? = null
+
+  fun checkAliveContexts(mouseEvent: MouseEvent) {
+    for (i in 0..myContextsTree.lastIndex) {
+      if (!myContextsTree[i].isAlive(mouseEvent)) {
+        // from i to myContextsTree.lastIndex contexts should be dropped
+        while (myContextsTree.lastIndex >= i) removeLastContext()
+        break
+      }
+    }
   }
 
-  private fun toolwindowCheck(component: Component, me: MouseEvent, cp: Point) {
-    if (WindowManagerImpl.getInstance().findVisibleFrame() !is IdeFrameImpl) return
-    val ideFrame = WindowManagerImpl.getInstance().findVisibleFrame() as IdeFrameImpl
-    if (ideFrame.project == null) return
-    val toolWindowManager = ToolWindowManagerImpl.getInstance(ideFrame.project!!)
-    val visibleToolWindows = toolWindowManager.toolWindowIds
-      .map { toolWindowId -> toolWindowManager.getToolWindow(toolWindowId) }
-      .filter { toolwindow -> toolwindow.isVisible }
-    println("Active toolwindows: $visibleToolWindows")
-    val toolwindow: ToolWindowImpl = visibleToolWindows
-                                       .filterIsInstance<ToolWindowImpl>()
-                                       .find { it.component.containsLocationOnScreen(me.locationOnScreen) } ?: return
-    println("Toolwindow with id(\"${(toolwindow.id)}\" hosts a cursor)")
+  fun addContext(context: Context) {
+    if (context.inContextTree()) return //do nothing if the context is the same
+    lastContext = context
+    writeFun(context.code)
+    myContextsTree.add(context)
   }
 
+  fun getSize() = myContextsTree.size
 
-  fun getContextDepth(): Int = contextTree.getSize()
-
-  fun clearContext() {
-    contextTree.clear()
+  fun clear() {
+    lastContext = null
+    myContextsTree.clear()
   }
 
-  private fun List<*>?.isNullOrEmpty(): Boolean = this == null || this.isEmpty()
-
-  fun checkContext(component: Component, me: MouseEvent, cp: Point) {
-//        toolwindowCheck(component, me, cp)
-    val globalContext = getGlobalApplicableContext(component, me, cp)
-    val localContextList: List<Context> = getLocalApplicableContext(component, me, cp)
-    contextTree.checkAliveContexts(me)
-    if (globalContext != null) contextTree.addContext(globalContext, me)
-    if (!localContextList.isNullOrEmpty()) contextTree.addContexts(localContextList, me)
+  fun addContexts(localContextList: List<Context>) {
+    localContextList.forEach { addContext(it) }
   }
 
-
-  private class ContextTree(val writeFun: (String) -> Unit) {
-
-    private val myContextsTree: ArrayList<Context> = ArrayList()
-    private var lastContext: Context? = null
-
-    private fun Context.inContextTree(): Boolean =
-      this@ContextTree.myContextsTree
-        .any { context ->
-          when (this.originalGenerator) {
-            is GlobalContextCodeGenerator -> {
-              context.originalGenerator is GlobalContextCodeGenerator<*> && this.component == context.component
-            }
-            is LocalContextCodeGenerator -> {
-              context.originalGenerator is LocalContextCodeGenerator<*> && this.component == context.component
-            }
-            else -> throw UnsupportedOperationException("Error: Unidentified context generator type!")
+  private fun Context.inContextTree(): Boolean =
+    this@ContextTree.myContextsTree
+      .any { context ->
+        when (this.originalGenerator) {
+          is GlobalContextCodeGenerator -> {
+            context.originalGenerator is GlobalContextCodeGenerator<*> && this.component == context.component
           }
-        }
-
-    private fun getLastGlobalContext(): Context? = myContextsTree.filter { context -> context.originalGenerator is GlobalContextCodeGenerator }.lastOrNull()
-
-    private fun removeLastContext() {
-      if (myContextsTree.isEmpty()) throw Exception("Error: unable to remove context from empty context tree")
-      if (myContextsTree.size == 1) {
-        myContextsTree.clear()
-        lastContext!!.originalGenerator.closeContext()
-        lastContext = null
-      }
-      else {
-        myContextsTree.removeAt(myContextsTree.lastIndex)
-        lastContext!!.originalGenerator.closeContext()
-        lastContext = myContextsTree.elementAt(myContextsTree.lastIndex)
-      }
-      writeFun("}")
-    }
-
-    fun checkAliveContexts(me: MouseEvent) {
-      for (i in (0..myContextsTree.lastIndex)) {
-        if (!myContextsTree.get(i).isAlive(me)) {
-          // from i to myContextsTree.lastIndex contexts should be dropped
-          while (myContextsTree.lastIndex >= i) removeLastContext()
-          break
+          is LocalContextCodeGenerator -> {
+            context.originalGenerator is LocalContextCodeGenerator<*> && this.component == context.component
+          }
+          else -> throw UnsupportedOperationException("Error: Unidentified context generator type!")
         }
       }
-    }
 
-    private fun Context.isAlive(me: MouseEvent): Boolean {
-      when (this.originalGenerator) {
-        is GlobalContextCodeGenerator -> {
-          return (this.component.isShowing && this.component.isEnabled)
-        }
-        is LocalContextCodeGenerator -> {
-          if (!(this.component.isEnabled && this.component.isShowing)) return false
-          val locationOnScreen = this.component.locationOnScreen
-          val visRect = (this.component as JComponent).visibleRect
-          visRect.location = locationOnScreen
-          return (visRect.contains(me.locationOnScreen))
-        }
-        else -> throw UnsupportedOperationException("Error: Unidentified context generator type!")
-      }
-    }
-
-    fun addContext(context: Context, me: MouseEvent) {
-      if (context.inContextTree()) return //do nothing if the context is the same
-      lastContext = context
-      writeFun(context.code)
-      myContextsTree.add(context)
-    }
-
-    fun getSize() = myContextsTree.size
-
-    fun clear() {
-      lastContext = null
+  private fun removeLastContext() {
+    if (myContextsTree.isEmpty()) throw Exception("Error: unable to remove context from empty context tree")
+    if (myContextsTree.size == 1) {
       myContextsTree.clear()
+      lastContext?.originalGenerator?.closeContext()
+      lastContext = null
     }
-
-    fun addContexts(localContextList: List<Context>, me: MouseEvent) {
-      localContextList.forEach { addContext(it, me) }
+    else {
+      myContextsTree.removeAt(myContextsTree.lastIndex)
+      lastContext?.originalGenerator?.closeContext()
+      lastContext = myContextsTree.elementAt(myContextsTree.lastIndex)
     }
-
+    writeFun("}")
   }
 
+  private fun Context.isAlive(mouseEvent: MouseEvent): Boolean {
+    when (this.originalGenerator) {
+      is GlobalContextCodeGenerator -> {
+        return (this.component.isShowing && this.component.isEnabled)
+      }
+      is LocalContextCodeGenerator -> {
+        if (!this.component.isEnabled || !this.component.isShowing) return false
+        val locationOnScreen = this.component.locationOnScreen
+        val visibleRect = (this.component as JComponent).visibleRect
+        visibleRect.location = locationOnScreen
+        return (visibleRect.contains(mouseEvent.locationOnScreen))
+      }
+      else -> throw UnsupportedOperationException("Error: Unidentified context generator type!")
+    }
+  }
 }
 
 
