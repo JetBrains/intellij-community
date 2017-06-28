51077f5a8aa4b8f5fbb9ddfd6d16e34588fa5906
Nicolay Mitropolsky <nicolay.mitropolsky@jetbrains.com>
2017-06-28 11:51:08 +0300

    `MakeVoidQuickFix` opens one window to warn about unmodifiable files

--- java/java-impl/src/com/intellij/codeInspection/MakeVoidQuickFix.java
+++ java/java-impl/src/com/intellij/codeInspection/MakeVoidQuickFix.java
@@ -20,6 +20,7 @@ import com.intellij.codeInspection.reference.RefElement;
 import com.intellij.codeInspection.reference.RefMethod;
 import com.intellij.openapi.application.WriteAction;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
 import com.intellij.psi.search.searches.OverridingMethodsSearch;
@@ -28,6 +29,7 @@ import com.intellij.psi.util.PsiUtil;
 import com.intellij.refactoring.changeSignature.ChangeSignatureProcessor;
 import com.intellij.refactoring.changeSignature.ParameterInfoImpl;
 import com.intellij.util.IncorrectOperationException;
+import com.intellij.util.SmartList;
 import com.siyeh.ig.controlflow.UnnecessaryReturnInspection;
 import com.siyeh.ig.psiutils.BlockUtils;
 import com.siyeh.ig.psiutils.SideEffectChecker;
@@ -80,9 +82,15 @@ public class MakeVoidQuickFix implements LocalQuickFix {
   }
 
   private static void makeMethodHierarchyVoid(Project project, @NotNull PsiMethod psiMethod) {
-    replaceReturnStatements(psiMethod);
-    for (final PsiMethod oMethod : OverridingMethodsSearch.search(psiMethod)) {
-      replaceReturnStatements(oMethod);
+    SmartList<PsiMethod> methodsToModify = new SmartList<>(psiMethod);
+    if (!ProgressManager.getInstance().runProcessWithProgressSynchronously(() -> {
+      methodsToModify.addAll(OverridingMethodsSearch.search(psiMethod).findAll());
+    }, InspectionsBundle.message("psi.search.overriding.progress"), true, project)) {
+      return;
+    }
+    if (!FileModificationService.getInstance().preparePsiElementsForWrite(methodsToModify)) return;
+    for (final PsiMethod method : methodsToModify) {
+      replaceReturnStatements(method);
     }
     final ChangeSignatureProcessor csp = new ChangeSignatureProcessor(project,
                                                                       psiMethod,
@@ -93,7 +101,6 @@ public class MakeVoidQuickFix implements LocalQuickFix {
   }
 
   private static void replaceReturnStatements(@NotNull final PsiMethod method) {
-    if (!FileModificationService.getInstance().preparePsiElementForWrite(method)) return;
     final PsiReturnStatement[] statements = PsiUtil.findReturnStatements(method);
     if (statements.length > 0) {
       WriteAction.run(() -> {
