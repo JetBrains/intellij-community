7ccc5647b87d7d40a254fdb5c5adb886b11de653
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2017-06-29 23:06:45 +0300

    Fix test data for PyRedeclarationInspectionTest.testUnderscorePrefixed.

--- python/testData/inspections/PyRedeclarationInspection/underscorePrefixed.py
+++ python/testData/inspections/PyRedeclarationInspection/underscorePrefixed.py
@@ -6,7 +6,7 @@ l = lambda _a: [_a for _a in []]
 
 
 f2e = [(f, e) for _a, e, f in [(1, 2, 3)]]
-e2f = [(e, f) for _a, e, f in [(1, 2, 3)]]
+e2f = [(e, f) for <warning descr="Redeclared '_a' defined above without usage">_a</warning>, e, f in [(1, 2, 3)]]
 
 array = [1, 2, 3]
-_a, mid, _a = array
+<warning descr="Redeclared '_a' defined above without usage">_a</warning>, mid, <warning descr="Redeclared '_a' defined above without usage">_a</warning> = array
