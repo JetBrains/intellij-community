7da112337fa504a3e0b509973556f8129d1cea9b
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-06-30 17:22:06 +0300

    Complete handler implementation for distinct by key

--- plugins/stream-debugger/src/main/java/com/intellij/debugger/streams/trace/impl/handler/DistinctByPredicateHandler.kt
+++ plugins/stream-debugger/src/main/java/com/intellij/debugger/streams/trace/impl/handler/DistinctByPredicateHandler.kt
@@ -36,10 +36,10 @@ class DistinctByPredicateHandler(callNumber: Int, call: IntermediateStreamCall) 
   private val myKeyExtractor: CallArgument
   private val myTypeAfter = call.typeAfter
   private val myVariableName: String = KEY_EXTRACTOR_VARIABLE_PREFIX + callNumber
-  private val myWrapperClassName = "Wrapper" + callNumber
-  private val myUtilityMap = HashMapVariableImpl(call.name + callNumber + "utilityMap",
-                                                 ClassTypeImpl(myWrapperClassName), GenericType.OBJECT, false)
-  private val myValuesAfter = HashMapVariableImpl(call.name + callNumber + "after", GenericType.INT, GenericType.OBJECT, true)
+  private val myBeforeTimes = ListVariableImpl(call.name + callNumber + "BeforeTimes", GenericType.INT)
+  private val myBeforeValues = ListVariableImpl(call.name + callNumber + "BeforeValues", GenericType.OBJECT)
+  private val myKeys = ListVariableImpl(call.name + callNumber + "Keys", GenericType.OBJECT)
+  private val myTime2ValueAfter = HashMapVariableImpl(call.name + callNumber + "after", GenericType.INT, GenericType.OBJECT, true)
 
   init {
     val arguments = call.arguments
@@ -52,34 +52,30 @@ class DistinctByPredicateHandler(callNumber: Int, call: IntermediateStreamCall) 
 
   override fun getVariables(): MutableList<Variable> {
     val variable = VariableImpl(myKeyExtractor.type, myVariableName, myKeyExtractor.text)
-    val variables = mutableListOf<Variable>(variable)
+    val variables = mutableListOf<Variable>(variable, myBeforeTimes, myBeforeValues, myTime2ValueAfter, myKeys)
     variables.addAll(myPeekHandler.variables)
 
     return variables
   }
 
   override fun getClassesDeclarations(): MutableList<String> {
-    return mutableListOf("class $myWrapperClassName { " + LINE_SEPARATOR +
-                         "  private final Object myObj;" + LINE_SEPARATOR +
-                         "  private final int myTime;" + LINE_SEPARATOR +
-                         "  Wrapper(Object obj, int time) { myObj = obj; myTime = time; }" + LINE_SEPARATOR +
-                         "  public boolean equals(Object other) { return myObj == other; }" + LINE_SEPARATOR +
-                         "  public int time() { return myTime; }" + LINE_SEPARATOR +
-                         "};" + LINE_SEPARATOR)
+    return mutableListOf()
   }
 
   override fun transformCall(call: IntermediateStreamCall): IntermediateStreamCall {
     return call.updateArguments(
       listOf(CallArgumentImpl(myKeyExtractor.type,
-                              "x -> $myVariableName.andThen(t -> {" +
-                              "  ${myUtilityMap.name}.put(new $myWrapperClassName(x), t);" +
-                              "  return t; " +
-                              "})" +
+                              "x -> $myVariableName.andThen(t -> {" + LINE_SEPARATOR +
+                              "  ${myBeforeTimes.name}.add(time.get());" + LINE_SEPARATOR +
+                              "  ${myBeforeValues.name}.add(x);" + LINE_SEPARATOR +
+                              "  ${myKeys.name}.add(t);" + LINE_SEPARATOR +
+                              "  return t; " + LINE_SEPARATOR +
+                              "})" + LINE_SEPARATOR +
                               ".apply(x)")))
   }
 
   override fun additionalCallsAfter(): MutableList<IntermediateStreamCall> {
-    val result = mutableListOf<IntermediateStreamCall>(PeekCall("x -> $myValuesAfter.put(time.get(), x)", myTypeAfter))
+    val result = mutableListOf<IntermediateStreamCall>(PeekCall("x -> ${myTime2ValueAfter.name}.put(time.get(), x)", myTypeAfter))
     result.addAll(myPeekHandler.additionalCallsAfter())
 
     return result
@@ -88,18 +84,27 @@ class DistinctByPredicateHandler(callNumber: Int, call: IntermediateStreamCall) 
   override fun prepareResult(): String {
     val peekPrepare = myPeekHandler.prepareResult()
 
-    val utilMapName = myUtilityMap.name
-    val keys2TimesBefore = HashMapVariableImpl("keys2Times", GenericType.OBJECT, ClassTypeImpl("List<Integer>"), false)
-    val buildMap = "for (final $myWrapperClassName wrapper : $utilMapName.keys()) {" + LINE_SEPARATOR +
-                   "  final Object key = $utilMapName.get(wrapper); " + LINE_SEPARATOR +
-                   "  ${keys2TimesBefore.name}.computeIfAbsent(key, k -> new ArrayList<Integer>()).add(wrapper.time());" +
+    val ifAbsent = "k -> new java.util.ArrayList<Integer>()"
+    val keys2TimesBefore = HashMapVariableImpl("keys2Times", GenericType.OBJECT, ClassTypeImpl("java.util.List<Integer>"), false)
+    val buildMap = "for (int i = 0; i < ${myKeys.name}.size(); i++) {" + LINE_SEPARATOR +
+                   "  final Object key = ${myKeys.name}.get(i); " + LINE_SEPARATOR +
+                   "  ${keys2TimesBefore.name}.computeIfAbsent(key, $ifAbsent).add(${myBeforeTimes.name}.get(i));" +
                    "}" + LINE_SEPARATOR
 
-    val valuesAfterMapName = myValuesAfter.name
+    val valuesAfterMapName = myTime2ValueAfter.name
     val transitions = HashMapVariableImpl("transitionsMap", GenericType.INT, GenericType.INT, false)
-    val buildTransitions = "for(final int afterTime : $valuesAfterMapName.keys()) {" + LINE_SEPARATOR +
+    val buildTransitions = "final boolean[] visited = new boolean[${myKeys.name}.size()];" + LINE_SEPARATOR +
+                           "for(final int afterTime : $valuesAfterMapName.keySet()) {" + LINE_SEPARATOR +
                            "  Object valueAfter = $valuesAfterMapName.get(afterTime);" + LINE_SEPARATOR +
-                           "  Object key = $utilMapName.get(new $myWrapperClassName(valueAfter, -1));" + LINE_SEPARATOR +
+                           "  Object key = null;" + LINE_SEPARATOR +
+                           "  for (int i = 0; i < visited.length; i++) {" + LINE_SEPARATOR +
+                           "    if (!visited[i] && valueAfter == ${myBeforeValues.name}.get(i)) {" + LINE_SEPARATOR +
+                           "      key = ${myKeys.name}.get(i); " + LINE_SEPARATOR +
+                           "      visited[i] = true;" + LINE_SEPARATOR +
+                           "      break;" + LINE_SEPARATOR +
+                           "    }" + LINE_SEPARATOR +
+                           "  }" + LINE_SEPARATOR +
+                           "assert key != null;" + LINE_SEPARATOR +
                            "  for(final int beforeTime : ${keys2TimesBefore.name}.get(key)) {" + LINE_SEPARATOR +
                            "    ${transitions.name}.put(beforeTime, afterTime);" +
                            "  }" + LINE_SEPARATOR +
@@ -115,7 +120,7 @@ class DistinctByPredicateHandler(callNumber: Int, call: IntermediateStreamCall) 
   }
 
   override fun getResultExpression(): String {
-    return ""
+    return "new Object[] { ${myPeekHandler.resultExpression}, transitionsArray }"
   }
 
   private fun IntermediateStreamCall.updateArguments(args: List<CallArgument>): IntermediateStreamCall {
