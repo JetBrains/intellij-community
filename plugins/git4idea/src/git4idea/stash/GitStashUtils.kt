aaaca8ded133f8de4201789bd570428983fbf319
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2017-07-01 19:04:48 +0300

    Unify git unstash procedures
    This fixes IDEA-174332

--- plugins/git4idea/src/git4idea/stash/GitStashUtils.kt
+++ plugins/git4idea/src/git4idea/stash/GitStashUtils.kt
@@ -17,17 +17,78 @@
 
 package git4idea.stash
 
+import com.intellij.dvcs.DvcsUtil
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.VcsException
+import com.intellij.openapi.vcs.VcsNotifier
+import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.Consumer
-import git4idea.commands.GitCommand
-import git4idea.commands.GitSimpleHandler
+import git4idea.commands.*
 import git4idea.config.GitConfigUtil
+import git4idea.merge.GitConflictResolver
 import git4idea.ui.StashInfo
 import git4idea.util.GitUIUtil
+import git4idea.util.GitUntrackedFilesHelper
+import git4idea.util.LocalChangesWouldBeOverwrittenHelper
 import git4idea.util.StringScanner
 import java.nio.charset.Charset
+
+/**
+ * Unstash the given root, handling common error scenarios.
+ */
+fun unstash(project: Project, root: VirtualFile, handler: GitLineHandler, conflictResolver: GitConflictResolver) {
+  unstash(project, listOf(root), { handler }, conflictResolver)
+}
+
+/**
+ * Unstash the given roots one by one, handling common error scenarios.
+ *
+ * If there's an error in one of the roots, stop and show the error.
+ * If there's a conflict, show the merge dialog, and if the conflicts get resolved, continue with other roots.
+ */
+fun unstash(project: Project,
+            roots: Collection<VirtualFile>,
+            handlerProvider: (VirtualFile) -> GitLineHandler,
+            conflictResolver: GitConflictResolver) {
+  val token = DvcsUtil.workingTreeChangeStarted(project)
+  try {
+    for (root in roots) {
+      val handler = handlerProvider(root)
+
+      val conflictDetector = GitSimpleEventDetector(GitSimpleEventDetector.Event.MERGE_CONFLICT_ON_UNSTASH)
+      val untrackedFilesDetector = GitUntrackedFilesOverwrittenByOperationDetector(root)
+      val localChangesDetector = GitLocalChangesWouldBeOverwrittenDetector(root, GitLocalChangesWouldBeOverwrittenDetector.Operation.MERGE)
+      handler.addLineListener(conflictDetector)
+      handler.addLineListener(untrackedFilesDetector)
+      handler.addLineListener(localChangesDetector)
+
+      val result = Git.getInstance().runCommand { handler }
+
+      VfsUtil.markDirtyAndRefresh(false, true, false, root)
+
+      if (conflictDetector.hasHappened()) {
+        val conflictsResolved = conflictResolver.merge()
+        if (!conflictsResolved) return
+      }
+      else if (untrackedFilesDetector.wasMessageDetected()) {
+        GitUntrackedFilesHelper.notifyUntrackedFilesOverwrittenBy(project, root, untrackedFilesDetector.relativeFilePaths, "unstash", null)
+        return
+      }
+      else if (localChangesDetector.wasMessageDetected()) {
+        LocalChangesWouldBeOverwrittenHelper.showErrorDialog(project, root, "unstash", localChangesDetector.relativeFilePaths)
+        return
+      }
+      else if (!result.success()) {
+        VcsNotifier.getInstance(project).notifyError("Unstash Failed", result.errorOutputAsHtmlString)
+        return
+      }
+    }
+  }
+  finally {
+    token.finish()
+  }
+}
 
 fun loadStashStack(project: Project, root: VirtualFile, consumer: Consumer<StashInfo>) {
   loadStashStack(project, root, Charset.forName(GitConfigUtil.getLogEncoding(project, root)), consumer)
