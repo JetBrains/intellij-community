4d636b291daee20cb15a83881f33ea83dcf34c59
peter <peter@jetbrains.com>
2017-07-03 21:37:24 +0200

    ApplyRandomIntentionsTest: perform mutations as random actions in a single test in different files

--- java/java-tests/testSrc/com/intellij/java/propertyBased/ApplyRandomIntentionsTest.java
+++ java/java-tests/testSrc/com/intellij/java/propertyBased/ApplyRandomIntentionsTest.java
@@ -15,18 +15,20 @@
  */
 package com.intellij.java.propertyBased;
 
-import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.psi.*;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.impl.PsiDocumentManagerImpl;
 import com.intellij.psi.util.PsiModificationTracker;
-import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.testFramework.SkipSlowTestLocally;
 import com.intellij.util.SystemProperties;
-import org.jetbrains.annotations.Nullable;
-import slowCheck.*;
+import com.intellij.util.containers.ContainerUtil;
+import slowCheck.CheckerSettings;
+import slowCheck.Generator;
+import slowCheck.IntDistribution;
+import slowCheck.PropertyChecker;
 
-import java.util.Objects;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.function.Consumer;
 
 @SkipSlowTestLocally
 public class ApplyRandomIntentionsTest extends AbstractApplyAndRevertTestCase {
@@ -34,109 +36,18 @@ public class ApplyRandomIntentionsTest extends AbstractApplyAndRevertTestCase {
   @Override
   public void setUp() throws Exception {
     super.setUp();
+    ((PsiDocumentManagerImpl)PsiDocumentManager.getInstance(myProject)).disableBackgroundCommit(getTestRootDisposable());
+  }
+
+  public void testIntentionsInDifferentFiles() throws Throwable {
     initCompiler();
-  }
-
-  public void testOpenFilesAndRevert() throws Throwable {
-    doOpenFilesAndRevert(null);
-  }
-
-  public void testOpenFilesAndRevertDeleteForeachInitializers() throws Throwable {
-    doOpenFilesAndRevert(psiFile -> {
-      WriteCommandAction.runWriteCommandAction(myProject, () -> PsiTreeUtil.findChildrenOfType(psiFile, PsiForStatement.class).stream()
-        .limit(20)
-        .forEach(stmt -> stmt.getInitialization().delete()));
-    });
-  }
-
-  public void testOpenFilesAndRevertDeleteSecondArgument() throws Throwable {
-    doOpenFilesAndRevert(psiFile -> {
-      WriteCommandAction.runWriteCommandAction(myProject, () -> PsiTreeUtil.findChildrenOfType(psiFile, PsiCallExpression.class)
-        .stream()
-        .filter(PsiElement::isValid)
-        .map(PsiCall::getArgumentList)
-        .filter(Objects::nonNull)
-        .filter(argList -> argList.getExpressions().length > 1)
-        .limit(20)
-        .forEach(argList -> {
-          if (!argList.isValid()) return;
-          PsiExpression arg = argList.getExpressions()[1];
-          if (!arg.isValid()) return;
-          arg.delete();
-        }));
-    });
-  }
-
-  public void testOpenFilesAndRevertAddNullArgument() throws Throwable {
-    doOpenFilesAndRevert(psiFile -> {
-      WriteCommandAction.runWriteCommandAction(myProject,
-                                                      () -> PsiTreeUtil.findChildrenOfType(psiFile, PsiMethodCallExpression.class).stream()
-                                                        .filter(PsiElement::isValid)
-                                                        .filter(call -> call.getArgumentList().getExpressions().length > 1)
-                                                        .forEach(call -> call.getArgumentList().add(JavaPsiFacade.getElementFactory(myProject).createExpressionFromText("null", call))));
-    });
-  }
-
-  public void testMakeAllMethodsVoid() throws Throwable {
-    doOpenFilesAndRevert(psiFile -> {
-      WriteCommandAction.runWriteCommandAction(myProject,
-                                               () -> PsiTreeUtil.findChildrenOfType(psiFile, PsiMethod.class).stream()
-                                                        .filter(method -> method.getReturnTypeElement() != null)
-                                                        .forEach(method -> method.getReturnTypeElement().replace(JavaPsiFacade.getElementFactory(myProject).createTypeElement(PsiType.VOID))));
-    });
-  }
-
-  private void doOpenFilesAndRevert(@Nullable Consumer<PsiFile> mutation) throws Throwable {
-    PsiManager psiManager = PsiManager.getInstance(myProject);
-    CheckerSettings settings = CheckerSettings.DEFAULT_SETTINGS;
-    PropertyChecker.forAll(settings.withIterationCount(10), javaFiles(), file -> {
-      if (mutation == null) {
-        checkCompiles(myCompilerTester.rebuild());
-      }
-
-      PsiFile psiFile = psiManager.findFile(file);
-      if (psiFile == null) return false;
-
-      Generator<InvokeIntention> genInvocation = Generator.from(data -> InvokeIntention.generate(psiFile, data)).noShrink();
-      PropertyChecker.forAll(settings.withIterationCount(20), Generator.nonEmptyLists(genInvocation), list -> {
-        PsiDocumentManager documentManager = PsiDocumentManager.getInstance(myProject);
-        changeAndRevert(() -> {
-          if (mutation != null) {
-            mutation.accept(psiFile);
-          }
-          documentManager.commitAllDocuments();
-          long modCount = psiManager.getModificationTracker().getModificationCount();
-          for (InvokeIntention invocation : list) {
-            String textBefore = psiFile.getText();
-            try {
-              invocation.invokeIntention();
-            }
-            catch (Throwable e) {
-              LOG.debug("File " + file.getName() + " text before applying " + invocation + ":\n" + textBefore);
-              throw e;
-            }
-          }
-          if (mutation == null && modCount != psiManager.getModificationTracker().getModificationCount()) {
-            checkCompiles(myCompilerTester.make());
-          }
-        });
-        return true;
-      });
-
-      return true;
-    });
-  }
-
-  public void testModificationsInDifferentFiles() throws Throwable {
-    PsiManager psiManager = PsiManager.getInstance(myProject);
-    PsiModificationTracker tracker = psiManager.getModificationTracker();
+    PsiModificationTracker tracker = PsiManager.getInstance(myProject).getModificationTracker();
 
     AtomicLong rebuildStamp = new AtomicLong();
 
     CheckerSettings settings = CheckerSettings.DEFAULT_SETTINGS.withIterationCount(30);
-    Generator<InvokeIntention> genIntention =
-      Generator.from(data -> InvokeIntention.generate(psiManager.findFile(javaFiles().generateValue(data)), data));
-
+    Generator<InvokeIntention> genIntention = psiJavaFiles().flatMap(
+      file -> Generator.from(data -> InvokeIntention.generate(file, data)));
     PropertyChecker.forAll(settings, Generator.listsOf(genIntention.noShrink()), list -> {
       long startModCount = tracker.getModificationCount();
       if (rebuildStamp.getAndSet(startModCount) != startModCount) {
@@ -144,12 +55,35 @@ public class ApplyRandomIntentionsTest extends AbstractApplyAndRevertTestCase {
       }
 
       changeAndRevert(() -> {
-        for (InvokeIntention invocation : list) {
-          invocation.invokeIntention();
-        }
+        MadTestingAction.runActions(list, myProject);
+        
         if (tracker.getModificationCount() != startModCount) {
           checkCompiles(myCompilerTester.make());
         }
+      });
+      return true;
+    });
+  }
+
+  public void testIntentionsAndModificationsInDifferentFiles() throws Throwable {
+    CheckerSettings settings = CheckerSettings.DEFAULT_SETTINGS.withIterationCount(50);
+    Generator<List<MadTestingAction>> genActionGroup = psiJavaFiles().flatMap(
+      file -> {
+        Generator<MadTestingAction> mutation = Generator.anyOf(DeleteRange.deletePsiRange(file),
+                                                               Generator.constant(new AddNullArgument(file)),
+                                                               Generator.constant(new DeleteForeachInitializers(file)),
+                                                               Generator.constant(new DeleteSecondArgument(file)),
+                                                               Generator.constant(new MakeAllMethodsVoid(file)));
+        Generator<MadTestingAction> allActions = Generator.frequency(2, Generator.from(data -> InvokeIntention.generate(file, data)),
+                                                                     1, Generator.constant(new RehighlightAllEditors(myProject)),
+                                                                     1, mutation);
+        return Generator.listsOf(IntDistribution.uniform(0, 5), allActions.noShrink());
+      });
+
+    PropertyChecker.forAll(settings, Generator.listsOf(genActionGroup).map(ContainerUtil::flatten), list -> {
+      changeAndRevert(() -> {
+        //System.out.println(list);
+        MadTestingAction.runActions(list, myProject);
       });
       return true;
     });
