7f72bd2e5f6f72d1858eea48540d9b9301db92d4
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2017-07-03 18:28:00 +0300

    PY-12634 Highlight decorator with space between '@' and name
    Added the same highlighting for '@' as for the name.
    
    (cherry picked from commit cb678d5)
    
    (cherry picked from commit d4c1e0e)

---
+++ python/testData/highlighting/spaceBetweenAtAndDecorator.py
@@ -0,0 +1,8 @@
+<info descr="null">@</info> <info descr="null">foo</info>
+def <info descr="null">f</info>():
+    pass
+
+
+<info descr="null">@</info> <info descr="null">bar</info>(1, 2, 3)
+def <info descr="null">g</info>():
+    pass
