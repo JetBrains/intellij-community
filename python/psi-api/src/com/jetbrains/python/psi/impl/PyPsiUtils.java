4c9262d6b4cc174bc00e1a923a20fcf2796b2e08
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2017-07-04 15:52:51 +0300

    Useless debug  removed

--- python/psi-api/src/com/jetbrains/python/psi/impl/PyPsiUtils.java
+++ python/psi-api/src/com/jetbrains/python/psi/impl/PyPsiUtils.java
@@ -362,15 +362,9 @@ public class PyPsiUtils {
     final PsiFile file = element.getContainingFile();
     if (file instanceof PyExpressionCodeFragment) {
       final PsiElement context = file.getContext();
-      if (LOG.isDebugEnabled()) {
-        LOG.debug("PyPsiUtil.getRealContext(" + element + ") is called. Returned " + context + ". Element inside code fragment");
-      }
       return context != null ? context : element;
     }
     else {
-      if (LOG.isDebugEnabled()) {
-        LOG.debug("PyPsiUtil.getRealContext(" + element + ") is called. Returned " + element + ".");
-      }
       return element;
     }
   }
