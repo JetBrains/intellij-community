4df5c1e27f8055783c3f6a4b57fc4d2ffa7773b1
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2017-07-05 19:15:31 +0300

    PY-7123 Structure for RST document incorrect

--- python/rest/src/com/jetbrains/rest/structureView/RestStructureViewElement.java
+++ python/rest/src/com/jetbrains/rest/structureView/RestStructureViewElement.java
@@ -16,84 +16,85 @@
 package com.jetbrains.rest.structureView;
 
 import com.intellij.ide.structureView.StructureViewTreeElement;
-import com.intellij.navigation.ItemPresentation;
+import com.intellij.ide.structureView.impl.common.PsiTreeElementBase;
+import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.NavigatablePsiElement;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiElementVisitor;
-import com.jetbrains.rest.psi.RestElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.jetbrains.rest.psi.RestTitle;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import javax.swing.*;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
 
 /**
  * Handles nodes in ReST Structure View.
  * User : catherine
  */
-public class RestStructureViewElement implements StructureViewTreeElement {
-
-  private NavigatablePsiElement myElement;
+public class RestStructureViewElement extends PsiTreeElementBase<NavigatablePsiElement> {
 
   public RestStructureViewElement(NavigatablePsiElement element) {
-    myElement = element;
+    super(element);
   }
-
-  public NavigatablePsiElement getValue() {
-    return myElement;
-  }
-
-  public void navigate(boolean requestFocus) {
-    myElement.navigate(requestFocus);
-  }
-
-  public boolean canNavigate() {
-    return myElement.canNavigate();
-  }
-
-  public boolean canNavigateToSource() {
-    return myElement.canNavigateToSource();
-  }
-
   @NotNull
-  public StructureViewTreeElement[] getChildren() {
-    final Set<RestElement> childrenElements = new LinkedHashSet<>();
-    myElement.acceptChildren(new PsiElementVisitor() {
-      @Override
-      public void visitElement(PsiElement element) {
-        if (element instanceof RestTitle && ((RestTitle)element).getName() != null)
-          childrenElements.add((RestElement)element);
-        else
-          element.acceptChildren(this);
-      }
-    });
-    StructureViewTreeElement[] children = new StructureViewTreeElement[childrenElements.size()];
-    int i = 0;
-    for (RestElement element : childrenElements) {
-      children[i] = new RestStructureViewElement(element);
-      i += 1;
+  @Override
+  public Collection<StructureViewTreeElement> getChildrenBase() {
+    Collection<StructureViewTreeElement> result = new LinkedList<>();
+
+    final NavigatablePsiElement element = getElement();
+    if (element == null) {
+      return result;
     }
 
-    return children;
+    final PsiFile file = element.getContainingFile();
+    final TextRange range = element.equals(file) ? TextRange.EMPTY_RANGE : element.getTextRange();
+    Pair<Character, Character> elementAdornments = element instanceof RestTitle ? ((RestTitle)element).getAdornments() : Pair.empty();
+
+    Pair<Character, Character> adornmentsToUse = null;
+    final Collection<RestTitle> titles = PsiTreeUtil.findChildrenOfType(file, RestTitle.class);
+    final HashSet<Pair<Character, Character>> usedAdornments = new HashSet<>();
+    boolean skipElements = false;
+
+    for (RestTitle child : titles) {
+      final Pair<Character, Character> childAdornments = child.getAdornments();
+
+      if (child.getTextRange().getStartOffset() >= range.getEndOffset()) {
+        final Character overline = childAdornments.getFirst();
+        final Character underline = childAdornments.getSecond();
+        if (adornmentsToUse == null) {
+          adornmentsToUse = childAdornments;
+        }
+        if (usedAdornments.contains(childAdornments) || underline == null) {
+          break;
+        }
+        if (underline.equals(adornmentsToUse.getSecond()) && ((adornmentsToUse.getFirst() == null && overline == null) ||
+                                                              adornmentsToUse.getFirst().equals(overline))) {
+          result.add(new RestStructureViewElement(child));
+        }
+      }
+      else {
+        if (element.equals(child)) {
+          skipElements = false;
+        }
+        else if (childAdornments.equals(elementAdornments)) {
+          skipElements = true;
+        }
+
+        if (!skipElements) {
+          usedAdornments.add(child.getAdornments());
+        }
+      }
+    }
+    return result;
   }
 
-  @NotNull
-  public ItemPresentation getPresentation() {
-    return new ItemPresentation() {
-      public String getPresentableText() {
-        return myElement.getName();
-      }
-
-      @Nullable
-      public String getLocationString() {
-        return null;
-      }
-
-      public Icon getIcon(boolean open) {
-        return null;
-      }
-    };
+  @Nullable
+  @Override
+  public String getPresentableText() {
+    final NavigatablePsiElement element = getElement();
+    return element != null ? element.getName() : "";
   }
 }
