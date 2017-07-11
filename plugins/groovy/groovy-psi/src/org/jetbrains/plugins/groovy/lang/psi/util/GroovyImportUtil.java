16650fd79fd162945f6951176d224bc74c6118e6
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2017-07-11 20:14:22 +0300

    [groovy] don't assume inner class as implicitly imported (IDEA-174811)

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/util/GroovyImportUtil.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/util/GroovyImportUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -148,7 +148,8 @@ public class GroovyImportUtil {
               }
             }
           }
-          else if (context == null && !(refElement.getParent() instanceof GrImportStatement) && refElement.getQualifier() == null) {
+          else if (context == null && !(refElement.getParent() instanceof GrImportStatement) && refElement.getQualifier() == null &&
+                   (!(resolved instanceof PsiClass) || ((PsiClass)resolved).getContainingClass() == null)) {
             addImplicitClass(resolved);
           }
         }
---
+++ plugins/groovy/testdata/optimizeImports/InnerInnerClass.groovy
@@ -0,0 +1,13 @@
+import IdeaInnerInnerClassImport.InnerClass.InnerInnerClass
+
+class IdeaInnerInnerClassImport {
+
+  InnerInnerClass outerReference
+
+  static class InnerClass {
+
+    static class InnerInnerClass {}
+
+    InnerInnerClass innerReference
+  }
+}
