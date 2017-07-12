b82eb6f22098da58e6138f5243f4d151b83aa9af
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2017-07-12 23:21:15 +0300

    PY-23926: Check teamcity plugin via setuptools api and do not add it if already registered.

--- python/helpers/pycharm/_jb_pytest_runner.py
+++ python/helpers/pycharm/_jb_pytest_runner.py
@@ -7,8 +7,7 @@ from _pytest.config import get_plugin_manager
 
 from _jb_runner_tools import jb_start_tests, jb_patch_separator, jb_doc_args, JB_DISABLE_BUFFERING
 from teamcity import pytest_plugin
-
-
+from pkg_resources import iter_entry_points
 
 if __name__ == '__main__':
     path, targets, additional_args = jb_start_tests()
@@ -22,7 +21,8 @@ if __name__ == '__main__':
     # to prevent "plugin already registered" problem we check it first
     plugins_to_load = []
     if not get_plugin_manager().hasplugin("pytest-teamcity"):
-        plugins_to_load.append(pytest_plugin)
+        if "pytest-teamcity" not in map(lambda e:e.name, iter_entry_points(group='pytest11', name=None)):
+            plugins_to_load.append(pytest_plugin)
 
     args = sys.argv[1:]
     if JB_DISABLE_BUFFERING and "-s" not in args:
