b537576498d5d773e1eddbb6ed4413cfeaa9611d
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2017-07-13 21:10:01 +0300

    Add "Restore defaults" to editor font options

--- platform/lang-impl/src/com/intellij/application/options/editor/fonts/AppEditorFontConfigurable.java
+++ platform/lang-impl/src/com/intellij/application/options/editor/fonts/AppEditorFontConfigurable.java
@@ -77,10 +77,7 @@ public class AppEditorFontConfigurable implements SearchableConfigurable, NoScro
   @Override
   public void reset() {
     getStoredPreferences().copyTo(getUIFontPreferences());
-    AppEditorFontOptionsPanel optionsPanel = getFontPanel().getOptionsPanel();
-    optionsPanel.updateWarning();
-    optionsPanel.updateOptionsList();
-    getFontPanel().updatePreview();
+    getFontPanel().getOptionsPanel().updateOnChangedFont();
   }
 
   @NotNull
