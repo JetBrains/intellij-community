1cfc238ae0d7eca9ebeb6cbc434a060a323dd81a
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2017-07-13 17:55:57 +0300

    IDEA-121819 $MODULE_DIR$ points to directory of the *.iml file

--- plugins/ant/src/com/intellij/lang/ant/config/execution/ExecutionHandler.java
+++ plugins/ant/src/com/intellij/lang/ant/config/execution/ExecutionHandler.java
@@ -50,6 +50,7 @@ import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.awt.*;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -77,7 +78,12 @@ public final class ExecutionHandler {
                                                        additionalProperties, antBuildListener, false);
     if (result != null) {
       try {
-        return result.get();
+        long l = System.currentTimeMillis();
+        try {
+          return result.get();
+        } finally {
+          new Throwable(EventQueue.isDispatchThread() + ": " + (System.currentTimeMillis() - l)).printStackTrace(System.out);
+        }
       }
       catch (InterruptedException | java.util.concurrent.ExecutionException e) {
         LOG.warn(e);
