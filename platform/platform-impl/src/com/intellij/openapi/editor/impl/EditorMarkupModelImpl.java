09078b7778b042e4795efdc7079c852f4c034f48
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2017-07-17 16:34:27 +0300

    Code Lens doesn't work in Darcula (after-review)

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorMarkupModelImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorMarkupModelImpl.java
@@ -46,7 +46,6 @@ import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.wm.IdeFocusManager;
 import com.intellij.ui.*;
 import com.intellij.ui.awt.RelativePoint;
-import com.intellij.ui.popup.BalloonPopupBuilderImpl;
 import com.intellij.util.Alarm;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.ui.ButtonlessScrollBarUI;
@@ -1343,7 +1342,7 @@ public class EditorMarkupModelImpl extends MarkupModelImpl implements EditorMark
             }
           }
         };
-        editorFragmentPreviewPanel.putClientProperty(BalloonPopupBuilderImpl.FORCED_NO_SHADOW, Boolean.TRUE);
+        editorFragmentPreviewPanel.putClientProperty(BalloonImpl.FORCED_NO_SHADOW, Boolean.TRUE);
         myEditorPreviewHint = new LightweightHint(editorFragmentPreviewPanel) {
 
           @Override
