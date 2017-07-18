8bb3cbc5648f79124431ecb01e3c33cfd233cf1a
Semyon Proshev <semyon.proshev@jetbrains.com>
2017-07-18 15:03:48 +0300

    PY-24432 Fixed: Unindent after ellipsis
    Consider `...` as a statement after that dedent should be made

--- python/src/com/jetbrains/python/formatter/PyBlock.java
+++ python/src/com/jetbrains/python/formatter/PyBlock.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -895,7 +895,18 @@ public class PyBlock implements ASTBlock {
       return false;
     }
     final PyStatement last = statements[statements.length - 1];
-    return last instanceof PyReturnStatement || last instanceof PyRaiseStatement || last instanceof PyPassStatement;
+    return last instanceof PyReturnStatement || last instanceof PyRaiseStatement || last instanceof PyPassStatement || isEllipsis(last);
+  }
+
+  private static boolean isEllipsis(@NotNull PyStatement statement) {
+    if (statement instanceof PyExpressionStatement) {
+      final PyExpression expression = ((PyExpressionStatement)statement).getExpression();
+      if (expression instanceof PyNoneLiteralExpression) {
+        return ((PyNoneLiteralExpression)expression).isEllipsis();
+      }
+    }
+
+    return false;
   }
 
   @Nullable
