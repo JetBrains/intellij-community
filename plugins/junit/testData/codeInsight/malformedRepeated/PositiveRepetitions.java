37b32370343ffb4a3227633f77c9621bcfa1265e
Timur Yuldashev <timur.yuldashev@jetbrains.com>
2017-07-20 20:53:10 +0300

    More tests for JUnit5MalformedParameterized and JUnit5MalformedRepeated
    Additional positive tests for JUnit5MalformedRepeated and more tests with TestReporter and TestInfo params for both inspections.

--- plugins/junit/testData/codeInsight/malformedRepeated/PositiveRepetitions.java
+++ plugins/junit/testData/codeInsight/malformedRepeated/PositiveRepetitions.java
@@ -2,5 +2,8 @@ import org.junit.jupiter.api.*;
 
 class WithRepeated {
   @RepeatedTest(<warning descr="The number of repetitions must be greater than zero">-1</warning>)
-  void repeatedTestNoParams() { }
+  void repeatedTestNegative() { }
+
+  @RepeatedTest(<warning descr="The number of repetitions must be greater than zero">0</warning>)
+  void repeatedTestBoundaryZerro() { }
 }
