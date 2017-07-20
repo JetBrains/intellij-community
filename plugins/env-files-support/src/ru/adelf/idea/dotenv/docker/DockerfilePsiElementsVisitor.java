408fe4f76656276627ac103c238bbda8ecd72f4b
Adel <adel.faiz@gmail.com>
2017-07-20 22:01:39 +0300

    Fix broken BC in IDEA 2017.2 Docker extension.
    GitOrigin-RevId: 27147a20c2333ed7ca88b0a596f8e59b37ba5818

--- plugins/env-files-support/src/ru/adelf/idea/dotenv/docker/DockerfilePsiElementsVisitor.java
+++ plugins/env-files-support/src/ru/adelf/idea/dotenv/docker/DockerfilePsiElementsVisitor.java
@@ -3,15 +3,11 @@ package ru.adelf.idea.dotenv.docker;
 import com.intellij.plugins.docker.dockerFile.parser.psi.DockerFileEnvRegularDeclaration;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiRecursiveElementVisitor;
-import javafx.util.Pair;
 import org.apache.commons.lang.StringUtils;
-import org.jetbrains.annotations.NotNull;
 import ru.adelf.idea.dotenv.models.KeyValuePsiElement;
 
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 class DockerfilePsiElementsVisitor extends PsiRecursiveElementVisitor {
     private final Collection<KeyValuePsiElement> collectedItems = new HashSet<>();
@@ -26,8 +22,8 @@ class DockerfilePsiElementsVisitor extends PsiRecursiveElementVisitor {
     }
 
     private void visitProperty(DockerFileEnvRegularDeclaration envRegularDeclaration) {
-        if(StringUtils.isNotBlank(envRegularDeclaration.getDeclaredName().getText())) {
-            collectedItems.add(new KeyValuePsiElement(envRegularDeclaration.getDeclaredName().getText(), envRegularDeclaration.getEnvRegularValue().getText(), envRegularDeclaration.getDeclaredName()));
+        if(StringUtils.isNotBlank(envRegularDeclaration.getDeclaredName().getText()) && envRegularDeclaration.getRegularValue() != null) {
+            collectedItems.add(new KeyValuePsiElement(envRegularDeclaration.getDeclaredName().getText(), envRegularDeclaration.getRegularValue().getText(), envRegularDeclaration.getDeclaredName()));
         }
     }
 
