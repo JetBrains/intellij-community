c47b497c8a5eb3dfb71eec4396547977c41b6b94
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2017-07-21 13:53:26 +0300

    IDEA-173532 Please don't create swing UI during filtration in `Go To Action` action [IDEA-CR-23169] (cherry picked from commit 83be208)

--- platform/platform-impl/src/com/intellij/ide/ui/OptionsTopHitProvider.java
+++ platform/platform-impl/src/com/intellij/ide/ui/OptionsTopHitProvider.java
@@ -18,9 +18,13 @@ package com.intellij.ide.ui;
 import com.intellij.ide.IdeBundle;
 import com.intellij.ide.SearchTopHitProvider;
 import com.intellij.ide.ui.search.OptionDescription;
+import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ApplicationBundle;
+import com.intellij.openapi.components.ComponentManager;
 import com.intellij.openapi.keymap.KeyMapBundle;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.codeStyle.MinusculeMatcher;
 import com.intellij.psi.codeStyle.NameUtil;
@@ -31,6 +35,10 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static com.intellij.openapi.application.ApplicationManager.getApplication;
+import static java.util.Collections.emptyList;
 
 /**
  * @author Konstantin Bulenkov
@@ -38,6 +46,16 @@ import java.util.List;
 public abstract class OptionsTopHitProvider implements SearchTopHitProvider {
   @NotNull
   public abstract Collection<OptionDescription> getOptions(@Nullable Project project);
+
+  private Collection<OptionDescription> getCachedOptions(@Nullable Project project) {
+    ComponentManager manager = project != null ? project : getApplication();
+    if (manager == null) return emptyList();
+
+    CachedOptions cache = manager.getUserData(CachedOptions.KEY);
+    if (cache == null) cache = new CachedOptions(manager);
+
+    return cache.map.computeIfAbsent(getClass(), type -> getOptions(project));
+  }
 
   @Override
   public final void consumeTopHits(@NonNls String pattern, Consumer<Object> collector, Project project) {
@@ -57,7 +75,7 @@ public abstract class OptionsTopHitProvider implements SearchTopHitProvider {
         pattern = pattern.substring(id.length()).trim().toLowerCase();
       }
       final MinusculeMatcher matcher = NameUtil.buildMatcher("*" + pattern, NameUtil.MatchingCaseSensitivity.NONE);
-      for (OptionDescription option : getOptions(project)) {
+      for (OptionDescription option : getCachedOptions(project)) {
         if (matcher.matches(option.getOption())) {
           collector.consume(option);
         }
@@ -89,5 +107,31 @@ public abstract class OptionsTopHitProvider implements SearchTopHitProvider {
    */
   @Deprecated
   public interface CoveredByToggleActions { // for search everywhere only
+  }
+
+  private static final class CachedOptions implements Disposable {
+    private static final Key<CachedOptions> KEY = Key.create("cached top hits");
+    private final ConcurrentHashMap<Class<?>, Collection<OptionDescription>> map = new ConcurrentHashMap<>();
+    private final ComponentManager manager;
+
+    private CachedOptions(ComponentManager manager) {
+      this.manager = manager;
+      Disposer.register(manager, this);
+      manager.putUserData(KEY, this);
+    }
+
+    @Override
+    public void dispose() {
+      manager.putUserData(KEY, null);
+      map.values().forEach(CachedOptions::dispose);
+    }
+
+    private static void dispose(Collection<OptionDescription> options) {
+      if (options != null) options.forEach(CachedOptions::dispose);
+    }
+
+    private static void dispose(OptionDescription option) {
+      if (option instanceof Disposable) Disposer.dispose((Disposable)option);
+    }
   }
 }
