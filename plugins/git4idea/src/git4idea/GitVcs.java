4d69a8f9d3398c60e9838596fd08f9a6425b2456
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2017-07-27 18:27:45 +0300

    Write currently used Git version to the log.
    (cherry picked from commit 558c0c4f45d62f05ffbfc5b178f0305c10f8e6f1)

--- plugins/git4idea/src/git4idea/GitVcs.java
+++ plugins/git4idea/src/git4idea/GitVcs.java
@@ -92,7 +92,7 @@ public class GitVcs extends AbstractVcs<CommittedChangeList> {
   public static final String NAME = "Git";
   public static final String ID = "git";
 
-  private static final Logger log = Logger.getInstance(GitVcs.class.getName());
+  private static final Logger LOG = Logger.getInstance(GitVcs.class.getName());
   private static final VcsKey ourKey = createKey(NAME);
 
   @Nullable private final ChangeProvider myChangeProvider;
@@ -255,7 +255,7 @@ public class GitVcs extends AbstractVcs<CommittedChangeList> {
         return GitRevisionNumber.resolve(myProject, root, revision);
       }
       catch (VcsException e) {
-        log.info("Unexpected problem with resolving the git revision number: ", e);
+        LOG.info("Unexpected problem with resolving the git revision number: ", e);
         throw e;
       }
     }
@@ -392,8 +392,8 @@ public class GitVcs extends AbstractVcs<CommittedChangeList> {
     final String executable = myAppSettings.getPathToGit();
     try {
       myVersion = GitVersion.identifyVersion(executable);
-      if (! myVersion.isSupported()) {
-        log.info("Unsupported Git version: " + myVersion);
+      LOG.info("Git version: " + myVersion);
+      if (!myVersion.isSupported()) {
         final String SETTINGS_LINK = "settings";
         final String UPDATE_LINK = "update";
         String message = String.format("The <a href='" + SETTINGS_LINK + "'>configured</a> version of Git is not supported: %s.<br/> " +
@@ -415,7 +415,9 @@ public class GitVcs extends AbstractVcs<CommittedChangeList> {
                                                        }
         );
       }
-    } catch (Exception e) {
+    }
+    catch (Exception e) {
+      LOG.warn(e);
       if (getExecutableValidator().checkExecutableAndNotifyIfNeeded()) { // check executable before notifying error
         final String reason = (e.getCause() != null ? e.getCause() : e).getMessage();
         String message = GitBundle.message("vcs.unable.to.run.git", executable, reason);
