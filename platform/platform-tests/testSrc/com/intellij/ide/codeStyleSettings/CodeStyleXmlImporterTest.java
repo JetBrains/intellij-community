44f8838364b0572fefeed84ad1304e4dd5575fec
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2017-08-01 18:50:31 +0300

    Code Style settings tests grouped together
    Base CodeStyleTestCase class

--- platform/platform-tests/testSrc/com/intellij/ide/codeStyleSettings/CodeStyleXmlImporterTest.java
+++ platform/platform-tests/testSrc/com/intellij/ide/codeStyleSettings/CodeStyleXmlImporterTest.java
@@ -1,6 +1,20 @@
-package com.intellij.application.options.codeStyle.importSettings;
+/*
+ * Copyright 2000-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.ide.codeStyleSettings;
 
-import com.intellij.openapi.application.ex.PathManagerEx;
 import com.intellij.openapi.options.SchemeFactory;
 import com.intellij.openapi.options.SchemeImportException;
 import com.intellij.openapi.vfs.VfsUtil;
@@ -9,13 +23,12 @@ import com.intellij.psi.codeStyle.CodeStyleScheme;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
 import com.intellij.psi.impl.source.codeStyle.CodeStyleSchemeImpl;
 import com.intellij.psi.impl.source.codeStyle.CodeStyleSchemeXmlImporter;
-import com.intellij.testFramework.LightPlatformTestCase;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 
-public class CodeStyleXmlImporterTest extends LightPlatformTestCase {
+public class CodeStyleXmlImporterTest extends CodeStyleTestCase {
   public void testStandardCodeStyleXml() throws SchemeImportException {
     CodeStyleSettings settings= importSettings();
     assertEquals(false, settings.AUTODETECT_INDENTS);
@@ -47,7 +60,7 @@ public class CodeStyleXmlImporterTest extends LightPlatformTestCase {
   
   @NotNull
   protected String getTestDataPath() {
-    return PathManagerEx.getTestDataPath() + "/../../../platform/lang-impl/testData/codeStyle/importSettings/";
+    return BASE_PATH + "importSettings/";
   }
 }
 
