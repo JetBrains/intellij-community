f357fa4d141b5bc98f2f6757291dbfeaefc35da9
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2017-08-02 16:23:13 +0300

    provide quickfix to convert guava's Preconditions.checkNotNull to Objects.requireNonNull IDEA-176709

--- plugins/InspectionGadgets/testsrc/com/siyeh/ig/fixes/asserttoif/IfCanBeAssertionInspectionTest.java
+++ plugins/InspectionGadgets/testsrc/com/siyeh/ig/fixes/asserttoif/IfCanBeAssertionInspectionTest.java
@@ -29,11 +29,41 @@ public class IfCanBeAssertionInspectionTest extends IGQuickFixesTestCase {
   protected void setUp() throws Exception {
     super.setUp();
     myFixture.enableInspections(new IfCanBeAssertionInspection());
+    myFixture.addClass("package java.util;\n" +
+                       "public class Objects {\n" +
+                       "    public static <T> T requireNonNull(T obj) {\n" +
+                       "        if (obj == null)\n" +
+                       "            throw new NullPointerException();\n" +
+                       "        return obj;\n" +
+                       "    }\n" +
+                       "    public static <T> T requireNonNull(T obj, String msg) {\n" +
+                       "        if (obj == null)\n" +
+                       "            throw new NullPointerException();\n" +
+                       "        return obj;\n" +
+                       "    }\n" +
+                       "}");
+    myFixture.addClass("package com.google.common.base;\n" +
+                       "public class Preconditions {\n" +
+                       "    public static <T> T checkNotNull(T obj) {\n" +
+                       "        if (obj == null)\n" +
+                       "            throw new NullPointerException();\n" +
+                       "        return obj;\n" +
+                       "    }\n" +
+                       "    public static <T> T checkNotNull(T obj, Object msg) {\n" +
+                       "        if (obj == null)\n" +
+                       "            throw new NullPointerException();\n" +
+                       "        return obj;\n" +
+                       "    }\n" +
+                       "}");
     myRelativePath = "asserttoif/if_to_assert";
-    myDefaultHint = InspectionGadgetsBundle.message("if.can.be.assertion.quickfix");
+    myDefaultHint = InspectionGadgetsBundle.message("if.can.be.assertion.replace.with.assertion.quickfix");
   }
 
   public void testRandomThrowable() { doTest(); }
   public void testParentheses() { doTest(); }
+  public void testPreconditions1() { doTest(InspectionGadgetsBundle.message("if.can.be.assertion.replace.with.objects.requirenonnull.quickfix")); }
+  public void testPreconditions2() { doTest(InspectionGadgetsBundle.message("if.can.be.assertion.replace.with.objects.requirenonnull.quickfix")); }
+  public void testPreconditions3() { doTest(InspectionGadgetsBundle.message("if.can.be.assertion.replace.with.objects.requirenonnull.quickfix")); }
+  public void testObjectsRequireNonNull() { doTest(InspectionGadgetsBundle.message("if.can.be.assertion.replace.with.objects.requirenonnull.quickfix")); }
   public void testNoCondition() { assertQuickfixNotAvailable(); }
 }
