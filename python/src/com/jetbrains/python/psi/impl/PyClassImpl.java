fb61e02b7fc76e0473137e3bd3c19a7f4bc73ed3
Semyon Proshev <semyon.proshev@jetbrains.com>
2017-08-02 18:34:49 +0300

    PY-24240 Fixed: False positive: "unexpected argument" inspection for multiple inheritance/mixin
    Transform object type to class inside PyClassImpl.getImplicitSuper
    
    (cherry picked from commit cdb868e)

--- python/src/com/jetbrains/python/psi/impl/PyClassImpl.java
+++ python/src/com/jetbrains/python/psi/impl/PyClassImpl.java
@@ -1495,7 +1495,7 @@ public class PyClassImpl extends PyBaseElementImpl<PyClassStub> implements PyCla
         .orElse(null);
     }
 
-    return objectType;
+    return objectType == null ? null : objectType.toClass();
   }
 
   @NotNull
