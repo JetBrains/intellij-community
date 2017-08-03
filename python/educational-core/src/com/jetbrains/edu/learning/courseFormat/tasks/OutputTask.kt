35dcc633e4bece7f90301acd582927202489a0c1
liana.bakradze <liana.bakradze@jetbrains.com>
2017-08-03 13:11:36 +0300

    documentation describing output tasks

--- python/educational-core/src/com/jetbrains/edu/learning/courseFormat/tasks/OutputTask.kt
+++ python/educational-core/src/com/jetbrains/edu/learning/courseFormat/tasks/OutputTask.kt
@@ -3,6 +3,11 @@ package com.jetbrains.edu.learning.courseFormat.tasks
 import com.intellij.openapi.project.Project
 import com.jetbrains.edu.learning.checker.OutputTaskChecker
 
+/**
+ * Task type that allows to test output without any test files.
+ * Correct output is specified in output.txt file which is invisible for student.
+ * @see OutputTaskChecker
+ */
 class OutputTask : Task() {
   companion object {
     @JvmField
