e2860061f406721193cc982e597c999289afc9e1
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2017-08-04 19:03:02 +0300

    Defensively copy the list of events to avoid CME: EA-105404
    The list should be unmodifiable at the time of creation,
    but it is a more complicated and dangerous change which will follow later.
    
    (cherry picked from commit 6d3db664c1cbe9fc6aca3b80c0b16c5c89317d6f)

--- plugins/git4idea/src/git4idea/repo/GitUntrackedFilesHolder.java
+++ plugins/git4idea/src/git4idea/repo/GitUntrackedFilesHolder.java
@@ -36,6 +36,8 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
 
+import static com.intellij.util.containers.ContainerUtil.newArrayList;
+
 /**
  * <p>
  *   Stores files which are untracked by the Git repository.
@@ -237,7 +239,7 @@ public class GitUntrackedFilesHolder implements Disposable, BulkFileListener {
 
   @Override
   public void after(@NotNull List<? extends VFileEvent> events) {
-    myEventsProcessor.add(events);
+    myEventsProcessor.add(newArrayList(events));
   }
 
   private void processEvents(@NotNull List<? extends VFileEvent> events) {
