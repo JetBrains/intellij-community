8ed5c932a2ddb2a967de255d92caae72ff013b68
Tor Norbye <tnorbye@google.com>
2017-08-08 13:34:33 -0700

    Import settings from 3.0 preview into 3.0.
    Test: Manual
    Change-Id: I4b54f4ff136fd23f7bc3eb4884e6fc0c57f78268

--- platform/platform-impl/src/com/intellij/openapi/application/ConfigImportHelper.java
+++ platform/platform-impl/src/com/intellij/openapi/application/ConfigImportHelper.java
@@ -181,10 +181,12 @@ public class ConfigImportHelper {
       File v2_0 = new File(PathManager.getDefaultConfigPathFor("AndroidStudio2.0"));
       File v2_1 = new File(PathManager.getDefaultConfigPathFor("AndroidStudio2.1"));
       File v2_2 = new File(PathManager.getDefaultConfigPathFor("AndroidStudio2.2"));
-      File preview = new File(PathManager.getDefaultConfigPathFor("AndroidStudioPreview2.3"));
       File v2_3 = new File(PathManager.getDefaultConfigPathFor("AndroidStudio2.3"));
       File v24preview = new File(PathManager.getDefaultConfigPathFor("AndroidStudioPreview2.4"));
-      for (File file : new File[] { v1_1, v1_2, v1_3, v1_4, v1_5, v2_0, v2_1, v2_2, preview, v2_3, v24preview }) {
+      File v30preview = new File(PathManager.getDefaultConfigPathFor("AndroidStudioPreview3.0"));
+      File v30 = new File(PathManager.getDefaultConfigPathFor("AndroidStudio3.0"));
+      for (File file : new File[]{v1_1, v1_2, v1_3, v1_4, v1_5, v2_0, v2_1, v2_2, v2_3, v24preview,
+          v30preview, v30}) {
         if (!file.isDirectory()) {
           continue;
         }
