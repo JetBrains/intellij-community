c5a20cc854816e1d5d4d8036d9b0c2658c3eabc1
Alexey Kudravtsev <cdr@intellij.com>
2017-08-09 12:11:19 +0300

    fix leaks in fork join

--- platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/PassExecutorService.java
+++ platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/PassExecutorService.java
@@ -57,10 +57,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.TestOnly;
 
 import java.util.*;
-import java.util.concurrent.CancellationException;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -84,6 +81,8 @@ class PassExecutorService implements Disposable {
   @Override
   public void dispose() {
     cancelAll(true);
+    // some workers could, although idle, still retain some thread references for some time causing leak hunter to frown
+    ForkJoinPool.commonPool().awaitQuiescence(1, TimeUnit.SECONDS);
     isDisposed = true;
   }
 
