af786f70dabd1b068d1eafb614f0b5f31d50ce3a
Konstantin Bulenkov <kb@jetbrains.com>
2017-08-11 20:29:43 +0200

    add utility methods for grouping statistics

--- platform/platform-impl/src/com/intellij/internal/statistic/UsagesCollector.java
+++ platform/platform-impl/src/com/intellij/internal/statistic/UsagesCollector.java
@@ -18,9 +18,11 @@ package com.intellij.internal.statistic;
 import com.intellij.internal.statistic.beans.GroupDescriptor;
 import com.intellij.internal.statistic.beans.UsageDescriptor;
 import com.intellij.openapi.extensions.ExtensionPointName;
+import com.intellij.openapi.util.Pair;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Set;
+import java.util.function.Function;
 
 public abstract class UsagesCollector {
 
@@ -31,4 +33,40 @@ public abstract class UsagesCollector {
 
   @NotNull
   public abstract GroupDescriptor getGroupId();
+
+  private static Pair<Double, Double> findBucket(double value, double... ranges) {
+    if (ranges.length == 0) throw new IllegalArgumentException("Constrains are empty");
+    if (value < ranges[0]) return Pair.create(null, ranges[0]);
+    for (int i = 1; i < ranges.length; i++) {
+      if (ranges[i] <= ranges[i - 1])
+        throw new IllegalArgumentException("Constrains are unsorted");
+
+      if (value < ranges[i]) {
+        return Pair.create(ranges[i - 1], ranges[i]);
+      }
+    }
+
+    return Pair.create(ranges[ranges.length - 1], null);
+  }
+
+  protected static String findBucket(long value, Function<Long, String> valueConverter, long...ranges) {
+    double[] dRanges = new double[ranges.length];
+    for (int i = 0; i < dRanges.length; i++) {
+      dRanges[i] = ranges[i];
+    }
+    return findBucket((double)value, (d) -> valueConverter.apply(d.longValue()), dRanges);
+  }
+
+  protected static String findBucket(double value, Function<Double, String> valueConverter, double...ranges) {
+    for (double range : ranges) {
+      if (range == value) {
+        return valueConverter.apply(value);
+      }
+    }
+
+    Pair<Double, Double> bucket = findBucket(value, ranges);
+    if (bucket.first == null) return "(*, " + valueConverter.apply(bucket.second) + ")";
+    if (bucket.second == null) return "(" + valueConverter.apply(bucket.first) + ", *)";
+    return "(" + valueConverter.apply(bucket.first) + ", " + valueConverter.apply(bucket.second) + ")";
+  }
 }
