31dcdc83fb4d9a93f2edc156b0269cab0e1d14a9
Alexander Zolotov <goldifit@gmail.com>
2017-08-14 21:20:49 +0300

    LargeFileAnnotator should use length of virtual file
    Otherwise, for file of near-limit size with \r\n separators code insight
    will be disabled, but the notification won't appear (GO-4290)

--- platform/lang-impl/src/com/intellij/codeInsight/highlighting/LargeFilesAnnotator.java
+++ platform/lang-impl/src/com/intellij/codeInsight/highlighting/LargeFilesAnnotator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,27 +19,29 @@ import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.PersistentFSConstants;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
 
-/**
- * @author Dmitry Avdeev
- *         Date: 5/17/13
- */
 public class LargeFilesAnnotator implements Annotator {
   @Override
   public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
     if (element instanceof PsiFile) {
-      int length = element.getTextLength();
+      long length = getFileLength((PsiFile)element);
       int limit = PersistentFSConstants.getMaxIntellisenseFileSize();
       if (length > limit) {
-        holder.createWarningAnnotation(element, "The file size (" + 
+        holder.createWarningAnnotation(element, "The file size (" +
                                                 StringUtil.formatFileSize(length) + ") " +
-                                                "exceeds configured limit (" + 
+                                                "exceeds configured limit (" +
                                                 StringUtil.formatFileSize(limit) + "). " +
                                                 "Code insight features are not available.").setFileLevelAnnotation(true);
       }
     }
+  }
+
+  private static long getFileLength(@NotNull PsiFile file) {
+    VirtualFile virtualFile = file.getVirtualFile();
+    return virtualFile != null ? virtualFile.getLength() : file.getTextLength();
   }
 }
