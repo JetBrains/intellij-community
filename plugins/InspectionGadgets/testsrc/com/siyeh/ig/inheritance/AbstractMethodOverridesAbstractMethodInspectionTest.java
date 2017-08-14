faaf3c60f31cfc13cd24fecb876ceaf1ff737b4e
Bas Leijdekkers <basleijdekkers@gmail.com>
2017-08-14 14:44:04 +0200

    IG: make test light

--- plugins/InspectionGadgets/testsrc/com/siyeh/ig/inheritance/AbstractMethodOverridesAbstractMethodInspectionTest.java
+++ plugins/InspectionGadgets/testsrc/com/siyeh/ig/inheritance/AbstractMethodOverridesAbstractMethodInspectionTest.java
@@ -1,13 +1,39 @@
+/*
+ * Copyright 2000-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.siyeh.ig.inheritance;
 
-import com.siyeh.ig.IGInspectionTestCase;
+import com.intellij.codeInspection.InspectionProfileEntry;
+import com.siyeh.ig.LightInspectionTestCase;
+import org.jetbrains.annotations.Nullable;
 
-public class AbstractMethodOverridesAbstractMethodInspectionTest extends IGInspectionTestCase {
+/**
+ * @author Bas Leijdekkers
+ */
+public class AbstractMethodOverridesAbstractMethodInspectionTest extends LightInspectionTestCase {
 
-  public void test() {
-    final AbstractMethodOverridesAbstractMethodInspection tool = new AbstractMethodOverridesAbstractMethodInspection();
-    tool.ignoreAnnotations = true;
-    tool.ignoreJavaDoc = true;
-    doTest("com/siyeh/igtest/inheritance/abstract_method_overrides_abstract_method", tool);
+  public void testAbstractMethodOverridesAbstractMethod() {
+    doTest();
+  }
+
+  @Nullable
+  @Override
+  protected InspectionProfileEntry getInspection() {
+    final AbstractMethodOverridesAbstractMethodInspection inspection = new AbstractMethodOverridesAbstractMethodInspection();
+    inspection.ignoreAnnotations = true;
+    inspection.ignoreJavaDoc = true;
+    return inspection;
   }
 }
