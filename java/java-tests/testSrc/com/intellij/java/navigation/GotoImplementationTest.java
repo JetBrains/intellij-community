c9f5397ac6116bad9477b34686eb2596e48fab8d
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2017-08-17 13:08:43 +0200

    cleanup: avoid second call to UIUtil.dispatchAllInvocationEvents

--- java/java-tests/testSrc/com/intellij/java/navigation/GotoImplementationTest.java
+++ java/java-tests/testSrc/com/intellij/java/navigation/GotoImplementationTest.java
@@ -30,7 +30,6 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiMethod;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.util.CommonProcessors;
-import com.intellij.util.ui.UIUtil;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -58,7 +57,6 @@ public class GotoImplementationTest extends CodeInsightTestCase {
     myProject = ProjectManagerEx.getInstanceEx().loadProject(projectFile.getPath());
 
     ProjectManagerEx.getInstanceEx().openTestProject(myProject);
-    UIUtil.dispatchAllInvocationEvents(); // startup activities
   }
 
   public void test() {
