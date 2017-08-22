27ba077018ce7583a43278545bb0fd5a0110416e
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2017-08-22 15:58:49 +0300

    FileUtil: getRelativePath should return relative the path from the base to the file regardless existence or the type of the base file.

--- platform/util/src/com/intellij/openapi/util/io/FileUtil.java
+++ platform/util/src/com/intellij/openapi/util/io/FileUtil.java
@@ -78,6 +78,15 @@ public class FileUtil extends FileUtilRt {
     return StringUtil.join(parts, File.separator);
   }
 
+  /**
+   * Gets the relative path from the {@code base} to the {@code file} regardless existence or the type of the {@code base}.
+   * <p>
+   * NOTE: if the file(not directory) passed as the {@code base} the result can not be used as a relative path from the {@code base} parent directory to the {@code file}
+   *
+   * @param base the base
+   * @param file the file
+   * @return the relative path from the {@code base} to the {@code file} or {@code null}
+   */
   @Nullable
   public static String getRelativePath(File base, File file) {
     return FileUtilRt.getRelativePath(base, file);
