a8c4b6be87ced0a25a8a62b48fc0aa58775c12cc
Michail Plushnikov <mplushnikov@gmail.com>
2017-08-22 11:03:01 +0200

    simplified mapping
    GitOrigin-RevId: 927d3a3934a221ce875ee6d9bb55fc161f064314

--- plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/util/LombokProcessorUtil.java
+++ plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/util/LombokProcessorUtil.java
@@ -23,11 +23,11 @@ import java.util.Map;
  */
 public class LombokProcessorUtil {
 
-  private static final Map<String, AccessLevel> ACCESS_LEVEL_MAP = new HashMap<String, AccessLevel>() {{
-    put(PsiModifier.PUBLIC, AccessLevel.PUBLIC);
-    put(PsiModifier.PACKAGE_LOCAL, AccessLevel.PACKAGE);
-    put(PsiModifier.PROTECTED, AccessLevel.PROTECTED);
-    put(PsiModifier.PRIVATE, AccessLevel.PRIVATE);
+  private static final Map<Integer, AccessLevel> ACCESS_LEVEL_MAP = new HashMap<Integer, AccessLevel>() {{
+    put(PsiUtil.ACCESS_LEVEL_PUBLIC, AccessLevel.PUBLIC);
+    put(PsiUtil.ACCESS_LEVEL_PACKAGE_LOCAL, AccessLevel.PACKAGE);
+    put(PsiUtil.ACCESS_LEVEL_PROTECTED, AccessLevel.PROTECTED);
+    put(PsiUtil.ACCESS_LEVEL_PRIVATE, AccessLevel.PRIVATE);
   }};
 
   @Nullable
@@ -87,12 +87,11 @@ public class LombokProcessorUtil {
     String value = "";
     final PsiModifierList modifierList = psiModifierListOwner.getModifierList();
     if (null != modifierList) {
-      final String accessModifier = PsiUtil.getAccessModifier(PsiUtil.getAccessLevel(modifierList));
-      if (null != accessModifier) {
-        final AccessLevel accessLevel = ACCESS_LEVEL_MAP.get(accessModifier);
-        if (null != accessLevel && !AccessLevel.PUBLIC.equals(accessLevel)) {
-          value = AccessLevel.class.getName() + "." + accessLevel;
-        }
+      final int accessLevelCode = PsiUtil.getAccessLevel(modifierList);
+
+      final AccessLevel accessLevel = ACCESS_LEVEL_MAP.get(accessLevelCode);
+      if (null != accessLevel && !AccessLevel.PUBLIC.equals(accessLevel)) {
+        value = AccessLevel.class.getName() + "." + accessLevel;
       }
     }
 
