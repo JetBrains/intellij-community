43d2bb6b74e352f09600c3da5c40250268d08e81
Raluca Sauciuc <ralucas@google.com>
2017-08-22 15:27:24 -0700

    [UITests] Implement JUnit's "perTest" forked test execution
    We can't use JUnit's forkMode attribute directly because the UI testing
    framework is using BootstrapUITests as the entry point, which to JUnit
    looks like an opaque test class. (BootstrapUITests was a fix for the
    classpath-too-long problem, similar to BootstrapTests used for unit
    tests; we should remove both of these eventually).
    
    The custom Ant task is therefore in charge of splitting up a test
    group requiring this execution style into individual test classes and
    scheduling them as separate JUnitTasks.
    
    Bug: 37144715
    Test: manual
    Change-Id: I4f9d3d43991daf45873df8deb2b555901d10de53

--- jps/antUiTest/src/com/android/antuitest/tasks/UiTestTask.java
+++ jps/antUiTest/src/com/android/antuitest/tasks/UiTestTask.java
@@ -21,13 +21,16 @@ import org.apache.tools.ant.Task;
 import org.apache.tools.ant.taskdefs.optional.junit.FormatterElement;
 import org.apache.tools.ant.taskdefs.optional.junit.JUnitTask;
 import org.apache.tools.ant.taskdefs.optional.junit.JUnitTest;
-import org.apache.tools.ant.types.*;
 import org.apache.tools.ant.types.Commandline.Argument;
+import org.apache.tools.ant.types.EnumeratedAttribute;
+import org.apache.tools.ant.types.Path;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.*;
@@ -44,11 +47,13 @@ public class UiTestTask extends Task {
 
   private static final String TEST_SUITE_CLASS_NAME = "com.android.tools.idea.tests.gui.GuiTestSuite";
   private static final String TEST_GROUP_CLASS_NAME = "com.android.tools.idea.tests.gui.framework.TestGroup";
+  private static final String TEST_RUNNER_CLASS_NAME = "com.android.tools.idea.tests.gui.framework.GuiTestSuiteRunner";
+  private static final String RUN_IN_ANNOTATION_NAME = "com.android.tools.idea.tests.gui.framework.RunIn";
 
   private String classpathFile;
   private List<String> testGroups = Collections.emptyList();
   private Path classpath;
-  private final List<Argument> jvmArgs = new ArrayList<Argument>();
+  private final List<Argument> jvmArgs = new ArrayList<>();
 
   public UiTestTask() throws Exception {
   }
@@ -92,7 +97,8 @@ public class UiTestTask extends Task {
   @Override
   public void execute() throws BuildException {
     try {
-      for (String testGroup : testGroups.isEmpty() ? getAllTestGroups() : testGroups) {
+      Map<String, Boolean> testTaskMapping = computeTestTasks(testGroups);
+      for (String testSpec : testTaskMapping.keySet()) {
         JUnitTask task = new JUnitTask();
         task.init();
         task.setProject(getProject());
@@ -106,8 +112,14 @@ public class UiTestTask extends Task {
         task.setPrintsummary((JUnitTask.SummaryAttribute) EnumeratedAttribute.getInstance(JUnitTask.SummaryAttribute.class, "true"));
 
         task.createJvmarg().setValue("-Dclasspath.file=" + classpathFile);
-        task.createJvmarg().setValue("-Dbootstrap.testcase=" + TEST_SUITE_CLASS_NAME);
-        task.createJvmarg().setValue("-Dui.test.group=" + testGroup);
+        if (testTaskMapping.get(testSpec)) {
+          // Single test execution
+          task.createJvmarg().setValue("-Dbootstrap.testcase=" + testSpec);
+        } else {
+          // Pass the entire test group, and let the test runner sift through GuiTestSuite.
+          task.createJvmarg().setValue("-Dbootstrap.testcase=" + TEST_SUITE_CLASS_NAME);
+          task.createJvmarg().setValue("-Dui.test.group=" + testSpec);
+        }
 
         Path testClasspath = task.createClasspath();
         testClasspath.add(classpath);
@@ -129,10 +141,10 @@ public class UiTestTask extends Task {
         JUnitTest jUnitTest = new JUnitTest("com.intellij.tests.BootstrapUITests");
         // Make sure we set a different outfile for each invocation, to avoid test reports clobbering each other. Previously, all
         // invocations would write to TEST-com.intellij.tests.BootstrapUITests.txt.
-        jUnitTest.setOutfile("TEST-" + testGroup);
+        jUnitTest.setOutfile("TEST-" + testSpec);
         task.addTest(jUnitTest);
 
-        log("Executing UI tests in " + testGroup);
+        log("Executing UI tests in " + testSpec);
         task.execute();
       }
     } catch (Exception ex) {
@@ -141,30 +153,65 @@ public class UiTestTask extends Task {
     }
   }
 
-  private List<String> getAllTestGroups() throws Exception {
-    Class<?> testGroupClass = createRuntimeClassLoader().loadClass(TEST_GROUP_CLASS_NAME);
+  // Prepare the mapping for splitting up test execution: a task can be either an entire test group or an individual test.
+  private Map<String, Boolean> computeTestTasks(List<String> testGroups) throws Exception {
+    ClassLoader classLoader = createRuntimeClassLoader();
+    Class<?> testGroupClass = classLoader.loadClass(TEST_GROUP_CLASS_NAME);
     Object[] testGroupValues = (Object[])testGroupClass.getMethod("values").invoke(null);
-    return Arrays.stream(testGroupValues).map(Object::toString).collect(Collectors.toList());
+
+    Object[] testClasses = (Object[]) classLoader.loadClass(TEST_RUNNER_CLASS_NAME)
+      .getMethod("getGuiTestClasses", Class.class)
+      .invoke(null, classLoader.loadClass(TEST_SUITE_CLASS_NAME));
+
+    Class<? extends Annotation> runInClass = classLoader.loadClass(RUN_IN_ANNOTATION_NAME).asSubclass(Annotation.class);
+
+    Map<String, Boolean> result = new HashMap<>();
+    for (Object testGroup : testGroupValues) {
+      if (testGroups.isEmpty() || testGroups.contains(testGroup.toString())) {
+        if ((Boolean)testGroupClass.getMethod("isForked").invoke(testGroup)) {
+          for (Object testClass : testClasses) {
+            if (shouldRunIn((Class<?>)testClass, runInClass, testGroup.toString())) {
+              result.put(((Class<?>)testClass).getCanonicalName(), true);
+            }
+          }
+        } else {
+          result.put(testGroup.toString(), false);
+        }
+      }
+    }
+    return result;
   }
 
   // Create a classloader based on the classpath contents from classpathFile.
   private ClassLoader createRuntimeClassLoader() throws Exception {
-    Collection<URL> urls = new LinkedHashSet<URL>();
+    Collection<URL> urls = new LinkedHashSet<>();
     File file = new File(classpathFile);
     try {
-      final BufferedReader reader = new BufferedReader(new FileReader(file));
-      try {
+      try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
         while (reader.ready()) {
           urls.add(new File(reader.readLine()).toURI().toURL());
         }
-      }
-      finally {
-        reader.close();
       }
       return new URLClassLoader(urls.toArray(new URL[urls.size()]));
     }
     catch (IOException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  // Returns true if klass or any of its methods is annotated with @RunIn(testGroup).
+  private static boolean shouldRunIn(Class<?> klass, Class<? extends Annotation> runInClass, String testGroup) throws Exception {
+    Method value = runInClass.getMethod("value");
+    Annotation ann = klass.getAnnotation(runInClass);
+    if (ann != null && testGroup.equals(value.invoke(ann).toString())) {
+      return true;
+    }
+    for (Method method : klass.getMethods()) {
+      ann = method.getAnnotation(runInClass);
+      if (ann != null && testGroup.equals(value.invoke(ann).toString())) {
+        return true;
+      }
+    }
+    return false;
   }
 }
