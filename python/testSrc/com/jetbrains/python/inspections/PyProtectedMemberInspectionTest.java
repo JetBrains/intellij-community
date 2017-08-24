272c03d776bbb1c672acd131bdb06cf598044151
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2017-08-24 20:01:01 +0300

    Raise weak warning about non-public API usage defined by __all__
    PY-14056 Fixed.

--- python/testSrc/com/jetbrains/python/inspections/PyProtectedMemberInspectionTest.java
+++ python/testSrc/com/jetbrains/python/inspections/PyProtectedMemberInspectionTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -77,6 +77,11 @@ public class PyProtectedMemberInspectionTest extends PyInspectionTestCase {
   }
 
   public void testModule() {
+    doMultiFileTest();
+  }
+
+  // PY-14056
+  public void testDunderAll() {
     doMultiFileTest();
   }
 
