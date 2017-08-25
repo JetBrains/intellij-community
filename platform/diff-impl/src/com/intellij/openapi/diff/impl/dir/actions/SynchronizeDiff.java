7cfa49544be720554d85de52e8b3d3960b61afdf
Gregory.Shrago <gregory.shrago@jetbrains.com>
2017-08-25 03:22:07 +0300

    IDEA-130881, IDEA-130881 show enter/space/click message if applicable

--- platform/diff-impl/src/com/intellij/openapi/diff/impl/dir/actions/SynchronizeDiff.java
+++ platform/diff-impl/src/com/intellij/openapi/diff/impl/dir/actions/SynchronizeDiff.java
@@ -16,15 +16,12 @@
 package com.intellij.openapi.diff.impl.dir.actions;
 
 import com.intellij.icons.AllIcons;
-import com.intellij.ide.diff.BackgroundOperatingDiffElement;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CustomShortcutSet;
 import com.intellij.openapi.actionSystem.ShortcutSet;
-import com.intellij.openapi.diff.impl.dir.DirDiffElementImpl;
 import com.intellij.openapi.diff.impl.dir.DirDiffTableModel;
 import com.intellij.openapi.util.SystemInfo;
-
-import java.util.List;
+import com.intellij.util.containers.JBIterable;
 
 import static com.intellij.ide.diff.DirDiffOperation.*;
 
@@ -44,20 +41,15 @@ public class SynchronizeDiff extends DirDiffAction {
   @Override
   public void update(AnActionEvent e) {
     super.update(e);
-    if (e.getPresentation().isEnabled() &&
-        (getModel().getSourceDir() instanceof BackgroundOperatingDiffElement ||
-         getModel().getTargetDir() instanceof BackgroundOperatingDiffElement)) {
-      List<DirDiffElementImpl> elements = mySelectedOnly ? getModel().getSelectedElements() : getModel().getElements();
-      for (DirDiffElementImpl dirDiffElement : elements) {
-        if ((dirDiffElement.getSource() == null || dirDiffElement.getSource().isOperationsEnabled()) &&
-            (dirDiffElement.getTarget() == null || dirDiffElement.getTarget().isOperationsEnabled()) &&
-            (dirDiffElement.getOperation() == COPY_FROM || dirDiffElement.getOperation() == COPY_TO || dirDiffElement.getOperation() == DELETE)) {
-          e.getPresentation().setEnabled(true);
-          return;
-        }
-      }
-      e.getPresentation().setEnabled(false);
+    if (!e.getPresentation().isEnabled()) {
+      return;
     }
+    boolean enabled = !JBIterable.from(mySelectedOnly ? getModel().getSelectedElements() : getModel().getElements())
+      .filter(d -> d.getOperation() == COPY_FROM || d.getOperation() == COPY_TO || d.getOperation() == DELETE)
+      .filter(d -> d.getSource() == null || d.getSource().isOperationsEnabled())
+      .filter(d -> d.getTarget() == null || d.getTarget().isOperationsEnabled())
+      .isEmpty();
+    e.getPresentation().setEnabled(enabled);
   }
 
   @Override
