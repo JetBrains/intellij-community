a6a028d8d13b4fe03d7235ab9cc861783c0b275b
Dmitriy Smirnov <dmitriy.smirnov@jetbrains.com>
2017-08-28 13:47:16 +0300

    IDEA-168059 Added Help for the Add remotes dialog

--- plugins/git4idea/src/git4idea/remote/GitConfigureRemotesDialog.kt
+++ plugins/git4idea/src/git4idea/remote/GitConfigureRemotesDialog.kt
@@ -13,21 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package git4idea.remote
 
 import com.intellij.dvcs.DvcsUtil
@@ -80,7 +66,7 @@ class GitConfigureRemotesDialog(val project: Project, val repositories: Collecti
     updateTableWidth()
   }
 
-  override fun createActions() = arrayOf(okAction)
+  override fun createActions() = arrayOf(helpAction,okAction)
 
   override fun getPreferredFocusedComponent() = table
 
@@ -291,6 +277,9 @@ private fun runInModalTask(title: String,
       border = null
     }
   }
+
+  override fun getHelpId() = "VCS.Git.Remotes";
+
 }
 
 private fun getModalityType() = if (Registry.`is`("ide.perProjectModality")) PROJECT else IDE
