5067b54af78855038dbb195c815d3fa1b7ffe27c
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2017-08-30 15:11:28 +0300

    inspection view: rerunFix should not be started inside write action

--- platform/lang-impl/src/com/intellij/codeInspection/offlineViewer/OfflineDescriptorResolveResult.java
+++ platform/lang-impl/src/com/intellij/codeInspection/offlineViewer/OfflineDescriptorResolveResult.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -221,6 +221,11 @@ class OfflineDescriptorResolveResult {
           psiFile = PsiManager.getInstance(project).findFile(file);
         }
         RunInspectionAction.runInspection(project, wrapper.getShortName(), file, null, psiFile);
+      }
+
+      @Override
+      public boolean startInWriteAction() {
+        return false;
       }
     };
     List<String> hints = offlineDescriptor.getHints();
