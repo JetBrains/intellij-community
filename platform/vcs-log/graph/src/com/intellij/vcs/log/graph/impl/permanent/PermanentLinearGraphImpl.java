234185f91fccd0e6cee651f3c51ecd30e4010c69
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2017-08-31 20:28:06 +0300

    [vcs-log] remove unused constructor
    It was used in tests, but not needed any longer.

--- platform/vcs-log/graph/src/com/intellij/vcs/log/graph/impl/permanent/PermanentLinearGraphImpl.java
+++ platform/vcs-log/graph/src/com/intellij/vcs/log/graph/impl/permanent/PermanentLinearGraphImpl.java
@@ -24,10 +24,8 @@ import com.intellij.vcs.log.graph.api.elements.GraphEdgeType;
 import com.intellij.vcs.log.graph.api.elements.GraphNode;
 import com.intellij.vcs.log.graph.utils.Flags;
 import com.intellij.vcs.log.graph.utils.IntList;
-import com.intellij.vcs.log.graph.utils.impl.BitSetFlags;
 import com.intellij.vcs.log.graph.utils.impl.CompressedIntList;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.TestOnly;
 
 import java.util.List;
 
@@ -44,11 +42,6 @@ public class PermanentLinearGraphImpl implements LinearGraph {
     mySimpleNodes = simpleNodes;
     myNodeToEdgeIndex = CompressedIntList.newInstance(nodeToEdgeIndex);
     myLongEdges = CompressedIntList.newInstance(longEdges);
-  }
-
-  @TestOnly
-  public PermanentLinearGraphImpl() {
-    this(new BitSetFlags(0), new int[0], new int[0]);
   }
 
   @Override
