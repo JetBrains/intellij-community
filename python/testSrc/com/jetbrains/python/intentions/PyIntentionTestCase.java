171f30823ba9f7371e88628900db39428cef4751
Andrey Vlasovskikh <andrey.vlasovskikh@gmail.com>
2017-08-31 13:52:26 +0300

    Added stub->AST checks for SDK to Python intention tests

--- python/testSrc/com/jetbrains/python/intentions/PyIntentionTestCase.java
+++ python/testSrc/com/jetbrains/python/intentions/PyIntentionTestCase.java
@@ -16,6 +16,7 @@
 package com.jetbrains.python.intentions;
 
 import com.intellij.codeInsight.intention.IntentionAction;
+import com.intellij.psi.PsiFile;
 import com.jetbrains.python.PythonTestUtil;
 import com.jetbrains.python.fixtures.PyTestCase;
 import com.jetbrains.python.psi.LanguageLevel;
@@ -39,23 +40,27 @@ public abstract class PyIntentionTestCase extends PyTestCase {
     }
   }
 
-  protected void doIntentionTest(final String hint, String ... files) {
+  protected void doIntentionTest(final String hint, String... files) {
     final String testFileName = getTestName(true);
+    final PsiFile file;
     if (files.length>0) {
-      myFixture.configureByFiles(files);
+      final PsiFile[] allFiles = myFixture.configureByFiles(files);
+      file = allFiles[0];
     } else {
-      myFixture.configureByFile(testFileName + ".py");
+      file = myFixture.configureByFile(testFileName + ".py");
     }
     final IntentionAction intentionAction = myFixture.findSingleIntention(hint);
     assertNotNull(intentionAction);
+    assertSdkRootsNotParsed(file);
     myFixture.launchAction(intentionAction);
     myFixture.checkResultByFile(testFileName + "_after.py", true);
   }
 
   protected void doNegativeTest(final String hint) {
     final String testFileName = getTestName(true);
-    myFixture.configureByFile(testFileName + ".py");
+    final PsiFile file = myFixture.configureByFile(testFileName + ".py");
     final IntentionAction intentionAction = myFixture.getAvailableIntention(hint);
     assertNull(intentionAction);
+    assertSdkRootsNotParsed(file);
   }
 }
