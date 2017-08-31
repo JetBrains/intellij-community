48273b157f74b61a1a3b72e1a15de399ceecbfe5
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2017-08-31 15:05:22 +0300

    Test implementations of the build tool window services added

---
+++ java/compiler/impl/src/com/intellij/build/internal/DummyBuildContentManager.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2000-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.build.internal;
+
+import com.intellij.build.BuildContentManager;
+import com.intellij.openapi.Disposable;
+import com.intellij.ui.content.Content;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+/**
+ * @author Vladislav.Soroka
+ */
+public class DummyBuildContentManager implements BuildContentManager {
+  @Override
+  public void addContent(Content content) {
+  }
+
+  @Override
+  public void removeContent(Content content) {
+  }
+
+  @Override
+  public void setSelectedContent(Content content) {
+  }
+
+  @Override
+  public void selectContent(String tabName) {
+  }
+
+  @Override
+  public void addTabbedContent(@NotNull JComponent contentComponent,
+                               @NotNull String groupPrefix,
+                               @NotNull String tabName,
+                               boolean select,
+                               @Nullable Icon icon,
+                               @Nullable Disposable childDisposable) {
+  }
+}
