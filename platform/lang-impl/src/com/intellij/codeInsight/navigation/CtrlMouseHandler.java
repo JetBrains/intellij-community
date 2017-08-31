75acc031beb03a338b6afc8f37c918dea5e3ed0b
peter <peter@jetbrains.com>
2017-08-31 12:34:18 +0200

    CtrlMouseHandler: use up-to-date file (EA-106637 - PIEAE: LeafPsiElement.invalid)

--- platform/lang-impl/src/com/intellij/codeInsight/navigation/CtrlMouseHandler.java
+++ platform/lang-impl/src/com/intellij/codeInsight/navigation/CtrlMouseHandler.java
@@ -795,9 +795,7 @@ public class CtrlMouseHandler extends AbstractProjectComponent {
     void execute(@NotNull BrowseMode browseMode) {
       myBrowseMode = browseMode;
 
-      Document document = myEditor.getDocument();
-      final PsiFile file = PsiDocumentManager.getInstance(myProject).getPsiFile(document);
-      if (file == null) return;
+      if (PsiDocumentManager.getInstance(myProject).getPsiFile(myEditor.getDocument()) == null) return;
 
       if (EditorUtil.inVirtualSpace(myEditor, myPosition)) {
         disposeHighlighter();
@@ -816,7 +814,7 @@ public class CtrlMouseHandler extends AbstractProjectComponent {
           @Nullable
           @Override
           public Continuation performInReadAction(@NotNull ProgressIndicator indicator) throws ProcessCanceledException {
-            return doExecute(file, offset);
+            return doExecute(offset);
           }
 
           @Override
@@ -827,8 +825,11 @@ public class CtrlMouseHandler extends AbstractProjectComponent {
     }
 
     @Nullable
-    private ReadTask.Continuation doExecute(@NotNull PsiFile file, int offset) {
+    private ReadTask.Continuation doExecute(int offset) {
       if (isTaskOutdated()) return null;
+
+      PsiFile file = PsiDocumentManager.getInstance(myProject).getPsiFile(myEditor.getDocument());
+      if (file == null) return null;
 
       final Info info;
       final DocInfo docInfo;
