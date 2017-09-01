867463e39cf982a104226bd1de455044d42ceb45
Alexander Zolotov <goldifit@gmail.com>
2017-09-01 15:50:31 +0300

    Do not log PCE (EA-106658)

--- platform/lang-impl/src/com/intellij/codeInsight/preview/ElementPreviewHintProvider.java
+++ platform/lang-impl/src/com/intellij/codeInsight/preview/ElementPreviewHintProvider.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.intellij.codeInsight.preview;
 
 import com.intellij.codeInsight.hint.HintManager;
@@ -5,6 +20,7 @@ import com.intellij.codeInsight.hint.HintManagerImpl;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.extensions.Extensions;
+import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.ui.LightweightHint;
@@ -69,6 +85,9 @@ public class ElementPreviewHintProvider implements ElementPreviewProvider {
       JComponent preview;
       try {
         preview = hintProvider.getPreviewComponent(element);
+      }
+      catch (ProcessCanceledException e) {
+        throw e;
       }
       catch (Exception e) {
         LOG.error(e);
