3a3763d22b7508bc5a9069fbae274043a0ef6bb5
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2017-09-03 23:07:31 +0300

    [vcs-log] remove unnecessary qualifier

--- platform/vcs-log/api/src/com/intellij/vcs/log/VcsLogProvider.java
+++ platform/vcs-log/api/src/com/intellij/vcs/log/VcsLogProvider.java
@@ -144,7 +144,7 @@ public interface VcsLogProvider {
   Collection<String> getContainingBranches(@NotNull VirtualFile root, @NotNull Hash commitHash) throws VcsException;
 
   /**
-   * In order to tune log for it's VCS, provider may set value to one of the properties specified in {@link com.intellij.vcs.log.VcsLogProperties}.
+   * In order to tune log for it's VCS, provider may set value to one of the properties specified in {@link VcsLogProperties}.
    *
    * @param property Property instance to return value for.
    * @param <T>      Type of property value.
