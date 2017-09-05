da57da1a5207377cf457844b3c161cbcc33241ec
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-09-05 18:20:22 +0300

    Remove icon for value highlighting

--- plugins/stream-debugger/src/main/java/icons/StreamDebuggerIcons.java
+++ plugins/stream-debugger/src/main/java/icons/StreamDebuggerIcons.java
@@ -24,6 +24,5 @@ import javax.swing.*;
  */
 public interface StreamDebuggerIcons {
   Icon RUN_STREAM_DEBUG_ACTION= IconLoader.getIcon("stream_debugger.png");
-  Icon VALUE_HIGHLIGHTED_ICON = IconLoader.getIcon("value_highlighting.png");
   Icon STREAM_CALL_TAB_ICON = IconLoader.getIcon("tab.png");
 }
