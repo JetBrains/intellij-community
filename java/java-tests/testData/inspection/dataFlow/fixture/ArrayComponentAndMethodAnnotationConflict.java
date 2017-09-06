89048ed8e7be7299fdc4364aed39302da10b6341
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2017-09-06 15:51:32 +0700

    array which elements are @NotNull -> array of @NotNull elements
    Review ID: IDEA-CR-24027

--- java/java-tests/testData/inspection/dataFlow/fixture/ArrayComponentAndMethodAnnotationConflict.java
+++ java/java-tests/testData/inspection/dataFlow/fixture/ArrayComponentAndMethodAnnotationConflict.java
@@ -19,17 +19,17 @@ class FooImpl implements Foo {
       return null;
     }
     else if (Math.random() > 0.5) {
-      @NotNull Object @Nullable [] arr1 = new Object[]{<warning descr="'null' is stored to an array which elements are @NotNull">null</warning>, new Object(), <warning descr="Expression 'Math.random() > 0.5 ? new Object() : null' might evaluate to null but is stored to an array of @NotNull elements">Math.random() > 0.5 ? new Object() : null</warning>};
-      @NotNull Object @Nullable [] arr2 = {<warning descr="'null' is stored to an array which elements are @NotNull">null</warning>, new Object(), <warning descr="Expression 'Math.random() > 0.5 ? new Object() : null' might evaluate to null but is stored to an array of @NotNull elements">Math.random() > 0.5 ? new Object() : null</warning>};
-      return new Object[]{<warning descr="'null' is stored to an array which elements are @NotNull">null</warning>, new Object(), <warning descr="Expression 'Math.random() > 0.5 ? new Object() : null' might evaluate to null but is stored to an array of @NotNull elements">Math.random() > 0.5 ? new Object() : null</warning>};
+      @NotNull Object @Nullable [] arr1 = new Object[]{<warning descr="'null' is stored to an array of @NotNull elements">null</warning>, new Object(), <warning descr="Expression 'Math.random() > 0.5 ? new Object() : null' might evaluate to null but is stored to an array of @NotNull elements">Math.random() > 0.5 ? new Object() : null</warning>};
+      @NotNull Object @Nullable [] arr2 = {<warning descr="'null' is stored to an array of @NotNull elements">null</warning>, new Object(), <warning descr="Expression 'Math.random() > 0.5 ? new Object() : null' might evaluate to null but is stored to an array of @NotNull elements">Math.random() > 0.5 ? new Object() : null</warning>};
+      return new Object[]{<warning descr="'null' is stored to an array of @NotNull elements">null</warning>, new Object(), <warning descr="Expression 'Math.random() > 0.5 ? new Object() : null' might evaluate to null but is stored to an array of @NotNull elements">Math.random() > 0.5 ? new Object() : null</warning>};
     }
-    return new @NotNull Object @Nullable []{<warning descr="'null' is stored to an array which elements are @NotNull">null</warning>, new Object(), <warning descr="Expression 'Math.random() > 0.5 ? new Object() : null' might evaluate to null but is stored to an array of @NotNull elements">Math.random() > 0.5 ? new Object() : null</warning>};
+    return new @NotNull Object @Nullable []{<warning descr="'null' is stored to an array of @NotNull elements">null</warning>, new Object(), <warning descr="Expression 'Math.random() > 0.5 ? new Object() : null' might evaluate to null but is stored to an array of @NotNull elements">Math.random() > 0.5 ? new Object() : null</warning>};
   }
 
   void test() {
     @NotNull Object @Nullable [] array = getNullableArrayOfNotNullObjects();
     assert array != null;
-    array[0] = <warning descr="'null' is stored to an array which elements are @NotNull">null</warning>;
+    array[0] = <warning descr="'null' is stored to an array of @NotNull elements">null</warning>;
     array[1] = <warning descr="Expression 'Math.random() > 0.5 ? null : \"foo\"' might evaluate to null but is stored to an array of @NotNull elements">Math.random() > 0.5 ? null : "foo"</warning>;
   }
 }
