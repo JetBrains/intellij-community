c3d6faf7d70f2f0696f99cc5396b773dffed69d9
Anna Kozlova <anna.kozlova@jetbrains.com>
2017-09-06 12:31:56 +0300

    NPE

--- java/java-impl/src/com/intellij/refactoring/copy/CopyClassesHandler.java
+++ java/java-impl/src/com/intellij/refactoring/copy/CopyClassesHandler.java
@@ -84,7 +84,7 @@ public class CopyClassesHandler extends CopyHandlerDelegateBase {
       final PsiFile containingFile = navigationElement.getContainingFile();
       if (!(containingFile instanceof PsiClassOwner &&
             JavaProjectRootsUtil.isOutsideJavaSourceRoot(containingFile))) {
-        if (PsiPackage.PACKAGE_INFO_CLS_FILE.equals(containingFile.getName())) continue;
+        if (containingFile != null && PsiPackage.PACKAGE_INFO_CLS_FILE.equals(containingFile.getName())) continue;
         PsiClass[] topLevelClasses = getTopLevelClasses(element);
         if (topLevelClasses == null) {
           if (element instanceof PsiDirectory) {
