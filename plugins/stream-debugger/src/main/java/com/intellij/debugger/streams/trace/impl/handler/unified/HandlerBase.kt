38461884439ef9e40d5f1cfe730528285262d7a1
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-09-07 10:51:32 +0300

    Add base class language-independent handlers

---
+++ plugins/stream-debugger/src/main/java/com/intellij/debugger/streams/trace/impl/handler/unified/HandlerBase.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2000-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.debugger.streams.trace.impl.handler.unified
+
+import com.intellij.debugger.streams.trace.IntermediateCallHandler
+import com.intellij.debugger.streams.trace.TerminatorCallHandler
+import com.intellij.debugger.streams.trace.TraceHandler
+import com.intellij.debugger.streams.trace.dsl.Dsl
+import com.intellij.debugger.streams.trace.dsl.VariableDeclaration
+
+/**
+ * @author Vitaliy.Bibaev
+ */
+abstract class HandlerBase private constructor(protected val dsl: Dsl) : TraceHandler {
+  override fun additionalVariablesDeclaration(): String {
+    return dsl.code {
+      for (declaration in getVariables()) {
+        declare(declaration)
+      }
+    }
+  }
+
+  protected abstract fun getVariables(): List<VariableDeclaration>
+
+  abstract class Intermediate(dsl: Dsl) : HandlerBase(dsl), IntermediateCallHandler
+
+  abstract class Terminal(dsl: Dsl) : HandlerBase(dsl), TerminatorCallHandler
+}
