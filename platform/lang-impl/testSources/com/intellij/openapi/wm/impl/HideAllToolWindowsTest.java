4e0c08871492741c46ff23a88900bedf42c07774
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2017-09-11 15:25:47 +0300

    Fix broken tests

--- platform/lang-impl/testSources/com/intellij/openapi/wm/impl/HideAllToolWindowsTest.java
+++ platform/lang-impl/testSources/com/intellij/openapi/wm/impl/HideAllToolWindowsTest.java
@@ -15,13 +15,9 @@
  */
 package com.intellij.openapi.wm.impl;
 
-import com.intellij.openapi.actionSystem.ActionManager;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.extensions.Extensions;
-import com.intellij.openapi.wm.ToolWindow;
-import com.intellij.openapi.wm.ToolWindowEP;
-import com.intellij.openapi.wm.ToolWindowType;
+import com.intellij.openapi.wm.*;
 import com.intellij.testFramework.MapDataContext;
 
 import java.util.*;
@@ -31,43 +27,31 @@ public class HideAllToolWindowsTest extends ToolWindowManagerTestCase {
     ToolWindowEP[] extensions = Extensions.getExtensions(ToolWindowEP.EP_NAME);
 
     Map<String, ToolWindowType> types = new HashMap<>();
-    Map<String, ToolWindowType> toRestore = new HashMap<>();
     List<ToolWindowType> cycle = new ArrayList<>(Arrays.asList(ToolWindowType.values()));
-    try {
-      for (int i = 0; i < extensions.length; i++) {
-        ToolWindowEP extension = extensions[i];
-        myManager.initToolWindow(extension);
-        ToolWindow window = myManager.getToolWindow(extension.id);
-        myManager.showToolWindow(extension.id);
+    for (int i = 0; i < extensions.length; i++) {
+      ToolWindowEP extension = extensions[i];
+      myManager.initToolWindow(extension);
+      myManager.showToolWindow(extension.id);
 
-        toRestore.put(extension.id, window.getType());
-
-        ToolWindowType type = cycle.get(i % cycle.size());
-        types.put(extension.id, type);
-        myManager.setToolWindowType(extension.id, type);
-      }
-      MapDataContext context = new MapDataContext();
-      context.put(CommonDataKeys.PROJECT, getProject());
-      Set<String> visibleIds = new HashSet<>();
-      for (String id : types.keySet()) {
-        if (myManager.getToolWindow(id).isVisible()) {
-          visibleIds.add(id);
-        }
-      }
-      ActionManager.getInstance().getAction("HideAllWindows").actionPerformed(AnActionEvent.createFromDataContext("", null, context));
-
-      for (String id : visibleIds) {
-        ToolWindow window = myManager.getToolWindow(id);
-        assertEquals(id + ":" + window.getType(),
-                     types.get(id) == ToolWindowType.FLOATING || types.get(id) == ToolWindowType.WINDOWED,
-                     window.isVisible());
+      ToolWindowType type = cycle.get(i % cycle.size());
+      types.put(extension.id, type);
+      myManager.setToolWindowType(extension.id, type);
+    }
+    MapDataContext context = new MapDataContext();
+    context.put(CommonDataKeys.PROJECT, getProject());
+    Set<String> visibleIds = new HashSet<>();
+    for (String id : types.keySet()) {
+      if (myManager.getToolWindow(id).isVisible()) {
+        visibleIds.add(id);
       }
     }
-    finally {
-      for (Map.Entry<String, ToolWindowType> entry : toRestore.entrySet()) {
-        myManager.getLayout().getInfo(entry.getKey(), false).setVisible(false);
-        myManager.setToolWindowType(entry.getKey(), entry.getValue());
-      }
+    ActionManager.getInstance().getAction("HideAllWindows").actionPerformed(AnActionEvent.createFromDataContext("", null, context));
+
+    for (String id : visibleIds) {
+      ToolWindow window = myManager.getToolWindow(id);
+      assertEquals(id + ":" + window.getType(),
+                   types.get(id) == ToolWindowType.FLOATING || types.get(id) == ToolWindowType.WINDOWED,
+                   window.isVisible());
     }
   }
 }
