ad9b32d937aa2866dc6b85f3728abb845d136b33
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2017-09-12 12:49:04 +0300

    PY-20401 Don't highlight anything else inside highlighted annotations

--- python/src/com/jetbrains/python/validation/PyAnnotator.java
+++ python/src/com/jetbrains/python/validation/PyAnnotator.java
@@ -17,11 +17,15 @@ package com.jetbrains.python.validation;
 
 import com.intellij.lang.ASTNode;
 import com.intellij.lang.annotation.Annotation;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.editor.colors.TextAttributesKey;
-import com.jetbrains.python.psi.PyElementVisitor;
 import com.intellij.lang.annotation.AnnotationHolder;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.editor.colors.EditorColorsManager;
+import com.intellij.openapi.editor.colors.TextAttributesKey;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.jetbrains.python.highlighting.PyHighlighter;
+import com.jetbrains.python.psi.PyAnnotation;
+import com.jetbrains.python.psi.PyElementVisitor;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -54,6 +58,9 @@ public abstract class PyAnnotator extends PyElementVisitor {
   }
 
   protected void addHighlightingAnnotation(@NotNull PsiElement target, @NotNull TextAttributesKey key) {
+    if (annotationHighlightingEnabled() && insideAnnotationValue(target) && key != PyHighlighter.PY_ANNOTATION) {
+      return;
+    }
     final String message = myTestMode ? key.getExternalName() : null;
     final Annotation annotation = getHolder().createInfoAnnotation(target, message);
     annotation.setTextAttributes(key);
@@ -61,5 +68,14 @@ public abstract class PyAnnotator extends PyElementVisitor {
 
   protected void addHighlightingAnnotation(@NotNull ASTNode target, @NotNull TextAttributesKey key) {
     addHighlightingAnnotation(target.getPsi(), key);
+  }
+
+  private static boolean insideAnnotationValue(@NotNull PsiElement target) {
+    final PyAnnotation annotation = PsiTreeUtil.getParentOfType(target, PyAnnotation.class);
+    return annotation != null && PsiTreeUtil.isAncestor(annotation.getValue(), target, false);
+  }
+
+  private static boolean annotationHighlightingEnabled() {
+    return !EditorColorsManager.getInstance().getGlobalScheme().getAttributes(PyHighlighter.PY_ANNOTATION).isEmpty();
   }
 }
