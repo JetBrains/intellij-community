7217de538305613784f06b3456a2928cfa570a58
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2017-09-13 19:11:59 +0300

    [file-history] extract show diff with local for old history into separate provider

--- platform/vcs-impl/src/com/intellij/openapi/vcs/history/actions/ShowDiffAfterWithLocalAction.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/history/actions/ShowDiffAfterWithLocalAction.java
@@ -17,17 +17,9 @@ package com.intellij.openapi.vcs.history.actions;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.AnActionExtensionProvider;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.ExtendableAction;
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.FilePath;
-import com.intellij.openapi.vcs.VcsDataKeys;
-import com.intellij.openapi.vcs.changes.ChangeListManager;
-import com.intellij.openapi.vcs.history.*;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.ObjectUtils;
 import org.jetbrains.annotations.NotNull;
 
 public class ShowDiffAfterWithLocalAction extends ExtendableAction implements DumbAware {
@@ -40,33 +32,10 @@ public class ShowDiffAfterWithLocalAction extends ExtendableAction implements Du
 
   @Override
   public void defaultActionPerformed(@NotNull AnActionEvent e) {
-    Project project = e.getRequiredData(CommonDataKeys.PROJECT);
-    if (ChangeListManager.getInstance(project).isFreezedWithNotification(null)) return;
-
-    VcsRevisionNumber currentRevisionNumber = e.getRequiredData(VcsDataKeys.HISTORY_SESSION).getCurrentRevisionNumber();
-    VcsFileRevision selectedRevision = e.getRequiredData(VcsDataKeys.VCS_FILE_REVISIONS)[0];
-    FilePath filePath = e.getRequiredData(VcsDataKeys.FILE_PATH);
-    VirtualFile virtualFile = e.getRequiredData(CommonDataKeys.VIRTUAL_FILE);
-
-    if (currentRevisionNumber != null && selectedRevision != null) {
-      DiffFromHistoryHandler diffHandler = ObjectUtils.notNull(e.getRequiredData(VcsDataKeys.HISTORY_PROVIDER).getHistoryDiffHandler(),
-                                                               new StandardDiffFromHistoryHandler());
-      diffHandler.showDiffForTwo(project, filePath,
-                                 selectedRevision, new CurrentRevision(virtualFile, currentRevisionNumber));
-    }
   }
 
   @Override
   public void defaultUpdate(@NotNull AnActionEvent e) {
-    VcsFileRevision[] selectedRevisions = e.getData(VcsDataKeys.VCS_FILE_REVISIONS);
-    VirtualFile virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE);
-    VcsHistorySession historySession = e.getData(VcsDataKeys.HISTORY_SESSION);
-
-    e.getPresentation().setVisible(true);
-    e.getPresentation().setEnabled(selectedRevisions != null && selectedRevisions.length == 1 && virtualFile != null &&
-                                   historySession != null && historySession.getCurrentRevisionNumber() != null &&
-                                   historySession.isContentAvailable(selectedRevisions[0]) &&
-                                   e.getData(VcsDataKeys.FILE_PATH) != null && e.getData(VcsDataKeys.HISTORY_PROVIDER) != null &&
-                                   e.getData(CommonDataKeys.PROJECT) != null);
+    e.getPresentation().setEnabledAndVisible(false);
   }
 }
