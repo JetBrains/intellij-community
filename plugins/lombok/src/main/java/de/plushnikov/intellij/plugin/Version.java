51a05f087d08a8f5f20dc981dde456c1574f4fa0
Michail Plushnikov <mplushnikov@gmail.com>
2017-09-13 09:21:01 +0200

    fixed #415
    GitOrigin-RevId: e89650ffca78d75ffac0416285cc4294d11ccb3d

--- plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/Version.java
+++ plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/Version.java
@@ -5,7 +5,7 @@ public interface Version {
   /**
    * Current plugin version.
    */
-  String PLUGIN_VERSION = "0.16";
+  String PLUGIN_VERSION = "0.15.17.2";
   /**
    * Current version of lombok plugin
    */
