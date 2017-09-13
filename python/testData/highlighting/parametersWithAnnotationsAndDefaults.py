d5fb34404e944ad3f8556aef4f15ac8d84b76664
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2017-09-13 14:49:57 +0300

    PY-20401 Use a lower highlighting level for annotations
    So that other annotations, such as the one for builtin symbols, always
    have priority over it, but it still was possible to, say, add
    a background color or a font style for type annotations using this new
    settings.

--- python/testData/highlighting/parametersWithAnnotationsAndDefaults.py
+++ python/testData/highlighting/parametersWithAnnotationsAndDefaults.py
@@ -1,2 +1,2 @@
-def <info descr="PY.FUNC_DEFINITION">f</info>(<info descr="PY.PARAMETER">p1</info>: <info descr="PY.ANNOTATION">int</info>, <info descr="PY.PARAMETER">p2</info>: <info descr="PY.ANNOTATION">int</info> = 42):
+def <info descr="PY.FUNC_DEFINITION">f</info>(<info descr="PY.PARAMETER">p1</info>: <info descr="PY.ANNOTATION"><info descr="PY.BUILTIN_NAME">int</info></info>, <info descr="PY.PARAMETER">p2</info>: <info descr="PY.ANNOTATION"><info descr="PY.BUILTIN_NAME">int</info></info> = 42):
     <info descr="PY.BUILTIN_NAME">print</info>(<info descr="PY.PARAMETER">p1</info>, <info descr="PY.PARAMETER">p2</info>)
--- python/testSrc/com/jetbrains/python/PythonHighlightingTest.java
+++ python/testSrc/com/jetbrains/python/PythonHighlightingTest.java
@@ -23,7 +23,6 @@ import com.intellij.openapi.editor.markup.TextAttributes;
 import com.jetbrains.python.documentation.PyDocumentationSettings;
 import com.jetbrains.python.documentation.docstrings.DocStringFormat;
 import com.jetbrains.python.fixtures.PyTestCase;
-import com.jetbrains.python.highlighting.PyHighlighter;
 import com.jetbrains.python.psi.LanguageLevel;
 import com.jetbrains.python.psi.impl.PythonLanguageLevelPusher;
 import org.jetbrains.annotations.NotNull;
@@ -372,13 +371,11 @@ public class PythonHighlightingTest extends PyTestCase {
 
   // PY-20401
   public void testAnnotations() {
-    createTemporaryColorScheme().setAttributes(PyHighlighter.PY_ANNOTATION, new TextAttributes(Color.RED, null, null, null, Font.PLAIN));
     runWithLanguageLevel(LanguageLevel.PYTHON36, this::doTest);
   }
 
   // PY-22729
   public void testParametersWithAnnotationsAndDefaults() {
-    createTemporaryColorScheme().setAttributes(PyHighlighter.PY_ANNOTATION, new TextAttributes(Color.RED, null, null, null, Font.PLAIN));
     runWithLanguageLevel(LanguageLevel.PYTHON30, this::doTest);
   }
 
@@ -403,12 +400,15 @@ public class PythonHighlightingTest extends PyTestCase {
   }
 
   private void doTest() {
-    final String TEST_PATH = "/highlighting/";
-    myFixture.testHighlighting(true, true, false, TEST_PATH + getTestName(true) + PyNames.DOT_PY);
+    doTest(true, true);
   }
 
   private void doTest(boolean checkWarnings, boolean checkInfos) {
-    myFixture.testHighlighting(checkWarnings, checkInfos, false, TEST_PATH + getTestName(true) + PyNames.DOT_PY);
+    myFixture.testHighlighting(checkWarnings, checkInfos, false, getTestName(true) + PyNames.DOT_PY);
   }
 
+  @Override
+  protected String getTestDataPath() {
+    return super.getTestDataPath() + "/highlighting/";
+  }
 }
