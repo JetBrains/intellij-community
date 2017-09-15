47699d54a1379aba5f933a0a08c938ce4dce6f26
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2017-09-15 12:54:23 +0300

    remove unused field

--- python/src/com/jetbrains/numpy/codeInsight/NumpyUfuncs.java
+++ python/src/com/jetbrains/numpy/codeInsight/NumpyUfuncs.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 public class NumpyUfuncs {
   private static final List<String> UFUNC_LIST = new ArrayList<>();
-  public static final List<String> UFUNC_METHODS = new ArrayList<>();
 
   public static boolean isUFunc(@Nullable final String name) {
     return UFUNC_LIST.contains(name);
