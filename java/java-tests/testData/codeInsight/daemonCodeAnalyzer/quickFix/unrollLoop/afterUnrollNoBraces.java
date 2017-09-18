7345e8b0efb6e8ee5d349367f6d3b2a2dde9736f
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2017-09-18 16:36:27 +0700

    UnrollLoopAction: fixes according to review IDEA-CR-24513
    1. Check that loop parameter is not written
    2. Support List.of, Collections.singleton, Collections.singletonList
    3. Support if w/o braces
    4. Support break not in last position

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/unrollLoop/afterUnrollNoBraces.java
@@ -0,0 +1,11 @@
+// "Unroll loop" "true"
+import java.util.Arrays;
+
+class Test {
+  void test() {
+      System.out.println("foo");
+      System.out.println("bar");
+  }
+
+  void foo(boolean b) {}
+}
