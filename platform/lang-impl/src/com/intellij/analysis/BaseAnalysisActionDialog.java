1d75021c744069d2511996811099d5a86368a731
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2017-09-18 11:28:42 +0300

    Run inspection by name dialog: incorrect radio group value change order IDEA-179055

--- platform/lang-impl/src/com/intellij/analysis/BaseAnalysisActionDialog.java
+++ platform/lang-impl/src/com/intellij/analysis/BaseAnalysisActionDialog.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -96,6 +96,14 @@ public class BaseAnalysisActionDialog extends DialogWrapper {
                                   @NotNull AnalysisUIOptions analysisUIOptions,
                                   @Nullable PsiElement context) {
     super(true);
+    //noinspection BoundFieldAssignment
+    myGroup = new ButtonGroup();
+    myGroup.add(myProjectButton);
+    myGroup.add(myModuleButton);
+    myGroup.add(myUncommitedFilesButton);
+    myGroup.add(myFileButton);
+    myGroup.add(myCustomScopeButton);
+
     Disposer.register(myDisposable, myScopeCombo);
     myAnalysisOptions = analysisUIOptions;
     myContext = context;
