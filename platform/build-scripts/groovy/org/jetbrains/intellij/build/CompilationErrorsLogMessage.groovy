677f11601f5de595b39ce90753e08a9597958384
nik <Nikolay.Chashnikov@jetbrains.com>
2017-09-18 10:01:04 +0300

    build scripts: group messages about compilation errors together
    It's inconvenient to have 99 separate 'Compilation error: java' groups in TeamCity messages about errors in the same file.

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/CompilationErrorsLogMessage.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/CompilationErrorsLogMessage.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,15 +20,13 @@ import groovy.transform.CompileStatic
  * @author nik
  */
 @CompileStatic
-class LogMessage {
-  enum Kind {
-    ERROR, WARNING, INFO, PROGRESS, BLOCK_STARTED, BLOCK_FINISHED, ARTIFACT_BUILT, @Deprecated COMPILATION_ERROR, COMPILATION_ERRORS, STATISTICS
-  }
-  final Kind kind
-  final String text
+class CompilationErrorsLogMessage extends LogMessage {
+  final String compilerName
+  final List<String> errorMessages
 
-  LogMessage(Kind kind, String text) {
-    this.kind = kind
-    this.text = text
+  CompilationErrorsLogMessage(String compilerName, List<String> errorMessages) {
+    super(Kind.COMPILATION_ERRORS, "$compilerName compilation errors")
+    this.errorMessages = errorMessages
+    this.compilerName = compilerName
   }
 }
