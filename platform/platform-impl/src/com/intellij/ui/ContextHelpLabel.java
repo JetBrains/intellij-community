7ad6f12a638e3939cd0cb70a54d929219085e409
Kirill Kirichenko <kirill.kirichenko@jetbrains.com>
2017-09-21 10:24:30 +0300

    ContextHelpLabel fixed to work with HelpTooltip

--- platform/platform-impl/src/com/intellij/ui/ContextHelpLabel.java
+++ platform/platform-impl/src/com/intellij/ui/ContextHelpLabel.java
@@ -16,50 +16,22 @@
 package com.intellij.ui;
 
 import com.intellij.icons.AllIcons;
-import com.intellij.ide.IdeTooltipManager;
-import com.intellij.ide.TooltipEvent;
-import com.intellij.openapi.ui.popup.Balloon;
+import com.intellij.ide.HelpTooltip;
 import com.intellij.ui.components.JBLabel;
-import com.intellij.util.ui.JBInsets;
-import com.intellij.util.ui.JBUI;
 import org.jetbrains.annotations.NotNull;
-
-import javax.swing.*;
-import java.awt.*;
 
 public class ContextHelpLabel extends JBLabel {
 
   public ContextHelpLabel(@NotNull String helpText) {
     super(AllIcons.General.ContextHelp);
-    IdeTooltipManager.getInstance().setCustomTooltip(this, new ContextHelpTooltip(this, helpText));
+    init(helpText);
   }
 
-  private static class ContextHelpTooltip extends TooltipWithClickableLinks.ForBrowser {
-    public ContextHelpTooltip(@NotNull JComponent component, @NotNull String text) {
-      super(component, text);
-
-      JBInsets insets = JBUI.insets(11, 10, 11, 17);
-      setBorderInsets(insets);
-      setPreferredPosition(Balloon.Position.below);
-      setCalloutShift(insets.top);
-      setBorderColor(new JBColor(Gray._161, new Color(91, 92, 94)));
-      setTextBackground(new JBColor(Gray._247, new Color(70, 72, 74)));
-      setTextForeground(new JBColor(Gray._33, Gray._191));
-    }
-
-    @Override
-    protected boolean canAutohideOn(TooltipEvent event) {
-      return event.getInputEvent() != null && super.canAutohideOn(event);
-    }
-
-    @Override
-    public int getShowDelay() {
-      return 0;
-    }
-
-    @Override
-    public boolean canBeDismissedOnTimeout() {
-      return true;
-    }
+  private void init(String helpText) {
+    new HelpTooltip().
+      setDescription(helpText).
+      setNeverHideOnTimeout(true).
+      setLocation(HelpTooltip.Alignment.HELP_BUTTON).
+      installOn(this);
   }
 }
