2ee8939dc02a437d535d84a06e42f5fa38408c58
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2017-09-22 14:36:37 +0700

    DataFlowRunner: exclude code fragments from misbalanced stack analysis
    Fixes test FragmentCompletionTest.testQualifierCastingInExpressionCodeFragment
    Review ID: IDEA-CR-24812

--- java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/DataFlowRunner.java
+++ java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/DataFlowRunner.java
@@ -188,7 +188,10 @@ public class DataFlowRunner {
           }
 
           DfaInstructionState[] after = acceptInstruction(visitor, instructionState);
-          if (LOG.isDebugEnabled() && instruction instanceof ControlTransferInstruction && after.length == 0) {
+          if (LOG.isDebugEnabled() &&
+              !(psiBlock instanceof PsiCodeFragment) &&
+              instruction instanceof ControlTransferInstruction &&
+              after.length == 0) {
             DfaMemoryState memoryState = instructionState.getMemoryState();
             if (!memoryState.isEmptyStack() && !(memoryState.peek() instanceof DfaControlTransferValue)) {
               LOG.error("Stack is corrupted at "+instructionState);
