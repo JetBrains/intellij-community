df91581dca06a9b4830369347cb35df4dbf56f6b
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2017-09-22 19:24:49 +0300

    [tests] converted to Kotlin (for better literals), part 2

--- platform/testFramework/testSrc/com/intellij/testFramework/ExpectedHighlightingDataTest.kt
+++ platform/testFramework/testSrc/com/intellij/testFramework/ExpectedHighlightingDataTest.kt
@@ -13,162 +13,97 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.intellij.testFramework;
+package com.intellij.testFramework
 
-import com.intellij.codeInsight.daemon.impl.HighlightInfo;
-import com.intellij.codeInsight.daemon.impl.HighlightInfoType;
-import com.intellij.lang.annotation.HighlightSeverity;
-import org.jetbrains.annotations.NotNull;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import com.intellij.codeInsight.daemon.impl.HighlightInfo
+import com.intellij.codeInsight.daemon.impl.HighlightInfoType
+import com.intellij.lang.annotation.HighlightSeverity
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.util.Arrays.asList
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+class ExpectedHighlightingDataTest {
+  private val TYPES = mapOf(
+    "err" to ExpectedHighlightingData.ExpectedHighlightingSet(HighlightSeverity.ERROR, false, true),
+    "warn" to ExpectedHighlightingData.ExpectedHighlightingSet(HighlightSeverity.WARNING, false, true),
+    "eol_err" to ExpectedHighlightingData.ExpectedHighlightingSet(HighlightSeverity.ERROR, true, true))
 
-import static java.util.Arrays.asList;
-import static java.util.Collections.singletonList;
-import static org.junit.Assert.assertEquals;
+  @Test fun empty() = doTest("text", emptyList(), "text")
 
-public class ExpectedHighlightingDataTest {
-  private static Map<String, ExpectedHighlightingData.ExpectedHighlightingSet> TYPES;
-
-  @BeforeClass
-  public static void setUp() {
-    TYPES = new HashMap<>();
-    TYPES.put("err", new ExpectedHighlightingData.ExpectedHighlightingSet(HighlightSeverity.ERROR, false, true));
-    TYPES.put("warn", new ExpectedHighlightingData.ExpectedHighlightingSet(HighlightSeverity.WARNING, false, true));
-    TYPES.put("eol_err", new ExpectedHighlightingData.ExpectedHighlightingSet(HighlightSeverity.ERROR, true, true));
-  }
-
-  @AfterClass
-  public static void tearDown() {
-    TYPES.clear();
-    TYPES = null;
-  }
-
-  @Test
-  public void empty() {
-    doTest("text", Collections.emptyList(), "text");
-  }
-
-  @Test
-  public void sequential() {
+  @Test fun sequential() =
     doTest("_my text_",
            asList(error(1, 3, "1"), error(4, 8, "2")),
-           "_<err descr=\"1\">my</err> <err descr=\"2\">text</err>_");
-  }
+           """_<err descr="1">my</err> <err descr="2">text</err>_""")
 
-  @Test
-  public void simpleNested() {
+  @Test fun simpleNested() =
     doTest("[(nested)]",
            asList(error(1, 9, "1"), error(2, 8, "2")),
-           "[<err descr=\"1\">(<err descr=\"2\">nested</err>)</err>]");
-  }
+           """[<err descr="1">(<err descr="2">nested</err>)</err>]""")
 
-  @Test
-  public void deepNested() {
+  @Test fun deepNested() =
     doTest("m1(m2(m3(m4(x))))",
            asList(error(3, 16, "m1"), error(6, 15, "m2"), error(9, 14, "m3"), error(12, 13, "m4")),
-           "m1(<err descr=\"m1\">m2(<err descr=\"m2\">m3(<err descr=\"m3\">m4(<err descr=\"m4\">x</err>)</err>)</err>)</err>)");
-  }
+           """m1(<err descr="m1">m2(<err descr="m2">m3(<err descr="m3">m4(<err descr="m4">x</err>)</err>)</err>)</err>)""")
 
-  @Test
-  public void sameStart() {
+  @Test fun sameStart() =
     doTest("same start",
            asList(error(0, 4, "1"), error(0, 10, "2")),
-           "<err descr=\"2\"><err descr=\"1\">same</err> start</err>");
-  }
+           """<err descr="2"><err descr="1">same</err> start</err>""")
 
-  @Test
-  public void sameEnd() {
+  @Test fun sameEnd() =
     doTest("same end",
            asList(error(0, 8, "1"), error(5, 8, "2")),
-           "<err descr=\"1\">same <err descr=\"2\">end</err></err>");
-  }
+           """<err descr="1">same <err descr="2">end</err></err>""")
 
-  @Test
-  public void sameBothBounds() {
+  @Test fun sameBothBounds() =
     doTest("same",
            asList(error(0, 4, "-"), warning(0, 4, "-")),
-           "<err descr=\"-\"><warn descr=\"-\">same</warn></err>");
-  }
+           """<err descr="-"><warn descr="-">same</warn></err>""")
 
-  @Test
-  public void samePriority() {
+  @Test fun samePriority() =
     doTest("_same_",
            asList(warning(1, 5, "1"), warning(1, 5, "2")),
-           "_<warn descr=\"1\"><warn descr=\"2\">same</warn></warn>_");
-  }
+           """_<warn descr="1"><warn descr="2">same</warn></warn>_""")
 
-  @Test
-  public void twoNests() {
+  @Test fun twoNests() =
     doTest("(two nests)",
            asList(error(0, 11, "-"), error(1, 4, "1"), error(5, 10, "2")),
-           "<err descr=\"-\">(<err descr=\"1\">two</err> <err descr=\"2\">nests</err>)</err>");
-  }
+           """<err descr="-">(<err descr="1">two</err> <err descr="2">nests</err>)</err>""")
 
-  @Test
-  public void realistic() {
+  @Test fun realistic() =
     doTest("one and (two nests)",
            asList(error(4, 7, "-"), error(8, 19, "-"), error(9, 12, "1"), error(13, 18, "2")),
-           "one <err descr=\"-\">and</err> <err descr=\"-\">(<err descr=\"1\">two</err> <err descr=\"2\">nests</err>)</err>");
-  }
+           """one <err descr="-">and</err> <err descr="-">(<err descr="1">two</err> <err descr="2">nests</err>)</err>""")
 
-  @Test
-  public void twoEOLs() {
+  @Test fun twoEOLs() =
     doTest("text\nmore text",
            asList(eolError(4, 4, "1"), eolError(4, 4, "2")),
-           "text<eol_err descr=\"2\"></eol_err><eol_err descr=\"1\"></eol_err>\nmore text");
-  }
+           """
+             text<eol_err descr="2"></eol_err><eol_err descr="1"></eol_err>
+             more text""".trimIndent())
 
-  @Test
-  public void eolAfterError() {
+  @Test fun eolAfterError() =
     doTest("some error\nmore text",
            asList(error(5, 10, "1"), eolError(10, 10, "2")),
-           "some <err descr=\"1\">error</err><eol_err descr=\"2\"></eol_err>\nmore text");
-  }
+           """
+             some <err descr="1">error</err><eol_err descr="2"></eol_err>
+             more text""".trimIndent())
 
-  @Test
-  public void consecutiveNests() {
+  @Test fun consecutiveNests() =
     doTest(" ab ",
            asList(error(1, 2, "a1"), error(1, 2, "a2"), error(2, 3, "b1"), error(2, 3, "b2")),
-           " <err descr=\"a1\"><err descr=\"a2\">a</err></err><err descr=\"b1\"><err descr=\"b2\">b</err></err> ");
-  }
+           """ <err descr="a1"><err descr="a2">a</err></err><err descr="b1"><err descr="b2">b</err></err> """)
 
-  @Test
-  public void firstEmpty() {
-    doTest(" ab ",
-           singletonList(error(0, 0, "a1")),
-           "<err descr=\"a1\"></err> ab ");
-  }
 
-  private static void doTest(String original, Collection<HighlightInfo> highlighting, String expected) {
-    String text = ExpectedHighlightingData.composeText(TYPES, highlighting, original);
-    assertEquals(expected, text);
-  }
+  private fun doTest(original: String, highlighting: Collection<HighlightInfo>, expected: String) =
+    assertEquals(expected, ExpectedHighlightingData.composeText(TYPES, highlighting, original))
 
-  private static HighlightInfo error(int start, int end, @NotNull String description) {
-    HighlightInfo.Builder builder = HighlightInfo.newHighlightInfo(HighlightInfoType.ERROR);
-    builder.range(start, end);
-    builder.descriptionAndTooltip(description);
-    return builder.createUnconditionally();
-  }
+  private fun error(start: Int, end: Int, description: String) =
+    HighlightInfo.newHighlightInfo(HighlightInfoType.ERROR).range(start, end).descriptionAndTooltip(description).createUnconditionally()
 
-  private static HighlightInfo warning(int start, int end, @NotNull String description) {
-    HighlightInfo.Builder builder = HighlightInfo.newHighlightInfo(HighlightInfoType.WARNING);
-    builder.range(start, end);
-    builder.descriptionAndTooltip(description);
-    return builder.createUnconditionally();
-  }
+  private fun warning(start: Int, end: Int, description: String) =
+    HighlightInfo.newHighlightInfo(HighlightInfoType.WARNING).range(start, end).descriptionAndTooltip(description).createUnconditionally()
 
-  private static HighlightInfo eolError(int start, int end, @NotNull String description) {
-    HighlightInfo.Builder builder = HighlightInfo.newHighlightInfo(HighlightInfoType.ERROR);
-    builder.range(start, end);
-    builder.description(description);
-    builder.endOfLine();
-    return builder.createUnconditionally();
-  }
+  private fun eolError(start: Int, end: Int, description: String) =
+    HighlightInfo.newHighlightInfo(HighlightInfoType.ERROR).range(start, end).description(description).endOfLine().createUnconditionally()
 }
