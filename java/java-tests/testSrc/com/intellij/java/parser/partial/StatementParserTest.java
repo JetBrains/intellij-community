324581b347cb2e5502ef415628b1c91578185d6d
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2017-09-26 12:13:34 +0200

    [java] parser fix: primitive types start a declaration

--- java/java-tests/testSrc/com/intellij/java/parser/partial/StatementParserTest.java
+++ java/java-tests/testSrc/com/intellij/java/parser/partial/StatementParserTest.java
@@ -49,8 +49,10 @@ public class StatementParserTest extends JavaParsingTestCase {
   public void testLocalVar4() { doParserTest("final var x;"); }
   public void testLocalVar5() { doParserTest("@A var x;"); }
   public void testLocalVar6() { doParserTest("@A var"); }
+  public void testLocalVar7() { doParserTest("int"); }
 
   public void testExprStatement0() { doParserTest("var"); }
+  public void testExprStatement1() { doParserTest("int."); }
 
   public void testDoNormal() { doParserTest("do{}while(true);"); }
   public void testDoIncomplete0() { doParserTest("do"); }
