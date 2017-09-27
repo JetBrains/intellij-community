dc8a28b32c308fad071f48640baa6505ed645e93
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2017-09-27 09:50:00 +0200

    [java] module dependency quick fixes
    Fixes availability of "Add module dependency" quick fix; ensures quick fix availability in tests; adds a test for IDEA-179293.

--- java/java-impl/src/com/intellij/codeInsight/daemon/impl/quickfix/AddModuleDependencyFix.java
+++ java/java-impl/src/com/intellij/codeInsight/daemon/impl/quickfix/AddModuleDependencyFix.java
@@ -1,18 +1,6 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o.
+// Use of this source code is governed by the Apache 2.0 license that can be
+// found in the LICENSE file.
 package com.intellij.codeInsight.daemon.impl.quickfix;
 
 import com.intellij.application.options.ModuleListCellRenderer;
@@ -31,6 +19,7 @@ import com.intellij.openapi.util.Couple;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.source.PsiJavaModuleReference;
+import com.intellij.psi.impl.source.resolve.JavaResolveUtil;
 import com.intellij.ui.components.JBList;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
@@ -63,11 +52,10 @@ class AddModuleDependencyFix extends OrderEntryFix {
 
     PsiElement psiElement = reference.getElement();
     Project project = psiElement.getProject();
-    PsiResolveHelper resolveHelper = JavaPsiFacade.getInstance(project).getResolveHelper();
     ProjectFileIndex fileIndex = ProjectRootManager.getInstance(project).getFileIndex();
     ModuleRootManager rootManager = ModuleRootManager.getInstance(currentModule);
     for (PsiClass aClass : classes) {
-      if (!resolveHelper.isAccessible(aClass, psiElement, aClass)) continue;
+      if (!isAccessible(aClass, psiElement)) continue;
       PsiFile psiFile = aClass.getContainingFile();
       if (psiFile == null) continue;
       VirtualFile virtualFile = psiFile.getVirtualFile();
@@ -77,6 +65,10 @@ class AddModuleDependencyFix extends OrderEntryFix {
         myModules.add(classModule);
       }
     }
+  }
+
+  private static boolean isAccessible(PsiClass aClass, PsiElement refElement) {
+    return JavaResolveUtil.isAccessible(aClass, aClass.getContainingClass(), aClass.getModifierList(), refElement, aClass, null);
   }
 
   public AddModuleDependencyFix(PsiJavaModuleReference reference,
