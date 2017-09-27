845f7f488742f9b9fb5cb3264a476d1cf658e996
fitermay <fiterman@gmail.com>
2017-09-26 23:08:32 -0400

    PY-26239: Fix find class usages performance
    Instead of searching for "__init__" text. Search for references named the same as the class but with __init__ method target

--- python/src/com/jetbrains/python/findUsages/PyClassFindUsagesHandler.java
+++ python/src/com/jetbrains/python/findUsages/PyClassFindUsagesHandler.java
@@ -16,33 +16,16 @@
 package com.jetbrains.python.findUsages;
 
 import com.intellij.psi.PsiElement;
-import com.jetbrains.python.PyNames;
 import com.jetbrains.python.psi.PyClass;
-import com.jetbrains.python.psi.PyFunction;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Collection;
-import java.util.Collections;
 
 /**
  * @author yole
  */
 public class PyClassFindUsagesHandler extends PyFindUsagesHandler {
-  private final PyClass myClass;
 
   public PyClassFindUsagesHandler(@NotNull PyClass psiElement) {
     super(psiElement);
-    myClass = psiElement;
-  }
-
-  @NotNull
-  @Override
-  public PsiElement[] getSecondaryElements() {
-    final PyFunction initMethod = myClass.findMethodByName(PyNames.INIT, false, null);
-    if (initMethod != null) {
-      return new PsiElement[] { initMethod };
-    }
-    return PsiElement.EMPTY_ARRAY;
   }
 
   @Override
@@ -50,11 +33,4 @@ public class PyClassFindUsagesHandler extends PyFindUsagesHandler {
     return true;
   }
 
-  @Override
-  protected Collection<String> getStringsToSearch(@NotNull PsiElement element) {
-    if (element instanceof PyFunction && PyNames.INIT.equals(((PyFunction) element).getName())) {
-      return Collections.emptyList();
-    }
-    return super.getStringsToSearch(element);
-  }
 }
