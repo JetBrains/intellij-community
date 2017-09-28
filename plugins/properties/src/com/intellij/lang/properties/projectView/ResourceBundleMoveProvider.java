fcb1eb2997c4377e86bb8c3e4720607ccd890809
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2017-09-28 12:47:06 +0300

    properties: make move resource bundle available with f6 (IDEA-179376)

--- plugins/properties/src/com/intellij/lang/properties/projectView/ResourceBundleMoveProvider.java
+++ plugins/properties/src/com/intellij/lang/properties/projectView/ResourceBundleMoveProvider.java
@@ -1,26 +1,16 @@
-/*
- * Copyright 2000-2010 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o.
+// Use of this source code is governed by the Apache 2.0 license that can be
+// found in the LICENSE file.
 package com.intellij.lang.properties.projectView;
 
 import com.intellij.lang.properties.ResourceBundle;
 import com.intellij.lang.properties.psi.PropertiesFile;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
+import com.intellij.refactoring.move.MoveCallback;
 import com.intellij.refactoring.move.MoveHandlerDelegate;
 import com.intellij.refactoring.move.moveFilesOrDirectories.MoveFilesOrDirectoriesHandler;
 import org.jetbrains.annotations.Nullable;
@@ -43,7 +33,7 @@ public class ResourceBundleMoveProvider extends MoveHandlerDelegate {
 
   @Override
   public boolean isValidTarget(PsiElement psiElement, PsiElement[] sources) {
-    return MoveFilesOrDirectoriesHandler.isValidTarget(psiElement);
+    return (psiElement == null && (sources == null || sources.length == 0)) || MoveFilesOrDirectoriesHandler.isValidTarget(psiElement);
   }
 
   @Override
@@ -65,5 +55,13 @@ public class ResourceBundleMoveProvider extends MoveHandlerDelegate {
       return source.getParent() == target;
     }
     return super.isMoveRedundant(source, target);
+  }
+
+  @Override
+  public void doMove(Project project,
+                     PsiElement[] elements,
+                     @Nullable PsiElement targetContainer,
+                     @Nullable MoveCallback callback) {
+    super.doMove(project, elements, targetContainer, callback);
   }
 }
