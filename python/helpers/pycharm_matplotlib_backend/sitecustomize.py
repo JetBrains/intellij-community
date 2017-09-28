16d7fa2ede39c5ae005814f5a9c11cd50c2aa3f4
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2017-09-28 15:41:41 +0300

    fix the problem on python 2 with empty getfilesystemdefaultencoding

--- python/helpers/pycharm_matplotlib_backend/sitecustomize.py
+++ python/helpers/pycharm_matplotlib_backend/sitecustomize.py
@@ -1,23 +1,40 @@
-import matplotlib
-matplotlib.use('module://backend_interagg')
+try:
+    import sys
 
-# We want to import users sitecustomize.py file if any
-import sys
-import os
+    old_getfilesystemencoding = None
+    if not sys.getfilesystemencoding():
+        old_getfilesystemencoding = sys.getfilesystemencoding
+        sys.getfilesystemencoding = lambda: 'UTF-8'
 
-sitecustomize = "sitecustomize"
-parent_dir = os.path.abspath(os.path.join(__file__, os.pardir))
-if parent_dir in sys.path:
-    sys.path.remove(parent_dir)
+    import matplotlib
 
-    if sitecustomize in sys.modules:
-        pycharm_sitecustomize_module = sys.modules.pop(sitecustomize)
+    if old_getfilesystemencoding:
+        sys.getfilesystemencoding = old_getfilesystemencoding
+    matplotlib.use('module://backend_interagg')
 
-        try:
-            import sitecustomize
-        except ImportError:
-            # return our module if we failed to find any other sitecustomize
-            # to prevent KeyError importing 'site.py'
-            sys.modules[sitecustomize] = pycharm_sitecustomize_module
+    # We want to import users sitecustomize.py file if any
+    import os
 
-    sys.path.append(parent_dir)
+    sitecustomize = "sitecustomize"
+    parent_dir = os.path.abspath(os.path.join(__file__, os.pardir))
+    if parent_dir in sys.path:
+        sys.path.remove(parent_dir)
+
+        if sitecustomize in sys.modules:
+            pycharm_sitecustomize_module = sys.modules.pop(sitecustomize)
+
+            try:
+                import sitecustomize
+            except ImportError:
+                # return our module if we failed to find any other sitecustomize
+                # to prevent KeyError importing 'site.py'
+                sys.modules[sitecustomize] = pycharm_sitecustomize_module
+
+        sys.path.append(parent_dir)
+except StandardError:
+    # fallback in case matplotlib is not loaded correctly
+    import sys
+
+    for key in sys.modules.keys():
+        if key.startswith("matplotlib"):
+            sys.modules.pop(key)
