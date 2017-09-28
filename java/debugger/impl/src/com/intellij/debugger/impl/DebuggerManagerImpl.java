0012b704cd546015229bab02400e7561cb9feabc
Egor.Ushakov <egor.ushakov@jetbrains.com>
2017-09-28 16:35:31 +0300

    debugger agent: support installed versions

--- java/debugger/impl/src/com/intellij/debugger/impl/DebuggerManagerImpl.java
+++ java/debugger/impl/src/com/intellij/debugger/impl/DebuggerManagerImpl.java
@@ -1,18 +1,6 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o.
+// Use of this source code is governed by the Apache 2.0 license that can be
+// found in the LICENSE file.
 package com.intellij.debugger.impl;
 
 import com.intellij.debugger.*;
@@ -49,7 +37,6 @@ import com.intellij.openapi.projectRoots.ex.JavaSdkUtil;
 import com.intellij.openapi.startup.StartupManager;
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.util.WriteExternalException;
-import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -469,17 +456,28 @@ public class DebuggerManagerImpl extends DebuggerManagerEx implements Persistent
 
     ApplicationManager.getApplication().runReadAction(() -> {
       JavaSdkUtil.addRtJar(parameters.getClassPath());
+
       if (Registry.is("debugger.capture.points.agent")) {
-        String path = PathUtil.getJarPathForClass(DebuggerManagerImpl.class);
-        //TODO: for now works only in debug mode
-        String agent = "-javaagent:" +
-                       FileUtil.toSystemDependentName(PathUtil.getParentPath(PathUtil.getParentPath(path))) +
-                       "/artifacts/debugger_agent/debugger-agent.jar";
-        if (Registry.is("debugger.capture.points.agent.debug")) {
-          agent += "=debug";
+        File classesRoot = new File(PathUtil.getJarPathForClass(DebuggerManagerImpl.class));
+        String agentName = "debugger-agent.jar";
+        File agentFile;
+        if (classesRoot.isFile()) {
+          agentFile = new File(classesRoot.getParentFile(), "rt/" + agentName);
         }
-        if (!parameters.getVMParametersList().hasParameter(agent)) {
-          parameters.getVMParametersList().add(agent);
+        else {
+          agentFile = new File(classesRoot.getParentFile().getParentFile(), "/artifacts/debugger_agent/" + agentName);
+        }
+        if (agentFile.exists()) {
+          String agent = "-javaagent:" + agentFile;
+          if (Registry.is("debugger.capture.points.agent.debug")) {
+            agent += "=debug";
+          }
+          if (!parameters.getVMParametersList().hasParameter(agent)) {
+            parameters.getVMParametersList().add(agent);
+          }
+        }
+        else {
+          LOG.warn("Capture agent not found: " + agentFile);
         }
       }
 
