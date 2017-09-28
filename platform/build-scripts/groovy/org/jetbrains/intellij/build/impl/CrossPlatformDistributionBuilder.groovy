399b064ba6fd3408c6dc0dca85603328e39b50ca
Alexander Zolotov <goldifit@gmail.com>
2017-09-28 19:21:32 +0300

    Exclude help from cross-platform distributions

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/CrossPlatformDistributionBuilder.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/CrossPlatformDistributionBuilder.groovy
@@ -1,18 +1,6 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o.
+// Use of this source code is governed by the Apache 2.0 license that can be
+// found in the LICENSE file.
 package org.jetbrains.intellij.build.impl
 
 import com.intellij.openapi.util.io.FileUtil
@@ -76,6 +64,7 @@ class CrossPlatformDistributionBuilder {
           exclude(name: "bin/*.exe.vmoptions")
           exclude(name: "bin/${executableName}*.exe")
           exclude(name: "bin/idea.properties")
+          exclude(name: "help/**")
         }
         zipfileset(dir: "$winDistPath/bin", prefix: "bin/win") {
           include(name: "fsnotifier*.exe")
@@ -124,6 +113,10 @@ class CrossPlatformDistributionBuilder {
 
           buildContext.macDistributionCustomizer.extraExecutables.each {
             exclude(name: it)
+          }
+
+          if (buildContext.macDistributionCustomizer.helpId) {
+            exclude(name: "Resources/${buildContext.macDistributionCustomizer.helpId}.help/**")
           }
         }
         if (!buildContext.macDistributionCustomizer.extraExecutables.isEmpty()) {
