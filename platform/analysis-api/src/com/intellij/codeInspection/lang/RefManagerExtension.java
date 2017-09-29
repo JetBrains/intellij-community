0c19a4c6958d9858ff9470bc82df02659c6c54f6
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2017-09-29 13:46:37 +0300

    inspection view: allow specify container element for every inspection for given language (IDEA-179773)

--- platform/analysis-api/src/com/intellij/codeInspection/lang/RefManagerExtension.java
+++ platform/analysis-api/src/com/intellij/codeInspection/lang/RefManagerExtension.java
@@ -1,27 +1,15 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.codeInspection.lang;
 
+import com.intellij.codeInspection.LocalInspectionTool;
 import com.intellij.codeInspection.reference.RefElement;
 import com.intellij.codeInspection.reference.RefEntity;
 import com.intellij.codeInspection.reference.RefVisitor;
 import com.intellij.lang.Language;
 import com.intellij.openapi.util.Key;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiNamedElement;
 import org.jdom.Element;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -41,6 +29,18 @@ public interface RefManagerExtension<T> {
 
   @Nullable
   RefElement createRefElement(PsiElement psiElement);
+
+  /**
+   * The method finds problem container (ex: method, class, file) that used to be shown as inspection view tree node.
+   * If method returns not null value then {@link LocalInspectionTool#getProblemElement(PsiElement)} will be ignored.
+   *
+   * @param psiElement
+   * @return container element for given psiElement
+   */
+  @Nullable
+  default PsiNamedElement getElementContainer(@NotNull PsiElement psiElement) {
+    return null;
+  }
 
   @Nullable
   RefEntity getReference(final String type, final String fqName);
