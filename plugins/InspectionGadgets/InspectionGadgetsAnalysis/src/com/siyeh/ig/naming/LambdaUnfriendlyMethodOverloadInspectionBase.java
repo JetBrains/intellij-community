6102359903d020334710bd47b9d3942d4160638b
Bas Leijdekkers <basleijdekkers@gmail.com>
2017-09-29 14:32:05 +0200

    IG: Fix "Lambda-unfriendly overload" false positive (IDEA-166385)

--- plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/naming/LambdaUnfriendlyMethodOverloadInspectionBase.java
+++ plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/naming/LambdaUnfriendlyMethodOverloadInspectionBase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -91,7 +91,7 @@ public class LambdaUnfriendlyMethodOverloadInspectionBase extends BaseInspection
         final PsiType functionalType = parameters[functionalIndex].getType();
         if (!areOtherParameterTypesConvertible(parameters, otherParameters, functionalIndex) ||
             !LambdaUtil.isFunctionalType(otherFunctionalType) ||
-            Objects.equals(functionalType, otherFunctionalType)) {
+            Objects.equals(((PsiClassType)functionalType).rawType(), ((PsiClassType)otherFunctionalType).rawType())) {
           continue;
         }
 
