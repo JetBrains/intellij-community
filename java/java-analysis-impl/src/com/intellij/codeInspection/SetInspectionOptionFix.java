c8ae5ff3dc63f502d23c8aabbc0015972c824925
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2017-09-29 18:35:29 +0700

    SetInspectionOptionFix: cancel icon; undoable

--- java/java-analysis-impl/src/com/intellij/codeInspection/SetInspectionOptionFix.java
+++ java/java-analysis-impl/src/com/intellij/codeInspection/SetInspectionOptionFix.java
@@ -4,12 +4,19 @@ package com.intellij.codeInspection;
 import com.intellij.codeInsight.intention.LowPriorityAction;
 import com.intellij.codeInspection.ex.InspectionProfileModifiableModelKt;
 import com.intellij.codeInspection.ex.InspectionToolWrapper;
+import com.intellij.icons.AllIcons;
+import com.intellij.openapi.command.undo.BasicUndoableAction;
+import com.intellij.openapi.command.undo.UndoManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Iconable;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.ReflectionUtil;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 
-public class SetInspectionOptionFix implements LocalQuickFix, LowPriorityAction {
+import javax.swing.*;
+
+public class SetInspectionOptionFix implements LocalQuickFix, LowPriorityAction, Iconable {
   private String myShortName;
   private final String myProperty;
   private final String myMessage;
@@ -37,12 +44,38 @@ public class SetInspectionOptionFix implements LocalQuickFix, LowPriorityAction 
   }
 
   @Override
+  public boolean startInWriteAction() {
+    return false;
+  }
+
+  @Override
   public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
+    setOption(project, myValue);
+    final VirtualFile vFile = descriptor.getPsiElement().getContainingFile().getVirtualFile();
+    UndoManager.getInstance(project).undoableActionPerformed(new BasicUndoableAction(vFile) {
+      @Override
+      public void undo() {
+        setOption(project, !myValue);
+      }
+
+      @Override
+      public void redo() {
+        setOption(project, myValue);
+      }
+    });
+  }
+
+  private void setOption(@NotNull Project project, boolean value) {
     InspectionProfileModifiableModelKt.modifyAndCommitProjectProfile(project, model -> {
       InspectionToolWrapper tool = model.getInspectionTool(myShortName, project);
       if(tool == null) return;
       InspectionProfileEntry inspection = tool.getTool();
-      ReflectionUtil.setField(inspection.getClass(), inspection, boolean.class, myProperty, myValue);
+      ReflectionUtil.setField(inspection.getClass(), inspection, boolean.class, myProperty, value);
     });
+  }
+
+  @Override
+  public Icon getIcon(int flags) {
+    return AllIcons.Actions.Cancel;
   }
 }
