e2ce2ac809185969b8449cc1a05835be67c0c6b7
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2017-10-02 17:40:17 +0300

    FIle moved to different package

--- python/src/com/jetbrains/python/PyClassTreeChooserDialog.kt
+++ python/src/com/jetbrains/python/PyClassTreeChooserDialog.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.jetbrains.python.debugger
+package com.jetbrains.python
 
 import com.google.common.collect.Lists
 import com.intellij.ide.util.AbstractTreeClassChooserDialog
