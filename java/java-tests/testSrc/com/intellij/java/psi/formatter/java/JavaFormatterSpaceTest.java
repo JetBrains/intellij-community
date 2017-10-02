ba1c99f43d197c7bfa9d3c1061ff458f743f21e1
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2017-10-02 12:55:47 +0200

    Cleanup (formatting)

--- java/java-tests/testSrc/com/intellij/java/psi/formatter/java/JavaFormatterSpaceTest.java
+++ java/java-tests/testSrc/com/intellij/java/psi/formatter/java/JavaFormatterSpaceTest.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.java.psi.formatter.java;
 
 import com.intellij.psi.codeStyle.CommonCodeStyleSettings;
@@ -24,7 +10,6 @@ import com.intellij.psi.codeStyle.CommonCodeStyleSettings;
  * @since Apr 29, 2010 5:50:34 PM
  */
 public class JavaFormatterSpaceTest extends AbstractJavaFormatterTest {
-
   public void testSpacingBetweenTypeParameters() {
     // Implied by IDEADEV-3666
     getSettings().SPACE_AFTER_COMMA = true;
@@ -150,6 +135,7 @@ public class JavaFormatterSpaceTest extends AbstractJavaFormatterTest {
     doTextTest(initial, formatted); // Expect spaces to be inserted after unary operators
   }
 
+  @SuppressWarnings("unused")
   public void _testJavadocMethodParams() {
     // Inspired by IDEA-42167
     // Disabled because the contents of the {@code tag} is not necessarily Java code and
@@ -652,7 +638,7 @@ public class JavaFormatterSpaceTest extends AbstractJavaFormatterTest {
       "int x = (1 + 2 + 3) * (1 + 2 + 2) * (1 + 2);"
     );
   }
-  
+
   public void testKeepFinalOnLine() {
     doClassTest(
       "public    static void bar(@NonNls final String[] args) {\n" +
@@ -684,5 +670,4 @@ public class JavaFormatterSpaceTest extends AbstractJavaFormatterTest {
       "    foo(100 , 200);\n" +
       "}");
   }
-  
 }
