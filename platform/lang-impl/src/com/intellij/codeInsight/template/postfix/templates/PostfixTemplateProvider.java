855f0fa56b3369e75baa9cbb73af6bda523cb421
Alexander Zolotov <goldifit@gmail.com>
2017-10-04 20:50:51 +0300

    Reformat

--- platform/lang-impl/src/com/intellij/codeInsight/template/postfix/templates/PostfixTemplateProvider.java
+++ platform/lang-impl/src/com/intellij/codeInsight/template/postfix/templates/PostfixTemplateProvider.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.template.postfix.templates;
 
 
@@ -23,7 +9,6 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Set;
 
 public interface PostfixTemplateProvider {
-
   /**
    * Return all templates registered in the provider
    */
@@ -54,7 +39,7 @@ public interface PostfixTemplateProvider {
 
   /**
    * Prepare file for checking availability of templates.
-   * Almost the same as {@link this#preExpand(com.intellij.psi.PsiFile, com.intellij.openapi.editor.Editor)} with several differences:
+   * Almost the same as {@link this#preExpand(PsiFile, Editor)} with several differences:
    * 1. Processes copy of file. So implementations can modify it without corrupting the real file.
    * 2. Could be invoked from anywhere (EDT, write-action, read-action, completion-thread etc.). So implementations should make 
    * additional effort to make changes in file.
