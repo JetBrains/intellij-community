68adc5148106f6f8c129b46598797388f04efbfe
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2017-10-04 18:04:50 +0300

    PY-22324 Removed SDK name shorten procedure in favor or just shorter names

--- python/src/com/jetbrains/python/sdk/PySdkListCellRenderer.java
+++ python/src/com/jetbrains/python/sdk/PySdkListCellRenderer.java
@@ -20,29 +20,20 @@ import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.projectRoots.SdkModificator;
 import com.intellij.openapi.projectRoots.SdkType;
 import com.intellij.openapi.util.IconLoader;
-import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.ui.LayeredIcon;
 import com.intellij.ui.ListCellRendererWrapper;
 import com.jetbrains.python.sdk.flavors.PythonSdkFlavor;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
-import java.io.File;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class PySdkListCellRenderer extends ListCellRendererWrapper<Object> {
   private final String myNullText;
   private final Map<Sdk, SdkModificator> mySdkModifiers;
   public static final String SEPARATOR = "separator";
 
-  final Pattern PYTHON_PATTERN = Pattern.compile("(\\d\\.?\\d\\.?\\d?)[ ]*\\(([^\\(\\)]*)\\)|(\\d\\.?\\d\\.?\\d?)[ ]*([^\\(\\)]*)");
-  private boolean isShortVersion;
-
-  public PySdkListCellRenderer(boolean shortVersion) {
-    isShortVersion = shortVersion;
+  public PySdkListCellRenderer() {
     myNullText = "";
     mySdkModifiers = null;
   }
@@ -65,10 +56,6 @@ public class PySdkListCellRenderer extends ListCellRendererWrapper<Object> {
       }
       else {
         name = sdk.getName();
-      }
-
-      if (isShortVersion){
-        name = shortenName(name);
       }
 
       if (PythonSdkType.isInvalid(sdk)) {
@@ -97,28 +84,6 @@ public class PySdkListCellRenderer extends ListCellRendererWrapper<Object> {
       setSeparator();
     else if (item == null)
       setText(myNullText);
-  }
-
-  private String shortenName(@NotNull String name) {
-    final Matcher matcher = PYTHON_PATTERN.matcher(name);
-    if (matcher.matches()) {
-      String path = matcher.group(2);
-      if (path != null) {
-        name = matcher.group(1) + " at " + path;
-      }
-      else {
-        path = matcher.group(4);
-        final int index = path.lastIndexOf(File.separator);
-        if (index > 0) {
-          path = path.substring(index);
-        }
-        name = matcher.group(3) + " at ..." + path;
-      }
-    }
-    else if (new File(name).exists()) {
-      name = FileUtil.getLocationRelativeToUserHome(name);
-    }
-    return name;
   }
 
   private static LayeredIcon wrapIconWithWarningDecorator(Icon icon) {
