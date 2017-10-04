0515ef67368da54b31e396e6cd022b7302ce954b
Egor.Ushakov <egor.ushakov@jetbrains.com>
2017-10-04 16:31:00 +0300

    debugger agent: handle asm issues

--- java/debugger/debugger-agent/src/com/intellij/rt/debugger/agent/CaptureAgent.java
+++ java/debugger/debugger-agent/src/com/intellij/rt/debugger/agent/CaptureAgent.java
@@ -66,9 +66,23 @@ public class CaptureAgent {
         }
       }
 
-      instrumentation.appendToSystemClassLoaderSearch(new JarFile(asmPath));
+      try {
+        instrumentation.appendToSystemClassLoaderSearch(new JarFile(asmPath));
+      }
+      catch (Exception e) {
+        String report = "Capture agent: unable to use the provided asm lib";
+        try {
+          Class.forName("org.jetbrains.org.objectweb.asm.MethodVisitor");
+          System.out.println(report + ", will use asm from the classpath");
+        }
+        catch (ClassNotFoundException e1) {
+          System.out.println(report + ", exiting");
+          return;
+        }
+      }
 
       instrumentation.addTransformer(new CaptureTransformer());
+
       for (Class aClass : instrumentation.getAllLoadedClasses()) {
         String name = aClass.getName().replaceAll("\\.", "/");
         if (myCapturePoints.containsKey(name) || myInsertPoints.containsKey(name)) {
