5218fd64b115deac09528c8e881f8cd5430155be
Alexander Zolotov <goldifit@gmail.com>
2017-10-05 15:36:53 +0300

    Remove pluginId from generated build.gradle, generate id-tag in plugin.xml (IDEA-180083)

--- plugins/gradle/src/org/jetbrains/plugins/gradle/frameworkSupport/GradleJavaFrameworkSupportProvider.java
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/frameworkSupport/GradleJavaFrameworkSupportProvider.java
@@ -1,23 +1,10 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.gradle.frameworkSupport;
 
 import com.intellij.framework.FrameworkTypeEx;
 import com.intellij.framework.addSupport.FrameworkSupportInModuleProvider;
 import com.intellij.icons.AllIcons;
+import com.intellij.openapi.externalSystem.model.project.ProjectId;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.roots.ModifiableModelsProvider;
 import com.intellij.openapi.roots.ModifiableRootModel;
@@ -58,7 +45,8 @@ public class GradleJavaFrameworkSupportProvider extends GradleFrameworkSupportPr
   }
 
   @Override
-  public void addSupport(@NotNull Module module,
+  public void addSupport(@NotNull ProjectId projectId,
+                         @NotNull Module module,
                          @NotNull ModifiableRootModel rootModel,
                          @NotNull ModifiableModelsProvider modifiableModelsProvider,
                          @NotNull BuildScriptDataBuilder buildScriptData) {
