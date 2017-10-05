2f4dd5c73d88746ee4f514c8262fb9e2fb9dd736
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2017-10-05 14:42:21 +0300

    Support gitlab archives
    PY-26364 fixed.

--- python/openapi/src/com/jetbrains/python/packaging/PyRequirement.java
+++ python/openapi/src/com/jetbrains/python/packaging/PyRequirement.java
@@ -1,18 +1,16 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.jetbrains.python.packaging;
 
 import com.intellij.openapi.editor.Document;
@@ -60,6 +58,10 @@ public class PyRequirement {
   @NotNull
   private static final Pattern GITHUB_ARCHIVE_URL =
     Pattern.compile("https?://github\\.com/[^/\\s]+/(?<" + NAME_GROUP + ">[^/\\s]+)/archive/\\S+" + COMMENT_REGEXP);
+
+  @NotNull
+  private static final Pattern GITLAB_ARCHIVE_URL =
+    Pattern.compile("https?://gitlab\\.com/[^/\\s]+/(?<" + NAME_GROUP + ">[^/\\s]+)/repository/\\S+" + COMMENT_REGEXP);
 
   @NotNull
   private static final Pattern ARCHIVE_URL =
@@ -309,9 +311,14 @@ public class PyRequirement {
 
   @Nullable
   public static PyRequirement fromLine(@NotNull String line) {
-    final PyRequirement githubArchiveUrl = parseGithubArchiveUrl(line);
+    final PyRequirement githubArchiveUrl = parseGitArchiveUrl(GITHUB_ARCHIVE_URL, line);
     if (githubArchiveUrl != null) {
       return githubArchiveUrl;
+    }
+
+    final PyRequirement gitlabArchiveUrl = parseGitArchiveUrl(GITLAB_ARCHIVE_URL, line);
+    if (gitlabArchiveUrl != null) {
+      return gitlabArchiveUrl;
     }
 
     final PyRequirement archiveUrl = parseArchiveUrl(line);
@@ -349,8 +356,8 @@ public class PyRequirement {
   }
 
   @Nullable
-  private static PyRequirement parseGithubArchiveUrl(@NotNull String line) {
-    final Matcher matcher = GITHUB_ARCHIVE_URL.matcher(line);
+  private static PyRequirement parseGitArchiveUrl(@NotNull Pattern pattern, @NotNull String line) {
+    final Matcher matcher = pattern.matcher(line);
 
     if (matcher.matches()) {
       return new PyRequirement(matcher.group(NAME_GROUP), Collections.emptyList(), Collections.singletonList(dropComments(line, matcher)));
