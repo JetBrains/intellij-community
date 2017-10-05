6d111aa8edfb212f77a305847c36cee1ab78821a
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2017-10-05 23:44:03 +0300

    [groovy] support @Immutable#copyWith (IDEA-125182)

---
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/transformations/immutable/immutable.kt
@@ -0,0 +1,6 @@
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.plugins.groovy.transformations.immutable
+
+internal val immutableCopyWithKind = "@Immutable#copyWith"
+internal val immutableOrigin = "by @Immutable"
+internal val immutableCopyWith = "copyWith"
