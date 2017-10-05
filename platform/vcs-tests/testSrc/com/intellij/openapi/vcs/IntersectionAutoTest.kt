612db9c8bc72e6b6e6b062df1b395e08a9bf56fb
Aleksey Pivovarov <ampivovarov@gmail.com>
2017-10-05 16:48:35 +0300

    vcs: update tests for StepIntersection

---
+++ platform/vcs-tests/testSrc/com/intellij/openapi/vcs/IntersectionAutoTest.kt
@@ -0,0 +1,73 @@
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.vcs
+
+import com.intellij.diff.DiffTestCase
+import com.intellij.openapi.util.TextRange
+import com.intellij.openapi.vcs.checkin.StepIntersection
+
+class IntersectionAutoTest : DiffTestCase() {
+  val RUNS = 100000
+  val LIST_SIZE = 25
+  val RANGE_SIZE = 20
+
+  fun testIntersection() {
+    doAutoTest(System.currentTimeMillis(), RUNS) {
+      val list1 = generateList(LIST_SIZE)
+      val list2 = generateList(LIST_SIZE)
+
+      val result1 = mutableListOf<Pair<TextRange, TextRange>>()
+      val result2 = mutableListOf<Pair<TextRange, TextRange>>()
+      StepIntersection.processIntersections(list1, list2, { it }, { it }, { it1, it2 -> result1.add(Pair(it1, it2)) })
+      dumbProcessIntersections(list1, list2, { it }, { it }, { it1, it2 -> result2.add(Pair(it1, it2)) })
+
+      if (result1 != result2) {
+        val x1 = result1.map { it.first }.toSet()
+        val x2 = result2.map { it.first }.toSet()
+
+        if (x1 != x2) { // TODO: some <Data, Area> can be ignored
+          println(list1.joinToString { "${it.startOffset}, ${it.endOffset}" })
+          println(list2.joinToString { "${it.startOffset}, ${it.endOffset}" })
+          println(result1)
+          println(result2)
+
+          assertEquals(x1, x2)
+        }
+      }
+    }
+  }
+
+  private fun generateList(maxLength: Int): List<TextRange> {
+    val result = mutableListOf<TextRange>()
+    val length = RNG.nextInt(maxLength)
+
+    var oldEnd = 0
+    for (i in 1..length) {
+      val range = generateRange(oldEnd, RANGE_SIZE, RANGE_SIZE)
+      result += range
+      oldEnd = range.endOffset + 1
+    }
+    return result
+  }
+
+  private fun generateRange(minStart: Int, maxOffset: Int, maxLength: Int): TextRange {
+    val start = minStart + RNG.nextInt(maxOffset)
+    val end = start + RNG.nextInt(maxLength) + 1
+    return TextRange(start, end)
+  }
+
+  private fun <T, V> dumbProcessIntersections(elements1: List<T>,
+                                              elements2: List<V>,
+                                              convertor1: (T) -> TextRange,
+                                              convertor2: (V) -> TextRange,
+                                              intersectionConsumer: (T, V) -> Unit) {
+    for (item1 in elements1) {
+      for (item2 in elements2) {
+        val range1 = convertor1(item1)
+        val range2 = convertor2(item2)
+        if (range1.intersects(range2)) {
+          intersectionConsumer(item1, item2)
+        }
+      }
+    }
+  }
+}
