7681193874381417b8d4ad48e07cd3574daf82e1
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2017-10-05 16:13:33 +0300

    Don't replace return type with receiver type if they have same class (PY-26269)

--- python/src/com/jetbrains/python/psi/impl/PyFunctionImpl.java
+++ python/src/com/jetbrains/python/psi/impl/PyFunctionImpl.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.python.psi.impl;
 
 import com.intellij.lang.ASTNode;
@@ -320,10 +306,16 @@ public class PyFunctionImpl extends PyBaseElementImpl<PyFunctionStub> implements
       // TODO: Currently we substitute only simple subclass types, but we could handle union and collection types as well
       if (returnType instanceof PyClassType) {
         final PyClassType returnClassType = (PyClassType)returnType;
+
         if (returnClassType.getPyClass() == getContainingClass()) {
           final PyType receiverType = context.getType(receiver);
-          if (receiverType instanceof PyClassType && PyTypeChecker.match(returnType, receiverType, context)) {
-            return returnClassType.isDefinition() ? receiverType : ((PyClassType)receiverType).toInstance();
+
+          if (receiverType instanceof PyClassType) {
+            final PyClassType receiverClassType = (PyClassType)receiverType;
+
+            if (receiverClassType.getPyClass() != returnClassType.getPyClass() && PyTypeChecker.match(returnType, receiverType, context)) {
+              return returnClassType.isDefinition() ? receiverType : receiverClassType.toInstance();
+            }
           }
         }
       }
