JetBrains/intellij-community

commit 06db4ef21520ee95b50004d964721c67abb85f94

vcs: do not use DataContext to expand DefaultActionGroup

platform/vcs-impl/src/com/intellij/openapi/vcs/changes/IgnoredViewDialog.java
@@ -31,7 +31,7 @@ public class IgnoredViewDialog extends SpecificFilesViewDialog {
   }
 
   @Override
-  protected void addCustomActions(@NotNull DefaultActionGroup group, @NotNull ActionToolbar actionToolbar) {
+  protected void addCustomActions(@NotNull DefaultActionGroup group) {
     ActionManager actionManager = ActionManager.getInstance();
     AnAction deleteAction =
       EmptyAction.registerWithShortcutSet("ChangesView.DeleteUnversioned.From.Dialog", CommonShortcuts.getDelete(), myView);

platform/vcs-impl/src/com/intellij/openapi/vcs/changes/SpecificFilesViewDialog.java
@@ -109,7 +109,7 @@ abstract class SpecificFilesViewDialog extends DialogWrapper {
     final DefaultActionGroup group = new DefaultActionGroup();
     final ActionToolbar actionToolbar = ActionManager.getInstance().createActionToolbar("SPECIFIC_FILES_DIALOG", group, true);
 
-    addCustomActions(group, actionToolbar);
+    addCustomActions(group);
 
     final CommonActionsManager cam = CommonActionsManager.getInstance();
     final Expander expander = new Expander();
@@ -123,7 +123,7 @@ abstract class SpecificFilesViewDialog extends DialogWrapper {
     myView.setShowFlatten(false);
   }
 
-  protected void addCustomActions(@NotNull DefaultActionGroup group, @NotNull ActionToolbar actionToolbar) {
+  protected void addCustomActions(@NotNull DefaultActionGroup group) {
   }
 
   @Override

platform/vcs-impl/src/com/intellij/openapi/vcs/changes/UnversionedViewDialog.java
@@ -16,17 +16,15 @@
 package com.intellij.openapi.vcs.changes;
 
 import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.actionSystem.ex.ActionUtil;
 import com.intellij.openapi.actionSystem.ex.AnActionListener;
-import com.intellij.openapi.actionSystem.impl.PresentationFactory;
-import com.intellij.openapi.actionSystem.impl.Utils;
-import com.intellij.openapi.application.impl.LaterInvocator;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vcs.changes.ui.ChangesListView;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UnversionedViewDialog extends SpecificFilesViewDialog {
@@ -39,8 +37,11 @@ public class UnversionedViewDialog extends SpecificFilesViewDialog {
   }
 
   @Override
-  protected void addCustomActions(@NotNull DefaultActionGroup group, @NotNull ActionToolbar actionToolbar) {
-    List<AnAction> actions = registerUnversionedActionsShortcuts(actionToolbar.getToolbarDataContext(), myView);
+  protected void addCustomActions(@NotNull DefaultActionGroup group) {
+    registerUnversionedActionsShortcuts(myView);
+
+    List<AnAction> actions = new ArrayList<>();
+    actions.add(getUnversionedActionGroup());
     // special shortcut for deleting a file
     actions.add(myDeleteActionWithCustomShortcut =
                   EmptyAction.registerWithShortcutSet("ChangesView.DeleteUnversioned.From.Dialog", CommonShortcuts.getDelete(), myView));
@@ -74,17 +75,8 @@ public class UnversionedViewDialog extends SpecificFilesViewDialog {
     return (ActionGroup)ActionManager.getInstance().getAction("Unversioned.Files.Dialog");
   }
 
-  @NotNull
-  public static List<AnAction> registerUnversionedActionsShortcuts(@NotNull DataContext dataContext, @NotNull JComponent component) {
-    ActionManager manager = ActionManager.getInstance();
-    List<AnAction> actions = ContainerUtil.newArrayList();
-
-    Utils.expandActionGroup(LaterInvocator.isInModalContext(), getUnversionedActionGroup(), actions, new PresentationFactory(), dataContext, "", manager);
-    for (AnAction action : actions) {
-      action.registerCustomShortcutSet(action.getShortcutSet(), component);
-    }
-
-    return actions;
+  public static void registerUnversionedActionsShortcuts(@NotNull JComponent component) {
+    ActionUtil.recursiveRegisterShortcutSet(getUnversionedActionGroup(), component, null);
   }
 
   @NotNull

platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/MultipleLocalChangeListsBrowser.java
@@ -18,7 +18,6 @@ package com.intellij.openapi.vcs.changes.ui;
 import com.intellij.diff.chains.DiffRequestChain;
 import com.intellij.diff.util.DiffUserDataKeysEx;
 import com.intellij.icons.AllIcons;
-import com.intellij.ide.DataManager;
 import com.intellij.ide.DeleteProvider;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.actionSystem.*;
@@ -113,10 +112,9 @@ public class MultipleLocalChangeListsBrowser extends CommitDialogChangesBrowser 
     if (myEnableUnversioned) {
       result.add(new ShowHideUnversionedFilesAction());
 
-      // TODO: do not access DataContext
       // We do not add "Delete" key shortcut for deleting unversioned files as this shortcut is already used to uncheck checkboxes in the tree.
       result.add(UnversionedViewDialog.getUnversionedActionGroup());
-      UnversionedViewDialog.registerUnversionedActionsShortcuts(DataManager.getInstance().getDataContext(this), myViewer);
+      UnversionedViewDialog.registerUnversionedActionsShortcuts(myViewer);
     }
     else {
       // avoid duplicated actions on toolbar

commit 075346426177675a1ebc5d129cc0485c5586d3da

vcs: listen for CLM events rather than for specific AnAction invocations
Refresh was broken in 06db4ef21520ee95b50004d964721c67abb85f94
as passed ActionGroup is no longer manually expanded.

platform/vcs-impl/src/com/intellij/openapi/vcs/changes/IgnoredViewDialog.java
@@ -15,8 +15,10 @@
  */
 package com.intellij.openapi.vcs.changes;
 
-import com.intellij.openapi.actionSystem.*;
-import com.intellij.openapi.actionSystem.ex.AnActionListener;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.CommonShortcuts;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.openapi.actionSystem.EmptyAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vcs.changes.ui.ChangesListView;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -32,17 +34,8 @@ public class IgnoredViewDialog extends SpecificFilesViewDialog {
 
   @Override
   protected void addCustomActions(@NotNull DefaultActionGroup group) {
-    ActionManager actionManager = ActionManager.getInstance();
     AnAction deleteAction =
       EmptyAction.registerWithShortcutSet("ChangesView.DeleteUnversioned.From.Dialog", CommonShortcuts.getDelete(), myView);
-    actionManager.addAnActionListener(new AnActionListener.Adapter() {
-      @Override
-      public void afterActionPerformed(AnAction action, DataContext dataContext, AnActionEvent event) {
-        if (action.equals(deleteAction)) {
-          refreshView();
-        }
-      }
-    }, myDisposable);
     group.add(deleteAction);
     myView.setMenuActions(new DefaultActionGroup(deleteAction));
   }

platform/vcs-impl/src/com/intellij/openapi/vcs/changes/SpecificFilesViewDialog.java
@@ -20,16 +20,17 @@ import com.intellij.ide.CommonActionsManager;
 import com.intellij.ide.TreeExpander;
 import com.intellij.ide.util.treeView.TreeState;
 import com.intellij.openapi.actionSystem.*;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.vcs.VcsBundle;
 import com.intellij.openapi.vcs.changes.ui.ChangesBrowserNode;
 import com.intellij.openapi.vcs.changes.ui.ChangesListView;
 import com.intellij.openapi.vcs.changes.ui.TreeModelBuilder;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.ui.GuiUtils;
 import com.intellij.ui.ScrollPaneFactory;
 import com.intellij.util.EditSourceOnDoubleClickHandler;
 import com.intellij.util.EditSourceOnEnterKeyHandler;
@@ -48,7 +49,6 @@ abstract class SpecificFilesViewDialog extends DialogWrapper {
   protected JPanel myPanel;
   protected final ChangesListView myView;
   protected final ChangeListManager myChangeListManager;
-  protected boolean myInRefresh;
   protected final Project myProject;
 
   protected SpecificFilesViewDialog(@NotNull Project project,
@@ -81,6 +81,16 @@ abstract class SpecificFilesViewDialog extends DialogWrapper {
     init();
     initData(initDataFiles);
     myView.setMinimumSize(new JBDimension(100, 100));
+
+
+    ChangeListAdapter changeListListener = new ChangeListAdapter() {
+      @Override
+      public void changeListUpdateDone() {
+        refreshView();
+      }
+    };
+    ChangeListManager.getInstance(myProject).addChangeListListener(changeListListener);
+    Disposer.register(myDisposable, () -> ChangeListManager.getInstance(myProject).removeChangeListListener(changeListListener));
   }
 
 
@@ -158,19 +168,11 @@ abstract class SpecificFilesViewDialog extends DialogWrapper {
   }
 
   protected void refreshView() {
-    ApplicationManager.getApplication().assertIsDispatchThread();
-
-    if (myInRefresh) return;
-    myInRefresh = true;
-
-    myChangeListManager.invokeAfterUpdate(() -> {
-      try {
+    GuiUtils.invokeLaterIfNeeded(() -> {
+      if (isVisible()) {
         initData(getFiles());
       }
-      finally {
-        myInRefresh = false;
-      }
-    }, InvokeAfterUpdateMode.BACKGROUND_NOT_CANCELLABLE, "", ModalityState.current());
+    }, ModalityState.stateForComponent(myView));
   }
 
   @NotNull

platform/vcs-impl/src/com/intellij/openapi/vcs/changes/UnversionedViewDialog.java
@@ -17,14 +17,12 @@ package com.intellij.openapi.vcs.changes;
 
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.actionSystem.ex.ActionUtil;
-import com.intellij.openapi.actionSystem.ex.AnActionListener;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vcs.changes.ui.ChangesListView;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UnversionedViewDialog extends SpecificFilesViewDialog {
@@ -38,28 +36,13 @@ public class UnversionedViewDialog extends SpecificFilesViewDialog {
   protected void addCustomActions(@NotNull DefaultActionGroup group) {
     registerUnversionedActionsShortcuts(myView);
 
-    List<AnAction> actions = new ArrayList<>();
-    actions.add(getUnversionedActionGroup());
-    // special shortcut for deleting a file
-    actions.add(EmptyAction.registerWithShortcutSet("ChangesView.DeleteUnversioned.From.Dialog", CommonShortcuts.getDelete(), myView));
+    EmptyAction.registerWithShortcutSet("ChangesView.DeleteUnversioned.From.Dialog", CommonShortcuts.getDelete(), myView);
 
-    refreshViewAfterActionPerformed(actions);
     group.add(getUnversionedActionGroup());
     final DefaultActionGroup secondGroup = new DefaultActionGroup();
     secondGroup.addAll(getUnversionedActionGroup());
 
     myView.setMenuActions(secondGroup);
-  }
-
-  private void refreshViewAfterActionPerformed(@NotNull final List<AnAction> actions) {
-    ActionManager.getInstance().addAnActionListener(new AnActionListener.Adapter() {
-      @Override
-      public void afterActionPerformed(AnAction action, DataContext dataContext, AnActionEvent event) {
-        if (actions.contains(action)) {
-          refreshView();
-        }
-      }
-    }, myDisposable);
   }
 
   @NotNull