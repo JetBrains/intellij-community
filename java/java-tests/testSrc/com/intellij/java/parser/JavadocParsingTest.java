aec34583a3871f837c706c8f99304fdf867426a6
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2017-10-05 18:03:05 +0200

    [java] parses references in @uses and @provides tags

--- java/java-tests/testSrc/com/intellij/java/parser/JavadocParsingTest.java
+++ java/java-tests/testSrc/com/intellij/java/parser/JavadocParsingTest.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.java.parser;
 
 public class JavadocParsingTest extends JavaParsingTestCase {
@@ -93,4 +79,8 @@ public class JavadocParsingTest extends JavaParsingTestCase {
   public void testValueQualified() { doTest(true); }
   public void testValueUnqualifiedWithHash() { doTest(true); }
   public void testValueUnqualifiedWithoutHash() { doTest(true); }
+
+  public void testThrowsTag() { doTest(true); }
+  public void testUsesTag() { doTest(true); }
+  public void testProvidesTag() { doTest(true); }
 }
