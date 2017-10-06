504300662d69326134c3c388b27ae4edb6abb3cb
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2017-10-06 17:39:58 +0700

    IDEA-179382 intention to change return type works when assigning but not when passing a return value as an argument to some different method

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/methodReturn/beforeMethodCallTwice.java
@@ -0,0 +1,20 @@
+// "Make 'getNumber' return 'java.lang.String'" "false"
+public class Test {
+  int number;
+
+  public int getNumber() {
+    return number;
+  }
+
+  public void consumeString(String s, int s1) {
+    // Do nothing
+  }
+
+  public void consumeString(Double s, int s1) {
+    // Do nothing
+  }
+
+  public Test() {
+    consumeString(getNu<caret>mber(), getNumber());
+  }
+}
