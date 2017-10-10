6e735a1a5f9cf63e20ac957b3b821bf32aef3dbb
Semyon Proshev <semyon.proshev@jetbrains.com>
2017-10-10 17:30:39 +0300

    Ability to enable/disable semantic highlighting independently (PY-26445)
    (cherry picked from commit a76dab0)

--- python/src/com/jetbrains/python/highlighting/PyRainbowVisitor.kt
+++ python/src/com/jetbrains/python/highlighting/PyRainbowVisitor.kt
@@ -1,21 +1,8 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.python.highlighting
 
 import com.intellij.codeInsight.daemon.RainbowVisitor
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
 import com.intellij.openapi.editor.colors.TextAttributesKey
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
@@ -30,6 +17,10 @@ class PyRainbowVisitor : RainbowVisitor() {
 
   companion object {
     private val IGNORED_NAMES = setOf(PyNames.NONE, PyNames.TRUE, PyNames.FALSE)
+    private val DEFAULT_HIGHLIGHTING_KEY = DefaultLanguageHighlighterColors.LOCAL_VARIABLE
+
+    @JvmStatic
+    val HIGHLIGHTING_KEYS = setOf(PyHighlighter.PY_PARAMETER, DEFAULT_HIGHLIGHTING_KEY)
   }
 
   override fun suitableForFile(file: PsiFile) = file is PyFile
@@ -132,8 +123,8 @@ class PyRainbowVisitor : RainbowVisitor() {
     }
   }
 
-  private fun addInfo(context: PsiElement, rainbowElement: PsiElement, name: String, colorKey: TextAttributesKey? = null) {
-    addInfo(getInfo(context, rainbowElement, name, colorKey))
+  private fun addInfo(context: PsiElement, rainbowElement: PsiElement, name: String, key: TextAttributesKey? = DEFAULT_HIGHLIGHTING_KEY) {
+    addInfo(getInfo(context, rainbowElement, name, key))
   }
 
   private fun getLeastCommonScope(elements: Collection<PsiElement>): ScopeOwner? {
