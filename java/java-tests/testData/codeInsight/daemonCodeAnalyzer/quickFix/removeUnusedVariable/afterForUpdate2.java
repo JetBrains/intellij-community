24a6c66183486ffafca0df349f3a412561a3fef5
Pavel Dolgov <pavel.dolgov@jetbrains.com>
2017-10-10 18:33:28 +0300

    Java: Fixed removing unused variable occurrences from 'for' loop's update and init clauses (IDEA-180217)

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/removeUnusedVariable/afterForUpdate2.java
@@ -0,0 +1,10 @@
+// "Remove variable 'problematic'" "true"
+class C {
+    Object foo = null;
+
+    void case02() {
+        for(int i = 10; i > 0; i--) {
+            System.out.println("index = " + i);
+        }
+    }
+}
