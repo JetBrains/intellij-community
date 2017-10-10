e453f8e8c6273e3bef71cd52299015b3f0374e9d
Alexey Kudravtsev <cdr@intellij.com>
2017-10-10 13:59:02 +0300

    lazy load/soft reference huge texts to conserve memory

--- platform/lang-impl/src/com/intellij/ide/fileTemplates/impl/FileTemplateBase.java
+++ platform/lang-impl/src/com/intellij/ide/fileTemplates/impl/FileTemplateBase.java
@@ -40,6 +40,7 @@ public abstract class FileTemplateBase implements FileTemplate {
   private String myText;
   private boolean myShouldReformatCode = DEFAULT_REFORMAT_CODE_VALUE;
   private boolean myLiveTemplateEnabled;
+  private boolean myLiveTemplateEnabledChanged;
 
   @Override
   public final boolean isReformatCode() {
@@ -51,11 +52,13 @@ public abstract class FileTemplateBase implements FileTemplate {
     myShouldReformatCode = reformat;
   }
 
+  @NotNull 
   public final String getQualifiedName() {
     return getQualifiedName(getName(), getExtension());
   }
 
-  public static String getQualifiedName(final String name, final String extension) {
+  @NotNull
+  public static String getQualifiedName(@NotNull String name, @NotNull String extension) {
     return FTManager.encodeFileName(name, extension);
   }
 
@@ -122,7 +125,12 @@ public abstract class FileTemplateBase implements FileTemplate {
 
   @Override
   public void setLiveTemplateEnabled(boolean value) {
+    myLiveTemplateEnabledChanged |= myLiveTemplateEnabled != value;
     myLiveTemplateEnabled = value;
+  }
+
+  public boolean isLiveTemplateEnabledChanged() {
+    return myLiveTemplateEnabledChanged;
   }
 
   public boolean isLiveTemplateEnabledByDefault() { return false; }
