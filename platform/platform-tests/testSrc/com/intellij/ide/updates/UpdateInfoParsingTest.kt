7729cad6cd2c885aabc82fba5f39ed241c2a066c
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2017-10-10 21:26:25 +0200

    [tests] update info tests ain't no unit anymore :(

--- platform/platform-tests/testSrc/com/intellij/ide/updates/UpdateInfoParsingTest.kt
+++ platform/platform-tests/testSrc/com/intellij/ide/updates/UpdateInfoParsingTest.kt
@@ -1,24 +1,11 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.updates
 
 import com.intellij.openapi.updateSettings.impl.ChannelStatus
 import com.intellij.openapi.updateSettings.impl.UpdateChannel
 import com.intellij.openapi.updateSettings.impl.UpdatesInfo
 import com.intellij.openapi.util.BuildNumber
+import com.intellij.testFramework.fixtures.BareTestFixtureTestCase
 import com.intellij.util.loadElement
 import org.junit.Assume.assumeTrue
 import org.junit.Test
@@ -28,7 +15,7 @@ import java.text.SimpleDateFormat
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
-class UpdateInfoParsingTest {
+class UpdateInfoParsingTest : BareTestFixtureTestCase() {
   @Test fun liveJetbrainsUpdateFile() {
     try {
       val info = load(URL("https://www.jetbrains.com/updates/updates.xml").readText())
