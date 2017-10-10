0012344bdcd756a17e7e575fc9688052bc2abf08
Anna.Kozlova <anna.kozlova@jetbrains.com>
2017-10-10 15:16:50 +0200

    precise rethrow: runtime exception special case for generic declared type only (IDEA-180035)
    (cherry picked from commit 79b362e) IDEA-CR-25324

--- java/java-psi-impl/src/com/intellij/psi/impl/source/tree/java/PsiCatchSectionImpl.java
+++ java/java-psi-impl/src/com/intellij/psi/impl/source/tree/java/PsiCatchSectionImpl.java
@@ -128,7 +128,12 @@ public class PsiCatchSectionImpl extends CompositePsiElement implements PsiCatch
       // ... and T is assignable to Ej ...
       List<PsiType> types = new ArrayList<>();
       for (PsiType type : uncaughtTypes) {
-        if (declaredType.isAssignableFrom(type) || type instanceof PsiClassType && ExceptionUtil.isUncheckedException((PsiClassType)type)) {
+        if (declaredType.isAssignableFrom(type) ||
+            // from 11.2.3 exception checking
+            // It is a compile-time error if a catch clause can catch checked exception class E1 and it is not the case that the try block corresponding to the catch clause can
+            // throw a checked exception class that is a subclass or superclass of E1, unless E1 is Exception or a superclass of Exception.
+            // so here unchecked exception can sneak throw Exception or Throwable catch type only
+            ExceptionUtil.isGeneralExceptionType(declaredType) && type instanceof PsiClassType && ExceptionUtil.isUncheckedException((PsiClassType)type)) {
           types.add(type);
         }
       }
