e01a6dafe3ab5be2bfdbb8f4c44458935507d27e
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2017-10-11 14:41:13 +0200

    [platform] resets levels for loggers removed from custom configuration
    (IDEA-CR-25368)

--- platform/platform-impl/src/com/intellij/diagnostic/DebugLogManager.kt
+++ platform/platform-impl/src/com/intellij/diagnostic/DebugLogManager.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.diagnostic
 
 import com.intellij.ide.util.PropertiesComponent
@@ -37,15 +23,21 @@ class DebugLogManager : ApplicationComponent {
 
   override fun initComponent() {
     val categories = getSavedCategories() +
-        // add categories from system properties (e.g. for tests on CI server)  
+        // add categories from system properties (e.g. for tests on CI server)
         fromString(System.getProperty(LOG_DEBUG_CATEGORIES_SYSTEM_PROPERTY), DebugLogLevel.DEBUG) +
         fromString(System.getProperty(LOG_TRACE_CATEGORIES_SYSTEM_PROPERTY), DebugLogLevel.TRACE)
-    
+
     applyCategories(categories)
   }
 
   private fun fromString(text: String?, level: DebugLogLevel) =
     if (text != null) StringUtil.splitByLines(text, true).map { Pair(it, level) }.toList() else emptyList()
+
+  fun clearCategories(categories: List<Pair<String, DebugLogLevel>>) {
+    categories.forEach {
+      LogManager.getLogger(it.first)?.level = null
+    }
+  }
 
   fun applyCategories(categories: List<Pair<String, DebugLogLevel>>) {
     applyCategories(categories, DebugLogLevel.DEBUG, Level.DEBUG)
