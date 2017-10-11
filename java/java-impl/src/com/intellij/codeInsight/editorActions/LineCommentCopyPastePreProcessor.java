dcf4294270c2d622fabf2900876a8575fff99141
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2017-10-11 16:11:25 +0300

    IDEA-178801 Multi-line pasting in line comments
    Trim trailing line breaks
    
    (cherry picked from commit f112695)(IDEA-CR-25377)

--- java/java-impl/src/com/intellij/codeInsight/editorActions/LineCommentCopyPastePreProcessor.java
+++ java/java-impl/src/com/intellij/codeInsight/editorActions/LineCommentCopyPastePreProcessor.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.editorActions;
 
 import com.intellij.lang.java.JavaLanguage;
@@ -20,6 +6,7 @@ import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.RawText;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiJavaFile;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
@@ -51,7 +38,7 @@ public class LineCommentCopyPastePreProcessor implements CopyPastePreProcessor {
       CodeStyleSettings codeStyleSettings = CodeStyleSettingsManager.getSettings(project);
       String lineStartReplacement = "\n" + chars.subSequence(lineStartOffset, firstNonWsLineOffset + 2) +
                                     (codeStyleSettings.getCommonSettings(JavaLanguage.INSTANCE).LINE_COMMENT_ADD_SPACE ? " " : "");
-      return text.replace("\n", lineStartReplacement);
+      return StringUtil.trimTrailing(text, '\n').replace("\n", lineStartReplacement);
     }
     else {
       return text;
