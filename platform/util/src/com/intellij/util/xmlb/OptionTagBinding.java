9fa62be77f12d975413122e0f1e5eb26f647b5c1
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2017-10-11 16:50:24 +0200

    XmlSerializer — remove control chars from attribute text
    (cherry picked from commit e7d86089348034f346b2c494fea80aa35bbaa3d7)

--- platform/util/src/com/intellij/util/xmlb/OptionTagBinding.java
+++ platform/util/src/com/intellij/util/xmlb/OptionTagBinding.java
@@ -1,18 +1,16 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.intellij.util.xmlb;
 
 import com.intellij.openapi.util.text.StringUtil;
@@ -66,7 +64,7 @@ class OptionTagBinding extends BasePrimitiveBinding {
 
     if (myConverter == null) {
       if (myBinding == null) {
-        targetElement.setAttribute(myValueAttribute, XmlSerializerImpl.convertToString(value));
+        targetElement.setAttribute(myValueAttribute, XmlSerializerImpl.removeControlChars(XmlSerializerImpl.convertToString(value)));
       }
       else if (myBinding instanceof BeanBinding && myValueAttribute.isEmpty()) {
         ((BeanBinding)myBinding).serializeInto(value, targetElement, filter);
@@ -79,7 +77,7 @@ class OptionTagBinding extends BasePrimitiveBinding {
       }
     }
     else {
-      targetElement.setAttribute(myValueAttribute, myConverter.toString(value));
+      targetElement.setAttribute(myValueAttribute, XmlSerializerImpl.removeControlChars(myConverter.toString(value)));
     }
     return targetElement;
   }
