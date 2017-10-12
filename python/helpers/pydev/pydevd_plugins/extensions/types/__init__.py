90869a8999b8d1d40011e6d5643f2bc59ad0c7eb
Elizaveta Shashkova <Elizaveta.Shashkova@jetbrains.com>
2017-10-12 18:53:10 +0300

    Updates from PyDev: Restoring use of setuptools for namespace packages and made initialization of extensions lazy.

--- python/helpers/pydev/pydevd_plugins/extensions/types/__init__.py
+++ python/helpers/pydev/pydevd_plugins/extensions/types/__init__.py
@@ -1,2 +1,5 @@
-import pkgutil
-__path__ = pkgutil.extend_path(__path__, __name__)
+try:
+    __import__('pkg_resources').declare_namespace(__name__)
+except ImportError:
+    import pkgutil
+    __path__ = pkgutil.extend_path(__path__, __name__)
