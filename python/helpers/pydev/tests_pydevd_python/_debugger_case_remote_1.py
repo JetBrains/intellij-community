d2574f96d8ef4aee7773344caafea732b522cbdf
Elizaveta Shashkova <Elizaveta.Shashkova@jetbrains.com>
2017-10-12 15:51:20 +0300

    Updates from PyDev: Fix tests

--- python/helpers/pydev/tests_pydevd_python/_debugger_case_remote_1.py
+++ python/helpers/pydev/tests_pydevd_python/_debugger_case_remote_1.py
@@ -4,12 +4,12 @@ if __name__ == '__main__':
     import os
     import _debugger_case_remote_2
     root_dirname = os.path.dirname(os.path.dirname(__file__))
-    
+
     if root_dirname not in sys.path:
         sys.path.append(root_dirname)
-        
+
     import pydevd
-    
+
     print('before pydevd.settrace')
     sys.stdout.flush()
     pydevd.settrace(port=8787, patch_multiprocessing=True)
@@ -18,6 +18,8 @@ if __name__ == '__main__':
     f = _debugger_case_remote_2.__file__
     if f.endswith('.pyc'):
         f = f[:-1]
+    elif f.endswith('$py.class'):
+        f = f[:-len('$py.class')] + '.py'
     print('before call')
     sys.stdout.flush()
     subprocess.check_call([sys.executable, '-u', f])
