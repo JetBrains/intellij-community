200645e2b5aa6704e60d180588182a87627a3f06
Elizaveta Shashkova <elizaveta.shashkova@jetbrains.com>
2017-10-12 19:30:22 +0300

    Updates from PyDev: get rid of'use_value_repr_instead_of_str'. Implement StrPresentationProvider for django forms to get the same functionality.
    (cherry picked from commit 768de61)

--- python/helpers/pydev/pydevd_plugins/extensions/types/helpers.py
+++ python/helpers/pydev/pydevd_plugins/extensions/types/helpers.py
@@ -10,3 +10,17 @@ def find_mod_attr(mod_name, attr):
         return None
     return getattr(mod, attr, None)
 
+
+def find_class_name(val):
+    class_name = str(val.__class__)
+    if class_name.find('.') != -1:
+        class_name = class_name.split('.')[-1]
+
+    elif class_name.find("'") != -1: #does not have '.' (could be something like <type 'int'>)
+        class_name = class_name[class_name.index("'") + 1:]
+
+    if class_name.endswith("'>"):
+        class_name = class_name[:-2]
+
+    return class_name
+
