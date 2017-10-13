9b2c0b4e8c27455dfe430a97a98efefb101bfffd
Yaroslav Pankratyev <yaroslav.pankratyev@jetbrains.com>
2017-10-13 14:41:07 +0700

    Replace assert with Logger#assertTrue in InspectionEngine + javadoc improvements

--- platform/core-api/src/com/intellij/psi/PsiRecursiveVisitor.java
+++ platform/core-api/src/com/intellij/psi/PsiRecursiveVisitor.java
@@ -2,7 +2,10 @@
 package com.intellij.psi;
 
 /**
- * Marker interface for recursive PSI visitors.
+ * Please use this interface to mark recursive visitors.
+ * This information can then be used by the runtime to find accidental performance issues, e.g. see assertions in
+ * com.intellij.codeInspection.LocalInspectionTool#processFile and
+ * com.intellij.codeInspection.InspectionEngine#createVisitorAndAcceptElements.
  */
 public interface PsiRecursiveVisitor {
 }
