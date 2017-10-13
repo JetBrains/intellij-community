9fb0778fb20da927c47b52d93140c8d86021ba4c
Anton Makeev <Anton.Makeev@jetbrains.com>
2017-10-13 18:26:36 +0200

    CPP-8417 Allow switching active build type without editing/creating new run configurations
    (cherry picked from commit bd96e45)

--- platform/lang-impl/src/com/intellij/execution/ProgramRunnerUtil.java
+++ platform/lang-impl/src/com/intellij/execution/ProgramRunnerUtil.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution;
 
 import com.intellij.execution.configurations.ConfigurationFactory;
@@ -58,6 +44,11 @@ public class ProgramRunnerUtil {
     executeConfigurationAsync(environment, showSettings, assignNewId, null);
   }
 
+  @NotNull
+  public static String getCannotRunOnErrorMessage(@NotNull RunProfile profile, @NotNull ExecutionTarget target) {
+    return StringUtil.escapeXml("Cannot run '" + profile.getName() + "' on '" + target.getDisplayName() + "'");
+  }
+
   public static void executeConfigurationAsync(@NotNull final ExecutionEnvironment environment, boolean showSettings, boolean assignNewId, ProgramRunner.Callback callback) {
     if (ExecutorRegistry.getInstance().isStarting(environment)) {
       return;
@@ -68,7 +59,7 @@ public class ProgramRunnerUtil {
     if (runnerAndConfigurationSettings != null) {
       if (!ExecutionTargetManager.canRun(environment)) {
         ExecutionUtil.handleExecutionError(environment, new ExecutionException(
-          StringUtil.escapeXml("Cannot run '" + environment.getRunProfile().getName() + "' on '" + environment.getExecutionTarget().getDisplayName() + "'")));
+          getCannotRunOnErrorMessage(environment.getRunProfile(), environment.getExecutionTarget())));
         return;
       }
 
