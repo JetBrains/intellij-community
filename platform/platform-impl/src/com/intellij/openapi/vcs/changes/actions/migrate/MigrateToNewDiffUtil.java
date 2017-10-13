14faa862a3f28173c7cc3d56f9ea609410ce76e7
Aleksey Pivovarov <AMPivovarov@gmail.com>
2017-10-14 00:58:39 +0300

    diff: migration from old API - support MergeRequest

--- platform/platform-impl/src/com/intellij/openapi/vcs/changes/actions/migrate/MigrateToNewDiffUtil.java
+++ platform/platform-impl/src/com/intellij/openapi/vcs/changes/actions/migrate/MigrateToNewDiffUtil.java
@@ -1,13 +1,22 @@
 package com.intellij.openapi.vcs.changes.actions.migrate;
 
 import com.intellij.diff.DiffContentFactory;
+import com.intellij.diff.DiffRequestFactory;
+import com.intellij.diff.InvalidDiffRequestException;
 import com.intellij.diff.contents.DiffContent;
+import com.intellij.diff.merge.MergeRequest;
+import com.intellij.diff.merge.MergeResult;
 import com.intellij.diff.requests.DiffRequest;
 import com.intellij.diff.requests.SimpleDiffRequest;
 import com.intellij.openapi.diff.SimpleContent;
+import com.intellij.openapi.diff.impl.mergeTool.MergeRequestImpl;
+import com.intellij.openapi.diff.impl.mergeTool.MergeVersion;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.util.Consumer;
+import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -48,5 +57,27 @@ public class MigrateToNewDiffUtil {
       Document document = assertNotNull(oldContent.getDocument());
       return factory.create(project, document, oldContent.getContentType());
     }
+  }
+
+  @NotNull
+  public static MergeRequest convertMergeRequest(@NotNull MergeRequestImpl request) throws InvalidDiffRequestException {
+    MergeRequestImpl.MergeContent mergeContent = assertNotNull(request.getMergeContent());
+    MergeVersion.MergeDocumentVersion mergeVersion = (MergeVersion.MergeDocumentVersion)mergeContent.getMergeVersion();
+
+    com.intellij.openapi.diff.SimpleContent leftContent = (SimpleContent)request.getContents()[0];
+    com.intellij.openapi.diff.SimpleContent rightContent = (SimpleContent)request.getContents()[2];
+    List<String> contents = ContainerUtil.list(leftContent.getText(), mergeVersion.getOriginalText(), rightContent.getText());
+
+    Document document = mergeContent.getDocument();
+
+    String windowTitle = request.getWindowTitle();
+    List<String> titles = Arrays.asList(request.getContentTitles());
+
+    Consumer<MergeResult> callback = result -> {
+      request.setResult(result == MergeResult.CANCEL ? DialogWrapper.CANCEL_EXIT_CODE : DialogWrapper.OK_EXIT_CODE);
+    };
+
+    return DiffRequestFactory.getInstance().createMergeRequest(request.getProject(), mergeContent.getContentType(), document, contents,
+                                                               windowTitle, titles, callback);
   }
 }
