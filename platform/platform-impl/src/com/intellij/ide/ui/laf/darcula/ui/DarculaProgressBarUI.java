bbb79aab07cfbd921b8b0a7d7e8ad65e365835c7
Alexey Ushakov <Alexey.Ushakov@jetbrains.com>
2017-10-13 10:14:50 +0300

    IDEA-174258 Painting progress bars eats CPU
    Fixed minor quality issues
    
    (reviewed by Vassiliy Kudryashov IDEA-CR-25315)
    (cherry picked from commit 553e4d0)

--- platform/platform-impl/src/com/intellij/ide/ui/laf/darcula/ui/DarculaProgressBarUI.java
+++ platform/platform-impl/src/com/intellij/ide/ui/laf/darcula/ui/DarculaProgressBarUI.java
@@ -95,7 +95,7 @@ public class DarculaProgressBarUI extends BasicProgressBarUI {
 
         BufferedImage pathTile = UIUtil.createImage(getPeriodLength(), barRectHeight, BufferedImage.TYPE_INT_ARGB);
         Graphics2D gpt = pathTile.createGraphics();
-        GraphicsUtil.setupAAPainting(g);
+        GraphicsUtil.setupAAPainting(gpt);
         gpt.setColor(new JBColor(Gray._165, Gray._88));
         gpt.fill(path);
 
@@ -105,7 +105,7 @@ public class DarculaProgressBarUI extends BasicProgressBarUI {
 
           TexturePaint pathPaint =
             new TexturePaint(pathTile, new Rectangle2D.Double(
-              getPeriodLength() - progressOffset,0, getPeriodLength(), barRectHeight));
+              Math.rint(getPeriodLength() - progressOffset),0, getPeriodLength(), barRectHeight));
           pathPaintList.add(pathPaint);
         }
 
