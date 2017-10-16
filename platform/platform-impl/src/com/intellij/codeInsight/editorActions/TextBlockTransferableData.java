571e8df2ac893ea47560b11b0acdcfc9251c0115
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2017-10-16 20:20:50 +0300

    IDEA-179890 'Copy as rich text' doesn't work when pasting to Keynote
    (cherry picked from commit c80418f)(IDEA-CR-25619)

--- platform/platform-impl/src/com/intellij/codeInsight/editorActions/TextBlockTransferableData.java
+++ platform/platform-impl/src/com/intellij/codeInsight/editorActions/TextBlockTransferableData.java
@@ -1,30 +1,26 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.codeInsight.editorActions;
 
 import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
 
 /**
  * @author yole
  */
 public interface TextBlockTransferableData {
+  int PLAIN_TEXT_PRIORITY = 0;
+
   DataFlavor getFlavor();
 
   int getOffsetCount();
   int getOffsets(final int[] offsets, final int index);
   int setOffsets(final int[] offsets, final int index);
+
+  /**
+   * Priority defines an order in which resulting transferable will mention available DataFlavor-s.
+   * 
+   * @see Transferable#getTransferDataFlavors()
+   */
+  default int getPriority() { return PLAIN_TEXT_PRIORITY; }
 }
