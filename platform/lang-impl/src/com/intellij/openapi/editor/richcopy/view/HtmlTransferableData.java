c80418fcdbaf390e1c63e41d53fb289af01e2c6b
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2017-10-16 20:20:50 +0300

    IDEA-179890 'Copy as rich text' doesn't work when pasting to Keynote

--- platform/lang-impl/src/com/intellij/openapi/editor/richcopy/view/HtmlTransferableData.java
+++ platform/lang-impl/src/com/intellij/openapi/editor/richcopy/view/HtmlTransferableData.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.editor.richcopy.view;
 
 import com.intellij.openapi.editor.richcopy.model.ColorRegistry;
@@ -32,7 +18,7 @@ import java.awt.datatransfer.DataFlavor;
  * @since 3/28/13 1:06 PM
  */
 public class HtmlTransferableData extends AbstractSyntaxAwareReaderTransferableData implements MarkupHandler {
-
+  public static final int PRIORITY = 200;
   @NotNull public static final DataFlavor FLAVOR = new DataFlavor("text/html; class=java.io.Reader; charset=UTF-8", "HTML text");
 
   private final int myTabSize;
@@ -214,5 +200,10 @@ public class HtmlTransferableData extends AbstractSyntaxAwareReaderTransferableD
       return false;
     }
     return true;
+  }
+
+  @Override
+  public int getPriority() {
+    return PRIORITY;
   }
 }
