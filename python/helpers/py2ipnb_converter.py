9f65d4e8134497b5d1b15f27cdfc6f5a9176cf3f
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2017-10-16 13:51:56 +0300

    PY-25639 "Convert to Jupyter Notebook" removed all line breaks in code.

--- python/helpers/py2ipnb_converter.py
+++ python/helpers/py2ipnb_converter.py
@@ -1,8 +1,7 @@
-import sys
-import nbformat
-from nbformat.v4 import new_code_cell, new_notebook
-
 import codecs
+import nbformat
+import sys
+from nbformat.v4 import new_code_cell, new_notebook
 
 name = sys.argv[-1][:-3]
 source = name + ".py"
@@ -13,14 +12,15 @@ def parse_python(file):
     with open(file, "r") as file_descriptor:
         lines = []
         for line in file_descriptor:
-            line = line.strip()
-            if line.startswith('# In[') and line.endswith(']:') and lines:
+            line_stripped = line.strip()
+            if line_stripped.startswith('# In[') and line_stripped.endswith(']:') and lines:
                 yield "".join(lines)
                 lines = []
                 continue
             lines.append(line)
         if lines:
             yield "".join(lines)
+
 
 cells = []
 for cell_content in parse_python(source):
