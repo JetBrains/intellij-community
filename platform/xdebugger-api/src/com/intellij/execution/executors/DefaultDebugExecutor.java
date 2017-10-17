5b67286689330caccab4bb58f7b8618199d85990
Elizaveta Shashkova <Elizaveta.Shashkova@jetbrains.com>
2017-10-17 18:43:24 +0300

    Change icon and description for debugger (PY-26526)

--- platform/xdebugger-api/src/com/intellij/execution/executors/DefaultDebugExecutor.java
+++ platform/xdebugger-api/src/com/intellij/execution/executors/DefaultDebugExecutor.java
@@ -18,6 +18,7 @@ package com.intellij.execution.executors;
 import com.intellij.execution.Executor;
 import com.intellij.execution.ExecutorRegistry;
 import com.intellij.icons.AllIcons;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.wm.ToolWindowId;
 import com.intellij.ui.UIBundle;
 import com.intellij.xdebugger.XDebuggerBundle;
@@ -31,7 +32,7 @@ import javax.swing.*;
  */
 public class DefaultDebugExecutor extends Executor {
   @NonNls public static final String EXECUTOR_ID = ToolWindowId.DEBUG;
-  private final String myStartActionText = XDebuggerBundle.message("debugger.runner.start.action.text");
+  private final String myStartActionText = "Run with &Debugging";
   private final String myDescription = XDebuggerBundle.message("string.debugger.runner.description");
 
   public String getToolWindowId() {
@@ -68,6 +69,16 @@ public class DefaultDebugExecutor extends Executor {
   @NotNull
   public String getStartActionText() {
     return myStartActionText;
+  }
+
+  private static String escapeMnemonicsInConfigurationName(String configurationName) {
+    return configurationName.replace("_", "__");
+  }
+
+  @Override
+  public String getStartActionText(String configurationName) {
+    final String name = configurationName != null ? escapeMnemonicsInConfigurationName(shortenNameIfNeed(configurationName)) : null;
+    return "Run" + (StringUtil.isEmpty(name) ? "" : " '" + name + "'") + " with _Debugging";
   }
 
   public String getDescription() {
