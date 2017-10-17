542ba81a1479b3432e9cd1f414f60501e0078f66
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2017-10-17 17:49:31 +0300

    Resolve custom member with context
    Now custom members could be unresolved in some contexts.
    That fact could lead to false positives in code insight, such as unresolved references.
    So you have to process such cases separately, e.g. by ignoring them.
    
    Pass contexts to module member providers.
    Fix context annotation and pass resolve context in class members providers.
    
    No more unstubbing in Py3UnresolvedReferencesInspectionTest.testMockPatchObject.

--- python/psi-api/src/com/jetbrains/python/psi/types/PyClassMembersProviderBase.java
+++ python/psi-api/src/com/jetbrains/python/psi/types/PyClassMembersProviderBase.java
@@ -1,23 +1,10 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.python.psi.types;
 
 import com.intellij.psi.PsiElement;
 import com.jetbrains.python.codeInsight.PyCustomMember;
 import com.jetbrains.python.psi.PyClass;
+import com.jetbrains.python.psi.resolve.PyResolveContext;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -30,24 +17,25 @@ import java.util.Collections;
 public class PyClassMembersProviderBase implements PyClassMembersProvider {
   @NotNull
   @Override
-  public Collection<PyCustomMember> getMembers(PyClassType clazz, PsiElement location, TypeEvalContext typeEvalContext) {
+  public Collection<PyCustomMember> getMembers(PyClassType clazz, PsiElement location, @NotNull TypeEvalContext context) {
     return Collections.emptyList();
   }
 
   @Override
-  public PsiElement resolveMember(PyClassType clazz, String name, @Nullable PsiElement location, TypeEvalContext context) {
-    final Collection<PyCustomMember> members = getMembers(clazz, location, context);
-    return resolveMemberByName(members, clazz, name);
+  public PsiElement resolveMember(PyClassType clazz, String name, @Nullable PsiElement location, @NotNull PyResolveContext resolveContext) {
+    final Collection<PyCustomMember> members = getMembers(clazz, location, resolveContext.getTypeEvalContext());
+    return resolveMemberByName(members, clazz, name, resolveContext);
   }
 
   @Nullable
   public static PsiElement resolveMemberByName(Collection<PyCustomMember> members,
                                                PyClassType clazz,
-                                               String name) {
+                                               String name,
+                                               @NotNull PyResolveContext resolveContext) {
     final PyClass pyClass = clazz.getPyClass();
     for (PyCustomMember member : members) {
       if (member.getName().equals(name)) {
-        return member.resolve(pyClass);
+        return member.resolve(pyClass, resolveContext);
       }
     }
     return null;
--- python/psi-api/src/com/jetbrains/python/psi/types/PyModuleMembersProvider.java
+++ python/psi-api/src/com/jetbrains/python/psi/types/PyModuleMembersProvider.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.python.psi.types;
 
 import com.intellij.openapi.extensions.ExtensionPointName;
@@ -22,6 +8,8 @@ import com.jetbrains.python.codeInsight.PyCustomMember;
 import com.jetbrains.python.psi.PyFile;
 import com.jetbrains.python.psi.PyPsiFacade;
 import com.jetbrains.python.psi.resolve.PointInImport;
+import com.jetbrains.python.psi.resolve.PyResolveContext;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
@@ -33,26 +21,26 @@ import java.util.Collections;
 public abstract class PyModuleMembersProvider {
   public static final ExtensionPointName<PyModuleMembersProvider> EP_NAME = ExtensionPointName.create("Pythonid.pyModuleMembersProvider");
 
-  public Collection<PyCustomMember> getMembers(PyFile module, PointInImport point) {
+  public Collection<PyCustomMember> getMembers(PyFile module, PointInImport point, @NotNull TypeEvalContext context) {
     final VirtualFile vFile = module.getVirtualFile();
     if (vFile != null) {
       final String qName = PyPsiFacade.getInstance(module.getProject()).findShortestImportableName(vFile, module);
       if (qName != null) {
-        return getMembersByQName(module, qName);
+        return getMembersByQName(module, qName, context);
       }
     }
     return Collections.emptyList();
   }
 
   @Nullable
-  public PsiElement resolveMember(PyFile module, String name) {
-    for (PyCustomMember o : getMembers(module, PointInImport.NONE)) {
+  public PsiElement resolveMember(PyFile module, String name, @NotNull PyResolveContext resolveContext) {
+    for (PyCustomMember o : getMembers(module, PointInImport.NONE, resolveContext.getTypeEvalContext())) {
       if (o.getName().equals(name)) {
-        return o.resolve(module);
+        return o.resolve(module, resolveContext);
       }
     }
     return null;
   }
 
-  protected abstract Collection<PyCustomMember> getMembersByQName(PyFile module, String qName);
+  protected abstract Collection<PyCustomMember> getMembersByQName(PyFile module, String qName, @NotNull TypeEvalContext context);
 }
