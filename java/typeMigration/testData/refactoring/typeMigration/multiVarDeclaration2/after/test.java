4fcc62681b40bbd7340019fce94eda33ace5357f
Bas Leijdekkers <basleijdekkers@gmail.com>
2017-10-18 15:17:52 +0200

    fix test data

--- java/typeMigration/testData/refactoring/typeMigration/multiVarDeclaration2/after/test.java
+++ java/typeMigration/testData/refactoring/typeMigration/multiVarDeclaration2/after/test.java
@@ -2,8 +2,7 @@ public class Test {
     Object a = "a", b = "";
 
     void mm() {
-        Object c = a;
-        Object d = b;
+        Object c = a;Object d = b;
         String ololo = "intellij-idea";
     }
 }
