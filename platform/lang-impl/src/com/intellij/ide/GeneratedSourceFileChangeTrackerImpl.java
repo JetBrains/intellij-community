c855ec0bffaafb1f83e854aeee8fb75a37b8749d
Alexander Zolotov <goldifit@gmail.com>
2017-10-19 14:22:23 +0300

    Do not track generated files outside the project
    (cherry picked from commit 587ee6e)
    IDEA-CR-25776

--- platform/lang-impl/src/com/intellij/ide/GeneratedSourceFileChangeTrackerImpl.java
+++ platform/lang-impl/src/com/intellij/ide/GeneratedSourceFileChangeTrackerImpl.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide;
 
 import com.intellij.AppTopics;
@@ -31,6 +17,7 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.GeneratedSourcesFilter;
 import com.intellij.openapi.roots.ModuleRootEvent;
 import com.intellij.openapi.roots.ModuleRootListener;
+import com.intellij.openapi.roots.ProjectFileIndex;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.EditorNotifications;
 import com.intellij.util.Alarm;
@@ -74,8 +61,10 @@ public class GeneratedSourceFileChangeTrackerImpl extends GeneratedSourceFileCha
     EditorFactory.getInstance().getEventMulticaster().addDocumentListener(new DocumentListener() {
       @Override
       public void documentChanged(DocumentEvent e) {
+        if (myProject.isDisposed()) return;
         VirtualFile file = myDocumentManager.getFile(e.getDocument());
-        if (file != null) {
+        ProjectFileIndex fileIndex = ProjectFileIndex.getInstance(myProject);
+        if (file != null && (fileIndex.isInContent(file) || fileIndex.isInLibrary(file))) {
           myFilesToCheck.add(file);
           myCheckingQueue.queue(check);
         }
