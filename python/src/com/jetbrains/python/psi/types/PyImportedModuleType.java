242a3bddb7977d6a03233e1af858486b8df9921c
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2017-10-19 15:55:55 +0300

    Add type eval context to PyModuleType.getCompletionVariantsAsLookupElements as parameter

--- python/src/com/jetbrains/python/psi/types/PyImportedModuleType.java
+++ python/src/com/jetbrains/python/psi/types/PyImportedModuleType.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.python.psi.types;
 
 import com.google.common.collect.Sets;
@@ -75,12 +61,15 @@ public class PyImportedModuleType implements PyType {
     return null;
   }
 
+  @Override
   public Object[] getCompletionVariants(String completionPrefix, PsiElement location, ProcessingContext context) {
     final List<LookupElement> result = new ArrayList<>();
     final PsiElement resolved = myImportedModule.resolve();
     if (resolved instanceof PyFile) {
       final PyModuleType moduleType = new PyModuleType((PyFile)resolved, myImportedModule);
-      result.addAll(moduleType.getCompletionVariantsAsLookupElements(location, context, false, false));
+      final TypeEvalContext typeEvalContext = TypeEvalContext.codeCompletion(location.getProject(), location.getContainingFile());
+
+      result.addAll(moduleType.getCompletionVariantsAsLookupElements(location, context, false, false, typeEvalContext));
     }
     else if (resolved instanceof PsiDirectory) {
       final PsiDirectory dir = (PsiDirectory)resolved;
@@ -96,6 +85,7 @@ public class PyImportedModuleType implements PyType {
     return ArrayUtil.toObjectArray(result);
   }
 
+  @Override
   public String getName() {
     return "imported module " + myImportedModule.getImportedPrefix().toString();
   }
