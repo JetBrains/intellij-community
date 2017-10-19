348d72d11da0cccf41687cb5d2cd7e8630d84cd1
Alexander Zolotov <goldifit@gmail.com>
2017-10-19 13:41:22 +0300

    Synthetic libraries improvements
    - type safe synthetic library node
    - update javadoc
    - less confusing default implementation of isShowInExternalLibrariesNode

--- platform/lang-impl/src/com/intellij/ide/projectView/impl/nodes/SyntheticLibraryElementNode.java
+++ platform/lang-impl/src/com/intellij/ide/projectView/impl/nodes/SyntheticLibraryElementNode.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.projectView.impl.nodes;
 
 import com.intellij.ide.projectView.PresentationData;
@@ -61,8 +47,7 @@ public class SyntheticLibraryElementNode extends ProjectViewNode<SyntheticLibrar
   @Override
   public String getName() {
     SyntheticLibrary library = getLibrary();
-    //noinspection CastToIncompatibleInterface
-    return ((ItemPresentation)library).getPresentableText();
+    return library instanceof ItemPresentation ? ((ItemPresentation)library).getPresentableText() : super.getName();
   }
 
   @NotNull
@@ -72,8 +57,10 @@ public class SyntheticLibraryElementNode extends ProjectViewNode<SyntheticLibrar
 
   @Override
   protected void update(PresentationData presentation) {
-    //noinspection CastToIncompatibleInterface
-    presentation.updateFrom((ItemPresentation)getLibrary());
+    SyntheticLibrary library = getLibrary();
+    if (library instanceof ItemPresentation) {
+      presentation.updateFrom((ItemPresentation)getLibrary());
+    }
   }
 
   @Nullable
