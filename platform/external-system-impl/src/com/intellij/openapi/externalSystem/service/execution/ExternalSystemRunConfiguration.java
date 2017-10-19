36da4be5082d7b4d0914efa2fc394e7adb45b92b
Dmitry Jemerov <yole@jetbrains.com>
2017-10-19 19:22:42 +0300

    IDEA-180868: Fix NPE when there is no module matching the external project path
    (cherry picked from commit 6d938f8)(IDEA-CR-25809)

--- platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/execution/ExternalSystemRunConfiguration.java
+++ platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/execution/ExternalSystemRunConfiguration.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.externalSystem.service.execution;
 
 import com.intellij.build.*;
@@ -196,7 +182,9 @@ public class ExternalSystemRunConfiguration extends LocatableConfigurationBase i
       VirtualFile file = VfsUtil.findFileByIoFile(new File(mySettings.getExternalProjectPath()), false);
       if (file != null) {
         Module module = DirectoryIndex.getInstance(getProject()).getInfoForFile(file).getModule();
-        scope = SearchScopeProvider.createSearchScope(ContainerUtil.ar(module));
+        if (module != null) {
+          scope = SearchScopeProvider.createSearchScope(ContainerUtil.ar(module));
+        }
       }
     }
     return scope;
