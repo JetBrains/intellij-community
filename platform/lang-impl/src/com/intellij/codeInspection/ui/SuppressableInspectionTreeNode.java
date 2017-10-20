2ef52ec6d11f95476b286d967856bc8abe758fcf
Alexey Gopachenko <neuro159@gmail.com>
2017-10-20 20:56:02 +0200

    Avoid object duplication
    (cherry picked from commit 0bf254c)

--- platform/lang-impl/src/com/intellij/codeInspection/ui/SuppressableInspectionTreeNode.java
+++ platform/lang-impl/src/com/intellij/codeInspection/ui/SuppressableInspectionTreeNode.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInspection.ui;
 
 import com.intellij.codeInspection.CommonProblemDescriptor;
@@ -20,11 +6,12 @@ import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.codeInspection.SuppressIntentionAction;
 import com.intellij.codeInspection.reference.RefElement;
 import com.intellij.codeInspection.reference.RefEntity;
+import com.intellij.concurrency.ConcurrentCollectionFactory;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Pair;
 import com.intellij.psi.PsiElement;
-import com.intellij.concurrency.ConcurrentCollectionFactory;
 import com.intellij.util.containers.ContainerUtil;
+import com.intellij.util.containers.Interner;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -176,6 +163,7 @@ public abstract class SuppressableInspectionTreeNode extends InspectionTreeNode 
   }
 
   private static class NodeState {
+    private static final Interner<NodeState> INTERNER = new Interner<>();
     private final boolean isValid;
     private final boolean isSuppressed;
     private final boolean isFixApplied;
@@ -199,9 +187,14 @@ public abstract class SuppressableInspectionTreeNode extends InspectionTreeNode 
 
       return true;
     }
+
+    @Override
+    public int hashCode() {
+     return (isValid ? 0x1 : 0) + (isFixApplied ? 0x2 : 0) + (isSuppressed ? 0x4 : 0);
+    }
   }
 
   protected NodeState calculateState() {
-    return new NodeState(isValid(), isAlreadySuppressedFromView(), isQuickFixAppliedFromView());
+    return NodeState.INTERNER.intern(new NodeState(isValid(), isAlreadySuppressedFromView(), isQuickFixAppliedFromView()));
   }
 }
