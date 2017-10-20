c16f8295a4b11e483b40811c5fecbdb139949cc9
Semyon Proshev <semyon.proshev@jetbrains.com>
2017-10-20 15:41:00 +0300

    Fix highlighting for empty raises (PY-26510)
    (cherry picked from commit 55949b3)

--- python/src/com/jetbrains/python/validation/TryExceptAnnotator.java
+++ python/src/com/jetbrains/python/validation/TryExceptAnnotator.java
@@ -1,22 +1,11 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.python.validation;
 
+import com.intellij.psi.util.PsiTreeUtil;
 import com.jetbrains.python.PyBundle;
 import com.jetbrains.python.psi.PyExceptPart;
+import com.jetbrains.python.psi.PyFinallyPart;
+import com.jetbrains.python.psi.PyRaiseStatement;
 import com.jetbrains.python.psi.PyTryExceptStatement;
 
 /**
@@ -27,7 +16,7 @@ import com.jetbrains.python.psi.PyTryExceptStatement;
 public class TryExceptAnnotator extends PyAnnotator {
   @Override
   public void visitPyTryExceptStatement(final PyTryExceptStatement node) {
-    PyExceptPart[] exceptParts = node.getExceptParts();
+    final PyExceptPart[] exceptParts = node.getExceptParts();
     boolean haveDefaultExcept = false;
     for (PyExceptPart part : exceptParts) {
       if (haveDefaultExcept) {
@@ -36,6 +25,13 @@ public class TryExceptAnnotator extends PyAnnotator {
       if (part.getExceptClass() == null) {
         haveDefaultExcept = true;
       }
+    }
+  }
+
+  @Override
+  public void visitPyRaiseStatement(PyRaiseStatement node) {
+    if (node.getExpressions().length == 0 && PsiTreeUtil.getParentOfType(node, PyExceptPart.class, PyFinallyPart.class) == null) {
+      markError(node, "No exception to reraise");
     }
   }
 }
