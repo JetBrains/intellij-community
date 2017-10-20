46616b527af8d3f6864968e45890b8f457f962e1
Aleksey Pivovarov <AMPivovarov@gmail.com>
2017-10-20 14:24:00 +0300

    git: collect usage statistics on common hosting platforms

--- plugins/git4idea/src/git4idea/GitStatisticsCollector.kt
+++ plugins/git4idea/src/git4idea/GitStatisticsCollector.kt
@@ -15,6 +15,7 @@
  */
 package git4idea
 
+import com.google.common.collect.HashMultiset
 import com.intellij.internal.statistic.AbstractProjectsUsagesCollector
 import com.intellij.internal.statistic.UsageTrigger
 import com.intellij.internal.statistic.beans.GroupDescriptor
@@ -22,7 +23,9 @@ import com.intellij.internal.statistic.beans.UsageDescriptor
 import com.intellij.internal.statistic.utils.getBooleanUsage
 import com.intellij.internal.statistic.utils.getCountingUsage
 import com.intellij.openapi.project.Project
+import com.intellij.util.io.URLUtil
 import git4idea.config.GitVcsSettings
+import git4idea.repo.GitRemote
 
 fun reportUsage(key: String) {
   UsageTrigger.trigger(key)
@@ -54,6 +57,11 @@ class GitStatisticsCollector : AbstractProjectsUsagesCollector() {
       usages.add(getCountingUsage("data.local.branches.count", branches.localBranches.size, listOf(0, 1, 2, 5, 8, 15, 30, 50)))
       usages.add(getCountingUsage("data.remote.branches.count", branches.remoteBranches.size, listOf(0, 1, 2, 5, 8, 15, 30, 100)))
       usages.add(getCountingUsage("data.remotes.in.project", repository.remotes.size, listOf(0, 1, 2, 5)))
+
+      val servers = repository.remotes.mapNotNull(this::getRemoteServerType).toCollection(HashMultiset.create())
+      for (serverName in servers) {
+        usages.add(getCountingUsage("data.remote.servers." + serverName, servers.count(serverName), listOf(0, 1, 2, 3, 5)))
+      }
     }
 
     return usages
@@ -61,5 +69,19 @@ class GitStatisticsCollector : AbstractProjectsUsagesCollector() {
 
   override fun getGroupId(): GroupDescriptor {
     return ID
+  }
+
+  private fun getRemoteServerType(remote: GitRemote): String? {
+    val hosts = remote.urls.map(URLUtil::parseHostFromSshUrl).distinct()
+
+    if (hosts.contains("github.com")) return "github.com"
+    if (hosts.contains("gitlab.com")) return "gitlab.com"
+    if (hosts.contains("bitbucket.org")) return "bitbucket.org"
+
+    if (remote.urls.any { it.contains("github") }) return "github.custom"
+    if (remote.urls.any { it.contains("gitlab") }) return "gitlab.custom"
+    if (remote.urls.any { it.contains("bitbucket") }) return "bitbucket.custom"
+
+    return null
   }
 }
