4597b110e9e97232b0408e798f1f80925cb7a896
Olga Strizhenko <olga.strizhenko@jetbrains.com>
2017-10-24 18:42:50 +0300

    Spellchecker: (minor) explicit type in WordsPanel

--- spellchecker/src/com/intellij/spellchecker/settings/SpellCheckerSettingsPane.java
+++ spellchecker/src/com/intellij/spellchecker/settings/SpellCheckerSettingsPane.java
@@ -256,7 +256,7 @@ public class SpellCheckerSettingsPane implements Disposable {
     }
   }
 
-  private static final class WordsPanel extends AddDeleteListPanel implements Disposable {
+  private static final class WordsPanel extends AddDeleteListPanel<String> implements Disposable {
     private final SpellCheckerManager manager;
 
     private WordsPanel(SpellCheckerManager manager) {
@@ -266,7 +266,7 @@ public class SpellCheckerSettingsPane implements Disposable {
     }
 
 
-    protected Object findItemToAdd() {
+    protected String findItemToAdd() {
       String word = Messages.showInputDialog(SpellCheckerBundle.message("enter.simple.word"),
                                              SpellCheckerBundle.message("add.new.word"), null);
       if (word == null) {
