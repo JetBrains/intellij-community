ec6931be4438be9693f5a1bfe6eec9619bce8a95
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2017-10-24 13:37:04 +0300

    PY-26627 Fix after review

--- platform/platform-impl/src/com/intellij/ide/actions/CreateDesktopEntryAction.java
+++ platform/platform-impl/src/com/intellij/ide/actions/CreateDesktopEntryAction.java
@@ -154,7 +154,7 @@ public class CreateDesktopEntryAction extends DumbAwareAction {
       throw new RuntimeException(ApplicationBundle.message("desktop.entry.icon.missing", binPath));
     }
 
-    String execPath = ourScript.getValue();
+    String execPath = String.format("\"%s\"", ourScript.getValue());
     if (execPath == null) {
       throw new RuntimeException(ApplicationBundle.message("desktop.entry.script.missing", binPath));
     }
