8235f60f1549f02f84da264f42baee2fa3889bef
Vasily Romanikhin <vasily.romanikhin@jetbrains.com>
2017-10-24 19:08:38 +0300

    CPP-744, CPP-6352: remote toolchain support; WSL: review fixes

--- platform/platform-tests/testSrc/com/intellij/execution/WSLUtilTest.java
+++ platform/platform-tests/testSrc/com/intellij/execution/WSLUtilTest.java
@@ -7,19 +7,20 @@ import com.intellij.execution.configurations.GeneralCommandLine;
 import com.intellij.execution.process.CapturingProcessHandler;
 import com.intellij.execution.process.ProcessOutput;
 import com.intellij.openapi.util.io.FileUtil;
-import com.intellij.testFramework.UsefulTestCase;
 import org.jetbrains.annotations.NotNull;
+import org.junit.Test;
 
 import java.io.File;
 
-public class WSLUtilTest extends UsefulTestCase {
+import static org.junit.Assert.*;
+import static org.junit.Assume.assumeTrue;
 
-  @Override
-  protected boolean shouldRunTest() {
-    return super.shouldRunTest() && WSLUtil.hasWSL();
-  }
+public class WSLUtilTest {
 
-  public void testWslToWinPath() throws Exception {
+  @Test
+  public void testWslToWinPath() {
+    assumeTrue(WSLUtil.hasWSL());
+
     assertWslPath("/usr/something/include", "%LOCALAPPDATA%\\lxss\\rootfs\\usr\\something\\include");
     assertWslPath("/usr/something/bin/gcc", "%LOCALAPPDATA%\\lxss\\rootfs\\usr\\something\\bin\\gcc");
 
@@ -32,7 +33,10 @@ public class WSLUtilTest extends UsefulTestCase {
     assertWslPath("/mnt/c/юникод", "c:\\юникод");
   }
 
+  @Test
   public void testWinToWslPath() {
+    assumeTrue(WSLUtil.hasWSL());
+
     assertWinPath("c:\\foo", "/mnt/c/foo");
     assertWinPath("c:\\temp\\KeepCase", "/mnt/c/temp/KeepCase");
 
@@ -40,7 +44,10 @@ public class WSLUtilTest extends UsefulTestCase {
     assertWinPath("%LOCALAPPDATA%\\lxss\\rootfs\\usr\\something\\bin\\gcc", "/usr/something/bin/gcc");
   }
 
+  @Test
   public void testPaths() {
+    assumeTrue(WSLUtil.hasWSL());
+
     final String originalWinPath = "c:\\usr\\something\\bin\\gcc";
     final String winPath = WSLUtil.getWindowsPath(WSLUtil.getWslPath(originalWinPath));
     assertEquals(originalWinPath, winPath);
@@ -50,13 +57,15 @@ public class WSLUtilTest extends UsefulTestCase {
     assertEquals(originalWslPath, wslPath);
   }
 
+  @Test
   public void testVersion() {
     final String version = WSLUtil.getWslVersion();
     assertTrue(WSLUtil.hasWSL() ? version != null : version == null);
   }
 
+  @Test
   public void testResolveSymlink() throws Exception {
-    if (!WSLUtil.hasWSL()) return;
+    assumeTrue(WSLUtil.hasWSL());
 
     final File winFile = FileUtil.createTempFile("the_file.txt", null);
     final File winSymlink = new File(new File(FileUtil.getTempDirectory()), "sym_link");
