0d94b37c18e1435f0d65b53fb37dc3cf99964dbd
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2017-10-24 17:45:35 +0300

    progress indicators are indeterminate by default
    more indicators fixed

--- java/java-impl/src/com/intellij/refactoring/move/moveClassesOrPackages/MoveClassesOrPackagesProcessor.java
+++ java/java-impl/src/com/intellij/refactoring/move/moveClassesOrPackages/MoveClassesOrPackagesProcessor.java
@@ -18,6 +18,8 @@ package com.intellij.refactoring.move.moveClassesOrPackages;
 import com.intellij.ide.util.EditorHelper;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.extensions.Extensions;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.Comparing;
@@ -453,6 +455,10 @@ public class MoveClassesOrPackagesProcessor extends BaseRefactoringProcessor {
 
     // Move files with correction of references.
 
+    ProgressIndicator indicator = ProgressManager.getInstance().getProgressIndicator();
+    if (indicator != null) {
+      indicator.setIndeterminate(false);
+    }
     try {
       final Map<PsiClass, Boolean> allClasses = new HashMap<>();
       for (PsiElement element : myElementsToMove) {
