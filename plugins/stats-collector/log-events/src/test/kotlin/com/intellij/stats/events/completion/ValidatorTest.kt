7fc296ea8d8fef8f5964c9d9c46694c6156a3a0d
Yaroslav Lepenkin <yaroslav.lepenkin@jetbrains.com>
2017-10-25 20:54:03 +0300

    version 530, fixed tests

--- plugins/stats-collector/log-events/src/test/kotlin/com/intellij/stats/events/completion/ValidatorTest.kt
+++ plugins/stats-collector/log-events/src/test/kotlin/com/intellij/stats/events/completion/ValidatorTest.kt
@@ -54,21 +54,21 @@ class ValidatorTest {
     fun testValidData() {
         val file = file("data/valid_data.txt")
         separator.filter(file.readLines())
-        assertThat(sessionStatuses["c4db55a9d878"]).isTrue()
+        assertThat(sessionStatuses["b007c3bf4550"]).isTrue()
     }
 
     @Test
     fun testDataWithAbsentFieldInvalid() {
         val file = file("data/absent_field.txt")
         separator.filter(file.readLines())
-        assertThat(sessionStatuses["c4db55a9d878"]).isFalse()
+        assertThat(sessionStatuses["b007c3bf4550"]).isFalse()
     }
 
     @Test
     fun testDataWithExtraFieldInvalid() {
         val file = file("data/extra_field.txt")
         separator.filter(file.readLines())
-        assertThat(sessionStatuses["c4db55a9d878"]).isFalse()
+        assertThat(sessionStatuses["b007c3bf4550"]).isFalse()
     }
 
 }
