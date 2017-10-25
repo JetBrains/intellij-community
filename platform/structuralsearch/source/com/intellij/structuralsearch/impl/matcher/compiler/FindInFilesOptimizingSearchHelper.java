01288a9bf08207e4da4a3671c9f727cc5e32ba88
Bas Leijdekkers <basleijdekkers@gmail.com>
2017-10-25 11:37:37 +0200

    SSR: fix index use problem (IDEA-181065)

--- platform/structuralsearch/source/com/intellij/structuralsearch/impl/matcher/compiler/FindInFilesOptimizingSearchHelper.java
+++ platform/structuralsearch/source/com/intellij/structuralsearch/impl/matcher/compiler/FindInFilesOptimizingSearchHelper.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.structuralsearch.impl.matcher.compiler;
 
 import com.intellij.openapi.project.Project;
@@ -67,7 +53,8 @@ class FindInFilesOptimizingSearchHelper extends OptimizingSearchHelperBase {
 
   @Override
   protected void doAddSearchWordInCode(final String refname) {
-    CacheManager.SERVICE.getInstance(myProject).processFilesWithWord(myFileProcessor, refname, UsageSearchContext.IN_CODE,
+    CacheManager.SERVICE.getInstance(myProject).processFilesWithWord(myFileProcessor, refname,
+                                                                     (short)(UsageSearchContext.IN_CODE | UsageSearchContext.IN_PLAIN_TEXT),
                                                                      (GlobalSearchScope)myScope, myCaseSensitive);
   }
 
@@ -93,7 +80,7 @@ class FindInFilesOptimizingSearchHelper extends OptimizingSearchHelperBase {
   public void endTransaction() {
     super.endTransaction();
     final THashSet<PsiFile> map = filesToScan;
-    if (map.size() > 0) map.clear();
+    if (!map.isEmpty()) map.clear();
     filesToScan = filesToScan2;
     filesToScan2 = map;
   }
