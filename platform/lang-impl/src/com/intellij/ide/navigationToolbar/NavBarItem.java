c89be8c71e541cf0e828c8ae9422e82abcc82a20
Renaud Paquay <rpaquay@google.com>
2017-10-26 17:09:00 +0300

    IDEA-181255 apply changes from AndroidStudio [3]
    a11y: Ensure navigation bar works nicely with screen readers

--- platform/lang-impl/src/com/intellij/ide/navigationToolbar/NavBarItem.java
+++ platform/lang-impl/src/com/intellij/ide/navigationToolbar/NavBarItem.java
@@ -25,11 +25,20 @@ import com.intellij.util.PlatformIcons;
 import com.intellij.util.containers.JBIterable;
 import com.intellij.util.ui.EmptyIcon;
 import com.intellij.util.ui.JBUI;
+import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import javax.accessibility.AccessibleAction;
+import javax.accessibility.AccessibleContext;
+import javax.accessibility.AccessibleRole;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.List;
 
 /**
  * @author Konstantin Bulenkov
@@ -73,6 +82,15 @@ public class NavBarItem extends SimpleColoredComponent implements DataProvider, 
       setMyBorder(null);
       setBorder(null);
       setPaintFocusBorder(false);
+      if (myPanel.allowNavItemsFocus()) {
+        // Take ownership of Tab/Shift-Tab navigation (to move focus out of nav bar panel), as
+        // navigation between items is handled by the Left/Right cursor keys. This is similar
+        // to the behavior a JRadioButton contained inside a GroupBox.
+        setFocusTraversalKeysEnabled(false);
+        setFocusable(true);
+        addKeyListener(new KeyHandler());
+        addFocusListener(new FocusHandler());
+      }
     }
 
     update();
@@ -174,8 +192,11 @@ public class NavBarItem extends SimpleColoredComponent implements DataProvider, 
   }
 
   public boolean isFocused() {
-    final Component focusOwner = KeyboardFocusManager.getCurrentKeyboardFocusManager().getFocusOwner();
-    return focusOwner == myPanel && !myPanel.isNodePopupActive();
+    if (myPanel.allowNavItemsFocus()) {
+      return UIUtil.isFocusAncestor(myPanel) && !myPanel.isNodePopupActive();
+    } else {
+      return myPanel.hasFocus() && !myPanel.isNodePopupActive();
+    }
   }
 
   public boolean isSelected() {
@@ -209,5 +230,100 @@ public class NavBarItem extends SimpleColoredComponent implements DataProvider, 
   @Override
   public Object getData(String dataId) {
     return myPanel.getDataImpl(dataId, () -> JBIterable.of(myObject));
+  }
+
+  @Override
+  public AccessibleContext getAccessibleContext() {
+    if (accessibleContext == null) {
+      accessibleContext = new AccessibleNavBarItem();
+    }
+    return accessibleContext;
+  }
+
+  protected class AccessibleNavBarItem extends AccessibleSimpleColoredComponent implements AccessibleAction {
+    @Override
+    public AccessibleRole getAccessibleRole() {
+      if (!isPopupElement()) {
+        return AccessibleRole.PUSH_BUTTON;
+      }
+      return super.getAccessibleRole();
+    }
+
+    @Override
+    public AccessibleAction getAccessibleAction() {
+      return this;
+    }
+
+    @Override
+    public int getAccessibleActionCount() {
+      return !isPopupElement() ? 1 : 0;
+    }
+
+    @Override
+    public String getAccessibleActionDescription(int i) {
+      if (i == 0 && !isPopupElement()) {
+        return UIManager.getString("AbstractButton.clickText");
+      }
+      return null;
+    }
+
+    @Override
+    public boolean doAccessibleAction(int i) {
+      if (i == 0 && !isPopupElement()) {
+        myPanel.getModel().setSelectedIndex(myIndex);
+      }
+      return false;
+    }
+  }
+
+  private class KeyHandler extends KeyAdapter {
+    // This listener checks if the key event is a KeyEvent.VK_TAB
+    // or shift + KeyEvent.VK_TAB event, consume the event
+    // if so and move the focus to next/previous component after/before
+    // the containing NavBarPanel.
+    @Override
+    public void keyPressed(KeyEvent e) {
+      if (e.getKeyCode() == KeyEvent.VK_TAB) {
+        // Check source is a nav bar item
+        if (e.getSource() instanceof NavBarItem) {
+          e.consume();
+          jumpToNextComponent(!e.isShiftDown());
+        }
+      }
+    }
+
+    void jumpToNextComponent(boolean next) {
+      // The base will be first or last NavBarItem in the NavBarPanel
+      NavBarItem focusBase = null;
+      List<NavBarItem> items = myPanel.getItems();
+      if (items.size() > 0) {
+        if (next) {
+          focusBase = items.get(items.size() - 1);
+        } else {
+          focusBase = items.get(0);
+        }
+      }
+
+      // Transfer focus
+      if (focusBase != null){
+        if (next) {
+          KeyboardFocusManager.getCurrentKeyboardFocusManager().focusNextComponent(focusBase);
+        } else {
+          KeyboardFocusManager.getCurrentKeyboardFocusManager().focusPreviousComponent(focusBase);
+        }
+      }
+    }
+  }
+
+  private class FocusHandler implements FocusListener {
+    @Override
+    public void focusGained(FocusEvent e) {
+      myPanel.fireNavBarItemFocusGained(e);
+    }
+
+    @Override
+    public void focusLost(FocusEvent e) {
+      myPanel.fireNavBarItemFocusLost(e);
+    }
   }
 }
