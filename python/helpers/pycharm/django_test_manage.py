1c9a8efa4981d5da07c80e31fe5c3682505633de
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2017-10-28 03:44:42 +0300

    PY-26660: Use real command "test" class, do not hardcode existing one
    Some apps (like) "django-tests-without-migrations" overwrite test command, so we use real command instead of hardcoded one
    
    (cherry picked from commit ca15153)

--- python/helpers/pycharm/django_test_manage.py
+++ python/helpers/pycharm/django_test_manage.py
@@ -23,7 +23,6 @@ except:
     pass
 
 from django.core import management
-from django.core.management.commands.test import Command
 
 try:
   # setup environment
@@ -56,51 +55,62 @@ if django.VERSION >= (1, 7):
         settings.configure()
     django.setup()
 
-class PycharmTestCommand(Command):
-  def get_runner(self):
-    TEST_RUNNER = 'django_test_runner.run_tests'
-    test_path = TEST_RUNNER.split('.')
-    # Allow for Python 2.5 relative paths
-    if len(test_path) > 1:
-      test_module_name = '.'.join(test_path[:-1])
-    else:
-      test_module_name = '.'
-    test_module = __import__(test_module_name, {}, {}, test_path[-1])
-    test_runner = getattr(test_module, test_path[-1])
-    return test_runner
 
-  def handle(self, *test_labels, **options):
-    # handle south migration in tests
-    commands = management.get_commands()
-    if hasattr(settings, "SOUTH_TESTS_MIGRATE") and not settings.SOUTH_TESTS_MIGRATE:
-      # point at the core syncdb command when creating tests
-      # tests should always be up to date with the most recent model structure
-      commands['syncdb'] = 'django.core'
-    elif 'south' in settings.INSTALLED_APPS:
-      try:
-        from south.management.commands import MigrateAndSyncCommand
-        commands['syncdb'] = MigrateAndSyncCommand()
-        from south.hacks import hacks
-        if hasattr(hacks, "patch_flush_during_test_db_creation"):
-          hacks.patch_flush_during_test_db_creation()
-      except ImportError:
+def _create_command():
+  """
+
+  Creates PycharmTestCommand that inherits real Command class.
+  Wrapped to method to make it is not called when module loaded but only when django fully initialized (lazy)
+
+  """
+  class PycharmTestCommand(ManagementUtility().fetch_command("test").__class__):
+    def get_runner(self):
+      TEST_RUNNER = 'django_test_runner.run_tests'
+      test_path = TEST_RUNNER.split('.')
+      # Allow for Python 2.5 relative paths
+      if len(test_path) > 1:
+        test_module_name = '.'.join(test_path[:-1])
+      else:
+        test_module_name = '.'
+      test_module = __import__(test_module_name, {}, {}, test_path[-1])
+      test_runner = getattr(test_module, test_path[-1])
+      return test_runner
+
+    def handle(self, *test_labels, **options):
+      # handle south migration in tests
+      commands = management.get_commands()
+      if hasattr(settings, "SOUTH_TESTS_MIGRATE") and not settings.SOUTH_TESTS_MIGRATE:
+        # point at the core syncdb command when creating tests
+        # tests should always be up to date with the most recent model structure
         commands['syncdb'] = 'django.core'
+      elif 'south' in settings.INSTALLED_APPS:
+        try:
+          from south.management.commands import MigrateAndSyncCommand
+          commands['syncdb'] = MigrateAndSyncCommand()
+          from south.hacks import hacks
+          if hasattr(hacks, "patch_flush_during_test_db_creation"):
+            hacks.patch_flush_during_test_db_creation()
+        except ImportError:
+          commands['syncdb'] = 'django.core'
 
-    verbosity = int(options.get('verbosity', 1))
-    interactive = options.get('interactive', True)
-    failfast = options.get('failfast', False)
-    TestRunner = self.get_runner()
+      verbosity = int(options.get('verbosity', 1))
+      interactive = options.get('interactive', True)
+      failfast = options.get('failfast', False)
+      TestRunner = self.get_runner()
 
-    if not inspect.ismethod(TestRunner):
-      our_options = {"verbosity" : int(verbosity), "interactive" : interactive, "failfast" : failfast}
-      options.update(our_options)
-      failures = TestRunner(test_labels, **options)
-    else:
-      test_runner = TestRunner(verbosity=verbosity, interactive=interactive, failfast=failfast)
-      failures = test_runner.run_tests(test_labels)
+      if not inspect.ismethod(TestRunner):
+        our_options = {"verbosity": int(verbosity), "interactive": interactive, "failfast": failfast}
+        options.update(our_options)
+        failures = TestRunner(test_labels, **options)
+      else:
+        test_runner = TestRunner(verbosity=verbosity, interactive=interactive, failfast=failfast)
+        failures = test_runner.run_tests(test_labels)
 
-    if failures:
-      sys.exit(bool(failures))
+      if failures:
+        sys.exit(bool(failures))
+
+  return PycharmTestCommand()
+
 
 class PycharmTestManagementUtility(ManagementUtility):
   def __init__(self, argv=None):
@@ -129,7 +139,8 @@ class PycharmTestManagementUtility(ManagementUtility):
       import _jb_runner_tools
       super(PycharmTestManagementUtility, self).execute()
     else:
-      PycharmTestCommand().run_from_argv(self.argv)
+      _create_command().run_from_argv(self.argv)
+
 
 if __name__ == "__main__":
 
