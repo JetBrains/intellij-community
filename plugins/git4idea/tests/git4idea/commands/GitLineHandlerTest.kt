574d8b071367cf995ee7e279b44883e1060a4a66
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2017-10-28 18:31:03 +0300

    [git] unify test examples

--- plugins/git4idea/tests/git4idea/commands/GitLineHandlerTest.kt
+++ plugins/git4idea/tests/git4idea/commands/GitLineHandlerTest.kt
@@ -9,37 +9,23 @@ import git4idea.test.GitSingleRepoTest
 class GitLineHandlerTest : GitSingleRepoTest() {
 
   fun `test lines split with LF`() {
-    `check handler with text`(listOf("line one\nline two\nline ", "three\nline four\nthe last one"),
-                              listOf("line one", "line two", "line three", "line four", "the last one"))
+    `check handler with text`(listOf("some lo", "ng line one\nso", "me lo", "ng line two", "\nsome long line three\n", "some lo", "n", "", "g line fou", "r\nthe last long line"),
+        listOf("some long line one", "some long line two", "some long line three", "some long line four", "the last long line"))
   }
 
   fun `test lines split with CRLF`() {
-    `check handler with text`(listOf("Do not go gentle", " into that good night,\r\nOld age should burn and rave at close of day;",
-                                     "\r\nRage, rage ", "against the ", "dying of the ", "light."),
-                              listOf("Do not go gentle into that good night,",
-                                     "Old age should burn and rave at close of day;",
-                                     "Rage, rage against the dying of the light."))
+    `check handler with text`(listOf("some", " long ", "line one\r\nsome long", " line two", "\r\n", "some long line thr", "ee\r\n", "some", " long li", "ne four\r\nth", "e last l", "ong line"),
+        listOf("some long line one", "some long line two", "some long line three", "some long line four", "the last long line"))
   }
 
   fun `test lines split with a mix`() {
-    `check handler with text`(listOf("A climber climbs", " with his guts, his brain, his so", "ul, and his feet\nAll",
-                                     " of the", "m bound for a cold and white world\n\rA world that is all ",
-                                     "up and up\n\rUp and up"),
-                              listOf("A climber climbs with his guts, his brain, his soul, and his feet",
-                                     "All of them bound for a cold and white world",
-                                     "A world that is all up and up",
-                                     "Up and up"))
+    `check handler with text`(listOf("some", " long line one\r\nso", "me long line two\n\r", "some long line three", "\n", "some long l", "ine four\r\nthe last ", "long line"),
+        listOf("some long line one", "some long line two", "some long line three", "some long line four", "the last long line"))
   }
 
   fun `test with a simple lines`() {
-    `check handler with text`(listOf("To be, or not to be, that is the question:\nWhether 'tis nobler in the mind to suffer\n" +
-                                     "The slings and arrows of outrageous fortune,\n" +
-                                     "Or to take arms against a sea of troubles\nAnd by opposing end them."),
-                              listOf("To be, or not to be, that is the question:",
-                                     "Whether 'tis nobler in the mind to suffer",
-                                     "The slings and arrows of outrageous fortune,",
-                                     "Or to take arms against a sea of troubles",
-                                     "And by opposing end them."))
+    `check handler with text`(listOf("some long line one\n", "some long line two\nsome long line three\n", "some long line four\nthe last long line"),
+        listOf("some long line one", "some long line two", "some long line three", "some long line four", "the last long line"))
   }
 
   private fun `check handler with text`(text: List<String>, expectedLines: List<String>) {
