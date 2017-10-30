8c0b059b4bdac36b5bf502cff0b8ff2dfc395a56
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2017-10-30 14:05:13 +0100

    [vfs] better alternative to VfsUtil.getPath() method (IDEA-180738)

--- platform/core-api/src/com/intellij/openapi/vfs/VfsUtilCore.java
+++ platform/core-api/src/com/intellij/openapi/vfs/VfsUtilCore.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vfs;
 
 import com.intellij.openapi.diagnostic.Logger;
@@ -178,6 +164,48 @@ public class VfsUtilCore {
       parent = parent.getParent();
     }
     return StringFactory.createShared(chars);
+  }
+
+  /**
+   * Returns the relative path from one virtual file to another.
+   * If {@code src} is a file, the path is calculated from its parent directory.
+   *
+   * @param src           the file or directory, from which the path is built
+   * @param dst           the file or directory, to which the path is built
+   * @param separatorChar the separator for the path components
+   * @return the relative path, or {@code null} if the files have no common ancestor
+   * @since 2018.1
+   */
+  @Nullable
+  public static String findRelativePath(@NotNull VirtualFile src, @NotNull VirtualFile dst, char separatorChar) {
+    if (!src.getFileSystem().equals(dst.getFileSystem())) {
+      return null;
+    }
+
+    if (!src.isDirectory()) {
+      src = src.getParent();
+      if (src == null) return null;
+    }
+
+    VirtualFile commonAncestor = getCommonAncestor(src, dst);
+    if (commonAncestor == null) return null;
+
+    StringBuilder buffer = new StringBuilder();
+
+    if (!Comparing.equal(src, commonAncestor)) {
+      while (!Comparing.equal(src, commonAncestor)) {
+        buffer.append("..").append(separatorChar);
+        src = src.getParent();
+      }
+    }
+
+    buffer.append(getRelativePath(dst, commonAncestor, separatorChar));
+
+    if (StringUtil.endsWithChar(buffer, separatorChar)) {
+      buffer.setLength(buffer.length() - 1);
+    }
+
+    return buffer.toString();
   }
 
   @Nullable
