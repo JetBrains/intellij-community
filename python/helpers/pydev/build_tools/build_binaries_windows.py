2222d3b8ac616a4cca46981c2296cca37a9bdc8b
fitermay <fiterman@gmail.com>
2017-10-30 15:33:27 -0400

    Build Pydevd windows binaries for Python 2.6

--- python/helpers/pydev/build_tools/build_binaries_windows.py
+++ python/helpers/pydev/build_tools/build_binaries_windows.py
@@ -72,11 +72,13 @@ miniconda32_envs = os.getenv('MINICONDA32_ENVS', r'C:\tools\Miniconda32\envs')
 miniconda64_envs = os.getenv('MINICONDA64_ENVS', r'C:\tools\Miniconda\envs')
 
 python_installations = [
+    r'%s\py26_32\python.exe' % miniconda32_envs,
     r'%s\py27_32\python.exe' % miniconda32_envs,
     r'%s\py34_32\python.exe' % miniconda32_envs,
     r'%s\py35_32\python.exe' % miniconda32_envs,
     r'%s\py36_32\python.exe' % miniconda32_envs,
 
+    r'%s\py26_64\python.exe' % miniconda64_envs,
     r'%s\py27_64\python.exe' % miniconda64_envs,
     r'%s\py34_64\python.exe' % miniconda64_envs,
     r'%s\py35_64\python.exe' % miniconda64_envs,
