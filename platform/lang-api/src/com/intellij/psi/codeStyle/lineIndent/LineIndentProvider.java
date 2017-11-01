9039b0421442a23b8a0f329c34f58f08fad2211d
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2017-11-01 17:15:43 +0300

    Add a possibility to cancel indent adjustment from LineIndentProvider

--- platform/lang-api/src/com/intellij/psi/codeStyle/lineIndent/LineIndentProvider.java
+++ platform/lang-api/src/com/intellij/psi/codeStyle/lineIndent/LineIndentProvider.java
@@ -25,6 +25,10 @@ import org.jetbrains.annotations.Nullable;
  * An interface for indentation calculation. Used in editor actions like Enter handling.
  */
 public interface LineIndentProvider {
+  /**
+   * Marker object to indicate that no further formatter-based indent adjustment must be made.
+   */
+  String DO_NOT_ADJUST = new String();
   
   /**
    * Calculates the indent that should be used for the line at specified offset in the specified
@@ -34,8 +38,9 @@ public interface LineIndentProvider {
    * @param editor   The current editor.
    * @param language Context language to be used at the current offset.
    * @param offset   The caret offset in the editor.
-   * @return The indent string (possibly consisting of tabs and/or white spaces), or null if
-   * LineIndentProvider can't calculate the indent (in this case no indent adjustment will be made).
+   * @return The indent string (possibly consisting of tabs and/or white spaces), {@code null} if LineIndentProvider can't calculate the
+   * indent (in this case indent calculation is delegated to formatter if smart indent mode is used) or {@link #DO_NOT_ADJUST} constant to
+   * leave the current caret position as is without any further formatter-based adjustment.
    */
   @Nullable
   String getLineIndent(@NotNull Project project, @NotNull Editor editor, Language language, int offset);
