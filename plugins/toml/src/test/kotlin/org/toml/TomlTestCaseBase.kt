6d6855a563474b2cf4525b718561b4e62d57ace9
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-11-03 23:49:16 +0300

    Drop old parser tests
    GitOrigin-RevId: a9eec3982c2f3524d4c3a93019a33289b0c459b7

--- plugins/toml/src/test/kotlin/org/toml/TomlTestCaseBase.kt
+++ plugins/toml/src/test/kotlin/org/toml/TomlTestCaseBase.kt
@@ -9,13 +9,12 @@ import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase
 
 abstract class TomlTestCaseBase : LightCodeInsightFixtureTestCase() {
 
-    final protected val fileName: String get() = "${camelToSnake(getTestName(true))}.toml"
+    protected val fileName: String get() = "${camelToSnake(getTestName(true))}.toml"
 
     companion object {
         @JvmStatic
         fun camelToSnake(camelCaseName: String): String =
             camelCaseName.split("(?=[A-Z])".toRegex())
-                .map { it.toLowerCase() }
-                .joinToString("_")
+                .joinToString("_") { it.toLowerCase() }
     }
 }
