286c79b616edbb5df3d7a8c03888226dc70c6126
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-11-04 01:18:42 +0300

    TOML: better toString for PSI elements
    GitOrigin-RevId: 9a651e590b8a1702f0370e8ed527280c10ec153b

--- plugins/toml/src/main/kotlin/org/toml/lang/psi/impl/Psi.kt
+++ plugins/toml/src/main/kotlin/org/toml/lang/psi/impl/Psi.kt
@@ -20,22 +20,30 @@ class TomlKeyValueImpl(node: ASTNode) : ASTWrapperPsiElement(node), TomlKeyValue
 
     override val value: TomlValue?
         get() = findChildByType(KEY)
+
+    override fun toString(): String = "TomlKeyValue"
 }
 
 
 class TomlKeyImpl(node: ASTNode) : ASTWrapperPsiElement(node), TomlKey {
     override fun getReferences(): Array<PsiReference>
         = ReferenceProvidersRegistry.getReferencesFromProviders(this)
+
+    override fun toString(): String = "TomlKey"
 }
 
 class TomlLiteralImpl(node: ASTNode) : ASTWrapperPsiElement(node), TomlLiteral {
     override fun getReferences(): Array<PsiReference>
         = ReferenceProvidersRegistry.getReferencesFromProviders(this)
+
+    override fun toString(): String = "TomlLiteral"
 }
 
 class TomlArrayImpl(node: ASTNode) : ASTWrapperPsiElement(node), TomlArray {
     override val elements: List<TomlValue>
         get() = findChildrenByType(VALUES)
+
+    override fun toString(): String = "TomlArray"
 }
 
 class TomlTableImpl(node: ASTNode) : ASTWrapperPsiElement(node), TomlTable {
@@ -44,16 +52,22 @@ class TomlTableImpl(node: ASTNode) : ASTWrapperPsiElement(node), TomlTable {
 
     override val entries: List<TomlKeyValue>
         get() = findChildrenByType(KEY_VALUE)
+
+    override fun toString(): String = "TomlTable"
 }
 
 class TomlTableHeaderImpl(node: ASTNode) : ASTWrapperPsiElement(node), TomlTableHeader {
     override val names: List<TomlKey>
         get() = findChildrenByType(KEY)
+
+    override fun toString(): String = "TomlTableHeader"
 }
 
 class TomlInlineTableImpl(node: ASTNode) : ASTWrapperPsiElement(node), TomlInlineTable {
     override val entries: List<TomlKeyValue>
         get() = findChildrenByType(KEY_VALUE)
+
+    override fun toString(): String = "TomlInlineTable"
 }
 
 class TomlArrayTableImpl(node: ASTNode) : ASTWrapperPsiElement(node), TomlArrayTable {
@@ -62,6 +76,8 @@ class TomlArrayTableImpl(node: ASTNode) : ASTWrapperPsiElement(node), TomlArrayT
 
     override val entries: List<TomlKeyValue>
         get() = findChildrenByType(KEY_VALUE)
+
+    override fun toString(): String = "TomlArrayTable"
 }
 
 fun createPsiElement(node: ASTNode) = when (node.elementType) {
