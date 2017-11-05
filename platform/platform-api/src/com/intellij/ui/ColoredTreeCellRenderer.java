6bd13423ff9a95ec2f92cd7737571fdd8b6d7aeb
Konstantin Bulenkov <kb@jetbrains.com>
2017-11-05 13:32:14 +0100

    remove Nimbus LnF

--- platform/platform-api/src/com/intellij/ui/ColoredTreeCellRenderer.java
+++ platform/platform-api/src/com/intellij/ui/ColoredTreeCellRenderer.java
@@ -1,18 +1,16 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.intellij.ui;
 
 import com.intellij.util.ui.EmptyIcon;
@@ -96,10 +94,6 @@ public abstract class ColoredTreeCellRenderer extends SimpleColoredComponent imp
 
     if (UIUtil.isUnderGTKLookAndFeel()){
       super.setOpaque(false);  // avoid nasty background
-      super.setIconOpaque(false);
-    }
-    else if (UIUtil.isUnderNimbusLookAndFeel() && selected && hasFocus) {
-      super.setOpaque(false);  // avoid erasing Nimbus focus frame
       super.setIconOpaque(false);
     }
     else if (WideSelectionTreeUI.isWideSelection(tree)) {
