5ca6f3dc794e1023ccd477dd7706329fb51a6a67
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2017-11-06 20:10:55 +0300

    PY-12002 KeywordArgumentCompletionUtil operates on type level
    (cherry picked from commit 891c4bf)

--- python/testData/inspections/PyTypeCheckerInspection/MapReturnElementType.py
+++ python/testData/inspections/PyTypeCheckerInspection/MapReturnElementType.py
@@ -1,5 +1,5 @@
 def test():
     xs = map(lambda x: x + 1, [1, 2, 3])
     print('foo' + xs[0])
-    ys = map(tuple, iter([1, 2, 3]))
+    ys = map(tuple, iter([[1, 2, 3]]))
     print(1 + <warning descr="Expected type 'int', got 'tuple' instead">ys[0]</warning>, 'bar' + <warning descr="Expected type 'AnyStr', got 'tuple' instead">ys[1]</warning>)
