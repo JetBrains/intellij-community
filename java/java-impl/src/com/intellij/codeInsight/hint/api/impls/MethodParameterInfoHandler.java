b070d05a0ded9090f2f0a220d16e1f223136d674
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2017-11-07 19:34:29 +0300

    IDEA-180895 Redundant parameter hint showing
    (updates following IDEA-CR-25831)

--- java/java-impl/src/com/intellij/codeInsight/hint/api/impls/MethodParameterInfoHandler.java
+++ java/java-impl/src/com/intellij/codeInsight/hint/api/impls/MethodParameterInfoHandler.java
@@ -189,7 +189,7 @@ public class MethodParameterInfoHandler implements ParameterInfoHandlerWithTabAc
               PsiMethod chosenMethod = CompletionMemory.getChosenMethod((PsiCall)parent);
               if (chosenMethod != null) {
                 int parametersCount = chosenMethod.getParameterList().getParametersCount();
-                if ((parametersCount == 1 || parametersCount == 2 && chosenMethod.isVarArgs()) && 
+                if ((parametersCount == 1 && !chosenMethod.isVarArgs() || parametersCount == 2 && chosenMethod.isVarArgs()) && 
                                             !overloadWithNoParametersExists(chosenMethod, context.getObjectsToView())) return false;
               }
             }
@@ -203,7 +203,10 @@ public class MethodParameterInfoHandler implements ParameterInfoHandlerWithTabAc
   private static boolean overloadWithNoParametersExists(PsiMethod method, Object[] candidates) {
     String methodName = method.getName();
     return ContainerUtil.find(candidates, c -> {
-      PsiMethod m = (PsiMethod)((CandidateInfo)c).getElement();
+      if (!(c instanceof CandidateInfo)) return false;
+      PsiElement e = ((CandidateInfo)c).getElement();
+      if (!(e instanceof PsiMethod)) return false;
+      PsiMethod m = (PsiMethod)e;
       return m.getParameterList().getParametersCount() == 0 && m.getName().equals(methodName);
     }) != null;
   }
