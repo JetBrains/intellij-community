c3773077b6f1d29e4ab5c13842679b987b908a1d
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2017-11-08 12:17:34 +0300

    IDEA-180573 "Navigate forward" doesn't work after "Navigate back"

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/IdeDocumentHistoryImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/IdeDocumentHistoryImpl.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.fileEditor.impl;
 
 import com.intellij.ide.ui.UISettings;
@@ -323,12 +309,7 @@ public class IdeDocumentHistoryImpl extends IdeDocumentHistory implements Projec
     final PlaceInfo info = myBackPlaces.removeLast();
 
     PlaceInfo current = getCurrentPlaceInfo();
-    if (current != null) {
-      if (!isSame(current, info)) {
-        putLastOrMerge(myForwardPlaces, current, Integer.MAX_VALUE);
-      }
-    }
-    putLastOrMerge(myForwardPlaces, info, Integer.MAX_VALUE);
+    if (current != null) myForwardPlaces.add(current);
 
     myBackInProgress = true;
     try {
