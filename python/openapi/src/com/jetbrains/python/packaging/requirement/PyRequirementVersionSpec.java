796bdfd6188cddf8a8adce1cb493e816b23fb4f4
Semyon Proshev <semyon.proshev@jetbrains.com>
2017-11-09 16:09:26 +0300

    Announce api changes in packaging (PY-22773)
    (cherry picked from commit 1f0aa91)
    
    (cherry picked from commit a2be2ad)

--- python/openapi/src/com/jetbrains/python/packaging/requirement/PyRequirementVersionSpec.java
+++ python/openapi/src/com/jetbrains/python/packaging/requirement/PyRequirementVersionSpec.java
@@ -1,18 +1,16 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.jetbrains.python.packaging.requirement;
 
 import com.intellij.openapi.util.Pair;
@@ -24,6 +22,9 @@ import java.util.Objects;
 
 import static com.intellij.webcore.packaging.PackageVersionComparator.VERSION_COMPARATOR;
 
+/**
+ * @apiNote This class will be converted to interface in 2018.2.
+ */
 public class PyRequirementVersionSpec {
 
   @NotNull
@@ -35,12 +36,20 @@ public class PyRequirementVersionSpec {
   @NotNull
   private final String myVersion;
 
+  /**
+   * @deprecated Use {@link com.jetbrains.python.packaging.PyRequirement} instead.
+   * This constructor will be removed in 2018.2.
+   */
   public PyRequirementVersionSpec(@NotNull PyRequirementRelation relation, @NotNull PyRequirementVersion version) {
     myRelation = relation;
     myParsedVersion = version;
     myVersion = myParsedVersion.getPresentableText();
   }
 
+  /**
+   * @deprecated Use {@link com.jetbrains.python.packaging.PyRequirement} instead.
+   * This constructor will be removed in 2018.2.
+   */
   public PyRequirementVersionSpec(@NotNull String version) {
     myRelation = PyRequirementRelation.STR_EQ;
     myParsedVersion = null;
