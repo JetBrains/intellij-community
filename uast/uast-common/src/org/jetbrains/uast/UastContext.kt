5c245f316fe6098028e21272d39664072e2cd264
Nicolay Mitropolsky <nicolay.mitropolsky@jetbrains.com>
2017-11-09 13:54:42 +0100

    Uast: `findPlugin` workaround for KT-18054

--- uast/uast-common/src/org/jetbrains/uast/UastContext.kt
+++ uast/uast-common/src/org/jetbrains/uast/UastContext.kt
@@ -42,7 +42,11 @@ class UastContext(val project: Project) : UastLanguagePlugin {
     get() = UastLanguagePlugin.getInstances()
 
   fun findPlugin(element: PsiElement): UastLanguagePlugin? {
-    val language = (element.containingFile ?: element).language // because sometimes java elements are used in another files (see Drools)
+    // we searching for plugin for file because sometimes java elements are used in another languages (see Drools)
+    val containingFile = element.containingFile?.takeIf {
+      it !is PsiCompiledFile // Dont trust compiled files because of KT-18054
+    }
+    val language = (containingFile ?: element).language
     return languagePlugins.firstOrNull { it.language == language }
   }
 
