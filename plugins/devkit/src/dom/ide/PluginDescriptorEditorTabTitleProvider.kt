78b176c8ff44b3c3df0148eb6855c372975dd868
Yann Cébron <yann.cebron@jetbrains.com>
2017-11-10 10:46:51 +0100

    IDEA-181935 Devkit: show ID/Name of plugin for plugin.xml files in editor tab
    move

--- plugins/devkit/src/dom/ide/PluginDescriptorEditorTabTitleProvider.kt
+++ plugins/devkit/src/dom/ide/PluginDescriptorEditorTabTitleProvider.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.idea.devkit
+package org.jetbrains.idea.devkit.dom.ide
 
 import com.intellij.openapi.fileEditor.impl.EditorTabTitleProvider
 import com.intellij.openapi.project.Project
