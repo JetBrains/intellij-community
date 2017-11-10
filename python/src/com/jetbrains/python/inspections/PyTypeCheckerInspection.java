872824ca2bd8f7b5eb148fceddbd210105c6c427
Semyon Proshev <semyon.proshev@jetbrains.com>
2017-11-11 00:00:06 +0300

    Remove unused type eval context
    (cherry picked from commit 4f2ab91)
    
    (cherry picked from commit ce9e2d2)

--- python/src/com/jetbrains/python/inspections/PyTypeCheckerInspection.java
+++ python/src/com/jetbrains/python/inspections/PyTypeCheckerInspection.java
@@ -1,6 +1,4 @@
-// Copyright 2000-2017 JetBrains s.r.o.
-// Use of this source code is governed by the Apache 2.0 license that can be
-// found in the LICENSE file.
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.python.inspections;
 
 import com.intellij.codeInspection.LocalInspectionToolSession;
@@ -99,7 +97,7 @@ public class PyTypeCheckerInspection extends PyInspection {
       final PyType returnType = myTypeEvalContext.getReturnType(function);
 
       if (function.isAsync() || function.isGenerator()) {
-        return Ref.deref(PyTypingTypeProvider.coroutineOrGeneratorElementType(returnType, myTypeEvalContext));
+        return Ref.deref(PyTypingTypeProvider.coroutineOrGeneratorElementType(returnType));
       }
 
       return returnType;
