841785401aacdad6975b139385517c7d736b2a12
Egor Ushakov <egor.ushakov@jetbrains.com>
2017-11-10 20:25:10 +0300

    IDEA-180561 VirtualMachineProxy classesByName and allClasses may be outdated - removed the class by name cache, creating a separated cache for method breakpoints
    (cherry picked from commit f39ef16) (IDEA-CR-26645)

--- java/debugger/impl/src/com/intellij/debugger/ui/breakpoints/MethodBreakpoint.java
+++ java/debugger/impl/src/com/intellij/debugger/ui/breakpoints/MethodBreakpoint.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 /*
  * Class MethodBreakpoint
@@ -32,6 +18,7 @@ import com.intellij.debugger.engine.evaluation.EvaluationContextImpl;
 import com.intellij.debugger.engine.requests.RequestManagerImpl;
 import com.intellij.debugger.impl.DebuggerUtilsEx;
 import com.intellij.debugger.impl.PositionUtil;
+import com.intellij.debugger.jdi.ClassesByName;
 import com.intellij.debugger.jdi.MethodBytecodeUtil;
 import com.intellij.debugger.requests.Requestor;
 import com.intellij.icons.AllIcons;
@@ -75,7 +62,7 @@ import javax.swing.*;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.function.Consumer;
+import java.util.function.BiConsumer;
 import java.util.stream.Stream;
 
 public class MethodBreakpoint extends BreakpointWithHighlighter<JavaMethodBreakpointProperties> implements MethodBreakpointBase {
@@ -134,9 +121,6 @@ public class MethodBreakpoint extends BreakpointWithHighlighter<JavaMethodBreakp
     if (request != null) {
       requestsManager.registerRequest(breakpoint, request);
       request.enable();
-      if (!Registry.is("debugger.classes.cache.fix")) {
-        debugProcess.getVirtualMachineProxy().clearCaches(); // to force reload classes available so far
-      }
     }
 
     AtomicReference<ProgressWindow> indicatorRef = new AtomicReference<>();
@@ -172,7 +156,8 @@ public class MethodBreakpoint extends BreakpointWithHighlighter<JavaMethodBreakp
     XDebuggerManager.getInstance(debugProcess.getProject()).getBreakpointManager().addBreakpointListener(listener, indicator);
     ProgressManager.getInstance().executeProcessUnderProgress(
       () -> processPreparedSubTypes(baseType,
-                                    subType -> createRequestForPreparedClassEmulated(breakpoint, debugProcess, subType, false),
+                                    (subType, classesByName) ->
+                                      createRequestForPreparedClassEmulated(breakpoint, debugProcess, subType, classesByName, false),
                                     indicator),
       indicator);
     if (indicator.isCanceled() && !changed.get()) {
@@ -189,11 +174,19 @@ public class MethodBreakpoint extends BreakpointWithHighlighter<JavaMethodBreakp
                                                     @NotNull DebugProcessImpl debugProcess,
                                                     @NotNull ReferenceType classType,
                                                     boolean base) {
+    createRequestForPreparedClassEmulated(breakpoint, debugProcess, classType, debugProcess.getVirtualMachineProxy().getClassesByName(), base);
+  }
+
+  static void createRequestForPreparedClassEmulated(@NotNull MethodBreakpointBase breakpoint,
+                                                    @NotNull DebugProcessImpl debugProcess,
+                                                    @NotNull ReferenceType classType,
+                                                    @NotNull ClassesByName classesByName,
+                                                    boolean base) {
     if (!base && !shouldCreateRequest(breakpoint, breakpoint.getXBreakpoint(), debugProcess, true)) {
       return;
     }
     try {
-      Method lambdaMethod = MethodBytecodeUtil.getLambdaMethod(classType, debugProcess.getVirtualMachineProxy());
+      Method lambdaMethod = MethodBytecodeUtil.getLambdaMethod(classType, classesByName);
       if (lambdaMethod != null &&
           !breakpoint
             .matchingMethods(StreamEx.of(((ClassType)classType).interfaces()).flatCollection(ReferenceType::allMethods), debugProcess)
@@ -210,7 +203,7 @@ public class MethodBreakpoint extends BreakpointWithHighlighter<JavaMethodBreakp
           breakpoint.disableEmulation();
           return;
         }
-        Method target = MethodBytecodeUtil.getBridgeTargetMethod(method, debugProcess.getVirtualMachineProxy());
+        Method target = MethodBytecodeUtil.getBridgeTargetMethod(method, classesByName);
         if (target != null && !ContainerUtil.isEmpty(DebuggerUtilsEx.allLineLocations(target))) {
           method = target;
         }
@@ -578,7 +571,9 @@ public class MethodBreakpoint extends BreakpointWithHighlighter<JavaMethodBreakp
     return StreamEx.empty();
   }
 
-  private static void processPreparedSubTypes(ReferenceType classType, Consumer<ReferenceType> consumer, ProgressIndicator progressIndicator) {
+  private static void processPreparedSubTypes(ReferenceType classType,
+                                              BiConsumer<ReferenceType, ClassesByName> consumer,
+                                              ProgressIndicator progressIndicator) {
     long start = 0;
     if (LOG.isDebugEnabled()) {
       start = System.currentTimeMillis();
@@ -603,11 +598,13 @@ public class MethodBreakpoint extends BreakpointWithHighlighter<JavaMethodBreakp
 
       progressIndicator.setText(DebuggerBundle.message("label.method.breakpoints.setting.breakpoints"));
 
+      ClassesByName classesByName = ClassesByName.createCache(allTypes);
+
       for (int i = 0; i < types.size(); i++) {
         if (progressIndicator.isCanceled()) {
           return;
         }
-        consumer.accept(types.get(i));
+        consumer.accept(types.get(i), classesByName);
 
         progressIndicator.setText2(i + "/" + types.size());
         progressIndicator.setFraction((double)i / types.size());
