7711f918031e7983fb3fa15393f55346355bac0d
Yann Cébron <yann.cebron@jetbrains.com>
2017-11-10 10:52:49 +0100

    [devkit] rename/move UseScopeEnlarger

--- plugins/devkit/src/dom/ide/PluginDescriptorUseScopeEnlarger.java
+++ plugins/devkit/src/dom/ide/PluginDescriptorUseScopeEnlarger.java
@@ -1,19 +1,5 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.jetbrains.idea.devkit;
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.idea.devkit.dom.ide;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -36,7 +22,7 @@ import org.jetbrains.idea.devkit.util.PsiUtil;
 
 import java.util.Collection;
 
-public class DevKitUseScopeEnlarger extends UseScopeEnlarger {
+public class PluginDescriptorUseScopeEnlarger extends UseScopeEnlarger {
 
   @Override
   public SearchScope getAdditionalUseScope(@NotNull PsiElement element) {
