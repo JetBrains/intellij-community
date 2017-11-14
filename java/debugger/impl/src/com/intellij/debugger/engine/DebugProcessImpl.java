764ee833937ce78b60892b433e6e947053a95ce0
Egor Ushakov <egor.ushakov@jetbrains.com>
2017-11-14 19:30:27 +0300

    IDEA-181643 When running multiple apps using run dashboard if the apps do not have "before launch build" enabled some will fail to launch

--- java/debugger/impl/src/com/intellij/debugger/engine/DebugProcessImpl.java
+++ java/debugger/impl/src/com/intellij/debugger/engine/DebugProcessImpl.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.debugger.engine;
 
 import com.intellij.Patches;
@@ -27,6 +13,7 @@ import com.intellij.debugger.engine.jdi.ThreadReferenceProxy;
 import com.intellij.debugger.engine.requests.MethodReturnValueWatcher;
 import com.intellij.debugger.engine.requests.RequestManagerImpl;
 import com.intellij.debugger.impl.*;
+import com.intellij.debugger.jdi.EmptyConnectorArgument;
 import com.intellij.debugger.jdi.StackFrameProxyImpl;
 import com.intellij.debugger.jdi.ThreadReferenceProxyImpl;
 import com.intellij.debugger.jdi.VirtualMachineProxyImpl;
@@ -474,6 +461,11 @@ public abstract class DebugProcessImpl extends UserDataHolderBase implements Deb
         final Connector.Argument portArg = myConnection.isUseSockets() ? myArguments.get("port") : myArguments.get("name");
         if (portArg != null) {
           portArg.setValue(address);
+
+          // to allow connector to listen on several auto generated addresses
+          if (address.length() == 0 || address.equals("0")) {
+            myArguments.put("argForUniqueness", new EmptyConnectorArgument());
+          }
         }
         //noinspection HardCodedStringLiteral
         final Connector.Argument timeoutArg = myArguments.get("timeout");
