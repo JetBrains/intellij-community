7849ab19d4eac4a4b26bfd28c2e4fc234ffb2adb
Dmitry.Krasilschikov <kradima@jetbrains.com>
2017-11-15 15:11:49 +0300

    idea welcome screen: IDEA-CR-25633 cleanup

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/FlatWelcomeFrame.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/FlatWelcomeFrame.java
@@ -919,7 +919,6 @@ public class FlatWelcomeFrame extends JFrame implements IdeFrame, Disposable, Ac
                                         @NotNull JPanel bottomPanel,
                                         @Nullable Runnable backAction) {
     bottomPanel.removeAll();
-    bottomPanel.setPreferredSize(JBUI.size(-1, UIUtil.isUnderDarcula() ? 44 : 40));
 
     if (SystemInfoRt.isUnix) {
       addCancelButton(bottomPanel, backAction);
@@ -947,11 +946,11 @@ public class FlatWelcomeFrame extends JFrame implements IdeFrame, Disposable, Ac
   }
 
   @Nullable
-  private static JComponent createCancelButton(@Nullable Runnable backAction) {
-    if (backAction == null) return null;
+  private static JComponent createCancelButton(@Nullable Runnable cancelAction) {
+    if (cancelAction == null) return null;
 
     JButton cancelButton = new JButton("Cancel");
-    cancelButton.addActionListener(e -> backAction.run());
+    cancelButton.addActionListener(e -> cancelAction.run());
 
     return cancelButton;
   }
