6a31a529006b408a6c9bee4f629212a2821a45a3
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2017-11-16 17:27:27 +0300

    IDEA-156074 "Cannot redo. Unexpected document state"

--- platform/platform-impl/src/com/intellij/openapi/command/impl/CommandMerger.java
+++ platform/platform-impl/src/com/intellij/openapi/command/impl/CommandMerger.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.command.impl;
 
 import com.intellij.openapi.command.UndoConfirmationPolicy;
@@ -67,16 +53,16 @@ public class CommandMerger {
   }
 
   public void commandFinished(String commandName, Object groupId, @NotNull CommandMerger nextCommandToMerge) {
+    // we do not want to spoil redo stack in situation, when some 'transparent' actions occurred right after undo.
+    if (!nextCommandToMerge.isTransparent() && nextCommandToMerge.hasActions()) clearRedoStacks(nextCommandToMerge);
+
     if (!shouldMerge(groupId, nextCommandToMerge)) {
       flushCurrentCommand();
       myManager.compact();
     }
     merge(nextCommandToMerge);
 
-    // we do not want to spoil redo stack in situation, when some 'transparent' actions occurred right after undo.
     if (nextCommandToMerge.isTransparent() || !hasActions()) return;
-
-    clearRedoStacks(nextCommandToMerge);
 
     myLastGroupId = groupId;
     if (myCommandName == null) myCommandName = commandName;
@@ -169,7 +155,7 @@ public class CommandMerger {
   }
 
   private void clearRedoStacks(@NotNull CommandMerger nextMerger) {
-    myManager.getRedoStacksHolder().clearStacks(isGlobal(), nextMerger.myAllAffectedDocuments);
+    myManager.getRedoStacksHolder().clearStacks(nextMerger.isGlobal(), nextMerger.myAllAffectedDocuments);
   }
 
   boolean isGlobal() {
@@ -207,7 +193,7 @@ public class CommandMerger {
     // are not dropped, since this means they did not occur after undo/redo
     UndoRedo undoRedo;
     while ((undoRedo = createUndoOrRedo(editor, true)) != null) {
-      if (!undoRedo.isTransparent()) break;
+      if (!undoRedo.isTemporary()) break;
       if (!undoRedo.execute(true, false)) return;
       if (!undoRedo.hasMoreActions()) break;
     }
