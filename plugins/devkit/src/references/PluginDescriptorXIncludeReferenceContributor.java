cca2d61aa3ea03ca299442234a45a5a770dac277
Yaroslav Pankratyev <yaroslav.pankratyev@jetbrains.com>
2017-11-16 13:37:41 +0700

    IDEA-166677 Resolve XIncludes in plugin.xml files relatively to resource roots

---
+++ plugins/devkit/src/references/PluginDescriptorXIncludeReferenceContributor.java
@@ -0,0 +1,52 @@
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.idea.devkit.references;
+
+import com.intellij.patterns.PatternCondition;
+import com.intellij.patterns.XmlAttributeValuePattern;
+import com.intellij.patterns.XmlPatterns;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.PsiReferenceContributor;
+import com.intellij.psi.PsiReferenceRegistrar;
+import com.intellij.psi.impl.source.resolve.reference.impl.providers.FileReferenceSet;
+import com.intellij.psi.impl.source.resolve.reference.impl.providers.XmlBaseReferenceProvider;
+import com.intellij.psi.xml.XmlAttributeValue;
+import com.intellij.util.ProcessingContext;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.idea.devkit.util.PsiUtil;
+
+/**
+ * XIncludes in plugin.xml files are resolved in a bit different way than usually.
+ * This class along with {@link PluginDescriptorXIncludeFileReferenceHelper} provides the way
+ * to resolve 'href's in 'include' tags relatively to module resource roots.
+ */
+public class PluginDescriptorXIncludeReferenceContributor extends PsiReferenceContributor {
+  @Override
+  public void registerReferenceProviders(@NotNull PsiReferenceRegistrar registrar) {
+    registrar.registerReferenceProvider(getPattern(), new XIncludeReferenceProvider(), PsiReferenceRegistrar.HIGHER_PRIORITY);
+  }
+
+  private static XmlAttributeValuePattern getPattern() {
+    return XmlPatterns.xmlAttributeValue().withLocalName("href")
+      .inside(true, XmlPatterns.xmlTag().withLocalName("include"))
+      .with(new PatternCondition<XmlAttributeValue>("XInclude inside plugin.xml") {
+        @Override
+        public boolean accepts(@NotNull XmlAttributeValue value, ProcessingContext context) {
+          return PsiUtil.isPluginXmlPsiElement(value);
+        }
+      });
+  }
+
+  private static class XIncludeReferenceProvider extends XmlBaseReferenceProvider {
+    public XIncludeReferenceProvider() {
+      super(true);
+    }
+
+    @NotNull
+    @Override
+    public PsiReference[] getReferencesByElement(@NotNull PsiElement element, @NotNull ProcessingContext context) {
+      // No customizations for PluginDescriptorXIncludeFileReferenceHelper to be used
+      return FileReferenceSet.createSet(element, false, false, false).getAllReferences();
+    }
+  }
+}
