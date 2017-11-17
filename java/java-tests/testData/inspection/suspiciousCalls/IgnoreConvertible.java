3c145081aee15f0d4cb85194e94976f0c4873352
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2017-11-17 16:11:48 +0700

    SuspiciousMethodCallUtil: reportConvertibleMethodCalls checkbox supported for removeAll
    fixes IDEA-126935 "Suspicious collections method calls": false positive if argument type is superset of collection type

--- java/java-tests/testData/inspection/suspiciousCalls/IgnoreConvertible.java
+++ java/java-tests/testData/inspection/suspiciousCalls/IgnoreConvertible.java
@@ -1,6 +1,4 @@
-import java.util.Map;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 
 class Clazz {
   void f(Map<Integer,String> map) {
@@ -12,5 +10,14 @@ class Clazz {
 
   void foo(List<? extends Number> c) {
     c.contains(<warning descr="'List<capture of ? extends Number>' may not contain objects of type 'String'">""</warning>);
+  }
+
+  // IDEA-126935
+  void test() {
+    List<Object> elementsToRemove = new ArrayList<Object>();
+    elementsToRemove.add("foo");
+
+    List<String> storage = new ArrayList<String>(Arrays.asList("foo", "bar"));
+    storage.removeAll(elementsToRemove);  // Warning here
   }
 }
