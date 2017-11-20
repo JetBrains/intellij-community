157c8ff07e50a9085c4b792195406d41247652c3
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2017-11-20 14:48:25 +0100

    [java] expands search scope for exports/opens module references (IDEA-179277)
    (IDEA-CR-27078)

--- java/java-psi-impl/src/com/intellij/psi/impl/source/PsiJavaModuleImpl.java
+++ java/java-psi-impl/src/com/intellij/psi/impl/source/PsiJavaModuleImpl.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.impl.source;
 
 import com.intellij.lang.ASTNode;
@@ -24,6 +10,8 @@ import com.intellij.psi.impl.java.stubs.JavaStubElementTypes;
 import com.intellij.psi.impl.java.stubs.PsiJavaModuleStub;
 import com.intellij.psi.impl.source.tree.JavaElementType;
 import com.intellij.psi.javadoc.PsiDocComment;
+import com.intellij.psi.search.ProjectScope;
+import com.intellij.psi.search.SearchScope;
 import com.intellij.psi.util.CachedValueProvider;
 import com.intellij.psi.util.CachedValuesManager;
 import com.intellij.psi.util.PsiModificationTracker;
@@ -172,6 +160,12 @@ public class PsiJavaModuleImpl extends JavaStubPsiElement<PsiJavaModuleStub> imp
     else {
       visitor.visitElement(this);
     }
+  }
+
+  @NotNull
+  @Override
+  public SearchScope getUseScope() {
+    return ProjectScope.getProjectScope(getProject());
   }
 
   @Override
