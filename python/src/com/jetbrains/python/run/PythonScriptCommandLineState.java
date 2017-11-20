afec1098675799d8d9f31bca70c9f9d479754ee3
Elizaveta Shashkova <elizaveta.shashkova@jetbrains.com>
2017-11-20 15:57:17 +0300

    Can't attach to local process on macOS with Python 3.6 (PY-26800)
    In case of attaching to process, sdkHomePath appears from the running process. So there might be situation, when there is no Sdk, defined for this path, but executable exists, it's correct, and we should use for it for attaching. So, check for Sdk nullability is needed only in case when Console is enabled for run configuration.
    
    (cherry picked from commit b389052)
    
    (cherry picked from commit 673e927)

--- python/src/com/jetbrains/python/run/PythonScriptCommandLineState.java
+++ python/src/com/jetbrains/python/run/PythonScriptCommandLineState.java
@@ -68,7 +68,6 @@ public class PythonScriptCommandLineState extends PythonCommandLineState {
                                  PythonProcessStarter processStarter,
                                  final CommandLinePatcher... patchers) throws ExecutionException {
     Project project = myConfig.getProject();
-    assert myConfig.getSdk() != null;
 
     if (myConfig.showCommandLineAfterwards() && !myConfig.emulateTerminal()) {
       if (executor.getId() == DefaultDebugExecutor.EXECUTOR_ID) {
@@ -85,6 +84,10 @@ public class PythonScriptCommandLineState extends PythonCommandLineState {
       PathMapper pathMapper = PydevConsoleRunner.getPathMapper(project, myConfig.getSdk(), settingsProvider);
       String workingDir = PydevConsoleRunnerFactory.getWorkingDir(project, module, pathMapper, settingsProvider);
       String[] setupFragment = PydevConsoleRunnerFactory.createSetupFragment(module, workingDir, pathMapper, settingsProvider);
+
+      if (myConfig.getSdk() == null) {
+        throw new ExecutionException("Cannot find SDK for Run configuration " + myConfig.getName());
+      }
 
       PythonScriptWithConsoleRunner runner =
         new PythonScriptWithConsoleRunner(project, myConfig.getSdk(), PyConsoleType.PYTHON, workingDir,
