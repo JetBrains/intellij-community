7dfa2932dacc6cbc68601e9a965e7dcfe2ac26dd
Ivan Semenov <ivan.semenov@jetbrains.com>
2017-11-20 14:50:49 +0300

    Do not send warnings and runnerw errors to output

--- plugins/git4idea/src/git4idea/commands/GitImplBase.java
+++ plugins/git4idea/src/git4idea/commands/GitImplBase.java
@@ -142,8 +142,17 @@ abstract class GitImplBase implements Git {
 
   // could be upper-cased, so should check case-insensitively
   public static final String[] ERROR_INDICATORS = {
-    "error:", "remote: error", "fatal:",
-    "Cannot", "Could not", "Interactive rebase already started", "refusing to pull", "cannot rebase:", "conflict",
-    "unable"
+    "warning:",
+    "error:",
+    "fatal:",
+    "remote: error",
+    "Cannot",
+    "Could not",
+    "Interactive rebase already started",
+    "refusing to pull",
+    "cannot rebase:",
+    "conflict",
+    "unable",
+    "runnerw:"
   };
 }
