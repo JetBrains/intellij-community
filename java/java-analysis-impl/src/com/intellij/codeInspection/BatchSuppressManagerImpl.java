ebb88790551b8b18953b526f399848f844da9200
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2017-11-20 16:54:32 +0300

    BatchSuppressManager: remove useless methods

--- java/java-analysis-impl/src/com/intellij/codeInspection/BatchSuppressManagerImpl.java
+++ java/java-analysis-impl/src/com/intellij/codeInspection/BatchSuppressManagerImpl.java
@@ -1,29 +1,12 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInspection;
 
 import com.intellij.codeInsight.daemon.HighlightDisplayKey;
 import com.intellij.codeInsight.daemon.impl.actions.*;
 import com.intellij.psi.PsiDocCommentOwner;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiModifierListOwner;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.Collection;
 
 public class BatchSuppressManagerImpl implements BatchSuppressManager {
   @NotNull
@@ -43,30 +26,6 @@ public class BatchSuppressManagerImpl implements BatchSuppressManager {
   @Override
   public boolean isSuppressedFor(@NotNull final PsiElement element, @NotNull final String toolId) {
     return JavaSuppressionUtil.getElementToolSuppressedIn(element, toolId) != null;
-  }
-
-  @Override
-  @Nullable
-  public PsiElement getElementMemberSuppressedIn(@NotNull final PsiDocCommentOwner owner, @NotNull final String inspectionToolID) {
-    return JavaSuppressionUtil.getElementMemberSuppressedIn(owner, inspectionToolID);
-  }
-
-  @Override
-  @Nullable
-  public PsiElement getAnnotationMemberSuppressedIn(@NotNull final PsiModifierListOwner owner, @NotNull final String inspectionToolID) {
-    return JavaSuppressionUtil.getAnnotationMemberSuppressedIn(owner, inspectionToolID);
-  }
-
-  @Override
-  @Nullable
-  public PsiElement getDocCommentToolSuppressedIn(@NotNull final PsiDocCommentOwner owner, @NotNull final String inspectionToolID) {
-    return JavaSuppressionUtil.getDocCommentToolSuppressedIn(owner, inspectionToolID);
-  }
-
-  @Override
-  @NotNull
-  public Collection<String> getInspectionIdsSuppressedInAnnotation(@NotNull final PsiModifierListOwner owner) {
-    return JavaSuppressionUtil.getInspectionIdsSuppressedInAnnotation(owner);
   }
 
   @Override
