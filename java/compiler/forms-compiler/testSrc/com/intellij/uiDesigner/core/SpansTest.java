e892f0e39feefde63a6dbf9f59fdc2807da69e25
Alexander Lobas <alexander.lobas@jetbrains.com>
2017-11-21 18:39:27 +0100

    Test fix: sets component size more than minimum size for all OS
    (cherry picked from commit 0999411)

--- java/compiler/forms-compiler/testSrc/com/intellij/uiDesigner/core/SpansTest.java
+++ java/compiler/forms-compiler/testSrc/com/intellij/uiDesigner/core/SpansTest.java
@@ -20,133 +20,103 @@ import junit.framework.TestCase;
 import javax.swing.*;
 import java.awt.*;
 
-public final class SpansTest extends TestCase{
-  
+public final class SpansTest extends TestCase {
+
   /**
    * button(can grow) | text field (want grow)
-   *   text field (want grow, span 2) 
-   */ 
+   * text field (want grow, span 2)
+   */
   public void test1() {
-    final GridLayoutManager layout = new GridLayoutManager(2,2, new Insets(0,0,0,0), 0, 0);
+    final GridLayoutManager layout = new GridLayoutManager(2, 2, new Insets(0, 0, 0, 0), 0, 0);
     final JPanel panel = new JPanel(layout);
 
     final JButton button = new JButton();
-    button.setPreferredSize(new Dimension(50, 10));
-    
+    button.setPreferredSize(new Dimension(80, 10));
+
     final JTextField field1 = new JTextField();
     field1.setPreferredSize(new Dimension(50, 10));
 
     final JTextField field2 = new JTextField();
 
-    panel.add(button, new GridConstraints(0,0,1,1,GridConstraints.ANCHOR_CENTER,GridConstraints.FILL_HORIZONTAL,
-      GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0));
+    panel.add(button, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL,
+                                          GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0));
 
-    panel.add(field1, new GridConstraints(0,1,1,1,GridConstraints.ANCHOR_CENTER,GridConstraints.FILL_HORIZONTAL,
-      GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0));
+    panel.add(field1, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL,
+                                          GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0));
 
-    panel.add(field2, new GridConstraints(1,0,1,2,GridConstraints.ANCHOR_CENTER,GridConstraints.FILL_HORIZONTAL,
-      GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0));
+    panel.add(field2, new GridConstraints(1, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL,
+                                          GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0));
 
-    final Dimension preferredSize = panel.getPreferredSize();
-    if (SystemInfo.isMac) {
-      assertEquals(125, preferredSize.width);
-    } else {
-      assertEquals(100, preferredSize.width);
-    }
+    assertEquals(130, panel.getPreferredSize().width);
 
     panel.setSize(new Dimension(500, panel.getHeight()));
     panel.doLayout();
 
     assertEquals(500, field2.getWidth());
-    if (SystemInfo.isMac) {
-      assertEquals(75, button.getWidth());
-      assertEquals(425, field1.getWidth());
-    } else {
-      assertEquals(50, button.getWidth());
-      assertEquals(450, field1.getWidth());
-    }
+    assertEquals(80, button.getWidth());
+    assertEquals(420, field1.getWidth());
   }
 
 
   /**
    * button(can grow) | text field (can grow)
-   *   text field (want grow, span 2)
+   * text field (want grow, span 2)
    */
   public void test2() {
-    final JPanel panel = new JPanel(new GridLayoutManager(2,2, new Insets(0,0,0,0), 0, 0));
+    final JPanel panel = new JPanel(new GridLayoutManager(2, 2, new Insets(0, 0, 0, 0), 0, 0));
 
     final JButton button = new JButton();
-    button.setPreferredSize(new Dimension(50, 10));
+    button.setPreferredSize(new Dimension(80, 10));
 
     final JTextField field1 = new JTextField();
-    field1.setPreferredSize(new Dimension(50, 10));
+    field1.setPreferredSize(new Dimension(80, 10));
 
     final JTextField field2 = new JTextField();
 
-    panel.add(button, new GridConstraints(0,0,1,1,GridConstraints.ANCHOR_CENTER,GridConstraints.FILL_HORIZONTAL,
-      GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0));
+    panel.add(button, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL,
+                                          GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0));
 
-    panel.add(field1, new GridConstraints(0,1,1,1,GridConstraints.ANCHOR_CENTER,GridConstraints.FILL_HORIZONTAL,
-      GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0));
+    panel.add(field1, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL,
+                                          GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0));
 
-    panel.add(field2, new GridConstraints(1,0,1,2,GridConstraints.ANCHOR_CENTER,GridConstraints.FILL_HORIZONTAL,
-      GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0));
+    panel.add(field2, new GridConstraints(1, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL,
+                                          GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0));
 
-    final Dimension preferredSize = panel.getPreferredSize();
-    if (SystemInfo.isMac) {
-      assertEquals(125, preferredSize.width);
-    } else {
-      assertEquals(100, preferredSize.width);
-    }
+    assertEquals(160, panel.getPreferredSize().width);
 
     panel.setSize(new Dimension(500, panel.getHeight()));
     panel.doLayout();
 
     assertEquals(500, field2.getWidth());
-    if (SystemInfo.isMac) {
-      assertEquals(263, button.getWidth());
-      assertEquals(237, field1.getWidth());
-    } else {
-      assertEquals(250, button.getWidth());
-      assertEquals(250, field1.getWidth());
-    }
+    assertEquals(250, button.getWidth());
+    assertEquals(250, field1.getWidth());
   }
 
   /**
    * button(can grow) | text field (want grow, span 2)
    */
   public void test3() {
-    final JPanel panel = new JPanel(new GridLayoutManager(1,3, new Insets(0,0,0,0), 0, 0));
+    final JPanel panel = new JPanel(new GridLayoutManager(1, 3, new Insets(0, 0, 0, 0), 0, 0));
 
     final JButton button = new JButton();
-    button.setPreferredSize(new Dimension(50, 10));
+    button.setPreferredSize(new Dimension(80, 10));
 
     final JTextField field1 = new JTextField();
     field1.setPreferredSize(new Dimension(110, 10));
 
-    panel.add(button, new GridConstraints(0,0,1,1,GridConstraints.ANCHOR_CENTER,GridConstraints.FILL_HORIZONTAL,
-      GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0));
+    panel.add(button, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL,
+                                          GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0));
 
-    panel.add(field1, new GridConstraints(0,1,1,2,GridConstraints.ANCHOR_CENTER,GridConstraints.FILL_HORIZONTAL,
-      GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0));
+    panel.add(field1, new GridConstraints(0, 1, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL,
+                                          GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0));
 
-    final Dimension preferredSize = panel.getPreferredSize();
-    if (SystemInfo.isMac) {
-      assertEquals(185, preferredSize.width);
-    } else {
-      assertEquals(160, preferredSize.width);
-    }
+    assertEquals(190, panel.getPreferredSize().width);
 
     panel.setSize(new Dimension(500, panel.getHeight()));
     panel.doLayout();
 
-    if (SystemInfo.isMac) {
-      assertEquals(75, button.getWidth());
-      assertEquals(425, field1.getWidth());
-    } else {
-      assertEquals(50, button.getWidth());
-      assertEquals(450, field1.getWidth());
-    }
+    assertEquals(80, button.getWidth());
+    assertEquals(420, field1.getWidth());
   }
 
   /**
@@ -154,7 +124,7 @@ public final class SpansTest extends TestCase{
    * text field 2 (want grow, span 2) | empty
    */
   public void test4() {
-    final GridLayoutManager layoutManager = new GridLayoutManager(2,3, new Insets(0,0,0,0), 0, 0);
+    final GridLayoutManager layoutManager = new GridLayoutManager(2, 3, new Insets(0, 0, 0, 0), 0, 0);
     final JPanel panel = new JPanel(layoutManager);
 
     final JButton button = new JButton();
@@ -166,14 +136,14 @@ public final class SpansTest extends TestCase{
     final JTextField field2 = new JTextField();
     field2.setPreferredSize(new Dimension(110, 10));
 
-    panel.add(button, new GridConstraints(0,0,1,2,GridConstraints.ANCHOR_CENTER,GridConstraints.FILL_HORIZONTAL,
-      GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0));
+    panel.add(button, new GridConstraints(0, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL,
+                                          GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0));
 
-    panel.add(field1, new GridConstraints(0,2,1,1,GridConstraints.ANCHOR_CENTER,GridConstraints.FILL_HORIZONTAL,
-      GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0));
+    panel.add(field1, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL,
+                                          GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0));
 
-    panel.add(field2, new GridConstraints(1,0,1,2,GridConstraints.ANCHOR_CENTER,GridConstraints.FILL_HORIZONTAL,
-      GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0));
+    panel.add(field2, new GridConstraints(1, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL,
+                                          GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0));
 
     final Dimension preferredSize = panel.getPreferredSize();
 
@@ -184,7 +154,7 @@ public final class SpansTest extends TestCase{
     assertEquals(GridConstraints.SIZEPOLICY_WANT_GROW, horizontalInfo.getCellSizePolicy(2));
 
     assertEquals(220, preferredSize.width);
-    
+
     panel.setSize(new Dimension(500, panel.getHeight()));
     panel.doLayout();
 
@@ -192,5 +162,4 @@ public final class SpansTest extends TestCase{
     assertEquals(250, field1.getWidth());
     assertEquals(250, field2.getWidth());
   }
-  
 }
