1fa9c01eacc47e4c621034f6837c5fd792409efa
Anton Tarasov <anton.tarasov@jetbrains.com>
2017-11-23 15:34:05 +0300

    IDEA-182597 xml icon is not replaced with plugin icon in second completion
    (cherry picked from commit 1f6d7066a76accff3c94e915fd5e052f1251bc2e)

--- platform/lang-impl/src/com/intellij/codeInsight/completion/FilePathCompletionContributor.java
+++ platform/lang-impl/src/com/intellij/codeInsight/completion/FilePathCompletionContributor.java
@@ -275,7 +275,7 @@ public class FilePathCompletionContributor extends CompletionContributor {
       myHelpers = helpers;
 
       myInfo = FileInfoManager.getFileAdditionalInfo(file);
-      myIcon = file.getFileType().getIcon();
+      myIcon = file.getIcon(0);
 
       myFile = file;
     }
@@ -290,7 +290,7 @@ public class FilePathCompletionContributor extends CompletionContributor {
     @Override
     public Object getObject() {
       return myFile;
-    }                                                                   
+    }
 
     @Override
     @NotNull
