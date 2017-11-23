43c625b5122c0f2117fa5030f2eb2e715bd38605
Egor Ushakov <egor.ushakov@jetbrains.com>
2017-11-23 13:29:38 +0300

    fixed lines painting for hidpi - some darcula fixes

--- platform/platform-impl/src/com/intellij/ide/ui/laf/darcula/ui/DarculaMenuBarBorder.java
+++ platform/platform-impl/src/com/intellij/ide/ui/laf/darcula/ui/DarculaMenuBarBorder.java
@@ -1,21 +1,8 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.ui.laf.darcula.ui;
 
 import com.intellij.util.ui.JBUI;
+import com.intellij.util.ui.UIUtil;
 
 import javax.swing.*;
 import javax.swing.border.Border;
@@ -31,10 +18,10 @@ public class DarculaMenuBarBorder implements Border, UIResource {
     g.translate(x, y);
     w--;h--;
     g.setColor(UIManager.getColor("MenuBar.darcula.borderColor"));
-    g.drawLine(0, h, w, h);
+    UIUtil.drawLine(g, 0, h, w, h);
     h--;
     g.setColor(UIManager.getColor("MenuBar.darcula.borderShadowColor"));
-    g.drawLine(0, h, w, h);
+    UIUtil.drawLine(g, 0, h, w, h);
     g.translate(-x, -y);
   }
 
