5be4c7c34728b654874402d77a8dbccb184d1cec
Nadya Zabrodina <Nadya.Zabrodina@jetbrains.com>
2017-11-23 18:24:17 +0300

    hg: make init command sync, reuse createRepository in integration;
    * cleanup;

--- plugins/hg4idea/src/org/zmlx/hg4idea/command/HgInitCommand.java
+++ plugins/hg4idea/src/org/zmlx/hg4idea/command/HgInitCommand.java
@@ -3,13 +3,10 @@ package org.zmlx.hg4idea.command;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.zmlx.hg4idea.execution.HgCommandExecutor;
 import org.zmlx.hg4idea.execution.HgCommandResult;
-import org.zmlx.hg4idea.execution.HgCommandResultHandler;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
 
 /**
  * Representation of the "hg init"
@@ -22,17 +19,9 @@ public class HgInitCommand {
     myProject = project;
   }
 
-  public void executeAsynchronously(@NotNull VirtualFile repositoryRoot, final HgCommandResultHandler resultHandler) {
-    final List<String> args = new ArrayList<>(1);
-    args.add(repositoryRoot.getPath());
+  public HgCommandResult execute(@NotNull VirtualFile repositoryRoot) {
     final HgCommandExecutor executor = new HgCommandExecutor(myProject, repositoryRoot.getPath());
     executor.setShowOutput(true);
-    executor.execute(null, "init", args, new HgCommandResultHandler() {
-      @Override
-      public void process(@Nullable HgCommandResult result) {
-        resultHandler.process(result);
-      }
-    });
+    return executor.executeInCurrentThread(null, "init", Collections.singletonList(repositoryRoot.getPath()));
   }
-
 }
