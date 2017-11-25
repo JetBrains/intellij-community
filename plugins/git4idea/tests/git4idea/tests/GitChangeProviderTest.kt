ff0727f957524e2c2810618ae1659e599e3d9b2a
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2017-11-25 16:01:19 +0300

    git tests: cleanup, kotlinify

--- plugins/git4idea/tests/git4idea/tests/GitChangeProviderTest.kt
+++ plugins/git4idea/tests/git4idea/tests/GitChangeProviderTest.kt
@@ -3,7 +3,7 @@ package git4idea.tests
 
 import com.intellij.openapi.progress.EmptyProgressIndicator
 import com.intellij.openapi.util.io.FileUtil
-import com.intellij.openapi.vcs.Executor.*
+import com.intellij.openapi.vcs.Executor.cd
 import com.intellij.openapi.vcs.FilePath
 import com.intellij.openapi.vcs.FileStatus
 import com.intellij.openapi.vcs.VcsTestUtil.*
@@ -12,15 +12,15 @@ import com.intellij.openapi.vcs.changes.ChangeListManager
 import com.intellij.openapi.vcs.changes.ContentRevision
 import com.intellij.openapi.vcs.changes.VcsModifiableDirtyScope
 import com.intellij.openapi.vfs.VfsUtil
-import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.testFramework.vcs.MockChangeListManagerGate
 import com.intellij.testFramework.vcs.MockChangelistBuilder
 import com.intellij.testFramework.vcs.MockDirtyScope
 import com.intellij.vcsUtil.VcsUtil
 import git4idea.status.GitChangeProvider
-import git4idea.test.*
-import junit.framework.TestCase
+import git4idea.test.GitSingleRepoTest
+import git4idea.test.addCommit
+import git4idea.test.createFileStructure
 import java.io.File
 import java.util.*
 
@@ -32,9 +32,9 @@ import java.util.*
  */
 abstract class GitChangeProviderTest : GitSingleRepoTest() {
 
-  protected lateinit var changeProvider: GitChangeProvider
+  private lateinit var changeProvider: GitChangeProvider
   protected lateinit var dirtyScope: VcsModifiableDirtyScope
-  protected lateinit var subDir: VirtualFile
+  private lateinit var subDir: VirtualFile
 
   protected lateinit var atxt: VirtualFile
   protected lateinit var btxt: VirtualFile
@@ -63,56 +63,6 @@ abstract class GitChangeProviderTest : GitSingleRepoTest() {
 
   private fun getVirtualFile(relativePath: String) = VfsUtil.findFileByIoFile(File(projectPath, relativePath), true)!!
 
-  protected fun modifyFileInBranches(filename: String, masterAction: FileAction, featureAction: FileAction) {
-    git("checkout -b feature")
-    performActionOnFileAndRecordToIndex(filename, "feature", featureAction)
-    repo.commit("commit to feature")
-    repo.checkout("master")
-    refresh()
-    performActionOnFileAndRecordToIndex(filename, "master", masterAction)
-    repo.commit("commit to master")
-    git("merge feature", true)
-    refresh()
-  }
-
-  protected enum class FileAction {
-    CREATE, MODIFY, DELETE, RENAME
-  }
-
-  private fun performActionOnFileAndRecordToIndex(filename: String, branchName: String, action: FileAction) {
-    val file = projectRoot.findChild(filename)
-    if (action != FileAction.CREATE) { // file doesn't exist yet
-      TestCase.assertNotNull("VirtualFile is null: " + filename, file)
-    }
-    when (action) {
-      GitChangeProviderTest.FileAction.CREATE -> {
-        val f = touch(filename, "initial content in branch " + branchName)
-        val createdFile = VfsUtil.findFileByIoFile(f, true)
-        dirty(createdFile)
-        repo.add(filename)
-      }
-      GitChangeProviderTest.FileAction.MODIFY -> {
-
-        overwrite(VfsUtilCore.virtualToIoFile(file!!), "new content in branch " + branchName)
-        dirty(file)
-        repo.add(filename)
-      }
-      GitChangeProviderTest.FileAction.DELETE -> {
-        dirty(file)
-        git("rm " + filename)
-      }
-      GitChangeProviderTest.FileAction.RENAME -> {
-        val newName = filename + "_" + branchName.replace("\\s".toRegex(), "_") + "_new"
-        dirty(file)
-        repo.mv(filename, newName)
-        projectRoot.refresh(false, true)
-        dirty(projectRoot.findChild(newName))
-      }
-      else -> {
-      }
-    }
-  }
-
   /**
    * Checks that the given files have respective statuses in the change list retrieved from myChangesProvider.
    * Pass null in the fileStatuses array to indicate that proper file has not changed.
@@ -123,11 +73,11 @@ abstract class GitChangeProviderTest : GitSingleRepoTest() {
       val fp = VcsUtil.getFilePath(virtualFiles[i])
       val status = fileStatuses[i]
       if (status == null) {
-        TestCase.assertFalse("File [" + tos(fp) + " shouldn't be in the changelist, but it was.", result.containsKey(fp))
+        assertFalse("File [" + tos(fp) + " shouldn't be in the changelist, but it was.", result.containsKey(fp))
         continue
       }
-      TestCase.assertTrue("File [" + tos(fp) + "] didn't change. Changes: " + tos(result), result.containsKey(fp))
-      TestCase.assertEquals("File statuses don't match for file [" + tos(fp) + "]", result[fp]!!.getFileStatus(), status)
+      assertTrue("File [${tos(fp)}] didn't change. Changes: [${result.values.joinToString(",") { tos(it) }}]", result.containsKey(fp))
+      assertEquals("File statuses don't match for file [${tos(fp)}]", result[fp]!!.fileStatus, status)
     }
   }
 
@@ -203,31 +153,16 @@ abstract class GitChangeProviderTest : GitSingleRepoTest() {
     dirtyScope.addDirtyFile(VcsUtil.getFilePath(file!!))
   }
 
-  protected fun tos(fp: FilePath): String? {
-    return FileUtil.getRelativePath(File(projectPath), fp.ioFile)
+  private fun tos(fp: FilePath) = FileUtil.getRelativePath(File(projectPath), fp.ioFile)
+
+  private fun tos(change: Change) = when (change.type) {
+    Change.Type.NEW -> "A: " + tos(change.afterRevision)!!
+    Change.Type.DELETED -> "D: " + tos(change.beforeRevision)!!
+    Change.Type.MOVED -> "M: " + tos(change.beforeRevision) + " -> " + tos(change.afterRevision)
+    Change.Type.MODIFICATION -> "M: " + tos(change.afterRevision)!!
+    else -> "~: " + tos(change.beforeRevision) + " -> " + tos(change.afterRevision)
   }
 
-  protected fun tos(change: Change): String {
-    when (change.type) {
-      Change.Type.NEW -> return "A: " + tos(change.afterRevision)!!
-      Change.Type.DELETED -> return "D: " + tos(change.beforeRevision)!!
-      Change.Type.MOVED -> return "M: " + tos(change.beforeRevision) + " -> " + tos(change.afterRevision)
-      Change.Type.MODIFICATION -> return "M: " + tos(change.afterRevision)!!
-      else -> return "~: " + tos(change.beforeRevision) + " -> " + tos(change.afterRevision)
-    }
-  }
-
-  protected fun tos(revision: ContentRevision?): String? {
-    return tos(revision!!.file)
-  }
-
-  protected fun tos(changes: Map<FilePath, Change>): String {
-    val stringBuilder = StringBuilder("[")
-    for (change in changes.values) {
-      stringBuilder.append(tos(change)).append(", ")
-    }
-    stringBuilder.append("]")
-    return stringBuilder.toString()
-  }
+  private fun tos(revision: ContentRevision?) = tos(revision!!.file)
 
 }
