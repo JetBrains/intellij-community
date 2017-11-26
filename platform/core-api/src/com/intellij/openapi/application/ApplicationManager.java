a98a4e5d3494628b36df3c3d099482c4a8811e6c
Konstantin Bulenkov <kb@jetbrains.com>
2017-11-26 01:55:48 +0100

    IDEA-182682 IntelliJ Idea can freeze for a few seconds before showing Splash screen

--- platform/core-api/src/com/intellij/openapi/application/ApplicationManager.java
+++ platform/core-api/src/com/intellij/openapi/application/ApplicationManager.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.application;
 
 import com.intellij.openapi.Disposable;
@@ -28,7 +14,7 @@ import java.lang.management.ManagementFactory;
  */
 public class ApplicationManager {
   protected static Application ourApplication;
-  private static final String ourPid = getCurrentProcessId();
+  private static String ourPid;
 
   private static String getCurrentProcessId() {
     try {
@@ -87,6 +73,10 @@ public class ApplicationManager {
   }
 
   public static String getApplicationPid() {
+    if (ourPid == null) {
+      ourPid = getCurrentProcessId();
+    }
+
     return String.valueOf(ourPid);
   }
 }
