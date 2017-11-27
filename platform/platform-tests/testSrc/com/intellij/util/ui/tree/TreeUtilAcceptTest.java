45d39bb55b3b3f359f6fe0c3a35a48cee09bbad3
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2017-11-27 19:23:29 +0300

    implement makeVisible for TreePath [IDEA-CR-26837] (cherry picked from commit 1997065)

--- platform/platform-tests/testSrc/com/intellij/util/ui/tree/TreeUtilAcceptTest.java
+++ platform/platform-tests/testSrc/com/intellij/util/ui/tree/TreeUtilAcceptTest.java
@@ -3,7 +3,6 @@ package com.intellij.util.ui.tree;
 
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.util.Disposer;
-import com.intellij.testFramework.PlatformTestUtil;
 import com.intellij.ui.tree.AsyncTreeModel;
 import com.intellij.ui.tree.TreeVisitor;
 import com.intellij.util.ArrayUtil;
@@ -25,6 +24,8 @@ import java.util.function.BiConsumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+import static com.intellij.testFramework.PlatformTestUtil.assertTreeEqual;
+import static com.intellij.ui.tree.TreePathUtil.convertArrayToTreePath;
 import static java.util.concurrent.TimeUnit.MINUTES;
 
 /**
@@ -127,7 +128,7 @@ public final class TreeUtilAcceptTest {
   }
 
   private static void testFind(@NotNull Supplier<Visitor> supplier, long count, String value) {
-    test((tree, promise) -> {
+    test(TreeUtilAcceptTest::root, (tree, promise) -> {
       @NotNull Visitor visitor = supplier.get();
       TreeUtil.accept(tree, visitor, path -> invokeSafely(promise, () -> {
         Assert.assertEquals(count, visitor.counter.get());
@@ -208,13 +209,13 @@ public final class TreeUtilAcceptTest {
   }
 
   private static void testShow(@NotNull Supplier<Visitor> supplier, @NotNull String expected) {
-    test((tree, promise) -> {
+    test(TreeUtilAcceptTest::root, (tree, promise) -> {
       @NotNull Visitor visitor = supplier.get();
       TreeUtil.accept(tree, visitor, path -> invokeSafely(promise, () -> {
         Assert.assertNotNull(path);
         tree.makeVisible(path);
         tree.setSelectionPath(path);
-        PlatformTestUtil.assertTreeEqual(tree, expected, true);
+        assertTreeEqual(tree, expected, true);
       }));
     });
   }
@@ -222,7 +223,7 @@ public final class TreeUtilAcceptTest {
 
   @Test
   public void testExpandOne() {
-    test((tree, promise) -> TreeUtil.expand(tree, 1, () -> invokeSafely(promise, () -> PlatformTestUtil.assertTreeEqual(
+    test(TreeUtilAcceptTest::root, (tree, promise) -> TreeUtil.expand(tree, 1, () -> invokeSafely(promise, () -> assertTreeEqual(
       tree, "-Root\n" +
             " +RootColor\n" +
             " +RootDigit\n" +
@@ -230,8 +231,26 @@ public final class TreeUtilAcceptTest {
   }
 
   @Test
+  public void testExpandTwo() {
+    test(TreeUtilAcceptTest::rootDeep, (tree, promise) -> TreeUtil.expand(tree, 2, () -> invokeSafely(promise, () -> assertTreeEqual(
+      tree, "-Root\n" +
+            " -1\n" +
+            "  +11\n" +
+            "  +12\n" +
+            "  +13\n" +
+            " -2\n" +
+            "  +21\n" +
+            "  +22\n" +
+            "  +23\n" +
+            " -3\n" +
+            "  +31\n" +
+            "  +32\n" +
+            "  +33\n"))));
+  }
+
+  @Test
   public void testExpandAll() {
-    test((tree, promise) -> TreeUtil.expandAll(tree, () -> invokeSafely(promise, () -> PlatformTestUtil.assertTreeEqual(
+    test(TreeUtilAcceptTest::root, (tree, promise) -> TreeUtil.expandAll(tree, () -> invokeSafely(promise, () -> assertTreeEqual(
       tree, "-Root\n" +
             " -RootColor\n" +
             "  RootColorRed\n" +
@@ -255,6 +274,178 @@ public final class TreeUtilAcceptTest {
             "  RootGreekEpsilon\n"))));
   }
 
+  @Test
+  public void testMakeVisible1() {
+    testMakeVisible("-Root\n" +
+                    " +[1]\n" +
+                    " +2\n" +
+                    " +3\n",
+                    "1");
+  }
+
+  @Test
+  public void testMakeVisible11() {
+    testMakeVisible("-Root\n" +
+                    " -1\n" +
+                    "  +[11]\n" +
+                    "  +12\n" +
+                    "  +13\n" +
+                    " +2\n" +
+                    " +3\n",
+                    "1", "11");
+  }
+
+  @Test
+  public void testMakeVisible111() {
+    testMakeVisible("-Root\n" +
+                    " -1\n" +
+                    "  -11\n" +
+                    "   +[111]\n" +
+                    "   +112\n" +
+                    "   +113\n" +
+                    "  +12\n" +
+                    "  +13\n" +
+                    " +2\n" +
+                    " +3\n",
+                    "1", "11", "111");
+  }
+
+  @Test
+  public void testMakeVisible1111() {
+    testMakeVisible("-Root\n" +
+                    " -1\n" +
+                    "  -11\n" +
+                    "   -111\n" +
+                    "    [1111]\n" +
+                    "    1112\n" +
+                    "    1113\n" +
+                    "   +112\n" +
+                    "   +113\n" +
+                    "  +12\n" +
+                    "  +13\n" +
+                    " +2\n" +
+                    " +3\n",
+                    "1", "11", "111", "1111");
+  }
+
+  @Test
+  public void testMakeVisible2() {
+    testMakeVisible("-Root\n" +
+                    " +1\n" +
+                    " +[2]\n" +
+                    " +3\n",
+                    "2");
+  }
+
+  @Test
+  public void testMakeVisible22() {
+    testMakeVisible("-Root\n" +
+                    " +1\n" +
+                    " -2\n" +
+                    "  +21\n" +
+                    "  +[22]\n" +
+                    "  +23\n" +
+                    " +3\n",
+                    "2", "22");
+  }
+
+  @Test
+  public void testMakeVisible222() {
+    testMakeVisible("-Root\n" +
+                    " +1\n" +
+                    " -2\n" +
+                    "  +21\n" +
+                    "  -22\n" +
+                    "   +221\n" +
+                    "   +[222]\n" +
+                    "   +223\n" +
+                    "  +23\n" +
+                    " +3\n",
+                    "2", "22", "222");
+  }
+
+  @Test
+  public void testMakeVisible2222() {
+    testMakeVisible("-Root\n" +
+                    " +1\n" +
+                    " -2\n" +
+                    "  +21\n" +
+                    "  -22\n" +
+                    "   +221\n" +
+                    "   -222\n" +
+                    "    2221\n" +
+                    "    [2222]\n" +
+                    "    2223\n" +
+                    "   +223\n" +
+                    "  +23\n" +
+                    " +3\n",
+                    "2", "22", "222", "2222");
+  }
+
+  @Test
+  public void testMakeVisible3() {
+    testMakeVisible("-Root\n" +
+                    " +1\n" +
+                    " +2\n" +
+                    " +[3]\n",
+                    "3");
+  }
+
+  @Test
+  public void testMakeVisible33() {
+    testMakeVisible("-Root\n" +
+                    " +1\n" +
+                    " +2\n" +
+                    " -3\n" +
+                    "  +31\n" +
+                    "  +32\n" +
+                    "  +[33]\n",
+                    "3", "33");
+  }
+
+  @Test
+  public void testMakeVisible333() {
+    testMakeVisible("-Root\n" +
+                    " +1\n" +
+                    " +2\n" +
+                    " -3\n" +
+                    "  +31\n" +
+                    "  +32\n" +
+                    "  -33\n" +
+                    "   +331\n" +
+                    "   +332\n" +
+                    "   +[333]\n",
+                    "3", "33", "333");
+  }
+
+  @Test
+  public void testMakeVisible3333() {
+    testMakeVisible("-Root\n" +
+                    " +1\n" +
+                    " +2\n" +
+                    " -3\n" +
+                    "  +31\n" +
+                    "  +32\n" +
+                    "  -33\n" +
+                    "   +331\n" +
+                    "   +332\n" +
+                    "   -333\n" +
+                    "    3331\n" +
+                    "    3332\n" +
+                    "    [3333]\n",
+                    "3", "33", "333", "3333");
+  }
+
+  private static void testMakeVisible(String expected, String... array) {
+    test(TreeUtilAcceptTest::rootDeep, (tree, promise)
+      -> TreeUtil.makeVisible(tree, new TreeVisitor.ByTreePath<>(true, convertArrayToTreePath(array), Object::toString), path
+      -> invokeSafely(promise, () -> {
+      Assert.assertNotNull(path);
+      tree.setSelectionPath(path);
+      assertTreeEqual(tree, expected, true);
+    })));
+  }
+
 
   private static void invokeLater(@NotNull AsyncPromise<Throwable> promise, @NotNull Runnable runnable) {
     EventQueue.invokeLater(() -> invokeSafely(promise, runnable, false));
@@ -274,17 +465,17 @@ public final class TreeUtilAcceptTest {
     }
   }
 
-  private static void test(BiConsumer<JTree, AsyncPromise<Throwable>> consumer) {
-    test(consumer, parent -> model(0, false, null));
-    test(consumer, parent -> model(10, false, null));
-    test(consumer, parent -> model(0, true, new Invoker.EDT(parent)));
-    test(consumer, parent -> model(0, false, new Invoker.EDT(parent)));
-    test(consumer, parent -> model(10, true, new Invoker.EDT(parent)));
-    test(consumer, parent -> model(10, false, new Invoker.EDT(parent)));
-    test(consumer, parent -> model(0, true, new Invoker.BackgroundThread(parent)));
-    test(consumer, parent -> model(0, false, new Invoker.BackgroundThread(parent)));
-    test(consumer, parent -> model(10, true, new Invoker.BackgroundThread(parent)));
-    test(consumer, parent -> model(10, false, new Invoker.BackgroundThread(parent)));
+  private static void test(Supplier<DefaultMutableTreeNode> supplier, BiConsumer<JTree, AsyncPromise<Throwable>> consumer) {
+    test(consumer, parent -> model(supplier.get(), 0, false, null));
+    test(consumer, parent -> model(supplier.get(), 10, false, null));
+    test(consumer, parent -> model(supplier.get(), 0, true, new Invoker.EDT(parent)));
+    test(consumer, parent -> model(supplier.get(), 0, false, new Invoker.EDT(parent)));
+    test(consumer, parent -> model(supplier.get(), 10, true, new Invoker.EDT(parent)));
+    test(consumer, parent -> model(supplier.get(), 10, false, new Invoker.EDT(parent)));
+    test(consumer, parent -> model(supplier.get(), 0, true, new Invoker.BackgroundThread(parent)));
+    test(consumer, parent -> model(supplier.get(), 0, false, new Invoker.BackgroundThread(parent)));
+    test(consumer, parent -> model(supplier.get(), 10, true, new Invoker.BackgroundThread(parent)));
+    test(consumer, parent -> model(supplier.get(), 10, false, new Invoker.BackgroundThread(parent)));
   }
 
   private static void test(BiConsumer<JTree, AsyncPromise<Throwable>> consumer, Function<Disposable, TreeModel> function) {
@@ -304,7 +495,7 @@ public final class TreeUtilAcceptTest {
           }
           else {
             tree.collapseRow(0); // because root node is expanded by default
-            PlatformTestUtil.assertTreeEqual(tree, "+Root\n");
+            assertTreeEqual(tree, "+Root\n");
             consumer.accept(tree, promise);
           }
         }
@@ -319,8 +510,8 @@ public final class TreeUtilAcceptTest {
     }
   }
 
-  private static TreeModel model(long delay, boolean showLoadingNode, Invoker invoker) {
-    TreeModel model = new DefaultTreeModel(root());
+  private static TreeModel model(DefaultMutableTreeNode root, long delay, boolean showLoadingNode, Invoker invoker) {
+    TreeModel model = new DefaultTreeModel(root);
     if (delay > 0) {
       model = new TreeModelWithDelay(model, delay);
     }
@@ -366,6 +557,49 @@ public final class TreeUtilAcceptTest {
                      node("RootGreekGamma"),
                      node("RootGreekDelta"),
                      node("RootGreekEpsilon")));
+  }
+
+  private static DefaultMutableTreeNode rootDeep() {
+    return node("Root",
+                node("1",
+                     node("11",
+                          node("111", "1111", "1112", "1113"),
+                          node("112", "1121", "1122", "1123"),
+                          node("113", "1131", "1132", "1133")),
+                     node("12",
+                          node("121", "1211", "1212", "1213"),
+                          node("122", "1221", "1222", "1223"),
+                          node("123", "1231", "1232", "1233")),
+                     node("13",
+                          node("131", "1311", "1312", "1313"),
+                          node("132", "1321", "1322", "1323"),
+                          node("133", "1331", "1332", "1333"))),
+                node("2",
+                     node("21",
+                          node("211", "2111", "2112", "2113"),
+                          node("212", "2121", "2122", "2123"),
+                          node("213", "2131", "2132", "2133")),
+                     node("22",
+                          node("221", "2211", "2212", "2213"),
+                          node("222", "2221", "2222", "2223"),
+                          node("223", "2231", "2232", "2233")),
+                     node("23",
+                          node("231", "2311", "2312", "2313"),
+                          node("232", "2321", "2322", "2323"),
+                          node("233", "2331", "2332", "2333"))),
+                node("3",
+                     node("31",
+                          node("311", "3111", "3112", "3113"),
+                          node("312", "3121", "3122", "3123"),
+                          node("313", "3131", "3132", "3133")),
+                     node("32",
+                          node("321", "3211", "3212", "3213"),
+                          node("322", "3221", "3222", "3223"),
+                          node("323", "3231", "3232", "3233")),
+                     node("33",
+                          node("331", "3311", "3312", "3313"),
+                          node("332", "3321", "3322", "3323"),
+                          node("333", "3331", "3332", "3333"))));
   }
 
   static class Visitor extends TreeVisitor.Base<TreePath> {
