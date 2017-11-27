e42d539c6eca92fcf04fd9e4f1439f974450d76e
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2017-11-27 19:58:06 +0300

    implicit toString searcher

---
+++ java/java-impl/src/com/intellij/psi/impl/search/ImplicitToStringSearcher.java
@@ -0,0 +1,184 @@
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.psi.impl.search;
+
+import com.intellij.openapi.application.QueryExecutorBase;
+import com.intellij.openapi.application.ReadAction;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.project.DumbService;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.*;
+import com.intellij.psi.impl.java.JavaBinaryPlusExpressionIndex;
+import com.intellij.psi.search.GlobalSearchScopeUtil;
+import com.intellij.psi.search.searches.MethodReferencesSearch;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.util.PsiUtil;
+import com.intellij.util.ArrayUtil;
+import com.intellij.util.IncorrectOperationException;
+import com.intellij.util.ObjectUtils;
+import com.intellij.util.Processor;
+import com.intellij.util.indexing.FileBasedIndex;
+import gnu.trove.THashMap;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
+
+public class ImplicitToStringSearcher extends QueryExecutorBase<PsiReference, MethodReferencesSearch.SearchParameters> {
+  private static final Logger LOG = Logger.getInstance(ImplicitToStringSearcher.class);
+
+  @Override
+  public void processQuery(@NotNull MethodReferencesSearch.SearchParameters parameters, @NotNull Processor<PsiReference> consumer) {
+    PsiMethod method = parameters.getMethod();
+    if (!"toString".equals(method.getName()) || method.getParameters().length != 0) {
+      return;
+    }
+    PsiClass targetClass = ReadAction.compute(() -> method.getContainingClass());
+    if (targetClass == null) {
+      return;
+    }
+    Project project = parameters.getProject();
+    DumbService dumbService = DumbService.getInstance(project);
+    Map<VirtualFile, int[]> fileOffsets = new THashMap<>();
+    dumbService.runReadActionInSmartMode(() -> {
+      FileBasedIndex.getInstance().processValues(JavaBinaryPlusExpressionIndex.INDEX_ID, Boolean.TRUE, null,
+                                                 (file, value) -> {
+                                                   ProgressManager.checkCanceled();
+                                                   fileOffsets.put(file, value.getOffsets());
+                                                   return true;
+                                                 }, GlobalSearchScopeUtil.toGlobalSearchScope(parameters.getEffectiveSearchScope(), project));
+
+    });
+
+    PsiManager psiManager = PsiManager.getInstance(project);
+    for (Map.Entry<VirtualFile, int[]> entry : fileOffsets.entrySet()) {
+      VirtualFile file = entry.getKey();
+      int[] offsets = entry.getValue();
+      ProgressManager.checkCanceled();
+      if (!processFile(file, offsets, psiManager, targetClass, method, consumer)) {
+        return;
+      }
+    }
+  }
+
+  private static boolean processFile(VirtualFile file, int[] offsets, PsiManager manager, PsiClass targetClass, PsiMethod targetMethod, Processor<PsiReference> consumer) {
+    return ReadAction.compute(() -> {
+      PsiFile psiFile = ObjectUtils.notNull(manager.findFile(file));
+      if (!(psiFile instanceof PsiJavaFile)) {
+        LOG.error("Non-java file " + psiFile + "; " + file);
+        return true;
+      }
+
+      for (int offset : offsets) {
+        PsiJavaToken plusToken = PsiTreeUtil.findElementOfClassAtOffset(psiFile, offset, PsiJavaToken.class, false);
+        if (plusToken == null) {
+          LOG.error("plusToken shouldn't be null in " + psiFile + " at " + offset);
+          continue;
+        }
+        PsiBinaryExpression binaryExpr = ObjectUtils.tryCast(plusToken.getParent(), PsiBinaryExpression.class);
+        if (binaryExpr == null) {
+          LOG.error("binaryExpr shouldn't be null for binary op  in " + psiFile + " at " + offset);
+          continue;
+        }
+
+        PsiExpression lOperand = binaryExpr.getLOperand();
+        PsiExpression rOperand = binaryExpr.getROperand();
+        if (rOperand == null) {
+          LOG.error("rOperand shouldn't be null for binary op in " + psiFile + " at " + offset);
+          continue;
+        }
+
+        if (!processBinaryExpression(lOperand, rOperand, consumer, targetClass, targetMethod)) {
+          return false;
+        }
+      }
+      return true;
+    });
+  }
+
+  private static boolean processBinaryExpression(@NotNull PsiExpression lhs,
+                                                 @NotNull PsiExpression rhs,
+                                                 @NotNull Processor<PsiReference> consumer,
+                                                 @NotNull PsiClass targetClass,
+                                                 @NotNull PsiMethod targetMethod) {
+    if (!processBinaryExpressionInOneDirection(lhs, rhs, consumer, targetClass, targetMethod)) {
+      return false;
+    }
+    return processBinaryExpressionInOneDirection(rhs, lhs, consumer, targetClass, targetMethod);
+  }
+
+  private static boolean processBinaryExpressionInOneDirection(@NotNull PsiExpression stringExpr,
+                                                               @NotNull PsiExpression expr,
+                                                               @NotNull Processor<PsiReference> consumer,
+                                                               @NotNull PsiClass psiClass,
+                                                               @NotNull PsiMethod targetMethod) {
+    PsiType strType = stringExpr.getType();
+    if (strType == null || !strType.equalsToText(CommonClassNames.JAVA_LANG_STRING)) return true;
+
+    PsiClass aClass = PsiUtil.resolveClassInClassTypeOnly(expr.getType());
+    if (aClass != null && PsiUtil.preferCompiledElement(aClass) == psiClass) {
+      return consumer.process(new MyImplicitToStringReference(expr, targetMethod));
+    }
+    return true;
+  }
+
+  private static class MyImplicitToStringReference implements PsiReference {
+    private final PsiElement myPlace;
+    private final PsiMethod myTargetMethod;
+
+    private MyImplicitToStringReference(PsiElement place, PsiMethod method) {
+      myPlace = place;
+      myTargetMethod = method;
+    }
+
+    @Override
+    public PsiElement getElement() {
+      return myPlace;
+    }
+
+    @Override
+    public TextRange getRangeInElement() {
+      return new TextRange(0, myPlace.getTextLength());
+    }
+
+    @Nullable
+    @Override
+    public PsiElement resolve() {
+      return myTargetMethod;
+    }
+
+    @NotNull
+    @Override
+    public String getCanonicalText() {
+      return myPlace.getText();
+    }
+
+    @Override
+    public PsiElement handleElementRename(String newElementName) throws IncorrectOperationException {
+      return null;
+    }
+
+    @Override
+    public PsiElement bindToElement(@NotNull PsiElement element) throws IncorrectOperationException {
+      return null;
+    }
+
+    @Override
+    public boolean isReferenceTo(PsiElement element) {
+      return element == myTargetMethod;
+    }
+
+    @NotNull
+    @Override
+    public Object[] getVariants() {
+      return ArrayUtil.EMPTY_OBJECT_ARRAY;
+    }
+
+    @Override
+    public boolean isSoft() {
+      return false;
+    }
+  }
+}
