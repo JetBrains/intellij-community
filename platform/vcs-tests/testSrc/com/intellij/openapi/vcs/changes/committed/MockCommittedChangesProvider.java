8f6f039ada91c3c3093886bd463ac047e1a9b6dc
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2017-11-27 11:55:55 +0100

    default implementation of createDefaultSettings

--- platform/vcs-tests/testSrc/com/intellij/openapi/vcs/changes/committed/MockCommittedChangesProvider.java
+++ platform/vcs-tests/testSrc/com/intellij/openapi/vcs/changes/committed/MockCommittedChangesProvider.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vcs.changes.committed;
 
 import com.intellij.openapi.util.Pair;
@@ -28,7 +14,6 @@ import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.testFramework.vcs.MockContentRevision;
 import com.intellij.util.AsynchConsumer;
 import com.intellij.vcsUtil.VcsUtil;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.DataInput;
@@ -42,12 +27,6 @@ import java.util.*;
 public class MockCommittedChangesProvider implements CachingCommittedChangesProvider<CommittedChangeListImpl, ChangeBrowserSettings> {
   private final List<CommittedChangeListImpl> myChangeLists = new ArrayList<>();
   private int myRefreshCount = 0;
-
-  @NotNull
-  @Override
-  public ChangeBrowserSettings createDefaultSettings() {
-    return new ChangeBrowserSettings();
-  }
 
   @Override
   public ChangesBrowserSettingsEditor createFilterUI(final boolean showDateFilter) {
@@ -125,8 +104,8 @@ public class MockCommittedChangesProvider implements CachingCommittedChangesProv
     return list;
   }
 
-  private CommittedChangeListImpl createList(final String name, final String author, final String comment,
-                                             final long date, final long number, final Change... changes) {
+  private static CommittedChangeListImpl createList(final String name, final String author, final String comment,
+                                                    final long date, final long number, final Change... changes) {
     final Collection<Change> changeList = new ArrayList<>();
     Collections.addAll(changeList, changes);
     return new CommittedChangeListImpl(name, comment, author, number, new Date(date), changeList);
