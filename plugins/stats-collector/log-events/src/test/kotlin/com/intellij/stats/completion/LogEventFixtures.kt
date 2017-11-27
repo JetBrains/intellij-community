9b448bcb499c2b595ed64ba5af71488cff701733
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-11-27 19:54:35 +0300

    Update serialize/deserialize tests (incl. user factors)

--- plugins/stats-collector/log-events/src/test/kotlin/com/intellij/stats/completion/LogEventFixtures.kt
+++ plugins/stats-collector/log-events/src/test/kotlin/com/intellij/stats/completion/LogEventFixtures.kt
@@ -22,7 +22,9 @@ object LogEventFixtures {
     
     val sessionId = "session-id-xxx"
 
-    val completion_started_3_items_shown = CompletionStartedEvent("", "", "", "1", sessionId, "Java", true, 1, Fixtures.lookupList, 0)
+    val completion_started_3_items_shown = CompletionStartedEvent("", "", "",
+            "1", sessionId, "Java", true, 1, Fixtures.lookupList,
+            Fixtures.userFactors, 0)
 
     val completion_cancelled = CompletionCancelledEvent("1", sessionId)
 
