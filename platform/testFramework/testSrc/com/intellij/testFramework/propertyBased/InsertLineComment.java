f6f68c5d423311255ed21c8f804183a27ff1f738
Anna.Kozlova <anna.kozlova@jetbrains.com>
2017-11-27 15:49:31 +0100

    NPE

--- platform/testFramework/testSrc/com/intellij/testFramework/propertyBased/InsertLineComment.java
+++ platform/testFramework/testSrc/com/intellij/testFramework/propertyBased/InsertLineComment.java
@@ -34,7 +34,7 @@ public class InsertLineComment extends ActionOnRange {
 
   @Override
   public String toString() {
-    return "LineComment{" + getVirtualFile().getPath() + " " + getCurrentRange().getStartOffset() + " " + myToInsert + "}";
+    return "LineComment{" + getVirtualFile().getPath() + " " + getCurrentStartOffset() + " " + myToInsert + "}";
   }
 
   @Override
