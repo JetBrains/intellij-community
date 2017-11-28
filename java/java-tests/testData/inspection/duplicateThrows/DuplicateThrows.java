0b75f44dca7b79f2914106ad3bcb455400912508
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2017-11-28 16:04:44 +0700

    DuplicateThrowsInspectionTest modernized

---
+++ java/java-tests/testData/inspection/duplicateThrows/DuplicateThrows.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+class X {
+ void f() throws
+            <warning descr="Duplicate throws">Exception</warning>,
+            Exception {
+ }
+ void f2() throws
+            Exception,
+            <warning descr="There is a more general exception, 'java.lang.Exception', in the throws list already.">IllegalArgumentException</warning> {
+ }
+ void f3() throws
+            FileNotFoundException,
+            EOFException {
+ }
+
+     public void TTT() throws
+     <warning descr="Duplicate throws">FileNotFoundException</warning>,
+     <warning descr="Duplicate throws">EOFException</warning>,
+     FileNotFoundException,
+     EOFException {
+     }
+
+}
