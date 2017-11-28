d98b315ab64c986e54985cd3c1ba683ede6ab1ca
Egor Ushakov <egor.ushakov@jetbrains.com>
2017-11-28 13:11:28 +0300

    IDEA-182737 IllegalStateException in GeneratedLocation.codeIndex - more methods implemented for GeneratedLocation

--- java/debugger/impl/src/com/intellij/debugger/jdi/GeneratedLocation.java
+++ java/debugger/impl/src/com/intellij/debugger/jdi/GeneratedLocation.java
@@ -4,7 +4,10 @@ package com.intellij.debugger.jdi;
 import com.intellij.debugger.engine.DebugProcessImpl;
 import com.intellij.util.containers.ContainerUtil;
 import com.sun.jdi.*;
+import com.sun.tools.jdi.LocationImpl;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 /**
  * @author egor
@@ -34,7 +37,7 @@ public class GeneratedLocation implements Location {
 
   @Override
   public long codeIndex() {
-    return -2; // for more or less correct LocationImpl.equals
+    return -2; // to be never equal to any LocationImpl
   }
 
   @Override
@@ -73,8 +76,28 @@ public class GeneratedLocation implements Location {
   }
 
   @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GeneratedLocation location = (GeneratedLocation)o;
+    return Objects.equals(myMethod, location.myMethod) &&
+           myLineNumber == location.myLineNumber &&
+           Objects.equals(myVirtualMachine, location.myVirtualMachine);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(myMethod, myLineNumber);
+  }
+
+  // Same as in LocationImpl
+  @Override
   public int compareTo(@NotNull Location o) {
-    throw new IllegalStateException();
+    int res = method().compareTo(o.method());
+    if (res != 0) {
+      return res;
+    }
+    return Long.compare(codeIndex(), o.codeIndex());
   }
 
   public String toString() {
