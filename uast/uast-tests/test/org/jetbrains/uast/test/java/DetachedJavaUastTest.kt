4119e3ee9e780d1e9f6be0f443122e8b01605612
Nicolay Mitropolsky <nicolay.mitropolsky@jetbrains.com>
2017-11-28 14:54:53 +0300

    [draft] `detachers` map

--- uast/uast-tests/test/org/jetbrains/uast/test/java/DetachedJavaUastTest.kt
+++ uast/uast-tests/test/org/jetbrains/uast/test/java/DetachedJavaUastTest.kt
@@ -2,55 +2,51 @@
 package org.jetbrains.uast.test.java
 
 import com.intellij.lang.java.JavaLanguage
-import com.intellij.psi.JavaPsiFacade
-import com.intellij.psi.PsiDeclarationStatement
-import com.intellij.psi.PsiFile
-import com.intellij.psi.PsiFileFactory
+import com.intellij.psi.*
 import com.intellij.psi.util.PsiTreeUtil
-import com.intellij.util.SmartList
 import junit.framework.TestCase
-import org.jetbrains.uast.UElement
 import org.jetbrains.uast.UFile
 import org.jetbrains.uast.toUElement
-import org.jetbrains.uast.visitor.UastVisitor
 import org.junit.Test
 import java.io.File
 
 class DetachedJavaUastTest : AbstractJavaUastTest() {
 
+  private val detachers = mapOf(
+    PsiStatement::class.java through PsiElementFactory::createStatementFromText
+  )
+
   override fun check(testName: String, file: UFile) {
-    val psiFile = file.psi
-    val declarations = PsiTreeUtil.collectElements(psiFile, { it is PsiDeclarationStatement })
-    if (declarations.isNotEmpty()) {
-      println("declarations in $testName[${declarations.size}]")
-      for (declaration in declarations) {
-//            println("   " + declaration.text)
-//            println("   as U:" + declaration.toUElement())
-        TestCase.assertNotNull("convert ${declaration.text} in $testName\"", declaration.toUElement())
-        val createStatementFromText = elementFactory.createStatementFromText(declaration.text, declaration)
-        TestCase.assertTrue("convert ${declaration.text} in $testName\"", createStatementFromText is PsiDeclarationStatement)
-        TestCase.assertNotNull("convert2 ${declaration.text} in $testName\"", createStatementFromText.toUElement())
+    val detachablePsiElements = PsiTreeUtil.collectElementsOfType(file.psi, *detachers.keys.toTypedArray())
+      .map { psiElement -> psiElement to detachers.entries.first { it.key.isAssignableFrom(psiElement.javaClass) }.value }
+
+    for ((element, converter) in detachablePsiElements) {
+      val attachedUElement = element.toUElement()
+      TestCase.assertNotNull("UElement should be defined for attached element with text '${element.text}' in $testName",
+                             attachedUElement)
+      val detachedPsiElement = converter.invoke(elementFactory, element.text, element)
+      val detachedUElement = try {
+        detachedPsiElement.toUElement()
       }
+      catch (e: Exception) {
+        throw AssertionError("failed to convert '${detachedPsiElement.text}' to Uast in $testName", e)
+      }
+      TestCase.assertNotNull("UElement should be defined for detached element with text '${detachedPsiElement.text}' in $testName\"",
+                             detachedUElement)
+      TestCase.assertEquals("attached and detached elements should be equal at least in type for '${element.text}' in $testName\"",
+                            attachedUElement?.javaClass, detachedUElement?.javaClass)
     }
+
   }
 
   //  @Test
   fun ntestAll() {
-    val elementFactory = JavaPsiFacade.getElementFactory(project)
     for (file in File("/Users/nickl-mac/IdeaProjects/IDEA").walkTopDown()
       .filter { it.isFile && it.extension == "java" && !it.absolutePath.contains("testData") }) {
       try {
-//        val psiFile = myFixture.configureByText(file.name, file.readText())
         val psiFile: PsiFile = PsiFileFactory.getInstance(project).createFileFromText(JavaLanguage.INSTANCE, file.readText())
         val uFile = psiFile.toUElement(UFile::class.java)!!
-//        if (declarations.isNotEmpty()) {
-//          uFile.collectElements(UDeclarationsExpression::class.java).let { udeclarations ->
-//            TestCase.assertTrue("nubmer of declarations  ${udeclarations.size} >= ${declarations.size} " +
-//                                "declarations = ${declarations.joinToString { "[${it.text}]" }} in ${file.absolutePath}",
-//                                udeclarations.size >= declarations.size)
-//          }
-//
-//        }
+
         check(file.absolutePath, uFile)
       }
       catch (e: Exception) {
@@ -58,9 +54,6 @@ class DetachedJavaUastTest : AbstractJavaUastTest() {
       }
 
     }
-
-//    myFixture.configureByFile()
-
   }
 
   @Test
@@ -77,6 +70,9 @@ class DetachedJavaUastTest : AbstractJavaUastTest() {
 
   @Test
   fun testJava() = doTest("Simple/Simple.java")
+
+  @Test
+  fun testFor() = doTest("Simple/For.java")
 
   @Test
   fun testClass() = doTest("Simple/SuperTypes.java")
@@ -101,14 +97,5 @@ class DetachedJavaUastTest : AbstractJavaUastTest() {
 
 }
 
-fun <T : UElement> UElement.collectElements(clazz: Class<T>): List<T> {
-  val result = SmartList<T>()
-  this.accept(object : UastVisitor {
-    override fun visitElement(node: UElement): Boolean {
-      if (clazz.isAssignableFrom(node.javaClass))
-        result.add(node as T)
-      return false
-    }
-  })
-  return result
-}
+private infix fun Class<out PsiElement>.through(detacher: (PsiElementFactory, String, PsiElement) -> PsiElement)
+  : Pair<Class<PsiElement>, (PsiElementFactory, String, PsiElement) -> PsiElement> = (this as Class<PsiElement>) to detacher
