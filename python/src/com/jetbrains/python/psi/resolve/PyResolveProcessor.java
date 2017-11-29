c43223355ac90c58ff00da2d928944b05c972586
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2017-11-29 12:47:45 +0300

    remove empty getHint() and handleEvent() from processors

--- python/src/com/jetbrains/python/psi/resolve/PyResolveProcessor.java
+++ python/src/com/jetbrains/python/psi/resolve/PyResolveProcessor.java
@@ -1,22 +1,7 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.python.psi.resolve;
 
 import com.google.common.collect.Maps;
-import com.intellij.openapi.util.Key;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiNamedElement;
 import com.intellij.psi.ResolveState;
@@ -83,16 +68,6 @@ public class PyResolveProcessor implements PsiScopeProcessor {
       }
     }
     return myOwner == null || myOwner == ScopeUtil.getScopeOwner(element);
-  }
-
-  @Nullable
-  @Override
-  public <T> T getHint(@NotNull Key<T> hintKey) {
-    return null;
-  }
-
-  @Override
-  public void handleEvent(@NotNull Event event, @Nullable Object associated) {
   }
 
   @NotNull
