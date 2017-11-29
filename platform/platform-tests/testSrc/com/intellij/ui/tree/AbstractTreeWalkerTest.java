01c6f2c9a0ca859db591099815f2229a7c2bac4b
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2017-11-29 21:15:32 +0300

    remove TreeVisitor.Base implementation

--- platform/platform-tests/testSrc/com/intellij/ui/tree/AbstractTreeWalkerTest.java
+++ platform/platform-tests/testSrc/com/intellij/ui/tree/AbstractTreeWalkerTest.java
@@ -167,7 +167,7 @@ public class AbstractTreeWalkerTest {
   }
 
   private static TreeVisitor createFinder(TreeNode node) {
-    return new TreeVisitor.ByComponent<Object>(node, o -> o) {
+    return new TreeVisitor.ByComponent<Object, Object>(node, o -> o) {
       @Override
       protected boolean contains(@NotNull Object pathComponent, @NotNull Object thisComponent) {
         return pathComponent instanceof Node && thisComponent instanceof Node && ((Node)thisComponent).isNodeAncestor((Node)pathComponent);
