2ef44bf3fa9032796107c79baabb58358f209066
Roman Ivanov <Roman.Ivanov@jetbrains.com>
2017-11-29 14:50:43 +0700

    create CleanerCapturingThisInspection

--- java/java-impl/src/com/intellij/codeInspection/CleanerCapturingThisInspection.java
+++ java/java-impl/src/com/intellij/codeInspection/CleanerCapturingThisInspection.java
@@ -1,24 +1,22 @@
 // Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInspection;
 
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.project.Project;
+import com.intellij.lang.jvm.JvmModifier;
 import com.intellij.psi.*;
-import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.util.InheritanceUtil;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.util.PsiUtil;
-import com.intellij.psi.util.RedundantCastUtil;
-import com.intellij.util.ObjectUtils;
-import com.siyeh.ig.callMatcher.CallMapper;
 import com.siyeh.ig.callMatcher.CallMatcher;
-import com.siyeh.ig.psiutils.CommentTracker;
-import com.siyeh.ig.psiutils.ExpressionUtils;
-import org.jetbrains.annotations.Nls;
+import one.util.streamex.StreamEx;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import static com.siyeh.ig.psiutils.MethodCallUtils.getQualifierMethodCall;
+import static com.intellij.util.ObjectUtils.tryCast;
 
 public class CleanerCapturingThisInspection extends AbstractBaseJavaLocalInspectionTool {
+
+  private static final CallMatcher CLEANER_REGISTER = CallMatcher.instanceCall(
+    "java.lang.ref.Cleaner", "register"
+  ).parameterCount(2);
 
   @NotNull
   @Override
@@ -28,10 +26,59 @@ public class CleanerCapturingThisInspection extends AbstractBaseJavaLocalInspect
     }
     return new JavaElementVisitor() {
       @Override
-      public void visitMethodCallExpression(PsiMethodCallExpression methodCall) {
-
+      public void visitMethodCallExpression(PsiMethodCallExpression call) {
+        if (!CLEANER_REGISTER.test(call)) return;
+        PsiExpression[] expressions = call.getArgumentList().getExpressions();
+        if (!(expressions[0] instanceof PsiThisExpression)) return;
+        PsiExpression runnableExpr = expressions[1];
+        PsiClass containingClass = PsiTreeUtil.getParentOfType(call, PsiClass.class);
+        if (containingClass == null) return;
+        PsiElement referenceNameElement = call.getMethodExpression().getReferenceNameElement();
+        if (referenceNameElement == null) return;
+        if (runnableExpr instanceof PsiMethodReferenceExpression) {
+          if (PsiMethodReferenceUtil.isStaticallyReferenced((PsiMethodReferenceExpression)runnableExpr)) return;
+        }
+        else if (runnableExpr instanceof PsiLambdaExpression) {
+          PsiLambdaExpression lambda = (PsiLambdaExpression)runnableExpr;
+          if (lambda.getParameterList().getParametersCount() != 0) return;
+          PsiElement lambdaBody = lambda.getBody();
+          if (lambdaBody == null) return;
+          if (!capturesThis(lambdaBody, containingClass)) {
+          }
+        } else if (runnableExpr instanceof PsiNewExpression) {
+          PsiNewExpression newExpression = (PsiNewExpression)runnableExpr;
+          if (newExpression.getAnonymousClass() == null) {
+            PsiJavaCodeReferenceElement classReference = newExpression.getClassReference();
+            if(classReference == null) return;
+            PsiClass aClass = tryCast(classReference.resolve(), PsiClass.class);
+            if(aClass == null) return;
+            if(!aClass.hasModifier(JvmModifier.STATIC)) return;
+          }
+        }
+        holder.registerProblem(runnableExpr, "Cleanable captures 'this' reference");
       }
     };
+  }
+
+  private static boolean capturesThis(@NotNull PsiElement lambdaBody, @NotNull PsiClass containingClass) {
+    return StreamEx.ofTree(lambdaBody, el -> StreamEx.of(lambdaBody.getChildren()))
+      .noneMatch(element -> {
+        if (element instanceof PsiThisExpression) {
+          return true;
+        }
+        else if (element instanceof PsiMethodCallExpression) {
+          PsiMethod method = tryCast(((PsiMethodCallExpression)element).getMethodExpression().resolve(), PsiMethod.class);
+          return method == null || method.hasModifierProperty(PsiModifier.STATIC);
+        }
+        else if (element instanceof PsiReferenceExpression) {
+          PsiField field = tryCast(((PsiReferenceExpression)element).resolve(), PsiField.class);
+          if (field == null) return false;
+          PsiClass fieldContainingClass = field.getContainingClass();
+          if (fieldContainingClass == null) return false;
+          return InheritanceUtil.isInheritorOrSelf(containingClass, fieldContainingClass, true);
+        }
+        return false;
+      });
   }
 
 }
