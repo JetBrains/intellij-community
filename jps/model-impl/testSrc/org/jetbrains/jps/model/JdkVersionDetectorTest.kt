806a3ef88f96122f3b37f4aee393e7bad66b65bc
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2017-11-30 13:59:58 +0100

    Cleanup (minor test optimization)

--- jps/model-impl/testSrc/org/jetbrains/jps/model/JdkVersionDetectorTest.kt
+++ jps/model-impl/testSrc/org/jetbrains/jps/model/JdkVersionDetectorTest.kt
@@ -5,24 +5,18 @@ import com.intellij.openapi.util.Bitness
 import com.intellij.openapi.util.SystemInfo
 import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.jps.model.java.JdkVersionDetector
-import org.junit.After
 import org.junit.Test
-import java.util.concurrent.Executors
 
 class JdkVersionDetectorTest {
-  private val service = Executors.newSingleThreadExecutor()
-
-  @After fun tearDown() = service.shutdown()
-
   @Test fun detectJdkVersion() {
     val jdkHome = System.getProperty("java.home")
-    val jdkVersion = JdkVersionDetector.getInstance().detectJdkVersion(jdkHome, { service.submit(it) })
+    val jdkVersion = JdkVersionDetector.getInstance().detectJdkVersion(jdkHome)
     assertThat(jdkVersion).contains("\"${SystemInfo.JAVA_VERSION.removeSuffix("-ea")}\"")
   }
 
   @Test fun detectJdkVersionInfo() {
     val jdkHome = System.getProperty("java.home")
-    val jdkVersion = JdkVersionDetector.getInstance().detectJdkVersionInfo(jdkHome, { service.submit(it) })
+    val jdkVersion = JdkVersionDetector.getInstance().detectJdkVersionInfo(jdkHome)
     assertThat(jdkVersion?.version).contains("\"${SystemInfo.JAVA_VERSION.removeSuffix("-ea")}\"")
     assertThat(jdkVersion?.bitness).isEqualTo(if (SystemInfo.is64Bit) Bitness.x64 else Bitness.x32)
   }
