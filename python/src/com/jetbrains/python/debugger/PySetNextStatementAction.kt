869ec0b322b6f85c663e3b05e60f53cb23ceaca0
Elizaveta Shashkova <Elizaveta.Shashkova@jetbrains.com>
2017-11-30 20:10:34 +0300

    "Jump To Cursor" action isn't visible, when non-Python configuration selected (PY-27227)

--- python/src/com/jetbrains/python/debugger/PySetNextStatementAction.kt
+++ python/src/com/jetbrains/python/debugger/PySetNextStatementAction.kt
@@ -63,7 +63,7 @@ class PySetNextStatementAction : XDebuggerActionBase(true) {
       }
 
       override fun isEnabled(project: Project, event: AnActionEvent): Boolean {
-        return super.isEnabled(project, event) && PyDebugSupportUtils.isPythonConfigurationSelected(project)
+        return super.isEnabled(project, event) && PyDebugSupportUtils.isCurrentPythonDebugProcess(project)
       }
     }
   }
@@ -71,7 +71,7 @@ class PySetNextStatementAction : XDebuggerActionBase(true) {
   override fun getHandler(debuggerSupport: DebuggerSupport) = setNextStatementActionHandler
 
   override fun isHidden(event: AnActionEvent): Boolean {
-    return !PyDebugSupportUtils.isPythonConfigurationSelected(event.getData(CommonDataKeys.PROJECT))
+    return !PyDebugSupportUtils.isCurrentPythonDebugProcess(event.getData(CommonDataKeys.PROJECT))
   }
 
   companion object {
