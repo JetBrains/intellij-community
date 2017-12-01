97535d8271f50d5443da32b99285af463ce1e14a
Anna.Kozlova <anna.kozlova@jetbrains.com>
2017-12-01 16:27:00 +0100

    preserve comments: split declarations

--- java/java-impl/src/com/intellij/codeInsight/intention/impl/SplitDeclarationAction.java
+++ java/java-impl/src/com/intellij/codeInsight/intention/impl/SplitDeclarationAction.java
@@ -27,6 +27,7 @@ import com.intellij.psi.javadoc.PsiDocComment;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.refactoring.util.RefactoringUtil;
 import com.intellij.util.IncorrectOperationException;
+import com.siyeh.ig.psiutils.CommentTracker;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -147,11 +148,12 @@ public class SplitDeclarationAction extends PsiElementBaseIntentionAction {
         .createStatementFromText(var.getName() + "=xxx;", null);
       statement = (PsiExpressionStatement)CodeStyleManager.getInstance(project).reformat(statement);
       PsiAssignmentExpression assignment = (PsiAssignmentExpression)statement.getExpression();
+      CommentTracker commentTracker = new CommentTracker();
       PsiExpression initializer = var.getInitializer();
       PsiExpression rExpression = RefactoringUtil.convertInitializerToNormalExpression(initializer, var.getType());
 
-      assignment.getRExpression().replace(rExpression);
-      initializer.delete();
+      commentTracker.replace(assignment.getRExpression(), rExpression);
+      commentTracker.deleteAndRestoreComments(initializer);
 
       PsiElement block = decl.getParent();
       if (block instanceof PsiForStatement) {
