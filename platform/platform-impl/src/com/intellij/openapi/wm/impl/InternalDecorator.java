a8a45569cd9f7bb5908cfc4406dfcfedcad233d8
Konstantin Bulenkov <kb@jetbrains.com>
2017-12-03 03:57:40 +0300

    IDEA-182873 Remove Help icon from tool window toolbars

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/InternalDecorator.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/InternalDecorator.java
@@ -1,20 +1,7 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.wm.impl;
 
+import com.intellij.ide.actions.ContextHelpAction;
 import com.intellij.ide.actions.ResizeToolWindowAction;
 import com.intellij.ide.actions.ToggleToolbarAction;
 import com.intellij.idea.ActionsBundle;
@@ -437,6 +424,15 @@ public final class InternalDecorator extends JPanel implements Queryable, DataPr
       group.addSeparator();
       group.add(new HideAction());
     }
+
+    group.addSeparator();
+    group.add(new ContextHelpAction() {
+      @Nullable
+      @Override
+      protected String getHelpId(DataContext dataContext) {
+        return myToolWindow.getHelpId();
+      }
+    });
     return group;
   }
 
