ff3d49c50d59ce52517d1335b0cbdc7b29648153
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2017-12-04 17:45:10 +0700

    UnrollLoopAction: comment handling fixed

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/unrollLoop/beforeUnrollCounting.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/unrollLoop/beforeUnrollCounting.java
@@ -1,7 +1,8 @@
 // "Unroll loop" "true"
 class Test {
   void test() {
-    fo<caret>r (int i = 0; i < 10; i++) {
+    fo<caret>r (int i = 0; i < 10; i++ // line comment
+    ) {
       System.out.println("Hi!" + i);
     }
   }
