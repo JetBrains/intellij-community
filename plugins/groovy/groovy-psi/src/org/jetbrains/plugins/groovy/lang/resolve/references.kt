03061b84f8eae7d159fb22595e141925f6087644
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2017-12-04 20:52:16 +0300

    [groovy] extract separate method for creating a reference array

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/resolve/references.kt
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/resolve/references.kt
@@ -1,9 +1,31 @@
-// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+/*
+ * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
 package org.jetbrains.plugins.groovy.lang.resolve
 
 import com.intellij.psi.JavaPsiFacade
 import com.intellij.psi.PsiClass
 import org.jetbrains.plugins.groovy.lang.psi.GrReferenceElement
+import org.jetbrains.plugins.groovy.lang.psi.api.GroovyPolyVariantReference
+
+fun referenceArray(left: GroovyPolyVariantReference?, right: GroovyPolyVariantReference?): Array<out GroovyPolyVariantReference> {
+  return if (left == null) {
+    if (right == null) {
+      GroovyPolyVariantReference.EMPTY_ARRAY
+    }
+    else {
+      arrayOf(right)
+    }
+  }
+  else {
+    if (right == null) {
+      arrayOf(left)
+    }
+    else {
+      arrayOf(right, left)
+    }
+  }
+}
 
 fun GrReferenceElement<*>.resolveClassFqn(): PsiClass? {
   val fqn = qualifiedReferenceName ?: return null
