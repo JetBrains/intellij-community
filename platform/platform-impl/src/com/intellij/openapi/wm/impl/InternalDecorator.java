6e0d33f2035b6a2bb1d6b9cf1680b36da86e47c4
Konstantin Bulenkov <kb@jetbrains.com>
2017-12-04 18:34:42 +0300

    support multiple help ids for tool windows

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/InternalDecorator.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/InternalDecorator.java
@@ -1,4 +1,6 @@
-// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+/*
+ * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
 package com.intellij.openapi.wm.impl;
 
 import com.intellij.ide.actions.ContextHelpAction;
@@ -430,6 +432,13 @@ public final class InternalDecorator extends JPanel implements Queryable, DataPr
       @Nullable
       @Override
       protected String getHelpId(DataContext dataContext) {
+        Content content = myToolWindow.getContentManager().getSelectedContent();
+        if (content != null) {
+          String helpId = content.getHelpId();
+          if (helpId != null) {
+            return helpId;
+          }
+        }
         return myToolWindow.getHelpId();
       }
     });
