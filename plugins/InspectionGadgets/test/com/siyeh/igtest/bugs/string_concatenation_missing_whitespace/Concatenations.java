27dcc344ee252f075d1d5d95a4575a54fe0bdf6c
Bas Leijdekkers <basleijdekkers@gmail.com>
2017-12-06 11:53:59 +0100

    make IG test light

--- plugins/InspectionGadgets/test/com/siyeh/igtest/bugs/string_concatenation_missing_whitespace/Concatenations.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/bugs/string_concatenation_missing_whitespace/Concatenations.java
@@ -3,11 +3,11 @@ package com.siyeh.igtest.bugs.string_concatenation_missing_whitespace;
 class Concatenations {
 
   void foo(int i) {
-    System.out.println("SELECT column" +
+    System.out.println("SELECT column" <warning descr="String literal concatenation missing whitespace">+</warning>
                        "FROM table");
     System.out.println("no:" + i);
-    System.out.println("i" + i);
-    System.out.println("i" + ((String)"j"));
+    System.out.println("i" <warning descr="String literal concatenation missing whitespace">+</warning> i);
+    System.out.println("i" <warning descr="String literal concatenation missing whitespace">+</warning> ((String)"j"));
     System.out.println('{' + "a" + '\'');
     String.format("aaaa%n" + "bbbb");
   }
