ea1a210b7609ecd97f79f99e8dd9e657fd12260d
Lucas Smaira <lsmaira@google.com>
2017-12-07 15:31:40 +0000

    [AIA] Replace proto dependency from static to generated
    Instant App proto classes should not be pre-generated and dependency
    static. Instead, it is now generated as a pre build step and library
    point to its expected output.
    
    Test: N/A
    Change-Id: I4f8b8f1188664275e31a6e91268f651a892f54e8

--- build/groovy/org/jetbrains/intellij/build/AndroidStudioLibraryLicenses.groovy
+++ build/groovy/org/jetbrains/intellij/build/AndroidStudioLibraryLicenses.groovy
@@ -97,7 +97,7 @@ class AndroidStudioLibraryLicenses {
                        license: "Eclipse Public License 1.0", licenseUrl: "http://www.eclipse.org/legal/epl-v10.html"),
     new LibraryLicense(name: "uiautomatorviewer.jar", libraryName: "uiautomatorviewer.jar", license: "Apache 2.0"),
     new LibraryLicense(name: "TightVNC", libraryName: "tightvnc", license: "Commercial License"),
-    new LibraryLicense(name: "Instant App Proto Manifest", libraryName: "aia-manifest-proto-1.0-jarjar.jar",
+    new LibraryLicense(name: "Instant App Proto Manifest", libraryName: "aia-manifest-proto-1.0-jarjar",
                        license: "Apache 2.0", url: "http://source.android.com/"),
     new LibraryLicense(name: "Archive Patcher",
                        libraryName: "archive-patcher",
