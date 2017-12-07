4de6161959f26f7abb54c25fff8e48ccf4301a1a
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2017-12-07 17:45:17 +0100

    VM property `idea.set.proxy.selector` to disable setting global proxy selector (useful for Upsource)

--- platform/platform-api/src/com/intellij/util/proxy/CommonProxy.java
+++ platform/platform-api/src/com/intellij/util/proxy/CommonProxy.java
@@ -1,18 +1,16 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.intellij.util.proxy;
 
 import com.intellij.CommonBundle;
@@ -22,6 +20,7 @@ import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VfsUtil;
+import com.intellij.util.SystemProperties;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.net.NetUtils;
 import gnu.trove.THashMap;
@@ -51,7 +50,13 @@ public class CommonProxy extends ProxySelector {
   private static final AtomicReference<Map<String, String>> ourProps = new AtomicReference<>();
 
   static {
-    ProxySelector.setDefault(ourInstance);
+    if (isUseProxySelector()) {
+      ProxySelector.setDefault(ourInstance);
+    }
+  }
+
+  private static boolean isUseProxySelector() {
+    return SystemProperties.getBooleanProperty("idea.set.proxy.selector", false);
   }
 
   private final Object myLock = new Object();
@@ -69,6 +74,10 @@ public class CommonProxy extends ProxySelector {
   }
 
   public static void isInstalledAssertion() {
+    if (!isUseProxySelector()) {
+      return;
+    }
+
     final ProxySelector aDefault = ProxySelector.getDefault();
     if (ourInstance != aDefault) {
       // to report only once
