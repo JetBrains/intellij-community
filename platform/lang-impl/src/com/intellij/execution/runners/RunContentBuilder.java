d86da68991e60206ba9ce499d2b975554c7e0327
Konstantin Bulenkov <kb@jetbrains.com>
2017-12-07 12:29:30 +0300

    remove help button from Run tool window

--- platform/lang-impl/src/com/intellij/execution/runners/RunContentBuilder.java
+++ platform/lang-impl/src/com/intellij/execution/runners/RunContentBuilder.java
@@ -1,17 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package com.intellij.execution.runners;
 
@@ -25,7 +13,6 @@ import com.intellij.execution.ui.*;
 import com.intellij.execution.ui.actions.CloseAction;
 import com.intellij.execution.ui.layout.PlaceInGrid;
 import com.intellij.icons.AllIcons;
-import com.intellij.ide.actions.ContextHelpAction;
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
@@ -160,8 +147,6 @@ public class RunContentBuilder extends RunTab {
     actionGroup.addSeparator();
     actionGroup.add(PinToolwindowTabAction.getPinAction());
     actionGroup.add(new CloseAction(myEnvironment.getExecutor(), contentDescriptor, myProject));
-    final String helpId = contentDescriptor.getHelpId();
-    actionGroup.add(new ContextHelpAction(helpId != null ? helpId : myEnvironment.getExecutor().getHelpId()));
     return actionGroup;
   }
 
