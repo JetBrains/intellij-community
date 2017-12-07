2283a715ae76b824e5c9fc27bcdcbaa42faa2c5d
Konstantin Bulenkov <kb@jetbrains.com>
2017-12-08 02:03:48 +0300

    remove help button from Module dependencies tool window

--- platform/lang-impl/src/com/intellij/moduleDependencies/ShowModuleDependenciesAction.java
+++ platform/lang-impl/src/com/intellij/moduleDependencies/ShowModuleDependenciesAction.java
@@ -1,17 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package com.intellij.moduleDependencies;
 
@@ -67,6 +55,7 @@ public class ShowModuleDependenciesAction extends AnAction {
     ModulesDependenciesPanel panel = new ModulesDependenciesPanel(project, modules);
     AnalysisScope scope = modules != null ? new AnalysisScope(modules) : new AnalysisScope(project);
     Content content = ContentFactory.SERVICE.getInstance().createContent(panel, scope.getDisplayName(), false);
+    content.setHelpId(ModulesDependenciesPanel.HELP_ID);
     content.setDisposer(panel);
     panel.setContent(content);
     DependenciesAnalyzeManager.getInstance(project).addContent(content);
