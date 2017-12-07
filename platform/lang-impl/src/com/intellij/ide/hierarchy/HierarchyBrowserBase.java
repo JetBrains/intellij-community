7d651ce015b02b7c8f8bc69a2d9779d88e860087
Konstantin Bulenkov <kb@jetbrains.com>
2017-12-07 18:56:07 +0300

    remove help button from Hierarchy tool window

--- platform/lang-impl/src/com/intellij/ide/hierarchy/HierarchyBrowserBase.java
+++ platform/lang-impl/src/com/intellij/ide/hierarchy/HierarchyBrowserBase.java
@@ -1,24 +1,11 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package com.intellij.ide.hierarchy;
 
 import com.intellij.ide.CommonActionsManager;
 import com.intellij.ide.DefaultTreeExpander;
 import com.intellij.ide.actions.CloseTabToolbarAction;
-import com.intellij.ide.actions.ContextHelpAction;
 import com.intellij.ide.util.treeView.AbstractTreeUi;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.actionSystem.*;
@@ -111,9 +98,6 @@ public abstract class HierarchyBrowserBase extends SimpleToolWindowPanel impleme
     actionGroup.add(actionManager.getAction(PinToolwindowTabAction.ACTION_NAME));
     actionGroup.add(CommonActionsManager.getInstance().createExportToTextFileAction(new ExporterToTextFileHierarchy(this)));
     actionGroup.add(new CloseAction());
-    if (helpID != null) {
-      actionGroup.add(new ContextHelpAction(helpID));
-    }
   }
 
   protected abstract JTree getCurrentTree();
