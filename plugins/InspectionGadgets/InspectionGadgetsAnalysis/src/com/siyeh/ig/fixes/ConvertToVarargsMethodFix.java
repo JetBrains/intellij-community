4fcfc76365fb8d6439c18e723d4b39fe8c959435
Bas Leijdekkers <basleijdekkers@gmail.com>
2017-12-08 17:31:24 +0100

    IG: add instanceof check (EA-112441)

--- plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/fixes/ConvertToVarargsMethodFix.java
+++ plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/fixes/ConvertToVarargsMethodFix.java
@@ -1,17 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package com.siyeh.ig.fixes;
 
@@ -95,7 +83,11 @@ public class ConvertToVarargsMethodFix extends InspectionGadgetsFix {
 
   private static void makeMethodCallsVarargs(Collection<PsiReferenceExpression> referenceExpressions) {
     for (final PsiReferenceExpression referenceExpression : referenceExpressions) {
-      final PsiMethodCallExpression methodCallExpression = (PsiMethodCallExpression)referenceExpression.getParent();
+      final PsiElement parent = referenceExpression.getParent();
+      if (!(parent instanceof PsiMethodCallExpression)) {
+        continue;
+      }
+      final PsiMethodCallExpression methodCallExpression = (PsiMethodCallExpression)parent;
       final PsiExpressionList argumentList = methodCallExpression.getArgumentList();
       final PsiExpression[] arguments = argumentList.getExpressions();
       if (arguments.length == 0) {
