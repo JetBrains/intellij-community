7f5631e8e4de490f11632363e2b28fe1c8bb2340
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2017-12-11 17:39:51 +0300

    [groovy] wrap trait method stub serialization in read action (EA-102274)
    (cherry picked from commit 64a5e99) (IDEA-CR-27764)

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/resolve/GroovyTraitMethodsFileIndex.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/resolve/GroovyTraitMethodsFileIndex.java
@@ -1,21 +1,10 @@
 /*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package org.jetbrains.plugins.groovy.lang.resolve;
 
 import com.intellij.ide.highlighter.JavaClassFileType;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.io.BufferExposingByteArrayOutputStream;
 import com.intellij.openapi.util.text.StringUtil;
@@ -117,7 +106,7 @@ public class GroovyTraitMethodsFileIndex extends SingleEntryFileBasedIndexExtens
   @Override
   public void save(@NotNull DataOutput out, PsiJavaFileStub value) throws IOException {
     BufferExposingByteArrayOutputStream buffer = new BufferExposingByteArrayOutputStream();
-    SerializationManagerEx.getInstanceEx().serialize(value, buffer);
+    ApplicationManager.getApplication().runReadAction(() -> SerializationManagerEx.getInstanceEx().serialize(value, buffer));
     out.writeInt(buffer.size());
     out.write(buffer.getInternalBuffer(), 0, buffer.size());
   }
