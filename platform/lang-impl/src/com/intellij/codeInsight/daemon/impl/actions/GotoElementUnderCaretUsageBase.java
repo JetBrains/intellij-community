319ec0a552a4554617f8a510036b5d53f99b2062
Petr Kudriavtsev <petr.kudriavtsev@jetbrains.com>
2017-12-12 22:44:21 +0300

    Fixing CPP-11457: Navigation between marked occurrences - improved going to the previous occurrence if caret is inside or at the right border of current occurrence

--- platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/actions/GotoElementUnderCaretUsageBase.java
+++ platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/actions/GotoElementUnderCaretUsageBase.java
@@ -46,7 +46,7 @@ import java.util.Comparator;
     final Ref<Integer> next = new Ref<>();
     DaemonCodeAnalyzerEx.processHighlights(editor.getDocument(), project, null, startOffset, endOffset, info -> {
       if (info.type == HighlightInfoType.ELEMENT_UNDER_CARET_READ || info.type == HighlightInfoType.ELEMENT_UNDER_CARET_WRITE) {
-        if (ordering.compare(info.startOffset, caretOffset) > 0) {
+        if (ordering.compare(info.startOffset, caretOffset) > 0 && ordering.compare(info.endOffset, caretOffset) > 0) {
           if (next.isNull() || ordering.compare(next.get(), info.startOffset) > 0) {
             next.set(info.startOffset);
           }
