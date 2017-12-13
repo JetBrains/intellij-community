3c188717745246d5f49cf22da97887954a010f8d
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2017-12-13 15:52:15 +0300

    Enable `threading` stubs (PY-22765)

--- python/src/com/jetbrains/python/codeInsight/typing/PyTypeShed.kt
+++ python/src/com/jetbrains/python/codeInsight/typing/PyTypeShed.kt
@@ -42,7 +42,7 @@ object PyTypeShed {
   private val ONLY_SUPPORTED_PY2_MINOR = 7
   private val SUPPORTED_PY3_MINORS = 2..7
   val WHITE_LIST = setOf(TYPING, "six", "__builtin__", "builtins", "exceptions", "types", "datetime", "functools", "shutil", "re", "time",
-                         "argparse", "uuid")
+                         "argparse", "uuid", "threading")
   private val BLACK_LIST = setOf<String>()
 
   /**
