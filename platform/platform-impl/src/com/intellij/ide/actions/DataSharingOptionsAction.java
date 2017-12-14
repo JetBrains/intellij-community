5a73440a00c793ece7102cb98cb37811440bdd31
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2017-12-14 22:13:52 +0300

    GDPR support: menu action for Data Sharing Options

---
+++ platform/platform-impl/src/com/intellij/ide/actions/DataSharingOptionsAction.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+package com.intellij.ide.actions;
+
+import com.intellij.ide.gdpr.Consent;
+import com.intellij.ide.gdpr.ConsentOptions;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.DumbAwareAction;
+import com.intellij.openapi.util.Pair;
+import com.intellij.ui.AppUIUtil;
+
+import java.util.Collection;
+
+public class DataSharingOptionsAction extends DumbAwareAction {
+  public DataSharingOptionsAction() {
+    super("Data Sharing Options...", "Data Sharing Options", null);
+  }
+
+  @Override
+  public void update(AnActionEvent e) {
+    e.getPresentation().setEnabledAndVisible(!ConsentOptions.getConsents().first.isEmpty());
+  }
+
+  @Override
+  public void actionPerformed(AnActionEvent e) {
+    final Pair<Collection<Consent>, Boolean> consentsToShow = ConsentOptions.getConsents();
+    try {
+      final Collection<Consent> result = AppUIUtil.confirmConsentOptions(consentsToShow.first);
+      if (result != null) {
+        ConsentOptions.setConsents(result);
+      }
+    }
+    catch (Exception ex) {
+      Logger.getInstance(DataSharingOptionsAction.class).warn(ex);
+    }
+  }
+}
