f18e6fff7dc906db3122b85283d6656ff06fd3ad
Anna.Kozlova <anna.kozlova@jetbrains.com>
2017-12-14 20:18:15 +0100

    fix testdata

--- java/java-tests/testData/inspection/redundantCast/lambda/EnumConstantArgument.java
+++ java/java-tests/testData/inspection/redundantCast/lambda/EnumConstantArgument.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 enum En {
-  A(new Y<>((List) new ArrayList<>()));
+  A(new Y<>((<warning descr="Casting 'new ArrayList<>()' to 'List' is redundant">List</warning>) new ArrayList<>()));
 
   En(Y<List<Object>> list) {
   }
