e41ffcef13e8e94c8539a7dc3c052fab53148489
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2017-12-14 18:43:18 +0300

    Error about assignments to frozen dataclasses (PY-27398)

---
+++ python/src/com/jetbrains/python/inspections/PyDataclassInspection.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+package com.jetbrains.python.inspections
+
+import com.intellij.codeInspection.LocalInspectionToolSession
+import com.intellij.codeInspection.ProblemHighlightType
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiElementVisitor
+import com.jetbrains.python.codeInsight.stdlib.parseDataclassParameters
+import com.jetbrains.python.psi.PyClass
+import com.jetbrains.python.psi.PyTargetExpression
+import com.jetbrains.python.psi.PyTypedElement
+import com.jetbrains.python.psi.types.PyClassType
+
+class PyDataclassInspection : PyInspection() {
+
+  override fun buildVisitor(holder: ProblemsHolder,
+                            isOnTheFly: Boolean,
+                            session: LocalInspectionToolSession): PsiElementVisitor = Visitor(holder, session)
+
+  private class Visitor(holder: ProblemsHolder, session: LocalInspectionToolSession) : PyInspectionVisitor(holder, session) {
+
+    override fun visitPyTargetExpression(node: PyTargetExpression?) {
+      super.visitPyTargetExpression(node)
+
+      val cls = getPyClass(node?.qualifier) ?: return
+      if (parseDataclassParameters(cls, myTypeEvalContext)?.frozen == true) {
+        registerProblem(node, "'${cls.name}' object attribute '${node!!.name}' is read-only", ProblemHighlightType.GENERIC_ERROR)
+      }
+    }
+
+    private fun getPyClass(element: PyTypedElement?): PyClass? {
+      return (element?.let { myTypeEvalContext.getType(it) } as? PyClassType)?.pyClass
+    }
+  }
+}
---
+++ python/testSrc/com/jetbrains/python/inspections/PyDataclassInspectionTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+package com.jetbrains.python.inspections;
+
+import com.intellij.testFramework.LightProjectDescriptor;
+import com.jetbrains.python.fixtures.PyInspectionTestCase;
+import com.jetbrains.python.psi.LanguageLevel;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class PyDataclassInspectionTest extends PyInspectionTestCase {
+
+  // PY-27398
+  public void testAssignmentsToFrozen() {
+    doTest();
+  }
+
+  @Override
+  protected void doTest() {
+    runWithLanguageLevel(
+      LanguageLevel.PYTHON37,
+      () -> {
+        myFixture.copyFileToProject(getTestCaseDirectory() + "/dataclasses.py", "dataclasses.py");
+        super.doTest();
+        assertProjectFilesNotParsed(myFixture.getFile());
+      }
+    );
+  }
+
+  @Nullable
+  @Override
+  protected LightProjectDescriptor getProjectDescriptor() {
+    return ourPy3Descriptor;
+  }
+
+  @NotNull
+  @Override
+  protected Class<? extends PyInspection> getInspectionClass() {
+    return PyDataclassInspection.class;
+  }
+}
