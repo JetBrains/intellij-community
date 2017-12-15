e7ebd7eaf1da752a7314bf376975285e3969ceeb
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2017-12-15 14:33:43 +0300

    IDEA-183660 Quick doc on a link

--- java/java-impl/src/com/intellij/codeInsight/JavaTargetElementEvaluator.java
+++ java/java-impl/src/com/intellij/codeInsight/JavaTargetElementEvaluator.java
@@ -1,17 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package com.intellij.codeInsight;
 
@@ -23,6 +11,7 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.util.Computable;
 import com.intellij.psi.*;
 import com.intellij.psi.javadoc.PsiDocTag;
+import com.intellij.psi.javadoc.PsiDocToken;
 import com.intellij.psi.search.LocalSearchScope;
 import com.intellij.psi.search.SearchScope;
 import com.intellij.psi.search.searches.ClassInheritorsSearch;
@@ -112,6 +101,7 @@ public class JavaTargetElementEvaluator extends TargetElementEvaluatorEx2 implem
   @NotNull
   public ThreeState isAcceptableReferencedElement(@NotNull final PsiElement element, final PsiElement referenceOrReferencedElement) {
     if (isEnumConstantReference(element, referenceOrReferencedElement)) return ThreeState.NO;
+    if (element instanceof PsiDocToken) return ThreeState.NO;
     return super.isAcceptableReferencedElement(element, referenceOrReferencedElement);
   }
 
