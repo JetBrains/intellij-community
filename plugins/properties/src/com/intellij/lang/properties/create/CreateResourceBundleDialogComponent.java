5f3671128fc547e83b1360e21bbb5fb735ce3e73
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2017-12-18 17:21:02 +0300

    fix capitalization

--- plugins/properties/src/com/intellij/lang/properties/create/CreateResourceBundleDialogComponent.java
+++ plugins/properties/src/com/intellij/lang/properties/create/CreateResourceBundleDialogComponent.java
@@ -1,17 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package com.intellij.lang.properties.create;
 
@@ -114,7 +102,7 @@ public class CreateResourceBundleDialogComponent {
       myComponent = new CreateResourceBundleDialogComponent(myProject, myDirectory, resourceBundle);
       init();
       initValidation();
-      setTitle(resourceBundle == null ? "Create resource bundle" : String.format("Add locales to resource bundle \'%s\'", resourceBundle.getBaseName()));
+      setTitle(resourceBundle == null ? "Create Resource Bundle" : "Add Locales to Resource Bundle " + resourceBundle.getBaseName());
     }
 
     @Override
