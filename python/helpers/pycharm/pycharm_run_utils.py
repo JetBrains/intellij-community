46dec51f368273b47060464492801d5a359e2311
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2017-12-19 00:57:07 +0300

    PY-27395: Cosmetic fixes
    Number of spaces  + one line moved under "else"

--- python/helpers/pycharm/pycharm_run_utils.py
+++ python/helpers/pycharm/pycharm_run_utils.py
@@ -28,17 +28,17 @@ def adjust_django_sys_path():
 
 def import_system_module(name):
   if sys.platform == "cli":    # hack for the ironpython
-      return __import__(name)
+    return __import__(name)
 
   if sys.version_info >= (2, 7):
-      import importlib
-      module = importlib.import_module(name)
-      module.__name__ = "pycharm_" + name
-      return module
+    import importlib
+    module = importlib.import_module(name)
+    module.__name__ = "pycharm_" + name
+    return module
   else:
-      import imp
-      f, filename, desc = imp.find_module(name)
-  return imp.load_module('pycharm_' + name, f, filename, desc)
+    import imp
+    f, filename, desc = imp.find_module(name)
+    return imp.load_module('pycharm_' + name, f, filename, desc)
 
 
 def getModuleName(prefix, cnt):
