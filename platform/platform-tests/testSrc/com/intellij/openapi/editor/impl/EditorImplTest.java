JetBrains/intellij-community

commit 97a3300bc24fdf81b277dc0b7f9547a3f683d68f

IDEA-174558 bug at pasting with sticky selection active

platform/platform-api/src/com/intellij/openapi/editor/EditorModificationUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -100,17 +100,6 @@ public class EditorModificationUtil {
   }
 
   private static int insertStringAtCaretNoScrolling(Editor editor, @NotNull String s, boolean toProcessOverwriteMode, boolean toMoveCaret, int caretShift) {
-    final SelectionModel selectionModel = editor.getSelectionModel();
-    if (selectionModel.hasSelection()) {
-      VisualPosition startPosition = selectionModel.getSelectionStartPosition();
-      if (editor.isColumnMode() && editor.getCaretModel().supportsMultipleCarets() && startPosition != null) {
-        editor.getCaretModel().moveToVisualPosition(startPosition);
-      }
-      else {
-        editor.getCaretModel().moveToOffset(selectionModel.getSelectionStart(), true);
-      }
-    }
-
     // There is a possible case that particular soft wraps become hard wraps if the caret is located at soft wrap-introduced virtual
     // space, hence, we need to give editor a chance to react accordingly.
     editor.getSoftWrapModel().beforeDocumentChangeAtCaret();
@@ -122,6 +111,7 @@ public class EditorModificationUtil {
     }
 
     Document document = editor.getDocument();
+    SelectionModel selectionModel = editor.getSelectionModel();
     if (editor.isInsertMode() || !toProcessOverwriteMode) {
       if (selectionModel.hasSelection()) {
         oldOffset = selectionModel.getSelectionStart();

platform/platform-tests/testSrc/com/intellij/openapi/editor/impl/EditorImplTest.java
@@ -477,4 +477,17 @@ public class EditorImplTest extends AbstractEditorTest {
     runFoldingOperation(() -> model.removeFoldRegion(regionRef.get()));
     assertEquals(new VisualPosition(1, 1), myEditor.offsetToVisualPosition(3));
   }
+  
+  public void testPasteWithStickySelection() throws Exception {
+    initText("<selection>abc<caret></selection>\ndef");
+    copy();
+    down();
+    home();
+    ((EditorEx)myEditor).setStickySelection(true);
+    right();
+    right();
+    right();
+    paste();
+    checkResultByText("abc\nabc<caret>");
+  }
 }

commit 93e888bd4f4bb1dd15a5c24b3c8b4bec0ec1f351

EA-107792 - IAE: DocumentImpl.replaceString
regression after commit 97a3300

platform/platform-api/src/com/intellij/openapi/editor/EditorModificationUtil.java
@@ -1,16 +1,6 @@
-// Copyright 2000-2017 JetBrains s.r.o.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
 package com.intellij.openapi.editor;
 
 import com.intellij.codeInsight.hint.HintManager;
@@ -101,7 +91,7 @@ public class EditorModificationUtil {
     // There is a possible case that particular soft wraps become hard wraps if the caret is located at soft wrap-introduced virtual
     // space, hence, we need to give editor a chance to react accordingly.
     editor.getSoftWrapModel().beforeDocumentChangeAtCaret();
-    int oldOffset = editor.getCaretModel().getOffset();
+    int oldOffset = editor.getSelectionModel().getSelectionStart();
 
     String filler = editor.getSelectionModel().hasSelection() ? "" : calcStringToFillVirtualSpace(editor);
     if (filler.length() > 0) {

platform/platform-tests/testSrc/com/intellij/openapi/editor/impl/EditorImplTest.java
@@ -519,4 +519,11 @@ public class EditorImplTest extends AbstractEditorTest {
     assertEquals(new LogicalPosition(0, 2), myEditor.getCaretModel().getLogicalPosition());
     assertEquals(new VisualPosition(0, 2), myEditor.getCaretModel().getVisualPosition());
   }
+
+  public void testTypingInOverwriteModeWithMultilineSelection() {
+    initText("a<selection>b\nc<caret></selection>d");
+    ((EditorEx)myEditor).setInsertMode(false);
+    type(' ');
+    checkResultByText("a <caret>");
+  }
 }