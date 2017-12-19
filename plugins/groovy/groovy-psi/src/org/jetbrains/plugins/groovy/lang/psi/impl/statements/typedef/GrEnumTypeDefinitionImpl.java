df3e2faff147141364ce4bab8810497bd881e9d4
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2017-12-19 19:56:16 +0300

    [groovy] @NotNull

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/statements/typedef/GrEnumTypeDefinitionImpl.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/statements/typedef/GrEnumTypeDefinitionImpl.java
@@ -1,4 +1,6 @@
-// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+/*
+ * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
 
 package org.jetbrains.plugins.groovy.lang.psi.impl.statements.typedef;
 
@@ -139,6 +141,7 @@ public class GrEnumTypeDefinitionImpl extends GrTypeDefinitionImpl implements Gr
     });
   }
 
+  @NotNull
   @Override
   public GrEnumConstant[] getEnumConstants() {
     GrEnumConstantList list = getEnumConstantList();
--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/statements/typedef/GrTypeDefinitionBodyBase.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/statements/typedef/GrTypeDefinitionBodyBase.java
@@ -1,17 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package org.jetbrains.plugins.groovy.lang.psi.impl.statements.typedef;
 
@@ -60,6 +48,7 @@ public abstract class GrTypeDefinitionBodyBase extends GrStubElementBase<EmptySt
     return "Type definition body";
   }
 
+  @NotNull
   @Override
   public GrField[] getFields() {
     GrVariableDeclaration[] declarations = getStubOrPsiChildren(GroovyElementTypes.VARIABLE_DEFINITION, GrVariableDeclaration.ARRAY_FACTORY);
@@ -76,11 +65,13 @@ public abstract class GrTypeDefinitionBodyBase extends GrStubElementBase<EmptySt
     return result.toArray(new GrField[result.size()]);
   }
 
+  @NotNull
   @Override
   public GrMethod[] getMethods() {
     return getStubOrPsiChildren(TokenSets.METHOD_DEFS, GrMethod.ARRAY_FACTORY);
   }
 
+  @NotNull
   @Override
   public GrMembersDeclaration[] getMemberDeclarations() {
     return findChildrenByClass(GrMembersDeclaration.class);
