71b4910f2cd369322ace5b7950fc52af2965661b
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2017-12-20 11:25:36 +0300

    validation for EA-103566

--- platform/lang-impl/src/com/intellij/codeInsight/folding/impl/FoldingUpdate.java
+++ platform/lang-impl/src/com/intellij/codeInsight/folding/impl/FoldingUpdate.java
@@ -1,17 +1,5 @@
 /*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 
 package com.intellij.codeInsight.folding.impl;
@@ -111,9 +99,17 @@ public class FoldingUpdate {
     final UpdateFoldRegionsOperation operation = new UpdateFoldRegionsOperation(project, editor, file, elementsToFold,
                                                                                 applyDefaultState ? EXCEPT_CARET_REGION : NO, 
                                                                                 !applyDefaultState, false);
+    long documentTimestamp = document.getModificationStamp();
+    int documentLength = document.getTextLength();
     AtomicBoolean alreadyExecuted = new AtomicBoolean();
     Runnable runnable = () -> {
       if (alreadyExecuted.compareAndSet(false, true)) {
+        if (documentTimestamp != editor.getDocument().getModificationStamp()) {
+          LOG.error("Document has changed since fold regions were calculated");
+        }
+        else if (documentLength != editor.getDocument().getTextLength()) {
+          LOG.error("Document length has changed since fold regions were calculated");
+        }
         editor.getFoldingModel().runBatchFoldingOperationDoNotCollapseCaret(operation);
       }
     };
