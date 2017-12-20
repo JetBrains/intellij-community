48bc14419573bcf9e94b96d0934c75a7cb825a29
Radomir Malaczek <radomirm@google.com>
2017-12-19 17:00:07 -0800

    Revert RefCountingRecordsTable changes
    This reverts commits:
    * 7bee6f896e1b52418ff86488a1c3800d5247a439
      Add logging for RefCountingRecordsTable
    * b1592014beb81eb24ed8cda0024ea211c8b65c43
      Limit logs reported by RefCountingRecordsTable
    
    Test: N/A
    
    Change-Id: Ief46979695e5599f508e3811d6e8a1b428570082

--- platform/util/src/com/intellij/util/io/storage/RefCountingRecordsTable.java
+++ platform/util/src/com/intellij/util/io/storage/RefCountingRecordsTable.java
@@ -19,11 +19,6 @@ import com.intellij.util.io.PagePool;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 class RefCountingRecordsTable extends AbstractRecordsTable {
   private static final int VERSION = 1;
@@ -56,69 +51,21 @@ class RefCountingRecordsTable extends AbstractRecordsTable {
     markDirty();
 
     int offset = getOffset(record, REF_COUNT_OFFSET);
-    final int currentRefCount = myStorage.getInt(offset);
-    addDebugHistoryEntry(record, "incRefCount: " + currentRefCount + " -> " + (currentRefCount + 1));
-    myStorage.putInt(offset, currentRefCount + 1);
+    myStorage.putInt(offset, myStorage.getInt(offset) + 1);
   }
-
-  private Map<Integer, List<Throwable>> myDebugHistory = new ConcurrentHashMap<Integer, List<Throwable>>();
-  // Report debug info only once to limit log size.
-  private static boolean ourWasObjectHistoryReported = false;
 
   public boolean decRefCount(int record) {
     markDirty();
 
     int offset = getOffset(record, REF_COUNT_OFFSET);
     int count = myStorage.getInt(offset);
-    addDebugHistoryEntry(record, "decRefCount: " + count + " -> " + (count - 1));
-    //assert count > 0; // TODO: Re-enable the assert and remove debug code: b/70639656
-    if (count <= 0) {
-      if (!ourWasObjectHistoryReported) {
-        ourWasObjectHistoryReported = true;
-        dumpObjectDebugHistory(record);
-      }
-    } else {
-      count--;
-      myStorage.putInt(offset, count);
-    }
+    assert count > 0;
+    count--;
+    myStorage.putInt(offset, count);
     return count == 0;
-  }
-
-  private void dumpObjectDebugHistory(int recordId) {
-    System.out.println("Illegal reference count for record: " + recordId);
-    if (myStorage != null) {
-      System.out.println("myStorage.isDisposed() = " + myStorage.isDisposed());
-      if (!myStorage.isDisposed()) {
-        System.out.println("myStorage.getFile() = " + myStorage.getFile());
-      }
-    }
-    System.out.println("Reference counting history:");
-    final List<Throwable> throwableList = myDebugHistory.get(recordId);
-    synchronized (throwableList) {
-      for (Throwable entry : throwableList) {
-        entry.printStackTrace(System.out);
-      }
-    }
   }
 
   public int getRefCount(int record) {
     return myStorage.getInt(getOffset(record, REF_COUNT_OFFSET));
-  }
-
-  private void addDebugHistoryEntry(int recordId, String message) {
-    List<Throwable> history = myDebugHistory.get(recordId);
-    if (history == null) {
-      history = new ArrayList<Throwable>();
-      myDebugHistory.put(recordId, history);
-    }
-    synchronized (history) {
-      history.add(new DebugEntryException(message));
-    }
-  }
-
-  private static class DebugEntryException extends Exception {
-    public DebugEntryException(String message) {
-      super(message);
-    }
   }
 }
