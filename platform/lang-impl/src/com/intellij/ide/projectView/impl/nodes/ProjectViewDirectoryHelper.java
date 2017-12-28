58cf802a5d2adad28879f0f478bba24f82931f91
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2017-12-28 19:24:22 +0300

    use JBIterable to collect parents

--- platform/lang-impl/src/com/intellij/ide/projectView/impl/nodes/ProjectViewDirectoryHelper.java
+++ platform/lang-impl/src/com/intellij/ide/projectView/impl/nodes/ProjectViewDirectoryHelper.java
@@ -1,17 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 
 package com.intellij.ide.projectView.impl.nodes;
@@ -44,6 +32,7 @@ import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.util.PsiUtilCore;
 import com.intellij.util.FontUtil;
 import com.intellij.util.containers.ContainerUtil;
+import com.intellij.util.containers.JBIterable;
 import gnu.trove.THashSet;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -157,14 +146,10 @@ public class ProjectViewDirectoryHelper {
       if (settings == null) return false; // unexpected
       if (!settings.isFlattenPackages() && settings.isHideEmptyMiddlePackages()) {
         if (element instanceof PsiDirectory) {
-          for (PsiDirectory dir : collectParents(directory, owner)) {
-            if (Comparing.equal(element, dir)) return true;
-          }
+          if (getParents(directory, owner).first(dir -> Comparing.equal(element, dir)) != null) return true;
         }
         else if (element instanceof VirtualFile) {
-          for (PsiDirectory dir : collectParents(directory, owner)) {
-            if (Comparing.equal(element, dir.getVirtualFile())) return true;
-          }
+          if (getParents(directory, owner).first(dir -> Comparing.equal(element, dir.getVirtualFile())) != null) return true;
         }
       }
     }
@@ -178,7 +163,7 @@ public class ProjectViewDirectoryHelper {
       PsiDirectory parent = directory.getParent();
       if (parent == null || skipDirectory(parent)) return true;
       if (isEmptyMiddleDirectory(directory, true, filter)) return false;
-      for (PsiDirectory dir : collectParents(directory, owner)) {
+      for (PsiDirectory dir : getParents(directory, owner)) {
         if (!dir.isValid()) return false;
         parent = dir.getParent();
         if (parent == null || skipDirectory(parent)) return false;
@@ -189,17 +174,10 @@ public class ProjectViewDirectoryHelper {
   }
 
   @NotNull
-  private static List<PsiDirectory> collectParents(PsiDirectory directory, Object owner) {
-    if (directory != null && owner instanceof PsiDirectory && PsiTreeUtil.isAncestor((PsiDirectory)owner, directory, true)) {
-      List<PsiDirectory> list = ContainerUtil.newSmartList();
-      while (true) {
-        directory = directory.getParent();
-        if (directory == null) break; // unexpected
-        if (directory.equals(owner)) return list;
-        list.add(directory);
-      }
-    }
-    return Collections.emptyList();
+  private static JBIterable<PsiDirectory> getParents(PsiDirectory directory, Object owner) {
+    return directory != null && owner instanceof PsiDirectory && PsiTreeUtil.isAncestor((PsiDirectory)owner, directory, true)
+           ? JBIterable.generate(directory.getParent(), PsiDirectory::getParent).takeWhile(dir -> dir != owner)
+           : JBIterable.empty();
   }
 
   @NotNull
