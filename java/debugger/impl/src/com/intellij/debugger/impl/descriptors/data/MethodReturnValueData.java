68d58b23c0b484d39cd1634e75dc7479067a4816
Egor Ushakov <egor.ushakov@jetbrains.com>
2017-12-28 16:14:31 +0300

    cleanup: use Objects.hash

--- java/debugger/impl/src/com/intellij/debugger/impl/descriptors/data/MethodReturnValueData.java
+++ java/debugger/impl/src/com/intellij/debugger/impl/descriptors/data/MethodReturnValueData.java
@@ -1,17 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package com.intellij.debugger.impl.descriptors.data;
 
@@ -22,6 +10,8 @@ import com.sun.jdi.Method;
 import com.sun.jdi.Value;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
 
 public final class MethodReturnValueData extends DescriptorData<MethodReturnValueDescriptorImpl>{
   private final @Nullable Value myReturnValue;
@@ -59,10 +49,7 @@ public final class MethodReturnValueData extends DescriptorData<MethodReturnValu
   }
 
   public int hashCode() {
-    int result;
-    result = (myReturnValue != null ? myReturnValue.hashCode() : 0);
-    result = 31 * result + myMethod.hashCode();
-    return result;
+    return Objects.hash(myReturnValue, myMethod);
   }
 
   public DisplayKey<MethodReturnValueDescriptorImpl> getDisplayKey() {
