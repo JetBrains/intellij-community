cfc01324fd959182744448d64ca068b84f8ab519
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2018-01-02 18:29:13 +0300

    2018

--- platform/testFramework/src/com/intellij/idea/Bombed.java
+++ platform/testFramework/src/com/intellij/idea/Bombed.java
@@ -1,17 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package com.intellij.idea;
 
@@ -25,7 +13,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
 public @interface Bombed {
-  int year() default 2017;
+  int year() default 2018;
   @JdkConstants.CalendarMonth int month();
   int day();
   int time() default 0;
