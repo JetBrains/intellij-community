e73f0bc598238cdd9f8a3c50f5d5f6c57f9dee0f
Anna.Kozlova <anna.kozlova@jetbrains.com>
2018-01-08 19:07:30 +0100

    type#getCanonycalText() : prepend 'anonymous' for unresolved base (IDEA-CR-28319)

--- java/java-psi-impl/src/com/intellij/psi/impl/source/PsiImmediateClassType.java
+++ java/java-psi-impl/src/com/intellij/psi/impl/source/PsiImmediateClassType.java
@@ -196,10 +196,10 @@ public class PsiImmediateClassType extends PsiClassType.Stub {
     if (aClass instanceof PsiAnonymousClass) {
       ClassResolveResult baseResolveResult = ((PsiAnonymousClass)aClass).getBaseClassType().resolveGenerics();
       PsiClass baseClass = baseResolveResult.getElement();
+      if (textType == TextType.INT_CANONICAL) {
+        buffer.append("anonymous ");
+      }
       if (baseClass != null) {
-        if (textType == TextType.INT_CANONICAL) {
-          buffer.append("anonymous ");
-        }
         buildText(baseClass, baseResolveResult.getSubstitutor(), buffer, textType, false);
       }
       else {
