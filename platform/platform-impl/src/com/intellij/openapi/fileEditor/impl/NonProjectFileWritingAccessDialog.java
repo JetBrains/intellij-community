069a690104a2f8452e42de60bbbc7481599323a0
Anton Makeev <Anton.Makeev@jetbrains.com>
2018-01-08 18:03:52 +0100

    NPE: NonProjectFileWritingAccessDialog.lambda$setTextAndMnemonicAndListeners$|setTextAndMnemonicAndListeners
    https://ea.jetbrains.com/browser/ea_problems/104051

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/NonProjectFileWritingAccessDialog.java
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/NonProjectFileWritingAccessDialog.java
@@ -1,17 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package com.intellij.openapi.fileEditor.impl;
 
@@ -89,8 +77,12 @@ public class NonProjectFileWritingAccessDialog extends DialogWrapper {
     button.setDisplayedMnemonicIndex(button.getText().indexOf(mnemonic));
     
     // enabled OK button when user selects an option
-    button.addActionListener(e -> button.getRootPane().setDefaultButton(getButton(getOKAction())));
-    button.addItemListener(e -> button.getRootPane().setDefaultButton(getButton(getOKAction())));
+    Runnable setDefaultButton = () -> {
+      JRootPane rootPane = button.getRootPane();
+      if (rootPane != null) rootPane.setDefaultButton(getButton(getOKAction()));
+    };
+    button.addActionListener(e -> setDefaultButton.run());
+    button.addItemListener(e -> setDefaultButton.run());
   }
 
   @Nullable
