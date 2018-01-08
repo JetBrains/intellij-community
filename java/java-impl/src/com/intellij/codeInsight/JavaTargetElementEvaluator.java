0d68a7724aec9a79ecff262bad6d42dc498f4dc7
Anna.Kozlova <anna.kozlova@jetbrains.com>
2018-01-08 19:10:35 +0100

    getUseScope() (IDEA-CR-28320)

--- java/java-impl/src/com/intellij/codeInsight/JavaTargetElementEvaluator.java
+++ java/java-impl/src/com/intellij/codeInsight/JavaTargetElementEvaluator.java
@@ -362,7 +362,7 @@ public class JavaTargetElementEvaluator extends TargetElementEvaluatorEx2 implem
     final PsiReferenceExpression referenceExpression = editor != null ? findReferenceExpression(editor) : null;
     if (referenceExpression != null && element instanceof PsiMethod) {
        if (!PsiUtil.canBeOverridden((PsiMethod)element)) {
-         return new LocalSearchScope(element.getContainingFile());
+         return element.getUseScope();
        }
       final PsiClass[] memberClass = getClassesWithMember(referenceExpression, (PsiMember)element);
       if (memberClass != null && memberClass.length == 1) {
