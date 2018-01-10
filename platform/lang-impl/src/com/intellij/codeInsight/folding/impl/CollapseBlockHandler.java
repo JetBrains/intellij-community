356657307b5d48da4f6b78d0279dc6351c624dfd
Alexey Utkin <alexey.utkin@jetbrains.com>
2018-01-10 18:33:05 +0300

    CPP-2906 Code Fold of control flow statements (if/else, do/while, for, switch)
    move java-code to shared package

--- platform/lang-impl/src/com/intellij/codeInsight/folding/impl/CollapseBlockHandler.java
+++ platform/lang-impl/src/com/intellij/codeInsight/folding/impl/CollapseBlockHandler.java
@@ -1,21 +1,11 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package com.intellij.codeInsight.folding.impl;
 
 import com.intellij.codeInsight.CodeInsightActionHandler;
+import com.intellij.codeInsight.folding.impl.EditorFoldingInfo;
+import com.intellij.codeInsight.folding.impl.FoldingUtil;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.FoldRegion;
@@ -44,7 +34,7 @@ public class CollapseBlockHandler implements CodeInsightActionHandler {
         element = file.findElementAt(editor.getCaretModel().getOffset());
       }
       if (element == null) return;
-      PsiCodeBlock block = PsiTreeUtil.getParentOfType(element, PsiCodeBlock.class);
+      PsiElement block = PsiTreeUtil.getParentOfType(element, PsiCodeBlock.class);
       FoldRegion previous = null;
       FoldRegion myPrevious = null;
       while (block != null) {
