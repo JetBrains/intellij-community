20ebd5fee1fbab69a31985377cd95265b976d87d
Anna.Kozlova <anna.kozlova@jetbrains.com>
2018-01-11 20:23:55 +0100

    postpone open files in the editor after write action completes
    EA-114507 - assert: NoSwingUnderWriteAction.lambda$watchForEvents$

--- platform/lang-impl/src/com/intellij/refactoring/move/moveFilesOrDirectories/MoveFilesOrDirectoriesProcessor.java
+++ platform/lang-impl/src/com/intellij/refactoring/move/moveFilesOrDirectories/MoveFilesOrDirectoriesProcessor.java
@@ -194,14 +194,15 @@ public class MoveFilesOrDirectoriesProcessor extends BaseRefactoringProcessor {
 
       retargetUsages(usages, oldToNewMap);
 
-      if (MoveFilesOrDirectoriesDialog.isOpenInEditor()) {
-        EditorHelper.openFilesInEditor(movedFiles.toArray(new PsiFile[movedFiles.size()]));
-      }
-
       // Perform CVS "add", "remove" commands on moved files.
 
       if (myMoveCallback != null) {
         myMoveCallback.refactoringCompleted();
+      }
+      if (MoveFilesOrDirectoriesDialog.isOpenInEditor()) {
+        ApplicationManager.getApplication().invokeLater(() ->
+          EditorHelper.openFilesInEditor(movedFiles.stream().filter(PsiElement::isValid).toArray(PsiFile[]::new))
+        );
       }
 
     }
