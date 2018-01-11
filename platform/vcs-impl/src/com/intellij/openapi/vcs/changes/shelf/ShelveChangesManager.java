fcb57bd75e0ec5d3a3fa2fbe388741444e829a1f
Aleksey Pivovarov <AMPivovarov@gmail.com>
2018-01-11 21:10:40 +0300

    vcs: support partial changes in Shelve Silently
    * properly group changes by their changelist
    * avoid rolling back changes from different changelists independently
       (Changes can be reverted both via Document and via VCS,
        causing file cache conflict)

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/shelf/ShelveChangesManager.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/shelf/ShelveChangesManager.java
@@ -426,13 +426,7 @@ public class ShelveChangesManager extends AbstractProjectComponent implements JD
       mySchemeManager.addScheme(changeList, false);
 
       if (rollback) {
-        final String operationName = UIUtil.removeMnemonic(RollbackChangesDialog.operationNameByChanges(myProject, changes));
-        boolean modalContext = ApplicationManager.getApplication().isDispatchThread() && LaterInvocator.isInModalContext();
-        if (progressIndicator != null) {
-          progressIndicator.startNonCancelableSection();
-        }
-        new RollbackWorker(myProject, operationName, modalContext).
-          doRollback(changes, true, null, VcsBundle.message("shelve.changes.action"));
+        rollbackChangesAfterShelve(changes);
       }
     }
     finally {
@@ -440,6 +434,18 @@ public class ShelveChangesManager extends AbstractProjectComponent implements JD
     }
 
     return changeList;
+  }
+
+  private void rollbackChangesAfterShelve(@NotNull Collection<Change> changes) {
+    final ProgressIndicator progressIndicator = ProgressManager.getInstance().getProgressIndicator();
+    if (progressIndicator != null) {
+      progressIndicator.startNonCancelableSection();
+    }
+
+    final String operationName = UIUtil.removeMnemonic(RollbackChangesDialog.operationNameByChanges(myProject, changes));
+    boolean modalContext = ApplicationManager.getApplication().isDispatchThread() && LaterInvocator.isInModalContext();
+    new RollbackWorker(myProject, operationName, modalContext).
+      doRollback(changes, true, null, VcsBundle.message("shelve.changes.action"));
   }
 
   @NotNull
@@ -747,18 +753,41 @@ public class ShelveChangesManager extends AbstractProjectComponent implements JD
   public List<ShelvedChangeList> shelveChangesInSeparatedLists(@NotNull Collection<Change> changes) {
     List<String> failedChangeLists = ContainerUtil.newArrayList();
     List<ShelvedChangeList> result = ContainerUtil.newArrayList();
-    List<LocalChangeList> changeListsCopy = ChangeListManager.getInstance(myProject).getChangeListsCopy();
-    for (LocalChangeList list : changeListsCopy) {
-      Collection<Change> changesForChangelist = ContainerUtil.intersection(list.getChanges(), changes);
-      if (changesForChangelist.isEmpty()) continue;
-      try {
-        result.add(shelveChanges(changesForChangelist, list.getName(), true));
+    List<Change> shelvedChanges = ContainerUtil.newArrayList();
+
+    List<LocalChangeList> changeLists = ChangeListManager.getInstance(myProject).getChangeLists();
+    for (LocalChangeList list : changeLists) {
+      HashSet<Change> changeSet = new HashSet<>(list.getChanges());
+
+      List<Change> changesForChangelist = new ArrayList<>();
+      for (Change change : changes) {
+        boolean inChangelist;
+        if (change instanceof ChangeListChange) {
+          inChangelist = ((ChangeListChange)change).getChangeListId().equals(list.getId());
+        }
+        else {
+          inChangelist = changeSet.contains(change);
+        }
+
+        if (inChangelist) {
+          changesForChangelist.add(change);
+        }
       }
-      catch (Exception e) {
-        LOG.warn(e);
-        failedChangeLists.add(list.getName());
+
+      if (!changesForChangelist.isEmpty()) {
+        try {
+          result.add(shelveChanges(changesForChangelist, list.getName(), false));
+          shelvedChanges.addAll(changesForChangelist);
+        }
+        catch (Exception e) {
+          LOG.warn(e);
+          failedChangeLists.add(list.getName());
+        }
       }
     }
+
+    rollbackChangesAfterShelve(shelvedChanges);
+
     if (!failedChangeLists.isEmpty()) {
       VcsNotifier.getInstance(myProject).notifyError("Shelf Failed", String
         .format("Shelving changes for %s [%s] failed", StringUtil.pluralize("changelist", failedChangeLists.size()),
