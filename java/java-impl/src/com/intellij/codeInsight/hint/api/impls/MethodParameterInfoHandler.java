7a1d26e77153548cb7060383f0a1345a2e6f6522
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2018-01-11 17:51:06 +0300

    fix hint popup for enum's 'valueOf' method

--- java/java-impl/src/com/intellij/codeInsight/hint/api/impls/MethodParameterInfoHandler.java
+++ java/java-impl/src/com/intellij/codeInsight/hint/api/impls/MethodParameterInfoHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ * Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package com.intellij.codeInsight.hint.api.impls;
 
@@ -308,7 +308,7 @@ public class MethodParameterInfoHandler implements ParameterInfoHandlerWithTabAc
 
       context.setUIComponentEnabled(i, enabled);
       if (candidates.length > 1 && enabled) {
-        if (chosenMethod == method) {
+        if (method.isEquivalentTo(chosenMethod)) {
           chosenInfo = candidate;
         }
 
