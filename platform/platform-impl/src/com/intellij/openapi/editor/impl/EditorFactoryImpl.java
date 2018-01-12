a804985b67d7cbb9c90fe4c4f86c7a03fa93936b
Egor Ushakov <egor.ushakov@jetbrains.com>
2018-01-12 14:32:03 +0300

    avoid using collection size in toArray calls

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorFactoryImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorFactoryImpl.java
@@ -1,17 +1,5 @@
 /*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package com.intellij.openapi.editor.impl;
 
@@ -45,7 +33,6 @@ import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.project.impl.ProjectLifecycleListener;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.ArrayUtil;
 import com.intellij.util.EventDispatcher;
 import com.intellij.util.SmartList;
 import com.intellij.util.containers.ContainerUtil;
@@ -270,7 +257,7 @@ public class EditorFactoryImpl extends EditorFactory implements ApplicationCompo
   @Override
   @NotNull
   public Editor[] getAllEditors() {
-    return ArrayUtil.stripTrailingNulls(myEditors.toArray(new Editor[myEditors.size()]));
+    return myEditors.toArray(Editor.EMPTY_ARRAY);
   }
 
   @Override
--- platform/platform-impl/src/com/intellij/openapi/progress/impl/ProgressManagerImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/progress/impl/ProgressManagerImpl.java
@@ -1,17 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package com.intellij.openapi.progress.impl;
 
@@ -27,7 +15,6 @@ import com.intellij.openapi.progress.util.ProgressWindow;
 import com.intellij.openapi.progress.util.SmoothProgressAdapter;
 import com.intellij.openapi.wm.WindowManager;
 import com.intellij.ui.SystemNotifications;
-import com.intellij.util.ArrayUtil;
 import com.intellij.util.concurrency.AppExecutorUtil;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.io.storage.HeavyProcessLatch;
@@ -184,7 +171,7 @@ public class ProgressManagerImpl extends CoreProgressManager implements Disposab
   protected CheckCanceledHook createCheckCanceledHook() {
     if (myHooks.isEmpty()) return null;
 
-    CheckCanceledHook[] activeHooks = ArrayUtil.stripTrailingNulls(myHooks.toArray(new CheckCanceledHook[0]));
+    CheckCanceledHook[] activeHooks = myHooks.toArray(new CheckCanceledHook[0]);
     return activeHooks.length == 1 ? activeHooks[0] : indicator -> {
       boolean result = false;
       for (CheckCanceledHook hook : activeHooks) {
