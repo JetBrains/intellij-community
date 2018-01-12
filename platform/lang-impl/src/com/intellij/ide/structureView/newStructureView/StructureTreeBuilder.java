81a1c31ed616fac639dc974850eb8b0735c68eca
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2018-01-12 14:46:45 +0100

    Need to rebuild ATM-based Structure View on structureView.ModelListener.onModelChanged()
    (cherry picked from commit 8d6644415eb9ce6315a60f0f7b1282b6fbb8f182)

--- platform/lang-impl/src/com/intellij/ide/structureView/newStructureView/StructureTreeBuilder.java
+++ platform/lang-impl/src/com/intellij/ide/structureView/newStructureView/StructureTreeBuilder.java
@@ -1,23 +1,10 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 
 package com.intellij.ide.structureView.newStructureView;
 
 import com.intellij.ide.CopyPasteUtil;
-import com.intellij.ide.structureView.ModelListener;
 import com.intellij.ide.structureView.StructureViewModel;
 import com.intellij.ide.structureView.StructureViewTreeElement;
 import com.intellij.ide.util.treeView.AbstractTreeBuilder;
@@ -28,13 +15,11 @@ import com.intellij.ide.util.treeView.smartTree.Group;
 import com.intellij.ide.util.treeView.smartTree.GroupWrapper;
 import com.intellij.ide.util.treeView.smartTree.SmartTreeStructure;
 import com.intellij.ide.util.treeView.smartTree.TreeElement;
-import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.util.StatusBarProgress;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Disposer;
 import com.intellij.psi.*;
 import com.intellij.psi.util.PsiModificationTracker;
 import com.intellij.util.Alarm;
@@ -63,23 +48,10 @@ public class StructureTreeBuilder extends AbstractTreeBuilder {
     myProject = project;
     myStructureModel = structureModel;
 
-    final ModelListener myModelListener = new ModelListener() {
-      @Override
-      public void onModelChanged() {
-        addRootToUpdate();
-      }
-    };
     PsiManager.getInstance(myProject).addPsiTreeChangeListener(new MyPsiTreeChangeListener(), this);
 
     CopyPasteUtil.addDefaultListener(this, this::addSubtreeToUpdateByElement);
     initRootNode();
-    myStructureModel.addModelListener(myModelListener);
-    Disposer.register(this, new Disposable() {
-      @Override
-      public void dispose() {
-        myStructureModel.removeModelListener(myModelListener);
-      }
-    });
 
     setCanYieldUpdate(!ApplicationManager.getApplication().isUnitTestMode());
   }
