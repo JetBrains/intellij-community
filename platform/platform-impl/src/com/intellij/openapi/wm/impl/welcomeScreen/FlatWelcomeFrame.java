c4b925c9a462d001ef991805a1953c4945d1faaf
Denis Fokin <denis.fokin@jetbrains.com>
2018-01-15 15:23:33 +0300

    IDEA-106716 Unity: Idea takes away focus after first window activation
    Suppress welcome screen focus auto request

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/FlatWelcomeFrame.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/FlatWelcomeFrame.java
@@ -108,6 +108,11 @@ public class FlatWelcomeFrame extends JFrame implements IdeFrame, Disposable, Ac
       size.width,
       size.height
     );
+
+    if (Registry.is("suppress.focus.stealing")) {
+      setAutoRequestFocus(false);
+    }
+
     // at this point a window insets may be unavailable,
     // so we need resize window when it is shown
     doWhenFirstShown(this, this::pack);
