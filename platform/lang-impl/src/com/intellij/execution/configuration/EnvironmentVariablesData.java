e71261701614e553c366446bf1d24a1889782b77
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2018-01-15 18:08:28 +0100

    Revert "Write envs ordered by keys"
    This reverts commit d0d43b9

--- platform/lang-impl/src/com/intellij/execution/configuration/EnvironmentVariablesData.java
+++ platform/lang-impl/src/com/intellij/execution/configuration/EnvironmentVariablesData.java
@@ -24,7 +24,6 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 /**
  * Holds environment variables configuration:
@@ -112,8 +111,7 @@ public class EnvironmentVariablesData {
       // com.intellij.execution.configuration.EnvironmentVariablesComponent.writeExternal
       envsElement.setAttribute(PASS_PARENT_ENVS, Boolean.FALSE.toString());
     }
-
-    for (Map.Entry<String, String> entry : new TreeMap<>(myEnvs).entrySet()) {
+    for (Map.Entry<String, String> entry : myEnvs.entrySet()) {
       Element envElement = new Element(ENV);
       envElement.setAttribute(NAME, entry.getKey());
       envElement.setAttribute(VALUE, entry.getValue());
