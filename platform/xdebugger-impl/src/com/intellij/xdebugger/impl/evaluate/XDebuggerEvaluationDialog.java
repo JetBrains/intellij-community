4bd2e6c6f24d4a82090c325111f488e552b701df
Egor Ushakov <egor.ushakov@jetbrains.com>
2018-01-18 12:11:15 +0300

    always select the result node on evaluate - fix for IDEA-CR-28582
    (cherry picked from commit 25eddf9) (IDEA-CR-28582)

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/evaluate/XDebuggerEvaluationDialog.java
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/evaluate/XDebuggerEvaluationDialog.java
@@ -8,7 +8,6 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
-import com.intellij.openapi.util.Condition;
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.wm.IdeFocusManager;
 import com.intellij.openapi.wm.WindowManager;
@@ -152,7 +151,7 @@ public class XDebuggerEvaluationDialog extends DialogWrapper {
     }.registerCustomShortcutSet(new CustomShortcutSet(KeyStroke.getKeyStroke(KeyEvent.VK_R, InputEvent.ALT_DOWN_MASK)), getRootPane(),
                                 myDisposable);
 
-    myTreePanel.getTree().expandNodesOnLoad(getFirstChildCondition());
+    myTreePanel.getTree().expandNodesOnLoad(XDebuggerEvaluationDialog::isFirstChild);
 
     EvaluationMode mode = XDebuggerSettingManagerImpl.getInstanceImpl().getGeneralSettings().getEvaluationDialogMode();
     if (mode == EvaluationMode.CODE_FRAGMENT && !myIsCodeFragmentEvaluationSupported) {
@@ -303,7 +302,7 @@ public class XDebuggerEvaluationDialog extends DialogWrapper {
     final XDebuggerTree tree = myTreePanel.getTree();
     tree.markNodesObsolete();
     tree.setRoot(new EvaluatingExpressionRootNode(this, tree), false);
-    tree.selectNodeOnLoad(getFirstChildCondition());
+    tree.selectNodeOnLoad(XDebuggerEvaluationDialog::isFirstChild);
 
     myResultPanel.invalidate();
 
@@ -320,8 +319,8 @@ public class XDebuggerEvaluationDialog extends DialogWrapper {
     }
   }
 
-  private static Condition<TreeNode> getFirstChildCondition() {
-    return node -> node.getParent() instanceof EvaluatingExpressionRootNode;
+  private static boolean isFirstChild(TreeNode node) {
+    return node.getParent() instanceof EvaluatingExpressionRootNode;
   }
 
   @Override
