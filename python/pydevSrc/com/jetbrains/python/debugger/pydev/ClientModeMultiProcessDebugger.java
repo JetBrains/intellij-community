b597f15d8d47426740829909c8b5c6a0714f5019
Alexander Koshevoy <Alexander.Koshevoy@jetbrains.com>
2018-01-18 17:11:14 +0300

    PY-27942 Debugger connection fixed for local docker-compose interpreter
    The connection strategy of "client mode" debugger reworked to meet local Python Docker Compose interpreter behavior. The `docker-compose` executable might start target Python script after a set of lengthy actions: building images, preparing network configuration, starting other services, etc. (technically we could use `docker-compose events` command to track this moment). Due to this property we need to try connecting repeatedly and effectively for a prolonged time with a reliable timeout.

--- python/pydevSrc/com/jetbrains/python/debugger/pydev/ClientModeMultiProcessDebugger.java
+++ python/pydevSrc/com/jetbrains/python/debugger/pydev/ClientModeMultiProcessDebugger.java
@@ -3,9 +3,10 @@ package com.jetbrains.python.debugger.pydev;
 import com.google.common.collect.Collections2;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.Pair;
+import com.intellij.util.ConcurrencyUtil;
+import com.intellij.util.containers.ContainerUtil;
 import com.intellij.xdebugger.XSourcePosition;
 import com.intellij.xdebugger.breakpoints.SuspendPolicy;
 import com.intellij.xdebugger.frame.XValueChildrenList;
@@ -15,22 +16,78 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * @author traff
+ * @see com.jetbrains.python.debugger.pydev.transport.ClientModeDebuggerTransport
  */
 public class ClientModeMultiProcessDebugger implements ProcessDebugger {
   private static final Logger LOG = Logger.getInstance(ClientModeMultiProcessDebugger.class);
+  public static final long CONNECTION_ATTEMPT_DELAY = TimeUnit.MILLISECONDS.toNanos(500L);
 
   private final IPyDebugProcess myDebugProcess;
   @NotNull private final String myHost;
   private final int myPort;
 
-  @NotNull private final RemoteDebugger myMainDebugger;
-  private final Object myOtherDebuggersObject = new Object();
-  private final List<RemoteDebugger> myOtherDebuggers = Lists.newArrayList();
+  private final Object myDebuggersObject = new Object();
+  /**
+   * Guarded by {@link #myDebuggersObject}.
+   */
+  private final List<RemoteDebugger> myDebuggers = Lists.newArrayList();
 
   private ThreadRegistry myThreadRegistry = new ThreadRegistry();
+
+  private static final int DEBUGGER_CONNECTION_TASKS = 10;
+
+  private final ExecutorService myExecutorService;
+
+  /**
+   * Guards {@link #myDebuggersRequests}.
+   */
+  private final Lock myLock = new ReentrantLock();
+
+  /**
+   * Represents the number of debugger process that awaits the connection from
+   * the IDE:
+   * <ul>
+   * <li>initialized with {@code 1} on {@link #waitForConnect()};</li>
+   * <li>increased on {@link RemoteDebugger#onProcessCreatedEvent()};</li>
+   * <li>decreased on successful {@link RemoteDebugger} connection (when handshake succeeded).</li>
+   * </ul>
+   * <p>
+   * Guarded by {@link #myLock}.
+   */
+  private int myDebuggersRequests = 0;
+
+  /**
+   * Occurs when {@link #myDebuggersRequests} > 0.
+   */
+  private final Condition myNotZeroRequests = myLock.newCondition();
+
+  private final Condition myTimeCondition = myLock.newCondition();
+
+  /**
+   * Guarded by {@link #myLock}.
+   */
+  private long myLastConnectionAttemptTime = System.nanoTime();
+
+  /**
+   * Indicates that this {@link ClientModeMultiProcessDebugger} has connected
+   * at least to one (the main one) Python debugging process.
+   * <p>
+   * <i>As Python debugging process does not start the underlying Python script
+   * before we send him {@link RunCommand} so the first process we connected to
+   * is the main Python script process.</i>
+   */
+  private final AtomicBoolean myConnected = new AtomicBoolean(false);
+
+  private final CountDownLatch myConnectedLatch = new CountDownLatch(1);
+
+  private final CompositeRemoteDebuggerCloseListener myCompositeListener = new CompositeRemoteDebuggerCloseListener();
 
   public ClientModeMultiProcessDebugger(@NotNull final IPyDebugProcess debugProcess,
                                         @NotNull String host, int port) {
@@ -38,7 +95,8 @@ public class ClientModeMultiProcessDebugger implements ProcessDebugger {
     myHost = host;
     myPort = port;
 
-    myMainDebugger = createDebugger();
+    ThreadFactory threadFactory = ConcurrencyUtil.newNamedThreadFactory("Debugger connection threads (" + host + ":" + port + ")");
+    myExecutorService = Executors.newFixedThreadPool(DEBUGGER_CONNECTION_TASKS, threadFactory);
   }
 
   @NotNull
@@ -46,9 +104,115 @@ public class ClientModeMultiProcessDebugger implements ProcessDebugger {
     return new RemoteDebugger(myDebugProcess, myHost, myPort) {
       @Override
       protected void onProcessCreatedEvent() {
-        ApplicationManager.getApplication().executeOnPooledThread(ClientModeMultiProcessDebugger.this::connectToSubprocess);
+        myLock.lock();
+        try {
+          myDebuggersRequests++;
+
+          myNotZeroRequests.signalAll();
+        }
+        finally {
+          myLock.unlock();
+        }
       }
     };
+  }
+
+  private void initializeDebuggerConnectionTasks() {
+    for (int i = 0; i < DEBUGGER_CONNECTION_TASKS; i++) {
+      myExecutorService.submit(() -> {
+        while (true) {
+          try {
+            runDebuggerConnection();
+          }
+          catch (InterruptedException e) {
+            if (LOG.isDebugEnabled()) {
+              LOG.debug(Thread.currentThread() + " interrupted");
+            }
+            // exit while loop
+            return;
+          }
+          catch (Exception e) {
+            LOG.debug(e);
+          }
+        }
+      });
+    }
+  }
+
+  private void runDebuggerConnection() throws Exception {
+    // 1. Wait for new requests
+    myLock.lock();
+    try {
+      do {
+        //noinspection WhileLoopSpinsOnField
+        while (myDebuggersRequests <= 0) {
+          myNotZeroRequests.await();
+        }
+
+        // we have process requests, let's check if we should start
+        do {
+          // check the latest time debugger stepped into connection
+
+          long currentTime = System.nanoTime();
+          long timeToStart = myLastConnectionAttemptTime + CONNECTION_ATTEMPT_DELAY;
+
+          if (timeToStart > currentTime) {
+            long timeToSleep = timeToStart - currentTime;
+
+            myTimeCondition.awaitNanos(timeToSleep);
+          }
+          else {
+            // it's time to run!
+            break;
+          }
+        }
+        while (myDebuggersRequests > 0);
+      }
+      while (myDebuggersRequests <= 0); // let's check requests again
+
+      // finally we made through it
+      myLastConnectionAttemptTime = System.nanoTime();
+    }
+    finally {
+      myLock.unlock();
+    }
+
+    // 2. Create new debugger and try to connect to the process
+    RemoteDebugger debugger = createDebugger();
+    // should either connect or timeout in 5 seconds
+    debugger.waitForConnect();
+
+    // 3. Handle successful debugger connection
+    myLock.lock();
+    try {
+      myDebuggersRequests--;
+
+      // let the next tasks attempt to connect
+      myLastConnectionAttemptTime = System.nanoTime() - CONNECTION_ATTEMPT_DELAY;
+      myTimeCondition.signalAll();
+    }
+    finally {
+      myLock.unlock();
+    }
+
+    // register successfully connected debugger
+    synchronized (myDebuggersObject) {
+      myDebuggers.add(debugger);
+    }
+
+    // notify `waitForConnect()` that we connected
+    if (myConnected.compareAndSet(false, true)) {
+      // add close listeners for the first accepted debugger
+      debugger.addCloseListener(myCompositeListener);
+
+      // must be counted down only the first time
+      myConnectedLatch.countDown();
+    }
+    else {
+      // for consequent processes we should init them by ourselves
+      myDebugProcess.init();
+      debugger.run();
+    }
   }
 
   @Override
@@ -60,54 +224,46 @@ public class ClientModeMultiProcessDebugger implements ProcessDebugger {
   public void waitForConnect() throws Exception {
     Thread.sleep(500L);
 
-    myMainDebugger.waitForConnect();
-  }
+    initializeDebuggerConnectionTasks();
 
-  private void connectToSubprocess() {
+    myLock.lock();
     try {
-      RemoteDebugger debugger = createDebugger();
-      debugger.waitForConnect();
+      if (myDebuggersRequests != 0) {
+        throw new IllegalStateException("waitForConnect() must be called once");
+      }
+      // initial number of requests
+      myDebuggersRequests = 1;
 
-      addDebugger(debugger);
+      // the first request appeared
+      myNotZeroRequests.signalAll();
+    }
+    finally {
+      myLock.unlock();
+    }
 
-      myDebugProcess.init();
-      debugger.run();
-
-      return;
-    }
-    catch (RuntimeException e) {
-      LOG.warn(e);
-    }
-    catch (Exception ignored) {
-    }
-    try {
-      //noinspection BusyWait
-      Thread.sleep(50L);
-    }
-    catch (InterruptedException ignored) {
-    }
-    LOG.debug("Could not connect to subprocess");
+    // waiting for the first connected thread
+    myConnectedLatch.await(60, TimeUnit.SECONDS);
   }
 
   @Override
   public void close() {
+    myExecutorService.shutdownNow();
+
     for (ProcessDebugger d : allDebuggers()) {
       d.close();
     }
   }
 
   private List<RemoteDebugger> allDebuggers() {
-    List<RemoteDebugger> result;
-    synchronized (myOtherDebuggersObject) {
-      result = new ArrayList<>(myOtherDebuggers.size() + 1);
-      result.add(myMainDebugger);
-      result.addAll(myOtherDebuggers);
+    synchronized (myDebuggersObject) {
+      return new ArrayList<>(myDebuggers);
     }
-    return result;
   }
 
   @Override
   public void disconnect() {
+    myExecutorService.shutdownNow();
+
     for (ProcessDebugger d : allDebuggers()) {
       d.disconnect();
     }
@@ -115,7 +271,14 @@ public class ClientModeMultiProcessDebugger implements ProcessDebugger {
 
   @Override
   public String handshake() throws PyDebuggerException {
-    return myMainDebugger.handshake();
+    synchronized (myDebuggersObject) {
+      if (!myDebuggers.isEmpty()) {
+        return myDebuggers.get(0).handshake();
+      }
+      else {
+        throw new IllegalStateException("waitForConnect() must be executed before handshake()");
+      }
+    }
   }
 
   @Override
@@ -169,7 +332,8 @@ public class ClientModeMultiProcessDebugger implements ProcessDebugger {
     else {
       // thread is not found in registry - lets search for it in attached debuggers
 
-      for (ProcessDebugger d : getOtherDebuggers()) {
+      List<RemoteDebugger> debuggers = getDebuggers();
+      for (ProcessDebugger d : debuggers) {
         for (PyThreadInfo thread : d.getThreads()) {
           if (threadId.equals(thread.getId())) {
             return d;
@@ -178,7 +342,7 @@ public class ClientModeMultiProcessDebugger implements ProcessDebugger {
       }
 
       //if not found then return main debugger
-      return myMainDebugger;
+      return debuggers.get(0);
     }
   }
 
@@ -196,7 +360,14 @@ public class ClientModeMultiProcessDebugger implements ProcessDebugger {
 
   @Override
   public String loadSource(String path) {
-    return myMainDebugger.loadSource(path);
+    synchronized (myDebuggersObject) {
+      if (!myDebuggers.isEmpty()) {
+        return myDebuggers.get(0).loadSource(path);
+      }
+      else {
+        throw new IllegalStateException("waitForConnect() must be executed before loadSource()");
+      }
+    }
   }
 
 
@@ -223,11 +394,11 @@ public class ClientModeMultiProcessDebugger implements ProcessDebugger {
 
   @Override
   public Collection<PyThreadInfo> getThreads() {
-    cleanOtherDebuggers();
+    cleanDebuggers();
 
     List<PyThreadInfo> threads = collectAllThreads();
 
-    if (!isOtherDebuggersEmpty()) {
+    if (!isDebuggersEmpty()) {
       //here we add process id to thread name in case there are more then one process
       return Collections.unmodifiableCollection(Collections2.transform(threads, t -> {
         String threadName = ThreadRegistry.threadName(t.getName(), t.getId());
@@ -247,11 +418,9 @@ public class ClientModeMultiProcessDebugger implements ProcessDebugger {
   private List<PyThreadInfo> collectAllThreads() {
     List<PyThreadInfo> result = Lists.newArrayList();
 
-    result.addAll(myMainDebugger.getThreads());
-
     //collect threads and add them to registry to faster access
     //we don't register mainDebugger as it is default if there is no mapping
-    for (RemoteDebugger d : myOtherDebuggers) {
+    for (RemoteDebugger d : myDebuggers) {
       result.addAll(d.getThreads());
       for (PyThreadInfo t : d.getThreads()) {
         myThreadRegistry.register(t.getId(), d);
@@ -261,8 +430,8 @@ public class ClientModeMultiProcessDebugger implements ProcessDebugger {
     return result;
   }
 
-  private void cleanOtherDebuggers() {
-    removeDisconnected(getOtherDebuggers());
+  private void cleanDebuggers() {
+    removeDisconnected(getDebuggers());
   }
 
   private void removeDisconnected(ArrayList<RemoteDebugger> debuggers) {
@@ -280,22 +449,22 @@ public class ClientModeMultiProcessDebugger implements ProcessDebugger {
         }
       }
 
-      synchronized (myOtherDebuggersObject) {
-        myOtherDebuggers.clear();
-        myOtherDebuggers.addAll(newList);
+      synchronized (myDebuggersObject) {
+        myDebuggers.clear();
+        myDebuggers.addAll(newList);
       }
     }
   }
 
-  private ArrayList<RemoteDebugger> getOtherDebuggers() {
-    synchronized (myOtherDebuggersObject) {
-      return Lists.newArrayList(myOtherDebuggers);
+  private ArrayList<RemoteDebugger> getDebuggers() {
+    synchronized (myDebuggersObject) {
+      return Lists.newArrayList(myDebuggers);
     }
   }
 
-  private boolean isOtherDebuggersEmpty() {
-    synchronized (myOtherDebuggersObject) {
-      return myOtherDebuggers.isEmpty();
+  private boolean isDebuggersEmpty() {
+    synchronized (myDebuggersObject) {
+      return myDebuggers.isEmpty();
     }
   }
 
@@ -320,7 +489,14 @@ public class ClientModeMultiProcessDebugger implements ProcessDebugger {
 
   @Override
   public void run() throws PyDebuggerException {
-    myMainDebugger.run();
+    synchronized (myDebuggersObject) {
+      if (!myDebuggers.isEmpty()) {
+        myDebuggers.get(0).run();
+      }
+      else {
+        throw new IllegalStateException("waitForConnect() must be executed before run()");
+      }
+    }
   }
 
   @Override
@@ -382,14 +558,15 @@ public class ClientModeMultiProcessDebugger implements ProcessDebugger {
     }
   }
 
-  private void addDebugger(RemoteDebugger debugger) {
-    synchronized (myOtherDebuggersObject) {
-      myOtherDebuggers.add(debugger);
-    }
-  }
-
+  /**
+   * Stores {@link RemoteDebuggerCloseListener} into the single composite
+   * listener. This composite listener is later added to the first connected
+   * {@link RemoteDebugger}.
+   *
+   * @param listener the listener to be added
+   */
   public void addCloseListener(RemoteDebuggerCloseListener listener) {
-    myMainDebugger.addCloseListener(listener);
+    myCompositeListener.addCloseListener(listener);
   }
 
   @Override
@@ -424,4 +601,26 @@ public class ClientModeMultiProcessDebugger implements ProcessDebugger {
     }
   }
 
+  private static class CompositeRemoteDebuggerCloseListener implements RemoteDebuggerCloseListener {
+    private final List<RemoteDebuggerCloseListener> myListeners = ContainerUtil.createLockFreeCopyOnWriteList();
+
+    public void addCloseListener(RemoteDebuggerCloseListener listener) {
+      myListeners.add(listener);
+    }
+
+    @Override
+    public void closed() {
+      myListeners.forEach(RemoteDebuggerCloseListener::closed);
+    }
+
+    @Override
+    public void communicationError() {
+      myListeners.forEach(RemoteDebuggerCloseListener::communicationError);
+    }
+
+    @Override
+    public void detached() {
+      myListeners.forEach(RemoteDebuggerCloseListener::detached);
+    }
+  }
 }
