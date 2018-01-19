f2a2a4a070222cf479d057859ce64f5510d0fd41
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2018-01-19 15:47:52 +0300

    PY-16412 Simplify generation of variable annotation from a type comment

--- python/src/com/jetbrains/python/codeInsight/intentions/PyConvertTypeCommentToVariableAnnotationIntention.java
+++ python/src/com/jetbrains/python/codeInsight/intentions/PyConvertTypeCommentToVariableAnnotationIntention.java
@@ -16,12 +16,10 @@
 package com.jetbrains.python.codeInsight.intentions;
 
 import com.google.common.collect.ImmutableMap;
-import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Ref;
 import com.intellij.psi.PsiComment;
-import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.PsiTreeUtil;
@@ -37,7 +35,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -49,10 +46,9 @@ public class PyConvertTypeCommentToVariableAnnotationIntention extends PyBaseInt
       final Map<PyTargetExpression, String> map = mapTargetsToAnnotations(typeComment);
       if (map != null) {
         if (typeComment.getParent() instanceof PyAssignmentStatement && map.size() == 1) {
-          final Document document = editor.getDocument();
-          runWithDocumentReleasedAndCommitted(project, document, () -> {
-            final PyTargetExpression target = ContainerUtil.getFirstItem(map.keySet());
-            assert target != null;
+          final PyTargetExpression target = ContainerUtil.getFirstItem(map.keySet());
+          assert target != null;
+          PyUtil.updateDocumentUnblockedAndCommitted(target, document -> {
             document.insertString(target.getTextRange().getEndOffset(), ": " + map.get(target));
           });
         }
@@ -62,16 +58,14 @@ public class PyConvertTypeCommentToVariableAnnotationIntention extends PyBaseInt
 
           final PyElementGenerator generator = PyElementGenerator.getInstance(project);
           final List<Map.Entry<PyTargetExpression, String>> entries = new ArrayList<>(map.entrySet());
-          Collections.reverse(entries);
 
-          PsiElement anchor = statement;
           for (Map.Entry<PyTargetExpression, String> entry : entries) {
             final PyTargetExpression target = entry.getKey();
             final String annotation = entry.getValue();
             final PyTypeDeclarationStatement declaration = generator.createFromText(LanguageLevel.PYTHON36,
                                                                                     PyTypeDeclarationStatement.class,
                                                                                     target.getText() + ": " + annotation);
-            anchor = statement.getParent().addBefore(declaration, anchor);
+            statement.getParent().addBefore(declaration, statement);
           }
         }
 
@@ -112,17 +106,6 @@ public class PyConvertTypeCommentToVariableAnnotationIntention extends PyBaseInt
   private static boolean isSuitableTypeComment(@NotNull PsiComment comment) {
     final String annotation = PyTypingTypeProvider.getTypeCommentValue(comment.getText());
     return annotation != null && mapTargetsToAnnotations(comment) != null;
-  }
-
-  public static void runWithDocumentReleasedAndCommitted(@NotNull Project project, @NotNull Document document, @NotNull Runnable runnable) {
-    final PsiDocumentManager manager = PsiDocumentManager.getInstance(project);
-    manager.doPostponedOperationsAndUnblockDocument(document);
-    try {
-      runnable.run();
-    }
-    finally {
-      manager.commitDocument(document);
-    }
   }
 
   @Nullable
