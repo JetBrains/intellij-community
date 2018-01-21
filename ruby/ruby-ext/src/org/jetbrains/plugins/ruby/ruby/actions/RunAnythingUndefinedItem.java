2eb7cb4811d8c0894df69fd29feec03b873d8691
Dmitry.Krasilschikov <kradima@jetbrains.com>
2018-01-21 21:02:51 +0300

    RUBY-20720 RUBY-20839 properly handle input waiting commands

--- ruby/ruby-ext/src/org/jetbrains/plugins/ruby/ruby/actions/RunAnythingUndefinedItem.java
+++ ruby/ruby-ext/src/org/jetbrains/plugins/ruby/ruby/actions/RunAnythingUndefinedItem.java
@@ -1,18 +1,29 @@
 package org.jetbrains.plugins.ruby.ruby.actions;
 
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.ExecutionManager;
 import com.intellij.execution.Executor;
+import com.intellij.execution.RunContentExecutor;
+import com.intellij.execution.configurations.GeneralCommandLine;
+import com.intellij.execution.executors.DefaultRunExecutor;
+import com.intellij.execution.process.KillableColoredProcessHandler;
+import com.intellij.execution.ui.ConsoleView;
+import com.intellij.execution.ui.ConsoleViewContentType;
+import com.intellij.execution.ui.RunContentDescriptor;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.containers.ContainerUtil;
+import com.intellij.util.execution.ParametersListUtil;
 import icons.RubyIcons;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.plugins.ruby.RBundle;
 import org.jetbrains.plugins.ruby.ruby.RModuleUtil;
-import org.jetbrains.plugins.ruby.ruby.run.ConsoleRunner;
-import org.jetbrains.plugins.ruby.ruby.run.MergingCommandLineArgumentsProvider;
 import org.jetbrains.plugins.ruby.ruby.run.RubyAbstractRunner;
 import org.jetbrains.plugins.ruby.rvm.RVMSupportUtil;
 import org.jetbrains.plugins.ruby.version.management.rbenv.gemsets.RbenvGemsetManager;
@@ -26,9 +37,8 @@ import java.util.Objects;
 
 public class RunAnythingUndefinedItem extends RunAnythingItem {
   static final Icon UNDEFINED_COMMAND_ICON = RubyIcons.RunAnything.Run_anything;
-  private static final String TITLE = "Command execution";
   private static final ArrayList<String> COMMANDS = ContainerUtil.newArrayList(
-    "bundle", "rake", "erb", "gem", "irb", "rdoc", "ruby");
+    "bundle", "rake", "erb", "gem", "irb", "rdoc", "ruby", "rails");
   @Nullable private final Module myModule;
   @NotNull private final String myCommandLine;
   @NotNull private final Project myProject;
@@ -56,14 +66,45 @@ public class RunAnythingUndefinedItem extends RunAnythingItem {
       command = getRbenvAwareCommand(sdk, env);
     }
 
-    final MergingCommandLineArgumentsProvider argumentsProvider =
-      new MergingCommandLineArgumentsProvider(command.split(" "),
-                                              null, null, Boolean.TRUE, env, sdk);
+    GeneralCommandLine commandLine = new GeneralCommandLine(ParametersListUtil.parse(command))
+      .withParentEnvironmentType(GeneralCommandLine.ParentEnvironmentType.CONSOLE)
+      .withEnvironment(env)
+      .withWorkDirectory(RunAnythingItem.getActualWorkDirectory(myProject, workDirectory));
 
-    String workingDir = RunAnythingItem.getActualWorkDirectory(myProject, workDirectory);
-    ConsoleRunner.run(myProject, myModule, null, null, null, null,
-                      ConsoleRunner.ProcessLaunchMode.BACKGROUND_TASK_WITH_PROGRESS, TITLE, workingDir,
-                      argumentsProvider, null, false);
+    runInConsole(commandLine);
+  }
+
+  private void runInConsole(@NotNull GeneralCommandLine commandLine) {
+    try {
+      KillableColoredProcessHandler processHandler = new KillableColoredProcessHandler(commandLine) {
+        @Override
+        protected void notifyProcessTerminated(int exitCode) {
+          RunContentDescriptor contentDescriptor
+            = ExecutionManager.getInstance(myProject).getContentManager()
+                              .findContentDescriptor(DefaultRunExecutor.getRunExecutorInstance(), this);
+
+          if (contentDescriptor != null && contentDescriptor.getExecutionConsole() instanceof ConsoleView) {
+            ((ConsoleView)contentDescriptor.getExecutionConsole())
+              .print(RBundle.message("run.anything.console.process.finished", exitCode), ConsoleViewContentType.SYSTEM_OUTPUT);
+          }
+          super.notifyProcessTerminated(exitCode);
+        }
+      };
+
+      final RunContentExecutor contentExecutor = new RunContentExecutor(myProject, processHandler)
+        .withTitle(RBundle.message("run.anything.console.title"))
+        .withStop(processHandler::destroyProcess, () -> !processHandler.isProcessTerminated())
+        .withActivateToolWindow(true);
+
+      ApplicationManager.getApplication().invokeLater(() -> {
+        if (!myProject.isDisposed()) {
+          contentExecutor.run();
+        }
+      });
+    }
+    catch (ExecutionException e) {
+      Messages.showInfoMessage(myProject, e.getMessage(), RBundle.message("run.anything.console.error.title"));
+    }
   }
 
   private String getRbenvAwareCommand(@NotNull Sdk sdk, @NotNull Map<String, String> env) {
