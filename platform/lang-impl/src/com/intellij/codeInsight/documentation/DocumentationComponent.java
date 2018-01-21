01c876f9fdd3c01be7488796e33ca9070c24d7cf
Dennis Ushakov <dennis.ushakov@gmail.com>
2018-01-21 13:33:38 +0300

    WEB-21129: fix NPE on buttons hover

--- platform/lang-impl/src/com/intellij/codeInsight/documentation/DocumentationComponent.java
+++ platform/lang-impl/src/com/intellij/codeInsight/documentation/DocumentationComponent.java
@@ -312,13 +312,29 @@ public class DocumentationComponent extends JPanel implements Disposable, DataPr
                   }
 
                   @Override
-                  public Shape modelToView(int pos, Shape a, Position.Bias b) {
-                    return null;
+                  public Shape modelToView(int pos, Shape a, Position.Bias b) throws BadLocationException {
+                    int p0 = getStartOffset();
+                    int p1 = getEndOffset();
+                    if ((pos >= p0) && (pos <= p1)) {
+                      Rectangle r = a.getBounds();
+                      if (pos == p1) {
+                        r.x += r.width;
+                      }
+                      r.width = 0;
+                      return r;
+                    }
+                    throw new BadLocationException(pos + " not in range " + p0 + "," + p1, pos);
                   }
 
                   @Override
-                  public int viewToModel(float x, float y, Shape a, Position.Bias[] biasReturn) {
-                    return 0;
+                  public int viewToModel(float x, float y, Shape a, Position.Bias[] bias) {
+                    Rectangle alloc = (Rectangle) a;
+                    if (x < alloc.x + (alloc.width / 2)) {
+                      bias[0] = Position.Bias.Forward;
+                      return getStartOffset();
+                    }
+                    bias[0] = Position.Bias.Backward;
+                    return getEndOffset();
                   }
                 };
               }
