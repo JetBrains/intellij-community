379fa712c59848bd60e205e623f2502a37d81f18
Konstantin Bulenkov <kb@jetbrains.com>
2018-01-22 14:42:07 +0100

    use JBColor for links

--- platform/vcs-log/impl/src/com/intellij/vcs/log/ui/frame/CommitPanel.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/ui/frame/CommitPanel.java
@@ -1,24 +1,10 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.vcs.log.ui.frame;
 
 import com.intellij.openapi.ui.VerticalFlowLayout;
 import com.intellij.ui.BrowserHyperlinkListener;
 import com.intellij.ui.ColorUtil;
-import com.intellij.ui.UI;
+import com.intellij.ui.JBColor;
 import com.intellij.ui.components.JBPanel;
 import com.intellij.ui.components.JBScrollPane;
 import com.intellij.util.Consumer;
@@ -198,7 +184,7 @@ public class CommitPanel extends JBPanel {
       Document document = getDocument();
       if (document instanceof HTMLDocument) {
         StyleSheet styleSheet = ((HTMLDocument)document).getStyleSheet();
-        String linkColor = "#" + ColorUtil.toHex(UI.getColor("link.foreground"));
+        String linkColor = "#" + ColorUtil.toHex(JBColor.link());
         styleSheet.addRule("a { color: " + linkColor + "; text-decoration: none;}");
       }
     }
