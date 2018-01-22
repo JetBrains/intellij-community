d98732b64a639dcca29761e9d4c76697827cf3dc
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2018-01-22 15:06:55 +0300

    PY-16412 Insert templates in the editor for generated variable annotations

--- python/src/com/jetbrains/python/codeInsight/intentions/PyTypeHintGenerationUtil.java
+++ python/src/com/jetbrains/python/codeInsight/intentions/PyTypeHintGenerationUtil.java
@@ -1,8 +1,18 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.python.codeInsight.intentions;
 
+import com.intellij.codeInsight.CodeInsightUtilCore;
+import com.intellij.codeInsight.template.TemplateBuilder;
+import com.intellij.codeInsight.template.TemplateBuilderFactory;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.fileEditor.OpenFileDescriptor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiComment;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.SmartPointerManager;
+import com.intellij.psi.SmartPsiElementPointer;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.jetbrains.python.psi.*;
 import com.jetbrains.python.psi.impl.PyPsiUtils;
@@ -16,24 +26,48 @@ import static com.jetbrains.python.psi.PyUtil.as;
 public class PyTypeHintGenerationUtil {
   private PyTypeHintGenerationUtil() {}
 
-  public static void insertVariableAnnotation(@NotNull PyTargetExpression target, @NotNull String annotation) {
+  public static void insertVariableAnnotation(@NotNull PyTargetExpression target, @NotNull String annotation, boolean startTemplate) {
     final LanguageLevel langLevel = LanguageLevel.forElement(target);
     if (langLevel.isOlderThan(LanguageLevel.PYTHON36)) {
       throw new IllegalArgumentException("Target '" + target.getText() + "' doesn't belong to Python 3.6+ project: " + langLevel);
     }
 
+    final Project project = target.getProject();
+    final PyAnnotationOwner createdAnnotationOwner;
     if (canUseInlineAnnotation(target)) {
+      final SmartPointerManager manager = SmartPointerManager.getInstance(project);
+      final PyAssignmentStatement assignment = (PyAssignmentStatement)target.getParent();
+      final SmartPsiElementPointer<PyAssignmentStatement> pointer = manager.createSmartPsiElementPointer(assignment);
       PyUtil.updateDocumentUnblockedAndCommitted(target, document -> {
         document.insertString(target.getTextRange().getEndOffset(), ": " + annotation);
       });
+      createdAnnotationOwner = pointer.getElement();
     }
     else {
-      final PyElementGenerator generator = PyElementGenerator.getInstance(target.getProject());
+      final PyElementGenerator generator = PyElementGenerator.getInstance(project);
       final String declarationText = target.getText() + ": " + annotation;
       final PyTypeDeclarationStatement declaration = generator.createFromText(langLevel, PyTypeDeclarationStatement.class, declarationText);
       final PyStatement statement = PsiTreeUtil.getParentOfType(target, PyStatement.class);
       assert statement != null;
-      statement.getParent().addBefore(declaration, statement);
+      final PyAnnotationOwner inserted = (PyAnnotationOwner)statement.getParent().addBefore(declaration, statement);
+      createdAnnotationOwner = CodeInsightUtilCore.forcePsiPostprocessAndRestoreElement(inserted);
+    }
+
+    if (startTemplate && createdAnnotationOwner != null) {
+      assert createdAnnotationOwner.getAnnotationValue() != null;
+
+      final int initialCaretOffset = createdAnnotationOwner.getTextRange().getStartOffset();
+      final VirtualFile updatedVirtualFile = createdAnnotationOwner.getContainingFile().getVirtualFile();
+      final OpenFileDescriptor descriptor = new OpenFileDescriptor(project, updatedVirtualFile, initialCaretOffset);
+      final Editor editor = FileEditorManager.getInstance(project).openTextEditor(descriptor, true);
+
+      if (editor != null) {
+        editor.getCaretModel().moveToOffset(initialCaretOffset);
+        final TemplateBuilder templateBuilder = TemplateBuilderFactory.getInstance().createTemplateBuilder(createdAnnotationOwner);
+        //noinspection ConstantConditions
+        templateBuilder.replaceElement(createdAnnotationOwner.getAnnotation().getValue(), createdAnnotationOwner.getAnnotationValue());
+        templateBuilder.run(editor, true);
+      }
     }
   }
 
