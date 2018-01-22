79e2a9707fe6282d914afdedd348093b4304834e
Konstantin Bulenkov <kb@jetbrains.com>
2018-01-22 14:19:05 +0100

    +JBColor.link() for links

--- platform/util/src/com/intellij/ui/JBColor.java
+++ platform/util/src/com/intellij/ui/JBColor.java
@@ -1,24 +1,11 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ui;
 
 import com.intellij.util.NotNullProducer;
 import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.color.ColorSpace;
 import java.awt.geom.AffineTransform;
@@ -52,6 +39,10 @@ public class JBColor extends Color {
     super(0);
     darkColor = null;
     func = function;
+  }
+
+  public static Color link() {
+    return UIManager.getColor("link.foreground");
   }
 
   public static void setDark(boolean dark) {
