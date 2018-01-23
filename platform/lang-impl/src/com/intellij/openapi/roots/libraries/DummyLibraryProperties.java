55057a2e9b36e1a86818aee72ea11676eaab4f59
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-01-23 08:54:03 +0100

    DummyLibraryProperties — add nullable annotation
    (cherry picked from commit 83bd690)

--- platform/lang-impl/src/com/intellij/openapi/roots/libraries/DummyLibraryProperties.java
+++ platform/lang-impl/src/com/intellij/openapi/roots/libraries/DummyLibraryProperties.java
@@ -1,7 +1,7 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.roots.libraries;
 
-import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
 * @author nik
@@ -15,7 +15,7 @@ public class DummyLibraryProperties extends LibraryProperties<Object> {
   }
 
   @Override
-  public void loadState(@NotNull Object state) {
+  public void loadState(@Nullable Object state) {
   }
 
   @Override
