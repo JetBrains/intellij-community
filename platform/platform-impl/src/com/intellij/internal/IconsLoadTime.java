ac272ef6081a37d8585d12c2b0fbb8d2479bdcbe
Anton Tarasov <anton.tarasov@jetbrains.com>
2018-01-23 19:15:12 +0300

    [followup] Log PNG/SVG icons load time statistics
    Now logs both PNG & SVG stats in one run.

--- platform/platform-impl/src/com/intellij/internal/IconsLoadTime.java
+++ platform/platform-impl/src/com/intellij/internal/IconsLoadTime.java
@@ -9,9 +9,9 @@ import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
-import com.intellij.openapi.util.registry.Registry;
 import com.intellij.util.FieldAccessor;
 import com.intellij.util.ImageLoader;
+import com.intellij.util.ImageLoader.ImageDesc.Type;
 import com.intellij.util.ImageLoader.LoadFunction;
 import org.jetbrains.annotations.NotNull;
 
@@ -35,10 +35,15 @@ public class IconsLoadTime extends DumbAwareAction {
   private static final int STATS_LIMIT = 10000;
   private static final int FIXED_SCOPE = 100; // log stats for a first fixed number of icons
 
-  private static final List<Integer> stats = IS_INTERNAL_MODE ? Collections.synchronizedList(new LinkedList<>()) : null; // load time per icon
+  // load time per icon
+  private static final List<Integer> statsSVG = IS_INTERNAL_MODE ? Collections.synchronizedList(new LinkedList<>()) : null;
+  private static final List<Integer> statsPNG = IS_INTERNAL_MODE ? Collections.synchronizedList(new LinkedList<>()) : null;
 
   static {
-    if (IS_INTERNAL_MODE) new FieldAccessor<>(ImageLoader.class, "measureLoad").set(null, (LoadFunction)func -> measure(Objects.requireNonNull(func)));
+    if (IS_INTERNAL_MODE) {
+      new FieldAccessor<>(ImageLoader.class, "measureLoad").set(null,
+        (LoadFunction)(func, type) -> measure(Objects.requireNonNull(func), Objects.requireNonNull(type)));
+    }
   }
 
   @Override
@@ -46,7 +51,13 @@ public class IconsLoadTime extends DumbAwareAction {
     log(false);
   }
 
-  private static void log(boolean measureStartupLoad) {
+  public static void log(boolean measureStartupLoad) {
+    log(measureStartupLoad, Type.PNG);
+    log(measureStartupLoad, Type.SVG);
+  }
+
+  private static void log(boolean measureStartupLoad, Type type) {
+    List<Integer> stats = getStats(type);
     if (stats == null || stats.isEmpty()) return;
 
     int size = stats.size();
@@ -56,31 +67,37 @@ public class IconsLoadTime extends DumbAwareAction {
 
     Function<Long, String> ms = (nano) -> String.format("%.02fms", nano / 1000000f);
 
-    LOG.info((Registry.is("ide.svg.icon") ? "SVG" : "PNG") +
-             " load time: " +
+    LOG.info(type + " load time: " +
              (measureStartupLoad ? "ide_startup=" : "total=") + ms.apply(sum) +
              ", average=" + ms.apply(average) +
              ", median=" + ms.apply(median) +
              "; number of icons: " + size);
   }
 
-  private static Image measure(LoadFunction func) throws IOException {
+  private static Image measure(LoadFunction func, Type type) throws IOException {
+    List<Integer> stats = getStats(type);
     boolean measure = stats.size() < STATS_LIMIT;
     long t = measure ? System.nanoTime() : 0;
 
-    Image img = func.load(null);
+    Image img = func.load(null, null);
 
     if (measure) {
       stats.add((int)(System.nanoTime() - t));
-      if (stats.size() == FIXED_SCOPE) log(false);
+      if (stats.size() == FIXED_SCOPE) log(false, type);
     }
     return img;
+  }
+
+  private static List<Integer> getStats(Type type) {
+    return type == Type.SVG ? statsSVG : statsPNG;
   }
 
   public static class StartupLoadTime implements StartupActivity, DumbAware {
     @Override
     public void runActivity(@NotNull Project project) {
-      if (IS_INTERNAL_MODE) log(true);
+      if (IS_INTERNAL_MODE) {
+        log(true);
+      }
     }
   }
 }
