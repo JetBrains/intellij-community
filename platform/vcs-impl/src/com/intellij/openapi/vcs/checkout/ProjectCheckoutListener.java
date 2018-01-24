6ee3463534a91f1766744f1e5d8ab7d0dc3df174
Anton Makeev <Anton.Makeev@jetbrains.com>
2018-01-24 18:02:14 +0100

    CPP-11585 Add auto-detect CmakeLists.txt and suggest to Load it

--- platform/vcs-impl/src/com/intellij/openapi/vcs/checkout/ProjectCheckoutListener.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/checkout/ProjectCheckoutListener.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vcs.checkout;
 
 import com.intellij.ide.highlighter.ProjectFileType;
@@ -22,10 +8,8 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vcs.VcsBundle;
-import org.apache.oro.io.GlobFilenameFilter;
 
 import java.io.File;
-import java.io.FilenameFilter;
 
 /**
  * @author yole
@@ -35,7 +19,7 @@ public class ProjectCheckoutListener implements CheckoutListener {
 
   @Override
   public boolean processCheckedOutDirectory(Project project, File directory) {
-    File[] files = directory.listFiles((FilenameFilter) new GlobFilenameFilter("*" + ProjectFileType.DOT_DEFAULT_EXTENSION));
+    File[] files = directory.listFiles((dir, name) -> dir.isFile() && name.endsWith(ProjectFileType.DOT_DEFAULT_EXTENSION));
     if (files != null && files.length > 0) {
       String message = VcsBundle.message("checkout.open.project.prompt", getProductNameWithArticle(), files[0].getPath());
       if (Messages.showYesNoDialog(project, message, VcsBundle.message("checkout.title"), Messages.getQuestionIcon()) == Messages.YES) {
