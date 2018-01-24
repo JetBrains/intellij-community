beaeda477feba98c8ea7ea497dd8d978c33f209c
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-01-24 15:24:59 +0100

    convert AsyncPromiseTest to kotlin

--- platform/platform-tests/testSrc/com/intellij/util/concurrency/AsyncPromiseTest.java
+++ platform/platform-tests/testSrc/com/intellij/util/concurrency/AsyncPromiseTest.java
@@ -1,177 +1,182 @@
 // Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.util.concurrency;
+package com.intellij.util.concurrency
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.concurrency.AsyncPromise;
-import org.jetbrains.concurrency.Promise;
-import org.junit.Test;
+import com.intellij.util.TimeoutUtil.sleep
+import org.jetbrains.concurrency.AsyncPromise
+import org.junit.Test
+import java.awt.EventQueue.invokeLater
+import java.awt.EventQueue.isDispatchThread
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
+private const val PRINT = false
 
-import static com.intellij.util.TimeoutUtil.sleep;
-import static java.awt.EventQueue.invokeLater;
-import static java.awt.EventQueue.isDispatchThread;
+private class CheckedException : Exception()
 
-public class AsyncPromiseTest {
-  private static final boolean PRINT = false;
+private fun isCheckedException(exception: Exception): Boolean {
+  return exception is CheckedException
+}
 
-  enum State {RESOLVE, REJECT, ERROR}
+private fun isMessageError(exception: Exception): Boolean {
+  return exception.javaClass.name == "org.jetbrains.concurrency.MessageError"
+}
 
-  enum When {NOW, AFTER, BEFORE}
+private fun log(message: String) {
+  if (PRINT) println(message)
+}
 
-  private static final class CheckedException extends Exception {
-  }
-
-  private static boolean isCheckedException(@NotNull Exception exception) {
-    return exception instanceof CheckedException;
-  }
-
-  private static boolean isMessageError(@NotNull Exception exception) {
-    return exception.getClass().getName().equals("org.jetbrains.concurrency.MessageError");
-  }
-
-  private static void log(@NotNull String message) {
-    if (PRINT) System.out.println(message);
-  }
-
-  private static AsyncPromise<String> promise(@NotNull State state, @NotNull When when) {
-    assert !isDispatchThread();
-    CountDownLatch latch = new CountDownLatch(1);
-    AsyncPromise<String> promise = new AsyncPromise<>();
-    Runnable task = () -> {
-      try {
-        sleep(10);
-        switch (state) {
-          case RESOLVE:
-            log("resolve promise");
-            promise.setResult("resolved");
-            break;
-          case REJECT:
-            log("reject promise");
-            promise.setError("rejected");
-            break;
-          case ERROR:
-            log("notify promise about error to preserve a cause");
-            promise.setError(new CheckedException());
-            break;
+private fun promise(state: AsyncPromiseTest.State, `when`: AsyncPromiseTest.When): AsyncPromise<String> {
+  assert(!isDispatchThread())
+  val latch = CountDownLatch(1)
+  val promise = AsyncPromise<String>()
+  val task = {
+    try {
+      sleep(10)
+      when (state) {
+        AsyncPromiseTest.State.RESOLVE -> {
+          log("resolve promise")
+          promise.setResult("resolved")
         }
-        latch.countDown();
+        AsyncPromiseTest.State.REJECT -> {
+          log("reject promise")
+          promise.setError("rejected")
+        }
+        AsyncPromiseTest.State.ERROR -> {
+          log("notify promise about error to preserve a cause")
+          promise.setError(CheckedException())
+        }
       }
-      catch (Throwable throwable) {
-        log("unexpected error that breaks current task");
-        throwable.printStackTrace();
-      }
-    };
-    switch (when) {
-      case NOW:
-        log("resolve promise immediately");
-        task.run();
-        break;
-      case AFTER:
-        log("resolve promise on another thread");
-        invokeLater(task);
-        break;
-      case BEFORE:
-        log("resolve promise on another thread before handler is set");
-        invokeLater(task);
-        sleep(50);
-        break;
+      latch.countDown()
     }
-    log("add processing handlers");
-    promise.processed(value -> log("promise is processed"));
+    catch (throwable: Throwable) {
+      log("unexpected error that breaks current task")
+      throwable.printStackTrace()
+    }
+  }
+
+  when (`when`) {
+    AsyncPromiseTest.When.NOW -> {
+      log("resolve promise immediately")
+      task()
+    }
+    AsyncPromiseTest.When.AFTER -> {
+      log("resolve promise on another thread")
+      invokeLater(task)
+    }
+    AsyncPromiseTest.When.BEFORE -> {
+      log("resolve promise on another thread before handler is set")
+      invokeLater(task)
+      sleep(50)
+    }
+  }
+  log("add processing handlers")
+  promise.processed { log("promise is processed") }
+  try {
+    log("wait for task completion")
+    latch.await(100, TimeUnit.MILLISECONDS)
+    if (0L == latch.count) return promise
+    throw AssertionError("task is not completed")
+  }
+  catch (exception: InterruptedException) {
+    throw AssertionError("task is interrupted", exception)
+  }
+}
+
+class AsyncPromiseTest {
+  internal enum class State {
+    RESOLVE, REJECT, ERROR
+  }
+
+  internal enum class When {
+    NOW, AFTER, BEFORE
+  }
+
+  @Test
+  fun testResolveNow() {
+    val promise = promise(State.RESOLVE, When.NOW)
+    assert("resolved" == promise.blockingGet(100))
+  }
+
+  @Test
+  fun testResolveAfterHandlerSet() {
+    val promise = promise(State.RESOLVE, When.AFTER)
+    assert("resolved" == promise.blockingGet(100))
+  }
+
+  @Test
+  fun testResolveBeforeHandlerSet() {
+    val promise = promise(State.RESOLVE, When.BEFORE)
+    assert("resolved" == promise.blockingGet(100))
+  }
+
+  @Test
+  fun testRejectNow() {
+    val promise = promise(State.REJECT, When.NOW)
     try {
-      log("wait for task completion");
-      latch.await(100, TimeUnit.MILLISECONDS);
-      if (0 == latch.getCount()) return promise;
-      throw new AssertionError("task is not completed");
+      assert(null == promise.blockingGet(100))
     }
-    catch (InterruptedException exception) {
-      throw new AssertionError("task is interrupted", exception);
+    catch (exception: Exception) {
+      if (!isMessageError(exception)) throw exception
     }
   }
 
   @Test
-  public void testResolveNow() {
-    Promise<String> promise = promise(State.RESOLVE, When.NOW);
-    assert "resolved".equals(promise.blockingGet(100));
-  }
-
-  @Test
-  public void testResolveAfterHandlerSet() {
-    Promise<String> promise = promise(State.RESOLVE, When.AFTER);
-    assert "resolved".equals(promise.blockingGet(100));
-  }
-
-  @Test
-  public void testResolveBeforeHandlerSet() {
-    Promise<String> promise = promise(State.RESOLVE, When.BEFORE);
-    assert "resolved".equals(promise.blockingGet(100));
-  }
-
-  @Test
-  public void testRejectNow() {
-    Promise<String> promise = promise(State.REJECT, When.NOW);
+  fun testRejectAfterHandlerSet() {
+    val promise = promise(State.REJECT, When.AFTER)
     try {
-      assert null == promise.blockingGet(100);
+      assert(null == promise.blockingGet(100))
     }
-    catch (Exception exception) {
-      if (!isMessageError(exception)) throw exception;
+    catch (exception: Exception) {
+      if (!isMessageError(exception)) throw exception
     }
+
   }
 
   @Test
-  public void testRejectAfterHandlerSet() {
-    Promise<String> promise = promise(State.REJECT, When.AFTER);
+  fun testRejectBeforeHandlerSet() {
+    val promise = promise(State.REJECT, When.BEFORE)
     try {
-      assert null == promise.blockingGet(100);
+      assert(null == promise.blockingGet(100))
     }
-    catch (Exception exception) {
-      if (!isMessageError(exception)) throw exception;
+    catch (exception: Exception) {
+      if (!isMessageError(exception)) throw exception
     }
+
   }
 
   @Test
-  public void testRejectBeforeHandlerSet() {
-    Promise<String> promise = promise(State.REJECT, When.BEFORE);
+  fun testErrorNow() {
+    val promise = promise(State.ERROR, When.NOW)
     try {
-      assert null == promise.blockingGet(100);
+      assert(null == promise.blockingGet(100))
     }
-    catch (Exception exception) {
-      if (!isMessageError(exception)) throw exception;
+    catch (exception: Exception) {
+      if (!isCheckedException(exception)) throw exception
     }
+
   }
 
   @Test
-  public void testErrorNow() {
-    Promise<String> promise = promise(State.ERROR, When.NOW);
+  fun testErrorAfterHandlerSet() {
+    val promise = promise(State.ERROR, When.AFTER)
     try {
-      assert null == promise.blockingGet(100);
+      assert(null == promise.blockingGet(100))
     }
-    catch (Exception exception) {
-      if (!isCheckedException(exception)) throw exception;
+    catch (exception: Exception) {
+      if (!isCheckedException(exception)) throw exception
     }
+
   }
 
   @Test
-  public void testErrorAfterHandlerSet() {
-    Promise<String> promise = promise(State.ERROR, When.AFTER);
+  fun testErrorBeforeHandlerSet() {
+    val promise = promise(State.ERROR, When.BEFORE)
     try {
-      assert null == promise.blockingGet(100);
+      assert(null == promise.blockingGet(100))
     }
-    catch (Exception exception) {
-      if (!isCheckedException(exception)) throw exception;
+    catch (exception: Exception) {
+      if (!isCheckedException(exception)) throw exception
     }
-  }
 
-  @Test
-  public void testErrorBeforeHandlerSet() {
-    Promise<String> promise = promise(State.ERROR, When.BEFORE);
-    try {
-      assert null == promise.blockingGet(100);
-    }
-    catch (Exception exception) {
-      if (!isCheckedException(exception)) throw exception;
-    }
   }
 }
