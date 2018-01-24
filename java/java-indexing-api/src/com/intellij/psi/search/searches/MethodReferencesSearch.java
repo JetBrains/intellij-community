974f236891626352e38930c65a1441c651c032d7
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2018-01-24 14:31:05 +0300

    revert add QueryParameters#isDirectSearch, add flag to ref/method search

--- java/java-indexing-api/src/com/intellij/psi/search/searches/MethodReferencesSearch.java
+++ java/java-indexing-api/src/com/intellij/psi/search/searches/MethodReferencesSearch.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.search.searches;
 
 import com.intellij.openapi.application.DumbAwareSearchParameters;
@@ -104,6 +90,13 @@ public class MethodReferencesSearch extends ExtensibleQueryFactory<PsiReference,
         myEffectiveScope = scope = myScope.intersectWith(PsiSearchHelper.SERVICE.getInstance(myMethod.getProject()).getUseScope(myMethod));
       }
       return scope;
+    }
+
+    /**
+     * @see ReferencesSearch.SearchParameters#isDirectSearch()
+     */
+    public boolean isDirectSearch() {
+      return false;
     }
   }
 
--- platform/indexing-api/src/com/intellij/psi/search/searches/ReferencesSearch.java
+++ platform/indexing-api/src/com/intellij/psi/search/searches/ReferencesSearch.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.search.searches;
 
 import com.intellij.openapi.application.DumbAwareSearchParameters;
@@ -117,6 +103,30 @@ public class ReferencesSearch extends ExtensibleQueryFactory<PsiReference, Refer
         myEffectiveScope = scope = myScope.intersectWith(useScope);
       }
       return scope;
+    }
+
+
+    /**
+     * Given:
+     * <pre>
+     *   class JavaClass {
+     *     int getFoo() { return 42; }
+     *   }
+     *
+     *   class GroovyClass {
+     *     def usageInGroovy(JavaClass jc) {
+     *       jc.foo       // resolves to special element which navigates to JavaClass.getFoo()
+     *       jc.getFoo()  // resolves to JavaClass.getFoo() directly
+     *     }
+     *   }
+     * </pre>
+     * If this method returns {@code true} then only {@code jc.getFoo()} will be found.<br/>
+     * All references were found since ages, the default value is {@code false} to preserve this behaviour.
+     *
+     * @return {@code true} when only direct hits are expected
+     */
+    public boolean isDirectSearch() {
+      return false;
     }
   }
 
