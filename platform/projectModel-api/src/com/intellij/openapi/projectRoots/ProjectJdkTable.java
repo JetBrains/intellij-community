527bc09337f12ab71736371edd4b0d7eeaead1e0
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2018-01-26 19:15:33 +0300

    [platform] correct comparison of SDK version strings (IDEA-185644)

--- platform/projectModel-api/src/com/intellij/openapi/projectRoots/ProjectJdkTable.java
+++ platform/projectModel-api/src/com/intellij/openapi/projectRoots/ProjectJdkTable.java
@@ -1,27 +1,13 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.projectRoots;
 
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.Result;
 import com.intellij.openapi.application.WriteAction;
 import com.intellij.openapi.components.ServiceManager;
-import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.util.Condition;
 import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.util.messages.Topic;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -56,12 +42,10 @@ public abstract class ProjectJdkTable {
   public Sdk findMostRecentSdk(@NotNull Condition<Sdk> condition) {
     Sdk found = null;
     for (Sdk each : getAllJdks()) {
-      if (!condition.value(each)) continue;
-      if (found == null) {
+      if (condition.value(each) &&
+          (found == null || StringUtil.compareVersionNumbers(each.getVersionString(), found.getVersionString()) > 0)) {
         found = each;
-        continue;
       }
-      if (Comparing.compare(each.getVersionString(), found.getVersionString()) > 0) found = each;
     }
     return found;
   }
@@ -111,7 +95,7 @@ public abstract class ProjectJdkTable {
   @NotNull
   public abstract SdkTypeId getSdkTypeByName(@NotNull String name);
 
-  @NotNull 
+  @NotNull
   public abstract Sdk createSdk(@NotNull String name, @NotNull SdkTypeId sdkType);
 
   public static final Topic<Listener> JDK_TABLE_TOPIC = Topic.create("Project JDK table", Listener.class);
