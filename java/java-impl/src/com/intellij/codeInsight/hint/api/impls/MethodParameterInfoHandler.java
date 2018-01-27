fb752a206f4910e04e80a7fe4bf843c43dbe6e32
peter <peter@jetbrains.com>
2018-01-27 16:08:54 +0100

    IDEA-184991 Parameter info popup doesn't highlight current item

--- java/java-impl/src/com/intellij/codeInsight/hint/api/impls/MethodParameterInfoHandler.java
+++ java/java-impl/src/com/intellij/codeInsight/hint/api/impls/MethodParameterInfoHandler.java
@@ -324,6 +324,9 @@ public class MethodParameterInfoHandler implements ParameterInfoHandlerWithTabAc
       }
 
       context.setUIComponentEnabled(i, enabled);
+      if (!enabled && context.getHighlightedParameter() == candidate) {
+        context.setHighlightedParameter(null);
+      }
       if (candidates.length > 1 && enabled) {
         if (PsiManager.getInstance(context.getProject()).areElementsEquivalent(chosenMethod, method)) {
           chosenInfo = candidate;
