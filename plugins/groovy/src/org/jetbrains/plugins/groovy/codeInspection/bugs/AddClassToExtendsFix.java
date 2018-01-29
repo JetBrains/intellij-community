1df8696a6c49e67fda1e464e72a3586a9afa6287
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2018-01-29 15:28:09 +0300

    [groovy] clean up unused operations

--- plugins/groovy/src/org/jetbrains/plugins/groovy/codeInspection/bugs/AddClassToExtendsFix.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/codeInspection/bugs/AddClassToExtendsFix.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.codeInspection.bugs;
 
 import com.intellij.codeInsight.generation.OverrideImplementUtil;
@@ -48,21 +34,7 @@ public class AddClassToExtendsFix extends GroovyFix {
 
     final PsiClass comparable =
       JavaPsiFacade.getInstance(project).findClass(CommonClassNames.JAVA_LANG_COMPARABLE, myPsiClass.getResolveScope());
-    PsiSubstitutor substitutor = PsiSubstitutor.EMPTY;
-    boolean addTypeParam = false;
-    if (comparable != null) {
-      final PsiTypeParameter[] typeParameters = comparable.getTypeParameters();
-      if (typeParameters.length == 1) {
-        final PsiElementFactory elementFactory = JavaPsiFacade.getElementFactory(project);
-        final PsiTypeParameter[] classParams = myPsiClass.getTypeParameters();
-        PsiSubstitutor innerSubstitutor = PsiSubstitutor.EMPTY;
-        for (PsiTypeParameter classParam : classParams) {
-          innerSubstitutor = innerSubstitutor.put(classParam, elementFactory.createType(classParam));
-        }
-        substitutor = substitutor.put(typeParameters[0], elementFactory.createType(myPsiClass, innerSubstitutor));
-        addTypeParam = true;
-      }
-    }
+    final boolean addTypeParam = comparable != null && comparable.getTypeParameters().length == 1;
 
     if (!InheritanceUtil.isInheritor(myPsiClass, CommonClassNames.JAVA_LANG_COMPARABLE)) {
       if (myPsiClass.isInterface()) {
