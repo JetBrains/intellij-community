b3d645876338e89b99a0abebec29e89a29b5d72e
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2018-01-29 15:31:27 +0300

    [groovy] fix psi class leak in AddClassToExtendsFix

--- plugins/groovy/src/org/jetbrains/plugins/groovy/codeInspection/bugs/AddClassToExtendsFix.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/codeInspection/bugs/AddClassToExtendsFix.java
@@ -17,62 +17,66 @@ import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.GrReferenceL
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.GrTypeDefinition;
 import org.jetbrains.plugins.groovy.lang.psi.api.types.GrCodeReferenceElement;
 
+import static com.intellij.psi.util.PointersKt.createSmartPointer;
+
 public class AddClassToExtendsFix extends GroovyFix {
-  private final GrTypeDefinition myPsiClass;
+  private final SmartPsiElementPointer<GrTypeDefinition> myPsiClassPointer;
   private final String myInterfaceName;
 
-  public AddClassToExtendsFix(GrTypeDefinition psiClass, String interfaceName) {
-    myPsiClass = psiClass;
+  public AddClassToExtendsFix(@NotNull GrTypeDefinition psiClass, @NotNull String interfaceName) {
+    myPsiClassPointer = createSmartPointer(psiClass);
     myInterfaceName = interfaceName;
   }
 
   @Override
   protected void doFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) throws IncorrectOperationException {
+    final GrTypeDefinition psiClass = myPsiClassPointer.getElement();
+    if (psiClass == null) return;
 
     GrReferenceList list;
     final GroovyPsiElementFactory factory = GroovyPsiElementFactory.getInstance(project);
 
     final PsiClass comparable =
-      JavaPsiFacade.getInstance(project).findClass(CommonClassNames.JAVA_LANG_COMPARABLE, myPsiClass.getResolveScope());
+      JavaPsiFacade.getInstance(project).findClass(CommonClassNames.JAVA_LANG_COMPARABLE, psiClass.getResolveScope());
     final boolean addTypeParam = comparable != null && comparable.getTypeParameters().length == 1;
 
-    if (!InheritanceUtil.isInheritor(myPsiClass, CommonClassNames.JAVA_LANG_COMPARABLE)) {
-      if (myPsiClass.isInterface()) {
-        list = myPsiClass.getExtendsClause();
+    if (!InheritanceUtil.isInheritor(psiClass, CommonClassNames.JAVA_LANG_COMPARABLE)) {
+      if (psiClass.isInterface()) {
+        list = psiClass.getExtendsClause();
         if (list == null) {
           list = factory.createExtendsClause();
 
-          PsiElement anchor = myPsiClass.getImplementsClause();
+          PsiElement anchor = psiClass.getImplementsClause();
           if (anchor == null) {
-            anchor = myPsiClass.getBody();
+            anchor = psiClass.getBody();
           }
           if (anchor == null) return;
-          list = (GrReferenceList)myPsiClass.addBefore(list, anchor);
-          myPsiClass.getNode().addLeaf(TokenType.WHITE_SPACE, " ", anchor.getNode());
-          myPsiClass.getNode().addLeaf(TokenType.WHITE_SPACE, " ", list.getNode());
+          list = (GrReferenceList)psiClass.addBefore(list, anchor);
+          psiClass.getNode().addLeaf(TokenType.WHITE_SPACE, " ", anchor.getNode());
+          psiClass.getNode().addLeaf(TokenType.WHITE_SPACE, " ", list.getNode());
         }
       }
       else {
-        list = myPsiClass.getImplementsClause();
+        list = psiClass.getImplementsClause();
         if (list == null) {
           list = factory.createImplementsClause();
-          PsiElement anchor = myPsiClass.getBody();
+          PsiElement anchor = psiClass.getBody();
           if (anchor == null) return;
-          list = (GrReferenceList)myPsiClass.addBefore(list, anchor);
-          myPsiClass.getNode().addLeaf(TokenType.WHITE_SPACE, " ", list.getNode());
-          myPsiClass.getNode().addLeaf(TokenType.WHITE_SPACE, " ", anchor.getNode());
+          list = (GrReferenceList)psiClass.addBefore(list, anchor);
+          psiClass.getNode().addLeaf(TokenType.WHITE_SPACE, " ", list.getNode());
+          psiClass.getNode().addLeaf(TokenType.WHITE_SPACE, " ", anchor.getNode());
         }
       }
 
 
       final GrCodeReferenceElement _ref =
-        factory.createReferenceElementFromText(myInterfaceName + (addTypeParam ? "<" + AddMethodFix.generateTypeText(myPsiClass) + ">" : ""));
+        factory.createReferenceElementFromText(myInterfaceName + (addTypeParam ? "<" + AddMethodFix.generateTypeText(psiClass) + ">" : ""));
       final GrCodeReferenceElement ref = (GrCodeReferenceElement)list.add(_ref);
       JavaCodeStyleManager.getInstance(project).shortenClassReferences(ref);
     }
-    if (comparable != null && !myPsiClass.isInterface()) {
+    if (comparable != null && !psiClass.isInterface()) {
       final PsiMethod baseMethod = comparable.getMethods()[0];
-      OverrideImplementUtil.overrideOrImplement(myPsiClass, baseMethod);
+      OverrideImplementUtil.overrideOrImplement(psiClass, baseMethod);
     }
   }
 
