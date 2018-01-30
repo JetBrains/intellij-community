c471398267612e24f0f62858a90799687ad08c0f
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2018-01-30 17:48:45 +0300

    Parse and highlight `async` and `await` as keywords in Python 3.7+ (PY-28247)
    Update compatibility inspection to raise warning only if Python 3.7+ is marked

--- python/testData/quickFixes/PyRenameElementQuickFixTest/renameAsyncClassInPy35.py
+++
@@ -1,2 +0,0 @@
-class <warning descr="'async' and 'await' are not recommended to be used as variable, class, function or module names. They will become proper keywords in Python 3.7.">a<caret>sync</warning>(object):
-    pass
