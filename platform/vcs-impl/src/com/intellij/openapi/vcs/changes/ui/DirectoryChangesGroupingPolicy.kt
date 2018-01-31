701c1641fbd8b6969a89fe86ab2756b8505d2cec
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2018-01-31 23:06:23 +0300

    Extract separate "none" and "directory" changes grouping policies
    (cherry picked from commit 52e3bb30ff4167abec84c63f60cb27ee2c9864f9)

---
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/DirectoryChangesGroupingPolicy.kt
@@ -0,0 +1,38 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.vcs.changes.ui
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vcs.changes.ui.TreeModelBuilder.DIRECTORY_CACHE
+import com.intellij.openapi.vcs.changes.ui.TreeModelBuilder.PATH_NODE_BUILDER
+
+import javax.swing.tree.DefaultTreeModel
+
+class DirectoryChangesGroupingPolicy(val project: Project, val model: DefaultTreeModel) : ChangesGroupingPolicy {
+  private val innerPolicy = ChangesGroupingPolicyFactory.getInstance(project)?.createGroupingPolicy(model)
+
+  override fun getParentNodeFor(nodePath: StaticFilePath, subtreeRoot: ChangesBrowserNode<*>): ChangesBrowserNode<*> {
+    innerPolicy?.getParentNodeFor(nodePath, subtreeRoot)?.let {
+      it.markAsHelperNode()
+      return it
+    }
+
+    generateSequence(nodePath.parent) { it.parent }.forEach { parentPath ->
+      DIRECTORY_CACHE.getValue(subtreeRoot)[parentPath.key]?.let { return it }
+
+      PATH_NODE_BUILDER.getRequired(subtreeRoot).apply(parentPath)?.let {
+        it.markAsHelperNode()
+
+        val grandParent = getParentNodeFor(parentPath, subtreeRoot)
+        model.insertNodeInto(it, grandParent, grandParent.childCount)
+        DIRECTORY_CACHE.getValue(subtreeRoot)[parentPath.key] = it
+        return it
+      }
+    }
+
+    return subtreeRoot
+  }
+
+  class Factory(val project: Project) : ChangesGroupingPolicyFactory() {
+    override fun createGroupingPolicy(model: DefaultTreeModel) = DirectoryChangesGroupingPolicy(project, model)
+  }
+}
