eb335c763307b4c57792a6787f11590376bb86ed
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2018-01-31 23:12:55 +0300

    Provide extension point to configure changes grouping policies
    (cherry picked from commit 6ac5951a1d72b39d4cddbfe03252c8524995cbe8)

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/ChangesGroupingPolicyFactory.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/ChangesGroupingPolicyFactory.java
@@ -16,12 +16,16 @@
 package com.intellij.openapi.vcs.changes.ui;
 
 import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.extensions.ExtensionPointName;
+import com.intellij.openapi.extensions.KeyedFactoryEPBean;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.tree.DefaultTreeModel;
 
 public abstract class ChangesGroupingPolicyFactory {
+  public static final ExtensionPointName<KeyedFactoryEPBean> EP_NAME = ExtensionPointName.create("com.intellij.changesGroupingPolicy");
+
   public static ChangesGroupingPolicyFactory getInstance(@NotNull Project project) {
     return ServiceManager.getService(project, ChangesGroupingPolicyFactory.class);
   }
