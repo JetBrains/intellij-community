f6d541ad1ee820a4793da30ba49833d37907b91a
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2018-02-01 18:03:57 +0300

    Indicate that folded regions contains highlighted text
    for search results, identifier highlighting, warnings, errors
    (IDEA-61533, IDEA-128613)

--- platform/lang-impl/src/com/intellij/application/options/colors/highlighting/HighlightData.java
+++ platform/lang-impl/src/com/intellij/application/options/colors/highlighting/HighlightData.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.application.options.colors.highlighting;
 
@@ -75,6 +61,7 @@ public class HighlightData {
           highlighter.setErrorStripeMarkColor(errorStripeColor);
           final String tooltip = displayText.get(myHighlightType);
           highlighter.setErrorStripeTooltip(tooltip);
+          highlighter.putUserData(RangeHighlighter.VISIBLE_IF_FOLDED, Boolean.TRUE);
         }
         catch (Exception e) {
           throw new RuntimeException(e);
