c1cdd4faa5be09dc6b901757fd864fba4872165d
Egor Ushakov <egor.ushakov@jetbrains.com>
2018-02-01 19:55:47 +0300

    IDEA-177411 Show external annotations in the code - disabled only in internal mode for now

--- java/java-impl/src/com/intellij/codeInsight/hints/JavaInlayParameterHintsProvider.kt
+++ java/java-impl/src/com/intellij/codeInsight/hints/JavaInlayParameterHintsProvider.kt
@@ -1,27 +1,18 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.hints
 
+import com.intellij.codeInsight.ExternalAnnotationsManager
+import com.intellij.codeInsight.InferredAnnotationsManager
 import com.intellij.codeInsight.completion.CompletionMemory
 import com.intellij.codeInsight.hints.HintInfo.MethodInfo
 import com.intellij.lang.java.JavaLanguage
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.psi.PsiCallExpression
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiMethod
+import com.intellij.psi.PsiModifierListOwner
 
 class JavaInlayParameterHintsProvider : InlayParameterHintsProvider {
   
@@ -43,8 +34,25 @@ class JavaInlayParameterHintsProvider : InlayParameterHintsProvider {
     if (element is PsiCallExpression) {
       return JavaInlayHintsProvider.hints(element).toList()
     }
+    if (element is PsiModifierListOwner && showAnnotations()) {
+      val externalAnnotations = ExternalAnnotationsManager.getInstance(element.project).findExternalAnnotations(element)
+      val inferredAnnotations = InferredAnnotationsManager.getInstance(element.project).findInferredAnnotations(element)
+
+      return (externalAnnotations.orEmpty().asSequence() + inferredAnnotations.asSequence())
+        .filter { it.nameReferenceElement != null }
+        .map { InlayInfo("@" + it.nameReferenceElement?.referenceName + it.parameterList.text, element.textRange.startOffset) }
+        .toList()
+    }
     return emptyList()
   }
+
+  private fun showAnnotations(): Boolean {
+    val value = Registry.get("java.annotations.show.inline")
+    if (value.isBoolean) return value.asBoolean()
+    return "internal" == value.asString() && ApplicationManager.getApplication().isInternal
+  }
+
+  override fun getInlayPresentation(inlayText: String): String = inlayText
 
   override fun canShowHintsWhenDisabled(): Boolean {
     return true
