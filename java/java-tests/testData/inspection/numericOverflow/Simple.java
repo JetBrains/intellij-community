025fb77f0d3462531add629837b823797426e2e6
Alexey Kudravtsev <cdr@intellij.com>
2018-02-01 16:01:26 +0300

    IDEA-151378 Numeric overflow in expression does not go away

--- java/java-tests/testData/inspection/numericOverflow/Simple.java
+++ java/java-tests/testData/inspection/numericOverflow/Simple.java
@@ -12,9 +12,9 @@ class c {
 
   void f() {
     float f1 = <warning descr="Numeric overflow in expression">1.0f / 0.0f</warning>;
-    <warning descr="Numeric overflow in expression">f1 = <warning descr="Numeric overflow in expression">FLOAT_MAX_VALUE + FLOAT_MAX_VALUE</warning></warning>;
-    <warning descr="Numeric overflow in expression">f1 = <warning descr="Numeric overflow in expression">FLOAT_MAX_VALUE * 2</warning></warning>;
-    <warning descr="Numeric overflow in expression">f1 = <warning descr="Numeric overflow in expression">2 / FLOAT_MIN_VALUE</warning></warning>;
+    f1 = <warning descr="Numeric overflow in expression">FLOAT_MAX_VALUE + FLOAT_MAX_VALUE</warning>;
+    f1 = <warning descr="Numeric overflow in expression">FLOAT_MAX_VALUE * 2</warning>;
+    f1 = <warning descr="Numeric overflow in expression">2 / FLOAT_MIN_VALUE</warning>;
     f1 = FLOAT_MIN_VALUE + 1;
     f1 = - FLOAT_MIN_VALUE;
     f1 = -FLOAT_MAX_VALUE;
@@ -22,52 +22,52 @@ class c {
 
     double d1 = <warning descr="Numeric overflow in expression">DOUBLE_MAX_VALUE - -DOUBLE_MAX_VALUE</warning>;
     d1 = DOUBLE_MAX_VALUE + 1;
-    <warning descr="Numeric overflow in expression">d1 = <warning descr="Numeric overflow in expression">2 * DOUBLE_MAX_VALUE</warning></warning>;
-    <warning descr="Numeric overflow in expression">d1 = <warning descr="Numeric overflow in expression">2 / DOUBLE_MIN_VALUE</warning></warning>;
-    <warning descr="Numeric overflow in expression">d1 = <warning descr="Numeric overflow in expression">2 / 0.0d</warning></warning>;
-    <warning descr="Numeric overflow in expression">d1 = <warning descr="Numeric overflow in expression">2 / 0.0</warning></warning>;
+    d1 = <warning descr="Numeric overflow in expression">2 * DOUBLE_MAX_VALUE</warning>;
+    d1 = <warning descr="Numeric overflow in expression">2 / DOUBLE_MIN_VALUE</warning>;
+    d1 = <warning descr="Numeric overflow in expression">2 / 0.0d</warning>;
+    d1 = <warning descr="Numeric overflow in expression">2 / 0.0</warning>;
     System.out.println(d1);
 
     int i1 = <warning descr="Numeric overflow in expression">INTEGER_MAX_VALUE + 1</warning>;
-    <warning descr="Numeric overflow in expression">i1 = <warning descr="Numeric overflow in expression">INTEGER_MAX_VALUE - 1 + 2</warning></warning>;
-    <warning descr="Numeric overflow in expression">i1 = <warning descr="Numeric overflow in expression">INTEGER_MAX_VALUE - INTEGER_MIN_VALUE</warning></warning>;
-    <warning descr="Numeric overflow in expression">i1 = <warning descr="Numeric overflow in expression">-INTEGER_MIN_VALUE</warning></warning>;
-    <warning descr="Numeric overflow in expression">i1 = <warning descr="Numeric overflow in expression">INTEGER_MIN_VALUE - 1</warning></warning>;
-    <warning descr="Numeric overflow in expression">i1 = <warning descr="Numeric overflow in expression">INTEGER_MIN_VALUE - INTEGER_MAX_VALUE</warning></warning>;
+    i1 = <warning descr="Numeric overflow in expression">INTEGER_MAX_VALUE - 1 + 2</warning>;
+    i1 = <warning descr="Numeric overflow in expression">INTEGER_MAX_VALUE - INTEGER_MIN_VALUE</warning>;
+    i1 = <warning descr="Numeric overflow in expression">-INTEGER_MIN_VALUE</warning>;
+    i1 = <warning descr="Numeric overflow in expression">INTEGER_MIN_VALUE - 1</warning>;
+    i1 = <warning descr="Numeric overflow in expression">INTEGER_MIN_VALUE - INTEGER_MAX_VALUE</warning>;
     i1 = INTEGER_MIN_VALUE + INTEGER_MAX_VALUE;
     i1 = - INTEGER_MAX_VALUE;
     i1 = - -INTEGER_MAX_VALUE;
-    <warning descr="Numeric overflow in expression">i1 = <warning descr="Numeric overflow in expression">INTEGER_MIN_VALUE * -1</warning></warning>;
-    <warning descr="Numeric overflow in expression">i1 = <warning descr="Numeric overflow in expression">INTEGER_MIN_VALUE * 2</warning></warning>;
-    <warning descr="Numeric overflow in expression">i1 = <warning descr="Numeric overflow in expression">INTEGER_MAX_VALUE * -2</warning></warning>;
+    i1 = <warning descr="Numeric overflow in expression">INTEGER_MIN_VALUE * -1</warning>;
+    i1 = <warning descr="Numeric overflow in expression">INTEGER_MIN_VALUE * 2</warning>;
+    i1 = <warning descr="Numeric overflow in expression">INTEGER_MAX_VALUE * -2</warning>;
     i1 = INTEGER_MAX_VALUE * -1;
-    <warning descr="Numeric overflow in expression">i1 = <warning descr="Numeric overflow in expression">2 / 0</warning></warning>;
-    <warning descr="Numeric overflow in expression">i1 = <warning descr="Numeric overflow in expression">INTEGER_MIN_VALUE / -1</warning></warning>;
-    <warning descr="Numeric overflow in expression">i1 = <warning descr="Numeric overflow in expression">1000 << 30</warning></warning>;
+    i1 = <warning descr="Numeric overflow in expression">2 / 0</warning>;
+    i1 = <warning descr="Numeric overflow in expression">INTEGER_MIN_VALUE / -1</warning>;
+    i1 = <warning descr="Numeric overflow in expression">1000 << 30</warning>;
     System.out.println(i1);
 
     long l1 = <warning descr="Numeric overflow in expression">LONG_MAX_VALUE + 1</warning>;
-    <warning descr="Numeric overflow in expression">l1 = <warning descr="Numeric overflow in expression">LONG_MAX_VALUE - 1 + 2</warning></warning>;
-    <warning descr="Numeric overflow in expression">l1 = <warning descr="Numeric overflow in expression">LONG_MAX_VALUE - LONG_MIN_VALUE</warning></warning>;
-    <warning descr="Numeric overflow in expression">l1 = <warning descr="Numeric overflow in expression">-LONG_MIN_VALUE</warning></warning>;
-    <warning descr="Numeric overflow in expression">l1 = <warning descr="Numeric overflow in expression">LONG_MIN_VALUE - 1</warning></warning>;
-    <warning descr="Numeric overflow in expression">l1 = <warning descr="Numeric overflow in expression">LONG_MIN_VALUE - LONG_MAX_VALUE</warning></warning>;
+    l1 = <warning descr="Numeric overflow in expression">LONG_MAX_VALUE - 1 + 2</warning>;
+    l1 = <warning descr="Numeric overflow in expression">LONG_MAX_VALUE - LONG_MIN_VALUE</warning>;
+    l1 = <warning descr="Numeric overflow in expression">-LONG_MIN_VALUE</warning>;
+    l1 = <warning descr="Numeric overflow in expression">LONG_MIN_VALUE - 1</warning>;
+    l1 = <warning descr="Numeric overflow in expression">LONG_MIN_VALUE - LONG_MAX_VALUE</warning>;
     l1 = LONG_MIN_VALUE + LONG_MAX_VALUE;
     l1 = - LONG_MAX_VALUE;
     l1 = - -LONG_MAX_VALUE;
-    <warning descr="Numeric overflow in expression">l1 = <warning descr="Numeric overflow in expression">-INTEGER_MIN_VALUE</warning></warning>;
+    l1 = <warning descr="Numeric overflow in expression">-INTEGER_MIN_VALUE</warning>;
     l1 =  -1L + INTEGER_MIN_VALUE;
-    <warning descr="Numeric overflow in expression">l1 = <warning descr="Numeric overflow in expression">LONG_MIN_VALUE * INTEGER_MIN_VALUE</warning></warning>;
-    <warning descr="Numeric overflow in expression">l1 = <warning descr="Numeric overflow in expression">LONG_MIN_VALUE * -1</warning></warning>;
-    <warning descr="Numeric overflow in expression">l1 = <warning descr="Numeric overflow in expression">LONG_MIN_VALUE * 2</warning></warning>;
-    <warning descr="Numeric overflow in expression">l1 = <warning descr="Numeric overflow in expression">LONG_MAX_VALUE * -2</warning></warning>;
+    l1 = <warning descr="Numeric overflow in expression">LONG_MIN_VALUE * INTEGER_MIN_VALUE</warning>;
+    l1 = <warning descr="Numeric overflow in expression">LONG_MIN_VALUE * -1</warning>;
+    l1 = <warning descr="Numeric overflow in expression">LONG_MIN_VALUE * 2</warning>;
+    l1 = <warning descr="Numeric overflow in expression">LONG_MAX_VALUE * -2</warning>;
     l1 = INTEGER_MIN_VALUE * -1L;
-    <warning descr="Numeric overflow in expression">l1 = <warning descr="Numeric overflow in expression">2 / 0L</warning></warning>;
-    <warning descr="Numeric overflow in expression">l1 = <warning descr="Numeric overflow in expression">2 % 0L</warning></warning>;
-    <warning descr="Numeric overflow in expression">l1 = <warning descr="Numeric overflow in expression">LONG_MIN_VALUE / -1</warning></warning>;
-    <warning descr="Numeric overflow in expression">l1 = <warning descr="Numeric overflow in expression">30 * 24 * 60 * 60 * 1000</warning></warning>;
-    <warning descr="Numeric overflow in expression">l1 = <warning descr="Numeric overflow in expression"><warning descr="Numeric overflow in expression">30000000 * 243232323 * <warning descr="Numeric overflow in expression">(<warning descr="Numeric overflow in expression">LONG_MAX_VALUE +3</warning>)</warning></warning> / 5</warning></warning>;
-    <warning descr="Numeric overflow in expression">l1 = <warning descr="Numeric overflow in expression">1000 << 62</warning></warning>;
+    l1 = <warning descr="Numeric overflow in expression">2 / 0L</warning>;
+    l1 = <warning descr="Numeric overflow in expression">2 % 0L</warning>;
+    l1 = <warning descr="Numeric overflow in expression">LONG_MIN_VALUE / -1</warning>;
+    l1 = <warning descr="Numeric overflow in expression">30 * 24 * 60 * 60 * 1000</warning>;
+    l1 = 30000000 * 243232323 * (<warning descr="Numeric overflow in expression">LONG_MAX_VALUE +3</warning>) / 5;
+    l1 = <warning descr="Numeric overflow in expression">1000 << 62</warning>;
     System.out.println(l1);
   }
 
@@ -82,4 +82,8 @@ class c {
   int iii = <warning descr="Numeric overflow in expression">2 % 0</warning>;
 
   double x = -Double.POSITIVE_INFINITY;
+
+  public void checkDonotOverlap(long lower) {
+      final long upper =  lower + <warning descr="Numeric overflow in expression">1000 * 31536000</warning>;
+  }
 }
--- java/java-tests/testSrc/com/intellij/java/codeInspection/NumericOverflowInspectionTest.java
+++ java/java-tests/testSrc/com/intellij/java/codeInspection/NumericOverflowInspectionTest.java
@@ -17,6 +17,7 @@ package com.intellij.java.codeInspection;
 
 import com.intellij.JavaTestUtil;
 import com.intellij.codeInspection.NumericOverflowInspection;
+import com.intellij.lang.annotation.HighlightSeverity;
 import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
 
 public class NumericOverflowInspectionTest extends LightCodeInsightFixtureTestCase {
@@ -27,8 +28,27 @@ public class NumericOverflowInspectionTest extends LightCodeInsightFixtureTestCa
 
   public void testSimple() { doTest(); }
 
-  private void doTest() {
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
     myFixture.enableInspections(new NumericOverflowInspection());
+  }
+
+  private void doTest() {
     myFixture.testHighlighting(getTestName(false) + ".java");
+  }
+
+  public void testUpdatesOnTyping() {
+    String text = "class My {\n" +
+                  "void d(long lower) {\n" +
+                  "        long upper =  lower + 1000<caret> * 31536000;\n" +
+                  "    }" +
+                  "}";
+    myFixture.configureByText("My.java", text);
+    assertOneElement(myFixture.doHighlighting(HighlightSeverity.WARNING));
+    myFixture.type('L');
+    assertEmpty(myFixture.doHighlighting(HighlightSeverity.WARNING));
+    myFixture.type('\b');
+    assertOneElement(myFixture.doHighlighting(HighlightSeverity.WARNING));
   }
 }
