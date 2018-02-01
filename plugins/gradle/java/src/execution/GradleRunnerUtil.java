685ba61380dc0e5f82566478843577d6c3408eea
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2018-02-01 09:07:29 +0300

    cleanup obsolete api

--- plugins/gradle/java/src/execution/GradleRunnerUtil.java
+++ plugins/gradle/java/src/execution/GradleRunnerUtil.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.gradle.execution;
 
 import com.intellij.execution.Location;
@@ -65,98 +51,6 @@ import static com.intellij.util.io.URLUtil.SCHEME_SEPARATOR;
  * @since 12/4/2015
  */
 public class GradleRunnerUtil {
-
-  /**
-   * @deprecated to be removed in the 2018.1 version
-   */
-  public static DuplexConsoleView attachTaskExecutionView(@NotNull final Project project,
-                                                          @NotNull final ConsoleView consoleView,
-                                                          final boolean isTaskConsoleEnabledByDefault,
-                                                          @Nullable final String stateStorageKey,
-                                                          @NotNull final ProcessHandler processHandler,
-                                                          @NotNull final ExternalSystemTaskId taskId) {
-    final String tripleStateStorageKey = stateStorageKey != null ? stateStorageKey + "_str" : null;
-    if (stateStorageKey != null && isTaskConsoleEnabledByDefault && !PropertiesComponent.getInstance().isValueSet(tripleStateStorageKey)) {
-      PropertiesComponent.getInstance().setValue(tripleStateStorageKey, Boolean.TRUE.toString());
-      PropertiesComponent.getInstance().setValue(stateStorageKey, Boolean.TRUE);
-    }
-
-    final TaskExecutionView gradleExecutionConsole = new TaskExecutionView(project);
-    final Ref<DuplexConsoleView> duplexConsoleViewRef = Ref.create();
-    final DuplexConsoleView duplexConsoleView =
-      new DuplexConsoleView<ConsoleView, ConsoleView>(gradleExecutionConsole, consoleView, stateStorageKey) {
-
-        @Override
-        public void enableConsole(boolean primary) {
-          super.enableConsole(primary);
-          if (stateStorageKey != null) {
-            PropertiesComponent.getInstance().setValue(tripleStateStorageKey, Boolean.toString(primary));
-          }
-        }
-
-        @NotNull
-        @Override
-        public AnAction[] createConsoleActions() {
-
-          final DefaultActionGroup textActionGroup = new DefaultActionGroup() {
-            @Override
-            public void update(AnActionEvent e) {
-              super.update(e);
-              if (duplexConsoleViewRef.get() != null) {
-                e.getPresentation().setVisible(!duplexConsoleViewRef.get().isPrimaryConsoleEnabled());
-              }
-            }
-          };
-          final AnAction[] consoleActions = consoleView.createConsoleActions();
-          for (AnAction anAction : consoleActions) {
-            textActionGroup.add(anAction);
-          }
-
-          final List<AnAction> anActions = ContainerUtil.newArrayList(super.createConsoleActions());
-          anActions.add(textActionGroup);
-          return ArrayUtil.toObjectArray(anActions, AnAction.class);
-        }
-      };
-
-    duplexConsoleViewRef.set(duplexConsoleView);
-
-    duplexConsoleView.setDisableSwitchConsoleActionOnProcessEnd(false);
-    duplexConsoleView.getSwitchConsoleActionPresentation().setIcon(AllIcons.Actions.ChangeView);
-    duplexConsoleView.getSwitchConsoleActionPresentation().setText(GradleBundle.message("gradle.runner.toggle.tree.text.action.name"));
-
-    final ExternalSystemProgressNotificationManager progressManager =
-      ServiceManager.getService(ExternalSystemProgressNotificationManager.class);
-    final ExternalSystemTaskNotificationListenerAdapter taskListener = new ExternalSystemTaskNotificationListenerAdapter() {
-      @Override
-      public void onStatusChange(@NotNull final ExternalSystemTaskNotificationEvent event) {
-        if (event instanceof ExternalSystemTaskExecutionEvent) {
-          UIUtil.invokeLaterIfNeeded(() -> {
-            if(((ExternalSystemTaskExecutionEvent)event).getProgressEvent() instanceof ExternalSystemProgressEventUnsupported) {
-              duplexConsoleView.enableConsole(false);
-            }
-            gradleExecutionConsole.onStatusChange((ExternalSystemTaskExecutionEvent)event);
-          });
-        }
-      }
-
-      @Override
-      public void onStart(@NotNull ExternalSystemTaskId id, final String workingDir) {
-        UIUtil.invokeLaterIfNeeded(() -> gradleExecutionConsole.setWorkingDir(workingDir));
-      }
-
-      @Override
-      public void onFailure(@NotNull ExternalSystemTaskId id, @NotNull final Exception e) {
-        UIUtil.invokeLaterIfNeeded(() -> gradleExecutionConsole.onFailure(e));
-      }
-
-      @Override
-      public void onEnd(@NotNull ExternalSystemTaskId id) {
-        progressManager.removeNotificationListener(this);
-      }
-    };
-    progressManager.addNotificationListener(taskId, taskListener);
-    return duplexConsoleView;
-  }
 
   @Nullable
   public static Location<PsiMethod> getMethodLocation(@NotNull Location contextLocation) {
