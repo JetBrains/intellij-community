e6e51584efd7e6d7e2a454902590e53a90184d47
Anna Kozlova <anna.kozlova@jetbrains.com>
2018-02-02 13:01:38 +0300

    unqualified getClass() type fixed (IDEA-186043)

--- java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/GenericsHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/GenericsHighlightingTest.java
@@ -429,4 +429,5 @@ public class GenericsHighlightingTest extends LightDaemonAnalyzerTestCase {
   public void testClassInWrongPackage() { doTest6(false); }
   public void testRecursiveParamBoundsWhenSuperSubstitution() { doTest6(false); }
   public void testCaptureForBoundCheck() { doTest6(false); }
+  public void testGetClassInAnonymous() { doTest6(false); }
 }
