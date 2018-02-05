fa8e6a32aba4fc1e14a1e3452d4f8aede9f0dd49
peter <peter@jetbrains.com>
2018-02-05 23:46:59 +0100

    clear smart pointers on uncommitted documents on test finish to avoid leaks

--- platform/core-impl/src/com/intellij/psi/impl/PsiDocumentManagerBase.java
+++ platform/core-impl/src/com/intellij/psi/impl/PsiDocumentManagerBase.java
@@ -151,7 +151,7 @@ public abstract class PsiDocumentManagerBase extends PsiDocumentManager implemen
   }
 
   @Nullable
-  private static VirtualFile getVirtualFile(@NotNull Document document) {
+  protected static VirtualFile getVirtualFile(@NotNull Document document) {
     final VirtualFile virtualFile = FileDocumentManager.getInstance().getFile(document);
     if (virtualFile == null || !virtualFile.isValid()) return null;
     return virtualFile;
@@ -888,7 +888,7 @@ public abstract class PsiDocumentManagerBase extends PsiDocumentManager implemen
     return info;
   }
 
-  private SmartPointerManagerImpl getSmartPointerManager() {
+  protected SmartPointerManagerImpl getSmartPointerManager() {
     return (SmartPointerManagerImpl)SmartPointerManager.getInstance(myProject);
   }
 
