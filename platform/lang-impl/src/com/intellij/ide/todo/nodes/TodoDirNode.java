cc7e48ee430b5e2d400ebc511cce835c2e1fd956
Anton Makeev <Anton.Makeev@jetbrains.com>
2018-02-06 11:52:56 +0100

    CPP-11991 Mark source files in the project view using overlay icons

--- platform/lang-impl/src/com/intellij/ide/todo/nodes/TodoDirNode.java
+++ platform/lang-impl/src/com/intellij/ide/todo/nodes/TodoDirNode.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.ide.todo.nodes;
 
@@ -35,7 +21,7 @@ import com.intellij.openapi.vcs.FileStatusManager;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.impl.file.DirectoryIconProvider;
+import com.intellij.psi.impl.file.SourceRootIconProvider;
 import com.intellij.ui.HighlightedRegion;
 import com.intellij.usageView.UsageTreeColors;
 import com.intellij.usageView.UsageTreeColorsScheme;
@@ -102,7 +88,7 @@ public final class TodoDirNode extends PsiDirectoryNode implements HighlightedRe
   protected void setupIcon(PresentationData data, PsiDirectory psiDirectory) {
     final VirtualFile virtualFile = psiDirectory.getVirtualFile();
     if (ProjectRootsUtil.isModuleContentRoot(virtualFile, psiDirectory.getProject())) {
-      data.setIcon(new DirectoryIconProvider().getIcon(psiDirectory, 0));
+      data.setIcon(new SourceRootIconProvider.DirectoryProvider().getIcon(psiDirectory, 0));
     } else {
       super.setupIcon(data, psiDirectory);
     }
