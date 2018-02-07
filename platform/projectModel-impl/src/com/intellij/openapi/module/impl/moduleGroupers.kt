c25725c8c7ae412df75d09c68a2cb987faff8bdd
nik <Nikolay.Chashnikov@jetbrains.com>
2018-02-07 19:37:57 +0100

    project configuration: improve automatic grouping by module names
    Don't split qualified name by dots in places where it leads to incorrect identifiers (e.g. after digits) to avoid creating unnecessary groups for modules which names include version numbers (IDEA-180295).

--- platform/projectModel-impl/src/com/intellij/openapi/module/impl/moduleGroupers.kt
+++ platform/projectModel-impl/src/com/intellij/openapi/module/impl/moduleGroupers.kt
@@ -3,7 +3,7 @@ package com.intellij.openapi.module.impl
 
 import com.intellij.openapi.module.*
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.text.StringUtil
+import org.jetbrains.annotations.TestOnly
 import java.util.*
 
 /**
@@ -26,19 +26,17 @@ private abstract class ModuleGrouperBase(protected val project: Project, protect
 }
 
 private class QualifiedNameGrouper(project: Project, model: ModifiableModuleModel?) : ModuleGrouperBase(project, model) {
-  override fun getGroupPath(module: Module): List<String> {
-    return getGroupPathByModuleName(getModuleName(module))
-  }
+  override fun getGroupPath(module: Module) = getGroupPathByModuleName(getModuleName(module))
 
   override fun getGroupPath(description: ModuleDescription) = getGroupPathByModuleName(description.name)
 
-  override fun getShortenedNameByFullModuleName(name: String) = StringUtil.getShortName(name)
+  override fun getShortenedNameByFullModuleName(name: String) = name.substringAfterLastDotNotFollowedByIncorrectChar()
 
-  override fun getGroupPathByModuleName(name: String) = name.split('.').dropLast(1)
+  override fun getGroupPathByModuleName(name: String) = name.splitByDotsJoiningIncorrectIdentifiers().dropLast(1)
 
-  override fun getModuleAsGroupPath(module: Module) = getModuleName(module).split('.')
+  override fun getModuleAsGroupPath(module: Module) = getModuleName(module).splitByDotsJoiningIncorrectIdentifiers()
 
-  override fun getModuleAsGroupPath(description: ModuleDescription) = description.name.split('.')
+  override fun getModuleAsGroupPath(description: ModuleDescription) = description.name.splitByDotsJoiningIncorrectIdentifiers()
 }
 
 private class ExplicitModuleGrouper(project: Project, model: ModifiableModuleModel?): ModuleGrouperBase(project, model) {
@@ -60,4 +58,44 @@ private class ExplicitModuleGrouper(project: Project, model: ModifiableModuleMod
   override fun getModuleAsGroupPath(module: Module) = null
 
   override fun getModuleAsGroupPath(description: ModuleDescription) = null
+}
+
+/**
+ * Split by dots where it doesn't lead to incorrect identifiers (i.e. "a.b-1.1" will be split to "a" and "b-1.1", not to "a", "b-1" and "1")
+ */
+private fun String.splitByDotsJoiningIncorrectIdentifiers(): List<String> {
+  var start = 0
+  var next = 1
+  val names = ArrayList<String>()
+  while (next < length) {
+    val end = indexOf('.', next)
+    if (end == -1 || end == length - 1) {
+      break
+    }
+    next = end + 1
+    if (this[end + 1].isJavaIdentifierStart()) {
+      names.add(substring(start, end))
+      start = end + 1
+    }
+  }
+  names.add(substring(start))
+  return names
+}
+
+/**
+ * Returns the same value as `splitByDotsNotFollowedByIncorrectChars().last()` but works more efficiently
+ */
+private fun String.substringAfterLastDotNotFollowedByIncorrectChar(): String {
+  if (length <= 1) return this
+  var i = lastIndexOf('.', length - 2)
+  while (i != -1 && !this[i + 1].isJavaIdentifierStart()) {
+    i = lastIndexOf('.', i-1)
+  }
+  if (i <= 0) return this
+  return substring(i+1)
+}
+
+@TestOnly
+public fun splitStringByDotsJoiningIncorrectIdentifiers(string: String): Pair<List<String>, String> {
+  return Pair(string.splitByDotsJoiningIncorrectIdentifiers(), string.substringAfterLastDotNotFollowedByIncorrectChar())
 }
