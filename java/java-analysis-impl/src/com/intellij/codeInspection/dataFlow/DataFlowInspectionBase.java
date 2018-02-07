7ea47bbbb0b0ca9b0220584808d0292402db6a41
peter <peter@jetbrains.com>
2018-02-07 15:49:02 +0100

    fix typo in dfa warning text

--- java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/DataFlowInspectionBase.java
+++ java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/DataFlowInspectionBase.java
@@ -562,13 +562,13 @@ public class DataFlowInspectionBase extends AbstractBaseJavaLocalInspectionTool 
       if (reportedAnchors.contains(expr)) continue;
 
       if (expr.getParent() instanceof PsiMethodReferenceExpression) {
-        holder.registerProblem(expr.getParent(), "Method reference argument might be null but passed to non annotated parameter");
+        holder.registerProblem(expr.getParent(), "Method reference argument might be null but passed to non-annotated parameter");
         continue;
       }
 
       final String text = isNullLiteralExpression(expr)
-                          ? "Passing <code>null</code> argument to non annotated parameter"
-                          : "Argument <code>#ref</code> #loc might be null but passed to non annotated parameter";
+                          ? "Passing <code>null</code> argument to non-annotated parameter"
+                          : "Argument <code>#ref</code> #loc might be null but passed to non-annotated parameter";
       List<LocalQuickFix> fixes = createNPEFixes((PsiExpression)expr, (PsiExpression)expr, holder.isOnTheFly());
       final PsiElement parent = expr.getParent();
       if (parent instanceof PsiExpressionList) {
--- java/java-tests/testData/inspection/dataFlow/fixture/NullLiteralArgumentDoesntReportedWhenMethodOnlyThrowAnException.java
+++ java/java-tests/testData/inspection/dataFlow/fixture/NullLiteralArgumentDoesntReportedWhenMethodOnlyThrowAnException.java
@@ -1,7 +1,7 @@
 class Test {
 
   void m() {
-    throwAnException(<warning descr="Passing 'null' argument to non annotated parameter">null</warning>);
+    throwAnException(<warning descr="Passing 'null' argument to non-annotated parameter">null</warning>);
   }
 
   static void throwAnException(String arg) {
