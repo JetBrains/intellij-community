71ed9ef8c2af345cd4d1b689e2fc8ebc645518d3
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2018-02-07 22:27:20 +0800

    Always use grouping policy when building vcs data tree models
    Deprecate methods which use just "showFlatten" parameter

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/TreeModelBuilder.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/TreeModelBuilder.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vcs.changes.ui;
 
 import com.intellij.openapi.diagnostic.Logger;
@@ -78,6 +64,10 @@ public class TreeModelBuilder {
   protected final static Comparator<FilePath> PATH_COMPARATOR = comparingInt(path -> path.getPath().length());
   protected final static Comparator<Change> CHANGE_COMPARATOR = comparing(ChangesUtil::getFilePath, PATH_COMPARATOR);
 
+  /**
+   * @deprecated Use {@link #TreeModelBuilder(Project, ChangesGroupingPolicyFactory)}.
+   */
+  @Deprecated
   public TreeModelBuilder(@NotNull Project project, boolean showFlatten) {
     this(project, ChangesGroupingSupport.collectFactories(project).getByKey(showFlatten ? NONE_GROUPING : DIRECTORY_GROUPING));
   }
@@ -94,6 +84,10 @@ public class TreeModelBuilder {
     return new DefaultTreeModel(ChangesBrowserNode.createRoot(project));
   }
 
+  /**
+   * @deprecated Use {@link TreeModelBuilder#buildFromChanges(Project, ChangesGroupingPolicyFactory, Collection, ChangeNodeDecorator)}.
+   */
+  @Deprecated
   @NotNull
   public static DefaultTreeModel buildFromChanges(@NotNull Project project,
                                                   boolean showFlatten,
@@ -105,29 +99,30 @@ public class TreeModelBuilder {
   }
 
   @NotNull
+  public static DefaultTreeModel buildFromChanges(@NotNull Project project,
+                                                  @NotNull ChangesGroupingPolicyFactory grouping,
+                                                  @NotNull Collection<? extends Change> changes,
+                                                  @Nullable ChangeNodeDecorator changeNodeDecorator) {
+    return new TreeModelBuilder(project, grouping)
+      .setChanges(changes, changeNodeDecorator)
+      .build();
+  }
+
+  @NotNull
   public static DefaultTreeModel buildFromFilePaths(@NotNull Project project,
-                                                    boolean showFlatten,
+                                                    @NotNull ChangesGroupingPolicyFactory grouping,
                                                     @NotNull Collection<FilePath> filePaths) {
-    return new TreeModelBuilder(project, showFlatten)
+    return new TreeModelBuilder(project, grouping)
       .setFilePaths(filePaths)
       .build();
   }
 
   @NotNull
   public static DefaultTreeModel buildFromChangeLists(@NotNull Project project,
-                                                      boolean showFlatten,
+                                                      @NotNull ChangesGroupingPolicyFactory grouping,
                                                       @NotNull Collection<? extends ChangeList> changeLists) {
-    return new TreeModelBuilder(project, showFlatten)
+    return new TreeModelBuilder(project, grouping)
       .setChangeLists(changeLists)
-      .build();
-  }
-
-  @NotNull
-  public static DefaultTreeModel buildFromVirtualFiles(@NotNull Project project,
-                                                       boolean showFlatten,
-                                                       @NotNull Collection<VirtualFile> virtualFiles) {
-    return new TreeModelBuilder(project, showFlatten)
-      .setVirtualFiles(virtualFiles, null)
       .build();
   }
 
