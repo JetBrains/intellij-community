c536e920b60e98a986b7bd8b695ace4c60004eb8
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2018-02-08 21:55:25 +0300

    Support abc.abstractstaticmethod and abc.abstractclassmethod (PY-14896)
    Modifier inference for functions decorated with these decorators was fixed.
    Usages of these decorators are highlighted as deprecated.

--- python/testData/deprecation/abcDeprecatedAbstracts.py
+++ python/testData/deprecation/abcDeprecatedAbstracts.py
@@ -32,3 +32,13 @@ class D(metaclass=abc.ABCMeta):
     <warning descr="'abc.abstractproperty' is deprecated since Python 3.3. Use 'property' with 'abc.abstractmethod' instead.">@foo.abstractproperty</warning>
     def prop(self):
         pass
+
+
+class A:
+    <warning descr="'abc.abstractclassmethod' is deprecated since Python 3.3. Use 'classmethod' with 'abc.abstractmethod' instead.">@abc.abstractclassmethod</warning>
+    def foo(cls):
+        pass
+
+    <warning descr="'abc.abstractstaticmethod' is deprecated since Python 3.3. Use 'staticmethod' with 'abc.abstractmethod' instead.">@abc.abstractstaticmethod</warning>
+    def bar():
+        pass
