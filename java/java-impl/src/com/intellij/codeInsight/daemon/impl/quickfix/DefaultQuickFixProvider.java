690384d7b4dedae7bcf07dab2b7804b46d8afa2b
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2018-02-09 18:59:49 +0300

    add StringUtil.isUpperCase

--- java/java-impl/src/com/intellij/codeInsight/daemon/impl/quickfix/DefaultQuickFixProvider.java
+++ java/java-impl/src/com/intellij/codeInsight/daemon/impl/quickfix/DefaultQuickFixProvider.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.daemon.impl.quickfix;
 
 import com.intellij.codeInsight.daemon.QuickFixActionRegistrar;
@@ -37,6 +23,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.EnumMap;
 import java.util.Map;
+
+import static com.intellij.openapi.util.text.StringUtil.isUpperCase;
 
 public class DefaultQuickFixProvider extends UnresolvedReferenceQuickFixProvider<PsiJavaCodeReferenceElement> {
   @Override
@@ -128,11 +116,7 @@ public class DefaultQuickFixProvider extends UnresolvedReferenceQuickFixProvider
   private static VariableKind getKind(@NotNull JavaCodeStyleManager styleManager, @NotNull PsiReferenceExpression refExpr) {
     final String reference = refExpr.getText();
 
-    boolean upperCase = true;
-    for (int i = 0; i < reference.length(); i++) {
-      if (!Character.isUpperCase(reference.charAt(i))) { upperCase = false; break; }
-    }
-    if (upperCase) {
+    if (isUpperCase(reference)) {
       return VariableKind.STATIC_FINAL_FIELD;
     }
 
