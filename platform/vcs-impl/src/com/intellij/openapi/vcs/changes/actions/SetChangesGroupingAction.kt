964dc96e22ecad372289d43c3531d8f78711d4de
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2018-02-09 13:47:23 +0800

    Make changes grouping actions toggleable, remove "None" grouping
    Note that currently still only one grouping could be set at a time, but each grouping could be unset (if it is set now) - so "none" grouping will be used in that case

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/SetChangesGroupingAction.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/SetChangesGroupingAction.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.ToggleAction
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.vcs.changes.ui.ChangesGroupingSupport
+import com.intellij.openapi.vcs.changes.ui.ChangesGroupingSupport.Companion.NONE_GROUPING
 
 abstract class SetChangesGroupingAction : ToggleAction(), DumbAware {
   init {
@@ -19,16 +20,10 @@ abstract class SetChangesGroupingAction : ToggleAction(), DumbAware {
   override fun isSelected(e: AnActionEvent) = getGroupingSupport(e)?.groupingKey == groupingKey
 
   override fun setSelected(e: AnActionEvent, state: Boolean) {
-    if (state) {
-      getGroupingSupport(e)!!.groupingKey = groupingKey
-    }
+    getGroupingSupport(e)!!.groupingKey = if (state) groupingKey else NONE_GROUPING
   }
 
   protected fun getGroupingSupport(e: AnActionEvent) = e.getData(ChangesGroupingSupport.KEY)
-}
-
-class SetNoneChangesGroupingAction : SetChangesGroupingAction() {
-  override val groupingKey get() = "none"
 }
 
 class SetDirectoryChangesGroupingAction : SetChangesGroupingAction() {
