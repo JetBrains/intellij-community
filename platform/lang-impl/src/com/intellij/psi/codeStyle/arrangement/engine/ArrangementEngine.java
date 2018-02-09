3ba77807e65ff9bfcb69e9468f8c4b206ef2749a
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2018-02-09 18:27:03 +0300

    Allow custom arrangment order [IDEA-181940]
    Modified pull request https://github.com/JetBrains/intellij-community/pull/715
    See also [IDEA-CR-29131]
    
    (cherry picked from commit 27be52a)
    
    Change-Id: I5537886be2188abbbc5a9bdbddff2418988d8033

--- platform/lang-impl/src/com/intellij/psi/codeStyle/arrangement/engine/ArrangementEngine.java
+++ platform/lang-impl/src/com/intellij/psi/codeStyle/arrangement/engine/ArrangementEngine.java
@@ -33,6 +33,7 @@ import com.intellij.psi.codeStyle.arrangement.match.ArrangementMatchRule;
 import com.intellij.psi.codeStyle.arrangement.match.ArrangementSectionRule;
 import com.intellij.psi.codeStyle.arrangement.std.ArrangementSettingsToken;
 import com.intellij.psi.codeStyle.arrangement.std.ArrangementStandardSettingsAware;
+import com.intellij.psi.codeStyle.arrangement.std.CustomArrangementOrderToken;
 import com.intellij.psi.codeStyle.arrangement.std.StdArrangementTokens;
 import com.intellij.util.containers.*;
 import com.intellij.util.containers.HashSet;
@@ -332,12 +333,17 @@ public class ArrangementEngine {
                                                                           @NotNull MultiMap<ArrangementMatchRule, E> elementsByRule,
                                                                           @NotNull ArrangementMatchRule rule) {
     if (elementsByRule.containsKey(rule)) {
-      final Collection<E> arrangedEntries = elementsByRule.remove(rule);
+      List<E> arrangedEntries = (List<E>)elementsByRule.remove(rule);
+      assert arrangedEntries != null;
 
-      // Sort by name if necessary.
-      if (StdArrangementTokens.Order.BY_NAME.equals(rule.getOrderType())) {
-        sortByName((List<E>)arrangedEntries);
+      ArrangementSettingsToken order = rule.getOrderType();
+      if (order instanceof CustomArrangementOrderToken) {
+        arrangedEntries.sort(((CustomArrangementOrderToken)order).getEntryComparator());
       }
+      else if (rule.getOrderType().equals(StdArrangementTokens.Order.BY_NAME)) {
+        sortByName(arrangedEntries);
+      }
+
       arranged.addAll(arrangedEntries);
       return arrangedEntries;
     }
