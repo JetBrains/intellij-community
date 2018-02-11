995c2bf74fb14deccc5bd31836283c547ec61b17
Alexander Koshevoy <Alexander.Koshevoy@jetbrains.com>
2018-02-11 21:47:16 +0300

    PY-28370 SSH Interpreter option for the new project window restored

--- python/src/com/jetbrains/python/sdk/add/PyAddSdkProvider.kt
+++ python/src/com/jetbrains/python/sdk/add/PyAddSdkProvider.kt
@@ -9,7 +9,7 @@ interface PyAddSdkProvider {
   /**
    * Returns [PyAddSdkView] if applicable.
    */
-  fun createView(project: Project?, existingSdks: List<Sdk>): PyAddSdkView?
+  fun createView(project: Project?, newProjectPath: String?, existingSdks: List<Sdk>): PyAddSdkView?
 
   companion object {
     val EP_NAME: ExtensionPointName<PyAddSdkProvider> = ExtensionPointName.create("Pythonid.pyAddSdkProvider")
