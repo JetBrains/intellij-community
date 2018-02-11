795807dffb8e81398f5bb16c9ce53847fbc68b7d
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2018-02-11 12:50:51 +0800

    Refactor "ChangesModuleGroupingPolicy" and factory
    (cherry picked from commit a7f92b5ecc919032b6f7ff1626dbfe46dd543bbe)

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/ChangesModuleGroupingPolicy.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/ChangesModuleGroupingPolicy.kt
@@ -6,52 +6,52 @@ import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.util.Comparing
+import com.intellij.openapi.util.NotNullLazyKey
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.vcs.changes.ui.DirectoryChangesGroupingPolicy.Companion.DIRECTORY_POLICY
-import java.util.*
 import javax.swing.tree.DefaultTreeModel
 
+private const val PROJECT_ROOT_TAG = "<Project Root>"
+private val HIDE_EXCLUDED_FILES = Registry.`is`("ide.hide.excluded.files")
+
 class ChangesModuleGroupingPolicy(val myProject: Project, val myModel: DefaultTreeModel) : ChangesGroupingPolicy {
-  private val myModuleCache = HashMap<Module?, ChangesBrowserNode<*>>()
   private val myIndex = ProjectFileIndex.getInstance(myProject)
 
   override fun getParentNodeFor(nodePath: StaticFilePath, subtreeRoot: ChangesBrowserNode<*>): ChangesBrowserNode<*>? {
     if (myProject.isDefault) return null
 
     val vFile = nodePath.resolve()
-    if (vFile != null && Comparing.equal(vFile, myIndex.getContentRootForFile(vFile, hideExcludedFiles()))) {
-      val module = myIndex.getModuleForFile(vFile, hideExcludedFiles())
+    if (vFile != null && Comparing.equal(vFile, myIndex.getContentRootForFile(vFile, HIDE_EXCLUDED_FILES))) {
+      val module = myIndex.getModuleForFile(vFile, HIDE_EXCLUDED_FILES)
       return getNodeForModule(module, nodePath, subtreeRoot)
     }
     return null
   }
 
   private fun getNodeForModule(module: Module?, nodePath: StaticFilePath, subtreeRoot: ChangesBrowserNode<*>): ChangesBrowserNode<*> {
-    var node: ChangesBrowserNode<*>? = myModuleCache[module]
-    if (node == null) {
-      val policy = DIRECTORY_POLICY.get(subtreeRoot)
-      val parent = if (policy != null && !isTopLevel(nodePath)) policy.getParentNodeInternal(nodePath, subtreeRoot) else subtreeRoot
+    MODULE_CACHE.getValue(subtreeRoot)[module]?.let { return it }
 
-      node = if (module == null) ChangesBrowserNode.create(myProject, PROJECT_ROOT_TAG) else ChangesBrowserModuleNode(module)
-      myModel.insertNodeInto(node, parent, parent.childCount)
-      myModuleCache[module] = node
-    }
+    val policy = DIRECTORY_POLICY.get(subtreeRoot)
+    val parent = if (policy != null && !isTopLevel(nodePath)) policy.getParentNodeInternal(nodePath, subtreeRoot) else subtreeRoot
+    val node = if (module == null) ChangesBrowserNode.create(myProject, PROJECT_ROOT_TAG) else ChangesBrowserModuleNode(module)
+
+    myModel.insertNodeInto(node, parent, parent.childCount)
+    MODULE_CACHE.getValue(subtreeRoot)[module] = node
+
     return node
   }
 
   private fun isTopLevel(nodePath: StaticFilePath): Boolean {
-    val parentPath = nodePath.parent
-    val parentFile = parentPath?.resolve()
+    val parentFile = nodePath.parent?.resolve()
+    return parentFile == null || myIndex.getContentRootForFile(parentFile, HIDE_EXCLUDED_FILES) == null
+  }
 
-    return parentFile == null || myIndex.getContentRootForFile(parentFile, hideExcludedFiles()) == null
+  class Factory(val project: Project) : ChangesGroupingPolicyFactory() {
+    override fun createGroupingPolicy(model: DefaultTreeModel): ChangesGroupingPolicy = ChangesModuleGroupingPolicy(project, model)
   }
 
   companion object {
-
-    const val PROJECT_ROOT_TAG = "<Project Root>"
-
-    private fun hideExcludedFiles(): Boolean {
-      return Registry.`is`("ide.hide.excluded.files")
-    }
+    val MODULE_CACHE = NotNullLazyKey.create<MutableMap<Module?, ChangesBrowserNode<*>>, ChangesBrowserNode<*>>(
+      "ChangesTree.ModuleCache") { _ -> mutableMapOf() }
   }
 }
