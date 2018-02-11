04457f6f23a36338add86d03b52df434b7b811b6
Alexander Koshevoy <Alexander.Koshevoy@jetbrains.com>
2018-02-12 00:21:46 +0300

    PY-28370 WizardView merged into PyAddSdkView
    Resolving the comment from the review IDEA-CR-29088.

--- python/src/com/jetbrains/python/sdk/add/PyAddSdkView.kt
+++ python/src/com/jetbrains/python/sdk/add/PyAddSdkView.kt
@@ -2,14 +2,18 @@
 package com.jetbrains.python.sdk.add
 
 import com.intellij.openapi.projectRoots.Sdk
-import com.jetbrains.python.sdk.add.wizard.WizardView
+import com.intellij.openapi.ui.ValidationInfo
+import com.jetbrains.python.sdk.add.wizard.WizardControlAction
+import com.jetbrains.python.sdk.add.wizard.WizardControlsListener
+import com.jetbrains.python.sdk.add.wizard.WizardStateListener
+import java.awt.Component
 import javax.swing.Icon
 
 /**
  * Represents the view for adding new Python SDK. It is used in
  * [PyAddSdkDialog].
  */
-interface PyAddSdkView : WizardView<Sdk> {
+interface PyAddSdkView {
   val panelName: String
 
   val icon: Icon
@@ -17,4 +21,46 @@ interface PyAddSdkView : WizardView<Sdk> {
   fun getOrCreateSdk(): Sdk?
 
   fun onSelected()
+
+  /**
+   * [WizardControlsListener.onStateChanged] is called right after these changes.
+   */
+  val actions: Map<WizardControlAction, Boolean>
+
+  /**
+   * The [component] *might* return the new [Component] after [next] or
+   * [previous].
+   */
+  val component: Component
+
+  /**
+   * @throws IllegalStateException
+   */
+  fun previous()
+
+  /**
+   * @throws IllegalStateException
+   */
+  fun next()
+
+  /**
+   * Creates SDK and returns it.
+   *
+   * If some error occurs an [Exception] is thrown.
+   *
+   * @throws Exception if SDK creation failed for some reason
+   */
+  fun finish(): Sdk
+
+  /**
+   * Returns the list of validation errors. The returned list is empty if there
+   * are no errors found.
+   *
+   * @see com.intellij.openapi.ui.DialogWrapper.doValidateAll
+   */
+  fun validateAll(): List<ValidationInfo>
+
+  fun addStateListener(stateListener: WizardStateListener)
+
+  fun addControlListener(listener: WizardControlsListener)
 }
