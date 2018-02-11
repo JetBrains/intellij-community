cbe89619c56409cd36f9517f6f9e6328581201b1
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2018-02-11 16:34:12 +0700

    JavaOverridingMethodsSearcher: use walking visitor

--- java/java-indexing-impl/src/com/intellij/psi/impl/search/JavaOverridingMethodsSearcher.java
+++ java/java-indexing-impl/src/com/intellij/psi/impl/search/JavaOverridingMethodsSearcher.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.impl.search;
 
 import com.intellij.ide.highlighter.JavaFileType;
@@ -101,15 +87,15 @@ public class JavaOverridingMethodsSearcher implements QueryExecutor<PsiMethod, O
         public void run() {
           PsiFile psiFile = PsiManager.getInstance(project).findFile(virtualFile);
           if (psiFile != null) {
-            psiFile.accept(new JavaRecursiveElementVisitor() {
+            psiFile.accept(new JavaRecursiveElementWalkingVisitor() {
               @Override
               public void visitClass(PsiClass candidate) {
                 ProgressManager.checkCanceled();
-                if (!success[0]) return;
                 PsiMethod overridingMethod = candidate.isInheritor(methodContainingClass, true)
                                              ? findOverridingMethod(candidate, method, methodContainingClass) : null;
                 if (overridingMethod != null && !consumer.process(overridingMethod)) {
                   success[0] = false;
+                  stopWalking();
                 }
                 else {
                   super.visitClass(candidate);
