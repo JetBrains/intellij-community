5805ca9d22339653b33719c790005e89531f1514
Aleksey Pivovarov <AMPivovarov@gmail.com>
2018-02-12 15:25:37 +0300

    EA-116019 - assert: ComponentManagerImpl.lambda$throwAlreadyDisposed$
    * perform under ReadAction to avoid "Project already disposed"
    * ContentRevisionCache takes ReadAction inside anyway

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ChangeListManagerImpl.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ChangeListManagerImpl.java
@@ -512,12 +512,13 @@ public class ChangeListManagerImpl extends ChangeListManagerEx implements Projec
         updateIgnoredFiles(dataHolder.getComposite());
       }
 
-      clearCurrentRevisionsCache(invalidated);
       // for the case of project being closed we need a read action here -> to be more consistent
       ApplicationManager.getApplication().runReadAction(() -> {
         if (myProject.isDisposed()) {
           return;
         }
+        clearCurrentRevisionsCache(invalidated);
+
         synchronized (myDataLock) {
           // do same modifications to change lists as was done during update + do delayed notifications
           dataHolder.notifyEnd();
