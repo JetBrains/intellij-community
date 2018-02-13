b248bb208be0e4cabe82dae4fb6d9917d4508899
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2018-02-13 10:42:35 +0300

    remove unused value written at serialization of EditorsSplitters

--- java/java-tests/testSrc/com/intellij/java/openapi/editor/impl/JavaFileEditorManagerTest.java
+++ java/java-tests/testSrc/com/intellij/java/openapi/editor/impl/JavaFileEditorManagerTest.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.java.openapi.editor.impl;
 
 import com.intellij.codeHighlighting.Pass;
@@ -41,7 +27,7 @@ public class JavaFileEditorManagerTest extends FileEditorManagerTestCase {
   public void testAsyncOpening() throws JDOMException, ExecutionException, InterruptedException, IOException {
     openFiles("<component name=\"FileEditorManager\">\n" +
               "    <leaf>\n" +
-              "      <file leaf-file-name=\"Bar.java\" pinned=\"false\" current=\"true\" current-in-tab=\"true\">\n" +
+              "      <file pinned=\"false\" current=\"true\" current-in-tab=\"true\">\n" +
               "        <entry file=\"file://$PROJECT_DIR$/src/Bar.java\">\n" +
               "          <provider selected=\"true\" editor-type-id=\"text-editor\">\n" +
               "            <state vertical-scroll-proportion=\"0.032882012\" vertical-offset=\"0\" max-vertical-offset=\"517\">\n" +
