b13dac2484a9c3b488ca50fddf11b5c7db57633e
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-02-13 16:59:58 +0100

    IDEA-CR-28749 support final fields with Set type

--- platform/util/src/com/intellij/util/xmlb/BeanBinding.java
+++ platform/util/src/com/intellij/util/xmlb/BeanBinding.java
@@ -368,7 +368,7 @@ public class BeanBinding extends NotNullDeserializeBinding {
 
     if (setter == null) {
       // check hasStoreAnnotations to ensure that this addition will not lead to regression (since there is a chance that there is some existing not-annotated list getters without setter)
-      return (List.class.isAssignableFrom(getter.getReturnType()) || Map.class.isAssignableFrom(getter.getReturnType())) && hasStoreAnnotations(getter);
+      return (Collection.class.isAssignableFrom(getter.getReturnType()) || Map.class.isAssignableFrom(getter.getReturnType())) && hasStoreAnnotations(getter);
     }
 
     if (setter.getAnnotation(Transient.class) != null || !getter.getReturnType().equals(setter.getParameterTypes()[0])) {
