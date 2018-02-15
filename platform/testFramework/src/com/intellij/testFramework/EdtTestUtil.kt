6243771a71c6c1e40fc01d6274315db38bc0cfa0
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2018-02-15 14:06:36 +0100

    Cleanup (VFS tests: JUnit 4, temp. directory, NIO 2)

--- platform/testFramework/src/com/intellij/testFramework/EdtTestUtil.kt
+++ platform/testFramework/src/com/intellij/testFramework/EdtTestUtil.kt
@@ -1,22 +1,9 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.testFramework
 
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.impl.ApplicationImpl
+import com.intellij.openapi.util.ThrowableComputable
 import com.intellij.util.ExceptionUtil
 import com.intellij.util.ThrowableRunnable
 import org.jetbrains.annotations.TestOnly
@@ -25,6 +12,9 @@ import javax.swing.SwingUtilities
 
 class EdtTestUtil {
   companion object {
+    @TestOnly @JvmStatic fun <V> runInEdtAndGet(computable: ThrowableComputable<V, Throwable>): V =
+      runInEdtAndGet { computable.compute() }
+
     @TestOnly @JvmStatic fun runInEdtAndWait(runnable: ThrowableRunnable<Throwable>) {
       runInEdtAndWait { runnable.run() }
     }
