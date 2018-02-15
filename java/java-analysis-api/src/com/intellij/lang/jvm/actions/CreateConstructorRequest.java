e423613412cacfd6341384a0d6ec222304ebb1bf
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2018-02-15 14:50:57 +0300

    [jvm-lang] convert api interfaces to java step 2: actual conversion

--- java/java-analysis-api/src/com/intellij/lang/jvm/actions/CreateConstructorRequest.java
+++ java/java-analysis-api/src/com/intellij/lang/jvm/actions/CreateConstructorRequest.java
@@ -1,4 +1,5 @@
-// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.lang.jvm.actions
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.lang.jvm.actions;
 
-interface CreateConstructorRequest : CreateExecutableRequest
+public interface CreateConstructorRequest extends CreateExecutableRequest {
+}
--- java/java-analysis-api/src/com/intellij/lang/jvm/actions/CreateMethodRequest.java
+++ java/java-analysis-api/src/com/intellij/lang/jvm/actions/CreateMethodRequest.java
@@ -1,9 +1,15 @@
-// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.lang.jvm.actions
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.lang.jvm.actions;
 
-interface CreateMethodRequest : CreateExecutableRequest {
+import org.jetbrains.annotations.NotNull;
 
-  val methodName: String
+import java.util.List;
 
-  val returnType: ExpectedTypes
+public interface CreateMethodRequest extends CreateExecutableRequest {
+
+  @NotNull
+  String getMethodName();
+
+  @NotNull
+  List<ExpectedType> getReturnType();
 }
