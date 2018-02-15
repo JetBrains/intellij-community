dcf2123d73421cf99a3d55839e47e6e7ef3acfa3
nik <Nikolay.Chashnikov@jetbrains.com>
2018-02-15 14:51:12 +0300

    build scripts: remove duplicated information about licenses
    Also test added to check that there will be no duplicates added.

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/CommunityLibraryLicenses.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/CommunityLibraryLicenses.groovy
@@ -77,7 +77,7 @@ class CommunityLibraryLicenses {
                        license: "BSD", url: "http://asm.objectweb.org/", licenseUrl: "http://asm.objectweb.org/license.html"),
     new LibraryLicense(name: "Automaton", libraryName: "automaton", version: "1.11", license: "BSD", url: "http://www.brics.dk/automaton/",
                        licenseUrl: "http://www.opensource.org/licenses/bsd-license.php"),
-    new LibraryLicense(name: "Axis", libraryName: "axis-1.4", version: "1.4", license: "Apache 2.0", url: "http://ws.apache.org/axis/",
+    new LibraryLicense(name: "Apache Axis", libraryName: "axis-1.4", version: "1.4", license: "Apache 2.0", url: "http://ws.apache.org/axis/",
                        licenseUrl: "http://www.apache.org/licenses/LICENSE-2.0.html"),
     new LibraryLicense(name: "bouncy-castle", version: "1.48", license: "MIT License", url: "http://bouncycastle.org",
                        licenseUrl: "http://bouncycastle.org/licence.html"),
