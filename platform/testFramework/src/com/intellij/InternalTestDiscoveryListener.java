b34161a9bcb25028d328b6c0b10594ef9f9e9b6a
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2018-02-15 19:09:06 +0300

    extract TestDiscoveryBasicListener
    - don't care about local indices
    - just call appropriate end points on the test discovery side

--- platform/testFramework/src/com/intellij/InternalTestDiscoveryListener.java
+++ platform/testFramework/src/com/intellij/InternalTestDiscoveryListener.java
@@ -4,11 +4,7 @@ package com.intellij;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.util.io.FileUtil;
-import com.intellij.util.ArrayUtil;
 import com.intellij.util.io.ZipUtil;
-import junit.framework.AssertionFailedError;
-import junit.framework.Test;
-import junit.framework.TestListener;
 
 import java.io.Closeable;
 import java.io.File;
@@ -19,7 +15,7 @@ import java.lang.reflect.Method;
  * Used in TestAll to collect data in command line
  */
 @SuppressWarnings({"unused", "UseOfSystemOutOrSystemErr", "CallToPrintStackTrace"})
-public class InternalTestDiscoveryListener implements TestListener, Closeable {
+public class InternalTestDiscoveryListener extends TestDiscoveryBasicListener implements Closeable {
   private final String myModuleName;
   private final String myTracesFile;
   private Object myDiscoveryIndex;
@@ -47,57 +43,6 @@ public class InternalTestDiscoveryListener implements TestListener, Closeable {
       }
     }
     return myDiscoveryIndex;
-  }
-
-  @Override
-  public void addError(Test test, Throwable t) {}
-
-  @Override
-  public void addFailure(Test test, AssertionFailedError t) {}
-
-  @Override
-  public void endTest(Test test) {
-    final String className = getClassName(test);
-    final String methodName = getMethodName(test);
-
-    try {
-      Object data = getData();
-      Method testEnded = data.getClass().getMethod("testDiscoveryEnded", String.class, String.class);
-      testEnded.invoke(data, className, methodName);
-    }
-    catch (Throwable t) {
-      t.printStackTrace();
-    }
-  }
-
-  private static String getMethodName(Test test) {
-    final String toString = test.toString();
-    final int braceIdx = toString.indexOf("(");
-    return braceIdx > 0 ? toString.substring(0, braceIdx) : toString;
-  }
-
-  private static String getClassName(Test test) {
-    final String toString = test.toString();
-    final int braceIdx = toString.indexOf("(");
-    return braceIdx > 0 && toString.endsWith(")") ? toString.substring(braceIdx + 1, toString.length() - 1) : null;
-  }
-
-  @Override
-  public void startTest(Test test) {
-    try {
-      Object data = getData();
-      Method testStarted = data.getClass().getMethod("testDiscoveryStarted", String.class, String.class);
-      testStarted.invoke(data, getClassName(test), getMethodName(test));
-    }
-    catch (Throwable t) {
-      t.printStackTrace();
-    }
-  }
-
-  protected Object getData() throws Exception {
-    return Class.forName("com.intellij.rt.coverage.data.TestDiscoveryProjectData")
-                .getMethod("getProjectData", ArrayUtil.EMPTY_CLASS_ARRAY)
-                .invoke(null, ArrayUtil.EMPTY_OBJECT_ARRAY);
   }
 
   @Override
