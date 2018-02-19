6afabb4486d6fec1278679ab7da828aff47af4ce
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2018-02-19 13:28:02 +0800

    Sync arrow button width for comboBox and option button in Darcula

--- platform/platform-impl/src/com/intellij/ide/ui/laf/darcula/ui/DarculaComboBoxUI.java
+++ platform/platform-impl/src/com/intellij/ide/ui/laf/darcula/ui/DarculaComboBoxUI.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.ui.laf.darcula.ui;
 
 import com.intellij.ide.ui.laf.darcula.DarculaUIUtil;
@@ -26,6 +12,7 @@ import com.intellij.util.ui.JBInsets;
 import com.intellij.util.ui.JBUI;
 import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import sun.swing.DefaultLookup;
 
 import javax.swing.*;
@@ -142,8 +129,7 @@ public class DarculaComboBoxUI extends BasicComboBoxUI implements Border, ErrorB
 
       @Override
       public Dimension getPreferredSize() {
-        Insets i = comboBox.getInsets();
-        return new Dimension(JBUI.scale(16) + i.left, JBUI.scale(22) + i.top + i.bottom);
+        return getArrowButtonPreferredSize(comboBox);
       }
     };
     button.setBorder(JBUI.Borders.empty());
@@ -153,6 +139,12 @@ public class DarculaComboBoxUI extends BasicComboBoxUI implements Border, ErrorB
 
   protected Color getArrowButtonFillColor(Color defaultColor) {
     return DarculaUIUtil.getArrowButtonFillColor(comboBox.hasFocus(), comboBox.isEnabled(), defaultColor);
+  }
+
+  @NotNull
+  static Dimension getArrowButtonPreferredSize(@Nullable JComboBox comboBox) {
+    Insets i = comboBox != null ? comboBox.getInsets() : getDefaultComboBoxInsets();
+    return new Dimension(JBUI.scale(16) + i.left, JBUI.scale(22) + i.top + i.bottom);
   }
 
   static Shape getArrowShape(Component button) {
@@ -173,9 +165,14 @@ public class DarculaComboBoxUI extends BasicComboBoxUI implements Border, ErrorB
     return path;
   }
 
+  @NotNull
+  private static JBInsets getDefaultComboBoxInsets() {
+    return JBUI.insets(4, 8, 4, 4);
+  }
+
   @Override
   protected Insets getInsets() {
-    return JBUI.insets(4, 8, 4, 4).asUIResource();
+    return getDefaultComboBoxInsets().asUIResource();
   }
 
   @Override
