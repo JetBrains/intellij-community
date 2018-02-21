e6e657221158016c8e9e3218ae96dad112c1b028
Dmitry.Krasilschikov <kradima@jetbrains.com>
2018-02-21 18:29:21 +0300

    IDEA-CR-29139 porting console foldings to new project aware model
    (cherry picked from commit f81f9503420f1c60f7d2262bbd9f91ff6222e56a)

--- platform/lang-impl/src/com/intellij/execution/console/SubstringConsoleFolding.java
+++ platform/lang-impl/src/com/intellij/execution/console/SubstringConsoleFolding.java
@@ -1,6 +1,7 @@
 package com.intellij.execution.console;
 
 import com.intellij.execution.ConsoleFolding;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.text.StringUtil;
 import org.jetbrains.annotations.NotNull;
 
@@ -17,12 +18,12 @@ public class SubstringConsoleFolding extends ConsoleFolding {
   }
 
   @Override
-  public boolean shouldFoldLine(@NotNull String line) {
+  public boolean shouldFoldLine(@NotNull Project project, @NotNull String line) {
     return mySettings.shouldFoldLine(line);
   }
 
   @Override
-  public String getPlaceholderText(@NotNull List<String> lines) {
+  public String getPlaceholderText(@NotNull Project project, @NotNull List<String> lines) {
     return " <" + lines.size() + " internal " + StringUtil.pluralize("call", lines.size()) + ">";
   }
 }
