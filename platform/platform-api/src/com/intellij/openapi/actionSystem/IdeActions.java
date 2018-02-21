2ffeb4acc5fb04de35685019cc086a62e7dccece
Konstantin Aleev <konstantin.aleev@jetbrains.com>
2018-02-21 13:33:00 +0300

    define constant for Update Running Application action id

--- platform/platform-api/src/com/intellij/openapi/actionSystem/IdeActions.java
+++ platform/platform-api/src/com/intellij/openapi/actionSystem/IdeActions.java
@@ -342,4 +342,6 @@ public interface IdeActions {
 
   @NonNls String ACTION_METHOD_OVERLOAD_SWITCH_UP = "MethodOverloadSwitchUp";
   @NonNls String ACTION_METHOD_OVERLOAD_SWITCH_DOWN = "MethodOverloadSwitchDown";
+
+  @NonNls String ACTION_UPDATE_RUNNING_APPLICATION = "UpdateRunningApplication";
 }
