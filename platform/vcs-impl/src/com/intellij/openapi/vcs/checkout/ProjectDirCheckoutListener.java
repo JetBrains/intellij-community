7321111c3808ee465845d13d31df1303dc4a1da6
Anton Makeev <Anton.Makeev@jetbrains.com>
2018-02-22 16:27:50 +0100

    WI-40852 Checkout from VCS produces invalid project on first opening
    Caused by incomplete '.idea' folder (missing modules.xml etc).
    Fixed by using different API (that is also used when opening projects via 'File | Open' and from Recent projects)
    
    original change:
    CPP-11585 When checking out a project from VCS, auto-detect CmakeLists.txt and suggest to load it

--- platform/vcs-impl/src/com/intellij/openapi/vcs/checkout/ProjectDirCheckoutListener.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/checkout/ProjectDirCheckoutListener.java
@@ -1,12 +1,15 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vcs.checkout;
 
-import com.intellij.ide.impl.ProjectUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vcs.VcsBundle;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.platform.PlatformProjectOpenProcessor;
 
 import java.io.File;
+import java.util.EnumSet;
 
 /**
  * @author irengrig
@@ -15,17 +18,22 @@ import java.io.File;
 public class ProjectDirCheckoutListener implements CheckoutListener {
   @Override
   public boolean processCheckedOutDirectory(Project project, File directory) {
-    // todo Rider project layout - several.idea.solution-name names
-    if (!new File(directory, Project.DIRECTORY_STORE_FOLDER).exists()) {
-      return false;
-    }
+    File ioDotIdea = new File(directory, Project.DIRECTORY_STORE_FOLDER);
 
+    VirtualFile dotIdea = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(ioDotIdea);
+    // todo Rider project layout - several.idea.solution-name names
+    if (dotIdea == null) return false;
+
+    
     String message = VcsBundle.message("checkout.open.project.dir.prompt",
                                        ProjectCheckoutListener.getProductNameWithArticle(), directory.getPath());
     if (Messages.showYesNoDialog(project, message, VcsBundle.message("checkout.title"), Messages.getQuestionIcon()) == Messages.YES) {
-      ProjectUtil.openProject(directory.getPath(), project, false);
+      PlatformProjectOpenProcessor.doOpenProject(dotIdea.getParent(), project, -1, null, 
+                                                 EnumSet.noneOf(PlatformProjectOpenProcessor.Option.class));
+      return true;
     }
-    return true;
+    
+    return false;
   }
 
   @Override
