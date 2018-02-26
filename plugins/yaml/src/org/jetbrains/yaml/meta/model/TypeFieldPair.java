e2591b719f79bcc8af383889401c538c2d6b4e56
Sergey.Anchipolevsky <sergey.anchipolevsky@jetbrains.com>
2018-02-26 17:34:41 +0300

    [yaml] tagged with @ApiStatus.Experimental (IDEA-CR-29807)
    (cherry picked from commit 87840d6)

--- plugins/yaml/src/org/jetbrains/yaml/meta/model/TypeFieldPair.java
+++ plugins/yaml/src/org/jetbrains/yaml/meta/model/TypeFieldPair.java
@@ -1,8 +1,10 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.yaml.meta.model;
 
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
+@ApiStatus.Experimental
 public class TypeFieldPair {
   @NotNull
   private final Field myField;
