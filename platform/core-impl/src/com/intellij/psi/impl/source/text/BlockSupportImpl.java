48e690c646dba084d9296e5b36d3ec034832e3ef
peter <peter@jetbrains.com>
2018-02-26 18:06:07 +0100

    [^cdr] BlockSupportImpl: fix NPE
    (cherry picked from commit 146f64c)

--- platform/core-impl/src/com/intellij/psi/impl/source/text/BlockSupportImpl.java
+++ platform/core-impl/src/com/intellij/psi/impl/source/text/BlockSupportImpl.java
@@ -212,7 +212,7 @@ public class BlockSupportImpl extends BlockSupport {
     if (fileImpl instanceof PsiCodeFragment) {
       FileElement parent = fileImpl.getTreeElement();
       PsiElement context = fileImpl.getContext();
-      FileElement holderElement = new DummyHolder(fileImpl.getManager(), context.isValid() ? context : null).getTreeElement();
+      FileElement holderElement = new DummyHolder(fileImpl.getManager(), context != null && context.isValid() ? context : null).getTreeElement();
       holderElement.rawAddChildren(fileImpl.createContentLeafElement(holderElement.getCharTable().intern(newFileText, 0, newFileText.length())));
       DiffLog diffLog = new DiffLog();
       diffLog.appendReplaceFileElement(parent, (FileElement)holderElement.getFirstChildNode());
