709e5a245c81922117a26ad5c4ae15b86506dafa
Alexey Gopachenko <neuro159@gmail.com>
2018-02-26 15:18:27 +0100

    Spellchecker - added debug info
    (cherry picked from commit 7cd47c7)

--- spellchecker/src/com/intellij/spellchecker/tokenizer/TokenizerBase.java
+++ spellchecker/src/com/intellij/spellchecker/tokenizer/TokenizerBase.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2011 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.spellchecker.tokenizer;
 
 import com.intellij.psi.PsiElement;
@@ -30,6 +16,11 @@ public class TokenizerBase<T extends PsiElement> extends Tokenizer<T> {
   }
   
   private final Splitter mySplitter;
+
+  @Override
+  public String toString() {
+    return "TokenizerBase(splitter=" + mySplitter+")";
+  }
 
   public TokenizerBase(Splitter splitter) {
     mySplitter = splitter;
