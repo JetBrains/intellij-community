ebc3e692e293805cc100419d45d6c38c2346aa11
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2018-02-26 18:00:44 +0300

    PY-28629 Test cleanup
    (cherry picked from commit 2a9839e)
    
    (cherry picked from commit 26f324d)

--- python/testSrc/com/jetbrains/python/PySkeletonResolveTest.java
+++ python/testSrc/com/jetbrains/python/PySkeletonResolveTest.java
@@ -1,7 +1,6 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.python;
 
-import com.intellij.psi.PsiElement;
 import com.intellij.testFramework.LightProjectDescriptor;
 import com.jetbrains.python.fixtures.PyLightProjectDescriptor;
 import com.jetbrains.python.fixtures.PyMultiFileResolveTestCase;
@@ -13,10 +12,6 @@ import org.jetbrains.annotations.Nullable;
  */
 public class PySkeletonResolveTest extends PyMultiFileResolveTestCase {
   protected static final PyLightProjectDescriptor ourPyDescriptor = new PyLightProjectDescriptor("WithBinaryModules");
-  @Override
-  public void setUp() throws Exception {
-    super.setUp();
-  }
 
   @Nullable
   @Override
@@ -25,10 +20,7 @@ public class PySkeletonResolveTest extends PyMultiFileResolveTestCase {
   }
 
   //PY-28629
-  public void testOldNumpyRelativeImport(){
-    PsiElement element = doResolve();
-    assertTrue(element instanceof PyFunction);
-
-
+  public void testOldNumpyRelativeImport() {
+    assertInstanceOf(doResolve(), PyFunction.class);
   }
 }
