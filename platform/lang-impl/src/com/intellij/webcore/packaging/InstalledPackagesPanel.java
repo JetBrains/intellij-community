5816d7e3df42a76519921dce1c47c0cb97536604
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2018-02-27 12:00:56 +0300

    PY-28651 Added shortcuts for installing and deleting packages
    The shortcuts are based on our common shortcuts set dependent on the
    selected keymap.

--- platform/lang-impl/src/com/intellij/webcore/packaging/InstalledPackagesPanel.java
+++ platform/lang-impl/src/com/intellij/webcore/packaging/InstalledPackagesPanel.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Lists;
 import com.intellij.icons.AllIcons;
 import com.intellij.ide.ActivityTracker;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonShortcuts;
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
@@ -97,12 +98,14 @@ public class InstalledPackagesPanel extends JPanel {
         }
       }
     };
+    myInstallButton.setShortcut(CommonShortcuts.getNew());
     myUninstallButton = new AnActionButton("Uninstall", IconUtil.getRemoveIcon()) {
       @Override
       public void actionPerformed(@NotNull AnActionEvent e) {
         uninstallAction();
       }
     };
+    myUninstallButton.setShortcut(CommonShortcuts.getDelete());
     ToolbarDecorator decorator =
       ToolbarDecorator.createDecorator(myPackagesTable).disableUpDownActions().disableAddAction().disableRemoveAction()
         .addExtraAction(myInstallButton)
