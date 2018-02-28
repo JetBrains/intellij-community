1ab3bfd9f0456c20addb03b94bb2e59b8850c329
Dana Dahlstrom <dahlstrom@google.com>
2018-02-28 07:29:03 -0800

    Revert Change I618391d5
    … in order to cherry-pick commit 0df5153.
    
    Change-Id: I61ca8786b29ce4e64d3013261a5b550359b4dfdd

--- platform/platform-impl/src/com/intellij/ide/gdpr/ConsentOptions.java
+++ platform/platform-impl/src/com/intellij/ide/gdpr/ConsentOptions.java
@@ -6,7 +6,6 @@ package com.intellij.ide.gdpr;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.intellij.openapi.application.ApplicationNamesInfo;
-import com.intellij.openapi.application.impl.ApplicationInfoImpl;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.io.FileUtil;
@@ -27,6 +26,7 @@ import java.util.stream.Stream;
 public final class ConsentOptions {
   private static final Logger LOG = Logger.getInstance("#com.intellij.ide.gdpr.ConsentOptions");
   private static final String CONSENTS_CONFIRMATION_PROPERTY = "jb.consents.confirmation.enabled";
+  private static final String BUNDLED_RESOURCE_PATH = "/consents.json";
   private static final String STATISTICS_OPTION_ID = "rsch.send.usage.stat";
 
   private static final class InstanceHolder {
@@ -45,8 +45,7 @@ public final class ConsentOptions {
 
       @NotNull
       public String readBundledConsents() {
-        String resourceName = "/consents-" + ApplicationInfoImpl.getShadowInstance().getShortCompanyName() + ".json";
-        return loadText(ConsentOptions.class.getResourceAsStream(resourceName));
+        return loadText(ConsentOptions.class.getResourceAsStream(BUNDLED_RESOURCE_PATH));
       }
 
       public void writeConfirmedConsents(@NotNull String data) throws IOException {
