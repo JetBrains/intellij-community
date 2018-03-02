dac5f890f42297f267145386d238d7ee64706d9d
Ivan Bessonov <ivan.bessonov@jetbrains.com>
2018-03-02 16:05:59 +0300

    IDEA-154970 added support of Bintray repositories to MavenRepositoryServicesManager

---
+++ java/java-impl/src/com/intellij/jarRepository/services/bintray/BintrayModel.java
@@ -0,0 +1,36 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.jarRepository.services.bintray;
+
+import java.util.List;
+
+/**
+ * @author ibessonov
+ */
+public class BintrayModel {
+
+  public static class Repository {
+    public String subject;
+    public String repo;
+
+    public Repository(String subject, String repo) {
+      this.subject = subject;
+      this.repo = repo;
+    }
+
+    public String getUrl() {
+      return getUrl(subject, repo);
+    }
+
+    public static String getUrl(String subject, String repo) {
+      return "https://dl.bintray.com/" + subject + (repo != null ? "/" + repo : "");
+    }
+  }
+
+  public static class Package {
+    public String desc;
+    public String owner;
+    public String repo;
+    public List<String> system_ids;
+    public List<String> versions;
+  }
+}
