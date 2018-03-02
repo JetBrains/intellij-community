e5854e6a03edfb49e6e5a2a8b8b0fc0f6f04255c
Bas Leijdekkers <basleijdekkers@gmail.com>
2018-03-02 14:20:33 +0100

    SSR: better problem notification (part of fix for IDEA-141767)

--- platform/structuralsearch/source/com/intellij/structuralsearch/impl/matcher/predicates/ScriptLog.java
+++ platform/structuralsearch/source/com/intellij/structuralsearch/impl/matcher/predicates/ScriptLog.java
@@ -1,24 +1,9 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.structuralsearch.impl.matcher.predicates;
 
-import com.intellij.notification.NotificationGroup;
 import com.intellij.notification.NotificationType;
 import com.intellij.openapi.project.Project;
-import com.intellij.structuralsearch.SSRBundle;
+import com.intellij.structuralsearch.plugin.ui.UIUtil;
 
 /**
  * @author Bas Leijdekkers
@@ -27,7 +12,6 @@ public class ScriptLog {
 
   public static final String SCRIPT_LOG_VAR_NAME = "__log__";
 
-  private static final NotificationGroup myEventLog = NotificationGroup.logOnlyGroup(SSRBundle.message("structural.search.title"));
   private final Project myProject;
 
   public ScriptLog(Project project) {
@@ -56,6 +40,6 @@ public class ScriptLog {
         break;
       }
     }
-    myEventLog.createNotification(location + String.valueOf(message), type).notify(myProject);
+    UIUtil.SSR_NOTIFICATION_GROUP.createNotification(location + String.valueOf(message), type).notify(myProject);
   }
 }
