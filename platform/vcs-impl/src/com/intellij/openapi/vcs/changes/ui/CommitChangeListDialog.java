JetBrains/intellij-community

commit 5c156006ae6b034175b73b9ca28b56916edf2b26

IDEA-187108 vcs: do not include initially excluded files in commit dialog
Fix case when non-active changelist is selected by default.
We reset inclusion states in `setSelectedCahngeList`,
so inclusion state should be initialized after changelist.

follow-up: 533a5d9c2cc266b76d66373ee3c334f4e608051c

platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/CommitChangeListDialog.java
@@ -310,9 +310,10 @@ public class CommitChangeListDialog extends DialogWrapper implements CheckinProj
       MultipleLocalChangeListsBrowser browser = new MultipleLocalChangeListsBrowser(project, true, true, myShowVcsCommit);
       myBrowser = browser;
 
-      browser.getViewer().setIncludedChanges(included);
       if (initialSelection != null) browser.setSelectedChangeList(initialSelection);
       myCommitMessageArea.setChangeList(browser.getSelectedChangeList());
+
+      browser.getViewer().setIncludedChanges(included);
 
       DiffCommitMessageEditor commitMessageEditor = new DiffCommitMessageEditor(myProject, myCommitMessageArea);
       browser.setBottomDiffComponent(commitMessageEditor);

commit 11ad4829a70f7c8dcaaf9ac13b226ff2432b8b14

IDEA-187706 vcs: preselect first included file in tree in commit dialog
bug introduced in: 5c156006ae6b034175b73b9ca28b56916edf2b26

platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/CommitChangeListDialog.java
@@ -312,8 +312,9 @@ public class CommitChangeListDialog extends DialogWrapper implements CheckinProj
 
       if (initialSelection != null) browser.setSelectedChangeList(initialSelection);
       myCommitMessageArea.setChangeList(browser.getSelectedChangeList());
-
       browser.getViewer().setIncludedChanges(included);
+      browser.getViewer().rebuildTree();
+      browser.getViewer().setKeepTreeState(true);
 
       DiffCommitMessageEditor commitMessageEditor = new DiffCommitMessageEditor(myProject, myCommitMessageArea);
       browser.setBottomDiffComponent(commitMessageEditor);
@@ -329,7 +330,6 @@ public class CommitChangeListDialog extends DialogWrapper implements CheckinProj
       });
 
       browser.getViewer().addSelectionListener(() -> SwingUtilities.invokeLater(() -> changeDetails()));
-      browser.getViewer().setKeepTreeState(true);
     }
 
     myChangesInfoCalculator = new ChangeInfoCalculator();