JetBrains/intellij-community

commit 3d678404dffb31ee1d271230a8f522c893470308

IDEA-131357 git: speedup bulk merge resolve using "accept ours/theirs"
`MergeProvider.loadRevisions` and modifications via VFS can be up to 20 times slower.

plugins/git4idea/src/git4idea/merge/GitMergeProvider.java
@@ -39,10 +39,7 @@ import com.intellij.vcsUtil.VcsUtil;
 import git4idea.GitFileRevision;
 import git4idea.GitRevisionNumber;
 import git4idea.GitUtil;
-import git4idea.commands.GitCommand;
-import git4idea.commands.GitLineHandler;
-import git4idea.commands.GitLineHandlerAdapter;
-import git4idea.commands.GitSimpleHandler;
+import git4idea.commands.*;
 import git4idea.history.GitHistoryUtils;
 import git4idea.i18n.GitBundle;
 import git4idea.repo.GitRepository;
@@ -546,6 +543,36 @@ public class GitMergeProvider implements MergeProvider2 {
       catch (VcsException e) {
         LOG.error("Unexpected exception during the git operation (" + file.getPath() + ")", e);
       }
+    }
+
+    @Override
+    public boolean acceptFileRevision(@NotNull VirtualFile file, @NotNull MergeSession.Resolution resolution) throws VcsException {
+      if (resolution != Resolution.AcceptedYours && resolution != Resolution.AcceptedTheirs) return false;
+      boolean isCurrent = resolution == Resolution.AcceptedYours;
+
+      Conflict c = myConflicts.get(file);
+      if (c == null) {
+        LOG.error("Conflict was not loaded for the file: " + file.getPath());
+        return false;
+      }
+
+      Conflict.Status status = isCurrent ? c.myStatusYours : c.myStatusTheirs;
+      switch (status) {
+        case MODIFIED:
+          GitLineHandler handler = new GitLineHandler(myProject, c.myRoot, GitCommand.CHECKOUT);
+          handler.addParameters(isCurrent ? "--ours" : "--theirs");
+          handler.endOptions();
+          handler.addRelativeFiles(Collections.singletonList(c.myFile));
+          GitCommandResult result = Git.getInstance().runCommand(handler);
+          if (!result.success()) throw new VcsException(result.getErrorOutputAsJoinedString());
+          break;
+        case DELETED:
+          break;
+        default:
+          throw new IllegalArgumentException("Unsupported status(" + file.getPath() + "): " + status);
+      }
+
+      return true;
     }
 
     /**

platform/vcs-api/src/com/intellij/openapi/vcs/merge/MergeSession.java
@@ -15,6 +15,7 @@
  */
 package com.intellij.openapi.vcs.merge;
 
+import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.ui.ColumnInfo;
 import org.jetbrains.annotations.NotNull;
@@ -58,4 +59,14 @@ public interface MergeSession {
    */
   void conflictResolvedForFile(@NotNull VirtualFile file, @NotNull Resolution resolution);
 
+  /**
+   * Called before {@link #conflictResolvedForFile} when the user executes "Accept Theirs" or "Accept Ours" action
+   * to update file content to the selected version.
+   *
+   * @param resolution AcceptedYours or AcceptedTheirs
+   * @return true if operation was performed, false if we should fall back to generic implementation.
+   */
+  default boolean acceptFileRevision(@NotNull VirtualFile file, @NotNull MergeSession.Resolution resolution) throws VcsException {
+    return false;
+  }
 }

platform/vcs-impl/src/com/intellij/openapi/vcs/merge/MultipleFileMergeDialog.java
@@ -163,13 +163,13 @@ public class MultipleFileMergeDialog extends DialogWrapper {
     myAcceptYoursButton.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(@NotNull ActionEvent e) {
-        acceptRevision(true);
+        acceptRevision(MergeSession.Resolution.AcceptedYours);
       }
     });
     myAcceptTheirsButton.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(@NotNull ActionEvent e) {
-        acceptRevision(false);
+        acceptRevision(MergeSession.Resolution.AcceptedTheirs);
       }
     });
     myTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
@@ -252,7 +252,7 @@ public class MultipleFileMergeDialog extends DialogWrapper {
     return "MultipleFileMergeDialog";
   }
 
-  private void acceptRevision(final boolean isCurrent) {
+  private void acceptRevision(@NotNull MergeSession.Resolution resolution) {
     FileDocumentManager.getInstance().saveAllDocuments();
     final Collection<VirtualFile> files = myTable.getSelection();
     if (!beforeResolve(files)) {
@@ -261,8 +261,9 @@ public class MultipleFileMergeDialog extends DialogWrapper {
 
     try {
       for (VirtualFile file : files) {
-        acceptFileRevision(file, isCurrent);
-        markFileProcessed(file, isCurrent ? MergeSession.Resolution.AcceptedYours : MergeSession.Resolution.AcceptedTheirs);
+        acceptFileRevision(file, resolution);
+        checkMarkModifiedProject(file);
+        markFileProcessed(file, resolution);
       }
     }
     catch (Exception e) {
@@ -273,13 +274,16 @@ public class MultipleFileMergeDialog extends DialogWrapper {
     updateModelFromFiles();
   }
 
-  private void acceptFileRevision(@NotNull VirtualFile file, boolean isCurrent) throws Exception {
+  private void acceptFileRevision(@NotNull VirtualFile file, @NotNull MergeSession.Resolution resolution) throws Exception {
     if (myMergeSession != null && !myMergeSession.canMerge(file)) return;
+
+    if (myMergeSession != null && myMergeSession.acceptFileRevision(file, resolution)) return;
 
     if (!DiffUtil.makeWritable(myProject, file)) {
       throw new IOException("File is read-only: " + file.getPresentableName());
     }
 
+    boolean isCurrent = resolution == MergeSession.Resolution.AcceptedYours;
     MergeData data = myProvider.loadRevisions(file);
 
     Ref<Exception> ex = new Ref<>();
@@ -291,7 +295,6 @@ public class MultipleFileMergeDialog extends DialogWrapper {
           }
           else {
             file.setBinaryContent(data.LAST);
-            checkMarkModifiedProject(file);
           }
         }
         catch (Exception e) {

commit 514edcfae192354c694f40828d347d3b893b5c45

IDEA-187702 git: fix inverted "accept theirs/ours" actions in merge conflict dialog during rebase
During the rebase, we may invert these two terms because of c3a12efed.
In this case, "Accept Theirs" acton should mimic "git checkout --ours file"
and not "git checkout --theirs" as one could assume.

This also fixes "error: path 'file.txt' does not have our version"
for Deleted-Modified conflicts (as we can't successfully checkout wrong version).

bug introduced in: 3d678404dffb31ee1d271230a8f522c893470308

plugins/git4idea/src/git4idea/merge/GitMergeProvider.java
@@ -559,8 +559,12 @@ public class GitMergeProvider implements MergeProvider2 {
       Conflict.Status status = isCurrent ? c.myStatusYours : c.myStatusTheirs;
       switch (status) {
         case MODIFIED:
+          String parameter = myReverseRoots.contains(c.myRoot)
+                             ? isCurrent ? "--theirs" : "--ours"
+                             : isCurrent ? "--ours" : "--theirs";
+
           GitLineHandler handler = new GitLineHandler(myProject, c.myRoot, GitCommand.CHECKOUT);
-          handler.addParameters(isCurrent ? "--ours" : "--theirs");
+          handler.addParameters(parameter);
           handler.endOptions();
           handler.addRelativeFiles(Collections.singletonList(c.myFile));
           GitCommandResult result = Git.getInstance().runCommand(handler);