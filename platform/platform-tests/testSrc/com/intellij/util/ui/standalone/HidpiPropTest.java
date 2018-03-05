af7f2d8a32fe99d3b259d9b4f053997e0fe6afe1
Anton Tarasov <anton.tarasov@jetbrains.com>
2018-03-05 19:16:16 +0300

    Add more standalone ui tests

--- platform/platform-tests/testSrc/com/intellij/util/ui/standalone/HidpiPropTest.java
+++ platform/platform-tests/testSrc/com/intellij/util/ui/standalone/HidpiPropTest.java
@@ -1,8 +1,10 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.util.ui;
+package com.intellij.util.ui.standalone;
 
 import com.intellij.idea.StartupUtil;
 import com.intellij.util.SystemProperties;
+import com.intellij.util.ui.TestScaleHelper;
+import com.intellij.util.ui.UIUtil;
 import org.junit.*;
 
 import java.awt.*;
@@ -15,26 +17,14 @@ import java.awt.*;
 public class HidpiPropTest {
   static final String HIDPI_PROP = "hidpi";
   static final String UI_SCALE_PROP = "sun.java2d.uiScale.enabled";
-  static final String STANDALONE_PROP = "intellij.test.standalone";
-
-  @Before
-  public void checkStandalone() {
-    Assume.assumeTrue("not in " + STANDALONE_PROP + " mode", SystemProperties.is(STANDALONE_PROP));
-  }
-
-  @Before
-  public void setState() {
-    TestScaleHelper.setProperty(HIDPI_PROP, "false");
-    StartupUtil.test_checkHiDPISettings();
-  }
-
-  @After
-  public void restoreState() {
-    TestScaleHelper.restoreProperties();
-  }
 
   @Test
   public void test() {
+    TestScaleHelper.assumeStandalone();
+
+    System.setProperty(HIDPI_PROP, "false");
+    StartupUtil.test_checkHiDPISettings();
+
     Graphics2D g = TestScaleHelper.createGraphics(2);
 
     Assert.assertFalse(UI_SCALE_PROP + " should be disabled", SystemProperties.is(UI_SCALE_PROP));
