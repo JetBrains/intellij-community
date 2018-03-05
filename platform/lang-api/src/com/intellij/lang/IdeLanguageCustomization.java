2c5e1111cc1867bbc99c5e2e823ae9c86a5684a1
nik <Nikolay.Chashnikov@jetbrains.com>
2018-03-05 15:02:03 +0300

    platform: add @Nullable annotation to 'getMainIdeLanguage'

--- platform/lang-api/src/com/intellij/lang/IdeLanguageCustomization.java
+++ platform/lang-api/src/com/intellij/lang/IdeLanguageCustomization.java
@@ -2,6 +2,7 @@
 package com.intellij.lang;
 
 import com.intellij.openapi.components.ServiceManager;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Override this service in your IDE to set which language is considered as the main language. It isn't supposed to be overridden in plugins.
@@ -15,6 +16,7 @@ public class IdeLanguageCustomization {
    * Returns the main language for which the IDE is supposed to be used or {@code null} if there is no single main language. This method is
    * used to customize IDE's UI, e.g. to move settings pages related to the main language to the top.
    */
+  @Nullable
   public Language getMainIdeLanguage() {
     return null;
   }
