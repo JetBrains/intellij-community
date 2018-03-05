ba9689ca32c93c6e6946d0414546be46cd4370d9
Bas Leijdekkers <basleijdekkers@gmail.com>
2018-03-05 15:51:11 +0100

    SSR: show variable name instead of filename in log

--- platform/structuralsearch/source/com/intellij/structuralsearch/impl/matcher/predicates/ScriptSupport.java
+++ platform/structuralsearch/source/com/intellij/structuralsearch/impl/matcher/predicates/ScriptSupport.java
@@ -24,7 +24,10 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Maxim.Mossienko
@@ -35,7 +38,7 @@ public class ScriptSupport {
    * We use a randomly generated uuid for this, so the chance of accidental collision with an existing variable name is extremely small.
    * This also enables to filter out this uuid from Groovy error messages, to clarify for which SSR variable the script failed.
    */
-  private static final String UUID = "a3cd264774bf4efb9ab609b250c5165c";
+  static final String UUID = "a3cd264774bf4efb9ab609b250c5165c";
 
   private final Script script;
   private final ScriptLog myScriptLog;
