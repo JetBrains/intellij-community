19699cde614c399481e5ccfa4a2be4447f8c57c5
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2018-03-06 17:58:09 +0300

    Fix and unify calculating corresponding loop for `break` and `continue` (PY-23189)

--- python/src/com/jetbrains/python/psi/impl/PyBreakStatementImpl.java
+++ python/src/com/jetbrains/python/psi/impl/PyBreakStatementImpl.java
@@ -16,10 +16,10 @@
 package com.jetbrains.python.psi.impl;
 
 import com.intellij.lang.ASTNode;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.util.PsiTreeUtil;
-import com.jetbrains.python.psi.*;
-import org.jetbrains.annotations.NotNull;
+import com.jetbrains.python.psi.PyBreakStatement;
+import com.jetbrains.python.psi.PyElementVisitor;
+import com.jetbrains.python.psi.PyLoopStatement;
+import com.jetbrains.python.psi.PyUtil;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -37,19 +37,6 @@ public class PyBreakStatementImpl extends PyElementImpl implements PyBreakStatem
 
   @Nullable
   public PyLoopStatement getLoopStatement() {
-    return getLoopStatement(this);
-  }
-
-  @Nullable
-  private static PyLoopStatement getLoopStatement(@NotNull PsiElement element) {
-    final PyLoopStatement loop = PsiTreeUtil.getParentOfType(element, PyLoopStatement.class);
-    if (loop instanceof PyStatementWithElse) {
-      final PyStatementWithElse stmt = (PyStatementWithElse)loop;
-      final PyElsePart elsePart = stmt.getElsePart();
-      if (PsiTreeUtil.isAncestor(elsePart, element, true)) {
-        return getLoopStatement(loop);
-      }
-    }
-    return loop;
+    return PyUtil.getCorrespondingLoop(this);
   }
 }
--- python/src/com/jetbrains/python/validation/BreakContinueAnnotator.java
+++ python/src/com/jetbrains/python/validation/BreakContinueAnnotator.java
@@ -16,8 +16,10 @@
 package com.jetbrains.python.validation;
 
 import com.intellij.psi.util.PsiTreeUtil;
-import com.jetbrains.python.psi.*;
-import org.jetbrains.annotations.Nullable;
+import com.jetbrains.python.psi.PyBreakStatement;
+import com.jetbrains.python.psi.PyContinueStatement;
+import com.jetbrains.python.psi.PyFinallyPart;
+import com.jetbrains.python.psi.PyLoopStatement;
 
 import static com.jetbrains.python.PyBundle.message;
 
@@ -27,22 +29,17 @@ import static com.jetbrains.python.PyBundle.message;
 public class BreakContinueAnnotator extends PyAnnotator {
   @Override
   public void visitPyBreakStatement(final PyBreakStatement node) {
-    if (getContainingLoop(node) == null) {
+    if (node.getLoopStatement() == null) {
       getHolder().createErrorAnnotation(node, message("ANN.break.outside.loop"));
     }
   }
 
-  @Nullable
-  private static PyLoopStatement getContainingLoop(PyStatement node) {
-    return PsiTreeUtil.getParentOfType(node, PyLoopStatement.class, false, PyFunction.class, PyClass.class);
-  }
-
   @Override
   public void visitPyContinueStatement(final PyContinueStatement node) {
-    if (getContainingLoop(node) == null) {
+    if (node.getLoopStatement() == null) {
       getHolder().createErrorAnnotation(node, message("ANN.continue.outside.loop"));
     }
-    else if (PsiTreeUtil.getParentOfType(node,  PyFinallyPart.class, false, PyLoopStatement.class) != null) {
+    else if (PsiTreeUtil.getParentOfType(node, PyFinallyPart.class, false, PyLoopStatement.class) != null) {
       getHolder().createErrorAnnotation(node, message("ANN.cant.continue.in.finally"));
     }
   }
