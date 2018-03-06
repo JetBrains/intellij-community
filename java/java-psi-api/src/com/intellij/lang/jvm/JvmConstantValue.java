9bf9d0fd6f11a9684979a95f1caeb235d63c7800
Nicolay Mitropolsky <nicolay.mitropolsky@jetbrains.com>
2018-03-06 19:00:26 +0300

    JvmAnnotation attributes

---
+++ java/java-psi-api/src/com/intellij/lang/jvm/JvmConstantValue.java
@@ -0,0 +1,11 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.lang.jvm;
+
+import org.jetbrains.annotations.Nullable;
+
+public interface JvmConstantValue extends JvmAnnotationAttributeValue {
+
+  @Nullable
+  JvmField getField();
+
+}
