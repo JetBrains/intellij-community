04e0376f77ca58a8420a61f92bb2d4537a4cc158
Nicolay Mitropolsky <nicolay.mitropolsky@jetbrains.com>
2018-03-07 17:49:42 +0300

    fixup: removing template methods from CreateAnnotationAction.kt

--- java/java-impl/src/com/intellij/lang/java/actions/CreateAnnotationAction.kt
+++ java/java-impl/src/com/intellij/lang/java/actions/CreateAnnotationAction.kt
@@ -2,20 +2,21 @@
 package com.intellij.lang.java.actions
 
 import com.intellij.codeInsight.FileModificationService
-import com.intellij.codeInsight.lookup.LookupElement
-import com.intellij.codeInsight.template.*
 import com.intellij.lang.jvm.actions.CreateAnnotationRequest
+import com.intellij.lang.jvm.actions.JvmAnnotationMemberValue
 import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.fileEditor.FileEditorManager
-import com.intellij.openapi.fileEditor.OpenFileDescriptor
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.TextRange
 import com.intellij.openapi.util.text.StringUtilRt
-import com.intellij.psi.*
+import com.intellij.psi.LanguageAnnotationSupport
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiModifierListOwner
 import com.intellij.psi.codeStyle.CodeStyleManager
 import com.intellij.psi.codeStyle.JavaCodeStyleManager
 
+
+private val LOG = logger<CreateAnnotationAction>()
 
 class CreateAnnotationAction(target: PsiModifierListOwner, override val request: CreateAnnotationRequest) :
   CreateTargetAction<PsiModifierListOwner>(target, request) {
@@ -44,45 +45,6 @@ class CreateAnnotationAction(target: PsiModifierListOwner, override val request:
     }
 
 
-  }
-
-  private fun createTemplate(psiLiteral: PsiLiteral): Template {
-    val builder = TemplateBuilderImpl(psiLiteral.containingFile)
-
-    val textRange = psiLiteral.textRange
-    val valueText = ElementManipulators.getValueText(psiLiteral)
-
-    val from = TextRange.from(textRange.startOffset + 1, valueText.length)
-    builder.replaceRange(from, createSuggestExpression(psiLiteral))
-    return builder.buildInlineTemplate()
-  }
-
-  private fun createSuggestExpression(psiLiteral: PsiLiteral): Expression {
-    return object : Expression() {
-      override fun calculateResult(context: ExpressionContext): Result {
-        PsiDocumentManager.getInstance(context.project).commitAllDocuments()
-        return TextResult(ElementManipulators.getValueText(psiLiteral))
-      }
-
-      override fun calculateQuickResult(context: ExpressionContext): Result? = calculateResult(context)
-
-      override fun calculateLookupItems(context: ExpressionContext): Array<LookupElement>? {
-        return psiLiteral.references.filter { !it.isSoft }
-          .flatMap { it.variants.mapNotNull { it as? LookupElement } }
-          .sortedBy { it.lookupString }
-          .toTypedArray()
-      }
-    }
-  }
-
-  companion object {
-
-    fun getEditor(modifierListOwner: PsiModifierListOwner): Editor {
-      val psiFile = modifierListOwner.containingFile
-      val project = psiFile.project
-      val virtualFile = psiFile.virtualFile!!
-      return FileEditorManager.getInstance(project).openTextEditor(OpenFileDescriptor(project, virtualFile, 0), false)!!
-    }
   }
 
 }
