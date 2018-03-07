a1e51def1ea032fcc042f9e8a278672f30b5b9bb
Anna.Kozlova <anna.kozlova@jetbrains.com>
2018-03-07 14:33:50 +0100

    provide examples for new color settings (IDEA-CR-30314)

--- java/java-impl/src/com/intellij/openapi/options/colors/pages/JavaColorSettingsPage.java
+++ java/java-impl/src/com/intellij/openapi/options/colors/pages/JavaColorSettingsPage.java
@@ -131,6 +131,9 @@ public class JavaColorSettingsPage implements RainbowColorSettingsPage, Inspecti
     ourTags.put("annotationAttributeName", JavaHighlightingColors.ANNOTATION_ATTRIBUTE_NAME_ATTRIBUTES);
     ourTags.put("javadocTagValue", JavaHighlightingColors.DOC_COMMENT_TAG_VALUE);
     ourTags.put("instanceFinalField", JavaHighlightingColors.INSTANCE_FINAL_FIELD_ATTRIBUTES);
+    ourTags.put("staticallyConstImported", JavaHighlightingColors.STATIC_FINAL_FIELD_IMPORTED_ATTRIBUTES);
+    ourTags.put("staticallyImported", JavaHighlightingColors.STATIC_FIELD_IMPORTED_ATTRIBUTES);
+    ourTags.put("static_imported_method", JavaHighlightingColors.STATIC_METHOD_CALL_IMPORTED_ATTRIBUTES);
   }
 
   @Override
@@ -168,6 +171,9 @@ public class JavaColorSettingsPage implements RainbowColorSettingsPage, Inspecti
     return
       "/* Block comment */\n" +
       "import <class>java.util.Date</class>;\n" +
+      "import static <interface>AnInterface</interface>.<static_final>CONSTANT</static_final>;\n" +
+      "import static <class>java.util.Date</class>.<static_method>parse</static_method>;\n" +
+      "import static <class>SomeClass</class>.<static>staticField</static>;\n" +
       "/**\n" +
       " * Doc comment here for <code>SomeClass</code>\n" +
       " * @param <javadocTagValue>T</javadocTagValue> type parameter\n" +
@@ -195,7 +201,7 @@ public class JavaColorSettingsPage implements RainbowColorSettingsPage, Inspecti
       "    long <localVar>localVar1</localVar>, <localVar>localVar2</localVar>, <localVar>localVar3</localVar>, <localVar>localVar4</localVar>;\n" +
       "    <error>int <localVar>localVar</localVar> = \"IntelliJ\"</error>; // Error, incompatible types\n" +
       "    <class>System</class>.<static>out</static>.<methodCall>println</methodCall>(<field>anotherString</field> + <inherited_method>toString</inherited_method>() + <localVar>localVar</localVar>);\n" +
-      "    long <localVar>time</localVar> = <class>Date</class>.<static_method><deprecated>parse</deprecated></static_method>(\"1.2.3\"); // Method is deprecated\n" +
+      "    long <localVar>time</localVar> = <static_imported_method><deprecated>parse</deprecated></static_imported_method>(\"1.2.3\"); // Method is deprecated\n" +
       "    new <class>Thread</class>().<for_removal>countStackFrames</for_removal>(); // Method is deprecated and marked for removal\n" +
       "    <reassignedLocalVar>reassignedValue</reassignedLocalVar> ++; \n" +
       "    <field>field</field>.<abstract_method>run</abstract_method>(); \n" +
@@ -204,7 +210,7 @@ public class JavaColorSettingsPage implements RainbowColorSettingsPage, Inspecti
       "        int <localVar>a</localVar> = <implicitAnonymousParameter>localVar</implicitAnonymousParameter>;\n" +
       "      }\n" +
       "    };\n" +
-      "    <reassignedParameter>reassignedParam</reassignedParameter> = new <constructorCall>ArrayList</constructorCall><<class>String</class>>().toArray(new int[0]);\n" +
+      "    <reassignedParameter>reassignedParam</reassignedParameter> = new <constructorCall>ArrayList</constructorCall><<class>String</class>>().toArray(new int[<staticallyConstImported>CONSTANT</staticallyConstImported>]);\n" +
       "  }\n" +
       "}\n" +
       "enum <enum>AnEnum</enum> { <static_final>CONST1</static_final>, <static_final>CONST2</static_final> }\n" +
@@ -213,6 +219,7 @@ public class JavaColorSettingsPage implements RainbowColorSettingsPage, Inspecti
       "  void <methodDeclaration>method</methodDeclaration>();\n" +
       "}\n" +
       "abstract class <abstractClass>SomeAbstractClass</abstractClass> {\n" +
+      "  protected int <field>instanceField</field> = <staticallyImported>staticField</staticallyImported>;\n" +
       "}";
   }
 
