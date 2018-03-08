36b1e622eba6fc7753326124f902800dc18be6f1
Anna.Kozlova <anna.kozlova@jetbrains.com>
2018-03-08 18:19:12 +0100

    inference: skip bounds checking for captured fresh variables (IDEA-187943)

--- java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/lambda/NewLambdaHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/lambda/NewLambdaHighlightingTest.java
@@ -162,6 +162,7 @@ public class NewLambdaHighlightingTest extends LightDaemonAnalyzerTestCase {
   public void testLambdaWithFormalTypeParameters() { doTest(); }
   public void testIDEA174924() { doTest(); }
   public void testVoidValueCompatibilityWithBreakInSwitch() { doTest(); }
+  public void testLambdaChainWithCapturedWildcards() { doTest(); }
   public void testExceptionInferenceForVarargMethods() { doTest(); }
   public void testConditionalBooleanAsFunctionalInterfaceType() { doTest(); }
   public void testUnhandledExceptionInLambdaChain() { doTest(); }
