26beece9550534d1b3b84de1a6e3c41252db6829
adelf <adel.faiz@gmail.com>
2018-03-08 18:58:22 +0300

    fix tests
    GitOrigin-RevId: 981ecdcaef9bcbf418588095c5e25a0595ec7fe4

--- plugins/env-files-support/tests/ru/adelf/idea/dotenv/tests/dotenv/DotEnvFileTest.java
+++ plugins/env-files-support/tests/ru/adelf/idea/dotenv/tests/dotenv/DotEnvFileTest.java
@@ -1,9 +1,6 @@
 package ru.adelf.idea.dotenv.tests.dotenv;
 
-import com.intellij.patterns.PlatformPatterns;
-import com.jetbrains.php.lang.PhpFileType;
 import ru.adelf.idea.dotenv.indexing.DotEnvKeysIndex;
-import ru.adelf.idea.dotenv.psi.DotEnvProperty;
 import ru.adelf.idea.dotenv.tests.DotEnvLightCodeInsightFixtureTestCase;
 
 import java.io.File;
@@ -22,17 +19,5 @@ public class DotEnvFileTest extends DotEnvLightCodeInsightFixtureTestCase {
 
     public void testEnvKeys() {
         assertIndexContains(DotEnvKeysIndex.KEY,"TEST", "TEST2", "EMPTY_KEY", "OFFSET_KEY");
-    }
-
-    public void testEnvFunctionCompletion() {
-        assertCompletionContains(PhpFileType.INSTANCE, "<?php\n" +
-                        "getenv('<caret>');\n",
-                "TEST", "TEST2", "EMPTY_KEY", "OFFSET_KEY"
-        );
-
-        assertNavigationMatch(PhpFileType.INSTANCE, "<?php\n" +
-                        "getenv('TEST<caret>');\n",
-                PlatformPatterns.psiElement(DotEnvProperty.class)
-        );
     }
 }
