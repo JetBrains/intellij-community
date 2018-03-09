5d3d24256616184b558b6422e4893e05342a5e59
Dmitry Jemerov <yole@jetbrains.com>
2018-03-09 16:23:09 +0100

    Extract CompletionLookupArranger into API

--- platform/lang-impl/src/com/intellij/codeInsight/completion/CompletionProcessBase.java
+++ platform/lang-impl/src/com/intellij/codeInsight/completion/CompletionProcessBase.java
@@ -1,10 +1,14 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.completion;
 
+import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.util.Disposer;
+import com.intellij.patterns.ElementPattern;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.awt.*;
 import java.util.function.Supplier;
 
 /**
@@ -14,6 +18,7 @@ public class CompletionProcessBase implements CompletionProcessEx, Disposable {
   protected final int myInvocationCount;
   protected final Object myLock = new String("CompletionProgressIndicator");
   protected OffsetsInFile myHostOffsets;
+  private CompletionParameters myParameters;
 
   public CompletionProcessBase(CompletionInitializationContext context) {
     myInvocationCount = context.getInvocationCount();
@@ -40,5 +45,27 @@ public class CompletionProcessBase implements CompletionProcessEx, Disposable {
 
   @Override
   public void dispose() {
+  }
+
+  @Override
+  public void itemSelected(LookupElement item, char aChar) {
+  }
+
+  @Override
+  public void addWatchedPrefix(int startOffset, ElementPattern<String> restartCondition) {
+  }
+
+  @Override
+  public void addAdvertisement(String message, @Nullable Color bgColor) {
+  }
+
+  @Override
+  public CompletionParameters getParameters() {
+    return myParameters;
+  }
+
+  @Override
+  public void setParameters(CompletionParameters parameters) {
+    myParameters = parameters;
   }
 }
