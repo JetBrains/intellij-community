828000c586b4f739279be63574added1e5997695
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2018-03-09 17:14:44 +0700

    CharsetObjectCanBeUsedInspectionTest: use Mock JDK 9 to access StandardCharsets
    Review ID: IDEA-CR-30242

--- java/java-tests/testData/inspection/charsetObjectCanBeUsed/afterStringUEE.java
+++ java/java-tests/testData/inspection/charsetObjectCanBeUsed/afterStringUEE.java
@@ -1,12 +1,13 @@
 // "Replace with 'StandardCharsets.UTF_16'" "true"
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 
 class Test {
   static final String UTF16 = "UTF-16";
 
   void test(byte[] bytes) {
     String string = null;
-      string = new String(bytes, java.nio.charset.StandardCharsets.UTF_16);
+      string = new String(bytes, StandardCharsets.UTF_16);
       if(string.startsWith("Foo")) {
       System.out.println("It's a foo!");
     }
--- java/java-tests/testData/inspection/charsetObjectCanBeUsed/afterStringUEEDoubleCatch.java
+++ java/java-tests/testData/inspection/charsetObjectCanBeUsed/afterStringUEEDoubleCatch.java
@@ -1,11 +1,12 @@
 // "Replace with 'StandardCharsets.UTF_16'" "true"
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 
 class Test {
   void test(byte[] bytes) {
     String string = null;
     try {
-      string = new String(bytes, java.nio.charset.StandardCharsets.UTF_16);
+      string = new String(bytes, StandardCharsets.UTF_16);
     } catch (Throwable t) {
       return;
     }
