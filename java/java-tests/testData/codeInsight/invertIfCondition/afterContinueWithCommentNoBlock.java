adf14b7ded3a316e90ef59cbfa2cbc2b5fa071f0
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2018-03-12 15:53:28 +0700

    InvertIfConditionAction: better comment handling

---
+++ java/java-tests/testData/codeInsight/invertIfCondition/afterContinueWithCommentNoBlock.java
@@ -0,0 +1,11 @@
+// "Invert 'if' condition" "true"
+class A {
+    void f(){
+        while (true) {
+            if (false) //comment
+            {
+                System.out.println();
+            }
+        }
+    }
+}
