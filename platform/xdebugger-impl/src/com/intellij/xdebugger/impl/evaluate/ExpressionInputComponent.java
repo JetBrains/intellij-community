4c5eda0913ba9df43852d55b120b3d96ed0b67b2
Egor Ushakov <egor.ushakov@jetbrains.com>
2018-03-12 15:24:31 +0300

    do not use editor caret width in tree inplace editors etc.

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/evaluate/ExpressionInputComponent.java
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/evaluate/ExpressionInputComponent.java
@@ -1,9 +1,10 @@
-// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.xdebugger.impl.evaluate;
 
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.ex.util.EditorUtil;
+import com.intellij.openapi.editor.impl.SettingsImpl;
 import com.intellij.openapi.keymap.KeymapUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.ui.components.JBLabel;
@@ -44,6 +45,7 @@ public class ExpressionInputComponent extends EvaluationInputComponent {
         Font font = EditorUtil.getEditorFont();
         editor.getColorsScheme().setEditorFontName(font.getFontName());
         editor.getColorsScheme().setEditorFontSize(font.getSize());
+        editor.getSettings().setLineCursorWidth(new SettingsImpl().getLineCursorWidth());
       }
     };
     myExpressionEditor.setExpression(expression);
