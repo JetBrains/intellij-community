344cd5da6a183f7325b222ff257d1f70c6f9673b
Anna.Kozlova <anna.kozlova@jetbrains.com>
2018-03-12 10:05:23 +0100

    `var` in lambda parameter list: ensure lambda parameter

--- java/java-psi-impl/src/com/intellij/psi/impl/source/PsiParameterImpl.java
+++ java/java-psi-impl/src/com/intellij/psi/impl/source/PsiParameterImpl.java
@@ -152,7 +152,7 @@ public class PsiParameterImpl extends JavaStubPsiElement<PsiParameterStub> imple
     myCachedType = null;
 
     PsiTypeElement typeElement = getTypeElement();
-    if (typeElement == null || typeElement.isInferredType()) {
+    if (typeElement == null || isLambdaParameter() && typeElement.isInferredType()) {
       assert isLambdaParameter() : this;
       return getLambdaParameterType(this);
     }
