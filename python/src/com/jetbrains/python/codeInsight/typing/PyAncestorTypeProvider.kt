ad2e18ab48d7c3a3122b750a13f1bb786f683441
Anton Bragin <Anton.Bragin@jetbrains.com>
2018-03-13 14:28:00 +0300

    PY-28897 Code cleaned
    (cherry picked from commit 70557159487399b71a8f61181772c993ccacd8b6)

--- python/src/com/jetbrains/python/codeInsight/typing/PyAncestorTypeProvider.kt
+++ python/src/com/jetbrains/python/codeInsight/typing/PyAncestorTypeProvider.kt
@@ -31,36 +31,6 @@ class PyAncestorTypeProvider : PyTypeProviderBase() {
     return null
   }
 
-  private fun getOverriddenFunctionType(function: PyFunction, context: TypeEvalContext): PyFunctionType? {
-    val overriddenFunction = getOverriddenFunction(function, context)
-    if (overriddenFunction != null) {
-      val type = context.getType(overriddenFunction)
-      if (type is PyFunctionType) {
-        return context.getType(overriddenFunction) as PyFunctionType?
-      }
-    }
-    return null
-  }
-
-  private fun getOverriddenFunction(function: PyFunction, context: TypeEvalContext): PyFunction? {
-    val superMethodSearchQuery = PySuperMethodsSearch.search(function, context)
-    val firstSuperMethod = superMethodSearchQuery.findFirst() as? PyFunction ?: return null
-
-    if (firstSuperMethod.decoratorList != null) {
-      if (firstSuperMethod.decoratorList!!.decorators
-        .mapNotNull { it.name }
-        .any { PyNames.OVERLOAD == it }) {
-        return null
-      }
-    }
-
-    val superClass = firstSuperMethod.containingClass
-    return if (superClass != null && PyNames.OBJECT != superClass.name) {
-      firstSuperMethod
-    }
-    else null
-  }
-
   override fun getReturnType(callable: PyCallable, context: TypeEvalContext): Ref<PyType>? {
     if (callable is PyFunction) {
       val typeFromSupertype = getReturnTypeFromSupertype(callable, context)
@@ -70,31 +40,58 @@ class PyAncestorTypeProvider : PyTypeProviderBase() {
     }
     return null
   }
+}
 
-  /**
-   * Get function return type from supertype.
-   *
-   * The only source of type information in current implementation is annotation. This is to avoid false positives,
-   * that may arise from non direct type estimations (not from annotation, nor form type comments).
-   *
-   * TODO: switch to return type direct usage when type information source will be available.
-   *
-   * @param function
-   * @param context
-   * @return
-   */
-  private fun getReturnTypeFromSupertype(function: PyFunction, context: TypeEvalContext): Ref<PyType>? {
-    val overriddenFunction = getOverriddenFunction(function, context)
 
-    if (overriddenFunction != null) {
-      val superFunctionAnnotation = getReturnTypeAnnotation(overriddenFunction, context)
-      if (superFunctionAnnotation != null) {
-        val typeRef = PyTypingTypeProvider.getType(superFunctionAnnotation, PyTypingTypeProvider.Context(context))
-        if (typeRef != null) {
-          return Ref.create(PyTypingTypeProvider.toAsyncIfNeeded(function, typeRef.get()))
-        }
-      }
+private fun getOverriddenFunctionType(function: PyFunction, context: TypeEvalContext): PyFunctionType? {
+  val overriddenFunction = getOverriddenFunction(function, context)
+  if (overriddenFunction != null) {
+    val type = context.getType(overriddenFunction)
+    if (type is PyFunctionType) {
+      return type
     }
-    return null
   }
+  return null
+}
+
+
+private fun getOverriddenFunction(function: PyFunction, context: TypeEvalContext): PyFunction? {
+  val superMethodSearchQuery = PySuperMethodsSearch.search(function, context)
+  val firstSuperMethod = superMethodSearchQuery.findFirst() as? PyFunction ?: return null
+
+  val decoratorList = firstSuperMethod.decoratorList
+  if (decoratorList != null) {
+    if (decoratorList.decorators
+        .mapNotNull { it.name }
+        .any { it == PyNames.OVERLOAD }) {
+      return null
+    }
+  }
+
+  val superClass = firstSuperMethod.containingClass
+  return if (superClass != null && PyNames.OBJECT != superClass.name) {
+    firstSuperMethod
+  }
+  else null
+}
+
+/**
+ * Get function return type from supertype.
+ *
+ * The only source of type information in current implementation is annotation. This is to avoid false positives,
+ * that may arise from non direct type estimations (not from annotation, nor form type comments).
+ *
+ * TODO: switch to return type direct usage when type information source will be available.
+ */
+private fun getReturnTypeFromSupertype(function: PyFunction, context: TypeEvalContext): Ref<PyType>? {
+  val overriddenFunction = getOverriddenFunction(function, context)
+
+  if (overriddenFunction != null) {
+    val superFunctionAnnotation = getReturnTypeAnnotation(overriddenFunction, context)
+    if (superFunctionAnnotation != null) {
+      val typeRef = PyTypingTypeProvider.getType(superFunctionAnnotation, PyTypingTypeProvider.Context(context))
+      typeRef?.let { return Ref.create(PyTypingTypeProvider.toAsyncIfNeeded(function, it.get())) }
+    }
+  }
+  return null
 }
