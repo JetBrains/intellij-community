JetBrains/intellij-community

commit dc53479816ba5c16e3a7257a14b31bb041cb2440

[file-history] call Application.isActive from EDT
EA-104077

platform/vcs-impl/src/com/intellij/openapi/vcs/history/FileHistoryPanelImpl.java
@@ -198,7 +198,7 @@ public class FileHistoryPanelImpl extends PanelWithActionsAndCloseButton impleme
       }
     };
 
-    Alarm updateAlarm = new Alarm(Alarm.ThreadToUse.POOLED_THREAD, this);
+    Alarm updateAlarm = new Alarm(Alarm.ThreadToUse.SWING_THREAD, this);
     // todo react to event?
     updateAlarm.addRequest(new Runnable() {
       public void run() {

commit 8915890190b31aa4236182deef7246d89fcaf3bc

[file-history, perforce] do not call shouldBeRefreshed on EDT
It causes queries to Perforce server in EDT.

The problem was introduced in dc53479816ba5c16e3a7257a14b31bb041cb2440, where execution was moved to EDT to call Application.isActive properly.

IDEA-188192

platform/vcs-impl/src/com/intellij/openapi/vcs/history/FileHistoryPanelImpl.java
@@ -201,17 +201,18 @@ public class FileHistoryPanelImpl extends PanelWithActionsAndCloseButton impleme
         if (myVcs.getProject().isDisposed()) {
           return;
         }
-        boolean refresh = ApplicationManager.getApplication().isActive()
-                          && !myInRefresh
-                          && myHistorySession.shouldBeRefreshed();
 
         updateAlarm.cancelAllRequests();
         if (updateAlarm.isDisposed()) return;
         updateAlarm.addRequest(this, 20000);
 
-        if (refresh) {
-          refreshUiAndScheduleDataRefresh(true);
-        }
+        if (!ApplicationManager.getApplication().isActive()) return;
+
+        ApplicationManager.getApplication().executeOnPooledThread(() -> {
+          if (!myInRefresh && myHistorySession.shouldBeRefreshed()) {
+            refreshUiAndScheduleDataRefresh(true);
+          }
+        });
       }
     }, 20000);
 