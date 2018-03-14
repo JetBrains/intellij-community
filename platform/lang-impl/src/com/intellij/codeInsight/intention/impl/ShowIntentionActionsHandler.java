e4fde0d57acc4715299512b3b1cfd7a936b03cb4
sergey.vasiliev <sergey.vasiliev@jetbrains.com>
2018-03-14 12:57:24 +0100

    Feature Usages Statistics: migrate old collectors
    (cherry picked from commit 7578d8d)

--- platform/lang-impl/src/com/intellij/codeInsight/intention/impl/ShowIntentionActionsHandler.java
+++ platform/lang-impl/src/com/intellij/codeInsight/intention/impl/ShowIntentionActionsHandler.java
@@ -21,7 +21,7 @@ import com.intellij.codeInspection.SuppressIntentionActionFromFix;
 import com.intellij.featureStatistics.FeatureUsageTracker;
 import com.intellij.featureStatistics.FeatureUsageTrackerImpl;
 import com.intellij.injected.editor.EditorWindow;
-import com.intellij.internal.statistic.customUsageCollectors.actions.IntentionsCollector;
+import com.intellij.internal.statistic.collectors.fus.actions.persistence.IntentionsCollector;
 import com.intellij.lang.injection.InjectedLanguageManager;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.TransactionGuard;
