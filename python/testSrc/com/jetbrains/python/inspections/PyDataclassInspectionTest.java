8c0ac1662c3f6e22aad82f5a36beffabf0c47f8b
Semyon Proshev <semyon.proshev@jetbrains.com>
2018-03-14 17:40:31 +0300

    Highlight removing frozen dataclass attribute (PY-28957)
    (cherry picked from commit 5d1e935)
    
    (cherry picked from commit 9b7d088)

--- python/testSrc/com/jetbrains/python/inspections/PyDataclassInspectionTest.java
+++ python/testSrc/com/jetbrains/python/inspections/PyDataclassInspectionTest.java
@@ -12,7 +12,7 @@ import org.jetbrains.annotations.Nullable;
 public class PyDataclassInspectionTest extends PyInspectionTestCase {
 
   // PY-27398
-  public void testAssignmentsToFrozen() {
+  public void testMutatingFrozen() {
     doTest();
   }
 
