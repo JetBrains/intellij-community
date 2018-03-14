e962e7c0f2ec01532cae2fe4ff0eb407087e78d9
Nicolay Mitropolsky <nicolay.mitropolsky@jetbrains.com>
2018-03-14 17:09:55 +0300

    `AnnotationAttributeValueRequest` complex cases omitted for a while

--- java/java-analysis-api/src/com/intellij/lang/jvm/actions/AnnotationAttributeValueRequest.kt
+++ java/java-analysis-api/src/com/intellij/lang/jvm/actions/AnnotationAttributeValueRequest.kt
@@ -1,16 +1,21 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.lang.jvm.actions
 
-import com.intellij.lang.jvm.JvmClass
-
 
 sealed class AnnotationAttributeValueRequest {
   data class PrimitiveValue(val value: Any) : AnnotationAttributeValueRequest()
   data class StringValue(val value: String) : AnnotationAttributeValueRequest()
-  data class ClassValue(val clazz: JvmClass) : AnnotationAttributeValueRequest()
-  data class ConstantValue(val reference: JvmField) : AnnotationAttributeValueRequest()
-  data class NestedAnnotation(val annotationRequest: AnnotationRequest) : AnnotationAttributeValueRequest()
-  data class ArrayValue(val members: List<AnnotationAttributeValueRequest>) : AnnotationAttributeValueRequest()
+  /*
+  There also could be additional cases namely:
+  ```
+     data class ClassValue(val clazz: JvmClass) : AnnotationAttributeValueRequest()
+     data class ConstantValue(val reference: JvmField) : AnnotationAttributeValueRequest()
+     data class NestedAnnotation(val annotationRequest: AnnotationRequest) : AnnotationAttributeValueRequest()
+     data class ArrayValue(val members: List<AnnotationAttributeValueRequest>) : AnnotationAttributeValueRequest()
+  ```
+  they are skipped until become necessary
+   */
+
 }
 
 class AnnotationAttributeRequest(val name: String, val value: AnnotationAttributeValueRequest)
