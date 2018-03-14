6d4f7359b357b741e101160b4c05c9e95adfc761
alexey.afanasiev <Alexey.Afanasiev@jetbrains.com>
2018-03-14 13:04:05 +0300

    Speed up GrAssignAutoTest

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GrAssignAutoTest.groovy
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GrAssignAutoTest.groovy
@@ -1,11 +1,10 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.lang.highlighting
 
 import com.intellij.codeInspection.InspectionProfileEntry
 import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.testFramework.LightProjectDescriptor
+import groovy.transform.CompileStatic
 import org.codehaus.groovy.control.MultipleCompilationErrorsException
 import org.jetbrains.annotations.NotNull
 import org.jetbrains.plugins.groovy.GroovyLightProjectDescriptor
@@ -18,8 +17,13 @@ import org.jetbrains.plugins.groovy.util.Slow
  * Current spec https://github.com/apache/groovy/blob/master/src/spec/doc/core-differences-java.adoc#conversions
  * Bug: https://issues.apache.org/jira/browse/GROOVY-7557
  */
+@CompileStatic
 @Slow
-class GrAssignAutoTestFalsePositiveTest extends GrHighlightingTestBase {
+class GrAssignAutoTest extends GrHighlightingTestBase {
+
+  private static final String CS = '''\
+    import groovy.transform.CompileStatic
+  '''
 
   List<String> types = ['boolean', 'int', 'double', 'String', 'BigDecimal', 'BigInteger', 'List', 'Object', 'Thread',
                         'List<BigDecimal>', 'List<BigInteger>', 'List<Integer>', 'List<String>', 'List<Object>', 'List<Thread>', 'boolean[]',
@@ -34,13 +38,13 @@ class GrAssignAutoTestFalsePositiveTest extends GrHighlightingTestBase {
   List<String> longValues = ['0l', '-1L', '+2L', '(Long)(+2)', '+1 as long', '1234_5678_9012_3456L', '0b101101101101L', '0246L', '0xffffL', '-0x77L', '0x7fff_ffff_ffff_ffffL', 'new Long("123")']
   List<String> bigIntegerValues = ['BigInteger.valueOf(1)', '-1 as BigInteger', '+2G', '-0G', '034G', '1234_5678_9012_3456G', '0b101101101101G', '0246G', '0xffffG', '-0x77G']
 
-  List<String> floatValues = ['1.1f', '-1.1f', '+0.002f', '-0F', '034F', '5_132.12F', '0b101101101101f', '0246F', '0xffffF', '0x1.0p0F', '1 as Float', '(float) 1.1']
-  List<String> doubleValues = ['1.1d', '-1.1d', '+0.002d', '-0D', '034D', '12_345_132.12D', '0b101101101101d', '0246d', '0xffffD', '0x1.0p0D', '1 as Double', '(double) 1.1']
+  List<String> floatValues = ['1.1f', '-1.1f', '+0.002f', '-0F', '034F', '5_132.12F', '0b101101101101f', '0246F', '0xffffF', '1 as Float', '(float) 1.1']
+  List<String> doubleValues = ['1.1d', '-1.1d', '+0.002d', '-0D', '034D', '12_345_132.12D', '0b101101101101d', '0246d', '0xffffD', '1 as Double', '(double) 1.1']
 
-  List<String> bigDecimalValues = ['1.1', '-1.2', '+0.002', '-0.0', '034.0G', '12_345_132.12g', '0x1.0p0', '1 as BigDecimal', '(BigDecimal) 1.1']
+  List<String> bigDecimalValues = ['1.1', '-1.2', '+0.002', '-0.0', '034.0G', '12_345_132.12g', '1 as BigDecimal', '(BigDecimal) 1.1']
 
   List<String> objectValues = ['new Object()', 'new Thread()', '"str"', 'null']
-  List<String> listValues = ['[]', '[1]', '[0L]', '[1.1]', '[1.2f]', '["str]', 'new ArrayList<>()', '[new Object()], [new Thread()]']
+  List<String> listValues = ['[]', '[1]', '[0L]', '[1.1]', '[1.2f]', '["str"]', 'new ArrayList<>()', '[new Object()]', '[new Thread()]']
 
   List<String> voidValues = ['print("")', '(Void)null']
 
@@ -57,8 +61,6 @@ class GrAssignAutoTestFalsePositiveTest extends GrHighlightingTestBase {
                         listValues +
                         voidValues
 
-  def shell = GroovyShell.newInstance()
-
   @Override
   @NotNull
   protected LightProjectDescriptor getProjectDescriptor() {
@@ -66,14 +68,12 @@ class GrAssignAutoTestFalsePositiveTest extends GrHighlightingTestBase {
   }
 
   @Override
-  InspectionProfileEntry[] getCustomInspections() { [new GroovyAssignabilityCheckInspection()] }
+  InspectionProfileEntry[] getCustomInspections() { [new GroovyAssignabilityCheckInspection()] as InspectionProfileEntry[] }
 
   void testParameterToLocal() {
     doTest '''
-        import groovy.transform.CompileStatic
-
         @CompileStatic
-        void method(%1$s param) {
+        void method%3$s(%1$s param) {
           %2$s local = param
         }
         ''',
@@ -103,10 +103,8 @@ class GrAssignAutoTestFalsePositiveTest extends GrHighlightingTestBase {
 
   void testParameterToReturn() {
     doTest '''
-        import groovy.transform.CompileStatic
-
         @CompileStatic
-        %2$s method(%1$s param) {
+        %2$s method%3$s(%1$s param) {
           return param
         }
         ''',
@@ -120,14 +118,12 @@ class GrAssignAutoTestFalsePositiveTest extends GrHighlightingTestBase {
 
   void testParameterMethodCall() {
     doTest '''
-        import groovy.transform.CompileStatic
-
-        def bar(%2$s param) {
+        def bar%3$s(%2$s param) {
         }
         
         @CompileStatic
-        def method(%1$s param) {
-          bar(param)
+        def method%3$s(%1$s param) {
+          bar%3$s(param)
         }
         ''',
            vectorProduct(types, types),
@@ -140,10 +136,8 @@ class GrAssignAutoTestFalsePositiveTest extends GrHighlightingTestBase {
 
   void testLocalAssignValue() {
     doTest '''
-        import groovy.transform.CompileStatic
-
         @CompileStatic
-        void method() {
+        void method%3$s() {
           %2$s param = %1$s
         }
         ''',
@@ -152,7 +146,9 @@ class GrAssignAutoTestFalsePositiveTest extends GrHighlightingTestBase {
            ['[] -> int', '[] -> double', '[] -> short', '[] -> byte', '[1.1] -> int', '[1.1] -> double', '[1.1] -> boolean[]',
             '[1.1] -> String[]', '[1.1] -> short', '[] -> int', '[1] -> int', '[1] -> double', '[1] -> int',
             '[1] -> boolean[]', '[1] -> String[]', '[0L] -> double', '[0L] -> boolean[]', '[0L] -> String[]', '[1.1] -> int',
-            '[1.2f] -> double', '[1.2f] -> boolean[]', '[1.2f] -> String[]', '[] -> int', '[1] -> int', '[1.1] -> int']
+            '[1.2f] -> double', '[1.2f] -> boolean[]', '[1.2f] -> String[]', '[] -> int', '[1] -> int', '[1.1] -> int',
+            '["str"] -> int', '["str"] -> double', '["str"] -> boolean[]', '["str"] -> short', '["str"] -> byte',
+            '[new Object()] -> boolean[]', '[new Object()] -> String[]', '[new Thread()] -> boolean[]', '[new Thread()] -> String[]']
   }
 
   void testWrongConstructorResolve() {
@@ -173,18 +169,22 @@ class GrAssignAutoTestFalsePositiveTest extends GrHighlightingTestBase {
   void doTest(String body, List<List<String>> arguments, List<String> wrongTrueByIdea, List<String> wrongFalseByIdea) {
     List<String> falseDiff = []
     List<String> trueDiff = []
-    Set<String> trueIssues = wrongTrueByIdea
-    Set<String> falseIssues = wrongFalseByIdea
-    for (List<String> args : arguments) {
-      String pair = "${args[0]} -> ${args[1]}"
-      String text = String.format(body, args[0], args[1])
-      boolean shellTest = shellTest(text)
-      boolean ideaTest = ideaTest(text)
-      if (shellTest != ideaTest) {
+
+    Set<String> trueIssues = wrongTrueByIdea as Set<String>
+    Set<String> falseIssues = wrongFalseByIdea as Set<String>
+
+    Set<Integer> shellErrors = shellTest(body, arguments)
+    Set<Integer> ideaErrors = ideaTest(body, arguments)
+
+    arguments.eachWithIndex { List<String> args, int index ->
+      def ideaTest = !ideaErrors.contains(index)
+      def shellTest = !shellErrors.contains(index)
+      if (ideaTest != shellTest) {
         def activeIssues = ideaTest ? trueIssues : falseIssues
         def activeDiff = ideaTest ? trueDiff : falseDiff
-        if (!activeIssues.remove(pair)) {
-          activeDiff.add(pair)
+        def pair = "${args[0]} -> ${args[1]}"
+        if (!activeIssues.remove(pair.toString())) {
+          activeDiff.add(pair.toString())
         }
       }
     }
@@ -195,22 +195,42 @@ class GrAssignAutoTestFalsePositiveTest extends GrHighlightingTestBase {
     assert trueIssues.isEmpty(), trueIssues.collect { "'$it'" }
   }
 
-  boolean ideaTest(String body) {
+  Set<Integer> ideaTest(String body, List<List<String>> arguments) {
+    def text = CS
+
+    arguments.eachWithIndex { List<String> args, int index ->
+      text += String.format(body, args[0], args[1], index)
+    }
+
+    def offsetLen = CS.readLines().size()
+    def lineCount = body.readLines().size() - 1
+
+    def res = new HashSet()
     myFixture.with {
-      configureByText('_.groovy', body)
+      configureByText('_.groovy', text)
       enableInspections(customInspections)
       def highlighting = doHighlighting(HighlightSeverity.ERROR)
-      return highlighting.size() == 0
+      highlighting.each {
+        def lineNumber = myFixture.editor.document.getLineNumber(it.startOffset)
+        res.add((lineNumber - offsetLen).intdiv(lineCount))
+      }
+      return res
     }
   }
 
-  boolean shellTest(String scriptText) {
-    try {
-      shell.evaluate(scriptText)
+  static Set<Integer> shellTest(String body, List<List<String>> arguments) {
+    def res = new HashSet()
+    def shell = new GroovyShell()
+    arguments.eachWithIndex { List<String> args, int index ->
+      def text = CS + String.format(body, args[0], args[1], index)
+      try {
+        shell.evaluate(text)
+      }
+      catch (MultipleCompilationErrorsException ignored) {
+        res.add(index)
+      }
     }
-    catch (MultipleCompilationErrorsException ignored) {
-      return false
-    }
-    return true
+
+    return res
   }
 }
