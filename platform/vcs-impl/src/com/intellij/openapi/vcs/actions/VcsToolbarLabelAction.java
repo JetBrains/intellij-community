c45afb57b1a8120298d9cc0ca6b7df8601bd2d89
Konstantin Bulenkov <kb@jetbrains.com>
2018-03-15 21:01:38 +0100

    IDEA-188346 VCS label disappears from toolbar if second project is opened

--- platform/vcs-impl/src/com/intellij/openapi/vcs/actions/VcsToolbarLabelAction.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/actions/VcsToolbarLabelAction.java
@@ -2,32 +2,33 @@
 package com.intellij.openapi.vcs.actions;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.ex.DefaultCustomComponentAction;
+import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.actionSystem.ex.CustomComponentAction;
+import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vcs.ProjectLevelVcsManager;
 import com.intellij.ui.components.JBLabel;
 import com.intellij.util.ui.JBUI;
-import com.intellij.util.ui.UIUtil;
 
 import javax.swing.*;
 
-public class VcsToolbarLabelAction extends DefaultCustomComponentAction {
-  public VcsToolbarLabelAction() {
-    super(createVcsLabel());
-  }
-
+public class VcsToolbarLabelAction extends DumbAwareAction implements CustomComponentAction {
   @Override
   public void update(AnActionEvent e) {
     e.getPresentation().setEnabled(false);
-
     Project project = e.getProject();
     e.getPresentation().setVisible(project != null && ProjectLevelVcsManager.getInstance(project).hasActiveVcss());
   }
 
-  private static JComponent createVcsLabel() {
-    JBLabel label = new JBLabel("VCS:");
-    label.setFont(UIUtil.getToolbarFont());
-    label.setBorder(JBUI.Borders.empty(0, 6, 0, 5));
-    return label;
+  @Override
+  public void actionPerformed(AnActionEvent e) {
+    //do nothing
+  }
+
+  @Override
+  public JComponent createCustomComponent(Presentation presentation) {
+    return new JBLabel("VCS:")
+      .withFont(JBUI.Fonts.toolbarFont())
+      .withBorder(JBUI.Borders.empty(0, 6, 0, 5));
   }
 }
