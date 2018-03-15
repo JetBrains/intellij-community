78224097b32d5e8e0cf040e32f0386b2a4141759
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2018-03-15 18:47:22 +0300

    IDEA-186172 The "Find in Path" dialog box is always on top
    Now in Windows we dispose popup on app deactivation

--- platform/lang-impl/src/com/intellij/find/impl/FindPopupPanel.java
+++ platform/lang-impl/src/com/intellij/find/impl/FindPopupPanel.java
@@ -16,6 +16,7 @@ import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.actionSystem.ex.DefaultCustomComponentAction;
 import com.intellij.openapi.actionSystem.impl.ActionButton;
 import com.intellij.openapi.actionSystem.impl.ActionToolbarImpl;
+import com.intellij.openapi.application.ApplicationActivationListener;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.command.CommandProcessor;
@@ -41,6 +42,7 @@ import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.wm.IdeFocusManager;
+import com.intellij.openapi.wm.IdeFrame;
 import com.intellij.openapi.wm.WindowManager;
 import com.intellij.openapi.wm.impl.IdeFrameImpl;
 import com.intellij.pom.Navigatable;
@@ -156,6 +158,16 @@ public class FindPopupPanel extends JBPanel implements FindUI {
     initByModel();
 
     ApplicationManager.getApplication().invokeLater(this::scheduleResultsUpdate, ModalityState.any());
+    if (SystemInfo.isWindows) {
+      ApplicationManager.getApplication().getMessageBus()
+                        .connect(myDisposable).subscribe(ApplicationActivationListener.TOPIC,
+                                                         new ApplicationActivationListener() {
+                                                           @Override
+                                                           public void applicationDeactivated(IdeFrame ideFrame) {
+                                                             closeImmediately();
+                                                           }
+                                                         });
+    }
   }
 
   @Override
@@ -254,8 +266,8 @@ public class FindPopupPanel extends JBPanel implements FindUI {
   protected boolean canBeClosed() {
     if (!myCanClose.get()) return false;
     if (myIsPinned.get()) return false;
-    if (!ApplicationManager.getApplication().isActive()) return false;
-    if (KeyboardFocusManager.getCurrentKeyboardFocusManager().getFocusedWindow() == null) return false;
+    if (!ApplicationManager.getApplication().isActive()) return SystemInfo.isWindows;
+    if (KeyboardFocusManager.getCurrentKeyboardFocusManager().getFocusedWindow() == null) return SystemInfo.isWindows;
     if (myFileMaskField.isPopupVisible()) {
       myFileMaskField.setPopupVisible(false);
       return false;
@@ -813,6 +825,13 @@ public class FindPopupPanel extends JBPanel implements FindUI {
     scheduleResultsUpdate();
   }
 
+  private void closeImmediately() {
+    if (canBeClosedImmediately() && myBalloon != null && myBalloon.isVisible()) {
+      myIsPinned.set(false);
+      myBalloon.cancel();
+    }
+  }
+
   //Some popups shown above may prevent panel closing, first of all we should close them
   private boolean canBeClosedImmediately() {
     boolean state = myIsPinned.get();
@@ -871,12 +890,8 @@ public class FindPopupPanel extends JBPanel implements FindUI {
 
   private void registerCloseAction(JBPopup popup) {
     final AnAction escape = ActionManager.getInstance().getAction("EditorEscape");
-    DumbAwareAction.create(e -> {
-      if (canBeClosedImmediately() && myBalloon != null && myBalloon.isVisible()) {
-        myIsPinned.set(false);
-        myBalloon.cancel();
-      }
-    }).registerCustomShortcutSet(escape == null ? CommonShortcuts.ESCAPE : escape.getShortcutSet(), popup.getContent(), popup);
+    DumbAwareAction.create(e -> closeImmediately())
+                   .registerCustomShortcutSet(escape == null ? CommonShortcuts.ESCAPE : escape.getShortcutSet(), popup.getContent(), popup);
   }
 
   @Override
