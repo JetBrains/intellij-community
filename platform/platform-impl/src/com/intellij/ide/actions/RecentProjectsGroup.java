667f16abfa64312e20c9943b96b3fb6a6e20f175
Konstantin Bulenkov <kb@jetbrains.com>
2018-03-16 14:05:25 +0100

    Don't show active project in main menu -> Open Recent

--- platform/platform-impl/src/com/intellij/ide/actions/RecentProjectsGroup.java
+++ platform/platform-impl/src/com/intellij/ide/actions/RecentProjectsGroup.java
@@ -1,21 +1,8 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.actions;
 
 import com.intellij.ide.RecentProjectsManager;
+import com.intellij.ide.ReopenProjectAction;
 import com.intellij.idea.ActionsBundle;
 import com.intellij.openapi.actionSystem.ActionGroup;
 import com.intellij.openapi.actionSystem.AnAction;
@@ -23,8 +10,11 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.util.SystemInfo;
+import com.intellij.openapi.util.text.StringUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Arrays;
 
 public class RecentProjectsGroup extends ActionGroup implements DumbAware {
   public RecentProjectsGroup() {
@@ -35,7 +25,15 @@ public class RecentProjectsGroup extends ActionGroup implements DumbAware {
   @Override
   @NotNull
   public AnAction[] getChildren(@Nullable AnActionEvent e) {
-    return RecentProjectsManager.getInstance().getRecentProjectsActions(true);
+    AnAction[] actions = RecentProjectsManager.getInstance().getRecentProjectsActions(true);
+    if (e != null && e.getProject() != null) {
+      return Arrays.stream(actions)
+                   .filter(action -> !(action instanceof ReopenProjectAction)
+                                     ||
+                                     !StringUtil.equals(((ReopenProjectAction)action).getProjectPath(), e.getProject().getBasePath()))
+                   .toArray(AnAction[]::new);
+    }
+    return actions;
   }
 
   @Override
