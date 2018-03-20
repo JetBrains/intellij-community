8f1b015e5dce8d6fa6f98757ca7901add2b37d16
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2018-03-20 20:23:01 +0300

    [core] extract #getAllBaseLanguageIdsWithAny into CompletionExtension

--- platform/core-api/src/com/intellij/lang/LanguageExtension.java
+++ platform/core-api/src/com/intellij/lang/LanguageExtension.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 /*
  * @author max
@@ -22,12 +8,14 @@ package com.intellij.lang;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.KeyedExtensionCollector;
 import com.intellij.util.containers.ContainerUtil;
-import gnu.trove.THashSet;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 public class LanguageExtension<T> extends KeyedExtensionCollector<T, Language> {
   private final T myDefaultImplementation;
@@ -123,19 +111,4 @@ public class LanguageExtension<T> extends KeyedExtensionCollector<T, Language> {
   protected Key<T> getLanguageCache() {
     return IN_LANGUAGE_CACHE;
   }
-
-  @NotNull
-  protected Set<String> getAllBaseLanguageIdsWithAny(@NotNull Language key) {
-    Set<String> allowed = new THashSet<>();
-    while (key != null) {
-      allowed.add(keyToString(key));
-      key = key.getBaseLanguage();
-    }
-    allowed.add("any");
-    for (MetaLanguage metaLanguage : MetaLanguage.all()) {
-      allowed.add(metaLanguage.getID());
-    }
-    return allowed;
-  }
-
 }
