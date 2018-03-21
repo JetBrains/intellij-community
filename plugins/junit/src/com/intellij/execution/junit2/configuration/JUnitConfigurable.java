0078a929099bcb7ef312f2c514e66b421c3cb867
Anna.Kozlova <anna.kozlova@jetbrains.com>
2018-03-21 13:13:13 +0100

    junit 5: migrate to single tag expression (IDEA-187029)

--- plugins/junit/src/com/intellij/execution/junit2/configuration/JUnitConfigurable.java
+++ plugins/junit/src/com/intellij/execution/junit2/configuration/JUnitConfigurable.java
@@ -320,7 +320,7 @@ public class JUnitConfigurable<T extends JUnitConfiguration> extends SettingsEdi
       configuration.setRepeatCount(1);
     }
     configuration.getPersistentData().setUniqueIds(setArrayFromText(myUniqueIdField));
-    configuration.getPersistentData().setTags(setArrayFromText(myTagsField));
+    configuration.getPersistentData().setTags(myTagsField.getComponent().getText());
     configuration.getPersistentData().setChangeList((String)myChangeListLabeledComponent.getComponent().getSelectedItem());
     myModel.apply(getModuleSelector().getModule(), configuration);
     applyHelpersTo(configuration);
@@ -361,8 +361,7 @@ public class JUnitConfigurable<T extends JUnitConfiguration> extends SettingsEdi
     String[] ids = configuration.getPersistentData().getUniqueIds();
     myUniqueIdField.getComponent().setText(ids != null ? StringUtil.join(ids, " ") : null);
 
-    String[] tags = configuration.getPersistentData().getTags();
-    myTagsField.getComponent().setText(tags != null ? StringUtil.join(tags, " ") : null);
+    myTagsField.getComponent().setText(configuration.getPersistentData().getTags());
 
     myCommonJavaParameters.reset(configuration);
     getModuleSelector().reset(configuration);
