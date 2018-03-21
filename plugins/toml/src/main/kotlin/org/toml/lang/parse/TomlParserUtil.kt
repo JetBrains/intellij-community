435d909cfce35d640f8eac92c3f136d3bb817ed1
vlad20012 <beskvlad@yandex.ru>
2018-03-21 23:24:35 +0300

    TOML: Don't allow a value on the next line after the key
    GitOrigin-RevId: d01b2535ed22f716864c005fa8f777796f08568e

--- plugins/toml/src/main/kotlin/org/toml/lang/parse/TomlParserUtil.kt
+++ plugins/toml/src/main/kotlin/org/toml/lang/parse/TomlParserUtil.kt
@@ -7,6 +7,7 @@ package org.toml.lang.parse
 
 import com.intellij.lang.PsiBuilder
 import com.intellij.lang.parser.GeneratedParserUtilBase
+import com.intellij.psi.TokenType
 import com.intellij.psi.tree.IElementType
 
 
@@ -21,4 +22,23 @@ object TomlParserUtil : GeneratedParserUtilBase() {
         }
         return false
     }
+
+    @JvmStatic
+    fun atSameLine(b: PsiBuilder, level: Int, parser: Parser): Boolean {
+        val marker = enter_section_(b)
+        addVariant(b, "VALUE")
+        b.tokenType // skip whitespace
+        val result = !isNextAfterNewLine(b) && parser.parse(b, level)
+        exit_section_(b, marker, null, result)
+        return result
+    }
+}
+
+private fun isNextAfterNewLine(b: PsiBuilder) =
+    b.rawLookup(-1) == TokenType.WHITE_SPACE && b.rawLookupText(-1).contains("\n")
+
+private fun PsiBuilder.rawLookupText(steps: Int): CharSequence {
+    val start = rawTokenTypeStart(steps)
+    val end = rawTokenTypeStart(steps + 1)
+    return if (start == -1 || end == -1) "" else originalText.subSequence(start, end)
 }
