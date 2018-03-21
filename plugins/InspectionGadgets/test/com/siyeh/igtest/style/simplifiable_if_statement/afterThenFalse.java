559f4ec74d24b2a120d3ea491c370dfd17ab7ff8
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2018-03-21 12:32:31 +0700

    Merge ReplaceIfWithConditionalIntention into SimplifiableIfStatement inspection
    Behavior is unified; method calls supported in SimplifiableIfStatement; join declaration and assignment automatically (IDEA-188480)

---
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/style/simplifiable_if_statement/afterThenFalse.java
@@ -0,0 +1,8 @@
+// "Replace 'if else' with '&&'" "GENERIC_ERROR_OR_WARNING"
+package com.siyeh.ipp.trivialif.replaceIfWithConditional;
+
+class Test {
+  boolean test(boolean a, boolean b, boolean c) {
+      return !a && (b || c);
+  }
+}
