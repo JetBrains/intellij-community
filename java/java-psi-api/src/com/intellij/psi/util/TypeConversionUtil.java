17914a8485c69d981fa72522f4a260974457c235
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2018-03-22 16:56:13 +0300

    [jvm-lang] add JvmPrimitiveTypeKind enum
    This eliminates dependency on PsiType.XXX types in JVM code, and
    provides ability to use custom JvmPrimitiveType-s as long as their kind
    is correct.
    
    Some plugins use PsiPrimitiveType with arbitrary name to represent fake
    types (since it was convenient not to implement all PsiType methods).
    This ability was preserved but deprecated.

--- java/java-psi-api/src/com/intellij/psi/util/TypeConversionUtil.java
+++ java/java-psi-api/src/com/intellij/psi/util/TypeConversionUtil.java
@@ -1,20 +1,7 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.util;
 
+import com.intellij.lang.jvm.types.JvmPrimitiveTypeKind;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectRootModificationTracker;
@@ -942,7 +929,7 @@ public class TypeConversionUtil {
         () -> {
           final JavaPsiFacade facade = JavaPsiFacade.getInstance(project);
           final Set<String> set = new THashSet<>();
-          for (final String qname : PsiPrimitiveType.getAllBoxedTypeNames()) {
+          for (final String qname : JvmPrimitiveTypeKind.getBoxedFqns()) {
             final PsiClass boxedClass = facade.findClass(qname, GlobalSearchScope.allScope(project));
             InheritanceUtil.processSupers(boxedClass, true, psiClass1 -> {
               ContainerUtil.addIfNotNull(set, psiClass1.getQualifiedName());
