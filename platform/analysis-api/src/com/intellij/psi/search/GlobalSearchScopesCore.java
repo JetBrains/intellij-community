bf30925e3eed88ac53b96822ebc0654e2a47f42d
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2018-03-23 15:36:54 +0300

    IDEA-187611: basic icons support int the scope chooser combobox

--- platform/analysis-api/src/com/intellij/psi/search/GlobalSearchScopesCore.java
+++ platform/analysis-api/src/com/intellij/psi/search/GlobalSearchScopesCore.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.search;
 
 import com.intellij.openapi.module.Module;
@@ -35,6 +21,7 @@ import com.intellij.util.containers.ContainerUtil;
 import java.util.HashSet;
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.Icon;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Objects;
@@ -106,6 +93,12 @@ public class GlobalSearchScopesCore {
     @Override
     public String getDisplayName() {
       return mySet.getName();
+    }
+
+    @NotNull
+    @Override
+    public Icon getDisplayIcon() {
+      return mySet.getIcon();
     }
 
     @NotNull
