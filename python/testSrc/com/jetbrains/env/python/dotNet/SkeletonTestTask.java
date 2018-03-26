f711edc17b47fbb5e78966005a57168e952cef0d
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2018-03-27 01:02:52 +0300

    test fix: use correct modality state

--- python/testSrc/com/jetbrains/env/python/dotNet/SkeletonTestTask.java
+++ python/testSrc/com/jetbrains/env/python/dotNet/SkeletonTestTask.java
@@ -116,7 +116,7 @@ class SkeletonTestTask extends PyExecutionFixtureTestTask {
     myFixture.enableInspections(PyUnresolvedReferencesInspection.class); // This inspection should suggest us to generate stubs
 
 
-    UIUtil.invokeAndWaitIfNeeded((Runnable)() -> {
+    ApplicationManager.getApplication().invokeAndWait(() -> {
       PsiDocumentManager.getInstance(myFixture.getProject()).commitAllDocuments();
       final String intentionName = PyBundle.message("sdk.gen.stubs.for.binary.modules", myUseQuickFixWithThisModuleOnly);
       IntentionAction intention = myFixture.findSingleIntention(intentionName);
@@ -132,7 +132,7 @@ class SkeletonTestTask extends PyExecutionFixtureTestTask {
                         Matchers.instanceOf(GenerateBinaryStubsFix.class));
       final Task fixTask = ((GenerateBinaryStubsFix)quickFix).getFixTask(myFixture.getFile());
       fixTask.run(new AbstractProgressIndicatorBase());
-    });
+    }, ModalityState.defaultModalityState());
 
     FileUtil.copy(skeletonFile, new File(myFixture.getTempDirPath(), skeletonFile.getName()));
     if (myExpectedSkeletonFile != null) {
