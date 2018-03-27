17e017e971e917f0ab57adac1c7181d201f8bd16
Irina.Chernushina <Irina.Chernushina@jetbrains.com>
2018-03-27 12:33:31 +0200

    v8 heap snapshots: take snapshot with v8-inspector
    - in debug, use existing vm connection; if --debug-brk was passed (for Node.js < 7.0.0), Run mode should be used to take snapshots
    - for run, pass --inspect=port to Node.js parameters from profiling extension and open vm connection from take snapshot action (close connection after the work is done)
    - simplify the logic since proxy for v8 heap snapshots is not needed any more
    - remove not needed fields from ui
    
    WEB-29269 Support Node.js heap profiling with Inspector APIs

--- platform/script-debugger/protocol/protocol-reader-runtime/src/org/jetbrains/jsonProtocol/EventMap.kt
+++ platform/script-debugger/protocol/protocol-reader-runtime/src/org/jetbrains/jsonProtocol/EventMap.kt
@@ -4,7 +4,7 @@ package org.jetbrains.jsonProtocol
 import com.intellij.util.containers.ContainerUtil
 import org.jetbrains.io.JsonReaderEx
 
-abstract class EventType<out T, in R : ResponseResultReader>(val methodName: String) {
+abstract class EventType<T, R : ResponseResultReader>(val methodName: String) {
   abstract fun read(protocolReader: R, reader: JsonReaderEx): T
 }
 
@@ -13,7 +13,7 @@ class EventMap<R : ResponseResultReader>(private val protocolReader: R) {
   private val nameToType = ContainerUtil.newConcurrentMap<String, EventType<*, R>>()
 
   fun <T : Any?> add(type: EventType<T, R>, handler: (T) -> Unit) {
-    nameToType[type.methodName] = type
+    nameToType.put(type.methodName, type)
     @Suppress("UNCHECKED_CAST")
     nameToHandler.getOrPut(type.methodName, { ContainerUtil.createLockFreeCopyOnWriteList() }).add(handler as (Any?) -> Unit)
   }
@@ -23,14 +23,14 @@ class EventMap<R : ResponseResultReader>(private val protocolReader: R) {
     nameToHandler[type.methodName]?.remove(handler as (Any?) -> Unit)
   }
 
-  fun <T> addMulti(vararg types: EventType<T, R>, eventHandler: (T) -> Unit) {
+  fun <T> addMulti(vararg types: EventType<out T, R>, eventHandler: (T) -> Unit) {
     for (type in types) {
       add(type, eventHandler)
     }
   }
 
   fun handleEvent(method: String, data: JsonReaderEx?) {
-    val handlers = nameToHandler[method]
+    val handlers = nameToHandler.get(method)
     if (handlers == null || handlers.isEmpty()) {
       return
     }
@@ -38,6 +38,13 @@ class EventMap<R : ResponseResultReader>(private val protocolReader: R) {
     val eventData = data?.let { nameToType[method]!!.read(protocolReader, it) }
     for (handler in handlers) {
       handler(eventData)
+    }
+  }
+
+  fun <T : Any?> handleEvent(type: EventType<T, R>, event: T) {
+    val handlers = nameToHandler.get(type.methodName) ?: return
+    for (handler in handlers) {
+      handler(event)
     }
   }
 }
