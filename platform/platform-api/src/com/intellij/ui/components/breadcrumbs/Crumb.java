6850bbd7029f5fd8c6c7695683ca20ebc64b6637
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2018-03-28 14:40:18 +0300

    WEB-31882 support context menu for breadcrumbs (second part)

--- platform/platform-api/src/com/intellij/ui/components/breadcrumbs/Crumb.java
+++ platform/platform-api/src/com/intellij/ui/components/breadcrumbs/Crumb.java
@@ -1,12 +1,16 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ui.components.breadcrumbs;
 
-import com.intellij.util.containers.ContainerUtil;
+import com.intellij.psi.PsiElement;
+import com.intellij.ui.breadcrumbs.BreadcrumbsProvider;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.Action;
 import javax.swing.Icon;
-import java.util.Collection;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 
 /**
  * @author Sergey.Malenkov
@@ -22,8 +26,8 @@ public interface Crumb {
    * @return a list of actions for context menu
    */
   @NotNull
-  default Collection<? extends Action> getContextActions() {
-    return ContainerUtil.emptyList();
+  default List<? extends Action> getContextActions() {
+    return emptyList();
   }
 
 
@@ -31,15 +35,24 @@ public interface Crumb {
     private final Icon icon;
     private final String text;
     private final String tooltip;
-    private final Collection<? extends Action> actions;
+    private final List<? extends Action> actions;
+
+    public Impl(@NotNull BreadcrumbsProvider provider, @NotNull PsiElement element) {
+      this(provider.getElementIcon(element),
+           provider.getElementInfo(element),
+           provider.getElementTooltip(element),
+           provider.getContextActions(element));
+    }
 
     public Impl(Icon icon, String text, String tooltip, Action... actions) {
+      this(icon, text, tooltip, actions == null || actions.length == 0 ? null : asList(actions));
+    }
+
+    public Impl(Icon icon, String text, String tooltip, List<? extends Action> actions) {
       this.icon = icon;
       this.text = text;
       this.tooltip = tooltip;
-      this.actions = actions == null || actions.length == 0
-                     ? ContainerUtil.emptyList()
-                     : ContainerUtil.list(actions);
+      this.actions = actions;
     }
 
     @Override
@@ -54,8 +67,8 @@ public interface Crumb {
 
     @NotNull
     @Override
-    public Collection<? extends Action> getContextActions() {
-      return actions;
+    public List<? extends Action> getContextActions() {
+      return actions != null ? actions : emptyList();
     }
 
     @Override
