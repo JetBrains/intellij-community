b6358044482b2bd089b50cb094ae5056f86c8cf2
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2018-03-30 13:19:49 +0300

    Use experimental hash tags in diagnostic messages

--- platform/lang-impl/src/com/intellij/formatting/RangesAssert.java
+++ platform/lang-impl/src/com/intellij/formatting/RangesAssert.java
@@ -16,13 +16,11 @@
 package com.intellij.formatting;
 
 import com.intellij.diagnostic.LogMessageEx;
-import com.intellij.lang.LanguageFormatting;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.formatter.FormattingDocumentModelImpl;
 import com.intellij.psi.impl.DebugUtil;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
@@ -31,31 +29,25 @@ class RangesAssert {
   
   public void assertInvalidRanges(final int startOffset, final int newEndOffset, FormattingDocumentModel model, String message) {
     final StringBuilder buffer = new StringBuilder();
-    buffer.append("Invalid formatting blocks:").append(message).append("\n");
-    buffer.append("Start offset:");
-    buffer.append(startOffset);
-    buffer.append(" end offset:");
-    buffer.append(newEndOffset);
-    buffer.append("\n");
 
-    int minOffset = Math.max(Math.min(startOffset, newEndOffset) - 20, 0);
-    int maxOffset = Math.min(Math.max(startOffset, newEndOffset) + 20, model.getTextLength());
-
-    buffer.append("Affected text fragment:[").append(minOffset).append(",").append(maxOffset).append("] - '")
-      .append(model.getText(new TextRange(minOffset, maxOffset))).append("'\n");
+    int minOffset = Math.max(Math.min(startOffset, newEndOffset), 0);
+    int maxOffset = Math.min(Math.max(startOffset, newEndOffset), model.getTextLength());
 
     final StringBuilder messageBuffer =  new StringBuilder();
-    messageBuffer.append("Invalid ranges during formatting");
+    messageBuffer.append(message);
     if (model instanceof FormattingDocumentModelImpl) {
-      messageBuffer.append(" in ").append(((FormattingDocumentModelImpl)model).getFile().getLanguage());
+      messageBuffer.append(" in #").append(((FormattingDocumentModelImpl)model).getFile().getLanguage().getDisplayName());
     }
+    messageBuffer.append(" #formatter");
+    messageBuffer.append("\nRange: [").append(startOffset).append(",").append(newEndOffset).append("], ")
+                 .append("text fragment: [").append(minOffset).append(",").append(maxOffset).append("] - '")
+                 .append(model.getText(new TextRange(minOffset, maxOffset))).append("'\n");
 
     buffer.append("File text:(").append(model.getTextLength()).append(")\n'");
     buffer.append(model.getText(new TextRange(0, model.getTextLength())).toString());
     buffer.append("'\n");
     buffer.append("model (").append(model.getClass()).append("): ").append(model);
 
-    Throwable currentThrowable = new Throwable();
     if (model instanceof FormattingDocumentModelImpl) {
       final FormattingDocumentModelImpl modelImpl = (FormattingDocumentModelImpl)model;
       buffer.append("Psi Tree:\n");
@@ -66,26 +58,9 @@ class RangesAssert {
         DebugUtil.treeToBuffer(buffer, root.getNode(), 0, false, true, true, true);
       }
       buffer.append('\n');
-      currentThrowable = makeLanguageStackTrace(currentThrowable, file);
     }
 
-    LogMessageEx.error(LOG, messageBuffer.toString(), currentThrowable, buffer.toString());
+    LogMessageEx.error(LOG, messageBuffer.toString(), buffer.toString());
   }
-  
-  private static Throwable makeLanguageStackTrace(@NotNull Throwable currentThrowable, @NotNull PsiFile file) {
-    Throwable langThrowable = new Throwable();
-    FormattingModelBuilder builder = LanguageFormatting.INSTANCE.forContext(file);
-    if (builder == null) return currentThrowable;
-    Class builderClass = builder.getClass();
-    Class declaringClass = builderClass.getDeclaringClass();
-    String guessedFileName = (declaringClass == null ? builderClass.getSimpleName() : declaringClass.getSimpleName())  + ".java";
-    StackTraceElement ste = new StackTraceElement(builder.getClass().getName(), "createModel", guessedFileName, 1);
-    StackTraceElement[] originalStackTrace = currentThrowable.getStackTrace();
-    StackTraceElement[] modifiedStackTrace = new StackTraceElement[originalStackTrace.length + 1];
-    System.arraycopy(originalStackTrace, 0, modifiedStackTrace, 1, originalStackTrace.length);
-    modifiedStackTrace[0] = ste;
-    langThrowable.setStackTrace(modifiedStackTrace);
-    return langThrowable;
-  }
-  
+
 }
