ae406b49c3aaa7c10e367bcf3fbd13711db4be47
Alexander Zolotov <goldifit@gmail.com>
2018-04-01 19:46:20 +0300

    SMTestProxy cleanup
    - deprecate full alias of addOutput method
    - finalize other alias methods, so client won't miss all ERR/OUT/SYS
    events
    - get rid of yellow code in tests

--- plugins/gradle/java/src/execution/test/runner/GradleSMTestProxy.java
+++ plugins/gradle/java/src/execution/test/runner/GradleSMTestProxy.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.gradle.execution.test.runner;
 
 import com.intellij.execution.testframework.Printable;
@@ -20,6 +6,7 @@ import com.intellij.execution.testframework.Printer;
 import com.intellij.execution.testframework.sm.runner.SMTestProxy;
 import com.intellij.execution.ui.ConsoleViewContentType;
 import com.intellij.openapi.util.Key;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -37,7 +24,7 @@ public class GradleSMTestProxy extends SMTestProxy {
   }
 
   @Override
-  public void addStdOutput(String output, Key outputType) {
+  public void addOutput(@NotNull String output, @NotNull Key outputType) {
     addLast(new Printable() {
       public void printOn(final Printer printer) {
         ConsoleViewContentType contentType = ConsoleViewContentType.getConsoleViewType(outputType);
