93c0277ec35be00bc23e071d74d5dd85a31e78f7
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2018-04-03 20:32:04 +0200

    [jvm-lang] defuse run line marker test

--- java/java-tests/testSrc/com/intellij/java/codeInsight/navigation/JvmRunLineMarkerTest.java
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/navigation/JvmRunLineMarkerTest.java
@@ -1,10 +1,7 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.java.codeInsight.navigation;
 
-import com.intellij.idea.Bombed;
 import com.intellij.openapi.util.registry.Registry;
-
-import java.util.Calendar;
 
 public class JvmRunLineMarkerTest extends RunLineMarkerTest {
 
@@ -12,11 +9,5 @@ public class JvmRunLineMarkerTest extends RunLineMarkerTest {
   protected void setUp() throws Exception {
     super.setUp();
     Registry.get("ide.jvm.run.marker").setValue(true, getTestRootDisposable());
-  }
-
-  @Bombed(month = Calendar.APRIL, day = 20, user = "Daniil Ovchinnikov", description = "will be resolved after IDEA-CR-30999 is merged")
-  @Override
-  public void testTestClassWithMain() {
-    super.testTestClassWithMain();
   }
 }
