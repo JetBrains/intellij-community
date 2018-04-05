f6ba4b6646382cc23b1d41b44658aa3157ef179a
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2018-04-06 00:55:24 +0300

    add GlobalSearchScope#union(Collection) overload

--- platform/core-api/src/com/intellij/psi/search/GlobalSearchScope.java
+++ platform/core-api/src/com/intellij/psi/search/GlobalSearchScope.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.search;
 
 import com.intellij.openapi.fileTypes.FileType;
@@ -22,8 +8,8 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.FileIndexFacade;
 import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.CompactVirtualFileSet;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiBundle;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
@@ -189,6 +175,18 @@ public abstract class GlobalSearchScope extends SearchScope implements ProjectAw
   @Contract(pure = true)
   public GlobalSearchScope uniteWith(@NotNull GlobalSearchScope scope) {
     return UnionScope.create(new GlobalSearchScope[]{this, scope});
+  }
+
+  @NotNull
+  @Contract(pure = true)
+  public static GlobalSearchScope union(@NotNull Collection<? extends GlobalSearchScope> scopes) {
+    if (scopes.isEmpty()) {
+      throw new IllegalArgumentException("Empty scope collection");
+    }
+    if (scopes.size() == 1) {
+      return scopes.iterator().next();
+    }
+    return UnionScope.create(scopes.toArray(EMPTY_ARRAY));
   }
 
   @NotNull
