cb05ef7bda16a6922bfef9cee19e6a61e4a61dcf
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2018-04-05 23:55:24 +0200

    add GlobalSearchScope#union(Collection) overload

--- platform/core-api/src/com/intellij/psi/search/GlobalSearchScope.java
+++ platform/core-api/src/com/intellij/psi/search/GlobalSearchScope.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.search;
 
 import com.intellij.openapi.fileTypes.FileType;
@@ -179,6 +165,18 @@ public abstract class GlobalSearchScope extends SearchScope implements ProjectAw
     if (scope == this) return scope;
 
     return new UnionScope(this, scope);
+  }
+
+  @NotNull
+  @Contract(pure = true)
+  public static GlobalSearchScope union(@NotNull Collection<? extends GlobalSearchScope> scopes) {
+    if (scopes.isEmpty()) {
+      throw new IllegalArgumentException("Empty scope collection");
+    }
+    if (scopes.size() == 1) {
+      return scopes.iterator().next();
+    }
+    return new UnionScope(scopes.toArray(new GlobalSearchScope[0]));
   }
 
   @NotNull
