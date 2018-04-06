6c96dc17be40053635348e6b348e5dc1371517f8
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2018-04-06 18:26:42 +0200

    add link between model reference search and old reference search

---
+++ java/java-indexing-impl/src/com/intellij/psi/impl/search/ModelToPsiReferenceSearcher.kt
@@ -0,0 +1,69 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.psi.impl.search
+
+import com.intellij.model.ModelElement
+import com.intellij.model.ModelReference
+import com.intellij.model.ModelService
+import com.intellij.model.search.DefaultModelReferenceSearchParameters
+import com.intellij.model.search.ModelReferenceSearch
+import com.intellij.openapi.application.QueryExecutorBase
+import com.intellij.psi.PsiReference
+import com.intellij.psi.search.QuerySearchRequest
+import com.intellij.psi.search.SearchRequestCollector
+import com.intellij.psi.search.searches.ReferencesSearch
+import com.intellij.util.AbstractQuery
+import com.intellij.util.PairProcessor
+import com.intellij.util.Processor
+import com.intellij.util.Query
+
+/**
+ * Includes [ModelReferenceSearch] results into [ReferencesSearch] results.
+ *
+ * @see PsiToModelSearchRequestor
+ */
+class ModelToPsiReferenceSearcher : QueryExecutorBase<PsiReference, ReferencesSearch.SearchParameters>(false) {
+
+  override fun processQuery(queryParameters: ReferencesSearch.SearchParameters, consumer: Processor<PsiReference>) {
+    if (queryParameters is PsiToModelSearchRequestor.ModelToPsiParameters) {
+      // search started from ModelReferenceSearch
+      // -> PsiToModelSearchRequestor queries ReferenceSearch with ModelToPsiParameters
+      // -> don't query ModelReferenceSearch again because we started from there
+      return
+    }
+    val modelElement = ModelService.adaptPsiElement(queryParameters.elementToSearch)
+    val modelParameters = PsiToModelParameters(modelElement, queryParameters)
+    val modelQuery = ModelReferenceSearch.search(modelParameters)
+    val psiQuery = adaptQuery(modelQuery)
+    val collector = queryParameters.optimizer
+    val nested = SearchRequestCollector(collector.searchSession)
+    val request = QuerySearchRequest(psiQuery, nested, false, PairProcessor { ref, _ -> consumer.process(ref) })
+    collector.searchQuery(request)
+  }
+
+  private fun adaptQuery(modelQuery: Query<ModelReference>): Query<PsiReference> {
+    return object : AbstractQuery<PsiReference>() {
+      override fun processResults(consumer: Processor<PsiReference>): Boolean = modelQuery.forEach(adaptProcessor(consumer))
+    }
+  }
+
+  private fun adaptProcessor(psiProcessor: Processor<PsiReference>): Processor<ModelReference> {
+    return Processor { modelReference ->
+      if (modelReference is PsiReference) {
+        psiProcessor.process(modelReference)
+      }
+      else {
+        TODO("Found reference: $modelReference, class: ${modelReference.javaClass}")
+      }
+    }
+  }
+
+  internal class PsiToModelParameters(
+    target: ModelElement,
+    psiParameters: ReferencesSearch.SearchParameters
+  ) : DefaultModelReferenceSearchParameters(
+    psiParameters.project,
+    target,
+    psiParameters.scopeDeterminedByUser,
+    psiParameters.isIgnoreAccessScope
+  )
+}
---
+++ java/java-indexing-impl/src/com/intellij/psi/impl/search/PsiToModelSearchRequestor.kt
@@ -0,0 +1,46 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.psi.impl.search
+
+import com.intellij.model.ModelReference
+import com.intellij.model.ModelService
+import com.intellij.model.search.ModelReferenceSearchParameters
+import com.intellij.model.search.SearchRequestor
+import com.intellij.model.search.SearchSession
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiReference
+import com.intellij.psi.search.searches.ReferencesSearch
+import com.intellij.util.CustomProcessorQuery
+import com.intellij.util.Processor
+import com.intellij.util.Query
+
+/**
+ * Includes [ReferencesSearch] results into [com.intellij.model.search.ModelReferenceSearch] results.
+ *
+ * @see ModelToPsiReferenceSearcher
+ */
+class PsiToModelSearchRequestor : SearchRequestor {
+
+  override fun collectSearchRequests(session: SearchSession, parameters: ModelReferenceSearchParameters) {
+    if (parameters is ModelToPsiReferenceSearcher.PsiToModelParameters) {
+      // search started from ReferencesSearch
+      // -> ModelToPsiReferenceSearcher queries ModelReferenceSearch with PsiToModelParameters
+      // -> don't query ReferenceSearch again because we started from there
+      return
+    }
+    val psiElement = ModelService.getPsiElement(parameters.target) ?: return
+    val psiSearchParameters = ModelToPsiParameters(psiElement, parameters)
+    val psiQuery: Query<PsiReference> = ReferencesSearch.search(psiSearchParameters)
+    session.searchSubQuery(CustomProcessorQuery(psiQuery, ::adaptProcessor))
+  }
+
+  private fun adaptProcessor(modelProcessor: Processor<ModelReference>) = Processor<PsiReference>(modelProcessor::process)
+
+  internal class ModelToPsiParameters(
+    target: PsiElement,
+    parameters: ModelReferenceSearchParameters
+  ) : ReferencesSearch.SearchParameters(
+    target,
+    parameters.originalSearchScope,
+    parameters.isIgnoreAccessScope
+  )
+}
