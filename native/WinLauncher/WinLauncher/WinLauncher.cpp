dd969da3a52da34ae175d3730c6d508b45fce58d
Vladimir.Orlov <Vladimir.Orlov@jetbrains.com>
2018-04-06 10:17:19 +0300

    Win launchers: added support java 9 and 10.
    IDEA-189436 IDEA fails to launch with JDK 10 set as IDEA_JDK_64.

--- native/WinLauncher/WinLauncher/WinLauncher.cpp
+++ native/WinLauncher/WinLauncher/WinLauncher.cpp
@@ -192,12 +192,20 @@ bool FindJVMInRegistryKey(const char* key, bool wow64_32)
 bool FindJVMInRegistryWithVersion(const char* version, bool wow64_32)
 {
   char* keyName = "Java Runtime Environment";
+  // starting from java 9 key name has been changed
+  char* jreKeyName = "JRE";
+  char* jdkKeyName = "JDK";
+
   bool foundJava = false;
   char buf[_MAX_PATH];
   //search jre in registry if the product doesn't require tools.jar
   if (LoadStdString(IDS_JDK_ONLY) != std::string("true")) {
     sprintf_s(buf, "Software\\JavaSoft\\%s\\%s", keyName, version);
     foundJava = FindJVMInRegistryKey(buf, wow64_32);
+    if (!foundJava) {
+      sprintf_s(buf, "Software\\JavaSoft\\%s\\%s", jreKeyName, version);
+      foundJava = FindJVMInRegistryKey(buf, wow64_32);
+    }
   }
 
   //search jdk in registry if the product requires tools.jar or jre isn't installed.
@@ -205,6 +213,10 @@ bool FindJVMInRegistryWithVersion(const char* version, bool wow64_32)
     keyName = "Java Development Kit";
     sprintf_s(buf, "Software\\JavaSoft\\%s\\%s", keyName, version);
     foundJava = FindJVMInRegistryKey(buf, wow64_32);
+    if (!foundJava) {
+      sprintf_s(buf, "Software\\JavaSoft\\%s\\%s", jdkKeyName, version);
+      foundJava = FindJVMInRegistryKey(buf, wow64_32);
+    }
   }
   return foundJava;
 }
@@ -214,6 +226,12 @@ bool FindJVMInRegistry()
 #ifndef _M_X64
   if (FindJVMInRegistryWithVersion("1.8", true))
     return true;
+  if (FindJVMInRegistryWithVersion("9", true))
+    return true;
+  if (FindJVMInRegistryWithVersion("10", true))
+    return true;
+
+  //obsolete java versions
   if (FindJVMInRegistryWithVersion("1.7", true))
     return true;
   if (FindJVMInRegistryWithVersion("1.6", true))
@@ -222,6 +240,12 @@ bool FindJVMInRegistry()
 
   if (FindJVMInRegistryWithVersion("1.8", false))
     return true;
+  if (FindJVMInRegistryWithVersion("9", false))
+    return true;
+  if (FindJVMInRegistryWithVersion("10", false))
+    return true;
+
+  //obsolete java versions
   if (FindJVMInRegistryWithVersion("1.7", false))
     return true;
   if (FindJVMInRegistryWithVersion("1.6", false))
