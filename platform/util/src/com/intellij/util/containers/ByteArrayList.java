26bb888708eb7d5bb597a414821a054179113988
nik <Nikolay.Chashnikov@jetbrains.com>
2018-04-06 15:11:03 +0300

    util: restore dummy version of ByteArrayList to avoid API incompatibilities

---
+++ platform/util/src/com/intellij/util/containers/ByteArrayList.java
@@ -0,0 +1,20 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.util.containers;
+
+import gnu.trove.TByteArrayList;
+
+/**
+ * @deprecated use {@link TByteArrayList instead}
+ */
+public class ByteArrayList extends TByteArrayList {
+  public ByteArrayList() {
+  }
+
+  public ByteArrayList(int capacity) {
+    super(capacity);
+  }
+
+  public ByteArrayList(byte[] values) {
+    super(values);
+  }
+}
