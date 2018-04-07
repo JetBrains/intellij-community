83ebaa7f6448756d885445acf8c1ed6b1764ddcf
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2018-04-07 09:03:24 +0300

    Extract common conjunct from if-else chain in ExtractIfConditionAction
    Fixes IDEA-179499 New inspection: Should break out common conjunct from "if / else if"

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/extractIfCondition/afterRepeating.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/extractIfCondition/afterRepeating.java
@@ -1,14 +1,13 @@
 // "Extract if (a)" "true"
-class TestThreadInspection {
+class Test {
   void f(boolean a, boolean b, boolean c){
+      /*2*/
       if (a) {
           if (b) {
               System.out.println("a&b");//first comment
           } else if (c) {
-              System.out.println("c");
+              System.out.println("a&c"); // three
           }
-      } else if (c) {
-          System.out.println("c");
       }
   }
 }
