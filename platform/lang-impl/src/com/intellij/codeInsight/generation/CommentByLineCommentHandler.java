a27a9a50774d7650db1685f76f48084f2fb72383
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2018-04-09 12:53:26 +0300

    EA-53735 - assert: CommentByLineCommentHandler.doUncommentLine

--- platform/lang-impl/src/com/intellij/codeInsight/generation/CommentByLineCommentHandler.java
+++ platform/lang-impl/src/com/intellij/codeInsight/generation/CommentByLineCommentHandler.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.codeInsight.generation;
 
@@ -530,6 +516,7 @@ public class CommentByLineCommentHandler extends MultiCaretCodeInsightActionHand
   private static boolean doUncommentLine(int line, Document document, Commenter commenter, int startOffset, int endOffset, boolean removeSpace) {
     String prefix = commenter.getLineCommentPrefix();
     if (prefix != null) {
+      int originalPrefixLength = prefix.length();
       if (removeSpace) prefix += ' ';
       CharSequence chars = document.getCharsSequence();
 
@@ -543,18 +530,16 @@ public class CommentByLineCommentHandler extends MultiCaretCodeInsightActionHand
           theEnd--;
         }
 
-
-        String lineText = document.getText(new TextRange(startOffset, theEnd));
-        if (lineText.indexOf(suffix) != -1) {
-          int start = startOffset + lineText.indexOf(suffix);
-          document.deleteString(start, start + suffix.length());
+        int prefixPos = CharArrayUtil.indexOf(chars, suffix, startOffset + originalPrefixLength, theEnd);
+        if (prefixPos != -1) {
+          document.deleteString(prefixPos, prefixPos + suffix.length());
         }
       }
 
       boolean matchesTrimmed = false;
       boolean commented = CharArrayUtil.regionMatches(chars, startOffset, prefix) ||
                           (matchesTrimmed = prefix.endsWith(" ") && CharArrayUtil.regionMatches(chars, startOffset, prefix.trim()));
-      assert commented;
+      assert commented : "Commenter: " + commenter;
 
       int charsToDelete = matchesTrimmed ? prefix.trim().length() : prefix.length();
       document.deleteString(startOffset, startOffset + charsToDelete);
