f72ba49ca7cc53fb5c60b6acdcb89400d2b55dcc
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2018-04-11 16:17:33 +0300

    svn: Convert "InfoStorage", "SvnBranchItem" to kotlin
    (cherry picked from commit aa1b659ad085f2105d7916ab56bf3355cd1e6bf4)
    (cherry picked from commit 5732e2b4de55c445d4fa97749f7f2a7729c0d581)

--- plugins/svn4idea/src/org/jetbrains/idea/svn/branchConfig/SvnBranchItem.kt
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/branchConfig/SvnBranchItem.kt
@@ -1,34 +1,8 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.idea.svn.branchConfig;
+package org.jetbrains.idea.svn.branchConfig
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.idea.svn.api.Url;
+import org.jetbrains.idea.svn.api.Url
 
-public class SvnBranchItem implements Comparable<SvnBranchItem> {
-  @NotNull private final Url myUrl;
-  private final long myCreationDateMillis;
-  private final long myRevision;
-
-  public SvnBranchItem(@NotNull Url url, long creationDateMillis, long revision) {
-    myUrl = url;
-    myCreationDateMillis = creationDateMillis;
-    myRevision = revision;
-  }
-
-  @NotNull
-  public Url getUrl() {
-    return myUrl;
-  }
-
-  public long getCreationDateMillis() {
-    return myCreationDateMillis;
-  }
-
-  public long getRevision() {
-    return myRevision;
-  }
-
-  public int compareTo(@NotNull SvnBranchItem item) {
-    return Long.compare(item.myCreationDateMillis, myCreationDateMillis);
-  }
+class SvnBranchItem(val url: Url, val creationDateMillis: Long, val revision: Long) : Comparable<SvnBranchItem> {
+  override fun compareTo(other: SvnBranchItem) = other.creationDateMillis.compareTo(creationDateMillis)
 }
