96e0c59d5f4b47f2dcbbc6f0c0cb1d0e611da2cc
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-04-11 12:18:21 +0200

    convert IconCache to kotlin

--- platform/platform-api/src/com/intellij/util/ui/IconCache.java
+++ platform/platform-api/src/com/intellij/util/ui/IconCache.java
@@ -1,71 +1,79 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.util.ui;
+package com.intellij.util.ui
 
-import com.intellij.openapi.util.IconLoader;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.annotations.TestOnly;
+import com.intellij.openapi.util.IconLoader
+import org.jetbrains.annotations.TestOnly
 
-import javax.swing.*;
-import java.util.HashMap;
+import javax.swing.*
+import java.util.HashMap
 
 /**
  * @author Konstantin Bulenkov
  */
-public class IconCache {
-  private static final HashMap<String, Icon> cache = new HashMap<>();
+object IconCache {
+  private val cache = HashMap<String, Icon>()
 
-  public static Icon getIcon(@NotNull String name, boolean editable, boolean selected, boolean focused, boolean enabled, boolean pressed) {
-    return getIcon(name, editable, selected, focused, enabled, pressed, true);
+  fun getIcon(name: String, editable: Boolean, selected: Boolean, focused: Boolean, enabled: Boolean, pressed: Boolean): Icon? {
+    return getIcon(name, editable, selected, focused, enabled, pressed, true)
   }
 
-  @Nullable
-  private static Icon getIcon(@NotNull String name, boolean editable, boolean selected, boolean focused, boolean enabled, boolean pressed, boolean findIfNotInCache) {
-    String key = name;
-    if (editable) key += "Editable";
-    if (selected) key+= "Selected";
+  private fun getIcon(name: String,
+                      editable: Boolean,
+                      selected: Boolean,
+                      focused: Boolean,
+                      enabled: Boolean,
+                      pressed: Boolean,
+                      findIfNotInCache: Boolean): Icon? {
+    var key = name
+    if (editable) key += "Editable"
+    if (selected) key += "Selected"
 
-    if (pressed) key += "Pressed";
-    else if (focused) key+= "Focused";
-    else if (!enabled) key+="Disabled";
+    if (pressed)
+      key += "Pressed"
+    else if (focused)
+      key += "Focused"
+    else if (!enabled) key += "Disabled"
 
-    String dir = "";
+    var dir = ""
 
     // For Mac blue theme and other LAFs use default directory icons
-    if (UIUtil.isUnderDefaultMacTheme()) dir = UIUtil.isGraphite() ? "graphite/" : "";
-    else if (UIUtil.isUnderWin10LookAndFeel()) dir = "win10/";
-    else if (UIUtil.isUnderDarcula()) dir = "darcula/";
-    else if (UIUtil.isUnderIntelliJLaF()) dir = "intellij/";
+    if (UIUtil.isUnderDefaultMacTheme())
+      dir = if (UIUtil.isGraphite()) "graphite/" else ""
+    else if (UIUtil.isUnderWin10LookAndFeel())
+      dir = "win10/"
+    else if (UIUtil.isUnderDarcula())
+      dir = "darcula/"
+    else if (UIUtil.isUnderIntelliJLaF()) dir = "intellij/"
 
-    key = dir + key;
+    key = dir + key
 
-    Icon icon = cache.get(key);
+    var icon: Icon? = cache[key]
     if (icon == null && findIfNotInCache) {
-      icon = IconLoader.findIcon("/com/intellij/ide/ui/laf/icons/" + key + ".png", IconCache.class, true);
-      cache.put(key, icon);
+      icon = IconLoader.findIcon("/com/intellij/ide/ui/laf/icons/$key.png", IconCache::class.java, true)
+      cache[key] = icon
     }
-    return icon;
+    return icon
   }
 
-  public static Icon getIcon(String name, boolean editable, boolean selected, boolean focused, boolean enabled) {
-    return getIcon(name, editable, selected, focused, enabled, false, true);
+  fun getIcon(name: String, editable: Boolean, selected: Boolean, focused: Boolean, enabled: Boolean): Icon? {
+    return getIcon(name, editable, selected, focused, enabled, false, true)
   }
 
-  public static Icon getIcon(String name, boolean selected, boolean focused, boolean enabled) {
-    return getIcon(name, false, selected, focused, enabled);
+  fun getIcon(name: String, selected: Boolean, focused: Boolean, enabled: Boolean): Icon? {
+    return getIcon(name, false, selected, focused, enabled)
   }
 
-  public static Icon getIcon(@NotNull String name, boolean selected, boolean focused) {
-    return getIcon(name, false, selected, focused, true);
+  fun getIcon(name: String, selected: Boolean, focused: Boolean): Icon? {
+    return getIcon(name, false, selected, focused, true)
   }
 
-  public static Icon getIcon(@NotNull String name) {
-    return getIcon(name, false, false, false, true, false, true);
+  fun getIcon(name: String): Icon? {
+    return getIcon(name, false, false, false, true, false, true)
   }
 
   // this method will be not required when this class will be converted to Kotlin (since Kotlin supports named parameters)
   @TestOnly
-  public static Icon getCachedIcon(@NotNull String name, boolean selected) {
-    return getIcon(name, false, selected, false, true, false, false);
+  fun getCachedIcon(name: String, selected: Boolean): Icon? {
+    return getIcon(name, false, selected, false, true, false, false)
   }
 }
