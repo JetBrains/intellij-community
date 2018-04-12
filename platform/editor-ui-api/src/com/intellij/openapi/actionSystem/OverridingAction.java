25854f4fc41c99827ec7d7712c2fa44172db94f1
Dmitry Jemerov <yole@jetbrains.com>
2018-04-12 16:51:15 +0200

    Overriding action API take 2 (IDEA-CR-31671)
    (cherry picked from commit 682c9c38ce8c599b3ad820f930ac177b5baa1e66)

--- platform/editor-ui-api/src/com/intellij/openapi/actionSystem/OverridingAction.java
+++ platform/editor-ui-api/src/com/intellij/openapi/actionSystem/OverridingAction.java
@@ -1,25 +1,10 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.actionSystem;
 
-import org.jetbrains.annotations.Nullable;
-
 /**
  * Action intended to replace an existing action using the "overrides" attribute in plugin.xml and receiving an instance of the action
  * being overridden.
+ * @see com.intellij.openapi.actionSystem.impl.ActionManagerImpl#getBaseAction
  */
-public abstract class OverridingAction extends AnAction {
-  /**
-   * The instance of the action which has been overridden by this action.
-   */
-  @Nullable
-  protected AnAction myBaseAction;
-
-  @Nullable
-  public AnAction getBaseAction() {
-    return myBaseAction;
-  }
-
-  public void setBaseAction(@Nullable AnAction baseAction) {
-    myBaseAction = baseAction;
-  }
+public interface OverridingAction {
 }
