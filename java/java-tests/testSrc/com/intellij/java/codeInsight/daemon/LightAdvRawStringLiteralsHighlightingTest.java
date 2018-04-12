7fe17dfc07740d5d7c4759728e95d1aac3e30a22
Anna.Kozlova <anna.kozlova@jetbrains.com>
2018-04-12 16:06:33 +0200

    raw string literals: disable all magic on paste (IDEA-189075)
    fix number of tics to preserve correct raw string literal

--- java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/LightAdvRawStringLiteralsHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/LightAdvRawStringLiteralsHighlightingTest.java
@@ -1,32 +1,55 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.java.codeInsight.daemon;
 
-import com.intellij.codeInsight.daemon.LightDaemonAnalyzerTestCase;
-import com.intellij.openapi.projectRoots.JavaSdkVersion;
-import com.intellij.openapi.projectRoots.Sdk;
-import com.intellij.pom.java.LanguageLevel;
-import com.intellij.testFramework.IdeaTestUtil;
+import com.intellij.JavaTestUtil;
+import com.intellij.openapi.actionSystem.IdeActions;
+import com.intellij.testFramework.LightProjectDescriptor;
+import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
+import org.jetbrains.annotations.NotNull;
 
-public class LightAdvRawStringLiteralsHighlightingTest extends LightDaemonAnalyzerTestCase {
-  private static final String BASE_PATH = "/codeInsight/daemonCodeAnalyzer/advRawStringLiteral";
+public class LightAdvRawStringLiteralsHighlightingTest extends LightCodeInsightFixtureTestCase {
 
   @Override
-  protected void setUp() throws Exception {
-    super.setUp();
-    setLanguageLevel(LanguageLevel.JDK_11_PREVIEW);
-    IdeaTestUtil.setTestVersion(JavaSdkVersion.JDK_11, getModule(), getTestRootDisposable());
+  protected String getBasePath() {
+    return JavaTestUtil.getRelativeJavaTestDataPath() + "/codeInsight/daemonCodeAnalyzer/advRawStringLiteral";
   }
 
-  private void doTest() {
-    doTest(BASE_PATH + "/" + getTestName(false) + ".java", false, false);
+  @NotNull
+  @Override
+  protected LightProjectDescriptor getProjectDescriptor() {
+    return JAVA_11_PREVIEW;
   }
 
   public void testStringAssignability() {
-    doTest();
+    doTestHighlighting();
   }
 
-  @Override
-  protected Sdk getProjectJDK() {
-    return IdeaTestUtil.getMockJdk9();
+  public void testPasteInRawStringLiteral() {
+    doTestPaste("class A {{String s = `q<caret>`;}}", "a\nb`\nc", "class A {{String s = ``qa\nb`\nc``;}}");
+  }
+
+  public void testPasteInRawStringLiteralNoTicInside() {
+    doTestPaste("class A {{String s = `a<caret>`;}}", "a\nb\nc", "class A {{String s = `aa\nb\nc`;}}");
+  }
+
+  private void doTestHighlighting() {
+    myFixture.configureByFile(getTestName(false) + ".java");
+    myFixture.checkHighlighting();
+  }
+
+  private void doTestPaste(String documentTextBefore, String textToPaste, String expectedDocumentText) {
+    myFixture.configureByText("plain.txt", "<selection>" + textToPaste + "</selection>");
+    performCopy();
+    myFixture.configureByText("a.java", documentTextBefore);
+    performPaste();
+    myFixture.checkResult(expectedDocumentText);
+  }
+
+  private void performPaste() {
+    myFixture.performEditorAction(IdeActions.ACTION_EDITOR_PASTE);
+  }
+
+  private void performCopy() {
+    myFixture.performEditorAction(IdeActions.ACTION_EDITOR_COPY);
   }
 }
