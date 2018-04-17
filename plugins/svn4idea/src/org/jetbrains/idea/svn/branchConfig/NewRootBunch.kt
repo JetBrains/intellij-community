b87f6b61f4abd6fb9c4ac496ceabd0fb5f09882e
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2018-04-17 14:00:29 +0300

    svn: Refactor "NewRootBunch"
    (cherry picked from commit 511b9ae8c5a0e4c062dc4dfd7b88c50f48113744)
    (cherry picked from commit 2dec91f9d91ccc3dedd7d4269cee4c400c6806ad)

--- plugins/svn4idea/src/org/jetbrains/idea/svn/branchConfig/NewRootBunch.kt
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/branchConfig/NewRootBunch.kt
@@ -1,119 +1,85 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.svn.branchConfig
 
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.application.ApplicationManager.getApplication
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.vcs.ProgressManagerQueue
-import org.jetbrains.annotations.CalledInBackground
 import org.jetbrains.idea.svn.SvnVcs
 import org.jetbrains.idea.svn.api.Url
-import org.jetbrains.idea.svn.commandLine.SvnBindException
 
-import java.util.*
+private val LOG = logger<NewRootBunch>()
 
 // synch is here
 class NewRootBunch(private val myProject: Project, private val myBranchesLoader: ProgressManagerQueue) {
   private val myLock = Any()
   private val myMap = mutableMapOf<VirtualFile, InfoStorage<SvnBranchConfigurationNew>>()
 
-  val mapCopy: Map<VirtualFile, SvnBranchConfigurationNew>
-    get() = synchronized(myLock) {
-      val result = mutableMapOf<VirtualFile, SvnBranchConfigurationNew>()
-      for (vf in myMap.keys) {
-        result[vf] = myMap[vf]!!.value
-      }
-      return result
+  val mapCopy get() = synchronized(myLock) { myMap.mapValues { it.value.value } }
+
+  fun updateForRoot(root: VirtualFile, config: InfoStorage<SvnBranchConfigurationNew>, reload: Boolean) = synchronized(myLock) {
+    val previous: SvnBranchConfigurationNew?
+    val override: Boolean
+    val existing = myMap[root]
+
+    if (existing == null) {
+      previous = null
+      override = true
+      myMap[root] = config
+    }
+    else {
+      previous = existing.value
+      override = existing.accept(config)
     }
 
-  fun updateForRoot(root: VirtualFile, config: InfoStorage<SvnBranchConfigurationNew>, reload: Boolean) {
-    synchronized(myLock) {
-      val previous: SvnBranchConfigurationNew?
-      val override: Boolean
-      val existing = myMap[root]
-
-      if (existing == null) {
-        previous = null
-        override = true
-        myMap[root] = config
-      }
-      else {
-        previous = existing.value
-        override = existing.accept(config)
-      }
-
-      if (reload && override) {
-        myBranchesLoader.run { reloadBranches(root, previous, config.value) }
-      }
+    if (reload && override) {
+      myBranchesLoader.run { reloadBranches(root, previous, config.value) }
     }
   }
 
-  fun updateBranches(root: VirtualFile, branchesParent: Url, items: InfoStorage<List<SvnBranchItem>>) {
-    synchronized(myLock) {
-      val existing = myMap[root]
-      if (existing == null) {
-        LOG.info("cannot update branches, branches parent not found: $branchesParent")
-      }
-      else {
-        existing.value.updateBranch(branchesParent.toString(), items)
-      }
+  fun updateBranches(root: VirtualFile, branchesParent: Url, items: InfoStorage<List<SvnBranchItem>>) = synchronized(myLock) {
+    val existing = myMap[root]
+    if (existing == null) {
+      LOG.info("cannot update branches, branches parent not found: ${branchesParent.toDecodedString()}")
+    }
+    else {
+      existing.value.updateBranch(branchesParent.toString(), items)
     }
   }
 
-  fun getConfig(root: VirtualFile): SvnBranchConfigurationNew {
-    synchronized(myLock) {
-      val value = myMap[root]
-      val result: SvnBranchConfigurationNew
-      if (value == null) {
-        result = SvnBranchConfigurationNew()
-        myMap[root] = InfoStorage(result, InfoReliability.empty)
-        myBranchesLoader.run(DefaultBranchConfigInitializer(myProject, this, root))
-      }
-      else {
-        result = value.value
-      }
-      return result
+  fun getConfig(root: VirtualFile) = synchronized(myLock) {
+    val value = myMap[root]
+    val result: SvnBranchConfigurationNew
+    if (value == null) {
+      result = SvnBranchConfigurationNew()
+      myMap[root] = InfoStorage(result, InfoReliability.empty)
+      myBranchesLoader.run(DefaultBranchConfigInitializer(myProject, this, root))
     }
+    else {
+      result = value.value
+    }
+    result
   }
 
   fun reloadBranchesAsync(root: VirtualFile, branchLocation: Url, reliability: InfoReliability) {
-    ApplicationManager.getApplication().executeOnPooledThread { reloadBranches(root, branchLocation, reliability, true) }
+    getApplication().executeOnPooledThread { reloadBranches(root, branchLocation, reliability, true) }
   }
 
   fun reloadBranches(root: VirtualFile, prev: SvnBranchConfigurationNew?, next: SvnBranchConfigurationNew) {
-    val oldUrls = if (prev == null) emptySet<Url>() else HashSet(prev.branchLocations)
+    val oldUrls = prev?.branchLocations?.toSet() ?: emptySet()
     val vcs = SvnVcs.getInstance(myProject)
     if (!vcs.isVcsBackgroundOperationsAllowed(root)) return
 
     for (newBranchUrl in next.branchLocations) {
       // check if cancel had been put
       if (!vcs.isVcsBackgroundOperationsAllowed(root)) return
-      if (!oldUrls.contains(newBranchUrl)) {
+      if (newBranchUrl !in oldUrls) {
         reloadBranches(root, newBranchUrl, InfoReliability.defaultValues, true)
       }
     }
   }
 
-  fun reloadBranches(root: VirtualFile, branchLocation: Url, reliability: InfoReliability, passive: Boolean) {
+  fun reloadBranches(root: VirtualFile, branchLocation: Url, reliability: InfoReliability, passive: Boolean) =
     BranchesLoader(myProject, this, branchLocation, reliability, root, passive).run()
-  }
-
-  @CalledInBackground
-  fun getWorkingBranch(svnurl: Url, root: VirtualFile): Url? {
-    var result: Url?
-
-    try {
-      result = myMap[root]?.value?.getWorkingBranch(svnurl)
-    }
-    catch (e: SvnBindException) {
-      result = null
-    }
-
-    return result
-  }
-
-  companion object {
-    private val LOG = Logger.getInstance("#org.jetbrains.idea.svn.branchConfig.NewRootBunch")
-  }
 }
