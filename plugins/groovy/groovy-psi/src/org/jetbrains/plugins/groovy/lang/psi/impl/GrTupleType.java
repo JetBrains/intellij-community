e2d4358096a6d1c32034449e3939817a9d65f404
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2018-04-17 17:26:52 +0300

    [groovy] partially revert d98b845
    (cherry picked from commit 40c7e80) (IDEA-CR-31828)

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/GrTupleType.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/GrTupleType.java
@@ -1,9 +1,22 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+/*
+ * Copyright 2000-2014 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.jetbrains.plugins.groovy.lang.psi.impl;
 
 import com.intellij.openapi.util.Comparing;
-import com.intellij.openapi.util.RecursionManager;
 import com.intellij.openapi.util.VolatileNotNullLazyValue;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.pom.java.LanguageLevel;
@@ -64,8 +77,7 @@ public abstract class GrTupleType extends GrLiteralClassType {
   @Override
   @NotNull
   public PsiType[] getParameters() {
-    PsiType[] parameters = RecursionManager.doPreventingRecursion(this, false, () -> myParameters.getValue());
-    return parameters == null ? PsiType.EMPTY_ARRAY : parameters;
+    return myParameters.getValue();
   }
 
   @Override
