d21efe177d18eff037ee3a0b7e285e80b977655e
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2018-04-17 14:54:27 +0200

    Get rid of Snappy library, 2 (IDEA-189365)

--- platform/util/src/com/intellij/util/io/DataInputOutputUtil.java
+++ platform/util/src/com/intellij/util/io/DataInputOutputUtil.java
@@ -24,6 +24,7 @@ import org.jetbrains.annotations.Nullable;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 /**
  * @author max
@@ -38,8 +39,36 @@ public class DataInputOutputUtil extends DataInputOutputUtilRt {
     return DataInputOutputUtilRt.readINT(record);
   }
 
+  public static int readINT(@NotNull ByteBuffer byteBuffer) {
+    final int val = byteBuffer.get() & 0xFF;
+    if (val < 192) {
+      return val;
+    }
+
+    int res = val - 192;
+    for (int sh = 6; ; sh += 7) {
+      int next = byteBuffer.get() & 0xFF;
+      res |= (next & 0x7F) << sh;
+      if ((next & 0x80) == 0) {
+        return res;
+      }
+    }
+  }
+
   public static void writeINT(@NotNull DataOutput record, int val) throws IOException {
     DataInputOutputUtilRt.writeINT(record, val);
+  }
+
+  public static void writeINT(@NotNull ByteBuffer byteBuffer, int val) {
+    if (0 > val || val >= 192) {
+      byteBuffer.put( (byte)(192 + (val & 0x3F)));
+      val >>>= 6;
+      while (val >= 128) {
+        byteBuffer.put((byte)((val & 0x7F) | 0x80));
+        val >>>= 7;
+      }
+    }
+    byteBuffer.put((byte)val);
   }
 
   public static long readLONG(@NotNull DataInput record) throws IOException {
