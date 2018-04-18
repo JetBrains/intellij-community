2edd95e086077814b180f072b91b8d8bbfe87b2e
Aleksey Pivovarov <AMPivovarov@gmail.com>
2018-04-18 19:06:52 +0300

    git: fix GitVcs creation for default project

--- plugins/git4idea/src/git4idea/diff/GitDiffProvider.java
+++ plugins/git4idea/src/git4idea/diff/GitDiffProvider.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package git4idea.diff;
 
 import com.intellij.openapi.project.Project;
@@ -52,7 +38,6 @@ public class GitDiffProvider implements DiffProvider, DiffMixin {
    * The context project
    */
   private final Project myProject;
-  private final ChangeListManager myChangeListManager;
 
   /**
    * A constructor
@@ -61,7 +46,6 @@ public class GitDiffProvider implements DiffProvider, DiffMixin {
    */
   public GitDiffProvider(@NotNull Project project) {
     myProject = project;
-    myChangeListManager = ChangeListManager.getInstance(myProject);
   }
 
   /**
@@ -114,7 +98,7 @@ public class GitDiffProvider implements DiffProvider, DiffMixin {
   }
 
   private boolean hasGoodFileStatus(VirtualFile file) {
-    FileStatus status = myChangeListManager.getStatus(file);
+    FileStatus status = ChangeListManager.getInstance(myProject).getStatus(file);
     return status == FileStatus.NOT_CHANGED ||
            status == FileStatus.DELETED ||
            status == FileStatus.MODIFIED ||
