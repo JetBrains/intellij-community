a69c8c47978a89002f9050084e8f0797621f2224
graann <Anna.Gromova@jetbrains.com>
2018-04-20 18:57:38 +0300

    tool window tabs close button position (#IDEA-190646)

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/content/TabbedContentTabLabel.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/content/TabbedContentTabLabel.java
@@ -8,6 +8,7 @@ import com.intellij.reference.SoftReference;
 import com.intellij.ui.content.TabbedContent;
 import com.intellij.util.ui.JBUI;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.awt.*;
 import java.lang.ref.Reference;
@@ -77,6 +78,12 @@ public class TabbedContentTabLabel extends ContentTabLabel {
       @Override
       public boolean getAvailable() {
         return hasMultipleTabs();
+      }
+
+      @Nullable
+      @Override
+      public Runnable getAction() {
+        return () -> selectContent();
       }
     });
     super.fillIcons(icons);
