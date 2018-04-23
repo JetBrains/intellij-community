e50af8cc24d6c784281db5681813bd3b5ef60d88
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2018-04-23 20:19:15 +0300

    RUBY-15844 Implemented resolve for anchors and aliases

--- plugins/yaml/src/org/jetbrains/yaml/psi/impl/YAMLAliasImpl.java
+++ plugins/yaml/src/org/jetbrains/yaml/psi/impl/YAMLAliasImpl.java
@@ -6,8 +6,10 @@ import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.impl.source.tree.LeafPsiElement;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.yaml.psi.YAMLAlias;
 import org.jetbrains.yaml.psi.YamlPsiElementVisitor;
+import org.jetbrains.yaml.resolve.YAMLAliasReference;
 
 /** Current implementation consists of 2 nodes: star symbol and name identifier */
 public class YAMLAliasImpl extends YAMLValueImpl implements YAMLAlias {
@@ -15,9 +17,28 @@ public class YAMLAliasImpl extends YAMLValueImpl implements YAMLAlias {
     super(node);
   }
 
+  @NotNull
+  @Override
+  public String getAliasName() {
+    LeafPsiElement identifier = getIdentifierPsi();
+    return identifier == null ? "" : identifier.getText();
+  }
+
+  @Override
+  public YAMLAliasReference getReference() {
+    return getIdentifierPsi() == null ? null : new YAMLAliasReference(this);
+  }
+
   @Override
   public String toString() {
     return "YAML alias";
+  }
+
+  /** For now it could not return null but better do not rely on it */
+  @Contract(pure = true)
+  @Nullable
+  public LeafPsiElement getIdentifierPsi() {
+    return (LeafPsiElement)getLastChild();
   }
 
   @Override
