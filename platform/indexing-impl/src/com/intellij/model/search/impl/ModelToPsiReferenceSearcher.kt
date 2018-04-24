6d902cea7bc01976cb642b14bb9f6f7b3ff9425b
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2018-04-24 14:44:26 +0300

    MRF: use CustomProcessorQuery in ModelToPsi searcher

--- platform/indexing-impl/src/com/intellij/model/search/impl/ModelToPsiReferenceSearcher.kt
+++ platform/indexing-impl/src/com/intellij/model/search/impl/ModelToPsiReferenceSearcher.kt
@@ -11,10 +11,9 @@ import com.intellij.psi.PsiReference
 import com.intellij.psi.search.QuerySearchRequest
 import com.intellij.psi.search.SearchRequestCollector
 import com.intellij.psi.search.searches.ReferencesSearch
-import com.intellij.util.AbstractQuery
+import com.intellij.util.CustomProcessorQuery
 import com.intellij.util.PairProcessor
 import com.intellij.util.Processor
-import com.intellij.util.Query
 
 /**
  * Includes [ModelReferenceSearch] results into [ReferencesSearch] results.
@@ -33,20 +32,15 @@ class ModelToPsiReferenceSearcher : QueryExecutorBase<PsiReference, ReferencesSe
     val modelElement = ModelService.adaptPsiElement(queryParameters.elementToSearch)
     val modelParameters = PsiToModelParameters(modelElement, queryParameters)
     val modelQuery = ModelReferenceSearch.search(modelParameters)
-    val psiQuery = adaptQuery(modelQuery)
-    val collector = queryParameters.optimizer
-    val nested = SearchRequestCollector(collector.searchSession)
-    val request = QuerySearchRequest(psiQuery, nested, false, PairProcessor { ref, _ -> consumer.process(ref) })
-    collector.searchQuery(request)
-  }
-
-  private fun adaptQuery(modelQuery: Query<ModelReference>): Query<PsiReference> {
-    return object : AbstractQuery<PsiReference>() {
-      override fun processResults(consumer: Processor<PsiReference>): Boolean = modelQuery.forEach(adaptProcessor(consumer))
+    val psiQuery = CustomProcessorQuery(modelQuery, this::adaptProcessor)
+    queryParameters.optimizer.apply {
+      val nested = SearchRequestCollector(searchSession)
+      val request = QuerySearchRequest(psiQuery, nested, false, PairProcessor { ref, _ -> consumer.process(ref) })
+      searchQuery(request)
     }
   }
 
-  private fun adaptProcessor(psiProcessor: Processor<PsiReference>): Processor<ModelReference> {
+  private fun adaptProcessor(psiProcessor: Processor<in PsiReference>): Processor<in ModelReference> {
     return Processor { modelReference ->
       if (modelReference is PsiReference) {
         psiProcessor.process(modelReference)
