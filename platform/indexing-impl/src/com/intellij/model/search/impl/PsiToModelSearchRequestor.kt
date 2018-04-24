d8f45c5133adec81f538f49e4ef46975b1457611
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2018-04-24 11:19:17 +0300

    MRF: move PSI-model-PSI searchers into intellij.platform.indexing.impl

--- platform/indexing-impl/src/com/intellij/model/search/impl/PsiToModelSearchRequestor.kt
+++ platform/indexing-impl/src/com/intellij/model/search/impl/PsiToModelSearchRequestor.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.psi.impl.search
+package com.intellij.model.search.impl
 
 import com.intellij.model.ModelService
 import com.intellij.model.search.ModelReferenceSearchParameters
