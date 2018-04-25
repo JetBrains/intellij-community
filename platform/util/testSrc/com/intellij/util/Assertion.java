e79939543bfdac6779aec21d3e0f6a7ffcb2983c
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-04-25 10:32:01 +0200

    remove unused methods

--- platform/util/testSrc/com/intellij/util/Assertion.java
+++ platform/util/testSrc/com/intellij/util/Assertion.java
@@ -10,7 +10,10 @@ import org.junit.Assert;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
 
 public class Assertion extends Assert {
   private StringConvertion myStringConvertion;
@@ -118,14 +121,6 @@ public class Assertion extends Assert {
     for (Object exp : expected) {
       assertTrue(String.format("Expected element %s was not found in the collection %s", exp, actual), actual.contains(exp));
     }
-  }
-
-  public static void compareUnordered(Collection expected, Collection actual) {
-    compareUnordered(expected.toArray(), actual);
-  }
-
-  public static void compareUnordered(Collection expected, Object[] actual) {
-    compareUnordered(expected, new ArrayList(Arrays.asList(actual)));
   }
 
   public void compareAll(List expected, List actual) {
@@ -264,14 +259,5 @@ public class Assertion extends Assert {
 
   public void containsAll(Object[] array, Object[] subArray) {
     containsAll(array, Arrays.asList(subArray));
-  }
-
-  private static Object[] asObjectArray(char[] chars) {
-    Object[] array = new Object[chars.length];
-    for (int i = 0; i < chars.length; i++) {
-      char c = chars[i];
-      array[i] = new Character(c);
-    }
-    return array;
   }
 }
