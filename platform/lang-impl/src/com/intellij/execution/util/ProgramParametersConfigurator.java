d6093331a00b1bd6aa056677e431ca18f1b0154a
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-04-25 17:17:19 +0200

    IDEA-190100 Error running individual unit test spec in Gradle project - module directory does not exist
    (cherry picked from commit b9081ab)

--- platform/lang-impl/src/com/intellij/execution/util/ProgramParametersConfigurator.java
+++ platform/lang-impl/src/com/intellij/execution/util/ProgramParametersConfigurator.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.util;
 
 import com.intellij.execution.CommonProgramRunConfigurationParameters;
@@ -24,6 +10,7 @@ import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.WorkingDirectoryProvider;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ExternalProjectSystemRegistry;
 import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.text.StringUtil;
@@ -43,6 +30,7 @@ public class ProgramParametersConfigurator {
   private static final ExtensionPointName<WorkingDirectoryProvider> WORKING_DIRECTORY_PROVIDER_EP_NAME= ExtensionPointName
     .create("com.intellij.module.workingDirectoryProvider");
   public static final String MODULE_WORKING_DIR = "%MODULE_WORKING_DIR%";
+  private static final String DEPRECATED_MODULE_DIR = "$" + PathMacroUtil.MODULE_DIR_MACRO_NAME + "$";
 
   public void configureConfiguration(SimpleProgramParameters parameters, CommonProgramRunConfigurationParameters configuration) {
     Project project = configuration.getProject();
@@ -74,7 +62,7 @@ public class ProgramParametersConfigurator {
     }
     workingDirectory = expandPath(workingDirectory, module, project);
     if (!FileUtil.isAbsolute(workingDirectory) && defaultWorkingDir != null) {
-      if (("$" + PathMacroUtil.MODULE_DIR_MACRO_NAME + "$").equals(workingDirectory)) {
+      if (DEPRECATED_MODULE_DIR.equals(workingDirectory)) {
         return defaultWorkingDir;
       }
 
@@ -119,6 +107,14 @@ public class ProgramParametersConfigurator {
   }
 
   protected String expandPath(@Nullable String path, Module module, Project project) {
+    // https://youtrack.jetbrains.com/issue/IDEA-190100
+    // if old macro is used (because stored in the default project and applied for a new imported project) and module file stored under .idea, use new module macro instead
+    if (module != null && DEPRECATED_MODULE_DIR.equals(path) &&
+        module.getModuleFilePath().contains("/" + Project.DIRECTORY_STORE_FOLDER + "/") &&
+        ExternalProjectSystemRegistry.getInstance().getExternalSource(module) != null /* not really required but to reduce possible impact */) {
+      return getDefaultWorkingDir(module);
+    }
+
     path = PathMacroManager.getInstance(project).expandPath(path);
     if (module != null) {
       path = PathMacroManager.getInstance(module).expandPath(path);
