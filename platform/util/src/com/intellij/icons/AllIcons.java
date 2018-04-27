38b5d2cc722199ecb8a5964c5a62719e4bbd3870
Aleksey Pivovarov <AMPivovarov@gmail.com>
2018-04-27 17:14:58 +0300

    IDEA-190693: PNGs to SVGs for "Select All"

--- platform/util/src/com/intellij/icons/AllIcons.java
+++ platform/util/src/com/intellij/icons/AllIcons.java
@@ -189,7 +189,7 @@ public class AllIcons {
     public static final Icon Search = IconLoader.getIcon("/actions/search.png"); // 16x16
     public static final Icon SearchNewLine = IconLoader.getIcon("/actions/searchNewLine.png"); // 16x16
     public static final Icon SearchNewLineHover = IconLoader.getIcon("/actions/searchNewLineHover.png"); // 16x16
-    public static final Icon Selectall = IconLoader.getIcon("/actions/selectall.png"); // 16x16
+    public static final Icon Selectall = IconLoader.getIcon("/actions/selectall.svg"); // 16x16
     public static final Icon Share = IconLoader.getIcon("/actions/share.png"); // 14x14
     public static final Icon ShortcutFilter = IconLoader.getIcon("/actions/shortcutFilter.png"); // 16x16
     public static final Icon Show = IconLoader.getIcon("/actions/show.svg"); // 16x16
