4996f6b201e4c59bfaec592f7015917112877e78
Dmitry Jemerov <yole@jetbrains.com>
2018-04-27 15:15:33 +0200

    Revert "Check if lookup is disposed in left/right handlers"
    This reverts commit fd32a7f
    
    Rider actually works correctly without this change
    
    IDEA-CR-31617

--- platform/lang-impl/src/com/intellij/codeInsight/lookup/impl/LookupActionHandler.java
+++ platform/lang-impl/src/com/intellij/codeInsight/lookup/impl/LookupActionHandler.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.codeInsight.lookup.impl;
 
@@ -194,7 +180,7 @@ public abstract class LookupActionHandler extends EditorActionHandler {
 
     @Override
     protected void executeInLookup(final LookupImpl lookup, DataContext context, Caret caret) {
-      if (!lookup.isCompletion() || lookup.isLookupDisposed()) {
+      if (!lookup.isCompletion()) {
         myOriginalHandler.execute(lookup.getEditor(), caret, context);
         return;
       }
@@ -214,10 +200,6 @@ public abstract class LookupActionHandler extends EditorActionHandler {
     @Override
     protected void executeInLookup(LookupImpl lookup, DataContext context, final Caret caret) {
       final Editor editor = lookup.getEditor();
-      if (lookup.isLookupDisposed()) {
-        myOriginalHandler.execute(lookup.getEditor(), caret, context);
-        return;
-      }
       final int offset = editor.getCaretModel().getOffset();
       final CharSequence seq = editor.getDocument().getCharsSequence();
       if (seq.length() <= offset || !lookup.isCompletion()) {
