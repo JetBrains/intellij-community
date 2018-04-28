8fdb64e38281a25d2525f1047672e09b8be02b6f
Gregory.Shrago <gregory.shrago@jetbrains.com>
2018-04-28 12:39:41 +0300

    make scratches stay in dumb mode
    (cherry picked from commit 25b80346baea24fa699a0c1f8a073d5f8da9c731)
    (cherry picked from commit a26921a229e6afe55c27448d55867feb33c6d86e)

--- platform/lang-impl/src/com/intellij/ide/scratch/ScratchProjectViewPane.java
+++ platform/lang-impl/src/com/intellij/ide/scratch/ScratchProjectViewPane.java
@@ -31,6 +31,7 @@ import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.PathManager;
 import com.intellij.openapi.fileTypes.LanguageFileType;
+import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.util.io.FileUtil;
@@ -178,7 +179,7 @@ public class ScratchProjectViewPane extends ProjectViewPane {
     return !isScratchesMergedIntoProjectTab();
   }
 
-  public static class MyStructureProvider implements TreeStructureProvider {
+  public static class MyStructureProvider implements TreeStructureProvider, DumbAware {
     @NotNull
     @Override
     public Collection<AbstractTreeNode> modify(@NotNull AbstractTreeNode parent,
