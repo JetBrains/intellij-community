afcfa4590f35a8b0da76dc8a4dba533727cc9936
Nicolay Mitropolsky <nicolay.mitropolsky@jetbrains.com>
2018-05-03 17:12:35 +0300

    [jvm-lang] making JvmMetaLanguage rely on `JvmDeclarationSearcher` definition
    not on `JvmLanguage` marker interface.

--- java/java-psi-api/src/com/intellij/lang/jvm/JvmMetaLanguage.java
+++ java/java-psi-api/src/com/intellij/lang/jvm/JvmMetaLanguage.java
@@ -2,20 +2,45 @@
 package com.intellij.lang.jvm;
 
 import com.intellij.lang.Language;
+import com.intellij.lang.LanguageExtensionPoint;
 import com.intellij.lang.MetaLanguage;
+import com.intellij.lang.jvm.source.JvmDeclarationSearcher;
+import com.intellij.openapi.extensions.ExtensionPoint;
+import com.intellij.openapi.extensions.Extensions;
+import com.intellij.openapi.extensions.ExtensionsArea;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @since 2018.2
  */
 public class JvmMetaLanguage extends MetaLanguage {
 
+  private final Set<String> supportedLanguages;
+
   protected JvmMetaLanguage() {
     super("JVM");
+    ExtensionsArea rootArea = Extensions.getRootArea();
+    if (rootArea.hasExtensionPoint(JvmDeclarationSearcher.EP.getName())) {
+      ExtensionPoint<LanguageExtensionPoint<JvmDeclarationSearcher>> jvmDeclarationSearcherEP =
+        rootArea.getExtensionPoint(JvmDeclarationSearcher.EP.getName());
+
+      LanguageExtensionPoint<JvmDeclarationSearcher>[] jvmDeclarationSearcherImplementations = jvmDeclarationSearcherEP.getExtensions();
+      supportedLanguages = Collections.unmodifiableSet(
+        Arrays.stream(jvmDeclarationSearcherImplementations).map(ep -> ep.getKey()).collect(Collectors.toSet())
+      );
+    }
+    else {
+      supportedLanguages = Collections.emptySet();
+    }
   }
 
   @Override
   public boolean matchesLanguage(@NotNull Language language) {
-    return language instanceof JvmLanguage;
+    return supportedLanguages.contains(language.getID());
   }
 }
