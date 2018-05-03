39c4106b2dd481c8d047fbdafc329d5c4efa88c7
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2018-05-03 18:35:27 +0200

    [project] migrates Windows file watcher build to CMake

--- native/fileWatcher/fileWatcher3.cpp
+++ native/fileWatcher/fileWatcher3.cpp
@@ -1,23 +1,9 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 #include <process.h>
 #include <stdio.h>
-#include <tchar.h>
 #include <windows.h>
+#include <winnetwk.h>
 
 struct WatchRootInfo {
     char driveLetter;
@@ -453,7 +439,7 @@ void FreeWatchRootsList() {
 
 // -- Main - filewatcher protocol ---------------------------------------------
 
-int _tmain(int argc, _TCHAR *argv[]) {
+int main(int argc, char *argv[]) {
     InitializeCriticalSection(&csOutput);
 
     for (int i = 0; i < 26; i++) {
