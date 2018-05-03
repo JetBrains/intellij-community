75b0d737a35da6396607d6a0fbec46495d69ff80
Gleb Leonov <Gleb.Leonov@jetbrains.com>
2018-05-03 16:17:20 +0300

    implement ui dispatcher for app-idea
    GitOrigin-RevId: aaa298b8203d7275de61bdc8aa6cdfec4354e661

---
+++ plugins/space/src/main/kotlin/circlet/runtime/ApplicationDispatcher.kt
@@ -0,0 +1,64 @@
+package circlet.runtime
+
+import com.intellij.openapi.application.*
+import kotlinx.coroutines.experimental.*
+import runtime.*
+import sun.management.snmp.jvminstr.JvmThreadInstanceEntryImpl.ThreadStateMap.Byte0.*
+import java.util.concurrent.*
+import kotlin.coroutines.experimental.*
+
+class ApplicationDispatcher(private val application: Application) : Dispatcher {
+    private val executor = Executors.newSingleThreadScheduledExecutor { runnable ->
+        val thread = Thread(runnable, "application auxiliary scheduler")
+        thread.isDaemon = true
+        thread
+    }
+    private val context = ApplicationCoroutineContext(application, executor)
+
+    override val coroutineContext: CoroutineContext
+        get() = context
+
+    override fun dispatch(runnable: () -> Unit) {
+        application.invokeLater(runnable)
+    }
+
+    override fun dispatch(delay: Int, r: () -> Unit): Cancellable {
+        val invoke = Runnable {
+            application.invokeLater(r)
+        }
+        val disposable = executor.schedule(invoke, delay.toLong(), TimeUnit.MILLISECONDS)
+        return TaskCancellable(disposable)
+    }
+
+    override fun dispatchInterval(delay: Int, interval: Int, r: () -> Unit): Cancellable {
+        val invoke = Runnable {
+            application.invokeLater(r)
+        }
+        val disposable = executor.scheduleAtFixedRate(invoke, delay.toLong(), interval.toLong(), TimeUnit.MILLISECONDS)
+        return TaskCancellable(disposable)
+    }
+}
+
+private class ApplicationCoroutineContext(private val application: Application, private val executor: ScheduledExecutorService) : CoroutineDispatcher(), Delay {
+    override fun dispatch(context: CoroutineContext, block: Runnable) {
+        application.invokeLater(block)
+    }
+
+    override fun scheduleResumeAfterDelay(time: Long, unit: TimeUnit, continuation: CancellableContinuation<Unit>) {
+        val toResume = Runnable {
+            application.invokeLater {
+                with(continuation) { this@ApplicationCoroutineContext.resumeUndispatched(Unit) }
+            }
+        }
+        executor.schedule(toResume, time, unit)
+    }
+
+    override fun invokeOnTimeout(time: Long, unit: TimeUnit, block: Runnable): DisposableHandle {
+        val toResume = Runnable {
+            application.invokeLater {
+                block.run()
+            }
+        }
+        return DisposableFutureHandle(executor.schedule(toResume, time, unit))
+    }
+}
---
+++ plugins/space/src/test/kotlin/circlet/runtime/TestApplicationDispatcher.kt
@@ -0,0 +1,105 @@
+package circlet.runtime
+
+import circlet.utils.*
+import com.intellij.mock.*
+import com.intellij.openapi.application.*
+import junit.framework.*
+import junit.framework.TestCase.*
+import kotlinx.coroutines.experimental.*
+import java.lang.Runnable
+import java.util.concurrent.*
+
+
+class TestApplicationDispatcher : TestCase() {
+    private val dispatcher: ApplicationDispatcher
+
+    init {
+        ApplicationManager.setApplication(TestApplication(), {})
+        dispatcher = ApplicationDispatcher(application)
+    }
+
+    fun testCoroutineDispatched() = runBlocking {
+        val job = async(dispatcher.coroutineContext) {
+            application.assertIsDispatchThread()
+        }
+        job.await()
+    }
+
+    fun testCoroutineDelay() = runBlocking {
+        val job = async(dispatcher.coroutineContext) {
+            delay(10)
+            application.assertIsDispatchThread()
+        }
+        job.await()
+    }
+
+    fun testDispatch() {
+        val runner = DispatchRunner(CountDownLatch(1))
+        dispatcher.dispatch {
+            runner.run {
+                application.assertIsDispatchThread()
+            }
+        }
+        runner.assertNoError()
+    }
+
+    fun testDispatchWithDelay() {
+        val runner = DispatchRunner(CountDownLatch(1))
+        dispatcher.dispatch(10) {
+            runner.run {
+                application.assertIsDispatchThread()
+            }
+        }
+        runner.assertNoError()
+    }
+
+    fun testDispatchInterval() {
+        val runner = DispatchRunner(CountDownLatch(2))
+        val cancellable = dispatcher.dispatchInterval(10, 10) {
+            runner.run {
+                application.assertIsDispatchThread()
+            }
+        }
+        runner.assertNoError()
+        cancellable.cancel()
+    }
+
+    private class DispatchRunner(val latch: CountDownLatch) {
+        var error: AssertionError? = null
+
+        fun run(runnable: () -> Unit) {
+            try {
+                runnable()
+            }
+            catch (ae: AssertionError) {
+                error = ae
+                throw ae
+            } finally {
+                latch.countDown()
+            }
+        }
+
+        fun assertNoError() {
+            latch.await()
+            if (error != null) {
+                throw error!!
+            }
+        }
+    }
+}
+
+private const val TEST_UI_DISPATCHER_NAME = "test ui dispatcher"
+
+private class TestApplication : MockApplication({}) {
+    val executor: ExecutorService = Executors.newSingleThreadExecutor { runnable ->
+        Thread(runnable, TEST_UI_DISPATCHER_NAME)
+    }
+
+    override fun assertIsDispatchThread() {
+        assertTrue(Thread.currentThread().name.startsWith(TEST_UI_DISPATCHER_NAME))
+    }
+
+    override fun invokeLater(runnable: Runnable) {
+        executor.execute(runnable)
+    }
+}
