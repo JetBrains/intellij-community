aa93337a662b06b9db7aa8086ce93c0a05092614
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2018-05-04 19:22:19 +0300

    Gradle: use javac, kotlinc output parsers for the sync to handle the compilation output of the 'buildSrc' sources

--- plugins/gradle/java/src/execution/build/output/GradleJavaOutputParserProvider.java
+++ plugins/gradle/java/src/execution/build/output/GradleJavaOutputParserProvider.java
@@ -6,12 +6,10 @@ import com.intellij.build.output.JavacOutputParser;
 import com.intellij.build.output.KotlincOutputParser;
 import com.intellij.openapi.externalSystem.model.ProjectSystemId;
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTask;
-import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskType;
 import com.intellij.openapi.externalSystem.service.execution.ExternalSystemOutputParserProvider;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.plugins.gradle.util.GradleConstants;
 
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -25,11 +23,7 @@ public class GradleJavaOutputParserProvider implements ExternalSystemOutputParse
 
   @Override
   public List<BuildOutputParser> getBuildOutputParsers(ExternalSystemTask task) {
-    if (task.getId().getType().equals(ExternalSystemTaskType.EXECUTE_TASK)) {
-      return ContainerUtil.list(new JavacOutputParser(), new KotlincOutputParser());
-    }
-    else {
-      return Collections.emptyList();
-    }
+    // use javac, kotlinc output parsers for the sync to handle the compilation output of the 'buildSrc' sources
+    return ContainerUtil.list(new JavacOutputParser(), new KotlincOutputParser());
   }
 }
