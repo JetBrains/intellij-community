6a893f75cc0700ad12fdeec125f2718e55262d19
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2018-05-04 18:30:13 +0300

    [git] move testCyclicRenames to GitHistoryTest

--- plugins/git4idea/tests/git4idea/history/GitHistoryTest.kt
+++ plugins/git4idea/tests/git4idea/history/GitHistoryTest.kt
@@ -15,11 +15,18 @@
  */
 package git4idea.history
 
+import com.intellij.dvcs.DvcsUtil
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.vcs.Executor
 import com.intellij.openapi.vcs.Executor.touch
+import com.intellij.openapi.vcs.history.VcsFileRevision
 import com.intellij.vcsUtil.VcsUtil
-import git4idea.test.GitSingleRepoTest
-import git4idea.test.add
-import git4idea.test.commit
+import git4idea.GitFileRevision
+import git4idea.test.*
+import junit.framework.TestCase
+import java.io.File
+import java.io.IOException
+import java.util.*
 
 class GitHistoryTest : GitSingleRepoTest() {
 
@@ -31,5 +38,88 @@ class GitHistoryTest : GitSingleRepoTest() {
 
     val history = GitFileHistory.collectHistory(project, VcsUtil.getFilePath(projectRoot, "a.txt"), "-1")
     assertEquals("Commit message is incorrect", MESSAGE, history[0].commitMessage)
+  }
+
+  // Inspired by IDEA-89347
+  @Throws(Exception::class)
+  fun `test cyclic rename`() {
+    val commits = ArrayList<TestCommit>()
+
+    commits.add(add("PostHighlightingPass.java", Executor.mkdir("source")))
+    commits.add(modify(commits.last().file))
+
+    commits.add(move(commits.last().file, Executor.mkdir("codeInside-impl")))
+    commits.add(modify(commits.last().file))
+
+    commits.add(move(commits.last().file, Executor.mkdir("codeInside")))
+    commits.add(modify(commits.last().file))
+
+    commits.add(move(commits.last().file, Executor.mkdir("lang-impl")))
+    commits.add(modify(commits.last().file))
+
+    commits.add(move(commits.last().file, Executor.child("source")))
+    commits.add(modify(commits.last().file))
+
+    commits.add(move(commits.last().file, Executor.mkdir("java")))
+    commits.add(modify(commits.last().file))
+
+    commits.reverse()
+
+    val vFile = VcsUtil.getVirtualFileWithRefresh(commits.first().file)
+    TestCase.assertNotNull(vFile)
+    val history = GitFileHistory.collectHistory(myProject, VcsUtil.getFilePath(vFile!!))
+    TestCase.assertEquals("History size doesn't match. Actual history: \n" + toReadable(history), commits.size, history.size)
+    TestCase.assertEquals("History is different.", toReadable(commits), toReadable(history))
+  }
+
+  private class TestCommit(val hash: String, val commitMessage: String, val file: File)
+
+  private fun move(file: File, dir: File): TestCommit {
+    repo.mv(file.path, dir.path)
+
+    val message = "Moved ${file.path} to ${dir.name}"
+    repo.addCommit(message)
+
+    return TestCommit(last(), message, File(dir, file.name))
+  }
+
+  @Throws(IOException::class)
+  private fun modify(file: File): TestCommit {
+    FileUtil.appendToFile(file, "Modified")
+
+    val message = "Modified ${file.path}"
+    repo.addCommit(message)
+
+    return TestCommit(last(), message, file)
+  }
+
+  private fun add(fileName: String, dir: File): TestCommit {
+    val file = touch(File(dir, fileName).path, "Initial content")
+
+    val message = "Created $fileName in ${dir.name}"
+    repo.addCommit(message)
+
+    return TestCommit(last(), message, file)
+  }
+
+  private fun toReadable(history: Collection<VcsFileRevision>): String {
+    val maxSubjectLength = history.map { it.commitMessage?.length ?: 0 }.max() ?: 0
+    val sb = StringBuilder()
+    for (revision in history) {
+      val rev = revision as GitFileRevision
+      val relPath = FileUtil.getRelativePath(File(projectPath), rev.path.ioFile)
+      sb.append(String.format("%s  %-" + maxSubjectLength + "s  %s%n", DvcsUtil.getShortHash(rev.hash), rev.commitMessage, relPath))
+    }
+    return sb.toString()
+  }
+
+  private fun toReadable(history: List<TestCommit>): String {
+    val maxSubjectLength = history.map { it.commitMessage.length }.max() ?: 0
+    val sb = StringBuilder()
+    for (commit in history) {
+      val relPath = FileUtil.getRelativePath(File(projectPath), commit.file)
+      sb.append(String.format("%s  %-" + maxSubjectLength + "s  %s%n", DvcsUtil.getShortHash(commit.hash), commit.commitMessage, relPath))
+    }
+    return sb.toString()
   }
 }
--- plugins/git4idea/tests/git4idea/history/GitHistoryUtilsTest.kt
+++ plugins/git4idea/tests/git4idea/history/GitHistoryUtilsTest.kt
@@ -15,23 +15,18 @@
  */
 package git4idea.history
 
-import com.intellij.dvcs.DvcsUtil.getShortHash
-import com.intellij.openapi.util.io.FileUtil
-import com.intellij.openapi.vcs.Executor
 import com.intellij.openapi.vcs.Executor.*
 import com.intellij.openapi.vcs.VcsException
 import com.intellij.openapi.vcs.history.VcsFileRevision
 import com.intellij.util.CollectConsumer
 import com.intellij.util.Consumer
 import com.intellij.util.ExceptionUtil
-import com.intellij.vcsUtil.VcsUtil
 import com.intellij.vcsUtil.VcsUtil.getFilePath
 import git4idea.GitFileRevision
 import git4idea.GitRevisionNumber
 import git4idea.test.*
 import junit.framework.TestCase
 import java.io.File
-import java.io.IOException
 import java.util.*
 
 /**
@@ -117,89 +112,6 @@ class GitHistoryUtilsTest : GitSingleRepoTest() {
 
   override fun makeInitialCommit(): Boolean {
     return false
-  }
-
-  // Inspired by IDEA-89347
-  @Throws(Exception::class)
-  fun testCyclicRename() {
-    val commits = ArrayList<TestCommit>()
-
-    commits.add(add("PostHighlightingPass.java", Executor.mkdir("source")))
-    commits.add(modify(commits.last().file))
-
-    commits.add(move(commits.last().file, Executor.mkdir("codeInside-impl")))
-    commits.add(modify(commits.last().file))
-
-    commits.add(move(commits.last().file, Executor.mkdir("codeInside")))
-    commits.add(modify(commits.last().file))
-
-    commits.add(move(commits.last().file, Executor.mkdir("lang-impl")))
-    commits.add(modify(commits.last().file))
-
-    commits.add(move(commits.last().file, child("source")))
-    commits.add(modify(commits.last().file))
-
-    commits.add(move(commits.last().file, Executor.mkdir("java")))
-    commits.add(modify(commits.last().file))
-
-    commits.reverse()
-
-    val vFile = VcsUtil.getVirtualFileWithRefresh(commits.first().file)
-    TestCase.assertNotNull(vFile)
-    val history = GitFileHistory.collectHistory(myProject, VcsUtil.getFilePath(vFile!!))
-    TestCase.assertEquals("History size doesn't match. Actual history: \n" + toReadable(history), commits.size, history.size)
-    TestCase.assertEquals("History is different.", toReadable(commits), toReadable(history))
-  }
-
-  private class TestCommit(val hash: String, val commitMessage: String, val file: File)
-
-  private fun move(file: File, dir: File): TestCommit {
-    repo.mv(file.path, dir.path)
-
-    val message = "Moved ${file.path} to ${dir.name}"
-    repo.addCommit(message)
-
-    return TestCommit(last(), message, File(dir, file.name))
-  }
-
-  @Throws(IOException::class)
-  private fun modify(file: File): TestCommit {
-    FileUtil.appendToFile(file, "Modified")
-
-    val message = "Modified ${file.path}"
-    repo.addCommit(message)
-
-    return TestCommit(last(), message, file)
-  }
-
-  private fun add(fileName: String, dir: File): TestCommit {
-    val file = touch(File(dir, fileName).path, "Initial content")
-
-    val message = "Created $fileName in ${dir.name}"
-    repo.addCommit(message)
-
-    return TestCommit(last(), message, file)
-  }
-
-  private fun toReadable(history: Collection<VcsFileRevision>): String {
-    val maxSubjectLength = history.map { it.commitMessage?.length ?: 0 }.max() ?: 0
-    val sb = StringBuilder()
-    for (revision in history) {
-      val rev = revision as GitFileRevision
-      val relPath = FileUtil.getRelativePath(File(projectPath), rev.path.ioFile)
-      sb.append(String.format("%s  %-" + maxSubjectLength + "s  %s%n", getShortHash(rev.hash), rev.commitMessage, relPath))
-    }
-    return sb.toString()
-  }
-
-  private fun toReadable(history: List<TestCommit>): String {
-    val maxSubjectLength = history.map { it.commitMessage.length }.max() ?: 0
-    val sb = StringBuilder()
-    for (commit in history) {
-      val relPath = FileUtil.getRelativePath(File(projectPath), commit.file)
-      sb.append(String.format("%s  %-" + maxSubjectLength + "s  %s%n", getShortHash(commit.hash), commit.commitMessage, relPath))
-    }
-    return sb.toString()
   }
 
   @Throws(Exception::class)
