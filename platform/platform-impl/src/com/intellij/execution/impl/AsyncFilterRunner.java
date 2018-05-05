39db3ff1e5ec4c3ed5f24ed822837d62b774fd18
Anton Makeev <Anton.Makeev@jetbrains.com>
2018-05-05 21:20:02 +0200

    [reverted] AsyncFilterRunner: add NonBlockingReadAction API for easier running of bg read actions
    reverted due to deadlock and regression

--- platform/platform-impl/src/com/intellij/execution/impl/AsyncFilterRunner.java
+++ platform/platform-impl/src/com/intellij/execution/impl/AsyncFilterRunner.java
@@ -1,43 +1,25 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.impl;
 
 import com.intellij.execution.filters.Filter;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
-import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.RangeMarker;
 import com.intellij.openapi.editor.impl.DocumentImpl;
 import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.util.ProgressIndicatorUtils;
 import com.intellij.util.TimeoutUtil;
 import com.intellij.util.concurrency.SequentialTaskExecutor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.concurrency.Promise;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -63,22 +45,25 @@ class AsyncFilterRunner {
     if (ApplicationManager.getApplication().isWriteAccessAllowed()) {
       runTasks();
       highlightAvailableResults();
-      return;
-    }
-
-    Promise<?> promise = ReadAction
-      .nonBlocking(this::runTasks)
-      .finishOnUiThread(ModalityState.any(), __ -> highlightAvailableResults())
-      .submit(ourExecutor);
-
-    if (isQuick(promise)) {
+    } else if (isQuick(ourExecutor.submit(this::runFiltersInBackground))) {
       highlightAvailableResults();
     }
   }
 
-  private static boolean isQuick(Promise<?> future) {
+  private void runFiltersInBackground() {
+    while (true) {
+      boolean finished = ProgressIndicatorUtils.runInReadActionWithWriteActionPriority(this::runTasks);
+      if (hasResults()) {
+        ApplicationManager.getApplication().invokeLater(this::highlightAvailableResults, ModalityState.any());
+      }
+      if (finished) return;
+      ProgressIndicatorUtils.yieldToPendingWriteActions();
+    }
+  }
+
+  private static boolean isQuick(Future<?> future) {
     try {
-      future.blockingGet(5, TimeUnit.MILLISECONDS);
+      future.get(5, TimeUnit.MILLISECONDS);
       return true;
     }
     catch (TimeoutException ignored) {
