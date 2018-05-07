5acf36e1670ba7facf6ee57e56f96c27b3f31805
Max Medvedev <maxim.medvedev@jetbrains.com>
2018-05-07 22:51:41 +0200

    [MultiCharQuoteHandler] add @NotNull annotation

--- python/src/com/jetbrains/python/editor/BaseQuoteHandler.java
+++ python/src/com/jetbrains/python/editor/BaseQuoteHandler.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.python.editor;
 
 import com.intellij.codeInsight.editorActions.MultiCharQuoteHandler;
@@ -23,6 +9,7 @@ import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.tree.TokenSet;
 import com.jetbrains.python.psi.PyStringLiteralUtil;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
@@ -134,7 +121,7 @@ public class BaseQuoteHandler extends SimpleTokenSetQuoteHandler implements Mult
 
   @Nullable
   @Override
-  public CharSequence getClosingQuote(HighlighterIterator iterator, int offset) {
+  public CharSequence getClosingQuote(@NotNull HighlighterIterator iterator, int offset) {
     Document document = iterator.getDocument();
     String text = document.getText();
     char theQuote = text.charAt(offset - 1);
