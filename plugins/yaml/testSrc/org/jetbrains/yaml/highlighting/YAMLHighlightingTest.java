35943e7670ed94450178d1baaa9688e7c5c0fa0e
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2018-05-10 15:17:26 +0300

    RUBY-21454 Implemented recursive alias inspection

--- plugins/yaml/testSrc/org/jetbrains/yaml/highlighting/YAMLHighlightingTest.java
+++ plugins/yaml/testSrc/org/jetbrains/yaml/highlighting/YAMLHighlightingTest.java
@@ -3,6 +3,7 @@ package org.jetbrains.yaml.highlighting;
 
 import com.intellij.openapi.application.ex.PathManagerEx;
 import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
+import org.jetbrains.yaml.inspections.YAMLRecursiveAliasInspection;
 import org.jetbrains.yaml.inspections.YAMLUnresolvedAliasInspection;
 
 public class YAMLHighlightingTest extends LightPlatformCodeInsightFixtureTestCase {
@@ -25,6 +26,11 @@ public class YAMLHighlightingTest extends LightPlatformCodeInsightFixtureTestCas
 
   public void testUnresolvedAlias() {
     myFixture.enableInspections(YAMLUnresolvedAliasInspection.class);
+    doTest();
+  }
+
+  public void testRecursiveAlias() {
+    myFixture.enableInspections(YAMLRecursiveAliasInspection.class);
     doTest();
   }
 
