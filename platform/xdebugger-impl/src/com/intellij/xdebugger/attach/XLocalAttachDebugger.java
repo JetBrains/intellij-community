39aa59adaa987aebb56df563968bdece192c8335
Viuginov Nickolay <nickolay.viuginov@jetbrains.com>
2018-05-11 19:28:38 +0300

    XLocalAttachDebugger is not deprecated anymore
    IDEA-CR-25164

--- platform/xdebugger-impl/src/com/intellij/xdebugger/attach/XLocalAttachDebugger.java
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/attach/XLocalAttachDebugger.java
@@ -6,21 +6,14 @@ import com.intellij.execution.process.ProcessInfo;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
-/**
- * @deprecated use {@link XAttachDebugger} instead
- */
 public interface XLocalAttachDebugger extends XAttachDebugger {
 
-  /**
-   * @deprecated use {@link XAttachDebugger#attachDebugSession(Project, XAttachHost, ProcessInfo)} instead
-   */
   void attachDebugSession(@NotNull Project project,
                           @NotNull ProcessInfo info) throws ExecutionException;
 
   default void attachDebugSession(@NotNull Project project,
                           @NotNull XAttachHost hostInfo,
                           @NotNull ProcessInfo info) throws ExecutionException {
-    if(hostInfo instanceof LocalAttachHost)
-      attachDebugSession(project, info);
+    attachDebugSession(project, info);
   }
 }
