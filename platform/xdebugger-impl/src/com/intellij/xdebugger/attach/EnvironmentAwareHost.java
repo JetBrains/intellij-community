d990b7fbda5743244708f7c9afcef23c2dc68feb
Viuginov Nickolay <nickolay.viuginov@jetbrains.com>
2018-05-11 19:00:34 +0300

    Attach OsHandlers added
    IDEA-CR-25164

--- platform/xdebugger-impl/src/com/intellij/xdebugger/attach/EnvironmentAwareHost.java
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/attach/EnvironmentAwareHost.java
@@ -3,115 +3,75 @@ package com.intellij.xdebugger.attach;
 
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.configurations.GeneralCommandLine;
-import com.intellij.execution.process.ProcessInfo;
+import com.intellij.execution.process.BaseProcessHandler;
+import com.intellij.execution.process.CapturingProcessRunner;
 import com.intellij.execution.process.ProcessOutput;
-import com.intellij.openapi.project.Project;
-import com.intellij.remote.RemoteSdkException;
+import com.intellij.xdebugger.attach.osHandlers.AttachOSHandler;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
 
+/**
+ * This abstract class represent {@link XAttachHost} with extended functional, such as executing {@link GeneralCommandLine},
+ * downloading files and getting OS of a host
+ */
 @ApiStatus.Experimental
 public abstract class EnvironmentAwareHost implements XAttachHost {
 
-  private OSType myOSType;
+  private AttachOSHandler myOsHandler = null;
 
   /**
-   * @param command commandLine to execute on this host
+   * @param commandLine commandLine to execute on this host
+   * @return {@link BaseProcessHandler}, with which the command is executed (for example with a timeout)
+   */
+  @NotNull
+  public abstract BaseProcessHandler getProcessHandler(@NotNull GeneralCommandLine commandLine) throws ExecutionException;
+
+  /**
+   * @param commandLine commandLine to execute on this host
    * @return output of the corresponding process
    */
   @NotNull
-  public abstract ProcessOutput execAndGetOutput(@Nullable Project project,
-                                 @NotNull GeneralCommandLine command) throws ExecutionException;
+  public ProcessOutput getProcessOutput(@NotNull GeneralCommandLine commandLine) throws ExecutionException {
+    BaseProcessHandler handler = getProcessHandler(commandLine);
+    CapturingProcessRunner runner = new CapturingProcessRunner(handler);
+    return runner.runProcess();
+  }
 
+  @NotNull
+  public AttachOSHandler getOsHandler() {
+    if(myOsHandler == null) {
+      myOsHandler = AttachOSHandler.getAttachOsHandler(this);
+    }
+
+    return myOsHandler;
+  }
 
   /**
-   * Retrieves remote file contents stream. May be used to sync parts of the debugged project.
-   * @param remotePath host-dependent path
+   * Retrieves file contents stream. May be used to sync parts of the debugged project.
+   *
+   * @param filePath path of the file on host machine
    * @return stream with file contents or <code>null</code> if the specified file does not exist
-   * @throws ExecutionException on stream retrieval error
+   * @throws IOException on stream retrieval error
    */
   @Nullable
-  public abstract InputStream getRemoteFile(@NotNull String remotePath) throws ExecutionException;
+  public abstract InputStream getFileContent(@NotNull String filePath) throws IOException;
 
   /**
-   * @return uid of user or -1 if error occurred
+   * Check if it is possible to read the file on host machine
+   *
+   * @param filePath path of the file on host machine
+   * @throws ExecutionException on stream retrieval error
    */
-  public int getUid(@NotNull Project project) throws ExecutionException {
-    OSType osType = getOsType(project);
+  public abstract boolean canReadFile(@NotNull String filePath) throws ExecutionException;
 
-    if (osType == OSType.LINUX || osType == OSType.MACOSX) {
-      try {
-        return getUidUnix(project);
-      }
-      catch (RemoteSdkException ex) {
-        return -1;
-      }
-    }
-    else {
-      return -1;
-    }
-  }
-
-  private int getUidUnix(Project project) throws ExecutionException {
-
-    GeneralCommandLine commandLine = new GeneralCommandLine(Arrays.asList("id", "-u"));
-    ProcessOutput uidOutput = execAndGetOutput(project, commandLine);
-
-    String uid = uidOutput.getStdout().trim();
-
-    try {
-      return Integer.valueOf(uid);
-    }
-    catch (NumberFormatException e) {
-      throw new ExecutionException("Error while parsing uid");
-    }
-  }
-
-  public OSType getOsType(Project project) throws ExecutionException {
-    if (myOSType != null) {
-      return myOSType;
-    }
-
-    try {
-      GeneralCommandLine getOsCommandLine = new GeneralCommandLine("uname", "-s");
-      ProcessOutput getOsOutput = execAndGetOutput(project, getOsCommandLine);
-
-      String osString = getOsOutput.getStdout().trim();
-
-      OSType osType;
-
-      switch (osString) {
-        case "Linux":
-          osType = OSType.LINUX;
-          break;
-        case "Darwin":
-          osType = OSType.MACOSX;
-          break;
-        case "WindowsNT":
-          osType = OSType.WINDOWS;
-          break;
-        default:
-          osType = OSType.UNKNOWN;
-          break;
-      }
-
-      return myOSType = osType;
-    }
-    catch (ExecutionException ex) {
-      throw new ExecutionException("Error while calculating the remote operating system");
-    }
-  }
-
-  abstract public boolean isSudoNeeded(@NotNull ProcessInfo info);
-
-  public enum OSType {
-    LINUX,
-    MACOSX,
-    WINDOWS,
-    UNKNOWN
-  }
+  /**
+   * File system prefix for files from this host. It should be noted that the prefixes must be different for different hosts.
+   * Path to the host file is obtained by concatenation of hostId and it's on-host path
+   */
+  @NotNull
+  public abstract String getFileSystemHostId();
 }
