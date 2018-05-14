25767607e879144b7ab5c64a0ef49294a1684669
Dmitry.Krasilschikov <kradima@jetbrains.com>
2018-05-14 14:48:00 +0300

    UX-157 IJP-56 simplify classes hierarchy

--- platform/lang-impl/src/com/intellij/ide/actions/runAnything/activity/RunAnythingActionHelpProvider.java
+++
@@ -1,8 +0,0 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.ide.actions.runAnything.activity;
-
-import com.intellij.openapi.actionSystem.AnAction;
-
-public abstract class RunAnythingActionHelpProvider<V extends AnAction> extends RunAnythingActionExecutionProvider<V>
-  implements RunAnythingHelpProviderBase<V>, RunAnythingActivityProvider<V> {
-}
--- platform/lang-impl/src/com/intellij/ide/actions/runAnything/activity/RunAnythingHelpProviderBase.java
+++ platform/lang-impl/src/com/intellij/ide/actions/runAnything/activity/RunAnythingHelpProviderBase.java
@@ -3,7 +3,6 @@ package com.intellij.ide.actions.runAnything.activity;
 
 import com.intellij.ide.actions.runAnything.items.RunAnythingHelpItem;
 import com.intellij.openapi.actionSystem.DataContext;
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.util.ui.EmptyIcon;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -11,8 +10,6 @@ import org.jetbrains.annotations.Nullable;
 import javax.swing.*;
 
 public interface RunAnythingHelpProviderBase<V> extends RunAnythingActivityProvider<V> {
-  Logger LOG = Logger.getInstance(RunAnythingHelpProviderBase.class);
-
   @Nullable
   @Override
   default RunAnythingHelpItem getHelpItem(@NotNull DataContext dataContext) {
