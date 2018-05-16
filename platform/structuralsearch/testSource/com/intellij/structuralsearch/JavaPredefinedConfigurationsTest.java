7754aa912e58d5db1830bdc4bd9dd122f6cf51d7
Bas Leijdekkers <basleijdekkers@gmail.com>
2018-05-16 13:43:48 +0200

    SSR: add some existing templates demonstrating features

--- platform/structuralsearch/testSource/com/intellij/structuralsearch/JavaPredefinedConfigurationsTest.java
+++ platform/structuralsearch/testSource/com/intellij/structuralsearch/JavaPredefinedConfigurationsTest.java
@@ -115,6 +115,42 @@ public class JavaPredefinedConfigurationsTest extends StructuralSearchTestCase {
                               "    case 3:" +
                               "    default:" +
                               "  }")));
+    testCases.put(SSRBundle.message("predefined.configuration.string.concatenations"),
+                  pair("class X {{" +
+                       "  String s = \"1\" + \"2\" + \"3\" + \"4\" + \"5\" + \"6\" + \"7\" + \"8\" + \"9\" + \"10\" + \"11\";" +
+                       "  String t = \"1\" + \"2\" + \"3\" + \"4\" + \"5\" + \"6\" + \"7\" + \"8\" + \"9\"+ \"10\";",
+                       asList("\"1\" + \"2\" + \"3\" + \"4\" + \"5\" + \"6\" + \"7\" + \"8\" + \"9\" + \"10\" + \"11\"")));
+    testCases.put(SSRBundle.message("predefined.configuration.assert.without.description"),
+                  pair("class X {{" +
+                       "  assert true;" +
+                       "  assert false : false;" +
+                       "  assert false : \"reason\";" +
+                       "}}",
+                       asList("assert true;")));
+    testCases.put(SSRBundle.message("predefined.configuration.labeled.break"),
+                  pair("class X {{" +
+                       "  break one;" +
+                       "  break;" +
+                       "  continue;" +
+                       "  continue here;" +
+                       "}}",
+                       asList("break one;")));
+    testCases.put(SSRBundle.message("predefined.configuration.method.returns.bounded.wildcard"),
+                  pair("abstract class X {" +
+                       "  List<? extends Number> one() {" +
+                       "    return null;" +
+                       "  }" +
+                       "  abstract List<? extends Number> ignore();" +
+                       "  List<?> two() {" +
+                       "    return null;" +
+                       "  }" +
+                       "  <T extends Number> T three() {" +
+                       "    return null;" +
+                       "  }" +
+                       "}",
+                       asList("List<? extends Number> one() {" +
+                              "    return null;" +
+                              "  }")));
   }
 
   public void testPredefinedConfigurations() {
@@ -125,7 +161,7 @@ public class JavaPredefinedConfigurationsTest extends StructuralSearchTestCase {
       final SearchConfiguration searchConfiguration = (SearchConfiguration)template;
       options = searchConfiguration.getMatchOptions();
       final List<MatchResult> matches = testMatcher.testFindMatches(testCase.first, options, true, StdFileTypes.JAVA, null, false);
-      assertEquals(testCase.second.size(), matches.size());
+      assertEquals(template.getName(), testCase.second.size(), matches.size());
       for (int i = 0; i < matches.size(); i++) {
         final String matchText = StructuralSearchUtil.getPresentableElement(matches.get(i).getMatch()).getText();
         assertEquals(template.getName(), testCase.second.get(i), matchText);
