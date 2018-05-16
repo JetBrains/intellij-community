88b3a4a14a48e34657d0fd76e43f8d8ecf61e708
Dmitry Jemerov <yole@jetbrains.com>
2018-05-16 19:39:57 +0200

    Power status on Mac

--- platform/platform-impl/src/com/intellij/util/system/PowerStatus.kt
+++ platform/platform-impl/src/com/intellij/util/system/PowerStatus.kt
@@ -1,9 +1,13 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.system
 
+import com.intellij.execution.ExecutionException
+import com.intellij.execution.configurations.GeneralCommandLine
+import com.intellij.execution.util.ExecUtil
 import com.intellij.jna.JnaLoader
 import com.intellij.openapi.util.SystemInfo
 import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.util.registry.Registry
 import com.sun.jna.Native
 import com.sun.jna.Structure
 import com.sun.jna.win32.StdCallLibrary
@@ -17,6 +21,7 @@ fun getPowerStatus(): PowerStatus {
   return when {
     SystemInfo.isWindows -> getWindowsPowerStatus()
     SystemInfo.isLinux -> getLinuxPowerStatus()
+    SystemInfo.isMac -> getMacPowerStatus()
     else -> PowerStatus.UNKNOWN
   }
 }
@@ -62,6 +67,40 @@ private val batteryFilePath by lazy {
   FileUtil.findFirstThatExist("/sys/class/power_supply/BAT0/status")
 }
 
+fun getMacPowerStatus(): PowerStatus {
+  val currentTime = System.currentTimeMillis()
+  lastMacPowerStatus?.let {
+    if (currentTime - lastMacPowerStatusUpdateTimestamp > Registry.intValue("ide.mac.power.status.refresh")) {
+      return it
+    }
+  }
+  val status = updateMacPowerStatus()
+  lastMacPowerStatusUpdateTimestamp = currentTime
+  lastMacPowerStatus = status
+  return status
+}
+
+private var lastMacPowerStatus: PowerStatus? = null
+private var lastMacPowerStatusUpdateTimestamp: Long = 0
+
+private fun updateMacPowerStatus(): PowerStatus {
+  try {
+    val output = ExecUtil.execAndGetOutput(GeneralCommandLine("/usr/bin/pmset", "-g", "batt"))
+    if (output.exitCode != 0 || output.stderr.isNotEmpty()) return PowerStatus.UNKNOWN
+    return when {
+      "'AC Power'" in output.stdout -> PowerStatus.AC_POWER
+      "'Battery Power'" in output.stdout -> PowerStatus.BATTERY
+      else -> PowerStatus.UNKNOWN
+    }
+  }
+  catch (e: ExecutionException) {
+    return PowerStatus.UNKNOWN
+  }
+}
+
 fun main(args: Array<String>) {
-  println("The current power status is ${getPowerStatus()}")
+  val startTime = System.nanoTime()
+  val powerStatus = getPowerStatus()
+  val elapsed = (System.nanoTime() - startTime) / 1000
+  println("The current power status is $powerStatus, retrieved in $elapsed useс")
 }
