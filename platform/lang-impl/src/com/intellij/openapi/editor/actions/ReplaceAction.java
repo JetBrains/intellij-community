e795cde44b273b7a47629e405e0e399aaf6ed853
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2018-05-17 23:10:23 +0300

    Remove unused inner class

--- platform/lang-impl/src/com/intellij/openapi/editor/actions/ReplaceAction.java
+++ platform/lang-impl/src/com/intellij/openapi/editor/actions/ReplaceAction.java
@@ -16,34 +16,10 @@
 
 package com.intellij.openapi.editor.actions;
 
-import com.intellij.ide.DataManager;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.actionSystem.DataContext;
-import com.intellij.openapi.actionSystem.PlatformDataKeys;
-import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.actionSystem.EditorAction;
-import com.intellij.openapi.editor.actionSystem.EditorActionHandler;
-import com.intellij.openapi.project.Project;
-import com.intellij.find.FindUtil;
-import org.jetbrains.annotations.NotNull;
 
 public class ReplaceAction extends EditorAction {
-  private static class Handler extends EditorActionHandler {
-    @Override
-    public void execute(@NotNull Editor editor, DataContext dataContext) {
-      Project project = CommonDataKeys.PROJECT.getData(DataManager.getInstance().getDataContext(editor.getComponent()));
-      FindUtil.replace(project, editor);
-    }
-
-    @Override
-    public boolean isEnabled(Editor editor, DataContext dataContext) {
-      Project project = CommonDataKeys.PROJECT.getData(DataManager.getInstance().getDataContext(editor.getComponent()));
-      return project != null;
-    }
-  }
-
   public ReplaceAction() {
     super(new IncrementalFindAction.Handler(true));
   }
-
 }
