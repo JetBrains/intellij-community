a0aad2293419e379130cdc3864be2fe171d8ad26
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2018-05-18 17:35:38 +0300

    log level lowered

--- platform/indexing-api/src/com/intellij/util/xml/NanoXmlUtil.java
+++ platform/indexing-api/src/com/intellij/util/xml/NanoXmlUtil.java
@@ -109,7 +109,7 @@ public class NanoXmlUtil {
       catch (ParserStoppedXmlException ignore) {
       }
       catch (XMLException e) {
-        LOG.warn(e);
+        LOG.info(e);
       }
     }
     catch (ClassNotFoundException | IllegalAccessException | InstantiationException e) {
