2b2d98aa8fc0527fbdd23e27d973ed2a21985f4b
Konstantin Bulenkov <kb@jetbrains.com>
2018-05-21 18:27:32 +0200

    a wrapper class for UITheme

---
+++ platform/platform-impl/src/com/intellij/ide/ui/laf/UIThemeBasedLookAndFeelInfo.java
@@ -0,0 +1,17 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.ide.ui.laf;
+
+import com.intellij.ide.ui.UITheme;
+import com.intellij.ide.ui.laf.darcula.DarculaLaf;
+
+import javax.swing.*;
+
+/**
+ * @author Konstantin Bulenkov
+ */
+public class UIThemeBasedLookAndFeelInfo extends UIManager.LookAndFeelInfo {
+
+  public UIThemeBasedLookAndFeelInfo(UITheme theme) {
+    super(theme.getName(), theme.isDark() ? DarculaLaf.class.getName() : IntelliJLaf.class.getName());
+  }
+}
