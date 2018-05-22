0a3fbe1241eb3db65d0bcbf71519f5c30f6f55bd
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2018-05-22 15:57:26 +0300

    [platform] fixes shell environment loading under Python 3 (IDEA-192345)

--- bin/mac/printenv.py
+++ bin/mac/printenv.py
@@ -12,6 +12,8 @@ if len(sys.argv) != 2:
 f = open(sys.argv[1], 'w')
 try:
     for key, value in os.environ.items():
+        if isinstance(key, unicode): key = key.encode('utf-8')
+        if isinstance(value, unicode): value = value.encode('utf-8')
         f.writelines([key, '=', value, '\0'])
 finally:
     f.close()
