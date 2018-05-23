JetBrains/intellij-community

commit 03ee61640fb66448ef01cf8699acdaf39af9d227

[groovy] extract some token sets from SpacingTokens
Express binary op set via sum of subsets.
This should help to implement IDEA-188427 and like.

plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/GroovyTokenSets.java
@@ -1,0 +1,33 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.plugins.groovy.lang.psi;
+
+import com.intellij.psi.tree.TokenSet;
+
+import static com.intellij.psi.tree.TokenSet.create;
+import static com.intellij.psi.tree.TokenSet.orSet;
+import static org.jetbrains.plugins.groovy.lang.psi.GroovyElementTypes.*;
+
+public interface GroovyTokenSets {
+
+  TokenSet LOGICAL_OPERATORS = create(T_LAND, T_LOR);
+  TokenSet EQUALITY_OPERATORS = create(T_EQ, T_NEQ);
+  TokenSet RELATIONAL_OPERATORS = create(T_GT, T_GE, T_LT, T_LE, T_COMPARE);
+  TokenSet BITWISE_OPERATORS = create(T_BAND, T_BOR, T_XOR);
+  TokenSet ADDITIVE_OPERATORS = create(T_PLUS, T_MINUS);
+  TokenSet MULTIPLICATIVE_OPERATORS = create(T_STAR, T_DIV, T_REM);
+  TokenSet SHIFT_OPERATORS = create(LEFT_SHIFT_SIGN, RIGHT_SHIFT_SIGN, RIGHT_SHIFT_UNSIGNED_SIGN);
+  TokenSet REGEX_OPERATORS = create(T_REGEX_FIND, T_REGEX_MATCH);
+  TokenSet RANGES = create(T_RANGE, T_RANGE_EX);
+  TokenSet OTHER_OPERATORS = create(KW_AS, KW_IN, T_POW, KW_INSTANCEOF);
+  TokenSet BINARY_OPERATORS = orSet(
+    LOGICAL_OPERATORS,
+    EQUALITY_OPERATORS,
+    RELATIONAL_OPERATORS,
+    BITWISE_OPERATORS,
+    ADDITIVE_OPERATORS,
+    MULTIPLICATIVE_OPERATORS,
+    SHIFT_OPERATORS,
+    RANGES,
+    OTHER_OPERATORS
+  );
+}

plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/highlighter/GroovySyntaxHighlighter.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package org.jetbrains.plugins.groovy.highlighter;
 
@@ -38,6 +24,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey;
+import static org.jetbrains.plugins.groovy.lang.psi.GroovyTokenSets.BINARY_OPERATORS;
 
 /**
  * @author ilyas
@@ -132,7 +119,7 @@ public class GroovySyntaxHighlighter extends SyntaxHighlighterBase {
     GroovyTokenTypes.mRBRACK
   );
 
-  static final TokenSet tOperators = TokenSet.orSet(TokenSets.BINARY_OP_SET, TokenSets.UNARY_OP_SET, TokenSets.ASSIGNMENTS);
+  static final TokenSet tOperators = TokenSet.orSet(BINARY_OPERATORS, TokenSets.UNARY_OP_SET, TokenSets.ASSIGNMENTS);
 
   static {
     fillMap(ATTRIBUTES, tLINE_COMMENTS, LINE_COMMENT);

plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/lexer/TokenSets.java
@@ -6,6 +6,7 @@ import com.intellij.psi.TokenType;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.tree.TokenSet;
 import com.intellij.util.containers.ContainerUtil;
+import org.jetbrains.plugins.groovy.lang.psi.GroovyTokenSets;
 
 import java.util.Map;
 
@@ -158,20 +159,8 @@ public interface TokenSets {
 
   TokenSet POSTFIX_UNARY_OP_SET = TokenSet.create(mDEC, mINC);
 
-  TokenSet BINARY_OP_SET = TokenSet.create(mBAND, mBOR, mBXOR,
-                                           mDIV, mEQUAL, mGE,
-                                           mGT, mLOR, mLT,
-                                           mLE, mMINUS, kAS,
-                                           kIN,
-                                           mMOD, mPLUS, mSTAR,
-                                           mSTAR_STAR, mNOT_EQUAL,
-                                           mCOMPARE_TO, mLAND,
-                                           kINSTANCEOF,
-                                           COMPOSITE_LSHIFT_SIGN,
-                                           COMPOSITE_RSHIFT_SIGN,
-                                           COMPOSITE_TRIPLE_SHIFT_SIGN,
-                                           mREGEX_FIND, mREGEX_MATCH,
-                                           mRANGE_INCLUSIVE, mRANGE_EXCLUSIVE);
+  @Deprecated
+  TokenSet BINARY_OP_SET = GroovyTokenSets.BINARY_OPERATORS;
 
   TokenSet PARENTHESIZED_BINARY_OP_SET = TokenSet.create(mEQUAL, mNOT_EQUAL);
 

plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/statements/expressions/GrBinaryExpressionImpl.java
@@ -1,6 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package org.jetbrains.plugins.groovy.lang.psi.impl.statements.expressions;
 
@@ -11,11 +9,12 @@ import com.intellij.psi.PsiType;
 import com.intellij.psi.tree.IElementType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.plugins.groovy.lang.lexer.TokenSets;
 import org.jetbrains.plugins.groovy.lang.psi.GroovyElementVisitor;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrBinaryExpression;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrExpression;
 import org.jetbrains.plugins.groovy.lang.psi.impl.statements.GrOperatorExpressionImpl;
+
+import static org.jetbrains.plugins.groovy.lang.psi.GroovyTokenSets.BINARY_OPERATORS;
 
 /**
  * @author ilyas
@@ -54,7 +53,7 @@ public abstract class GrBinaryExpressionImpl extends GrOperatorExpressionImpl im
   @Override
   @NotNull
   public PsiElement getOperationToken() {
-    return findNotNullChildByType(TokenSets.BINARY_OP_SET);
+    return findNotNullChildByType(BINARY_OPERATORS);
   }
 
   @Nullable

plugins/groovy/src/org/jetbrains/plugins/groovy/formatter/models/spacing/SpacingTokens.java
@@ -1,51 +1,21 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package org.jetbrains.plugins.groovy.formatter.models.spacing;
 
 import com.intellij.psi.tree.TokenSet;
-import org.jetbrains.plugins.groovy.lang.lexer.GroovyTokenTypes;
-import org.jetbrains.plugins.groovy.lang.parser.GroovyElementTypes;
+
+import static com.intellij.psi.tree.TokenSet.create;
+import static org.jetbrains.plugins.groovy.lang.psi.GroovyElementTypes.*;
 
 /**
  * @author ilyas
  */
 public interface SpacingTokens {
 
-  TokenSet LEFT_BRACES = TokenSet.create(GroovyTokenTypes.mLPAREN, GroovyTokenTypes.mLBRACK, GroovyTokenTypes.mLCURLY);
-  TokenSet RIGHT_BRACES = TokenSet.create(GroovyTokenTypes.mRPAREN, GroovyTokenTypes.mRBRACK, GroovyTokenTypes.mRCURLY);
+  TokenSet LEFT_BRACES = create(T_LPAREN, T_LBRACK, T_LBRACE);
+  TokenSet RIGHT_BRACES = create(T_RPAREN, T_RBRACK, T_RBRACE);
 
-  TokenSet INDEX_OR_ARRAY = TokenSet.create(GroovyElementTypes.PATH_INDEX_PROPERTY, GroovyElementTypes.ARRAY_TYPE,
-                                            GroovyElementTypes.ARRAY_DECLARATOR);
-
-  TokenSet PREFIXES = TokenSet.create(GroovyTokenTypes.mDEC, GroovyTokenTypes.mINC, GroovyTokenTypes.mAT, GroovyTokenTypes.mBNOT,
-                                      GroovyTokenTypes.mLNOT);
-  TokenSet POSTFIXES = TokenSet.create(GroovyTokenTypes.mDEC, GroovyTokenTypes.mINC);
-  TokenSet PREFIXES_OPTIONAL = TokenSet.create(GroovyTokenTypes.mPLUS, GroovyTokenTypes.mMINUS);
-
-  TokenSet RANGES = TokenSet.create(GroovyTokenTypes.mRANGE_EXCLUSIVE, GroovyTokenTypes.mRANGE_INCLUSIVE);
-
-  TokenSet LOGICAL_OPERATORS = TokenSet.create(GroovyTokenTypes.mLAND, GroovyTokenTypes.mLOR);
-  TokenSet EQUALITY_OPERATORS = TokenSet.create(GroovyTokenTypes.mEQUAL, GroovyTokenTypes.mNOT_EQUAL);
-  TokenSet RELATIONAL_OPERATORS = TokenSet.create(GroovyTokenTypes.mGT, GroovyTokenTypes.mGE, GroovyTokenTypes.mLT, GroovyTokenTypes.mLE,
-                                                  GroovyTokenTypes.mCOMPARE_TO);
-  TokenSet BITWISE_OPERATORS = TokenSet.create(GroovyTokenTypes.mBAND, GroovyTokenTypes.mBOR, GroovyTokenTypes.mBXOR);
-  TokenSet ADDITIVE_OPERATORS = TokenSet.create(GroovyTokenTypes.mPLUS, GroovyTokenTypes.mMINUS);
-  TokenSet MULTIPLICATIVE_OPERATORS = TokenSet.create(GroovyTokenTypes.mSTAR, GroovyTokenTypes.mDIV, GroovyTokenTypes.mMOD);
-  TokenSet SHIFT_OPERATORS = TokenSet.create(GroovyElementTypes.COMPOSITE_LSHIFT_SIGN, GroovyElementTypes.COMPOSITE_RSHIFT_SIGN,
-                                             GroovyElementTypes.COMPOSITE_TRIPLE_SHIFT_SIGN);
-  TokenSet REGEX_OPERATORS = TokenSet.create(GroovyTokenTypes.mREGEX_FIND, GroovyTokenTypes.mREGEX_MATCH);
+  TokenSet POSTFIXES = create(T_DEC, T_INC);
+  TokenSet PREFIXES = create(T_DEC, T_INC, T_AT, T_BNOT, T_NOT);
+  TokenSet PREFIXES_OPTIONAL = create(T_PLUS, T_MINUS);
 }

plugins/groovy/src/org/jetbrains/plugins/groovy/formatter/processors/GroovySpacingProcessor.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package org.jetbrains.plugins.groovy.formatter.processors;
 
@@ -73,6 +59,8 @@ import org.jetbrains.plugins.groovy.lang.psi.api.types.GrArrayTypeElement;
 import org.jetbrains.plugins.groovy.lang.psi.api.types.GrTypeArgumentList;
 import org.jetbrains.plugins.groovy.lang.psi.api.types.GrTypeParameterList;
 import org.jetbrains.plugins.groovy.lang.psi.impl.PsiImplUtil;
+
+import static org.jetbrains.plugins.groovy.lang.psi.GroovyTokenSets.*;
 
 /**
  * @author ilyas
@@ -771,16 +759,16 @@ public class GroovySpacingProcessor extends GroovyElementVisitor {
   public void visitBinaryExpression(@NotNull GrBinaryExpression expression) {
 
     @SuppressWarnings("SimplifiableConditionalExpression" )
-    boolean spaceAround = isLeftOrRight(SpacingTokens.LOGICAL_OPERATORS)        ? mySettings.SPACE_AROUND_LOGICAL_OPERATORS :
-                          isLeftOrRight(SpacingTokens.EQUALITY_OPERATORS)       ? mySettings.SPACE_AROUND_EQUALITY_OPERATORS :
-                          isLeftOrRight(SpacingTokens.RELATIONAL_OPERATORS)     ? mySettings.SPACE_AROUND_RELATIONAL_OPERATORS :
-                          isLeftOrRight(SpacingTokens.BITWISE_OPERATORS)        ? mySettings.SPACE_AROUND_BITWISE_OPERATORS :
-                          isLeftOrRight(SpacingTokens.ADDITIVE_OPERATORS)       ? mySettings.SPACE_AROUND_ADDITIVE_OPERATORS :
-                          isLeftOrRight(SpacingTokens.MULTIPLICATIVE_OPERATORS) ? mySettings.SPACE_AROUND_MULTIPLICATIVE_OPERATORS :
-                          isLeftOrRight(SpacingTokens.SHIFT_OPERATORS)          ? mySettings.SPACE_AROUND_SHIFT_OPERATORS :
-                          isLeftOrRight(SpacingTokens.REGEX_OPERATORS)          ? myGroovySettings.SPACE_AROUND_REGEX_OPERATORS :
+    boolean spaceAround = isLeftOrRight(LOGICAL_OPERATORS) ? mySettings.SPACE_AROUND_LOGICAL_OPERATORS :
+                          isLeftOrRight(EQUALITY_OPERATORS) ? mySettings.SPACE_AROUND_EQUALITY_OPERATORS :
+                          isLeftOrRight(RELATIONAL_OPERATORS) ? mySettings.SPACE_AROUND_RELATIONAL_OPERATORS :
+                          isLeftOrRight(BITWISE_OPERATORS) ? mySettings.SPACE_AROUND_BITWISE_OPERATORS :
+                          isLeftOrRight(ADDITIVE_OPERATORS) ? mySettings.SPACE_AROUND_ADDITIVE_OPERATORS :
+                          isLeftOrRight(MULTIPLICATIVE_OPERATORS) ? mySettings.SPACE_AROUND_MULTIPLICATIVE_OPERATORS :
+                          isLeftOrRight(SHIFT_OPERATORS) ? mySettings.SPACE_AROUND_SHIFT_OPERATORS :
+                          isLeftOrRight(REGEX_OPERATORS) ? myGroovySettings.SPACE_AROUND_REGEX_OPERATORS :
                           isLeftOrRight(GroovyTokenTypes.kIN);
-    if (TokenSets.BINARY_OP_SET.contains(myType2)) {
+    if (BINARY_OPERATORS.contains(myType2)) {
       createDependentLFSpacing(mySettings.BINARY_OPERATION_SIGN_ON_NEXT_LINE, spaceAround, expression.getTextRange());
     }
     else {

plugins/groovy/src/org/jetbrains/plugins/groovy/formatter/processors/GroovySpacingProcessorBasic.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package org.jetbrains.plugins.groovy.formatter.processors;
 
@@ -47,6 +33,8 @@ import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.GrAnonymousC
 import org.jetbrains.plugins.groovy.lang.psi.api.types.GrCodeReferenceElement;
 import org.jetbrains.plugins.groovy.lang.psi.api.types.GrTypeArgumentList;
 import org.jetbrains.plugins.groovy.lang.psi.api.util.GrStatementOwner;
+
+import static org.jetbrains.plugins.groovy.lang.psi.GroovyTokenSets.RANGES;
 
 /**
  * @author ilyas
@@ -143,7 +131,7 @@ public abstract class GroovySpacingProcessorBasic {
       return NO_SPACING_WITH_NEWLINE;
     }
 
-    if (SpacingTokens.RANGES.contains(leftType) || SpacingTokens.RANGES.contains(rightType)) {
+    if (RANGES.contains(leftType) || RANGES.contains(rightType)) {
       return NO_SPACING_WITH_NEWLINE;
     }
 

commit cf740ba9aa3671b36dd4b8c44dd1dde764de2bfb

[groovy] include regex operators into binary operators set
Fix issues introduced in 03ee616.

plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/GroovyTokenSets.java
@@ -27,6 +27,7 @@ public interface GroovyTokenSets {
     ADDITIVE_OPERATORS,
     MULTIPLICATIVE_OPERATORS,
     SHIFT_OPERATORS,
+    REGEX_OPERATORS,
     RANGES,
     OTHER_OPERATORS
   );