9007827a8ccf793fb2191c2f819aea3734b79e2f
peter <peter@jetbrains.com>
2018-05-23 14:18:50 +0200

    checkPsiMatchesTextIgnoringNonCode: compare element types
    not custom toString, which might include comments that we ignore

--- platform/core-impl/src/com/intellij/psi/impl/DebugUtil.java
+++ platform/core-impl/src/com/intellij/psi/impl/DebugUtil.java
@@ -401,7 +401,7 @@ public class DebugUtil {
   public static String psiToStringIgnoringNonCode(@NotNull PsiElement element) {
     StringBuilder buffer = new StringBuilder();
     ((TreeElement)element.getNode()).acceptTree(
-      new TreeToBuffer(buffer, 0, true, false, false, true, null) {
+      new TreeToBuffer(buffer, 0, true, false, false, false, null) {
         @Override
         protected boolean shouldSkipNode(TreeElement node) {
           return super.shouldSkipNode(node) || node instanceof PsiErrorElement || node instanceof PsiComment || 
