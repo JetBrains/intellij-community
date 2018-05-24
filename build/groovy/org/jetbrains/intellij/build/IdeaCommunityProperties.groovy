5b17ffc3a07c7cadf49b60b01bf902a1efd2b1f4
nik <Nikolay.Chashnikov@jetbrains.com>
2018-05-24 10:16:06 +0300

    ui: use special icons for IntelliJ IDEA EAPs on Linux and Windows (IDEA-166447)

--- build/groovy/org/jetbrains/intellij/build/IdeaCommunityProperties.groovy
+++ build/groovy/org/jetbrains/intellij/build/IdeaCommunityProperties.groovy
@@ -67,6 +67,7 @@ class IdeaCommunityProperties extends BaseIdeaProperties {
     return new WindowsDistributionCustomizer() {
       {
         icoPath = "$projectHome/platform/icons/src/idea_CE.ico"
+        icoPathForEAP = "$projectHome/build/conf/ideaCE/win/images/idea_CE_EAP.ico"
         installerImagesPath = "$projectHome/build/conf/ideaCE/win/images"
         fileAssociations = ["java", "groovy", "kt"]
       }
@@ -92,6 +93,7 @@ class IdeaCommunityProperties extends BaseIdeaProperties {
     return new LinuxDistributionCustomizer() {
       {
         iconPngPath = "$projectHome/platform/icons/src/icon_CE_128.png"
+        iconPngPathForEAP = "$projectHome/build/conf/ideaCE/linux/images/icon_CE_EAP_128.png"
         snapName = "intellij-idea-community"
         snapDescription =
           "The most intelligent Java IDE. Every aspect of IntelliJ IDEA is specifically designed to maximize developer productivity. " +
