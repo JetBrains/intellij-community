7615d88e35d18a31eb40c20280f102435bd171cf
Ivan Semenov <ivan.semenov@jetbrains.com>
2018-05-24 18:04:06 +0300

    [github] Ask for a new token when account is selected for git operation
    Token can be missing due to psafe wipe (i.e. memory-only mode)

--- plugins/github/src/org/jetbrains/plugins/github/authentication/GithubAuthenticationManager.kt
+++ plugins/github/src/org/jetbrains/plugins/github/authentication/GithubAuthenticationManager.kt
@@ -1,6 +1,8 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.github.authentication
 
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.application.invokeAndWaitIfNeed
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import git4idea.DialogManager
@@ -12,6 +14,7 @@ import org.jetbrains.plugins.github.authentication.accounts.GithubAccount
 import org.jetbrains.plugins.github.authentication.accounts.GithubAccountManager
 import org.jetbrains.plugins.github.authentication.accounts.GithubProjectDefaultAccountHolder
 import org.jetbrains.plugins.github.authentication.ui.GithubLoginDialog
+import javax.swing.JComponent
 
 /**
  * Entry point for interactions with Github authentication subsystem
@@ -26,6 +29,31 @@ class GithubAuthenticationManager internal constructor(private val accountManage
   @CalledInAny
   internal fun getTokenForAccount(account: GithubAccount): String? = accountManager.getTokenForAccount(account)
 
+  @JvmOverloads
+  @CalledInAny
+  internal fun getOrRequestTokenForAccount(account: GithubAccount,
+                                           project: Project? = null,
+                                           parentComponent: JComponent? = null,
+                                           modalityStateSupplier: () -> ModalityState = { ModalityState.any() }): String? {
+    return getTokenForAccount(account) ?: invokeAndWaitIfNeed(modalityStateSupplier()) { requestNewToken(account, project, parentComponent) }
+  }
+
+  @CalledInAwt
+  private fun requestNewToken(account: GithubAccount, project: Project?, parentComponent: JComponent?): String? {
+    val dialog = GithubLoginDialog(project, parentComponent)
+      .withServer(account.server.toString(), false)
+      .withCredentials(account.name)
+      .withToken()
+
+    DialogManager.show(dialog)
+    if (!dialog.isOK) return null
+
+    val token = dialog.getToken()
+    account.name = dialog.getLogin()
+    accountManager.updateAccountToken(account, token)
+    return token
+  }
+
   @CalledInAwt
   fun requestNewAccount(project: Project): GithubAccount? {
     fun isAccountUnique(name: String, server: GithubServerPath) =
@@ -33,13 +61,12 @@ class GithubAuthenticationManager internal constructor(private val accountManage
 
     val dialog = GithubLoginDialog(project, null, ::isAccountUnique)
     DialogManager.show(dialog)
-    if (dialog.isOK) {
-      val account = GithubAccountManager.createAccount(dialog.getLogin(), dialog.getServer())
-      accountManager.accounts += account
-      accountManager.updateAccountToken(account, dialog.getToken())
-      return account
-    }
-    return null
+    if (!dialog.isOK) return null
+
+    val account = GithubAccountManager.createAccount(dialog.getLogin(), dialog.getServer())
+    accountManager.accounts += account
+    accountManager.updateAccountToken(account, dialog.getToken())
+    return account
   }
 
   @TestOnly
--- plugins/github/src/org/jetbrains/plugins/github/authentication/ui/GithubLoginDialog.kt
+++ plugins/github/src/org/jetbrains/plugins/github/authentication/ui/GithubLoginDialog.kt
@@ -42,7 +42,7 @@ import javax.swing.*
 import javax.swing.event.HyperlinkEvent
 import javax.swing.text.html.HTMLEditorKit
 
-class GithubLoginDialog @JvmOverloads constructor(private val project: Project,
+class GithubLoginDialog @JvmOverloads constructor(private val project: Project? = null,
                                                   parent: JComponent? = null,
                                                   private val isAccountUnique: (name: String, server: GithubServerPath) -> Boolean = { _, _ -> true },
                                                   title: String = "Log In to Github",
