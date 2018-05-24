f863775a7e680adf8bab620ce7765d610f289459
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-05-24 10:38:35 +0200

    IDEA-192579 fix text node serialization if contains space
    (cherry picked from commit 2d7b074034ff673625bd5e1e94a148f038e3e0c8)

--- platform/configuration-store-impl/testSrc/BinaryXmlOutputterTest.kt
+++ platform/configuration-store-impl/testSrc/BinaryXmlOutputterTest.kt
@@ -1,3 +1,4 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.configurationStore
 
 import com.intellij.openapi.util.JDOMUtil
@@ -9,6 +10,13 @@ import org.junit.Test
 class BinaryXmlOutputterTest {
   @Test fun noAttributes() {
     test("""<foo />""")
+  }
+
+  @Test fun textWithSpaces() {
+    test("""
+      <foo>
+        <bar>Hello world</bar>
+      </foo>""".trimMargin())
   }
 
   @Test fun attributes() {
