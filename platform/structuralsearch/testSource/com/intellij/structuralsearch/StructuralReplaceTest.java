5cebf2f239a72ee25c6a026d87e5b09b65b9d616
Bas Leijdekkers <basleijdekkers@gmail.com>
2018-05-25 11:32:00 +0200

    SSR: replace array type elements correctly (IDEA-192548)

--- platform/structuralsearch/testSource/com/intellij/structuralsearch/StructuralReplaceTest.java
+++ platform/structuralsearch/testSource/com/intellij/structuralsearch/StructuralReplaceTest.java
@@ -2439,4 +2439,26 @@ public class StructuralReplaceTest extends StructuralReplaceTestCase {
                  "}",
                  replace(in, "new '_X<'_p+>()", "new $X$</*0*/$p$>()", true));
   }
+
+  public void testArrays() {
+    String in = "public abstract class Bar {\n" +
+                "    String[] x;\n" +
+                "    abstract String[] foo(String[] x);\n" +
+                "}";
+
+    assertEquals("should keep array brackets 1",
+                 "public abstract class Bar {\n" +
+                 "    String[] x;\n" +
+                 "    abstract String[] foo(String[] x);\n" +
+                 "}",
+                 replace(in, "'_FieldType 'Field = '_Init?;", "$FieldType$ $Field$ = $Init$;", true));
+
+    assertEquals("should keep array brackets 2",
+                 "public abstract class Bar {\n" +
+                 "    String[] x;\n" +
+                 "    abstract String[] foo (String[] x );\n" +
+                 "}",
+                 replace(in, "'_ReturnType '_Method('_ParameterType '_Parameter*);",
+                         "$ReturnType$ $Method$ ($ParameterType$ $Parameter$);", true));
+  }
 }
