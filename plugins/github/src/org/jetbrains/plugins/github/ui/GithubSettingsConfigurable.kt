aff8eeaeae26aa3a0043ee622bdcf1a9a77ee068
Ivan Semenov <ivan.semenov@jetbrains.com>
2018-05-25 16:53:24 +0300

    [github] Update settings view when psafe is cleared

--- plugins/github/src/org/jetbrains/plugins/github/ui/GithubSettingsConfigurable.kt
+++ plugins/github/src/org/jetbrains/plugins/github/ui/GithubSettingsConfigurable.kt
@@ -1,10 +1,13 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.github.ui
 
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.service
 import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.options.ConfigurableBase
 import com.intellij.openapi.project.Project
+import org.jetbrains.plugins.github.authentication.accounts.AccountTokenChangedListener
+import org.jetbrains.plugins.github.authentication.accounts.GithubAccount
 import org.jetbrains.plugins.github.authentication.accounts.GithubAccountManager
 import org.jetbrains.plugins.github.authentication.accounts.GithubProjectDefaultAccountHolder
 import org.jetbrains.plugins.github.util.GithubSettings
@@ -15,6 +18,16 @@ class GithubSettingsConfigurable(private val project: Project) :
                                                                                          GithubUtil.SERVICE_DISPLAY_NAME,
                                                                                          "settings.github"),
   Configurable.NoMargin {
+
+  init {
+    ApplicationManager.getApplication().messageBus
+      .connect(project)
+      .subscribe(GithubAccountManager.ACCOUNT_TOKEN_CHANGED_TOPIC, object : AccountTokenChangedListener {
+        override fun tokenChanged(account: GithubAccount) {
+          if (!isModified) reset()
+        }
+      })
+  }
 
   override fun getSettings(): GithubSettingsHolder {
     return GithubSettingsHolder(service(), service(), project.service())
