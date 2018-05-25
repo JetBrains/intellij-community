a2f9ef0a4356c88bf81a43e87dcb6ba98a967257
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2018-05-25 16:03:06 +0300

    Cleanup (minor optimization; formatting)

--- platform/platform-impl/src/com/intellij/diagnostic/ITNReporter.kt
+++ platform/platform-impl/src/com/intellij/diagnostic/ITNReporter.kt
@@ -41,7 +41,27 @@ open class ITNReporter : ErrorReportSubmitter() {
                       additionalInfo: String?,
                       parentComponent: Component,
                       consumer: Consumer<SubmittedReportInfo>): Boolean {
-    return submit(events[0], parentComponent, consumer, ErrorBean(events[0].throwable, IdeaLogger.ourLastActionId), additionalInfo)
+    val event = events[0]
+
+    val errorBean = ErrorBean(event.throwable, IdeaLogger.ourLastActionId)
+    errorBean.message = event.message
+    errorBean.description = additionalInfo
+
+    setPluginInfo(event, errorBean)
+
+    val data = event.data
+    if (data is AbstractMessage) {
+      errorBean.assigneeId = data.assigneeId
+      errorBean.attachments = data.includedAttachments
+    }
+
+    if (previousExceptionThreadId != 0) {
+      errorBean.previousException = previousExceptionThreadId
+    }
+
+    val project = CommonDataKeys.PROJECT.getData(DataManager.getInstance().getDataContext(parentComponent))
+
+    return submit(errorBean, parentComponent, consumer, project)
   }
 
   /**
@@ -73,40 +93,19 @@ private fun showMessageDialog(parentComponent: Component, project: Project?, mes
   }
 }
 
-private fun submit(event: IdeaLoggingEvent,
-                   parentComponent: Component,
-                   callback: Consumer<SubmittedReportInfo>,
-                   errorBean: ErrorBean,
-                   description: String?): Boolean {
+private fun submit(errorBean: ErrorBean, parentComponent: Component, callback: Consumer<SubmittedReportInfo>, project: Project?): Boolean {
   var credentials = ErrorReportConfigurable.getCredentials()
   if (credentials.hasOnlyUserName()) {
     // ask password only if user name was specified
     if (!showJetBrainsAccountDialog(parentComponent).showAndGet()) {
       return false
     }
-
     credentials = ErrorReportConfigurable.getCredentials()
   }
 
-  errorBean.description = description
-  errorBean.message = event.message
-
-  if (previousExceptionThreadId != 0) {
-    errorBean.previousException = previousExceptionThreadId
-  }
-
-  setPluginInfo(event, errorBean)
-
-  val data = event.data
-  if (data is AbstractMessage) {
-    errorBean.assigneeId = data.assigneeId
-    errorBean.attachments = data.includedAttachments
-  }
-
-  val project = CommonDataKeys.PROJECT.getData(DataManager.getInstance().getDataContext(parentComponent))
   ITNProxy.sendError(project, credentials?.userName, credentials?.getPasswordAsString(), errorBean,
                      { threadId -> onSuccess(threadId, callback, project) },
-                     { e -> onError(e, parentComponent, project, callback, event, errorBean, description) })
+                     { e -> onError(e, errorBean, parentComponent, callback, project) })
   return true
 }
 
@@ -120,39 +119,36 @@ private fun onSuccess(threadId: Int, callback: Consumer<SubmittedReportInfo>, pr
     IdeErrorsDialog.appendSubmissionInformation(reportInfo, text)
     text.append('.').append("<br/>").append(DiagnosticBundle.message("error.report.gratitude"))
     val content = XmlStringUtil.wrapInHtml(text)
-    ReportMessages.GROUP.createNotification(ReportMessages.ERROR_REPORT, content, NotificationType.INFORMATION,
-                                            NotificationListener.URL_OPENING_LISTENER).setImportant(false).notify(project)
+    ReportMessages.GROUP
+      .createNotification(ReportMessages.ERROR_REPORT, content, NotificationType.INFORMATION, NotificationListener.URL_OPENING_LISTENER)
+      .setImportant(false)
+      .notify(project)
   }
 }
 
-private fun onError(e: Exception,
-                    parentComponent: Component,
-                    project: Project?,
-                    callback: Consumer<SubmittedReportInfo>,
-                    event: IdeaLoggingEvent,
-                    errorBean: ErrorBean,
-                    description: String?) {
+private fun onError(e: Exception, errorBean: ErrorBean, parentComponent: Component, callback: Consumer<SubmittedReportInfo>, project: Project?) {
   Logger.getInstance(ITNReporter::class.java).info("reporting failed: $e")
   ApplicationManager.getApplication().invokeLater {
+    if (e is UpdateAvailableException) {
+      val message = DiagnosticBundle.message("error.report.new.eap.build.message", e.message)
+      showMessageDialog(parentComponent, project, message, CommonBundle.getWarningTitle(), Messages.getWarningIcon())
+      callback.consume(SubmittedReportInfo(SubmittedReportInfo.SubmissionStatus.FAILED))
+      return@invokeLater
+    }
+
     val msg = when (e) {
       is NoSuchEAPUserException -> DiagnosticBundle.message("error.report.authentication.failed")
       is InternalEAPException -> DiagnosticBundle.message("error.report.posting.failed", e.message)
       else -> DiagnosticBundle.message("error.report.sending.failure")
     }
-
-    if (e is UpdateAvailableException) {
-      val message = DiagnosticBundle.message("error.report.new.eap.build.message", e.message)
-      showMessageDialog(parentComponent, project, message, CommonBundle.getWarningTitle(), Messages.getWarningIcon())
-      callback.consume(SubmittedReportInfo(SubmittedReportInfo.SubmissionStatus.FAILED))
-    }
-    else if (!MessageDialogBuilder.yesNo(ReportMessages.ERROR_REPORT, msg).project(project).isYes) {
+    if (!MessageDialogBuilder.yesNo(ReportMessages.ERROR_REPORT, msg).project(project).isYes) {
       callback.consume(SubmittedReportInfo(SubmittedReportInfo.SubmissionStatus.FAILED))
     }
     else {
       if (e is NoSuchEAPUserException) {
         showJetBrainsAccountDialog(parentComponent, project).show()
       }
-      ApplicationManager.getApplication().invokeLater { submit(event, parentComponent, callback, errorBean, description) }
+      ApplicationManager.getApplication().invokeLater { submit(errorBean, parentComponent, callback, project) }
     }
   }
 }
