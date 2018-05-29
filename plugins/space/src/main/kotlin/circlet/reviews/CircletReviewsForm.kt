aff7cbc0f9c520c2718fcf64cd16a0b9de0f9f9d
Ivan Buryak <Ivan.Buryak@jetbrains.com>
2018-05-29 16:44:16 +0700

    Do not reload reviews while already request is launched
    GitOrigin-RevId: 006b16a0ae4dd4b137d857a042567e9e433fa57a

--- plugins/space/src/main/kotlin/circlet/reviews/CircletReviewsForm.kt
+++ plugins/space/src/main/kotlin/circlet/reviews/CircletReviewsForm.kt
@@ -8,6 +8,7 @@ import circlet.settings.*
 import com.intellij.openapi.project.*
 import kotlinx.coroutines.experimental.*
 import runtime.*
+import runtime.async.*
 import runtime.reactive.*
 import javax.swing.*
 
@@ -17,17 +18,39 @@ class CircletReviewsForm(private val project: Project, override val lifetime: Li
     lateinit var panel: JPanel
         private set
 
-    fun reload() {
-        launch(UiDispatch.coroutineContext) {
-            val reviews = project.clientOrNull?.codeReview?.listReviews(
-                BatchInfo(null, 30), ProjectKey(project.settings.projectKey.value), false
-            )?.data ?: return@launch
+    private val reloader = updater<Unit>("Reviews Reloader") {
+        reloadImpl()
+    }
 
-            reload(reviews)
-        }
+    fun reload() {
+        reloader.offer()
+    }
+
+    private suspend fun reloadImpl() {
+        val reviews = project.clientOrNull?.codeReview?.listReviews(
+            BatchInfo(null, 30), ProjectKey(project.settings.projectKey.value), false
+        )?.data ?: return
+
+        reload(reviews)
     }
 
     private fun reload(reviews: List<CodeReviewShortInfo>) {
         println("reviews = $reviews")
     }
+}
+
+private fun <T> Lifetimed.updater(name: String, update: suspend (T) -> Unit): Channel<T> {
+    val channel = boundedChannel<T>(name, 0, lifetime)
+
+    launch(UiDispatch.coroutineContext, start = CoroutineStart.UNDISPATCHED) {
+        channel.forEach {
+            update(it)
+        }
+    }
+
+    return channel
+}
+
+private fun Channel<Unit>.offer() {
+    offer(Unit)
 }
