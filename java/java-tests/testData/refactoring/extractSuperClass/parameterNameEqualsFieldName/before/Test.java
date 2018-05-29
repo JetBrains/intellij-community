e5604d0b2129d68ddad22f274fcbb90e545b9410
Anna Kozlova <anna.kozlova@jetbrains.com>
2018-05-29 17:23:23 +0300

    extract super: ensure moved members order (IDEA-192850)

--- java/java-tests/testData/refactoring/extractSuperClass/parameterNameEqualsFieldName/before/Test.java
+++ java/java-tests/testData/refactoring/extractSuperClass/parameterNameEqualsFieldName/before/Test.java
@@ -1,8 +1,10 @@
 public class Test {
     int a;
     int b;
-    public Test(int a, int b) {
+    int c;
+    public Test(int a, int b, int c) {
         this.a = a;
         this.b = b;
+        this.c = c;
     }
 }
