cd56cab1bf4055d261f607a8151d75ce3c603362
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2018-05-30 12:13:32 +0700

    ObjectAllocationInLoop: support new object returning methods; minor improvements
    Fixes IDEA-184914 Object allocation in loop doesn't detect LocalDate.now()

--- plugins/InspectionGadgets/test/com/siyeh/igtest/performance/object_allocation_in_loop/ObjectAllocationInLoop.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/performance/object_allocation_in_loop/ObjectAllocationInLoop.java
@@ -1,10 +1,47 @@
 package com.siyeh.igtest.performance.object_allocation_in_loop;
 
+import java.util.regex.*;
+
 class ObjectAllocationInLoop {
 
   void m() {
     while (true) {
       new <warning descr="Object allocation 'new Object()' in loop">Object</warning>();
     }
+  }
+
+  void m1() {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < 10; i++) {
+      if(sb != null) {
+        sb.append(i);
+      } else {
+        sb = new StringBuilder(String.valueOf(i));
+      }
+    }
+  }
+
+  void m2() {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < 10; i++) {
+      if (sb == null) {
+        sb = new StringBuilder(String.valueOf(i));
+      }
+      else {
+        sb.append(i);
+      }
+    }
+  }
+
+  boolean checkPatterns(String[] patterns) {
+    for (String pattern : patterns) {
+      try {
+        Pattern.<warning descr="Indirect object allocation via 'compile()' call in loop">compile</warning>(pattern);
+      }
+      catch (PatternSyntaxException exception) {
+        return false;
+      }
+    }
+    return true;
   }
 }
