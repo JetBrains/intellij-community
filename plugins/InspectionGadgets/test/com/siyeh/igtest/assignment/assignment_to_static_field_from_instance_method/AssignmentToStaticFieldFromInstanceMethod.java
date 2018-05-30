4e06a9deb23fecde0bc523fb4a92466830547653
Vladimir Koshelev <Vladimir.Koshelev@jetbrains.com>
2018-05-30 22:02:56 +0300

    AssignmentToStaticFieldFromInstanceMethod: fix static field case
    IDEA-CR-33151

--- plugins/InspectionGadgets/test/com/siyeh/igtest/assignment/assignment_to_static_field_from_instance_method/AssignmentToStaticFieldFromInstanceMethod.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/assignment/assignment_to_static_field_from_instance_method/AssignmentToStaticFieldFromInstanceMethod.java
@@ -3,6 +3,8 @@ package com.siyeh.igtest.assignment.assignment_to_static_field_from_instance_met
 public class AssignmentToStaticFieldFromInstanceMethod {
     private static int x = 32;
     private int y = <warning descr="Assignment to static field 'x' from instance context">x</warning> = 8;
+    private static int z = ++x;
+
 
     public void foo()
     {
