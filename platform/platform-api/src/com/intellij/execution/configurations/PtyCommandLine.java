e7b450c975ba004e972e7b8256253d50ca4823e8
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2018-05-30 13:48:46 +0300

    pty: add setInitialColumns/setInitialRows to PtyCommandLine

--- platform/platform-api/src/com/intellij/execution/configurations/PtyCommandLine.java
+++ platform/platform-api/src/com/intellij/execution/configurations/PtyCommandLine.java
@@ -19,15 +19,18 @@ import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.PathManager;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.registry.Registry;
 import com.intellij.util.ArrayUtil;
 import com.pty4j.PtyProcessBuilder;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -44,14 +47,39 @@ public class PtyCommandLine extends GeneralCommandLine {
   private static final Logger LOG = Logger.getInstance("#com.intellij.execution.configurations.PtyCommandLine");
   private static final String RUN_PROCESSES_WITH_PTY = "run.processes.with.pty";
 
+  private static final String UNIX_PTY_INIT = "unix.pty.init";
+  private static final String UNIX_PTY_COLUMNS = "unix.pty.cols";
+  private static final String UNIX_PTY_ROWS = "unix.pty.rows";
+
+  private static final String WIN_PTY_COLUMNS = "win.pty.cols";
+  private static final String WIN_PTY_ROWS = "win.pty.rows";
+
   public static boolean isEnabled() {
     return Registry.is(RUN_PROCESSES_WITH_PTY);
   }
 
   private boolean myUseCygwinLaunch;
   private boolean myConsoleMode = true;
+  private int myInitialColumns = -1;
+  private int myInitialRows = -1;
 
   public PtyCommandLine() { }
+
+  public void setUseCygwinLaunch(boolean useCygwinLaunch) {
+    myUseCygwinLaunch = useCygwinLaunch;
+  }
+
+  public void setConsoleMode(boolean consoleMode) {
+    myConsoleMode = consoleMode;
+  }
+
+  public void setInitialColumns(int initialColumns) {
+    myInitialColumns = initialColumns;
+  }
+
+  public void setInitialRows(int initialRows) {
+    myInitialRows = initialRows;
+  }
 
   @NotNull
   @Override
@@ -80,14 +108,6 @@ public class PtyCommandLine extends GeneralCommandLine {
     return super.startProcess(commands);
   }
 
-  public void setUseCygwinLaunch(boolean useCygwinLaunch) {
-    myUseCygwinLaunch = useCygwinLaunch;
-  }
-
-  public void setConsoleMode(boolean consoleMode) {
-    myConsoleMode = consoleMode;
-  }
-
   private static File getPtyLogFile() {
     Application app = ApplicationManager.getApplication();
     return app != null && app.isEAP() ? new File(PathManager.getLogPath(), "pty.log") : null;
@@ -95,6 +115,51 @@ public class PtyCommandLine extends GeneralCommandLine {
 
   @NotNull
   public Process startProcessWithPty(@NotNull List<String> commands) throws IOException {
+    List<Pair<String, String>> backup = new ArrayList<>();
+    try {
+      if (SystemInfo.isUnix && (myInitialColumns > 0 || myInitialRows > 0)) {
+        setSystemProperty(UNIX_PTY_INIT, Boolean.toString(true), backup);
+        if (myInitialColumns > 0) {
+          setSystemProperty(UNIX_PTY_COLUMNS, Integer.toString(myInitialColumns), backup);
+        }
+        if (myInitialRows > 0) {
+          setSystemProperty(UNIX_PTY_ROWS, Integer.toString(myInitialRows), backup);
+        }
+      }
+      else if (SystemInfo.isWindows) {
+        if (myInitialColumns > 0) {
+          setSystemProperty(WIN_PTY_COLUMNS, Integer.toString(myInitialColumns), backup);
+        }
+        if (myInitialRows > 0) {
+          setSystemProperty(WIN_PTY_ROWS, Integer.toString(myInitialRows), backup);
+        }
+      }
+      return doStartProcessWithPty(commands);
+    }
+    finally {
+      for (Pair<String, String> pair : backup) {
+        setSystemProperty(pair.first, pair.second, null);
+      }
+    }
+  }
+
+  private static void setSystemProperty(@NotNull String propertyName,
+                                        @Nullable String newPropertyValue,
+                                        @Nullable List<Pair<String, String>> backup) {
+    if (backup != null) {
+      String oldValue = System.getProperty(propertyName);
+      backup.add(Pair.create(propertyName, oldValue));
+    }
+    if (newPropertyValue != null) {
+      System.setProperty(propertyName, newPropertyValue);
+    }
+    else {
+      System.clearProperty(propertyName);
+    }
+  }
+
+  @NotNull
+  private Process doStartProcessWithPty(@NotNull List<String> commands) throws IOException {
     Map<String, String> env = new HashMap<>();
     setupEnvironment(env);
 
