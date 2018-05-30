0796495e71bc48798638032e0d5f3571c11acb81
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2018-05-30 19:10:19 +0300

    PY-30153: Suggest fixtures for other fixtures
    "getFixtures" is now smart enough to understand if provided function is allowed to use fixtures.
    
    Only tests (when pytest is enabled) and other fixtures can be used with fixtures.

--- python/src/com/jetbrains/python/testing/pyTestFixtures/PyTestFixtureCompletionContributor.kt
+++ python/src/com/jetbrains/python/testing/pyTestFixtures/PyTestFixtureCompletionContributor.kt
@@ -5,32 +5,20 @@ import com.intellij.openapi.module.ModuleUtilCore
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.util.ProcessingContext
-import com.intellij.util.ThreeState
-import com.jetbrains.python.PyNames
 import com.jetbrains.python.codeInsight.completion.PythonLookupElement
 import com.jetbrains.python.psi.PyParameterList
 import com.jetbrains.python.psi.types.TypeEvalContext
-import com.jetbrains.python.testing.PyTestFrameworkService
-import com.jetbrains.python.testing.TestRunnerService
-import com.jetbrains.python.testing.isTestElement
 import icons.PythonIcons
 
-private val pyTestName = PyTestFrameworkService.getSdkReadableNameByFramework(PyNames.PY_TEST)
 
 private object PyTestFixtureCompletionProvider : CompletionProvider<CompletionParameters>() {
   override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext?, result: CompletionResultSet) {
     val pyFunction = PsiTreeUtil.getParentOfType(parameters.position, PyParameterList::class.java)?.containingFunction ?: return
     val module = ModuleUtilCore.findModuleForPsiElement(pyFunction) ?: return
     val typeEvalContext = TypeEvalContext.userInitiated(pyFunction.project, pyFunction.containingFile)
-
-    if (TestRunnerService.getInstance(module).projectConfiguration != pyTestName) {
-      return
-    }
     val usedParams = pyFunction.getParameters(typeEvalContext).mapNotNull { it.name }.toSet()
-    if (isTestElement(pyFunction, ThreeState.UNSURE, typeEvalContext)) {
-      getFixtures(module).map { it.name }.filter { !usedParams.contains(it) }.forEach {
-        result.addElement(PythonLookupElement(it, false, PythonIcons.Python.Function))
-      }
+    getFixtures(module, pyFunction, typeEvalContext).map { it.name }.filter { !usedParams.contains(it) }.forEach {
+      result.addElement(PythonLookupElement(it, false, PythonIcons.Python.Function))
     }
   }
 }
