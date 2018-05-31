fa76f87efb6fa80c88ad8a73a5243f1e4611f057
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2018-05-31 16:17:48 +0300

    vcs: Convert "CommitLegendPanel" to kotlin

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/CommitLegendPanel.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/CommitLegendPanel.kt
@@ -1,80 +1,51 @@
-/*
- * Copyright 2000-2012 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
-package com.intellij.openapi.vcs.changes.ui;
+package com.intellij.openapi.vcs.changes.ui
 
-import com.intellij.openapi.vcs.FileStatus;
-import com.intellij.openapi.vcs.VcsBundle;
-import com.intellij.ui.SimpleColoredComponent;
-import com.intellij.ui.SimpleTextAttributes;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.vcs.FileStatus
+import com.intellij.openapi.vcs.VcsBundle
+import com.intellij.ui.SimpleColoredComponent
+import com.intellij.ui.SimpleTextAttributes
 
-import javax.swing.*;
+open class CommitLegendPanel(private val myInfoCalculator: InfoCalculator) {
+  private val myRootPanel = SimpleColoredComponent()
+  private val isPanelEmpty get() = !myRootPanel.iterator().hasNext()
 
-public class CommitLegendPanel {
+  val component get() = myRootPanel
 
-  @NotNull private final SimpleColoredComponent myRootPanel;
-  @NotNull private final InfoCalculator myInfoCalculator;
-
-  public CommitLegendPanel(@NotNull InfoCalculator infoCalculator) {
-    myInfoCalculator = infoCalculator;
-    myRootPanel = new SimpleColoredComponent();
+  open fun update() {
+    myRootPanel.clear()
+    appendText(myInfoCalculator.new, myInfoCalculator.includedNew, FileStatus.ADDED, VcsBundle.message("commit.legend.new"))
+    appendText(myInfoCalculator.modified, myInfoCalculator.includedModified, FileStatus.MODIFIED,
+               VcsBundle.message("commit.legend.modified"))
+    appendText(myInfoCalculator.deleted, myInfoCalculator.includedDeleted, FileStatus.DELETED, VcsBundle.message("commit.legend.deleted"))
+    appendText(myInfoCalculator.unversioned, myInfoCalculator.includedUnversioned, FileStatus.UNKNOWN,
+               VcsBundle.message("commit.legend.unversioned"))
   }
 
-  @NotNull
-  public JComponent getComponent() {
-    return myRootPanel;
-  }
-
-  public void update() {
-    myRootPanel.clear();
-    appendText(myInfoCalculator.getNew(), myInfoCalculator.getIncludedNew(), FileStatus.ADDED, VcsBundle.message("commit.legend.new"));
-    appendText(myInfoCalculator.getModified(), myInfoCalculator.getIncludedModified(), FileStatus.MODIFIED, VcsBundle.message("commit.legend.modified"));
-    appendText(myInfoCalculator.getDeleted(), myInfoCalculator.getIncludedDeleted(), FileStatus.DELETED, VcsBundle.message("commit.legend.deleted"));
-    appendText(myInfoCalculator.getUnversioned(), myInfoCalculator.getIncludedUnversioned(), FileStatus.UNKNOWN,
-               VcsBundle.message("commit.legend.unversioned"));
-  }
-
-  protected void appendText(int total, int included, @NotNull FileStatus fileStatus, @NotNull String labelName) {
+  protected fun appendText(total: Int, included: Int, fileStatus: FileStatus, labelName: String) {
     if (total > 0) {
-      if (!isPanelEmpty()) {
-        appendSpace();
+      if (!isPanelEmpty) {
+        appendSpace()
       }
-      String pattern = total == included ? "%s %d" : "%s %d of %d";
-      String text = String.format(pattern, labelName, included, total);
-      myRootPanel.append(text, new SimpleTextAttributes(SimpleTextAttributes.STYLE_PLAIN, fileStatus.getColor()));
+      val pattern = if (total == included) "%s %d" else "%s %d of %d"
+      val text = String.format(pattern, labelName, included, total)
+      myRootPanel.append(text, SimpleTextAttributes(SimpleTextAttributes.STYLE_PLAIN, fileStatus.color))
     }
   }
 
-  private boolean isPanelEmpty() {
-    return !myRootPanel.iterator().hasNext();
+  protected fun appendSpace() {
+    myRootPanel.append("   ")
   }
 
-  protected final void appendSpace() {
-    myRootPanel.append("   ");
-  }
-
-  public interface InfoCalculator {
-    int getNew();
-    int getModified();
-    int getDeleted();
-    int getUnversioned();
-    int getIncludedNew();
-    int getIncludedModified();
-    int getIncludedDeleted();
-    int getIncludedUnversioned();
+  interface InfoCalculator {
+    val new: Int
+    val modified: Int
+    val deleted: Int
+    val unversioned: Int
+    val includedNew: Int
+    val includedModified: Int
+    val includedDeleted: Int
+    val includedUnversioned: Int
   }
 }
