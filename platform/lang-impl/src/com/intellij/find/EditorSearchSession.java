bfa99ff9cc89f6bd9e2604f5cbe59bf666340800
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2018-06-01 18:06:36 +0300

    Find in editor: add link to regex syntax tutorial

--- platform/lang-impl/src/com/intellij/find/EditorSearchSession.java
+++ platform/lang-impl/src/com/intellij/find/EditorSearchSession.java
@@ -4,6 +4,7 @@ package com.intellij.find;
 
 import com.intellij.execution.impl.ConsoleViewUtil;
 import com.intellij.find.editorHeaderActions.*;
+import com.intellij.find.impl.RegExHelpPopup;
 import com.intellij.find.impl.livePreview.LivePreviewController;
 import com.intellij.find.impl.livePreview.SearchResults;
 import com.intellij.ide.ui.UISettings;
@@ -109,6 +110,8 @@ public class EditorSearchSession implements SearchSession,
       .addExtraSearchActions(new ToggleMatchCase(),
                              new ToggleWholeWordsOnlyAction(),
                              new ToggleRegex(),
+                             new DefaultCustomComponentAction(
+                               () -> RegExHelpPopup.createRegExLink("<html><body><b>?</b></body></html>", null, null)),
                              new StatusTextAction(),
                              new DefaultCustomComponentAction(() -> myClickToHighlightLabel))
       .addSearchFieldActions(new RestorePreviousSettingsAction())
