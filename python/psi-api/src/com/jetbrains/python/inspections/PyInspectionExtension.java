342c81e2622376d5842277ec404d5c7821cb5ad5
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2018-06-04 18:15:35 +0300

    PY-30249: Compatibility fix (after IDEA-CR-33182)

--- python/psi-api/src/com/jetbrains/python/inspections/PyInspectionExtension.java
+++ python/psi-api/src/com/jetbrains/python/inspections/PyInspectionExtension.java
@@ -17,7 +17,18 @@ import java.util.List;
 public abstract class PyInspectionExtension {
   public static final ExtensionPointName<PyInspectionExtension> EP_NAME = ExtensionPointName.create("Pythonid.inspectionExtension");
 
-  public boolean ignoreUnused(PsiElement local, TypeEvalContext evalContext) {
+  public boolean ignoreUnused(PsiElement local, @NotNull TypeEvalContext evalContext) {
+    @SuppressWarnings("deprecation")
+    final boolean result = ignoreUnused(local);
+    return result;
+  }
+
+  /**
+   * @deprecated  use {@link #ignoreUnused(PsiElement, TypeEvalContext)} instead.
+   * Will be removed in 2019.2
+   */
+  @Deprecated
+  public boolean ignoreUnused(PsiElement local) {
     return false;
   }
 
@@ -44,7 +55,7 @@ public abstract class PyInspectionExtension {
   /**
    * Checks if unresolved reference could be ignored.
    *
-   * @param node element containing reference
+   * @param node      element containing reference
    * @param reference unresolved reference
    * @return true if the unresolved reference could be ignored
    */
