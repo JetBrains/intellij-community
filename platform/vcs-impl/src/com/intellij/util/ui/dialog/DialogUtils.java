51c21b5cb390e9277519c055ddb15c817f1beba5
Ivan Semenov <ivan.semenov@jetbrains.com>
2018-06-04 23:33:41 +0300

    [vcs] Fix github tests

--- platform/vcs-impl/src/com/intellij/util/ui/dialog/DialogUtils.java
+++ platform/vcs-impl/src/com/intellij/util/ui/dialog/DialogUtils.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.ui.dialog;
 
+import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.ui.DialogWrapper;
@@ -14,13 +15,20 @@ public final class DialogUtils {
   private DialogUtils() {}
 
   public static void invokeLaterAfterDialogShown(@NotNull DialogWrapper dialog, @NotNull Runnable action) {
-    dialog.getWindow().addWindowListener(new WindowAdapter() {
-      @Override
-      public void windowOpened(WindowEvent e) {
-        Window window = e.getWindow();
-        ApplicationManager.getApplication().invokeLater(action, ModalityState.stateForComponent(window));
-        window.removeWindowListener(this);
-      }
-    });
+    Application application = ApplicationManager.getApplication();
+    if (application.isHeadlessEnvironment()) {
+      // there's nothing to tie the action to, so we just schedule it as a next task
+      application.invokeLater(action, ModalityState.any());
+    }
+    else {
+      dialog.getWindow().addWindowListener(new WindowAdapter() {
+        @Override
+        public void windowOpened(WindowEvent e) {
+          Window window = e.getWindow();
+          application.invokeLater(action, ModalityState.stateForComponent(window));
+          window.removeWindowListener(this);
+        }
+      });
+    }
   }
 }
