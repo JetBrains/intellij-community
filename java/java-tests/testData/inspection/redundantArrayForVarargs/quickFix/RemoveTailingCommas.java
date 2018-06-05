2314c866f977d11c1ea6cc05f3433dcac75bbf3f
Anna Kozlova <anna.kozlova@jetbrains.com>
2018-06-05 17:50:18 +0300

    keep comments on remove redundant array initializer (IDEA-193337)

--- java/java-tests/testData/inspection/redundantArrayForVarargs/quickFix/RemoveTailingCommas.java
+++ java/java-tests/testData/inspection/redundantArrayForVarargs/quickFix/RemoveTailingCommas.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        display(new S<caret>tring[]{
+        display(new //c1
+                  S<caret>tring[]{
                 "hi",
         });
     }
