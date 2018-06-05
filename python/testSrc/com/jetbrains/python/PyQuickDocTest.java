8074425e21af9148ee866aade97578c30b1c7173
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2018-06-05 18:10:03 +0300

    PY-29717 Don't render separate table with parameters for Epytext docstrings

--- python/testSrc/com/jetbrains/python/PyQuickDocTest.java
+++ python/testSrc/com/jetbrains/python/PyQuickDocTest.java
@@ -496,6 +496,10 @@ public class PyQuickDocTest extends LightMarkedTestCase {
     checkHTMLOnly();
   }
 
+  public void testParamDescriptionEpytext() {
+    checkHTMLOnly();
+  }
+
   public void testPackage() {
     myFixture.copyDirectoryToProject(getTestName(false), "");
     final VirtualFile file = myFixture.findFileInTempDir("pkg/__init__.py");
