JetBrains/intellij-community

commit a76c97907250a0eba6d2c66a821a57102a303f78

support custom logic for reusing RunContentDescriptor (WEB-9523)

platform/lang-impl/src/com/intellij/execution/ui/RunContentManagerImpl.java
@@ -432,9 +432,13 @@ public class RunContentManagerImpl implements RunContentManager, Disposable {
       return null;
     }
     final RunContentDescriptor oldDescriptor = getRunContentDescriptorByContent(content);
-    if (oldDescriptor != null && !oldDescriptor.isContentReuseProhibited() ) {
-      //content.setExecutionId(executionId);
-      return oldDescriptor;
+    if (oldDescriptor != null) {
+      if (oldDescriptor.isContentReuseProhibited()) {
+        return null;
+      }
+      if (descriptor != null && oldDescriptor.getReusePolicy().canBeReusedBy(descriptor)) {
+        return oldDescriptor;
+      }
     }
 
     return null;

platform/lang-api/src/com/intellij/execution/ui/RunContentDescriptor.java
@@ -42,6 +42,7 @@ public class RunContentDescriptor implements Disposable {
   private final Icon myIcon;
   private final String myHelpId;
   private RunnerLayoutUi myRunnerLayoutUi = null;
+  private RunContentDescriptorReusePolicy myReusePolicy = RunContentDescriptorReusePolicy.DEFAULT;
 
   private boolean myActivateToolWindowWhenAdded = true;
   private boolean myReuseToolWindowActivation = false;
@@ -277,5 +278,16 @@ public class RunContentDescriptor implements Disposable {
   @ApiStatus.Experimental
   public boolean isHiddenContent() {
     return false;
+  }
+
+  @NotNull
+  @ApiStatus.Experimental
+  public RunContentDescriptorReusePolicy getReusePolicy() {
+    return myReusePolicy;
+  }
+
+  @ApiStatus.Experimental
+  public void setReusePolicy(@NotNull RunContentDescriptorReusePolicy reusePolicy) {
+    myReusePolicy = reusePolicy;
   }
 }

platform/lang-api/src/com/intellij/execution/ui/RunContentDescriptorReusePolicy.java
@@ -1,0 +1,24 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.execution.ui;
+
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @see RunContentDescriptor#isContentReuseProhibited()
+ */
+@ApiStatus.Experimental
+public abstract class RunContentDescriptorReusePolicy {
+
+  public static final RunContentDescriptorReusePolicy DEFAULT = new RunContentDescriptorReusePolicy() {
+    @Override
+    public boolean canBeReusedBy(@NotNull RunContentDescriptor newDescriptor) {
+      return true;
+    }
+  };
+
+  /**
+   * @return true if {@link RunContentDescriptor} instance with this policy can be reused by {@code newDescriptor}
+   */
+  public abstract boolean canBeReusedBy(@NotNull RunContentDescriptor newDescriptor);
+}

commit d28b3b262089cea6c83a762cb776c9c49f23b26a

reuse oldDescriptor if descriptor is null (IDEA-192962)
Regression after a76c97907250a0eba6d2c66a821a57102a303f78, previous behaviour is restored.

platform/lang-impl/src/com/intellij/execution/ui/RunContentManagerImpl.java
@@ -436,7 +436,7 @@ public class RunContentManagerImpl implements RunContentManager, Disposable {
       if (oldDescriptor.isContentReuseProhibited()) {
         return null;
       }
-      if (descriptor != null && oldDescriptor.getReusePolicy().canBeReusedBy(descriptor)) {
+      if (descriptor == null || oldDescriptor.getReusePolicy().canBeReusedBy(descriptor)) {
         return oldDescriptor;
       }
     }