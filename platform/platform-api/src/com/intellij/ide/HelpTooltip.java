3b5c898df70ed58ee3aed0d5d26ddd9bf47f3098
Kirill Kirichenko <kirill.kirichenko@jetbrains.com>
2018-06-05 15:37:35 +0300

    Help tooltip cleanup

--- platform/platform-api/src/com/intellij/ide/HelpTooltip.java
+++ platform/platform-api/src/com/intellij/ide/HelpTooltip.java
@@ -27,7 +27,6 @@ import com.intellij.ui.awt.RelativePoint;
 import com.intellij.ui.components.labels.LinkLabel;
 import com.intellij.ui.components.panels.VerticalLayout;
 import com.intellij.util.Alarm;
-import com.intellij.util.NotNullProducer;
 import com.intellij.util.ui.GraphicsUtil;
 import com.intellij.util.ui.JBEmptyBorder;
 import com.intellij.util.ui.JBUI;
@@ -474,21 +473,6 @@ public class HelpTooltip implements Disposable {
   private static Font modifyFont(Font font) {
     int deltaSize = JBUI.scale(UIManager.getInt("HelpTooltip.fontSizeDelta"));
     return font.deriveFont((float)font.getSize() + deltaSize);
-  }
-
-  private static class Producer implements NotNullProducer<Color> {
-    private final String resourceName;
-
-    private Producer(String resourceName) {
-      this.resourceName = resourceName;
-    }
-
-    @NotNull
-    @Override
-    public Color produce() {
-      Color c = UIManager.getColor(resourceName);
-      return c != null ? c : JBColor.black; // Return bogus color in headless mode
-    }
   }
 
   private class Header extends JPanel {
