d926633610250aabf849c92e6d0c57aee4c23825
Dmitry Jemerov <yole@jetbrains.com>
2018-06-06 10:23:49 +0200

    Browse repo at revision: better toolwindow management
    Focus newly added contents (IDEA-193259), don't open multiple copies
    of same repo/revision, close toolwindow when all contents are closed

--- platform/vcs-impl/src/com/intellij/openapi/vcs/impl/RepositoryBrowser.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/impl/RepositoryBrowser.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vcs.impl
 
+import com.intellij.ide.impl.ContentManagerWatcher
 import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.fileChooser.FileChooserDescriptor
 import com.intellij.openapi.fileChooser.ex.FileSystemTreeImpl
@@ -10,6 +11,7 @@ import com.intellij.openapi.fileTypes.FileTypeManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.vfs.AbstractVcsVirtualFile
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowAnchor
 import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.ui.ScrollPaneFactory
@@ -19,53 +21,74 @@ import java.awt.BorderLayout
 import javax.swing.Icon
 import javax.swing.JPanel
 
+const val TOOLWINDOW_ID = "Repositories"
+
 fun showRepositoryBrowser(project: Project, root: AbstractVcsVirtualFile, title: String) {
   val toolWindowManager = ToolWindowManager.getInstance(project)
-  val repoToolWindow = toolWindowManager.getToolWindow("Repositories")
-                         ?: toolWindowManager.registerToolWindow("Repositories", true, ToolWindowAnchor.LEFT)
+  val repoToolWindow = toolWindowManager.getToolWindow(TOOLWINDOW_ID)
+                       ?: registerRepositoriesToolWindow(toolWindowManager)
 
-  val contentPanel = createRepositoryBrowserPanel(project, root)
+  for (content in repoToolWindow.contentManager.contents) {
+    val component = content.component as? RepositoryBrowserPanel ?: continue
+    if (component.root == root) {
+      repoToolWindow.contentManager.setSelectedContent(content)
+      return
+    }
+  }
+
+  val contentPanel = RepositoryBrowserPanel(project, root)
 
   val content = ContentFactory.SERVICE.getInstance().createContent(contentPanel, title, true)
   repoToolWindow.contentManager.addContent(content)
+  repoToolWindow.contentManager.setSelectedContent(content, true)
   repoToolWindow.activate(null)
 }
 
-private fun createRepositoryBrowserPanel(project: Project,
-                                         root: AbstractVcsVirtualFile): JPanel {
-  val fileChooserDescriptor = object : FileChooserDescriptor(true, false, false, false, false, true) {
-    override fun getRoots(): List<VirtualFile> = listOf(root)
+private fun registerRepositoriesToolWindow(toolWindowManager: ToolWindowManager): ToolWindow {
+  val toolWindow = toolWindowManager.registerToolWindow(TOOLWINDOW_ID, true, ToolWindowAnchor.LEFT)
+  ContentManagerWatcher(toolWindow, toolWindow.contentManager)
+  return toolWindow
+}
 
-    override fun getIcon(file: VirtualFile): Icon? {
-      if (file.isDirectory) {
-        return PlatformIcons.FOLDER_ICON
-      }
-      return FileTypeManager.getInstance().getFileTypeByFileName(file.name).icon
-    }
-  }
-  val fileSystemTree = FileSystemTreeImpl(project, fileChooserDescriptor)
-  fileSystemTree.addOkAction {
-    val files = fileSystemTree.selectedFiles
-    for (file in files) {
-      FileEditorManager.getInstance(project).openFile(file, true)
-    }
-  }
+class RepositoryBrowserPanel(
+  private val project: Project,
+  val root: AbstractVcsVirtualFile
+) : JPanel(BorderLayout()), DataProvider {
+  private val fileSystemTree: FileSystemTreeImpl
 
-  val actionGroup = DefaultActionGroup()
-  actionGroup.add(ActionManager.getInstance().getAction(IdeActions.ACTION_EDIT_SOURCE))
-  fileSystemTree.registerMouseListener(actionGroup)
+  init {
+    val fileChooserDescriptor = object : FileChooserDescriptor(true, false, false, false, false, true) {
+      override fun getRoots(): List<VirtualFile> = listOf(root)
 
-  val scrollPane = ScrollPaneFactory.createScrollPane(fileSystemTree.tree)
-
-  val dataProviderPanel: JPanel = object : JPanel(BorderLayout()), DataProvider {
-    override fun getData(dataId: String?): Any? {
-      return when {
-        CommonDataKeys.VIRTUAL_FILE_ARRAY.`is`(dataId) -> fileSystemTree.selectedFiles
-        CommonDataKeys.NAVIGATABLE_ARRAY.`is`(dataId) -> fileSystemTree.selectedFiles.map { OpenFileDescriptor(project, it) }.toTypedArray()
-        else -> null
+      override fun getIcon(file: VirtualFile): Icon? {
+        if (file.isDirectory) {
+          return PlatformIcons.FOLDER_ICON
+        }
+        return FileTypeManager.getInstance().getFileTypeByFileName(file.name).icon
       }
     }
+    fileSystemTree = FileSystemTreeImpl(project, fileChooserDescriptor)
+    fileSystemTree.addOkAction {
+      val files = fileSystemTree.selectedFiles
+      for (file in files) {
+        FileEditorManager.getInstance(project).openFile(file, true)
+      }
+    }
+
+    val actionGroup = DefaultActionGroup()
+    actionGroup.add(ActionManager.getInstance().getAction(IdeActions.ACTION_EDIT_SOURCE))
+    fileSystemTree.registerMouseListener(actionGroup)
+
+    val scrollPane = ScrollPaneFactory.createScrollPane(fileSystemTree.tree)
+
+    add(scrollPane, BorderLayout.CENTER)
   }
-  dataProviderPanel.add(scrollPane, BorderLayout.CENTER)
-  return dataProviderPanel
+
+  override fun getData(dataId: String?): Any? {
+    return when {
+      CommonDataKeys.VIRTUAL_FILE_ARRAY.`is`(dataId) -> fileSystemTree.selectedFiles
+      CommonDataKeys.NAVIGATABLE_ARRAY.`is`(dataId) -> fileSystemTree.selectedFiles.map { OpenFileDescriptor(project, it) }.toTypedArray()
+      else -> null
+    }
+  }
 }
