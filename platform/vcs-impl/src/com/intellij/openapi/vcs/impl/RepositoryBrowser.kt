3536ac0d3960c0e679e7b0d0391add5166794ed6
Dmitry Jemerov <yole@jetbrains.com>
2018-06-06 10:33:57 +0200

    Browse repo at revision: don't expand top-level directories

--- platform/vcs-impl/src/com/intellij/openapi/vcs/impl/RepositoryBrowser.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/impl/RepositoryBrowser.kt
@@ -2,9 +2,14 @@
 package com.intellij.openapi.vcs.impl
 
 import com.intellij.ide.impl.ContentManagerWatcher
+import com.intellij.ide.util.treeView.AbstractTreeBuilder
+import com.intellij.ide.util.treeView.AbstractTreeStructure
+import com.intellij.ide.util.treeView.NodeDescriptor
 import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.fileChooser.FileChooserDescriptor
 import com.intellij.openapi.fileChooser.ex.FileSystemTreeImpl
+import com.intellij.openapi.fileChooser.ex.RootFileElement
+import com.intellij.openapi.fileChooser.impl.FileTreeBuilder
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.OpenFileDescriptor
 import com.intellij.openapi.fileTypes.FileTypeManager
@@ -18,8 +23,11 @@ import com.intellij.ui.ScrollPaneFactory
 import com.intellij.ui.content.ContentFactory
 import com.intellij.util.PlatformIcons
 import java.awt.BorderLayout
+import java.util.*
 import javax.swing.Icon
 import javax.swing.JPanel
+import javax.swing.JTree
+import javax.swing.tree.DefaultTreeModel
 
 const val TOOLWINDOW_ID = "Repositories"
 
@@ -67,7 +75,20 @@ class RepositoryBrowserPanel(
         return FileTypeManager.getInstance().getFileTypeByFileName(file.name).icon
       }
     }
-    fileSystemTree = FileSystemTreeImpl(project, fileChooserDescriptor)
+    fileSystemTree = object : FileSystemTreeImpl(project, fileChooserDescriptor) {
+      override fun createTreeBuilder(tree: JTree?,
+                                     treeModel: DefaultTreeModel?,
+                                     treeStructure: AbstractTreeStructure?,
+                                     comparator: Comparator<NodeDescriptor<Any>>?,
+                                     descriptor: FileChooserDescriptor?,
+                                     onInitialized: Runnable?): AbstractTreeBuilder {
+        return object : FileTreeBuilder(tree, treeModel, treeStructure, comparator, descriptor, onInitialized) {
+          override fun isAutoExpandNode(nodeDescriptor: NodeDescriptor<*>): Boolean {
+            return nodeDescriptor.element is RootFileElement
+          }
+        }
+      }
+    }
     fileSystemTree.addOkAction {
       val files = fileSystemTree.selectedFiles
       for (file in files) {
