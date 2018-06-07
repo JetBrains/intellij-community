5460a867d8cde9eda9d1993f877f7c398208a3f8
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2018-06-07 17:13:39 +0300

    PY-30359 PY-11425 Properly handle directive aliases in tag-based docstrings
    Namely, collect documented parameters, keyword arguments, raised
    exceptions and their respective descriptions across all possible
    directive aliases.

--- python/testSrc/com/jetbrains/python/EpydocStringTest.java
+++ python/testSrc/com/jetbrains/python/EpydocStringTest.java
@@ -23,6 +23,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 
 import static com.intellij.testFramework.UsefulTestCase.assertOrderedEquals;
+import static com.intellij.testFramework.UsefulTestCase.assertSameElements;
 
 /**
  * @author yole
@@ -121,5 +122,46 @@ public class EpydocStringTest extends TestCase {
   public void testRemoveNestedInlineMarkup() {
     assertEquals("(ParsedDocstring, list of Field)",
                  EpydocString.removeInlineMarkup("C{(L{ParsedDocstring}, list of L{Field})}"));
+  }
+
+  public void testDifferentExceptionTags() {
+    final EpydocString docstring = createEpydocDocString("@raises Exception1: foo\n" +
+                                                         "@except Exception2: bar");
+    assertSameElements(docstring.getRaisedExceptions(), "Exception1", "Exception2");
+    assertEquals("foo", docstring.getRaisedExceptionDescription("Exception1"));
+    assertEquals("bar", docstring.getRaisedExceptionDescription("Exception2"));
+  }
+
+  public void testFieldAliases() {
+    final EpydocString docstring = createEpydocDocString("@param p1: p1 description\n" +
+                                                         "@parameter p2: p2 description\n" +
+                                                         "@arg p3: p3 description\n" +
+                                                         "@argument p4: p4 description\n" +
+                                                         "\n" +
+                                                         "@kwarg key1: key1 description\n" +
+                                                         "@keyword key2: key2 description\n" +
+                                                         "@kwparam key3: key3 description\n" +
+                                                         "\n" +
+                                                         "@raises Exc1: Exc1 description  \n" +
+                                                         "@raise Exc2: Exc2 description \n" +
+                                                         "@except Exc3: Exc3 description \n" +
+                                                         "@exception Exc4: Exc4 description ");
+
+    assertSameElements(docstring.getParameters(), "p1", "p2", "p3", "p4");
+    assertEquals("p1 description", docstring.getParamDescription("p1"));
+    assertEquals("p2 description", docstring.getParamDescription("p2"));
+    assertEquals("p3 description", docstring.getParamDescription("p3"));
+    assertEquals("p4 description", docstring.getParamDescription("p4"));
+
+    assertSameElements(docstring.getKeywordArguments(), "key1", "key2", "key3");
+    assertEquals("key1 description", docstring.getKeywordArgumentDescription("key1"));
+    assertEquals("key2 description", docstring.getKeywordArgumentDescription("key2"));
+    assertEquals("key3 description", docstring.getKeywordArgumentDescription("key3"));
+
+    assertSameElements(docstring.getRaisedExceptions(), "Exc1", "Exc2", "Exc3", "Exc4");
+    assertEquals("Exc1 description", docstring.getRaisedExceptionDescription("Exc1"));
+    assertEquals("Exc2 description", docstring.getRaisedExceptionDescription("Exc2"));
+    assertEquals("Exc3 description", docstring.getRaisedExceptionDescription("Exc3"));
+    assertEquals("Exc4 description", docstring.getRaisedExceptionDescription("Exc4"));
   }
 }
