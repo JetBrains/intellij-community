JetBrains/intellij-community

commit 54c98acf3e01e65fec15a93848144d0de1d4d6ff

IDEA-171869 Win10 LaF combobox popups reworked

platform/platform-impl/src/com/intellij/ide/ui/laf/intellij/WinIntelliJComboBoxUI.java
@@ -25,6 +25,8 @@ import com.intellij.util.ui.JBDimension;
 import com.intellij.util.ui.JBInsets;
 import com.intellij.util.ui.JBUI;
 import com.intellij.util.ui.UIUtil;
+import com.intellij.util.ui.components.BorderLayoutPanel;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import javax.swing.border.Border;
@@ -32,11 +34,13 @@ import javax.swing.event.PopupMenuEvent;
 import javax.swing.plaf.ComponentUI;
 import javax.swing.plaf.basic.BasicArrowButton;
 import javax.swing.plaf.basic.BasicComboBoxEditor;
+import javax.swing.plaf.basic.BasicComboPopup;
 import javax.swing.plaf.basic.ComboPopup;
 import javax.swing.text.JTextComponent;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.Path2D;
+import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 /**
@@ -571,15 +575,67 @@ public class WinIntelliJComboBoxUI extends DarculaComboBoxUI {
 
 
   @Override protected ComboPopup createPopup() {
-    ComboPopup popup = super.createPopup();
-    if (popup instanceof JPopupMenu) {
-      ((JPopupMenu)popup).addPopupMenuListener(new PopupMenuListenerAdapter() {
-        @Override public void popupMenuWillBecomeInvisible(PopupMenuEvent e) {
-          comboBox.repaint();
+    return new BasicComboPopup(comboBox) {
+      @Override
+      protected void configurePopup() {
+        super.configurePopup();
+        addPopupMenuListener(new PopupMenuListenerAdapter() {
+          @Override
+          public void popupMenuWillBecomeInvisible(PopupMenuEvent e) {
+            comboBox.repaint();
+          }
+        });
+        setBorder(UIManager.getBorder("PopupMenu.border"));
+      }
+
+      @Override
+      protected void configureList() {
+        super.configureList();
+        list.setBackground(UIManager.getColor("TextField.background"));
+        wrapRenderer();
+      }
+
+      protected PropertyChangeListener createPropertyChangeListener() {
+        PropertyChangeListener listener = super.createPropertyChangeListener();
+        return new PropertyChangeListener() {
+          @Override
+          public void propertyChange(PropertyChangeEvent evt) {
+            listener.propertyChange(evt);
+            if ("renderer".equals(evt.getPropertyName())) {
+              wrapRenderer();
+            }
+          }
+        };
+      }
+
+      private void wrapRenderer() {
+        ListCellRenderer<Object> renderer = list.getCellRenderer();
+        if (!(renderer instanceof ComboBoxRendererWrapper) && renderer != null) {
+          list.setCellRenderer(new ComboBoxRendererWrapper(renderer));
         }
-      });
+      }
+
+      @Override
+      public void show(Component invoker, int x, int y) {
+        super.show(invoker, x, y - JBUI.scale(1)); // Move one pixel up to align with combobox border
+      }
+    };
+  }
+
+  private static class ComboBoxRendererWrapper implements ListCellRenderer<Object> {
+    private final ListCellRenderer<Object> myRenderer;
+
+    public ComboBoxRendererWrapper(@NotNull ListCellRenderer<Object> renderer) {
+      myRenderer = renderer;
     }
 
-    return popup;
+    @Override
+    public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+      Component c = myRenderer.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+      BorderLayoutPanel panel = JBUI.Panels.simplePanel(c).withBorder(JBUI.Borders.empty(0, 4, 0, 1));
+      panel.setBackground(c.getBackground());
+      return panel;
+    }
   }
+
 }

platform/platform-api/src/com/intellij/ui/ColoredListCellRenderer.java
@@ -66,8 +66,9 @@ public abstract class ColoredListCellRenderer<T> extends SimpleColoredComponent 
         setOpaque(true);
         setBackground(selected ? list.getSelectionBackground() : null);
       }
-    }
-    else {
+    } else if (UIUtil.isUnderWin10LookAndFeel()) {
+      setBackground(selected ? list.getSelectionBackground() : list.getBackground());
+    } else {
       setBackground(selected ? list.getSelectionBackground() : null);
     }
 

platform/platform-impl/src/com/intellij/ide/ui/laf/intellij/WinIntelliJPopupBorder.java
@@ -1,0 +1,61 @@
+/*
+ * Copyright 2000-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.ide.ui.laf.intellij;
+
+import com.intellij.util.ui.JBInsets;
+import com.intellij.util.ui.JBUI;
+
+import javax.swing.*;
+import javax.swing.border.AbstractBorder;
+import javax.swing.plaf.UIResource;
+import java.awt.*;
+import java.awt.geom.Path2D;
+
+public class WinIntelliJPopupBorder extends AbstractBorder implements UIResource {
+  @Override public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+    Graphics2D g2 = (Graphics2D)g.create();
+    try {
+      g2.setColor(UIManager.getDefaults().getColor("TextField.hoverBorderColor"));
+      g2.fill(getBorderShape(c, new Rectangle(x, y, width, height)));
+    } finally {
+      g2.dispose();
+    }
+  }
+
+  private Shape getBorderShape(Component c, Rectangle rect) {
+    Path2D border = new Path2D.Double(Path2D.WIND_EVEN_ODD);
+    if ("ComboPopup.popup".equals(c.getName())) {
+      JBInsets.removeFrom(rect, JBUI.insets(0, 1));
+    }
+
+    border.append(rect, false);
+
+    Rectangle innerRect = new Rectangle(rect);
+    JBInsets.removeFrom(innerRect, JBUI.insets(1));
+    border.append(innerRect, false);
+
+    return border;
+  }
+
+  @Override
+  public Insets getBorderInsets(Component c) {
+    if ("ComboPopup.popup".equals(c.getName())) {
+      return JBUI.insets(1, 2).asUIResource();
+    } else {
+      return JBUI.insets(1).asUIResource();
+    }
+  }
+}

platform/platform-impl/src/com/intellij/ide/ui/laf/intellijlaf_native.properties
@@ -175,7 +175,7 @@ FileView.fileIcon=AllIcons.FileTypes.Unknown
 darcula.selectionBackground=4A6EB7
 darcula.selectionForeground=ffffff
 
-PopupMenu.border=com.intellij.ide.ui.laf.darcula.ui.DarculaPopupMenuBorder
+PopupMenu.border=com.intellij.ide.ui.laf.intellij.WinIntelliJPopupBorder
 
 MenuBar.border=com.intellij.ide.ui.laf.intellij.WinMenuBarBorder
 

commit 17b1f3f4682f62d74b1d60d655ff855f4367b447

IDEA-193617 Fix a11y of combo boxes
This addresses a regression introduced by change 54c98acf.

See comment in code for description of the fix.

platform/platform-impl/src/com/intellij/ide/ui/laf/intellij/WinIntelliJComboBoxUI.java
@@ -571,6 +571,7 @@ public class WinIntelliJComboBoxUI extends DarculaComboBoxUI {
       BorderLayoutPanel panel = JBUI.Panels.simplePanel(c).withBorder(
         list.getComponentOrientation().isLeftToRight() ? JBUI.Borders.empty(0, 5, 0, 1) : JBUI.Borders.empty(0, 1, 0, 5));
       panel.setBackground(c.getBackground());
+      panel.setDelegateAccessibleContextToWrappedComponent(true);
       return panel;
     }
   }

platform/util/src/com/intellij/util/ui/components/BorderLayoutPanel.java
@@ -17,14 +17,20 @@ package com.intellij.util.ui.components;
 
 import com.intellij.ui.components.JBPanel;
 import com.intellij.util.ui.JBUI;
+import com.intellij.util.ui.accessibility.AccessibleContextDelegate;
 import org.jetbrains.annotations.NotNull;
 
+import javax.accessibility.Accessible;
+import javax.accessibility.AccessibleContext;
+import javax.accessibility.AccessibleRole;
 import java.awt.*;
 
 /**
  * @author Konstantin Bulenkov
  */
 public class BorderLayoutPanel extends JBPanel<BorderLayoutPanel> {
+  private boolean myDelegateAccessibleContext;
+
   public BorderLayoutPanel() {
     this(0, 0);
   }
@@ -61,5 +67,59 @@ public class BorderLayoutPanel extends JBPanel<BorderLayoutPanel> {
   public BorderLayoutPanel addToBottom(@NotNull Component comp) {
     add(comp, BorderLayout.SOUTH);
     return this;
+  }
+
+  /**
+   * Enables delegating the {@link AccessibleContext} implementation of this panel to the first (and only) component contained
+   * in this panel.
+   *
+   * By delegating to the inner component, we essentially remove this panel from the Accessibility component tree.
+   *
+   * The reason we need this is that many screen readers don't always know how to deal with labels wrapped in panels.
+   * For example, they expect items of list boxes or combo boxes to have the {@link AccessibleRole#LABEL} as well as some text.
+   */
+  public void setDelegateAccessibleContextToWrappedComponent(boolean delegateAccessibleContext) {
+    if (delegateAccessibleContext != myDelegateAccessibleContext) {
+      this.accessibleContext = null;
+    }
+    myDelegateAccessibleContext = delegateAccessibleContext;
+  }
+
+  @Override
+  public AccessibleContext getAccessibleContext() {
+    if (this.accessibleContext == null) {
+      if (myDelegateAccessibleContext) {
+        if (getComponentCount() == 1) {
+          AccessibleContext context = getComponent(0).getAccessibleContext();
+          if (context != null) {
+            this.accessibleContext = new MyAccessibleContextDelegate(context);
+          }
+        }
+      }
+    }
+    return super.getAccessibleContext();
+  }
+
+  private class MyAccessibleContextDelegate extends AccessibleContextDelegate {
+    public MyAccessibleContextDelegate(AccessibleContext context) {super(context);}
+
+    /**
+     * The parent should be the Swing parent of this panel, not the parent of the wrapped context,
+     * because the parent of the wrapped context is ourselves, i.e. this would result in
+     * an infinite accessible parent chain.
+     */
+    @Override
+    public Accessible getAccessibleParent() {
+      if (accessibleParent != null) {
+        return accessibleParent;
+      }
+      else {
+        Container parent = getParent();
+        if (parent instanceof Accessible) {
+          return (Accessible)parent;
+        }
+      }
+      return null;
+    }
   }
 }