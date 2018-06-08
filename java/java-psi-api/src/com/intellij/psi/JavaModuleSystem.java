6d77729f17f3b96a7d78f8078371558022ca2867
peter <peter@jetbrains.com>
2018-06-08 14:47:22 +0200

    diagnostics for EA-120906 - assert: FileManagerImpl.findDirectory

--- java/java-psi-api/src/com/intellij/psi/JavaModuleSystem.java
+++ java/java-psi-api/src/com/intellij/psi/JavaModuleSystem.java
@@ -3,6 +3,7 @@ package com.intellij.psi;
 
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.psi.util.PsiUtil;
+import com.intellij.psi.util.PsiUtilCore;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -14,8 +15,11 @@ public interface JavaModuleSystem {
   @NotNull String getName();
 
   default boolean isAccessible(@NotNull PsiClass target, @NotNull PsiElement place) {
+    PsiFile targetFile = target.getContainingFile();
+    PsiUtilCore.ensureValid(targetFile);
+
     String packageName = PsiUtil.getPackageName(target);
-    return packageName == null || isAccessible(packageName, target.getContainingFile(), place);
+    return packageName == null || isAccessible(packageName, targetFile, place);
   }
 
   boolean isAccessible(@NotNull String targetPackageName, @Nullable PsiFile targetFile, @NotNull PsiElement place);
