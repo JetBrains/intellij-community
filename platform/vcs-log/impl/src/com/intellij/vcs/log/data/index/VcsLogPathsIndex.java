41f187354548551b617c8cabfd2302fe7a30acc7
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2018-06-08 21:11:18 +0300

    [vcs-log] reformat

--- platform/vcs-log/impl/src/com/intellij/vcs/log/data/index/VcsLogPathsIndex.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/data/index/VcsLogPathsIndex.java
@@ -111,7 +111,7 @@ public class VcsLogPathsIndex extends VcsLogFullDetailsIndex<List<VcsLogPathsInd
   @NotNull
   private Set<Integer> getPathIds(@NotNull Collection<FilePath> paths) throws IOException {
     Set<Integer> allPathIds = ContainerUtil.newHashSet();
-    for (FilePath path : paths) {
+    for (FilePath path: paths) {
       allPathIds.add(myPathsIndexer.myPathsEnumerator.enumerate(path.getPath()));
     }
     return allPathIds;
@@ -130,7 +130,7 @@ public class VcsLogPathsIndex extends VcsLogFullDetailsIndex<List<VcsLogPathsInd
 
     outer:
     while (!startIds.isEmpty()) {
-      for (int currentPathId : startIds) {
+      for (int currentPathId: startIds) {
         boolean foundCommit = !iterateCommitIdsAndValues(currentPathId, (changesList, commitId) -> {
           Set<Integer> otherNames = getOtherNames(changesList);
           if (commitId == commit) {
@@ -138,7 +138,7 @@ public class VcsLogPathsIndex extends VcsLogFullDetailsIndex<List<VcsLogPathsInd
             resultIds.addAll(otherNames);
             return false;
           }
-          for (Integer otherPath : otherNames) {
+          for (Integer otherPath: otherNames) {
             if (!allIds.contains(otherPath)) {
               newIds.add(otherPath);
             }
@@ -153,7 +153,7 @@ public class VcsLogPathsIndex extends VcsLogFullDetailsIndex<List<VcsLogPathsInd
     }
 
     Set<FilePath> result = ContainerUtil.newHashSet();
-    for (Integer id : resultIds) {
+    for (Integer id: resultIds) {
       result.add(VcsUtil.getFilePath(myPathsIndexer.myPathsEnumerator.valueOf(id)));
     }
     return result;
@@ -166,11 +166,11 @@ public class VcsLogPathsIndex extends VcsLogFullDetailsIndex<List<VcsLogPathsInd
     Set<Integer> allIds = ContainerUtil.newHashSet(startIds);
     Set<Integer> newIds = ContainerUtil.newHashSet();
     while (!startIds.isEmpty()) {
-      for (int currentPathId : startIds) {
+      for (int currentPathId: startIds) {
         FilePath currentPath = VcsUtil.getFilePath(myPathsIndexer.myPathsEnumerator.valueOf(currentPathId));
         iterateCommitIdsAndValues(currentPathId, (changesList, commitId) -> {
           Set<Integer> otherNames = getOtherNames(changesList);
-          for (int renamed : otherNames) {
+          for (int renamed: otherNames) {
             if (!allIds.contains(renamed)) {
               newIds.add(renamed);
             }
@@ -191,7 +191,7 @@ public class VcsLogPathsIndex extends VcsLogFullDetailsIndex<List<VcsLogPathsInd
                                               @NotNull TIntHashSet commits)
     throws StorageException {
     Set<Integer> renames = ContainerUtil.newHashSet();
-    for (Integer key : newPathIds) {
+    for (Integer key: newPathIds) {
       iterateCommitIdsAndValues(key, (value, commit) -> {
         commits.add(commit);
         renames.addAll(ContainerUtil.filter(getOtherNames(value), r -> !allPathIds.contains(r)));
@@ -214,7 +214,7 @@ public class VcsLogPathsIndex extends VcsLogFullDetailsIndex<List<VcsLogPathsInd
   @NotNull
   private static Set<Integer> getOtherNames(@NotNull List<ChangeData> changesList) {
     Set<Integer> otherNames = ContainerUtil.newHashSet();
-    for (ChangeData data : changesList) {
+    for (ChangeData data: changesList) {
       if (data != null && data.otherPath != -1) {
         otherNames.add(data.otherPath);
       }
@@ -230,7 +230,7 @@ public class VcsLogPathsIndex extends VcsLogFullDetailsIndex<List<VcsLogPathsInd
     private PathsIndexer(@NotNull PersistentEnumeratorBase<String> enumerator, @NotNull Set<VirtualFile> roots) {
       myPathsEnumerator = enumerator;
       myRoots = newTroveSet(FileUtil.PATH_HASHING_STRATEGY);
-      for (VirtualFile root : roots) {
+      for (VirtualFile root: roots) {
         myRoots.add(root.getPath());
       }
     }
@@ -256,7 +256,7 @@ public class VcsLogPathsIndex extends VcsLogFullDetailsIndex<List<VcsLogPathsInd
         else {
           moves = ContainerUtil.newHashSet();
           changedPaths = ContainerUtil.newHashSet();
-          for (Change change : inputData.getChanges()) {
+          for (Change change: inputData.getChanges()) {
             if (change.getAfterRevision() != null) changedPaths.add(change.getAfterRevision().getFile().getPath());
             if (change.getBeforeRevision() != null) changedPaths.add(change.getBeforeRevision().getFile().getPath());
             if (change.getType().equals(Change.Type.MOVED)) {
@@ -291,190 +291,190 @@ public class VcsLogPathsIndex extends VcsLogFullDetailsIndex<List<VcsLogPathsInd
       }
 
       return result;
-  }
-
-  private void addChangeToResult(@NotNull Map<Integer, List<ChangeData>> commitChangesMap, int parent,
-                                 int parentsCount, @NotNull String afterPath, @Nullable String beforePath) throws IOException {
-    int afterId = myPathsEnumerator.enumerate(afterPath);
-    List<ChangeData> changeDataList = getOrCreateChangeDataListForPath(commitChangesMap, afterId, parentsCount);
-    if (beforePath == null) {
-      addChange(changeDataList, parent, new ChangeData(ChangeKind.MODIFIED, -1));
     }
-    else {
-      int beforeId = myPathsEnumerator.enumerate(beforePath);
-      if (beforeId == afterId && !SystemInfo.isFileSystemCaseSensitive) {
-        // case only rename in case insensitive file system
-        // since ids for before and after paths are the same we just treating this rename as a modification
+
+    private void addChangeToResult(@NotNull Map<Integer, List<ChangeData>> commitChangesMap, int parent,
+                                   int parentsCount, @NotNull String afterPath, @Nullable String beforePath) throws IOException {
+      int afterId = myPathsEnumerator.enumerate(afterPath);
+      List<ChangeData> changeDataList = getOrCreateChangeDataListForPath(commitChangesMap, afterId, parentsCount);
+      if (beforePath == null) {
         addChange(changeDataList, parent, new ChangeData(ChangeKind.MODIFIED, -1));
       }
       else {
-        addChange(changeDataList, parent, new ChangeData(ChangeKind.RENAMED_TO, beforeId));
-        List<ChangeData> beforeChangeDataList = getOrCreateChangeDataListForPath(commitChangesMap, beforeId, parentsCount);
-        addChange(beforeChangeDataList, parent, new ChangeData(ChangeKind.RENAMED_FROM, afterId));
-      }
-    }
-  }
-
-  @NotNull
-  private static List<ChangeData> getOrCreateChangeDataListForPath(@NotNull Map<Integer, List<ChangeData>> pathIdToChangeDataListsMap,
-                                                                   int pathId, int parentsCount) {
-    List<ChangeData> changeDataList = pathIdToChangeDataListsMap.get(pathId);
-    if (changeDataList == null) {
-      changeDataList = ContainerUtil.newSmartList();
-      for (int i = 0; i < parentsCount; i++) {
-        changeDataList.add(null);
-      }
-      pathIdToChangeDataListsMap.put(pathId, changeDataList);
-    }
-    return changeDataList;
-  }
-
-  private static void addChange(@NotNull List<ChangeData> changeDataList, int parentIndex, @NotNull ChangeData change) {
-    ChangeData existingChange = changeDataList.get(parentIndex);
-    // most of the time, existing change is null
-    // but in case insensitive fs it is possible to have several changes for one file
-    // example two changes: R: abc -> AAA, D: aaa
-    // in this case we keep rename information
-    if (existingChange == null || (existingChange.kind != ChangeKind.RENAMED_FROM && existingChange.kind != ChangeKind.RENAMED_TO)) {
-      changeDataList.set(parentIndex, change);
-    }
-  }
-
-  @NotNull
-  private static Collection<String> getParentPaths(@NotNull Collection<String> paths, @NotNull VirtualFile root) {
-    Set<String> result = ContainerUtil.newHashSet();
-    for (String path : paths) {
-      while (!path.isEmpty() && !result.contains(path)) {
-        result.add(path);
-        if (FileUtil.PATH_HASHING_STRATEGY.equals(root.getPath(), path)) break;
-
-        path = PathUtil.getParentPath(path);
-      }
-    }
-    return result;
-  }
-
-  @NotNull
-  public PersistentEnumeratorBase<String> getPathsEnumerator() {
-    return myPathsEnumerator;
-  }
-}
-
-private static class ChangeDataListKeyDescriptor implements DataExternalizer<List<ChangeData>> {
-  @Override
-  public void save(@NotNull DataOutput out, List<ChangeData> value) throws IOException {
-    DataInputOutputUtil.writeINT(out, value.size());
-    for (ChangeData data : value) {
-      if (data == null) {
-        out.writeBoolean(false);
-      }
-      else {
-        out.writeBoolean(true);
-        out.writeByte(data.kind.id);
-        if (data.kind == ChangeKind.RENAMED_TO || data.kind == ChangeKind.RENAMED_FROM) {
-          out.writeInt(data.otherPath);
-        }
-      }
-    }
-  }
-
-  @Override
-  public List<ChangeData> read(@NotNull DataInput in) throws IOException {
-    List<ChangeData> value = ContainerUtil.newSmartList();
-
-    int size = DataInputOutputUtil.readINT(in);
-    for (int i = 0; i < size; i++) {
-      if (in.readBoolean()) {
-        ChangeKind kind = ChangeKind.getKind(in.readByte());
-        int otherPath;
-        if (kind == ChangeKind.RENAMED_TO || kind == ChangeKind.RENAMED_FROM) {
-          otherPath = in.readInt();
+        int beforeId = myPathsEnumerator.enumerate(beforePath);
+        if (beforeId == afterId && !SystemInfo.isFileSystemCaseSensitive) {
+          // case only rename in case insensitive file system
+          // since ids for before and after paths are the same we just treating this rename as a modification
+          addChange(changeDataList, parent, new ChangeData(ChangeKind.MODIFIED, -1));
         }
         else {
-          otherPath = -1;
+          addChange(changeDataList, parent, new ChangeData(ChangeKind.RENAMED_TO, beforeId));
+          List<ChangeData> beforeChangeDataList = getOrCreateChangeDataListForPath(commitChangesMap, beforeId, parentsCount);
+          addChange(beforeChangeDataList, parent, new ChangeData(ChangeKind.RENAMED_FROM, afterId));
         }
-        value.add(new ChangeData(kind, otherPath));
-      }
-      else {
-        value.add(null);
       }
     }
 
-    return value;
-  }
-}
-
-public static class ChangeData {
-  @NotNull public final ChangeKind kind;
-  public final int otherPath;
-
-  public ChangeData(@NotNull ChangeKind kind, int otherPath) {
-    this.kind = kind;
-    this.otherPath = otherPath;
-  }
-
-  public boolean isRename() {
-    return kind.equals(ChangeKind.RENAMED_FROM) || kind.equals(ChangeKind.RENAMED_TO);
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    ChangeData data = (ChangeData)o;
-    return otherPath == data.otherPath &&
-           kind == data.kind;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(kind, otherPath);
-  }
-}
-
-public enum ChangeKind {
-  MODIFIED((byte)0),
-  RENAMED_FROM((byte)1),
-  RENAMED_TO((byte)2);
-
-  public final byte id;
-
-  ChangeKind(byte id) {
-    this.id = id;
-  }
-
-  public static ChangeKind getKind(byte id) {
-    switch (id) {
-      case (0):
-        return MODIFIED;
-      case (1):
-        return RENAMED_FROM;
-      case (2):
-        return RENAMED_TO;
+    @NotNull
+    private static List<ChangeData> getOrCreateChangeDataListForPath(@NotNull Map<Integer, List<ChangeData>> pathIdToChangeDataListsMap,
+                                                                     int pathId, int parentsCount) {
+      List<ChangeData> changeDataList = pathIdToChangeDataListsMap.get(pathId);
+      if (changeDataList == null) {
+        changeDataList = ContainerUtil.newSmartList();
+        for (int i = 0; i < parentsCount; i++) {
+          changeDataList.add(null);
+        }
+        pathIdToChangeDataListsMap.put(pathId, changeDataList);
+      }
+      return changeDataList;
     }
-    throw new IllegalArgumentException("No change kind with id " + id);
-  }
-}
 
-private static class ToLowerCaseStringDescriptor implements KeyDescriptor<String> {
-  @Override
-  public int getHashCode(String value) {
-    return CaseInsensitiveStringHashingStrategy.INSTANCE.computeHashCode(value);
+    private static void addChange(@NotNull List<ChangeData> changeDataList, int parentIndex, @NotNull ChangeData change) {
+      ChangeData existingChange = changeDataList.get(parentIndex);
+      // most of the time, existing change is null
+      // but in case insensitive fs it is possible to have several changes for one file
+      // example two changes: R: abc -> AAA, D: aaa
+      // in this case we keep rename information
+      if (existingChange == null || (existingChange.kind != ChangeKind.RENAMED_FROM && existingChange.kind != ChangeKind.RENAMED_TO)) {
+        changeDataList.set(parentIndex, change);
+      }
+    }
+
+    @NotNull
+    private static Collection<String> getParentPaths(@NotNull Collection<String> paths, @NotNull VirtualFile root) {
+      Set<String> result = ContainerUtil.newHashSet();
+      for (String path: paths) {
+        while (!path.isEmpty() && !result.contains(path)) {
+          result.add(path);
+          if (FileUtil.PATH_HASHING_STRATEGY.equals(root.getPath(), path)) break;
+
+          path = PathUtil.getParentPath(path);
+        }
+      }
+      return result;
+    }
+
+    @NotNull
+    public PersistentEnumeratorBase<String> getPathsEnumerator() {
+      return myPathsEnumerator;
+    }
   }
 
-  @Override
-  public boolean isEqual(String val1, String val2) {
-    return CaseInsensitiveStringHashingStrategy.INSTANCE.equals(val1, val2);
+  private static class ChangeDataListKeyDescriptor implements DataExternalizer<List<ChangeData>> {
+    @Override
+    public void save(@NotNull DataOutput out, List<ChangeData> value) throws IOException {
+      DataInputOutputUtil.writeINT(out, value.size());
+      for (ChangeData data: value) {
+        if (data == null) {
+          out.writeBoolean(false);
+        }
+        else {
+          out.writeBoolean(true);
+          out.writeByte(data.kind.id);
+          if (data.kind == ChangeKind.RENAMED_TO || data.kind == ChangeKind.RENAMED_FROM) {
+            out.writeInt(data.otherPath);
+          }
+        }
+      }
+    }
+
+    @Override
+    public List<ChangeData> read(@NotNull DataInput in) throws IOException {
+      List<ChangeData> value = ContainerUtil.newSmartList();
+
+      int size = DataInputOutputUtil.readINT(in);
+      for (int i = 0; i < size; i++) {
+        if (in.readBoolean()) {
+          ChangeKind kind = ChangeKind.getKind(in.readByte());
+          int otherPath;
+          if (kind == ChangeKind.RENAMED_TO || kind == ChangeKind.RENAMED_FROM) {
+            otherPath = in.readInt();
+          }
+          else {
+            otherPath = -1;
+          }
+          value.add(new ChangeData(kind, otherPath));
+        }
+        else {
+          value.add(null);
+        }
+      }
+
+      return value;
+    }
   }
 
-  @Override
-  public void save(@NotNull DataOutput out, String value) throws IOException {
-    IOUtil.writeUTF(out, value.toLowerCase());
+  public static class ChangeData {
+    @NotNull public final ChangeKind kind;
+    public final int otherPath;
+
+    public ChangeData(@NotNull ChangeKind kind, int otherPath) {
+      this.kind = kind;
+      this.otherPath = otherPath;
+    }
+
+    public boolean isRename() {
+      return kind.equals(ChangeKind.RENAMED_FROM) || kind.equals(ChangeKind.RENAMED_TO);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      ChangeData data = (ChangeData)o;
+      return otherPath == data.otherPath &&
+             kind == data.kind;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(kind, otherPath);
+    }
   }
 
-  @Override
-  public String read(@NotNull DataInput in) throws IOException {
-    return IOUtil.readUTF(in);
+  public enum ChangeKind {
+    MODIFIED((byte)0),
+    RENAMED_FROM((byte)1),
+    RENAMED_TO((byte)2);
+
+    public final byte id;
+
+    ChangeKind(byte id) {
+      this.id = id;
+    }
+
+    public static ChangeKind getKind(byte id) {
+      switch (id) {
+        case (0):
+          return MODIFIED;
+        case (1):
+          return RENAMED_FROM;
+        case (2):
+          return RENAMED_TO;
+      }
+      throw new IllegalArgumentException("No change kind with id " + id);
+    }
   }
-}
+
+  private static class ToLowerCaseStringDescriptor implements KeyDescriptor<String> {
+    @Override
+    public int getHashCode(String value) {
+      return CaseInsensitiveStringHashingStrategy.INSTANCE.computeHashCode(value);
+    }
+
+    @Override
+    public boolean isEqual(String val1, String val2) {
+      return CaseInsensitiveStringHashingStrategy.INSTANCE.equals(val1, val2);
+    }
+
+    @Override
+    public void save(@NotNull DataOutput out, String value) throws IOException {
+      IOUtil.writeUTF(out, value.toLowerCase());
+    }
+
+    @Override
+    public String read(@NotNull DataInput in) throws IOException {
+      return IOUtil.readUTF(in);
+    }
+  }
 }
