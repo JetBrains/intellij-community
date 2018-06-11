1be0a93673558fe7a1d2a16ffbf714000ce1da74
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2018-06-11 20:58:39 +0300

    ScopeView: support projects which base dir does not belong to any module

--- platform/platform-impl/src/com/intellij/ui/tree/project/ProjectFileNode.java
+++ platform/platform-impl/src/com/intellij/ui/tree/project/ProjectFileNode.java
@@ -1,35 +1,21 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ui.tree.project;
 
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.project.Project;
+import com.intellij.openapi.extensions.AreaInstance;
+import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiManager;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public interface ProjectFileNode {
   @NotNull
-  Module getModule();
+  Object getRootID();
 
   @NotNull
   VirtualFile getVirtualFile();
 
-  default boolean isValid() {
-    return !getModule().isDisposed() && getVirtualFile().isValid();
-  }
-
-  @Nullable
-  default PsiElement getPsiElement() {
-    VirtualFile file = getVirtualFile();
-    if (!file.isValid()) return null;
-    Module module = getModule();
-    if (module.isDisposed()) return null;
-    Project project = module.getProject();
-    if (project.isDisposed()) return null;
-    return file.isDirectory()
-           ? PsiManager.getInstance(project).findDirectory(file)
-           : PsiManager.getInstance(project).findFile(file);
+  default boolean contains(@NotNull VirtualFile file, @NotNull AreaInstance area, boolean strict) {
+    Object id = getRootID();
+    if (id instanceof AreaInstance && !id.equals(area)) return false;
+    return VfsUtilCore.isAncestor(getVirtualFile(), file, strict);
   }
 }
