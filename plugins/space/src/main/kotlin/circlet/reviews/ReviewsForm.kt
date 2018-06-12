c2cafc0bd14a77ed181ee46ab65dca78becf8ffe
Maxim.Podkolzine <Maxim.Podkolzine@jetbrains.com>
2018-06-12 12:15:58 +0200

    compilation
    GitOrigin-RevId: 05621d491ddbb4b38ac094d544deee92e2c29c80

--- plugins/space/src/main/kotlin/circlet/reviews/ReviewsForm.kt
+++ plugins/space/src/main/kotlin/circlet/reviews/ReviewsForm.kt
@@ -32,7 +32,7 @@ class ReviewsForm(private val project: Project, parentLifetime: Lifetime) :
 
     private suspend fun reloadImpl() {
         val reviews = project.clientOrNull?.codeReview?.listReviews(
-            BatchInfo(null, 30), ProjectKey(project.settings.projectKey.value), null, null
+            BatchInfo(null, 30), ProjectKey(project.settings.projectKey.value), null, null, null, null, null, ReviewSorting.CreatedAtDesc
         )?.data ?: return
 
         reload(reviews)
