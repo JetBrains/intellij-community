2d2b14e494db14b6f48e60ef1a9d4c3170f52cb0
Ivan Semenov <ivan.semenov@jetbrains.com>
2018-06-13 17:58:31 +0300

    [vcs] Allow loading repository url suggestions separately
    Some hosting service integrations (like github) can provide loaders that will load urls in several steps.
    It is better to allow displaying partial results.

--- platform/dvcs-api/src/com/intellij/dvcs/hosting/RepositoryListLoader.java
+++ platform/dvcs-api/src/com/intellij/dvcs/hosting/RepositoryListLoader.java
@@ -4,14 +4,19 @@
 package com.intellij.dvcs.hosting;
 
 import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.util.Pair;
 import org.jetbrains.annotations.CalledInBackground;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
  * Allows to query remote service for a list of available VCS repositories with the current IDEA settings.
  * Can be used to suggest the list of repositories that can be checked out.
+ * <p>
+ * Implement either {@link #getAvailableRepositories(ProgressIndicator)} to load everything in a single request
+ * or {@link #getAvailableRepositoriesFromMultipleSources(ProgressIndicator)} to load in several requests
  */
 public interface RepositoryListLoader {
   /**
@@ -24,7 +29,26 @@ public interface RepositoryListLoader {
    */
   boolean enable();
 
+  /**
+   * Load repository urls in a single requests
+   */
   @CalledInBackground
   @NotNull
-  List<String> getAvailableRepositories(@NotNull ProgressIndicator progressIndicator) throws RepositoryListLoadingException;
+  default List<String> getAvailableRepositories(@NotNull ProgressIndicator progressIndicator) throws RepositoryListLoadingException {
+    return Collections.emptyList();
+  }
+
+  /**
+   * Load repository urls in multiple requests with ability to show partial result
+   */
+  @CalledInBackground
+  @NotNull
+  default Pair<List<String>, List<RepositoryListLoadingException>> getAvailableRepositoriesFromMultipleSources(@NotNull ProgressIndicator progressIndicator) {
+    try {
+      return Pair.create(getAvailableRepositories(progressIndicator), Collections.emptyList());
+    }
+    catch (RepositoryListLoadingException e) {
+      return Pair.create(Collections.emptyList(), Collections.singletonList(e));
+    }
+  }
 }
