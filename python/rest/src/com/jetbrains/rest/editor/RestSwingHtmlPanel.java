9d7b03d9be7d406bdbdc2d14accfec84adf3d7d1
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2018-06-18 17:38:56 +0300

    move owasp back to the markdown

--- python/rest/src/com/jetbrains/rest/editor/RestSwingHtmlPanel.java
+++ python/rest/src/com/jetbrains/rest/editor/RestSwingHtmlPanel.java
@@ -1,51 +1,13 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.rest.editor;
 
-import com.intellij.openapi.util.NotNullLazyValue;
 import com.intellij.ui.components.JBScrollPane;
 import org.jetbrains.annotations.NotNull;
-import org.owasp.html.HtmlPolicyBuilder;
-import org.owasp.html.PolicyFactory;
-import org.owasp.html.Sanitizers;
 
 import javax.swing.*;
 import javax.swing.text.html.HTMLEditorKit;
 
 public class RestSwingHtmlPanel implements RestPreviewPanel {
-
-  final static NotNullLazyValue<PolicyFactory> SANITIZER_VALUE = new NotNullLazyValue<PolicyFactory>() {
-    @NotNull
-    @Override
-    protected PolicyFactory compute() {
-      return Sanitizers.BLOCKS
-        .and(Sanitizers.FORMATTING)
-        .and(new HtmlPolicyBuilder()
-               .allowUrlProtocols("file", "http", "https").allowElements("img")
-               .allowAttributes("alt", "src", "title").onElements("img")
-               .allowAttributes("border", "height", "width").onElements("img")
-               .toFactory())
-        .and(new HtmlPolicyBuilder()
-               .allowUrlProtocols("file", "http", "https", "mailto").allowElements("a")
-               .allowAttributes("href", "title").onElements("a")
-               .toFactory())
-        .and(new HtmlPolicyBuilder()
-               .allowStandardUrlProtocols()
-               .allowElements("table", "tr", "td", "th", "caption", "thead", "tbody", "tfoot")
-               .allowAttributes("summary").onElements("table")
-               .allowAttributes("align", "valign")
-               .onElements("table", "tr", "td", "th", "thead", "tbody", "tfoot")
-               .allowTextIn("table")
-               .toFactory())
-        .and(new HtmlPolicyBuilder()
-               .allowStandardUrlProtocols()
-               .allowElements("pre", "span")
-               .toFactory())
-        .and(new HtmlPolicyBuilder()
-               .allowElements("code", "tr")
-               .allowAttributes("class").onElements("code", "tr")
-               .toFactory());
-    }
-  };
 
   private final JTextPane myPane;
   private final JScrollPane myScrollPane;
@@ -58,7 +20,6 @@ public class RestSwingHtmlPanel implements RestPreviewPanel {
 
   @Override
   public void setHtml(@NotNull String html) {
-    html = "<html>" + SANITIZER_VALUE.getValue().sanitize(html) + "</html>";
     myPane.setText(html);
   }
 
