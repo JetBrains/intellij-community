21cf39958c9957f8f14149d26bc88d67b2fbcdb9
Anton Lobov <Anton.Lobov@jetbrains.com>
2018-06-18 13:55:08 +0200

    WEB-33404 Add an API to suppress JSON widget for particular files

---
+++ json/src/com/jetbrains/jsonSchema/extension/JsonWidgetSuppressor.java
@@ -0,0 +1,17 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.jetbrains.jsonSchema.extension;
+
+import com.intellij.openapi.extensions.ExtensionPointName;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+
+public interface JsonWidgetSuppressor {
+  ExtensionPointName<JsonWidgetSuppressor> EXTENSION_POINT_NAME = ExtensionPointName.create("Json.Widget.Suppressor");
+
+  /**
+   * Allows to suppress JSON widget for particular files
+   * This method can access indexes and PSI
+   */
+  boolean suppressSwitcherWidget(@NotNull VirtualFile file, @NotNull Project project);
+}
