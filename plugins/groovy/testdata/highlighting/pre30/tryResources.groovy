94ddc1e9dfe42ab368874f38b7126fd580500a63
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2018-06-18 13:46:36 +0300

    [groovy] fix a typo in error message, add a resolve test (IDEA-188429)
    (IDEA-CR-33695)
    (cherry picked from commit d32d762dc243e79a3699e27e8ddf6b622559838a)

--- plugins/groovy/testdata/highlighting/pre30/tryResources.groovy
+++ plugins/groovy/testdata/highlighting/pre30/tryResources.groovy
@@ -1,3 +1,3 @@
-try <error descr="'try' with resources in not supported in current version">(</error>
+try <error descr="'try' with resources is not supported in current version">(</error>
   def a = 1
 ) {}
