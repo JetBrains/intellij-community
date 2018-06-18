00d78f102c190fa42715a460b18925f9ac38027d
nik <Nikolay.Chashnikov@jetbrains.com>
2018-06-18 09:54:59 +0300

    cleanup: use computeIfAbsent to simplify code

--- jps/jps-builders/src/org/jetbrains/jps/incremental/IncProjectBuilder.java
+++ jps/jps-builders/src/org/jetbrains/jps/incremental/IncProjectBuilder.java
@@ -1406,15 +1406,8 @@ public class IncProjectBuilder {
   }
 
   private void storeBuilderStatistics(Builder builder, long elapsedTime, int processedFiles) {
-    if (!myElapsedTimeNanosByBuilder.containsKey(builder)) {
-      myElapsedTimeNanosByBuilder.putIfAbsent(builder, new AtomicLong());
-    }
-    myElapsedTimeNanosByBuilder.get(builder).addAndGet(elapsedTime);
-
-    if (!myNumberOfSourcesProcessedByBuilder.containsKey(builder)) {
-      myNumberOfSourcesProcessedByBuilder.putIfAbsent(builder, new AtomicInteger());
-    }
-    myNumberOfSourcesProcessedByBuilder.get(builder).addAndGet(processedFiles);
+    myElapsedTimeNanosByBuilder.computeIfAbsent(builder, b -> new AtomicLong()).addAndGet(elapsedTime);
+    myNumberOfSourcesProcessedByBuilder.computeIfAbsent(builder, b -> new AtomicInteger()).addAndGet(processedFiles);
   }
 
   private static void saveInstrumentedClasses(ChunkBuildOutputConsumerImpl outputConsumer) throws IOException {
