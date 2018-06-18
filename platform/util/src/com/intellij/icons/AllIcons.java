5ab8320cb797311f71a45da14e6aa74471b9264d
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2018-06-18 12:54:09 +0300

    Add new icons for verified breakpoints
    (cherry picked from commit 71968ca527733e10000197bfbd2dc7d2bef3cda2) (IDEA-CR-33705)

--- platform/util/src/com/intellij/icons/AllIcons.java
+++ platform/util/src/com/intellij/icons/AllIcons.java
@@ -471,6 +471,9 @@ public class AllIcons {
     public static final Icon Db_verified_breakpoint = IconLoader.getIcon("/debugger/db_verified_breakpoint.svg"); // 12x12
     public static final Icon Db_verified_field_breakpoint = IconLoader.getIcon("/debugger/db_verified_field_breakpoint.svg"); // 12x12
     public static final Icon Db_verified_method_breakpoint = IconLoader.getIcon("/debugger/db_verified_method_breakpoint.svg"); // 12x12
+    public static final Icon Db_verified_no_suspend_breakpoint = IconLoader.getIcon("/debugger/db_verified_no_suspend_breakpoint.svg"); // 12x12
+    public static final Icon Db_verified_no_suspend_field_breakpoint = IconLoader.getIcon("/debugger/db_verified_no_suspend_field_breakpoint.svg"); // 12x12
+    public static final Icon Db_verified_no_suspend_method_breakpoint = IconLoader.getIcon("/debugger/db_verified_no_suspend_method_breakpoint.svg"); // 12x12
 
     /** @deprecated to be removed in IDEA 2020 - use AllIcons.Debugger.Db_exception_breakpoint */
     @SuppressWarnings("unused")
