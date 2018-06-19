cb6bc829cfa097df6dd497d6260b8579f22f9311
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2018-06-19 07:40:44 +0300

    [platform] restores API used by EA plugin

--- platform/platform-impl/src/com/intellij/execution/impl/EditorHyperlinkSupport.java
+++ platform/platform-impl/src/com/intellij/execution/impl/EditorHyperlinkSupport.java
@@ -1,6 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.impl;
 
 import com.intellij.execution.filters.Filter;
@@ -186,6 +184,17 @@ public class EditorHyperlinkSupport {
   @Nullable
   public HyperlinkInfo getHyperlinkInfoByLineAndCol(final int line, final int col) {
     return getHyperlinkAt(myEditor.logicalPositionToOffset(new LogicalPosition(line, col)));
+  }
+
+  /**
+   * @deprecated for binary compatibility with older plugins
+   * @see #createHyperlink(int, int, TextAttributes, HyperlinkInfo)
+   */
+  public void addHyperlink(final int highlightStartOffset,
+                           final int highlightEndOffset,
+                           @Nullable final TextAttributes highlightAttributes,
+                           @NotNull final HyperlinkInfo hyperlinkInfo) {
+    createHyperlink(highlightStartOffset, highlightEndOffset, highlightAttributes, hyperlinkInfo);
   }
 
   @NotNull
