48d78f41c391eb07a37d9115e5847f454b329cea
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2018-06-19 17:41:52 +0300

    [stream-debugger] Fix JBIterable chain detection

--- plugins/stream-debugger/src/com/intellij/debugger/streams/psi/impl/InheritanceBasedChainDetector.kt
+++ plugins/stream-debugger/src/com/intellij/debugger/streams/psi/impl/InheritanceBasedChainDetector.kt
@@ -2,7 +2,9 @@
 package com.intellij.debugger.streams.psi.impl
 
 import com.intellij.debugger.streams.psi.ChainDetector
+import com.intellij.lang.jvm.JvmModifier;
 import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiMethod
 import com.intellij.psi.PsiMethodCallExpression
 import com.intellij.psi.PsiType
 import com.intellij.psi.util.InheritanceUtil
@@ -17,10 +19,13 @@ class InheritanceBasedChainDetector(private val baseClassName: String) : ChainDe
   }
 
   override fun isIntermediateCall(callExpression: PsiMethodCallExpression): Boolean {
-    return isStreamType(callExpression.resolveMethod()?.returnType)
+    val method = callExpression.resolveMethod() ?: return false
+    return !method.isStatic() && isStreamType(method.parent as? PsiClass) && isStreamType(callExpression.resolveMethod()?.returnType)
   }
 
   private fun isStreamType(type: PsiType?): Boolean = InheritanceUtil.isInheritor(type, baseClassName)
 
   private fun isStreamType(type: PsiClass?): Boolean = InheritanceUtil.isInheritor(type, baseClassName)
+
+  private fun PsiMethod.isStatic(): Boolean = this.hasModifier(JvmModifier.STATIC)
 }
