4f048d886adb51dedbda25a9e4eb8e42558d237f
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2018-06-19 15:08:26 +0300

    Cleanup (test data formatting)

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/advLVTI/FailedInferenceWithLeftTypeVar.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/advLVTI/FailedInferenceWithLeftTypeVar.java
@@ -4,12 +4,10 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 class MyTest {
-
   private void getMethods(final Stream<Method> stream) {
     final var collector = Collectors.toMap(null, c -> {
       System.out.println(c);
       return new LinkedHashMap<String, Integer>();
     }, (v1, v2) -> v2, null);
-
   }
 }
