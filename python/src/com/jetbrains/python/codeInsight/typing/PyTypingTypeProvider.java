6c70d897b9d4526ec8d168d5efd938c1b80f07d1
Semyon Proshev <semyon.proshev@jetbrains.com>
2018-06-19 21:03:27 +0300

    Move `io.open`, `_io.open` and `pathlib.Path.open` call type processing (PY-27972, PY-28155, PY-15949, PY-22845)
    `io.open` is no more processed because it resolves to `_io.open` in Py2 and `builtins.open` in Py3
    
    (cherry picked from commit 49273cd0f28a0a06722cdf76669073f584a1b2a4)

--- python/src/com/jetbrains/python/codeInsight/typing/PyTypingTypeProvider.java
+++ python/src/com/jetbrains/python/codeInsight/typing/PyTypingTypeProvider.java
@@ -386,8 +386,16 @@ public class PyTypingTypeProvider extends PyTypeProviderBase {
         .orElse(null);
     }
 
-    if (callSite instanceof PyCallExpression && "open".equals(functionQName)) {
-      return getOpenFunctionCallType(function, (PyCallExpression)callSite, LanguageLevel.forElement(callSite), context);
+    if (callSite instanceof PyCallExpression) {
+      final LanguageLevel level = "open".equals(functionQName)
+                                  ? LanguageLevel.forElement(callSite)
+                                  : "pathlib.Path.open".equals(functionQName) || "_io.open".equals(functionQName)
+                                    ? LanguageLevel.PYTHON34
+                                    : null;
+
+      if (level != null) {
+        return getOpenFunctionCallType(function, (PyCallExpression)callSite, level, context);
+      }
     }
 
     if (callSite instanceof PyCallExpression && NEW_TYPE.equals(functionQName)) {
@@ -423,13 +431,9 @@ public class PyTypingTypeProvider extends PyTypeProviderBase {
     if (stub != null) {
       final PyTypingNewTypeStub customStub = stub.getCustomStub(PyTypingNewTypeStub.class);
       if (customStub != null) {
-        final String newTypeName = customStub.getName();
-        final String classTypeStr = customStub.getClassType();
-        if (newTypeName != null && classTypeStr != null) {
-          final PyType type = Ref.deref(getStringBasedType(classTypeStr, referenceTarget, context));
-          if (type instanceof PyClassType) {
-            return new PyTypingNewType((PyClassType)type, true, newTypeName);
-          }
+        final PyType type = Ref.deref(getStringBasedType(customStub.getClassType(), referenceTarget, context));
+        if (type instanceof PyClassType) {
+          return new PyTypingNewType((PyClassType)type, true, customStub.getName());
         }
       }
     }
@@ -1272,7 +1276,7 @@ public class PyTypingTypeProvider extends PyTypeProviderBase {
   @NotNull
   private static List<PsiElement> tryResolvingOnStubs(@NotNull PyReferenceExpression expression,
                                                       @NotNull TypeEvalContext context) {
-    
+
     final QualifiedName qualifiedName = expression.asQualifiedName();
     final PyFile pyFile = as(FileContextUtil.getContextFile(expression), PyFile.class);
 
@@ -1435,7 +1439,7 @@ public class PyTypingTypeProvider extends PyTypeProviderBase {
     public TypeEvalContext getTypeContext() {
       return myContext;
     }
-    
+
     @NotNull
     public Set<PsiElement> getExpressionCache() {
       return myCache;
