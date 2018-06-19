1e2c2950964e6f8453ee27b4ef24db83480497ef
Egor Ushakov <egor.ushakov@jetbrains.com>
2018-06-19 16:40:14 +0300

    Async stack traces support for BoundedTaskExecutor

--- platform/util/src/com/intellij/util/concurrency/BoundedTaskExecutor.java
+++ platform/util/src/com/intellij/util/concurrency/BoundedTaskExecutor.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.concurrency;
 
 import com.intellij.Patches;
@@ -21,6 +7,7 @@ import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.util.*;
 import com.intellij.util.containers.ContainerUtil;
+import org.jetbrains.annotations.Async;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 
@@ -139,7 +126,7 @@ public class BoundedTaskExecutor extends AbstractExecutorService {
   }
 
   @Override
-  public void execute(@NotNull Runnable task) {
+  public void execute(@NotNull @Async.Schedule Runnable task) {
     if (isShutdown() && !(task instanceof LastTask)) {
       throw new RejectedExecutionException("Already shutdown");
     }
@@ -209,17 +196,7 @@ public class BoundedTaskExecutor extends AbstractExecutorService {
               Runnable task = currentTask.get();
               do {
                 currentTask.set(task);
-                try {
-                  task.run();
-                }
-                catch (Throwable e) {
-                  // do not lose queued tasks because of this exception
-                  try {
-                    LOG.error(e);
-                  }
-                  catch (Throwable ignored) {
-                  }
-                }
+                doRun(task);
                 task = pollOrGiveUp(status);
               }
               while (task != null);
@@ -240,6 +217,21 @@ public class BoundedTaskExecutor extends AbstractExecutorService {
     catch (RuntimeException e) {
       myStatus.decrementAndGet();
       throw e;
+    }
+  }
+
+  // Extracted to have a capture point
+  private static void doRun(@Async.Execute Runnable task) {
+    try {
+      task.run();
+    }
+    catch (Throwable e) {
+      // do not lose queued tasks because of this exception
+      try {
+        LOG.error(e);
+      }
+      catch (Throwable ignored) {
+      }
     }
   }
 
