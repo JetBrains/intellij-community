e4e02bca43ac0836b94d295f6c68b70ef01c2578
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-06-19 12:41:55 +0200

    IDEA-192088 bundle rhino in Community

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/CommunityLibraryLicenses.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/CommunityLibraryLicenses.groovy
@@ -426,6 +426,11 @@ class CommunityLibraryLicenses {
     new LibraryLicense(name: "JetBrains Annotations for Java 5", libraryName: "jetbrains-annotations-java5", version: "16.0.1", license: "Apache 2.0",
                        url: "https://github.com/JetBrains/java-annotations"),
 
+    new LibraryLicense(name: "Rhino JavaScript Engine", libraryName: "rhino", version: "1.7.8", license: "MPL 1.1",
+                       url: "http://www.mozilla.org/rhino/", licenseUrl: "http://www.mozilla.org/MPL/MPL-1.1.html"),
+    new LibraryLicense(name: "delight-rhino-sandbox", libraryName: "delight-rhino-sandbox", version: "0.0.6", license: "Apache 2.0",
+                       url: "https://github.com/javadelight/delight-rhino-sandbox", licenseUrl: "https://github.com/javadelight/delight-rhino-sandbox/blob/master/LICENSE.TXT"),
+
     jetbrainsLibrary("intellij-coverage"),
     jetbrainsLibrary("intellij-test-discovery"),
     jetbrainsLibrary("CoverageReport"),
