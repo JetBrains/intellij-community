def35d9bf877d9d084d36ff1c04bc42f7b26f3d3
Ivan Buryak <Ivan.Buryak@jetbrains.com>
2018-06-19 16:33:46 +0700

    Get rid of ConnectionConfigurableForm
    GitOrigin-RevId: fe0c1a1a24676a7db20de1e1cfa7d3392a0cb55a

--- plugins/space/src/main/kotlin/circlet/settings/ConnectionConfigurable.kt
+++ plugins/space/src/main/kotlin/circlet/settings/ConnectionConfigurable.kt
@@ -3,14 +3,19 @@ package circlet.settings
 import circlet.messages.*
 import com.intellij.openapi.options.*
 import com.intellij.openapi.project.*
+import com.intellij.ui.components.*
+import com.intellij.uiDesigner.core.*
 import javax.swing.*
 
 class ConnectionConfigurable(private val project: Project) : SearchableConfigurable {
-    private val form = ConnectionConfigurableForm()
+    private val panel = JPanel(GridLayoutManager(3, 2))
+
+    private val serverUrlField = JBTextField()
+    private val projectKeyField = JBTextField()
 
     private val serverUrlWithProtocol: String
         get() {
-            val serverUrl = form.serverUrlField.text.trim()
+            val serverUrl = serverUrlField.text.trim()
 
             if (serverUrl.isNotEmpty() && !serverUrl.contains("://")) {
                 return "https://$serverUrl"
@@ -19,7 +24,64 @@ class ConnectionConfigurable(private val project: Project) : SearchableConfigura
             return serverUrl
         }
 
-    private val projectKeyTrimmed: String get() = form.projectKeyField.text.trim()
+    private val projectKeyTrimmed: String get() = projectKeyField.text.trim()
+
+    init {
+        panel.add(
+            JBLabel(CircletBundle.message("connection-configurable.server-url-label")).apply {
+                labelFor = serverUrlField
+            },
+            GridConstraints().apply {
+                row = 0
+                column = 0
+                vSizePolicy = GridConstraints.SIZEPOLICY_FIXED
+                hSizePolicy = GridConstraints.SIZEPOLICY_FIXED
+                anchor = GridConstraints.ANCHOR_WEST
+            }
+        )
+        panel.add(
+            serverUrlField,
+            GridConstraints().apply {
+                row = 0
+                column = 1
+                vSizePolicy = GridConstraints.SIZEPOLICY_FIXED
+                fill = GridConstraints.FILL_HORIZONTAL
+            }
+        )
+
+        panel.add(
+            JBLabel(CircletBundle.message("connection-configurable.project-key-label")).apply {
+                labelFor = projectKeyField
+            },
+            GridConstraints().apply {
+                row = 1
+                column = 0
+                vSizePolicy = GridConstraints.SIZEPOLICY_FIXED
+                hSizePolicy = GridConstraints.SIZEPOLICY_FIXED
+                anchor = GridConstraints.ANCHOR_WEST
+            }
+        )
+        panel.add(
+            projectKeyField,
+            GridConstraints().apply {
+                row = 1
+                column = 1
+                vSizePolicy = GridConstraints.SIZEPOLICY_FIXED
+                fill = GridConstraints.FILL_HORIZONTAL
+            }
+        )
+
+        panel.add(
+            Spacer(),
+            GridConstraints().apply {
+                row = 2
+                column = 1
+                vSizePolicy = GridConstraints.SIZEPOLICY_CAN_GROW or GridConstraints.SIZEPOLICY_WANT_GROW
+                hSizePolicy = GridConstraints.SIZEPOLICY_FIXED
+                fill = GridConstraints.FILL_VERTICAL
+            }
+        )
+    }
 
     override fun isModified(): Boolean {
         val settings = project.settings
@@ -37,20 +99,20 @@ class ConnectionConfigurable(private val project: Project) : SearchableConfigura
         val serverUrl = serverUrlWithProtocol
 
         settings.serverUrl.value = serverUrl
-        form.serverUrlField.text = serverUrl
+        serverUrlField.text = serverUrl
 
         val projectKey = projectKeyTrimmed
 
         settings.projectKey.value = projectKey
-        form.projectKeyField.text = projectKey
+        projectKeyField.text = projectKey
     }
 
-    override fun createComponent(): JComponent? = form.panel
+    override fun createComponent(): JComponent? = panel
 
     override fun reset() {
         val settings = project.settings
 
-        form.serverUrlField.text = settings.serverUrl.value
-        form.projectKeyField.text = settings.projectKey.value
+        serverUrlField.text = settings.serverUrl.value
+        projectKeyField.text = settings.projectKey.value
     }
 }
