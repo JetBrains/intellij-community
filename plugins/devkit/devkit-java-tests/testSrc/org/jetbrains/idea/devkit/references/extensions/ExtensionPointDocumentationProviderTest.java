2f697223cfc2570e2720ed7637f6e5e2e7108de5
Yann Cébron <yann.cebron@jetbrains.com>
2018-06-20 17:13:54 +0200

    IDEA-194306 Devkit: fix quick/full doc for extension point w/ "qualifiedName"

--- plugins/devkit/devkit-java-tests/testSrc/org/jetbrains/idea/devkit/references/extensions/ExtensionPointDocumentationProviderTest.java
+++ plugins/devkit/devkit-java-tests/testSrc/org/jetbrains/idea/devkit/references/extensions/ExtensionPointDocumentationProviderTest.java
@@ -41,4 +41,33 @@ public class ExtensionPointDocumentationProviderTest extends LightCodeInsightFix
                  "<div class='content'>\n   MyExtensionPoint JavaDoc.\n </div><table class='sections'><p></table></div>",
                  provider.generateDoc(docElement, null));
   }
+
+  public void testExtensionPointDocumentationQualifiedName() {
+    myFixture.configureByFiles("extensionPointDocumentationQualifiedName.xml", "bar/MyExtensionPoint.java");
+
+    final PsiElement docElement =
+      DocumentationManager.getInstance(getProject()).findTargetElement(myFixture.getEditor(),
+                                                                       myFixture.getFile());
+    DocumentationProvider provider = DocumentationManager.getProviderFromElement(docElement);
+
+    String epDefinition = "[" + myModule.getName() + "]<br/>" +
+                          "<b>com.my.bar</b> (extensionPointDocumentationQualifiedName.xml)<br/>" +
+                          "<a href=\"psi_element://bar.MyExtensionPoint\"><code>MyExtensionPoint</code></a>";
+
+    assertEquals(epDefinition,
+                 provider.getQuickNavigateInfo(docElement, null));
+
+    assertEquals("<div class='definition'><pre><b>com.my.bar</b><br>" +
+                 "<a href=\"psi_element://bar.MyExtensionPoint\"><code>MyExtensionPoint</code></a><br>" +
+                 "extensionPointDocumentationQualifiedName.xml" +
+                 "<table class='sections'>" +
+                 "<tr><td valign='top' class='section'><p>attributeName:</td><td valign='top'><a href=\"psi_element://java.lang.String\"><code>String</code></a></td>" +
+                 "<tr><td valign='top' class='section'><p>&lt;tagName&gt;:</td><td valign='top'><a href=\"psi_element://java.lang.Integer\"><code>Integer</code></a></td></table>" +
+                 "</pre></div>" +
+                 "<div class='content'>" +
+                 "<em>Extension Point Implementation Class</em>" +
+                 "<div class='definition'><pre>bar<br>public interface <b>MyExtensionPoint</b></pre></div>" +
+                 "<div class='content'>\n   MyExtensionPoint JavaDoc.\n </div><table class='sections'><p></table></div>",
+                 provider.generateDoc(docElement, null));
+  }
 }
