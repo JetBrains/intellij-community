6964b1ead7e1966d247502044a41bfc7d49f5663
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2018-06-20 19:49:31 +0300

    IDEA-194273 NullPointerException on finding RemoteDesktopService

--- platform/platform-api/src/com/intellij/ide/RemoteDesktopService.java
+++ platform/platform-api/src/com/intellij/ide/RemoteDesktopService.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ServiceManager;
 
 public abstract class RemoteDesktopService {
@@ -9,7 +10,7 @@ public abstract class RemoteDesktopService {
   }
 
   public static boolean isRemoteSession() {
-    return getInstance().isRemoteDesktopConnected();
+    return ApplicationManager.getApplication() != null && getInstance().isRemoteDesktopConnected();
   }
 
   public abstract boolean isRemoteDesktopConnected();
