22902433da26d9c299942ccddde648a27e41c403
Ivan Buryak <Ivan.Buryak@jetbrains.com>
2018-06-21 12:35:14 +0700

    Register deserializers
    GitOrigin-RevId: 01fc36ab379775514cf1bdc890d838253ef6dabe

--- plugins/space/src/main/kotlin/circlet/components/CircletApplicationComponent.kt
+++ plugins/space/src/main/kotlin/circlet/components/CircletApplicationComponent.kt
@@ -1,6 +1,8 @@
 package circlet.components
 
+import circlet.client.api.impl.*
 import circlet.klogging.impl.*
+import circlet.platform.client.serialization.*
 import circlet.runtime.*
 import com.intellij.openapi.components.*
 import klogging.impl.*
@@ -11,5 +13,7 @@ class CircletApplicationComponent : ApplicationComponent {
         KLoggerStaticFactory.customFactory = ErrorToWarningKLoggers
 
         mutableUiDispatch = ApplicationUiDispatch
+
+        ApiClassesDeserializer(ExtendableSerializationRegistry.global).registerDeserializers()
     }
 }
