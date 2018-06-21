c17c25c8c78382d47c9f98144dd390130c0b6142
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2018-06-21 11:19:51 +0300

    do not cast superclass to subclass

--- platform/platform-impl/src/com/intellij/ui/ExpandableActions.java
+++ platform/platform-impl/src/com/intellij/ui/ExpandableActions.java
@@ -1,24 +1,10 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ui;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.DumbAwareAction;
 
-import javax.swing.*;
+import javax.swing.JComponent;
 import java.util.function.Consumer;
 
 import static com.intellij.openapi.actionSystem.PlatformDataKeys.CONTEXT_COMPONENT;
@@ -43,13 +29,6 @@ public abstract class ExpandableActions extends DumbAwareAction {
   }
 
   @Override
-  public void update(AnActionEvent event) {
-    boolean expected = this instanceof Collapse;
-    Expandable expandable = getExpandable(event);
-    event.getPresentation().setEnabled(expandable != null && expandable.isExpanded() == expected);
-  }
-
-  @Override
   public void actionPerformed(AnActionEvent event) {
     Expandable expandable = getExpandable(event);
     if (expandable != null) consumer.accept(expandable);
@@ -59,11 +38,23 @@ public abstract class ExpandableActions extends DumbAwareAction {
     public Expand() {
       super(Expandable::expand);
     }
+
+    @Override
+    public void update(AnActionEvent event) {
+      Expandable expandable = getExpandable(event);
+      event.getPresentation().setEnabled(expandable != null && !expandable.isExpanded());
+    }
   }
 
   public static final class Collapse extends ExpandableActions {
     public Collapse() {
       super(Expandable::collapse);
+    }
+
+    @Override
+    public void update(AnActionEvent event) {
+      Expandable expandable = getExpandable(event);
+      event.getPresentation().setEnabled(expandable != null && expandable.isExpanded());
     }
   }
 }
