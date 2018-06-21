3c226e97bc159d3642f482ed47c94408d6a4a582
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2018-06-21 18:15:27 +0300

    IDEA-178635: Refactor ExpandableSupport to show editors in popup
    (cherry picked from commit c3fce0b7efa9ca5c61a4b31307ae615437aa355e)

--- platform/platform-api/src/com/intellij/ui/components/fields/ExpandableTextField.java
+++ platform/platform-api/src/com/intellij/ui/components/fields/ExpandableTextField.java
@@ -3,14 +3,24 @@ package com.intellij.ui.components.fields;
 
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.ui.Expandable;
+import com.intellij.ui.components.JBScrollBar;
+import com.intellij.ui.components.JBScrollPane;
 import com.intellij.util.Function;
 import com.intellij.util.execution.ParametersListUtil;
+import com.intellij.util.ui.JBInsets;
+import com.intellij.util.ui.JBUI;
+import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.*;
+import javax.swing.text.JTextComponent;
+import java.awt.*;
 import java.util.List;
 
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
+import static javax.swing.BorderFactory.createEmptyBorder;
+import static javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS;
 
 /**
  * @author Sergey Malenkov
@@ -32,9 +42,72 @@ public class ExpandableTextField extends ExtendableTextField implements Expandab
    * @see ParametersListUtil
    */
   public ExpandableTextField(@NotNull Function<String, List<String>> parser, @NotNull Function<List<String>, String> joiner) {
-    Function<String, String> onShow = text -> StringUtil.join(parser.fun(text), "\n");
-    Function<String, String> onHide = text -> joiner.fun(asList(StringUtil.splitByLines(text)));
-    support = new ExpandableSupport.TextComponent<>(this, onShow, onHide);
+    Function<? super String, String> onShow = text -> StringUtil.join(parser.fun(text), "\n");
+    Function<? super String, String> onHide = text -> joiner.fun(asList(StringUtil.splitByLines(text)));
+    support = new ExpandableSupport<JTextComponent>(this, onShow, onHide) {
+      @Override
+      protected Content prepare(@NotNull JTextComponent field, @NotNull Function<? super String, String> onShow) {
+        Font font = field.getFont();
+        FontMetrics metrics = font == null ? null : field.getFontMetrics(font);
+        int height = metrics == null ? 16 : metrics.getHeight();
+        Dimension size = new Dimension(height * 32, height * 16);
+
+        JTextArea area = new JTextArea(onShow.fun(field.getText()));
+        area.putClientProperty(Expandable.class, this);
+        area.setEditable(field.isEditable());
+        area.setBackground(field.getBackground());
+        area.setForeground(field.getForeground());
+        area.setFont(font);
+        area.setWrapStyleWord(true);
+        area.setLineWrap(true);
+        copyCaretPosition(field, area);
+        UIUtil.addUndoRedoActions(area);
+
+        JLabel label = createLabel(createCollapseExtension());
+        label.setBorder(JBUI.Borders.empty(5, 0, 5, 5));
+
+        JBScrollPane pane = new JBScrollPane(area);
+        pane.setVerticalScrollBarPolicy(VERTICAL_SCROLLBAR_ALWAYS);
+        pane.getVerticalScrollBar().add(JBScrollBar.LEADING, label);
+        pane.getVerticalScrollBar().setBackground(area.getBackground());
+
+        Insets insets = field.getInsets();
+        Insets margin = field.getMargin();
+        if (margin != null) {
+          insets.top += margin.top;
+          insets.left += margin.left;
+          insets.right += margin.right;
+          insets.bottom += margin.bottom;
+        }
+
+        JBInsets.addTo(size, insets);
+        JBInsets.addTo(size, pane.getInsets());
+        pane.setPreferredSize(size);
+        pane.setViewportBorder(insets != null
+                               ? createEmptyBorder(insets.top, insets.left, insets.bottom, insets.right)
+                               : createEmptyBorder());
+        return new Content() {
+          @NotNull
+          @Override
+          public JComponent getContentComponent() {
+            return pane;
+          }
+
+          @Override
+          public JComponent getFocusableComponent() {
+            return area;
+          }
+
+          @Override
+          public void cancel(@NotNull Function<? super String, String> onHide) {
+            if (field.isEditable()) {
+              field.setText(onHide.fun(area.getText()));
+              copyCaretPosition(area, field);
+            }
+          }
+        };
+      }
+    };
     putClientProperty("monospaced", true);
     setExtensions(createExtensions());
   }
@@ -70,5 +143,13 @@ public class ExpandableTextField extends ExtendableTextField implements Expandab
   @Override
   public void expand() {
     support.expand();
+  }
+
+  private static void copyCaretPosition(JTextComponent source, JTextComponent destination) {
+    try {
+      destination.setCaretPosition(source.getCaretPosition());
+    }
+    catch (Exception ignored) {
+    }
   }
 }
