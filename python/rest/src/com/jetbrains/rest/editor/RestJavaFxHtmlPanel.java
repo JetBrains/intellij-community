2bf9205ad64d23f9142756d70e49e1fd4f8bc736
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2018-06-21 11:22:59 +0300

    PY-30470 reST preview: provide dark theme
    (cherry picked from commit dcf0ec19da4d1048a1d9802f9764679244980a91)

--- python/rest/src/com/jetbrains/rest/editor/RestJavaFxHtmlPanel.java
+++ python/rest/src/com/jetbrains/rest/editor/RestJavaFxHtmlPanel.java
@@ -1,7 +1,42 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.rest.editor;
 
+import com.intellij.ide.ui.LafManager;
+import com.intellij.ide.ui.LafManagerListener;
+import com.intellij.ide.ui.laf.darcula.DarculaLookAndFeelInfo;
 import com.intellij.ui.javafx.JavaFxHtmlPanel;
+import javafx.scene.text.FontSmoothingType;
+import javafx.scene.web.WebView;
+
+import java.net.URL;
 
 public class RestJavaFxHtmlPanel extends JavaFxHtmlPanel implements RestPreviewPanel {
+  public RestJavaFxHtmlPanel() {
+    super();
+    LafManager.getInstance().addLafManagerListener(new RestLafManagerListener());
+
+    runInPlatformWhenAvailable(() -> {
+      final WebView webView = getWebViewGuaranteed();
+      webView.fontSmoothingTypeProperty().setValue(FontSmoothingType.LCD);
+      updateLaf(LafManager.getInstance().getCurrentLookAndFeel() instanceof DarculaLookAndFeelInfo);
+    });
+  }
+
+  private class RestLafManagerListener implements LafManagerListener {
+    @Override
+    public void lookAndFeelChanged(LafManager manager) {
+      updateLaf(manager.getCurrentLookAndFeel() instanceof DarculaLookAndFeelInfo);
+    }
+  }
+
+  private URL getStyle(boolean isDarcula) {
+    return getClass().getResource(isDarcula ? "/styles/darcula.css" : "/styles/default.css");
+  }
+
+  private void updateLaf(boolean isDarcula) {
+    runInPlatformWhenAvailable(() -> {
+      final WebView webView = getWebViewGuaranteed();
+      webView.getEngine().setUserStyleSheetLocation(getStyle(isDarcula).toExternalForm());
+    });
+  }
 }
