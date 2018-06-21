b848c45c91b856dea0ee9790c7409b38a82119a6
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2018-06-21 21:50:37 +0300

    Remote Desktop: Optimization for OS type, instance caching

--- platform/platform-api/src/com/intellij/ide/RemoteDesktopService.java
+++ platform/platform-api/src/com/intellij/ide/RemoteDesktopService.java
@@ -3,13 +3,21 @@ package com.intellij.ide;
 
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.util.SystemInfo;
 
 public abstract class RemoteDesktopService {
+  private static volatile RemoteDesktopService ourInstance;
+
   public static RemoteDesktopService getInstance() {
-    return ServiceManager.getService(RemoteDesktopService.class);
+    RemoteDesktopService service = ourInstance;
+    if (service == null) {
+      ourInstance = service = ServiceManager.getService(RemoteDesktopService.class);
+    }
+    return service;
   }
 
   public static boolean isRemoteSession() {
+    if (!SystemInfo.isWindows) return false;
     return ApplicationManager.getApplication() != null && getInstance().isRemoteDesktopConnected();
   }
 
