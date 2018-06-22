e2f71e4e41892c38d35bef8a8e145ab4e4692e87
Viktoria.Shirunova <viktoria.shirunova@jetbrains.com>
2018-06-22 16:30:36 +0300

    [gui-test] IDEA-CR-34101 add function waitUntilIconsShown - waits until all specified kinds of gutter icons are shown and in required quantities
    (cherry picked from commit d825b0e0a31501f181a7371f9f1618875ea3b34d)

--- platform/testGuiFramework/src/com/intellij/testGuiFramework/fixtures/GutterFixture.kt
+++ platform/testGuiFramework/src/com/intellij/testGuiFramework/fixtures/GutterFixture.kt
@@ -5,6 +5,8 @@ import com.intellij.icons.AllIcons
 import com.intellij.openapi.editor.impl.DocumentMarkupModel
 import com.intellij.openapi.editor.markup.MarkupModel
 import com.intellij.testGuiFramework.impl.GuiTestUtilKt.runOnEdt
+import com.intellij.testGuiFramework.impl.GuiTestUtilKt.waitUntil
+import org.fest.swing.exception.WaitTimedOutError
 
 /**
  * @author Artem.Gainanov
@@ -23,34 +25,55 @@ class GutterFixture(private val myIde: IdeFrameFixture) {
   }
 
   val gutterIcons: Map<GutterIcon, List<Int>>
-  /**
-   * Returns the map of gutter icon and the list of lines with this icon
-   * For example {BREAKPOINT=[], DISABLED_BREAKPOINT=[], CSS_COLOR=[], RUN_SCRIPT=[],
-   * IMPLEMENTATION=[14, 16, 15], IMPLEMENTED=[2, 1, 4, 3], OVERRIDES=[17],
-   * OVERRIDDEN=[7, 8]}
-   */
-  get() {
-    val result = GutterIcon.values().map { Pair(it, mutableListOf<Int>()) }.toMap()
-    runOnEdt {
-      for (highlighter in getMarkupModel().allHighlighters) {
-        val lineNumber = myIde.editor.editor.document.getLineNumber(highlighter.startOffset) + 1
-        if (highlighter.gutterIconRenderer?.icon != null) {
-          if (highlighter.gutterIconRenderer?.javaClass?.name == "com.intellij.psi.css.browse.CssColorGutterRenderer") {
-            result[GutterIcon.CSS_COLOR]!!.add(lineNumber)
-          }
-          when (highlighter.gutterIconRenderer?.icon) {
-            AllIcons.Gutter.ImplementingMethod -> result[GutterIcon.IMPLEMENTATION]!!.add(lineNumber)
-            AllIcons.Gutter.OverridenMethod -> result[GutterIcon.OVERRIDDEN]!!.add(lineNumber)
-            AllIcons.Gutter.ImplementedMethod -> result[GutterIcon.IMPLEMENTED]!!.add(lineNumber)
-            AllIcons.Gutter.OverridingMethod -> result[GutterIcon.OVERRIDES]!!.add(lineNumber)
-            AllIcons.RunConfigurations.TestState.Run -> result[GutterIcon.RUN_SCRIPT]!!.add(lineNumber)
-            AllIcons.Debugger.Db_set_breakpoint -> result[GutterIcon.BREAKPOINT]!!.add(lineNumber)
-            AllIcons.Debugger.Db_disabled_breakpoint -> result[GutterIcon.DISABLED_BREAKPOINT]!!.add(lineNumber)
+    /**
+     * Returns the map of gutter icon and the list of lines with this icon
+     * For example {BREAKPOINT=[], DISABLED_BREAKPOINT=[], CSS_COLOR=[], RUN_SCRIPT=[],
+     * IMPLEMENTATION=[14, 16, 15], IMPLEMENTED=[2, 1, 4, 3], OVERRIDES=[17],
+     * OVERRIDDEN=[7, 8]}
+     */
+    get() {
+      val result = GutterIcon.values().map { Pair(it, mutableListOf<Int>()) }.toMap()
+      runOnEdt {
+        for (highlighter in getMarkupModel().allHighlighters) {
+          val lineNumber = myIde.editor.editor.document.getLineNumber(highlighter.startOffset) + 1
+          if (highlighter.gutterIconRenderer?.icon != null) {
+            if (highlighter.gutterIconRenderer?.javaClass?.name == "com.intellij.psi.css.browse.CssColorGutterRenderer") {
+              result[GutterIcon.CSS_COLOR]!!.add(lineNumber)
+            }
+            when (highlighter.gutterIconRenderer?.icon) {
+              AllIcons.Gutter.ImplementingMethod -> result[GutterIcon.IMPLEMENTATION]!!.add(lineNumber)
+              AllIcons.Gutter.OverridenMethod -> result[GutterIcon.OVERRIDDEN]!!.add(lineNumber)
+              AllIcons.Gutter.ImplementedMethod -> result[GutterIcon.IMPLEMENTED]!!.add(lineNumber)
+              AllIcons.Gutter.OverridingMethod -> result[GutterIcon.OVERRIDES]!!.add(lineNumber)
+              AllIcons.RunConfigurations.TestState.Run -> result[GutterIcon.RUN_SCRIPT]!!.add(lineNumber)
+              AllIcons.Debugger.Db_set_breakpoint -> result[GutterIcon.BREAKPOINT]!!.add(lineNumber)
+              AllIcons.Debugger.Db_disabled_breakpoint -> result[GutterIcon.DISABLED_BREAKPOINT]!!.add(lineNumber)
+            }
           }
         }
       }
+      return result.mapValues { it.value.toList() }
     }
-    return result.mapValues { it.value.toList() }
+
+  /**
+   * Waits until all specified kinds of gutter icons are shown and in required quantities
+   * @param expectedIcons map where key is expected icon kind and value number of expected icons of this kind
+   * @throws AssertionError if time is out and expected icons either not found at all or not found in expected quantity
+   * */
+  fun waitUntilIconsShown(expectedIcons: Map<GutterIcon, Int>) {
+    return try {
+      waitUntil("Wait for ${expectedIcons.map { "${it.key}(${it.value})" }} on gutter panel are shown") {
+        gutterIcons.filter {
+          it.value.isNotEmpty() &&
+          expectedIcons.containsKey(it.key) &&
+          expectedIcons[it.key] == it.value.size
+        }.size == expectedIcons.size
+      }
+    }
+    catch (e: WaitTimedOutError) {
+      // time is out and expected icons either not found at all or not found in expected quantity
+      throw  AssertionError("Gutter icons ${expectedIcons.map { "${it.key}(${it.value})" }} not found. Actual state is ${gutterIcons.filterValues { it.isNotEmpty() }}")
+    }
   }
 
   /**
