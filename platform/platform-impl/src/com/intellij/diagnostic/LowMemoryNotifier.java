64ba19e9f353feaedd5e511f078f1d5eeb7e1fef
Shaverdova Elena <Elena.Shaverdova@jetbrains.com>
2018-06-22 20:20:26 +0200

    Move listener to component, see comments in IDEA-CR-33698
    FUS-87 IDE and Project Lifecycle

--- platform/platform-impl/src/com/intellij/diagnostic/LowMemoryNotifier.java
+++ platform/platform-impl/src/com/intellij/diagnostic/LowMemoryNotifier.java
@@ -1,16 +1,21 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.diagnostic;
 
+import com.intellij.featureStatistics.fusCollectors.AppLifecycleUsageTriggerCollector;
 import com.intellij.ide.IdeBundle;
+import com.intellij.internal.statistic.eventLog.FeatureUsageLogger;
+import com.intellij.internal.statistic.service.fus.collectors.FUSApplicationUsageTrigger;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationAction;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.openapi.util.LowMemoryWatcher;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collections;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static com.intellij.openapi.util.LowMemoryWatcher.LowMemoryWatcherType.ONLY_AFTER_GC;
@@ -18,6 +23,19 @@ import static com.intellij.openapi.util.LowMemoryWatcher.LowMemoryWatcherType.ON
 public class LowMemoryNotifier implements ApplicationComponent {
   private final LowMemoryWatcher myWatcher = LowMemoryWatcher.register(this::onLowMemorySignalReceived, ONLY_AFTER_GC);
   private final AtomicBoolean myNotificationShown = new AtomicBoolean();
+
+  @Override
+  public void initComponent() {
+    IdePerformanceListener.Adapter handler = new IdePerformanceListener.Adapter() {
+      @Override
+      public void uiFreezeFinished(int lengthInSeconds) {
+        FUSApplicationUsageTrigger.getInstance().trigger(AppLifecycleUsageTriggerCollector.class, "ide.freeze");
+        FeatureUsageLogger.INSTANCE.log("lifecycle",
+                                        "ide.freeze", Collections.singletonMap("durationSeconds", lengthInSeconds));
+      }
+    };
+    ApplicationManager.getApplication().getMessageBus().connect().subscribe(IdePerformanceListener.TOPIC, handler);
+  }
 
   private void onLowMemorySignalReceived() {
     if (myNotificationShown.compareAndSet(false, true)) {
