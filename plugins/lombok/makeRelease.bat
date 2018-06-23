4f602285ce3b4e5361cd65048ec0298629b1c283
Michail Plushnikov <mplushnikov@gmail.com>
2018-06-23 21:59:18 +0200

    updated versions, compatibility for 2018.2 EAP #481
    GitOrigin-RevId: 79d17b437296bbe49b88de9722dfdc4061bafaa1

--- plugins/lombok/makeRelease.bat
+++ plugins/lombok/makeRelease.bat
@@ -2,7 +2,7 @@
 
 SET pluginVersion=0.18
 
-for %%X in (2017.2 2017.2.6 2017.3 2017.3.1 2017.3.2 2017.3.3 2017.3.4) do call :buildPlugin %%X
+for %%X in (2017.2 2017.3 2018.1 LATEST-EAP-SNAPSHOT) do call :buildPlugin %%X
 
 :buildPlugin
 SETLOCAL
@@ -10,6 +10,5 @@ echo Called with %1
 SET IDEA_VERSION=%1
 call gradlew clean
 call gradlew buildPlugin check
-call gradlew publishPlugin
 copy build\distributions\lombok-plugin-%pluginVersion%.zip distro\lombok-plugin-%pluginVersion%-%1.zip
 ENDLOCAL & SET result=%retval%
