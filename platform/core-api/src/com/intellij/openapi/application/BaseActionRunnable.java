5b18cff9ecc35783148641e5231d603b5549753e
Alexey Kudravtsev <cdr@intellij.com>
2018-06-24 14:24:58 +0300

    restored deprecated method for Tara plugin compatibility

--- platform/core-api/src/com/intellij/openapi/application/BaseActionRunnable.java
+++ platform/core-api/src/com/intellij/openapi/application/BaseActionRunnable.java
@@ -41,11 +41,9 @@ public abstract class BaseActionRunnable<T> {
     return execute();
   }
 
-  //<editor-fold desc="Deprecated stuff.">
   /** @deprecated use {@link ApplicationManager#getApplication()} (to be removed in IDEA 2018) */
   @Deprecated
   protected Application getApplication() {
     return ApplicationManager.getApplication();
   }
-  //</editor-fold>
 }
