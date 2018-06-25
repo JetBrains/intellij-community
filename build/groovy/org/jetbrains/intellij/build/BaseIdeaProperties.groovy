300037705ea05de6b92bd9970abefe34db74262f
peter <peter@jetbrains.com>
2018-06-25 16:59:50 +0200

    make jetCheck an external library jar

--- build/groovy/org/jetbrains/intellij/build/BaseIdeaProperties.groovy
+++ build/groovy/org/jetbrains/intellij/build/BaseIdeaProperties.groovy
@@ -87,7 +87,6 @@ abstract class BaseIdeaProperties extends ProductProperties {
         withModule("intellij.java.uast", JAVA_IMPL_JAR, JAVA_RESOURCES_JAR)
 
         withModule("intellij.java.rt", "idea_rt.jar", null)
-        withModule("intellij.tools.jetCheck", "jetCheck.jar")
         withArtifact("debugger-agent", "rt")
         withProjectLibrary("Eclipse")
         withProjectLibrary("jgoodies-common")
