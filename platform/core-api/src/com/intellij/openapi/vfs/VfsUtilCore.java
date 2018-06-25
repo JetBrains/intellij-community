ad8eb4ac0441cb8756e6bea29442918bdbcf7208
neuro159 <neuro159@gmail.com>
2018-06-25 22:01:35 +0200

    Performance - VFS child visiting determinism (sorted)

--- platform/core-api/src/com/intellij/openapi/vfs/VfsUtilCore.java
+++ platform/core-api/src/com/intellij/openapi/vfs/VfsUtilCore.java
@@ -1,6 +1,4 @@
-/*
- * Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vfs;
 
 import com.intellij.openapi.diagnostic.Logger;
@@ -308,6 +306,7 @@ public class VfsUtilCore {
           childrenIterable = visitor.getChildrenIterable(file);
           if (childrenIterable == null) {
             children = file.getChildren();
+            Arrays.sort(children, Comparator.comparing(VirtualFile::getPresentableUrl));
           }
         }
       }
