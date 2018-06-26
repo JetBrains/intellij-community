28dd42e1192a39846dadf61f06bd733772102a8c
Anna Kozlova <anna.kozlova@jetbrains.com>
2018-06-26 13:23:29 +0300

    testdata for IDEA-120800

--- java/java-tests/testSrc/com/intellij/java/refactoring/IntroduceParameterTest.java
+++ java/java-tests/testSrc/com/intellij/java/refactoring/IntroduceParameterTest.java
@@ -340,6 +340,10 @@ public class IntroduceParameterTest extends LightRefactoringTestCase  {
     doTest(IntroduceParameterRefactoring.REPLACE_FIELDS_WITH_GETTERS_ALL, true, false, true, false);
   }
 
+  public void testWrapVarargsParameter() {
+    doTest(IntroduceParameterRefactoring.REPLACE_FIELDS_WITH_GETTERS_ALL, true, false, true, false);
+  }
+
   private void doTestThroughHandler() {
     configureByFile("/refactoring/introduceParameter/before" + getTestName(false) + ".java");
     boolean enabled = true;
