78a8978dd6503d699acbb7d0065d346209a9c74c
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2018-06-26 16:52:20 +0300

    [platform] avoids loading file content to show an icon (IDEA-193936)

--- platform/core-api/src/com/intellij/ide/presentation/VirtualFilePresentation.java
+++ platform/core-api/src/com/intellij/ide/presentation/VirtualFilePresentation.java
@@ -1,23 +1,10 @@
-/*
- * Copyright 2000-2013 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.presentation;
 
 import com.intellij.ide.TypePresentationService;
 import com.intellij.openapi.fileTypes.DirectoryFileType;
 import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.fileTypes.FileTypeRegistry;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.IconUtil;
 import com.intellij.util.PlatformIcons;
@@ -29,7 +16,6 @@ import javax.swing.*;
  * @author yole
  */
 public class VirtualFilePresentation {
-
   public static Icon getIcon(@NotNull VirtualFile vFile) {
     return IconUtil.getIcon(vFile, 0, null);
   }
@@ -39,7 +25,7 @@ public class VirtualFilePresentation {
     if (icon != null) {
       return icon;
     }
-    FileType fileType = vFile.getFileType();
+    FileType fileType = FileTypeRegistry.getInstance().getFileTypeByFileName(vFile.getNameSequence());
     if (vFile.isDirectory() && vFile.isInLocalFileSystem() && !(fileType instanceof DirectoryFileType)) {
       return PlatformIcons.FOLDER_ICON;
     }
