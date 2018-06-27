5fd7b6f993becf854d5977fb9968515fbed21033
pavel.nikitin <pavel.nikitin@jetbrains.com>
2018-06-27 19:49:43 +0300

    add Novelty interface

---
+++ platform/on-air-index/src/intellij/platform/onair/storage/api/Novelty.java
@@ -0,0 +1,9 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package intellij.platform.onair.storage.api;
+
+public interface Novelty {
+
+  long alloc(byte[] bytes);
+
+  void update(long id, byte[] bytes);
+}
