a20fc2a997324dff79b386059ffe08a5397e7844
pavel.nikitin <pavel.nikitin@jetbrains.com>
2018-06-27 19:34:23 +0300

    move stuff around

--- platform/on-air-index/src/intellij/platform/onair/storage/api/Storage.java
+++ platform/on-air-index/src/intellij/platform/onair/storage/api/Storage.java
@@ -1,5 +1,5 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package intellij.platform.onair.tree;
+package intellij.platform.onair.storage.api;
 
 public interface Storage {
   byte[] lookup(long hash);
--- platform/on-air-index/testSrc/intellij/platform/onair/tree/AirTreeTest.java
+++ platform/on-air-index/testSrc/intellij/platform/onair/tree/AirTreeTest.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package intellij.platform.onair.tree;
 
+import intellij.platform.onair.storage.api.Storage;
 import org.junit.Assert;
 import org.junit.Test;
 
