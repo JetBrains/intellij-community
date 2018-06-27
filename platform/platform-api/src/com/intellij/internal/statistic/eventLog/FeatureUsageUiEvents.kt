7d1929102eff455b073078d5ae16593d3c72a5f7
Svetlana.Zemlyanskaya <svetlana.zemlyanskaya@jetbrains.com>
2018-06-27 14:51:14 +0200

    FB-125 Filter settings and dialogs from third-party plugins
    rw=IDEA-CR-33847
    (cherry picked from commit 81d4c054e0d416e42a357597127f40f5af2c0a07)

--- platform/platform-api/src/com/intellij/internal/statistic/eventLog/FeatureUsageUiEvents.kt
+++ platform/platform-api/src/com/intellij/internal/statistic/eventLog/FeatureUsageUiEvents.kt
@@ -1,63 +1,37 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.internal.statistic.eventLog
 
-import com.intellij.openapi.ui.DialogWrapper
-import com.intellij.util.containers.ContainerUtil
+import com.intellij.openapi.components.ServiceManager
 
-object FeatureUsageUiEvents {
-  private const val DIALOGS_ID = "ui.dialogs"
-  private const val SETTINGS_ID = "ui.settings"
+fun getUiEventLogger(): FeatureUsageUiEvents {
+  return ServiceManager.getService(FeatureUsageUiEvents::class.java) ?: EmptyFeatureUsageUiEvents
+}
 
-  private val SELECT_CONFIGURABLE_DATA = HashMap<String, Any>()
-  private val APPLY_CONFIGURABLE_DATA = HashMap<String, Any>()
-  private val RESET_CONFIGURABLE_DATA = HashMap<String, Any>()
+interface FeatureUsageUiEvents {
+  fun logSelectConfigurable(name: String, context: Class<*>)
 
-  private val SHOW_DIALOG_DATA = ContainerUtil.newHashMap<String, Any>()
-  private val CLOSE_OK_DIALOG_DATA = ContainerUtil.newHashMap<String, Any>()
-  private val CLOSE_CANCEL_DIALOG_DATA = ContainerUtil.newHashMap<String, Any>()
-  private val CLOSE_CUSTOM_DIALOG_DATA = ContainerUtil.newHashMap<String, Any>()
+  fun logApplyConfigurable(name: String, context: Class<*>)
 
-  init {
-    SELECT_CONFIGURABLE_DATA["type"] = "select"
-    APPLY_CONFIGURABLE_DATA["type"] = "apply"
-    RESET_CONFIGURABLE_DATA["type"] = "reset"
+  fun logResetConfigurable(name: String, context: Class<*>)
 
-    SHOW_DIALOG_DATA["type"] = "show"
-    CLOSE_OK_DIALOG_DATA["type"] = "close"
-    CLOSE_OK_DIALOG_DATA["code"] = DialogWrapper.OK_EXIT_CODE
-    CLOSE_CANCEL_DIALOG_DATA["type"] = "close"
-    CLOSE_CANCEL_DIALOG_DATA["code"] = DialogWrapper.CANCEL_EXIT_CODE
-    CLOSE_CUSTOM_DIALOG_DATA["type"] = "close"
-    CLOSE_CUSTOM_DIALOG_DATA["code"] = DialogWrapper.NEXT_USER_EXIT_CODE
+  fun logShowDialog(name: String, context: Class<*>)
+
+  fun logCloseDialog(name: String, exitCode: Int, context: Class<*>)
+}
+
+object EmptyFeatureUsageUiEvents : FeatureUsageUiEvents {
+  override fun logSelectConfigurable(name: String, context: Class<*>) {
   }
 
-  fun logSelectConfigurable(name: String) {
-    FeatureUsageLogger.log(SETTINGS_ID, name, SELECT_CONFIGURABLE_DATA)
+  override fun logApplyConfigurable(name: String, context: Class<*>) {
   }
 
-  fun logApplyConfigurable(name: String) {
-    FeatureUsageLogger.log(SETTINGS_ID, name, APPLY_CONFIGURABLE_DATA)
+  override fun logResetConfigurable(name: String, context: Class<*>) {
   }
 
-  fun logResetConfigurable(name: String) {
-    FeatureUsageLogger.log(SETTINGS_ID, name, RESET_CONFIGURABLE_DATA)
+  override fun logShowDialog(name: String, context: Class<*>) {
   }
 
-  fun logShowDialog(name: String) {
-    FeatureUsageLogger.log(DIALOGS_ID, name, SHOW_DIALOG_DATA)
-  }
-
-  fun logCloseDialog(name: String, exitCode: Int) {
-    if (FeatureUsageLogger.isEnabled()) {
-      if (exitCode == DialogWrapper.OK_EXIT_CODE) {
-        FeatureUsageLogger.log(DIALOGS_ID, name, CLOSE_OK_DIALOG_DATA)
-      }
-      else if (exitCode == DialogWrapper.CANCEL_EXIT_CODE) {
-        FeatureUsageLogger.log(DIALOGS_ID, name, CLOSE_CANCEL_DIALOG_DATA)
-      }
-      else {
-        FeatureUsageLogger.log(DIALOGS_ID, name, CLOSE_CUSTOM_DIALOG_DATA)
-      }
-    }
+  override fun logCloseDialog(name: String, exitCode: Int, context: Class<*>) {
   }
 }
