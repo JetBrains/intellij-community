4788908d8e2a0ff91954fbc025a77243f66c9cf4
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2018-06-27 14:53:17 +0300

    Create plugin module

---
+++ plugins/hack18-clean-history/src/chm/ChmCheckinHandlerFactory.kt
@@ -0,0 +1,48 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package chm
+
+import com.intellij.openapi.vcs.CheckinProjectPanel
+import com.intellij.openapi.vcs.changes.CommitExecutor
+import com.intellij.openapi.vcs.checkin.CheckinHandler
+import com.intellij.openapi.vcs.checkin.VcsCheckinHandlerFactory
+import com.intellij.openapi.vcs.ui.RefreshableOnComponent
+import com.intellij.ui.NonFocusableCheckBox
+import com.intellij.util.PairConsumer
+import com.intellij.util.ui.JBUI
+import git4idea.GitVcs
+import java.awt.event.KeyEvent
+import javax.swing.JComponent
+
+class ChmCheckinHandlerFactory : VcsCheckinHandlerFactory(GitVcs.getKey()) {
+  override fun createVcsHandler(panel: CheckinProjectPanel): CheckinHandler {
+    return MyCheckinHandler(panel)
+  }
+}
+
+class MyCheckinHandler(panel: CheckinProjectPanel) : CheckinHandler() {
+  private val vcs = GitVcs.getInstance(panel.project)
+  private val configurationPanel = MyConfigurationPanel()
+
+  override fun beforeCheckin(executor: CommitExecutor?, additionalDataConsumer: PairConsumer<Any, Any>): ReturnResult {
+    return ReturnResult.COMMIT
+  }
+
+  override fun getBeforeCheckinConfigurationPanel() = configurationPanel
+
+  class MyConfigurationPanel : RefreshableOnComponent {
+    private val cleanupCommit = NonFocusableCheckBox("Cleanup Commit")
+
+    override fun getComponent(): JComponent {
+      cleanupCommit.mnemonic = KeyEvent.VK_C
+      cleanupCommit.isSelected = true
+      return JBUI.Panels.simplePanel(cleanupCommit)
+    }
+
+    override fun refresh() {}
+
+    override fun saveState() {}
+
+    override fun restoreState() {}
+
+  }
+}
