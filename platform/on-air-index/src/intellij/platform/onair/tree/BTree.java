950644058d70b1fdd3b000d1f99a052b2bc45608
pavel.nikitin <pavel.nikitin@jetbrains.com>
2018-06-27 19:03:10 +0300

    move stuff around

--- platform/on-air-index/src/intellij/platform/onair/tree/BTree.java
+++ platform/on-air-index/src/intellij/platform/onair/tree/BTree.java
@@ -1,5 +1,5 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package jetbrains.onair.tree;
+package intellij.platform.onair.tree;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
