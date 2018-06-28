d0ad282d01673021393b6d820012a4222b84e233
Anna Kozlova <anna.kozlova@jetbrains.com>
2018-06-28 14:44:23 +0300

    enable add initializer on variable declaration (IDEA-194742)

--- java/java-analysis-impl/src/com/intellij/codeInsight/daemon/impl/analysis/PostHighlightingVisitor.java
+++ java/java-analysis-impl/src/com/intellij/codeInsight/daemon/impl/analysis/PostHighlightingVisitor.java
@@ -7,7 +7,6 @@ import com.intellij.codeInsight.daemon.JavaErrorMessages;
 import com.intellij.codeInsight.daemon.UnusedImportProvider;
 import com.intellij.codeInsight.daemon.impl.*;
 import com.intellij.codeInsight.daemon.impl.quickfix.QuickFixAction;
-import com.intellij.codeInsight.intention.EmptyIntentionAction;
 import com.intellij.codeInsight.intention.IntentionAction;
 import com.intellij.codeInsight.intention.QuickFixFactory;
 import com.intellij.codeInspection.InspectionProfile;
@@ -271,7 +270,7 @@ class PostHighlightingVisitor {
       if (!referenced && !UnusedSymbolUtil.isImplicitWrite(myProject, variable, progress)) {
         String message = JavaErrorMessages.message("local.variable.is.not.assigned", identifier.getText());
         final HighlightInfo unusedSymbolInfo = UnusedSymbolUtil.createUnusedSymbolInfo(identifier, message, myDeadCodeInfoType);
-        QuickFixAction.registerQuickFixAction(unusedSymbolInfo, new EmptyIntentionAction(UnusedSymbolLocalInspectionBase.DISPLAY_NAME), myDeadCodeKey);
+        QuickFixAction.registerQuickFixAction(unusedSymbolInfo, QuickFixFactory.getInstance().createAddVariableInitializerFix(variable), myDeadCodeKey);
         return unusedSymbolInfo;
       }
     }
