2ed2ddd2747ccf86c9ef7d4715a122ba021f78d6
Dmitry Kozhevnikov <Dmitry.Kozhevnikov@jetbrains.com>
2018-06-28 20:51:56 +0300

    [editor conflicts] Compare: pick one branch

--- plugins/git4idea/src/git4idea/editorConflicts/intentions/CompareChangesAction.kt
+++ plugins/git4idea/src/git4idea/editorConflicts/intentions/CompareChangesAction.kt
@@ -10,29 +10,122 @@ import com.intellij.diff.tools.simple.SimpleDiffTool
 import com.intellij.diff.util.DiffUserDataKeysEx
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.colors.EditorColors
+import com.intellij.openapi.editor.colors.EditorColorsManager
+import com.intellij.openapi.editor.markup.HighlighterLayer
+import com.intellij.openapi.editor.markup.HighlighterTargetArea
+import com.intellij.openapi.editor.markup.RangeHighlighter
+import com.intellij.openapi.editor.markup.TextAttributes
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vcs.getNextMarker
-import com.intellij.openapi.vcs.getSectionInnerRange
-import com.intellij.openapi.vcs.substring
+import com.intellij.openapi.ui.popup.JBPopupAdapter
+import com.intellij.openapi.ui.popup.JBPopupFactory
+import com.intellij.openapi.ui.popup.LightweightWindowEvent
+import com.intellij.openapi.util.TextRange
+import com.intellij.openapi.vcs.*
 import com.intellij.psi.PsiElement
+import com.intellij.psi.SmartPointerManager
+import com.intellij.psi.SmartPsiElementPointer
+import javax.swing.ListSelectionModel
 
 class CompareChangesAction(element: PsiElement) : ConflictsIntention(element, "Compare with...") {
   override fun doInvoke(project: Project, editor: Editor, d: Document, marker: PsiElement) {
-    val otherMarker = getNextMarker(marker) ?: return
-    val oneMoreMarker = getNextMarker(otherMarker) ?: return
+    getOtherMarkerAndRun(marker, editor) { thisMarker, otherMarker ->
+      val nextForThis = getNextMarker(thisMarker) ?: return@getOtherMarkerAndRun
+      val nextForOther = getNextMarker(otherMarker) ?: return@getOtherMarkerAndRun
 
-    val leftText = d.substring(getSectionInnerRange(marker, otherMarker, d))
-    val rightText = d.substring(getSectionInnerRange(otherMarker, oneMoreMarker, d))
+      val leftText = d.substring(getSectionInnerRange(thisMarker, nextForThis, d))
+      val rightText = d.substring(getSectionInnerRange(otherMarker, nextForOther, d))
 
-    val fileType = marker.containingFile.fileType
-    val content1 = DiffContentFactory.getInstance().createEditable(project, leftText, fileType)
-    val content2 = DiffContentFactory.getInstance().createEditable(project, rightText, fileType)
+      val fileType = thisMarker.containingFile.fileType
+      val content1 = DiffContentFactory.getInstance().createEditable(project, leftText, fileType)
+      val content2 = DiffContentFactory.getInstance().createEditable(project, rightText, fileType)
 
-    val request = SimpleDiffRequest("Diff", content1, content2, "Left", "Right")
+      val request = SimpleDiffRequest("Diff", content1, content2, "Left", "Right")
 
-    val chain = SimpleDiffRequestChain(request)
-    chain.putUserData(DiffUserDataKeysEx.FORCE_DIFF_TOOL, SimpleDiffTool.INSTANCE)
+      val chain = SimpleDiffRequestChain(request)
+      chain.putUserData(DiffUserDataKeysEx.FORCE_DIFF_TOOL, SimpleDiffTool.INSTANCE)
 
-    DiffManager.getInstance().showDiff(project, chain, DiffDialogHints.DEFAULT)
+      DiffManager.getInstance().showDiff(project, chain, DiffDialogHints.DEFAULT)
+    }
+  }
+
+  private fun getOtherMarkerAndRun(marker: PsiElement, editor: Editor, continuation: (PsiElement, PsiElement) -> Unit) {
+    val group = getMarkerGroup(marker)
+    val type = getConflictMarkerType(marker) ?: return
+
+    val otherPtrs = EditorConflictSupport.ConflictMarkerType.values()
+      .filter { it != type && it != EditorConflictSupport.ConflictMarkerType.AfterLast }
+      .mapNotNull { group.getMarker(it) }
+      .map { SmartPointerManager.createPointer(it) }
+    val thisPtr = SmartPointerManager.createPointer(marker)
+
+    showPopup(editor, otherPtrs) { selectedElement ->
+      val thisElement = thisPtr.element ?: return@showPopup
+      continuation(thisElement, selectedElement)
+    }
+  }
+
+
+  private fun showPopup(editor: Editor,
+                        otherPtrs: List<SmartPsiElementPointer<PsiElement>>,
+                        continuation: (PsiElement) -> Unit) {
+
+    val highlighter = Highlighter(editor)
+
+    data class ModelItem(val elementPtr: SmartPsiElementPointer<PsiElement>) {
+      override fun toString() = elementPtr.element?.text ?: "obsolete"
+    }
+
+    JBPopupFactory.getInstance()
+      .createPopupChooserBuilder(otherPtrs.map(::ModelItem))
+      .setTitle("Pick other section")
+      .setMovable(false)
+      .setSelectionMode(ListSelectionModel.SINGLE_SELECTION)
+      .setResizable(false)
+      .setRequestFocus(true)
+      .setItemChosenCallback {
+        if (it != null)
+          continuation(it.elementPtr.element ?: return@setItemChosenCallback)
+      }
+      .setItemSelectedCallback {
+        if (it != null && !editor.isDisposed) {
+          val marker = it.elementPtr.element ?: return@setItemSelectedCallback
+          val nextMarker = getNextMarker(marker) ?: return@setItemSelectedCallback
+          highlighter.highlight(getSectionInnerRange(marker, nextMarker, editor.document))
+        }
+      }
+      .addListener(object : JBPopupAdapter() {
+        override fun onClosed(event: LightweightWindowEvent) {
+          highlighter.dropHighlight()
+        }
+      })
+      .createPopup()
+      .showInBestPositionFor(editor)
+
+  }
+}
+
+private class Highlighter(private val myEditor: Editor) {
+  private var myActiveHighlighter: RangeHighlighter? = null
+
+  fun highlight(range: TextRange) {
+    dropHighlight()
+    addHighlighter(range)
+  }
+
+  private fun addHighlighter(r: TextRange) {
+    myActiveHighlighter = myEditor.markupModel.addRangeHighlighter(r.startOffset, r.endOffset, HighlighterLayer.ADDITIONAL_SYNTAX,
+                                                                   getAttributes(),
+                                                                   HighlighterTargetArea.EXACT_RANGE)
+  }
+
+  private fun getAttributes(): TextAttributes? {
+    val manager = EditorColorsManager.getInstance()
+    return manager.globalScheme.getAttributes(EditorColors.DELETED_TEXT_ATTRIBUTES)
+  }
+
+  fun dropHighlight() {
+    myActiveHighlighter?.dispose()
+    myActiveHighlighter = null
   }
 }
