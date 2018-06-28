3f441244bb1054afd7f5145ec9ee753977ac8868
Andrew Yefanov <1134togo@gmail.com>
2018-06-28 23:57:25 +0300

    IDEA-147781 ColorChooser: Opacity changing doesn't affect the results

--- java/java-impl/src/com/intellij/codeInsight/daemon/impl/JavaColorProvider.java
+++ java/java-impl/src/com/intellij/codeInsight/daemon/impl/JavaColorProvider.java
@@ -207,8 +207,28 @@ public class JavaColorProvider implements ElementColorProvider {
       command = () -> {
         switch (type) {
           case INT:
+            if (color.getAlpha() == 255) {
+              replaceInt(expr[0], color.getRGB(), true);
+            }
+            else {
+              PsiElementFactory factory = JavaPsiFacade.getElementFactory(argumentList.getProject());
+              argumentList.add(factory.createExpressionFromText("true", null));
+              replaceInt(expr[0], color.getRGB() | color.getAlpha() << 24, true, true);
+            }
           case INT_BOOL:
-            replaceInt(expr[0], color.getRGB(), true);
+            if ("true".equals(expr[1].getText())) {
+              replaceInt(expr[0], color.getRGB() | color.getAlpha() << 24, true, true);
+            }
+            else {
+              if (color.getAlpha() == 255) {
+                replaceInt(expr[0], color.getRGB(), true);
+              }
+              else {
+                PsiElementFactory factory = JavaPsiFacade.getElementFactory(argumentList.getProject());
+                expr[1].replace(factory.createExpressionFromText("true", null));
+                replaceInt(expr[0], color.getRGB() | color.getAlpha() << 24, true, true);
+              }
+            }
             return;
           case INT_x3:
           case INT_x4:
@@ -219,7 +239,9 @@ public class JavaColorProvider implements ElementColorProvider {
               replaceInt(expr[3], color.getAlpha());
             }
             else if (color.getAlpha() != 255) {
-              //todo add alpha
+              PsiElementFactory factory = JavaPsiFacade.getElementFactory(argumentList.getProject());
+              String text = String.valueOf(color.getAlpha());
+              argumentList.add(factory.createExpressionFromText(text, null));
             }
             return;
           case FLOAT_x3:
@@ -232,7 +254,9 @@ public class JavaColorProvider implements ElementColorProvider {
               replaceFloat(expr[3], rgba.length == 4 ? rgba[3] : 0f);
             }
             else if (color.getAlpha() != 255) {
-              //todo add alpha
+              PsiElementFactory factory = JavaPsiFacade.getElementFactory(argumentList.getProject());
+              String text = String.valueOf(color.getAlpha());
+              argumentList.add(factory.createExpressionFromText(text + "f", null));
             }
         }
       };
@@ -257,7 +281,7 @@ public class JavaColorProvider implements ElementColorProvider {
         text = "0x";
         Color c = new Color(newValue, hasAlpha);
         if (hasAlpha) {
-          text += Integer.toHexString(c.getAlpha());
+          text += Integer.toHexString(c.getAlpha()).toUpperCase();
         }
         text += ColorUtil.toHex(c).toUpperCase();
       }
