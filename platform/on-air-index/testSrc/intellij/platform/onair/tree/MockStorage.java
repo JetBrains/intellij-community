fe961e3c9a75d8715d6263f848977c92555419f5
pavel.nikitin <pavel.nikitin@jetbrains.com>
2018-06-28 15:59:46 +0300

    use better mock hash

--- platform/on-air-index/testSrc/intellij/platform/onair/tree/MockStorage.java
+++ platform/on-air-index/testSrc/intellij/platform/onair/tree/MockStorage.java
@@ -1,15 +1,16 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package intellij.platform.onair.tree;
 
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
 import intellij.platform.onair.storage.api.Address;
 import intellij.platform.onair.storage.api.Storage;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class MockStorage implements Storage {
-  private static final long MOCK_HIGH_BYTES = 0xCAFEBABE;
+  private static final HashFunction HASH = Hashing.goodFastHash(128);
 
   public final ConcurrentHashMap<Address, byte[]> data = new ConcurrentHashMap<>();
 
@@ -22,7 +23,10 @@ public class MockStorage implements Storage {
   }
 
   public Address store(byte[] bytes) {
-    final Address result = new Address(MOCK_HIGH_BYTES, getHash(bytes));
+    byte[] hashCode = HASH.hashBytes(bytes).asBytes();
+    long lowBytes = ByteUtils.readUnsignedLong(hashCode, 0, 8);
+    long highBytes = ByteUtils.readUnsignedLong(hashCode, 0, 8);
+    final Address result = new Address(highBytes, normalizeLowBytes(lowBytes));
     final byte[] existing = data.putIfAbsent(result, bytes);
     if (existing != null) {
       if (!Arrays.equals(bytes, existing)) {
@@ -32,17 +36,13 @@ public class MockStorage implements Storage {
     return result;
   }
 
-  private static long getHash(@NotNull byte[] what) {
-    int result = 1;
-    for (final byte element : what) {
-      result = 31 * result + (element & 0xff);
+  private static long normalizeLowBytes(long address) {
+    if (address < 0) {
+      return address;
     }
-    if (result == 0) {
+    if (address == 0) {
       return Long.MIN_VALUE;
     }
-    if (result > 0) {
-      return -result;
-    }
-    return result;
+    return -address;
   }
 }
