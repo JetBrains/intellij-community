6b52f536388790ac3b00ac5f529e0a4fbe3e84aa
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2018-06-29 17:48:14 +0300

    [groovy] convert ResolveLocalTest to JUnit 4

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/resolve/ResolveLocalTest.groovy
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/resolve/ResolveLocalTest.groovy
@@ -1,40 +1,55 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.lang.resolve
 
-import com.intellij.testFramework.LightProjectDescriptor
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture
 import groovy.transform.CompileStatic
 import org.jetbrains.plugins.groovy.GroovyProjectDescriptors
-import org.jetbrains.plugins.groovy.LightGroovyTestCase
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrVariable
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.params.GrParameter
+import org.jetbrains.plugins.groovy.util.EdtRule
+import org.jetbrains.plugins.groovy.util.FixtureRule
 import org.jetbrains.plugins.groovy.util.ResolveTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.RuleChain
+import org.junit.rules.TestRule
 
 @CompileStatic
-class ResolveLocalTest extends LightGroovyTestCase implements ResolveTest {
+class ResolveLocalTest implements ResolveTest {
 
-  final LightProjectDescriptor projectDescriptor = GroovyProjectDescriptors.GROOVY_3_0
+  public final FixtureRule myFixtureRule = new FixtureRule(GroovyProjectDescriptors.GROOVY_3_0, '')
+  public final @Rule TestRule myRules = RuleChain.outerRule(myFixtureRule).around(new EdtRule())
 
-  void 'test resource variable from try block'() {
+  @Override
+  CodeInsightTestFixture getFixture() { myFixtureRule.fixture }
+
+  @Test
+  void 'resource variable from try block'() {
     resolveTest 'try (def a) { println <caret>a }', GrVariable
   }
 
-  void 'test resource variable from catch block'() {
+  @Test
+  void 'resource variable from catch block'() {
     resolveTest 'try (def a) {} catch(e) { println <caret>a }', null
   }
 
-  void 'test resource variable from finally block'() {
+  @Test
+  void 'resource variable from finally block'() {
     resolveTest 'try (def a) {} finally { println <caret>a }', null
   }
 
-  void 'test resource variable from another resource'() {
+  @Test
+  void 'resource variable from another resource'() {
     resolveTest 'try (def a; def b = <caret>a) {}', GrVariable
   }
 
-  void 'test forward resource variable'() {
+  @Test
+  void 'forward resource variable'() {
     resolveTest 'try (def b = <caret>a; def a) {}', null
   }
 
-  void 'test parameter from resource initializer'() {
+  @Test
+  void 'parameter from resource initializer'() {
     resolveTest 'def foo(param) { try (def a = <caret>param) {} }', GrParameter
   }
 }
