f2bc913c9ff96b20c6e9960c5e56055634b34a18
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2018-06-29 16:06:43 +0700

    MagicConstantInspection: fix handling of Calendar.get() (Review ID: IDEA-CR-33807)

--- java/java-tests/testData/inspection/magic/SpecialCases.java
+++ java/java-tests/testData/inspection/magic/SpecialCases.java
@@ -13,18 +13,41 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import org.intellij.lang.annotations.MagicConstant;
+
 import java.util.Calendar;
 
-class SpecialCases {
+import static java.util.Calendar.*;
+
+public class SpecialCases {
+  @MagicConstant(intValues = {JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER})
+  int getMonth(Calendar c) {
+    return <warning descr="Should be one of: Calendar.JANUARY, Calendar.FEBRUARY, Calendar.MARCH, Calendar.APRIL, Calendar.MAY, ...">c.get(DAY_OF_WEEK)</warning>;
+  }
+
+  @MagicConstant(intValues = {JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER})
+  int getMonthCorrect(Calendar c) {
+    return c.get(MONTH);
+  }
+
+  void acceptMonth(@MagicConstant(intValues = {JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER}) int month) {}
+
+  void testAccept(Calendar c) {
+    acceptMonth(<warning descr="Should be one of: Calendar.JANUARY, Calendar.FEBRUARY, Calendar.MARCH, Calendar.APRIL, Calendar.MAY, ...">c.getFirstDayOfWeek()</warning>);
+    acceptMonth(JANUARY);
+    acceptMonth(<warning descr="Should be one of: Calendar.JANUARY, Calendar.FEBRUARY, Calendar.MARCH, Calendar.APRIL, Calendar.MAY, ...">c.get(DAY_OF_WEEK)</warning>);
+    acceptMonth(c.get(MONTH));
+  }
+
   void test() {
     Calendar instance = Calendar.getInstance();
-    if(instance.get(Calendar.DAY_OF_WEEK) == Calendar.MONDAY) {}
-    if(instance.get(Calendar.DAY_OF_WEEK) == <warning descr="Should be one of: Calendar.SUNDAY, Calendar.MONDAY, Calendar.TUESDAY, Calendar.WEDNESDAY, Calendar.THURSDAY, ...">Calendar.FEBRUARY</warning>) {}
-    if(instance.get(Calendar.MONTH) == <warning descr="Should be one of: Calendar.JANUARY, Calendar.FEBRUARY, Calendar.MARCH, Calendar.APRIL, Calendar.MAY, ...">Calendar.MONDAY</warning>) {}
-    if(instance.get(Calendar.MONTH) == Calendar.FEBRUARY) {}
-    if(instance.get(Calendar.MONTH) == <warning descr="Should be one of: Calendar.JANUARY, Calendar.FEBRUARY, Calendar.MARCH, Calendar.APRIL, Calendar.MAY, ...">Calendar.AM</warning>) {}
-    if(instance.get(Calendar.AM_PM) == Calendar.AM) {}
-    if(instance.get(Calendar.AM_PM) == <warning descr="Should be one of: Calendar.AM, Calendar.PM">Calendar.MONDAY</warning>) {}
+    if(instance.get(Calendar.DAY_OF_WEEK) == MONDAY) {}
+    if(instance.get(Calendar.DAY_OF_WEEK) == <warning descr="Should be one of: Calendar.SUNDAY, Calendar.MONDAY, Calendar.TUESDAY, Calendar.WEDNESDAY, Calendar.THURSDAY, ...">FEBRUARY</warning>) {}
+    if(instance.get(Calendar.MONTH) == <warning descr="Should be one of: Calendar.JANUARY, Calendar.FEBRUARY, Calendar.MARCH, Calendar.APRIL, Calendar.MAY, ...">MONDAY</warning>) {}
+    if(instance.get(Calendar.MONTH) == FEBRUARY) {}
+    if(instance.get(Calendar.MONTH) == <warning descr="Should be one of: Calendar.JANUARY, Calendar.FEBRUARY, Calendar.MARCH, Calendar.APRIL, Calendar.MAY, ...">AM</warning>) {}
+    if(instance.get(Calendar.AM_PM) == AM) {}
+    if(instance.get(Calendar.AM_PM) == <warning descr="Should be one of: Calendar.AM, Calendar.PM">MONDAY</warning>) {}
     if(instance.get(Calendar.DATE) == 1) {}
   }
 }
