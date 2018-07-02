37fdaf65053e078d6f9d719d109e5e16f3029acf
Aleksey Pivovarov <AMPivovarov@gmail.com>
2018-07-02 16:27:25 +0300

    merge: convert tests to Kotlin

--- platform/vcs-tests/testSrc/com/intellij/openapi/diff/impl/incrementalMerge/MergeBuilderTest.kt
+++ platform/vcs-tests/testSrc/com/intellij/openapi/diff/impl/incrementalMerge/MergeBuilderTest.kt
@@ -13,147 +13,136 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.intellij.openapi.diff.impl.incrementalMerge;
+package com.intellij.openapi.diff.impl.incrementalMerge
 
-import com.intellij.idea.IdeaLogger;
-import com.intellij.openapi.diff.impl.highlighting.FragmentSide;
-import com.intellij.openapi.diff.impl.util.ContextLogger;
-import com.intellij.openapi.util.TextRange;
-import com.intellij.util.Assertion;
-import junit.framework.TestCase;
+import com.intellij.idea.IdeaLogger
+import com.intellij.openapi.diff.impl.highlighting.FragmentSide
+import com.intellij.openapi.diff.impl.util.ContextLogger
+import com.intellij.openapi.util.TextRange
+import com.intellij.util.Assertion
+import junit.framework.TestCase
 
-import java.util.List;
+class MergeBuilderTest : TestCase() {
+  private val myMergeBuilder = MergeBuilder(ContextLogger("TEST"))
+  private val CHECK = Assertion()
 
-public class MergeBuilderTest extends TestCase {
-  private final MergeBuilder myMergeBuilder = new MergeBuilder(new ContextLogger("TEST"));
-  private final Assertion CHECK = new Assertion();
-
-  public void testEqual() {
-    addLeft(new TextRange(0, 1), new TextRange(0, 1));
-    addRight(new TextRange(0, 1), new TextRange(0, 1));
-    CHECK.empty(finish(1, 1, 1));
+  fun testEqual() {
+    addLeft(TextRange(0, 1), TextRange(0, 1))
+    addRight(TextRange(0, 1), TextRange(0, 1))
+    CHECK.empty(finish(1, 1, 1))
   }
 
-  public void testWholeConflict() {
+  fun testWholeConflict() {
     CHECK.singleElement(finish(1, 2, 3),
-                        fragment(new TextRange(0, 1), new TextRange(0, 2), new TextRange(0, 3)));
+                        fragment(TextRange(0, 1), TextRange(0, 2), TextRange(0, 3)))
   }
 
-  public void testTailInsert() {
-    TextRange range = new TextRange(0, 1);
-    addLeft(range, range);
-    addRight(range, range);
+  fun testTailInsert() {
+    val range = TextRange(0, 1)
+    addLeft(range, range)
+    addRight(range, range)
     CHECK.singleElement(finish(1, 1, 2),
-                        fragment(new TextRange(1, 1), new TextRange(1, 1), new TextRange(1, 2)));
+                        fragment(TextRange(1, 1), TextRange(1, 1), TextRange(1, 2)))
   }
 
-  public void testSameInsertsConflicts1() {
-    TextRange base = new TextRange(0, 1);
-    TextRange version = new TextRange(1, 2);
-    addLeft(base, version);
-    addRight(base, version);
+  fun testSameInsertsConflicts1() {
+    val base = TextRange(0, 1)
+    val version = TextRange(1, 2)
+    addLeft(base, version)
+    addRight(base, version)
     CHECK.singleElement(finish(2, 1, 2),
-                        fragment(new TextRange(0, 1), new TextRange(0, 0), new TextRange(0, 1)));
+                        fragment(TextRange(0, 1), TextRange(0, 0), TextRange(0, 1)))
   }
 
-  public void testSameInsertsConflicts2() {
-    TextRange base = new TextRange(1, 2);
-    TextRange version = new TextRange(0, 1);
-    addLeft(base, version);
-    addRight(base, version);
-    CHECK.compareAll(new MergeFragment[]{
-      fragment(new TextRange(0, 0), new TextRange(0, 1), new TextRange(0, 0)),
-      fragment(new TextRange(1, 2), new TextRange(2, 2), new TextRange(1, 2))
-    }, finish(2, 2, 2));
+  fun testSameInsertsConflicts2() {
+    val base = TextRange(1, 2)
+    val version = TextRange(0, 1)
+    addLeft(base, version)
+    addRight(base, version)
+    CHECK.compareAll(
+      arrayOf(fragment(TextRange(0, 0), TextRange(0, 1), TextRange(0, 0)), fragment(TextRange(1, 2), TextRange(2, 2), TextRange(1, 2))),
+      finish(2, 2, 2))
   }
 
-  public void testHeadInsert() {
-    TextRange range = new TextRange(0, 1);
-    addRight(range, new TextRange(1, 2));
-    addLeft(range, range);
+  fun testHeadInsert() {
+    val range = TextRange(0, 1)
+    addRight(range, TextRange(1, 2))
+    addLeft(range, range)
     CHECK.singleElement(finish(1, 1, 2),
-                        fragment(new TextRange(0, 0), new TextRange(0, 0), new TextRange(0, 1)));
+                        fragment(TextRange(0, 0), TextRange(0, 0), TextRange(0, 1)))
   }
 
-  public void testOneSideChange() {
-    addRight(new TextRange(0, 2), new TextRange(0, 2));
-    addLeft(new TextRange(1, 2), new TextRange(2, 3));
+  fun testOneSideChange() {
+    addRight(TextRange(0, 2), TextRange(0, 2))
+    addLeft(TextRange(1, 2), TextRange(2, 3))
     CHECK.singleElement(finish(3, 2, 2),
-                        fragment(new TextRange(0, 2), new TextRange(0, 1), new TextRange(0, 1)));
+                        fragment(TextRange(0, 2), TextRange(0, 1), TextRange(0, 1)))
   }
 
-  public void testNotAllignedConflict() {
-    addLeft(new TextRange(1, 3), new TextRange(0, 2));
-    addRight(new TextRange(2, 4), new TextRange(1, 3));
-    CHECK.compareAll(new MergeFragment[]{
-      fragment(new TextRange(0, 1), new TextRange(0, 2), new TextRange(0, 1)),
-      fragment(new TextRange(2, 3), new TextRange(3, 4), new TextRange(2, 3))
-    }, finish(3, 4, 3));
+  fun testNotAllignedConflict() {
+    addLeft(TextRange(1, 3), TextRange(0, 2))
+    addRight(TextRange(2, 4), TextRange(1, 3))
+    CHECK.compareAll(
+      arrayOf(fragment(TextRange(0, 1), TextRange(0, 2), TextRange(0, 1)), fragment(TextRange(2, 3), TextRange(3, 4), TextRange(2, 3))),
+      finish(3, 4, 3))
   }
 
-  public void testBug() {
-    addRight(new TextRange(0, 1), new TextRange(0, 1));
-    addLeft(new TextRange(0, 2), new TextRange(0, 2));
-    CHECK.compareAll(new MergeFragment[]{
-      fragment(new TextRange(1, 3), new TextRange(1, 2), new TextRange(1, 1)),
-    }, finish(3, 2, 1));
+  fun testBug() {
+    addRight(TextRange(0, 1), TextRange(0, 1))
+    addLeft(TextRange(0, 2), TextRange(0, 2))
+    CHECK.compareAll(arrayOf(fragment(TextRange(1, 3), TextRange(1, 2), TextRange(1, 1))), finish(3, 2, 1))
   }
 
-  public void testMultiChanges() {
-    addLeft(new TextRange(1, 8), new TextRange(1, 8));
-    addRight(new TextRange(1, 2), new TextRange(0, 1));
-    addRight(new TextRange(3, 4), new TextRange(1, 2));
-    addRight(new TextRange(4, 5), new TextRange(3, 4));
-    addRight(new TextRange(6, 7), new TextRange(5, 6));
-    addLeft(new TextRange(9, 10), new TextRange(9, 10));
-    CHECK.compareAll(new MergeFragment[]{
-      fragment(new TextRange(0, 1), new TextRange(0, 1), new TextRange(0, 0)),
-      fragment(new TextRange(2, 3), new TextRange(2, 3), new TextRange(1, 1)),
-      fragment(new TextRange(4, 4), new TextRange(4, 4), new TextRange(2, 3)),
-      fragment(new TextRange(5, 6), new TextRange(5, 6), new TextRange(4, 5)),
-      fragment(new TextRange(7, 10), new TextRange(7, 10), new TextRange(6, 7))
-    }, finish(10, 10, 7));
+  fun testMultiChanges() {
+    addLeft(TextRange(1, 8), TextRange(1, 8))
+    addRight(TextRange(1, 2), TextRange(0, 1))
+    addRight(TextRange(3, 4), TextRange(1, 2))
+    addRight(TextRange(4, 5), TextRange(3, 4))
+    addRight(TextRange(6, 7), TextRange(5, 6))
+    addLeft(TextRange(9, 10), TextRange(9, 10))
+    CHECK.compareAll(
+      arrayOf(fragment(TextRange(0, 1), TextRange(0, 1), TextRange(0, 0)), fragment(TextRange(2, 3), TextRange(2, 3), TextRange(1, 1)),
+              fragment(TextRange(4, 4), TextRange(4, 4), TextRange(2, 3)), fragment(TextRange(5, 6), TextRange(5, 6), TextRange(4, 5)),
+              fragment(TextRange(7, 10), TextRange(7, 10), TextRange(6, 7))), finish(10, 10, 7))
   }
 
-  public void testNoIntersection() {
-    addLeft(new TextRange(0, 1), new TextRange(0, 1));
-    addRight(new TextRange(0, 2), new TextRange(0, 2));
-    addLeft(new TextRange(3, 5), new TextRange(1, 3));
-    addRight(new TextRange(4, 5), new TextRange(2, 3));
-    CHECK.compareAll(new MergeFragment[]{
-      fragment(new TextRange(1, 2), new TextRange(1, 4), new TextRange(1, 2))
-    }, finish(3, 5, 3));
+  fun testNoIntersection() {
+    addLeft(TextRange(0, 1), TextRange(0, 1))
+    addRight(TextRange(0, 2), TextRange(0, 2))
+    addLeft(TextRange(3, 5), TextRange(1, 3))
+    addRight(TextRange(4, 5), TextRange(2, 3))
+    CHECK.compareAll(arrayOf(fragment(TextRange(1, 2), TextRange(1, 4), TextRange(1, 2))), finish(3, 5, 3))
   }
 
-  private MergeFragment fragment(TextRange left, TextRange base, TextRange right) {
-    return new MergeFragment(left, base, right);
+  private fun fragment(left: TextRange, base: TextRange, right: TextRange): MergeFragment {
+    return MergeFragment(left, base, right)
   }
 
-  private void addRight(TextRange base, TextRange right) {
-    myMergeBuilder.add(base, right, FragmentSide.SIDE2);
+  private fun addRight(base: TextRange, right: TextRange) {
+    myMergeBuilder.add(base, right, FragmentSide.SIDE2)
   }
 
-  private void addLeft(TextRange base, TextRange left) {
-    myMergeBuilder.add(base, left, FragmentSide.SIDE1);
+  private fun addLeft(base: TextRange, left: TextRange) {
+    myMergeBuilder.add(base, left, FragmentSide.SIDE1)
   }
 
-  private List<MergeFragment> finish(int left, int base, int right) {
-    return myMergeBuilder.finish(left, base, right);
+  private fun finish(left: Int, base: Int, right: Int): List<MergeFragment> {
+    return myMergeBuilder.finish(left, base, right)
   }
 
-  @Override
-  protected void runTest() throws Throwable {
+  @Throws(Throwable::class)
+  override fun runTest() {
     try {
-      super.runTest();
+      super.runTest()
     }
     finally {
-      if (IdeaLogger.ourErrorsOccurred != null) throw IdeaLogger.ourErrorsOccurred;
+      if (IdeaLogger.ourErrorsOccurred != null) throw IdeaLogger.ourErrorsOccurred
     }
   }
 
-  @Override
-  protected void setUp() throws Exception {
-    super.setUp();
-    IdeaLogger.ourErrorsOccurred = null;
+  @Throws(Exception::class)
+  override fun setUp() {
+    super.setUp()
+    IdeaLogger.ourErrorsOccurred = null
   }
 }
