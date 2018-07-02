11eda9d7c9b23f0bc76ac307250829b621753cab
Aleksey Pivovarov <AMPivovarov@gmail.com>
2018-07-02 16:28:45 +0300

    merge: migrate tests from old API

--- platform/vcs-tests/testSrc/com/intellij/openapi/diff/impl/incrementalMerge/MergeBuilderTest.kt
+++ platform/vcs-tests/testSrc/com/intellij/openapi/diff/impl/incrementalMerge/MergeBuilderTest.kt
@@ -15,134 +15,196 @@
  */
 package com.intellij.openapi.diff.impl.incrementalMerge
 
-import com.intellij.idea.IdeaLogger
+import com.intellij.diff.DiffTestCase
+import com.intellij.diff.comparison.ComparisonMergeUtil
+import com.intellij.diff.comparison.iterables.DiffIterableUtil
+import com.intellij.diff.comparison.iterables.DiffIterableUtil.fair
+import com.intellij.diff.util.MergeRange
+import com.intellij.diff.util.Range
 import com.intellij.openapi.diff.impl.highlighting.FragmentSide
 import com.intellij.openapi.diff.impl.util.ContextLogger
 import com.intellij.openapi.util.TextRange
-import com.intellij.util.Assertion
-import junit.framework.TestCase
 
-class MergeBuilderTest : TestCase() {
-  private val myMergeBuilder = MergeBuilder(ContextLogger("TEST"))
-  private val CHECK = Assertion()
-
+class MergeBuilderTest : DiffTestCase() {
   fun testEqual() {
-    addLeft(TextRange(0, 1), TextRange(0, 1))
-    addRight(TextRange(0, 1), TextRange(0, 1))
-    CHECK.empty(finish(1, 1, 1))
+    test(1, 1, 1) {
+      addLeft(TextRange(0, 1), TextRange(0, 1))
+      addRight(TextRange(0, 1), TextRange(0, 1))
+    }
   }
 
   fun testWholeConflict() {
-    CHECK.singleElement(finish(1, 2, 3),
-                        fragment(TextRange(0, 1), TextRange(0, 2), TextRange(0, 3)))
+    test(1, 2, 3) {
+      addExpected(TextRange(0, 1), TextRange(0, 2), TextRange(0, 3))
+    }
   }
 
   fun testTailInsert() {
-    val range = TextRange(0, 1)
-    addLeft(range, range)
-    addRight(range, range)
-    CHECK.singleElement(finish(1, 1, 2),
-                        fragment(TextRange(1, 1), TextRange(1, 1), TextRange(1, 2)))
+    test(1, 1, 2) {
+      val range = TextRange(0, 1)
+      addLeft(range, range)
+      addRight(range, range)
+
+      addExpected(TextRange(1, 1), TextRange(1, 1), TextRange(1, 2))
+    }
   }
 
   fun testSameInsertsConflicts1() {
-    val base = TextRange(0, 1)
-    val version = TextRange(1, 2)
-    addLeft(base, version)
-    addRight(base, version)
-    CHECK.singleElement(finish(2, 1, 2),
-                        fragment(TextRange(0, 1), TextRange(0, 0), TextRange(0, 1)))
+    test(2, 1, 2) {
+      val base = TextRange(0, 1)
+      val version = TextRange(1, 2)
+      addLeft(base, version)
+      addRight(base, version)
+
+      addExpected(TextRange(0, 1), TextRange(0, 0), TextRange(0, 1))
+    }
   }
 
   fun testSameInsertsConflicts2() {
-    val base = TextRange(1, 2)
-    val version = TextRange(0, 1)
-    addLeft(base, version)
-    addRight(base, version)
-    CHECK.compareAll(
-      arrayOf(fragment(TextRange(0, 0), TextRange(0, 1), TextRange(0, 0)), fragment(TextRange(1, 2), TextRange(2, 2), TextRange(1, 2))),
-      finish(2, 2, 2))
+    test(2, 2, 2) {
+      val base = TextRange(1, 2)
+      val version = TextRange(0, 1)
+      addLeft(base, version)
+      addRight(base, version)
+
+      addExpected(TextRange(0, 0), TextRange(0, 1), TextRange(0, 0))
+      addExpected(TextRange(1, 2), TextRange(2, 2), TextRange(1, 2))
+    }
   }
 
   fun testHeadInsert() {
-    val range = TextRange(0, 1)
-    addRight(range, TextRange(1, 2))
-    addLeft(range, range)
-    CHECK.singleElement(finish(1, 1, 2),
-                        fragment(TextRange(0, 0), TextRange(0, 0), TextRange(0, 1)))
+    test(1, 1, 2) {
+      val range = TextRange(0, 1)
+      addRight(range, TextRange(1, 2))
+      addLeft(range, range)
+
+      addExpected(TextRange(0, 0), TextRange(0, 0), TextRange(0, 1))
+    }
   }
 
   fun testOneSideChange() {
-    addRight(TextRange(0, 2), TextRange(0, 2))
-    addLeft(TextRange(1, 2), TextRange(2, 3))
-    CHECK.singleElement(finish(3, 2, 2),
-                        fragment(TextRange(0, 2), TextRange(0, 1), TextRange(0, 1)))
+    test(3, 2, 2) {
+      addRight(TextRange(0, 2), TextRange(0, 2))
+      addLeft(TextRange(1, 2), TextRange(2, 3))
+
+      addExpected(TextRange(0, 2), TextRange(0, 1), TextRange(0, 1))
+    }
   }
 
   fun testNotAllignedConflict() {
-    addLeft(TextRange(1, 3), TextRange(0, 2))
-    addRight(TextRange(2, 4), TextRange(1, 3))
-    CHECK.compareAll(
-      arrayOf(fragment(TextRange(0, 1), TextRange(0, 2), TextRange(0, 1)), fragment(TextRange(2, 3), TextRange(3, 4), TextRange(2, 3))),
-      finish(3, 4, 3))
+    test(3, 4, 3) {
+      addLeft(TextRange(1, 3), TextRange(0, 2))
+      addRight(TextRange(2, 4), TextRange(1, 3))
+
+      addExpected(TextRange(0, 1), TextRange(0, 2), TextRange(0, 1))
+      addExpected(TextRange(2, 3), TextRange(3, 4), TextRange(2, 3))
+    }
   }
 
   fun testBug() {
-    addRight(TextRange(0, 1), TextRange(0, 1))
-    addLeft(TextRange(0, 2), TextRange(0, 2))
-    CHECK.compareAll(arrayOf(fragment(TextRange(1, 3), TextRange(1, 2), TextRange(1, 1))), finish(3, 2, 1))
+    test(3, 2, 1) {
+      addRight(TextRange(0, 1), TextRange(0, 1))
+      addLeft(TextRange(0, 2), TextRange(0, 2))
+
+      addExpected(TextRange(1, 3), TextRange(1, 2), TextRange(1, 1))
+    }
   }
 
   fun testMultiChanges() {
-    addLeft(TextRange(1, 8), TextRange(1, 8))
-    addRight(TextRange(1, 2), TextRange(0, 1))
-    addRight(TextRange(3, 4), TextRange(1, 2))
-    addRight(TextRange(4, 5), TextRange(3, 4))
-    addRight(TextRange(6, 7), TextRange(5, 6))
-    addLeft(TextRange(9, 10), TextRange(9, 10))
-    CHECK.compareAll(
-      arrayOf(fragment(TextRange(0, 1), TextRange(0, 1), TextRange(0, 0)), fragment(TextRange(2, 3), TextRange(2, 3), TextRange(1, 1)),
-              fragment(TextRange(4, 4), TextRange(4, 4), TextRange(2, 3)), fragment(TextRange(5, 6), TextRange(5, 6), TextRange(4, 5)),
-              fragment(TextRange(7, 10), TextRange(7, 10), TextRange(6, 7))), finish(10, 10, 7))
+    test(10, 10, 7) {
+      addLeft(TextRange(1, 8), TextRange(1, 8))
+      addRight(TextRange(1, 2), TextRange(0, 1))
+      addRight(TextRange(3, 4), TextRange(1, 2))
+      addRight(TextRange(4, 5), TextRange(3, 4))
+      addRight(TextRange(6, 7), TextRange(5, 6))
+      addLeft(TextRange(9, 10), TextRange(9, 10))
+
+      addExpected(TextRange(0, 1), TextRange(0, 1), TextRange(0, 0))
+      addExpected(TextRange(2, 3), TextRange(2, 3), TextRange(1, 1))
+      addExpected(TextRange(4, 4), TextRange(4, 4), TextRange(2, 3))
+      addExpected(TextRange(5, 6), TextRange(5, 6), TextRange(4, 5))
+      addExpected(TextRange(7, 10), TextRange(7, 10), TextRange(6, 7))
+    }
   }
 
   fun testNoIntersection() {
-    addLeft(TextRange(0, 1), TextRange(0, 1))
-    addRight(TextRange(0, 2), TextRange(0, 2))
-    addLeft(TextRange(3, 5), TextRange(1, 3))
-    addRight(TextRange(4, 5), TextRange(2, 3))
-    CHECK.compareAll(arrayOf(fragment(TextRange(1, 2), TextRange(1, 4), TextRange(1, 2))), finish(3, 5, 3))
-  }
+    test(3, 5, 3) {
+      addLeft(TextRange(0, 1), TextRange(0, 1))
+      addRight(TextRange(0, 2), TextRange(0, 2))
+      addLeft(TextRange(3, 5), TextRange(1, 3))
+      addRight(TextRange(4, 5), TextRange(2, 3))
 
-  private fun fragment(left: TextRange, base: TextRange, right: TextRange): MergeFragment {
-    return MergeFragment(left, base, right)
-  }
-
-  private fun addRight(base: TextRange, right: TextRange) {
-    myMergeBuilder.add(base, right, FragmentSide.SIDE2)
-  }
-
-  private fun addLeft(base: TextRange, left: TextRange) {
-    myMergeBuilder.add(base, left, FragmentSide.SIDE1)
-  }
-
-  private fun finish(left: Int, base: Int, right: Int): List<MergeFragment> {
-    return myMergeBuilder.finish(left, base, right)
-  }
-
-  @Throws(Throwable::class)
-  override fun runTest() {
-    try {
-      super.runTest()
-    }
-    finally {
-      if (IdeaLogger.ourErrorsOccurred != null) throw IdeaLogger.ourErrorsOccurred
+      addExpected(TextRange(1, 2), TextRange(1, 4), TextRange(1, 2))
     }
   }
 
-  @Throws(Exception::class)
-  override fun setUp() {
-    super.setUp()
-    IdeaLogger.ourErrorsOccurred = null
+  private fun test(leftCount: Int, baseCount: Int, rightCount: Int,
+                   block: Test.() -> Unit) {
+    val test = TestNew(leftCount, baseCount, rightCount)
+    block(test)
+    test.check()
+
+    val oldTest = TestOld(leftCount, baseCount, rightCount)
+    block(oldTest)
+    oldTest.check()
+  }
+
+  private interface Test {
+    fun addLeft(base: TextRange, left: TextRange)
+    fun addRight(base: TextRange, right: TextRange)
+    fun addExpected(left: TextRange, base: TextRange, right: TextRange)
+  }
+
+  private inner class TestNew(val leftCount: Int, val baseCount: Int, val rightCount: Int) : Test {
+    private val leftUnchanged: MutableList<Range> = mutableListOf()
+    private val rightUnchanged: MutableList<Range> = mutableListOf()
+    private val expected: MutableList<MergeRange> = mutableListOf()
+
+    override fun addLeft(base: TextRange, left: TextRange) {
+      leftUnchanged += Range(base.startOffset, base.endOffset,
+                             left.startOffset, left.endOffset)
+    }
+
+    override fun addRight(base: TextRange, right: TextRange) {
+      rightUnchanged += Range(base.startOffset, base.endOffset,
+                              right.startOffset, right.endOffset)
+    }
+
+    override fun addExpected(left: TextRange, base: TextRange, right: TextRange) {
+      expected += MergeRange(left.startOffset, left.endOffset,
+                             base.startOffset, base.endOffset,
+                             right.startOffset, right.endOffset)
+    }
+
+    fun check() {
+      val left = fair(DiffIterableUtil.createUnchanged(leftUnchanged, baseCount, leftCount))
+      val right = fair(DiffIterableUtil.createUnchanged(rightUnchanged, baseCount, rightCount))
+      val actual = ComparisonMergeUtil.buildFair(left, right, INDICATOR)
+
+      assertEquals(expected, actual)
+    }
+  }
+
+  private inner class TestOld(val leftCount: Int, val baseCount: Int, val rightCount: Int) : Test {
+    private val builder = MergeBuilder(ContextLogger("TEST"))
+    private val expected: MutableList<MergeFragment> = mutableListOf()
+
+    override fun addLeft(base: TextRange, left: TextRange) {
+      builder.add(base, left, FragmentSide.SIDE1)
+    }
+
+    override fun addRight(base: TextRange, right: TextRange) {
+      builder.add(base, right, FragmentSide.SIDE2)
+    }
+
+    override fun addExpected(left: TextRange, base: TextRange, right: TextRange) {
+      expected += MergeFragment(left, base, right)
+    }
+
+    fun check() {
+      val actual = builder.finish(leftCount, baseCount, rightCount)
+
+      assertEquals(expected, actual)
+    }
   }
 }
