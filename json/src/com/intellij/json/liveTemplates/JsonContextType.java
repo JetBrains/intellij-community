b9ba921555f6e0f98fea11586748a082a399f360
Anton Lobov <Anton.Lobov@jetbrains.com>
2018-07-03 15:09:26 +0200

    IDEA-194519 json: Unintended autocomplete when typing B or P in literals

--- json/src/com/intellij/json/liveTemplates/JsonContextType.java
+++ json/src/com/intellij/json/liveTemplates/JsonContextType.java
@@ -2,21 +2,10 @@ package com.intellij.json.liveTemplates;
 
 import com.intellij.codeInsight.template.FileTypeBasedContextType;
 import com.intellij.json.JsonBundle;
-import com.intellij.json.JsonElementTypes;
 import com.intellij.json.JsonFileType;
 import com.intellij.json.psi.JsonFile;
-import com.intellij.json.psi.JsonPsiUtil;
-import com.intellij.json.psi.JsonStringLiteral;
-import com.intellij.json.psi.JsonValue;
-import com.intellij.patterns.ElementPattern;
-import com.intellij.patterns.PatternCondition;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.NotNull;
-
-import static com.intellij.patterns.PlatformPatterns.psiElement;
-import static com.intellij.patterns.StandardPatterns.or;
 
 /**
  * @author Konstantin.Ulitin
@@ -28,18 +17,6 @@ public class JsonContextType extends FileTypeBasedContextType {
 
   @Override
   public boolean isInContext(@NotNull PsiFile file, int offset) {
-    if (!(file instanceof JsonFile)) {
-      return false;
-    }
-    final ElementPattern<PsiElement> illegalPattern = or(psiElement().inside(JsonStringLiteral.class),
-                                                         psiElement().inside(psiElement(JsonValue.class)
-                                                                               .with(new PatternCondition<PsiElement>("insidePropertyKey") {
-                                                                                 @Override
-                                                                                 public boolean accepts(@NotNull PsiElement element,
-                                                                                                        ProcessingContext context) {
-                                                                                   return JsonPsiUtil.isPropertyKey(element);
-                                                                                 }
-                                                                               })).beforeLeaf(psiElement(JsonElementTypes.COLON)));
-    return !illegalPattern.accepts(file.findElementAt(offset));
+    return file instanceof JsonFile;
   }
 }
