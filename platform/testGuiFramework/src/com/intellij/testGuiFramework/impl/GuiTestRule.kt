4579d2f7dbe9bcf09b4d3801bbba0300752752dd
Sergey Karashevich <sergey.karashevich@jetbrains.com>
2018-07-04 12:41:35 +0300

    [gui-test] GuiTestRule#tearDown do not close project in a DumbMode (fix for GUI-127)
    Signed-off-by: Sergey Karashevich <sergey.karashevich@jetbrains.com>

--- platform/testGuiFramework/src/com/intellij/testGuiFramework/impl/GuiTestRule.kt
+++ platform/testGuiFramework/src/com/intellij/testGuiFramework/impl/GuiTestRule.kt
@@ -6,6 +6,7 @@ import com.intellij.ide.GeneralSettings
 import com.intellij.ide.impl.ProjectUtil
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.TransactionGuard
+import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.util.io.FileUtil
@@ -134,8 +135,9 @@ class GuiTestRule : TestRule {
     private fun tearDownProject() {
       if (myProjectPath != null) {
         val ideFrameFixture = IdeFrameFixture.find(robot(), myProjectPath, null)
+        ideFrameFixture.waitForStartingIndexing()
         if (ideFrameFixture.target().isShowing) {
-          ideFrameFixture.closeProject()
+          DumbService.getInstance(ideFrameFixture.project).repeatUntilPassesInSmartMode { ideFrameFixture.closeProject() }
         }
         FileUtilRt.delete(myProjectPath!!)
       }
@@ -143,7 +145,9 @@ class GuiTestRule : TestRule {
         try {
           val ideFrameFixture = IdeFrameFixture.find(robot(), null, null, 2)
           if (ideFrameFixture.target().isShowing)
-            ideFrameFixture.closeProject()
+            DumbService.getInstance(ideFrameFixture.project).repeatUntilPassesInSmartMode {
+              ideFrameFixture.closeProject()
+            }
         }
         catch (e: ComponentLookupException) {
           // do nothing because ideFixture is already closed
