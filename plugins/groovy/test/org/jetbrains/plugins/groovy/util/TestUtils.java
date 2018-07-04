0443dea0615a4fb94c19097f151a54c24c9eb4ce
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2018-07-04 13:19:53 +0700

    Fixed erroneous asserts

--- plugins/groovy/test/org/jetbrains/plugins/groovy/util/TestUtils.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/util/TestUtils.java
@@ -173,7 +173,7 @@ public abstract class TestUtils {
     input.add(content);
 
     Assert.assertTrue("No data found in source file", input.size() > 0);
-    Assert.assertNotNull("Test output points to null", input.size() > 1);
+    Assert.assertTrue("Test output points to null", input.size() > 1);
 
     return input;
   }
