c41a87f926342dbbb11730356ce8444fdeaffe85
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2018-07-05 16:27:43 +0300

    [groovy] update 'Convert Java-style array' intention to work with array initializers (IDEA-188432)
    (IDEA-CR-34583)
    (cherry picked from commit cb3aec7645f6e4be0c31f88c1a97687a30234ebb)

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/api/GrArrayInitializer.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/api/GrArrayInitializer.java
@@ -1,8 +1,16 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.lang.psi.api;
 
+import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.groovy.lang.psi.GroovyPsiElement;
 
 public interface GrArrayInitializer extends GroovyPsiElement {
 
+  @NotNull
+  PsiElement getLBrace();
+
+  @Nullable
+  PsiElement getRBrace();
 }
