fce523a26e9d433933f1e9d5f817353f7803af4f
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2018-07-05 20:39:23 +0300

    svn: Refactor "SvnCommitTest" - unify assertion logic

--- plugins/svn4idea/testSource/org/jetbrains/idea/svn/SvnCommitTest.java
+++ plugins/svn4idea/testSource/org/jetbrains/idea/svn/SvnCommitTest.java
@@ -4,7 +4,6 @@ package org.jetbrains.idea.svn;
 import com.intellij.openapi.vcs.FilePath;
 import com.intellij.openapi.vcs.FileStatus;
 import com.intellij.openapi.vcs.VcsConfiguration;
-import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vcs.changes.Change;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -15,11 +14,15 @@ import org.junit.Test;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static com.intellij.openapi.util.text.StringUtil.join;
+import static com.intellij.util.ArrayUtil.toObjectArray;
+import static com.intellij.util.FunctionUtil.nullConstant;
+import static com.intellij.util.containers.ContainerUtil.map;
+import static com.intellij.util.lang.CompoundRuntimeException.throwIfNotEmpty;
 import static org.junit.Assert.*;
 
 public class SvnCommitTest extends SvnTestCase {
@@ -35,7 +38,7 @@ public class SvnCommitTest extends SvnTestCase {
     final VirtualFile file = createFileInCommand(myWorkingCopyDir, "a.txt", "123");
     refreshChanges();
 
-    checkinFile(file, FileStatus.ADDED);
+    assertCommit(file, FileStatus.ADDED);
   }
 
   @Test
@@ -43,12 +46,12 @@ public class SvnCommitTest extends SvnTestCase {
     final VirtualFile file = createFileInCommand(myWorkingCopyDir, "a.txt", "123");
     refreshChanges();
 
-    checkinFile(file, FileStatus.ADDED);
+    assertCommit(file, FileStatus.ADDED);
 
     renameFileInCommand(file, "aRenamed.txt");
     refreshChanges();
 
-    checkinFile(file, FileStatus.MODIFIED);
+    assertCommit(file, FileStatus.MODIFIED);
   }
 
   @Test
@@ -57,13 +60,13 @@ public class SvnCommitTest extends SvnTestCase {
     final VirtualFile file2 = createFileInCommand(myWorkingCopyDir, "aRenamed.txt", "1235");
     refreshChanges();
 
-    checkinFiles(file, file2);
+    assertCommit(file, file2);
 
     renameFileInCommand(file, file.getName() + "7.txt");
     renameFileInCommand(file2, "a.txt");
     refreshChanges();
 
-    checkinFiles(file, file2);
+    assertCommit(file, file2);
   }
 
   @Test
@@ -73,12 +76,12 @@ public class SvnCommitTest extends SvnTestCase {
     final VirtualFile file2 = createFileInCommand(dir, "b.txt", "1235");
     refreshChanges();
 
-    checkinFiles(dir, file, file2);
+    assertCommit(dir, file, file2);
 
     renameFileInCommand(dir, dir.getName() + "dd");
     refreshChanges();
 
-    checkinFiles(dir, file, file2);
+    assertCommit(dir, file, file2);
   }
 
   @Test
@@ -90,13 +93,13 @@ public class SvnCommitTest extends SvnTestCase {
     final VirtualFile file2 = createFileInCommand(dir, "b.txt", "1235");
     refreshChanges();
 
-    checkinFiles(dir, file, file2);
+    assertCommit(dir, file, file2);
 
     final FilePath dirPath = VcsUtil.getFilePath(dir.getPath(), true);
     deleteFileInCommand(dir);
     refreshChanges();
 
-    checkinPaths(dirPath);
+    assertCommit(dirPath);
   }
 
   @Test
@@ -134,57 +137,38 @@ public class SvnCommitTest extends SvnTestCase {
     editFileInCommand(vf2, "2317468732ghdwwe7y348rf csdjcjksw");
     refreshChanges();
 
-    HashSet<String> strings = checkinFiles(vf1, vf2);
+    Set<String> strings = assertCommit(vf1, vf2);
     assertEquals(join(strings, "\n"), 1, strings.size());
   }
 
-  private void checkinPaths(FilePath... files) {
-    final List<Change> changes = new ArrayList<>();
-    for (FilePath file : files) {
-      final Change change = changeListManager.getChange(file);
-      assertNotNull(change);
-      changes.add(change);
-    }
-    final List<VcsException> exceptions = vcs.getCheckinEnvironment().commit(changes, "test comment list");
-    assertTrue(exceptions == null || exceptions.isEmpty());
-    refreshChanges();
-
-    for (FilePath file : files) {
-      final Change changeA = changeListManager.getChange(file);
-      assertNull(changeA);
-    }
-  }
-
-  private HashSet<String> checkinFiles(VirtualFile... files) {
-    final List<Change> changes = new ArrayList<>();
-    for (VirtualFile file : files) {
-      final Change change = changeListManager.getChange(file);
-      assertNotNull(change);
-      changes.add(change);
-    }
-    final HashSet<String> feedback = new HashSet<>();
-    final List<VcsException> exceptions = vcs.getCheckinEnvironment().commit(changes, "test comment list", o -> null, feedback);
-    if (exceptions !=null && ! exceptions.isEmpty()) {
-      exceptions.get(0).printStackTrace();
-    }
-    assertTrue(exceptions == null || exceptions.isEmpty());
-    refreshChanges();
-
-    for (VirtualFile file : files) {
-      final Change changeA = changeListManager.getChange(file);
-      assertNull(changeA);
-    }
-    return feedback;
-  }
-
-  protected void checkinFile(VirtualFile file, FileStatus status) {
+  private void assertCommit(VirtualFile file, FileStatus status) {
     final Change change = changeListManager.getChange(file);
     assertNotNull(change);
     assertEquals(status, change.getFileStatus());
-    final List<VcsException> exceptions = vcs.getCheckinEnvironment().commit(Collections.singletonList(change), "test comment");
-    assertTrue(exceptions == null || exceptions.isEmpty());
+
+    assertCommit(file);
+  }
+
+  private Set<String> assertCommit(VirtualFile... files) {
+    return assertCommit(toObjectArray(map(files, VcsUtil::getFilePath), FilePath.class));
+  }
+
+  private Set<String> assertCommit(FilePath... files) {
+    final List<Change> changes = new ArrayList<>();
+    for (FilePath file : files) {
+      final Change change = changeListManager.getChange(file);
+      assertNotNull(change);
+      changes.add(change);
+    }
+    Set<String> feedback = new HashSet<>();
+    //noinspection unchecked
+    throwIfNotEmpty((List)vcs.getCheckinEnvironment().commit(changes, "test comment list", nullConstant(), feedback));
     refreshChanges();
-    final Change changeA = changeListManager.getChange(file);
-    assertNull(changeA);
+
+    for (FilePath file : files) {
+      assertNull(changeListManager.getChange(file));
+    }
+
+    return feedback;
   }
 }
