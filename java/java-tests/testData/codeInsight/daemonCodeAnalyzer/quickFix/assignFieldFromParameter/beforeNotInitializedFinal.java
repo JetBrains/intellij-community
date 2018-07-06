ee605ddc4699d9f96de317f29873f93e92cf2ecc
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2018-07-06 17:50:26 +0700

    AssignFieldFromParameterAction: check whether final field is already initialized

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/assignFieldFromParameter/beforeNotInitializedFinal.java
@@ -0,0 +1,11 @@
+// "Assign Parameter to Field 'myStr'" "true"
+
+
+class Foo1 {
+    final String myStr;
+    Foo1(String s<caret>tr) {
+        if(Math.random() > 0.5) {
+        } else {
+        }
+    }
+}
