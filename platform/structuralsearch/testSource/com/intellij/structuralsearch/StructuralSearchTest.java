3eb243cfe5e283da7d788805cdb8e77f19fafa46
Bas Leijdekkers <basleijdekkers@gmail.com>
2018-07-06 16:36:50 +0200

    SSR: implement explicit support for repeated variables (IDEA-195166)

--- platform/structuralsearch/testSource/com/intellij/structuralsearch/StructuralSearchTest.java
+++ platform/structuralsearch/testSource/com/intellij/structuralsearch/StructuralSearchTest.java
@@ -3092,4 +3092,28 @@ public class StructuralSearchTest extends StructuralSearchTestCase {
     assertEquals("should find defaults", 3, findMatchesCount(in, "default:"));
     assertEquals("should find cases", 8, findMatchesCount(in, "case '_a :"));
   }
+
+  public void testRepeatedVars() {
+    String in = "class SomePageObject {\n" +
+                    "  @FindBy(name = \"first-name\")\n" +
+                    "  private WebElement name;\n" +
+                    "\n" +
+                    "  @FindBy(name = \"first-name\")\n" +
+                    "  private WebElement firstName;\n" +
+                    "}" +
+                    "class SomePageObject2 {\n" +
+                    "  @FindBy(name = \"last-name\")\n" +
+                    "  private WebElement name;\n" +
+                    "\n" +
+                    "  @FindBy(name = \"first-name\")\n" +
+                    "  private WebElement firstName;\n" +
+                    "}";
+
+    assertEquals("find repeated annotation value", 1, findMatchesCount(in, "class '_Class {\n" +
+                                                                           "  @FindBy(name='_value)\n" +
+                                                                           "  '_FieldType '_field = '_init?;\n" +
+                                                                           "  @FindBy(name='_value)\n" +
+                                                                           "  '_FieldType2 'field2 = '_init2?;\n" +
+                                                                           "}"));
+  }
 }
