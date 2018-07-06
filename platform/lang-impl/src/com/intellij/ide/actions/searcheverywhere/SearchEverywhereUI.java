4c3c174a8ebe5161f472e88d6cb2699bd7ca7d9c
Mikhail Sokolov <mikhail.sokolov@jetbrains.com>
2018-07-06 17:32:44 +0300

    IDEA-193644 New SE: Hint area fixes

--- platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/SearchEverywhereUI.java
+++ platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/SearchEverywhereUI.java
@@ -169,7 +169,7 @@ public class SearchEverywhereUI extends BorderLayoutPanel implements Disposable,
     String hint = IdeBundle.message("searcheverywhere.history.shortcuts.hint",
                                     KeymapUtil.getKeystrokeText(SearchTextField.ALT_SHOW_HISTORY_KEYSTROKE),
                                     KeymapUtil.getKeystrokeText(SearchTextField.SHOW_HISTORY_KEYSTROKE));
-    JLabel hintLabel = HintUtil.createAdComponent(hint, JBUI.Borders.empty(), SwingConstants.LEFT);
+    JLabel hintLabel = HintUtil.createAdComponent(hint, JBUI.Borders.emptyLeft(8), SwingConstants.LEFT);
     hintLabel.setOpaque(false);
     hintLabel.setForeground(JBColor.GRAY);
     pnl.add(hintLabel, BorderLayout.SOUTH);
