9a11b24ec38ce6eb8533b18eb5315cce769efe59
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2018-07-09 11:14:32 +0700

    MakeCallChainIntoCallSequence: fix assignment when type differs

--- plugins/IntentionPowerPak/src/com/siyeh/ipp/concatenation/MakeCallChainIntoCallSequenceIntention.java
+++ plugins/IntentionPowerPak/src/com/siyeh/ipp/concatenation/MakeCallChainIntoCallSequenceIntention.java
@@ -72,7 +72,7 @@ public class MakeCallChainIntoCallSequenceIntention extends Intention {
       appendStatement = (PsiStatement)grandParent;
       if (parent instanceof PsiAssignmentExpression && grandParent instanceof PsiExpressionStatement) {
         final PsiAssignmentExpression assignment = (PsiAssignmentExpression)parent;
-        final PsiExpression lhs = assignment.getLExpression();
+        final PsiExpression lhs = PsiUtil.skipParenthesizedExprDown(assignment.getLExpression());
         if (!(lhs instanceof PsiReferenceExpression)) {
           return;
         }
@@ -84,22 +84,20 @@ public class MakeCallChainIntoCallSequenceIntention extends Intention {
         final PsiVariable variable = (PsiVariable)target;
         final PsiType variableType = variable.getType();
         if (variableType.equals(rootType)) {
-          targetText = lhs.getText();
+          targetText = tracker.text(lhs);
           final PsiJavaToken token = assignment.getOperationSign();
           firstStatement = targetText + token.getText() + root.getText() + ';';
           showRenameTemplate = false;
+          variableDeclaration = null;
         }
         else {
           targetText = "x";
           showRenameTemplate = true;
-          if (JavaCodeStyleSettings.getInstance(element.getContainingFile()).GENERATE_FINAL_LOCALS) {
-            firstStatement = "final " + rootType.getCanonicalText() + ' ' + targetText + '=' + root.getText() + ';';
-          }
-          else {
-            firstStatement = rootType.getCanonicalText() + ' ' + targetText + '=' + root.getText() + ';';
-          }
+          final String firstAssignment = rootType.getCanonicalText() + ' ' + targetText + '=' + root.getText() + ';';
+          firstStatement = (JavaCodeStyleSettings.getInstance(element.getContainingFile()).GENERATE_FINAL_LOCALS ? "final " : "") +
+                           firstAssignment;
+          variableDeclaration = tracker.text(lhs) + '=';
         }
-        variableDeclaration = null;
       }
       else {
         final PsiDeclarationStatement declaration = (PsiDeclarationStatement)appendStatement;
