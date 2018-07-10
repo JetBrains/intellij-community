3fadd35b2fe44884a0648eee6541470e0f456037
Aleksey Pivovarov <AMPivovarov@gmail.com>
2018-07-10 14:22:03 +0300

    vcs: cleanup - remove unused method
    (cherry picked from commit 33eea04afc223d2dd3ed424b90c52c5968ef14cb)

--- platform/vcs-api/src/com/intellij/openapi/vcs/changes/VcsDirtyScope.java
+++ platform/vcs-api/src/com/intellij/openapi/vcs/changes/VcsDirtyScope.java
@@ -107,10 +107,6 @@ public abstract class VcsDirtyScope {
 
   public abstract boolean belongsTo(final FilePath path, final Consumer<AbstractVcs> vcsConsumer);
 
-  public Collection<VirtualFile> getAffectedContentRootsWithCheck() {
-    return getAffectedContentRoots();
-  }
-  
   public boolean wasEveryThingDirty() {
     return false;
   }
