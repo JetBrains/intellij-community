0a9ddf41641c78eb67665261f3add48b423743e1
Anna.Kozlova <anna.kozlova@jetbrains.com>
2018-07-11 10:53:35 +0200

    fix testdata

--- plugins/java-i18n/testData/inspections/i18n/NonNlsEquals.java
+++ plugins/java-i18n/testData/inspections/i18n/NonNlsEquals.java
@@ -1,9 +1,9 @@
 class Foo {
     void test(@org.jetbrains.annotations.NonNls String s, String s1) {
         if (s.equals("bar")) {}
-        if (s1.equals("bar")) {}
+        if (s1.equals(<warning descr="Hard coded string literal: \"bar\"">"bar"</warning>)) {}
         if ("bar".equals(s)) {}
-        if ("bar".equals(s1)) {}
+        if (<warning descr="Hard coded string literal: \"bar\"">"bar"</warning>.equals(s1)) {}
         if (("bar").equals((s))) {}
         if ((s).equals(("bar"))) {}
     }
