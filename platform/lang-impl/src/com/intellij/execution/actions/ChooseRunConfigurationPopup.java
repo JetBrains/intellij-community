fe43d289f8412aefd35ddba8b00a223a7773cfb2
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2018-07-12 00:25:38 +0300

    IDEA-193775 No easy way to delete a run configuration
    Assign 'Delete' key to delete selected run configuration in popup
    
    (cherry picked from commit d5926e478d5bbb42d44090c713086147784548e1)
    IDEA-CR-34857

--- platform/lang-impl/src/com/intellij/execution/actions/ChooseRunConfigurationPopup.java
+++ platform/lang-impl/src/com/intellij/execution/actions/ChooseRunConfigurationPopup.java
@@ -38,7 +38,6 @@ import com.intellij.openapi.ui.popup.ListPopupStep;
 import com.intellij.openapi.ui.popup.ListSeparator;
 import com.intellij.openapi.ui.popup.PopupStep;
 import com.intellij.openapi.ui.popup.util.BaseListPopupStep;
-import com.intellij.ui.CommonActionsPanel;
 import com.intellij.ui.popup.WizardPopup;
 import com.intellij.ui.popup.list.ListPopupImpl;
 import com.intellij.ui.popup.list.PopupListElementRenderer;
@@ -144,7 +143,7 @@ public class ChooseRunConfigurationPopup implements ExecutorProvider {
       }
     });
 
-    popup.registerAction("deleteConfiguration", KeymapUtil.getKeyStroke(CommonActionsPanel.getCommonShortcut(CommonActionsPanel.Buttons.REMOVE)),
+    popup.registerAction("deleteConfiguration", KeyStroke.getKeyStroke("DELETE"),
                          new AbstractAction() {
                            @Override
                            public void actionPerformed(ActionEvent e) {
