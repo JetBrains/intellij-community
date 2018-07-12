c6fbea055674412c72b1001090c2544f8cb0a075
Artem Bochkarev <artem.bochkarev@jetbrains.com>
2018-07-12 10:10:54 +0700

    MacBook touch bar: minimized dependency on the Application object
    Initialize runners-group when Application has been initialized. Now
    touchbar can be shown before Application created, for example : User Agreement Dialog

--- platform/platform-impl/src/com/intellij/ide/MacOSApplicationProvider.java
+++ platform/platform-impl/src/com/intellij/ide/MacOSApplicationProvider.java
@@ -138,7 +138,7 @@ public class MacOSApplicationProvider {
       });
       installAutoUpdateMenu();
 
-      TouchBarsManager.subscribeProjectManager();
+      TouchBarsManager.onApplicationInitialized();
     }
 
     private static void installAutoUpdateMenu() {
