2489eb035454fb1507146a5035366bb08f71024b
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2018-07-13 16:54:33 +0300

    test mpved to community

---
+++ java/java-tests/testSrc/com/intellij/openapi/roots/ui/configuration/LanguageLevelComboTest.java
@@ -0,0 +1,54 @@
+package com.intellij.openapi.roots.ui.configuration;
+
+import com.intellij.openapi.application.ex.PathManagerEx;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.project.ex.ProjectManagerEx;
+import com.intellij.openapi.roots.LanguageLevelProjectExtension;
+import com.intellij.pom.java.LanguageLevel;
+import com.intellij.testFramework.IdeaTestUtil;
+import com.intellij.testFramework.LightPlatformTestCase;
+
+public class LanguageLevelComboTest extends LightPlatformTestCase {
+
+  private Project myProject;
+  private LanguageLevelCombo myCombo;
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    myProject = ProjectManager.getInstance().loadAndOpenProject(PathManagerEx.getTestDataPath("/ide/project"));
+    myCombo = new LanguageLevelCombo("default") {
+      @Override
+      protected LanguageLevel getDefaultLevel() {
+        return null;
+      }
+    };
+    myCombo.reset(myProject);
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    try {
+      ProjectManagerEx.getInstanceEx().closeAndDispose(myProject);
+    }
+    finally {
+      super.tearDown();
+    }
+  }
+
+  public void testDefaultLanguageLevelForBrokenSdk() {
+    assertEquals("default", myCombo.getSelectedItem());
+    assertNull(myCombo.getSelectedLevel());
+  }
+
+  public void testPreserveLanguageLevel() {
+    LanguageLevelProjectExtension extension = LanguageLevelProjectExtension.getInstance(myProject);
+    extension.setDefault(false);
+    extension.setLanguageLevel(LanguageLevel.JDK_1_5);
+    myCombo.reset(myProject);
+
+    myCombo.sdkUpdated(IdeaTestUtil.getMockJdk17(), false);
+    assertEquals(LanguageLevel.JDK_1_5, myCombo.getSelectedLevel());
+  }
+}
