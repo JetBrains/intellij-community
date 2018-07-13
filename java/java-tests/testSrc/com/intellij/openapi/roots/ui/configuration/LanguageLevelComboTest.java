c8478ab534f87fe1a405d18d1218053521a0ea52
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2018-07-13 17:14:56 +0300

    copyright

--- java/java-tests/testSrc/com/intellij/openapi/roots/ui/configuration/LanguageLevelComboTest.java
+++ java/java-tests/testSrc/com/intellij/openapi/roots/ui/configuration/LanguageLevelComboTest.java
@@ -1,3 +1,4 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.roots.ui.configuration;
 
 import com.intellij.openapi.application.ex.PathManagerEx;
