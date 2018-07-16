b88cfd79adb7d4312999fc066beaaf4e00b6cda9
Anton Lobov <Anton.Lobov@jetbrains.com>
2018-07-16 15:04:08 +0200

    JsonSchemaService.isSchemaFile requires read action

--- json/src/com/jetbrains/jsonSchema/JsonSchemaVfsListener.java
+++ json/src/com/jetbrains/jsonSchema/JsonSchemaVfsListener.java
@@ -75,7 +75,7 @@ public class JsonSchemaVfsListener extends BulkVirtualFileListenerAdapter {
         Collection<VirtualFile> scope = new HashSet<>(myDirtySchemas);
         myDirtySchemas.removeAll(scope);
 
-        Collection<VirtualFile> finalScope = ContainerUtil.filter(scope, file -> myService.isApplicableToFile(file) && myService.isSchemaFile(file));
+        Collection<VirtualFile> finalScope = ReadAction.compute(() -> ContainerUtil.filter(scope, file -> myService.isApplicableToFile(file) && myService.isSchemaFile(file)));
         if (finalScope.isEmpty()) return;
         myProject.getMessageBus().syncPublisher(JSON_SCHEMA_CHANGED).run();
 
