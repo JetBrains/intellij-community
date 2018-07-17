579fd4bd3e35e359f33997e20fa185e9327b532b
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-07-17 12:28:51 +0200

    create result only if need

--- platform/platform-impl/src/com/intellij/openapi/fileTypes/impl/AbstractFileType.java
+++ platform/platform-impl/src/com/intellij/openapi/fileTypes/impl/AbstractFileType.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.fileTypes.impl;
 
 import com.intellij.ide.highlighter.FileTypeRegistrator;
@@ -331,12 +331,12 @@ public class AbstractFileType extends UserFileType<AbstractFileType> implements 
 
   @NotNull
   public static List<Trinity<FileNameMatcher, String, Boolean>> readRemovedAssociations(@NotNull Element element) {
-    List<Trinity<FileNameMatcher, String, Boolean>> result = new SmartList<>();
     List<Element> children = element.getChildren(ELEMENT_REMOVED_MAPPING);
     if (children.isEmpty()) {
       return Collections.emptyList();
     }
 
+    List<Trinity<FileNameMatcher, String, Boolean>> result = new SmartList<>();
     for (Element mapping : children) {
       String ext = mapping.getAttributeValue(ATTRIBUTE_EXT);
       FileNameMatcher matcher = ext == null ? FileTypeManager.parseFromString(mapping.getAttributeValue(ATTRIBUTE_PATTERN)) : new ExtensionFileNameMatcher(ext);
