5f6bb44c1e26d1ebd0fbf4a4e50402903fcb9bb7
Bas Leijdekkers <basleijdekkers@gmail.com>
2018-07-17 11:12:19 +0300

    IG: disable equals() to equality quickfix on top-level equals() call

--- java/testFramework/src/com/siyeh/ig/LightInspectionTestCase.java
+++ java/testFramework/src/com/siyeh/ig/LightInspectionTestCase.java
@@ -1,22 +1,9 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.siyeh.ig;
 
 import com.intellij.codeHighlighting.HighlightDisplayLevel;
 import com.intellij.codeInsight.daemon.HighlightDisplayKey;
+import com.intellij.codeInsight.intention.IntentionAction;
 import com.intellij.codeInspection.InspectionProfileEntry;
 import com.intellij.codeInspection.ex.InspectionProfileImpl;
 import com.intellij.openapi.module.ModuleManager;
@@ -92,6 +79,17 @@ public abstract class LightInspectionTestCase extends LightCodeInsightFixtureTes
 
   protected final void doTest(@Language("JAVA") @NotNull String classText) {
     doTest(classText, "X.java");
+  }
+
+  protected final void assertQuickFixNotAvailable(String name) {
+    assertEmpty(myFixture.filterAvailableIntentions(name));
+  }
+
+  protected final void checkQuickFix(String name, @Language("JAVA") String result) {
+    final IntentionAction intention = myFixture.getAvailableIntention(name);
+    assertNotNull(intention);
+    myFixture.launchAction(intention);
+    myFixture.checkResult(result);
   }
 
   protected final void doTest(@Language("JAVA") @NotNull String classText, String fileName) {
