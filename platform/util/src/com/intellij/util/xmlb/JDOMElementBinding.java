3542e1b202b5c6f7e97b7233df624a61e45e0b95
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-07-19 11:18:06 +0200

    do not required tag annotation

--- platform/util/src/com/intellij/util/xmlb/JDOMElementBinding.java
+++ platform/util/src/com/intellij/util/xmlb/JDOMElementBinding.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.xmlb;
 
 import com.intellij.openapi.util.text.StringUtil;
@@ -31,13 +17,8 @@ class JDOMElementBinding extends NotNullDeserializeBinding implements MultiNodeB
     super(accessor);
 
     Tag tag = myAccessor.getAnnotation(Tag.class);
-    assert tag != null : "jdom.Element property without @Tag annotation: " + accessor;
-
-    String tagName = tag.value();
-    if (StringUtil.isEmpty(tagName)) {
-      tagName = myAccessor.getName();
-    }
-    myTagName = tagName;
+    String tagName = tag == null ? null : tag.value();
+    myTagName = StringUtil.isEmpty(tagName) ? myAccessor.getName() : tagName;
   }
 
   @Override
