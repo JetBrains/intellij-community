21bb60816327383375ddd3d0fbe07d83796372c1
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2018-07-19 11:32:29 +0200

    Cleanup (warnings in test classes)

--- java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/ModuleHighlightingTest.kt
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/ModuleHighlightingTest.kt
@@ -1,6 +1,4 @@
-/*
- * Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.java.codeInsight.daemon
 
 import com.intellij.codeInsight.daemon.impl.JavaHighlightInfoTypes
@@ -290,9 +288,9 @@ class ModuleHighlightingTest : LightJava9ModulesCodeInsightFixtureTestCase() {
         }}
         """.trimIndent()
     if (moduleFileInTests != checkFileInTests) {
-      checkFileText = Regex("(<error [^>]+>)([^<]+)(</error>)").replace(checkFileText, {
+      checkFileText = Regex("(<error [^>]+>)([^<]+)(</error>)").replace(checkFileText) {
         if (it.value.contains("unreachable")) it.value else it.groups[2]!!.value
-      })
+      }
     }
     highlight("test.java", checkFileText, checkFileInTests)
   }
