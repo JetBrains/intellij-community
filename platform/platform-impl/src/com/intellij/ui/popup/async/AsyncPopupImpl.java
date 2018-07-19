eabcfe3dbfeacc9f36239aae19278c3bb9c21fe3
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2018-07-19 16:24:11 +0300

    Remove 'loading' animated icon for async submenu 'Loading...'

--- platform/platform-impl/src/com/intellij/ui/popup/async/AsyncPopupImpl.java
+++ platform/platform-impl/src/com/intellij/ui/popup/async/AsyncPopupImpl.java
@@ -9,7 +9,6 @@ import com.intellij.ui.components.JBLabel;
 import com.intellij.ui.popup.NextStepHandler;
 import com.intellij.ui.popup.WizardPopup;
 import com.intellij.util.Alarm;
-import com.intellij.util.ui.AsyncProcessIcon;
 import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -77,7 +76,7 @@ public class AsyncPopupImpl extends WizardPopup implements Runnable {
   protected JComponent createContent() {
     if (myPanel != null) return myPanel;
     myPanel = new JPanel(new BorderLayout());
-    myPanel.add(new AsyncProcessIcon("Async Popup Step"), BorderLayout.WEST);
+    //myPanel.add(new AsyncProcessIcon("Async Popup Step"), BorderLayout.WEST);
     JBLabel label = new JBLabel("Loading...");
     label.setForeground(UIUtil.getLabelDisabledForeground());
     myPanel.add(label, BorderLayout.CENTER);
