ba48528f2279b743184cf32efaca55e9d87b34e0
Alexey Kudravtsev <cdr@intellij.com>
2018-07-19 18:41:11 +0300

    generalize checkInheritance

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/GroovyDirectInheritorsSearcher.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/GroovyDirectInheritorsSearcher.java
@@ -67,7 +67,7 @@ public class GroovyDirectInheritorsSearcher implements QueryExecutor<PsiClass, D
       RelaxedDirectInheritorChecker checker = dumbService.runReadActionInSmartMode(() -> new RelaxedDirectInheritorChecker(clazz));
 
       for (PsiClass candidate : candidates) {
-        if (!queryParameters.isCheckInheritance() || dumbService.runReadActionInSmartMode(() -> checker.checkInheritance(candidate))) {
+        if (!queryParameters.isCheckInheritance() || dumbService.runReadActionInSmartMode(() -> checker.checkInheritance(candidate, false))) {
           if (!consumer.process(candidate)) {
             return false;
           }
