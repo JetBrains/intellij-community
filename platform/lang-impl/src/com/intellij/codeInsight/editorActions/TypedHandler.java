1488e1c699d7e30992da36369b2180d3479a29cd
Max Medvedev <maxim.medvedev@jetbrains.com>
2018-07-22 22:02:11 +0300

    Revert "OC-16351 [swift override operator completion] provide caret position for invokeAutoPopup"
    This reverts commit d601157c

--- platform/lang-impl/src/com/intellij/codeInsight/editorActions/TypedHandler.java
+++ platform/lang-impl/src/com/intellij/codeInsight/editorActions/TypedHandler.java
@@ -289,7 +289,7 @@ public class TypedHandler extends TypedActionHandlerBase {
       final PsiElement element = file.findElementAt(offset);
       if (element != null) {
         for (CompletionContributor contributor : CompletionContributor.forLanguageHonorDumbness(element.getLanguage(), file.getProject())) {
-          if (contributor.invokeAutoPopup(element, charTyped, offset + 1)) {
+          if (contributor.invokeAutoPopup(element, charTyped)) {
             LOG.debug(contributor + " requested completion autopopup when typing '" + charTyped + "'");
             return true;
           }
