4fa5f9b71c20de104e26f444b9db226d10988bc7
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2018-07-23 17:40:51 +0300

    vcs: Refactor "ClearCommittedAction"

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/committed/ClearCommittedAction.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/committed/ClearCommittedAction.kt
@@ -1,14 +1,12 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vcs.changes.committed
 
-import com.intellij.icons.AllIcons
-import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.vcs.changes.ui.ChangesViewContentManager
 
-class ClearCommittedAction : AnAction("Clear", "Clears cached revisions", AllIcons.Vcs.Remove), DumbAware {
+class ClearCommittedAction : DumbAwareAction() {
   override fun actionPerformed(e: AnActionEvent) {
     val project = e.getData(CommonDataKeys.PROJECT)!!
     val panel = ChangesViewContentManager.getInstance(project).getActiveComponent(CommittedChangesPanel::class.java)!!
