4a987bd3a6c2c339faecff037481fcb6e5144812
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-07-24 16:29:25 +0200

    fix compilation error (kotlin checks not null)

--- platform/platform-api/src/com/intellij/ui/HyperlinkLabel.java
+++ platform/platform-api/src/com/intellij/ui/HyperlinkLabel.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.ui;
 
@@ -71,7 +57,7 @@ public class HyperlinkLabel extends HighlightableComponent {
   public HyperlinkLabel(String text) {
     this(text, UIUtil.getLabelBackground());
   }
-  
+
   public HyperlinkLabel(String text, Color background) {
     this(text, PlatformColors.BLUE, background, PlatformColors.BLUE);
   }
@@ -178,7 +164,7 @@ public class HyperlinkLabel extends HighlightableComponent {
   }
 
   @Override
-  public void setText(String text) {
+  public void setText(@Nullable String text) {
     applyFont();
     myUseIconAsLink = false;
     super.setText(text);
