45899d86b67c4627bee10f99e68b98042600522d
Bas Leijdekkers <basleijdekkers@gmail.com>
2018-07-31 23:14:22 +0200

    IG: avoid trying to create a variable with vararg type (IDEA-196543)
    for "Extract variable as local variable" quick fix

--- plugins/InspectionGadgets/testsrc/com/siyeh/ig/fixes/ExtractParameterAsLocalVariableFixTest.java
+++ plugins/InspectionGadgets/testsrc/com/siyeh/ig/fixes/ExtractParameterAsLocalVariableFixTest.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.siyeh.ig.fixes;
 
 import com.siyeh.InspectionGadgetsBundle;
@@ -216,6 +202,25 @@ public class ExtractParameterAsLocalVariableFixTest extends IGQuickFixesTestCase
            "        System.out.println(o);\n" +
            "    }\n" +
            "    \n" +
+           "}");
+  }
+
+  public void testVarargs() {
+    doTest(InspectionGadgetsBundle.message("extract.parameter.as.local.variable.quickfix"),
+           "class RecordImpl<C> {\n" +
+           "    public <T extends Enum<T>> Map<T, Object> fillEnumObjectMap(Map<T, Object> map, T... selectedColumns) {\n" +
+           "        /**/selectedColumns = null;\n" +
+           "        return map;\n" +
+           "    }\n" +
+           "\n" +
+           "}",
+
+           "class RecordImpl<C> {\n" +
+           "    public <T extends Enum<T>> Map<T, Object> fillEnumObjectMap(Map<T, Object> map, T... selectedColumns) {\n" +
+           "        T[] o = null;\n" +
+           "        return map;\n" +
+           "    }\n" +
+           "\n" +
            "}");
   }
 }
