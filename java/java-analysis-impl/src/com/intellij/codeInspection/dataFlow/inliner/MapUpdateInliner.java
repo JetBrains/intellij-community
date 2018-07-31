d790b657a7eccbf480930a794c16c5c30fb56ee7
Tagir Valeev <tagir.valeev@jetbrains.com>
2018-07-31 21:42:58 +0700

    MapUpdateInliner: flush Map.size specifically instead of flushing all fields
    Flushing all fields is overkill and doesn't work if Map is a local object (unlike normal method call inlining does not mark it as escaped)
    Fixes IDEA-196415 False positive on code inspection with Map.isEmpty always 'true' when computeIfAbsent is used to add mapping

--- java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/inliner/MapUpdateInliner.java
+++ java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/inliner/MapUpdateInliner.java
@@ -6,6 +6,10 @@ package com.intellij.codeInspection.dataFlow.inliner;
 import com.intellij.codeInsight.Nullability;
 import com.intellij.codeInspection.dataFlow.CFGBuilder;
 import com.intellij.codeInspection.dataFlow.NullabilityProblemKind;
+import com.intellij.codeInspection.dataFlow.SpecialField;
+import com.intellij.codeInspection.dataFlow.value.DfaUnknownValue;
+import com.intellij.codeInspection.dataFlow.value.DfaValue;
+import com.intellij.codeInspection.dataFlow.value.DfaValueFactory;
 import com.intellij.psi.CommonClassNames;
 import com.intellij.psi.PsiExpression;
 import com.intellij.psi.PsiMethodCallExpression;
@@ -39,13 +43,13 @@ public class MapUpdateInliner implements CallInliner {
       String name = Objects.requireNonNull(call.getMethodExpression().getReferenceName());
       switch (name) {
         case "computeIfAbsent":
-          inlineComputeIfAbsent(builder, key, function, type);
+          inlineComputeIfAbsent(builder, qualifier, key, function, type);
           break;
         case "computeIfPresent":
-          inlineComputeIfPresent(builder, key, function, type);
+          inlineComputeIfPresent(builder, qualifier, key, function, type);
           break;
         case "compute":
-          inlineCompute(builder, key, function, type);
+          inlineCompute(builder, qualifier, key, function, type);
           break;
         default:
           throw new IllegalStateException("Unsupported name: " + name);
@@ -77,20 +81,31 @@ public class MapUpdateInliner implements CallInliner {
         .swap()
         .invokeFunction(2, function)
         .end()
-        .flushFields();
+        .chain(b -> flushSize(qualifier, b));
       return true;
     }
     return false;
   }
 
-  private static void inlineComputeIfAbsent(@NotNull CFGBuilder builder, PsiExpression key, PsiExpression function, PsiType type) {
+  private static void flushSize(PsiExpression qualifier, CFGBuilder builder) {
+    DfaValueFactory factory = builder.getFactory();
+    DfaValue value = factory.createValue(qualifier);
+    DfaValue size = SpecialField.MAP_SIZE.createValue(factory, value);
+    builder.assignAndPop(size, DfaUnknownValue.getInstance());
+  }
+
+  private static void inlineComputeIfAbsent(@NotNull CFGBuilder builder,
+                                            PsiExpression qualifier,
+                                            PsiExpression key,
+                                            PsiExpression function,
+                                            PsiType type) {
     builder
       .pushExpression(key) // stack: .. key
       .evaluateFunction(function)
       .pushUnknown() // stack: .. key; get() result
       .ifNull() // stack: .. key
       .invokeFunction(1, function) // stack: .. mapping result
-      .flushFields()
+      .chain(b -> flushSize(qualifier, b))
       .elseBranch()
       .pop()
       .push(builder.getFactory().createTypeValue(type, Nullability.NOT_NULL))
@@ -98,7 +113,7 @@ public class MapUpdateInliner implements CallInliner {
   }
 
   private static void inlineComputeIfPresent(@NotNull CFGBuilder builder,
-                                             PsiExpression key,
+                                             PsiExpression qualifier, PsiExpression key,
                                              PsiExpression function,
                                              PsiType type) {
     builder
@@ -108,7 +123,7 @@ public class MapUpdateInliner implements CallInliner {
       .ifNotNull() // stack: .. key
       .push(builder.getFactory().createTypeValue(type, Nullability.NOT_NULL))
       .invokeFunction(2, function) // stack: .. mapping result
-      .flushFields()
+      .chain(b -> flushSize(qualifier, b))
       .elseBranch()
       .pop()
       .pushNull()
@@ -116,7 +131,7 @@ public class MapUpdateInliner implements CallInliner {
   }
 
   private static void inlineCompute(@NotNull CFGBuilder builder,
-                                    PsiExpression key,
+                                    PsiExpression qualifier, PsiExpression key,
                                     PsiExpression function,
                                     PsiType type) {
     builder
@@ -124,6 +139,6 @@ public class MapUpdateInliner implements CallInliner {
       .evaluateFunction(function)
       .push(builder.getFactory().createTypeValue(type, Nullability.NULLABLE))
       .invokeFunction(2, function) // stack: .. mapping result
-      .flushFields();
+      .chain(b -> flushSize(qualifier, b));
   }
 }
