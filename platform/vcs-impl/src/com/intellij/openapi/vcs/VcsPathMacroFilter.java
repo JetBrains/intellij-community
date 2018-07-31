3d9336f1e949d32d3eb56ee7f41a4e60c9badd51
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-07-31 16:53:56 +0200

    do not collapse/expand macro for attributes named `comment`

--- platform/vcs-impl/src/com/intellij/openapi/vcs/VcsPathMacroFilter.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/VcsPathMacroFilter.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vcs;
 
 import com.intellij.openapi.application.PathMacroFilter;
@@ -18,6 +18,11 @@ public class VcsPathMacroFilter extends PathMacroFilter {
       return true;
     }
     if ("option".equals(parentName) && "LAST_COMMIT_MESSAGE".equals(parent.getAttributeValue("name"))) {
+      return true;
+    }
+
+    // ignore comment field of ChangeListManager.list and in general any attribute named "comment" since in most cases value should be stored as is
+    if ("comment".equals(attribute.getName())) {
       return true;
     }
     return false;
