66271b1d7cfc3ab57537914385238f613b5b3c23
Michael Golubev <michael.golubev@jetbrains.com>
2018-07-31 10:07:35 +0200

    IDEA-183287 - Support Elytron security for WildFly 11+ (IDEA-CR-35502)
    - allows server remote agent to depend on some other agent jars
    - - added RemoteAgentManager.Builder # withModuleDependency to be called multiple times
    
    (cherry picked from commit 2b60e2d)

--- platform/remote-servers/impl/src/com/intellij/remoteServer/agent/impl/RemoteAgentManagerImpl.java
+++ platform/remote-servers/impl/src/com/intellij/remoteServer/agent/impl/RemoteAgentManagerImpl.java
@@ -6,6 +6,7 @@ import com.intellij.remoteServer.agent.RemoteAgentManager;
 import com.intellij.remoteServer.agent.RemoteAgentProxyFactory;
 import com.intellij.util.Base64;
 import com.intellij.util.PathUtil;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -28,34 +29,95 @@ public class RemoteAgentManagerImpl extends RemoteAgentManager {
                                                String agentClassName,
                                                Class<?> pluginClass) throws Exception {
 
-    List<Class<?>> allCommonJarClasses = new ArrayList<>(commonJarClasses);
-    allCommonJarClasses.add(RemoteAgent.class);
-    allCommonJarClasses.add(Base64.class);
-    allCommonJarClasses.add(agentInterface);
+    Builder<T> builder = createAgentBuilder(agentProxyFactory, agentInterface, pluginClass)
+      .withRtDependencies(commonJarClasses)
+      .withInstanceLibraries(instanceLibraries)
+      .withModuleDependency(specificsRuntimeModuleName, specificsBuildJarPath);
 
-    List<File> libraries = new ArrayList<>(instanceLibraries);
-
-    for (Class<?> clazz : allCommonJarClasses) {
-      libraries.add(new File(PathUtil.getJarPathForClass(clazz)));
-    }
-
-    File plugin = new File(PathUtil.getJarPathForClass(pluginClass));
-    String allPluginsDir = plugin.getParent();
-    if (plugin.isDirectory()) {
-      // runtime behavior
-      File specificsModule = new File(allPluginsDir, specificsRuntimeModuleName);
-      libraries.add(specificsModule);
-    }
-    else {
-      // build behavior
-      File specificsDir = new File(allPluginsDir, FileUtil.toSystemDependentName(specificsBuildJarPath));
-      libraries.add(specificsDir);
-    }
-
-    return agentProxyFactory.createProxy(libraries, agentInterface, agentClassName);
+    return builder.buildAgent(agentClassName);
   }
 
+  @Override
+  public <T extends RemoteAgent> Builder<T> createAgentBuilder(@NotNull RemoteAgentProxyFactory agentProxyFactory,
+                                                               @NotNull Class<T> agentInterface,
+                                                               @NotNull Class<?> pluginClass) {
+    return new AgentBuilderImpl<>(agentProxyFactory, agentInterface, pluginClass);
+  }
+
+  @Override
   public RemoteAgentProxyFactory createReflectiveThreadProxyFactory(ClassLoader callerClassLoader) {
     return new RemoteAgentReflectiveThreadProxyFactory(myClassLoaderCache, callerClassLoader);
+  }
+
+  private static class AgentBuilderImpl<T extends RemoteAgent> extends Builder<T> {
+    private final List<Class<?>> myRtClasses = new ArrayList<>();
+    private final List<File> myInstanceLibraries = new ArrayList<>();
+    private final List<File> myModuleDependencies = new ArrayList<>();
+
+    private final RemoteAgentProxyFactory myAgentProxyFactory;
+    private final Class<T> myAgentInterface;
+
+    private final String myAllPluginsRoot;
+    private final boolean myRuntimeNotBuild;
+
+    public AgentBuilderImpl(@NotNull RemoteAgentProxyFactory agentProxyFactory,
+                            @NotNull Class<T> agentInterface,
+                            @NotNull Class<?> pluginClass) {
+      myAgentProxyFactory = agentProxyFactory;
+      myAgentInterface = agentInterface;
+
+      File plugin = new File(PathUtil.getJarPathForClass(pluginClass));
+      myAllPluginsRoot = plugin.getParent();
+      myRuntimeNotBuild = plugin.isDirectory();
+    }
+
+    @Override
+    public T buildAgent(@NotNull String agentClassName) throws Exception {
+      List<File> libraries = listLibraryFiles();
+      return myAgentProxyFactory.createProxy(libraries, myAgentInterface, agentClassName);
+    }
+
+    @NotNull
+    private List<File> listLibraryFiles() {
+      List<File> result = new ArrayList<>(myInstanceLibraries);
+
+      List<Class<?>> allRtClasses = new ArrayList<>(myRtClasses);
+      allRtClasses.add(RemoteAgent.class);
+      allRtClasses.add(Base64.class);
+      allRtClasses.add(myAgentInterface);
+
+      for (Class<?> clazz : allRtClasses) {
+        result.add(new File(PathUtil.getJarPathForClass(clazz)));
+      }
+
+      result.addAll(myModuleDependencies);
+
+      return result;
+    }
+
+    @Override
+    public Builder<T> withRtDependency(@NotNull Class<?> rtClass) {
+      myRtClasses.add(rtClass);
+      return this;
+    }
+
+    @Override
+    public Builder<T> withInstanceLibraries(@NotNull List<File> libraries) {
+      myInstanceLibraries.addAll(libraries);
+      return this;
+    }
+
+    @Override
+    public Builder<T> withModuleDependency(@NotNull String runtimeModuleName, @NotNull String buildPathToJar) {
+      if (myRuntimeNotBuild) {
+        File specificsModule = new File(myAllPluginsRoot, runtimeModuleName);
+        myModuleDependencies.add(specificsModule);
+      }
+      else {
+        File specificsDir = new File(myAllPluginsRoot, FileUtil.toSystemDependentName(buildPathToJar));
+        myModuleDependencies.add(specificsDir);
+      }
+      return this;
+    }
   }
 }
