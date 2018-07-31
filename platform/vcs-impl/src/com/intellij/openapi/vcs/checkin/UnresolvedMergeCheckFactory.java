421153ec0a311adcfce3c03818306ec084d2d80c
Aleksey Pivovarov <AMPivovarov@gmail.com>
2018-07-31 12:37:04 +0300

    IDEA-83415 git: warn about unresolved conflicts in repository before commit

--- platform/vcs-impl/src/com/intellij/openapi/vcs/checkin/UnresolvedMergeCheckFactory.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/checkin/UnresolvedMergeCheckFactory.java
@@ -6,7 +6,11 @@ import com.intellij.openapi.vcs.CheckinProjectPanel;
 import com.intellij.openapi.vcs.FileStatus;
 import com.intellij.openapi.vcs.changes.Change;
 import com.intellij.openapi.vcs.changes.CommitContext;
+import com.intellij.openapi.vcs.changes.CommitExecutor;
+import com.intellij.openapi.vcs.checkin.CheckinHandler.ReturnResult;
+import com.intellij.util.PairConsumer;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Checks if there are unresolved conflicts selected to commit.
@@ -16,25 +20,36 @@ import org.jetbrains.annotations.NotNull;
 public class UnresolvedMergeCheckFactory extends CheckinHandlerFactory {
   @NotNull
   @Override
-  public CheckinHandler createHandler(@NotNull final CheckinProjectPanel panel, @NotNull CommitContext commitContext) {
+  public CheckinHandler createHandler(@NotNull CheckinProjectPanel panel, @NotNull CommitContext commitContext) {
     return new CheckinHandler() {
       @Override
-      public ReturnResult beforeCheckin() {
-        if (containsUnresolvedConflicts(panel)) {
-          int answer = Messages.showYesNoDialog(panel.getComponent(), "Are you sure you want to commit changes with unresolved conflicts?",
-                                                "Unresolved Conflicts", Messages.getWarningIcon());
-          return answer == Messages.YES ? ReturnResult.COMMIT : ReturnResult.CANCEL;
+      public ReturnResult beforeCheckin(@Nullable CommitExecutor executor, PairConsumer<Object, Object> additionalDataConsumer) {
+        for (UnresolvedMergeCheckProvider extension : UnresolvedMergeCheckProvider.EP_NAME.getExtensions()) {
+          ReturnResult result = extension.checkUnresolvedConflicts(panel, commitContext, executor);
+          if (result != null) return result;
         }
-        return ReturnResult.COMMIT;
+        return performDefaultCheck(panel);
       }
     };
+  }
+
+  @NotNull
+  private static ReturnResult performDefaultCheck(@NotNull CheckinProjectPanel panel) {
+    if (containsUnresolvedConflicts(panel)) {
+      int answer = Messages.showYesNoDialog(panel.getComponent(), "Are you sure you want to commit changes with unresolved conflicts?",
+                                            "Unresolved Conflicts", Messages.getWarningIcon());
+      if (answer != Messages.YES) return ReturnResult.CANCEL;
+    }
+    return ReturnResult.COMMIT;
   }
 
   private static boolean containsUnresolvedConflicts(@NotNull CheckinProjectPanel panel) {
     for (Change change : panel.getSelectedChanges()) {
       FileStatus status = change.getFileStatus();
-      if (status.equals(FileStatus.MERGE) || status.equals(FileStatus.MERGED_WITH_BOTH_CONFLICTS) ||
-          status.equals(FileStatus.MERGED_WITH_CONFLICTS) || status.equals(FileStatus.MERGED_WITH_PROPERTY_CONFLICTS)) {
+      if (status.equals(FileStatus.MERGE) ||
+          status.equals(FileStatus.MERGED_WITH_BOTH_CONFLICTS) ||
+          status.equals(FileStatus.MERGED_WITH_CONFLICTS) ||
+          status.equals(FileStatus.MERGED_WITH_PROPERTY_CONFLICTS)) {
         return true;
       }
     }
