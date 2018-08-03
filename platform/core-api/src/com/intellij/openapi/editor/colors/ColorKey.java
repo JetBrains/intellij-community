5b92165a236c4efc2e9f0597c34c9f6fda202ad5
Alexey Kudravtsev <cdr@intellij.com>
2018-08-03 17:17:08 +0300

    make fields final, avoid field modifications in getter

--- platform/core-api/src/com/intellij/openapi/editor/colors/ColorKey.java
+++ platform/core-api/src/com/intellij/openapi/editor/colors/ColorKey.java
@@ -17,35 +17,37 @@ package com.intellij.openapi.editor.colors;
 
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.ui.ColorUtil;
-import com.intellij.util.containers.ConcurrentFactoryMap;
+import com.intellij.util.ConcurrencyUtil;
 import com.intellij.util.containers.JBIterable;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.awt.*;
-import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public final class ColorKey implements Comparable<ColorKey> {
   private static final Logger LOG = Logger.getInstance("#com.intellij.openapi.editor.colors.ColorKey");
-  private static final Color NULL_COLOR = ColorUtil.marker("NULL_COLOR");
 
-  private static final Map<String, ColorKey> ourRegistry = ConcurrentFactoryMap.createMap(ColorKey::new);
+  private static final ConcurrentMap<String, ColorKey> ourRegistry = new ConcurrentHashMap<>();
 
   private final String myExternalName;
-  private Color myDefaultColor = NULL_COLOR;
-  private ColorKey myFallbackColorKey;
+  private final Color myDefaultColor;
+  private final ColorKey myFallbackColorKey;
 
-  private ColorKey(@NotNull String externalName) {
+  private ColorKey(@NotNull String externalName, Color defaultColor, ColorKey fallBackColorKey) {
     myExternalName = externalName;
+    myDefaultColor = defaultColor;
+    myFallbackColorKey = fallBackColorKey;
   }
 
   @NotNull
   public static ColorKey find(@NotNull String externalName) {
-    return ourRegistry.get(externalName);
+    return ourRegistry.computeIfAbsent(externalName, s -> new ColorKey(s,null,null));
   }
 
+  @Override
   public String toString() {
     return myExternalName;
   }
@@ -61,27 +63,12 @@ public final class ColorKey implements Comparable<ColorKey> {
   }
 
   public Color getDefaultColor() {
-    if (myDefaultColor == NULL_COLOR) {
-      myDefaultColor = null;
-    }
-
     return myDefaultColor;
   }
 
   @Nullable
   public ColorKey getFallbackColorKey() {
     return myFallbackColorKey;
-  }
-
-  public void setFallbackColorKey(@Nullable ColorKey fallbackColorKey) {
-    myFallbackColorKey = fallbackColorKey;
-    if (fallbackColorKey != null) {
-      JBIterable<ColorKey> it = JBIterable.generate(fallbackColorKey, o -> o == this ? null : o.myFallbackColorKey);
-      if (it.find(o -> o == this) == this) {
-        String cycle = StringUtil.join(it.map(ColorKey::getExternalName), "->");
-        LOG.error("Cycle detected: " + cycle);
-      }
-    }
   }
 
   @NotNull
@@ -91,18 +78,27 @@ public final class ColorKey implements Comparable<ColorKey> {
 
   @NotNull
   public static ColorKey createColorKey(@NonNls @NotNull String externalName, @Nullable ColorKey fallbackColorKey) {
-    ColorKey key = createColorKey(externalName);
-    key.setFallbackColorKey(fallbackColorKey);
-    return key;
+    ColorKey existing = ourRegistry.get(externalName);
+    if (existing != null) return existing;
+    ColorKey newKey = new ColorKey(externalName, null, fallbackColorKey);
+    ColorKey res = ConcurrencyUtil.cacheOrGet(ourRegistry, externalName, newKey);
+
+    if (fallbackColorKey != null) {
+      JBIterable<ColorKey> it = JBIterable.generate(fallbackColorKey, o -> o == res ? null : o.myFallbackColorKey);
+      if (it.find(o -> o == res) == res) {
+        String cycle = StringUtil.join(it.map(ColorKey::getExternalName), "->");
+        LOG.error("Cycle detected: " + cycle);
+      }
+    }
+
+    return res;
   }
 
   @NotNull
   public static ColorKey createColorKey(@NonNls @NotNull String externalName, @Nullable Color defaultColor) {
-    ColorKey key = createColorKey(externalName);
-
-    if (key.getDefaultColor() == null) {
-      key.myDefaultColor = defaultColor;
-    }
-    return key;
+    ColorKey existing = ourRegistry.get(externalName);
+    if (existing != null) return existing;
+    ColorKey newKey = new ColorKey(externalName, defaultColor, null);
+    return ConcurrencyUtil.cacheOrGet(ourRegistry, externalName, newKey);
   }
 }
