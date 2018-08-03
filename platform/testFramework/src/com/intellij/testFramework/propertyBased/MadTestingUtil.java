eec27350369a1d06f2cb8c1b6c5f1d6a9174a187
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2018-08-03 18:41:09 +0300

    add test to check that read accessors on all elements in the file don't throw exceptions

--- platform/testFramework/src/com/intellij/testFramework/propertyBased/MadTestingUtil.java
+++ platform/testFramework/src/com/intellij/testFramework/propertyBased/MadTestingUtil.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.testFramework.propertyBased;
 
 import com.intellij.codeInsight.intention.IntentionAction;
@@ -35,6 +21,7 @@ import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx;
 import com.intellij.openapi.fileTypes.FileTypeManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Condition;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.util.io.FileUtil;
@@ -64,6 +51,7 @@ import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.lang.reflect.Method;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
@@ -321,6 +309,20 @@ public class MadTestingUtil {
     return Generator.sampledFrom(new DeleteRange(file),
                                  new CheckPsiTextConsistency(file),
                                  new InsertString(file));
+  }
+
+  /**
+   * @param skipCondition should return {@code true} if particular accessor method should be ignored
+   * @return function returning generator of actions checking that
+   * read accessors on all PSI elements in the file don't throw exceptions when invoked.
+   */
+  @NotNull
+  public static Function<PsiFile, Generator<? extends MadTestingAction>> randomEditsWithPsiAccessorChecks(Condition<? super Method> skipCondition) {
+    return file -> Generator.sampledFrom(
+      new InsertString(file),
+      new DeleteRange(file),
+      new CheckPsiReadAccessors(file, skipCondition)
+    );
   }
 
   public static boolean isAfterError(PsiFile file, int offset) {
