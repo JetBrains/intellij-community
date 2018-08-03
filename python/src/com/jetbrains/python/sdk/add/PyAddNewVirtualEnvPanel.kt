12ae93369d6747af41559e667dbaa5b17d558618
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2018-08-03 20:52:22 +0300

    PY-31048 Restore auto-detection of virtualenvs inside projects upon opening them for the first time
    We used to detect them using an ad-hoc solution in the `PythonSdkConfigurator`. Since we switched to re-using
    `detectVirtualEnvs()` which is based on `PythonSdkFlavor.suggestHomePaths()`, the detection stopped working. The reason
    was the we didn't have the project in the default data context during its creation and it was needed as a starting
    point for searching for virtualenvs. We now pass an explicit module for it.
    
    (cherry picked from commit d37d561e9809adffd8f98e77da858dc792e43e61)

--- python/src/com/jetbrains/python/sdk/add/PyAddNewVirtualEnvPanel.kt
+++ python/src/com/jetbrains/python/sdk/add/PyAddNewVirtualEnvPanel.kt
@@ -62,7 +62,7 @@ class PyAddNewVirtualEnvPanel(private val project: Project?,
 
   override val panelName: String = "New environment"
   override val icon: Icon = PythonIcons.Python.Virtualenv
-  private val baseSdkField = PySdkPathChoosingComboBox(findBaseSdks(existingSdks), null).apply {
+  private val baseSdkField = PySdkPathChoosingComboBox(findBaseSdks(existingSdks, module), null).apply {
     val preferredSdkPath = PySdkSettings.instance.preferredVirtualEnvBaseSdk
     val detectedPreferredSdk = items.find { it.homePath == preferredSdkPath }
     selectedSdk = when {
