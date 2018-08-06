0f693e5c8fa839a2ab4fe00ae194a926abce8d8f
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-08-06 17:33:42 +0200

    extract flushChunkedResponse

--- platform/platform-impl/src/org/jetbrains/io/FileResponses.kt
+++ platform/platform-impl/src/org/jetbrains/io/FileResponses.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.io
 
 import io.netty.channel.Channel
@@ -29,6 +15,13 @@ import java.util.*
 import javax.activation.MimetypesFileTypeMap
 
 private val FILE_MIMETYPE_MAP = MimetypesFileTypeMap()
+
+fun flushChunkedResponse(channel: Channel, isKeepAlive: Boolean) {
+  val future = channel.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT)
+  if (!isKeepAlive) {
+    future.addListener(ChannelFutureListener.CLOSE)
+  }
+}
 
 object FileResponses {
   fun getContentType(path: String): String {
@@ -61,7 +54,7 @@ object FileResponses {
   fun sendFile(request: HttpRequest, channel: Channel, file: Path, extraHeaders: HttpHeaders = EmptyHttpHeaders.INSTANCE) {
     val response = prepareSend(request, channel, Files.getLastModifiedTime(file).toMillis(), file.fileName.toString(), extraHeaders) ?: return
 
-    val keepAlive = response.addKeepAliveIfNeed(request)
+    val isKeepAlive = response.addKeepAliveIfNeed(request)
 
     var fileWillBeClosed = false
     val raf: RandomAccessFile
@@ -96,9 +89,6 @@ object FileResponses {
       }
     }
 
-    val future = channel.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT)
-    if (!keepAlive) {
-      future.addListener(ChannelFutureListener.CLOSE)
-    }
+    flushChunkedResponse(channel, isKeepAlive)
   }
 }
