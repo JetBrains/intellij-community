6a3bea4e6a7ed2b4428a8971f63a9918d191c336
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-08-10 12:47:02 +0200

    IdeDocumentHistoryFunctionalTest in kotlin to use raw strings

--- platform/platform-tests/testSrc/com/intellij/openapi/fileEditor/IdeDocumentHistoryFunctionalTest.kt
+++ platform/platform-tests/testSrc/com/intellij/openapi/fileEditor/IdeDocumentHistoryFunctionalTest.kt
@@ -1,98 +1,90 @@
-// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.openapi.fileEditor;
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.fileEditor
 
-import com.intellij.openapi.actionSystem.IdeActions;
-import com.intellij.openapi.fileEditor.ex.IdeDocumentHistory;
-import com.intellij.openapi.fileEditor.impl.IdeDocumentHistoryImpl;
-import com.intellij.testFramework.EditorTestUtil;
+import com.intellij.openapi.actionSystem.IdeActions
+import com.intellij.testFramework.EditorTestUtil
 
-public class IdeDocumentHistoryFunctionalTest extends HeavyFileEditorManagerTestCase {
-  @Override
-  public void setUp() throws Exception {
-    super.setUp();
-    ((IdeDocumentHistoryImpl)IdeDocumentHistory.getInstance(getProject())).projectOpened();
+internal class IdeDocumentHistoryFunctionalTest : HeavyFileEditorManagerTestCase() {
+  fun testNavigateBetweenEditLocations() {
+    myFixture.configureByText("${getTestName(false)}.txt",
+                              """<caret>line1
+
+
+
+line2
+
+
+
+line3""")
+    myFixture.type(' ')
+    moveCaret4LinesDown()
+    myFixture.type(' ')
+    moveCaret4LinesDown()
+
+    EditorTestUtil.executeAction(editor, IdeActions.ACTION_GOTO_LAST_CHANGE)
+    myFixture.checkResult(""" line1
+
+
+
+l <caret>ine2
+
+
+
+line3""")
+    EditorTestUtil.executeAction(editor, IdeActions.ACTION_GOTO_LAST_CHANGE)
+    myFixture.checkResult(""" <caret>line1
+
+
+
+l ine2
+
+
+
+line3""")
+    EditorTestUtil.executeAction(editor, IdeActions.ACTION_GOTO_NEXT_CHANGE)
+    myFixture.checkResult(""" line1
+
+
+
+l <caret>ine2
+
+
+
+line3""")
+    EditorTestUtil.executeAction(editor, IdeActions.ACTION_GOTO_NEXT_CHANGE)
+    myFixture.checkResult(""" line1
+
+
+
+l <caret>ine2
+
+
+
+line3""")
   }
 
-  public void testNavigateBetweenEditLocations() {
-    myFixture.configureByText(getTestName(false) + ".txt",
-                              "<caret>line1\n" +
-                              "\n" +
-                              "\n" +
-                              "\n" +
-                              "line2\n" +
-                              "\n" +
-                              "\n" +
-                              "\n" +
-                              "line3");
-    myFixture.type(' ');
-    moveCaret4LinesDown();
-    myFixture.type(' ');
-    moveCaret4LinesDown();
-
-    EditorTestUtil.executeAction(getEditor(), IdeActions.ACTION_GOTO_LAST_CHANGE);
-    myFixture.checkResult(" line1\n" +
-                          "\n" +
-                          "\n" +
-                          "\n" +
-                          "l <caret>ine2\n" +
-                          "\n" +
-                          "\n" +
-                          "\n" +
-                          "line3");
-    EditorTestUtil.executeAction(getEditor(), IdeActions.ACTION_GOTO_LAST_CHANGE);
-    myFixture.checkResult(" <caret>line1\n" +
-                          "\n" +
-                          "\n" +
-                          "\n" +
-                          "l ine2\n" +
-                          "\n" +
-                          "\n" +
-                          "\n" +
-                          "line3");
-    EditorTestUtil.executeAction(getEditor(), IdeActions.ACTION_GOTO_NEXT_CHANGE);
-    myFixture.checkResult(" line1\n" +
-                          "\n" +
-                          "\n" +
-                          "\n" +
-                          "l <caret>ine2\n" +
-                          "\n" +
-                          "\n" +
-                          "\n" +
-                          "line3");
-    EditorTestUtil.executeAction(getEditor(), IdeActions.ACTION_GOTO_NEXT_CHANGE);
-    myFixture.checkResult(" line1\n" +
-                          "\n" +
-                          "\n" +
-                          "\n" +
-                          "l <caret>ine2\n" +
-                          "\n" +
-                          "\n" +
-                          "\n" +
-                          "line3");
-  }
-
-  public void testForwardToANearPlace() {
+  fun testForwardToANearPlace() {
     myFixture.configureByText(getTestName(false) + ".java",
-                              "class AA {}\n" +
-                              "\n" +
-                              "class BV extends A<caret>A {}");
-    EditorTestUtil.executeAction(getEditor(), IdeActions.ACTION_GOTO_DECLARATION);
-    myFixture.checkResult("class <caret>AA {}\n" +
-                          "\n" +
-                          "class BV extends AA {}");
-    EditorTestUtil.executeAction(getEditor(), IdeActions.ACTION_GOTO_BACK);
-    myFixture.checkResult("class AA {}\n" +
-                          "\n" +
-                          "class BV extends A<caret>A {}");
-    EditorTestUtil.executeAction(getEditor(), IdeActions.ACTION_GOTO_FORWARD);
-    myFixture.checkResult("class <caret>AA {}\n" +
-                          "\n" +
-                          "class BV extends AA {}");
+                              """class AA {}
+
+class BV extends A<caret>A {}""")
+    EditorTestUtil.executeAction(editor, IdeActions.ACTION_GOTO_DECLARATION)
+    myFixture.checkResult("""class <caret>AA {}
+
+class BV extends AA {}""")
+    EditorTestUtil.executeAction(editor, IdeActions.ACTION_GOTO_BACK)
+    myFixture.checkResult("""class AA {}
+
+class BV extends A<caret>A {}""")
+    EditorTestUtil.executeAction(editor, IdeActions.ACTION_GOTO_FORWARD)
+    myFixture.checkResult("""class <caret>AA {}
+
+class BV extends AA {}""")
   }
 
-  private void moveCaret4LinesDown() {
-    for (int i = 0; i < 4; i++) {
-      EditorTestUtil.executeAction(getEditor(), IdeActions.ACTION_EDITOR_MOVE_CARET_DOWN);
+  private fun moveCaret4LinesDown() {
+    for (i in 0..3) {
+      EditorTestUtil.executeAction(editor, IdeActions.ACTION_EDITOR_MOVE_CARET_DOWN)
     }
   }
 }
