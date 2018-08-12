e859ed74bd61758a4b53cbfbc90004d0eff25041
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2018-08-12 12:50:50 +0300

    Formatter excluded files reimplemented using scopes

--- platform/platform-tests/testSrc/com/intellij/formatting/fileSet/FileSetPatternDescriptorTest.java
+++ platform/platform-tests/testSrc/com/intellij/formatting/fileSet/FileSetPatternDescriptorTest.java
@@ -3,35 +3,35 @@ package com.intellij.formatting.fileSet;
 
 import com.intellij.formatting.FileSetTestCase;
 
-public class FileSetDescriptorTest extends FileSetTestCase {
+public class FileSetPatternDescriptorTest extends FileSetTestCase {
 
   public void testSimpleFileMask() {
-    FileSetDescriptor entry = new FileSetDescriptor("*.java");
+    PatternDescriptor entry = new PatternDescriptor("*.java");
     assertTrue(entry.matches(getProject(), createFile("src/test.java")));
     assertFalse(entry.matches(getProject(), createFile("src/test.txt")));
     assertTrue(entry.matches(getProject(), createFile("file3.java")));
   }
 
   public void testFileMaskWithDir() {
-    FileSetDescriptor entry = new FileSetDescriptor("/src/*.java");
+    PatternDescriptor entry = new PatternDescriptor("/src/*.java");
     assertTrue(entry.matches(getProject(), createFile("src/test.java")));
     assertFalse(entry.matches(getProject(), createFile("src/test.txt")));
     assertFalse(entry.matches(getProject(), createFile("file3.java")));
   }
 
   public void testExtendedDirSpec() {
-    FileSetDescriptor entry = new FileSetDescriptor("/src/**/test/*.java");
+    PatternDescriptor entry = new PatternDescriptor("/src/**/test/*.java");
     assertTrue(entry.matches(getProject(), createFile("src/test/file1.java")));
     assertTrue(entry.matches(getProject(), createFile("src/dir1/test/file1.java")));
     assertTrue(entry.matches(getProject(), createFile("src/dir1/dir2/test/file1.java")));
     assertFalse(entry.matches(getProject(), createFile("src/dir1/dir2/test/dir3/file1.java")));
-    FileSetDescriptor entry2 = new FileSetDescriptor("/src/**/test/**/*.java");
+    PatternDescriptor entry2 = new PatternDescriptor("/src/**/test/**/*.java");
     assertTrue(entry2.matches(getProject(), createFile("src/test/a/file1.java")));
     assertTrue(entry2.matches(getProject(), createFile("src/b/test/a/file1.java")));
   }
 
   public void testAnyCharMarks() {
-    FileSetDescriptor entry = new FileSetDescriptor("/test-??/**/test?.java");
+    PatternDescriptor entry = new PatternDescriptor("/test-??/**/test?.java");
     assertFalse(entry.matches(getProject(), createFile("test-1/test2.java")));
     assertTrue(entry.matches(getProject(), createFile("test-12/test3.java")));
     assertTrue(entry.matches(getProject(), createFile("test-12/test/testa.java")));
@@ -39,7 +39,7 @@ public class FileSetDescriptorTest extends FileSetTestCase {
 
 
   public void testRelativeDirTest() {
-    FileSetDescriptor entry = new FileSetDescriptor("test/*.java");
+    PatternDescriptor entry = new PatternDescriptor("test/*.java");
     assertTrue(entry.matches(getProject(), createFile("src/test/file1.java")));
     assertTrue(entry.matches(getProject(), createFile("test/file2.java")));
     assertTrue(entry.matches(getProject(), createFile("a/b/c/d/test/file3.java")));
