f06462012fe2cd67480a8022b91b415d78481c86
Anna.Kozlova <anna.kozlova@jetbrains.com>
2018-08-13 10:22:04 +0200

    fix String#format again

--- platform/util/src/com/intellij/util/ui/UIUtil.java
+++ platform/util/src/com/intellij/util/ui/UIUtil.java
@@ -3295,7 +3295,7 @@ public class UIUtil {
         float scale = isJreHiDPIEnabled() ? 1f : JBUI.discreteScale(dpi / 96f); // no scaling in JRE-HiDPI mode
         DEF_SYSTEM_FONT_SIZE = font.getSize() / scale; // derive actual system base font size
         if (JBUI.SCALE_VERBOSE) {
-          LOG.info(String.format("DEF_SYSTEM_FONT_SIZE: %.2f", DEF_SYSTEM_FONT_SIZE, dpi));
+          LOG.info(String.format("DEF_SYSTEM_FONT_SIZE: %.2f", DEF_SYSTEM_FONT_SIZE));
         }
       }
       else if (!SystemInfo.isJetBrainsJvm) {
