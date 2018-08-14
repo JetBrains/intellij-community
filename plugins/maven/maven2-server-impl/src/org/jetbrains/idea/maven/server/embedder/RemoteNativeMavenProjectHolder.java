173dfd5d03a55055e69f702572bf3100a6a0b619
Alexey Kudravtsev <cdr@intellij.com>
2018-08-14 16:06:17 +0300

    get rid of util-rt dependency

--- plugins/maven/maven2-server-impl/src/org/jetbrains/idea/maven/server/embedder/RemoteNativeMavenProjectHolder.java
+++ plugins/maven/maven2-server-impl/src/org/jetbrains/idea/maven/server/embedder/RemoteNativeMavenProjectHolder.java
@@ -1,20 +1,22 @@
 package org.jetbrains.idea.maven.server.embedder;
 
-import com.intellij.util.containers.ContainerUtil;
 import org.apache.maven.project.MavenProject;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.idea.maven.server.NativeMavenProjectHolder;
 
+import java.lang.ref.Reference;
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
 import java.util.Map;
 
 public class RemoteNativeMavenProjectHolder implements NativeMavenProjectHolder {
-  private static final Map<Integer, RemoteNativeMavenProjectHolder> myMap = ContainerUtil.createWeakValueMap();
+  private static final Map<Integer, Reference<RemoteNativeMavenProjectHolder>> myMap = new HashMap<Integer, Reference<RemoteNativeMavenProjectHolder>>();
 
   private final MavenProject myMavenProject;
 
   public RemoteNativeMavenProjectHolder(@NotNull MavenProject mavenProject) {
     myMavenProject = mavenProject;
-    myMap.put(getId(), this);
+    myMap.put(getId(), new WeakReference<RemoteNativeMavenProjectHolder>(this));
   }
 
   @Override
@@ -24,7 +26,8 @@ public class RemoteNativeMavenProjectHolder implements NativeMavenProjectHolder 
 
   @NotNull
   public static MavenProject findProjectById(int id) {
-    RemoteNativeMavenProjectHolder result = myMap.get(id);
+    Reference<RemoteNativeMavenProjectHolder> reference = myMap.get(id);
+    RemoteNativeMavenProjectHolder result = reference == null ? null : reference.get();
     if (result == null) {
       throw new RuntimeException("NativeMavenProjectHolder not found for id: " + id);
     }
