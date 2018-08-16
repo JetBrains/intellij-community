a0337f71b2c4dc00abab888091d52d235fd6f94a
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2018-08-16 17:26:01 +0300

    [gui-tests] lazy property initialize in GuiTestOptions

--- platform/testGuiFramework/src/com/intellij/testGuiFramework/launcher/GuiTestOptions.kt
+++ platform/testGuiFramework/src/com/intellij/testGuiFramework/launcher/GuiTestOptions.kt
@@ -25,65 +25,48 @@ object GuiTestOptions {
 
   private const val NO_NEED_TO_FILTER_TESTS: String = "NO_NEED_TO_FILTER_TESTS"
 
-  val configPath: String
-    get() = getSystemProperty("idea.config.path", configDefaultPath)
-  val systemPath: String
-    get() = getSystemProperty("idea.system.path", systemDefaultPath)
-  val isDebug: Boolean
-    get() = getSystemProperty("idea.debug.mode", false)
-  val suspendDebug: String
-    get() = if (isDebug) "y" else "n"
-  val isInternal: Boolean
-    get() = getSystemProperty("idea.is.internal", true)
-  val useAppleScreenMenuBar: Boolean
-    get() = getSystemProperty("apple.laf.useScreenMenuBar", false)
-  val debugPort: Int
-    get() = getSystemProperty("idea.gui.test.debug.port", 5009)
-  val bootClasspath: String
-    get() = getSystemProperty("idea.gui.test.bootclasspath", "../out/classes/production/intellij.platform.boot")
-  val encoding: String
-    get() = getSystemProperty("idea.gui.test.encoding", "UTF-8")
-  val xmxSize: Int
-    get() = getSystemProperty("idea.gui.test.xmx", 512)
+  val configPath: String by lazy { getSystemProperty("idea.config.path", configDefaultPath) }
+  val systemPath: String by lazy { getSystemProperty("idea.system.path", systemDefaultPath) }
+  val isDebug: Boolean by lazy { getSystemProperty("idea.debug.mode", false) }
+  val suspendDebug: String by lazy { if (isDebug) "y" else "n" }
+  val isInternal: Boolean by lazy { getSystemProperty("idea.is.internal", true) }
+  val useAppleScreenMenuBar: Boolean by lazy { getSystemProperty("apple.laf.useScreenMenuBar", false) }
+  val debugPort: Int by lazy { getSystemProperty("idea.gui.test.debug.port", 5009) }
+  val bootClasspath: String by lazy { getSystemProperty("idea.gui.test.bootclasspath", "../out/classes/production/intellij.platform.boot") }
+  val encoding: String by lazy { getSystemProperty("idea.gui.test.encoding", "UTF-8") }
+  val xmxSize: Int by lazy { getSystemProperty("idea.gui.test.xmx", 512) }
+
   //used for restarted and resumed test to qualify from what point to start
-  val resumeInfo: String
-    get() = getSystemProperty(RESUME_LABEL, "DEFAULT")
-  val resumeTestName: String
-    get() = getSystemProperty(RESUME_TEST, "undefined")
+  val resumeInfo: String by lazy { getSystemProperty(RESUME_LABEL, "DEFAULT") }
+  val resumeTestName: String by lazy { getSystemProperty(RESUME_TEST, "undefined") }
 
-
-  val shouldTestsBeFiltered: Boolean
-    get() = (filteredListOfTests != NO_NEED_TO_FILTER_TESTS)
+  val shouldTestsBeFiltered: Boolean by lazy { (filteredListOfTests != NO_NEED_TO_FILTER_TESTS) }
   //system property to set what tests should be run. -Didea.gui.test.filter=ShortClassName1,ShortClassName2
-  val filteredListOfTests: String
-    get() = getSystemProperty(FILTER_KEY, NO_NEED_TO_FILTER_TESTS)
+  val filteredListOfTests: String by lazy { getSystemProperty(FILTER_KEY, NO_NEED_TO_FILTER_TESTS) }
 
-  val screenRecorderJarDirPath: String?
-    get() = System.getProperty("idea.gui.test.screenrecorder.jar.dir.path")
-  val testsToRecord: List<String>
-    get() = System.getProperty("idea.gui.test.screenrecorder.tests_to_record")?.split(";") ?: emptyList()
-  val videoDuration: Long
-    get() = getSystemProperty("idea.gui.test.screenrecorder.video_duration_in_minutes", 3)
+  val screenRecorderJarDirPath: String? by lazy { System.getProperty("idea.gui.test.screenrecorder.jar.dir.path") }
+  val testsToRecord: List<String> by lazy { System.getProperty("idea.gui.test.screenrecorder.tests_to_record")
+                                                  ?.split(";")
+                                            ?: emptyList() }
+  val videoDuration: Long by lazy { getSystemProperty("idea.gui.test.screenrecorder.video_duration_in_minutes", 3).toLong() }
 
-  private val configDefaultPath: String
-    get() {
-      return try {
-        "${PathManager.getHomePath()}/config"
-      }
-      catch (e: RuntimeException) {
-        "../config"
-      }
+  private val configDefaultPath: String by lazy {
+    try {
+      "${PathManager.getHomePath()}/config"
     }
-
-  private val systemDefaultPath: String
-    get() {
-      return try {
-        "${PathManager.getHomePath()}/system"
-      }
-      catch (e: RuntimeException) {
-        "../system"
-      }
+    catch (e: RuntimeException) {
+      "../config"
     }
+  }
+
+  private val systemDefaultPath: String by lazy {
+    try {
+      "${PathManager.getHomePath()}/system"
+    }
+    catch (e: RuntimeException) {
+      "../system"
+    }
+  }
 
   private inline fun <reified ReturnType> getSystemProperty(key: String, defaultValue: ReturnType): ReturnType {
     val value = System.getProperty(key) ?: return defaultValue
