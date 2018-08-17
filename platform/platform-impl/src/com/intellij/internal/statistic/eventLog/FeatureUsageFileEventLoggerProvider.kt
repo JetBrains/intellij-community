104769381de57218ea49af59701158a8182fd6b9
Svetlana.Zemlyanskaya <svetlana.zemlyanskaya@jetbrains.com>
2018-08-17 15:32:57 +0200

    FUS: separate logging logic and event preprocessing

--- platform/platform-impl/src/com/intellij/internal/statistic/eventLog/FeatureUsageFileEventLoggerProvider.kt
+++ platform/platform-impl/src/com/intellij/internal/statistic/eventLog/FeatureUsageFileEventLoggerProvider.kt
@@ -2,15 +2,41 @@
 package com.intellij.internal.statistic.eventLog
 
 import com.intellij.internal.statistic.utils.StatisticsUploadAssistant
+import com.intellij.openapi.application.ApplicationAdapter
+import com.intellij.openapi.application.ApplicationInfo
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.util.BuildNumber
 import com.intellij.openapi.util.registry.Registry
+import java.util.*
 
 class FeatureUsageFileEventLoggerProvider : FeatureUsageEventLoggerProvider {
   override fun createLogger(): FeatureUsageEventLogger {
-    return FeatureUsageFileEventLogger()
+    val sessionId = UUID.randomUUID().toString().shortedUUID()
+    val build = ApplicationInfo.getInstance().build.asBuildNumber()
+    val logger = FeatureUsageFileEventLogger(sessionId, build, "-1", "2", FeatureUsageLogEventWriter())
+
+    ApplicationManager.getApplication().addApplicationListener(object : ApplicationAdapter() {
+      override fun applicationExiting() {
+        logger.dispose()
+      }
+    })
+    return logger
   }
 
-  override fun isEnabled() : Boolean {
+  private fun String.shortedUUID(): String {
+    val start = this.lastIndexOf('-')
+    if (start > 0 && start + 1 < this.length) {
+      return this.substring(start + 1)
+    }
+    return this
+  }
+
+  private fun BuildNumber.asBuildNumber(): String {
+    val str = this.asStringWithoutProductCodeAndSnapshot()
+    return if (str.endsWith(".")) str + "0" else str
+  }
+
+  override fun isEnabled(): Boolean {
     return StatisticsUploadAssistant.isSendAllowed() &&
            Registry.`is`("feature.usage.event.log.collect.and.upload") &&
            !ApplicationManager.getApplication().isUnitTestMode
