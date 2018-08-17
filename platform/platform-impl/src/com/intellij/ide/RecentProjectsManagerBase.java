ad694d5cfc842de2fd639cfd52b96a09ea4c30b3
Konstantin Bulenkov <kb@jetbrains.com>
2018-08-17 19:01:24 +0200

    fix hanging on saving
    (cherry picked from commit 4139fd8), IDEA-189321, IDEA-197212

--- platform/platform-impl/src/com/intellij/ide/RecentProjectsManagerBase.java
+++ platform/platform-impl/src/com/intellij/ide/RecentProjectsManagerBase.java
@@ -1,7 +1,6 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide;
 
-import com.intellij.execution.process.OSProcessUtil;
 import com.intellij.ide.impl.ProjectUtil;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.application.ApplicationInfo;
