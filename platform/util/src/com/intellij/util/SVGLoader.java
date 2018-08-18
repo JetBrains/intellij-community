f29855395db661dc2421bca48201d87a319e191b
Konstantin Bulenkov <kb@jetbrains.com>
2018-08-18 18:17:34 +0300

    API for patching SVG colors

--- platform/util/src/com/intellij/util/SVGLoader.java
+++ platform/util/src/com/intellij/util/SVGLoader.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util;
 
 import com.intellij.openapi.diagnostic.Logger;
@@ -57,6 +43,7 @@ import static com.intellij.util.ui.JBUI.ScaleType.PIX_SCALE;
  */
 public class SVGLoader {
   private static final Logger LOG = Logger.getInstance("#com.intellij.util.SVGLoader");
+  private static SvgColorPatcher ourColorPatcher = null;
 
   public static final NotNullValue<Double> MAX_SIZE = new NotNullValue<Double>() {
     @NotNull
@@ -229,8 +216,19 @@ public class SVGLoader {
     if (document == null) {
       throw new IOException("document not created");
     }
+    patchColors(document);
     myInput = new TranscoderInput(document);
     mySize = Size.parse(document).scale(scale);
+  }
+
+  private static void patchColors(Document document) {
+    if (ourColorPatcher != null) {
+      ourColorPatcher.patchColors(document.getDocumentElement());
+    }
+  }
+
+  public static void setColorPatcher(@Nullable SvgColorPatcher colorPatcher) {
+    ourColorPatcher = colorPatcher;
   }
 
   private BufferedImage createImage() throws TranscoderException {
@@ -257,6 +255,10 @@ public class SVGLoader {
     catch (IOException e) {
       throw new IllegalStateException(e);
     }
+  }
+
+  public interface SvgColorPatcher {
+    void patchColors(Element svg);
   }
 
   /**
