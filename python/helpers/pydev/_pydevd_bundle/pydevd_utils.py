ae21753e41e9d838f91d802d48fc157faec6cf90
Elizaveta Shashkova <Elizaveta.Shashkova@jetbrains.com>
2018-08-22 20:55:47 +0300

    PyDev: Fix for finding whether filename is in project roots when root is inside site-packages and vice versa.

--- python/helpers/pydev/_pydevd_bundle/pydevd_utils.py
+++ python/helpers/pydev/_pydevd_bundle/pydevd_utils.py
@@ -2,6 +2,7 @@ from __future__ import nested_scopes
 import traceback
 import os
 import warnings
+import pydevd_file_utils
 
 try:
     from urllib import quote
@@ -9,9 +10,14 @@ except:
     from urllib.parse import quote  # @UnresolvedImport
 
 import inspect
-from _pydevd_bundle.pydevd_constants import IS_PY3K
+from _pydevd_bundle.pydevd_constants import IS_PY3K, get_global_debugger
 import sys
 from _pydev_bundle import pydev_log
+
+
+
+def _normpath(filename):
+    return pydevd_file_utils.get_abs_path_real_path_and_base_from_file(filename)[0]
 
 
 def save_main_module(file, module_name):
@@ -47,8 +53,8 @@ def to_number(x):
 
         l = x.find('(')
         if l != -1:
-            y = x[0:l-1]
-            #print y
+            y = x[0:l - 1]
+            # print y
             try:
                 n = float(y)
                 return n
@@ -69,13 +75,17 @@ def compare_object_attrs_key(x):
         return -1, to_string(x)
 
 
+
 if IS_PY3K:
+
     def is_string(x):
         return isinstance(x, str)
 
 else:
+
     def is_string(x):
         return isinstance(x, basestring)
+
 
 def to_string(x):
     if is_string(x):
@@ -83,17 +93,22 @@ def to_string(x):
     else:
         return str(x)
 
+
 def print_exc():
     if traceback:
         traceback.print_exc()
 
+
 if IS_PY3K:
+
     def quote_smart(s, safe='/'):
         return quote(s, safe)
+
 else:
+
     def quote_smart(s, safe='/'):
         if isinstance(s, unicode):
-            s =  s.encode('utf-8')
+            s = s.encode('utf-8')
 
         return quote(s, safe)
 
@@ -123,49 +138,107 @@ def get_clsname_for_code(code, frame):
     return clsname
 
 _PROJECT_ROOTS_CACHE = []
+_LIBRARY_ROOTS_CACHE = []
 _FILENAME_TO_IN_SCOPE_CACHE = {}
 
-def set_project_roots(project_roots):
-    from _pydevd_bundle.pydevd_comm import get_global_debugger
+
+def _convert_to_str_and_clear_empty(roots):
     if sys.version_info[0] <= 2:
         # In py2 we need bytes for the files.
-        project_roots = [
-            root if not isinstance(root, unicode) else root.encode(sys.getfilesystemencoding()) 
-            for root in project_roots
+        roots = [
+            root if not isinstance(root, unicode) else root.encode(sys.getfilesystemencoding())
+            for root in roots
         ]
-    pydev_log.debug("IDE_PROJECT_ROOTS %s\n" % project_roots)
-    new_roots = []
-    for root in project_roots:
-        new_roots.append(os.path.normcase(root))
-    
-    # Leave only the last one added.
-    _PROJECT_ROOTS_CACHE.append(new_roots)
-    del _PROJECT_ROOTS_CACHE[:-1]
 
+    new_roots = []
+    for root in roots:
+        assert isinstance(root, str), '%s not str (found: %s)' % (root, type(root))
+        if root:
+            new_roots.append(root)
+    return new_roots
+
+
+def _clear_caches_related_to_scope_changes():
     # Clear related caches.
     _FILENAME_TO_IN_SCOPE_CACHE.clear()
     debugger = get_global_debugger()
     if debugger is not None:
         debugger.clear_skip_caches()
 
+
+def _set_roots(roots, cache):
+    roots = _convert_to_str_and_clear_empty(roots)
+    new_roots = []
+    for root in roots:
+        new_roots.append(_normpath(root))
+    cache.append(new_roots)
+    # Leave only the last one added.
+    del cache[:-1]
+    _clear_caches_related_to_scope_changes()
+    return new_roots
+
+
+def _get_roots(cache, env_var, set_when_not_cached, get_default_val=None):
+    if not cache:
+        roots = os.getenv(env_var, None)
+        if roots is not None:
+            roots = roots.split(os.pathsep)
+        else:
+            if not get_default_val:
+                roots = []
+            else:
+                roots = get_default_val()
+        if not roots:
+            pydev_log.warn('%s being set to empty list.' % (env_var,))
+        set_when_not_cached(roots)
+    return cache[-1]  # returns the roots with case normalized
+
+
+def _get_default_library_roots():
+    # Provide sensible defaults if not in env vars.
+    import site
+    roots = [sys.prefix]
+    if hasattr(sys, 'base_prefix'):
+        roots.append(sys.base_prefix)
+    if hasattr(sys, 'real_prefix'):
+        roots.append(sys.real_prefix)
+
+    if hasattr(site, 'getusersitepackages'):
+        site_paths = site.getusersitepackages()
+        if isinstance(site_paths, list):
+            for site_path in site_paths:
+                roots.append(site_path)
+        else:
+            roots.append(site_paths)
+
+    if hasattr(site, 'getsitepackages'):
+        site_paths = site.getsitepackages()
+        if isinstance(site_paths, list):
+            for site_path in site_paths:
+                roots.append(site_path)
+        else:
+            roots.append(site_paths)
+    return roots
+
+
+# --- Project roots
+def set_project_roots(project_roots):
+    project_roots = _set_roots(project_roots, _PROJECT_ROOTS_CACHE)
+    pydev_log.debug("IDE_PROJECT_ROOTS %s\n" % project_roots)
+
+
 def _get_project_roots(project_roots_cache=_PROJECT_ROOTS_CACHE):
-    # Note: the project_roots_cache is the same instance among the many calls to the method
-    if not project_roots_cache:
-        roots = os.getenv('IDE_PROJECT_ROOTS', '').split(os.pathsep)
-        set_project_roots(roots)
-    return project_roots_cache[-1] # returns the project roots with case normalized
+    return _get_roots(project_roots_cache, 'IDE_PROJECT_ROOTS', set_project_roots)
 
 
-def _get_library_roots(library_roots_cache=[]):
-    # Note: the project_roots_cache is the same instance among the many calls to the method
-    if not library_roots_cache:
-        roots = os.getenv('LIBRARY_ROOTS', '').split(os.pathsep)
-        pydev_log.debug("LIBRARY_ROOTS %s\n" % roots)
-        new_roots = []
-        for root in roots:
-            new_roots.append(os.path.normcase(root))
-        library_roots_cache.append(new_roots)
-    return library_roots_cache[-1] # returns the project roots with case normalized
+# --- Library roots
+def set_library_roots(roots):
+    roots = _set_roots(roots, _LIBRARY_ROOTS_CACHE)
+    pydev_log.debug("LIBRARY_ROOTS %s\n" % roots)
+
+
+def _get_library_roots(library_roots_cache=_LIBRARY_ROOTS_CACHE):
+    return _get_roots(library_roots_cache, 'LIBRARY_ROOTS', set_library_roots, _get_default_library_roots)
 
 
 def in_project_roots(filename, filename_to_in_scope_cache=_FILENAME_TO_IN_SCOPE_CACHE):
@@ -175,26 +248,32 @@ def in_project_roots(filename, filename_to_in_scope_cache=_FILENAME_TO_IN_SCOPE_
     except:
         project_roots = _get_project_roots()
         original_filename = filename
-        if not os.path.isabs(filename) and not filename.startswith('<'):
-            filename = os.path.abspath(filename)
-        filename = os.path.normcase(filename)
+        if not filename.startswith('<'):
+            filename = _normpath(filename)
+        filename = _normpath(filename)
+
+        found_in_project = []
         for root in project_roots:
             if root and filename.startswith(root):
-                filename_to_in_scope_cache[original_filename] = True
-                break
-        else: # for else (only called if the break wasn't reached).
-            filename_to_in_scope_cache[original_filename] = False
+                found_in_project.append(root)
 
-        if filename_to_in_scope_cache[original_filename]:
-            # additional check if interpreter is situated in a project directory
-            library_roots = _get_library_roots()
-            for root in library_roots:
-                if root and filename.startswith(root):
-                    filename_to_in_scope_cache[original_filename] = False
-                    break
+        found_in_library = []
+        library_roots = _get_library_roots()
+        for root in library_roots:
+            if root and filename.startswith(root):
+                found_in_library.append(root)
 
-        # at this point it must be loaded.
-        return filename_to_in_scope_cache[original_filename]
+        in_project = False
+        if found_in_project:
+            if not found_in_library:
+                in_project = True
+            else:
+                # found in both, let's see which one has the higher path
+                if max(len(x) for x in found_in_project) > max(len(x) for x in found_in_library):
+                    in_project = True
+
+        filename_to_in_scope_cache[original_filename] = in_project
+        return in_project
 
 
 def is_filter_enabled():
