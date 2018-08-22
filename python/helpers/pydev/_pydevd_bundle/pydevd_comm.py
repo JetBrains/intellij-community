dc9df6d3df3783f7aeb4f0d098d028986066351c
Elizaveta Shashkova <Elizaveta.Shashkova@jetbrains.com>
2018-08-22 16:59:40 +0300

    PyDev: Fixes in path translations.

--- python/helpers/pydev/_pydevd_bundle/pydevd_comm.py
+++ python/helpers/pydev/_pydevd_bundle/pydevd_comm.py
@@ -90,6 +90,7 @@ from _pydevd_bundle import pydevd_xml
 from _pydevd_bundle import pydevd_tracing
 from _pydevd_bundle import pydevd_vm_type
 from pydevd_file_utils import get_abs_path_real_path_and_base_from_frame, norm_file_to_client
+import pydevd_file_utils
 import os
 import sys
 import traceback
@@ -744,7 +745,7 @@ class NetCommandFactory:
 
                 abs_path_real_path_and_base = get_abs_path_real_path_and_base_from_frame(curr_frame)
 
-                myFile = norm_file_to_client(abs_path_real_path_and_base[0])
+                myFile = pydevd_file_utils.norm_file_to_client(abs_path_real_path_and_base[0])
                 if file_system_encoding.lower() != "utf-8" and hasattr(myFile, "decode"):
                     # myFile is a byte string encoded using the file system encoding
                     # convert it to utf8
--- python/helpers/pydev/pydevd_file_utils.py
+++ python/helpers/pydev/pydevd_file_utils.py
@@ -41,32 +41,28 @@ r'''
         machine for the paths that'll actually have breakpoints).
 '''
 
-
-
-
-from _pydevd_bundle.pydevd_constants import IS_PY2, IS_PY3K
+from _pydevd_bundle.pydevd_constants import IS_PY2, IS_PY3K, DebugInfoHolder
 from _pydev_bundle._pydev_filesystem_encoding import getfilesystemencoding
 import json
-import os
 import os.path
 import sys
 import traceback
 
-os_normcase = os.path.normcase
+_os_normcase = os.path.normcase
 basename = os.path.basename
 exists = os.path.exists
 join = os.path.join
 
 try:
-    rPath = os.path.realpath  #@UndefinedVariable
+    rPath = os.path.realpath  # @UndefinedVariable
 except:
     # jython does not support os.path.realpath
     # realpath is a no-op on systems without islink support
     rPath = os.path.abspath
 
-#defined as a list of tuples where the 1st element of the tuple is the path in the client machine
-#and the 2nd element is the path in the server machine.
-#see module docstring for more details.
+# defined as a list of tuples where the 1st element of the tuple is the path in the client machine
+# and the 2nd element is the path in the server machine.
+# see module docstring for more details.
 try:
     PATHS_FROM_ECLIPSE_TO_PYTHON = json.loads(os.environ.get('PATHS_FROM_ECLIPSE_TO_PYTHON', '[]'))
 except Exception:
@@ -81,49 +77,86 @@ else:
         # Converting json lists to tuple
         PATHS_FROM_ECLIPSE_TO_PYTHON = [tuple(x) for x in PATHS_FROM_ECLIPSE_TO_PYTHON]
 
-
-#example:
-#PATHS_FROM_ECLIPSE_TO_PYTHON = [
+# example:
+# PATHS_FROM_ECLIPSE_TO_PYTHON = [
 #  (r'd:\temp\temp_workspace_2\test_python\src\yyy\yyy',
 #   r'd:\temp\temp_workspace_2\test_python\src\hhh\xxx')
-#]
+# ]
 
 
-normcase = os_normcase # May be rebound on set_ide_os
+convert_to_long_pathname = lambda filename:filename
+convert_to_short_pathname = lambda filename:filename
+get_path_with_real_case = lambda filename:filename
 
-CTYPES_AVAILABLE = True
-try:
-    import ctypes
-except ImportError:
-    CTYPES_AVAILABLE = False
-
-
-def convert_to_long_pathname(filename):
-    if CTYPES_AVAILABLE:
-        buf = ctypes.create_unicode_buffer(260)
-        GetLongPathName = ctypes.windll.kernel32.GetLongPathNameW
-        if IS_PY2:
-            filename = unicode(filename)
-        rv = GetLongPathName(filename, buf, 260)
-        if rv != 0 and rv <= 260:
-            return buf.value
-    return filename
-
-
-def norm_case(filename):
-    # `normcase` doesn't lower case on Python 2 for non-English locale, but Java side does it,
-    # so we should do it manually
-    if '~' in filename:
-        filename = convert_to_long_pathname(filename)
-
-    filename = os_normcase(filename)
-    enc = getfilesystemencoding()
-    if IS_PY3K or enc is None or enc.lower() == "utf-8":
-        return filename
+if sys.platform == 'win32':
     try:
-        return filename.decode(enc).lower().encode(enc)
+        import ctypes
+        from ctypes.wintypes import MAX_PATH, LPCWSTR, LPWSTR, DWORD
+
+        GetLongPathName = ctypes.windll.kernel32.GetLongPathNameW
+        GetLongPathName.argtypes = [LPCWSTR, LPWSTR, DWORD]
+        GetLongPathName.restype = DWORD
+
+        GetShortPathName = ctypes.windll.kernel32.GetShortPathNameW
+        GetShortPathName.argtypes = [LPCWSTR, LPWSTR, DWORD]
+        GetShortPathName.restype = DWORD
+
+        def _convert_to_long_pathname(filename):
+            buf = ctypes.create_unicode_buffer(MAX_PATH)
+
+            if IS_PY2:
+                filename = filename.decode(getfilesystemencoding())
+            rv = GetLongPathName(filename, buf, MAX_PATH)
+            if rv != 0 and rv <= MAX_PATH:
+                return buf.value
+            return filename
+
+        def _convert_to_short_pathname(filename):
+            buf = ctypes.create_unicode_buffer(MAX_PATH)
+
+            if IS_PY2:
+                filename = filename.decode(getfilesystemencoding())
+            rv = GetShortPathName(filename, buf, MAX_PATH)
+            if rv != 0 and rv <= MAX_PATH:
+                return buf.value
+            return filename
+
+        def _get_path_with_real_case(filename):
+            ret = convert_to_long_pathname(convert_to_short_pathname(filename))
+            # This doesn't handle the drive letter properly (it'll be unchanged).
+            # Make sure the drive letter is always uppercase.
+            if len(ret) > 1 and ret[1] == ':' and ret[0].islower():
+                return ret[0].upper() + ret[1:]
+            return ret
+
+        # Check that it actually works
+        _get_path_with_real_case(__file__)
     except:
-        return filename
+        # Something didn't quite work out, leave no-op conversions in place.
+        if DebugInfoHolder.DEBUG_TRACE_LEVEL > 2:
+            traceback.print_exc()
+    else:
+        convert_to_long_pathname = _convert_to_long_pathname
+        convert_to_short_pathname = _convert_to_short_pathname
+        get_path_with_real_case = _get_path_with_real_case
+
+if sys.platform == 'win32':
+
+    def normcase(filename):
+        # `normcase` doesn't lower case on Python 2 for non-English locale, but Java
+        # side does it, so we should do it manually.
+        if '~' in filename:
+            filename = convert_to_long_pathname(filename)
+
+        filename = _os_normcase(filename)
+        return filename.lower()
+
+else:
+    def normcase(filename):
+        return filename # no-op
+
+
+_ide_os = 'WINDOWS' if sys.platform == 'win32' else 'UNIX'
 
 
 def set_ide_os(os):
@@ -131,32 +164,21 @@ def set_ide_os(os):
     We need to set the IDE os because the host where the code is running may be
     actually different from the client (and the point is that we want the proper
     paths to translate from the client to the server).
+
+    :param os:
+        'UNIX' or 'WINDOWS'
     '''
-    global normcase
-    if os == 'UNIX':
-        normcase = lambda f:f #Change to no-op if the client side is on unix/mac.
-    else:
-        if sys.platform == 'win32':
-            normcase = norm_case
-        else:
-            normcase = os_normcase
+    assert os in ('WINDOWS', 'UNIX')
 
-    # After setting the ide OS, apply the normcase to the existing paths.
-
-    # Note: not using enumerate nor list comprehension because it may not be available in older python versions...
-    i = 0
-    for path in PATHS_FROM_ECLIPSE_TO_PYTHON[:]:
-        PATHS_FROM_ECLIPSE_TO_PYTHON[i] = (normcase(path[0]), normcase(path[1]))
-        i += 1
+    global _ide_os
+    _ide_os = os
 
 
 DEBUG_CLIENT_SERVER_TRANSLATION = False
 
-#caches filled as requested during the debug session
+# Caches filled as requested during the debug session.
 NORM_PATHS_CONTAINER = {}
 NORM_PATHS_AND_BASE_CONTAINER = {}
-NORM_FILENAME_TO_SERVER_CONTAINER = {}
-NORM_FILENAME_TO_CLIENT_CONTAINER = {}
 
 
 def _NormFile(filename):
@@ -183,15 +205,15 @@ def _NormPaths(filename):
 
 def _NormPath(filename, normpath):
     r = normpath(filename)
-    #cache it for fast access later
+    # cache it for fast access later
     ind = r.find('.zip')
     if ind == -1:
         ind = r.find('.egg')
     if ind != -1:
-        ind+=4
+        ind += 4
         zip_path = r[:ind]
         if r[ind] == "!":
-            ind+=1
+            ind += 1
         inner_path = r[ind:]
         if inner_path.startswith('/') or inner_path.startswith('\\'):
             inner_path = inner_path[1:]
@@ -202,6 +224,8 @@ def _NormPath(filename, normpath):
 
 
 ZIP_SEARCH_CACHE = {}
+
+
 def exists(file):
     if os.path.exists(file):
         return file
@@ -211,10 +235,10 @@ def exists(file):
         ind = file.find('.egg')
 
     if ind != -1:
-        ind+=4
+        ind += 4
         zip_path = file[:ind]
         if file[ind] == "!":
-            ind+=1
+            ind += 1
         inner_path = file[ind:]
         try:
             zip = ZIP_SEARCH_CACHE[zip_path]
@@ -238,8 +262,8 @@ def exists(file):
     return None
 
 
-#Now, let's do a quick test to see if we're working with a version of python that has no problems
-#related to the names generated...
+# Now, let's do a quick test to see if we're working with a version of python that has no problems
+# related to the names generated...
 try:
     try:
         code = rPath.func_code
@@ -256,13 +280,14 @@ try:
         NORM_SEARCH_CACHE = {}
 
         initial_norm_paths = _NormPaths
-        def _NormPaths(filename):  #Let's redefine _NormPaths to work with paths that may be incorrect
+
+        def _NormPaths(filename):  # Let's redefine _NormPaths to work with paths that may be incorrect
             try:
                 return NORM_SEARCH_CACHE[filename]
             except KeyError:
                 abs_path, real_path = initial_norm_paths(filename)
                 if not exists(real_path):
-                    #We must actually go on and check if we can find it as if it was a relative path for some of the paths in the pythonpath
+                    # We must actually go on and check if we can find it as if it was a relative path for some of the paths in the pythonpath
                     for path in sys.path:
                         abs_path, real_path = initial_norm_paths(join(path, filename))
                         if exists(real_path):
@@ -276,65 +301,66 @@ try:
                 return abs_path, real_path
 
 except:
-    #Don't fail if there's something not correct here -- but at least print it to the user so that we can correct that
+    # Don't fail if there's something not correct here -- but at least print it to the user so that we can correct that
     traceback.print_exc()
 
-norm_file_to_client = _AbsFile
-norm_file_to_server = _NormFile
+# Note: as these functions may be rebound, users should always import
+# pydevd_file_utils and then use:
+#
+# pydevd_file_utils.norm_file_to_client
+# pydevd_file_utils.norm_file_to_server
+#
+# instead of importing any of those names to a given scope.
+
+def _original_file_to_client(filename, cache={}):
+    try:
+        return cache[filename]
+    except KeyError:
+        cache[filename] = get_path_with_real_case(_AbsFile(filename))
+    return cache[filename]
+
+_original_file_to_server = _NormFile
+
+norm_file_to_client = _original_file_to_client
+norm_file_to_server = _original_file_to_server
+
 
 def setup_client_server_paths(paths):
     '''paths is the same format as PATHS_FROM_ECLIPSE_TO_PYTHON'''
 
-    global NORM_FILENAME_TO_SERVER_CONTAINER
-    global NORM_FILENAME_TO_CLIENT_CONTAINER
-    global PATHS_FROM_ECLIPSE_TO_PYTHON
     global norm_file_to_client
     global norm_file_to_server
 
-    NORM_FILENAME_TO_SERVER_CONTAINER = {}
-    NORM_FILENAME_TO_CLIENT_CONTAINER = {}
-    PATHS_FROM_ECLIPSE_TO_PYTHON = paths[:]
+    norm_filename_to_server_container = {}
+    norm_filename_to_client_container = {}
+    initial_paths = list(paths)
+    paths_from_eclipse_to_python = initial_paths[:]
 
-    if not PATHS_FROM_ECLIPSE_TO_PYTHON:
-        #no translation step needed (just inline the calls)
-        norm_file_to_client = _AbsFile
-        norm_file_to_server = _NormFile
+    # Apply normcase to the existing paths to follow the os preferences.
+
+    for i, path in enumerate(paths_from_eclipse_to_python[:]):
+        paths_from_eclipse_to_python[i] = (normcase(path[0]), normcase(path[1]))
+
+    if not paths_from_eclipse_to_python:
+        # no translation step needed (just inline the calls)
+        norm_file_to_client = _original_file_to_client
+        norm_file_to_server = _original_file_to_server
         return
 
-    #Work on the client and server slashes.
-    eclipse_sep = None
-    python_sep = None
-    for eclipse_prefix, server_prefix in PATHS_FROM_ECLIPSE_TO_PYTHON:
-        if eclipse_sep is not None and python_sep is not None:
-            break
+    # Work on the client and server slashes.
+    python_sep = '\\' if sys.platform == 'win32' else '/'
+    eclipse_sep = '\\' if _ide_os == 'WINDOWS' else '/'
 
-        if eclipse_sep is None:
-            for c in eclipse_prefix:
-                if c in ('/', '\\'):
-                    eclipse_sep = c
-                    break
-
-        if python_sep is None:
-            for c in server_prefix:
-                if c in ('/', '\\'):
-                    python_sep = c
-                    break
-
-    #If they're the same or one of them cannot be determined, just make it all None.
-    if eclipse_sep == python_sep or eclipse_sep is None or python_sep is None:
-        eclipse_sep = python_sep = None
-
-
-    #only setup translation functions if absolutely needed!
+    # only setup translation functions if absolutely needed!
     def _norm_file_to_server(filename):
-        #Eclipse will send the passed filename to be translated to the python process
-        #So, this would be 'NormFileFromEclipseToPython'
+        # Eclipse will send the passed filename to be translated to the python process
+        # So, this would be 'NormFileFromEclipseToPython'
         try:
-            return NORM_FILENAME_TO_SERVER_CONTAINER[filename]
+            return norm_filename_to_server_container[filename]
         except KeyError:
-            #used to translate a path from the client to the debug server
+            # used to translate a path from the client to the debug server
             translated = normcase(filename)
-            for eclipse_prefix, server_prefix in PATHS_FROM_ECLIPSE_TO_PYTHON:
+            for eclipse_prefix, server_prefix in paths_from_eclipse_to_python:
                 if translated.startswith(eclipse_prefix):
                     if DEBUG_CLIENT_SERVER_TRANSLATION:
                         sys.stderr.write('pydev debugger: replacing to server: %s\n' % (translated,))
@@ -345,49 +371,70 @@ def setup_client_server_paths(paths):
             else:
                 if DEBUG_CLIENT_SERVER_TRANSLATION:
                     sys.stderr.write('pydev debugger: to server: unable to find matching prefix for: %s in %s\n' % \
-                        (translated, [x[0] for x in PATHS_FROM_ECLIPSE_TO_PYTHON]))
+                                     (translated, [x[0] for x in paths_from_eclipse_to_python]))
 
-            #Note that when going to the server, we do the replace first and only later do the norm file.
-            if eclipse_sep is not None:
+            # Note that when going to the server, we do the replace first and only later do the norm file.
+            if eclipse_sep != python_sep:
                 translated = translated.replace(eclipse_sep, python_sep)
             translated = _NormFile(translated)
 
-            NORM_FILENAME_TO_SERVER_CONTAINER[filename] = translated
+            norm_filename_to_server_container[filename] = translated
             return translated
 
     def _norm_file_to_client(filename):
-        #The result of this method will be passed to eclipse
-        #So, this would be 'NormFileFromPythonToEclipse'
+        # The result of this method will be passed to eclipse
+        # So, this would be 'NormFileFromPythonToEclipse'
         try:
-            return NORM_FILENAME_TO_CLIENT_CONTAINER[filename]
+            return norm_filename_to_client_container[filename]
         except KeyError:
-            #used to translate a path from the debug server to the client
+            # used to translate a path from the debug server to the client
             translated = _NormFile(filename)
-            for eclipse_prefix, python_prefix in PATHS_FROM_ECLIPSE_TO_PYTHON:
+
+            # After getting the real path, let's get it with the path with
+            # the real case and then obtain a new normalized copy, just in case
+            # the path is different now.
+            translated_proper_case = get_path_with_real_case(translated)
+            translated = _NormFile(translated_proper_case)
+
+            if sys.platform == 'win32':
+                if translated.lower() != translated_proper_case.lower():
+                    translated_proper_case = translated
+                    if DEBUG_CLIENT_SERVER_TRANSLATION:
+                        sys.stderr.write(
+                            'pydev debugger: _NormFile changed path (from: %s to %s)\n' % (
+                                translated_proper_case, translated))
+
+            for i, (eclipse_prefix, python_prefix) in enumerate(paths_from_eclipse_to_python):
                 if translated.startswith(python_prefix):
                     if DEBUG_CLIENT_SERVER_TRANSLATION:
                         sys.stderr.write('pydev debugger: replacing to client: %s\n' % (translated,))
-                    translated = translated.replace(python_prefix, eclipse_prefix)
+
+                    # Note: use the non-normalized version.
+                    eclipse_prefix = initial_paths[i][0]
+                    translated = eclipse_prefix + translated_proper_case[len(python_prefix):]
                     if DEBUG_CLIENT_SERVER_TRANSLATION:
                         sys.stderr.write('pydev debugger: sent to client: %s\n' % (translated,))
                     break
             else:
                 if DEBUG_CLIENT_SERVER_TRANSLATION:
                     sys.stderr.write('pydev debugger: to client: unable to find matching prefix for: %s in %s\n' % \
-                        (translated, [x[1] for x in PATHS_FROM_ECLIPSE_TO_PYTHON]))
+                                     (translated, [x[1] for x in paths_from_eclipse_to_python]))
+                    translated = translated_proper_case
 
-            if eclipse_sep is not None:
+            if eclipse_sep != python_sep:
                 translated = translated.replace(python_sep, eclipse_sep)
 
-            #The resulting path is not in the python process, so, we cannot do a _NormFile here,
-            #only at the beginning of this method.
-            NORM_FILENAME_TO_CLIENT_CONTAINER[filename] = translated
+            # The resulting path is not in the python process, so, we cannot do a _NormFile here,
+            # only at the beginning of this method.
+            norm_filename_to_client_container[filename] = translated
             return translated
 
     norm_file_to_server = _norm_file_to_server
     norm_file_to_client = _norm_file_to_client
 
+
 setup_client_server_paths(PATHS_FROM_ECLIPSE_TO_PYTHON)
+
 
 # For given file f returns tuple of its absolute path, real path and base name
 def get_abs_path_real_path_and_base_from_file(f):
@@ -405,9 +452,9 @@ def get_abs_path_real_path_and_base_from_frame(frame):
     try:
         return NORM_PATHS_AND_BASE_CONTAINER[frame.f_code.co_filename]
     except:
-        #This one is just internal (so, does not need any kind of client-server translation)
+        # This one is just internal (so, does not need any kind of client-server translation)
         f = frame.f_code.co_filename
-        if f is not None and f.startswith (('build/bdist.','build\\bdist.')):
+        if f is not None and f.startswith (('build/bdist.', 'build\\bdist.')):
             # files from eggs in Python 2.7 have paths like build/bdist.linux-x86_64/egg/<path-inside-egg>
             f = frame.f_globals['__file__']
         if f is not None:
