44163597fe55e348cf0fb694dd51f8de848cd03d
Elizaveta Shashkova <Elizaveta.Shashkova@jetbrains.com>
2018-08-22 16:51:07 +0300

    PyDev: Fix issue where additional_info is None. #PyDev-923

--- python/helpers/pydev/_pydevd_bundle/pydevd_cython_wrapper.py
+++ python/helpers/pydev/_pydevd_bundle/pydevd_cython_wrapper.py
@@ -1,9 +1,17 @@
 try:
-    from _pydevd_bundle_ext.pydevd_cython import trace_dispatch, PyDBAdditionalThreadInfo, global_cache_skips, global_cache_frame_skips
-    import _pydevd_bundle_ext.pydevd_cython
+    try:
+        from _pydevd_bundle_ext.pydevd_cython import trace_dispatch, PyDBAdditionalThreadInfo, set_additional_thread_info, global_cache_skips, global_cache_frame_skips
+        import _pydevd_bundle_ext.pydevd_cython
 
-    # this version number can be unavailable in old versions of compiled extensions
-    version = getattr(_pydevd_bundle_ext.pydevd_cython, 'version', 0)
+        # this version number can be unavailable in old versions of compiled extensions
+        version = getattr(_pydevd_bundle_ext.pydevd_cython, 'version', 0)
+    except ImportError:
+        from _pydevd_bundle.pydevd_cython import trace_dispatch, PyDBAdditionalThreadInfo, set_additional_thread_info, global_cache_skips, \
+            global_cache_frame_skips
+        import _pydevd_bundle.pydevd_cython
+
+        # this version number can be unavailable in old versions of compiled extensions
+        version = getattr(_pydevd_bundle.pydevd_cython, 'version', 0)
 
 except ImportError:
     try:
@@ -31,8 +39,8 @@ except ImportError:
         check_name = '_pydevd_bundle.%s' % (mod_name,)
         mod = __import__(check_name)
         mod = getattr(mod, mod_name)
-        trace_dispatch, PyDBAdditionalThreadInfo, global_cache_skips, global_cache_frame_skips = \
-            mod.trace_dispatch, mod.PyDBAdditionalThreadInfo, mod.global_cache_skips, mod.global_cache_frame_skips
+        trace_dispatch, PyDBAdditionalThreadInfo, set_additional_thread_info, global_cache_skips, global_cache_frame_skips = \
+            mod.trace_dispatch, mod.PyDBAdditionalThreadInfo, mod.set_additional_thread_info, mod.global_cache_skips, mod.global_cache_frame_skips
         version = getattr(mod, 'version', 0)
     except ImportError:
         raise
