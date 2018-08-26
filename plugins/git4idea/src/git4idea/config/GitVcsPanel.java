JetBrains/intellij-community

commit 4d43a7b4b86b7dc01cda75968f445f122f7d6e7d

git: remove usages of deprecated `GitVcsSettings.isIdeaSsh()`
* remove settings migration logic

plugins/git4idea/src/git4idea/config/GitVcsPanel.java
@@ -192,7 +192,7 @@ public class GitVcsPanel implements ConfigurableUi<GitVcsConfigurable.GitVcsSett
     String projectSettingsPathToGit = projectSettings.getPathToGit();
     myGitField.setText(ObjectUtils.coalesce(projectSettingsPathToGit, myApplicationGitPath));
     myProjectGitPathCheckBox.setSelected(projectSettingsPathToGit != null);
-    mySSHExecutableComboBox.setSelectedItem(projectSettings.isIdeaSsh() ? IDEA_SSH : NATIVE_SSH);
+    mySSHExecutableComboBox.setSelectedItem(applicationSettings.getIdeaSsh());
     mySSHOverrideAskpassCheckbox.setSelected(applicationSettings.isOverrideSshAskPass());
     myAutoUpdateIfPushRejected.setSelected(projectSettings.autoUpdateIfPushRejected());
     mySyncControl.setSelected(projectSettings.getSyncSetting() == DvcsSyncSettings.Value.SYNC);
@@ -222,7 +222,7 @@ public class GitVcsPanel implements ConfigurableUi<GitVcsConfigurable.GitVcsSett
     GitSharedSettings sharedSettings = settings.getSharedSettings();
 
     return isGitPathModified(applicationSettings, projectSettings) ||
-           (projectSettings.isIdeaSsh() != IDEA_SSH.equals(mySSHExecutableComboBox.getSelectedItem())) ||
+           applicationSettings.getIdeaSsh() != mySSHExecutableComboBox.getSelectedItem() ||
            applicationSettings.isOverrideSshAskPass() != mySSHOverrideAskpassCheckbox.isSelected() ||
            !projectSettings.autoUpdateIfPushRejected() == myAutoUpdateIfPushRejected.isSelected() ||
            ((projectSettings.getSyncSetting() == DvcsSyncSettings.Value.SYNC) != mySyncControl.isSelected() ||

plugins/git4idea/src/git4idea/GitStatisticsCollector.kt
@@ -8,6 +8,7 @@ import com.intellij.internal.statistic.utils.getBooleanUsage
 import com.intellij.internal.statistic.utils.getCountingUsage
 import com.intellij.openapi.project.Project
 import com.intellij.util.io.URLUtil
+import git4idea.config.GitVcsApplicationSettings
 import git4idea.config.GitVcsSettings
 import git4idea.config.GitVersion
 import git4idea.repo.GitRemote
@@ -17,13 +18,14 @@ class GitStatisticsCollector : ProjectUsagesCollector() {
   override fun getUsages(project: Project): Set<UsageDescriptor> {
     val repositoryManager = GitUtil.getRepositoryManager(project)
     val settings = GitVcsSettings.getInstance(project)
+    val appSettings = GitVcsApplicationSettings.getInstance()
     val repositories = repositoryManager.repositories
     val usages = hashSetOf<UsageDescriptor>()
 
     usages.add(UsageDescriptor("config.repo.sync." + settings.syncSetting.name, 1))
     usages.add(UsageDescriptor("config.update.type." + settings.updateType.name, 1))
     usages.add(UsageDescriptor("config.save.policy." + settings.updateChangesPolicy().name, 1))
-    usages.add(getBooleanUsage("config.ssh", settings.isIdeaSsh))
+    usages.add(getBooleanUsage("config.ssh", appSettings.isUseIdeaSsh))
 
     usages.add(getBooleanUsage("config.push.autoupdate", settings.autoUpdateIfPushRejected()))
     usages.add(getBooleanUsage("config.push.update.all.roots", settings.shouldUpdateAllRootsIfPushRejected()))

plugins/git4idea/src/git4idea/commands/GitHandlerAuthenticationManager.java
@@ -10,7 +10,6 @@ import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.io.URLUtil;
 import com.intellij.util.net.HttpConfigurable;
 import git4idea.config.GitVcsApplicationSettings;
-import git4idea.config.GitVcsSettings;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -50,7 +49,7 @@ public class GitHandlerAuthenticationManager implements AutoCloseable {
   public static GitHandlerAuthenticationManager prepare(@NotNull Project project, @NotNull GitLineHandler handler) throws IOException {
     GitHandlerAuthenticationManager manager = new GitHandlerAuthenticationManager(project, handler);
     manager.prepareHttpAuth();
-    if (GitVcsSettings.getInstance(project).isIdeaSsh()) {
+    if (GitVcsApplicationSettings.getInstance().isUseIdeaSsh()) {
       manager.prepareSshAuth();
     }
     else if (GitVcsApplicationSettings.getInstance().isOverrideSshAskPass()) {

plugins/git4idea/src/git4idea/config/GitVcsApplicationSettings.java
@@ -72,6 +72,10 @@ public class GitVcsApplicationSettings implements PersistentStateComponent<GitVc
     return myState.SSH_EXECUTABLE;
   }
 
+  public boolean isUseIdeaSsh() {
+    return getIdeaSsh() == SshExecutable.IDEA_SSH;
+  }
+
   public boolean isOverrideSshAskPass() {
     return myState.OVERRIDE_SSH_ASK_PASS;
   }

plugins/git4idea/src/git4idea/config/GitVcsSettings.java
@@ -43,7 +43,6 @@ public class GitVcsSettings implements PersistentStateComponent<GitVcsSettings.S
 
     // The previously entered authors of the commit (up to {@value #PREVIOUS_COMMIT_AUTHORS_LIMIT})
     public List<String> PREVIOUS_COMMIT_AUTHORS = new ArrayList<>();
-    public GitVcsApplicationSettings.SshExecutable SSH_EXECUTABLE = GitVcsApplicationSettings.SshExecutable.IDEA_SSH;
     // The policy that specifies how files are saved before update or rebase
     public UpdateChangesPolicy UPDATE_CHANGES_POLICY = UpdateChangesPolicy.STASH;
     public UpdateMethod UPDATE_TYPE = UpdateMethod.BRANCH_DEFAULT;
@@ -297,10 +296,7 @@ public class GitVcsSettings implements PersistentStateComponent<GitVcsSettings.S
    */
   @Deprecated
   public boolean isIdeaSsh() {
-    if (getAppSettings().getIdeaSsh() == null) { // app setting has not been initialized yet => migrate the project setting there
-      getAppSettings().setIdeaSsh(myState.SSH_EXECUTABLE);
-    }
-    return getAppSettings().getIdeaSsh() == GitVcsApplicationSettings.SshExecutable.IDEA_SSH;
+    return getAppSettings().isUseIdeaSsh();
   }
 
   @NotNull

commit 4ec4337de5afb453169c66b34b1c822fdb135458

git: fix "Native/Built-in SSH" option combobox in settings
fix regression after 4d43a7b4b86b7dc01cda75968f445f122f7d6e7d
caused by mixed usages of `SshExecutable.IDEA_SSH` and `GitVcsPanel.IDEA_SSH`

plugins/git4idea/src/git4idea/config/GitVcsPanel.java
@@ -48,8 +48,8 @@ import static com.intellij.util.containers.ContainerUtil.sorted;
  */
 public class GitVcsPanel implements ConfigurableUi<GitVcsConfigurable.GitVcsSettingsHolder> {
 
-  private static final String IDEA_SSH = GitBundle.getString("git.vcs.config.ssh.mode.idea"); // IDEA ssh value
-  private static final String NATIVE_SSH = GitBundle.getString("git.vcs.config.ssh.mode.native"); // Native SSH value
+  private static final String IDEA_SSH_NAME = GitBundle.getString("git.vcs.config.ssh.mode.idea"); // IDEA ssh value
+  private static final String NATIVE_SSH_NAME = GitBundle.getString("git.vcs.config.ssh.mode.native"); // Native SSH value
 
   @NotNull private final Project myProject;
   @NotNull private final GitExecutableManager myExecutableManager;
@@ -60,7 +60,7 @@ public class GitVcsPanel implements ConfigurableUi<GitVcsConfigurable.GitVcsSett
   private JComponent myRootPanel;
   private TextFieldWithBrowseButton myGitField;
   private JBCheckBox myProjectGitPathCheckBox;
-  private JComboBox mySSHExecutableComboBox; // Type of SSH executable to use
+  private JComboBox<String> mySSHExecutableComboBox; // Type of SSH executable to use
   private JCheckBox myAutoUpdateIfPushRejected;
   private JBCheckBox mySyncControl;
   private JCheckBox myAutoCommitOnCherryPick;
@@ -84,11 +84,11 @@ public class GitVcsPanel implements ConfigurableUi<GitVcsConfigurable.GitVcsSett
     myProject = project;
     myExecutableManager = executableManager;
     mySSHExecutableComboBox.addItemListener(item -> {
-      mySSHOverrideAskpassCheckbox.setVisible(NATIVE_SSH.equals(item.getItem()));
+      mySSHOverrideAskpassCheckbox.setVisible(NATIVE_SSH_NAME.equals(item.getItem()));
     });
-    mySSHExecutableComboBox.addItem(IDEA_SSH);
-    mySSHExecutableComboBox.addItem(NATIVE_SSH);
-    mySSHExecutableComboBox.setSelectedItem(IDEA_SSH);
+    mySSHExecutableComboBox.addItem(IDEA_SSH_NAME);
+    mySSHExecutableComboBox.addItem(NATIVE_SSH_NAME);
+    mySSHExecutableComboBox.setSelectedItem(IDEA_SSH_NAME);
     mySSHExecutableComboBox
       .setToolTipText(GitBundle.message("git.vcs.config.ssh.mode.tooltip", ApplicationNamesInfo.getInstance().getFullProductName()));
     myTestButton.addActionListener(e -> testExecutable());
@@ -192,7 +192,7 @@ public class GitVcsPanel implements ConfigurableUi<GitVcsConfigurable.GitVcsSett
     String projectSettingsPathToGit = projectSettings.getPathToGit();
     myGitField.setText(ObjectUtils.coalesce(projectSettingsPathToGit, myApplicationGitPath));
     myProjectGitPathCheckBox.setSelected(projectSettingsPathToGit != null);
-    mySSHExecutableComboBox.setSelectedItem(applicationSettings.getIdeaSsh());
+    mySSHExecutableComboBox.setSelectedItem(applicationSettings.isUseIdeaSsh() ? IDEA_SSH_NAME : NATIVE_SSH_NAME);
     mySSHOverrideAskpassCheckbox.setSelected(applicationSettings.isOverrideSshAskPass());
     myAutoUpdateIfPushRejected.setSelected(projectSettings.autoUpdateIfPushRejected());
     mySyncControl.setSelected(projectSettings.getSyncSetting() == DvcsSyncSettings.Value.SYNC);
@@ -222,7 +222,7 @@ public class GitVcsPanel implements ConfigurableUi<GitVcsConfigurable.GitVcsSett
     GitSharedSettings sharedSettings = settings.getSharedSettings();
 
     return isGitPathModified(applicationSettings, projectSettings) ||
-           applicationSettings.getIdeaSsh() != mySSHExecutableComboBox.getSelectedItem() ||
+           applicationSettings.isUseIdeaSsh() != IDEA_SSH_NAME.equals(mySSHExecutableComboBox.getSelectedItem()) ||
            applicationSettings.isOverrideSshAskPass() != mySSHOverrideAskpassCheckbox.isSelected() ||
            !projectSettings.autoUpdateIfPushRejected() == myAutoUpdateIfPushRejected.isSelected() ||
            ((projectSettings.getSyncSetting() == DvcsSyncSettings.Value.SYNC) != mySyncControl.isSelected() ||
@@ -259,7 +259,7 @@ public class GitVcsPanel implements ConfigurableUi<GitVcsConfigurable.GitVcsSett
       projectSettings.setPathToGit(null);
     }
 
-    applicationSettings.setIdeaSsh(IDEA_SSH.equals(mySSHExecutableComboBox.getSelectedItem()) ?
+    applicationSettings.setIdeaSsh(IDEA_SSH_NAME.equals(mySSHExecutableComboBox.getSelectedItem()) ?
                                    GitVcsApplicationSettings.SshExecutable.IDEA_SSH :
                                    GitVcsApplicationSettings.SshExecutable.NATIVE_SSH);
     applicationSettings.setOverrideSshAskPass(mySSHOverrideAskpassCheckbox.isSelected());