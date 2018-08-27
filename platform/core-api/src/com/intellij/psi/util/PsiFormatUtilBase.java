4baeb745d0903437f5d55e0804e0ecac1f548845
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2018-08-27 20:12:02 +0300

    Cleanup (typos; formatting)

--- platform/core-api/src/com/intellij/psi/util/PsiFormatUtilBase.java
+++ platform/core-api/src/com/intellij/psi/util/PsiFormatUtilBase.java
@@ -1,26 +1,10 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.util;
 
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.util.BitUtil;
 
 public abstract class PsiFormatUtilBase {
-
   public static final int SHOW_NAME = 0x0001; // variable, method, class
   public static final int SHOW_TYPE = 0x0002; // variable, method
   public static final int TYPE_AFTER = 0x0004; // variable, method
@@ -65,5 +49,4 @@ public abstract class PsiFormatUtilBase {
     }
     return true;
   }
-
 }
