601a3bfb918f0e4379b8edc041bfb62c56c23952
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2018-08-27 12:54:35 +0300

    cleanup: missed "final"

--- plugins/tasks/tasks-core/src/com/intellij/tasks/youtrack/lang/YouTrackLanguage.java
+++ plugins/tasks/tasks-core/src/com/intellij/tasks/youtrack/lang/YouTrackLanguage.java
@@ -8,7 +8,7 @@ import com.intellij.lang.Language;
  */
 public class YouTrackLanguage extends Language implements DependentLanguage {
 
-  public static YouTrackLanguage INSTANCE = new YouTrackLanguage();
+  public final static YouTrackLanguage INSTANCE = new YouTrackLanguage();
 
   private YouTrackLanguage() {
     super("YouTrack");
