3eda95851972c9fff2a85fda3b983c52d8d1959c
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2018-08-28 15:53:17 +0300

    IDEA-197998 Multiline TODOs: multiline values are not displayed in toolbar on macOS

--- platform/lang-impl/src/com/intellij/ide/todo/TodoPanel.java
+++ platform/lang-impl/src/com/intellij/ide/todo/TodoPanel.java
@@ -110,6 +110,7 @@ abstract class TodoPanel extends SimpleToolWindowPanel implements OccurenceNavig
     UIUtil.setLineStyleAngled(myTree);
     myTree.setShowsRootHandles(true);
     myTree.setRootVisible(false);
+    myTree.setRowHeight(0); // enable variable-height rows
     myTree.setCellRenderer(new TodoCompositeRenderer());
     EditSourceOnDoubleClickHandler.install(myTree);
     new TreeSpeedSearch(myTree);
