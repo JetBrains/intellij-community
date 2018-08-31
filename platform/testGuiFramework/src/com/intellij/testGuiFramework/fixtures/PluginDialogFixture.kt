1e2ca3db9bc7a040ea0fd1140a4cadb20d3373f6
Eugene Nizienko <eugene.nizienko@jetbrains.com>
2018-08-31 14:47:33 +0300

    [gui-test] IDEA-CR-36541 review fixes

--- platform/testGuiFramework/src/com/intellij/testGuiFramework/fixtures/PluginDialogFixture.kt
+++ platform/testGuiFramework/src/com/intellij/testGuiFramework/fixtures/PluginDialogFixture.kt
@@ -137,7 +137,7 @@ class PluginDialogFixture(robot: Robot, pluginDialog: JDialog) : JDialogFixture(
       waitFor {
         val pluginPathTextField: JTextField =
           waitUntilFound(target(), JTextField::class.java, Timeouts.defaultTimeout) { it.isEnabled && it.isShowing }
-        JTextComponentFixture(robot(), pluginPathTextField).deleteText().enterText(pluginPath).text()
+        JTextComponentFixture(robot(), pluginPathTextField).deleteText().enterText(pluginPath)
         pluginPathTextField.text == pluginPath
       }
     }
--- platform/testGuiFramework/src/com/intellij/testGuiFramework/util/RepeatUtils.kt
+++ platform/testGuiFramework/src/com/intellij/testGuiFramework/util/RepeatUtils.kt
@@ -1,18 +1,20 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.testGuiFramework.util
 
+import org.fest.swing.exception.WaitTimedOutError
+
 /*
-  Wait for @condition during @seconds
+  Wait for @condition during @timeOutSeconds
  */
-inline fun waitFor(seconds: Int = 5, crossinline condition: () -> Boolean) {
-  val endTime = System.currentTimeMillis() + seconds * 1000
+inline fun waitFor(timeOutSeconds: Int = 5, intervalMillis: Long = 500, crossinline condition: () -> Boolean) {
+  val endTime = System.currentTimeMillis() + timeOutSeconds * 1000
   while (System.currentTimeMillis() < endTime) {
     if (condition()) {
       return
     }
     else {
-      Thread.sleep(500)
+      Thread.sleep(intervalMillis)
     }
   }
-  throw IllegalStateException("Failed to wait for condition in $seconds seconds")
+  throw WaitTimedOutError("Failed to wait for condition in $timeOutSeconds seconds")
 }
