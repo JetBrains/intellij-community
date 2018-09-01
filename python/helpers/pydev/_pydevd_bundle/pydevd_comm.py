6e2b3ab24e72915b2bcc3fd69d3d9ed867a6b68f
Alexander Koshevoy <Alexander.Koshevoy@jetbrains.com>
2018-09-01 22:07:19 +0300

    Remove dependency on pydevd_thrift from pydevd_comm.py
    Move ThriftAbstractGetValueAsyncThread and ThriftGetValueAsyncThreadConsole from pydevd_comm.py to pydev_console_commands.py.

--- python/helpers/pydev/_pydevd_bundle/pydevd_comm.py
+++ python/helpers/pydev/_pydevd_bundle/pydevd_comm.py
@@ -80,7 +80,6 @@ if IS_IRONPYTHON:
 import pydevconsole
 from _pydevd_bundle import pydevd_vars
 from _pydevd_bundle import pydevd_xml
-from _pydevd_bundle import pydevd_thrift
 from _pydevd_bundle import pydevd_tracing
 from _pydevd_bundle import pydevd_vm_type
 from pydevd_file_utils import get_abs_path_real_path_and_base_from_frame, norm_file_to_client
@@ -1545,42 +1544,6 @@ class GetValueAsyncThreadConsole(AbstractGetValueAsyncThread):
     def send_result(self, xml):
         if self.frame_accessor is not None:
             self.frame_accessor.ReturnFullValue(self.seq, xml.getvalue())
-
-
-class ThriftAbstractGetValueAsyncThread(PyDBDaemonThread):
-    """
-    Abstract class for a thread, which evaluates values for async variables
-    """
-    def __init__(self, server, seq, var_objects):
-        PyDBDaemonThread.__init__(self)
-        self.server = server
-        self.seq = seq
-        self.var_objs = var_objects
-        self.cancel_event = threading.Event()
-
-    def send_result(self, xml):
-        raise NotImplementedError()
-
-    def _on_run(self):
-        start = time.time()
-        values = []
-        for (var_obj, name) in self.var_objs:
-            current_time = time.time()
-            if current_time - start > ASYNC_EVAL_TIMEOUT_SEC or self.cancel_event.is_set():
-                break
-            # pydev_console_thrift.DebugValue()
-            values.append(pydevd_thrift.var_to_struct(var_obj, name, evaluate_full_value=True))
-        self.send_result(values)
-
-
-class ThriftGetValueAsyncThreadConsole(ThriftAbstractGetValueAsyncThread):
-    """
-    A thread for evaluation async values, which returns result for Console
-    Send result directly to Console's server
-    """
-    def send_result(self, values):
-        if self.server is not None:
-            self.server.returnFullValue(self.seq, values)
 
 
 #=======================================================================================================================
