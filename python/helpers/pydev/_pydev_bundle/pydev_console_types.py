4955a5498b2ee37645ae99f4b618476046cc11b2
Alexander Koshevoy <Alexander.Koshevoy@jetbrains.com>
2018-09-02 20:35:51 +0300

    Extract Command PyDev class to pydev_console_types.py

--- python/helpers/pydev/_pydev_bundle/pydev_console_types.py
+++ python/helpers/pydev/_pydev_bundle/pydev_console_types.py
@@ -7,3 +7,29 @@ class CodeFragment:
         self.text = self.text + "\n" + code_fragment.text
         if not code_fragment.is_single_line:
             self.is_single_line = False
+
+
+class Command:
+    def __init__(self, interpreter, code_fragment):
+        """
+        :type code_fragment: CodeFragment
+        :type interpreter: InteractiveConsole
+        """
+        self.interpreter = interpreter
+        self.code_fragment = code_fragment
+        self.more = None
+
+
+    def symbol_for_fragment(code_fragment):
+        if code_fragment.is_single_line:
+            symbol = 'single'
+        else:
+            symbol = 'exec' # Jython doesn't support this
+        return symbol
+    symbol_for_fragment = staticmethod(symbol_for_fragment)
+
+    def run(self):
+        text = self.code_fragment.text
+        symbol = self.symbol_for_fragment(self.code_fragment)
+
+        self.more = self.interpreter.runsource(text, '<input>', symbol)
