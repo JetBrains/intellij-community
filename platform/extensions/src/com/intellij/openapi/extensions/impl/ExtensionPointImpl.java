461576061c89507e854566dc149fb1d6caf6b485
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-09-03 15:44:26 +0200

    IDEA-CR-36569 add a couple of human words

--- platform/extensions/src/com/intellij/openapi/extensions/impl/ExtensionPointImpl.java
+++ platform/extensions/src/com/intellij/openapi/extensions/impl/ExtensionPointImpl.java
@@ -31,8 +31,9 @@ public final class ExtensionPointImpl<T> implements ExtensionPoint<T> {
   private final Kind myKind;
 
   private volatile List<T> myExtensionsCache;
-  // https://bugs.openjdk.java.net/browse/JDK-6260652
-  // https://youtrack.jetbrains.com/issue/IDEA-198172
+  // Since JDK 9 Arrays.ArrayList.toArray() doesn't return T[] array (https://bugs.openjdk.java.net/browse/JDK-6260652),
+  // but instead returns Object[], so, we cannot use toArray() anymore.
+  // Only array.clone should be used because of performance reasons (https://youtrack.jetbrains.com/issue/IDEA-198172).
   private volatile T[] myExtensionsCacheAsArray;
 
   private final ExtensionsAreaImpl myOwner;
