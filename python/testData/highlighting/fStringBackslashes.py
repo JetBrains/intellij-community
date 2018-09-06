3b109f009259df1e8a59101787e83ff5ebd3e85b
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2018-09-06 14:31:57 +0300

    PY-31442 Disable f-string injections, check that warnings about backslashes are not duplicated

--- python/testData/highlighting/fStringBackslashes.py
+++ python/testData/highlighting/fStringBackslashes.py
@@ -3,3 +3,4 @@ f'{<error descr="expression expected"><error descr="Expression fragments inside 
 f'{<error descr="expression expected"><error descr="Expression fragments inside f-strings cannot include backslashes">\</error>N{GREEK SMALL LETTER ALPHA}</error>}'
 f'{Formatable():\n\t}'
 f'{42:{<error descr="expression expected"><error descr="Expression fragments inside f-strings cannot include backslashes">\</error>t</error>}}'
+f'{f"""{"<error descr="Expression fragments inside f-strings cannot include backslashes">\</error>n"}"""}'
