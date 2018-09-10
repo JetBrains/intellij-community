3a675796d40bd141e2df8cd1ec12ec1c72a2f393
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2018-09-10 22:44:33 +0300

    [file-history] move static methods to FileHistoryUtil class

--- platform/vcs-log/impl/src/com/intellij/vcs/log/history/FileHistoryUi.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/history/FileHistoryUi.java
@@ -24,12 +24,6 @@ import com.intellij.openapi.vcs.changes.Change;
 import com.intellij.openapi.vcs.changes.ContentRevision;
 import com.intellij.openapi.vcs.changes.committed.CommittedChangesTreeBrowser;
 import com.intellij.openapi.vcs.history.VcsFileRevision;
-import com.intellij.openapi.vcs.history.VcsFileRevisionEx;
-import com.intellij.openapi.vcs.history.VcsHistoryUtil;
-import com.intellij.openapi.vcs.vfs.VcsFileSystem;
-import com.intellij.openapi.vcs.vfs.VcsVirtualFile;
-import com.intellij.openapi.vcs.vfs.VcsVirtualFolder;
-import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.JBColor;
 import com.intellij.util.ObjectUtils;
@@ -60,7 +54,6 @@ import java.awt.*;
 import java.util.List;
 import java.util.*;
 
-import static com.intellij.util.ObjectUtils.chooseNotNull;
 import static com.intellij.util.ObjectUtils.notNull;
 
 public class FileHistoryUi extends AbstractVcsLogUi {
@@ -116,27 +109,14 @@ public class FileHistoryUi extends AbstractVcsLogUi {
   @Nullable
   public VirtualFile createVcsVirtualFile(@NotNull VcsFullCommitDetails details) {
     VcsFileRevision revision = createRevision(details);
-    return createVcsVirtualFile(revision);
-  }
-
-  @Nullable
-  public VirtualFile createVcsVirtualFile(@Nullable VcsFileRevision revision) {
-    if (!VcsHistoryUtil.isEmpty(revision)) {
-      if (revision instanceof VcsFileRevisionEx) {
-        FilePath path = ((VcsFileRevisionEx)revision).getPath();
-        return path.isDirectory()
-               ? new VcsVirtualFolder(path.getPath(), null, VcsFileSystem.getInstance())
-               : new VcsVirtualFile(path.getPath(), revision, VcsFileSystem.getInstance());
-      }
-    }
-    return null;
+    return FileHistoryUtil.createVcsVirtualFile(revision);
   }
 
   @Nullable
   public VcsFileRevision createRevision(@Nullable VcsFullCommitDetails details) {
     if (details != null && !(details instanceof LoadingDetails)) {
       List<Change> changes = collectRelevantChanges(details);
-      for (Change change: changes) {
+      for (Change change : changes) {
         ContentRevision revision = change.getAfterRevision();
         if (revision != null) {
           return new VcsLogFileRevision(details, revision, revision.getFile());
@@ -155,7 +135,7 @@ public class FileHistoryUi extends AbstractVcsLogUi {
     if (myPath.isDirectory()) return myPath;
 
     List<Change> changes = collectRelevantChanges(details);
-    for (Change change: changes) {
+    for (Change change : changes) {
       ContentRevision revision = change.getAfterRevision();
       if (revision != null) {
         return revision.getFile();
@@ -168,22 +148,10 @@ public class FileHistoryUi extends AbstractVcsLogUi {
   @NotNull
   public List<Change> collectRelevantChanges(@NotNull VcsFullCommitDetails details) {
     Set<FilePath> fileNames = getFileNames(details);
-    return collectRelevantChanges(details,
-                                  change -> myPath.isDirectory() ? affectsDirectories(change, fileNames) : affectsFiles(change, fileNames));
-  }
-
-  @NotNull
-  private static List<Change> collectRelevantChanges(@NotNull VcsFullCommitDetails details,
-                                                     @NotNull Condition<Change> isRelevant) {
-    List<Change> changes = ContainerUtil.filter(details.getChanges(), isRelevant);
-    if (!changes.isEmpty()) return changes;
-    if (details.getParents().size() > 1) {
-      for (int parent = 0; parent < details.getParents().size(); parent++) {
-        List<Change> changesToParent = ContainerUtil.filter(details.getChanges(parent), isRelevant);
-        if (!changesToParent.isEmpty()) return changesToParent;
-      }
-    }
-    return Collections.emptyList();
+    return FileHistoryUtil.collectRelevantChanges(details,
+                                                  change -> myPath.isDirectory()
+                                                            ? FileHistoryUtil.affectsDirectories(change, fileNames)
+                                                            : FileHistoryUtil.affectsFiles(change, fileNames));
   }
 
   @NotNull
@@ -201,21 +169,11 @@ public class FileHistoryUi extends AbstractVcsLogUi {
     return names;
   }
 
-  private static boolean affectsFiles(@NotNull Change change, @NotNull Set<FilePath> files) {
-    ContentRevision revision = notNull(chooseNotNull(change.getAfterRevision(), change.getBeforeRevision()));
-    return files.contains(revision.getFile());
-  }
-
-  private static boolean affectsDirectories(@NotNull Change change, @NotNull Set<FilePath> directories) {
-    FilePath file = notNull(chooseNotNull(change.getAfterRevision(), change.getBeforeRevision())).getFile();
-    return ContainerUtil.find(directories, dir -> VfsUtilCore.isAncestor(dir.getIOFile(), file.getIOFile(), false)) != null;
-  }
-
-  @NotNull
+  @NotNull                                    
   public List<Change> collectChanges(@NotNull List<VcsFullCommitDetails> detailsList, boolean onlyRelevant) {
     List<Change> changes = ContainerUtil.newArrayList();
     List<VcsFullCommitDetails> detailsListReversed = ContainerUtil.reverse(detailsList);
-    for (VcsFullCommitDetails details: detailsListReversed) {
+    for (VcsFullCommitDetails details : detailsListReversed) {
       changes.addAll(onlyRelevant ? collectRelevantChanges(details) : details.getChanges());
     }
 
