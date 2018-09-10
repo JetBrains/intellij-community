bf848a3610e870762bf3426d5559e96ff5d54812
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2018-09-10 12:55:30 +0300

    fix NPE in StubViewerPsiBasedTree

--- platform/lang-impl/src/com/intellij/internal/psiView/stubtree/StubViewerPsiBasedTree.java
+++ platform/lang-impl/src/com/intellij/internal/psiView/stubtree/StubViewerPsiBasedTree.java
@@ -211,8 +211,10 @@ public class StubViewerPsiBasedTree implements ViewerPsiBasedTree {
       (treeNode) -> treeNode instanceof DefaultMutableTreeNode &&
                     ((StubTreeNode)((DefaultMutableTreeNode)treeNode).getUserObject()).getStub() == stubElement
     );
-    TreePath path = TreePathUtil.pathToTreeNode(node);
-    myStubTree.getSelectionModel().setSelectionPath(path);
+    if (node != null) {
+      TreePath path = TreePathUtil.pathToTreeNode(node);
+      myStubTree.getSelectionModel().setSelectionPath(path);
+    }
   }
 
   private class StubTreeSelectionListener implements TreeSelectionListener {
