f6386ff510edeaebbcc425088e963a66e937c5e9
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2018-09-11 19:23:56 +0300

    vcs: Implement component to show current branch in commit dialog

---
+++ platform/vcs-api/src/com/intellij/vcs/branch/BranchStateProvider.java
@@ -0,0 +1,19 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.vcs.branch;
+
+import com.intellij.openapi.extensions.ExtensionPointName;
+import com.intellij.openapi.vcs.FilePath;
+import org.jetbrains.annotations.CalledInAwt;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public interface BranchStateProvider {
+  ExtensionPointName<BranchStateProvider> EP_NAME = ExtensionPointName.create("com.intellij.vcs.branchStateProvider");
+
+  /**
+   * Returns information about current branch of the given path.
+   */
+  @CalledInAwt
+  @Nullable
+  BranchData getCurrentBranch(@NotNull FilePath path);
+}
--- platform/vcs-log/impl/src/com/intellij/vcs/log/ui/render/LabelPainter.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/ui/render/LabelPainter.java
@@ -1,39 +1,11 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.vcs.log.ui.render;
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 import com.intellij.openapi.ui.GraphicsConfig;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vcs.changes.ui.CurrentBranchComponent;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.ColorUtil;
 import com.intellij.ui.JBColor;
@@ -74,9 +46,9 @@ public class LabelPainter {
   private static final String THREE_DOTS = "...";
   private static final String TWO_DOTS = "..";
   private static final String SEPARATOR = "/";
-  @SuppressWarnings("UseJBColor") private static final JBColor BACKGROUND = new JBColor(Color.BLACK, Color.WHITE);
-  private static final float BALANCE = 0.08f;
-  private static final JBColor TEXT_COLOR = new JBColor(new Color(0x7a7a7a), new Color(0x909090));
+  private static final JBColor BACKGROUND = CurrentBranchComponent.BACKGROUND;
+  private static final double BALANCE = CurrentBranchComponent.BALANCE;
+  private static final JBColor TEXT_COLOR = CurrentBranchComponent.TEXT_COLOR;
 
   @NotNull private final VcsLogData myLogData;
   @NotNull private final JComponent myComponent;
