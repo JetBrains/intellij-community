e90779759497f5b11ecaff794ae852bd3a05df7f
Nikita Skvortsov <nikita.skvortsov@jetbrains.com>
2018-09-11 16:45:39 +0300

    import list of Maven repositories available in the Gradle project

---
+++ plugins/gradle/java/src/service/project/MavenRepositoriesProjectResolver.kt
@@ -0,0 +1,50 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.plugins.gradle.service.project
+
+import com.intellij.externalSystem.MavenRepositoryData
+import com.intellij.openapi.externalSystem.model.DataNode
+import com.intellij.openapi.externalSystem.model.ProjectKeys
+import com.intellij.openapi.externalSystem.model.project.ModuleData
+import com.intellij.openapi.externalSystem.model.project.ProjectData
+import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
+import org.gradle.tooling.model.idea.IdeaModule
+import org.gradle.tooling.model.idea.IdeaProject
+import org.jetbrains.plugins.gradle.model.RepositoriesModel
+import org.jetbrains.plugins.gradle.util.GradleConstants
+
+
+class MavenRepositoriesProjectResolver: AbstractProjectResolverExtension() {
+  override fun populateProjectExtraModels(gradleProject: IdeaProject, ideProject: DataNode<ProjectData>) {
+    val repositories = resolverCtx.getExtraProject(RepositoriesModel::class.java)
+    addRepositoriesToProject(ideProject, repositories)
+
+    super.populateProjectExtraModels(gradleProject, ideProject)
+  }
+
+  override fun populateModuleExtraModels(gradleModule: IdeaModule, ideModule: DataNode<ModuleData>) {
+    val repositories = resolverCtx.getExtraProject(gradleModule, RepositoriesModel::class.java)
+    val ideProject = ExternalSystemApiUtil.findParent(ideModule, ProjectKeys.PROJECT)
+    ideProject?.let { addRepositoriesToProject(it, repositories) }
+
+    super.populateModuleExtraModels(gradleModule, ideModule)
+  }
+
+  override fun getExtraProjectModelClasses(): MutableSet<Class<*>> {
+    return mutableSetOf(RepositoriesModel::class.java)
+  }
+
+  private fun addRepositoriesToProject(ideProject: DataNode<ProjectData>,
+                                       repositories: RepositoriesModel?) {
+    if (repositories != null) {
+      val knownRepositories = ExternalSystemApiUtil.getChildren(ideProject, MavenRepositoryData.KEY)
+        .asSequence()
+        .map { it.data }
+        .toSet()
+
+      repositories.all.asSequence()
+        .map { it -> MavenRepositoryData(GradleConstants.SYSTEM_ID, it.name, it.url) }
+        .filter { it -> !knownRepositories.contains(it) }
+        .forEach { it -> ideProject.addChild(DataNode(MavenRepositoryData.KEY, it, ideProject)) }
+    }
+  }
+}
