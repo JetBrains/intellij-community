42d5b577112d1333f701bc77c5779ef565a778f4
Aleksey Pivovarov <AMPivovarov@gmail.com>
2018-09-12 19:30:11 +0300

    vcs: extract panel to show CommittedChangeList

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/ChangeListViewerDialog.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/ChangeListViewerDialog.java
@@ -3,62 +3,29 @@
 package com.intellij.openapi.vcs.changes.ui;
 
 import com.intellij.CommonBundle;
-import com.intellij.openapi.actionSystem.ActionManager;
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.DataProvider;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
-import com.intellij.openapi.ui.Splitter;
-import com.intellij.openapi.vcs.AbstractVcs;
-import com.intellij.openapi.vcs.VcsActions;
 import com.intellij.openapi.vcs.VcsBundle;
-import com.intellij.openapi.vcs.VcsDataKeys;
 import com.intellij.openapi.vcs.changes.Change;
-import com.intellij.openapi.vcs.changes.ChangeList;
-import com.intellij.openapi.vcs.changes.committed.CommittedChangesBrowser;
 import com.intellij.openapi.vcs.changes.committed.CommittedChangesBrowserUseCase;
-import com.intellij.openapi.vcs.changes.issueLinks.IssueLinkHtmlRenderer;
 import com.intellij.openapi.vcs.versionBrowser.CommittedChangeList;
-import com.intellij.openapi.vcs.versionBrowser.CommittedChangeListImpl;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.ui.BrowserHyperlinkListener;
-import com.intellij.ui.ScrollPaneFactory;
-import com.intellij.ui.SeparatorFactory;
-import com.intellij.util.containers.ContainerUtil;
-import com.intellij.util.ui.JBUI;
-import com.intellij.util.ui.UIUtil;
-import com.intellij.xml.util.XmlStringUtil;
-import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.Collection;
-import java.util.Date;
-import java.util.List;
 
-/**
- * @author max
- */
-public class ChangeListViewerDialog extends DialogWrapper implements DataProvider {
-  private Project myProject;
-  private CommittedChangeList myChangeList;
-  private CommittedChangesBrowser myChangesBrowser;
-  private JEditorPane myCommitMessageArea;
-  private Change[] myChanges;
-  private JScrollPane commitMessageScroll;
-  private VirtualFile myToSelect;
+public class ChangeListViewerDialog extends DialogWrapper {
+  private final CommittedChangeListPanel myChangesPanel;
 
   public ChangeListViewerDialog(@NotNull Project project, @NotNull CommittedChangeList changeList) {
     this(project, changeList, null);
   }
 
   public ChangeListViewerDialog(@NotNull Project project, @NotNull CommittedChangeList changeList, @Nullable VirtualFile toSelect) {
-    super(project, true);
-    myToSelect = toSelect;
-    initCommitMessageArea(project, changeList);
-    initDialog(project, changeList);
+    this(project, null, changeList, toSelect);
   }
 
   public ChangeListViewerDialog(@NotNull Project project, @NotNull Collection<Change> changes) {
@@ -66,15 +33,19 @@ public class ChangeListViewerDialog extends DialogWrapper implements DataProvide
   }
 
   public ChangeListViewerDialog(Component parent, @NotNull Project project, @NotNull Collection<Change> changes) {
-    super(parent, true);
-    initDialog(project, new CommittedChangeListImpl("", "", "", -1, new Date(0), changes));
+    this(project, parent, CommittedChangeListPanel.createChangeList(changes), null);
+    myChangesPanel.setShowCommitMessage(false);
   }
 
-  private void initDialog(final Project project, final CommittedChangeList changeList) {
-    myProject = project;
-    myChangeList = changeList;
-    final Collection<Change> changes = myChangeList.getChanges();
-    myChanges = changes.toArray(new Change[0]);
+  private ChangeListViewerDialog(@NotNull Project project,
+                                 @Nullable Component parentComponent,
+                                 @NotNull CommittedChangeList changeList,
+                                 @Nullable VirtualFile toSelect) {
+    super(project, parentComponent, true, IdeModalityType.IDE);
+
+    myChangesPanel = new CommittedChangeListPanel(project);
+    myChangesPanel.setChangeList(changeList, toSelect);
+    myChangesPanel.setDescription(getDescription());
 
     setTitle(VcsBundle.message("dialog.title.changes.browser"));
     setCancelButtonText(CommonBundle.message("close.action.name"));
@@ -83,23 +54,11 @@ public class ChangeListViewerDialog extends DialogWrapper implements DataProvide
     init();
   }
 
-  private void initCommitMessageArea(final Project project, final CommittedChangeList changeList) {
-    myCommitMessageArea = new JEditorPane(UIUtil.HTML_MIME, "");
-    myCommitMessageArea.setBorder(JBUI.Borders.empty(3));
-    myCommitMessageArea.setEditable(false);
-    @NonNls final String text = IssueLinkHtmlRenderer.formatTextIntoHtml(project, changeList.getComment().trim());
-    myCommitMessageArea.setBackground(UIUtil.getComboBoxDisabledBackground());
-    myCommitMessageArea.addHyperlinkListener(BrowserHyperlinkListener.INSTANCE);
-    commitMessageScroll = ScrollPaneFactory.createScrollPane(myCommitMessageArea);
-    myCommitMessageArea.setText(text);
-    myCommitMessageArea.setCaretPosition(0);
-  }
-
   /**
    * @param inAir true if changes are not related to known VCS roots (ex: local changes, file history, etc)
    */
   public void markChangesInAir(boolean inAir) {
-    myChangesBrowser.setUseCase(inAir ? CommittedChangesBrowserUseCase.IN_AIR : null);
+    myChangesPanel.getChangesBrowser().setUseCase(inAir ? CommittedChangesBrowserUseCase.IN_AIR : null);
   }
 
 
@@ -109,49 +68,8 @@ public class ChangeListViewerDialog extends DialogWrapper implements DataProvide
   }
 
   @Override
-  public Object getData(@NotNull @NonNls final String dataId) {
-    if (VcsDataKeys.CHANGES.is(dataId)) {
-      return myChanges;
-    }
-    if (VcsDataKeys.VCS.is(dataId)) {
-      AbstractVcs vcs = myChangeList.getVcs();
-      return vcs == null ? null : vcs.getKeyInstanceMethod();
-    }
-    if (VcsDataKeys.CHANGE_LISTS.is(dataId)) {
-      return new ChangeList[]{myChangeList};
-    }
-    return null;
-  }
-
-  @Override
   public JComponent createCenterPanel() {
-    final JPanel mainPanel = new JPanel();
-    mainPanel.setLayout(new BorderLayout());
-    final Splitter splitter = new Splitter(true, 0.8f);
-    myChangesBrowser = new MyChangesBrowser(myProject);
-    myChangesBrowser.setChangesToDisplay(myChangeList.getChanges());
-    if (myToSelect != null) myChangesBrowser.getViewer().selectFile(myToSelect);
-
-    splitter.setFirstComponent(myChangesBrowser);
-
-    if (myCommitMessageArea != null) {
-      JPanel commitPanel = new JPanel(new BorderLayout());
-      JComponent separator = SeparatorFactory.createSeparator(VcsBundle.message("label.commit.comment"), myCommitMessageArea);
-      commitPanel.add(separator, BorderLayout.NORTH);
-      commitPanel.add(commitMessageScroll, BorderLayout.CENTER);
-
-      splitter.setSecondComponent(commitPanel);
-    }
-    mainPanel.add(splitter, BorderLayout.CENTER);
-
-    final String description = getDescription();
-    if (description != null) {
-      JPanel descPanel = new JPanel();
-      descPanel.add(new JLabel(XmlStringUtil.wrapInHtml(description)));
-      descPanel.setBorder(BorderFactory.createEtchedBorder());
-      mainPanel.add(descPanel, BorderLayout.NORTH);
-    }
-    return mainPanel;
+    return myChangesPanel;
   }
 
   @NotNull
@@ -159,33 +77,19 @@ public class ChangeListViewerDialog extends DialogWrapper implements DataProvide
   protected Action[] createActions() {
     Action cancelAction = getCancelAction();
     cancelAction.putValue(DEFAULT_ACTION, Boolean.TRUE);
-    return new Action[] {cancelAction};
+    return new Action[]{cancelAction};
   }
 
   @Override
   public JComponent getPreferredFocusedComponent() {
-    return myChangesBrowser.getPreferredFocusedComponent();
+    return myChangesPanel.getPreferredFocusedComponent();
   }
 
   /**
    * @return description that is added to the top of this dialog. May be null - then no description is shown.
    */
-  protected @Nullable String getDescription() {
+  @Nullable
+  protected String getDescription() {
     return null;
-  }
-
-  private static class MyChangesBrowser extends CommittedChangesBrowser {
-    MyChangesBrowser(@NotNull Project project) {
-      super(project);
-    }
-
-    @NotNull
-    @Override
-    protected List<AnAction> createPopupMenuActions() {
-      return ContainerUtil.append(
-        super.createPopupMenuActions(),
-        ActionManager.getInstance().getAction(VcsActions.ACTION_COPY_REVISION_NUMBER)
-      );
-    }
   }
 }
