c75f5c1b086f6721fcf2886cbebcf67b56d563e8
Anton Lobov <Anton.Lobov@jetbrains.com>
2018-09-13 10:41:48 +0200

    WEB-10307 Multiline TODOs for CSS/LESS/SCSS/Stylus/PostCSS

--- platform/testFramework/src/com/intellij/editor/TodoItemsTestCase.java
+++ platform/testFramework/src/com/intellij/editor/TodoItemsTestCase.java
@@ -1,22 +1,53 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.codeInsight.daemon.todo;
+package com.intellij.editor;
 
-import com.intellij.codeInsight.daemon.LightDaemonAnalyzerTestCase;
+import com.intellij.codeHighlighting.Pass;
 import com.intellij.codeInsight.daemon.impl.HighlightInfo;
 import com.intellij.codeInsight.daemon.impl.HighlightInfoType;
+import com.intellij.injected.editor.EditorWindow;
+import com.intellij.lang.injection.InjectedLanguageManager;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.impl.DocumentImpl;
 import com.intellij.openapi.util.Segment;
 import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiFile;
 import com.intellij.testFramework.EditorTestUtil;
+import com.intellij.testFramework.LightPlatformCodeInsightTestCase;
+import com.intellij.testFramework.fixtures.impl.CodeInsightTestFixtureImpl;
+import com.intellij.util.ArrayUtil;
+import gnu.trove.TIntArrayList;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public abstract class TodoItemsTestCase extends LightDaemonAnalyzerTestCase {
+public abstract class TodoItemsTestCase extends LightPlatformCodeInsightTestCase {
   protected abstract String getFileExtension();
+
+  @NotNull
+  protected List<HighlightInfo> doHighlighting() {
+    PsiDocumentManager.getInstance(getProject()).commitAllDocuments();
+
+    TIntArrayList toIgnoreList = new TIntArrayList();
+
+    toIgnoreList.add(Pass.UPDATE_FOLDING);
+
+    toIgnoreList.add(Pass.LOCAL_INSPECTIONS);
+    toIgnoreList.add(Pass.WHOLE_FILE_LOCAL_INSPECTIONS);
+
+    int[] toIgnore = toIgnoreList.isEmpty() ? ArrayUtil.EMPTY_INT_ARRAY : toIgnoreList.toNativeArray();
+    Editor editor = getEditor();
+    PsiFile file = getFile();
+    if (editor instanceof EditorWindow) {
+      editor = ((EditorWindow)editor).getDelegate();
+      file = InjectedLanguageManager.getInstance(file.getProject()).getTopLevelFile(file);
+    }
+    return CodeInsightTestFixtureImpl.instantiateAndRun(file, editor, toIgnore, false);
+  }
 
   protected void testTodos(String text) {
     configureFromFileText("Foo." + getFileExtension(), text);
@@ -75,8 +106,44 @@ public abstract class TodoItemsTestCase extends LightDaemonAnalyzerTestCase {
     return b.toString();
   }
 
-  @Override
-  protected boolean doInspections() {
-    return false;
+  protected abstract boolean supportsCStyleSingleLineComments();
+  protected abstract boolean supportsCStyleMultiLineComments();
+
+  public void testSuccessiveLineComments() {
+    if (!supportsCStyleSingleLineComments()) return;
+    testTodos("// [TODO first line]\n" +
+              "//      [second line]");
+  }
+
+  public void testSuccessiveLineCommentsAfterEditing() {
+    if (!supportsCStyleSingleLineComments()) return;
+    testTodos("// [TODO first line]\n" +
+              "// <caret>second line");
+    type("     ");
+    checkTodos("// [TODO first line]\n" +
+               "//      [second line]");
+  }
+
+  public void testAllLinesLoseHighlightingWithFirstLine() {
+    if (!supportsCStyleSingleLineComments()) return;
+    testTodos("// [TO<caret>DO first line]\n" +
+              "//      [second line]");
+    delete();
+    checkTodos("// TOO first line\n" +
+               "//      second line");
+  }
+
+  public void testContinuationIsNotOverlappedWithFollowingTodo() {
+    if (!supportsCStyleSingleLineComments()) return;
+    testTodos("// [TODO first line]\n" +
+              "//  [TODO second line]");
+  }
+
+  public void testContinuationInBlockCommentWithStars() {
+    if (!supportsCStyleMultiLineComments()) return;
+    testTodos("/*\n" +
+              " * [TODO first line]\n" +
+              " *  [second line]\n" +
+              " */");
   }
 }
