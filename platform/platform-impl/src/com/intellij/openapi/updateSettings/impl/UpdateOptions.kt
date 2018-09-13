262db4a4e3cd0f2aa5314216139b8d93f1e31c2e
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-09-13 09:10:49 +0300

    IDEA-CR-36785 move to a new file UpdateSettingsProvider

--- platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/UpdateOptions.kt
+++ platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/UpdateOptions.kt
@@ -2,15 +2,8 @@
 package com.intellij.openapi.updateSettings.impl
 
 import com.intellij.openapi.components.BaseState
-import com.intellij.openapi.diagnostic.logger
-import com.intellij.openapi.diagnostic.runAndLogException
-import com.intellij.openapi.extensions.ProjectExtensionPointName
-import com.intellij.openapi.project.ProjectManager
 import com.intellij.util.xmlb.annotations.CollectionBean
 import com.intellij.util.xmlb.annotations.OptionTag
-import org.jetbrains.annotations.ApiStatus
-
-private val LOG = logger<UpdateOptions>()
 
 class UpdateOptions : BaseState() {
   @get:CollectionBean
@@ -45,26 +38,4 @@ class UpdateOptions : BaseState() {
 
   @get:OptionTag("THIRD_PARTY_PLUGINS_ALLOWED")
   var isThirdPartyPluginsAllowed by property(false)
-}
-
-@ApiStatus.Experimental
-private val UPDATE_SETTINGS_PROVIDER_EP = ProjectExtensionPointName<UpdateSettingsProvider>("com.intellij.updateSettingsProvider")
-
-internal fun addPluginRepositories(to: MutableList<String>) {
-  for (project in ProjectManager.getInstance().openProjects) {
-    if (!project.isInitialized || project.isDisposed) {
-      continue
-    }
-    
-    for (provider in UPDATE_SETTINGS_PROVIDER_EP.getExtensions(project)) {
-      LOG.runAndLogException {
-        to.addAll(provider.getPluginRepositories())
-      }
-    }
-  }
-}
-
-@ApiStatus.Experimental
-interface UpdateSettingsProvider {
-  fun getPluginRepositories(): List<String>
 }
