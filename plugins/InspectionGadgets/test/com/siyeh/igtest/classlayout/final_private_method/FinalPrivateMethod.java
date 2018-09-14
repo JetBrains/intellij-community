59b33bd882158ab81f32c6ac3bbb1ce4b434375a
Bas Leijdekkers <basleijdekkers@gmail.com>
2018-09-14 17:15:18 +0200

    IG: warn about private final methods that have @SafeVarargs under java 9

--- plugins/InspectionGadgets/test/com/siyeh/igtest/classlayout/final_private_method/FinalPrivateMethod.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/classlayout/final_private_method/FinalPrivateMethod.java
@@ -5,7 +5,7 @@ public class FinalPrivateMethod {
   private <warning descr="'private' method declared 'final'">final</warning> void foo() {};
 
   <error descr="@SafeVarargs is not allowed on methods with fixed arity">@java.lang.SafeVarargs</error>
-  private <warning descr="'private' method declared 'final'">final</warning> void foo(String s) {}
+  private final void foo(String s) {}
 
   @java.lang.SafeVarargs
   private final <T> void foo(T... i) {}
