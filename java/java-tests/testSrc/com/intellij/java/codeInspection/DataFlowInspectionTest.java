65fb8e8c77261b2aa60c2972cbc5f5239cba0f72
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2018-09-16 12:56:43 +0700

    MaxLoop test

--- java/java-tests/testSrc/com/intellij/java/codeInspection/DataFlowInspectionTest.java
+++ java/java-tests/testSrc/com/intellij/java/codeInspection/DataFlowInspectionTest.java
@@ -646,4 +646,5 @@ public class DataFlowInspectionTest extends DataFlowInspectionTestCase {
 
   public void testIncompleteArrayAccessInLoop() { doTest(); }
   public void testSameArguments() { doTest(); }
+  public void testMaxLoop() { doTest(); }
 }
