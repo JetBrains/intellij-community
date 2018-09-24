4b74d87090985d6a38bdda993afe2a3ddccece25
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2018-09-24 17:37:31 +0300

    restore bug compatibility

--- java/java-analysis-impl/src/com/intellij/codeInspection/reference/RefJavaUtilImpl.java
+++ java/java-analysis-impl/src/com/intellij/codeInspection/reference/RefJavaUtilImpl.java
@@ -667,7 +667,7 @@ public class RefJavaUtilImpl extends RefJavaUtil {
     UElement parent = skipParenthesises(expression);
     return !(parent instanceof UBinaryExpression) ||
            !(((UBinaryExpression)parent).getOperator() instanceof UastBinaryOperator.AssignOperator) ||
-           UastUtils.isChildOf(((UBinaryExpression)parent).getRightOperand(), expression, false);
+           UastUtils.isUastChildOf(((UBinaryExpression)parent).getRightOperand(), expression, false);
   }
 
   private static boolean isOnAssignmentLeftHand(@NotNull UElement expression) {
@@ -675,7 +675,7 @@ public class RefJavaUtilImpl extends RefJavaUtil {
     if (parent == null) return false;
     return parent instanceof UBinaryExpression
            && ((UBinaryExpression)parent).getOperator() instanceof UastBinaryOperator.AssignOperator
-           && UastUtils.isChildOf(expression, ((UBinaryExpression)parent).getLeftOperand(), false);
+           && UastUtils.isUastChildOf(expression, ((UBinaryExpression)parent).getLeftOperand(), false);
   }
 
   private static UElement skipParenthesises(@NotNull UElement expression) {
