99614a63425774df58eea3ac92e2b20af1633663
Anna.Kozlova <anna.kozlova@jetbrains.com>
2018-09-25 11:50:43 +0200

    junit: fix array pattern

--- plugins/junit_rt/src/com/intellij/junit4/ExpectedPatterns.java
+++ plugins/junit_rt/src/com/intellij/junit4/ExpectedPatterns.java
@@ -31,7 +31,7 @@ public class ExpectedPatterns extends AbstractExpectedPatterns {
     "expected same:<(.*)> was not:<(.*)>",
     "\nexpected: \"(.*)\"\n\\s*but: was \"(.*)\"",
     "expected: (.*)\n\\s*but: was (.*)",
-    "expected: (.*)\n\\s*but was: (.*)",
+    "expected: (.*)\\s*but was: (.*)",
     "expecting:\\s*<(.*)> to be equal to:\\s*<(.*)>\\s*but was not"
   };
 
