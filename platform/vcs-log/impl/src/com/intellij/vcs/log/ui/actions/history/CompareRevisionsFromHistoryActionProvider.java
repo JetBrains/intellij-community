36810812f3d9e53c61bf433409a1b2a01a31edce
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2018-09-25 20:00:18 +0300

    [file-history] split compare action into two: for files and for folders

--- platform/vcs-log/impl/src/com/intellij/vcs/log/ui/actions/history/CompareRevisionsFromHistoryActionProvider.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/ui/actions/history/CompareRevisionsFromHistoryActionProvider.java
@@ -23,18 +23,19 @@ import com.intellij.openapi.ui.MessageType;
 import com.intellij.openapi.vcs.FilePath;
 import com.intellij.openapi.vcs.VcsDataKeys;
 import com.intellij.openapi.vcs.changes.Change;
-import com.intellij.openapi.vcs.changes.actions.diff.ShowDiffAction;
 import com.intellij.openapi.vcs.history.VcsDiffUtil;
 import com.intellij.openapi.vcs.ui.VcsBalloonProblemNotifier;
 import com.intellij.util.containers.ContainerUtil;
-import com.intellij.vcs.log.*;
+import com.intellij.vcs.log.CommitId;
+import com.intellij.vcs.log.VcsFullCommitDetails;
+import com.intellij.vcs.log.VcsLog;
+import com.intellij.vcs.log.VcsLogDiffHandler;
 import com.intellij.vcs.log.history.FileHistoryUi;
 import com.intellij.vcs.log.statistics.VcsLogUsageTriggerCollector;
 import com.intellij.vcs.log.ui.VcsLogInternalDataKeys;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.event.KeyEvent;
-import java.util.Collections;
 import java.util.List;
 
 import static com.intellij.util.ObjectUtils.notNull;
@@ -47,7 +48,8 @@ public class CompareRevisionsFromHistoryActionProvider implements AnActionExtens
 
   @Override
   public boolean isActive(@NotNull AnActionEvent e) {
-    return e.getData(VcsLogInternalDataKeys.FILE_HISTORY_UI) != null;
+    FilePath filePath = e.getData(VcsDataKeys.FILE_PATH);
+    return e.getData(VcsLogInternalDataKeys.FILE_HISTORY_UI) != null && filePath != null && filePath.isDirectory();
   }
 
   @Override
@@ -59,29 +61,22 @@ public class CompareRevisionsFromHistoryActionProvider implements AnActionExtens
       e.getPresentation().setEnabledAndVisible(false);
       return;
     }
-
     e.getPresentation().setVisible(true);
 
-    List<CommitId> commits = ui.getVcsLog().getSelectedCommits();
+    VcsLog log = ui.getVcsLog();
+    updateActionText(e, log);
+
     if (e.getInputEvent() instanceof KeyEvent) {
       e.getPresentation().setEnabled(true);
-    }
-    else {
-      if (commits.size() == 2) {
-        e.getPresentation().setEnabled(e.getData(VcsLogInternalDataKeys.LOG_DIFF_HANDLER) != null);
-      }
-      else {
-        e.getPresentation().setEnabled(commits.size() == 1);
-      }
+      return;
     }
 
+    List<CommitId> commits = log.getSelectedCommits();
     if (commits.size() == 2) {
-      e.getPresentation().setText(COMPARE_TEXT);
-      e.getPresentation().setDescription(COMPARE_DESCRIPTION);
+      e.getPresentation().setEnabled(e.getData(VcsLogInternalDataKeys.LOG_DIFF_HANDLER) != null);
     }
     else {
-      e.getPresentation().setText(DIFF_TEXT);
-      e.getPresentation().setDescription(DIFF_DESCRIPTION);
+      e.getPresentation().setEnabled(commits.size() == 1);
     }
   }
 
@@ -108,18 +103,24 @@ public class CompareRevisionsFromHistoryActionProvider implements AnActionExtens
 
     if (commits.size() != 1) return;
 
-    if (!filePath.isDirectory()) {
-      ShowDiffAction.showDiffForChange(project, Collections.singletonList(ui.getSelectedChange()));
+    List<Integer> commitIds = ContainerUtil.map(commits, c -> ui.getLogData().getCommitIndex(c.getHash(), c.getRoot()));
+    ui.getLogData().getCommitDetailsGetter().loadCommitsData(commitIds, details -> {
+      VcsFullCommitDetails detail = notNull(ContainerUtil.getFirstItem(details));
+      List<Change> changes = ui.collectRelevantChanges(detail);
+      VcsDiffUtil.showChangesDialog(project, "Changes in " + detail.getId().toShortString() + " for " + filePath.getName(),
+                                    ContainerUtil.newArrayList(changes));
+    }, t -> VcsBalloonProblemNotifier.showOverChangesView(project, "Could not load selected commits: " + t.getMessage(),
+                                                          MessageType.ERROR), null);
+  }
+
+  public static void updateActionText(@NotNull AnActionEvent e, @NotNull VcsLog log) {
+    if (log.getSelectedCommits().size() >= 2) {
+      e.getPresentation().setText(COMPARE_TEXT);
+      e.getPresentation().setDescription(COMPARE_DESCRIPTION);
     }
     else {
-      List<Integer> commitIds = ContainerUtil.map(commits, c -> ui.getLogData().getCommitIndex(c.getHash(), c.getRoot()));
-      ui.getLogData().getCommitDetailsGetter().loadCommitsData(commitIds, details -> {
-        VcsFullCommitDetails detail = notNull(ContainerUtil.getFirstItem(details));
-        List<Change> changes = ui.collectRelevantChanges(detail);
-        VcsDiffUtil.showChangesDialog(project, "Changes in " + detail.getId().toShortString() + " for " + filePath.getName(),
-                                      ContainerUtil.newArrayList(changes));
-      }, t -> VcsBalloonProblemNotifier.showOverChangesView(project, "Could not load selected commits: " + t.getMessage(),
-                                                            MessageType.ERROR), null);
+      e.getPresentation().setText(DIFF_TEXT);
+      e.getPresentation().setDescription(DIFF_DESCRIPTION);
     }
   }
 }
