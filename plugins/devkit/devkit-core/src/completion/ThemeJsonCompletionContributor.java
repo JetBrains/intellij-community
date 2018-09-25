3f37e02b9530e35751c95f2190e024107ddb673f
Yaroslav Pankratyev <yaroslav.pankratyev@jetbrains.com>
2018-09-25 12:31:57 +0700

    IDEA-199415 Prototype of UIDefaults keys completion in IntelliJ theme files

---
+++ plugins/devkit/devkit-core/src/completion/ThemeJsonCompletionContributor.java
@@ -0,0 +1,107 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.idea.devkit.completion;
+
+import com.google.common.collect.Lists;
+import com.intellij.codeInsight.completion.CompletionContributor;
+import com.intellij.codeInsight.completion.CompletionParameters;
+import com.intellij.codeInsight.completion.CompletionResultSet;
+import com.intellij.codeInsight.completion.CompletionType;
+import com.intellij.codeInsight.lookup.LookupElement;
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.json.psi.JsonFile;
+import com.intellij.json.psi.JsonProperty;
+import com.intellij.json.psi.JsonReferenceExpression;
+import com.intellij.json.psi.JsonStringLiteral;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.impl.source.tree.LeafPsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.idea.devkit.themes.ThemeJsonSchemaProviderFactory;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * Completion in IntelliJ theme files.
+ */
+public class ThemeJsonCompletionContributor extends CompletionContributor {
+  @Override
+  public void fillCompletionVariants(@NotNull CompletionParameters parameters, @NotNull CompletionResultSet result) {
+    if (!isThemeJsonFile(parameters.getOriginalFile())) return;
+
+    PsiElement position = parameters.getPosition();
+    if (!(position instanceof LeafPsiElement)) return;
+
+    PsiElement parent = position.getParent();
+    if (parent instanceof JsonStringLiteral || parent instanceof JsonReferenceExpression) {
+      PsiElement parentParent = parent.getParent();
+      if (!(parentParent instanceof JsonProperty)) return;
+      handleJsonProperty(position, (JsonProperty)parentParent, parameters, result);
+    }
+    else if (parent instanceof JsonProperty) {
+      handleJsonProperty(position, (JsonProperty)parent, parameters, result);
+    }
+  }
+
+  private static boolean isThemeJsonFile(@Nullable PsiFile file) {
+    return file instanceof JsonFile && ThemeJsonSchemaProviderFactory.isAllowedFileName(file.getName());
+  }
+
+  private static void handleJsonProperty(@NotNull PsiElement element,
+                                         @NotNull JsonProperty property,
+                                         @NotNull CompletionParameters parameters,
+                                         @NotNull CompletionResultSet result) {
+    if (parameters.getCompletionType() != CompletionType.BASIC) return;
+    if (!isInsideUiProperty(property)) return;
+
+    PsiElement sibling;
+    while ((sibling = element.getPrevSibling()) != null) {
+      if (":".equals(sibling.getText())) return; // no completion for values
+    }
+
+    List<JsonProperty> parentProperties = PsiTreeUtil.collectParents(property, JsonProperty.class, false, e -> {
+      //TODO check that it is TOP-LEVEL 'ui'  property
+      return e instanceof JsonProperty && "ui".equals(((JsonProperty)e).getName());
+    });
+
+    String presentNamePart = Lists.reverse(parentProperties).stream()
+      .map(p -> p.getName())
+      .collect(Collectors.joining("."));
+
+    Iterable<LookupElement> lookupElements = getLookupElements(presentNamePart);
+    result.addAllElements(lookupElements);
+  }
+
+  private static boolean isInsideUiProperty(@NotNull JsonProperty property) {
+    PsiElement parent = property;
+    while ((parent = parent.getParent()) != null) {
+      if (!(parent instanceof JsonProperty)) continue;
+      if ("ui".equals(((JsonProperty)parent).getName())) return true;
+    }
+    return false;
+  }
+
+  private static Iterable<LookupElement> getLookupElements(@NotNull String presentNamePart) {
+    Predicate<String> conditionFilter;
+    Function<String, String> mapFunction;
+    if (presentNamePart.startsWith("*")) {
+      //TODO darcula etc. should be proposed after *?
+      String tail = presentNamePart.substring(1);
+      conditionFilter = key -> key.endsWith(tail);
+      mapFunction = key -> key.substring(presentNamePart.length() - 1); // + 1 for dot, - 2 for '*.'
+    }
+    else {
+      conditionFilter = key -> key.startsWith(presentNamePart);
+      mapFunction = key -> key.substring(presentNamePart.length() + 1); // + 1 for dot
+    }
+    return UiDefaultsHardcodedKeys.UI_DEFAULTS_KEYS.stream()
+      .filter(conditionFilter)
+      .map(mapFunction)
+      .map(key -> LookupElementBuilder.create(key))
+      .collect(Collectors.toSet());
+  }
+}
