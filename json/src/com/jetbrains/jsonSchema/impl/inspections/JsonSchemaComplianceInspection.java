eb5db5342f241c13ccaf631670a9bbbe90b884cf
Anton Lobov <Anton.Lobov@jetbrains.com>
2018-09-26 10:11:57 +0200

    WEB-24726 JSON Schema: highlight unresolved $ref references

--- json/src/com/jetbrains/jsonSchema/impl/inspections/JsonSchemaComplianceInspection.java
+++ json/src/com/jetbrains/jsonSchema/impl/inspections/JsonSchemaComplianceInspection.java
@@ -1,28 +1,25 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.jetbrains.jsonSchema.impl;
+package com.jetbrains.jsonSchema.impl.inspections;
 
-import com.intellij.codeHighlighting.HighlightDisplayLevel;
-import com.intellij.codeInspection.LocalInspectionTool;
 import com.intellij.codeInspection.LocalInspectionToolSession;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.codeInspection.ui.MultipleCheckboxOptionsPanel;
 import com.intellij.json.JsonBundle;
 import com.intellij.json.psi.JsonElementVisitor;
-import com.intellij.json.psi.JsonFile;
 import com.intellij.json.psi.JsonValue;
-import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
-import com.intellij.psi.PsiFile;
-import com.intellij.util.ObjectUtils;
 import com.jetbrains.jsonSchema.extension.JsonLikePsiWalker;
 import com.jetbrains.jsonSchema.ide.JsonSchemaService;
+import com.jetbrains.jsonSchema.impl.JsonComplianceCheckerOptions;
+import com.jetbrains.jsonSchema.impl.JsonSchemaComplianceChecker;
+import com.jetbrains.jsonSchema.impl.JsonSchemaObject;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 
-public class JsonSchemaComplianceInspection extends LocalInspectionTool {
+public class JsonSchemaComplianceInspection extends JsonSchemaBasedInspectionBase {
   public boolean myCaseInsensitiveEnum = false;
 
   @Override
@@ -31,24 +28,11 @@ public class JsonSchemaComplianceInspection extends LocalInspectionTool {
     return JsonBundle.message("json.schema.inspection.compliance.name");
   }
 
-  @NotNull
   @Override
-  public HighlightDisplayLevel getDefaultLevel() {
-    return HighlightDisplayLevel.WARNING;
-  }
-
-  @NotNull
-  @Override
-  public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly, @NotNull LocalInspectionToolSession session) {
-    PsiFile file = holder.getFile();
-    JsonValue root = file instanceof JsonFile ? ObjectUtils.tryCast(file.getFirstChild(), JsonValue.class) : null;
-    if (root == null) return PsiElementVisitor.EMPTY_VISITOR;
-
-    JsonSchemaService service = JsonSchemaService.Impl.get(file.getProject());
-    VirtualFile virtualFile = file.getViewProvider().getVirtualFile();
-    if (!service.isApplicableToFile(virtualFile)) return PsiElementVisitor.EMPTY_VISITOR;
-    final JsonSchemaObject rootSchema = service.getSchemaObject(virtualFile);
-    if (rootSchema == null) return PsiElementVisitor.EMPTY_VISITOR;
+  protected PsiElementVisitor doBuildVisitor(@NotNull JsonValue root, @Nullable JsonSchemaObject schema, @NotNull JsonSchemaService service,
+                                             @NotNull ProblemsHolder holder,
+                                             @NotNull LocalInspectionToolSession session) {
+    if (schema == null) return PsiElementVisitor.EMPTY_VISITOR;
     JsonComplianceCheckerOptions options = new JsonComplianceCheckerOptions(myCaseInsensitiveEnum);
 
     return new JsonElementVisitor() {
@@ -56,7 +40,7 @@ public class JsonSchemaComplianceInspection extends LocalInspectionTool {
       public void visitElement(PsiElement element) {
         if (element == root) {
           // perform this only for the root element, because the checker traverses the hierarchy itself
-          annotate(element, rootSchema, holder, session, options);
+          annotate(element, schema, holder, session, options);
         }
         super.visitElement(element);
       }
