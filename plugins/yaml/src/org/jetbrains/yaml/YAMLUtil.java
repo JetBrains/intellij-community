7ab3591fef179fe706696461cec575de7a77bfa9
Henry Wylde <henry.wylde@jetbrains.com>
2018-09-27 13:06:00 +0200

    RUBY-4811 don't remove previous/next comments when deleting key-value element
    Do delete comments on the same line though

--- plugins/yaml/src/org/jetbrains/yaml/YAMLUtil.java
+++ plugins/yaml/src/org/jetbrains/yaml/YAMLUtil.java
@@ -4,7 +4,9 @@ import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.tree.TokenSet;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.util.PsiUtilCore;
 import com.intellij.util.IncorrectOperationException;
 import com.intellij.util.ObjectUtils;
 import org.jetbrains.annotations.ApiStatus;
@@ -16,11 +18,14 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Supplier;
 
 /**
  * @author oleg
  */
 public class YAMLUtil {
+  private static final TokenSet BLANK_LINE_ELEMENTS = TokenSet.andNot(YAMLElementTypes.BLANK_ELEMENTS, YAMLElementTypes.EOL_ELEMENTS);
+
 
   @NotNull
   public static String getFullKey(final YAMLKeyValue yamlKeyValue) {
@@ -308,5 +313,39 @@ public class YAMLUtil {
     return offset;
   }
 
+  /**
+   * Deletes surrounding whitespace contextually. First attempts to delete {@link YAMLTokenTypes#COMMENT}s on the same line and
+   * {@link YAMLElementTypes#SPACE_ELEMENTS} forward, otherwise it will delete {@link YAMLElementTypes#SPACE_ELEMENTS} backward.
+   * <p>
+   * This is useful for maintaining consistent formatting.
+   * <p>
+   * E.g.,
+   * <pre>{@code
+   * foo:
+   *   bar: value1 # Same line comment
+   *   # Next line comment
+   *   baz: value2
+   * }</pre>
+   * becomes
+   * <pre>{@code
+   * foo:
+   *   bar: value1 # Next line comment
+   *   baz: value2
+   * }</pre>
+   */
+  public static void deleteSurroundingWhitespace(@NotNull final PsiElement element) {
+    if (element.getNextSibling() != null) {
+      deleteElementsOfType(element::getNextSibling, BLANK_LINE_ELEMENTS);
+      deleteElementsOfType(element::getNextSibling, YAMLElementTypes.SPACE_ELEMENTS);
+    }
+    else {
+      deleteElementsOfType(element::getPrevSibling, YAMLElementTypes.SPACE_ELEMENTS);
+    }
+  }
 
+  private static void deleteElementsOfType(@NotNull final Supplier<PsiElement> element, @NotNull final TokenSet types) {
+    while (element.get() != null && types.contains(PsiUtilCore.getElementType(element.get()))) {
+      element.get().delete();
+    }
+  }
 }
