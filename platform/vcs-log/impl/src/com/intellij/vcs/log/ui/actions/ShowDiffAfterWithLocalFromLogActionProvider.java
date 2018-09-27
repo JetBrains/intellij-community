f48915dc980628e376ff7f8987ba85fc78206de3
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2018-09-27 22:44:24 +0300

    [vcs-log] rename and move ShowDiffAfterWithLocalFromLogActionProvider

--- platform/vcs-log/impl/src/com/intellij/vcs/log/ui/actions/ShowDiffAfterWithLocalFromLogActionProvider.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/ui/actions/ShowDiffAfterWithLocalFromLogActionProvider.java
@@ -1,19 +1,5 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.intellij.vcs.log.ui.actions.history;
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.vcs.log.ui.actions;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.AnActionExtensionProvider;
@@ -38,7 +24,7 @@ import java.util.List;
 import static com.intellij.util.ObjectUtils.notNull;
 import static com.intellij.util.containers.ContainerUtil.getFirstItem;
 
-public class ShowDiffAfterWithLocalFromHistoryActionProvider implements AnActionExtensionProvider {
+public class ShowDiffAfterWithLocalFromLogActionProvider implements AnActionExtensionProvider {
   @Nullable
   private static FilePath getFilePath(@NotNull AnActionEvent e, @NotNull VcsLog log) {
     FilePath filePath = e.getData(VcsDataKeys.FILE_PATH);
