28cf5055931f35ffc7336149e3ed71f7779011d5
Nikita Skvortsov <nikita.skvortsov@jetbrains.com>
2018-09-28 16:22:57 +0300

    fix inspections IDEA-197121 IDEA-CR-37180

--- aether-dependency-resolver/src/org/jetbrains/idea/maven/aether/ArtifactRepositoryManager.java
+++ aether-dependency-resolver/src/org/jetbrains/idea/maven/aether/ArtifactRepositoryManager.java
@@ -275,18 +275,18 @@ public class ArtifactRepositoryManager {
    * @param constraint - version or range constraint of original library
    * @return resulting relaxed constraint to select annotations artifact.
    */
-  private Set<VersionConstraint> relaxForAnnotations(VersionConstraint constraint) {
+  private static Set<VersionConstraint> relaxForAnnotations(VersionConstraint constraint) {
     String annotationsConstraint = constraint.toString();
 
     final Version version = constraint.getVersion();
     if (version != null) {
-      final String major = version.toString().split("\\.|-|_")[0];
+      final String major = version.toString().split("[.\\-_]")[0];
       annotationsConstraint = "[" + major + ", " + version.toString() + "-an10000]";
     }
 
     final VersionRange range = constraint.getRange();
     if (range != null) {
-      final String majorLower = range.getLowerBound().getVersion().toString().split("\\.|-|_")[0];
+      final String majorLower = range.getLowerBound().getVersion().toString().split("[.\\-_]")[0];
 
       String upper = range.getUpperBound().isInclusive()
                      ? range.getUpperBound().toString() + "-an10000]"
