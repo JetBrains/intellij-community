f453b03fe29f525188aaa5dcbcfaff2cc8438f87
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-09-30 08:57:27 +0200

    add note that NotificationAction is more preferable way

--- platform/platform-impl/src/com/intellij/notification/SingletonNotificationManager.kt
+++ platform/platform-impl/src/com/intellij/notification/SingletonNotificationManager.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.notification
 
+import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindowManager
 
@@ -9,21 +10,19 @@ import java.util.concurrent.atomic.AtomicReference
 class SingletonNotificationManager(private val group: NotificationGroup, private val type: NotificationType, private val defaultListener: NotificationListener? = null) {
   private val notification = AtomicReference<Notification>()
 
-  private val expiredListener by lazy {
-    Runnable {
-      val currentNotification = notification.get()
-      if (currentNotification != null && currentNotification.isExpired) {
-        notification.compareAndSet(currentNotification, null)
-      }
+  private val expiredListener = Runnable {
+    val currentNotification = notification.get()
+    if (currentNotification != null && currentNotification.isExpired) {
+      notification.compareAndSet(currentNotification, null)
     }
   }
 
   fun notify(content: String, project: Project?): Boolean {
-    return notify("", content, project, defaultListener)
+    return notify("", content, project)
   }
 
   @JvmOverloads
-  fun notify(title: String, content: String, project: Project? = null, listener: NotificationListener? = defaultListener): Boolean {
+  fun notify(title: String = "", content: String, project: Project? = null, listener: NotificationListener? = defaultListener, action: AnAction? = null): Boolean {
     val oldNotification = notification.get()
     // !oldNotification.isExpired() is not enough - notification could be closed, but not expired
     if (oldNotification != null) {
@@ -37,6 +36,9 @@ class SingletonNotificationManager(private val group: NotificationGroup, private
     }
 
     val newNotification = group.createNotification(title, content, type, listener)
+    if (action != null) {
+      newNotification.addAction(action)
+    }
     newNotification.whenExpired(expiredListener)
     notification.set(newNotification)
     newNotification.notify(project)
