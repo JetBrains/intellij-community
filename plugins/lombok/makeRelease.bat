4ba0b63265b0f7bc8c05e1c4955e3229362efad4
Michail Plushnikov <mplushnikov@gmail.com>
2018-10-01 21:44:48 +0200

    updated scripts
    GitOrigin-RevId: eadf4308a080a1b290caa8fa1d0f1451296cea16

--- plugins/lombok/makeRelease.bat
+++ plugins/lombok/makeRelease.bat
@@ -2,7 +2,7 @@
 
 SET pluginVersion=0.21
 
-for %%X in (2017.2 2017.3 2018.1 2018.2 183.2153.8 LATEST-EAP-SNAPSHOT) do call :buildPlugin %%X
+for %%X in (2017.2 2017.3 2018.1 2018.2 183.2153.8) do call :buildPlugin %%X
 ECHO All Done
 
 echo.&pause&goto:eof
@@ -11,7 +11,6 @@ echo.&pause&goto:eof
 SETLOCAL
 echo Called with %1
 SET IDEA_VERSION=%1
-call gradlew clean
-call gradlew build
+call gradlew clean build
 xcopy build\distributions\lombok-plugin-%pluginVersion%.zip distro\lombok-plugin-%pluginVersion%-%1.zip*
 ENDLOCAL & SET result=%retval%
