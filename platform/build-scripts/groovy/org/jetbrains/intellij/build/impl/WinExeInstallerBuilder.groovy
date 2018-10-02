d496c4df36b40d98c57c3c849c8a3fd3ec413fcf
Vladimir.Orlov <Vladimir.Orlov@jetbrains.com>
2018-10-02 13:11:24 +0300

    IDEA-199519 Uninstall: The IDE installation folder is not deleted after uninstallation
    IDEA-CR-37456

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/WinExeInstallerBuilder.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/WinExeInstallerBuilder.groovy
@@ -17,6 +17,7 @@ package org.jetbrains.intellij.build.impl
 
 import com.intellij.openapi.util.SystemInfoRt
 import org.jetbrains.intellij.build.BuildContext
+import org.jetbrains.intellij.build.JvmArchitecture
 import org.jetbrains.intellij.build.WindowsDistributionCustomizer
 
 import static com.intellij.openapi.util.io.FileUtil.toSystemDependentName
@@ -118,6 +119,13 @@ class WinExeInstallerBuilder {
       }
       generator.generateInstallerFile(new File(box, "nsiconf/idea_win.nsh"))
       generator.generateUninstallerFile(new File(box, "nsiconf/unidea_win.nsh"))
+
+      String jre32Dir = buildContext.bundledJreManager.extractWinJre(JvmArchitecture.x32)
+      if (jre32Dir != null) {
+        def generatorForJre32Dir = new NsisFileListGenerator()
+        generatorForJre32Dir.addDirectory(jre32Dir + "/jre32")
+        generatorForJre32Dir.generateUninstallerFile("\$INSTDIR\\jre32", new File(box, "nsiconf/un_jre32_win.nsh"))
+      }
     }
     catch (IOException e) {
       buildContext.messages.error("Failed to generated list of files for NSIS installer: $e")
