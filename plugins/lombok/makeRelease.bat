f9ad475500050904f2d4126c9a7b1f2c1ea2c512
Michail Plushnikov <mplushnikov@gmail.com>
2018-10-03 10:45:15 +0200

    bump version
    GitOrigin-RevId: d345f89bf0001a5a7ae72d732e1898826c2e8a6d

--- plugins/lombok/makeRelease.bat
+++ plugins/lombok/makeRelease.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-SET pluginVersion=0.21
+SET pluginVersion=0.22
 
 for %%X in (2017.2 2017.3 2018.1 2018.2 183.2153.8) do call :buildPlugin %%X
 ECHO All Done
--- plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/Version.java
+++ plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/Version.java
@@ -5,7 +5,7 @@ public interface Version {
   /**
    * Current plugin version.
    */
-  String PLUGIN_VERSION = "0.21";
+  String PLUGIN_VERSION = "0.22";
   /**
    * Current version of lombok plugin
    */
