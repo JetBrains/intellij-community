ebfa9961ee10316e6dfa742b22da6a7b4c587a01
Bas Leijdekkers <basleijdekkers@gmail.com>
2018-10-03 17:51:56 +0200

    IG: don't leave empty when removing recursion (IDEA-146785)

--- plugins/InspectionGadgets/test/com/siyeh/igfixes/performance/tail_recursion/Dependency3.after.java
+++ plugins/InspectionGadgets/test/com/siyeh/igfixes/performance/tail_recursion/Dependency3.after.java
@@ -1,11 +1,11 @@
 class C {
 
-    int calculate(int one, int two, int three) {
-        while (true) {
-            int one1 = one;
-            one = three + two;
-            two = two + one1;
-            three = one1;
-        }
-    }
+  int calculate(int one, int two, int three) {
+      while (true) {
+          int one1 = one;
+          one = three + two;
+          two = two + one1;
+          three = one1;
+      }
+  }
 }
