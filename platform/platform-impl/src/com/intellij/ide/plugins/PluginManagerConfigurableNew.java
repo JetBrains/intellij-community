0596a48a22025610380fc7607ae252365f10b67f
Alexander Lobas <Alexander.Lobas@jetbrains.com>
2018-10-04 22:17:53 +0300

    Add log info for IDEA-193890

--- platform/platform-impl/src/com/intellij/ide/plugins/PluginManagerConfigurableNew.java
+++ platform/platform-impl/src/com/intellij/ide/plugins/PluginManagerConfigurableNew.java
@@ -771,6 +771,9 @@ public class PluginManagerConfigurableNew
     registerCopyProvider(myUpdatesPanel);
 
     Runnable runnable = () -> {
+      PluginManagerMain.LOG.info("=== Before update info BuildNumber.currentVersion(): " + BuildNumber.currentVersion() + " ===");
+      PluginManagerMain.LOG.info("=== Before update info ApplicationInfoImpl.getShadowInstance().getApiVersion(): " +
+                                 ApplicationInfoImpl.getShadowInstance().getApiVersion() + " ===");
       Collection<PluginDownloader> updates = UpdateChecker.getPluginUpdates();
 
       ApplicationManager.getApplication().invokeLater(() -> {
