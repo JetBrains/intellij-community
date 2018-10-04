6f89a8efc863712c9e1faa44db1dcac972951ba3
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2018-10-04 12:42:34 +0700

    JUnit4ClassesRequestBuilder: use direct class access

--- plugins/junit_rt/src/com/intellij/junit4/JUnit4ClassesRequestBuilder.java
+++ plugins/junit_rt/src/com/intellij/junit4/JUnit4ClassesRequestBuilder.java
@@ -22,7 +22,7 @@ public class JUnit4ClassesRequestBuilder {
   public static Request getClassesRequest(String suiteName, Class[] classes) {
     try {
       return (Request)Class.forName("org.junit.internal.requests.ClassesRequest")
-                    .getConstructor(new Class[]{String.class, new Class[0].getClass()})
+                    .getConstructor(new Class[]{String.class, Class[].class})
                     .newInstance(new Object[]{suiteName, classes});
     }
     catch (Exception e) {
