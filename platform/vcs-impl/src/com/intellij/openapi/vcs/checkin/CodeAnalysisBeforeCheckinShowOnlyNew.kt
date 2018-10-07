a13122795e65404d307113f40d2421d5e2c65aa8
Vladimir Koshelev <Vladimir.Koshelev@jetbrains.com>
2018-10-07 19:34:02 +0200

    CodeAnalysisBeforeChecking: use a single model task for all analyses
    Minor cleanups:
    - CodeAnalysisBeforeCheckinShowOnlyNow: rename StashOperation -> ShelveOperation
    - fix working with ShelveChangeManager
    - ShelveOperation: run save and load in non-cancelableSections
    - CodeAnalysisBeforeChecking: add logging on VcsException
    - CodeAnalysisBeforeChecking: fix formatting
    - CodeAnalysisBeforeCheckinShowOnlyNew: make class internal

--- platform/vcs-impl/src/com/intellij/openapi/vcs/checkin/CodeAnalysisBeforeCheckinShowOnlyNew.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/checkin/CodeAnalysisBeforeCheckinShowOnlyNew.kt
@@ -5,9 +5,13 @@ package com.intellij.openapi.vcs.checkin
 import com.intellij.codeInsight.CodeSmellInfo
 import com.intellij.diff.tools.util.text.LineOffsetsUtil
 import com.intellij.diff.util.Range
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.CodeSmellDetector
+import com.intellij.openapi.vcs.VcsBundle
 import com.intellij.openapi.vcs.VcsException
 import com.intellij.openapi.vcs.changes.Change
 import com.intellij.openapi.vcs.changes.ChangeListManager
@@ -15,45 +19,45 @@ import com.intellij.openapi.vcs.changes.ChangeListManagerEx
 import com.intellij.openapi.vcs.changes.LocalChangeList
 import com.intellij.openapi.vcs.changes.shelf.ShelveChangesManager
 import com.intellij.openapi.vcs.changes.shelf.ShelvedChangeList
-import com.intellij.openapi.vcs.changes.ui.RollbackWorker
 import com.intellij.openapi.vcs.ex.compareLines
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.psi.PsiDocumentManager
-import com.intellij.psi.PsiFile
 import com.intellij.util.containers.ContainerUtil
 import com.intellij.util.containers.MultiMap
 
 
-object CodeAnalysisBeforeCheckinShowOnlyNew {
+internal object CodeAnalysisBeforeCheckinShowOnlyNew {
+  val LOG = Logger.getInstance(CodeAnalysisBeforeCheckinShowOnlyNew.javaClass)
 
   @JvmStatic
-  fun runAnalysis(project: Project, selectedFiles: List<VirtualFile>) : List<CodeSmellInfo> {
+  fun runAnalysis(project: Project, selectedFiles: List<VirtualFile>, progressIndicator: ProgressIndicator) : List<CodeSmellInfo> {
     val codeSmellDetector = CodeSmellDetector.getInstance(project)
     val newCodeSmells = codeSmellDetector.findCodeSmells(selectedFiles)
     val location2CodeSmell = MultiMap<Pair<VirtualFile, Int>, CodeSmellInfo>()
-    val file2Changes = HashMap<PsiFile, List<Range>>()
+    val file2Changes = HashMap<VirtualFile, List<Range>>()
     val changeListManager = ChangeListManager.getInstance(project)
     newCodeSmells.forEach { codeSmellInfo ->
-      val psiFile = PsiDocumentManager.getInstance(project).getPsiFile(codeSmellInfo.document) ?: return@forEach
-      val unchanged = file2Changes.getOrPut(psiFile) {
+      val virtualFile = FileDocumentManager.getInstance().getFile(codeSmellInfo.document) ?: return@forEach
+      val unchanged = file2Changes.getOrPut(virtualFile) {
         try {
-          val contentFromVcs = changeListManager.getChange(psiFile.virtualFile)?.beforeRevision?.content ?: return@getOrPut emptyList()
+          val contentFromVcs = changeListManager.getChange(virtualFile)?.beforeRevision?.content ?: return@getOrPut emptyList()
           val documentContent = codeSmellInfo.document.immutableCharSequence
           ContainerUtil.newArrayList(compareLines(documentContent, contentFromVcs,
-                                         LineOffsetsUtil.create(documentContent), LineOffsetsUtil.create(contentFromVcs)).iterateUnchanged())
+                                                  LineOffsetsUtil.create(documentContent),
+                                                  LineOffsetsUtil.create(contentFromVcs)).iterateUnchanged())
 
         }
         catch (e: VcsException) {
+          LOG.warn("Couldn't load content", e)
           emptyList()
         }
       }
       val startLine = codeSmellInfo.startLine
       val range = unchanged.firstOrNull { it.start1 <= startLine && startLine < it.end1 } ?: return@forEach
-      location2CodeSmell.putValue(Pair(psiFile.virtualFile, range.start2 + startLine - range.start1), codeSmellInfo)
+      location2CodeSmell.putValue(Pair(virtualFile, range.start2 + startLine - range.start1), codeSmellInfo)
     }
 
     val commonCodeSmells = HashSet<CodeSmellInfo>()
-    runAnalysisAfterShelvingSync(project) {
+    runAnalysisAfterShelvingSync(project, progressIndicator) {
       codeSmellDetector.findCodeSmells(selectedFiles.filter { it.exists() }).forEach { oldCodeSmell ->
         val file = FileDocumentManager.getInstance().getFile(oldCodeSmell.document) ?: return@forEach
         location2CodeSmell[Pair(file, oldCodeSmell.startLine)].forEach inner@{ newCodeSmell ->
@@ -67,46 +71,52 @@ object CodeAnalysisBeforeCheckinShowOnlyNew {
     return newCodeSmells.filter { !commonCodeSmells.contains(it) }
   }
 
-  private fun runAnalysisAfterShelvingSync(project: Project, afterShelve: () -> Unit) {
-    val operation = StashOperation(project)
+  private fun runAnalysisAfterShelvingSync(project: Project, progressIndicator: ProgressIndicator,  afterShelve: () -> Unit) {
+    val operation = ShelveOperation(project)
     operation.changeListManager.blockModalNotifications()
-    val changes = operation.save()
-    val rollbackWorker = RollbackWorker(project, "Code Analysis", true)
     operation.changeListManager.freeze("Performing rollback")
-    rollbackWorker.doRollback(changes, true, false, null, null)
     try {
-      afterShelve()
-    }
-    finally {
+      val progressManager = ProgressManager.getInstance()
+      progressManager.executeNonCancelableSection { operation.save(progressIndicator) }
       try {
-        operation.load()
+        afterShelve()
       }
       finally {
-        operation.changeListManager.unblockModalNotifications()
-        operation.changeListManager.unfreeze()
+        progressManager.executeNonCancelableSection { operation.load(progressIndicator) }
       }
+    } finally {
+      operation.changeListManager.unblockModalNotifications()
+      operation.changeListManager.unfreeze()
     }
   }
 
-  private class StashOperation(project: Project) {
+  private class ShelveOperation(project: Project) {
     val changeListManager = ChangeListManager.getInstance(project) as ChangeListManagerEx
     private val shelveChangeManager = ShelveChangesManager.getInstance(project)
     private var shelvedChangeListPairs = ArrayList<Pair<LocalChangeList, ShelvedChangeList>>()
 
-    fun save(): List<Change>  {
+    fun save(progressIndicator: ProgressIndicator): List<Change>  {
       val changes = ArrayList<Change>()
-      changeListManager.changeLists.forEach {
-        val shelveChanges = shelveChangeManager.shelveChanges(it.changes, it.name, false)
+      var i = 0
+      val changeLists = changeListManager.changeLists
+      val size = changeLists.size
+      changeLists.forEach {
+        progressIndicator.fraction = (i++).toDouble() / size.toDouble()
+        progressIndicator.text = VcsBundle.message("searching.for.code.smells.shelving", it.name)
+        val shelveChanges = shelveChangeManager.shelveChanges(it.changes, it.name, true, true)
         changes.addAll(it.changes)
         shelvedChangeListPairs.add(Pair(it, shelveChanges))
       }
       return changes
     }
 
-    fun load() {
+    fun load(progressIndicator: ProgressIndicator) {
+      var i = 0
+      val size = shelvedChangeListPairs.size
       shelvedChangeListPairs.forEach { (local, shelved) ->
-        shelveChangeManager.unshelveChangeList(shelved, null, null, local, false)
-        shelveChangeManager.deleteChangeList(shelved)
+        progressIndicator.fraction = (i++).toDouble() / size.toDouble()
+        progressIndicator.text = VcsBundle.message("searching.for.code.smells.unshelving", shelved.name)
+        shelveChangeManager.unshelveChangeList(shelved, null, null, local, false, true, false, null, null)
       }
     }
   }
