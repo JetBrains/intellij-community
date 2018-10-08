0065eab8d6095d54806f81ccc86095a2ddca5e75
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2018-10-08 16:11:56 +0300

    PY-31999 Explicitly resolve executable path in customized PATH
    Windows doesn't use PATH from the environment passed to a newly created process, so we have to search for our
    executable explicitly.
    
    On macOS GeneralCommandLine has a workaround that tries to resolve an explicit path, but it doesn't look into PATH of
    the passed environment.

--- python/src/com/jetbrains/python/run/runAnything/pyRunAnythingUtil.kt
+++ python/src/com/jetbrains/python/run/runAnything/pyRunAnythingUtil.kt
@@ -1,13 +1,17 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.python.run.runAnything
 
+import com.intellij.execution.configurations.GeneralCommandLine
+import com.intellij.execution.configurations.PathEnvironmentVariableUtil
 import com.intellij.ide.actions.runAnything.RunAnythingUtil
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.module.ModuleUtil
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.projectRoots.Sdk
+import com.intellij.openapi.util.SystemInfo
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.util.EnvironmentUtil
 import com.jetbrains.python.sdk.PythonSdkType
 
 /**
@@ -24,3 +28,21 @@ internal fun VirtualFile.findPythonSdk(project: Project): Sdk? {
   return PythonSdkType.findPythonSdk(module)
 }
 
+internal fun GeneralCommandLine.findExecutableInPath(): String? {
+  val executable = exePath
+  if ("/" in executable || "\\" in executable) return executable
+  val paths = listOfNotNull(effectiveEnvironment["PATH"], System.getenv("PATH"), EnvironmentUtil.getValue("PATH"))
+  return paths
+    .asSequence()
+    .mapNotNull { path ->
+      if (SystemInfo.isWindows) {
+        PathEnvironmentVariableUtil.getWindowsExecutableFileExtensions()
+          .mapNotNull { ext -> PathEnvironmentVariableUtil.findInPath("$executable.$ext", path, null)?.path }
+          .firstOrNull()
+      }
+      else {
+        PathEnvironmentVariableUtil.findInPath(executable, path, null)?.path
+      }
+    }
+    .firstOrNull()
+}
