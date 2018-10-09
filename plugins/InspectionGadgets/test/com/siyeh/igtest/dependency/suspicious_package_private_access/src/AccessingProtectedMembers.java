755c01f92a3bd0ef47e084a168c715b8e32cd4e2
nik <Nikolay.Chashnikov@jetbrains.com>
2018-10-09 17:06:59 +0300

    IDEA-CR-37678: java: use common util to get presentable name for PsiElement in SuspiciousPackagePrivateAccessInspection (IDEA-CR-37678)
    (cherry picked from commit d020509636fc631d2c3e43386ea3048407305b77)

--- plugins/InspectionGadgets/test/com/siyeh/igtest/dependency/suspicious_package_private_access/src/AccessingProtectedMembers.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/dependency/suspicious_package_private_access/src/AccessingProtectedMembers.java
@@ -3,8 +3,8 @@ package xxx;
 class AccessingProtectedMembersNotFromSubclass {
   void foo() {
     ProtectedMembers aClass = new ProtectedMembers();
-    aClass.<warning descr="Method 'ProtectedMembers.method' is protected and used not from a subclass here, but declared in a different module 'dep'">method</warning>();
-    ProtectedMembers.<warning descr="Method 'ProtectedMembers.staticMethod' is protected and used not from a subclass here, but declared in a different module 'dep'">staticMethod</warning>();
+    aClass.<warning descr="Method ProtectedMembers.method() is protected and used not from a subclass here, but declared in a different module 'dep'">method</warning>();
+    ProtectedMembers.<warning descr="Method ProtectedMembers.staticMethod() is protected and used not from a subclass here, but declared in a different module 'dep'">staticMethod</warning>();
   }
 }
 
@@ -15,7 +15,7 @@ class AccessingProtectedMembersFromSubclass extends ProtectedMembers {
     ProtectedMembers.staticMethod();
 
     ProtectedMembers aClass = new ProtectedMembers();
-    aClass.<warning descr="Method 'ProtectedMembers.method' is protected and used not from a subclass here, but declared in a different module 'dep'">method</warning>();
+    aClass.<warning descr="Method ProtectedMembers.method() is protected and used not from a subclass here, but declared in a different module 'dep'">method</warning>();
     AccessingProtectedMembersFromSubclass myInstance = new AccessingProtectedMembersFromSubclass();
     myInstance.method();
   }
