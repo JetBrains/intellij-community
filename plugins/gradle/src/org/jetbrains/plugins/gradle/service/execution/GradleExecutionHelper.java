cbeed01ca42ecd60665f6e10e9bbd37b4a25dce7
Jerome Dochez <jedo@google.com>
2018-10-09 14:28:57 -0700

    removed deprecated option
    Gradle 5.0 has deprecated --recompile-scripts
    see https://github.com/gradle/gradle/issues/1425
    
    Gradle considers that it is useless to have such flags as caching
    should always be correct.
    
    Test: Existing
    Bug: N/A
    Change-Id: Ib5ee902d60ea4302b9cea81b56b2417152d98f01

--- plugins/gradle/src/org/jetbrains/plugins/gradle/service/execution/GradleExecutionHelper.java
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/service/execution/GradleExecutionHelper.java
@@ -136,7 +136,6 @@ public class GradleExecutionHelper {
       if (!settings.getArguments().contains("--quiet") && !settings.getArguments().contains("--debug")) {
         settings.withArgument("--info");
       }
-      settings.withArgument("--recompile-scripts");
     }
 
     if (!settings.getArguments().isEmpty()) {
