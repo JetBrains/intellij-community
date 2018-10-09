547ffda8eb03219b0f1de8fd08a221b3d35f8cc2
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2018-10-09 10:46:26 +0300

    IDEA-196305 Can't run tests using gutter run icons in gradle project when Groovy plugin disabled
    (cherry picked from commit cd3773d75dd32d68b2df212113a89ccd965b0daa)

--- plugins/gradle/java/src/execution/GradleRunnerUtil.java
+++ plugins/gradle/java/src/execution/GradleRunnerUtil.java
@@ -24,7 +24,6 @@ import org.jetbrains.plugins.gradle.settings.GradleExtensionsSettings;
 import org.jetbrains.plugins.gradle.settings.GradleProjectSettings;
 import org.jetbrains.plugins.gradle.settings.GradleSettings;
 import org.jetbrains.plugins.gradle.util.GradleConstants;
-import org.jetbrains.plugins.groovy.lang.psi.GroovyFile;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrApplicationStatement;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrExpression;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrReferenceExpression;
@@ -94,11 +93,11 @@ public class GradleRunnerUtil {
   }
 
   public static boolean isFromGroovyGradleScript(@NotNull PsiElement element) {
-    PsiFile file = element.getContainingFile();
-    if (!(file instanceof GroovyFile)) {
-      return false;
-    }
-    return GradleConstants.EXTENSION.equals(file.getVirtualFile().getExtension());
+    PsiFile psiFile = element.getContainingFile();
+    if (psiFile == null) return false;
+    VirtualFile virtualFile = psiFile.getVirtualFile();
+    if (virtualFile == null) return false;
+    return GradleConstants.EXTENSION.equals(virtualFile.getExtension());
   }
 
   @NotNull
