da09811b79f70838f4e2c503bb6ce9f1707e4082
Nadya Zabrodina <Nadya.Zabrodina@jetbrains.com>
2018-10-09 13:25:39 +0300

    shelf: refactoring - change SHELVED_CHANGELIST data keys to list

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/shelf/RestoreShelvedChange.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/shelf/RestoreShelvedChange.java
@@ -6,7 +6,10 @@ import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vcs.VcsBundle;
+import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
 
 public class RestoreShelvedChange extends DumbAwareAction {
   public RestoreShelvedChange() {
@@ -16,18 +19,19 @@ public class RestoreShelvedChange extends DumbAwareAction {
   @Override
   public void update(@NotNull final AnActionEvent e) {
     final Project project = e.getData(CommonDataKeys.PROJECT);
-    final ShelvedChangeList[] recycledChanges = e.getData(ShelvedChangesViewManager.SHELVED_RECYCLED_CHANGELIST_KEY);
+    final Collection<ShelvedChangeList> recycledChanges = e.getData(ShelvedChangesViewManager.SHELVED_RECYCLED_CHANGELIST_KEY);
     e.getPresentation().setText(VcsBundle.message("vcs.shelf.action.restore.text"));
     e.getPresentation().setDescription(VcsBundle.message("vcs.shelf.action.restore.description"));
-    e.getPresentation().setEnabled((project != null) && ((recycledChanges != null) && (recycledChanges.length == 1)));
+    e.getPresentation().setEnabled(project != null && recycledChanges != null);
   }
 
   @Override
   public void actionPerformed(@NotNull final AnActionEvent e) {
-    final Project project = e.getData(CommonDataKeys.PROJECT);
-    final ShelvedChangeList[] recycledChanges = e.getData(ShelvedChangesViewManager.SHELVED_RECYCLED_CHANGELIST_KEY);
-    if (recycledChanges != null && recycledChanges.length == 1) {
-      ShelveChangesManager.getInstance(project).restoreList(recycledChanges[0]);
+    final Project project = e.getRequiredData(CommonDataKeys.PROJECT);
+    final Collection<ShelvedChangeList> recycledChanges = e.getData(ShelvedChangesViewManager.SHELVED_RECYCLED_CHANGELIST_KEY);
+    if (recycledChanges != null && recycledChanges.size() == 1) {
+      //noinspection ConstantConditions
+      ShelveChangesManager.getInstance(project).restoreList(ContainerUtil.getFirstItem(recycledChanges));
     }
   }
 }
