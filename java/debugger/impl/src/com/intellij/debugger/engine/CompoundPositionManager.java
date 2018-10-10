5df37c013b7a562d98a7af98e1f791de83e486ae
Egor Ushakov <egor.ushakov@jetbrains.com>
2018-10-10 19:11:07 +0300

    EA-123580 - IAE: CompoundPositionManager.lambda$null$

--- java/debugger/impl/src/com/intellij/debugger/engine/CompoundPositionManager.java
+++ java/debugger/impl/src/com/intellij/debugger/engine/CompoundPositionManager.java
@@ -99,7 +99,11 @@ public class CompoundPositionManager extends PositionManagerEx implements MultiR
 
       return iterate(positionManager -> {
         SourcePosition res1 = positionManager.getSourcePosition(location);
-        mySourcePositionCache.put(location, res1);
+        try {
+          mySourcePositionCache.put(location, res1);
+        }
+        catch (IllegalArgumentException ignored) { // Invalid method id
+        }
         return res1;
       }, null, null, false);
     });
