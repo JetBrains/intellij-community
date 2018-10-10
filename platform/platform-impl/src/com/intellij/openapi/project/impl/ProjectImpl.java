81032fc16b0042a25dacc3b7fa21824e50af2317
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-10-10 18:01:28 +0200

    ensure that expensive save operation is not performed before startupActivityPassed

--- platform/platform-impl/src/com/intellij/openapi/project/impl/ProjectImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/project/impl/ProjectImpl.java
@@ -325,6 +325,12 @@ public class ProjectImpl extends PlatformComponentManagerImpl implements Project
       return;
     }
 
+    // ensure that expensive save operation is not performed before startupActivityPassed
+    if (!isInitialized()) {
+      LOG.debug("Skip save for " + getName() + ": not initialized");
+      return;
+    }
+
     if (!mySavingInProgress.compareAndSet(false, true)) {
       return;
     }
