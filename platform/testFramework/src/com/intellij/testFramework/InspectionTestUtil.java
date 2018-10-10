39d2b1c8116d4545f02170a7aea8578dca75519d
Dmitry Jemerov <yole@jetbrains.com>
2018-10-10 11:48:15 +0200

    rdserver: Control enabled inspections in tests via protocol

--- platform/testFramework/src/com/intellij/testFramework/InspectionTestUtil.java
+++ platform/testFramework/src/com/intellij/testFramework/InspectionTestUtil.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.testFramework;
 
 import com.intellij.analysis.AnalysisScope;
@@ -182,6 +168,11 @@ public class InspectionTestUtil {
   @NotNull
   public static <T extends InspectionProfileEntry> List<InspectionProfileEntry> instantiateTools(@NotNull Collection<Class<? extends T>> inspections) {
     Set<String> classNames = JBIterable.from(inspections).transform(Class::getName).toSet();
+    return instantiateTools(classNames);
+  }
+
+  @NotNull
+  public static List<InspectionProfileEntry> instantiateTools(Set<String> classNames) {
     List<InspectionProfileEntry> tools = JBIterable.of(LocalInspectionEP.LOCAL_INSPECTION, InspectionEP.GLOBAL_INSPECTION)
       .flatten((o) -> Arrays.asList(o.getExtensions()))
       .filter((o) -> classNames.contains(o.implementationClass))
