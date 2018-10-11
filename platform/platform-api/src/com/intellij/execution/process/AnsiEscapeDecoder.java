6ca8772f43ba399a52a1eab24c9f08ec2f4eabc7
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2018-10-11 10:31:51 +0300

    AnsiEscapeDecoder refactoring
    IDEA-CR-37844

--- platform/platform-api/src/com/intellij/execution/process/AnsiEscapeDecoder.java
+++ platform/platform-api/src/com/intellij/execution/process/AnsiEscapeDecoder.java
@@ -15,15 +15,17 @@
  */
 package com.intellij.execution.process;
 
+import com.intellij.execution.process.AnsiStreamingLexer.AnsiElementType;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.Pair;
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.util.ObjectUtils;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+
+import static com.intellij.execution.process.AnsiStreamingLexer.SGR;
+import static com.intellij.execution.process.AnsiStreamingLexer.TEXT;
 
 /**
  * See <a href="http://en.wikipedia.org/wiki/ANSI_escape_code">ANSI escape code</a>.
@@ -31,15 +33,12 @@ import java.util.List;
  * @author traff
  */
 public class AnsiEscapeDecoder {
-  private static final char ESC_CHAR = '\u001B'; // Escape sequence start character
-  private static final String CSI = ESC_CHAR + "["; // "Control Sequence Initiator"
-  private static final String M_CSI = "m" + CSI;
-
   private final ColoredOutputTypeRegistry myColoredOutputTypeRegistry = ColoredOutputTypeRegistry.getInstance();
-  private String myUnhandledStdout;
-  private String myUnhandledStderr;
-  private ProcessOutputType myCurrentStdoutOutputType;
-  private ProcessOutputType myCurrentStderrOutputType;
+
+  private final AnsiStreamingLexer myStdoutLexer = new AnsiStreamingLexer();
+  private final AnsiStreamingLexer myStderrLexer = new AnsiStreamingLexer();
+  private final AnsiTerminalEmulator myStdoutEmulator = new AnsiTerminalEmulator();
+  private final AnsiTerminalEmulator myStderrEmulator = new AnsiTerminalEmulator();
 
   /**
    * Parses ansi-color codes from text and sends text fragments with color attributes to textAcceptor
@@ -50,185 +49,54 @@ public class AnsiEscapeDecoder {
    *                     It can implement ColoredChunksAcceptor to receive list of pairs (text, attribute).
    */
   public void escapeText(@NotNull String text, @NotNull Key outputType, @NotNull ColoredTextAcceptor textAcceptor) {
-    text = prependUnhandledText(text, outputType);
-    int pos = 0;
-    int findEscSeqFromIndex = 0;
+    AnsiStreamingLexer effectiveLexer;
+    AnsiTerminalEmulator effectiveEmulator;
+    if (ProcessOutputType.isStdout(outputType)) {
+      effectiveLexer = myStdoutLexer;
+      effectiveEmulator = myStdoutEmulator;
+    }
+    else if (ProcessOutputType.isStderr(outputType)) {
+      effectiveLexer = myStderrLexer;
+      effectiveEmulator = myStderrEmulator;
+    }
+    else {
+      processTextWithoutAnsi(text, outputType, textAcceptor);
+      return;
+    }
+
+    effectiveLexer.append(text);
+    effectiveLexer.advance();
+
     List<Pair<String, Key>> chunks = null;
-    int unhandledSuffixLength = 0;
-    while (true) {
-      int escSeqBeginInd = findEscSeqBeginIndex(text, findEscSeqFromIndex);
-      if (escSeqBeginInd < 0) {
-        if (escSeqBeginInd < -1) {
-          unhandledSuffixLength = decodeUnhandledSuffixLength(escSeqBeginInd);
-        }
-        break;
+    AnsiElementType elementType;
+    while ((elementType = effectiveLexer.getElementType()) != null) {
+      String elementText = effectiveLexer.getElementTextSmart();
+      assert elementText != null;
+      if (elementType == SGR) {
+        effectiveEmulator.processSgr(elementText);
       }
-      int escSeqEndInd = findConsecutiveEscSequencesEndIndex(text, escSeqBeginInd);
-      if (escSeqEndInd < 0) {
-        if (escSeqEndInd == -1) {
-          // malformed escape sequence => add ESC[
-          findEscSeqFromIndex = escSeqBeginInd + CSI.length();
-        }
-        else {
-          unhandledSuffixLength = decodeUnhandledSuffixLength(escSeqEndInd);
-          break;
-        }
+      else if (elementType == TEXT) {
+        chunks = processTextChunk(chunks, elementText, outputType, textAcceptor);
       }
       else {
-        assert escSeqBeginInd <= escSeqEndInd;
-        if (pos < escSeqBeginInd) {
-          chunks = processTextChunk(chunks, text.substring(pos, escSeqBeginInd), outputType, textAcceptor);
-        }
-        pos = escSeqEndInd + 1;
-        findEscSeqFromIndex = pos;
-        if (text.charAt(escSeqEndInd) == 'm') {
-          String escSeq = text.substring(escSeqBeginInd, escSeqEndInd + 1);
-          // this is a simple fix for RUBY-8996:
-          // we replace several consecutive escape sequences with one which contains all these sequences
-          String colorAttribute = StringUtil.replace(escSeq, M_CSI, ";");
-          ProcessOutputType resultType = myColoredOutputTypeRegistry.getOutputType(colorAttribute, outputType);
-          if (resultType.isStdout()) {
-            myCurrentStdoutOutputType = resultType;
-          }
-          else if (resultType.isStderr()) {
-            myCurrentStderrOutputType = resultType;
-          }
-        }
+        assert elementType == AnsiStreamingLexer.CONTROL;
+        // Commands other than SGR are unhandled currently. Extend here when it's needed.
       }
+      effectiveLexer.advance();
     }
-    if (pos < text.length() - unhandledSuffixLength) {
-      chunks = processTextChunk(chunks, text.substring(pos, text.length() - unhandledSuffixLength), outputType, textAcceptor);
-    }
-    updateUnhandledSuffix(text, outputType, unhandledSuffixLength);
     if (chunks != null && textAcceptor instanceof ColoredChunksAcceptor) {
       ((ColoredChunksAcceptor)textAcceptor).coloredChunksAvailable(chunks);
     }
   }
 
-  private void updateUnhandledSuffix(@NotNull String text, @NotNull Key outputType, int unhandledSuffixLength) {
-    String unhandledSuffix = unhandledSuffixLength > 0 ? text.substring(text.length() - unhandledSuffixLength) : null;
-    if (ProcessOutputType.isStdout(outputType)) {
-      myUnhandledStdout = unhandledSuffix;
-    }
-    else if (ProcessOutputType.isStderr(outputType)) {
-      myUnhandledStderr = unhandledSuffix;
-    }
-  }
-
-  @NotNull
-  private String prependUnhandledText(@NotNull String text, @NotNull Key outputType) {
-    String prevUnhandledText = null;
-    if (ProcessOutputType.isStdout(outputType)) {
-      prevUnhandledText = myUnhandledStdout;
-      myUnhandledStdout = null;
-    }
-    else if (ProcessOutputType.isStderr(outputType)) {
-      prevUnhandledText = myUnhandledStderr;
-      myUnhandledStderr = null;
-    }
-    return prevUnhandledText != null ? prevUnhandledText + text : text;
-  }
-
   /**
-   * Returns the index of the first occurrence of CSI within the passed string that is greater than or equal to {@code fromIndex},
-   * or negative number if CSI is not found: -1 - (length of text suffix to keep in case of an incomplete CSI).
+   * Fallback method for the {@link ProcessOutputTypes#SYSTEM system} output type. ANSI is not supported
    */
-  private static int findEscSeqBeginIndex(@NotNull String text, int fromIndex) {
-    int ind = text.indexOf(CSI.charAt(0), fromIndex);
-    if (ind == -1) {
-      return -1;
+  private void processTextWithoutAnsi(@NotNull String text, @NotNull Key outputType, @NotNull ColoredTextAcceptor textAcceptor) {
+    List<Pair<String, Key>> chunks = processTextChunk(null, text, outputType, textAcceptor);
+    if (chunks != null && textAcceptor instanceof ColoredChunksAcceptor) {
+      ((ColoredChunksAcceptor)textAcceptor).coloredChunksAvailable(chunks);
     }
-    if (ind == text.length() - 1) {
-      return encodeUnhandledSuffixLength(text, ind);
-    }
-    return text.charAt(ind + 1) == CSI.charAt(1) ? ind : -1;
-  }
-
-  /**
-   * Returns end index of all consecutive escape sequences started at {@code firstEscSeqBeginInd}, or
-   * a negative number if not found. Since an escape sequence could be split among several subsequent output chunks
-   * (e.g. because of automatic flushing of standard stream when its buffer is full), it should be parsed
-   * when all the needed output chunks are available. To achieve that, the return value encodes
-   * the length of the passed string suffix to keep in case of an incomplete last escape sequence:
-   *  {@code -1 - (length of string suffix to keep in case of an incomplete last escape sequence)}.  </p>
-   * If the return value is -1, no string suffix should be kept => a malformed escape sequence has been encountered.
-   * If the return value is less than -1, no actual handing of the incomplete escape sequence should be performed,
-   * the string suffix length should be decoded with {@code #decodeUnhandledSuffixLength(the return value)} and the suffix
-   * should be preserved until the next output chunk is available.
-   */
-  private static int findConsecutiveEscSequencesEndIndex(@NotNull String text, int firstEscSeqBeginInd) {
-    int escSeqBeginInd = firstEscSeqBeginInd;
-    int lastMatchedColorEscSeqEndInd = -1;
-    int escSeqEndInd;
-    while ((escSeqEndInd = findEscSeqEndIndex(text, escSeqBeginInd)) >= 0) {
-      if (text.charAt(escSeqEndInd) != 'm') {
-        // Handle non-color escape sequences separately
-        // ColoredOutputTypeRegistry expects only color escape sequences and in a single consecutive text chunk
-        return lastMatchedColorEscSeqEndInd > 0 ? lastMatchedColorEscSeqEndInd : escSeqEndInd;
-      }
-      escSeqBeginInd = escSeqEndInd + 1;
-      lastMatchedColorEscSeqEndInd = escSeqEndInd;
-      if (escSeqEndInd + 1 >= text.length()) {
-        return encodeUnhandledSuffixLength(text, firstEscSeqBeginInd);
-      }
-      if (text.charAt(escSeqEndInd + 1) != CSI.charAt(0)) {
-        break;
-      }
-      if (escSeqEndInd + 2 >= text.length()) {
-        return encodeUnhandledSuffixLength(text, firstEscSeqBeginInd);
-      }
-      if (text.charAt(escSeqEndInd + 2) != CSI.charAt(1)) {
-        break;
-      }
-    }
-    if (escSeqEndInd < -1) {
-      return encodeUnhandledSuffixLength(text, firstEscSeqBeginInd);
-    }
-    return lastMatchedColorEscSeqEndInd;
-  }
-
-  /**
-   * @implSpec {@code The ESC [ is followed by any number (including none) of "parameter bytes" in the
-   * range 0x30-0x3F (ASCII 0-9:;<=>?), then by any number of "intermediate bytes" in the range 0x20-0x2F (ASCII space and
-   * !"#$%&'()*+,-./), then finally by a single "final byte" in the range 0x40-0x7E (ASCII @A-Z[\]^_`a-z{|}~).}
-   * @implNote Also, there are different sequences, <a href="http://en.wikipedia.org/wiki/ANSI_escape_code#Escape_sequences">aside CSI</a>
-   */
-  private static int findEscSeqEndIndex(@NotNull String text, int escSeqBeginInd) {
-    int parameterEndInd = escSeqBeginInd + CSI.length();
-    while (parameterEndInd < text.length()) {
-      char ch = text.charAt(parameterEndInd);
-      if (0x30 <= ch && ch <= 0x3F) {
-        parameterEndInd++;
-      }
-      else {
-        break;
-      }
-    }
-    while (parameterEndInd < text.length()) {
-      char ch = text.charAt(parameterEndInd);
-      if (0x20 <= ch && ch <= 0x2F) {
-        parameterEndInd++;
-      }
-      else {
-        break;
-      }
-    }
-    if (parameterEndInd == text.length()) {
-      return encodeUnhandledSuffixLength(text, escSeqBeginInd);
-    }
-    char lastChar = text.charAt(parameterEndInd);
-    return 0x40 <= lastChar && lastChar <= 0x7E ? parameterEndInd : -1;
-  }
-
-  private static int encodeUnhandledSuffixLength(@NotNull String text, int suffixStartInd) {
-    return -1 - (text.length() - suffixStartInd);
-  }
-
-  private static int decodeUnhandledSuffixLength(int encodedUnhandledSuffixLength) {
-    if (encodedUnhandledSuffixLength >= -1) {
-      throw new AssertionError();
-    }
-    return -encodedUnhandledSuffixLength - 1;
   }
 
   @Nullable
@@ -252,10 +120,10 @@ public class AnsiEscapeDecoder {
   @NotNull
   protected Key getCurrentOutputAttributes(@NotNull Key outputType) {
     if (ProcessOutputType.isStdout(outputType)) {
-      return ObjectUtils.notNull(myCurrentStdoutOutputType, outputType);
+      return myStdoutEmulator.isInitialState() ? outputType : myColoredOutputTypeRegistry.getOutputType(myStdoutEmulator, outputType);
     }
     if (ProcessOutputType.isStderr(outputType)) {
-      return ObjectUtils.notNull(myCurrentStderrOutputType, outputType);
+      return myStderrEmulator.isInitialState() ? outputType : myColoredOutputTypeRegistry.getOutputType(myStderrEmulator, outputType);
     }
     return outputType;
   }
--- platform/platform-tests/testSrc/com/intellij/execution/process/AnsiEscapeDecoderTest.java
+++ platform/platform-tests/testSrc/com/intellij/execution/process/AnsiEscapeDecoderTest.java
@@ -2,6 +2,8 @@ package com.intellij.execution.process;
 
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.testFramework.PlatformTestCase;
 import com.intellij.testFramework.LightPlatformTestCase;
 import com.intellij.testFramework.PlatformTestUtil;
 import com.intellij.util.Consumer;
@@ -18,6 +20,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * important: ansi decoder returns indexed colors set using 30-37, 40-47, 90-97 and 100-107 codes using 38;5;color_number/48;5;color_number
+ */
 public class AnsiEscapeDecoderTest extends LightPlatformTestCase {
 
   private static final String STDOUT_KEY = ProcessOutputTypes.STDOUT.toString();
@@ -28,105 +33,162 @@ public class AnsiEscapeDecoderTest extends LightPlatformTestCase {
     check(new ColoredText("simple text", ProcessOutputTypes.STDOUT).addExpected("simple text", STDOUT_KEY));
   }
 
+  public void testSequentialSgr() {
+    check(new ColoredText("This is \u001B[1;3mItalic Bold", ProcessOutputTypes.STDOUT)
+            .addExpected("This is ", STDOUT_KEY)
+            .addExpected("Italic Bold", "1;3m")
+    );
+  }
+
+  public void testSequentialSgrOneByOne() {
+    check(new ColoredText("This is \u001B[1m\u001B[3mItalic Bold", ProcessOutputTypes.STDOUT)
+            .addExpected("This is ", STDOUT_KEY)
+            .addExpected("Italic Bold", "1;3m")
+    );
+  }
+
   public void testSingleColoredChunk() {
     check(new ColoredText("Chrome 35.0.1916 (Linux): Executed 0 of 1\u001B[32m SUCCESS\u001B[39m (0 secs / 0 secs)\n",
                           ProcessOutputTypes.STDOUT)
             .addExpected("Chrome 35.0.1916 (Linux): Executed 0 of 1", STDOUT_KEY)
-            .addExpected(" SUCCESS", "\u001B[32m")
-            .addExpected(" (0 secs / 0 secs)\n", "\u001B[39m"));
+            .addExpected(" SUCCESS", "38;5;2m")
+            .addExpected(" (0 secs / 0 secs)\n", STDOUT_KEY));
   }
 
   public void testCompoundEscSeq() {
     check(new ColoredText("E\u001B[41m\u001B[37mE\u001B[0mE", ProcessOutputTypes.STDOUT)
             .addExpected("E", STDOUT_KEY)
-            .addExpected("E", "\u001B[41;37m")
+            .addExpected("E", "48;5;1;38;5;7m")
             .addExpected("E", STDOUT_KEY));
   }
 
   public void testOtherEscSeq() {
     check(new ColoredText("Plain\u001B[32mGreen\u001B[39mNormal\u001B[1A\u001B[2K\u001B[31mRed\u001B[39m", ProcessOutputTypes.STDOUT)
             .addExpected("Plain", STDOUT_KEY)
-            .addExpected("Green", "\u001B[32m")
-            .addExpected("Normal", "\u001B[39m")
-            .addExpected("Red", "\u001B[31m"));
+            .addExpected("Green", "38;5;2m")
+            .addExpected("Normal", STDOUT_KEY)
+            .addExpected("Red", "38;5;1m"));
   }
 
   public void testPrivateSequence() {
-    check(new ColoredText("\u001B[0;32mgreen\u001B[0m\u001B[0K\u001B[?25l\n", ProcessOutputTypes.STDOUT)
-            .addExpected("green", "\u001B[0;32m")
+    check(new ColoredText("\u001B[0m\u001B[32mgreen\u001B[0m\u001B[0K\u001B[?25l\n", ProcessOutputTypes.STDOUT)
+            .addExpected("green", "38;5;2m")
             .addExpected("\n", STDOUT_KEY)
     );
   }
 
-  public void testMalformedSequence() {
+  public void testMalformedSequence1() {
     check(false, Collections.singletonList(new ColoredText("\u001B[32mGreen\u001B[\1World\n", ProcessOutputTypes.STDOUT)
-            .addExpected("Green\u001B[\1World\n", "\u001B[32m")
-    ));
-    check(false, Collections.singletonList(new ColoredText("\u001B\n", ProcessOutputTypes.STDOUT)
-            .addExpected("\u001B\n", ProcessOutputTypes.STDOUT.toString())
-    ));
-    check(false, Collections.singletonList(new ColoredText("\u001B[\n", ProcessOutputTypes.STDOUT)
-            .addExpected("\u001B[\n", ProcessOutputTypes.STDOUT.toString())
-    ));
-    check(false, ContainerUtil.newArrayList(
-      new ColoredText("\u001B\nHello,", ProcessOutputTypes.STDOUT)
-        .addExpected("\u001B\nHello,", ProcessOutputTypes.STDOUT.toString()),
-      new ColoredText("\u001B[31mWorld", ProcessOutputTypes.STDOUT)
-        .addExpected("World", "\u001B[31m")
-    ));
-    check(false, ContainerUtil.newArrayList(
-      new ColoredText("\u001BHello,", ProcessOutputTypes.STDOUT)
-        .addExpected("\u001BHello,", ProcessOutputTypes.STDOUT.toString())
-    ));
-    check(false, ContainerUtil.newArrayList(
-      new ColoredText("\u001B[Hello,", ProcessOutputTypes.STDOUT)
-        .addExpected("ello,", ProcessOutputTypes.STDOUT.toString())
-    ));
-    check(false, ContainerUtil.newArrayList(
-      new ColoredText("something[\u001B]asdf[\u001B[]", ProcessOutputTypes.STDOUT)
-        .addExpected("something[\u001B]asdf[\u001B[]", ProcessOutputTypes.STDOUT.toString())
+                                             .addExpected("Green", "38;5;2m")
+                                             .addExpected("\u001B[\1World\n", "38;5;2m")
     ));
   }
 
-  public void testIncompleteEscapeSequences() {
+  public void testMalformedSequence2() {
+    check(false, Collections.singletonList(new ColoredText("\u001B\n", ProcessOutputTypes.STDOUT)
+                                             .addExpected("\u001B\n", ProcessOutputTypes.STDOUT.toString())
+    ));
+  }
+
+  public void testMalformedSequence3() {
+    check(false, Collections.singletonList(new ColoredText("\u001B[\n", ProcessOutputTypes.STDOUT)
+                                             .addExpected("\u001B[\n", ProcessOutputTypes.STDOUT.toString())
+    ));
+  }
+
+  public void testMalformedSequence4() {
+    check(false, ContainerUtil.newArrayList(
+      new ColoredText("\u001B\nHello,", ProcessOutputTypes.STDOUT)
+        .addExpected("\u001B\nHello,", STDOUT_KEY),
+      new ColoredText("\u001B[31mWorld", ProcessOutputTypes.STDOUT)
+        .addExpected("World", "38;5;1m")
+    ));
+  }
+
+  public void testMalformedSequence5() {
+    check(false, ContainerUtil.newArrayList(
+      new ColoredText("\u001BHello,", ProcessOutputTypes.STDOUT)
+        .addExpected("\u001BHello,", STDOUT_KEY)
+    ));
+  }
+
+  public void testMalformedSequence6() {
+    check(false, ContainerUtil.newArrayList(
+      new ColoredText("\u001B[Hello,", ProcessOutputTypes.STDOUT)
+        .addExpected("ello,", STDOUT_KEY)
+    ));
+  }
+
+  public void testMalformedSequence7() {
+    check(false, ContainerUtil.newArrayList(
+      new ColoredText("something[\u001B]asdf[\u001B[]", ProcessOutputTypes.STDOUT)
+        .addExpected("something[", STDOUT_KEY)
+        .addExpected("\u001B]asdf[", STDOUT_KEY)
+    ));
+  }
+
+  public void testIncompleteEscapeSequence1() {
     check(true, ContainerUtil.newArrayList(
       new ColoredText("\u001B", ProcessOutputTypes.STDOUT),
-      new ColoredText("[33m Hello\u001B[3", ProcessOutputTypes.STDOUT).addExpected(" Hello", "\u001B[33m"),
-      new ColoredText("4m, Work!", ProcessOutputTypes.STDOUT).addExpected(", Work!", "\u001B[34m")
+      new ColoredText("[33m Hello\u001B[3", ProcessOutputTypes.STDOUT)
+        .addExpected(" Hello", "38;5;3m"),
+      new ColoredText("4m, Work!", ProcessOutputTypes.STDOUT)
+        .addExpected(", Work!", "38;5;4m")
     ));
+  }
+
+  public void testIncompleteEscapeSequence2() {
 
     check(true, ContainerUtil.newArrayList(
-      new ColoredText("\u001B[1;33m<\u001B[34mnamespace\u001B[1", ProcessOutputTypes.STDOUT)
-        .addExpected("<", "\u001B[1;33m")
-        .addExpected("namespace", "\u001B[34m"),
-      new ColoredText(
-        ";33m:abcd\u001B[0m\u001B[1;33m>\u001B[0m0\u001B[1;33m</\u001B[34mnamespace\u001B[1;33m:abcd\u001B[0m\u001B[1;33m>\u001B[0m",
-        ProcessOutputTypes.STDOUT)
-        .addExpected(":abcd", "\u001B[1;33m")
-        .addExpected(">", "\u001B[0;1;33m")
-        .addExpected("0", "stdout")
-        .addExpected("</", "\u001B[1;33m")
-        .addExpected("namespace", "\u001B[34m")
-        .addExpected(":abcd", "\u001B[1;33m")
-        .addExpected(">", "\u001B[0;1;33m")
+      new ColoredText("\u001B[1m\u001B[33m<" +
+                      "\u001B[34mnamespace" +
+                      "\u001B[1m", ProcessOutputTypes.STDOUT)
+        .addExpected("<", "1;38;5;3m")
+        .addExpected("namespace", "1;38;5;4m"),
+      new ColoredText("\u001B[33m:abcd" +
+                      "\u001B[0m" +
+                      "\u001B[1;33m>" +
+                      "\u001B[0m0" +
+                      "\u001B[1;33m</" +
+                      "\u001B[34mnamespace" +
+                      "\u001B[1;33m:abcd" +
+                      "\u001B[0;1;33m>" +
+                      "\u001B[0m",
+                      ProcessOutputTypes.STDOUT)
+        .addExpected(":abcd", "1;38;5;3m")
+        .addExpected(">", "1;38;5;3m")
+        .addExpected("0", STDOUT_KEY)
+        .addExpected("</", "1;38;5;3m")
+        .addExpected("namespace", "1;38;5;4m")
+        .addExpected(":abcd", "1;38;5;3m")
+        .addExpected(">", "1;38;5;3m")
     ));
+  }
+
+  public void testIncompleteEscapeSequence3() {
     check(false, ContainerUtil.newArrayList(
-      new ColoredText("\u001B[1;31m red\u001B[0m normal\u001B[1;32m green\u001B", ProcessOutputTypes.STDOUT)
-        .addExpected(" red", "\u001B[1;31m")
+      new ColoredText("\u001B[1m\u001B[31m red" +
+                      "\u001B[0m normal" +
+                      "\u001B[1m\u001B[32m green" +
+                      "\u001B", ProcessOutputTypes.STDOUT)
+        .addExpected(" red", "1;38;5;1m")
         .addExpected(" normal", STDOUT_KEY)
-        .addExpected(" green", "\u001B[1;32m"),
-      new ColoredText("[0m\n", ProcessOutputTypes.STDOUT).addExpected("\n", STDOUT_KEY)
+        .addExpected(" green", "1;38;5;2m"),
+      new ColoredText("[0m\n", ProcessOutputTypes.STDOUT)
+        .addExpected("\n", STDOUT_KEY)
     ));
   }
 
   public void testStderr() {
     check(true, ContainerUtil.newArrayList(
-      new ColoredText("\u001B[33m Hello,", ProcessOutputTypes.STDOUT).addExpected(" Hello,", "\u001B[33m"),
-      new ColoredText("World!\n", ProcessOutputTypes.STDERR).addExpected("World!\n", STDERR_KEY),
+      new ColoredText("\u001B[33m Hello,", ProcessOutputTypes.STDOUT)
+        .addExpected(" Hello,", "38;5;3m"),
+      new ColoredText("World!\n", ProcessOutputTypes.STDERR)
+        .addExpected("World!\n", STDERR_KEY),
       new ColoredText("\u001B[41m Changed stderr background", ProcessOutputTypes.STDERR)
-        .addExpected(" Changed stderr background", "\u001B[41m"),
+        .addExpected(" Changed stderr background", "48;5;1m"),
       new ColoredText("Unchanged stdout background", ProcessOutputTypes.STDOUT)
-        .addExpected("Unchanged stdout background", "\u001B[33m")
+        .addExpected("Unchanged stdout background", "38;5;3m")
     ));
   }
 
@@ -137,10 +199,8 @@ public class AnsiEscapeDecoderTest extends LightPlatformTestCase {
   private static void check(boolean testCharByCharProcessing, @NotNull List<ColoredText> texts) {
     AnsiEscapeDecoder decoder = new AnsiEscapeDecoder();
     List<Pair<String, String>> actualColoredChunks = ContainerUtil.newArrayList();
-    //noinspection CodeBlock2Expr
-    AnsiEscapeDecoder.ColoredTextAcceptor acceptor = (text, attributes) -> {
-      actualColoredChunks.add(Pair.create(text, attributes.toString()));
-    };
+    AnsiEscapeDecoder.ColoredTextAcceptor acceptor = (text, attributes) ->
+      actualColoredChunks.add(Pair.create(text, StringUtil.trimStart(attributes.toString(), "\u001b[0;")));
     for (ColoredText text : texts) {
       decoder.escapeText(text.myRawText, text.myOutputType, acceptor);
     }
