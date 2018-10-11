86813c89e191990ce959e1f942f78d5117b2d7dc
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2018-10-11 12:49:44 +0300

    terminal: avoid dependency TerminalArrangementManager on TerminalView
    Previously, NPE happened if TerminalArrangementManager.getState() was called prior to TerminalView.initTerminal
    
    (cherry picked from commit 5cb46e1ae2875dd3b2de0a03c7b764bc29a4c9fc)
    
    https://upsource.jetbrains.com/IDEA/review/IDEA-CR-37863

--- plugins/terminal/src/org/jetbrains/plugins/terminal/TerminalArrangementManager.java
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/TerminalArrangementManager.java
@@ -2,8 +2,12 @@
 package org.jetbrains.plugins.terminal;
 
 import com.intellij.openapi.components.*;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.registry.Registry;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentManager;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -12,31 +16,61 @@ import org.jetbrains.annotations.Nullable;
 })
 public class TerminalArrangementManager implements PersistentStateComponent<TerminalArrangementState> {
 
+  private static final Logger LOG = Logger.getInstance(TerminalArrangementManager.class);
+
   private final Project myProject;
+  private ToolWindow myTerminalToolWindow;
   private TerminalArrangementState myState;
 
   public TerminalArrangementManager(@NotNull Project project) {
     myProject = project;
   }
 
+  void setToolWindow(@NotNull ToolWindow terminalToolWindow) {
+    myTerminalToolWindow = terminalToolWindow;
+  }
+
   @Nullable
   @Override
   public TerminalArrangementState getState() {
-    return TerminalView.getInstance(myProject).getArrangementState();
+    return isAvailable() ? calcArrangementState() : null;
   }
 
   @Override
   public void loadState(@NotNull TerminalArrangementState state) {
-    myState = state;
+    if (isAvailable()) {
+      myState = state;
+    }
   }
 
   @Nullable
   TerminalArrangementState getArrangementState() {
-    return Registry.is("terminal.persistant.tabs") ? myState : null;
+    return myState;
+  }
+
+  @NotNull
+  private TerminalArrangementState calcArrangementState() {
+    TerminalArrangementState arrangementState = new TerminalArrangementState();
+    if (myTerminalToolWindow == null) {
+      LOG.warn("Unavailable " + TerminalToolWindowFactory.TOOL_WINDOW_ID + " toolwindow");
+      return arrangementState;
+    }
+    ContentManager contentManager = myTerminalToolWindow.getContentManager();
+    for (Content content : contentManager.getContents()) {
+      TerminalTabState tabState = new TerminalTabState();
+      tabState.myTabName = content.getTabName();
+      arrangementState.myTabStates.add(tabState);
+    }
+    arrangementState.mySelectedTabIndex = contentManager.getIndexOfContent(contentManager.getSelectedContent());
+    return arrangementState;
   }
 
   @NotNull
   static TerminalArrangementManager getInstance(@NotNull Project project) {
     return ServiceManager.getService(project, TerminalArrangementManager.class);
+  }
+
+  private static boolean isAvailable() {
+    return Registry.is("terminal.persistant.tabs");
   }
 }
