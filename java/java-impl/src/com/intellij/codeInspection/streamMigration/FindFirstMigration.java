1cb5311997f3f2cd7ecee119556937e7695eb851
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2018-10-11 11:57:57 +0700

    FindFirstMigration: desugar primitive assignments to orElse

--- java/java-impl/src/com/intellij/codeInspection/streamMigration/FindFirstMigration.java
+++ java/java-impl/src/com/intellij/codeInspection/streamMigration/FindFirstMigration.java
@@ -42,7 +42,9 @@ class FindFirstMigration extends BaseStreamApiMigration {
       PsiStatement[] statements = tb.getStatements();
       if (statements.length != 2) return null;
       PsiAssignmentExpression assignment = ExpressionUtils.getAssignment(statements[0]);
-      if (assignment == null || tb.getVariable().getType() instanceof PsiPrimitiveType) {
+      if (assignment == null ||
+          (tb.getVariable().getType() instanceof PsiPrimitiveType &&
+           !ExpressionUtils.isReferenceTo(assignment.getRExpression(), tb.getVariable()))) {
         // if we found an assignment with primitive stream variable, then we are not assigning to local variable
         // (see StreamApiMigrationInspection#findMigrationForBreak), thus it could be handled via ifPresent()
         if(!(statements[0] instanceof PsiExpressionStatement)) return null;
