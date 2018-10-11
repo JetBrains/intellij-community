6bd0f9e3be540afe98b66ae0cbc25cdcceb7de61
sergey.vasiliev <sergey.vasiliev@jetbrains.com>
2018-10-11 10:12:55 +0200

    FUSUsageContext unmodifiable data map

--- platform/platform-impl/src/com/intellij/internal/statistic/service/fus/collectors/FUSUsageContext.java
+++ platform/platform-impl/src/com/intellij/internal/statistic/service/fus/collectors/FUSUsageContext.java
@@ -6,6 +6,7 @@ import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
 
@@ -46,13 +47,9 @@ public class FUSUsageContext {
     }
   }
 
-  private FUSUsageContext(@NotNull Map<String, String> context) {
-    data = context;
-  }
-
   @NotNull
   public Map<String, String> getData() {
-    return data;
+    return Collections.unmodifiableMap(data);
   }
 
   public static String getOSNameContextData() {
