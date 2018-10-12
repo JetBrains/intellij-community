70bb48b1bc30ff93b83fc1e56b7deb6b60271b1a
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2018-10-12 13:44:55 +0300

    IDEA-200210: preliminary support for light and dark high contrast themes
    [IDEA-CR-37993] (cherry picked from commit 1ac7233a64d71b7ac4d60f7d52c49a94d915398b)

--- platform/platform-impl/src/com/intellij/ide/ui/AccessibilityConfigurable.java
+++ platform/platform-impl/src/com/intellij/ide/ui/AccessibilityConfigurable.java
@@ -5,6 +5,7 @@ import com.intellij.ide.GeneralSettings;
 import com.intellij.ide.IdeBundle;
 import com.intellij.ide.plugins.newui.VerticalLayout;
 import com.intellij.ide.ui.laf.UIThemeBasedLookAndFeelInfo;
+import com.intellij.ide.ui.laf.darcula.DarculaLookAndFeelInfo;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ServiceKt;
 import com.intellij.openapi.diagnostic.Logger;
@@ -15,16 +16,17 @@ import com.intellij.openapi.options.SearchableConfigurable;
 import com.intellij.util.ui.JBUI;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import javax.swing.UIManager.LookAndFeelInfo;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Predicate;
 
 import static com.intellij.ide.actions.QuickChangeLookAndFeel.switchLafAndUpdateUI;
 
 public final class AccessibilityConfigurable implements SearchableConfigurable {
   public static final String ID = "preferences.accessibility";
-  private static final LookAndFeelInfo HIGH_CONTRAST = findHighContrast();
   private static final Logger LOG = Logger.getInstance(AccessibilityConfigurable.class);
   private final AtomicReference<View> viewReference = new AtomicReference<>();
 
@@ -68,40 +70,56 @@ public final class AccessibilityConfigurable implements SearchableConfigurable {
   }
 
   @Override
+  public void reset() {
+    View view = viewReference.get();
+    if (view != null) view.reset();
+  }
+
+  @Override
   public void apply() {
     View view = viewReference.get();
     if (view != null) view.apply();
   }
 
-  private static LookAndFeelInfo findHighContrast() {
+  private static boolean isDarcula(LookAndFeelInfo info) {
+    return info instanceof DarculaLookAndFeelInfo;
+  }
+
+  private static boolean isDefault(LookAndFeelInfo info) {
+    return !isDarcula(info) && !isHighContrast(info);
+  }
+
+  private static boolean isHighContrast(LookAndFeelInfo info) {
+    return isHighContrastLight(info) || isHighContrastDark(info);
+  }
+
+  private static boolean isHighContrastLight(@SuppressWarnings("unused") LookAndFeelInfo info) {
+    return false; // TODO: support light theme in future releases
+  }
+
+  private static boolean isHighContrastDark(LookAndFeelInfo info) {
+    if (info instanceof UIThemeBasedLookAndFeelInfo) {
+      UITheme theme = ((UIThemeBasedLookAndFeelInfo)info).getTheme();
+      if ("JetBrainsHighContrastTheme".equals(theme.getId())) return true;
+    }
+    return false;
+  }
+
+  @Nullable
+  private static LookAndFeelInfo findLookAndFeel(@NotNull Predicate<LookAndFeelInfo> predicate) {
     for (LookAndFeelInfo info : LafManager.getInstance().getInstalledLookAndFeels()) {
-      if (info instanceof UIThemeBasedLookAndFeelInfo) {
-        UITheme theme = ((UIThemeBasedLookAndFeelInfo)info).getTheme();
-        if ("JetBrainsHighContrastTheme".equals(theme.getId())) return info;
-      }
+      if (predicate.test(info)) return info;
     }
     return null;
   }
 
   private static final class View {
-    private final JPanel pMain = new JPanel();
+    private final JPanel pMain = new JPanel(new VerticalLayout(JBUI.scale(5)));
     private final JCheckBox cbHighContrast = new JCheckBox(IdeBundle.message("checkbox.support.high.contrast"));
     private final JCheckBox cbScreenReaders = new JCheckBox(IdeBundle.message("checkbox.support.screen.readers"));
     private final ColorBlindnessPanel pColorBlindness = new ColorBlindnessPanel();
 
     View() {
-      UISettings ui = UISettings.getInstance();
-      cbHighContrast.setEnabled(HIGH_CONTRAST != null);
-      cbHighContrast.setSelected(ui.getHighContrast());
-      pColorBlindness.setColorBlindness(ui.getColorBlindness());
-
-      GeneralSettings general = GeneralSettings.getInstance();
-      cbScreenReaders.setSelected(general.isSupportScreenReaders());
-      if (GeneralSettings.isSupportScreenReadersOverridden()) {
-        cbScreenReaders.setEnabled(false);
-        cbScreenReaders.setToolTipText("The option is overridden by the JVM property: \"" + GeneralSettings.SUPPORT_SCREEN_READERS + "\"");
-      }
-      pMain.setLayout(new VerticalLayout(JBUI.scale(5)));
       pMain.add(cbHighContrast);
       pMain.add(cbScreenReaders);
       pMain.add(pColorBlindness);
@@ -120,6 +138,20 @@ public final class AccessibilityConfigurable implements SearchableConfigurable {
       return false;
     }
 
+    void reset() {
+      UISettings ui = UISettings.getInstance();
+      cbHighContrast.setEnabled(findLookAndFeel(AccessibilityConfigurable::isHighContrast) != null);
+      cbHighContrast.setSelected(ui.getHighContrast());
+      pColorBlindness.setColorBlindness(ui.getColorBlindness());
+
+      GeneralSettings general = GeneralSettings.getInstance();
+      cbScreenReaders.setSelected(general.isSupportScreenReaders());
+      if (GeneralSettings.isSupportScreenReadersOverridden()) {
+        cbScreenReaders.setEnabled(false);
+        cbScreenReaders.setToolTipText("The option is overridden by the JVM property: \"" + GeneralSettings.SUPPORT_SCREEN_READERS + "\"");
+      }
+    }
+
     void apply() {
       UISettings ui = UISettings.getInstance();
       boolean updateHighContrast = cbHighContrast.isEnabled() && cbHighContrast.isSelected() != ui.getHighContrast();
@@ -133,14 +165,39 @@ public final class AccessibilityConfigurable implements SearchableConfigurable {
       boolean updateScreenReaders = cbScreenReaders.isEnabled() && cbScreenReaders.isSelected() != general.isSupportScreenReaders();
       if (updateScreenReaders) general.setSupportScreenReaders(cbScreenReaders.isSelected());
 
-      if (updateHighContrast || updateColorBlindness) {
-        if (updateHighContrast) {
-          if (cbHighContrast.isSelected()) {
-            if (HIGH_CONTRAST != null && HIGH_CONTRAST != LafManager.getInstance().getCurrentLookAndFeel()) {
-              switchLafAndUpdateUI(LafManager.getInstance(), HIGH_CONTRAST, true);
+      if (updateHighContrast) {
+        LookAndFeelInfo replacement = null;
+        LookAndFeelInfo current = LafManager.getInstance().getCurrentLookAndFeel();
+        if (cbHighContrast.isSelected()) {
+          if (!isHighContrast(current)) {
+            if (isDarcula(current)) {
+              // replace the Darcula L&F with dark high contrast theme
+              replacement = findLookAndFeel(AccessibilityConfigurable::isHighContrastDark);
+            }
+            else {
+              // replace another L&F with light high contrast theme
+              replacement = findLookAndFeel(AccessibilityConfigurable::isHighContrastLight);
+            }
+            if (replacement == null) {
+              // try to find any high contrast theme
+              replacement = findLookAndFeel(AccessibilityConfigurable::isHighContrast);
             }
           }
         }
+        else if (isHighContrastLight(current)) {
+          // replace light high contrast theme with the Default L&F
+          replacement = findLookAndFeel(AccessibilityConfigurable::isDefault);
+        }
+        else if (isHighContrastDark(current)) {
+          // replace dark high contrast theme with Darcula L&F
+          replacement = findLookAndFeel(AccessibilityConfigurable::isDarcula);
+        }
+        if (replacement != null) {
+          // replace current L&F with the found one
+          switchLafAndUpdateUI(LafManager.getInstance(), replacement, true);
+        }
+      }
+      if (updateHighContrast || updateColorBlindness) {
         ui.fireUISettingsChanged();
       }
       if (updateColorBlindness) {
