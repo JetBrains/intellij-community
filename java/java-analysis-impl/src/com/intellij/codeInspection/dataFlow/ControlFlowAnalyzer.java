ed64e02bee2df4eaef0e8bb7d5f9c54da5b83a8a
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2018-10-12 10:10:55 +0700

    ControlFlowAnalyzer#acceptBinaryRightOperand: properly convert type for comparison
    Fixes IDEA-200364 Checking equality between int and double confuses "Constant condition and exception" inspection

--- java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/ControlFlowAnalyzer.java
+++ java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/ControlFlowAnalyzer.java
@@ -1423,7 +1423,7 @@ public class ControlFlowAnalyzer extends JavaElementVisitor {
                                         TypeConversionUtil.isNumericType(lType) &&
                                         TypeConversionUtil.isNumericType(rType);
 
-    PsiType castType = comparingPrimitiveNumeric ? TypeConversionUtil.isFloatOrDoubleType(lType) ? PsiType.DOUBLE : PsiType.LONG : type;
+    PsiType castType = comparingPrimitiveNumeric ? TypeConversionUtil.unboxAndBalanceTypes(lType, rType) : type;
 
     if (!comparingRef) {
       generateBoxingUnboxingInstructionFor(lExpr,castType);
--- java/java-tests/testData/inspection/dataFlow/fixture/WideningToDouble.java
+++ java/java-tests/testData/inspection/dataFlow/fixture/WideningToDouble.java
@@ -5,4 +5,12 @@ class WideningToDouble {
     boolean trueValue = <warning descr="Condition 'x == y' is always 'true'">x == y</warning>; // ASSIGNMENT
     System.out.println(trueValue); // prints "true"
   }
+
+  void test() {
+    int y = 2;
+    if (<warning descr="Condition 'y == 2.1' is always 'false'">y == 2.1</warning>) {
+      System.out.println("true");
+    } else
+    System.out.println("false");
+  }
 }
