e7dadd22473cc3dbcebecb894dc950699ec52a65
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2018-10-12 12:59:35 +0300

    [groovy] get rid of GrLightMethodBuilder#addParameter(String, PsiType, boolean) usages

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/builder/JsonBuilderContributor.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/builder/JsonBuilderContributor.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.builder;
 
 import com.intellij.psi.PsiClass;
@@ -71,13 +57,13 @@ public class JsonBuilderContributor extends BuilderMethodsContributor {
 
     // (Iterable, Closure)
     method = createMethod(name, clazz, place);
-    method.addParameter("value", TypesUtil.createIterableType(place, null), false);
+    method.addParameter("value", TypesUtil.createIterableType(place, null));
     method.addAndGetParameter("c", GROOVY_LANG_CLOSURE, false).putUserData(DELEGATES_TO_KEY, DELEGATE_FQN);
     if (!processor.process(method)) return false;
 
     // (Object[], Closure)
     method = createMethod(name, clazz, place);
-    method.addParameter("value", TypesUtil.getJavaLangObject(place).createArrayType(), false);
+    method.addParameter("value", TypesUtil.getJavaLangObject(place).createArrayType());
     method.addAndGetParameter("c", GROOVY_LANG_CLOSURE, false).putUserData(DELEGATES_TO_KEY, DELEGATE_FQN);
     return processor.process(method);
   }
--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/builder/JsonDelegateContributor.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/builder/JsonDelegateContributor.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.builder;
 
 import com.intellij.psi.*;
@@ -54,14 +40,14 @@ public class JsonDelegateContributor extends BuilderMethodsContributor {
     // T (T)
     method = createMethod(name, clazz, place);
     genericType = facade.getElementFactory().createType(method.addTypeParameter("T"));
-    method.addParameter("value", genericType, false);
+    method.addParameter("value", genericType);
     method.setReturnType(genericType);
     if (!processor.process(method)) return false;
 
     // List<T> (T[], Closure)
     method = createMethod(name, clazz, place);
     genericType = facade.getElementFactory().createType(method.addTypeParameter("T"));
-    method.addParameter("values", genericType.createArrayType(), false);
+    method.addParameter("values", genericType.createArrayType());
     method.addAndGetParameter("c", GROOVY_LANG_CLOSURE, false).putUserData(DELEGATES_TO_KEY, DELEGATE_FQN);
     method.setReturnType(TypesUtil.createListType(place, genericType));
     if (!processor.process(method)) return false;
@@ -69,7 +55,7 @@ public class JsonDelegateContributor extends BuilderMethodsContributor {
     // List<T> (Iterable<T>, Closure)
     method = createMethod(name, clazz, place);
     genericType = facade.getElementFactory().createType(method.addTypeParameter("T"));
-    method.addParameter("values", TypesUtil.createIterableType(place, genericType), false);
+    method.addParameter("values", TypesUtil.createIterableType(place, genericType));
     method.addAndGetParameter("c", GROOVY_LANG_CLOSURE, false).putUserData(DELEGATES_TO_KEY, DELEGATE_FQN);
     method.setReturnType(TypesUtil.createListType(place, genericType));
     if (!processor.process(method)) return false;
