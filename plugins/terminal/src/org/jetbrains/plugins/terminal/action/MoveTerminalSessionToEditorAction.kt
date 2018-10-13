bcb4c0ed0a0419537ef513ef92e70ac37c6ceb56
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2018-10-13 14:50:36 +0300

    terminal: move actions to action package, remove unused RenameTerminalSession.form

--- plugins/terminal/src/org/jetbrains/plugins/terminal/action/MoveTerminalSessionToEditorAction.kt
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/action/MoveTerminalSessionToEditorAction.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.terminal
+package org.jetbrains.plugins.terminal.action
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.fileEditor.FileEditorManager
@@ -8,6 +8,7 @@ import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.ui.content.Content
 import com.intellij.ui.tabs.TabInfo
+import org.jetbrains.plugins.terminal.TerminalView
 import org.jetbrains.plugins.terminal.vfs.TerminalSessionVirtualFileImpl
 
 class MoveTerminalSessionToEditorAction : TerminalSessionContextMenuActionBase(), DumbAware {
--- plugins/terminal/src/org/jetbrains/plugins/terminal/action/RenameTerminalSessionAction.kt
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/action/RenameTerminalSessionAction.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.terminal
+package org.jetbrains.plugins.terminal.action
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.PlatformDataKeys
