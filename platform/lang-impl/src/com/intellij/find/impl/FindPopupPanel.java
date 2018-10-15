9640ec9b72a2c587a737ec1f8b25d1c38fcca917
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2018-10-15 22:17:00 +0300

    Find In Path: <Enter> should close module/directory/scope popups
    (cherry picked from commit 863eceb8bdeea7b5288f3f12a2a1217bba22fede)
    IDEA-CR-38133

--- platform/lang-impl/src/com/intellij/find/impl/FindPopupPanel.java
+++ platform/lang-impl/src/com/intellij/find/impl/FindPopupPanel.java
@@ -605,6 +605,9 @@ public class FindPopupPanel extends JBPanel implements FindUI {
           myFileMaskField.hidePopup();
           return;
         }
+        if (myScopeUI.hideAllPopups()) {
+          return;
+        }
         if (enterAsOK) {
           myOkActionListener.actionPerformed(null);
         }
