690283c714758bd77b03977285ca47b07a4c3f35
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2018-10-15 19:21:01 +0300

    [groovy] add GroovyCallReference and extend GroovyMethodCallReference from it

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/resolve/api/GroovyCallReference.kt
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/resolve/api/GroovyCallReference.kt
@@ -1,13 +1,9 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.lang.resolve.api
 
-import com.intellij.psi.PsiType
+import org.jetbrains.plugins.groovy.lang.psi.api.GroovyReference
 
-interface GroovyMethodCallReference : GroovyCallReference {
+interface GroovyCallReference : GroovyReference {
 
-  val realReference: Boolean
-
-  val receiver: PsiType?
-
-  val methodName: String
+  val arguments: Arguments?
 }
