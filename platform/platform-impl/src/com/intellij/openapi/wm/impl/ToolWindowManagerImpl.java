JetBrains/intellij-community

commit 1d40e12e7feb9b1dfbc521e02e47613f4c36beaf

IDEA-197106 Fix race condition when opening a project
When a project is opened, the thread that is performing the openProject
method call (task within ProjectManagerImpl:loadAndOpenProject())
is being run in parallel to ToolWindowManagerImpl:updateToolWindowHeader,
which results in a modification to the myIdToInfo THashMap.

This simultaneous access and modification results in a
ConcurrentModificationException being thrown.

Close #865

platform/platform-impl/src/com/intellij/openapi/wm/impl/ToolWindowManagerImpl.java
@@ -36,10 +36,7 @@ import com.intellij.openapi.wm.impl.commands.*;
 import com.intellij.ui.BalloonImpl;
 import com.intellij.ui.ColorUtil;
 import com.intellij.ui.awt.RelativePoint;
-import com.intellij.util.Alarm;
-import com.intellij.util.ArrayUtil;
-import com.intellij.util.EventDispatcher;
-import com.intellij.util.ObjectUtils;
+import com.intellij.util.*;
 import com.intellij.util.messages.MessageBusConnection;
 import com.intellij.util.ui.EdtInvocationManager;
 import com.intellij.util.ui.PositionTracker;
@@ -119,7 +116,7 @@ public class ToolWindowManagerImpl extends ToolWindowManagerEx implements Persis
     waiting, pressed, released, hold
   }
 
-  private final Alarm myUpdateHeadersAlarm = new Alarm();
+  private final SingleAlarm myUpdateHeadersAlarm = new SingleAlarm(() -> updateToolWindowHeaders(), 50, this);
   private final CommandProcessor myCommandProcessor = new CommandProcessor();
 
   public ToolWindowManagerImpl(final Project project,
@@ -132,8 +129,7 @@ public class ToolWindowManagerImpl extends ToolWindowManagerEx implements Persis
       return;
     }
 
-    MessageBusConnection busConnection = project.getMessageBus().connect();
-
+    MessageBusConnection busConnection = project.getMessageBus().connect(this);
     busConnection.subscribe(ToolWindowManagerListener.TOPIC, myDispatcher.getMulticaster());
     busConnection.subscribe(AnActionListener.TOPIC, new AnActionListener() {
       @Override
@@ -146,10 +142,17 @@ public class ToolWindowManagerImpl extends ToolWindowManagerEx implements Persis
     busConnection.subscribe(ProjectManager.TOPIC, new ProjectManagerListener() {
       @Override
       public void projectOpened(@NotNull Project project) {
-        if (project == myProject) {
-          //noinspection TestOnlyProblems
-          init();
+        if (project != myProject) {
+          return;
         }
+
+        //noinspection TestOnlyProblems
+        init();
+
+        PropertyChangeListener focusListener = it -> myUpdateHeadersAlarm.request();
+        KeyboardFocusManager keyboardFocusManager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
+        keyboardFocusManager.addPropertyChangeListener("focusOwner", focusListener);
+        Disposer.register(ToolWindowManagerImpl.this, () -> keyboardFocusManager.removePropertyChangeListener(focusListener));
       }
 
       @Override
@@ -175,15 +178,6 @@ public class ToolWindowManagerImpl extends ToolWindowManagerEx implements Persis
         });
       }
     });
-
-    PropertyChangeListener focusListener = it -> {
-      if ("focusOwner".equals(it.getPropertyName())) {
-        myUpdateHeadersAlarm.cancelAllRequests();
-        myUpdateHeadersAlarm.addRequest(this::updateToolWindowHeaders, 50);
-      }
-    };
-    KeyboardFocusManager.getCurrentKeyboardFocusManager().addPropertyChangeListener(focusListener);
-    Disposer.register(this, () -> KeyboardFocusManager.getCurrentKeyboardFocusManager().removePropertyChangeListener(focusListener));
 
     Predicate<AWTEvent> predicate = event ->
       event.getID() == FocusEvent.FOCUS_LOST
@@ -419,7 +413,7 @@ public class ToolWindowManagerImpl extends ToolWindowManagerEx implements Persis
     LOG.assertTrue(myFrame != null);
 
     myToolWindowsPane = new ToolWindowsPane(myFrame, this);
-    Disposer.register(myProject, myToolWindowsPane);
+    Disposer.register(this, myToolWindowsPane);
     ((IdeRootPane)myFrame.getRootPane()).setToolWindowsPane(myToolWindowsPane);
     myFrame.setTitle(FrameTitleBuilder.getInstance().getProjectTitle(myProject));
     ((IdeRootPane)myFrame.getRootPane()).updateToolbar();
@@ -432,7 +426,7 @@ public class ToolWindowManagerImpl extends ToolWindowManagerEx implements Persis
         resetHoldState();
       }
       return false;
-    }, myProject);
+    }, this);
 
     UIUtil.putClientProperty(
       myToolWindowsPane, UIUtil.NOT_IN_HIERARCHY_COMPONENTS, new Iterable<JComponent>() {

commit a6dc5d87907d64c0ca604a4728bad9973a4f0b34

IDEA-CR-38177: IDEA-197106: fix project leak introduced in 1d40e12e
`removePropertyChangeListener` should use the same name as passed in addPropertyChangeListener

(cherry picked from commit b6a57a93a93ce4d2636cbdb4362564f5abaa9f59)

platform/platform-impl/src/com/intellij/openapi/wm/impl/ToolWindowManagerImpl.java
@@ -152,7 +152,8 @@ public class ToolWindowManagerImpl extends ToolWindowManagerEx implements Persis
         PropertyChangeListener focusListener = it -> myUpdateHeadersAlarm.request();
         KeyboardFocusManager keyboardFocusManager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
         keyboardFocusManager.addPropertyChangeListener("focusOwner", focusListener);
-        Disposer.register(ToolWindowManagerImpl.this, () -> keyboardFocusManager.removePropertyChangeListener(focusListener));
+        Disposer.register(ToolWindowManagerImpl.this,
+                          () -> keyboardFocusManager.removePropertyChangeListener("focusOwner", focusListener));
       }
 
       @Override