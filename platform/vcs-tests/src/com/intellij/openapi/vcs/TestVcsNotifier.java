20bd40f41c588290091f1dfd3a71766fc16b4ffe
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2018-10-16 07:16:20 -0700

    vcs: revert automatic git ignore generation for 183 branch
    Decided to postpone until the next release, due to several usability issues.
    
    This reverts commits:
    383465d91b1821bfc9e111d843ab94189cb4cc07
    2207915a5aa54734da23bd68debc28402f506622
    11b8fb508f6c8c1d566190f4d446ac99ea7def57
    5a7f87db360ea0dd7e5c28a8d058d7760caad214
    7643050c397349408e9cba5b6419c8a038f6efc3
    d54a3e49ca5186000207c6619fe31eb39a16fdba
    b857985dd879a6c4c4b701a8c1b006b5032ec7d1
    53e7c82689e3b66ae965dc4f878d77f5a3ffd44f

--- platform/vcs-tests/src/com/intellij/openapi/vcs/TestVcsNotifier.java
+++ platform/vcs-tests/src/com/intellij/openapi/vcs/TestVcsNotifier.java
@@ -3,45 +3,30 @@ package com.intellij.openapi.vcs;
 
 import com.intellij.notification.Notification;
 import com.intellij.openapi.project.Project;
-import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
 
 public class TestVcsNotifier extends VcsNotifier {
 
   private static final String TEST_NOTIFICATION_GROUP = "Test";
 
-  private final List<Notification> myNotifications = ContainerUtil.newArrayList();
+  private Notification myLastNotification;
 
   public TestVcsNotifier(@NotNull Project project) {
     super(project);
   }
 
   public Notification getLastNotification() {
-    return ContainerUtil.getLastItem(myNotifications);
-  }
-
-  @Nullable
-  public Notification findExpectedNotification(@NotNull Notification expectedNotification) {
-    return myNotifications
-      .stream()
-      .filter((notification ->
-                 expectedNotification.getType().equals(notification.getType())
-                 && expectedNotification.getTitle().equals(notification.getTitle())
-                 && expectedNotification.getContent().equals(notification.getContent())))
-      .findAny().orElse(null);
+    return myLastNotification;
   }
 
   @Override
   @NotNull
   public Notification notify(@NotNull Notification notification) {
-    myNotifications.add(notification);
-    return notification;
+    myLastNotification = notification;
+    return myLastNotification;
   }
 
   public void cleanup() {
-    myNotifications.clear();
+    myLastNotification = null;
   }
 }
