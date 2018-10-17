4b23890f12f43cbd0651fb3fcfa970b44563b964
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-10-17 14:56:57 +0200

    IDEA-CR-38250 GithubAccountManager IS roamable

--- plugins/github/src/org/jetbrains/plugins/github/authentication/accounts/GithubAccountManager.kt
+++ plugins/github/src/org/jetbrains/plugins/github/authentication/accounts/GithubAccountManager.kt
@@ -5,7 +5,6 @@ import com.intellij.credentialStore.*
 import com.intellij.ide.passwordSafe.PasswordSafe
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.PersistentStateComponent
-import com.intellij.openapi.components.RoamingType
 import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
 import com.intellij.openapi.diagnostic.Logger
@@ -18,8 +17,8 @@ import kotlin.properties.Delegates.observable
  * Handles application-level Github accounts
  */
 @State(name = "GithubAccounts", storages = [
-  Storage(value = "github.xml", roamingType = RoamingType.DISABLED),
-  Storage(value = "github_settings.xml", deprecated = true, roamingType = RoamingType.DISABLED)
+  Storage(value = "github.xml"),
+  Storage(value = "github_settings.xml", deprecated = true)
 ])
 internal class GithubAccountManager(private val passwordSafe: PasswordSafe) : PersistentStateComponent<Array<GithubAccount>> {
   var accounts: Set<GithubAccount> by observable(setOf()) { _, oldValue, newValue ->
