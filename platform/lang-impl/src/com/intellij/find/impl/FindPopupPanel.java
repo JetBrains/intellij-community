4773600933cf2af184aadfdd9ffad29d852ef0b6
Denis Fokin <denis.fokin@jetbrains.com>
2018-10-17 18:06:55 +0300

    IDEA-199986 Find/Replace in Path closes immediately
    Second try
    
    (cherry picked from commit f664e55519398c27f83eb449898ff6c9a3258517)
    IDEA-CR-38220

--- platform/lang-impl/src/com/intellij/find/impl/FindPopupPanel.java
+++ platform/lang-impl/src/com/intellij/find/impl/FindPopupPanel.java
@@ -289,19 +289,6 @@ public class FindPopupPanel extends JBPanel implements FindUI {
         panelSize.width *= 1.15;
       }
       w.setSize(prev != null ? prev : panelSize);
-      w.addWindowFocusListener(new WindowAdapter() {
-        @Override
-        public void windowLostFocus(WindowEvent e) {
-          Window oppositeWindow = e.getOppositeWindow();
-          if (oppositeWindow == w || oppositeWindow != null && oppositeWindow.getOwner() == w) {
-            return;
-          }
-          if (canBeClosed() || (!myIsPinned.get() && oppositeWindow != null)) {
-            //closeImmediately();
-            myDialog.doCancelAction();
-          }
-        }
-      });
 
       IdeEventQueue.getInstance().getPopupManager().closeAllPopups(false);
       if (showPoint != null) {
@@ -310,6 +297,26 @@ public class FindPopupPanel extends JBPanel implements FindUI {
         w.setLocationRelativeTo(parent);
       }
       myDialog.show();
+
+      w.addWindowListener(new WindowAdapter() {
+        @Override
+        public void windowOpened(WindowEvent e) {
+          w.addWindowFocusListener(new WindowAdapter() {
+            @Override
+            public void windowLostFocus(WindowEvent e) {
+              Window oppositeWindow = e.getOppositeWindow();
+              if (oppositeWindow == w || oppositeWindow != null && oppositeWindow.getOwner() == w) {
+                return;
+              }
+              if (canBeClosed() || (!myIsPinned.get() && oppositeWindow != null)) {
+                //closeImmediately();
+                myDialog.doCancelAction();
+              }
+            }
+          });
+        }
+      });
+
       JRootPane rootPane = getRootPane();
       if (rootPane != null) {
         if (myHelper.isReplaceState()) {
