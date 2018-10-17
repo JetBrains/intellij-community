cdb80682a4608e9d46ec8eb1ade0fb5cbe361853
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2018-10-17 19:30:15 +0300

    move

--- java/java-tests/testSrc/com/intellij/java/structureView/JavaTreeStructureTest.java
+++ java/java-tests/testSrc/com/intellij/java/structureView/JavaTreeStructureTest.java
@@ -1,7 +1,5 @@
-// Copyright 2000-2017 JetBrains s.r.o.
-// Use of this source code is governed by the Apache 2.0 license that can be
-// found in the LICENSE.txt file.
-package com.intellij.projectView;
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.java.structureView;
 
 import com.intellij.JavaTestUtil;
 import com.intellij.ide.structureView.impl.java.PublicElementsFilter;
--- java/java-tests/testSrc/com/intellij/java/structureView/LightJavaStructureViewTest.java
+++ java/java-tests/testSrc/com/intellij/java/structureView/LightJavaStructureViewTest.java
@@ -1,5 +1,5 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.projectView;
+package com.intellij.java.structureView;
 
 import com.intellij.ide.structureView.impl.java.FieldsFilter;
 import com.intellij.ide.structureView.impl.java.JavaAnonymousClassesNodeProvider;
