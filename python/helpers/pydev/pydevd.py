626209a21238f29bd42042a526c265b253248ee0
Elizaveta Shashkova <Elizaveta.Shashkova@jetbrains.com>
2018-10-18 17:36:25 +0300

    PY-27910 Support built-in breakpoint in Python 3.7

--- python/helpers/pydev/pydevd.py
+++ python/helpers/pydev/pydevd.py
@@ -56,10 +56,33 @@ __version__ = '.'.join(__version_info_str__)
 #IMPORTANT: pydevd_constants must be the 1st thing defined because it'll keep a reference to the original sys._getframe
 
 
+def install_breakpointhook(pydevd_breakpointhook=None):
+    if pydevd_breakpointhook is None:
+        def pydevd_breakpointhook(*args, **kwargs):
+            hookname = os.getenv('PYTHONBREAKPOINT')
+            if hookname is not None and len(hookname) > 0 and hasattr(sys, '__breakpointhook__'):
+                sys.__breakpointhook__(*args, **kwargs)
+            else:
+                py_db = get_global_debugger()
+                if (py_db is not None) and (py_db.frame_eval_func is not None):
+                    from _pydevd_frame_eval.pydevd_frame_tracing import suspend_at_builtin_breakpoint
+                    suspend_at_builtin_breakpoint()
+                else:
+                    settrace(*args, **kwargs)
 
+    if sys.version_info[0:2] >= (3, 7):
+        # There are some choices on how to provide the breakpoint hook. Namely, we can provide a
+        # PYTHONBREAKPOINT which provides the import path for a method to be executed or we
+        # can override sys.breakpointhook.
+        # pydevd overrides sys.breakpointhook instead of providing an environment variable because
+        # it's possible that the debugger starts the user program but is not available in the
+        # PYTHONPATH (and would thus fail to be imported if PYTHONBREAKPOINT was set to pydevd.settrace).
+        # Note that the implementation still takes PYTHONBREAKPOINT in account (so, if it was provided
+        # by someone else, it'd still work).
+        sys.breakpointhook = pydevd_breakpointhook
 
-
-
+# Install the breakpoint hook at import time.
+install_breakpointhook()
 
 SUPPORT_PLUGINS = not IS_JYTH_LESS25
 PluginManager = None
