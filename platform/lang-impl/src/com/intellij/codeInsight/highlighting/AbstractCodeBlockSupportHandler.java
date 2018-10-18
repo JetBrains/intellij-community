4dad921ba674dffe236749b10fc81c61cb584b58
Vladimir Koshelev <Vladimir.Koshelev@jetbrains.com>
2018-10-18 11:01:25 +0200

    refactor AbstractCodeBlockSupportHandler API
    IDEA-CR-38298

--- platform/lang-impl/src/com/intellij/codeInsight/highlighting/AbstractCodeBlockSupportHandler.java
+++ platform/lang-impl/src/com/intellij/codeInsight/highlighting/AbstractCodeBlockSupportHandler.java
@@ -3,17 +3,16 @@ package com.intellij.codeInsight.highlighting;
 
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.tree.TokenSet;
 import com.intellij.psi.util.PsiUtilCore;
+import com.intellij.util.ObjectUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 /**
  * This implementation collects specific direct children of top level elements and specific direct children of specific composite elements.
@@ -22,95 +21,106 @@ import java.util.Map;
 public abstract class AbstractCodeBlockSupportHandler implements CodeBlockSupportHandler {
 
   /**
-   * @return top-level elements tokens to be handled, e.g. IF_STATEMENT, UNLESS_STATEMENT
+   * @return all element types which don't have a parent, e.g. IF_STATEMENT <br>
+   * <br>
+   * {@link #getDirectChildrenElementTypes} should never return these element types
    */
   @NotNull
   protected abstract TokenSet getTopLevelElementTypes();
 
   /**
-   * @return direct tokens of {@code parentElementType} to be handled.
-   * e.g IF_STATEMENT => [kwIF, kwUNLESS, kwEND]
+   * @return elements which should be highlighted. These elements don't have children<br>
+   * <br>
+   * {@link #getDirectChildrenElementTypes} should return an empty TokenSet for an element type if it's a keyword
+   */
+  @NotNull
+  protected abstract TokenSet getKeywordElementTypes();
+
+  /**
+   * @return block element types which should be used for navigation
+   */
+  @NotNull
+  protected abstract TokenSet getBlockElementTypes();
+
+  /**
+   *  The method defines a highlighting tree. <br>
+   *  Highlighting tree nodes are PSI elements. <br>
+   *  A highlighting tree has an edge from A to B if and only if <br>
+   *    - A is a parent for B in the PSI tree
+   *    - <code>#getDirectChildrenElementTypes(getElementType(A)).contains(getElementType(B))</code>
+   *
+   *  If two keyword elements have a common ancestor in a highlighting tree they will be highlighted together <br>
+   *  The method should be consistent with {@link #getTopLevelElementTypes()} and {@link #getKeywordElementTypes()}<br>
+   *  <br>
+   *  For example, for the given if statement<br>
+   *  <br>
+   *  <code>
+   *    if expr  <br>
+   *       <br>
+   *    elsif expr <br>
+   *       <br>
+   *    else      <br>
+   *      <br>
+   *    end       <br>
+   *  </code>   <br>
+   *
+   *
+   *  the children should be defined as follows:<br>
+   *  <br>
+   *  IF_STATEMENT => {kIF, ELSIF_BLOCK, ELSE_BLOCK, kEND} <br>
+   *  ELSIF_BLOCK => {kELSEIF} <br>
+   *  ELSE_BLOCK => {kELSE} <br>
    */
   @NotNull
   protected abstract TokenSet getDirectChildrenElementTypes(@Nullable IElementType parentElementType);
 
-  /**
-   * @return map of direct composite child elements to tokens to extract from them.
-   * E.g.: IF_STATEMENT => {ELSE_BRANCH => kwELSE, ELSIF_BRANCH => kwELSIF, ...}
-   */
-  @NotNull
-  protected abstract Map<IElementType, IElementType> getIndirectChildrenMap(@Nullable IElementType parentElementType);
-
   @NotNull
   @Override
   public List<TextRange> getCodeBlockMarkerRanges(@NotNull PsiElement elementAtCursor) {
-    PsiElement parentElement = elementAtCursor.getParent();
-    if (parentElement == null) {
+    TokenSet keywordElementTypes = getKeywordElementTypes();
+    if (!keywordElementTypes.contains(PsiUtilCore.getElementType(elementAtCursor))) {
       return Collections.emptyList();
     }
-
-    IElementType elementType = PsiUtilCore.getElementType(elementAtCursor);
-    IElementType parentElementType = PsiUtilCore.getElementType(parentElement);
-    TokenSet directChildrenElementTypes = getDirectChildrenElementTypes(parentElementType);
-
-    if (directChildrenElementTypes.contains(elementType)) {
-      return computeMarkersRanges(parentElement);
-    }
-
-    PsiElement grandParentElement = parentElement.getParent();
-    if (grandParentElement == null) {
+    final PsiElement rootElement = getParentByTokenSet(elementAtCursor, getTopLevelElementTypes());
+    if (rootElement == null) {
       return Collections.emptyList();
     }
-    Map<IElementType, IElementType> indirectChildrenMap = getIndirectChildrenMap(PsiUtilCore.getElementType(grandParentElement));
-
-    if (elementType == indirectChildrenMap.get(parentElementType)) {
-      return computeMarkersRanges(grandParentElement);
-    }
-
-    return Collections.emptyList();
+    return computeMarkersRanges(rootElement, keywordElementTypes);
   }
 
   @NotNull
-  private List<TextRange> computeMarkersRanges(@NotNull PsiElement rootElement) {
-    IElementType rootElementType = PsiUtilCore.getElementType(rootElement);
-    TokenSet directChildrenTypes = getDirectChildrenElementTypes(rootElementType);
-    Map<IElementType, IElementType> indirectChildrenMap = getIndirectChildrenMap(rootElementType);
+  @Override
+  public TextRange getCodeBlockRange(@NotNull PsiElement elementAtCursor) {
+    return ObjectUtils.notNull(
+      ObjectUtils.doIfNotNull(getParentByTokenSet(elementAtCursor, getBlockElementTypes()), PsiElement::getTextRange),
+      TextRange.EMPTY_RANGE);
+  }
 
+  @NotNull
+  private List<TextRange> computeMarkersRanges(@NotNull PsiElement rootElement, @NotNull TokenSet keywordsElementTypes) {
+    final IElementType type = PsiUtilCore.getElementType(rootElement);
+    if (keywordsElementTypes.contains(type)) {
+      return Collections.singletonList(rootElement.getTextRange());
+    }
+    TokenSet directChildrenTypes = getDirectChildrenElementTypes(type);
     List<TextRange> result = new ArrayList<>();
     PsiElement currentElement = rootElement.getFirstChild();
     while (currentElement != null) {
       IElementType currentElementType = PsiUtilCore.getElementType(currentElement);
       if (directChildrenTypes.contains(currentElementType)) {
-        result.add(currentElement.getTextRange());
-      }
-      else {
-        if (indirectChildrenMap.containsKey(currentElementType)) {
-          PsiElement firstChild = currentElement.getFirstChild();
-          if (PsiUtilCore.getElementType(firstChild) == indirectChildrenMap.get(currentElementType)) {
-            result.add(firstChild.getTextRange());
-          }
-        }
+        result.addAll(computeMarkersRanges(currentElement, keywordsElementTypes));
       }
       currentElement = currentElement.getNextSibling();
     }
     return result;
   }
 
-  @NotNull
-  @Override
-  public TextRange getCodeBlockRange(@NotNull PsiElement elementAtCursor) {
-    PsiElement run = elementAtCursor;
-    while (run != null && !(run instanceof PsiFile)) {
-      IElementType currentElementType = PsiUtilCore.getElementType(run);
-      if (getTopLevelElementTypes().contains(currentElementType)) {
-        return run.getTextRange();
-      }
-      IElementType parentElementType = PsiUtilCore.getElementType(run.getParent());
-      if (getIndirectChildrenMap(parentElementType).containsKey(currentElementType)) {
-        return run.getTextRange();
-      }
+  @Nullable
+  private static PsiElement getParentByTokenSet(@NotNull PsiElement element, @NotNull TokenSet tokenSet) {
+    PsiElement run = element;
+    while (run != null && !tokenSet.contains(PsiUtilCore.getElementType(run))) {
       run = run.getParent();
     }
-    return TextRange.EMPTY_RANGE;
+    return run;
   }
 }
