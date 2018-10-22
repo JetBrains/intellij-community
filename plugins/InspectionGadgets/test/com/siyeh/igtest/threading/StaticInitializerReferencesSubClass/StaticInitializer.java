a4976dd018fc29ed7d309dcda03fe1c7093ed978
peter <peter@jetbrains.com>
2018-10-22 11:37:13 +0200

    IDEA-200775 'Static initializer references subclass' inspection doesn't report anonymous class

--- plugins/InspectionGadgets/test/com/siyeh/igtest/threading/StaticInitializerReferencesSubClass/StaticInitializer.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/threading/StaticInitializerReferencesSubClass/StaticInitializer.java
@@ -1,12 +1,20 @@
 class Super {
   static Super C1 = new <warning descr="Referencing subclass Sub from superclass Super initializer might lead to class loading deadlock">Sub</warning>();
+  static Super C1_ANONYMOUS = new <warning descr="Referencing subclass Sub from superclass Super initializer might lead to class loading deadlock">Sub</warning>() {};
   static Object C2 = <warning descr="Referencing subclass Sub from superclass Super initializer might lead to class loading deadlock">Sub</warning>.create();
   static final Sub C3;
   static final Sub C4 = SubFactory.<warning descr="Referencing subclass Sub from superclass Super initializer might lead to class loading deadlock">create</warning>();
   static final String C5 = SubFactory.<warning descr="Referencing subclass Sub from superclass Super initializer might lead to class loading deadlock">create</warning>().toString();
   static Super USED_PRIVATE = new <warning descr="Referencing subclass MyUsedSub from superclass Super initializer might lead to class loading deadlock">MyUsedSub</warning>();
 
-  static Object OK_INSIDE_ANONYMOUS = new Object() {{ Sub s = new Sub(); }};
+  static Object INSIDE_ANONYMOUS = new Object() {
+    {
+      Sub s = new <warning descr="Referencing subclass Sub from superclass Super initializer might lead to class loading deadlock">Sub</warning>();
+    }
+    void foo() {
+      Sub s = new Sub(); // ok inside method
+    }
+  };
   static Runnable OK_INSIDE_LAMBDA = () -> { Sub s = new Sub(); };
   static Object OK_UNRELATED = "abc";
   static Super OK_SAME = new Super();
