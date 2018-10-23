6e5990bfec17be07a6893b9bc9484238a4d202de
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-10-23 13:48:06 +0200

    simplify, specify map size

--- platform/projectModel-impl/src/com/intellij/application/options/ReplacePathToMacroMap.java
+++ platform/projectModel-impl/src/com/intellij/application/options/ReplacePathToMacroMap.java
@@ -7,8 +7,8 @@ import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.util.text.StringUtilRt;
 import com.intellij.util.ArrayUtil;
-import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.containers.ContainerUtilRt;
+import com.intellij.util.containers.hash.LinkedHashMap;
 import gnu.trove.TObjectIntHashMap;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
@@ -161,8 +161,7 @@ public class ReplacePathToMacroMap extends PathMacroMap {
     return newText.toString();
   }
 
-  private static int getIndex(@NotNull final Map.Entry<String, String> s) {
-    final String replacement = s.getValue();
+  private static int getIndex(@NotNull String replacement) {
     if (replacement.contains("..")) return 1;
     if (replacement.contains("$" + PathMacroUtil.USER_HOME_NAME + "$")) return 1;
     if (replacement.contains("$" + PathMacroUtil.APPLICATION_HOME_DIR + "$")) return 1;
@@ -185,13 +184,13 @@ public class ReplacePathToMacroMap extends PathMacroMap {
     if (myPathsIndex == null || myPathsIndex.size() != myMacroMap.size()) {
       List<Map.Entry<String, String>> entries = new ArrayList<>(myMacroMap.entrySet());
 
-      final TObjectIntHashMap<Map.Entry<String, String>> weights = new TObjectIntHashMap<>();
+      final TObjectIntHashMap<String> weights = new TObjectIntHashMap<>(entries.size());
       for (Map.Entry<String, String> entry : entries) {
-        weights.put(entry, getIndex(entry) * 512 + stripPrefix(entry.getKey()));
+        weights.put(entry.getKey(), getIndex(entry.getValue()) * 512 + stripPrefix(entry.getKey()));
       }
 
-      ContainerUtil.sort(entries, (o1, o2) -> weights.get(o2) - weights.get(o1));
-      myPathsIndex = ContainerUtil.map2List(entries, entry -> entry.getKey());
+      entries.sort((o1, o2) -> weights.get(o2.getKey()) - weights.get(o1.getKey()));
+      myPathsIndex = ContainerUtilRt.map2List(entries, entry -> entry.getKey());
     }
     return myPathsIndex;
   }
