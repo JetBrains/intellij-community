4a31ed81f341c0d2f71b27638aa45986dead5489
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-10-23 15:50:59 +0200

    do not create file templates dir if nothing changed

--- platform/lang-impl/src/com/intellij/ide/fileTemplates/impl/FileTemplatesLoader.java
+++ platform/lang-impl/src/com/intellij/ide/fileTemplates/impl/FileTemplatesLoader.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.fileTemplates.impl;
 
 import com.intellij.ide.fileTemplates.FileTemplateManager;
@@ -178,7 +164,7 @@ class FileTemplatesLoader {
     for (final String path : children) {
       if (!path.endsWith(FTManager.TEMPLATE_EXTENSION_SUFFIX)) continue;
       for (FTManager ftManager : myAllManagers) {
-        File managerRoot = ftManager.getConfigRoot(false);
+        File managerRoot = ftManager.getConfigRoot();
         String relativePath = configDir.equals(managerRoot) ? "" : FileUtil.getRelativePath(configDir, managerRoot)+"/";
         String prefix = FileUtil.toSystemIndependentName(relativePath);
         if (matchesPrefix(path, prefix)) {
