1be832ef445357c7012fae914e3889034d78a58b
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2018-10-24 14:54:37 +0200

    Add tests for PY-9061

---
+++ python/testSrc/com/jetbrains/python/PyExternalDocTest.kt
@@ -0,0 +1,36 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.jetbrains.python
+
+import com.intellij.codeInsight.documentation.DocumentationManager
+import com.intellij.lang.documentation.CompositeDocumentationProvider
+import com.intellij.lang.documentation.ExternalDocumentationProvider
+import com.intellij.psi.PsiElement
+import com.intellij.testFramework.LightProjectDescriptor
+import com.jetbrains.python.fixtures.PyTestCase
+import junit.framework.TestCase
+
+class PyExternalDocTest : PyTestCase() {
+
+  fun testBuiltins() { // PY-9061
+    val pythonBuiltinsHelp = "https://docs.python.org/3.4 Mock SDK/library/functions.html"
+    doTest("x = su<caret>m([1, 2, 3,])", "${pythonBuiltinsHelp}#sum")
+    doTest("f = ope<caret>n())", "${pythonBuiltinsHelp}#open")
+  }
+
+  private fun doTest(text: String, expectedUrl: String) {
+    myFixture.configureByText(getTestName(false) + ".py", text)
+
+    TestCase.assertEquals(expectedUrl, getDocUrl(myFixture.elementAtCaret))
+  }
+
+  private fun getDocUrl(element: PsiElement): String? {
+    val provider = DocumentationManager.getProviderFromElement(element)
+
+    val urls = provider.getUrlFor(element, element)
+
+    TestCase.assertEquals(1, urls!!.size)
+    return urls[0]
+  }
+
+  override fun getProjectDescriptor() = PyTestCase.ourPy3Descriptor
+}
