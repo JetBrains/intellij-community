6daaf1272e95fa5716e62f5c17a77f5ef547ec9f
Bas Leijdekkers <basleijdekkers@gmail.com>
2018-10-25 17:25:09 +0200

    SSR: only store the last search plan instead of the entire compile context

--- platform/structuralsearch/testSource/com/intellij/structuralsearch/OptimizedSearchScanTest.java
+++ platform/structuralsearch/testSource/com/intellij/structuralsearch/OptimizedSearchScanTest.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.structuralsearch;
 
 import com.intellij.openapi.projectRoots.Sdk;
@@ -24,7 +24,7 @@ public class OptimizedSearchScanTest extends StructuralSearchTestCase {
 
   private String findWordsToBeUsedWhenSearchingFor(final String s) {
     findMatchesCount("{}", s);
-    return PatternCompiler.getLastFindPlan();
+    return PatternCompiler.getLastSearchPlan();
   }
 
   public void testClassByQName() {
