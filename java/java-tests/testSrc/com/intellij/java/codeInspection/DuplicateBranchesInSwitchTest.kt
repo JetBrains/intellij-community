0d221c7aefd2f91599397c5200decab795bb6db4
Pavel Dolgov <pavel.dolgov@jetbrains.com>
2018-10-26 16:24:12 +0300

    Java: Highlight identical branches in 'switch' statement (IDEA-181304)

---
+++ java/java-tests/testSrc/com/intellij/java/codeInspection/DuplicateBranchesInSwitchTest.kt
@@ -0,0 +1,38 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.java.codeInspection
+
+import com.intellij.JavaTestUtil
+import com.intellij.codeInspection.DuplicateBranchesInSwitchInspection
+import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase
+
+/**
+ * @author Pavel.Dolgov
+ */
+class DuplicateBranchesInSwitchTest : LightCodeInsightFixtureTestCase() {
+  val inspection = DuplicateBranchesInSwitchInspection()
+
+  override fun setUp() {
+    super.setUp()
+    myFixture.enableInspections(inspection)
+  }
+
+  override fun getBasePath() = JavaTestUtil.getRelativeJavaTestDataPath() + "/inspection/duplicateBranchesInSwitch"
+
+  fun testSimple() = doTest()
+  fun testReturn() = doTest()
+  fun testThrow() = doTest()
+  fun testContinue() = doTest()
+  fun testFallThrough() = doTest()
+  fun testAllFallThrough() = doTest()
+  fun testNoLastBreak() = doTest()
+  fun testFallThroughToBreak() = doTest()
+  fun testThreeDuplicates() = doTest()
+  fun testTwoCaseLabels() = doTest()
+  fun testComplexBranches() = doTest()
+  fun testBreakWithLabel() = doTest()
+  fun testBreakAndReturnUnderIf() = doTest()
+
+  private fun doTest() {
+    myFixture.testHighlighting("${getTestName(false)}.java")
+  }
+}
