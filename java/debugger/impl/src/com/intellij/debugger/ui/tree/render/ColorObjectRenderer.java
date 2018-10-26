0e1b935228e22dddaa60505b2ff5833f31012a17
Egor Ushakov <egor.ushakov@jetbrains.com>
2018-10-26 15:22:13 +0300

    IDEA-CR-38738: IDEA-201267 - better fix after review IDEA-CR-38738
    (cherry picked from commit 06c93be11f6f01763202ed85f165cf84a0ccb53f)

--- java/debugger/impl/src/com/intellij/debugger/ui/tree/render/ColorObjectRenderer.java
+++ java/debugger/impl/src/com/intellij/debugger/ui/tree/render/ColorObjectRenderer.java
@@ -1,13 +1,18 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.debugger.ui.tree.render;
 
+import com.intellij.debugger.engine.DebugProcessImpl;
+import com.intellij.debugger.engine.SuspendContextImpl;
 import com.intellij.debugger.engine.evaluation.EvaluateException;
 import com.intellij.debugger.engine.evaluation.EvaluationContext;
+import com.intellij.debugger.engine.evaluation.EvaluationContextImpl;
+import com.intellij.debugger.engine.events.SuspendContextCommandImpl;
 import com.intellij.debugger.settings.NodeRendererSettings;
 import com.intellij.debugger.ui.tree.ValueDescriptor;
 import com.intellij.util.ui.ColorIcon;
 import com.intellij.util.ui.JBUI;
 import com.sun.jdi.*;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.*;
@@ -30,19 +35,36 @@ class ColorObjectRenderer extends CompoundReferenceRenderer {
         if (refType instanceof ClassType) {
           Value rgbValue = null;
           Method getRGBMethod = ((ClassType)refType).concreteMethodByName("getRGB", "()I");
-          if (getRGBMethod == null || getRGBMethod.declaringType().name().equals(getClassName())) { // getRGB is not overridden
-            Field valueField = refType.fieldByName("value");
-            if (valueField != null) {
-              rgbValue = objRef.getValue(valueField);
+          if (getRGBMethod != null) {
+            ReferenceType rgbMethodDeclaringType = getRGBMethod.declaringType();
+            if (rgbMethodDeclaringType.name().equals(getClassName())) { // getRGB is not overridden
+              Field valueField = rgbMethodDeclaringType.fieldByName("value");
+              if (valueField != null) {
+                rgbValue = objRef.getValue(valueField);
+              }
             }
-          }
-          if (rgbValue == null && getRGBMethod != null) {
-            rgbValue = evaluationContext.getDebugProcess().invokeMethod(evaluationContext, objRef, getRGBMethod, Collections.emptyList());
-          }
-          if (rgbValue instanceof IntegerValue) {
-            @SuppressWarnings("UseJBColor")
-            Color color = new Color(((IntegerValue)rgbValue).value(), true);
-            return JBUI.scale(new ColorIcon(16, 12, color, true));
+            if (rgbValue instanceof IntegerValue) {
+              return createIcon((IntegerValue)rgbValue);
+            }
+            else {
+              EvaluationContextImpl evalContext = ((EvaluationContextImpl)evaluationContext);
+              DebugProcessImpl debugProcess = evalContext.getDebugProcess();
+              debugProcess.getManagerThread().schedule(new SuspendContextCommandImpl(evalContext.getSuspendContext()) {
+                @Override
+                public void contextAction(@NotNull SuspendContextImpl suspendContext) {
+                  try {
+                    Value rgbValue = debugProcess.invokeMethod(evaluationContext, objRef, getRGBMethod, Collections.emptyList());
+                    if (rgbValue instanceof IntegerValue) {
+                      descriptor.setValueIcon(createIcon((IntegerValue)rgbValue));
+                      listener.labelChanged();
+                    }
+                  }
+                  catch (EvaluateException e) {
+                    LOG.info(e);
+                  }
+                }
+              });
+            }
           }
         }
       }
@@ -51,5 +73,10 @@ class ColorObjectRenderer extends CompoundReferenceRenderer {
       }
     }
     return null;
+  }
+
+  private static Icon createIcon(IntegerValue rgbValue) {
+    //noinspection UseJBColor
+    return JBUI.scale(new ColorIcon(16, 12, new Color(rgbValue.value(), true), true));
   }
 }
