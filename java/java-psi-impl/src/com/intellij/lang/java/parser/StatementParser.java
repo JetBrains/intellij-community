38d708ebc17a4b5e8514745adac20cf3ed06af26
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2018-10-26 14:34:49 +0200

    Cleanup (warnings)

--- java/java-psi-impl/src/com/intellij/lang/java/parser/StatementParser.java
+++ java/java-psi-impl/src/com/intellij/lang/java/parser/StatementParser.java
@@ -1,6 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.lang.java.parser;
 
 import com.intellij.codeInsight.daemon.JavaErrorMessages;
@@ -380,7 +378,6 @@ public class StatementParser {
     if (builder.getTokenType() != JavaTokenType.COMMA) {
       expressionStatement = expr.precede();
       done(expressionStatement, JavaElementType.EXPRESSION_STATEMENT);
-      expressionStatement.setCustomEdgeTokenBinders(null, WhitespacesBinders.DEFAULT_RIGHT_BINDER);
     }
     else {
       PsiBuilder.Marker expressionList = expr.precede();
@@ -397,8 +394,9 @@ public class StatementParser {
 
       done(expressionList, JavaElementType.EXPRESSION_LIST);
       done(expressionStatement, JavaElementType.EXPRESSION_LIST_STATEMENT);
-      expressionStatement.setCustomEdgeTokenBinders(null, WhitespacesBinders.DEFAULT_RIGHT_BINDER);
     }
+
+    expressionStatement.setCustomEdgeTokenBinders(null, WhitespacesBinders.DEFAULT_RIGHT_BINDER);
   }
 
   @NotNull
