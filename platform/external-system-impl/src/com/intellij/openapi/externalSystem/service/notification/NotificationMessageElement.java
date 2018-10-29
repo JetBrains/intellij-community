3736b77b040c37bb403e9f8f527e38d630dc03e2
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2018-10-29 13:34:16 +0300

    Simplify some usages of UIUtil.getTree****ground

--- platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/notification/NotificationMessageElement.java
+++ platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/notification/NotificationMessageElement.java
@@ -121,12 +121,7 @@ public class NotificationMessageElement extends NavigatableMessageElement {
       StyleConstants.setForeground(style, JBColor.GRAY);
     }
     else {
-      if (selected) {
-        StyleConstants.setForeground(style, UIUtil.getTreeSelectionForeground(hasFocus));
-      }
-      else {
-        StyleConstants.setForeground(style, UIUtil.getTreeForeground());
-      }
+      StyleConstants.setForeground(style, UIUtil.getTreeForeground(selected, hasFocus));
     }
 
     if (tree != null && WideSelectionTreeUI.isWideSelection(tree)) {
