436b14b2e9cc0e384c7b0e21e2486bfce15f535f
Anna.Kozlova <anna.kozlova@jetbrains.com>
2018-10-30 11:48:20 +0100

    test builder: cleanup using explicit types

--- platform/smRunner/src/com/intellij/execution/testframework/sm/runner/SMTRunnerTreeBuilder.java
+++ platform/smRunner/src/com/intellij/execution/testframework/sm/runner/SMTRunnerTreeBuilder.java
@@ -10,11 +10,11 @@ import com.intellij.openapi.Disposable;
 import com.intellij.ui.tree.StructureTreeModel;
 import com.intellij.util.Alarm;
 
-import javax.swing.JTree;
+import javax.swing.*;
 
 public class SMTRunnerTreeBuilder implements Disposable, AbstractTestTreeBuilderBase<SMTestProxy> {
   private final JTree myTree;
-  private final AbstractTreeStructure myTreeStructure;
+  private final SMTRunnerTreeStructure myTreeStructure;
   private boolean myDisposed;
   private StructureTreeModel myTreeModel;
   private final Alarm mySelectionAlarm = new Alarm(Alarm.ThreadToUse.SWING_THREAD, this);
@@ -38,10 +38,6 @@ public class SMTRunnerTreeBuilder implements Disposable, AbstractTestTreeBuilder
     return myDisposed;
   }
 
-  
-  public SMTRunnerTreeStructure getSMRunnerTreeStructure() {
-    return ((SMTRunnerTreeStructure)getTreeStructure());
-  }
 
   public void updateTestsSubtree(final SMTestProxy parentTestProxy) {
     getTreeModel().invalidate(parentTestProxy, true);
@@ -51,7 +47,7 @@ public class SMTRunnerTreeBuilder implements Disposable, AbstractTestTreeBuilder
     return myTree;
   }
 
-  public AbstractTreeStructure getTreeStructure() {
+  public SMTRunnerTreeStructure getTreeStructure() {
     return myTreeStructure;
   }
 
