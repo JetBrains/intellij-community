bc51dab4b436bd56fff45d14666eb9aaf225e219
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2018-11-01 13:59:19 +0300

    IJI-24 sort compatiblePluginsToIgnore alphabetically; additional comments

--- build/groovy/org/jetbrains/intellij/build/BaseIdeaProperties.groovy
+++ build/groovy/org/jetbrains/intellij/build/BaseIdeaProperties.groovy
@@ -137,24 +137,25 @@ abstract class BaseIdeaProperties extends ProductProperties {
     modulesToCompileTests = ["intellij.platform.jps.build"]
     productLayout.buildAllCompatiblePlugins = true
     productLayout.compatiblePluginsToIgnore = [
+      /* Internal plugins */
       "intellij.akka",
-      "intellij.changesManager",
-      "intellij.cidr.externalSystem",
-      "intellij.chronon",
-      "intellij.flex.uiDesigner.tests",
-      "intellij.php.workshop",
-      "intellij.shortcutPromoter",
-      "intellij.flex.profiler",
-      "intellij.kotlinNative.platformDeps",
-      "intellij.clion.plugin",
-
       "intellij.asp",
-      "intellij.play",
-      "intellij.java.coverage.emma",
+      "intellij.cidr.externalSystem",
+      "intellij.changesManager",
+      "intellij.chronon",
+      "intellij.flex.profiler",
+      "intellij.flex.uiDesigner.tests",
       "intellij.haml",
-      "intellij.tasks.timeTracking",
-      "intellij.restClient",
+      "intellij.java.coverage.emma",
+      "intellij.kotlinNative.platformDeps",
       "intellij.maven.ext",
+      "intellij.play",
+      "intellij.restClient",
+      "intellij.shortcutPromoter",
+      "intellij.tasks.timeTracking",
+
+      /* CLion plugin until it's internal */
+      "intellij.clion.plugin",
 
       /* PHPStorm related plugins */
       "intellij.apacheConfig",
@@ -172,6 +173,7 @@ abstract class BaseIdeaProperties extends ProductProperties {
       "intellij.php.docker",
       "intellij.php.impl",
       "intellij.php.remoteInterpreter",
+      "intellij.php.workshop",
       "intellij.twig",
       "intellij.wordPress",
     ]
