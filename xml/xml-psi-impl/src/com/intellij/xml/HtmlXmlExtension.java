857dc95f21dac147be0e095302aa104f937b0eac
Artem Gainanov <Artem.Gainanov@jetbrains.com>
2018-11-01 18:37:04 +0700

    IDEA-CR-38907: WEB-35461 more tests. isRequiredAttributeImplicitlyPresent simply check
    (cherry picked from commit b0bd16ee017cb542d3c3dfc520fc56b433125f47)

--- xml/xml-psi-impl/src/com/intellij/xml/HtmlXmlExtension.java
+++ xml/xml-psi-impl/src/com/intellij/xml/HtmlXmlExtension.java
@@ -3,6 +3,7 @@ package com.intellij.xml;
 import com.intellij.ide.highlighter.HtmlFileType;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.xml.XmlDocument;
+import com.intellij.psi.xml.XmlTag;
 import com.intellij.xml.util.HtmlUtil;
 import org.jetbrains.annotations.Nullable;
 
@@ -30,5 +31,10 @@ public class HtmlXmlExtension extends DefaultXmlExtension {
     System.arraycopy(namespaces, 0, newNamespaces, 0, namespaces.length);
     newNamespaces[namespaces.length] = new String[] {"xlink", "http://www.w3.org/1999/xlink"};
     return newNamespaces;
+  }
+
+  @Override
+  public boolean isRequiredAttributeImplicitlyPresent(XmlTag tag, String attrName) {
+    return tag.getAttribute(attrName) != null;
   }
 }
