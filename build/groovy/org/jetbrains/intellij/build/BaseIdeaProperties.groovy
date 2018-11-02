a9f1a646f88cb14e1e251eac573486611e31f8f1
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2018-11-02 11:32:53 +0300

    IDEA-CR-39068 add IU list of compatiblePluginsToIgnore to IC list only when ultimate is building
    (cherry picked from commit c8fd24c53ce14733cc3ba03cec3070aa63c9eb56)

--- build/groovy/org/jetbrains/intellij/build/BaseIdeaProperties.groovy
+++ build/groovy/org/jetbrains/intellij/build/BaseIdeaProperties.groovy
@@ -1,7 +1,4 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-
-
-
 package org.jetbrains.intellij.build
 
 import com.intellij.util.SystemProperties
@@ -136,7 +133,9 @@ abstract class BaseIdeaProperties extends ProductProperties {
     additionalModulesToCompile = ["intellij.tools.jps.build.standalone"]
     modulesToCompileTests = ["intellij.platform.jps.build"]
 
-    productLayout.prepareCustomPluginRepositoryForPublishedPlugins = SystemProperties.getBooleanProperty('intellij.build.prepare.plugin.repository', false)
+    productLayout.buildAllCompatiblePlugins = true
+    productLayout.prepareCustomPluginRepositoryForPublishedPlugins =
+      SystemProperties.getBooleanProperty('intellij.build.prepare.plugin.repository', false)
   }
 
   @Override
