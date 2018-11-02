2cb77abe5b1603db37b9c0bd6f55eb1da7b2270e
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2018-11-02 18:10:53 +0300

    IDEA-201494: Shortcut chooser dialog cannot be submitted from keyboard
    [IDEA-CR-39143] (cherry picked from commit 3ca2dd2f810403fe3ed49f3c6c2948cb5f6206a7)

--- platform/platform-impl/src/com/intellij/openapi/keymap/impl/ui/ShortcutTextField.java
+++ platform/platform-impl/src/com/intellij/openapi/keymap/impl/ui/ShortcutTextField.java
@@ -2,22 +2,32 @@
 
 package com.intellij.openapi.keymap.impl.ui;
 
+import com.intellij.icons.AllIcons.General;
 import com.intellij.openapi.keymap.KeymapUtil;
+import com.intellij.openapi.ui.JBPopupMenu;
 import com.intellij.openapi.util.registry.Registry;
 import com.intellij.ui.KeyStrokeAdapter;
+import com.intellij.ui.components.fields.ExtendableTextField;
 import com.intellij.util.ui.accessibility.ScreenReader;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import javax.swing.text.DefaultCaret;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
-public final class ShortcutTextField extends JTextField {
+public final class ShortcutTextField extends ExtendableTextField {
   private KeyStroke myKeyStroke;
 
-  ShortcutTextField() {
+  ShortcutTextField(boolean isFocusTraversalKeysEnabled) {
     enableEvents(AWTEvent.KEY_EVENT_MASK);
-    setFocusTraversalKeysEnabled(false);
+    setFocusTraversalKeysEnabled(isFocusTraversalKeysEnabled);
+    if (isFocusTraversalKeysEnabled) {
+      setExtensions(Extension.create(General.InlineAdd, General.InlineAddHover, getPopupTooltip(), this::showPopup));
+    }
     setCaret(new DefaultCaret() {
       @Override
       public boolean isVisible() {
@@ -28,7 +38,6 @@ public final class ShortcutTextField extends JTextField {
 
   private static boolean absolutelyUnknownKey (KeyEvent e) {
     return e.getKeyCode() == 0
-           && e.getExtendedKeyCode() == 0
            && e.getKeyChar() == KeyEvent.CHAR_UNDEFINED
            && e.getKeyLocation() == KeyEvent.KEY_LOCATION_UNKNOWN
            && e.getExtendedKeyCode() == 0;
@@ -36,9 +45,14 @@ public final class ShortcutTextField extends JTextField {
 
   @Override
   protected void processKeyEvent(KeyEvent e) {
+    int keyCode = e.getKeyCode();
+    if (getFocusTraversalKeysEnabled() && e.getModifiers() == 0 && e.getModifiersEx() == 0) {
+      if (keyCode == KeyEvent.VK_ESCAPE || (keyCode == KeyEvent.VK_ENTER && myKeyStroke != null)) {
+        super.processKeyEvent(e);
+        return;
+      }
+    }
     if (e.getID() == KeyEvent.KEY_PRESSED) {
-      int keyCode = e.getKeyCode();
-
       if (keyCode != KeyEvent.VK_SHIFT &&
           keyCode != KeyEvent.VK_ALT &&
           keyCode != KeyEvent.VK_CONTROL &&
@@ -51,7 +65,7 @@ public final class ShortcutTextField extends JTextField {
     }
     // Ensure TAB/Shift-TAB work as focus traversal keys, otherwise
     // there is no proper way to move the focus outside the text field.
-    if (ScreenReader.isActive()) {
+    if (!getFocusTraversalKeysEnabled() && ScreenReader.isActive()) {
       setFocusTraversalKeysEnabled(true);
       try {
         KeyboardFocusManager.getCurrentKeyboardFocusManager().processKeyEvent(this, e);
@@ -88,6 +102,62 @@ public final class ShortcutTextField extends JTextField {
     if (text == null || text.isEmpty()) {
       myKeyStroke = null;
       firePropertyChange("keyStroke", null, null);
+    }
+  }
+
+  private void showPopup() {
+    JBPopupMenu menu = new JBPopupMenu();
+    getKeyStrokes().forEach(stroke -> menu.add(getPopupAction(stroke)));
+    Insets insets = getInsets();
+    menu.show(this, getWidth() - insets.right, insets.top);
+  }
+
+  @NotNull
+  private Action getPopupAction(@NotNull KeyStroke stroke) {
+    return new AbstractAction("Set " + KeymapUtil.getKeystrokeText(stroke)) {
+      @Override
+      public void actionPerformed(ActionEvent event) {
+        setKeyStroke(stroke);
+      }
+    };
+  }
+
+  @NotNull
+  private String getPopupTooltip() {
+    StringBuilder sb = new StringBuilder();
+    String prefix = "Set shortcuts with ";
+    for (KeyStroke stroke : getKeyStrokes()) {
+      if (0 == stroke.getModifiers()) {
+        sb.append(prefix).append(KeymapUtil.getKeystrokeText(stroke));
+        prefix = ", ";
+      }
+    }
+    return sb.append(" keys").toString();
+  }
+
+  @NotNull
+  private Iterable<KeyStroke> getKeyStrokes() {
+    ArrayList<KeyStroke> list = new ArrayList<>();
+    addKeyStrokes(list, getFocusTraversalKeys(KeyboardFocusManager.FORWARD_TRAVERSAL_KEYS));
+    addKeyStrokes(list, getFocusTraversalKeys(KeyboardFocusManager.BACKWARD_TRAVERSAL_KEYS));
+    addKeyStrokes(list, getFocusTraversalKeys(KeyboardFocusManager.UP_CYCLE_TRAVERSAL_KEYS));
+    addKeyStrokes(list, getFocusTraversalKeys(KeyboardFocusManager.DOWN_CYCLE_TRAVERSAL_KEYS));
+
+    list.add(0, KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0));
+    list.add(1, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0));
+    return list;
+  }
+
+  private static void addKeyStrokes(@NotNull ArrayList<KeyStroke> list, @Nullable Iterable<AWTKeyStroke> strokes) {
+    if (strokes != null) {
+      for (AWTKeyStroke stroke : strokes) {
+        int keyCode = stroke.getKeyCode();
+        if (keyCode != KeyEvent.VK_UNDEFINED) {
+          list.add(stroke instanceof KeyStroke
+                   ? (KeyStroke)stroke
+                   : KeyStroke.getKeyStroke(keyCode, stroke.getModifiers(), stroke.isOnKeyRelease()));
+        }
+      }
     }
   }
 }
