e0794d4416447bb63f4e33b7182fbb421dcb3511
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2018-11-02 13:24:45 +0100

    [tests] adds "ignored build in the middle of patch chain" test case

--- platform/platform-tests/testSrc/com/intellij/ide/updates/UpdateStrategyTest.kt
+++ platform/platform-tests/testSrc/com/intellij/ide/updates/UpdateStrategyTest.kt
@@ -250,6 +250,20 @@ class UpdateStrategyTest : BareTestFixtureTestCase() {
     assertThat(result.patches?.size).isNull()
   }
 
+  @Test fun `allow ignored builds in the middle of a chain`() {
+    val result = check("IU-183.3795.13", ChannelStatus.EAP, """
+      <channel id="IDEA_EAP" status="eap" licensing="eap">
+        <build number="183.4139.22" version="2018.3 EAP">
+          <patch from="183.3975.18" size="1"/>
+        </build>
+        <build number="183.3975.18" version="2018.3 EAP">
+          <patch from="183.3795.13" size="1"/>
+        </build>
+      </channel>""", listOf("183.3975.18"))
+    assertBuild("183.4139.22", result.newBuild)
+    assertThat(result.patches?.chain).isEqualTo(listOf("183.3795.13", "183.3975.18", "183.4139.22").map(BuildNumber::fromString))
+  }
+
   private fun check(currentBuild: String,
                     selectedChannel: ChannelStatus,
                     testData: String,
