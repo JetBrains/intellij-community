cec376d9bcef8d7d006bc4e01e6714d8e67bf329
Viuginov Nickolay <nickolay.viuginov@jetbrains.com>
2018-11-02 16:52:37 +0300

    increase visibility of some AttachToProcessAction methods
    IDEA-CR-39132
    
    (cherry picked from commit 88352870b1372f230baa6108d450f9b6d57363cd)

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/actions/AttachToProcessAction.java
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/actions/AttachToProcessAction.java
@@ -119,7 +119,7 @@ public class AttachToProcessAction extends AnAction {
   }
 
   @NotNull
-  private static List<AttachItem> getTopLevelItems(@NotNull ProgressIndicator indicator, @NotNull Project project) {
+  protected List<AttachItem> getTopLevelItems(@NotNull ProgressIndicator indicator, @NotNull Project project) {
     List<AttachToProcessItem> localAttachToProcessItems = collectAttachProcessItems(
       project, LocalAttachHost.INSTANCE, indicator
     );
@@ -145,8 +145,8 @@ public class AttachToProcessAction extends AnAction {
   }
 
   @NotNull
-  public static List<AttachItem> collectAttachHostsItems(@NotNull final Project project,
-                                                         @NotNull ProgressIndicator indicator) {
+  public List<AttachItem> collectAttachHostsItems(@NotNull final Project project,
+                                                  @NotNull ProgressIndicator indicator) {
 
     List<AttachItem> currentItems = ContainerUtil.newArrayList();
 
@@ -412,18 +412,18 @@ public class AttachToProcessAction extends AnAction {
     }
 
     @Nullable
-    Icon getIcon(@NotNull Project project) {
+    protected Icon getIcon(@NotNull Project project) {
       return myGroup.getItemIcon(project, myInfo, myDataHolder);
     }
 
-    abstract boolean hasSubStep();
+    protected abstract boolean hasSubStep();
 
-    abstract String getText(@NotNull Project project);
+    protected abstract String getText(@NotNull Project project);
 
     @Nullable
-    abstract String getTooltipText(@NotNull Project project);
+    protected abstract String getTooltipText(@NotNull Project project);
 
-    abstract List<AttachToProcessItem> getSubItems();
+    protected abstract List<AttachToProcessItem> getSubItems();
 
     @Override
     public int compareTo(AttachItem<T> compareItem) {
@@ -610,8 +610,8 @@ public class AttachToProcessAction extends AnAction {
     }
   }
 
-  private static class AttachListStep extends MyBasePopupStep<AttachItem> implements ListPopupStepEx<AttachItem> {
-    AttachListStep(@NotNull List<AttachItem> items, @Nullable String title, @NotNull Project project) {
+  public static class AttachListStep extends MyBasePopupStep<AttachItem> implements ListPopupStepEx<AttachItem> {
+    public AttachListStep(@NotNull List<AttachItem> items, @Nullable String title, @NotNull Project project) {
       super(project, title, items);
     }
 
