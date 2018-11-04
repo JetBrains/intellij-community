982ff65963fd55fb63080d5df877856f2b1cefa9
Michail Plushnikov <mplushnikov@gmail.com>
2018-11-04 13:28:26 +0100

    added simplified example for #437
    GitOrigin-RevId: ebbed77419732107b0fe4f48305818d59be71fba

---
+++ plugins/lombok/test-manual/src/main/java/de/plushnikov/constructor/Issue437Simple.java
@@ -0,0 +1,51 @@
+package de.plushnikov.constructor;
+
+import lombok.AllArgsConstructor;
+
+import java.util.function.BiConsumer;
+
+public class Issue437Simple {
+
+  private static final Action<TransmittalActionDocument> TRANSMITTALACTION = Action.of(MetaDocument::setLastTransmittal);
+  private static final Action<CommentActionDocument> COMMENTACTION = Action.of(MetaDocument::setLastComment);
+
+  @AllArgsConstructor(staticName = "of")
+  private static class Action<T extends ActionDocument> {
+
+    private final BiConsumer<MetaDocument, T> lastAction;
+
+//    private Action(BiConsumer<MetaDocument, T> lastAction) {
+//      this.lastAction = lastAction;
+//    }
+//
+//    static <T extends ActionDocument> Action<T> of(BiConsumer<MetaDocument, T> lastAction) {
+//      return new Action<>(lastAction);
+//    }
+  }
+
+  private static abstract class ActionDocument {
+
+  }
+
+  private static class MetaDocument {
+
+    private CommentActionDocument lastComment;
+    private TransmittalActionDocument lastTransmittal;
+
+    public void setLastTransmittal(TransmittalActionDocument lastAction) {
+      lastTransmittal = lastAction;
+    }
+
+    public void setLastComment(CommentActionDocument lastComment) {
+      this.lastComment = lastComment;
+    }
+  }
+
+  private static class TransmittalActionDocument extends ActionDocument {
+
+  }
+
+  private static class CommentActionDocument extends ActionDocument {
+
+  }
+}
