a297f27c58e23a386e68d001d05c459b031b6ca6
Stanislav Dombrovsky <stanislav.dombrovsky@jetbrains.com>
2018-11-06 17:49:32 +0300

    OC-18040 File | New | File from Xcode template... leads to crash [leaks fixed]
    (cherry picked from commit 6e1724923f3da294508e436eeca85d332d8182c0)

--- platform/util/src/com/intellij/ui/mac/foundation/Foundation.java
+++ platform/util/src/com/intellij/ui/mac/foundation/Foundation.java
@@ -264,6 +264,10 @@ public class Foundation {
     }
   }
 
+  public static ID autorelease(ID id){
+    return Foundation.invoke(id, "autorelease");
+  }
+
   public static boolean isMainThread() {
     return invoke("NSThread", "isMainThread").intValue() > 0;
   }
