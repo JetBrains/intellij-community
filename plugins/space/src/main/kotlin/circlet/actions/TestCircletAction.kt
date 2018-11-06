42f1cdb374002a82ebde864f4c5384b3d3cb3d3f
Gleb Leonov <Gleb.Leonov@jetbrains.com>
2018-11-06 12:10:50 +0300

    replace deprecated "launch" with "GlobalScope.launch" since the former is removed in coroutines 1.0
    GitOrigin-RevId: e4b446d33ae2fd2b7a0b456ac1109168a3d7bf68

--- plugins/space/src/main/kotlin/circlet/actions/TestCircletAction.kt
+++ plugins/space/src/main/kotlin/circlet/actions/TestCircletAction.kt
@@ -19,7 +19,7 @@ class TestCircletAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
 
 
-        launch(Ui) {
+        GlobalScope.launch(Ui, CoroutineStart.DEFAULT, null, {
             val project = e.project!!
             val result = project.connection.loginModel!!.client.me.info()
 
@@ -42,6 +42,6 @@ class TestCircletAction : AnAction() {
 
             val c = service<RepositoryComponent>()
             c.repositoryList.addAll(repositories)
-        }
+        })
     }
 }
