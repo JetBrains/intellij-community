9810c7f9047b603325e92a240b9e6d9ad6598be8
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2018-11-06 16:27:02 +0300

    avoid copy pasting in line markers

--- platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/LineMarkersPass.java
+++ platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/LineMarkersPass.java
@@ -258,8 +258,8 @@ public class LineMarkersPass extends TextEditorHighlightingPass {
   }
 
   @NotNull
-  public static LineMarkerInfo createMethodSeparatorLineMarker(@NotNull PsiElement startFrom, @NotNull EditorColorsManager colorsManager) {
-    LineMarkerInfo info = new LineMarkerInfo<>(
+  public static LineMarkerInfo<PsiElement> createMethodSeparatorLineMarker(@NotNull PsiElement startFrom, @NotNull EditorColorsManager colorsManager) {
+    LineMarkerInfo<PsiElement> info = new LineMarkerInfo<>(
       startFrom,
       startFrom.getTextRange(),
       null,
