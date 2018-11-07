4c26e6799abaea59d42bf8270f5005d5b126d4d1
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2018-11-07 19:59:06 +0300

    svn: Refactor "CommitInfo" - renames

--- plugins/svn4idea/src/org/jetbrains/idea/svn/annotate/SvnAnnotationProvider.java
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/annotate/SvnAnnotationProvider.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.svn.annotate;
 
 import com.intellij.openapi.application.ApplicationManager;
@@ -270,7 +270,7 @@ public class SvnAnnotationProvider implements AnnotationProvider, VcsCacheableAn
         progress.checkCanceled();
       }
 
-      result.setLineInfo(lineNumber, info, mergeInfo != null && info.getRevision() > mergeInfo.getRevision() ? mergeInfo : null);
+      result.setLineInfo(lineNumber, info, mergeInfo != null && info.getRevisionNumber() > mergeInfo.getRevisionNumber() ? mergeInfo : null);
     };
   }
 
--- plugins/svn4idea/src/org/jetbrains/idea/svn/browse/DirectoryEntry.java
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/browse/DirectoryEntry.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.svn.browse;
 
 import com.intellij.util.ObjectUtils;
@@ -61,7 +61,7 @@ public class DirectoryEntry extends BaseNodeDescription implements Comparable<Di
   }
 
   public long getRevision() {
-    return myCommitInfo.getRevision();
+    return myCommitInfo.getRevisionNumber();
   }
 
   public String getAuthor() {
