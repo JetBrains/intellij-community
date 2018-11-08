403b07bc79a0bff2f41768bae29d76938711a5cb
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2018-11-08 14:39:16 +0300

    IDEA-CR-39539 don't build compatible plugins for a single IC build
    It's impossible to build all IC-compatible plugins, because almost all such plugins exist in ultimate repository
    So building them should be done only together with ultimate build
    
    (cherry picked from commit 5fd0e62a7b1b951d2e83ffe6f40d35b2c3d66baa)

--- build/groovy/org/jetbrains/intellij/build/BaseIdeaProperties.groovy
+++ build/groovy/org/jetbrains/intellij/build/BaseIdeaProperties.groovy
@@ -1,7 +1,6 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.intellij.build
 
-import com.intellij.util.SystemProperties
 import org.jetbrains.intellij.build.impl.PlatformLayout
 
 import java.util.function.Consumer
@@ -132,10 +131,6 @@ abstract class BaseIdeaProperties extends ProductProperties {
 
     additionalModulesToCompile = ["intellij.tools.jps.build.standalone"]
     modulesToCompileTests = ["intellij.platform.jps.build"]
-
-    productLayout.buildAllCompatiblePlugins = true
-    productLayout.prepareCustomPluginRepositoryForPublishedPlugins =
-      SystemProperties.getBooleanProperty('intellij.build.prepare.plugin.repository', false)
   }
 
   @Override
