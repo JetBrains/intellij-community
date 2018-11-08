5231c17ae631c98d4be171ca643d22043d8294ef
Aleksey Pivovarov <AMPivovarov@gmail.com>
2018-11-08 19:40:39 +0300

    IDEA-201408 vcs: build "Local Changes" tree in background thread

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/TreeModelBuilder.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/TreeModelBuilder.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vcs.changes.ui;
 
+import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.util.Key;
@@ -371,8 +372,8 @@ public class TreeModelBuilder {
     }
 
     StaticFilePath pathKey = getKey(change);
-    ChangesBrowserNode<?> parentNode =
-      notNull(GROUPING_POLICY.getRequired(subtreeRoot).getParentNodeFor(pathKey, subtreeRoot), subtreeRoot);
+    ChangesBrowserNode<?> parentNode = ReadAction.compute(
+      () -> notNull(GROUPING_POLICY.getRequired(subtreeRoot).getParentNodeFor(pathKey, subtreeRoot), subtreeRoot));
     ChangesBrowserNode<?> cachingRoot = BaseChangesGroupingPolicy.getCachingRoot(parentNode, subtreeRoot);
 
     myModel.insertNodeInto(node, parentNode, myModel.getChildCount(parentNode));
