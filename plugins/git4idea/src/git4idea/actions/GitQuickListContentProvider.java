da1f40a7f262060245d09a7ccef1abd8b7b870d8
Aleksey Pivovarov <AMPivovarov@gmail.com>
2018-11-08 16:44:27 +0300

    vcs: remove duplication

--- plugins/git4idea/src/git4idea/actions/GitQuickListContentProvider.java
+++ plugins/git4idea/src/git4idea/actions/GitQuickListContentProvider.java
@@ -1,15 +1,16 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package git4idea.actions;
 
-import com.intellij.dvcs.actions.DvcsQuickListContentProvider;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.vcs.actions.VcsQuickListContentProviderBase;
 import git4idea.GitVcs;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class GitQuickListContentProvider extends DvcsQuickListContentProvider {
+public class GitQuickListContentProvider extends VcsQuickListContentProviderBase {
   @NotNull
   @Override
   protected String getVcsName() {
@@ -17,12 +18,14 @@ public class GitQuickListContentProvider extends DvcsQuickListContentProvider {
   }
 
   @Override
-  protected void addVcsSpecificActions(@NotNull ActionManager manager, @NotNull List<AnAction> actions) {
+  protected List<AnAction> collectVcsSpecificActions(@NotNull ActionManager manager) {
+    List<AnAction> actions = new ArrayList<>();
     add("Git.Branches", manager, actions);
     add("Vcs.Push", manager, actions);
     add("Git.Stash", manager, actions);
     add("Git.Unstash", manager, actions);
 
     add("Git.ResolveConflicts", manager, actions);
+    return actions;
   }
 }
--- plugins/hg4idea/src/org/zmlx/hg4idea/provider/HgQuickListProvider.java
+++ plugins/hg4idea/src/org/zmlx/hg4idea/provider/HgQuickListProvider.java
@@ -1,19 +1,16 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.zmlx.hg4idea.provider;
 
-import com.intellij.dvcs.actions.DvcsQuickListContentProvider;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.vcs.actions.VcsQuickListContentProviderBase;
 import org.jetbrains.annotations.NotNull;
 import org.zmlx.hg4idea.HgVcs;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class HgQuickListProvider extends DvcsQuickListContentProvider {
-
-  private static final Logger LOG = Logger.getInstance(HgQuickListProvider.class.getName());
-
+public class HgQuickListProvider extends VcsQuickListContentProviderBase {
   @NotNull
   @Override
   protected String getVcsName() {
@@ -21,10 +18,12 @@ public class HgQuickListProvider extends DvcsQuickListContentProvider {
   }
 
   @Override
-  protected void addVcsSpecificActions(@NotNull ActionManager manager, @NotNull List<AnAction> actions) {
+  protected List<AnAction> collectVcsSpecificActions(@NotNull ActionManager manager) {
+    List<AnAction> actions = new ArrayList<>();
     add("hg4idea.branches", manager, actions);
     add("hg4idea.pull", manager, actions);
     add("Vcs.Push", manager, actions);
     add("hg4idea.updateTo", manager, actions);
+    return actions;
   }
 }
