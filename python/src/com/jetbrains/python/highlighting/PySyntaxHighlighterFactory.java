9ad31d400700a9961360dbce7ca794b53a4963e6
Anton Bragin <Anton.Bragin@jetbrains.com>
2018-11-08 15:09:05 +0300

    PY-32591 Question and exclamation marks are no longer highlighted as bad characters in console
    Check performed by PydevConsoleRunner.isInPydevConsole made independent on console initialization.
    PyConsoleHighlightingLexer put back into work for console code lexing.

--- python/src/com/jetbrains/python/highlighting/PySyntaxHighlighterFactory.java
+++ python/src/com/jetbrains/python/highlighting/PySyntaxHighlighterFactory.java
@@ -1,11 +1,15 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.python.highlighting;
 
+import com.intellij.injected.editor.VirtualFileWindow;
 import com.intellij.openapi.fileTypes.SyntaxHighlighter;
 import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
 import com.intellij.util.containers.FactoryMap;
+import com.jetbrains.python.console.PydevConsoleRunner;
 import com.jetbrains.python.console.parsing.PyConsoleHighlightingLexer;
 import com.jetbrains.python.lexer.PythonHighlightingLexer;
 import com.jetbrains.python.psi.LanguageLevel;
@@ -34,6 +38,9 @@ public class PySyntaxHighlighterFactory extends SyntaxHighlighterFactory {
     final LanguageLevel level = project != null && virtualFile != null ?
                                 PyUtil.getLanguageLevelForVirtualFile(project, virtualFile) :
                                 LanguageLevel.getDefault();
+    if (useConsoleLexer(project, virtualFile)) {
+      return myConsoleMap.get(level);
+    }
     return getSyntaxHighlighterForLanguageLevel(level);
   }
 
@@ -41,7 +48,15 @@ public class PySyntaxHighlighterFactory extends SyntaxHighlighterFactory {
    * Returns a syntax highlighter targeting the specified version of Python.
    */
   @NotNull
-  public SyntaxHighlighter getSyntaxHighlighterForLanguageLevel(@NotNull LanguageLevel version) {
-    return myMap.get(version);
+  public SyntaxHighlighter getSyntaxHighlighterForLanguageLevel(@NotNull LanguageLevel level) {
+    return myMap.get(level);
+  }
+
+  private static boolean useConsoleLexer(@Nullable final Project project, @Nullable final VirtualFile virtualFile) {
+    if (virtualFile == null || project == null || virtualFile instanceof VirtualFileWindow) {
+      return false;
+    }
+    PsiFile psiFile = PsiManager.getInstance(project).findFile(virtualFile);
+    return psiFile != null && PydevConsoleRunner.isInPydevConsole(psiFile);
   }
 }
