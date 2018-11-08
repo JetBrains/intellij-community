JetBrains/intellij-community

commit 2af28891cdc9a83605622e3395515b4e1828bd95

speed up opening of editor containing a lot of carets

platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorImpl.java
@@ -909,9 +909,7 @@ public final class EditorImpl extends UserDataHolderBase implements EditorEx, Hi
     myCaretModel.updateVisualPosition();
 
     // make sure carets won't appear at invalid positions (e.g. on Tab width change)
-    for (Caret caret : getCaretModel().getAllCarets()) {
-      caret.moveToOffset(caret.getOffset());
-    }
+    getCaretModel().doWithCaretMerging(() -> myCaretModel.getAllCarets().forEach(caret -> caret.moveToOffset(caret.getOffset())));
 
     if (myVirtualFile != null && myProject != null) {
       final EditorNotifications editorNotifications = EditorNotifications.getInstance(myProject);

commit ca9f3cd09b20fcdef8700d5d55ec02efd3ad38bf

IDEA-201882 Scrolling position jumps on completion
regression after commit 2af28891 (speed up opening of editor containing a lot of carets)

platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorImpl.java
@@ -1566,7 +1566,7 @@ public final class EditorImpl extends UserDataHolderBase implements EditorEx, Hi
       return;
     }
 
-    if (getCaretModel().getOffset() < e.getOffset() || getCaretModel().getOffset() > e.getOffset() + e.getNewLength())  {
+    if (getCaretModel().getOffset() < e.getOffset() || getCaretModel().getOffset() > e.getOffset() + e.getOldLength())  {
       saveCaretRelativePosition();
     }
     else {