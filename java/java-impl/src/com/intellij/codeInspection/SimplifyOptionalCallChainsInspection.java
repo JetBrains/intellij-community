6a5c208e49ea4651f4b74acd88d71542a771168a
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2018-11-09 15:31:53 +0700

    IDEA-CR-39590: SimplifyOptionalCallChainsInspection: specify lambda parameter types when converting method reference to lambda
    Fixes IDEA-201902 Simplify optional chain (use Optional.stream())
    
    (cherry picked from commit 1056b3a3298d40b1d4d1abdc962362644a11d52d)

--- java/java-impl/src/com/intellij/codeInspection/SimplifyOptionalCallChainsInspection.java
+++ java/java-impl/src/com/intellij/codeInspection/SimplifyOptionalCallChainsInspection.java
@@ -4,7 +4,6 @@ package com.intellij.codeInspection;
 import com.intellij.codeInspection.dataFlow.CommonDataflow;
 import com.intellij.codeInspection.dataFlow.DfaFactType;
 import com.intellij.codeInspection.util.LambdaGenerationUtil;
-import com.intellij.codeInspection.util.OptionalUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.pom.java.LanguageLevel;
 import com.intellij.psi.*;
@@ -152,7 +151,7 @@ public class SimplifyOptionalCallChainsInspection extends AbstractBaseJavaLocalI
       if (qualifier == null) return;
       String opt = qualifier.getText();
       PsiParameter parameter = parameters[0];
-      String proposed = OptionalUtil.generateOptionalUnwrap(opt, parameter, trueArg, falseArg, call.getType(), useOrElseGet);
+      String proposed = generateOptionalUnwrap(opt, parameter, trueArg, falseArg, call.getType(), useOrElseGet);
       String canonicalOrElse;
       if (useOrElseGet && !ExpressionUtils.isSafelyRecomputableExpression(falseArg)) {
         canonicalOrElse = ".orElseGet(() -> " + falseArg.getText() + ")";
@@ -168,7 +167,7 @@ public class SimplifyOptionalCallChainsInspection extends AbstractBaseJavaLocalI
         } else if(opt.length() > 10) {
           // should be a parseable expression
           opt = "(($))";
-          String template = OptionalUtil.generateOptionalUnwrap(opt, parameter, trueArg, falseArg, call.getType(), useOrElseGet);
+          String template = generateOptionalUnwrap(opt, parameter, trueArg, falseArg, call.getType(), useOrElseGet);
           displayCode =
             PsiExpressionTrimRenderer.render(JavaPsiFacade.getElementFactory(parameter.getProject()).createExpressionFromText(template, call));
           displayCode = displayCode.replaceFirst(Pattern.quote(opt), "..");
@@ -207,7 +206,10 @@ public class SimplifyOptionalCallChainsInspection extends AbstractBaseJavaLocalI
       return (PsiLambdaExpression)expression;
     }
     if (expression instanceof PsiMethodReferenceExpression) {
-      return LambdaRefactoringUtil.createLambda((PsiMethodReferenceExpression)expression, true);
+      PsiMethodReferenceExpression methodRef = (PsiMethodReferenceExpression)expression;
+      PsiLambdaExpression lambda = LambdaRefactoringUtil.createLambda(methodRef, true);
+      LambdaRefactoringUtil.specifyLambdaParameterTypes(methodRef.getFunctionalInterfaceType(), lambda);
+      return lambda;
     }
     return null;
   }
