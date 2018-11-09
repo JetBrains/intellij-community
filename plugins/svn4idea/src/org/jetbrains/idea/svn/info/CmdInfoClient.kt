c89eeac5d0f03a9975254ef8bac8d055a26aef82
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2018-11-09 20:39:45 +0300

    svn: Convert "CmdInfoClient" to kotlin

--- plugins/svn4idea/src/org/jetbrains/idea/svn/info/CmdInfoClient.kt
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/info/CmdInfoClient.kt
@@ -1,190 +1,168 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.jetbrains.idea.svn.info;
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.idea.svn.info
 
-import com.intellij.execution.process.ProcessOutput;
-import com.intellij.execution.process.ProcessOutputTypes;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.util.Key;
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.openapi.vfs.CharsetToolkit;
-import com.intellij.util.containers.ContainerUtil;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.idea.svn.api.BaseSvnClient;
-import org.jetbrains.idea.svn.api.Depth;
-import org.jetbrains.idea.svn.api.Revision;
-import org.jetbrains.idea.svn.api.Target;
-import org.jetbrains.idea.svn.commandLine.*;
-import org.xml.sax.SAXException;
+import com.intellij.execution.process.ProcessOutput
+import com.intellij.execution.process.ProcessOutputTypes
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.util.Key
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.openapi.vfs.CharsetToolkit
+import com.intellij.util.containers.ContainerUtil
+import org.jetbrains.idea.svn.api.BaseSvnClient
+import org.jetbrains.idea.svn.api.Depth
+import org.jetbrains.idea.svn.api.Revision
+import org.jetbrains.idea.svn.api.Target
+import org.jetbrains.idea.svn.commandLine.*
+import org.xml.sax.SAXException
+import java.io.ByteArrayInputStream
+import java.io.File
+import java.io.IOException
+import javax.xml.parsers.ParserConfigurationException
+import javax.xml.parsers.SAXParserFactory
 
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.util.Collection;
-import java.util.List;
+private val LOG = logger<CmdInfoClient>()
 
-public class CmdInfoClient extends BaseSvnClient implements InfoClient {
+private fun parseResult(base: File?, result: String?): Info? {
+  val handler = CollectInfoHandler()
 
-  private static final Logger LOG = Logger.getInstance(CmdInfoClient.class);
+  parseResult(handler, base, result)
 
-  private String execute(@NotNull List<String> parameters, @NotNull File path) throws SvnBindException {
+  return handler.info
+}
+
+private fun parseResult(handler: InfoConsumer, base: File?, result: String?) {
+  if (StringUtil.isEmptyOrSpaces(result)) {
+    return
+  }
+
+  val infoHandler = SvnInfoHandler(base) { info ->
+    try {
+      handler.consume(info)
+    }
+    catch (e: SvnBindException) {
+      throw SvnExceptionWrapper(e)
+    }
+  }
+
+  parseResult(result!!, infoHandler)
+}
+
+private fun parseResult(result: String, handler: SvnInfoHandler) {
+  try {
+    val parser = SAXParserFactory.newInstance().newSAXParser()
+
+    parser.parse(ByteArrayInputStream(result.trim { it <= ' ' }.toByteArray(CharsetToolkit.UTF8_CHARSET)), handler)
+  }
+  catch (e: SvnExceptionWrapper) {
+    LOG.info("info output $result")
+    throw SvnBindException(e.cause)
+  }
+  catch (e: IOException) {
+    LOG.info("info output $result")
+    throw SvnBindException(e)
+  }
+  catch (e: SAXException) {
+    LOG.info("info output $result")
+    throw SvnBindException(e)
+  }
+  catch (e: ParserConfigurationException) {
+    LOG.info("info output $result")
+    throw SvnBindException(e)
+  }
+}
+
+private fun buildParameters(target: Target, revision: Revision?, depth: Depth?): List<String> {
+  val parameters = ContainerUtil.newArrayList<String>()
+
+  CommandUtil.put(parameters, depth)
+  CommandUtil.put(parameters, revision)
+  CommandUtil.put(parameters, target)
+  parameters.add("--xml")
+
+  return parameters
+}
+
+class CmdInfoClient : BaseSvnClient(), InfoClient {
+  private fun execute(parameters: List<String>, path: File): String {
     // workaround: separately capture command output - used in exception handling logic to overcome svn 1.8 issue (see below)
-    final ProcessOutput output = new ProcessOutput();
-    LineCommandListener listener = new LineCommandAdapter() {
-      @Override
-      public void onLineAvailable(String line, Key outputType) {
-        if (outputType == ProcessOutputTypes.STDOUT) {
-          output.appendStdout(line);
+    val output = ProcessOutput()
+    val listener = object : LineCommandAdapter() {
+      override fun onLineAvailable(line: String, outputType: Key<*>) {
+        if (outputType === ProcessOutputTypes.STDOUT) {
+          output.appendStdout(line)
         }
       }
-    };
+    }
 
     try {
-      CommandExecutor command = execute(myVcs, Target.on(path), SvnCommandName.info, parameters, listener);
+      val command = execute(myVcs, Target.on(path), SvnCommandName.info, parameters, listener)
 
-      return command.getOutput();
+      return command.output
     }
-    catch (SvnBindException e) {
-      final String text = StringUtil.notNullize(e.getMessage());
+    catch (e: SvnBindException) {
+      val text = StringUtil.notNullize(e.message)
       if (text.contains("W155010")) {
         // if "svn info" is executed for several files at once, then this warning could be printed only for some files, but info for other
         // files should be parsed from output
-        return output.getStdout();
+        return output.stdout
       }
       // not a working copy exception
       // "E155007: '' is not a working copy"
-      if (text.contains("is not a working copy") && StringUtil.isNotEmpty(output.getStdout())) {
+      if (text.contains("is not a working copy") && StringUtil.isNotEmpty(output.stdout)) {
         // TODO: Seems not reproducible in 1.8.4
         // workaround: as in subversion 1.8 "svn info" on a working copy root outputs such error for parent folder,
         // if there are files with conflicts.
         // but the requested info is still in the output except root closing tag
-        return output.getStdout() + "</info>";
+        return output.stdout + "</info>"
       }
-      throw e;
+      throw e
     }
   }
 
-  @Nullable
-  private static Info parseResult(@Nullable File base, @Nullable String result) throws SvnBindException {
-    CollectInfoHandler handler = new CollectInfoHandler();
+  @Throws(SvnBindException::class)
+  override fun doInfo(path: File, revision: Revision?): Info? {
+    val base = CommandUtil.requireExistingParent(path)
 
-    parseResult(handler, base, result);
-
-    return handler.getInfo();
+    return parseResult(base, execute(buildParameters(Target.on(path), revision, Depth.EMPTY), path))
   }
 
-  private static void parseResult(@NotNull final InfoConsumer handler, @Nullable File base, @Nullable String result)
-    throws SvnBindException {
-    if (StringUtil.isEmptyOrSpaces(result)) {
-      return;
-    }
+  @Throws(SvnBindException::class)
+  override fun doInfo(target: Target, revision: Revision?): Info? {
+    assertUrl(target)
 
-    final SvnInfoHandler infoHandler = new SvnInfoHandler(base, info -> {
-      try {
-        handler.consume(info);
-      }
-      catch (SvnBindException e) {
-        throw new SvnExceptionWrapper(e);
-      }
-    });
+    val command = execute(myVcs, target, SvnCommandName.info, buildParameters(target, revision, Depth.EMPTY), null)
 
-    parseResult(result, infoHandler);
+    return parseResult(null, command.output)
   }
 
-  private static void parseResult(@NotNull String result, @NotNull SvnInfoHandler handler) throws SvnBindException {
-    try {
-      SAXParser parser = SAXParserFactory.newInstance().newSAXParser();
-
-      parser.parse(new ByteArrayInputStream(result.trim().getBytes(CharsetToolkit.UTF8_CHARSET)), handler);
-    }
-    catch (SvnExceptionWrapper e) {
-      LOG.info("info output " + result);
-      throw new SvnBindException(e.getCause());
-    } catch (IOException | SAXException | ParserConfigurationException e) {
-      LOG.info("info output " + result);
-      throw new SvnBindException(e);
-    }
-  }
-
-  @NotNull
-  private static List<String> buildParameters(@NotNull Target target, @Nullable Revision revision, @Nullable Depth depth) {
-    List<String> parameters = ContainerUtil.newArrayList();
-
-    CommandUtil.put(parameters, depth);
-    CommandUtil.put(parameters, revision);
-    CommandUtil.put(parameters, target);
-    parameters.add("--xml");
-
-    return parameters;
-  }
-
-  @Override
-  public Info doInfo(@NotNull File path, @Nullable Revision revision) throws SvnBindException {
-    File base = CommandUtil.requireExistingParent(path);
-
-    return parseResult(base, execute(buildParameters(Target.on(path), revision, Depth.EMPTY), path));
-  }
-
-  @Override
-  public Info doInfo(@NotNull Target target, @Nullable Revision revision) throws SvnBindException {
-    assertUrl(target);
-
-    CommandExecutor command = execute(myVcs, target, SvnCommandName.info, buildParameters(target, revision, Depth.EMPTY), null);
-
-    return parseResult(null, command.getOutput());
-  }
-
-  @Override
-  public void doInfo(@NotNull Collection<File> paths, @Nullable InfoConsumer handler) throws SvnBindException {
-    File base = ContainerUtil.getFirstItem(paths);
+  @Throws(SvnBindException::class)
+  override fun doInfo(paths: Collection<File>, handler: InfoConsumer?) {
+    var base = ContainerUtil.getFirstItem(paths)
 
     if (base != null) {
-      base = CommandUtil.requireExistingParent(base);
+      base = CommandUtil.requireExistingParent(base)
 
-      List<String> parameters = ContainerUtil.newArrayList();
-      for (File file : paths) {
-        CommandUtil.put(parameters, file);
+      val parameters = ContainerUtil.newArrayList<String>()
+      for (file in paths) {
+        CommandUtil.put(parameters, file)
       }
-      parameters.add("--xml");
+      parameters.add("--xml")
 
       // Currently do not handle exceptions here like in SvnVcs.handleInfoException - just continue with parsing in case of warnings for
       // some of the requested items
-      String result = execute(parameters, base);
+      val result = execute(parameters, base)
       if (handler != null) {
-        parseResult(handler, base, result);
+        parseResult(handler, base, result)
       }
     }
   }
+}
 
-  private static class CollectInfoHandler implements InfoConsumer {
+private class CollectInfoHandler : InfoConsumer {
+  var info: Info? = null
+    private set
 
-    @Nullable private Info myInfo;
-
-    @Override
-    public void consume(Info info) {
-      myInfo = info;
-    }
-
-    @Nullable
-    public Info getInfo() {
-      return myInfo;
-    }
+  override fun consume(info: Info) {
+    this.info = info
   }
 }
