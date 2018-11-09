8902d37d01702914660c91143a09bf3c96e0f3dc
Shaverdova Elena <Elena.Shaverdova@jetbrains.com>
2018-11-09 22:31:42 +0100

    IDEA-CR-39591: Move pluginId report into ide.error group to help uniting consequent errors
    (cherry picked from commit e81b8a840f695f20976b7459f88b04c235363b68)

--- platform/platform-impl/src/com/intellij/featureStatistics/fusCollectors/AppLifecycleUsageTriggerCollector.java
+++ platform/platform-impl/src/com/intellij/featureStatistics/fusCollectors/AppLifecycleUsageTriggerCollector.java
@@ -3,8 +3,11 @@ package com.intellij.featureStatistics.fusCollectors;
 
 import com.intellij.internal.statistic.service.fus.collectors.ApplicationUsageTriggerCollector;
 import com.intellij.internal.statistic.service.fus.collectors.FUSApplicationUsageTrigger;
+import com.intellij.internal.statistic.service.fus.collectors.FUSUsageContext;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.util.text.StringUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class AppLifecycleUsageTriggerCollector extends ApplicationUsageTriggerCollector {
   private static final Logger LOG = Logger.getInstance("#com.intellij.featureStatistics.fusCollectors.AppLifecycleUsageTriggerCollector");
@@ -15,16 +18,12 @@ public class AppLifecycleUsageTriggerCollector extends ApplicationUsageTriggerCo
     return "statistics.lifecycle.app";
   }
 
-  public static void onError(boolean isOOM, boolean isMappingFailed) {
+  public static void onError(boolean isOOM, boolean isMappingFailed, @Nullable String pluginId) {
     try {
-      String key = "ide.error";
-      if (isOOM) {
-        key += ".oom";
-      }
-      if (isMappingFailed) {
-        key += ".mappingFailed";
-      }
-      FUSApplicationUsageTrigger.getInstance().trigger(AppLifecycleUsageTriggerCollector.class, key);
+      FUSApplicationUsageTrigger.getInstance().trigger(AppLifecycleUsageTriggerCollector.class, "ide.error",
+                                                       FUSUsageContext.create("OOM:" + isOOM,
+                                                                              "mappingFailed:" + isMappingFailed,
+                                                                              "pluginId:" + StringUtil.notNullize(pluginId, "unknown")));
     }
     catch (Exception e) {
       LOG.warn(e);
