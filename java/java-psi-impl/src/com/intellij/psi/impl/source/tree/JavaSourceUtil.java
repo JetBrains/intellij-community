984c5ac4e077aa9350003ead2c60f1f61611a1cc
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2018-11-12 14:45:29 +0100

    Cleanup (duplicates; formatting)

--- java/java-psi-impl/src/com/intellij/psi/impl/source/tree/JavaSourceUtil.java
+++ java/java-psi-impl/src/com/intellij/psi/impl/source/tree/JavaSourceUtil.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.impl.source.tree;
 
 import com.intellij.lang.ASTFactory;
@@ -26,7 +12,7 @@ import com.intellij.psi.impl.PsiImplUtil;
 import com.intellij.psi.impl.source.DummyHolder;
 import com.intellij.psi.impl.source.SourceJavaCodeReference;
 import com.intellij.psi.impl.source.SourceTreeToPsiMap;
-import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.impl.source.tree.java.ReplaceExpressionUtil;
 import com.intellij.psi.tree.TokenSet;
 import com.intellij.util.CharTable;
 import com.intellij.util.IncorrectOperationException;
@@ -114,13 +100,17 @@ public class JavaSourceUtil {
     return LightTreeUtil.toFilteredString(tree, node, REF_FILTER);
   }
 
-  public static TreeElement addParenthToReplacedChild(@NotNull IElementType parenthType,
-                                                      @NotNull TreeElement newChild,
-                                                      @NotNull PsiManager manager) {
-    CompositeElement parenthExpr = ASTFactory.composite(parenthType);
+  @NotNull
+  public static TreeElement addParenthToReplacedChild(@NotNull ASTNode child, @NotNull TreeElement newChild, @NotNull PsiManager manager) {
+    boolean needParenth = ElementType.EXPRESSION_BIT_SET.contains(child.getElementType()) &&
+                          ElementType.EXPRESSION_BIT_SET.contains(newChild.getElementType()) &&
+                          ReplaceExpressionUtil.isNeedParenthesis(child, newChild);
+    if (!needParenth) return newChild;
+
+    CompositeElement parenthExpr = ASTFactory.composite(JavaElementType.PARENTH_EXPRESSION);
 
     TreeElement dummyExpr = (TreeElement)newChild.clone();
-    final CharTable charTableByTree = SharedImplUtil.findCharTableByTree(newChild);
+    CharTable charTableByTree = SharedImplUtil.findCharTableByTree(newChild);
     new DummyHolder(manager, parenthExpr, null, charTableByTree);
     parenthExpr.putUserData(CharTable.CHAR_TABLE_KEY, charTableByTree);
     parenthExpr.rawAddChildren(ASTFactory.leaf(JavaTokenType.LPARENTH, "("));
