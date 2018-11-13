41e1b154be282b72b6395ecc1d176f1e1601eddd
peter <peter@jetbrains.com>
2018-11-13 19:30:03 +0100

    remove busy waits from jps/groovyc interaction to save CPU

--- plugins/groovy/rt-constants/src/org/jetbrains/groovy/compiler/rt/GroovyRtConstants.java
+++ plugins/groovy/rt-constants/src/org/jetbrains/groovy/compiler/rt/GroovyRtConstants.java
@@ -37,7 +37,6 @@ public class GroovyRtConstants {
   public static final String OPTIMIZE = "optimize";
   public static final String GROOVYC_STUB_GENERATION_FAILED = "Groovyc stub generation failed";
 
-  public static final String STUBS_GENERATED = "Stubs generated";
   public static final String JAVAC_COMPLETED = "Javac completed";
   public static final String BUILD_ABORTED = "Build aborted";
 
