8bc8dec02e1f905c18aec44151a60a1e4096f784
peter <peter@jetbrains.com>
2018-11-13 20:51:35 +0100

    more speedup for DaemonRespondToChangesTest.testExpressionListsWithManyStringLiteralsHighlightingPerformance
    less code, less duplication, less CPU usage

--- java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/impl/DaemonRespondToChangesTest.java
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/impl/DaemonRespondToChangesTest.java
@@ -1231,7 +1231,7 @@ public class DaemonRespondToChangesTest extends DaemonAnalyzerTestCase {
                   "}";
     configureByText(StdFileTypes.JAVA, text);
 
-    PlatformTestUtil.startPerformanceTest("highlighting many string literals", 30_000, () -> {
+    PlatformTestUtil.startPerformanceTest("highlighting many string literals", 15_000, () -> {
       assertEmpty(highlightErrors());
 
       type("k");
