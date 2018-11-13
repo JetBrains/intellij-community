1c860e2ef6a0698a5327ef9b0cfb90b1c5dcd9a3
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2018-11-13 23:13:14 +0300

    [vcs-log-graph] move getCorrespondingParent tests to separate class GraphUtilTests

--- platform/vcs-log/graph/test/com/intellij/vcs/log/graph/utils/BfsTests.kt
+++ platform/vcs-log/graph/test/com/intellij/vcs/log/graph/utils/BfsTests.kt
@@ -1,54 +1,12 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.vcs.log.graph.utils
 
-import com.intellij.vcs.log.graph.TestGraphBuilder
-import com.intellij.vcs.log.graph.asString
 import com.intellij.vcs.log.graph.graph
 import com.intellij.vcs.log.graph.utils.impl.BitSetFlags
 import org.junit.Test
 import kotlin.test.assertEquals
 
 class BfsTests {
-
-  private fun assertCorrespondingParent(startNode: Int, endNode: Int, expectedParent: Int, graphBuilder: TestGraphBuilder.() -> Unit) {
-    val graph = graph(graphBuilder)
-    val actualParent = LinearGraphUtils.asLiteLinearGraph(graph).getCorrespondingParent(startNode, endNode, BitSetFlags(graph.nodesCount()))
-    assertEquals(expectedParent, actualParent,
-                 "Incorrect parent found when walking from ${startNode} to ${endNode} in ${graph.asString(true)}")
-  }
-
-  @Test
-  fun `one parent`() {
-    assertCorrespondingParent(0, 5, 1) {
-      0(1)
-      1(2)
-      2(3, 4)
-      3(5)
-      4(5)
-      5()
-    }
-  }
-
-  @Test
-  fun `target parent`() {
-    assertCorrespondingParent(0, 2, 2) {
-      0(1, 2)
-      1(3)
-      2(3)
-      3()
-    }
-  }
-
-  @Test
-  fun `merge commit`() {
-    assertCorrespondingParent(0, 4, 2) {
-      0(1, 2)
-      1(3)
-      2(3, 4)
-      3()
-      4()
-    }
-  }
 
   @Test
   fun `test visited`() {
