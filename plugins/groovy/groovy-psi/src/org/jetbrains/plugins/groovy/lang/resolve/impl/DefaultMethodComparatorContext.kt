c8e406a7a5a28653640ae75b0dfcacfce51393d2
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2018-11-13 17:32:02 +0300

    [groovy] expose ErasedArgument for using it elsewhere

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/resolve/impl/DefaultMethodComparatorContext.kt
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/resolve/impl/DefaultMethodComparatorContext.kt
@@ -3,15 +3,14 @@ package org.jetbrains.plugins.groovy.lang.resolve.impl
 
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiType
-import com.intellij.psi.util.TypeConversionUtil
 import org.jetbrains.plugins.groovy.lang.resolve.GrMethodComparator
-import org.jetbrains.plugins.groovy.lang.resolve.api.Argument
 import org.jetbrains.plugins.groovy.lang.resolve.api.Arguments
+import org.jetbrains.plugins.groovy.lang.resolve.api.ErasedArgument
 
 class DefaultMethodComparatorContext(private val place: PsiElement, arguments: Arguments?) : GrMethodComparator.Context {
 
   private val erasedArguments by lazy {
-    arguments?.map(DefaultMethodComparatorContext::ErasedArgument)
+    arguments?.map(::ErasedArgument)
   }
 
   override fun getArguments(): Arguments? = erasedArguments
@@ -21,10 +20,4 @@ class DefaultMethodComparatorContext(private val place: PsiElement, arguments: A
   override fun getPlace(): PsiElement = place
 
   override fun isConstructor(): Boolean = false
-
-  private class ErasedArgument(original: Argument) : Argument {
-    override val type: PsiType? by lazy {
-      TypeConversionUtil.erasure(original.topLevelType)
-    }
-  }
 }
