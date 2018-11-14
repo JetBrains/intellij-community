5c983af87539357523d155430b519e6f4b14fa04
peter <peter@jetbrains.com>
2018-11-14 15:25:11 +0100

    backport mad testing fixes

--- platform/testFramework/src/com/intellij/testFramework/propertyBased/InvokeIntention.java
+++ platform/testFramework/src/com/intellij/testFramework/propertyBased/InvokeIntention.java
@@ -36,7 +36,6 @@ import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.*;
-import com.intellij.psi.impl.DebugUtil;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.util.PsiUtilBase;
 import com.intellij.testFramework.PsiTestUtil;
@@ -86,19 +85,22 @@ public class InvokeIntention extends ActionOnFile {
     Editor editor = FileEditorManager.getInstance(project).openTextEditor(new OpenFileDescriptor(project, getVirtualFile(), offset), true);
     assert editor != null;
 
-    boolean containsErrorElements = MadTestingUtil.containsErrorElements(getFile().getViewProvider());
+    PsiDocumentManager.getInstance(project).commitAllDocuments();
+
+    FileViewProvider viewProvider = getFile().getViewProvider();
+    boolean containsErrorElements = MadTestingUtil.containsErrorElements(viewProvider);
+
     List<HighlightInfo> errors = highlightErrors(project, editor);
     boolean hasErrors = !errors.isEmpty() || containsErrorElements;
-
-    String treesBefore = getFile().getViewProvider().getAllFiles().stream().map(f -> DebugUtil.psiToString(f, false)).collect(
-      Collectors.joining("\n\n"));
 
     PsiFile file = PsiUtilBase.getPsiFileInEditor(editor, getProject());
     assert file != null;
     List<IntentionAction> intentions = getAvailableIntentions(editor, file);
     // Do not reuse originally passed offset here, sometimes it's adjusted by Editor
     PsiElement currentElement = file.findElementAt(editor.getCaretModel().getOffset());
-    intentions = wrapAndCheck(env, editor, currentElement, containsErrorElements, hasErrors, intentions);
+    if (!containsErrorElements) {
+      intentions = wrapAndCheck(env, editor, currentElement, hasErrors, intentions);
+    }
     IntentionAction intention = chooseIntention(env, intentions);
     if (intention == null) return;
 
@@ -147,13 +149,6 @@ public class InvokeIntention extends ActionOnFile {
           message += ".\nIf it's by design that " + intentionString + " doesn't change source files, " +
                      "it should return false from 'startInWriteAction'";
         }
-        message += "\n  Debug info: " + treesBefore + "\n\nafter:" +
-                   file.getViewProvider().getAllFiles().stream().map(
-                     f ->
-                       DebugUtil.psiToString(f, false) +
-                       "\n\n" +
-                       SyntaxTraverser.psiTraverser(file).filter(PsiErrorElement.class).toList()
-                   ).collect(Collectors.joining("\n\n"));
         throw new AssertionError(message);
       }
 
@@ -186,7 +181,6 @@ public class InvokeIntention extends ActionOnFile {
   private List<IntentionAction> wrapAndCheck(Environment env,
                                              Editor editor,
                                              PsiElement currentElement,
-                                             boolean containsErrorElements,
                                              boolean hasErrors,
                                              List<IntentionAction> intentions) {
     if (currentElement == null) return intentions;
@@ -219,14 +213,13 @@ public class InvokeIntention extends ActionOnFile {
     List<String> messages = new ArrayList<>();
 
     boolean newContainsErrorElements = MadTestingUtil.containsErrorElements(getFile().getViewProvider());
-    if (newContainsErrorElements != containsErrorElements) {
-      messages.add(newContainsErrorElements ? "File contains parse errors after wrapping" : "File parse errors were fixed after wrapping");
+    if (newContainsErrorElements) {
+      messages.add("File contains parse errors after wrapping");
     }
     else {
-      boolean newHasErrors = !highlightErrors(project, editor).isEmpty() || containsErrorElements;
+      boolean newHasErrors = !highlightErrors(project, editor).isEmpty();
       if (newHasErrors != hasErrors) {
-        messages
-          .add(newHasErrors ? "File contains errors after wrapping" : "File errors were fixed after wrapping");
+        messages.add(newHasErrors ? "File contains errors after wrapping" : "File errors were fixed after wrapping");
       }
     }
     intentions = getAvailableIntentions(editor, file);
@@ -258,8 +251,8 @@ public class InvokeIntention extends ActionOnFile {
 
   private static String describeIntentions(Map<String, IntentionAction> intentionMap) {
     return EntryStream.of(intentionMap)
-                   .mapKeyValue(MadTestingUtil::getIntentionDescription)
-                   .map("\t"::concat).joining("\n");
+      .mapKeyValue(MadTestingUtil::getIntentionDescription)
+      .map("\t"::concat).joining("\n");
   }
 
   private void restoreAfterPotentialPsiTextInconsistency() {
@@ -268,11 +261,11 @@ public class InvokeIntention extends ActionOnFile {
 
   protected List<String> extractCommentsReformattedToSingleWhitespace(PsiFile file) {
     return PsiTreeUtil.findChildrenOfType(file, PsiComment.class)
-                      .stream()
-                      .filter(myPolicy::trackComment)
-                      .map(PsiElement::getText)
-                      .map(text -> text.replaceAll("[\\s*]+", " "))
-                      .collect(Collectors.toList());
+      .stream()
+      .filter(myPolicy::trackComment)
+      .map(PsiElement::getText)
+      .map(text -> text.replaceAll("[\\s*]+", " "))
+      .collect(Collectors.toList());
   }
 
   private static void checkNoNewErrors(Project project, Editor editor, String intentionString, IntentionPolicy policy) {
