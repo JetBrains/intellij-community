JetBrains/intellij-community

commit b2a6c9dd480937fd724e76ac88fdffd816cae5b1

[vcs-log] pass only files in specific root to provider

platform/vcs-log/impl/src/com/intellij/vcs/log/visible/VcsLogFiltererImpl.java
@@ -18,6 +18,7 @@ package com.intellij.vcs.log.visible;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.vcs.FilePath;
 import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.containers.ContainerUtil;
@@ -312,12 +313,9 @@ public class VcsLogFiltererImpl implements VcsLogFilterer {
         continue;
       }
 
-      VcsLogFilterCollection rootSpecificCollection = filterCollection;
-      if (rootSpecificCollection.get(VcsLogFilterCollection.ROOT_FILTER) != null) {
-        rootSpecificCollection = new VcsLogFilterCollectionBuilder(filterCollection)
-          .with(new VcsLogStructureFilterImpl(ContainerUtil.newHashSet(VcsLogUtil.getFilteredFilesForRoot(root, filterCollection))))
-          .build();
-      }
+      Set<FilePath> filesForRoot = VcsLogUtil.getFilteredFilesForRoot(root, filterCollection);
+      VcsLogStructureFilterImpl structureFilter = filesForRoot.isEmpty() ? null : new VcsLogStructureFilterImpl(filesForRoot);
+      VcsLogFilterCollection rootSpecificCollection = new VcsLogFilterCollectionBuilder(filterCollection).with(structureFilter).build();
 
       List<TimedVcsCommit> matchingCommits = entry.getValue().getCommitsMatchingFilter(root, rootSpecificCollection, maxCount);
       commits.addAll(ContainerUtil.map(matchingCommits, commit -> new CommitId(commit.getId(), root)));

commit e1cb84864cc89dd6a32af40832c0f61f9a5f07e4

[vcs-log] remove structure filter if filtered files are not inside the root
Got broken in b2a6c9dd480937fd724e76ac88fdffd816cae5b1, where it was not taken into account that method "with" does not nothing when given a null filter.

EA-130165

platform/vcs-log/impl/src/com/intellij/vcs/log/visible/VcsLogFiltererImpl.java
@@ -326,8 +326,13 @@ public class VcsLogFiltererImpl implements VcsLogFilterer {
       }
 
       Set<FilePath> filesForRoot = VcsLogUtil.getFilteredFilesForRoot(root, filterCollection);
-      VcsLogStructureFilter structureFilter = filesForRoot.isEmpty() ? null : VcsLogFilterObject.fromPaths(filesForRoot);
-      VcsLogFilterCollection rootSpecificCollection = VcsLogFiltersKt.with(filterCollection, structureFilter);
+      VcsLogFilterCollection rootSpecificCollection;
+      if (filesForRoot.isEmpty()) {
+        rootSpecificCollection = VcsLogFiltersKt.without(filterCollection, VcsLogFilterCollection.STRUCTURE_FILTER);
+      }
+      else {
+        rootSpecificCollection = VcsLogFiltersKt.with(filterCollection, VcsLogFilterObject.fromPaths(filesForRoot));
+      }
 
       List<TimedVcsCommit> matchingCommits = entry.getValue().getCommitsMatchingFilter(root, rootSpecificCollection, maxCount);
       commits.addAll(ContainerUtil.map(matchingCommits, commit -> new CommitId(commit.getId(), root)));