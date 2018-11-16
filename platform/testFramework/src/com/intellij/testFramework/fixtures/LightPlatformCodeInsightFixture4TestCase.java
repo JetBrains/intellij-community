83016f264eff6a7d1934e59758ede56a153d43c4
Gregory.Shrago <gregory.shrago@jetbrains.com>
2018-11-16 04:06:27 +0300

    introduce LightPlatformCodeInsightFixture4TestCase for JUnit4 tests

--- platform/testFramework/src/com/intellij/testFramework/fixtures/LightPlatformCodeInsightFixture4TestCase.java
+++ platform/testFramework/src/com/intellij/testFramework/fixtures/LightPlatformCodeInsightFixture4TestCase.java
@@ -1,7 +1,9 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.testFramework;
+package com.intellij.testFramework.fixtures;
 
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.testFramework.EdtTestUtil;
+import com.intellij.testFramework.TestRunnerUtil;
 import org.junit.Rule;
 import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
@@ -12,7 +14,7 @@ import org.junit.runners.model.Statement;
  * @author gregsh
  */
 @RunWith(JUnit4.class)
-public abstract class LightPlatform4TestCase extends LightPlatformTestCase {
+public abstract class LightPlatformCodeInsightFixture4TestCase extends LightPlatformCodeInsightFixtureTestCase {
   @Rule
   public TestRule rule = (base, description) -> new Statement() {
     @Override
