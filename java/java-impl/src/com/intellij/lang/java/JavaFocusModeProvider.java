e4dcefe5ae95320d00c526d15072c14e41ae9291
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2018-11-18 22:25:08 +0300

    focus mode: update the model synchronously
    relates to IDEA-102757

--- java/java-impl/src/com/intellij/lang/java/JavaFocusModeProvider.java
+++ java/java-impl/src/com/intellij/lang/java/JavaFocusModeProvider.java
@@ -3,16 +3,21 @@ package com.intellij.lang.java;
 
 import com.intellij.codeInsight.daemon.impl.focusMode.FocusModeProvider;
 import com.intellij.openapi.util.Segment;
-import com.intellij.psi.*;
-import com.intellij.psi.util.PsiTreeUtil;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.SyntaxTraverser;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class JavaFocusModeProvider implements FocusModeProvider {
-  @Nullable
+  @NotNull
   @Override
-  public Segment calcFocusRange(int primaryCaretOffset, PsiFile file) {
-    NavigatablePsiElement parent = PsiTreeUtil
-      .getParentOfType(file.findElementAt(primaryCaretOffset), PsiMethod.class, PsiClass.class, PsiClassInitializer.class, PsiFile.class);
-    return parent == null ? null : parent.getTextRange();
+  public List<? extends Segment> calcFocusZones(@NotNull PsiFile file) {
+    return SyntaxTraverser.psiTraverser(file)
+      .postOrderDfsTraversal()
+      .filter(e -> e instanceof PsiClass || e instanceof PsiMethod)
+      .map(e -> e.getTextRange()).toList();
   }
 }
