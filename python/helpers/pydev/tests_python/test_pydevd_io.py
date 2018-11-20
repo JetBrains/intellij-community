b5961b16a12121d7b37a706980d89fb0c390167d
Elizaveta Shashkova <Elizaveta.Shashkova@jetbrains.com>
2018-11-20 20:48:52 +0300

    PY-31575 Make sure that str is passed to NetCommand in DebugConsoleStdIn.

--- python/helpers/pydev/tests_python/test_pydevd_io.py
+++ python/helpers/pydev/tests_python/test_pydevd_io.py
@@ -12,3 +12,38 @@ def test_io_redirector():
     # Check that we don't fail creating the IORedirector if the original
     # doesn't have a 'buffer'.
     IORedirector(MyRedirection1(), MyRedirection2(), wrap_buffer=True)
+
+
+class DummyWriter(object):
+
+    __slots__ = ['commands', 'command_meanings']
+
+    def __init__(self):
+        self.commands = []
+        self.command_meanings = []
+
+    def add_command(self, cmd):
+        from _pydevd_bundle.pydevd_comm import ID_TO_MEANING
+        meaning = ID_TO_MEANING[str(cmd.id)]
+        self.command_meanings.append(meaning)
+        self.commands.append(cmd)
+
+
+def test_debug_console():
+    from _pydev_bundle.pydev_console_utils import DebugConsoleStdIn
+    from pydevd import PyDB
+
+    class OriginalStdin(object):
+
+        def readline(self):
+            return 'read'
+
+    original_stdin = OriginalStdin()
+
+    py_db = PyDB(set_as_global=False)
+    py_db.writer = DummyWriter()
+    debug_console_std_in = DebugConsoleStdIn(py_db, original_stdin)
+    assert debug_console_std_in.readline() == 'read'
+
+    assert py_db.writer.command_meanings == ['CMD_INPUT_REQUESTED', 'CMD_INPUT_REQUESTED']
+
