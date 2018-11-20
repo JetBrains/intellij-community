e80556ee8d702579071ab3265838013a8b5aab9a
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-11-20 17:21:49 +0100

    add selectAsync to reduce usages of ActionCallback

--- platform/lang-impl/src/com/intellij/ide/favoritesTreeView/FavoritesViewTreeBuilder.java
+++ platform/lang-impl/src/com/intellij/ide/favoritesTreeView/FavoritesViewTreeBuilder.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.favoritesTreeView;
 
 import com.intellij.ProjectTopics;
@@ -39,6 +25,8 @@ import com.intellij.util.messages.MessageBusConnection;
 import com.intellij.util.ui.tree.TreeUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.concurrency.Promise;
+import org.jetbrains.concurrency.Promises;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
@@ -145,12 +133,12 @@ public class FavoritesViewTreeBuilder extends BaseProjectTreeBuilder {
 
   @NotNull
   @Override
-  public ActionCallback select(Object element, VirtualFile file, boolean requestFocus) {
+  public Promise<Object> selectAsync(Object element, VirtualFile file, boolean requestFocus) {
     final DefaultMutableTreeNode node = findSmartFirstLevelNodeByElement(element);
     if (node != null) {
-      return TreeUtil.selectInTree(node, requestFocus, getTree());
+      return Promises.toPromise(TreeUtil.selectInTree(node, requestFocus, getTree()));
     }
-    return super.select(element, file, requestFocus);
+    return super.selectAsync(element, file, requestFocus);
   }
 
   @Nullable
