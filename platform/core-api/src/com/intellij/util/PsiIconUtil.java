771f31dd1b0e021f69005374eb6bff033d2fdab0
peter <peter@jetbrains.com>
2018-11-20 07:32:43 +0100

    PsiIconUtil cleanup
    less code, no extension loading in <clinit>

--- platform/core-api/src/com/intellij/util/PsiIconUtil.java
+++ platform/core-api/src/com/intellij/util/PsiIconUtil.java
@@ -13,29 +13,16 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
-import java.util.List;
 
 public class PsiIconUtil {
   @Nullable
   public static Icon getProvidersIcon(@NotNull PsiElement element, @Iconable.IconFlags int flags) {
-    final boolean dumb = DumbService.getInstance(element.getProject()).isDumb();
-    for (final IconProvider iconProvider : getIconProviders()) {
-      if (dumb && !DumbService.isDumbAware(iconProvider)) {
-        continue;
-      }
-
-      final Icon icon = iconProvider.getIcon(element, flags);
+    DumbService dumbService = DumbService.getInstance(element.getProject());
+    for (IconProvider provider : dumbService.filterByDumbAwareness(IconProvider.EXTENSION_POINT_NAME.getExtensionList())) {
+      Icon icon = provider.getIcon(element, flags);
       if (icon != null) return icon;
     }
     return null;
   }
 
-  private static class IconProviderHolder {
-    private static final List<IconProvider> ourIconProviders = IconProvider.EXTENSION_POINT_NAME.getExtensionList();
-  }
-
-  @NotNull
-  private static List<IconProvider> getIconProviders() {
-    return IconProviderHolder.ourIconProviders;
-  }
 }
