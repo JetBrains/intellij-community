e6c76865600acad463ea6e41de4e307d1ed2c495
Svetlana.Zemlyanskaya <svetlana.zemlyanskaya@jetbrains.com>
2018-11-21 16:34:32 +0100

    IDEA-CR-40240: FUS: report file types defined in listed plugins
    (cherry picked from commit 8e95e3f9517cb962baa34e7fbc146d0c993a28dc)

--- platform/platform-impl/src/com/intellij/internal/statistic/collectors/fus/fileTypes/FileTypeEditUsageTriggerCollector.java
+++ platform/platform-impl/src/com/intellij/internal/statistic/collectors/fus/fileTypes/FileTypeEditUsageTriggerCollector.java
@@ -1,7 +1,11 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.internal.statistic.collectors.fus.fileTypes;
 
+import com.intellij.internal.statistic.service.fus.collectors.FUSProjectUsageTrigger;
 import com.intellij.internal.statistic.service.fus.collectors.ProjectUsageTriggerCollector;
+import com.intellij.internal.statistic.utils.StatisticsUtilKt;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
 public class FileTypeEditUsageTriggerCollector extends ProjectUsageTriggerCollector {
@@ -10,5 +14,11 @@ public class FileTypeEditUsageTriggerCollector extends ProjectUsageTriggerCollec
   @Override
   public String getGroupId() {
     return "statistics.file.types.edit";
+  }
+
+  public static void trigger(@NotNull Project project, @NotNull FileType fileType) {
+    if (StatisticsUtilKt.getPluginType(fileType.getClass()).isSafeToReport()) {
+      FUSProjectUsageTrigger.getInstance(project).trigger(FileTypeEditUsageTriggerCollector.class, fileType.getName());
+    }
   }
 }
