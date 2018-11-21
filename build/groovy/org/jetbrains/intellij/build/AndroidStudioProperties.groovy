0449f2808e4546b0a79a9c9208e6bcac77c5a39c
estebandlc <estebandlc@google.com>
2018-11-20 16:40:55 -0800

    Adds the new tracer module
    Test: N/A
    Bug: N/A
    Change-Id: I1fe152c3c315f5a11a9b896b0c161c621e325e4e

--- build/groovy/org/jetbrains/intellij/build/AndroidStudioProperties.groovy
+++ build/groovy/org/jetbrains/intellij/build/AndroidStudioProperties.groovy
@@ -185,6 +185,7 @@ class AndroidStudioProperties extends BaseIdeaProperties {
       withModule("android.sdktools.ddmlib", "sdk-tools.jar")
       withModule("android.sdktools.dvlib", "sdk-tools.jar")
       withModule("android.sdktools.deployer", "sdk-tools.jar")
+      withModule("android.sdktools.tracer", "sdk-tools.jar")
       withModule("android.sdktools.draw9patch", "sdk-tools.jar")
       withModule("android.sdktools.instant-run-client", "sdk-tools.jar")
       withModule("android.sdktools.instant-run-common", "sdk-tools.jar")
