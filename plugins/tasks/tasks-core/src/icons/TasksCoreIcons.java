0ff0e85451b05e6148e10133fb1690c8ba643bb3
develar <develar@gmail.com>
2018-11-22 16:10:40 +0100

    tasks — move icons to resource root

--- plugins/tasks/tasks-core/src/icons/TasksCoreIcons.java
+++ plugins/tasks/tasks-core/src/icons/TasksCoreIcons.java
@@ -1,4 +1,7 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+/*
+ * Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
 package icons;
 
 import com.intellij.openapi.util.IconLoader;
@@ -9,7 +12,7 @@ import javax.swing.*;
  * NOTE THIS FILE IS AUTO-GENERATED
  * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
  */
-public class TasksCoreIcons {
+public final class TasksCoreIcons {
   private static Icon load(String path) {
     return IconLoader.getIcon(path, TasksCoreIcons.class);
   }
@@ -26,17 +29,12 @@ public class TasksCoreIcons {
   public static final Icon Clock = load("/icons/clock.svg"); // 13x13
   public static final Icon Feature = load("/icons/feature.png"); // 16x16
   public static final Icon Fogbugz = load("/icons/fogbugz.png"); // 16x16
-
-  /** @deprecated to be removed in IDEA 2020 - use AllIcons.Vcs.Vendors.Github */
-  @SuppressWarnings("unused")
-  @Deprecated
-  public static final Icon Github = load("/vcs/vendors/github.svg", com.intellij.icons.AllIcons.class);
   public static final Icon Gitlab = load("/icons/gitlab.svg"); // 16x16
   public static final Icon Jira = load("/icons/jira.png"); // 16x16
   public static final Icon Lighthouse = load("/icons/lighthouse.png"); // 16x16
   public static final Icon Mantis = load("/icons/mantis.png"); // 16x16
 
-  public static class Pivotal {
+  public final static class Pivotal {
     public static final Icon Bug = load("/icons/pivotal/bug.png"); // 16x16
     public static final Icon Chore = load("/icons/pivotal/chore.png"); // 16x16
     public static final Icon Feature = load("/icons/pivotal/feature.png"); // 16x16
@@ -52,4 +50,9 @@ public class TasksCoreIcons {
   public static final Icon Trac = load("/icons/trac.png"); // 16x16
   public static final Icon Trello = load("/icons/trello.png"); // 16x16
   public static final Icon Youtrack = load("/icons/youtrack.png"); // 16x16
+
+  /** @deprecated to be removed in IDEA 2020 - use AllIcons.Vcs.Vendors.Github */
+  @SuppressWarnings("unused")
+  @Deprecated
+  public static final Icon Github = load("/vcs/vendors/github.svg", com.intellij.icons.AllIcons.class);
 }
