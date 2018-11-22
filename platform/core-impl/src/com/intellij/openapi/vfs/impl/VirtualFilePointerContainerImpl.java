7cc58da1329c02f9d1278fd48a0cdd9ac61bb3d3
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2018-11-22 18:32:12 +0300

    IDEA-202743 Library that contains only a single folder is shown as "libs and one more file" in Project Structure

--- platform/core-impl/src/com/intellij/openapi/vfs/impl/VirtualFilePointerContainerImpl.java
+++ platform/core-impl/src/com/intellij/openapi/vfs/impl/VirtualFilePointerContainerImpl.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vfs.impl;
 
 import com.intellij.ide.highlighter.ArchiveFileType;
@@ -251,8 +237,6 @@ public class VirtualFilePointerContainerImpl extends TraceableDisposable impleme
         VirtualFile jarDirectory = jarDirectoryPtr.getFile();
         if (jarDirectory != null) {
           // getFiles() must return files under jar directories but must not return jarDirectories themselves
-          // getUrls() must return class roots plus jar directories
-          cachedUrls.add(jarDirectory.getUrl());
           cachedDirectories.remove(jarDirectory);
 
           VirtualFile[] children = jarDirectory.getChildren();
@@ -271,8 +255,6 @@ public class VirtualFilePointerContainerImpl extends TraceableDisposable impleme
         VirtualFile jarDirectory = jarDirectoryPtr.getFile();
         if (jarDirectory != null) {
           // getFiles() must return files under jar directories but must not return jarDirectories themselves
-          // getUrls() must return class roots plus jar directories
-          cachedUrls.add(jarDirectory.getUrl());
           cachedDirectories.remove(jarDirectory);
 
           VfsUtilCore.visitChildrenRecursively(jarDirectory, new VirtualFileVisitor() {
