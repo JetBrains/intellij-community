15aecb0fa6ac28944e7bc17c94de51dafbef43e2
Vladislav Shishov <vladislav.shishov@jetbrains.com>
2018-11-23 13:09:36 +0300

    [gui-test] add class target to RecordVideo annotation

--- platform/testGuiFramework/src/com/intellij/testGuiFramework/util/RecordVideo.kt
+++ platform/testGuiFramework/src/com/intellij/testGuiFramework/util/RecordVideo.kt
@@ -3,6 +3,6 @@ package com.intellij.testGuiFramework.util
 
 // Note: SCREENRECORDER_JAR_DIR env variable must be exported to make video recording work
 
-@Target(AnnotationTarget.FUNCTION)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class RecordVideo
