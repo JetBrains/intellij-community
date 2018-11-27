277c0cde030613b1ea443ddd8e7e541a4a86b4c0
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2018-11-27 12:12:52 +0300

    IDEA-CR-40781 extract universal stub generation to separate step

--- python/build/groovy/org/jetbrains/intellij/build/pycharm/PyCharmPropertiesBase.groovy
+++ python/build/groovy/org/jetbrains/intellij/build/pycharm/PyCharmPropertiesBase.groovy
@@ -15,12 +15,14 @@
  */
 package org.jetbrains.intellij.build.pycharm
 
+import groovy.io.FileType
 import org.jetbrains.intellij.build.*
 
 /**
  * @author nik
  */
 abstract class PyCharmPropertiesBase extends ProductProperties {
+
   PyCharmPropertiesBase() {
     baseFileName = "pycharm"
     reassignAltClickToMultipleCarets = true
@@ -46,44 +48,102 @@ abstract class PyCharmPropertiesBase extends ProductProperties {
       }
     }
 
-    new PyPrebuiltIndicesGenerator().generateResources(context)
+    File folderWithUnzipContent = new File(context.paths.temp, "unzips")
+    folderWithUnzipContent.mkdirs()
+    unzipArchives(context, folderWithUnzipContent)
 
+    File temporaryIndexFolder = new File(context.paths.temp, "index")
+    temporaryIndexFolder.deleteDir()
+    temporaryIndexFolder.mkdirs()
+
+    boolean forceGenerate = false
+    if (PyCharmBuildOptions.usePrebuiltStubs) {
+      File stubsArchive = new File(context.paths.projectHome, PyCharmBuildOptions.prebuiltStubsArchive)
+      context.ant.echo("Unzip prebuilt stubs ${stubsArchive.absolutePath}")
+      context.ant.unzip(src: stubsArchive.absolutePath, dest: "${temporaryIndexFolder.absolutePath}")
+
+      try {
+        String stubsVersionPath = new FileNameFinder()
+          .getFileNames("${temporaryIndexFolder.absolutePath}/Python", "sdk-stubs.version").
+          first()
+        int stubsFromArchiveVersion = new File(stubsVersionPath).readLines()[0].toInteger()
+        context.ant.echo("Stubs version $stubsFromArchiveVersion")
+        int stubVersion = getStubVersion(context)
+        context.ant.echo("But should be $stubVersion")
+
+        forceGenerate = stubsFromArchiveVersion != stubVersion
+      }
+      catch (ignored) {
+        forceGenerate = true
+      }
+    }
+
+    if (forceGenerate || !PyCharmBuildOptions.usePrebuiltStubs) {
+      temporaryIndexFolder.deleteDir()
+      temporaryIndexFolder.mkdir()
+      generateUniversalStubs(context, folderWithUnzipContent, temporaryIndexFolder)
+    }
+
+    context.ant.echo("Generate indices")
+    generateIndices(context, folderWithUnzipContent, temporaryIndexFolder)
+    context.ant.echo("Copy indices")
     context.ant.copy(todir: "$targetDirectory/index", failonerror: !context.options.isInDevelopmentMode) {
-      fileset(dir: "$context.paths.temp/index", erroronmissingdir: !context.options.isInDevelopmentMode) {
+      fileset(dir: temporaryIndexFolder.absolutePath, erroronmissingdir: !context.options.isInDevelopmentMode) {
         include(name: "**")
       }
     }
+
+    folderWithUnzipContent.deleteDir()
+    temporaryIndexFolder.deleteDir()
   }
 
   @Override
   String getEnvironmentVariableBaseName(ApplicationInfoProperties applicationInfo) {
     "PYCHARM"
   }
-}
 
-class PyPrebuiltIndicesGenerator implements ResourcesGenerator {
-  @Override
-  File generateResources(BuildContext context) {
+  private void unzipArchives(BuildContext context, File destination) {
+    File tempFolder = new File(context.paths.temp, "zips")
+    tempFolder.mkdirs()
+    tempFolder.deleteOnExit()
+    context.ant.copy(todir: tempFolder.absolutePath, failonerror: !context.options.isInDevelopmentMode) {
+      fileset(dir: "$context.paths.projectHome/python-distributions", erroronmissingdir: !context.options.isInDevelopmentMode) {
+        include(name: "*.zip")
+      }
+      fileset(dir: "$context.paths.projectHome/skeletons", erroronmissingdir: !context.options.isInDevelopmentMode) {
+        include(name: "*.zip")
+      }
+    }
+
+    tempFolder.eachFileRecurse(FileType.FILES) {
+      if (it.name.endsWith('.zip')) {
+        context.ant.unzip(src: it.absolutePath, dest: "${destination.absolutePath}/${it.name}")
+      }
+    }
+  }
+
+  private int getStubVersion(BuildContext context) {
+    List<String> buildClasspath = context.getModuleRuntimeClasspath(context.findModule("intellij.python.tools"), false)
+
+    context.ant.java(classname: "com.jetbrains.python.tools.GetPyStubsVersionKt", fork: true, outputproperty: "stubsVersion") {
+      classpath {
+        buildClasspath.each {
+          pathelement(location: it)
+        }
+      }
+    }
+    return context.ant.project.properties.stubsVersion as int
+  }
+
+  private void generateUniversalStubs(BuildContext context, File from, File to) {
     CompilationTasks.create(context).compileModules(["intellij.python.tools"])
     List<String> buildClasspath = context.getModuleRuntimeClasspath(context.findModule("intellij.python.tools"), false)
 
-    String zipPath = "$context.paths.temp/zips"
-    String outputPath = "$context.paths.temp/index"
-
     context.messages.block("Generate universal stubs") {
-      context.ant.copy(todir: zipPath, failonerror: !context.options.isInDevelopmentMode) {
-        fileset(dir: "$context.paths.projectHome/python-distributions", erroronmissingdir: !context.options.isInDevelopmentMode) {
-          include(name: "*.zip")
-        }
-        fileset(dir: "$context.paths.projectHome/skeletons", erroronmissingdir: !context.options.isInDevelopmentMode) {
-          include(name: "*.zip")
-        }
-      }
-
-      context.ant.java(classname: "com.jetbrains.python.tools.PyPrebuiltIndicesGeneratorKt", fork: true) {
+      context.ant.java(classname: "com.jetbrains.python.tools.PythonUniversalStubsBuilderKt", fork: true) {
         jvmarg(line: "-ea -Xmx1000m")
-        arg(value: zipPath)
-        arg(value: outputPath)
+        arg(value: from.absolutePath)
+        arg(value: to.absolutePath)
         classpath {
           buildClasspath.each {
             pathelement(location: it)
@@ -91,7 +151,21 @@ class PyPrebuiltIndicesGenerator implements ResourcesGenerator {
         }
       }
     }
+  }
 
-    return new File(outputPath)
+  private void generateIndices(BuildContext context, File from, File to) {
+    CompilationTasks.create(context).compileModules(["intellij.python.tools"])
+    List<String> buildClasspath = context.getModuleRuntimeClasspath(context.findModule("intellij.python.tools"), false)
+
+    context.ant.java(classname: "com.jetbrains.python.tools.IndicesBuilderKt", fork: true) {
+      jvmarg(line: "-ea -Xmx1000m")
+      arg(value: from.absolutePath)
+      arg(value: to.absolutePath)
+      classpath {
+        buildClasspath.each {
+          pathelement(location: it)
+        }
+      }
+    }
   }
 }
