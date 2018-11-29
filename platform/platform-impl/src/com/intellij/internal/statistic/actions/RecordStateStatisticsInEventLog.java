c3aefb6df4d0d4f163d6386d26c367c77c756302
Svetlana.Zemlyanskaya <svetlana.zemlyanskaya@jetbrains.com>
2018-11-29 16:24:10 +0100

    FUS: add internal action for testing state collectors

---
+++ platform/platform-impl/src/com/intellij/internal/statistic/actions/RecordStateStatisticsInEventLog.java
@@ -0,0 +1,44 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.internal.statistic.actions;
+
+import com.intellij.internal.statistic.service.fus.collectors.FUStatisticRecorder;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.MessageType;
+import com.intellij.openapi.ui.popup.Balloon;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
+import org.jetbrains.annotations.NotNull;
+
+public class RecordStateStatisticsInEventLog extends AnAction {
+
+  @Override
+  public void actionPerformed(@NotNull AnActionEvent e) {
+    final Project project = e.getProject();
+    if (project == null) {
+      return;
+    }
+
+    ProgressManager.getInstance().run(new Task.Backgroundable(project, "Collecting Feature Usages In Event Log", false) {
+      @Override
+      public void run(@NotNull ProgressIndicator indicator) {
+        FUStatisticRecorder.collect();
+
+        ApplicationManager.getApplication().invokeLater(
+          () -> showNotification(project, e, "Collecting and recording events was finished")
+        );
+      }
+    });
+  }
+
+  protected void showNotification(@NotNull Project project, @NotNull AnActionEvent event, @NotNull String message) {
+    JBPopupFactory.getInstance().
+      createHtmlTextBalloonBuilder(message, MessageType.INFO, null).
+      setFadeoutTime(2000).setDisposable(project).createBalloon().
+      show(JBPopupFactory.getInstance().guessBestPopupLocation(event.getDataContext()), Balloon.Position.below);
+  }
+}
