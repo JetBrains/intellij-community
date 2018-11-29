9612e7430a63c46c887f0ed2a1294d395b48a782
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-11-29 18:03:44 +0100

    value attribute check is enough

--- platform/projectModel-impl/src/com/intellij/configurationStore/JbXmlOutputter.kt
+++ platform/projectModel-impl/src/com/intellij/configurationStore/JbXmlOutputter.kt
@@ -508,7 +508,7 @@ open class JbXmlOutputter @JvmOverloads constructor(lineSeparator: String = "\n"
     var name: String? = element.name
 
     @Suppress("SpellCheckingInspection")
-    if (BaseXmlOutputter.isNameIndicatesSensitiveInformation(name!!) && name != "omniauth-multipassword") {
+    if (BaseXmlOutputter.isNameIndicatesSensitiveInformation(name!!)) {
       logSensitiveInformationError(name, "Element")
     }
 
