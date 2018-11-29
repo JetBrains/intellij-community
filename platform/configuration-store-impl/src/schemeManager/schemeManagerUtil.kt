5dd58a224435d8a957adc5e47a3d50150fe13c1e
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-11-29 14:16:13 +0100

    schemeManager: do not log ProcessCanceledException

--- platform/configuration-store-impl/src/schemeManager/schemeManagerUtil.kt
+++ platform/configuration-store-impl/src/schemeManager/schemeManagerUtil.kt
@@ -1,7 +1,9 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.configurationStore.schemeManager
 
 import com.intellij.configurationStore.LOG
 import com.intellij.openapi.options.ExternalizableScheme
+import com.intellij.openapi.progress.ProcessCanceledException
 
 internal fun ExternalizableScheme.renameScheme(newName: String) {
   if (newName != name) {
@@ -13,6 +15,9 @@ internal fun ExternalizableScheme.renameScheme(newName: String) {
 internal inline fun catchAndLog(fileName: String, runnable: (fileName: String) -> Unit) {
   try {
     runnable(fileName)
+  }
+  catch (e: ProcessCanceledException) {
+    throw e
   }
   catch (e: Throwable) {
     LOG.error("Cannot read scheme $fileName", e)
