bb263848eafe5b7b1597b22098f7d225ab5e0e49
Dmitriy.Panov <Dmitriy.Panov@jetbrains.com>
2018-11-29 20:17:13 +0300

    ensure GradleRunner is launched on JDK8

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/CompilationContextImpl.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/CompilationContextImpl.groovy
@@ -16,6 +16,7 @@ import org.jetbrains.jps.model.JpsGlobal
 import org.jetbrains.jps.model.JpsModel
 import org.jetbrains.jps.model.JpsProject
 import org.jetbrains.jps.model.artifact.JpsArtifactService
+import org.jetbrains.jps.model.java.JdkVersionDetector
 import org.jetbrains.jps.model.java.JpsJavaClasspathKind
 import org.jetbrains.jps.model.java.JpsJavaDependenciesEnumerator
 import org.jetbrains.jps.model.java.JpsJavaExtensionService
@@ -76,6 +77,10 @@ class CompilationContextImpl implements CompilationContext {
     projectHome = toCanonicalPath(projectHome)
     def jdk8Home = toCanonicalPath(JdkUtils.computeJdkHome(messages, "jdk8Home", "$projectHome/build/jdk/1.8", "JDK_18_x64"))
     def kotlinHome = toCanonicalPath("$communityHome/build/dependencies/build/kotlin/Kotlin")
+
+    if (!JdkVersionDetector.instance.detectJdkVersionInfo(gradleJdk).version.contains("1.8.")) {
+      gradle = new GradleRunner(dependenciesProjectDir, messages, jdk8Home)
+    }
 
     def model = loadProject(projectHome, jdk8Home, kotlinHome, messages, ant)
     def oldToNewModuleName = loadModuleRenamingHistory(projectHome, messages) + loadModuleRenamingHistory(communityHome, messages)
