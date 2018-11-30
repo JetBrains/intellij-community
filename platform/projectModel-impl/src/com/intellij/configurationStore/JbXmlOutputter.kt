89b700f22490700a7c23cdf460edd12dea531b5a
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2018-11-30 13:06:19 +0100

    Cleanup (weird import)

--- platform/projectModel-impl/src/com/intellij/configurationStore/JbXmlOutputter.kt
+++ platform/projectModel-impl/src/com/intellij/configurationStore/JbXmlOutputter.kt
@@ -20,7 +20,7 @@ import org.jdom.output.Format
 import java.io.IOException
 import java.io.StringWriter
 import java.io.Writer
-import java.util.*
+import java.util.Collections
 import javax.xml.transform.Result
 import kotlin.collections.HashSet
 
