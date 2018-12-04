dd6709ddd6bf07edbc1e7d6b748946a1c6b6ee14
Vladislav.Soroka <vladislav.soroka@jetbrains.com>
2018-12-04 07:14:53 +0100

    IDEA-CR-40845: IDEA-203517 Gradle test runner unable to find test task with type extended from `org.gradle.api.tasks.testing.Test`
    (cherry picked from commit 9d032f512b0d715a754258611e4cb2575041be3e)

--- plugins/gradle/tooling-extension-api/src/org/jetbrains/plugins/gradle/model/ExternalTask.java
+++ plugins/gradle/tooling-extension-api/src/org/jetbrains/plugins/gradle/model/ExternalTask.java
@@ -19,7 +19,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.Serializable;
-import java.util.Collection;
 
 /**
  * @author Vladislav.Soroka
@@ -36,4 +35,6 @@ public interface ExternalTask extends Serializable {
   String getGroup();
   @Nullable
   String getType();
+
+  boolean isTest();
 }
