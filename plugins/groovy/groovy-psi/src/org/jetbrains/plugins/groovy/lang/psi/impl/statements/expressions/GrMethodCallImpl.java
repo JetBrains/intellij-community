aaa0e04bf130a53916d8d7743963f66d3b1bddb8
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2018-12-04 18:08:10 +0300

    [groovy] diagnostic for EA-132329

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/statements/expressions/GrMethodCallImpl.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/statements/expressions/GrMethodCallImpl.java
@@ -21,6 +21,8 @@ import org.jetbrains.plugins.groovy.lang.psi.impl.statements.expressions.path.Gr
 import org.jetbrains.plugins.groovy.lang.resolve.api.GroovyMethodCallReference;
 import org.jetbrains.plugins.groovy.lang.resolve.impl.GrImplicitCallReference;
 
+import static org.jetbrains.plugins.groovy.lang.psi.impl.PsiImplUtil.LOG;
+
 /**
  * @author Maxim.Medvedev
  */
@@ -72,7 +74,14 @@ public abstract class GrMethodCallImpl extends GrCallExpressionImpl implements G
     if (implicitCallReference != null) {
       return implicitCallReference.multiResolve(incompleteCode);
     }
-    return ((GrReferenceExpression)getInvokedExpression()).multiResolve(incompleteCode);
+    final GrExpression invokedExpression = getInvokedExpression();
+    if (invokedExpression instanceof GrReferenceExpression) {
+      return ((GrReferenceExpression)invokedExpression).multiResolve(incompleteCode);
+    }
+    else {
+      LOG.error("Invoked expression is not a reference expression and there is no implicit call reference: '" + getText() + "'");
+      return GroovyResolveResult.EMPTY_ARRAY;
+    }
   }
 
   @Override
