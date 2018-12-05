02712df5304a25b164fc860c0fba4f008c3bf2da
Egor Ushakov <egor.ushakov@jetbrains.com>
2018-12-05 14:33:08 +0300

    honor registry key disable.winp when doing soft kill

--- platform/platform-impl/src/com/intellij/execution/process/KillableProcessHandler.java
+++ platform/platform-impl/src/com/intellij/execution/process/KillableProcessHandler.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.process;
 
 import com.intellij.execution.ExecutionException;
@@ -25,7 +11,6 @@ import com.intellij.openapi.util.registry.Registry;
 import com.intellij.remote.RemoteProcess;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
-import org.jvnet.winp.WinProcess;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -164,7 +149,7 @@ public class KillableProcessHandler extends OSProcessHandler implements Killable
       if (myMediatedProcess) {
         return RunnerMediator.destroyProcess(myProcess, true);
       }
-      if (myShouldKillProcessSoftlyWithWinP) {
+      if (myShouldKillProcessSoftlyWithWinP && !Registry.is("disable.winp")) {
         try {
           if (!myProcess.isAlive()) {
             OSProcessUtil.logSkippedActionWithTerminatedProcess(myProcess, "destroy", getCommandLine());
