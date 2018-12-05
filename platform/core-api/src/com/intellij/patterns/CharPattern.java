35b9521f1b50f021d4f654d03c3cb8654de094f3
Dmitry Jemerov <yole@jetbrains.com>
2018-12-05 10:49:35 +0100

    Keep commonly used CharPatterns as static fields (IDEA-CR-40833)

--- platform/core-api/src/com/intellij/patterns/CharPattern.java
+++ platform/core-api/src/com/intellij/patterns/CharPattern.java
@@ -8,49 +8,65 @@ import org.jetbrains.annotations.NotNull;
  * @author peter
  */
 public class CharPattern extends ObjectPattern<Character, CharPattern> {
-
-  private final CharPattern myJavaIdentifierPartPattern = with(new PatternCondition<Character>("javaIdentifierPart") {
-    @Override
-    public boolean accepts(@NotNull final Character character, final ProcessingContext context) {
-      return Character.isJavaIdentifierPart(character.charValue());
-    }
-  });
-  private final CharPattern myJavaIdentifierStartPattern = with(new PatternCondition<Character>("javaIdentifierStart") {
-    @Override
-    public boolean accepts(@NotNull final Character character, final ProcessingContext context) {
-      return Character.isJavaIdentifierStart(character.charValue());
-    }
-  });
-  private final CharPattern myWhitespacePattern = with(new PatternCondition<Character>("whitespace") {
-    @Override
-    public boolean accepts(@NotNull final Character character, final ProcessingContext context) {
-      return Character.isWhitespace(character.charValue());
-    }
-  });
-  private final CharPattern myLetterOrDigitPattern = with(new PatternCondition<Character>("letterOrDigit") {
-    @Override
-    public boolean accepts(@NotNull final Character character, final ProcessingContext context) {
-      return Character.isLetterOrDigit(character.charValue());
-    }
-  });
+  private static final CharPattern ourJavaIdentifierStartCharacter = StandardPatterns.character().javaIdentifierStart();
+  private static final CharPattern ourJavaIdentifierPartCharacter = StandardPatterns.character().javaIdentifierPart();
+  private static final CharPattern ourWhitespaceCharacter = StandardPatterns.character().whitespace();
+  private static final CharPattern ourLetterOrDigitCharacter = StandardPatterns.character().letterOrDigit();
 
   protected CharPattern() {
     super(Character.class);
+
   }
 
   public CharPattern javaIdentifierPart() {
-    return myJavaIdentifierPartPattern;
+    return with(new PatternCondition<Character>("javaIdentifierPart") {
+      @Override
+      public boolean accepts(@NotNull final Character character, final ProcessingContext context) {
+        return Character.isJavaIdentifierPart(character.charValue());
+      }
+    });
   }
 
   public CharPattern javaIdentifierStart() {
-    return myJavaIdentifierStartPattern;
+    return with(new PatternCondition<Character>("javaIdentifierStart") {
+      @Override
+      public boolean accepts(@NotNull final Character character, final ProcessingContext context) {
+        return Character.isJavaIdentifierStart(character.charValue());
+      }
+    });
   }
 
   public CharPattern whitespace() {
-    return myWhitespacePattern;
+    return with(new PatternCondition<Character>("whitespace") {
+      @Override
+      public boolean accepts(@NotNull final Character character, final ProcessingContext context) {
+        return Character.isWhitespace(character.charValue());
+      }
+    });
   }
 
   public CharPattern letterOrDigit() {
-    return myLetterOrDigitPattern;
+    return with(new PatternCondition<Character>("letterOrDigit") {
+      @Override
+      public boolean accepts(@NotNull final Character character, final ProcessingContext context) {
+        return Character.isLetterOrDigit(character.charValue());
+      }
+    });
+  }
+
+  public static CharPattern javaIdentifierStartCharacter() {
+    return ourJavaIdentifierStartCharacter;
+  }
+
+  public static CharPattern javaIdentifierPartCharacter() {
+    return ourJavaIdentifierPartCharacter;
+  }
+
+  public static CharPattern letterOrDigitCharacter() {
+    return ourLetterOrDigitCharacter;
+  }
+
+  public static CharPattern whitespaceCharacter() {
+    return ourWhitespaceCharacter;
   }
 }
