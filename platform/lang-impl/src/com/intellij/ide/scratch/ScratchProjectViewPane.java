e8d60bbbb7ad409b8f10cc74c5efde44d2770130
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2018-12-06 14:52:47 +0300

    IDEA-203616: document RootsProvider

--- platform/lang-impl/src/com/intellij/ide/scratch/ScratchProjectViewPane.java
+++ platform/lang-impl/src/com/intellij/ide/scratch/ScratchProjectViewPane.java
@@ -285,8 +285,7 @@ public class ScratchProjectViewPane extends ProjectViewPane {
     @NotNull
     @Override
     public Collection<VirtualFile> getRoots() {
-      VirtualFile root = getVirtualFile();
-      return root == null ? EMPTY_ROOTS : Collections.singleton(root);
+      return getDefaultRootsFor(getVirtualFile());
     }
 
     @NotNull
