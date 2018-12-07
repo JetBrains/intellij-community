121973f6b678e212a671d4e867940dc6ed719474
Ivan Buryak <Ivan.Buryak@jetbrains.com>
2018-12-07 12:32:14 +0700

    IDEA-203775 Fix NPE in `CodeStyleSettings.isFileFullyCoveredByRange`
    This fixes NPE introduced in 0d3b69df6efacd86b47a38364b8812f69b24f693. Ex., `PsiBinaryFileImpl#getTextRange` returns `null`.

--- platform/lang-api/src/com/intellij/psi/codeStyle/CodeStyleSettings.java
+++ platform/lang-api/src/com/intellij/psi/codeStyle/CodeStyleSettings.java
@@ -1028,7 +1028,7 @@ public class CodeStyleSettings extends LegacyCodeStyleSettings implements Clonea
   }
 
   private static boolean isFileFullyCoveredByRange(@NotNull PsiFile file, @Nullable TextRange formatRange) {
-    return file.getTextRange().equals(formatRange);
+    return formatRange != null && formatRange.equals(file.getTextRange());
   }
 
   private static void logIndentOptions(@NotNull PsiFile file,
