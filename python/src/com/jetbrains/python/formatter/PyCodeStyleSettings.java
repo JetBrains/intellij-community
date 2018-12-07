84443a8468565db7fc719e077093059740456982
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2018-12-07 17:17:40 +0700

    Remove a stale comment

--- python/src/com/jetbrains/python/formatter/PyCodeStyleSettings.java
+++ python/src/com/jetbrains/python/formatter/PyCodeStyleSettings.java
@@ -64,7 +64,6 @@ public class PyCodeStyleSettings extends CustomCodeStyleSettings {
 
   public int BLANK_LINES_AROUND_TOP_LEVEL_CLASSES_FUNCTIONS = 2;
   public int BLANK_LINES_BEFORE_FIRST_METHOD = 0;
-  // TODO make boolean (it needs special editor in BlankLinesPanel)
   public boolean BLANK_LINE_AT_FILE_END = true;
 
   public boolean ALIGN_COLLECTIONS_AND_COMPREHENSIONS = true;
