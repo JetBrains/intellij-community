69f0f2eefc17ec14d1205f737bd0068e573f48c2
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-12-08 19:49:48 +0100

    fix LightJavadocGeneratorTest

--- java/java-tests/testSrc/com/intellij/java/codeInsight/javadoc/LightJavadocGeneratorTest.kt
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/javadoc/LightJavadocGeneratorTest.kt
@@ -3,22 +3,26 @@ package com.intellij.java.codeInsight.javadoc
 
 import com.intellij.codeInsight.javadoc.JavaDocInfoGeneratorFactory
 import com.intellij.psi.PsiJavaFile
-import com.intellij.testFramework.LightProjectDescriptor
 import com.intellij.testFramework.assertions.Assertions.assertThat
 import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase
 
 class LightJavadocGeneratorTest : LightCodeInsightFixtureTestCase() {
-  override fun getProjectDescriptor(): LightProjectDescriptor = JAVA_9
+  override fun getProjectDescriptor() = JAVA_9
 
-  fun testPlainModule() = doTestModule("module M.N { }", """<div class='definition'><pre>module <b>M.N</b></pre></div>""".trimIndent())
+  fun testPlainModule() {
+    doTestModule("module M.N { }", """<div class='definition'><pre>module <b>M.N</b></pre></div>""".trimIndent())
+  }
 
-  fun testDocumentedModule() = doTestModule("/** One humble module. */\n@Deprecated\nmodule M.N { }", """<div class='definition'><pre>@<a href="psi_element://java.lang.Deprecated"><code>Deprecated</code></a>
-module <b>M.N</b></pre></div><div class='content'> One humble module. </div><table class='sections'><p></table>""".trimIndent())
+  fun testDocumentedModule() {
+    doTestModule("/** One humble module. */\n@Deprecated\nmodule M.N { }",
+      // trailing space after first line is expected, so, to ensure that it will be not trimmed, 2 raw literals are used instead of one.
+     """<div class='definition'><pre>@<a href="psi_element://java.lang.Deprecated"><code>Deprecated</code></a> """ + "\n" +
+     """module <b>M.N</b></pre></div><div class='content'> One humble module. </div><table class='sections'><p></table>""")
+  }
 
   private fun doTestModule(text: String, expected: String) {
-    val file = myFixture.configureByText("module-info.java", text)
-    val module = (file as PsiJavaFile).moduleDeclaration!!
-    val docInfo = JavaDocInfoGeneratorFactory.create(project, module).generateDocInfo(null)!!.replace("&nbsp;", " ")
+    val file = myFixture.configureByText("module-info.java", text) as PsiJavaFile
+    val docInfo = JavaDocInfoGeneratorFactory.create(project, file.moduleDeclaration!!).generateDocInfo(null)!!.replace("&nbsp;", " ")
     assertThat(docInfo).isEqualTo(expected)
   }
 }
