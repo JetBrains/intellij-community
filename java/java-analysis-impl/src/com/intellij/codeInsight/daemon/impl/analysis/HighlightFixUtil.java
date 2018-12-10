3f16a0ec142a7fce7fca53be9ce9a8e8f0b97b43
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2018-12-10 13:56:31 +0300

    use inter-language modifier fix inside java analysis IDEA-202508

--- java/java-analysis-impl/src/com/intellij/codeInsight/daemon/impl/analysis/HighlightFixUtil.java
+++ java/java-analysis-impl/src/com/intellij/codeInsight/daemon/impl/analysis/HighlightFixUtil.java
@@ -13,6 +13,7 @@ import com.intellij.lang.jvm.JvmModifier;
 import com.intellij.lang.jvm.JvmModifiersOwner;
 import com.intellij.lang.jvm.actions.JvmElementActionFactories;
 import com.intellij.lang.jvm.actions.MemberRequestsKt;
+import com.intellij.lang.jvm.util.JvmUtil;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
@@ -102,29 +103,31 @@ public class HighlightFixUtil {
       PsiModifierList modifierListCopy = facade.getElementFactory().createFieldFromText("int a;", null).getModifierList();
       assert modifierListCopy != null;
       modifierListCopy.setModifierProperty(PsiModifier.STATIC, modifierList.hasModifierProperty(PsiModifier.STATIC));
-      String minModifier = PsiModifier.PACKAGE_LOCAL;
+      int minAccessLevel = PsiUtil.ACCESS_LEVEL_PACKAGE_LOCAL;
       if (refElement.hasModifierProperty(PsiModifier.PACKAGE_LOCAL)) {
-        minModifier = PsiModifier.PROTECTED;
+        minAccessLevel = PsiUtil.ACCESS_LEVEL_PROTECTED;
       }
       if (refElement.hasModifierProperty(PsiModifier.PROTECTED)) {
-        minModifier = PsiModifier.PUBLIC;
+        minAccessLevel = PsiUtil.ACCESS_LEVEL_PUBLIC;
       }
       PsiClass containingClass = refElement.getContainingClass();
       if (containingClass != null && containingClass.isInterface()) {
-        minModifier = PsiModifier.PUBLIC;
+        minAccessLevel = PsiUtil.ACCESS_LEVEL_PUBLIC;
       }
-      String[] modifiers = {PsiModifier.PACKAGE_LOCAL, PsiModifier.PROTECTED, PsiModifier.PUBLIC,};
-      for (int i = ArrayUtil.indexOf(modifiers, minModifier); i < modifiers.length; i++) {
-        @PsiModifier.ModifierConstant String modifier = modifiers[i];
-        modifierListCopy.setModifierProperty(modifier, true);
+      int[] accessLevels = {PsiUtil.ACCESS_LEVEL_PACKAGE_LOCAL, PsiUtil.ACCESS_LEVEL_PROTECTED, PsiUtil.ACCESS_LEVEL_PUBLIC,};
+      for (int i = ArrayUtil.indexOf(accessLevels, minAccessLevel); i < accessLevels.length; i++) {
+        @PsiUtil.AccessLevel
+        int level = accessLevels[i];
+        modifierListCopy.setModifierProperty(PsiUtil.getAccessModifier(level), true);
         if (facade.getResolveHelper().isAccessible(refElement, modifierListCopy, place, accessObjectClass, fileResolveScope)) {
-          IntentionAction fix = QUICK_FIX_FACTORY.createModifierListFix(refElement, modifier, true, true);
+          List<IntentionAction> fixes = JvmElementActionFactories
+            .createModifierActions(refElement, MemberRequestsKt.modifierRequest(JvmUtil.getAccessModifier(level), true));
           TextRange fixRange = new TextRange(errorResult.startOffset, errorResult.endOffset);
           PsiElement ref = place.getReferenceNameElement();
           if (ref != null) {
             fixRange = fixRange.union(ref.getTextRange());
           }
-          QuickFixAction.registerQuickFixAction(errorResult, fixRange, fix);
+          QuickFixAction.registerQuickFixActions(errorResult, fixRange, fixes);
         }
       }
     }
