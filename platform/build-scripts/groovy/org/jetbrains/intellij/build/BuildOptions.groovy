515813956d6617e9d843d049c1e2a5a53fcccadd
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2018-12-10 10:06:45 +0100

    move Toolbox Lite-Gen related parameters away from BuildOptions.gradle. Inline more variables to shorten the code.

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/BuildOptions.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/BuildOptions.groovy
@@ -125,19 +125,4 @@ class BuildOptions {
    * Specifies JRE build to be bundled with distributions. If {@code null} then jdkBuild from gradle.properties will be used.
    */
   String bundledJreBuild = System.getProperty("intellij.build.bundled.jre.build")
-
-
-  /**
-   * if {@code true} the build will use JetBrains Toolbox LiteGen to generate a toolbox one-click
-   * install link for the product(s) from the current build.
-   * @see #toolboxLiteGenVersion
-   */
-  boolean buildToolboxLiteGenLink = Boolean.getBoolean("intellij.build.toolbox.litegen")
-
-  /**
-   * specifies the version of JetBrains Toolbox LiteGen used to generate a toolbox one-click
-   * install link for the product(s) from the current build.
-   * @see #buildToolboxLiteGenLink
-   */
-  String toolboxLiteGenVersion = System.getProperty("intellij.build.toolbox.litegen.version")
 }
