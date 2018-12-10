c12b71623ee52e806c8187f32f66196617282aab
nik <Nikolay.Chashnikov@jetbrains.com>
2018-12-10 14:45:39 +0300

    platform: allow specifying custom suffix to version name (IDEA-203795)
    Before there were no sane way to specify a version suffix other than "EAP" (e.g. "Preview", "Beta", or "RC"). (It was possible to append it to 'minor' or 'micro' attribute, but it's a hack which may cause problems.)

--- platform/core-impl/src/com/intellij/openapi/application/impl/ApplicationInfoImpl.java
+++ platform/core-impl/src/com/intellij/openapi/application/impl/ApplicationInfoImpl.java
@@ -42,6 +42,7 @@ public class ApplicationInfoImpl extends ApplicationInfoEx {
   private String myFullVersionFormat;
   private String myBuildNumber;
   private String myApiVersion;
+  private String myVersionSuffix;
   private String myCompanyName = "JetBrains s.r.o.";
   private String myCopyrightStart = "2000";
   private String myShortCompanyName;
@@ -258,7 +259,9 @@ public class ApplicationInfoImpl extends ApplicationInfoEx {
     else {
       result = StringUtil.notNullize(myMajorVersion, "0") + '.' + StringUtil.notNullize(myMinorVersion, "0");
     }
-    if (isEAP()) result += " EAP";
+    if (!StringUtil.isEmpty(myVersionSuffix)) {
+      result += " " + myVersionSuffix;
+    }
     return result;
   }
 
@@ -611,6 +614,10 @@ public class ApplicationInfoImpl extends ApplicationInfoEx {
       myFullVersionFormat = versionElement.getAttributeValue(ATTRIBUTE_FULL);
       myCodeName = versionElement.getAttributeValue(ATTRIBUTE_CODENAME);
       myEAP = Boolean.parseBoolean(versionElement.getAttributeValue(ATTRIBUTE_EAP));
+      myVersionSuffix = versionElement.getAttributeValue("suffix");
+      if (myVersionSuffix == null && myEAP) {
+        myVersionSuffix = "EAP";
+      }
     }
 
     Element companyElement = getChild(parentNode, ELEMENT_COMPANY);
