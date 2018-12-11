6184d7569a97ac5a7d318fdd18e75a1e50126c08
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-12-11 14:55:33 +0100

    don't duplicate state annotations — platform is able to grab from parent

--- platform/lang-impl/src/com/intellij/ide/RecentDirectoryProjectsManagerEx.java
+++ platform/lang-impl/src/com/intellij/ide/RecentDirectoryProjectsManagerEx.java
@@ -1,9 +1,6 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide;
 
-import com.intellij.openapi.components.RoamingType;
-import com.intellij.openapi.components.State;
-import com.intellij.openapi.components.Storage;
 import com.intellij.openapi.project.Project;
 import com.intellij.platform.ModuleAttachProcessor;
 import com.intellij.util.messages.MessageBus;
@@ -12,7 +9,6 @@ import org.jetbrains.annotations.NotNull;
 /**
  * @author yole
  */
-@State(name = "RecentDirectoryProjectsManager", storages = @Storage(value = "recentProjectDirectories.xml", roamingType = RoamingType.DISABLED))
 public class RecentDirectoryProjectsManagerEx extends RecentDirectoryProjectsManager {
   public RecentDirectoryProjectsManagerEx(MessageBus messageBus) {
     super(messageBus);
