89305e6918552738871b10d58e045a77c6996828
Dmitry Jemerov <yole@jetbrains.com>
2018-12-13 19:42:24 +0100

    Decouple implementation view from PSI

--- java/java-impl/src/com/intellij/codeInsight/hint/actions/ShowSiblingsAction.java
+++ java/java-impl/src/com/intellij/codeInsight/hint/actions/ShowSiblingsAction.java
@@ -1,22 +1,9 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.hint.actions;
 
 import com.intellij.codeInsight.TargetElementUtil;
 import com.intellij.codeInsight.daemon.impl.PsiElementListNavigator;
+import com.intellij.codeInsight.hint.PsiImplementationViewElement;
 import com.intellij.codeInsight.navigation.BackgroundUpdaterTask;
 import com.intellij.ide.util.MethodCellRenderer;
 import com.intellij.ide.util.PsiClassListCellRenderer;
@@ -92,7 +79,7 @@ public class ShowSiblingsAction extends ShowImplementationsAction {
                             @NotNull PsiElement element) {
     final PsiElement[] impls = getSelfAndImplementations(editor, element, createImplementationsSearcher(), false);
     final String text = SymbolPresentationUtil.getSymbolPresentableText(element);
-    showImplementations(impls, project, text, editor, file, element, invokedFromEditor, invokedByShortcut);
+    showImplementations(ContainerUtil.map(impls, PsiImplementationViewElement::new), project, text, editor, file, element, invokedFromEditor, invokedByShortcut);
   }
 
   @Override
--- platform/lang-impl/src/com/intellij/internal/ImageDuplicateResultsDialog.java
+++ platform/lang-impl/src/com/intellij/internal/ImageDuplicateResultsDialog.java
@@ -1,21 +1,8 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.internal;
 
 import com.intellij.codeInsight.hint.ImplementationViewComponent;
+import com.intellij.codeInsight.hint.PsiImplementationViewElement;
 import com.intellij.ide.DataManager;
 import com.intellij.ide.ui.search.SearchUtil;
 import com.intellij.ide.util.PropertiesComponent;
@@ -31,7 +18,6 @@ import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
 import com.intellij.ui.*;
@@ -197,7 +183,7 @@ public class ImageDuplicateResultsDialog extends DialogWrapper {
         if (file != null) {
           final PsiFile psiFile = PsiManager.getInstance(myProject).findFile(file);
           if (psiFile != null) {
-            final ImplementationViewComponent viewComponent = new ImplementationViewComponent(new PsiElement[]{psiFile}, 0);
+            final ImplementationViewComponent viewComponent = new ImplementationViewComponent(Collections.singletonList(new PsiImplementationViewElement(psiFile)), 0);
             final TreeSelectionListener listener = new TreeSelectionListener() {
               @Override
               public void valueChanged(TreeSelectionEvent e) {
@@ -205,7 +191,7 @@ public class ImageDuplicateResultsDialog extends DialogWrapper {
                 if (selection != null) {
                   final PsiFile newElement = PsiManager.getInstance(myProject).findFile(selection);
                   if (newElement != null) {
-                    viewComponent.update(new PsiElement[]{newElement}, 0);
+                    viewComponent.update(Collections.singletonList(new PsiImplementationViewElement(newElement)), 0);
                   }
                 }
               }
