790d3c8e1a88ae24bee863507b3bfb2d12f68f74
Dmitry Zhuravlev <dmitry.zhuravlev@jetbrains.com>
2018-12-14 20:18:33 +0300

    vcs-ignore: fix gitignore checker test
    make vfs refresh to gitignore file created via file IO

--- plugins/git4idea/tests/git4idea/ignore/GitIgnoredCheckerTest.kt
+++ plugins/git4idea/tests/git4idea/ignore/GitIgnoredCheckerTest.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.application.WriteAction
 import com.intellij.openapi.vcs.Ignored
 import com.intellij.openapi.vcs.NotIgnored
 import com.intellij.openapi.vcs.VcsIgnoreChecker
+import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VfsUtilCore
 import git4idea.GitVcs
@@ -19,19 +20,23 @@ private const val folderName = "new_folder"
 class GitIgnoredCheckerTest : GitPlatformTest() {
 
   private lateinit var gitIgnoreChecker: VcsIgnoreChecker
+  private lateinit var gitIgnore: File
 
   override fun setUp() {
     super.setUp()
     createRepository(project, projectPath)
     gitIgnoreChecker = VcsIgnoreChecker.EXTENSION_POINT_NAME.getExtensionList(project).find { it.supportedVcs == GitVcs.getKey() }
       ?: throw IllegalStateException("Cannot find registered GitRootChecker")
+    gitIgnore = File("$projectPath/${GitRepositoryFiles.GITIGNORE}").apply {
+      createNewFile()
+      LocalFileSystem.getInstance().refreshIoFiles(setOf(this))
+    }
   }
 
   fun `test ignored in gitignore`() {
     val dir = WriteAction.computeAndWait<File, IOException> {
       VfsUtilCore.virtualToIoFile(VfsUtil.createDirectoryIfMissing(projectRoot, folderName))
     }
-    val gitIgnore = File("$projectPath/${GitRepositoryFiles.GITIGNORE}")
 
     gitIgnore.writeText("$folderName/")
     assertTrue(gitIgnoreChecker.isIgnored(projectRoot, dir) is Ignored)
@@ -47,7 +52,7 @@ class GitIgnoredCheckerTest : GitPlatformTest() {
     val dir = WriteAction.computeAndWait<File, IOException> {
       VfsUtilCore.virtualToIoFile(VfsUtil.createDirectoryIfMissing(projectRoot, folderName))
     }
-    val gitIgnore = File("$projectPath/${GitRepositoryFiles.GITIGNORE}")
+
     assertTrue(gitIgnoreChecker.isIgnored(projectRoot, dir) is NotIgnored)
 
     gitIgnore.writeText("!$folderName/")
