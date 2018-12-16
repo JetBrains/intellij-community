d2b4a3e4f85b0abe1013138e0927f0db34b4d09f
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2018-12-16 18:03:45 +0700

    Cleanup in PythonEnterHandler
    * Got rid of unnecessary string builders for trivial concatenation.
    * Extracted a common part of an "if" statement.
    * Gave a variable clearer name.

--- python/src/com/jetbrains/python/editor/PythonEnterHandler.java
+++ python/src/com/jetbrains/python/editor/PythonEnterHandler.java
@@ -149,33 +149,28 @@ public class PythonEnterHandler extends EnterHandlerDelegateAdapter {
         final String quote = stringElement.getQuote();
 
         // Don't split in the middle of an escape sequence
-        final boolean nextIsBackslash = "\\".equals(doc.getText(TextRange.from(offset - 1, 1)));
-        final boolean isEscapedQuote = quote.equals(doc.getText(TextRange.from(offset, 1))) && nextIsBackslash;
-        final boolean isEscapedBackslash = "\\".equals(doc.getText(TextRange.from(offset - 2, 1))) && nextIsBackslash;
-        if (nextIsBackslash && !isEscapedQuote && !isEscapedBackslash) return Result.Continue;
+        final boolean afterBackslash = "\\".equals(doc.getText(TextRange.from(offset - 1, 1)));
+        final boolean isEscapedQuote = quote.equals(doc.getText(TextRange.from(offset, 1))) && afterBackslash;
+        final boolean isEscapedBackslash = "\\".equals(doc.getText(TextRange.from(offset - 2, 1))) && afterBackslash;
+        if (afterBackslash && !isEscapedQuote && !isEscapedBackslash) return Result.Continue;
 
         myPostprocessShift = pref.length() + quote.length();
 
         if (PsiTreeUtil.getParentOfType(stringElement, IMPLICIT_WRAP_CLASSES) != null) {
-          final StringBuilder replacementString = new StringBuilder();
-          replacementString.append(quote).append(pref).append(quote);
-          doc.insertString(offset, replacementString);
+          doc.insertString(offset, quote + pref + quote);
           caretOffset.set(caretOffset.get() + 1);
-          return Result.Continue;
         }
         else {
-          final StringBuilder replacementString = new StringBuilder();
           int insertionOffset = offset;
           if (isEscapedQuote) {
             // Preserve the escaped quote, split after it
             caretOffset.set(caretOffset.get() + 1);
             insertionOffset++;
           }
-          replacementString.append(quote).append(" \\").append(pref).append(quote);
-          doc.insertString(insertionOffset, replacementString);
+          doc.insertString(insertionOffset, quote + " \\" + pref + quote);
           caretOffset.set(caretOffset.get() + 3);
-          return Result.Continue;
         }
+        return Result.Continue;
       }
     }
 
