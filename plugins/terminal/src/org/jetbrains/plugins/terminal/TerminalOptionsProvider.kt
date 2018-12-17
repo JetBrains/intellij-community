JetBrains/intellij-community

commit d633ea4419819881e94c28e3c6fc80429c08403d

terminal: no need in state defensive copying

plugins/terminal/src/org/jetbrains/plugins/terminal/TerminalOptionsProvider.kt
@@ -11,10 +11,9 @@ import java.io.File
 /**
  * @author traff
  */
-
 @State(name = "TerminalOptionsProvider", storages = [(Storage("terminal.xml"))])
 class TerminalOptionsProvider : PersistentStateComponent<TerminalOptionsProvider.State> {
-  private val myState = State()
+  private var myState = State()
 
   var shellPath: String? by ValueWithDefault(State::myShellPath, myState) { defaultShellPath }
 
@@ -23,18 +22,7 @@ class TerminalOptionsProvider : PersistentStateComponent<TerminalOptionsProvider
   }
 
   override fun loadState(state: State) {
-    myState.myCloseSessionOnLogout = state.myCloseSessionOnLogout
-    myState.myReportMouse = state.myReportMouse
-    myState.mySoundBell = state.mySoundBell
-    myState.myTabName = state.myTabName
-    myState.myCopyOnSelection = state.myCopyOnSelection
-    myState.myPasteOnMiddleMouseButton = state.myPasteOnMiddleMouseButton
-    myState.myOverrideIdeShortcuts = state.myOverrideIdeShortcuts
-    myState.myShellIntegration = state.myShellIntegration
-    myState.myShellPath = state.myShellPath
-    myState.myHighlightHyperlinks = state.myHighlightHyperlinks
-    myState.myUserSpecifiedEnvs = LinkedHashMap(state.myUserSpecifiedEnvs)
-    myState.myPassParentEnvs = state.myPassParentEnvs
+    myState = state
   }
 
   fun closeSessionOnLogout(): Boolean {

commit 469b139283a7dcff130158f50e3bdc8bc1555750

terminal: do not capture empty State instance by shellPath property (IDEA-204321)
Regression after d633ea4419819881e94c28e3c6fc80429c08403d

plugins/terminal/src/org/jetbrains/plugins/terminal/TerminalOptionsProvider.kt
@@ -17,14 +17,20 @@ import java.io.File
 class TerminalOptionsProvider : PersistentStateComponent<TerminalOptionsProvider.State> {
   private var myState = State()
 
-  var shellPath: String? by ValueWithDefault(State::myShellPath, myState) { defaultShellPath }
-
   override fun getState(): State? {
     return myState
   }
 
   override fun loadState(state: State) {
     myState = state
+  }
+
+  fun getShellPath(): String? {
+    return myState.myShellPath ?: defaultShellPath()
+  }
+
+  fun setShellPath(shellPath: String) {
+    myState.myShellPath = shellPath
   }
 
   fun closeSessionOnLogout(): Boolean {
@@ -120,26 +126,20 @@ class TerminalOptionsProvider : PersistentStateComponent<TerminalOptionsProvider
     myState.envDataOptions.set(envData)
   }
 
-  val defaultShellPath: String
-    get() {
-      val shell = System.getenv("SHELL")
-
-      if (shell != null && File(shell).canExecute()) {
-        return shell
-      }
-
-      if (SystemInfo.isUnix) {
-        if (File("/bin/bash").exists()) {
-          return "/bin/bash"
-        }
-        else {
-          return "/bin/sh"
-        }
-      }
-      else {
-        return "cmd.exe"
-      }
+  private fun defaultShellPath(): String {
+    val shell = System.getenv("SHELL")
+    if (shell != null && File(shell).canExecute()) {
+      return shell
     }
+    if (SystemInfo.isUnix) {
+      val bashPath = "/bin/bash"
+      if (File(bashPath).exists()) {
+        return bashPath
+      }
+      return "/bin/sh"
+    }
+    return "cmd.exe"
+  }
 
   companion object {
     val instance: TerminalOptionsProvider