73b5c670ef434c20f7496c84f5e248260c6ac194
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2018-12-17 19:05:28 +0300

    recognizing web references in string literals

--- platform/lang-impl/src/com/intellij/psi/impl/source/resolve/reference/UrlReferenceContributor.java
+++ platform/lang-impl/src/com/intellij/psi/impl/source/resolve/reference/UrlReferenceContributor.java
@@ -10,7 +10,7 @@ import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class WebReferenceContributor extends PsiReferenceContributor {
+public class UrlReferenceContributor extends PsiReferenceContributor {
   @Override
   public void registerReferenceProviders(@NotNull PsiReferenceRegistrar registrar) {
 
