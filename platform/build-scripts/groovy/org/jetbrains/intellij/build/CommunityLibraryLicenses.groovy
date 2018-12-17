7951fe15a251d37b0fcaffe4a0f60c6462fc2fae
nik <Nikolay.Chashnikov@jetbrains.com>
2018-12-17 13:22:16 +0300

    IDEA project configuration: merge kotlinx-coroutines-core library to kotlinx-coroutines-jdk8
    This way we won't need to add the both libraries to dependencies of modules which use coroutines and change the versions synchronously.

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/CommunityLibraryLicenses.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/CommunityLibraryLicenses.groovy
@@ -493,7 +493,6 @@ class CommunityLibraryLicenses {
     jetbrainsLibrary("KotlinJavaRuntime"),
     jetbrainsLibrary("KotlinScriptRuntime"),
     jetbrainsLibrary("KotlinTest"),
-    jetbrainsLibrary("kotlinx-coroutines-core"),
     jetbrainsLibrary("kotlinx-coroutines-jdk8"),
     jetbrainsLibrary("Maven Embedder"),
     jetbrainsLibrary("optimizedFileManager.jar"),
