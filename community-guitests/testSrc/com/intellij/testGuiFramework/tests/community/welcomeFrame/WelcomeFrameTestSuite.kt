4af60ad9c55f69d41edc9ce9d03ed114c6b2964e
Sergey Karashevich <sergey.karashevich@jetbrains.com>
2018-12-19 01:30:45 +0300

    [gui-test] add FirstStart (GUI-207)

--- community-guitests/testSrc/com/intellij/testGuiFramework/tests/community/welcomeFrame/WelcomeFrameTestSuite.kt
+++ community-guitests/testSrc/com/intellij/testGuiFramework/tests/community/welcomeFrame/WelcomeFrameTestSuite.kt
@@ -4,19 +4,31 @@
 
 package com.intellij.testGuiFramework.tests.community.welcomeFrame
 
+import com.intellij.testGuiFramework.framework.FirstStartWith
 import com.intellij.testGuiFramework.framework.GuiTestSuite
+import com.intellij.testGuiFramework.framework.GuiTestSuiteRunner
 import com.intellij.testGuiFramework.framework.RunWithIde
 import com.intellij.testGuiFramework.framework.param.GuiTestSuiteParam
 import com.intellij.testGuiFramework.launcher.ide.CommunityIde
+import com.intellij.testGuiFramework.launcher.ide.CommunityIdeFirstStart
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import org.junit.runners.Suite
 
 @RunWith(Suite::class)
 @Suite.SuiteClasses(
+  WelcomeFrameTestSuite.Regular::class,
   WelcomeFrameTestSuite.Repeated::class
 )
 class WelcomeFrameTestSuite : GuiTestSuite() {
+
+  @RunWith(GuiTestSuiteRunner::class)
+  @RunWithIde(CommunityIde::class)
+  @FirstStartWith(CommunityIdeFirstStart::class)
+  @Suite.SuiteClasses(
+    WelcomeFrameTest::class
+  )
+  class Regular
 
   @RunWithIde(CommunityIde::class)
   @Suite.SuiteClasses(
