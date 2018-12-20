41d73153021a0383c3de53011c030566c4edd316
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2018-12-20 18:44:18 +0100

    Cleanup (corrects crazy test formatting)

--- java/java-tests/testSrc/com/intellij/java/codeInsight/javadoc/LightJavadocGeneratorTest.kt
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/javadoc/LightJavadocGeneratorTest.kt
@@ -9,16 +9,14 @@ import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase
 class LightJavadocGeneratorTest : LightCodeInsightFixtureTestCase() {
   override fun getProjectDescriptor() = JAVA_9
 
-  fun testPlainModule() {
-    doTestModule("module M.N { }", """<div class='definition'><pre>module <b>M.N</b></pre></div>""".trimIndent())
-  }
+  fun testPlainModule() = doTestModule(
+    "module M.N { }",
+    "<div class='definition'><pre>module <b>M.N</b></pre></div>")
 
-  fun testDocumentedModule() {
-    doTestModule("/** One humble module. */\n@Deprecated\nmodule M.N { }",
-      // trailing space after first line is expected, so, to ensure that it will be not trimmed, 2 raw literals are used instead of one.
-     """<div class='definition'><pre>@<a href="psi_element://java.lang.Deprecated"><code>Deprecated</code></a> """ + "\n" +
-     """module <b>M.N</b></pre></div><div class='content'> One humble module. </div><table class='sections'><p></table>""")
-  }
+  fun testDocumentedModule() = doTestModule(
+    "/** One humble module. */\n@Deprecated\nmodule M.N { }",
+    "<div class='definition'><pre>@<a href=\"psi_element://java.lang.Deprecated\"><code>Deprecated</code></a> \n" +
+    "module <b>M.N</b></pre></div><div class='content'> One humble module. </div><table class='sections'><p></table>")
 
   private fun doTestModule(text: String, expected: String) {
     val file = myFixture.configureByText("module-info.java", text) as PsiJavaFile
