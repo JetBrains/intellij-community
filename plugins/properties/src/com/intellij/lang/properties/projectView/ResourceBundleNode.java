68dd246a4df70e2e311b87dc0dff21b3ddc11ba1
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2018-12-20 13:57:18 +0300

    resource bundle: check underlying rb is valid when do operations in project view IDEA-180097

--- plugins/properties/src/com/intellij/lang/properties/projectView/ResourceBundleNode.java
+++ plugins/properties/src/com/intellij/lang/properties/projectView/ResourceBundleNode.java
@@ -67,7 +67,9 @@ public class ResourceBundleNode extends ProjectViewNode<ResourceBundle> implemen
 
   @Override
   public VirtualFile getVirtualFile() {
-    final List<PropertiesFile> list = getResourceBundle().getPropertiesFiles();
+    ResourceBundle rb = getResourceBundle();
+    if (!rb.isValid()) return null;
+    final List<PropertiesFile> list = rb.getPropertiesFiles();
     if (!list.isEmpty()) {
       return list.get(0).getVirtualFile();
     }
@@ -77,7 +79,10 @@ public class ResourceBundleNode extends ProjectViewNode<ResourceBundle> implemen
   @Override
   public void update(@NotNull PresentationData presentation) {
     presentation.setIcon(AllIcons.Nodes.ResourceBundle);
-    presentation.setPresentableText(PropertiesBundle.message("project.view.resource.bundle.tree.node.text", ObjectUtils.notNull(getValue()).getBaseName()));
+    ResourceBundle rb = getResourceBundle();
+    if (rb.isValid()) {
+      presentation.setPresentableText(PropertiesBundle.message("project.view.resource.bundle.tree.node.text", rb.getBaseName()));
+    }
   }
 
   @Override
