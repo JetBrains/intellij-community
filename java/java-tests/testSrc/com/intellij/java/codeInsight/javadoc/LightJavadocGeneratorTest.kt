a146e673ebd569927553e0483a59354ab48e1475
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2018-12-21 15:18:26 +0100

    [tests] more Javadoc link transforming test cases

--- java/java-tests/testSrc/com/intellij/java/codeInsight/javadoc/LightJavadocGeneratorTest.kt
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/javadoc/LightJavadocGeneratorTest.kt
@@ -2,6 +2,7 @@
 package com.intellij.java.codeInsight.javadoc
 
 import com.intellij.codeInsight.javadoc.JavaDocInfoGeneratorFactory
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiJavaFile
 import com.intellij.testFramework.assertions.Assertions.assertThat
 import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase
@@ -16,11 +17,36 @@ class LightJavadocGeneratorTest : LightCodeInsightFixtureTestCase() {
   fun testDocumentedModule() = doTestModule(
     "/** One humble module. */\n@Deprecated\nmodule M.N { }",
     "<div class='definition'><pre>@<a href=\"psi_element://java.lang.Deprecated\"><code>Deprecated</code></a> \n" +
-    "module <b>M.N</b></pre></div><div class='content'> One humble module. </div><table class='sections'><p></table>")
+    "module <b>M.N</b></pre></div><div class='content'> One humble module. </div>")
 
+  fun testRootedClassLink() = doTestLink("{@docRoot}/java/lang/Character.html#unicode", "psi_element://java.lang.Character###unicode")
+  fun testRootedPackageLink() = doTestLink("{@docRoot}/java/util/package-summary.html", "psi_element://java.util")
+  fun testRootedFileLink() = doTestLink("{@docRoot}/java/lang/doc-files/ValueBased.html", "../../java/lang/doc-files/ValueBased.html")
+
+  fun testRelativeClassLink() = doTestLink("../../java/lang/Character.html#unicode", "psi_element://java.lang.Character###unicode")
+  fun testRelativePackageLink() = doTestLink("../../java/util/package-summary.html", "psi_element://java.util")
+  fun testRelativeFileLink() = doTestLink("../../java/lang/doc-files/ValueBased.html", "../../java/lang/doc-files/ValueBased.html")
+
+  //<editor-fold desc="Helpers.">
   private fun doTestModule(text: String, expected: String) {
     val file = myFixture.configureByText("module-info.java", text) as PsiJavaFile
-    val docInfo = JavaDocInfoGeneratorFactory.create(project, file.moduleDeclaration!!).generateDocInfo(null)!!.replace("&nbsp;", " ")
-    assertThat(docInfo).isEqualTo(expected)
+    assertThat(generateDoc(file.moduleDeclaration!!)).isEqualTo(expected)
   }
+
+  private fun doTestLink(link: String, expected: String) {
+    doTestClass(
+      "package a.b;\n/** A <a href=\"${link}\">link</a>. */\ninterface I { }",
+      "<div class='definition'><pre>a.b<br>interface <b>I</b></pre></div><div class='content'> A <a href=\"${expected}\">link</a>. </div>")
+  }
+
+  private fun doTestClass(text: String, expected: String) {
+    val file = myFixture.configureByText("a.java", text) as PsiJavaFile
+    assertThat(generateDoc(file.classes[0])).isEqualTo(expected)
+  }
+
+  private fun generateDoc(element: PsiElement): String? =
+    JavaDocInfoGeneratorFactory.create(project, element).generateDocInfo(null)
+      ?.replace("&nbsp;", " ")
+      ?.substringBeforeLast("<table class='sections'><p></table>")
+  //</editor-fold>
 }
