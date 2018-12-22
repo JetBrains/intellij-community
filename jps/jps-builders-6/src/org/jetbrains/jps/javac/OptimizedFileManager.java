92988e314e73307a5aa3a2ba2a807f74878526a9
Rostislav Krasny <rostigm@gmail.com>
2018-12-22 03:48:56 +0200

    1st stage of fixing IDEA-204628
    IntelliJ IDEA can be build by other IntelliJ IDEA but not yet by the 'ant' command.
    I can't figure out how to add the lib/tools.jar of JDK into the compile time classpath.
    Ant calls Gradle and in Gradle you can call 'org.gradle.internal.jvm.Jvm.current().toolsJar'
    to get full path into the lib/tools.jar library, but I didn't find the right place to put it in.
    Standard examples of using it, found in Google, don't work here.

--- jps/jps-builders-6/src/org/jetbrains/jps/javac/OptimizedFileManager.java
+++ jps/jps-builders-6/src/org/jetbrains/jps/javac/OptimizedFileManager.java
@@ -1,24 +1,8 @@
-/*
- * Copyright 2000-2012 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.jps.javac;
 
-import com.sun.tools.javac.util.Context;
-import com.sun.tools.javac.util.JCDiagnostic;
+import com.sun.tools.javac.util.*;
 import com.sun.tools.javac.util.List;
-import com.sun.tools.javac.util.ListBuffer;
 
 import javax.lang.model.SourceVersion;
 import javax.tools.*;
@@ -300,27 +284,33 @@ class OptimizedFileManager extends DefaultFileManager {
       this.f = f;
     }
 
+    @Override
     public InputStream openInputStream() throws IOException {
       return new FileInputStream(f);
     }
 
+    @Override
     public Reader openReader(boolean ignoreEncodingErrors) throws IOException {
       throw new UnsupportedOperationException();
     }
 
+    @Override
     public OutputStream openOutputStream() throws IOException {
       throw new UnsupportedOperationException();
     }
 
+    @Override
     public Writer openWriter() throws IOException {
       throw new UnsupportedOperationException();
     }
 
     @Deprecated
+    @Override
     public String getName() {
       return f.getPath();
     }
 
+    @Override
     public boolean isNameCompatible(String simpleName, JavaFileObject.Kind kind) {
       final String n = simpleName + kind.extension;
       final String fileName = f.getName();
@@ -340,18 +330,22 @@ class OptimizedFileManager extends DefaultFileManager {
 
     /** @deprecated see bug 6410637 */
     @Deprecated
+    @Override
     public String getPath() {
       return f.getPath();
     }
 
+    @Override
     public long getLastModified() {
       return f.lastModified();
     }
 
+    @Override
     public boolean delete() {
       return f.delete();
     }
 
+    @Override
     public CharBuffer getCharContent(boolean ignoreEncodingErrors) throws IOException {
       CharBuffer cb;
       if (ourUseContentCache) {
@@ -403,6 +397,7 @@ class OptimizedFileManager extends DefaultFileManager {
       return f.hashCode();
     }
 
+    @Override
     public URI toUri() {
       try {
         return convertToURI(f.getPath());
@@ -542,6 +537,7 @@ class OptimizedFileManager extends DefaultFileManager {
 
   private static volatile boolean ourPathCacheClearProblem = false;
 
+  @Override
   public void close() {
     try {
       super.close();
