7bda30a218858604e2a945940dfd3a4b27a6cd93
Bas Leijdekkers <basleijdekkers@gmail.com>
2018-12-26 21:30:02 +0100

    IG: report integer division in floating point context in comparisons (IDEA-204762)

--- plugins/InspectionGadgets/test/com/siyeh/igtest/numeric/integer_division_in_floating_point_context/IntegerDivisionInFloatingPointContext.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/numeric/integer_division_in_floating_point_context/IntegerDivisionInFloatingPointContext.java
@@ -7,5 +7,10 @@ public class IntegerDivisionInFloatingPointContext {
         double y = <warning descr="'1 / 2 / 3': integer division in floating-point context">1 / 2 / 3</warning>;
         double z = <warning descr="'1 / 2 / 3 / 4.0': integer division in floating-point context">1 / 2 / 3 / 4.0</warning>;
         double z1 = 1.0 / 2 / 3 / 4.0;
+        float a =  (3 + <warning descr="'3/5': integer division in floating-point context">3/5</warning>) + 8 + 8;
+    }
+
+    void comparison(int intValue, int anotherIntValue) {
+        if(<warning descr="'intValue/anotherIntValue': integer division in floating-point context">intValue/anotherIntValue</warning> > 0.1);
     }
 }
