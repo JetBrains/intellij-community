751912f2d438f0a757a2279b963d48e1dd432422
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2018-12-27 21:55:23 +0300

    vcs: Refactor "DefaultCommitResultHandler"

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/DefaultCommitResultHandler.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/DefaultCommitResultHandler.kt
@@ -7,16 +7,23 @@ import com.intellij.openapi.vcs.VcsException
 import com.intellij.openapi.vcs.VcsNotifier
 import com.intellij.openapi.vcs.changes.CommitResultHandler
 import com.intellij.openapi.vcs.changes.ui.CommitHelper.collectErrors
-import java.util.*
+
+private val FROM = listOf("<", ">")
+private val TO = listOf("&lt;", "&gt;")
+
+/*
+  Commit message is passed to NotificationManagerImpl#doNotify and displayed as HTML.
+  Thus HTML tag braces (< and >) should be escaped,
+  but only they since the text is passed directly to HTML <BODY> tag and is not a part of an attribute or else.
+ */
+private fun escape(s: String) = replace(s, FROM, TO)
+
+private fun hasOnlyWarnings(exceptions: List<VcsException>) = exceptions.all { it.isWarning }
 
 class DefaultCommitResultHandler(private val myHelper: CommitHelper) : CommitResultHandler {
-  override fun onSuccess(commitMessage: String) {
-    reportResult()
-  }
 
-  override fun onFailure() {
-    reportResult()
-  }
+  override fun onSuccess(commitMessage: String) = reportResult()
+  override fun onFailure() = reportResult()
 
   private fun reportResult() {
     val allExceptions = myHelper.exceptions
@@ -26,60 +33,45 @@ class DefaultCommitResultHandler(private val myHelper: CommitHelper) : CommitRes
 
     val notifier = VcsNotifier.getInstance(myHelper.project)
     val message = getCommitSummary()
-    if (errorsSize > 0) {
-      val title = pluralize(message("message.text.commit.failed.with.error"), errorsSize)
-      notifier.notifyError(title, message)
-    }
-    else if (warningsSize > 0) {
-      val title = pluralize(message("message.text.commit.finished.with.warning"), warningsSize)
-      notifier.notifyImportantWarning(title, message)
-    }
-    else {
-      notifier.notifySuccess(message)
+
+    when {
+      errorsSize > 0 -> {
+        val title = pluralize(message("message.text.commit.failed.with.error"), errorsSize)
+        notifier.notifyError(title, message)
+      }
+      warningsSize > 0 -> {
+        val title = pluralize(message("message.text.commit.finished.with.warning"), warningsSize)
+        notifier.notifyImportantWarning(title, message)
+      }
+      else -> notifier.notifySuccess(message)
     }
   }
 
-  private fun getCommitSummary(): String {
-    val content = StringBuilder(getFileSummaryReport())
+  private fun getCommitSummary() = StringBuilder(getFileSummaryReport()).apply {
     val commitMessage = myHelper.commitMessage
     if (!isEmpty(commitMessage)) {
-      content.append(": ").append(escape(commitMessage))
+      append(": ").append(escape(commitMessage))
     }
     val feedback = myHelper.feedback
     if (!feedback.isEmpty()) {
-      content.append("<br/>")
-      content.append(join(feedback, "<br/>"))
+      append("<br/>")
+      append(join(feedback, "<br/>"))
     }
     val exceptions = myHelper.exceptions
     if (!hasOnlyWarnings(exceptions)) {
-      content.append("<br/>")
-      content.append(join(exceptions, { it.message }, "<br/>"))
+      append("<br/>")
+      append(join(exceptions, { it.message }, "<br/>"))
     }
-    return content.toString()
-  }
+  }.toString()
 
   private fun getFileSummaryReport(): String {
     val failed = myHelper.failedToCommitChanges.size
     val committed = myHelper.changes.size - failed
-    var fileSummary = committed.toString() + " " + pluralize("file", committed) + " committed"
+
+    var fileSummary = "$committed ${pluralize("file", committed)} committed"
     if (failed > 0) {
-      fileSummary += ", " + failed + " " + pluralize("file", failed) + " failed to commit"
+      fileSummary += ", $failed ${pluralize("file", failed)} failed to commit"
     }
     return fileSummary
-  }
-
-  /*
-    Commit message is passed to NotificationManagerImpl#doNotify and displayed as HTML.
-    Thus HTML tag braces (< and >) should be escaped,
-    but only they since the text is passed directly to HTML <BODY> tag and is not a part of an attribute or else.
-   */
-  private fun escape(s: String): String {
-    val FROM = Arrays.asList("<", ">")
-    val TO = Arrays.asList("&lt;", "&gt;")
-    return replace(s, FROM, TO)
-  }
-
-  private fun hasOnlyWarnings(exceptions: List<VcsException>): Boolean {
-    return exceptions.stream().allMatch { it.isWarning }
   }
 }
