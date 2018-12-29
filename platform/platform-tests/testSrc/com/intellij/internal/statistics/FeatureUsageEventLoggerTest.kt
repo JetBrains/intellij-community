cf38c4ad149eaddf4ad1bd435378a86e62ef6b8e
Kirill Smelov <kirill.smelov@jetbrains.com>
2018-12-29 15:10:45 +0300

    Revert "Remove unused import"
    This reverts commit 45dddf26

--- platform/platform-tests/testSrc/com/intellij/internal/statistics/FeatureUsageEventLoggerTest.kt
+++ platform/platform-tests/testSrc/com/intellij/internal/statistics/FeatureUsageEventLoggerTest.kt
@@ -5,6 +5,7 @@ import com.intellij.internal.statistic.eventLog.*
 import com.intellij.util.containers.ContainerUtil
 import org.junit.Test
 import java.io.File
+import java.util.*
 import java.util.concurrent.TimeUnit
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
