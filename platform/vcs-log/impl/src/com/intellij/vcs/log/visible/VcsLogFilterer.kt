59090d854fceea16d95e87a17b920fe28c7f35bc
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2018-12-31 17:45:30 +0300

    [vcs-log] convert VcsLogFilterer to kotlin

--- platform/vcs-log/impl/src/com/intellij/vcs/log/visible/VcsLogFilterer.kt
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/visible/VcsLogFilterer.kt
@@ -1,19 +1,17 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.vcs.log.visible;
+package com.intellij.vcs.log.visible
 
-import com.intellij.openapi.util.Pair;
-import com.intellij.vcs.log.VcsLogFilterCollection;
-import com.intellij.vcs.log.data.DataPack;
-import com.intellij.vcs.log.graph.PermanentGraph;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.util.Pair
+import com.intellij.vcs.log.VcsLogFilterCollection
+import com.intellij.vcs.log.data.DataPack
+import com.intellij.vcs.log.graph.PermanentGraph
 
-public interface VcsLogFilterer {
+interface VcsLogFilterer {
 
-  boolean canFilterEmptyPack(@NotNull VcsLogFilterCollection filters);
+  fun canFilterEmptyPack(filters: VcsLogFilterCollection): Boolean
 
-  @NotNull
-  Pair<VisiblePack, CommitCountStage> filter(@NotNull DataPack pack,
-                                             @NotNull PermanentGraph.SortType type,
-                                             @NotNull VcsLogFilterCollection filters,
-                                             @NotNull CommitCountStage count);
+  fun filter(pack: DataPack,
+             type: PermanentGraph.SortType,
+             filters: VcsLogFilterCollection,
+             count: CommitCountStage): Pair<VisiblePack, CommitCountStage>
 }
