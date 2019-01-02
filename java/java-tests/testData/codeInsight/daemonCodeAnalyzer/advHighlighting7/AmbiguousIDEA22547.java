2e7f05efd00f02fe2c4901274d68bd6876f79b28
Anna.Kozlova <anna.kozlova@jetbrains.com>
2019-01-02 13:33:30 +0100

    advHighlighting -> light test case

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/advHighlighting7/AmbiguousIDEA22547.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/advHighlighting7/AmbiguousIDEA22547.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2011 JetBrains s.r.o.
+ * Copyright 2000-2012 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,8 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package P1;
+package pck;
 
-public class C2 {
-    public static int Foo;
+import static pck.Assert.assertEquals;
+
+class Test {
+  void test() {
+    assertEquals<error descr="Ambiguous method call: both 'Assert.assertEquals(Object, Object)' and 'Assert.assertEquals(long, long)' match">(100L, Long.valueOf(100L))</error>;
+  }
 }
