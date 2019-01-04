4f181ec75bc5d3d7ed78615812313de6d28b6ce8
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-01-05 01:54:43 +0300

    code style
    GitOrigin-RevId: 5bfb118f6f4a6f03ff705214a763031aea0bf5d6

--- plugins/sh/tests/com/intellij/bash/parser/BashParserTest.java
+++ plugins/sh/tests/com/intellij/bash/parser/BashParserTest.java
@@ -12,20 +12,20 @@ public class BashParserTest extends ParsingTestCase {
     return "testData";
   }
 
-  public void testShebang() { doTest(true); }
-  public void testFirst()   { doTest(true); }
-  public void testLines()   { doTest(true); }
-  public void testCase()    { doTest(true); }
-  public void testEcho()    { doTest(true); }
-  public void testFor()     { doTest(true); }
-  public void testSelect()  { doTest(true); }
-  public void testExpr()    { doTest(true); }
-  public void testHeredoc() { doTest(true); }
-  public void testHereEof() { doTest(true); }
-  public void testIf()      { doTest(true); }
-  public void testConditional() { doTest(true); }
-  public void testBacktick(){ doTest(true); }
-  public void testFunction(){ doTest(true); }
-  public void testTrap()    { doTest(true); }
-  public void testMacports(){ doTest(true); }
+  public void testShebang()       { doTest(true); }
+  public void testFirst()         { doTest(true); }
+  public void testLines()         { doTest(true); }
+  public void testCase()          { doTest(true); }
+  public void testEcho()          { doTest(true); }
+  public void testFor()           { doTest(true); }
+  public void testSelect()        { doTest(true); }
+  public void testExpr()          { doTest(true); }
+  public void testHeredoc()       { doTest(true); }
+  public void testHereEof()       { doTest(true); }
+  public void testIf()            { doTest(true); }
+  public void testConditional()   { doTest(true); }
+  public void testBacktick()      { doTest(true); }
+  public void testFunction()      { doTest(true); }
+  public void testTrap()          { doTest(true); }
+  public void testMacports()      { doTest(true); }
 }
