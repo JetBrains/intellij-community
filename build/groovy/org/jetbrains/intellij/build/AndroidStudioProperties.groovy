8db5be981121478067525b20b1eed6c59aaf7e83
Hannah Chu <hanchu@google.com>
2019-01-04 10:17:05 -0800

    Add module for Runtime Pipeline
    This is where the pipeline functionality
    will go when separated from profilers.
    
    Bug: 122481054
    Test: N/A
    Change-Id: Ieed88d73179b0c913168a98ba42f97a722b24cbe

--- build/groovy/org/jetbrains/intellij/build/AndroidStudioProperties.groovy
+++ build/groovy/org/jetbrains/intellij/build/AndroidStudioProperties.groovy
@@ -143,6 +143,7 @@ class AndroidStudioProperties extends BaseIdeaProperties {
       withModule("intellij.android.observable.ui", "android.jar")
       withModule("android.sdktools.flags", "android.jar")
       withModule("intellij.android.layout-inspector", "android.jar")
+      withModule("intellij.android.runtimePipeline", "android.jar")
       withModule("intellij.android.designer", "android.jar")
       withModule("intellij.android.naveditor", "android.jar")
       withModule("intellij.android.sdkUpdates", "android.jar")
