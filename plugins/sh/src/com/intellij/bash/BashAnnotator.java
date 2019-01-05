5eef8ec170a4f088b27fd84d633e99fbc69692d1
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-01-05 23:41:22 +0300

    highlight vars inside strings
    GitOrigin-RevId: 1a671b36e3e74a2994278c983c7d8085ddd226e4

--- plugins/sh/src/com/intellij/bash/BashAnnotator.java
+++ plugins/sh/src/com/intellij/bash/BashAnnotator.java
@@ -1,23 +1,34 @@
 package com.intellij.bash;
 
 import com.intellij.bash.psi.BashString;
-import com.intellij.bash.psi.BashVisitor;
+import com.intellij.bash.psi.BashVariable;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
-import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiRecursiveElementWalkingVisitor;
 import org.jetbrains.annotations.NotNull;
 
 import static com.intellij.bash.BashSyntaxHighlighter.STRING;
+import static com.intellij.bash.BashSyntaxHighlighter.VAR_USE;
 
 public class BashAnnotator implements Annotator {
   @Override
-  public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
-    element.accept(new BashVisitor() {
+  public void annotate(@NotNull PsiElement o, @NotNull AnnotationHolder holder) {
+    if (o instanceof BashString) {
+      holder.createInfoAnnotation(o, null).setTextAttributes(STRING);
+      highlightVariables(o, holder);
+    }
+  }
+
+  private void highlightVariables(@NotNull PsiElement container, @NotNull AnnotationHolder holder) {
+    new PsiRecursiveElementWalkingVisitor() {
       @Override
-      public void visitString(@NotNull BashString o) {
-        holder.createInfoAnnotation(TextRange.from(o.getTextOffset(), o.getTextLength()), null).setTextAttributes(STRING);
+      public void visitElement(PsiElement element) {
+        if (element instanceof BashVariable) {
+          holder.createInfoAnnotation(element, null).setTextAttributes(VAR_USE);
+        }
+        super.visitElement(element);
       }
-    });
+    }.visitElement(container);
   }
 }
