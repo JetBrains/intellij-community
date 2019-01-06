aea8082a3d704b2c5c77c62cd79ce22e4c87e7f2
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-01-06 21:14:35 +0300

    don't double web refs
    GitOrigin-RevId: 6f9fd64a26bbc44aba01aa3a526a4fb68d724e3c

--- plugins/sh/src/com/intellij/bash/BashUrlReferenceContributor.java
+++ plugins/sh/src/com/intellij/bash/BashUrlReferenceContributor.java
@@ -7,6 +7,7 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReferenceContributor;
 import com.intellij.psi.PsiReferenceRegistrar;
 import com.intellij.psi.impl.source.resolve.reference.ArbitraryPlaceUrlReferenceProvider;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -17,7 +18,8 @@ public class BashUrlReferenceContributor extends PsiReferenceContributor {
     registrar.registerReferenceProvider(new PsiElementPattern.Capture<PsiElement>(PsiElement.class) {
       @Override
       public boolean accepts(@Nullable Object o, ProcessingContext context) {
-        return o instanceof BashString || o instanceof BashSimpleCommand;
+        return (o instanceof BashString && PsiTreeUtil.getParentOfType((PsiElement) o, BashSimpleCommand.class) == null)
+            || o instanceof BashSimpleCommand;
       }
     }, new ArbitraryPlaceUrlReferenceProvider(), PsiReferenceRegistrar.LOWER_PRIORITY);
   }
