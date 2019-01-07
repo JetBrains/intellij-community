d7e14d75df78e1e579b687366e1ee70ac55284ec
Anna.Kozlova <anna.kozlova@jetbrains.com>
2019-01-07 17:50:36 +0100

    AIOOBE

--- java/java-impl/src/com/intellij/codeInspection/deadCode/UnusedParametersInspection.java
+++ java/java-impl/src/com/intellij/codeInspection/deadCode/UnusedParametersInspection.java
@@ -112,7 +112,7 @@ class UnusedParametersInspection extends GlobalJavaBatchInspectionTool {
                   for (int i = 0; i < derived.length && !found[0]; i++) {
                     if (scope == null || !scope.contains(derived[i])) {
                       final PsiParameter[] parameters = derived[i].getParameterList().getParameters();
-                      if (parameters.length < idx) continue;
+                      if (idx >= parameters.length) continue;
                       PsiParameter psiParameter = parameters[idx];
                       ReferencesSearch.search(psiParameter, helper.getUseScope(psiParameter), false)
                                       .forEach(new PsiReferenceProcessorAdapter(
