b0e8fd191fcaf3dd2c5487729bab13ad1ad6c5a6
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-01-09 15:32:26 +0100

    fix "Not a directory" exception report

--- platform/projectModel-api/src/com/intellij/util/io/path.kt
+++ platform/projectModel-api/src/com/intellij/util/io/path.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.io
 
 import com.intellij.openapi.diagnostic.Logger
@@ -208,7 +208,7 @@ fun Path.basicAttributesIfExists(): BasicFileAttributes? {
   try {
     return Files.readAttributes(this, BasicFileAttributes::class.java)
   }
-  catch (ignored: NoSuchFileException) {
+  catch (ignored: FileSystemException) {
     return null
   }
 }
