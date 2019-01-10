7096bc4ae3b6f9cdce7849a97c12d315cc93946c
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-01-11 00:20:58 +0300

    add a simple doc provider which relies on info command
    GitOrigin-RevId: 3dc6e8a0d6a921ee2d3e6c1a990bffa65dd2eccd

---
+++ plugins/sh/src/com/intellij/bash/BashDocumentationProvider.java
@@ -0,0 +1,75 @@
+package com.intellij.bash;
+
+import com.intellij.bash.psi.BashGenericCommandDirective;
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.configurations.GeneralCommandLine;
+import com.intellij.execution.process.ProcessOutput;
+import com.intellij.execution.util.ExecUtil;
+import com.intellij.lang.documentation.AbstractDocumentationProvider;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.util.AtomicNullableLazyValue;
+import com.intellij.openapi.util.NullableLazyValue;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.impl.source.tree.LeafPsiElement;
+import com.intellij.util.EnvironmentUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+
+public class BashDocumentationProvider extends AbstractDocumentationProvider {
+  private static final int TIMEOUT_IN_MILLISECONDS = 3 * 1000;
+  private final static Logger LOG = Logger.getInstance(BashDocumentationProvider.class);
+
+  private static final NullableLazyValue<String> myManExecutable = new AtomicNullableLazyValue<String>() {
+    @Nullable
+    @Override
+    protected String compute() {
+      String path = EnvironmentUtil.getValue("PATH");
+      if (path != null) {
+        for (String dir : StringUtil.tokenize(path, File.pathSeparator)) {
+          File file = new File(dir, "info");
+          if (file.canExecute()) return file.getAbsolutePath();
+        }
+      }
+      return null;
+    }
+  };
+
+  @Override
+  public String generateDoc(PsiElement o, PsiElement originalElement) {
+    if (o instanceof LeafPsiElement && ((LeafPsiElement) o).getElementType() == BashTypes.WORD) {
+      if (!(o.getParent() instanceof BashGenericCommandDirective)) return null;
+      try {
+        return wrapIntoHtml(fetchInfo(o.getText()));
+      }
+      catch (ExecutionException e) {
+        LOG.warn(e);
+      }
+    }
+    return null;
+  }
+
+  @Nullable
+  @Override
+  public PsiElement getCustomDocumentationElement(@NotNull Editor editor, @NotNull PsiFile file, @Nullable PsiElement contextElement) {
+    return contextElement;
+  }
+
+  private String fetchInfo(String commandName) throws ExecutionException {
+    String manExecutable = myManExecutable.getValue();
+    if (manExecutable == null) return "Can't find info in your $PATH";
+
+    GeneralCommandLine commandLine = new GeneralCommandLine(manExecutable).withParameters(commandName);
+    ProcessOutput output = ExecUtil.execAndGetOutput(commandLine, TIMEOUT_IN_MILLISECONDS);
+    return output.getExitCode() != 0 ? output.getStderr() : output.getStdout();
+  }
+
+  @Nullable
+  private static String wrapIntoHtml(@Nullable String s) {
+    return s == null ? null : "<html><body><pre>" + s + "</pre></body></html>";
+  }
+}
