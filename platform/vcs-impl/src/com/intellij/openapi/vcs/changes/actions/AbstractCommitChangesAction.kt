0c17d7d5873729c5e0da5da34b1c2489b7c611c1
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2019-01-10 22:38:37 +0300

    vcs: Convert "AbstractCommitChangesAction" and inheritors to kotlin

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/AbstractCommitChangesAction.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/AbstractCommitChangesAction.kt
@@ -1,77 +1,54 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
-package com.intellij.openapi.vcs.changes.actions;
+package com.intellij.openapi.vcs.changes.actions
 
-import com.intellij.openapi.actionSystem.ActionPlaces;
-import com.intellij.openapi.actionSystem.Presentation;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.FilePath;
-import com.intellij.openapi.vcs.FileStatus;
-import com.intellij.openapi.vcs.ProjectLevelVcsManager;
-import com.intellij.openapi.vcs.actions.AbstractCommonCheckinAction;
-import com.intellij.openapi.vcs.actions.VcsContext;
-import com.intellij.openapi.vcs.changes.Change;
-import com.intellij.openapi.vcs.changes.ChangeList;
-import com.intellij.openapi.vcs.changes.ChangeListManager;
-import com.intellij.util.ArrayUtil;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.actionSystem.ActionPlaces
+import com.intellij.openapi.actionSystem.Presentation
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vcs.FilePath
+import com.intellij.openapi.vcs.FileStatus
+import com.intellij.openapi.vcs.ProjectLevelVcsManager
+import com.intellij.openapi.vcs.actions.AbstractCommonCheckinAction
+import com.intellij.openapi.vcs.actions.VcsContext
+import com.intellij.openapi.vcs.changes.Change
+import com.intellij.openapi.vcs.changes.ChangeListManager
+import com.intellij.util.ArrayUtil
+import com.intellij.util.ObjectUtils.notNull
+import java.util.stream.Stream
 
-import java.util.stream.Stream;
+abstract class AbstractCommitChangesAction : AbstractCommonCheckinAction() {
+  override fun getRoots(context: VcsContext): Array<FilePath> = AbstractCommonCheckinAction.getAllContentRoots(context)
 
-import static com.intellij.util.ObjectUtils.notNull;
+  override fun approximatelyHasRoots(dataContext: VcsContext): Boolean = ProjectLevelVcsManager.getInstance(
+    dataContext.project!!).hasAnyMappings()
 
-public abstract class AbstractCommitChangesAction extends AbstractCommonCheckinAction {
-  @NotNull
-  @Override
-  protected FilePath[] getRoots(@NotNull VcsContext context) {
-    return getAllContentRoots(context);
-  }
+  override fun update(vcsContext: VcsContext, presentation: Presentation) {
+    super.update(vcsContext, presentation)
 
-  @Override
-  protected boolean approximatelyHasRoots(@NotNull VcsContext dataContext) {
-    return ProjectLevelVcsManager.getInstance(dataContext.getProject()).hasAnyMappings();
-  }
+    if (presentation.isEnabledAndVisible) {
+      val changes = vcsContext.selectedChanges
 
-  @Override
-  protected void update(@NotNull VcsContext vcsContext, @NotNull Presentation presentation) {
-    super.update(vcsContext, presentation);
+      if (vcsContext.place == ActionPlaces.CHANGES_VIEW_POPUP) {
+        val changeLists = vcsContext.selectedChangeLists
 
-    if (presentation.isEnabledAndVisible()) {
-      Change[] changes = vcsContext.getSelectedChanges();
-
-      if (vcsContext.getPlace().equals(ActionPlaces.CHANGES_VIEW_POPUP)) {
-        ChangeList[] changeLists = vcsContext.getSelectedChangeLists();
-
-        presentation.setEnabled(!ArrayUtil.isEmpty(changeLists)
-                                ? changeLists.length == 1 && !changeLists[0].getChanges().isEmpty()
-                                : !ArrayUtil.isEmpty(changes));
+        presentation.isEnabled = if (!ArrayUtil.isEmpty(changeLists))
+          changeLists!!.size == 1 && !changeLists[0].changes.isEmpty()
+        else
+          !ArrayUtil.isEmpty(changes)
       }
 
-      if (presentation.isEnabled() && !ArrayUtil.isEmpty(changes)) {
-        disableIfAnyHijackedChanges(notNull(vcsContext.getProject()), presentation, changes);
+      if (presentation.isEnabled && !ArrayUtil.isEmpty(changes)) {
+        disableIfAnyHijackedChanges(notNull(vcsContext.project), presentation, changes!!)
       }
     }
   }
 
-  private static void disableIfAnyHijackedChanges(@NotNull Project project, @NotNull Presentation presentation, @NotNull Change[] changes) {
-    ChangeListManager manager = ChangeListManager.getInstance(project);
-    boolean hasHijackedChanges =
-      Stream.of(changes).anyMatch(change -> change.getFileStatus() == FileStatus.HIJACKED && manager.getChangeList(change) == null);
+  private fun disableIfAnyHijackedChanges(project: Project, presentation: Presentation, changes: Array<Change>) {
+    val manager = ChangeListManager.getInstance(project)
+    val hasHijackedChanges = Stream.of(*changes).anyMatch { change ->
+      change.fileStatus === FileStatus.HIJACKED && manager.getChangeList(change) == null
+    }
 
-    presentation.setEnabled(!hasHijackedChanges);
+    presentation.isEnabled = !hasHijackedChanges
   }
 }
--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/shelf/ShelveChangesAction.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/shelf/ShelveChangesAction.kt
@@ -1,37 +1,15 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
-package com.intellij.openapi.vcs.changes.shelf;
+package com.intellij.openapi.vcs.changes.shelf
 
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.VcsBundle;
-import com.intellij.openapi.vcs.actions.VcsContext;
-import com.intellij.openapi.vcs.changes.CommitExecutor;
-import com.intellij.openapi.vcs.changes.actions.AbstractCommitChangesAction;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vcs.VcsBundle
+import com.intellij.openapi.vcs.actions.VcsContext
+import com.intellij.openapi.vcs.changes.CommitExecutor
+import com.intellij.openapi.vcs.changes.actions.AbstractCommitChangesAction
 
-public class ShelveChangesAction extends AbstractCommitChangesAction {
-  @Override
-  protected String getActionName(@NotNull VcsContext dataContext) {
-    return VcsBundle.message("shelve.changes.action");
-  }
+class ShelveChangesAction : AbstractCommitChangesAction() {
+  override fun getActionName(dataContext: VcsContext): String = VcsBundle.message("shelve.changes.action")
 
-  @Override @Nullable
-  protected CommitExecutor getExecutor(@NotNull Project project) {
-    return new ShelveChangesCommitExecutor(project);
-  }
+  override fun getExecutor(project: Project): CommitExecutor = ShelveChangesCommitExecutor(project)
 }
