f02a9abe7d24116f87a252ba75dac02cc6e6850d
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2019-01-11 19:18:41 +0300

    vcs: Convert "CommonCheckinFilesAction", "CommonCheckinProjectAction" to kotlin

--- platform/vcs-impl/src/com/intellij/openapi/vcs/actions/CommonCheckinFilesAction.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/actions/CommonCheckinFilesAction.kt
@@ -1,127 +1,95 @@
-/*
- * Copyright 2000-2012 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.intellij.openapi.vcs.actions;
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.vcs.actions
 
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.openapi.vcs.*;
-import com.intellij.openapi.vcs.changes.Change;
-import com.intellij.openapi.vcs.changes.ChangeListManager;
-import com.intellij.openapi.vcs.changes.LocalChangeList;
-import com.intellij.openapi.vcs.checkin.CheckinEnvironment;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.ObjectUtils;
-import com.intellij.vcsUtil.VcsUtil;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.openapi.vcs.*
+import com.intellij.openapi.vcs.changes.Change
+import com.intellij.openapi.vcs.changes.ChangeListManager
+import com.intellij.openapi.vcs.changes.LocalChangeList
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.util.ObjectUtils
+import com.intellij.util.containers.getIfSingle
+import com.intellij.vcsUtil.VcsUtil
+import java.util.*
+import java.util.stream.Collectors
+import java.util.stream.Stream
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+open class CommonCheckinFilesAction : AbstractCommonCheckinAction() {
+  override fun getActionName(dataContext: VcsContext): String {
+    val actionName = Optional.ofNullable(dataContext.project)
+      .map { project -> getCommonVcs(getRootsStream(dataContext), project) }
+      .map { it!!.checkinEnvironment }
+      .map { it!!.checkinOperationName }
+      .orElse(VcsBundle.message("vcs.command.name.checkin"))
 
-import static com.intellij.util.containers.UtilKt.getIfSingle;
-
-public class CommonCheckinFilesAction extends AbstractCommonCheckinAction {
-  @Override
-  protected String getActionName(@NotNull VcsContext dataContext) {
-    String actionName = Optional.ofNullable(dataContext.getProject())
-      .map(project -> getCommonVcs(getRootsStream(dataContext), project))
-      .map(AbstractVcs::getCheckinEnvironment)
-      .map(CheckinEnvironment::getCheckinOperationName)
-      .orElse(VcsBundle.message("vcs.command.name.checkin"));
-
-    return modifyCheckinActionName(dataContext, actionName);
+    return modifyCheckinActionName(dataContext, actionName)
   }
 
-  private String modifyCheckinActionName(@NotNull VcsContext dataContext, String checkinActionName) {
-    String result = checkinActionName;
-    List<FilePath> roots = getRootsStream(dataContext).limit(2).collect(Collectors.toList());
+  private fun modifyCheckinActionName(dataContext: VcsContext, checkinActionName: String): String {
+    var result = checkinActionName
+    val roots = getRootsStream(dataContext).limit(2).collect(Collectors.toList())
 
     if (!roots.isEmpty()) {
-      String messageKey = roots.get(0).isDirectory() ? "action.name.checkin.directory" : "action.name.checkin.file";
-      result = VcsBundle.message(StringUtil.pluralize(messageKey, roots.size()), checkinActionName);
+      val messageKey = if (roots[0].isDirectory) "action.name.checkin.directory" else "action.name.checkin.file"
+      result = VcsBundle.message(StringUtil.pluralize(messageKey, roots.size), checkinActionName)
     }
 
-    return result;
+    return result
   }
 
-  @Override
-  protected String getMnemonicsFreeActionName(@NotNull VcsContext context) {
-    return modifyCheckinActionName(context, VcsBundle.message("vcs.command.name.checkin.no.mnemonics"));
+  override fun getMnemonicsFreeActionName(context: VcsContext): String {
+    return modifyCheckinActionName(context, VcsBundle.message("vcs.command.name.checkin.no.mnemonics"))
   }
 
-  @Nullable
-  @Override
-  protected LocalChangeList getInitiallySelectedChangeList(@NotNull VcsContext context, @NotNull Project project) {
-    ChangeListManager manager = ChangeListManager.getInstance(project);
-    LocalChangeList defaultChangeList = manager.getDefaultChangeList();
-    LocalChangeList result = null;
+  override fun getInitiallySelectedChangeList(context: VcsContext, project: Project): LocalChangeList? {
+    val manager = ChangeListManager.getInstance(project)
+    val defaultChangeList = manager.defaultChangeList
+    var result: LocalChangeList? = null
 
-    for (FilePath root : getRoots(context)) {
-      if (root.getVirtualFile() == null) continue;
+    for (root in getRoots(context)) {
+      if (root.virtualFile == null) continue
 
-      Collection<Change> changes = manager.getChangesIn(root);
+      val changes = manager.getChangesIn(root)
       if (defaultChangeList != null && containsAnyChange(defaultChangeList, changes)) {
-        return defaultChangeList;
+        return defaultChangeList
       }
-      result = changes.stream().findFirst().map(manager::getChangeList).orElse(null);
+      result = changes.stream().findFirst().map { manager.getChangeList(it) }.orElse(null)
     }
 
-    return ObjectUtils.chooseNotNull(result, defaultChangeList);
+    return ObjectUtils.chooseNotNull(result, defaultChangeList)
   }
 
-  @Override
-  protected boolean approximatelyHasRoots(@NotNull VcsContext dataContext) {
-    ChangeListManager manager = ChangeListManager.getInstance(dataContext.getProject());
+  override fun approximatelyHasRoots(dataContext: VcsContext): Boolean {
+    val manager = ChangeListManager.getInstance(dataContext.project!!)
 
     return getRootsStream(dataContext)
-      .map(FilePath::getVirtualFile)
-      .filter(Objects::nonNull)
-      .anyMatch(file -> isApplicableRoot(file, manager.getStatus(file), dataContext));
+      .map { it.virtualFile }
+      .filter { Objects.nonNull(it) }
+      .anyMatch { file -> isApplicableRoot(file!!, manager.getStatus(file), dataContext) }
   }
 
-  protected boolean isApplicableRoot(@NotNull VirtualFile file, @NotNull FileStatus status, @NotNull VcsContext dataContext) {
-    return status != FileStatus.UNKNOWN && status != FileStatus.IGNORED;
+  protected open fun isApplicableRoot(file: VirtualFile, status: FileStatus, dataContext: VcsContext): Boolean {
+    return status !== FileStatus.UNKNOWN && status !== FileStatus.IGNORED
   }
 
-  @NotNull
-  @Override
-  protected FilePath[] getRoots(@NotNull VcsContext context) {
-    return context.getSelectedFilePaths();
+  override fun getRoots(context: VcsContext): Array<FilePath> {
+    return context.selectedFilePaths
   }
 
-  @NotNull
-  protected Stream<FilePath> getRootsStream(@NotNull VcsContext context) {
-    return context.getSelectedFilePathsStream();
+  protected fun getRootsStream(context: VcsContext): Stream<FilePath> {
+    return context.selectedFilePathsStream
   }
 
-  private static boolean containsAnyChange(@NotNull LocalChangeList changeList, @NotNull Collection<Change> changes) {
-    return changes.stream().anyMatch(changeList.getChanges()::contains);
+  private fun containsAnyChange(changeList: LocalChangeList, changes: Collection<Change>): Boolean {
+    return changes.stream().anyMatch { changeList.changes.contains(it) }
   }
 
-  @Nullable
-  private static AbstractVcs getCommonVcs(@NotNull Stream<? extends FilePath> roots, @NotNull Project project) {
-    return getIfSingle(
-      roots.map(root -> VcsUtil.getVcsFor(project, root))
-        .filter(Objects::nonNull)
-        .distinct()
-        .limit(Math.min(2, ProjectLevelVcsManager.getInstance(project).getAllActiveVcss().length))
-    );
+  private fun getCommonVcs(roots: Stream<out FilePath>, project: Project): AbstractVcs<*>? {
+    return roots.map { root -> VcsUtil.getVcsFor(project, root) }
+      .filter { Objects.nonNull(it) }
+      .distinct()
+      .limit(Math.min(2, ProjectLevelVcsManager.getInstance(project).allActiveVcss.size).toLong())
+      .getIfSingle()
   }
 }
--- platform/vcs-impl/src/com/intellij/openapi/vcs/actions/CommonCheckinProjectAction.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/actions/CommonCheckinProjectAction.kt
@@ -1,55 +1,29 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.intellij.openapi.vcs.actions;
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.vcs.actions
 
-import com.intellij.idea.ActionsBundle;
-import com.intellij.openapi.vcs.FilePath;
-import com.intellij.openapi.vcs.ProjectLevelVcsManager;
-import com.intellij.openapi.vcs.VcsBundle;
-import com.intellij.vcsUtil.VcsUtil;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.idea.ActionsBundle
+import com.intellij.openapi.vcs.FilePath
+import com.intellij.openapi.vcs.ProjectLevelVcsManager
+import com.intellij.openapi.vcs.VcsBundle
+import com.intellij.vcsUtil.VcsUtil
+import java.util.stream.Collectors.toList
+import java.util.stream.Stream
 
-import java.util.stream.Stream;
+open class CommonCheckinProjectAction : AbstractCommonCheckinAction() {
+  override fun getRoots(context: VcsContext): Array<FilePath> {
+    val manager = ProjectLevelVcsManager.getInstance(context.project!!)
 
-public class CommonCheckinProjectAction extends AbstractCommonCheckinAction {
-
-  @Override
-  @NotNull
-  protected FilePath[] getRoots(@NotNull VcsContext context) {
-    ProjectLevelVcsManager manager = ProjectLevelVcsManager.getInstance(context.getProject());
-
-    return Stream.of(manager.getAllActiveVcss())
-        .filter(vcs -> vcs.getCheckinEnvironment() != null)
-        .flatMap(vcs -> Stream.of(manager.getRootsUnderVcs(vcs)))
-        .map(VcsUtil::getFilePath)
-        .toArray(FilePath[]::new);
+    return Stream.of(*manager.allActiveVcss)
+      .filter { vcs -> vcs.checkinEnvironment != null }
+      .flatMap { vcs -> Stream.of(*manager.getRootsUnderVcs(vcs)) }
+      .map { VcsUtil.getFilePath(it) }
+      .collect(toList())
+      .toTypedArray()
   }
 
-  @Override
-  protected boolean approximatelyHasRoots(@NotNull VcsContext dataContext) {
-    return true;
-  }
+  override fun approximatelyHasRoots(dataContext: VcsContext): Boolean = true
 
-  @Override
-  protected String getActionName(@NotNull VcsContext dataContext) {
-    return ActionsBundle.message("action.CheckinProject.text");
-  }
+  override fun getActionName(dataContext: VcsContext): String = ActionsBundle.message("action.CheckinProject.text")
 
-  @Override
-  protected String getMnemonicsFreeActionName(@NotNull VcsContext context) {
-    return VcsBundle.message("vcs.command.name.checkin.no.mnemonics");
-  }
+  override fun getMnemonicsFreeActionName(context: VcsContext): String = VcsBundle.message("vcs.command.name.checkin.no.mnemonics")
 }
