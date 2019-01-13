161076b423f3d99342cc6fe81bee41b95fb617a6
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2019-01-13 15:51:00 +0300

    vcs: Refactor "ChangeListWorker.getChangesUnder()" - return "Set" instead of "List"

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ChangeListWorker.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ChangeListWorker.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vcs.changes;
 
 import com.intellij.openapi.application.ReadAction;
@@ -43,6 +29,8 @@ import org.jetbrains.annotations.Nullable;
 import java.io.File;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
+
+import static java.util.stream.Collectors.toSet;
 
 /** should work under _external_ lock
  * just logic here: do modifications to group of change lists
@@ -398,17 +386,15 @@ public class ChangeListWorker {
   }
 
   @NotNull
-  public List<Change> getChangesUnder(@NotNull FilePath dirPath) {
-    List<Change> changes = new ArrayList<>();
-    for (Change change : myIdx.getChanges()) {
-      ContentRevision after = change.getAfterRevision();
-      ContentRevision before = change.getBeforeRevision();
-      if (after != null && after.getFile().isUnder(dirPath, false) ||
-          before != null && before.getFile().isUnder(dirPath, false)) {
-        changes.add(change);
-      }
-    }
-    return changes;
+  public Set<Change> getChangesUnder(@NotNull FilePath dirPath) {
+    return myIdx.getChanges().stream().filter(change -> isChangeUnder(dirPath, change)).collect(toSet());
+  }
+
+  private static boolean isChangeUnder(@NotNull FilePath parent, @NotNull Change change) {
+    ContentRevision after = change.getAfterRevision();
+    ContentRevision before = change.getBeforeRevision();
+    return after != null && after.getFile().isUnder(parent, false) ||
+           before != null && before.getFile().isUnder(parent, false);
   }
 
   @Nullable
