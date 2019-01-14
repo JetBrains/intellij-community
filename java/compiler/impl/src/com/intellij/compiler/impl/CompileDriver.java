e7983783ff8c6848c11333d4c451912731fc2e65
Eugene Zhuravlev <jeka@intellij.com>
2019-01-14 12:20:46 +0100

    compile session naming (IDEA-174920)

--- java/compiler/impl/src/com/intellij/compiler/impl/CompileDriver.java
+++ java/compiler/impl/src/com/intellij/compiler/impl/CompileDriver.java
@@ -372,10 +372,11 @@ public class CompileDriver {
                        final CompilerMessage message) {
     ApplicationManager.getApplication().assertIsDispatchThread();
 
-    final String contentName = CompilerBundle.message(forceCompile ? "compiler.content.name.compile" : "compiler.content.name.make");
     final boolean isUnitTestMode = ApplicationManager.getApplication().isUnitTestMode();
-    final CompilerTask compileTask = new CompilerTask(myProject, contentName, isUnitTestMode, !withModalProgress, true,
-                                                      isCompilationStartedAutomatically(scope), withModalProgress);
+    final String name = CompilerBundle.message(isRebuild ? "compiler.content.name.rebuild" : forceCompile ? "compiler.content.name.recompile" : "compiler.content.name.make");
+    final CompilerTask compileTask = new CompilerTask(
+      myProject, name, isUnitTestMode, !withModalProgress, true, isCompilationStartedAutomatically(scope), withModalProgress
+    );
 
     StatusBar.Info.set("", myProject, "Compiler");
     // ensure the project model seen by build process is up-to-date
