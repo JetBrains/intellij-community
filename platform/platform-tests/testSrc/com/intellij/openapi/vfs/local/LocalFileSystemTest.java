39f78ad74779bc7f3c90fddfd01a6bc814a65214
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-01-15 13:58:06 +0100

    [tests] correct assertion

--- platform/platform-tests/testSrc/com/intellij/openapi/vfs/local/LocalFileSystemTest.java
+++ platform/platform-tests/testSrc/com/intellij/openapi/vfs/local/LocalFileSystemTest.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vfs.local;
 
 import com.intellij.ide.GeneralSettings;
@@ -231,7 +231,7 @@ public class LocalFileSystemTest extends BareTestFixtureTestCase {
       String newName = "new_temp_file";
       VirtualFile copy = WriteAction.compute(() -> fileToCopy.copy(this, toVDir, newName));
       assertEquals(newName, copy.getName());
-      assertArrayEquals(copy.contentsToByteArray(), byteContent);
+      assertArrayEquals(byteContent, copy.contentsToByteArray());
     });
   }
 
