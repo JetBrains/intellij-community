4f064864ffc7b74ebba793940a3406107f909596
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-01-16 19:54:53 +0300

    [groovy] log error in case of unexpected elements passed into processor

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/resolve/processors/KindsResolverProcessor.kt
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/resolve/processors/KindsResolverProcessor.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.lang.resolve.processors
 
 import com.intellij.psi.PsiElement
@@ -45,7 +45,7 @@ open class KindsResolverProcessor(
     }
     else if (kind !in kinds) {
       if (state[sorryCannotKnowElementKind] != true) {
-        log.warn("Unneeded kind: $kind. ${elementInfo(element)}")
+        log.error("Unneeded kind: $kind. ${elementInfo(element)}")
       }
     }
     else if (kind !in candidates) {
