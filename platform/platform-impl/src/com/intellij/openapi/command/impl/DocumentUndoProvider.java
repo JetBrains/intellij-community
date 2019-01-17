60317ad9ae4980bd7ceb08bc5e2aed3633781417
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-01-17 21:47:53 +0100

    fix JspComplexHighlightingTest

--- platform/platform-impl/src/com/intellij/openapi/command/impl/DocumentUndoProvider.java
+++ platform/platform-impl/src/com/intellij/openapi/command/impl/DocumentUndoProvider.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.command.impl;
 
 import com.intellij.openapi.Disposable;
@@ -25,10 +25,9 @@ public class DocumentUndoProvider implements Disposable {
   private final Project myProject;
 
   DocumentUndoProvider(Project project) {
-    MyEditorDocumentListener documentListener = new MyEditorDocumentListener();
     myProject = project;
 
-    EditorFactory.getInstance().getEventMulticaster().addDocumentListener(documentListener, this);
+    EditorFactory.getInstance().getEventMulticaster().addDocumentListener(new MyEditorDocumentListener(), this);
   }
 
   @Override
