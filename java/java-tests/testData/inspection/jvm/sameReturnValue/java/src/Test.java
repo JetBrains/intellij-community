9d72f0473f49bc2937553bbd2e9bc316a8057377
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2019-01-17 13:48:18 +0300

    return value is the same const: do not process returns of containing methods IDEA-204977 (add test)

--- java/java-tests/testData/inspection/jvm/sameReturnValue/java/src/Test.java
+++ java/java-tests/testData/inspection/jvm/sameReturnValue/java/src/Test.java
@@ -4,7 +4,17 @@ class Test {
     return 0;
   }
 
+  void test() {
+    Comparator<String> cmp = new Comparator<String>() {
+      @Override
+      public int compare(String a, String b) {
+        return 0;
+      }
+    };
+  }
+
   public static void main(String[] args) {
     System.out.println(new Test().xxx());
+    new Test().test();
   }
 }
