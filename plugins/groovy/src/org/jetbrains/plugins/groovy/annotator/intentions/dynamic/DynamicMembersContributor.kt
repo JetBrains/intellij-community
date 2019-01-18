db47f8c87265faa0baabe97968e59be6f1116cc2
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-01-18 14:57:17 +0300

    [groovy] convert DynamicMembersContributor to .kt

--- plugins/groovy/src/org/jetbrains/plugins/groovy/annotator/intentions/dynamic/DynamicMembersContributor.kt
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/annotator/intentions/dynamic/DynamicMembersContributor.kt
@@ -1,48 +1,34 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.jetbrains.plugins.groovy.annotator.intentions.dynamic;
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.plugins.groovy.annotator.intentions.dynamic
 
-import com.intellij.psi.*;
-import com.intellij.psi.scope.PsiScopeProcessor;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.plugins.groovy.lang.psi.impl.statements.expressions.ClassUtil;
-import org.jetbrains.plugins.groovy.lang.resolve.NonCodeMembersContributor;
-import org.jetbrains.plugins.groovy.lang.resolve.ResolveUtil;
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiType
+import com.intellij.psi.ResolveState
+import com.intellij.psi.scope.PsiScopeProcessor
+import org.jetbrains.plugins.groovy.lang.psi.impl.statements.expressions.ClassUtil
+import org.jetbrains.plugins.groovy.lang.resolve.NonCodeMembersContributor
+import org.jetbrains.plugins.groovy.lang.resolve.ResolveUtil
 
 /**
  * @author peter
  */
-public class DynamicMembersContributor extends NonCodeMembersContributor {
-  @Override
-  public void processDynamicElements(@NotNull PsiType qualifierType,
-                                     PsiClass aClass,
-                                     @NotNull PsiScopeProcessor processor,
-                                     @NotNull PsiElement place,
-                                     @NotNull ResolveState state) {
-    if (aClass == null) return;
+class DynamicMembersContributor : NonCodeMembersContributor() {
 
-    final DynamicManager manager = DynamicManager.getInstance(aClass.getProject());
-
-    for (String qName : ClassUtil.getSuperClassesWithCache(aClass).keySet()) {
-      for (PsiMethod method : manager.getMethods(qName)) {
-        if (!ResolveUtil.processElement(processor, method, state)) return;
+  override fun processDynamicElements(qualifierType: PsiType,
+                                      aClass: PsiClass?,
+                                      processor: PsiScopeProcessor,
+                                      place: PsiElement,
+                                      state: ResolveState) {
+    if (aClass == null) return
+    val manager = DynamicManager.getInstance(aClass.project)
+    for (qName in ClassUtil.getSuperClassesWithCache(aClass).keys) {
+      for (method in manager.getMethods(qName)) {
+        if (!ResolveUtil.processElement(processor, method, state)) return
       }
 
-      for (PsiVariable var : manager.getProperties(qName)) {
-        if (!ResolveUtil.processElement(processor, var, state)) return;
+      for (property in manager.getProperties(qName)) {
+        if (!ResolveUtil.processElement(processor, property, state)) return
       }
     }
   }
