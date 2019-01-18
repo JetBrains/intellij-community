89e046bd0296ffd8a80b315953233ba25f7e955b
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-01-18 15:06:37 +0300

    [groovy] dynamic members: ask processor whether it wants methods or properties (EA-132536)

--- plugins/groovy/src/org/jetbrains/plugins/groovy/annotator/intentions/dynamic/DynamicMembersContributor.kt
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/annotator/intentions/dynamic/DynamicMembersContributor.kt
@@ -9,6 +9,8 @@ import com.intellij.psi.scope.PsiScopeProcessor
 import org.jetbrains.plugins.groovy.lang.psi.impl.statements.expressions.ClassUtil
 import org.jetbrains.plugins.groovy.lang.resolve.NonCodeMembersContributor
 import org.jetbrains.plugins.groovy.lang.resolve.ResolveUtil
+import org.jetbrains.plugins.groovy.lang.resolve.shouldProcessMethods
+import org.jetbrains.plugins.groovy.lang.resolve.shouldProcessProperties
 
 /**
  * @author peter
@@ -21,14 +23,20 @@ class DynamicMembersContributor : NonCodeMembersContributor() {
                                       place: PsiElement,
                                       state: ResolveState) {
     if (aClass == null) return
+    val shouldProcessMethods = processor.shouldProcessMethods()
+    val shouldProcessProperties = processor.shouldProcessProperties()
+    if (!shouldProcessMethods && !shouldProcessProperties) return
     val manager = DynamicManager.getInstance(aClass.project)
     for (qName in ClassUtil.getSuperClassesWithCache(aClass).keys) {
-      for (method in manager.getMethods(qName)) {
-        if (!ResolveUtil.processElement(processor, method, state)) return
+      if (shouldProcessMethods) {
+        for (method in manager.getMethods(qName)) {
+          if (!ResolveUtil.processElement(processor, method, state)) return
+        }
       }
-
-      for (property in manager.getProperties(qName)) {
-        if (!ResolveUtil.processElement(processor, property, state)) return
+      if (shouldProcessProperties) {
+        for (property in manager.getProperties(qName)) {
+          if (!ResolveUtil.processElement(processor, property, state)) return
+        }
       }
     }
   }
--- plugins/groovy/test/org/jetbrains/plugins/groovy/util/BaseTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/util/BaseTest.java
@@ -1,6 +1,7 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.util;
 
+import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.testFramework.fixtures.CodeInsightTestFixture;
 import org.jetbrains.annotations.NotNull;
@@ -18,6 +19,11 @@ public interface BaseTest {
 
   @NotNull
   CodeInsightTestFixture getFixture();
+
+  @NotNull
+  default Project getProject() {
+    return getFixture().getProject();
+  }
 
   @NotNull
   default GroovyFile getGroovyFile() {
