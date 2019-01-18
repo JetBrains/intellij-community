b80d42615537258d2141e608aabde6834f1c68c9
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2019-01-18 12:21:53 +0300

    ^^ add time measurement

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/shelf/ShelveChangesManager.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/shelf/ShelveChangesManager.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vcs.changes.shelf;
 
+import com.google.common.base.Stopwatch;
 import com.intellij.concurrency.JobScheduler;
 import com.intellij.configurationStore.XmlSerializer;
 import com.intellij.openapi.Disposable;
@@ -418,6 +419,7 @@ public class ShelveChangesManager implements PersistentStateComponent<Element>, 
                                          final boolean rollback,
                                          boolean markToBeDeleted,
                                          boolean honorExcludedFromCommit) throws IOException, VcsException {
+    Stopwatch stopwatch = Stopwatch.createStarted();
     final ProgressIndicator progressIndicator = ProgressManager.getInstance().getProgressIndicator();
     if (progressIndicator != null) {
       progressIndicator.setText(VcsBundle.message("shelve.changes.progress.title"));
@@ -464,7 +466,7 @@ public class ShelveChangesManager implements PersistentStateComponent<Element>, 
     finally {
       notifyStateChanged();
     }
-
+    LOG.debug(String.format("shelving %d %s took %s", changes.size(), StringUtil.pluralize("file", changes.size()), stopwatch));
     return changeList;
   }
 
