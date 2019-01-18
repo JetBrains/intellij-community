d5074c759c233970955f9c556040027d6d0b52c1
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2019-01-18 22:17:48 +0300

    deprecation inspection fix for field references

--- java/java-analysis-impl/src/com/intellij/codeInspection/deprecation/deprecationInspectionQuickFixes.kt
+++ java/java-analysis-impl/src/com/intellij/codeInspection/deprecation/deprecationInspectionQuickFixes.kt
@@ -16,6 +16,7 @@
 package com.intellij.codeInspection.deprecation
 
 import com.intellij.codeInspection.LocalQuickFixOnPsiElement
+import com.intellij.lang.jvm.JvmModifier
 import com.intellij.openapi.project.Project
 import com.intellij.psi.*
 import com.intellij.psi.codeStyle.JavaCodeStyleManager
@@ -23,6 +24,21 @@ import com.intellij.psi.util.PsiFormatUtil
 import com.intellij.psi.util.PsiFormatUtilBase
 import com.intellij.util.ObjectUtils
 import org.jetbrains.annotations.Nls
+
+private fun generateQualifierText(expr: PsiReferenceExpression,
+                                  replacementMethod: PsiMember): String {
+  val qualifierExpression = expr.qualifierExpression
+  val isReplacementStatic = replacementMethod.hasModifierProperty(PsiModifier.STATIC)
+  return if (qualifierExpression != null && !isReplacementStatic) {
+    qualifierExpression.text + "."
+  }
+  else if (isReplacementStatic) {
+    replacementMethod.containingClass!!.qualifiedName!! + "."
+  }
+  else {
+    ""
+  }
+}
 
 internal class ReplaceMethodCallFix(expr: PsiMethodCallExpression, replacementMethod: PsiMethod) : LocalQuickFixOnPsiElement(expr) {
   private val myReplacementMethodPointer =
@@ -43,21 +59,37 @@ internal class ReplaceMethodCallFix(expr: PsiMethodCallExpression, replacementMe
     val expr = ObjectUtils.tryCast(startElement, PsiMethodCallExpression::class.java) ?: return
     val replacementMethod = myReplacementMethodPointer.element ?: return
 
-    val qualifierExpression = expr.methodExpression.qualifierExpression
-    val isReplacementStatic = replacementMethod.hasModifierProperty(PsiModifier.STATIC)
-    val qualifierText = if (qualifierExpression != null && !isReplacementStatic) {
-      qualifierExpression.text + "."
-    }
-    else if (isReplacementStatic) {
-      replacementMethod.containingClass!!.qualifiedName!! + "."
-    }
-    else {
-      ""
-    }
+    val qualifierText = generateQualifierText(expr.methodExpression, replacementMethod)
 
     val elementFactory = JavaPsiFacade.getElementFactory(project)
     val newMethodCall = elementFactory.createExpressionFromText(qualifierText + replacementMethod.name + expr.argumentList.text, expr)
     val replaced = expr.replace(newMethodCall) as PsiMethodCallExpression
     JavaCodeStyleManager.getInstance(project).shortenClassReferences(replaced.methodExpression)
   }
+}
+
+internal class ReplaceFieldReferenceFix(expr: PsiReferenceExpression, replacementField: PsiField) : LocalQuickFixOnPsiElement(expr) {
+  private val myReplacementMethodPointer =
+    SmartPointerManager.getInstance(replacementField.project).createSmartPsiElementPointer(replacementField)
+  private val myReplacementText =
+    PsiFormatUtil.formatVariable(replacementField, PsiFormatUtilBase.SHOW_CONTAINING_CLASS or PsiFormatUtilBase.SHOW_NAME, PsiSubstitutor.EMPTY)
+
+  override fun getFamilyName(): String {
+    return "Replace Field Reference"
+  }
+
+  override fun getText(): String {
+    return "Replace field reference with $myReplacementText"
+  }
+
+  override fun invoke(project: Project, file: PsiFile, startElement: PsiElement, endElement: PsiElement) {
+    val expr = ObjectUtils.tryCast(startElement, PsiReferenceExpression::class.java) ?: return
+    val replacementField = myReplacementMethodPointer.element ?: return
+
+    val qualifierText = generateQualifierText(expr, replacementField)
+
+    val replaced = expr.replace(JavaPsiFacade.getElementFactory(project).createExpressionFromText(qualifierText + replacementField.name, expr))
+    JavaCodeStyleManager.getInstance(project).shortenClassReferences(replaced)
+  }
+
 }
