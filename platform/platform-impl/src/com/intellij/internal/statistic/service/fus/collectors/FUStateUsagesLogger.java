2214e570cc3fd718944f85e848810d31041b3094
Svetlana.Zemlyanskaya <svetlana.zemlyanskaya@jetbrains.com>
2019-01-19 14:14:44 +0100

    FUS: record a system event when collector is invoked

--- platform/platform-impl/src/com/intellij/internal/statistic/service/fus/collectors/FUStateUsagesLogger.java
+++ platform/platform-impl/src/com/intellij/internal/statistic/service/fus/collectors/FUStateUsagesLogger.java
@@ -7,7 +7,6 @@ import com.intellij.internal.statistic.eventLog.FeatureUsageGroup;
 import com.intellij.internal.statistic.eventLog.FeatureUsageLogger;
 import com.intellij.internal.statistic.utils.StatisticsUtilKt;
 import com.intellij.openapi.application.ex.ApplicationManagerEx;
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -19,6 +18,11 @@ import java.util.stream.Collectors;
 import static com.intellij.internal.statistic.utils.StatisticsUploadAssistant.LOCK;
 
 public class FUStateUsagesLogger implements UsagesCollectorConsumer {
+  /**
+   * System event which indicates that the collector was called, can be used to calculate the base line
+   */
+  private static final String INVOKED = "invoked";
+
   public static FUStateUsagesLogger create() { return new FUStateUsagesLogger(); }
 
   public void logProjectStates(@NotNull Project project) {
@@ -57,14 +61,16 @@ public class FUStateUsagesLogger implements UsagesCollectorConsumer {
                                              @Nullable FUSUsageContext context,
                                              @NotNull Set<UsageDescriptor> usages) {
 
+    final FeatureUsageLogger logger = FeatureUsageLogger.INSTANCE;
+    final FeatureUsageGroup group = new FeatureUsageGroup(groupId, 1);
     usages = usages.stream().filter(descriptor -> descriptor.getValue() > 0).collect(Collectors.toSet());
     if (!usages.isEmpty()) {
-      final FeatureUsageLogger logger = FeatureUsageLogger.INSTANCE;
       final Map<String, ?> groupData = StatisticsUtilKt.createData(project, context);
       for (UsageDescriptor usage : usages) {
         final Map<String, Object> eventData = StatisticsUtilKt.mergeWithEventData(groupData, usage.getContext(), usage.getValue());
-        logger.logState(new FeatureUsageGroup(groupId, 1), usage.getKey(), eventData); // todo !!!
+        logger.logState(group, usage.getKey(), eventData); // todo !!!
       }
     }
+    logger.logState(group, INVOKED);
   }
 }
