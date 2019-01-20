0e9aa6ac1f71711ecb7422e32ae9ddd349bcc42c
Konstantin Bulenkov <kb@jetbrains.com>
2019-01-20 20:03:44 +0100

    make getPanelBackground() return variable color

--- platform/util/src/com/intellij/util/ui/UIUtil.java
+++ platform/util/src/com/intellij/util/ui/UIUtil.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.ui;
 
 import com.intellij.BundleBase;
@@ -1259,7 +1259,7 @@ public class UIUtil {
   }
 
   public static Color getPanelBackground() {
-    return UIManager.getColor("Panel.background");
+    return JBColor.PanelBackground;
   }
 
   public static Color getEditorPaneBackground() {
