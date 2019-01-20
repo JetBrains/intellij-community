6ba953a9a35fb4541af9fe2531bed285e5d9348e
Konstantin Bulenkov <kb@jetbrains.com>
2019-01-20 20:02:23 +0100

    add named color constant for panel background

--- platform/util/src/com/intellij/ui/JBColor.java
+++ platform/util/src/com/intellij/ui/JBColor.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ui;
 
 import com.intellij.util.NotNullProducer;
@@ -22,6 +22,8 @@ import static com.intellij.util.ObjectUtils.notNull;
  */
 @SuppressWarnings("UseJBColor")
 public class JBColor extends Color {
+  public final static Color PanelBackground = namedColor("Panel.background", 0xffffff);
+
   private static class Lazy {
     private static volatile boolean DARK = UIUtil.isUnderDarcula();
   }
