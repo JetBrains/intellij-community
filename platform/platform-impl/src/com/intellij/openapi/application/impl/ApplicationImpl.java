d06c4d3cf0758d318a750c9ab8f3efb54e68f7cf
peter <peter@jetbrains.com>
2019-01-21 18:22:08 +0100

    add a comment about executeOnPooledThread/PooledThreadExecutor difference

--- platform/platform-impl/src/com/intellij/openapi/application/impl/ApplicationImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/application/impl/ApplicationImpl.java
@@ -308,6 +308,7 @@ public class ApplicationImpl extends PlatformComponentManagerImpl implements App
 
       @Override
       public void run() {
+        // see the comment in "executeOnPooledThread(Callable)"
         try (AccessToken ignored = myLock.applyReadPrivilege(suspensionId)) {
           action.run();
         }
@@ -331,6 +332,11 @@ public class ApplicationImpl extends PlatformComponentManagerImpl implements App
     return ourThreadExecutorsService.submit(new Callable<T>() {
       @Override
       public T call() {
+        // This is very special magic only needed by threads that need read actions and can be executed
+        // during "executeSuspendingWriteAction" (e.g. dumb mode, indexing). Threads created via "executeOnPooledThread"
+        // in these circumstances may run read actions immediately, instead of waiting until the write action is resumed and finished.
+
+        // For everyone else, "executeOnPooledThread" should be equivalent to "AppExecutorUtil" AKA "PooledThreadExecutor" pool
         try (AccessToken ignored = myLock.applyReadPrivilege(suspensionId)) {
           return action.call();
         }
