f89b477a6887fb07587adf92882465b329c643a2
Nikolay Korolev <Nikolai.Korolev@jetbrains.com>
2019-01-21 14:04:35 +0300

    IDEA-CR-41371 RUBY-22991 hintsInRootElementArea checks if a root element is a document
    (cherry picked from commit 3e6562272e7f92d528ddbfadbd8caf15eb637682)

--- platform/lang-impl/src/com/intellij/codeInsight/hints/ParameterHintsPass.java
+++ platform/lang-impl/src/com/intellij/codeInsight/hints/ParameterHintsPass.java
@@ -150,14 +150,21 @@ public class ParameterHintsPass extends EditorBoundHighlightingPass {
     int elementStart = range.getStartOffset();
     int elementEnd = range.getEndOffset();
 
-    return manager.getParameterHintsInRange(myEditor, elementStart + 1, elementEnd - 1);
+    // Adding hints on the borders is allowed only in case root element is a document
+    // See: canShowHintsAtOffset
+    if (myDocument.getTextLength() != range.getLength()) {
+      ++elementStart;
+      --elementEnd;
+    }
+
+    return manager.getParameterHintsInRange(myEditor, elementStart, elementEnd);
   }
 
   /**
    * Adding hints on the borders of root element (at startOffset or endOffset)
    * is allowed only in the case when root element is a document
    *
-   * @return true iff a given offset can be used for hint rendering
+   * @return true if a given offset can be used for hint rendering
    */
   private boolean canShowHintsAtOffset(int offset) {
     TextRange rootRange = myRootElement.getTextRange();
