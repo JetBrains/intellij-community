9bbe47b443399605fbb9927c5aab2548b2785a7d
Dmitriy.Panov <Dmitriy.Panov@jetbrains.com>
2019-01-21 19:00:30 +0300

    IJI-9: dev<=IntelliJIcons sync: local run configurations are back, fixup

--- platform/build-scripts/icons/src/org/jetbrains/intellij/build/images/sync/ideaSyncFromIconsRepo.kt
+++ platform/build-scripts/icons/src/org/jetbrains/intellij/build/images/sync/ideaSyncFromIconsRepo.kt
@@ -11,6 +11,7 @@ import java.io.File
 
 fun main(args: Array<String>) {
   echo("Syncing icons..")
+  if (args.isNotEmpty()) System.setProperty(Context.iconsCommitHashesToSyncArg, args.joinToString())
   checkIcons()
   echo("Generating classes..")
   generateIconsClasses()
