18b296a2c059069e95b69c31b55d5ecaac567b34
Aleksey Pivovarov <AMPivovarov@gmail.com>
2019-01-23 01:21:03 +0300

    vcs: modify project mappings in bulk

--- platform/vcs-api/src/com/intellij/openapi/vcs/ProjectLevelVcsManager.java
+++ platform/vcs-api/src/com/intellij/openapi/vcs/ProjectLevelVcsManager.java
@@ -245,6 +245,7 @@ public abstract class ProjectLevelVcsManager {
   /**
    * This method can be used only when initially loading the project configuration!
    */
+  @Deprecated
   public abstract void setDirectoryMapping(final String path, final String activeVcsName);
 
   public abstract void setDirectoryMappings(final List<VcsDirectoryMapping> items);
