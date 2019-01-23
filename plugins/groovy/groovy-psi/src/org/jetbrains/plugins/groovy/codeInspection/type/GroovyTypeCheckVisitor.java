72c2124d7ed4e2a8f394d6a12d0f413bfed107de
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-01-23 18:14:18 +0300

    [groovy] spock: get rid of spock logic in type check highlighting (IDEA-205861)

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/codeInspection/type/GroovyTypeCheckVisitor.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/codeInspection/type/GroovyTypeCheckVisitor.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.codeInspection.type;
 
 import com.intellij.codeInspection.LocalQuickFix;
@@ -421,8 +421,6 @@ public class GroovyTypeCheckVisitor extends BaseInspectionVisitor {
 
   private void checkOperator(@NotNull CallInfo<? extends GrBinaryExpression> info) {
     if (hasErrorElements(info.getCall())) return;
-
-    if (isSpockTimesOperator(info.getCall())) return;
 
     GroovyResolveResult[] results = info.multiResolve();
     GroovyResolveResult resolveResult = info.advancedResolve();
