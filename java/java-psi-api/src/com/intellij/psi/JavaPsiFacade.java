a136c8d291007b8e62ed64cf85a0ef686ae78915
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-01-23 13:30:44 +0100

    Cleanup (warnings)

--- java/java-psi-api/src/com/intellij/psi/JavaPsiFacade.java
+++ java/java-psi-api/src/com/intellij/psi/JavaPsiFacade.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2012 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi;
 
 import com.intellij.openapi.components.ServiceManager;
@@ -28,7 +14,7 @@ import org.jetbrains.annotations.Nullable;
  */
 public abstract class JavaPsiFacade {
   private static final NotNullLazyKey<JavaPsiFacade, Project> INSTANCE_KEY = ServiceManager.createLazyKey(JavaPsiFacade.class);
-  
+
   public static JavaPsiFacade getInstance(Project project) {
     return INSTANCE_KEY.getValue(project);
   }
@@ -101,7 +87,7 @@ public abstract class JavaPsiFacade {
    * Returns the name helper for the project, which can be used to validate
    * and parse Java identifiers.
    *
-   * @deprecated use {@link com.intellij.psi.PsiNameHelper#getInstance(com.intellij.openapi.project.Project)}
+   * @deprecated use {@link PsiNameHelper#getInstance(Project)}
    * @return the name helper instance.
    */
   @Deprecated
