c561afe4f0c7f7b1a82089b567b48999a1b9ac9f
Liana Bakradze <liana.bakradze@jetbrains.com>
2019-01-24 19:29:43 +0300

    pre-register notification group for Run ToolWindow

--- platform/testRunner/src/com/intellij/execution/testframework/TestsUIUtil.java
+++ platform/testRunner/src/com/intellij/execution/testframework/TestsUIUtil.java
@@ -28,6 +28,7 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.MessageType;
 import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.wm.AppIconScheme;
+import com.intellij.openapi.wm.ToolWindowId;
 import com.intellij.openapi.wm.ToolWindowManager;
 import com.intellij.pom.Navigatable;
 import com.intellij.psi.PsiElement;
@@ -47,6 +48,11 @@ public class TestsUIUtil {
 
   public static final Color PASSED_COLOR = new Color(0, 128, 0);
   private static final String TESTS = "tests";
+
+  static {
+    //pre-register notification group for Run ToolWindow to show it in notifications settings
+    NotificationGroup.toolWindowGroup(getTestResultsNotificationDisplayId(ToolWindowId.RUN), ToolWindowId.RUN);
+  }
 
   private TestsUIUtil() {
   }
@@ -145,7 +151,7 @@ public class TestsUIUtil {
     final MessageType type = testResultPresentation.getType();
 
     if (!Comparing.strEqual(toolWindowManager.getActiveToolWindowId(), windowId)) {
-      String displayId = "Test Results: " + windowId;
+      String displayId = getTestResultsNotificationDisplayId(windowId);
       NotificationGroup group = NotificationGroup.findRegisteredGroup(displayId);
       if (group == null) {
         group = NotificationGroup.toolWindowGroup(displayId, windowId);
@@ -155,6 +161,10 @@ public class TestsUIUtil {
 
     NOTIFICATION_GROUP.createNotification(balloonText, type).notify(project);
     SystemNotifications.getInstance().notify("TestRunner", title, text);
+  }
+
+  private static String getTestResultsNotificationDisplayId(@NotNull String toolWindowId) {
+    return "Test Results: " + toolWindowId;
   }
 
   public static String getTestSummary(AbstractTestProxy proxy) {
