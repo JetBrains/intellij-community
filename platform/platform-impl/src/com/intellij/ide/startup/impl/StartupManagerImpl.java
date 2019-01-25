438b59adf34cebaa3b0689eb7a31320eb0b8743d
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-01-25 17:47:19 +0100

    [platform] project directory FS stats collector (IDEA-135546)

--- platform/platform-impl/src/com/intellij/ide/startup/impl/StartupManagerImpl.java
+++ platform/platform-impl/src/com/intellij/ide/startup/impl/StartupManagerImpl.java
@@ -1,9 +1,10 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.startup.impl;
 
 import com.intellij.diagnostic.PerformanceWatcher;
 import com.intellij.ide.startup.ServiceNotReadyException;
 import com.intellij.ide.startup.StartupManagerEx;
+import com.intellij.internal.statistic.collectors.fus.project.FsCaseSensitivityTriggerCollector;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationListener;
 import com.intellij.notification.NotificationType;
@@ -269,6 +270,8 @@ public class StartupManagerImpl extends StartupManagerEx {
           new Notification(Notifications.SYSTEM_MESSAGES_GROUP_ID, title, text, NotificationType.WARNING, NotificationListener.URL_OPENING_LISTENER),
           myProject);
       }
+
+      FsCaseSensitivityTriggerCollector.trigger(myProject, actual);
     }
     catch (FileNotFoundException e) {
       LOG.warn(e);
