6b7fa3d97e733935d961e6ac8b2ad646411deeae
Kirill Timofeev <Kirill.Timofeev@jetbrains.com>
2019-01-25 14:29:04 +0300

    [profiler] add missing FlameGraph.* keys to NAMED_COLORS

--- plugins/devkit/devkit-core/src/completion/UiDefaultsHardcodedKeys.java
+++ plugins/devkit/devkit-core/src/completion/UiDefaultsHardcodedKeys.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.devkit.completion;
 
 import com.intellij.util.containers.ContainerUtil;
@@ -1317,15 +1317,21 @@ public final class UiDefaultsHardcodedKeys {
     "FlameGraph.JVMFocusBackground",
     "FlameGraph.JVMFocusSearchNotMatchedBackground",
     "FlameGraph.JVMSearchNotMatchedBackground",
+    "FlameGraph.JVMFrameForeground",
+    "FlameGraph.JVMFocusedFrameForeground",
     "FlameGraph.frameForeground",
     "FlameGraph.nativeBackground",
     "FlameGraph.nativeFocusBackground",
     "FlameGraph.nativeFocusSearchNotMatchedBackground",
     "FlameGraph.nativeSearchNotMatchedBackground",
+    "FlameGraph.nativeFrameForeground",
+    "FlameGraph.nativeFocusedFrameForeground",
     "FlameGraph.parentBackground",
     "FlameGraph.parentFocusBackground",
     "FlameGraph.parentFocusSearchNotMatchedBackground",
     "FlameGraph.parentSearchNotMatchedBackground",
+    "FlameGraph.parentFrameForeground",
+    "FlameGraph.parentFocusedFrameForeground",
     "Git.Log.Ref.Head",
     "Git.Log.Ref.LocalBranch",
     "Git.Log.Ref.Other",
