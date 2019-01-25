6c211c2e31df8a1b36bf960973d0f07fedb84037
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-01-25 14:38:16 +0300

    [groovy] don't use bundled groovy for tests

--- plugins/groovy/test/org/jetbrains/plugins/groovy/GroovyProjectDescriptors.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/GroovyProjectDescriptors.java
@@ -1,6 +1,9 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy;
 
+import com.intellij.openapi.projectRoots.JavaSdk;
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.testFramework.IdeaTestUtil;
 import com.intellij.testFramework.LightProjectDescriptor;
 
 public interface GroovyProjectDescriptors {
@@ -8,7 +11,7 @@ public interface GroovyProjectDescriptors {
   TestLibrary LIB_GROOVY_2_1 = new RepositoryTestLibrary("org.codehaus.groovy:groovy-all:2.1.0");
   TestLibrary LIB_GROOVY_2_2 = new RepositoryTestLibrary("org.codehaus.groovy:groovy:2.2.0");
   TestLibrary LIB_GROOVY_2_3 = new RepositoryTestLibrary("org.codehaus.groovy:groovy:2.3.0");
-  TestLibrary LIB_GROOVY_2_4 = new RepositoryTestLibrary("org.codehaus.groovy:groovy:2.4.16");
+  TestLibrary LIB_GROOVY_2_4 = new RepositoryTestLibrary("org.codehaus.groovy:groovy-all:2.4.16");
   TestLibrary LIB_GROOVY_2_5 = new RepositoryTestLibrary("org.codehaus.groovy:groovy:2.5.5");
   TestLibrary LIB_GROOVY_3_0 = new RepositoryTestLibrary("org.codehaus.groovy:groovy:3.0.0-alpha-2");
 
@@ -17,4 +20,13 @@ public interface GroovyProjectDescriptors {
   LightProjectDescriptor GROOVY_2_3 = new LibraryLightProjectDescriptor(LIB_GROOVY_2_3);
   LightProjectDescriptor GROOVY_2_5 = new LibraryLightProjectDescriptor(LIB_GROOVY_2_5);
   LightProjectDescriptor GROOVY_3_0 = new LibraryLightProjectDescriptor(LIB_GROOVY_3_0);
+
+  TestLibrary LIB_GROOVY_LATEST = LIB_GROOVY_2_4;
+  LightProjectDescriptor GROOVY_LATEST = new LibraryLightProjectDescriptor(LIB_GROOVY_LATEST);
+  LightProjectDescriptor GROOVY_LATEST_REAL_JDK = new LibraryLightProjectDescriptor(LIB_GROOVY_LATEST) {
+    @Override
+    public Sdk getSdk() {
+      return JavaSdk.getInstance().createJdk("TEST_JDK", IdeaTestUtil.requireRealJdkHome(), false);
+    }
+  };
 }
