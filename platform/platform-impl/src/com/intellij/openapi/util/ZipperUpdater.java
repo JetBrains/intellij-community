00c30cf1c3139a217dec73fa220a11490c540655
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2019-01-26 18:25:48 +0300

    vcs: mark files as vcs-dirty under a dispose aware indicator
    Otherwise already disposed and similar exceptions may appear: IDEA-205978

--- platform/platform-impl/src/com/intellij/openapi/util/ZipperUpdater.java
+++ platform/platform-impl/src/com/intellij/openapi/util/ZipperUpdater.java
@@ -1,10 +1,11 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.util;
 
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.progress.SomeQueue;
+import com.intellij.openapi.progress.util.BackgroundTaskUtil;
 import com.intellij.util.Alarm;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.TestOnly;
@@ -58,7 +59,7 @@ public class ZipperUpdater {
               if (!myRaised) return;
               myRaised = false;
             }
-            runnable.run();
+            BackgroundTaskUtil.runUnderDisposeAwareIndicator(myAlarm, runnable);
             synchronized (myLock) {
               myIsEmpty = !myRaised;
             }
