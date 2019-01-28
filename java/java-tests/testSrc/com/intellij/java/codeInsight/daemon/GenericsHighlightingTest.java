582bee8427d14b3d356c9fd8b93bd8f0154b22d1
Anna.Kozlova <anna.kozlova@jetbrains.com>
2019-01-28 18:25:32 +0100

    type distinction when assignment to parameter bound doesn't work (IDEA-205883)

--- java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/GenericsHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/GenericsHighlightingTest.java
@@ -416,6 +416,7 @@ public class GenericsHighlightingTest extends LightDaemonAnalyzerTestCase {
   public void testReturnTypeSubstitutableForSameOverrideEquivalentMethods() { doTest7(false); }
   public void testCaptureConversionWithWildcardBounds() { doTest7(false); }
   public void testIDEA106811() { doTest7(false); }
+  public void testTypeParameterDistinction() { doTest7(false); }
   public void testRawTypeCheckForNestedClassWithOuterClassTypeParameters() { doTest7(false); }
   public void testArrayContainsInTypeParameterWithSerializableBound() { doTest7(true); }
   public void testIntersectTypeParameterBounds() { doTest7(false); }
