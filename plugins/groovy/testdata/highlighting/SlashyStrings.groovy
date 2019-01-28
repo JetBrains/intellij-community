1947fef1ab7b5cf81b2eeb1a5d68d8428ad38da2
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-01-28 12:52:50 +0300

    [groovy] fix test data

--- plugins/groovy/testdata/highlighting/SlashyStrings.groovy
+++ plugins/groovy/testdata/highlighting/SlashyStrings.groovy
@@ -1,8 +1,8 @@
 print (/abc/)
-print (<error descr="Multi-line slashy strings are not supported in Groovy 1.6.9-sources">/ab
+print (<error descr="Multi-line slashy strings are not supported in Groovy 1.6.9">/ab
 c/</error>)
 
 print (/abc${false}def/)
 
-print (<error descr="Multi-line slashy strings are not supported in Groovy 1.6.9-sources">/abc${false}d
+print (<error descr="Multi-line slashy strings are not supported in Groovy 1.6.9">/abc${false}d
 ef/</error>)
