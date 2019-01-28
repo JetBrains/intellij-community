b58c037462c06ed8c1d1c47d1d9da8f9014b8171
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2019-01-28 23:52:16 +0300

    terminal: show default path for empty fields (shell path and starting directory) with fix

--- plugins/terminal/src/org/jetbrains/plugins/terminal/TerminalSettingsPanel.java
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/TerminalSettingsPanel.java
@@ -3,7 +3,7 @@ package org.jetbrains.plugins.terminal;
 
 import com.google.common.collect.Lists;
 import com.intellij.execution.configuration.EnvironmentVariablesTextFieldWithBrowseButton;
-import com.intellij.openapi.fileChooser.FileChooserDescriptor;
+import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.options.UnnamedConfigurable;
 import com.intellij.openapi.ui.TextComponentAccessor;
@@ -13,6 +13,7 @@ import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.ui.DocumentAdapter;
 import com.intellij.ui.IdeBorderFactory;
 import com.intellij.ui.components.JBCheckBox;
+import com.intellij.ui.components.JBTextField;
 import com.intellij.uiDesigner.core.GridConstraints;
 import com.intellij.uiDesigner.core.GridLayoutManager;
 import com.intellij.util.containers.ContainerUtil;
@@ -23,6 +24,7 @@ import javax.swing.event.DocumentEvent;
 import java.awt.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Supplier;
 
 /**
  * @author traff
@@ -59,42 +61,8 @@ public class TerminalSettingsPanel {
     myProjectSettingsPanel.setBorder(IdeBorderFactory.createTitledBorder("Project settings"));
     myGlobalSettingsPanel.setBorder(IdeBorderFactory.createTitledBorder("Application settings"));
 
-    FileChooserDescriptor fileChooserDescriptor = new FileChooserDescriptor(true, false, false, false, false, false);
-
-    myShellPathField.addBrowseFolderListener(
-      "",
-      "Shell executable path",
-      null,
-      fileChooserDescriptor,
-      TextComponentAccessor.TEXT_FIELD_WHOLE_TEXT);
-
-    fileChooserDescriptor = new FileChooserDescriptor(false, true, false, false, false, false);
-
-    myStartDirectoryField.addBrowseFolderListener(
-      "",
-      "Starting directory",
-      null,
-      fileChooserDescriptor,
-      TextComponentAccessor.TEXT_FIELD_WHOLE_TEXT);
-
-    myShellPathField.getTextField().getDocument().addDocumentListener(new DocumentAdapter() {
-      @Override
-      protected void textChanged(@NotNull DocumentEvent e) {
-        myShellPathField
-          .getTextField().setForeground(StringUtil.equals(myShellPathField.getText(), myProjectOptionsProvider.getDefaultShellPath()) ?
-                                        getDefaultValueColor() : getChangedValueColor());
-      }
-    });
-
-    myStartDirectoryField.getTextField().getDocument().addDocumentListener(new DocumentAdapter() {
-      @Override
-      protected void textChanged(@NotNull DocumentEvent e) {
-        myStartDirectoryField
-          .getTextField()
-          .setForeground(StringUtil.equals(myStartDirectoryField.getText(), myProjectOptionsProvider.getDefaultStartingDirectory()) ?
-                         getDefaultValueColor() : getChangedValueColor());
-      }
-    });
+    configureShellPathField();
+    configureStartDirectoryField();
 
     List<Component> customComponents = ContainerUtil.newArrayList();
     for (LocalTerminalCustomizer c : LocalTerminalCustomizer.EP_NAME.getExtensions()) {
@@ -122,6 +90,42 @@ public class TerminalSettingsPanel {
     }
 
     return myWholePanel;
+  }
+
+  private void configureStartDirectoryField() {
+    myStartDirectoryField.addBrowseFolderListener(
+      "",
+      "Starting directory",
+      null,
+      FileChooserDescriptorFactory.createSingleFolderDescriptor(),
+      TextComponentAccessor.TEXT_FIELD_WHOLE_TEXT
+    );
+    setupTextFieldDefaultValue(myStartDirectoryField.getTextField(), () -> myProjectOptionsProvider.getDefaultStartingDirectory());
+  }
+
+  private void configureShellPathField() {
+    myShellPathField.addBrowseFolderListener(
+      "",
+      "Shell executable path",
+      null,
+      FileChooserDescriptorFactory.createSingleFileNoJarsDescriptor(),
+      TextComponentAccessor.TEXT_FIELD_WHOLE_TEXT
+    );
+    setupTextFieldDefaultValue(myShellPathField.getTextField(), () -> myOptionsProvider.defaultShellPath());
+  }
+
+  private void setupTextFieldDefaultValue(@NotNull JTextField textField, @NotNull Supplier<String> defaultValueSupplier) {
+    String defaultShellPath = defaultValueSupplier.get();
+    if (StringUtil.isEmptyOrSpaces(defaultShellPath)) return;
+    textField.getDocument().addDocumentListener(new DocumentAdapter() {
+      @Override
+      protected void textChanged(@NotNull DocumentEvent e) {
+        textField.setForeground(defaultShellPath.equals(textField.getText()) ? getDefaultValueColor() : getChangedValueColor());
+      }
+    });
+    if (textField instanceof JBTextField) {
+      ((JBTextField)textField).getEmptyText().setText(defaultShellPath);
+    }
   }
 
   public boolean isModified() {
