16841a553ec13efc224d8b3cd60b03a3e9109683
Svetlana.Zemlyanskaya <svetlana.zemlyanskaya@jetbrains.com>
2019-01-28 15:25:48 +0100

    FUS: switch lifecycle collector to event data

--- platform/platform-impl/src/com/intellij/featureStatistics/fusCollectors/LifecycleUsageTriggerCollector.java
+++ platform/platform-impl/src/com/intellij/featureStatistics/fusCollectors/LifecycleUsageTriggerCollector.java
@@ -3,59 +3,57 @@ package com.intellij.featureStatistics.fusCollectors;
 
 import com.intellij.internal.statistic.eventLog.FeatureUsageData;
 import com.intellij.internal.statistic.eventLog.FeatureUsageGroup;
-import com.intellij.internal.statistic.eventLog.FeatureUsageLogger;
+import com.intellij.internal.statistic.service.fus.collectors.FUSCounterUsageLogger;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.text.StringUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Map;
-
 public class LifecycleUsageTriggerCollector {
   private static final Logger LOG = Logger.getInstance("#com.intellij.featureStatistics.fusCollectors.LifecycleUsageTriggerCollector");
   private static final FeatureUsageGroup LIFECYCLE = new FeatureUsageGroup("lifecycle", 2);
 
   public static void onIdeStart() {
-    FeatureUsageLogger.INSTANCE.log(LIFECYCLE, "ide.start");
+    FUSCounterUsageLogger.logEvent(LIFECYCLE, "ide.start");
   }
 
   public static void onIdeClose(boolean restart) {
-    final Map<String, Object> data = new FeatureUsageData().addData("restart", restart).build();
-    FeatureUsageLogger.INSTANCE.log(LIFECYCLE, "ide.close", data);
+    final FeatureUsageData data = new FeatureUsageData().addData("restart", restart);
+    FUSCounterUsageLogger.logEvent(LIFECYCLE, "ide.close", data);
   }
 
   public static void onProjectOpenFinished(@NotNull Project project, long time) {
-    final Map<String, Object> data = new FeatureUsageData().
+    final FeatureUsageData data = new FeatureUsageData().
       addProject(project).
-      addData("time_ms", time).build();
-    FeatureUsageLogger.INSTANCE.log(LIFECYCLE, "project.opening.finished", data);
+      addData("time_ms", time);
+    FUSCounterUsageLogger.logEvent(LIFECYCLE, "project.opening.finished", data);
   }
 
   public static void onProjectOpened(@NotNull Project project) {
-    final Map<String, Object> data = new FeatureUsageData().addProject(project).build();
-    FeatureUsageLogger.INSTANCE.log(LIFECYCLE, "project.opened", data);
+    final FeatureUsageData data = new FeatureUsageData().addProject(project);
+    FUSCounterUsageLogger.logEvent(LIFECYCLE, "project.opened", data);
   }
 
   public static void onProjectClosed(@NotNull Project project) {
-    final Map<String, Object> data = new FeatureUsageData().addProject(project).build();
-    FeatureUsageLogger.INSTANCE.log(LIFECYCLE, "project.closed", data);
+    final FeatureUsageData data = new FeatureUsageData().addProject(project);
+    FUSCounterUsageLogger.logEvent(LIFECYCLE, "project.closed", data);
   }
 
   public static void onFreeze(int lengthInSeconds) {
-    final FeatureUsageData builder =
+    final FeatureUsageData data =
       new FeatureUsageData().addData("duration_s", lengthInSeconds).addData("duration_grouped", toLengthGroup(lengthInSeconds));
-    FeatureUsageLogger.INSTANCE.log(LIFECYCLE, "ide.freeze", builder.build());
+    FUSCounterUsageLogger.logEvent(LIFECYCLE, "ide.freeze", data);
   }
 
   public static void onError(boolean isOOM, boolean isMappingFailed, @Nullable String pluginId) {
     try {
-      final FeatureUsageData builder =
+      final FeatureUsageData data =
         new FeatureUsageData().
           addData("oom", isOOM).
           addData("mapping_failed", isMappingFailed).
           addData("plugin", StringUtil.notNullize(pluginId, "unknown"));
-      FeatureUsageLogger.INSTANCE.log(LIFECYCLE, "ide.error", builder.build());
+      FUSCounterUsageLogger.logEvent(LIFECYCLE, "ide.error", data);
     }
     catch (Exception e) {
       LOG.warn(e);
