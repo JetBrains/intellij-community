8e08f23213fd63127592d3108d285ad80cc9862f
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-01-28 15:29:52 +0100

    extract BeanExtensionPoint and InterfaceExtensionPoint to delegate creation of adapter to ExtensionPoint
    Instead of using switch, simply use different classes for different implementations and overload method to provide corresponding implementation. No need to keep kind of extension point (EP), and be prepared to a new kinds of EP.

---
+++ platform/extensions/src/com/intellij/openapi/extensions/impl/BeanExtensionPoint.java
@@ -0,0 +1,22 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.extensions.impl;
+
+import com.intellij.openapi.extensions.PluginDescriptor;
+import com.intellij.openapi.util.JDOMUtil;
+import org.jdom.Element;
+import org.jetbrains.annotations.NotNull;
+
+public final class BeanExtensionPoint<T> extends ExtensionPointImpl<T> {
+  BeanExtensionPoint(@NotNull String name,
+                     @NotNull String className,
+                     @NotNull ExtensionsAreaImpl owner,
+                     @NotNull PluginDescriptor pluginDescriptor) {
+    super(name, className, owner, pluginDescriptor);
+  }
+
+  @Override
+  @NotNull
+  ExtensionComponentAdapter createAdapter(@NotNull Element extensionElement, @NotNull PluginDescriptor pluginDescriptor) {
+    return doCreateAdapter(getClassName(), extensionElement, !JDOMUtil.isEmpty(extensionElement), pluginDescriptor);
+  }
+}
