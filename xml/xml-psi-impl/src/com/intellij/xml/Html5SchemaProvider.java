5b537f02ed2d6fde01f33f33838ec3b994647de1
Dennis Ushakov <dennis.ushakov@gmail.com>
2019-01-30 12:57:03 +0300

    IDEA-CR-42632

--- xml/xml-psi-impl/src/com/intellij/xml/Html5SchemaProvider.java
+++ xml/xml-psi-impl/src/com/intellij/xml/Html5SchemaProvider.java
@@ -3,6 +3,7 @@ package com.intellij.xml;
 
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.util.AtomicNotNullLazyValue;
 import com.intellij.openapi.util.NotNullLazyValue;
 import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.util.io.URLUtil;
@@ -15,27 +16,9 @@ import java.net.URL;
  */
 public abstract class Html5SchemaProvider {
   private static final Logger LOG = Logger.getInstance("#com.intellij.xml.Html5SchemaProvider");
-  private static final NotNullLazyValue<String> HTML5_SCHEMA_LOCATION = new NotNullLazyValue<String>() {
-    @NotNull
-    @Override
-    protected String compute() {
-      return loadLocation(getInstance().getHtmlSchemaLocation(), "HTML5_SCHEMA");
-    }
-  };
-  private static final NotNullLazyValue<String> XHTML5_SCHEMA_LOCATION = new NotNullLazyValue<String>() {
-    @NotNull
-    @Override
-    protected String compute() {
-      return loadLocation(getInstance().getXhtmlSchemaLocation(), "XHTML5_SCHEMA");
-    }
-  };
-  private static final NotNullLazyValue<String> CHARS_DTD_LOCATION = new NotNullLazyValue<String>() {
-    @NotNull
-    @Override
-    protected String compute() {
-      return loadLocation(getInstance().getCharsLocation(), "CHARS_DTD");
-    }
-  };
+  private static final NotNullLazyValue<String> HTML5_SCHEMA_LOCATION = AtomicNotNullLazyValue.createValue(() -> loadLocation(getInstance().getHtmlSchemaLocation(), "HTML5_SCHEMA"));
+  private static final NotNullLazyValue<String> XHTML5_SCHEMA_LOCATION = AtomicNotNullLazyValue.createValue(() -> loadLocation(getInstance().getXhtmlSchemaLocation(), "XHTML5_SCHEMA"));
+  private static final NotNullLazyValue<String> CHARS_DTD_LOCATION = AtomicNotNullLazyValue.createValue(() -> loadLocation(getInstance().getCharsLocation(), "CHARS_DTD"));
 
   private static String loadLocation(URL url, String id) {
     String location = VfsUtilCore.urlToPath(VfsUtilCore.fixURLforIDEA(
