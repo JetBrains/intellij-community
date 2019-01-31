8ce47fc790d8873e7104bb6e6fd78e8c13d0711e
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2019-01-31 13:02:22 +0300

    merging Android Studio 3.3: missed android-kotlin.jar

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/CommunityRepositoryModules.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/CommunityRepositoryModules.groovy
@@ -320,6 +320,7 @@ class CommunityRepositoryModules {
       withResourceFromModule("intellij.android.core","lib/commons-compress-1.8.1.jar", "lib")
       withResourceFromModule("intellij.android.core","lib/javawriter-2.2.1.jar", "lib")
       withResourceFromModule("intellij.android.core","lib/android-extensions-ide.jar", "lib")
+      withResourceFromModule("intellij.android.core","lib/android-kotlin.jar", "lib")
 
       withResourceFromModule("intellij.android.core","lib/androidWidgets", "lib/androidWidgets")
       withResourceFromModule("intellij.android.artwork","resources/device-art-resources", "lib/device-art-resources")
