cee9e743663f51548e36ee5cf8f377eb79366f3c
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-01-31 13:31:35 +0100

    prefer to use ProjectExtensionPointName

--- platform/lang-impl/src/com/intellij/ide/impl/ProjectViewSelectInTarget.java
+++ platform/lang-impl/src/com/intellij/ide/impl/ProjectViewSelectInTarget.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.impl;
 
 import com.intellij.ide.CompositeSelectInTarget;
@@ -139,8 +139,7 @@ public abstract class ProjectViewSelectInTarget extends SelectInTargetPsiWrapper
   }
 
   private TreeStructureProvider[] getProvidersDumbAware() {
-    TreeStructureProvider[] allProviders = TreeStructureProvider.EP_NAME.getExtensions(myProject);
-    List<TreeStructureProvider> dumbAware = DumbService.getInstance(myProject).filterByDumbAwareness(allProviders);
+    List<TreeStructureProvider> dumbAware = DumbService.getInstance(myProject).filterByDumbAwareness(TreeStructureProvider.EP.getExtensions(myProject));
     return dumbAware.toArray(new TreeStructureProvider[0]);
   }
 
