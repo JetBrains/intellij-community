24f1d986c1f451ac03749d6231d93215d09c3b38
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2019-02-04 16:38:46 +0300

    Gradle: allow to send custom test events for Gradle test runner using internal xml protocol

--- plugins/gradle/tooling-extension-impl/src/org/jetbrains/plugins/gradle/tooling/builder/ExternalProjectBuilderImpl.groovy
+++ plugins/gradle/tooling-extension-impl/src/org/jetbrains/plugins/gradle/tooling/builder/ExternalProjectBuilderImpl.groovy
@@ -157,7 +157,8 @@ class ExternalProjectBuilderImpl implements ModelBuilderService {
         externalTask.QName = task.name
         externalTask.description = task.description
         externalTask.group = task.group ?: "other"
-        externalTask.test = task instanceof Test
+        def ext = task.getExtensions()?.extraProperties
+        externalTask.test = (task instanceof Test) || (ext?.has("idea.internal.test") && ext.get("idea.internal.test") == true)
         externalTask.type = ProjectExtensionsDataBuilderImpl.getType(task)
         result.put(externalTask.name, externalTask)
       }
