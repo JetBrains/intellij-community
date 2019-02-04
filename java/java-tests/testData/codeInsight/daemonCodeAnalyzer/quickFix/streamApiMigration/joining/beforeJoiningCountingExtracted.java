15e0df11f9f806c035c87e8da9672792605e5dfb
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2019-02-04 14:19:52 +0700

    IDEA-206497 Replace with stream: support intermediate map operations in counting loops

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/streamApiMigration/joining/beforeJoiningCountingExtracted.java
@@ -0,0 +1,17 @@
+// "Replace with collect" "true"
+
+import java.util.List;
+
+public class Test {
+  String foo(String[] lines) {
+    final StringBuilder result = new StringBuilder();
+    fo<caret>r (int i = 0; i < lines.length; i++) {
+      final String line = lines[i];// extracted into a separate var
+      if (i > 0) {
+        result.append('\n');
+      }
+      result.append(line);
+    }
+    return result.toString();
+  }
+}
