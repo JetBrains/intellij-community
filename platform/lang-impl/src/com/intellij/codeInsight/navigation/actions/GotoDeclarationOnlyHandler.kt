c955be737ead760f52cdee74807125790fcc6e34
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-02-05 11:38:17 +0300

    record "navigation.goto.declaration" in GotoDeclarationOnlyHandler
    It turns out that FeatureUsageTracker is used for Productivity Guide, so we gotta record this feature usage.
    The feature is recorded in GotoDeclarationOnlyHandler only so it doesn't matter through what action (GotoDeclaration or
    GotoDeclarationOnly) the user invokes it.

--- platform/lang-impl/src/com/intellij/codeInsight/navigation/actions/GotoDeclarationOnlyHandler.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/navigation/actions/GotoDeclarationOnlyHandler.kt
@@ -7,6 +7,7 @@ import com.intellij.codeInsight.TargetElementUtil
 import com.intellij.codeInsight.hint.HintManager
 import com.intellij.codeInsight.navigation.NavigationUtil
 import com.intellij.codeInsight.navigation.actions.GotoDeclarationAction.*
+import com.intellij.featureStatistics.FeatureUsageTracker
 import com.intellij.ide.util.DefaultPsiElementCellRenderer
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.DumbService
@@ -22,6 +23,8 @@ object GotoDeclarationOnlyHandler : CodeInsightActionHandler {
   override fun startInWriteAction(): Boolean = false
 
   override fun invoke(project: Project, editor: Editor, file: PsiFile) {
+    FeatureUsageTracker.getInstance().triggerFeatureUsed("navigation.goto.declaration")
+
     val dumbService = DumbService.getInstance(project)
     try {
       dumbService.isAlternativeResolveEnabled = true
