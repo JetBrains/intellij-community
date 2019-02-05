37a48d8e0b0f93c46acf4f18c475230e274aaf51
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-02-05 11:30:37 +0100

    [tests] improves file lookup in DirectoryIndexRestoreTest

--- java/java-tests/testSrc/com/intellij/java/openapi/roots/impl/DirectoryIndexRestoreTest.java
+++ java/java-tests/testSrc/com/intellij/java/openapi/roots/impl/DirectoryIndexRestoreTest.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.java.openapi.roots.impl;
 
 import com.intellij.openapi.roots.ContentIterator;
@@ -44,11 +30,11 @@ public class DirectoryIndexRestoreTest extends IdeaTestCase {
 
     LocalFileSystem fs = LocalFileSystem.getInstance();
     File temp = createTempDirectory();
-    myTempVFile = fs.findFileByIoFile(temp);
+    myTempVFile = fs.refreshAndFindFileByIoFile(temp);
     assertNotNull(myTempVFile);
     File root = new File(temp, "top/d1/d2/root");
     assertTrue(root.mkdirs());
-    VirtualFile rootVFile = fs.findFileByIoFile(root);
+    VirtualFile rootVFile = fs.refreshAndFindFileByIoFile(root);
     assertNotNull(rootVFile);
 
     VirtualFile moduleDir = createChildDirectory(rootVFile, "module");
