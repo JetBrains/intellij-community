7720a8c4c8f3b2f26675d6eaa5518cd50be6dcd9
Nikita Katkov <Nikita.Katkov@jetbrains.com>
2019-02-05 17:32:41 +0300

    used AnnotationUtil inside annotation based checkers

--- jvm/jvm-analysis-impl/src/com/intellij/codeInspection/blockingCallsDetection/AnnotationBasedBlockingMethodChecker.java
+++ jvm/jvm-analysis-impl/src/com/intellij/codeInspection/blockingCallsDetection/AnnotationBasedBlockingMethodChecker.java
@@ -1,15 +1,11 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInspection.blockingCallsDetection;
 
-import com.intellij.codeInsight.ExternalAnnotationsManager;
+import com.intellij.codeInsight.AnnotationUtil;
 import com.intellij.psi.*;
-import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class AnnotationBasedBlockingMethodChecker implements BlockingMethodChecker, PersistentStateChecker {
 
@@ -33,17 +29,7 @@ public class AnnotationBasedBlockingMethodChecker implements BlockingMethodCheck
   }
 
   static boolean hasAnnotation(PsiModifierListOwner owner, List<String> annotationsFQNames) {
-    //AnnotationUtil.isAnnotated() fixme
-
-
-    boolean hasAnnotation = annotationsFQNames.stream()
-      .anyMatch(annotation -> owner.hasAnnotation(annotation));
-    if (hasAnnotation) return true;
-
-    PsiAnnotation[] externalAnnotations = ExternalAnnotationsManager.getInstance(owner.getProject()).findExternalAnnotations(owner);
-    if (externalAnnotations == null) return false;
-    Set<String> externalAnnotationsFQNames =
-      Arrays.stream(externalAnnotations).map(PsiAnnotation::getQualifiedName).collect(Collectors.toSet());
-    return ContainerUtil.intersects(annotationsFQNames, externalAnnotationsFQNames);
+    // AnnotationUtil#isAnnotated doesn't use caching inside
+    return AnnotationUtil.findAnnotation(owner, annotationsFQNames, false) != null;
   }
 }
