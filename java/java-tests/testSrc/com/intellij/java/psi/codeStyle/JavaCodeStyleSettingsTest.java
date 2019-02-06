c40d30a6eb028a5d607071c0678522328b03575e
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2019-02-06 21:25:51 +0300

    Fixed IDEA-206778 Export codestyle settings to JSON: export fails when non-default values are provided
    Namely "Next line if wrapped"

--- java/java-tests/testSrc/com/intellij/java/psi/codeStyle/JavaCodeStyleSettingsTest.java
+++ java/java-tests/testSrc/com/intellij/java/psi/codeStyle/JavaCodeStyleSettingsTest.java
@@ -83,6 +83,7 @@ public class JavaCodeStyleSettingsTest extends CodeStyleTestCase {
     commonJavaSettings.METHOD_PARAMETERS_WRAP = CommonCodeStyleSettings.WRAP_AS_NEEDED;
     commonJavaSettings.CALL_PARAMETERS_WRAP = CommonCodeStyleSettings.WRAP_ON_EVERY_ITEM;
     commonJavaSettings.WRAP_ON_TYPING = CommonCodeStyleSettings.WrapOnTyping.WRAP.intValue;
+    commonJavaSettings.METHOD_BRACE_STYLE = CommonCodeStyleSettings.NEXT_LINE_IF_WRAPPED;
     final JavaCodeStyleSettings javaSettings = settings.getCustomSettings(JavaCodeStyleSettings.class);
     javaSettings.FIELD_NAME_PREFIX = "m_";
     javaSettings.STATIC_FIELD_NAME_SUFFIX = "_s";
