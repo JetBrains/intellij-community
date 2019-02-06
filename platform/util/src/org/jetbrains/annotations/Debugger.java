821e130b70f57935e1e80c96df8d7e3ebeed3e52
Egor Ushakov <egor.ushakov@jetbrains.com>
2019-02-06 13:09:07 +0300

    IDEA-CR-42880: fixes for review

--- platform/util/src/org/jetbrains/annotations/Debugger.java
+++ platform/util/src/org/jetbrains/annotations/Debugger.java
@@ -1,6 +1,8 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.annotations;
 
+import org.intellij.lang.annotations.Language;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
@@ -15,8 +17,13 @@ public class Debugger {
    */
   @Target(ElementType.TYPE)
   public @interface Renderer {
+    @Language(value = "JAVA", prefix = "class Renderer{String text(){return ", suffix = ";}}")
     String text() default "";
+
+    @Language(value = "JAVA", prefix = "class Renderer{String[] childrenArray(){return ", suffix = ";}}")
     String childrenArray() default "";
+
+    @Language(value = "JAVA", prefix = "class Renderer{boolean hasChildren(){return ", suffix = ";}}")
     String hasChildren() default "";
   }
 }
