67c23e9a2fe0b60011b1aae6573494514bf69cbd
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2019-02-06 18:18:03 +0300

    fix testdata quotes

--- java/java-tests/testData/inspection/nullableProblems/NullPassedToNotNullParameter.java
+++ java/java-tests/testData/inspection/nullableProblems/NullPassedToNotNullParameter.java
@@ -1,11 +1,11 @@
 import org.jetbrains.annotations.NotNull;
 
 class Test {
-  void someMethod(<warning descr="Parameter annotated @NotNull should not receive null as an argument">@NotNull</warning> Object warn, <warning descr="Parameter annotated @NotNull should not receive null as an argument">@NotNull</warning> Object o2, <warning descr="Parameter annotated @NotNull should not receive null as an argument">@NotNull</warning> Object warn1) {
+  void someMethod(<warning descr="Parameter annotated @NotNull should not receive 'null' as an argument">@NotNull</warning> Object warn, <warning descr="Parameter annotated @NotNull should not receive 'null' as an argument">@NotNull</warning> Object o2, <warning descr="Parameter annotated @NotNull should not receive 'null' as an argument">@NotNull</warning> Object warn1) {
 
   }
 
-  static void someStaticMethod(Object notAnnotated, <warning descr="Parameter annotated @NotNull should not receive null as an argument">@NotNull</warning> Object o2, <warning descr="Parameter annotated @NotNull should not receive null as an argument">@NotNull</warning> Object warn2) {
+  static void someStaticMethod(Object notAnnotated, <warning descr="Parameter annotated @NotNull should not receive 'null' as an argument">@NotNull</warning> Object o2, <warning descr="Parameter annotated @NotNull should not receive 'null' as an argument">@NotNull</warning> Object warn2) {
 
   }
 
