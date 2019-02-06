080c635a652a9e4c10e8e9822eff935c243dc6c0
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-02-06 15:06:39 +0100

    editor notification provider should be app level

--- platform/lang-impl/src/com/intellij/semantic/SemServiceImpl.java
+++ platform/lang-impl/src/com/intellij/semantic/SemServiceImpl.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.semantic;
 
 import com.intellij.openapi.application.ApplicationManager;
@@ -142,7 +128,7 @@ public class SemServiceImpl extends SemService{
 
   @Override
   @Nullable
-  public <T extends SemElement> List<T> getSemElements(final SemKey<T> key, @NotNull final PsiElement psi) {
+  public <T extends SemElement> List<T> getSemElements(@NotNull SemKey<T> key, @NotNull final PsiElement psi) {
     List<T> cached = _getCachedSemElements(key, true, psi);
     if (cached != null) {
       return cached;
@@ -205,7 +191,7 @@ public class SemServiceImpl extends SemService{
   }
 
   @Nullable
-  private <T extends SemElement> List<T> _getCachedSemElements(SemKey<T> key, boolean paranoid, final PsiElement element) {
+  private <T extends SemElement> List<T> _getCachedSemElements(@NotNull SemKey<T> key, boolean paranoid, final PsiElement element) {
     final SemCacheChunk chunk = obtainChunk(element);
     if (chunk == null) return null;
 
