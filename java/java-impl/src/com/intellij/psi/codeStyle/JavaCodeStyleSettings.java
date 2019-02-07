d3973a1addc30a337b08c22e2a268122a052330c
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2019-02-07 13:45:09 +0300

    Rename "replace_int_sum..." to "replace_sum" (IDEA-CR-42964)

--- java/java-impl/src/com/intellij/psi/codeStyle/JavaCodeStyleSettings.java
+++ java/java-impl/src/com/intellij/psi/codeStyle/JavaCodeStyleSettings.java
@@ -107,7 +107,7 @@ public class JavaCodeStyleSettings extends CustomCodeStyleSettings implements Im
   public boolean REPLACE_INSTANCEOF_AND_CAST = false;
   public boolean REPLACE_NULL_CHECK = true;
 
-  @Property(externalName = "replace_int_sum_lambda_with_method_ref")
+  @Property(externalName = "replace_sum_lambda_with_method_ref")
   public boolean REPLACE_SUM = true;
 
   public boolean SPACES_WITHIN_ANGLE_BRACKETS;
