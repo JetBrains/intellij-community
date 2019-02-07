aef8103ee3de19589383fe9e56546fee36daf0bf
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-02-07 14:19:42 +0100

    [platform] corrects patch suffix detection on macOS (IDEA-206868)

--- platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/UpdateInstaller.kt
+++ platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/UpdateInstaller.kt
@@ -187,7 +187,7 @@ object UpdateInstaller {
   private fun getTempDir() = File(PathManager.getTempPath(), "patch-update")
 
   private fun getJdkSuffix(): String {
-    val jreHome = File(PathManager.getHomePath(), if (SystemInfo.isMac) "Contents/jdk" else "jre64")
+    val jreHome = File(PathManager.getHomePath(), if (SystemInfo.isMac) "jdk" else "jre64")
     if (!jreHome.exists()) return "-no-jdk"
     val releaseFile = File(jreHome, if (SystemInfo.isMac) "Contents/Home/release" else "release")
     val version = try {
