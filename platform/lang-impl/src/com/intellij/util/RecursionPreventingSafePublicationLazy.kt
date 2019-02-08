6a9001b39bb8ab2655109cf9e7b1db59520ead6b
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-02-08 16:16:00 +0300

    move RecursionPreventingSafePublicationLazy to lang-impl, add utility function `recursionSafeLazy`

--- platform/lang-impl/src/com/intellij/util/RecursionPreventingSafePublicationLazy.kt
+++ platform/lang-impl/src/com/intellij/util/RecursionPreventingSafePublicationLazy.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.util
+package com.intellij.util
 
 import com.intellij.openapi.util.RecursionManager
 import com.intellij.util.ObjectUtils.notNullize
--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/util/functions.kt
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/util/functions.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.util
 
+import com.intellij.util.RecursionPreventingSafePublicationLazy
 import java.util.function.Consumer
 import com.intellij.util.Consumer as JBConsumer
 
