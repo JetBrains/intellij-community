04279096e4a25954bcaf76d5720f3a3819dd3768
Nikita Skvortsov <nikita.skvortsov@jetbrains.com>
2019-02-08 19:33:19 +0300

    IDEA-CR-43035 cleanup configuration test

--- plugins/gradle/java/testSources/execution/test/runner/GradleTestRunConfigurationProducerTest.kt
+++ plugins/gradle/java/testSources/execution/test/runner/GradleTestRunConfigurationProducerTest.kt
@@ -54,74 +54,41 @@ class GradleTestRunConfigurationProducerTest : GradleTestRunConfigurationProduce
   }
 
   @Test
-  fun `test configuration from context`() {
+  fun `test configuration producer properly matches configuration with context`() {
     val projectData = generateAndImportTemplateProject()
     runReadActionAndWait {
-      val context1 = getContextByLocation(projectData["project"]["TestCase"]["test1"].element)
-      val context2 = getContextByLocation(projectData["project"]["TestCase"].element)
-      val context3 = getContextByLocation(projectData["project"]["pkg.TestCase"].element.containingFile.containingDirectory)
-      val context4 = getContextByLocation(projectData["project"]["TestCase"]["test2"].element)
-      val context5 = getContextByLocation(projectData["project"]["TestCase"]["test1"].element)
-      val context6 = getContextByLocation(projectData["project"]["TestCase"].element)
-      val context7 = getContextByLocation(projectData["project"]["pkg.TestCase"].element.containingFile.containingDirectory)
-      val context8 = getContextByLocation(projectData["project"]["TestCase"]["test2"].element)
-      val configurationFromContext1 = getConfigurationFromContext(context1)
-      val configurationFromContext2 = getConfigurationFromContext(context2)
-      val configurationFromContext3 = getConfigurationFromContext(context3)
-      val configurationFromContext4 = getConfigurationFromContext(context4)
-      val configurationFromContext5 = getConfigurationFromContext(context5)
-      val configurationFromContext6 = getConfigurationFromContext(context6)
-      val configurationFromContext7 = getConfigurationFromContext(context7)
-      val configurationFromContext8 = getConfigurationFromContext(context8)
-      val configuration1 = configurationFromContext1.configuration as ExternalSystemRunConfiguration
-      val configuration2 = configurationFromContext2.configuration as ExternalSystemRunConfiguration
-      val configuration3 = configurationFromContext3.configuration as ExternalSystemRunConfiguration
-      val configuration4 = configurationFromContext4.configuration as ExternalSystemRunConfiguration
-      val configuration5 = configurationFromContext5.configuration as ExternalSystemRunConfiguration
-      val configuration6 = configurationFromContext6.configuration as ExternalSystemRunConfiguration
-      val configuration7 = configurationFromContext7.configuration as ExternalSystemRunConfiguration
-      val configuration8 = configurationFromContext8.configuration as ExternalSystemRunConfiguration
-      val producer1 = configurationFromContext1.configurationProducer
-      val producer2 = configurationFromContext2.configurationProducer
-      val producer3 = configurationFromContext3.configurationProducer
-      val producer4 = configurationFromContext4.configurationProducer
-      assertTrue(producer1.isConfigurationFromContext(configuration1, context1))
-      assertFalse(producer1.isConfigurationFromContext(configuration1, context2))
-      assertFalse(producer1.isConfigurationFromContext(configuration1, context3))
-      assertFalse(producer1.isConfigurationFromContext(configuration1, context4))
-      assertFalse(producer2.isConfigurationFromContext(configuration2, context1))
-      assertTrue(producer2.isConfigurationFromContext(configuration2, context2))
-      assertFalse(producer2.isConfigurationFromContext(configuration2, context3))
-      assertFalse(producer2.isConfigurationFromContext(configuration2, context4))
-      assertFalse(producer3.isConfigurationFromContext(configuration3, context1))
-      assertFalse(producer3.isConfigurationFromContext(configuration3, context2))
-      assertTrue(producer3.isConfigurationFromContext(configuration3, context3))
-      assertFalse(producer3.isConfigurationFromContext(configuration3, context4))
-      assertFalse(producer4.isConfigurationFromContext(configuration4, context1))
-      assertFalse(producer4.isConfigurationFromContext(configuration4, context2))
-      assertFalse(producer4.isConfigurationFromContext(configuration4, context3))
-      assertTrue(producer4.isConfigurationFromContext(configuration4, context4))
-      assertTrue(producer1.isConfigurationFromContext(configuration5, context5))
-      assertTrue(producer2.isConfigurationFromContext(configuration6, context6))
-      assertTrue(producer3.isConfigurationFromContext(configuration7, context7))
-      assertTrue(producer4.isConfigurationFromContext(configuration8, context8))
-      assertTrue(producer1.isConfigurationFromContext(configuration1, context5))
-      assertTrue(producer2.isConfigurationFromContext(configuration2, context6))
-      assertTrue(producer3.isConfigurationFromContext(configuration3, context7))
-      assertTrue(producer4.isConfigurationFromContext(configuration4, context8))
-      assertTrue(producer1.isConfigurationFromContext(configuration5, context1))
-      assertTrue(producer2.isConfigurationFromContext(configuration6, context2))
-      assertTrue(producer3.isConfigurationFromContext(configuration7, context3))
-      assertTrue(producer4.isConfigurationFromContext(configuration8, context4))
-      val context = getContextByLocation(
-        projectData["project"]["TestCase"]["test1"].element,
-        projectData["project"]["pkg.TestCase"]["test1"].element,
-        projectData["module"]["ModuleTestCase"]["test1"].element
-      )
-      assertFalse(producer1.isConfigurationFromContext(configuration1, context))
-      assertFalse(producer2.isConfigurationFromContext(configuration2, context))
-      assertFalse(producer3.isConfigurationFromContext(configuration3, context))
-      assertFalse(producer4.isConfigurationFromContext(configuration4, context))
+      val locations = listOf(projectData["project"]["TestCase"]["test1"].element,
+                             projectData["project"]["TestCase"].element,
+                             projectData["project"]["pkg.TestCase"].element.containingFile.containingDirectory,
+                             projectData["project"]["TestCase"]["test2"].element)
+
+      val contexts = (locations + locations).map { getContextByLocation(it) } // with duplicate locations
+      val cfgFromCtx = contexts.map { getConfigurationFromContext(it) }
+      val configurations = cfgFromCtx.map { it.configuration as ExternalSystemRunConfiguration }
+      val methodProd = getConfigurationProducer<TestMethodGradleConfigurationProducer>()
+      val classProd = getConfigurationProducer<TestClassGradleConfigurationProducer>()
+      val packageProd = getConfigurationProducer<AllInPackageGradleConfigurationProducer>()
+
+      for ((j, configuration) in configurations.withIndex()) {
+        for ((k, context) in contexts.withIndex()) {
+          val cfgMatchesContext = j % locations.size == k % locations.size
+
+          val isPsiMethod = context.psiLocation is PsiMethod
+          assertEquals(isPsiMethod && cfgMatchesContext, methodProd.isConfigurationFromContext(configuration, context))
+
+          val isPsiClass = context.psiLocation is PsiClass
+          assertEquals(isPsiClass && cfgMatchesContext, classProd.isConfigurationFromContext(configuration, context))
+
+          val isPsiDir = context.psiLocation is PsiDirectory
+          assertEquals(isPsiDir && cfgMatchesContext, packageProd.isConfigurationFromContext(configuration, context))
+        }
+      }
+
+      val context = getContextByLocation(*locations.toTypedArray())
+
+      assertFalse(methodProd.isConfigurationFromContext(configurations[0], context))
+      assertFalse(classProd.isConfigurationFromContext(configurations[1], context))
+      assertFalse(packageProd.isConfigurationFromContext(configurations[2], context))
     }
   }
 
