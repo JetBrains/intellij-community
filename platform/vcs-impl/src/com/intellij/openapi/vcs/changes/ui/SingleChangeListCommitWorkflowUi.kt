1f1258e2853543ed7b54427904e16dbc9ea8c802
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2019-02-10 20:52:08 +0300

    vcs: Refactor commit dialog - provide events for "default commit" and "commit executor" invocation

---
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/SingleChangeListCommitWorkflowUi.kt
@@ -0,0 +1,14 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.vcs.changes.ui
+
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.vcs.changes.CommitExecutor
+import java.util.*
+
+interface SingleChangeListCommitWorkflowUi {
+  fun addExecutorListener(listener: CommitExecutorListener, parent: Disposable)
+}
+
+interface CommitExecutorListener : EventListener {
+  fun executorCalled(executor: CommitExecutor?)
+}
