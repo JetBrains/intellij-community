cf160d8259fb98ef4003283063529be8c4c087fb
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2019-02-11 18:39:22 +0300

    Add a script to compile all binary modules in test data directory
    GitOrigin-RevId: b4da703157f1397dac2760ca844d43143bae5cdc

---
+++ python/helpers/pycharm_generator_utils/test/data/SkeletonCaching/binaries/compile_binaries.py
@@ -0,0 +1,19 @@
+import os
+
+from Cython.Build.Cythonize import main as cythonize
+
+_binaries_dir = os.path.dirname(os.path.abspath(__file__))
+_cythonize_options = ['--inplace']
+
+
+def main():
+    for dir_path, _, file_names in os.walk(_binaries_dir):
+        for file_name in file_names:
+            mod_name, ext = os.path.splitext(file_name)
+            if ext == '.pyx':
+                cythonize(_cythonize_options + [os.path.join(dir_path, file_name)])
+                os.remove(os.path.join(dir_path, mod_name + '.c'))
+
+
+if __name__ == '__main__':
+    main()
