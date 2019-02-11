5937d333203325c1ba146e1783d3ffbff29f966d
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-02-11 19:53:32 +0100

    [build] supports incremental updating in product manifest builder (IDEA-CR-43018)

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/ManifestBuilder.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/ManifestBuilder.groovy
@@ -4,6 +4,8 @@ package org.jetbrains.intellij.build.impl
 import com.intellij.openapi.util.text.StringUtil
 import groovy.transform.CompileDynamic
 import groovy.transform.CompileStatic
+import org.jetbrains.annotations.NotNull
+import org.jetbrains.annotations.Nullable
 import org.jetbrains.intellij.build.BuildContext
 
 import java.nio.channels.FileChannel
@@ -17,26 +19,38 @@ import java.util.function.Function
  */
 @CompileStatic
 class ManifestBuilder {
+  private static final String NAME = "product-info"
+  private static final String CHARSET = "UTF-8"
+  private static final String SEPARATOR = " *"
+
+  public final String manifestName
+
   private final MessageDigest digest
 
   ManifestBuilder(BuildContext context) {
     digest = MessageDigest.getInstance(context.options.hashAlgorithm)
+    String ext = digest.algorithm.toLowerCase(Locale.ENGLISH).replace("-", "")
+    manifestName = "${NAME}.${ext}"
   }
 
-  String buildManifest(List<String> paths, String basePath, Function<String, String> mapper = null) {
-    Map<String, byte[]> hashes = collectHashes(paths, mapper)
-    String ext = digest.algorithm.toLowerCase(Locale.ENGLISH).replace("-", "")
-    String path = "${basePath}.${ext}"
-    writeHashes(hashes, path)
-    return path
+  void buildManifest(@NotNull List<String> paths, @NotNull String targetDirectory, @Nullable Function<String, String> mapper = null) {
+    Map<String, byte[]> hashes = new TreeMap<String, byte[]>()
+    updateHashes(hashes, paths, mapper)
+    writeHashes(hashes, new File(targetDirectory, manifestName))
+  }
+
+  void updateManifest(@NotNull List<String> paths, @NotNull String targetDirectory) {
+    Map<String, byte[]> hashes = new TreeMap<String, byte[]>()
+    File file = new File(targetDirectory, manifestName)
+    loadHashes(file, hashes)
+    updateHashes(hashes, paths, null)
+    writeHashes(hashes, file)
   }
 
   @CompileDynamic
-  private SortedMap<String, byte[]> collectHashes(List<String> paths, Function<String, String> mapper) {
-    Map<String, byte[]> hashes = new TreeMap<String, byte[]>()
-
-    paths.each {
-      Path root = new File(it).toPath()
+  private void updateHashes(Map<String, byte[]> hashes, List<String> paths, @Nullable Function<String, String> mapper) {
+    for (path in paths) {
+      Path root = new File(path).toPath()
       Files.walkFileTree(root, EnumSet.of(FileVisitOption.FOLLOW_LINKS), Integer.MAX_VALUE, new SimpleFileVisitor<Path>() {
         @Override
         FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
@@ -51,20 +65,27 @@ class ManifestBuilder {
         }
       })
     }
-
-    return hashes
   }
 
-  private static String writeHashes(SortedMap<String, byte[]> hashes, String path) {
-    new File(path).parentFile.mkdirs()
-    new PrintWriter(path, "UTF-8").withWriter {
-      hashes.each { relPath, digest ->
-        it.print(StringUtil.toHexString(digest))
-        it.print(" *")
+  private static void loadHashes(File file, Map<String, byte[]> hashes) {
+    new BufferedReader(new InputStreamReader(new FileInputStream(file), CHARSET)).eachLine { line ->
+      int p = line.indexOf(SEPARATOR)
+      assert p > 0 : "Invalid input: '$line'"
+      String relPath = line.substring(p + SEPARATOR.length())
+      byte[] hash = StringUtil.parseHexString(line.substring(0, p))
+      hashes[relPath] = hash
+    }
+  }
+
+  private static void writeHashes(Map<String, byte[]> hashes, File file) {
+    file.parentFile.mkdirs()
+    new PrintWriter(file, CHARSET).withWriter {
+      hashes.each { relPath, hash ->
+        it.print(StringUtil.toHexString(hash))
+        it.print(SEPARATOR)
         it.print(relPath)
         it.println()
       }
     }
-    return path
   }
 }
