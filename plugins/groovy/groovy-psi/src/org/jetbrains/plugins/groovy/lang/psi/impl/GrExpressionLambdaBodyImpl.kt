310384e13dc3b518b2df70ecc3d67982390d120f
alexey.afanasiev <alexey.afanasiev@jetbrains.com>
2019-02-12 17:27:31 +0300

    [groovy] IDEA-CR-43128 IDEA-202693 Support lambda in control flow builder
    (cherry picked from commit b81ed1bc06d7f03383b574a2afd1c09f16ae273d)

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/GrExpressionLambdaBodyImpl.kt
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/GrExpressionLambdaBodyImpl.kt
@@ -4,33 +4,25 @@ package org.jetbrains.plugins.groovy.lang.psi.impl
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiType
 import com.intellij.psi.util.parentOfType
-import com.intellij.util.IncorrectOperationException
 import org.jetbrains.plugins.groovy.lang.psi.GroovyElementVisitor
-import org.jetbrains.plugins.groovy.lang.psi.api.GrLambdaBody
+import org.jetbrains.plugins.groovy.lang.psi.api.GrExpressionLambdaBody
 import org.jetbrains.plugins.groovy.lang.psi.api.GrLambdaExpression
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrStatement
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrExpression
 import org.jetbrains.plugins.groovy.lang.psi.controlFlow.Instruction
 import org.jetbrains.plugins.groovy.lang.psi.controlFlow.impl.ControlFlowBuilder
 
-class GrExpressionLambdaBodyImpl(node: ASTNode) : GroovyPsiElementImpl(node), GrLambdaBody {
-  override fun getReturnType(): PsiType? = getExpression().type
+class GrExpressionLambdaBodyImpl(node: ASTNode) : GroovyPsiElementImpl(node), GrExpressionLambdaBody {
+  override fun getReturnType(): PsiType? = expression.type
 
   override fun getLambdaExpression(): GrLambdaExpression = requireNotNull(parentOfType())
 
-  fun getExpression() : GrExpression = findNotNullChildByClass(GrExpression::class.java)
+  override fun getExpression() : GrExpression = findNotNullChildByClass(GrExpression::class.java)
 
-  override fun accept(visitor: GroovyElementVisitor) = visitor.visitLambdaBody(this)
+  override fun accept(visitor: GroovyElementVisitor) = visitor.visitExpressionLambdaBody(this)
 
   override fun toString(): String = "Lambda body"
 
   override fun getControlFlow(): Array<Instruction> = ControlFlowBuilder(project).buildControlFlow(this)
 
   override fun isTopControlFlowOwner(): Boolean = true
-
-  override fun addStatementBefore(statement: GrStatement, anchor: GrStatement?): GrStatement {
-    throw IncorrectOperationException("Can't add statement in a single expression form of body")
-  }
-
-  override fun getStatements(): Array<GrStatement> = arrayOf(getExpression())
 }
