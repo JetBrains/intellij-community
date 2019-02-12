c7be59c8b7ac1dbb84308d40a7271841bd1b64df
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2019-02-12 20:17:44 +0300

    external prebuilt index dialog

---
+++ platform/lang-impl/src/org/jetbrains/index/actions/ExternalPrebuiltIndexManager.java
@@ -0,0 +1,38 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.index.actions;
+
+import com.intellij.ide.util.PropertiesComponent;
+import com.intellij.openapi.fileChooser.FileChooser;
+import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.util.registry.Registry;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+
+public class ExternalPrebuiltIndexManager {
+  private final Project myProject;
+
+  private volatile String myIndexPath;
+  private volatile boolean myEnabled;
+
+  public ExternalPrebuiltIndexManager(@NotNull Project project) {
+    myIndexPath = PropertiesComponent.getInstance(project).getValue("external.prebuilt.indices.path");
+    myEnabled = Registry.is("use.external.prebuilt.indices") && PropertiesComponent.getInstance(project).getBoolean("external.prebuilt.indices.for.project", true);
+    myProject = project;
+  }
+
+  private void showDialog() {
+    if (Messages.showYesNoDialog(myProject, "Would you like specify prebuilt indices directory", "Prebuilt Indices", null) == Messages.YES) {
+      PropertiesComponent.getInstance(myProject).setValue("external.prebuilt.indices.for.project", true);
+      VirtualFile file = FileChooser.chooseFile(FileChooserDescriptorFactory.createSingleFolderDescriptor(), myProject, null);
+      if (file != null) {
+        String path = file.getPath();
+        PropertiesComponent.getInstance(myProject).setValue("external.prebuilt.indices.path", path);
+        myIndexPath = path;
+      }
+    }
+    myEnabled = false;
+    PropertiesComponent.getInstance(myProject).setValue("external.prebuilt.indices.for.project", false);
+  }
+}
