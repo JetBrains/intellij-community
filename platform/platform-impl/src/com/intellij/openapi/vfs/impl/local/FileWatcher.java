044f62565ea15b04762d4cf230baf7b431b7dbc7
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-02-12 20:27:10 +0100

    Cleanup (formatting)

--- platform/platform-impl/src/com/intellij/openapi/vfs/impl/local/FileWatcher.java
+++ platform/platform-impl/src/com/intellij/openapi/vfs/impl/local/FileWatcher.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vfs.impl.local;
 
 import com.google.common.util.concurrent.MoreExecutors;
@@ -190,8 +176,10 @@ public class FileWatcher {
     LOG.warn(cause);
 
     if (myFailureShown.compareAndSet(false, true)) {
+      NotificationGroup group = NOTIFICATION_GROUP.getValue();
       String title = ApplicationBundle.message("watcher.slow.sync");
-      ApplicationManager.getApplication().invokeLater(() -> Notifications.Bus.notify(NOTIFICATION_GROUP.getValue().createNotification(title, cause, NotificationType.WARNING, listener)), ModalityState.NON_MODAL);
+      ApplicationManager.getApplication().invokeLater(
+        () -> Notifications.Bus.notify(group.createNotification(title, cause, NotificationType.WARNING, listener)), ModalityState.NON_MODAL);
     }
   }
 
