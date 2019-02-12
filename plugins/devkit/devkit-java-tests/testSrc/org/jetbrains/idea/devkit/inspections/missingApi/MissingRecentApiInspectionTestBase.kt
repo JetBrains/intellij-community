467dc32d73aaf739b623b96870009c3ff917904a
Yann Cébron <yann.cebron@jetbrains.com>
2019-02-12 15:45:11 +0100

    [devkit] MissingRecentApiInspectionTestBase: better tearDown()

--- plugins/devkit/devkit-java-tests/testSrc/org/jetbrains/idea/devkit/inspections/missingApi/MissingRecentApiInspectionTestBase.kt
+++ plugins/devkit/devkit-java-tests/testSrc/org/jetbrains/idea/devkit/inspections/missingApi/MissingRecentApiInspectionTestBase.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.devkit.inspections.missingApi
 
 import com.intellij.codeInsight.AnnotationUtil
@@ -38,6 +38,9 @@ abstract class MissingRecentApiInspectionTestBase : PluginModuleTestCase() {
       //Dispose IDEA module or JDK and all attached roots.
       PluginProjectWithIdeaLibraryDescriptor.disposeIdeaLibrary(project)
       PluginProjectWithIdeaJdkDescriptor.disposeIdeaJdk()
+    }
+    catch (e: Throwable) {
+      addSuppressedException(e)
     }
     finally {
       super.tearDown()
