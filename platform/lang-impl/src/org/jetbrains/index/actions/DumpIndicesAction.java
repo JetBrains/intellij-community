4d49b3d321772f68b4c863f454f69daf7192b25e
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2019-02-12 12:01:30 +0300

    cleanup

--- platform/lang-impl/src/org/jetbrains/index/actions/DumpIndicesAction.java
+++ platform/lang-impl/src/org/jetbrains/index/actions/DumpIndicesAction.java
@@ -1,41 +1,22 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.index.actions;
 
-import com.google.common.hash.HashCode;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.fileChooser.FileChooser;
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
-import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Ref;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
-import com.intellij.psi.stubs.FileContentHashing;
-import com.intellij.psi.stubs.HashCodeDescriptor;
-import com.intellij.psi.stubs.StubUpdatingIndex;
-import com.intellij.util.indexing.*;
-import com.intellij.util.io.DataExternalizer;
-import com.intellij.util.io.DataInputOutputUtil;
-import com.intellij.util.io.PersistentHashMap;
-import gnu.trove.THashMap;
-import gnu.trove.THashSet;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.index.IndexGeneratorKt;
 import org.jetbrains.index.OnDiskContentHashVerifier;
 import org.jetbrains.index.ProjectContentPrebuiltIndexer;
 
-import java.io.DataInput;
-import java.io.DataOutput;
 import java.io.File;
-import java.io.IOException;
-import java.util.Map;
-import java.util.Set;
 
 public class DumpIndicesAction extends AnAction {
   public DumpIndicesAction() {
@@ -48,7 +29,6 @@ public class DumpIndicesAction extends AnAction {
     if (project == null) return;
     VirtualFile dir = FileChooser.chooseFile(FileChooserDescriptorFactory.createSingleFolderDescriptor(), project, null);
     if (dir == null) return;
-    PsiManager psiManager = PsiManager.getInstance(project);
 
     ProgressManager.getInstance().run(new Task.Modal(project, "Dumping Indices...", false) {
       @Override
