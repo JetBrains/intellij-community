8f5a7e2f78d781fdb1c17cd2cde520b990f5d05e
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2019-02-13 18:34:31 +0300

    svn: Convert "MergeInfoCached" to kotlin

--- plugins/svn4idea/src/org/jetbrains/idea/svn/mergeinfo/MergeInfoCached.kt
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/mergeinfo/MergeInfoCached.kt
@@ -1,38 +1,12 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.idea.svn.mergeinfo;
+package org.jetbrains.idea.svn.mergeinfo
 
-import com.intellij.openapi.vcs.versionBrowser.CommittedChangeList;
-import com.intellij.util.containers.ContainerUtil;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.vcs.versionBrowser.CommittedChangeList
 
-import java.util.Map;
+class MergeInfoCached(map: Map<Long, MergeCheckResult> = emptyMap(), private val copyRevision: Long = -1L) {
+  val map: MutableMap<Long, MergeCheckResult> = map.toMutableMap()
 
-public class MergeInfoCached {
+  fun copy(): MergeInfoCached = MergeInfoCached(map, copyRevision)
 
-  @NotNull private final Map<Long, MergeCheckResult> myMap;
-  private final long myCopyRevision;
-
-  public MergeInfoCached() {
-    myMap = ContainerUtil.newHashMap();
-    myCopyRevision = -1;
-  }
-
-  public MergeInfoCached(@NotNull Map<Long, MergeCheckResult> map, long copyRevision) {
-    myMap = ContainerUtil.newHashMap(map);
-    myCopyRevision = copyRevision;
-  }
-
-  @NotNull
-  public Map<Long, MergeCheckResult> getMap() {
-    return myMap;
-  }
-
-  @NotNull
-  public MergeInfoCached copy() {
-    return new MergeInfoCached(myMap, myCopyRevision);
-  }
-
-  public boolean copiedAfter(@NotNull CommittedChangeList list) {
-    return myCopyRevision != -1 && myCopyRevision >= list.getNumber();
-  }
+  fun copiedAfter(list: CommittedChangeList): Boolean = copyRevision != -1L && copyRevision >= list.number
 }
