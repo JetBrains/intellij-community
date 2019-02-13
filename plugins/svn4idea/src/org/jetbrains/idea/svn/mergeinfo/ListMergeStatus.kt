b9904bc14e592c7641314a0c66fe0f7350ed69a1
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2019-02-13 16:06:52 +0300

    svn: Convert "ListMergeStatus" to kotlin

--- plugins/svn4idea/src/org/jetbrains/idea/svn/mergeinfo/ListMergeStatus.kt
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/mergeinfo/ListMergeStatus.kt
@@ -1,49 +1,24 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.idea.svn.mergeinfo;
+package org.jetbrains.idea.svn.mergeinfo
 
-import com.intellij.icons.AllIcons;
-import icons.SvnIcons;
-import org.jetbrains.annotations.Contract;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.icons.AllIcons
+import icons.SvnIcons
+import javax.swing.Icon
 
-import javax.swing.*;
-
-public enum ListMergeStatus {
+enum class ListMergeStatus(val icon: Icon?) {
   COMMON(SvnIcons.Common),
   MERGED(SvnIcons.Integrated),
   NOT_MERGED(SvnIcons.Notintegrated),
   ALIEN(null),
   REFRESHING(AllIcons.Nodes.Unknown);
 
-  @Nullable
-  private final Icon myIcon;
-
-  ListMergeStatus(@Nullable final Icon icon) {
-    myIcon = icon;
-  }
-
-  @Nullable
-  public Icon getIcon() {
-    return myIcon;
-  }
-
-  @Contract(value = "null -> null; !null -> !null", pure = true)
-  public static ListMergeStatus from(@Nullable MergeCheckResult mergeCheckResult) {
-    ListMergeStatus result = null;
-
-    if (mergeCheckResult != null) {
-      switch (mergeCheckResult) {
-        case MERGED:
-          result = MERGED;
-          break;
-        case COMMON:
-          result = COMMON;
-          break;
-        default:
-          result = NOT_MERGED;
-      }
+  companion object {
+    @JvmStatic
+    fun from(mergeCheckResult: MergeCheckResult?): ListMergeStatus? = when (mergeCheckResult) {
+      MergeCheckResult.MERGED -> MERGED
+      MergeCheckResult.COMMON -> COMMON
+      null -> null
+      else -> NOT_MERGED
     }
-
-    return result;
   }
 }
