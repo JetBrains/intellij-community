02e34d40cf9f6cde7fb79357f172499ba64bd252
Viktoria.Shirunova <viktoria.shirunova@jetbrains.com>
2019-02-13 17:24:25 +0300

    [gui-test] GuiTestCase.findDialog: added possibility to search with predicate

--- platform/testGuiFramework/src/com/intellij/testGuiFramework/impl/GuiTestCase.kt
+++ platform/testGuiFramework/src/com/intellij/testGuiFramework/impl/GuiTestCase.kt
@@ -420,9 +420,10 @@ open class GuiTestCase {
   /**
    * Finds JDialog with a specific title (if title is null showing dialog should be only one)
    */
-  private fun findDialog(title: String?, ignoreCaseTitle: Boolean, timeout: Timeout): JDialog =
+  fun findDialog(title: String?, ignoreCaseTitle: Boolean, timeout: Timeout, predicate: FinderPredicate = Predicate.equality): JDialog =
     waitUntilFound(null, JDialog::class.java, timeout) {
-      title?.equals(it.title, ignoreCaseTitle)?.and(it.isShowing && it.isEnabled && it.isVisible) ?: true
+      it.isShowing && it.isEnabled && it.isVisible
+      && if(title != null) predicate(it.title, title) else true
     }
 
   fun exists(fixture: () -> AbstractComponentFixture<*, *, *>): Boolean {
