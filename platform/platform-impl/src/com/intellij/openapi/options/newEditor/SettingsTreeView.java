019b9b50249ac080b6ed026d7720900d4d97515d
Kirill Kirichenko <kirill.kirichenko@jetbrains.com>
2019-02-13 11:30:23 +0300

    IDEA-198424 Add keys: changed items
    (cherry picked from commit 3db31139831044a78d50bc638c6bf30724b4b962)

--- platform/platform-impl/src/com/intellij/openapi/options/newEditor/SettingsTreeView.java
+++ platform/platform-impl/src/com/intellij/openapi/options/newEditor/SettingsTreeView.java
@@ -60,9 +60,8 @@ import java.util.*;
 public class SettingsTreeView extends JComponent implements Accessible, Disposable, OptionsEditorColleague {
   private static final int ICON_GAP = 5;
   private static final String NODE_ICON = "settings.tree.view.icon";
-  private static final Color WRONG_CONTENT = JBColor.RED;
-  private static final Color MODIFIED_CONTENT = JBColor.BLUE;
-  public static final Color FOREGROUND = new JBColor(Gray.x1A, Gray.xBB);
+  private static final Color WRONG_CONTENT = JBColor.namedColor("Tree.errorForeground", JBColor.RED);
+  private static final Color MODIFIED_CONTENT = JBColor.namedColor("Tree.modifiedItemForeground", JBColor.BLUE);
 
   final SimpleTree myTree;
   final FilteringTreeBuilder myBuilder;
@@ -123,7 +122,7 @@ public class SettingsTreeView extends JComponent implements Accessible, Disposab
         }
         if (myHeader == null) {
           myHeader = new JLabel();
-          myHeader.setForeground(FOREGROUND);
+          myHeader.setForeground(UIUtil.getTreeForeground());
           myHeader.setIconTextGap(ICON_GAP);
           myHeader.setBorder(BorderFactory.createEmptyBorder(1, 10 + getLeftMargin(0), 0, 0));
         }
@@ -605,7 +604,7 @@ public class SettingsTreeView extends JComponent implements Accessible, Disposab
       myTextLabel.setFont(isGroup ? myTree.getFont() : UIUtil.getLabelFont());
 
       // update font color for modified configurables
-      myTextLabel.setForeground(selected ? UIUtil.getTreeSelectionForeground() : FOREGROUND);
+      myTextLabel.setForeground(selected ? UIUtil.getTreeSelectionForeground(true) : UIUtil.getTreeForeground());
       if (!selected && node != null) {
         Configurable configurable = node.myConfigurable;
         if (configurable != null) {
