c66283865122bdd1c32f8909b3fed9de99352162
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-02-14 12:58:18 +0300

    IDEA-CR-43343: [groovy] convert GroovyResolveCacheTest to JUnit 4
    (cherry picked from commit 6d13b4d45e88862a7b594c080a9147aa3eb6ca6b)

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/resolve/GroovyResolveCacheTest.groovy
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/resolve/GroovyResolveCacheTest.groovy
@@ -3,29 +3,27 @@ package org.jetbrains.plugins.groovy.lang.resolve
 
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.psi.PsiFileFactory
-import com.intellij.testFramework.LightProjectDescriptor
 import groovy.transform.CompileStatic
 import org.jetbrains.plugins.groovy.GroovyFileType
-import org.jetbrains.plugins.groovy.GroovyLightProjectDescriptor
-import org.jetbrains.plugins.groovy.LightGroovyTestCase
 import org.jetbrains.plugins.groovy.lang.psi.GroovyFile
 import org.jetbrains.plugins.groovy.lang.psi.GroovyPsiElementFactory
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrNewExpression
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.members.GrMethod
 import org.jetbrains.plugins.groovy.transformations.AstTransformationSupport
+import org.jetbrains.plugins.groovy.util.GroovyLatestTest
+import org.junit.Test
 
 import static com.intellij.codeInsight.AnnotationUtil.findAnnotationInHierarchy
 
 @CompileStatic
-class GroovyResolveCacheTest extends LightGroovyTestCase {
+class GroovyResolveCacheTest extends GroovyLatestTest {
 
-  final LightProjectDescriptor projectDescriptor = GroovyLightProjectDescriptor.GROOVY_LATEST
-
+  @Test
   void 'test drop caches on out of code block change'() {
     int counter = 0
     AstTransformationSupport.EP_NAME.getPoint(null).registerExtension({
       counter++
-    } as AstTransformationSupport, myFixture.testRootDisposable)
+    } as AstTransformationSupport, fixture.testRootDisposable)
     def file = fixture.addFileToProject(
       '_.groovy', 'class Super { def foo() {} }'
     ) as GroovyFile
@@ -35,7 +33,7 @@ class GroovyResolveCacheTest extends LightGroovyTestCase {
     assert clazz.methods.size() == 6
     assert counter == 1
 
-    WriteCommandAction.runWriteCommandAction(project) {
+    WriteCommandAction.runWriteCommandAction(fixture.project) {
       clazz.methods.first().delete()
     }
 
@@ -44,11 +42,12 @@ class GroovyResolveCacheTest extends LightGroovyTestCase {
     assert counter == 2
   }
 
+  @Test
   void 'test do not drop caches on code block change'() {
     int counter = 0
     AstTransformationSupport.EP_NAME.getPoint(null).registerExtension({
       counter++
-    } as AstTransformationSupport, myFixture.testRootDisposable)
+    } as AstTransformationSupport, fixture.testRootDisposable)
     def file = fixture.addFileToProject(
       '_.groovy', 'class Super { def foo() { 1 } }'
     ) as GroovyFile
@@ -59,7 +58,7 @@ class GroovyResolveCacheTest extends LightGroovyTestCase {
     assert (clazz.methods.first() as GrMethod).block.statements.size() == 1
     assert counter == 1
 
-    WriteCommandAction.runWriteCommandAction(project) {
+    WriteCommandAction.runWriteCommandAction(fixture.project) {
       (clazz.methods.first() as GrMethod).block.statements.first().delete()
     }
 
@@ -69,14 +68,15 @@ class GroovyResolveCacheTest extends LightGroovyTestCase {
     assert counter == 1
   }
 
+  @Test
   void 'test drop caches on non physical context change'() {
-    def fileFactory = PsiFileFactory.getInstance(project)
+    def fileFactory = PsiFileFactory.getInstance(fixture.project)
     def file = fileFactory.createFileFromText(
       "a.groovy", GroovyFileType.GROOVY_FILE_TYPE, "class Super { @Deprecated void foo(){} }"
     ) as GroovyFile
     def superClass = file.typeDefinitions.first()
 
-    def factory = GroovyPsiElementFactory.getInstance(project)
+    def factory = GroovyPsiElementFactory.getInstance(fixture.project)
     def expression = factory.createExpressionFromText("new Super() { void foo(){} }", superClass) as GrNewExpression
     def subClass = expression.anonymousClassDefinition
 
