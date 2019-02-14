96a69ff78b3bd54f20a5e5cbfd0ace64f0e5de68
Dmitriy.Panov <Dmitriy.Panov@jetbrains.com>
2019-02-14 14:18:14 +0300

    IDEA-CR-43389: IJI-117: jdkBuild property format is changed to <optional_version+update_number_in_any_format>b<build_number> (to support promotion  through TC)
    (cherry picked from commit 8f2b255751860562352374270a938f11260edfa0)

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/MacDistributionBuilder.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/MacDistributionBuilder.groovy
@@ -116,8 +116,8 @@ class MacDistributionBuilder extends OsSpecificDistributionBuilder {
         buildContext.executeStep("Build .dmg artifact for macOS", BuildOptions.MAC_DMG_STEP) {
           MacDmgBuilder.signAndBuildDmg(buildContext, customizer, buildContext.proprietaryBuildTools.macHostProperties, macZipPath)
           if (secondJreBuild != null) {
-            def seconJreVersion = buildContext.bundledJreManager.getSecondJreVersion()
-            def jreArchive = "jbrsdk-${seconJreVersion}${buildContext.bundledJreManager.jreArchiveSuffix(secondJreBuild, JvmArchitecture.x64, 'osx')}"
+            def secondJreVersion = buildContext.bundledJreManager.getSecondJreVersion()
+            def jreArchive = "jbrsdk-${buildContext.bundledJreManager.jreArchiveSuffix(secondJreBuild, secondJreVersion, JvmArchitecture.x64, 'osx')}"
             File archive = new File(buildContext.bundledJreManager.jreDir(), jreArchive)
             if (archive.file) {
               MacDmgBuilder.signAndBuildDmg(buildContext, customizer, buildContext.proprietaryBuildTools.macHostProperties, macZipPath, archive.absolutePath)
