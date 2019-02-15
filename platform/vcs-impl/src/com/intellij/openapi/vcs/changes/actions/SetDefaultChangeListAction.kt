9d26c395d26c100b59d1647931d57e9d90145c80
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2019-02-15 14:54:33 +0300

    vcs: Convert "SetDefaultChangeListAction" to kotlin

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/SetDefaultChangeListAction.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/SetDefaultChangeListAction.kt
@@ -1,36 +1,29 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
-package com.intellij.openapi.vcs.changes.actions;
+package com.intellij.openapi.vcs.changes.actions
 
-import com.intellij.openapi.actionSystem.ActionPlaces;
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.VcsDataKeys;
-import com.intellij.openapi.vcs.changes.ChangeList;
-import com.intellij.openapi.vcs.changes.ChangeListManager;
-import com.intellij.openapi.vcs.changes.LocalChangeList;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.actionSystem.ActionPlaces
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.vcs.VcsDataKeys
+import com.intellij.openapi.vcs.changes.ChangeListManager
+import com.intellij.openapi.vcs.changes.LocalChangeList
 
-public class SetDefaultChangeListAction extends AnAction implements DumbAware {
-  @Override
-  public void update(@NotNull AnActionEvent e) {
-    ChangeList[] lists = e.getData(VcsDataKeys.CHANGE_LISTS);
-    final boolean visible =
-      lists != null && lists.length == 1 && lists[0] instanceof LocalChangeList && !((LocalChangeList)lists[0]).isDefault();
-    e.getPresentation().setEnabled(visible);
-    if (e.getPlace().equals(ActionPlaces.CHANGES_VIEW_POPUP)) {
-      e.getPresentation().setVisible(visible);
+class SetDefaultChangeListAction : AnAction(), DumbAware {
+  override fun update(e: AnActionEvent) {
+    val lists = e.getData(VcsDataKeys.CHANGE_LISTS)
+    val visible = lists != null && lists.size == 1 && lists[0] is LocalChangeList && !(lists[0] as LocalChangeList).isDefault
+    e.presentation.isEnabled = visible
+    if (e.place == ActionPlaces.CHANGES_VIEW_POPUP) {
+      e.presentation.isVisible = visible
     }
   }
 
-  @Override
-  public void actionPerformed(@NotNull AnActionEvent e) {
-    Project project = e.getData(CommonDataKeys.PROJECT);
-    final ChangeList[] lists = e.getData(VcsDataKeys.CHANGE_LISTS);
-    assert lists != null;
-    ChangeListManager.getInstance(project).setDefaultChangeList((LocalChangeList)lists[0]);
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.getData(CommonDataKeys.PROJECT)
+    val lists = e.getData(VcsDataKeys.CHANGE_LISTS)!!
+    ChangeListManager.getInstance(project!!).defaultChangeList = lists[0] as LocalChangeList
   }
 }
