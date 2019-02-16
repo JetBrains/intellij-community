4ab187e8b2c4688a8a26b95039694a7e7f7352ac
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2019-02-16 17:50:30 +0300

    vcs-log: remove unused constructor

--- platform/vcs-log/impl/src/com/intellij/vcs/log/visible/VisiblePackRefresherImpl.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/visible/VisiblePackRefresherImpl.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.vcs.log.visible;
 
 import com.intellij.openapi.Disposable;
@@ -43,14 +43,6 @@ public class VisiblePackRefresherImpl implements VisiblePackRefresher, Disposabl
   @NotNull private final List<VisiblePackChangeListener> myVisiblePackChangeListeners = ContainerUtil.createLockFreeCopyOnWriteList();
 
   @NotNull private volatile State myState;
-
-  public VisiblePackRefresherImpl(@NotNull Project project,
-                                  @NotNull VcsLogData logData,
-                                  @NotNull PermanentGraph.SortType initialSortType,
-                                  @NotNull VcsLogFilterer builder,
-                                  @NotNull String logId) {
-    this(project, logData, VcsLogFilterObject.collection(), initialSortType, builder, logId);
-  }
 
   public VisiblePackRefresherImpl(@NotNull Project project,
                                   @NotNull VcsLogData logData,
