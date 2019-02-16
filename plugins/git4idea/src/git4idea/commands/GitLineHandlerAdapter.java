6468efa9c1fba9691bfefd16d1f5dcabc3a01a13
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2019-02-16 17:42:50 +0300

    git: clarify the deprecation reason and mention the alternative.

--- plugins/git4idea/src/git4idea/commands/GitLineHandlerAdapter.java
+++ plugins/git4idea/src/git4idea/commands/GitLineHandlerAdapter.java
@@ -1,18 +1,14 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package git4idea.commands;
 
 import com.intellij.openapi.util.Key;
 
 /**
- * An adapter for line handler
+ * @deprecated Use the {@link GitLineHandlerListener}.
  */
 @Deprecated
 public class GitLineHandlerAdapter implements GitLineHandlerListener {
-  /**
-   * {@inheritDoc}
-   */
   @Override
   public void onLineAvailable(final String line, final Key outputType) {
-    // do nothing
   }
 }
