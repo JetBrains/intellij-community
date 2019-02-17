a82d09f74ea7be2f2c0c29ea545b85088f07ffe3
Artem Bochkarev <artem.bochkarev@jetbrains.com>
2019-02-17 18:11:35 +0300

    touchbar: load scrubber items by parts (to speedup creation)
    for example, git-branches popup of IDEA-project has thousands of items

--- native/MacTouchBar/src/Scrubber.m
+++ native/MacTouchBar/src/Scrubber.m
@@ -11,7 +11,10 @@ static const int g_interItemSpacings = 5;
 @interface ScrubberItem : NSObject
 @property (retain, nonatomic) NSString * text;
 @property (retain, nonatomic) NSImage * img;
-@property (nonatomic) execute jaction;
+@property (nonatomic) int index;
+@property (nonatomic) int positionInsideScrubber;
+@property (nonatomic) bool visible;
+@property (nonatomic) bool enabled;
 @end
 
 @implementation ScrubberItem
@@ -21,49 +24,44 @@ static const int g_interItemSpacings = 5;
 {
     ScrubberItemView * _lastSelected;
 }
-@property (retain, nonatomic) NSMutableArray * items;
+@property (retain, nonatomic) NSMutableArray * itemsCache;
+@property (retain, nonatomic) NSMutableArray * visibleItems;
+@property (nonatomic) executeScrubberItem delegate;
+@property (nonatomic) updateScrubberCache updateCache;
 @end
-
-static NSMutableArray * _convertItems(ScrubberItemData * items, int count) {
-    NSMutableArray * nsarray = [[[NSMutableArray alloc] initWithCapacity:count] autorelease];
-    for (int c = 0; c < count; ++c) {
-        ScrubberItem * si = [[[ScrubberItem alloc] init] autorelease];
-        si.text = createString(&items[c]);
-        si.img = createImg(&items[c]);
-        si.jaction = items[c].action;
-        [nsarray addObject:si];
-    }
-    return nsarray;
-}
 
 @implementation NSScrubberContainer
 
 - (NSInteger)numberOfItemsForScrubber:(NSScrubber *)scrubber {
     // NOTE: called from AppKit
-    nstrace(@"scrubber [%@]: items count %d", self.identifier, self.items.count);
-    return self.items.count;
+    nstrace(@"scrubber [%@]: items count %lu, visible %lu", self.identifier, self.itemsCache.count, self.visibleItems.count);
+    return self.visibleItems.count;
 }
 
 - (NSScrubberItemView *)scrubber:(NSScrubber *)scrubber viewForItemAtIndex:(NSInteger)itemIndex {
     // NOTE: called from AppKit-thread (creation when TB becomes visible), uses default autorelease-pool (create before event processing)
-    nstrace(@"scrubber [%@]: create viewForItemAtIndex %d", self.identifier, itemIndex);
+    nstrace(@"scrubber [%@]: create viewForItemAtIndex %lu", self.identifier, itemIndex);
     ScrubberItemView *itemView = [scrubber makeItemWithIdentifier:g_scrubberItemIdentifier owner:nil];
 
-    ScrubberItem * itemData = [self.items objectAtIndex:itemIndex];
+    ScrubberItem * itemData = [self.visibleItems objectAtIndex:itemIndex];
     if (itemData == nil) {
-        nserror(@"scrubber [%@]: null item-data at index %d", self.identifier, itemIndex);
+        nserror(@"scrubber [%@]: null item-data at index %lu", self.identifier, itemIndex);
         return nil;
     }
 
     [itemView setImgAndText:itemData.img text:itemData.text];
+    [itemView setEnabled:itemData.enabled];
+
+    if (itemIndex == self.visibleItems.count - 1)
+        (*self.updateCache)();
     return itemView;
 }
 
 - (NSSize)scrubber:(NSScrubber *)scrubber layout:(NSScrubberFlowLayout *)layout sizeForItemAtIndex:(NSInteger)itemIndex {
     // NOTE: called from AppKit-thread (creation when TB becomes visible), uses default autorelease-pool (create before event processing)
-    ScrubberItem * itemData = [self.items objectAtIndex:itemIndex];
+    ScrubberItem * itemData = [self.visibleItems objectAtIndex:itemIndex];
     if (itemData == nil) {
-        nserror(@"scrubber [%@]: null item-data at index %d", self.identifier, itemIndex);
+        nserror(@"scrubber [%@]: null item-data at index %lu", self.identifier, itemIndex);
         return NSMakeSize(0, 0);
     }
 
@@ -77,34 +75,78 @@ static NSMutableArray * _convertItems(ScrubberItemData * items, int count) {
 }
 
 - (void)scrubber:(NSScrubber *)scrubber didSelectItemAtIndex:(NSInteger)selectedIndex {
-    ScrubberItem * itemData = [self.items objectAtIndex:selectedIndex];
+    ScrubberItem * itemData = [self.visibleItems objectAtIndex:selectedIndex];
     if (itemData == nil) {
-        nserror(@"scrubber [%@]: called didSelectItemAtIndex %d, but item-data at this index is null", self.identifier, selectedIndex);
+        nserror(@"scrubber [%@]: called didSelectItemAtIndex %lu, but item-data at this index is null", self.identifier, selectedIndex);
         return;
     }
 
     NSScrubberItemView * view = [scrubber itemViewForItemAtIndex:selectedIndex];
-    if (view != nil) {
-        if (_lastSelected != nil)
-            [_lastSelected setBackgroundSelected:false];
-
-        _lastSelected = (ScrubberItemView *) view;
-        [_lastSelected setBackgroundSelected:true];
+    if (view == nil) {
+        nserror(@"scrubber [%@]: called didSelectItemAtIndex %lu, but item-view at this index is null", self.identifier, selectedIndex);
+        return;
     }
 
-    nstrace(@"scrubber [%@]: perform action of scrubber item at %d", self.identifier, selectedIndex);
-    (*itemData.jaction)();
+    if (_lastSelected != nil)
+        [_lastSelected setBackgroundSelected:false];
+
+    if (((ScrubberItemView *)view).isEnabled) {
+        _lastSelected = (ScrubberItemView *) view;
+        [_lastSelected setBackgroundSelected:true];
+
+        nstrace(@"scrubber [%@]: perform action of scrubber item at %lu", self.identifier, selectedIndex);
+        (*_delegate)(itemData.index);
+    }
 }
 
 @end
 
+static int _fillCache(NSMutableArray * cache, NSMutableArray * visibleItems, void* items, int byteCount) {
+    const int prevCacheSize = cache.count;
+    const int prevVisibleSize = visibleItems.count;
+    const char * p = items;
+    const int itemsCount = *((int*)p);
+    p += 4;
+    nstrace(@"items count = %d, bytes = %d", itemsCount, byteCount);
+    for (int c = 0; c < itemsCount; ++c) {
+        ScrubberItem * si = [[[ScrubberItem alloc] init] autorelease];
+        const int txtLen = *((int*)p);
+        p += 4;
+        si.text = txtLen == 0 ? [NSString stringWithUTF8String:""] : [NSString stringWithUTF8String:p];
+        p += txtLen + 1;
+        //NSLog(@"\t len=%d, txt=%@, offset=%d", txtLen, si.text, p - (char*)items);
+
+        const int w = *((int*)p);
+        p += 4;
+        const int h = *((int*)p);
+        p += 4;
+        //NSLog(@"\t w=%d, h=%d", w, h);
+
+        si.img = w <= 0 || h <= 0 ? nil : createImgFrom4ByteRGBA((const unsigned char *)p, w, h);
+        si.index = c + prevCacheSize;
+        si.positionInsideScrubber = c + prevVisibleSize;
+        si.visible = true;
+        si.enabled = true;
+        [cache addObject:si];
+        [visibleItems addObject:si];
+
+        p += w*h*4;
+        //NSLog(@"\t offset=%d", p - (char*)items);
+    }
+}
+
 // NOTE: called from AppKit-thread (creation when TB becomes visible), uses default autorelease-pool (create before event processing)
-id createScrubber(const char* uid, int itemWidth, ScrubberItemData * items, int count) {
+id createScrubber(const char* uid, int itemWidth, executeScrubberItem delegate, updateScrubberCache updater, void* packedItems, int byteCount) {
     NSString * nsid = [NSString stringWithUTF8String:uid];
     nstrace(@"create scrubber [%@] (thread: %@)", nsid, [NSThread currentThread]);
 
     NSScrubberContainer * scrubberItem = [[NSScrubberContainer alloc] initWithIdentifier:nsid]; // create non-autorelease object to be owned by java-wrapper
-    scrubberItem.items = _convertItems(items, count);
+    scrubberItem.itemsCache = [[[NSMutableArray alloc] initWithCapacity:10/*empiric average popup items count*/] autorelease];
+    scrubberItem.visibleItems = [[[NSMutableArray alloc] initWithCapacity:10/*empiric average popup items count*/] autorelease];
+    scrubberItem.delegate = delegate;
+    scrubberItem.updateCache = updater;
+
+    _fillCache(scrubberItem.itemsCache, scrubberItem.visibleItems, packedItems, byteCount);
 
     NSScrubber *scrubber = [[[NSScrubber alloc] initWithFrame:NSMakeRect(0, 0, itemWidth, g_heightOfTouchBar)] autorelease];
 
@@ -130,19 +172,104 @@ id createScrubber(const char* uid, int itemWidth, ScrubberItemData * items, int 
     return scrubberItem;
 }
 
-void updateScrubber(id scrubObj, int itemWidth, ScrubberItemData * items, int count) {
-    // NOTE: called from EDT (when update UI)
-    NSScrubberContainer * container = scrubObj; // TODO: check types
-    nstrace(@"async update scrubber [%@] (thread: %@)", container.identifier, [NSThread currentThread]);
-    NSAutoreleasePool * edtPool = [[NSAutoreleasePool alloc] init];
-    NSMutableArray * nsitems = _convertItems(items, count);
+// NOTE: called from AppKit (when show last cached item and need update cache with new items)
+void appendScrubberItems(id scrubObj, void* packedItems, int byteCount) {
+    NSScrubberContainer *container = scrubObj;
+    nstrace(@"scrubber [%@]: called appendScrubberItems", container.identifier);
+
+    const int visibleItemsCountPrev = container.visibleItems.count;
+    _fillCache(container.itemsCache, container.visibleItems, packedItems, byteCount);
+
     dispatch_async(dispatch_get_main_queue(), ^{
-        container.items = nsitems;
-        NSScrubber * scrubber = container.view;     // TODO: check types
-//        nstrace(@"\tinvalidate layout of scrubber [%@] (thread: %@)", container.identifier, [NSThread currentThread]);
-        [scrubber.scrubberLayout invalidateLayout];
-//        nstrace(@"\treload scrubber [%@] (thread: %@)", container.identifier, [NSThread currentThread]);
-        [scrubber reloadData];
+        NSIndexSet *indexSet = [NSIndexSet indexSetWithIndexesInRange:NSMakeRange(visibleItemsCountPrev, container.visibleItems.count - visibleItemsCountPrev)];
+        [container.view insertItemsAtIndexes:indexSet];
     });
-    [edtPool release];
+}
+
+// NOTE: called from EDT (when update UI)
+void enableScrubberItems(id scrubObj, void* itemIndices, int count, bool enabled) {
+    NSScrubberContainer *container = scrubObj;
+    NSScrubber *scrubber = container.view;
+    const int sizeInBytes = sizeof(int)*count;
+    int *indices = malloc(sizeInBytes);
+    memcpy(indices, itemIndices, sizeInBytes);
+
+    dispatch_async(dispatch_get_main_queue(), ^{
+        for (int c = 0; c < count; ++c) {
+            ScrubberItem * itemData = [container.itemsCache objectAtIndex:indices[c]];
+            if (itemData == nil) {
+                nserror(@"scrubber [%@]: called enableScrubberItem %d, but item-data at this index is null", container.identifier, indices[c]);
+                continue;
+            }
+            itemData.enabled = enabled;
+            if (itemData.positionInsideScrubber < 0) {
+                nstrace(@"scrubber [%@]: called enableScrubberItem %d, but item is hidden", container.identifier, indices[c]);
+                continue;
+            }
+
+            NSScrubberItemView *view = [scrubber itemViewForItemAtIndex:itemData.positionInsideScrubber];
+            if (view == nil) {
+                nstrace(@"scrubber [%@]: called enableScrubberItem %d, but item-view at this index is null", container.identifier, indices[c]);
+                continue;
+            }
+            [((ScrubberItemView *) view) setEnabled:enabled];
+        }
+        free(indices);
+    });
+}
+
+// NOTE: called from EDT (when update UI)
+void showScrubberItems(id scrubObj, void* itemIndices, int count, bool show) {
+    NSScrubberContainer * container = scrubObj;
+    NSScrubber *scrubber = container.view;
+
+    const int sizeInBytes = sizeof(int)*count;
+    int *indices = malloc(sizeInBytes);
+    memcpy(indices, itemIndices, sizeInBytes);
+
+    dispatch_async(dispatch_get_main_queue(), ^{
+        // 1. mark items
+        for (int c = 0; c < count; ++c) {
+            ScrubberItem *itemData = [container.itemsCache objectAtIndex:indices[c]];
+            if (itemData == nil) {
+                nserror(@"scrubber [%@]: called showScrubberItems %d, but item-data at this index is null", container.identifier, indices[c]);
+                continue;
+            }
+            itemData.visible = show;
+        }
+
+        // 2. recalculate positions
+        NSMutableIndexSet *indexSet = [[[NSMutableIndexSet alloc] init] autorelease];
+        [container.visibleItems removeAllObjects];
+        int position = 0;
+        for (int c = 0; c < container.itemsCache.count; ++c) {
+            const ScrubberItem * si = [container.itemsCache objectAtIndex:c];
+            if (si == nil)
+                continue;
+            if (si.visible) {
+                if (si.positionInsideScrubber < 0) {
+                    // item is visible now => insert into scrubber
+                    [indexSet addIndex:position];
+                }
+                si.positionInsideScrubber = position++;
+                [container.visibleItems addObject:si];
+            } else {
+                if (si.positionInsideScrubber >= 0) {
+                    // item is hidden now => remove from scrubber
+                    [indexSet addIndex:si.positionInsideScrubber];
+                    si.positionInsideScrubber = -1;
+                }
+            }
+        }
+
+        if (show) {
+            nstrace(@"\t show %d items", [indexSet count]);
+            [scrubber insertItemsAtIndexes:indexSet];
+        } else {
+            nstrace(@"\t hide %d items", [indexSet count]);
+            [scrubber removeItemsAtIndexes:indexSet];
+        }
+
+        free(indices);
+    });
 }
