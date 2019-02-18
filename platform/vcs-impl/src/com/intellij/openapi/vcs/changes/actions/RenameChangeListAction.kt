25e5eb23d1c22f5cf9ca8739eea067640584e8d2
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2019-02-18 15:37:36 +0300

    vcs: Convert "RenameChangeListAction" to kotlin

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/RenameChangeListAction.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/RenameChangeListAction.kt
@@ -1,66 +1,56 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
-package com.intellij.openapi.vcs.changes.actions;
+package com.intellij.openapi.vcs.changes.actions
 
-import com.intellij.openapi.actionSystem.ActionPlaces;
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.VcsDataKeys;
-import com.intellij.openapi.vcs.changes.Change;
-import com.intellij.openapi.vcs.changes.ChangeList;
-import com.intellij.openapi.vcs.changes.ChangeListManager;
-import com.intellij.openapi.vcs.changes.LocalChangeList;
-import com.intellij.openapi.vcs.changes.ui.EditChangelistDialog;
-import com.intellij.util.ArrayUtil;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.actionSystem.ActionPlaces
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.vcs.VcsDataKeys
+import com.intellij.openapi.vcs.changes.ChangeListManager
+import com.intellij.openapi.vcs.changes.LocalChangeList
+import com.intellij.openapi.vcs.changes.ui.EditChangelistDialog
+import com.intellij.util.ArrayUtil
 
-public class RenameChangeListAction extends AnAction implements DumbAware {
+class RenameChangeListAction : AnAction(), DumbAware {
 
-  @Override
-  public void update(@NotNull AnActionEvent e) {
-    LocalChangeList target = getTargetChangeList(e);
-    final boolean visible = target != null && !target.isReadOnly();
-    e.getPresentation().setEnabled(visible);
-    if (e.getPlace().equals(ActionPlaces.CHANGES_VIEW_POPUP)) {
-      e.getPresentation().setVisible(visible);
+  override fun update(e: AnActionEvent) {
+    val target = getTargetChangeList(e)
+    val visible = target != null && !target.isReadOnly
+    e.presentation.isEnabled = visible
+    if (e.place == ActionPlaces.CHANGES_VIEW_POPUP) {
+      e.presentation.isVisible = visible
     }
   }
 
-  @Override
-  public void actionPerformed(@NotNull AnActionEvent e) {
-    Project project = e.getData(CommonDataKeys.PROJECT);
-    LocalChangeList target = getTargetChangeList(e);
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.getData(CommonDataKeys.PROJECT)
+    val target = getTargetChangeList(e)
     if (target != null) {
-      new EditChangelistDialog(project, target).show();
+      EditChangelistDialog(project, target).show()
     }
   }
 
-  @Nullable
-  private static LocalChangeList getTargetChangeList(@NotNull AnActionEvent e) {
-    Project project = e.getProject();
-    if (project == null) return null;
-    ChangeList[] lists = e.getData(VcsDataKeys.CHANGE_LISTS);
+  private fun getTargetChangeList(e: AnActionEvent): LocalChangeList? {
+    val project = e.project ?: return null
+    val lists = e.getData(VcsDataKeys.CHANGE_LISTS)
     if (!ArrayUtil.isEmpty(lists)) {
-      if (lists.length == 1) {
-        return ChangeListManager.getInstance(project).findChangeList(lists[0].getName());
+      return if (lists!!.size == 1) {
+        ChangeListManager.getInstance(project).findChangeList(lists[0].name)
       }
-      return null;
+      else null
     }
-    Change[] changes = e.getData(VcsDataKeys.CHANGES);
-    if (changes == null) return null;
+    val changes = e.getData(VcsDataKeys.CHANGES) ?: return null
 
-    LocalChangeList result = null;
-    for (Change change : changes) {
-      LocalChangeList cl = ChangeListManager.getInstance(project).getChangeList(change);
+    var result: LocalChangeList? = null
+    for (change in changes) {
+      val cl = ChangeListManager.getInstance(project).getChangeList(change)
       if (result == null)
-        result = cl;
-      else if (cl != null && !cl.equals(result))
-        return null;
+        result = cl
+      else if (cl != null && cl != result)
+        return null
     }
-    return result;
+    return result
   }
 }
