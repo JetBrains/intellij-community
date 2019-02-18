d3f780a0549f8fc4e94710c9f2151ab6c514a6cf
alexey.afanasiev <alexey.afanasiev@jetbrains.com>
2019-02-18 13:33:59 +0300

    [groovy] IDEA-CR-43481 IDEA-202693 Formatter for lambda expressions

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/formatter/LambdaFormatterTest.groovy
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/formatter/LambdaFormatterTest.groovy
@@ -1,102 +1,45 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.lang.formatter
 
-import com.intellij.psi.codeStyle.CommonCodeStyleSettings
-import org.jetbrains.plugins.groovy.GroovyFileType
-import org.jetbrains.plugins.groovy.codeStyle.GroovyCodeStyleSettings
 import org.jetbrains.plugins.groovy.util.TestUtils
 
 class LambdaFormatterTest extends GroovyFormatterTestCase {
 
-  final String basePath = TestUtils.testDataPath + "groovy/formatter/lambda"
+  final String basePath = TestUtils.testDataPath + "groovy/formatter/lambda/"
 
-  void testBraceStyle1() {
-    groovySettings.KEEP_SIMPLE_LAMBDAS_IN_ONE_LINE = false
-    groovySettings.LAMBDA_BRACE_STYLE = CommonCodeStyleSettings.END_OF_LINE
-    doTest()
-  }
+  void testBraceStyle1() { doTest() }
 
-  void testBraceStyle2() {
-    groovySettings.KEEP_SIMPLE_LAMBDAS_IN_ONE_LINE = false
-    groovySettings.LAMBDA_BRACE_STYLE = CommonCodeStyleSettings.NEXT_LINE
-    doTest()
-  }
+  void testBraceStyle2() { doTest() }
 
-  void testBraceStyle3() {
-    groovySettings.KEEP_SIMPLE_LAMBDAS_IN_ONE_LINE = false
-    groovySettings.LAMBDA_BRACE_STYLE = CommonCodeStyleSettings.NEXT_LINE_IF_WRAPPED
-    doTest()
-  }
+  void testBraceStyle3() { doTest() }
 
-  void testBraceStyle4() {
-    groovySettings.KEEP_SIMPLE_LAMBDAS_IN_ONE_LINE = false
-    groovySettings.LAMBDA_BRACE_STYLE = CommonCodeStyleSettings.NEXT_LINE_SHIFTED
-    doTest()
-  }
+  void testBraceStyle4() { doTest() }
 
-  void testBraceStyle5() {
-    groovySettings.KEEP_SIMPLE_LAMBDAS_IN_ONE_LINE = false
-    groovySettings.LAMBDA_BRACE_STYLE = CommonCodeStyleSettings.NEXT_LINE_SHIFTED2
-    doTest()
-  }
+  void testBraceStyle5() { doTest() }
 
-  void testBraceStyle6() {
-    groovySettings.KEEP_SIMPLE_LAMBDAS_IN_ONE_LINE = false
-    groovySettings.LAMBDA_BRACE_STYLE = CommonCodeStyleSettings.NEXT_LINE
-    groovySettings.SPACE_AROUND_LAMBDA_ARROW = false
-    doTest()
-  }
+  void testBraceStyle6() { doTest() }
 
-  void testBraceStyle7() {
-    groovySettings.LAMBDA_BRACE_STYLE = CommonCodeStyleSettings.NEXT_LINE
-    groovySettings.SPACE_AROUND_LAMBDA_ARROW = false
-    groovySettings.KEEP_SIMPLE_LAMBDAS_IN_ONE_LINE = true
-    doTest()
-  }
+  void testBraceStyle7() { doTest() }
 
-  void testBraceStyle8() {
-    groovySettings.LAMBDA_BRACE_STYLE = CommonCodeStyleSettings.NEXT_LINE
-    groovySettings.SPACE_AROUND_LAMBDA_ARROW = false
-    groovySettings.SPACE_WITHIN_BRACES = false
-    doTest()
-  }
+  void testBraceStyle8() { doTest() }
 
-  void testOneLineLambda1() {
-    doTest()
-  }
+  void testOneLineLambda1() { doTest() }
 
-  void testOneLineLambda2() {
-    doTest()
-  }
+  void testOneLineLambda2() { doTest() }
 
-  void testOneLineLambda3() {
-    doTest()
-  }
+  void testOneLineLambda3() { doTest() }
 
-  void testOneLineLambda4() {
-    doTest()
-  }
+  void testOneLineLambda4() { doTest() }
 
-  void testOneLineLambda5() {
-    doTest()
-  }
+  void testOneLineLambda5() { doTest() }
 
-  void testOneLineLambda6() {
-    groovySettings.SPACE_AROUND_LAMBDA_ARROW = false
-    doTest()
-  }
+  void testOneLineLambda6() { doTest() }
 
-  void testOneLineLambda7() {
-    groovySettings.LAMBDA_BRACE_STYLE = CommonCodeStyleSettings.NEXT_LINE
-    doTest()
-  }
+  void testOneLineLambda7() { doTest() }
 
   void testParams() { doTest() }
 
-  void testLambdaParametersAligned() {
-    groovySettings.ALIGN_MULTILINE_PARAMETERS_IN_CALLS = true
-    doTest()
-  }
+  void testLambdaParametersAligned() { doTest() }
 
   void testAlignLambdaBraceWithCall() { doTest() }
 
@@ -112,102 +55,39 @@ class LambdaFormatterTest extends GroovyFormatterTestCase {
 
   void testChainCall5() { doTest() }
 
-  void testChainCall6() {
-    doTest()
-  }
+  void testChainCall6() { doTest() }
 
-  void testChainCall7() {
-    groovySettings.KEEP_SIMPLE_LAMBDAS_IN_ONE_LINE = false
-    doTest()
-  }
+  void testChainCall7() { doTest() }
 
-  void testChainCallWithSingleExpressionLambda1() {
-    doTest()
-  }
+  void testChainCallWithSingleExpressionLambda1() { doTest() }
 
-  void testChainCallWithSingleExpressionLambda2() {
-    getGroovySettings().KEEP_LINE_BREAKS = false
-    doTest()
-  }
+  void testChainCallWithSingleExpressionLambda2() { doTest() }
 
-  void testNoFlyingGeese() {
-    myTempSettings.getCustomSettings(GroovyCodeStyleSettings.class).SPACE_IN_NAMED_ARGUMENT = false
-    doTest()
-  }
+  void testNoFlyingGeese() { doTest() }
 
-  void testNoFlyingGeese2() {
-    getGroovySettings().LAMBDA_BRACE_STYLE = CommonCodeStyleSettings.NEXT_LINE
-    doTest()
-  }
+  void testNoFlyingGeese2() { doTest() }
 
-  void testSpacesAroundArrow1() {
-    groovySettings.KEEP_SIMPLE_LAMBDAS_IN_ONE_LINE = false
-    doTest()
-  }
+  void testSpacesAroundArrow1() { doTest() }
 
-  void testSpacesAroundArrow2() {
-    doTest()
-  }
+  void testSpacesAroundArrow2() { doTest() }
 
-  void testSpacesAroundArrow3() {
-    groovySettings.KEEP_SIMPLE_LAMBDAS_IN_ONE_LINE = false
-    getGroovySettings().LAMBDA_BRACE_STYLE = CommonCodeStyleSettings.NEXT_LINE_SHIFTED
-    doTest()
-  }
+  void testSpacesAroundArrow3() { doTest() }
 
-  void testSpacesAroundArrow4() {
-    getGroovySettings().KEEP_LINE_BREAKS = false
-    doTest()
-  }
+  void testSpacesAroundArrow4() { doTest() }
 
-  void testSpacesAroundArrow5() {
-    doTest()
-  }
+  void testSpacesAroundArrow5() { doTest() }
 
-  void testSpacesAroundArrow6() {
-    groovySettings.KEEP_SIMPLE_LAMBDAS_IN_ONE_LINE = false
-    groovySettings.SPACE_AROUND_LAMBDA_ARROW = false
-    doTest()
-  }
+  void testSpacesAroundArrow6() { doTest() }
 
-  void testSpacesAroundArrow7() {
-    groovySettings.SPACE_AROUND_LAMBDA_ARROW = false
-    doTest()
-  }
+  void testSpacesAroundArrow7() { doTest() }
 
-  void testSpacesAroundArrow8() {
-    groovySettings.KEEP_SIMPLE_LAMBDAS_IN_ONE_LINE = false
-    groovySettings.LAMBDA_BRACE_STYLE = CommonCodeStyleSettings.NEXT_LINE_SHIFTED2
-    groovySettings.SPACE_AROUND_LAMBDA_ARROW = false
-    doTest()
-  }
+  void testSpacesAroundArrow8() { doTest() }
 
-  void testSpacesAroundArrow9() {
-    getGroovySettings().LAMBDA_BRACE_STYLE = CommonCodeStyleSettings.NEXT_LINE
-    groovySettings.SPACE_AROUND_LAMBDA_ARROW = false
-    doTest()
-  }
+  void testSpacesAroundArrow9() { doTest() }
 
-  void testSpacesAroundArrow10() {
-    groovySettings.SPACE_AROUND_LAMBDA_ARROW = false
-    doTest()
-  }
+  void testSpacesAroundArrow10() { doTest() }
 
-  void doTest() {
-    doTest(getTestName(true).replace('$', '/') + ".test")
-  }
-
-  protected void doTest(String fileName) {
-    String path = testDataPath + "/" + fileName
-    def (String input) = TestUtils.readInput(path)
-    checkParsing(input, fileName)
-  }
-
-  protected void checkParsing(String input, String path) {
-    myFixture.configureByText(GroovyFileType.GROOVY_FILE_TYPE, input)
-    doFormat(myFixture.getFile())
-    final String prefix = input + '\n-----\n'
-    myFixture.configureByText('test.txt', prefix + myFixture.getFile().getText())
-    myFixture.checkResultByFile(path, false)
+  private doTest() {
+    doTest(getTestName(true) + ".test")
   }
 }
