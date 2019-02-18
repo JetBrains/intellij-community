2f3178f77853978c1fcd7c95b6a03a2c63ae06c9
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2019-02-18 19:42:48 +0300

    Add more tests on skeleton regeneration depending on required_gen_version

---
+++ python/helpers/pycharm_generator_utils/test/data/SkeletonCaching/skeleton_not_regenerated_for_upgraded_generator_without_explicit_version_stamp/before/cache/sha256:mod/mod.py
@@ -0,0 +1,18 @@
+# encoding: utf-8
+# module mod
+# by generator 0.1
+# no doc
+
+# imports
+
+# functions
+
+def func(*args, **kwargs): # real signature unknown
+    """ Docstring. """
+    pass
+
+# no classes
+# variables with complex values
+
+__test__ = {}
+
