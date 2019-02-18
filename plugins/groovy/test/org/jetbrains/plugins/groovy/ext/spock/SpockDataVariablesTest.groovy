887189cb02248be7ac82970ca6bfc165ed2e11d9
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-02-18 11:54:39 +0300

    IDEA-CR-43539: [groovy] spock: add data variables test
    (cherry picked from commit cfbfb46cf3fc75e93e1b4f21db373fea0660c169)

---
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/ext/spock/SpockDataVariablesTest.groovy
@@ -0,0 +1,74 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.plugins.groovy.ext.spock
+
+import groovy.transform.CompileStatic
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.members.GrMethod
+import org.jetbrains.plugins.groovy.util.BaseTest
+import org.junit.Ignore
+import org.junit.Test
+
+import static com.intellij.testFramework.UsefulTestCase.assertContainsElements
+
+@CompileStatic
+class SpockDataVariablesTest extends SpockTestBase implements BaseTest {
+
+  private Map<String, SpockVariableDescriptor> variableMap(String text) {
+    def file = configureByText """\
+class FooSpec extends spock.lang.Specification {
+  def feature() {
+    $text
+  }
+}
+"""
+    def spec = file.typeDefinitions.first()
+    def feature = (GrMethod)spec.findMethodsByName("feature").first()
+    return SpockUtils.getVariableMap(feature)
+  }
+
+  private void testVariableNames(String text, String... names) {
+    def variables = variableMap(text)
+    assert names.length == variables.size()
+    assertContainsElements(variables.values()*.name, names)
+  }
+
+  @Test
+  void 'where block with additional labels'() {
+    testVariableNames 'foo: abc: where: bar = 1', 'bar'
+  }
+
+  @Test
+  void 'derived parameterization'() {
+    testVariableNames 'where: bar = 1', 'bar'
+  }
+
+  @Test
+  void 'simple parameterization'() {
+    testVariableNames 'where: bar << [1]', 'bar'
+  }
+
+  @Test
+  void 'multi parameterization'() {
+    testVariableNames 'where: [bar, baz] << [[1,2]]', 'bar', 'baz'
+  }
+
+  @Ignore
+  @Test
+  void 'empty single column table'() {
+    testVariableNames 'where: bar | _', 'bar'
+  }
+
+  @Test
+  void 'empty multi column table'() {
+    testVariableNames 'where: bar | baz | bad', 'bar', 'baz', 'bad'
+  }
+
+  @Test
+  void 'derived parameterization after table'() {
+    testVariableNames '''\
+where: 
+bar | baz | bad
+1 | 2 | 3
+foo = bar * baz
+''', 'bar', 'baz', 'bad', 'foo'
+  }
+}
