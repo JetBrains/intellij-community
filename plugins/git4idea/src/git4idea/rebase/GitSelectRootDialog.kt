329588c0d8e4d8880d730d1f56a55400ea832191
Aleksey Pivovarov <AMPivovarov@gmail.com>
2019-02-19 14:27:35 +0300

    git: guess current repository from context in root chooser

--- plugins/git4idea/src/git4idea/rebase/GitSelectRootDialog.kt
+++ plugins/git4idea/src/git4idea/rebase/GitSelectRootDialog.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.ComboBox
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.ui.layout.*
+import git4idea.branch.GitBranchUtil
 import git4idea.repo.GitRepository
 import git4idea.util.GitUIUtil
 import javax.swing.JComponent
@@ -20,12 +21,18 @@ class GitSelectRootDialog(project: Project,
 
   init {
     roots.forEach { rootComboBox.addItem(it) }
-    rootComboBox.selectedItem = defaultRoot ?: roots.first()
+    rootComboBox.selectedItem = defaultRoot ?: guessCurrentRepository(project, roots)
     rootComboBox.renderer = GitUIUtil.getRepositoryListCellRenderer()
 
     setTitle(title)
     setOKButtonText(title)
     init()
+  }
+
+  private fun guessCurrentRepository(project: Project, roots: Collection<GitRepository>): GitRepository {
+    val repository = GitBranchUtil.getCurrentRepository(project)
+    if (repository != null && roots.contains(repository)) return repository
+    return roots.first()
   }
 
   override fun createCenterPanel(): JComponent? {
