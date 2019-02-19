64e947165ee4f97f36ad312564b82e5d817f50b3
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-02-19 13:45:12 +0300

    [groovy] convert ImplicitClosureCallPredicate to kt

--- plugins/groovy/src/org/jetbrains/plugins/groovy/intentions/closure/ImplicitClosureCallPredicate.kt
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/intentions/closure/ImplicitClosureCallPredicate.kt
@@ -1,49 +1,25 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.jetbrains.plugins.groovy.intentions.closure;
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.plugins.groovy.intentions.closure
 
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiType;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.plugins.groovy.intentions.base.PsiElementPredicate;
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrExpression;
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.path.GrMethodCallExpression;
-import org.jetbrains.plugins.groovy.lang.psi.util.ErrorUtil;
-import org.jetbrains.plugins.groovy.lang.psi.util.GroovyCommonClassNames;
+import com.intellij.psi.PsiElement
+import org.jetbrains.plugins.groovy.intentions.base.PsiElementPredicate
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.path.GrMethodCallExpression
+import org.jetbrains.plugins.groovy.lang.psi.util.ErrorUtil
+import org.jetbrains.plugins.groovy.lang.psi.util.GroovyCommonClassNames
 
-class ImplicitClosureCallPredicate implements PsiElementPredicate {
+internal object ImplicitClosureCallPredicate : PsiElementPredicate {
 
-    @Override
-    public boolean satisfiedBy(@NotNull PsiElement element) {
-        if (!(element instanceof GrMethodCallExpression)) {
-            return false;
-        }
-        final GrMethodCallExpression call = (GrMethodCallExpression) element;
-        final GrExpression invokedExpression = call.getInvokedExpression();
-        if (invokedExpression == null) {
-            return false;
-        }
-        final PsiType type = invokedExpression.getType();
-        if(type == null)
-        {
-            return false;
-        }
-        if (!type.equalsToText(GroovyCommonClassNames.GROOVY_LANG_CLOSURE)) {
-            return false;
-        }
-        return !ErrorUtil.containsError(element);
+  override fun satisfiedBy(element: PsiElement): Boolean {
+    if (element !is GrMethodCallExpression) {
+      return false
     }
+    val invokedExpression = element.invokedExpression
+    val type = invokedExpression.type ?: return false
+    if (!type.equalsToText(GroovyCommonClassNames.GROOVY_LANG_CLOSURE)) {
+      return false
+    }
+    else {
+      return !ErrorUtil.containsError(element)
+    }
+  }
 }
