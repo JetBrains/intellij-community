9d27334ccac42a013add8588ca1e5b3c9ea6b8c4
ThomasR <thomasr@fantasymail.de>
2019-02-19 06:15:26 +0100

    Add folding

---
+++ src/main/java/de/thomasrosenau/diffplugin/DiffFoldingBuilder.java
@@ -0,0 +1,99 @@
+/*
+ Copyright 2019 Thomas Rosenau
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+package de.thomasrosenau.diffplugin;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import com.intellij.lang.ASTNode;
+import com.intellij.lang.folding.FoldingBuilderEx;
+import com.intellij.lang.folding.FoldingDescriptor;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
+import de.thomasrosenau.diffplugin.psi.impl.DiffConsoleCommandImpl;
+import de.thomasrosenau.diffplugin.psi.impl.DiffContextHunkImpl;
+import de.thomasrosenau.diffplugin.psi.impl.DiffNormalHunkImpl;
+import de.thomasrosenau.diffplugin.psi.impl.DiffUnifiedHunkImpl;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+// TODO: add tests
+public class DiffFoldingBuilder extends FoldingBuilderEx {
+    @NotNull
+    @Override
+    public FoldingDescriptor[] buildFoldRegions(@NotNull PsiElement root, @NotNull Document document, boolean quick) {
+        ArrayList<FoldingDescriptor> result = new ArrayList<>();
+        buildFileFoldingRegions(root, result);
+        buildHunkFoldingRegions(root, result);
+        return result.toArray(new FoldingDescriptor[] {});
+    }
+
+    private void buildFileFoldingRegions(@NotNull PsiElement root, @NotNull ArrayList<FoldingDescriptor> result) {
+        Collection<DiffConsoleCommandImpl> commands = PsiTreeUtil
+                .findChildrenOfType(root, DiffConsoleCommandImpl.class);
+        DiffConsoleCommandImpl[] commandsAsArray = commands.toArray(new DiffConsoleCommandImpl[] {});
+        for (int i = 0; i < commandsAsArray.length; i++) {
+            DiffConsoleCommandImpl command = commandsAsArray[i];
+            int end;
+            if (i < commandsAsArray.length - 1) {
+                end = commandsAsArray[i + 1].getTextOffset() - 1;
+            } else {
+                end = root.getTextLength();
+            }
+            result.add(new FoldingDescriptor(command, new TextRange(command.getTextOffset(), end)));
+        }
+    }
+
+    private void buildHunkFoldingRegions(@NotNull PsiElement root, @NotNull ArrayList<FoldingDescriptor> result) {
+        Collection<PsiElement> hunks = PsiTreeUtil.findChildrenOfType(root, DiffContextHunkImpl.class);
+        hunks.addAll(PsiTreeUtil.findChildrenOfType(root, DiffUnifiedHunkImpl.class));
+        hunks.addAll(PsiTreeUtil.findChildrenOfType(root, DiffNormalHunkImpl.class));
+        for (PsiElement element : hunks) {
+            TextRange range = element.getTextRange();
+            if (element.getText().endsWith("\n")) {
+                range = new TextRange(range.getStartOffset(), range.getEndOffset() - 1);
+            }
+            result.add(new FoldingDescriptor(element, range));
+        }
+
+    }
+
+    @Override
+    public boolean isCollapsedByDefault(@NotNull ASTNode node) {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public String getPlaceholderText(@NotNull ASTNode node) {
+        PsiElement psiNode = node.getPsi();
+        if (psiNode instanceof DiffContextHunkImpl) {
+            PsiElement fromNode = ((DiffContextHunkImpl) psiNode).getContextHunkFrom().getFirstChild();
+            String fromText = fromNode.getText();
+            PsiElement toNode = ((DiffContextHunkImpl) psiNode).getContextHunkTo().getFirstChild();
+            String toText = toNode.getText();
+            return "@@ -" + fromText.substring(4, fromText.length() - 5) + " +" +
+                    toText.substring(4, toText.length() - 5) + " @@";
+        } else if (psiNode instanceof DiffNormalHunkImpl) {
+            return psiNode.getFirstChild().getFirstChild().getText();
+        } else {
+            return psiNode.getFirstChild().getText();
+        }
+    }
+}
