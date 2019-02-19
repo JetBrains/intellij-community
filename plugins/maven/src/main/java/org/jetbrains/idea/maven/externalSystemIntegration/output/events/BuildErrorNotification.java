3bd7ce6313b95039d6613443d44ba02f169297f2
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2019-02-19 17:30:12 +0300

    IDEA-CR-43732: IDEA-207490, IDEA-207493 - fix navigation and kotlin files
    (cherry picked from commit 0482006f9decc003a54969efd999279d4ff02fa0)

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/externalSystemIntegration/output/events/BuildErrorNotification.java
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/externalSystemIntegration/output/events/BuildErrorNotification.java
@@ -7,6 +7,8 @@ import com.intellij.build.events.MessageEvent;
 import com.intellij.build.events.impl.FileMessageEventImpl;
 import com.intellij.build.events.impl.MessageEventImpl;
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskId;
+import com.intellij.openapi.util.SystemInfo;
+import com.intellij.openapi.util.io.FileUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.idea.maven.externalSystemIntegration.output.LogMessageType;
@@ -18,8 +20,17 @@ import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class BuildErrorNotification implements MavenLoggedEventParser {
-  private static final Pattern LINE_AND_COLUMN = Pattern.compile("[^\\d]*?(\\d+)[^\\d]*(\\d*)[^\\d]*");
+public abstract class BuildErrorNotification implements MavenLoggedEventParser {
+  private static final Pattern LINE_AND_COLUMN = Pattern.compile("[^\\d]*?(\\d+)[^\\d]*(\\d*)[])]");
+  private final String myLanguage;
+  private final String myExtension;
+  private final String myMessageGroup;
+
+  protected BuildErrorNotification(String language, String extension, String messageGroup) {
+    myLanguage = language;
+    myExtension = extension;
+    myMessageGroup = messageGroup;
+  }
 
   @Override
   public boolean supportsType(@Nullable LogMessageType type) {
@@ -31,38 +42,61 @@ public class BuildErrorNotification implements MavenLoggedEventParser {
                               @NotNull MavenLogEntryReader.MavenLogEntry logLine,
                               @NotNull MavenLogEntryReader logEntryReader,
                               @NotNull Consumer<? super BuildEvent> messageConsumer) {
+
     String line = logLine.getLine();
-    int fileNameIdx = line.indexOf(".java:");
+    if (line.endsWith("java.lang.OutOfMemoryError")) {
+      messageConsumer.accept(new MessageEventImpl(id, MessageEvent.Kind.ERROR, myMessageGroup,
+                                                  "Out of memory.", line));
+      return true;
+    }
+    int fileNameIdx = line.indexOf("." + myExtension + ":");
     if (fileNameIdx < 0) {
-      return notifyError(id, line, messageConsumer);
+      return false;
     }
     int fullFileNameIdx = line.indexOf(":", fileNameIdx);
     if (fullFileNameIdx < 0) {
-      return notifyError(id, line, messageConsumer);
+      return false;
     }
-    File parsedFile = new File(line.substring(0, fileNameIdx) + ".java");
-    int messageIdx = line.indexOf(' ', fullFileNameIdx);
-    FilePosition position = withLineAndColumn(parsedFile, line.substring(fullFileNameIdx, messageIdx), messageIdx, fullFileNameIdx);
+    int start = SystemInfo.isWindows && line.charAt(0) == '/' ? 1 : 0;
+    String filename = FileUtil.toSystemDependentName(line.substring(start, fileNameIdx) + "." + myExtension);
+
+    File parsedFile = new File(filename);
+    String lineWithPosition = line.substring(fullFileNameIdx);
+    Matcher matcher = LINE_AND_COLUMN.matcher(lineWithPosition);
+    String message;
+    FilePosition position;
+    if (matcher.find()) {
+      position = withLineAndColumn(parsedFile, matcher);
+      message = lineWithPosition.substring(matcher.end());
+    }
+    else {
+      position = new FilePosition(parsedFile, 0, 0);
+      message = lineWithPosition;
+    }
+
+
+    String errorMessage = getErrorMessage(position, message);
     messageConsumer
-      .accept(new FileMessageEventImpl(id, MessageEvent.Kind.ERROR, COMPILER_MESSAGES_GROUP, line.substring(messageIdx), line,
+      .accept(new FileMessageEventImpl(id, MessageEvent.Kind.ERROR, myMessageGroup, errorMessage, errorMessage,
                                        position));
     return true;
   }
 
-  private static boolean notifyError(ExternalSystemTaskId id,
-                                     String line,
-                                     Consumer<? super BuildEvent> messageConsumer) {
-    messageConsumer
-      .accept(new MessageEventImpl(id, MessageEvent.Kind.ERROR, null, line, line));
-    return true;
+  @NotNull
+  private String getErrorMessage(@NotNull FilePosition position, @NotNull String message) {
+    if (position.getStartLine() == 0) {
+      return "Error: " + myLanguage + ":" + message;
+    }
+    if (position.getStartColumn() == 0) {
+      return "Error:(" + (position.getStartLine() + 1) + ") " + myLanguage + ":" + message;
+    }
+    return "Error:(" + (position.getStartLine() + 1) + "," + (position.getStartColumn() + 1) + ") " + myLanguage + ":" + message;
   }
 
   @NotNull
-  private static FilePosition withLineAndColumn(File toTest, String line, int spaceAfterFileIdx, int fullFileNameIdx) {
-    if (spaceAfterFileIdx < 0) return new FilePosition(toTest, 0, 0);
-    Matcher matcher = LINE_AND_COLUMN.matcher(line);
+  private static FilePosition withLineAndColumn(File toTest, Matcher matcher) {
     try {
-      if (matcher.matches() && matcher.groupCount() == 2) {
+      if (matcher.groupCount() == 2) {
         if (matcher.start(2) < 0) {
           return new FilePosition(toTest, Integer.valueOf(matcher.group(1)) - 1, 0);
         }
@@ -70,7 +104,6 @@ public class BuildErrorNotification implements MavenLoggedEventParser {
           return new FilePosition(toTest, Integer.valueOf(matcher.group(1)) - 1, Integer.valueOf(matcher.group(2)) - 1);
         }
       }
-
       return new FilePosition(toTest, 0, 0);
     }
     catch (NumberFormatException ignore) {
