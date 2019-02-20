572f29e8956f3b155edc53f959d1c917ca948766
Egor Ushakov <egor.ushakov@jetbrains.com>
2019-02-20 17:43:49 +0300

    IDEA-207528 Unable to do smart step into the second filter method

--- java/debugger/impl/src/com/intellij/debugger/engine/LambdaAsyncMethodFilter.java
+++ java/debugger/impl/src/com/intellij/debugger/engine/LambdaAsyncMethodFilter.java
@@ -1,33 +1,23 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.debugger.engine;
 
 import com.intellij.debugger.SourcePosition;
 import com.intellij.debugger.engine.evaluation.EvaluateException;
 import com.intellij.debugger.engine.evaluation.EvaluationContextImpl;
 import com.intellij.debugger.impl.DebuggerUtilsEx;
+import com.intellij.debugger.jdi.MethodBytecodeUtil;
 import com.intellij.debugger.jdi.StackFrameProxyImpl;
 import com.intellij.debugger.ui.breakpoints.StepIntoBreakpoint;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiMethod;
 import com.intellij.util.containers.ContainerUtil;
+import com.sun.jdi.Location;
+import com.sun.jdi.Method;
 import com.sun.jdi.ObjectReference;
 import com.sun.jdi.Value;
 import com.sun.jdi.event.LocatableEvent;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * @author egor
@@ -43,11 +33,25 @@ public class LambdaAsyncMethodFilter extends BasicStepMethodFilter {
   }
 
   @Override
+  public boolean locationMatches(DebugProcessImpl process, @NotNull StackFrameProxyImpl frameProxy) throws EvaluateException {
+    if (super.locationMatches(process, frameProxy)) {
+      Value lambdaReference = getLambdaReference(frameProxy);
+      if (lambdaReference instanceof ObjectReference) {
+        Method lambdaMethod = MethodBytecodeUtil.getLambdaMethod(
+          ((ObjectReference)lambdaReference).referenceType(), process.getVirtualMachineProxy().getClassesByNameProvider());
+        Location location = lambdaMethod != null ? ContainerUtil.getFirstItem(DebuggerUtilsEx.allLineLocations(lambdaMethod)) : null;
+        return location != null && myMethodFilter.locationMatches(process, location);
+      }
+    }
+    return false;
+  }
+
+  @Override
   public int onReached(SuspendContextImpl context, RequestHint hint) {
     try {
       StackFrameProxyImpl proxy = context.getFrameProxy();
       if (proxy != null) {
-        Value lambdaReference = ContainerUtil.getOrElse(proxy.getArgumentValues(), myParamNo, null);
+        Value lambdaReference = getLambdaReference(proxy);
         if (lambdaReference instanceof ObjectReference) {
           final SourcePosition pos = myMethodFilter.getBreakpointPosition();
           if (pos != null) {
@@ -63,6 +67,11 @@ public class LambdaAsyncMethodFilter extends BasicStepMethodFilter {
     catch (EvaluateException ignore) {
     }
     return RequestHint.STOP;
+  }
+
+  @Nullable
+  private Value getLambdaReference(StackFrameProxyImpl proxy) throws EvaluateException {
+    return ContainerUtil.getOrElse(proxy.getArgumentValues(), myParamNo, null);
   }
 
   private static class LambdaInstanceBreakpoint extends StepIntoBreakpoint {
