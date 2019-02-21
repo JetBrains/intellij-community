04dd342c29702c2dbadb960ecdc547c362cf6f14
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-02-21 15:15:58 +0300

    [groovy] move HighlightSink to proper package (IDEA-201921)

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/highlighting/HighlightSink.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/highlighting/HighlightSink.java
@@ -1,5 +1,5 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.codeInspection.type;
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.plugins.groovy.highlighting;
 
 import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.ProblemHighlightType;
