519d030868890e4ab99180f3b8b70c2576c72769
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-02-24 12:08:03 +0100

    Cleanup (unused import)

--- platform/platform-impl/src/com/intellij/openapi/util/registry/RegistryExtensionCollector.kt
+++ platform/platform-impl/src/com/intellij/openapi/util/registry/RegistryExtensionCollector.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.util.registry
 
-import com.intellij.internal.statistic.utils.getPluginInfoByDescriptor
 import com.intellij.internal.statistic.utils.getPluginInfoById
 import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.extensions.PluginAware
