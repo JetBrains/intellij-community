79fbfc8cbea827c172f6b80bee1ce531c9b854ef
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-02-25 18:47:53 +0300

    IDEA-CR-44058: [groovy] don't use ResolveHighlightingVisitor in @CS conversion (IDEA-201921)
    (cherry picked from commit 6232ec0f04e37338b4d82a734478170fc3f04d24)

--- plugins/groovy/src/org/jetbrains/plugins/groovy/refactoring/convertToStatic/DynamicFeaturesVisitor.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/refactoring/convertToStatic/DynamicFeaturesVisitor.java
@@ -5,7 +5,6 @@ import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.groovy.annotator.InaccessibleElementVisitor;
-import org.jetbrains.plugins.groovy.annotator.ResolveHighlightingVisitor;
 import org.jetbrains.plugins.groovy.annotator.VisitorCallback;
 import org.jetbrains.plugins.groovy.lang.psi.GroovyFile;
 import org.jetbrains.plugins.groovy.lang.psi.GroovyRecursiveElementVisitor;
@@ -16,20 +15,20 @@ import org.jetbrains.plugins.groovy.lang.psi.api.types.GrCodeReferenceElement;
 public class DynamicFeaturesVisitor extends GroovyRecursiveElementVisitor {
   private final VisitorCallback myCallback;
   private final InaccessibleElementVisitor accessibilityChecker;
-  private final ResolveHighlightingVisitor resolveVisitor;
 
   public DynamicFeaturesVisitor(@NotNull GroovyFile file, @NotNull Project project, @NotNull VisitorCallback callback) {
     accessibilityChecker = new InaccessibleElementVisitor(file, project, callback);
-    resolveVisitor = new ResolveHighlightingVisitor(file, project, callback);
     myCallback = callback;
   }
 
   @Override
   public void visitReferenceExpression(@NotNull GrReferenceExpression referenceExpression) {
     accessibilityChecker.visitReferenceExpression(referenceExpression);
-    resolveVisitor.visitReferenceExpression(referenceExpression);
-    if (referenceExpression.getType() == null) {
-      PsiElement resolved = referenceExpression.resolve();
+    PsiElement resolved = referenceExpression.resolve();
+    if (resolved == null) {
+      myCallback.trigger(referenceExpression, null);
+    }
+    else if (referenceExpression.getType() == null) {
       if (!(resolved instanceof GrMethod) || !((GrMethod)resolved).isConstructor()) myCallback.trigger(referenceExpression, null);
     }
     super.visitReferenceExpression(referenceExpression);
