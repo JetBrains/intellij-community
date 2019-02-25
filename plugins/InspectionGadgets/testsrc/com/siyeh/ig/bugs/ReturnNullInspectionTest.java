0b5d233ddc74b63dc02614210de9ea3cb1e1873c
Bas Leijdekkers <basleijdekkers@gmail.com>
2019-02-25 12:34:29 +0100

    IDEA-CR-44688: IG: Don't warn about "return of null" in Map.compute() lambdas (IDEA-207197, IDEA-145390, IDEA-197185)
    and fix false negative on methods inferred to be nullable
    and ignore anonymous class and lambda's when ignore private is enabled.
    
    (cherry picked from commit e09d72d8a91365bd88f5907e4c04e8a01d86c222)

--- plugins/InspectionGadgets/testsrc/com/siyeh/ig/bugs/ReturnNullInspectionTest.java
+++ plugins/InspectionGadgets/testsrc/com/siyeh/ig/bugs/ReturnNullInspectionTest.java
@@ -1,22 +1,10 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.siyeh.ig.bugs;
 
 import com.intellij.codeInspection.InspectionProfileEntry;
+import com.intellij.testFramework.LightProjectDescriptor;
 import com.siyeh.ig.LightInspectionTestCase;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -41,14 +29,9 @@ public class ReturnNullInspectionTest extends LightInspectionTestCase {
     return inspection;
   }
 
+  @NotNull
   @Override
-  protected String[] getEnvironmentClasses() {
-    return new String[] {
-      "package java.util;" +
-      "public final class Optional<T> {}",
-
-      "package java.util;" +
-      "public final class OptionalInt {}"
-    };
+  protected LightProjectDescriptor getProjectDescriptor() {
+    return JAVA_8;
   }
 }
