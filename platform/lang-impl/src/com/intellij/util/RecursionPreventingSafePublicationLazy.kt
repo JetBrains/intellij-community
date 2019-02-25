39d52d243e7111aab4376f4d90c24070db871f89
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-02-25 16:34:38 +0300

    recursionSafeLazy: allow to specify recursion key

--- platform/lang-impl/src/com/intellij/util/RecursionPreventingSafePublicationLazy.kt
+++ platform/lang-impl/src/com/intellij/util/RecursionPreventingSafePublicationLazy.kt
@@ -9,11 +9,12 @@ import java.util.concurrent.atomic.AtomicReference
 /**
  * Same as [SafePublicationLazyImpl], but returns `null` in case of computation recursion occurred.
  */
-class RecursionPreventingSafePublicationLazy<T>(initializer: () -> T) : Lazy<T?> {
+class RecursionPreventingSafePublicationLazy<T>(recursionKey: Any?, initializer: () -> T) : Lazy<T?> {
 
   @Volatile
   private var initializer: (() -> T)? = { notNullize(initializer()) }
   private val valueRef: AtomicReference<T> = AtomicReference()
+  private val recursionKey: Any = recursionKey ?: this
 
   override val value: T?
     get() {
@@ -29,7 +30,7 @@ class RecursionPreventingSafePublicationLazy<T>(initializer: () -> T) : Lazy<T?>
       }
 
       val stamp = ourRecursionGuard.markStack()
-      val newValue = ourRecursionGuard.doPreventingRecursion(this, false, initializerValue)
+      val newValue = ourRecursionGuard.doPreventingRecursion(recursionKey, false, initializerValue)
       // In case of recursion don't update [valueRef] and don't clear [initializer].
       if (newValue === null) {
         // Recursion occurred for this lazy.
