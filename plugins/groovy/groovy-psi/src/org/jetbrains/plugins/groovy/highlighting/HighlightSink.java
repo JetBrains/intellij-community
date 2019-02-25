3b0112c2aec63c7873d457ceb728e25c16643c08
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-02-25 17:05:56 +0300

    IDEA-CR-44058: [groovy] accept IntentionAction-s in HighlightSink (IDEA-201921)
    (cherry picked from commit 89ff57d9b38179f84a6516e9152b45a3836c837a)

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/highlighting/HighlightSink.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/highlighting/HighlightSink.java
@@ -1,10 +1,16 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.highlighting;
 
+import com.intellij.codeInsight.intention.IntentionAction;
 import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.psi.PsiElement;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+import static org.jetbrains.plugins.groovy.annotator.intentions.QuickfixUtil.intentionsToFixes;
+import static org.jetbrains.plugins.groovy.annotator.intentions.QuickfixUtil.fixesToIntentions;
 
 public interface HighlightSink {
 
@@ -16,8 +22,17 @@ public interface HighlightSink {
     registerProblem(highlightElement, ProblemHighlightType.GENERIC_ERROR, message, fixes);
   }
 
-  void registerProblem(@NotNull PsiElement highlightElement,
-                       @NotNull ProblemHighlightType highlightType,
-                       @NotNull String message,
-                       @NotNull LocalQuickFix... fixes);
+  default void registerProblem(@NotNull PsiElement highlightElement,
+                               @NotNull ProblemHighlightType highlightType,
+                               @NotNull String message,
+                               @NotNull LocalQuickFix... fixes) {
+    registerProblem(highlightElement, highlightType, message, fixesToIntentions(highlightElement, fixes));
+  }
+
+  default void registerProblem(@NotNull PsiElement highlightElement,
+                               @NotNull ProblemHighlightType highlightType,
+                               @NotNull String message,
+                               @NotNull List<? extends IntentionAction> actions) {
+    registerProblem(highlightElement, highlightType, message, intentionsToFixes(highlightElement, actions));
+  }
 }
