2470d2c6d62e8155a74722b3f2d7725d95c88514
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-02-26 23:32:09 +0100

    Cleanup (pointless override; typos)

--- platform/platform-api/src/com/intellij/execution/process/OSProcessHandler.java
+++ platform/platform-api/src/com/intellij/execution/process/OSProcessHandler.java
@@ -1,16 +1,4 @@
-// Copyright 2000-2017 JetBrains s.r.o.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.process;
 
 import com.intellij.execution.ExecutionException;
@@ -31,12 +19,11 @@ import org.jetbrains.annotations.Nullable;
 import java.io.File;
 import java.nio.charset.Charset;
 import java.util.Set;
-import java.util.concurrent.Future;
 
 public class OSProcessHandler extends BaseOSProcessHandler {
   private static final Logger LOG = Logger.getInstance("#com.intellij.execution.process.OSProcessHandler");
   private static final Set<String> REPORTED_EXECUTIONS = ContainerUtil.newConcurrentSet();
-  private static final long ALLOWED_TIMEOUT_THRESHHOLD = 10;
+  private static final long ALLOWED_TIMEOUT_THRESHOLD = 10;
 
   public static final Key<Set<File>> DELETE_FILES_ON_TERMINATION = Key.create("OSProcessHandler.FileToDelete");
 
@@ -69,7 +56,7 @@ public class OSProcessHandler extends BaseOSProcessHandler {
 
   @Override
   public boolean waitFor(long timeoutInMilliseconds) {
-    if (timeoutInMilliseconds > ALLOWED_TIMEOUT_THRESHHOLD) {
+    if (timeoutInMilliseconds > ALLOWED_TIMEOUT_THRESHOLD) {
       checkEdtAndReadAction(this);
     }
     return super.waitFor(timeoutInMilliseconds);
@@ -140,12 +127,6 @@ public class OSProcessHandler extends BaseOSProcessHandler {
       c = c.getSuperclass();
     }
     return false;
-  }
-
-  @NotNull
-  @Override
-  protected Future<?> executeOnPooledThread(@NotNull Runnable task) {
-    return super.executeOnPooledThread(task);  // to maintain binary compatibility?
   }
 
   @Override
