8bad5fef63b9ebdfc995e2a44433e0c03c91d186
Ivan Semenov <ivan.semenov@jetbrains.com>
2019-02-26 19:05:35 +0300

    [github] Lower level API for PR data loader
    Extract interface

--- plugins/github/src/org/jetbrains/plugins/github/pullrequest/data/GithubPullRequestsDataLoader.kt
+++ plugins/github/src/org/jetbrains/plugins/github/pullrequest/data/GithubPullRequestsDataLoader.kt
@@ -1,75 +1,19 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.github.pullrequest.data
 
-import com.google.common.cache.CacheBuilder
 import com.intellij.openapi.Disposable
-import com.intellij.openapi.progress.ProgressManager
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.Disposer
-import com.intellij.util.EventDispatcher
-import git4idea.commands.Git
-import git4idea.repo.GitRemote
-import git4idea.repo.GitRepository
 import org.jetbrains.annotations.CalledInAwt
-import org.jetbrains.plugins.github.api.GithubApiRequestExecutor
-import org.jetbrains.plugins.github.api.GithubFullPath
-import org.jetbrains.plugins.github.api.GithubServerPath
-import java.util.*
 
-internal class GithubPullRequestsDataLoader(private val project: Project,
-                                            private val progressManager: ProgressManager,
-                                            private val git: Git,
-                                            private val requestExecutor: GithubApiRequestExecutor,
-                                            private val repository: GitRepository,
-                                            private val remote: GitRemote,
-                                            private val serverPath: GithubServerPath,
-                                            private val repoPath: GithubFullPath) : Disposable {
-
-  private var isDisposed = false
-  private val cache = CacheBuilder.newBuilder()
-    .removalListener<Long, GithubPullRequestDataProviderImpl> {
-      Disposer.dispose(it.value)
-      invalidationEventDispatcher.multicaster.providerChanged(it.key)
-    }
-    .maximumSize(5)
-    .build<Long, GithubPullRequestDataProviderImpl>()
-
-  private val invalidationEventDispatcher = EventDispatcher.create(ProviderChangedListener::class.java)
+internal interface GithubPullRequestsDataLoader {
+  @CalledInAwt
+  fun getDataProvider(number: Long): GithubPullRequestDataProvider
 
   @CalledInAwt
-  fun reloadDetails(number: Long) {
-    cache.getIfPresent(number)?.reloadDetails()
-  }
+  fun findDataProvider(number: Long): GithubPullRequestDataProvider?
 
   @CalledInAwt
-  fun invalidateData(number: Long) {
-    cache.invalidate(number)
-  }
+  fun invalidateAllData()
 
   @CalledInAwt
-  fun invalidateAllData() {
-    cache.invalidateAll()
-  }
-
-  @CalledInAwt
-  fun getDataProvider(number: Long): GithubPullRequestDataProvider {
-    if (isDisposed) throw IllegalStateException("Already disposed")
-
-    return cache.get(number) {
-      GithubPullRequestDataProviderImpl(project, progressManager, git, requestExecutor, repository, remote, serverPath,
-                                        repoPath.user, repoPath.repository, number)
-    }
-  }
-
-  fun addProviderChangesListener(listener: ProviderChangedListener, disposable: Disposable) =
-    invalidationEventDispatcher.addListener(listener, disposable)
-
-  override fun dispose() {
-    invalidateAllData()
-    isDisposed = true
-  }
-
-  interface ProviderChangedListener : EventListener {
-    fun providerChanged(pullRequestNumber: Long)
-  }
+  fun addInvalidationListener(disposable: Disposable, listener: (Long) -> Unit)
 }
