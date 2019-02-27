b60fb37504275aaf8335cd91e6b317b673ca76fa
Yann Cébron <yann.cebron@jetbrains.com>
2019-02-27 17:37:31 +0100

    [devkit] Tests: avoid leaking VFP

--- plugins/devkit/devkit-core/src/build/PluginBuildConfiguration.java
+++ plugins/devkit/devkit-core/src/build/PluginBuildConfiguration.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.devkit.build;
 
 import com.intellij.openapi.application.ReadAction;
@@ -109,6 +109,11 @@ public class PluginBuildConfiguration implements PersistentStateComponent<Plugin
   @TestOnly
   public void setPluginXmlFromVirtualFile(VirtualFile virtualFile) {
     myPluginXmlContainer.getConfiguration().replaceConfigFile(PluginDescriptorConstants.META_DATA, virtualFile.getUrl());
+  }
+
+  @TestOnly
+  public void cleanupForNextTest() {
+    myPluginXmlContainer.getConfiguration().removeConfigFiles(PluginDescriptorConstants.META_DATA);
   }
 
   private void createDescriptor(final String url) {
