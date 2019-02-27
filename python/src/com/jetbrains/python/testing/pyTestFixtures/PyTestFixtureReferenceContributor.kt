342cf611fbdcaaf6ef43121a8f8c03db736c9f93
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2019-02-27 21:52:00 +0300

    Separate reference contributors to provide language attribute

--- python/src/com/jetbrains/python/testing/pyTestFixtures/PyTestFixtureReferenceContributor.kt
+++ python/src/com/jetbrains/python/testing/pyTestFixtures/PyTestFixtureReferenceContributor.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.python.testing.pyTestFixtures
 
 import com.intellij.openapi.util.Ref
@@ -61,7 +61,7 @@ private object PyTestReferenceProvider : PsiReferenceProvider() {
   }
 }
 
-object PyTestFixtureReferenceContributor : PsiReferenceContributor() {
+class PyTestFixtureReferenceContributor : PsiReferenceContributor() {
 
   override fun registerReferenceProviders(registrar: PsiReferenceRegistrar) {
     registrar.registerReferenceProvider(PlatformPatterns.psiElement(PyParameter::class.java), PyTestReferenceProvider,
