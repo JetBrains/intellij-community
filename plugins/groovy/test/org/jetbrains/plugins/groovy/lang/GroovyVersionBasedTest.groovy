37c31396382a6994597b212d5cb78a5da9bc76d5
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-02-28 12:00:00 +0300

    IDEA-CR-44235: [groovy] highlight lambdas in < 3.x (IDEA-202693)
    (cherry picked from commit 750af162b43496dd36643f34a99a53b685da8181)

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/GroovyVersionBasedTest.groovy
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/GroovyVersionBasedTest.groovy
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.lang
 
 import groovy.transform.CompileStatic
@@ -27,4 +27,6 @@ abstract class GroovyVersionBasedTest extends LightGroovyTestCase implements Hig
   void 'test try resources'() { highlightingTest() }
 
   void 'test array initializers'() { highlightingTest() }
+
+  void 'test lambdas'() { highlightingTest() }
 }
---
+++ plugins/groovy/testdata/highlighting/pre30/lambdas.groovy
@@ -0,0 +1,3 @@
+foo(() <error descr="Lambdas are not supported in current version">-></error> bar)
+foo(() <error descr="Lambdas are not supported in current version">-></error> {})
+foo(it <error descr="Lambdas are not supported in current version">-></error> it)
