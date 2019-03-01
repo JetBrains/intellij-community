3b44bc77557d002f67e014fbc3c99c42619f0d50
Konstantin Aleev <konstantin.aleev@jetbrains.com>
2019-03-01 11:45:10 +0300

    remote servers actions cleanup
    (cherry picked from commit 095b0d2761acf8f737aeadd32700e3d839f1d645)

--- platform/lang-impl/src/com/intellij/execution/dashboard/actions/RunDashboardActionUtils.java
+++ platform/lang-impl/src/com/intellij/execution/dashboard/actions/RunDashboardActionUtils.java
@@ -3,6 +3,7 @@ package com.intellij.execution.dashboard.actions;
 
 import com.intellij.execution.dashboard.RunDashboardRunConfigurationNode;
 import com.intellij.execution.dashboard.tree.GroupingNode;
+import com.intellij.execution.services.ServiceViewActionUtils;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.util.containers.JBIterable;
@@ -11,39 +12,18 @@ import com.intellij.util.containers.TreeTraversal;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.List;
-
 class RunDashboardActionUtils {
   private RunDashboardActionUtils() {
   }
 
   @NotNull
   static JBIterable<RunDashboardRunConfigurationNode> getTargets(@NotNull AnActionEvent e) {
-    return getTargets(e, RunDashboardRunConfigurationNode.class);
+    return ServiceViewActionUtils.getTargets(e, RunDashboardRunConfigurationNode.class);
   }
 
   @Nullable
   static RunDashboardRunConfigurationNode getTarget(@NotNull AnActionEvent e) {
-    Object[] items = e.getData(PlatformDataKeys.SELECTED_ITEMS);
-    if (items == null || items.length != 1 || !(items[0] instanceof RunDashboardRunConfigurationNode)) return null;
-
-    return (RunDashboardRunConfigurationNode)items[0];
-  }
-
-  @NotNull
-  static <T> JBIterable<T> getTargets(@NotNull AnActionEvent e, @NotNull Class<T> clazz) {
-    Object[] items = e.getData(PlatformDataKeys.SELECTED_ITEMS);
-    if (items == null) return JBIterable.empty();
-
-    List<T> result = new ArrayList<>();
-    for (Object item : items) {
-      if (!clazz.isInstance(item)) {
-        return JBIterable.empty();
-      }
-      result.add(clazz.cast(item));
-    }
-    return JBIterable.from(result);
+    return ServiceViewActionUtils.getTarget(e, RunDashboardRunConfigurationNode.class);
   }
 
   @NotNull
