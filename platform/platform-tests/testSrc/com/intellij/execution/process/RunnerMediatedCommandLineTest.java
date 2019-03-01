f382b133309fa638db78083f71a7869610708aeb
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-03-01 13:47:40 +0100

    compilation fix

--- platform/platform-tests/testSrc/com/intellij/execution/process/RunnerMediatedCommandLineTest.java
+++ platform/platform-tests/testSrc/com/intellij/execution/process/RunnerMediatedCommandLineTest.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.process;
 
 import com.intellij.execution.GeneralCommandLineTest;
@@ -23,7 +23,6 @@ public class RunnerMediatedCommandLineTest extends GeneralCommandLineTest {
     return commandLine;
   }
 
-  @Override
   protected void assumeCanTestWindowsShell() {
     assumeTrue(false);
   }
