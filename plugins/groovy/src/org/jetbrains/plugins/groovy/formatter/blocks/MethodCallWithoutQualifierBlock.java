8e350306f14601c17ec1418bfb54f6bb5ccea8bd
alexey.afanasiev <alexey.afanasiev@jetbrains.com>
2019-03-01 12:26:01 +0300

    [groovy] IDEA-207367 Unexpected formatting of code with a dot on a newline

--- plugins/groovy/src/org/jetbrains/plugins/groovy/formatter/blocks/MethodCallWithoutQualifierBlock.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/formatter/blocks/MethodCallWithoutQualifierBlock.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2013 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.formatter.blocks;
 
 import com.intellij.formatting.Block;
@@ -56,7 +42,7 @@ public class MethodCallWithoutQualifierBlock extends GroovyBlock {
       mySubBlocks = new ArrayList<>();
       final Indent indent = Indent.getContinuationWithoutFirstIndent();
       mySubBlocks.add(new GroovyBlock(myNameElement.getNode(), indent, Wrap.createWrap(WrapType.NONE, false), myContext));
-      new GroovyBlockGenerator(this).addNestedChildrenSuffix(mySubBlocks, null, myTopLevel, myChildren, myChildren.size());
+      new GroovyBlockGenerator(this).addNestedChildrenSuffix(mySubBlocks, myTopLevel, myChildren.subList(1, myChildren.size()));
     }
     return mySubBlocks;
   }
