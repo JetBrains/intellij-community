5cbc50acdc88857001349b936f4bcf258f3f5246
nik <Nikolay.Chashnikov@jetbrains.com>
2019-03-05 10:40:48 +0300

    actions: improve fix of presentation of Java-specific New Project/Module action in non-java IDEs (IDEA-195719)
    This is an improvement of the previous fix (7108be979f993). Now the naming logic is extracted to a method in NewProjectOrModuleAction interface, and variants for different actions are specified explicitly in ProjectBundle.properties. Also text isn't changed for actions on Welcome Screen.

--- java/idea-ui/src/com/intellij/ide/actions/NewProjectAction.java
+++ java/idea-ui/src/com/intellij/ide/actions/NewProjectAction.java
@@ -10,17 +10,14 @@ import com.intellij.openapi.actionSystem.ActionPlaces;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectBundle;
 import com.intellij.openapi.roots.ui.configuration.ModulesProvider;
-import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.wm.impl.welcomeScreen.NewWelcomeScreen;
 import org.jetbrains.annotations.NotNull;
 
-public class NewProjectAction extends AnAction implements DumbAware {
-  private static final Logger LOG = Logger.getInstance(NewProjectAction.class);
-
+public class NewProjectAction extends AnAction implements DumbAware, NewProjectOrModuleAction {
   @Override
   public boolean startInTransaction() {
     return true;
@@ -38,24 +35,29 @@ public class NewProjectAction extends AnAction implements DumbAware {
     if (NewWelcomeScreen.isNewWelcomeScreen(e)) {
       e.getPresentation().setIcon(AllIcons.Welcome.CreateNewProject);
     }
-    updatePresentationForNonJavaIdes(this, e);
+    updateActionText(this, e);
   }
 
-  public static void updatePresentationForNonJavaIdes(@NotNull AnAction action, @NotNull AnActionEvent e) {
-    String defaultText = action.getTemplatePresentation().getText();
-    LOG.assertTrue(defaultText != null);
-    String text = StringUtil.trimStart(defaultText, "New ");
-    if (!IdeLanguageCustomization.getInstance().getPrimaryIdeLanguages().contains(JavaLanguage.INSTANCE)) {
-      text = "Java " + text;
-    }
-    if (!isInvokedFromNewSubMenu(action, e)) {
-      text = "New " + text;
-    }
-    e.getPresentation().setText(text);
+  @NotNull
+  @Override
+  public String getActionText(boolean isInNewSubmenu, boolean isInJavaIde) {
+    return ProjectBundle.message("new.project.action.text", isInNewSubmenu ? 1 : 0, isInJavaIde ? 1 : 0);
   }
 
-  private static boolean isInvokedFromNewSubMenu(@NotNull AnAction action,
-                                                 @NotNull AnActionEvent e) {
+  public static <T extends AnAction & NewProjectOrModuleAction> void updateActionText(@NotNull T action, @NotNull AnActionEvent e) {
+    String actionText;
+    if (NewWelcomeScreen.isNewWelcomeScreen(e)) {
+      actionText = action.getTemplateText();
+    }
+    else {
+      boolean inJavaIde = IdeLanguageCustomization.getInstance().getPrimaryIdeLanguages().contains(JavaLanguage.INSTANCE);
+      boolean fromNewSubMenu = isInvokedFromNewSubMenu(action, e);
+      actionText = action.getActionText(fromNewSubMenu, inJavaIde);
+    }
+    e.getPresentation().setText(actionText);
+  }
+
+  private static boolean isInvokedFromNewSubMenu(@NotNull AnAction action, @NotNull AnActionEvent e) {
     return NewActionGroup.isActionInNewPopupMenu(action) &&
            (ActionPlaces.MAIN_MENU.equals(e.getPlace()) || ActionPlaces.isPopupPlace(e.getPlace()));
   }
