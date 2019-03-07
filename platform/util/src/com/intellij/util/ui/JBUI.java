35e4b814d3646cf5867f3bcd969d31db74171aa4
Kirill Kirichenko <kirill.kirichenko@jetbrains.com>
2019-03-07 16:13:31 +0300

    IDEA-201671 Post rebase fixes for 191

--- platform/util/src/com/intellij/util/ui/JBUI.java
+++ platform/util/src/com/intellij/util/ui/JBUI.java
@@ -973,11 +973,6 @@ public class JBUI {
       for (UpdateListener l : listeners) {
         l.contextUpdated();
       }
-
-      @NotNull
-      public static Color hoverSeparatorColor() {
-        return JBColor.namedColor("ActionButton.hoverSeparatorColor", new JBColor(Gray.xB3, Gray.x6B));
-      }
     }
 
     protected boolean update(@NotNull Scale scale, double value) {
@@ -1552,6 +1547,11 @@ public class JBUI {
       @NotNull
       public static Color hoverBorder() {
         return JBColor.namedColor("ActionButton.hoverBorderColor", Gray.xDF);
+      }
+
+      @NotNull
+      public static Color hoverSeparatorColor() {
+        return JBColor.namedColor("ActionButton.hoverSeparatorColor", new JBColor(Gray.xB3, Gray.x6B));
       }
     }
 
