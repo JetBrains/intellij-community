63c61a12d1378b1f9c69deec50f97f0d86358065
Artemiy Sartakov <Artemiy.Sartakov@jetbrains.com>
2019-03-07 13:47:30 +0700

    Java8MapApiInspection: cr fixes (IDEA-CR-44452)
    1. VariableNameGenerator instead of codeStyleManager#suggestUniqueVariableName
    2. use tracker for inner comments
    3. simplified references search using various util methods
    4. added option to disable replaceAll suggestion

--- java/java-tests/testData/inspection/java8MapApi/beforeReplaceAllEntrySetShadowVar.java
+++ java/java-tests/testData/inspection/java8MapApi/beforeReplaceAllEntrySetShadowVar.java
@@ -6,6 +6,7 @@ import java.util.Map;
 public class Main {
   public void test() {
     String k = "another var";
+    String key = "one more";
     Map<String, String> map = new HashMap<>();
     map.put("foo", "bar");
     for<caret> (Map.Entry<String, String> entry : map.entrySet()) {
