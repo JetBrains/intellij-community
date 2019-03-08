b8083aa17db8211a61adb88e9df397d6665a333a
Yann Cébron <yann.cebron@jetbrains.com>
2019-03-08 16:51:32 +0100

    ResourceBundleFromEditorRenameHandler: fix dangling Javadoc

--- plugins/properties/src/com/intellij/lang/properties/refactoring/rename/ResourceBundleFromEditorRenameHandler.java
+++ plugins/properties/src/com/intellij/lang/properties/refactoring/rename/ResourceBundleFromEditorRenameHandler.java
@@ -1,10 +1,6 @@
 /*
  * Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
-
-/**
- * @author Alexey
- */
 package com.intellij.lang.properties.refactoring.rename;
 
 import com.intellij.ide.util.treeView.smartTree.TreeElement;
