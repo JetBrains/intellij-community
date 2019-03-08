8bd98099fc6333b43cfd168652ddc172e8af0d7d
Bas Leijdekkers <basleijdekkers@gmail.com>
2019-03-08 20:00:05 +0100

    insert @Override annotation in "Unsafe return statements visitor" inspection quickfix

--- plugins/devkit/devkit-core/src/inspections/internal/UnsafeReturnStatementVisitorInspection.java
+++ plugins/devkit/devkit-core/src/inspections/internal/UnsafeReturnStatementVisitorInspection.java
@@ -1,21 +1,8 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.devkit.inspections.internal;
 
 import com.intellij.codeInsight.daemon.impl.analysis.HighlightNamesUtil;
+import com.intellij.codeInsight.generation.OverrideImplementUtil;
 import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.codeInspection.ProblemsHolder;
@@ -111,8 +98,14 @@ public class UnsafeReturnStatementVisitorInspection extends DevKitInspectionBase
       PsiElement element = descriptor.getPsiElement();
       if (element instanceof PsiClass) {
         final PsiElementFactory factory = JavaPsiFacade.getElementFactory(project);
-        for (String method : myMethods) {
-          element.add(factory.createMethodFromText(method, element));
+        final PsiClass aClass = (PsiClass)element;
+        for (String methodText : myMethods) {
+          final PsiMethod method = factory.createMethodFromText(methodText, element);
+          PsiMethod overridden = aClass.findMethodBySignature(method, true);
+          if (overridden != null) {
+            OverrideImplementUtil.annotateOnOverrideImplement(method, aClass, overridden);
+          }
+          aClass.add(method);
         }
       }
     }
