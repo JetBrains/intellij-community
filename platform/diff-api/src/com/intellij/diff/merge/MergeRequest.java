a66d5521cc702c1b2f91f974188f7ab3f10455c6
Yann Cébron <yann.cebron@jetbrains.com>
2019-03-08 17:21:48 +0100

    MergeRequest: fix Javadoc reference

--- platform/diff-api/src/com/intellij/diff/merge/MergeRequest.java
+++ platform/diff-api/src/com/intellij/diff/merge/MergeRequest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2019 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,7 +34,7 @@ public abstract class MergeRequest implements UserDataHolder {
   /**
    * Called on conflict resolve end. Should be called exactly once for each request, that was shown.
    *
-   * MergeRequest should keep initial state of its content and restore it on {@link MergeResult.CANCEL}
+   * MergeRequest should keep initial state of its content and restore it on {@link MergeResult#CANCEL}
    */
   @CalledInAwt
   public abstract void applyResult(@NotNull MergeResult result);
