8ce3ae6ef8f2cdea7ef241e1a7295e405169a2d8
Yann Cébron <yann.cebron@jetbrains.com>
2019-03-08 16:37:25 +0100

    [images] remove dangling Javadoc

--- images/src/org/intellij/images/vfs/IfsUtil.java
+++ images/src/org/intellij/images/vfs/IfsUtil.java
@@ -13,9 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-/** $Id$ */
-
 package org.intellij.images.vfs;
 
 import com.intellij.openapi.diagnostic.Logger;
