6c4a8c27682ecf0414f4ef0e4f2edb3d547139ab
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2019-03-09 19:42:06 +0300

    git: remove an obsolete text from a javadoc

--- plugins/git4idea/src/git4idea/repo/GitRepository.java
+++ plugins/git4idea/src/git4idea/repo/GitRepository.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2011 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package git4idea.repo;
 
 import com.intellij.dvcs.ignore.VcsRepositoryIgnoredFilesHolder;
@@ -95,12 +81,6 @@ public interface GitRepository extends Repository {
 
   /**
    * Returns remotes defined in this Git repository.
-   * It is different from {@link git4idea.repo.GitConfig#getRemotes()} because remotes may be defined not only in {@code .git/config},
-   * but in {@code .git/remotes/} or even {@code .git/branches} as well.
-   * On the other hand, it is a very old way to define remotes and we are not going to implement this until needed.
-   * See <a href="http://thread.gmane.org/gmane.comp.version-control.git/182960">discussion in the Git mailing list</a> that confirms
-   * that remotes a defined in {@code .git/config} only nowadays.
-   * @return GitRemotes defined for this repository.
    */
   @NotNull
   Collection<GitRemote> getRemotes();
