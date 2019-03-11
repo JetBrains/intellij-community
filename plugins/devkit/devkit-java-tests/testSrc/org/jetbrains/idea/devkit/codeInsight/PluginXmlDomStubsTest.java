b5465e2ac0375fef701cb9f92e840d771c23e452
Yann Cébron <yann.cebron@jetbrains.com>
2019-03-11 18:00:25 +0100

    [devkit] DOM Dependency#getOptional -> @Stubbed

--- plugins/devkit/devkit-java-tests/testSrc/org/jetbrains/idea/devkit/codeInsight/PluginXmlDomStubsTest.java
+++ plugins/devkit/devkit-java-tests/testSrc/org/jetbrains/idea/devkit/codeInsight/PluginXmlDomStubsTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2017 JetBrains s.r.o.
+ * Copyright 2000-2019 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,6 +41,7 @@ public class PluginXmlDomStubsTest extends DomStubTest {
                   "    Element:name:pluginName\n" +
                   "    Element:depends:anotherPlugin\n" +
                   "      Attribute:config-file:anotherPlugin.xml\n" +
+                  "      Attribute:optional:true\n" +
                   "    Element:module\n" +
                   "      Attribute:value:myModule\n" +
                   "    Element:extensionPoints\n" +
