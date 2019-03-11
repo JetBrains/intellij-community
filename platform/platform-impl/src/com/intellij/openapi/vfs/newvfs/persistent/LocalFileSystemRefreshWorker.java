a7ed453d0d34c154d52b72d4f3042f1c564f998f
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2019-03-11 19:00:09 +0100

    cancel event generation earlier

--- platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/persistent/LocalFileSystemRefreshWorker.java
+++ platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/persistent/LocalFileSystemRefreshWorker.java
@@ -211,6 +211,7 @@ class LocalFileSystemRefreshWorker {
 
       RefreshingFileVisitor refreshingFileVisitor = new RefreshingFileVisitor(dir, refreshContext, null, Arrays.asList(children));
       refreshingFileVisitor.visit(dir);
+      if (myCancelled) break;
 
       // generating events unless a directory was changed in between
       boolean hasEvents = ReadAction.compute(() -> {
@@ -247,6 +248,7 @@ class LocalFileSystemRefreshWorker {
       if (cached.isEmpty() && wanted.isEmpty()) return;
       RefreshingFileVisitor refreshingFileVisitor = new RefreshingFileVisitor(dir, refreshContext, wanted, cached);
       refreshingFileVisitor.visit(dir);
+      if (myCancelled) break;
 
       // generating events unless a directory was changed in between
       boolean hasEvents = ReadAction.compute(() -> {
