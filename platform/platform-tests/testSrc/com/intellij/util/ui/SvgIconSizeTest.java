JetBrains/intellij-community

commit b31ea5a973dbd9104c5c06cffca72a6d3101e169

Replace ScaleContext.update(Scale, double) with ScaleContext.setScale(Scale)
This better corresponds the object Scale nature, and reduces the code a little.

platform/util/src/com/intellij/util/ui/JBUIScale.java
@@ -1,7 +1,6 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.ui;
 
-import com.intellij.openapi.util.CopyableIcon;
 import com.intellij.openapi.util.Pair;
 import com.intellij.util.Function;
 import gnu.trove.TDoubleObjectHashMap;
@@ -180,10 +179,9 @@ public class JBUIScale {
       return type;
     }
 
-    @NotNull
-    Scale newOrThis(double value) {
-      if (this.value == value) return this;
-      return type.of(value);
+    @Override
+    public boolean equals(Object obj) {
+      return this == obj; // can rely on default impl due to caching
     }
 
     @Override
@@ -271,7 +269,7 @@ public class JBUIScale {
     @NotNull
     public static UserScaleContext create(@NotNull Scale... scales) {
       UserScaleContext ctx = create();
-      for (Scale s : scales) ctx.update(s);
+      for (Scale s : scales) ctx.setScale(s);
       return ctx;
     }
 
@@ -295,6 +293,29 @@ public class JBUIScale {
 
     protected double derivePixScale() {
       return usrScale.value * objScale.value;
+    }
+
+    /**
+     * Sets the new scale (system scale is ignored). Use {@link ScaleType#of(double)} to provide the new scale.
+     *
+     * @param scale the new scale to set
+     * @return whether the new scale updated the current value
+     * @see ScaleType#of(double)
+     */
+    public boolean setScale(@NotNull Scale scale) {
+      boolean updated = false;
+      switch (scale.type) {
+        case USR_SCALE:
+          updated = !usrScale.equals(scale);
+          usrScale = scale;
+          break;
+        case OBJ_SCALE:
+          updated = !objScale.equals(scale);
+          objScale = scale;
+          break;
+        case SYS_SCALE: return false;
+      }
+      return onUpdated(updated);
     }
 
     /**
@@ -340,23 +361,7 @@ public class JBUIScale {
      * @return whether any of the scale factors has been updated
      */
     public boolean update() {
-      return onUpdated(update(usrScale, JBUI.scale(1f)));
-    }
-
-    /**
-     * Updates the provided scale if necessary (system scale is ignored)
-     *
-     * @param scale the new scale
-     * @return whether the scale factor has been updated
-     */
-    public boolean update(@NotNull Scale scale) {
-      boolean updated = false;
-      switch (scale.type) {
-        case USR_SCALE: updated = update(usrScale, scale.value); break;
-        case OBJ_SCALE: updated = update(objScale, scale.value); break;
-        case SYS_SCALE: break;
-      }
-      return onUpdated(updated);
+      return onUpdated(setScale(USR_SCALE.of(JBUI.scale(1f))));
     }
 
     /**
@@ -371,8 +376,8 @@ public class JBUIScale {
     }
 
     protected <T extends UserScaleContext> boolean updateAll(@NotNull T ctx) {
-      boolean updated = update(usrScale, ctx.usrScale.value);
-      return update(objScale, ctx.objScale.value) || updated;
+      boolean updated = setScale(ctx.usrScale);
+      return setScale(ctx.objScale) || updated;
     }
 
     @Override
@@ -419,17 +424,6 @@ public class JBUIScale {
       for (UpdateListener l : listeners) {
         l.contextUpdated();
       }
-    }
-
-    protected boolean update(@NotNull Scale scale, double value) {
-      Scale newScale = scale.newOrThis(value);
-      if (newScale == scale) return false;
-      switch (scale.type) {
-        case USR_SCALE: usrScale = newScale; break;
-        case OBJ_SCALE: objScale = newScale; break;
-        case SYS_SCALE: break;
-      }
-      return true;
     }
 
     @NotNull
@@ -505,12 +499,7 @@ public class JBUIScale {
     }
 
     protected ScaleContext(@NotNull Scale scale) {
-      switch (scale.type) {
-        case USR_SCALE: update(usrScale, scale.value); break;
-        case SYS_SCALE: update(sysScale, scale.value); break;
-        case OBJ_SCALE: update(objScale, scale.value); break;
-      }
-      pixScale = derivePixScale();
+      setScale(scale);
     }
 
     /**
@@ -571,7 +560,7 @@ public class JBUIScale {
     @NotNull
     public static ScaleContext create(@NotNull Scale... scales) {
       ScaleContext ctx = create();
-      for (Scale s : scales) ctx.update(s);
+      for (Scale s : scales) ctx.setScale(s);
       return ctx;
     }
 
@@ -616,10 +605,10 @@ public class JBUIScale {
      */
     @Override
     public boolean update() {
-      boolean updated = update(usrScale, JBUI.scale(1f));
+      boolean updated = setScale(USR_SCALE.of(JBUI.scale(1f)));
       if (compRef != null) {
         Component comp = compRef.get();
-        if (comp != null) updated = update(sysScale, sysScale(comp)) || updated;
+        if (comp != null) updated = setScale(SYS_SCALE.of(sysScale(comp))) || updated;
       }
       return onUpdated(updated);
     }
@@ -629,9 +618,13 @@ public class JBUIScale {
      * Also includes the system scale.
      */
     @Override
-    public boolean update(@NotNull Scale scale) {
-      if (scale.type == SYS_SCALE) return onUpdated(update(sysScale, scale.value));
-      return super.update(scale);
+    public boolean setScale(@NotNull Scale scale) {
+      if (scale.type == SYS_SCALE) {
+        boolean updated = !sysScale.equals(scale);
+        sysScale = scale;
+        return onUpdated(updated);
+      }
+      return super.setScale(scale);
     }
 
     @Override
@@ -643,18 +636,7 @@ public class JBUIScale {
       if (compRef != null) compRef.clear();
       compRef = context.compRef;
 
-      return update(sysScale, context.sysScale.value) || updated;
-    }
-
-    @Override
-    protected boolean update(@NotNull Scale scale, double value) {
-      if (scale.type == SYS_SCALE) {
-        Scale newScale = scale.newOrThis(value);
-        if (newScale == scale) return false;
-        sysScale = newScale;
-        return true;
-      }
-      return super.update(scale, value);
+      return setScale(context.sysScale) || updated;
     }
 
     @Override
@@ -734,18 +716,18 @@ public class JBUIScale {
     double getScale(@NotNull DerivedScaleType type);
 
     /**
-     * Updates the provided scale in the context
+     * Sets the new scale in the context
      *
-     * @return whether the provided scale has been changed
+     * @return whether the provided scale updated the current value
      */
-    boolean updateScale(@NotNull Scale scale);
+    boolean setScale(@NotNull Scale scale);
   }
 
-  public static class ScaleContextAwareImpl<T extends UserScaleContext> implements ScaleContextAware {
+  private static class ScaleContextAwareImpl<T extends UserScaleContext> implements ScaleContextAware {
     @NotNull
     private final T myScaleContext;
 
-    public ScaleContextAwareImpl(@NotNull T ctx) {
+    ScaleContextAwareImpl(@NotNull T ctx) {
       myScaleContext = ctx;
     }
 
@@ -771,8 +753,8 @@ public class JBUIScale {
     }
 
     @Override
-    public boolean updateScale(@NotNull Scale scale) {
-      return getScaleContext().update(scale);
+    public boolean setScale(@NotNull Scale scale) {
+      return getScaleContext().setScale(scale);
     }
   }
 

images/src/org/intellij/images/ui/ImageComponent.java
@@ -20,7 +20,6 @@ package org.intellij.images.ui;
 
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.ui.JBUIScale.ScaleContext;
-import com.intellij.util.ui.JBUIScale;
 import org.intellij.images.ImagesBundle;
 import org.intellij.images.editor.ImageDocument;
 import org.intellij.images.options.GridOptions;
@@ -37,7 +36,6 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.List;
 
-import static com.intellij.util.ui.JBUIScale.ScaleContext.*;
 import static com.intellij.util.ui.JBUIScale.ScaleType.OBJ_SCALE;
 
 /**
@@ -285,7 +283,7 @@ public class ImageComponent extends JComponent {
         @Override
         public Rectangle getBounds(double scale) {
             ScaleContext ctx = ScaleContext.create(myComponent);
-            ctx.update(OBJ_SCALE.of(scale));
+            ctx.setScale(OBJ_SCALE.of(scale));
             return cachedBounds.getOrProvide(ctx);
         }
 

images/src/org/intellij/images/vfs/IfsUtil.java
@@ -31,7 +31,6 @@ import com.intellij.util.LogicalRoot;
 import com.intellij.util.LogicalRootsManager;
 import com.intellij.util.SVGLoader;
 import com.intellij.util.ui.JBUIScale.ScaleContext;
-import com.intellij.util.ui.JBUIScale;
 import org.apache.commons.imaging.ImageReadException;
 import org.apache.commons.imaging.common.bytesource.ByteSourceArray;
 import org.apache.commons.imaging.formats.ico.IcoImageParser;
@@ -133,7 +132,7 @@ public final class IfsUtil {
             @Override
             public BufferedImage apply(Double zoom, Component ancestor) {
               ScaleContext ctx = ScaleContext.create(ancestor);
-              ctx.update(OBJ_SCALE.of(zoom));
+              ctx.setScale(OBJ_SCALE.of(zoom));
               return cache.getOrProvide(ctx);
             }
           }));

platform/platform-tests/testSrc/com/intellij/util/ui/CompositeIconPaintTestHelper.java
@@ -47,7 +47,7 @@ public abstract class CompositeIconPaintTestHelper {
     JBUI.setUserScaleFactor((float)ctx.getScale(USR_SCALE));
 
     ScaleContext ctx_noObjScale = ctx.copy();
-    ctx_noObjScale.update(OBJ_SCALE.of(1));
+    ctx_noObjScale.setScale(OBJ_SCALE.of(1));
 
     String[] cellIconsPaths = getCellIconsPaths();
     int count = cellIconsPaths.length;

platform/util/src/com/intellij/openapi/util/IconLoader.java
@@ -738,7 +738,7 @@ public final class IconLoader {
         ScaleContext ctx = getScaleContext();
         if (scale != 1) {
           ctx = ctx.copy();
-          ctx.update(OBJ_SCALE.of(scale));
+          ctx.setScale(OBJ_SCALE.of(scale));
         }
 
         ImageIcon icon = SoftReference.dereference(scaledIconsCache.get(key(ctx)));

platform/util/src/com/intellij/util/ui/JBCachingScalableIcon.java
@@ -35,7 +35,7 @@ public abstract class JBCachingScalableIcon<T extends JBCachingScalableIcon> ext
 
     if (myScaledIconCache == null || myScaledIconCache.getScale() != scale) {
       myScaledIconCache = copy();
-      myScaledIconCache.updateScale(OBJ_SCALE.of(scale));
+      myScaledIconCache.setScale(OBJ_SCALE.of(scale));
     }
     return myScaledIconCache;
   }

platform/util/src/com/intellij/util/ui/JBScalableIcon.java
@@ -1,10 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.ui;
 
-import com.intellij.openapi.util.CopyableIcon;
 import com.intellij.openapi.util.ScalableIcon;
-import com.intellij.util.ui.JBUIScale.ScaleContext;
-import com.intellij.util.ui.JBUIScale.ScaleContextSupport;
 import com.intellij.util.ui.JBUIScale.UserScaleContext;
 import com.intellij.util.ui.JBUIScale.UserScaleContextSupport;
 import org.jetbrains.annotations.NotNull;
@@ -98,7 +95,7 @@ public abstract class JBScalableIcon extends UserScaleContextSupport implements 
   @Override
   @NotNull
   public Icon scale(float scale) {
-    updateScale(OBJ_SCALE.of(scale));
+    setScale(OBJ_SCALE.of(scale));
     return this;
   }
 

platform/util/src/com/intellij/util/ui/JBUI.java
@@ -1133,6 +1133,10 @@ public class JBUI {
     public boolean update(@Nullable BaseScaleContext ctx) {
       return super.update(ctx);
     }
+
+    public boolean update(@NotNull Scale scale) {
+      return setScale(scale);
+    }
   }
 
   /**
@@ -1162,12 +1166,7 @@ public class JBUI {
     }
 
     protected ScaleContext(@NotNull Scale scale) {
-      switch (scale.type()) {
-        case USR_SCALE: update(usrScale, scale.value()); break;
-        case SYS_SCALE: update(sysScale, scale.value()); break;
-        case OBJ_SCALE: update(objScale, scale.value()); break;
-      }
-      pixScale = derivePixScale();
+      setScale(scale);
     }
   }
 

commit 83095e7907d8af965deaa95b4109e6962941d95f

Fix regression revealed by SvgIconSizeTest, EffectPainterTest
Caused by b31ea5a

platform/util/src/com/intellij/util/ui/JBUIScale.java
@@ -536,6 +536,7 @@ public class JBUIScale {
     }
 
     protected ScaleContext(@NotNull Scale scale) {
+      this();
       setScale(scale);
     }
 

platform/platform-tests/testSrc/com/intellij/util/ui/SvgIconSizeTest.java
@@ -40,6 +40,16 @@ public class SvgIconSizeTest {
     test(ScaleContext.create(SYS_SCALE.of(1)));
     test(ScaleContext.create(SYS_SCALE.of(2)));
 
+    float currentSysScale = JBUI.sysScale();
+    if (currentSysScale != 2) {
+      JBUI.setSystemScaleFactor(2);
+      /*
+       * Test with the system scale equal to the current system scale.
+       */
+      test(ScaleContext.create(SYS_SCALE.of(2)));
+      JBUI.setSystemScaleFactor(currentSysScale);
+    }
+
     /*
      * Test overridden size.
      */