aafc1fa82513c165d6aefcd59eb565212b0e6f80
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-03-14 18:22:25 +0100

    cleanup — use ApplicationStarter.EP_NAME

--- platform/platform-api/src/com/intellij/ExtensionPoints.java
+++ platform/platform-api/src/com/intellij/ExtensionPoints.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij;
 
 import com.intellij.openapi.diagnostic.ErrorReportSubmitter;
@@ -54,18 +54,6 @@ public interface ExtensionPoints extends ToolExtensionPoints {
    */
   @NonNls
   String JUNIT_PATCHER = "com.intellij.junitPatcher";
-
-  /**
-   * This extensions allows to run custom [command-line] application based on IDEA platform
-   * <pre>
-   * &lt;extensions xmlns="com.intellij"&gt;
-   *   &lt;applicationStarter implementation="my.plugin.package.MyApplicationStarter"/&gt;
-   * &lt;/extensions&gt;
-   * </pre>
-   * my.plugin.package.MyApplicationStarter class must implement {@link com.intellij.openapi.application.ApplicationStarter} interface.
-   */
-  @NonNls
-  String APPLICATION_STARTER = "com.intellij.appStarter";
 
   @NonNls
   String ANT_BUILD_GEN = "com.intellij.antBuildGen";
--- platform/platform-api/src/com/intellij/openapi/application/ApplicationStarter.java
+++ platform/platform-api/src/com/intellij/openapi/application/ApplicationStarter.java
@@ -1,33 +1,23 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.application;
 
-import com.intellij.ExtensionPoints;
 import com.intellij.openapi.extensions.ExtensionPointName;
 import org.jetbrains.annotations.NonNls;
 
 /**
- * Implementers of the interface declared via {@link com.intellij.ExtensionPoints#APPLICATION_STARTER}
- * contribute to a command-line processing capability of an application.
+ * This extension point allows to run custom [command-line] application based on IDEA platform
+ * <pre>
+ * &lt;extensions xmlns="com.intellij"&gt;
+ *   &lt;applicationStarter implementation="my.plugin.package.MyApplicationStarter"/&gt;
+ * &lt;/extensions&gt;
+ * </pre>
+ * my.plugin.package.MyApplicationStarter class must implement {@link ApplicationStarter} interface.
  *
  * @author max
  * @see ApplicationStarterEx
  */
 public interface ApplicationStarter {
-  ExtensionPointName<ApplicationStarter> EP_NAME = ExtensionPointName.create(ExtensionPoints.APPLICATION_STARTER);
+  ExtensionPointName<ApplicationStarter> EP_NAME = new ExtensionPointName<>("com.intellij.appStarter");
 
   /**
    * Command-line switch to start with this runner.
