JetBrains/intellij-community

commit 988c9a74e39edee52350f86a164e111de7de20f7

IDEA-207040 Update CompletionPopup keys. Modify infoForeground colors.

platform/lang-impl/src/com/intellij/codeInsight/lookup/impl/LookupCellRenderer.java
@@ -55,7 +55,7 @@ public class LookupCellRenderer implements ListCellRenderer {
 
   public static final Color BACKGROUND_COLOR = JBColor.namedColor("CompletionPopup.background", new JBColor(new Color(235, 244, 254), JBColor.background()));
   public static final Color FOREGROUND_COLOR = JBColor.namedColor("CompletionPopup.foreground", JBColor.foreground());
-  private static final Color GRAYED_FOREGROUND_COLOR = JBColor.namedColor("CompletionPopup.infoForeground",  new JBColor(Gray._150, Gray._110));
+  private static final Color GRAYED_FOREGROUND_COLOR = JBColor.namedColor("CompletionPopup.infoForeground",  new JBColor(Gray.x37, Gray.xA3));
   private static final Color SELECTED_BACKGROUND_COLOR = JBColor.namedColor("CompletionPopup.selectionBackground", new Color(0, 82, 164));
   public static final Color SELECTED_NON_FOCUSED_BACKGROUND_COLOR = JBColor.namedColor("CompletionPopup.selectionInactiveBackground", new JBColor(0x6e8ea2, 0x55585a));
   public static final Color SELECTED_FOREGROUND_COLOR = JBColor.namedColor("CompletionPopup.selectionForeground", new JBColor(JBColor.WHITE, JBColor.foreground()));
@@ -119,8 +119,6 @@ public class LookupCellRenderer implements ListCellRenderer {
 
     myIsSelected = isSelected;
     final LookupElement item = (LookupElement)value;
-    final Color foreground = getForegroundColor(isSelected);
-    final Color grayedForeground = getGrayedForeground(isSelected);
     final Color background = nonFocusedSelection ? SELECTED_NON_FOCUSED_BACKGROUND_COLOR :
                              isSelected ? SELECTED_BACKGROUND_COLOR : BACKGROUND_COLOR;
 
@@ -158,7 +156,9 @@ public class LookupCellRenderer implements ListCellRenderer {
 
     myNameComponent.clear();
     myNameComponent.setBackground(background);
-    allowedWidth -= setItemTextLabel(item, foreground, isSelected, presentation, allowedWidth);
+
+    Color itemColor = isSelected ? SELECTED_FOREGROUND_COLOR : JBColor.namedColor("CompletionPopup.foreground", presentation.getItemTextForeground());
+    allowedWidth -= setItemTextLabel(item, itemColor, isSelected, presentation, allowedWidth);
 
     Font font = myLookup.getCustomFont(item, false);
     if (font == null) {
@@ -168,7 +168,7 @@ public class LookupCellRenderer implements ListCellRenderer {
     myTypeLabel.setFont(font);
     myNameComponent.setIcon(augmentIcon(myLookup.getEditor(), presentation.getIcon(), myEmptyIcon));
 
-
+    final Color grayedForeground = getGrayedForeground(isSelected);
     myTypeLabel.clear();
     if (allowedWidth > 0) {
       allowedWidth -= setTypeTextLabel(item, background, grayedForeground, presentation, isSelected ? getMaxWidth() : allowedWidth, isSelected, nonFocusedSelection, normalMetrics);
@@ -229,10 +229,6 @@ public class LookupCellRenderer implements ListCellRenderer {
       width += icon.getIconWidth() + component.getIconTextGap();
     }
     return width;
-  }
-
-  private static Color getForegroundColor(boolean isSelected) {
-    return isSelected ? SELECTED_FOREGROUND_COLOR : FOREGROUND_COLOR;
   }
 
   private int getMaxWidth() {

commit 046626fef8081c30f882135342d0345f534c7773

IDEA-208982 Fix problem where first used color is registered as CompletionPopup.foreground
Caused by 988c9a74e39edee52350f86a164e111de7de20f7

platform/lang-api/src/com/intellij/codeInsight/lookup/LookupElementPresentation.java
@@ -37,7 +37,7 @@ public class LookupElementPresentation {
   private String myItemText;
   private String myTypeText;
   private boolean myStrikeout;
-  private Color myItemTextForeground = JBColor.foreground();
+  private Color myItemTextForeground = JBColor.namedColor("CompletionPopup.foreground", JBColor.foreground());
   private boolean myItemTextBold;
   private boolean myItemTextUnderlined;
   private boolean myItemTextItalic;

platform/lang-impl/src/com/intellij/codeInsight/lookup/impl/LookupCellRenderer.java
@@ -158,7 +158,7 @@ public class LookupCellRenderer implements ListCellRenderer {
     myNameComponent.clear();
     myNameComponent.setBackground(background);
 
-    Color itemColor = isSelected ? SELECTED_FOREGROUND_COLOR : JBColor.namedColor("CompletionPopup.foreground", presentation.getItemTextForeground());
+    Color itemColor = isSelected ? SELECTED_FOREGROUND_COLOR : presentation.getItemTextForeground();
     allowedWidth -= setItemTextLabel(item, itemColor, isSelected, presentation, allowedWidth);
 
     Font font = myLookup.getCustomFont(item, false);