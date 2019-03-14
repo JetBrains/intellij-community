0e0af4fbe9042bd14f327bc245995d3ab41b6935
Yann Cébron <yann.cebron@jetbrains.com>
2019-03-14 15:27:27 +0100

    EA-139675 - NPE: NavigationGutterIconBuilder.calcPsiTargets / add assertion, use IAE

--- xml/dom-impl/src/com/intellij/codeInsight/navigation/NavigationGutterIconBuilder.java
+++ xml/dom-impl/src/com/intellij/codeInsight/navigation/NavigationGutterIconBuilder.java
@@ -323,7 +323,10 @@ public class NavigationGutterIconBuilder<T> {
     final List<SmartPsiElementPointer> list = new ArrayList<>(targets.size());
     for (final T target : targets) {
       for (final PsiElement psiElement : converter.fun(target)) {
-        assert psiElement != null : converter + " returned null element";
+        if (psiElement == null) {
+          throw new IllegalArgumentException(converter + " returned null element");
+        }
+
         if (elements.add(psiElement) && psiElement.isValid()) {
           list.add(manager.createSmartPsiElementPointer(psiElement));
         }
