1ec18fac5fef77b3cde675888f16689bd193701f
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2019-03-15 02:01:42 +0300

    extract SnapshotInputMappingIndex

---
+++ platform/lang-impl/src/com/intellij/util/indexing/SnapshotInputMappingIndex.java
@@ -0,0 +1,23 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.util.indexing;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.Map;
+
+public interface SnapshotInputMappingIndex<Key, Value, Input> {
+  @NotNull
+  Map<Key, Value> readData(int hashId) throws IOException;
+
+  @NotNull
+  Map<Key, Value> readDataOrMap(@NotNull Input content);
+
+  int getHashId(@NotNull Input content) throws IOException;
+
+  void flush() throws IOException;
+
+  void clear() throws IOException;
+
+  void close() throws IOException;
+}
