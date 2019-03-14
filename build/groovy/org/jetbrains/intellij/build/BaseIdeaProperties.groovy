023aab01c895e69a23c0d1b5dfec543b65607198
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2019-03-14 20:04:09 +0300

    [stats-collector] Revert "Do not bundle stats-collector plugin (for release branch only)"
    This reverts commit 6129b91a
    
    (cherry picked from commit a0c9af46eee55d6409cbb289c7a4618e52716072)

--- build/groovy/org/jetbrains/intellij/build/BaseIdeaProperties.groovy
+++ build/groovy/org/jetbrains/intellij/build/BaseIdeaProperties.groovy
@@ -91,7 +91,8 @@ abstract class BaseIdeaProperties extends ProductProperties {
     "intellij.xml.langInjection",
     "intellij.java.langInjection.jps",
     "intellij.java.debugger.streams",
-    "intellij.android.smali"
+    "intellij.android.smali",
+    "intellij.statsCollector"
   ]
   protected static final Map<String, String> CE_CLASS_VERSIONS = [
     "": "1.8",
@@ -163,8 +164,6 @@ abstract class BaseIdeaProperties extends ProductProperties {
         removeVersionFromProjectLibraryJarNames("JUnit3") //for compatibility with users projects which refer to IDEA_HOME/lib/junit.jar
       }
     } as Consumer<PlatformLayout>
-
-    productLayout.setPluginPublishingSpec("intellij.statsCollector", new PluginPublishingSpec(includeIntoDirectoryForAutomaticUploading: false))
 
     additionalModulesToCompile = ["intellij.tools.jps.build.standalone"]
     modulesToCompileTests = ["intellij.platform.jps.build"]
