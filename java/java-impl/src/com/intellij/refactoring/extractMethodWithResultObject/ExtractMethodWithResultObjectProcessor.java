57dfa856ed1f23f9cbf9acc60ad5cd598f1158cd
Pavel Dolgov <pavel.dolgov@jetbrains.com>
2019-03-15 17:39:19 +0300

    ExtractMethodWithResultObject - prepare

--- java/java-impl/src/com/intellij/refactoring/extractMethodWithResultObject/ExtractMethodWithResultObjectProcessor.java
+++ java/java-impl/src/com/intellij/refactoring/extractMethodWithResultObject/ExtractMethodWithResultObjectProcessor.java
@@ -4,8 +4,16 @@ package com.intellij.refactoring.extractMethodWithResultObject;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiMember;
+import com.intellij.psi.controlFlow.ControlFlowUtil;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.refactoring.extractMethod.ControlFlowWrapper;
+import com.intellij.refactoring.extractMethod.PrepareFailedException;
+import com.intellij.refactoring.introduceField.ElementToWorkOn;
 import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Pavel.Dolgov
@@ -16,13 +24,22 @@ class ExtractMethodWithResultObjectProcessor {
   private final Project myProject;
   private final Editor myEditor;
   private final PsiElement[] myElements;
+  private final PsiElement myCodeFragmentMember;
+  private final ControlFlowWrapper myControlFlowWrapper;
+
+  private PsiClass myTargetClass;
 
   @NonNls static final String REFACTORING_NAME = "Extract Method With Result Object";
 
-  ExtractMethodWithResultObjectProcessor(@NonNls Project project, @NonNls Editor editor, @NonNls PsiElement[] elements) {
+  ExtractMethodWithResultObjectProcessor(@NonNls Project project, @NonNls Editor editor, @NonNls PsiElement[] elements)
+    throws PrepareFailedException {
     myProject = project;
     myEditor = editor;
     myElements = elements;
+
+    PsiElement codeFragment = ControlFlowUtil.findCodeFragment(elements[0]);
+    myCodeFragmentMember = getCodeFragmentMember(codeFragment);
+    myControlFlowWrapper = new ControlFlowWrapper(myProject, codeFragment, myElements);
   }
 
   Project getProject() {
@@ -33,8 +50,26 @@ class ExtractMethodWithResultObjectProcessor {
     return myEditor;
   }
 
+  private static PsiElement getCodeFragmentMember(@NotNull PsiElement codeFragment) {
+    PsiElement codeFragmentMember = codeFragment.getUserData(ElementToWorkOn.PARENT);
+    if (codeFragmentMember == null) {
+      codeFragmentMember = codeFragment.getParent();
+    }
+    if (codeFragmentMember == null) {
+      PsiElement context = codeFragment.getContext();
+      LOG.assertTrue(context != null, "code fragment context is null");
+      codeFragmentMember = ControlFlowUtil.findCodeFragment(context).getParent();
+    }
+    return codeFragmentMember;
+  }
+
   boolean prepare() {
     LOG.info("prepare");
+
+    myTargetClass = myCodeFragmentMember instanceof PsiMember
+                    ? ((PsiMember)myCodeFragmentMember).getContainingClass()
+                    : PsiTreeUtil.getParentOfType(myCodeFragmentMember, PsiClass.class);
+
     return true;
   }
 
