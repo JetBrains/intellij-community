fce10ac34ba3f81ba6e2f608fae19d1f05e53b2c
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2019-03-18 16:22:20 +0300

    IDEA-CR-45146: assertion added to catch attempts to save ES data in edt in production

--- platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/project/manage/ExternalProjectsDataStorage.java
+++ platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/project/manage/ExternalProjectsDataStorage.java
@@ -3,6 +3,7 @@ package com.intellij.openapi.externalSystem.service.project.manage;
 
 import com.intellij.concurrency.ConcurrentCollectionFactory;
 import com.intellij.configurationStore.SettingsSavingComponentJavaAdapter;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.PathManagerEx;
 import com.intellij.openapi.components.*;
 import com.intellij.openapi.diagnostic.Logger;
@@ -138,6 +139,9 @@ public class ExternalProjectsDataStorage implements SettingsSavingComponentJavaA
   public synchronized void doSave() {
     if (!changed.compareAndSet(true, false)) {
       return;
+    }
+    if (!ApplicationManager.getApplication().isUnitTestMode()) {
+      LOG.assertTrue(!ApplicationManager.getApplication().isDispatchThread(), "Should not be called on EDT");
     }
     try {
       doSave(myProject, myExternalRootProjects.values());
