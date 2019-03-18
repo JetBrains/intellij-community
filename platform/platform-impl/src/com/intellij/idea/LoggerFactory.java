eb763846a39c0fd99fc95f05a3396ba1c391158d
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-03-18 18:15:52 +0100

    LoggerFactory - make init static

--- platform/platform-impl/src/com/intellij/idea/LoggerFactory.java
+++ platform/platform-impl/src/com/intellij/idea/LoggerFactory.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.idea;
 
 import com.intellij.openapi.application.PathManager;
@@ -10,11 +10,13 @@ import com.intellij.util.text.CharSequenceReader;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.xml.DOMConfigurator;
 import org.jdom.Document;
+import org.jdom.JDOMException;
 import org.jdom.output.DOMOutputter;
 import org.jetbrains.annotations.NotNull;
 import org.w3c.dom.Element;
 
 import java.io.File;
+import java.io.IOException;
 
 @SuppressWarnings({"CallToPrintStackTrace", "UseOfSystemOutOrSystemErr"})
 public class LoggerFactory implements Logger.Factory {
@@ -29,44 +31,36 @@ public class LoggerFactory implements Logger.Factory {
   @NotNull
   @Override
   public synchronized Logger getLoggerInstance(@NotNull String name) {
-    try {
-      if (!myInitialized) {
+    if (!myInitialized) {
+      try {
         init();
+        myInitialized = true;
       }
-    }
-    catch (Exception e) {
-      e.printStackTrace();
+      catch (Exception e) {
+        e.printStackTrace();
+      }
     }
 
     return new IdeaLogger(org.apache.log4j.Logger.getLogger(name));
   }
 
-  private void init() {
-    try {
-      System.setProperty("log4j.defaultInitOverride", "true");
+  private static void init() throws IOException, JDOMException {
+    System.setProperty("log4j.defaultInitOverride", "true");
 
-      File logXmlFile = PathManager.getLogFile();
+    String text = FileUtilRt.loadFile(PathManager.getLogFile());
+    text = StringUtil.replace(text, SYSTEM_MACRO, StringUtil.replace(PathManager.getSystemPath(), "\\", "\\\\"));
+    text = StringUtil.replace(text, APPLICATION_MACRO, StringUtil.replace(PathManager.getHomePath(), "\\", "\\\\"));
+    text = StringUtil.replace(text, LOG_DIR_MACRO, StringUtil.replace(PathManager.getLogPath(), "\\", "\\\\"));
 
-      String text = FileUtilRt.loadFile(logXmlFile);
-      text = StringUtil.replace(text, SYSTEM_MACRO, StringUtil.replace(PathManager.getSystemPath(), "\\", "\\\\"));
-      text = StringUtil.replace(text, APPLICATION_MACRO, StringUtil.replace(PathManager.getHomePath(), "\\", "\\\\"));
-      text = StringUtil.replace(text, LOG_DIR_MACRO, StringUtil.replace(PathManager.getLogPath(), "\\", "\\\\"));
-
-      File file = new File(PathManager.getLogPath());
-      if (!file.mkdirs() && !file.exists()) {
-        System.err.println("Cannot create log directory: " + file);
-      }
-
-      // DOMConfigurator really wants Document
-      @SuppressWarnings("deprecation")
-      Document document = JDOMUtil.loadDocument(new CharSequenceReader(text));
-      Element element = new DOMOutputter().output(document).getDocumentElement();
-      new DOMConfigurator().doConfigure(element, LogManager.getLoggerRepository());
-
-      myInitialized = true;
+    File file = new File(PathManager.getLogPath());
+    if (!file.mkdirs() && !file.exists()) {
+      System.err.println("Cannot create log directory: " + file);
     }
-    catch (Exception e) {
-      e.printStackTrace();
-    }
+
+    // DOMConfigurator really wants Document
+    @SuppressWarnings("deprecation")
+    Document document = JDOMUtil.loadDocument(new CharSequenceReader(text));
+    Element element = new DOMOutputter().output(document).getDocumentElement();
+    new DOMConfigurator().doConfigure(element, LogManager.getLoggerRepository());
   }
 }
