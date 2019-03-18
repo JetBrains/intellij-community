44ec1de99bd20466117638edcbc1eb7fcbb5a9af
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2019-03-18 15:14:49 +0100

    annotate OptionsContainingConfigurable#processListOptions with @NotNull
    Follow-up after IDEA-CR-45168.

--- platform/lang-impl/src/com/intellij/application/options/codeStyle/CodeStyleMainPanel.java
+++ platform/lang-impl/src/com/intellij/application/options/codeStyle/CodeStyleMainPanel.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.application.options.codeStyle;
 
@@ -270,6 +270,7 @@ public class CodeStyleMainPanel extends JPanel implements TabbedLanguageCodeStyl
     return mySettingsPanels.get(scheme.getName()).isModified();
   }
 
+  @NotNull
   public Set<String> processListOptions() {
     final CodeStyleScheme defaultScheme = CodeStyleSchemes.getInstance().getDefaultScheme();
     final NewCodeStyleSettingsPanel panel = ensurePanel(defaultScheme);
