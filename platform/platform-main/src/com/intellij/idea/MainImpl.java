6c5473388ad176bd46b6dc4975a6ba2915cf5884
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-03-19 16:53:30 +0100

    IDEA-CR-45009 IdeaApplication#<init> may be called on a stray thread - calling the same method from different threads makes it less predictable

--- platform/platform-main/src/com/intellij/idea/MainImpl.java
+++ platform/platform-main/src/com/intellij/idea/MainImpl.java
@@ -1,11 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.idea;
 
-import com.intellij.ide.plugins.PluginManager;
-import com.intellij.openapi.application.ConfigImportHelper;
 import com.intellij.util.PlatformUtils;
-
-import javax.swing.*;
 
 @SuppressWarnings({"UnusedDeclaration"})
 public class MainImpl {
@@ -14,21 +10,9 @@ public class MainImpl {
   /**
    * Called from PluginManager via reflection.
    */
-  protected static void start(final String[] args) {
+  protected static void start(final String[] args) throws Exception {
     System.setProperty(PlatformUtils.PLATFORM_PREFIX_KEY, PlatformUtils.getPlatformPrefix(PlatformUtils.IDEA_CE_PREFIX));
 
-    StartupUtil.prepareAndStart(args, newConfigFolder -> {
-      if (newConfigFolder && !ConfigImportHelper.isConfigImported()) {
-        //noinspection SSBasedInspection
-        SwingUtilities.invokeLater(() -> {
-          PluginManager.installExceptionHandler();
-          StartupUtil.runStartupWizard();
-          IdeaApplication.initApplication(args);
-        });
-      }
-      else {
-        IdeaApplication.initApplication(args);
-      }
-    });
+    StartupUtil.prepareAndStart(args, newConfigFolder -> IdeaApplication.initApplication(args));
   }
 }
