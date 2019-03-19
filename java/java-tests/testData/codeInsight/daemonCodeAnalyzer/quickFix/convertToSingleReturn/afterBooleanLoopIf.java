ddf27e0bf9568397adb1dde777434c7b8fbe1e68
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2019-03-19 15:00:31 +0700

    IDEA-209056 Action to transform method with multiple returns into the method with single exit point
    First implementation

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/convertToSingleReturn/afterBooleanLoopIf.java
@@ -0,0 +1,21 @@
+// "Transform body to single exit-point form" "true"
+class Test {
+    boolean test(String[] arr) {
+        boolean result = false;
+        boolean finished = false;
+        if (arr != null) {
+            System.out.println("ok");
+            for (String s : arr) {
+                if (s.isEmpty()) {
+                    finished = true;
+                    break;
+                }
+                System.out.println(s);
+            }
+            if (!finished) {
+                result = true;
+            }
+        }
+        return result;
+    }
+}
