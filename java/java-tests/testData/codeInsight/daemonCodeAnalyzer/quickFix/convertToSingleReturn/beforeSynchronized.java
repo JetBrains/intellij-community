38f40261844a48ee18682639ffdbb41b0ba8709f
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2019-03-19 15:48:01 +0700

    IDEA-209056 Switch, try-catch, synchronized support; avoid conflicting var names; fix reads of uninitialized vars

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/convertToSingleReturn/beforeSynchronized.java
@@ -0,0 +1,10 @@
+// "Transform body to single exit-point form" "true"
+class Test {
+    String <caret>test(int x) {
+        synchronized(this) {
+            if(x == 0) return "foo";
+            if(x == 1) return "bar";
+            return "baz";
+        }
+    }
+}
