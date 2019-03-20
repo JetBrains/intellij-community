1b499ee3246de0fd98709854361bcf27b6fd5d79
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-03-20 08:17:37 +0100

    ApplicationImpl - do not hold splash, instead pass it directly to load

--- platform/platform-impl/src/com/intellij/idea/CommandLineApplication.java
+++ platform/platform-impl/src/com/intellij/idea/CommandLineApplication.java
@@ -1,23 +1,10 @@
-/*
- * Copyright 2000-2013 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.idea;
 
 import com.intellij.ide.impl.DataManagerImpl;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.application.ex.ApplicationManagerEx;
+import com.intellij.openapi.application.impl.ApplicationImpl;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.UserDataHolderBase;
 import org.jetbrains.annotations.NonNls;
@@ -42,7 +29,7 @@ public class CommandLineApplication {
     LOG.assertTrue(ourInstance == null, "Only one instance allowed.");
     //noinspection AssignmentToStaticFieldFromInstanceMethod
     ourInstance = this;
-    ApplicationManagerEx.createApplication(isInternal, isUnitTestMode, isHeadless, true, appName, null);
+    new ApplicationImpl(isInternal, isUnitTestMode, isHeadless, true, appName);
   }
 
   public Object getData(@NotNull String dataId) {
