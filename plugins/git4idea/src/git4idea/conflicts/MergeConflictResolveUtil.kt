2d7d2048e5e5c1c6790e0a82e44fc117484c6feb
Aleksey Pivovarov <AMPivovarov@gmail.com>
2019-03-21 01:51:05 +0300

    git: show notification in editor during non-modal conflict resolve

--- plugins/git4idea/src/git4idea/conflicts/MergeConflictResolveUtil.kt
+++ plugins/git4idea/src/git4idea/conflicts/MergeConflictResolveUtil.kt
@@ -9,6 +9,7 @@ import com.intellij.diff.merge.MergeResult
 import com.intellij.diff.merge.MergeUtil
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.progress.runBackgroundableTask
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.WindowWrapper
@@ -17,17 +18,19 @@ import com.intellij.openapi.util.io.FileTooBigException
 import com.intellij.openapi.vcs.VcsNotifier
 import com.intellij.openapi.vcs.changes.VcsDirtyScopeManager
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.ui.EditorNotificationPanel
+import com.intellij.ui.EditorNotifications
+import com.intellij.ui.LightColors
 import com.intellij.util.Consumer
 import com.intellij.util.ui.UIUtil
 import org.jetbrains.annotations.CalledInAwt
-import java.awt.Window
 import java.awt.event.WindowAdapter
 import java.awt.event.WindowEvent
 import javax.swing.JFrame
 
 object MergeConflictResolveUtil {
   private val LOG = Logger.getInstance(MergeConflictResolveUtil::class.java)
-  private val ACTIVE_MERGE_WINDOW = Key.create<JFrame>("ResolveConflictsWindow")
+  private val ACTIVE_MERGE_WINDOW = Key.create<WindowWrapper>("ResolveConflictsWindow")
 
   @JvmStatic
   @CalledInAwt
@@ -59,7 +62,7 @@ object MergeConflictResolveUtil {
       MergeUtil.putRevisionInfos(request, mergeData)
       // TODO: notify MergeRequestProcessor about conflict invalidation
 
-      val windowHandler = Consumer<WindowWrapper> { wrapper -> putActiveWindowKey(wrapper.window, file) }
+      val windowHandler = Consumer<WindowWrapper> { wrapper -> putActiveWindowKey(project, wrapper, file) }
       val hints = DiffDialogHints(WindowWrapper.Mode.FRAME, null, windowHandler)
 
       // TODO: support non-modal external tools (notification?)
@@ -76,22 +79,40 @@ object MergeConflictResolveUtil {
     }
   }
 
-  private fun putActiveWindowKey(frame: Window, file: VirtualFile) {
-    if (frame !is JFrame) return
+  private fun putActiveWindowKey(project: Project, wrapper: WindowWrapper, file: VirtualFile) {
+    val window = wrapper.window
+    if (window !is JFrame) return
 
-    file.putUserData(ACTIVE_MERGE_WINDOW, frame)
+    file.putUserData(ACTIVE_MERGE_WINDOW, wrapper)
+    EditorNotifications.getInstance(project).updateNotifications(file)
 
-    frame.addWindowListener(object : WindowAdapter() {
+    window.addWindowListener(object : WindowAdapter() {
       override fun windowClosed(e: WindowEvent?) {
-        frame.removeWindowListener(this)
+        window.removeWindowListener(this)
         file.putUserData(ACTIVE_MERGE_WINDOW, null)
+        EditorNotifications.getInstance(project).updateNotifications(file)
       }
     })
   }
 
   fun focusActiveMergeWindow(file: VirtualFile?): Boolean {
-    val window = file?.getUserData(ACTIVE_MERGE_WINDOW) ?: return false
-    UIUtil.toFront(window)
+    val wrapper = file?.getUserData(ACTIVE_MERGE_WINDOW) ?: return false
+    UIUtil.toFront(wrapper.window)
     return true
+  }
+
+  class NotificationProvider : EditorNotifications.Provider<EditorNotificationPanel>() {
+    private val KEY = Key.create<EditorNotificationPanel>("MergeConflictResolveUtil.NotificationProvider")
+
+    override fun getKey(): Key<EditorNotificationPanel> = KEY
+
+    override fun createNotificationPanel(file: VirtualFile, fileEditor: FileEditor, project: Project): EditorNotificationPanel? {
+      val wrapper = file.getUserData(ACTIVE_MERGE_WINDOW) ?: return null
+      val panel = EditorNotificationPanel(LightColors.SLIGHTLY_GREEN)
+      panel.setText("Merge conflicts resolve in progress.")
+      panel.createActionLabel("Focus Window") { UIUtil.toFront(wrapper.window) }
+      panel.createActionLabel("Cancel Resolve") { wrapper.close() }
+      return panel
+    }
   }
 }
