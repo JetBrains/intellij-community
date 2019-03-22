bfc718bee1c33fef4ee322cbfb78366e2e3a5353
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-03-22 13:24:56 +0100

    forbid constructor injection for interface extensions - part 6

--- plugins/coverage-common/src/com/intellij/coverage/AbstractCoverageProjectViewNodeDecorator.java
+++ plugins/coverage-common/src/com/intellij/coverage/AbstractCoverageProjectViewNodeDecorator.java
@@ -1,17 +1,24 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.coverage;
 
 import com.intellij.ide.projectView.ProjectViewNodeDecorator;
+import com.intellij.openapi.project.Project;
 import com.intellij.ui.ColoredTreeCellRenderer;
 import com.intellij.ui.SimpleTextAttributes;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Roman.Chernyatchik
  */
 public abstract class AbstractCoverageProjectViewNodeDecorator implements ProjectViewNodeDecorator {
   private final CoverageDataManager myCoverageDataManager;
-    
+
   public AbstractCoverageProjectViewNodeDecorator(final CoverageDataManager coverageDataManager) {
     myCoverageDataManager = coverageDataManager;
+  }
+
+  public AbstractCoverageProjectViewNodeDecorator(@NotNull Project project) {
+    myCoverageDataManager = CoverageDataManager.getInstance(project);
   }
 
   protected CoverageDataManager getCoverageDataManager() {
