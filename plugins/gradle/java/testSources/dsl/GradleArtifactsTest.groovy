9c1c0c7c5b28a425b57661a39caa78c17095e879
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-03-25 18:05:38 +0300

    [gradle] clean up GradleArtifactsContributor, add tests

--- plugins/gradle/java/testSources/dsl/GradleArtifactsTest.groovy
+++ plugins/gradle/java/testSources/dsl/GradleArtifactsTest.groovy
@@ -1,6 +1,8 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.gradle.dsl
 
+import com.intellij.psi.PsiMethod
+import com.intellij.testFramework.RunAll
 import groovy.transform.CompileStatic
 import org.jetbrains.plugins.gradle.highlighting.GradleHighlightingBaseTest
 import org.jetbrains.plugins.groovy.util.ResolveTest
@@ -13,8 +15,14 @@ class GradleArtifactsTest extends GradleHighlightingBaseTest implements ResolveT
 
   @Test
   void artifactsTest() {
-    importProject("")
-    'artifacts closure delegate'()
+    importProject("apply plugin: 'java'")
+    new RunAll().append {
+      'artifacts closure delegate'()
+    } append {
+      'artifacts member'()
+    } append {
+      'artifacts unresolved reference'()
+    } run()
   }
 
   @Override
@@ -25,6 +33,18 @@ class GradleArtifactsTest extends GradleHighlightingBaseTest implements ResolveT
   void 'artifacts closure delegate'() {
     doTest('artifacts { <caret> }') {
       closureDelegateTest(GRADLE_API_ARTIFACT_HANDLER, 1)
+    }
+  }
+
+  void 'artifacts member'() {
+    doTest('artifacts { <caret>add("conf", "notation") }') {
+      methodTest(resolveTest(PsiMethod), "add", GRADLE_API_ARTIFACT_HANDLER)
+    }
+  }
+
+  void 'artifacts unresolved reference'() {
+    doTest('artifacts { <caret>foo }') {
+      resolveTest(null)
     }
   }
 }
