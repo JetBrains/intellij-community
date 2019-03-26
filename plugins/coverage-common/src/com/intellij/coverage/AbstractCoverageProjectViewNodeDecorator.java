JetBrains/intellij-community

commit bfc718bee1c33fef4ee322cbfb78366e2e3a5353

forbid constructor injection for interface extensions - part 6

plugins/coverage-common/src/com/intellij/coverage/AbstractCoverageProjectViewNodeDecorator.java
@@ -1,17 +1,24 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.coverage;
 
 import com.intellij.ide.projectView.ProjectViewNodeDecorator;
+import com.intellij.openapi.project.Project;
 import com.intellij.ui.ColoredTreeCellRenderer;
 import com.intellij.ui.SimpleTextAttributes;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Roman.Chernyatchik
  */
 public abstract class AbstractCoverageProjectViewNodeDecorator implements ProjectViewNodeDecorator {
   private final CoverageDataManager myCoverageDataManager;
-    
+
   public AbstractCoverageProjectViewNodeDecorator(final CoverageDataManager coverageDataManager) {
     myCoverageDataManager = coverageDataManager;
+  }
+
+  public AbstractCoverageProjectViewNodeDecorator(@NotNull Project project) {
+    myCoverageDataManager = CoverageDataManager.getInstance(project);
   }
 
   protected CoverageDataManager getCoverageDataManager() {

plugins/coverage-common/src/com/intellij/coverage/CoverageProjectViewDirectoryNodeDecorator.java
@@ -1,20 +1,23 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.coverage;
 
 import com.intellij.ide.projectView.PresentationData;
 import com.intellij.ide.projectView.ProjectViewNode;
+import com.intellij.openapi.project.Project;
 import com.intellij.packageDependencies.ui.PackageDependenciesNode;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.SmartPsiElementPointer;
 import com.intellij.ui.ColoredTreeCellRenderer;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author yole
  */
-public class CoverageProjectViewDirectoryNodeDecorator extends AbstractCoverageProjectViewNodeDecorator {
-  public CoverageProjectViewDirectoryNodeDecorator(final CoverageDataManager coverageDataManager) {
-    super(coverageDataManager);
+final class CoverageProjectViewDirectoryNodeDecorator extends AbstractCoverageProjectViewNodeDecorator {
+  CoverageProjectViewDirectoryNodeDecorator(@NotNull Project project) {
+    super(project);
   }
 
   @Override
@@ -75,5 +78,4 @@ public class CoverageProjectViewDirectoryNodeDecorator extends AbstractCoverageP
       data.setLocationString(informationString);
     }
   }
-
 }

plugins/coverage/src/com/intellij/coverage/CoverageProjectViewClassNodeDecorator.java
@@ -1,3 +1,4 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.coverage;
 
 import com.intellij.ide.projectView.PresentationData;
@@ -12,16 +13,13 @@ import com.intellij.psi.SmartPsiElementPointer;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.util.PsiUtilCore;
 import com.intellij.ui.ColoredTreeCellRenderer;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-/**
- * @author yole
- */
-public class CoverageProjectViewClassNodeDecorator extends AbstractCoverageProjectViewNodeDecorator {
-  public CoverageProjectViewClassNodeDecorator(final CoverageDataManager coverageDataManager) {
-    super(coverageDataManager);
+final class CoverageProjectViewClassNodeDecorator extends AbstractCoverageProjectViewNodeDecorator {
+  CoverageProjectViewClassNodeDecorator(@NotNull Project project) {
+    super(project);
   }
-
 
   @Override
   public void decorate(PackageDependenciesNode node, ColoredTreeCellRenderer cellRenderer) {

images/src/org/intellij/images/editor/impl/ImageFileEditorProvider.java
@@ -37,15 +37,9 @@ import org.jetbrains.annotations.NotNull;
 final class ImageFileEditorProvider implements FileEditorProvider, DumbAware {
   @NonNls private static final String EDITOR_TYPE_ID = "images";
 
-  private final ImageFileTypeManager typeManager;
-
-  ImageFileEditorProvider(ImageFileTypeManager typeManager) {
-    this.typeManager = typeManager;
-  }
-
   @Override
   public boolean accept(@NotNull Project project, @NotNull VirtualFile file) {
-    return typeManager.isImage(file);
+    return ImageFileTypeManager.getInstance().isImage(file);
   }
 
   @Override

java/debugger/impl/src/com/intellij/debugger/ui/tree/render/ColorObjectRenderer.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.debugger.ui.tree.render;
 
 import com.intellij.debugger.engine.DebugProcessImpl;
@@ -7,7 +7,6 @@ import com.intellij.debugger.engine.evaluation.EvaluateException;
 import com.intellij.debugger.engine.evaluation.EvaluationContext;
 import com.intellij.debugger.engine.evaluation.EvaluationContextImpl;
 import com.intellij.debugger.engine.events.SuspendContextCommandImpl;
-import com.intellij.debugger.settings.NodeRendererSettings;
 import com.intellij.debugger.ui.tree.ValueDescriptor;
 import com.intellij.util.ui.ColorIcon;
 import com.intellij.util.ui.JBUI;
@@ -19,8 +18,8 @@ import java.awt.*;
 import java.util.Collections;
 
 class ColorObjectRenderer extends CompoundReferenceRenderer {
-  ColorObjectRenderer(final NodeRendererSettings rendererSettings) {
-    super(rendererSettings, "Color", null, null);
+  ColorObjectRenderer() {
+    super("Color", null, null);
     setClassName("java.awt.Color");
     setEnabled(true);
   }

platform/lang-impl/src/com/intellij/ide/bookmarks/BookmarksFavoriteListProvider.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.bookmarks;
 
 import com.intellij.ide.IdeBundle;
@@ -36,13 +22,9 @@ import java.util.List;
 import java.util.Set;
 
 public class BookmarksFavoriteListProvider extends AbstractFavoritesListProvider<Bookmark> implements BookmarksListener {
-  private final BookmarkManager myBookmarkManager;
-  private final FavoritesManager myFavoritesManager;
-
-  public BookmarksFavoriteListProvider(Project project, BookmarkManager bookmarkManager, FavoritesManager favoritesManager) {
+  public BookmarksFavoriteListProvider(Project project) {
     super(project, "Bookmarks");
-    myBookmarkManager = bookmarkManager;
-    myFavoritesManager = favoritesManager;
+
     project.getMessageBus().connect(project).subscribe(BookmarksListener.TOPIC, this);
     updateChildren();
   }
@@ -70,7 +52,7 @@ public class BookmarksFavoriteListProvider extends AbstractFavoritesListProvider
   private void updateChildren() {
     if (myProject.isDisposed()) return;
     myChildren.clear();
-    List<Bookmark> bookmarks = myBookmarkManager.getValidBookmarks();
+    List<Bookmark> bookmarks = BookmarkManager.getInstance(myProject).getValidBookmarks();
     for (final Bookmark bookmark : bookmarks) {
       AbstractTreeNode<Bookmark> child = new AbstractTreeNode<Bookmark>(myProject, bookmark) {
         @NotNull
@@ -103,7 +85,7 @@ public class BookmarksFavoriteListProvider extends AbstractFavoritesListProvider
       child.setParent(myNode);
       myChildren.add(child);
     }
-    myFavoritesManager.fireListeners(getListName(myProject));
+    FavoritesManager.getInstance(myProject).fireListeners(getListName(myProject));
   }
 
   @Nullable

plugins/IntelliLang/java-support/org/intellij/plugins/intelliLang/inject/java/ConcatenationInjector.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.intellij.plugins.intelliLang.inject.java;
 
 import com.intellij.lang.Language;
@@ -61,10 +47,10 @@ public class ConcatenationInjector implements ConcatenationAwareInjector {
   private final LanguageInjectionSupport mySupport;
 
 
-  public ConcatenationInjector(Configuration configuration, Project project, TemporaryPlacesRegistry temporaryPlacesRegistry) {
-    myConfiguration = configuration;
+  public ConcatenationInjector(Project project) {
+    myConfiguration = Configuration.getProjectInstance(project);
     myProject = project;
-    myTemporaryPlacesRegistry = temporaryPlacesRegistry;
+    myTemporaryPlacesRegistry = TemporaryPlacesRegistry.getInstance(project);
     mySupport = InjectorUtils.findNotNullInjectionSupport(JavaLanguageInjectionSupport.JAVA_SUPPORT_ID);
 
   }
@@ -462,7 +448,7 @@ public class ConcatenationInjector implements ConcatenationAwareInjector {
         return true;
       }
     }
-    else if (parent instanceof PsiPolyadicExpression || 
+    else if (parent instanceof PsiPolyadicExpression ||
              parent instanceof PsiParenthesizedExpression ||
              parent instanceof PsiConditionalExpression) {
       return true;

plugins/IntelliLang/src/org/intellij/plugins/intelliLang/inject/CommentLanguageInjector.java
@@ -1,7 +1,9 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.intellij.plugins.intelliLang.inject;
 
 import com.intellij.lang.injection.MultiHostInjector;
 import com.intellij.lang.injection.MultiHostRegistrar;
+import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiComment;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiLanguageInjectionHost;
@@ -18,7 +20,6 @@ import java.util.List;
  * @author gregsh
  */
 public class CommentLanguageInjector implements MultiHostInjector {
-
   private final LanguageInjectionSupport[] mySupports;
   private final LanguageInjectionSupport myInjectorSupport = new AbstractLanguageInjectionSupport() {
     @NotNull
@@ -39,9 +40,8 @@ public class CommentLanguageInjector implements MultiHostInjector {
     }
   };
 
-
-  /** */
-  public CommentLanguageInjector(Configuration configuration) {
+  public CommentLanguageInjector(@NotNull Project project) {
+    Configuration.getProjectInstance(project);
     List<LanguageInjectionSupport> supports = new ArrayList<>(InjectorUtils.getActiveInjectionSupports());
     supports.add(myInjectorSupport);
     mySupports = supports.toArray(new LanguageInjectionSupport[0]);

commit 2f1577fdfbd1b357291216749beebcf798c9a6ae

coverage: fix NPE if no extension for CoverageDataManager is loaded (a regression after bfc718b)

plugins/coverage-common/src/com/intellij/coverage/AbstractCoverageProjectViewNodeDecorator.java
@@ -6,14 +6,16 @@ import com.intellij.openapi.project.Project;
 import com.intellij.ui.ColoredTreeCellRenderer;
 import com.intellij.ui.SimpleTextAttributes;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * @author Roman.Chernyatchik
  */
 public abstract class AbstractCoverageProjectViewNodeDecorator implements ProjectViewNodeDecorator {
+  @Nullable
   private final CoverageDataManager myCoverageDataManager;
 
-  public AbstractCoverageProjectViewNodeDecorator(final CoverageDataManager coverageDataManager) {
+  public AbstractCoverageProjectViewNodeDecorator(@Nullable CoverageDataManager coverageDataManager) {
     myCoverageDataManager = coverageDataManager;
   }
 
@@ -21,6 +23,7 @@ public abstract class AbstractCoverageProjectViewNodeDecorator implements Projec
     myCoverageDataManager = CoverageDataManager.getInstance(project);
   }
 
+  @Nullable
   protected CoverageDataManager getCoverageDataManager() {
     return myCoverageDataManager;
   }

plugins/coverage-common/src/com/intellij/coverage/CoverageProjectViewDirectoryNodeDecorator.java
@@ -22,12 +22,14 @@ final class CoverageProjectViewDirectoryNodeDecorator extends AbstractCoveragePr
 
   @Override
   public void decorate(PackageDependenciesNode node, ColoredTreeCellRenderer cellRenderer) {
+    final CoverageDataManager manager = getCoverageDataManager();
+    if (manager == null) return;
+
     final PsiElement element = node.getPsiElement();
     if (element == null || !element.isValid()) {
       return;
     }
 
-    final CoverageDataManager manager = getCoverageDataManager();
     final CoverageSuitesBundle currentSuite = manager.getCurrentSuitesBundle();
     final CoverageAnnotator coverageAnnotator = currentSuite != null ? currentSuite.getAnnotator(element.getProject()) : null;
     if (coverageAnnotator == null) {
@@ -50,6 +52,7 @@ final class CoverageProjectViewDirectoryNodeDecorator extends AbstractCoveragePr
   @Override
   public void decorate(ProjectViewNode node, PresentationData data) {
     final CoverageDataManager manager = getCoverageDataManager();
+    if (manager == null) return;
     final CoverageSuitesBundle currentSuite = manager.getCurrentSuitesBundle();
     final CoverageAnnotator coverageAnnotator = currentSuite != null ? currentSuite.getAnnotator(node.getProject())
                                                                      : null;

plugins/coverage/src/com/intellij/coverage/CoverageProjectViewClassNodeDecorator.java
@@ -23,12 +23,14 @@ final class CoverageProjectViewClassNodeDecorator extends AbstractCoverageProjec
 
   @Override
   public void decorate(PackageDependenciesNode node, ColoredTreeCellRenderer cellRenderer) {
+    final CoverageDataManager dataManager = getCoverageDataManager();
+    if (dataManager == null) return;
+
     final PsiElement element = node.getPsiElement();
     if (element == null || !element.isValid()) {
       return;
     }
 
-    final CoverageDataManager dataManager = getCoverageDataManager();
     final CoverageSuitesBundle currentSuite = dataManager.getCurrentSuitesBundle();
     final Project project = element.getProject();
 
@@ -49,6 +51,7 @@ final class CoverageProjectViewClassNodeDecorator extends AbstractCoverageProjec
   @Override
   public void decorate(ProjectViewNode node, PresentationData data) {
     final CoverageDataManager coverageDataManager = getCoverageDataManager();
+    if (coverageDataManager == null) return;
     final CoverageSuitesBundle currentSuite = coverageDataManager.getCurrentSuitesBundle();
 
     final Project project = node.getProject();