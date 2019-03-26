ae898556ebcb3a396223bbad397ada71ddd56b89
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-03-26 14:44:38 +0300

    [gradle] feed single ArtifactHandler method per configuration
    In this case we lose a warning when no arguments are passed (`compile()`), but there is a single completion variant
    per configuration, and it's more acceptable for users which doesn't even know that Gradle scripts are in Groovy.

--- plugins/gradle/java/src/service/resolve/GradleArtifactHandlerContributor.kt
+++ plugins/gradle/java/src/service/resolve/GradleArtifactHandlerContributor.kt
@@ -3,20 +3,19 @@ package org.jetbrains.plugins.gradle.service.resolve
 
 import com.intellij.psi.*
 import com.intellij.psi.scope.PsiScopeProcessor
-import icons.GradleIcons.*
+import icons.GradleIcons.Gradle
 import org.jetbrains.plugins.gradle.service.resolve.GradleCommonClassNames.GRADLE_API_ARTIFACT_HANDLER
-import org.jetbrains.plugins.gradle.service.resolve.GradleCommonClassNames.GRADLE_API_CONFIGURABLE_PUBLISH_ARTIFACT
-import org.jetbrains.plugins.groovy.lang.psi.impl.statements.expressions.TypesUtil.createType
 import org.jetbrains.plugins.groovy.lang.psi.impl.statements.expressions.TypesUtil.getJavaLangObject
 import org.jetbrains.plugins.groovy.lang.psi.impl.synthetic.GrLightMethodBuilder
-import org.jetbrains.plugins.groovy.lang.psi.util.GroovyCommonClassNames.GROOVY_LANG_CLOSURE
 import org.jetbrains.plugins.groovy.lang.resolve.NonCodeMembersContributor
-import org.jetbrains.plugins.groovy.lang.resolve.delegatesTo.DELEGATES_TO_KEY
-import org.jetbrains.plugins.groovy.lang.resolve.delegatesTo.DELEGATES_TO_STRATEGY_KEY
 import org.jetbrains.plugins.groovy.lang.resolve.getName
 import org.jetbrains.plugins.groovy.lang.resolve.shouldProcessMethods
 
 class GradleArtifactHandlerContributor : NonCodeMembersContributor() {
+
+  companion object {
+    const val ourMethodKind: String = "gradle:artifactsConfigurationMethod"
+  }
 
   override fun getParentClassName(): String? = GRADLE_API_ARTIFACT_HANDLER
 
@@ -31,36 +30,21 @@ class GradleArtifactHandlerContributor : NonCodeMembersContributor() {
     val data = GradleExtensionsContributor.getExtensionsFor(place) ?: return
     val methodName = processor.getName(state)
     val manager = place.manager
-    val objectType = getJavaLangObject(place)
-    val objectVarargType = PsiEllipsisType(objectType)
-    val closureType = createType(GROOVY_LANG_CLOSURE, place)
-    val configurableArtifactType = createType(GRADLE_API_CONFIGURABLE_PUBLISH_ARTIFACT, place)
+    val objectVarargType = PsiEllipsisType(getJavaLangObject(place))
 
     // TODO store configurations in a map
     for (configuration in data.configurations) {
       val configurationName = configuration.name ?: continue
       if (methodName != null && configurationName != methodName) continue
 
-      val method1 = GrLightMethodBuilder(manager, configurationName).apply {
+      val method = GrLightMethodBuilder(manager, configurationName).apply {
+        methodKind = ourMethodKind
         containingClass = clazz
-        returnType = configurableArtifactType
-        addParameter("artifactNotation", objectType)
-        addAndGetParameter("configureClosure", closureType).apply {
-          putUserData(DELEGATES_TO_KEY, GRADLE_API_CONFIGURABLE_PUBLISH_ARTIFACT)
-          putUserData(DELEGATES_TO_STRATEGY_KEY, 1)
-        }
+        returnType = null
+        addParameter("artifactNotation", objectVarargType)
         setBaseIcon(Gradle)
       }
-      if (!processor.execute(method1, state)) return
-
-      val method2 = GrLightMethodBuilder(manager, configurationName).apply {
-        containingClass = clazz
-        returnType = PsiType.NULL
-        addParameter("artifactNotation", objectType)
-        addParameter("artifactNotations", objectVarargType)
-        setBaseIcon(Gradle)
-      }
-      if (!processor.execute(method2, state)) return
+      if (!processor.execute(method, state)) return
     }
   }
 }
