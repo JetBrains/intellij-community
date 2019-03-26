c56cbb3f0f27c7784d6b7d63a9b06b21794f16b4
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-03-26 14:44:38 +0300

    [gradle] feed single ArtifactHandler method per configuration
    In this case we lose a warning when no arguments are passed (`compile()`), but there is a single completion variant
    per configuration, and it's more acceptable for users which doesn't even know that Gradle scripts are in Groovy.

--- plugins/gradle/java/src/service/resolve/GradleArtifactsContributor.kt
+++ plugins/gradle/java/src/service/resolve/GradleArtifactsContributor.kt
@@ -2,13 +2,13 @@
 package org.jetbrains.plugins.gradle.service.resolve
 
 import groovy.lang.Closure
-import org.jetbrains.plugins.gradle.service.resolve.GradleCommonClassNames.GRADLE_API_ARTIFACT_HANDLER
-import org.jetbrains.plugins.gradle.service.resolve.GradleCommonClassNames.GRADLE_API_PROJECT
+import org.jetbrains.plugins.gradle.service.resolve.GradleCommonClassNames.*
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.blocks.GrClosableBlock
-import org.jetbrains.plugins.groovy.lang.psi.impl.statements.expressions.TypesUtil
+import org.jetbrains.plugins.groovy.lang.psi.impl.statements.expressions.TypesUtil.createType
 import org.jetbrains.plugins.groovy.lang.psi.patterns.GroovyClosurePattern
 import org.jetbrains.plugins.groovy.lang.psi.patterns.groovyClosure
 import org.jetbrains.plugins.groovy.lang.psi.patterns.psiMethod
+import org.jetbrains.plugins.groovy.lang.psi.patterns.withKind
 import org.jetbrains.plugins.groovy.lang.resolve.delegatesTo.DelegatesToInfo
 
 /**
@@ -18,11 +18,17 @@ class GradleArtifactsContributor : GradleMethodContextContributor {
 
   companion object {
     val artifactsClosure: GroovyClosurePattern = groovyClosure().inMethod(psiMethod(GRADLE_API_PROJECT, "artifacts"))
+    val artifactClosure: GroovyClosurePattern = groovyClosure().inMethod(
+      psiMethod(GRADLE_API_ARTIFACT_HANDLER).withKind(GradleArtifactHandlerContributor.ourMethodKind)
+    )
   }
 
   override fun getDelegatesToInfo(closure: GrClosableBlock): DelegatesToInfo? {
     if (artifactsClosure.accepts(closure)) {
-      return DelegatesToInfo(TypesUtil.createType(GRADLE_API_ARTIFACT_HANDLER, closure), Closure.DELEGATE_FIRST)
+      return DelegatesToInfo(createType(GRADLE_API_ARTIFACT_HANDLER, closure), Closure.DELEGATE_FIRST)
+    }
+    if (artifactClosure.accepts(closure)) {
+      return DelegatesToInfo(createType(GRADLE_API_CONFIGURABLE_PUBLISH_ARTIFACT, closure), Closure.DELEGATE_FIRST)
     }
     return null
   }
