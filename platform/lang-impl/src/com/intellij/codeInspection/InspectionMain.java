8e9bfbd5b698a069df556a69e50a5488a5ee0963
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-03-26 09:18:09 +0100

    cleanup

--- platform/lang-impl/src/com/intellij/codeInspection/InspectionMain.java
+++ platform/lang-impl/src/com/intellij/codeInspection/InspectionMain.java
@@ -1,19 +1,4 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInspection;
 
 import com.intellij.openapi.application.ApplicationStarter;
@@ -56,15 +41,16 @@ public class InspectionMain implements ApplicationStarter {
       printHelp();
     }
 
-
     try {
       for (int i = 4; i < args.length; i++) {
         String arg = args[i];
         if ("-profileName".equals(arg)) {
           myApplication.myProfileName = args[++i];
-        } else if ("-profilePath".equals(arg)) {
+        }
+        else if ("-profilePath".equals(arg)) {
           myApplication.myProfilePath = args[++i];
-        } else if ("-d".equals(arg)) {
+        }
+        else if ("-d".equals(arg)) {
           myApplication.mySourceDirectory = args[++i];
         }
         else if ("-v0".equals(arg)) {
@@ -79,7 +65,7 @@ public class InspectionMain implements ApplicationStarter {
         else if ("-v3".equals(arg)) {
           myApplication.setVerboseLevel(3);
         }
-        else if ("-e".equals(arg)){
+        else if ("-e".equals(arg)) {
           myApplication.myRunWithEditorSettings = true;
         }
         else if ("-t".equals(arg)) {
