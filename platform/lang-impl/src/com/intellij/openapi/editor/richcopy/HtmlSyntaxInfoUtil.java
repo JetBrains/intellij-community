57c08a93cc6e176e481a9bfcec668ce59a92ef4f
anstarovoyt <andrey.starovoyt@jetbrains.com>
2019-03-27 18:55:02 +0300

    js: add semantic keywords to example highlighting

--- platform/lang-impl/src/com/intellij/openapi/editor/richcopy/HtmlSyntaxInfoUtil.java
+++ platform/lang-impl/src/com/intellij/openapi/editor/richcopy/HtmlSyntaxInfoUtil.java
@@ -2,60 +2,79 @@
 package com.intellij.openapi.editor.richcopy;
 
 import com.intellij.ide.highlighter.HighlighterFactory;
-import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.colors.EditorColorsScheme;
 import com.intellij.openapi.editor.highlighter.EditorHighlighter;
-import com.intellij.openapi.editor.impl.DocumentMarkupModel;
-import com.intellij.openapi.editor.markup.MarkupModel;
 import com.intellij.openapi.editor.richcopy.model.SyntaxInfo;
 import com.intellij.openapi.editor.richcopy.view.HtmlSyntaxInfoReader;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public final class HtmlSyntaxInfoUtil {
+  
   @NotNull
-  public static HtmlSyntaxInfoReader createHtmlReader(@NotNull PsiFile file,
-                                                      @NotNull Document document,
-                                                      @NotNull EditorColorsScheme schemeToUse,
-                                                      int startOffset,
-                                                      int endOffset) {
-    MarkupModel markupModel = DocumentMarkupModel.forDocument(document, file.getProject(), true);
-    CharSequence text = document.getText();
-    SyntaxInfoBuilder.Context context = new SyntaxInfoBuilder.Context(text, schemeToUse, 0);
-    EditorHighlighter highlighter = HighlighterFactory.createHighlighter(file.getViewProvider().getVirtualFile(),
-                                                                         schemeToUse, file.getProject());
+  public static HtmlSyntaxInfoReader createHighlighterHtmlReader(@NotNull PsiFile file,
+                                                                 @NotNull CharSequence text,
+                                                                 @Nullable SyntaxInfoBuilder.RangeIterator ownRangeIterator,
+                                                                 @NotNull EditorColorsScheme schemeToUse,
+                                                                 int startOffset,
+                                                                 int endOffset) {
+    EditorHighlighter highlighter =
+      HighlighterFactory.createHighlighter(file.getViewProvider().getVirtualFile(), schemeToUse, file.getProject());
     highlighter.setText(text);
-    SyntaxInfoBuilder.MyMarkupIterator
-      markupIterator = SyntaxInfoBuilder.createMarkupIterator(highlighter, text, schemeToUse, markupModel, startOffset, endOffset);
+
+    SyntaxInfoBuilder.HighlighterRangeIterator highlighterRangeIterator =
+      new SyntaxInfoBuilder.HighlighterRangeIterator(highlighter, startOffset, endOffset);
+    ownRangeIterator = ownRangeIterator == null
+                       ? highlighterRangeIterator
+                       : new SyntaxInfoBuilder.CompositeRangeIterator(schemeToUse, highlighterRangeIterator, ownRangeIterator);
+
+    return createHighlighterHtmlReader(text, ownRangeIterator, schemeToUse, endOffset);
+  }
+
+  private static HtmlSyntaxInfoReader createHighlighterHtmlReader(@NotNull CharSequence text,
+                                                                  @NotNull SyntaxInfoBuilder.RangeIterator ownRangeIterator,
+                                                                  @NotNull EditorColorsScheme schemeToUse,
+                                                                  int endOffset) {
+    SyntaxInfoBuilder.Context context = new SyntaxInfoBuilder.Context(text, schemeToUse, 0);
+    SyntaxInfoBuilder.MyMarkupIterator iterator = new SyntaxInfoBuilder.MyMarkupIterator(text, ownRangeIterator, schemeToUse);
+
     try {
-      context.iterate(markupIterator, endOffset);
+      context.iterate(iterator, endOffset);
     }
     finally {
-      markupIterator.dispose();
+      iterator.dispose();
     }
     SyntaxInfo info = context.finish();
-    HtmlSyntaxInfoReader data = new HtmlSyntaxInfoReader(info, 2) {
-      @Override
-      protected void appendCloseTags() {
-        myResultBuffer.append("</pre>");
-      }
-
-      @Override
-      protected void appendStartTags() {
-        myResultBuffer.append("<pre>");
-      }
-
-      @Override
-      protected void defineBackground(int id, @NotNull StringBuilder styleBuffer) {
-
-      }
-
-      @Override
-      protected void appendFontFamilyRule(@NotNull StringBuilder styleBuffer, int fontFamilyId) {
-        
-      }
-    };
+    HtmlSyntaxInfoReader data = new SimpleHtmlSyntaxInfoReader(info);
     data.setRawText(text.toString());
     return data;
+  }
+
+  private static class SimpleHtmlSyntaxInfoReader extends HtmlSyntaxInfoReader {
+    
+    public SimpleHtmlSyntaxInfoReader(SyntaxInfo info) {
+      super(info, 2);
+    }
+
+    @Override
+    protected void appendCloseTags() {
+     
+    }
+
+    @Override
+    protected void appendStartTags() {
+      
+    }
+
+    @Override
+    protected void defineBackground(int id, @NotNull StringBuilder styleBuffer) {
+
+    }
+
+    @Override
+    protected void appendFontFamilyRule(@NotNull StringBuilder styleBuffer, int fontFamilyId) {
+
+    }
   }
 }
