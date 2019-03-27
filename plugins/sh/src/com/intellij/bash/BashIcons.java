f21f368bcbd6bb7a1e2aaeacd0296bdb9be07b00
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-03-28 00:47:50 +0300

    let's update the icon
    GitOrigin-RevId: 105d95138cf03e27e954fa34b23ac7a33685efc6

--- plugins/sh/src/com/intellij/bash/BashIcons.java
+++ plugins/sh/src/com/intellij/bash/BashIcons.java
@@ -1,9 +1,9 @@
 package com.intellij.bash;
 
-import com.intellij.icons.AllIcons;
+import com.intellij.openapi.util.IconLoader;
 
 import javax.swing.*;
 
 public class BashIcons {
-  public static final Icon FILE_TYPE = AllIcons.Debugger.Console;
+  public static final Icon FILE_TYPE = IconLoader.getIcon("/icons/bash.svg");
 }
