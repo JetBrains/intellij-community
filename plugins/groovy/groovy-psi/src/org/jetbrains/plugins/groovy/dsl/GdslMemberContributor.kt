680e9a18b82ce707f533cc49138298b2143fab46
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-03-27 15:10:45 +0300

    [groovy] gdsl: extract separate non-code member contributor

---
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/dsl/GdslMemberContributor.kt
@@ -0,0 +1,17 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.plugins.groovy.dsl
+
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiType
+import com.intellij.psi.ResolveState
+import com.intellij.psi.scope.PsiScopeProcessor
+import org.jetbrains.plugins.groovy.lang.resolve.NonCodeMembersContributor
+
+class GdslMemberContributor : NonCodeMembersContributor() {
+
+  override fun processDynamicElements(qualifierType: PsiType, processor: PsiScopeProcessor, place: PsiElement, state: ResolveState) {
+    GroovyDslFileIndex.processExecutors(qualifierType, place) { holder, descriptor ->
+      holder.processMembers(descriptor, processor, state)
+    }
+  }
+}
