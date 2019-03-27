5171d9399d205796448db4a3c884b8e299c15571
Bas Leijdekkers <basleijdekkers@gmail.com>
2019-03-27 11:43:13 +0100

    IG: don't warn when surrounding class is anonymous (IDEA-209563)
    for "Unqualified inner class access" inspection

--- plugins/InspectionGadgets/src/com/siyeh/ig/style/UnqualifiedInnerClassAccessInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/style/UnqualifiedInnerClassAccessInspection.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2010-2018 Bas Leijdekkers
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.siyeh.ig.style;
 
 import com.intellij.codeInspection.CleanupLocalInspectionTool;
@@ -35,10 +21,13 @@ import org.jetbrains.annotations.NotNull;
 import javax.swing.*;
 import java.util.*;
 
+/**
+ * @author Bas Leijdekkers
+ */
 public class UnqualifiedInnerClassAccessInspection extends BaseInspection implements CleanupLocalInspectionTool{
 
   @SuppressWarnings({"PublicField"})
-  public boolean ignoreReferencesToLocalInnerClasses = false;
+  public boolean ignoreReferencesToLocalInnerClasses = true;
 
   @Override
   public JComponent createOptionsPanel() {
@@ -288,7 +277,7 @@ public class UnqualifiedInnerClassAccessInspection extends BaseInspection implem
           return;
       }
       final PsiClass containingClass = aClass.getContainingClass();
-      if (containingClass == null) {
+      if (containingClass == null || containingClass instanceof PsiAnonymousClass) {
         return;
       }
       if (ignoreReferencesToLocalInnerClasses) {
