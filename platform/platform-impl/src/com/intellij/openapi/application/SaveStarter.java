666ca70c7f66a68dd918890cf4a3d0fd48d22851
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-03-28 23:42:46 +0100

    Cleanup (warnings; formatting)

--- platform/platform-impl/src/com/intellij/openapi/application/SaveStarter.java
+++ platform/platform-impl/src/com/intellij/openapi/application/SaveStarter.java
@@ -1,6 +1,7 @@
-// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.application;
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class SaveStarter extends ApplicationStarterBase {
@@ -14,7 +15,7 @@ public class SaveStarter extends ApplicationStarterBase {
   }
 
   @Override
-  protected void processCommand(String[] args, @Nullable String currentDirectory) throws Exception {
+  protected void processCommand(@NotNull String[] args, @Nullable String currentDirectory) {
     saveAll();
   }
 }
