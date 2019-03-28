0375686a84306b14af4af48adf940cd39dec7a47
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-03-28 22:52:01 +0300

    update the file icon
    GitOrigin-RevId: fcb35ac8322b7d2265390ae26bd00e3a4564f1fe

--- plugins/sh/src/com/intellij/bash/BashIcons.java
+++ plugins/sh/src/com/intellij/bash/BashIcons.java
@@ -4,6 +4,6 @@ import com.intellij.openapi.util.IconLoader;
 
 import javax.swing.*;
 
-public class BashIcons {
-  public static final Icon FILE_TYPE = IconLoader.getIcon("/icons/bash.svg");
+class BashIcons {
+  static final Icon FILE_TYPE = IconLoader.getIcon("/icons/bashFile.svg");
 }
