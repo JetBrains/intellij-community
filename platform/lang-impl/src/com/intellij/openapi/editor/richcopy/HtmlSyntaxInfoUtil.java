1c516754a1cee4481af62c2a0257444a521bff19
anstarovoyt <andrey.starovoyt@jetbrains.com>
2019-03-29 16:16:05 +0300

    platform html syntax: simplify api
    (cherry picked from commit 2f76cb98d376a0fde7c9a2fe584c6beefb879574)

--- platform/lang-impl/src/com/intellij/openapi/editor/richcopy/HtmlSyntaxInfoUtil.java
+++ platform/lang-impl/src/com/intellij/openapi/editor/richcopy/HtmlSyntaxInfoUtil.java
@@ -2,6 +2,7 @@
 package com.intellij.openapi.editor.richcopy;
 
 import com.intellij.ide.highlighter.HighlighterFactory;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.colors.EditorColorsScheme;
 import com.intellij.openapi.editor.highlighter.EditorHighlighter;
 import com.intellij.openapi.editor.richcopy.model.SyntaxInfo;
@@ -10,15 +11,17 @@ import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.IOException;
+
 public final class HtmlSyntaxInfoUtil {
   
-  @NotNull
-  public static HtmlSyntaxInfoReader createHighlighterHtmlReader(@NotNull PsiFile file,
-                                                                 @NotNull CharSequence text,
-                                                                 @Nullable SyntaxInfoBuilder.RangeIterator ownRangeIterator,
-                                                                 @NotNull EditorColorsScheme schemeToUse,
-                                                                 int startOffset,
-                                                                 int endOffset) {
+  @Nullable
+  public static CharSequence getHtmlContent(@NotNull PsiFile file,
+                                            @NotNull CharSequence text,
+                                            @Nullable SyntaxInfoBuilder.RangeIterator ownRangeIterator,
+                                            @NotNull EditorColorsScheme schemeToUse,
+                                            int startOffset,
+                                            int endOffset) {
     EditorHighlighter highlighter =
       HighlighterFactory.createHighlighter(file.getViewProvider().getVirtualFile(), schemeToUse, file.getProject());
     highlighter.setText(text);
@@ -29,29 +32,35 @@ public final class HtmlSyntaxInfoUtil {
                        ? highlighterRangeIterator
                        : new SyntaxInfoBuilder.CompositeRangeIterator(schemeToUse, highlighterRangeIterator, ownRangeIterator);
 
-    return createHighlighterHtmlReader(text, ownRangeIterator, schemeToUse, endOffset);
+    return getHtmlContent(text, ownRangeIterator, schemeToUse, endOffset);
   }
 
-  private static HtmlSyntaxInfoReader createHighlighterHtmlReader(@NotNull CharSequence text,
-                                                                  @NotNull SyntaxInfoBuilder.RangeIterator ownRangeIterator,
-                                                                  @NotNull EditorColorsScheme schemeToUse,
-                                                                  int endOffset) {
+  @Nullable
+  public static CharSequence getHtmlContent(@NotNull CharSequence text,
+                                            @NotNull SyntaxInfoBuilder.RangeIterator ownRangeIterator,
+                                            @NotNull EditorColorsScheme schemeToUse,
+                                            int stopOffset) {
     SyntaxInfoBuilder.Context context = new SyntaxInfoBuilder.Context(text, schemeToUse, 0);
     SyntaxInfoBuilder.MyMarkupIterator iterator = new SyntaxInfoBuilder.MyMarkupIterator(text, ownRangeIterator, schemeToUse);
 
     try {
-      context.iterate(iterator, endOffset);
+      context.iterate(iterator, stopOffset);
     }
     finally {
       iterator.dispose();
     }
     SyntaxInfo info = context.finish();
-    HtmlSyntaxInfoReader data = new SimpleHtmlSyntaxInfoReader(info);
-    data.setRawText(text.toString());
-    return data;
+    try (HtmlSyntaxInfoReader data = new SimpleHtmlSyntaxInfoReader(info)) {
+      data.setRawText(text.toString());
+      return data.getBuffer();
+    }
+    catch (IOException e) {
+      Logger.getInstance(HtmlSyntaxInfoUtil.class).error(e);
+    }
+    return null;
   }
 
-  private static class SimpleHtmlSyntaxInfoReader extends HtmlSyntaxInfoReader {
+  private final static class SimpleHtmlSyntaxInfoReader extends HtmlSyntaxInfoReader {
     
     public SimpleHtmlSyntaxInfoReader(SyntaxInfo info) {
       super(info, 2);
