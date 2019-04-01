c0d81b38b2185d2b590548a4482d9734b98bea1e
Dmitry.Krasilschikov <kradima@jetbrains.com>
2019-04-01 21:01:53 +0300

    IDEA-209684 hide RA popup on a popup appearing

--- platform/lang-impl/src/com/intellij/ide/actions/runAnything/RunAnythingPopupUI.java
+++ platform/lang-impl/src/com/intellij/ide/actions/runAnything/RunAnythingPopupUI.java
@@ -179,6 +179,8 @@ public class RunAnythingPopupUI extends BigPopupUI {
             myAlarm.cancelAllRequests();
 
             ApplicationManager.getApplication().invokeLater(() -> ActionToolbarImpl.updateAllToolbarsImmediately());
+
+            searchFinishedHandler.run();
           }
           finally {
             result.setDone();
