8ae6c1e21358d2a576c404685b9e7d274fcf8701
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-04-01 16:59:23 +0200

    IDEA-CR-45975: [groovy] parse attribute property reference (IDEA-209923)
    (cherry picked from commit c0eac01abd4439650c5411b133f63d26f54f69cd)

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/parser/ExpressionsParsingTest.groovy
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/parser/ExpressionsParsingTest.groovy
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.lang.parser
 /**
  * @author peter
@@ -341,6 +341,8 @@ class ExpressionsParsingTest extends GroovyParsingTestCase {
   void testpath$path8() throws Throwable { doTest() }
 
   void testpath$path9() throws Throwable { doTest() }
+
+  void testpath$path16() throws Throwable { doTest() }
 
   void testpath$regexp() { doTest() }
 
