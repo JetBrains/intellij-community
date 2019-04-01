9dc7d54f55e8a60b6fbe157ae5b01cfa6693151b
Dmitry Zhuravlev <dmitry.zhuravlev@jetbrains.com>
2019-04-01 17:24:53 +0300

    git: set READ_OPTIONAL_LOCKING policy for ls-files command

--- plugins/git4idea/src/git4idea/commands/GitCommand.java
+++ plugins/git4idea/src/git4idea/commands/GitCommand.java
@@ -50,7 +50,7 @@ public class GitCommand {
   public static final GitCommand FETCH = read("fetch");  // fetch is a read-command, because it doesn't modify the index
   public static final GitCommand INIT = write("init");
   public static final GitCommand LOG = read("log");
-  public static final GitCommand LS_FILES = read("ls-files");
+  public static final GitCommand LS_FILES = readOptional("ls-files");
   public static final GitCommand LS_TREE = read("ls-tree");
   public static final GitCommand LS_REMOTE = read("ls-remote");
   public static final GitCommand MERGE = write("merge");
