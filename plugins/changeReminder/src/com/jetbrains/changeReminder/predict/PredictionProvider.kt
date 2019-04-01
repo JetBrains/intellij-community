ea89ff0d15e7400a942d8b7157d17dece11fd268
Nikolay.Rykunov <Nikolay.Rykunov@jetbrains.com>
2019-04-01 14:43:33 +0300

    Implement collecting of factors for new prediction model

--- plugins/changeReminder/src/com/jetbrains/changeReminder/predict/PredictionProvider.kt
+++ plugins/changeReminder/src/com/jetbrains/changeReminder/predict/PredictionProvider.kt
@@ -1,8 +1,11 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.changeReminder.predict
 
+import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vcs.FilePath
+import com.jetbrains.changeReminder.prediction.model.MLWhiteBox
 import com.jetbrains.changeReminder.repository.Commit
+import kotlin.math.max
 import kotlin.math.min
 
 /**
@@ -12,45 +15,104 @@ import kotlin.math.min
  */
 class PredictionProvider(private val minProb: Double = 0.3) {
   companion object {
-    private const val m = 3.2
-    private const val commitSize = 8.0
-    private const val historyLength = 20
+    private const val HISTORY_DEPTH = 20
+    private const val MAX_RELATED_FILES_COUNT = 150
+    private const val MAX_HISTORY_COMMIT_SIZE = 15
+    private const val DEFAULT_MIN_VALUE = 470_000_000_000.0
   }
 
-  private class VoteProvider(private val m: Double) {
-    private var votesCounter = 0.0
-    private var votesSum = 0.0
+  private class FactorCounter {
+    var sum = 0.0
+      private set
+    var max = 0.0
+      private set
+    var min = DEFAULT_MIN_VALUE
+      private set
 
-    private fun R() = if (votesCounter != 0.0) votesSum / votesCounter else 0.0
-
-    fun result(): Double {
-      val v = votesCounter
-      return (v / (m + v)) * R() + (m / (m + v)) * 0.25
+    fun append(newValue: Number): FactorCounter {
+      return append(newValue.toDouble())
     }
 
-    fun vote(rate: Double) {
-      votesCounter += 1.0
-      votesSum += rate
+    private fun append(newValue: Double): FactorCounter {
+      sum += newValue
+      max = max(max, newValue)
+      min = min(min, newValue)
+
+      return this
     }
   }
 
-  private fun vote(fileCommits: Set<Commit>, commitFiles: Set<FilePath>): Map<FilePath, Double> {
-    val candidates = HashMap<FilePath, VoteProvider>()
-    val commits = fileCommits
-      .sortedBy { it.time }
-      .reversed()
-      .take(historyLength)
-    for (fileCommit in commits) {
-      for (secondFile in fileCommit.files) {
-        if (secondFile in commitFiles) {
-          continue
-        }
-        val currentRate = min(1.0, commitSize / fileCommit.files.size.toDouble())
-        candidates.getOrPut(secondFile) { VoteProvider(m) }.vote(currentRate)
+  private fun collectFileFactors(commit: Commit, fileToPredict: FilePath, fileToPredictCommits: Set<Commit>): DoubleArray {
+    val factors = DoubleArray(Factor.values().size)
+
+    val timeDistanceCounter = FactorCounter()
+    val intersectionCounters = mutableMapOf<FilePath, FactorCounter>()
+    val countFilesCounter = FactorCounter()
+    val filePrefixCounter = FactorCounter()
+    val pathPrefixCounter = FactorCounter()
+    var authorCommitted = 0.0
+
+    fileToPredictCommits.forEach { fileToPredictCommit ->
+      val filesFromCommit = fileToPredictCommit.files.filter { file -> file in commit.files }
+
+      countFilesCounter.append(filesFromCommit.size)
+      if (filesFromCommit.isNotEmpty()) {
+        timeDistanceCounter.append(commit.time - fileToPredictCommit.time)
+      }
+
+      if (authorCommitted != 1.0 && fileToPredictCommit.author.startsWith(commit.author)) {
+        authorCommitted = 1.0
+      }
+
+      filesFromCommit.forEach { file ->
+        intersectionCounters.getOrPut(file) { FactorCounter() }.append(1)
       }
     }
 
-    return candidates.mapValues { it.value.result() }
+    val fileToPredictPath = fileToPredict.path
+    val fileToPredictName = fileToPredict.name
+    commit.files.forEach {
+      pathPrefixCounter.append(StringUtil.commonPrefixLength(fileToPredictPath, it.path))
+      filePrefixCounter.append(StringUtil.commonPrefixLength(fileToPredictName, it.name))
+    }
+
+    factors[Factor.MAX_INTERSECTION.ordinal] = intersectionCounters.values.maxBy { it.sum }?.sum ?: 0.0
+    factors[Factor.SUM_INTERSECTION.ordinal] = intersectionCounters.values.sumByDouble { it.sum }
+    factors[Factor.MIN_DISTANCE_TIME.ordinal] = timeDistanceCounter.min
+    factors[Factor.COMMIT_SIZE.ordinal] = commit.files.size.toDouble()
+    factors[Factor.MAX_DISTANCE_TIME.ordinal] = timeDistanceCounter.max
+    factors[Factor.AVG_DISTANCE_TIME.ordinal] = timeDistanceCounter.sum / fileToPredictCommits.size.toDouble()
+    factors[Factor.MAX_COUNT.ordinal] = countFilesCounter.max
+    factors[Factor.MIN_COUNT.ordinal] = countFilesCounter.min
+    factors[Factor.AUTHOR_COMMITTED_THE_FILE.ordinal] = authorCommitted
+    factors[Factor.MAX_PREFIX_PATH.ordinal] = pathPrefixCounter.max
+    factors[Factor.MAX_PREFIX_FILE_NAME.ordinal] = filePrefixCounter.max
+
+    return factors
+  }
+
+
+  private fun getRelatedFiles(sortedFilesHistory: Map<FilePath, List<Commit>>): Map<FilePath, Set<Commit>> {
+    val relatedFiles = mutableMapOf<FilePath, MutableSet<Commit>>()
+    val commitFiles = sortedFilesHistory.keys
+
+    for (depth in 0 until HISTORY_DEPTH) {
+      if (relatedFiles.size > MAX_RELATED_FILES_COUNT) {
+        break
+      }
+      sortedFilesHistory.values.forEach { commits ->
+        if (depth < commits.size) {
+          val commitFromHistory = commits[depth]
+          if (commitFromHistory.files.size > MAX_HISTORY_COMMIT_SIZE) {
+            commitFromHistory.files.filter { it !in commitFiles }.forEach { relatedFile ->
+              relatedFiles.getOrPut(relatedFile) { mutableSetOf() }.add(commitFromHistory)
+            }
+          }
+        }
+      }
+    }
+
+    return relatedFiles
   }
 
   /**
@@ -60,22 +122,16 @@ class PredictionProvider(private val minProb: Double = 0.3) {
    * @param maxPredictedFileCount maximum files to be predicted
    * @return list of forgotten files
    */
-  fun predictForgottenFiles(filesHistory: Map<FilePath, Set<Commit>>, maxPredictedFileCount: Int = 5): List<FilePath> {
-    val candidates = HashMap<FilePath, Double>()
-
-    for (commits in filesHistory.values) {
-      val currentVotes = vote(commits, filesHistory.keys)
-      for ((currentFile, currentVote) in currentVotes) {
-        candidates.merge(currentFile, currentVote, Double::plus)
+  fun predictForgottenFiles(commit: Commit, filesHistory: Map<FilePath, Set<Commit>>, maxPredictedFileCount: Int = 5): List<FilePath> =
+    getRelatedFiles(filesHistory.mapValues { it.value.sortedByDescending { commit -> commit.time } })
+      .asSequence()
+      .map { (fileToPredict, fileToPredictCommits) ->
+        val fileScore = MLWhiteBox.makePredict(collectFileFactors(commit, fileToPredict, fileToPredictCommits))
+        fileToPredict to fileScore
       }
-    }
-
-    return candidates
-      .mapValues { it.value / filesHistory.size }
-      .filterValues { it > minProb }
-      .toList()
+      .filter { it.second > minProb }
       .sortedByDescending { it.second }
       .take(maxPredictedFileCount)
       .map { it.first }
-  }
+      .toList()
 }
