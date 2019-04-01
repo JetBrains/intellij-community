4c3efbb4d5535235d0e16aca3edcff9050e356d6
Ying Chen <chying@google.com>
2019-04-01 16:29:35 -0700

    Fix Gradle Daemons window with Gradle 5.3
    The constructor of DaemonParameters class in Gradle tooling API
    is updated since Gradle 5.3. It now takes FileCollectionFactory
    as required parameter.
    This change calls the constructor by reflection if current Gradle
    version is newer than 5.3.
    
    Change-Id: I21eeb8f1e95267892ab378a98ca4517eb3d50e07

--- plugins/gradle/src/org/jetbrains/plugins/gradle/internal/daemon/DaemonAction.java
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/internal/daemon/DaemonAction.java
@@ -3,15 +3,19 @@
  */
 package org.jetbrains.plugins.gradle.internal.daemon;
 
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import org.gradle.api.internal.file.DefaultFileCollectionFactory;
+import org.gradle.api.internal.file.FileCollectionFactory;
+import org.gradle.api.logging.Logging;
 import org.gradle.initialization.BuildLayoutParameters;
 import org.gradle.internal.logging.events.OutputEvent;
 import org.gradle.internal.logging.events.OutputEventListener;
 import org.gradle.internal.service.ServiceRegistry;
 import org.gradle.launcher.daemon.client.DaemonClientFactory;
 import org.gradle.launcher.daemon.configuration.DaemonParameters;
+import org.gradle.util.GradleVersion;
 import org.jetbrains.annotations.NotNull;
-
-import java.io.File;
 
 /**
  * @author Vladislav.Soroka
@@ -23,12 +27,29 @@ public abstract class DaemonAction {
     myServiceDirectoryPath = serviceDirectoryPath;
   }
 
-  protected ServiceRegistry getDaemonServices(DaemonClientFactory daemonClientFactory) {
+  protected ServiceRegistry getDaemonServices(DaemonClientFactory daemonClientFactory)
+    throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
     BuildLayoutParameters layout = new BuildLayoutParameters();
     if (myServiceDirectoryPath != null && !myServiceDirectoryPath.isEmpty()) {
       layout.setGradleUserHomeDir(new File(myServiceDirectoryPath));
     }
-    DaemonParameters daemonParameters = new DaemonParameters(layout);
+    DaemonParameters daemonParameters;
+    boolean isGradle5Dot3OrNewer = GradleVersion.current().getBaseVersion().compareTo(GradleVersion.version("5.3")) >= 0;
+    if (!isGradle5Dot3OrNewer) {
+      daemonParameters = new DaemonParameters(layout);
+    }
+    else {
+      try {
+        //noinspection JavaReflectionMemberAccess
+        daemonParameters = DaemonParameters.class.getConstructor(BuildLayoutParameters.class, FileCollectionFactory.class)
+          .newInstance(layout, new DefaultFileCollectionFactory());
+      }
+      catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+        Logging.getLogger(this.getClass())
+          .error("Cannot create DaemonParameters by reflection, gradle version " + GradleVersion.current(), e);
+        throw e;
+      }
+    }
     return daemonClientFactory.createStopDaemonServices(new OutputEventListener() {
       @Override
       public void onOutput(OutputEvent event) { }
