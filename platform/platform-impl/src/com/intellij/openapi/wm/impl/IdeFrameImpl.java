9f68e5d3e7c134c18fc0df4dce945c67fc146c21
graann <Anna.Gromova@jetbrains.com>
2019-04-02 18:26:42 +0300

    Borderless UI. (#IDEA-205359)

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/IdeFrameImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/IdeFrameImpl.java
@@ -44,6 +44,7 @@ import com.intellij.ui.BalloonLayoutImpl;
 import com.intellij.ui.ScreenUtil;
 import com.intellij.ui.mac.MacMainFrameDecorator;
 import com.intellij.util.containers.ContainerUtil;
+import com.intellij.util.ui.JBDimension;
 import com.intellij.util.ui.JBUI;
 import com.intellij.util.ui.UIUtil;
 import com.intellij.util.ui.accessibility.AccessibleContextAccessor;
@@ -97,6 +98,7 @@ public class IdeFrameImpl extends JFrame implements IdeFrameEx, AccessibleContex
     size.height= Math.min(1000, size.height - 40);
     setSize(size);
     setLocationRelativeTo(null);
+    setMinimumSize(new Dimension(340, getMinimumSize().height));
 
     if (Registry.is("suppress.focus.stealing") &&
         Registry.is("suppress.focus.stealing.auto.request.focus") &&
@@ -255,6 +257,14 @@ public class IdeFrameImpl extends JFrame implements IdeFrameEx, AccessibleContex
   @Override
   public synchronized void setMaximizedBounds(Rectangle bounds) {
     super.setMaximizedBounds(null);
+  }
+
+  @Override
+  public void setExtendedState(int state) {
+    if (getExtendedState() == Frame.NORMAL && (state & Frame.MAXIMIZED_BOTH) != 0) {
+      getRootPane().putClientProperty("normalBounds", getBounds());
+    }
+    super.setExtendedState(state);
   }
 
   private void setupCloseAction() {
