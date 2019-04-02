6b525926091e6ae7da86636df01476352b66eef4
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-04-02 23:40:55 +0300

    format with shfmt, initial
    GitOrigin-RevId: a6f330a030f1cd193f8228d2e36aee883a804e07

---
+++ plugins/sh/src/com/intellij/bash/formatter/BashExternalFormatter.java
@@ -0,0 +1,106 @@
+package com.intellij.bash.formatter;
+
+import com.intellij.bash.psi.BashFile;
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.configurations.GeneralCommandLine;
+import com.intellij.execution.process.CapturingProcessAdapter;
+import com.intellij.execution.process.OSProcessHandler;
+import com.intellij.execution.process.ProcessEvent;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.WriteAction;
+import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.openapi.command.undo.UndoConstants;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.util.registry.Registry;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.codeStyle.ExternalFormatProcessor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+
+// todo: rewrite with the future API, see IDEA-203568
+public class BashExternalFormatter implements ExternalFormatProcessor {
+  @Override
+  public boolean activeForFile(@NotNull PsiFile file) {
+    return file instanceof BashFile;
+  }
+
+  @Nullable
+  @Override
+  public TextRange format(@NotNull PsiFile source, @NotNull TextRange range, boolean canChangeWhiteSpacesOnly) {
+    doFormat(source.getProject(), source.getVirtualFile());
+    return range;
+  }
+
+  private void doFormat(@NotNull Project project, @Nullable VirtualFile file) {
+    if (file == null || !file.exists()) return;
+    String shFmtExecutable = Registry.stringValue("bash.shfmt.path");
+    if (StringUtil.isEmpty(shFmtExecutable)) return;
+    String filePath = file.getPath();
+    String realPath = FileUtil.toSystemDependentName(filePath);
+    if (!new File(realPath).exists()) return;
+
+    FileDocumentManager documentManager = FileDocumentManager.getInstance();
+    Document document = FileDocumentManager.getInstance().getDocument(file);
+    if (document == null) return;
+
+    long before = document.getModificationStamp();
+    documentManager.saveDocument(document);
+
+    try {
+      GeneralCommandLine commandLine = new GeneralCommandLine()
+          .withParentEnvironmentType(GeneralCommandLine.ParentEnvironmentType.CONSOLE)
+          .withExePath(shFmtExecutable)
+          .withParameters(realPath);
+
+      OSProcessHandler handler = new OSProcessHandler(commandLine);
+      handler.addProcessListener(new CapturingProcessAdapter() {
+        @Override
+        public void processTerminated(@NotNull ProcessEvent event) {
+          int exitCode = event.getExitCode();
+          if (exitCode == 0) {
+            String text = getOutput().getStdout();
+            ApplicationManager.getApplication().invokeLater(() -> {
+              long after = document.getModificationStamp();
+              if (after > before) {
+                // todo: document has already been changed
+                return;
+              }
+              CommandProcessor.getInstance().executeCommand(project, () -> {
+                WriteAction.run(() -> {
+                  document.setText(text);
+                  FileDocumentManager.getInstance().saveDocument(document);
+                });
+                file.putUserData(UndoConstants.FORCE_RECORD_UNDO, null);
+              }, "Reformat Code with " + getId(), null, document);
+            });
+          }
+          else {
+            showFailedNotification();
+          }
+        }
+      });
+      handler.startNotify();
+    }
+    catch (ExecutionException e) {
+      showFailedNotification();
+    }
+  }
+
+  private static void showFailedNotification() {
+    // todo: add notification
+  }
+
+  @NotNull
+  @Override
+  public String getId() {
+    return "shfmt";
+  }
+}
