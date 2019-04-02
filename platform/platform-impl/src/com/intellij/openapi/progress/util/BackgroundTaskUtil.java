7411e649bbc77065af9a5acd6f5b754f4cd7042b
Aleksey Pivovarov <AMPivovarov@gmail.com>
2019-04-02 16:05:26 +0300

    vcs: execute callbacks under correct progress indicator even in synchronous mode

--- platform/platform-impl/src/com/intellij/openapi/progress/util/BackgroundTaskUtil.java
+++ platform/platform-impl/src/com/intellij/openapi/progress/util/BackgroundTaskUtil.java
@@ -84,7 +84,7 @@ public class BackgroundTaskUtil {
     if (forceEDT) {
       ProgressIndicator indicator = new EmptyProgressIndicator(modality);
       try {
-        Runnable callback = backgroundTask.fun(indicator);
+        Runnable callback = ProgressManager.getInstance().runProcess(() -> backgroundTask.fun(indicator), indicator);
         finish(callback, indicator);
       }
       catch (ProcessCanceledException ignore) {
