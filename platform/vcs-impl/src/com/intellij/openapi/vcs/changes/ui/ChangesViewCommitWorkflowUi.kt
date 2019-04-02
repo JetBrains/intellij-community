c8da9b7162b9dfe1c2c80a3409d17c12e66cd128
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2019-04-02 12:40:29 +0300

    vcs: Add "Show Commit Options" action for non-modal commit

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/ChangesViewCommitWorkflowUi.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/ChangesViewCommitWorkflowUi.kt
@@ -1,8 +1,11 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vcs.changes.ui
 
+import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.vcs.changes.CommitWorkflowUi
 
 interface ChangesViewCommitWorkflowUi : CommitWorkflowUi {
   var isDefaultCommitActionEnabled: Boolean
+
+  fun showCommitOptions(options: CommitOptions, isFromToolbar: Boolean, dataContext: DataContext)
 }
