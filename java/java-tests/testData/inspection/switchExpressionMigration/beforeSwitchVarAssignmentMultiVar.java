60dc045f654581495504a51664434c9d271735f2
Roman.Ivanov <Roman.Ivanov@jetbrains.com>
2019-04-02 11:24:15 +0700

    EnhancedSwitchMigration: generate proper assignment, skip default branch if exists:  IDEA-208192

---
+++ java/java-tests/testData/inspection/switchExpressionMigration/beforeSwitchVarAssignmentMultiVar.java
@@ -0,0 +1,16 @@
+// "Replace with 'switch' expression" "true"
+import java.util.*;
+
+class SwitchExpressionMigration {
+  private static void m() {
+    String s = "foo";
+    int x = 5;
+    System.out.println(s);
+
+    switch<caret> (x) {
+      case 1: s = "bar";break;
+      case 2: s = "baz";break;
+      default: s = "zuq";break;
+    }
+  }
+}
