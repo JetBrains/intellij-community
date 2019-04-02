0ce6568fdfbd4a272c56d31ebd674995ef7f6edf
Svetlana.Zemlyanskaya <svetlana.zemlyanskaya@jetbrains.com>
2019-04-02 13:34:05 +0200

    FUS: move internal classes to impl module

--- platform/platform-impl/src/com/intellij/internal/statistic/eventLog/FeatureUsageLogger.kt
+++ platform/platform-impl/src/com/intellij/internal/statistic/eventLog/FeatureUsageLogger.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.internal.statistic.eventLog
 
 import com.intellij.openapi.application.ApplicationManager
@@ -16,9 +16,9 @@ import java.io.File
  * Note: FeatureUsageCollector API use this class under the hood.
  * Therefore, if you record statistic with FeatureUsageCollector API there's no need to record events in event log manually.
  *
- * @see com.intellij.internal.statistic.service.fus.collectors.FeatureUsagesCollector
- * @see com.intellij.internal.statistic.service.fus.collectors.ApplicationUsageTriggerCollector
- * @see com.intellij.internal.statistic.service.fus.collectors.ProjectUsageTriggerCollector
+ * @see com.intellij.internal.statistic.service.fus.collectors.FUCounterUsageLogger
+ * @see com.intellij.internal.statistic.service.fus.collectors.ApplicationUsagesCollector
+ * @see com.intellij.internal.statistic.service.fus.collectors.ProjectUsagesCollector
  */
 object FeatureUsageLogger {
   private val ourLogger : FeatureUsageEventLogger
