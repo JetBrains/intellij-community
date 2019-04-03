79af9f449cd335c37b14169149513a2bb0834510
Yann Cébron <yann.cebron@jetbrains.com>
2019-04-03 15:52:17 +0200

    [devkit] IdeaJdkConfigurable: use ComboBox

--- plugins/devkit/devkit-core/src/projectRoots/IdeaJdkConfigurable.java
+++ plugins/devkit/devkit-core/src/projectRoots/IdeaJdkConfigurable.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.devkit.projectRoots;
 
 import com.intellij.openapi.application.ApplicationManager;
@@ -8,6 +8,7 @@ import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
 import com.intellij.openapi.projectRoots.*;
 import com.intellij.openapi.projectRoots.impl.ProjectJdkImpl;
 import com.intellij.openapi.roots.OrderRootType;
+import com.intellij.openapi.ui.ComboBox;
 import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -35,7 +36,7 @@ public class IdeaJdkConfigurable implements AdditionalDataConfigurable {
 
   private final JLabel myInternalJreLabel = new JLabel("Internal Java Platform:");
   private final DefaultComboBoxModel<Sdk> myJdksModel = new DefaultComboBoxModel<>();
-  private final JComboBox<Sdk> myInternalJres = new JComboBox<>(myJdksModel);
+  private final JComboBox<Sdk> myInternalJres = new ComboBox<>(myJdksModel);
 
   private Sdk myIdeaJdk;
 
