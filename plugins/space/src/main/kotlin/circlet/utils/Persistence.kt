e16d191d441d5ade1d96fcd3142104791b072816
Leonid Khachaturov <Leonid.Khachaturov@jetbrains.com>
2019-04-04 14:46:14 +0200

    Cleanup
    GitOrigin-RevId: 2eb4e36b6fab069ec8beaf5f331bf331cddb223a

--- plugins/space/src/main/kotlin/circlet/utils/Persistence.kt
+++ plugins/space/src/main/kotlin/circlet/utils/Persistence.kt
@@ -32,11 +32,11 @@ object IdeaPasswordSafePersistence : Persistence {
     }
 
     override fun forEach(lifetime: Lifetime, key: String, callback: (String?) -> Unit) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented")
     }
 
     override suspend fun getAllKeys(): List<String> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented")
     }
 
     override suspend fun clear() {
