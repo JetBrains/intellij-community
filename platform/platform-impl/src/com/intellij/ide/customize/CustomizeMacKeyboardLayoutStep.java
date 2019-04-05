60e82593a4f812cda84da12ce4ed79129233782e
Dmitry Jemerov <yole@jetbrains.com>
2019-04-05 19:19:25 +0200

    Use full product name in keymap selection screen

--- platform/platform-impl/src/com/intellij/ide/customize/CustomizeMacKeyboardLayoutStep.java
+++ platform/platform-impl/src/com/intellij/ide/customize/CustomizeMacKeyboardLayoutStep.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.customize;
 
 import com.intellij.CommonBundle;
@@ -33,7 +19,7 @@ public class CustomizeMacKeyboardLayoutStep extends AbstractCustomizeWizardStep 
                    "table {margin:0px; cell-padding:0px; border:none;}" +
                    "</style>";
 
-    JRadioButton macRadioButton = new JRadioButton("I've never used " + ApplicationNamesInfo.getInstance().getProductName());
+    JRadioButton macRadioButton = new JRadioButton("I've never used " + ApplicationNamesInfo.getInstance().getFullProductName());
     macRadioButton.setOpaque(false);
     JPanel macPanel = createBigButtonPanel(new VerticalFlowLayout(), macRadioButton,
                                            () -> WelcomeWizardUtil.setWizardKeymap(KeymapManager.MAC_OS_X_10_5_PLUS_KEYMAP));
@@ -46,7 +32,7 @@ public class CustomizeMacKeyboardLayoutStep extends AbstractCustomizeWizardStep 
                             "</table></body></html>"));
     add(macPanel);
 
-    JRadioButton defaultRadioButton = new JRadioButton("I used " + ApplicationNamesInfo.getInstance().getProductName() + " before");
+    JRadioButton defaultRadioButton = new JRadioButton("I used " + ApplicationNamesInfo.getInstance().getFullProductName() + " before");
     defaultRadioButton.setOpaque(false);
     JPanel defaultPanel = createBigButtonPanel(new VerticalFlowLayout(), defaultRadioButton,
                                                () -> WelcomeWizardUtil.setWizardKeymap(KeymapManager.MAC_OS_X_KEYMAP));
