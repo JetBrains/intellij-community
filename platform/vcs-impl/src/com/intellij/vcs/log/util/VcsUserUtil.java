0e1acf39ad38e1784e76be94ec60d8e497ba4836
Nikolay.Rykunov <Nikolay.Rykunov@jetbrains.com>
2019-04-06 12:46:05 +0300

    Introduce new util method which provide VcsUser from String

--- platform/vcs-impl/src/com/intellij/vcs/log/util/VcsUserUtil.java
+++ platform/vcs-impl/src/com/intellij/vcs/log/util/VcsUserUtil.java
@@ -1,22 +1,11 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.vcs.log.util;
 
 import com.intellij.openapi.util.Couple;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.util.ArrayUtil;
 import com.intellij.vcs.log.VcsUser;
+import com.intellij.vcs.log.impl.VcsUserImpl;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -31,6 +20,20 @@ public class VcsUserUtil {
   @NotNull
   public static String toExactString(@NotNull VcsUser user) {
     return getString(user.getName(), user.getEmail());
+  }
+
+  @Nullable
+  public static VcsUser fromExactString(@NotNull String exactString) {
+    String[] tokens = exactString.trim().split("[< ]+");
+    if (tokens.length < 2) {
+      return null;
+    }
+    String name = StringUtil.join(ArrayUtil.remove(tokens, tokens.length - 1), " ");
+    String email = StringUtil.trimEnd(tokens[tokens.length - 1], '>');
+    if (!email.contains("@")) {
+      return null;
+    }
+    return new VcsUserImpl(name, email);
   }
 
   @NotNull
