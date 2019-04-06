a447d3f3d61323099dee40e705c43ca730d7d38b
Nikolay.Rykunov <Nikolay.Rykunov@jetbrains.com>
2019-04-06 12:54:42 +0300

    Simplify commit author correction and add test with empty email

--- plugins/git4idea/src/git4idea/checkin/GitCommitAuthorCorrector.java
+++ plugins/git4idea/src/git4idea/checkin/GitCommitAuthorCorrector.java
@@ -1,21 +1,11 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package git4idea.checkin;
 
+import com.intellij.vcs.log.VcsUser;
 import org.jetbrains.annotations.NotNull;
+
+import static com.intellij.vcs.log.util.VcsUserUtil.fromExactString;
+import static com.intellij.vcs.log.util.VcsUserUtil.toExactString;
 
 /**
  * Corrects some simple but popular mistakes on the author format.<p/>
@@ -25,31 +15,10 @@ class GitCommitAuthorCorrector {
 
   @NotNull
   public static String correct(@NotNull String author) {
-    author = author.trim();
-
-    int openBrace = author.indexOf('<');
-    int closeBrace = author.indexOf('>');
-
-    if (openBrace < 0) { // email should open with "<"
-      int at = author.lastIndexOf("@");
-      if (at < 0) {
-        return author;
-      }
-      int email = author.lastIndexOf(' ', at - 1);
-      if (email < 0) {
-        return author;
-      }
-      author = author.substring(0, email + 1) + "<" + author.substring(email + 1);
+    VcsUser user = fromExactString(author);
+    if (user == null) {
+      return author;
     }
-    else if (openBrace > 0 && author.charAt(openBrace - 1) != ' ') { // insert space before email
-      author = author.substring(0, openBrace) + " " + author.substring(openBrace);
-    }
-
-    if (closeBrace < 0) { // email should close with ">"
-      author += ">";
-    }
-
-    return author;
+    return toExactString(user);
   }
-
 }
