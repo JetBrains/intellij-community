e54bf01bc916b5147140ac2b2873c1f82fe10dc2
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-04-07 09:23:45 +0200

    move StartUpPerformanceReporter to proper package

--- platform/built-in-server/src/org/jetbrains/ide/StartUpMeasurementService.kt
+++ platform/built-in-server/src/org/jetbrains/ide/StartUpMeasurementService.kt
@@ -1,7 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.ide
 
-import com.intellij.ide.StartUpPerformanceReporter
+import com.intellij.diagnostic.StartUpPerformanceReporter
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.ex.ApplicationInfoEx
 import com.intellij.openapi.startup.StartupActivity
