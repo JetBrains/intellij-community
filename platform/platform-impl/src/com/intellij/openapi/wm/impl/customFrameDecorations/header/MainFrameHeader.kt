bfac356b354b020f55b592a21a89dd3938074408
graann <Anna.Gromova@jetbrains.com>
2019-04-07 15:54:42 +0300

    Refactoring. Borderless UI. (#IDEA-205359)

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/customFrameDecorations/header/MainFrameHeader.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/customFrameDecorations/header/MainFrameHeader.kt
@@ -5,13 +5,12 @@ import com.intellij.ide.DataManager
 import com.intellij.openapi.actionSystem.ex.ActionManagerEx
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.impl.IdeMenuBar
-import com.intellij.openapi.wm.impl.customFrameDecorations.titleLabel.CustomDecorationPath
+import com.intellij.openapi.wm.impl.customFrameDecorations.header.titleLabel.CustomDecorationPath
 import com.intellij.ui.awt.RelativeRectangle
 import com.intellij.util.ui.JBUI
 import net.miginfocom.swing.MigLayout
 import java.awt.Frame
 import java.awt.Rectangle
-import java.awt.event.WindowAdapter
 import java.util.ArrayList
 import javax.swing.JFrame
 import javax.swing.JPanel
