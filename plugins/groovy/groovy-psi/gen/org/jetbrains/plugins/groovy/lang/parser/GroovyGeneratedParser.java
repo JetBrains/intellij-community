2fb4907506002ddfe56784a251ee88644841ab4b
alexey.afanasiev <alexey.afanasiev@jetbrains.com>
2019-04-08 10:45:41 +0300

    IDEA-210375 Disable parsing lambda expressions in initializers of closure parameters

--- plugins/groovy/groovy-psi/gen/org/jetbrains/plugins/groovy/lang/parser/GroovyGeneratedParser.java
+++ plugins/groovy/groovy-psi/gen/org/jetbrains/plugins/groovy/lang/parser/GroovyGeneratedParser.java
@@ -1,3 +1,5 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
 // This is a generated file. Not intended for manual editing.
 package org.jetbrains.plugins.groovy.lang.parser;
 
@@ -2499,7 +2501,13 @@ public class GroovyGeneratedParser implements PsiParser, LightPsiParser {
   }
 
   /* ********************************************************** */
-  // <<comma_list (mb_nl parse_parameter)>>
+  // <<closureParameter parse_parameter>>
+  static boolean closure_parameter(PsiBuilder b, int l) {
+    return closureParameter(b, l + 1, GroovyGeneratedParser::parse_parameter);
+  }
+
+  /* ********************************************************** */
+  // <<comma_list (mb_nl closure_parameter)>>
   public static boolean closure_parameter_list(PsiBuilder b, int l) {
     if (!recursion_guard_(b, l, "closure_parameter_list")) return false;
     boolean r;
@@ -2509,13 +2517,13 @@ public class GroovyGeneratedParser implements PsiParser, LightPsiParser {
     return r;
   }
 
-  // mb_nl parse_parameter
+  // mb_nl closure_parameter
   private static boolean closure_parameter_list_0_0(PsiBuilder b, int l) {
     if (!recursion_guard_(b, l, "closure_parameter_list_0_0")) return false;
     boolean r;
     Marker m = enter_section_(b);
     r = mb_nl(b, l + 1);
-    r = r && parse_parameter(b, l + 1);
+    r = r && closure_parameter(b, l + 1);
     exit_section_(b, m, null, r);
     return r;
   }
@@ -8295,24 +8303,66 @@ public class GroovyGeneratedParser implements PsiParser, LightPsiParser {
     return r;
   }
 
-  // lambda_expression_base | clear_variants_and_fail
+  // !<<isClosureParameter>> lambda_expression_base | clear_variants_and_fail
   public static boolean lambda_expression(PsiBuilder b, int l) {
     if (!recursion_guard_(b, l, "lambda_expression")) return false;
     boolean r;
     Marker m = enter_section_(b, l, _NONE_, LAMBDA_EXPRESSION, "<lambda expression>");
-    r = lambda_expression_base(b, l + 1);
+    r = lambda_expression_0(b, l + 1);
     if (!r) r = clear_variants_and_fail(b, l + 1);
     exit_section_(b, l, m, r, false, null);
     return r;
   }
 
-  // single_parameter_lambda_expression_base | clear_variants_and_fail
+  // !<<isClosureParameter>> lambda_expression_base
+  private static boolean lambda_expression_0(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "lambda_expression_0")) return false;
+    boolean r;
+    Marker m = enter_section_(b);
+    r = lambda_expression_0_0(b, l + 1);
+    r = r && lambda_expression_base(b, l + 1);
+    exit_section_(b, m, null, r);
+    return r;
+  }
+
+  // !<<isClosureParameter>>
+  private static boolean lambda_expression_0_0(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "lambda_expression_0_0")) return false;
+    boolean r;
+    Marker m = enter_section_(b, l, _NOT_);
+    r = !isClosureParameter(b, l + 1);
+    exit_section_(b, l, m, r, false, null);
+    return r;
+  }
+
+  // !<<isClosureParameter>> single_parameter_lambda_expression_base | clear_variants_and_fail
   public static boolean single_parameter_lambda_expression(PsiBuilder b, int l) {
     if (!recursion_guard_(b, l, "single_parameter_lambda_expression")) return false;
     boolean r;
     Marker m = enter_section_(b, l, _NONE_, LAMBDA_EXPRESSION, "<single parameter lambda expression>");
-    r = single_parameter_lambda_expression_base(b, l + 1);
+    r = single_parameter_lambda_expression_0(b, l + 1);
     if (!r) r = clear_variants_and_fail(b, l + 1);
+    exit_section_(b, l, m, r, false, null);
+    return r;
+  }
+
+  // !<<isClosureParameter>> single_parameter_lambda_expression_base
+  private static boolean single_parameter_lambda_expression_0(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "single_parameter_lambda_expression_0")) return false;
+    boolean r;
+    Marker m = enter_section_(b);
+    r = single_parameter_lambda_expression_0_0(b, l + 1);
+    r = r && single_parameter_lambda_expression_base(b, l + 1);
+    exit_section_(b, m, null, r);
+    return r;
+  }
+
+  // !<<isClosureParameter>>
+  private static boolean single_parameter_lambda_expression_0_0(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "single_parameter_lambda_expression_0_0")) return false;
+    boolean r;
+    Marker m = enter_section_(b, l, _NOT_);
+    r = !isClosureParameter(b, l + 1);
     exit_section_(b, l, m, r, false, null);
     return r;
   }
