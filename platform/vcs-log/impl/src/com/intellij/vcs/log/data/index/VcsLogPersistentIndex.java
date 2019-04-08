JetBrains/intellij-community

commit 9b62f1e5e2c75f0032763dbcbc7b5f63075464f6

[vcs-log] introduce CompressedDetails for indexing
Current implementation of VcsFullCommitDetails/VcsIndexableDetails stores changes in form of VcsFileStatusInfo or (when parsed) Change, thus sometimes using a lot of memory. Since VcsLogPathsIndex eventually converts paths to integers to store them on disk, it makes sense to just move this step forward. VcsLogIndexer receives PathsEncoder from VcsLogPersistentIndex in order to do the conversion, and stores resulting integers in CompressedDetails instead of VcsFullCommitDetails/VcsIndexableDetails.

IDEA-178090, IDEA-167341

platform/vcs-log/impl/src/com/intellij/vcs/log/data/index/VcsLogPersistentIndex.java
@@ -40,11 +40,14 @@ import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.indexing.StorageException;
 import com.intellij.util.io.*;
 import com.intellij.vcs.log.*;
+import com.intellij.vcs.log.impl.VcsIndexableLogProvider;
+import com.intellij.vcs.log.impl.VcsLogIndexer;
 import com.intellij.vcs.log.data.SingleTaskController;
 import com.intellij.vcs.log.data.VcsLogProgress;
 import com.intellij.vcs.log.data.VcsLogStorage;
 import com.intellij.vcs.log.data.VcsLogStorageImpl;
-import com.intellij.vcs.log.impl.*;
+import com.intellij.vcs.log.impl.FatalErrorHandler;
+import com.intellij.vcs.log.impl.HeavyAwareExecutor;
 import com.intellij.vcs.log.statistics.VcsLogIndexCollector;
 import com.intellij.vcs.log.util.*;
 import com.intellij.vcsUtil.VcsUtil;
@@ -178,7 +181,7 @@ public class VcsLogPersistentIndex implements VcsLogModifiableIndex, Disposable 
         continue;
       }
 
-      mySingleTaskController.request(new IndexingRequest(root, commits, isFull, false));
+      mySingleTaskController.request(new IndexingRequest(root, myIndexStorage.paths.getPathsEncoder(), commits, isFull, false));
     }
 
     if (isFull) {
@@ -187,7 +190,7 @@ public class VcsLogPersistentIndex implements VcsLogModifiableIndex, Disposable 
     }
   }
 
-  private void storeDetail(@NotNull VcsFullCommitDetails detail) {
+  private void storeDetail(@NotNull VcsLogIndexer.CompressedDetails detail) {
     if (myIndexStorage == null) return;
     try {
       int index = myStorage.getCommitIndex(detail.getId(), detail.getRoot());
@@ -195,10 +198,10 @@ public class VcsLogPersistentIndex implements VcsLogModifiableIndex, Disposable 
       myIndexStorage.messages.put(index, detail.getFullMessage());
       myIndexStorage.trigrams.update(index, detail);
       myIndexStorage.users.update(index, detail);
-      if (detail instanceof VcsIndexableDetails) myIndexStorage.paths.update(index, (VcsIndexableDetails)detail);
+      myIndexStorage.paths.update(index, detail);
       myIndexStorage.parents.put(index, ContainerUtil.map(detail.getParents(), p -> myStorage.getCommitIndex(p, detail.getRoot())));
       // we know the whole graph without timestamps now
-      if (!(detail instanceof VcsIndexableDetails) || ((VcsIndexableDetails)detail).hasRenames()) {
+      if (detail.hasRenames()) {
         myIndexStorage.renames.put(index);
       }
       if (!detail.getAuthor().equals(detail.getCommitter())) {
@@ -279,8 +282,9 @@ public class VcsLogPersistentIndex implements VcsLogModifiableIndex, Disposable 
   @Override
   public synchronized void reindexWithRenames(int commit, @NotNull VirtualFile root) {
     LOG.assertTrue(myRoots.contains(root));
-    if (hasRenames(commit)) return;
-    mySingleTaskController.request(new IndexingRequest(root, TroveUtil.singleton(commit), false, true));
+    if (hasRenames(commit) || myIndexStorage == null) return;
+    mySingleTaskController.request(new IndexingRequest(root, myIndexStorage.paths.getPathsEncoder(),
+                                                       TroveUtil.singleton(commit), false, true));
   }
 
   @Nullable
@@ -474,6 +478,7 @@ public class VcsLogPersistentIndex implements VcsLogModifiableIndex, Disposable 
     private static final int FLUSHED_COMMITS_NUMBER = 15000;
     @NotNull private final VirtualFile myRoot;
     @NotNull private final TIntHashSet myCommits;
+    @NotNull private final VcsLogIndexer.PathsEncoder myPathsEncoder;
     private final boolean myFull;
     private final boolean myReindex;
 
@@ -481,8 +486,13 @@ public class VcsLogPersistentIndex implements VcsLogModifiableIndex, Disposable 
     @NotNull private final AtomicInteger myOldCommits = new AtomicInteger();
     private volatile long myStartTime;
 
-    IndexingRequest(@NotNull VirtualFile root, @NotNull TIntHashSet commits, boolean full, boolean reindex) {
+    IndexingRequest(@NotNull VirtualFile root,
+                    @NotNull VcsLogIndexer.PathsEncoder encoder,
+                    @NotNull TIntHashSet commits,
+                    boolean full,
+                    boolean reindex) {
       myRoot = root;
+      myPathsEncoder = encoder;
       myCommits = commits;
       myFull = full;
       myReindex = reindex;
@@ -595,7 +605,7 @@ public class VcsLogPersistentIndex implements VcsLogModifiableIndex, Disposable 
         indicator.checkCanceled();
 
         List<String> hashes = TroveUtil.map2List(batch, value -> myStorage.getCommitId(value).getHash().asString());
-        myIndexers.get(myRoot).readFullDetails(myRoot, hashes, detail -> {
+        myIndexers.get(myRoot).readFullDetails(myRoot, hashes, myPathsEncoder, detail -> {
           storeDetail(detail);
           myNewIndexedCommits.incrementAndGet();
 
@@ -609,7 +619,7 @@ public class VcsLogPersistentIndex implements VcsLogModifiableIndex, Disposable 
     public void indexAll(@NotNull ProgressIndicator indicator) throws VcsException {
       displayProgress(indicator);
 
-      myIndexers.get(myRoot).readAllFullDetails(myRoot, details -> {
+      myIndexers.get(myRoot).readAllFullDetails(myRoot, myPathsEncoder, details -> {
         storeDetail(details);
 
         if (myNewIndexedCommits.incrementAndGet() % FLUSHED_COMMITS_NUMBER == 0) flush();

plugins/git4idea/src/git4idea/history/GitCompressedDetailsCollector.kt
@@ -1,0 +1,119 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package git4idea.history
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.util.registry.Registry
+import com.intellij.openapi.vcs.changes.Change
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.util.PathUtil
+import com.intellij.vcs.log.VcsLogObjectsFactory
+import com.intellij.vcs.log.impl.VcsLogIndexer
+import git4idea.log.GitCompressedDetails
+import gnu.trove.TIntHashSet
+import gnu.trove.TIntIntHashMap
+import gnu.trove.TIntObjectHashMap
+import kotlin.math.max
+
+internal class GitCompressedDetailsCollector(project: Project, root: VirtualFile, pathsEncoder: VcsLogIndexer.PathsEncoder) :
+  GitDetailsCollector<GitCompressedRecord, GitCompressedDetails>(project, root, CompressedRecordBuilder(root, pathsEncoder)) {
+  
+  override fun createCommit(records: List<GitCompressedRecord>,
+                            factory: VcsLogObjectsFactory,
+                            renameLimit: GitCommitRequirements.DiffRenameLimit): GitCompressedDetails {
+    val hasRenames = when (renameLimit) {
+      GitCommitRequirements.DiffRenameLimit.INFINITY -> true
+      GitCommitRequirements.DiffRenameLimit.GIT_CONFIG -> false // need to know the value from git.config to give correct answer
+      GitCommitRequirements.DiffRenameLimit.NO_RENAMES -> false
+      GitCommitRequirements.DiffRenameLimit.REGISTRY -> {
+        val renameLimitEstimate = records.map { it.renameLimitEstimate }.max() ?: 0
+        renameLimitEstimate <= Registry.intValue("git.diff.renameLimit")
+      }
+    }
+    val metadata = GitLogUtil.createMetadata(root, records.first(), factory)
+    return GitCompressedDetails(metadata, records.map { it.changes }, records.map { it.renames }, hasRenames)
+  }
+
+  override fun createRecordsCollector(preserveOrder: Boolean,
+                                      consumer: (List<GitCompressedRecord>) -> Unit): GitLogRecordCollector<GitCompressedRecord> {
+    return if (preserveOrder)
+      object : GitLogRecordCollector<GitCompressedRecord>(project, root, consumer) {
+        override fun createEmptyCopy(r: GitCompressedRecord): GitCompressedRecord =
+          GitCompressedRecord(r.options, TIntObjectHashMap(), TIntIntHashMap(), 0, r.isSupportsRawBody)
+      }
+    else
+      object : GitLogUnorderedRecordCollector<GitCompressedRecord>(project, root, consumer) {
+        override fun getSize(r: GitCompressedRecord) = r.changes.size() + r.renames.size()
+        override fun createEmptyCopy(r: GitCompressedRecord): GitCompressedRecord =
+          GitCompressedRecord(r.options, TIntObjectHashMap(), TIntIntHashMap(), 0, r.isSupportsRawBody)
+      }
+  }
+
+}
+
+internal class CompressedRecordBuilder(private val root: VirtualFile,
+                                       private val pathsEncoder: VcsLogIndexer.PathsEncoder) : GitLogRecordBuilder<GitCompressedRecord> {
+  private var changes = TIntObjectHashMap<Change.Type>()
+  private var parents = TIntHashSet()
+  private var renames = TIntIntHashMap()
+  private var sourcesCount = 0
+  private var targetsCount = 0
+
+
+  override fun addPath(type: Change.Type, firstPath: String, secondPath: String?) {
+    if (secondPath != null) {
+      changes.put(pathsEncoder.encode(absolutePath(firstPath), false), Change.Type.DELETED)
+      changes.put(pathsEncoder.encode(absolutePath(secondPath), false), Change.Type.NEW)
+      addParents(firstPath)
+      addParents(secondPath)
+    }
+    else {
+      changes.put(pathsEncoder.encode(absolutePath(firstPath), false), type)
+      addParents(firstPath)
+    }
+    when (type) {
+      Change.Type.NEW -> targetsCount++
+      Change.Type.DELETED -> sourcesCount++
+      else -> {
+      }
+    }
+  }
+
+  private fun addParents(path: String) {
+    var parentPath = PathUtil.getParentPath(path)
+    var parentPathId = pathsEncoder.encode(parentPath, true)
+
+    while (!parents.contains(parentPathId)) {
+      if (FileUtil.PATH_HASHING_STRATEGY.equals(root.path, parentPath)) break
+
+      parents.add(parentPathId)
+
+      parentPath = PathUtil.getParentPath(parentPath)
+      parentPathId = pathsEncoder.encode(parentPath, true)
+    }
+  }
+
+  private fun absolutePath(firstPath: String) = root.path + "/" + firstPath
+
+  override fun build(options: MutableMap<GitLogParser.GitLogOption, String>, supportsRawBody: Boolean): GitCompressedRecord {
+    parents.forEach {
+      changes.put(it, Change.Type.MODIFICATION)
+      true
+    }
+    return GitCompressedRecord(options, changes, renames, max(sourcesCount, targetsCount), supportsRawBody)
+  }
+
+  override fun clear() {
+    changes = TIntObjectHashMap()
+    parents = TIntHashSet()
+    renames = TIntIntHashMap()
+    sourcesCount = 0
+    targetsCount = 0
+  }
+}
+
+internal class GitCompressedRecord(options: MutableMap<GitLogParser.GitLogOption, String>,
+                                   val changes: TIntObjectHashMap<Change.Type>,
+                                   val renames: TIntIntHashMap,
+                                   val renameLimitEstimate: Int,
+                                   supportsRawBody: Boolean) : GitLogRecord(options, supportsRawBody)

platform/vcs-log/impl/src/com/intellij/vcs/log/data/index/VcsLogPathsIndex.java
@@ -4,13 +4,11 @@ package com.intellij.vcs.log.data.index;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.Couple;
-import com.intellij.openapi.util.Pair;
-import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vcs.FilePath;
+import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vcs.changes.Change;
 import com.intellij.util.BooleanFunction;
 import com.intellij.util.Consumer;
-import com.intellij.util.PathUtil;
 import com.intellij.util.SmartList;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.indexing.DataIndexer;
@@ -20,14 +18,13 @@ import com.intellij.util.indexing.impl.ForwardIndex;
 import com.intellij.util.indexing.impl.KeyCollectionBasedForwardIndex;
 import com.intellij.util.io.*;
 import com.intellij.vcs.log.VcsLogIndexService;
+import com.intellij.vcs.log.impl.VcsLogIndexer;
 import com.intellij.vcs.log.data.VcsLogStorage;
 import com.intellij.vcs.log.impl.FatalErrorHandler;
-import com.intellij.vcs.log.impl.VcsIndexableDetails;
 import com.intellij.vcs.log.util.StorageId;
 import com.intellij.vcsUtil.VcsUtil;
 import gnu.trove.TByteObjectHashMap;
 import gnu.trove.THashMap;
-import gnu.trove.THashSet;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -39,7 +36,7 @@ import java.io.IOException;
 import java.util.*;
 import java.util.function.ObjIntConsumer;
 
-public class VcsLogPathsIndex extends VcsLogFullDetailsIndex<List<VcsLogPathsIndex.ChangeKind>, VcsIndexableDetails> {
+public class VcsLogPathsIndex extends VcsLogFullDetailsIndex<List<VcsLogPathsIndex.ChangeKind>, VcsLogIndexer.CompressedDetails> {
   private static final Logger LOG = Logger.getInstance(VcsLogPathsIndex.class);
   public static final String PATHS = "paths";
   public static final String INDEX_PATHS_IDS = "paths-ids";
@@ -58,7 +55,7 @@ public class VcsLogPathsIndex extends VcsLogFullDetailsIndex<List<VcsLogPathsInd
 
   @Nullable
   @Override
-  protected ForwardIndex<Integer, List<VcsLogPathsIndex.ChangeKind>> createForwardIndex(@NotNull IndexExtension<Integer, List<ChangeKind>, VcsIndexableDetails> extension)
+  protected ForwardIndex<Integer, List<VcsLogPathsIndex.ChangeKind>> createForwardIndex(@NotNull IndexExtension<Integer, List<ChangeKind>, VcsLogIndexer.CompressedDetails> extension)
     throws IOException {
     if (!VcsLogIndexService.isPathsForwardIndexRequired()) return super.createForwardIndex(extension);
     return new KeyCollectionBasedForwardIndex<Integer, List<ChangeKind>>(extension) {
@@ -153,6 +150,21 @@ public class VcsLogPathsIndex extends VcsLogFullDetailsIndex<List<VcsLogPathsInd
     iterateCommitIdsAndValues(pathId, consumer);
   }
 
+  @NotNull
+  VcsLogIndexer.PathsEncoder getPathsEncoder() {
+    return new VcsLogIndexer.PathsEncoder() {
+      @Override
+      public int encode(@NotNull String path, boolean isDirectory) throws VcsException {
+        try {
+          return myPathsIndexer.myPathsEnumerator.enumerate(new LightFilePath(path, isDirectory));
+        }
+        catch (IOException e) {
+          throw new VcsException(e);
+        }
+      }
+    };
+  }
+
   @Override
   public void dispose() {
     super.dispose();
@@ -172,7 +184,7 @@ public class VcsLogPathsIndex extends VcsLogFullDetailsIndex<List<VcsLogPathsInd
     return VcsUtil.getFilePath(lightFilePath.getPath(), lightFilePath.isDirectory());
   }
 
-  private static class PathsIndexer implements DataIndexer<Integer, List<ChangeKind>, VcsIndexableDetails> {
+  private static class PathsIndexer implements DataIndexer<Integer, List<ChangeKind>, VcsLogIndexer.CompressedDetails> {
     @NotNull private final VcsLogStorage myStorage;
     @NotNull private final PersistentEnumeratorBase<LightFilePath> myPathsEnumerator;
     @NotNull private final PersistentHashMap<Couple<Integer>, Collection<Couple<Integer>>> myRenamesMap;
@@ -191,25 +203,21 @@ public class VcsLogPathsIndex extends VcsLogFullDetailsIndex<List<VcsLogPathsInd
 
     @NotNull
     @Override
-    public Map<Integer, List<ChangeKind>> map(@NotNull VcsIndexableDetails inputData) {
+    public Map<Integer, List<ChangeKind>> map(@NotNull VcsLogIndexer.CompressedDetails inputData) {
       Map<Integer, List<ChangeKind>> result = new THashMap<>();
 
-      String rootPath = inputData.getRoot().getPath();
       // its not exactly parents count since it is very convenient to assume that initial commit has one parent
       int parentsCount = inputData.getParents().isEmpty() ? 1 : inputData.getParents().size();
       for (int parentIndex = 0; parentIndex < parentsCount; parentIndex++) {
         try {
-          Set<String> processedParents = new THashSet<>();
+          int finalParentIndex = parentIndex;
           Collection<Couple<Integer>> renames = new SmartList<>();
-          for (Pair<String, String> renamedPath : inputData.getRenamedPaths(parentIndex)) {
-            int beforeId = myPathsEnumerator.enumerate(new LightFilePath(renamedPath.first, false));
-            int afterId = myPathsEnumerator.enumerate(new LightFilePath(renamedPath.second, false));
+          inputData.getRenamedPaths(parentIndex).forEachEntry((beforeId, afterId) -> {
             renames.add(Couple.of(beforeId, afterId));
-            getOrCreateChangeKindList(result, beforeId, parentsCount).set(parentIndex, ChangeKind.REMOVED);
-            getOrCreateChangeKindList(result, afterId, parentsCount).set(parentIndex, ChangeKind.ADDED);
-            addParentsToResult(result, parentIndex, parentsCount, renamedPath.second, rootPath, processedParents);
-            addParentsToResult(result, parentIndex, parentsCount, renamedPath.first, rootPath, processedParents);
-          }
+            getOrCreateChangeKindList(result, beforeId, parentsCount).set(finalParentIndex, ChangeKind.REMOVED);
+            getOrCreateChangeKindList(result, afterId, parentsCount).set(finalParentIndex, ChangeKind.ADDED);
+            return true;
+          });
 
           if (renames.size() > 0) {
             int commit = myStorage.getCommitIndex(inputData.getId(), inputData.getRoot());
@@ -217,11 +225,10 @@ public class VcsLogPathsIndex extends VcsLogFullDetailsIndex<List<VcsLogPathsInd
             myRenamesMap.put(Couple.of(parent, commit), renames);
           }
 
-          for (Map.Entry<String, Change.Type> modifiedPath : inputData.getModifiedPaths(parentIndex).entrySet()) {
-            getOrCreateChangeKindList(result, new LightFilePath(modifiedPath.getKey(), false), parentsCount)
-              .set(parentIndex, createChangeData(modifiedPath.getValue()));
-            addParentsToResult(result, parentIndex, parentsCount, modifiedPath.getKey(), rootPath, processedParents);
-          }
+          inputData.getModifiedPaths(parentIndex).forEachEntry((pathId, changeType) -> {
+            getOrCreateChangeKindList(result, pathId, parentsCount).set(finalParentIndex, createChangeData(changeType));
+            return true;
+          });
         }
         catch (IOException e) {
           myFatalErrorConsumer.consume(e);
@@ -229,27 +236,6 @@ public class VcsLogPathsIndex extends VcsLogFullDetailsIndex<List<VcsLogPathsInd
       }
 
       return result;
-    }
-
-    private void addParentsToResult(@NotNull Map<Integer, List<ChangeKind>> result,
-                                    int parent, int parentsCount,
-                                    @NotNull String path, @NotNull String rootPath,
-                                    @NotNull Set<? super String> processedParents) throws IOException {
-      String parentPath = PathUtil.getParentPath(path);
-      while (!processedParents.contains(parentPath)) {
-        if (FileUtil.PATH_HASHING_STRATEGY.equals(rootPath, parentPath)) break;
-
-        processedParents.add(parentPath);
-        getOrCreateChangeKindList(result, new LightFilePath(parentPath, true), parentsCount).set(parent, ChangeKind.MODIFIED);
-        parentPath = PathUtil.getParentPath(parentPath);
-      }
-    }
-
-    @NotNull
-    private List<ChangeKind> getOrCreateChangeKindList(@NotNull Map<Integer, List<ChangeKind>> pathIdToChangeDataListsMap,
-                                                       @NotNull LightFilePath path, int parentsCount) throws IOException {
-      int pathId = myPathsEnumerator.enumerate(path);
-      return getOrCreateChangeKindList(pathIdToChangeDataListsMap, pathId, parentsCount);
     }
 
     @NotNull

platform/vcs-log/impl/src/com/intellij/vcs/log/impl/VcsLogIndexer.java
@@ -3,9 +3,12 @@ package com.intellij.vcs.log.impl;
 
 import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vcs.VcsKey;
+import com.intellij.openapi.vcs.changes.Change;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.Consumer;
-import com.intellij.vcs.log.VcsFullCommitDetails;
+import com.intellij.vcs.log.VcsCommitMetadata;
+import gnu.trove.TIntIntHashMap;
+import gnu.trove.TIntObjectHashMap;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -17,7 +20,8 @@ public interface VcsLogIndexer {
    * Allows to skip full rename detection to make things faster. For git, for example, this would be adding diff.renameLimit=x to the command.
    */
   void readFullDetails(@NotNull VirtualFile root, @NotNull List<String> hashes,
-                       @NotNull Consumer<? super VcsFullCommitDetails> commitConsumer,
+                       @NotNull VcsLogIndexer.PathsEncoder encoder,
+                       @NotNull Consumer<? super CompressedDetails> commitConsumer,
                        boolean fast)
     throws VcsException;
 
@@ -26,8 +30,23 @@ public interface VcsLogIndexer {
    * <p/>
    * Reports commits to the consumer to avoid creation & even temporary storage of a too large commits collection.
    */
-  void readAllFullDetails(@NotNull VirtualFile root, @NotNull Consumer<? super VcsFullCommitDetails> commitConsumer) throws VcsException;
+  void readAllFullDetails(@NotNull VirtualFile root, @NotNull VcsLogIndexer.PathsEncoder encoder,
+                          @NotNull Consumer<? super CompressedDetails> commitConsumer) throws VcsException;
 
   @NotNull
   VcsKey getSupportedVcs();
+
+  interface CompressedDetails extends VcsCommitMetadata {
+    @NotNull
+    TIntObjectHashMap<Change.Type> getModifiedPaths(int parent);
+
+    @NotNull
+    TIntIntHashMap getRenamedPaths(int parent);
+
+    boolean hasRenames();
+  }
+
+  interface PathsEncoder {
+    int encode(@NotNull String path, boolean isDirectory) throws VcsException;
+  }
 }

plugins/git4idea/src/git4idea/history/GitLogUtil.java
@@ -243,8 +243,8 @@ public class GitLogUtil {
   }
 
   @NotNull
-  private static VcsCommitMetadata createMetadata(@NotNull VirtualFile root, @NotNull GitLogRecord record,
-                                                  @NotNull VcsLogObjectsFactory factory) {
+  static VcsCommitMetadata createMetadata(@NotNull VirtualFile root, @NotNull GitLogRecord record,
+                                          @NotNull VcsLogObjectsFactory factory) {
     List<Hash> parents = ContainerUtil.map(record.getParentsHashes(), factory::createHash);
     return factory.createCommitMetadata(factory.createHash(record.getHash()), parents, record.getCommitTime(), root, record.getSubject(),
                                         record.getAuthorName(), record.getAuthorEmail(), record.getFullMessage(),

plugins/git4idea/src/git4idea/log/GitLogIndexer.kt
@@ -4,40 +4,45 @@ package git4idea.log
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.VcsException
 import com.intellij.openapi.vcs.VcsKey
+import com.intellij.openapi.vcs.changes.Change
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.ArrayUtil
 import com.intellij.util.Consumer
-import com.intellij.vcs.log.VcsFullCommitDetails
+import com.intellij.vcs.log.VcsCommitMetadata
 import com.intellij.vcs.log.impl.VcsLogIndexer
 import git4idea.GitVcs
 import git4idea.history.GitCommitRequirements
 import git4idea.history.GitCommitRequirements.DiffInMergeCommits
 import git4idea.history.GitCommitRequirements.DiffRenameLimit
-import git4idea.history.GitFullDetailsCollector
+import git4idea.history.GitCompressedDetailsCollector
 import git4idea.history.GitLogUtil
 import git4idea.log.GitLogProvider.isRepositoryReady
 import git4idea.log.GitLogProvider.shouldIncludeRootChanges
 import git4idea.repo.GitRepositoryManager
+import gnu.trove.TIntIntHashMap
+import gnu.trove.TIntObjectHashMap
 
 class GitLogIndexer(private val project: Project,
                     private val repositoryManager: GitRepositoryManager) : VcsLogIndexer {
 
   @Throws(VcsException::class)
-  override fun readAllFullDetails(root: VirtualFile, commitConsumer: Consumer<in VcsFullCommitDetails>) {
+  override fun readAllFullDetails(root: VirtualFile, encoder: VcsLogIndexer.PathsEncoder,
+                                  commitConsumer: Consumer<in VcsLogIndexer.CompressedDetails>) {
     if (!isRepositoryReady(repositoryManager, root)) {
       return
     }
 
     val requirements = GitCommitRequirements(shouldIncludeRootChanges(repositoryManager, root), DiffRenameLimit.REGISTRY,
                                              DiffInMergeCommits.DIFF_TO_PARENTS, false)
-    GitFullDetailsCollector(project, root).readFullDetails(commitConsumer, requirements, true,
-                                                           *ArrayUtil.toStringArray(GitLogUtil.LOG_ALL))
+    GitCompressedDetailsCollector(project, root, encoder).readFullDetails(commitConsumer, requirements, true,
+                                                                          *ArrayUtil.toStringArray(GitLogUtil.LOG_ALL))
   }
 
   @Throws(VcsException::class)
   override fun readFullDetails(root: VirtualFile,
                                hashes: List<String>,
-                               commitConsumer: Consumer<in VcsFullCommitDetails>,
+                               encoder: VcsLogIndexer.PathsEncoder,
+                               commitConsumer: Consumer<in VcsLogIndexer.CompressedDetails>,
                                fast: Boolean) {
     if (!isRepositoryReady(repositoryManager, root)) {
       return
@@ -46,10 +51,29 @@ class GitLogIndexer(private val project: Project,
     val renameLimit = if (fast) DiffRenameLimit.REGISTRY else DiffRenameLimit.INFINITY
     val requirements = GitCommitRequirements(shouldIncludeRootChanges(repositoryManager, root), renameLimit,
                                              DiffInMergeCommits.DIFF_TO_PARENTS, false)
-    GitFullDetailsCollector(project, root).readFullDetailsForHashes(hashes, requirements, fast, commitConsumer)
+    GitCompressedDetailsCollector(project, root, encoder).readFullDetailsForHashes(hashes, requirements, fast, commitConsumer)
   }
 
   override fun getSupportedVcs(): VcsKey {
     return GitVcs.getKey()
+  }
+}
+
+class GitCompressedDetails(private val metadata: VcsCommitMetadata,
+                           private val changes: List<TIntObjectHashMap<Change.Type>>,
+                           private val renames: List<TIntIntHashMap>,
+                           private val hasRenames: Boolean) : VcsCommitMetadata by metadata,
+                                                              VcsLogIndexer.CompressedDetails {
+
+  override fun getModifiedPaths(parent: Int): TIntObjectHashMap<Change.Type> {
+    return changes[parent]
+  }
+
+  override fun getRenamedPaths(parent: Int): TIntIntHashMap {
+    return renames[parent]
+  }
+
+  override fun hasRenames(): Boolean {
+    return hasRenames
   }
 }

commit caccf885ad8b227143b5d27803d0499b42928bcd

[git] index absolute paths for parent directories
Parent directories indexing got broken in 9b62f1e5e2c75f0032763dbcbc7b5f63075464f6 during move from VcsLogPathsIndex to CompressedRecordBuilder.

Bump index version to get rid of incorrect paths that could have been written in the index since the breakage.

platform/vcs-log/impl/src/com/intellij/vcs/log/data/index/VcsLogPersistentIndex.java
@@ -71,7 +71,7 @@ import static com.intellij.vcs.log.util.PersistentUtil.calcLogId;
 
 public class VcsLogPersistentIndex implements VcsLogModifiableIndex, Disposable {
   private static final Logger LOG = Logger.getInstance(VcsLogPersistentIndex.class);
-  private static final int VERSION = 12;
+  private static final int VERSION = 13;
   private static final VcsLogProgress.ProgressKey INDEXING = new VcsLogProgress.ProgressKey("index");
 
   @NotNull private final Project myProject;

plugins/git4idea/src/git4idea/history/GitCompressedDetailsCollector.kt
@@ -50,14 +50,11 @@ internal class CompressedRecordBuilder(private val root: VirtualFile,
 
   override fun addPath(type: Change.Type, firstPath: String, secondPath: String?) {
     if (secondPath != null) {
-      changes.put(pathsEncoder.encode(absolutePath(firstPath), false), Change.Type.DELETED)
-      changes.put(pathsEncoder.encode(absolutePath(secondPath), false), Change.Type.NEW)
-      addParents(firstPath)
-      addParents(secondPath)
+      addPath(firstPath, Change.Type.DELETED)
+      addPath(secondPath, Change.Type.NEW)
     }
     else {
-      changes.put(pathsEncoder.encode(absolutePath(firstPath), false), type)
-      addParents(firstPath)
+      addPath(firstPath, type)
     }
     when (type) {
       Change.Type.NEW -> targetsCount++
@@ -65,6 +62,12 @@ internal class CompressedRecordBuilder(private val root: VirtualFile,
       else -> {
       }
     }
+  }
+
+  private fun addPath(path: String, type: Change.Type) {
+    val absolutePath = absolutePath(path)
+    changes.put(pathsEncoder.encode(absolutePath, false), type)
+    addParents(absolutePath)
   }
 
   private fun addParents(path: String) {