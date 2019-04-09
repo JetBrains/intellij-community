c3dee1f9f6efee465b1449a5b3433da0d95d9b03
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-04-09 14:59:03 +0300

    more on typing ", ', and `
    GitOrigin-RevId: f471a0f8cab2070e7af31df9571a46d6efbe4072

--- plugins/sh/src/com/intellij/bash/BashQuoteHandler.java
+++ plugins/sh/src/com/intellij/bash/BashQuoteHandler.java
@@ -2,24 +2,9 @@ package com.intellij.bash;
 
 import com.intellij.bash.lexer.BashTokenTypes;
 import com.intellij.codeInsight.editorActions.SimpleTokenSetQuoteHandler;
-import com.intellij.openapi.editor.highlighter.HighlighterIterator;
-import com.intellij.psi.tree.IElementType;
 
 public class BashQuoteHandler extends SimpleTokenSetQuoteHandler {
   public BashQuoteHandler() {
     super(BashTokenTypes.BAD_CHARACTER, BashTypes.RAW_STRING, BashTypes.QUOTE, BashTypes.BACKQUOTE);
-  }
-
-  @Override
-  public boolean isClosingQuote(HighlighterIterator iterator, int offset) {
-    final IElementType tokenType = iterator.getTokenType();
-
-    if (myLiteralTokenSet.contains(tokenType)) {
-      int start = iterator.getStart();
-      int end = iterator.getEnd();
-      return end - start >= 1 && offset == end - 1;
-    }
-
-    return false;
   }
 }
--- plugins/sh/tests/com/intellij/bash/editing/BashTypingTest.java
+++ plugins/sh/tests/com/intellij/bash/editing/BashTypingTest.java
@@ -1,5 +1,6 @@
 package com.intellij.bash.editing;
 
+import com.intellij.testFramework.LightPlatformCodeInsightTestCase;
 import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
 
 public class BashTypingTest extends LightCodeInsightFixtureTestCase {
@@ -8,13 +9,23 @@ public class BashTypingTest extends LightCodeInsightFixtureTestCase {
   public void testRawStringAfter()      { doTypingTest("'<caret>'", "'", "''<caret>");          }
   public void testString()              { doTypingTest("<caret>", "\"", "\"<caret>\"");         }
   public void testStringAfter()         { doTypingTest("\"<caret>\"", "\"", "\"\"<caret>");     }
-  public void testBackQuote()           { doTypingTest("<caret>", "`", "`<caret>`");           }
-  public void testBackQuoteAfter()      { doTypingTest("`<caret>`", "`", "``<caret>");         }
+  public void testBackQuote()           { doTypingTest("<caret>", "`", "`<caret>`");            }
+  public void testBackQuoteAfter()      { doTypingTest("`<caret>`", "`", "``<caret>");          }
+
+  public void testStringBackspace()     { doBackspaceTest("\"<caret>\"", "<caret>");            }
+  public void testRawStringBackspace()  { doBackspaceTest("'<caret>'", "<caret>");              }
+  public void testQuoteBackspace()      { doBackspaceTest("`<caret>`", "<caret>");              }
   // @formatter:on
 
   private void doTypingTest(String before, String forType, String after) {
     myFixture.configureByText("a.sh", before);
     myFixture.type(forType);
+    myFixture.checkResult(after);
+  }
+
+  private void doBackspaceTest(String before, String after) {
+    myFixture.configureByText("a.sh", before);
+    LightPlatformCodeInsightTestCase.backspace(getEditor(), getProject());
     myFixture.checkResult(after);
   }
 }
