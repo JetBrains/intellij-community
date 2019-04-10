48b382ec5d82fc0e38b376e280eca9b03e292808
Bas Leijdekkers <basleijdekkers@gmail.com>
2019-04-10 10:43:26 +0200

    SSR: scope chooser combo can't handle multiple init() calls (IDEA-210612)

--- platform/structuralsearch/source/com/intellij/structuralsearch/plugin/ui/ScopePanel.java
+++ platform/structuralsearch/source/com/intellij/structuralsearch/plugin/ui/ScopePanel.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.structuralsearch.plugin.ui;
 
 import com.intellij.application.options.ModulesComboBox;
@@ -132,7 +132,7 @@ public class ScopePanel extends JPanel {
       myDirectoryComboBox.setRecursive(directoryScope.isWithSubdirectories());
     }
     else if (selectedScope != null) {
-      myScopesComboBox.init(myProject, true, false, selectedScope.getDisplayName(), SCOPE_FILTER);
+      myScopesComboBox.selectItem(selectedScope);
     }
     myToolbar.updateActionsImmediately();
     ((CardLayout)myScopeDetailsPanel.getLayout()).show(myScopeDetailsPanel, myScopeType.toString());
