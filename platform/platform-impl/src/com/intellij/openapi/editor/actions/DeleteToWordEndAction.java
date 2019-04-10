13600e1d53cae83cbfa0a41b334d46b66fd7339f
Eldar Abusalimov <eldar.abusalimov@jetbrains.com>
2019-04-10 05:10:25 +0300

    IDEABKL-5365: DeleteToWordBoundaryHandler: Extract common code

--- platform/platform-impl/src/com/intellij/openapi/editor/actions/DeleteToWordEndAction.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/actions/DeleteToWordEndAction.java
@@ -16,52 +16,8 @@
 
 package com.intellij.openapi.editor.actions;
 
-import com.intellij.openapi.actionSystem.DataContext;
-import com.intellij.openapi.command.CommandProcessor;
-import com.intellij.openapi.editor.Caret;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.EditorModificationUtil;
-import com.intellij.openapi.editor.actionSystem.EditorWriteActionHandler;
-import com.intellij.openapi.ide.CopyPasteManager;
-import com.intellij.openapi.util.TextRange;
-
 public class DeleteToWordEndAction extends TextComponentEditorAction {
   public DeleteToWordEndAction() {
-    super(new Handler(false));
-  }
-
-  static class Handler extends EditorWriteActionHandler {
-    
-    private final boolean myNegateCamelMode;
-
-    Handler(boolean negateCamelMode) {
-      super(true);
-      myNegateCamelMode = negateCamelMode;
-    }
-
-    @Override
-    public void executeWriteAction(Editor editor, Caret caret, DataContext dataContext) {
-      CommandProcessor.getInstance().setCurrentCommandGroupId(EditorActionUtil.DELETE_COMMAND_GROUP);
-      CopyPasteManager.getInstance().stopKillRings();
-
-      boolean camelMode = editor.getSettings().isCamelWords();
-      if (myNegateCamelMode) {
-        camelMode = !camelMode;
-      }
-
-      if (editor.getSelectionModel().hasSelection()) {
-        EditorModificationUtil.deleteSelectedText(editor);
-        return;
-      }
-
-      deleteToWordEnd(editor, camelMode);
-    }
-  }
-
-  private static void deleteToWordEnd(Editor editor, boolean camelMode) {
-    final TextRange range = EditorActionUtil.getRangeToWordEnd(editor, camelMode, true);
-    if (!range.isEmpty()) {
-      editor.getDocument().deleteString(range.getStartOffset(), range.getEndOffset());
-    }
+    super(new DeleteToWordBoundaryHandler(false, false));
   }
 }
--- platform/platform-impl/src/com/intellij/openapi/editor/actions/DeleteToWordStartAction.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/actions/DeleteToWordStartAction.java
@@ -4,52 +4,8 @@
 
 package com.intellij.openapi.editor.actions;
 
-import com.intellij.openapi.actionSystem.DataContext;
-import com.intellij.openapi.command.CommandProcessor;
-import com.intellij.openapi.editor.Caret;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.EditorModificationUtil;
-import com.intellij.openapi.editor.actionSystem.EditorWriteActionHandler;
-import com.intellij.openapi.ide.CopyPasteManager;
-import com.intellij.openapi.util.TextRange;
-
 public class DeleteToWordStartAction extends TextComponentEditorAction {
   public DeleteToWordStartAction() {
-    super(new Handler(false));
-  }
-
-  static class Handler extends EditorWriteActionHandler {
-
-    private final boolean myNegateCamelMode;
-
-    Handler(boolean negateCamelMode) {
-      super(true);
-      myNegateCamelMode = negateCamelMode;
-    }
-
-    @Override
-    public void executeWriteAction(Editor editor, Caret caret, DataContext dataContext) {
-      CommandProcessor.getInstance().setCurrentCommandGroupId(EditorActionUtil.DELETE_COMMAND_GROUP);
-      CopyPasteManager.getInstance().stopKillRings();
-
-      boolean camelMode = editor.getSettings().isCamelWords();
-      if (myNegateCamelMode) {
-        camelMode = !camelMode;
-      }
-
-      if (editor.getSelectionModel().hasSelection()) {
-        EditorModificationUtil.deleteSelectedText(editor);
-        return;
-      }
-
-      deleteToWordStart(editor, camelMode);
-    }
-  }
-
-  private static void deleteToWordStart(Editor editor, boolean camelMode) {
-    final TextRange range = EditorActionUtil.getRangeToWordStart(editor, camelMode, true);
-    if (!range.isEmpty()) {
-      editor.getDocument().deleteString(range.getStartOffset(), range.getEndOffset());
-    }
+    super(new DeleteToWordBoundaryHandler(true, false));
   }
 }
