39d6b5ab88c9c9cb31b39e609c974c2a0911a86c
Konstantin Aleev <konstantin.aleev@jetbrains.com>
2019-04-11 13:51:59 +0300

    [service-view] simplify contributor

--- platform/lang-api/src/com/intellij/execution/services/ServiceViewContributor.java
+++ platform/lang-api/src/com/intellij/execution/services/ServiceViewContributor.java
@@ -1,9 +1,6 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.services;
 
-import com.intellij.navigation.ItemPresentation;
-import com.intellij.openapi.actionSystem.ActionGroup;
-import com.intellij.openapi.actionSystem.DataProvider;
 import com.intellij.openapi.project.Project;
 import com.intellij.util.messages.Topic;
 import org.jetbrains.annotations.ApiStatus;
@@ -12,79 +9,31 @@ import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
 import java.util.List;
 
 @ApiStatus.Experimental
-public interface ServiceViewContributor<T, Group, State> {
+public interface ServiceViewContributor<T> {
   Topic<ServiceListener> TOPIC = Topic.create("services topic", ServiceListener.class, Topic.BroadcastDirection.TO_CHILDREN);
 
   @NotNull
-  List<T> getNodes(@NotNull Project project);
-
-  List<Group> getGroups(@NotNull T node);
+  ServiceViewDescriptor getViewDescriptor();
 
   @NotNull
-  State getState(@NotNull T node);
+  List<T> getServices(@NotNull Project project);
 
   @NotNull
-  ViewDescriptor getGroupDescriptor(@NotNull Group group);
-
-  @NotNull
-  ViewDescriptor getNodeDescriptor(@NotNull T node);
-
-  @NotNull
-  ViewDescriptor getStateDescriptor(@NotNull State group);
+  ServiceViewDescriptor getServiceDescriptor(@NotNull T service);
 
   @Nullable
   default ViewDescriptorRenderer getViewDescriptorRenderer() {
     return null;
   }
 
-  @Nullable
-  default SubtreeDescriptor<?> getNodeSubtree(@NotNull T node) { return null; }
-
-  interface SubtreeDescriptor<TT> {
-
-    @NotNull
-    List<TT> getItems();
-
-    @NotNull
-    ViewDescriptor getItemDescriptor(@NotNull TT item);
-
-    @Nullable
-    default SubtreeDescriptor<?> getNodeSubtree(@NotNull TT node) { return null; }
-  }
-
-  interface ViewDescriptor {
-    JComponent getContentComponent();
-
-    ActionGroup getToolbarActions();
-
-    default ActionGroup getPopupActions() {
-      return getToolbarActions();
-    }
-
-    ItemPresentation getPresentation();
-
-    DataProvider getDataProvider();
-
-    default void onNodeSelected() {
-    }
-
-    default void onNodeUnselected() {
-    }
-
-    default boolean handleDoubleClick(@NotNull MouseEvent event) {
-      return false;
-    }
-  }
-
   interface ViewDescriptorRenderer {
     @Nullable
     Component getRendererComponent(JComponent parent,
                                    Object value,
-                                   ViewDescriptor viewDescriptor,
+                                   ServiceViewDescriptor viewDescriptor,
                                    boolean selected,
                                    boolean hasFocus);
   }
