0dbed2f95612e13e0228e879e4ea2caf060bbd0a
Michail Plushnikov <mplushnikov@gmail.com>
2019-04-11 21:57:09 +0200

    fixed #619, #606 allow root access to new testdata directory
    GitOrigin-RevId: 1d61cf994555119d2f572f842df2c22ce8e22d05

--- plugins/lombok/src/test/java/de/plushnikov/intellij/plugin/processor/FieldNameConstantsOldTest.java
+++ plugins/lombok/src/test/java/de/plushnikov/intellij/plugin/processor/FieldNameConstantsOldTest.java
@@ -1,6 +1,9 @@
 package de.plushnikov.intellij.plugin.processor;
 
+import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess;
 import de.plushnikov.intellij.plugin.AbstractLombokParsingTestCase;
+
+import java.io.File;
 
 /**
  * Unit tests for @FieldNameConstants annotation from old version of lombok
@@ -8,6 +11,14 @@ import de.plushnikov.intellij.plugin.AbstractLombokParsingTestCase;
 public class FieldNameConstantsOldTest extends AbstractLombokParsingTestCase {
 
   private static final String OLD_LOMBOK_SRC_PATH = "./old";
+
+  @Override
+  public void setUp() throws Exception {
+    VfsRootAccess.allowRootAccess(new File(getTestDataPath(), getBasePath()).getCanonicalPath(),
+      new File(OLD_LOMBOK_SRC_PATH).getCanonicalPath());
+
+    super.setUp();
+  }
 
   public void testFieldnameconstants$FieldNameConstantsOldBasic() {
     super.loadFilesFrom(OLD_LOMBOK_SRC_PATH);
