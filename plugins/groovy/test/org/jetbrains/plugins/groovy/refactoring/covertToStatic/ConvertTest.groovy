e8d7ab239612ac91d2289ea8a0fc909fa1f85737
alexey.afanasiev <alexey.afanasiev@jetbrains.com>
2019-04-12 13:39:21 +0300

    IDEA-206717 Dont add second @CompileDynamic in "@Convert to @CompileStatic" action

--- plugins/groovy/test/org/jetbrains/plugins/groovy/refactoring/covertToStatic/ConvertTest.groovy
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/refactoring/covertToStatic/ConvertTest.groovy
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.refactoring.covertToStatic
 
 import org.jetbrains.plugins.groovy.LightGroovyTestCase
@@ -45,6 +45,10 @@ class ConvertTest extends LightGroovyTestCase {
   }
 
   void testMarkupPrintScript() {
+    doTest()
+  }
+
+  void testDontAddAnnotationTwice() {
     doTest()
   }
 
