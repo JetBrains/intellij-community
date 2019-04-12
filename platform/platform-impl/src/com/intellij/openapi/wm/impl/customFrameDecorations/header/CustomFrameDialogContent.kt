9bf988063b5ca674ba355e315969d08e8f48cf99
graann <Anna.Gromova@jetbrains.com>
2019-04-12 17:47:54 +0300

    Borderless UI: Welcome screen is cut (shorter then should be) and can not be resized (#IDEA-210940)
    (cherry picked from commit 1f49fc455d01f4b887463853db48e814f6700fb3)

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/customFrameDecorations/header/CustomFrameDialogContent.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/customFrameDecorations/header/CustomFrameDialogContent.kt
@@ -8,33 +8,45 @@ import java.awt.Container
 import java.awt.Window
 import javax.swing.*
 
-class CustomFrameDialogContent private constructor(window: Window, content: Container, val titleBackgroundColor: Color? = null) {
+class CustomFrameDialogContent private constructor(window: Window, content: Container, titleBackgroundColor: Color? = null): CustomFrameViewHolder {
     companion object {
+        @JvmStatic
         fun getContent(window: Window, content: JComponent) = getContent(window, content, null)
 
+        @JvmStatic
         fun getContent(window: Window, content: JComponent, titleBackgroundColor: Color? = null): JComponent {
+            return getCustomContentHolder(window, content, titleBackgroundColor).content
+        }
+
+        @JvmStatic
+        fun getCustomContentHolder(window: Window, content: JComponent, titleBackgroundColor: Color? = null): CustomFrameViewHolder {
             val rootPane: JRootPane? = when (window) {
                 is JWindow -> window.rootPane
                 is JDialog -> {
-                    if (window.isUndecorated) return content
-                    window.rootPane
+                    if (window.isUndecorated) null
+                    else window.rootPane
                 }
                 is JFrame -> window.rootPane
                 else -> null
             }
 
-            rootPane ?: return content
+            rootPane ?: return object : CustomFrameViewHolder {
+                override val content: JComponent
+                    get() = content
+                override val headerHeight: Int
+                    get() = 0
+            }
 
             JdkEx.setHasCustomDecoration(window)
-            val custom = CustomFrameDialogContent(window, content, titleBackgroundColor)
-            return custom.getView()
+            return CustomFrameDialogContent(window, content, titleBackgroundColor)
+
         }
     }
 
     private val panel = JPanel(MigLayout("novisualpadding, ins 0, gap 0, fill, flowy", "", "[min!][]"))
+    private val header: CustomHeader = CustomHeader.create(window)
 
     init {
-        val header: CustomHeader = CustomHeader.create(window)
         titleBackgroundColor?.let {
             header.background = it
         }
@@ -47,5 +59,13 @@ class CustomFrameDialogContent private constructor(window: Window, content: Cont
         panel.add(pane, "wmin 0, grow")
     }
 
-    fun getView(): JComponent = panel
+    override val content: JComponent
+        get() = panel
+    override val headerHeight: Int
+        get() = header.preferredSize.height
+}
+
+interface CustomFrameViewHolder {
+    val content: JComponent
+    val headerHeight: Int
 }
