b17b43f1689b9e3d48494baa893f80935649ffe4
dmitriy.tseyler <dmitriy.tseyler@jetbrains.com>
2019-04-17 17:19:55 +0300

    service view: autoscroll to/from

--- platform/lang-impl/src/com/intellij/execution/services/ServiceView.java
+++ platform/lang-impl/src/com/intellij/execution/services/ServiceView.java
@@ -9,6 +9,7 @@ import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.util.RecursionManager;
+import com.intellij.pom.Navigatable;
 import com.intellij.ui.tree.TreeVisitor;
 import com.intellij.util.ObjectUtils;
 import com.intellij.util.containers.ContainerUtil;
@@ -21,6 +22,7 @@ import java.awt.*;
 import java.util.List;
 
 class ServiceView extends JPanel implements Disposable {
+  private final Project myProject;
   private final ServiceViewState myState;
   private final ServiceViewTree myTree;
   private final ServiceViewUi myUi;
@@ -29,6 +31,7 @@ class ServiceView extends JPanel implements Disposable {
 
   ServiceView(@NotNull Project project, @NotNull ServiceViewUi ui, @NotNull ServiceViewState state) {
     super(new BorderLayout());
+    myProject = project;
     myState = state;
     myUi = ui;
 
@@ -96,6 +99,10 @@ class ServiceView extends JPanel implements Disposable {
 
     if (newDescriptor != null) {
       newDescriptor.onNodeSelected();
+    }
+    if (newDescriptor instanceof Navigatable) {
+      Navigatable navigatable = (Navigatable)newDescriptor;
+      if (ServiceViewManagerImpl.isAutoScrollToSourceEnabled(myProject) && navigatable.canNavigate()) navigatable.navigate(false);
     }
 
     myUi.setDetailsComponent(newDescriptor == null ? null : newDescriptor.getContentComponent());
--- platform/lang-impl/src/com/intellij/execution/services/ServiceViewManagerImpl.java
+++ platform/lang-impl/src/com/intellij/execution/services/ServiceViewManagerImpl.java
@@ -2,19 +2,30 @@
 package com.intellij.execution.services;
 
 import com.intellij.icons.AllIcons;
+import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.ide.util.treeView.TreeState;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.openapi.actionSystem.ToggleAction;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
 import com.intellij.openapi.components.StoragePathMacros;
 import com.intellij.openapi.extensions.ExtensionPointName;
+import com.intellij.openapi.fileEditor.FileEditor;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowAnchor;
 import com.intellij.openapi.wm.ToolWindowId;
 import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.openapi.wm.ex.ToolWindowEx;
 import com.intellij.ui.AppUIUtil;
+import com.intellij.ui.AutoScrollFromSourceHandler;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import com.intellij.util.xmlb.annotations.Property;
@@ -28,6 +39,9 @@ import javax.swing.*;
 
 @State(name = "ServiceViewManager", storages = @Storage(StoragePathMacros.WORKSPACE_FILE))
 public final class ServiceViewManagerImpl implements ServiceViewManager, PersistentStateComponent<ServiceViewManagerImpl.State> {
+  private static final String AUTO_SCROLL_TO_SOURCE_PROPERTY = "service.view.auto.scroll.to.source";
+  private static final String AUTO_SCROLL_FROM_SOURCE_PROPERTY = "service.view.auto.scroll.from.source";
+
   static final ExtensionPointName<ServiceViewContributor> EP_NAME =
     ExtensionPointName.create("com.intellij.serviceViewContributor");
 
@@ -64,6 +78,10 @@ public final class ServiceViewManagerImpl implements ServiceViewManager, Persist
     Disposer.register(toolWindowContent, () -> myServiceView = null);
 
     toolWindow.getContentManager().addContent(toolWindowContent);
+
+    ToolWindowEx toolWindowEx = (ToolWindowEx)toolWindow;
+    toolWindowEx.setAdditionalGearActions(new DefaultActionGroup(ToggleAutoScrollAction.toSource(), ToggleAutoScrollAction.fromSource()));
+    toolWindowEx.setTitleActions(ServiceViewAutoScrollFromSourceHandler.install(myProject, toolWindow));
   }
 
   @NotNull
@@ -119,9 +137,10 @@ public final class ServiceViewManagerImpl implements ServiceViewManager, Persist
     });
   }
 
-  private ToolWindow createToolWindow(ToolWindowManager toolWindowManager, boolean available) {
-    ToolWindow toolWindow = toolWindowManager.registerToolWindow(getToolWindowId(), true, ToolWindowAnchor.BOTTOM,
-                                                                 myProject, true);
+  @NotNull
+  private ToolWindowEx createToolWindow(ToolWindowManager toolWindowManager, boolean available) {
+    String id = getToolWindowId();
+    ToolWindowEx toolWindow = (ToolWindowEx)toolWindowManager.registerToolWindow(id, true, ToolWindowAnchor.BOTTOM, myProject, true);
     toolWindow.setIcon(getToolWindowIcon());
     toolWindow.setAvailable(available, null);
     createToolWindowContent(toolWindow);
@@ -151,6 +170,14 @@ public final class ServiceViewManagerImpl implements ServiceViewManager, Persist
     public ServiceViewState viewState = new ServiceViewState();
   }
 
+  static boolean isAutoScrollToSourceEnabled(@NotNull Project project) {
+    return PropertiesComponent.getInstance(project).getBoolean(AUTO_SCROLL_TO_SOURCE_PROPERTY);
+  }
+
+  private static boolean isAutoScrollFromSourceEnabled(@NotNull Project project) {
+    return PropertiesComponent.getInstance(project).getBoolean(AUTO_SCROLL_FROM_SOURCE_PROPERTY);
+  }
+
   private static String getToolWindowId() {
     return ToolWindowId.SERVICES;
   }
@@ -161,5 +188,103 @@ public final class ServiceViewManagerImpl implements ServiceViewManager, Persist
 
   static String getToolWindowContextHelpId() {
     return HELP_ID;
+  }
+
+  private static class ToggleAutoScrollAction extends ToggleAction implements DumbAware {
+    private final String myProperty;
+
+    ToggleAutoScrollAction(@NotNull String text, @NotNull String property) {
+      super(text);
+      myProperty = property;
+    }
+
+    @Override
+    public boolean isSelected(@NotNull AnActionEvent e) {
+      Project project = e.getProject();
+      return project != null && PropertiesComponent.getInstance(project).getBoolean(myProperty);
+    }
+
+    @Override
+    public void setSelected(@NotNull AnActionEvent e, boolean state) {
+      Project project = e.getProject();
+      if (project != null) PropertiesComponent.getInstance(project).setValue(myProperty, state);
+    }
+
+    @NotNull
+    static AnAction toSource() {
+      return new ToggleAutoScrollAction("Autoscroll to Source", AUTO_SCROLL_TO_SOURCE_PROPERTY);
+    }
+
+    @NotNull
+    static AnAction fromSource() {
+      return new ToggleAutoScrollAction("Autoscroll from Source", AUTO_SCROLL_FROM_SOURCE_PROPERTY);
+    }
+  }
+
+  private static class ServiceViewAutoScrollFromSourceHandler extends AutoScrollFromSourceHandler {
+    ServiceViewAutoScrollFromSourceHandler(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+      super(project, toolWindow.getComponent(), toolWindow.getContentManager());
+    }
+
+    @Override
+    protected boolean isAutoScrollEnabled() {
+      return isAutoScrollFromSourceEnabled(myProject);
+    }
+
+    @Override
+    protected void setAutoScrollEnabled(boolean enabled) {
+      PropertiesComponent.getInstance(myProject).setValue(AUTO_SCROLL_FROM_SOURCE_PROPERTY, true);
+    }
+
+    @Override
+    protected void selectElementFromEditor(@NotNull FileEditor editor) {
+      select(editor);
+    }
+
+    private boolean select(@NotNull FileEditor editor) {
+      for (ServiceViewContributor extension : EP_NAME.getExtensions()) {
+        if (!(extension instanceof ServiceViewFileEditorContributor)) continue;
+        if (selectContributorNode(editor, (ServiceViewFileEditorContributor)extension)) return true;
+      }
+      return false;
+    }
+
+    private boolean selectContributorNode(@NotNull FileEditor editor, @NotNull ServiceViewFileEditorContributor extension) {
+      Object service = extension.findService(myProject, editor);
+      if (service == null) return false;
+      if (service instanceof ServiceViewFileEditorContributor) {
+        if (selectContributorNode(editor, (ServiceViewFileEditorContributor)service)) return true;
+      }
+      ServiceViewManager.getInstance(myProject).selectNode(service, true, true);
+      return true;
+    }
+
+    @NotNull
+    public static AnAction install(@NotNull Project project, @NotNull ToolWindow window) {
+      ServiceViewAutoScrollFromSourceHandler handler = new ServiceViewAutoScrollFromSourceHandler(project, window);
+      handler.install();
+      return new ScrollFromEditorAction(handler);
+    }
+  }
+
+  private static class ScrollFromEditorAction extends DumbAwareAction {
+    private final ServiceViewAutoScrollFromSourceHandler myScrollFromHandler;
+
+    ScrollFromEditorAction(ServiceViewAutoScrollFromSourceHandler scrollFromHandler) {
+      super("Scroll from Source", "Select node open in the active editor", AllIcons.General.Locate);
+      myScrollFromHandler = scrollFromHandler;
+    }
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+      Project project = e.getProject();
+      if (project == null) return;
+      FileEditorManager manager = FileEditorManager.getInstance(project);
+      FileEditor[] editors = manager.getSelectedEditors();
+      if (editors.length == 0) return;
+      for (FileEditor editor : editors) {
+        if (myScrollFromHandler.select(editor)) return;
+      }
+    }
   }
 }
