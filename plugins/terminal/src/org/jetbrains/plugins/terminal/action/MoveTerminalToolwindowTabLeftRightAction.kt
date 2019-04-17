817417da3c9b39aff0cdadfd8e1ee3fad61300aa
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2019-04-17 22:30:31 +0300

    terminal: use kotlin properly; fix tests (IDEA-CR-46423)
    (cherry picked from commit 5bce63b1a5f15ad841495c6b878434138b0b9662)
    
    https://upsource.jetbrains.com/IDEA/review/IDEA-CR-46423
    
    GitOrigin-RevId: 985aa1aeb42532873a5d0daf7f04de1a068a74f6

--- plugins/terminal/src/org/jetbrains/plugins/terminal/action/MoveTerminalToolwindowTabLeftRightAction.kt
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/action/MoveTerminalToolwindowTabLeftRightAction.kt
@@ -5,11 +5,10 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.ui.content.Content
-import java.util.*
 
 open class MoveTerminalToolwindowTabLeftRightAction(private val moveLeft: Boolean) : TerminalSessionContextMenuActionBase(), DumbAware {
   override fun actionPerformed(e: AnActionEvent, toolWindow: ToolWindow, content: Content?) {
-    move(content);
+    move(content)
   }
 
   override fun update(e: AnActionEvent, activeToolWindow: ToolWindow, selectedContent: Content?) {
@@ -22,16 +21,20 @@ open class MoveTerminalToolwindowTabLeftRightAction(private val moveLeft: Boolea
   fun isAvailable(content: Content?) : Boolean {
     val manager = content?.manager ?: return false
     val ind = manager.getIndexOfContent(content)
-    return if (moveLeft) ind > 0 else ind >= 0 && ind < manager.getContentCount() - 1
+    return if (moveLeft) ind > 0 else ind >= 0 && ind < manager.contentCount - 1
   }
 
   fun move(content: Content?) {
     val manager = content?.manager ?: return
     val ind = manager.getIndexOfContent(content)
     val otherInd = if (moveLeft) ind - 1 else ind + 1
-    if (ind >= 0 && otherInd >= 0 && otherInd < manager.getContentCount()) {
-      val otherContent = Objects.requireNonNull<Content>(manager.getContent(otherInd))
+    if (ind >= 0 && otherInd >= 0 && otherInd < manager.contentCount) {
+      val otherContent = manager.getContent(otherInd)!!
       manager.removeContent(otherContent, false, false, false).doWhenDone { manager.addContent(otherContent, ind) }
     }
   }
 }
+
+class MoveTerminalToolWindowTabLeftAction : MoveTerminalToolwindowTabLeftRightAction(true)
+
+class MoveTerminalToolWindowTabRightAction : MoveTerminalToolwindowTabLeftRightAction(false)
