aa1b72ba7ab7d7cf5d3c9ed27cf122d44dec0410
Pavel Dolgov <pavel.dolgov@jetbrains.com>
2019-04-18 15:38:04 +0300

    Java: Fixed modifiers when extracting method from default or private method in interface (IDEA-211141)

--- java/java-impl/src/com/intellij/refactoring/extractMethod/ExtractMethodProcessor.java
+++ java/java-impl/src/com/intellij/refactoring/extractMethod/ExtractMethodProcessor.java
@@ -33,6 +33,7 @@ import com.intellij.openapi.util.*;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.wm.WindowManager;
+import com.intellij.pom.java.LanguageLevel;
 import com.intellij.psi.*;
 import com.intellij.psi.codeStyle.*;
 import com.intellij.psi.controlFlow.ControlFlow;
@@ -1634,16 +1635,32 @@ public class ExtractMethodProcessor implements MatchProvider {
       throwsList.add(JavaPsiFacade.getElementFactory(myManager.getProject()).createReferenceElementByType(exception));
     }
 
-    if (myTargetClass.isInterface() && PsiUtil.isLanguageLevel8OrHigher(myTargetClass)) {
-      final PsiMethod containingMethod = PsiTreeUtil.getParentOfType(myCodeFragmentMember, PsiMethod.class, false);
-      if (containingMethod != null && containingMethod.hasModifierProperty(PsiModifier.DEFAULT)) {
-        PsiUtil.setModifierProperty(newMethod, PsiModifier.DEFAULT, true);
-      }
-      PsiUtil.setModifierProperty(newMethod, PsiModifier.PUBLIC, false);
-      PsiUtil.setModifierProperty(newMethod, PsiModifier.PRIVATE, false);
-      PsiUtil.setModifierProperty(newMethod, PsiModifier.PROTECTED, false);
+    if (myTargetClass.isInterface()) {
+      updateModifiersInInterface(newMethod);
     }
     return (PsiMethod)myStyleManager.reformat(newMethod);
+  }
+
+  private void updateModifiersInInterface(PsiMethod newMethod) {
+    LanguageLevel languageLevel = PsiUtil.getLanguageLevel(myTargetClass);
+    if (languageLevel.isAtLeast(LanguageLevel.JDK_1_8)) {
+      if (!isStatic()) {
+        final PsiMethod containingMethod = PsiTreeUtil.getParentOfType(myCodeFragmentMember, PsiMethod.class, false);
+        if (containingMethod != null && containingMethod.hasModifierProperty(PsiModifier.DEFAULT)) {
+          if (languageLevel.isAtLeast(LanguageLevel.JDK_1_9)) {
+            PsiUtil.setModifierProperty(newMethod, PsiModifier.PRIVATE, true); // don't increase the API surface
+          }
+          else {
+            PsiUtil.setModifierProperty(newMethod, PsiModifier.DEFAULT, true);
+          }
+        }
+      }
+      PsiUtil.setModifierProperty(newMethod, PsiModifier.PUBLIC, false);
+      PsiUtil.setModifierProperty(newMethod, PsiModifier.PROTECTED, false);
+      if (isStatic() || !languageLevel.isAtLeast(LanguageLevel.JDK_1_9)) {
+        PsiUtil.setModifierProperty(newMethod, PsiModifier.PRIVATE, false);
+      }
+    }
   }
 
   protected boolean defineVariablesForUnselectedParameters() {
