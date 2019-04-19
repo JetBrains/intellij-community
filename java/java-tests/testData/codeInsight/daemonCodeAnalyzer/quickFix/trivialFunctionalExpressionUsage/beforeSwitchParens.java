179a83f60ea9c2c6405780563ef58d047a275f04
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2019-04-19 11:55:23 +0700

    TrivialFunctionalExpressionUsageInspection: support parentheses for anonymous class

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/trivialFunctionalExpressionUsage/beforeSwitchParens.java
@@ -0,0 +1,9 @@
+// "Replace call with method body" "true"
+
+import java.util.function.IntSupplier;
+
+public class Main {
+  public void call() {
+    switch((new IntSupplier() {public int getAsInt() { return 0;}}.getA<caret>sInt())) {}
+  }
+}
