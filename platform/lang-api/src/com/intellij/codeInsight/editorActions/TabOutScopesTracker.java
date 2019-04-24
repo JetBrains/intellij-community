487c5817038f6bb7585b07f9a5a521a505a27ef0
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2019-04-24 19:43:57 +0300

    IDEA-211180 Jump outside closing bracket with Tab should jump after the semicolon
    following IDEA-CR-46526
    
    GitOrigin-RevId: 2666a3018ff95e49f516d64252641f566282cf6e

--- platform/lang-api/src/com/intellij/codeInsight/editorActions/TabOutScopesTracker.java
+++ platform/lang-api/src/com/intellij/codeInsight/editorActions/TabOutScopesTracker.java
@@ -28,15 +28,15 @@ public interface TabOutScopesTracker {
    * just inserted pair of quotes/brackets.
    */
   default void registerEmptyScope(@NotNull Editor editor, int offset) {
-    registerEmptyScope(editor, offset, 1);
+    registerEmptyScope(editor, offset, offset + 1);
   }
 
   /**
    * Same as {@link #registerEmptyScope(Editor, int)} but allows to set custom caret shift on exiting the scope. This is for the cases when scope suffix contains more than one character (e.g. a closing parenthesis and a semicolon).
    *
-   * @param caretShift a positive value, representing desired caret offset shift when Tab is used to exit the scope
+   * @param tabOutOffset position where caret should be moved when Tab is used to exit the scope (should be larger than {@code offset})
    */
-  void registerEmptyScope(@NotNull Editor editor, int offset, int caretShift);
+  void registerEmptyScope(@NotNull Editor editor, int offset, int tabOutOffset);
 
   /**
    * Checks whether given offset is at the end of tracked scope (so if caret is located at that offset, Tab key can be used to move out of
@@ -47,7 +47,7 @@ public interface TabOutScopesTracker {
   /**
    * Removes a tracked scope (if any) ending at the given offset.
    *
-   * @return a positive value indicating required caret shift, if there was a scope ending at given offset, and zero otherwise
+   * @return target caret position where caret should be moved, if there was a scope ending at given offset, and -1 otherwise
    *
    * @see #hasScopeEndingAt(Editor, int)
    * @see #registerEmptyScope(Editor, int, int)
