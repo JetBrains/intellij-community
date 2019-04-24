e84bde69724c140041cae5d3af0519c1a24aae89
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2019-04-24 19:00:08 +0200

    Annotate InlayParameterHints Option class id parameter as NonNls
    GitOrigin-RevId: c9e3bcf8717aeeb404c32429e2d7a6dbc84d5117

--- platform/lang-api/src/com/intellij/codeInsight/hints/InlayParameterHintsProvider.kt
+++ platform/lang-api/src/com/intellij/codeInsight/hints/InlayParameterHintsProvider.kt
@@ -18,6 +18,7 @@ package com.intellij.codeInsight.hints
 import com.intellij.codeInsight.hints.settings.ParameterNameHintsSettings
 import com.intellij.lang.Language
 import com.intellij.lang.LanguageExtension
+import org.jetbrains.annotations.NonNls
 
 
 object InlayParameterHintsExtension : LanguageExtension<InlayParameterHintsProvider>("com.intellij.codeInsight.parameterNameHints")
@@ -99,7 +100,7 @@ sealed class HintInfo {
 
 }
 
-data class Option(val id: String,
+data class Option(@NonNls val id: String,
                   val name: String,
                   val defaultValue: Boolean) {
 
