94ac4f622e76d086d2a2105f0fa550f23976d8c8
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-04-25 12:09:21 +0200

    [platform] makes WindowsElevationUtil tolerant to missing JNA
    GitOrigin-RevId: 7ed99953b41afac6fb0f50f600a3027aac235a8f

--- platform/util/src/com/intellij/openapi/util/io/win32/WindowsElevationUtil.java
+++ platform/util/src/com/intellij/openapi/util/io/win32/WindowsElevationUtil.java
@@ -1,13 +1,12 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.util.io.win32;
 
+import com.intellij.jna.JnaLoader;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.util.AtomicNotNullLazyValue;
 import com.intellij.openapi.util.SystemInfo;
 import com.sun.jna.Structure;
 import com.sun.jna.platform.win32.*;
 import com.sun.jna.ptr.IntByReference;
-import org.jetbrains.annotations.NotNull;
 
 public class WindowsElevationUtil {
   private static final Logger LOG = Logger.getInstance(WindowsElevationUtil.class);
@@ -15,61 +14,58 @@ public class WindowsElevationUtil {
   private WindowsElevationUtil() { }
 
   /**
-   * returns true if current user is having administrator privileges AND this code is running under elevation
+   * Returns {@code true} if this process is running under elevated privileges.
    */
   public static boolean isUnderElevation() {
-    return IS_UNDER_ELEVATION.getValue();
+    return SystemInfo.isWindows && JnaLoader.isLoaded() && Holder.elevated;
   }
 
-  private static final AtomicNotNullLazyValue<Boolean> IS_UNDER_ELEVATION = new AtomicNotNullLazyValue<Boolean>() {
-    @NotNull
-    @Override
-    protected Boolean compute() {
-      if (!SystemInfo.isWindows) {
-        return false;
-      }
+  private static class Holder {
+    static final boolean elevated;
 
+    static {
+      boolean result = false;
       try {
-        return isElevated();
-      } catch (Throwable t) {
-        LOG.warn("Unexpected exception in WindowsElevationUtil.IS_UNDER_ELEVATION", t);
-        return false;
+        result = isElevated();
       }
+      catch (Throwable t) {
+        LOG.warn(t);
+      }
+      elevated = result;
     }
 
-    private boolean isElevated() {
-      WinNT.HANDLEByReference processTokenHandle = new WinNT.HANDLEByReference();
+    private static boolean isElevated() {
+      WinNT.HANDLEByReference tokenHandle = new WinNT.HANDLEByReference();
 
-      boolean openProcessRc = Advapi32.INSTANCE.OpenProcessToken(
-        Kernel32.INSTANCE.GetCurrentProcess(),
-        WinNT.TOKEN_ADJUST_PRIVILEGES | WinNT.TOKEN_QUERY, processTokenHandle);
-      if (!openProcessRc) {
+      boolean rc = Advapi32.INSTANCE.OpenProcessToken(
+        Kernel32.INSTANCE.GetCurrentProcess(), WinNT.TOKEN_ADJUST_PRIVILEGES | WinNT.TOKEN_QUERY, tokenHandle);
+      if (!rc) {
         int lastError = Kernel32.INSTANCE.GetLastError();
-        LOG.warn("Unable to OpenProcessToken: error " + lastError + ": " + Kernel32Util.formatMessageFromLastErrorCode(lastError));
+        LOG.warn("OpenProcessToken: " + lastError + ' ' + Kernel32Util.formatMessageFromLastErrorCode(lastError));
         return false;
       }
 
       try {
         IntByReference cbNeeded = new IntByReference(0);
         TOKEN_ELEVATION tokenElevation = new TOKEN_ELEVATION();
-        boolean getTokenInformationRc = Advapi32.INSTANCE.GetTokenInformation(
-          processTokenHandle.getValue(), WinNT.TOKEN_INFORMATION_CLASS.TokenElevation,
-          tokenElevation, tokenElevation.size(), cbNeeded);
-        if (!getTokenInformationRc) {
+        rc = Advapi32.INSTANCE.GetTokenInformation(
+          tokenHandle.getValue(), WinNT.TOKEN_INFORMATION_CLASS.TokenElevation, tokenElevation, tokenElevation.size(), cbNeeded);
+        if (!rc) {
           int lastError = Kernel32.INSTANCE.GetLastError();
-          LOG.warn("Unable to GetTokenInformation: error " + lastError + ": " + Kernel32Util.formatMessageFromLastErrorCode(lastError));
+          LOG.warn("GetTokenInformation: " + lastError + ' ' + Kernel32Util.formatMessageFromLastErrorCode(lastError));
           return false;
         }
 
         return tokenElevation.TokenIsElevated.intValue() != 0;
-      } finally {
-        Kernel32.INSTANCE.CloseHandle(processTokenHandle.getValue());
+      }
+      finally {
+        Kernel32.INSTANCE.CloseHandle(tokenHandle.getValue());
       }
     }
-  };
 
-  @Structure.FieldOrder("TokenIsElevated")
-  public static class TOKEN_ELEVATION extends Structure {
-    public WinDef.DWORD TokenIsElevated = new WinDef.DWORD(0);
+    @Structure.FieldOrder("TokenIsElevated")
+    public static class TOKEN_ELEVATION extends Structure {
+      public WinDef.DWORD TokenIsElevated = new WinDef.DWORD(0);
+    }
   }
 }
