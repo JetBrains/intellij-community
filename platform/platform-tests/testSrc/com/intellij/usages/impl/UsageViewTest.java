21d031849bf97b86780b4fbb61592911511c798a
Alexey Kudravtsev <cdr@intellij.com>
2019-04-26 18:29:24 +0300

    sick and tired of hunting tests leaking documents, clear undo queue myself
    GitOrigin-RevId: c2437a3ed6934c6089da5d049bf4f82b45b4ed3c

--- platform/platform-tests/testSrc/com/intellij/usages/impl/UsageViewTest.java
+++ platform/platform-tests/testSrc/com/intellij/usages/impl/UsageViewTest.java
@@ -12,6 +12,8 @@ import com.intellij.openapi.actionSystem.DataProvider;
 import com.intellij.openapi.actionSystem.TypeSafeDataProvider;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.command.impl.UndoManagerImpl;
+import com.intellij.openapi.command.undo.UndoManager;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.fileTypes.BinaryFileDecompiler;
@@ -44,6 +46,9 @@ import java.util.Set;
 
 public class UsageViewTest extends LightPlatformCodeInsightFixtureTestCase {
   public void testUsageViewDoesNotHoldPsiFilesOrDocuments() {
+    // sick and tired of hunting tests leaking documents
+    ((UndoManagerImpl)UndoManager.getInstance(getProject())).flushCurrentCommandMerger();
+
     boolean[] foundLeaksBeforeTest = new boolean[1];
     Condition<Object> isReallyLeak = file -> {
       if (file instanceof PsiFile && !((PsiFile)file).isPhysical()) return false;
