70f4c358e93485166db0761625a64048687b24e0
Leonid Bushuev <leonid.bushuev@jetbrains.com>
2019-04-26 20:23:41 +0300

    SQL code style per dialect: UI: fix indent editor
    (cherry picked from commit 3cb00f5c500523717c42f404b2f38fd1822d6db0)
    
    GitOrigin-RevId: 105fe6b21112fa75c05483a7692ad25e0efb6078

--- platform/lang-impl/src/com/intellij/application/options/TabbedLanguageCodeStylePanel.java
+++ platform/lang-impl/src/com/intellij/application/options/TabbedLanguageCodeStylePanel.java
@@ -585,7 +585,7 @@ public abstract class TabbedLanguageCodeStylePanel extends CodeStyleAbstractPane
     }
 
     @Nullable
-    private CommonCodeStyleSettings.IndentOptions getIndentOptions(CodeStyleSettings settings) {
+    protected CommonCodeStyleSettings.IndentOptions getIndentOptions(CodeStyleSettings settings) {
       return settings.getCommonSettings(getDefaultLanguage()).getIndentOptions();
     }
 
