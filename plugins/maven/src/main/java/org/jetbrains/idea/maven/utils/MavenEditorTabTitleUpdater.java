142386d406259b4bae991fdb105acf7440cb3d1f
develar <develar@gmail.com>
2019-04-26 12:36:16 +0200

    cleanup
    GitOrigin-RevId: 0e5f51122ee40a09e1d68cd748bc3e4bdeeb94de

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/utils/MavenEditorTabTitleUpdater.java
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/utils/MavenEditorTabTitleUpdater.java
@@ -1,21 +1,6 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.maven.utils;
 
-import com.intellij.openapi.components.BaseComponent;
 import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Pair;
@@ -27,14 +12,13 @@ import org.jetbrains.idea.maven.project.MavenProjectsTree;
 
 import java.util.List;
 
-public class MavenEditorTabTitleUpdater extends MavenSimpleProjectComponent implements BaseComponent {
-  public MavenEditorTabTitleUpdater(Project project) {
+final class MavenEditorTabTitleUpdater extends MavenSimpleProjectComponent {
+  MavenEditorTabTitleUpdater(Project project) {
     super(project);
-  }
 
-  @Override
-  public void initComponent() {
-    if (!isNormalProject()) return;
+    if (!isNormalProject()) {
+      return;
+    }
 
     MavenProjectsManager.getInstance(myProject).addProjectsTreeListener(new MavenProjectsTree.Listener() {
       @Override
@@ -44,7 +28,7 @@ public class MavenEditorTabTitleUpdater extends MavenSimpleProjectComponent impl
     });
   }
 
-  private void updateTabName(final List<MavenProject> projects) {
+  private void updateTabName(@NotNull List<MavenProject> projects) {
     MavenUtil.invokeLater(myProject, () -> {
       for (MavenProject each : projects) {
         FileEditorManagerEx.getInstanceEx(myProject).updateFilePresentation(each.getFile());
