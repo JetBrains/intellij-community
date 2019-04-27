c65b56408bc437b8531eb7b488146219be9e0636
Jens Glathe <jens.glathe@oldschoolsolutions.biz>
2019-04-27 10:28:12 +0200

    create an executable named fsnotifier-linux-aarch64 as should be generated by getExecutable()

--- native/fsNotifier/linux/Jetson_make.sh
+++ native/fsNotifier/linux/Jetson_make.sh
@@ -5,6 +5,6 @@ CC_FLAGS="-O2 -Wall -Wextra -Wpedantic -std=c11 -D_DEFAULT_SOURCE"
 VER=$(date "+%Y%m%d.%H%M")
 sed -i.bak "s/#define VERSION .*/#define VERSION \"${VER}\"/" fsnotifier.h && rm fsnotifier.h.bak
 
-echo "compiling fort Jetson Nano (aarch64)"
-cc ${CC_FLAGS} -o fsnotifier-aarch64 main.c inotify.c util.c && chmod 755 fsnotifier-aarch64
+echo "compiling fort Jetson Nano (linux-aarch64)"
+cc ${CC_FLAGS} -o fsnotifier-aarch64 main.c inotify.c util.c && chmod 755 fsnotifier-linux-aarch64
 
