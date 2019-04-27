83aaba72033bf09c746f85c03c748b8525c1b38d
develar <develar@gmail.com>
2019-04-27 08:34:14 +0200

    extract class MapStoredProperty to separate file
    GitOrigin-RevId: e73aab520392b6247dc352e6c31b6c267abe05cb

--- platform/projectModel-api/src/com/intellij/configurationStore/properties/CollectionStoredProperty.kt
+++ platform/projectModel-api/src/com/intellij/configurationStore/properties/CollectionStoredProperty.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.configurationStore.properties
 
 import com.intellij.openapi.components.BaseState
@@ -52,43 +52,4 @@ open class CollectionStoredProperty<E, C : MutableCollection<E>>(protected val v
 
 internal class ListStoredProperty<T> : CollectionStoredProperty<T, SmartList<T>>(SmartList()) {
   override fun getModificationCount() = value.modificationCount.toLong()
-}
-
-class MapStoredProperty<K: Any, V>(private val value: MutableMap<K, V>) : StoredPropertyBase<MutableMap<K, V>>() {
-  override val jsonType: JsonSchemaType
-    get() = JsonSchemaType.OBJECT
-
-  override fun isEqualToDefault() = value.isEmpty()
-
-  override operator fun getValue(thisRef: BaseState, property: KProperty<*>) = value
-
-  override fun setValue(thisRef: BaseState, property: KProperty<*>, @Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE") newValue: MutableMap<K, V>) {
-    if (doSetValue(value, newValue)) {
-      thisRef.intIncrementModificationCount()
-    }
-  }
-
-  private fun doSetValue(old: MutableMap<K, V>, new: Map<K, V>): Boolean {
-    if (old == new) {
-      return false
-    }
-
-    old.clear()
-    old.putAll(new)
-    return true
-  }
-
-  override fun equals(other: Any?) = this === other || (other is MapStoredProperty<*, *> && value == other.value)
-
-  override fun hashCode() = value.hashCode()
-
-  override fun toString() = if (isEqualToDefault()) "" else value.toString()
-
-  override fun setValue(other: StoredProperty<MutableMap<K, V>>): Boolean {
-    @Suppress("UNCHECKED_CAST")
-    return doSetValue(value, (other as MapStoredProperty<K, V>).value)
-  }
-
-  @Suppress("FunctionName")
-  fun __getValue() = value
 }
---
+++ platform/projectModel-api/src/com/intellij/configurationStore/properties/MapStoredProperty.kt
@@ -0,0 +1,47 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.configurationStore.properties
+
+import com.intellij.openapi.components.BaseState
+import com.intellij.openapi.components.JsonSchemaType
+import com.intellij.openapi.components.StoredProperty
+import com.intellij.openapi.components.StoredPropertyBase
+import kotlin.reflect.KProperty
+
+class MapStoredProperty<K: Any, V>(private val value: MutableMap<K, V>) : StoredPropertyBase<MutableMap<K, V>>() {
+  override val jsonType: JsonSchemaType
+    get() = JsonSchemaType.OBJECT
+
+  override fun isEqualToDefault() = value.isEmpty()
+
+  override operator fun getValue(thisRef: BaseState, property: KProperty<*>) = value
+
+  override fun setValue(thisRef: BaseState, property: KProperty<*>, @Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE") newValue: MutableMap<K, V>) {
+    if (doSetValue(value, newValue)) {
+      thisRef.intIncrementModificationCount()
+    }
+  }
+
+  private fun doSetValue(old: MutableMap<K, V>, new: Map<K, V>): Boolean {
+    if (old == new) {
+      return false
+    }
+
+    old.clear()
+    old.putAll(new)
+    return true
+  }
+
+  override fun equals(other: Any?) = this === other || (other is MapStoredProperty<*, *> && value == other.value)
+
+  override fun hashCode() = value.hashCode()
+
+  override fun toString() = if (isEqualToDefault()) "" else value.toString()
+
+  override fun setValue(other: StoredProperty<MutableMap<K, V>>): Boolean {
+    @Suppress("UNCHECKED_CAST")
+    return doSetValue(value, (other as MapStoredProperty<K, V>).value)
+  }
+
+  @Suppress("FunctionName")
+  fun __getValue() = value
+}
