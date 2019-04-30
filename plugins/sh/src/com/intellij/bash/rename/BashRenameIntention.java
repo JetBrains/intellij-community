27e6b84447eb45180e0038d2286ec6f69b6695ee
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2019-04-30 18:50:35 +0300

    extract BashSelectAllOccurrencesAction, use shortcut of rename refactoring
    BashSelectAllOccurrencesAction knows better word boundaries for bash and restores caret on Escape properly
    
    GitOrigin-RevId: 62be98af8600414fe4d572c6ccd70b0548d03f45

--- plugins/sh/src/com/intellij/bash/rename/BashRenameIntention.java
+++ plugins/sh/src/com/intellij/bash/rename/BashRenameIntention.java
@@ -1,25 +1,28 @@
 package com.intellij.bash.rename;
 
-import com.intellij.bash.highlighting.BashTextOccurrencesUtil;
 import com.intellij.codeInsight.intention.impl.BaseIntentionAction;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.editor.Caret;
-import com.intellij.openapi.editor.CaretState;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.actions.IncrementalFindAction;
+import com.intellij.openapi.editor.actionSystem.EditorAction;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiFile;
 import com.intellij.util.IncorrectOperationException;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class BashRenameIntention extends BaseIntentionAction {
 
-  private static final Logger LOG = Logger.getInstance(BashRenameIntention.class);
+  private BashSelectAllOccurrencesAction mySelectAllOccurrencesAction;
+
+  public BashRenameIntention() {
+  }
+
+  @NotNull
+  private EditorAction getAction() {
+    if (mySelectAllOccurrencesAction == null) {
+      mySelectAllOccurrencesAction = BashSelectAllOccurrencesAction.findAction();
+    }
+    return mySelectAllOccurrencesAction;
+  }
 
   @Nls(capitalization = Nls.Capitalization.Sentence)
   @NotNull
@@ -41,45 +44,13 @@ public class BashRenameIntention extends BaseIntentionAction {
 
   @Override
   public boolean isAvailable(@NotNull Project project, Editor editor, PsiFile file) {
-    return editor.getProject() != null
-        && editor.getCaretModel().supportsMultipleCarets()
-        && !IncrementalFindAction.SEARCH_DISABLED.get(editor, false);
+    EditorAction action = getAction();
+    return action.getHandler().isEnabled(editor, editor.getCaretModel().getPrimaryCaret(), null);
   }
 
   @Override
   public void invoke(@NotNull Project project, Editor editor, PsiFile file) throws IncorrectOperationException {
-    List<Caret> carets = editor.getCaretModel().getAllCarets();
-    if (carets.size() != 1) return;
-    Caret caret = carets.get(0);
-    int caretOffset = caret.getOffset();
-    TextRange caretTextRange;
-    boolean matchExactWords;
-    if (caret.hasSelection()) {
-      caretTextRange = new TextRange(editor.getSelectionModel().getSelectionStart(),
-                                     editor.getSelectionModel().getSelectionEnd());
-      matchExactWords = false;
-    }
-    else {
-      caretTextRange = BashTextOccurrencesUtil.findTextRangeOfIdentifierAtCaret(editor);
-      matchExactWords = true;
-      if (caretTextRange == null) return;
-    }
-    CharSequence documentText = editor.getDocument().getImmutableCharSequence();
-    CharSequence textToFind = caretTextRange.subSequence(documentText);
-    List<TextRange> occurrences = BashTextOccurrencesUtil.findAllOccurrences(documentText, textToFind, matchExactWords);
-    if (!occurrences.remove(caretTextRange)) {
-      LOG.error("Cannot find " + caretTextRange + " in all occurrences");
-    }
-    occurrences.add(0, caretTextRange); // workaround to restore the original caret on Escape
-
-    List<CaretState> caretStates = new ArrayList<>();
-    for (TextRange occurrence : occurrences) {
-      int caretInSelection = occurrence.getStartOffset() + caretOffset - caretTextRange.getStartOffset();
-      CaretState caretState = new CaretState(editor.offsetToLogicalPosition(caretInSelection),
-          editor.offsetToLogicalPosition(occurrence.getStartOffset()),
-          editor.offsetToLogicalPosition(occurrence.getEndOffset()));
-      caretStates.add(caretState);
-    }
-    editor.getCaretModel().setCaretsAndSelections(caretStates);
+    EditorAction action = getAction();
+    action.getHandler().execute(editor, editor.getCaretModel().getPrimaryCaret(), null);
   }
 }
---
+++ plugins/sh/src/com/intellij/bash/rename/BashSelectAllOccurrencesAction.java
@@ -0,0 +1,89 @@
+package com.intellij.bash.rename;
+
+import com.intellij.bash.highlighting.BashTextOccurrencesUtil;
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.CaretState;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.actionSystem.EditorAction;
+import com.intellij.openapi.editor.actionSystem.EditorActionHandler;
+import com.intellij.openapi.util.TextRange;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.intellij.openapi.editor.actions.IncrementalFindAction.SEARCH_DISABLED;
+
+public class BashSelectAllOccurrencesAction extends EditorAction {
+
+  private static final Logger LOG = Logger.getInstance(BashSelectAllOccurrencesAction.class);
+  private static final String ACTION_ID = "BashSelectAllOccurrences";
+
+  protected BashSelectAllOccurrencesAction() {
+    super(new Handler());
+  }
+
+  static BashSelectAllOccurrencesAction findAction() {
+    AnAction action = ActionManager.getInstance().getAction(ACTION_ID);
+    if (action == null) {
+      LOG.error("Cannot find " + ACTION_ID);
+      return new BashSelectAllOccurrencesAction();
+    }
+    if (action instanceof BashSelectAllOccurrencesAction) {
+      return (BashSelectAllOccurrencesAction)action;
+    }
+    LOG.error("Cannot cast " + action.getClass() + " to " + BashSelectAllOccurrencesAction.class);
+    return new BashSelectAllOccurrencesAction();
+  }
+
+  private static class Handler extends EditorActionHandler {
+    @Override
+    public boolean isEnabledForCaret(@NotNull Editor editor, @NotNull Caret caret, DataContext dataContext) {
+      return editor.getProject() != null
+          && editor.getCaretModel().supportsMultipleCarets()
+          && !SEARCH_DISABLED.get(editor, false);
+    }
+
+    @Override
+    public void doExecute(@NotNull final Editor editor, @Nullable Caret c, DataContext dataContext) {
+      List<Caret> carets = editor.getCaretModel().getAllCarets();
+      if (carets.size() != 1) return;
+      Caret caret = carets.get(0);
+      int caretOffset = caret.getOffset();
+      TextRange caretTextRange;
+      boolean matchExactWords;
+      if (caret.hasSelection()) {
+        caretTextRange = new TextRange(editor.getSelectionModel().getSelectionStart(),
+            editor.getSelectionModel().getSelectionEnd());
+        matchExactWords = false;
+      }
+      else {
+        caretTextRange = BashTextOccurrencesUtil.findTextRangeOfIdentifierAtCaret(editor);
+        matchExactWords = true;
+        if (caretTextRange == null) return;
+      }
+      CharSequence documentText = editor.getDocument().getImmutableCharSequence();
+      CharSequence textToFind = caretTextRange.subSequence(documentText);
+      List<TextRange> occurrences = BashTextOccurrencesUtil.findAllOccurrences(documentText, textToFind, matchExactWords);
+      if (!occurrences.remove(caretTextRange)) {
+        LOG.error("Cannot find " + caretTextRange + " in all occurrences");
+      }
+      occurrences.add(0, caretTextRange); // workaround to restore the original caret on Escape
+
+      List<CaretState> caretStates = new ArrayList<>();
+      for (TextRange occurrence : occurrences) {
+        int caretInSelection = occurrence.getStartOffset() + caretOffset - caretTextRange.getStartOffset();
+        CaretState caretState = new CaretState(editor.offsetToLogicalPosition(caretInSelection),
+            editor.offsetToLogicalPosition(occurrence.getStartOffset()),
+            editor.offsetToLogicalPosition(occurrence.getEndOffset()));
+        caretStates.add(caretState);
+      }
+      editor.getCaretModel().setCaretsAndSelections(caretStates);
+    }
+  }
+}
