4529cf2dbfdd4119c517a217b364b6cf6866e978
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-04-30 18:26:32 +0200

    hasComponent not required - just compare to null
    GitOrigin-RevId: 84f7276857dc03cf14647902fb17da1454b6ea71

--- platform/platform-api/src/com/intellij/openapi/wm/IdeFocusManager.java
+++ platform/platform-api/src/com/intellij/openapi/wm/IdeFocusManager.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.wm;
 
 import com.intellij.openapi.actionSystem.CommonDataKeys;
@@ -206,7 +206,7 @@ public abstract class IdeFocusManager implements FocusRequestor {
     IdeFocusManager fm = null;
 
     Application app = ApplicationManager.getApplication();
-    if (app != null && app.hasComponent(IdeFocusManager.class)) {
+    if (app != null) {
       fm = app.getComponent(IdeFocusManager.class);
     }
 
