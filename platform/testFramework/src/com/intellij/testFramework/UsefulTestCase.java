84cd07219616f4931179c0544a3ccc71b3241509
Michael Bogdanov <mikhael.bogdanov@jetbrains.com>
2019-04-30 18:17:36 +0200

    Support test parallelization in UsefulTestCase
     Commit prevents folder clashing on parallel test execution.
    
    (cherry picked from commit a3a889906dba1c559eb8a9bb71943756a066e267)
    
    GitOrigin-RevId: cc691624ef1b2a4fc1db161da4c946ed07460d3f

--- platform/testFramework/src/com/intellij/testFramework/UsefulTestCase.java
+++ platform/testFramework/src/com/intellij/testFramework/UsefulTestCase.java
@@ -117,7 +117,7 @@ public abstract class UsefulTestCase extends TestCase {
       String testName =  FileUtil.sanitizeFileName(getTestName(true));
       if (StringUtil.isEmptyOrSpaces(testName)) testName = "";
       testName = new File(testName).getName(); // in case the test name contains file separators
-      myTempDir = new File(ORIGINAL_TEMP_DIR, TEMP_DIR_MARKER + testName).getPath();
+      myTempDir = FileUtil.createTempDirectory(TEMP_DIR_MARKER + testName, "", false).getPath();
       FileUtil.resetCanonicalTempPathCache(myTempDir);
     }
     boolean isStressTest = isStressTest();
