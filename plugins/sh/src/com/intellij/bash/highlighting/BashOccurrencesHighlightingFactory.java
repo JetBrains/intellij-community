a870566a7a40363d23cb8df972103dbcfc1f2c46
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-05-01 19:57:33 +0100

    bash text occurrences highlighting: honor CodeInsightSettings.HIGHLIGHT_IDENTIFIER_UNDER_CARET
    GitOrigin-RevId: 28304663f81e6d7003ba33dc451dfbda5dc3beab

--- plugins/sh/src/com/intellij/bash/highlighting/BashOccurrencesHighlightingFactory.java
+++ plugins/sh/src/com/intellij/bash/highlighting/BashOccurrencesHighlightingFactory.java
@@ -1,6 +1,7 @@
 package com.intellij.bash.highlighting;
 
 import com.intellij.bash.psi.BashFile;
+import com.intellij.codeInsight.CodeInsightSettings;
 import com.intellij.codeInsight.highlighting.HighlightUsagesHandlerBase;
 import com.intellij.codeInsight.highlighting.HighlightUsagesHandlerFactoryBase;
 import com.intellij.openapi.editor.Editor;
@@ -13,8 +14,12 @@ public class BashOccurrencesHighlightingFactory extends HighlightUsagesHandlerFa
   @Nullable
   @Override
   public HighlightUsagesHandlerBase createHighlightUsagesHandler(@NotNull Editor editor, @NotNull PsiFile file, @NotNull PsiElement target) {
-    return file instanceof BashFile
-        ? new BashOccurrencesHighlightUsagesHandler(editor, file)
-        : null;
+    if (editor.isOneLineMode() ||
+        !CodeInsightSettings.getInstance().HIGHLIGHT_IDENTIFIER_UNDER_CARET ||
+        !(file instanceof BashFile)
+    ) {
+      return null;
+    }
+    return new BashOccurrencesHighlightUsagesHandler(editor, file);
   }
 }
