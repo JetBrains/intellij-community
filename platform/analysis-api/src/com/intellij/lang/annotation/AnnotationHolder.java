1bdaef6506b6660ddedfcf52cf8b72c4f0545a86
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-05-02 20:49:33 +0200

    Cleanup (warnings; formatting)
    GitOrigin-RevId: 4173f884eb589d5ef8ebcd2276dedffe3b6c3a82

--- platform/analysis-api/src/com/intellij/lang/annotation/AnnotationHolder.java
+++ platform/analysis-api/src/com/intellij/lang/annotation/AnnotationHolder.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2013 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.lang.annotation;
 
 import com.intellij.lang.ASTNode;
@@ -25,7 +11,7 @@ import org.jetbrains.annotations.Nullable;
  * Allows a custom language plugin to define annotations for files in that language.
  *
  * @author max
- * @see Annotator#annotate(com.intellij.psi.PsiElement, AnnotationHolder)
+ * @see Annotator#annotate(PsiElement, AnnotationHolder)
  */
 public interface AnnotationHolder {
   /**
@@ -163,7 +149,9 @@ public interface AnnotationHolder {
    * @param htmlTooltip  the tooltip to show (usually the message, but escaped as HTML and surrounded by a {@code <html>} tag
    * @return the annotation (which can be modified to set additional annotation parameters)
    */
-  Annotation createAnnotation(@NotNull HighlightSeverity severity, @NotNull TextRange range, @Nullable String message,
+  Annotation createAnnotation(@NotNull HighlightSeverity severity,
+                              @NotNull TextRange range,
+                              @Nullable String message,
                               @Nullable String htmlTooltip);
 
   @NotNull
