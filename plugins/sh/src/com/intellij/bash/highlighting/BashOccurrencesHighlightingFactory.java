1d16f7c3154f02aa8e487d2f18d0629882673de1
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-05-02 22:17:58 +0100

    fix after review
    GitOrigin-RevId: 74f3c66d427e89ae1876b11a261597e0f6e68d87

--- plugins/sh/src/com/intellij/bash/highlighting/BashOccurrencesHighlightingFactory.java
+++ plugins/sh/src/com/intellij/bash/highlighting/BashOccurrencesHighlightingFactory.java
@@ -1,7 +1,6 @@
 package com.intellij.bash.highlighting;
 
 import com.intellij.bash.psi.BashFile;
-import com.intellij.codeInsight.CodeInsightSettings;
 import com.intellij.codeInsight.highlighting.HighlightUsagesHandlerBase;
 import com.intellij.codeInsight.highlighting.HighlightUsagesHandlerFactoryBase;
 import com.intellij.openapi.editor.Editor;
@@ -14,12 +13,8 @@ public class BashOccurrencesHighlightingFactory extends HighlightUsagesHandlerFa
   @Nullable
   @Override
   public HighlightUsagesHandlerBase createHighlightUsagesHandler(@NotNull Editor editor, @NotNull PsiFile file, @NotNull PsiElement target) {
-    if (editor.isOneLineMode() ||
-        !CodeInsightSettings.getInstance().HIGHLIGHT_IDENTIFIER_UNDER_CARET ||
-        !(file instanceof BashFile)
-    ) {
-      return null;
-    }
-    return new BashOccurrencesHighlightUsagesHandler(editor, file);
+    return !editor.isOneLineMode() && file instanceof BashFile
+        ? new BashOccurrencesHighlightUsagesHandler(editor, file)
+        : null;
   }
 }
