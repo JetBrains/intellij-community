d907d69c9dcb39d9ee0a0f2783b25e2968522dda
Bas Leijdekkers <basleijdekkers@gmail.com>
2019-05-03 14:51:53 +0200

    IG: normalize mutually dependent variables in for loop correctly (IDEA-CR-46203)
    and care for comments as well
    
    GitOrigin-RevId: 0457e9beddae1ffe8165ed8b98d5ac5ff1e723ae

--- plugins/InspectionGadgets/test/com/siyeh/igfixes/style/cstyle_array_declaration/InForLoop.after.java
+++ plugins/InspectionGadgets/test/com/siyeh/igfixes/style/cstyle_array_declaration/InForLoop.after.java
@@ -1,7 +1,7 @@
 class InForLoop {
   void x(int i) {
   if (true) {
-      int[] is = {};
-      for (int[] ii = {0}; i < 10; i++) {}
+      int[] ii = {0};
+      for (int[] is = {}; i < 10; i++) {}
   }
 }}
