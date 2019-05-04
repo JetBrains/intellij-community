1fc5c8af75636a27732bf747f4845ff5e11c6f04
mibac138 <5672750+mibac138@users.noreply.github.com>
2019-05-04 13:11:06 +0200

    T: Add tests to TomlPsiFactory
    GitOrigin-RevId: d727e62f6084e2b2ac495325102f321c4543c47e

---
+++ plugins/toml/src/test/kotlin/org/toml/lang/psi/TomlPsiFactoryTest.kt
@@ -0,0 +1,55 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.toml.lang.psi
+
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase
+
+class TomlPsiFactoryTest : LightPlatformCodeInsightFixtureTestCase() {
+    private val factory: TomlPsiFactory get() = TomlPsiFactory(project)
+
+    fun `test create value`() {
+        val value = factory.createValue("\"value\"")
+        assertEquals("\"value\"", value.text)
+    }
+
+    fun `test create key`() {
+        val key = factory.createKey("key")
+        assertEquals("key", key.text)
+    }
+
+    fun `test create key value 1`() {
+        val keyValue = factory.createKeyValue("key")
+        assertEquals("key", keyValue.key.text)
+    }
+
+    fun `test create key value 2`() {
+        val keyValue = factory.createKeyValue("key", "\"value\"")
+        assertEquals("key", keyValue.key.text)
+        assertEquals("\"value\"", keyValue.value!!.text)
+    }
+
+    fun `test create table`() {
+        val table = factory.createTable("key1.key2")
+        assertEquals("[key1.key2]", table.text)
+    }
+
+    fun `test create table header`() {
+        val tableHeader = factory.createTableHeader("key1.key2")
+        assertEquals("[key1.key2]", tableHeader.text)
+        assertEquals(listOf("key1", "key2"), tableHeader.names.map { it.text })
+    }
+
+    fun `test create array`() {
+        val array = factory.createArray("1, [2], [[3]]")
+        assertEquals("[1, [2], [[3]]]", array.text)
+        assertEquals(listOf("1", "[2]", "[[3]]"), array.elements.map { it.text })
+    }
+
+    fun `test create inline table`() {
+        val inlineTable = factory.createInlineTable("key = \"value\"")
+        assertEquals("{key = \"value\"}", inlineTable.text)
+    }
+}
