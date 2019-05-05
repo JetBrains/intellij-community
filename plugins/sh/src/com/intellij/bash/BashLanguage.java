fab83af94821aad4da04eef45d04523650a6ae0a
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-05-05 20:42:39 +0100

    remove empty lines
    GitOrigin-RevId: fdb5ac0482c71dcb0c798eedd9f31d9e11614256

--- plugins/sh/src/com/intellij/bash/BashLanguage.java
+++ plugins/sh/src/com/intellij/bash/BashLanguage.java
@@ -1,6 +1,5 @@
 package com.intellij.bash;
 
-
 import com.intellij.bash.highlighter.BashHighlighterFactory;
 import com.intellij.lang.Language;
 import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory;
@@ -10,7 +9,6 @@ public class BashLanguage extends Language {
 
   public BashLanguage() {
     super("Shell Script", "application/x-bsh", "application/x-sh", "text/x-script.sh");
-
     SyntaxHighlighterFactory.LANGUAGE_FACTORY.addExplicitExtension(this, new BashHighlighterFactory());
   }
 }
