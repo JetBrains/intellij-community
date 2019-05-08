6b218131a6a9dd49dc849505d73ed9b4ca993e36
Kirill Likhodedov <Kirill.Likhodedov@gmail.com>
2019-05-08 16:56:44 +0300

    git: kotlinify
    GitOrigin-RevId: 054f8bc64fb6ba88acde6db481ae22b6b73a9972

--- plugins/git4idea/src/git4idea/update/GitUpdateSession.kt
+++ plugins/git4idea/src/git4idea/update/GitUpdateSession.kt
@@ -3,15 +3,11 @@ package git4idea.update
 
 import com.intellij.dvcs.DvcsUtil.getShortNames
 import com.intellij.dvcs.DvcsUtil.getShortRepositoryName
-import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vcs.VcsException
 import com.intellij.openapi.vcs.update.UpdateSession
 import com.intellij.util.containers.MultiMap
 import git4idea.repo.GitRepository
 
-/**
- * Git update session implementation
- */
 class GitUpdateSession(private val result: Boolean, private val skippedRoots: Map<GitRepository, String>) : UpdateSession {
 
   override fun getExceptions(): List<VcsException> {
@@ -28,18 +24,18 @@ class GitUpdateSession(private val result: Boolean, private val skippedRoots: Ma
     if (skippedRoots.isEmpty()) return null
 
     if (skippedRoots.size == 1) {
-      val repo = skippedRoots.keys.iterator().next()
+      val repo = skippedRoots.keys.first()
       return "${getShortRepositoryName(repo)} was skipped (${skippedRoots[repo]})"
     }
 
     val prefix = "Skipped ${skippedRoots.size} repositories: <br/>"
     val grouped = groupByReasons(skippedRoots)
     if (grouped.keySet().size == 1) {
-      val reason = grouped.keySet().iterator().next()
+      val reason = grouped.keySet().first()
       return prefix + getShortNames(grouped.get(reason)) + " (" + reason + ")"
     }
 
-    return prefix + StringUtil.join(grouped.keySet(), { reason -> getShortNames(grouped.get(reason)) + " (" + reason + ")" }, "<br/>")
+    return prefix + grouped.keySet().joinToString("<br/>") { reason -> getShortNames(grouped.get(reason)) + " (" + reason + ")" }
   }
 
   private fun groupByReasons(skippedRoots: Map<GitRepository, String>): MultiMap<String, GitRepository> {
