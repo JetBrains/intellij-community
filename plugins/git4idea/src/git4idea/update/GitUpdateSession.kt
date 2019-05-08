931068b708a8ea4b01122ba65c53d77b1c3b1d5f
Kirill Likhodedov <Kirill.Likhodedov@gmail.com>
2019-05-08 16:52:55 +0300

    git: convert to Kotlin
    Will be significantly changed in subsequent commits.
    
    GitOrigin-RevId: 11aceeecf3a1be9438afe40f7104e15a55e7b5bb

--- plugins/git4idea/src/git4idea/update/GitUpdateSession.kt
+++ plugins/git4idea/src/git4idea/update/GitUpdateSession.kt
@@ -1,72 +1,50 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package git4idea.update;
+package git4idea.update
 
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.openapi.vcs.VcsException;
-import com.intellij.openapi.vcs.update.UpdateSession;
-import com.intellij.util.containers.MultiMap;
-import git4idea.repo.GitRepository;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-
-import static com.intellij.dvcs.DvcsUtil.getShortNames;
-import static com.intellij.dvcs.DvcsUtil.getShortRepositoryName;
+import com.intellij.dvcs.DvcsUtil.getShortNames
+import com.intellij.dvcs.DvcsUtil.getShortRepositoryName
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.openapi.vcs.VcsException
+import com.intellij.openapi.vcs.update.UpdateSession
+import com.intellij.util.containers.MultiMap
+import git4idea.repo.GitRepository
 
 /**
  * Git update session implementation
  */
-public class GitUpdateSession implements UpdateSession {
-  private final boolean myResult;
-  @NotNull private final Map<GitRepository, String> mySkippedRoots;
+class GitUpdateSession(private val result: Boolean, private val skippedRoots: Map<GitRepository, String>) : UpdateSession {
 
-  public GitUpdateSession(boolean result, @NotNull Map<GitRepository, String> roots) {
-    myResult = result;
-    mySkippedRoots = roots;
+  override fun getExceptions(): List<VcsException> {
+    return emptyList()
   }
 
-  @Override
-  @NotNull
-  public List<VcsException> getExceptions() {
-    return Collections.emptyList();
+  override fun onRefreshFilesCompleted() {}
+
+  override fun isCanceled(): Boolean {
+    return !result
   }
 
-  @Override
-  public void onRefreshFilesCompleted() {
-  }
+  override fun getAdditionalNotificationContent(): String? {
+    if (skippedRoots.isEmpty()) return null
 
-  @Override
-  public boolean isCanceled() {
-    return !myResult;
-  }
-
-  @Nullable
-  @Override
-  public String getAdditionalNotificationContent() {
-    if (mySkippedRoots.isEmpty()) return null;
-
-    if (mySkippedRoots.size() == 1) {
-      GitRepository repo = mySkippedRoots.keySet().iterator().next();
-      return getShortRepositoryName(repo) + " was skipped (" + mySkippedRoots.get(repo) + ")";
+    if (skippedRoots.size == 1) {
+      val repo = skippedRoots.keys.iterator().next()
+      return "${getShortRepositoryName(repo)} was skipped (${skippedRoots[repo]})"
     }
 
-    String prefix = "Skipped " + mySkippedRoots.size() + " repositories: <br/>";
-    MultiMap<String, GitRepository> grouped = groupByReasons(mySkippedRoots);
-    if (grouped.keySet().size() == 1) {
-      String reason = grouped.keySet().iterator().next();
-      return prefix + getShortNames(grouped.get(reason)) + " (" + reason + ")";
+    val prefix = "Skipped ${skippedRoots.size} repositories: <br/>"
+    val grouped = groupByReasons(skippedRoots)
+    if (grouped.keySet().size == 1) {
+      val reason = grouped.keySet().iterator().next()
+      return prefix + getShortNames(grouped.get(reason)) + " (" + reason + ")"
     }
 
-    return prefix + StringUtil.join(grouped.keySet(), reason -> getShortNames(grouped.get(reason)) + " (" + reason + ")", "<br/>");
+    return prefix + StringUtil.join(grouped.keySet(), { reason -> getShortNames(grouped.get(reason)) + " (" + reason + ")" }, "<br/>")
   }
 
-  @NotNull
-  private static MultiMap<String, GitRepository> groupByReasons(@NotNull Map<GitRepository, String> skippedRoots) {
-    MultiMap<String, GitRepository> result = MultiMap.create();
-    skippedRoots.forEach((file, s) -> result.putValue(s, file));
-    return result;
+  private fun groupByReasons(skippedRoots: Map<GitRepository, String>): MultiMap<String, GitRepository> {
+    val result = MultiMap.create<String, GitRepository>()
+    skippedRoots.forEach { (file, s) -> result.putValue(s, file) }
+    return result
   }
 }
