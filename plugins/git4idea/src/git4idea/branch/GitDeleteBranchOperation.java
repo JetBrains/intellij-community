3061e8e4eb66b85ad6b21d58308f8369e5067bd9
Kirill Likhodedov <Kirill.Likhodedov@gmail.com>
2019-05-09 09:43:04 +0300

    git: don't start background process inside background process
    GitBrancher already performs remote branch deletion under a background Task.
    
    GitOrigin-RevId: 645379ed52d3dacbad1eda06a9493479d7ce02c5

--- plugins/git4idea/src/git4idea/branch/GitDeleteBranchOperation.java
+++ plugins/git4idea/src/git4idea/branch/GitDeleteBranchOperation.java
@@ -316,16 +316,11 @@ class GitDeleteBranchOperation extends GitBranchOperation {
   }
 
   private void deleteTrackedBranchInBackground() {
-    new Task.Backgroundable(myProject, "Deleting Remote Branch " + myBranchName + "...") {
-      @Override
-      public void run(@NotNull ProgressIndicator indicator) {
-        GitBrancher brancher = GitBrancher.getInstance(getProject());
-        MultiMap<String, GitRepository> grouped = groupTrackedBranchesByName();
-        for (String remoteBranch : grouped.keySet()) {
-          brancher.deleteRemoteBranch(remoteBranch, new ArrayList<>(grouped.get(remoteBranch)));
-        }
-      }
-    }.queue();
+    GitBrancher brancher = GitBrancher.getInstance(myProject);
+    MultiMap<String, GitRepository> grouped = groupTrackedBranchesByName();
+    for (String remoteBranch : grouped.keySet()) {
+      brancher.deleteRemoteBranch(remoteBranch, new ArrayList<>(grouped.get(remoteBranch)));
+    }
   }
 
   @NotNull
