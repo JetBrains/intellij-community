cee231a37cd5cc4e922fd7a8efe81d8a80aa3d74
Anton Lobov <Anton.Lobov@jetbrains.com>
2019-05-09 09:55:31 +0200

    json: cleanup unused utility methods
    GitOrigin-RevId: d370d3c0fe68bcffb8666ab11bff51fa5e14351e

--- json/src/com/intellij/json/JsonUtil.java
+++ json/src/com/intellij/json/JsonUtil.java
@@ -4,16 +4,13 @@ package com.intellij.json;
 import com.intellij.json.psi.*;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.fileTypes.LanguageFileType;
-import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.util.ObjectUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @author Mikhail Golubev
@@ -53,22 +50,6 @@ public class JsonUtil {
     final JsonProperty property = object.findProperty(name);
     if (property == null) return null;
     return ObjectUtils.tryCast(property.getValue(), clazz);
-  }
-
-  @Nullable
-  public static List<String> getChildAsStringList(@NotNull final JsonObject object, @NotNull final String name) {
-    final JsonArray array = getPropertyValueOfType(object, name, JsonArray.class);
-    if (array != null) return array.getValueList().stream().filter(value -> value instanceof JsonStringLiteral)
-      .map(value -> StringUtil.unquoteString(value.getText())).collect(Collectors.toList());
-    return null;
-  }
-
-  @Nullable
-  public static List<String> getChildAsSingleStringOrList(@NotNull final JsonObject object, @NotNull final String name) {
-    final List<String> list = getChildAsStringList(object, name);
-    if (list != null) return list;
-    final JsonStringLiteral literal = getPropertyValueOfType(object, name, JsonStringLiteral.class);
-    return literal == null ? null : Collections.singletonList(StringUtil.unquoteString(literal.getText()));
   }
 
   public static boolean isArrayElement(@NotNull PsiElement element) {
