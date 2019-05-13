f7a8abac0807f83dad21821bb74e175dfd8dc37e
Valentin Fondaratov <valentin.fondaratov@jetbrains.com>
2019-05-13 22:32:40 +0300

    make StructureTreeModel accept parentDisposable
    it's a more proper fix for b32526e4d9a507ec19e1e8c0db901def5c1d2ec7
    
    GitOrigin-RevId: 79637beb9839f433079a406cdfaca55bf9cff241

--- platform/platform-impl/src/com/intellij/ui/tree/StructureTreeModel.java
+++ platform/platform-impl/src/com/intellij/ui/tree/StructureTreeModel.java
@@ -7,10 +7,12 @@ import com.intellij.ide.util.treeView.NodeDescriptor;
 import com.intellij.ide.util.treeView.ValidateableNode;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.util.Disposer;
 import com.intellij.util.concurrency.Invoker;
 import com.intellij.util.concurrency.InvokerSupplier;
 import com.intellij.util.ui.tree.AbstractTreeModel;
 import com.intellij.util.ui.tree.TreeUtil;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.concurrency.AsyncPromise;
@@ -44,20 +46,42 @@ public class StructureTreeModel<Structure extends AbstractTreeStructure>
   private final Structure structure;
   private volatile Comparator<? super Node> comparator;
 
-  private StructureTreeModel(@NotNull Structure structure, boolean background) {
+  private StructureTreeModel(@NotNull Structure structure, boolean background, @NotNull Disposable parentDisposable) {
     this.structure = structure;
     description = format(structure.toString());
     invoker = background
               ? new Invoker.BackgroundThread(this)
               : new Invoker.EDT(this);
+    Disposer.register(parentDisposable, this);
   }
 
+  /**
+   * @deprecated Please use {@link #StructureTreeModel(AbstractTreeStructure, Disposable)}
+   */
+  @Deprecated
+  @ApiStatus.ScheduledForRemoval(inVersion = "2019.3")
   public StructureTreeModel(@NotNull Structure structure) {
-    this(structure, true);
+    this(structure, Disposer.newDisposable());
   }
 
-  public StructureTreeModel(@NotNull Structure structure, @NotNull Comparator<? super NodeDescriptor> comparator) {
-    this(structure);
+  /**
+   * @deprecated Please use {@link #StructureTreeModel(AbstractTreeStructure, Comparator, Disposable)}
+   */
+  @Deprecated
+  @ApiStatus.ScheduledForRemoval(inVersion = "2019.3")
+  public StructureTreeModel(@NotNull Structure structure,
+                            @NotNull Comparator<? super NodeDescriptor> comparator) {
+    this(structure, comparator, Disposer.newDisposable());
+  }
+
+  public StructureTreeModel(@NotNull Structure structure, @NotNull Disposable parentDisposable) {
+    this(structure, true, parentDisposable);
+  }
+
+  public StructureTreeModel(@NotNull Structure structure,
+                            @NotNull Comparator<? super NodeDescriptor> comparator,
+                            @NotNull Disposable parentDisposable) {
+    this(structure, parentDisposable);
     this.comparator = wrapToNodeComparator(comparator);
   }
 
