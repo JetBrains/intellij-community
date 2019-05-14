e749d0eb85b26ca8151629cfd8a65ec35fbcbcad
graann <Anna.Gromova@jetbrains.com>
2019-05-14 16:20:59 +0300

    Implement new Editor Tabs UI: New Tabs UI: IDE no longer shows tips when no editor tabs are open (#IDEA-212553)
    GitOrigin-RevId: 4482dae6a0348d90338abc87ec60bd612d5c6c66

--- platform/platform-api/src/com/intellij/ui/tabs/newImpl/JBTabsImpl.java
+++ platform/platform-api/src/com/intellij/ui/tabs/newImpl/JBTabsImpl.java
@@ -1272,6 +1272,18 @@ public class JBTabsImpl extends JComponent
   }
 
   void revalidateAndRepaint(final boolean layoutNow) {
+    if (myVisibleInfos.isEmpty()) {
+      setOpaque(false);
+      final Component nonOpaque = UIUtil.findUltimateParent(this);
+      if (nonOpaque != null && getParent() != null) {
+        final Rectangle toRepaint = SwingUtilities.convertRectangle(getParent(), getBounds(), nonOpaque);
+        nonOpaque.repaint(toRepaint.x, toRepaint.y, toRepaint.width, toRepaint.height);
+      }
+    }
+    else {
+      setOpaque(true);
+    }
+
     if (layoutNow) {
       validate();
     }
@@ -1703,10 +1715,6 @@ public class JBTabsImpl extends JComponent
 
   @Override
   protected void paintComponent(final Graphics g) {
-    g.clearRect(0, 0, getWidth(), getHeight());
-    myTabPainter.fillBackground((Graphics2D)g, new Rectangle(0, 0, getWidth(), getHeight()));
-    myBorder.paintBorder(this, g, 0, 0, getWidth(), getHeight());
-
     super.paintComponent(g);
 
     if (myVisibleInfos.isEmpty()) {
@@ -1714,11 +1722,14 @@ public class JBTabsImpl extends JComponent
         UISettings.setupAntialiasing(g);
         UIUtil.drawCenteredString((Graphics2D)g, getBounds(), myEmptyText);
       }
+      return;
     }
-    else {
-      if (!isStealthModeEffective() && !isHideTabs()) {
-        myLastPaintedSelection = getSelectedInfo();
-      }
+
+    myTabPainter.fillBackground((Graphics2D)g, new Rectangle(0, 0, getWidth(), getHeight()));
+    myBorder.paintBorder(this, g, 0, 0, getWidth(), getHeight());
+
+    if (!isStealthModeEffective() && !isHideTabs()) {
+      myLastPaintedSelection = getSelectedInfo();
     }
   }
 
