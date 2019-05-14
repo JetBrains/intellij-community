f5590346525a70148b1a338ab631d32c899ed565
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-05-14 18:03:41 +0300

    sh plugin: update copyright header
    GitOrigin-RevId: cb3f55c29bfb08a726ebb149d09d561ac5d59c06

--- plugins/sh/src/com/intellij/sh/lexer/ShLexer.java
+++ plugins/sh/src/com/intellij/sh/lexer/ShLexer.java
@@ -1,3 +1,4 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.sh.lexer;
 
 import com.intellij.lexer.FlexAdapter;
