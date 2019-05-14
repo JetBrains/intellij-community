628b44b1cb57497ec5bf0828a13de3932525a7e0
Ivan Semenov <ivan.semenov@jetbrains.com>
2019-05-14 15:53:08 +0300

    [github] Use jackson for API data conversion
    GitOrigin-RevId: 97ada97e4506afb7a77cee4e9e633bb9e7a6cca8

--- plugins/github/src/org/jetbrains/plugins/github/api/data/GithubCommit.java
+++ plugins/github/src/org/jetbrains/plugins/github/api/data/GithubCommit.java
@@ -1,83 +1,20 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.github.api.data;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.io.mandatory.Mandatory;
-import org.jetbrains.io.mandatory.RestModel;
 
 import java.util.Date;
 import java.util.List;
 
-@RestModel
 @SuppressWarnings("UnusedDeclaration")
 public class GithubCommit extends GithubCommitSha {
   private GithubUser author;
   private GithubUser committer;
 
-  @Mandatory private GitCommit commit;
+  private GitCommit commit;
 
-  @Mandatory private List<GithubCommitSha> parents;
-
-  @RestModel
-  public static class GitCommit {
-    private String url;
-    @Mandatory private String message;
-
-    @Mandatory private GitUser author;
-    @Mandatory private GitUser committer;
-
-    @NotNull
-    public String getMessage() {
-      return message;
-    }
-
-    @NotNull
-    public GitUser getAuthor() {
-      return author;
-    }
-
-    @NotNull
-    public GitUser getCommitter() {
-      return committer;
-    }
-  }
-
-  @RestModel
-  public static class GitUser {
-    @Mandatory private String name;
-    @Mandatory private String email;
-    @Mandatory private Date date;
-
-    @NotNull
-    public String getName() {
-      return name;
-    }
-
-    @NotNull
-    public String getEmail() {
-      return email;
-    }
-
-    @NotNull
-    public Date getDate() {
-      return date;
-    }
-  }
+  private List<GithubCommitSha> parents;
 
   @Nullable
   public GithubUser getAuthor() {
@@ -97,5 +34,49 @@ public class GithubCommit extends GithubCommitSha {
   @NotNull
   public GitCommit getCommit() {
     return commit;
+  }
+
+  public static class GitCommit {
+    private String url;
+    private String message;
+
+    private GitUser author;
+    private GitUser committer;
+
+    @NotNull
+    public String getMessage() {
+      return message;
+    }
+
+    @NotNull
+    public GitUser getAuthor() {
+      return author;
+    }
+
+    @NotNull
+    public GitUser getCommitter() {
+      return committer;
+    }
+  }
+
+  public static class GitUser {
+    private String name;
+    private String email;
+    private Date date;
+
+    @NotNull
+    public String getName() {
+      return name;
+    }
+
+    @NotNull
+    public String getEmail() {
+      return email;
+    }
+
+    @NotNull
+    public Date getDate() {
+      return date;
+    }
   }
 }
