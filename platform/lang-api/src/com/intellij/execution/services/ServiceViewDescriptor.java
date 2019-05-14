11a30640327603adbf5254e8a3e2340b72f71d26
Konstantin Aleev <konstantin.aleev@jetbrains.com>
2019-05-14 20:20:24 +0300

    [service-view] persist extracted tabs
    GitOrigin-RevId: ff650570eb779a4c8ac23cfdbbbe4a44d0a19680

--- platform/lang-api/src/com/intellij/execution/services/ServiceViewDescriptor.java
+++ platform/lang-api/src/com/intellij/execution/services/ServiceViewDescriptor.java
@@ -17,6 +17,11 @@ public interface ServiceViewDescriptor {
   ItemPresentation getPresentation();
 
   @Nullable
+  default String getId() {
+    return getPresentation().getPresentableText();
+  }
+
+  @Nullable
   default JComponent getContentComponent() {
     return null;
   }
