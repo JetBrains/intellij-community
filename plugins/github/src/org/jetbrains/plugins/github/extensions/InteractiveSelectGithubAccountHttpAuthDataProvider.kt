eea5a78c32fe234579c74a39b7b87bd63ff766f5
Ivan Semenov <ivan.semenov@jetbrains.com>
2019-05-15 16:29:39 +0300

    [github] Ability to login to git with github even if there are no accounts
    Issue: IDEA-196905
    GitOrigin-RevId: ea9a81979ba5cbd8cd880dece66cd55f1f1e9d5c

--- plugins/github/src/org/jetbrains/plugins/github/extensions/InteractiveSelectGithubAccountHttpAuthDataProvider.kt
+++ plugins/github/src/org/jetbrains/plugins/github/extensions/InteractiveSelectGithubAccountHttpAuthDataProvider.kt
@@ -1,8 +1,6 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.github.extensions
 
-import com.intellij.openapi.application.ModalityState
-import com.intellij.openapi.application.invokeAndWaitIfNeeded
 import com.intellij.openapi.project.Project
 import com.intellij.util.AuthData
 import git4idea.DialogManager
@@ -14,9 +12,9 @@ import org.jetbrains.plugins.github.authentication.ui.GithubChooseAccountDialog
 import org.jetbrains.plugins.github.util.GithubUtil
 import java.awt.Component
 
-internal class InteractiveGithubHttpAuthDataProvider(private val project: Project,
-                                                     private val potentialAccounts: Collection<GithubAccount>,
-                                                     private val authenticationManager: GithubAuthenticationManager) : InteractiveGitHttpAuthDataProvider {
+internal class InteractiveSelectGithubAccountHttpAuthDataProvider(private val project: Project,
+                                                                  private val potentialAccounts: Collection<GithubAccount>,
+                                                                  private val authenticationManager: GithubAuthenticationManager) : InteractiveGitHttpAuthDataProvider {
 
   @CalledInAwt
   override fun getAuthData(parentComponent: Component?): AuthData? {
@@ -31,10 +29,10 @@ internal class InteractiveGithubHttpAuthDataProvider(private val project: Projec
     DialogManager.show(dialog)
     if (!dialog.isOK) return null
     val account = dialog.account
-    val token = invokeAndWaitIfNeeded(parentComponent?.let(ModalityState::stateForComponent) ?: ModalityState.any()) {
-      authenticationManager.getTokenForAccount(account) ?: authenticationManager.requestNewToken(account, project, parentComponent)
-    } ?: return null
+    val token = authenticationManager.getTokenForAccount(account)
+                ?: authenticationManager.requestNewToken(account, project, parentComponent)
+                ?: return null
     if (dialog.setDefault) authenticationManager.setDefaultAccount(project, account)
-    return AuthData(GithubUtil.GIT_AUTH_PASSWORD_SUBSTITUTE, token)
+    return GithubHttpAuthDataProvider.GithubAccountAuthData(account, GithubUtil.GIT_AUTH_PASSWORD_SUBSTITUTE, token)
   }
 }
