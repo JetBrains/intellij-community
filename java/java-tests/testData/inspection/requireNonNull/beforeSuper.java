a5f684906fabd5e0b348c32cdc9846e8ea5bf916
Roman.Ivanov <Roman.Ivanov@jetbrains.com>
2019-05-20 15:34:16 +0700

    TryFinallyCanBeTryWithResourcesInspection: non resources variables used outside of try should stay out of try
    GitOrigin-RevId: e83a1b2b5fef7a005a94665c0246e816e25ae9f3

---
+++ java/java-tests/testData/inspection/requireNonNull/beforeSuper.java
@@ -0,0 +1,19 @@
+// "Replace condition with Objects.requireNonNullElse" "false"
+
+class Component {
+  public Component add(Component component, int index) {
+    return new Component();
+  }
+}
+
+class Usage extends Component {
+  protected Component cont = new Usage();
+
+  @Override
+  public Component add(Component component, int index) {
+    if<caret> (cont == null) {
+      return (super.add(component, index));
+    }
+    return cont.add(component, index);
+  }
+}
