b29def763221d85650f201f8c9d595fa3ec8f141
Svetlana.Zemlyanskaya <svetlana.zemlyanskaya@jetbrains.com>
2019-05-20 17:33:40 +0200

    FUS: implement util validator for module facet type (FUS-385)
    GitOrigin-RevId: d33a1832e8463097911e1b2d4d1048e01e83a3ac

--- platform/platform-impl/src/com/intellij/internal/statistic/collectors/fus/FacetTypeUsageCollector.java
+++ platform/platform-impl/src/com/intellij/internal/statistic/collectors/fus/FacetTypeUsageCollector.java
@@ -3,15 +3,21 @@ package com.intellij.internal.statistic.collectors.fus;
 
 import com.intellij.facet.Facet;
 import com.intellij.facet.FacetManager;
+import com.intellij.facet.FacetType;
 import com.intellij.internal.statistic.beans.UsageDescriptor;
+import com.intellij.internal.statistic.eventLog.validator.ValidationResultType;
+import com.intellij.internal.statistic.eventLog.validator.rules.EventContext;
+import com.intellij.internal.statistic.eventLog.validator.rules.impl.CustomUtilsWhiteListRule;
 import com.intellij.internal.statistic.service.fus.collectors.ProjectUsagesCollector;
 import com.intellij.internal.statistic.utils.PluginInfo;
 import com.intellij.internal.statistic.utils.PluginInfoDetectorKt;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -34,5 +40,37 @@ public class FacetTypeUsageCollector extends ProjectUsagesCollector {
   @Override
   public String getGroupId() {
     return "module.facets";
+  }
+
+  public static class FacetTypeUtilValidator extends CustomUtilsWhiteListRule {
+
+    @Override
+    public boolean acceptRuleId(@Nullable String ruleId) {
+      return "facets_type".equals(ruleId);
+    }
+
+    @NotNull
+    @Override
+    protected ValidationResultType doValidate(@NotNull String data, @NotNull EventContext context) {
+      if ("third.party".equals(data)) return ValidationResultType.ACCEPTED;
+
+      final FacetType facet = findFacetById(data);
+      if (facet == null) {
+        return ValidationResultType.REJECTED;
+      }
+      final PluginInfo info = PluginInfoDetectorKt.getPluginInfo(facet.getClass());
+      return info.isDevelopedByJetBrains() ? ValidationResultType.ACCEPTED : ValidationResultType.REJECTED;
+    }
+
+    @Nullable
+    private static FacetType findFacetById(@NotNull String data) {
+      final FacetType[] facets = FacetType.EP_NAME.getExtensions();
+      for (FacetType facet : facets) {
+        if (StringUtil.equals(facet.getStringId(), data)) {
+          return facet;
+        }
+      }
+      return null;
+    }
   }
 }
