720508fb67540099407432112129e7a86d4b2f88
Dmitry Zhuravlev <dmitry.zhuravlev@jetbrains.com>
2019-05-22 14:24:55 +0300

    vcs-ignore: run test in case if corresponding setting enabled
    GitOrigin-RevId: c413d88b0e11ddf93084617cfe2995f02eb9f041

--- plugins/git4idea/tests/git4idea/ignore/GitIgnoredToExcludedSynchronizerTest.kt
+++ plugins/git4idea/tests/git4idea/ignore/GitIgnoredToExcludedSynchronizerTest.kt
@@ -5,14 +5,21 @@ import com.intellij.dvcs.ignore.VcsRepositoryIgnoredFilesHolderBase
 import com.intellij.openapi.application.invokeAndWaitIfNeeded
 import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.roots.ModuleRootManager
+import com.intellij.openapi.vcs.VcsApplicationSettings
 import com.intellij.openapi.vfs.VirtualFile
 import git4idea.repo.GitRepositoryFiles.GITIGNORE
 import git4idea.test.GitSingleRepoTest
+import org.junit.Assume.assumeTrue
 
 class GitIgnoredToExcludedSynchronizerTest : GitSingleRepoTest() {
 
   private lateinit var out: VirtualFile
   private lateinit var excluded: VirtualFile
+
+  override fun setUp() {
+    super.setUp()
+    assumeTrue(VcsApplicationSettings.getInstance().MARK_IGNORED_AS_EXCLUDED)
+  }
 
   override fun setUpModule() {
     runWriteAction {
