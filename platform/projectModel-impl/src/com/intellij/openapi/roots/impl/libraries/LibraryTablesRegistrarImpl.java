d1727fe6355efdbfde3d06a5bc5f58efed391967
nik <Nikolay.Chashnikov@jetbrains.com>
2019-05-23 18:27:17 +0300

    deprecate ProjectLibraryTable.getInstance method and mark ProjectLibraryTable as internal
    This is needed to help third-party plugins get rid of usages of class from impl package.
    
    GitOrigin-RevId: ee1b774399890681c50abe1708689c77015964e9

--- platform/projectModel-impl/src/com/intellij/openapi/roots/impl/libraries/LibraryTablesRegistrarImpl.java
+++ platform/projectModel-impl/src/com/intellij/openapi/roots/impl/libraries/LibraryTablesRegistrarImpl.java
@@ -15,14 +15,15 @@
  */
 package com.intellij.openapi.roots.impl.libraries;
 
+import com.intellij.openapi.Disposable;
+import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.libraries.LibraryTable;
 import com.intellij.openapi.roots.libraries.LibraryTablesRegistrar;
-import com.intellij.openapi.Disposable;
 import com.intellij.util.SmartList;
-import java.util.HashMap;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -38,7 +39,7 @@ public class LibraryTablesRegistrarImpl extends LibraryTablesRegistrar implement
   @Override
   @NotNull
   public LibraryTable getLibraryTable(@NotNull Project project) {
-    return ProjectLibraryTable.getInstance(project);
+    return ServiceManager.getService(project, ProjectLibraryTable.class);
   }
 
   @Override
