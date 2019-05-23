ee889a631c4df30470a79ac756b5f53c17174c4c
Yann Cébron <yann.cebron@jetbrains.com>
2019-05-23 16:15:26 +0200

    IDEA-214817 Remove support for <helpset> from Devkit
    GitOrigin-RevId: 7836178653e749e04bdf9cc4bfb6c74665e94815

--- plugins/devkit/devkit-core/src/inspections/PluginXmlDomInspection.java
+++ plugins/devkit/devkit-core/src/inspections/PluginXmlDomInspection.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.devkit.inspections;
 
 import com.intellij.ExtensionPoints;
@@ -207,6 +207,9 @@ public class PluginXmlDomInspection extends BasicDomElementsInspection<IdeaPlugi
         if (element instanceof Component.Project) {
           annotateProjectComponent((Component.Project)element, holder);
         }
+      }
+      else if (element instanceof Helpset) {
+        highlightRedundant(element, DevKitBundle.message("inspections.plugin.xml.deprecated.helpset"), holder);
       }
     }
 
