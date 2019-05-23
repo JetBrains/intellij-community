4bc8b0d38d1072b60d3ab72c43c363e208dfe666
Svetlana.Zemlyanskaya <svetlana.zemlyanskaya@jetbrains.com>
2019-05-23 14:00:27 +0200

    FUS: implement util validator for registries (FUS-375)
    GitOrigin-RevId: 2feefcb13ffbe4902e7e778f06ae82add414c72b

--- platform/platform-impl/src/com/intellij/internal/statistic/collectors/fus/RegistryApplicationUsagesCollector.java
+++ platform/platform-impl/src/com/intellij/internal/statistic/collectors/fus/RegistryApplicationUsagesCollector.java
@@ -2,11 +2,19 @@
 package com.intellij.internal.statistic.collectors.fus;
 
 import com.intellij.internal.statistic.beans.UsageDescriptor;
+import com.intellij.internal.statistic.eventLog.validator.ValidationResultType;
+import com.intellij.internal.statistic.eventLog.validator.rules.EventContext;
+import com.intellij.internal.statistic.eventLog.validator.rules.impl.CustomUtilsWhiteListRule;
 import com.intellij.internal.statistic.service.fus.collectors.ApplicationUsagesCollector;
+import com.intellij.internal.statistic.utils.PluginInfo;
 import com.intellij.internal.statistic.utils.PluginInfoDetectorKt;
+import com.intellij.openapi.application.ExperimentalFeature;
 import com.intellij.openapi.application.Experiments;
 import com.intellij.openapi.util.registry.Registry;
+import com.intellij.openapi.util.registry.RegistryValue;
+import com.intellij.openapi.util.text.StringUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -25,12 +33,11 @@ public class RegistryApplicationUsagesCollector extends ApplicationUsagesCollect
   @NotNull
   static Set<UsageDescriptor> getChangedValuesUsages() {
     Set<UsageDescriptor> registry = Registry.getAll().stream()
-      .filter(key -> key.isChangedFromDefault() && !key.isContributedByThirdPartyPlugin())
+      .filter(key -> key.isChangedFromDefault())
       .map(key -> new UsageDescriptor(key.getKey()))
       .collect(Collectors.toSet());
 
     Set<UsageDescriptor> experiments = Arrays.stream(Experiments.EP_NAME.getExtensions())
-      .filter(f -> PluginInfoDetectorKt.getPluginInfo(f.getClass()).isDevelopedByJetBrains())
       .filter(f -> Experiments.isFeatureEnabled(f.id))
       .map(f -> new UsageDescriptor(f.id))
       .collect(Collectors.toSet());
@@ -44,5 +51,39 @@ public class RegistryApplicationUsagesCollector extends ApplicationUsagesCollect
   @Override
   public String getGroupId() {
     return "platform.registry.application";
+  }
+
+  public static class RegistryUtilValidator extends CustomUtilsWhiteListRule {
+    @Override
+    public boolean acceptRuleId(@Nullable String ruleId) {
+      return "registry_key".equals(ruleId);
+    }
+
+    @NotNull
+    @Override
+    protected ValidationResultType doValidate(@NotNull String data, @NotNull EventContext context) {
+      final ExperimentalFeature feature = findFeatureById(data);
+      if (feature != null) {
+        final PluginInfo info = PluginInfoDetectorKt.getPluginInfo(feature.getClass());
+        if (StringUtil.equals(data, context.eventId)) {
+          context.setPluginInfo(info);
+        }
+        return info.isDevelopedByJetBrains() ? ValidationResultType.ACCEPTED : ValidationResultType.THIRD_PARTY;
+      }
+
+
+      final RegistryValue value = Registry.get(data);
+      return !value.isContributedByThirdPartyPlugin() ? ValidationResultType.ACCEPTED : ValidationResultType.THIRD_PARTY;
+    }
+
+    @Nullable
+    private static ExperimentalFeature findFeatureById(@NotNull String featureId) {
+      for (ExperimentalFeature feature : Experiments.EP_NAME.getExtensions()) {
+        if (StringUtil.equals(feature.id, featureId)) {
+          return feature;
+        }
+      }
+      return null;
+    }
   }
 }
