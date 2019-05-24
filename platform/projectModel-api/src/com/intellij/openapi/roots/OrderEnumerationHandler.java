b93a0f70f9898222a790b9a9cc4bf041bf7ac7c0
nik <Nikolay.Chashnikov@jetbrains.com>
2019-05-24 12:05:09 +0300

    api: annotate some classes from project model API as OverrideOnly
    Methods of such classes aren't supposed to be called from plugins, and such usages may complicate reorganizations of project model API.
    
    GitOrigin-RevId: 5a5a3c7959518f9a2aabfa06215910a50d62e05f

--- platform/projectModel-api/src/com/intellij/openapi/roots/OrderEnumerationHandler.java
+++ platform/projectModel-api/src/com/intellij/openapi/roots/OrderEnumerationHandler.java
@@ -18,13 +18,12 @@ package com.intellij.openapi.roots;
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
 
-/**
- * @author nik
- */
+@ApiStatus.OverrideOnly
 public abstract class OrderEnumerationHandler {
   public static final ExtensionPointName<Factory> EP_NAME =
     ExtensionPointName.create("com.intellij.orderEnumerationHandlerFactory");
