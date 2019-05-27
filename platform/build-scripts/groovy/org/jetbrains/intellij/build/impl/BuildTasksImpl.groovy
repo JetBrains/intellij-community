4a3016eb2181dc665aeec3856ffe8571f34d7960
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2019-05-27 19:05:09 +0300

    Revert "get rid of intellij.build.toolbox.litegen parameter and use BuildOptions.TOOLBOX_LITE_GEN_STEP to skip it instead"
    This reverts commit 9aac8ee4
    
    GitOrigin-RevId: 635964931e531281354b09887f284a29fae1b3bc

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/BuildTasksImpl.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/BuildTasksImpl.groovy
@@ -360,27 +360,29 @@ idea.fatal.error.notification=disabled
 
       List<String> paths = runInParallel(tasks).findAll { it != null }
 
-      buildContext.executeStep("Building Toolbox Lite-Gen Links", BuildOptions.TOOLBOX_LITE_GEN_STEP) {
+      if (Boolean.getBoolean("intellij.build.toolbox.litegen")) {
         if (buildContext.buildNumber == null) {
           buildContext.messages.warning("Toolbox LiteGen is not executed - it does not support SNAPSHOT build numbers")
-        } else if (buildContext.options.targetOS != BuildOptions.OS_ALL) {
-          buildContext.messages.warning("Toolbox LiteGen is not executed - it doesn't support specific OS were selection")
-        } else {
-          String toolboxLiteGenVersion = System.getProperty("intellij.build.toolbox.litegen.version")
-          if (toolboxLiteGenVersion == null) {
-            buildContext.messages.error("Toolbox Lite-Gen version is not specified!")
-          } else {
-            String[] liteGenArgs = [
-              'runToolboxLiteGen',
-              "-Pintellij.build.toolbox.litegen.version=${toolboxLiteGenVersion}",
-              //NOTE[jo]: right now we assume all installer files are created under the same path
-              "-Pintellij.build.artifacts=${buildContext.paths.artifacts}",
-              "-Pintellij.build.productCode=${buildContext.productProperties.productCode}",
-              "-Pintellij.build.isEAP=${buildContext.applicationInfo.isEAP}",
-              "-Pintellij.build.output=${buildContext.paths.buildOutputRoot}/toolbox-lite-gen",
-            ]
+        }
+        else {
+          buildContext.executeStep("Building Toolbox Lite-Gen Links", BuildOptions.TOOLBOX_LITE_GEN_STEP) {
+            String toolboxLiteGenVersion = System.getProperty("intellij.build.toolbox.litegen.version")
+            if (toolboxLiteGenVersion == null) {
+              buildContext.messages.error("Toolbox Lite-Gen version is not specified!")
+            }
+            else {
+              String[] liteGenArgs = [
+                'runToolboxLiteGen',
+                "-Pintellij.build.toolbox.litegen.version=${toolboxLiteGenVersion}",
+                //NOTE[jo]: right now we assume all installer files are created under the same path
+                "-Pintellij.build.artifacts=${buildContext.paths.artifacts}",
+                "-Pintellij.build.productCode=${buildContext.productProperties.productCode}",
+                "-Pintellij.build.isEAP=${buildContext.applicationInfo.isEAP}",
+                "-Pintellij.build.output=${buildContext.paths.buildOutputRoot}/toolbox-lite-gen",
+              ]
 
-            buildContext.gradle.run('Run Toolbox LiteGen', liteGenArgs)
+              buildContext.gradle.run('Run Toolbox LiteGen', liteGenArgs)
+            }
           }
         }
       }
