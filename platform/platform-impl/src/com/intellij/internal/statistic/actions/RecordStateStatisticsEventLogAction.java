a8536294eaee05da95c8c9ea953f6021f31dcbf8
Aleksey Pivovarov <AMPivovarov@gmail.com>
2019-05-30 16:17:23 +0300

    ui: add link to "Record State Collectors" action output
    GitOrigin-RevId: 75d09dc642484ad8867eea042a69362d3f585e69

--- platform/platform-impl/src/com/intellij/internal/statistic/actions/RecordStateStatisticsEventLogAction.java
+++ platform/platform-impl/src/com/intellij/internal/statistic/actions/RecordStateStatisticsEventLogAction.java
@@ -4,16 +4,21 @@ package com.intellij.internal.statistic.actions;
 import com.intellij.internal.statistic.eventLog.fus.FeatureUsageLogger;
 import com.intellij.internal.statistic.service.fus.collectors.FUStateUsagesLogger;
 import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationAction;
 import com.intellij.notification.NotificationType;
-import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
 
 public class RecordStateStatisticsEventLogAction extends AnAction {
   private static final FUStateUsagesLogger myStatesLogger = new FUStateUsagesLogger();
@@ -29,17 +34,24 @@ public class RecordStateStatisticsEventLogAction extends AnAction {
       @Override
       public void run(@NotNull ProgressIndicator indicator) {
         FeatureUsageLogger.INSTANCE.rollOver();
+        File logFile = FeatureUsageLogger.INSTANCE.getConfig().getActiveLogFile();
+        VirtualFile logVFile = logFile != null ? LocalFileSystem.getInstance().findFileByIoFile(logFile) : null;
         myStatesLogger.logApplicationStates();
         myStatesLogger.logProjectStates(project);
 
         ApplicationManager.getApplication().invokeLater(
-          () -> showNotification(project, "Finished collecting and recording events")
+          () -> {
+            Notification notification = new Notification("FeatureUsageStatistics", "Feature Usage Statistics",
+                                                         "Finished collecting and recording events", NotificationType.INFORMATION);
+            if (logVFile != null) {
+              notification.addAction(NotificationAction.createSimple("Show Log File", () -> {
+                FileEditorManager.getInstance(project).openFile(logVFile, true);
+              }));
+            }
+            notification.notify(project);
+          }
         );
       }
     });
-  }
-
-  protected void showNotification(@NotNull Project project, @NotNull String message) {
-    Notifications.Bus.notify(new Notification("FeatureUsageStatistics", "Feature Usage Statistics", message, NotificationType.INFORMATION), project);
   }
 }
