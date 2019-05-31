f6c6f86b5869882eecb9415a5a66f8980ff519bb
Alexander Zolotov <goldifit@gmail.com>
2019-05-31 14:10:29 +0300

    Remove hand cursor from ErrorsLabel
    there is changing background is used hover indication now
    
    GitOrigin-RevId: 65ebd0b94c8d306b27d306020b10115637f1827e

--- platform/platform-impl/src/com/intellij/diagnostic/IdeErrorsIcon.java
+++ platform/platform-impl/src/com/intellij/diagnostic/IdeErrorsIcon.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.diagnostic;
 
 import com.intellij.icons.AllIcons;
@@ -6,7 +6,6 @@ import com.intellij.openapi.wm.StatusBarWidget;
 import com.intellij.ui.AnimatedIcon.Blinking;
 
 import javax.swing.*;
-import java.awt.Cursor;
 
 import static com.intellij.util.ui.EmptyIcon.ICON_16;
 
@@ -22,12 +21,10 @@ class IdeErrorsIcon extends JLabel {
     Icon myUnreadIcon = !myEnableBlink ? AllIcons.Ide.FatalError : new Blinking(AllIcons.Ide.FatalError);
     if (state != null && state != MessagePool.State.NoErrors) {
       setIcon(state == MessagePool.State.ReadErrors ? AllIcons.Ide.FatalError_read : myUnreadIcon);
-      setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
       setToolTipText(DiagnosticBundle.message("error.notification.tooltip"));
     }
     else {
       setIcon(ICON_16);
-      setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
       setToolTipText(null);
     }
   }
