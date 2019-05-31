d08274d19dc321b03fb8873377513ae80a4e3f20
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2019-05-31 16:16:18 +0300

    Raise a warning on inherited `Final`s reassignment on class level (PEP 591) (PY-34945)
    GitOrigin-RevId: ee369c3232878b6a3fa548a0a8ca976febb1a19f

--- python/testData/inspections/PyFinalInspection/ImportedClassFinalReassignment/a.py
+++ python/testData/inspections/PyFinalInspection/ImportedClassFinalReassignment/a.py
@@ -8,3 +8,6 @@ class B(A):
         <warning descr="'a' is 'Final' and could not be reassigned">cls.a</warning> = 6
 
 <warning descr="'a' is 'Final' and could not be reassigned">B.a</warning> = 7
+
+class C(A):
+    <warning descr="'A.a' is 'Final' and could not be reassigned">a</warning> = 8
