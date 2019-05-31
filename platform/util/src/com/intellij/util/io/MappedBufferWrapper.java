575eddff0daa083b18e89afa43c12d4550c41c5c
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-05-31 17:16:32 +0200

    [platform] extracting byte buffer cleaning util
    GitOrigin-RevId: 70347006c79b76a3e16062ca5b42c19080f2b1a0

--- platform/util/src/com/intellij/util/io/MappedBufferWrapper.java
+++ platform/util/src/com/intellij/util/io/MappedBufferWrapper.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.io;
 
 import com.intellij.openapi.diagnostic.Logger;
@@ -42,10 +28,13 @@ public abstract class MappedBufferWrapper extends ByteBufferWrapper {
   @Override
   public final void unmap() {
     long started = IOStatistics.DEBUG ? System.currentTimeMillis() : 0;
-    MappedByteBuffer buffer = myBuffer;
-    myBuffer = null;
-    if (!clean(buffer, isDirty())) {
-      LOG.error("Unmapping failed for: " + myFile);
+
+    if (myBuffer != null) {
+      if (isDirty()) tryForce(myBuffer);
+      if (!ByteBufferUtil.cleanBuffer(myBuffer)) {
+        LOG.error("Unmapping failed for: " + myFile);
+      }
+      myBuffer = null;
     }
 
     if (IOStatistics.DEBUG) {
@@ -68,16 +57,6 @@ public abstract class MappedBufferWrapper extends ByteBufferWrapper {
       myBuffer = buffer = map();
     }
     return buffer;
-  }
-
-  private static boolean clean(final MappedByteBuffer buffer, boolean dirty) {
-    if (buffer == null) return true;
-
-    if (dirty && !tryForce(buffer)) {
-      return false;
-    }
-
-    return DirectBufferWrapper.disposeDirectBuffer(buffer);
   }
 
   private static boolean tryForce(MappedByteBuffer buffer) {
