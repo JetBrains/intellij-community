c4a3c392c016b7cad724471118df7800b6b24dbe
Max Medvedev <maxim.medvedev@jetbrains.com>
2019-06-03 12:39:24 +0300

    IDEA-215441 "Show F1, F2, etc on the Touchbar" option is not shown after it was turned on
    GitOrigin-RevId: 3e1b0d3ee1fd1ca563e3638fd9987f7b34818a34

--- platform/platform-impl/src/com/intellij/ui/mac/touchbar/NST.java
+++ platform/platform-impl/src/com/intellij/ui/mac/touchbar/NST.java
@@ -10,7 +10,6 @@ import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.util.SystemInfoRt;
 import com.intellij.openapi.util.registry.Registry;
 import com.intellij.ui.mac.foundation.ID;
-import com.intellij.ui.mac.foundation.NSDefaults;
 import com.intellij.util.loader.NativeLibraryLoader;
 import com.sun.jna.Memory;
 import com.sun.jna.Native;
@@ -81,9 +80,6 @@ public class NST {
         final String appId = Utils.getAppId();
         if (appId == null || appId.isEmpty()) {
           LOG.debug("can't obtain application id from NSBundle");
-        } else if (NSDefaults.isShowFnKeysEnabled(appId)) {
-          LOG.info("nst library was loaded, but user enabled fn-keys in touchbar");
-          ourNSTLibrary = null;
         }
       }
     } catch (Throwable e) {
