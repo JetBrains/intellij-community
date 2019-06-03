6b6794585aca58e280b04f6fe10684af616dbd8c
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2019-06-03 11:22:45 +0200

    review feedback
    - use getChildTextTrim instead of getChild / getTextTrim
    - use "name" tag value if "id" tag is missing
    
    GitOrigin-RevId: 9f84eba563450dd1a0ef44f8481d164c362ae10e

--- platform/core-impl/src/com/intellij/ide/plugins/IdeaPluginDescriptorImpl.java
+++ platform/core-impl/src/com/intellij/ide/plugins/IdeaPluginDescriptorImpl.java
@@ -139,11 +139,12 @@ public class IdeaPluginDescriptorImpl implements IdeaPluginDescriptor {
       return;
     }
 
-    Element id = element.getChild("id");
-    if (id == null || !PluginManagerCore.disabledPlugins().contains(id.getTextTrim())) {
+    String pluginId = element.getChildTextTrim("id");
+    if (pluginId == null) pluginId = element.getChildTextTrim("name");
+    if (pluginId == null || !PluginManagerCore.disabledPlugins().contains(pluginId)) {
       JDOMXIncluder.resolveNonXIncludeElement(element, url, ignoreMissingInclude, pathResolver);
     } else {
-      if (LOG.isDebugEnabled()) LOG.debug("Skipping resolving of " + id.getTextTrim() + " from " + url);
+      if (LOG.isDebugEnabled()) LOG.debug("Skipping resolving of " + pluginId + " from " + url);
     }
     readExternal(element, stringInterner);
   }
