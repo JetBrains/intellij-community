7aa14d56556962dafc99a45ad6ac132ac0dfa1e6
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2019-06-04 12:05:12 +0700

    Collection.size() is pure now
    GitOrigin-RevId: 38ffe33c688113972126220352035f26aeb189b3

--- java/java-tests/testData/refactoring/inlineMethod/UnusedParameter.java
+++ java/java-tests/testData/refactoring/inlineMethod/UnusedParameter.java
@@ -1,9 +1,12 @@
 
-import java.util.Set;
 import java.util.Map;
 
 class Temp {
-  public Object foo(Set<String> bar) {
+  class Set {
+    native Object size();
+  }
+  
+  public Object foo(Set bar) {
     if (bar.size() < 2)
       return <caret>abortWithCorruptDataError("" + bar.size()); // Inline this
 
