5fe687d55712fcea22e55768c2d546f53065b7a1
Mikhail Mazurkevich <mikhail.mazurkevich@jetbrains.com>
2019-06-04 17:53:52 +0300

    [intellij-sh] IDEA-215398 Add the logic of reusing previously runned terminal
    We searching for preferred process which is in focus now or was previously
    runned and doesn't have running command. If we didn't find it we create
    the new terminal session.
    
    GitOrigin-RevId: 914859473d9f73ba7475c5e25ff3be0279996b8c

--- plugins/sh/src/com/intellij/sh/run/ShTerminalRunner.java
+++ plugins/sh/src/com/intellij/sh/run/ShTerminalRunner.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.sh.run;
 
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.vfs.CharsetToolkit;
@@ -10,55 +11,108 @@ import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowManager;
 import com.intellij.openapi.wm.impl.ToolWindowImpl;
 import com.intellij.sh.psi.ShFile;
+import com.intellij.terminal.JBTerminalWidget;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentManager;
+import com.jediterm.terminal.ProcessTtyConnector;
 import com.pty4j.PtyProcess;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.plugins.terminal.LocalTerminalDirectRunner;
-import org.jetbrains.plugins.terminal.TerminalOptionsProvider;
-import org.jetbrains.plugins.terminal.TerminalToolWindowFactory;
-import org.jetbrains.plugins.terminal.TerminalView;
+import org.jetbrains.plugins.terminal.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 
 import static com.intellij.sh.ShStringUtil.quote;
 
 public class ShTerminalRunner extends ShRunner {
+  private static final Logger LOG = Logger.getInstance(LocalTerminalDirectRunner.class);
+
   @Override
   public void run(@NotNull ShFile file) {
     Project project = file.getProject();
     TerminalView terminalView = TerminalView.getInstance(file.getProject());
     ToolWindow window = ToolWindowManager.getInstance(project).getToolWindow(TerminalToolWindowFactory.TOOL_WINDOW_ID);
-    if (window != null && window.isAvailable()) {
-      ((ToolWindowImpl) window).ensureContentInitialized();
-      window.activate(null);
+    if (window == null) return;
+
+    if (window.isAvailable()) {
+      ((ToolWindowImpl)window).ensureContentInitialized();
     }
-    terminalView.createNewSession(new LocalTerminalDirectRunner(project) {
-      @Override
-      protected PtyProcess createProcess(@Nullable String directory, @Nullable String commandHistoryFilePath) throws ExecutionException {
-        PtyProcess process = super.createProcess(directory, commandHistoryFilePath);
-        Pair<String, String> fileCommand = createCommandLine(file);
-        if (fileCommand.first != null) {
-          try {
-            process.getOutputStream().write(fileCommand.first.getBytes(CharsetToolkit.UTF8_CHARSET));
-          }
-          catch (IOException ex) {
-            throw new ExecutionException("Fail to start " + fileCommand.first, ex);
-          }
-        }
-        else {
-          throw new ExecutionException(fileCommand.second, null);
-        }
-        return process;
+
+    ContentManager contentManager = window.getContentManager();
+    Pair<Content, Process> pair = getSuitableProcess(contentManager);
+    if (pair != null) {
+      try {
+        window.activate(null);
+        contentManager.setSelectedContent(pair.first);
+        runCommand(pair.second, file);
       }
-    });
+      catch (ExecutionException e) {
+        LOG.warn("Error running terminal", e);
+      }
+    }
+    else {
+      terminalView.createNewSession(new LocalTerminalDirectRunner(project) {
+        @Override
+        protected PtyProcess createProcess(@Nullable String directory, @Nullable String commandHistoryFilePath) throws ExecutionException {
+          PtyProcess process = super.createProcess(directory, commandHistoryFilePath);
+          runCommand(process, file);
+          return process;
+        }
+      });
+    }
   }
 
   @Override
   public boolean isAvailable(@NotNull Project project) {
     ToolWindow window = ToolWindowManager.getInstance(project).getToolWindow(TerminalToolWindowFactory.TOOL_WINDOW_ID);
     return window != null && window.isAvailable();
+  }
+
+  @Nullable
+  private static Pair<Content, Process> getSuitableProcess(@NotNull ContentManager contentManager) {
+    Content selectedContent = contentManager.getSelectedContent();
+    if (selectedContent != null) {
+      Pair<Content, Process> pair = getSuitableProcess(selectedContent);
+      if (pair != null) return pair;
+    }
+
+    return Arrays.stream(contentManager.getContents())
+      .map(ShTerminalRunner::getSuitableProcess)
+      .filter(Objects::nonNull)
+      .findFirst()
+      .orElse(null);
+  }
+
+  @Nullable
+  private static Pair<Content, Process> getSuitableProcess(@NotNull Content content) {
+    JBTerminalWidget widget = TerminalView.getWidgetByContent(content);
+    if (widget == null) return null;
+    if (widget.getTtyConnector() instanceof ProcessTtyConnector) {
+      ProcessTtyConnector ttyConnector = (ProcessTtyConnector)widget.getTtyConnector();
+      if (!TerminalUtil.hasRunningCommands(ttyConnector)) {
+        return Pair.create(content, ttyConnector.getProcess());
+      }
+    }
+    return null;
+  }
+
+  private static void runCommand(@NotNull Process process, @NotNull ShFile file) throws ExecutionException {
+    Pair<String, String> fileCommand = createCommandLine(file);
+    if (fileCommand.first != null) {
+      try {
+        process.getOutputStream().write(fileCommand.first.getBytes(CharsetToolkit.UTF8_CHARSET));
+      }
+      catch (IOException ex) {
+        throw new ExecutionException("Fail to start " + fileCommand.first, ex);
+      }
+    }
+    else {
+      throw new ExecutionException(fileCommand.second, null);
+    }
   }
 
   @NotNull
