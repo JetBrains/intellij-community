68e4355150833f4225a73dfab504ba2dfa8164e0
Dmitry Jemerov <yole@jetbrains.com>
2019-06-05 19:00:18 +0200

    Javadoc for FileTypeBean (IDEA-CR-47980)
    GitOrigin-RevId: d410fa72571d2b4f218d1db20362794ffa0e5f3e

--- platform/platform-api/src/com/intellij/openapi/fileTypes/FileTypeBean.java
+++ platform/platform-api/src/com/intellij/openapi/fileTypes/FileTypeBean.java
@@ -2,15 +2,21 @@
 package com.intellij.openapi.fileTypes;
 
 import com.intellij.openapi.extensions.AbstractExtensionPointBean;
+import com.intellij.util.SmartList;
 import com.intellij.util.xmlb.annotations.Attribute;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class FileTypeBean extends AbstractExtensionPointBean {
-  private final List<FileNameMatcher> myExtraMatchers = new ArrayList<>();
+  private final List<FileNameMatcher> myExtraMatchers = new SmartList<>();
 
+  /**
+   * Name of the class implementing the file type (must be a subclass of {@link FileType}). This can be omitted
+   * if the fileType declaration is used to add extensions to an existing file type (in this case, only 'name'
+   * and 'extensions' attributes must be specified).
+   */
   @Attribute("implementationClass")
   public String implementationClass;
 
@@ -20,20 +26,33 @@ public class FileTypeBean extends AbstractExtensionPointBean {
   @Attribute("fieldName")
   public String fieldName;
 
+  /**
+   * Name of the file type. Needs to match the return value of {@link FileType#getName()}.
+   */
   @Attribute("name")
   @NotNull
   public String name;
 
+  /**
+   * Semicolon-separated list of extensions to be associated with the file type. Extensions
+   * must not be prefixed with a `.`.
+   */
   @Attribute("extensions")
   public String extensions;
 
+  /**
+   * For file types that extend {@link LanguageFileType}, this must be set to the ID of the language
+   * returned by {@link LanguageFileType#getLanguage()}.
+   */
   @Attribute("language")
   public String language;
 
+  @ApiStatus.Internal
   public void addMatchers(List<FileNameMatcher> matchers) {
     myExtraMatchers.addAll(matchers);
   }
 
+  @ApiStatus.Internal
   public List<FileNameMatcher> getExtraMatchers() {
     return myExtraMatchers;
   }
