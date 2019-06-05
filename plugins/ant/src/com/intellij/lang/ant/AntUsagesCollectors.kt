a2bbb9550d6123253821040c85abd703f69579ff
Anton Makeev <Anton.Makeev@jetbrains.com>
2019-06-05 16:45:22 +0200

    FUS-407 Build Tools: extract dynamic parts from event id to event data and merge small collector together
    GitOrigin-RevId: 0d365787bb5dfa756b089284f662c46330bcdfb0

--- plugins/ant/src/com/intellij/lang/ant/AntUsagesCollectors.kt
+++ plugins/ant/src/com/intellij/lang/ant/AntUsagesCollectors.kt
@@ -1,11 +1,11 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.lang.ant
 
-import com.intellij.internal.statistic.beans.UsageDescriptor
+import com.intellij.internal.statistic.beans.MetricEvent
+import com.intellij.internal.statistic.beans.newBooleanMetric
 import com.intellij.internal.statistic.collectors.fus.actions.persistence.ActionsCollectorImpl
 import com.intellij.internal.statistic.service.fus.collectors.FUCounterUsageLogger
 import com.intellij.internal.statistic.service.fus.collectors.ProjectUsagesCollector
-import com.intellij.internal.statistic.utils.getBooleanUsage
 import com.intellij.lang.ant.config.AntConfiguration
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
@@ -14,20 +14,20 @@ import com.intellij.openapi.project.Project
 class AntSettingsCollector : ProjectUsagesCollector() {
   override fun getGroupId() = "build.ant.state"
 
-  override fun getUsages(project: Project): Set<UsageDescriptor> {
+  override fun getMetrics(project: Project): Set<MetricEvent> {
     val buildFiles = AntConfiguration.getInstance(project).buildFileList
     if (buildFiles.isEmpty()) return emptySet()
 
-    val usages = mutableSetOf<UsageDescriptor>()
+    val usages = mutableSetOf<MetricEvent>()
 
     // to have a total users base line to calculate pertentages of settings
-    usages.add(getBooleanUsage("hasAntProjects", true))
+    usages.add(newBooleanMetric("hasAntProjects", true))
 
     for (each in buildFiles) {
-      usages.add(getBooleanUsage("isRunInBackground", each.isRunInBackground))
-      usages.add(getBooleanUsage("isColoredOutputMessages", each.isColoredOutputMessages))
-      usages.add(getBooleanUsage("isViewClosedWhenNoErrors", each.isViewClosedWhenNoErrors))
-      usages.add(getBooleanUsage("isCollapseFinishedTargets", each.isCollapseFinishedTargets))
+      usages.add(newBooleanMetric("isRunInBackground", each.isRunInBackground))
+      usages.add(newBooleanMetric("isColoredOutputMessages", each.isColoredOutputMessages))
+      usages.add(newBooleanMetric("isViewClosedWhenNoErrors", each.isViewClosedWhenNoErrors))
+      usages.add(newBooleanMetric("isCollapseFinishedTargets", each.isCollapseFinishedTargets))
     }
 
     return usages
