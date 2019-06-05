b524745152194c3816a35c6ca26da3f50165fb18
Roman.Ivanov <Roman.Ivanov@jetbrains.com>
2019-06-05 14:09:06 +0700

    InlayHints: migrate annotation hints to provider model
    GitOrigin-RevId: 8a0de6f22597fa336c92590ba94199293a341e8a

---
+++ java/java-impl/src/com/intellij/codeInsight/hints/AnnotationInlayProvider.kt
@@ -0,0 +1,167 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.codeInsight.hints
+
+import com.intellij.codeInsight.ExternalAnnotationsManager
+import com.intellij.codeInsight.InferredAnnotationsManager
+import com.intellij.codeInsight.MakeInferredAnnotationExplicit
+import com.intellij.codeInsight.hints.presentation.InsetPresentation
+import com.intellij.codeInsight.hints.presentation.MenuOnClickPresentation
+import com.intellij.codeInsight.javadoc.JavaDocInfoGenerator
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.ApplicationBundle
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.*
+import com.intellij.ui.layout.*
+import javax.swing.JComponent
+import kotlin.reflect.KMutableProperty0
+
+class AnnotationInlayProvider : InlayHintsProvider<AnnotationInlayProvider.Settings> {
+  override fun getCollectorFor(file: PsiFile,
+                               editor: Editor,
+                               settings: Settings,
+                               sink: InlayHintsSink): InlayHintsCollector? {
+    val project = file.project
+    return object : FactoryInlayHintsCollector(editor) {
+      override fun collect(element: PsiElement, editor: Editor, sink: InlayHintsSink): Boolean {
+        if (element is PsiModifierListOwner) {
+          var annotations = emptySequence<PsiAnnotation>()
+          if (settings.showExternal) {
+            annotations += ExternalAnnotationsManager.getInstance(project).findExternalAnnotations(element).orEmpty()
+          }
+          if (settings.showInferred) {
+            annotations += InferredAnnotationsManager.getInstance(project).findInferredAnnotations(element)
+          }
+
+          val shownAnnotations = mutableSetOf<String>()
+          annotations.forEach {
+            val nameReferenceElement = it.nameReferenceElement
+            if (nameReferenceElement != null && element.modifierList != null &&
+                (shownAnnotations.add(nameReferenceElement.qualifiedName) || JavaDocInfoGenerator.isRepeatableAnnotationType(it))) {
+              val offset = element.modifierList!!.textRange.startOffset
+              val presentation = annotationPresentation(it)
+              val menuOnClick = MenuOnClickPresentation(presentation, project) {
+                val makeExplicit = InsertAnnotationAction(project, file, element)
+                listOf(
+                  makeExplicit,
+                  ToggleSettingsAction("Turn off external annotations", settings::showExternal),
+                  ToggleSettingsAction("Turn off inferred annotations", settings::showInferred)
+                )
+              }
+              sink.addInlineElement(offset, true, menuOnClick)
+            }
+          }
+        }
+        return true
+      }
+
+      private fun annotationPresentation(annotation: PsiAnnotation): InsetPresentation = with(factory) {
+        val nameReferenceElement = annotation.nameReferenceElement
+        val parameterList = annotation.parameterList
+        inset(
+          roundWithBackground(seq(
+            smallText("@"),
+            psiSingleReference(smallText(nameReferenceElement?.referenceName ?: "")) { nameReferenceElement?.resolve() },
+            parametersPresentation(parameterList)
+          )),
+          left = 1,
+          right = 1
+        )
+
+      }
+
+      private fun parametersPresentation(parameterList: PsiAnnotationParameterList) = with(factory) {
+        val attributes = parameterList.attributes
+        when {
+          attributes.isEmpty() -> smallText("()")
+          else -> insideParametersPresentation(attributes, collapsed = parameterList.textLength > 30)
+        }
+      }
+
+      private fun insideParametersPresentation(attributes: Array<PsiNameValuePair>, collapsed: Boolean) = with(factory) {
+        collapsible(
+          smallText("("),
+          smallText("..."),
+          {
+            join(
+              presentations = attributes.map { pairPresentation(it) },
+              separator = { smallText(", ") }
+            )
+          },
+          smallText(")"),
+          collapsed
+        )
+      }
+
+      private fun pairPresentation(attribute: PsiNameValuePair) = with(factory) {
+        seq(
+          psiSingleReference(smallText(attribute.name ?: ""), resolve = { attribute.reference?.resolve() }),
+          smallText(" = "),
+          smallText(attribute.value?.text ?: "") // TODO values references, at least annotations and enum names
+        )
+      }
+    }
+  }
+
+  override fun createSettings(): Settings = Settings(showInferred = true, showExternal = true)
+
+  override val name: String
+    get() = "Annotations"
+  override val key: SettingsKey<Settings>
+    get() = ourKey
+  override val previewText: String?
+    get() = "" // TODO
+
+  override fun createConfigurable(settings: Settings): ImmediateConfigurable {
+    return object : ImmediateConfigurable {
+      override fun createComponent(listener: ChangeListener): JComponent {
+        return panel {
+          row {
+            checkBox(ApplicationBundle.message("editor.appearance.show.external.annotations"), settings::showExternal)
+          }
+          row {
+            checkBox(ApplicationBundle.message("editor.appearance.show.external.annotations"), settings::showExternal)
+          }
+        }
+      }
+    }
+  }
+
+  companion object {
+    val ourKey: SettingsKey<Settings> = SettingsKey("annotation.hints")
+  }
+
+  data class Settings(var showInferred: Boolean = true, var showExternal: Boolean = true)
+
+
+  class ToggleSettingsAction(val text: String, val prop: KMutableProperty0<Boolean>) : AnAction() {
+
+    override fun update(e: AnActionEvent) {
+      val presentation = e.presentation
+      presentation.text = text
+    }
+
+    override fun actionPerformed(e: AnActionEvent) {
+      prop.set(!prop.get())
+    }
+
+  }
+}
+
+class InsertAnnotationAction(
+  private val project: Project,
+  private val file: PsiFile,
+  private val element: PsiModifierListOwner
+) : AnAction() {
+  override fun update(e: AnActionEvent) {
+    e.presentation.text = "Insert annotation"
+  }
+
+  override fun actionPerformed(e: AnActionEvent) {
+    val intention = MakeInferredAnnotationExplicit()
+    if (intention.isAvailable(project, file, element)) {
+      intention.makeAnnotationsExplicit(project, file, element)
+    }
+  }
+}
