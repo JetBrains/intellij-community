555a2c2a75f047fb1b1826f9ed7f0ecda90e9722
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-06-05 18:03:10 +0300

    [groovy] use args file command line shortening for Java 9+ in Console (IDEA-215543)
    GitOrigin-RevId: 5cb75db36494f91cb48d02c7be924720557cd9a3

--- plugins/groovy/src/org/jetbrains/plugins/groovy/console/GroovyConsole.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/console/GroovyConsole.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.console;
 
 import com.intellij.execution.ExecutionException;
@@ -21,6 +21,8 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.projectRoots.JdkUtil;
+import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -204,7 +206,22 @@ public class GroovyConsole {
     res.getProgramParametersList().addAll("-p", GroovyScriptRunner.getPathInConf("console.groovy"));
     res.setWorkingDirectory(getWorkingDirectory(module));
     res.setUseDynamicClasspath(true);
+    if (useArgsFile(res)) {
+      res.setArgFile(true);
+    }
     return res;
+  }
+
+  private static boolean useArgsFile(@NotNull JavaParameters res) {
+    Sdk jdk = res.getJdk();
+    if (jdk == null) {
+      return false;
+    }
+    String rootPath = jdk.getHomePath();
+    if (rootPath == null) {
+      return false;
+    }
+    return JdkUtil.isModularRuntime(rootPath);
   }
 
   private static Consumer<Module> restarter(final Project project, final VirtualFile file) {
