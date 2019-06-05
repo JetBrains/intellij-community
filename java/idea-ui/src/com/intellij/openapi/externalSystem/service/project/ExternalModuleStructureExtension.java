b31d346b80f8cb6cb9befa1b2bd50a6153083f49
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2019-06-05 13:03:14 +0300

    IDEA-CR-48232: ES: extract packaging modifiable models to java plugin IDEA-187832
    (cherry picked from commit 67f8209b711806a587d041fd1adc9529ea002916)
    
    GitOrigin-RevId: b1781398c441d99dd937b3c86a1fd60b7854d820

--- java/idea-ui/src/com/intellij/openapi/externalSystem/service/project/ExternalModuleStructureExtension.java
+++ java/idea-ui/src/com/intellij/openapi/externalSystem/service/project/ExternalModuleStructureExtension.java
@@ -1,25 +1,12 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.intellij.openapi.externalSystem.service.project.manage;
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.externalSystem.service.project;
 
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.externalSystem.ExternalSystemManager;
 import com.intellij.openapi.externalSystem.ExternalSystemModulePropertyManager;
 import com.intellij.openapi.externalSystem.model.ProjectSystemId;
+import com.intellij.openapi.externalSystem.service.project.manage.ExternalProjectsManagerImpl;
 import com.intellij.openapi.externalSystem.settings.AbstractExternalSystemSettings;
 import com.intellij.openapi.externalSystem.settings.ExternalProjectSettings;
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil;
@@ -46,7 +33,6 @@ import java.util.Map;
 public class ExternalModuleStructureExtension extends ModuleStructureExtension {
 
   private static final Logger LOG = Logger.getInstance(ExternalModuleStructureExtension.class);
-  @SuppressWarnings("StatefulEp")
   @Nullable
   private Project myProject;
   private boolean isExternalSystemsInvolved;
