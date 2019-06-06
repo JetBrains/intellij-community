249bbd6abec7a19017fdf0e667567592c2082ec9
Anton Lobov <Anton.Lobov@jetbrains.com>
2019-06-06 17:06:33 +0200

    json: migrate file types to fileType extension point, drop obsolete factory
    GitOrigin-RevId: bf7040c4220f3e4f55c0139321183345cc5abe44

--- json/src/com/intellij/json/JsonFileTypeFactory.java
+++ json/src/com/intellij/json/JsonFileTypeFactory.java
@@ -6,7 +6,9 @@ import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Mikhail Golubev
+ * @deprecated use &lt;fileType&gt; extension point instead
  */
+@Deprecated
 public class JsonFileTypeFactory extends FileTypeFactory {
   @Override
   public void createFileTypes(@NotNull FileTypeConsumer consumer) {
