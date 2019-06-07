3ba43376049fa6fa3f21086416771309ba5887cf
Yann Cébron <yann.cebron@jetbrains.com>
2019-06-07 11:40:40 +0200

    ChooseByNameContributor: Javadoc
    GitOrigin-RevId: 74dfce166483976bbef4141b6d482d8eda7a0723

--- platform/lang-api/src/com/intellij/navigation/ChooseByNameContributor.java
+++ platform/lang-api/src/com/intellij/navigation/ChooseByNameContributor.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2019 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.navigation;
 
 import com.intellij.openapi.extensions.ExtensionPointName;
@@ -25,14 +11,14 @@ import org.jetbrains.annotations.NotNull;
  * Consider extending {@link ChooseByNameContributorEx} for better performance.
  *
  * @see com.intellij.navigation.ChooseByNameContributorEx
+ * @see GotoClassContributor
  * @see ChooseByNameRegistry
  */
-
 public interface ChooseByNameContributor {
+
   ExtensionPointName<ChooseByNameContributor> CLASS_EP_NAME = ExtensionPointName.create("com.intellij.gotoClassContributor");
   ExtensionPointName<ChooseByNameContributor> SYMBOL_EP_NAME = ExtensionPointName.create("com.intellij.gotoSymbolContributor");
   ExtensionPointName<ChooseByNameContributor> FILE_EP_NAME = ExtensionPointName.create("com.intellij.gotoFileContributor");
-
 
   /**
    * Returns the list of names for the specified project to which it is possible to navigate
