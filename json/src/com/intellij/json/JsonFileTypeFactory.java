4779389b757c33d8b97393aecb46115108808af1
Anton Lobov <Anton.Lobov@jetbrains.com>
2019-06-07 17:23:38 +0200

    json: temporarily restore file type factory (the change to 'fileType' broke several configs)
    GitOrigin-RevId: 54834237220e6f6939429602bace0b2b896ac6db

--- json/src/com/intellij/json/JsonFileTypeFactory.java
+++ json/src/com/intellij/json/JsonFileTypeFactory.java
@@ -6,9 +6,8 @@ import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Mikhail Golubev
- * @deprecated use &lt;fileType&gt; extension point instead
+ *
  */
-@Deprecated
 public class JsonFileTypeFactory extends FileTypeFactory {
   @Override
   public void createFileTypes(@NotNull FileTypeConsumer consumer) {
