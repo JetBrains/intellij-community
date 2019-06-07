e0ca8818b0cd754fd426dbc8f062759fd67453f1
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2019-06-07 18:12:13 +0300

    PY-33194 Make "New Project..." action in PyCharm available during indexing
    No new project types available in PyCharm require indices for any aspect of
    their operation, so the action "New Project" should be available while indexing
    and all the project types (both PyCharm Community and PyCharm Professional)
    should function properly:
    
        * Pure Python
        * Django
        * Flask
        * Google App Engine
        * Pyramid
        * Web2Py
        * Scientific
        * Angular CLI
        * Angular JS
        * Bootstrap
        * Foundation
        * HTML5 Boilerplate
        * React App
        * React Native
        * Vue.js
    
    GitOrigin-RevId: 3a44aae68e0fa16cabd1de01f30b7d6b907f968f

--- python/ide/src/com/jetbrains/python/newProject/PyCharmNewProjectAction.java
+++ python/ide/src/com/jetbrains/python/newProject/PyCharmNewProjectAction.java
@@ -1,11 +1,11 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.python.newProject;
 
-import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.DumbAwareAction;
 import org.jetbrains.annotations.NotNull;
 
-public class PyCharmNewProjectAction extends AnAction {
+public class PyCharmNewProjectAction extends DumbAwareAction {
 
   @Override
   public void actionPerformed(@NotNull final AnActionEvent e) {
