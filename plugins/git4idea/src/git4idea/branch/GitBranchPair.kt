85b9a10ccd85ce7f76153e54b5d1a99d88c4a418
Kirill Likhodedov <Kirill.Likhodedov@gmail.com>
2019-06-09 11:38:18 +0100

    git: allow the second branch in GitBranchPair to be any kind of GitBranch
    Not necessarily the remote branch. It can be useful for local operations (merging a local branch), and doesn't seem to harm others: no code relies on the fact that it is a remote branch
    
    GitOrigin-RevId: d101b80dd081aa3beaa24d90fba342b6c98e4070

--- plugins/git4idea/src/git4idea/branch/GitBranchPair.kt
+++ plugins/git4idea/src/git4idea/branch/GitBranchPair.kt
@@ -15,10 +15,10 @@
  */
 package git4idea.branch
 
+import git4idea.GitBranch
 import git4idea.GitLocalBranch
-import git4idea.GitRemoteBranch
 
 /**
  * Holder for Git branch and the branch it is "connected" with. It is tracked branch or so called "matched" branch.
  */
-data class GitBranchPair(val source: GitLocalBranch, val target: GitRemoteBranch)
+data class GitBranchPair(val source: GitLocalBranch, val target: GitBranch)
