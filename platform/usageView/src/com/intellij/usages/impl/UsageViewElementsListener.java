184ab6eb08d8c1b7c3f7a7adf0b0ca301ce65a75
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2019-06-10 13:20:22 +0300

    exclude javascript dynamic usages by default
    IDEA-CR-48079
    
    GitOrigin-RevId: e4889d489e6d6680099dc1d213de442909243f8d

---
+++ platform/usageView/src/com/intellij/usages/impl/UsageViewElementsListener.java
@@ -0,0 +1,16 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.usages.impl;
+
+import com.intellij.openapi.extensions.ExtensionPointName;
+import com.intellij.usages.Usage;
+import org.jetbrains.annotations.NotNull;
+
+public interface UsageViewElementsListener {
+  ExtensionPointName<UsageViewElementsListener> EP_NAME = ExtensionPointName.create("com.intellij.usageViewElementsListener");
+
+  default void beforeUsageAdded(@NotNull Usage usage) {}
+
+  default boolean isExcludedByDefault(@NotNull Usage usage) {
+    return false;
+  }
+}
