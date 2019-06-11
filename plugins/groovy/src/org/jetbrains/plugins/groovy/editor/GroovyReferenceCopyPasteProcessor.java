2fdd9938cf21674dcb9a0a395bf61c4edab11568
Anna Kozlova <anna.kozlova@jetbrains.com>
2019-06-11 11:26:11 +0200

    copy paste: add import sby default, add notification & allow to delete when wrong (IDEA-210515)
    GitOrigin-RevId: 2c63c3ce112749a34dd18b176d7b8dd3d81df6b5

--- plugins/groovy/src/org/jetbrains/plugins/groovy/editor/GroovyReferenceCopyPasteProcessor.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/editor/GroovyReferenceCopyPasteProcessor.java
@@ -20,17 +20,20 @@ import com.intellij.codeInsight.editorActions.ReferenceData;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.RangeMarker;
 import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.*;
 import com.intellij.util.IncorrectOperationException;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.groovy.lang.psi.GrReferenceElement;
+import org.jetbrains.plugins.groovy.lang.psi.GroovyFile;
 import org.jetbrains.plugins.groovy.lang.psi.api.GroovyResolveResult;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrReferenceExpression;
 import org.jetbrains.plugins.groovy.lang.psi.api.toplevel.imports.GrImportStatement;
 import org.jetbrains.plugins.groovy.lang.psi.util.PsiUtil;
 
 import java.util.ArrayList;
+import java.util.Set;
 
 /**
  * @author peter
@@ -63,6 +66,16 @@ public class GroovyReferenceCopyPasteProcessor extends CopyPasteReferenceProcess
             }
           }
         }
+      }
+    }
+  }
+
+  @Override
+  protected void removeImports(PsiFile file, Set<String> imports) {
+    GroovyFile groovyFile = (GroovyFile)file;
+    for (GrImportStatement statement : groovyFile.getImportStatements()) {
+      if (imports.contains(statement.getImportedName())) {
+        groovyFile.removeImport(statement);
       }
     }
   }
@@ -117,7 +130,8 @@ public class GroovyReferenceCopyPasteProcessor extends CopyPasteReferenceProcess
 
   @Override
   protected void restoreReferences(ReferenceData[] referenceData,
-                                   GrReferenceElement[] refs) {
+                                   GrReferenceElement[] refs,
+                                   Set<String> imported) {
     for (int i = 0; i < refs.length; i++) {
       GrReferenceElement reference = refs[i];
       if (reference == null) continue;
@@ -128,12 +142,14 @@ public class GroovyReferenceCopyPasteProcessor extends CopyPasteReferenceProcess
         if (refClass != null) {
           if (refData.staticMemberName == null) {
             reference.bindToElement(refClass);
+            imported.add(refData.qClassName);
           }
           else {
             LOG.assertTrue(reference instanceof GrReferenceExpression);
             PsiMember member = findMember(refData, refClass);
             if (member != null) {
               ((GrReferenceExpression)reference).bindToElementViaStaticImport(member);
+              imported.add(StringUtil.getQualifiedName(refData.qClassName, refData.staticMemberName));
             }
           }
         }
