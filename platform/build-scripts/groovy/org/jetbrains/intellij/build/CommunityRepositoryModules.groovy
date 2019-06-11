9f8084cd9eda977d5b2830abf122a1f2a2b3e0fc
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2019-06-11 17:44:03 +0300

    IDEA-CR-48509: android plugin layout fixed: restored instant-run classes and converted libs
    (cherry picked from commit 8c8fe5f749f7682fb023603f475cf48c9c42da77)
    
    GitOrigin-RevId: c8452d979f244ecacf1156f1f3a01f3ed01871c6

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/CommunityRepositoryModules.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/CommunityRepositoryModules.groovy
@@ -282,6 +282,9 @@ class CommunityRepositoryModules {
       withModule("intellij.android.adt.ui.model", "adt-ui.jar")
       withModule("intellij.android.layoutlib", "layoutlib-loader.jar")
 
+      withModule("android.sdktools.instant-run-client", "sdk-tools.jar")
+      withModule("android.sdktools.instant-run-common", "sdk-tools.jar")
+
       withModule("intellij.android.jps", "jps/android-jps-plugin.jar", null)
 
       withProjectLibrary("freemarker") //todo[nik] move to module libraries
@@ -330,6 +333,9 @@ class CommunityRepositoryModules {
       withProjectLibrary("com.android.tools.build:builder-model")
       withProjectLibrary("com.android.tools.build:builder-test-api")
       withProjectLibrary("com.android.tools.pixelprobe:pixelprobe")
+
+      withProjectLibrary("org.jetbrains.intellij.deps.android.tools:perflib")
+      withProjectLibrary("org.jetbrains.intellij.deps.android.tools:layoutInspector")
 
       additionalModulesToJars.entrySet().each {
         withModule(it.key, it.value)
