e2a5c4161ffc65e9fd3314042bfc551c51c47ceb
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-06-12 21:49:17 +0200

    (IDEA-CR-48283) [platform] drops unnecessary reflection from `UrlClassLoader.Builder`
    + cleanup (pointless annotations; typos; formatting)
    
    GitOrigin-RevId: 1eb630e453e53fc8f8dac503de267ca7a92c7232

--- platform/bootstrap/src/com/intellij/ide/BootstrapClassLoaderUtil.java
+++ platform/bootstrap/src/com/intellij/ide/BootstrapClassLoaderUtil.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide;
 
 import com.intellij.idea.Main;
@@ -62,7 +62,7 @@ public class BootstrapClassLoaderUtil extends ClassUtilCore {
       }
     }
 
-    UrlClassLoader.Builder<?> builder = UrlClassLoader.build()
+    UrlClassLoader.Builder builder = UrlClassLoader.build()
       .urls(filterClassPath(new ArrayList<>(classpath)))
       .allowLock()
       .usePersistentClasspathIndexForLocalClassDirectories()
@@ -259,7 +259,7 @@ public class BootstrapClassLoaderUtil extends ClassUtilCore {
   private static class TransformingLoader extends UrlClassLoader {
     private final List<BytecodeTransformer> myTransformers;
 
-    TransformingLoader(@NotNull Builder<?> builder, List<BytecodeTransformer> transformers) {
+    TransformingLoader(@NotNull Builder builder, List<BytecodeTransformer> transformers) {
       super(builder);
       myTransformers = Collections.unmodifiableList(transformers);
     }
