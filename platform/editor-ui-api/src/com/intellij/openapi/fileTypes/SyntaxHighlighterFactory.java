5cdcacc9965f3cc846d95a647f3df9d91bef004c
Yann Cébron <yann.cebron@jetbrains.com>
2019-06-13 15:06:54 +0200

    SyntaxHighlighterFactory: Javadoc
    GitOrigin-RevId: 18c6467ff164112b884cbcc80a0664ef1a500d88

--- platform/editor-ui-api/src/com/intellij/openapi/fileTypes/SyntaxHighlighterFactory.java
+++ platform/editor-ui-api/src/com/intellij/openapi/fileTypes/SyntaxHighlighterFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2019 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,6 +23,7 @@ import org.jetbrains.annotations.Nullable;
 
 /**
  * @author max
+ * @see SingleLazyInstanceSyntaxHighlighterFactory
  */
 public abstract class SyntaxHighlighterFactory {
   public static final SyntaxHighlighterLanguageFactory LANGUAGE_FACTORY = new SyntaxHighlighterLanguageFactory();
