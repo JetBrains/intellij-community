d2cec89f27be5a0a9893176c47a15d0b73815612
Alexey Kudravtsev <cdr@intellij.com>
2019-06-13 18:50:09 +0300

    remove Deprecated on top of Internal to avoid unnecessary noise in legitimate usages
    GitOrigin-RevId: 9db31ce31afe58e614323d3c57071ee94595b6e3

--- platform/core-impl/src/com/intellij/openapi/application/ex/ApplicationEx.java
+++ platform/core-impl/src/com/intellij/openapi/application/ex/ApplicationEx.java
@@ -132,14 +132,12 @@ public interface ApplicationEx extends Application {
   boolean tryRunReadAction(@NotNull Runnable action);
 
   /** DO NOT USE */
-  @Deprecated
   @ApiStatus.Internal
   default void executeByImpatientReader(@NotNull Runnable runnable) throws ApplicationUtil.CannotRunReadActionException {
     runnable.run();
   }
 
   /** DO NOT USE */
-  @Deprecated
   @ApiStatus.Internal
   default boolean isInImpatientReader() {
     return false;
