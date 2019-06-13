96952b1a127a0ce105751f5a6972148e01c871fd
Yann Cébron <yann.cebron@jetbrains.com>
2019-06-13 15:30:07 +0200

    [editor-ui-api] fix deprecation problems
    GitOrigin-RevId: 8378c8ce27b453673250d38793e1ded90413caab

--- platform/editor-ui-api/src/com/intellij/openapi/editor/colors/EditorColorsManager.java
+++ platform/editor-ui-api/src/com/intellij/openapi/editor/colors/EditorColorsManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2019 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,6 +38,9 @@ public abstract class EditorColorsManager {
 
   public abstract void addColorsScheme(@NotNull EditorColorsScheme scheme);
 
+  /**
+   * @deprecated Does nothing, left for API compatibility.
+   */
   @Deprecated
   public abstract void removeAllSchemes();
 
