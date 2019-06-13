6a1c2d709bd97c028aedfe099184b3bea31450a6
Dmitry Jemerov <yole@jetbrains.com>
2019-06-13 16:43:27 +0200

    Support collapsing actions in notifications in the other direction
    GitOrigin-RevId: 90fc1b4970f198ccea2f2f60aa93cb0f13357fc7

--- platform/platform-api/src/com/intellij/notification/Notification.java
+++ platform/platform-api/src/com/intellij/notification/Notification.java
@@ -39,6 +39,12 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * @author Alexander Lobas
  */
 public class Notification {
+  /**
+   * Which actions to keep and which to show under the "Actions" dropdown link if actions do not fit horizontally
+   * into the width of the notification.
+   */
+  public enum CollapseActionsDirection { KEEP_LEFTMOST, KEEP_RIGHTMOST }
+
   private static final Logger LOG = Logger.getInstance("#com.intellij.notification.Notification");
   private static final DataKey<Notification> KEY = DataKey.create("Notification");
 
@@ -54,6 +60,7 @@ public class Notification {
   private NotificationListener myListener;
   private String myDropDownText;
   private List<AnAction> myActions;
+  private CollapseActionsDirection myCollapseActionsDirection = CollapseActionsDirection.KEEP_RIGHTMOST;
   private AnAction myContextHelpAction;
 
   private final AtomicBoolean myExpired = new AtomicBoolean(false);
@@ -249,6 +256,14 @@ public class Notification {
   public Notification setDropDownText(@NotNull String dropDownText) {
     myDropDownText = dropDownText;
     return this;
+  }
+
+  public CollapseActionsDirection getCollapseActionsDirection() {
+    return myCollapseActionsDirection;
+  }
+
+  public void setCollapseActionsDirection(CollapseActionsDirection collapseActionsDirection) {
+    myCollapseActionsDirection = collapseActionsDirection;
   }
 
   @NotNull
