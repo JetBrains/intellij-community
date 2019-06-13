0a62c9b8c2f7cc8c728a021dab8e52b90d68a2f4
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2019-06-13 18:36:09 +0300

    vcs: Refactor "ChangeNodeDecorator" - @NotNull, fix warnings
    GitOrigin-RevId: b77d29414d03ef9f6976157750da2fd6b605f9d5

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/patch/ApplyPatchDifferentiatedDialog.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/patch/ApplyPatchDifferentiatedDialog.java
@@ -957,7 +957,7 @@ public class ApplyPatchDifferentiatedDialog extends DialogWrapper {
 
   private class MyChangeNodeDecorator implements ChangeNodeDecorator {
     @Override
-    public void decorate(Change change, SimpleColoredComponent component, boolean isShowFlatten) {
+    public void decorate(@NotNull Change change, @NotNull SimpleColoredComponent component, boolean isShowFlatten) {
       if (change instanceof AbstractFilePatchInProgress.PatchChange) {
         final AbstractFilePatchInProgress.PatchChange patchChange = (AbstractFilePatchInProgress.PatchChange)change;
         final AbstractFilePatchInProgress patchInProgress = patchChange.getPatchInProgress();
@@ -995,7 +995,7 @@ public class ApplyPatchDifferentiatedDialog extends DialogWrapper {
     }
 
     @Override
-    public void preDecorate(Change change, ChangesBrowserNodeRenderer renderer, boolean showFlatten) {
+    public void preDecorate(@NotNull Change change, @NotNull ChangesBrowserNodeRenderer renderer, boolean showFlatten) {
     }
   }
 
