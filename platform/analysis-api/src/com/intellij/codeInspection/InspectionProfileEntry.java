14ad9a451d19163adbd14505f1326b64c68119af
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2019-06-14 11:08:41 +0200

    added javadoc with @deprecated tag / description
    GitOrigin-RevId: 87032baa0294db69dc74ef9a95c1519c828eb1ab

--- platform/analysis-api/src/com/intellij/codeInspection/InspectionProfileEntry.java
+++ platform/analysis-api/src/com/intellij/codeInspection/InspectionProfileEntry.java
@@ -409,6 +409,7 @@ public abstract class InspectionProfileEntry implements BatchSuppressableTool {
     return null;
   }
 
+  /** @deprecated This method is no longer internally used */
   @Deprecated
   @ApiStatus.ScheduledForRemoval(inVersion = "2020.1")
   @Nullable
