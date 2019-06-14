5c1ead6cc907abf8d4825cdcddf58a3e6910bcc8
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-06-14 17:05:03 +0300

    deprecate and schedule for removal unused com.intellij.psi.PsiReferenceService#HINTS
    GitOrigin-RevId: c1bb6f34248a8f1c1dc37230678e025cc941e843

--- platform/core-api/src/com/intellij/psi/PsiReferenceService.java
+++ platform/core-api/src/com/intellij/psi/PsiReferenceService.java
@@ -1,22 +1,9 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi;
 
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.util.Key;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -27,6 +14,8 @@ import java.util.List;
  */
 public abstract class PsiReferenceService {
 
+  @ApiStatus.ScheduledForRemoval(inVersion = "2020.1")
+  @Deprecated
   public static final Key<Hints> HINTS = Key.create("HINTS");
 
   public static PsiReferenceService getService() {
