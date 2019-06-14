8d02ebabb6efe2629e55defff22657f633f9f478
Yann Cébron <yann.cebron@jetbrains.com>
2019-06-14 13:43:22 +0200

    InspectionToolProvider: Javadoc
    GitOrigin-RevId: 98cdbc5219602690a9b3c18776f4886b80981919

--- platform/analysis-api/src/com/intellij/codeInspection/InspectionToolProvider.java
+++ platform/analysis-api/src/com/intellij/codeInspection/InspectionToolProvider.java
@@ -6,12 +6,16 @@ import org.jetbrains.annotations.NotNull;
 
 /**
  * Extension that implements this interface will be automatically queried for inspection tool classes.
+ * <p>
+ * In most cases, one will use {@link LocalInspectionEP#LOCAL_INSPECTION} and {@link InspectionEP#GLOBAL_INSPECTION} extension points for
+ * direct registration of local/global inspections.
  */
 public interface InspectionToolProvider {
   ExtensionPointName<InspectionToolProvider> EXTENSION_POINT_NAME = ExtensionPointName.create("com.intellij.inspectionToolProvider");
 
   /**
    * Query method for inspection tools provided by a plugin.
+   *
    * @return classes that extend {@link InspectionProfileEntry}
    */
   @NotNull
