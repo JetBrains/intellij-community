fdca78d6f3e58f7bceebb60a935267f8b25f9f8c
Yann Cébron <yann.cebron@jetbrains.com>
2019-06-14 17:24:55 +0200

    [indexing-api] fix deprecation problems
    (cherry picked from commit 67980a6d6dc55fbad9c307b9c55958b1934702ff) IDEA-CR-48448
    
    GitOrigin-RevId: ef00189e682a117a458378d99e98cc8597728138

--- platform/indexing-api/src/com/intellij/psi/search/searches/ReferencesSearch.java
+++ platform/indexing-api/src/com/intellij/psi/search/searches/ReferencesSearch.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.search.searches;
 
 import com.intellij.openapi.application.DumbAwareSearchParameters;
@@ -74,7 +74,7 @@ public class ReferencesSearch extends ExtensibleQueryFactory<PsiReference, Refer
 
 
     /**
-     * Same as {@link #getScopeDeterminedByUser()}. Use {@link #getEffectiveSearchScope} instead
+     * @deprecated Same as {@link #getScopeDeterminedByUser()}, use {@link #getEffectiveSearchScope} instead
      */
     @Deprecated
     @NotNull
