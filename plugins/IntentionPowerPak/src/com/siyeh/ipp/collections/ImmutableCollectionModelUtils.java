eb84f5f9b16c2c47d9bcb0caa816316e82ce899a
Artemiy Sartakov <Artemiy.Sartakov@jetbrains.com>
2019-06-14 17:12:27 +0700

    ReplaceWithMutableCollectionIntention: fixed variable declaration in case when original call invalidated (IDEA-CR-46450)
    GitOrigin-RevId: 8d6cfcc522bd9de0faba1d3f2552bccb7a23f0d3

--- plugins/IntentionPowerPak/src/com/siyeh/ipp/collections/ImmutableCollectionModelUtils.java
+++ plugins/IntentionPowerPak/src/com/siyeh/ipp/collections/ImmutableCollectionModelUtils.java
@@ -195,7 +195,7 @@ class ImmutableCollectionModelUtils {
       String[] names = getNameSuggestions(call, type);
       if (names.length == 0) return;
       String name = names[0];
-      PsiDeclarationStatement declaration = createDeclaration(name, type, model, statement);
+      PsiDeclarationStatement declaration = createDeclaration(name, type, call, model, statement);
       if (declaration == null) return;
       PsiVariable declaredVariable = (PsiVariable)declaration.getDeclaredElements()[0];
       PsiElement anchor = addUpdates(name, model, declaration);
@@ -211,6 +211,7 @@ class ImmutableCollectionModelUtils {
     @Nullable
     private PsiDeclarationStatement createDeclaration(@NotNull String name,
                                                       @NotNull PsiType type,
+                                                      @NotNull PsiMethodCallExpression call,
                                                       @NotNull ImmutableCollectionModel model,
                                                       @NotNull PsiStatement usage) {
       String initializerText = model.myType.getInitializerText(model.myIsVarArgCall ? null : model.myCall.getText());
@@ -218,7 +219,7 @@ class ImmutableCollectionModelUtils {
       PsiType rhsType = initializer.getType();
       if (rhsType == null) return null;
       if (!TypeUtils.areConvertible(type, rhsType)) {
-        type = ExpectedTypeUtils.findExpectedType(model.myCall, false);
+        type = ExpectedTypeUtils.findExpectedType(call, false);
       }
       if (type == null) return null;
       PsiDeclarationStatement declaration = myElementFactory.createVariableDeclarationStatement(name, type, initializer);
