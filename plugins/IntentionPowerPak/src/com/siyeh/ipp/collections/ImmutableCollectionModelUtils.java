58a22f282e1526758069e7912b9bc75bc7d3cb33
Artemiy Sartakov <Artemiy.Sartakov@jetbrains.com>
2019-06-17 10:37:34 +0700

    ReplaceWithMutableCollectionIntention: update method call in model after invalidation (IDEA-CR-46450)
    GitOrigin-RevId: 97809fb06f5aa81df8cd47e323cae247fb435d09

--- plugins/IntentionPowerPak/src/com/siyeh/ipp/collections/ImmutableCollectionModelUtils.java
+++ plugins/IntentionPowerPak/src/com/siyeh/ipp/collections/ImmutableCollectionModelUtils.java
@@ -175,27 +175,27 @@ class ImmutableCollectionModelUtils {
       PsiStatement statement = ObjectUtils.tryCast(RefactoringUtil.getParentStatement(call, false), PsiStatement.class);
       if (statement == null) return;
 
+      model.myCall = call;
       String assignedVariable = model.myAssignedVariable;
       if (assignedVariable != null) {
-        String initializerText = model.myType.getInitializerText(model.myIsVarArgCall ? null : model.myCall.getText());
+        String initializerText = model.myType.getInitializerText(model.myIsVarArgCall ? null : call.getText());
         PsiReplacementUtil.replaceExpressionAndShorten(call, initializerText, new CommentTracker());
         PsiElement anchor = addUpdates(assignedVariable, model, statement);
         if (myEditor != null) myEditor.getCaretModel().moveToOffset(anchor.getTextRange().getEndOffset());
       }
       else {
-        createVariable(call, statement, model);
+        createVariable(statement, model);
       }
     }
 
-    private void createVariable(@NotNull PsiMethodCallExpression call,
-                                @NotNull PsiStatement statement,
-                                @NotNull ImmutableCollectionModel model) {
+    private void createVariable(@NotNull PsiStatement statement, @NotNull ImmutableCollectionModel model) {
+      PsiMethodCallExpression call = model.myCall;
       PsiType type = call.getType();
       if (type == null) return;
       String[] names = getNameSuggestions(call, type);
       if (names.length == 0) return;
       String name = names[0];
-      PsiDeclarationStatement declaration = createDeclaration(name, type, call, model, statement);
+      PsiDeclarationStatement declaration = createDeclaration(name, type, model, statement);
       if (declaration == null) return;
       PsiVariable declaredVariable = (PsiVariable)declaration.getDeclaredElements()[0];
       PsiElement anchor = addUpdates(name, model, declaration);
@@ -211,7 +211,6 @@ class ImmutableCollectionModelUtils {
     @Nullable
     private PsiDeclarationStatement createDeclaration(@NotNull String name,
                                                       @NotNull PsiType type,
-                                                      @NotNull PsiMethodCallExpression call,
                                                       @NotNull ImmutableCollectionModel model,
                                                       @NotNull PsiStatement usage) {
       String initializerText = model.myType.getInitializerText(model.myIsVarArgCall ? null : model.myCall.getText());
@@ -219,7 +218,7 @@ class ImmutableCollectionModelUtils {
       PsiType rhsType = initializer.getType();
       if (rhsType == null) return null;
       if (!TypeUtils.areConvertible(type, rhsType)) {
-        type = ExpectedTypeUtils.findExpectedType(call, false);
+        type = ExpectedTypeUtils.findExpectedType(model.myCall, false);
       }
       if (type == null) return null;
       PsiDeclarationStatement declaration = myElementFactory.createVariableDeclarationStatement(name, type, initializer);
@@ -296,7 +295,7 @@ class ImmutableCollectionModelUtils {
    */
   static class ImmutableCollectionModel {
 
-    private final PsiMethodCallExpression myCall;
+    private PsiMethodCallExpression myCall;
     private final CollectionType myType;
     private final boolean myIsVarArgCall;
     private final PsiExpression[] myArgs;
