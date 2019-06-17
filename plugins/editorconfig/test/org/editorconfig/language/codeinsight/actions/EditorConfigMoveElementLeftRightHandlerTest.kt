1add695580a76741de5bc049b3ce2f32305dcc52
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2019-06-17 18:00:28 +0300

    Remove dependency on Java test framework
    GitOrigin-RevId: 92b6c1e566fb9c55db80d8beb5cf09d508163633

--- plugins/editorconfig/test/org/editorconfig/language/codeinsight/actions/EditorConfigMoveElementLeftRightHandlerTest.kt
+++ plugins/editorconfig/test/org/editorconfig/language/codeinsight/actions/EditorConfigMoveElementLeftRightHandlerTest.kt
@@ -3,11 +3,13 @@ package org.editorconfig.language.codeinsight.actions
 
 import com.intellij.openapi.actionSystem.IdeActions
 import com.intellij.openapi.fileEditor.FileDocumentManager
-import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
 
-class EditorConfigMoveElementLeftRightHandlerTest : LightJavaCodeInsightFixtureTestCase() {
+class EditorConfigMoveElementLeftRightHandlerTest : BasePlatformTestCase() {
   override fun getBasePath() =
     "/plugins/editorconfig/testData/org/editorconfig/language/codeinsight/actions/moveLeftRight/"
+
+  override fun isCommunity(): Boolean = true
 
   fun testMoveCentralValue() = doTest()
   fun testMoveFirstValue() = doTest()
