1de4fe6654c48e3de11e04d831d922760649a88e
Bas Leijdekkers <basleijdekkers@gmail.com>
2019-06-17 15:20:57 +0200

    SSR: Auto-insert pair $ when editing search/replace template (IDEA-216174)
    GitOrigin-RevId: 86c744363eb5a4ab7f6c5c228428448ad5c92073

--- platform/structuralsearch/source/com/intellij/structuralsearch/plugin/ui/StructuralSearchTypedHandler.java
+++ platform/structuralsearch/source/com/intellij/structuralsearch/plugin/ui/StructuralSearchTypedHandler.java
@@ -1,8 +1,10 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.structuralsearch.plugin.ui;
 
+import com.intellij.codeInsight.CodeInsightSettings;
 import com.intellij.codeInsight.editorActions.TypedHandlerDelegate;
 import com.intellij.openapi.editor.*;
+import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiFile;
@@ -30,7 +32,10 @@ public class StructuralSearchTypedHandler extends TypedHandlerDelegate {
     if (c == '$') {
       final SelectionModel selectionModel = editor.getSelectionModel();
       final String selectedText = selectionModel.getSelectedText();
-      if (!StringUtil.isEmpty(selectedText) && !selectedText.contains("$")) {
+      if (!StringUtil.isEmpty(selectedText)) {
+        if (selectedText.contains("$") || CodeInsightSettings.getInstance().SURROUND_SELECTION_ON_QUOTE_TYPED) {
+          return Result.CONTINUE;
+        }
         final Document document = editor.getDocument();
         final List<RangeMarker> rangeMarkers = new SmartList<>();
         rangeMarkers.add(document.createRangeMarker(selectionModel.getSelectionStart(), selectionModel.getSelectionEnd()));
@@ -76,5 +81,40 @@ public class StructuralSearchTypedHandler extends TypedHandlerDelegate {
       }
     }
     return super.beforeSelectionRemoved(c, project, editor, file);
+  }
+
+  @NotNull
+  @Override
+  public Result beforeCharTyped(char c,
+                                @NotNull Project project,
+                                @NotNull Editor editor,
+                                @NotNull PsiFile file,
+                                @NotNull FileType fileType) {
+    if (editor.getUserData(SubstitutionShortInfoHandler.CURRENT_CONFIGURATION_KEY) == null) {
+      return Result.CONTINUE;
+    }
+    if (c == '$' && CodeInsightSettings.getInstance().AUTOINSERT_PAIR_BRACKET) {
+      final Document document = editor.getDocument();
+      final CaretModel caretModel = editor.getCaretModel();
+      final Caret caret = caretModel.getCurrentCaret();
+      final LogicalPosition position = caret.getLogicalPosition();
+      final int lineStart = document.getLineStartOffset(position.line);
+      final CharSequence text = document.getCharsSequence();
+      final int index = lineStart + position.column;
+      if (index < text.length() && text.charAt(index) == '$') {
+        caret.setSelection(index, index + 1);
+      }
+      final CharSequence line = text.subSequence(lineStart, lineStart + position.column);
+      boolean $ = false;
+      for (int i = 0, max = line.length(); i < max; i++) {
+        if (line.charAt(i) == '$') {
+          $ = !$;
+        }
+      }
+      if (!$) {
+        document.insertString(lineStart + position.column, "$");
+      }
+    }
+    return Result.CONTINUE;
   }
 }
