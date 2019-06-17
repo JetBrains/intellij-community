a176325c78c0694b8390d60c7730eb3aa5752065
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2019-06-17 19:38:20 +0300

    signapp.sh: fix shellcheck inspections
    (cherry picked from commit e077f25bc0ceb2139b6f15714669e82731ce1525)
    
    GitOrigin-RevId: d6424400d19ae9666bd87e0e5ce6a34d4b4d509c

--- platform/build-scripts/tools/mac/scripts/signapp.sh
+++ platform/build-scripts/tools/mac/scripts/signapp.sh
@@ -14,93 +14,98 @@ CODESIGN_STRING=$5
 HELP_DIR_NAME=$6
 NOTARIZE=$7
 
-cd $(dirname $0)
+cd "$(dirname "$0")"
 
 function log() {
   echo "$(date '+[%H:%M:%S]') $*"
 }
 
-log "Deleting ${EXPLODED}..."
-test -d ${EXPLODED} && chmod -R u+wx ${EXPLODED}/*
-rm -rf ${EXPLODED}
-mkdir ${EXPLODED}
+log "Deleting $EXPLODED ..."
+if test -d "$EXPLODED"; then
+  find "$EXPLODED" -mindepth 1 -maxdepth 1 -exec chmod -R u+wx '{}' \;
+fi
+rm -rf "$EXPLODED"
+mkdir "$EXPLODED"
 
-log "Unzipping ${INPUT_FILE} to ${EXPLODED}..."
-unzip -q ${INPUT_FILE} -d ${EXPLODED}/
-rm ${INPUT_FILE}
-BUILD_NAME=$(ls ${EXPLODED}/)
+log "Unzipping $INPUT_FILE to $EXPLODED ..."
+unzip -q "$INPUT_FILE" -d "$EXPLODED"
+rm "$INPUT_FILE"
+BUILD_NAME="$(ls "$EXPLODED")"
+log "$INPUT_FILE unzipped and removed"
 
-if [ $# -eq 8 ] && [ -f $8 ]; then
+if [ $# -eq 8 ] && [ -f "$8" ]; then
   archiveJDK="$8"
+  log "Preparing jdk $archiveJDK..."
   log "Modifying Info.plist"
-  sed -i -e 's/1.6\*/1.6\+/' ${EXPLODED}/"$BUILD_NAME"/Contents/Info.plist
-  jdk=jdk-bundled
+  sed -i -e 's/1.6\*/1.6\+/' "$EXPLODED/$BUILD_NAME/Contents/Info.plist"
+  jdk="jdk-bundled"
   if [[ $1 == *custom-jdk-bundled* ]]; then
-    jdk=custom-"$jdk"
+    jdk="custom-$jdk"
   fi
-  rm -f ${EXPLODED}/"$BUILD_NAME"/Contents/Info.plist-e
+  rm -f "$EXPLODED/$BUILD_NAME/Contents/Info.plist-e"
   log "Info.plist has been modified"
-  log "Copying JDK: $archiveJDK to ${EXPLODED}/"$BUILD_NAME"/Contents"
-  tar xvf $archiveJDK -C ${EXPLODED}/"$BUILD_NAME"/Contents --exclude='._jdk'
-  chmod -R u+w ${EXPLODED}/"$BUILD_NAME"/Contents/*
+  log "Copying JDK: $archiveJDK to $EXPLODED/$BUILD_NAME/Contents"
+  tar xvf "$archiveJDK" -C "$EXPLODED/$BUILD_NAME/Contents" --exclude='._jdk'
+  find "$EXPLODED/$BUILD_NAME/Contents/" -mindepth 1 -maxdepth 1 -exec chmod -R u+w '{}' \;
   log "JDK has been copied"
-  rm -f $archiveJDK
+  rm -f "$archiveJDK"
 fi
 
-if [ $HELP_DIR_NAME != "no-help" ]; then
-  HELP_DIR=${EXPLODED}/"$BUILD_NAME"/Contents/Resources/"$HELP_DIR_NAME"/Contents/Resources/English.lproj/
+if [ "$HELP_DIR_NAME" != "no-help" ]; then
+  HELP_DIR="$EXPLODED/$BUILD_NAME/Contents/Resources/$HELP_DIR_NAME/Contents/Resources/English.lproj/"
   log "Building help indices for $HELP_DIR"
   hiutil -Cagvf "$HELP_DIR/search.helpindex" "$HELP_DIR"
 fi
 
-#enable nullglob option to ensure that 'for' cycles don't iterate if nothing matches to the file pattern
-shopt -s nullglob
+find "$EXPLODED/$BUILD_NAME/Contents/bin" \
+  -maxdepth 1 -type f -name '*.jnilib' -print0 |
+  while IFS= read -r -d $'\0' file; do
+    if [ -f "$file" ]; then
+      log "Linking $file"
+      b="$(basename "$file" .jnilib)"
+      ln -sf "$b.jnilib" "$(dirname "$file")/$b.dylib"
+    fi
+  done
 
-for f in "${EXPLODED}/$BUILD_NAME"/Contents/bin/*.jnilib ; do
-  if [ -f "$f" ]; then
-    b="$(basename "$f" .jnilib)"
-    ln -sf "$b.jnilib" "$(dirname "$f")/$b.dylib"
-  fi
-done
+find "$EXPLODED/$BUILD_NAME/Contents/" \
+  -maxdepth 1 -type f -name '*.txt' -print0 |
+  while IFS= read -r -d $'\0' file; do
+    if [ -f "$file" ]; then
+      log "Moving $file"
+      mv "$file" "$EXPLODED/$BUILD_NAME/Contents/Resources"
+    fi
+  done
 
-for f in "${EXPLODED}/$BUILD_NAME"/Contents/*.txt ; do
-  if [ -f "$f" ]; then
-    log "Moving $f"
-    mv "$f" ${EXPLODED}/"$BUILD_NAME"/Contents/Resources
-  fi
-done
-
-for f in "${EXPLODED}/$BUILD_NAME"/Contents/* ; do
-  if [ -f "$f" ] && [ $(basename -- "$f") != "Info.plist" ] ; then
-    log "Only Info.plist file is allowed in Contents directory but $f is found"
-    exit 1
-  fi
-done
-shopt -u nullglob
+non_plist=$(find "$EXPLODED/$BUILD_NAME/Contents/" -maxdepth 1 -type f -and -not -name 'Info.plist' | wc -l)
+if [[ $non_plist -gt 0 ]]; then
+  log "Only Info.plist file is allowed in Contents directory but found $non_plist file(s):"
+  log "$(find "$EXPLODED/$BUILD_NAME/Contents/" -maxdepth 1 -type f -and -not -name 'Info.plist')"
+  exit 1
+fi
 
 log "Unlocking keychain..."
 # Make sure *.p12 is imported into local KeyChain
-security unlock-keychain -p "${PASSWORD}" "/Users/${USERNAME}/Library/Keychains/login.keychain"
+security unlock-keychain -p "$PASSWORD" "/Users/$USERNAME/Library/Keychains/login.keychain"
 
 attempt=1
 limit=3
 set +e
-while [ $attempt -le $limit ]
-do
-  log "Signing (attempt $attempt) ${EXPLODED}/$BUILD_NAME ..."
-  ./sign.sh "${EXPLODED}/$BUILD_NAME" "$CODESIGN_STRING"
-  if [ "$?" != "0" ]; then
-    let "attempt += 1"
+while [[ $attempt -le $limit ]]; do
+  log "Signing (attempt $attempt) $EXPLODED/$BUILD_NAME ..."
+  ./sign.sh "$EXPLODED/$BUILD_NAME" "$CODESIGN_STRING"
+  ec=$?
+  if [[ $ec -ne 0 ]]; then
+    ((attempt += 1))
     if [ $attempt -eq $limit ]; then
       set -e
     fi
     log "Signing failed, wait for 30 sec and try to sign again"
-    sleep 30;
+    sleep 30
   else
     log "Signing done"
-    codesign -v ${EXPLODED}/"$BUILD_NAME" -vvvvv
+    codesign -v "$EXPLODED/$BUILD_NAME" -vvvvv
     log "Check sign done"
-    let "attempt += $limit"
+    ((attempt += limit))
   fi
 done
 
@@ -108,20 +113,22 @@ set -e
 
 if [ "$NOTARIZE" = "yes" ]; then
   log "Notarizing..."
+  # shellcheck disable=SC1090
   source "$HOME/.notarize_token"
-  ./notarize.sh "${EXPLODED}/$BUILD_NAME" "$APPLE_USERNAME" "$APPLE_PASSWORD"
+  ./notarize.sh "$EXPLODED/$BUILD_NAME" "$APPLE_USERNAME" "$APPLE_PASSWORD"
 
   log "Stapling..."
-  xcrun stapler staple "${EXPLODED}/$BUILD_NAME"
+  xcrun stapler staple "$EXPLODED/$BUILD_NAME"
 else
   log "Notarization disabled"
   log "Stapling disabled"
 fi
 
-log "Zipping ${BUILD_NAME} to ${INPUT_FILE}..."
-cd ${EXPLODED}
-ditto -c -k --sequesterRsrc --keepParent "${BUILD_NAME}" ../${INPUT_FILE}
-log "Finished zipping"
-cd ..
-rm -rf ${EXPLODED}
+log "Zipping $BUILD_NAME to $INPUT_FILE ..."
+(
+  cd "$EXPLODED"
+  ditto -c -k --sequesterRsrc --keepParent "$BUILD_NAME" "../$INPUT_FILE"
+  log "Finished zipping"
+)
+rm -rf "$EXPLODED"
 log "Done"
