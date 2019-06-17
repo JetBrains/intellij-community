a117e7858e94e0e6cd089bf98c86a5f353bd2db0
Dmitry Jemerov <yole@jetbrains.com>
2019-06-17 12:56:11 +0200

    Validator for gutter_icon (FUS-421)
    GitOrigin-RevId: ced994e2fc1c2ccaa6f6503e1fe3ece1b6e0637d

--- platform/platform-impl/src/com/intellij/internal/statistic/collectors/fus/PluginInfoWhiteListRule.kt
+++ platform/platform-impl/src/com/intellij/internal/statistic/collectors/fus/PluginInfoWhiteListRule.kt
@@ -5,8 +5,9 @@ import com.intellij.internal.statistic.eventLog.validator.ValidationResultType
 import com.intellij.internal.statistic.eventLog.validator.rules.EventContext
 import com.intellij.internal.statistic.eventLog.validator.rules.impl.CustomWhiteListRule
 
-class ProjectTypeWhiteListRule : CustomWhiteListRule() {
-  override fun acceptRuleId(ruleId: String?) = ruleId == "project_type" || ruleId == "framework"
+class PluginInfoWhiteListRule : CustomWhiteListRule() {
+  override fun acceptRuleId(ruleId: String?) =
+    ruleId == "project_type" || ruleId == "framework" || ruleId == "gutter_icon"
 
   override fun doValidate(data: String, context: EventContext): ValidationResultType {
     return acceptWhenReportedByPluginFromPluginRepository(context)
