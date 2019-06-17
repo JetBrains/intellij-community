d70797d4306332578e80adfa639a484d7427835a
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2019-06-17 17:26:58 +0300

    Improve logging in macOS shell scripts
    GitOrigin-RevId: 87e8ea1be4e04208721a7af01df59d275c32cd90

--- platform/build-scripts/tools/mac/scripts/makedmg.sh
+++ platform/build-scripts/tools/mac/scripts/makedmg.sh
@@ -8,13 +8,17 @@ cd $(dirname $0)
 
 EXPLODED=$2.exploded
 
+function log() {
+  echo "$(date '+[%H:%M:%S]') $*"
+}
+
 test -d ${EXPLODED} && chmod -R u+wx ${EXPLODED}/*
 rm -rf ${EXPLODED}
 rm -f $1.dmg
 rm -f pack.temp.dmg
 
 mkdir ${EXPLODED}
-echo "Unzipping $1.sit to ${EXPLODED}..."
+log "Unzipping $1.sit to ${EXPLODED}..."
 ditto -x -k $1.sit ${EXPLODED}/
 
 rm $1.sit
@@ -33,7 +37,7 @@ ln -s /Applications ${EXPLODED}/"Applications"
 dd if=/dev/zero of=${EXPLODED}/DSStorePlaceHolder bs=1024 count=512
 stat ${EXPLODED}/DSStorePlaceHolder
 
-echo "Creating unpacked r/w disk image ${VOLNAME}..."
+log "Creating unpacked r/w disk image ${VOLNAME}..."
 hdiutil create -srcfolder ./${EXPLODED} -volname "$VOLNAME" -anyowners -nospotlight -quiet -fs HFS+ -fsargs "-c c=64,a=16,e=16" -format UDRW $2.temp.dmg
 
 # check if the image already mounted
@@ -42,12 +46,12 @@ if [ -d "/Volumes/$1" ]; then
   limit=5
   while [ $attempt -le $limit ]
   do
-    echo "/Volumes/$1 - the image is already mounted.  This build will wait for unmount for 1 min (up to 5 times)."
+    log "/Volumes/$1 - the image is already mounted.  This build will wait for unmount for 1 min (up to 5 times)."
     sleep 60;
     let "attempt += 1"
     if [ -d "/Volumes/$1" ]; then
       if [ $attempt -ge $limit ]; then
-        echo "/Volumes/$1 - the image is still mounted. By the reason the build will be stopped."
+        log "/Volumes/$1 - the image is still mounted. By the reason the build will be stopped."
         rm -rf ${EXPLODED}
         rm -f $2.temp.dmg
         exit 1
@@ -57,14 +61,14 @@ if [ -d "/Volumes/$1" ]; then
 fi
 
 # mount this image
-echo "Mounting unpacked r/w disk image..."
+log "Mounting unpacked r/w disk image..."
 device=$(hdiutil attach -readwrite -noverify -mountpoint /Volumes/"$1" -noautoopen $2.temp.dmg | egrep '^/dev/' | sed 1q | awk '{print $1.dmg}')
-echo "Mounted as ${device}."
+log "Mounted as ${device}."
 sleep 10
 find /Volumes/"$1" -maxdepth 1
 
 # set properties
-echo "Updating $VOLNAME disk image styles..."
+log "Updating $VOLNAME disk image styles..."
 stat /Volumes/"$1"/DSStorePlaceHolder || true
 rm /Volumes/"$1"/DSStorePlaceHolder
 perl makedmg.pl "$VOLNAME" ${BG_PIC} "$1"
@@ -72,9 +76,11 @@ perl makedmg.pl "$VOLNAME" ${BG_PIC} "$1"
 sync;sync;sync
 hdiutil detach ${device}
 
-echo "Compressing r/w disk image to $1.dmg..."
+log "Compressing r/w disk image to $1.dmg..."
 hdiutil convert $2.temp.dmg -quiet -format UDZO -imagekey zlib-level=9 -o $1.dmg
 rm -f $2.temp.dmg
 
 hdiutil internet-enable -no $1.dmg
 rm -rf ${EXPLODED}
+
+log "Done"
