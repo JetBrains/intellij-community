f8d4afc59d633345ca67cd2447e940a8ff2ce6c2
Bas Leijdekkers <basleijdekkers@gmail.com>
2019-06-17 11:07:26 +0200

    IDEA-CR-49457: IG: warn about newline and tab unicode escapes (IDEA-216319)
    (cherry picked from commit 2533db001e2d30a7dc8ea04fa1bdbd1921649435)
    
    GitOrigin-RevId: e0f597a3ba1e0242958e4fb59149537fa983dc08

--- plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/internationalization/UnnecessaryUnicodeEscapeInspection.java
+++ plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/internationalization/UnnecessaryUnicodeEscapeInspection.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.siyeh.ig.internationalization;
 
 import com.intellij.codeInspection.ProblemDescriptor;
@@ -56,6 +42,12 @@ public class UnnecessaryUnicodeEscapeInspection extends BaseInspection {
   @Override
   protected String buildErrorString(Object... infos) {
     final Character c = (Character)infos[0];
+    if (c == '\n') {
+      return InspectionGadgetsBundle.message("unnecessary.unicode.escape.problem.newline.descriptor");
+    }
+    else if (c == '\t') {
+      return InspectionGadgetsBundle.message("unnecessary.unicode.escape.problem.tab.descriptor");
+    }
     return InspectionGadgetsBundle.message("unnecessary.unicode.escape.problem.descriptor", c);
   }
 
@@ -78,6 +70,12 @@ public class UnnecessaryUnicodeEscapeInspection extends BaseInspection {
     @NotNull
     @Override
     public String getName() {
+      if (c == '\n') {
+        return "Replace with line feed character";
+      }
+      else if (c == '\t') {
+        return "Replace with tab character";
+      }
       return "Replace with '" + c + "'";
     }
 
@@ -145,13 +143,15 @@ public class UnnecessaryUnicodeEscapeInspection extends BaseInspection {
           final int escapeEnd = nextChar + 4;
           final char d = (char)Integer.parseInt(text.substring(nextChar, escapeEnd), 16);
           final int type = Character.getType(d);
-          if (type == Character.CONTROL ||
-              type == Character.FORMAT ||
-              type == Character.PRIVATE_USE ||
-              type == Character.SURROGATE ||
-              type == Character.UNASSIGNED ||
-              type == Character.LINE_SEPARATOR ||
-              type == Character.PARAGRAPH_SEPARATOR) {
+          if (type == Character.CONTROL && d != '\n' && d != '\t') {
+            continue;
+          }
+          else if (type == Character.FORMAT ||
+                   type == Character.PRIVATE_USE ||
+                   type == Character.SURROGATE ||
+                   type == Character.UNASSIGNED ||
+                   type == Character.LINE_SEPARATOR ||
+                   type == Character.PARAGRAPH_SEPARATOR) {
             continue;
           }
           if (type == Character.SPACE_SEPARATOR && d != ' ') {
