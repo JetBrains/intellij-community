3798b5f4fcebf67def9449f2bfa658cedfa333bf
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2019-06-18 19:53:10 +0300

    shelf: Convert "ShelveSilentlyAction" to kotlin
    (cherry picked from commit d8e088ae30fb46b21778f390d6b3ee96488a7094)
    
    GitOrigin-RevId: 15d30687aacdbcaaccaa44857856203de95165ad

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/ShelveSilentlyAction.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/ShelveSilentlyAction.kt
@@ -1,46 +1,25 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.intellij.openapi.vcs.changes.actions;
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.vcs.changes.actions
 
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.fileEditor.FileDocumentManager;
-import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.VcsDataKeys;
-import com.intellij.openapi.vcs.changes.Change;
-import com.intellij.openapi.vcs.changes.shelf.ShelveChangesManager;
-import com.intellij.util.ObjectUtils;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.vcs.VcsDataKeys
+import com.intellij.openapi.vcs.changes.shelf.ShelveChangesManager
 
-import java.util.Arrays;
+class ShelveSilentlyAction : DumbAwareAction() {
+  override fun update(e: AnActionEvent) {
+    val project = e.project
+    val changes = e.getData(VcsDataKeys.CHANGES)
 
-public class ShelveSilentlyAction extends AnAction implements DumbAware {
-
-  @Override
-  public void actionPerformed(@NotNull AnActionEvent e) {
-    final Project project = ObjectUtils.assertNotNull(getEventProject(e));
-    FileDocumentManager.getInstance().saveAllDocuments();
-    Change[] changes = ObjectUtils.assertNotNull(e.getData(VcsDataKeys.CHANGES));
-    ShelveChangesManager.getInstance(project).shelveSilentlyUnderProgress(Arrays.asList(changes));
+    e.presentation.isEnabled = project != null && !changes.isNullOrEmpty()
   }
 
-  @Override
-  public void update(@NotNull AnActionEvent e) {
-    Change[] changes = e.getData(VcsDataKeys.CHANGES);
-    e.getPresentation().setEnabled(getEventProject(e) != null && changes != null && changes.length > 0);
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.project!!
+    val changes = e.getData(VcsDataKeys.CHANGES)!!
+
+    FileDocumentManager.getInstance().saveAllDocuments()
+    ShelveChangesManager.getInstance(project).shelveSilentlyUnderProgress(changes.toList())
   }
 }
