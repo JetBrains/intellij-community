6085d8e2d44476232398fa1e2d7501ed181f513d
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-06-18 13:36:17 +0200

    IDEA-CR-48635: [java] 'yield' statement: parser/PSI (IDEA-216410)
    GitOrigin-RevId: e80cf7a8fa2bdecc91c110cca128b4fcfe7dab92

--- java/java-psi-api/src/com/intellij/psi/JavaElementVisitor.java
+++ java/java-psi-api/src/com/intellij/psi/JavaElementVisitor.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi;
 
 import com.intellij.psi.javadoc.*;
@@ -33,6 +33,10 @@ public abstract class JavaElementVisitor extends PsiElementVisitor {
   }
 
   public void visitBreakStatement(PsiBreakStatement statement) {
+    visitStatement(statement);
+  }
+
+  public void visitYieldStatement(PsiYieldStatement statement) {
     visitStatement(statement);
   }
 
