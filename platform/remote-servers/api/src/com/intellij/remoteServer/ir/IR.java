8cff75dfd4d69333398b390a2cc94e89ca4205da
Alexander Zolotov <goldifit@gmail.com>
2019-06-18 20:32:20 +0200

    Add some implementations
    GitOrigin-RevId: 05d469d80988513bbbb6ee08099068a712e5d972

--- platform/remote-servers/api/src/com/intellij/remoteServer/ir/IR.java
+++ platform/remote-servers/api/src/com/intellij/remoteServer/ir/IR.java
@@ -1,10 +1,15 @@
 package com.intellij.remoteServer.ir;
 
+import com.intellij.execution.CommandLineUtil;
 import com.intellij.execution.Platform;
 import com.intellij.execution.configurations.GeneralCommandLine;
 import com.intellij.execution.process.ProcessHandler;
+import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -19,7 +24,7 @@ public class IR {
 
   public interface RemoteRunner {
     Platform getRemotePlatform();
-    
+
     RemoteValue createPathValue(@NotNull String path);
 
     RemoteValue createPortValue(int port);
@@ -49,37 +54,82 @@ public class IR {
   }
 
   public interface RemoteValue {
-    @NotNull
+    RemoteValue EMPTY_VALUE = e -> null;
+
+    @Nullable
     String toString(@NotNull RemoteEnvironment environment);
   }
 
-  public class StringRemoteValue implements RemoteValue {
+  public static class StringRemoteValue implements RemoteValue {
     private final String myString;
 
     public StringRemoteValue(@NotNull String string) {
       myString = string;
     }
 
+    @Nullable
     @Override
-    @NotNull
     public String toString(@NotNull RemoteEnvironment environment) {
       return myString;
     }
   }
 
-  public interface NewCommandLine {
+  public static class NewCommandLine {
+    private RemoteValue myExePath = RemoteValue.EMPTY_VALUE;
+    private RemoteValue myWorkingDirectory = RemoteValue.EMPTY_VALUE;
+    private final List<RemoteValue> myParameters = new ArrayList<>();
+    private final Map<String, RemoteValue> myEnvironment = new HashMap<>();
 
     /**
      * {@link GeneralCommandLine#getPreparedCommandLine()}
      */
-    List<String> prepareCommandLine(@NotNull RemoteEnvironment target);
+    public List<String> prepareCommandLine(@NotNull RemoteEnvironment target) {
+      // todo something with working directory and environment?
+      List<String> parameters = ContainerUtil.mapNotNull(myParameters, v -> v.toString(target));
+      return CommandLineUtil.toCommandLine(myExePath.toString(), parameters, target.getPlatform());
+    }
 
-    void setExePath(RemoteValue exePath);
+    public void setExePath(@NotNull RemoteValue exePath) {
+      myExePath = exePath;
+    }
 
-    void setWorkingDirectory(RemoteValue workingDirectory);
+    public void setWorkingDirectory(@NotNull RemoteValue workingDirectory) {
+      myWorkingDirectory = workingDirectory;
+    }
 
-    void addParameter(RemoteValue parameter);
+    public void addParameter(@NotNull RemoteValue parameter) {
+      myParameters.add(parameter);
+    }
 
-    void addEnvironmentVariable(String name, RemoteValue value);
+    public void addEnvironmentVariable(String name, RemoteValue value) {
+      myEnvironment.put(name, value);
+    }
+  }
+
+  public static class LocalRunner implements RemoteRunner {
+    @Override
+    public Platform getRemotePlatform() {
+      return Platform.current();
+    }
+  
+    @Override
+    public RemoteValue createPathValue(@NotNull String path) {
+      return new StringRemoteValue(path);
+    }
+  
+    @Override
+    public RemoteValue createPortValue(int port) {
+      return e -> String.valueOf(port);
+    }
+  
+    @Override
+    public RemoteEnvironmentRequest createRequest() {
+      return null;
+    }
+  
+    @Override
+    public RemoteEnvironment prepareRemoteEnvironment(RemoteEnvironmentRequest request) throws Exception {
+      return null;
+    }
   }
 }
