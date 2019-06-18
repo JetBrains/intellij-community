bcc450575174d5e1d9db901fc8981698847729a6
Dmitry Jemerov <yole@jetbrains.com>
2019-06-18 15:38:35 +0200

    Don't run detection from content for StubVirtualFile
    GitOrigin-RevId: cf33436c3c02da365fcc2144ec493b793c323d82

--- platform/platform-impl/src/com/intellij/openapi/fileTypes/impl/FileTypeManagerImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/fileTypes/impl/FileTypeManagerImpl.java
@@ -1072,6 +1072,9 @@ public class FileTypeManagerImpl extends FileTypeManagerEx implements Persistent
     if (fileTypeByFileName != UnknownFileType.INSTANCE) {
       return false;
     }
+    if (file instanceof StubVirtualFile) {
+      return false;
+    }
 
     FileType detectedFromContentFileType = file.getUserData(DETECTED_FROM_CONTENT_FILE_TYPE_KEY);
     if (detectedFromContentFileType != null) {
