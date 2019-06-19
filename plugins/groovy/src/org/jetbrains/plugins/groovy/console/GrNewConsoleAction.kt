70047ae5f9dbd2c1cc3c55cafa32c2e6cbd3e729
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-06-19 15:12:41 +0300

    [groovy] console: convert GrNewConsoleAction to kotlin
    GitOrigin-RevId: dfc1aedfa279a485e8132c79610e663f50ae138d

--- plugins/groovy/src/org/jetbrains/plugins/groovy/console/GrNewConsoleAction.kt
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/console/GrNewConsoleAction.kt
@@ -1,52 +1,42 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.groovy.console;
+package org.jetbrains.plugins.groovy.console
 
-import com.intellij.execution.console.ConsoleHistoryController;
-import com.intellij.ide.scratch.ScratchFileService;
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.LangDataKeys;
-import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.plugins.groovy.config.GroovyFacetUtil;
+import com.intellij.execution.console.ConsoleHistoryController
+import com.intellij.ide.scratch.ScratchFileService
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.LangDataKeys
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.module.Module
+import org.jetbrains.plugins.groovy.config.GroovyFacetUtil
 
-import static org.jetbrains.plugins.groovy.console.GroovyConsoleUtilKt.getAnyApplicableModule;
+class GrNewConsoleAction : AnAction() {
 
-public class GrNewConsoleAction extends AnAction {
-  @Override
-  public void update(@NotNull AnActionEvent e) {
-    e.getPresentation().setEnabledAndVisible(getModule(e) != null);
+  override fun update(e: AnActionEvent) {
+    e.presentation.isEnabledAndVisible = getModule(e) != null
   }
 
-  @Override
-  public void actionPerformed(@NotNull AnActionEvent e) {
-    final Project project = e.getProject();
-    final Module module = getModule(e);
-    if (project == null || module == null) return;
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.project
+    val module = getModule(e)
+    if (project == null || module == null) return
 
-    final VirtualFile contentFile = ConsoleHistoryController.getContentFile(
+    val contentFile = ConsoleHistoryController.getContentFile(
       GroovyConsoleRootType.getInstance(),
       GroovyConsoleRootType.CONTENT_ID,
       ScratchFileService.Option.create_new_always
-    );
-    assert contentFile != null;
-    GroovyConsole.createConsole(project, contentFile, module);
-    FileEditorManager.getInstance(project).openFile(contentFile, true);
+    ) ?: return
+    GroovyConsole.createConsole(project, contentFile, module)
+    FileEditorManager.getInstance(project).openFile(contentFile, true)
   }
 
-  @Nullable
-  protected Module getModule(AnActionEvent e) {
-    Project project = e.getProject();
-    if (project == null) return null;
+  protected fun getModule(e: AnActionEvent): Module? {
+    val project = e.project ?: return null
 
-    Module module = e.getData(LangDataKeys.MODULE);
+    var module = e.getData(LangDataKeys.MODULE)
     if (!GroovyFacetUtil.isSuitableModule(module)) {
-      module = getAnyApplicableModule(project);
+      module = getAnyApplicableModule(project)
     }
-    return module;
+    return module
   }
 }
