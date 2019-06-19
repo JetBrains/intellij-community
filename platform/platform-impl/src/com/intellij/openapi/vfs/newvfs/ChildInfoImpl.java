117eaeec949cec2eac8422ac8e8547a55990237c
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-06-19 15:10:31 +0200

    Cleanup (formatting; typos)
    GitOrigin-RevId: 47a3232aaad700bc3eb13521ce22c8b79d0b6ddd

--- platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/ChildInfoImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/ChildInfoImpl.java
@@ -10,19 +10,16 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 @ApiStatus.Internal
-// internal class for data transfer from refresh worker to persistent FS impl, do not use
 public class ChildInfoImpl implements ChildInfo {
   public static final int UNKNOWN_ID_YET = -1;
+
   private final int id;
   private final int nameId;
   private final String symLinkTarget;
-  @Nullable // null means children are unknown
-  private final ChildInfo[] children;
+  private final @Nullable("null means children are unknown") ChildInfo[] children;
 
-  // inlined FileAttributes to reduce memory
-  private final byte fileAttributesType;
-  @FileAttributes.Flags
-  private final byte flags; // -1 means getFileAttributes == null
+  private final byte fileAttributesType;  // inlined FileAttributes to reduce memory
+  private final @FileAttributes.Flags byte flags; // -1 means getFileAttributes == null
   private final long length;
   private final long lastModified;
 
@@ -39,7 +36,7 @@ public class ChildInfoImpl implements ChildInfo {
     this.id = id;
     this.children = children;
     this.symLinkTarget = symLinkTarget;
-    if (id <= 0 && id != UNKNOWN_ID_YET) throw new IllegalArgumentException("expected id > 0, got: "+id);
+    if (id <= 0 && id != UNKNOWN_ID_YET) throw new IllegalArgumentException("Invalid ID: " + id);
     if (attributes == null) {
       fileAttributesType = -1;
       flags = -1;
@@ -52,19 +49,6 @@ public class ChildInfoImpl implements ChildInfo {
       length = attributes.length;
       lastModified = attributes.lastModified;
     }
-  }
-
-  @Override
-  public String toString() {
-    return nameId + " id: " + id
-           + " (" + getFileAttributes() + ")"
-           + (children == null ? "" :
-              "\n  " + StringUtil.join(children, info -> info.toString().replaceAll("\n", "\n  "), "\n  "));
-  }
-
-  @Override
-  public FileAttributes getFileAttributes() {
-    return flags == -1 ? null : FileAttributes.createFrom(fileAttributesType, flags, length, lastModified);
   }
 
   @Override
@@ -94,4 +78,14 @@ public class ChildInfoImpl implements ChildInfo {
     return children;
   }
 
+  @Override
+  public FileAttributes getFileAttributes() {
+    return flags == -1 ? null : FileAttributes.createFrom(fileAttributesType, flags, length, lastModified);
+  }
+
+  @Override
+  public String toString() {
+    return nameId + " id: " + id + " (" + getFileAttributes() + ")" +
+           (children == null ? "" : "\n  " + StringUtil.join(children, info -> info.toString().replaceAll("\n", "\n  "), "\n  "));
+  }
 }
