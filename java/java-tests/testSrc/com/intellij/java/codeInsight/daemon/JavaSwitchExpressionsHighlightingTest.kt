06d3d4199924ddaebcce30de6d92ecafef5f164e
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-06-20 23:03:17 +0200

    [java] 'yield' statement: basic highlighting (IDEA-216410)
    GitOrigin-RevId: 4edae910f16a27c2a46068c2d8b6fce4674e0571

--- java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/JavaSwitchExpressionsHighlightingTest.kt
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/JavaSwitchExpressionsHighlightingTest.kt
@@ -1,7 +1,10 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.java.codeInsight.daemon
 
 import com.intellij.JavaTestUtil
+import com.intellij.openapi.roots.LanguageLevelModuleExtension
+import com.intellij.openapi.roots.ModuleRootModificationUtil
+import com.intellij.pom.java.LanguageLevel
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
 
 class JavaSwitchExpressionsHighlightingTest : LightJavaCodeInsightFixtureTestCase() {
@@ -17,6 +20,17 @@ class JavaSwitchExpressionsHighlightingTest : LightJavaCodeInsightFixtureTestCas
   fun testEnhancedSwitchDefinitelyAssigned() = doTest()
   fun testEnhancedSwitchUnreachable() = doTest()
   fun testSwitchExpressionHasResult() = doTest()
+
+  fun testYieldStatements() = try {
+    level(LanguageLevel.JDK_13_PREVIEW)
+    doTest()
+  }
+  finally {
+    level(LanguageLevel.JDK_12_PREVIEW)
+  }
+
+  private fun level(level: LanguageLevel) =
+    ModuleRootModificationUtil.updateModel(module) { it.getModuleExtension(LanguageLevelModuleExtension::class.java).languageLevel = level }
 
   private fun doTest() {
     myFixture.configureByFile("${getTestName(false)}.java")
