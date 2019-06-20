cfca48fed33966c25ee43d52d53704dd26a26439
Alexander Lobas <Alexander.Lobas@jetbrains.com>
2019-06-20 22:25:57 +0300

    IDEA-203286 Paste From History appends paste history number to pasted content.
    (cherry picked from commit e2eabcb76ee79ae0917474786dc1a522a9a40132)
    (cherry picked from commit cfd438299530fc1729ca52fae19182d273863a7a)
    
    GitOrigin-RevId: d8e96ddd17efb390c511e98a1102f18ef11603f7

--- platform/platform-impl/src/com/intellij/openapi/editor/actions/ContentChooser.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/actions/ContentChooser.java
@@ -4,6 +4,7 @@ package com.intellij.openapi.editor.actions;
 import com.intellij.CommonBundle;
 import com.intellij.icons.AllIcons;
 import com.intellij.ide.ui.SplitterProportionsDataImpl;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.EditorFactory;
@@ -127,7 +128,9 @@ public abstract class ContentChooser<Data> extends DialogWrapper {
 
     MyListCellRenderer renderer = new MyListCellRenderer();
     myList.setCellRenderer(renderer);
-    myList.addKeyListener(new KeyAdapter() {
+    myList.addKeyListener(new KeyListener() {
+      boolean doConsume;
+
       @Override
       public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_DELETE) {
@@ -160,9 +163,24 @@ public abstract class ContentChooser<Data> extends DialogWrapper {
             if (idx < myAllContents.size()) {
               myList.setSelectedIndex(idx);
               e.consume();
-              doOKAction();
+              doConsume = true;
+              ApplicationManager.getApplication().invokeLater(() -> doOKAction());
             }
           }
+        }
+      }
+
+      @Override
+      public void keyTyped(KeyEvent e) {
+        if (doConsume) {
+          e.consume();
+        }
+      }
+
+      @Override
+      public void keyReleased(KeyEvent e) {
+        if (doConsume) {
+          e.consume();
         }
       }
     });
