ec65861938183eac5d99a9936a45c9579ca9dede
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-06-21 16:58:14 +0300

    [groovy] cleanup: remove unused methods from psi
    GitOrigin-RevId: bf6caf637d0b0d04e3c3b695bfe32943a19d3bee

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/statements/GrForStatementImpl.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/statements/GrForStatementImpl.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.lang.psi.impl.statements;
 
 import com.intellij.lang.ASTNode;
@@ -67,10 +67,5 @@ public class GrForStatementImpl extends GroovyPsiElementImpl implements GrForSta
   @Override
   public PsiElement getRParenth() {
     return findChildByType(GroovyTokenTypes.mRPAREN);
-  }
-
-  @Override
-  public PsiElement getLParenth() {
-    return findChildByType(GroovyTokenTypes.mLPAREN);
   }
 }
