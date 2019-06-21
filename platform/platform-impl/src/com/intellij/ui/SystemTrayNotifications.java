c2df07ee639c1db9fb5a94b0b08ca3552d0e7d22
Alexander Lobas <Alexander.Lobas@jetbrains.com>
2019-06-21 19:37:04 +0300

    IDEA-207320 Empty system tray icon with no popup hint or right-click menu (logging)
    (cherry picked from commit b640db0b12493fc896699b4d4545ca786182b6b7)
    
    GitOrigin-RevId: 196239f1513f7a1d25549db205051e0c4b481ba3

--- platform/platform-impl/src/com/intellij/ui/SystemTrayNotifications.java
+++ platform/platform-impl/src/com/intellij/ui/SystemTrayNotifications.java
@@ -2,6 +2,7 @@
 package com.intellij.ui;
 
 import com.intellij.openapi.application.impl.ApplicationInfoImpl;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.wm.IdeFocusManager;
 import com.intellij.openapi.wm.IdeFrame;
 import com.intellij.util.ui.UIUtil;
@@ -20,9 +21,22 @@ final class SystemTrayNotifications implements SystemNotificationsImpl.Notifier 
   @Nullable
   static synchronized SystemTrayNotifications getWin10Instance() throws AWTException {
     if (ourWin10Instance == null && SystemTray.isSupported()) {
+      Logger logger = Logger.getInstance(SystemNotifications.class);
       Image image = AppUIUtil.loadApplicationIcon16();
       if (image == null) {
+        logger.info("=== No system tray IDE icon (" + ApplicationInfoImpl.getShadowInstance().getApplicationSvgIconUrl() + ") ===");
+
         image = UIUtil.createImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+        Graphics g = image.getGraphics();
+        //noinspection UseJBColor
+        g.setColor(Color.red);
+        g.fillRect(0, 0, image.getWidth(null), image.getHeight(null));
+        g.dispose();
+
+        logger.info("=== Use \"red square\" stub for system tray IDE icon ===");
+      }
+      else {
+        logger.info("=== System tray IDE icon: " + image + " ===");
       }
       ourWin10Instance = new SystemTrayNotifications(image, TrayIcon.MessageType.INFO);
     }
