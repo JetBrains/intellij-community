4ed42c3cf10c4dd834c558849c6d558acea88f36
Anna Kozlova <anna.kozlova@jetbrains.com>
2019-06-24 09:55:54 +0200

    IDEA-CR-48919: java: yield/break completion + migrate tests to java 13
    GitOrigin-RevId: 504da5e24cbb8a809cc2677f07fed1683c92c988

--- java/java-tests/testData/codeInsight/completion/normal/BreakInSwitchExpression.java
+++ java/java-tests/testData/codeInsight/completion/normal/BreakInSwitchExpression.java
@@ -2,7 +2,7 @@ public class ConstConfig {
     int test(int x) {
         return switch (x) {
             case 1:
-                if (Math.random() > 0.5) br<caret>
+                if (Math.random() > 0.5) yi<caret>
         };
     }
 }
--- java/java-tests/testData/codeInsight/completion/normal/BreakInSwitchExpression_after.java
+++ java/java-tests/testData/codeInsight/completion/normal/BreakInSwitchExpression_after.java
@@ -2,7 +2,7 @@ public class ConstConfig {
     int test(int x) {
         return switch (x) {
             case 1:
-                if (Math.random() > 0.5) break <caret>
+                if (Math.random() > 0.5) yield <caret>
         };
     }
 }
