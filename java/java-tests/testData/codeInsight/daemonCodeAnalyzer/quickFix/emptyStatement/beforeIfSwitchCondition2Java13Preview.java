16713bc6a9a75b7465052ad795d1e3f0aecac528
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-06-25 06:23:25 +0200

    IDEA-CR-49013: [java] 'yield' statements support in the side-effect extractor
    GitOrigin-RevId: fbaaeb57f3dc357b238944db553f83c7e4c2e815

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/emptyStatement/beforeIfSwitchCondition2Java13Preview.java
@@ -0,0 +1,14 @@
+// "Extract side effects as an 'if' statement" "true"
+class Z {
+
+  void z() {
+    i<caret>f (foo ? switch(0) {
+      case 0: yield false;
+      case 1: yield true;
+      default: yield new Foo().getBar();
+    } : switch(0) {
+      case 0: yield false;
+      case 1: yield true;
+      default: yield false;}) {}
+  }
+}
