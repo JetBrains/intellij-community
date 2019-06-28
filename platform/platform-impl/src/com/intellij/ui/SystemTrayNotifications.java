19cbd7049bc4c016d2ccd104213510c2f2be28cf
Alexander Lobas <Alexander.Lobas@jetbrains.com>
2019-06-28 12:48:57 +0300

    IDEA-207320 Empty system tray icon with no popup hint or right-click menu (cleanup)
    GitOrigin-RevId: 465e1cca2364de0b3d59aed91d57717a0afdaceb

--- platform/platform-impl/src/com/intellij/ui/SystemTrayNotifications.java
+++ platform/platform-impl/src/com/intellij/ui/SystemTrayNotifications.java
@@ -2,12 +2,8 @@
 package com.intellij.ui;
 
 import com.intellij.openapi.application.impl.ApplicationInfoImpl;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.util.IconLoader;
 import com.intellij.openapi.wm.IdeFocusManager;
 import com.intellij.openapi.wm.IdeFrame;
-import com.intellij.ui.paint.PaintUtil;
-import com.intellij.ui.scale.DerivedScaleType;
 import com.intellij.ui.scale.ScaleContext;
 import com.intellij.util.IconUtil;
 import com.intellij.util.ui.ImageUtil;
@@ -17,7 +13,6 @@ import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
 
 /**
  * @author Alexander Lobas
@@ -34,33 +29,9 @@ final class SystemTrayNotifications implements SystemNotificationsImpl.Notifier 
   }
 
   @NotNull
-  private static Logger getLogger() {
-    return Logger.getInstance(SystemTrayNotifications.class);
-  }
-
-  @NotNull
   private static Image createImage() {
     Icon icon = AppUIUtil.loadSmallApplicationIcon(ScaleContext.create());
     return ImageUtil.toBufferedImage(IconUtil.toImage(icon));
-  }
-
-  @NotNull
-  private static Image createStubImage() {
-    getLogger().info("=== Use \"red square\" stub for system tray IDE icon ===");
-
-    Image image = UIUtil.createImage(16, 16, BufferedImage.TYPE_INT_ARGB);
-    Graphics g = image.getGraphics();
-
-    try {
-      //noinspection UseJBColor
-      g.setColor(Color.red);
-      g.fillRect(0, 0, image.getWidth(null), image.getHeight(null));
-    }
-    finally {
-      g.dispose();
-    }
-
-    return image;
   }
 
   private final TrayIcon myTrayIcon;
