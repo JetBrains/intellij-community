b67f5561eec38295947ab306a202a56161baa365
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2019-06-30 13:57:43 +0300

    vcs: Convert "UnresolvedMergeCheckFactory" to kotlin
    GitOrigin-RevId: 729471a95dbc3fdd1415292b309ecd867f9a9974

--- platform/vcs-impl/src/com/intellij/openapi/vcs/checkin/UnresolvedMergeCheckFactory.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/checkin/UnresolvedMergeCheckFactory.kt
@@ -1,58 +1,44 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.openapi.vcs.checkin;
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.vcs.checkin
 
-import com.intellij.openapi.ui.Messages;
-import com.intellij.openapi.vcs.CheckinProjectPanel;
-import com.intellij.openapi.vcs.FileStatus;
-import com.intellij.openapi.vcs.changes.Change;
-import com.intellij.openapi.vcs.changes.CommitContext;
-import com.intellij.openapi.vcs.changes.CommitExecutor;
-import com.intellij.openapi.vcs.checkin.CheckinHandler.ReturnResult;
-import com.intellij.util.PairConsumer;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.ui.Messages.getWarningIcon
+import com.intellij.openapi.ui.Messages.showYesNoDialog
+import com.intellij.openapi.vcs.CheckinProjectPanel
+import com.intellij.openapi.vcs.FileStatus.*
+import com.intellij.openapi.vcs.changes.CommitContext
+import com.intellij.openapi.vcs.changes.CommitExecutor
+import com.intellij.util.PairConsumer
 
 /**
  * Checks if there are unresolved conflicts selected to commit.
- *
- * @author Kirill Likhodedov
  */
-public class UnresolvedMergeCheckFactory extends CheckinHandlerFactory {
-  @NotNull
-  @Override
-  public CheckinHandler createHandler(@NotNull CheckinProjectPanel panel, @NotNull CommitContext commitContext) {
-    return new CheckinHandler() {
-      @Override
-      public ReturnResult beforeCheckin(@Nullable CommitExecutor executor, PairConsumer<Object, Object> additionalDataConsumer) {
-        for (UnresolvedMergeCheckProvider extension : UnresolvedMergeCheckProvider.EP_NAME.getExtensions()) {
-          ReturnResult result = extension.checkUnresolvedConflicts(panel, commitContext, executor);
-          if (result != null) return result;
-        }
-        return performDefaultCheck(panel);
-      }
-    };
+class UnresolvedMergeCheckFactory : CheckinHandlerFactory() {
+  override fun createHandler(panel: CheckinProjectPanel, commitContext: CommitContext): CheckinHandler =
+    UnresolvedMergeCheckHandler(panel, commitContext)
+}
+
+private val MERGE_STATUSES = setOf(MERGE, MERGED_WITH_BOTH_CONFLICTS, MERGED_WITH_CONFLICTS, MERGED_WITH_PROPERTY_CONFLICTS)
+
+private class UnresolvedMergeCheckHandler(
+  private val panel: CheckinProjectPanel,
+  private val commitContext: CommitContext
+) : CheckinHandler() {
+
+  override fun beforeCheckin(executor: CommitExecutor?, additionalDataConsumer: PairConsumer<Any, Any>): ReturnResult {
+    val providerResult = UnresolvedMergeCheckProvider.EP_NAME.extensions.asSequence()
+      .mapNotNull { it.checkUnresolvedConflicts(panel, commitContext, executor) }
+      .firstOrNull()
+    return providerResult ?: performDefaultCheck()
   }
 
-  @NotNull
-  private static ReturnResult performDefaultCheck(@NotNull CheckinProjectPanel panel) {
-    if (containsUnresolvedConflicts(panel)) {
-      int answer = Messages.showYesNoDialog(panel.getComponent(), "Are you sure you want to commit changes with unresolved conflicts?",
-                                            "Unresolved Conflicts", Messages.getWarningIcon());
-      if (answer != Messages.YES) return ReturnResult.CANCEL;
-    }
-    return ReturnResult.COMMIT;
+  private fun performDefaultCheck(): ReturnResult = if (panel.hasUnresolvedConflicts()) askUser() else ReturnResult.COMMIT
+
+  private fun askUser(): ReturnResult {
+    val answer = showYesNoDialog(panel.component, "Are you sure you want to commit changes with unresolved conflicts?",
+                                 "Unresolved Conflicts", getWarningIcon())
+    return if (answer != Messages.YES) ReturnResult.CANCEL else ReturnResult.COMMIT
   }
 
-  private static boolean containsUnresolvedConflicts(@NotNull CheckinProjectPanel panel) {
-    for (Change change : panel.getSelectedChanges()) {
-      FileStatus status = change.getFileStatus();
-      if (status.equals(FileStatus.MERGE) ||
-          status.equals(FileStatus.MERGED_WITH_BOTH_CONFLICTS) ||
-          status.equals(FileStatus.MERGED_WITH_CONFLICTS) ||
-          status.equals(FileStatus.MERGED_WITH_PROPERTY_CONFLICTS)) {
-        return true;
-      }
-    }
-    return false;
-  }
+  private fun CheckinProjectPanel.hasUnresolvedConflicts() = selectedChanges.any { it.fileStatus in MERGE_STATUSES }
 }
