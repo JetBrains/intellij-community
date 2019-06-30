bc4afe45e27d7a4cade6988a4526e15c29b0cb6b
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-07-01 00:09:11 +0200

    [tests] loading UAST test data into temp. FS to ensure correct language level
    GitOrigin-RevId: fc08701a200b0cef69fce98a7e86e3e7a9dc813f

--- plugins/groovy/groovy-uast-tests/test/org/jetbrains/plugins/groovy/uast/AbstractGroovyUastTest.kt
+++ plugins/groovy/groovy-uast-tests/test/org/jetbrains/plugins/groovy/uast/AbstractGroovyUastTest.kt
@@ -1,9 +1,7 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.uast
 
 import com.intellij.openapi.application.ex.PathManagerEx
-import com.intellij.openapi.vfs.LocalFileSystem
-import com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.uast.test.common.RenderLogTestBase
 import org.jetbrains.uast.test.env.AbstractUastFixtureTest
 import java.io.File
@@ -13,15 +11,10 @@ abstract class AbstractGroovyUastTest : AbstractUastFixtureTest() {
     val TEST_GROOVY_MODEL_DIR = File(PathManagerEx.getCommunityHomePath(), "plugins/groovy/groovy-uast-tests/testData")
   }
 
-  override fun getVirtualFile(testName: String): VirtualFile {
-    val localPath = File(TEST_GROOVY_MODEL_DIR, testName).path
-    val vFile = LocalFileSystem.getInstance().findFileByPath(localPath)
-    return vFile ?: throw IllegalStateException("Couldn't find virtual file for $localPath")
-  }
+  override fun getTestFile(testName: String): File = File(TEST_GROOVY_MODEL_DIR, testName)
 }
 
 abstract class AbstractGroovyRenderLogTest : AbstractGroovyUastTest(), RenderLogTestBase {
   override fun getTestFile(testName: String, ext: String) =
     File(File(TEST_GROOVY_MODEL_DIR, testName).canonicalPath.substringBeforeLast('.') + '.' + ext)
-
 }
--- uast/uast-tests/test/org/jetbrains/uast/test/java/AbstractJavaUastTest.kt
+++ uast/uast-tests/test/org/jetbrains/uast/test/java/AbstractJavaUastTest.kt
@@ -1,9 +1,7 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.uast.test.java
 
 import com.intellij.openapi.application.ex.PathManagerEx
-import com.intellij.openapi.vfs.LocalFileSystem
-import com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.uast.test.env.AbstractUastFixtureTest
 import java.io.File
 
@@ -12,9 +10,5 @@ abstract class AbstractJavaUastTest : AbstractUastFixtureTest() {
     val TEST_JAVA_MODEL_DIR = File(PathManagerEx.getCommunityHomePath(), "uast/uast-tests/java")
   }
 
-  override fun getVirtualFile(testName: String): VirtualFile {
-    val localPath = File(TEST_JAVA_MODEL_DIR, testName).path
-    val vFile = LocalFileSystem.getInstance().findFileByPath(localPath)
-    return vFile ?: throw IllegalStateException("Couldn't find virtual file for $localPath")
-  }
+  override fun getTestFile(testName: String): File = File(TEST_JAVA_MODEL_DIR, testName)
 }
