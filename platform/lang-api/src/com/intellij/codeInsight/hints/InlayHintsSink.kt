5c8b7de264ca2624d78b7f5c6f28721bde1ec83c
Roman.Ivanov <Roman.Ivanov@jetbrains.com>
2019-07-01 11:31:56 +0700

    Inlay hints: add clarifying doc
    GitOrigin-RevId: bf2e00d8590fc89aed939923d12415ee9fde8895

--- platform/lang-api/src/com/intellij/codeInsight/hints/InlayHintsSink.kt
+++ platform/lang-api/src/com/intellij/codeInsight/hints/InlayHintsSink.kt
@@ -13,6 +13,8 @@ interface InlayHintsSink {
 
   /**
    * Adds block element to underlying editor.
+   * Offset doesn't affects position of the inlay in the line, it will be drawn in the very beginning of the line.
+   * Presentation must shift itself (see com.intellij.openapi.editor.ex.util.EditorUtil#getPlainSpaceWidth)
    * Note, that single provider may add only one presentation to the given offset. This requirement may be relaxed in future.
    * @see [com.intellij.openapi.editor.InlayModel.addBlockElement]
    */
