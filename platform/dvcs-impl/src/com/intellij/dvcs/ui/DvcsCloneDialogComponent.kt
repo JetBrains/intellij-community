f075678c023ee8538188e3144a9d394777c9d3c6
Aleksandr Krasilnikov <aleksandr.krasilnikov@jetbrains.com>
2019-07-01 20:08:33 +0300

    [clonedialog] Implement VcsCloneComponent for dynamic VCS
    GitOrigin-RevId: 387be3ae137041c01336921c316d918ff0050248

---
+++ platform/dvcs-impl/src/com/intellij/dvcs/ui/DvcsCloneDialogComponent.kt
@@ -0,0 +1,98 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.dvcs.ui
+
+import com.intellij.dvcs.DvcsRememberedInputs
+import com.intellij.dvcs.repo.ClonePathProvider
+import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.TextFieldWithBrowseButton
+import com.intellij.openapi.ui.ValidationInfo
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.openapi.vcs.CheckoutProvider
+import com.intellij.openapi.vcs.ui.VcsCloneComponent
+import com.intellij.ui.DocumentAdapter
+import com.intellij.ui.components.JBTextField
+import com.intellij.ui.layout.*
+import com.intellij.util.containers.ContainerUtil
+import java.nio.file.InvalidPathException
+import java.nio.file.Paths
+import javax.swing.JPanel
+import javax.swing.event.DocumentEvent
+
+abstract class DvcsCloneDialogComponent(var project: Project,
+                                        private var vcsDirectoryName: String,
+                                        rememberedInputs: DvcsRememberedInputs) : VcsCloneComponent {
+  private val mainPanel: JPanel
+  private val urlEditor = JBTextField()
+  private val directoryField = MyTextFieldWithBrowseButton(ClonePathProvider.defaultParentDirectoryPath(project, rememberedInputs))
+
+  init {
+    val fcd = FileChooserDescriptorFactory.createSingleFolderDescriptor()
+    fcd.isShowFileSystemRoots = true
+    fcd.isHideIgnored = false
+    directoryField.addBrowseFolderListener(DvcsBundle.getString("clone.destination.directory.browser.title"),
+                                           DvcsBundle.getString("clone.destination.directory.browser.description"),
+                                           project,
+                                           fcd)
+    mainPanel = panel {
+      row("URL:") { urlEditor(CCFlags.growX) }
+      row("Directory:") { directoryField(CCFlags.growX) }
+    }
+
+    urlEditor.document.addDocumentListener(object : DocumentAdapter() {
+      override fun textChanged(e: DocumentEvent) {
+        directoryField.trySetChildPath(defaultDirectoryPath(urlEditor.text.trim()))
+      }
+    })
+  }
+
+  private fun defaultDirectoryPath(url: String): String {
+    return StringUtil.trimEnd(ClonePathProvider.relativeDirectoryPathForVcsUrl(project, url), vcsDirectoryName)
+  }
+
+  override fun getView() = mainPanel
+
+  override fun isOkEnabled(): Boolean {
+    return false
+  }
+
+  override fun doValidateAll(): List<ValidationInfo> {
+    val list = ArrayList<ValidationInfo>()
+    ContainerUtil.addIfNotNull(list, CloneDvcsValidationUtils.checkDirectory(directoryField.text, directoryField.textField))
+    ContainerUtil.addIfNotNull(list, CloneDvcsValidationUtils.checkRepositoryURL(urlEditor, urlEditor.text.trim()))
+    ContainerUtil.addIfNotNull(list, CloneDvcsValidationUtils.createDestination(directoryField.text))
+    return list
+  }
+
+  abstract override fun doClone(project: Project, listener: CheckoutProvider.Listener)
+
+  fun getDirectory() = directoryField.text
+  fun getUrl() = urlEditor.text
+
+  class MyTextFieldWithBrowseButton constructor(defaultParentPath: String) : TextFieldWithBrowseButton() {
+    private val myDefaultParentPath = Paths.get(defaultParentPath).toAbsolutePath()
+    private var myModifiedByUser = false
+
+    init {
+      text = myDefaultParentPath.toString()
+      textField.document.addDocumentListener(object : DocumentAdapter() {
+        override fun textChanged(e: DocumentEvent) {
+          myModifiedByUser = true
+        }
+      })
+    }
+
+    fun trySetChildPath(child: String) {
+      if (!myModifiedByUser) {
+        try {
+          text = myDefaultParentPath.resolve(child).toString()
+        }
+        catch (ignored: InvalidPathException) {
+        }
+        finally {
+          myModifiedByUser = false
+        }
+      }
+    }
+  }
+}
