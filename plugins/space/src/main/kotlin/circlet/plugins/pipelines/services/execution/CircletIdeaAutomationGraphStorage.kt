016a5469a4d8293bf85d47161f855fd3a380fb5b
Anton Spilnyy <anton.spilnyy@jetbrains.com>
2019-07-02 10:12:27 +0200

    [idea plugin] [automation] more for running task
    GitOrigin-RevId: da5e94a2c797fe93949a3ca391a19a645faa85e9

--- plugins/space/src/main/kotlin/circlet/plugins/pipelines/services/execution/CircletIdeaAutomationGraphStorage.kt
+++ plugins/space/src/main/kotlin/circlet/plugins/pipelines/services/execution/CircletIdeaAutomationGraphStorage.kt
@@ -8,15 +8,160 @@ import circlet.pipelines.engine.utils.*
 import circlet.pipelines.utils.*
 import libraries.klogging.*
 
-class CircletIdeaAutomationGraphStorage: AutomationGraphStorage {
+//copypasted from server. todo remove
+class AfterTransactionCallback {
+    private val hooks = mutableListOf<suspend () -> Unit>()
+
+    fun afterTransaction(body: suspend () -> Unit) {
+        hooks.add(body)
+    }
+
+    suspend fun run() {
+        hooks.forEach { it() }
+    }
+}
+
+class CircletIdeaAGraphMetaEntity(
+    override val id: Long,
+    override val originalMeta: ProjectAction) : AGraphMetaEntity
+
+class CircletIdeaAGraphExecutionEntity(
+    override val id: Long,
+    override var triggerTime: Long,
+    override var startTime: Long?,
+    override var endTime: Long?,
+    override var status: ExecutionStatus,
+    override val graph: AGraphMetaEntity,
+    override var executionMeta: ProjectAction,
+    private val jobsList: List<AJobExecutionEntity>) : AGraphExecutionEntity {
+
+    override val jobs: Sequence<AJobExecutionEntity>
+        get() = jobsList.asSequence()
+}
+
+
+class CircletIdeaAJobExecutionEntity(
+    override val id: Long,
+    override var triggerTime: Long,
+    override var startTime: Long?,
+    override var endTime: Long?,
+    override var status: ExecutionStatus,
+    override val graph: AGraphExecutionEntity,
+    override val meta: ProjectJob.Process<*, *>) : AJobExecutionEntity
+
+
+class TaskLongIdStorage {
+    private var nextId : Long = 0
+    private val map = mutableMapOf<String, Long>()
+    fun getOrCreateId(stringId: String) : Long {
+        return map.getOrPut(stringId) {
+            nextId++
+        }
+    }
+}
+
+class CircletIdeaGraphStorageTransaction(private val task: ProjectTask) : GraphStorageTransaction {
+
+    private val callback = AfterTransactionCallback()
+    private val idStorage = TaskLongIdStorage()
+
+    companion object : KLogging()
+
+    suspend fun executeAfterTransactionHooks() {
+        callback.run()
+    }
+
+    override fun executeAfterTransaction(body: suspend () -> Unit) {
+        logger.debug { "executeAfterTransaction" }
+        callback.afterTransaction(body)
+    }
+
+    override fun getAllNotFinishedJobs(graphExecution: AGraphExecutionEntity): Iterable<AJobExecutionEntity> {
+        TODO("getAllNotFinishedJobs not implemented")
+    }
+
+    override fun findMetaTask(metaTaskId: Long): AGraphMetaEntity? {
+        logger.debug { "findMetaTask $metaTaskId" }
+        return CircletIdeaAGraphMetaEntity(metaTaskId, task)
+    }
+
+    override fun createTaskExecution(metaTask: AGraphMetaEntity, taskStartContext: TaskStartContext): AGraphExecutionEntity {
+        logger.debug { "createTaskExecution $metaTask" }
+        val now = System.currentTimeMillis()
+        val jobs = mutableListOf<AJobExecutionEntity>()
+        val graphExecutionEntity = CircletIdeaAGraphExecutionEntity(
+            metaTask.id,
+            now,
+            null,
+            null,
+            ExecutionStatus.PENDING,
+            metaTask,
+            metaTask.originalMeta,
+            jobs)
+
+        metaTask.originalMeta.jobs.traverseJobs {
+            jobs.add(CircletIdeaAJobExecutionEntity(idStorage.getOrCreateId(it.id), now, null, null, ExecutionStatus.SCHEDULED, graphExecutionEntity, it))
+
+        }
+
+        return graphExecutionEntity
+    }
+
+    private fun ProjectJob.traverseJobs(action: (ProjectJob.Process.Container) -> Unit) {
+        when (val job = this) {
+            is ProjectJob.CompositeJob -> {
+                job.children.forEach {
+                    it.traverseJobs(action)
+                }
+            }
+            is ProjectJob.Process.Container -> {
+                action(job)
+            }
+            is ProjectJob.Process.VM -> {
+                logger.warn { "ProjectJob.Process.VM is not supported" }
+            }
+        }
+    }
+
+    override fun createSshKey(graphExecution: AGraphExecutionEntity, fingerPrint: String) {
+        logger.debug { "createSshKey $fingerPrint" }
+    }
+
+    override fun batchCreateJobExecutions(graphExecution: AGraphExecutionEntity, bootstrapJob: ProjectJob.Process.Container): Sequence<AJobExecutionEntity> {
+        val res = mutableListOf<AJobExecutionEntity>()
+        res.addAll(graphExecution.jobs)
+        res.add(CircletIdeaAJobExecutionEntity(
+            idStorage.getOrCreateId(bootstrapJob.id),
+            System.currentTimeMillis(),
+            null,
+            null,
+            ExecutionStatus.SCHEDULED,
+            graphExecution, bootstrapJob))
+        return res.asSequence()
+    }
+
+    override fun findJobExecutionByWorkerId(workerId: String): AJobExecutionEntity? {
+        TODO("findJobExecutionByWorkerId not implemented")
+    }
+}
+
+class CircletIdeaAutomationGraphStorage(private val task: ProjectTask) : AutomationGraphStorage {
     override suspend fun <T> invoke(body: (GraphStorageTransaction) -> T): T {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val tx = CircletIdeaGraphStorageTransaction(task)
+        val res = body(tx)
+        tx.executeAfterTransactionHooks()
+        return res
     }
 }
 
 class CircletIdeaAutomationBootstrapper: AutomationBootstrapper {
     override fun createBootstrapJob(execution: AGraphExecutionEntity, repository: RepositoryData, orgUrl: String): ProjectJob.Process.Container {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val container = ProjectJob.Process.Container(
+            "hello-world",
+            ProjectJob.ProcessData(
+                exec = ProjectJob.ProcessExecutable.ContainerExecutable.DefaultCommand(emptyList())))
+        container.applyIds()
+        return container
     }
 }
 
