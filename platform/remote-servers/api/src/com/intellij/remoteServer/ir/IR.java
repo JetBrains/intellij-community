c2261a1fba8224a6f70bdec6032a66d020915d56
Michael Golubev <michael.golubev@jetbrains.com>
2019-07-02 13:48:34 +0200

    instrumental rating
    - beautify API,
    - - don't require environment to resolve values
    - - in and out values
    - - better names
    - switch implementation and clients to the new API
    
    GitOrigin-RevId: ed1949be98dd63ede3e8e61d61b2d79eca93643a

--- platform/remote-servers/api/src/com/intellij/remoteServer/ir/IR.java
+++ platform/remote-servers/api/src/com/intellij/remoteServer/ir/IR.java
@@ -3,6 +3,7 @@ package com.intellij.remoteServer.ir;
 import com.intellij.execution.CommandLineUtil;
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.configurations.GeneralCommandLine;
+import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.util.Pair;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
@@ -15,62 +16,109 @@ import java.util.Map;
 
 public class IR {
   public interface RemoteRunner {
+    //static Process runSimpleProcess(String commandLine, ProgressIndicator indicator) {
+    //  return null;
+    //}
+
     @NotNull
     RemotePlatform getRemotePlatform();
 
     RemoteEnvironmentRequest createRequest();
 
-    RemoteEnvironment prepareRemoteEnvironment(RemoteEnvironmentRequest request);
+    RemoteEnvironment prepareRemoteEnvironment(RemoteEnvironmentRequest request, ProgressIndicator indicator);
   }
 
   public interface RemoteEnvironment {
     RemotePlatform getRemotePlatform();
 
-    // todo: string or remoteValue or pathId?
-    //String findRemotePath(String path);
-
-    Process createProcess(NewCommandLine commandLine) throws ExecutionException;
+    Process createProcess(NewCommandLine commandLine, ProgressIndicator indicator) throws ExecutionException;
   }
 
   public interface RemoteEnvironmentRequest {
-    RemoteValue createPathValue(@NotNull String path);
+    RemotePlatform getRemotePlatform();
 
-    RemoteValue createPortValue(int port);
+    //TODO: change to Path
+    RemoteValue<String> createUpload(@NotNull String localPath);
+
+    RemoteValue<Integer> bindRemotePort(int remotePort);
+
+    RemoteLocation createRemoteLocation(@Nullable String remotePath/*, @Nullable Object __options*/);
+
+    RemoteLocation importLocation(RemoteLocation otherLoc, RemoteEnvironment originalEnv);
   }
 
-  public interface RemoteValue {
-    RemoteValue EMPTY_VALUE = e -> null;
+  public interface RemoteValue<T> {
 
-    //todo[remoteServers]: rename? it's easy to accidentally use toString() instead of toString(env)
-    @Nullable
-    String toString(@NotNull RemoteEnvironment environment);
-  }
+    RemoteValue EMPTY_VALUE = new RemoteValue() {
+      @Override
+      public Object getLocalValue() {
+        return null;
+      }
 
-  public static class StringRemoteValue implements RemoteValue {
-    private final String myString;
+      @Override
+      public Object getRemoteValue() {
+        return null;
+      }
+    };
 
-    public StringRemoteValue(@NotNull String string) {
-      myString = string;
+    static <V> RemoteValue<V> EMPTY_VALUE() {
+      //noinspection unchecked
+      return EMPTY_VALUE;
     }
 
-    @Nullable
+    //todo[remoteServers]: rename? it's easy to accidentally use toString() instead of toString(env)
+    //@Nullable
+    //String toString(@NotNull RemoteEnvironment environment);
+
+    T getLocalValue();
+
+    T getRemoteValue();
+  }
+
+  public interface RemoteLocation {
+    void dispose();
+
+    String getRemotePath();
+
+    //Path download(ProgressIndicator indicator);
+  }
+
+  public static class FixedValue<T> implements RemoteValue<T> {
+    private final T myValue;
+
+    public FixedValue(@NotNull T value) {
+      myValue = value;
+    }
+
     @Override
-    public String toString(@NotNull RemoteEnvironment environment) {
-      return myString;
+    public T getRemoteValue() {
+      return myValue;
+    }
+
+    @Override
+    public T getLocalValue() {
+      return myValue;
+    }
+  }
+
+  public static class StringFixedValue extends FixedValue<String> {
+
+    public StringFixedValue(@NotNull String string) {
+      super(string);
     }
   }
 
   public static class NewCommandLine {
-    private RemoteValue myExePath = RemoteValue.EMPTY_VALUE;
-    private RemoteValue myWorkingDirectory = RemoteValue.EMPTY_VALUE;
-    private final List<RemoteValue> myParameters = new ArrayList<>();
-    private final Map<String, RemoteValue> myEnvironment = new HashMap<>();
+    private RemoteValue<String> myExePath = RemoteValue.EMPTY_VALUE();
+    private RemoteValue<String> myWorkingDirectory = RemoteValue.EMPTY_VALUE();
+    private final List<RemoteValue<String>> myParameters = new ArrayList<>();
+    private final Map<String, RemoteValue<String>> myEnvironment = new HashMap<>();
 
     /**
      * {@link GeneralCommandLine#getPreparedCommandLine()}
      */
     public List<String> prepareCommandLine(@NotNull RemoteEnvironment target) {
-      String command = myExePath.toString(target);
+      String command = myExePath.getRemoteValue();
       if (command == null) {
         // todo: handle this properly
         throw new RuntimeException("Cannot find command");
@@ -78,24 +126,24 @@ public class IR {
       return CommandLineUtil.toCommandLine(command, getParameters(target), target.getRemotePlatform().getPlatform());
     }
 
-    public void setExePath(@NotNull RemoteValue exePath) {
+    public void setExePath(@NotNull RemoteValue<String> exePath) {
       myExePath = exePath;
     }
 
     public void setExePath(@NotNull String exePath) {
-      myExePath = new StringRemoteValue(exePath);
+      myExePath = new StringFixedValue(exePath);
     }
 
-    public void setWorkingDirectory(@NotNull RemoteValue workingDirectory) {
+    public void setWorkingDirectory(@NotNull RemoteValue<String> workingDirectory) {
       myWorkingDirectory = workingDirectory;
     }
 
-    public void addParameter(@NotNull RemoteValue parameter) {
+    public void addParameter(@NotNull RemoteValue<String> parameter) {
       myParameters.add(parameter);
     }
 
     public void addParameter(@NotNull String parameter) {
-      myParameters.add(new StringRemoteValue(parameter));
+      myParameters.add(new StringFixedValue(parameter));
     }
 
     public void addEnvironmentVariable(String name, RemoteValue value) {
@@ -103,27 +151,28 @@ public class IR {
     }
 
     public void addEnvironmentVariable(String name, String value) {
-      myEnvironment.put(name, new StringRemoteValue(value));
+      myEnvironment.put(name, new StringFixedValue(value));
     }
 
+    //todo: all `target`s are not used
     public String getExePath(@NotNull RemoteEnvironment target) {
-      return myExePath.toString(target);
+      return myExePath.getRemoteValue();
     }
 
     @Nullable
     public String getWorkingDirectory(@NotNull RemoteEnvironment target) {
-      return myWorkingDirectory.toString(target);
+      return myWorkingDirectory.getRemoteValue();
     }
 
     @NotNull
     public List<String> getParameters(@NotNull RemoteEnvironment target) {
-      return ContainerUtil.mapNotNull(myParameters, v -> v.toString(target));
+      return ContainerUtil.mapNotNull(myParameters, RemoteValue::getRemoteValue);
     }
 
     @NotNull
     public Map<String, String> getEnvironmentVariables(@NotNull RemoteEnvironment target) {
       return ContainerUtil.map2MapNotNull(myEnvironment.entrySet(), e -> {
-        String value = e.getValue().toString(target);
+        String value = e.getValue().getRemoteValue();
         return value != null ? Pair.create(e.getKey(), value) : null;
       });
     }
@@ -141,19 +190,34 @@ public class IR {
       return new RemoteEnvironmentRequest() {
 
         @Override
-        public RemoteValue createPathValue(@NotNull String path) {
-          return new StringRemoteValue(path);
+        public RemotePlatform getRemotePlatform() {
+          return LocalRunner.this.getRemotePlatform();
         }
 
         @Override
-        public RemoteValue createPortValue(int port) {
-          return e -> String.valueOf(port);
+        public RemoteValue<String> createUpload(@NotNull String localPath) {
+          return new StringFixedValue(localPath);
+        }
+
+        @Override
+        public RemoteValue<Integer> bindRemotePort(int remotePort) {
+          return new FixedValue<>(remotePort);
+        }
+
+        @Override
+        public RemoteLocation createRemoteLocation(@Nullable String remotePath) {
+          return null;
+        }
+
+        @Override
+        public RemoteLocation importLocation(RemoteLocation otherLoc, RemoteEnvironment originalEnv) {
+          return null;
         }
       };
     }
 
     @Override
-    public RemoteEnvironment prepareRemoteEnvironment(RemoteEnvironmentRequest request) {
+    public RemoteEnvironment prepareRemoteEnvironment(RemoteEnvironmentRequest request, ProgressIndicator indicator) {
       return new LocalRemoteEnvironment();
     }
   }
@@ -165,7 +229,7 @@ public class IR {
     }
 
     @Override
-    public Process createProcess(NewCommandLine commandLine) throws ExecutionException {
+    public Process createProcess(NewCommandLine commandLine, ProgressIndicator indicator) throws ExecutionException {
       return createGeneralCommandLine(commandLine).createProcess();
     }
 
