a4091e60515edc98bb159584fae327e22600ceb4
Aleksei Kniazev <aleksei.kniazev@jetbrains.com>
2019-07-02 15:56:08 +0300

    IDEA-CR-49176: function inline: remove docstring, keep the first comment, unless it's a type comment(PY-36714, PY-36491)
    GitOrigin-RevId: d7de37f5a7b3b62cc6015e2ff3dbdad5cafe3641

--- python/testData/refactoring/inlineFunction/keepingComments/main.py
+++ python/testData/refactoring/inlineFunction/keepingComments/main.py
@@ -1,6 +1,9 @@
 def foo():
     # comment on the first line
+    print(42)  # inline comment
+    # comment 1
     print(42)
+    # comment 2
     return 42
 
 
---
+++ python/testData/refactoring/inlineFunction/removingTypeComment/main.py
@@ -0,0 +1,7 @@
+def foo():
+    # type: () -> int
+    print(42)
+    return 42
+
+
+res = fo<caret>o()
