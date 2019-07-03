4ad79a27b2908f1a4c0dc60bd80c28a98a8384f6
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-07-03 17:35:21 +0200

    use editor.xml instead of editor.codeinsight.xml
    GitOrigin-RevId: d4aa2880440eefddd44798b45d1b30f431b82821

--- platform/editor-ui-ex/src/com/intellij/codeInsight/folding/CodeFoldingSettingsImpl.java
+++ platform/editor-ui-ex/src/com/intellij/codeInsight/folding/CodeFoldingSettingsImpl.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.folding;
 
 import com.intellij.openapi.components.PersistentStateComponent;
@@ -10,10 +10,10 @@ import org.jetbrains.annotations.NotNull;
 /**
  * @author yole
  */
-@State(
-  name = "CodeFoldingSettings",
-  storages = @Storage("editor.codeinsight.xml")
-)
+@State(name = "CodeFoldingSettings", storages = {
+  @Storage("editor.xml"),
+  @Storage(value = "editor.codeinsight.xml", deprecated = true),
+})
 public class CodeFoldingSettingsImpl extends CodeFoldingSettings implements PersistentStateComponent<CodeFoldingSettings> {
   @Override
   public CodeFoldingSettings getState() {
