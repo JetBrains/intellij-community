14f9f818a547a9c8ed556525553c4019e78ff578
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2019-07-03 17:18:15 +0200

    Dump newly created indices with LOG.debug (IDEA-217462)
    Complete list of indices being built is needed quite rarely.
    
    (cherry picked from commit c0e218f04c829a5076a493df92aa4a400d85673c)
    (cherry picked from commit b440de27f4b7d6ea7864e30fa5b70324629c27a8)
    
    GitOrigin-RevId: c0fb1646f044a89b8d729b474f28de569ec456c3

--- platform/lang-impl/src/com/intellij/util/indexing/FileBasedIndexImpl.java
+++ platform/lang-impl/src/com/intellij/util/indexing/FileBasedIndexImpl.java
@@ -351,7 +351,7 @@ public class FileBasedIndexImpl extends FileBasedIndex implements BaseComponent,
         versionChanged = true;
         LOG.info("Version has changed for index " + name + ". The index will be rebuilt.");
       } else {
-        LOG.info("Index " + name + " will be built.");
+        LOG.debug("Index " + name + " will be built.");
       }
       if (extension.hasSnapshotMapping() && versionChanged) {
         FileUtil.deleteWithRenaming(IndexInfrastructure.getPersistentIndexRootDir(name));
