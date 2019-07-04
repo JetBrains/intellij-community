b9acfbb5c24782b1cd71142eaf2b9d8ad592ad3d
anstarovoyt <andrey.starovoyt@jetbrains.com>
2019-07-04 11:41:31 +0300

    html: restore old method
    GitOrigin-RevId: abf8c5ee84e60883f0f1f89b58afee2a7f6dd285

--- xml/xml-psi-impl/src/com/intellij/xml/XmlExtension.java
+++ xml/xml-psi-impl/src/com/intellij/xml/XmlExtension.java
@@ -18,6 +18,7 @@ import com.intellij.psi.xml.XmlDocument;
 import com.intellij.psi.xml.XmlFile;
 import com.intellij.psi.xml.XmlTag;
 import com.intellij.xml.util.XmlUtil;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -154,6 +155,14 @@ public abstract class XmlExtension {
 
   public boolean shouldBeInserted(final XmlAttributeDescriptor descriptor) {
     return descriptor.isRequired();
+  }
+
+  @SuppressWarnings("unused") //for binary compatibility
+  @ApiStatus.ScheduledForRemoval(inVersion = "2019.3")
+  public AttributeValuePresentation getAttributeValuePresentation(@Nullable XmlAttributeDescriptor descriptor,
+                                                                  @NotNull String defaultAttributeQuote,
+                                                                  @NotNull PsiElement context) { 
+    return getAttributeValuePresentation(null, "", defaultAttributeQuote);
   }
 
   @NotNull
