f20c2f6fe6358e540e84e4016e5e8f3ca44cb86a
Dmitry Jemerov <yole@jetbrains.com>
2019-07-05 11:46:51 +0200

    Smart keys configurable for HTML/XML/CSS converted to UI DSL
    GitOrigin-RevId: 26f07dfd028a66b93e7e2dead2ee98d78d842156

---
+++ xml/impl/src/com/intellij/application/options/editor/WebSmartKeysConfigurable.kt
@@ -0,0 +1,70 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.application.options.editor
+
+import com.intellij.openapi.options.BoundConfigurable
+import com.intellij.openapi.ui.DialogPanel
+import com.intellij.ui.layout.*
+import com.intellij.xml.XmlBundle
+
+private val model = WebEditorOptions.getInstance()
+val myAutomaticallyInsertClosingTagCheckBox = CheckboxDescriptor("Insert closing tag on tag completion",
+                                                                 PropertyBinding(model::isAutomaticallyInsertClosingTag,
+                                                                                 model::setAutomaticallyInsertClosingTag))
+val myAutomaticallyInsertRequiredAttributesCheckBox = CheckboxDescriptor("Insert required attributes on tag completion",
+                                                                         PropertyBinding(model::isAutomaticallyInsertClosingTag,
+                                                                                         model::setAutomaticallyInsertClosingTag))
+val myAutomaticallyInsertRequiredSubTagsCheckBox = CheckboxDescriptor("Insert required subtags on tag completion",
+                                                                      PropertyBinding(model::isAutomaticallyInsertClosingTag,
+                                                                                      model::setAutomaticallyInsertClosingTag))
+val myAutomaticallyStartAttributeAfterCheckBox = CheckboxDescriptor("Start attribute on tag completion",
+                                                                    PropertyBinding(model::isAutomaticallyStartAttribute,
+                                                                                    model::setAutomaticallyStartAttribute))
+val myAddQuotasForAttributeValue = CheckboxDescriptor("Add quotes for attribute value on typing '=' and attribute completion",
+                                                      PropertyBinding(model::isInsertQuotesForAttributeValue,
+                                                                      model::setInsertQuotesForAttributeValue))
+val myAutoCloseTagCheckBox = CheckboxDescriptor("Auto-close tag on typing '</'",
+                                                PropertyBinding(model::isAutoCloseTag, model::setAutoCloseTag))
+val mySyncTagEditing = CheckboxDescriptor("Simultaneous '<tag></tag>' editing",
+                                          PropertyBinding(model::isSyncTagEditing, model::setSyncTagEditing))
+val mySelectWholeCssIdentifierOnDoubleClick = CheckboxDescriptor("Select whole CSS identifiers on double click",
+                                                                 PropertyBinding(model::isSelectWholeCssIdentifierOnDoubleClick,
+                                                                                 model::setSelectWholeCssIdentifierOnDoubleClick))
+
+class WebSmartKeysConfigurable(val model: WebEditorOptions) : BoundConfigurable("Web") {
+  override fun createPanel(): DialogPanel {
+    return panel {
+      row {
+        titledRow(XmlBundle.message("xml.editor.options.misc.title")) {
+          row {
+            checkBox(myAutomaticallyInsertClosingTagCheckBox)
+          }
+          row {
+            checkBox(myAutomaticallyInsertRequiredAttributesCheckBox)
+          }
+          row {
+            checkBox(myAutomaticallyInsertRequiredSubTagsCheckBox)
+          }
+          row {
+            checkBox(myAutomaticallyStartAttributeAfterCheckBox)
+          }
+          row {
+            checkBox(myAddQuotasForAttributeValue)
+          }
+          row {
+            checkBox(myAutoCloseTagCheckBox)
+          }
+          row {
+            checkBox(mySyncTagEditing)
+          }
+        }
+      }
+      row {
+        titledRow("CSS") {
+          row {
+            checkBox(mySelectWholeCssIdentifierOnDoubleClick)
+          }
+        }
+      }
+    }
+  }
+}
