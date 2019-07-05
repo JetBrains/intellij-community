17390ac32da9194ee0daa2828468eef63a2b1dfe
Dmitry Jemerov <yole@jetbrains.com>
2019-07-05 12:27:42 +0200

    Merge PySmartKeysOptions and PySmartKeysOptions as there's no reason for them to be separate any more
    GitOrigin-RevId: c0dc8b347e7f89a5f76c79aab8db5839e95c0941

--- python/src/com/jetbrains/python/codeInsight/PySmartKeysOptions.java
+++ python/src/com/jetbrains/python/codeInsight/PySmartKeysOptions.java
@@ -17,16 +17,29 @@ package com.jetbrains.python.codeInsight;
 
 import com.intellij.codeInsight.CodeInsightSettings;
 import com.intellij.openapi.options.BeanConfigurable;
-import com.intellij.openapi.options.UnnamedConfigurable;
+import com.intellij.openapi.options.Configurable;
+import com.jetbrains.python.PyBundle;
+import org.jetbrains.annotations.Nls;
 
 /**
  * @author yole
  */
-public class PySmartKeysOptions extends BeanConfigurable<CodeInsightSettings> implements UnnamedConfigurable {
+public class PySmartKeysOptions extends BeanConfigurable<CodeInsightSettings> implements Configurable {
   public PySmartKeysOptions() {
     super(CodeInsightSettings.getInstance());
     //CodeInsightSettings.getInstance().REFORMAT_ON_PASTE = CodeInsightSettings.NO_REFORMAT;   //TODO: remove combobox from settings
-    CodeInsightSettings settings = getInstance();
-    checkBox("Smart indent pasted lines", ()->settings.INDENT_TO_CARET_ON_PASTE, v->settings.INDENT_TO_CARET_ON_PASTE=v);
+    CodeInsightSettings commonSettings = CodeInsightSettings.getInstance();
+    checkBox("Smart indent pasted lines", ()->commonSettings.INDENT_TO_CARET_ON_PASTE, v->commonSettings.INDENT_TO_CARET_ON_PASTE=v);
+
+    PyCodeInsightSettings settings = PyCodeInsightSettings.getInstance();
+    checkBox(PyBundle.message("smartKeys.insert.backslash.in.statement.on.enter"), ()->settings.INSERT_BACKSLASH_ON_WRAP, v-> settings.INSERT_BACKSLASH_ON_WRAP=v);
+    checkBox(PyBundle.message("smartKeys.insert.self.in.method"), ()->settings.INSERT_SELF_FOR_METHODS, v->settings.INSERT_SELF_FOR_METHODS=v);
+    checkBox(PyBundle.message("smartKeys.insert.type.placeholder.in.docstring.stub"), ()->settings.INSERT_TYPE_DOCSTUB, v->settings.INSERT_TYPE_DOCSTUB=v);
+  }
+
+  @Nls(capitalization = Nls.Capitalization.Title)
+  @Override
+  public String getDisplayName() {
+    return "Python";
   }
 }
