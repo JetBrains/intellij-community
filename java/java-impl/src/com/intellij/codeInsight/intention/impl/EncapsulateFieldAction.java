b1b2e9a35eb7dbe5ad7692504845d0ad1a9b0604
Max Medvedev <maxim.medvedev@jetbrains.com>
2019-07-07 13:23:36 +0300

    IDEA-217722 Encapsulate field: relax intention availability
    don't restrict the intention to qualified references of non-final public fields. Suggest it everywhere
    
    GitOrigin-RevId: b2dff8f21ea551feac9366dc935cfcc861a1643b

--- java/java-impl/src/com/intellij/codeInsight/intention/impl/EncapsulateFieldAction.java
+++ java/java-impl/src/com/intellij/codeInsight/intention/impl/EncapsulateFieldAction.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.intention.impl;
 
 import com.intellij.codeInsight.CodeInsightBundle;
@@ -44,12 +30,11 @@ public class EncapsulateFieldAction extends BaseRefactoringIntentionAction {
 
   @Override
   public boolean isAvailable(@NotNull Project project, Editor editor, @NotNull PsiElement element) {
-    if (element instanceof SyntheticElement){
+    if (element instanceof SyntheticElement) {
       return false;
     }
 
-    final PsiField field = getField(element);
-    return field != null && !field.hasModifierProperty(PsiModifier.FINAL) && !field.hasModifierProperty(PsiModifier.PRIVATE);
+    return getField(element) != null;
   }
 
   @Override
@@ -74,16 +59,9 @@ public class EncapsulateFieldAction extends BaseRefactoringIntentionAction {
       return null;
     }
     final PsiReferenceExpression ref = (PsiReferenceExpression)parent;
-    final PsiExpression qualifier = ref.getQualifierExpression();
-    if (qualifier == null || qualifier instanceof PsiThisExpression) {
-      return null;
-    }
 
     final PsiElement resolved = ref.resolve();
-    if (!(resolved instanceof PsiField)) {
-      return null;
-    }
-    return (PsiField)resolved;
+    return resolved instanceof PsiField ? (PsiField)resolved : null;
   }
 
   @Override
