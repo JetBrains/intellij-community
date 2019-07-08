d57fb7c59d18e6293b4a7823b3f6d9efa66256d3
Anton Spilnyy <anton.spilnyy@jetbrains.com>
2019-07-08 13:42:07 +0200

    [idea plugin] [automation] dont' add logFactory then running tests
    GitOrigin-RevId: 05a276d017034fbac60d3837a8ad3530f2ed35ea

--- plugins/space/src/main/kotlin/circlet/components/CircletApplicationComponent.kt
+++ plugins/space/src/main/kotlin/circlet/components/CircletApplicationComponent.kt
@@ -5,13 +5,17 @@ import circlet.client.api.impl.*
 import circlet.klogging.impl.*
 import circlet.platform.api.serialization.*
 import circlet.runtime.*
+import com.intellij.openapi.application.*
 import com.intellij.openapi.components.*
 import libraries.klogging.KLoggerStaticFactory
 import runtime.*
 
 class CircletApplicationComponent : ApplicationComponent {
     init {
-        KLoggerStaticFactory.customFactory = KLoggerFactoryIdea
+        val application = ApplicationManager.getApplication()
+        if (!application.isUnitTestMode && !application.isHeadlessEnvironment) {
+            KLoggerStaticFactory.customFactory = KLoggerFactoryIdea
+        }
 
         mutableUiDispatch = ApplicationUiDispatch
 
