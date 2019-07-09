112c150226bbb7a65ef2aefee4c8b466a1254772
graann <Anna.Gromova@jetbrains.com>
2019-07-09 14:24:40 +0300

    Project name and file path is not shown on window title bar after reopening project if "Main menu" is switched off (#IDEA-217563)
    (cherry picked from commit 7aab12dd3bd9373554e7ed5376101bd4f5ff0545)
    
    GitOrigin-RevId: 78c358d41c86bac67d607a5b43608c25f498e9b8

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/customFrameDecorations/header/MainFrameHeader.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/customFrameDecorations/header/MainFrameHeader.kt
@@ -4,6 +4,7 @@ package com.intellij.openapi.wm.impl.customFrameDecorations.header
 import com.intellij.ide.DataManager
 import com.intellij.ide.ui.UISettings
 import com.intellij.ide.ui.UISettingsListener
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.actionSystem.ex.ActionManagerEx
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
@@ -15,77 +16,58 @@ import com.intellij.util.ui.JBUI
 import net.miginfocom.swing.MigLayout
 import java.awt.Frame
 import java.awt.Rectangle
-import java.awt.event.WindowAdapter
-import java.awt.event.WindowEvent
 import java.util.*
 import javax.swing.JFrame
 import javax.swing.JPanel
+import javax.swing.SwingUtilities
 import javax.swing.border.Border
 import javax.swing.event.ChangeListener
 
 class MainFrameHeader(frame: JFrame) : FrameHeader(frame){
-  private var mySelectedEditorFilePath: CustomDecorationPath
+  private val mySelectedEditorFilePath: CustomDecorationPath
   private val myIdeMenu: IdeMenuBar
   private var changeListener: ChangeListener
+
+  private val mainMenuUpdater: UISettingsListener
+
+  private var disposable: Disposable? = null
 
   init {
     layout = MigLayout("novisualpadding, fillx, ins 0, gap 0, top", "$H_GAP[pref!]$H_GAP[][pref!]")
     add(productIcon)
 
-    val frameDisposer = Disposer.newDisposable()
-    Disposer.register(ApplicationManager.getApplication(), frameDisposer)
-    frame.addWindowListener(object: WindowAdapter() {
-      override fun windowClosed(e: WindowEvent?) {
-        Disposer.dispose(frameDisposer)
-      }
-    })
     myIdeMenu = object : IdeMenuBar(ActionManagerEx.getInstanceEx(), DataManager.getInstance()) {
       override fun getBorder(): Border? {
         return JBUI.Borders.empty()
       }
     }
 
-    val pane = JPanel()
-    pane.isOpaque = false
-    setCustomFrameTopBorder({ myState != Frame.MAXIMIZED_VERT && myState != Frame.MAXIMIZED_BOTH }, {true})
-    mySelectedEditorFilePath = CustomDecorationPath(frame)
-    relayoutFrameHeader(pane, true)
-
-    val mainMenuUpdater = UISettingsListener {
-      if (myIdeMenu.parent == null && UISettings.instance.showMainMenu) {
-        relayoutFrameHeader(pane, true)
-      }
-      else if (!UISettings.instance.showMainMenu) {
-        relayoutFrameHeader(pane, false)
-      }
+    mainMenuUpdater = UISettingsListener {
+      myIdeMenu.isVisible = UISettings.instance.showMainMenu
+      SwingUtilities.invokeLater { updateCustomDecorationHitTestSpots() }
     }
-    ApplicationManager.getApplication().messageBus.connect(frameDisposer).subscribe(UISettingsListener.TOPIC, mainMenuUpdater)
-    mainMenuUpdater.uiSettingsChanged(UISettings.instance)
-
 
     changeListener = ChangeListener {
       updateCustomDecorationHitTestSpots()
     }
+
+    mySelectedEditorFilePath = CustomDecorationPath(frame)
+
+    val menuHolder = JPanel(MigLayout("fill, ins 0, novisualpadding, hidemode 2", "[pref!][]"))
+
+    menuHolder.isOpaque = false
+    menuHolder.add(myIdeMenu, "wmin 0, w pref!, top, growy")
+    menuHolder.add(mySelectedEditorFilePath.getView(), "left, growx, wmin 0, gapafter $H_GAP, gapbottom 1")
+
+    add(menuHolder, "wmin 0, grow")
+    add(buttonPanes.getView(), "top, wmin pref")
+
+    setCustomFrameTopBorder({ myState != Frame.MAXIMIZED_VERT && myState != Frame.MAXIMIZED_BOTH }, {true})
+    myIdeMenu.isVisible = UISettings.instance.showMainMenu
   }
 
   fun setProject(project: Project) {
     mySelectedEditorFilePath.setProject(project)
-  }
-
-  private fun relayoutFrameHeader(pane: JPanel, showMainMenu: Boolean) {
-    pane.layout = if(showMainMenu) {
-      MigLayout("fill, ins 0, novisualpadding", "[pref!][]")
-    } else MigLayout("fill, ins 0, novisualpadding", "[]")
-
-    removeAll()
-    add(productIcon)
-    pane.removeAll()
-    if (showMainMenu) {
-      pane.add(myIdeMenu, "wmin 0, wmax pref, top, growy")
-    }
-    pane.add(mySelectedEditorFilePath.getView(), "center, growx, wmin 0, gapafter $H_GAP, gapbottom 1")
-    add(pane, "wmin 0, grow")
-    add(buttonPanes.getView(), "top, wmin pref")
   }
 
   override fun updateActive() {
@@ -95,26 +77,42 @@ class MainFrameHeader(frame: JFrame) : FrameHeader(frame){
 
   override fun installListeners() {
     myIdeMenu.selectionModel.addChangeListener(changeListener)
+    val disp = Disposer.newDisposable()
+    Disposer.register(ApplicationManager.getApplication(), disp)
+
+    ApplicationManager.getApplication().messageBus.connect(disp).subscribe(UISettingsListener.TOPIC, mainMenuUpdater)
+    mainMenuUpdater.uiSettingsChanged(UISettings.instance)
+    disposable = disp
+
     super.installListeners()
   }
 
   override fun uninstallListeners() {
     myIdeMenu.selectionModel.removeChangeListener(changeListener)
+    disposable?.let {
+      if (!Disposer.isDisposed(it))
+        Disposer.dispose(it)
+      disposable = null
+    }
+
     super.uninstallListeners()
   }
 
   override fun getHitTestSpots(): ArrayList<RelativeRectangle> {
     val hitTestSpots = super.getHitTestSpots()
-    val menuRect = Rectangle(myIdeMenu.size)
 
-    val state = frame.extendedState
-    if (state != Frame.MAXIMIZED_VERT && state != Frame.MAXIMIZED_BOTH) {
+    if(myIdeMenu.isVisible) {
+      val menuRect = Rectangle(myIdeMenu.size)
+
+      val state = frame.extendedState
+      if (state != Frame.MAXIMIZED_VERT && state != Frame.MAXIMIZED_BOTH) {
         val topGap = Math.round((menuRect.height / 3).toFloat())
         menuRect.y += topGap
-        menuRect.height -=topGap
+        menuRect.height -= topGap
+      }
+      hitTestSpots.add(RelativeRectangle(myIdeMenu, menuRect))
     }
 
-    hitTestSpots.add(RelativeRectangle(myIdeMenu, menuRect))
     hitTestSpots.addAll(mySelectedEditorFilePath.getListenerBounds())
 
     return hitTestSpots
