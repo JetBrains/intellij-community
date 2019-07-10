956d70a30cfeca774728f5f3e4f32c097feef974
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2019-07-10 17:31:34 +0200

    WEB-39865 Vue.js: IDE hangs when fixing formatting of particular vue.js code
    GitOrigin-RevId: dcfdae3cef235dab7af504b9b8551d48869ed2eb

--- xml/impl/src/com/intellij/lang/html/HtmlFormattingModelBuilder.java
+++ xml/impl/src/com/intellij/lang/html/HtmlFormattingModelBuilder.java
@@ -21,28 +21,24 @@ package com.intellij.lang.html;
 
 import com.intellij.formatting.FormattingModel;
 import com.intellij.formatting.FormattingModelBuilder;
-import com.intellij.lang.ASTNode;
-import com.intellij.openapi.util.TextRange;
+import com.intellij.lang.xml.XmlFormattingModel;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
 import com.intellij.psi.formatter.FormattingDocumentModelImpl;
 import com.intellij.psi.formatter.xml.HtmlPolicy;
 import com.intellij.psi.formatter.xml.XmlBlock;
-import com.intellij.psi.impl.source.SourceTreeToPsiMap;
-import com.intellij.lang.xml.XmlFormattingModel;
 import org.jetbrains.annotations.NotNull;
 
 public class HtmlFormattingModelBuilder implements FormattingModelBuilder {
   @Override
   @NotNull
-    public FormattingModel createModel(final PsiElement element, final CodeStyleSettings settings) {
+  public FormattingModel createModel(final PsiElement element, final CodeStyleSettings settings) {
     final PsiFile psiFile = element.getContainingFile();
     final FormattingDocumentModelImpl documentModel = FormattingDocumentModelImpl.createOn(psiFile);
     return new XmlFormattingModel(psiFile,
-                                                           new XmlBlock(SourceTreeToPsiMap.psiElementToTree(psiFile),
-                                                                        null, null, new HtmlPolicy(settings, documentModel), null, null, false),
-                                                           documentModel);
+                                  new XmlBlock(element.getNode(),
+                                               null, null, new HtmlPolicy(settings, documentModel), null, null, false),
+                                  documentModel);
   }
-
 }
