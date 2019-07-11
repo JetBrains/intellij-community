a1020660f00e1a47d4738ea99088d5fd939fa161
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2019-07-11 17:56:29 +0700

    IDEA-CR-49994: DfaBoxedValue.Factory.createBoxed: do not reuse qualifier box if type differs
    Fixes IDEA-217880 Long.equals((long) Integer) reported as "always 'false'"
    
    (cherry picked from commit c3fbbbf4528cd66041e9e157a493cd6082b78c46)
    
    GitOrigin-RevId: 0172cbae80893e22becd31af85581f31cf3ce3c4

--- java/java-tests/testData/inspection/dataFlow/fixture/NewBoxedNumberEquality.java
+++ java/java-tests/testData/inspection/dataFlow/fixture/NewBoxedNumberEquality.java
@@ -2,6 +2,12 @@ import java.util.*;
 
 class Testcase {
 
+  void intToLong() {
+    Integer i = 2;
+    Long l = 2L;
+    System.out.println(<warning descr="Result of 'l.equals((long) i)' is always 'true'">l.equals((long) i)</warning>);
+  }
+
   private static final Long TEST = new Long(1234);
 
   public static void main(String[] args) {
