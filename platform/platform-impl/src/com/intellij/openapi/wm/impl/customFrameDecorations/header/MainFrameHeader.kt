9bc66277898b76c36ef651cfa347d3dc6d4a028c
graann <anna.gromova@jetbrains.com>
2019-07-12 14:46:45 +0300

    Borderless UI: Use TitlePane.background only (#IDEA-211054)
    GitOrigin-RevId: 08229036f709ae654bfa681da19d4ed9fb6831b4

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/customFrameDecorations/header/MainFrameHeader.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/customFrameDecorations/header/MainFrameHeader.kt
@@ -1,29 +1,22 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.wm.impl.customFrameDecorations.header
 
-import com.intellij.ide.DataManager
 import com.intellij.ide.ui.UISettings
 import com.intellij.ide.ui.UISettingsListener
 import com.intellij.openapi.Disposable
-import com.intellij.openapi.actionSystem.ActionGroup
-import com.intellij.openapi.actionSystem.ex.ActionManagerEx
-import com.intellij.openapi.actionSystem.impl.ActionMenu
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.wm.impl.IdeMenuBar
 import com.intellij.openapi.wm.impl.customFrameDecorations.header.titleLabel.CustomDecorationPath
 import com.intellij.ui.awt.RelativeRectangle
-import com.intellij.util.ui.JBUI
 import net.miginfocom.swing.MigLayout
 import java.awt.Frame
-import java.awt.Graphics
 import java.awt.Rectangle
 import java.util.*
 import javax.swing.JFrame
 import javax.swing.JPanel
 import javax.swing.SwingUtilities
-import javax.swing.border.Border
 import javax.swing.event.ChangeListener
 
 class MainFrameHeader(frame: JFrame) : FrameHeader(frame){
@@ -39,23 +32,7 @@ class MainFrameHeader(frame: JFrame) : FrameHeader(frame){
     layout = MigLayout("novisualpadding, fillx, ins 0, gap 0, top", "$H_GAP[pref!]$H_GAP[][pref!]")
     add(productIcon)
 
-    myIdeMenu = object : IdeMenuBar(ActionManagerEx.getInstanceEx(), DataManager.getInstance()) {
-      override fun getBorder(): Border? {
-        return JBUI.Borders.empty()
-      }
-
-      override fun paintBackground(g: Graphics?) {
-
-      }
-
-      override fun createActionMenu(enableMnemonics: Boolean, isDarkMenu: Boolean, action: ActionGroup?): ActionMenu {
-        val actionMenu = super.createActionMenu(enableMnemonics, isDarkMenu, action)
-
-        return actionMenu
-      }
-    }.apply {
-      isOpaque = false
-    }
+    myIdeMenu = CustomHeaderMenuBar()
 
     mainMenuUpdater = UISettingsListener {
       myIdeMenu.isVisible = UISettings.instance.showMainMenu
