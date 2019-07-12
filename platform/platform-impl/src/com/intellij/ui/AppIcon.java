1e77cde4f6a9a0db4635f907e6aae92ddb52d201
Anton Tarasov <anton.tarasov@jetbrains.com>
2019-07-12 18:35:55 +0300

    IDEA-217895 DND in trees on macOS shows doubled image on HiDPI
    GitOrigin-RevId: 3d9729aeb0bec2af135bc2b628e252338a7ea92f

--- platform/platform-impl/src/com/intellij/ui/AppIcon.java
+++ platform/platform-impl/src/com/intellij/ui/AppIcon.java
@@ -9,15 +9,13 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.SystemInfo;
-import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.wm.AppIconScheme;
 import com.intellij.openapi.wm.IdeFrame;
 import com.intellij.openapi.wm.WindowManager;
 import com.intellij.util.IconUtil;
-import com.intellij.util.MethodInvocator;
 import com.intellij.util.ui.ImageUtil;
-import com.intellij.util.ui.ImageUtil.MultiResolutionImageWrapper;
+import com.intellij.util.ui.MultiResolutionImageProvider;
 import com.intellij.util.ui.UIUtil;
 import com.sun.jna.platform.win32.WinDef;
 import org.apache.commons.imaging.common.BinaryOutputStream;
@@ -172,8 +170,8 @@ public abstract class AppIcon {
         if (appImage == null) return null;
 
         // [tav] expecting two resolution variants for the dock icon: 128x128, 256x256
-        if (MultiResolutionImageWrapper.isMultiResolutionImage(appImage)) {
-          List<Image> variants = MultiResolutionImageWrapper.wrap(appImage).getResolutionVariants();
+        if (MultiResolutionImageProvider.isMultiResolutionImage(appImage)) {
+          List<Image> variants = MultiResolutionImageProvider.getAccessor(appImage).getResolutionVariants();
           int width = appImage.getWidth(null);
           for (Image img : variants) {
               if (img.getWidth(null) > width) {
