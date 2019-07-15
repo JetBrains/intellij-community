42bcbb02e28ae741114a3ff679132f34d3fd3a03
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-07-15 21:07:16 +0200

    remove unused EclipseIcons
    GitOrigin-RevId: 073deb43d3d48ca1cdaebec6703deb6596f34f87

--- plugins/eclipse/src/org/jetbrains/idea/eclipse/importWizard/EclipseImportBuilder.java
+++ plugins/eclipse/src/org/jetbrains/idea/eclipse/importWizard/EclipseImportBuilder.java
@@ -1,21 +1,8 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.eclipse.importWizard;
 
 import com.intellij.application.options.CodeStyle;
+import com.intellij.icons.AllIcons;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
@@ -59,7 +46,6 @@ import com.intellij.projectImport.ProjectImportBuilder;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
 import gnu.trove.THashMap;
 import gnu.trove.THashSet;
-import icons.EclipseIcons;
 import org.jdom.Element;
 import org.jdom.JDOMException;
 import org.jetbrains.annotations.NotNull;
@@ -103,7 +89,7 @@ public class EclipseImportBuilder extends ProjectImportBuilder<String> implement
 
   @Override
   public Icon getIcon() {
-    return EclipseIcons.Eclipse;
+    return AllIcons.Providers.Eclipse;
   }
 
   @Override
@@ -445,7 +431,7 @@ public class EclipseImportBuilder extends ProjectImportBuilder<String> implement
       final FileChooserDescriptor fileChooserDescriptor = new FileChooserDescriptor(false, true, false, false, false, false) {
         @Override
         public Icon getIcon(final VirtualFile file) {
-          return looksLikeEclipse(file) ? dressIcon(file, EclipseIcons.Eclipse) : super.getIcon(file);
+          return looksLikeEclipse(file) ? dressIcon(file, AllIcons.Providers.Eclipse) : super.getIcon(file);
         }
 
         private boolean looksLikeEclipse(final VirtualFile file) {
