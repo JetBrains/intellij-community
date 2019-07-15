03040ec17aee87d9b0a5b7f7cfaf6e778a7abf2a
Dmitriy.Panov <Dmitriy.Panov@jetbrains.com>
2019-07-15 16:03:29 +0300

    IDEA-CR-50152: executing Toolbox Lite-Gen on JBR11
    (cherry picked from commit 6a1ded65ec65961ddde5e117d9e2fc16cf78fff2)
    
    GitOrigin-RevId: 3315f6785963a44f2183634428783c12cc0d5877

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/BuildOptions.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/BuildOptions.groovy
@@ -141,7 +141,8 @@ class BuildOptions {
   /**
    * Specifies Jetbrains JDK version to be used by build scripts, 8 by default.
    */
-  int jdkVersion = System.getProperty("intellij.build.jdk.version", "8").toInteger()
+  static final String JDK_VERSION_OPTION = "intellij.build.jdk.version"
+  int jdkVersion = System.getProperty(JDK_VERSION_OPTION, "8").toInteger()
 
   /**
    * Specifies an algorithm to build distribution checksums.
