6978f10765125477beebe42ddaf8c980787d68c6
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-07-15 21:28:51 +0200

    IDEA-218345 Deadlock while loading AllIcons' subclasses because of circular references
    (cherry picked from commit 8349ce5d0aad5a41ccc000e573ec232189c4e5bd)
    (cherry picked from commit 36129f81d54f799ba43d69c0dcc2353f58cfa8fc)
    
    GitOrigin-RevId: a5d89e0fcf54ba4fc0a26d874eabf91cac237d79

--- platform/build-scripts/icons/src/org/jetbrains/intellij/build/images/ImageCollector.kt
+++ platform/build-scripts/icons/src/org/jetbrains/intellij/build/images/ImageCollector.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.intellij.build.images
 
 import com.intellij.openapi.util.io.FileUtil
@@ -281,7 +281,7 @@ internal class ImageCollector(private val projectHome: Path, private val iconsOn
               val replacement = replacementString?.substringAfter('@')?.trim()
               val replacementContextClazz = StringUtil.nullize(replacementString?.substringBefore('@', "")?.trim())
 
-              val deprecatedData = DeprecationData(comment, replacement, replacementContextClazz, replacementReference = computeReplacementReference(comment))
+              val deprecatedData = DeprecationData(comment, replacement, replacementContextClazz, replacementReference = null)
               answer.deprecated.add(DeprecatedEntry(compilePattern(dir, root, pattern), deprecatedData))
 
               if (!pattern.contains('*') && !pattern.startsWith('/')) {
