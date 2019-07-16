6e61c7f3c321ddb9d8cafdc422de3b52ed7081bb
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2019-07-16 18:27:10 +0700

    IDEA-CR-50178: IDEA-216810 Unexpected behaviour with "Transform Method to Single Exit Point" / Inline refactoring
    (cherry picked from commit 31a77ebb86f3da40caeda5e8ca60ccec51ebfb13)
    
    GitOrigin-RevId: 8d41184c5c264da494a826979cb3a2659ca2f8a1

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/convertToSingleReturn/afterTwoBranchesDefault.java
@@ -0,0 +1,12 @@
+// "Transform body to single exit-point form" "true"
+class Test {
+    private String nameByIndex(int colourIndex) {
+        String result = "Blue";
+        if (colourIndex == 1) {
+            result = "Red";
+        } else if (colourIndex == 2) {
+            result = "Green";
+        }
+        return result;
+    }
+}
