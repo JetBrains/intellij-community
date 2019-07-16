f404d1c5ced03f0f7294f835777e315fad0e19b8
Konstantin Bulenkov <kb@jetbrains.com>
2019-07-16 18:21:49 +0200

    Cleanup after review IDEA-CR-50080
    GitOrigin-RevId: 95e6a1fbdd40d48e1fc952aaa7c11c9e1be8bfd3

--- platform/platform-impl/src/com/intellij/internal/statistic/collectors/fus/ui/UiInfoUsageCollector.java
+++ platform/platform-impl/src/com/intellij/internal/statistic/collectors/fus/ui/UiInfoUsageCollector.java
@@ -50,8 +50,8 @@ public class UiInfoUsageCollector extends ApplicationUsagesCollector {
     set.add(new UsageDescriptor("recent.files", 1, new FeatureUsageData().addData("grouped", recentPeriod(recent()))));
 
     UISettings ui = UISettings.getInstance();
-    add(set, "Show Editor Tabs In Single Row", ui.getScrollTabLayoutInEditor() ? 1 : 0);
-    add(set, "Hide Editor Tabs If Needed", ui.getScrollTabLayoutInEditor() && ui.getHideTabsIfNeeded() ? 1 : 0);
+    add(set, "Show.Editor.Tabs.In.Single.Row", ui.getScrollTabLayoutInEditor() ? 1 : 0);
+    add(set, "Hide.Editor.Tabs.If.Needed", ui.getScrollTabLayoutInEditor() && ui.getHideTabsIfNeeded() ? 1 : 0);
     add(set, "Block.cursor", EditorSettingsExternalizable.getInstance().isBlockCursor() ? 1 : 0);
     add(set, "Line.Numbers", EditorSettingsExternalizable.getInstance().isLineNumbersShown() ? 1 : 0);
     add(set, "Gutter.Icons", EditorSettingsExternalizable.getInstance().areGutterIconsShown() ? 1 : 0);
