d8b4f6b62aa2ea1ca2b38b9b5ddd94854fbab4c9
Alexander Sedov <alexander.sedov@jetbrains.com>
2019-07-17 16:04:58 +0300

    Fixed app-idea job execution provider's storage
    GitOrigin-RevId: 358bff8e98457e924685866c95a03c0f2dc52313

--- plugins/space/src/main/kotlin/circlet/plugins/pipelines/services/execution/CircletIdeaAutomationGraphStorage.kt
+++ plugins/space/src/main/kotlin/circlet/plugins/pipelines/services/execution/CircletIdeaAutomationGraphStorage.kt
@@ -3,11 +3,11 @@ package circlet.plugins.pipelines.services.execution
 import circlet.pipelines.config.api.*
 import circlet.pipelines.engine.api.storage.*
 
-class CircletIdeaAutomationGraphStorage(internal val task: ProjectTask) : AutomationGraphStorage<GraphStorageTransaction> {
+class CircletIdeaAutomationGraphStorage(internal val task: ProjectTask) : AutomationGraphStorage<CircletIdeaGraphStorageTransaction> {
     internal val idStorage = TaskLongIdStorage()
     internal val storedExecutions = mutableMapOf<Long, AJobExecutionEntity<*>>()
 
-    override suspend fun <T> invoke(body: (GraphStorageTransaction) -> T): T {
+    override suspend fun <T> invoke(body: (CircletIdeaGraphStorageTransaction) -> T): T {
         val tx = CircletIdeaGraphStorageTransaction(this)
         val res = body(tx)
         tx.executeAfterTransactionHooks()
