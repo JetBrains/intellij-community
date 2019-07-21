e77145a64d7d4367b85867c14a8c240cb4569e85
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-07-21 20:27:44 +0200

    [java] diagnostics for EA-208028
    GitOrigin-RevId: 9ab37816ab410faed4ec33ac0fd29b36d007683b

--- java/java-psi-impl/src/com/intellij/psi/impl/PsiElementFactoryImpl.java
+++ java/java-psi-impl/src/com/intellij/psi/impl/PsiElementFactoryImpl.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.impl;
 
 import com.intellij.lang.*;
@@ -181,7 +181,7 @@ public class PsiElementFactoryImpl extends PsiJavaParserFacadeImpl implements Ps
       return createMethodFromText(builder.toString(), null).getTypeParameters()[0];
     }
     catch (RuntimeException e) {
-      throw new IncorrectOperationException("type parameter text: " + builder.toString());
+      throw new IncorrectOperationException("type parameter text: " + builder.toString(), (Throwable)e);
     }
   }
 
