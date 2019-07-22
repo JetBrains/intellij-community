a537d61c4c19c687adfd879fa78f84e370ccf8e6
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2019-07-22 14:59:02 +0300

    Code style group help topic (for SQL)
    GitOrigin-RevId: 34510e5dadaea646cf955c7eb0b4da3084fd96d3

--- platform/lang-api/src/com/intellij/psi/codeStyle/CodeStyleGroup.java
+++ platform/lang-api/src/com/intellij/psi/codeStyle/CodeStyleGroup.java
@@ -3,6 +3,7 @@ package com.intellij.psi.codeStyle;
 
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Represents a group of code style configurables sharing the same {@code CodeStyleGroup} instance. The group is shown as a separate node
@@ -11,6 +12,7 @@ import org.jetbrains.annotations.NotNull;
 public class CodeStyleGroup {
   private final String myDisplayName;
   private final String myId;
+  private final String myHelpTopic;
 
   /**
    * Constructor.
@@ -19,8 +21,20 @@ public class CodeStyleGroup {
    * @param displayName The display name as shown in settings UI.
    */
   public CodeStyleGroup(@NotNull String groupId, @NotNull String displayName) {
+    this(groupId, displayName, null);
+  }
+
+  /**
+   * Constructor.
+   *
+   * @param groupId     The unique group ID.
+   * @param displayName The display name as shown in settings UI.
+   * @param helpTopic   The help topic associated with the group.
+   */
+  public CodeStyleGroup(@NotNull String groupId, @NotNull String displayName, @Nullable String helpTopic) {
     myId = groupId;
     myDisplayName = displayName;
+    myHelpTopic = helpTopic;
   }
 
   /**
@@ -36,5 +50,10 @@ public class CodeStyleGroup {
    */
   public String getId() {
     return myId;
+  }
+
+  @Nullable
+  public String getHelpTopic() {
+    return myHelpTopic;
   }
 }
