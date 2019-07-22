90b5f4cc11d3245a314d9f7d1382b1336c771774
Dennis Ushakov <dennis.ushakov@gmail.com>
2019-07-22 20:23:09 +0300

    IDEA-CR-50519: don't check empty tags in svg/math (WEB-25670)
    GitOrigin-RevId: b7ab36c38c3ad07950ab9f710f281f93471ec0c4

--- xml/xml-psi-impl/src/com/intellij/xml/HtmlXmlExtension.java
+++ xml/xml-psi-impl/src/com/intellij/xml/HtmlXmlExtension.java
@@ -3,7 +3,9 @@ package com.intellij.xml;
 import com.intellij.ide.highlighter.HtmlFileType;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.xml.XmlDocument;
+import com.intellij.psi.xml.XmlTag;
 import com.intellij.xml.util.HtmlUtil;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -30,5 +32,17 @@ public class HtmlXmlExtension extends DefaultXmlExtension {
     System.arraycopy(namespaces, 0, newNamespaces, 0, namespaces.length);
     newNamespaces[namespaces.length] = new String[] {"xlink", "http://www.w3.org/1999/xlink"};
     return newNamespaces;
+  }
+
+  @Override
+  public boolean isSelfClosingTagAllowed(@NotNull XmlTag tag) {
+    XmlTag parent = tag;
+    while (parent != null) {
+      if ("svg".equals(parent.getName()) || "math".equals(parent.getName())) {
+        return true;
+      }
+      parent = parent.getParentTag();
+    }
+    return super.isSelfClosingTagAllowed(tag);
   }
 }
