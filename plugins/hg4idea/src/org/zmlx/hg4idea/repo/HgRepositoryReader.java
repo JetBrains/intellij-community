9a3bace65029377522f758cb6881d3dfbcc96131
Nadya Zabrodina <Nadya.Zabrodina@jetbrains.com>
2019-07-22 17:00:19 +0300

    hg: read info from internal hg files using several attempts with timeout
    * internal hg files may be used by another (hg process)
    * fix blinking HgReferenceValidatorTest
    
    GitOrigin-RevId: 5ca3af794247669e65a9483816e7c614342635cb

--- plugins/hg4idea/src/org/zmlx/hg4idea/repo/HgRepositoryReader.java
+++ plugins/hg4idea/src/org/zmlx/hg4idea/repo/HgRepositoryReader.java
@@ -20,11 +20,13 @@ import org.zmlx.hg4idea.util.HgVersion;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static com.intellij.openapi.util.io.FileUtilRt.doIOOperation;
 
 /**
  * Reads information about the Hg repository from Hg service files located in the {@code .hg} folder.
@@ -116,11 +118,18 @@ public class HgRepositoryReader {
 
   @NotNull
   private static byte[] readHashBytesFromFile(@NotNull File file) throws IOException {
-    byte[] bytes;
-    try (InputStream stream = new FileInputStream(file)) {
-      bytes = FileUtil.loadBytes(stream, 20);
+    try (final FileInputStream stream = doIOOperation(lastAttempt -> {
+      try {
+        return new FileInputStream(file);
+      }
+      catch (FileNotFoundException e) {
+        if (lastAttempt) throw e;
+        return null;
+      }
+    })) {
+      assert stream != null;
+      return FileUtil.loadBytes(stream, 20);
     }
-    return bytes;
   }
 
   /**
