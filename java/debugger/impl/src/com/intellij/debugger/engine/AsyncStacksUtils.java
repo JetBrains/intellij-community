25db57876addf706207df2fad73f92099caecdbb
Egor Ushakov <egor.ushakov@jetbrains.com>
2019-07-23 12:22:47 +0300

    speedup lambdas gathering by skipping non-lambda methods early
    GitOrigin-RevId: 92f607b7e68235fa321c7fc49c4324bf672433c2

--- java/debugger/impl/src/com/intellij/debugger/engine/AsyncStacksUtils.java
+++ java/debugger/impl/src/com/intellij/debugger/engine/AsyncStacksUtils.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.debugger.engine;
 
 import com.intellij.debugger.engine.evaluation.EvaluateException;
@@ -223,13 +223,9 @@ public class AsyncStacksUtils {
   private static Location findLocation(DebugProcessImpl debugProcess, ReferenceType type, String methodName, int line) {
     if (type != null && line >= 0) {
       for (Method method : type.methodsByName(methodName)) {
-        try {
-          List<Location> locations = method.locationsOfLine(DebugProcess.JAVA_STRATUM, null, line);
-          if (!locations.isEmpty()) {
-            return locations.get(0);
-          }
-        }
-        catch (AbsentInformationException ignored) {
+        List<Location> locations = DebuggerUtilsEx.locationsOfLine(method, line);
+        if (!locations.isEmpty()) {
+          return locations.get(0);
         }
       }
     }
