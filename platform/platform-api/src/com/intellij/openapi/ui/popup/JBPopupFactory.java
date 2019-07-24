c1a52edcdd3053e258d8c449112e84a3cf241c42
Yann Cébron <yann.cebron@jetbrains.com>
2019-07-24 10:39:13 +0200

    JBPopupFactory: javadoc cleanup, shorten link texts (IDEA-CR-50567)
    (cherry picked from commit 692be726881ee9525cd4ecdd35b2dad09604f76d) IDEA-CR-50567
    
    GitOrigin-RevId: 113f921b64be367a1817fa9e31a34c14239a6660

--- platform/platform-api/src/com/intellij/openapi/ui/popup/JBPopupFactory.java
+++ platform/platform-api/src/com/intellij/openapi/ui/popup/JBPopupFactory.java
@@ -29,14 +29,14 @@ import java.util.List;
  * <p>Lightweight Swing components, located in the contained window's layered pane. Cannot extend beyond window bounds.</p>
  * <ul>
  *   <li>
- *     <p>{@link Balloon} interface (implemented in {@link com.intellij.ui.BalloonImpl})</p>
+ *     <p>{@link Balloon} interface (implemented in {@link com.intellij.ui.BalloonImpl BalloonImpl})</p>
  *     <p>Platform's lowest-level lightweight popup component. Supports title bar, shadow, callout pointer at specified side, animated
  *        showing/hiding, fade-out after delay, two layers for positioning, hiding on condition (mouse move, mouse click, key press),
  *        custom mouse click handler, custom action buttons, notification about showing/hiding.</p>
  *     <p>Usually created via {@link JBPopupFactory#createBalloonBuilder}.</p>
  *   </li>
  *   <li>
- *     <p>{@link com.intellij.ide.IdeTooltip} and {@link com.intellij.ide.IdeTooltipManager}</p>
+ *     <p>{@link com.intellij.ide.IdeTooltip IdeTooltip} and {@link com.intellij.ide.IdeTooltipManager IdeTooltipManager}</p>
  *     <p>Subsystem which replaces (by default) Swing's ToolTipManager. Allows to define a custom mouse hover tooltip for any UI
  *        component. Tooltip can also be explicitly requested to show in the given location.</p>
  *     <p>Uses balloons to show tooltips internally (but doesn't expose this logic).</p>
@@ -47,7 +47,7 @@ import java.util.List;
  * <p>Components using a separate (OS-recognized) window. Can have location and size not limited by the parent window.</p>
  * <ul>
  *   <li>
- *     <p>{@link JBPopup} interface (implemented in {@link com.intellij.ui.popup.AbstractPopup})</p>
+ *     <p>{@link JBPopup} interface (implemented in {@link com.intellij.ui.popup.AbstractPopup AbstractPopup})</p>
  *     <p>Platform's lowest-level heavyweight popup component. Supports title and footer (ad) bar, resizing using mouse, fitting size to
  *        screen bounds, notification about showing/hiding/resizing/moving, hiding on condition (mouse exit, mouse click, key press,
  *        window deactivation), speed search, saving and restoring previously used location and size, custom keyboard actions for the
@@ -59,15 +59,15 @@ import java.util.List;
  * <h3>Combined (lightweight/heavyweight)</h3>
  * <ul>
  *   <li>
- *     <p>{@link com.intellij.ui.Hint} interface (implemented in {@link com.intellij.ui.LightweightHint})</p>
+ *     <p>{@link com.intellij.ui.Hint Hint} interface (implemented in {@link com.intellij.ui.LightweightHint LightweightHint})</p>
  *     <p>By default, if content fits the layered pane, tries to use lightweight components to display it (either using
- *        {@link com.intellij.ide.IdeTooltipManager}, or by directly adding content to the layered pane).
+ *        {@link com.intellij.ide.IdeTooltipManager IdeTooltipManager}, or by directly adding content to the layered pane).
  *        If the content doesn't fit, uses heavyweight approach (via {@link JBPopup}).</p>
  *     <p>Usually created directly (via constructor).</p>
  *   </li>
  *   <li>
  *     <p>{@link HintManager}</p>
- *     <p>Mostly used for showing hints in editor (works via {@link com.intellij.ui.LightweightHint}), but also has a method to show a hint at arbitrary
+ *     <p>Mostly used for showing hints in editor (works via {@link com.intellij.ui.LightweightHint LightweightHint}), but also has a method to show a hint at arbitrary
  *        location (works via {@link JBPopup}). Supports hiding of hints on additional conditions (caret movement, scrolling in editor, document
  *        change, showing of another hint), update of hint position on scrolling in editor, hiding hint after delay. The class also has
  *        utility methods to calculate appropriate hint position for the given location in editor coordinates
