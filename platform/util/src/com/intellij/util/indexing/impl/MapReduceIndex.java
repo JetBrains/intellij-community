68e760a5bba01f035d68a742bea1243688fe59c2
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2019-07-25 15:51:37 +0300

    use more relaxed types
    GitOrigin-RevId: cb8c8702c7b6e942ad10ce4f084c5ff9e6c28716

--- platform/util/src/com/intellij/util/indexing/impl/MapReduceIndex.java
+++ platform/util/src/com/intellij/util/indexing/impl/MapReduceIndex.java
@@ -365,8 +365,6 @@ public abstract class MapReduceIndex<Key,Value, Input> implements InvertedIndex<
     }
   }
 
-  public void removeTransientDataForFile(int inputId) {}
-
   public static <Key, Value> void checkValuesHaveProperEqualsAndHashCode(@NotNull Map<Key, Value> data,
                                                                          @NotNull IndexId<Key, Value> indexId,
                                                                          @NotNull DataExternalizer<Value> valueExternalizer) {
