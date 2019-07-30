5c4860d45a4eca7f1a20ab06c680af842dde3608
Anna Kozlova <anna.kozlova@jetbrains.com>
2019-07-30 12:00:01 -0700

    (IDEA-CR-48854) command line shortener: use default shortening method based on jdk used for run (IDEA-211257)
    GitOrigin-RevId: f81f530c8e5ccc0cbded2c4ef418114f2edae298

--- platform/lang-api/src/com/intellij/execution/configurations/SimpleJavaParameters.java
+++ platform/lang-api/src/com/intellij/execution/configurations/SimpleJavaParameters.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.configurations;
 
 import com.intellij.execution.CantRunException;
@@ -106,9 +92,16 @@ public class SimpleJavaParameters extends SimpleProgramParameters {
 
   public void setUseDynamicClasspath(@Nullable Project project) {
     Sdk jdk = getJdk();
-    ShortenCommandLine mode = myArgFile || myUseClasspathJar || myClasspathFile 
-                              ? ShortenCommandLine.getDefaultMethod(project, jdk != null ? jdk.getHomePath() : null) 
-                              : ShortenCommandLine.NONE; //explicitly disabled in UI
+    ShortenCommandLine mode = ShortenCommandLine.NONE;
+    if (JdkUtil.useDynamicClasspath(project) && (myArgFile || myUseClasspathJar || myClasspathFile)) {
+      String rootPath = jdk != null ? jdk.getHomePath() : null;
+      if (rootPath != null && JdkUtil.isModularRuntime(rootPath)) {
+        mode = ShortenCommandLine.ARGS_FILE;
+      }
+      else {
+        mode = myUseClasspathJar ? ShortenCommandLine.MANIFEST : ShortenCommandLine.CLASSPATH_FILE;
+      }
+    }
     setShortenCommandLine(mode, project);
   }
 
