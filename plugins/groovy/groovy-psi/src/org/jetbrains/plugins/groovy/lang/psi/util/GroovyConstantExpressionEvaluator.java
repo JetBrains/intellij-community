1926821bbd5b388fc1806274f182ef4633db4c1a
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-07-30 18:44:28 +0300

    IDEA-CR-50840: [groovy] don't run resolve while building CFG (IDEA-218929)
    (cherry picked from commit 6b2b6acd95ad258f12f97d092d18db7624df22ff)
    
    GitOrigin-RevId: d7d4804a43f13045187f04495a9c747da156dfa2

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/util/GroovyConstantExpressionEvaluator.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/util/GroovyConstantExpressionEvaluator.java
@@ -1,15 +1,17 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.lang.psi.util;
 
 import com.intellij.psi.PsiConstantEvaluationHelper;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiField;
 import com.intellij.psi.impl.ConstantExpressionEvaluator;
+import org.jetbrains.annotations.ApiStatus.Internal;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrExpression;
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrParenthesizedExpression;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrReferenceExpression;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.literals.GrLiteral;
+
+import static org.jetbrains.plugins.groovy.lang.psi.util.PsiUtilKt.skipParenthesesDown;
 
 /**
  * @author peter
@@ -18,17 +20,24 @@ public class GroovyConstantExpressionEvaluator implements ConstantExpressionEval
 
   @Nullable
   public static Object evaluate(@Nullable GrExpression expression) {
-    if (expression instanceof GrParenthesizedExpression) {
-      return evaluate(((GrParenthesizedExpression)expression).getOperand());
-    }
-    if (expression instanceof GrLiteral) {
-      return ((GrLiteral)expression).getValue();
+    GrExpression operand = skipParenthesesDown(expression);
+    if (operand instanceof GrLiteral) {
+      return ((GrLiteral)operand).getValue();
     }
     if (expression instanceof GrReferenceExpression) {
       PsiElement resolved = ((GrReferenceExpression)expression).resolve();
       if (resolved instanceof PsiField) {
         return ((PsiField)resolved).computeConstantValue();
       }
+    }
+    return null;
+  }
+
+  @Internal
+  public static Object evaluateNoResolve(@Nullable GrExpression expression) {
+    GrExpression operand = skipParenthesesDown(expression);
+    if (operand instanceof GrLiteral) {
+      return ((GrLiteral)operand).getValue();
     }
     return null;
   }
