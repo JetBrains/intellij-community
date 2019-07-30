4352ee62e37a31412df3ff42595255d16461a4de
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2019-07-30 16:36:56 +0300

    PsiShortNamesCache.getAllClassNames: restore compatibility with Kotlin 1.3.31-release-IJ2019.2-1 plugin.
    GitOrigin-RevId: 3964ebb77d005d98fde8bf6fa33751fd3c0de26f

--- java/java-indexing-api/src/com/intellij/psi/search/PsiShortNamesCache.java
+++ java/java-indexing-api/src/com/intellij/psi/search/PsiShortNamesCache.java
@@ -9,9 +9,12 @@ import com.intellij.psi.PsiField;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiMethod;
 import com.intellij.util.ArrayUtilRt;
+import com.intellij.util.CommonProcessors;
 import com.intellij.util.Processor;
 import com.intellij.util.containers.ContainerUtil;
+import com.intellij.util.containers.HashSet;
 import com.intellij.util.indexing.IdFilter;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -78,6 +81,19 @@ public abstract class PsiShortNamesCache {
 
   public boolean processAllClassNames(@NotNull Processor<String> processor, @NotNull GlobalSearchScope scope, @Nullable IdFilter filter) {
     return ContainerUtil.process(getAllClassNames(), processor);
+  }
+
+  /**
+   * Adds the names of all classes in the project and (optionally) libraries
+   * to the specified set.
+   *
+   * @param dest the set to add the names to.
+   * @deprecated use {@link #processAllClassNames}
+   */
+  @Deprecated
+  @ApiStatus.ScheduledForRemoval(inVersion = "2020.1")
+  public void getAllClassNames(@NotNull HashSet<String> dest) {
+    processAllClassNames(new CommonProcessors.CollectProcessor<>(dest));
   }
 
   /**
