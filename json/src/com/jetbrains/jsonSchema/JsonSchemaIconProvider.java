b988ed47073d6d7e742b9439a6fb77689f37271f
Anton Lobov <Anton.Lobov@jetbrains.com>
2019-07-31 18:48:59 +0200

    json schema icon provider: reorder the less expensive check before
    GitOrigin-RevId: 655297243b7e7ba0dd9195573e83c87a0c74850e

--- json/src/com/jetbrains/jsonSchema/JsonSchemaIconProvider.java
+++ json/src/com/jetbrains/jsonSchema/JsonSchemaIconProvider.java
@@ -22,7 +22,7 @@ public class JsonSchemaIconProvider implements FileIconProvider {
     if (project != null
         && JsonSchemaEnabler.EXTENSION_POINT_NAME.getExtensionList().stream().anyMatch(e -> e.canBeSchemaFile(file))) {
       JsonSchemaService service = JsonSchemaService.Impl.get(project);
-      if (service.isSchemaFile(file) && service.isApplicableToFile(file)) {
+      if (service.isApplicableToFile(file) && service.isSchemaFile(file)) {
         return AllIcons.FileTypes.JsonSchema;
       }
     }
