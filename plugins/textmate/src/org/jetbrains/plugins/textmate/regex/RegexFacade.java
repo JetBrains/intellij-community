55e074a64a4a72a31f30c91c62e8eaca3f1417d4
Dennis Ushakov <dennis.ushakov@gmail.com>
2019-07-31 14:17:36 +0300

    specific class for last match (IDEA-CR-50844)
    GitOrigin-RevId: dfdff3bf4ec7c9257504d745151f6c2bc6d1de71

--- plugins/textmate/src/org/jetbrains/plugins/textmate/regex/RegexFacade.java
+++ plugins/textmate/src/org/jetbrains/plugins/textmate/regex/RegexFacade.java
@@ -5,7 +5,6 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.progress.ProgressManager;
-import com.intellij.openapi.util.Trinity;
 import org.jcodings.specific.UTF8Encoding;
 import org.jetbrains.annotations.NotNull;
 import org.joni.Matcher;
@@ -24,7 +23,7 @@ public class RegexFacade {
   @NotNull
   private final Regex myRegex;
 
-  private final ThreadLocal<Trinity<Object, Integer, MatchData>> matchResult = new ThreadLocal<>();
+  private final ThreadLocal<LastMatch> matchResult = new ThreadLocal<>();
 
   private RegexFacade(@NotNull String regexString) {
     byte[] bytes = regexString.getBytes(StandardCharsets.UTF_8);
@@ -44,10 +43,10 @@ public class RegexFacade {
   }
 
   public MatchData match(@NotNull StringWithId string, int byteOffset) {
-    Trinity<Object, Integer, MatchData> lastResult = matchResult.get();
-    Object lastId = lastResult != null ? lastResult.first : null;
-    int lastOffset = lastResult != null ? lastResult.second : Integer.MAX_VALUE;
-    MatchData lastMatch = lastResult != null ? lastResult.third : MatchData.NOT_MATCHED;
+    LastMatch lastResult = matchResult.get();
+    Object lastId = lastResult != null ? lastResult.lastId : null;
+    int lastOffset = lastResult != null ? lastResult.lastOffset : Integer.MAX_VALUE;
+    MatchData lastMatch = lastResult != null ? lastResult.lastMatch : MatchData.NOT_MATCHED;
 
     if (lastId == string.id && lastOffset <= byteOffset) {
       if (!lastMatch.matched() || lastMatch.byteOffset().getStartOffset() >= byteOffset) {
@@ -62,7 +61,7 @@ public class RegexFacade {
     int matchIndex = matcher.search(byteOffset, string.bytes.length, Option.CAPTURE_GROUP);
     lastMatch = matchIndex > -1 ? MatchData.fromRegion(matcher.getEagerRegion()) : MatchData.NOT_MATCHED;
     checkMatched(lastMatch, string);
-    matchResult.set(new Trinity<>(lastId, lastOffset, lastMatch));
+    matchResult.set(new LastMatch(lastId, lastOffset, lastMatch));
     return lastMatch;
   }
 
@@ -90,6 +89,18 @@ public class RegexFacade {
     }
     catch (ExecutionException e) {
       return new RegexFacade(regexString);
+    }
+  }
+
+  private class LastMatch {
+    private final Object lastId;
+    private final int lastOffset;
+    private final MatchData lastMatch;
+
+    private LastMatch(Object id, int offset, MatchData data) {
+      lastId = id;
+      lastOffset = offset;
+      lastMatch = data;
     }
   }
 }
