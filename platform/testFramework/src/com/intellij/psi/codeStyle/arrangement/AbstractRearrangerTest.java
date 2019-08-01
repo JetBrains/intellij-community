5cbd3feff22b210d68c47e83cd7fe4d3ee54b28f
Maxim Kropotov <maxim.kropotov@jetbrains.com>
2019-08-01 18:22:16 +0300

    AbstractRearrangerTest: add test method without string-keyed map
    GitOrigin-RevId: da903c09ace6f9bd7a2271d465d75f1d7c6040af

--- platform/testFramework/src/com/intellij/psi/codeStyle/arrangement/AbstractRearrangerTest.java
+++ platform/testFramework/src/com/intellij/psi/codeStyle/arrangement/AbstractRearrangerTest.java
@@ -19,7 +19,7 @@ import com.intellij.psi.codeStyle.arrangement.model.ArrangementAtomMatchConditio
 import com.intellij.psi.codeStyle.arrangement.model.ArrangementMatchCondition;
 import com.intellij.psi.codeStyle.arrangement.std.*;
 import com.intellij.testFramework.fixtures.BasePlatformTestCase;
-import com.intellij.util.Function;
+import com.intellij.util.ObjectUtils;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -122,10 +122,29 @@ public abstract class AbstractRearrangerTest extends BasePlatformTestCase {
   }
 
   protected void doTest(@NotNull Map<String, ?> args) {
+    @SuppressWarnings("unchecked") List<ArrangementGroupingRule> groupingRules =
+      ObjectUtils.coalesce((List<ArrangementGroupingRule>)args.get("groups"), Collections.emptyList());
+
+    List<?> rules = (List<?>)args.get("rules");
+    List<ArrangementSectionRule> sectionRules = getSectionRules(rules);
+
+    @SuppressWarnings("unchecked")
+    List<StdArrangementRuleAliasToken> aliases =
+      ObjectUtils.coalesce((List<StdArrangementRuleAliasToken>)args.get("aliases"), Collections.emptyList());
+    
+    final StdArrangementSettings arrangementSettings = new StdArrangementExtendableSettings(groupingRules, sectionRules, aliases);
+    
     String text = (String)args.get("initial");
     String expected = (String)args.get("expected");
     @SuppressWarnings("unchecked") List<TextRange> ranges = (List<TextRange>)args.get("ranges");
 
+    doTestWithSettings(text, expected, arrangementSettings, ranges);
+  }
+
+  protected void doTestWithSettings(@NotNull String text,
+                                    @NotNull String expected,
+                                    @Nullable ArrangementSettings arrangementSettings, 
+                                    @Nullable List<TextRange> ranges) {
     Info info = parse(text);
     if (!isEmpty(ranges) && !isEmpty(info.ranges)) {
       fail("Duplicate ranges set: explicit: " + ranges + ", " + "derived: " + info.ranges + ", text:\n" + text);
@@ -144,20 +163,10 @@ public abstract class AbstractRearrangerTest extends BasePlatformTestCase {
       });
     }
 
-    @SuppressWarnings("unchecked") List<ArrangementGroupingRule> groupingRules = (List<ArrangementGroupingRule>)args.get("groups");
-    if (groupingRules == null) groupingRules = Collections.emptyList();
-
-    List<?> rules = (List<?>)args.get("rules");
-    List<ArrangementSectionRule> sectionRules = getSectionRules(rules);
-
-    @SuppressWarnings("unchecked")
-    List<StdArrangementRuleAliasToken> aliases = (List<StdArrangementRuleAliasToken>)args.get("aliases");
-    CommonCodeStyleSettings settings = CodeStyle.getSettings(myFixture.getProject()).getCommonSettings(language);
-    final StdArrangementSettings arrangementSettings =
-      aliases == null ?
-      new StdArrangementSettings(groupingRules, sectionRules) :
-      new StdArrangementExtendableSettings(groupingRules, sectionRules, aliases);
-    settings.setArrangementSettings(arrangementSettings);
+    if (arrangementSettings != null) {
+      CommonCodeStyleSettings settings = CodeStyle.getSettings(myFixture.getProject()).getCommonSettings(language);
+      settings.setArrangementSettings(arrangementSettings);
+    }
     ArrangementEngine engine = ServiceManager.getService(myFixture.getProject(), ArrangementEngine.class);
     CommandProcessor.getInstance().executeCommand(getProject(), ()-> engine.arrange(myFixture.getEditor(), myFixture.getFile(), info.ranges), null, null);
 
@@ -172,10 +181,14 @@ public abstract class AbstractRearrangerTest extends BasePlatformTestCase {
     }
   }
 
-  protected List<ArrangementSectionRule> getSectionRules(List<?> rules) {
-    List<ArrangementSectionRule> sectionRules = Collections.emptyList();
-    if (rules != null) sectionRules = ContainerUtil.map(rules, (Function<Object, ArrangementSectionRule>)o -> o instanceof ArrangementSectionRule ? (ArrangementSectionRule)o : ArrangementSectionRule.create((StdArrangementMatchRule)o));
-    return sectionRules;
+  @NotNull
+  protected List<ArrangementSectionRule> getSectionRules(@Nullable List<?> rules) {
+    if (rules == null) {
+      return ContainerUtil.emptyList();
+    }
+    return ContainerUtil.map(rules, o -> o instanceof ArrangementSectionRule
+                                         ? (ArrangementSectionRule)o
+                                         : ArrangementSectionRule.create((StdArrangementMatchRule)o));
   }
 
   private static boolean isEmpty(Collection<?> collection) {
