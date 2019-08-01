826305cc5605dfe9bf7044fe112689d6509e772b
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2019-08-01 18:03:39 +0300

    vcs: Remove "Commit" action from "Local Changes" tab toolbar/popup for non-modal commit (IDEA-219688)
    (cherry picked from commit bdb6c0ca13880ef2a72382b4d3197fc6af88f30d)
    
    GitOrigin-RevId: 4c05f59e91e05ae3ec7b392cd7f1b336a2788789

--- platform/vcs-impl/src/com/intellij/openapi/vcs/actions/CommonCheckinProjectAction.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/actions/CommonCheckinProjectAction.kt
@@ -1,11 +1,31 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vcs.actions
 
+import com.intellij.openapi.actionSystem.ActionPlaces.CHANGES_VIEW_POPUP
+import com.intellij.openapi.actionSystem.ActionPlaces.CHANGES_VIEW_TOOLBAR
+import com.intellij.openapi.actionSystem.Presentation
 import com.intellij.openapi.vcs.FilePath
 import com.intellij.openapi.vcs.ProjectLevelVcsManager
 import com.intellij.vcsUtil.VcsUtil.getFilePath
 
+private val LOCAL_CHANGES_ACTION_PLACES = setOf(CHANGES_VIEW_TOOLBAR, CHANGES_VIEW_POPUP)
+
+private fun isInLocalChanges(context: VcsContext) = context.place in LOCAL_CHANGES_ACTION_PLACES
+
+private fun isNonModalCommit(context: VcsContext) = context.project?.getNonModalCommitWorkflowHandler() != null
+
 open class CommonCheckinProjectAction : AbstractCommonCheckinAction() {
+  private fun isCommitProjectAction() = this::class.java == CommonCheckinProjectAction::class.java
+
+  override fun update(vcsContext: VcsContext, presentation: Presentation) {
+    if (isCommitProjectAction() && isInLocalChanges(vcsContext) && isNonModalCommit(vcsContext)) {
+      presentation.isEnabledAndVisible = false
+    }
+    else {
+      super.update(vcsContext, presentation)
+    }
+  }
+
   override fun getRoots(dataContext: VcsContext): Array<FilePath> =
     ProjectLevelVcsManager.getInstance(dataContext.project!!).allVcsRoots
       .filter { it.vcs!!.checkinEnvironment != null }
