609a83546429f0fa7e0fa41763c5f633146fa2e2
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-08-02 17:14:39 +0200

    cleanup
    GitOrigin-RevId: 60655798d43c9eb214fa11d798d779ce7c667f38

--- platform/extensions/src/com/intellij/util/pico/CachingConstructorInjectionComponentAdapter.java
+++ platform/extensions/src/com/intellij/util/pico/CachingConstructorInjectionComponentAdapter.java
@@ -4,7 +4,10 @@ package com.intellij.util.pico;
 import com.intellij.util.ExceptionUtil;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
-import org.picocontainer.*;
+import org.picocontainer.Parameter;
+import org.picocontainer.PicoContainer;
+import org.picocontainer.PicoInitializationException;
+import org.picocontainer.PicoIntrospectionException;
 import org.picocontainer.defaults.*;
 
 import java.lang.reflect.Constructor;
@@ -76,7 +79,7 @@ public class CachingConstructorInjectionComponentAdapter extends InstantiatingCo
 
   @NotNull
   private Object doGetComponentInstance(@NotNull PicoContainer guardedContainer) {
-    final Constructor constructor;
+    Constructor<?> constructor;
     try {
       constructor = getGreediestSatisfiableConstructor(guardedContainer);
     }
@@ -100,8 +103,8 @@ public class CachingConstructorInjectionComponentAdapter extends InstantiatingCo
   }
 
   @NotNull
-  private Object[] getConstructorArguments(PicoContainer container, @NotNull Constructor ctor) {
-    Class[] parameterTypes = ctor.getParameterTypes();
+  private Object[] getConstructorArguments(PicoContainer container, @NotNull Constructor<?> ctor) {
+    Class<?>[] parameterTypes = ctor.getParameterTypes();
     Object[] result = new Object[parameterTypes.length];
     Parameter[] currentParameters = parameters != null ? parameters : createDefaultParameters(parameterTypes);
 
@@ -113,18 +116,18 @@ public class CachingConstructorInjectionComponentAdapter extends InstantiatingCo
 
   @NotNull
   @Override
-  protected Constructor getGreediestSatisfiableConstructor(@NotNull PicoContainer container) throws
+  protected Constructor<?> getGreediestSatisfiableConstructor(@NotNull PicoContainer container) throws
                                                                                     PicoIntrospectionException,
                                                                                     AssignabilityRegistrationException, NotConcreteRegistrationException {
-    final Set<Constructor> conflicts = new HashSet<>();
-    final Set<List<Class>> unsatisfiableDependencyTypes = new HashSet<>();
-    List<Constructor> sortedMatchingConstructors = getSortedMatchingConstructors();
-    Constructor greediestConstructor = null;
+    final Set<Constructor<?>> conflicts = new HashSet<>();
+    final Set<List<Class<?>>> unsatisfiableDependencyTypes = new HashSet<>();
+    List<Constructor<?>> sortedMatchingConstructors = getSortedMatchingConstructors();
+    Constructor<?> greediestConstructor = null;
     int lastSatisfiableConstructorSize = -1;
-    Class unsatisfiedDependencyType = null;
-    for (Constructor constructor : sortedMatchingConstructors) {
+    Class<?> unsatisfiedDependencyType = null;
+    for (Constructor<?> constructor : sortedMatchingConstructors) {
       boolean failedDependency = false;
-      Class[] parameterTypes = constructor.getParameterTypes();
+      Class<?>[] parameterTypes = constructor.getParameterTypes();
       Parameter[] currentParameters = parameters != null ? parameters : createDefaultParameters(parameterTypes);
 
       // remember: all constructors with less arguments than the given parameters are filtered out already
@@ -174,8 +177,8 @@ public class CachingConstructorInjectionComponentAdapter extends InstantiatingCo
     return greediestConstructor;
   }
 
-  private List<Constructor> getSortedMatchingConstructors() {
-    List<Constructor> matchingConstructors = new ArrayList<>();
+  private List<Constructor<?>> getSortedMatchingConstructors() {
+    List<Constructor<?>> matchingConstructors = new ArrayList<>();
     // filter out all constructors that will definitely not match
     for (Constructor<?> constructor : getConstructors()) {
       if ((parameters == null || constructor.getParameterTypes().length == parameters.length) &&
