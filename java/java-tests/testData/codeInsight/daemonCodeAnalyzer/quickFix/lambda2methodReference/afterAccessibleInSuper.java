616e3ca0300769e77e514ce9392bf65091c41c28
Anna Kozlova <anna.kozlova@jetbrains.com>
2019-08-05 21:11:13 +0200

    method references: allow access to protected methods through super (IDEA-217862)
    GitOrigin-RevId: f1d86f6127a64146e87998423f3034e38a46409c

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/lambda2methodReference/afterAccessibleInSuper.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/lambda2methodReference/afterAccessibleInSuper.java
@@ -2,5 +2,5 @@
 import java.util.Random;
 import java.util.function.Function;
 class Bar extends Random {
-    Function<Integer, Integer>  s = (i) -> super.ne<caret>xt(i);
+    Function<Integer, Integer>  s = super::next;
 }
