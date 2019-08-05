bddef24b8486c3a60f2b6dc99fce94cf0c53e5fa
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-08-05 22:16:56 +0200

    Cleanup (dead code; IDEA-CR-50885)
    GitOrigin-RevId: 397bd71dbda16e993ec2d2b39f92ae3bf4ef38cb

--- platform/util/src/com/intellij/util/io/OpenChannelsCache.java
+++ platform/util/src/com/intellij/util/io/OpenChannelsCache.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2013 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 /*
  * @author max
@@ -83,15 +69,12 @@ public class OpenChannelsCache { // TODO: Will it make sense to have a backgroun
 
     if (descriptor != null) {
       assert !descriptor.isLocked();
-
-      AntivirusDetector.getInstance().execute(() -> {
-        try {
-          descriptor.getChannel().close();
-        }
-        catch (IOException e) {
-          throw new RuntimeException(e);
-        }
-      });
+      try {
+        descriptor.getChannel().close();
+      }
+      catch (IOException e) {
+        throw new RuntimeException(e);
+      }
     }
   }
 
