b7f8e08af94cdff62b1f13c1011ef2e590539b03
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-08-07 00:43:56 +0200

    [ui] corrects "generate javadoc" dialog title (IDEA-215362)
    GitOrigin-RevId: b5c189936b67f789a077754aad31601cdabe401d

--- java/java-impl/src/com/intellij/javadoc/actions/GenerateJavadocAction.java
+++ java/java-impl/src/com/intellij/javadoc/actions/GenerateJavadocAction.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.javadoc.actions;
 
 import com.intellij.analysis.AnalysisScope;
@@ -58,6 +58,11 @@ public final class GenerateJavadocAction extends BaseAnalysisAction{
       myConfigurable.disposeUIResources();
       myConfigurable = null;
     }
+  }
+
+  @Override
+  protected @NotNull String getDialogTitle() {
+    return JavadocBundle.message("javadoc.generate.title");
   }
 
   @Override
