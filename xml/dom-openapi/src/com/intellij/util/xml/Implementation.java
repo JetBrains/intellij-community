97290ae758b96dc6404a800d29022eee95c465f5
Yann Cébron <yann.cebron@jetbrains.com>
2019-08-06 13:08:45 +0200

    [dom] Implementation: Javadoc cleanup
    GitOrigin-RevId: b9d5f405e37b25fe4e568eee41a944c802414aff

--- xml/dom-openapi/src/com/intellij/util/xml/Implementation.java
+++ xml/dom-openapi/src/com/intellij/util/xml/Implementation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2019 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,7 +24,7 @@ import java.lang.annotation.Target;
  * Specifies DOM interface implementation. Implementation should be abstract class with only
  * non-DOM methods defined, it should implement the annotated interface.
  *
- * @deprecated use extension point com.intellij.util.xml.impl.DomImplementationClassEP#EP_NAME
+ * @deprecated use extension point {@code com.intellij.dom.implementation} instead
  */
 @Deprecated
 @Retention(RetentionPolicy.RUNTIME)
