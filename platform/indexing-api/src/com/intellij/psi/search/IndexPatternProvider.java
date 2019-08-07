e1bd831df218bba97c69ec7f2e4c3a575d2334af
Yann Cébron <yann.cebron@jetbrains.com>
2019-08-07 17:59:02 +0200

    IndexPatternProvider: javadoc cleanup
    GitOrigin-RevId: aa1f0148a0d1a91d7087b226cd72036fe7a3ba45

--- platform/indexing-api/src/com/intellij/psi/search/IndexPatternProvider.java
+++ platform/indexing-api/src/com/intellij/psi/search/IndexPatternProvider.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.search;
 
 import com.intellij.openapi.extensions.ExtensionPointName;
@@ -24,8 +10,8 @@ import java.beans.PropertyChangeListener;
 
 /**
  * Provider of regular expression patterns the occurrences of which in the comments of
- * source code files are indexed by IDEA. Implementations of this interface need to be registered
- * as extensions for the {@code indexPatternProvider} extension point.
+ * source code files are indexed.
+ * Register via {@code com.intellij.indexPatternProvider} extension point.
  *
  * @author yole
  * @see com.intellij.psi.search.searches.IndexPatternSearch
@@ -44,9 +30,8 @@ public interface IndexPatternProvider {
   @NonNls String PROP_INDEX_PATTERNS = "indexPatterns";
 
   /**
-   * Returns the list of index patterns the occurrences of which should be indexed by IDEA.
-   *
-   * @return the array of index patterns
+   * Returns index patterns the occurrences of which should be indexed.
    */
-  @NotNull IndexPattern[] getIndexPatterns();
+  @NotNull
+  IndexPattern[] getIndexPatterns();
 }
