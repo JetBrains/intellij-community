67862fe347316df16107733270edb919018793a4
Dennis Ushakov <dennis.ushakov@gmail.com>
2019-08-07 11:51:52 +0300

    update deprecation warning
    GitOrigin-RevId: 11ef283d7c7cf5db3205d4574500faf1b5f5b0da

--- platform/platform-impl/src/com/intellij/openapi/fileTypes/impl/FileTypeManagerImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/fileTypes/impl/FileTypeManagerImpl.java
@@ -1176,7 +1176,7 @@ public class FileTypeManagerImpl extends FileTypeManagerEx implements Persistent
 
   @Override
   public void registerFileType(@NotNull final FileType type, @NotNull final List<? extends FileNameMatcher> defaultAssociations) {
-    DeprecatedMethodException.report("Use FileTypeFactory instead.");
+    DeprecatedMethodException.report("Use fileType extension instead.");
     ApplicationManager.getApplication().runWriteAction(() -> {
       fireBeforeFileTypesChanged();
       registerFileTypeWithoutNotification(type, defaultAssociations, true);
