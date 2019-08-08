6c6dd9d29ae00024e196595574e162c95954c88d
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-08-08 08:49:40 +0200

    cleanup - use MessagesService
    GitOrigin-RevId: a61b454fb30211e5ddc907e0edab8d095da84846

--- platform/platform-api/src/com/intellij/openapi/ui/Messages.java
+++ platform/platform-api/src/com/intellij/openapi/ui/Messages.java
@@ -283,7 +283,6 @@ public class Messages {
                                int focusedOptionIndex,
                                @Nullable Icon icon,
                                @Nullable DialogWrapper.DoNotAskOption doNotAskOption) {
-
     return MessagesService.getInstance()
       .showMessageDialog(null, null, message, title, options, defaultOptionIndex, focusedOptionIndex, icon, doNotAskOption, false);
   }
@@ -816,21 +815,12 @@ public class Messages {
     showDialog(project, message, title, new String[]{OK_BUTTON}, 0, getErrorIcon());
   }
 
-  public static void showErrorDialog(@NotNull Component component,
+  public static void showErrorDialog(@Nullable Component component,
                                      String message,
                                      @NotNull @Nls(capitalization = Nls.Capitalization.Title) String title) {
-    try {
-      if (canShowMacSheetPanel()) {
-        MacMessages.getInstance().showErrorDialog(title, message, OK_BUTTON, SwingUtilities.getWindowAncestor(component));
-        return;
-      }
-    }
-    catch (MessageException ignored) {/*rollback the message and show a dialog*/}
-    catch (Exception reportThis) {
-      LOG.error(reportThis);
-    }
-
-    showDialog(component, message, title, new String[]{OK_BUTTON}, 0, getErrorIcon());
+    MessagesService.getInstance().showMessageDialog(null, component, message, title,
+      /* options = */ new String[]{OK_BUTTON},
+      /* defaultOptionIndex = */ 0, /* focusedOptionIndex = */ 0, getErrorIcon(), null, false);
   }
 
   public static void showErrorDialog(@NotNull Component component, String message) {
@@ -856,18 +846,7 @@ public class Messages {
    * @see #showErrorDialog(Component, String, String)
    */
   public static void showErrorDialog(String message, @NotNull @Nls(capitalization = Nls.Capitalization.Title) String title) {
-    try {
-      if (canShowMacSheetPanel()) {
-        MacMessages.getInstance().showErrorDialog(title, message, OK_BUTTON, null);
-        return;
-      }
-    }
-    catch (MessageException ignored) {/*rollback the message and show a dialog*/}
-    catch (Exception reportThis) {
-      LOG.error(reportThis);
-    }
-
-    showDialog(message, title, new String[]{OK_BUTTON}, 0, getErrorIcon());
+    showErrorDialog((Component)null, message, title);
   }
 
   public static void showWarningDialog(@Nullable Project project,
