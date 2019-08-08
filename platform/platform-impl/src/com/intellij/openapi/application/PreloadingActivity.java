c0d4ed10cc52904e06849908c73e8386b79da852
peter <peter@jetbrains.com>
2019-08-08 21:02:06 +0300

    clarify PreloadingActivity semantics
    GitOrigin-RevId: f4e1ddc58295fa4ba838e13ad62b1a44eb891594

--- platform/platform-impl/src/com/intellij/openapi/application/PreloadingActivity.java
+++ platform/platform-impl/src/com/intellij/openapi/application/PreloadingActivity.java
@@ -8,7 +8,14 @@ import org.jetbrains.annotations.NotNull;
 /**
  * An activity to be executed in background on IDE startup. It may load some classes or other configuration
  * so that when something (e.g. an action) is invoked the first time in the UI, there's no visible pause
- * while required stuff is being lazily loaded.
+ * while required stuff is being lazily loaded.<p></p>
+ *
+ * Preloading activities should not have any side effects except for improving subsequent performance, so that
+ * if they are not executed for any reason, the behavior of the IDE remains the same.<p></p>
+ *
+ * Being eager and unspecific, preloading should be considered last resort in optimization.
+ * Please prefer other ways of speeding up things, by e.g. reducing the amount of classloading and initialization
+ * necessary when some functionality is invoked the first time.
  *
  * @author peter
  */
