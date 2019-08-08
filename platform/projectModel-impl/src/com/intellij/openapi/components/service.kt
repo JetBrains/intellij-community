9631ddd1f666b0e3d36a42159b4d8e3fccefc222
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-08-08 18:45:07 +0200

    kotlin helpers - use component manager directly to get service
    GitOrigin-RevId: 502e570601332e03335910d28bb9541a5b70817c

--- platform/projectModel-impl/src/com/intellij/openapi/components/service.kt
+++ platform/projectModel-impl/src/com/intellij/openapi/components/service.kt
@@ -1,18 +1,19 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.components
 
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.impl.ComponentManagerImpl
 import com.intellij.openapi.components.impl.stores.IComponentStore
 import com.intellij.openapi.project.Project
 import com.intellij.project.ProjectStoreOwner
 
-inline fun <reified T : Any> service(): T = ServiceManager.getService(T::class.java)
+inline fun <reified T : Any> service(): T = ApplicationManager.getApplication().getService(T::class.java, true)
 
-inline fun <reified T : Any> serviceOrNull(): T? = ServiceManager.getService(T::class.java)
+inline fun <reified T : Any> serviceOrNull(): T? = ApplicationManager.getApplication().getService(T::class.java, true)
 
-inline fun <reified T : Any> serviceIfCreated(): T? = ServiceManager.getServiceIfCreated(T::class.java)
+inline fun <reified T : Any> serviceIfCreated(): T? = ApplicationManager.getApplication().getServiceIfCreated(T::class.java)
 
-inline fun <reified T : Any> Project.service(): T = ServiceManager.getService(this, T::class.java)
+inline fun <reified T : Any> Project.service(): T = getService(T::class.java, true)
 
 val ComponentManager.stateStore: IComponentStore
   get() {
