e44eec3c4cade892a281fbc07e314d1f11f0e44b
Kirill Likhodedov <Kirill.Likhodedov@gmail.com>
2019-08-11 10:38:43 +0300

    Define git config gc.auto for the repository according to team-wide shared settings
    The default gc.auto setting is not efficient for some big projects (including IntelliJ IDEA itself), because such projects produce a lot of garbage, and git gc gets called too often. It makes sense to redefine the setting for such repositories. Since Git doesn't have a mechanism for sharing git config values, let's use intellij.yaml for this task.
    
    If there is no explicit gc.auto value in the local .git/config, then set it to the value defined for the project in intellij.yaml.
    
    GitOrigin-RevId: 2756489489263d23c0d8e7e2975044f68345b68c

---
+++ plugins/git4idea/src/git4idea/config/GitSetupProjectConfig.kt
@@ -0,0 +1,44 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package git4idea.config
+
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.vcs.ProjectLevelVcsManager
+import com.intellij.openapi.vcs.impl.ProjectLevelVcsManagerImpl
+import com.intellij.openapi.vcs.impl.VcsInitObject
+import com.intellij.util.concurrency.NonUrgentExecutor
+import git4idea.repo.GitRepository
+import git4idea.repo.GitRepositoryManager
+
+const val GC_AUTO = "gc.auto"
+
+class GitSetupProjectConfig: StartupActivity {
+  override fun runActivity(project: Project) {
+    (ProjectLevelVcsManager.getInstance(project) as ProjectLevelVcsManagerImpl).addInitializationRequest(VcsInitObject.AFTER_COMMON) {
+      if (ApplicationManager.getApplication().isDispatchThread) {
+        NonUrgentExecutor.getInstance().execute {
+          setupGcAutoIfNeeded(project)
+        }
+      }
+      else {
+        setupGcAutoIfNeeded(project)
+      }
+    }
+  }
+
+  private fun setupGcAutoIfNeeded(project: Project) {
+    val gcAuto = GitVcsSettings.getInstance(project).state.gcAuto
+    if (gcAuto != null) {
+      val rootsToUpdate = mutableListOf<GitRepository>()
+      for (repo in GitRepositoryManager.getInstance(project).repositories) {
+        val value = GitConfigUtil.getValue(project, repo.root, GC_AUTO)
+        if (value == null) rootsToUpdate.add(repo)
+      }
+
+      for (repo in rootsToUpdate) {
+        GitConfigUtil.setValue(project, repo.root, GC_AUTO, gcAuto, "--local")
+      }
+    }
+  }
+}
