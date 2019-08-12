ae5cda745c90e7ba0d1d98607f804f439839168f
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2019-08-12 17:30:08 +0300

    FUS-400: Validate packages.
    We only accept packages that really exist on interpreter
    
    GitOrigin-RevId: aad6f37f30a21cde4f7f6814c4e8ddb0a1d7362d

--- python/src/com/jetbrains/python/statistics/PyPackageUsagesCollector.kt
+++ python/src/com/jetbrains/python/statistics/PyPackageUsagesCollector.kt
@@ -3,12 +3,17 @@ package com.jetbrains.python.statistics
 
 import com.intellij.internal.statistic.beans.MetricEvent
 import com.intellij.internal.statistic.eventLog.FeatureUsageData
+import com.intellij.internal.statistic.eventLog.validator.ValidationResultType
+import com.intellij.internal.statistic.eventLog.validator.rules.EventContext
+import com.intellij.internal.statistic.eventLog.validator.rules.impl.CustomWhiteListRule
 import com.intellij.internal.statistic.service.fus.collectors.ProjectUsagesCollector
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
+import com.intellij.reference.SoftReference
 import com.jetbrains.extensions.getSdk
 import com.jetbrains.python.packaging.PyPIPackageCache
 import com.jetbrains.python.packaging.PyPackageManager
+import java.lang.ref.WeakReference
 
 /**
  * Reports usages of packages and versions
@@ -29,8 +34,8 @@ private fun getPackages(project: Project): Set<MetricEvent> {
     val usageData = FeatureUsageData().addPythonSpecificInfo(sdk)
     app.runReadAction {
       PyPackageManager.getInstance(sdk).getRequirements(module)?.apply {
-        val packageNames = PyPIPackageCache.getInstance().packageNames
-        filter { it.name in packageNames }.forEach { req ->
+        val packageNames = getPyPiPackagesCache()
+        filter { it.name.toLowerCase() in packageNames }.forEach { req ->
           val version = req.versionSpecs.firstOrNull()?.version?.trim() ?: "unknown"
           result.add(MetricEvent("python_package_installed",
                                  usageData.copy() // Not to calculate interpreter on each call
@@ -41,4 +46,23 @@ private fun getPackages(project: Project): Set<MetricEvent> {
     }
   }
   return result
+}
+
+private fun getPyPiPackagesCache() = PyPIPackageCache.getInstance().packageNames.map(String::toLowerCase).toSet()
+
+
+class PyPackageUsagesWhiteListRule : CustomWhiteListRule() {
+  private var packagesRef: WeakReference<Set<String>>? = null
+  @Synchronized
+  private fun getPackages(): Set<String> {
+    SoftReference.dereference(packagesRef)?.let { return it }
+    val pyPiPackages = getPyPiPackagesCache()
+    packagesRef = WeakReference(pyPiPackages)
+    return pyPiPackages
+  }
+
+  override fun acceptRuleId(ruleId: String?) = "python_packages" == ruleId
+
+  override fun doValidate(data: String, context: EventContext) =
+    if (data.toLowerCase() in getPackages()) ValidationResultType.ACCEPTED else ValidationResultType.REJECTED
 }
