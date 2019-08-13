8846ce9b6ed08813d8f868e642afed775b4a4d14
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2019-08-13 12:44:57 +0300

    IDEA-219137 - Dependency completion doesn't suggest modules from the current project
    GitOrigin-RevId: 1c6b544f31e54d0df510dd1011e97e4d2547529a

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/onlinecompletion/ProjectModulesCompletionProvider.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/onlinecompletion/ProjectModulesCompletionProvider.kt
@@ -13,53 +13,20 @@ import java.io.IOException
 class ProjectModulesCompletionProvider(private val myProject: Project) : DependencyCompletionProvider {
 
   @Throws(IOException::class)
-  override fun findGroupCandidates(template: MavenCoordinate, searchParameters: SearchParameters): List<MavenDependencyCompletionItem> {
-    return doQuery(template) {
-      startWith(it.groupId, template.groupId)
-    }
-  }
+  override fun findGroupCandidates(template: MavenCoordinate, searchParameters: SearchParameters) = modulesAsMavenId()
+  override fun findArtifactCandidates(template: MavenCoordinate, searchParameters: SearchParameters) = modulesAsMavenId()
+  override fun findAllVersions(template: MavenCoordinate, searchParameters: SearchParameters) = modulesAsMavenId()
 
-
-  private fun doQuery(template: MavenCoordinate, predicate: (MavenCoordinate) -> Boolean): List<MavenDependencyCompletionItem> {
+  private fun modulesAsMavenId(): List<MavenDependencyCompletionItem> {
     return MavenProjectsManager.getInstance(myProject).projects.asSequence()
-      .map { it.getMavenId() }
-      .filter(predicate)
+      .map { it.mavenId }
       .map { MavenDependencyCompletionItem(it.groupId, it.artifactId, it.version, PROJECT) }
       .toList()
   }
 
   @Throws(IOException::class)
-  override fun findArtifactCandidates(template: MavenCoordinate, searchParameters: SearchParameters): List<MavenDependencyCompletionItem> {
-    return doQuery(template) {
-      equals(it.groupId, template.groupId) && startWith(it.artifactId, template.artifactId);
-    }
-  }
-
-  @Throws(IOException::class)
-  override fun findAllVersions(template: MavenCoordinate, searchParameters: SearchParameters): List<MavenDependencyCompletionItem> {
-    return doQuery(template) {
-      equals(it.groupId, template.groupId) && equals(it.artifactId, template.artifactId);
-    }
-  }
-
-  @Throws(IOException::class)
   override fun findClassesByString(str: String, searchParameters: SearchParameters): List<MavenDependencyCompletionItemWithClass> {
-    return emptyList();
+    return emptyList()
   }
 
-  private fun startWith(str: String?, prefix: String?): Boolean {
-    if (str == null) {
-      return false;
-    }
-    if (prefix.isNullOrBlank()) return true;
-
-    return str.startsWith(prefix);
-  }
-
-  private fun equals(str1: String?, str2: String?): Boolean {
-    if (str1 == null || str2 == null) {
-      return false;
-    }
-    return str1 == str2;
-  }
 }
