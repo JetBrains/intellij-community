090855044b4b575c8513dbcc696498593f1d3d8b
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2019-08-16 18:28:58 +0300

    fix flaky tests: wait until tree is build
    GitOrigin-RevId: f223df390b81982381458e7c53e6614a0e674980

--- platform/testFramework/src/com/intellij/testFramework/FileStructureTestBase.java
+++ platform/testFramework/src/com/intellij/testFramework/FileStructureTestBase.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.testFramework;
 
 import com.intellij.openapi.util.Disposer;
@@ -75,6 +75,7 @@ public abstract class FileStructureTestBase extends CodeInsightFixtureTestCase {
 
   protected void checkResult() {
     String expectedFileName = getTestDataPath() + "/" + PathUtil.makeFileName(getTestName(false), "tree");
+    PlatformTestUtil.waitWhileBusy(myPopupFixture.getTree());
     assertSameLinesWithFile(expectedFileName, PlatformTestUtil.print(myPopupFixture.getTree(), true).trim());
   }
 }
