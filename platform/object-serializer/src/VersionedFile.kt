dd7f51372823a45e8d7fde64f976681997fc251c
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-08-16 21:24:58 +0200

    [platform] migrates serializer store to SafeFileOutputStream
    GitOrigin-RevId: 1194e7891c8daced1ee5c3b3061b59a355ce95f7

--- platform/object-serializer/src/VersionedFile.kt
+++ platform/object-serializer/src/VersionedFile.kt
@@ -3,7 +3,7 @@ package com.intellij.serialization
 
 import com.intellij.util.ParameterizedTypeImpl
 import com.intellij.util.io.move
-import com.intellij.util.io.writeSafe
+import com.intellij.util.io.safeOutputStream
 import net.jpountz.lz4.LZ4FrameInputStream
 import net.jpountz.lz4.LZ4FrameOutputStream
 import java.io.IOException
@@ -29,11 +29,8 @@ data class VersionedFile @JvmOverloads constructor(val file: Path, val version: 
   @Throws(IOException::class)
   @JvmOverloads
   fun <T> writeList(data: Collection<T>, itemClass: Class<T>, configuration: WriteConfiguration = versionedFileDefaultWriteConfiguration) {
-    file.writeSafe { fileOut ->
-      val out = when {
-        isCompressed -> LZ4FrameOutputStream(fileOut, LZ4FrameOutputStream.BLOCKSIZE.SIZE_4MB)
-        else -> fileOut.buffered(fileBufferSize)
-      }
+    file.safeOutputStream().use {
+      val out = if (isCompressed) LZ4FrameOutputStream(it, LZ4FrameOutputStream.BLOCKSIZE.SIZE_4MB) else it
 
       ObjectSerializer.instance.serializer.writeVersioned(data, out, version, originalType = ParameterizedTypeImpl(data.javaClass, itemClass), configuration = configuration)
     }
