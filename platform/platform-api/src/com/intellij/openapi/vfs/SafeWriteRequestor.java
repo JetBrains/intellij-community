b9a049d308e2bdd86f1d297aa6bdf12209d9fe69
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-08-16 11:41:42 +0200

    [platform] non-destructive fast SafeFileOutputStream (IDEA-152607/IDEA-155415/IDEA-161663/EA-210021)
    (cherry picked from commit 73a19c7511079313d21fa9353b9a98473c5a1121)
    (cherry picked from commit 7499d912cbbdbfa71f34045b25bb2f20a69a6fdf)
    
    GitOrigin-RevId: 97e688fa94ad229c6dc696b43a69df0fc3df07c3

--- platform/platform-api/src/com/intellij/openapi/vfs/SafeWriteRequestor.java
+++ platform/platform-api/src/com/intellij/openapi/vfs/SafeWriteRequestor.java
@@ -1,5 +1,7 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vfs;
+
+import com.intellij.ide.GeneralSettings;
 
 import java.nio.file.Path;
 
@@ -10,4 +12,8 @@ import java.nio.file.Path;
  *
  * @author max
  */
-public interface SafeWriteRequestor { }
+public interface SafeWriteRequestor {
+  static boolean shouldUseSafeWrite(Object requestor) {
+    return requestor instanceof SafeWriteRequestor && GeneralSettings.getInstance().isUseSafeWrite();
+  }
+}
