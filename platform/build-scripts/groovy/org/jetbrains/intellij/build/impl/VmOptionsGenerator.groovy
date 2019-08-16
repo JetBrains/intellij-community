b065e1983b87575ead24f979ca894d26731b2c91
Egor Ushakov <egor.ushakov@jetbrains.com>
2019-08-16 17:42:13 +0300

    IDEA-220848 Disable "WARNING: An illegal reflective access operation has occurred"
    GitOrigin-RevId: 47c118d6629c5d502674a5ce87204d304f23226a

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/VmOptionsGenerator.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/VmOptionsGenerator.groovy
@@ -21,6 +21,7 @@ class VmOptionsGenerator {
       '-XX:-OmitStackTraceInFastThrow',
       '-Djdk.attach.allowAttachSelf=true',
       '-Dkotlinx.coroutines.debug=off',
+      '-Djdk.module.illegalAccess.silent=true',
     ]
 
   static String computeVmOptions(JvmArchitecture arch, boolean isEAP, ProductProperties productProperties) {
