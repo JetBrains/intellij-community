133f3d84d6e7258c969b572c61e6647500b55f34
Kirill Likhodedov <Kirill.Likhodedov@gmail.com>
2019-08-18 19:16:05 +0300

    vcs-log: make Update Project share the same recent filters as the main log: IDEA-220907
    GitOrigin-RevId: b71b56ea19d6d23805d26dbae433fd3f126c841f

--- plugins/git4idea/src/git4idea/update/GitUpdateProjectInfoLogProperties.java
+++ plugins/git4idea/src/git4idea/update/GitUpdateProjectInfoLogProperties.java
@@ -1,54 +1,62 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package git4idea.update;
 
+import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
 import com.intellij.openapi.components.StoragePathMacros;
-import com.intellij.util.xmlb.annotations.XCollection;
+import com.intellij.openapi.project.Project;
 import com.intellij.vcs.log.impl.VcsLogApplicationSettings;
 import com.intellij.vcs.log.impl.VcsLogProjectTabsProperties;
 import com.intellij.vcs.log.impl.VcsLogUiPropertiesImpl;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 @State(
   name = "Git.Update.Project.Info.Tabs.Properties",
   storages = {@Storage(StoragePathMacros.WORKSPACE_FILE)})
-public class GitUpdateProjectInfoLogProperties extends VcsLogUiPropertiesImpl<GitUpdateProjectInfoLogProperties.MyState> {
+public class GitUpdateProjectInfoLogProperties extends VcsLogUiPropertiesImpl<VcsLogUiPropertiesImpl.State> {
 
-  public MyState commonState = new MyState();
+  @NotNull private final Project myProject;
+  public VcsLogUiPropertiesImpl.State commonState = new VcsLogUiPropertiesImpl.State();
 
-  public static class MyState extends VcsLogUiPropertiesImpl.State {
-    @XCollection Map<String, List<VcsLogProjectTabsProperties.RecentGroup>> RECENT_FILTERS = new HashMap<>();
-  }
-
-  public GitUpdateProjectInfoLogProperties(@NotNull VcsLogApplicationSettings appSettings) {
+  public GitUpdateProjectInfoLogProperties(@NotNull Project project, @NotNull VcsLogApplicationSettings appSettings) {
     super(appSettings);
+    myProject = project;
   }
 
   @NotNull
   @Override
-  public MyState getState() {
+  public VcsLogUiPropertiesImpl.State getState() {
     return commonState;
   }
 
   @NotNull
   @Override
   public List<List<String>> getRecentlyFilteredGroups(@NotNull String filterName) {
-    return VcsLogProjectTabsProperties.getRecentGroup(commonState.RECENT_FILTERS, filterName);
+    VcsLogProjectTabsProperties.State state = getCommonState();
+    return state != null ? VcsLogProjectTabsProperties.getRecentGroup(state.RECENT_FILTERS, filterName) : Collections.emptyList();
   }
 
   @Override
-  public void loadState(@NotNull MyState state) {
+  public void loadState(@NotNull VcsLogUiPropertiesImpl.State state) {
     commonState = state;
   }
 
   @Override
   public void addRecentlyFilteredGroup(@NotNull String filterName, @NotNull Collection<String> values) {
-    VcsLogProjectTabsProperties.addRecentGroup(commonState.RECENT_FILTERS, filterName, values);
+    VcsLogProjectTabsProperties.State state = getCommonState();
+    if (state != null) {
+      VcsLogProjectTabsProperties.addRecentGroup(state.RECENT_FILTERS, filterName, values);
+    }
+  }
+
+  @Nullable
+  private VcsLogProjectTabsProperties.State getCommonState() {
+    return ServiceManager.getService(myProject, VcsLogProjectTabsProperties.class).getState();
   }
 }
