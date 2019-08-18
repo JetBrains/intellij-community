fdc3f50d63fcf6fd7fdff093175b4619ae79c68e
Aleksandr Krasilnikov <aleksandr.krasilnikov@jetbrains.com>
2019-08-18 18:02:49 +0300

    vcs: VcsCloneDialogUiSpec cleanup: unused constants and javadoc
    GitOrigin-RevId: 0c24afee367297b5ea0ac263d1a5aefcf418c453

--- platform/vcs-impl/src/com/intellij/util/ui/cloneDialog/VcsCloneDialogUiSpec.kt
+++ platform/vcs-impl/src/com/intellij/util/ui/cloneDialog/VcsCloneDialogUiSpec.kt
@@ -4,19 +4,11 @@ package com.intellij.util.ui.cloneDialog
 import com.intellij.util.ui.JBInsets
 import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.JBValue
-import com.intellij.util.ui.UIUtil
 
 /**
- * Contains a lot of UI specific constants for clone dialog that can be helpful for external implementations.
+ * Contains a lot of UI related constants for clone dialog that can be helpful for external implementations.
  */
 object VcsCloneDialogUiSpec {
-  object Dialog {
-    val mainComponentParentInsets = UIUtil.PANEL_REGULAR_INSETS.let {
-      // use empty right inset to align the scroll bar to the edge of panel
-      JBInsets(it.top, it.left, it.bottom, 0)
-    }
-  }
-
   object ExtensionsList {
     val iconSize = JBValue.UIInteger("VcsCloneDialog.iconSize", 22)
     const val iconTitleGap = 6
@@ -24,8 +16,6 @@ object VcsCloneDialogUiSpec {
   }
 
   object Components {
-    // insets for component without full-height scrollbar
-    val rightInsets = JBUI.insetsRight((UIUtil.PANEL_REGULAR_INSETS.right))
     val avatarSize = JBValue.UIInteger("VcsCloneDialog.Accounts.AvatarSize", 24)
   }
 }
