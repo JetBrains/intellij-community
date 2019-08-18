2edf49350d28e888d4f5f3adf3c8ef16ee0d3328
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-08-18 15:37:37 +0300

    [groovy] register file type in plugin.xml
    GitOrigin-RevId: 0517beff157090ddc8763ba611e4fe0227ff32db

--- plugins/groovy/src/org/jetbrains/plugins/groovy/GroovyFileTypeLoader.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/GroovyFileTypeLoader.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package org.jetbrains.plugins.groovy;
 
@@ -22,12 +8,14 @@ import com.intellij.openapi.util.text.StringUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.groovy.extensions.GroovyScriptTypeDetector;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 /**
  * @author ilyas
  */
-public class GroovyFileTypeLoader extends FileTypeFactory{
+public class GroovyFileTypeLoader extends FileTypeFactory {
 
   public static Set<String> getCustomGroovyScriptExtensions() {
     final LinkedHashSet<String> strings = new LinkedHashSet<>();
@@ -39,15 +27,8 @@ public class GroovyFileTypeLoader extends FileTypeFactory{
     return strings;
   }
 
-  private static List<String> getAllGroovyExtensions() {
-    final ArrayList<String> strings = new ArrayList<>();
-    strings.add(GroovyFileType.DEFAULT_EXTENSION);
-    strings.addAll(getCustomGroovyScriptExtensions());
-    return strings;
-  }
-
   @Override
   public void createFileTypes(@NotNull FileTypeConsumer consumer) {
-    consumer.consume(GroovyFileType.GROOVY_FILE_TYPE, StringUtil.join(getAllGroovyExtensions(), ";"));
+    consumer.consume(GroovyFileType.GROOVY_FILE_TYPE, StringUtil.join(getCustomGroovyScriptExtensions(), ";"));
   }
 }
