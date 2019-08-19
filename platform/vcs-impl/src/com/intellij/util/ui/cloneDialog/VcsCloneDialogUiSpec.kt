6368b78d7bb9d928cbdbd3f3bc6a7c970a94c378
Aleksandr Krasilnikov <aleksandr.krasilnikov@jetbrains.com>
2019-08-19 17:47:25 +0300

    vcs: Implement custom popup menu for clone dialog
    GitOrigin-RevId: e37847069516058fd626be7d98e33f0c0b578760

--- platform/vcs-impl/src/com/intellij/util/ui/cloneDialog/VcsCloneDialogUiSpec.kt
+++ platform/vcs-impl/src/com/intellij/util/ui/cloneDialog/VcsCloneDialogUiSpec.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.ui.cloneDialog
 
-import com.intellij.util.ui.JBInsets
 import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.JBValue
 
@@ -17,5 +16,6 @@ object VcsCloneDialogUiSpec {
 
   object Components {
     val avatarSize = JBValue.UIInteger("VcsCloneDialog.Accounts.AvatarSize", 24)
+    val popupMenuAvatarSize = JBValue.UIInteger("VcsCloneDialog.Accounts.Popup.AvatarSize", 40)
   }
 }
