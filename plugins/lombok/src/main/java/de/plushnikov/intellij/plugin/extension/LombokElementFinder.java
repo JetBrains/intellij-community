b838f071b7d2d0171711c0a4a9ef0f4d55846da1
Michail Plushnikov <mplushnikov@gmail.com>
2019-08-21 19:40:56 +0200

    simplify lombok inner class finder
    GitOrigin-RevId: 00ac4f1a8f8ff84c11b015ad1a1f9124a012498d

--- plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/extension/LombokElementFinder.java
+++ plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/extension/LombokElementFinder.java
@@ -3,18 +3,18 @@ package de.plushnikov.intellij.plugin.extension;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElementFinder;
-import com.intellij.psi.impl.java.stubs.index.JavaFullClassNameIndex;
+import com.intellij.psi.impl.file.impl.JavaFileManager;
 import com.intellij.psi.search.GlobalSearchScope;
-import de.plushnikov.intellij.plugin.util.PsiAnnotationSearchUtil;
-import lombok.Builder;
-import lombok.experimental.FieldNameConstants;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collection;
-import java.util.stream.Stream;
-
 public class LombokElementFinder extends PsiElementFinder {
+
+  private final JavaFileManager myFileManager;
+
+  public LombokElementFinder(Project project) {
+    myFileManager = JavaFileManager.getInstance(project);
+  }
 
   @Nullable
   @Override
@@ -23,6 +23,7 @@ public class LombokElementFinder extends PsiElementFinder {
     if (lastDot < 0) {
       return null;
     }
+
     final String parentName = qualifiedName.substring(0, lastDot);
     final String shortName = qualifiedName.substring(lastDot + 1);
 
@@ -30,37 +31,12 @@ public class LombokElementFinder extends PsiElementFinder {
       return null;
     }
 
-    final PsiClass parentClass = getPsiClassForFQN(parentName, scope);
+    final PsiClass parentClass = myFileManager.findClass(parentName, scope);
     if (null != parentClass) {
-      if (PsiAnnotationSearchUtil.isAnnotatedWith(parentClass, Builder.class, FieldNameConstants.class)) {
-        return Stream.of(parentClass.getInnerClasses()).filter(c->c.getName().equals(shortName)).findFirst().orElse(null);
-//        return parentClass.findInnerClassByName(shortName, false);
-      } else {
-//        final Collection<PsiMethod> psiMethods = PsiClassUtil.collectClassMethodsIntern(parentClass);
-//        for (PsiMethod psiMethod : psiMethods) {
-//          if (PsiAnnotationSearchUtil.isAnnotatedWith(psiMethod, Builder.class)) {
-//            return parentClass.findInnerClassByName(shortName, false);
-//          }
-//        }
-      }
+      return parentClass.findInnerClassByName(shortName, false);
     }
 
     return null;
-  }
-
-  private PsiClass getPsiClassForFQN(@NotNull String parentName, @NotNull GlobalSearchScope scope) {
-    final Project project = scope.getProject();
-    if (null != project) {
-      final Collection<PsiClass> classes = getJavaFullClassNameIndexInstance().get(parentName.hashCode(), project, scope);
-      if (!classes.isEmpty()) {
-        return classes.iterator().next();
-      }
-    }
-    return null;
-  }
-
-  JavaFullClassNameIndex getJavaFullClassNameIndexInstance() {
-    return JavaFullClassNameIndex.getInstance();
   }
 
   @NotNull
