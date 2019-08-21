a79ec1f885380612d8ec7fe0a3a4d43959073dd5
Dmitry Jemerov <yole@jetbrains.com>
2019-08-21 11:06:15 +0200

    Register syntax highlighters directly in XML when possible (work in progress)
    GitOrigin-RevId: 3480dde4157a0132d7e294ce8e4a68706ed724b4

--- plugins/sh/src/com/intellij/sh/ShLanguage.java
+++ plugins/sh/src/com/intellij/sh/ShLanguage.java
@@ -2,14 +2,11 @@
 package com.intellij.sh;
 
 import com.intellij.lang.Language;
-import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory;
-import com.intellij.sh.highlighter.ShHighlighterFactory;
 
 public class ShLanguage extends Language {
   public static final Language INSTANCE = new ShLanguage();
 
   public ShLanguage() {
     super("Shell Script", "application/x-bsh", "application/x-sh", "text/x-script.sh");
-    SyntaxHighlighterFactory.LANGUAGE_FACTORY.addExplicitExtension(this, new ShHighlighterFactory());
   }
 }
