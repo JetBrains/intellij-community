a64538829d3ca0b80e47b18a7b99c6ad5ff1c210
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-08-21 22:14:54 +0200

    [ui] moves date and IO error dialog messages to a platform bundle
    GitOrigin-RevId: 0257c6336d7ce134a178d0bf6de6f83268cc5d5e

--- platform/platform-api/src/com/intellij/util/net/IOExceptionDialog.java
+++ platform/platform-api/src/com/intellij/util/net/IOExceptionDialog.java
@@ -1,27 +1,13 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.net;
 
-import com.intellij.CommonBundle;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.util.Ref;
 import com.intellij.ui.GuiUtils;
+import com.intellij.ui.UIBundle;
 import com.intellij.util.ObjectUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -37,7 +23,7 @@ public class IOExceptionDialog extends DialogWrapper {
   public IOExceptionDialog(String title, String errorText)  {
     super((Project)null, true);
     setTitle(title);
-    setOKButtonText(CommonBundle.message("dialog.ioexception.tryagain"));
+    setOKButtonText(UIBundle.message("io.error.dialog.retry"));
 
     myErrorLabel = new JTextArea();
     myErrorLabel.setEditable(false);
@@ -61,7 +47,7 @@ public class IOExceptionDialog extends DialogWrapper {
   @Override
   protected Action[] createLeftSideActions() {
     return new Action[] {
-      new AbstractAction(CommonBundle.message("dialog.ioexception.proxy")) {
+      new AbstractAction(UIBundle.message("io.error.dialog.no.proxy")) {
         @Override
         public void actionPerformed(@NotNull ActionEvent e) {
           HttpConfigurable.editConfigurable(ObjectUtils.tryCast(e.getSource(), JComponent.class));
--- platform/platform-api/src/com/intellij/util/ui/SelectDateDialog.java
+++ platform/platform-api/src/com/intellij/util/ui/SelectDateDialog.java
@@ -1,28 +1,13 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.ui;
 
-import com.intellij.CommonBundle;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.ui.UIBundle;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.Date;
-
 
 public class SelectDateDialog extends DialogWrapper {
   private CalendarView myCalendarView;
@@ -31,14 +16,14 @@ public class SelectDateDialog extends DialogWrapper {
   public SelectDateDialog(Project project) {
     super(project, true);
     init();
-    setTitle(CommonBundle.message("dialog.title.choose.date"));
+    setTitle(UIBundle.message("date.dialog.title"));
     pack();
   }
 
   public SelectDateDialog(Component component) {
     super(component, true);
     init();
-    setTitle(CommonBundle.message("dialog.title.choose.date"));
+    setTitle(UIBundle.message("date.dialog.title"));
     pack();
   }
 
