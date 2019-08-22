d89b20e65d13dc13e91208dd2e9caece6dda4124
Oleg Stotsky <oleg.stotsky@jetbrains.com>
2019-08-22 20:45:50 +0300

    Add JpsCachePluginComponent
    GitOrigin-RevId: 5ddc6753c9ee1d4c571ad80d951a8c16fda9676e

---
+++ plugins/jps-cache/src/com/intellij/jps/cache/JpsCachePluginComponent.java
@@ -0,0 +1,100 @@
+package com.intellij.jps.cache;
+
+import com.intellij.openapi.application.PathManager;
+import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ProjectFileIndex;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileManager;
+import com.intellij.openapi.vfs.newvfs.BulkFileListener;
+import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class JpsCachePluginComponent implements ProjectComponent {
+  private static final Logger LOG = Logger.getInstance("com.intellij.jps.cache.JpsCachePluginComponent");
+  Project project;
+  ProjectRootManager projectRootManager;
+  ProjectFileIndex projectFileIndex;
+  PersistentCachingModuleHashingService moduleHashingService;
+  Map<Module, List<VirtualFile>> moduleToContentSourceRoots;
+
+  public JpsCachePluginComponent(Project project) {
+    this.project = project;
+    this.projectRootManager = ProjectRootManager.getInstance(project);
+    this.projectFileIndex = ProjectFileIndex.getInstance(project);
+    File hashCacheFile = new File(getCacheFileName());
+    try {
+      this.moduleHashingService = new PersistentCachingModuleHashingService(hashCacheFile);
+    }
+    catch (IOException e) {
+      LOG.warn(e);
+    }
+  }
+
+  @Override
+  public void projectOpened() {
+    VirtualFile[] sourcesRoots = projectRootManager.getContentSourceRoots();
+    project.getMessageBus().connect().subscribe(VirtualFileManager.VFS_CHANGES, new VFSListener());
+    moduleToContentSourceRoots =
+      Arrays.stream(sourcesRoots).collect(Collectors.groupingBy(sourceRoot -> projectFileIndex.getModuleForFile(sourceRoot)));
+    for (Map.Entry<Module, List<VirtualFile>> entry : moduleToContentSourceRoots.entrySet()) {
+      String moduleName = entry.getKey().getName();
+      File[] sourceRoots = entry.getValue().stream().map(virtualFile -> new File(virtualFile.getPath())).toArray(File[]::new);
+      try {
+        moduleHashingService.hashDirectoriesAndPersist(moduleName, sourceRoots);
+      }
+      catch (IOException e) {
+        LOG.warn(e);
+      }
+    }
+  }
+
+  private String getCacheFileName() {
+    File pluginsDir = new File(PathManager.getPluginsPath());
+    String projectPathHash = "";
+    try {
+      MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+      messageDigest.update(project.getBasePath().getBytes());
+      projectPathHash = new String(messageDigest.digest());
+    }
+    catch (NoSuchAlgorithmException e) {
+      LOG.warn(e);
+    }
+    return FileUtil.join(pluginsDir.getPath(), project.getName() + "_" + projectPathHash, "hashCaches");
+  }
+
+  private class VFSListener implements BulkFileListener {
+    @Override
+    public void after(@NotNull List<? extends VFileEvent> events) {
+      Module[] affectedModules =
+        events.stream().map(vFileEvent -> vFileEvent.getFile()).map(projectFileIndex::getModuleForFile).toArray(Module[]::new);
+      for (Module affectedModule : affectedModules) {
+        List<VirtualFile> virtualContentSourceRoots = moduleToContentSourceRoots.get(affectedModule);
+        if (virtualContentSourceRoots == null) {
+          continue;
+        }
+        File[] contentSourceRoots =
+          virtualContentSourceRoots.stream().map(virtualFile -> new File(virtualFile.getPath())).toArray(File[]::new);
+        try {
+          moduleHashingService.hashDirectoriesAndPersist(affectedModule.getName(), contentSourceRoots);
+        }
+        catch (IOException e) {
+          LOG.error(e);
+        }
+      }
+    }
+  }
+}
