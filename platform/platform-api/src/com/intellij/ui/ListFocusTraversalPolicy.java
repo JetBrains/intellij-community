d8f21a125a1fb0765bffe193c5bf86649b307162
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-08-22 20:34:36 +0300

    IDEA-CR-51695: set focus order in Find in Path dialog (IDEA-119140)
    (cherry picked from commit 0fc403afa9a91775438ae16824fa60eb856eea9a)
    
    GitOrigin-RevId: 147c781883aa7fc7b4853a182cc7c8e1d0666fad

---
+++ platform/platform-api/src/com/intellij/ui/ListFocusTraversalPolicy.java
@@ -0,0 +1,77 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.ui;
+
+import gnu.trove.TObjectIntHashMap;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+import java.util.List;
+
+/**
+ * Policy which defines explicit focus component cycle.
+ */
+public class ListFocusTraversalPolicy extends ContainerOrderFocusTraversalPolicy {
+
+  private final Component[] myComponents;
+  private final TObjectIntHashMap<Component> myComponentToIndex;
+
+  public ListFocusTraversalPolicy(@NotNull List<? extends Component> components) {
+    myComponents = components.toArray(new Component[0]);
+    myComponentToIndex = indexMap(myComponents);
+  }
+
+  @Override
+  public Component getFirstComponent(Container aContainer) {
+    return myComponents[0];
+  }
+
+  @Override
+  public Component getLastComponent(Container aContainer) {
+    return myComponents[myComponents.length - 1];
+  }
+
+  @Override
+  protected boolean accept(Component aComponent) {
+    return super.accept(aComponent) && aComponent.isShowing();
+  }
+
+  @Override
+  public Component getComponentAfter(Container aContainer, Component aComponent) {
+    if (!myComponentToIndex.containsKey(aComponent)) {
+      return null;
+    }
+    for (int index = myComponentToIndex.get(aComponent) + 1; index < myComponents.length; index++) {
+      Component result = myComponents[index];
+      if (accept(result)) {
+        return result;
+      }
+    }
+    return null;
+  }
+
+  @Override
+  public Component getComponentBefore(Container aContainer, Component aComponent) {
+    if (!myComponentToIndex.containsKey(aComponent)) {
+      return null;
+    }
+    for (int index = myComponentToIndex.get(aComponent) - 1; index >= 0; index--) {
+      Component result = myComponents[index];
+      if (accept(result)) {
+        return result;
+      }
+    }
+    return null;
+  }
+
+  @NotNull
+  private static <X> TObjectIntHashMap<X> indexMap(@NotNull X[] array) {
+    TObjectIntHashMap<X> map = new TObjectIntHashMap<>(array.length);
+    for (X x : array) {
+      if (!map.contains(x)) {
+        map.put(x, map.size());
+      }
+    }
+    map.compact();
+    return map;
+  }
+}
