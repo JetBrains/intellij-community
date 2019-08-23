7cb3cae22ecaf78660d76d0866e64df6a893a36e
Anton Tarasov <anton.tarasov@jetbrains.com>
2019-08-23 14:27:51 +0300

    JBR-1783 [windows] light top border on inactive dark frame
    (cherry picked from commit 826ff79b7af2fadfa0825281fa68937904830dd3)
    
    GitOrigin-RevId: 115326cf1b0f71aaec41c084aeecdb4cb0da5630

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/customFrameDecorations/header/CustomHeader.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/customFrameDecorations/header/CustomHeader.kt
@@ -19,6 +19,9 @@ import com.intellij.ui.scale.ScaleContext
 import com.intellij.ui.scale.ScaleType
 import com.intellij.util.ui.JBFont
 import com.intellij.util.ui.JBUI
+import com.intellij.util.ui.UIUtil
+import com.sun.jna.platform.win32.Advapi32Util
+import com.sun.jna.platform.win32.WinReg
 import java.awt.*
 import java.awt.event.*
 import javax.swing.*
@@ -253,7 +256,7 @@ abstract class CustomHeader(private val window: Window) : JPanel(), Disposable {
         }
 
         override fun paintBorder(c: Component, g: Graphics, x: Int, y: Int, width: Int, height: Int) {
-            if (isTopNeeded() && (myActive && isAffectsBorder() || !myActive)) {
+            if (isTopNeeded() && (myActive && isAffectsBorder()) || (!myActive && UIUtil.isUnderIntelliJLaF())) {
                 g.color = if (myActive) activeColor else inactiveColor
                 LinePainter2D.paint(g as Graphics2D, x.toDouble(), y.toDouble(), width.toDouble(), y.toDouble())
             }
