6e2de9a2be950f209e033ef1aae60a0abd71339c
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2019-08-25 23:52:43 +0300

    implement internal tracing profiler
    Actions and proto-agent to instrument current IDE instance and a toolwindow to show collected information in real time.
    
    GitOrigin-RevId: 30f015ac1abc8af634ae7cc21ae05a9c26fca734

---
+++ platform/util/src/com/intellij/diagnostic/tracing/MethodTracerData.java
@@ -0,0 +1,60 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.diagnostic.tracing;
+
+public class MethodTracerData {
+  public final String id;
+  public final String className;
+  public final String methodName;
+  public final long invocationCount;
+  public final long nonRecursiveCount;
+  public final long totalTime;
+  public final long maxTime;
+
+  public final long countOnEdt;
+  public final long timeOnEdt;
+  public final long maxTimeOnEdt;
+
+  public final int maxRecursionDepth;
+
+  public MethodTracerData(String id,
+                          String className,
+                          String methodName,
+                          long invocationCount,
+                          long nonRecursiveCount,
+                          long totalTime,
+                          long maxTime,
+                          long countOnEdt,
+                          long timeOnEdt,
+                          long maxTimeOnEdt,
+                          int maxRecursionDepth) {
+    this.id = id;
+    this.className = className;
+    this.methodName = methodName;
+    this.invocationCount = invocationCount;
+    this.nonRecursiveCount = nonRecursiveCount;
+    this.totalTime = totalTime;
+    this.maxTime = maxTime;
+    this.maxRecursionDepth = maxRecursionDepth;
+    this.countOnEdt = countOnEdt;
+    this.timeOnEdt = timeOnEdt;
+    this.maxTimeOnEdt = maxTimeOnEdt;
+  }
+
+  public double getAvgTime() {
+    return average(totalTime, nonRecursiveCount);
+  }
+
+  public double getAvgTimeEdt() {
+    return average(timeOnEdt, countOnEdt);
+  }
+
+  private static double average(long total, long count) {
+    if (count == 0) {
+      return 0;
+    }
+    double avg = (double)total / count;
+
+    return (double)(Math.round(avg * 100)) / 100;
+  }
+
+}
