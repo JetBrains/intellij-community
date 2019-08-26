b17805f4c7c37edd7f7667a1876b4767dda44610
Aleksey Pivovarov <AMPivovarov@gmail.com>
2019-08-26 18:34:44 +0300

    vcs: cleanup tests
    GitOrigin-RevId: fbf8b7bd86a2fc013145ab1629c3de5dc01187c4

--- platform/vcs-tests/testSrc/com/intellij/openapi/vcs/DirectoryMappingListTest.kt
+++ platform/vcs-tests/testSrc/com/intellij/openapi/vcs/DirectoryMappingListTest.kt
@@ -23,53 +23,57 @@ import java.io.File
 class DirectoryMappingListTest : HeavyPlatformTestCase() {
   private val BASE_PATH = "/vcs/directoryMappings/"
 
-  private lateinit var myMappings: NewMappings
-  private lateinit var myProjectRoot: VirtualFile
-  private lateinit var myRootPath: String
-  private lateinit var myVcsManager: ProjectLevelVcsManagerImpl
+  private lateinit var mappings: NewMappings
+  private lateinit var projectRoot: VirtualFile
+  private lateinit var rootPath: String
+
+  private lateinit var vcsManager: ProjectLevelVcsManagerImpl
+
+  private lateinit var vcsMock: MockAbstractVcs
+  private lateinit var vcsMock2: MockAbstractVcs
+  private lateinit var vcsCVS: MockAbstractVcs
 
   override fun setUpProject() {
     val root = FileUtil.toSystemIndependentName(VcsTestUtil.getTestDataPath() + BASE_PATH)
 
-    myProjectRoot = PsiTestUtil.createTestProjectStructure(getTestName(true), null, root, myFilesToDelete, false)
-    myRootPath = myProjectRoot.path
+    projectRoot = PsiTestUtil.createTestProjectStructure(getTestName(true), null, root, myFilesToDelete, false)
+    rootPath = projectRoot.path
 
-    myProject = ProjectManagerEx.getInstanceEx().loadProject("$myRootPath/directoryMappings.ipr")
+    myProject = ProjectManagerEx.getInstanceEx().loadProject("$rootPath/directoryMappings.ipr")
     ProjectManagerEx.getInstanceEx().openTestProject(myProject)
     UIUtil.dispatchAllInvocationEvents() // startup activities
 
     val startupManager = StartupManager.getInstance(myProject) as StartupManagerImpl
     startupManager.runStartupActivities()
-    val vcses = AllVcses.getInstance(myProject)
-    vcses.registerManually(MockAbstractVcs(myProject, "mock"))
-    vcses.registerManually(MockAbstractVcs(myProject, "CVS"))
-    vcses.registerManually(MockAbstractVcs(myProject, "mock2"))
 
-    myVcsManager = ProjectLevelVcsManager.getInstance(myProject) as ProjectLevelVcsManagerImpl
-    myMappings = NewMappings(myProject, myVcsManager, DefaultVcsRootPolicy.getInstance(myProject))
-    Disposer.register(testRootDisposable, myMappings)
+    vcsMock = MockAbstractVcs(myProject, "mock")
+    vcsMock2 = MockAbstractVcs(myProject, "mock2")
+    vcsCVS = MockAbstractVcs(myProject, "CVS")
+
+    val vcses = AllVcses.getInstance(myProject)
+    vcses.registerManually(vcsMock)
+    vcses.registerManually(vcsMock2)
+    vcses.registerManually(vcsCVS)
+
+    vcsManager = ProjectLevelVcsManager.getInstance(myProject) as ProjectLevelVcsManagerImpl
+    mappings = NewMappings(myProject, vcsManager, DefaultVcsRootPolicy.getInstance(myProject))
+    Disposer.register(testRootDisposable, mappings)
     startupManager.runPostStartupActivities()
-    myVcsManager.waitForInitialized()
+    vcsManager.waitForInitialized()
   }
 
   fun testMappingsFilter() {
-    (myVcsManager.findVcsByName("mock") as MockAbstractVcs).setAllowNestedRoots(true)
+    vcsMock.setAllowNestedRoots(true)
 
-    val pathsStr = listOf("$myRootPath/a",
-                          "$myRootPath/a/b",
-                          "$myRootPath/def",
-                          "$myRootPath/a-b",
-                          "$myRootPath/a-b/d-e",
-                          "$myRootPath/a-b1/d-e")
-    val a = myProjectRoot.findChild("a")!!
-    createChildDirectory(a, "b")
-    createChildDirectory(myProjectRoot, "def")
-    val ab = myProjectRoot.findChild("a-b")!!
-    val ab1 = createChildDirectory(myProjectRoot, "a-b1")
-    createChildDirectory(ab, "d-e")
-    createChildDirectory(ab1, "d-e")
+    val pathsStr = listOf("$rootPath/a",
+                          "$rootPath/a/b",
+                          "$rootPath/def",
+                          "$rootPath/a-b",
+                          "$rootPath/a-b/d-e",
+                          "$rootPath/a-b1/d-e")
+    createDirectories(pathsStr)
 
-    myVcsManager.directoryMappings = listOf(
+    vcsManager.directoryMappings = listOf(
       VcsDirectoryMapping(pathsStr[0], "mock"),
       VcsDirectoryMapping(pathsStr[1], "mock"),
       VcsDirectoryMapping(pathsStr[2], "mock"),
@@ -77,122 +81,119 @@ class DirectoryMappingListTest : HeavyPlatformTestCase() {
       VcsDirectoryMapping(pathsStr[4], "mock2"),
       VcsDirectoryMapping(pathsStr[5], "mock2"))
 
-    val paths = mutableListOf<FilePath>()
-    for (path in pathsStr) {
-      paths.add(VcsUtil.getFilePath(path, true))
-    }
+    assertEquals(6, vcsManager.directoryMappings.size)
+    assertEquals(3, vcsManager.getRootsUnderVcs(vcsMock).size)
+    assertEquals(2, vcsManager.getRootsUnderVcs(vcsMock2).size) // No nested roots allowed
 
-    assertEquals(6, myVcsManager.directoryMappings.size)
+    val paths = pathsStr.map { VcsUtil.getFilePath(it, true) }
     val filePaths = DescindingFilesFilter.filterDescindingFiles(paths.toTypedArray(), myProject)
     assertEquals(5, filePaths.size)
   }
 
   fun testSamePrefix() {
-    val childA = myProjectRoot.findChild("a")!!
-    val childAB = myProjectRoot.findChild("a-b")!!
+    val childA = projectRoot.findChild("a")!!
+    val childAB = projectRoot.findChild("a-b")!!
 
-    myMappings.setMapping("$myRootPath/a", "CVS")
-    myMappings.setMapping("$myRootPath/a-b", "mock2")
-    assertEquals(2, myMappings.directoryMappings.size)
-    myMappings.cleanupMappings()
-    assertEquals(2, myMappings.directoryMappings.size)
+    mappings.setMapping("$rootPath/a", "CVS")
+    mappings.setMapping("$rootPath/a-b", "mock2")
+    assertEquals(2, mappings.directoryMappings.size)
+
+    mappings.cleanupMappings()
+    assertEquals(2, mappings.directoryMappings.size)
     assertEquals("mock2", getVcsFor(childAB))
     assertEquals("CVS", getVcsFor(childA))
   }
 
   fun testSamePrefixEmpty() {
-    val childAB = myProjectRoot.findChild("a-b")!!
+    val childAB = projectRoot.findChild("a-b")!!
 
-    myMappings.setMapping("$myRootPath/a", "CVS")
+    mappings.setMapping("$rootPath/a", "CVS")
     assertNull(getVcsFor(childAB))
   }
 
   fun testSame() {
-    myMappings.setMapping("$myRootPath/parent/path1", "CVS")
-    myMappings.setMapping("$myRootPath\\parent\\path2", "CVS")
+    mappings.setMapping("$rootPath/parent/path1", "CVS")
+    mappings.setMapping("$rootPath\\parent\\path2", "CVS")
 
     val children = listOf(
-      "$myRootPath\\parent\\path1",
-      "$myRootPath/parent/path1",
-      "$myRootPath\\parent\\path1",
-      "$myRootPath\\parent\\path2",
-      "$myRootPath/parent/path2",
-      "$myRootPath\\parent\\path2"
+      "$rootPath\\parent\\path1",
+      "$rootPath/parent/path1",
+      "$rootPath\\parent\\path1",
+      "$rootPath\\parent\\path2",
+      "$rootPath/parent/path2",
+      "$rootPath\\parent\\path2"
     )
-    createFiles(children)
+    createDirectories(children)
 
     for (child in children) {
-      myMappings.setMapping(child, "CVS")
-      myMappings.cleanupMappings()
-      assertEquals("cleanup failed: $child", 2, myMappings.directoryMappings.size)
+      mappings.setMapping(child, "CVS")
+      mappings.cleanupMappings()
+      assertEquals("cleanup failed: $child", 2, mappings.directoryMappings.size)
     }
 
     for (child in children) {
-      myMappings.setMapping(child, "CVS")
-      assertEquals("cleanup failed: $child", 2, myMappings.directoryMappings.size)
+      mappings.setMapping(child, "CVS")
+      assertEquals("cleanup failed: $child", 2, mappings.directoryMappings.size)
     }
   }
 
   fun testHierarchy() {
-    myMappings.setMapping("$myRootPath/parent", "CVS")
+    mappings.setMapping("$rootPath/parent", "CVS")
 
     val children = listOf(
-      "$myRootPath/parent/child1",
-      "$myRootPath/parent/middle/child2",
-      "$myRootPath/parent/middle/child3"
+      "$rootPath/parent/child1",
+      "$rootPath/parent/middle/child2",
+      "$rootPath/parent/middle/child3"
     )
-    createFiles(children)
+    createDirectories(children)
 
     for (child in children) {
-      myMappings.setMapping(child, "CVS")
-      myMappings.cleanupMappings()
-      assertEquals("cleanup failed: $child", 1, myMappings.directoryMappings.size)
+      mappings.setMapping(child, "CVS")
+      mappings.cleanupMappings()
+      assertEquals("cleanup failed: $child", 1, mappings.directoryMappings.size)
     }
   }
 
   fun testNestedInnerCopy() {
-    myMappings.setMapping("$myRootPath/parent", "CVS")
-    myMappings.setMapping("$myRootPath/parent/child", "mock")
+    mappings.setMapping("$rootPath/parent", "CVS")
+    mappings.setMapping("$rootPath/parent/child", "mock")
 
     val children = listOf(
-      "$myRootPath/parent/child1",
-      "$myRootPath\\parent\\middle\\child2",
-      "$myRootPath/parent/middle/child3",
-      "$myRootPath/parent/child/inner"
+      "$rootPath/parent/child1",
+      "$rootPath\\parent\\middle\\child2",
+      "$rootPath/parent/middle/child3",
+      "$rootPath/parent/child/inner"
     )
-    createFiles(children)
+    val files = createDirectories(children)
 
-    myMappings.waitMappedRootsUpdate()
+    mappings.waitMappedRootsUpdate()
 
     val awaitedVcsNames = listOf("CVS", "CVS", "CVS", "mock")
-    val lfs = LocalFileSystem.getInstance()
     for (i in children.indices) {
-      val child = children[i]
-      val vf = lfs.refreshAndFindFileByIoFile(File(child))!!
-      assertNotNull("No file for: $child", vf)
-      val mapping = getMappingFor(vf)
-      assertNotNull("No mapping for: $vf", mapping)
-      assertEquals(awaitedVcsNames[i], mapping!!.vcs)
+      val mapping = getMappingFor(files[i])
+      assertEquals(awaitedVcsNames[i], mapping?.vcs)
     }
   }
 
-  private fun createFiles(paths: List<String>) {
-    for (path in paths) {
-      val file = File(FileUtil.toSystemDependentName(path))
-      val created = file.mkdirs()
-      assertTrue("Can't create file: $file", created || file.isDirectory)
-      myFilesToDelete.add(file)
-    }
-    LocalFileSystem.getInstance().refreshIoFiles(myFilesToDelete)
+  private fun createDirectories(paths: List<String>): List<VirtualFile> {
+    return paths.map { createDirectory(it) }
+  }
+
+  private fun createDirectory(path: String): VirtualFile {
+    val file = File(FileUtil.toSystemDependentName(path))
+    val created = file.exists() && file.isDirectory || file.mkdirs()
+    assertTrue("Can't create file: $file", created)
+    myFilesToDelete.add(file)
+    return LocalFileSystem.getInstance().refreshAndFindFileByIoFile(file)!!
   }
 
   private fun getVcsFor(file: VirtualFile): String? {
-    val root = myMappings.getMappedRootFor(file)
+    val root = mappings.getMappedRootFor(file)
     return root?.vcs?.name
   }
 
   private fun getMappingFor(file: VirtualFile): VcsDirectoryMapping? {
-    val root = myMappings.getMappedRootFor(file)
+    val root = mappings.getMappedRootFor(file)
     return root?.mapping
   }
 }
