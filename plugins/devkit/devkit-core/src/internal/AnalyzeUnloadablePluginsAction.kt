222a71a4b73115460b5dbb85fc6848802caecf0a
Dmitry Jemerov <yole@jetbrains.com>
2019-08-26 13:57:40 +0200

    Report to show which plugins can be unloaded without restart and which EPs block that
    GitOrigin-RevId: 3e2de9bfe823e1da9d6e94689f2fb993e1b3fc2d

---
+++ plugins/devkit/devkit-core/src/internal/AnalyzeUnloadablePluginsAction.kt
@@ -0,0 +1,126 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.idea.devkit.internal
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.runReadAction
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.psi.search.FilenameIndex
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.xml.XmlFile
+import com.intellij.ui.components.dialog
+import com.intellij.ui.layout.*
+import com.intellij.util.xml.DomManager
+import org.jetbrains.idea.devkit.dom.Extension
+import org.jetbrains.idea.devkit.dom.IdeaPlugin
+import org.jetbrains.idea.devkit.util.DescriptorUtil
+import org.jetbrains.jps.model.java.JavaResourceRootType
+import org.jetbrains.jps.model.java.JavaSourceRootType
+import org.jetbrains.jps.model.module.JpsModuleSourceRootType
+import javax.swing.JScrollPane
+import javax.swing.JTextArea
+
+/**
+ * @author yole
+ */
+class AnalyzeUnloadablePluginsAction : AnAction() {
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.project ?: return
+
+    val result = mutableListOf<PluginUnloadabilityStatus>()
+    ProgressManager.getInstance().runProcessWithProgressSynchronously(
+      {
+        val rootTypes = setOf<JpsModuleSourceRootType<*>>(JavaSourceRootType.SOURCE, JavaResourceRootType.RESOURCE)
+        runReadAction {
+          val pluginXmlFiles = FilenameIndex.getFilesByName(project, "plugin.xml", GlobalSearchScope.projectScope(project))
+          for (pluginXmlFile in pluginXmlFiles) {
+            ProgressManager.checkCanceled()
+            if (pluginXmlFile !is XmlFile) continue
+            if (!ProjectRootManager.getInstance(project).fileIndex.isUnderSourceRootOfType(pluginXmlFile.virtualFile, rootTypes)) {
+              continue
+            }
+            val ideaPlugin = DescriptorUtil.getIdeaPlugin(pluginXmlFile) ?: continue
+            result.add(analyzeUnloadable(project, ideaPlugin))
+          }
+        }
+      }, "Analyzing plugins", true, e.project)
+
+    showReport(project, result)
+  }
+
+  private fun showReport(project: Project, result: MutableList<PluginUnloadabilityStatus>) {
+    val report = buildString {
+      if (result.any { it.analysisErrors.isNotEmpty() }) {
+        appendln("Analysis errors:")
+        for (status in result.filter { it.analysisErrors.isNotEmpty() }) {
+          appendln(status.pluginId)
+          for (analysisError in status.analysisErrors) {
+            appendln(analysisError)
+          }
+          appendln()
+        }
+      }
+
+      val unloadablePlugins = result.filter { it.componentCount == 0 && it.nonDynamicEPs.isEmpty() }
+      appendln("Can unload ${unloadablePlugins.size} plugins:")
+      for (status in unloadablePlugins) {
+        appendln(status.pluginId)
+      }
+      appendln()
+
+      appendln("Plugins using components:")
+      for (status in result.filter { it.componentCount > 0 }) {
+        appendln(status.pluginId)
+      }
+      appendln()
+
+      val epUsagesMap = mutableMapOf<String, Int>()
+      for (pluginUnloadabilityStatus in result) {
+        for (ep in pluginUnloadabilityStatus.nonDynamicEPs) {
+          epUsagesMap[ep] = epUsagesMap.getOrDefault(ep, 0) + 1
+        }
+      }
+
+      appendln("EP usage statistics:")
+      val epUsagesList = epUsagesMap.toList().sortedByDescending { it.second }
+      for (pair in epUsagesList) {
+        appendln("${pair.second}: ${pair.first}")
+      }
+    }
+
+    dialog("Plugin Analysis Report", panel = panel {
+      row {
+        JScrollPane(JTextArea(report, 20, 80))()
+      }
+    }).show()
+  }
+
+  private fun analyzeUnloadable(project: Project, ideaPlugin: IdeaPlugin): PluginUnloadabilityStatus {
+    val nonDynamicEPs = mutableSetOf<String>()
+    val analysisErrors = mutableListOf<String>()
+    for (extensionTag in ideaPlugin.extensions.flatMap { it.xmlTag.subTags.toList() }) {
+      val domElement = DomManager.getDomManager(project).getDomElement(extensionTag)
+      val extension = domElement as? Extension ?: continue
+      val ep = extension.extensionPoint
+      if (ep == null) {
+        analysisErrors.add("Cannot resolve EP ${extension.xmlElementName}")
+        continue
+      }
+      if (ep.dynamic.value != true) {
+        nonDynamicEPs.add(ep.effectiveQualifiedName)
+      }
+    }
+    val componentCount = ideaPlugin.applicationComponents.flatMap { it.components }.size +
+                         ideaPlugin.projectComponents.flatMap { it.components }.size
+    return PluginUnloadabilityStatus(ideaPlugin.pluginId ?: "?", nonDynamicEPs, componentCount, analysisErrors)
+  }
+}
+
+private data class PluginUnloadabilityStatus(
+  val pluginId: String,
+  val nonDynamicEPs: Set<String>,
+  val componentCount: Int,
+  val analysisErrors: List<String>
+)
