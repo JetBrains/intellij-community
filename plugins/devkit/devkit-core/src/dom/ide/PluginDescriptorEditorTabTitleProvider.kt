4e825062bea34a32faee4bf9d81ea8188acf5e38
Yann Cébron <yann.cebron@jetbrains.com>
2019-08-27 17:31:39 +0200

    [devkit] PluginDescriptorEditorTabTitleProvider: handle <null> pluginID
    GitOrigin-RevId: 625c3ae15c2502b28aa94f33e2f6b2ad2492d83a

--- plugins/devkit/devkit-core/src/dom/ide/PluginDescriptorEditorTabTitleProvider.kt
+++ plugins/devkit/devkit-core/src/dom/ide/PluginDescriptorEditorTabTitleProvider.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.devkit.dom.ide
 
 import com.intellij.ide.plugins.PluginManagerCore
@@ -17,6 +17,6 @@ class PluginDescriptorEditorTabTitleProvider : EditorTabTitleProvider {
     val xmlFile = PsiManager.getInstance(project).findFile(file) as? XmlFile ?: return null
 
     val ideaPlugin = DescriptorUtil.getIdeaPlugin(xmlFile) ?: return null
-    return "${PluginManagerCore.PLUGIN_XML} (${ideaPlugin.pluginId})"
+    return "${PluginManagerCore.PLUGIN_XML} (${ideaPlugin.pluginId ?: "<unknown>"})"
   }
 }
