404233f94a638ee563bc79e468166d97d9aae975
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2019-08-27 16:23:03 +0300

    [stats-collector] Extract interface ContextFeatures
    GitOrigin-RevId: 5c9b94bed1172d416a9a7f3be9d3c097fb2dbe7e

---
+++ platform/lang-api/src/com/intellij/codeInsight/completion/ml/ContextFeatures.java
@@ -0,0 +1,16 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.codeInsight.completion.ml;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public interface ContextFeatures {
+  @Nullable
+  Boolean binaryValue(@NotNull String name);
+
+  @Nullable
+  Double floatValue(@NotNull String name);
+
+  @Nullable
+  String categoricalValue(@NotNull String name);
+}
