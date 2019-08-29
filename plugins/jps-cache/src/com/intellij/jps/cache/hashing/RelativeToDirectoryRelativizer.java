1d74b7fdbfb91ba807bb93a5bd522f77c0e7184c
Oleg Stotsky <oleg.stotsky@jetbrains.com>
2019-08-29 14:28:50 +0300

    Make RelativeToDirectoryRelativizer platform independent
    GitOrigin-RevId: 0858f82ed2a16dc5559c6b87dd7e29f80c4bfc17

--- plugins/jps-cache/src/com/intellij/jps/cache/hashing/RelativeToDirectoryRelativizer.java
+++ plugins/jps-cache/src/com/intellij/jps/cache/hashing/RelativeToDirectoryRelativizer.java
@@ -1,18 +1,21 @@
 package com.intellij.jps.cache.hashing;
 
+import com.intellij.openapi.util.io.FileUtilRt;
+
 import java.io.File;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 class RelativeToDirectoryRelativizer implements PathRelativizer {
-  private final String rootPath;
+  private final Path rootPath;
 
   RelativeToDirectoryRelativizer(String rootModulePath) {
-    this.rootPath = rootModulePath;
+    this.rootPath = Paths.get(rootModulePath);
   }
 
   @Override
   public String relativize(File target) {
-    return Paths.get(rootPath).relativize(Paths.get(target.getPath())).toString();
+    return FileUtilRt.toSystemIndependentName(rootPath.relativize(Paths.get(target.getPath())).toString());
   }
 }
 
