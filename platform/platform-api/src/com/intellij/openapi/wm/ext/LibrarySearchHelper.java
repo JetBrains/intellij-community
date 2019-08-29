3479e37686dcd93d4867a00cacc99ebc408fabc9
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2019-08-29 17:32:10 +0300

    @NotNull project parameter in LibrarySearchHelper
    GitOrigin-RevId: 3450d0a8a3892b86f56f7a1349504239e143a58d

--- platform/platform-api/src/com/intellij/openapi/wm/ext/LibrarySearchHelper.java
+++ platform/platform-api/src/com/intellij/openapi/wm/ext/LibrarySearchHelper.java
@@ -1,7 +1,8 @@
 package com.intellij.openapi.wm.ext;
 
 import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
 
 public interface LibrarySearchHelper {
-    boolean isLibraryExists(Project project);
+    boolean isLibraryExists(@NotNull Project project);
 }
