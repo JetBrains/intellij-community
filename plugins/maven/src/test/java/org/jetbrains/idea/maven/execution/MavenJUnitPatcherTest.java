6cbff7150a68515d6cc469efd1dc7ecad8aff4e9
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2019-08-30 17:15:59 +0700

    MavenJUnitPatcherTest#testAddClassPath
    GitOrigin-RevId: ff7f15a5abb246909a7ef7ce911d8e0e7feff338

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/execution/MavenJUnitPatcherTest.java
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/execution/MavenJUnitPatcherTest.java
@@ -21,6 +21,8 @@ import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.idea.maven.MavenImportingTestCase;
 import org.jetbrains.idea.maven.project.MavenProjectSettings;
 
+import java.util.List;
+
 import static java.util.Arrays.asList;
 
 public class MavenJUnitPatcherTest extends MavenImportingTestCase {
@@ -31,6 +33,44 @@ public class MavenJUnitPatcherTest extends MavenImportingTestCase {
     MavenProjectSettings.getInstance(myProject).getTestRunningSettings().setPassArgLine(true);
     MavenProjectSettings.getInstance(myProject).getTestRunningSettings().setPassEnvironmentVariables(true);
     MavenProjectSettings.getInstance(myProject).getTestRunningSettings().setPassSystemProperties(true);
+  }
+
+  public void testAddClassPath() {
+    VirtualFile m1 = createModulePom("m1", "<groupId>test</groupId>\n" +
+                                           "<artifactId>m1</artifactId>\n" +
+                                           "<version>1</version>\n" +
+                                           "<dependencies>\n" +
+                                           "  <dependency>\n" +
+                                           "    <groupId>test</groupId>\n" +
+                                           "    <artifactId>m2</artifactId>\n" +
+                                           "    <version>1</version>\n" +
+                                           "  </dependency>\n" +
+                                           "</dependencies>\n" +
+                                           "<build>\n" +
+                                           "  <plugins>\n" +
+                                           "    <plugin>\n" +
+                                           "      <groupId>org.apache.maven.plugins</groupId>\n" +
+                                           "      <artifactId>maven-surefire-plugin</artifactId>\n" +
+                                           "      <version>2.16</version>\n" +
+                                           "      <configuration>\n" +
+                                           "        <additionalClasspathElements>\n" +
+                                           "          <additionalClasspathElement>path/to/additional/resources</additionalClasspathElement>\n" +
+                                           "          <additionalClasspathElement>path/to/additional/jar</additionalClasspathElement>\n" +
+                                           "          <additionalClasspathElement>path/to/csv/jar1</additionalClasspathElement>\n" +
+                                           "        </additionalClasspathElements>\n" +
+                                           "      </configuration>\n" +
+                                           "    </plugin>\n" +
+                                           "  </plugins>\n" +
+                                           "</build>\n");
+
+    importProjects(m1);
+    Module module = getModule("m1");
+
+    MavenJUnitPatcher mavenJUnitPatcher = new MavenJUnitPatcher();
+    JavaParameters javaParameters = new JavaParameters();
+    mavenJUnitPatcher.patchJavaParameters(module, javaParameters);
+    List<String> classPath = javaParameters.getClassPath().getPathList();
+    assertEquals(asList("path/to/additional/resources", "path/to/additional/jar", "path/to/csv/jar1"), classPath);
   }
 
   public void testArgList() {
