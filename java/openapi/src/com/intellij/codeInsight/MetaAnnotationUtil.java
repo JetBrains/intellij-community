78efd6e964219029c0b8235112e3048d7d79e71a
peter <peter@jetbrains.com>
2019-08-30 12:34:49 +0200

    simpler getAllAnnotationFilesScope (IDEA-CR-51755)
    GitOrigin-RevId: 9ebfd07585250103c11052b32b42d9415e473cd9

--- java/openapi/src/com/intellij/codeInsight/MetaAnnotationUtil.java
+++ java/openapi/src/com/intellij/codeInsight/MetaAnnotationUtil.java
@@ -13,23 +13,29 @@
 // limitations under the License.
 package com.intellij.codeInsight;
 
-import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.fileTypes.FileTypeManager;
+import com.intellij.openapi.fileTypes.StdFileTypes;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileWithId;
 import com.intellij.psi.*;
+import com.intellij.psi.compiled.ClassFileDecompilers;
+import com.intellij.psi.impl.java.stubs.index.JavaStubIndexKeys;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.search.searches.AnnotatedElementsSearch;
-import com.intellij.psi.util.CachedValue;
-import com.intellij.psi.util.CachedValueProvider;
-import com.intellij.psi.util.CachedValuesManager;
-import com.intellij.psi.util.PsiModificationTracker;
+import com.intellij.psi.search.searches.DirectClassInheritorsSearch;
+import com.intellij.psi.stubs.StubIndex;
+import com.intellij.psi.util.*;
 import com.intellij.util.containers.ConcurrentFactoryMap;
 import com.intellij.util.containers.ContainerUtil;
+import com.intellij.util.indexing.IdIterator;
 import gnu.trove.THashSet;
+import gnu.trove.TIntHashSet;
 import gnu.trove.TObjectHashingStrategy;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -70,7 +76,7 @@ public abstract class MetaAnnotationUtil {
         }
 
         // limit search to files containing annotations
-        GlobalSearchScope effectiveSearchScope = ServiceManager.getService(project, MetaAnnotationUtil.class).getAllAnnotationFilesScope(project).intersectWith(moduleScope);
+        GlobalSearchScope effectiveSearchScope = getAllAnnotationFilesScope(project).intersectWith(moduleScope);
         return getAnnotationTypesWithChildren(annotationClass, effectiveSearchScope);
       });
       return CachedValueProvider.Result.create(factoryMap, PsiModificationTracker.JAVA_STRUCTURE_MODIFICATION_COUNT);
@@ -125,7 +131,94 @@ public abstract class MetaAnnotationUtil {
     return classes;
   }
 
-  protected abstract GlobalSearchScope getAllAnnotationFilesScope(Project project);
+  private static GlobalSearchScope getAllAnnotationFilesScope(Project project) {
+    return CachedValuesManager.getManager(project).getCachedValue(project, () -> {
+      GlobalSearchScope javaScope = new FileIdScope(project, getJavaAnnotationInheritorIds(project));
+      GlobalSearchScope otherScope = searchForAnnotationInheritorsInOtherLanguages(project);
+      return CachedValueProvider.Result.createSingleDependency(
+        javaScope.uniteWith(otherScope),
+        PsiModificationTracker.MODIFICATION_COUNT);
+    });
+  }
+
+  @NotNull
+  private static GlobalSearchScope searchForAnnotationInheritorsInOtherLanguages(Project project) {
+    Set<VirtualFile> allAnnotationFiles = new HashSet<>();
+    for (PsiClass javaLangAnnotation : JavaPsiFacade.getInstance(project)
+      .findClasses(CommonClassNames.JAVA_LANG_ANNOTATION_ANNOTATION, GlobalSearchScope.allScope(project))) {
+      DirectClassInheritorsSearch.search(javaLangAnnotation, new NonJavaScope(project), false).forEach(annotationClass -> {
+        ProgressManager.checkCanceled();
+        ContainerUtil.addIfNotNull(allAnnotationFiles, PsiUtilCore.getVirtualFile(annotationClass));
+        return true;
+      });
+    }
+
+    return GlobalSearchScope.filesWithLibrariesScope(project, allAnnotationFiles);
+  }
+
+  @NotNull
+  private static TIntHashSet getJavaAnnotationInheritorIds(Project project) {
+    IdIterator iterator = StubIndex.getInstance().getContainingIds(JavaStubIndexKeys.SUPER_CLASSES, "Annotation", project,
+                                                                   GlobalSearchScope.allScope(project));
+    TIntHashSet idSet = new TIntHashSet();
+    while (iterator.hasNext()) {
+      idSet.add(iterator.next());
+    }
+    return idSet;
+  }
+
+  private static class FileIdScope extends GlobalSearchScope {
+    private final TIntHashSet myIdSet;
+
+    FileIdScope(Project project, TIntHashSet idSet) {
+      super(project);
+      myIdSet = idSet;
+    }
+
+    @Override
+    public boolean isSearchInModuleContent(@NotNull Module aModule) {
+      return true;
+    }
+
+    @Override
+    public boolean isSearchInLibraries() {
+      return true;
+    }
+
+    @Override
+    public boolean contains(@NotNull VirtualFile file) {
+      return file instanceof VirtualFileWithId && myIdSet.contains(((VirtualFileWithId)file).getId());
+    }
+  }
+
+  private static class NonJavaScope extends GlobalSearchScope {
+    NonJavaScope(Project project) {
+      super(project);
+    }
+
+    @Override
+    public boolean isSearchInModuleContent(@NotNull Module aModule) {
+      return true;
+    }
+
+    @Override
+    public boolean isSearchInLibraries() {
+      return true;
+    }
+
+    @Override
+    public boolean contains(@NotNull VirtualFile file) {
+      if (FileTypeManager.getInstance().isFileOfType(file, StdFileTypes.JAVA)) {
+        return false;
+      }
+
+      if (FileTypeManager.getInstance().isFileOfType(file, StdFileTypes.CLASS)) {
+        return ClassFileDecompilers.find(file) instanceof ClassFileDecompilers.Full;
+      }
+
+      return true;
+    }
+  }
 
   private static void collectClassWithChildren(PsiClass psiClass, Set<? super PsiClass> classes, GlobalSearchScope scope) {
     classes.add(psiClass);
