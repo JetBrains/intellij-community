55d5aa7209bbcdb7668585781a755f61c987ca5c
Anastasia Ivanova <anastasia.ivanova@jetbrains.com>
2019-08-30 15:20:11 +0700

    IDEA-CR-51967: FUS: add action to customize whitelist path
    GitOrigin-RevId: e708c4c759f8fd8931a3ff59013af842c768e97a

--- platform/platform-impl/src/com/intellij/internal/statistic/eventLog/validator/persistence/EventLogWhitelistPersistence.java
+++ platform/platform-impl/src/com/intellij/internal/statistic/eventLog/validator/persistence/EventLogWhitelistPersistence.java
@@ -1,8 +1,11 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.internal.statistic.eventLog.validator.persistence;
 
+import com.intellij.internal.statistic.eventLog.EventLogExternalSettingsService;
+import com.intellij.internal.statistic.service.fus.FUStatisticsWhiteListGroupsService;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.util.text.StringUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -12,23 +15,36 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 
-
 public class EventLogWhitelistPersistence extends BaseEventLogWhitelistPersistence {
+  public static final String WHITE_LIST_DATA_FILE = "white-list.json";
   private static final Logger LOG =
     Logger.getInstance("#com.intellij.internal.statistic.eventLog.validator.persistence.EventLogWhitelistPersistence");
-
-  private static final String WHITE_LIST_DATA_FILE = "white-list.json";
+  @NotNull
+  private final String myRecorderId;
+  @NotNull
+  private final EventLogExternalSettingsService mySettingsService;
+  @NotNull
+  private final EventLogWhitelistSettingsPersistence myWhitelistSettingsPersistence;
 
   public EventLogWhitelistPersistence(@NotNull String recorderId) {
-    super(recorderId, WHITE_LIST_DATA_FILE);
+    myRecorderId = recorderId;
+    mySettingsService = new EventLogExternalSettingsService(recorderId);
+    myWhitelistSettingsPersistence = EventLogWhitelistSettingsPersistence.getInstance();
   }
 
   @Override
   @Nullable
   public String getCachedWhitelist() {
     try {
-      File file = getWhitelistFile();
-      if (!file.exists()) initBuiltinWhiteList(file);
+      WhitelistPathSettings settings = myWhitelistSettingsPersistence.getPathSettings(myRecorderId);
+      File file;
+      if (settings != null && settings.isUseCustomPath()) {
+        file = new File(settings.getCustomPath());
+      }
+      else {
+        file = getDefaultPath();
+        if (!file.exists()) initBuiltinWhiteList(file);
+      }
       if (file.exists()) return FileUtil.loadFile(file);
     }
     catch (IOException e) {
@@ -37,11 +53,36 @@ public class EventLogWhitelistPersistence extends BaseEventLogWhitelistPersisten
     return null;
   }
 
-  public void cacheWhiteList(@NotNull String gsonWhiteListContent, long lastModified) {
+  public void updateWhiteListIfNeeded() {
+    WhitelistPathSettings settings = myWhitelistSettingsPersistence.getPathSettings(myRecorderId);
+    if (settings != null && settings.isUseCustomPath()) {
+      return;
+    }
+
+    final long lastModified = FUStatisticsWhiteListGroupsService.lastModifiedWhitelist(mySettingsService);
+    if (LOG.isTraceEnabled()) {
+      LOG.trace(
+        "Loading whitelist, last modified cached=" + getLastModified() +
+        ", last modified on the server=" + lastModified
+      );
+    }
+
+    if (lastModified <= 0 || lastModified > getLastModified()) {
+      final String content = FUStatisticsWhiteListGroupsService.loadWhiteListFromServer(mySettingsService);
+      if (StringUtil.isNotEmpty(content)) {
+        cacheWhiteList(content, lastModified);
+        if (LOG.isTraceEnabled()) {
+          LOG.trace("Update local whitelist, last modified cached=" + getLastModified());
+        }
+      }
+    }
+  }
+
+  private void cacheWhiteList(@NotNull String gsonWhiteListContent, long lastModified) {
     try {
-      final File file = getWhitelistFile();
+      final File file = getDefaultPath();
       FileUtil.writeToFile(file, gsonWhiteListContent);
-      EventLogWhitelistSettingsPersistence.getInstance().setLastModified(myRecorderId, lastModified);
+      myWhitelistSettingsPersistence.setLastModified(myRecorderId, lastModified);
     }
     catch (IOException e) {
       LOG.error(e);
@@ -59,10 +100,15 @@ public class EventLogWhitelistPersistence extends BaseEventLogWhitelistPersisten
   }
 
   private String builtinWhiteListPath() {
-    return "resources/" + FUS_WHITELIST_PATH + "/" + myRecorderId + "/" + myWhitelistFileName;
+    return "resources/" + FUS_WHITELIST_PATH + "/" + myRecorderId + "/" + WHITE_LIST_DATA_FILE;
   }
 
-  public long getLastModified() {
-    return EventLogWhitelistSettingsPersistence.getInstance().getLastModified(myRecorderId);
+  private long getLastModified() {
+    return myWhitelistSettingsPersistence.getLastModified(myRecorderId);
+  }
+
+  @NotNull
+  private File getDefaultPath() throws IOException {
+    return getDefaultWhitelistPath(myRecorderId, WHITE_LIST_DATA_FILE);
   }
 }
