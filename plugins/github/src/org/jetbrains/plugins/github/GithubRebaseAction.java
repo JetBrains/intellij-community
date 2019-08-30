7147c13fb8f82c22d9ef40166c89452d932fc251
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2019-08-30 14:38:40 +0700

    GithubRebaseAction: use default branch reported by GitHub instead of hardcoded "master" branch (part of IDEA-123266)
    GitOrigin-RevId: 68a694b4f8c0aebe84446389e0b4e23654df2b9d

--- plugins/github/src/org/jetbrains/plugins/github/GithubRebaseAction.java
+++ plugins/github/src/org/jetbrains/plugins/github/GithubRebaseAction.java
@@ -25,6 +25,7 @@ import git4idea.util.GitPreservingProcess;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.github.api.*;
+import org.jetbrains.plugins.github.api.data.GithubRepo;
 import org.jetbrains.plugins.github.api.data.GithubRepoDetailed;
 import org.jetbrains.plugins.github.authentication.accounts.GithubAccount;
 import org.jetbrains.plugins.github.util.GithubGitHelper;
@@ -42,6 +43,7 @@ import static git4idea.fetch.GitFetchSupport.fetchSupport;
 public class GithubRebaseAction extends AbstractGithubUrlGroupingAction {
   private static final Logger LOG = GithubUtil.LOG;
   private static final String CANNOT_PERFORM_GITHUB_REBASE = "Can't perform GitHub rebase";
+  private static final String UPSTREAM_REMOTE = "upstream";
 
   public GithubRebaseAction() {
     super("Rebase my GitHub fork", "Rebase your GitHub forked repository relative to the origin", AllIcons.Vcs.Vendors.Github);
@@ -63,7 +65,7 @@ public class GithubRebaseAction extends AbstractGithubUrlGroupingAction {
       GithubNotifications.showError(project, CANNOT_PERFORM_GITHUB_REBASE, "Invalid Github remote: " + remoteUrl);
       return;
     }
-    new RebaseTask(project, executor, Git.getInstance(), account.getServer(), repository, repoPath, "upstream/master").queue();
+    new RebaseTask(project, executor, Git.getInstance(), account.getServer(), repository, repoPath).queue();
   }
 
   private static class RebaseTask extends Task.Backgroundable {
@@ -72,22 +74,19 @@ public class GithubRebaseAction extends AbstractGithubUrlGroupingAction {
     @NotNull private final GithubServerPath myServer;
     @NotNull private final GitRepository myRepository;
     @NotNull private final GHRepositoryPath myRepoPath;
-    @NotNull private final String myOnto;
 
     RebaseTask(@NotNull Project project,
                @NotNull GithubApiRequestExecutor requestExecutor,
                @NotNull Git git,
                @NotNull GithubServerPath server,
                @NotNull GitRepository repository,
-               @NotNull GHRepositoryPath repoPath,
-               @NotNull String rebaseOnto) {
+               @NotNull GHRepositoryPath repoPath) {
       super(project, "Rebasing GitHub Fork...");
       myRequestExecutor = requestExecutor;
       myGit = git;
       myServer = server;
       myRepository = repository;
       myRepoPath = repoPath;
-      myOnto = rebaseOnto;
     }
 
     @Override
@@ -100,21 +99,28 @@ public class GithubRebaseAction extends AbstractGithubUrlGroupingAction {
         if ((upstreamRemoteUrl = configureUpstreamRemote(indicator)) == null) return;
       }
 
-      if (isUpstreamWithSameUsername(indicator, upstreamRemoteUrl)) {
+      GHRepositoryPath userAndRepo = GithubUrlUtil.getUserAndRepositoryFromRemoteUrl(upstreamRemoteUrl);
+      if (userAndRepo == null) {
+        GithubNotifications.showError(myProject, CANNOT_PERFORM_GITHUB_REBASE, "Can't validate upstream remote: " + upstreamRemoteUrl);
+        return;
+      }
+      if (isUpstreamWithSameUsername(indicator, userAndRepo)) {
         GithubNotifications.showError(myProject, CANNOT_PERFORM_GITHUB_REBASE,
                                       "Configured upstream seems to be your own repository: " + upstreamRemoteUrl);
         return;
       }
+      String name = getDefaultBranchName(indicator, userAndRepo);
+      String onto = UPSTREAM_REMOTE + "/" + name;
 
       LOG.info("Fetching upstream");
       indicator.setText("Fetching upstream...");
-      if (!fetchParent(indicator)) {
+      if (!fetchParent()) {
         return;
       }
 
       LOG.info("Rebasing current branch");
-      indicator.setText("Rebasing current branch...");
-      rebaseCurrentBranch(indicator);
+      indicator.setText("Rebasing current branch onto " + onto + "...");
+      rebaseCurrentBranch(indicator, onto);
     }
 
     @Nullable
@@ -127,19 +133,31 @@ public class GithubRebaseAction extends AbstractGithubUrlGroupingAction {
         .map(GitRemote::getFirstUrl).orElse(null);
     }
 
-    private boolean isUpstreamWithSameUsername(@NotNull ProgressIndicator indicator, @NotNull String upstreamRemoteUrl) {
+    private boolean isUpstreamWithSameUsername(@NotNull ProgressIndicator indicator, @NotNull GHRepositoryPath userAndRepo) {
       try {
-        GHRepositoryPath userAndRepo = GithubUrlUtil.getUserAndRepositoryFromRemoteUrl(upstreamRemoteUrl);
-        if (userAndRepo == null) {
-          GithubNotifications.showError(myProject, CANNOT_PERFORM_GITHUB_REBASE, "Can't validate upstream remote: " + upstreamRemoteUrl);
-          return true;
-        }
         String username = myRequestExecutor.execute(indicator, GithubApiRequests.CurrentUser.get(myServer)).getLogin();
         return userAndRepo.getOwner().equals(username);
       }
       catch (IOException e) {
         GithubNotifications.showError(myProject, CANNOT_PERFORM_GITHUB_REBASE, "Can't get user information");
         return true;
+      }
+    }
+
+    private String getDefaultBranchName(@NotNull ProgressIndicator indicator, @NotNull GHRepositoryPath userAndRepo) {
+      try {
+        GithubRepo repo = myRequestExecutor.execute(indicator,
+                                                    GithubApiRequests.Repos.get(myServer, userAndRepo.getOwner(), userAndRepo.getRepository()));
+        if (repo == null) {
+          GithubNotifications.showError(myProject, CANNOT_PERFORM_GITHUB_REBASE, "Can't retrieve upstream information for " + userAndRepo);
+          return null;
+        }
+        return repo.getDefaultBranch();
+      }
+      catch (IOException e) {
+        GithubNotifications
+          .showError(myProject, CANNOT_PERFORM_GITHUB_REBASE, "Can't retrieve upstream information for " + userAndRepo, e.getMessage());
+        return null;
       }
     }
 
@@ -162,11 +180,11 @@ public class GithubRebaseAction extends AbstractGithubUrlGroupingAction {
       LOG.info("Adding GitHub parent as a remote host");
       indicator.setText("Adding GitHub parent as a remote host...");
       try {
-        myGit.addRemote(myRepository, "upstream", parentRepoUrl).throwOnError();
+        myGit.addRemote(myRepository, UPSTREAM_REMOTE, parentRepoUrl).throwOnError();
       }
       catch (VcsException e) {
         GithubNotifications
-          .showError(myProject, CANNOT_PERFORM_GITHUB_REBASE, "Could not configure \"upstream\" remote:\n" + e.getMessage());
+          .showError(myProject, CANNOT_PERFORM_GITHUB_REBASE, "Could not configure \"" + UPSTREAM_REMOTE + "\" remote:\n" + e.getMessage());
         return null;
       }
       myRepository.update();
@@ -187,8 +205,8 @@ public class GithubRebaseAction extends AbstractGithubUrlGroupingAction {
       }
     }
 
-    private boolean fetchParent(@NotNull final ProgressIndicator indicator) {
-      GitRemote remote = GitUtil.findRemoteByName(myRepository, "upstream");
+    private boolean fetchParent() {
+      GitRemote remote = GitUtil.findRemoteByName(myRepository, UPSTREAM_REMOTE);
       if (remote == null) {
         LOG.warn("Couldn't find remote " + " remoteName " + " in " + myRepository);
         return false;
@@ -196,25 +214,25 @@ public class GithubRebaseAction extends AbstractGithubUrlGroupingAction {
       return fetchSupport(myProject).fetch(myRepository, remote).showNotificationIfFailed();
     }
 
-    private void rebaseCurrentBranch(@NotNull ProgressIndicator indicator) {
+    private void rebaseCurrentBranch(@NotNull ProgressIndicator indicator, String onto) {
       try (AccessToken ignore = DvcsUtil.workingTreeChangeStarted(myProject, "Rebase")) {
         List<VirtualFile> rootsToSave = Collections.singletonList(myRepository.getRoot());
         GitVcsSettings.UpdateChangesPolicy saveMethod = GitVcsSettings.getInstance(myProject).updateChangesPolicy();
-        GitPreservingProcess process = new GitPreservingProcess(myProject, myGit, rootsToSave, "Rebasing", myOnto,
+        GitPreservingProcess process = new GitPreservingProcess(myProject, myGit, rootsToSave, "Rebasing", onto,
                                                                 saveMethod, indicator,
-                                                                () -> doRebaseCurrentBranch(indicator));
+                                                                () -> doRebaseCurrentBranch(indicator, onto));
         process.execute();
       }
     }
 
-    private void doRebaseCurrentBranch(@NotNull ProgressIndicator indicator) {
+    private void doRebaseCurrentBranch(@NotNull ProgressIndicator indicator, String onto) {
       GitRepositoryManager repositoryManager = GitUtil.getRepositoryManager(myProject);
       GitRebaser rebaser = new GitRebaser(myProject, myGit, indicator);
       VirtualFile root = myRepository.getRoot();
 
       GitLineHandler handler = new GitLineHandler(myProject, root, GitCommand.REBASE);
       handler.setStdoutSuppressed(false);
-      handler.addParameters(myOnto);
+      handler.addParameters(onto);
 
       final GitRebaseProblemDetector rebaseConflictDetector = new GitRebaseProblemDetector();
       handler.addLineListener(rebaseConflictDetector);
@@ -227,7 +245,7 @@ public class GithubRebaseAction extends AbstractGithubUrlGroupingAction {
       handler.addLineListener(GitStandardProgressAnalyzer.createListener(indicator));
 
       String oldText = indicator.getText();
-      indicator.setText("Rebasing onto " + myOnto + "...");
+      indicator.setText("Rebasing onto " + onto + "...");
       GitCommandResult rebaseResult = myGit.runCommand(handler);
       indicator.setText(oldText);
       repositoryManager.updateRepository(root);
