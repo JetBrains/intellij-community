14512d293d3d1962102c208a34e5f9cec5a3a1da
Alexander Zolotov <zolotov@jetbrains.com>
2019-08-30 18:17:36 +0300

    Convert tcServiceMessages library to maven (IDEA-220225)
    GitOrigin-RevId: 6673dcae49008a735f8d28b921167f68bd4056ad

--- platform/smRunner/testSrc/com/intellij/execution/testframework/sm/runner/SMTRunnerIntegrationTest.java
+++ platform/smRunner/testSrc/com/intellij/execution/testframework/sm/runner/SMTRunnerIntegrationTest.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.testframework.sm.runner;
 
 import com.intellij.execution.executors.DefaultRunExecutor;
@@ -43,12 +29,8 @@ public class SMTRunnerIntegrationTest extends LightPlatformTestCase {
     super.setUp();
     SMTRunnerConsoleProperties properties = new SMTRunnerConsoleProperties(new MockRuntimeConfiguration(getProject()),
                                                                            TEST_FRAMEWORK_NAME,
-                                                                           DefaultRunExecutor.getRunExecutorInstance()) {
-      @Override
-      public boolean isIdBasedTestTree() {
-        return true;
-      }
-    };
+                                                                           DefaultRunExecutor.getRunExecutorInstance());
+    properties.setIdBasedTestTree(true);
     myConsole = (SMTRunnerConsoleView)SMTestRunnerConnectionUtil.createConsole(TEST_FRAMEWORK_NAME, properties);
     myResultsViewer = myConsole.getResultsViewer();
     myRootNode = myResultsViewer.getTestsRootNode();
@@ -71,7 +53,7 @@ public class SMTRunnerIntegrationTest extends LightPlatformTestCase {
     }
   }
 
-  public void multiTestings() {
+  public void testMultiTestings() {
     notifyStdoutLineAvailable("##teamcity[enteredTheMatrix]");
     notifyStdoutLineAvailable("##teamcity[testingStarted]");
     assertState(0, 0, 0, ColorProgressBar.GREEN);
@@ -87,7 +69,7 @@ public class SMTRunnerIntegrationTest extends LightPlatformTestCase {
     assertState(0, 0, 0, ColorProgressBar.GREEN);
   }
 
-  public void failedThenSuccessTestings() {
+  public void testFailedThenSuccessTestings() {
     notifyStdoutLineAvailable("##teamcity[enteredTheMatrix]");
     notifyStdoutLineAvailable("##teamcity[testingStarted]");
     notifyStdoutLineAvailable("##teamcity[testSuiteStarted nodeId='1' parentNodeId='0' name='sum-test.js']");
