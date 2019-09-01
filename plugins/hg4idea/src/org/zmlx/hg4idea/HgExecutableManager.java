b0db35dbac7026097f5d05c43e7db75760bc656f
Aleksandr Krasilnikov <aleksandr.krasilnikov@jetbrains.com>
2019-09-01 17:42:32 +0300

    hg: detect hg executable in environment variables
    In order to display full path of detected hg executable (for consistency with how it works for Git)
    Example: "Auto-detected: hg.exe" -> "Auto-detected: C:\Program Files\TortoiseHg\hg.exe"
    
    GitOrigin-RevId: ce556331cade58c1202f60e97f4561a8436c9bfb

--- plugins/hg4idea/src/org/zmlx/hg4idea/HgExecutableManager.java
+++ plugins/hg4idea/src/org/zmlx/hg4idea/HgExecutableManager.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.zmlx.hg4idea;
 
+import com.intellij.execution.configurations.PathEnvironmentVariableUtil;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.AtomicNotNullLazyValue;
@@ -8,6 +9,7 @@ import com.intellij.openapi.util.SystemInfo;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -63,6 +65,13 @@ public class HgExecutableManager {
    */
   @NotNull
   private static String identifyDefaultHgExecutable() {
+    File hgExecutableFromPath = PathEnvironmentVariableUtil.findInPath(SystemInfo.isWindows ? DEFAULT_WINDOWS_HG : DEFAULT_UNIX_HG,
+                                                                       PathEnvironmentVariableUtil.getPathVariableValue(),
+                                                                       null);
+    if (hgExecutableFromPath != null) {
+      return hgExecutableFromPath.getPath();
+    }
+
     String[] paths;
     String programName;
     if (SystemInfo.isWindows) {
