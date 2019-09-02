7d7d57bd060581c7e5698807b89f04a34819bf7c
Dmitry Jemerov <yole@jetbrains.com>
2019-09-02 19:14:36 +0200

    Mark as ScheduledForRemoval
    GitOrigin-RevId: 57c6a8b5e9618b5b6b308ba82d56040f2beb862a

--- platform/platform-impl/src/com/intellij/ide/plugins/PluginManagerConfigurableNew.java
+++ platform/platform-impl/src/com/intellij/ide/plugins/PluginManagerConfigurableNew.java
@@ -1,12 +1,15 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.plugins;
 
+import org.jetbrains.annotations.ApiStatus;
+
 import java.awt.*;
 
 /**
  * Preserved for API compatibility
  */
 @Deprecated
+@ApiStatus.ScheduledForRemoval(inVersion = "2020.1")
 public class PluginManagerConfigurableNew {
   public static final Color MAIN_BG_COLOR = PluginManagerConfigurable.MAIN_BG_COLOR;
 }
