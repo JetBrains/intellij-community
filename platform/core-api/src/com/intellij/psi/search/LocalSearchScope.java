23d4a62d3cc16bcb100961a4af4573f7c59b94f4
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-09-02 16:57:12 +0300

    add ability to restrict search scope by file language
    GitOrigin-RevId: 146a4ec08cd1bd3a3ed4b07e87a1e7a590f2cea9

--- platform/core-api/src/com/intellij/psi/search/LocalSearchScope.java
+++ platform/core-api/src/com/intellij/psi/search/LocalSearchScope.java
@@ -1,20 +1,7 @@
-/*
- * Copyright 2000-2019 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.search;
 
+import com.intellij.lang.LanguageMatcher;
 import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.fileTypes.FileType;
@@ -29,6 +16,7 @@ import com.intellij.psi.StubBasedPsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.util.PsiUtilCore;
 import com.intellij.util.ArrayUtil;
+import com.intellij.util.containers.ContainerUtil;
 import gnu.trove.THashSet;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
@@ -284,6 +272,23 @@ public class LocalSearchScope extends SearchScope {
           result.add(element);
         }
       }
+      return result.isEmpty()
+             ? EMPTY
+             : new LocalSearchScope(PsiUtilCore.toPsiElementArray(result), scope.getDisplayName(), scope.isIgnoreInjectedPsi());
+    });
+  }
+
+  @Contract(pure = true)
+  @NotNull
+  static LocalSearchScope getScopeRestrictedByFileLanguage(@NotNull LocalSearchScope scope, @NotNull LanguageMatcher matcher) {
+    if (scope == EMPTY) {
+      return EMPTY;
+    }
+    return ReadAction.compute(() -> {
+      List<PsiElement> result = ContainerUtil.filter(
+        scope.getScope(),
+        element -> matcher.matchesLanguage(element.getContainingFile().getLanguage())
+      );
       return result.isEmpty()
              ? EMPTY
              : new LocalSearchScope(PsiUtilCore.toPsiElementArray(result), scope.getDisplayName(), scope.isIgnoreInjectedPsi());
