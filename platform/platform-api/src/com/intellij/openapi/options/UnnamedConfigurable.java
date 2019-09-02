77991cb1aa45a5ce760b908ef85f326a412ba173
Yann Cébron <yann.cebron@jetbrains.com>
2019-09-02 16:09:36 +0200

    UnnamedConfigurable: javadoc cosmetics
    GitOrigin-RevId: 85947e3eaadab2b4c668c92174e7d850cd62b655

--- platform/platform-api/src/com/intellij/openapi/options/UnnamedConfigurable.java
+++ platform/platform-api/src/com/intellij/openapi/options/UnnamedConfigurable.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.options;
 
 import org.jetbrains.annotations.Nullable;
@@ -21,7 +7,7 @@ import javax.swing.*;
 
 /**
  * This interface represents a configurable component that provides a Swing form to configure some settings.
- * Sometimes IDEA instantiates it on a background thread, so it is not recommended to create any Swing component in a constructor.
+ * Sometimes the IDE instantiates it on a background thread, so it is not recommended to create any Swing component in a constructor.
  * Use the <a href="https://docs.oracle.com/javase/tutorial/uiswing/concurrency/dispatch.html">EDT</a> instead.
  *
  * @author lesya
@@ -30,11 +16,11 @@ public interface UnnamedConfigurable {
   /**
    * Creates new Swing form that enables user to configure the settings.
    * Usually this method is called on the EDT, so it should not take a long time.
-   *
+   * <p>
    * Also this place is designed to allocate resources (subscriptions/listeners etc.)
-   * @see #disposeUIResources
    *
    * @return new Swing form to show, or {@code null} if it cannot be created
+   * @see #disposeUIResources
    */
   @Nullable
   JComponent createComponent();
