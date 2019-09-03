68c0cf3784000aea8ac2c397e8eefe4e4ebc39ca
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2019-09-03 15:49:52 +0300

    [stats-collector] Introduce API for ML models and extension point for completion models
    GitOrigin-RevId: 62b27bd04467fd40133b485a4d06a0b9159bae0c

---
+++ platform/platform-impl/src/com/intellij/internal/ml/completion/CompletionRankingModelBase.kt
@@ -0,0 +1,33 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.internal.ml.completion
+
+import com.intellij.internal.ml.DecisionFunction
+import com.intellij.internal.ml.FeatureMapper
+import com.intellij.internal.ml.ModelMetadata
+
+
+abstract class CompletionRankingModelBase(private val metadata: ModelMetadata) : DecisionFunction {
+  override fun getFeaturesOrder(): Array<FeatureMapper> {
+    return metadata.featuresOrder
+  }
+
+  override fun version(): String? {
+    return metadata.version
+  }
+
+  override fun getRequiredFeatures(): List<String> = emptyList()
+
+  override fun getUnknownFeatures(features: Collection<String>): List<String> {
+    var unknownFeatures: MutableList<String>? = null
+    for (featureName in features) {
+      if (featureName !in metadata.knownFeatures) {
+        if (unknownFeatures == null) {
+          unknownFeatures = mutableListOf()
+        }
+        unknownFeatures.add(featureName)
+      }
+    }
+
+    return unknownFeatures ?: emptyList()
+  }
+}
---
+++ platform/platform-impl/src/com/intellij/internal/ml/completion/JarCompletionModelProvider.kt
@@ -0,0 +1,20 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.internal.ml.completion
+
+import com.intellij.internal.ml.DecisionFunction
+import com.intellij.internal.ml.FeaturesInfo
+import com.intellij.internal.ml.ModelMetadata
+
+abstract class JarCompletionModelProvider(private val displayName: String,
+                                          private val resourceDirectory: String) : RankingModelProvider {
+  private val lazyModel: DecisionFunction by lazy {
+    val metadata = FeaturesInfo.buildFromResources(resourceDirectory)
+    createModel(metadata)
+  }
+
+  override fun getModel(): DecisionFunction = lazyModel
+
+  override fun getDisplayNameInSettings(): String = displayName
+
+  protected abstract fun createModel(metadata: ModelMetadata): DecisionFunction
+}
