6898998e8a0d35343f9e851c253388823f9c6a52
maksim.pelevin <maksim.pelevin@jetbrains.com>
2019-09-03 16:48:17 +0300

    IDEA-CR-51968 IDEA-56579 Use cast for setDefaults
    GitOrigin-RevId: a5cb53ef969de0dc6c056f41e8235a9894ff5a31

--- java/java-impl/src/com/intellij/find/findUsages/JavaFindUsagesDialog.java
+++ java/java-impl/src/com/intellij/find/findUsages/JavaFindUsagesDialog.java
@@ -46,9 +46,7 @@ public abstract class JavaFindUsagesDialog<T extends JavaFindUsagesOptions> exte
   @Override
   protected void init() {
     myIncludeOverloadedMethodsAvailable = myPsiElement instanceof PsiMethod && MethodSignatureUtil.hasOverloads((PsiMethod)myPsiElement);
-    if (myFindUsagesOptions instanceof JavaFindUsagesOptions) {
-      ((JavaFindUsagesOptions)myFindUsagesOptions).setDefaults(myProject);
-    }
+    ((T)myFindUsagesOptions).setDefaults(myProject);
     super.init();
   }
 
