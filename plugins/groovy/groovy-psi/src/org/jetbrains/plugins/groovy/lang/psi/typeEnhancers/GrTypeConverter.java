b33f1425ef2ea02b21bbaab895972178b8b5b1d6
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-09-04 18:58:22 +0300

    [groovy] remove GrTypeConverter#isAllowedInMethodCall
    GitOrigin-RevId: c05bce95d0752377c9f097b133af7507b819fadc

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/typeEnhancers/GrTypeConverter.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/typeEnhancers/GrTypeConverter.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.lang.psi.typeEnhancers;
 
 import com.intellij.openapi.extensions.ExtensionPointName;
@@ -61,28 +47,8 @@ public abstract class GrTypeConverter {
     return expression instanceof GrLiteral ? (GrLiteral)expression : null;
   }
 
-  /**
-   * @deprecated see {@link #isApplicableTo(org.jetbrains.plugins.groovy.lang.psi.typeEnhancers.GrTypeConverter.ApplicableTo)}
-   */
-  @Deprecated
-  public boolean isAllowedInMethodCall() {
-    return false;
-  }
-
   public boolean isApplicableTo(@NotNull ApplicableTo position) {
-    switch (position) {
-      case EXPLICIT_CAST:
-        return false;
-      case ASSIGNMENT:
-        return true;
-      case METHOD_PARAMETER:
-        //noinspection deprecation
-        return isAllowedInMethodCall();
-      case RETURN_VALUE:
-        return true;
-      default:
-        return false;
-    }
+    return position == ApplicableTo.ASSIGNMENT || position == ApplicableTo.RETURN_VALUE;
   }
 
   /**
