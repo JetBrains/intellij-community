16b922adabb8bd0ad3e2c046455194778b93dc1c
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2019-09-05 20:55:52 +0300

    [git] simplify return type
    (cherry picked from commit 668b4a3673a68f6f06e85bdad26d9c7ff427320c)
    
    GitOrigin-RevId: d9daf51026f14982c22c085327e21406f93e288d

--- plugins/git4idea/src/git4idea/update/GitUpdateInfoAsLog.kt
+++ plugins/git4idea/src/git4idea/update/GitUpdateInfoAsLog.kt
@@ -73,7 +73,7 @@ class GitUpdateInfoAsLog(private val project: Project,
     if (!isPathFilterSet()) {
       // if no path filters is set, we don't need the log to show the notification
       // => schedule the log tab and return the data
-      val (logUi, factory) = createLogTabInEdtAndWait(logManager)
+      val factory = createLogTabInEdtAndWait(logManager)
       return NotificationData(commitsAndFiles.updatedFilesCount, commitsAndFiles.receivedCommitsCount, null,
                               getViewCommitsAction(factory, factory.rangeFilter))
     }
@@ -135,12 +135,12 @@ class GitUpdateInfoAsLog(private val project: Project,
     return CommitsAndFiles(updatedFilesCount, updatedCommitsCount)
   }
 
-  private fun createLogTabInEdtAndWait(logManager: VcsLogManager): Pair<VcsLogUiImpl, MyLogUiFactory> {
-    val logUi = Ref.create<Pair<VcsLogUiImpl, MyLogUiFactory>>()
+  private fun createLogTabInEdtAndWait(logManager: VcsLogManager): MyLogUiFactory {
+    val logFactory = Ref.create<MyLogUiFactory>()
     ApplicationManager.getApplication().invokeAndWait {
-      logUi.set(createLogTab(logManager, null))
+      logFactory.set(createLogTab(logManager, null))
     }
-    return logUi.get()
+    return logFactory.get()
   }
 
   private fun getViewCommitsAction(factory: MyLogUiFactory, rangeFilter: VcsLogRangeFilter): Runnable {
@@ -157,14 +157,13 @@ class GitUpdateInfoAsLog(private val project: Project,
   }
 
   private fun createLogTab(logManager: VcsLogManager,
-                           listenerGetter: ((VcsLogUiImpl, MyLogUiFactory) -> VisiblePackChangeListener)?):
-    Pair<VcsLogUiImpl, MyLogUiFactory> {
+                           listenerGetter: ((VcsLogUiImpl, MyLogUiFactory) -> VisiblePackChangeListener)?): MyLogUiFactory {
     val rangeFilter = VcsLogFilterObject.fromRange(ranges.values.map {
       VcsLogRangeFilter.RefRange(it.start.asString(), it.end.asString())
     })
     val logUiFactory = MyLogUiFactory(logManager, rangeFilter, listenerGetter)
-    val logUi = createLogUiAndTab(logManager, logUiFactory, select = false)
-    return Pair(logUi, logUiFactory)
+    createLogUiAndTab(logManager, logUiFactory, select = false)
+    return logUiFactory
   }
 
   private fun createLogUiAndTab(logManager: VcsLogManager, logUiFactory: MyLogUiFactory, select: Boolean): VcsLogUiImpl {
