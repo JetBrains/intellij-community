8f3decfc085837a8d1b4f1b1eadfc8f48d1883ea
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2019-09-05 11:51:54 +0300

    Make `PlatformUtils#isIdeaEducational` public
    GitOrigin-RevId: e40b7bb00de0c016a8a5c2a76234aeffaf4fab60

--- platform/core-api/src/com/intellij/util/PlatformUtils.java
+++ platform/core-api/src/com/intellij/util/PlatformUtils.java
@@ -66,7 +66,7 @@ public final class PlatformUtils {
     return is(IDEA_CE_PREFIX);
   }
 
-  private static boolean isIdeaEducational() {
+  public static boolean isIdeaEducational() {
     return is(IDEA_EDU_PREFIX);
   }
 
