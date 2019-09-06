0cb30397a9227ca4845c4925e0d32c8cbb3c2735
Alexander Zolotov <zolotov@jetbrains.com>
2019-09-06 16:03:49 +0300

    IDEA-220388 Disabled Insert paired brackets is ignored
    GitOrigin-RevId: d3724ebe38878326d62160110d419229c2dd02a1

--- plugins/textmate/tests/org/jetbrains/plugins/textmate/editor/TextMateTypedHandlerTest.java
+++ plugins/textmate/tests/org/jetbrains/plugins/textmate/editor/TextMateTypedHandlerTest.java
@@ -1,9 +1,9 @@
 package org.jetbrains.plugins.textmate.editor;
 
+import com.intellij.codeInsight.CodeInsightSettings;
 import org.jetbrains.plugins.textmate.TextMateAcceptanceTestCase;
 
 public class TextMateTypedHandlerTest extends TextMateAcceptanceTestCase {
-
   public void testQuotes_1() {
     doTest("<caret>", "md_hack", "\"", "\"<caret>\"");
   }
@@ -46,6 +46,30 @@ public class TextMateTypedHandlerTest extends TextMateAcceptanceTestCase {
 
   public void testDisabledSmartBraces() {
     doTest("\\<caret>", "text", "`", "\\`<caret>");
+  }
+
+  public void testDisableQuotes() {
+    CodeInsightSettings settings = CodeInsightSettings.getInstance();
+    boolean oldValue = settings.AUTOINSERT_PAIR_QUOTE;
+    try {
+      settings.AUTOINSERT_PAIR_QUOTE = false;
+      doTest("<caret>", "md_hack", "\"", "\"<caret>");
+    }
+    finally {
+      settings.AUTOINSERT_PAIR_QUOTE = oldValue;
+    }
+  }
+
+  public void testDisableBraces() {
+    CodeInsightSettings settings = CodeInsightSettings.getInstance();
+    boolean oldValue = settings.AUTOINSERT_PAIR_BRACKET;
+    try {
+      settings.AUTOINSERT_PAIR_BRACKET = false;
+      doTest("<html><caret></html>", "md_hack", "<", "<html><<caret></html>");
+    }
+    finally {
+      settings.AUTOINSERT_PAIR_BRACKET = oldValue;
+    }
   }
 
   private void doTest(String source, String extension, String toType, String expected) {
