4cd9ec5840c56a38a24d23a4b1910145b7c72062
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2019-09-07 10:54:35 +0300

    Make it possible to define more team-wide git configs in intellij.yaml, and add support for windows-only core.longpaths
    GitOrigin-RevId: 55f40e6d4346df8dbb5171d81f3bea8c9f505265

--- plugins/git4idea/src/git4idea/config/GitSetupProjectConfig.kt
+++ plugins/git4idea/src/git4idea/config/GitSetupProjectConfig.kt
@@ -4,40 +4,48 @@ package git4idea.config
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.util.SystemInfo
 import com.intellij.openapi.vcs.impl.ProjectLevelVcsManagerImpl
 import com.intellij.openapi.vcs.impl.VcsInitObject
 import com.intellij.util.concurrency.NonUrgentExecutor
 import git4idea.repo.GitRepository
 import git4idea.repo.GitRepositoryManager
 
-const val GC_AUTO = "gc.auto"
-
 class GitSetupProjectConfig: StartupActivity.Background {
   override fun runActivity(project: Project) {
     ProjectLevelVcsManagerImpl.getInstanceImpl(project).addInitializationRequest(VcsInitObject.AFTER_COMMON) {
       if (ApplicationManager.getApplication().isDispatchThread) {
         NonUrgentExecutor.getInstance().execute {
-          setupGcAutoIfNeeded(project)
+          setupConfigIfNeeded(project)
         }
       }
       else {
-        setupGcAutoIfNeeded(project)
+        setupConfigIfNeeded(project)
       }
     }
   }
 
-  private fun setupGcAutoIfNeeded(project: Project) {
-    val gcAuto = GitVcsSettings.getInstance(project).state.gcAuto
-    if (gcAuto != null) {
-      val rootsToUpdate = mutableListOf<GitRepository>()
-      for (repo in GitRepositoryManager.getInstance(project).repositories) {
-        val value = GitConfigUtil.getValue(project, repo.root, GC_AUTO)
-        if (value == null) rootsToUpdate.add(repo)
-      }
+  private fun setupConfigIfNeeded(project: Project) {
+    val settings = GitVcsSettings.getInstance(project).state
+    for (configVar in ConfigVariables.values()) {
+      val settingsValue = configVar.settingsGetter(settings)
+      if (settingsValue != null) {
+        val rootsToUpdate = mutableListOf<GitRepository>()
+        for (repo in GitRepositoryManager.getInstance(project).repositories) {
+          val value = GitConfigUtil.getValue(project, repo.root, configVar.gitName)
+          if (value == null) rootsToUpdate.add(repo)
+        }
 
-      for (repo in rootsToUpdate) {
-        GitConfigUtil.setValue(project, repo.root, GC_AUTO, gcAuto, "--local")
+        for (repo in rootsToUpdate) {
+          GitConfigUtil.setValue(project, repo.root, configVar.gitName, settingsValue, "--local")
+        }
       }
     }
+  }
+
+  private enum class ConfigVariables(val gitName: String, val settingsGetter: (GitVcsOptions) -> String?) {
+    GC_AUTO("gc.auto", { it.gcAuto }),
+    CORE_LONGPATHS("core.longpaths", { if (SystemInfo.isWindows) it.coreLongpaths else null })
   }
 }
+
