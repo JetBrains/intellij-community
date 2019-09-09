cbea6dfc5357c798f32ef9cba73aa7761e9e6ce1
Denis Fokin <denis.fokin@jetbrains.com>
2019-09-09 17:33:06 +0300

    IDEA-221988 Escape key does not return focus to the Editor
    GitOrigin-RevId: 28832449eb3c08015254149baecaf4c4447f9d7e

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/ToolWindowManagerImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/ToolWindowManagerImpl.java
@@ -1752,7 +1752,7 @@ public class ToolWindowManagerImpl extends ToolWindowManagerEx implements Persis
     }
 
     final IdeFrame frame = FocusManagerImpl.getInstance().getLastFocusedFrame();
-    if (frame instanceof ProjectFrameHelper && ((ProjectFrameHelper)frame).getFrame().isActive()) {
+    if (frame instanceof IdeFrameImpl && ((IdeFrameImpl)frame).isActive()) {
       FileEditorManagerEx fem = FileEditorManagerEx.getInstanceEx(Objects.requireNonNull(frame.getProject()));
       EditorsSplitters splitters = activeWindow != null ? fem.getSplittersFor(activeWindow) : null;
       return splitters != null ? splitters : fem.getSplitters();
