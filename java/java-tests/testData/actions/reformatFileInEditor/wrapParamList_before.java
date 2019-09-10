a1c0d8bf1d3a038ef239cbcaa69c3f46fa52500f
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2019-09-10 18:37:13 +0300

    A unit test for IDEA-165776
    GitOrigin-RevId: 2f56dbb1680facc8fc3a7da54d69648b2e998339

---
+++ java/java-tests/testData/actions/reformatFileInEditor/wrapParamList_before.java
@@ -0,0 +1,13 @@
+class Test {
+  void foo() {
+    learnEvaluationAfterState(problem,
+                              trainer,
+                              afterState,
+                              nextTurnState,
+                              currentAlpha,
+                              concurrencyInLayer,
+                              modifiedArg1,
+                              modifiedArg2,
+                              modifiedArg3);
+  }
+}
