94fff784be7f09be4673d7dd22ace35bb31d51f7
Yann Cébron <yann.cebron@jetbrains.com>
2019-09-11 18:00:26 +0200

    PushedFilePropertiesUpdater.filePropertiesChanged: @deprecation javadoc
    GitOrigin-RevId: aecc529f8308aedf33f680f07fd7f2b2e5e1e7f8

--- platform/projectModel-api/src/com/intellij/openapi/roots/impl/PushedFilePropertiesUpdater.java
+++ platform/projectModel-api/src/com/intellij/openapi/roots/impl/PushedFilePropertiesUpdater.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2019 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,7 +30,7 @@ public abstract class PushedFilePropertiesUpdater {
   public abstract void pushAll(final FilePropertyPusher... pushers);
 
   /**
-   * Use {@link #filePropertiesChanged(VirtualFile, Condition)}
+   * @deprecated Use {@link #filePropertiesChanged(VirtualFile, Condition)}
    */
   @Deprecated
   public abstract void filePropertiesChanged(@NotNull final VirtualFile file);
