d0bbab20406df0398609b8beea230ad6bea766cd
Michael Golubev <michael.golubev@jetbrains.com>
2019-09-12 20:38:08 +0200

    Remote environments: introduce language runtimes
    - sample java-like runtime added [temporarily]
    - to be moved to java module
    
    GitOrigin-RevId: 108dbf644690b6b592f4f1d8335d7ae09dd94c34

---
+++ platform/remote-servers/api/src/com/intellij/remoteServer/ir/runtime/sample/SampleLanguageRuntimeType.kt
@@ -0,0 +1,48 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.remoteServer.ir.runtime.sample
+
+import com.intellij.execution.RunnerAndConfigurationSettings
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.options.BoundConfigurable
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.DialogPanel
+import com.intellij.remoteServer.ir.config.BaseExtendableConfiguration.Companion.getTypeImpl
+import com.intellij.remoteServer.ir.runtime.LanguageRuntimeType
+import com.intellij.ui.layout.*
+
+class SampleLanguageRuntimeType : LanguageRuntimeType<SampleLanguageRuntimeConfiguration>(TYPE_ID) {
+  override val icon = AllIcons.FileTypes.Java
+
+  override val displayName = "Sample Language Runtime"
+
+  override fun isApplicableTo(runConfig: RunnerAndConfigurationSettings) = true
+
+  override fun createDefaultConfig() = SampleLanguageRuntimeConfiguration()
+
+  override fun createSerializer(config: SampleLanguageRuntimeConfiguration): PersistentStateComponent<*> = config
+
+  override fun createConfigurable(project: Project, config: SampleLanguageRuntimeConfiguration) = SampleLanguageRuntimeUI(config)
+
+  companion object {
+    @JvmStatic
+    val TYPE_ID = "languageRuntime:sample"
+  }
+
+  class SampleLanguageRuntimeUI(val config: SampleLanguageRuntimeConfiguration)
+    : BoundConfigurable(config.displayName, config.getTypeImpl().helpTopic) {
+
+    override fun createPanel(): DialogPanel {
+      return panel {
+        titledRow("Java configuration") {
+          row("JDK home path:") {
+            textField(config::homePath)
+          }
+          row("Application folder:") {
+            textField(config::applicationFolder)
+          }
+        }
+      }
+    }
+  }
+}
