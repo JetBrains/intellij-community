6b229d8272a7c6f360573f6ce2e352d1b631f11b
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2019-09-12 17:45:43 +0300

    Made tensorflow resolve more complex as its internal paths had been changed in 1.15.0rc0 and 2.0.0rc0 (PY-37589)
    (cherry picked from commit 39e18cf4ca56eb2379d2664e5ad07647985943cb)
    
    GitOrigin-RevId: ce34e8fcd9201dafb3ab13f714c8275e765eca6b

--- python/src/com/jetbrains/tensorFlow/PyTensorFlowImportResolver.kt
+++ python/src/com/jetbrains/tensorFlow/PyTensorFlowImportResolver.kt
@@ -5,43 +5,33 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.util.QualifiedName
 import com.jetbrains.python.psi.impl.PyImportResolver
 import com.jetbrains.python.psi.resolve.PyQualifiedNameResolveContext
-import com.jetbrains.python.psi.resolve.resolveQualifiedName
 import org.jetbrains.annotations.ApiStatus
-
-internal const val KERAS: String = "tensorflow.python.keras.api._v[__VERSION__].keras"
-internal const val ESTIMATOR: String = "tensorflow_estimator.python.estimator.api._v[__VERSION__].estimator"
-internal const val OTHERS: String = "tensorflow._api.v[__VERSION__]"
-
-internal fun resolveInTensorFlow(qualifiedNameTemplate: String, context: PyQualifiedNameResolveContext): Sequence<List<PsiElement>> {
-  return sequenceOf("2", "1")
-    .map { qualifiedNameTemplate.replaceFirst("[__VERSION__]", it) }
-    .map { QualifiedName.fromDottedString(it) }
-    .map { resolveQualifiedName(it, context) }
-}
-
-internal fun takeFirstResolvedInTensorFlow(qualifiedNameTemplate: String, context: PyQualifiedNameResolveContext): PsiElement? {
-  return resolveInTensorFlow(qualifiedNameTemplate, context).mapNotNull { it.firstOrNull() }.firstOrNull()
-}
 
 @ApiStatus.Internal
 @ApiStatus.NonExtendable
 class PyTensorFlowImportResolver : PyImportResolver {
 
   override fun resolveImportReference(name: QualifiedName, context: PyQualifiedNameResolveContext, withRoots: Boolean): PsiElement? {
-    // resolve `from tensorflow.<reference>` reference
-    // tensorflow submodules and subpackages are appended in runtime and have original location in other places
-
-    return when {
-      name.matchesPrefix(QualifiedName.fromComponents("tensorflow", "keras")) -> {
-        takeFirstResolvedInTensorFlow("$KERAS.${name.removeHead(2)}", context.copyWithoutForeign())
-      }
-      name.matchesPrefix(QualifiedName.fromComponents("tensorflow", "estimator")) -> {
-        takeFirstResolvedInTensorFlow("$ESTIMATOR.${name.removeHead(2)}", context.copyWithoutForeign())
-      }
-      name.firstComponent == "tensorflow" && name.componentCount >= 2 -> {
-        takeFirstResolvedInTensorFlow("$OTHERS.${name.removeHead(1)}", context.copyWithoutForeign())
-      }
-      else -> null
+    return if (isTensorFlowMember(name)) {
+      val resolveContext = context.copyWithoutForeign()
+      resolveImportReference(name, resolveContext, getTensorFlowPathConfig(resolveContext.sdk))
     }
+    else {
+      null
+    }
+  }
+
+  private fun isTensorFlowMember(name: QualifiedName) = name.firstComponent == "tensorflow" && name.componentCount > 1
+
+  private fun resolveImportReference(name: QualifiedName,
+                                     context: PyQualifiedNameResolveContext,
+                                     pathConfig: Pair<Map<String, String>, String>): PsiElement? {
+    pathConfig.first.forEach { (module, path) ->
+      if (name.components[1] == module) {
+        return takeFirstResolvedInTensorFlow("$path.${name.removeHead(2)}", context)
+      }
+    }
+
+    return takeFirstResolvedInTensorFlow("${pathConfig.second}.${name.removeHead(1)}", context)
   }
 }
