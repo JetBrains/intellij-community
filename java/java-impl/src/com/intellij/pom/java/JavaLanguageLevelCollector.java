d120589fcde2bd26ebb58a3efe5dde55043c91d5
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2019-09-12 15:48:04 +0700

    JavaLanguageLevelCollector: report separately preview flag; gather reports for all modules
    IDEA-CR-51146, FUS-479
    
    GitOrigin-RevId: 57a11364d30c044fea99d70774c1f4e8e7c5dba2

--- java/java-impl/src/com/intellij/pom/java/JavaLanguageLevelCollector.java
+++ java/java-impl/src/com/intellij/pom/java/JavaLanguageLevelCollector.java
@@ -2,11 +2,18 @@
 package com.intellij.pom.java;
 
 import com.intellij.internal.statistic.beans.MetricEvent;
+import com.intellij.internal.statistic.eventLog.FeatureUsageData;
 import com.intellij.internal.statistic.service.fus.collectors.ProjectUsagesCollector;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.LanguageLevelModuleExtension;
+import com.intellij.openapi.roots.LanguageLevelModuleExtensionImpl;
 import com.intellij.openapi.roots.LanguageLevelProjectExtension;
+import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -26,9 +33,22 @@ public class JavaLanguageLevelCollector extends ProjectUsagesCollector {
 
     LanguageLevelProjectExtension instance = LanguageLevelProjectExtension.getInstance(project);
     if (instance != null) {
-      usages.add(newMetric("PROJECT_LANGUAGE_LEVEL", instance.getLanguageLevel()));
+      Set<LanguageLevel> languageLevels = EnumSet.noneOf(LanguageLevel.class);
+      for (Module module : ModuleManager.getInstance(project).getModules()) {
+        LanguageLevelModuleExtension moduleExtension = LanguageLevelModuleExtensionImpl.getInstance(module);
+        LanguageLevel languageLevel = instance.getLanguageLevel();
+        if (moduleExtension != null && moduleExtension.getLanguageLevel() != null) {
+          languageLevel = moduleExtension.getLanguageLevel();
+        }
+        ContainerUtil.addIfNotNull(languageLevels, languageLevel);
+      }
+      for (LanguageLevel level : languageLevels) {
+        FeatureUsageData data = new FeatureUsageData()
+          .addData("version", level.toJavaVersion().feature)
+          .addData("preview", level.isPreview());
+        usages.add(newMetric("PROJECT_LANGUAGE_LEVEL", data));
+      }
     }
-
     return usages;
   }
 }
