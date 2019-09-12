b38057980972043671d53b9a56672b5f21716e8b
Yann Cébron <yann.cebron@jetbrains.com>
2019-09-12 15:54:01 +0200

    TestClassConfigurationProducer: add @ScheduledForRemoval
    GitOrigin-RevId: b260dec9a987edfffab12c87be9aabb5089595b1

--- plugins/junit/src/com/intellij/execution/junit/TestClassConfigurationProducer.java
+++ plugins/junit/src/com/intellij/execution/junit/TestClassConfigurationProducer.java
@@ -6,9 +6,10 @@ import com.intellij.execution.configurations.ConfigurationFactory;
 import com.intellij.execution.testframework.AbstractInClassConfigurationProducer;
 import com.intellij.openapi.util.Ref;
 import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
-//to be deleted in 2018
+@ApiStatus.ScheduledForRemoval(inVersion = "2018")
 @Deprecated
 public class TestClassConfigurationProducer extends AbstractInClassConfigurationProducer<JUnitConfiguration> {
   public TestClassConfigurationProducer() {
