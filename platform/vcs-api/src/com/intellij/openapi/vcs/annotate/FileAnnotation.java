e1255e1abd2a52b468123962e5972ace2382ae1c
Yann Cébron <yann.cebron@jetbrains.com>
2019-09-12 14:59:09 +0200

    FileAnnotation.revisionsNotEmpty: add @deprecation javadoc
    GitOrigin-RevId: 6b618c91b335dbf60f5e189804be29cf3e6786d7

--- platform/vcs-api/src/com/intellij/openapi/vcs/annotate/FileAnnotation.java
+++ platform/vcs-api/src/com/intellij/openapi/vcs/annotate/FileAnnotation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2019 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -229,7 +229,9 @@ public abstract class FileAnnotation {
     myReloader = reloader;
   }
 
-
+  /**
+   * @deprecated does nothing
+   */
   @Deprecated
   public boolean revisionsNotEmpty() {
     return true;
