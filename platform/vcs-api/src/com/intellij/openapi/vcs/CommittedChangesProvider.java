4814f0dc161930276e81744d16291917ea19c6f8
Yann Cébron <yann.cebron@jetbrains.com>
2019-09-12 14:51:36 +0200

    CommittedChangesProvider.getLocationFor: add @deprecation javadoc
    GitOrigin-RevId: 334c7853972b90f03ec3907622af52a246ddb85a

--- platform/vcs-api/src/com/intellij/openapi/vcs/CommittedChangesProvider.java
+++ platform/vcs-api/src/com/intellij/openapi/vcs/CommittedChangesProvider.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vcs;
 
 import com.intellij.openapi.util.Pair;
@@ -31,6 +31,9 @@ public interface CommittedChangesProvider<T extends CommittedChangeList, U exten
   @Nullable
   RepositoryLocation getLocationFor(FilePath root);
 
+  /**
+   * @deprecated use {@link #getLocationFor(FilePath)}
+   */
   @SuppressWarnings("unused")
   @Deprecated // Required for compatibility with external plugins.
   @Nullable
