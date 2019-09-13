305d68eebc70c549b1f529418db2170c805c5a4b
Alexander Koshevoy <Alexander.Koshevoy@jetbrains.com>
2019-09-13 17:00:51 +0300

    PY-37206 Move buildRemoteCommandLine() method from general RemoteProcessUtil to PyRemoteInterpreterCommandLineUtil
    GitOrigin-RevId: 873b68d2a3e303c2e12f40cb727c62329e59b662

--- platform/platform-impl/src/com/intellij/remote/RemoteProcessUtil.java
+++ platform/platform-impl/src/com/intellij/remote/RemoteProcessUtil.java
@@ -3,7 +3,6 @@ package com.intellij.remote;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
-import com.intellij.openapi.util.Pair;
 import com.intellij.util.AbstractPathMapper;
 import com.intellij.util.ArrayUtilRt;
 import com.intellij.util.PathMapper;
@@ -12,36 +11,10 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
 public class RemoteProcessUtil {
-  /**
-   * Builds {@code &lt;command, working directory&gt;} tuple appropriate for execution on the remote host for provided {@code command} and
-   * {@code workingDir} using specified {@code pathMapper}.
-   */
-  public static Pair<String[], String> buildRemoteCommandLine(@NotNull AbstractPathMapper pathMapper,
-                                                              @NotNull String[] command,
-                                                              @Nullable String workingDir,
-                                                              @NotNull String interpreterPath) {
-    command = Arrays.copyOf(command, command.length);
-
-    for (int i = 0; i < command.length; i++) {
-      if (pathMapper.canReplaceLocal(command[i])) {
-        command[i] = RemoteFile.detectSystemByPath(interpreterPath).
-          createRemoteFile(pathMapper.convertToRemote(command[i])).getPath();
-      }
-      if (isPathList(command[i])) {
-        command[i] = remapPathsList(command[i], pathMapper, interpreterPath);
-      }
-    }
-    command[1] = pathMapper.convertToRemote(command[1]); //convert helper path
-    command[0] = interpreterPath;
-
-    return Pair.create(command, toRemoteFileSystemStyle(workingDir));
-  }
-
   @Contract("null -> null")
   public static String toRemoteFileSystemStyle(@Nullable String path) {
     if (path == null) {
@@ -64,19 +37,5 @@ public class RemoteProcessUtil {
       mappedPaths.add(new RemoteFile(pathMapper.convertToRemote(path), isWin).getPath());
     }
     return Joiner.on(isWin ? ';' : ':').join(mappedPaths);
-  }
-
-  private static boolean isPathList(String value) {
-    List<String> paths = Lists.newArrayList(value.split(File.pathSeparator));
-
-    if (paths.size() > 1) {
-      for (String p : paths) {
-        if (new File(p).exists()) {
-          return true;
-        }
-      }
-    }
-
-    return false;
   }
 }
