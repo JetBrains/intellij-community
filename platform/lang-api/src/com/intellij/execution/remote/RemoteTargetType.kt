ca61e041a540255b8f66fc1ebdf977dd246a4a33
Michael Golubev <michael.golubev@jetbrains.com>
2019-09-15 22:51:56 +0200

    Remote environments: new target wizard - initial API
    GitOrigin-RevId: 50beba0e77d8ff7afa62d963fdf9dcf3d49beb3a

--- platform/lang-api/src/com/intellij/execution/remote/RemoteTargetType.kt
+++ platform/lang-api/src/com/intellij/execution/remote/RemoteTargetType.kt
@@ -2,12 +2,17 @@
 package com.intellij.execution.remote
 
 import com.intellij.execution.remote.target.IRExecutionTarget
+import com.intellij.ide.wizard.AbstractWizardStepEx
 import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.project.Project
 
 //TODO: suggest "predefined" configurations (e.g one per every configured SFTP connection)
 abstract class RemoteTargetType<C : RemoteTargetConfiguration>(id: String) : BaseExtendableType<C>(id) {
   fun createExecutionTarget(project: Project, config: C) = IRExecutionTarget(project, config)
+
+  open fun providesNewWizard(project: Project, runtimeType: LanguageRuntimeType<*>?): Boolean = false
+
+  open fun createStepsForNewWizard(project: Project, runtimeType: LanguageRuntimeType<*>?): List<AbstractWizardStepEx>? = null
 
   abstract fun createRunner(project: Project, config: C): IR.RemoteRunner
 
