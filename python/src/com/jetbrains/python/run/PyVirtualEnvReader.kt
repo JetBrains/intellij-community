567347aa7a721ee4b22c33eb6610c27bac0f39bf
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2019-09-16 18:56:25 +0300

    Add `JAVA_HOME` and `JAVA_LD_LIBRARY_PATH` to white-listed virtualenv variables (PY-37406)
    GitOrigin-RevId: e02c84f6de6f93f4e5d79ae5c04cfa8819fff7a9

--- python/src/com/jetbrains/python/run/PyVirtualEnvReader.kt
+++ python/src/com/jetbrains/python/run/PyVirtualEnvReader.kt
@@ -21,7 +21,7 @@ class PyVirtualEnvReader(val virtualEnvSdkPath: String) : EnvironmentUtil.ShellE
     private val virtualEnvVars = listOf("PATH", "PS1", "VIRTUAL_ENV", "PYTHONHOME", "PROMPT", "_OLD_VIRTUAL_PROMPT",
                                         "_OLD_VIRTUAL_PYTHONHOME", "_OLD_VIRTUAL_PATH", "CONDA_SHLVL", "CONDA_PROMPT_MODIFIER",
                                         "CONDA_PREFIX", "CONDA_DEFAULT_ENV",
-                                        "GDAL_DATA", "PROJ_LIB")
+                                        "GDAL_DATA", "PROJ_LIB", "JAVA_HOME", "JAVA_LD_LIBRARY_PATH")
 
     /**
      * Filter envs that are setup by the activate script, adding other variables from the different shell can break the actual shell.
