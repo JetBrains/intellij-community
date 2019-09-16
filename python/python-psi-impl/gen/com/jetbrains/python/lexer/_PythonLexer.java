843d74524f93e65227cf0321b078227fa911a7c1
Alexey Kudravtsev <cdr@intellij.com>
2019-09-16 14:35:04 +0300

    fix tools/lexer/build.xml: correct paths, add missing lexers, cleanup skeletons, patch path; restore damaged formatting
    GitOrigin-RevId: 4bff2a5b5dc1f01d90d470e6d7e65e4f7dbc7e9b

--- python/python-psi-impl/gen/com/jetbrains/python/lexer/_PythonLexer.java
+++ python/python-psi-impl/gen/com/jetbrains/python/lexer/_PythonLexer.java
@@ -874,13 +874,14 @@ return yylength()-s.length();
     zzAtEOF  = false;
     zzAtBOL = true;
     zzEndRead = end;
+    fStringHelper.reset();
     yybegin(initialState);
   }
 
   /**
    * Refills the input buffer.
    *
-   * @return      <code>false</code>, iff there was new input.
+   * @return      {@code false}, iff there was new input.
    *
    * @exception   java.io.IOException  if any I/O-Error occurs
    */
@@ -916,7 +917,7 @@ return yylength()-s.length();
 
 
   /**
-   * Returns the character at position <tt>pos</tt> from the
+   * Returns the character at position {@code pos} from the
    * matched text.
    *
    * It is equivalent to yytext().charAt(pos), but faster
@@ -940,7 +941,7 @@ return yylength()-s.length();
 
 
   /**
-   * Reports an error that occured while scanning.
+   * Reports an error that occurred while scanning.
    *
    * In a wellformed scanner (no or only correct usage of
    * yypushback(int) and a match-all fallback rule) this method
