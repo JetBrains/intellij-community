ab495ddbfb2c5adbba5b4b6d3852bfd57571ac47
Bas Leijdekkers <basleijdekkers@gmail.com>
2019-09-17 17:20:10 +0200

    IPP: Create JUnit5 assertion (IDEA-222593)
    GitOrigin-RevId: e2a58d960429af35df4ef4806aee40874cafe6ee

--- plugins/IntentionPowerPak/testSrc/com/siyeh/ipp/junit/CreateAssertIntentionTest.java
+++ plugins/IntentionPowerPak/testSrc/com/siyeh/ipp/junit/CreateAssertIntentionTest.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.siyeh.ipp.junit;
 
 import com.siyeh.IntentionPowerPackBundle;
@@ -30,6 +16,7 @@ public class CreateAssertIntentionTest extends IPPTestCase {
   public void testAssertFalse() { doTest(); }
   public void testConflictingMethod() { doTest(); }
   public void testNonConflictingMethod() { doTest(); }
+  public void testJUnit5Test() { doTest(); }
 
   @Override
   protected String getRelativePath() {
@@ -57,7 +44,21 @@ public class CreateAssertIntentionTest extends IPPTestCase {
                        "public abstract class TestCase {" +
                        "  static public void assertTrue(boolean condition) {}" +
                        "  static public void assertFalse(boolean condition) {}" +
-                       "}"
-    );
+                       "}");
+
+    myFixture.addClass("package org.junit.jupiter.api;" +
+                       "public @interface Test {}");
+    myFixture.addClass("package org.junit.platform.commons.annotation;" +
+                       "public @interface Testable {}");
+    myFixture.addClass("package org.junit.jupiter.api;\n" +
+                       "public final class Assertions {\n" +
+                       "    public static void assertArrayEquals(Object[] expected, Object[] actual) {}\n" +
+                       "    public static void assertArrayEquals(Object[] expected, Object[] actual, String message) {}\n" +
+                       "    public static void assertEquals(Object expected, Object actual) {}\n" +
+                       "    public static void assertTrue(boolean expected) {}\n" +
+                       "    public static void assertEquals(Object expected, Object actual, String message) {}\n" +
+                       "    public static void assertTrue(Object expected, String message) {}\n" +
+                       "    public static void fail(String message) {}" +
+                       "}");
   }
 }
