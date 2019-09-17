444b3b78f351e08009ffc1f3f6d6d98264eaca84
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2019-09-17 21:49:55 +0300

    Enable `urllib` stubs (PY-15489)
    GitOrigin-RevId: 904f2172be4b16ab9e9cb0d2642afa71b7431afc

---
+++ python/helpers/typeshed/stdlib/3/urllib/error.pyi
@@ -0,0 +1,12 @@
+from typing import Dict, Union
+from urllib.response import addinfourl
+
+# Stubs for urllib.error
+
+class URLError(IOError):
+    reason: Union[str, BaseException]
+class HTTPError(URLError, addinfourl):
+    code: int
+    headers: Dict[str, str]
+    def __init__(self, url, code, msg, hdrs, fp) -> None: ...
+class ContentTooShortError(URLError): ...
