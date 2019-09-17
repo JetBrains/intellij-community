f3e291aeb8ae67d0d52c8d0e374330375d9bf1ec
Michael Golubev <michael.golubev@jetbrains.com>
2019-09-17 18:30:02 +0200

    API quality fix: remove long deprecated methods
    - DeploymentConfigurationManager.createAndRunConfiguration(.., ..)
    - ServersToolWindowContent.constructor(.., ..)
    - breaks existing google plugin, but it has limited `until=2019.2`
    
    GitOrigin-RevId: d799c7556cba77c5f6c82d82af371cd3164de41b

--- platform/remote-servers/api/src/com/intellij/remoteServer/configuration/deployment/DeploymentConfigurationManager.java
+++ platform/remote-servers/api/src/com/intellij/remoteServer/configuration/deployment/DeploymentConfigurationManager.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2019 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.remoteServer.configuration.deployment;
 
 import com.intellij.execution.RunnerAndConfigurationSettings;
@@ -20,7 +6,6 @@ import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.remoteServer.ServerType;
 import com.intellij.remoteServer.configuration.RemoteServer;
-import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -38,15 +23,7 @@ public abstract class DeploymentConfigurationManager {
   @NotNull
   public abstract List<RunnerAndConfigurationSettings> getDeploymentConfigurations(@NotNull ServerType<?> serverType);
 
-  /**
-   * @deprecated Will be removed after 173, use more granular methods added @since 173
-   */
-  @Deprecated
-  @ApiStatus.ScheduledForRemoval(inVersion = "2017.3")
-  public abstract void createAndRunConfiguration(@NotNull ServerType<?> serverType,
-                                                 @Nullable RemoteServer<?> remoteServer);
-
   public abstract void createAndRunConfiguration(@NotNull ServerType<?> serverType,
                                                  @Nullable RemoteServer<?> remoteServer,
-                                                 @Nullable DeploymentSourceType sourceType);
+                                                 @Nullable DeploymentSourceType<?> sourceType);
 }
