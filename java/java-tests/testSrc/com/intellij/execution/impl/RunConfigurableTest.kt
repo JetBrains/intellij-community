ef8a3f419d02616940764cec2c5842b6a7e1cec1
Artemiy Sartakov <Artemiy.Sartakov@jetbrains.com>
2019-09-17 18:27:18 +0700

    RunConfigurable : added support for multiple items dnd (IDEA-93940)
    GitOrigin-RevId: a687ef799ebffcc303f95dc1667d580fbe41345a

--- java/java-tests/testSrc/com/intellij/execution/impl/RunConfigurableTest.kt
+++ java/java-tests/testSrc/com/intellij/execution/impl/RunConfigurableTest.kt
@@ -94,8 +94,8 @@ internal class RunConfigurableTest {
         assertCannot(j, i, BELOW)
       }
     }
-    assertCan(3, 3, BELOW)
-    assertCan(3, 3, ABOVE)
+    assertCannot(3, 3, BELOW)
+    assertCannot(3, 3, ABOVE)
     assertCannot(3, 2, BELOW)
     assertCan(3, 2, ABOVE)
     assertCannot(3, 1, BELOW)
@@ -112,6 +112,9 @@ internal class RunConfigurableTest {
     assertCannot(15, 11, INTO)
     assertCannot(18, 21, ABOVE)
     assertCan(15, 21, ABOVE)
+
+    assertCannot(arrayOf(1, 2, 3), 2, BELOW)
+    assertCan(arrayOf(2, 12), 9, INTO)
 
     assertThat(model.isDropInto(tree, 2, 9)).isTrue()
     assertThat(model.isDropInto(tree, 2, 1)).isTrue()
@@ -134,28 +137,35 @@ internal class RunConfigurableTest {
       tree.expandPath(TreePath(node.path))
     }
 
-    assertThat(ORDER.mapIndexed { index, _ -> RunConfigurable.getKind(tree.getPathForRow(index).lastPathComponent as DefaultMutableTreeNode) }).containsExactly(*ORDER)
+    assertThat(ORDER.mapIndexed { index, _ ->
+      RunConfigurable.getKind(tree.getPathForRow(index).lastPathComponent as DefaultMutableTreeNode)
+    }).containsExactly(*ORDER)
   }
 
   private fun assertCan(oldIndex: Int, newIndex: Int, position: RowsDnDSupport.RefinedDropSupport.Position) {
-    assertDrop(oldIndex, newIndex, position, true)
+    assertDrop(arrayOf(oldIndex), newIndex, position, true)
   }
 
   private fun assertCannot(oldIndex: Int, newIndex: Int, position: RowsDnDSupport.RefinedDropSupport.Position) {
-    assertDrop(oldIndex, newIndex, position, false)
+    assertDrop(arrayOf(oldIndex), newIndex, position, false)
   }
 
-  private fun assertDrop(oldIndex: Int, newIndex: Int, position: RowsDnDSupport.RefinedDropSupport.Position, canDrop: Boolean) {
-    val message = StringBuilder()
-    message.append("(").append(oldIndex).append(")").append(tree.getPathForRow(oldIndex)).append("->")
-    message.append("(").append(newIndex).append(")").append(tree.getPathForRow(newIndex)).append(position)
+  private fun assertCannot(oldIndices: Array<Int>, newIndex: Int, position: RowsDnDSupport.RefinedDropSupport.Position) {
+    assertDrop(oldIndices, newIndex, position, false)
+  }
+
+  private fun assertCan(oldIndices: Array<Int>, newIndex: Int, position: RowsDnDSupport.RefinedDropSupport.Position) {
+    assertDrop(oldIndices, newIndex, position, true)
+  }
+
+  private fun assertDrop(oldIndices: Array<Int>, newIndex: Int, position: RowsDnDSupport.RefinedDropSupport.Position, canDrop: Boolean) {
+    assertThat(oldIndices.isNotEmpty())
+    tree.selectionPaths = oldIndices.map { tree.getPathForRow(it) }.toTypedArray()
     if (canDrop) {
-      // message.toString()
-      assertThat(model.canDrop(oldIndex, newIndex, position)).isTrue()
+      assertThat(model.canDrop(oldIndices[0], newIndex, position)).isTrue()
     }
     else {
-      // message.toString()
-      assertThat(model.canDrop(oldIndex, newIndex, position)).isFalse()
+      assertThat(model.canDrop(oldIndices[0], newIndex, position)).isFalse()
     }
   }
 
@@ -194,6 +204,7 @@ internal class RunConfigurableTest {
     doExpand()
     assertFalse(model.canDrop(2, 0, ABOVE))
     assertThat(configurable.isModified).isFalse()
+    tree.selectionPath = tree.getPathForRow(2)
     model.drop(2, 14, ABOVE)
     assertThat(configurable.isModified).isTrue()
     configurable.apply()
@@ -214,6 +225,7 @@ internal class RunConfigurableTest {
                                                                        "All in titled4",
                                                                        "All in titled5")
     assertThat(configurable.isModified).isFalse()
+    tree.selectionPath = tree.getPathForRow(4)
     model.drop(4, 8, BELOW)
     configurable.apply()
     assertThat(runManager.allSettings.joinToString("\n") { "[${it.type.displayName}] [${it.folderName ?: ""}] ${it.name}" }).isEqualTo("""
@@ -268,6 +280,34 @@ internal class RunConfigurableTest {
      [5]
      Application: CodeGenerator (level: WORKSPACE)
      JUnit: All in titled5 (level: TEMPORARY)
+    """.trimIndent())
+  }
+
+  @Test
+  fun insertMultiple() {
+    doExpand()
+    assertThat(configurable.isModified).isFalse()
+    tree.selectionPaths = arrayOf(tree.getPathForRow(3), tree.getPathForRow(6))
+    model.drop(3, 9, INTO)
+    assertThat(configurable.isModified).isTrue()
+    configurable.apply()
+    val runManager = configurable.runManager
+    assertThat(runManager.allSettings.joinToString("\n") { "[${it.type.displayName}] [${it.folderName ?: ""}] ${it.name}" }).isEqualTo("""
+      [Application] [1] CodeGenerator
+      [Application] [1] UI
+      [Application] [1] AuTest
+      [Application] [1] OutAndErr
+      [Application] [1] C148C_TersePrincess
+      [Application] [2] Renamer
+      [Application] [2] Simples
+      [Application] [2] Periods
+      [Application] [3] C148E_Porcelain
+      [Application] [3] ErrAndOut
+      [JUnit] [4] All in titled
+      [JUnit] [4] All in titled2
+      [JUnit] [5] All in titled3
+      [JUnit] [5] All in titled4
+      [JUnit] [] All in titled5
     """.trimIndent())
   }
 }
