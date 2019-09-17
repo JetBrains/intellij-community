fb6dc94e3e616cf221130b2f44f77091976255ee
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-09-17 22:27:43 +0200

    Cleanup (deprecated API; typos; formatting)
    GitOrigin-RevId: 3a6f460ba5163f9d4049c47daa238ba7aefc0762

--- java/java-analysis-api/src/com/intellij/codeInspection/BaseJavaLocalInspectionTool.java
+++ java/java-analysis-api/src/com/intellij/codeInspection/BaseJavaLocalInspectionTool.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInspection;
 
 import com.intellij.codeInsight.daemon.HighlightDisplayKey;
@@ -6,12 +6,9 @@ import com.intellij.psi.PsiElement;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
-/**
- * @deprecated superfluous, extend {@link AbstractBaseJavaLocalInspectionTool} / {@link CustomSuppressableInspectionTool} instead
- * (to be removed in IDEA 2019).
- */
-@ApiStatus.ScheduledForRemoval(inVersion = "2019")
+/** @deprecated superfluous, extend {@link AbstractBaseJavaLocalInspectionTool} / {@link CustomSuppressableInspectionTool} */
 @Deprecated
+@ApiStatus.ScheduledForRemoval(inVersion = "2021.3")
 public abstract class BaseJavaLocalInspectionTool extends AbstractBaseJavaLocalInspectionTool implements CustomSuppressableInspectionTool {
   @NotNull
   @Override
