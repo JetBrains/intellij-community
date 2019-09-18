4071f15119dd6272e9ba3717b1a8b66f649723e3
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2019-09-18 11:47:44 +0300

    Removed deprecated getWindowsPath method
    GitOrigin-RevId: 94a01415a5ebce45c2b5a1dd010f1015c2c8e828

--- platform/platform-impl/src/com/intellij/execution/wsl/WSLUtil.java
+++ platform/platform-impl/src/com/intellij/execution/wsl/WSLUtil.java
@@ -143,17 +143,6 @@ public class WSLUtil {
   }
 
   /**
-   * @deprecated in order to make custom wsl mount paths work, use {@link WSLUtil#getWindowsPath(java.lang.String, java.lang.String)} or
-   * {@link WSLDistribution#getWslPath(java.lang.String)}
-   */
-  @Deprecated
-  @ApiStatus.ScheduledForRemoval(inVersion = "2019.2")
-  @Nullable
-  public static String getWindowsPath(@NotNull String wslPath) {
-    return getWindowsPath(wslPath, WSLDistribution.DEFAULT_WSL_MNT_ROOT);
-  }
-
-  /**
    * @return Windows-dependent path for a file, pointed by {@code wslPath} in WSL or null if path is unmappable.
    * For example, {@code getWindowsPath("/mnt/c/Users/file.txt") returns "c:\Users\file.txt"}
    */
