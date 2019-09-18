cfbb5aed0115f2d775eacb5c91af70b9faaea926
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2019-09-18 21:16:07 +0300

    fix compilation
    GitOrigin-RevId: 292fffa3a3d964cd6329fcab29e238d088dde57e

--- platform/lang-impl/src/com/intellij/util/indexing/jetcache/JetCacheService.kt
+++ platform/lang-impl/src/com/intellij/util/indexing/jetcache/JetCacheService.kt
@@ -8,8 +8,9 @@ import com.intellij.openapi.util.io.ByteArraySequence
 import com.intellij.util.indexing.ContentHashesSupport
 import com.intellij.util.indexing.FileBasedIndexExtension
 import com.intellij.util.indexing.ID
-import org.jetbrains.jetcache.JcKey
-import org.jetbrains.jetcache.JcValue
+import com.jetbrains.jetcache.model.JetCacheModel
+//import org.jetbrains.jetcache.JcKey
+//import org.jetbrains.jetcache.JcValue
 import org.jetbrains.jetcache.JetCache
 
 import java.util.concurrent.ConcurrentHashMap
@@ -37,26 +38,26 @@ class JetCacheService {
 
     //TODO start from here
     val jetCache: JetCache? = null
-    jetCache!!.getMuliple(JcKey(hash)).whenComplete(object : BiConsumer<Array<JcValue>?, Throwable?> {
-      override fun accept(t: Array<JcValue>?, u: Throwable?) {
-        if (u != null) {
-          LOG.error(u)
-        }
-        val keys = t!!
-        jetCache.get(keys = null!!, onReceived = { jcKey, bytes ->
-          val hashAndId = ContentHashesSupport.splitHashAndId(jcKey.serialized)
-          if (hashAndId != null) {
-            val id = hashAndId.second
-            val contentHash = hashAndId.first
-            getStorage(id)?.put(contentHash, ByteArraySequence(bytes))
-          }
-        }, onFinished = { success ->
-          if (!success) {
-            LOG.error("Can't load keys")
-          }
-        })
-      }
-    })
+    //jetCache!!.getMuliple(JcKey(hash)).whenComplete(object : BiConsumer<Array<JcValue>?, Throwable?> {
+    //  override fun accept(t: Array<JcValue>?, u: Throwable?) {
+    //    if (u != null) {
+    //      LOG.error(u)
+    //    }
+    //    val keys = t!!
+    //    jetCache.get(keys = null!!, onReceived = { jcKey, bytes ->
+    //      val hashAndId = ContentHashesSupport.splitHashAndId(jcKey.serialized)
+    //      if (hashAndId != null) {
+    //        val id = hashAndId.second
+    //        val contentHash = hashAndId.first
+    //        getStorage(id)?.put(contentHash, ByteArraySequence(bytes))
+    //      }
+    //    }, onFinished = { success ->
+    //      if (!success) {
+    //        LOG.error("Can't load keys")
+    //      }
+    //    })
+    //  }
+    //})
   }
 
   fun <K, V> getStorage(indexId: ID<K, V>): JetCacheLocalStorage<K, V>? {
