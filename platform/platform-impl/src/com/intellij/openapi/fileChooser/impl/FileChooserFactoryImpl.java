2ff3599347ae882c009ff51124adbca843966f40
Artem Bochkarev <artem.bochkarev@jetbrains.com>
2019-09-18 18:58:50 +0300

    IDEA-222937: disabled native file chooser for macOS Catalina
    cherry-picked from 0660e2a8
    
    IDEA-CR-52623
    
    GitOrigin-RevId: b089825d83c8ff4a21a162800141602e10f4be3f

--- platform/platform-impl/src/com/intellij/openapi/fileChooser/impl/FileChooserFactoryImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/fileChooser/impl/FileChooserFactoryImpl.java
@@ -79,6 +79,7 @@ public class FileChooserFactoryImpl extends FileChooserFactory {
 
   private static boolean useNativeMacChooser(final FileChooserDescriptor descriptor) {
     return SystemInfo.isMac &&
+           !SystemInfo.isMacOSCatalina && // temp solution (wait for OS stabilization), see IDEA-222937 Disable native file chooser for macOS Catalina
            !descriptor.isForcedToUseIdeaFileChooser() &&
            SystemProperties.getBooleanProperty("native.mac.file.chooser.enabled", true) &&
            Registry.is("ide.mac.file.chooser.native") &&
