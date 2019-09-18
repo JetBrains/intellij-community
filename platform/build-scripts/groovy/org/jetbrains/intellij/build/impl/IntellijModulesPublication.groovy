c1482efe875e750d5f31dfa4d2362a499516feed
Dmitriy.Panov <Dmitriy.Panov@jetbrains.com>
2019-09-18 16:30:19 +0300

    [maven modules publiction] more warnings on artifacts generation/publication
    GitOrigin-RevId: bccc6839b07b8fc7913d0e287bfd4c2cfff85b1b

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/IntellijModulesPublication.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/IntellijModulesPublication.groovy
@@ -79,8 +79,14 @@ class IntellijModulesPublication {
       if (jar.exists()) {
         deployJar(jar, pom)
       }
+      else {
+        context.messages.warning("$it.name jar is not found")
+      }
       if (sources.exists()) {
         deploySources(sources, coordinates)
+      }
+      else {
+        context.messages.warning("$it.name sources is not found")
       }
     }
   }
