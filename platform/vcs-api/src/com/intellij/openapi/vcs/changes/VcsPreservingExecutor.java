24d8a73d379e6dffc5dd19947946067c2767438f
Vladimir Koshelev <vladimir.koshelev@jetbrains.com>
2019-09-18 18:26:24 +0200

    Refactoring: move VcsPreservingExecutor interface to vcs-api
    GitOrigin-RevId: eb4270bc1dd5bfd1a3739294cee0f4393961c01f

--- platform/vcs-api/src/com/intellij/openapi/vcs/changes/VcsPreservingExecutor.java
+++ platform/vcs-api/src/com/intellij/openapi/vcs/changes/VcsPreservingExecutor.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vcs.changes;
 
 import com.intellij.openapi.extensions.ExtensionPointName;
@@ -39,6 +39,6 @@ public interface VcsPreservingExecutor {
         return;
       }
     }
-    new DefaultPreservingExecutor(project, rootsToSave, operationTitle, indicator, operation).execute();
+    // shouldn't be reachable
   }
 }
