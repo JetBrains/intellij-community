7523cb3b0b253a967457761f84cb0a35ddab54aa
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-09-20 17:18:50 +0200

    cleanup
    GitOrigin-RevId: 3cb554f02e7f52eea91e6416db1edc1f61700ee4

--- platform/indexing-impl/src/com/intellij/psi/impl/cache/impl/id/IdIndex.java
+++ platform/indexing-impl/src/com/intellij/psi/impl/cache/impl/id/IdIndex.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.psi.impl.cache.impl.id;
 
@@ -45,7 +31,7 @@ import java.util.Map;
  */
 public class IdIndex extends FileBasedIndexExtension<IdIndexEntry, Integer> implements DocumentChangeDependentIndex {
   @NonNls public static final ID<IdIndexEntry, Integer> NAME = ID.create("IdIndex");
-  
+
   private final FileBasedIndex.InputFilter myInputFilter = file -> isIndexable(file.getFileType());
 
   public static final boolean ourSnapshotMappingsEnabled = SystemProperties.getBooleanProperty("idea.index.snapshot.mappings.enabled", true);
@@ -61,7 +47,7 @@ public class IdIndex extends FileBasedIndexExtension<IdIndexEntry, Integer> impl
       return Integer.valueOf(in.readByte() & UsageSearchContext.ANY);
     }
   };
-  
+
   private final KeyDescriptor<IdIndexEntry> myKeyDescriptor = new InlineKeyDescriptor<IdIndexEntry>() {
     @Override
     public IdIndexEntry fromInt(int n) {
@@ -73,7 +59,7 @@ public class IdIndex extends FileBasedIndexExtension<IdIndexEntry, Integer> impl
       return idIndexEntry.getWordHashCode();
     }
   };
-  
+
   private final DataIndexer<IdIndexEntry, Integer, FileContent> myIndexer = new DataIndexer<IdIndexEntry, Integer, FileContent>() {
     @Override
     @NotNull
@@ -126,7 +112,7 @@ public class IdIndex extends FileBasedIndexExtension<IdIndexEntry, Integer> impl
   public FileBasedIndex.InputFilter getInputFilter() {
     return myInputFilter;
   }
-  
+
   public static boolean isIndexable(FileType fileType) {
     return fileType instanceof LanguageFileType ||
            fileType instanceof CustomSyntaxTableFileType ||
