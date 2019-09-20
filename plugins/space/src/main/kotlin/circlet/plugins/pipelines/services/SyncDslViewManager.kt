6a052430cb9931d370db1aa16a7632dfd3719906
Aleksandr Krasilnikov <aleksandr.krasilnikov@jetbrains.com>
2019-09-20 12:06:14 +0300

    [idea plugin] fix compilation
    GitOrigin-RevId: 48e922aebec466048765035767b44e211742c39b

--- plugins/space/src/main/kotlin/circlet/plugins/pipelines/services/SyncDslViewManager.kt
+++ plugins/space/src/main/kotlin/circlet/plugins/pipelines/services/SyncDslViewManager.kt
@@ -3,7 +3,7 @@ package circlet.plugins.pipelines.services
 import com.intellij.build.*
 import com.intellij.openapi.project.*
 
-class SyncDslViewManager(project: Project, buildContentManager: BuildContentManager) : AbstractViewManager(project, buildContentManager) {
+class SyncDslViewManager(project: Project, buildContentManager: BuildContentManager) : AbstractViewManager(project) {
     public override fun getViewName(): String {
         return "CircletAutoDSL"
     }
