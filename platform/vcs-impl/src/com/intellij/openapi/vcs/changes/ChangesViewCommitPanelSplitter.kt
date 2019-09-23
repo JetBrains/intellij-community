cc6d0c8dacf15f92ccc5ffa87af60d089b357ad5
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2019-09-23 22:20:22 +0300

    vcs: non-modal: Move initial splitter proportion calculation to "doLayout()" (IDEA-221875)
    Callback of previously used "UiNotifyConnector#doWhenFirstShown()" could be invoked when splitter height was 0
    
    GitOrigin-RevId: ab1bbb09438896b3a1d9171a22b5fabe80df2882

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ChangesViewCommitPanelSplitter.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ChangesViewCommitPanelSplitter.kt
@@ -3,30 +3,23 @@ package com.intellij.openapi.vcs.changes
 
 import com.intellij.ide.util.PropertiesComponent
 import com.intellij.ui.OnePixelSplitter
-import com.intellij.util.ui.update.UiNotifyConnector.doWhenFirstShown
-import javax.swing.JComponent
 
 private const val PROPORTION_KEY = "ChangesViewManager.COMMIT_SPLITTER_PROPORTION"
 
 private class ChangesViewCommitPanelSplitter : OnePixelSplitter(true, PROPORTION_KEY, 1.0f) {
   private var isProportionSet = PropertiesComponent.getInstance().isValueSet(PROPORTION_KEY)
 
-  override fun setSecondComponent(component: JComponent?) {
-    super.setSecondComponent(component)
-
-    if (component != null && !isProportionSet) {
-      calculateProportion()
-    }
+  override fun doLayout() {
+    calculateInitialProportion()
+    super.doLayout()
   }
 
-  private fun calculateProportion() =
-    doWhenFirstShown(this) {
-      val component = secondComponent ?: return@doWhenFirstShown
-      if (isProportionSet) return@doWhenFirstShown
+  private fun calculateInitialProportion() {
+    if (isProportionSet || height <= 0 || secondComponent == null) return
 
-      isProportionSet = true
-      proportion = 1.0f - (component.preferredSize.getHeight().toFloat() / height).coerceIn(0.05f, 0.95f)
-    }
+    isProportionSet = true
+    proportion = 1.0f - (secondComponent.preferredSize.getHeight().toFloat() / height).coerceIn(0.05f, 0.95f)
+  }
 
   override fun loadProportion() {
     if (isProportionSet) super.loadProportion()
