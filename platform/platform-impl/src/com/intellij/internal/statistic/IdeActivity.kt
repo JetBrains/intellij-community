53ec4e4126d7a07e7dda4a87348ab3fd7c8e55ed
Anton Makeev <Anton.Makeev@jetbrains.com>
2019-09-24 17:33:38 +0200

    DA-9 FUS-501 Report long-running IDE activities: simplified refresh events reporting
    (cherry picked from commit 2382281c44e02cbac362a2fdcfbe6c0be3bcab0b)
    
    GitOrigin-RevId: af9c9b5d4b8aff484010902c6ba5d2b90b9dd7f7

--- platform/platform-impl/src/com/intellij/internal/statistic/IdeActivity.kt
+++ platform/platform-impl/src/com/intellij/internal/statistic/IdeActivity.kt
@@ -2,15 +2,10 @@
 package com.intellij.internal.statistic
 
 import com.intellij.internal.statistic.eventLog.FeatureUsageData
-import com.intellij.internal.statistic.eventLog.fus.FeatureUsageLogger
 import com.intellij.internal.statistic.service.fus.collectors.FUCounterUsageLogger
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.application.constraints.isDisposed
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.Disposer
-import com.intellij.util.Alarm
-import com.intellij.util.SingleAlarm
+import org.jetbrains.annotations.ApiStatus
 import java.util.concurrent.atomic.AtomicInteger
 import java.util.function.Consumer
 
@@ -18,6 +13,7 @@ private val LOG = Logger.getInstance("#com.intellij.internal.statistic.IdeActivi
 
 private enum class State { NOT_STARTED, STARTED, FINISHED }
 
+@ApiStatus.Internal
 class IdeActivity @JvmOverloads constructor(private val projectOrNullForApplication: Project?,
                                             private val group: String,
                                             private val activityName: String? = null) {
@@ -76,52 +72,5 @@ class IdeActivity @JvmOverloads constructor(private val projectOrNullForApplicat
     @JvmOverloads
     fun started(projectOrNullForApplication: Project?, group: String, activityName: String? = null): IdeActivity =
       IdeActivity(projectOrNullForApplication, group, activityName).started()
-  }
-}
-
-class DelayedIdeActivity @JvmOverloads constructor(val group: String, val activityName: String? = null) {
-  private val disposable = Disposer.newDisposable()
-  private var activity: IdeActivity? = null
-
-  private var state = State.NOT_STARTED;
-
-  fun started() : DelayedIdeActivity {
-    synchronized(this) {
-      LOG.assertTrue(state == State.NOT_STARTED, state.name)
-      state = State.STARTED;
-
-      if (FeatureUsageLogger.getConfig().isRecordEnabled()) { // avoid unnecessary work, when disabled
-        val delay = 1000
-
-        val application = ApplicationManager.getApplication()
-        application.runReadAction {
-          if (application.isDisposed) return@runReadAction
-
-          Disposer.register(application, disposable)
-          SingleAlarm(Runnable { delayedStart(delay) }, delay, Alarm.ThreadToUse.POOLED_THREAD, disposable).request()
-        }
-      }
-    }
-    return this
-  }
-
-  private fun delayedStart(@Suppress("SameParameterValue") delay: Int) {
-    synchronized(this) {
-      if (disposable.isDisposed) return
-      activity = IdeActivity(null, group, activityName).startedWithData(Consumer { data ->
-        data.addData("activity_start_delay", delay)
-      })
-    }
-  }
-
-  fun finished() : DelayedIdeActivity{
-    synchronized(this) {
-      LOG.assertTrue(state == State.STARTED, state.name)
-      state = State.FINISHED;
-
-      Disposer.dispose(disposable)
-      activity?.finished()
-    }
-    return this
   }
 }
