c1bbdfcd96a38903dcad73cbbe42e3ddf9327524
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2019-09-25 14:50:53 +0300

    js debugger: don't rely on string hashcode
    GitOrigin-RevId: 722e30141934381804057ab1cf1379de8a1a0adf

--- platform/script-debugger/backend/src/debugger/sourcemap/SourceMapDataCache.kt
+++ platform/script-debugger/backend/src/debugger/sourcemap/SourceMapDataCache.kt
@@ -1,28 +1,24 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.debugger.sourcemap
 
-import com.intellij.openapi.util.text.StringHash
-import com.intellij.reference.SoftReference
-import gnu.trove.TLongObjectHashMap
+import com.intellij.util.containers.ContainerUtil
+import java.util.*
 
 object SourceMapDataCache {
 
-  private val map: TLongObjectHashMap<SoftReference<SourceMapDataEx>> = TLongObjectHashMap()
+  private val cache: MutableMap<SourceMapDataImpl, SourceMapDataEx> =
+    Collections.synchronizedMap(ContainerUtil.createSoftKeySoftValueMap<SourceMapDataImpl, SourceMapDataEx>())
 
   fun getOrCreate(sourceMapData: String, mapDebugName: String? = null): SourceMapDataEx? {
-    val hash = StringHash.calc(sourceMapData)
-    val ref = map[hash]
-    val cachedData = SoftReference.deref(ref)
-    if (cachedData != null) {
-      return cachedData
-    }
-
     val data = parseMapSafely(sourceMapData, mapDebugName) ?: return null
+    val value = cache[data]
+    if (value != null) return value
+
     val sourceIndexToMappings = calculateReverseMappings(data)
     val generatedMappings = GeneratedMappingList(data.mappings)
     val result = SourceMapDataEx(data, sourceIndexToMappings, generatedMappings)
 
-    map.put(hash, SoftReference(result))
+    cache[data] = result
 
     return result
   }
