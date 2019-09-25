512c1210d747e75908bd74fc38b2429619111f95
Aleksey Pivovarov <AMPivovarov@gmail.com>
2019-09-25 13:53:58 +0300

    diff: do not warn about suspicious JComponent size in tests
    GitOrigin-RevId: f4afa06d8e49ca5cf03d3187541b6b3102ce7bd6

--- platform/diff-impl/src/com/intellij/diff/tools/util/base/DiffViewerBase.java
+++ platform/diff-impl/src/com/intellij/diff/tools/util/base/DiffViewerBase.java
@@ -61,7 +61,8 @@ public abstract class DiffViewerBase implements DiffViewer, DataProvider {
   @NotNull
   @Override
   public final FrameDiffTool.ToolbarComponents init() {
-    if (LOG.isDebugEnabled() && (getComponent().getWidth() <= 0 || getComponent().getHeight() <= 0)) {
+    if (LOG.isDebugEnabled() && !ApplicationManager.getApplication().isHeadlessEnvironment() &&
+        (getComponent().getWidth() <= 0 || getComponent().getHeight() <= 0)) {
       LOG.warn("Diff shown for a hidden component, initial scroll position might be invalid", new Throwable());
     }
 
