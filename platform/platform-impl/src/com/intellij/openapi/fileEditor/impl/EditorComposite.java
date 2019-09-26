2a4d986d431c9dc744effa425a3b8bad79644da5
Anna.Kozlova <anna.kozlova@jetbrains.com>
2019-09-26 21:23:19 +0200

    file editors statistics: include anonymize file path
    (cherry picked from commit 2682161dd1faa26a11f302795881b1e7a5e1ccc5)
    
    GitOrigin-RevId: 95522c040f45b4b4317a620a563a0c7ce4c44e53

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/EditorComposite.java
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/EditorComposite.java
@@ -20,6 +20,7 @@ import com.intellij.openapi.fileEditor.ex.FileEditorProviderManager;
 import com.intellij.openapi.fileEditor.ex.FileEditorWithProvider;
 import com.intellij.openapi.fileEditor.ex.IdeDocumentHistory;
 import com.intellij.openapi.project.DumbService;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.Pair;
@@ -103,7 +104,8 @@ public abstract class EditorComposite implements Disposable {
     myFileEditorManager = fileEditorManager;
     myInitialFileTimeStamp     = myFile.getTimeStamp();
 
-    Disposer.register(fileEditorManager.getProject(), this);
+    Project project = fileEditorManager.getProject();
+    Disposer.register(project, this);
 
     if(editors.length > 1){
       myTabbedPaneWrapper = createTabbedPaneWrapper(editors);
@@ -123,7 +125,7 @@ public abstract class EditorComposite implements Disposable {
     myFocusWatcher = new FocusWatcher();
     myFocusWatcher.install(myComponent);
 
-    fileEditorManager.getProject().getMessageBus().connect(this).subscribe(
+    project.getMessageBus().connect(this).subscribe(
           FileEditorManagerListener.FILE_EDITOR_MANAGER, new FileEditorManagerListener() {
       @Override
       public void selectionChanged(@NotNull final FileEditorManagerEvent event) {
@@ -137,9 +139,13 @@ public abstract class EditorComposite implements Disposable {
             if (newEditor != null) {
               newEditor.selectNotify();
 
-              FUCounterUsageLogger.getInstance().logEvent("file.editor",
-                                                          "alternative.file.editor.selected",
-                                                          new FeatureUsageData().addData("fileEditor", newEditor.getClass().getName()));
+              FUCounterUsageLogger.getInstance().logEvent(
+                project,
+                "file.editor",
+                "alternative.file.editor.selected",
+                new FeatureUsageData()
+                  .addData("fileEditor", newEditor.getClass().getName())
+                  .addAnonymizedPath(newFile.getPath()));
             }
             ((FileEditorProviderManagerImpl)FileEditorProviderManager.getInstance()).providerSelected(EditorComposite.this);
             ((IdeDocumentHistoryImpl)IdeDocumentHistory.getInstance(myFileEditorManager.getProject())).onSelectionChanged();
