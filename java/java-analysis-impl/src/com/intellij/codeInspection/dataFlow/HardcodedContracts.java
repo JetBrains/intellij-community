272518b1bdcfc48c8840ff2c6a9b46da54e3ddd4
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2019-09-26 10:59:12 +0700

    IDEA-223549 "Constant conditions & exceptions" should handle Objects.requireNonNullElse
    GitOrigin-RevId: c73699d42137d7eafdfba49ed415065f312faabf

--- java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/HardcodedContracts.java
+++ java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/HardcodedContracts.java
@@ -154,7 +154,17 @@ public class HardcodedContracts {
                 StandardMethodContract.fromText("null,!null->false"),
                 StandardMethodContract.fromText("!null,null->false")
               ))
-    .register(enumValues(), ContractProvider.of(StandardMethodContract.fromText("->new")));
+    .register(enumValues(), ContractProvider.of(StandardMethodContract.fromText("->new")))
+    // Convert the following to external annotation once we support Java 9+ external annotations (IDEA-198249)
+    .register(staticCall(JAVA_UTIL_OBJECTS, "requireNonNullElse").parameterCount(2),
+              ContractProvider.of(
+                StandardMethodContract.fromText("!null,_->param1"),
+                StandardMethodContract.fromText("null,!null->param2"),
+                StandardMethodContract.fromText("null,null->fail")))
+    .register(staticCall(JAVA_UTIL_OBJECTS, "requireNonNullElseGet").parameterCount(2),
+              ContractProvider.of(
+                StandardMethodContract.fromText("!null,_->param1"),
+                StandardMethodContract.fromText("null,_->!null")));
 
   public static List<MethodContract> getHardcodedContracts(@NotNull PsiMethod method, @Nullable PsiMethodCallExpression call) {
     PsiClass owner = method.getContainingClass();
