fc6cb83f7fee7959c378256cb5ff8cdef5154955
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2019-09-28 13:37:13 +0300

    Force trailing spaces removal if explicitly specified in .editorconfig
    Fixes IDEA-208267 .editorconfig `trim_trailing_whitespace` is ignored for Markdown files
    
    GitOrigin-RevId: 9c4b8604524b0650ba19a2a45204cba97b09c10c

--- platform/core-api/src/com/intellij/openapi/editor/StripTrailingSpacesFilter.java
+++ platform/core-api/src/com/intellij/openapi/editor/StripTrailingSpacesFilter.java
@@ -55,6 +55,17 @@ public interface StripTrailingSpacesFilter {
   };
 
   /**
+   * Forcibly allows all trailing spaces to be removed regardless of other filters. The actual removal depends
+   * only on provided options.
+   */
+  StripTrailingSpacesFilter ENFORCED_REMOVAL = new StripTrailingSpacesFilter() {
+    @Override
+    public boolean isStripSpacesAllowedForLine(int line) {
+      return true;
+    }
+  };
+
+  /**
    * @param line  The document line. Lines are from 0 to {@link Document#getLineCount()} - 1 inclusive.
    * @return True if trailing spaces can be removed from the line, false otherwise.
    */
