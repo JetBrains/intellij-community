d0784652ede3647f2c1ed5763440d1b2395a3d29
Ivan Semenov <ivan.semenov@jetbrains.com>
2019-09-30 16:15:20 +0300

    [github] Separate handling list of remotes and contents management
    GitOrigin-RevId: ca32275ddbc0003e8cc9417ac9cf380d9220f426

--- plugins/github/src/org/jetbrains/plugins/github/pullrequest/GHPRToolWindowTabsManager.kt
+++ plugins/github/src/org/jetbrains/plugins/github/pullrequest/GHPRToolWindowTabsManager.kt
@@ -3,147 +3,61 @@ package org.jetbrains.plugins.github.pullrequest
 
 import com.intellij.dvcs.repo.VcsRepositoryManager
 import com.intellij.dvcs.repo.VcsRepositoryMappingListener
-import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.runInEdt
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.Disposer
-import com.intellij.openapi.util.Key
 import com.intellij.openapi.vcs.changes.ui.ChangesViewContentManager
-import com.intellij.openapi.vcs.changes.ui.ChangesViewContentProvider
-import com.intellij.openapi.wm.ToolWindowManager
-import com.intellij.ui.content.Content
-import com.intellij.ui.content.ContentFactory
-import com.intellij.vcsUtil.VcsImplUtil
 import git4idea.repo.GitRepository
 import git4idea.repo.GitRepositoryChangeListener
-import git4idea.repo.GitRepositoryManager
 import org.jetbrains.annotations.CalledInAwt
-import org.jetbrains.annotations.Nls
-import org.jetbrains.plugins.github.authentication.GithubAuthenticationManager
+import org.jetbrains.plugins.github.util.CollectionDelta
 import org.jetbrains.plugins.github.util.GitRemoteUrlCoordinates
-import javax.swing.JPanel
+import org.jetbrains.plugins.github.util.GithubGitHelper
+import kotlin.properties.Delegates.observable
 
 @Service
 internal class GHPRToolWindowTabsManager(private val project: Project) {
-  private val changesViewContentManager = ChangesViewContentManager.getInstance(project)
+  private val gitHelper = GithubGitHelper.getInstance()
+  private val contentManager = GHPRToolWindowsTabsContentManager(project, ChangesViewContentManager.getInstance(project))
+
+  private var remoteUrls by observable(setOf<GitRemoteUrlCoordinates>()) { _, oldValue, newValue ->
+    val delta = CollectionDelta(oldValue, newValue)
+    for (item in delta.removedItems) {
+      contentManager.removeTab(item)
+    }
+    for (item in delta.newItems) {
+      contentManager.addTab(item)
+    }
+  }
 
   @CalledInAwt
   fun createPullRequestsTab(remoteUrl: GitRemoteUrlCoordinates) {
-    changesViewContentManager.addContent(createContent(remoteUrl))
-    updateTabNames()
-  }
-
-  private fun updateTabNames() {
-    val contents = changesViewContentManager.findContents { it.remoteUrl != null }
-    if (contents.size == 1) contents.single().displayName = GROUP_PREFIX
-    else {
-      // prefix with root name if there are duplicate remote names
-      val prefixRoot = contents.map { it.remoteUrl!!.remote }.groupBy { it.name }.values.any { it.size > 1 }
-      for (content in contents) {
-        val remoteUrl = content.remoteUrl!!
-        if (prefixRoot) {
-          val shortRootName = VcsImplUtil.getShortVcsRootName(project, remoteUrl.repository.root)
-          content.displayName = "$GROUP_PREFIX: $shortRootName/${remoteUrl.remote.name}"
-        }
-        else {
-          content.displayName = "$GROUP_PREFIX: ${remoteUrl.remote.name}"
-        }
-      }
-    }
+    remoteUrls = remoteUrls + remoteUrl
   }
 
   @CalledInAwt
   fun showPullRequestsTabIfExists(remoteUrl: GitRemoteUrlCoordinates): Boolean {
-    val content = changesViewContentManager.findContents {
-      it.remoteUrl == remoteUrl
-    }.firstOrNull() ?: return false
-
-    ToolWindowManager.getInstance(project).getToolWindow(ChangesViewContentManager.TOOLWINDOW_ID)?.show {
-      changesViewContentManager.setSelectedContent(content, true)
-    }
+    if (!remoteUrls.contains(remoteUrl)) return false
+    contentManager.focusTab(remoteUrl)
     return true
-  }
-
-  private fun createContent(remoteUrl: GitRemoteUrlCoordinates): Content {
-    val disposable = Disposer.newDisposable().also {
-      Disposer.register(it, Disposable { updateTabNames() })
-    }
-
-    return ContentFactory.SERVICE.getInstance().createContent(JPanel(null), GROUP_PREFIX, false).apply {
-      isCloseable = true
-      disposer = disposable
-      description = remoteUrl.url
-      this.remoteUrl = remoteUrl
-      putUserData(ChangesViewContentManager.ORDER_WEIGHT_KEY, ChangesViewContentManager.TabOrderWeight.LAST.weight)
-      putUserData(ChangesViewContentManager.CONTENT_PROVIDER_SUPPLIER_KEY) {
-        object : ChangesViewContentProvider {
-          override fun initContent() =
-            GHPRAccountsComponent(GithubAuthenticationManager.getInstance(), project, remoteUrl, disposable)
-
-          override fun disposeContent() = Disposer.dispose(disposable)
-        }
-      }
-    }
   }
 
   init {
     val busConnection = project.messageBus.connect()
     busConnection.subscribe(VcsRepositoryManager.VCS_REPOSITORY_MAPPING_UPDATED, VcsRepositoryMappingListener {
       runInEdt {
-        val repositories = GitRepositoryManager.getInstance(project).repositories
-        removeContentsForRemovedRepositories(repositories)
+        removeObsoleteUrls(gitHelper.getPossibleRemoteUrlCoordinates(project))
       }
     })
 
-    busConnection.subscribe(GitRepository.GIT_REPO_CHANGE, GitRepositoryChangeListener { repository ->
+    busConnection.subscribe(GitRepository.GIT_REPO_CHANGE, GitRepositoryChangeListener {
       runInEdt {
-        removeContentsForRemovedRemotes(repository)
-        removeContentsForRemovedRemoteUrls(repository)
+        removeObsoleteUrls(gitHelper.getPossibleRemoteUrlCoordinates(project))
       }
     })
   }
 
-  private fun removeContentsForRemovedRepositories(repositories: List<GitRepository>) {
-    findAndRemoveContents {
-      !repositories.contains(it.repository)
-    }
-  }
-
-  private fun removeContentsForRemovedRemotes(repository: GitRepository) {
-    findAndRemoveContents {
-      it.repository == repository && !repository.remotes.contains(it.remote)
-    }
-  }
-
-  private fun removeContentsForRemovedRemoteUrls(repository: GitRepository) {
-    val urls = repository.remotes.map { it.urls }.flatten().toSet()
-    findAndRemoveContents {
-      it.repository == repository && !urls.contains(it.url)
-    }
-  }
-
-  private fun findAndRemoveContents(predicate: (GitRemoteUrlCoordinates) -> Boolean) {
-    val contents = changesViewContentManager.findContents {
-      val parameters = it.remoteUrl
-      parameters != null && predicate(parameters)
-    }
-    for (content in contents) {
-      changesViewContentManager.removeContent(content)
-    }
-    updateTabNames()
-  }
-
-  private var Content.remoteUrl
-    get() = getUserData(REMOTE_URL)
-    set(value) {
-      putUserData(REMOTE_URL, value)
-    }
-
-  companion object {
-    @Nls
-    private const val GROUP_PREFIX = "Pull Requests"
-
-    private val REMOTE_URL = Key<GitRemoteUrlCoordinates>("GHPR_REMOTE_URL")
+  private fun removeObsoleteUrls(urls: Set<GitRemoteUrlCoordinates>) {
+    remoteUrls = remoteUrls.filter { urls.contains(it) }.toSet()
   }
 }
