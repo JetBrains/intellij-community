7282c56d7a2dc07fe638e3504d3a86e93b600823
Svetlana.Zemlyanskaya <svetlana.zemlyanskaya@jetbrains.com>
2019-09-30 17:43:57 +0200

    FUS: validate file scheme by custom rule (FUS-527)
    GitOrigin-RevId: c8313af1df56442a5b678b8bf79f3f2145432786

--- platform/platform-impl/src/com/intellij/internal/statistic/collectors/fus/fileTypes/FileTypeUsageCounterCollector.java
+++ platform/platform-impl/src/com/intellij/internal/statistic/collectors/fus/fileTypes/FileTypeUsageCounterCollector.java
@@ -2,11 +2,15 @@
 package com.intellij.internal.statistic.collectors.fus.fileTypes;
 
 import com.intellij.internal.statistic.eventLog.FeatureUsageData;
+import com.intellij.internal.statistic.eventLog.validator.ValidationResultType;
+import com.intellij.internal.statistic.eventLog.validator.rules.EventContext;
+import com.intellij.internal.statistic.eventLog.validator.rules.impl.CustomWhiteListRule;
 import com.intellij.internal.statistic.service.fus.collectors.FUCounterUsageLogger;
 import com.intellij.internal.statistic.utils.StatisticsUtilKt;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.serviceContainer.BaseKeyedLazyInstance;
 import com.intellij.util.KeyedLazyInstance;
@@ -65,6 +69,28 @@ public class FileTypeUsageCounterCollector {
     @Override
     public String getKey() {
       return schema;
+    }
+  }
+
+  public static final class FileTypeSchemaValidator extends CustomWhiteListRule {
+
+    @Override
+    public boolean acceptRuleId(@Nullable String ruleId) {
+      return "file_type_schema".equals(ruleId);
+    }
+
+    @NotNull
+    @Override
+    protected ValidationResultType doValidate(@NotNull String data, @NotNull EventContext context) {
+      if (isThirdPartyValue(data)) return ValidationResultType.ACCEPTED;
+
+      for (FileTypeUsageSchemaDescriptorEP<FileTypeUsageSchemaDescriptor> ext : EP.getExtensionList()) {
+        if (StringUtil.equals(ext.schema, data)) {
+          return StatisticsUtilKt.getPluginType(ext.getInstance().getClass()).isSafeToReport() ?
+                 ValidationResultType.ACCEPTED : ValidationResultType.THIRD_PARTY;
+        }
+      }
+      return ValidationResultType.REJECTED;
     }
   }
 }
