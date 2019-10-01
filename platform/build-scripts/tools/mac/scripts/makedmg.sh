93160a414a2e7d9864871cee2c25251a9666a0a2
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2019-10-01 13:14:41 +0300

    build scripts: macos: sanitize strings makedmg.sh, simplifications
    GitOrigin-RevId: c13eb1d093f1ab47c6ba8eb158ea8df09b02ee3c

--- platform/build-scripts/tools/mac/scripts/makedmg.sh
+++ platform/build-scripts/tools/mac/scripts/makedmg.sh
@@ -4,43 +4,48 @@
 #immediately exit script with an error if a command fails
 set -euo pipefail
 
-cd $(dirname $0)
+cd "$(dirname "$0")"
 
-EXPLODED=$2.exploded
+EXPLODED="$2.exploded"
+SOURCE_SIT="$1.sit"
+RESULT_DMG="$1.dmg"
+TEMP_DMG="$2.temp.dmg"
+BG_PIC="$2.png"
 
 function log() {
   echo "$(date '+[%H:%M:%S]') $*"
 }
 
-test -d ${EXPLODED} && chmod -R u+wx ${EXPLODED}/*
-rm -rf ${EXPLODED}
-rm -f $1.dmg
-rm -f pack.temp.dmg
+test -d "$EXPLODED" && find "$EXPLODED" -maxdepth 1 -exec chmod -R u+wx '{}' \;
+rm -rf "$EXPLODED"
+rm -f "$RESULT_DMG"
+rm -f "$TEMP_DMG"
 
-mkdir ${EXPLODED}
-log "Unzipping $1.sit to ${EXPLODED}..."
-ditto -x -k $1.sit ${EXPLODED}/
+mkdir "$EXPLODED"
+log "Unzipping ${SOURCE_SIT} to ${EXPLODED}..."
+ditto -x -k "$SOURCE_SIT" "${EXPLODED}/"
 
-rm $1.sit
-BUILD_NAME=$(ls ${EXPLODED}/)
-VOLNAME=`echo $BUILD_NAME | sed 's/\.app$//'`
-BG_PIC="$2.png"
+rm "$SOURCE_SIT"
+BUILD_NAME=$(ls "$EXPLODED")
+log "BUILD_NAME is $BUILD_NAME"
+VOLNAME="${BUILD_NAME%.app}"
+log "VOLNAME is $VOLNAME"
 
-chmod a+x ${EXPLODED}/"$BUILD_NAME"/Contents/MacOS/*
-chmod a+x ${EXPLODED}/"$BUILD_NAME"/Contents/bin/*.sh
-chmod a+x ${EXPLODED}/"$BUILD_NAME"/Contents/bin/fs*
+chmod a+x "${EXPLODED}/$BUILD_NAME/Contents"/MacOS/*
+chmod a+x "${EXPLODED}/$BUILD_NAME/Contents"/bin/*.sh
+chmod a+x "${EXPLODED}/$BUILD_NAME/Contents"/bin/fs*
 
-mkdir ${EXPLODED}/.background
-mv ${BG_PIC} ${EXPLODED}/.background
-ln -s /Applications ${EXPLODED}/"Applications"
+mkdir "${EXPLODED}/.background"
+mv "${BG_PIC}" "${EXPLODED}/.background"
+ln -s /Applications "${EXPLODED}/Applications"
 # allocate space for .DS_Store
 # it's ok to have relatively big (10 MB) empty file, those space would be compressed in resulted dmg
 # otherwise 'no space left on device' errors may occure on attempt to generate relatively small .DS_Store (12 KB)
-dd if=/dev/zero of=${EXPLODED}/DSStorePlaceHolder bs=1024 count=10240
-stat ${EXPLODED}/DSStorePlaceHolder
+dd if=/dev/zero of="${EXPLODED}/DSStorePlaceHolder" bs=1024 count=10240
+stat "${EXPLODED}/DSStorePlaceHolder"
 
 log "Creating unpacked r/w disk image ${VOLNAME}..."
-hdiutil create -srcfolder ./${EXPLODED} -volname "$VOLNAME" -anyowners -nospotlight -quiet -fs HFS+ -fsargs "-c c=64,a=16,e=16" -format UDRW $2.temp.dmg
+hdiutil create -srcfolder "./${EXPLODED}" -volname "$VOLNAME" -anyowners -nospotlight -quiet -fs HFS+ -fsargs "-c c=64,a=16,e=16" -format UDRW "$TEMP_DMG"
 
 # check if the image already mounted
 if [ -d "/Volumes/$1" ]; then
@@ -54,8 +59,8 @@ if [ -d "/Volumes/$1" ]; then
     if [ -d "/Volumes/$1" ]; then
       if [ $attempt -ge $limit ]; then
         log "/Volumes/$1 - the image is still mounted. By the reason the build will be stopped."
-        rm -rf ${EXPLODED}
-        rm -f $2.temp.dmg
+        rm -rf "$EXPLODED"
+        rm -f "$TEMP_DMG"
         exit 1
       fi
     fi
@@ -64,25 +69,25 @@ fi
 
 # mount this image
 log "Mounting unpacked r/w disk image..."
-device=$(hdiutil attach -readwrite -noverify -mountpoint /Volumes/"$1" -noautoopen $2.temp.dmg | egrep '^/dev/' | sed 1q | awk '{print $1.dmg}')
-log "Mounted as ${device}."
+device=$(hdiutil attach -readwrite -noverify -mountpoint "/Volumes/$1" -noautoopen "$TEMP_DMG" | grep '^/dev/' | awk 'NR==1{print $1}')
+log "Mounted as $device"
 sleep 10
-find /Volumes/"$1" -maxdepth 1
+find "/Volumes/$1" -maxdepth 1
 
 # set properties
 log "Updating $VOLNAME disk image styles..."
-stat /Volumes/"$1"/DSStorePlaceHolder || true
-rm /Volumes/"$1"/DSStorePlaceHolder
-perl makedmg.pl "$VOLNAME" ${BG_PIC} "$1"
+stat "/Volumes/$1/DSStorePlaceHolder"
+rm "/Volumes/$1/DSStorePlaceHolder"
+perl makedmg.pl "$VOLNAME" "$BG_PIC" "$1"
 
 sync;sync;sync
-hdiutil detach ${device}
+hdiutil detach "$device"
 
-log "Compressing r/w disk image to $1.dmg..."
-hdiutil convert $2.temp.dmg -quiet -format UDZO -imagekey zlib-level=9 -o $1.dmg
-rm -f $2.temp.dmg
+log "Compressing r/w disk image to ${RESULT_DMG}..."
+hdiutil convert "$TEMP_DMG" -quiet -format UDZO -imagekey zlib-level=9 -o "$RESULT_DMG"
+rm -f "$TEMP_DMG"
 
-hdiutil internet-enable -no $1.dmg
-rm -rf ${EXPLODED}
+hdiutil internet-enable -no "$RESULT_DMG"
+rm -rf "$EXPLODED"
 
 log "Done"
