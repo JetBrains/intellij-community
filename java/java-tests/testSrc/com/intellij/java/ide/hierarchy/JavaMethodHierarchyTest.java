JetBrains/intellij-community

commit c4118540b926875162fcf22391a476bb4ab41d6a

super method searcher fix

source/com/intellij/ide/hierarchy/method/MethodHierarchyUtil.java
@@ -3,23 +3,26 @@ package com.intellij.ide.hierarchy.method;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiMethod;
 import com.intellij.psi.PsiModifier;
-import com.intellij.psi.util.MethodSignatureBackedByPsiMethod;
-import com.intellij.psi.search.searches.SuperMethodsSearch;
+import com.intellij.psi.PsiSubstitutor;
+import com.intellij.psi.util.MethodSignatureUtil;
+import com.intellij.psi.util.MethodSignature;
 
 final class MethodHierarchyUtil {
   public static PsiMethod findBaseMethodInClass(final PsiMethod baseMethod, final PsiClass aClass, final boolean checkBases) {
     if (baseMethod == null) return null; // base method is invalid
     if (cannotBeOverridding(baseMethod)) return null;
-    final MethodSignatureBackedByPsiMethod signature = SuperMethodsSearch.search(baseMethod, aClass, checkBases, false).findFirst();
-    return signature == null ? null : signature.getMethod();
+    /*if (!checkBases) return MethodSignatureUtil.findMethodBySignature(aClass, signature, false);*/
+    return MethodSignatureUtil.findMethodBySuperMethod(aClass, baseMethod, checkBases);
+    /*final MethodSignatureBackedByPsiMethod signature = SuperMethodsSearch.search(baseMethod, aClass, checkBases, false).findFirst();
+    return signature == null ? null : signature.getMethod();*/
   }
 
   private static boolean cannotBeOverridding(final PsiMethod method) {
     final PsiClass parentClass = method.getContainingClass();
     return parentClass == null
-        || method.isConstructor()
-        || method.hasModifierProperty(PsiModifier.STATIC)
-        || method.hasModifierProperty(PsiModifier.PRIVATE);
+           || method.isConstructor()
+           || method.hasModifierProperty(PsiModifier.STATIC)
+           || method.hasModifierProperty(PsiModifier.PRIVATE);
   }
 
 }

codeInsight/impl/com/intellij/codeInsight/daemon/impl/OverriddenMarkersPass.java
@@ -138,7 +138,7 @@ public class OverriddenMarkersPass extends TextEditorHighlightingPass {
       PsiSubstitutor substitutor = TypeConversionUtil.getSuperClassSubstitutor(parentClass, derived, PsiSubstitutor.EMPTY);
       if (substitutor == null) substitutor = PsiSubstitutor.EMPTY;
       MethodSignature signature = method.getSignature(substitutor);
-      PsiMethod method1 = MethodSignatureUtil.findMethodBySuperSignature(derived, signature);
+      PsiMethod method1 = MethodSignatureUtil.findMethodBySuperSignature(derived, signature, false);
       if (method1 != null) {
         if (method1.hasModifierProperty(PsiModifier.STATIC) ||
             (method.hasModifierProperty(PsiModifier.PACKAGE_LOCAL) && !manager.arePackagesTheSame(parentClass, derived))) {

openapi/src/com/intellij/psi/util/MethodSignatureUtil.java
@@ -216,13 +216,27 @@ public class MethodSignatureUtil {
     return null;
   }
 
-  public static PsiMethod findMethodBySuperSignature(final PsiClass aClass, MethodSignature methodSignature) {
-    List<Pair<PsiMethod, PsiSubstitutor>> pairs = aClass.findMethodsAndTheirSubstitutorsByName(methodSignature.getName(), false);
+  public static PsiMethod findMethodBySuperSignature(final PsiClass aClass, MethodSignature methodSignature, final boolean checkBases) {
+    List<Pair<PsiMethod, PsiSubstitutor>> pairs = aClass.findMethodsAndTheirSubstitutorsByName(methodSignature.getName(), checkBases);
     for (Pair<PsiMethod, PsiSubstitutor> pair : pairs) {
       PsiMethod method = pair.first;
       PsiSubstitutor substitutor = pair.second;
       MethodSignature foundMethodSignature = method.getSignature(substitutor);
       if (isSubsignature(methodSignature, foundMethodSignature)) return method;
+    }
+    return null;
+  }
+
+  public static PsiMethod findMethodBySuperMethod(final PsiClass aClass, PsiMethod method, final boolean checkBases) {
+    List<Pair<PsiMethod, PsiSubstitutor>> pairs = aClass.findMethodsAndTheirSubstitutorsByName(method.getName(), checkBases);
+    for (Pair<PsiMethod, PsiSubstitutor> pair : pairs) {
+      PsiMethod candidate = pair.first;
+      PsiSubstitutor substitutor = pair.second;
+      MethodSignature candidateSignature = candidate.getSignature(substitutor);
+      PsiSubstitutor superSubstitutor = TypeConversionUtil.getClassSubstitutor(method.getContainingClass(), candidate.getContainingClass(), substitutor);
+      if (superSubstitutor == null) continue;
+      MethodSignature superSignature = method.getSignature(superSubstitutor);
+      if (isSubsignature(superSignature, candidateSignature)) return candidate;
     }
     return null;
   }

source/com/intellij/psi/impl/PsiSuperMethodImplUtil.java
@@ -1,6 +1,5 @@
 package com.intellij.psi.impl;
 
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.Key;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.source.HierarchicalMethodSignatureImpl;
@@ -14,7 +13,6 @@ import java.util.*;
 
 public class PsiSuperMethodImplUtil {
   private static final Key<CachedValue<Map<MethodSignature, HierarchicalMethodSignatureImpl>>> SIGNATURES_KEY = Key.create("MAP_KEY");
-  private static final Logger LOG = Logger.getInstance("#com.intellij.psi.impl.PsiSuperMethodImplUtil");
 
   private PsiSuperMethodImplUtil() {
   }
@@ -64,15 +62,10 @@ public class PsiSuperMethodImplUtil {
     return true;
   }
 
-  public static HierarchicalMethodSignatureImpl getHierarchicalMethodSignature(final PsiClass parentClass, final PsiMethod method) {
-    return getSignaturesMap(parentClass).get(method.getSignature(PsiSubstitutor.EMPTY));
-  }
-
   public static PsiMethod findDeepestSuperMethod(PsiMethod method) {
     if (!canHaveSuperMethod(method, true, false)) return null;
     return DeepestSuperMethodsSearch.search(method).findFirst();
   }
-
 
   private static void buildMethodHierarchy(PsiClass aClass,
                                            PsiSubstitutor substitutor,

source/com/intellij/psi/impl/search/JavaOverridingMethodsSearcher.java
@@ -31,7 +31,7 @@ public class JavaOverridingMethodsSearcher implements QueryExecutor<PsiMethod, O
         PsiSubstitutor substitutor = TypeConversionUtil.getSuperClassSubstitutor(parentClass, inheritor,
                                                                                  PsiSubstitutor.EMPTY);
         MethodSignature signature = method.getSignature(substitutor);
-        PsiMethod method1 = MethodSignatureUtil.findMethodBySuperSignature(inheritor, signature);
+        PsiMethod method1 = MethodSignatureUtil.findMethodBySuperSignature(inheritor, signature, false);
         if (method1 == null ||
             method1.hasModifierProperty(PsiModifier.STATIC) ||
             (method.hasModifierProperty(PsiModifier.PACKAGE_LOCAL) && !method1.getManager().arePackagesTheSame(parentClass, inheritor))) {

source/com/intellij/psi/impl/search/MethodDeepestSuperSearcher.java
@@ -22,11 +22,7 @@ public class MethodDeepestSuperSearcher implements QueryExecutor<PsiMethod, PsiM
   private static final Logger LOG = Logger.getInstance("#com.intellij.psi.impl.search.MethodDeepestSuperSearcher");
 
   public boolean execute(final PsiMethod method, final Processor<PsiMethod> consumer) {
-    PsiClass aClass = method.getContainingClass();
-    if (aClass == null) return true;
-
-    HierarchicalMethodSignatureImpl hierarchical = PsiSuperMethodImplUtil.getHierarchicalMethodSignature(aClass, method);
-    LOG.assertTrue(hierarchical != null);
+    final HierarchicalMethodSignature hierarchical = method.getHierarchicalMethodSignature();
     HierarchicalMethodSignature deepest = findDeepestSuperOrSelfSignature(hierarchical);
     if (deepest == hierarchical) return true;
     if (deepest != null) return consumer.process(deepest.getMethod());

commit 991047113e285fed5f826d69b57a46dfc9e3ca0f

MethodHierarchyTreeStructure: fixed super-methods hierarchy (was broken in c4118540b926 13 years ago!)
IDEA-110828 Method Hierarchy view should start at the top level class/interface where the method is defined, not at the class where the action has been invoked

GitOrigin-RevId: b0341033878442aa18d77219c622136295d8b876

java/java-impl/src/com/intellij/ide/hierarchy/method/MethodHierarchyTreeStructure.java
@@ -9,14 +9,13 @@ import com.intellij.psi.*;
 import com.intellij.psi.search.SearchScope;
 import com.intellij.psi.search.searches.ClassInheritorsSearch;
 import com.intellij.psi.search.searches.FunctionalExpressionSearch;
+import com.intellij.util.ArrayUtil;
 import com.intellij.util.ArrayUtilRt;
+import one.util.streamex.StreamEx;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public final class MethodHierarchyTreeStructure extends HierarchyTreeStructure {
   private final SmartPsiElementPointer<PsiMethod> myMethod;
@@ -35,109 +34,59 @@ public final class MethodHierarchyTreeStructure extends HierarchyTreeStructure {
   }
 
   private HierarchyNodeDescriptor buildHierarchyElement(final Project project, final PsiMethod method) {
-    final PsiClass suitableBaseClass = findSuitableBaseClass(method);
-
-    final ArrayList<PsiClass> superClasses = createSuperClasses(suitableBaseClass);
-
-    if (!suitableBaseClass.equals(method.getContainingClass())) {
-      superClasses.add(0, suitableBaseClass);
-    }
-
-    // remove from the top of the branch the classes that contain no 'method'
-    for(int i = superClasses.size() - 1; i >= 0; i--){
-      final PsiClass psiClass = superClasses.get(i);
-
-      if (MethodHierarchyUtil.findBaseMethodInClass(method, psiClass, false) == null) {
-        superClasses.remove(i);
-      }
-      else {
-        break;
-      }
-    }
+    List<PsiClass> superClasses = buildSuperChain(method);
+    PsiClass containingClass = method.getContainingClass();
+    assert containingClass != null;
 
     HierarchyNodeDescriptor descriptor = null;
-    for(int i = superClasses.size() - 1; i >= 0; i--){
-      final PsiClass superClass = superClasses.get(i);
+    for (final PsiClass superClass : superClasses) {
       final HierarchyNodeDescriptor newDescriptor = new MethodHierarchyNodeDescriptor(project, descriptor, superClass, false, this);
-      if (descriptor != null){
-        descriptor.setCachedChildren(new HierarchyNodeDescriptor[] {newDescriptor});
+      if (descriptor != null) {
+        descriptor.setCachedChildren(new HierarchyNodeDescriptor[]{newDescriptor});
       }
       descriptor = newDescriptor;
     }
-    final HierarchyNodeDescriptor newDescriptor = new MethodHierarchyNodeDescriptor(project, descriptor, method.getContainingClass(), true, this);
+    final HierarchyNodeDescriptor newDescriptor = new MethodHierarchyNodeDescriptor(project, descriptor, containingClass, true, this);
     if (descriptor != null) {
       descriptor.setCachedChildren(new HierarchyNodeDescriptor[] {newDescriptor});
     }
     return newDescriptor;
   }
 
-  private static ArrayList<PsiClass> createSuperClasses(PsiClass aClass) {
-    if (!aClass.isValid()) {
-      return new ArrayList<>();
+  private static List<PsiClass> buildSuperChain(PsiMethod method) {
+    List<PsiClass> superClasses = new ArrayList<>();
+    while (true) {
+      PsiMethod superMethod = ArrayUtil.getFirstElement(method.findSuperMethods());
+      if (superMethod == null || superClasses.contains(superMethod.getContainingClass())) break;
+      superClasses.addAll(findInheritanceChain(method.getContainingClass(), superMethod.getContainingClass()));
+      method = superMethod;
     }
-
-    final ArrayList<PsiClass> superClasses = new ArrayList<>();
-    while (!isJavaLangObject(aClass)) {
-      final PsiClass aClass1 = aClass;
-      final PsiClass[] superTypes = aClass1.getSupers();
-      PsiClass superType = null;
-      // find class first
-      for (final PsiClass type : superTypes) {
-        if (!type.isInterface() && !isJavaLangObject(type)) {
-          superType = type;
-          break;
-        }
-      }
-      // if we haven't found a class, try to find an interface
-      if (superType == null) {
-        for (final PsiClass type : superTypes) {
-          if (!isJavaLangObject(type)) {
-            superType = type;
-            break;
-          }
-        }
-      }
-      if (superType == null) break;
-      if (superClasses.contains(superType)) break;
-      superClasses.add(superType);
-      aClass = superType;
-    }
-
     return superClasses;
   }
 
-  private static boolean isJavaLangObject(final PsiClass aClass) {
-    return CommonClassNames.JAVA_LANG_OBJECT.equals(aClass.getQualifiedName());
-  }
-
-  private static PsiClass findSuitableBaseClass(final PsiMethod method) {
-    final PsiClass containingClass = method.getContainingClass();
-
-    if (containingClass instanceof PsiAnonymousClass) {
-      return containingClass;
-    }
-
-    final PsiClass superClass = containingClass.getSuperClass();
-    if (superClass == null) {
-      return containingClass;
-    }
-
-    if (MethodHierarchyUtil.findBaseMethodInClass(method, superClass, true) == null) {
-      for (final PsiClass anInterface : containingClass.getInterfaces()) {
-        if (MethodHierarchyUtil.findBaseMethodInClass(method, anInterface, true) != null) {
-          return anInterface;
+  private static List<PsiClass> findInheritanceChain(PsiClass subClass, PsiClass superClass) {
+    Map<PsiClass, PsiClass> inheritanceMap = new HashMap<>();
+    Queue<PsiClass> workQueue = new ArrayDeque<>();
+    workQueue.add(subClass);
+    while (!workQueue.isEmpty()) {
+      PsiClass cls = workQueue.poll();
+      for (PsiClass sup : StreamEx.of(cls.getInterfaces()).prepend(cls.getSuperClass()).nonNull()) {
+        if (!inheritanceMap.containsKey(sup)) {
+          inheritanceMap.put(sup, cls);
+          workQueue.offer(sup);
+          if (sup == superClass) {
+            return StreamEx.iterate(superClass, c -> c != subClass, inheritanceMap::get).toList();
+          }
         }
       }
     }
-
-    return containingClass;
+    return Collections.emptyList();
   }
 
   @Nullable
   public final PsiMethod getBaseMethod() {
     return myMethod.getElement();
   }
-
 
   @NotNull
   @Override

java/java-impl/src/com/intellij/ide/hierarchy/method/MethodHierarchyUtil.java
@@ -24,10 +24,15 @@ final class MethodHierarchyUtil {
   public static PsiMethod findBaseMethodInClass(final PsiMethod baseMethod, final PsiClass aClass, final boolean checkBases) {
     if (baseMethod == null) return null; // base method is invalid
     if (cannotBeOverridding(baseMethod)) return null;
-    /*if (!checkBases) return MethodSignatureUtil.findMethodBySignature(aClass, signature, false);*/
-    return MethodSignatureUtil.findMethodBySuperMethod(aClass, baseMethod, checkBases);
-    /*final MethodSignatureBackedByPsiMethod signature = SuperMethodsSearch.search(baseMethod, aClass, checkBases, false).findFirst();
-    return signature == null ? null : signature.getMethod();*/
+    PsiMethod subMethod = MethodSignatureUtil.findMethodBySuperMethod(aClass, baseMethod, checkBases);
+    if (subMethod != null) return subMethod;
+    PsiMethod[] methods = baseMethod.findSuperMethods(aClass);
+    for (PsiMethod method : methods) {
+      if (checkBases || method.getContainingClass() == aClass) {
+        return method;
+      }
+    }
+    return null;
   }
 
   private static boolean cannotBeOverridding(final PsiMethod method) {

java/java-tests/testData/ide/hierarchy/method/CyclicInheritance/CyclicInheritance_verification.xml
@@ -1,0 +1,7 @@
+<node text="C  ()">
+  <node text="B  ()">
+    <node text="A  ()">
+      <node text="D  ()" base="true"/>
+    </node>
+  </node>
+</node>

java/java-tests/testData/ide/hierarchy/method/CyclicInheritance/X.java
@@ -1,0 +1,12 @@
+interface A extends C {
+  void foo();
+}
+interface B extends A {
+  void foo();
+}
+interface C extends B {
+  void foo();
+}
+class D implements C {
+  void foo();
+}

java/java-tests/testData/ide/hierarchy/method/InterfaceInheritance/InterfaceInheritance_verification.xml
@@ -1,0 +1,7 @@
+<node text="B  ()">
+  <node text="C  ()">
+    <node text="E  ()" base="true">
+      <node text="F  ()"/>
+    </node>
+  </node>
+</node>

java/java-tests/testData/ide/hierarchy/method/InterfaceInheritance/X.java
@@ -1,0 +1,16 @@
+interface A {
+  void foo();
+}
+interface B {
+  void bar();
+}
+interface C extends A, B {}
+interface D {
+  void bar(String arg);
+}
+interface E extends D, C {
+  default void bar() {}
+}
+class F implements E {
+  void bar() {}
+}

java/java-tests/testData/ide/hierarchy/method/NoHierarchy/A.java
@@ -1,0 +1,3 @@
+class A {
+  void foo() {}
+}

java/java-tests/testData/ide/hierarchy/method/NoHierarchy/NoHierarchy_verification.xml
@@ -1,0 +1,1 @@
+<node text="A  ()" base="true"/>

java/java-tests/testData/ide/hierarchy/method/ObjectMethod/ObjectMethod_verification.xml
@@ -1,0 +1,3 @@
+<node text="Object  (java.lang)">
+  <node text="Foo  ()" base="true"/>
+</node>

java/java-tests/testData/ide/hierarchy/method/ObjectMethod/X.java
@@ -1,0 +1,5 @@
+class Foo {
+  int hashCode() {
+    return 0;
+  }
+}

java/java-tests/testData/ide/hierarchy/method/OnlyDown/OnlyDown_verification.xml
@@ -1,0 +1,7 @@
+<node text="X  ()" base="true">
+  <node text="YY  ()"/>
+  <node text="YYY  ()"/>
+  <node text="Y  ()">
+    <node text="Z  ()"/>
+  </node>
+</node>

java/java-tests/testData/ide/hierarchy/method/OnlyDown/X.java
@@ -1,0 +1,14 @@
+class X {
+  Object m(String s) {}
+}
+class Y extends X {
+}
+class Z extends Y {
+  String m(String s) {}
+}
+class YY extends X {
+  Object m(String s) {}
+}
+class YYY extends X {
+  String m(Object s) {}
+}

java/java-tests/testData/ide/hierarchy/method/OnlyDownHide/OnlyDownHide_verification.xml
@@ -1,0 +1,6 @@
+<node text="X  (foo)" base="true">
+  <node text="Y  (foo)">
+    <node text="Z  (foo)"/>
+  </node>
+  <node text="YY  (foo)"/>
+</node>

java/java-tests/testData/ide/hierarchy/method/OnlyDownHide/X.java
@@ -1,0 +1,16 @@
+package foo;
+
+class X {
+  Object m(String s) {}
+}
+class Y extends X {
+}
+class Z extends Y {
+  String m(String s) {}
+}
+class YY extends X {
+  Object m(String s) {}
+}
+class YYY extends X {
+  String m(Object s) {}
+}

java/java-tests/testData/ide/hierarchy/method/OnlyUp/OnlyUp_verification.xml
@@ -1,0 +1,5 @@
+<node text="X  ()">
+  <node text="Y  ()">
+    <node text="Z  ()" base="true"/>
+  </node>
+</node>

java/java-tests/testData/ide/hierarchy/method/OnlyUp/X.java
@@ -1,0 +1,14 @@
+class X {
+  Object m(String s) {}
+}
+class Y extends X {
+}
+class Z extends Y {
+  String m(String s) {}
+}
+class YY extends X {
+  Object m(String s) {}
+}
+class YYY extends X {
+  String m(Object s) {}
+}

java/java-tests/testData/ide/hierarchy/method/UpAndDown/UpAndDown_verification.xml
@@ -1,0 +1,5 @@
+<node text="X  ()">
+  <node text="Y  ()" base="true">
+    <node text="Z  ()"/>
+  </node>
+</node>

java/java-tests/testData/ide/hierarchy/method/UpAndDown/X.java
@@ -1,0 +1,12 @@
+class X<C> {
+  void foo(C c) {}
+}
+class Y<D extends Number> extends X<D> {
+  void foo(D d) {}
+}
+class Z extends Y<Integer> {
+  void foo(Integer i) {}
+}
+class T extends Y<Integer> {
+  void foo(Long l) {} // doesn't override
+}

java/java-tests/testSrc/com/intellij/java/ide/hierarchy/JavaMethodHierarchyTest.java
@@ -1,0 +1,99 @@
+/*
+ * Copyright 2000-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.java.ide.hierarchy;
+
+import com.intellij.JavaTestUtil;
+import com.intellij.ide.hierarchy.HierarchyBrowserBaseEx;
+import com.intellij.ide.hierarchy.HierarchyBrowserManager;
+import com.intellij.ide.hierarchy.method.MethodHierarchyTreeStructure;
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.psi.JavaPsiFacade;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.search.ProjectScope;
+import com.intellij.testFramework.IdeaTestUtil;
+import com.intellij.testFramework.codeInsight.hierarchy.HierarchyViewTestBase;
+import org.jetbrains.annotations.NotNull;
+
+public class JavaMethodHierarchyTest extends HierarchyViewTestBase {
+  @NotNull
+  @Override
+  protected String getTestDataPath() {
+    return JavaTestUtil.getJavaTestDataPath();
+  }
+
+  @Override
+  protected String getBasePath() {
+    return "ide/hierarchy/method/" + getTestName(false);
+  }
+
+  public void testNoHierarchy() throws Exception {
+    doTest("A", "foo", "A.java");
+  }
+
+  public void testOnlyUp() throws Exception {
+    doTest("Z", "m", "X.java");
+  }
+
+  public void testOnlyDown() throws Exception {
+    doTest("X", "m", "X.java");
+  }
+
+  public void testOnlyDownHide() throws Exception {
+    doTestHideIrrelevantClasses("foo.X", "m", "X.java");
+  }
+
+  public void testUpAndDown() throws Exception {
+    doTestHideIrrelevantClasses("Y", "foo", "X.java");
+  }
+
+  public void testObjectMethod() throws Exception {
+    doTest("Foo", "hashCode", "X.java");
+  }
+
+  public void testInterfaceInheritance() throws Exception {
+    doTest("E", "bar", "X.java");
+  }
+
+  public void testCyclicInheritance() throws Exception {
+    doTest("D", "foo", "X.java");
+  }
+
+  private void doTest(final String classFqn, final String methodName, final String... fileNames) throws Exception {
+    doHierarchyTest(() -> {
+      final PsiClass psiClass = JavaPsiFacade.getInstance(getProject()).findClass(classFqn, ProjectScope.getProjectScope(getProject()));
+      final PsiMethod method = psiClass.findMethodsByName(methodName, false) [0];
+      return new MethodHierarchyTreeStructure(getProject(), method, HierarchyBrowserBaseEx.SCOPE_PROJECT);
+    }, fileNames);
+  }
+
+  private void doTestHideIrrelevantClasses(String classFqn, String methodName, String... fileNames) throws Exception {
+    HierarchyBrowserManager.State state = HierarchyBrowserManager.getInstance(myProject).getState();
+    assertNotNull(state);
+    state.HIDE_CLASSES_WHERE_METHOD_NOT_IMPLEMENTED = true;
+    try {
+      doTest(classFqn, methodName, fileNames);
+    }
+    finally {
+      state.HIDE_CLASSES_WHERE_METHOD_NOT_IMPLEMENTED = false;
+    }
+  }
+
+  @Override
+  protected Sdk getTestProjectJdk() {
+    return IdeaTestUtil.getMockJdk18();
+  }
+}