324a09af110857e1e8d38ea71fe26353622ec388
Yann Cébron <yann.cebron@jetbrains.com>
2019-10-01 18:13:59 +0200

    FileDocumentManagerListener: add @see javadoc
    GitOrigin-RevId: 46b59bb6ef85e7324730924972b36f563205a35f

--- platform/platform-api/src/com/intellij/openapi/fileEditor/FileDocumentManagerListener.java
+++ platform/platform-api/src/com/intellij/openapi/fileEditor/FileDocumentManagerListener.java
@@ -1,6 +1,7 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.fileEditor;
 
+import com.intellij.AppTopics;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -8,6 +9,9 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.EventListener;
 
+/**
+ * @see AppTopics#FILE_DOCUMENT_SYNC
+ */
 public interface FileDocumentManagerListener extends EventListener {
 
   /**
