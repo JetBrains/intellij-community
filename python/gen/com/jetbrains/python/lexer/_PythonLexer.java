a69bce1a73b45c07c7ff672f8b9367843cd2acdb
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2019-10-02 12:23:26 +0300

    IDEA-CR-53124: PY-37842 Restore proper resetting of f-string specific state in Python lexer
    It was accidentally removed as part of assignment expression support
    (the lexer was regenerated without the corresponding skeleton), but
    then brought back in master together with other changes in
    4bff2a5b5dc1f01d90d470e6d7e65e4f7dbc7e9b.
    
    GitOrigin-RevId: 78642db4292a23078ba671c50cfc997c304f5a80

--- python/gen/com/jetbrains/python/lexer/_PythonLexer.java
+++ python/gen/com/jetbrains/python/lexer/_PythonLexer.java
@@ -874,6 +874,7 @@ return yylength()-s.length();
     zzAtEOF  = false;
     zzAtBOL = true;
     zzEndRead = end;
+    fStringHelper.reset();
     yybegin(initialState);
   }
 
@@ -940,7 +941,7 @@ return yylength()-s.length();
 
 
   /**
-   * Reports an error that occured while scanning.
+   * Reports an error that occurred while scanning.
    *
    * In a wellformed scanner (no or only correct usage of
    * yypushback(int) and a match-all fallback rule) this method
