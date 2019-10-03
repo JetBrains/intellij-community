87d79781653ece7fc2c18ccc6072ce112b1ddea9
peter <peter@jetbrains.com>
2019-10-03 21:37:19 +0200

    IdeKeyEventDispatcher: extract colder execution path into a method for JITc-friendliness
    GitOrigin-RevId: db851ebba5ebc4254b3694deea27584dabaf7729

--- platform/platform-impl/src/com/intellij/openapi/keymap/impl/IdeKeyEventDispatcher.java
+++ platform/platform-impl/src/com/intellij/openapi/keymap/impl/IdeKeyEventDispatcher.java
@@ -737,17 +737,20 @@ public final class IdeKeyEventDispatcher implements Disposable {
       }
     }
 
-    final List<AnAction> actions = myContext.getActions();
-
+    List<AnAction> actions = myContext.getActions();
     if (actions.size() > 1) {
-      final List<AnAction> readOnlyActions = Collections.unmodifiableList(actions);
-      for (ActionPromoter promoter : ActionPromoter.EP_NAME.getExtensions()) {
-        final List<AnAction> promoted = promoter.promote(readOnlyActions, myContext.getDataContext());
-        if (promoted == null || promoted.isEmpty()) continue;
+      rearrangeByPromoters(actions);
+    }
+  }
 
-        actions.removeAll(promoted);
-        actions.addAll(0, promoted);
-      }
+  private void rearrangeByPromoters(List<AnAction> actions) {
+    List<AnAction> readOnlyActions = Collections.unmodifiableList(actions);
+    for (ActionPromoter promoter : ActionPromoter.EP_NAME.getExtensions()) {
+      List<AnAction> promoted = promoter.promote(readOnlyActions, myContext.getDataContext());
+      if (promoted == null || promoted.isEmpty()) continue;
+
+      actions.removeAll(promoted);
+      actions.addAll(0, promoted);
     }
   }
 
