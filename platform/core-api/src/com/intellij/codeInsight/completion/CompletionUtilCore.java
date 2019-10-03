21c7740fa40d835b431683e09b318c4d3dd076bc
Artsiom Chapialiou <achapialiou@gmail.com>
2019-10-02 23:06:53 -0400

    separate DUMMY_IDENTIFIER from the main text

--- platform/core-api/src/com/intellij/codeInsight/completion/CompletionUtilCore.java
+++ platform/core-api/src/com/intellij/codeInsight/completion/CompletionUtilCore.java
@@ -24,6 +24,6 @@ public class CompletionUtilCore {
   /**
    * A default string that is inserted to the file before completion to guarantee that there'll always be some non-empty element there
    */
-  public static @NonNls final String DUMMY_IDENTIFIER = "IntellijIdeaRulezzz ";
-  public static @NonNls final String DUMMY_IDENTIFIER_TRIMMED = "IntellijIdeaRulezzz";
+  public static @NonNls final String DUMMY_IDENTIFIER = "_IntellijIdeaRulezzz ";
+  public static @NonNls final String DUMMY_IDENTIFIER_TRIMMED = "_IntellijIdeaRulezzz";
 }
