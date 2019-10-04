34bf7d4c987e854cb0d1bc9f9efcafece3d6e18f
Dmitry Jemerov <yole@jetbrains.com>
2019-10-04 17:48:16 +0200

    Allow dynamic unloading for lang.foldingBuilder, lang.commenter and lang.formatter
    GitOrigin-RevId: d28c472392bcd212b63da8a2f3ecbb3f7343c031

--- platform/core-api/src/com/intellij/lang/folding/LanguageFolding.java
+++ platform/core-api/src/com/intellij/lang/folding/LanguageFolding.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.lang.folding;
 
 import com.intellij.lang.ASTNode;
@@ -6,9 +6,11 @@ import com.intellij.lang.Language;
 import com.intellij.lang.LanguageExtension;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.project.DumbService;
 import com.intellij.psi.PsiElement;
+import com.intellij.util.KeyedLazyInstance;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -20,11 +22,13 @@ import java.util.List;
  * @author Konstantin Bulenkov
  */
 public class LanguageFolding extends LanguageExtension<FoldingBuilder> {
+  public static final ExtensionPointName<KeyedLazyInstance<FoldingBuilder>> EP_NAME = ExtensionPointName.create("com.intellij.lang.foldingBuilder");
   public static final LanguageFolding INSTANCE = new LanguageFolding();
+
   private static final Logger LOG = Logger.getInstance(LanguageFolding.class);
 
   private LanguageFolding() {
-    super("com.intellij.lang.foldingBuilder");
+    super(EP_NAME);
   }
 
   /**
