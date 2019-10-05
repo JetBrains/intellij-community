5a5cd65df447eeded8c7c667aaa50a9e5fbefca9
Dana Dahlstrom <dahlstrom@google.com>
2019-10-04 18:00:00 -0700

    Add library containing org.eclipse.xtext.xbase.lib
    … apparently misnamed in cidr by commit 14b1f85
    and unused until commit 78f3f11.
    
    Change-Id: If39608a414823671fda32280b97877c6e3ca7537

--- build/groovy/org/jetbrains/intellij/build/AndroidStudioLibraryLicenses.groovy
+++ build/groovy/org/jetbrains/intellij/build/AndroidStudioLibraryLicenses.groovy
@@ -132,6 +132,7 @@ class AndroidStudioLibraryLicenses {
     new LibraryLicense(name: "TightVNC", libraryName: "tightvnc", license: "Commercial License"),
     new LibraryLicense(name: "Transport Pipeline", libraryName: "transport-proto", license: "Apache 2.0",
                        licenseUrl: "http://www.apache.org/licenses/LICENSE-2.0"),
+    new LibraryLicense(name: "Xtext", libraryName: "org.eclipse.xtext.xbase", license: "Eclipse Public License 1.0"),
     new LibraryLicense(name: "Instant App Proto Manifest", libraryName: "aia-proto",
                        license: "Apache 2.0", url: "http://source.android.com/"),
     new LibraryLicense(name: "Archive Patcher",
