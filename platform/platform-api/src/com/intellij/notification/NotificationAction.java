334e8268bc932611c4205d63d89e194e46c15d2b
Dmitry Jemerov <yole@jetbrains.com>
2019-10-08 18:30:50 +0200

    Easier API for creating expiring notifications
    Partial cherry-pick of 2bd83563c9b8910e78851d0443336d7255e874dd
    
    GitOrigin-RevId: 56c90f32b82c7751b92e85b16526c0f2948d78e2

--- platform/platform-api/src/com/intellij/notification/NotificationAction.java
+++ platform/platform-api/src/com/intellij/notification/NotificationAction.java
@@ -1,9 +1,10 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.notification;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.DumbAwareAction;
+import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -39,5 +40,14 @@ public abstract class NotificationAction extends DumbAwareAction {
   @NotNull
   public static NotificationAction createSimple(@NotNull String text, @NotNull Runnable performAction) {
     return create(text, (event, notification) -> performAction.run());
+  }
+
+  @NotNull
+  public static NotificationAction createSimpleExpiring(@NotNull @Nls(capitalization = Nls.Capitalization.Title) String text,
+                                                @NotNull Runnable performAction) {
+    return create(text, (event, notification) -> {
+      performAction.run();
+      notification.expire();
+    });
   }
 }
