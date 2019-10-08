9f1363b53c1bedc778f7deeaae8b801ff69639b5
Dmitry Jemerov <yole@jetbrains.com>
2019-10-08 18:48:28 +0200

    Assorted fixes and improvements to plugins advertiser notifications
    GitOrigin-RevId: d0051bf4ba22e646631e0056f35d24bfa8e60b50

--- platform/core-api/src/com/intellij/openapi/application/IdeUrlTrackingParametersProvider.java
+++ platform/core-api/src/com/intellij/openapi/application/IdeUrlTrackingParametersProvider.java
@@ -1,6 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.application;
 
 import com.intellij.openapi.components.ServiceManager;
@@ -22,6 +20,14 @@ public class IdeUrlTrackingParametersProvider {
    */
   @NotNull
   public String augmentUrl(@NotNull String originalUrl) {
+    return originalUrl;
+  }
+
+  /**
+   * @return {@code originalUrl} with appended parameters
+   */
+  @NotNull
+  public String augmentUrl(@NotNull String originalUrl, String campaignId) {
     return originalUrl;
   }
 }
