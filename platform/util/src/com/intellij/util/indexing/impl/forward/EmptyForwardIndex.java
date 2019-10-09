f8151e8af1bb9db7ac35e76ab01b02a3c5d14f7a
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2019-10-09 08:45:25 +0300

    don't use forward index for single entry indexes
    GitOrigin-RevId: 6dc4afce26f0a7dde9a6183a797f26d1dc45e0a1

--- platform/util/src/com/intellij/util/indexing/impl/forward/EmptyForwardIndex.java
+++ platform/util/src/com/intellij/util/indexing/impl/forward/EmptyForwardIndex.java
@@ -1,12 +1,12 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.psi.stubs;
+package com.intellij.util.indexing.impl.forward;
 
 import com.intellij.openapi.util.io.ByteArraySequence;
 import com.intellij.util.indexing.impl.forward.ForwardIndex;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-class EmptyForwardIndex implements ForwardIndex {
+public class EmptyForwardIndex implements ForwardIndex {
   @Nullable
   @Override
   public ByteArraySequence get(@NotNull Integer key) {
