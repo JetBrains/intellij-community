d3618f7276616a412220e5123c77675b48822186
Artemiy Sartakov <Artemiy.Sartakov@jetbrains.com>
2019-10-09 11:21:41 +0700

    JavaEnterInTextBlockHandler: separate enter handler for text blocks (IDEA-223911)
    1. auto-formatting in blank text block
    2. proper caret offset in text block with content
    2. semicolon after block in simple cases
    3. disable insert of closing quotes in case when parser appended next text block open quotes to the current text block
    
    GitOrigin-RevId: cb2bdf293dfc3a18d1aab055d379b9e92f0cff2e

---
+++ java/java-impl/src/com/intellij/codeInsight/editorActions/JavaEnterInTextBlockHandler.java
@@ -0,0 +1,91 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.codeInsight.editorActions;
+
+import com.intellij.codeInsight.editorActions.enter.EnterInStringLiteralHandler;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.LogicalPosition;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.*;
+import com.intellij.psi.codeStyle.CodeStyleManager;
+import com.intellij.psi.impl.source.tree.java.PsiLiteralExpressionImpl;
+import com.intellij.util.ObjectUtils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class JavaEnterInTextBlockHandler extends EnterInStringLiteralHandler {
+
+  @Override
+  public Result postProcessEnter(@NotNull PsiFile file, @NotNull Editor editor, @NotNull DataContext dataContext) {
+    PsiDocumentManager.getInstance(file.getProject()).commitDocument(editor.getDocument());
+    PsiLiteralExpressionImpl textBlock = getTextBlock(file, editor.getCaretModel().getOffset());
+    if (textBlock == null) return super.postProcessEnter(file, editor, dataContext);
+    int offset = editor.getCaretModel().getOffset();
+    Document document = editor.getDocument();
+    if (isContentAtTheEnd(textBlock)) {
+      CodeStyleManager.getInstance(textBlock.getProject()).reformat(textBlock);
+      offset = editor.getCaretModel().getOffset();
+    }
+    else if (!isAtBlockStart(document, textBlock, offset)) {
+      return super.postProcessEnter(file, editor, dataContext);
+    }
+    editor.getCaretModel().moveToOffset(findOffset(editor, document, textBlock, offset));
+    return Result.Continue;
+  }
+
+  private static boolean isContentAtTheEnd(@NotNull PsiLiteralExpressionImpl textBlock) {
+    String text = textBlock.getTextBlockText();
+    if (text == null) return false;
+    boolean foundContent = false;
+    for (int i = 0; i < text.length(); i++) {
+      if (!isWhitespaceOrNewLine(text.charAt(i))) {
+        foundContent = true;
+      }
+      else if (foundContent) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private static boolean isAtBlockStart(@NotNull Document document, @NotNull PsiLiteralExpressionImpl textBlock, int offset) {
+    String text = document.getText(new TextRange(textBlock.getTextOffset(), offset));
+    if (!text.startsWith("\"\"\"")) return false;
+    text = text.substring(3);
+    return text.chars().allMatch(JavaEnterInTextBlockHandler::isWhitespaceOrNewLine);
+  }
+
+  private static boolean isWhitespaceOrNewLine(int c) {
+    return Character.isWhitespace(c) || c == '\n';
+  }
+
+  private static int findOffset(@NotNull Editor editor,
+                                @NotNull Document document,
+                                @NotNull PsiLiteralExpressionImpl textBlock,
+                                int offset) {
+    TextRange afterOffset = new TextRange(offset, textBlock.getTextRange().getEndOffset());
+    String text = document.getText(afterOffset);
+    int line = document.getLineNumber(offset);
+    int lineStart = document.getLineStartOffset(line);
+    int start = text.indexOf('\n') + 1;
+    for (int i = start; i < text.length(); i++) {
+      char c = text.charAt(i);
+      if (isWhitespaceOrNewLine(c)) continue;
+      int column = editor.offsetToLogicalPosition(offset + i).column;
+      String indent = StringUtil.repeatSymbol(' ', column);
+      document.replaceString(lineStart, offset, indent);
+      return editor.logicalPositionToOffset(new LogicalPosition(line, column));
+    }
+    return offset;
+  }
+
+  @Nullable
+  private static PsiLiteralExpressionImpl getTextBlock(@NotNull PsiFile file, int offset) {
+    if (!(file instanceof PsiJavaFile)) return null;
+    PsiJavaToken token = ObjectUtils.tryCast(file.findElementAt(offset), PsiJavaToken.class);
+    if (token == null || token.getTokenType() != JavaTokenType.TEXT_BLOCK_LITERAL) return null;
+    return ObjectUtils.tryCast(token.getParent(), PsiLiteralExpressionImpl.class);
+  }
+}
--- java/java-impl/src/com/intellij/codeInsight/editorActions/JavaQuoteHandler.java
+++ java/java-impl/src/com/intellij/codeInsight/editorActions/JavaQuoteHandler.java
@@ -7,8 +7,10 @@ import com.intellij.openapi.editor.highlighter.HighlighterIterator;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.source.tree.ElementType;
+import com.intellij.psi.impl.source.tree.java.PsiLiteralExpressionImpl;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.tree.TokenSet;
+import com.intellij.util.ObjectUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -99,22 +101,37 @@ public class JavaQuoteHandler extends SimpleTokenSetQuoteHandler implements Java
   public boolean hasNonClosedLiteral(Editor editor, HighlighterIterator iterator, int offset) {
     if (iterator.getTokenType() == JavaTokenType.TEXT_BLOCK_LITERAL) {
       Document document = iterator.getDocument();
-      if (document != null && StringUtil.equals(document.getCharsSequence().subSequence(iterator.getStart(), offset + 1), "\"\"\"")) {
-        return true;
+      if (document != null) {
+        String text = document.getText();
+        boolean hasOpenQuotes = StringUtil.equals(text.substring(iterator.getStart(), offset + 1), "\"\"\"");
+        if (hasOpenQuotes) {
+          boolean hasCloseQuotes = StringUtil.contains(text.substring(offset + 1, iterator.getEnd()), "\"\"\"");
+          if (!hasCloseQuotes) return true;
+          // check if parser interpreted next text block start quotes as end quotes for the current one
+          int nTextBlockQuotes = StringUtil.getOccurrenceCount(text.substring(iterator.getEnd()), "\"\"\"");
+          return nTextBlockQuotes % 2 != 0;
+        }
       }
     }
     return super.hasNonClosedLiteral(editor, iterator, offset);
   }
 
   @Override
-  public void insertClosingQuote(@NotNull Editor editor, int offset, @NotNull CharSequence closingQuote) {
-    if (closingQuote.charAt(0) == '`') {
-      editor.getDocument().insertString(offset, " " + closingQuote);
-      editor.getSelectionModel().setSelection(offset, offset + 1);
-    }
-    else {
-      editor.getDocument().insertString(offset, "\n" + closingQuote);
-      editor.getCaretModel().moveToOffset(offset + 1);
-    }
+  public boolean needSemicolonAfter(@NotNull PsiElement element) {
+    PsiLiteralExpressionImpl literal = ObjectUtils.tryCast(element.getParent(), PsiLiteralExpressionImpl.class);
+    if (literal == null || literal.getLiteralElementType() != JavaTokenType.TEXT_BLOCK_LITERAL) return false;
+    PsiJavaToken token = ObjectUtils.tryCast(literal.getNextSibling(), PsiJavaToken.class);
+    if (token != null && token.getTokenType() == JavaTokenType.SEMICOLON) return false;
+    String text = literal.getText();
+    if (text.indexOf('\n') != -1) return false;
+    PsiVariable variable = ObjectUtils.tryCast(literal.getParent(), PsiVariable.class);
+    if (variable != null && variable.getInitializer() == literal) return true;
+    PsiAssignmentExpression assignment = ObjectUtils.tryCast(literal.getParent(), PsiAssignmentExpression.class);
+    return assignment != null && assignment.getRExpression() == literal;
+  }
+
+  @Override
+  public void insertClosingQuote(@NotNull Editor editor, int offset, @NotNull PsiFile file, @NotNull CharSequence closingQuote) {
+    editor.getDocument().insertString(offset, "\"\"\"");
   }
 }
