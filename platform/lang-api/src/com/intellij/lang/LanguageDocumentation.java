27dc9e2e29421230bc2ece601b0b9bcf88a3ad8f
Dmitry Jemerov <yole@jetbrains.com>
2019-10-09 18:20:57 +0200

    Reimplement CompositeDocumentationProvider building so that it can be cached
    GitOrigin-RevId: 424ed3d9dd2e37601eb808304b2c91db795e9f94

--- platform/lang-api/src/com/intellij/lang/LanguageDocumentation.java
+++ platform/lang-api/src/com/intellij/lang/LanguageDocumentation.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 /*
  * @author max
@@ -32,11 +18,19 @@ public class LanguageDocumentation extends LanguageExtension<DocumentationProvid
     super("com.intellij.lang.documentationProvider");
   }
 
+  /**
+   * This method is left to preserve binary compatibility.
+   */
   @Override
   public DocumentationProvider forLanguage(@NotNull final Language l) {
-    final List<DocumentationProvider> providers = allForLanguage(l);
+    return super.forLanguage(l);
+  }
+
+  @Override
+  protected DocumentationProvider findForLanguage(@NotNull Language language) {
+    final List<DocumentationProvider> providers = allForLanguage(language);
     if (providers.size() < 2) {
-      return super.forLanguage(l);
+      return super.findForLanguage(language);
     }
     return CompositeDocumentationProvider.wrapProviders(providers);
   }
