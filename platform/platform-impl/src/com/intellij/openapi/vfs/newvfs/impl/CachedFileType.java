7af6f92bc4ad1c21c866244891d3b30994a4a26b
peter <peter@jetbrains.com>
2019-10-11 12:53:59 +0200

    file type cache: a shorter and more correct way to listen to project PSI modifications
    GitOrigin-RevId: a5dd06a8276d818c74f8fc3f2eb07a497e3a9c0b

--- platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/impl/CachedFileType.java
+++ platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/impl/CachedFileType.java
@@ -1,12 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vfs.newvfs.impl;
 
-import com.intellij.openapi.application.Application;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.fileTypes.FileType;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManager;
-import com.intellij.openapi.project.ProjectManagerListener;
 import com.intellij.openapi.vfs.newvfs.persistent.PersistentFS;
 import com.intellij.psi.util.PsiModificationTracker;
 import com.intellij.util.containers.ContainerUtil;
@@ -15,7 +10,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.ConcurrentMap;
 
-class CachedFileType {
+public class CachedFileType {
   private static final ConcurrentMap<FileType, CachedFileType> ourInterner = ContainerUtil.newConcurrentMap();
 
   private final long modCount = vfsModCount();
@@ -45,16 +40,6 @@ class CachedFileType {
     }
   }
 
-  static {
-    Application app = ApplicationManager.getApplication();
-    app.getMessageBus().connect().subscribe(ProjectManager.TOPIC, new ProjectManagerListener() {
-      @Override
-      public void projectOpened(@NotNull Project project) {
-        project.getMessageBus().connect().subscribe(PsiModificationTracker.TOPIC, CachedFileType::clearCache);
-      }
-    });
-  }
-
   private static void clearCache() {
     synchronized (ourInterner) {
       for (CachedFileType value : ourInterner.values()) {
@@ -62,6 +47,13 @@ class CachedFileType {
         value.fileType = null;
       }
       ourInterner.clear();
+    }
+  }
+
+  public static class PsiListener implements PsiModificationTracker.Listener {
+    @Override
+    public void modificationCountChanged() {
+      clearCache();
     }
   }
 }
