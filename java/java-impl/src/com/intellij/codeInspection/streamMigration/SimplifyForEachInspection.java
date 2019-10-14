93c83aec85e6f7d2a68445352a2adaf778ee14dc
Bas Leijdekkers <basleijdekkers@gmail.com>
2019-10-14 15:12:28 +0200

    indicate method calls with '()' in inspection names
    GitOrigin-RevId: f92b0d4bae7978663cfde0560dea122a1a096724

--- java/java-impl/src/com/intellij/codeInspection/streamMigration/SimplifyForEachInspection.java
+++ java/java-impl/src/com/intellij/codeInspection/streamMigration/SimplifyForEachInspection.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInspection.streamMigration;
 
 import com.intellij.codeInsight.daemon.GroupNames;
@@ -46,7 +46,7 @@ public class SimplifyForEachInspection extends AbstractBaseJavaLocalInspectionTo
   @NotNull
   @Override
   public String getDisplayName() {
-    return "forEach call can be simplified";
+    return InspectionsBundle.message("inspection.simplify.foreach.display.name");
   }
 
   @NotNull
