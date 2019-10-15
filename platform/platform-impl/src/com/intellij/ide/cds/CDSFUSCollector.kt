9c9ec3bac391acb9cf15d822d1ab139da265457a
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2019-10-15 13:59:31 +0200

    cds+ij - add FUS reporting
    GitOrigin-RevId: 2a52f65bad48753d71840056a8f9367ecd995ba5

---
+++ platform/platform-impl/src/com/intellij/ide/cds/CDSFUSCollector.kt
@@ -0,0 +1,13 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.ide.cds
+
+import com.intellij.internal.statistic.beans.newBooleanMetric
+import com.intellij.internal.statistic.service.fus.collectors.ApplicationUsagesCollector
+
+class CDSApplicationUsagesCollector : ApplicationUsagesCollector() {
+  override fun getGroupId() = "intellij.cds"
+
+  override fun getMetrics() = setOf(
+    newBooleanMetric(groupId, CDSManager.isRunningWithCDS)
+  )
+}
