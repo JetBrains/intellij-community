255c10e26fb8a9ac261baba512f6c3c4aaee71da
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-10-16 13:49:34 +0300

    IDEA-CR-53603: [groovy] resolve safe cast references using new algorithm
    (cherry picked from commit 9ec9013288e4c1a58665d43246e322ff2ee54ee3)
    
    GitOrigin-RevId: 16eabdb6bf96ef85b7bf4a4f1ef8d8d4ee4048db

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/resolve/references/GrSafeCastReference.kt
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/resolve/references/GrSafeCastReference.kt
@@ -1,14 +1,16 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.lang.resolve.references
 
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiType
+import org.jetbrains.plugins.groovy.lang.psi.api.GroovyResolveResult
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrSafeCastExpression
 import org.jetbrains.plugins.groovy.lang.psi.impl.statements.expressions.HardcodedGroovyMethodConstants.AS_TYPE
 import org.jetbrains.plugins.groovy.lang.psi.impl.statements.expressions.TypesUtil.createJavaLangClassType
 import org.jetbrains.plugins.groovy.lang.resolve.api.Arguments
 import org.jetbrains.plugins.groovy.lang.resolve.api.GroovyMethodCallReferenceBase
 import org.jetbrains.plugins.groovy.lang.resolve.api.LazyTypeArgument
+import org.jetbrains.plugins.groovy.lang.resolve.impl.resolveImpl2
 
 class GrSafeCastReference(element: GrSafeCastExpression) : GroovyMethodCallReferenceBase<GrSafeCastExpression>(element) {
 
@@ -19,4 +21,6 @@ class GrSafeCastReference(element: GrSafeCastExpression) : GroovyMethodCallRefer
   override val methodName: String get() = AS_TYPE
 
   override val arguments: Arguments? get() = listOf(LazyTypeArgument { createJavaLangClassType(element.castTypeElement?.type, element) })
+
+  override fun doResolve(incomplete: Boolean): Collection<GroovyResolveResult> = resolveImpl2(incomplete)
 }
