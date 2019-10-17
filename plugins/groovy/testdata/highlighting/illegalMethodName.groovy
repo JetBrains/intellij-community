aeee56a75d9b7515bcf7bd2b025db037c95d942c
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-10-17 19:46:45 +0300

    IDEA-CR-53720: [groovy] highlight illegal method identifiers (IDEA-192769)
    (cherry picked from commit 61bdac543a7f0aeb011129c783021f253126e72b)
    
    GitOrigin-RevId: 9f1b60c49a802facb37799e3d4a7da76cf766070

---
+++ plugins/groovy/testdata/highlighting/illegalMethodName.groovy
@@ -0,0 +1,19 @@
+class CC {
+  def <warning>'.'</warning>() {}
+  def <warning>';'</warning>() {}
+  def <warning>'['</warning>() {}
+  def <warning>'/'</warning>() {}
+  def <warning>'<'</warning>() {}
+  def <warning>'>'</warning>() {}
+  def <warning>':'</warning>() {}
+}
+
+class CU {
+  def <warning>'\u002e'</warning>() {}
+  def <warning>'\u003b'</warning>() {}
+  def <warning>'\u005b'</warning>() {}
+  def <warning>'\u002f'</warning>() {}
+  def <warning>'\u003c'</warning>() {}
+  def <warning>'\u003e'</warning>() {}
+  def <warning>'\u003a'</warning>() {}
+}
