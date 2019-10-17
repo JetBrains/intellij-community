69547047a6a6ae6e48c56bee3607415a3503bcf7
Andrey Vokin <andrey.vokin@jetbrains.com>
2019-10-17 16:36:06 +0200

    [Cucumber Java] prepare adapters for Cucumber SM message producer
    So that common part of CucumberJvmFormatter do not depend on cucumber library version interface CucumberJvmAdapter was introduced
    
    GitOrigin-RevId: 33d4ed4034c1795c1ecdcef9ceb050c1737ae349

---
+++ plugins/cucumber-jvm-formatter/src/org/jetbrains/plugins/cucumber/java/run/CucumberJvm2Adapter.java
@@ -0,0 +1,199 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.plugins.cucumber.java.run;
+
+import cucumber.api.TestCase;
+import cucumber.api.TestStep;
+import cucumber.api.event.*;
+import gherkin.events.PickleEvent;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import static org.jetbrains.plugins.cucumber.java.run.CucumberJvmSMFormatterUtil.FILE_RESOURCE_PREFIX;
+
+public class CucumberJvm2Adapter {
+  public static class IdeaTestCaseEvent implements CucumberJvmAdapter.IdeaTestCaseEvent {
+    private final IdeaTestCase myTestCase;
+
+    public IdeaTestCaseEvent(TestCaseStarted testCaseStarted) {
+      myTestCase = new IdeaTestCase(testCaseStarted.testCase);
+    }
+
+    public IdeaTestCaseEvent(TestCaseFinished testCaseFinished) {
+      myTestCase = new IdeaTestCase(testCaseFinished.testCase);
+    }
+
+
+    @Override
+    public CucumberJvmAdapter.IdeaTestCase getTestCase() {
+      return myTestCase;
+    }
+
+    @Override
+    public String getUri() {
+      return myTestCase.getUri();
+    }
+  }
+
+  public static class IdeaTestStepEvent implements CucumberJvmAdapter.IdeaTestStepEvent {
+    private final IdeaTestStep myTestStep;
+
+    public IdeaTestStepEvent(TestStepStarted testStepStarted) {
+      this(testStepStarted.testStep);
+    }
+
+    public IdeaTestStepEvent(TestStep testStep) {
+      myTestStep = new IdeaTestStep(testStep);
+    }
+
+    @Override
+    public CucumberJvmAdapter.IdeaTestStep getTestStep() {
+      return myTestStep;
+    }
+  }
+
+  public static class IdeaTestStepFinishedEvent extends IdeaTestStepEvent implements CucumberJvmAdapter.IdeaTestStepFinishedEvent {
+    TestStepFinished myRealEvent;
+
+    public IdeaTestStepFinishedEvent(TestStepFinished testStepFinished) {
+      super(testStepFinished.testStep);
+      myRealEvent = testStepFinished;
+    }
+
+    @Override
+    public Status getResult() {
+      switch (myRealEvent.result.getStatus()) {
+        case PASSED: return Status.PASSED;
+        case PENDING: return Status.PENDING;
+        case SKIPPED: return Status.SKIPPED;
+        default: return Status.FAILED;
+      }
+    }
+
+    @Override
+    public Long getDuration() {
+      return myRealEvent.result.getDuration() != null ? myRealEvent.result.getDuration() / 1000000: 0;
+    }
+
+    @Override
+    public String getErrorMessage() {
+      return myRealEvent.result.getErrorMessage();
+    }
+  }
+
+  public static class IdeaTestSourceReadEvent implements CucumberJvmAdapter.IdeaTestSourceReadEvent {
+    private final String myUri;
+
+    private final String mySource;
+
+    public IdeaTestSourceReadEvent(TestSourceRead testSourceRead) {
+      myUri = testSourceRead.uri;
+      mySource = testSourceRead.source;
+    }
+
+    @Override
+    public String getUri() {
+      return myUri;
+    }
+
+    @Override
+    public String getSource() {
+      return mySource;
+    }
+  }
+
+  public static class IdeaTestCase implements CucumberJvmAdapter.IdeaTestCase {
+    private final TestCase myRealTestCase;
+
+    IdeaTestCase(TestCase realTestCase) {
+      myRealTestCase = realTestCase;
+    }
+
+    @Override
+    public boolean isScenarioOutline() {
+      PickleEvent pickleEvent = getPickleEvent(myRealTestCase);
+      return pickleEvent != null && pickleEvent.pickle.getLocations().size() > 1;
+    }
+
+    @Override
+    public String getUri() {
+      return myRealTestCase.getUri();
+    }
+
+    @Override
+    public int getScenarioOutlineLine() {
+      PickleEvent pickleEvent = getPickleEvent(myRealTestCase);
+      if (pickleEvent != null) {
+        return pickleEvent.pickle.getLocations().get(pickleEvent.pickle.getLocations().size() - 1).getLine();
+      }
+      return 0;
+    }
+
+    @Override
+    public int getLine() {
+      return myRealTestCase.getLine();
+    }
+
+    @Override
+    public String getScenarioName() {
+      return myRealTestCase.getName();
+    }
+  }
+
+  public static class IdeaTestStep implements CucumberJvmAdapter.IdeaTestStep {
+    private final TestStep myRealStep;
+
+    public IdeaTestStep(TestStep realStep) {
+      myRealStep = realStep;
+    }
+
+    @Override
+    public String getLocation() {
+      if (myRealStep.isHook()) {
+        try {
+          Field definitionMatchField = myRealStep.getClass().getSuperclass().getDeclaredField("definitionMatch");
+          definitionMatchField.setAccessible(true);
+          Object definitionMatchFieldValue = definitionMatchField.get(myRealStep);
+
+          Field hookDefinitionField = definitionMatchFieldValue.getClass().getDeclaredField("hookDefinition");
+          hookDefinitionField.setAccessible(true);
+          Object hookDefinitionFieldValue = hookDefinitionField.get(definitionMatchFieldValue);
+
+          Field methodField = hookDefinitionFieldValue.getClass().getDeclaredField("method");
+          methodField.setAccessible(true);
+          Object methodFieldValue = methodField.get(hookDefinitionFieldValue);
+          if (methodFieldValue instanceof Method) {
+            Method method = (Method)methodFieldValue;
+            return String.format("java:test://%s/%s", method.getDeclaringClass().getName(), method.getName());
+          }
+        }
+        catch (Exception ignored) {
+        }
+        return "";
+      }
+      return FILE_RESOURCE_PREFIX + myRealStep.getStepLocation() + ":" + myRealStep.getStepLine();
+    }
+
+    @Override
+    public String getStepName() {
+      String stepName;
+      if (myRealStep.isHook()) {
+        stepName = "Hook: " + myRealStep.getHookType().toString();
+      } else {
+        stepName = myRealStep.getStepText();
+      }
+      return stepName;
+    }
+  }
+
+  private static PickleEvent getPickleEvent(TestCase testCase) {
+    try {
+      Field pickleEventField = testCase.getClass().getDeclaredField("pickleEvent");
+      pickleEventField.setAccessible(true);
+      return (PickleEvent)pickleEventField.get(testCase);
+    }
+    catch (Exception ignored) {
+    }
+    return null;
+  }
+}
