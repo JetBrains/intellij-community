04eafd206b87094466cb6f936f8836bf15b565ec
Mikhail Mazurkevich <mikhail.mazurkevich@jetbrains.com>
2019-10-17 11:29:05 +0300

    [jps-cache] Remove redundant key descriptor form timestamp storage
    GitOrigin-RevId: 2236e7954c3b53e21183aea76f73b4ca12739372

--- jps/jps-builders/src/org/jetbrains/jps/incremental/storage/FileTimestampStorage.java
+++ jps/jps-builders/src/org/jetbrains/jps/incremental/storage/FileTimestampStorage.java
@@ -24,8 +24,7 @@ public class FileTimestampStorage extends AbstractStateStorage<File, TimestampPe
   private final File myTimestampsRoot;
 
   public FileTimestampStorage(File dataStorageRoot, BuildTargetsState targetsState) throws IOException {
-    super(new File(calcStorageRoot(dataStorageRoot), "data"), ProjectStamps.PORTABLE_CACHES ? new PortableFileKeyDescriptor() : new FileKeyDescriptor(),
-          new StateExternalizer());
+    super(new File(calcStorageRoot(dataStorageRoot), "data"), new FileKeyDescriptor(), new StateExternalizer());
     myTimestampsRoot = calcStorageRoot(dataStorageRoot);
     myTargetsState = targetsState;
   }
