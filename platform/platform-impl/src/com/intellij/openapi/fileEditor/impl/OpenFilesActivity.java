739f90998a2b18f0fc7bc69e402d6cebdc17c18e
Konstantin Bulenkov <kb@jetbrains.com>
2019-10-17 19:42:41 +0200

    Open readme file if there are no files opened during startup
    (cherry picked from commit f916699dca3aeee9be5a4e695838d4dee69ac29e)
    
    GitOrigin-RevId: d1c2e4d7b5fa50a650f7ae90fd122353fa329e0c

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/OpenFilesActivity.java
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/OpenFilesActivity.java
@@ -1,11 +1,14 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.fileEditor.impl;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectUtil;
 import com.intellij.openapi.startup.StartupActivity;
+import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 
 final class OpenFilesActivity implements StartupActivity.DumbAware {
@@ -24,5 +27,19 @@ final class OpenFilesActivity implements StartupActivity.DumbAware {
     FileEditorManagerImpl manager = (FileEditorManagerImpl)fileEditorManager;
     manager.getMainSplitters().openFiles();
     manager.initDockableContentFactory();
+
+    if (manager.getOpenFiles().length == 0) {
+      findAndOpenReadme(project, manager);
+    }
+  }
+
+  private static void findAndOpenReadme(@NotNull Project project, FileEditorManagerImpl manager) {
+    VirtualFile dir = ProjectUtil.guessProjectDir(project);
+    if (dir != null) {
+      VirtualFile readme = dir.findChild("README.md");
+      if (readme != null && !readme.isDirectory()) {
+        ApplicationManager.getApplication().invokeLater(() -> manager.openFile(readme, true));
+      }
+    }
   }
 }
