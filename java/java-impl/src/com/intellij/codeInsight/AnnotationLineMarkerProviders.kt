6352ba42968f3f254b5068eaf5d6763fc13248f5
Dmitry Jemerov <yole@jetbrains.com>
2019-10-18 13:48:57 +0200

    Separate option for showing marker with inferred contract annotations
    GitOrigin-RevId: cc2a776e5d0ab00ec238e769666d0d64fff98810

--- java/java-impl/src/com/intellij/codeInsight/AnnotationLineMarkerProviders.kt
+++ java/java-impl/src/com/intellij/codeInsight/AnnotationLineMarkerProviders.kt
@@ -1,8 +1,13 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight
 
+import com.intellij.codeInsight.javadoc.AnnotationDocGenerator
 import com.intellij.codeInsight.javadoc.NonCodeAnnotationGenerator
 import com.intellij.psi.PsiModifierListOwner
+import org.jetbrains.annotations.Contract
+
+private val AnnotationDocGenerator.isContract: Boolean
+  get() = targetClass?.qualifiedName == Contract::class.java.name
 
 class ExternalAnnotationLineMarkerProvider : NonCodeAnnotationsLineMarkerProvider() {
   override fun hasAnnotationsToShow(owner: PsiModifierListOwner): Boolean {
@@ -12,13 +17,25 @@ class ExternalAnnotationLineMarkerProvider : NonCodeAnnotationsLineMarkerProvide
   override fun getName() = "External annotations"
 }
 
-class InferredAnnotationsLineMarkerProvider : NonCodeAnnotationsLineMarkerProvider() {
+class InferredNullabilityAnnotationsLineMarkerProvider : NonCodeAnnotationsLineMarkerProvider() {
   override fun hasAnnotationsToShow(owner: PsiModifierListOwner): Boolean {
     val values = NonCodeAnnotationGenerator.getSignatureNonCodeAnnotations(owner).values()
     // Don't show two markers
-    return values.any { it.isInferred } && values.none { !it.isInferred }
+    return values.any { it.isInferred && !it.isContract } && values.none { !it.isInferred }
   }
 
+  override fun getName() = "Inferred nullability annotations"
+}
 
-  override fun getName() = "Inferred annotations"
+class InferredContractAnnotationsLineMarkerProvider : NonCodeAnnotationsLineMarkerProvider() {
+  override fun hasAnnotationsToShow(owner: PsiModifierListOwner): Boolean {
+    val values = NonCodeAnnotationGenerator.getSignatureNonCodeAnnotations(owner).values()
+    return values.any { it.isInferred && it.isContract } &&
+           values.none { it.isInferred && !it.isContract } /* don't show when there's a marker for inferred nullability */ &&
+           values.none { !it.isInferred } // don't show when there's a marker for external
+  }
+
+  override fun isEnabledByDefault() = false
+
+  override fun getName() = "Inferred contract annotations"
 }
