117d7fae4f962d46bdb47f078a2c7eaa6ecb1aaf
peter <peter@jetbrains.com>
2019-10-21 15:21:01 +0200

    add more instructions to UnivocityTest
    GitOrigin-RevId: 3a98c42c23d42dd7dbf810bb43736d8454744d36

--- java/java-tests/testSrc/com/intellij/java/propertyBased/UnivocityTest.java
+++ java/java-tests/testSrc/com/intellij/java/propertyBased/UnivocityTest.java
@@ -125,7 +125,10 @@ public class UnivocityTest extends AbstractApplyAndRevertTestCase {
   protected String getTestDataPath() {
     File file = new File(PathManager.getHomePath(), "univocity-parsers");
     if (!file.exists()) {
-      fail("Cannot find univocity project, execute this in project home: git clone https://github.com/JetBrains/univocity-parsers.git");
+      fail("Cannot find univocity project:\n" +
+           "  execute this in project home: git clone https://github.com/JetBrains/univocity-parsers.git\n" +
+           "  open the just cloned univocity-parsers project in IntelliJ IDEA, let it download all the libraries, close the IDE\n" +
+           "  execute this in univocity-parsers directory: git reset HEAD --hard");
     }
     return file.getAbsolutePath();
   }
