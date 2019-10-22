d9ee2ad11ea1c3f0f8bbe920ff4b0ef2b2bc0fd3
Dmitry Jemerov <yole@jetbrains.com>
2019-10-22 17:59:45 +0200

    "Analyze unloadable plugins" also reports owners of extension points
    GitOrigin-RevId: a4e2902915348d6ee0c5bfeb8ccae4b964af7055

--- plugins/devkit/devkit-core/src/internal/AnalyzeUnloadablePluginsAction.kt
+++ plugins/devkit/devkit-core/src/internal/AnalyzeUnloadablePluginsAction.kt
@@ -5,14 +5,20 @@ import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.runReadAction
+import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.openapi.vcs.ProjectLevelVcsManager
+import com.intellij.openapi.vcs.annotate.FileAnnotation
+import com.intellij.openapi.vcs.annotate.LineAnnotationAspect
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.search.FilenameIndex
 import com.intellij.psi.search.GlobalSearchScopesCore
 import com.intellij.psi.xml.XmlFile
 import com.intellij.ui.components.dialog
 import com.intellij.ui.layout.*
+import org.jetbrains.idea.devkit.dom.ExtensionPoint
 import org.jetbrains.idea.devkit.dom.IdeaPlugin
 import org.jetbrains.idea.devkit.util.DescriptorUtil
 import org.jetbrains.jps.model.java.JavaModuleSourceRootTypes
@@ -27,6 +33,7 @@ class AnalyzeUnloadablePluginsAction : AnAction() {
     val project = e.project ?: return
 
     val result = mutableListOf<PluginUnloadabilityStatus>()
+    val extensionPointOwners = ExtensionPointOwners()
     val show = ProgressManager.getInstance().runProcessWithProgressSynchronously(
       {
         runReadAction {
@@ -46,17 +53,18 @@ class AnalyzeUnloadablePluginsAction : AnAction() {
             }
 
             val ideaPlugin = DescriptorUtil.getIdeaPlugin(pluginXmlFile) ?: continue
-            val status = analyzeUnloadable(ideaPlugin)
+            val status = analyzeUnloadable(ideaPlugin, extensionPointOwners)
             result.add(status)
             pi.text = status.pluginId
           }
         }
       }, "Analyzing Plugins", true, e.project)
 
-    if (show) showReport(project, result)
+    if (show) showReport(project, result, extensionPointOwners)
+    extensionPointOwners.dispose()
   }
 
-  private fun showReport(project: Project, result: List<PluginUnloadabilityStatus>) {
+  private fun showReport(project: Project, result: List<PluginUnloadabilityStatus>, extensionPointOwners: ExtensionPointOwners) {
     val report = buildString {
       if (result.any { it.analysisErrors.isNotEmpty() }) {
         appendln("Analysis errors:")
@@ -106,7 +114,18 @@ class AnalyzeUnloadablePluginsAction : AnAction() {
       appendln("EP usage statistics (${epUsagesMap.size} non-dynamic EPs remaining):")
       val epUsagesList = epUsagesMap.toList().sortedByDescending { it.second }
       for (pair in epUsagesList) {
-        appendln("${pair.second}: ${pair.first}")
+        appendln("${pair.second}: ${pair.first} (${extensionPointOwners.getOwner(pair.first)})")
+      }
+
+      appendln()
+      appendln("EPs grouped by owner:")
+      for (owner in extensionPointOwners.getSortedOwners()) {
+        val owned = extensionPointOwners.getOwnedEPs(owner)
+        appendln("$owner: ${owned.size}")
+        for (ep in owned) {
+          appendln(ep)
+        }
+        appendln()
       }
     }
 
@@ -117,16 +136,16 @@ class AnalyzeUnloadablePluginsAction : AnAction() {
     }).show()
   }
 
-  private fun analyzeUnloadable(ideaPlugin: IdeaPlugin): PluginUnloadabilityStatus {
+  private fun analyzeUnloadable(ideaPlugin: IdeaPlugin, extensionPointOwners: ExtensionPointOwners): PluginUnloadabilityStatus {
     val unspecifiedDynamicEPs = mutableSetOf<String>()
     val nonDynamicEPs = mutableSetOf<String>()
     val analysisErrors = mutableListOf<String>()
-    var componentCount = analyzePluginFile(ideaPlugin, analysisErrors, nonDynamicEPs, unspecifiedDynamicEPs)
+    var componentCount = analyzePluginFile(ideaPlugin, analysisErrors, nonDynamicEPs, unspecifiedDynamicEPs, extensionPointOwners)
 
     for (dependency in ideaPlugin.dependencies) {
       val depXmlFile = DescriptorUtil.resolveDependencyToXmlFile(dependency) ?: continue
       val depIdeaPlugin = DescriptorUtil.getIdeaPlugin(depXmlFile) ?: continue
-      componentCount += analyzePluginFile(depIdeaPlugin, analysisErrors, nonDynamicEPs, unspecifiedDynamicEPs)
+      componentCount += analyzePluginFile(depIdeaPlugin, analysisErrors, nonDynamicEPs, unspecifiedDynamicEPs, extensionPointOwners)
     }
 
     return PluginUnloadabilityStatus(
@@ -138,13 +157,16 @@ class AnalyzeUnloadablePluginsAction : AnAction() {
   private fun analyzePluginFile(ideaPlugin: IdeaPlugin,
                                 analysisErrors: MutableList<String>,
                                 nonDynamicEPs: MutableSet<String>,
-                                unspecifiedDynamicEPs: MutableSet<String>): Int {
+                                unspecifiedDynamicEPs: MutableSet<String>,
+                                extensionPointOwners: ExtensionPointOwners): Int {
     for (extension in ideaPlugin.extensions.flatMap { it.collectExtensions() }) {
       val ep = extension.extensionPoint
       if (ep == null) {
         analysisErrors.add("Cannot resolve EP ${extension.xmlElementName}")
         continue
       }
+      extensionPointOwners.discoverOwner(ep)
+
       when (ep.dynamic.value) {
         false -> nonDynamicEPs.add(ep.effectiveQualifiedName)
         null -> unspecifiedDynamicEPs.add(ep.effectiveQualifiedName)
@@ -164,3 +186,38 @@ private data class PluginUnloadabilityStatus(
   val componentCount: Int,
   val analysisErrors: List<String>
 )
+
+private class ExtensionPointOwners {
+  private val annotations = mutableMapOf<VirtualFile, FileAnnotation?>()
+  private val owners = mutableMapOf<String, String?>()
+  private val epsByOwner = mutableMapOf<String, MutableList<String>>()
+
+  fun dispose() {
+    annotations.values.filterNotNull().forEach(FileAnnotation::dispose)
+  }
+
+  fun getOwner(qName: String) = owners[qName]
+
+  fun discoverOwner(ep: ExtensionPoint) {
+    if (ep.effectiveQualifiedName in owners) return
+    val vFile = ep.xmlTag.containingFile.virtualFile
+    val fileAnnotation = annotations.getOrPut(vFile) {
+      val vcs = ProjectLevelVcsManager.getInstance(ep.xmlTag.project).getVcsFor(vFile) ?: return@getOrPut null
+      vcs.annotationProvider?.annotate(vFile)
+    }
+    val authorAspect = fileAnnotation?.aspects?.find { it.id == LineAnnotationAspect.AUTHOR }
+    if (authorAspect == null) {
+      owners[ep.effectiveQualifiedName] = null
+      return
+    }
+
+    val tagStartOffset = ep.xmlTag.textRange.startOffset
+    val startLine = FileDocumentManager.getInstance().getDocument(vFile)?.getLineNumber(tagStartOffset) ?: return
+    val owner = authorAspect.getValue(startLine)
+    owners[ep.effectiveQualifiedName] = owner
+    epsByOwner.getOrPut(owner) { mutableListOf() }.add(ep.effectiveQualifiedName)
+  }
+
+  fun getSortedOwners() = epsByOwner.keys.sortedByDescending { epsByOwner[it]!!.size }
+  fun getOwnedEPs(owner: String) = epsByOwner[owner]!!
+}
