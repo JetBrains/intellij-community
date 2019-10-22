163c7e992bd52878f3fbc7ae495602ddc1950015
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2019-10-22 18:00:17 +0300

    [vcs-log] move VcsLogStructureFilterImpl to the package with other filter classes
    GitOrigin-RevId: 8a36673ea7ea44dbb1d91596d181c98a97180404

--- platform/vcs-log/impl/src/com/intellij/vcs/log/visible/filters/VcsLogStructureFilterImpl.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/visible/filters/VcsLogStructureFilterImpl.java
@@ -1,19 +1,5 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.intellij.vcs.log.data;
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.vcs.log.visible.filters;
 
 import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.util.Condition;
@@ -37,10 +23,9 @@ public class VcsLogStructureFilterImpl implements VcsLogDetailsFilter, VcsLogStr
   @NotNull private final Collection<FilePath> myFiles;
 
   /**
-   * @deprecated use {@link com.intellij.vcs.log.visible.filters.VcsLogFilterObject#fromPaths(Collection)}
+   * Use {@link com.intellij.vcs.log.visible.filters.VcsLogFilterObject#fromPaths(Collection)}
    */
-  @Deprecated
-  public VcsLogStructureFilterImpl(@NotNull Collection<FilePath> files) {
+  protected VcsLogStructureFilterImpl(@NotNull Collection<FilePath> files) {
     myFiles = files;
   }
 
