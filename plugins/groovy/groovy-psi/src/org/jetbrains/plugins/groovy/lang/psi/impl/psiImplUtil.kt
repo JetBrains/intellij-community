19fe36073ef2af4335187d08e973bfaeae2bbae5
alexey.afanasiev <alexey.afanasiev@jetbrains.com>
2019-10-23 17:17:39 +0300

    [groovy] IDEA-224630 Extract Method results in invalid code
    (cherry picked from commit 4d48aed4e4209e10e10a6a4d570ebcbf822279a6)
    
    GitOrigin-RevId: 9a6ae9eb124abaca61485ff9178074c056dee51d

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/psiImplUtil.kt
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/psiImplUtil.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.lang.psi.impl
 
 import com.intellij.openapi.util.Key
@@ -21,6 +21,7 @@ import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrParametersOwner
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrVariable
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrVariableDeclaration
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.blocks.GrClosableBlock
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.clauses.GrForInClause
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrMethodCall
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrReferenceExpression
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrSafeCastExpression
@@ -151,7 +152,7 @@ private fun GrCodeReferenceElement.isInClosureSafeCast(): Boolean {
  * @return `true` if variable is declared in given block(nested closure and method blocks excluded)
  */
 fun GrVariable.isDeclaredIn(block: GrControlFlowOwner): Boolean {
-  if (this is GrParameter) {
+  if (this is GrParameter && this.parent !is GrForInClause) {
     val parametersOwner = getParentOfType(block, GrParametersOwner::class.java, false)
     return declarationScope == parametersOwner
   }
