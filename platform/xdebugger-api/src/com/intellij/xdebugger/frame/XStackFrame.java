1af6c916796aad44139a5928aeaa2ade3d3491d0
Egor Ushakov <egor.ushakov@jetbrains.com>
2019-10-23 18:46:33 +0300

    cleanup: removed unused
    GitOrigin-RevId: d69820963436f46e38d230cf58e4aa8dd06048b0

--- platform/xdebugger-api/src/com/intellij/xdebugger/frame/XStackFrame.java
+++ platform/xdebugger-api/src/com/intellij/xdebugger/frame/XStackFrame.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.xdebugger.frame;
 
@@ -44,12 +44,6 @@ public abstract class XStackFrame extends XValueContainer {
   @Nullable
   public XSourcePosition getSourcePosition() {
     return null;
-  }
-
-  /**
-   * Update info about stack frame
-   */
-  public void refresh() {
   }
 
   /**
