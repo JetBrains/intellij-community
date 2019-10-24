f5e279c1299ead7f7bc396f26cdcfce2c6fc9b1e
Egor Ushakov <egor.ushakov@jetbrains.com>
2019-10-24 15:45:11 +0300

    cleanup: removed deprecated
    GitOrigin-RevId: 1f83d34beeee78a579ff108b99ce3cdb6b48cc9b

--- java/debugger/openapi/src/com/intellij/debugger/DebuggerManager.java
+++ java/debugger/openapi/src/com/intellij/debugger/DebuggerManager.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.debugger;
 
 import com.intellij.debugger.engine.DebugProcess;
@@ -6,7 +6,6 @@ import com.intellij.debugger.engine.DebugProcessListener;
 import com.intellij.execution.process.ProcessHandler;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiClass;
-import com.intellij.util.Function;
 
 public abstract class DebuggerManager {
   public static DebuggerManager getInstance(Project project) {
@@ -26,10 +25,4 @@ public abstract class DebuggerManager {
   public abstract void removeClassNameMapper(NameMapper mapper);
 
   public abstract String getVMClassQualifiedName(PsiClass aClass);
-
-  /**
-   * @deprecated use {@link PositionManagerFactory} extension point instead
-   */
-  @Deprecated
-  public abstract void registerPositionManagerFactory(Function<DebugProcess, PositionManager> factory);
 }
