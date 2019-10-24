e4fd95b5cf1ea496a639c1e55b7be4293c649c70
Aleksandr <lekan1992@gmail.com>
2019-10-24 15:32:43 +0300

    - added additional highlight filter for lazy field initialization
    GitOrigin-RevId: 8352ef34ba4aecb13646b88022d451e497c38dc7

--- plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/extension/LombokHighlightErrorFilter.java
+++ plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/extension/LombokHighlightErrorFilter.java
@@ -1,34 +1,40 @@
 package de.plushnikov.intellij.plugin.extension;
 
+import java.util.regex.Pattern;
 import com.intellij.codeInsight.daemon.impl.HighlightInfo;
 import com.intellij.codeInsight.daemon.impl.HighlightInfoFilter;
 import com.intellij.lang.annotation.HighlightSeverity;
+import com.intellij.openapi.editor.colors.CodeInsightColors;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import de.plushnikov.intellij.plugin.handler.EqualsAndHashCodeCallSuperHandler;
 import de.plushnikov.intellij.plugin.handler.LazyGetterHandler;
 import de.plushnikov.intellij.plugin.handler.OnXAnnotationHandler;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.regex.Pattern;
 
 public class LombokHighlightErrorFilter implements HighlightInfoFilter {
   private static final Pattern UNINITIALIZED_MESSAGE = Pattern.compile("Variable '.+' might not have been initialized");
   private static final Pattern LOMBOK_ANYANNOTATIONREQUIRED = Pattern.compile("Incompatible types\\. Found: '__*', required: 'lombok.*AnyAnnotation\\[\\]'");
   private static final String REDUNDANT_DEFAULT_PARAMETER_VALUE_ASSIGNMENT = "Redundant default parameter value assignment";
+  private static final Pattern METHOD_INVOCATION_WILL_PRODUCE_NPE = Pattern.compile("Method invocation '.*' will produce 'NullPointerException'");
 
   @Override
   public boolean accept(@NotNull HighlightInfo highlightInfo, @Nullable PsiFile file) {
     if (null != file) {
-
+      String description = StringUtil.notNullize(highlightInfo.getDescription());
       if (HighlightSeverity.WARNING.equals(highlightInfo.getSeverity())) {
-        if (REDUNDANT_DEFAULT_PARAMETER_VALUE_ASSIGNMENT.equals(highlightInfo.getDescription())) {
+        if (REDUNDANT_DEFAULT_PARAMETER_VALUE_ASSIGNMENT.equals(description)) {
           return !EqualsAndHashCodeCallSuperHandler.isEqualsAndHashCodeCallSuperDefault(highlightInfo, file);
+        } else if (CodeInsightColors.WARNINGS_ATTRIBUTES.equals(highlightInfo.type.getAttributesKey())) {
+
+          // field should have lazy getter and should be initialized in constructors
+          if (METHOD_INVOCATION_WILL_PRODUCE_NPE.matcher(description).matches()) {
+            return !LazyGetterHandler.isLazyGetterHandled(highlightInfo, file)
+              || !LazyGetterHandler.isInitializedInConstructors(highlightInfo, file);
+          }
         }
       } else if (HighlightSeverity.ERROR.equals(highlightInfo.getSeverity())) {
-
-        final String description = StringUtil.notNullize(highlightInfo.getDescription());
 
         // Handling LazyGetter
         if (uninitializedField(description) && LazyGetterHandler.isLazyGetterHandled(highlightInfo, file)) {
--- plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/handler/LazyGetterHandler.java
+++ plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/handler/LazyGetterHandler.java
@@ -2,18 +2,24 @@ package de.plushnikov.intellij.plugin.handler;
 
 import com.intellij.codeInsight.daemon.impl.HighlightInfo;
 import com.intellij.psi.PsiAnnotation;
+import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiField;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiIdentifier;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.PsiReferenceExpression;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.siyeh.ig.psiutils.InitializationUtils;
+import org.jetbrains.annotations.NotNull;
 import de.plushnikov.intellij.plugin.util.PsiAnnotationSearchUtil;
 import de.plushnikov.intellij.plugin.util.PsiAnnotationUtil;
 import lombok.Getter;
 
 public class LazyGetterHandler {
 
-  public static boolean isLazyGetterHandled(HighlightInfo highlightInfo, PsiFile file) {
+  public static boolean isLazyGetterHandled(@NotNull HighlightInfo highlightInfo, @NotNull PsiFile file) {
     PsiElement element = file.findElementAt(highlightInfo.getStartOffset());
     if (!(element instanceof PsiIdentifier)) {
       return false;
@@ -25,5 +31,47 @@ public class LazyGetterHandler {
 
     final PsiAnnotation getterAnnotation = PsiAnnotationSearchUtil.findAnnotation(field, Getter.class);
     return null != getterAnnotation && PsiAnnotationUtil.getBooleanAnnotationValue(getterAnnotation, "lazy", false);
+  }
+
+  public static boolean isInitializedInConstructors(@NotNull HighlightInfo highlightInfo, @NotNull PsiFile file) {
+    PsiElement element = file.findElementAt(highlightInfo.getStartOffset());
+    if (!(element instanceof PsiIdentifier)) {
+      return false;
+    }
+    PsiElement parent = element.getParent();
+    if (!(parent instanceof PsiReferenceExpression)) {
+      return false;
+    }
+    PsiElement qualifier = ((PsiReferenceExpression) parent).getQualifier();
+    if (qualifier == null) {
+      return false;
+    }
+    PsiReference reference = qualifier.getReference();
+    if (reference == null) {
+      return false;
+    }
+    PsiElement field = reference.resolve();
+    if (!(field instanceof PsiField)) {
+      return false;
+    }
+    PsiClass containingClass = ((PsiField) field).getContainingClass();
+    if (containingClass == null) {
+      return false;
+    }
+    return isInitializedInConstructors((PsiField) field, containingClass);
+  }
+
+  public static boolean isInitializedInConstructors(@NotNull PsiField field, @NotNull PsiClass aClass) {
+    final PsiMethod[] constructors = aClass.getConstructors();
+    if (constructors.length == 0) {
+      return false;
+    }
+
+    for (final PsiMethod constructor : constructors) {
+      if (!InitializationUtils.methodAssignsVariableOrFails(constructor, field)) {
+        return false;
+      }
+    }
+    return true;
   }
 }
