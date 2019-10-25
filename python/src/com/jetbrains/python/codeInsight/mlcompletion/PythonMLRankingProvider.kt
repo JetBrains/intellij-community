2202ed8c46e5522d5061345d55ae0fae7c078f62
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2019-10-25 08:54:35 +0300

    IDEA-CR-53045 [stats-collector] Move python ML model into intellij.python.community.impl module
    GitOrigin-RevId: 7ec6a655b7c6f0945afbfb56adf10ee654b2b72b

--- python/src/com/jetbrains/python/codeInsight/mlcompletion/PythonMLRankingProvider.kt
+++ python/src/com/jetbrains/python/codeInsight/mlcompletion/PythonMLRankingProvider.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.jetbrains.python.codeInsight.completion.ml
+package com.jetbrains.python.codeInsight.mlcompletion
 
 import com.completion.ranker.model.python.MLWhiteBox
 import com.intellij.internal.ml.DecisionFunction
