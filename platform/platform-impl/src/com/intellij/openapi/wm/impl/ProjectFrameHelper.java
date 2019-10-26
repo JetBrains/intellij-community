253c83ee2544bb79232ea263e1d250d5c34d6f8e
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-10-26 18:46:06 +0200

    cleanup
    GitOrigin-RevId: 9ad2f3ad27878baeec121a10f08195316d1e7a89

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/ProjectFrameHelper.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/ProjectFrameHelper.java
@@ -34,7 +34,6 @@ import com.intellij.ui.BalloonLayout;
 import com.intellij.ui.BalloonLayoutImpl;
 import com.intellij.ui.ScreenUtil;
 import com.intellij.ui.content.Content;
-import com.intellij.ui.mac.MacMainFrameDecorator;
 import com.intellij.util.io.SuperUserStatus;
 import com.intellij.util.ui.JBUI;
 import com.intellij.util.ui.UIUtil;
