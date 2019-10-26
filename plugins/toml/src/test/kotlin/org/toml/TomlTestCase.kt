9aa8da6016a23f19dc976f0d8a15bd237f2e56dd
Arseniy Pendryak <a.pendryak@yandex.ru>
2019-10-27 00:19:18 +0300

    TOML: introduce escape lexer for Toml
    GitOrigin-RevId: c0b7f5636f117aa29bb87e0b9a701bf41a2d4d4b

---
+++ plugins/toml/src/test/kotlin/org/toml/TomlTestCase.kt
@@ -0,0 +1,12 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.toml
+
+import com.intellij.ext.TestCase
+
+interface TomlTestCase : TestCase {
+    override val testFileExtension: String get() = "toml"
+}
