cb8fe4eeff447e9500838ec198d0470e0fa4ea05
Arseniy Pendryak <a.pendryak@yandex.ru>
2019-10-27 00:17:29 +0300

    T: Move common code for lexer tests to `util` module
    GitOrigin-RevId: 9354ec213b179eb3e7a0430c1907856436c2628c

---
+++ plugins/toml/core/src/test/kotlin/com/intellij/ext/TestCase.kt
@@ -0,0 +1,32 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package com.intellij.ext
+
+import java.nio.file.Path
+import java.nio.file.Paths
+
+interface TestCase {
+    val testFileExtension: String
+    fun getTestDataPath(): String
+    fun getTestName(lowercaseFirstLetter: Boolean): String
+
+    companion object {
+        const val testResourcesPath = "src/test/resources"
+
+        @JvmStatic
+        fun camelOrWordsToSnake(name: String): String {
+            if (' ' in name) return name.trim().replace(" ", "_")
+
+            return name.split("(?=[A-Z])".toRegex()).joinToString("_", transform = String::toLowerCase)
+        }
+    }
+}
+
+fun TestCase.pathToSourceTestFile(): Path =
+    Paths.get("${TestCase.testResourcesPath}/${getTestDataPath()}/${getTestName(true)}.$testFileExtension")
+
+fun TestCase.pathToGoldTestFile(): Path =
+    Paths.get("${TestCase.testResourcesPath}/${getTestDataPath()}/${getTestName(true)}.txt")
