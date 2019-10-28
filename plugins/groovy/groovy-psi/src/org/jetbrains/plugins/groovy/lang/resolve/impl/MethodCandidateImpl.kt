bcc1b8d5d4f4edef638ad02ebe22dbb59e00a717
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-10-28 14:45:41 +0300

    IDEA-CR-54364: [groovy] resolve: obtain receiver argument from resolve state
    (cherry picked from commit de65ade760361a2f7df4faf773247cb55ed8d014)
    
    GitOrigin-RevId: 2b4caff2034de1c4b080c229bf7c46c2df65d06c

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/resolve/impl/MethodCandidateImpl.kt
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/resolve/impl/MethodCandidateImpl.kt
@@ -5,18 +5,21 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiMethod
 import com.intellij.psi.PsiSubstitutor
 import com.intellij.psi.PsiType
+import org.jetbrains.plugins.groovy.lang.resolve.api.Argument
 import org.jetbrains.plugins.groovy.lang.resolve.api.ArgumentMapping
 import org.jetbrains.plugins.groovy.lang.resolve.api.Arguments
 import org.jetbrains.plugins.groovy.lang.resolve.api.GroovyMethodCandidate
 import org.jetbrains.plugins.groovy.util.recursionAwareLazy
 
 class MethodCandidateImpl(
-  override val receiverType: PsiType?,
+  private val receiver: Argument?,
   override val method: PsiMethod,
   erasureSubstitutor: PsiSubstitutor,
   arguments: Arguments?,
   context: PsiElement
 ) : GroovyMethodCandidate {
+
+  override val receiverType: PsiType? get() = receiver?.type
 
   override val argumentMapping: ArgumentMapping? by recursionAwareLazy {
     arguments?.let {
