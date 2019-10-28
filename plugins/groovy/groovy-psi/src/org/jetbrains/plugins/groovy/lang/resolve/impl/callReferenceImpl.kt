dccdf09d737a4dbae57eecd8962e823a6c77fe49
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-10-28 15:01:41 +0300

    [groovy] resolve: put receiver Argument into resolve state when resolving method references
    GitOrigin-RevId: e8f1923a25aadab00edcfafe95cf6152518728ba

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/resolve/impl/callReferenceImpl.kt
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/resolve/impl/callReferenceImpl.kt
@@ -5,12 +5,11 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiType
 import com.intellij.psi.ResolveState
 import org.jetbrains.plugins.groovy.lang.psi.api.GroovyResolveResult
-import org.jetbrains.plugins.groovy.lang.psi.impl.statements.expressions.TypesUtil
 import org.jetbrains.plugins.groovy.lang.psi.impl.statements.expressions.rValueProcessor
 import org.jetbrains.plugins.groovy.lang.psi.impl.statements.expressions.resolveKinds
 import org.jetbrains.plugins.groovy.lang.resolve.api.Argument
 import org.jetbrains.plugins.groovy.lang.resolve.api.Arguments
-import org.jetbrains.plugins.groovy.lang.resolve.processReceiverType
+import org.jetbrains.plugins.groovy.lang.resolve.processReceiver
 import org.jetbrains.plugins.groovy.lang.resolve.processors.MethodProcessor
 
 fun resolveWithArguments(receiver: Argument,
@@ -18,16 +17,14 @@ fun resolveWithArguments(receiver: Argument,
                          arguments: Arguments?,
                          place: PsiElement): List<GroovyResolveResult> {
   val state = ResolveState.initial()
-  val receiverType = receiver.type ?: TypesUtil.getJavaLangObject(place)
-
   val methodProcessor = MethodProcessor(methodName, place, arguments, PsiType.EMPTY_ARRAY)
-  receiverType.processReceiverType(methodProcessor, state, place)
+  receiver.processReceiver(methodProcessor, state, place)
   methodProcessor.applicableCandidates?.let {
     return it
   }
 
   val propertyProcessor = rValueProcessor(methodName, place, resolveKinds(true))
-  receiverType.processReceiverType(propertyProcessor, state, place)
+  receiver.processReceiver(propertyProcessor, state, place)
   val properties = propertyProcessor.results
   if (properties.size == 1) {
     return properties
