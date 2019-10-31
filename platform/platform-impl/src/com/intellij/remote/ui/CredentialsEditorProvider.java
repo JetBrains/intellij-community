d7adbdb76d6374dd3aa95a4362374abb2acf8259
Alexander Koshevoy <Alexander.Koshevoy@jetbrains.com>
2019-10-31 12:27:13 +0300

    PY-39996 Introduce RemoteSdkEditorForm interface to collapse the parameters of credentials editor factory method
    GitOrigin-RevId: 5563bb43248d9b9682af494c74e9e44888d0b6e0

--- platform/platform-impl/src/com/intellij/remote/ui/CredentialsEditorProvider.java
+++ platform/platform-impl/src/com/intellij/remote/ui/CredentialsEditorProvider.java
@@ -1,11 +1,9 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.remote.ui;
 
-import com.intellij.openapi.Disposable;
 import com.intellij.openapi.project.Project;
 import com.intellij.remote.ext.CredentialsEditor;
 import com.intellij.remote.ext.CredentialsLanguageContribution;
-import com.intellij.ui.StatusPanel;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -15,11 +13,7 @@ public interface CredentialsEditorProvider {
 
   CredentialsEditor createEditor(@Nullable Project project,
                                  CredentialsLanguageContribution languageContribution,
-                                 StatusPanel statusPanel,
-                                 @Nullable Runnable validator,
-                                 @NotNull Disposable disposable,
-                                 @Nullable BundleAccessor bundleAccessor,
-                                 @NotNull SdkScopeController sdkScopeController);
+                                 @NotNull RemoteSdkEditorForm parentForm);
 
   String getDefaultInterpreterPath(BundleAccessor bundleAccessor);
 }
