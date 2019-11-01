ebcf1ed948dd7203c7501872a0683b1195ae680c
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2019-11-01 18:50:53 +0300

    svn: Allow running svn tests on linux TeamCity agents
    Use svn 1.9.7 binaries
    
    0ea3c22b6d8d925b5be26b723df37bb173debcd9, 1e04f3084176224b3d6c0bccab3a5230293c9744 follow-up
    
    GitOrigin-RevId: 26e424a4d0de7287b63ce63a892d983578e3535d

--- plugins/svn4idea/testSource/org/jetbrains/idea/svn/SvnTestCase.java
+++ plugins/svn4idea/testSource/org/jetbrains/idea/svn/SvnTestCase.java
@@ -15,7 +15,6 @@ import com.intellij.openapi.startup.StartupManager;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.ui.TestDialog;
 import com.intellij.openapi.util.SystemInfo;
-import com.intellij.openapi.util.io.IoTestUtil;
 import com.intellij.openapi.vcs.ProjectLevelVcsManager;
 import com.intellij.openapi.vcs.VcsConfiguration;
 import com.intellij.openapi.vcs.VcsException;
@@ -44,7 +43,6 @@ import org.jetbrains.idea.svn.actions.CreateExternalAction;
 import org.jetbrains.idea.svn.api.Url;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.rules.ExternalResource;
 
@@ -61,15 +59,14 @@ import static com.intellij.openapi.util.io.FileUtil.*;
 import static com.intellij.openapi.util.text.StringUtil.isEmptyOrSpaces;
 import static com.intellij.openapi.vfs.VfsUtilCore.virtualToIoFile;
 import static com.intellij.testFramework.EdtTestUtil.runInEdtAndWait;
-import static com.intellij.testFramework.UsefulTestCase.*;
+import static com.intellij.testFramework.UsefulTestCase.assertDoesntExist;
+import static com.intellij.testFramework.UsefulTestCase.assertExists;
 import static com.intellij.util.ObjectUtils.notNull;
 import static com.intellij.util.containers.ContainerUtil.map2Array;
 import static com.intellij.util.lang.CompoundRuntimeException.throwIfNotEmpty;
 import static java.util.Collections.singletonMap;
 import static org.jetbrains.idea.svn.SvnUtil.parseUrl;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public abstract class SvnTestCase extends AbstractJunitVcsTestCase {
   @ClassRule public static final ApplicationRule appRule = new ApplicationRule();
@@ -112,13 +109,6 @@ public abstract class SvnTestCase extends AbstractJunitVcsTestCase {
   @NotNull
   public static String getPluginHome() {
     return getPluginHomePath("svn4idea");
-  }
-
-  @BeforeClass
-  public static void assumeWindowsUnderTeamCity() {
-    if (IS_UNDER_TEAMCITY) {
-      IoTestUtil.assumeWindows();
-    }
   }
 
   @Before
