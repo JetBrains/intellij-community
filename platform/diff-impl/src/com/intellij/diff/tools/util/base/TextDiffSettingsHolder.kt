3507d3c11570264ae1616fdd2cb2e0654ecee6ae
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2019-11-02 17:33:05 +0300

    vcs: Do not show line numbers in Local Changes diff preview by default
    GitOrigin-RevId: 31ec17bf75e6e77a8c430306184d65507b5afa63

--- platform/diff-impl/src/com/intellij/diff/tools/util/base/TextDiffSettingsHolder.kt
+++ platform/diff-impl/src/com/intellij/diff/tools/util/base/TextDiffSettingsHolder.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.diff.tools.util.base
 
 import com.intellij.diff.util.DiffPlaces
@@ -162,6 +162,7 @@ class TextDiffSettingsHolder : PersistentStateComponent<TextDiffSettingsHolder.S
     val settings = PlaceSettings()
     if (place == DiffPlaces.CHANGES_VIEW) {
       settings.EXPAND_BY_DEFAULT = false
+      settings.SHOW_LINE_NUMBERS = false
     }
     if (place == DiffPlaces.COMMIT_DIALOG) {
       settings.EXPAND_BY_DEFAULT = false
