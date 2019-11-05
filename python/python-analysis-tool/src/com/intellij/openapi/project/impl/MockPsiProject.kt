daadf8725589f6adc90177243fdf532d08ffcf1c
Stanislav Utikeev <stanislav.utikeev@jetbrains.com>
2019-11-05 15:53:41 +0300

    Fix MockPsiProject constructor
    GitOrigin-RevId: b9438f58db0cb12dbf3d8198dd523b809fc82d14

--- python/python-analysis-tool/src/com/intellij/openapi/project/impl/MockPsiProject.kt
+++ python/python-analysis-tool/src/com/intellij/openapi/project/impl/MockPsiProject.kt
@@ -4,19 +4,16 @@ import com.intellij.ide.plugins.ContainerDescriptor
 import com.intellij.ide.plugins.IdeaPluginDescriptorImpl
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.Application
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.serviceContainer.PlatformComponentManagerImpl
 
-class MockPsiProject(application: Application, parentDisposable: Disposable = application) : PlatformComponentManagerImpl(application), Project {
+class MockPsiProject(application: Application, parentDisposable: Disposable = application) : PlatformComponentManagerImpl(application as PlatformComponentManagerImpl), Project {
   init {
     Disposer.register(parentDisposable, this)
     myPicoContainer.registerComponentInstance(Project::class.java, this)
   }
-
-  constructor() : this(ApplicationManager.getApplication())
 
   override fun getContainerDescriptor(pluginDescriptor: IdeaPluginDescriptorImpl): ContainerDescriptor =
     pluginDescriptor.project
