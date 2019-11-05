9510777b63576713acebd317f8e338b0156f3dcf
Aleksey Pivovarov <AMPivovarov@gmail.com>
2019-11-05 19:48:03 +0300

    ui: extract common class for toolbar label
    GitOrigin-RevId: ef3ae99e724134b1b80861ebb252550db6ec07b6

--- plugins/github/src/org/jetbrains/plugins/github/util/GHToolbarLabelAction.kt
+++ plugins/github/src/org/jetbrains/plugins/github/util/GHToolbarLabelAction.kt
@@ -1,25 +1,10 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.github.util
 
-import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.Presentation
-import com.intellij.openapi.actionSystem.ex.CustomComponentAction
-import com.intellij.openapi.project.DumbAwareAction
-import com.intellij.util.ui.JBUI
-import javax.swing.JComponent
-import javax.swing.JLabel
+import com.intellij.openapi.actionSystem.ex.ToolbarLabelAction
 
-class GHToolbarLabelAction(private val text: String) : DumbAwareAction(), CustomComponentAction {
-  override fun update(e: AnActionEvent) {
-    e.presentation.isEnabled = false
-    e.presentation.isVisible = true
+class GHToolbarLabelAction(text: String) : ToolbarLabelAction() {
+  init {
+    templatePresentation.text = text
   }
-
-  override fun actionPerformed(e: AnActionEvent) {}
-
-  override fun createCustomComponent(presentation: Presentation, place: String): JComponent =
-    JLabel(text).apply {
-      font = JBUI.Fonts.toolbarFont()
-      border = JBUI.Borders.empty(0, 6, 0, 5)
-    }
 }
