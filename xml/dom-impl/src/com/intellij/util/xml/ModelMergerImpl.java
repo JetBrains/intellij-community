0d6acb5fb27afa86dfff5a67c72dfed217f8345f
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-11-06 08:37:00 +0100

    reduce class loading - avoid JBTree usage in ReflectionUtil (part 1)
    GitOrigin-RevId: 06583bb098c68f92c66e745cae5c815bea0136c7

--- xml/dom-impl/src/com/intellij/util/xml/ModelMergerImpl.java
+++ xml/dom-impl/src/com/intellij/util/xml/ModelMergerImpl.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.xml;
 
 import com.intellij.openapi.util.Pair;
@@ -332,15 +318,11 @@ public final class ModelMergerImpl implements ModelMerger {
   }
 
   @Nullable
-  private static Method findPrimaryKeyAnnotatedMethod(final Method method, final Class aClass) {
-    if (method.getReturnType() != void.class && method.getParameterTypes().length == 0) {
-      for (Method each : new JavaMethodSignature(method).getAllMethods(aClass)) {
-        if (each.getAnnotation(PrimaryKey.class) != null) {
-          return each;
-        }
-      }
+  private static Method findPrimaryKeyAnnotatedMethod(@NotNull Method sampleMethod, @NotNull Class<?> aClass) {
+    if (sampleMethod.getParameterCount() != 0 || sampleMethod.getReturnType() == void.class) {
+      return null;
     }
-    return null;
+    return JavaMethodSignature.findMethod(sampleMethod, aClass, method -> method.isAnnotationPresent(PrimaryKey.class));
   }
 
   private List<Object> getMergedImplementations(final Method method,
