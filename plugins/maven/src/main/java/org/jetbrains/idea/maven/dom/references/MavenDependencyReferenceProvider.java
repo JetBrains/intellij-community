9c878f524c6896aace5c6f9d720c43b732394719
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2019-11-06 16:42:32 +0300

    IDEA-226331 - fix MavenDependencyReferenceProvider
    GitOrigin-RevId: 5d4f24fad5f7e8ceaa8215ee2b311021bb42703a

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/dom/references/MavenDependencyReferenceProvider.java
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/dom/references/MavenDependencyReferenceProvider.java
@@ -48,7 +48,7 @@ public class MavenDependencyReferenceProvider extends PsiReferenceProvider imple
       };
     }
 
-    int lastDelim = text.indexOf(secondDelim + 1);
+    int lastDelim = text.indexOf(':',secondDelim + 1);
     if (lastDelim == -1) {
       lastDelim = text.length();
     }
