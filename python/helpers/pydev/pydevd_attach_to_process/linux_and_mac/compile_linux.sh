dd2b7b30b7dff80f21523d56e825d3b90d4be72d
Andrey Lisin <andrey.lisin@jetbrains.com>
2019-11-06 15:47:22 +0300

    PY-38704 Backport attach to process updates from PyDev (IDEA-CR-54791)
    (cherry picked from commit 2ae85368c63f1a282c4358d8610a5724767871a2)
    
    GitOrigin-RevId: b35457cb99631edfb091ac472bd5dcab25eea390

---
+++ python/helpers/pydev/pydevd_attach_to_process/linux_and_mac/compile_linux.sh
@@ -0,0 +1,9 @@
+g++ -m64 -shared -o attach_linux_amd64.so -fPIC -nostartfiles attach.cpp
+mv attach_linux_amd64.so ../attach_linux_amd64.so
+echo Compiled amd64
+
+echo Note: may need "sudo apt-get install libx32gcc-4.8-dev" and "sudo apt-get install libc6-dev-i386" and "sudo apt-get install g++-multilib" to compile 32 bits
+
+g++ -m32 -shared -o attach_linux_x86.so -fPIC -nostartfiles attach.cpp
+mv attach_linux_x86.so ../attach_linux_x86.so
+echo Compiled x86
