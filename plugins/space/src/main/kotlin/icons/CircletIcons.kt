86326f08773826826c187aae19d96ee73ca85ccf
Aleksandr Krasilnikov <aleksandr.krasilnikov@jetbrains.com>
2019-11-09 15:56:08 +0300

    [idea plugin] convert CircletIcons.kt to object
    GitOrigin-RevId: 534931b0e06665c00794071045204156ae00bddf

--- plugins/space/src/main/kotlin/icons/CircletIcons.kt
+++ plugins/space/src/main/kotlin/icons/CircletIcons.kt
@@ -2,9 +2,7 @@ package icons
 
 import com.intellij.openapi.util.*
 
-interface CircletIcons {
-    companion object {
-        @JvmField
-        val mainIcon = IconLoader.getIcon("/icons/main.svg", CircletIcons::class.java)
-    }
+object CircletIcons {
+    @JvmField
+    val mainIcon = IconLoader.getIcon("/icons/main.svg", CircletIcons::class.java)
 }
