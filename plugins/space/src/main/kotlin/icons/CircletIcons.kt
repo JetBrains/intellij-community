effb9a66abad80dcaba698a7ab78b82db531e4a0
Aleksandr Krasilnikov <aleksandr.krasilnikov@jetbrains.com>
2019-11-09 15:56:41 +0300

    [idea plugin] add 13x13 main icon for tool windows
    GitOrigin-RevId: 7b881248275a0d8e25be7d06292142ad0508d343

--- plugins/space/src/main/kotlin/icons/CircletIcons.kt
+++ plugins/space/src/main/kotlin/icons/CircletIcons.kt
@@ -5,4 +5,6 @@ import com.intellij.openapi.util.*
 object CircletIcons {
     @JvmField
     val mainIcon = IconLoader.getIcon("/icons/main.svg", CircletIcons::class.java)
+    @JvmField
+    val mainToolWindowIcon = IconLoader.getIcon("/icons/main_toolwindow.svg", CircletIcons::class.java) // 13x13
 }
