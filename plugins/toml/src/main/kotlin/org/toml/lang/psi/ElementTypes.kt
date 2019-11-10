354e9c7af0f86a45d62bb36c81c13da439bf5a89
Arseniy Pendryak <a.pendryak@yandex.ru>
2019-11-10 14:52:05 +0300

    TOML: introduce `kind` property for `TomlLiteral`
    String kind also provides API to get offsets and value of string literals (similar with Rust literals)
    
    GitOrigin-RevId: ac6c0e2b67250655235e0c93f779e89a623f451c

--- plugins/toml/src/main/kotlin/org/toml/lang/psi/ElementTypes.kt
+++ plugins/toml/src/main/kotlin/org/toml/lang/psi/ElementTypes.kt
@@ -11,6 +11,7 @@ import com.intellij.psi.tree.IElementType
 import com.intellij.psi.tree.TokenSet
 import org.toml.ide.icons.TomlIcons
 import org.toml.lang.TomlLanguage
+import org.toml.lang.psi.TomlElementTypes.*
 
 class TomlTokenType(debugName: String) : IElementType(debugName, TomlLanguage)
 class TomlCompositeType(debugName: String) : IElementType(debugName, TomlLanguage)
@@ -25,4 +26,20 @@ object TomlFileType : LanguageFileType(TomlLanguage) {
     override fun getCharset(file: VirtualFile, content: ByteArray) = "UTF-8"
 }
 
-val TOML_COMMENTS = TokenSet.create(TomlElementTypes.COMMENT)
+val TOML_COMMENTS = TokenSet.create(COMMENT)
+
+val TOML_STRING_LITERALS = TokenSet.create(
+    BASIC_STRING,
+    MULTILINE_BASIC_STRING,
+    LITERAL_STRING,
+    MULTILINE_LITERAL_STRING
+)
+
+val TOML_LITERALS = TokenSet.orSet(
+    TOML_STRING_LITERALS,
+    TokenSet.create(
+        BOOLEAN,
+        NUMBER,
+        DATE_TIME
+    )
+)
