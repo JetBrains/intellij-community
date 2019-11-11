241a89b4a6a5e1b07528d25a8e662cb6ad9f46cc
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2019-11-11 14:26:16 +0700

    MethodElement#copyElement override removed
    It used to set the method context to the surrounding class type parameter list, instead of the class itself. This caused inability to resolve inner types referenced in a method copy (IDEA-226338). Looks like everything is working without this override.
    
    GitOrigin-RevId: b1d42c9bf5ef86a5df098656eff690304399e1e6

--- java/java-tests/testSrc/com/intellij/java/refactoring/inline/InlineMethodTest.java
+++ java/java-tests/testSrc/com/intellij/java/refactoring/inline/InlineMethodTest.java
@@ -542,6 +542,10 @@ public class InlineMethodTest extends LightRefactoringTestCase {
   public void testEnumStaticMethod() {
     doTest();
   }
+  
+  public void testTypeParameterMethodRefArgument() {
+    doTest();
+  }
 
   @Override
   protected Sdk getProjectJDK() {
