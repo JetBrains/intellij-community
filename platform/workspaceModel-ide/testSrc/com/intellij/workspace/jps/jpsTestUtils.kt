eba7eea60c8d7e22e15eb36b8827a3461a52b6b1
nik <Nikolay.Chashnikov@jetbrains.com>
2019-11-11 16:32:06 +0300

    [workspace model] use forward slashes in test to fix it on Windows, take 2
    GitOrigin-RevId: 04f9112eb1611fe6fa982d3ef7d13943fed5eba9

--- platform/workspaceModel-ide/testSrc/com/intellij/workspace/jps/jpsTestUtils.kt
+++ platform/workspaceModel-ide/testSrc/com/intellij/workspace/jps/jpsTestUtils.kt
@@ -89,10 +89,10 @@ internal fun JpsFileContentWriterImpl.writeFiles(baseProjectDir: File) {
 }
 
 internal fun assertDirectoryMatches(actualDir: File, expectedDir: File, filesToIgnore: Set<String>, componentsToIgnore: List<String>) {
-  val actualFiles = actualDir.walk().filter { it.isFile }.associateBy { FileUtil.getRelativePath(actualDir.absolutePath, it.absolutePath, '/') }
+  val actualFiles = actualDir.walk().filter { it.isFile }.associateBy { FileUtil.toSystemIndependentName(FileUtil.getRelativePath(actualDir, it)!!) }
   val expectedFiles = expectedDir.walk()
     .filter { it.isFile }
-    .associateBy { FileUtil.getRelativePath(expectedDir.absolutePath, it.absolutePath, '/') }
+    .associateBy { FileUtil.toSystemIndependentName(FileUtil.getRelativePath(expectedDir, it)!!) }
     .filterKeys { it !in filesToIgnore }
 
   for (actualPath in actualFiles.keys) {
