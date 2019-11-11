b1c052e164cff889d9242472d52102f12f66fd50
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2019-11-11 19:19:45 +0300

    File drop handler
    GitOrigin-RevId: f63ce628f0e0331381868359e10de16d71ddac86

--- platform/platform-impl/src/com/intellij/ide/lightEdit/LightEditorManager.java
+++ platform/platform-impl/src/com/intellij/ide/lightEdit/LightEditorManager.java
@@ -9,10 +9,12 @@ import com.intellij.openapi.editor.EditorKind;
 import com.intellij.openapi.editor.ex.EditorEx;
 import com.intellij.openapi.editor.highlighter.EditorHighlighter;
 import com.intellij.openapi.editor.highlighter.EditorHighlighterFactory;
+import com.intellij.openapi.editor.impl.EditorImpl;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.EventDispatcher;
+import com.intellij.util.ObjectUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -27,6 +29,8 @@ class LightEditorManager implements Disposable {
   Editor createEditor(@NotNull Document document) {
     Editor editor = EditorFactory.getInstance().createEditor(
       document, ProjectManager.getInstance().getDefaultProject(), EditorKind.MAIN_EDITOR);
+    ObjectUtils.consumeIfCast(editor, EditorImpl.class,
+                              editorImpl -> editorImpl.setDropHandler(new LightEditDropHandler()));
     myEditors.add(editor);
     return editor;
   }
