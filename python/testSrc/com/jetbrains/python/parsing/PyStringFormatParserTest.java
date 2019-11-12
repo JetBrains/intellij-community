9846716abbe63452cba2075a4085b7c97665d0fb
Stanislav Utikeev <stanislav.utikeev@jetbrains.com>
2019-11-12 15:50:11 +0300

    Move parsing tests to according package
    GitOrigin-RevId: 36342ae76f07ee46ccb2c0e84344849daff54c4c

--- python/testSrc/com/jetbrains/python/parsing/PyStringFormatParserTest.java
+++ python/testSrc/com/jetbrains/python/parsing/PyStringFormatParserTest.java
@@ -1,5 +1,5 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.jetbrains.python.breadcrumbs;
+package com.jetbrains.python.parsing;
 
 import com.intellij.openapi.util.TextRange;
 import com.intellij.testFramework.UsefulTestCase;
