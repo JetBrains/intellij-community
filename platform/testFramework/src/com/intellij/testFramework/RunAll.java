cc879060cb4d07dfb06c792cddba2556e590baa9
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-11-14 19:14:11 +0300

    Revert "RunAll: add ability to run action for bunch of different inputs"
    This reverts commit dfdfcc57
    
    GitOrigin-RevId: 5cdd443d9e9d61c8635ed3b0bf9c28843e1e4bec

--- platform/testFramework/src/com/intellij/testFramework/RunAll.java
+++ platform/testFramework/src/com/intellij/testFramework/RunAll.java
@@ -1,19 +1,15 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.testFramework;
 
 import com.intellij.util.SmartList;
-import com.intellij.util.ThrowableConsumer;
-import com.intellij.util.ThrowablePairConsumer;
 import com.intellij.util.ThrowableRunnable;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.lang.CompoundRuntimeException;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Runs all given runnables and throws all the caught exceptions at the end.
@@ -33,19 +29,15 @@ public class RunAll implements Runnable {
   }
 
   @SafeVarargs
-  @Contract(pure = true)
+  @Contract(pure=true)
   public final RunAll append(@NotNull ThrowableRunnable<Throwable>... actions) {
-    return new RunAll(ContainerUtil.concat(
-      myActions,
-      actions.length == 1 ? Collections.singletonList(actions[0]) : ContainerUtil.newArrayList(actions)
-    ));
+    return new RunAll(ContainerUtil.concat(myActions, actions.length == 1 ? Collections.singletonList(actions[0]) : ContainerUtil.newArrayList(actions)));
   }
 
   @Override
   public void run() {
     run(Collections.emptyList());
   }
-
   public void run(@NotNull List<? extends Throwable> suppressedExceptions) {
     List<Throwable> throwables = collectExceptions();
     throwables.addAll(0, suppressedExceptions);
@@ -67,17 +59,5 @@ public class RunAll implements Runnable {
       }
     }
     return errors;
-  }
-
-  @NotNull
-  public static <T, E extends Throwable> RunAll runAll(@NotNull Collection<? extends T> input,
-                                                       @NotNull ThrowableConsumer<? super T, ? extends E> action) {
-    return new RunAll(ContainerUtil.map(input, it -> (ThrowableRunnable<E>)() -> action.consume(it)));
-  }
-
-  @NotNull
-  public static <K, V, E extends Throwable> RunAll runAll(@NotNull Map<? extends K, ? extends V> input,
-                                                          @NotNull ThrowablePairConsumer<? super K, ? super V, ? extends E> action) {
-    return runAll(input.entrySet(), e -> action.consume(e.getKey(), e.getValue()));
   }
 }
