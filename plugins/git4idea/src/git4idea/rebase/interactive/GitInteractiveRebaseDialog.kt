2c46c4274879cd7e9931f21a70d910d65d8e785e
Nikolay Rykunov <Nikolay.Rykunov@jetbrains.com>
2019-11-15 20:23:58 +0300

    [git] Interactive Rebase Dialog: Introduce simple dialog with rebasing commits subjects and their details
    GitOrigin-RevId: 9c63e5f236152307d5a580604ec94366619027a2

---
+++ plugins/git4idea/src/git4idea/rebase/interactive/GitInteractiveRebaseDialog.kt
@@ -0,0 +1,140 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package git4idea.rebase.interactive
+
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.openapi.vcs.VcsException
+import com.intellij.openapi.vcs.changes.Change
+import com.intellij.openapi.vcs.changes.committed.CommittedChangesTreeBrowser
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.ui.*
+import com.intellij.ui.table.JBTable
+import com.intellij.util.ui.EditableModel
+import com.intellij.util.ui.JBDimension
+import com.intellij.util.ui.JBUI
+import com.intellij.util.ui.components.BorderLayoutPanel
+import com.intellij.vcs.log.VcsCommitMetadata
+import com.intellij.vcs.log.ui.details.FullCommitDetailsListPanel
+import git4idea.history.GitCommitRequirements
+import git4idea.history.GitLogUtil
+import git4idea.i18n.GitBundle
+import git4idea.rebase.GitRebaseEntry
+import git4idea.rebase.GitRebaseEntryWithDetails
+import org.jetbrains.annotations.CalledInBackground
+import javax.swing.JComponent
+import javax.swing.JTable
+import javax.swing.ListSelectionModel
+import javax.swing.table.AbstractTableModel
+import kotlin.math.max
+import kotlin.math.min
+
+internal class GitInteractiveRebaseDialog(
+  project: Project,
+  root: VirtualFile,
+  entries: List<GitRebaseEntryWithDetails>
+) : DialogWrapper(project, true) {
+  companion object {
+    private const val DETAILS_PROPORTION = "Git.Interactive.Rebase.Details.Proportion"
+    private const val DIMENSION_KEY = "Git.Interactive.Rebase.Dialog"
+
+    private const val DIALOG_HEIGHT = 450
+    private const val DIALOG_WIDTH = 800
+  }
+
+  private val commitsTableModel = CommitsTableModel(entries)
+  private val commitsTable = CommitsTable(commitsTableModel)
+  private val modalityState = ModalityState.stateForComponent(window)
+  private val fullCommitDetailsListPanel = object : FullCommitDetailsListPanel(project, disposable, modalityState) {
+    @CalledInBackground
+    @Throws(VcsException::class)
+    override fun loadChanges(commits: List<VcsCommitMetadata>): List<Change> {
+      val changes = mutableListOf<Change>()
+      GitLogUtil.readFullDetailsForHashes(project, root, commits.map { it.id.asString() }, GitCommitRequirements.DEFAULT) { gitCommit ->
+        changes.addAll(gitCommit.changes)
+      }
+      return CommittedChangesTreeBrowser.zipChanges(changes)
+    }
+  }
+
+  init {
+    commitsTable.selectionModel.addListSelectionListener { e ->
+      if (!e.valueIsAdjusting) {
+        fullCommitDetailsListPanel.commitsSelected(commitsTable.selectedRows.map { commitsTableModel.entries[it].commitDetails })
+      }
+    }
+
+    title = GitBundle.getString("rebase.editor.title")
+    setOKButtonText(GitBundle.getString("rebase.editor.button"))
+    init()
+  }
+
+  override fun getDimensionServiceKey() = DIMENSION_KEY
+
+  override fun createCenterPanel() = BorderLayoutPanel().apply {
+    val tablePanel = ToolbarDecorator.createDecorator(commitsTable)
+      .setAsUsualTopToolbar()
+      .setPanelBorder(IdeBorderFactory.createBorder(SideBorder.TOP))
+      .disableAddAction()
+      .disableRemoveAction()
+      .createPanel()
+      .apply {
+        border = JBUI.Borders.emptyTop(4)
+      }
+
+    val detailsSplitter = OnePixelSplitter(DETAILS_PROPORTION, 0.5f).apply {
+      firstComponent = tablePanel
+      secondComponent = fullCommitDetailsListPanel
+    }
+    addToCenter(detailsSplitter)
+    preferredSize = JBDimension(DIALOG_WIDTH, DIALOG_HEIGHT)
+  }
+
+  override fun getStyle() = DialogStyle.COMPACT
+
+  fun getEntries(): List<GitRebaseEntry> = commitsTableModel.entries
+
+  override fun getPreferredFocusedComponent(): JComponent = commitsTable
+}
+
+private class CommitsTableModel(initialEntries: List<GitRebaseEntryWithDetails>) : AbstractTableModel(), EditableModel {
+  companion object {
+    const val SUBJECT_COLUMN = 0
+  }
+
+  private val _entries: MutableList<GitRebaseEntryWithDetails> = initialEntries.toMutableList()
+  val entries: List<GitRebaseEntryWithDetails> = _entries
+
+  override fun getRowCount() = _entries.size
+
+  override fun getColumnCount() = SUBJECT_COLUMN + 1
+
+  override fun getValueAt(rowIndex: Int, columnIndex: Int): Any = when (columnIndex) {
+    SUBJECT_COLUMN -> _entries[rowIndex].subject
+    else -> throw IllegalArgumentException("Unsupported column index: $columnIndex")
+  }
+
+  override fun exchangeRows(oldIndex: Int, newIndex: Int) {
+    val movingElement: GitRebaseEntryWithDetails = _entries.removeAt(oldIndex)
+    _entries.add(newIndex, movingElement)
+    fireTableRowsUpdated(min(oldIndex, newIndex), max(oldIndex, newIndex))
+  }
+
+  override fun canExchangeRows(oldIndex: Int, newIndex: Int) = true
+
+  override fun removeRow(idx: Int) {
+    throw UnsupportedOperationException()
+  }
+
+  override fun addRow() {
+    throw UnsupportedOperationException()
+  }
+}
+
+private class CommitsTable(val model: CommitsTableModel) : JBTable(model) {
+  init {
+    setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION)
+    intercellSpacing = JBUI.emptySize()
+    tableHeader = null
+  }
+}
