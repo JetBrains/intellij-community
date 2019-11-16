f7aed3314702c6aacd941c9d2e29f5bb4c6c8522
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-11-16 10:59:21 +0100

    better name for "editor restoring till paint" activity
    GitOrigin-RevId: a759370b82560f004df9105d39e9413b5f6adea9

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/EditorsSplitters.java
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/EditorsSplitters.java
@@ -67,7 +67,7 @@ import java.util.concurrent.CopyOnWriteArraySet;
 import static com.intellij.openapi.wm.ToolWindowId.PROJECT_VIEW;
 
 public class EditorsSplitters extends IdePanePanel implements UISettingsListener, Disposable {
-  public static final Key<Activity> OPEN_FILES_ACTIVITY = Key.create("open.files.start.timestamp");
+  public static final Key<Activity> OPEN_FILES_ACTIVITY = Key.create("open.files.activity");
   private static final Logger LOG = Logger.getInstance(EditorsSplitters.class);
   private static final String PINNED = "pinned";
   private static final String CURRENT_IN_TAB = "current-in-tab";
@@ -245,7 +245,7 @@ public class EditorsSplitters extends IdePanePanel implements UISettingsListener
       return;
     }
 
-    ApplicationManager.getApplication().putUserData(OPEN_FILES_ACTIVITY, StartUpMeasurer.startActivity("restoreEditorsTillPaint"));
+    ApplicationManager.getApplication().putUserData(OPEN_FILES_ACTIVITY, StartUpMeasurer.startActivity("editor restoring till paint"));
     Activity restoringEditors = StartUpMeasurer.startMainActivity("editor restoring");
     JPanel component = myUIBuilder.process(mySplittersElement, getTopPanel());
     if (component != null) {
