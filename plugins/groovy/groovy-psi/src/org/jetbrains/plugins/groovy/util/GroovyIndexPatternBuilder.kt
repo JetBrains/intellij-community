0658f979b0d76e8c73e13f03024dccc5e98a2408
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-11-18 13:54:37 +0300

    [groovy] convert GroovyIndexPatternBuilder to kt
    GitOrigin-RevId: 879e90ae0132b8afbaa541cc9ed6932cb4602c9a

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/util/GroovyIndexPatternBuilder.kt
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/util/GroovyIndexPatternBuilder.kt
@@ -1,52 +1,31 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.jetbrains.plugins.groovy.util;
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.plugins.groovy.util
 
-import com.intellij.lexer.Lexer;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.impl.search.IndexPatternBuilder;
-import com.intellij.psi.tree.IElementType;
-import com.intellij.psi.tree.TokenSet;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.plugins.groovy.lang.lexer.GroovyLexer;
-import org.jetbrains.plugins.groovy.lang.lexer.GroovyTokenTypes;
-import org.jetbrains.plugins.groovy.lang.lexer.TokenSets;
-import org.jetbrains.plugins.groovy.lang.psi.GroovyFile;
+import com.intellij.lexer.Lexer
+import com.intellij.psi.PsiFile
+import com.intellij.psi.impl.search.IndexPatternBuilder
+import com.intellij.psi.tree.IElementType
+import com.intellij.psi.tree.TokenSet
+import org.jetbrains.plugins.groovy.lang.lexer.GroovyLexer
+import org.jetbrains.plugins.groovy.lang.lexer.GroovyTokenTypes
+import org.jetbrains.plugins.groovy.lang.lexer.TokenSets
+import org.jetbrains.plugins.groovy.lang.psi.GroovyFile
 
-public class GroovyIndexPatternBuilder implements IndexPatternBuilder {
-    @Override
-    public Lexer getIndexingLexer(@NotNull PsiFile file) {
-        if (file instanceof GroovyFile) {
-            return new GroovyLexer();
-        }
-        return null;
-    }
+class GroovyIndexPatternBuilder : IndexPatternBuilder {
 
-    @Override
-    public TokenSet getCommentTokenSet(@NotNull PsiFile file) {
-      return TokenSets.ALL_COMMENT_TOKENS;
-    }
+  override fun getIndexingLexer(file: PsiFile): Lexer? {
+    return if (file is GroovyFile) GroovyLexer() else null
+  }
 
-    @Override
-    public int getCommentStartDelta(IElementType tokenType) {
-      return 0;
-    }
+  override fun getCommentTokenSet(file: PsiFile): TokenSet? {
+    return TokenSets.ALL_COMMENT_TOKENS
+  }
 
-    @Override
-    public int getCommentEndDelta(IElementType tokenType) {
-      return tokenType == GroovyTokenTypes.mML_COMMENT ? 2 : 0;
-    }
+  override fun getCommentStartDelta(tokenType: IElementType): Int {
+    return 0
+  }
+
+  override fun getCommentEndDelta(tokenType: IElementType): Int {
+    return if (tokenType === GroovyTokenTypes.mML_COMMENT) 2 else 0
+  }
 }
