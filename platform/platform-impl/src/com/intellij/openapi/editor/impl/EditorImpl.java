9c939f00a5257f34f2d97990566bd128ad428fac
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2019-11-18 16:30:27 +0300

    IDEA-CR-55412: IDEA-226837 Caret jumps to another line when moving using left/right arrows
    (cherry picked from commit 4272914ed0934cf33575aaf21b816733fae47fa6)
    
    GitOrigin-RevId: 09ed2d45dfa857b0df95d5dcc3054227d32c7fe4

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorImpl.java
@@ -1880,7 +1880,7 @@ public final class EditorImpl extends UserDataHolderBase implements EditorEx, Hi
       g.fillRect(clip.x, clip.y, clip.width, clip.height);
       return;
     }
-    if (myUpdateCursor) {
+    if (myUpdateCursor && !myPurePaintingMode) {
       setCursorPosition();
       myUpdateCursor = false;
     }
