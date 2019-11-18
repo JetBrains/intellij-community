cd43d0d006629cd3b6a85a3bcb05e0549327d37c
sergey.shkredov <sergey.shkredov@jetbrains.com>
2019-11-18 22:54:14 +0300

    Fix repos order.
    GitOrigin-RevId: ad0aef272f0db4aa723196a6670d82e67921d8e2

--- plugins/space/src/main/kotlin/circlet/ui/clone/CircletCloneComponentViewModel.kt
+++ plugins/space/src/main/kotlin/circlet/ui/clone/CircletCloneComponentViewModel.kt
@@ -1,17 +1,13 @@
 package circlet.ui.clone
 
 import circlet.client.api.*
-import circlet.platform.api.*
 import circlet.platform.client.*
 import circlet.ui.*
 import circlet.workspaces.*
-import com.intellij.util.concurrency.*
 import com.intellij.util.ui.cloneDialog.*
-import kotlinx.coroutines.*
 import libraries.coroutines.extra.*
 import runtime.*
 import runtime.reactive.*
-import java.awt.image.*
 
 class CircletCloneComponentViewModel(
     override val lifetime: Lifetime,
@@ -21,45 +17,42 @@ class CircletCloneComponentViewModel(
     private val starService: Star,
     private val circletImageLoader: CircletImageLoader
 ) : Lifetimed {
-    private val appDispatcher = AppExecutorUtil.getAppExecutorService().asCoroutineDispatcher()
 
     val isLoading: MutableProperty<Boolean> = Property.createMutable(false)
 
     val me: MutableProperty<TD_MemberProfile> = workspace.me
-
-    val profileAvatar: MutableProperty<BufferedImage?> = mapInit(null) {
-        val avatarId = me.value.avatar ?: return@mapInit null
-        circletImageLoader.loadImageAsync(avatarId).await()
-    }
 
     val repos = xTransformedPagedListOnFlux<PR_Project, CircletCloneListItem>(
         client = workspace.client,
         batchSize = 10,
         keyFn = { it.id },
         result = { allProjects ->
-            val projectsWithRepos = repositoryService.getRepositories(allProjects.map { project -> project.key })
+
+            val projectsWithRepos = repositoryService.getRepositories(allProjects.map { project -> project.key }).groupBy { it.project.key }
+
             val starredProjectKeys = starService.starredProjects().map(PR_Project::key).toHashSet()
 
             val result = mutableListOf<CircletCloneListItem>()
-            for (prRepos in projectsWithRepos) {
-                val project = prRepos.project
-
-                val isStarred = starredProjectKeys.contains(project.key)
-
-                for (repo in prRepos.repos) {
-                    val detailsProperty = mutableProperty<RepoDetails?>(null)
-                    val item = CircletCloneListItem(project, isStarred, repo, detailsProperty)
-                    item.visible.forEach(lifetime) {
-                        item.visible.forEach(lifetime) {
-                            launch(lifetime, Ui) {
-                                if (it) {
-                                    detailsProperty.value = repositoryService.repositoryDetails(project.key, repo.name)
+            allProjects.forEach { project ->
+                val projectRepos = projectsWithRepos[project.key]
+                if (projectRepos != null) {
+                    val isStarred = starredProjectKeys.contains(project.key)
+                    projectRepos.forEach { projectRepo ->
+                        projectRepo.repos.forEach { repo ->
+                            val detailsProperty = mutableProperty<RepoDetails?>(null)
+                            val item = CircletCloneListItem(project, isStarred, repo, detailsProperty)
+                            item.visible.forEach(lifetime) {
+                                item.visible.forEach(lifetime) {
+                                    launch(lifetime, Ui) {
+                                        if (it) {
+                                            detailsProperty.value = repositoryService.repositoryDetails(project.key, repo.name)
+                                        }
+                                    }
                                 }
                             }
+                            result.add(item)
                         }
                     }
-                    result.add(item)
-
                 }
             }
             result
