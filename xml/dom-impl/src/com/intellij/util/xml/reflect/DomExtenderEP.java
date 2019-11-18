496ef54fc8ae907b363f15eaff27e4d9045b8190
peter <peter@jetbrains.com>
2019-11-18 20:40:13 +0100

    make main DOM extension points dynamic (IDEA-225622)
    GitOrigin-RevId: c3f70f1e5efda968b412519aa54872de3701fbae

--- xml/dom-impl/src/com/intellij/util/xml/reflect/DomExtenderEP.java
+++ xml/dom-impl/src/com/intellij/util/xml/reflect/DomExtenderEP.java
@@ -16,13 +16,18 @@
 package com.intellij.util.xml.reflect;
 
 import com.intellij.diagnostic.PluginException;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.extensions.AbstractExtensionPointBean;
+import com.intellij.openapi.extensions.ExtensionPointListener;
 import com.intellij.openapi.extensions.ExtensionPointName;
+import com.intellij.openapi.extensions.PluginDescriptor;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.stubs.StubIndex;
 import com.intellij.util.xml.impl.DomInvocationHandler;
 import com.intellij.util.xmlb.annotations.Attribute;
 import com.intellij.xml.util.XmlUtil;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -31,9 +36,24 @@ import org.jetbrains.annotations.Nullable;
  */
 public class DomExtenderEP extends AbstractExtensionPointBean {
 
+  @ApiStatus.Internal
   public static final ExtensionPointName<DomExtenderEP> EP_NAME = ExtensionPointName.create("com.intellij.dom.extender");
 
   private static final Logger LOG = Logger.getInstance(DomExtenderEP.class);
+
+  static {
+    EP_NAME.addExtensionPointListener(new ExtensionPointListener<DomExtenderEP>() {
+      @Override
+      public void extensionAdded(@NotNull DomExtenderEP extension, @NotNull PluginDescriptor pluginDescriptor) {
+        StubIndex.getInstance().forceRebuild(new Throwable());
+      }
+
+      @Override
+      public void extensionRemoved(@NotNull DomExtenderEP extension, @NotNull PluginDescriptor pluginDescriptor) {
+        StubIndex.getInstance().forceRebuild(new Throwable());
+      }
+    }, ApplicationManager.getApplication());
+  }
 
   @Attribute("domClass")
   public String domClassName;
