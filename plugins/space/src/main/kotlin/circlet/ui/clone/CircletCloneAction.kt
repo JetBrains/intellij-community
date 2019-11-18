0ba076d3dc7a2a22517d12f5b769c980aad10a35
Aleksandr Krasilnikov <aleksandr.krasilnikov@jetbrains.com>
2019-11-18 12:16:35 +0300

    [idea plugin] add login and logout actions
    GitOrigin-RevId: 98e117a01bb09e15d2c02493c76fb736e1dcd04d

--- plugins/space/src/main/kotlin/circlet/ui/clone/CircletCloneAction.kt
+++ plugins/space/src/main/kotlin/circlet/ui/clone/CircletCloneAction.kt
@@ -1,16 +1,16 @@
 package circlet.ui.clone
 
+import circlet.actions.*
 import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.project.*
 import com.intellij.openapi.vcs.*
 import com.intellij.util.ui.cloneDialog.*
-import icons.*
 
 class CircletCloneAction : DumbAwareAction() {
     override fun update(e: AnActionEvent) {
         val projectExists = e.project != null
         e.presentation.isEnabledAndVisible = projectExists
-        e.presentation.icon = if (e.place == ActionPlaces.ACTION_SEARCH) CircletIcons.mainIcon else null
+        CircletActionUtils.showIconInActionSearch(e)
     }
 
     override fun actionPerformed(e: AnActionEvent) {
