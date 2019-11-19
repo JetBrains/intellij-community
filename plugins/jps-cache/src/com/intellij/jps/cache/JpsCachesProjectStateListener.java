2a56e9e29a8e8bda622fb4d71d05fb20b298e894
Mikhail Mazurkevich <mikhail.mazurkevich@jetbrains.com>
2019-11-19 22:34:49 +0300

    [jps-cache] Clear jps state after registry key value change
    GitOrigin-RevId: 9180c4b50969a79fc56d08ea4ae70de29ba6e460

--- plugins/jps-cache/src/com/intellij/jps/cache/JpsCachesProjectStateListener.java
+++ plugins/jps-cache/src/com/intellij/jps/cache/JpsCachesProjectStateListener.java
@@ -1,20 +1,27 @@
 package com.intellij.jps.cache;
 
+import com.intellij.compiler.server.BuildManager;
 import com.intellij.jps.cache.loader.JpsOutputLoaderManager;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
+import com.intellij.openapi.util.registry.Registry;
+import com.intellij.openapi.util.registry.RegistryValue;
+import com.intellij.openapi.util.registry.RegistryValueListener;
 import git4idea.repo.GitRepository;
 import git4idea.repo.GitRepositoryChangeListener;
 import org.jetbrains.annotations.NotNull;
 
-public class JpsCachesProjectStateListener implements StartupActivity, GitRepositoryChangeListener {
+public class JpsCachesProjectStateListener implements StartupActivity, GitRepositoryChangeListener, RegistryValueListener {
   private static final Logger LOG = Logger.getInstance("com.intellij.jps.cache.JpsCachesProjectStateListener");
+  private static final String PORTABLE_CACHES_KEY = "compiler.build.portable.caches";
   private String previousCommitId = "";
 
   @Override
   public void runActivity(@NotNull Project project) {
     project.getMessageBus().connect().subscribe(GitRepository.GIT_REPO_CHANGE, this);
+    Registry.get(PORTABLE_CACHES_KEY).addListener(this, ApplicationManager.getApplication());
   }
 
   @Override
@@ -24,5 +31,13 @@ public class JpsCachesProjectStateListener implements StartupActivity, GitReposi
     if (currentRevision == null || previousCommitId.equals(currentRevision)) return;
     previousCommitId = currentRevision;
     JpsOutputLoaderManager.getInstance(repository.getProject()).notifyAboutNearestCache();
+  }
+
+  @Override
+  public void beforeValueChanged(@NotNull RegistryValue value) { }
+
+  @Override
+  public void afterValueChanged(@NotNull RegistryValue value) {
+    BuildManager.getInstance().clearState();
   }
 }
