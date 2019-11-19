14b78ce15dccdb05d91414c08aaec6dd0c773d2f
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-11-19 19:43:30 +0300

    cleanup
    GitOrigin-RevId: 11608ce190deb50f1f5eb6a1843d86d2f179ce4b

--- java/java-impl/src/com/intellij/slicer/JavaSliceUsage.java
+++ java/java-impl/src/com/intellij/slicer/JavaSliceUsage.java
@@ -57,7 +57,7 @@ public class JavaSliceUsage extends SliceUsage {
 
   @Override
   protected void processUsagesFlownDownTo(PsiElement element, Processor<SliceUsage> uniqueProcessor) {
-    SliceUtil.processUsagesFlownDownTo(element, uniqueProcessor, this, mySubstitutor, indexNesting,syntheticField);
+    SliceUtil.processUsagesFlownDownTo(element, uniqueProcessor, this, mySubstitutor, indexNesting, syntheticField);
   }
 
   @Override
@@ -71,15 +71,6 @@ public class JavaSliceUsage extends SliceUsage {
   @NotNull
   public PsiSubstitutor getSubstitutor() {
     return mySubstitutor;
-  }
-
-  public int getIndexNesting() {
-    return indexNesting;
-  }
-
-  @NotNull
-  public String getSyntheticField() {
-    return syntheticField;
   }
 
   @Override
