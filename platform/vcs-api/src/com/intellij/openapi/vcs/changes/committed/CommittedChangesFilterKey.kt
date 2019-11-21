efa84e64c995515bf27751344b32d342054b3e0b
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2019-11-21 19:48:28 +0300

    vcs: Convert "CommittedChangesFilterKey", "CommittedChangesFilterPriority", "VcsCommittedViewAuxiliary" to kotlin
    GitOrigin-RevId: d51622b3039652546f81366e59e7b3f30c30787e

--- platform/vcs-api/src/com/intellij/openapi/vcs/changes/committed/CommittedChangesFilterKey.kt
+++ platform/vcs-api/src/com/intellij/openapi/vcs/changes/committed/CommittedChangesFilterKey.kt
@@ -1,57 +1,18 @@
-/*
- * Copyright 2000-2010 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.intellij.openapi.vcs.changes.committed;
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.vcs.changes.committed
 
-import org.jetbrains.annotations.NotNull;
+enum class CommittedChangesFilterPriority(val priority: Int) {
+  TEXT(0),
+  STRUCTURE(1),
+  USER(2),
+  MERGE(3),
+  NONE(4)
+}
 
-/**
-* @author irengrig
-*/
-public class CommittedChangesFilterKey implements Comparable<CommittedChangesFilterKey> {
-  private final CommittedChangesFilterPriority myPriority;
-  @NotNull
-  private final String myId;
+data class CommittedChangesFilterKey(
+  private val id: String,
+  private val priority: CommittedChangesFilterPriority
+) : Comparable<CommittedChangesFilterKey> {
 
-  public CommittedChangesFilterKey(@NotNull final String id, final CommittedChangesFilterPriority priority) {
-    myId = id;
-    myPriority = priority;
-  }
-
-  @Override
-  public int compareTo(final CommittedChangesFilterKey o) {
-    return Integer.compare(myPriority.getPriority(), o.myPriority.getPriority());
-  }
-
-  @Override
-  public boolean equals(final Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-
-    final CommittedChangesFilterKey key = (CommittedChangesFilterKey)o;
-
-    if (myPriority.getPriority() != key.myPriority.getPriority()) return false;
-    if (!myId.equals(key.myId)) return false;
-
-    return true;
-  }
-
-  @Override
-  public int hashCode() {
-    int result = myPriority.getPriority();
-    result = 31 * result + myId.hashCode();
-    return result;
-  }
+  override fun compareTo(other: CommittedChangesFilterKey): Int = compareValuesBy(this, other) { priority.priority }
 }
