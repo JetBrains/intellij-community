e297d52a98ec0140f23e43b7a3ab67bdb124f5b9
Ivan Semenov <ivan.semenov@jetbrains.com>
2019-11-21 17:04:05 +0300

    [git] Queue git fetch operations for the same remote
    GitOrigin-RevId: 5de863394d18275357be081ac907e3bbd71dc787

--- plugins/git4idea/src/git4idea/fetch/GitFetchSupportImpl.kt
+++ plugins/git4idea/src/git4idea/fetch/GitFetchSupportImpl.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package git4idea.fetch
 
 import com.intellij.dvcs.MultiMessage
@@ -53,6 +39,8 @@ internal class GitFetchSupportImpl(private val project: Project) : GitFetchSuppo
 
   private val git get() = Git.getInstance() as GitImpl
   private val progressManager get() = ProgressManager.getInstance()
+
+  private val fetchQueue = GitRemoteOperationQueueImpl()
 
   override fun getDefaultRemoteToFetch(repository: GitRepository): GitRemote? {
     val remotes = repository.remotes
@@ -135,8 +123,10 @@ internal class GitFetchSupportImpl(private val project: Project) : GitFetchSuppo
         commonIndicator.checkCanceled()
         lateinit var result: SingleRemoteResult
         ProgressManager.getInstance().executeProcessUnderProgress({
-          commonIndicator.checkCanceled()
-          result = doFetch(repository, remote, authenticationGate)
+                                                                    commonIndicator.checkCanceled()
+                                                                    result = fetchQueue.executeForRemote(repository, remote) {
+                                                                      doFetch(repository, remote, authenticationGate)
+                                                                    }
         }, commonIndicator)
         result
       }
