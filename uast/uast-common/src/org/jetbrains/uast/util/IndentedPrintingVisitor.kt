bd2941640518c03070f6dfbfdfa9ff3bda7f5efc
Nicolay Mitropolsky <nicolay.mitropolsky@jetbrains.com>
2019-11-21 18:03:43 +0300

    [devkit, uast] *Dump UAST Tree By Each PsiElement* action added (IDEA-210615)
    GitOrigin-RevId: 20d5ca7e598c51b21090e269f09d865b0377d858

--- uast/uast-common/src/org/jetbrains/uast/util/IndentedPrintingVisitor.kt
+++ uast/uast-common/src/org/jetbrains/uast/util/IndentedPrintingVisitor.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.uast.test.common
+package org.jetbrains.uast.util
 
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementVisitor
