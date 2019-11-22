0884a17c1ad64a65b1dea2674a75c14452c3a9ec
Dmitry Jemerov <yole@jetbrains.com>
2019-11-22 16:40:54 +0100

    CompositeFoldingBuilder now handles the case of folding descriptors overriding getPlaceholderText()
    GitOrigin-RevId: 9c70bdf7714540f34699aa5105a0707109535580

--- platform/core-api/src/com/intellij/lang/folding/CompositeFoldingBuilder.java
+++ platform/core-api/src/com/intellij/lang/folding/CompositeFoldingBuilder.java
@@ -6,15 +6,13 @@ import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.PossiblyDumbAware;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Used by LanguageFolding class if more than one FoldingBuilder were specified
@@ -125,10 +123,26 @@ public class CompositeFoldingBuilder extends FoldingBuilderEx implements Possibl
             foldingDescriptor.getGroup(),
             foldingDescriptor.getDependencies(),
             foldingDescriptor.isNonExpandable(),
-            foldingDescriptor.getCachedPlaceholderText(),
+            choosePlaceholderText(foldingDescriptor),
             foldingDescriptor.isCollapsedByDefault());
       myFoldingDescriptor = foldingDescriptor;
       myBuilder = builder;
+    }
+
+    private static String choosePlaceholderText(@NotNull FoldingDescriptor foldingDescriptor) {
+      String cachedText = foldingDescriptor.getCachedPlaceholderText();
+      // Some folding descriptors override the getPlaceholderText() method. If they don't, the default implementation
+      // in CompositeFoldingBuilder will return the element text. In this case, we'll need to ensure that the
+      // getPlaceholderText() will be delegate to the folding builder, which we achieve by not storing any cached text.
+      String textFromGetText = foldingDescriptor.getPlaceholderText();
+      boolean placeholderTextIsFallback = Objects.equals(textFromGetText, foldingDescriptor.getElement().getText());
+      if (cachedText == null && placeholderTextIsFallback) {
+        return null;
+      }
+      if (!placeholderTextIsFallback) {
+        return textFromGetText;
+      }
+      return cachedText;
     }
 
     @NotNull
