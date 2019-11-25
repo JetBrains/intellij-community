61bd14e4ff7f07c39e7396fda67d2240d1a8bdbb
Alexander Zolotov <zolotov@jetbrains.com>
2019-11-25 15:57:05 +0300

    Runtime environments: delete redundant key
    GitOrigin-RevId: 00a881264a292849e753b0caba48cdb4d9e98822

--- java/debugger/impl/src/com/intellij/debugger/impl/GenericDebuggerRunner.java
+++ java/debugger/impl/src/com/intellij/debugger/impl/GenericDebuggerRunner.java
@@ -21,7 +21,6 @@ import com.intellij.execution.runners.JavaPatchableProgramRunner;
 import com.intellij.execution.ui.RunContentDescriptor;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.options.SettingsEditor;
-import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.xdebugger.XDebugProcess;
 import com.intellij.xdebugger.XDebugProcessStarter;
@@ -32,7 +31,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class GenericDebuggerRunner extends JavaPatchableProgramRunner<GenericDebuggerRunnerSettings> {
-  public static final Key<Integer> REMOTE_PORT_KEY = Key.create("REMOTE_PORT_KEY");
   @Override
   public boolean canRun(@NotNull final String executorId, @NotNull final RunProfile profile) {
     return executorId.equals(DefaultDebugExecutor.EXECUTOR_ID) && profile instanceof ModuleRunProfile
