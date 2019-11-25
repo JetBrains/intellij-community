641220affe7a0018beafd6ab5e2831643b89c7c6
Sergei Vorobyov <Sergey.Vorobyov@jetbrains.com>
2019-11-25 12:23:17 +0300

    IDEA-CR-55634 renamed SuperCompoundParallelOperationTrace.kt to AnonymousParallelOperationTrace.kt
    GitOrigin-RevId: 198a7ebd5d693678c689bb8fd527356a3a95c40c

--- platform/platform-impl/src/com/intellij/openapi/observable/operations/AnonymousParallelOperationTrace.kt
+++ platform/platform-impl/src/com/intellij/openapi/observable/operations/AnonymousParallelOperationTrace.kt
@@ -1,7 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.observable.operations
 
-class SuperCompoundParallelOperationTrace(debugName: String? = null) {
+class AnonymousParallelOperationTrace(debugName: String? = null) {
   private val delegate = CompoundParallelOperationTrace<Nothing?>(debugName)
 
   fun startOperation() = delegate.startOperation()
