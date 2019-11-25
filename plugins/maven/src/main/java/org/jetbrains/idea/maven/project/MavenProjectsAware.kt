72822d57729d5b36abaa5640ec1164c7ee8ece87
Sergei Vorobyov <Sergey.Vorobyov@jetbrains.com>
2019-11-25 12:39:56 +0300

    IDEA-CR-55634 removed redundant scanning of file system
    GitOrigin-RevId: bf0831a9abe9a04a3cf2254072eb67ca459e3da9

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/project/MavenProjectsAware.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/project/MavenProjectsAware.kt
@@ -9,7 +9,6 @@ import com.intellij.openapi.externalSystem.autoimport.ExternalSystemRefreshStatu
 import com.intellij.openapi.observable.properties.AtomicBooleanProperty
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.io.FileUtil
-import com.intellij.util.PathUtil
 import org.jetbrains.idea.maven.model.MavenConstants
 import org.jetbrains.idea.maven.utils.MavenUtil
 import java.io.File
@@ -36,17 +35,6 @@ class MavenProjectsAware(
     manager.forceUpdateAllProjectsOrFindAllAvailablePomFiles()
   }
 
-  private fun isPomFile(path: String): Boolean {
-    return MavenUtil.isPotentialPomFile(path) && projectsTree.isPotentialProject(path)
-  }
-
-  private fun isProfilesFile(path: String): Boolean {
-    if (PathUtil.getFileName(path) != MavenConstants.PROFILES_XML) return false
-    val profilesDirectory = path.removeSuffix(MavenConstants.PROFILES_XML)
-    val possiblePoms = MavenConstants.POM_NAMES.map { profilesDirectory + it }
-    return possiblePoms.any { projectsTree.isPotentialProject(it) }
-  }
-
   private fun collectAllSettingsFiles(): Set<String> {
     val settingsFiles = ArrayList<String?>()
     settingsFiles.add(manager.generalSettings.effectiveUserSettingsIoFile?.path)
@@ -57,20 +45,24 @@ class MavenProjectsAware(
     return settingsFiles.mapNotNull { FileUtil.toCanonicalPath(it) }.toSet()
   }
 
+  private fun hasPomFile(rootDirectory: String): Boolean {
+    val possiblePoms = MavenConstants.POM_NAMES.map { join(rootDirectory, it) }
+    return possiblePoms.any { projectsTree.isPotentialProject(it) }
+  }
+
   private fun collectAllProjectSettings(): List<String> {
     val settingsFiles = ArrayList<String>()
     for (mavenProject in projectsTree.projects) {
       val rootDirectory = mavenProject.directory
       settingsFiles.addAll(mavenProject.modulePaths)
-      settingsFiles.add(File(rootDirectory, MavenConstants.JVM_CONFIG_RELATIVE_PATH).path)
-      settingsFiles.add(File(rootDirectory, MavenConstants.MAVEN_CONFIG_RELATIVE_PATH).path)
-      settingsFiles.addAll(File(rootDirectory).children.filter { isPomFile(it) || isProfilesFile(it) })
+      settingsFiles.add(join(rootDirectory, MavenConstants.JVM_CONFIG_RELATIVE_PATH))
+      settingsFiles.add(join(rootDirectory, MavenConstants.MAVEN_CONFIG_RELATIVE_PATH))
+      if (hasPomFile(rootDirectory)) settingsFiles.add(join(rootDirectory, MavenConstants.PROFILES_XML))
     }
     return settingsFiles
   }
 
-  private val File.children: List<String>
-    get() = listFiles()?.map { it.path } ?: emptyList()
+  private fun join(parentPath: String, relativePath: String) = File(parentPath, relativePath).path
 
   init {
     manager.addManagerListener(object : MavenProjectsManager.Listener {
