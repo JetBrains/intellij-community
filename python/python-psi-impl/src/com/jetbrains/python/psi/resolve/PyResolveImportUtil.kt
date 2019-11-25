d17915c217993130f3077bbfb63b7c063c0242fd
Aleksei Kniazev <aleksei.kniazev@jetbrains.com>
2019-11-25 20:11:41 +0300

    IDEA-CR-55788: review fixes for skeleton resolve (PY-39171)
    (cherry picked from commit fb625427f270b53aa2fc5d5e813be99a3dd6d992)
    
    (cherry picked from commit 5bf636d52b710f1f4c86934e105a0789504c7101)
    
    GitOrigin-RevId: 31f9c131797cd7a4f10ad57d4fdf3a82f0fc4b50

--- python/python-psi-impl/src/com/jetbrains/python/psi/resolve/PyResolveImportUtil.kt
+++ python/python-psi-impl/src/com/jetbrains/python/psi/resolve/PyResolveImportUtil.kt
@@ -363,16 +363,16 @@ private fun filterTopPriorityResults(resolved: List<PsiElement>, module: Module?
     // stub packages can be partial
     groupedResults.containsKey(Priority.STUB_PACKAGE)
       && groupedResults.headMap(Priority.STUB_PACKAGE).isEmpty() -> firstResultWithFallback(groupedResults, Priority.STUB_PACKAGE)
-    groupedResults.containsKey(Priority.SKELETON) -> firstResultWithFallback(groupedResults, Priority.SKELETON, inverseOrder = true)
+    groupedResults.containsKey(Priority.SKELETON) -> firstResultWithFallback(groupedResults, Priority.SKELETON).reversed()
     else -> listOf(groupedResults.values.first().first())
   }
 }
 
-private fun firstResultWithFallback(results: SortedMap<Priority, MutableList<PsiElement>>, priority: Priority, inverseOrder: Boolean = false): List<PsiElement> {
+private fun firstResultWithFallback(results: SortedMap<Priority, MutableList<PsiElement>>, priority: Priority): List<PsiElement> {
   val first = results[priority]!!.first()
   val nextByPriority = results.tailMap(priority).values.asSequence().drop(1).take(1).flatten().firstOrNull()
 
-  return if (inverseOrder) listOfNotNull(nextByPriority, first) else listOfNotNull(first, nextByPriority)
+  return listOfNotNull(first, nextByPriority)
 }
 
 /**
@@ -391,7 +391,7 @@ private fun resolvedElementPriority(element: PsiElement, module: Module?) = when
 
 fun isInSkeletons(element: PsiElement): Boolean {
   val sdk = PythonSdkUtil.findPythonSdk(element) ?: return false
-  val vFile = (if (element is PsiDirectory) element.virtualFile else element.containingFile.virtualFile) ?: return false
+  val vFile = (if (element is PsiDirectory) element.virtualFile else element.containingFile?.virtualFile) ?: return false
   return PythonSdkUtil.isFileInSkeletons(vFile, sdk)
 }
 
