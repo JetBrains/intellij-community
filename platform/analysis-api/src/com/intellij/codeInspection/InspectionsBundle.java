df428c725767a540075c3ec5b57ea7de49372a61
Petr Golubev <petr.golubev@jetbrains.com>
2019-11-26 15:06:26 +0300

    mv InspectionsBundle.properties to community/platform/analysis-api/src/*
    GitOrigin-RevId: aff26b06258bb5805a881c37eaddcb26273970b8

--- platform/analysis-api/src/com/intellij/codeInspection/InspectionsBundle.java
+++ platform/analysis-api/src/com/intellij/codeInspection/InspectionsBundle.java
@@ -29,7 +29,7 @@ import java.util.ResourceBundle;
 public class InspectionsBundle {
   private static Reference<ResourceBundle> ourBundle;
 
-  public static final String BUNDLE = "messages.InspectionsBundle";
+  public static final String BUNDLE = "com.intellij.codeInspection.InspectionsBundle";
 
   private InspectionsBundle() { }
 
