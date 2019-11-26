0f9e29bbce2ca359081c74fa04a7e0564833584b
Bas Leijdekkers <basleijdekkers@gmail.com>
2019-11-26 16:17:40 +0100

    IG: determine if context is static correctly (IDEA-227475)
    for "Local variable hides field" inspection
    
    GitOrigin-RevId: d0f40e9674457ad95eb330597baaf9416face229

--- plugins/InspectionGadgets/test/com/siyeh/igtest/visibility/local_variable_hiding_member_variable/LocalVariableHidingMemberVariableReportAllFields.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/visibility/local_variable_hiding_member_variable/LocalVariableHidingMemberVariableReportAllFields.java
@@ -50,3 +50,16 @@ class Outer {
     }.run();
   }
 }
+class IdeaBloopers13
+{
+  public static final IdeaBloopers13 staticField2 = new IdeaBloopers13()
+  {
+    public void x()
+    {
+      //IdeaBloopers13.this.stringValue = null;  <-- Java error: non-static variable this cannot be referenced from a static context
+      String <warning descr="Local variable 'stringValue' hides field in class 'null'">stringValue</warning> = ""; //IntellijIdea inspection: Local variable 'stringValue' hides field in class 'IdeaBloopers13'.
+    }
+  };
+
+  private String stringValue;
+}
--- plugins/InspectionGadgets/testsrc/com/siyeh/ig/visibility/LocalVariableHidingMemberVariableInspectionTest.java
+++ plugins/InspectionGadgets/testsrc/com/siyeh/ig/visibility/LocalVariableHidingMemberVariableInspectionTest.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.siyeh.ig.visibility;
 
 import com.intellij.codeInspection.InspectionProfileEntry;
@@ -25,10 +11,7 @@ public class LocalVariableHidingMemberVariableInspectionTest extends LightJavaIn
     doTest();
   }
 
-  public void testLocalVariableHidingMemberVariable2() {
-    final LocalVariableHidingMemberVariableInspection inspection = new LocalVariableHidingMemberVariableInspection();
-    inspection.m_ignoreStaticMethods = false;
-    myFixture.enableInspections(inspection);
+  public void testLocalVariableHidingMemberVariableReportAllFields() {
     doTest();
   }
 
@@ -36,7 +19,10 @@ public class LocalVariableHidingMemberVariableInspectionTest extends LightJavaIn
   @Override
   protected InspectionProfileEntry getInspection() {
     final LocalVariableHidingMemberVariableInspection inspection = new LocalVariableHidingMemberVariableInspection();
-    inspection.m_ignoreStaticMethods = true;
+    if (getTestName(false).endsWith("ReportAllFields")) {
+      inspection.m_ignoreInvisibleFields = false;
+      inspection.m_ignoreStaticMethods = false;
+    }
     return inspection;
   }
 
