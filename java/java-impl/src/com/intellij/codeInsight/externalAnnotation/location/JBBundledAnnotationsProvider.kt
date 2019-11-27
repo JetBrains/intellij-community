f19027dea282ccf56aa343a319c9e0c62bcf0cea
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2019-11-27 17:30:04 +0300

    AnnotationsLocationProvider: add Project as API parameter.
    GitOrigin-RevId: f459081ea5213b4db5b6b503d49dfaf2aed8629c

--- java/java-impl/src/com/intellij/codeInsight/externalAnnotation/location/JBBundledAnnotationsProvider.kt
+++ java/java-impl/src/com/intellij/codeInsight/externalAnnotation/location/JBBundledAnnotationsProvider.kt
@@ -8,6 +8,7 @@ import com.google.gson.stream.JsonToken
 import com.google.gson.stream.JsonWriter
 import com.intellij.ide.extensionResources.ExtensionsRootType
 import com.intellij.ide.plugins.PluginManagerCore
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.libraries.Library
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.util.text.VersionComparatorUtil
@@ -16,7 +17,8 @@ class JBBundledAnnotationsProvider : AnnotationsLocationProvider {
   private val myPluginId = PluginManagerCore.CORE_ID
   private val knownAnnotations: Map<String, Map<VersionRange, AnnotationsLocation>> by lazy { buildAnnotations() }
 
-  override fun getLocations(library: Library,
+  override fun getLocations(project: Project,
+                            library: Library,
                             artifactId: String?,
                             groupId: String?,
                             version: String?): Collection<AnnotationsLocation> {
