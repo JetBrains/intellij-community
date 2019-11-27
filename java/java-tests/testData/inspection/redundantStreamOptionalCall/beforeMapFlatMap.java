c3a89fa80f43da3cc99222690606f3268b7c92aa
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2019-11-27 14:46:59 +0700

    IDEA-227393 Suggest replacing `.map(X).flatMap(Function.identity())` with `.flatMap(X)`
    GitOrigin-RevId: a988dc04828e62c39846c6e6005946f26656be9a

---
+++ java/java-tests/testData/inspection/redundantStreamOptionalCall/beforeMapFlatMap.java
@@ -0,0 +1,11 @@
+// "Remove 'flatMap' call" "true"
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+class Flat {
+  public static Stream<SomeClazz> main(Stream<Stream<SomeClazz>> objectStreams) {
+    return objectStreams.map(Stream::distinct).flatMap<caret>(Function.identity());
+  }
+  private static class SomeClazz {
+  }
+}
