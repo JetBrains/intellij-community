fe55d7d0758198f1a1337509fced001c9fffc20c
Sergei Vorobyov <Sergey.Vorobyov@jetbrains.com>
2019-11-27 15:40:11 +0300

    [auto-import] removed intermediary disposable
    GitOrigin-RevId: db63d026bf2b30cdb8909342022bdd035258eb0f

--- platform/external-system-api/src/com/intellij/openapi/externalSystem/autoimport/ExternalSystemProjectTracker.kt
+++ platform/external-system-api/src/com/intellij/openapi/externalSystem/autoimport/ExternalSystemProjectTracker.kt
@@ -1,13 +1,12 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.externalSystem.autoimport
 
-import com.intellij.openapi.Disposable
 import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.project.Project
 import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Experimental
-interface ExternalSystemProjectTracker : Disposable {
+interface ExternalSystemProjectTracker {
 
   /**
    * Starts tracking of project settings that will be defined by [projectAware]
