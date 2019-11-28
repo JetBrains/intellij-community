0eb556e1a2776798aae7fef22e1dceda72e606ab
sergey.anchipolevsky <sergey.anchipolevsky@jetbrains.com>
2019-11-28 18:28:50 +0300

    Revert: IDEA-199963 Simple workaround to improve formatting for template languages note: workaround worked even worse than original behavior in may cases
    GitOrigin-RevId: 4d02c47ab09b458e4093778c9c0b6a9128fd2b1b

--- plugins/yaml/src/org/jetbrains/yaml/formatter/YAMLFormattingContext.java
+++ plugins/yaml/src/org/jetbrains/yaml/formatter/YAMLFormattingContext.java
@@ -7,7 +7,6 @@ import com.intellij.psi.PsiFile;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
 import com.intellij.psi.formatter.FormatterUtil;
 import com.intellij.psi.impl.source.tree.TreeUtil;
-import com.intellij.psi.templateLanguages.OuterLanguageElement;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.util.PsiUtilCore;
 import com.intellij.util.containers.FactoryMap;
@@ -139,9 +138,6 @@ class YAMLFormattingContext {
 
   @Nullable
   Indent computeBlockIndent(@NotNull ASTNode node) {
-    if (node.getPsi() instanceof OuterLanguageElement) {
-      return DIRECT_NORMAL_INDENT;
-    }
     IElementType nodeType = PsiUtilCore.getElementType(node);
     IElementType parentType = PsiUtilCore.getElementType(node.getTreeParent());
     IElementType grandParentType = parentType == null ? null : PsiUtilCore.getElementType(node.getTreeParent().getTreeParent());
