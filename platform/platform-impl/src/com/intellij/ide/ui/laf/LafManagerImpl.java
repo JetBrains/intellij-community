1c20dd4a2bb01e7c9109cf49949b00f5176169b4
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-12-01 19:04:11 +0300

    use singleton map
    GitOrigin-RevId: 2808fa96456a2c8d56dc67df0c163509439d0978

--- platform/platform-impl/src/com/intellij/ide/ui/laf/LafManagerImpl.java
+++ platform/platform-impl/src/com/intellij/ide/ui/laf/LafManagerImpl.java
@@ -654,8 +654,7 @@ public final class LafManagerImpl extends LafManager implements PersistentStateC
     if (SystemInfo.isMacOSElCapitan) {
       // Text family should be used for relatively small sizes (<20pt), don't change to Display
       // see more about SF https://medium.com/@mach/the-secret-of-san-francisco-fonts-4b5295d9a745#.2ndr50z2v
-      HashMap<AttributedCharacterIterator.Attribute, Object> attributes = new HashMap<>();
-      attributes.put(TextAttribute.KERNING, TextAttribute.KERNING_ON);
+      Map<AttributedCharacterIterator.Attribute, Integer> attributes = Collections.singletonMap(TextAttribute.KERNING, TextAttribute.KERNING_ON);
       Font font = new Font(SystemInfo.isMacOSCatalina ? ".AppleSystemUIFont" : ".SF NS Text", style, size).deriveFont(attributes);
       if (!StartupUiUtil.isDialogFont(font)) {
         return new FontUIResource(font);
