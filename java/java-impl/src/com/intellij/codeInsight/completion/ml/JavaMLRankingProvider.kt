a1614b3696b985dba726b79e170299f0aac1b45c
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2019-12-02 12:57:38 +0300

    [ml-completion] Update Java model
    GitOrigin-RevId: a327a3d58172de15ec290274201a64b6e78f2d56

--- java/java-impl/src/com/intellij/codeInsight/completion/ml/JavaMLRankingProvider.kt
+++ java/java-impl/src/com/intellij/codeInsight/completion/ml/JavaMLRankingProvider.kt
@@ -1,7 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.completion.ml
 
-import com.completion.ranker.model.java.MLWhiteBox
+import com.completion.ranker.model.java.MLCompletionModel
 import com.intellij.internal.ml.*
 import com.intellij.internal.ml.completion.CompletionRankingModelBase
 import com.intellij.internal.ml.completion.JarCompletionModelProvider
@@ -10,7 +10,7 @@ import com.intellij.lang.Language
 class JavaMLRankingProvider : JarCompletionModelProvider("Java", "java_features") {
   override fun createModel(metadata: ModelMetadata): DecisionFunction {
     return object : CompletionRankingModelBase(metadata) {
-      override fun predict(features: DoubleArray?): Double = MLWhiteBox.makePredict(features)
+      override fun predict(features: DoubleArray?): Double = MLCompletionModel.makePredict(features)
     }
   }
 
