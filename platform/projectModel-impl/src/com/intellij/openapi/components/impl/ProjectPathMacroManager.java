730d923718bdd51aa5a63caa6fd7076c459e36a5
Semyon Kolton <semyon.kolton@jetbrains.com>
2019-12-04 12:44:56 +0300

    CPP-18172 Do not expand $PROJECT_NAME$ macro for default project - it doesn't make sense
    (cherry picked from commit 2d7894cb3360a84e3a6e9c16fcd5222417ebcdcf)
    
    GitOrigin-RevId: d23c2affaf1dbceaa438b115516478458dca65c5

--- platform/projectModel-impl/src/com/intellij/openapi/components/impl/ProjectPathMacroManager.java
+++ platform/projectModel-impl/src/com/intellij/openapi/components/impl/ProjectPathMacroManager.java
@@ -32,7 +32,9 @@ public final class ProjectPathMacroManager extends PathMacroManager {
   public ExpandMacroToPathMap getExpandMacroMap() {
     final ExpandMacroToPathMap result = super.getExpandMacroMap();
     addFileHierarchyReplacements(result, PathMacroUtil.PROJECT_DIR_MACRO_NAME, myProject.getBasePath());
-    result.addMacroExpand("PROJECT_NAME", myProject.getName());
+    if (!myProject.isDefault()) {
+      result.addMacroExpand(PathMacroUtil.PROJECT_NAME_MACRO_NAME, myProject.getName());
+    }
     return result;
   }
 
