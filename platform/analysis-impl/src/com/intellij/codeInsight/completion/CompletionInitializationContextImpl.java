2a94d37673d6e304b9012ceb0117d2d14362b4aa
Stanislav Utikeev <stanislav.utikeev@jetbrains.com>
2019-12-05 17:27:33 +0300

    Move CompletionInitializationContextImpl to analysis-impl
    Replace PsiUtilBase.getLanguageInEditor in completion by getting PsiFile language
    
    GitOrigin-RevId: c6cf11d3d283ffbc19181e7099445585d8159f15

--- platform/analysis-impl/src/com/intellij/codeInsight/completion/CompletionInitializationContextImpl.java
+++ platform/analysis-impl/src/com/intellij/codeInsight/completion/CompletionInitializationContextImpl.java
@@ -1,10 +1,10 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.completion;
 
+import com.intellij.lang.Language;
 import com.intellij.openapi.editor.Caret;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.util.PsiUtilBase;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -14,10 +14,11 @@ class CompletionInitializationContextImpl extends CompletionInitializationContex
   private final OffsetsInFile myHostOffsets;
 
   CompletionInitializationContextImpl(Editor editor,
-                                             @NotNull Caret caret,
-                                             PsiFile file,
-                                             CompletionType completionType, int invocationCount) {
-    super(editor, caret, PsiUtilBase.getLanguageInEditor(editor, file.getProject()), file, completionType, invocationCount);
+                                      @NotNull Caret caret,
+                                      Language language,
+                                      PsiFile file,
+                                      CompletionType completionType, int invocationCount) {
+    super(editor, caret, language, file, completionType, invocationCount);
     myHostOffsets = new OffsetsInFile(file, getOffsetMap()).toTopLevelFile();
   }
 
