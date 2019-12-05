e808c494c48474077d20f7729292dd1889cd1dec
Roman.Ivanov <Roman.Ivanov@jetbrains.com>
2019-12-05 18:25:06 +0700

    make PsiRecordComponent field
    GitOrigin-RevId: 78a2c1cde5027a2338f0ede97aea1130445c57da

--- java/java-psi-api/src/com/intellij/psi/PsiRecordComponent.java
+++ java/java-psi-api/src/com/intellij/psi/PsiRecordComponent.java
@@ -1,6 +1,6 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi;
 
-public interface PsiRecordComponent extends PsiJvmMember, PsiVariable {
+public interface PsiRecordComponent extends PsiJvmMember, PsiField {
   PsiRecordComponent[] EMPTY_ARRAY = new PsiRecordComponent[]{};
 }
