66299c626f5b859bf224674167991f7ba08c5ac5
Nicolay Mitropolsky <nicolay.mitropolsky@jetbrains.com>
2019-12-06 16:54:54 +0300

    [intelli-lang] removing deprecated members
    GitOrigin-RevId: bbf4ed4259fc01a4935e977fa20f5086266a5062

--- plugins/IntelliLang/src/org/intellij/plugins/intelliLang/Configuration.java
+++ plugins/IntelliLang/src/org/intellij/plugins/intelliLang/Configuration.java
@@ -478,10 +478,10 @@ public class Configuration extends SimpleModificationTracker implements Persiste
    * @deprecated use {@link #replaceInjectionsWithUndo(Project, PsiFile, List, List, List)},
    * and consider passing non-null {@code hostFile} to make undo-redo registered for this file,
    * especially when {@code psiElementsToRemove} is null (IDEA-109366)
-   * To be removed in IDEA 2019.2
+   * To be removed in IDEA 2020.1
    */
   @Deprecated
-  @ApiStatus.ScheduledForRemoval(inVersion = "2019.2")
+  @ApiStatus.ScheduledForRemoval(inVersion = "2020.1")
   public void replaceInjectionsWithUndo(final Project project,
                                         final List<? extends BaseInjection> newInjections,
                                         final List<? extends BaseInjection> originalInjections,
@@ -506,20 +506,6 @@ public class Configuration extends SimpleModificationTracker implements Persiste
 
   protected void replaceInjectionsWithUndoInner(final List<? extends BaseInjection> add, final List<? extends BaseInjection> remove) {
     replaceInjections(add, remove, false);
-  }
-
-  /**
-   * @deprecated use {@link #replaceInjectionsWithUndo(Project, PsiFile, Object, Object, boolean, List, PairProcessor)},
-   * and consider passing non-null {@code hostFile} to make undo-redo registered for this file,
-   * especially when {@code psiElementsToRemove} is null (IDEA-109366)
-   * To be removed in IDEA 2019.2
-   */
-  @Deprecated
-  @ApiStatus.ScheduledForRemoval(inVersion = "2019.2")
-  public static <T> void replaceInjectionsWithUndo(final Project project, final T add, final T remove,
-                                                   final List<? extends PsiElement> psiElementsToRemove,
-                                                   final PairProcessor<T, T> actualProcessor) {
-    replaceInjectionsWithUndo(project, null, add, remove, true, psiElementsToRemove, actualProcessor);
   }
 
   public static <T> void replaceInjectionsWithUndo(final Project project,
