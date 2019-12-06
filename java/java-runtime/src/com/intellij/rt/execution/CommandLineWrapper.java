6efb86e2d6ae2a8b3a99cbeaa6b81c9884bfa6f5
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-12-06 18:06:32 +0100

    [platform] an assertion against using CommandLineWrapper on Java 9+
    GitOrigin-RevId: 91376fd70fc9991954f8da3bd90c9cb7c801a820

--- java/java-runtime/src/com/intellij/rt/execution/CommandLineWrapper.java
+++ java/java-runtime/src/com/intellij/rt/execution/CommandLineWrapper.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.rt.execution;
 
 import java.io.*;
@@ -27,6 +13,8 @@ import java.util.jar.JarInputStream;
 import java.util.jar.Manifest;
 
 /**
+ * Do not use this command-line method on Java 9+ - use @argfile instead.
+ *
  * @author anna
  * @noinspection SSBasedInspection
  */
@@ -44,6 +32,12 @@ public class CommandLineWrapper {
   }
 
   public static void main(String[] args) throws Exception {
+    try {
+      Class.forName("java.lang.Module");
+      throw new IllegalStateException("`CommandLineWrapper` is ill-suited for launching apps on Java 9+. Please use @arg files instead.");
+    }
+    catch (ClassNotFoundException ignored) { }
+
     File file = new File(args[0]);
     AppData appData = args[0].endsWith(".jar") ? loadMainClassFromClasspathJar(file, args) : loadMainClassWithCustomLoader(file, args);
 
