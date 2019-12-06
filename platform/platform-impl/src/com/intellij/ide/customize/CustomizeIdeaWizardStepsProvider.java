4ce5ad13db236b4368ad636a522d2034402279d3
Konstantin Bulenkov <kb@jetbrains.com>
2019-12-06 17:32:16 +0100

    Update featured plugins for IntelliJ IDEA (MP-2729)
    GitOrigin-RevId: 8367032d9fa7096c51772894b98c85312e86d6a9

--- platform/platform-impl/src/com/intellij/ide/customize/CustomizeIdeaWizardStepsProvider.java
+++ platform/platform-impl/src/com/intellij/ide/customize/CustomizeIdeaWizardStepsProvider.java
@@ -1,9 +1,11 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.customize;
 
+import com.intellij.util.PlatformUtils;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.Map;
 
 public class CustomizeIdeaWizardStepsProvider implements CustomizeIDEWizardStepsProvider {
   @Override
@@ -18,7 +20,22 @@ public class CustomizeIdeaWizardStepsProvider implements CustomizeIDEWizardSteps
       steps.add(new CustomizeLauncherScriptStep());
     }
 
-    PluginGroups groups = new PluginGroups();
+    PluginGroups groups = new PluginGroups() {
+      @Override
+      protected void initFeaturedPlugins(@NotNull Map<String, String> featuredPlugins) {
+        featuredPlugins.put("Scala", "Custom Languages:Plugin for Scala language support:org.intellij.scala");
+        if (PlatformUtils.isIdeaUltimate()) {
+          featuredPlugins.put("Node JS", "Web Development:Support for Node.js projects:NodeJS");
+        }
+        featuredPlugins.put("Grazie", "Spellcheck:Intelligent spelling and grammar checks:tanvd.grazi");
+        featuredPlugins.put("AWS Toolkit", "Cloud Support:Helps you create, test, and debug serverless applications built using the AWS Serverless Application Model:aws.toolkit");
+        featuredPlugins.put("IntelliJ Light Theme", "Themes:IntelliJ Light is a new light theme for IntelliJ-based IDEs:com.jetbrains.lightThemePreview");
+        featuredPlugins.put("Big Data Tools", "Tools Integration:Zeppelin and Spark support:com.intellij.bigdatatools");
+        featuredPlugins.put("EduTools", "Code tools:Learn and teach programming languages such as Kotlin, Java, and Python in the form of coding tasks and custom verification tests:com.jetbrains.edu");
+        featuredPlugins.put("Key Promoter X", "Code tools:Helps you to learn essential shortcuts while you are working:Key Promoter X");
+        addTrainingPlugin(featuredPlugins);
+      }
+    };
     steps.add(new CustomizePluginsStepPanel(groups));
     steps.add(new CustomizeFeaturedPluginsStepPanel(groups));
   }
