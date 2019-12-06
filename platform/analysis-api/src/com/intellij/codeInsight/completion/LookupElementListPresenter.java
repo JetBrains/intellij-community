125ae45d615ffda61a8df49650dd21cd704effda
Stanislav Utikeev <stanislav.utikeev@jetbrains.com>
2019-12-06 15:27:30 +0300

    Move LookupElementListPresenter to analysis-api
    Also remove deprecated LookupImpl.FocusDegree from LookupElementListPresenter
    
    GitOrigin-RevId: 42a8f13b569bae987ce0ae46cd6c955461994709

--- platform/analysis-api/src/com/intellij/codeInsight/completion/LookupElementListPresenter.java
+++ platform/analysis-api/src/com/intellij/codeInsight/completion/LookupElementListPresenter.java
@@ -1,10 +1,8 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.completion;
 
 import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupFocusDegree;
-import com.intellij.codeInsight.lookup.impl.LookupImpl;
-import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -26,13 +24,6 @@ public interface LookupElementListPresenter {
   int getSelectedIndex();
 
   int getLastVisibleIndex();
-
-  /**
-   * @deprecated Use {@link #getLookupFocusDegree()}
-   */
-  @Deprecated
-  @ApiStatus.ScheduledForRemoval(inVersion = "2020.3")
-  LookupImpl.FocusDegree getFocusDegree();
 
   @NotNull
   LookupFocusDegree getLookupFocusDegree();
