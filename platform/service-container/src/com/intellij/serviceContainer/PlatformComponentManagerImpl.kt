JetBrains/intellij-community

commit 9170fa8a9808487e30fcf2625402e0d61e8ca19d

ConstructorParameterResolver - skip String
GitOrigin-RevId: 115640fa0a3e73397602129e1eb02aa7cf8bfca1

platform/service-container/src/com/intellij/serviceContainer/PlatformComponentManagerImpl.kt
@@ -500,11 +500,12 @@ abstract class PlatformComponentManagerImpl @JvmOverloads constructor(internal v
     }
   }
 
-  final override fun <T : Any> instantiateClassWithConstructorInjection(aClass: Class<T>, key: Any, pluginId: PluginId?): T {
+  final override fun <T : Any> instantiateClassWithConstructorInjection(aClass: Class<T>, key: Any, pluginId: PluginId): T {
     return instantiateUsingPicoContainer(aClass, key, pluginId, this, constructorParameterResolver)
   }
 
-  internal open fun isContainerInTestMode() = false
+  internal open val isGetComponentAdapterOfTypeCheckEnabled: Boolean
+    get() = true
 
   final override fun <T : Any> instantiateExtensionWithPicoContainerOnlyIfNeeded(className: String?, pluginDescriptor: PluginDescriptor?): T {
     val pluginId = pluginDescriptor?.pluginId ?: PluginId.getId("unknown")

platform/extensions/src/com/intellij/openapi/components/ComponentManager.java
@@ -136,7 +136,7 @@ public interface ComponentManager extends UserDataHolder, Disposable, AreaInstan
   }
 
   @ApiStatus.Internal
-  default <T> T instantiateClassWithConstructorInjection(@NotNull Class<T> aClass, @NotNull Object key, @Nullable PluginId pluginId) {
+  default <T> T instantiateClassWithConstructorInjection(@NotNull Class<T> aClass, @NotNull Object key, @NotNull PluginId pluginId) {
     //noinspection unchecked
     return (T)new CachingConstructorInjectionComponentAdapter(key, aClass, null, true).getComponentInstance(getPicoContainer());
   }

platform/service-container/src/com/intellij/serviceContainer/ConstructorInjectionComponentAdapter.kt
@@ -1,11 +1,9 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.serviceContainer
 
+import com.intellij.diagnostic.PluginException
 import com.intellij.openapi.extensions.PluginId
 import gnu.trove.THashSet
-import org.picocontainer.PicoInitializationException
-import org.picocontainer.PicoIntrospectionException
-import org.picocontainer.defaults.TooManySatisfiableConstructorsException
 import java.io.File
 import java.lang.reflect.Constructor
 import java.lang.reflect.InvocationTargetException
@@ -13,7 +11,7 @@ import java.nio.file.Path
 
 internal fun <T> instantiateUsingPicoContainer(aClass: Class<*>,
                                                requestorKey: Any,
-                                               pluginId: PluginId?,
+                                               pluginId: PluginId,
                                                componentManager: PlatformComponentManagerImpl,
                                                parameterResolver: ConstructorParameterResolver): T {
   val sortedMatchingConstructors = getSortedMatchingConstructors(aClass)
@@ -48,13 +46,15 @@ internal fun isNotApplicableClass(type: Class<*>): Boolean {
   return type.isPrimitive || type.isEnum || type.isArray ||
          Collection::class.java.isAssignableFrom(type) ||
          Map::class.java.isAssignableFrom(type) ||
-         type === File::class.java || type === Path::class.java
+         type === String::class.java ||
+         type === File::class.java ||
+         type === Path::class.java
 }
 
 private fun getGreediestSatisfiableConstructor(aClass: Class<*>,
                                                sortedMatchingConstructors: Array<Constructor<*>>,
                                                requestorKey: Any,
-                                               pluginId: PluginId?,
+                                               pluginId: PluginId,
                                                componentManager: PlatformComponentManagerImpl,
                                                parameterResolver: ConstructorParameterResolver,
                                                isExtensionSupported: Boolean): Pair<Constructor<*>, Array<Class<*>>> {
@@ -126,7 +126,7 @@ private fun getGreediestSatisfiableConstructor(aClass: Class<*>,
 
   when {
     !conflicts.isNullOrEmpty() -> {
-      throw TooManySatisfiableConstructorsException(aClass, conflicts)
+      throw PluginException("Too many satisfiable constructors: ${sortedMatchingConstructors.joinToString { it.toString() }}", pluginId)
     }
     greediestConstructor != null -> {
       return Pair(greediestConstructor, greediestConstructorParameterTypes!!)
@@ -134,23 +134,18 @@ private fun getGreediestSatisfiableConstructor(aClass: Class<*>,
     !unsatisfiableDependencyTypes.isNullOrEmpty() -> {
       // second (and final) round
       if (isExtensionSupported) {
-        throw PicoIntrospectionException("${aClass.name} has unsatisfied dependency: $unsatisfiedDependencyType among unsatisfiable dependencies: " +
-                                         "$unsatisfiableDependencyTypes where $componentManager was the leaf container being asked for dependencies.")
+        throw PluginException("${aClass.name} has unsatisfied dependency: $unsatisfiedDependencyType among unsatisfiable dependencies: " +
+                              "$unsatisfiableDependencyTypes where $componentManager was the leaf container being asked for dependencies.", pluginId)
       }
       else {
         return getGreediestSatisfiableConstructor(aClass, sortedMatchingConstructors, requestorKey, pluginId, componentManager, parameterResolver, true)
       }
     }
     else -> {
-      throw createNoSatisfiableConstructorError(aClass)
+      throw PluginException("The specified parameters not match any of the following constructors: " +
+                            "${aClass.declaredConstructors.joinToString(separator = "\n") { it.toString() }}\n" + "for $aClass", pluginId)
     }
   }
-}
-
-private fun createNoSatisfiableConstructorError(aClass: Class<*>): RuntimeException {
-  return PicoInitializationException("The specified parameters not match any of the following constructors: " +
-                                     "${aClass.declaredConstructors.joinToString(separator = "\n") { it.toString() }}\n" +
-                                     "for $aClass")
 }
 
 private val constructorComparator = Comparator<Constructor<*>> { c0, c1 -> c1.parameterCount - c0.parameterCount }

platform/service-container/src/com/intellij/serviceContainer/ConstructorParameterResolver.kt
@@ -30,7 +30,7 @@ internal class ConstructorParameterResolver {
                    requestorClass: Class<*>,
                    requestorConstructor: Constructor<*>,
                    expectedType: Class<*>,
-                   pluginId: PluginId?,
+                   pluginId: PluginId,
                    isExtensionSupported: Boolean): Boolean {
     if (isLightService(expectedType) ||
         expectedType === ComponentManager::class.java ||
@@ -45,7 +45,7 @@ internal class ConstructorParameterResolver {
                       requestorClass: Class<*>,
                       requestorConstructor: Constructor<*>,
                       expectedType: Class<*>,
-                      pluginId: PluginId?): Any? {
+                      pluginId: PluginId): Any? {
     if (expectedType === ComponentManager::class.java) {
       return componentManager
     }
@@ -66,18 +66,13 @@ internal class ConstructorParameterResolver {
     }
   }
 
-  private fun handleUnsatisfiedDependency(componentManager: PlatformComponentManagerImpl, requestorClass: Class<*>, expectedType: Class<*>, pluginId: PluginId?): Any? {
+  private fun handleUnsatisfiedDependency(componentManager: PlatformComponentManagerImpl, requestorClass: Class<*>, expectedType: Class<*>, pluginId: PluginId): Any? {
     val extension = componentManager.extensionArea.findExtensionByClass(expectedType) ?: return null
     val message = "Do not use constructor injection to get extension instance (requestorClass=${requestorClass.name}, extensionClass=${expectedType.name})"
     val app = componentManager.getApplication()
     @Suppress("SpellCheckingInspection")
-    if (app != null && app.isUnitTestMode && pluginId?.idString != "org.jetbrains.kotlin" && pluginId?.idString != "Lombook Plugin") {
-      if (pluginId == null) {
-        throw RuntimeException(message)
-      }
-      else {
-        throw PluginException(message, pluginId)
-      }
+    if (app != null && app.isUnitTestMode && pluginId.idString != "org.jetbrains.kotlin" && pluginId.idString != "Lombook Plugin") {
+      throw PluginException(message, pluginId)
     }
     else {
       LOG.warn(message)
@@ -90,7 +85,7 @@ internal class ConstructorParameterResolver {
                                 requestorKey: Any,
                                 requestorClass: Class<*>,
                                 requestorConstructor: Constructor<*>,
-                                @Suppress("UNUSED_PARAMETER") pluginId: PluginId?): ComponentAdapter? {
+                                @Suppress("UNUSED_PARAMETER") pluginId: PluginId): ComponentAdapter? {
     val container = componentManager.picoContainer
     val byKey = container.getComponentAdapter(expectedType)
     if (byKey != null && requestorKey != byKey.componentKey) {
@@ -110,7 +105,7 @@ internal class ConstructorParameterResolver {
       return null
     }
 
-    if (componentManager.getApplication() != null) {
+    if (componentManager.isGetComponentAdapterOfTypeCheckEnabled) {
       LOG.error("getComponentAdapterOfType is used to get ${expectedType.name} (requestorClass=${requestorClass.name}, requestorConstructor=${requestorConstructor})." +
                 "\n\nProbably constructor should be marked as NonInjectable.")
     }

platform/service-container/testSrc/com/intellij/serviceContainer/ConstructorInjectionTest.kt
@@ -1,13 +1,8 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.serviceContainer
 
-import com.intellij.configurationStore.StateStorageManager
-import com.intellij.openapi.components.PersistentStateComponent
 import com.intellij.openapi.components.Service
-import com.intellij.openapi.components.ServiceDescriptor
-import com.intellij.openapi.components.impl.stores.IComponentStore
 import com.intellij.openapi.extensions.DefaultPluginDescriptor
-import com.intellij.util.messages.MessageBus
 import org.junit.Test
 
 class ConstructorInjectionTest {
@@ -15,7 +10,7 @@ class ConstructorInjectionTest {
 
   @Test
   fun `interface extension`() {
-    val componentManager = TestComponentManager()
+    val componentManager = TestComponentManager(isGetComponentAdapterOfTypeCheckEnabled = false)
     val area = componentManager.extensionArea
     val point = area.registerPoint("bar", Bar::class.java, pluginDescriptor)
     @Suppress("DEPRECATION")
@@ -27,7 +22,6 @@ class ConstructorInjectionTest {
   @Test
   fun `resolve light service`() {
     val componentManager = TestComponentManager()
-    componentManager.registerService(IComponentStore::class.java, TestComponentStore::class.java, pluginDescriptor, false)
     componentManager.instantiateClassWithConstructorInjection(BarServiceClient::class.java, BarServiceClient::class.java.name, pluginDescriptor.pluginId)
   }
 }
@@ -42,31 +36,3 @@ private class Foo(@Suppress("UNUSED_PARAMETER") bar: BarImpl)
 private class BarService
 
 private class BarServiceClient(@Suppress("UNUSED_PARAMETER") bar: BarService)
-
-private class TestComponentStore : IComponentStore {
-  override val storageManager: StateStorageManager
-    get() = TODO("not implemented")
-
-  override fun setPath(path: String) {
-  }
-
-  override fun initComponent(component: Any, serviceDescriptor: ServiceDescriptor?) {
-  }
-
-  override fun initPersistencePlainComponent(component: Any, key: String) {
-  }
-
-  override fun reloadStates(componentNames: Set<String>, messageBus: MessageBus) {
-  }
-
-  override fun reloadState(componentClass: Class<out PersistentStateComponent<*>>) {
-  }
-
-  override fun isReloadPossible(componentNames: Set<String>) = false
-
-  override suspend fun save(forceSavingAllSettings: Boolean) {
-  }
-
-  override fun saveComponent(component: PersistentStateComponent<*>) {
-  }
-}

platform/service-container/testSrc/com/intellij/serviceContainer/ServiceContainerTest.kt
@@ -1,8 +1,6 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.serviceContainer
 
-import com.intellij.ide.plugins.IdeaPluginDescriptorImpl
-import com.intellij.openapi.application.Application
 import com.intellij.openapi.components.ComponentManager
 import com.intellij.openapi.extensions.DefaultPluginDescriptor
 import org.assertj.core.api.Assertions.assertThatThrownBy
@@ -23,12 +21,13 @@ class ServiceContainerTest {
     }
       .hasMessageContaining("Cyclic service initialization")
   }
-}
 
-internal class TestComponentManager : PlatformComponentManagerImpl(null, setExtensionsRootArea = false /* must work without */) {
-  override fun getContainerDescriptor(pluginDescriptor: IdeaPluginDescriptorImpl) = pluginDescriptor.appContainerDescriptor
-
-  override fun getApplication(): Application? = null
+  @Test
+  fun `service as data class`() {
+    val componentManager = TestComponentManager()
+    componentManager.registerService(ServiceAsDataClass::class.java, ServiceAsDataClass::class.java, testPluginDescriptor, false)
+    componentManager.getService(ServiceAsDataClass::class.java)
+  }
 }
 
 private class C1(componentManager: ComponentManager) {
@@ -42,3 +41,5 @@ private class C2(componentManager: ComponentManager) {
     componentManager.getService(C1::class.java)
   }
 }
+
+private data class ServiceAsDataClass(var a: String? = null, var b: String? = null)

platform/service-container/testSrc/com/intellij/serviceContainer/TestComponentManager.kt
@@ -1,0 +1,51 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.serviceContainer
+
+import com.intellij.configurationStore.StateStorageManager
+import com.intellij.ide.plugins.IdeaPluginDescriptorImpl
+import com.intellij.openapi.application.Application
+import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.ServiceDescriptor
+import com.intellij.openapi.components.impl.stores.IComponentStore
+import com.intellij.openapi.extensions.DefaultPluginDescriptor
+import com.intellij.util.messages.MessageBus
+
+internal val testPluginDescriptor = DefaultPluginDescriptor("test")
+
+internal class TestComponentManager(override var isGetComponentAdapterOfTypeCheckEnabled: Boolean = true) : PlatformComponentManagerImpl(null, setExtensionsRootArea = false /* must work without */) {
+  init {
+    registerService(IComponentStore::class.java, TestComponentStore::class.java, testPluginDescriptor, false)
+  }
+
+  override fun getContainerDescriptor(pluginDescriptor: IdeaPluginDescriptorImpl) = pluginDescriptor.appContainerDescriptor
+
+  override fun getApplication(): Application? = null
+}
+
+private class TestComponentStore : IComponentStore {
+  override val storageManager: StateStorageManager
+    get() = TODO("not implemented")
+
+  override fun setPath(path: String) {
+  }
+
+  override fun initComponent(component: Any, serviceDescriptor: ServiceDescriptor?) {
+  }
+
+  override fun initPersistencePlainComponent(component: Any, key: String) {
+  }
+
+  override fun reloadStates(componentNames: Set<String>, messageBus: MessageBus) {
+  }
+
+  override fun reloadState(componentClass: Class<out PersistentStateComponent<*>>) {
+  }
+
+  override fun isReloadPossible(componentNames: Set<String>) = false
+
+  override suspend fun save(forceSavingAllSettings: Boolean) {
+  }
+
+  override fun saveComponent(component: PersistentStateComponent<*>) {
+  }
+}

commit e37d32272d4d5060de6be48b938a8154d53340ba

Disable failing check enabled by commit 9170fa8a
Change-Id: Ia687b7ef0bef4983a165386b188568d8d8272b65
Issue: 145809499

platform/service-container/src/com/intellij/serviceContainer/PlatformComponentManagerImpl.kt
@@ -501,7 +501,7 @@ abstract class PlatformComponentManagerImpl @JvmOverloads constructor(internal v
   }
 
   internal open val isGetComponentAdapterOfTypeCheckEnabled: Boolean
-    get() = true
+    get() = false  // Android Studio: b/145809499
 
   final override fun <T : Any> instantiateExtensionWithPicoContainerOnlyIfNeeded(className: String?, pluginDescriptor: PluginDescriptor?): T {
     val pluginId = pluginDescriptor?.pluginId ?: PluginId.getId("unknown")