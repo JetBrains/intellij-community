9ace90be9b76c1d801f2d8ef4e38ac0e93023d44
Nicolay Mitropolsky <nicolay.mitropolsky@jetbrains.com>
2019-12-06 15:32:05 +0300

    [uast] removing the deprecated `UastLiteralReferenceProvider`
    GitOrigin-RevId: 3fc06d00d8852f2f8a21d1cf4c4dfff936a3124f

--- uast/uast-common/src/com/intellij/psi/UastInjectionHostReferenceProvider.kt
+++ uast/uast-common/src/com/intellij/psi/UastInjectionHostReferenceProvider.kt
@@ -21,26 +21,3 @@ abstract class UastInjectionHostReferenceProvider : UastReferenceProvider() {
                                              context: ProcessingContext): Array<PsiReference>
 
 }
-
-/**
- * NOTE: Consider using [UastInjectionHostReferenceProvider] instead, because [PsiLanguageInjectionHost] could
- * correspond not only to [ULiteralExpression] in general case.
- * @see sourceInjectionHost
- */
-@Deprecated("use UastInjectionHostReferenceProvider instead", ReplaceWith("UastInjectionHostReferenceProvider"))
-@ApiStatus.ScheduledForRemoval(inVersion = "2020.1")
-abstract class UastLiteralReferenceProvider : UastReferenceProvider() {
-
-  override val supportedUElementTypes: List<Class<out UElement>> = listOf(ULiteralExpression::class.java)
-
-  override fun getReferencesByElement(element: UElement, context: ProcessingContext): Array<PsiReference> {
-    val uLiteral = element as? ULiteralExpression ?: return PsiReference.EMPTY_ARRAY
-    val host = uLiteral.psiLanguageInjectionHost ?: return PsiReference.EMPTY_ARRAY
-    return getReferencesByULiteral(uLiteral, host, context)
-  }
-
-  abstract fun getReferencesByULiteral(uLiteral: ULiteralExpression,
-                                       host: PsiLanguageInjectionHost,
-                                       context: ProcessingContext): Array<PsiReference>
-
-}
