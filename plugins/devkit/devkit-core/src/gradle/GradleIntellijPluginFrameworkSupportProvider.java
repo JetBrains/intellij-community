7ce587b2cb0278b69502f501f91119d9dea0185c
Yann Cébron <yann.cebron@jetbrains.com>
2019-12-09 10:45:42 +0100

    [devkit] gradle-intellij-plugin fallback 0.4.15
    GitOrigin-RevId: d9bd6e67d08de9614deaef748fcc7b941a52be5c

--- plugins/devkit/devkit-core/src/gradle/GradleIntellijPluginFrameworkSupportProvider.java
+++ plugins/devkit/devkit-core/src/gradle/GradleIntellijPluginFrameworkSupportProvider.java
@@ -56,7 +56,7 @@ public class GradleIntellijPluginFrameworkSupportProvider extends KotlinDslGradl
   private static final String LATEST_GRADLE_VERSION_KEY = "LATEST_GRADLE_VERSION_KEY";
   private static final String LATEST_UPDATING_TIME_KEY = "LATEST_UPDATING_TIME_KEY";
 
-  private static final String FALLBACK_VERSION = "0.4.14";
+  private static final String FALLBACK_VERSION = "0.4.15";
   protected static final String HELP_COMMENT = "// See https://github.com/JetBrains/gradle-intellij-plugin/\n";
 
   private static class Lazy {
