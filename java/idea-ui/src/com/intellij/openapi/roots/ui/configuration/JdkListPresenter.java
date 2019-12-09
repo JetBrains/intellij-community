7950dc719afa39f9d4d4b27669edb2dca2410a52
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2019-12-09 20:40:40 +0100

    jdk-combobox - extract helper
    GitOrigin-RevId: 949acb90f2f7ac59beaa08fce8f4862154d4c463

--- java/idea-ui/src/com/intellij/openapi/roots/ui/configuration/JdkListPresenter.java
+++ java/idea-ui/src/com/intellij/openapi/roots/ui/configuration/JdkListPresenter.java
@@ -134,11 +134,7 @@ public abstract class JdkListPresenter extends ColoredListCellRenderer<JdkComboB
       if (icon1 == null) icon1 = IconUtil.getAddIcon();
       Icon icon = icon1;
       setIcon(icon);
-      //for macOS, let's try removing Bundle internals
-      home = StringUtil.trimEnd(home, "/Contents/Home");
-      home = StringUtil.trimEnd(home, "/Contents/MacOS");
-      home = StringUtil.shortenTextWithEllipsis(home, 50, 30);
-      append(home);
+      append(presentDetectedSdkPath(home));
       if (version == null) version = type.getPresentableName();
       append(" " + version, SimpleTextAttributes.GRAYED_ATTRIBUTES);
     }
@@ -195,5 +191,14 @@ public abstract class JdkListPresenter extends ColoredListCellRenderer<JdkComboB
     else {
       customizeCellRenderer(list, new NoneJdkComboBoxItem(), index, selected, hasFocus);
     }
+  }
+
+  @NotNull
+  public static String presentDetectedSdkPath(@NotNull String home) {
+    //for macOS, let's try removing Bundle internals
+    home = StringUtil.trimEnd(home, "/Contents/Home");
+    home = StringUtil.trimEnd(home, "/Contents/MacOS");
+    home = StringUtil.shortenTextWithEllipsis(home, 50, 30);
+    return home;
   }
 }
