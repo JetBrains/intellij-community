617c99e71a2f45a6edd66f1aff10af20339fb99f
Yann Cébron <yann.cebron@jetbrains.com>
2019-12-10 17:38:45 +0100

    DomExtender: JavaDoc, cleanup
    GitOrigin-RevId: 5f979331cf701bdc0abf53389e8cd08ee8d1ad68

--- xml/dom-impl/src/com/intellij/util/xml/reflect/DomExtenderEP.java
+++ xml/dom-impl/src/com/intellij/util/xml/reflect/DomExtenderEP.java
@@ -1,28 +1,11 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.xml.reflect;
 
 import com.intellij.diagnostic.PluginException;
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.extensions.AbstractExtensionPointBean;
-import com.intellij.openapi.extensions.ExtensionPointListener;
-import com.intellij.openapi.extensions.ExtensionPointName;
-import com.intellij.openapi.extensions.PluginDescriptor;
+import com.intellij.openapi.extensions.*;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.stubs.StubIndex;
 import com.intellij.util.xml.impl.DomInvocationHandler;
@@ -34,6 +17,7 @@ import org.jetbrains.annotations.Nullable;
 
 /**
  * @author peter
+ * @see DomExtender
  */
 public class DomExtenderEP extends AbstractExtensionPointBean {
 
@@ -62,8 +46,11 @@ public class DomExtenderEP extends AbstractExtensionPointBean {
     }, app);
   }
 
+  @RequiredElement
   @Attribute("domClass")
   public String domClassName;
+
+  @RequiredElement
   @Attribute("extenderClass")
   public String extenderClassName;
 
