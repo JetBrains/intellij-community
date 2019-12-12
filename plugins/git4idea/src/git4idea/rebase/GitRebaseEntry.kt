065d9f98eb1eab02814ab0db5fd7699246ba5637
Nikolay Rykunov <Nikolay.Rykunov@jetbrains.com>
2019-12-12 13:11:33 +0300

    [git] Interactive Rebase: Initialize knownActions lazily
    If knownActions is initialized statically, NPE may occur (if we use one of the objects before using knownActions).
    
    GitOrigin-RevId: 66e40a82268da92240373fbdecca50ec75d84d05

--- plugins/git4idea/src/git4idea/rebase/GitRebaseEntry.kt
+++ plugins/git4idea/src/git4idea/rebase/GitRebaseEntry.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package git4idea.rebase
 
 import com.intellij.vcs.log.VcsCommitMetadata
@@ -37,7 +23,9 @@ internal open class GitRebaseEntry(var action: Action, val commit: String, val s
 
     companion object {
       @JvmStatic
-      val knownActions = listOf(PICK, EDIT, DROP, SQUASH, REWORD, FIXUP)
+      val knownActions by lazy {
+        listOf(PICK, EDIT, DROP, SQUASH, REWORD, FIXUP)
+      }
 
       @JvmStatic
       fun getKnownActionsArray(): Array<Action> = knownActions.toTypedArray()
