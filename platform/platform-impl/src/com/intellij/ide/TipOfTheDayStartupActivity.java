b6a67d820826d9eb1a123bb683d26f395e9f74c6
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2019-12-12 21:01:04 +0300

    IDEA-225867 Tips of the day: show tips on startup only once per day
    GitOrigin-RevId: 8257a2c05594dfd1e355966e0974ee7afae37f77

--- platform/platform-impl/src/com/intellij/ide/TipOfTheDayStartupActivity.java
+++ platform/platform-impl/src/com/intellij/ide/TipOfTheDayStartupActivity.java
@@ -6,60 +6,25 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
-import com.intellij.openapi.ui.popup.JBPopupFactory;
-import com.intellij.openapi.wm.ex.WindowManagerEx;
-import com.intellij.openapi.wm.impl.FloatingDecorator;
-import com.intellij.openapi.wm.impl.IdeFrameImpl;
 import com.intellij.util.Alarm;
-import com.intellij.util.text.DateFormatUtil;
 import org.jetbrains.annotations.NotNull;
 
-import javax.swing.*;
-import java.util.stream.Stream;
-
 public final class TipOfTheDayStartupActivity implements StartupActivity.DumbAware {
-  private static final long minInitialDelay = 15 * DateFormatUtil.MINUTE;
-  private static final long maxInitialDelay = 45 * DateFormatUtil.MINUTE;
-  private static final long userInactivityDelay = DateFormatUtil.MINUTE;
+
+  private Alarm myAlarm;
 
   @Override
   public void runActivity(@NotNull final Project project) {
-    if (ApplicationManager.getApplication().isUnitTestMode()) return;
+    if (ApplicationManager.getApplication().isUnitTestMode() || myAlarm != null) return;
 
-    //Random initial delay before start tracking user activity
-    Alarm initializer = new Alarm(project);
-    initializer.addRequest(() -> initActivityChecker(project),
-                           (minInitialDelay + Math.round(Math.random() * (maxInitialDelay - minInitialDelay))), ModalityState.any());
-  }
-
-  private static void initActivityChecker(@NotNull Project project) {
-    if (project.isDisposed()) return;
-
-    Alarm myAlarm = new Alarm(project);
-    Runnable showTipsRunnable = () -> showTipsIfNeed(project);
-
-    // After some period of inactivity when user don't type and don't click we try to catch a good moment to show "Tips of the Day"
-    IdeEventQueue.getInstance().addActivityListener(() -> {
-      myAlarm.cancelAllRequests();
-      myAlarm.addRequest(showTipsRunnable, userInactivityDelay, ModalityState.NON_MODAL);
-    }, project);
+    myAlarm = new Alarm(project);
+    myAlarm.addRequest(() -> showTipsIfNeed(project), 5000, ModalityState.any());
   }
 
   private static void showTipsIfNeed(@NotNull Project project) {
-    if (project.isDisposed()) return;
-
-    if (TipDialog.canBeShownAutomaticallyNow()) {
-      IdeFrameImpl frame = WindowManagerEx.getInstanceEx().getFrame(project);
-      if (frame != null
-          && frame.isActive()//project frame has to be active now
-          && Stream.of(frame.getOwnedWindows())
-            .noneMatch(window -> window instanceof JDialog && !(window instanceof FloatingDecorator) && window.isShowing())//no dialogs above the frame
-          && !JBPopupFactory.getInstance().isPopupActive()//no popups above the frame
-          && !IdeTooltipManager.getInstance().hasCurrent()//no tooltips are shown
-      ) {
+    if (!project.isDisposed() && TipDialog.canBeShownAutomaticallyNow()) {
         TipsOfTheDayUsagesCollector.triggerShow("automatically");
         TipDialog.showForProject(project);
-      }
     }
   }
 }
