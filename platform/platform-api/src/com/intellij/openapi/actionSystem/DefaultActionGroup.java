3eb991692e2d962e29cd69fbf3f59a0e76ad1be8
Vassiliy.Kudryashov <vassiliy.kudryashov@jetbrains.com>
2019-12-13 23:25:27 +0300

    IDEA-CR-56591: IDEA-228450 Run buttons disappear from toolbar on restart
    (cherry picked from commit 3e817922bb1a748f1c0000e5d13fc8c4dce368ae)
    
    GitOrigin-RevId: 1cd62b76fca6d01cf7ea7d840cf4eb62a8f99968

--- platform/platform-api/src/com/intellij/openapi/actionSystem/DefaultActionGroup.java
+++ platform/platform-api/src/com/intellij/openapi/actionSystem/DefaultActionGroup.java
@@ -40,6 +40,7 @@ public class DefaultActionGroup extends ActionGroup {
    * Contains instances of Pair
    */
   private final List<Pair<AnAction, Constraints>> myPairs = ContainerUtil.createLockFreeCopyOnWriteList();
+  private int myModificationStamp = 0;
 
   public DefaultActionGroup() {
     this(null, false);
@@ -72,6 +73,14 @@ public class DefaultActionGroup extends ActionGroup {
     super(shortName, popup);
   }
 
+  private void incrementModificationStamp() {
+    myModificationStamp++;
+  }
+
+  public int getModificationStamp() {
+    return myModificationStamp;
+  }
+
   private void addActions(@NotNull List<? extends AnAction> actions) {
     Set<Object> actionSet = new HashSet<>();
     List<AnAction> uniqueActions = new ArrayList<>(actions.size());
@@ -84,6 +93,7 @@ public class DefaultActionGroup extends ActionGroup {
       uniqueActions.add(action);
     }
     mySortedChildren.addAll(uniqueActions);
+    incrementModificationStamp();
   }
 
   /**
@@ -158,7 +168,7 @@ public class DefaultActionGroup extends ActionGroup {
       myPairs.add(Pair.create(action, constraint));
     }
     addAllToSortedList(actionManager);
-
+    incrementModificationStamp();
     return new ActionInGroup(this, action);
   }
 
@@ -237,6 +247,7 @@ public class DefaultActionGroup extends ActionGroup {
         Pair<AnAction, Constraints> pair = myPairs.get(i);
         if (pair.first.equals(action) || (pair.first instanceof ActionStub && ((ActionStub)pair.first).getId().equals(id))) {
           myPairs.remove(i);
+          incrementModificationStamp();
           break;
         }
       }
@@ -249,6 +260,7 @@ public class DefaultActionGroup extends ActionGroup {
   public final void removeAll() {
     mySortedChildren.clear();
     myPairs.clear();
+    incrementModificationStamp();
   }
 
   /**
@@ -258,6 +270,7 @@ public class DefaultActionGroup extends ActionGroup {
     int index = mySortedChildren.indexOf(oldAction);
     if (index >= 0) {
       mySortedChildren.set(index, newAction);
+      incrementModificationStamp();
       return true;
     }
     else {
@@ -265,6 +278,7 @@ public class DefaultActionGroup extends ActionGroup {
         Pair<AnAction, Constraints> pair = myPairs.get(i);
         if (pair.first.equals(newAction)) {
           myPairs.set(i, Pair.create(newAction, pair.second));
+          incrementModificationStamp();
           return true;
         }
       }
@@ -285,6 +299,7 @@ public class DefaultActionGroup extends ActionGroup {
 
     myPairs.clear();
     myPairs.addAll(other.myPairs);
+    incrementModificationStamp();
   }
 
   @NotNull
