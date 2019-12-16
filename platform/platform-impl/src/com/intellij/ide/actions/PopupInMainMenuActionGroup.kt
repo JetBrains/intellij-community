8374a4974d13a4b1578d09ba7108bed04959728e
Dmitry Jemerov <yole@jetbrains.com>
2019-12-16 15:35:57 +0100

    Don't show Paste menu as group in popup menus (avoid submenu with single item)
    GitOrigin-RevId: 4d2341dfc151cb5abf663dab12749db1c1d8d8c6

--- platform/platform-impl/src/com/intellij/ide/actions/PopupInMainMenuActionGroup.kt
+++ platform/platform-impl/src/com/intellij/ide/actions/PopupInMainMenuActionGroup.kt
@@ -1,10 +1,9 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.refactoring.actions
+package com.intellij.ide.actions
 
-import com.intellij.ide.actions.NonTrivialActionGroup
 import com.intellij.openapi.actionSystem.ActionPlaces
 
-class ExtractActionGroup : NonTrivialActionGroup() {
+class PopupInMainMenuActionGroup : NonTrivialActionGroup() {
   override fun isPopup(place: String): Boolean {
     return place == ActionPlaces.MAIN_MENU
   }
