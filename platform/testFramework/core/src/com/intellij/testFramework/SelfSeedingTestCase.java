348a83f3860025234532c29e6700750bf8429523
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2019-12-16 17:22:10 +0300

    English and minor refactorings
    GitOrigin-RevId: 959ca48777ae92df9b5176fc56c5efc9cedad8f7

--- platform/testFramework/core/src/com/intellij/testFramework/SelfSeedingTestCase.java
+++ platform/testFramework/core/src/com/intellij/testFramework/SelfSeedingTestCase.java
@@ -4,9 +4,13 @@ package com.intellij.testFramework;
 import com.intellij.TestCaseLoader;
 
 /**
- * Test classes marked with this interface are not seeded in buckets in {@link TestCaseLoader#addClassIfTestCase(java.lang.Class, java.lang.String) normal way}.
- * Such classes supposed to seed tests by themselves with
+ * Test classes marked with this interface are not seeded in buckets in the {@link TestCaseLoader#addClassIfTestCase(java.lang.Class, java.lang.String) normal way}.
+ * Such classes are supposed to seed tests by themselves with the
  * {@link TestCaseLoader#shouldBucketTests()} and {@link TestCaseLoader#matchesCurrentBucket(java.lang.String)} methods.
  */
 public interface SelfSeedingTestCase {
+  static void assertParentOf(Class<?> clazz) {
+    assert SelfSeedingTestCase.class.isAssignableFrom(clazz) :
+      "To make tests balancing work properly on the TeamCity, the test class should implement " + SelfSeedingTestCase.class.getName();
+  }
 }
