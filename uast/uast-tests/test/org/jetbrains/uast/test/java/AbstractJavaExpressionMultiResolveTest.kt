46b8dbe4c21e72efb9e7543a6fa3c0609273985c
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2019-12-16 22:22:58 +0300

    UAST: JavaConstructorUCallExpression multiResolve() must resolve to PsiMethod in case there is explicit constructor
    GitOrigin-RevId: 3ebd0a55953d0482638287aa8b90cdb386f67e9b

---
+++ uast/uast-tests/test/org/jetbrains/uast/test/java/AbstractJavaExpressionMultiResolveTest.kt
@@ -0,0 +1,45 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.uast.test.java
+
+import com.intellij.psi.PsiCodeBlock
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiIdentifier
+import com.intellij.psi.PsiModifierListOwner
+import org.jetbrains.uast.*
+import org.jetbrains.uast.test.common.visitUFileAndGetResult
+import org.jetbrains.uast.test.env.assertEqualsToFile
+import org.jetbrains.uast.util.IndentedPrintingVisitor
+import java.io.File
+
+abstract class AbstractJavaExpressionMultiResolveTest : AbstractJavaUastTest() {
+  private fun UFile.resolvableWithTargets() = object : IndentedPrintingVisitor(PsiCodeBlock::class, PsiModifierListOwner::class) {
+    override fun render(element: PsiElement): CharSequence? =
+      element
+        .takeIf { it !is PsiIdentifier } // no sense to handle PsiIdentifier, see IDEA-207979
+        ?.toUElementOfType<UExpression>()?.let { ref ->
+          StringBuilder().apply {
+            val parent = ref.uastParent
+            append(parent?.asLogString())
+            append(" -> ")
+            append(ref.asLogString())
+            append(" -> ")
+            append(multiResolve(ref))
+          }
+        }
+  }.visitUFileAndGetResult(this)
+
+  private fun multiResolve(ref: UExpression): String {
+    if (ref is UMultiResolvable) {
+      return "[M] " + ref.multiResolve().joinToString(",") { it.element?.javaClass?.simpleName ?: "null" }
+    }
+    if (ref is UResolvable) {
+      return "[S] " + (ref.resolve()?.toString() ?: "null")
+    }
+    return "null"
+  }
+
+  override fun check(testName: String, file: UFile) {
+    val expected = File(testDataPath, testName.substringBeforeLast('.') + ".resolved.txt")
+    assertEqualsToFile("resolved", expected, file.resolvableWithTargets())
+  }
+}
