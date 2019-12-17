edb78e38fa5c6b5d408c8d1db257355854a1f960
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-12-17 09:46:06 +0100

    JavaPatchableProgramRunner as async runner
    GitOrigin-RevId: 961e3b53baa6f6fb565254e65155f39368bdce6a

--- java/execution/openapi/src/com/intellij/execution/runners/JavaPatchableProgramRunner.java
+++ java/execution/openapi/src/com/intellij/execution/runners/JavaPatchableProgramRunner.java
@@ -5,10 +5,16 @@ import com.intellij.execution.ExecutionException;
 import com.intellij.execution.Executor;
 import com.intellij.execution.configurations.JavaParameters;
 import com.intellij.execution.configurations.RunProfile;
+import com.intellij.execution.configurations.RunProfileState;
 import com.intellij.execution.configurations.RunnerSettings;
+import com.intellij.execution.ui.RunContentDescriptor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.concurrency.Promise;
+import org.jetbrains.concurrency.Promises;
 
-public abstract class JavaPatchableProgramRunner<Settings extends RunnerSettings> extends GenericProgramRunner<Settings> {
-  public abstract void patch(JavaParameters javaParameters, RunnerSettings settings, RunProfile runProfile, final boolean beforeExecution) throws ExecutionException;
+public abstract class JavaPatchableProgramRunner<Settings extends RunnerSettings> extends AsyncProgramRunner<Settings> {
+  public abstract void patch(JavaParameters javaParameters, RunnerSettings settings, RunProfile runProfile, final boolean beforeExecution)
+    throws ExecutionException;
 
   protected static void runCustomPatchers(JavaParameters javaParameters, Executor executor, RunProfile runProfile) {
     if (runProfile != null) {
@@ -17,4 +23,13 @@ public abstract class JavaPatchableProgramRunner<Settings extends RunnerSettings
       });
     }
   }
+
+  @NotNull
+  @Override
+  protected final Promise<RunContentDescriptor> execute(@NotNull ExecutionEnvironment environment, @NotNull RunProfileState state)
+    throws ExecutionException {
+    return Promises.resolvedPromise(doExecute(state, environment));
+  }
+
+  protected abstract RunContentDescriptor doExecute(@NotNull RunProfileState state, @NotNull ExecutionEnvironment environment) throws ExecutionException;
 }
