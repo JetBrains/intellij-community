a1c8e561a74879a6d8d128df86986f8f60859db0
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-12-17 20:09:44 +0300

    Revert "more dynamic bundles"
    This reverts commit fca0e915
    
    GitOrigin-RevId: fc2eec2fbf6ce48c3d446e04d799299558cd76c3

--- platform/platform-api/src/com/intellij/idea/ActionsBundle.java
+++ platform/platform-api/src/com/intellij/idea/ActionsBundle.java
@@ -15,36 +15,48 @@
  */
 package com.intellij.idea;
 
-import com.intellij.DynamicBundle;
+import com.intellij.CommonBundle;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.PropertyKey;
 
+import java.lang.ref.Reference;
+import java.lang.ref.SoftReference;
+import java.util.ResourceBundle;
+
 /**
  * @author Eugene Zhuravlev
  */
-public class ActionsBundle extends DynamicBundle {
-  @NonNls private static final String IDEA_ACTIONS_BUNDLE = "messages.ActionsBundle";
-
-  private static final ActionsBundle ourInstance = new ActionsBundle();
-
-  private ActionsBundle() {
-    super(IDEA_ACTIONS_BUNDLE);
-  }
+public class ActionsBundle {
 
   public static String message(@NotNull @PropertyKey(resourceBundle = IDEA_ACTIONS_BUNDLE) String key, @NotNull Object... params) {
-    return ourInstance.getMessage(key, params);
+    return CommonBundle.message(getBundle(), key, params);
   }
 
+  private static Reference<ResourceBundle> ourBundle;
+  @NonNls private static final String IDEA_ACTIONS_BUNDLE = "messages.ActionsBundle";
+
+  @SuppressWarnings({"UnresolvedPropertyKey"})
   public static String actionText(@NonNls String actionId) {
     return message("action." + actionId + ".text");
   }
 
+  @SuppressWarnings({"UnresolvedPropertyKey"})
   public static String groupText(@NonNls String actionId) {
     return message("group." + actionId + ".text");
   }
 
+  @SuppressWarnings({"UnresolvedPropertyKey"})
   public static String actionDescription(@NonNls String actionId) {
     return message("action." + actionId + ".description");
+  }
+
+  private static ResourceBundle getBundle() {
+    ResourceBundle bundle = com.intellij.reference.SoftReference.dereference(ourBundle);
+    if (bundle == null) {
+      bundle = ResourceBundle.getBundle(IDEA_ACTIONS_BUNDLE);
+      ourBundle = new SoftReference<>(bundle);
+    }
+    return bundle;
   }
 }
--- platform/platform-api/src/com/intellij/openapi/application/ApplicationBundle.java
+++ platform/platform-api/src/com/intellij/openapi/application/ApplicationBundle.java
@@ -15,23 +15,36 @@
  */
 package com.intellij.openapi.application;
 
-import com.intellij.DynamicBundle;
+import com.intellij.CommonBundle;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.PropertyKey;
 
+import java.lang.ref.Reference;
+import java.lang.ref.SoftReference;
+import java.util.ResourceBundle;
+
 /**
  * Provides access to localized properties of the IntelliJ Platform.
  */
-public class ApplicationBundle extends DynamicBundle {
-  @NonNls public static final String BUNDLE = "messages.ApplicationBundle";
-  private static final ApplicationBundle ourInstance = new ApplicationBundle();
+public class ApplicationBundle {
 
   public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, @NotNull Object... params) {
-    return ourInstance.getMessage(key, params);
+    return CommonBundle.message(getBundle(), key, params);
   }
 
+  private static Reference<ResourceBundle> ourBundle;
+  @NonNls public static final String BUNDLE = "messages.ApplicationBundle";
+
   private ApplicationBundle() {
-    super(BUNDLE);
+  }
+
+  private static ResourceBundle getBundle() {
+    ResourceBundle bundle = com.intellij.reference.SoftReference.dereference(ourBundle);
+    if (bundle == null) {
+      bundle = ResourceBundle.getBundle(BUNDLE);
+      ourBundle = new SoftReference<>(bundle);
+    }
+    return bundle;
   }
 }
