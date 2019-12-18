02f7d0cc080982669ff33d36fedb69cc3065cf00
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-12-19 00:03:38 +0300

    more dynamic bundles
    GitOrigin-RevId: 98b3edadb6a2065600efe0e9a3c410f4a3f1bce8

--- platform/tasks-platform-impl/src/com/intellij/tasks/TaskBundle.java
+++ platform/tasks-platform-impl/src/com/intellij/tasks/TaskBundle.java
@@ -1,39 +1,18 @@
 package com.intellij.tasks;
 
-import com.intellij.CommonBundle;
-import com.intellij.reference.SoftReference;
+import com.intellij.DynamicBundle;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.PropertyKey;
 
-import java.lang.ref.Reference;
-import java.util.ResourceBundle;
-
-/**
- * Contains common and repository specific messages for "Tasks and Contexts" subsystem.
- * Initialization logic follows the same pattern as most of the other bundles in project.
- *
- * @author Mikhail Golubev
- */
-public class TaskBundle {
-
-  private static Reference<ResourceBundle> ourBundle;
+public class TaskBundle extends DynamicBundle {
   @NonNls private static final String BUNDLE = "com.intellij.tasks.TaskBundle";
+  private static final TaskBundle INSTANCE = new TaskBundle();
 
-  private TaskBundle() {
-    // empty
-  }
+  private TaskBundle() { super(BUNDLE); }
 
+  @NotNull
   public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, @NotNull Object... params) {
-    return CommonBundle.message(getBundle(), key, params);
-  }
-
-  private static ResourceBundle getBundle() {
-    ResourceBundle bundle = SoftReference.dereference(ourBundle);
-    if (bundle == null) {
-      bundle = ResourceBundle.getBundle(BUNDLE);
-      ourBundle = new SoftReference<>(bundle);
-    }
-    return bundle;
+    return INSTANCE.getMessage(key, params);
   }
 }
