b7eecceba0a0089c1b5d2f6bfed703359c576bfc
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2019-12-18 17:45:21 +0300

    LibraryDependentToolWindow: rethrow ProcessCanceledException from getLibrarySearchHelper()
    GitOrigin-RevId: cdb732cad44cf0984bf504a0270d1d4bd8131911

--- platform/platform-api/src/com/intellij/openapi/wm/ext/LibraryDependentToolWindow.java
+++ platform/platform-api/src/com/intellij/openapi/wm/ext/LibraryDependentToolWindow.java
@@ -4,13 +4,15 @@ package com.intellij.openapi.wm.ext;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.extensions.ExtensionPointName;
+import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.wm.ToolWindowEP;
 import com.intellij.util.xmlb.annotations.Attribute;
 
 public final class LibraryDependentToolWindow extends ToolWindowEP {
   private static final Logger LOG = Logger.getInstance(LibraryDependentToolWindow.class);
 
-  public static final ExtensionPointName<LibraryDependentToolWindow> EXTENSION_POINT_NAME = new ExtensionPointName<>("com.intellij.library.toolWindow");
+  public static final ExtensionPointName<LibraryDependentToolWindow> EXTENSION_POINT_NAME =
+    new ExtensionPointName<>("com.intellij.library.toolWindow");
 
   private LibrarySearchHelper myLibrarySearchHelper;
 
@@ -20,9 +22,13 @@ public final class LibraryDependentToolWindow extends ToolWindowEP {
   public LibrarySearchHelper getLibrarySearchHelper() {
     if (myLibrarySearchHelper == null) {
       try {
-        myLibrarySearchHelper = ApplicationManager.getApplication().instantiateExtensionWithPicoContainerOnlyIfNeeded(librarySearchClass, pluginDescriptor);
+        myLibrarySearchHelper =
+          ApplicationManager.getApplication().instantiateExtensionWithPicoContainerOnlyIfNeeded(librarySearchClass, pluginDescriptor);
       }
-      catch(Exception e) {
+      catch (ProcessCanceledException e) {
+        throw e;
+      }
+      catch (Exception e) {
         LOG.error(e);
         return null;
       }
