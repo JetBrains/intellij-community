583831dc700381de5dd228c3e1013e1da895e476
Konstantin Bulenkov <kb@jetbrains.com>
2019-12-20 01:45:03 +0300

    fix huge empty space at right
    GitOrigin-RevId: 8b50867f0e71951ad5ed0195cab071c682e74ca5

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/status/MemoryUsagePanel.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/status/MemoryUsagePanel.java
@@ -7,7 +7,6 @@ import com.intellij.openapi.wm.StatusBar;
 import com.intellij.ui.Gray;
 import com.intellij.ui.JBColor;
 import com.intellij.ui.UIBundle;
-import com.intellij.ui.scale.JBUIScale;
 import com.intellij.util.concurrency.EdtExecutorService;
 import com.intellij.util.ui.JBUI;
 import com.intellij.util.ui.UIUtil;
@@ -28,21 +27,18 @@ import static com.intellij.openapi.util.io.FileUtilRt.MEGABYTE;
 public final class MemoryUsagePanel extends TextPanel implements CustomStatusBarWidget, Activatable {
   public static final String WIDGET_ID = "Memory";
 
-  private static final int INDENT = 6;
   private static final Color USED_COLOR = JBColor.namedColor("MemoryIndicator.usedBackground", new JBColor(Gray._185, Gray._110));
   private static final Color UNUSED_COLOR = JBColor.namedColor("MemoryIndicator.allocatedBackground", new JBColor(Gray._215, Gray._90));
 
-  private final String mySample;
   private long myLastAllocated = -1;
   private long myLastUsed = -1;
   private ScheduledFuture<?> myFuture;
+  private final long myMaxMemory = Math.min(Runtime.getRuntime().maxMemory() / MEGABYTE, 9999);
 
   public MemoryUsagePanel() {
-    long max = Math.min(Runtime.getRuntime().maxMemory() / MEGABYTE, 9999);
-    mySample = UIBundle.message("memory.usage.panel.message.text", max, max);
-
     setOpaque(false);
     setFocusable(false);
+    setTextAlignment(CENTER_ALIGNMENT);
 
     addMouseListener(new MouseAdapter() {
       @Override
@@ -104,7 +100,7 @@ public final class MemoryUsagePanel extends TextPanel implements CustomStatusBar
   @Override
   public void paintComponent(Graphics g) {
     Dimension size = getSize();
-    int barWidth = size.width - INDENT;
+    int barWidth = size.width;
 
     Runtime rt = Runtime.getRuntime();
     long maxMem = rt.maxMemory();
@@ -117,37 +113,24 @@ public final class MemoryUsagePanel extends TextPanel implements CustomStatusBar
 
     // background
     g.setColor(UIUtil.getPanelBackground());
-    g.fillRect(0, 0, barWidth, size.height);
+    g.fillRect(0, 0, barWidth, size.height - 1);
 
     // gauge (used)
     g.setColor(USED_COLOR);
-    g.fillRect(0, 0, usedBarLength, size.height);
+    g.fillRect(0, 0, usedBarLength, size.height - 1);
 
     // gauge (unused)
     g.setColor(UNUSED_COLOR);
-    g.fillRect(usedBarLength, 0, unusedBarLength, size.height);
+    g.fillRect(usedBarLength, 0, unusedBarLength, size.height - 1);
 
     //text
     super.paintComponent(g);
   }
 
   @Override
-  public Dimension getPreferredSize() {
-    FontMetrics metrics = getFontMetrics(getFont());
-    Insets insets = getInsets();
-    int width = metrics.stringWidth(mySample) + insets.left + insets.right + JBUIScale.scale(2) + INDENT;
-    int height = metrics.getHeight() + insets.top + insets.bottom + JBUIScale.scale(2);
-    return new Dimension(width, height);
-  }
-
-  @Override
-  public Dimension getMinimumSize() {
-    return getPreferredSize();
-  }
-
-  @Override
-  public Dimension getMaximumSize() {
-    return getPreferredSize();
+  protected String getTextForPreferredSize() {
+    long sample = myMaxMemory < 1000 ? 999 : 9999;
+    return " " + UIBundle.message("memory.usage.panel.message.text", sample, sample);
   }
 
   private void updateState() {
