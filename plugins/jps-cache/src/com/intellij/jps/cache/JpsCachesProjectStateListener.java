2b9ee46d4ea00de45d5faad6eaa9e8bea0756cc7
Mikhail Mazurkevich <mikhail.mazurkevich@jetbrains.com>
2019-12-19 22:46:43 +0300

    [jps-cache] IDEA-229430 Track only remote branch hash instead of local for notifications
    GitOrigin-RevId: 372e3ec3ca58716602ff9c37986f90bbfeafa232

--- plugins/jps-cache/src/com/intellij/jps/cache/JpsCachesProjectStateListener.java
+++ plugins/jps-cache/src/com/intellij/jps/cache/JpsCachesProjectStateListener.java
@@ -2,6 +2,8 @@ package com.intellij.jps.cache;
 
 import com.intellij.jps.cache.loader.JpsOutputLoaderManager;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.vcs.log.Hash;
+import git4idea.GitLocalBranch;
 import git4idea.repo.GitRepository;
 import git4idea.repo.GitRepositoryChangeListener;
 import org.jetbrains.annotations.NotNull;
@@ -12,10 +14,16 @@ public class JpsCachesProjectStateListener implements GitRepositoryChangeListene
 
   @Override
   public void repositoryChanged(@NotNull GitRepository repository) {
-    LOG.debug("Catch repository git event");
-    String currentRevision = repository.getInfo().getCurrentRevision();
+    GitLocalBranch branch = repository.getCurrentBranch();
+    if (branch == null) {
+      LOG.warn("Repository is not on a branch");
+      return;
+    }
+    Hash commitHash = repository.getInfo().getRemoteBranchesWithHashes().get(branch.findTrackedBranch(repository));
+    String currentRevision = commitHash.toString();
     if (currentRevision == null || previousCommitId.equals(currentRevision)) return;
     previousCommitId = currentRevision;
+    LOG.info("Remote repository commit changed to " + currentRevision);
     JpsOutputLoaderManager.getInstance(repository.getProject()).notifyAboutNearestCache();
   }
 }
