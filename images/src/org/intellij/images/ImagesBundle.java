c46a6e5cecd4a97dae51c899f30c5b232408b6df
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-12-19 14:22:06 +0300

    more bundles
    GitOrigin-RevId: 43153b989b5ef5fbf36fbbc4e4aed28d9cf7199f

--- images/src/org/intellij/images/ImagesBundle.java
+++ images/src/org/intellij/images/ImagesBundle.java
@@ -15,36 +15,19 @@
  */
 package org.intellij.images;
 
-import com.intellij.CommonBundle;
+import com.intellij.DynamicBundle;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.PropertyKey;
 
-import java.lang.ref.Reference;
-import java.lang.ref.SoftReference;
-import java.util.ResourceBundle;
-
-/**
- * @author max
- */
-public class ImagesBundle {
-
-  public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, @NotNull Object... params) {
-    return CommonBundle.message(getBundle(), key, params);
-  }
-
-  private static Reference<ResourceBundle> ourBundle;
+public class ImagesBundle extends DynamicBundle {
   @NonNls private static final String BUNDLE = "org.intellij.images.ImagesBundle";
+  private static final ImagesBundle INSTANCE = new ImagesBundle();
 
-  private ImagesBundle() {
-  }
+  private ImagesBundle() { super(BUNDLE); }
 
-  private static ResourceBundle getBundle() {
-    ResourceBundle bundle = com.intellij.reference.SoftReference.dereference(ourBundle);
-    if (bundle == null) {
-      bundle = ResourceBundle.getBundle(BUNDLE);
-      ourBundle = new SoftReference<>(bundle);
-    }
-    return bundle;
+  @NotNull
+  public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, @NotNull Object... params) {
+    return INSTANCE.getMessage(key, params);
   }
 }
