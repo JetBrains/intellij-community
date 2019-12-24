a5b191fe4354dc6241d1f4cd9cd16c3910ac9266
Vladislav Beskrovny <Vladislav.Beskrovny@jetbrains.com>
2019-12-24 18:59:27 +0300

    Fix IDEA-222358 Linux native memory leak
    GitOrigin-RevId: 6b7619f9472e03405a5f97c6630cb46046724d82

--- platform/util/src/com/intellij/util/io/DirectBufferWrapper.java
+++ platform/util/src/com/intellij/util/io/DirectBufferWrapper.java
@@ -1,12 +1,23 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.io;
 
-import java.io.File;
+import com.intellij.openapi.util.SystemInfo;
+import com.intellij.util.ConcurrencyUtil;
+import com.intellij.util.SystemProperties;
+
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.file.Path;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
 
 abstract class DirectBufferWrapper extends ByteBufferWrapper {
+  // Fixes IDEA-222358 Linux native memory leak. Please do not replace to BoundedTaskExecutor
+  private static final ExecutorService ourAllocator =
+    SystemInfo.isLinux && SystemProperties.getBooleanProperty("idea.limit.paged.storage.allocators", true)
+    ? ConcurrencyUtil.newSingleThreadExecutor("DirectBufferWrapper allocation thread")
+    : null;
+
   private volatile ByteBuffer myBuffer;
 
   DirectBufferWrapper(Path file, long offset, long length) {
@@ -22,9 +33,33 @@ abstract class DirectBufferWrapper extends ByteBufferWrapper {
   public ByteBuffer getBuffer() throws IOException {
     ByteBuffer buffer = myBuffer;
     if (buffer == null) {
-      myBuffer = buffer = create();
+      myBuffer = buffer = doCreate();
     }
     return buffer;
+  }
+
+  private ByteBuffer doCreate() throws IOException {
+    if (ourAllocator != null) {
+      // Fixes IDEA-222358 Linux native memory leak
+      try {
+        return ourAllocator.submit(this::create).get();
+      }
+      catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+      catch (ExecutionException e) {
+        Throwable cause = e.getCause();
+        if (cause instanceof IOException) {
+          throw (IOException)cause;
+        }
+        else {
+          throw new RuntimeException(e);
+        }
+      }
+    }
+    else {
+      return create();
+    }
   }
 
   protected abstract ByteBuffer create() throws IOException;
