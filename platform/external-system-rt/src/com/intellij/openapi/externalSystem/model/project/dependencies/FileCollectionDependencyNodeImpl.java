5db45ad4f13d701c7f50e7cc41c2701841d4c22a
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2019-12-26 08:00:52 +0300

    [Gradle] Dependencies graph: show file dependencies IDEA-218166
    GitOrigin-RevId: fdb2ff39053db1ed2c9bda279f2de4f8c73c5273

---
+++ platform/external-system-rt/src/com/intellij/openapi/externalSystem/model/project/dependencies/FileCollectionDependencyNodeImpl.java
@@ -0,0 +1,30 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.externalSystem.model.project.dependencies;
+
+import com.intellij.serialization.PropertyMapping;
+import org.jetbrains.annotations.NotNull;
+
+public class FileCollectionDependencyNodeImpl extends AbstractDependencyNode implements FileCollectionDependencyNode {
+
+  private final String displayName;
+  private final String path;
+
+  @PropertyMapping({"id", "displayName", "path"})
+  public FileCollectionDependencyNodeImpl(long id, @NotNull String displayName, @NotNull String path) {
+    super(id);
+    this.displayName = displayName;
+    this.path = path;
+  }
+
+  @Override
+  @NotNull
+  public String getPath() {
+    return path;
+  }
+
+  @NotNull
+  @Override
+  public String getDisplayName() {
+    return displayName;
+  }
+}
