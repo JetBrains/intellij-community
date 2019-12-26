82fdb2db4415ff2d2af5d323dda41ab1de669ef6
Nikolay Rykunov <Nikolay.Rykunov@jetbrains.com>
2019-12-26 09:30:02 +0300

    [git] Rebase: Add forgotten interactive rebase call in test
    GitOrigin-RevId: e38b0e6c804472c128574259fdf2eaf253a9250a

--- plugins/git4idea/tests/git4idea/rebase/GitSingleRepoRebaseTest.kt
+++ plugins/git4idea/tests/git4idea/rebase/GitSingleRepoRebaseTest.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package git4idea.rebase
 
 import com.intellij.dvcs.DvcsUtil
@@ -25,6 +11,7 @@ import git4idea.branch.GitBranchUiHandler
 import git4idea.branch.GitBranchWorker
 import git4idea.branch.GitRebaseParams
 import git4idea.config.GitVersionSpecialty
+import git4idea.rebase.interactive.GitInteractiveRebaseDialog
 import git4idea.repo.GitRepository
 import git4idea.test.*
 import org.junit.Assume
@@ -509,7 +496,11 @@ class GitSingleRepoRebaseTest : GitRebaseBaseTest() {
   fun `test cancel in interactive rebase should show no error notification`() {
     repo.`diverge feature and master`()
 
-    dialogManager.onDialog(GitRebaseEditor::class.java) { DialogWrapper.CANCEL_EXIT_CODE }
+    dialogManager.onDialog(GitInteractiveRebaseDialog::class.java) {
+      DialogWrapper.CANCEL_EXIT_CODE
+    }
+
+    rebaseInteractively()
 
     assertNoErrorNotification()
     assertNoRebaseInProgress(repo)
