533ec6804da415d2397f99b736c68b5d331a1041
Nikita Eshkeev <kastolom@gmail.com>
2019-12-28 01:46:35 +0300

    IDEA-229943: Enhance type inference for extracted variables
    Intellij IDEA used to travers through all known @FunctionalInterfaces
    and checks if their signatures match the examined method reference when
    extracting a local variable. It used to skip @FunctionalInterfaces which
    have generic return types due to inexhaustibleness cases in
    TypeConversionUtil#isAssignable().
    
    This patch adds a new case to check if the examined types are type
    parameters.
    
    Signed-off-by: Nikita Eshkeev <kastolom@gmail.com>

--- java/java-psi-api/src/com/intellij/psi/util/TypeConversionUtil.java
+++ java/java-psi-api/src/com/intellij/psi/util/TypeConversionUtil.java
@@ -853,6 +853,12 @@ public class TypeConversionUtil {
              && rText.endsWith(lText)
              && rText.charAt(rText.length() - lText.length() - 1) == '.';
     }
+
+    final PsiClass leftClass = PsiUtil.resolveClassInType(left);
+    if (leftClass instanceof PsiTypeParameter) {
+      return true;
+    }
+
     return isClassAssignable(leftResult, rightResult, allowUncheckedConversion, left.getResolveScope(), capture);
   }
 
