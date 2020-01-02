c3e494e7a69cc02b5f043c7c0a0f49682a6f9adc
Dmitry Jemerov <yole@jetbrains.com>
2020-01-02 13:08:13 +0100

    Mark base interfaces of components as deprecated
    GitOrigin-RevId: bc15653becdd051dbe44fcea02f9587958eb3bfa

--- platform/core-api/src/com/intellij/openapi/components/ProjectComponent.java
+++ platform/core-api/src/com/intellij/openapi/components/ProjectComponent.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.components;
 
 /**
@@ -12,7 +12,11 @@ package com.intellij.openapi.components;
  * {@link #initComponent()} and {@link #projectOpened()} methods will be called for each project even if user doesn't use any feature of your
  * plugin. So consider using specific extensions instead to ensure that the plugin will not impact IDE performance until user calls its
  * actions explicitly.</strong>
+ *
+ * @deprecated Components are deprecated; please see http://www.jetbrains.org/intellij/sdk/docs/basics/plugin_structure/plugin_components.html for
+ * guidelines on migrating to other APIs.
  */
+@Deprecated
 public interface ProjectComponent extends BaseComponent {
   /**
    * Invoked when the project corresponding to this component instance is opened.<p>
