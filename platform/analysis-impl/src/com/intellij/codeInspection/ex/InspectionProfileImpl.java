7713d706c5cae6c09bab459152baf320a42c695f
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2020-01-03 11:18:44 +0100

    InspectionProfileImpl: clear tools cache on project disposal.
    GitOrigin-RevId: 4b736da9e2fcdf8509731d7b5630902e955745f9

--- platform/analysis-impl/src/com/intellij/codeInspection/ex/InspectionProfileImpl.java
+++ platform/analysis-impl/src/com/intellij/codeInspection/ex/InspectionProfileImpl.java
@@ -501,6 +501,12 @@ public class InspectionProfileImpl extends NewInspectionProfile {
         getProfileManager().fireProfileChanged(InspectionProfileImpl.this);
       }
     }, project);
+    if (project != null) {
+      Disposer.register(project, () -> {
+        myTools.keySet().forEach(HighlightDisplayKey::unregister);
+        myTools.clear();
+      });
+    }
 
     DFSTBuilder<String> builder = new DFSTBuilder<>(GraphGenerator.generate(new InboundSemiGraph<String>() {
       @NotNull
