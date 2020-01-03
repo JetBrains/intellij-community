c77c574970377f221ef1aa5bdf1722408860fe89
Dmitry Jemerov <yole@jetbrains.com>
2020-01-03 14:10:55 +0100

    Allow load/unload for <syntaxHighlighter>
    GitOrigin-RevId: 5fa38a9f5b566b6372d0e8256dcc44ff00dffb18

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/text/EditorHighlighterUpdater.java
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/text/EditorHighlighterUpdater.java
@@ -14,6 +14,7 @@ import com.intellij.openapi.editor.highlighter.EditorHighlighter;
 import com.intellij.openapi.editor.highlighter.EditorHighlighterFactory;
 import com.intellij.openapi.extensions.ExtensionPointListener;
 import com.intellij.openapi.extensions.ExtensionPointName;
+import com.intellij.openapi.extensions.KeyedFactoryEPBean;
 import com.intellij.openapi.extensions.PluginDescriptor;
 import com.intellij.openapi.fileTypes.*;
 import com.intellij.openapi.fileTypes.impl.AbstractFileType;
@@ -56,6 +57,18 @@ public class EditorHighlighterUpdater {
     updateHighlightersOnExtensionsChange(parentDisposable, LanguageSyntaxHighlighters.EP_NAME);
     updateHighlightersOnExtensionsChange(parentDisposable, SyntaxHighlighterLanguageFactory.EP_NAME);
     updateHighlightersOnExtensionsChange(parentDisposable, FileTypeEditorHighlighterProviders.EP_NAME);
+
+    SyntaxHighlighter.EP_NAME.addExtensionPointListener(new ExtensionPointListener<KeyedFactoryEPBean>() {
+      @Override
+      public void extensionAdded(@NotNull KeyedFactoryEPBean extension, @NotNull PluginDescriptor pluginDescriptor) {
+        checkUpdateHighlighters(extension.key, false);
+      }
+
+      @Override
+      public void extensionRemoved(@NotNull KeyedFactoryEPBean extension, @NotNull PluginDescriptor pluginDescriptor) {
+        checkUpdateHighlighters(extension.key, true);
+      }
+    }, parentDisposable);
   }
 
   private <T> void updateHighlightersOnExtensionsChange(@NotNull Disposable parentDisposable, @NotNull ExtensionPointName<KeyedLazyInstance<T>> epName) {
