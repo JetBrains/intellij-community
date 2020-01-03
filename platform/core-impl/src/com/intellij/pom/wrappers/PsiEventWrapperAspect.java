987efb9e5fbd4ca1d05d54377810988c70ec56f0
Sergey Stupin <sergey.stupin@jetbrains.com>
2020-01-03 16:29:10 +0100

    [python-analysis-tool] psi modifications should be reflected in document
    GitOrigin-RevId: a2da401a85d240200ff22b22c3a839b721122de1

--- platform/core-impl/src/com/intellij/pom/wrappers/PsiEventWrapperAspect.java
+++ platform/core-impl/src/com/intellij/pom/wrappers/PsiEventWrapperAspect.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.pom.wrappers;
 
@@ -29,7 +15,7 @@ import com.intellij.pom.tree.events.TreeChangeEvent;
 import com.intellij.pom.tree.events.impl.TreeChangeEventImpl;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.impl.PsiDocumentManagerImpl;
+import com.intellij.psi.impl.PsiDocumentManagerBase;
 import com.intellij.psi.impl.PsiManagerImpl;
 import com.intellij.psi.impl.PsiToDocumentSynchronizer;
 import com.intellij.psi.impl.source.DummyHolder;
@@ -66,7 +52,7 @@ public class PsiEventWrapperAspect implements PomModelAspect{
 
   private static void promoteNonPhysicalChangesToDocument(ASTNode rootElement, PsiFile file) {
     if (file instanceof DummyHolder) return;
-    if (((PsiDocumentManagerImpl)PsiDocumentManager.getInstance(file.getProject())).isCommitInProgress()) return;
+    if (((PsiDocumentManagerBase)PsiDocumentManager.getInstance(file.getProject())).isCommitInProgress()) return;
     
     VirtualFile vFile = file.getViewProvider().getVirtualFile();
     if (vFile instanceof LightVirtualFile && !(vFile instanceof VirtualFileWindow)) {
