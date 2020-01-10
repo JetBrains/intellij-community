d2867f7295851df74c3b6ddd03ac107ba9f6f01a
Elena Shaverdova <elena.shaverdova@jetbrains.com>
2020-01-10 16:02:20 +0100

    Unified SSH: Move ssh terminal and external tools to ssh configs from deployment servers
    GitOrigin-RevId: 1fbd833b187dc1cd6e7d51d4589308add6afecba

--- platform/platform-impl/src/com/intellij/remote/RemoteConnectionType.java
+++ platform/platform-impl/src/com/intellij/remote/RemoteConnectionType.java
@@ -1,3 +1,4 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.remote;
 
 import com.intellij.openapi.diagnostic.Logger;
@@ -18,6 +19,10 @@ public enum RemoteConnectionType {
    * Web deployment server  
    */
   DEPLOYMENT_SERVER,
+  /**
+   * Ssh config
+   */
+  SSH_CONFIG,
   /**
    * Remote SDK
    */
