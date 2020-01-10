e9fc0388a9ab4d03dc9607ed7612948b1ef7c0a5
Bas Leijdekkers <basleijdekkers@gmail.com>
2020-01-10 15:32:17 +0100

    SSR: return default pattern context when none is specified
    GitOrigin-RevId: d09621bee265edb41bc4416c9f3421231118cab2

--- platform/structuralsearch/source/com/intellij/structuralsearch/StructuralSearchUtil.java
+++ platform/structuralsearch/source/com/intellij/structuralsearch/StructuralSearchUtil.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.structuralsearch;
 
 import com.intellij.lang.Language;
@@ -226,11 +226,11 @@ public class StructuralSearchUtil {
     return stripAccents(normalizeWhiteSpace(text));
   }
 
-  public static PatternContext findPatternContextByID(String id, Language language) {
+  public static PatternContext findPatternContextByID(@Nullable String id, @NotNull Language language) {
     return findPatternContextByID(id, getProfileByLanguage(language));
   }
 
-  public static PatternContext findPatternContextByID(String id, StructuralSearchProfile profile) {
+  public static PatternContext findPatternContextByID(@Nullable String id, @Nullable StructuralSearchProfile profile) {
     if (profile == null) {
       return null;
     }
