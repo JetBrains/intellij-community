613ccb07bd80a55b7f9136e329cd8339e690997d
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2020-01-10 18:11:08 +0300

    Open the Project view on project startup if there are no open files and no open toolwindows
    GitOrigin-RevId: 3b4a93f832836df49e2683bc107059a1511b94e0

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/OpenFilesActivity.java
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/OpenFilesActivity.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.fileEditor.impl;
 
 import com.intellij.ide.util.RunOnceUtil;
@@ -13,9 +13,13 @@ import com.intellij.openapi.startup.StartupActivity;
 import com.intellij.openapi.util.Ref;
 import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowManager;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+
+import static com.intellij.openapi.wm.ToolWindowId.PROJECT_VIEW;
 
 final class OpenFilesActivity implements StartupActivity {
   @Override
@@ -42,9 +46,13 @@ final class OpenFilesActivity implements StartupActivity {
       manager.initDockableContentFactory();
       if (!manager.hasOpenFiles()) {
         EditorsSplitters.stopOpenFilesActivity(project);
-        if (ApplicationManager.getApplication().isHeadlessEnvironment() &&
-            Registry.is("ide.open.readme.md.on.startup")) {
-          RunOnceUtil.runOnceForProject(project, "ShowReadmeOnStart", () -> findAndOpenReadme(project, manager));
+        if (!ApplicationManager.getApplication().isHeadlessEnvironment()) {
+          if (Registry.is("ide.open.readme.md.on.startup")) {
+            RunOnceUtil.runOnceForProject(project, "ShowReadmeOnStart", () -> findAndOpenReadme(project, manager));
+          }
+          if (Registry.is("ide.open.project.view.on.startup")) {
+            RunOnceUtil.runOnceForProject(project, "OpenProjectViewOnStart", () -> openProjectView(project));
+          }
         }
       }
     }, project.getDisposed());
@@ -59,5 +67,15 @@ final class OpenFilesActivity implements StartupActivity {
         ApplicationManager.getApplication().invokeLater(() -> manager.openFile(readme, true), project.getDisposed());
       }
     }
+  }
+
+  private static void openProjectView(@NotNull Project project) {
+    ToolWindowManager manager = ToolWindowManager.getInstance(project);
+    manager.invokeLater(() -> {
+      if (null == manager.getActiveToolWindowId()) {
+        ToolWindow window = manager.getToolWindow(PROJECT_VIEW);
+        if (window != null) window.activate(null);
+      }
+    });
   }
 }
