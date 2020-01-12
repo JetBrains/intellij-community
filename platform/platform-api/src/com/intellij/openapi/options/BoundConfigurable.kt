b6019af4647eca9b3d4790fb74b412db05d7773b
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2020-01-12 20:35:33 +0300

    nls bound configurable
    GitOrigin-RevId: a3acd87f23c459c4dd6e5da443be2ed358af0c06

--- platform/platform-api/src/com/intellij/openapi/options/BoundConfigurable.kt
+++ platform/platform-api/src/com/intellij/openapi/options/BoundConfigurable.kt
@@ -5,12 +5,14 @@ import com.intellij.openapi.Disposable
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.openapi.util.ClearableLazyValue
 import com.intellij.openapi.util.Disposer
+import org.jetbrains.annotations.Nls
+import org.jetbrains.annotations.NonNls
 import javax.swing.JComponent
 
 /**
  * @author yole
  */
-abstract class BoundConfigurable(private val displayName: String, private val helpTopic: String? = null) : Configurable {
+abstract class BoundConfigurable(@Nls private val displayName: String, @NonNls private val helpTopic: String? = null) : Configurable {
   var disposable: Disposable? = null
     private set
 
