6a74ee307dc327d20108180c10322ba8c049aaec
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2020-01-13 19:59:10 +0300

    cherry-pick commit 571edd6d from master
    IDEA-226333 SVN (possibly other VCS too?) version control in an infinite refresh loop
    
    GitOrigin-RevId: d3bfd309b100d07eaca6140e3bc33d9cca75708d

--- platform/vcs-impl/src/com/intellij/openapi/vcs/impl/FileStatusManagerImpl.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/impl/FileStatusManagerImpl.java
@@ -11,8 +11,8 @@ import com.intellij.openapi.editor.colors.ColorKey;
 import com.intellij.openapi.editor.colors.EditorColorsListener;
 import com.intellij.openapi.editor.colors.EditorColorsManager;
 import com.intellij.openapi.editor.colors.EditorColorsScheme;
-import com.intellij.openapi.editor.event.BulkAwareDocumentListener;
 import com.intellij.openapi.editor.event.DocumentEvent;
+import com.intellij.openapi.editor.event.DocumentListener;
 import com.intellij.openapi.extensions.impl.ExtensionPointImpl;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.DumbAwareRunnable;
@@ -20,6 +20,7 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.startup.StartupManager;
 import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.NotNullLazyValue;
 import com.intellij.openapi.vcs.FileStatus;
 import com.intellij.openapi.vcs.FileStatusListener;
@@ -102,17 +103,32 @@ public final class FileStatusManagerImpl extends FileStatusManager implements Di
     }
   }
 
-  static final class FileStatusManagerDocumentListener implements BulkAwareDocumentListener {
+  static final class FileStatusManagerDocumentListener implements DocumentListener {
+    private final Key<Boolean> CHANGED = Key.create("FileStatusManagerDocumentListener.document.changed");
+
     @Override
-    public void documentChangedNonBulk(@NotNull DocumentEvent event) {
+    public void documentChanged(@NotNull DocumentEvent event) {
       if (event.getOldLength() == 0 && event.getNewLength() == 0) {
         return;
       }
-      bulkUpdateFinished(event.getDocument());
+      Document document = event.getDocument();
+      if (document.isInBulkUpdate()) {
+        document.putUserData(CHANGED, Boolean.TRUE);
+      }
+      else {
+        refreshFileStatus(document);
+      }
     }
 
     @Override
     public void bulkUpdateFinished(@NotNull Document document) {
+      if (document.getUserData(CHANGED) != null) {
+        document.putUserData(CHANGED, null);
+        refreshFileStatus(document);
+      }
+    }
+
+    private static void refreshFileStatus(@NotNull Document document) {
       VirtualFile file = FileDocumentManager.getInstance().getFile(document);
       if (file == null) {
         return;
