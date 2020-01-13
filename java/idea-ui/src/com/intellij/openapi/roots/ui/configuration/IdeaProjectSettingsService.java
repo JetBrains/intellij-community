6e24b6a4ec8e9f6f405e328f483d9fdb7a5767dc
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2020-01-13 11:11:11 +0100

    deprecation message improved for IdeaProjectSettingsService#chooseAndSetSdk
    GitOrigin-RevId: 7cd89bc9a6d44a441f93e09d27ad2a5b964dfe2f

--- java/idea-ui/src/com/intellij/openapi/roots/ui/configuration/IdeaProjectSettingsService.java
+++ java/idea-ui/src/com/intellij/openapi/roots/ui/configuration/IdeaProjectSettingsService.java
@@ -17,6 +17,7 @@ package com.intellij.openapi.roots.ui.configuration;
 
 import com.intellij.compiler.actions.ArtifactAwareProjectSettingsService;
 import com.intellij.ide.projectView.impl.ModuleGroup;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.options.ShowSettingsUtil;
 import com.intellij.openapi.project.Project;
@@ -137,10 +138,22 @@ public class IdeaProjectSettingsService extends ProjectSettingsService implement
 
   private Sdk myDeprecatedChosenSdk = null;
 
+  /**
+   * @deprecated Please use {@link SdkPopupFactory} instead.
+   *
+   * Many usages of that API are too bogus and do duplicate similar code all other the place.
+   * It is not even possible to filter unneeded SDK types or SDK instances in the dialog.
+   *
+   * This method is no longer supported and behaves a bit broken: the first call returns {@code null},
+   * the second call may return a chosen SDK from the first call (only once). This is the way to
+   * avoid breaking the older code scenarios.
+   */
   @Override
+  @Deprecated
   public Sdk chooseAndSetSdk() {
-    //noinspection deprecation
-    super.chooseAndSetSdk();
+    Logger
+      .getInstance(getClass())
+      .warn("Call to the deprecated ProjectSettingsService#chooseAndSetSdk method. Please use new API instead");
 
     if (myDeprecatedChosenSdk != null) {
       Sdk chosenSdk = myDeprecatedChosenSdk;
