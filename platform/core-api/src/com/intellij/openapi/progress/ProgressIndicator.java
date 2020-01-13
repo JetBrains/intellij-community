d0198b144f946fbfd20711caf3e0bb808312becd
Alexey Kudravtsev <cdr@intellij.com>
2020-01-13 16:34:15 +0300

    add lifecycle warning doc for setFraction() (IDEA-CR-57295)
    GitOrigin-RevId: 3ad72d01174edd369336f9bfabb2080bdebc1789

--- platform/core-api/src/com/intellij/openapi/progress/ProgressIndicator.java
+++ platform/core-api/src/com/intellij/openapi/progress/ProgressIndicator.java
@@ -55,6 +55,7 @@ import org.jetbrains.annotations.Nullable;
  *   <li>{@link #start()} can be called only once after the indicator was created. (Or also after {@link #stop()}, if the indicator is reusable - see {@link com.intellij.openapi.progress.util.AbstractProgressIndicatorBase#isReuseable()})</li>
  *   <li>{@link #stop()} can be called only once after {@link #start()}</li>
  *   <li>{@link #setModalityProgress(ProgressIndicator)} can be called only before {@link #start()}</li>
+ *   <li>{@link #setFraction(double)}/{@link #getFraction()} can be called only after {@code setIndeterminate(false)}</li>
  * </ul>
  */
 public interface ProgressIndicator {
