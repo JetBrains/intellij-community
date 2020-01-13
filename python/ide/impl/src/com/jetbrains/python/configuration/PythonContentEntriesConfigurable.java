63338625ba28fa4b1d774469cfb92b26df2185e1
Dmitry.Krasilschikov <kradima@jetbrains.com>
2020-01-13 23:36:04 +0200

    i10n Python configurables
    GitOrigin-RevId: d785abdf038cb909c9ff7f280dd180ac61011e7f

--- python/ide/impl/src/com/jetbrains/python/configuration/PythonContentEntriesConfigurable.java
+++ python/ide/impl/src/com/jetbrains/python/configuration/PythonContentEntriesConfigurable.java
@@ -22,6 +22,7 @@ import com.intellij.openapi.options.NonDefaultProjectConfigurable;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ui.configuration.PlatformContentEntriesConfigurable;
 import com.intellij.util.PlatformUtils;
+import com.jetbrains.python.PyBundle;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.jps.model.java.JavaSourceRootType;
 
@@ -30,7 +31,7 @@ import org.jetbrains.jps.model.java.JavaSourceRootType;
  */
 public class PythonContentEntriesConfigurable extends ModuleAwareProjectConfigurable implements NonDefaultProjectConfigurable {
   public PythonContentEntriesConfigurable(final Project project) {
-    super(project, "Project Structure", "reference.settingsdialog.project.structure");
+    super(project, PyBundle.message("configurable.PythonContentEntriesConfigurable.display.name"), "reference.settingsdialog.project.structure");
   }
 
   @NotNull
