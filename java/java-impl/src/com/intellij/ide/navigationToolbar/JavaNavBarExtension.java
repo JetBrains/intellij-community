e8f81bbf4ad07b31cea472d611a7c0062949e75c
Dmitry Jemerov <yole@jetbrains.com>
2020-01-14 18:13:20 +0100

    Fix navbar in decompiled Java code
    GitOrigin-RevId: 9cbaec8626c6bd3b14a1124e3673f1ba28b7a83f

--- java/java-impl/src/com/intellij/ide/navigationToolbar/JavaNavBarExtension.java
+++ java/java-impl/src/com/intellij/ide/navigationToolbar/JavaNavBarExtension.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.navigationToolbar;
 
 import com.intellij.analysis.AnalysisScopeBundle;
@@ -108,7 +108,8 @@ public class JavaNavBarExtension extends AbstractNavBarModelExtension {
       PsiElement psiElement = psiFile.findElementAt(editor.getCaretModel().getOffset());
       if (psiElement != null && psiElement.getLanguage() == JavaLanguage.INSTANCE) {
         PsiMember member = PsiTreeUtil.getParentOfType(psiElement, PsiMember.class);
-        return member instanceof PsiField ? member.getContainingClass() : member;
+        PsiElement containingMember = member instanceof PsiField ? member.getContainingClass() : member;
+        return containingMember == null ? null : containingMember.getOriginalElement();
       }
     }
     return null;
