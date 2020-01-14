78bf47b5e5a5d22846a2bc3b8feba3c577a1f0c3
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2020-01-14 17:18:07 +0300

    git: use a determinate progress indicator for downloading Git: IDEA-221692
    GitOrigin-RevId: bbfd5d0f412ba762cbc4aa84ce26fbae8acf09d0

--- plugins/git4idea/src/git4idea/config/InlineErrorNotifier.kt
+++ plugins/git4idea/src/git4idea/config/InlineErrorNotifier.kt
@@ -1,16 +1,17 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package git4idea.config
 
+import com.intellij.ide.plugins.newui.TwoLineProgressIndicator
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.application.invokeAndWaitIfNeeded
-import com.intellij.openapi.progress.util.BackgroundTaskUtil
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.ui.DialogWrapper
-import com.intellij.ui.JBColor
+import com.intellij.openapi.util.Disposer
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.labels.LinkLabel
-import com.intellij.util.ui.AsyncProcessIcon
 import com.intellij.util.ui.components.BorderLayoutPanel
 import org.jetbrains.annotations.CalledInAwt
 import javax.swing.JComponent
@@ -18,7 +19,7 @@ import javax.swing.JPanel
 import javax.swing.SwingConstants
 
 internal interface InlineComponent {
-  fun showProgress(text: String)
+  fun showProgress(text: String): ProgressIndicator
   fun showError(errorText: String, link: LinkLabel<*>? = null)
   fun showMessage(text: String)
   fun hideProgress()
@@ -49,13 +50,14 @@ internal open class InlineErrorNotifier(private val inlineComponent: InlineCompo
 
   @CalledInAwt
   override fun executeTask(title: String, cancellable: Boolean, action: () -> Unit) {
-    inlineComponent.showProgress(title)
+    val pi = inlineComponent.showProgress(title)
     isTaskInProgress = true
+    Disposer.register(disposable, Disposable { pi.cancel() })
     ApplicationManager.getApplication().executeOnPooledThread {
       try {
-        BackgroundTaskUtil.runUnderDisposeAwareIndicator(disposable, Runnable {
+        ProgressManager.getInstance().runProcess(Runnable {
           action()
-        })
+        }, pi)
       }
       finally {
         invokeAndWaitIfNeeded(modalityState) {
@@ -85,23 +87,21 @@ internal open class InlineErrorNotifier(private val inlineComponent: InlineCompo
 }
 
 class GitExecutableInlineComponent(private val container: BorderLayoutPanel, private val panelToValidate: JPanel?) : InlineComponent {
-  private val busyIcon: AsyncProcessIcon = createBusyIcon()
 
-  override fun showProgress(text: String) {
+  override fun showProgress(text: String): ProgressIndicator {
     container.removeAll()
-    busyIcon.resume()
 
-    val label = JBLabel(text).apply {
-      foreground = JBColor.GRAY
+    val pi = TwoLineProgressIndicator(true).apply {
+      this.text = text
     }
 
-    container.addToLeft(busyIcon)
-    container.addToCenter(label)
+    container.addToLeft(pi.component)
     panelToValidate?.validate()
+
+    return pi
   }
 
   override fun showError(errorText: String, link: LinkLabel<*>?) {
-    busyIcon.suspend()
     container.removeAll()
 
     val label = multilineLabel(errorText).apply {
@@ -117,7 +117,6 @@ class GitExecutableInlineComponent(private val container: BorderLayoutPanel, pri
   }
 
   override fun showMessage(text: String) {
-    busyIcon.suspend()
     container.removeAll()
 
     container.addToLeft(JBLabel(text))
@@ -125,16 +124,9 @@ class GitExecutableInlineComponent(private val container: BorderLayoutPanel, pri
   }
 
   override fun hideProgress() {
-    busyIcon.suspend()
     container.removeAll()
 
     panelToValidate?.validate()
-  }
-
-  private fun createBusyIcon(): AsyncProcessIcon = AsyncProcessIcon(
-    toString()).apply {
-    isOpaque = false
-    setPaintPassiveIcon(false)
   }
 
   private fun multilineLabel(text: String): JComponent = JBLabel(text).apply {
