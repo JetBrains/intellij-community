e9435ed94d5aceaeff63b1fd00b88beb9ad2597a
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-01-15 15:37:37 +0100

    add javadoc about projectCloseHandler EP
    GitOrigin-RevId: b120ef83a7039f865456dbeafc3f90caeb91ce78

--- platform/projectModel-api/src/com/intellij/openapi/project/ProjectCloseHandler.java
+++ platform/projectModel-api/src/com/intellij/openapi/project/ProjectCloseHandler.java
@@ -1,8 +1,11 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.project;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Register via {@code com.intellij.projectCloseHandler} extension point.
+ */
 public interface ProjectCloseHandler {
   /**
    * Checks whether the project can be closed.
