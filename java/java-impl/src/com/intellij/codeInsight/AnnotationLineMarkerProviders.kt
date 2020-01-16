a1da37d487349b9e8dc5c6bd12ba04d52ff38772
Dmitry.Krasilschikov <kradima@jetbrains.com>
2020-01-16 18:16:03 +0200

    localize line markers
    GitOrigin-RevId: e6f546aee481b1af5462b7a48718bee5c117338e

--- java/java-impl/src/com/intellij/codeInsight/AnnotationLineMarkerProviders.kt
+++ java/java-impl/src/com/intellij/codeInsight/AnnotationLineMarkerProviders.kt
@@ -1,10 +1,13 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight
 
-class ExternalAnnotationLineMarkerProvider : NonCodeAnnotationsLineMarkerProvider("External annotations", LineMarkerType.External)
+class ExternalAnnotationLineMarkerProvider : NonCodeAnnotationsLineMarkerProvider(
+  CodeInsightBundle.message("line.marker.type.external.annotations"), LineMarkerType.External)
 
-class InferredNullabilityAnnotationsLineMarkerProvider : NonCodeAnnotationsLineMarkerProvider("Inferred nullability annotations", LineMarkerType.InferredNullability)
+class InferredNullabilityAnnotationsLineMarkerProvider : NonCodeAnnotationsLineMarkerProvider(
+  CodeInsightBundle.message("line.marker.type.inferred.nullability.annotations"), LineMarkerType.InferredNullability)
 
-class InferredContractAnnotationsLineMarkerProvider : NonCodeAnnotationsLineMarkerProvider("Inferred contract annotations", LineMarkerType.InferredContract) {
+class InferredContractAnnotationsLineMarkerProvider : NonCodeAnnotationsLineMarkerProvider(
+  CodeInsightBundle.message("line.marker.type.inferred.contract.annotations"), LineMarkerType.InferredContract) {
   override fun isEnabledByDefault() = false
 }
