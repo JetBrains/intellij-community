3516badcb139ba857c131cc4ee0d3a9bf4d5e36d
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-01-17 11:45:56 +0100

    document getting service flow
    GitOrigin-RevId: 09537caa4a2e79417f6febad0e70c8da18551540

--- platform/core-api/src/com/intellij/openapi/startup/StartupManager.java
+++ platform/core-api/src/com/intellij/openapi/startup/StartupManager.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.startup;
 
 import com.intellij.openapi.components.ServiceManager;
@@ -6,8 +6,7 @@ import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Allows registering activities that are run during project loading. Methods of StartupManager are typically
- * called from {@link com.intellij.openapi.components.ProjectComponent#projectOpened()}.
+ * Allows registering activities that are run during project loading.
  *
  * @see StartupActivity
  */
