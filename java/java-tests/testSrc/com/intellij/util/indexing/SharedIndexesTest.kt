5d7b2f37e0b20b1d70efc08bc05138e78b7e110f
Dmitro Batko <dmitry.batkovich@jetbrains.com>
2020-01-18 17:37:01 +0300

    restore file based index after shared index test
    GitOrigin-RevId: 9cb7a6577af6fb00b5ecef8d8f06c67bd883d57e

--- java/java-tests/testSrc/com/intellij/util/indexing/SharedIndexesTest.kt
+++ java/java-tests/testSrc/com/intellij/util/indexing/SharedIndexesTest.kt
@@ -20,10 +20,10 @@ import java.nio.file.Path
 class SharedIndexesTest : LightJavaCodeInsightFixtureTestCase() {
 
   private val tempDirPath: Path by lazy { FileUtil.createTempDirectory("shared-indexes-test", "").toPath() }
-  private val indexZip: Path by lazy {  tempDirPath.resolve(tempDirPath.fileName.toString() + ".zip") }
 
   fun testSharedIndexesForProject() {
-    val javaPsiFile = myFixture.configureByText("A.java", """
+    try {
+      val javaPsiFile = myFixture.configureByText("A.java", """
       public class A { 
         public void foo() {
           //Comment
@@ -31,34 +31,41 @@ class SharedIndexesTest : LightJavaCodeInsightFixtureTestCase() {
       }
     """.trimIndent())
 
-    val virtualFile = javaPsiFile.virtualFile
+      val virtualFile = javaPsiFile.virtualFile
 
-    val indexZip = tempDirPath.resolve(tempDirPath.fileName.toString() + ".zip")
+      val indexZip = tempDirPath.resolve(tempDirPath.fileName.toString() + ".zip")
 
-    val chunks = arrayListOf<DumpIndexAction.IndexChunk>()
-    chunks += DumpIndexAction.IndexChunk(setOf(virtualFile), "source")
+      val chunks = arrayListOf<DumpIndexAction.IndexChunk>()
+      chunks += DumpIndexAction.IndexChunk(setOf(virtualFile), "source")
 
-    DumpIndexAction.exportIndices(project, chunks, tempDirPath, EmptyProgressIndicator())
+      DumpIndexAction.exportIndices(project, chunks, tempDirPath, EmptyProgressIndicator())
 
+      restartFileBasedIndex(indexZip)
+
+      val fileBasedIndex = FileBasedIndex.getInstance()
+
+      val map = fileBasedIndex.getFileData(StubUpdatingIndex.INDEX_ID, virtualFile, project)
+      TestCase.assertTrue(map.isNotEmpty())
+
+      val values = fileBasedIndex.getValues(IdIndex.NAME, IdIndexEntry("Comment", true), GlobalSearchScope.allScope(project))
+      TestCase.assertEquals(UsageSearchContext.IN_COMMENTS.toInt(), values.single())
+    } finally {
+      restartFileBasedIndex(null)
+    }
+  }
+
+  private fun restartFileBasedIndex(indexZip: Path?) {
     val indexSwitcher = FileBasedIndexSwitcher(FileBasedIndex.getInstance() as FileBasedIndexImpl)
     ApplicationManager.getApplication().runWriteAction {
       indexSwitcher.turnOff()
     }
 
     FileUtil.delete(PathManager.getIndexRoot())
-    System.setProperty("prebuilt.hash.index.zip", indexZip.toAbsolutePath().toString())
+    System.setProperty("prebuilt.hash.index.zip", indexZip?.toAbsolutePath().toString())
 
     ApplicationManager.getApplication().runWriteAction {
       indexSwitcher.turnOn()
     }
-
-    val fileBasedIndex = FileBasedIndex.getInstance()
-
-    val map = fileBasedIndex.getFileData(StubUpdatingIndex.INDEX_ID, virtualFile, project)
-    TestCase.assertTrue(map.isNotEmpty())
-
-    val values = fileBasedIndex.getValues(IdIndex.NAME, IdIndexEntry("Comment", true), GlobalSearchScope.allScope(project))
-    TestCase.assertEquals(UsageSearchContext.IN_COMMENTS.toInt(), values.single())
   }
 
 }
