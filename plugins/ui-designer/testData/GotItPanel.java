4de5184083dd8800813b976af28ee38dfd838c8e
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2020-01-19 03:16:18 +0300

    fix test on mac
    GitOrigin-RevId: 66d33d7d95bd663b0e6d1e6bf80c5657f2a40cfa

--- plugins/ui-designer/testData/GotItPanel.java
+++ plugins/ui-designer/testData/GotItPanel.java
@@ -1,10 +1,3 @@
-import com.intellij.ide.IdeTooltipManager;
-import com.intellij.ui.scale.JBUIScale;
-import com.intellij.util.ui.GraphicsUtil;
-import com.intellij.util.ui.StartupUiUtil;
-import com.intellij.util.ui.UIUtil;
-import org.jetbrains.annotations.NotNull;
-
 import javax.swing.*;
 import java.awt.*;
 
