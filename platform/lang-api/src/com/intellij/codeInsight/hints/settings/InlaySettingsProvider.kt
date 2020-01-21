e2c4eb74d77fc934658d7cdca036fb14dd8b8c97
Dennis Ushakov <dennis.ushakov@jetbrains.com>
2020-01-21 12:35:06 +0300

    inlay settings: dynamic
    GitOrigin-RevId: 39112503ae30daf72b62862fba3898f3b61b98aa

--- platform/lang-api/src/com/intellij/codeInsight/hints/settings/InlaySettingsProvider.kt
+++ platform/lang-api/src/com/intellij/codeInsight/hints/settings/InlaySettingsProvider.kt
@@ -23,7 +23,7 @@ interface InlaySettingsProvider {
   fun getSupportedLanguages(project: Project): Collection<Language>
 
   object EP {
-    private val EXTENSION_POINT_NAME =
+    val EXTENSION_POINT_NAME =
       ExtensionPointName.create<InlaySettingsProvider>("com.intellij.config.inlaySettingsProvider")
 
     fun getExtensions(): Array<out InlaySettingsProvider> {
