b936699772bd1c36f4b10f0ce4b3309e0b538ddb
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2020-01-21 17:41:52 +0700

    IDEA-229842 Convert compact constructor to canonical
    GitOrigin-RevId: 6edd200e51ec300bf1068d2f040b837fea3f6e1f

---
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/compactToCanonical/beforeAnnotations.java
@@ -0,0 +1,20 @@
+// "Convert compact constructor to canonical" "true"
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+public record Test(int x, @ParamAnno int y, @FieldAnno int... other) {
+    @ConstructorAnno
+    <caret>public Test {
+        this.x = Math.abs(x);
+        if (other == null) other = new int[0];
+    }
+}
+
+@Target(ElementType.PARAMETER)
+@interface ParamAnno {}
+
+@Target(ElementType.FIELD)
+@interface FieldAnno {}
+
+@Target(ElementType.CONSTRUCTOR)
+@interface ConstructorAnno {}
