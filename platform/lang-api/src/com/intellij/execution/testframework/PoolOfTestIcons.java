66ecdf18e34d9a1d18d39a8cffcd6b7e812e0c4b
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2020-01-22 00:03:58 +0300

    Remove deprecated icons in AllIcons.RunConfigurations
    GitOrigin-RevId: 8e51e2029f6361da957911002232041173e7c0a2

--- platform/lang-api/src/com/intellij/execution/testframework/PoolOfTestIcons.java
+++ platform/lang-api/src/com/intellij/execution/testframework/PoolOfTestIcons.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.testframework;
 
 import com.intellij.icons.AllIcons;
@@ -27,13 +13,6 @@ public interface PoolOfTestIcons {
   Icon FAILED_ICON = AllIcons.RunConfigurations.TestFailed;
   Icon ERROR_ICON = AllIcons.RunConfigurations.TestError;
   Icon NOT_RAN = AllIcons.RunConfigurations.TestNotRan;
-  /**
-   * @deprecated to be removed in IDEA 2020, use AllIcons.Process.Step_N
-   */
-  @SuppressWarnings("unused")
-  @Deprecated
-  @ApiStatus.ScheduledForRemoval(inVersion = "2020")
-  Icon LOADING_ICON = AllIcons.RunConfigurations.LoadingTree;
   Icon TERMINATED_ICON = AllIcons.RunConfigurations.TestTerminated;
   Icon IGNORED_ICON = AllIcons.RunConfigurations.TestIgnored;
   Icon ERROR_ICON_MARK = AllIcons.Nodes.ErrorMark;
