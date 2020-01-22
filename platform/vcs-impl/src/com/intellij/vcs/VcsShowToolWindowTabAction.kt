23202b2278679310d061aa34256fd46a751700ae
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2020-01-22 21:25:35 +0300

    vcs: Convert "VcsShowToolWindowTabAction" and inheritors to kotlin
    GitOrigin-RevId: 056cd5cc02242a1495f6edae5a0ed228634e988f

--- platform/vcs-impl/src/com/intellij/vcs/VcsShowToolWindowTabAction.kt
+++ platform/vcs-impl/src/com/intellij/vcs/VcsShowToolWindowTabAction.kt
@@ -1,51 +1,48 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.vcs;
+package com.intellij.vcs
 
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.project.DumbAwareAction;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.changes.ui.ChangesViewContentManager;
-import com.intellij.openapi.wm.ToolWindow;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vcs.changes.ui.ChangesViewContentManager
+import com.intellij.openapi.vcs.changes.ui.ChangesViewContentManager.Companion.LOCAL_CHANGES
+import com.intellij.openapi.vcs.changes.ui.ChangesViewContentManager.Companion.SHELF
+import com.intellij.openapi.vcs.changes.ui.ChangesViewContentManager.Companion.getToolWindowFor
+import com.intellij.openapi.wm.ToolWindow
 
-import static com.intellij.openapi.vcs.changes.ui.ChangesViewContentManager.getToolWindowFor;
-import static com.intellij.util.ObjectUtils.assertNotNull;
+class VcsShowLocalChangesAction : VcsShowToolWindowTabAction() {
+  override val tabName: String get() = LOCAL_CHANGES
+}
 
-public abstract class VcsShowToolWindowTabAction extends DumbAwareAction {
+class VcsShowShelfAction : VcsShowToolWindowTabAction() {
+  override val tabName: String get() = SHELF
+}
 
-  @Override
-  public void actionPerformed(@NotNull AnActionEvent e) {
-    final Project project = e.getRequiredData(CommonDataKeys.PROJECT);
-    ToolWindow toolWindow = assertNotNull(getToolWindow(project));
-    final ChangesViewContentManager changesViewContentManager = (ChangesViewContentManager)ChangesViewContentManager.getInstance(project);
-    final String tabName = getTabName();
+abstract class VcsShowToolWindowTabAction : DumbAwareAction() {
+  protected abstract val tabName: String
 
-    if (toolWindow.isActive() && changesViewContentManager.isContentSelected(tabName)) {
-        toolWindow.hide(null);
+  override fun update(e: AnActionEvent) {
+    e.presentation.isEnabledAndVisible = getToolWindow(e.project) != null
+  }
+
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.project!!
+    val toolWindow = getToolWindow(project)!!
+    val changesViewContentManager = ChangesViewContentManager.getInstance(project) as ChangesViewContentManager
+
+    if (toolWindow.isActive && changesViewContentManager.isContentSelected(tabName)) {
+      toolWindow.hide(null)
     }
     else {
-      toolWindow.activate(() -> {
-        if (!changesViewContentManager.isContentSelected(tabName)) {
-          changesViewContentManager.selectContent(tabName, true);
-        }
-      }, true, true);
+      toolWindow.activate(
+        {
+          if (!changesViewContentManager.isContentSelected(tabName)) {
+            changesViewContentManager.selectContent(tabName, true)
+          }
+        }, true, true
+      )
     }
   }
 
-  @Nullable
-  private ToolWindow getToolWindow(@Nullable Project project) {
-    if (project == null) return null;
-    return getToolWindowFor(project, getTabName());
-  }
-
-  @Override
-  public void update(@NotNull AnActionEvent e) {
-    super.update(e);
-    e.getPresentation().setEnabledAndVisible(getToolWindow(e.getProject()) != null);
-  }
-
-  @NotNull
-  protected abstract String getTabName();
+  private fun getToolWindow(project: Project?): ToolWindow? = project?.let { getToolWindowFor(it, tabName) }
 }
