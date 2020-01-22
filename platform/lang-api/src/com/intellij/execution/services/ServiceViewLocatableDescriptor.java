4cef8e2f5c80aadc8b9d14fc3df9a09a5b8aca51
Konstantin Aleev <konstantin.aleev@jetbrains.com>
2020-01-22 20:38:50 +0300

    Revert "[service-view] make model invoker without read action"
    This reverts commit b5c6b372
    
    GitOrigin-RevId: 21cba2594e7d5abcbdecfd6956a53693b1637307

--- platform/lang-api/src/com/intellij/execution/services/ServiceViewLocatableDescriptor.java
+++ platform/lang-api/src/com/intellij/execution/services/ServiceViewLocatableDescriptor.java
@@ -2,15 +2,11 @@
 package com.intellij.execution.services;
 
 import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
 
-/**
- * {@link ServiceViewDescriptor} may implement this interface in order to enable navigation from sources to a service.
- */
+@ApiStatus.Experimental
 public interface ServiceViewLocatableDescriptor {
-  /**
-   * Invoked on background thread under read action.
-   */
   @Nullable
   default VirtualFile getVirtualFile() {
     return null;
