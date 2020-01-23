3fc5d965fcffb7c2a6ff0351fc8e0a498d770e9f
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2020-01-23 10:37:10 +0100

    Cleanup (restoring corrupted (c) header)
    GitOrigin-RevId: ac5a540598e87a8cda8630341ed40f86141b572f

--- platform/platform-resources/src/launcher.py
+++ platform/platform-resources/src/launcher.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env $PYTHON$
-#  Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+# Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+# -*- coding: utf-8 -*-
 
 import os
 import socket
