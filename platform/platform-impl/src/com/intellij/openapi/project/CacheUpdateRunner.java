0857e7b3d7eef276431d0fea79e6b58a5a9e2e5d
Dmitro Batko <dmitry.batkovich@jetbrains.com>
2020-01-24 22:30:24 +0300

    Revert: don't use synchronous invoke&wait in processSomeFilesWhileUserIsInactive (002748cb)
    GitOrigin-RevId: 1988362f7e9ffa2d25fd9f5cdbb11ffe66fa734b

--- platform/platform-impl/src/com/intellij/openapi/project/CacheUpdateRunner.java
+++ platform/platform-impl/src/com/intellij/openapi/project/CacheUpdateRunner.java
@@ -18,7 +18,6 @@ import com.intellij.openapi.progress.util.ProgressIndicatorUtils;
 import com.intellij.openapi.progress.util.ProgressWrapper;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.Key;
-import com.intellij.openapi.util.Ref;
 import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.ConcurrencyUtil;
@@ -115,14 +114,7 @@ public class CacheUpdateRunner {
     };
     final Application application = ApplicationManager.getApplication();
     Disposable listenerDisposable = Disposer.newDisposable();
-    Ref<Boolean> shouldSetUpListener = new Ref<>(true);
-    application.invokeLater(() -> {
-      synchronized (shouldSetUpListener) {
-        if (shouldSetUpListener.get()) {
-          application.addApplicationListener(canceller, listenerDisposable);
-        }
-      }
-    }, ModalityState.any());
+    application.invokeAndWait(() -> application.addApplicationListener(canceller, listenerDisposable), ModalityState.any());
 
     final AtomicBoolean isFinished = new AtomicBoolean();
     try {
@@ -144,10 +136,7 @@ public class CacheUpdateRunner {
       }
     }
     finally {
-      synchronized (shouldSetUpListener) {
-        shouldSetUpListener.set(false);
-        Disposer.dispose(listenerDisposable);
-      }
+      Disposer.dispose(listenerDisposable);
     }
 
     return isFinished.get();
