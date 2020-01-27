da3c3d24550c90be92888482fff6eadf3f1030a9
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-01-27 19:49:40 +0100

    TestEditorManagerImpl - release editor if cannot init it
    GitOrigin-RevId: a23cae8320c617dae256ae8aa4751687f88eb816

--- java/java-tests/testSrc/com/intellij/lang/LanguageSubstitutorLoadUnloadTest.kt
+++ java/java-tests/testSrc/com/intellij/lang/LanguageSubstitutorLoadUnloadTest.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.lang
 
 import com.intellij.ide.plugins.loadExtensionWithText
@@ -13,22 +13,21 @@ import com.intellij.testFramework.UsefulTestCase
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
 
 class LanguageSubstitutorLoadUnloadTest : LightJavaCodeInsightFixtureTestCase() {
-  
   fun testBefore() {
     myFixture.configureByText("dummy.txt", "package hello;")
   }
-  
+
   fun testLoadUnload() {
     val beforeLoading = myFixture.configureByText("dummy.txt", "package hello;")
     UsefulTestCase.assertInstanceOf(beforeLoading.language, PlainTextLanguage::class.java)
-    
+
     val virtualFile = beforeLoading.virtualFile
     val text = "<lang.substitutor language=\"TEXT\" implementationClass=\"${TextToJavaSubstitutor::class.java.name}\"/>"
     val disposable = loadExtensionWithText(text, javaClass.classLoader)
     val psiManager = PsiManager.getInstance(myFixture.project)
     val afterLoading = psiManager.findFile(virtualFile)
     UsefulTestCase.assertInstanceOf(afterLoading!!.language, JavaLanguage::class.java)
-    
+
     Disposer.dispose(disposable)
     val afterUnloading = psiManager.findFile(virtualFile)
     UsefulTestCase.assertInstanceOf(afterUnloading!!.language, PlainTextLanguage::class.java)
