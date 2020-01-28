f54065ca6f2e375a7d12a048835dfbd84f112439
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-01-28 12:27:12 +0100

    IDEA-231592 XmlSerializationException: Can't serialize instance of class git4idea.config.GitVcsOptions
    (cherry picked from commit 7946645d2bf973e4c89a6a3e2db92cc8968de76b)
    
    GitOrigin-RevId: ea1c942c49da953514a68e9547fcc646d68fb0ec

--- platform/dvcs-impl/src/com/intellij/dvcs/branch/DvcsBranchSettings.kt
+++ platform/dvcs-impl/src/com/intellij/dvcs/branch/DvcsBranchSettings.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.dvcs.branch
 
 import com.intellij.openapi.components.BaseState
@@ -12,7 +12,7 @@ class DvcsBranchSettings : BaseState() {
   var excludedFavorites by property(BranchStorage())
   @get:Tag("branch-grouping")
   @get:XCollection(style = XCollection.Style.v2)
-  var groupingKeyIds by stringSet(defaultGroupingKey.id)
+  val groupingKeyIds by stringSet(defaultGroupingKey.id)
 }
 
 private val defaultGroupingKey = GroupingKey.GROUPING_BY_DIRECTORY
