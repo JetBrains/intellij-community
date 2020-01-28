c40649f53f9442e91bfb87602238243ee6e7c68b
Max Medvedev <maxim.medvedev@jetbrains.com>
2020-01-28 17:31:52 +0100

    several @NotNulls
    GitOrigin-RevId: dcbce877ba11f52f5287db3f1d9215724e92bd6b

--- platform/core-impl/src/com/intellij/psi/impl/SyntheticFileSystemItem.java
+++ platform/core-impl/src/com/intellij/psi/impl/SyntheticFileSystemItem.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 /*
  * @author max
@@ -26,7 +26,7 @@ public abstract class SyntheticFileSystemItem extends PsiElementBase implements 
   protected final Project myProject;
   protected final PsiManager myManager;
 
-  public SyntheticFileSystemItem(Project project) {
+  public SyntheticFileSystemItem(@NotNull Project project) {
     myProject = project;
     myManager = PsiManager.getInstance(myProject);
   }
