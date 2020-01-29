8a8c3cf94271ca0dff975843ca2ef3151de33d08
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2020-01-29 13:26:20 +0300

    i18n for plugins configurable
    GitOrigin-RevId: 03b29e32c81bbb12c2732cf228b6112070fd80c0

--- platform/platform-impl/src/com/intellij/ide/plugins/newui/PluginsGroup.java
+++ platform/platform-impl/src/com/intellij/ide/plugins/newui/PluginsGroup.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.plugins.newui;
 
+import com.intellij.ide.IdeBundle;
 import com.intellij.ide.plugins.IdeaPluginDescriptor;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.ui.components.labels.LinkLabel;
@@ -65,7 +66,7 @@ public class PluginsGroup {
   }
 
   public void titleWithCount(int enabled) {
-    title = myTitlePrefix + " (" + enabled + " of " + descriptors.size() + " enabled)";
+    title = IdeBundle.message("plugins.configurable.title.with.count", myTitlePrefix, enabled, descriptors.size());
     updateTitle();
   }
 
