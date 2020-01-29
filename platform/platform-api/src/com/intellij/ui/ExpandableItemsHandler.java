338fed830038ee1b7487dce045ec67f1faae82ba
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2020-01-29 17:35:09 +0300

    IDEA-230665: add API to expand truncated node text only on mouse hover, not on selection
    GitOrigin-RevId: 4fb96eb45dae8bfbe6b5b5229880c6614fb0428a

--- platform/platform-api/src/com/intellij/ui/ExpandableItemsHandler.java
+++ platform/platform-api/src/com/intellij/ui/ExpandableItemsHandler.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2010 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ui;
 
 import com.intellij.openapi.util.Key;
@@ -21,6 +7,14 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Collection;
 
 public interface ExpandableItemsHandler<T> {
+  /**
+   * This key can be set to a component to ignore processing of mouse events.
+   */
+  Key<Boolean> IGNORE_MOUSE_HOVER = Key.create("IGNORE_MOUSE_HOVER");
+  /**
+   * This key can be set to a component to ignore processing of item selection.
+   */
+  Key<Boolean> IGNORE_ITEM_SELECTION = Key.create("IGNORE_ITEM_SELECTION");
   /**
    * This key is used to disable showing a popup for expandable item.
    * It can be set to a renderer component to disable a popup for specific item.
