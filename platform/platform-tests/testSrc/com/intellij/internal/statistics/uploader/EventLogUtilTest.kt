ffa1aa21cb06a911ba51e421735747a853b238bc
Svetlana.Zemlyanskaya <svetlana.zemlyanskaya@jetbrains.com>
2020-01-29 17:44:06 +0100

    FUS: remove circular dependency on testFramework (IDEA-228935)
    GitOrigin-RevId: 0ff52c665aa0c97cf7d94f60567b2771af89911c

--- platform/platform-tests/testSrc/com/intellij/internal/statistics/uploader/EventLogUtilTest.kt
+++ platform/platform-tests/testSrc/com/intellij/internal/statistics/uploader/EventLogUtilTest.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.internal.statistic.uploader
+package com.intellij.internal.statistics.uploader
 
 import com.intellij.internal.statistic.StatisticsEventLogUtil
 import com.intellij.testFramework.UsefulTestCase
