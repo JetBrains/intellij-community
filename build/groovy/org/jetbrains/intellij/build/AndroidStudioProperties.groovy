f66313d5c523437b0138aad990a3b6c686de63bd
Sergey Prigogin <sprigogin@google.com>
2020-01-30 11:47:45 -0800

    Add module intellij.android.emulator
    Change-Id: Icd2fa4d3f42108e4d895d41405293f72a3a9cc5d

--- build/groovy/org/jetbrains/intellij/build/AndroidStudioProperties.groovy
+++ build/groovy/org/jetbrains/intellij/build/AndroidStudioProperties.groovy
@@ -146,6 +146,7 @@ class AndroidStudioProperties extends BaseIdeaProperties {
       withModule("intellij.android.app-inspection.ide", "android.jar")
       withModule("intellij.android.databinding", "android.jar")
       withModule("intellij.android.debuggers", "android.jar")
+      withModule("intellij.android.emulator", "android.jar")
       withModule("intellij.android.lang", "android.jar")
       withModule("intellij.android.lang-databinding", "android.jar")
       withModule("intellij.android.mlkit", "android.jar")
