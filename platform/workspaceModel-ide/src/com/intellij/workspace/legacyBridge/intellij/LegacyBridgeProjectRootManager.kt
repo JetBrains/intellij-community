86ccb3501eb721384731145d1e90a92408c4ac75
Alex Plate <aleksei.plate@jetbrains.com>
2020-01-30 14:31:10 +0300

    [Workspace Model] Libraries changes should not fire events if the library is not included in any order entry
    GitOrigin-RevId: ab2fb57ce09166c8dba712422dd3d5d2484943b5

--- platform/workspaceModel-ide/src/com/intellij/workspace/legacyBridge/intellij/LegacyBridgeProjectRootManager.kt
+++ platform/workspaceModel-ide/src/com/intellij/workspace/legacyBridge/intellij/LegacyBridgeProjectRootManager.kt
@@ -1,14 +1,19 @@
 package com.intellij.workspace.legacyBridge.intellij
 
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.projectRoots.ProjectJdkTable
 import com.intellij.openapi.projectRoots.Sdk
+import com.intellij.openapi.roots.LibraryOrderEntry
+import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.roots.RootProvider
 import com.intellij.openapi.roots.impl.ProjectRootManagerComponent
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.util.EmptyRunnable
+import com.intellij.workspace.api.EntityChange
 import com.intellij.workspace.api.EntityStoreChanged
+import com.intellij.workspace.api.LibraryEntity
 import com.intellij.workspace.bracket
 import com.intellij.workspace.ide.WorkspaceModelChangeListener
 import com.intellij.workspace.ide.WorkspaceModelTopics
@@ -23,13 +28,19 @@ class LegacyBridgeProjectRootManager(project: Project) : ProjectRootManagerCompo
     bus.subscribe(WorkspaceModelTopics.CHANGED, object : WorkspaceModelChangeListener {
       override fun beforeChanged(event: EntityStoreChanged) {
         if (myProject.isDisposed || Disposer.isDisposing(myProject)) return
-        getBatchSession(false).beforeRootsChanged()
+
+        val performUpdate = processChanges(event, project)
+
+        if (performUpdate) getBatchSession(false).beforeRootsChanged()
       }
 
       override fun changed(event: EntityStoreChanged) {
         if (myProject.isDisposed || Disposer.isDisposing(myProject)) return
         LOG.bracket("ProjectRootManager.EntityStoreChange") {
-          getBatchSession(false).rootsChanged()
+
+          val performUpdate = processChanges(event, project)
+
+          if (performUpdate) getBatchSession(false).rootsChanged()
         }
       }
     })
@@ -51,5 +62,29 @@ class LegacyBridgeProjectRootManager(project: Project) : ProjectRootManagerCompo
         jdk.rootProvider.removeRootSetChangedListener(listener)
       }
     })
+  }
+
+  /** Library changes should not fire any events if the library is not included in any of order entries */
+  private fun processChanges(events: EntityStoreChanged, project: Project): Boolean {
+    val libraryChanges = events.getChanges(LibraryEntity::class.java)
+    return if (libraryChanges.isNotEmpty() && libraryChanges.count() == events.getAllChanges().count()) {
+      for (event in libraryChanges) {
+        val res = when (event) {
+          is EntityChange.Added -> libraryHasOrderEntry(event.entity.name, project)
+          is EntityChange.Removed -> libraryHasOrderEntry(event.entity.name, project)
+          is EntityChange.Replaced -> libraryHasOrderEntry(event.newEntity.name, project)
+        }
+        if (res) return true
+      }
+      return false
+    } else true
+  }
+
+  private fun libraryHasOrderEntry(name: String, project: Project): Boolean {
+    ModuleManager.getInstance(project).modules.forEach { module ->
+      val exists = ModuleRootManager.getInstance(module).orderEntries.any { it is LibraryOrderEntry && it.libraryName == name }
+      if (exists) return true
+    }
+    return false
   }
 }
