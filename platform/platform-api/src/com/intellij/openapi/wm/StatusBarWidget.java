a9a3c994b50648d24694f39a56652634f01990aa
Dmitry.Krasilschikov <kradima@jetbrains.com>
2020-01-30 19:23:59 +0200

    Annotate by @Nls: `getTooltipText()`
    GitOrigin-RevId: c6caf5226638207ebef6d7d0981cbcd006a999e1

--- platform/platform-api/src/com/intellij/openapi/wm/StatusBarWidget.java
+++ platform/platform-api/src/com/intellij/openapi/wm/StatusBarWidget.java
@@ -7,6 +7,7 @@ import com.intellij.openapi.util.SystemInfo;
 import com.intellij.util.Consumer;
 import com.intellij.util.ui.JBUI;
 import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -53,6 +54,7 @@ public interface StatusBarWidget extends Disposable {
 
   interface WidgetPresentation {
     @Nullable
+    @Nls(capitalization = Nls.Capitalization.Sentence)
     String getTooltipText();
 
     @Nullable
