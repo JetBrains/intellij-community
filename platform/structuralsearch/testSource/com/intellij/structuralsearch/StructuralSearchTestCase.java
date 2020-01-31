9be86cc28144964fda7ef80be0d5e84659fc6a4e
Bas Leijdekkers <basleijdekkers@gmail.com>
2020-01-31 17:06:49 +0100

    SSR: simplify optimized search scan tests
    GitOrigin-RevId: 365166207de3f9b8ebacbd51b34b4fda0993fe86

--- platform/structuralsearch/testSource/com/intellij/structuralsearch/StructuralSearchTestCase.java
+++ platform/structuralsearch/testSource/com/intellij/structuralsearch/StructuralSearchTestCase.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.structuralsearch;
 
 import com.intellij.codeInsight.daemon.quickFix.LightQuickFixTestCase;
@@ -37,6 +37,14 @@ public abstract class StructuralSearchTestCase extends LightQuickFixTestCase {
     testMatcher = null;
     options = null;
     super.tearDown();
+  }
+
+  protected String getSearchPlan(String query, LanguageFileType fileType) {
+    final MatchOptions matchOptions = new MatchOptions();
+    matchOptions.fillSearchCriteria(query);
+    matchOptions.setFileType(fileType);
+    PatternCompiler.compilePattern(getProject(), matchOptions, true, true);
+    return PatternCompiler.getLastSearchPlan();
   }
 
   protected int findMatchesCount(String in, String pattern, LanguageFileType fileType) {
