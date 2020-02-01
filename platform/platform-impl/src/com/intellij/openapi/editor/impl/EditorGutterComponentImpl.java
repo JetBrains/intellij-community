0c35a0ba369949bc39580729628400c4c872efba
Konstantin Bulenkov <kb@jetbrains.com>
2020-02-01 21:51:16 +0100

    An attempt to fix folding icons painting issues on Windows and Linux
    GitOrigin-RevId: 640a7e1df1a4e083576acf63cfdaa4550c8c85c4

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorGutterComponentImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorGutterComponentImpl.java
@@ -40,6 +40,7 @@ import com.intellij.openapi.progress.util.ProgressIndicatorBase;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.GraphicsConfig;
 import com.intellij.openapi.ui.popup.Balloon;
 import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.util.Ref;
@@ -66,7 +67,10 @@ import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.ui.*;
 import com.intellij.util.ui.JBValue.JBValueGroup;
 import com.intellij.util.ui.accessibility.ScreenReader;
-import gnu.trove.*;
+import gnu.trove.THashSet;
+import gnu.trove.TIntArrayList;
+import gnu.trove.TIntObjectHashMap;
+import gnu.trove.TIntObjectProcedure;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -1320,13 +1324,43 @@ class EditorGutterComponentImpl extends EditorGutterComponentEx implements Mouse
     double[] dxPoints = {x1, x1, x2, x2, centerX};
     double[] dyPoints = {y + baseHeight, y, y, y + baseHeight, y + height + (height < 0 ? 1 : 0)};
 
-    g.setColor(myEditor.getBackgroundColor());
-    LinePainter2D.fillPolygon(g, dxPoints, dyPoints, 5, StrokeType.CENTERED_CAPS_SQUARE, sw, RenderingHints.VALUE_ANTIALIAS_ON);
+    if (!SystemInfo.isMac && Registry.is("ide.editor.alternative.folding.icons.painting")) {
+      GraphicsConfig config = GraphicsUtil.setupAAPainting(g);
+      g.setStroke(new BasicStroke((float)getStrokeWidth(), BasicStroke.CAP_SQUARE, BasicStroke.JOIN_ROUND));
 
-    g.setColor(getOutlineColor(active));
-    LinePainter2D.paintPolygon(g, dxPoints, dyPoints, 5, StrokeType.CENTERED_CAPS_SQUARE, sw, RenderingHints.VALUE_ANTIALIAS_ON);
+      int ix1 = (int)Math.round(x1);
+      int ix2 = (int)Math.round(x2);
+      int[] xPoints = {ix1, ix1, ix2, ix2, (int)Math.round(centerX)};
+      int iy1 = (int)Math.round(y + baseHeight);
+      int iy2 = (int)Math.round(y);
+      int[] yPoints = {iy1, iy2, iy2, iy1, (int)Math.round(y + height + (height < 0 ? 1 : 0))};
+      //xPoints[4] -= 1;
 
-    drawLine(g, false, centerX, centerY, width, sw);
+      if (xPoints[4] - xPoints[0] != xPoints[3] - xPoints[4]) {
+        xPoints[0] += (xPoints[4] - xPoints[0]) - (xPoints[3] - xPoints[4]);
+        xPoints[1] = xPoints[0];
+      }
+
+      g.setColor(myEditor.getBackgroundColor());
+      g.fillPolygon(xPoints, yPoints, 5);
+
+      g.setColor(getOutlineColor(active));
+      g.drawPolygon(xPoints, yPoints, 5);
+
+      int w = xPoints[3] - xPoints[0];
+      int off = (int)Math.round(getSquareInnerOffset(w));
+      int minusY = (int)Math.round(centerY);
+      g.drawLine(xPoints[0] + off, minusY, xPoints[3] - off, minusY);
+      config.restore();
+    } else {
+      g.setColor(myEditor.getBackgroundColor());
+      LinePainter2D.fillPolygon(g, dxPoints, dyPoints, 5, StrokeType.CENTERED_CAPS_SQUARE, sw, RenderingHints.VALUE_ANTIALIAS_ON);
+
+      g.setColor(getOutlineColor(active));
+      LinePainter2D.paintPolygon(g, dxPoints, dyPoints, 5, StrokeType.CENTERED_CAPS_SQUARE, sw, RenderingHints.VALUE_ANTIALIAS_ON);
+
+      drawLine(g, false, centerX, centerY, width, sw);
+    }
   }
 
   private void drawLine(Graphics2D g, boolean vertical, double centerX, double centerY, double width, double strokeWidth) {
@@ -1348,16 +1382,37 @@ class EditorGutterComponentImpl extends EditorGutterComponentEx implements Mouse
     Rectangle2D rect = RectanglePainter2D.align(g,
                                                 EnumSet.of(LinePainter2D.Align.CENTER_X, LinePainter2D.Align.CENTER_Y),
                                                 centerX, centerY, width, width, StrokeType.CENTERED, sw);
-
     g.setColor(myEditor.getBackgroundColor());
     RectanglePainter2D.FILL.paint(g, rect, null, StrokeType.CENTERED, sw, RenderingHints.VALUE_ANTIALIAS_OFF);
 
     g.setColor(getOutlineColor(active));
     RectanglePainter2D.DRAW.paint(g, rect, null, StrokeType.CENTERED, sw, RenderingHints.VALUE_ANTIALIAS_OFF);
 
-    drawLine(g, false, centerX, centerY, width, sw);
-    if (plus) {
-      drawLine(g, true, centerX, centerY, width, sw);
+    if (!SystemInfo.isMac && Registry.is("ide.editor.alternative.folding.icons.painting")) {
+      double dx1 = rect.getX();
+      double dx2 = dx1 + rect.getWidth() - 1;
+      int x1 = (int)Math.round(dx1);
+      int x2 = (int)Math.round(dx2);
+      int cX = (int)Math.round(centerX);
+      int cY = (int)Math.round(centerY);
+
+      if (cX - x1 != x2 - cX) {
+        x1 +=  (x2 - cX) - (cX - x1);
+      }
+
+      GraphicsConfig config = GraphicsUtil.setupAAPainting(g);
+      g.setStroke(new BasicStroke((float)getStrokeWidth(), BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER));
+      int off = (int)Math.round(getSquareInnerOffset(x2 - x1));
+      g.drawLine(x1 + off, cY, x2 - off, cY);
+      if (plus) {
+        g.drawLine((x1 + x2) / 2, cY - (x2 - x1 - 2*off) / 2, (x1 + x2) / 2, cY + (x2 - x1 - 2*off) / 2);
+      }
+      config.restore();
+    } else {
+      drawLine(g, false, centerX, centerY, width, sw);
+      if (plus) {
+        drawLine(g, true, centerX, centerY, width, sw);
+      }
     }
   }
 
