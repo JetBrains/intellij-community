7742d2bc70c64013668d03c590f1db6db4fd19da
Olga.Klisho <Olga.Klisho@jetbrains.com>
2020-02-04 00:39:38 +0300

    IDEA-CR-57753 tests common environment extracted
    GitOrigin-RevId: fa0c29742766ca0d4f219d2f1708f0fcdf25a8bd

--- plugins/junit/testData/codeInsight/malformedParameterized/streamArgumentsMethodFix/StreamArgumentsMethod.after.java
+++ plugins/junit/testData/codeInsight/malformedParameterized/streamArgumentsMethodFix/StreamArgumentsMethod.after.java
@@ -1,5 +1,3 @@
-// "Create method 'Stream parmeters()'" "true"
-
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
