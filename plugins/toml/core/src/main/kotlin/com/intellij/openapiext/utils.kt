49c14d8d6dd744d7401771bc958349d705ae8b43
ortem <ortem00@gmail.com>
2020-02-04 12:38:45 +0300

    Replace `Application.isInternal` with `isInternal`
    GitOrigin-RevId: 83cc62ec3960d81c439c808311aff27a71e68421

--- plugins/toml/core/src/main/kotlin/com/intellij/openapiext/utils.kt
+++ plugins/toml/core/src/main/kotlin/com/intellij/openapiext/utils.kt
@@ -10,3 +10,4 @@ import com.intellij.openapi.application.ApplicationManager
 val isUnitTestMode: Boolean get() = ApplicationManager.getApplication().isUnitTestMode
 val isHeadlessEnvironment: Boolean get() = ApplicationManager.getApplication().isHeadlessEnvironment
 val isDispatchThread: Boolean get() = ApplicationManager.getApplication().isDispatchThread
+val isInternal: Boolean get() = ApplicationManager.getApplication().isInternal
