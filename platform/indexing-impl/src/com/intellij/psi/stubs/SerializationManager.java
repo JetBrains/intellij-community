1838cc83338e6a63504580ac8fe13f20041180a0
Dmitro Batko <dmitry.batkovich@jetbrains.com>
2020-02-04 14:33:04 +0300

    indexes: move stub index related stuff from indexing-api to indexing-impl
    GitOrigin-RevId: a4c288b5e6019854c02571da9822736d7ffdf94a

--- platform/indexing-impl/src/com/intellij/psi/stubs/SerializationManager.java
+++ platform/indexing-impl/src/com/intellij/psi/stubs/SerializationManager.java
@@ -1,14 +1,11 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.stubs;
 
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.util.Computable;
-import com.intellij.psi.tree.IElementType;
-import com.intellij.psi.tree.StubFileElementType;
 import org.jetbrains.annotations.ApiStatus;
 
-import java.util.List;
-
+@ApiStatus.Internal
 public abstract class SerializationManager {
   public static SerializationManager getInstance() {
     return ApplicationManager.getApplication().getService(SerializationManager.class);
