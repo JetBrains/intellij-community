14a81b289e7d8a256a756f0cf96d3ff919454387
Dmitriy.Panov <Dmitriy.Panov@jetbrains.com>
2020-02-06 14:59:56 +0300

    IDEA-232217 Apple credentials removed from env vars
    GitOrigin-RevId: 3f9e6fe8a8fce3647e8ea63076a6e87b0a486c6e

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/MacDmgBuilder.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/MacDmgBuilder.groovy
@@ -21,7 +21,6 @@ class MacDmgBuilder {
   private final MacHostProperties macHostProperties
   private final String remoteDir
   private final MacDistributionCustomizer customizer
-  private static final def ENV_FOR_MAC_BUILDER = ['APPL_USER', 'APPL_PASSWORD', 'ARTIFACTORY_URL']
 
   private MacDmgBuilder(BuildContext buildContext, MacDistributionCustomizer customizer, String remoteDir, MacHostProperties macHostProperties) {
     this.customizer = customizer
@@ -268,15 +267,8 @@ class MacDmgBuilder {
                            notarize ? "yes" : "no",
                            customizer.bundleIdentifier,
       ]
-      def env = ''
-      ENV_FOR_MAC_BUILDER.each {
-        def value = System.getenv(it)
-        if (value != null && !value.isEmpty()) {
-          env += "$it=$value "
-        }
-      }
 
-      sshExec("$env$remoteDir/signapp.sh ${args.join(" ")}", "signapp.log")
+      sshExec("$remoteDir/signapp.sh ${args.join(" ")}", "signapp.log")
 
       buildContext.messages.progress("Downloading signed ${targetFile.name} from ${macHostProperties.host}")
       ant.delete(file: targetFile.path)
