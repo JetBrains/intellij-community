cd62c6abba52a8a8bfef6ca9de68bdfc6a1ce55a
Alexander Lobas <Alexander.Lobas@jetbrains.com>
2020-02-07 20:43:20 +0300

    IDEA-208558 Wrong counting of downloaded enabled plugins in the tab Installed
    GitOrigin-RevId: bb8ab548fee345f21287ed4485fb4ac252907702

--- platform/platform-impl/src/com/intellij/ide/plugins/newui/PluginsGroup.java
+++ platform/platform-impl/src/com/intellij/ide/plugins/newui/PluginsGroup.java
@@ -3,6 +3,7 @@ package com.intellij.ide.plugins.newui;
 
 import com.intellij.ide.IdeBundle;
 import com.intellij.ide.plugins.IdeaPluginDescriptor;
+import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.ui.components.labels.LinkLabel;
 import com.intellij.util.containers.ContainerUtil;
@@ -12,6 +13,7 @@ import org.jetbrains.annotations.NotNull;
 import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Alexander Lobas
@@ -57,8 +59,9 @@ public class PluginsGroup {
 
   public void titleWithEnabled(@NotNull MyPluginModel pluginModel) {
     int enabled = 0;
+    Map<PluginId, Boolean> enabledMap = pluginModel.getEnabledMap();
     for (IdeaPluginDescriptor descriptor : descriptors) {
-      if (pluginModel.isEnabled(descriptor)) {
+      if (enabledMap.get(descriptor.getPluginId()) != null && pluginModel.isEnabled(descriptor)) {
         enabled++;
       }
     }
