30fecec399edcccbbb3d8cc5ebb39dd23274ba4f
Nikolay Rykunov <Nikolay.Rykunov@jetbrains.com>
2020-02-10 12:16:10 +0300

    [git] Interactive Rebase Dialog: Use proper class in tests
    GitOrigin-RevId: 4a35c1b96ca64f8e0109d7cb9ff82e6adf86d294

--- plugins/git4idea/tests/git4idea/rebase/GitSingleRepoRebaseTest.kt
+++ plugins/git4idea/tests/git4idea/rebase/GitSingleRepoRebaseTest.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package git4idea.rebase
 
 import com.intellij.dvcs.DvcsUtil
@@ -11,7 +11,7 @@ import git4idea.branch.GitBranchUiHandler
 import git4idea.branch.GitBranchWorker
 import git4idea.branch.GitRebaseParams
 import git4idea.config.GitVersionSpecialty
-import git4idea.rebase.interactive.GitInteractiveRebaseDialog
+import git4idea.rebase.interactive.dialog.GitInteractiveRebaseDialog
 import git4idea.repo.GitRepository
 import git4idea.test.*
 import org.junit.Assume
