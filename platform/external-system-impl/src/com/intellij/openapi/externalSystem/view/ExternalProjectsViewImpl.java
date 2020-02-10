05dea86fd1e3c88a6aa5cafe249d91b78997ad18
Nikita Skvortsov <nikita.skvortsov@jetbrains.com>
2020-02-10 16:20:50 +0300

    IDEA-CR-58232: [gradle] Fix fully recursive nodes update in Gradle toolwindow. Only update loaded nodes, use async tree api IDEA-232092
    (cherry picked from commit 8cc4ceb18a3118892d77a571ec87a81386d5d139)
    
    GitOrigin-RevId: b81ce149f08263cae5c77aee51b7aa354b85ae05

--- platform/external-system-impl/src/com/intellij/openapi/externalSystem/view/ExternalProjectsViewImpl.java
+++ platform/external-system-impl/src/com/intellij/openapi/externalSystem/view/ExternalProjectsViewImpl.java
@@ -189,23 +189,13 @@ public class ExternalProjectsViewImpl extends SimpleToolWindowPanel implements D
     });
 
     getShortcutsManager().addListener(() -> {
-      scheduleTasksUpdate();
-
-      scheduleStructureRequest(() -> {
-        assert myStructure != null;
-        myStructure.updateNodes(RunConfigurationNode.class);
-      });
+      scheduleTaskAndRunConfigUpdate();
     });
 
     getTaskActivator().addListener(new ExternalSystemTaskActivator.Listener() {
       @Override
       public void tasksActivationChanged() {
-        scheduleTasksUpdate();
-
-        scheduleStructureRequest(() -> {
-          assert myStructure != null;
-          myStructure.updateNodes(RunConfigurationNode.class);
-        });
+        scheduleTaskAndRunConfigUpdate();
       }
     });
 
@@ -254,6 +244,14 @@ public class ExternalProjectsViewImpl extends SimpleToolWindowPanel implements D
 
     scheduleStructureUpdate();
   }
+
+  private void scheduleTaskAndRunConfigUpdate() {
+    scheduleStructureRequest(() -> {
+      assert myStructure != null;
+      myStructure.updateNodesAsync(new SmartList<>(TaskNode.class, RunConfigurationNode.class));
+    });
+  }
+
 
   @Override
   public void handleDoubleClickOrEnter(@NotNull ExternalSystemNode node, @Nullable String actionId, InputEvent inputEvent) {
@@ -523,14 +521,7 @@ public class ExternalProjectsViewImpl extends SimpleToolWindowPanel implements D
       for (T tasksNode : myStructure.getNodes(nodeClass)) {
         tasksNode.cleanUpCache();
       }
-      myStructure.updateNodes(nodeClass);
-    });
-  }
-
-  private void scheduleTasksUpdate() {
-    scheduleStructureRequest(() -> {
-      assert myStructure != null;
-      myStructure.updateNodes(TaskNode.class);
+      myStructure.updateNodesAsync(Collections.singleton(nodeClass));
     });
   }
 
