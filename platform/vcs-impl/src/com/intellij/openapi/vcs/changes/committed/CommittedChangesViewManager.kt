2fbce8054549e53608e544b2370d0042198c4db5
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-02-11 12:42:41 +0100

    fix compilation
    GitOrigin-RevId: 3de2463a25ebe681d7e4febc361f22add418bb20

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/committed/CommittedChangesViewManager.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/committed/CommittedChangesViewManager.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vcs.changes.committed
 
 import com.intellij.openapi.Disposable
@@ -35,7 +35,7 @@ class CommittedChangesViewManager(private val project: Project) : ChangesViewCon
     createCommittedChangesPanel().let {
       panel = it
       content.component = it
-      content.disposer = Disposable { panel = null }
+      content.setDisposer(Disposable { panel = null })
 
       with(project.messageBus.connect(it)) {
         subscribe(VCS_CONFIGURATION_CHANGED, VcsListener { runInEdtIfNotDisposed { updateCommittedChangesProvider() } })
--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/committed/IncomingChangesViewProvider.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/committed/IncomingChangesViewProvider.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vcs.changes.committed
 
 import com.intellij.openapi.Disposable
@@ -26,7 +26,7 @@ class IncomingChangesViewProvider(private val project: Project) : ChangesViewCon
     createIncomingChangesBrowser().let {
       browser = it
       content.component = it
-      content.disposer = Disposable { browser = null }
+      content.setDisposer(Disposable { browser = null })
 
       project.messageBus.connect(it).subscribe(COMMITTED_TOPIC, IncomingChangesListener())
 
