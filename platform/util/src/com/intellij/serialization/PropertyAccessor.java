e4bfebac0a38687c58c3c7f5f2df416fdf62379e
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2020-02-11 23:05:06 +0100

    [platform] deserialization NPE diagnostics (EA-222967)
    GitOrigin-RevId: 7d54fd371cc1a56e607f88c2f0a80f85e063d56d

--- platform/util/src/com/intellij/serialization/PropertyAccessor.java
+++ platform/util/src/com/intellij/serialization/PropertyAccessor.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.serialization;
 
 import com.intellij.openapi.diagnostic.Logger;
@@ -91,6 +91,9 @@ public final class PropertyAccessor implements MutableAccessor {
 
   @Override
   public void set(@NotNull Object host, @Nullable Object value) {
+    if (myWriteMethod == null) {
+      throw new SerializationException(host.getClass().getName() + "::" + myName + " lacks a setter");
+    }
     try {
       myWriteMethod.invoke(host, value);
     }
