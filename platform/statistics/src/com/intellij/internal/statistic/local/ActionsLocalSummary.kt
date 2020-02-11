7ba541eaa5296a031622bf1f0660bfe99f4fe94c
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-02-11 13:13:33 +0100

    IDEA-232670 Don't sync the actions_summary file
    GitOrigin-RevId: 63116912a699887b0d679e41c7157a0c31baa0e6

--- platform/statistics/src/com/intellij/internal/statistic/local/ActionsLocalSummary.kt
+++ platform/statistics/src/com/intellij/internal/statistic/local/ActionsLocalSummary.kt
@@ -1,25 +1,34 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.internal.statistic.local
 
-import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.actionSystem.ex.AnActionListener
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.components.PersistentStateComponent
-import com.intellij.openapi.components.State
-import com.intellij.openapi.components.Storage
+import com.intellij.openapi.components.*
+import com.intellij.util.xmlb.annotations.XMap
 import java.util.*
-import kotlin.collections.HashMap
 
-@State(name = "ActionsLocalSummary", storages = [Storage("actions_summary.xml")])
-class ActionsLocalSummary : PersistentStateComponent<ActionsLocalSummary> {
-  data class ActionSummary(var times: Long = 0, var last: Date = Date())
-
-  var data: MutableMap<String, ActionSummary> = HashMap()
+@State(name = "ActionsLocalSummary", storages = [Storage("actionSummary.xml", roamingType = RoamingType.DISABLED)])
+internal class ActionsLocalSummary : SimplePersistentStateComponent<ActionsLocalSummaryState>(ActionsLocalSummaryState()) {
+  companion object {
+    val instance: ActionsLocalSummary
+      get() = ApplicationManager.getApplication().getService(ActionsLocalSummary::class.java)
+  }
 
   fun updateActionsSummary(actionId: String) {
+    state.updateActionsSummary(actionId)
+  }
+}
+
+internal class ActionsLocalSummaryState : BaseState() {
+  internal data class ActionSummary(var times: Long = 0, var last: Date = Date())
+
+  @get:XMap
+  val data by map<String, ActionSummary>()
+
+  internal fun updateActionsSummary(actionId: String) {
     val actionSummary = data[actionId]
     if (actionSummary == null) {
       data[actionId] = ActionSummary(1, Date())
@@ -28,24 +37,12 @@ class ActionsLocalSummary : PersistentStateComponent<ActionsLocalSummary> {
       actionSummary.last = Date()
       actionSummary.times++
     }
-  }
-
-  override fun getState(): ActionsLocalSummary {
-    return this
-  }
-
-  override fun loadState(state: ActionsLocalSummary) {
-    data = state.data
-  }
-
-  companion object {
-    val instance: ActionsLocalSummary
-      get() = ApplicationManager.getApplication().getService(ActionsLocalSummary::class.java)
+    incrementModificationCount()
   }
 }
 
-class ActionsLocalSummaryListener : AnActionListener {
+private class ActionsLocalSummaryListener : AnActionListener {
   override fun beforeActionPerformed(action: AnAction, dataContext: DataContext, event: AnActionEvent) {
-    ActionsLocalSummary.instance.updateActionsSummary(ActionManager.getInstance().getId(action) ?: action.javaClass.name)
+    ActionsLocalSummary.instance.updateActionsSummary(event.actionManager.getId(action) ?: action.javaClass.name)
   }
 }
