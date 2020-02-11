2dbd813d00f06a5474aaf0fe1d970b1f2b8be7fa
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2020-02-11 20:34:15 +0300

    Minor inspection fixes in DevKit
    GitOrigin-RevId: 2a1a3c5d0fe39467538ff965c6f595a437723c4d

--- plugins/devkit/devkit-core/src/actions/NewActionDialog.java
+++ plugins/devkit/devkit-core/src/actions/NewActionDialog.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.devkit.actions;
 
 import com.intellij.icons.AllIcons;
@@ -23,7 +9,6 @@ import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.keymap.KeymapUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
-import com.intellij.openapi.ui.TextFieldWithBrowseButton;
 import com.intellij.openapi.ui.ValidationInfo;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiDirectory;
@@ -75,7 +60,6 @@ public class NewActionDialog extends DialogWrapper implements ActionData {
   private JButton myClearSecondKeystroke;
   private final ShortcutTextField myFirstKeystrokeEdit;
   private final ShortcutTextField mySecondKeystrokeEdit;
-  private TextFieldWithBrowseButton myIconEdit;
   private final Project myProject;
   private final PsiDirectory myDirectory;
   private ButtonGroup myAnchorButtonGroup;
@@ -254,7 +238,6 @@ public class NewActionDialog extends DialogWrapper implements ActionData {
   }
 
   private static String getKeystrokeText(KeyStroke keyStroke) {
-    //noinspection HardCodedStringLiteral
     return keyStroke != null ?
            keyStroke.toString().replaceAll("pressed ", "").replaceAll("released ", "") :
            null;
