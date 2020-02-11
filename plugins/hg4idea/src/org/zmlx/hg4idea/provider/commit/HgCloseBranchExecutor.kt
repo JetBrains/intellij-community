dd6eb2b59c75c56b4d1bb168fb132aa1042f44ba
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2020-02-11 13:45:13 +0530

    hg: Convert "HgCloseBranchExecutor", "HgMQNewExecutor" to kotlin
    GitOrigin-RevId: a57d50ebf8ca80f1235e622332ce095f3e2f4611

--- plugins/hg4idea/src/org/zmlx/hg4idea/provider/commit/HgCloseBranchExecutor.kt
+++ plugins/hg4idea/src/org/zmlx/hg4idea/provider/commit/HgCloseBranchExecutor.kt
@@ -1,45 +1,22 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.zmlx.hg4idea.provider.commit;
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.zmlx.hg4idea.provider.commit
 
-import com.intellij.openapi.vcs.changes.CommitContext;
-import com.intellij.openapi.vcs.changes.CommitExecutorBase;
-import com.intellij.openapi.vcs.changes.CommitSession;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NotNull;
-import org.zmlx.hg4idea.repo.HgRepository;
+import com.intellij.openapi.vcs.changes.CommitContext
+import com.intellij.openapi.vcs.changes.CommitExecutorBase
+import com.intellij.openapi.vcs.changes.CommitSession
+import org.jetbrains.annotations.Nls
+import org.zmlx.hg4idea.repo.HgRepository
 
-import java.util.Collection;
+class HgCloseBranchExecutor(private val myCheckinEnvironment: HgCheckinEnvironment) : CommitExecutorBase() {
+  override fun areChangesRequired(): Boolean = false
 
-public class HgCloseBranchExecutor extends CommitExecutorBase {
-
-  @NotNull private final HgCheckinEnvironment myCheckinEnvironment;
-  @NotNull private static final String CLOSE_BRANCH_TITLE = "Commit And Close";
-
-
-  public HgCloseBranchExecutor(@NotNull HgCheckinEnvironment environment) {
-    myCheckinEnvironment = environment;
-  }
-
-  @Override
-  public boolean areChangesRequired() {
-    return false;
-  }
-
-  @NotNull
   @Nls
-  @Override
-  public String getActionText() {
-    return CLOSE_BRANCH_TITLE;
+  override fun getActionText(): String = "Commit And Close"
+
+  override fun createCommitSession(commitContext: CommitContext): CommitSession {
+    myCheckinEnvironment.setCloseBranch(true)
+    return CommitSession.VCS_COMMIT
   }
 
-  @NotNull
-  @Override
-  public CommitSession createCommitSession(@NotNull CommitContext commitContext) {
-    myCheckinEnvironment.setCloseBranch(true);
-    return CommitSession.VCS_COMMIT;
-  }
-
-  public void setRepositories(@NotNull Collection<HgRepository> repositories) {
-    myCheckinEnvironment.setRepos(repositories);
-  }
+  fun setRepositories(repositories: Collection<HgRepository>) = myCheckinEnvironment.setRepos(repositories)
 }
--- plugins/hg4idea/src/org/zmlx/hg4idea/provider/commit/HgMQNewExecutor.kt
+++ plugins/hg4idea/src/org/zmlx/hg4idea/provider/commit/HgMQNewExecutor.kt
@@ -1,31 +1,18 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.zmlx.hg4idea.provider.commit;
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.zmlx.hg4idea.provider.commit
 
-import com.intellij.openapi.vcs.changes.CommitContext;
-import com.intellij.openapi.vcs.changes.CommitExecutor;
-import com.intellij.openapi.vcs.changes.CommitSession;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.vcs.changes.CommitContext
+import com.intellij.openapi.vcs.changes.CommitExecutor
+import com.intellij.openapi.vcs.changes.CommitSession
+import org.jetbrains.annotations.Nls
 
-public class HgMQNewExecutor implements CommitExecutor {
-  //todo:should be moved to create patch dialog as an EP -> create patch with...  MQ
-  @NotNull private final HgCheckinEnvironment myCheckinEnvironment;
-
-  public HgMQNewExecutor(@NotNull HgCheckinEnvironment checkinEnvironment) {
-    myCheckinEnvironment = checkinEnvironment;
-  }
-
-  @NotNull
+//todo:should be moved to create patch dialog as an EP -> create patch with...  MQ
+class HgMQNewExecutor(private val myCheckinEnvironment: HgCheckinEnvironment) : CommitExecutor {
   @Nls
-  @Override
-  public String getActionText() {
-    return "Create M&Q Patch";
-  }
+  override fun getActionText(): String = "Create M&Q Patch"
 
-  @NotNull
-  @Override
-  public CommitSession createCommitSession(@NotNull CommitContext commitContext) {
-    myCheckinEnvironment.setMqNew();
-    return CommitSession.VCS_COMMIT;
+  override fun createCommitSession(commitContext: CommitContext): CommitSession {
+    myCheckinEnvironment.setMqNew()
+    return CommitSession.VCS_COMMIT
   }
 }
