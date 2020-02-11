004e102cdf9f2649c2beb9e2c329d2731cd3fc15
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2020-02-11 16:43:27 +0300

    Move FileContent and FileContentQueue to 'intellij.platform.lang.impl' module.
    GitOrigin-RevId: ecb02f9b1d6f1fc2a66d4f8c9b2b37020f10c757

--- platform/lang-impl/src/com/intellij/util/indexing/caches/FileContent.java
+++ platform/lang-impl/src/com/intellij/util/indexing/caches/FileContent.java
@@ -1,5 +1,5 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.ide.caches;
+package com.intellij.util.indexing.caches;
 
 import com.intellij.openapi.util.UserDataHolderBase;
 import com.intellij.openapi.vfs.VirtualFile;
