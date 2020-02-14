a11d35b9a577102f9f6c075f9da68e71506f3554
Bas Leijdekkers <basleijdekkers@gmail.com>
2020-02-14 11:22:38 +0100

    SSR: try to fix test
    GitOrigin-RevId: c5dab91bc014502ae1870c34579068ebe2229b9d

--- platform/structuralsearch/testSource/com/intellij/structuralsearch/SSRSerializationTest.java
+++ platform/structuralsearch/testSource/com/intellij/structuralsearch/SSRSerializationTest.java
@@ -3,7 +3,6 @@ package com.intellij.structuralsearch;
 
 import com.intellij.codeInspection.ex.*;
 import com.intellij.openapi.fileTypes.StdFileTypes;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.JDOMUtil;
 import com.intellij.openapi.util.registry.Registry;
 import com.intellij.profile.codeInspection.BaseInspectionProfileManager;
@@ -14,7 +13,6 @@ import com.intellij.structuralsearch.plugin.ui.SearchConfiguration;
 import com.intellij.testFramework.fixtures.CodeInsightFixtureTestCase;
 import org.jdom.Element;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,12 +22,13 @@ import java.util.List;
  */
 public class SSRSerializationTest extends CodeInsightFixtureTestCase {
 
-  private TestInspectionProfile myProfile;
+  private InspectionProfileImpl myProfile;
   private SSBasedInspection myInspection;
 
   @Override
   protected void setUp() throws Exception {
     super.setUp();
+    InspectionProfileImpl.INIT_INSPECTIONS = true;
     Registry.get("ssr.separate.inspections").setValue(true, getTestRootDisposable());
     myInspection = new SSBasedInspection();
     final SearchConfiguration configuration1 = new SearchConfiguration("i", "user defined");
@@ -41,16 +40,18 @@ public class SSRSerializationTest extends CodeInsightFixtureTestCase {
 
       @Override
       public @NotNull List<InspectionToolWrapper> createTools() {
-        return Arrays.asList(new LocalInspectionToolWrapper(myInspection) {
-          @Override
-          public boolean isEnabledByDefault() {
-            return true; // this should no longer be necessary when SSBasedInspection is enabled by default.
-          }
-        });
+        return Arrays.asList(new LocalInspectionToolWrapper(myInspection));
       }
     };
-    myProfile = new TestInspectionProfile(supplier);
-    myProfile.initialize(getProject());
+    myProfile = new InspectionProfileImpl("test", supplier, (BaseInspectionProfileManager)InspectionProfileManager.getInstance());
+    myProfile.enableTool(SSBasedInspection.SHORT_NAME, getProject());
+    myProfile.initInspectionTools(getProject());
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    InspectionProfileImpl.INIT_INSPECTIONS = false;
+    super.tearDown();
   }
 
   private String buildXmlFromProfile() {
@@ -60,8 +61,8 @@ public class SSRSerializationTest extends CodeInsightFixtureTestCase {
   }
 
   public void testSimple() {
-    final List<Tools> tools = myProfile.getAllEnabledInspectionTools(getProject());
-    assertEquals("SSBasedInspection and 1 child tool should be enabled", 2, tools.size());
+    final InspectionToolWrapper[] tools = myProfile.getInspectionTools(null);
+    assertEquals("SSBasedInspection and 1 child tool should be available", 2, tools.length);
   }
 
   public void testDefaultToolsNotWritten() {
@@ -76,12 +77,12 @@ public class SSRSerializationTest extends CodeInsightFixtureTestCase {
 
   public void testModifiedToolShouldBeWritten() {
     final Configuration configuration = myInspection.getConfigurations().get(0);
-    myProfile.setToolEnabled(configuration.getUuid().toString(), false);
+    myProfile.setToolEnabled(configuration.getUuid().toString(), true);
 
     final String expected =
       "<entry version=\"1.0\">\n" +
       "  <option name=\"myName\" value=\"test\" />\n" +
-      "  <inspection_tool class=\"865c0c0b-4ab0-3063-a5ca-a3387c1a8741\" enabled=\"false\" level=\"WARNING\" enabled_by_default=\"false\" />\n" +
+      "  <inspection_tool class=\"865c0c0b-4ab0-3063-a5ca-a3387c1a8741\" enabled=\"true\" level=\"WARNING\" enabled_by_default=\"true\" />\n" +
       "  <inspection_tool class=\"SSBasedInspection\" enabled=\"true\" level=\"WARNING\" enabled_by_default=\"true\">\n" +
       "    <searchConfiguration name=\"i\" text=\"int i;\" recursive=\"false\" caseInsensitive=\"false\" type=\"JAVA\" />\n" +
       "  </inspection_tool>\n" +
@@ -101,17 +102,5 @@ public class SSRSerializationTest extends CodeInsightFixtureTestCase {
       "  </inspection_tool>\n" +
       "</entry>";
     assertEquals(expected, buildXmlFromProfile());
-  }
-
-  private static class TestInspectionProfile extends InspectionProfileImpl {
-
-    TestInspectionProfile(InspectionToolsSupplier supplier) {
-      super("test", supplier, (BaseInspectionProfileManager)InspectionProfileManager.getInstance());
-    }
-
-    @Override
-    public void initialize(@Nullable Project project) {
-      super.initialize(project);
-    }
   }
 }
