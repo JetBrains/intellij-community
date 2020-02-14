e2bed53ae0682d6115e8e816b2a76cd62d4763b0
Eldar Abusalimov <eldar.abusalimov@jetbrains.com>
2020-02-14 05:16:36 +0300

    CPP-3153: PtyCommandLine: Don't create PTY process with input file
    GitOrigin-RevId: 2bbf6fd46fe439295f2da5f316538c2255ce8004

--- platform/platform-util-io/src/com/intellij/execution/configurations/GeneralCommandLine.java
+++ platform/platform-util-io/src/com/intellij/execution/configurations/GeneralCommandLine.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.configurations;
 
 import com.intellij.execution.CommandLineUtil;
@@ -284,6 +284,10 @@ public class GeneralCommandLine implements UserDataHolder {
 
   public void setRedirectErrorStream(boolean redirectErrorStream) {
     withRedirectErrorStream(redirectErrorStream);
+  }
+
+  public File getInputFile() {
+    return myInputFile;
   }
 
   @NotNull
