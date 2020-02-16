a728e1a947564deb41797f4d8e5eb1307008406c
Valentin Kipyatkov <valentin@jetbrains.com>
2020-02-16 16:01:34 +0200

    Moved implementations of suggested rename and change signature into lang-impl module to facilitate use of internal API
    GitOrigin-RevId: 557e0d3c34cfb21e5c049d8648d4a132ed8a345e

--- platform/lang-api/src/com/intellij/refactoring/suggested/SuggestedRefactoringExecution.kt
+++ platform/lang-api/src/com/intellij/refactoring/suggested/SuggestedRefactoringExecution.kt
@@ -1,77 +1,12 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.refactoring.suggested
 
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.application.runWriteAction
-import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.Messages
-import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
-import com.intellij.refactoring.RefactoringBundle
-import com.intellij.refactoring.RefactoringFactory
 
 /**
  * A service performing actual changes in the code when user executes suggested refactoring.
  */
 abstract class SuggestedRefactoringExecution(protected val refactoringSupport: SuggestedRefactoringSupport) {
-  open fun rename(data: SuggestedRenameData, project: Project, editor: Editor) {
-    val relativeCaretOffset = editor.caretModel.offset - anchorOffset(data.declaration)
-
-    val newName = data.declaration.name!!
-    runWriteAction {
-      data.declaration.setName(data.oldName)
-    }
-
-    val refactoring = RefactoringFactory.getInstance(project).createRename(data.declaration, newName, true, true)
-    refactoring.respectAllAutomaticRenames()
-
-    if (refactoring.hasNonCodeUsages() && !ApplicationManager.getApplication().isHeadlessEnvironment) {
-      val question = RefactoringBundle.message("suggested.refactoring.rename.text.occurrences", data.oldName, newName)
-      val result = Messages.showOkCancelDialog(
-        project,
-        question,
-        RefactoringBundle.message("suggested.refactoring.rename.text.occurrences.title"),
-        RefactoringBundle.message("suggested.refactoring.rename.with.preview.button.text"),
-        RefactoringBundle.message("suggested.refactoring.ignore.button.text"),
-        Messages.getQuestionIcon()
-      )
-      if (result != Messages.OK) {
-        refactoring.isSearchInComments = false
-        refactoring.isSearchInNonJavaFiles = false
-      }
-    }
-
-    refactoring.run()
-
-    if (data.declaration.isValid) {
-      editor.caretModel.moveToOffset(relativeCaretOffset + anchorOffset(data.declaration))
-    }
-  }
-
-  open fun changeSignature(
-    data: SuggestedChangeSignatureData,
-    newParameterValues: List<NewParameterValue>,
-    project: Project,
-    editor: Editor
-  ) {
-    val preparedData = prepareChangeSignature(data)
-
-    val relativeCaretOffset = editor.caretModel.offset - anchorOffset(data.declaration)
-
-    val restoreNewSignature = runWriteAction {
-      data.restoreInitialState(refactoringSupport)
-    }
-
-    performChangeSignature(data, newParameterValues, preparedData)
-
-    runWriteAction {
-      PsiDocumentManager.getInstance(project).doPostponedOperationsAndUnblockDocument(editor.document)
-      restoreNewSignature()
-      editor.caretModel.moveToOffset(relativeCaretOffset + anchorOffset(data.declaration))
-    }
-  }
-
   /**
    * Prepares data for Change Signature refactoring while the declaration has state with user changes (the new signature).
    */
@@ -82,10 +17,6 @@ abstract class SuggestedRefactoringExecution(protected val refactoringSupport: S
    * before user changes (the old signature). The list of imports is also reverted to the original state.
    */
   abstract fun performChangeSignature(data: SuggestedChangeSignatureData, newParameterValues: List<NewParameterValue>, preparedData: Any?)
-
-  private fun anchorOffset(declaration: PsiElement): Int {
-    return refactoringSupport.nameRange(declaration)?.startOffset ?: declaration.startOffset
-  }
 
   /**
    * Use this implementation of [SuggestedRefactoringExecution], if only Rename refactoring is supported for the language.
