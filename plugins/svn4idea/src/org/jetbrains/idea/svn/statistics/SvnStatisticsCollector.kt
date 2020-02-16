3deee4e14bcfe64d002ef0ea8746b9e8861eb83e
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-02-16 16:13:21 +0100

    do not export kotlinx-coroutines-jdk8 and module statistics from platform-impl
    GitOrigin-RevId: 1e0f98fedd06dc7789d808025d53b2424141adca

--- plugins/svn4idea/src/org/jetbrains/idea/svn/statistics/SvnStatisticsCollector.kt
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/statistics/SvnStatisticsCollector.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.svn.statistics
 
 import com.intellij.internal.statistic.beans.MetricEvent
@@ -9,7 +9,7 @@ import com.intellij.openapi.project.Project
 import org.jetbrains.idea.svn.NestedCopyType
 import org.jetbrains.idea.svn.SvnVcs
 
-class SvnStatisticsCollector : ProjectUsagesCollector() {
+private class SvnStatisticsCollector : ProjectUsagesCollector() {
   override fun getGroupId() = "svn.configuration"
 
   override fun getMetrics(project: Project): Set<MetricEvent> {
