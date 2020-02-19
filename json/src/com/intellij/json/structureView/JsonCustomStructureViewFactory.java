b680e0d92c66479d469c1c0daa9e551cffd86a7f
Nikita Katkov <Nikita.Katkov@jetbrains.com>
2020-02-19 22:29:17 +0300

    [swagger-plugin] IDEA-233433 Custom structure view factories support for json files
    GitOrigin-RevId: 84a75407c298fff2e6d94f202e45881a7618917c

---
+++ json/src/com/intellij/json/structureView/JsonCustomStructureViewFactory.java
@@ -0,0 +1,24 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.json.structureView;
+
+import com.intellij.ide.structureView.StructureViewBuilder;
+import com.intellij.json.psi.JsonFile;
+import com.intellij.openapi.extensions.ExtensionPointName;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Used for customization of default structure view for json files.
+ */
+public interface JsonCustomStructureViewFactory {
+  ExtensionPointName<JsonCustomStructureViewFactory> EP_NAME = ExtensionPointName.create("com.intellij.json.customStructureViewFactory");
+
+  /**
+   * First not-null builder received from all registered extensions will be used for building structure view.
+   * If extensions list is empty, default implementation is used.
+   *
+   * @return a structure view builder for the given Json file or null if the file doesn't need customized structure view.
+   */
+  @Nullable
+  StructureViewBuilder getStructureViewBuilder(@NotNull final JsonFile jsonFile);
+}
--- json/src/com/intellij/json/structureView/JsonStructureViewBuilderFactory.java
+++ json/src/com/intellij/json/structureView/JsonStructureViewBuilderFactory.java
@@ -1,11 +1,14 @@
 package com.intellij.json.structureView;
 
+import com.intellij.ide.impl.StructureViewWrapperImpl;
 import com.intellij.ide.structureView.StructureViewBuilder;
 import com.intellij.ide.structureView.StructureViewModel;
 import com.intellij.ide.structureView.TreeBasedStructureViewBuilder;
 import com.intellij.json.psi.JsonFile;
 import com.intellij.lang.PsiStructureViewFactory;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.extensions.ExtensionPointUtil;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -14,11 +17,25 @@ import org.jetbrains.annotations.Nullable;
  * @author Mikhail Golubev
  */
 public class JsonStructureViewBuilderFactory implements PsiStructureViewFactory {
+
+  public JsonStructureViewBuilderFactory() {
+    JsonCustomStructureViewFactory.EP_NAME.addExtensionPointListener(
+      () -> ApplicationManager.getApplication().getMessageBus().syncPublisher(StructureViewWrapperImpl.STRUCTURE_CHANGED).run(),
+      ExtensionPointUtil.createKeyedExtensionDisposable(this, PsiStructureViewFactory.EP_NAME.getPoint(null)));
+  }
+
   @Nullable
   @Override
   public StructureViewBuilder getStructureViewBuilder(@NotNull final PsiFile psiFile) {
     if (!(psiFile instanceof JsonFile)) {
       return null;
+    }
+
+    for (JsonCustomStructureViewFactory extension : JsonCustomStructureViewFactory.EP_NAME.getExtensionList()) {
+      final StructureViewBuilder builder = extension.getStructureViewBuilder((JsonFile)psiFile);
+      if (builder != null) {
+        return builder;
+      }
     }
     
     return new TreeBasedStructureViewBuilder() {
