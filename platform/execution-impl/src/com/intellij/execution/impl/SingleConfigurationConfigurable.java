e558acfc91729fa83c0054bbf5bab8b1c84d48fb
Alexander Zolotov <zolotov@jetbrains.com>
2020-02-20 12:20:16 +0300

    Avoid NPE on non-initialized form
    GitOrigin-RevId: e545e2ae5cc324447f9b864d74b4d618c375aacb

--- platform/execution-impl/src/com/intellij/execution/impl/SingleConfigurationConfigurable.java
+++ platform/execution-impl/src/com/intellij/execution/impl/SingleConfigurationConfigurable.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.impl;
 
 import com.intellij.execution.ExecutionBundle;
@@ -180,7 +180,10 @@ public final class SingleConfigurationConfigurable<Config extends RunConfigurati
         return getEditor();
       }
       if (RUN_ON_TARGET_NAME_KEY.is(dataId)) {
-        return ((RunOnTargetComboBox)myComponent.myRunOnComboBox).getSelectedTargetName();
+        RunOnTargetComboBox runOnComboBox = (RunOnTargetComboBox) myComponent.myRunOnComboBox;
+        if (runOnComboBox != null) {
+          return runOnComboBox.getSelectedTargetName();
+        }
       }
       return null;
     });
