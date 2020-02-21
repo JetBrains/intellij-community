1008bf03b9a6c5d343dfab429a7fac51f69b0301
Ivan Semenov <ivan.semenov@jetbrains.com>
2020-02-21 18:05:59 +0300

    IDEA-CR-59918: [github] Pull gutter renderer inside controller to allow disposing inlay
    GitOrigin-RevId: dc24aefb29ed3c13c837fa124d4ed463dcfbde27

--- plugins/github/src/org/jetbrains/plugins/github/pullrequest/comment/ui/GHPREditorCommentableRangesController.kt
+++ plugins/github/src/org/jetbrains/plugins/github/pullrequest/comment/ui/GHPREditorCommentableRangesController.kt
@@ -1,22 +1,25 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.github.pullrequest.comment.ui
 
 import com.intellij.diff.util.LineRange
 import com.intellij.diff.util.Side
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.editor.ex.RangeHighlighterEx
 import com.intellij.openapi.editor.ex.util.EditorUtil
 import com.intellij.openapi.editor.impl.event.MarkupModelListener
+import com.intellij.openapi.editor.markup.GutterIconRenderer
 import com.intellij.openapi.editor.markup.HighlighterLayer
 import com.intellij.openapi.editor.markup.HighlighterTargetArea
+import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.util.Disposer
 import gnu.trove.TIntHashSet
-import org.jetbrains.plugins.github.pullrequest.comment.viewer.GHPRCreateDiffCommentIconRenderer
 import org.jetbrains.plugins.github.ui.util.SingleValueModel
 import org.jetbrains.plugins.github.util.GithubUIUtil
 import java.awt.event.KeyEvent
+import javax.swing.Icon
 import javax.swing.JComponent
 import javax.swing.KeyStroke
 
@@ -32,7 +35,8 @@ class GHPREditorCommentableRangesController(commentableRanges: SingleValueModel<
     val listenerDisposable = Disposer.newDisposable()
     editor.markupModel.addMarkupModelListener(listenerDisposable, object : MarkupModelListener {
       override fun beforeRemoved(highlighter: RangeHighlighterEx) {
-        val iconRenderer = highlighter.gutterIconRenderer as? GHPRCreateDiffCommentIconRenderer ?: return
+        val iconRenderer = highlighter.gutterIconRenderer as? CreateCommentIconRenderer ?: return
+        iconRenderer.disposeInlay()
         commentableLines.remove(iconRenderer.line)
       }
     })
@@ -53,36 +57,51 @@ class GHPREditorCommentableRangesController(commentableRanges: SingleValueModel<
       editor.markupModel
         .addRangeHighlighterAndChangeAttributes(start, end, HighlighterLayer.LAST, null, HighlighterTargetArea.EXACT_RANGE,
                                                 false) { highlighter ->
-          highlighter.gutterIconRenderer = GHPRCreateDiffCommentIconRenderer(i, object : DumbAwareAction() {
-            private var inlay: Pair<JComponent, Disposable>? = null
-
-            override fun actionPerformed(e: AnActionEvent) {
-              if (inlay?.let { GithubUIUtil.focusPanel(it.first) } != null) return
-
-              val (side, line) = lineLocationCalculator(i) ?: return
-
-              val component =
-                componentFactory.createCommentComponent(side, line) {
-                  inlay?.let { Disposer.dispose(it.second) }
-                  inlay = null
-                }
-              val disposable = inlaysManager.insertAfter(i, component) ?: return
-              val newInlay = component to disposable
-              //TODO: replace with focus listeners
-              component.registerKeyboardAction({
-                                                 disposable.let {
-                                                   Disposer.dispose(it)
-                                                   inlay = null
-                                                 }
-                                               },
-                                               KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
-                                               JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT)
-              GithubUIUtil.focusPanel(component)
-
-              inlay = newInlay
-            }
-          })
+          highlighter.gutterIconRenderer = CreateCommentIconRenderer(i)
         }
+    }
+  }
+
+  private inner class CreateCommentIconRenderer(val line: Int) : GutterIconRenderer(), DumbAware {
+
+    private var inlay: Pair<JComponent, Disposable>? = null
+
+    override fun getClickAction(): DumbAwareAction = CreateCommentAction(line)
+    override fun isNavigateAction() = true
+    override fun getIcon(): Icon = AllIcons.General.InlineAdd
+    override fun equals(other: Any?): Boolean = other is CreateCommentIconRenderer && line == other.line
+    override fun hashCode(): Int = line.hashCode()
+
+    private inner class CreateCommentAction(private val editorLine: Int) : DumbAwareAction() {
+
+      override fun actionPerformed(e: AnActionEvent) {
+        if (inlay?.let { GithubUIUtil.focusPanel(it.first) } != null) return
+
+        val (side, line) = lineLocationCalculator(editorLine) ?: return
+
+        val component =
+          componentFactory.createCommentComponent(side, line) {
+            inlay?.let { Disposer.dispose(it.second) }
+            inlay = null
+          }
+        val disposable = inlaysManager.insertAfter(editorLine, component) ?: return
+        val newInlay = component to disposable
+        component.registerKeyboardAction({
+                                           disposable.let {
+                                             Disposer.dispose(it)
+                                             inlay = null
+                                           }
+                                         },
+                                         KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+                                         JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT)
+        GithubUIUtil.focusPanel(component)
+
+        inlay = newInlay
+      }
+    }
+
+    fun disposeInlay() {
+      inlay?.second?.let { Disposer.dispose(it) }
     }
   }
 }
