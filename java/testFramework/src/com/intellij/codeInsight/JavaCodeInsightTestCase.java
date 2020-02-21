c028919af978e13b1f543ef5dd0c99cd1ec30722
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-02-21 11:26:53 +0100

    do not get PostprocessReformattingAspect as component
    GitOrigin-RevId: 0fa698b35192e54930e2a54e5f5975b67408f0b4

--- java/testFramework/src/com/intellij/codeInsight/JavaCodeInsightTestCase.java
+++ java/testFramework/src/com/intellij/codeInsight/JavaCodeInsightTestCase.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight;
 
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer;
@@ -416,7 +416,7 @@ public abstract class JavaCodeInsightTestCase extends JavaPsiTestCase {
 
   protected void checkResultByFile(@NotNull final String filePath, final boolean stripTrailingSpaces) throws Exception {
     WriteCommandAction.writeCommandAction(getProject()).run(() -> {
-      getProject().getComponent(PostprocessReformattingAspect.class).doPostponedFormatting();
+      PostprocessReformattingAspect.getInstance(getProject()).doPostponedFormatting();
       if (stripTrailingSpaces) {
         ((DocumentImpl)myEditor.getDocument()).stripTrailingSpaces(getProject());
       }
