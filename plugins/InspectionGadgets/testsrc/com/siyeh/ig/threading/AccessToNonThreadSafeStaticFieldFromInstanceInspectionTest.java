239c3907b93da5d71af242b9ddbe78fd82b7f1e8
Bas Leijdekkers <basleijdekkers@gmail.com>
2020-02-22 15:06:30 +0100

    IG: expand list of non-thread-safe types (part of IDEA-151419)
    GitOrigin-RevId: a2b14778bfc53fa871cf43f8ca1d8270a780d99a

--- plugins/InspectionGadgets/testsrc/com/siyeh/ig/threading/AccessToNonThreadSafeStaticFieldFromInstanceInspectionTest.java
+++ plugins/InspectionGadgets/testsrc/com/siyeh/ig/threading/AccessToNonThreadSafeStaticFieldFromInstanceInspectionTest.java
@@ -1,3 +1,4 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.siyeh.ig.threading;
 
 import com.intellij.codeInspection.LocalInspectionTool;
@@ -15,6 +16,9 @@ public class AccessToNonThreadSafeStaticFieldFromInstanceInspectionTest extends 
       "import java.util.Date;" +
       "public abstract class DateFormat {" +
       "  public String format(Date d) {}" +
+      "  public static DateFormat getDateInstance() {" +
+      "    return null;" +
+      "  }" +
       "}",
       "package java.text;" +
       "public class SimpleDateFormat extends DateFormat {" +
@@ -29,7 +33,7 @@ public class AccessToNonThreadSafeStaticFieldFromInstanceInspectionTest extends 
            "import java.text.SimpleDateFormat;" +
            "class C {" +
            "    private static final SimpleDateFormat df = new SimpleDateFormat(\"yyyy-MM-dd\");" +
-           "    private String s = /*Access to non thread-safe static field 'df' of type 'java.text.SimpleDateFormat'*/df/**/.format(new Date());" +
+           "    private String s = /*Access to non-thread-safe static field 'df' of type 'java.text.SimpleDateFormat'*/df/**/.format(new Date());" +
            "}");
   }
 
@@ -38,12 +42,24 @@ public class AccessToNonThreadSafeStaticFieldFromInstanceInspectionTest extends 
            "import java.text.DateFormat;" +
            "import java.text.SimpleDateFormat;" +
            "class C {" +
-           "    private static final DateFormat df = new SimpleDateFormat(\"yyyy-MM-dd\");" +
+           "    private static final Object df = new SimpleDateFormat(\"yyyy-MM-dd\");" +
            "    private static final Date d = new Date();" +
-           "    private static final String s1 = df.format(d);" +
+           "    private static final String s1 = ((DateFormat)df).format(d);" +
            "    String m() {" +
-           "      return /*Access to non thread-safe static field 'df' of type 'java.text.SimpleDateFormat'*/df/**/.format(d);" +
+           "      return ((DateFormat)/*Access to non-thread-safe static field 'df' of type 'java.text.SimpleDateFormat'*/df/**/).format(d);" +
            "    }" +
+           "}");
+  }
+
+  public void testGetDateInstance() {
+    doTest("import java.util.Date;" +
+           "import java.text.DateFormat;" +
+           "import java.text.SimpleDateFormat;" +
+           "class MyClass {" +
+           "  private static final DateFormat F = DateFormat.getDateInstance();" +
+           "  public String getFormat() {" +
+           "    return /*Access to non-thread-safe static field 'F' of type 'java.text.DateFormat'*/F/**/.format(new Date());" +
+           "  }" +
            "}");
   }
 
