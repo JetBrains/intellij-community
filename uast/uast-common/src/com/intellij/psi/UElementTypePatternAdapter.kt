329b59c61af02819d61bfe5f5e1f23e27a7957b5
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2020-02-25 15:31:02 +0300

    IDEA-CR-58423 UAST: remove RecursionManager from UElement type pattern adapter
    GitOrigin-RevId: fea031fc289dbce4a870100680b68f4a96f5b08f

--- uast/uast-common/src/com/intellij/psi/UElementTypePatternAdapter.kt
+++ uast/uast-common/src/com/intellij/psi/UElementTypePatternAdapter.kt
@@ -1,8 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi
 
-import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.util.RecursionManager
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.patterns.ElementPattern
 import com.intellij.patterns.ElementPatternCondition
 import com.intellij.patterns.InitialPatternCondition
@@ -15,15 +14,14 @@ internal class UElementTypePatternAdapter(private val supportedUElementTypes: Li
   override fun accepts(o: Any?, context: ProcessingContext?): Boolean {
     if (o !is PsiElement) return false
     if (context == null) {
-      Logger.getInstance(UElementTypePatternAdapter::class.java).error("UElementTypePatternAdapter should not be called with null context")
+      logger<UElementTypePatternAdapter>().error("UElementTypePatternAdapter should not be called with null context")
       return false
     }
 
-    return RecursionManager.doPreventingRecursion(this, false) {
-      if (getOrCreateCachedElement(o, context, supportedUElementTypes) == null) return@doPreventingRecursion false
-      context.put(REQUESTED_PSI_ELEMENT, o)
-      true
-    } ?: false
+    if (getOrCreateCachedElement(o, context, supportedUElementTypes) == null) return false
+
+    context.put(REQUESTED_PSI_ELEMENT, o)
+    return true
   }
 
   private val condition = ElementPatternCondition(object : InitialPatternCondition<PsiElement>(PsiElement::class.java) {
