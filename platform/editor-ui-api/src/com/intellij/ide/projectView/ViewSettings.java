d7fc0cdd81d04efdd4c1e40483fa7d3fa1481ad3
Anna.Kozlova <anna.kozlova@jetbrains.com>
2020-02-25 10:34:50 +0100

    view settings: ensure super doesn't attempt to initialize inheritor
    GitOrigin-RevId: c4970d856de6a2f4e72ca3f31c7a097cc906b888

--- platform/editor-ui-api/src/com/intellij/ide/projectView/ViewSettings.java
+++ platform/editor-ui-api/src/com/intellij/ide/projectView/ViewSettings.java
@@ -11,7 +11,42 @@ public interface ViewSettings extends NodeOptions {
   /**
    * The default view settings for the project view.
    */
-  ViewSettings DEFAULT = Immutable.DEFAULT;
+  ViewSettings DEFAULT = new ViewSettings() {
+    @Override
+    public boolean isShowMembers() {
+      return false;
+    }
+
+    @Override
+    public boolean isAbbreviatePackageNames() {
+      return false;
+    }
+
+    @Override
+    public boolean isFlattenPackages() {
+      return false;
+    }
+
+    @Override
+    public boolean isHideEmptyMiddlePackages() {
+      return false;
+    }
+
+    @Override
+    public boolean isShowLibraryContents() {
+      return false;
+    }
+
+    @Override
+    public boolean isStructureView() {
+      return false;
+    }
+
+    @Override
+    public boolean isShowModules() {
+      return true;
+    }
+  };
 
   /**
    * @return {@code true} if directories (folders or packages) should be separated from files, {@code false} otherwise.
