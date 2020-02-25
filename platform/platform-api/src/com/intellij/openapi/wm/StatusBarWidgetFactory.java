a6fea6c0698fbf78bb0b53825de99ebc26d5819d
Yann Cébron <yann.cebron@jetbrains.com>
2020-02-25 16:33:35 +0100

    StatusBarWidgetFactory: javadoc
    (cherry picked from commit 91bc3841d21183da30932711731a514e044016bf) IDEA-CR-58829
    
    GitOrigin-RevId: b8fc0b7b49b12d008c458a8c1c479d2b177463ef

--- platform/platform-api/src/com/intellij/openapi/wm/StatusBarWidgetFactory.java
+++ platform/platform-api/src/com/intellij/openapi/wm/StatusBarWidgetFactory.java
@@ -9,24 +9,23 @@ import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Extension point for adding new widgets to status bar
+ * Extension point for adding user-configurable widgets to the status bar.
  */
 @ApiStatus.Experimental
 public interface StatusBarWidgetFactory {
   ExtensionPointName<StatusBarWidgetFactory> EP_NAME = new ExtensionPointName<>("com.intellij.statusBarWidgetFactory");
 
   /**
-   * @return widget identifier. It's used to store visibility settings for a particular widgets.
+   * @return Widget identifier. Used to store visibility settings.
    */
   @NonNls
   @NotNull
   String getId();
 
   /**
-   * @return widget's display name. It's used to refer a widget in UI.
-   * <p>
-   * E.g. It might be used for "Enable/disable <display name>" action names
-   * or for checkbox texts in the settings.
+   * @return Widget's display name. Used to refer a widget in UI,
+   * e.g. for "Enable/disable &lt;display name>" action names
+   * or for checkbox texts in settings.
    */
   @Nls
   @NotNull
@@ -38,9 +37,11 @@ public interface StatusBarWidgetFactory {
    * `False` means that IDE won't try to create a widget or will dispose it on {@link com.intellij.openapi.wm.impl.status.widget.StatusBarWidgetsManager#updateWidget} call.
    * <p>
    * E.g. `false` can be returned for
-   * - notifications widget if Event log is shown as a tool window
-   * - memory indicator widget if it is disabled in the appearance settings
-   * - git widget if there are no git repos in a project
+   * <ul>
+   * <li>notifications widget if Event log is shown as a tool window</li>
+   * <li>memory indicator widget if it is disabled in the appearance settings</li>
+   * <li>git widget if there are no git repos in a project</li>
+   * </ul>
    * <p>
    * Whenever availability is changed, you need to call {@link com.intellij.openapi.wm.impl.status.widget.StatusBarWidgetsManager#updateWidget(StatusBarWidgetFactory)}
    * explicitly to get status bar updated.
@@ -48,13 +49,13 @@ public interface StatusBarWidgetFactory {
   boolean isAvailable(@NotNull Project project);
 
   /**
-   * Creates a widget to be added on the status bar.
+   * Creates a widget to be added to the status bar.
    * <p>
    * Once the method is invoked on project initialization, the widget won't be recreated or disposed implicitly.
    * <p>
-   * You may need to recreate If you still need to recreate a widget if:
-   * - its availability is changed. See {@link #isAvailable(Project)}
-   * - its visibility is changed. See {@link com.intellij.openapi.wm.impl.status.widget.StatusBarWidgetSettings}
+   * You may need to recreate it if:
+   * <li>its availability has changed. See {@link #isAvailable(Project)}</li>
+   * <li>its visibility has changed. See {@link com.intellij.openapi.wm.impl.status.widget.StatusBarWidgetSettings}</li>
    * <p>
    * To do this, you need to explicitly invoke {@link com.intellij.openapi.wm.impl.status.widget.StatusBarWidgetsManager#updateWidget(StatusBarWidgetFactory)}
    * to recreate the widget and re-add it to the status bar.
@@ -65,30 +66,28 @@ public interface StatusBarWidgetFactory {
   void disposeWidget(@NotNull StatusBarWidget widget);
 
   /**
-   * @return whether widget can be enabled on the given status bar right now.
-   * Status bar's context menu with enable/disable action heavily depends on the result of this method.
+   * @return Returns whether the widget can be enabled on the given status bar right now.
+   * Status bar's context menu with enable/disable action depends on the result of this method.
    * <p>
    * E.g. {@link com.intellij.openapi.wm.impl.status.EditorBasedWidget} are available if editor is opened in a frame that given status bar is attached to
    * <p>
-   * Based can be useful for creating editor based widgets {@link com.intellij.openapi.wm.impl.status.widget.StatusBarEditorBasedWidgetFactory}
+   * For creating editor based widgets see also {@link com.intellij.openapi.wm.impl.status.widget.StatusBarEditorBasedWidgetFactory}
    */
   boolean canBeEnabledOn(@NotNull StatusBar statusBar);
 
   /**
-   * @return true if widget should be created by default.
-   * Otherwise, a user will need to enabled it explicitely via Status bar context menu or via the Settings.
+   * @return {@code true} if the widget should be created by default.
+   * Otherwise, the user must enable it explicitly via status bar context menu or settings.
    */
   default boolean isEnabledByDefault() {
     return true;
   }
 
   /**
-   * @return whether user should be able to enable or disable the widget.
+   * @return Returns whether the user should be able to enable or disable the widget.
    * <p>
-   * Some widgets are controlled by application-level settings (e.g. Memory indicator)
-   * or cannot be disabled (e.g. Write thread indicator).
-   * <p>
-   * So they should not be shown neither in the Status bar context menu nor in the Settings.
+   * Some widgets are controlled by application-level settings (e.g., Memory indicator)
+   * or cannot be disabled (e.g., Write thread indicator) and thus shouldn't be configurable via status bar context menu or settings.
    */
   default boolean isConfigurable() {
     return true;
