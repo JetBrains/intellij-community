f9be13c7c103ad21f112be15638ec788917d2222
Alexander Zolotov <goldifit@gmail.com>
2020-02-25 15:49:11 +0300

    Migrate pycharm pro and community to the new tips&tricks bundles
    GitOrigin-RevId: 6c4cff942a62ff19c70a767322e33409ed3674bc

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/CommunityLibraryLicenses.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/CommunityLibraryLicenses.groovy
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.intellij.build
 
 import groovy.transform.CompileStatic
@@ -624,5 +624,6 @@ class CommunityLibraryLicenses {
     jetbrainsLibrary("rd-text"),
     jetbrainsLibrary("tcServiceMessages"),
     jetbrainsLibrary("tips-idea-ce"),
+    jetbrainsLibrary("tips-pycharm-community"),
   ] as List<LibraryLicense>
 }
