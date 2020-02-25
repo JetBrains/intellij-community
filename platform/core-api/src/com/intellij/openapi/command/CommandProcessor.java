9204d83405084042b11014ffffbfed2a6928c9ab
Peter Gromov <peter@jetbrains.com>
2020-02-25 11:14:45 +0100

    IDEA-CR-58788: revert making PostprocessReformattingAspect a service
    because PomModel should know about it from the very beginning, deterministically
    
    (cherry picked from commit 90b4f1597609ac6f8b64d4628a32954cd1f5090d)
    
    GitOrigin-RevId: d5d2ed3e51a304dd101aa7b8dbc2ac3a47b6faa4

--- platform/core-api/src/com/intellij/openapi/command/CommandProcessor.java
+++ platform/core-api/src/com/intellij/openapi/command/CommandProcessor.java
@@ -1,8 +1,9 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.command;
 
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -18,7 +19,7 @@ import org.jetbrains.annotations.Nullable;
  */
 public abstract class CommandProcessor {
   public static CommandProcessor getInstance() {
-    return ApplicationManager.getApplication().getService(CommandProcessor.class);
+    return ServiceManager.getService(CommandProcessor.class);
   }
 
   /**
