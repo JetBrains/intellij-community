a76c5df4f8d4100882956434ca01261fe8384382
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2020-02-26 12:20:53 +0300

    build scripts: show compilation progress
    GitOrigin-RevId: ffc8b206c07336db2824517bf790d2d5cc584177

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/JpsCompilationRunner.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/JpsCompilationRunner.groovy
@@ -1,33 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.intellij.build.impl
 
 import com.intellij.openapi.diagnostic.CompositeLogger
@@ -334,7 +305,7 @@ class JpsCompilationRunner {
 
     private void reportProgress(Collection<? extends BuildTarget<?>> targets, String targetSpecificMessage) {
       def targetsString = targets.collect { StringUtil.decapitalize(it.presentableName) }.join(", ")
-      String progressText = progress > 0 ? " (${(int)(100 * progress)}%)" : ""
+      String progressText = progress >= 0 ? " (${(int)(100 * progress)}%)" : ""
       String targetSpecificText = !targetSpecificMessage.isEmpty() ? ", $targetSpecificMessage" : ""
       context.messages.progress("Compiling$progressText: $targetsString$targetSpecificText")
     }
