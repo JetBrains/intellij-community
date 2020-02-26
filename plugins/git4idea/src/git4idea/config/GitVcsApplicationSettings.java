10cd4d0334b269ada98bde92dc9e4a499526039c
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2020-02-26 21:22:35 +0300

    git: drop the built-in ssh client: IDEA-230903
    Native is the only option in UI since 2019.1, and nobody seem to have any problems with it.
    
    GitOrigin-RevId: 413e129c17b146bb55d4c9479eadecf6f9dd7602

--- plugins/git4idea/src/git4idea/config/GitVcsApplicationSettings.java
+++ plugins/git4idea/src/git4idea/config/GitVcsApplicationSettings.java
@@ -1,9 +1,8 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package git4idea.config;
 
 import com.intellij.openapi.components.*;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.registry.Registry;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -55,10 +54,6 @@ public final class GitVcsApplicationSettings implements PersistentStateComponent
 
   public void setPathToGit(@Nullable String pathToGit) {
     myState.myPathToGit = pathToGit;
-  }
-
-  public boolean isUseIdeaSsh() {
-    return Registry.is("git.use.builtin.ssh");
   }
 
   public boolean isIgnoreWhitespaces() {
