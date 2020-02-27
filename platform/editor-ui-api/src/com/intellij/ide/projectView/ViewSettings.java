d4b6bdd100a909ae8a6d19040bd375005918f1ac
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2020-02-27 17:14:59 +0300

    cleanup: add default methods and create corresponding tests
    GitOrigin-RevId: 37c631e4c55e975d1775029f0045bd16d699a425

--- platform/editor-ui-api/src/com/intellij/ide/projectView/ViewSettings.java
+++ platform/editor-ui-api/src/com/intellij/ide/projectView/ViewSettings.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.projectView;
 
 import com.intellij.ide.util.treeView.NodeOptions;
@@ -12,40 +12,6 @@ public interface ViewSettings extends NodeOptions {
    * The default view settings for the project view.
    */
   ViewSettings DEFAULT = new ViewSettings() {
-    @Override
-    public boolean isShowMembers() {
-      return false;
-    }
-
-    @Override
-    public boolean isAbbreviatePackageNames() {
-      return false;
-    }
-
-    @Override
-    public boolean isFlattenPackages() {
-      return false;
-    }
-
-    @Override
-    public boolean isHideEmptyMiddlePackages() {
-      return false;
-    }
-
-    @Override
-    public boolean isShowLibraryContents() {
-      return false;
-    }
-
-    @Override
-    public boolean isStructureView() {
-      return false;
-    }
-
-    @Override
-    public boolean isShowModules() {
-      return true;
-    }
   };
 
   /**
@@ -60,21 +26,27 @@ public interface ViewSettings extends NodeOptions {
    *
    * @return true if class members are displayed, false otherwise.
    */
-  boolean isShowMembers();
+  default boolean isShowMembers() {
+    return false;
+  }
 
   /**
    * Checks if the project view displays the structure view pane.
    *
    * @return true if the structure view is displayed, false otherwise.
    */
-  boolean isStructureView();
+  default boolean isStructureView() {
+    return false;
+  }
 
   /**
    * Checks if modules are shown on the "Packages" tab of the project view.
    *
    * @return true if the modules are shown, false otherwise.
    */
-  boolean isShowModules();
+  default boolean isShowModules() {
+    return true;
+  }
 
   /**
    * @return {@code true} if modules should be shown in a flat list without grouping accordingly to qualified names
