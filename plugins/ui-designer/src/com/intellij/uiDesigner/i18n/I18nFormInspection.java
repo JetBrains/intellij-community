1f573793e23579066141fc775d4fdc8f999d7210
nik <Nikolay.Chashnikov@jetbrains.com>
2020-02-27 12:16:32 +0300

    IDEA-CR-58937: [ui designer] don't highlight properties annotated as @NonNls via external annotation as hard coded literals (IDEA-233945)
    (cherry picked from commit 872735ef784fafd79871fe83c5f9781f2fa7536a)
    
    GitOrigin-RevId: fa6a268bf5f9ee9655f353d989f9b8e7c8febce8

--- plugins/ui-designer/src/com/intellij/uiDesigner/i18n/I18nFormInspection.java
+++ plugins/ui-designer/src/com/intellij/uiDesigner/i18n/I18nFormInspection.java
@@ -100,7 +100,7 @@ public class I18nFormInspection extends StringDescriptorInspection {
       PsiParameter[] parameters = setter.getParameterList().getParameters();
       if (parameters.length == 1 &&
           "java.lang.String".equals(parameters[0].getType().getCanonicalText()) &&
-          AnnotationUtil.isAnnotated(parameters [0], AnnotationUtil.NON_NLS, 0)) {
+          AnnotationUtil.isAnnotated(parameters [0], AnnotationUtil.NON_NLS, AnnotationUtil.CHECK_EXTERNAL)) {
         return true;
       }
     }
