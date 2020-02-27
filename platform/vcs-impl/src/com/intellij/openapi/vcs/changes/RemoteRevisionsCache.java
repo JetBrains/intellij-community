a4eda0490188b9a6fd94c1faebdb58c623d36507
Aleksey Pivovarov <AMPivovarov@gmail.com>
2020-02-27 18:30:17 +0300

    vcs: abort background task on project dispose
    GitOrigin-RevId: c7ec0a4744ad30764a10cfd10e334218f78f3e6d

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/RemoteRevisionsCache.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/RemoteRevisionsCache.java
@@ -1,7 +1,6 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vcs.changes;
 
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.progress.ProcessCanceledException;
@@ -118,13 +117,14 @@ public class RemoteRevisionsCache implements VcsListener {
     if (! VcsConfiguration.getInstance(myProject).isChangedOnServerEnabled()) {
       manageAlarm();
     } else {
-      ApplicationManager.getApplication().executeOnPooledThread(() -> {
+      BackgroundTaskUtil.executeOnPooledThread(myProject, () -> {
         try {
           updateRoots();
           myRemoteRevisionsNumbersCache.directoryMappingChanged();
           myRemoteRevisionsStateCache.directoryMappingChanged();
           manageAlarm();
-        } catch (ProcessCanceledException ignore) {
+        }
+        catch (ProcessCanceledException ignore) {
         }
       });
     }
