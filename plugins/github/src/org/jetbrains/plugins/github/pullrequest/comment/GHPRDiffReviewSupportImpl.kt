bf4768ee119e4dcadc014ee6e536406e4a4ce709
Ivan Semenov <ivan.semenov@jetbrains.com>
2020-02-28 16:16:35 +0300

    IDEA-CR-59918: [github] Use loading model for review threads
    Better UX for toggling comment visibility
    
    GitOrigin-RevId: d012c9b6843a4772809aad224bca297ff24be91f

--- plugins/github/src/org/jetbrains/plugins/github/pullrequest/comment/GHPRDiffReviewSupportImpl.kt
+++ plugins/github/src/org/jetbrains/plugins/github/pullrequest/comment/GHPRDiffReviewSupportImpl.kt
@@ -8,7 +8,6 @@ import com.intellij.diff.tools.util.side.TwosideTextDiffViewer
 import com.intellij.diff.util.Range
 import com.intellij.execution.process.ProcessIOExecutorService
 import com.intellij.openapi.Disposable
-import com.intellij.openapi.diagnostic.logger
 import org.jetbrains.plugins.github.api.data.GHUser
 import org.jetbrains.plugins.github.api.data.pullrequest.GHPullRequestReviewThread
 import org.jetbrains.plugins.github.pullrequest.avatars.CachingGithubAvatarIconsProvider
@@ -17,9 +16,11 @@ import org.jetbrains.plugins.github.pullrequest.comment.viewer.GHPRSimpleOneside
 import org.jetbrains.plugins.github.pullrequest.comment.viewer.GHPRTwosideDiffViewerReviewThreadsHandler
 import org.jetbrains.plugins.github.pullrequest.comment.viewer.GHPRUnifiedDiffViewerReviewThreadsHandler
 import org.jetbrains.plugins.github.pullrequest.data.GHPRReviewDataProvider
+import org.jetbrains.plugins.github.pullrequest.ui.GHCompletableFutureLoadingModel
+import org.jetbrains.plugins.github.pullrequest.ui.GHLoadingModel
+import org.jetbrains.plugins.github.pullrequest.ui.GHSimpleLoadingModel
 import org.jetbrains.plugins.github.pullrequest.ui.changes.GHPRCreateDiffCommentParametersHelper
 import org.jetbrains.plugins.github.ui.util.SingleValueModel
-import org.jetbrains.plugins.github.util.handleOnEdt
 import org.jetbrains.plugins.github.util.successAsync
 import kotlin.properties.Delegates
 
@@ -31,19 +32,20 @@ class GHPRDiffReviewSupportImpl(private val reviewDataProvider: GHPRReviewDataPr
                                 private val currentUser: GHUser)
   : GHPRDiffReviewSupport {
 
+  private var reviewThreadsLoadingModel: GHSimpleLoadingModel<List<GHPRDiffReviewThreadMapping>>? = null
   private val reviewThreadsModel = SingleValueModel<List<GHPRDiffReviewThreadMapping>?>(null)
 
-  override var isLoadingReviewThreads: Boolean = false
-    private set
+  override val isLoadingReviewThreads: Boolean
+    get() = reviewThreadsLoadingModel?.loading == true
 
   override var showReviewThreads by Delegates.observable(true) { _, _, newValue ->
-    if (newValue) reloadReviewThreads()
-    else reviewThreadsModel.value = null
+    updateReviewThreads()
   }
 
   override fun install(viewer: DiffViewerBase) {
     val diffRangesModel = SingleValueModel(if (reviewDataProvider.canComment()) diffRanges else null)
-    loadReviewThreads(reviewThreadsModel, viewer)
+
+    loadReviewThreads(viewer)
 
     val componentsFactory = GHPRDiffEditorReviewComponentsFactoryImpl(reviewDataProvider,
                                                                       createCommentParametersHelper,
@@ -63,32 +65,29 @@ class GHPRDiffReviewSupportImpl(private val reviewDataProvider: GHPRReviewDataPr
     reviewDataProvider.resetReviewThreads()
   }
 
-  private fun loadReviewThreads(threadsModel: SingleValueModel<List<GHPRDiffReviewThreadMapping>?>, disposable: Disposable) {
-    doLoadReviewThreads(threadsModel, disposable)
+  private fun loadReviewThreads(disposable: Disposable) {
+    val loadingModel = GHCompletableFutureLoadingModel<List<GHPRDiffReviewThreadMapping>>(disposable).apply {
+      addStateChangeListener(object : GHLoadingModel.StateChangeListener {
+        override fun onLoadingCompleted() = updateReviewThreads()
+      })
+    }
+    reviewThreadsLoadingModel = loadingModel
+
+    doLoadReviewThreads(loadingModel)
     reviewDataProvider.addReviewThreadsListener(disposable) {
-      doLoadReviewThreads(threadsModel, disposable)
+      doLoadReviewThreads(loadingModel)
     }
   }
 
-  private fun doLoadReviewThreads(threadsModel: SingleValueModel<List<GHPRDiffReviewThreadMapping>?>, disposable: Disposable) {
-    isLoadingReviewThreads = true
-    reviewDataProvider.loadReviewThreads()
-      .successAsync(ProcessIOExecutorService.INSTANCE) {
-        it.mapNotNull(reviewThreadMapper)
-      }
-      .handleOnEdt(disposable) { result, error ->
-        if (result != null) {
-          if (showReviewThreads)
-            threadsModel.value = result
-        }
-        if (error != null) {
-          LOG.info("Failed to load review threads", error)
-        }
-        isLoadingReviewThreads = false
-      }
+  private fun doLoadReviewThreads(model: GHCompletableFutureLoadingModel<List<GHPRDiffReviewThreadMapping>>) {
+    model.future = reviewDataProvider.loadReviewThreads().successAsync(ProcessIOExecutorService.INSTANCE) {
+      it.mapNotNull(reviewThreadMapper)
+    }
   }
 
-  companion object {
-    val LOG = logger<GHPRDiffReviewSupportImpl>()
+  private fun updateReviewThreads() {
+    val loadingModel = reviewThreadsLoadingModel ?: return
+    if (loadingModel.loading) return
+    if (showReviewThreads) reviewThreadsModel.value = loadingModel.result else reviewThreadsModel.value = null
   }
 }
