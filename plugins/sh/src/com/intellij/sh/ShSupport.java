840a401ce0ed2677348c0b2cc799687c0f342bfe
jansorg <github@joachim-ansorg.de>
2020-02-28 10:56:47 +0300

    IDEA-CR-59015: [intellij-sh] Allow variable declarations outside of an assignment command
    (cherry picked from commit c00d87d10b12b8fc80721c0db86537a015f36cfe)
    
    GitOrigin-RevId: 844c7bfca6b51fe96d0a4e45eae1648ad40dc0b2

--- plugins/sh/src/com/intellij/sh/ShSupport.java
+++ plugins/sh/src/com/intellij/sh/ShSupport.java
@@ -5,11 +5,18 @@ import com.intellij.execution.Executor;
 import com.intellij.execution.configurations.RunProfileState;
 import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.openapi.components.ServiceManager;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
+import com.intellij.psi.impl.source.resolve.reference.ReferenceProvidersRegistry;
+import com.intellij.sh.codeInsight.ShFunctionReference;
+import com.intellij.sh.psi.ShLiteral;
+import com.intellij.sh.psi.ShString;
 import com.intellij.sh.psi.ShVariable;
 import com.intellij.sh.run.ShRunConfiguration;
 import com.intellij.sh.run.ShRunConfigurationProfileState;
+import com.intellij.util.ArrayUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public interface ShSupport {
   static ShSupport getInstance() { return ServiceManager.getService(ShSupport.class); }
@@ -24,8 +31,29 @@ public interface ShSupport {
                                         @NotNull ShRunConfiguration configuration);
 
   PsiReference @NotNull [] getVariableReferences(@NotNull ShVariable v);
-  
-  
+
+  PsiReference @NotNull [] getLiteralReferences(@NotNull ShLiteral o);
+
+  /**
+   * Retrieve the name of the literal, if it's used as a variable declaration.
+   *
+   * @param l the literal
+   * @return The name of the literal, if it's used as a variable declaration. Otherwise {@code null} is returned.
+   * @see com.intellij.psi.PsiNameIdentifierOwner
+   */
+  @Nullable
+  String getName(@NotNull ShLiteral l);
+
+  /**
+   * Retrieve the name identifier of this literal, if it's used as a variable declaration.
+   *
+   * @param l The literal
+   * @return A non-null element representing the name if the literal is used as a variable declaration. Otherwise {@code null} is returned.
+   * @see com.intellij.psi.PsiNameIdentifierOwner
+   */
+  @Nullable
+  PsiElement getNameIdentifier(@NotNull ShLiteral l);
+
   class Impl implements ShSupport {
     @Override
     public boolean isExternalFormatterEnabled() { return true; }
@@ -44,6 +72,23 @@ public interface ShSupport {
     @Override
     public PsiReference @NotNull [] getVariableReferences(@NotNull ShVariable v) {
       return PsiReference.EMPTY_ARRAY;
+    }
+
+    @Override
+    public PsiReference @NotNull [] getLiteralReferences(@NotNull ShLiteral o) {
+      return o instanceof ShString || o.getWord() != null
+             ? ArrayUtil.prepend(new ShFunctionReference(o), ReferenceProvidersRegistry.getReferencesFromProviders(o))
+             : PsiReference.EMPTY_ARRAY;
+    }
+
+    @Override
+    public @Nullable String getName(@NotNull ShLiteral l) {
+      return null;
+    }
+
+    @Override
+    public @Nullable PsiElement getNameIdentifier(@NotNull ShLiteral l) {
+      return null;
     }
   }
 }
