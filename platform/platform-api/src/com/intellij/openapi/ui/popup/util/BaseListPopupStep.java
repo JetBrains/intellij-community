00e2abfa1f0325ca1799cfbe0b261fdc8391c2a1
Dmitry.Krasilschikov <kradima@jetbrains.com>
2020-03-01 17:16:44 +0200

    `@Nls` for `BaseListPopupStep` title
    GitOrigin-RevId: 8542a85c427b546a3bd2576ff5893c4abf18f645

--- platform/platform-api/src/com/intellij/openapi/ui/popup/util/BaseListPopupStep.java
+++ platform/platform-api/src/com/intellij/openapi/ui/popup/util/BaseListPopupStep.java
@@ -4,6 +4,7 @@ package com.intellij.openapi.ui.popup.util;
 import com.intellij.openapi.ui.popup.ListPopupStep;
 import com.intellij.openapi.ui.popup.ListSeparator;
 import com.intellij.openapi.ui.popup.PopupStep;
+import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -20,33 +21,33 @@ public class BaseListPopupStep<T> extends BaseStep<T> implements ListPopupStep<T
   private int myDefaultOptionIndex = -1;
 
   @SafeVarargs
-  public BaseListPopupStep(@Nullable String title, T @NotNull ... values) {
+  public BaseListPopupStep(@Nls(capitalization = Nls.Capitalization.Title) @Nullable String title, T @NotNull ... values) {
     this(title, values, new Icon[]{});
   }
 
-  public BaseListPopupStep(@Nullable String title, List<? extends T> values) {
+  public BaseListPopupStep(@Nls(capitalization = Nls.Capitalization.Title) @Nullable String title, List<? extends T> values) {
     this(title, values, new ArrayList<>());
   }
 
-  public BaseListPopupStep(@Nullable String title, T[] values, Icon[] icons) {
+  public BaseListPopupStep(@Nls(capitalization = Nls.Capitalization.Title) @Nullable String title, T[] values, Icon[] icons) {
     this(title, Arrays.asList(values), Arrays.asList(icons));
   }
 
-  public BaseListPopupStep(@Nullable String aTitle, @NotNull List<? extends T> aValues, Icon aSameIcon) {
+  public BaseListPopupStep(@Nls(capitalization = Nls.Capitalization.Title) @Nullable String title, @NotNull List<? extends T> aValues, Icon aSameIcon) {
     List<Icon> icons = new ArrayList<>();
     for (int i = 0; i < aValues.size(); i++) {
       icons.add(aSameIcon);
     }
-    init(aTitle, aValues, icons);
+    init(title, aValues, icons);
   }
 
-  public BaseListPopupStep(@Nullable String title, @NotNull List<? extends T> values, List<? extends Icon> icons) {
+  public BaseListPopupStep(@Nls(capitalization = Nls.Capitalization.Title) @Nullable String title, @NotNull List<? extends T> values, List<? extends Icon> icons) {
     init(title, values, icons);
   }
 
   protected BaseListPopupStep() { }
 
-  protected final void init(@Nullable String title, @NotNull List<? extends T> values, @Nullable List<? extends Icon> icons) {
+  protected final void init(@Nls(capitalization = Nls.Capitalization.Title) @Nullable String title, @NotNull List<? extends T> values, @Nullable List<? extends Icon> icons) {
     myTitle = title;
     myValues = new ArrayList<>(values);
     myIcons = icons;
