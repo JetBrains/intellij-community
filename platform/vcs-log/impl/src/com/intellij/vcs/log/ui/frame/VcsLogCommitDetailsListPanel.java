9755df1582b942eb29fdf16029ec9df1e0981367
Nikolay Rykunov <Nikolay.Rykunov@jetbrains.com>
2020-03-02 16:03:49 +0300

    [vcs] IDEA-234274: Show empty text if model is empty. Revalidate content panel when it was rebuilt
    (cherry picked from commit 928f64d6d7dfcd16b27583f74035b8bc70deb8f7)
    
    GitOrigin-RevId: 145a81f5615b2f896a5f075d4bd0cd66fbb797ae

--- platform/vcs-log/impl/src/com/intellij/vcs/log/ui/frame/VcsLogCommitDetailsListPanel.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/ui/frame/VcsLogCommitDetailsListPanel.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.vcs.log.ui.frame;
 
 import com.google.common.primitives.Ints;
@@ -160,7 +160,6 @@ public class VcsLogCommitDetailsListPanel extends CommitDetailsListPanel<CommitP
     @Override
     protected void onSelection(int @NotNull [] selection) {
       cancelResolve();
-      setStatusText("");
 
       int shownPanelsCount = rebuildPanel(selection.length);
       mySelection = Ints.asList(Arrays.copyOf(selection, shownPanelsCount));
@@ -212,6 +211,7 @@ public class VcsLogCommitDetailsListPanel extends CommitDetailsListPanel<CommitP
     private void setEmpty(@Nls @NotNull String text) {
       setStatusText(text);
       mySelection = ContainerUtil.emptyList();
+      setCommits(ContainerUtil.emptyList());
     }
   }
 }
