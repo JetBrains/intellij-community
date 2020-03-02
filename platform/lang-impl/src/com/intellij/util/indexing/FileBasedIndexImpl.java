084a9a843e8ae9fd1444bbf3033557449350b1a9
Dmitro Batko <dmitry.batkovich@jetbrains.com>
2020-03-02 21:26:00 +0300

    indexes: reset storage buffering handler state on plugin load/unload (IDEA-234122)
    GitOrigin-RevId: 366f0a3b8b80447c84352d2adc0a23d5865f269c

--- platform/lang-impl/src/com/intellij/util/indexing/FileBasedIndexImpl.java
+++ platform/lang-impl/src/com/intellij/util/indexing/FileBasedIndexImpl.java
@@ -300,6 +300,7 @@ public final class FileBasedIndexImpl extends FileBasedIndexEx {
 
   void initComponent() {
     LOG.assertTrue(myRegisteredIndexes == null);
+    myStorageBufferingHandler.resetState();
     myRegisteredIndexes = new RegisteredIndexes(myFileDocumentManager, this);
     myRegisteredIndexes.initializeIndexes(new FileBasedIndexDataInitialization(this));
   }
