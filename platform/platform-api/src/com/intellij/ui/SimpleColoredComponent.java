e175040fc08ac72d2f050b487a5742fa7a1b1bb5
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2020-03-03 17:44:14 +0300

    IDEA-233160: Editor tab names are cut on macOS Catalina
    GitOrigin-RevId: 836f4f0c92d2dc4e7b3e9c37cfe3b98e9c5fc37d

--- platform/platform-api/src/com/intellij/ui/SimpleColoredComponent.java
+++ platform/platform-api/src/com/intellij/ui/SimpleColoredComponent.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ui;
 
 import com.intellij.ide.BrowserUtil;
@@ -103,6 +103,7 @@ public class SimpleColoredComponent extends JComponent implements Accessible, Co
   @Override
   public void updateUI() {
     UISettings.setupComponentAntialiasing(this);
+    putClientProperty(RenderingHints.KEY_FRACTIONALMETRICS, UIManager.getDefaults().get(RenderingHints.KEY_FRACTIONALMETRICS));
   }
 
   @NotNull
