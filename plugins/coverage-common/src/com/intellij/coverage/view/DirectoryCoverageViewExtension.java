df5e5b720397e8b49914f68d4f141192d7d6d40b
Dmitry.Krasilschikov <kradima@jetbrains.com>
2020-03-03 18:28:29 +0200

    IDEA-231343 localize `Coverage` strings
    GitOrigin-RevId: d13c07d4bd722e8126ae6bf694af1f74cd9c77e9

--- plugins/coverage-common/src/com/intellij/coverage/view/DirectoryCoverageViewExtension.java
+++ plugins/coverage-common/src/com/intellij/coverage/view/DirectoryCoverageViewExtension.java
@@ -2,6 +2,7 @@
 package com.intellij.coverage.view;
 
 import com.intellij.coverage.CoverageAnnotator;
+import com.intellij.coverage.CoverageBundle;
 import com.intellij.coverage.CoverageSuitesBundle;
 import com.intellij.ide.util.treeView.AbstractTreeNode;
 import com.intellij.openapi.application.ReadAction;
@@ -32,7 +33,8 @@ public class DirectoryCoverageViewExtension extends CoverageViewExtension {
 
   @Override
   public ColumnInfo[] createColumnInfos() {
-    return new ColumnInfo[]{new ElementColumnInfo(), new PercentageCoverageColumnInfo(1, "Statistics, %", mySuitesBundle, myStateBean)};
+    return new ColumnInfo[]{new ElementColumnInfo(),
+      new PercentageCoverageColumnInfo(1, CoverageBundle.message("table.column.name.statistics"), mySuitesBundle, myStateBean)};
   }
 
   @Override
@@ -40,7 +42,7 @@ public class DirectoryCoverageViewExtension extends CoverageViewExtension {
     String statInfo = myAnnotator.getDirCoverageInformationString((PsiDirectory)node.getValue(),
                                                                   mySuitesBundle,
                                                                   myCoverageDataManager);
-    statInfo = StringUtil.notNullize(statInfo, "No coverage");
+    statInfo = StringUtil.notNullize(statInfo, CoverageBundle.message("node.summary.no.coverage"));
     return statInfo + " in '" + node.toString() + "'";
   }
 
