525a10a3c306524ba871c89c2d2a6c6e3b10ab93
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2020-03-03 12:58:40 +0300

    Icon antialiasing [IDEA-CR-59176]
    GitOrigin-RevId: 2193d0eb47435e16b8625bb829197d3e7ec15252

--- platform/platform-impl/src/com/intellij/ide/lightEdit/LightEditSaveStatusIcon.java
+++ platform/platform-impl/src/com/intellij/ide/lightEdit/LightEditSaveStatusIcon.java
@@ -1,8 +1,10 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.lightEdit;
 
+import com.intellij.openapi.ui.GraphicsConfig;
 import com.intellij.ui.scale.JBUIScale;
 import com.intellij.util.ui.ColorIcon;
+import com.intellij.util.ui.GraphicsUtil;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
@@ -18,12 +20,14 @@ public class LightEditSaveStatusIcon extends ColorIcon {
 
   @Override
   public void paintIcon(Component component, Graphics g, int i, int j) {
+    final GraphicsConfig config = GraphicsUtil.setupAAPainting(g);
     final int iconWidth = getIconWidth();
     final int iconHeight = getIconHeight();
     g.setColor(getIconColor());
 
     final int size = Math.min(iconHeight, iconWidth);
     g.fillOval(i, j, size, size);
+    config.restore();
   }
 
   static Icon create(@NotNull Color color) {
