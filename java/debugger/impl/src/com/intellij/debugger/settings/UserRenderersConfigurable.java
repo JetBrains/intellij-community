23cb4658535437848a5f7d6b5a96414a2de44915
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2020-03-03 17:19:23 +0300

    remove unused code
    GitOrigin-RevId: 148b091892e44d30bcc358e4fe355f25c606a3c8

--- java/debugger/impl/src/com/intellij/debugger/settings/UserRenderersConfigurable.java
+++ java/debugger/impl/src/com/intellij/debugger/settings/UserRenderersConfigurable.java
@@ -176,10 +176,6 @@ public final class UserRenderersConfigurable extends JPanel implements Configura
   }
 
   private class AddAction implements AnActionButtonRunnable {
-    //public AddAction() {
-    //  super(JavaDebuggerBundle.message("button.add"), JavaDebuggerBundle.message("user.renderers.configurable.button.description.add"), ADD_ICON);
-    //}
-
     @Override
     public void run(AnActionButton button) {
       NodeRenderer renderer = (NodeRenderer)NodeRendererSettings.getInstance().createRenderer(CompoundTypeRenderer.UNIQUE_ID);
@@ -189,11 +185,6 @@ public final class UserRenderersConfigurable extends JPanel implements Configura
   }
 
   private class RemoveAction implements AnActionButtonRunnable {
-    //public RemoveAction() {
-    //  super(JavaDebuggerBundle.message("button.remove"), JavaDebuggerBundle.message("user.renderers.configurable.button.description.remove"), REMOVE_ICON);
-    //}
-
-
     @Override
     public void run(AnActionButton button) {
       myRendererChooser.getSelectedElements().forEach(myRendererChooser::removeElement);
@@ -225,9 +216,6 @@ public final class UserRenderersConfigurable extends JPanel implements Configura
     private final boolean myMoveUp;
 
     MoveAction(boolean up) {
-      //super(up? JavaDebuggerBundle.message("button.move.up") : JavaDebuggerBundle.message("button.move.down"),
-      //      up? JavaDebuggerBundle.message("user.renderers.configurable.button.description.move.up") : JavaDebuggerBundle.message("user.renderers.configurable.button.description.move.down"),
-      //      up? UP_ICON : DOWN_ICON );
       myMoveUp = up;
     }
 
