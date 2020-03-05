b328b5f2b75f5d2aae2bb68bc5bddf805adb7a46
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2020-03-05 17:37:03 +0300

    [tabs] remove incorrect annotation
    GitOrigin-RevId: c0e51f73f0c9b53d4a172e5d147dfffb3d9cc29b

--- platform/vcs-log/impl/src/com/intellij/vcs/log/impl/VcsLogTabsManager.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/impl/VcsLogTabsManager.java
@@ -93,7 +93,7 @@ public class VcsLogTabsManager {
 
   @NotNull
   private static String generateShortDisplayName(@NotNull VcsLogUi ui) {
-    @Nullable VcsLogFilterCollection filters = ui.getFilterUi().getFilters();
+    VcsLogFilterCollection filters = ui.getFilterUi().getFilters();
     if (filters.isEmpty()) return VcsLogBundle.message("vcs.log.tab.suffix.no.filters");
     return StringUtil.shortenTextWithEllipsis(VcsLogFiltersKt.getPresentation(filters), 150, 20);
   }
