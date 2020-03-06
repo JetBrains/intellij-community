182623cfd8538ee01dc3d707bb74d3f4e6aa24aa
Dmitry Jemerov <yole@jetbrains.com>
2020-03-06 11:56:47 +0100

    IDEA-CR-59561: Don't query custom regions when requesting structure view members in navbar (IDEA-234691)
    (cherry picked from commit 00ef092ee3f765438abe29ebf6106345b39b51d8)
    
    GitOrigin-RevId: cd550f8dbb69d9e87cd1a4a751f2b3b1292e7280

--- plugins/yaml/src/org/jetbrains/yaml/structureView/DuplicatedPsiTreeElementBase.java
+++ plugins/yaml/src/org/jetbrains/yaml/structureView/DuplicatedPsiTreeElementBase.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.yaml.structureView;
 
 import com.intellij.ide.structureView.StructureViewTreeElement;
@@ -78,7 +78,7 @@ abstract class DuplicatedPsiTreeElementBase<T extends PsiElement & Navigatable> 
   }
 
   private StructureViewTreeElement @NotNull [] doGetChildren() {
-    return PsiTreeElementBase.mergeWithExtensions(getElement(), getChildrenBase());
+    return PsiTreeElementBase.mergeWithExtensions(getElement(), getChildrenBase(), false);
   }
 
   @Override
