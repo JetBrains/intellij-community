3c31dd7944743abe0927cdd1a4cd35d077e3d937
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2020-03-10 13:54:34 +0300

    Provide ability to start IDE on clean configs: IDEA-233305
    Add an action "Restore Default Settings" to start with clean configs, teach the "Import Settings" action to be able to choose not only an exported "settings.zip" file, but also a config directory.
    
    Both actions create a marker file "migrate.config" with a path with original config dir to migrate from (import settings) or without any content (restore default), and restart the IDE. On the next start the IDE reads the file and overwrites current config if needed.
    
    A backup is created before overwriting settings. To support the case when settings are migrate from the backup dir, current settings are backed up first to a temp directory, and then moved to the standard backup directory after migration.
    
    (cherry picked from commit cc2b9ba9f620162f3a95324eac4856739311a496)
    
    GitOrigin-RevId: d4d447603bb4b121e58b33f413ed322f015d3c11

--- platform/platform-impl/src/com/intellij/idea/StartupUtil.java
+++ platform/platform-impl/src/com/intellij/idea/StartupUtil.java
@@ -19,10 +19,7 @@ import com.intellij.ide.instrument.WriteIntentLockInstrumenter;
 import com.intellij.ide.plugins.PluginManagerCore;
 import com.intellij.ide.ui.laf.IntelliJLaf;
 import com.intellij.jna.JnaLoader;
-import com.intellij.openapi.application.ApplicationInfo;
-import com.intellij.openapi.application.ApplicationNamesInfo;
-import com.intellij.openapi.application.ConfigImportHelper;
-import com.intellij.openapi.application.PathManager;
+import com.intellij.openapi.application.*;
 import com.intellij.openapi.application.impl.ApplicationInfoImpl;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.IconLoader;
@@ -191,7 +188,9 @@ public final class StartupUtil {
     activity = activity.endAndStart("config path existence check");
 
     // this check must be performed before system directories are locked
-    boolean configImportNeeded = !Main.isHeadless() && !Files.exists(configPath);
+    boolean configImportNeeded = !Main.isHeadless() &&
+                                 (!Files.exists(configPath) ||
+                                  CustomConfigMigrationOptionKt.needsCustomConfigMigration());
 
     activity = activity.endAndStart("system dirs checking");
     // note: uses config directory
