6cfcf116035387d6f5f42344064913bcc5253d24
Aleksey Pivovarov <AMPivovarov@gmail.com>
2020-03-11 17:17:13 +0300

    ui: javadoc
    GitOrigin-RevId: 26ba37497c591bfec21459bccbf36a8c204a0764

--- platform/platform-impl/src/com/intellij/internal/inspector/UiInspectorContextProvider.java
+++ platform/platform-impl/src/com/intellij/internal/inspector/UiInspectorContextProvider.java
@@ -3,6 +3,12 @@ package com.intellij.internal.inspector;
 
 import java.util.List;
 
+/**
+ * Allows to pass additional information about component to the UI Inspector.
+ * <p>
+ * Can be registered on a {@link javax.swing.JComponent} using {@link UiInspectorUtil#registerProvider}
+ * or implemented by {@link java.awt.Component}/{@link javax.accessibility.AccessibleContext} directly.
+ */
 public interface UiInspectorContextProvider {
   List<PropertyBean> getUiInspectorContext();
 }
