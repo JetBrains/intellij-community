593806f3853884a621d16f77cf652792c44a5cca
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-03-11 11:02:25 +0100

    IDEA-CR-59780 fix getting service
    (cherry picked from commit 76907588ce2d759c39a6b3cb564e1a44d43e29a5)
    
    GitOrigin-RevId: bc16e10f1fec0546eb7bd00a735131931e191f1a

--- plugins/svn4idea/src/org/jetbrains/idea/svn/branchConfig/SvnLoadedBranchesStorage.kt
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/branchConfig/SvnLoadedBranchesStorage.kt
@@ -25,7 +25,7 @@ internal class SvnLoadedBranchesStorage(private val project: Project) {
 
   companion object {
     @JvmStatic
-    fun getInstance() = service<SvnLoadedBranchesStorage>()
+    fun getInstance(project: Project) = project.service<SvnLoadedBranchesStorage>()
   }
 
   init {
