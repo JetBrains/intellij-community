6e3ab68cbfcdcc04df8d38b5b9c6f3be47c9fe07
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2020-03-11 20:48:33 +0300

    [vcs] convert ChangesBrowserFilePathNode to kotlin
    GitOrigin-RevId: 1178fcf86e536f47c65b0ba1727e15956c6568bd

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/ChangesBrowserFilePathNode.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/ChangesBrowserFilePathNode.kt
@@ -1,119 +1,91 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.vcs.changes.ui
 
-package com.intellij.openapi.vcs.changes.ui;
-
-import com.intellij.openapi.util.SystemInfo;
-import com.intellij.openapi.util.io.FileUtil;
-import com.intellij.openapi.vcs.FilePath;
-import com.intellij.openapi.vcs.FileStatus;
-import com.intellij.openapi.vcs.changes.Change;
-import com.intellij.openapi.vcs.changes.ChangesUtil;
-import com.intellij.ui.SimpleTextAttributes;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.util.SystemInfo
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.vcs.FilePath
+import com.intellij.openapi.vcs.FileStatus
+import com.intellij.openapi.vcs.changes.Change
+import com.intellij.openapi.vcs.changes.ChangesUtil
+import com.intellij.ui.SimpleTextAttributes
 
 /**
  * @author yole
  */
-public class ChangesBrowserFilePathNode extends ChangesBrowserNode<FilePath> {
+open class ChangesBrowserFilePathNode(userObject: FilePath, val status: FileStatus?) : ChangesBrowserNode<FilePath>(userObject) {
+  constructor(userObject: FilePath) : this(userObject, null)
 
-  @Nullable
-  private FileStatus status;
+  private val filePath: FilePath
+    get() = getUserObject()
 
-  public ChangesBrowserFilePathNode(@NotNull FilePath userObject, @Nullable FileStatus status) {
-    this(userObject);
-    this.status = status;
+  override fun isFile(): Boolean {
+    return !filePath.isDirectory
   }
 
-  public ChangesBrowserFilePathNode(FilePath userObject) {
-    super(userObject);
+  override fun isDirectory(): Boolean {
+    return filePath.isDirectory && isLeaf
   }
 
-  @Override
-  protected boolean isFile() {
-    return !getUserObject().isDirectory();
-  }
-
-  @Override
-  protected boolean isDirectory() {
-    return getUserObject().isDirectory() && isLeaf();
-  }
-
-  @Override
-  public void render(@NotNull ChangesBrowserNodeRenderer renderer, boolean selected, boolean expanded, boolean hasFocus) {
-    final FilePath path = (FilePath)userObject;
-
-    if (renderer.isShowFlatten() && isLeaf()) {
-      renderer.append(path.getName(), getTextAttributes());
-      appendParentPath(renderer, path.getParentPath());
+  override fun render(renderer: ChangesBrowserNodeRenderer,
+                      selected: Boolean,
+                      expanded: Boolean,
+                      hasFocus: Boolean) {
+    val path = filePath
+    if (renderer.isShowFlatten && isLeaf) {
+      renderer.append(path.name, textAttributes)
+      appendParentPath(renderer, path.parentPath)
     }
     else {
-      renderer.append(getRelativePath(path), getTextAttributes());
+      renderer.append(getRelativePath(path), textAttributes)
+    }
+    if (!isLeaf) {
+      appendCount(renderer)
+    }
+    renderer.setIcon(path, path.isDirectory || !isLeaf)
+  }
+
+  private val textAttributes: SimpleTextAttributes
+    get() = if (status != null) SimpleTextAttributes(SimpleTextAttributes.STYLE_PLAIN, status.color)
+    else SimpleTextAttributes.REGULAR_ATTRIBUTES
+
+  protected open fun getRelativePath(path: FilePath): String {
+    return getRelativePath(safeCastToFilePath(getParent()), path)
+  }
+
+  override fun getTextPresentation(): String {
+    return getRelativePath(filePath)
+  }
+
+  override fun toString(): String {
+    return FileUtil.toSystemDependentName(filePath.path)
+  }
+
+  override fun getSortWeight(): Int {
+    return if (filePath.isDirectory) DIRECTORY_PATH_SORT_WEIGHT else FILE_PATH_SORT_WEIGHT
+  }
+
+  override fun compareUserObjects(o2: FilePath): Int {
+    return compareFilePaths(filePath, o2)
+  }
+
+  companion object {
+    @JvmStatic
+    fun safeCastToFilePath(node: ChangesBrowserNode<*>): FilePath? {
+      if (node is ChangesBrowserModuleNode) {
+        return node.moduleRoot
+      }
+      val o = node.userObject
+      if (o is FilePath) return o
+      if (o is Change) return ChangesUtil.getAfterPath(o)
+      return null
     }
 
-    if (!isLeaf()) {
-      appendCount(renderer);
+    fun getRelativePath(parent: FilePath?, child: FilePath): String {
+      val isLocal = !child.isNonLocal
+      val caseSensitive = isLocal && SystemInfo.isFileSystemCaseSensitive
+      var result = if (parent != null) FileUtil.getRelativePath(parent.path, child.path, '/', caseSensitive) else null
+      result = result ?: child.path
+      return if (isLocal) FileUtil.toSystemDependentName(result) else result
     }
-
-    renderer.setIcon(path, path.isDirectory() || !isLeaf());
-  }
-
-  @NotNull
-  private SimpleTextAttributes getTextAttributes() {
-    return status != null
-           ? new SimpleTextAttributes(SimpleTextAttributes.STYLE_PLAIN, status.getColor())
-           : SimpleTextAttributes.REGULAR_ATTRIBUTES;
-  }
-
-  @NotNull
-  protected String getRelativePath(FilePath path) {
-    return getRelativePath(safeCastToFilePath(getParent()), path);
-  }
-
-  @Override
-  public String getTextPresentation() {
-    return getRelativePath(getUserObject());
-  }
-
-  @Override
-  public String toString() {
-    return FileUtil.toSystemDependentName(getUserObject().getPath());
-  }
-
-  @Nullable
-  public static FilePath safeCastToFilePath(ChangesBrowserNode node) {
-    if (node instanceof ChangesBrowserModuleNode) {
-      return ((ChangesBrowserModuleNode)node).getModuleRoot();
-    }
-
-    Object o = node.getUserObject();
-    if (o instanceof FilePath) return (FilePath)o;
-    if (o instanceof Change) {
-      return ChangesUtil.getAfterPath((Change)o);
-    }
-    return null;
-  }
-
-  @NotNull
-  public static String getRelativePath(@Nullable FilePath parent, @NotNull FilePath child) {
-    boolean isLocal = !child.isNonLocal();
-    boolean caseSensitive = isLocal && SystemInfo.isFileSystemCaseSensitive;
-    String result = parent != null ? FileUtil.getRelativePath(parent.getPath(), child.getPath(), '/', caseSensitive) : null;
-
-    result = result == null ? child.getPath() : result;
-
-    return isLocal ? FileUtil.toSystemDependentName(result) : result;
-  }
-
-  @Override
-  public int getSortWeight() {
-    if (((FilePath)userObject).isDirectory()) return DIRECTORY_PATH_SORT_WEIGHT;
-    return FILE_PATH_SORT_WEIGHT;
-  }
-
-  @Override
-  public int compareUserObjects(final FilePath o2) {
-    return compareFilePaths(getUserObject(), o2);
   }
 }
-
