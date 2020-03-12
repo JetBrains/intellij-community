932b345ca7c37d9fc2515883a286af240ca8143f
Vladislav Tankov <vdtankov@gmail.com>
2020-03-13 01:03:41 +0300

    [Grazie] Enable by default check in documentation and comments for enabled languages.
    GitOrigin-RevId: 91402b5217132b0457d4f7a9911e1bcb270c07ba

--- plugins/grazie/src/main/kotlin/com/intellij/grazie/config/CheckingContext.kt
+++ plugins/grazie/src/main/kotlin/com/intellij/grazie/config/CheckingContext.kt
@@ -4,5 +4,5 @@ import com.intellij.util.xmlb.annotations.Property
 
 data class CheckingContext(@Property val isCheckInCommitMessagesEnabled: Boolean = false,
                            @Property val isCheckInStringLiteralsEnabled: Boolean = false,
-                           @Property val isCheckInCommentsEnabled: Boolean = false,
-                           @Property val isCheckInDocumentationEnabled: Boolean = false)
+                           @Property val isCheckInCommentsEnabled: Boolean = true,
+                           @Property val isCheckInDocumentationEnabled: Boolean = true)
