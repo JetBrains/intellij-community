9e29891330063ac6b879c11b89c3e834e253dd25
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-03-13 07:37:16 +0100

    add NotNull for icon class
    GitOrigin-RevId: 6651a21134be715b980de5de4d042914dd989989

--- platform/xdebugger-impl/src/icons/PlatformDebuggerImplIcons.java
+++ platform/xdebugger-impl/src/icons/PlatformDebuggerImplIcons.java
@@ -1,7 +1,8 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package icons;
 
 import com.intellij.ui.IconManager;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 
@@ -10,26 +11,26 @@ import javax.swing.*;
  * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
  */
 public final class PlatformDebuggerImplIcons {
-  private static Icon load(String path) {
+  private static @NotNull Icon load(@NotNull String path) {
     return IconManager.getInstance().getIcon(path, PlatformDebuggerImplIcons.class);
   }
 
 
   public final static class Actions {
-    /** 16x16 */ public static final Icon Force_run_to_cursor = load("/icons/actions/force_run_to_cursor.svg");
-    /** 16x16 */ public static final Icon Force_step_into = load("/icons/actions/force_step_into.svg");
-    /** 16x16 */ public static final Icon Force_step_over = load("/icons/actions/force_step_over.svg");
+    /** 16x16 */ public static final @NotNull Icon Force_run_to_cursor = load("/icons/actions/force_run_to_cursor.svg");
+    /** 16x16 */ public static final @NotNull Icon Force_step_into = load("/icons/actions/force_step_into.svg");
+    /** 16x16 */ public static final @NotNull Icon Force_step_over = load("/icons/actions/force_step_over.svg");
 
   }
 
   public final static class MemoryView {
-    /** 16x16 */ public static final Icon Active = load("/icons/memoryView/active.svg");
+    /** 16x16 */ public static final @NotNull Icon Active = load("/icons/memoryView/active.svg");
 
   }
 
   public final static class PinToTop {
-    /** 16x16 */ public static final Icon PinnedItem = load("/icons/pinToTop/pinnedItem.svg");
-    /** 16x16 */ public static final Icon UnpinnedItem = load("/icons/pinToTop/unpinnedItem.svg");
+    /** 16x16 */ public static final @NotNull Icon PinnedItem = load("/icons/pinToTop/pinnedItem.svg");
+    /** 16x16 */ public static final @NotNull Icon UnpinnedItem = load("/icons/pinToTop/unpinnedItem.svg");
 
   }
 }
