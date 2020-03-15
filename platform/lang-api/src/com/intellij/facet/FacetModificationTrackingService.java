8ecfe1678237af7381fb7ed9946a9285537aa9e0
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-03-15 09:01:41 +0100

    get rid of ModuleServiceManager
    (cherry picked from commit 91f39625139a964430a3d14829216d3c5f49a220)
    
    GitOrigin-RevId: 4c679ef33c58d7774af8bc56d75514d32157b974

--- platform/lang-api/src/com/intellij/facet/FacetModificationTrackingService.java
+++ platform/lang-api/src/com/intellij/facet/FacetModificationTrackingService.java
@@ -1,24 +1,9 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.facet;
 
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.ModuleServiceManager;
 import com.intellij.openapi.util.ModificationTracker;
 import com.intellij.openapi.util.ModificationTrackerListener;
 import org.jetbrains.annotations.ApiStatus;
@@ -28,11 +13,11 @@ import org.jetbrains.annotations.NotNull;
 public abstract class FacetModificationTrackingService {
 
   public static FacetModificationTrackingService getInstance(@NotNull Module module) {
-    return ModuleServiceManager.getService(module, FacetModificationTrackingService.class);
+    return module.getService(FacetModificationTrackingService.class);
   }
 
   public static FacetModificationTrackingService getInstance(@NotNull Facet<?> facet) {
-    return ModuleServiceManager.getService(facet.getModule(), FacetModificationTrackingService.class);
+    return facet.getModule().getService(FacetModificationTrackingService.class);
   }
 
   @NotNull
