0f4d8695e97d39e7db9f2edcd28e2fd236b4e409
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2020-03-16 18:27:40 +0700

    IDEA-235296 Refactor ensureCodeBlock: AddAssertStatementFix updated
    GitOrigin-RevId: e1e2f6021664be235b663876fe6629729fd0a4ef

--- java/java-impl/src/com/intellij/codeInspection/dataFlow/DataFlowInspection.java
+++ java/java-impl/src/com/intellij/codeInspection/dataFlow/DataFlowInspection.java
@@ -34,10 +34,7 @@ import com.intellij.util.SmartList;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.ui.JBUI;
 import com.siyeh.ig.fixes.IntroduceVariableFix;
-import com.siyeh.ig.psiutils.ControlFlowUtils;
-import com.siyeh.ig.psiutils.ExpressionUtils;
-import com.siyeh.ig.psiutils.ParenthesesUtils;
-import com.siyeh.ig.psiutils.SideEffectChecker;
+import com.siyeh.ig.psiutils.*;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -144,7 +141,7 @@ public class DataFlowInspection extends DataFlowInspectionBase {
     PsiExpression operand = castExpression.getOperand();
     PsiTypeElement typeElement = castExpression.getCastType();
     if (typeElement != null && operand != null) {
-      if (!alwaysFails && !SideEffectChecker.mayHaveSideEffects(operand) && ControlFlowUtils.canExtractStatement(castExpression)) {
+      if (!alwaysFails && !SideEffectChecker.mayHaveSideEffects(operand) && CodeBlockSurrounder.canSurround(castExpression)) {
         String suffix = " instanceof " + typeElement.getText();
         fixes.add(new AddAssertStatementFix(ParenthesesUtils.getText(operand, PsiPrecedenceUtil.RELATIONAL_PRECEDENCE) + suffix));
         if (onTheFly && SurroundWithIfFix.isAvailable(operand)) {
@@ -187,8 +184,7 @@ public class DataFlowInspection extends DataFlowInspectionBase {
       }
       else if (!ExpressionUtils.isNullLiteral(qualifier) && !SideEffectChecker.mayHaveSideEffects(qualifier))  {
         String suffix = " != null";
-        if (PsiUtil.getLanguageLevel(qualifier).isAtLeast(LanguageLevel.JDK_1_4) &&
-            ControlFlowUtils.canExtractStatement(expression)) {
+        if (PsiUtil.getLanguageLevel(qualifier).isAtLeast(LanguageLevel.JDK_1_4) && CodeBlockSurrounder.canSurround(expression)) {
           String replacement = ParenthesesUtils.getText(qualifier, ParenthesesUtils.EQUALITY_PRECEDENCE) + suffix;
           fixes.add(new AddAssertStatementFix(replacement));
         }
--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/addAssert/beforeLambda.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/quickFix/addAssert/beforeLambda.java
@@ -1,7 +1,9 @@
 // "Assert 'container != null'" "true"
+import java.util.function.Supplier;
+
 class A{
   void test(){
     Object container = null;
-    Runnable r = () -> container == null ? container.toS<caret>tring() : "";
+    Supplier<String> r = () -> container == null ? container.toS<caret>tring() : "";
   }
 }
