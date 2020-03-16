fc92eb0e2f2adaf7649faa3e0476b82b049ed81d
Artemiy Sartakov <Artemiy.Sartakov@jetbrains.com>
2020-03-16 15:11:38 +0700

    Problems view: remove document listener when selection changed (IDEA-CR-59765)
    GitOrigin-RevId: 7ee3d280165307c526c2069211544abfa21be1f9

--- java/java-impl/src/com/intellij/codeInsight/daemon/problems/pass/ProjectProblemFileSelectionListener.java
+++ java/java-impl/src/com/intellij/codeInsight/daemon/problems/pass/ProjectProblemFileSelectionListener.java
@@ -12,6 +12,8 @@ import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.fileEditor.FileEditorManagerEvent;
 import com.intellij.openapi.fileEditor.FileEditorManagerListener;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.project.ProjectManagerListener;
 import com.intellij.openapi.startup.StartupActivity;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.registry.Registry;
@@ -19,12 +21,13 @@ import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiJavaFile;
 import com.intellij.psi.PsiManager;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
 
 import static com.intellij.util.ObjectUtils.tryCast;
 
-class ProjectProblemFileSelectionListener implements FileEditorManagerListener, BulkAwareDocumentListener {
+class ProjectProblemFileSelectionListener implements FileEditorManagerListener, BulkAwareDocumentListener, ProjectManagerListener {
 
   private static final Key<Boolean> PROJECT_PROBLEM_FILE_CHANGE_LISTENER_KEY = Key.create("ProjectProblemFileChangeListenerKey");
 
@@ -36,12 +39,48 @@ class ProjectProblemFileSelectionListener implements FileEditorManagerListener, 
 
   @Override
   public void selectionChanged(@NotNull FileEditorManagerEvent event) {
-    VirtualFile virtualFile = event.getNewFile();
-    if (virtualFile == null) return;
+    VirtualFile oldFile = event.getOldFile();
+    if (oldFile != null) removeListener(oldFile);
+    VirtualFile newFile = event.getNewFile();
+    if (newFile == null) return;
+    PsiJavaFile psiFile = tryCast(PsiManager.getInstance(myProject).findFile(newFile), PsiJavaFile.class);
+    if (psiFile == null) return;
+    ProjectProblemPassUtils.removeOldInlays(psiFile);
+    addListener(newFile);
+  }
+
+  @Override
+  public void projectClosingBeforeSave(@NotNull Project project) {
+    Arrays.stream(FileEditorManager.getInstance(project).getSelectedFiles()).forEach(vf -> removeListener(vf));
+  }
+
+  private void addListener(VirtualFile virtualFile) {
+    if (virtualFile instanceof VirtualFileWindow) return;
     PsiJavaFile psiFile = tryCast(PsiManager.getInstance(myProject).findFile(virtualFile), PsiJavaFile.class);
     if (psiFile == null) return;
-    registerListener(virtualFile);
-    ProjectProblemPassUtils.removeOldInlays(psiFile);
+    Document document = getDocument(virtualFile);
+    if (document == null) return;
+    if (Boolean.TRUE.equals(document.getUserData(PROJECT_PROBLEM_FILE_CHANGE_LISTENER_KEY))) return;
+    document.addDocumentListener(this);
+    document.putUserData(PROJECT_PROBLEM_FILE_CHANGE_LISTENER_KEY, true);
+  }
+
+  private void removeListener(VirtualFile virtualFile) {
+    if (virtualFile == null || virtualFile instanceof VirtualFileWindow) return;
+    if (!(PsiManager.getInstance(myProject).findFile(virtualFile) instanceof PsiJavaFile)) return;
+    Document document = getDocument(virtualFile);
+    if (document == null) return;
+    if (!Boolean.TRUE.equals(document.getUserData(PROJECT_PROBLEM_FILE_CHANGE_LISTENER_KEY))) return;
+    document.removeDocumentListener(this);
+    document.putUserData(PROJECT_PROBLEM_FILE_CHANGE_LISTENER_KEY, null);
+  }
+
+  @Nullable
+  private static Document getDocument(@NotNull VirtualFile virtualFile) {
+    FileDocumentManager documentManager = FileDocumentManager.getInstance();
+    Document document = documentManager.getDocument(virtualFile);
+    if (document == null) return null;
+    return document;
   }
 
   @Override
@@ -54,16 +93,7 @@ class ProjectProblemFileSelectionListener implements FileEditorManagerListener, 
     PsiJavaFile psiFile = tryCast(PsiManager.getInstance(myProject).findFile(virtualFile), PsiJavaFile.class);
     if (psiFile == null) return;
     SnapshotUpdater.storeContent(psiFile, content);
-  }
-
-  private void registerListener(@NotNull VirtualFile virtualFile) {
-    if (virtualFile instanceof VirtualFileWindow) return;
-    FileDocumentManager documentManager = FileDocumentManager.getInstance();
-    Document document = documentManager.getDocument(virtualFile);
-    if (document == null) return;
-    if (Boolean.TRUE.equals(document.getUserData(PROJECT_PROBLEM_FILE_CHANGE_LISTENER_KEY))) return;
-    document.addDocumentListener(this);
-    document.putUserData(PROJECT_PROBLEM_FILE_CHANGE_LISTENER_KEY, true);
+    removeListener(virtualFile);
   }
 
   public static class MyStartupActivity implements StartupActivity {
@@ -71,8 +101,9 @@ class ProjectProblemFileSelectionListener implements FileEditorManagerListener, 
     public void runActivity(@NotNull Project project) {
       if (!Registry.is("project.problems.view") && !ApplicationManager.getApplication().isUnitTestMode()) return;
       ProjectProblemFileSelectionListener listener = new ProjectProblemFileSelectionListener(project);
-      project.getMessageBus().connect().subscribe(FileEditorManagerListener.FILE_EDITOR_MANAGER, listener);
-      Arrays.stream(FileEditorManager.getInstance(project).getSelectedFiles()).forEach(vf -> listener.registerListener(vf));
+      Arrays.stream(FileEditorManager.getInstance(project).getSelectedFiles()).forEach(vf -> listener.addListener(vf));
+      project.getMessageBus().connect().subscribe(FILE_EDITOR_MANAGER, listener);
+      ProjectManager.getInstance().addProjectManagerListener(project, listener);
     }
   }
 }
