3f8e206fefc5ad15a8a6dda20bbfa76fb5f9f2eb
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-03-16 08:32:52 +0100

    cleanup
    GitOrigin-RevId: b51d87f531ac9e27439e3aabb6c1169613d7c84f

--- platform/core-api/src/com/intellij/openapi/command/CommandProcessor.java
+++ platform/core-api/src/com/intellij/openapi/command/CommandProcessor.java
@@ -1,9 +1,8 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.command;
 
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -19,7 +18,7 @@ import org.jetbrains.annotations.Nullable;
  */
 public abstract class CommandProcessor {
   public static CommandProcessor getInstance() {
-    return ServiceManager.getService(CommandProcessor.class);
+    return ApplicationManager.getApplication().getService(CommandProcessor.class);
   }
 
   /**
