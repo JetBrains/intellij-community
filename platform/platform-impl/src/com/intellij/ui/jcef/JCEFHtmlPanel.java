74345a8841bdb04c4a8a911bc61753432c2c6b8e
Viktor Shatrov <Viktor.Shatrov@jetbrains.com>
2020-03-16 18:22:17 +0300

    JCEF: add default context menu for JCEFHtmlPanel
    - disable context menu actions by default
    - add debug command for internal mode
    
    GitOrigin-RevId: 45ecd64bc0a69c4095b9c504298162f60c9db314

--- platform/platform-impl/src/com/intellij/ui/jcef/JCEFHtmlPanel.java
+++ platform/platform-impl/src/com/intellij/ui/jcef/JCEFHtmlPanel.java
@@ -3,9 +3,24 @@ package com.intellij.ui.jcef;
 
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.wm.impl.IdeFrameImpl;
 import com.intellij.util.ObjectUtils;
+import org.cef.browser.CefBrowser;
+import org.cef.browser.CefFrame;
+import org.cef.callback.CefContextMenuParams;
+import org.cef.callback.CefMenuModel;
+import org.cef.handler.CefContextMenuHandler;
+import org.cef.handler.CefContextMenuHandlerAdapter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import static org.cef.callback.CefMenuModel.MenuId.MENU_ID_USER_LAST;
 
 /**
  * @author tav
@@ -15,6 +30,8 @@ public class JCEFHtmlPanel extends JBCefBrowser {
 
   @NotNull
   private final String myUrl;
+  protected CefContextMenuHandler myDefaultContextMenuHandler;
+  private static final int DEBUG_COMMAND_ID = MENU_ID_USER_LAST;
 
   static {
     Disposer.register(ApplicationManager.getApplication(), () -> {
@@ -32,6 +49,47 @@ public class JCEFHtmlPanel extends JBCefBrowser {
     if (client != ourCefClient) {
       Disposer.register(this, client);
     }
+    boolean isInternal = ApplicationManager.getApplication().isInternal();
+    myDefaultContextMenuHandler = new CefContextMenuHandlerAdapter() {
+      @Override
+      public void onBeforeContextMenu(CefBrowser browser, CefFrame frame, CefContextMenuParams params, CefMenuModel model) {
+        model.clear();
+        if (isInternal) {
+          model.addItem(DEBUG_COMMAND_ID, "Open DevTools");
+        }
+      }
+
+      @Override
+      public boolean onContextMenuCommand(CefBrowser browser, CefFrame frame, CefContextMenuParams params, int commandId, int eventFlags) {
+        if (commandId == DEBUG_COMMAND_ID) {
+          openDevtools(browser);
+          return true;
+        }
+        return false;
+      }
+    };
+    client.addContextMenuHandler(myDefaultContextMenuHandler, this.getCefBrowser());
+  }
+
+  private void openDevtools(CefBrowser browser) {
+    Window activeFrame = IdeFrameImpl.getActiveFrame();
+    if (activeFrame == null) return;
+    Rectangle bounds = activeFrame.getGraphicsConfiguration().getBounds();
+    JFrame frame = new IdeFrameImpl();
+
+    frame.setTitle("JCEF DevTools");
+    frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    frame.setBounds(bounds.width / 4, bounds.height / 4, bounds.width / 2, bounds.height / 2);
+    frame.setLayout(new BorderLayout());
+    JBCefBrowser devTools = new JBCefBrowser(browser.getDevTools());
+    frame.add(devTools.getComponent(), BorderLayout.CENTER);
+    frame.addWindowListener(new WindowAdapter() {
+      @Override
+      public void windowClosed(WindowEvent e) {
+        Disposer.dispose(devTools);
+      }
+    });
+    frame.setVisible(true);
   }
 
   public void setHtml(@NotNull String html) {
