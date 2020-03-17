19795e7344862f7c9cea0655e9e06013c96aa8e4
Aleksey Pivovarov <AMPivovarov@gmail.com>
2020-03-17 16:43:33 +0300

    vcs: cleanup - inline helper class
    GitOrigin-RevId: 37e275244dd8628c226c663a7905f2c2abe747bf

--- platform/vcs-impl/src/com/intellij/ide/actions/StartUseVcsAction.java
+++ platform/vcs-impl/src/com/intellij/ide/actions/StartUseVcsAction.java
@@ -2,13 +2,17 @@
 package com.intellij.ide.actions;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.project.DumbAwareAction;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.vcs.AbstractVcs;
 import com.intellij.openapi.vcs.ProjectLevelVcsManager;
 import com.intellij.openapi.vcs.VcsBundle;
+import com.intellij.openapi.vcs.impl.ProjectLevelVcsManagerImpl;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class StartUseVcsAction extends DumbAwareAction {
   public StartUseVcsAction() {
@@ -17,8 +21,9 @@ public class StartUseVcsAction extends DumbAwareAction {
 
   @Override
   public void update(@NotNull AnActionEvent e) {
-    VcsDataWrapper data = new VcsDataWrapper(e);
-    boolean enabled = data.enabled();
+    Project project = e.getData(CommonDataKeys.PROJECT);
+
+    boolean enabled = isEnabled(project);
 
     Presentation presentation = e.getPresentation();
     presentation.setEnabledAndVisible(enabled);
@@ -29,22 +34,24 @@ public class StartUseVcsAction extends DumbAwareAction {
 
   @Override
   public void actionPerformed(@NotNull final AnActionEvent e) {
-    VcsDataWrapper data = new VcsDataWrapper(e);
-    boolean enabled = data.enabled();
-    if (!enabled) {
-      return;
-    }
+    Project project = e.getRequiredData(CommonDataKeys.PROJECT);
+    if (!isEnabled(project)) return;
 
-    StartUseVcsDialog dialog = new StartUseVcsDialog(data);
+    StartUseVcsDialog dialog = new StartUseVcsDialog(project);
     dialog.show();
     if (dialog.getExitCode() == DialogWrapper.OK_EXIT_CODE) {
       String vcsName = dialog.getVcs();
       if (vcsName.length() > 0) {
-        ProjectLevelVcsManager manager = data.getManager();
-        AbstractVcs vcs = manager.findVcsByName(vcsName);
+        AbstractVcs vcs = ProjectLevelVcsManager.getInstance(project).findVcsByName(vcsName);
         assert vcs != null : "No vcs found for name " + vcsName;
         vcs.enableIntegration();
       }
     }
+  }
+
+  private static boolean isEnabled(@Nullable Project project) {
+    if (project == null) return false;
+    ProjectLevelVcsManagerImpl manager = ProjectLevelVcsManagerImpl.getInstanceImpl(project);
+    return manager.haveVcses() && !manager.hasAnyMappings();
   }
 }
