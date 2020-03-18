6b98562d56376553f0848455a4b9a0032a9355e0
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2020-03-18 23:01:41 +0300

    add ability to pause coroutines
    GitOrigin-RevId: 170fc45dfd9c060ee5b6b8ebf9df2c34a3f22f70

--- platform/util-ex/src/com/intellij/openapi/progress/coroutines.kt
+++ platform/util-ex/src/com/intellij/openapi/progress/coroutines.kt
@@ -3,10 +3,26 @@
 
 package com.intellij.openapi.progress
 
-import org.jetbrains.annotations.ApiStatus
 import com.intellij.openapi.util.Computable
 import kotlinx.coroutines.*
+import org.jetbrains.annotations.ApiStatus
 import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.coroutineContext
+
+/**
+ * Checks whether the coroutine is active, and throws [CancellationException] if the coroutine is cancelled.
+ * This function might suspend if the coroutine is paused,
+ * or yield if the coroutine has a lower priority while higher priority task is running.
+ *
+ * @throws CancellationException if the coroutine is cancelled; the exception is also thrown if coroutine is cancelled while suspended
+ * @see ensureActive
+ * @see coroutineSuspender
+ */
+suspend fun checkCancelled() {
+  val ctx = coroutineContext
+  ctx.ensureActive() // standard check first
+  ctx[CoroutineSuspenderElementKey]?.checkPaused() // will suspend if paused
+}
 
 /**
  * The method has same semantics as [runBlocking],
