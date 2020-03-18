9146f8d106edd06bfcd8bc4130e4377a9d18fc19
Daniil Tsaryov <daniil.tsaryov@jetbrains.com>
2020-03-18 18:25:34 +0300

    IDEA-235134 NPE when trying to disable synced branches feature
    GitOrigin-RevId: 6070f2b4a1ccbb0780320756ab018a6d64c4532d

--- platform/dvcs-impl/src/com/intellij/dvcs/branch/DvcsBranchPopup.java
+++ platform/dvcs-impl/src/com/intellij/dvcs/branch/DvcsBranchPopup.java
@@ -97,7 +97,7 @@ public abstract class DvcsBranchPopup<Repo extends Repository> {
     Notification notification = STANDARD_NOTIFICATION.createNotification("Branch Operations Are Executed on All Roots", "", NotificationType.INFORMATION, null);
     notification
       .addAction(NotificationAction.createSimple(DvcsBundle.messagePointer("action.NotificationAction.DvcsBranchPopup.text.disable"), () -> {
-      ShowSettingsUtil.getInstance().showSettingsDialog(myProject, myVcs.getConfigurable().getDisplayName());
+      ShowSettingsUtil.getInstance().showSettingsDialog(myProject, myVcs.getDisplayName());
       if (myVcsSettings.getSyncSetting() == DvcsSyncSettings.Value.DONT_SYNC) {
         notification.expire();
       }
