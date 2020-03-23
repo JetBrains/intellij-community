6b9ec8195fbb51aa6980a5cbd01660e1f3dae21e
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2020-03-23 20:35:22 +0300

    terminal: run fish as a login shell (IDEA-232983)
    (cherry picked from commit 31c5ac64eff596f39233e9acc65eb655616dd84f)
    
    GitOrigin-RevId: bed091bb43d855efa6b8c3f675b71a2bd07ced22

--- plugins/terminal/src/org/jetbrains/plugins/terminal/LocalTerminalDirectRunner.java
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/LocalTerminalDirectRunner.java
@@ -48,8 +48,8 @@ public class LocalTerminalDirectRunner extends AbstractTerminalRunner<PtyProcess
   private static final String XDG_CONFIG_HOME = "XDG_CONFIG_HOME";
   private static final String IJ_COMMAND_HISTORY_FILE_ENV = "__INTELLIJ_COMMAND_HISTFILE__";
   private static final String LOGIN_SHELL = "LOGIN_SHELL";
-  private static final ImmutableList<String> LOGIN_CLI_OPTIONS = ImmutableList.of("--login", "-l");
-  private static final String LOGIN_CLI_OPTION = LOGIN_CLI_OPTIONS.get(0);
+  private static final String LOGIN_CLI_OPTION = "--login";
+  private static final ImmutableList<String> LOGIN_CLI_OPTIONS = ImmutableList.of(LOGIN_CLI_OPTION, "-l");
   private static final String INTERACTIVE_CLI_OPTION = "-i";
   private static final String BASH_NAME = "bash";
   private static final String SH_NAME = "sh";
@@ -311,10 +311,14 @@ public class LocalTerminalDirectRunner extends AbstractTerminalRunner<PtyProcess
   }
 
   private static boolean isLoginOptionAvailable(@NotNull String shellName) {
-    return shellName.equals(BASH_NAME) || (SystemInfo.isMac && shellName.equals(SH_NAME)) || shellName.equals(ZSH_NAME);
+    return isBashZshFish(shellName);
   }
 
   private static boolean isInteractiveOptionAvailable(@NotNull String shellName) {
+    return isBashZshFish(shellName);
+  }
+
+  private static boolean isBashZshFish(@NotNull String shellName) {
     return shellName.equals(BASH_NAME) || (SystemInfo.isMac && shellName.equals(SH_NAME)) ||
            shellName.equals(ZSH_NAME) || shellName.equals(FISH_NAME);
   }
