a47ee3612f698d6f814134b465e0dbe928b507db
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2020-03-24 14:45:36 +0300

    IDEA-235674: Close view icon popup in Git history on the second click
    (cherry picked from commit 5e5a6ad8b2c6b07e792936724dc2d6d9ce8364e5)
    
    GitOrigin-RevId: 468942a869f3ae779a47b75459494c17051b7306

--- platform/vcs-log/impl/src/com/intellij/vcs/log/ui/actions/VcsLogToolbarPopupActionGroup.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/ui/actions/VcsLogToolbarPopupActionGroup.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.vcs.log.ui.actions;
 
 import com.intellij.openapi.actionSystem.ActionButtonComponent;
@@ -22,6 +8,7 @@ import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.openapi.ui.popup.ListPopup;
+import com.intellij.ui.popup.util.PopupState;
 import com.intellij.vcs.log.VcsLogDataKeys;
 import com.intellij.vcs.log.VcsLogUi;
 import com.intellij.vcs.log.ui.VcsLogActionPlaces;
@@ -31,6 +18,7 @@ import java.awt.*;
 import java.awt.event.InputEvent;
 
 public class VcsLogToolbarPopupActionGroup extends DefaultActionGroup {
+  private final PopupState myPopupState = new PopupState();
 
   @Override
   public boolean isDumbAware() {
@@ -44,9 +32,11 @@ public class VcsLogToolbarPopupActionGroup extends DefaultActionGroup {
 
   @Override
   public void actionPerformed(@NotNull AnActionEvent e) {
+    if (myPopupState.isRecentlyHidden()) return; // do not show new popup
     ListPopup popup = JBPopupFactory.getInstance().createActionGroupPopup(null, this, e.getDataContext(),
                                                                           JBPopupFactory.ActionSelectionAid.MNEMONICS, true,
                                                                           VcsLogActionPlaces.VCS_LOG_TOOLBAR_POPUP_PLACE);
+    popup.addListener(myPopupState);
     InputEvent inputEvent = e.getInputEvent();
     if (inputEvent == null) {
       popup.showInFocusCenter();
