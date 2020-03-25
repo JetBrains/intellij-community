de0396ad4158332341827a21aed1361b66fb5c3f
Egor Ushakov <egor.ushakov@jetbrains.com>
2020-03-25 12:21:47 +0300

    fixed failed first idea test start during indexing
    GitOrigin-RevId: 4ff7587fa10043e851f019f01503cf0e2d5cd61a

--- plugins/devkit/devkit-core/src/util/PsiUtil.java
+++ plugins/devkit/devkit-core/src/util/PsiUtil.java
@@ -1,23 +1,10 @@
-/*
- * Copyright 2000-2019 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.devkit.util;
 
 import com.intellij.codeInsight.AnnotationUtil;
 import com.intellij.codeInspection.dataFlow.StringExpressionHelper;
 import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.util.Key;
@@ -234,8 +221,10 @@ public class PsiUtil {
       return false;
     }
 
-    GlobalSearchScope scope = GlobalSearchScopesCore.projectProductionScope(project);
-    return JavaPsiFacade.getInstance(project).findClass(IDE_PROJECT_MARKER_CLASS, scope) != null;
+    return DumbService.getInstance(project).computeWithAlternativeResolveEnabled(() -> {
+      GlobalSearchScope scope = GlobalSearchScopesCore.projectProductionScope(project);
+      return JavaPsiFacade.getInstance(project).findClass(IDE_PROJECT_MARKER_CLASS, scope) != null;
+    });
   }
 
   @NotNull
