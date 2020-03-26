383e24c8783926c1d1c16e072afb191769b1668c
Alex Plate <aleksei.plate@jetbrains.com>
2020-03-26 14:06:20 +0300

    [Workspace Model] Extract containers into separate package
    GitOrigin-RevId: a1408b2a0032f740122a8b81ce3c5e693e03faac

--- platform/workspaceModel-core/src/pstorage/RefsTable.kt
+++ platform/workspaceModel-core/src/pstorage/RefsTable.kt
@@ -1,6 +1,8 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.workspace.api.pstorage
 
+import com.intellij.workspace.api.pstorage.containers.IntIntBiMultiMap
+import com.intellij.workspace.api.pstorage.containers.IntIntMultiMap
 import kotlin.reflect.KProperty1
 
 open class RefsTable(
