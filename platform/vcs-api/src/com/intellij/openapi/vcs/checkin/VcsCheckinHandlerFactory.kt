242c74095000e27b927fd89b58310d0fd127350b
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2020-03-26 18:53:19 +0300

    vcs: Convert "VcsCheckinHandlerFactory" to kotlin
    GitOrigin-RevId: ca8186b38046bd667612722f35e636bda9c1932a

--- platform/vcs-api/src/com/intellij/openapi/vcs/checkin/VcsCheckinHandlerFactory.kt
+++ platform/vcs-api/src/com/intellij/openapi/vcs/checkin/VcsCheckinHandlerFactory.kt
@@ -1,52 +1,27 @@
-/*
- * Copyright 2000-2011 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.intellij.openapi.vcs.checkin;
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.vcs.checkin
 
-import com.intellij.openapi.extensions.ExtensionPointName;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.CheckinProjectPanel;
-import com.intellij.openapi.vcs.VcsKey;
-import com.intellij.openapi.vcs.changes.CommitContext;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.extensions.ExtensionPointName
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vcs.CheckinProjectPanel
+import com.intellij.openapi.vcs.VcsKey
+import com.intellij.openapi.vcs.changes.CommitContext
 
-public abstract class VcsCheckinHandlerFactory implements BaseCheckinHandlerFactory {
-  public static final ExtensionPointName<VcsCheckinHandlerFactory> EP_NAME = ExtensionPointName.create("com.intellij.vcsCheckinHandlerFactory");
+abstract class VcsCheckinHandlerFactory protected constructor(val key: VcsKey) :
+  BaseCheckinHandlerFactory {
 
-  private final VcsKey myKey;
+  override fun createHandler(panel: CheckinProjectPanel, commitContext: CommitContext): CheckinHandler {
+    if (!panel.vcsIsAffected(key.name)) return CheckinHandler.DUMMY
 
-  protected VcsCheckinHandlerFactory(@NotNull final VcsKey key) {
-    myKey = key;
+    return createVcsHandler(panel)
   }
 
-  @NotNull
-  @Override
-  public CheckinHandler createHandler(@NotNull CheckinProjectPanel panel, @NotNull CommitContext commitContext) {
-    if (! panel.vcsIsAffected(myKey.getName())) return CheckinHandler.DUMMY;
-    return createVcsHandler(panel);
-  }
+  protected abstract fun createVcsHandler(panel: CheckinProjectPanel): CheckinHandler
 
-  @NotNull
-  protected abstract CheckinHandler createVcsHandler(CheckinProjectPanel panel);
+  override fun createSystemReadyHandler(project: Project): BeforeCheckinDialogHandler? = null
 
-  public VcsKey getKey() {
-    return myKey;
-  }
-
-  @Override
-  public BeforeCheckinDialogHandler createSystemReadyHandler(@NotNull Project project) {
-    return null;
+  companion object {
+    @JvmStatic
+    val EP_NAME: ExtensionPointName<VcsCheckinHandlerFactory> = ExtensionPointName.create("com.intellij.vcsCheckinHandlerFactory")
   }
 }
