bf6666815c68151b2280767ec7efef60ac127006
Anton Tarasov <anton.tarasov@jetbrains.com>
2020-03-27 12:36:29 +0300

    Refine JBCefBrowser.LoadDeferrer
    (cherry picked from commit 5375b3a92b6495544bcc66cd4ce97c290bf939d6)
    
    GitOrigin-RevId: 19010ba5786c472d1f7ed0e991fc7632920adfb6

--- platform/platform-api/src/com/intellij/ui/jcef/JBCefBrowser.java
+++ platform/platform-api/src/com/intellij/ui/jcef/JBCefBrowser.java
@@ -50,13 +50,19 @@ public class JBCefBrowser implements JBCefDisposable {
     @Nullable protected final String myHtml;
     @NotNull protected final String myUrl;
 
-    LoadDeferrer(@NotNull String url) {
-      this(null, url);
-    }
-
-    LoadDeferrer(@Nullable String html, @NotNull String url) {
+    private LoadDeferrer(@Nullable String html, @NotNull String url) {
       myHtml = html;
       myUrl = url;
+    }
+
+    @NotNull
+    public static LoadDeferrer urlDeferrer(String url) {
+      return new LoadDeferrer(null, url);
+    }
+
+    @NotNull
+    public static LoadDeferrer htmlDeferrer(String html, String url) {
+      return new LoadDeferrer(html, url);
     }
 
     public void load(@NotNull CefBrowser browser) {
@@ -143,7 +149,7 @@ public class JBCefBrowser implements JBCefDisposable {
       myCefBrowser.loadURL(url);
     }
     else {
-      myLoadDeferrer = new LoadDeferrer(url);
+      myLoadDeferrer = LoadDeferrer.urlDeferrer(url);
     }
   }
 
@@ -158,7 +164,7 @@ public class JBCefBrowser implements JBCefDisposable {
       myCefBrowser.loadString(html, url);
     }
     else {
-      myLoadDeferrer = new LoadDeferrer(html, url);
+      myLoadDeferrer = LoadDeferrer.htmlDeferrer(html, url);
     }
   }
 
