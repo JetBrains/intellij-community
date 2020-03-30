d9076ed333874524393494aa268e3083218762d6
Dmitry Jemerov <yole@jetbrains.com>
2020-03-30 19:51:49 +0200

    Nicer Kotlin API for reporting changed settings to FUS
    GitOrigin-RevId: 5846cf07e4305ccf461861385a160e2e5a68c815

--- platform/diff-impl/src/com/intellij/diff/statistics/DiffUsagesCollector.kt
+++ platform/diff-impl/src/com/intellij/diff/statistics/DiffUsagesCollector.kt
@@ -9,7 +9,7 @@ import com.intellij.diff.tools.util.base.TextDiffSettingsHolder.TextDiffSettings
 import com.intellij.diff.util.DiffPlaces
 import com.intellij.internal.statistic.beans.MetricEvent
 import com.intellij.internal.statistic.beans.addBoolIfDiffers
-import com.intellij.internal.statistic.beans.addIfDiffers
+import com.intellij.internal.statistic.beans.addMetricsIfDiffers
 import com.intellij.internal.statistic.eventLog.FeatureUsageData
 import com.intellij.internal.statistic.service.fus.collectors.ApplicationUsagesCollector
 import java.util.*
@@ -37,19 +37,21 @@ class DiffUsagesCollector : ApplicationUsagesCollector() {
       val textSettings = TextDiffSettings.getSettings(place)
       val defaultTextSettings = TextDiffSettings.getDefaultSettings(place)
 
-      addBoolIfDiffers(set, textSettings, defaultTextSettings, { it.isEnableSyncScroll }, "sync.scroll", data)
-      addIfDiffers(set, textSettings, defaultTextSettings, { it.ignorePolicy }, "ignore.policy", data)
-      addIfDiffers(set, textSettings, defaultTextSettings, { it.highlightPolicy }, "highlight.policy", data)
-      addIfDiffers(set, textSettings, defaultTextSettings, { it.highlightingLevel }, "show.warnings.policy", data)
-      addIfDiffers(set, textSettings, defaultTextSettings, { it.contextRange }, "context.range", data)
-      addBoolIfDiffers(set, textSettings, defaultTextSettings, { !it.isExpandByDefault }, "collapse.unchanged", data)
-      addBoolIfDiffers(set, textSettings, defaultTextSettings, { it.isShowLineNumbers }, "show.line.numbers", data)
-      addBoolIfDiffers(set, textSettings, defaultTextSettings, { it.isUseSoftWraps }, "use.soft.wraps", data)
+      addMetricsIfDiffers(set, textSettings, defaultTextSettings, data) {
+        addBool("sync.scroll" ) { it.isEnableSyncScroll }
+        add("ignore.policy") { it.ignorePolicy }
+        add("highlight.policy") { it.highlightPolicy }
+        add("show.warnings.policy") { it.highlightingLevel }
+        add("context.range") { it.contextRange }
+        addBool("collapse.unchanged") { !it.isExpandByDefault }
+        addBool("show.line.numbers") { it.isShowLineNumbers }
+        addBool("use.soft.wraps") { it.isUseSoftWraps }
+        addBool("enable.read.lock") { it.isReadOnlyLock }
+        add("show.breadcrumbs") { it.breadcrumbsPlacement }
+        addBool("merge.apply.non.conflicted") { it.isAutoApplyNonConflictedChanges }
+        addBool("merge.enable.lst.markers") { it.isEnableLstGutterMarkersInMerge }
+      }
       addBoolIfDiffers(set, diffSettings, defaultDiffSettings, { isUnifiedToolDefault(it) }, "use.unified.diff", data)
-      addBoolIfDiffers(set, textSettings, defaultTextSettings, { it.isReadOnlyLock }, "enable.read.lock", data)
-      addIfDiffers(set, textSettings, defaultTextSettings, { it.breadcrumbsPlacement }, "show.breadcrumbs", data)
-      addBoolIfDiffers(set, textSettings, defaultTextSettings, { it.isAutoApplyNonConflictedChanges }, "merge.apply.non.conflicted", data)
-      addBoolIfDiffers(set, textSettings, defaultTextSettings, { it.isEnableLstGutterMarkersInMerge }, "merge.enable.lst.markers", data)
     }
 
     val diffSettings = DiffSettings.getSettings(DiffPlaces.DEFAULT)
