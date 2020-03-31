57c0b4281ecf6381b4d2d565a190d97eb9c77cfe
Alexandr Suhinin <alexandr.suhinin@jetbrains.com>
2020-03-31 11:15:41 +0300

    fix test data: indents
    GitOrigin-RevId: 363f48a56331a1b4b1a17a975c0d746a196fa4bf

--- java/java-tests/testData/refactoring/extractMethodNew/PassFieldAsParameterAndMakeStatic_after.java
+++ java/java-tests/testData/refactoring/extractMethodNew/PassFieldAsParameterAndMakeStatic_after.java
@@ -1,10 +1,10 @@
 class X {
   private int myI;
   void foo() {
-    newMethod(this.myI);
+      newMethod(this.myI);
   }
 
-  private static void newMethod(int myI) {
-    int i = myI;
-  }
+    private static void newMethod(int myI) {
+        int i = myI;
+    }
 }
