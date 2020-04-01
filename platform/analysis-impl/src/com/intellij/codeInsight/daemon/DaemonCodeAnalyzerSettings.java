ab13facf9b2f5e06a38647bd642f1cd56214869b
Alexey Kudravtsev <cdr@intellij.com>
2020-04-01 13:33:37 +0300

    cleanup
    GitOrigin-RevId: a33fa51a718e5cb994edb3d9a92b9dd87145731f

--- platform/analysis-impl/src/com/intellij/codeInsight/daemon/DaemonCodeAnalyzerSettings.java
+++ platform/analysis-impl/src/com/intellij/codeInsight/daemon/DaemonCodeAnalyzerSettings.java
@@ -31,8 +31,8 @@ public class DaemonCodeAnalyzerSettings {
     return myAutoReparseDelay;
   }
 
-  public void setAutoReparseDelay(int value) {
-    myAutoReparseDelay = value;
+  public void setAutoReparseDelay(int millis) {
+    myAutoReparseDelay = millis;
   }
 
   @OptionTag("ERROR_STRIPE_MARK_MIN_HEIGHT")
@@ -46,14 +46,14 @@ public class DaemonCodeAnalyzerSettings {
 
   protected boolean myShowAddImportHints = true;
   public String NO_AUTO_IMPORT_PATTERN = "[a-z].?";
-  public boolean SHOW_METHOD_SEPARATORS = false;
+  public boolean SHOW_METHOD_SEPARATORS;
 
   @Transient
   public boolean isCodeHighlightingChanged(DaemonCodeAnalyzerSettings oldSettings) {
     return false;
   }
 
-  @OptionTag(value = "SHOW_ADD_IMPORT_HINTS")
+  @OptionTag("SHOW_ADD_IMPORT_HINTS")
   public boolean isImportHintEnabled() {
     return myShowAddImportHints;
   }
@@ -62,7 +62,7 @@ public class DaemonCodeAnalyzerSettings {
     myShowAddImportHints = isImportHintEnabled;
   }
 
-  @OptionTag(value = "SUPPRESS_WARNINGS")
+  @OptionTag("SUPPRESS_WARNINGS")
   public boolean isSuppressWarnings() {
     return mySuppressWarnings;
   }
