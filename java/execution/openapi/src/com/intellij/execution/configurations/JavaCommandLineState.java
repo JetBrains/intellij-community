d04cf6cd883a778733a002f2cfe2231cfcd40069
Alexander Zolotov <zolotov@jetbrains.com>
2020-04-02 18:40:17 +0300

    [run.targets] fill request before running the environment preparation
    - some targets (SSH) can be prepared only when request is fulfilled
    - different runs shouldn't share prepared environment and requests (it could happen on re-run action)
    
    GitOrigin-RevId: 5b0589904bbfaf56e9edbc588045d7704f0d07d7

--- java/execution/openapi/src/com/intellij/execution/configurations/JavaCommandLineState.java
+++ java/execution/openapi/src/com/intellij/execution/configurations/JavaCommandLineState.java
@@ -1,20 +1,26 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.configurations;
 
+import com.intellij.execution.ExecutionBundle;
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.process.OSProcessHandler;
 import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.execution.target.TargetEnvironmentAwareRunProfileState;
 import com.intellij.execution.target.TargetEnvironmentConfiguration;
 import com.intellij.execution.target.TargetEnvironmentRequest;
 import com.intellij.execution.target.TargetedCommandLineBuilder;
 import com.intellij.execution.target.local.LocalTargetEnvironmentFactory;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.progress.EmptyProgressIndicator;
+import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.util.registry.Registry;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public abstract class JavaCommandLineState extends CommandLineState implements JavaCommandLine {
+public abstract class JavaCommandLineState extends CommandLineState implements JavaCommandLine, TargetEnvironmentAwareRunProfileState {
+  private static final Logger LOG = Logger.getInstance(JavaCommandLineState.class);
   private JavaParameters myParams;
+  private TargetedCommandLineBuilder myCommandLine;
 
   protected JavaCommandLineState(@NotNull ExecutionEnvironment environment) {
     super(environment);
@@ -44,9 +50,36 @@ public abstract class JavaCommandLineState extends CommandLineState implements J
 
   protected abstract JavaParameters createJavaParameters() throws ExecutionException;
 
+  @Override
+  public synchronized void prepareTargetEnvironmentRequest(@NotNull TargetEnvironmentRequest request,
+                                                           @Nullable TargetEnvironmentConfiguration configuration,
+                                                           @NotNull ProgressIndicator progressIndicator) throws ExecutionException {
+    progressIndicator.setText(ExecutionBundle.message("progress.text.prepare.target.requirements"));
+    myCommandLine = createTargetedCommandLine(request, configuration);
+  }
+
+  @NotNull
+  protected synchronized TargetedCommandLineBuilder getTargetedCommandLine() {
+    if (myCommandLine != null) {
+      return myCommandLine;
+    }
+
+    LOG.error("Command line hasn't been built yet. " +
+              "Probably you need to run environment#getPreparedTargetEnvironment first.");
+    try {
+      getEnvironment().getPreparedTargetEnvironment(this, new EmptyProgressIndicator());
+      return myCommandLine;
+    }
+    catch (ExecutionException e) {
+      LOG.error(e);
+      throw new RuntimeException(e);
+    }
+  }
+
   @NotNull
   protected TargetedCommandLineBuilder createTargetedCommandLine(@NotNull TargetEnvironmentRequest request,
-                                                                 @Nullable TargetEnvironmentConfiguration configuration) throws ExecutionException {
+                                                                 @Nullable TargetEnvironmentConfiguration configuration)
+    throws ExecutionException {
     SimpleJavaParameters javaParameters = getJavaParameters();
     if (!javaParameters.isDynamicClasspath()) {
       javaParameters.setUseDynamicClasspath(getEnvironment().getProject());
