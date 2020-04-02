d782499bd3540c33e8a0ecdec588271e6cd259d4
Alexander Zolotov <zolotov@jetbrains.com>
2020-04-02 18:47:24 +0300

    [run.targets] prepare docker volumes bindings at the preparation stage
    + add some progress info to the output
    + get rid of `getRequest` in environment
    
    GitOrigin-RevId: 3245cebfbd087a0026de4adff9cdc6b4155805c6

--- platform/lang-api/src/com/intellij/execution/target/TargetEnvironment.java
+++ platform/lang-api/src/com/intellij/execution/target/TargetEnvironment.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.target;
 
 import com.intellij.execution.ExecutionException;
@@ -15,8 +15,6 @@ import org.jetbrains.annotations.NotNull;
 public interface TargetEnvironment {
   @NotNull
   Process createProcess(@NotNull TargetedCommandLine commandLine, @NotNull ProgressIndicator indicator) throws ExecutionException;
-
-  TargetEnvironmentRequest getRequest();
 
   @NotNull
   TargetPlatform getRemotePlatform();
--- platform/lang-api/src/com/intellij/execution/target/local/LocalTargetEnvironment.java
+++ platform/lang-api/src/com/intellij/execution/target/local/LocalTargetEnvironment.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.target.local;
 
 import com.intellij.execution.CantRunException;
@@ -19,12 +19,6 @@ public class LocalTargetEnvironment implements TargetEnvironment {
 
   public LocalTargetEnvironment(@NotNull TargetEnvironmentRequest request) {
     myRequest = request;
-  }
-
-  @NotNull
-  @Override
-  public TargetEnvironmentRequest getRequest() {
-    return myRequest;
   }
 
   @NotNull
