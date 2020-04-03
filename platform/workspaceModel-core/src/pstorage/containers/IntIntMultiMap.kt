608eca39d30ba9207304bdcb5a9ec6ada733e1e6
Alex Plate <aleksei.plate@jetbrains.com>
2020-04-03 16:12:01 +0300

    [Workspace Model] Update IntIntMultiMap implementation
    GitOrigin-RevId: f136889cadc746e7c05ed3127a035995b6256c1f

--- platform/workspaceModel-core/src/pstorage/containers/IntIntMultiMap.kt
+++ platform/workspaceModel-core/src/pstorage/containers/IntIntMultiMap.kt
@@ -38,14 +38,15 @@ internal sealed class IntIntMultiMap(
   }
 
   override operator fun get(key: Int): IntSequence {
-    if (key !in links) return IntSequence.Empty
-
-    return RoIntSequence(values, links[key])
+    if (key !in links) return EmptyIntSequence
+    val idx = links[key]
+    if (idx >= 0) return SingleResultIntSequence(idx)
+    return RoMultiResultIntSequence(values, idx.unpack())
   }
 
   abstract fun toMutable(): MutableIntIntMultiMap
 
-  private class RoIntSequence(
+  private class RoMultiResultIntSequence(
     private val values: IntArray?,
     private var idx: Int
   ) : IntSequence() {
@@ -95,22 +96,22 @@ internal sealed class MutableIntIntMultiMap(
   }
 
   override fun get(key: Int): IntSequence {
-    if (key !in links) return IntSequence.Empty
+    if (key !in links) return EmptyIntSequence
 
+    var idx = links[key]
+    if (idx >= 0) return SingleResultIntSequence(idx)
+
+    // idx is a link to  values
+    idx = idx.unpack()
     val size = size(key)
-    val startId = links[key]
-    val vals = values.sliceArray(startId until (startId + size))
-    return if (vals.isEmpty()) {
-      IntSequence.Empty
-    }
-    else {
-      vals[vals.lastIndex] = vals.last().unpack()
-      RwIntSequence(vals)
-    }
+    val vals = values.sliceArray(idx until (idx + size))
+    vals[vals.lastIndex] = vals.last().unpack()
+    return RwIntSequence(vals)
+
   }
 
   fun put(key: Int, value: Int) {
-    put(key, intArrayOf(value))
+    putAll(key, intArrayOf(value))
   }
 
   private fun exists(value: Int, startRange: Int, endRange: Int): Boolean {
@@ -121,56 +122,99 @@ internal sealed class MutableIntIntMultiMap(
     return false
   }
 
-  fun put(key: Int, newValues: IntArray) {
-    if (key in links) {
-      val idx = links[key]
-      val endIndexInclusive = idx + size(key)
+  fun putAll(key: Int, newValues: IntArray): Boolean {
+    if (newValues.isEmpty()) return false
+    return if (key in links) {
+      var idx = links[key]
+      if (idx < 0) {
+        // Adding new values to existing that are already stored in the [values] array
+        idx = idx.unpack()
+        val endIndexInclusive = idx + size(key)
 
-      val filteredValues = if (distinctValues) {
-        newValues.filterNot { exists(it, idx, endIndexInclusive - 1) }.toTypedArray().toIntArray()
+        val filteredValues = if (distinctValues) {
+          newValues.filterNot { exists(it, idx, endIndexInclusive - 1) }.toTypedArray().toIntArray()
+        }
+        else newValues
+
+        val newValuesSize = filteredValues.size
+
+        val newArray = IntArray(values.size + newValuesSize)
+        values.copyInto(newArray, 0, 0, endIndexInclusive)
+        if (endIndexInclusive + newValuesSize < newArray.size) {
+          values.copyInto(newArray, endIndexInclusive + newValuesSize, endIndexInclusive)
+        }
+        filteredValues.forEachIndexed { index, value ->
+          newArray[endIndexInclusive + index] = value
+        }
+        newArray[endIndexInclusive + newValuesSize - 1] = filteredValues.last().pack()
+        val oldPrevValue = newArray[endIndexInclusive - 1]
+        newArray[endIndexInclusive - 1] = oldPrevValue.unpack()
+        this.values = newArray
+
+        // Update existing links
+        rightShiftLinks(idx, newValuesSize)
+
+        true // Returned value
       }
-      else newValues
+      else {
+        // This map already contains value, but it's stored directly in the [links]
+        // We should take this value, prepend to the new values and store them into [values]
+        val newValuesSize = newValues.size
+        val arraySize = values.size
+        val newArray = IntArray(arraySize + newValuesSize + 1) // plus one for the value from links
 
-      val newValuesSize = filteredValues.size
+        values.copyInto(newArray) // Put all previous values into array
+        newArray[arraySize] = idx // Put an existing value into array
+        newValues.copyInto(newArray, arraySize + 1)  // Put all new values
+        newArray[arraySize + newValuesSize] = newValues.last().pack()  // Mark last value as the last one
 
-      val newArray = IntArray(values.size + newValuesSize)
-      values.copyInto(newArray, 0, 0, endIndexInclusive)
-      if (endIndexInclusive + newValuesSize < newArray.size) {
-        values.copyInto(newArray, endIndexInclusive + newValuesSize, endIndexInclusive)
-      }
-      filteredValues.forEachIndexed { index, value ->
-        newArray[endIndexInclusive + index] = value
-      }
-      newArray[endIndexInclusive + newValuesSize - 1] = filteredValues.last().pack()
-      val oldPrevValue = newArray[endIndexInclusive - 1]
-      newArray[endIndexInclusive - 1] = oldPrevValue.unpack()
-      this.values = newArray
+        this.values = newArray
 
-      links.keys().forEach { keyToUpdate ->
-        val valueToUpdate = links[keyToUpdate]
-        if (valueToUpdate > idx) links.put(keyToUpdate, valueToUpdate + newValuesSize)
+        links.put(key, arraySize.pack())
+
+        true // Returned value
       }
     }
     else {
+      // This key wasn't stored in the store before
       val newValuesSize = newValues.size
-      val arraySize = values.size
-      val newArray = IntArray(arraySize + newValuesSize)
-      values.copyInto(newArray)
+      if (newValuesSize > 1) {
+        // There is more than one element in new values, so we should store them in [values]
+        val arraySize = values.size
+        val newArray = IntArray(arraySize + newValuesSize)
 
-      newValues.forEachIndexed { index, value ->
-        newArray[arraySize + index] = value
+        values.copyInto(newArray)
+        newValues.copyInto(newArray, arraySize)  // Put all new values
+
+        newArray[arraySize + newValuesSize - 1] = newValues.last().pack()
+        this.values = newArray
+
+        links.put(key, arraySize.pack())
+
+        true // Returned value
       }
-      newArray[arraySize + newValuesSize - 1] = newValues.last().pack()
-      this.values = newArray
+      else {
+        // Great! Only one value to store. No need to allocate memory in the [values]
+        links.put(key, newValues.single())
 
-      links.put(key, arraySize)
+        true // Returned value
+      }
     }
   }
 
   fun remove(key: Int) {
     if (key !in links) return
 
-    val idx = links[key]
+    var idx = links[key]
+
+    if (idx >= 0) {
+      // Only one value in the store
+      links.remove(key)
+      return
+    }
+
+    idx = idx.unpack()
+
     val size = values.size
 
     val sizeToRemove = size(key)
@@ -182,22 +226,30 @@ internal sealed class MutableIntIntMultiMap(
 
     links.remove(key)
 
-    links.keys().forEach { keyToUpdate ->
-      val valueToUpdate = links[keyToUpdate]
-      if (valueToUpdate > idx) links.put(keyToUpdate, valueToUpdate - sizeToRemove)
-    }
+    // Update existing links
+    rightShiftLinks(idx, -sizeToRemove)
   }
 
   fun remove(key: Int, value: Int): Boolean {
     if (key !in links) return false
 
     var idx = links[key]
+
+    if (idx >= 0) {
+      if (value == idx) {
+        links.remove(key)
+        return true
+      }
+      else return false
+    }
+
+    idx = idx.unpack()
+
     val valuesStartIndex = idx
     val size = values.size
     var foundIndex = -1
 
     // Search for the value in the values list
-    // In this loop last value is skipped in search (because it's negative and we should unpack it)
     var removeLast = false
     var valueUnderIdx: Int
     do {
@@ -216,28 +268,52 @@ internal sealed class MutableIntIntMultiMap(
         foundIndex = idx
         break
       }
+      idx++
     }
-    while (idx++ != size)
-    val removingLastValue = removeLast && idx == valuesStartIndex
+    while (true)
 
     // There is no such value by this key
     if (foundIndex == -1) return false
 
-    val newArray = IntArray(size - 1)
-    values.copyInto(newArray, 0, 0, foundIndex)
-    values.copyInto(newArray, foundIndex, foundIndex + 1)
-    values = newArray
-    if (removeLast && !removingLastValue) {
-      values[foundIndex - 1] = values[foundIndex - 1].pack()
+    // If there is only two values for the key remains, after removing one of them we should put the remaining value directly into [links]
+    val remainsOneValueInContainer = removeLast && idx == valuesStartIndex + 1   // Removing last value of two values
+                                     || idx == valuesStartIndex && values[idx + 1] < 0 // Removing first value of two values
+
+    return if (!remainsOneValueInContainer) {
+      val newArray = IntArray(size - 1)
+      values.copyInto(newArray, 0, 0, foundIndex)
+      values.copyInto(newArray, foundIndex, foundIndex + 1)
+      values = newArray
+      if (removeLast) {
+        values[foundIndex - 1] = values[foundIndex - 1].pack()
+      }
+
+      rightShiftLinks(idx, -1)
+
+      true
     }
+    else {
+      val remainedValue = if (removeLast) values[idx - 1] else values[idx + 1].unpack()
+      val newArray = IntArray(size - 2)
+      values.copyInto(newArray, 0, 0, foundIndex)
+      values.copyInto(newArray, foundIndex, foundIndex + 2)
+      values = newArray
 
-    if (removingLastValue) links.remove(key)
+      links.put(key, remainedValue)
 
+      rightShiftLinks(idx, -2)
+
+      true
+    }
+  }
+
+  private fun rightShiftLinks(idx: Int, shiftTo: Int) {
     links.keys().forEach { keyToUpdate ->
       val valueToUpdate = links[keyToUpdate]
-      if (valueToUpdate > idx) links.put(keyToUpdate, valueToUpdate - 1)
+      if (valueToUpdate >= 0) return@forEach
+      val unpackedValue = valueToUpdate.unpack()
+      if (unpackedValue > idx) links.put(keyToUpdate, (unpackedValue + shiftTo).pack())
     }
-    return true
   }
 
   fun clear() {
@@ -264,38 +340,41 @@ internal sealed class AbstractIntIntMultiMap(
     if (key !in links) return
 
     var idx = links[key]
-    val size = values.size
+    if (idx >= 0) {
+      // It's value
+      action(idx)
+      return
+    }
+
+    // It's a link to values
+    idx = idx.unpack()
 
     var value: Int
     do {
-      value = values[idx]
+      value = values[idx++]
       if (value < 0) break
       action(value)
     }
-    while (idx++ != size)
+    while (true)
 
     action(value.unpack())
   }
 
+  /** This method works o(n) */
   fun size(key: Int): Int {
     if (key !in links) return 0
 
     var idx = links[key]
-    var res = 0
-    val size = values.size
+    if (idx >= 0) return 1
 
-    while (idx != size && values[idx++] >= 0) res++
+    idx = idx.unpack()
+
+    // idx is a link to values
+    var res = 0
+
+    while (values[idx++] >= 0) res++
 
     return res + 1
-  }
-
-
-  private fun exists(value: Int, startRange: Int, endRange: Int): Boolean {
-    for (i in startRange until endRange) {
-      if (values[i] == value) return true
-    }
-    if (values[endRange] == value.pack()) return true
-    return false
   }
 
   operator fun contains(key: Int): Boolean = key in links
@@ -305,8 +384,7 @@ internal sealed class AbstractIntIntMultiMap(
   abstract fun copy(): AbstractIntIntMultiMap
 
   protected fun doCopy(): Pair<IntArray, TIntIntHashMap> {
-    val newLinks = TIntIntHashMap()
-    links.forEachEntry { key, value -> newLinks.put(key, value); true }
+    val newLinks = TIntIntHashMap().clone() as TIntIntHashMap
     val newValues = values.clone()
     return newValues to newLinks
   }
@@ -328,7 +406,7 @@ internal sealed class AbstractIntIntMultiMap(
 
     /**
      * Please use this method only for debugging purposes.
-     * Some of implementations doesn't have any memory overhead when using this [IntSequence]
+     * Some of implementations doesn't have any memory overhead when using [IntSequence].
      */
     @TestOnly
     internal fun toArray(): IntArray {
@@ -348,11 +426,17 @@ internal sealed class AbstractIntIntMultiMap(
         }
       }
     }
+  }
 
-    internal object Empty : IntSequence() {
-      override val iterator: IntIterator = IntArray(0).iterator()
+  protected class SingleResultIntSequence(
+    value: Int
+  ) : IntSequence() {
+    override val iterator: IntIterator = intArrayOf(value).iterator()
+  }
 
-      override fun <T> map(transformation: (Int) -> T): Sequence<T> = emptySequence()
-    }
+  protected object EmptyIntSequence : IntSequence() {
+    override val iterator: IntIterator = IntArray(0).iterator()
+
+    override fun <T> map(transformation: (Int) -> T): Sequence<T> = emptySequence()
   }
 }
