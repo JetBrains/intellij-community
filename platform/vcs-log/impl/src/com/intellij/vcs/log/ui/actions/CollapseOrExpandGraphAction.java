4c3869077cdae353ce9c95bddc37ff82ca29402c
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2020-04-05 00:53:38 +0300

    [vcs-log] simplify code, do not disable the action when VcsLogUiProperties instance is null
    GitOrigin-RevId: 445421b9e509967cc990c86b625c9dc4335e8baa

--- platform/vcs-log/impl/src/com/intellij/vcs/log/ui/actions/CollapseOrExpandGraphAction.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/ui/actions/CollapseOrExpandGraphAction.java
@@ -16,6 +16,7 @@
 package com.intellij.vcs.log.ui.actions;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.DumbAwareAction;
@@ -62,23 +63,14 @@ abstract class CollapseOrExpandGraphAction extends DumbAwareAction {
     MainVcsLogUi ui = e.getData(VcsLogInternalDataKeys.MAIN_UI);
     VcsLogUiProperties properties = e.getData(VcsLogInternalDataKeys.LOG_UI_PROPERTIES);
 
-    if (ui != null && !ui.getDataPack().isEmpty() && properties != null && properties.exists(MainVcsLogUiProperties.BEK_SORT_TYPE)) {
-      e.getPresentation().setEnabled(true);
-      if (!ui.getFilterUi().getFilters().getDetailsFilters().isEmpty()) {
-        e.getPresentation().setEnabled(false);
-      }
-
-      if (properties.get(MainVcsLogUiProperties.BEK_SORT_TYPE) == PermanentGraph.SortType.LinearBek) {
-        e.getPresentation().setText(myMergesAction.get());
-        e.getPresentation().setDescription(myMergesDescription.get());
-      }
-      else {
-        e.getPresentation().setText(myLinearBranchesAction.get());
-        e.getPresentation().setDescription(myLinearBranchesDescription.get());
-      }
+    e.getPresentation().setEnabled(ui != null && !ui.getDataPack().isEmpty() &&
+                                   ui.getFilterUi().getFilters().getDetailsFilters().isEmpty());
+    if (properties != null && properties.exists(MainVcsLogUiProperties.BEK_SORT_TYPE) &&
+        properties.get(MainVcsLogUiProperties.BEK_SORT_TYPE) == PermanentGraph.SortType.LinearBek) {
+      e.getPresentation().setText(myMergesAction.get());
+      e.getPresentation().setDescription(myMergesDescription.get());
     }
     else {
-      e.getPresentation().setEnabled(false);
       e.getPresentation().setText(myLinearBranchesAction.get());
       e.getPresentation().setDescription(myLinearBranchesDescription.get());
     }
