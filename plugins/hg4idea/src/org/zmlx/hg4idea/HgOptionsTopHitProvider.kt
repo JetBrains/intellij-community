9d4df0c1a34ab7748bfeb577357d5be612732724
Aleksey Pivovarov <AMPivovarov@gmail.com>
2020-04-06 18:29:50 +0300

    vcs: i18n TopHitProviders
    * get rid of reflection
    
    GitOrigin-RevId: 0075df4597eb4f0f2f25c1b1a4a8ab7aeab6646f

--- plugins/hg4idea/src/org/zmlx/hg4idea/HgOptionsTopHitProvider.kt
+++ plugins/hg4idea/src/org/zmlx/hg4idea/HgOptionsTopHitProvider.kt
@@ -1,43 +1,32 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.zmlx.hg4idea;
+package org.zmlx.hg4idea
 
-import com.intellij.ide.ui.PublicMethodBasedOptionDescription;
-import com.intellij.ide.ui.search.BooleanOptionDescription;
-import com.intellij.ide.ui.search.OptionDescription;
-import com.intellij.openapi.components.ServiceManager;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.configurable.VcsOptionsTopHitProviderBase;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.application.options.editor.CheckboxDescriptor
+import com.intellij.ide.ui.search.OptionDescription
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vcs.configurable.VcsOptionsTopHitProviderBase
+import com.intellij.ui.layout.*
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
+private val hgOptionGroupName get() = HgBundle.message("hg4idea.mercurial")
+private fun configuration(project: Project) = HgProjectSettings.getInstance(project)
 
-final class HgOptionsTopHitProvider extends VcsOptionsTopHitProviderBase {
-  @NotNull
-  @Override
-  public String getId() {
-    return "vcs";
+// @formatter:off
+private fun cdCheckIncomingOutgoing(project: Project): CheckboxDescriptor =           CheckboxDescriptor(HgBundle.message("hg4idea.configuration.check.incoming.outgoing"), PropertyBinding({ configuration(project).isCheckIncomingOutgoing }, { configuration(project).isCheckIncomingOutgoing = it }), groupName = hgOptionGroupName)
+private fun cdIgnoreWhitespacesInAnnotations(project: Project): CheckboxDescriptor =  CheckboxDescriptor(HgBundle.message("hg4idea.configuration.ignore.whitespace.in.annotate"), PropertyBinding({ configuration(project).isWhitespacesIgnoredInAnnotations }, { configuration(project).setIgnoreWhitespacesInAnnotations(it) }), groupName = hgOptionGroupName)
+// @formatter:on
+
+internal class HgOptionsTopHitProvider : VcsOptionsTopHitProviderBase() {
+  override fun getId(): String {
+    return "vcs"
   }
 
-  @NotNull
-  @Override
-  public Collection<OptionDescription> getOptions(@NotNull Project project) {
+  override fun getOptions(project: Project): Collection<OptionDescription> {
     if (isEnabled(project, HgVcs.getInstance(project))) {
-      return Collections.unmodifiableCollection(Arrays.asList(
-        option(project, "Mercurial: Check for incoming and outgoing changesets", "isCheckIncomingOutgoing", "setCheckIncomingOutgoing"),
-        option(project, "Mercurial: Ignore whitespace differences in annotations", "isWhitespacesIgnoredInAnnotations",
-               "setIgnoreWhitespacesInAnnotations")));
+      return listOf(
+        cdCheckIncomingOutgoing(project),
+        cdIgnoreWhitespacesInAnnotations(project)
+      ).map(CheckboxDescriptor::asOptionDescriptor)
     }
-    return Collections.emptyList();
-  }
-
-  private static BooleanOptionDescription option(final Project project, String option, String getter, String setter) {
-    return new PublicMethodBasedOptionDescription(option, "vcs.Mercurial", getter, setter) {
-      @Override
-      public Object getInstance() {
-        return ServiceManager.getService(project, HgProjectSettings.class);
-      }
-    };
+    return emptyList()
   }
 }
--- plugins/svn4idea/src/org/jetbrains/idea/svn/SvnOptionsTopHitProvider.kt
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/SvnOptionsTopHitProvider.kt
@@ -1,44 +1,38 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.idea.svn;
+package org.jetbrains.idea.svn
 
-import com.intellij.ide.ui.PublicMethodBasedOptionDescription;
-import com.intellij.ide.ui.search.BooleanOptionDescription;
-import com.intellij.ide.ui.search.OptionDescription;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.configurable.VcsOptionsTopHitProviderBase;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.application.options.editor.CheckboxDescriptor
+import com.intellij.ide.ui.search.OptionDescription
+import com.intellij.openapi.application.ApplicationNamesInfo
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vcs.configurable.VcsOptionsTopHitProviderBase
+import com.intellij.ui.layout.*
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 
-final class SvnOptionsTopHitProvider extends VcsOptionsTopHitProviderBase {
-  @NotNull
-  @Override
-  public String getId() {
-    return "vcs";
+private val svnOptionGroupName get() = SvnBundle.message("subversion.name")
+private fun configuration(project: Project) = SvnConfiguration.getInstance(project)
+
+// @formatter:off
+private fun cdCheckMergeInfo(project: Project): CheckboxDescriptor =                  CheckboxDescriptor(SvnBundle.message("settings.check.mergeinfo"), PropertyBinding({ configuration(project).isCheckNestedForQuickMerge }, { configuration(project).isCheckNestedForQuickMerge = it }), groupName = svnOptionGroupName)
+private fun cdShowMergeSource(project: Project): CheckboxDescriptor =                 CheckboxDescriptor(SvnBundle.message("annotation.show.merge.sources.default.text"), PropertyBinding({ configuration(project).isShowMergeSourcesInAnnotate }, { configuration(project).isShowMergeSourcesInAnnotate = it }), groupName = svnOptionGroupName)
+private fun cdIgnoreWhitespacesInAnnotations(project: Project): CheckboxDescriptor =  CheckboxDescriptor(SvnBundle.message("svn.option.ignore.whitespace.in.annotate"), PropertyBinding({ configuration(project).isIgnoreSpacesInAnnotate }, { configuration(project).isIgnoreSpacesInAnnotate = it }), groupName = svnOptionGroupName)
+private fun cdUseGeneralProxy(project: Project): CheckboxDescriptor =                 CheckboxDescriptor(SvnBundle.message("use.idea.proxy.as.default", ApplicationNamesInfo.getInstance().productName), PropertyBinding({ configuration(project).isUseDefaultProxy }, { configuration(project).isUseDefaultProxy = it }), groupName = svnOptionGroupName)
+// @formatter:on
+
+internal class SvnOptionsTopHitProvider : VcsOptionsTopHitProviderBase() {
+  override fun getId(): String {
+    return "vcs"
   }
 
-  @NotNull
-  @Override
-  public Collection<OptionDescription> getOptions(@NotNull Project project) {
+  override fun getOptions(project: Project): Collection<OptionDescription> {
     if (isEnabled(project, SvnVcs.getInstance(project))) {
-      final SvnConfiguration config = SvnConfiguration.getInstance(project);
-      return Collections.unmodifiableCollection(Arrays.asList(
-        option(config, "Subversion: Check svn:mergeinfo in target subtree when preparing for merge", "isCheckNestedForQuickMerge", "setCheckNestedForQuickMerge"),
-        option(config, "Subversion: Show merge source in history and annotations", "isShowMergeSourcesInAnnotate", "setShowMergeSourcesInAnnotate"),
-        option(config, "Subversion: Ignore whitespace differences in annotations", "isIgnoreSpacesInAnnotate", "setIgnoreSpacesInAnnotate"),
-        option(config, "Subversion: Use IDEA general proxy settings as default for Subversion", "isUseDefaultProxy", "setUseDefaultProxy")));
+      return listOf(
+        cdCheckMergeInfo(project),
+        cdShowMergeSource(project),
+        cdIgnoreWhitespacesInAnnotations(project),
+        cdUseGeneralProxy(project)
+      ).map(CheckboxDescriptor::asOptionDescriptor)
     }
-    return Collections.emptyList();
-  }
-
-  private static BooleanOptionDescription option(final Object instance, String option, String getter, String setter) {
-    return new PublicMethodBasedOptionDescription(option, "vcs.Subversion", getter, setter) {
-      @Override
-      public Object getInstance() {
-        return instance;
-      }
-    };
+    return emptyList()
   }
 }
