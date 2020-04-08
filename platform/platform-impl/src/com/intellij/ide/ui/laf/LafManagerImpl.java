c7369acc326e217c886d8065af2abe6302dd79ea
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2020-04-08 13:12:11 +0300

    Provide utility methods to enable and disable font kerning
    GitOrigin-RevId: da8e89ee07e1295c0a943619b59e5c11780cca30

--- platform/platform-impl/src/com/intellij/ide/ui/laf/LafManagerImpl.java
+++ platform/platform-impl/src/com/intellij/ide/ui/laf/LafManagerImpl.java
@@ -60,11 +60,11 @@ import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.font.TextAttribute;
-import java.text.AttributedCharacterIterator;
 import java.util.List;
 import java.util.*;
 import java.util.function.BooleanSupplier;
+
+import static com.intellij.util.FontUtil.enableKerning;
 
 @State(name = "LafManager", storages = @Storage(value = "laf.xml", roamingType = RoamingType.PER_OS))
 public final class LafManagerImpl extends LafManager implements PersistentStateComponent<Element>, Disposable {
@@ -628,8 +628,7 @@ public final class LafManagerImpl extends LafManager implements PersistentStateC
     if (SystemInfo.isMacOSElCapitan) {
       // Text family should be used for relatively small sizes (<20pt), don't change to Display
       // see more about SF https://medium.com/@mach/the-secret-of-san-francisco-fonts-4b5295d9a745#.2ndr50z2v
-      Map<AttributedCharacterIterator.Attribute, Integer> attributes = Collections.singletonMap(TextAttribute.KERNING, TextAttribute.KERNING_ON);
-      Font font = new Font(SystemInfo.isMacOSCatalina ? ".AppleSystemUIFont" : ".SF NS Text", style, size).deriveFont(attributes);
+      Font font = enableKerning(new Font(SystemInfo.isMacOSCatalina ? ".AppleSystemUIFont" : ".SF NS Text", style, size));
       if (!StartupUiUtil.isDialogFont(font)) {
         return new FontUIResource(font);
       }
