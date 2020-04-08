6739bf953eb5d5ea6da3a5db8e4043ba71f2b902
Florian Kistner <florian.kistner@jetbrains.com>
2020-04-08 23:15:40 +0200

    EA-221723 Backport tests to 201
    GitOrigin-RevId: ff970db5919f531f5d3572a51f3b69cb0072ad86

--- platform/platform-tests/testSrc/com/intellij/openapi/progress/impl/BackgroundableProcessIndicatorTest.kt
+++ platform/platform-tests/testSrc/com/intellij/openapi/progress/impl/BackgroundableProcessIndicatorTest.kt
@@ -4,53 +4,73 @@ package com.intellij.openapi.progress.impl
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.Task
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
-import com.intellij.openapi.wm.ex.StatusBarEx
+import com.intellij.openapi.util.Pair
+import com.intellij.openapi.util.use
 import com.intellij.openapi.wm.impl.IdeFrameImpl
 import com.intellij.openapi.wm.impl.ProjectFrameHelper
-import com.intellij.testFramework.FileEditorManagerTestCase
-import com.intellij.testFramework.PlatformTestUtil
+import com.intellij.openapi.wm.impl.status.IdeStatusBarImpl
 import com.intellij.testFramework.SkipInHeadlessEnvironment
-import kotlinx.coroutines.*
-import java.util.concurrent.CyclicBarrier
-import java.util.concurrent.TimeUnit
+import com.intellij.util.concurrency.Semaphore
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
 
 @SkipInHeadlessEnvironment
-class BackgroundableProcessIndicatorTest : FileEditorManagerTestCase() {
-  private val TIMEOUT_MS = 30_000L
+class BackgroundableProcessIndicatorTest : ProgressWindowTestCase<IdeStatusBarImpl, Pair<Task.Backgroundable, BackgroundableProcessIndicator>>() {
+  private lateinit var statusBar: IdeStatusBarImpl
 
-  fun `test can start off EDT`(): Unit = runBlocking {
-    withTimeout(TIMEOUT_MS) {
-      val statusBar = frame().statusBar as StatusBarEx
-      val barrier = CyclicBarrier(2)
+  override fun setUp(): Unit = super.setUp().also {
+    val frameHelper = ProjectFrameHelper(IdeFrameImpl(), null)
+    Disposer.register(testRootDisposable, frameHelper)
+    frameHelper.init()
+    statusBar = frameHelper.statusBar as IdeStatusBarImpl
+  }
 
-      launch(Dispatchers.Default) {
-        suspendCancellableCoroutine<Unit> { cont ->
-          val task = testTask {
-            cont.resume(Unit)
-          }
-          val indicator = BackgroundableProcessIndicator(task.project, task, task, statusBar)
-          cont.invokeOnCancellation { indicator.cancel() }
-          barrier.await(TIMEOUT_MS, TimeUnit.MILLISECONDS)
-          ProgressManager.getInstance().runProcessWithProgressAsynchronously(task, indicator)
+  override fun Pair<Task.Backgroundable, BackgroundableProcessIndicator>.use(block: () -> Unit) {
+    second.use { block() }
+  }
+
+  override fun createProcess(): Pair<Task.Backgroundable, BackgroundableProcessIndicator> {
+    val task = TestTask(project)
+    val indicator = BackgroundableProcessIndicator(task.project, task, task, statusBar)
+    return Pair(task, indicator)
+  }
+
+  override suspend fun runProcessOnEdt(process: Pair<Task.Backgroundable, BackgroundableProcessIndicator>, block: () -> Unit) {
+    ProgressManager.getInstance().runProcess(block, process.second)
+  }
+
+  override suspend fun createAndRunProcessOffEdt(deferredProcess: CompletableDeferred<Pair<Task.Backgroundable, BackgroundableProcessIndicator>>, mayComplete: Semaphore) {
+    suspendCancellableCoroutine<Unit> { cont ->
+      val task = object : TestTask(project) {
+        override fun run(indicator: ProgressIndicator) {
+          mayComplete.waitFor(TIMEOUT_MS)
+          cont.resume(Unit)
         }
       }
+      val indicator = BackgroundableProcessIndicator(task.project, task, task, statusBar)
+      cont.invokeOnCancellation { indicator.cancel() }
 
-      barrier.await(TIMEOUT_MS, TimeUnit.MILLISECONDS)
-      PlatformTestUtil.dispatchAllInvocationEventsInIdeEventQueue()
+      deferredProcess.complete(Pair(task, indicator))
+      ProgressManager.getInstance().runProcessWithProgressAsynchronously(task, indicator)
     }
   }
 
-  private fun frame(): ProjectFrameHelper = ProjectFrameHelper(IdeFrameImpl(), null).also {
-    Disposer.register(testRootDisposable, it)
-    it.init()
+  override fun showDialog(process: Pair<Task.Backgroundable, BackgroundableProcessIndicator>) {
+    process.second.showDialog()
   }
 
-  private inline fun testTask(crossinline block: (ProgressIndicator) -> Unit = {}): Task.Backgroundable =
-    object : Task.Backgroundable(project, "Test Title") {
-      override fun run(indicator: ProgressIndicator) {
-        block(indicator)
-      }
-    }
+  override fun assertUninitialized(process: Pair<Task.Backgroundable, BackgroundableProcessIndicator>) {
+    assertEmpty("Unexpected background processes", statusBar.backgroundProcesses)
+  }
+
+  override fun assertInitialized(process: Pair<Task.Backgroundable, BackgroundableProcessIndicator>) {
+    assertContainsOrdered(statusBar.backgroundProcesses, process)
+  }
+
+  private open class TestTask(project: Project) : Task.Backgroundable(project, "Test Task") {
+    override fun run(indicator: ProgressIndicator): Unit = Unit
+  }
 }
