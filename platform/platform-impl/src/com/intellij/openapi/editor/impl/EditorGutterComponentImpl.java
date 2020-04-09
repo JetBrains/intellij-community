835c50e00dd5e683854d9124a150674fc4dbf561
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2020-04-09 15:55:04 +0300

    IDEA-237052 Ability to change the width of free painters area in the gutter
    GitOrigin-RevId: d1dd29465682e56ceb75157e507be2d3d6191e6a

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorGutterComponentImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/EditorGutterComponentImpl.java
@@ -165,6 +165,8 @@ final class EditorGutterComponentImpl extends EditorGutterComponentEx implements
   private boolean myRightFreePaintersAreaShown;
   boolean myForceLeftFreePaintersAreaShown;
   boolean myForceRightFreePaintersAreaShown;
+  private short myForcedLeftFreePaintersAreaWidth = -1;
+  private short myForcedRightFreePaintersAreaWidth = -1;
   private int myLastNonDumbModeIconAreaWidth;
   boolean myDnDInProgress;
   @Nullable private AccessibleGutterLine myAccessibleGutterLine;
@@ -1589,11 +1591,15 @@ final class EditorGutterComponentImpl extends EditorGutterComponentEx implements
   }
 
   private int getLeftFreePaintersAreaWidth() {
-    return myLeftFreePaintersAreaShown ? FREE_PAINTERS_LEFT_AREA_WIDTH.get() : 0;
+    return myLeftFreePaintersAreaShown ? myForcedLeftFreePaintersAreaWidth < 0 ? FREE_PAINTERS_LEFT_AREA_WIDTH.get()
+                                                                               : myForcedLeftFreePaintersAreaWidth
+                                       : 0;
   }
 
   private int getRightFreePaintersAreaWidth() {
-    return myRightFreePaintersAreaShown ? FREE_PAINTERS_RIGHT_AREA_WIDTH.get() : 0;
+    return myRightFreePaintersAreaShown ? myForcedRightFreePaintersAreaWidth < 0 ? FREE_PAINTERS_RIGHT_AREA_WIDTH.get()
+                                                                                 : myForcedRightFreePaintersAreaWidth
+                                        : 0;
   }
 
   @Override
@@ -2055,6 +2061,18 @@ final class EditorGutterComponentImpl extends EditorGutterComponentEx implements
   @Override
   public void setForceShowRightFreePaintersArea(boolean value) {
     myForceRightFreePaintersAreaShown = value;
+  }
+
+  @Override
+  public void setLeftFreePaintersAreaWidth(int widthInPixels) {
+    if (widthInPixels < 0 || widthInPixels > Short.MAX_VALUE) throw new IllegalArgumentException();
+    myForcedLeftFreePaintersAreaWidth = (short)widthInPixels;
+  }
+
+  @Override
+  public void setRightFreePaintersAreaWidth(int widthInPixels) {
+    if (widthInPixels < 0 || widthInPixels > Short.MAX_VALUE) throw new IllegalArgumentException();
+    myForcedRightFreePaintersAreaWidth = (short)widthInPixels;
   }
 
   @Override
