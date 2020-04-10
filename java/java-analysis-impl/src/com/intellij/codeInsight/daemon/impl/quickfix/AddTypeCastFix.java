f6bfce59989a3fc0412bd691380e158089f2a6f0
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2020-04-10 16:57:43 +0700

    AddTypeCastFix: preview-friendly
    GitOrigin-RevId: c2eefda5d065c43eeadf72c61aad18fa9849bddc

--- java/java-analysis-impl/src/com/intellij/codeInsight/daemon/impl/quickfix/AddTypeCastFix.java
+++ java/java-analysis-impl/src/com/intellij/codeInsight/daemon/impl/quickfix/AddTypeCastFix.java
@@ -5,9 +5,11 @@ package com.intellij.codeInsight.daemon.impl.quickfix;
 import com.intellij.codeInsight.daemon.QuickFixActionRegistrar;
 import com.intellij.codeInsight.daemon.QuickFixBundle;
 import com.intellij.codeInsight.guess.GuessManager;
+import com.intellij.codeInsight.intention.FileModifier;
 import com.intellij.codeInsight.intention.HighPriorityAction;
 import com.intellij.codeInsight.intention.impl.BaseIntentionAction;
 import com.intellij.codeInspection.LocalQuickFixAndIntentionActionOnPsiElement;
+import com.intellij.codeInspection.ex.QuickFixWrapper;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
@@ -154,5 +156,11 @@ public class AddTypeCastFix extends LocalQuickFixAndIntentionActionOnPsiElement 
       }
       registrar.register(fixRange, new AddTypeCastFix(conjunct, qualifier, "add.qualifier.typecast.text"), null);
     }
+  }
+
+  @Override
+  public @Nullable FileModifier getFileModifierForPreview(@NotNull PsiFile target) {
+    PsiExpression expression = (PsiExpression)getStartElement();
+    return expression == null ? null : new AddTypeCastFix(myType, QuickFixWrapper.findSameElementInCopy(expression, target));
   }
 }
