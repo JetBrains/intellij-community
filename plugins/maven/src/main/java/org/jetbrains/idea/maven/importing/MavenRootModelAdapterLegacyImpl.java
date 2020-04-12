b7024838972f0f2a655ad96dbeebea24d849b974
dmitriy.tseyler <dmitriy.tseyler@jetbrains.com>
2020-04-12 22:33:01 +0300

    [fleet] fix maven to use vfs instead of java.io
    GitOrigin-RevId: 70412e22145589eeb151348b45ddf63f0602ee94

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/importing/MavenRootModelAdapterLegacyImpl.java
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/importing/MavenRootModelAdapterLegacyImpl.java
@@ -12,6 +12,7 @@ import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.Ref;
 import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.vfs.JarFileSystem;
+import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFileManager;
 import com.intellij.pom.java.LanguageLevel;
@@ -31,6 +32,7 @@ import org.jetbrains.jps.model.java.JpsJavaExtensionService;
 import org.jetbrains.jps.model.module.JpsModuleSourceRootType;
 
 import java.io.File;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Set;
 
@@ -190,7 +192,7 @@ public class MavenRootModelAdapterLegacyImpl implements MavenRootModelAdapterInt
   }
 
   private boolean exists(String path) {
-    return new File(toPath(path).getPath()).exists();
+    return VfsUtil.findFile(Paths.get(toPath(path).getPath()), true) != null;
   }
 
   @Override
