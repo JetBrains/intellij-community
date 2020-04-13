JetBrains/kotlin

IDEA-235182 Javadoc Rendered View Cuts Off Text
improve text wrapping: wrap at dot, if possible, not at arbitrary character

intellij-monorepo: 20f30f02c818f4f90dbca0a6afa5a1816a16b732

intellij-community/platform/lang-impl/src/com/intellij/codeInsight/documentation/render/DocRenderPassFactory.java
@@ -26,11 +26,15 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DocRenderPassFactory implements TextEditorHighlightingPassFactoryRegistrar, TextEditorHighlightingPassFactory {
   private static final Logger LOG = Logger.getInstance(DocRenderPassFactory.class);
   private static final Key<Long> MODIFICATION_STAMP = Key.create("doc.render.modification.stamp");
   private static final Key<Boolean> ICONS_ENABLED = Key.create("doc.render.icons.enabled");
+  private static final Pattern END_OF_BODY_PATTERN = Pattern.compile("</body>|</html>");
+  private static final char ZERO_WIDTH_SPACE = '\u200b';
 
   @Override
   public void registerHighlightingPassFactory(@NotNull TextEditorHighlightingPassRegistrar registrar, @NotNull Project project) {
@@ -103,7 +107,16 @@ public class DocRenderPassFactory implements TextEditorHighlightingPassFactoryRe
   }
 
   private static String preProcess(String text) {
-    return DocumentationComponent.addExternalLinksIcon(text);
+    String result = DocumentationComponent.addExternalLinksIcon(text);
+    // add zero width space to document's body, this makes JEditorPane wrap text not just at spaces, but e.g. on dots
+    Matcher matcher = END_OF_BODY_PATTERN.matcher(result);
+    if (matcher.find()) {
+      int endOfBody = matcher.start();
+      return result.substring(0, endOfBody) + ZERO_WIDTH_SPACE + result.substring(endOfBody);
+    }
+    else {
+      return result + ZERO_WIDTH_SPACE;
+    }
   }
 
   public static void applyItemsToRender(@NotNull Editor editor,

JetBrains/intellij-community

(cherry picked from commit 20f30f02c818f4f90dbca0a6afa5a1816a16b732)(IDEA-CR-61315)
GitOrigin-RevId: bca7676f4ff57621716b114c43ce91d464245e78

platform/lang-impl/src/com/intellij/codeInsight/documentation/render/DocRenderPassFactory.java
@@ -25,11 +25,15 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DocRenderPassFactory implements TextEditorHighlightingPassFactoryRegistrar, TextEditorHighlightingPassFactory {
   private static final Logger LOG = Logger.getInstance(DocRenderPassFactory.class);
   private static final Key<Long> MODIFICATION_STAMP = Key.create("doc.render.modification.stamp");
   private static final Key<Boolean> ICONS_ENABLED = Key.create("doc.render.icons.enabled");
+  private static final Pattern END_OF_BODY_PATTERN = Pattern.compile("</body>|</html>");
+  private static final char ZERO_WIDTH_SPACE = '\u200b';
 
   @Override
   public void registerHighlightingPassFactory(@NotNull TextEditorHighlightingPassRegistrar registrar, @NotNull Project project) {
@@ -93,11 +97,23 @@ public class DocRenderPassFactory implements TextEditorHighlightingPassFactoryRe
           text = DocumentationManager.getProviderFromElement(owner).generateRenderedDoc(owner);
         }
       }
-      return text == null ? CodeInsightBundle.message("doc.render.not.available.text") : text;
+      return text == null ? CodeInsightBundle.message("doc.render.not.available.text") : preProcess(text);
     }
     catch (IndexNotReadyException e) {
       LOG.warn(e);
       return CodeInsightBundle.message("doc.render.dumb.mode.text");
+    }
+  }
+
+  private static String preProcess(String text) {
+    // add zero width space to document's body, this makes JEditorPane wrap text not just at spaces, but e.g. on dots
+    Matcher matcher = END_OF_BODY_PATTERN.matcher(text);
+    if (matcher.find()) {
+      int endOfBody = matcher.start();
+      return text.substring(0, endOfBody) + ZERO_WIDTH_SPACE + text.substring(endOfBody);
+    }
+    else {
+      return text + ZERO_WIDTH_SPACE;
     }
   }
 