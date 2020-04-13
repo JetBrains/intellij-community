288f39f4d4cc443773b698ce4e587a62034c15a4
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2020-04-13 10:59:30 +0700

    SerializableCtorInspection: do not warn if annotation is not resolved
    GitOrigin-RevId: 4fb96a6f47e7c6220f0b84981ee089cb23073723

--- plugins/devkit/devkit-core/src/inspections/internal/SerializableCtorInspection.java
+++ plugins/devkit/devkit-core/src/inspections/internal/SerializableCtorInspection.java
@@ -14,9 +14,13 @@ import org.jetbrains.idea.devkit.inspections.DevKitInspectionBase;
 import java.util.Objects;
 
 public class SerializableCtorInspection extends DevKitInspectionBase {
+  private static final String PROPERTY_MAPPING_ANNOTATION = "com.intellij.serialization.PropertyMapping";
 
   @Override
   protected PsiElementVisitor buildInternalVisitor(@NotNull ProblemsHolder holder, boolean isOnTheFly) {
+    if (JavaPsiFacade.getInstance(holder.getProject()).findClass(PROPERTY_MAPPING_ANNOTATION, holder.getFile().getResolveScope()) == null) {
+      return PsiElementVisitor.EMPTY_VISITOR;
+    }
     return new JavaElementVisitor() {
       @Override
       public void visitClass(PsiClass aClass) {
@@ -26,8 +30,7 @@ public class SerializableCtorInspection extends DevKitInspectionBase {
           return;
         PsiMethod[] constructors = aClass.getConstructors();
         for (PsiMethod constructor : constructors) {
-          String fqn = "com.intellij.serialization.PropertyMapping";
-          if (constructor.getNameIdentifier() != null && constructor.getAnnotation(fqn) == null) {
+          if (constructor.getNameIdentifier() != null && constructor.getAnnotation(PROPERTY_MAPPING_ANNOTATION) == null) {
             StringBuilder builder = new StringBuilder("@PropertyMapping({");
             JvmParameter[] parameters = constructor.getParameters();
             for (int i = 0; i < parameters.length; i++) {
@@ -44,7 +47,7 @@ public class SerializableCtorInspection extends DevKitInspectionBase {
             PsiAnnotation annotation = JavaPsiFacade.getElementFactory(aClass.getProject())
               .createAnnotationFromText(builder.append("})").toString(), aClass);
             holder.registerProblem(constructor.getNameIdentifier(), DevKitBundle.message("inspection.serializable.constructor.message"),
-                                   new AddAnnotationPsiFix(fqn, constructor, annotation.getParameterList().getAttributes()));
+                                   new AddAnnotationPsiFix(PROPERTY_MAPPING_ANNOTATION, constructor, annotation.getParameterList().getAttributes()));
           }
         }
       }
