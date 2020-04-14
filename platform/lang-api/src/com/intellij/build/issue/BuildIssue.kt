e9c70802baf00d34ce8cba82c73ecedd11f42518
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2020-04-14 15:21:27 +0300

    i18n - added NLS annotattions to localization of build tools events
    i18n - added NLS annotattions to localization of build tools events
    
    GitOrigin-RevId: fd31698504317fe64e67006e2f8962fa040da859

--- platform/lang-api/src/com/intellij/build/issue/BuildIssue.kt
+++ platform/lang-api/src/com/intellij/build/issue/BuildIssue.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.build.issue
 
+import com.intellij.build.events.BuildEventsNls
 import com.intellij.openapi.project.Project
 import com.intellij.pom.Navigatable
 import org.jetbrains.annotations.ApiStatus
@@ -10,8 +11,8 @@ import org.jetbrains.annotations.ApiStatus
  */
 @ApiStatus.Experimental
 interface BuildIssue {
-  val title: String
-  val description: String
+  val title: @BuildEventsNls.Title String
+  val description: @BuildEventsNls.Description String
   val quickFixes: List<BuildIssueQuickFix>
   fun getNavigatable(project: Project): Navigatable?
 }
