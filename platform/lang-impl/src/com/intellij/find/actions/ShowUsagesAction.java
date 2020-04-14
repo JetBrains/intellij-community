24fadc811cf43e127d7e29b46476adfa21e91c0c
Dmitry Jemerov <yole@jetbrains.com>
2020-04-14 11:51:26 +0200

    Change FUS reporting for show usages popup settings (IDEA-237275)
    (cherry picked from commit 2f6c255288bb1bc4200704caa3516eca7243c4eb)
    
    GitOrigin-RevId: 9350161bf076901feba9fe796da1b876e6f42511

--- platform/lang-impl/src/com/intellij/find/actions/ShowUsagesAction.java
+++ platform/lang-impl/src/com/intellij/find/actions/ShowUsagesAction.java
@@ -15,7 +15,8 @@ import com.intellij.icons.AllIcons;
 import com.intellij.ide.DataManager;
 import com.intellij.ide.IdeBundle;
 import com.intellij.ide.util.gotoByName.ModelDiff;
-import com.intellij.internal.statistic.service.fus.collectors.FUCounterUsageLogger;
+import com.intellij.internal.statistic.service.fus.collectors.UIEventId;
+import com.intellij.internal.statistic.service.fus.collectors.UIEventLogger;
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ReadAction;
@@ -498,7 +499,7 @@ public class ShowUsagesAction extends AnAction implements PopupAction, HintManag
   }
 
   private static void showDialog(@NotNull FindUsagesHandlerBase handler, @NotNull Consumer<@NotNull FindUsagesOptions> optionsConsumer) {
-    FUCounterUsageLogger.getInstance().logEvent("toolbar", "ShowUsagesPopup.showSettings");
+    UIEventLogger.logUIEvent(UIEventId.ShowUsagesPopupShowSettings);
     AbstractFindUsagesDialog dialog;
     if (handler instanceof FindUsagesHandlerUi) {
       dialog = ((FindUsagesHandlerUi)handler).getFindUsagesDialog(false, false, false);
