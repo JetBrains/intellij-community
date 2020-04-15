e04e0e1f014e6eea4ea18f69260a4b6d938b7eba
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2020-04-15 15:17:14 +0300

    IDEA-CR-61498: [groovy] IDEA-231867 use single-quoted strings in @SuppressWarnings
    (cherry picked from commit 12d7decad0e8351fe22cfd8497b5849532af60bd)
    
    GitOrigin-RevId: 830dfd0b3dc8fb174ffa069c428e2f4d38e7cd31

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GrUnusedDefTest.groovy
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GrUnusedDefTest.groovy
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.lang.highlighting
 
 import com.intellij.codeInspection.InspectionProfileEntry
@@ -147,14 +147,14 @@ class Aaaa {}
 
   void 'test suppress with "unused"'() {
     testHighlighting '''\
-class <caret><warning descr="Class Aaaa is unused">Aaaa</warning> {} 
+class <caret><warning descr="Class Aaaa is unused">Aaaa</warning> {}
 '''
     def action = fixture.findSingleIntention 'Suppress'
     fixture.launchAction(action)
 
     fixture.checkResult '''\
-@SuppressWarnings("unused")
-class <caret>Aaaa {} 
+@SuppressWarnings('unused')
+class <caret>Aaaa {}
 '''
   }
 }
