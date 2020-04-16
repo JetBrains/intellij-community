639a86e76e9bd5a459d938c97375e6bba8aa1ddf
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-04-16 20:29:15 +0200

    no need to use HeavyProcessLatch for services
    GitOrigin-RevId: eaa89f6607c187f1545a2f355e05655a5d99e0c7

--- platform/service-container/src/com/intellij/serviceContainer/ServiceComponentAdapter.kt
+++ platform/service-container/src/com/intellij/serviceContainer/ServiceComponentAdapter.kt
@@ -9,7 +9,6 @@ import com.intellij.openapi.extensions.PluginDescriptor
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.util.Disposer
-import com.intellij.util.io.storage.HeavyProcessLatch
 
 internal class ServiceComponentAdapter(val descriptor: ServiceDescriptor,
                                        pluginDescriptor: PluginDescriptor,
@@ -34,19 +33,16 @@ internal class ServiceComponentAdapter(val descriptor: ServiceDescriptor,
       }
     }
 
-    // heavy to prevent storages from flushing and blocking FS
-    HeavyProcessLatch.INSTANCE.processStarted("Creating service $implementationClassName", HeavyProcessLatch.Type.Paused).use {
-      if (indicator == null) {
-        return createAndInitialize(componentManager, implementationClass)
-      }
-
-      // don't use here computeInNonCancelableSection - it is kotlin and no need of such awkward and stack-trace unfriendly methods
-      var instance: T? = null
-      ProgressManager.getInstance().executeNonCancelableSection {
-        instance = createAndInitialize(componentManager, implementationClass)
-      }
-      return instance!!
+    if (indicator == null) {
+      return createAndInitialize(componentManager, implementationClass)
     }
+
+    // don't use here computeInNonCancelableSection - it is kotlin and no need of such awkward and stack-trace unfriendly methods
+    var instance: T? = null
+    ProgressManager.getInstance().executeNonCancelableSection {
+      instance = createAndInitialize(componentManager, implementationClass)
+    }
+    return instance!!
   }
 
   private fun <T : Any> createAndInitialize(componentManager: ComponentManagerImpl, implementationClass: Class<T>): T {
