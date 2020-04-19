dc2493f71d190e28fb14e57c5b3367a54be1514c
Dmitriy.Panov <Dmitriy.Panov@jetbrains.com>
2020-04-19 21:46:40 +0300

    IJI-268 android tools-base usages are removed
    GitOrigin-RevId: e8817f8cf998159c5ede5d7d036ba6f910468f44

--- getPlugins.bat
+++ getPlugins.bat
@@ -1,2 +1,1 @@
-call git clone git://git.jetbrains.org/idea/android.git android
-call git clone git://git.jetbrains.org/idea/adt-tools-base.git android/tools-base
+call git clone git@github.com:JetBrains/android.git android
