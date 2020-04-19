f0a246a63f80802d5766b092dd6312400417b1f1
Alexander Koshevoy <Alexander.Koshevoy@jetbrains.com>
2020-04-19 16:42:50 +0300

    IDEA-CR-61657: PY-40431 Extract messages from TestsUIUtil to TestRunnerBundle
    GitOrigin-RevId: a5ac0aac006891e50443839bc7d961061d735fce

---
+++ platform/testRunner/src/com/intellij/execution/testframework/TestRunnerBundle.java
@@ -0,0 +1,28 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.execution.testframework;
+
+import com.intellij.DynamicBundle;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.PropertyKey;
+
+import java.util.function.Supplier;
+
+public class TestRunnerBundle extends DynamicBundle {
+  @NonNls private static final String BUNDLE = "messages.TestRunnerBundle";
+  private static final TestRunnerBundle INSTANCE = new TestRunnerBundle();
+
+  private TestRunnerBundle() {
+    super(BUNDLE);
+  }
+
+  @NotNull
+  public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, Object @NotNull ... params) {
+    return INSTANCE.getMessage(key, params);
+  }
+
+  @NotNull
+  public static Supplier<String> messagePointer(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, Object @NotNull ... params) {
+    return INSTANCE.getLazyMessage(key, params);
+  }
+}
