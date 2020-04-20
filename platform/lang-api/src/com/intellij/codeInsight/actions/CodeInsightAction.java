fc0205a4b29577509f6847e05a7000d49c3989a6
Yann Cébron <yann.cebron@jetbrains.com>
2020-04-20 16:02:34 +0200

    CodeInsightAction: javadoc
    GitOrigin-RevId: 9f24e4467d3e424ac345cce26462d604992694d9

--- platform/lang-api/src/com/intellij/codeInsight/actions/CodeInsightAction.java
+++ platform/lang-api/src/com/intellij/codeInsight/actions/CodeInsightAction.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.codeInsight.actions;
 
@@ -19,6 +19,8 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
+ * Use {@link MultiCaretCodeInsightAction} for supporting multiple carets.
+ *
  * @author Dmitry Avdeev
  */
 public abstract class CodeInsightAction extends AnAction implements UpdateInBackground {
