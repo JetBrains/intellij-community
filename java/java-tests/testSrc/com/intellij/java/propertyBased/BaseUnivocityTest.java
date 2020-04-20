2c36f0dddbf2972ffa96f35ad11fce3812fb4b9b
Peter Gromov <peter@jetbrains.com>
2020-04-20 21:27:52 +0200

    univocity tests: don't use highest language level (14) which can't be compiled by internal JDK
    GitOrigin-RevId: 397991ade008fc8c854fff97c655e5996dc4b014

--- java/java-tests/testSrc/com/intellij/java/propertyBased/BaseUnivocityTest.java
+++ java/java-tests/testSrc/com/intellij/java/propertyBased/BaseUnivocityTest.java
@@ -2,11 +2,32 @@
 package com.intellij.java.propertyBased;
 
 
+import com.intellij.compiler.CompilerTestUtil;
 import com.intellij.openapi.application.PathManager;
+import com.intellij.openapi.application.WriteAction;
+import com.intellij.openapi.projectRoots.ProjectJdkTable;
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.openapi.projectRoots.impl.JavaAwareProjectJdkTableImpl;
+import com.intellij.openapi.roots.LanguageLevelProjectExtension;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.pom.java.LanguageLevel;
 
 import java.io.File;
 
 public abstract class BaseUnivocityTest extends AbstractApplyAndRevertTestCase {
+
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    Sdk jdk = JavaAwareProjectJdkTableImpl.getInstanceEx().getInternalJdk();
+    WriteAction.run(() -> {
+      ProjectJdkTable.getInstance().addJdk(jdk, getTestRootDisposable());
+      ProjectRootManager.getInstance(myProject).setProjectSdk(jdk);
+      LanguageLevelProjectExtension.getInstance(myProject).setLanguageLevel(LanguageLevel.JDK_1_7);
+    });
+    CompilerTestUtil.saveApplicationSettings();
+  }
+
   @Override
   protected String getTestDataPath() {
     File file = new File(PathManager.getHomePath(), "univocity-parsers");
