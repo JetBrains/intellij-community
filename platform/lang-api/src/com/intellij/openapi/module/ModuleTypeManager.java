a810473dd33ede66e8812ede65d460ea399e7f60
nik <Nikolay.Chashnikov@jetbrains.com>
2020-04-20 16:16:18 +0300

    [platform] make moduleType extension dynamic (IDEA-235291)
    Module type is stored as a string in Module instance so we shouldn't change it if ModuleType is loaded or unloaded. Other usages of ModuleType are local to some dialogs (e.g. New Project Wizard) so they shouldn't prevent unloading as well.
    
    GitOrigin-RevId: 01db8800dd454f8ceea13e5423961fa5ec1c511c

--- platform/lang-api/src/com/intellij/openapi/module/ModuleTypeManager.java
+++ platform/lang-api/src/com/intellij/openapi/module/ModuleTypeManager.java
@@ -2,6 +2,7 @@
 package com.intellij.openapi.module;
 
 import com.intellij.openapi.components.ServiceManager;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
 
 public abstract class ModuleTypeManager {
@@ -9,7 +10,15 @@ public abstract class ModuleTypeManager {
     return ServiceManager.getService(ModuleTypeManager.class);
   }
 
+  /**
+   * This method is intended for internal use only. Use {@code com.intellij.moduleType} extension point to register custom module type in
+   * a plugin.
+   */
+  @ApiStatus.Internal
   public abstract void registerModuleType(ModuleType<?> type);
+
+  @ApiStatus.Internal
+  public abstract void unregisterModuleType(ModuleType<?> type);
 
   public abstract ModuleType<?>[] getRegisteredTypes();
 
