114c81ab9a5714ffc01ef0b9bddd3a43d28f2f63
Elena Shaverdova <elena.shaverdova@jetbrains.com>
2020-04-20 22:08:54 +0200

    IDEA-CR-60959 Prefix for i18n for IDEA-CR-60959 Fix balloon border and position for dumb actions notification in IDEA-227118
    (cherry picked from commit 3378037416d2de48bb4f4df7623454e8f102890c)
    (cherry picked from commit 4ebeb1be7d7dd85fa87126bdbd2c65f7b20a288c)
    
    GitOrigin-RevId: 4ee07d9e961757151d2be3e2fa7b78ecdb53df53

--- platform/platform-impl/src/com/intellij/openapi/keymap/impl/IdeKeyEventDispatcher.java
+++ platform/platform-impl/src/com/intellij/openapi/keymap/impl/IdeKeyEventDispatcher.java
@@ -721,20 +721,20 @@ public final class IdeKeyEventDispatcher implements Disposable {
       }
     }
     if (actionNames.isEmpty()) {
-      return getUnavailableMessage(IdeBundle.message("this.action"), false);
+      return getUnavailableMessage(IdeBundle.message("dumb.balloon.this.action"), false);
     }
     else if (actionNames.size() == 1) {
       return getUnavailableMessage("'" + actionNames.get(0) + "'", false);
     }
     else {
-      return getUnavailableMessage(IdeBundle.message("none.of.the.following.actions"), true) +
+      return getUnavailableMessage(IdeBundle.message("dumb.balloon.none.of.the.following.actions"), true) +
              ": " + StringUtil.join(actionNames, ", ");
     }
   }
 
   public static @NotNull @Nls String getUnavailableMessage(@NotNull @Nls String action, boolean plural) {
-    return plural ? IdeBundle.message("0.are.not.available.while.indexing", action) :
-           IdeBundle.message("0.is.not.available.while.indexing", action);
+    return plural ? IdeBundle.message("dumb.balloon.0.are.not.available.while.indexing", action) :
+           IdeBundle.message("dumb.balloon.0.is.not.available.while.indexing", action);
   }
 
   private static DumbModeWarningListener dumbModeWarningListener  = null;
