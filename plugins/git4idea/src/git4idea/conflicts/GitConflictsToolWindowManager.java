4e328899b01bc47a78eeb466d36cd3a7c718c9f4
Aleksey Pivovarov <AMPivovarov@gmail.com>
2020-04-21 16:56:18 +0300

    git: update conflicts toolwindow on repositories changes
    GitOrigin-RevId: a51739372be3ae3a00b9992e1a1d03fe8bb9ce96

--- plugins/git4idea/src/git4idea/conflicts/GitConflictsToolWindowManager.java
+++ plugins/git4idea/src/git4idea/conflicts/GitConflictsToolWindowManager.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package git4idea.conflicts;
 
+import com.intellij.dvcs.repo.VcsRepositoryMappingListener;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.Service;
 import com.intellij.openapi.project.Project;
@@ -66,6 +67,22 @@ public final class GitConflictsToolWindowManager {
 
       Project project = repository.getProject();
       GitConflictsToolWindowManager service = project.getService(GitConflictsToolWindowManager.class);
+      service.scheduleUpdate();
+    }
+  }
+
+  public static class MyRepositoryListener implements VcsRepositoryMappingListener {
+    private final Project myProject;
+
+    public MyRepositoryListener(@NotNull Project project) {
+      myProject = project;
+    }
+
+    @Override
+    public void mappingChanged() {
+      if (!Registry.is("git.merge.conflicts.toolwindow")) return;
+
+      GitConflictsToolWindowManager service = myProject.getService(GitConflictsToolWindowManager.class);
       service.scheduleUpdate();
     }
   }
