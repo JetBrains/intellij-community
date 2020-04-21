8339715a3b3cbe3b46041182b9433238bda6889c
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2020-04-21 18:05:36 +0300

    Make DjangoAwarePythonConsoleRunnerFactory an application service (PY-41923)
    GitOrigin-RevId: af84cc2362992afd34129cc56443cd5ff8c3eed1

--- python/src/com/jetbrains/python/console/PythonConsoleRunnerFactory.java
+++ python/src/com/jetbrains/python/console/PythonConsoleRunnerFactory.java
@@ -1,7 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.python.console;
 
-import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
 import com.jetbrains.python.run.PythonRunConfiguration;
@@ -11,7 +11,7 @@ import org.jetbrains.annotations.Nullable;
 public abstract class PythonConsoleRunnerFactory {
   @NotNull
   public static PythonConsoleRunnerFactory getInstance() {
-    return ApplicationManager.getApplication().getComponent(PythonConsoleRunnerFactory.class);
+    return ServiceManager.getService(PythonConsoleRunnerFactory.class);
   }
 
   @NotNull
