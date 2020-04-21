dd5ce8946c31b8bbf6d82ff82e42c670c852e2d8
Nikita Eshkeev <nikita.eshkeev@jetbrains.com>
2020-04-21 16:54:21 +0300

    [codeInsight] IDEA-238023 Call to substring is redundant shown twice in popup
    Signed-off-by: Nikita Eshkeev <nikita.eshkeev@jetbrains.com>
    
    GitOrigin-RevId: 30593b19d067250f769f802f902cf9541d89a501

---
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/redundancy/redundant_string_operation/SubstringInAppend.java
@@ -0,0 +1,16 @@
+
+public class SubstringInAppend {
+  static {
+    StringBuilder sb = new StringBuilder();
+    sb.append("xxx".<warning descr="Call to 'substring' is redundant">substring</warning>(3, 5));
+  }
+
+  SubstringInAppend() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("xxx".<warning descr="Call to 'substring' is redundant">substring</warning>(3, 5));
+  }
+
+  public void doWork(StringBuilder sb) {
+    sb.append("xxx".<warning descr="Call to 'substring' is redundant">substring</warning>(3, 5));
+  }
+}
