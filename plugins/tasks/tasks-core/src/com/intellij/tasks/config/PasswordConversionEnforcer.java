e152555b6707cb96e93201bdfb174572a7a572c4
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-04-22 20:09:31 +0200

    IDEA-CR-61852 preloadingActivity is not intended for such task
    GitOrigin-RevId: 551fb58dc2522f06c15ac4594a2a2d9a4bb28fc4

--- plugins/tasks/tasks-core/src/com/intellij/tasks/config/PasswordConversionEnforcer.java
+++ plugins/tasks/tasks-core/src/com/intellij/tasks/config/PasswordConversionEnforcer.java
@@ -1,16 +1,21 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.tasks.config;
 
+import com.intellij.configurationStore.SettingsSavingComponentJavaAdapter;
 import com.intellij.ide.util.PropertiesComponent;
-import com.intellij.openapi.application.PreloadingActivity;
-import com.intellij.openapi.progress.ProgressIndicator;
-import org.jetbrains.annotations.NotNull;
 
-final class PasswordConversionEnforcer extends PreloadingActivity {
+final class PasswordConversionEnforcer implements SettingsSavingComponentJavaAdapter {
   private static final String ENFORCED = "tasks.pass.word.conversion.enforced";
+  private boolean isDone;
 
   @Override
-  public void preload(@NotNull ProgressIndicator indicator) {
+  public void doSave() {
+    if (isDone) {
+      return;
+    }
+
+    isDone = true;
+
     PropertiesComponent propertyComponent = PropertiesComponent.getInstance();
     if (!propertyComponent.isValueSet(ENFORCED)) {
       RecentTaskRepositories.getInstance();
