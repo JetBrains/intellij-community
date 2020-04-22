9b85decae4d7e739ddcc7d780f8a3b2770a9cd04
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2020-04-22 15:41:25 +0200

    HtmlLanguageCodeStyleConfigurable depends on HtmlCodeStylePanelExtension.EP_NAME (IDEA-CR-61811)
    GitOrigin-RevId: e1373ee3f0650d23150de0b836ffef891500bd60

--- xml/impl/src/com/intellij/application/options/HtmlLanguageCodeStyleSettingsProvider.java
+++ xml/impl/src/com/intellij/application/options/HtmlLanguageCodeStyleSettingsProvider.java
@@ -17,10 +17,15 @@ package com.intellij.application.options;
 
 import com.intellij.lang.Language;
 import com.intellij.lang.html.HTMLLanguage;
+import com.intellij.openapi.extensions.BaseExtensionPointName;
+import com.intellij.openapi.options.Configurable;
 import com.intellij.psi.codeStyle.*;
 import com.intellij.psi.formatter.xml.HtmlCodeStyleSettings;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+import java.util.Collections;
 
 /**
  * @author Rustam Vishnyakov
@@ -55,17 +60,7 @@ public class HtmlLanguageCodeStyleSettingsProvider extends LanguageCodeStyleSett
   @Override
   public @NotNull CodeStyleConfigurable createConfigurable(@NotNull CodeStyleSettings baseSettings,
                                                            @NotNull CodeStyleSettings modelSettings) {
-    return new CodeStyleAbstractConfigurable(baseSettings, modelSettings, getDisplayName()) {
-      @Override
-      protected CodeStyleAbstractPanel createPanel(final CodeStyleSettings settings) {
-        return new HtmlCodeStyleMainPanel(getCurrentSettings(), settings);
-      }
-
-      @Override
-      public String getHelpTopic() {
-        return "reference.settingsdialog.IDE.globalcodestyle.html";
-      }
-    };
+    return new HtmlLanguageCodeStyleConfigurable(baseSettings, modelSettings, getDisplayName());
   }
 
   @Override
@@ -77,5 +72,31 @@ public class HtmlLanguageCodeStyleSettingsProvider extends LanguageCodeStyleSett
   @Override
   public IndentOptionsEditor getIndentOptionsEditor() {
     return new HtmlIndentOptionsEditor();
+  }
+
+  private static class HtmlLanguageCodeStyleConfigurable extends CodeStyleAbstractConfigurable implements Configurable.WithEpDependencies {
+    private static final Collection<BaseExtensionPointName<?>> DEPENDENCIES =
+      Collections.singletonList(HtmlCodeStylePanelExtension.EP_NAME);
+
+    private HtmlLanguageCodeStyleConfigurable(@NotNull CodeStyleSettings baseSettings,
+                                              @NotNull CodeStyleSettings modelSettings,
+                                              @NotNull String displayName) {
+      super(baseSettings, modelSettings, displayName);
+    }
+
+    @Override
+    protected CodeStyleAbstractPanel createPanel(final CodeStyleSettings settings) {
+      return new HtmlCodeStyleMainPanel(getCurrentSettings(), settings);
+    }
+
+    @Override
+    public String getHelpTopic() {
+      return "reference.settingsdialog.IDE.globalcodestyle.html";
+    }
+
+    @Override
+    public @NotNull Collection<BaseExtensionPointName<?>> getDependencies() {
+      return DEPENDENCIES;
+    }
   }
 }
