46c7b4a7ff500118ad807b31ee951e7469a3ed57
Vassiliy.Kudryashov <vassiliy.kudryashov@jetbrains.com>
2020-04-22 12:40:55 +0300

    IDEA-230986 Z-Order for dialog validation error
    GitOrigin-RevId: e890ea4ee6fa90ac799d4ff3be3afee2e5ae0a3c

--- platform/execution-impl/src/com/intellij/execution/impl/SingleConfigurationConfigurable.java
+++ platform/execution-impl/src/com/intellij/execution/impl/SingleConfigurationConfigurable.java
@@ -2,16 +2,8 @@
 package com.intellij.execution.impl;
 
 import com.intellij.configurationStore.Scheme_implKt;
-import com.intellij.execution.ExecutionBundle;
-import com.intellij.execution.Executor;
-import com.intellij.execution.RunManager;
-import com.intellij.execution.RunOnTargetComboBox;
-import com.intellij.execution.RunnerAndConfigurationSettings;
-import com.intellij.execution.configurations.ConfigurationPerRunnerSettings;
-import com.intellij.execution.configurations.LocatableConfigurationBase;
-import com.intellij.execution.configurations.RunConfiguration;
-import com.intellij.execution.configurations.RunnerSettings;
-import com.intellij.execution.configurations.RuntimeConfigurationException;
+import com.intellij.execution.*;
+import com.intellij.execution.configurations.*;
 import com.intellij.execution.runners.ProgramRunner;
 import com.intellij.execution.target.LanguageRuntimeType;
 import com.intellij.execution.target.TargetEnvironmentAwareRunProfile;
@@ -20,12 +12,7 @@ import com.intellij.execution.target.TargetEnvironmentsManager;
 import com.intellij.icons.AllIcons;
 import com.intellij.ide.DataManager;
 import com.intellij.openapi.Disposable;
-import com.intellij.openapi.actionSystem.ActionPlaces;
-import com.intellij.openapi.actionSystem.ActionToolbar;
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DataKey;
-import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.actionSystem.impl.ActionButton;
 import com.intellij.openapi.application.Experiments;
 import com.intellij.openapi.diagnostic.Logger;
@@ -64,9 +51,14 @@ import com.intellij.util.ui.JBUI;
 import com.intellij.util.ui.UI;
 import com.intellij.util.ui.UIUtil;
 import gnu.trove.THashSet;
-import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import org.jetbrains.annotations.*;
+
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -74,22 +66,6 @@ import java.util.Collection;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
-import javax.swing.Icon;
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JSeparator;
-import javax.swing.JTextField;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.PlainDocument;
-import org.jetbrains.annotations.Contract;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.annotations.SystemIndependent;
 
 public final class SingleConfigurationConfigurable<Config extends RunConfiguration> extends BaseRCSettingsConfigurable {
   private static final LayeredIcon GEAR_WITH_DROPDOWN_ICON = new LayeredIcon(AllIcons.General.GearPlain, AllIcons.General.Dropdown);
@@ -589,6 +565,7 @@ public final class SingleConfigurationConfigurable<Config extends RunConfigurati
     private JPanel myRunOnPanelInner;
 
     private Runnable myQuickFix = null;
+    private boolean myWindowResizedOnce = false;
 
     MyValidatableComponent() {
       myNameLabel.setLabelFor(myNameText);
@@ -737,6 +714,12 @@ public final class SingleConfigurationConfigurable<Config extends RunConfigurati
           myQuickFix = quickFix;
         }
         myValidationPanel.setVisible(true);
+        Window window = UIUtil.getWindow(myWholePanel);
+        if (!myWindowResizedOnce && window != null && window.isShowing()) {
+          Dimension size = window.getSize();
+          window.setSize(size.width, size.height + myValidationPanel.getPreferredSize().height);
+          myWindowResizedOnce = true;
+        }
       }
       else {
         mySeparator.setVisible(false);
