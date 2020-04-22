081d26aaddbe5c41c8634e1e0380bfef65ae91df
Svetlana.Zemlyanskaya <Svetlana.Zemlyanskaya@jetbrains.com>
2020-04-22 17:02:26 +0200

    FUS: remove old deprecated API
    GitOrigin-RevId: addf1681436f56942bd30b321976bc079b8556fb

--- platform/platform-tests/testSrc/com/intellij/internal/statistics/StatisticsUtilTest.kt
+++ platform/platform-tests/testSrc/com/intellij/internal/statistics/StatisticsUtilTest.kt
@@ -1,75 +1,44 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.internal.statistics
 
-import com.intellij.internal.statistic.beans.UsageDescriptor
+import com.intellij.internal.statistic.beans.MetricEvent
+import com.intellij.internal.statistic.beans.newMetric
 import com.intellij.internal.statistic.eventLog.EventLogConfiguration
+import com.intellij.internal.statistic.utils.StatisticsUtil
 import com.intellij.internal.statistic.utils.StatisticsUtil.getNextPowerOfTwo
-import com.intellij.internal.statistic.utils.getCountingStepName
-import com.intellij.internal.statistic.utils.getCountingUsage
+import com.intellij.testFramework.LightPlatformTestCase
 import org.junit.Test
 import kotlin.test.assertEquals
 
-class StatisticsUtilTest {
+@Suppress("SameParameterValue")
+class StatisticsUtilTest : LightPlatformTestCase() {
   @Test
   fun test_counting_usage() {
     val steps = listOf(0, 1, 2, 10, 1000, 10 * 1000, 1000 * 1000)
-    assertCountingUsage("test.value.count.0", 0, steps)
-    assertCountingUsage("test.value.count.1", 1, steps)
-    assertCountingUsage("test.value.count.2+", 2, steps)
-    assertCountingUsage("test.value.count.2+", 3, steps)
-    assertCountingUsage("test.value.count.2+", 9, steps)
-    assertCountingUsage("test.value.count.10+", 10, steps)
-    assertCountingUsage("test.value.count.10+", 500, steps)
-    assertCountingUsage("test.value.count.1K+", 1000, steps)
-    assertCountingUsage("test.value.count.10K+", 200 * 1000, steps)
-    assertCountingUsage("test.value.count.10K+", 999999, steps)
-    assertCountingUsage("test.value.count.1M+", 1000 * 1000, steps)
-    assertCountingUsage("test.value.count.1M+", 2000 * 1000, steps)
-  }
-
-  @Test
-  fun test_infinite_counting_usage() {
-    assertInfiniteCountingUsage("test.value.count.<0", -1000)
-    assertInfiniteCountingUsage("test.value.count.<0", -1)
-    assertInfiniteCountingUsage("test.value.count.0", 0)
-    assertInfiniteCountingUsage("test.value.count.1", 1)
-    assertInfiniteCountingUsage("test.value.count.2", 2)
-    assertInfiniteCountingUsage("test.value.count.3+", 3)
-    assertInfiniteCountingUsage("test.value.count.3+", 4)
-    assertInfiniteCountingUsage("test.value.count.5+", 5)
-    assertInfiniteCountingUsage("test.value.count.5+", 6)
-    assertInfiniteCountingUsage("test.value.count.5+", 7)
-    assertInfiniteCountingUsage("test.value.count.5+", 9)
-    assertInfiniteCountingUsage("test.value.count.10+", 10)
-    assertInfiniteCountingUsage("test.value.count.10+", 14)
-    assertInfiniteCountingUsage("test.value.count.15+", 15)
-    assertInfiniteCountingUsage("test.value.count.15+", 29)
-    assertInfiniteCountingUsage("test.value.count.30+", 30)
-    assertInfiniteCountingUsage("test.value.count.30+", 49)
-    assertInfiniteCountingUsage("test.value.count.50+", 50)
-    assertInfiniteCountingUsage("test.value.count.50+", 99)
-    assertInfiniteCountingUsage("test.value.count.100+", 100)
-    assertInfiniteCountingUsage("test.value.count.100+", 341)
-    assertInfiniteCountingUsage("test.value.count.1K+", 1000)
-    assertInfiniteCountingUsage("test.value.count.100K+", 200 * 1000)
-    assertInfiniteCountingUsage("test.value.count.500K+", 999999)
-    assertInfiniteCountingUsage("test.value.count.1M+", 1000 * 1000)
-    assertInfiniteCountingUsage("test.value.count.1M+", 2000 * 1000)
-    assertInfiniteCountingUsage("test.value.count.5M+", 6000 * 1000)
-    assertInfiniteCountingUsage("test.value.count.100M+", 2000 * 1000 * 100)
-    assertInfiniteCountingUsage("test.value.count.MANY", 2000 * 1000 * 1000)
+    assertCountingUsage("test.value.count", "0", 0, steps)
+    assertCountingUsage("test.value.count", "1", 1, steps)
+    assertCountingUsage("test.value.count", "2+", 2, steps)
+    assertCountingUsage("test.value.count", "2+", 3, steps)
+    assertCountingUsage("test.value.count", "2+", 9, steps)
+    assertCountingUsage("test.value.count", "10+", 10, steps)
+    assertCountingUsage("test.value.count", "10+", 500, steps)
+    assertCountingUsage("test.value.count", "1K+", 1000, steps)
+    assertCountingUsage("test.value.count", "10K+", 200 * 1000, steps)
+    assertCountingUsage("test.value.count", "10K+", 999999, steps)
+    assertCountingUsage("test.value.count", "1M+", 1000 * 1000, steps)
+    assertCountingUsage("test.value.count", "1M+", 2000 * 1000, steps)
   }
 
   @Test
   fun `test counting usage on empty list`() {
     val emptySteps = listOf<Int>()
-    assertCountingUsage("test.value.count.1", 1, emptySteps)
+    assertCountingUsage("test.value.count", "1", 1, emptySteps)
   }
 
   @Test
   fun `test counting usage if value is less than the first step`() {
     val steps = listOf(1, 5, 10)
-    assertCountingUsage("test.value.count.<1", 0, steps)
+    assertCountingUsage("test.value.count", "<1", 0, steps)
   }
 
   @Test
@@ -96,23 +65,20 @@ class StatisticsUtilTest {
 
   private fun doTestHashing(salt: ByteArray, data: String, expected: String) {
     val actual = EventLogConfiguration.hashSha256(salt, data)
-    assertEquals(expected, actual, "Hashing algorithm was changed for '$data'")
+    assertEquals("Hashing algorithm was changed for '$data'", expected, actual)
   }
 
-  private fun assertCountingUsage(expectedKey: String, actualValue: Int, steps: List<Int>) {
-    assertUsage(expectedKey, 1, getCountingUsage("test.value.count", actualValue, steps), "Incorrect key for value '$actualValue'")
+  private fun assertCountingUsage(expectedKey: String, expectedValue: String, actualValue: Int, steps: List<Int>) {
+    val metric = getCountingUsage("test.value.count", actualValue, steps)
+    assertUsage(expectedKey, expectedValue, metric, "Incorrect key for value '$actualValue'")
   }
 
-  private fun assertInfiniteCountingUsage(expectedKey: String, actualValue: Int) {
-    assertUsage(expectedKey, 1, getCountingUsage("test.value.count", actualValue), "Incorrect key for value '$actualValue'")
+  private fun assertUsage(key: String, value: String, metric: MetricEvent, message: String? = null) {
+    assertEquals(message, key, metric.eventId)
+    assertEquals(message, value, metric.data.build()["value"])
   }
 
-  private fun assertUsage(key: String, value: Int, actualUsage: UsageDescriptor, message: String? = null) {
-    assertEquals(key, actualUsage.key, message)
-    assertEquals(value, actualUsage.value, message)
+  private fun getCountingUsage(key: String, value: Int, steps: List<Int>) : MetricEvent {
+    return newMetric(key, StatisticsUtil.getCountingStepName(value, steps))
   }
-}
-
-private fun getCountingUsage(key: String, value: Int, steps: List<Int>) : UsageDescriptor {
-  return UsageDescriptor("$key." + getCountingStepName(value, steps), 1)
 }
