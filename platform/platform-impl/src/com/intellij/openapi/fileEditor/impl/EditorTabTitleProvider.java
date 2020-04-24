fe1545a319462f5e02ba8b81b93e7a33524edb57
Yann Cébron <yann.cebron@jetbrains.com>
2020-04-24 20:21:19 +0200

    EditorTabTitleProvider: javadoc
    GitOrigin-RevId: b24122d60732089c83bfeebf5dbbae970057fcf9

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/EditorTabTitleProvider.java
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/EditorTabTitleProvider.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.fileEditor.impl;
 
 import com.intellij.openapi.extensions.ExtensionPointName;
@@ -8,6 +8,10 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
+ * Provides custom name/tooltip for editor tab instead of filename/path.
+ * <p>
+ * Implement {@link com.intellij.openapi.project.DumbAware} to be active during indexing.
+ *
  * @author yole
  */
 public interface EditorTabTitleProvider {
