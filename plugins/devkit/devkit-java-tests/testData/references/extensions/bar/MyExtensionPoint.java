85546692bc25040e47733f73db1a5f37ad260781
Yann Cébron <yann.cebron@jetbrains.com>
2020-04-24 16:46:26 +0200

    [devkit] Extension Point Docs: enumerate EP's beanClass properties (IDEA-228390)
    GitOrigin-RevId: cc681134d848128abec2740aa84cb20aa5f92d40

--- plugins/devkit/devkit-java-tests/testData/references/extensions/bar/MyExtensionPoint.java
+++ plugins/devkit/devkit-java-tests/testData/references/extensions/bar/MyExtensionPoint.java
@@ -1,7 +1,18 @@
 package bar;
 
+import com.intellij.openapi.extensions.RequiredElement;
+import com.intellij.util.xmlb.annotations.Attribute;
+import com.intellij.util.xmlb.annotations.Tag;
+
 /**
  * MyExtensionPoint JavaDoc.
  */
-public interface MyExtensionPoint {
+public class MyExtensionPoint {
+
+  @RequiredElement
+  @Attribute
+  public String implementationClass;
+
+  @Tag
+  public Integer intValue;
 }
