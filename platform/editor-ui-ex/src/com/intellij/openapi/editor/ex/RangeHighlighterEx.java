7a210721b5ac23644e50ebbaa8d1f118c609887b
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2020-04-24 09:11:08 +0200

    RangeHighlighterEx#isPersistent - add javadoc
    GitOrigin-RevId: 25950d085344fa83a2525af15b36b0249c93e4f0

--- platform/editor-ui-ex/src/com/intellij/openapi/editor/ex/RangeHighlighterEx.java
+++ platform/editor-ui-ex/src/com/intellij/openapi/editor/ex/RangeHighlighterEx.java
@@ -3,8 +3,10 @@
 package com.intellij.openapi.editor.ex;
 
 import com.intellij.openapi.editor.FoldRegion;
+import com.intellij.openapi.editor.markup.HighlighterTargetArea;
 import com.intellij.openapi.editor.markup.RangeHighlighter;
 import com.intellij.openapi.editor.markup.TextAttributes;
+import com.intellij.util.Consumer;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Comparator;
@@ -33,6 +35,12 @@ public interface RangeHighlighterEx extends RangeHighlighter, RangeMarkerEx {
    */
   boolean isVisibleIfFolded();
 
+  /**
+   * If {@code true}, this highlighter is persistent and is retained between code analyzer runs and IDE restarts.
+   *
+   * @see MarkupModelEx#addPersistentLineHighlighter(int, int, TextAttributes)
+   * @see MarkupModelEx#addRangeHighlighterAndChangeAttributes(int, int, int, TextAttributes, HighlighterTargetArea, boolean, Consumer)
+   */
   default boolean isPersistent() {
     return false;
   }
