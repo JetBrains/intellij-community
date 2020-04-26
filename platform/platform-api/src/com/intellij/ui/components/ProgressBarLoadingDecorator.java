6e5b8c672b27b170664bbb75ccfa50d86bf437ce
Aleksey Pivovarov <AMPivovarov@gmail.com>
2020-04-26 17:31:12 +0300

    ui: extract loading decorator with progress bar
    GitOrigin-RevId: a852b8fb7fe6563f1f49433e5661cd88276bc605

---
+++ platform/platform-api/src/com/intellij/ui/components/ProgressBarLoadingDecorator.java
@@ -0,0 +1,53 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.ui.components;
+
+import com.intellij.openapi.Disposable;
+import com.intellij.openapi.ui.LoadingDecorator;
+import com.intellij.ui.components.panels.NonOpaquePanel;
+import com.intellij.util.ui.AsyncProcessIcon;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class ProgressBarLoadingDecorator extends LoadingDecorator {
+  private final AtomicBoolean loadingStarted = new AtomicBoolean(false);
+  private JProgressBar myProgressBar;
+
+  public ProgressBarLoadingDecorator(@NotNull JPanel contentPanel, @NotNull Disposable disposable, int startDelayMs) {
+    super(contentPanel, disposable, startDelayMs, true);
+  }
+
+  @Override
+  protected NonOpaquePanel customizeLoadingLayer(JPanel parent, JLabel text, AsyncProcessIcon icon) {
+    parent.setLayout(new BorderLayout());
+    NonOpaquePanel result = new NonOpaquePanel();
+    result.setLayout(new BoxLayout(result, BoxLayout.Y_AXIS));
+    myProgressBar = new JProgressBar();
+    myProgressBar.setIndeterminate(true);
+    myProgressBar.putClientProperty("ProgressBar.stripeWidth", 2);
+    myProgressBar.putClientProperty("ProgressBar.flatEnds", Boolean.TRUE);
+    result.add(myProgressBar);
+    parent.add(result, BorderLayout.NORTH);
+    return result;
+  }
+
+  @NotNull
+  public JProgressBar getProgressBar() {
+    return myProgressBar;
+  }
+
+  public void startLoading() {
+    if (loadingStarted.compareAndSet(false, true)) {
+      super.startLoading(false);
+    }
+  }
+
+  @Override
+  public void stopLoading() {
+    if (loadingStarted.compareAndSet(true, false)) {
+      super.stopLoading();
+    }
+  }
+}
