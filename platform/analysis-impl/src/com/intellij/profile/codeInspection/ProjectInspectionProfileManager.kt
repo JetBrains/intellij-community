18df1a01638386708dc003f6f8865636aa35003f
Alexey Kudravtsev <cdr@intellij.com>
2020-04-27 00:08:28 +0300

    restart highlighting on inspection profile change
    GitOrigin-RevId: 112918556a611e9140c13af13539c3ef115b80f9

--- platform/analysis-impl/src/com/intellij/profile/codeInspection/ProjectInspectionProfileManager.kt
+++ platform/analysis-impl/src/com/intellij/profile/codeInspection/ProjectInspectionProfileManager.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.profile.codeInspection
 
+import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
 import com.intellij.codeInspection.ex.InspectionProfileImpl
 import com.intellij.codeInspection.ex.InspectionToolRegistrar
 import com.intellij.configurationStore.*
@@ -201,6 +202,7 @@ open class ProjectInspectionProfileManager(val project: Project) : BaseInspectio
         state.projectProfile = name
       }
     }
+    DaemonCodeAnalyzer.getInstance(project).restart()
   }
 
   @Synchronized
