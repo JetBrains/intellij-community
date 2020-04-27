a777d5cf4f7dd57d8a361c1a7c96c817ffe319b6
Yann Cébron <yann.cebron@jetbrains.com>
2020-04-27 14:43:29 +0200

    [devkit] DOM: extract non-i18n typeName constants
    GitOrigin-RevId: acde9044ff553799cac90d3bdcc64b6eea5cf4fa

---
+++ plugins/devkit/devkit-core/src/dom/DevkitDomPresentationConstants.java
@@ -0,0 +1,15 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.idea.devkit.dom;
+
+import org.jetbrains.annotations.NonNls;
+
+public final class DevkitDomPresentationConstants {
+
+  @NonNls public static final String ACTION = "Action";
+  @NonNls public static final String GROUP = "Group";
+
+  @NonNls public static final String EXTENSION = "Extension";
+  @NonNls public static final String EXTENSION_POINT = "Extension Point";
+
+  @NonNls public static final String PLUGIN = "Plugin";
+}
--- plugins/devkit/devkit-core/src/dom/Extension.java
+++ plugins/devkit/devkit-core/src/dom/Extension.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.devkit.dom;
 
 import com.intellij.ide.plugins.IdeaPluginDescriptorImpl;
@@ -9,7 +9,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.idea.devkit.dom.impl.ExtensionOrderConverter;
 
-@Presentation(typeName = "Extension")
+@Presentation(typeName = DevkitDomPresentationConstants.EXTENSION)
 public interface Extension extends DomElement {
 
   String ID_ATTRIBUTE = "id";
