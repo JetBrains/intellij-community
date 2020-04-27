6b116f8c22223e39da2cf33dda437501331b3181
Yann Cébron <yann.cebron@jetbrains.com>
2020-04-27 11:41:54 +0200

    [devkit] DOM: move PluginDescriptorDomFileSearchScopeProvider to proper package
    GitOrigin-RevId: 5a4814bc9fd37b50bc9eea67671b56ec1e46d5d5

--- plugins/devkit/devkit-core/src/dom/ide/PluginDescriptorDomFileSearchScopeProvider.java
+++ plugins/devkit/devkit-core/src/dom/ide/PluginDescriptorDomFileSearchScopeProvider.java
@@ -1,5 +1,5 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.idea.devkit.dom.impl;
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.idea.devkit.dom.ide;
 
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.DataContext;
