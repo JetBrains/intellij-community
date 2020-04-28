7e6d1548c643139f5f15f59e50f92267226e56e0
Jens Ole Lauridsen <jlauridsen@google.com>
2020-04-28 14:00:48 -0700

    IDEA-239606 Avoid hang on exit in tests
    Bug: 154978922
    Change-Id: Ibc10a997f3a8ed44b29aa1607cc3a1ab59c68ab4

--- platform/lang-impl/src/com/intellij/psi/stubs/StubIndexImpl.java
+++ platform/lang-impl/src/com/intellij/psi/stubs/StubIndexImpl.java
@@ -613,6 +613,8 @@ public final class StubIndexImpl extends StubIndexEx implements PersistentStateC
   }
 
   public void dispose() {
+    if (!myInitialized) return;  // Android Studio: IDEA-239606 Avoid hang on exit in tests
+
     try {
       for (UpdatableIndex<?, ?, ?> index : getAsyncState().myIndices.values()) {
         index.dispose();
