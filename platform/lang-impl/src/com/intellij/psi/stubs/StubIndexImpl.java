9454ded618723df580e682484704409661d095f3
Jens Ole Lauridsen <jlauridsen@google.com>
2020-04-28 12:56:39 -0700

    IDEA-239606 Make StubIndexImpl.dispose() a noop if never used
    Without this there may be tests that will hang on exit, from the
    ProgressIndicatorUtils.awaitWithCheckCanceled call from
    StubIndexImpl.getAsyncState.

--- platform/lang-impl/src/com/intellij/psi/stubs/StubIndexImpl.java
+++ platform/lang-impl/src/com/intellij/psi/stubs/StubIndexImpl.java
@@ -613,6 +613,7 @@ public final class StubIndexImpl extends StubIndexEx implements PersistentStateC
   }
 
   public void dispose() {
+    if (!myInitialized) return;
     try {
       for (UpdatableIndex<?, ?, ?> index : getAsyncState().myIndices.values()) {
         index.dispose();
