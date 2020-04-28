5b63042f7f597ba21f4b3e81d4efefd251587e12
Aleksandr Krasilnikov <aleksandr.krasilnikov@jetbrains.com>
2020-04-28 22:50:15 +0300

    [space plugin] CircletCloneExtension: simplify reading server name
    GitOrigin-RevId: 76d39eeed5f16a891b6b3e2e91fbcc5a5837b61c

--- plugins/space/src/main/kotlin/circlet/vcs/clone/CircletCloneExtension.kt
+++ plugins/space/src/main/kotlin/circlet/vcs/clone/CircletCloneExtension.kt
@@ -1,6 +1,7 @@
 package circlet.vcs.clone
 
 import circlet.components.circletWorkspace
+import circlet.settings.*
 import circlet.ui.cleanupUrl
 import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.project.Project
@@ -21,9 +22,9 @@ class CircletCloneExtension : VcsCloneDialogExtension {
     override fun getName(): String = ProductName
 
     override fun getAdditionalStatusLines(): List<VcsCloneDialogExtensionStatusLine> {
-        // todo: simplify host obtaining
-        val server = circletWorkspace.workspace.value?.client?.server?.let { cleanupUrl(it) }
-        return listOf(VcsCloneDialogExtensionStatusLine.greyText(server ?: "No account"))
+        val server = cleanupUrl(CircletSettings.getInstance().serverSettings.server)
+        val status = if (server.isBlank()) "No account" else server
+        return listOf(VcsCloneDialogExtensionStatusLine.greyText(status))
     }
 
     override fun createMainComponent(project: Project, modalityState: ModalityState): VcsCloneDialogExtensionComponent {
