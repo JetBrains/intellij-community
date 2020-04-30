b6072ac8f0f565a741630396e344277743d050a6
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2020-04-30 13:44:09 +0300

    IDEA-236390 Implement new UI for editing run configuration settings
    remove option tooltip
    
    GitOrigin-RevId: 2e2a149f84a48a16eae75cf3f8c814a29f25c4db

--- platform/platform-api/src/com/intellij/execution/ui/TagButton.java
+++ platform/platform-api/src/com/intellij/execution/ui/TagButton.java
@@ -3,10 +3,12 @@ package com.intellij.execution.ui;
 
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.Disposable;
+import com.intellij.openapi.options.OptionsBundle;
 import com.intellij.openapi.ui.popup.IconButton;
 import com.intellij.openapi.ui.popup.JBPopup;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.ui.InplaceButton;
+import com.intellij.ui.UIBundle;
 import com.intellij.ui.awt.RelativePoint;
 import com.intellij.util.ui.JBUI;
 
@@ -45,7 +47,7 @@ public class TagButton extends JButton implements Disposable {
         }
         else if ((MouseEvent.MOUSE_ENTERED == me.getID() || MouseEvent.MOUSE_MOVED == me.getID()) &&
                 (myPopup == null || !myPopup.isVisible())) {
-          myCloseButton = new InplaceButton(new IconButton("Close", AllIcons.Actions.DeleteTag, AllIcons.Actions.DeleteTagHover),
+          myCloseButton = new InplaceButton(new IconButton(OptionsBundle.message("tag.button.tooltip"), AllIcons.Actions.DeleteTag, AllIcons.Actions.DeleteTagHover),
                               a -> { action.run(); hidePopup(); });
           myCloseButton.setOpaque(false);
           myCloseButton.setPreferredSize(new Dimension(16, 16));
