5b079513c0b7a677ac510ebf92d1ee4eca881d12
Alex Plate <aleksei.plate@jetbrains.com>
2020-05-01 13:58:16 +0300

    [Workspace Model] Refactor workspaceModel listener in legacy bridge
    GitOrigin-RevId: f4819209587bb4de22b09754b03e67619b19f09e

--- platform/workspaceModel-ide/src/com/intellij/workspace/legacyBridge/facet/FacetEntityChangeListener.kt
+++ platform/workspaceModel-ide/src/com/intellij/workspace/legacyBridge/facet/FacetEntityChangeListener.kt
@@ -3,6 +3,7 @@ package com.intellij.workspace.legacyBridge.facet
 
 import com.intellij.facet.FacetManager
 import com.intellij.facet.FacetManagerBase
+import com.intellij.openapi.components.service
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
@@ -12,30 +13,57 @@ import com.intellij.workspace.api.FacetEntity
 import com.intellij.workspace.api.ModuleEntity
 import com.intellij.workspace.ide.WorkspaceModelChangeListener
 
-internal class FacetEntityChangeListener(private val project: Project) : WorkspaceModelChangeListener {
-  override fun beforeChanged(event: EntityStoreChanged) {
-    val changes = event.getChanges(FacetEntity::class.java)
-    loop@ for (change in changes) {
-      when (change) {
-        is EntityChange.Added -> {
-          val manager = getFacetManager(change.entity.module) ?: continue@loop
-          manager.publisher.beforeFacetAdded(manager.model.getOrCreateFacet(change.entity))
-        }
-        is EntityChange.Removed -> {
-          val manager = getFacetManager(change.entity.module) ?: continue@loop
-          val facet = manager.model.getFacet(change.entity) ?: continue@loop
-          manager.publisher.beforeFacetRemoved(facet)
-        }
-        is EntityChange.Replaced -> {
-          val manager = getFacetManager(change.newEntity.module) ?: continue@loop
-          if (change.newEntity.name != change.oldEntity.name) {
-            manager.publisher.beforeFacetRenamed(manager.model.getOrCreateFacet(change.newEntity))
-          }
+internal class FacetEntityChangeListener(private val project: Project) {
+
+  fun processBeforeChange(change: EntityChange<FacetEntity>) {
+    when (change) {
+      is EntityChange.Added -> {
+        val manager = getFacetManager(change.entity.module) ?: return
+        manager.publisher.beforeFacetAdded(manager.model.getOrCreateFacet(change.entity))
+      }
+      is EntityChange.Removed -> {
+        val manager = getFacetManager(change.entity.module) ?: return
+        val facet = manager.model.getFacet(change.entity) ?: return
+        manager.publisher.beforeFacetRemoved(facet)
+      }
+      is EntityChange.Replaced -> {
+        val manager = getFacetManager(change.newEntity.module) ?: return
+        if (change.newEntity.name != change.oldEntity.name) {
+          manager.publisher.beforeFacetRenamed(manager.model.getOrCreateFacet(change.newEntity))
         }
       }
     }
   }
 
+  fun processChange(change: EntityChange<FacetEntity>) {
+    when (change) {
+      is EntityChange.Added -> {
+        val manager = getFacetManager(change.entity.module) ?: return
+        val facet = manager.model.getOrCreateFacet(change.entity)
+        manager.model.updateEntity(change.entity, change.entity)
+        FacetManagerBase.setFacetName(facet, change.entity.name)
+        facet.initFacet()
+        manager.model.facetsChanged()
+        manager.publisher.facetAdded(facet)
+      }
+      is EntityChange.Removed -> {
+        val manager = getFacetManager(change.entity.module) ?: return
+        val facet = manager.model.removeEntity(change.entity) ?: return
+        Disposer.dispose(facet)
+        manager.model.facetsChanged()
+        manager.publisher.facetRemoved(facet)
+      }
+      is EntityChange.Replaced -> {
+        val manager = getFacetManager(change.newEntity.module) ?: return
+        val facet = manager.model.updateEntity(change.oldEntity, change.newEntity) ?: return
+        FacetManagerBase.setFacetName(facet, change.newEntity.name)
+        manager.model.facetsChanged()
+        if (change.oldEntity.name != change.newEntity.name) {
+          manager.publisher.facetRenamed(facet, change.oldEntity.name)
+        }
+      }
+    }
+  }
 
   private val FacetManagerViaWorkspaceModel.publisher
     get() = module.messageBus.syncPublisher(FacetManager.FACETS_TOPIC)
@@ -45,36 +73,7 @@ internal class FacetEntityChangeListener(private val project: Project) : Workspa
     return FacetManager.getInstance(module) as? FacetManagerViaWorkspaceModel
   }
 
-  override fun changed(event: EntityStoreChanged) {
-    val changes = event.getChanges(FacetEntity::class.java)
-    loop@ for (change in changes) {
-      when (change) {
-        is EntityChange.Added -> {
-          val manager = getFacetManager(change.entity.module) ?: continue@loop
-          val facet = manager.model.getOrCreateFacet(change.entity)
-          manager.model.updateEntity(change.entity, change.entity)
-          FacetManagerBase.setFacetName(facet, change.entity.name)
-          facet.initFacet()
-          manager.model.facetsChanged()
-          manager.publisher.facetAdded(facet)
-        }
-        is EntityChange.Removed -> {
-          val manager = getFacetManager(change.entity.module) ?: continue@loop
-          val facet = manager.model.removeEntity(change.entity) ?: continue@loop
-          Disposer.dispose(facet)
-          manager.model.facetsChanged()
-          manager.publisher.facetRemoved(facet)
-        }
-        is EntityChange.Replaced -> {
-          val manager = getFacetManager(change.newEntity.module) ?: continue@loop
-          val facet = manager.model.updateEntity(change.oldEntity, change.newEntity) ?: continue@loop
-          FacetManagerBase.setFacetName(facet, change.newEntity.name)
-          manager.model.facetsChanged()
-          if (change.oldEntity.name != change.newEntity.name) {
-            manager.publisher.facetRenamed(facet, change.oldEntity.name)
-          }
-        }
-      }
-    }
+  companion object {
+    fun getInstance(project: Project) = project.service<FacetEntityChangeListener>()
   }
 }
