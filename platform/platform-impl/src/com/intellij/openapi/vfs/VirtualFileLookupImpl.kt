20316895a41e73dfed05dfe14a15e1515c2b88f2
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2020-05-02 16:32:05 +0200

    vfs: implement generic VirtualFile lookup service
    The service supports all possible ways of finding VirtualFile from:
    - URL
    - java.io.File
    - java.nio.path.Path
    - String representation
    
    The aim is use provide the standard and the command way for conversions.
    In the coming commits we add extension point to allow custom nio filesystems
    to play nicely with VFS. In addition to that, all other lookup methods would
    be made to use that API
    
    GitOrigin-RevId: b09b2e53889722075699eeab4023f16b495d78d7

---
+++ platform/platform-impl/src/com/intellij/openapi/vfs/VirtualFileLookupImpl.kt
@@ -0,0 +1,46 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.vfs
+
+import com.intellij.openapi.util.io.FileUtil
+import java.io.File
+import java.nio.file.FileSystems
+import java.nio.file.Path
+
+internal data class VirtualFileLookupImpl(
+  val withRefresh: Boolean = false
+): VirtualFileLookup {
+  override fun withRefresh() = copy(withRefresh = true)
+
+  override fun fromIoFile(file: File): VirtualFile? = fromNioPath(file.toPath())
+
+  override fun fromPath(path: String): VirtualFile? {
+    return findWithFilesSystem(LocalFileSystem.getInstance(), FileUtil.toSystemDependentName(path))
+  }
+
+  override fun fromNioPath(path: Path): VirtualFile? {
+    if (path.fileSystem == FileSystems.getDefault()) {
+      return findWithFilesSystem(LocalFileSystem.getInstance(), path.toFile().absolutePath)
+    }
+
+    //TODO[jo] add EP to check for custom FS implementations for non-default filesystems
+    return null
+  }
+
+  override fun fromUrl(url: String): VirtualFile? {
+    val protocol = VirtualFileManager.extractProtocol(url) ?: return null
+    val path = VirtualFileManager.extractPath(url)
+    val fs = VirtualFileManager.getInstance().getFileSystem(protocol) ?: return null
+    return findWithFilesSystem(fs, path)
+  }
+
+  private fun findWithFilesSystem(fs: VirtualFileSystem, path: String): VirtualFile? {
+    return when {
+      withRefresh -> fs.refreshAndFindFileByPath(path)
+      else -> fs.findFileByPath(path)
+    }
+  }
+}
+
+internal class VirtualFileLookupServiceImpl: VirtualFileLookupService {
+  override fun newLookup() = VirtualFileLookupImpl()
+}
