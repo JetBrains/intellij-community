b600f006c21f2504822676f7c941ea10a1c2dbdc
Alexander Zolotov <zolotov@jetbrains.com>
2020-05-04 20:26:17 +0300

    [status bar] do not allow to hide widget with empty display name in non-internal mode (CPP-20148)
    GitOrigin-RevId: 3fa58fbede9ef7cfd1b9cde82bab07b143171401

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/status/widget/StatusBarWidgetProviderToFactoryAdapter.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/status/widget/StatusBarWidgetProviderToFactoryAdapter.java
@@ -62,6 +62,11 @@ public final class StatusBarWidgetProviderToFactoryAdapter implements StatusBarW
   }
 
   @Override
+  public boolean isConfigurable() {
+    return !getDisplayName().isEmpty();
+  }
+
+  @Override
   public boolean canBeEnabledOn(@NotNull StatusBar statusBar) {
     return true;
   }
