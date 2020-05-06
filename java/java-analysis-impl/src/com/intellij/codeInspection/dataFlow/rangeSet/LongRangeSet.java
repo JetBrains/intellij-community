799c902d55621c9c662fba4e27598fbe3533cb66
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2020-05-06 11:06:01 +0700

    LongRangeSet: subtract fixed for modRange
    GitOrigin-RevId: e366f4e42e83bddc3fa898c55de616dbbaf13d5f

--- java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/rangeSet/LongRangeSet.java
+++ java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/rangeSet/LongRangeSet.java
@@ -44,7 +44,7 @@ public abstract class LongRangeSet {
   LongRangeSet() {}
 
   /**
-   * Subtracts given set from the current
+   * Subtracts given set from the current. May return bigger set (containing some additional elements) if exact subtraction is impossible.
    *
    * @param other set to subtract
    * @return a new set
@@ -73,7 +73,7 @@ public abstract class LongRangeSet {
   public abstract LongRangeSet intersect(@NotNull LongRangeSet other);
 
   /**
-   * Merge current set with other
+   * Merge current set with other. May return bigger set if exact representation is impossible.
    *
    * @param other other set to merge with
    * @return a new set
@@ -1368,18 +1368,27 @@ public abstract class LongRangeSet {
         return new RangeSet(new long[]{myFrom, value - 1, value + 1, myTo});
       }
       if (other instanceof Range) {
+        LongRangeSet toJoin = Empty.EMPTY;
         long from = ((Range)other).myFrom;
         long to = ((Range)other).myTo;
         if (to < myFrom || from > myTo) return this;
-        if (from <= myFrom && to >= myTo) return Empty.EMPTY;
+        if (other instanceof ModRange) {
+          ModRange modRange = (ModRange)other;
+          long newBits = ~modRange.myBits;
+          if (modRange.myMod < 64) {
+            newBits &= (1L << modRange.myMod) - 1;
+          }
+          toJoin = modRange(Math.max(from, myFrom), Math.min(to, myTo), modRange.myMod, newBits);
+        }
+        if (from <= myFrom && to >= myTo) return toJoin;
         if (from > myFrom && to < myTo) {
-          return new RangeSet(new long[]{myFrom, from - 1, to + 1, myTo});
+          return new RangeSet(new long[]{myFrom, from - 1, to + 1, myTo}).unite(toJoin);
         }
         if (from <= myFrom) {
-          return range(to + 1, myTo);
+          return range(to + 1, myTo).unite(toJoin);
         }
         assert to >= myTo;
-        return range(myFrom, from - 1);
+        return range(myFrom, from - 1).unite(toJoin);
       }
       long[] ranges = ((RangeSet)other).myRanges;
       LongRangeSet result = this;
@@ -1738,6 +1747,11 @@ public abstract class LongRangeSet {
     @Override
     public boolean contains(long value) {
       return super.contains(value) && isSet(myBits, remainder(value, myMod));
+    }
+
+    @Override
+    public @NotNull LongRangeSet subtract(@NotNull LongRangeSet other) {
+      return super.subtract(other);
     }
 
     @NotNull
