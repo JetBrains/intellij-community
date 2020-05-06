b7fde6791b031bd840959392f211d970d1625d0f
Lada Gagina <lada.gagina@jetbrains.com>
2020-05-06 14:27:35 +0300

    PY-40431 i18n: Add non-python UI strings to bundles
    GitOrigin-RevId: 90b88f80e0c8d1cb1c2ae3a016024c817d74271e

--- platform/smRunner/src/com/intellij/execution/testframework/sm/runner/history/actions/ImportTestsFromFileAction.java
+++ platform/smRunner/src/com/intellij/execution/testframework/sm/runner/history/actions/ImportTestsFromFileAction.java
@@ -14,8 +14,8 @@ import org.jetbrains.annotations.Nullable;
 
 public class ImportTestsFromFileAction extends AbstractImportTestsAction {
   public ImportTestsFromFileAction() {
-    super(null, "Import Tests from File...", 
-          "Import the results of a test execution from an XML file", 
+    super(null, SmRunnerBundle.message("sm.test.runner.import.test"),
+          SmRunnerBundle.message("sm.test.runner.import.test.description"),
           AllIcons.ToolbarDecorator.Import);
   }
 
