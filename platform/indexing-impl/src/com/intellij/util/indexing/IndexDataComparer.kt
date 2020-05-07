7ed3543a90ffdffc3747a17c7d053224af670c5c
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2020-05-07 17:49:22 +0300

    Indexes: move IndexDataComparer and IndexDataPresenter to indexing-impl module.
    GitOrigin-RevId: 1d26aa93e4ff4dfb0b2a21a49b8a092a94d20bbe

--- platform/indexing-impl/src/com/intellij/util/indexing/IndexDataComparer.kt
+++ platform/indexing-impl/src/com/intellij/util/indexing/IndexDataComparer.kt
@@ -1,11 +1,10 @@
-package com.intellij.util.indexing.diagnostic.dump.output
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.util.indexing
 
 import com.intellij.psi.stubs.ObjectStubBase
 import com.intellij.psi.stubs.SerializedStubTree
 import com.intellij.psi.stubs.Stub
 import com.intellij.util.containers.hash.EqualityPolicy
-import com.intellij.util.indexing.FileBasedIndexExtension
-import com.intellij.util.indexing.SingleEntryFileBasedIndexExtension
 
 object IndexDataComparer {
 
