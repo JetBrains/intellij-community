cf9a68d7e0092966d0bec64dbf06f5214e907a3d
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-05-07 20:34:55 +0200

    use IntArrayList instead of trove, use Int2ObjectOpenHashMap instead of trove TIntObjectHashMap
    GitOrigin-RevId: e5768441bb3bdc3cbdf8a65bf954b5dc2b588944

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/AccessibleGutterLine.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/AccessibleGutterLine.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.editor.impl;
 
 import com.intellij.codeInsight.daemon.GutterMark;
@@ -175,7 +175,7 @@ class AccessibleGutterLine extends JPanel {
       for (int i = 0; i < myGutter.myTextAnnotationGutters.size(); i++) {
         TextAnnotationGutterProvider gutterProvider = myGutter.myTextAnnotationGutters.get(i);
         if (tooltipText == null) tooltipText = gutterProvider.getToolTip(myLogicalLineNum, editor); // [tav] todo: take first non-null?
-        int annotationSize = myGutter.myTextAnnotationGutterSizes.get(i);
+        int annotationSize = myGutter.myTextAnnotationGutterSizes.getInt(i);
         buf.append(notNull(gutterProvider.getLineText(myLogicalLineNum, editor), ""));
         width += annotationSize;
       }
