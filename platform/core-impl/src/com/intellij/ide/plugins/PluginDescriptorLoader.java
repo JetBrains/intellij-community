81896c590a063d6c7998e4469831cd0372d83800
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2020-05-07 17:34:11 +0300

    Add support for unpacked plugins in classpath
    Mostly with Kotlin plugin in mind, when running from sources.
    
    GitOrigin-RevId: 858dd41b51d2eda2b8325882db11958cabd4b8b7

--- platform/core-impl/src/com/intellij/ide/plugins/PluginDescriptorLoader.java
+++ platform/core-impl/src/com/intellij/ide/plugins/PluginDescriptorLoader.java
@@ -267,7 +267,12 @@ public final class PluginDescriptorLoader {
       else if (URLUtil.JAR_PROTOCOL.equals(resource.getProtocol())) {
         String path = resource.getFile();
         file = urlToFile(path.substring(0, path.indexOf(URLUtil.JAR_SEPARATOR)));
-        return loadDescriptorFromJar(file, pathName, loadingContext.pathResolver, loadingContext, null);
+        Path pluginPath = null;
+        if (file.getParent() != null && file.getParent().endsWith("lib")) {
+          // Support for unpacked plugins in classpath. E.g. .../community/build/dependencies/build/kotlin/Kotlin/lib/kotlin-plugin.jar
+          pluginPath = file.getParent().getParent();
+        }
+        return loadDescriptorFromJar(file, pathName, loadingContext.pathResolver, loadingContext, pluginPath);
       }
       else {
         return null;
