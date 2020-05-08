8dccb02b43502ec7bc906a4c07381063b63408ee
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2020-05-08 20:10:49 +0300

    [git] Rebase: Extract commit details loading as utility methods and generalize it for collection of commits
    GitOrigin-RevId: 9369e6f6b88c1355e8e0af37308ce412a68d8cbd

--- plugins/git4idea/src/git4idea/rebase/GitRewordAction.kt
+++ plugins/git4idea/src/git4idea/rebase/GitRewordAction.kt
@@ -2,27 +2,15 @@
 package git4idea.rebase
 
 import com.intellij.dvcs.repo.Repository
-import com.intellij.notification.NotificationType
-import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.diagnostic.logger
-import com.intellij.openapi.progress.ProcessCanceledException
 import com.intellij.openapi.progress.ProgressIndicator
-import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vcs.VcsException
-import com.intellij.openapi.vcs.VcsNotifier
 import com.intellij.vcs.log.VcsCommitMetadata
-import com.intellij.vcs.log.VcsShortCommitDetails
-import com.intellij.vcs.log.data.VcsLogData
-import com.intellij.vcs.log.impl.VcsCommitMetadataImpl
-import com.intellij.vcs.log.util.VcsLogUtil
 import com.intellij.vcs.log.util.VcsUserUtil.getShortPresentation
 import git4idea.i18n.GitBundle
 import git4idea.rebase.log.GitNewCommitMessageActionDialog
+import git4idea.rebase.log.getOrLoadDetails
 import git4idea.repo.GitRepository
-
-private val LOG: Logger = logger<GitRewordAction>()
 
 internal class GitRewordAction : GitSingleCommitEditingAction() {
   override val prohibitRebaseDuringRebasePolicy = ProhibitRebaseDuringRebasePolicy.Prohibit(
@@ -30,52 +18,12 @@ internal class GitRewordAction : GitSingleCommitEditingAction() {
   )
 
   override fun actionPerformedAfterChecks(commitEditingData: SingleCommitEditingData) {
-    val details = getOrLoadDetails(commitEditingData.project, commitEditingData.logData, commitEditingData.selectedCommit)
+    val details = getOrLoadDetails(commitEditingData.project, commitEditingData.logData, listOf(commitEditingData.selectedCommit))
 
-    RewordNewCommitMessageActionDialog(commitEditingData, details).show()
-  }
-
-  private fun getOrLoadDetails(project: Project, data: VcsLogData, commit: VcsShortCommitDetails): VcsCommitMetadata {
-    return commit as? VcsCommitMetadata
-           ?: getCommitDataFromCache(data, commit)
-           ?: loadCommitData(project, data, commit)
-           ?: throw ProcessCanceledException()
+    RewordNewCommitMessageActionDialog(commitEditingData, details.first()).show()
   }
 
   override fun getFailureTitle(): String = GitBundle.getString("rebase.log.reword.action.failure.title")
-
-  private fun getCommitDataFromCache(data: VcsLogData, commit: VcsShortCommitDetails): VcsCommitMetadata? {
-    val commitIndex = data.getCommitIndex(commit.id, commit.root)
-    val commitData = data.commitDetailsGetter.getCommitDataIfAvailable(commitIndex)
-    if (commitData != null) return commitData
-
-    val message = data.index.dataGetter?.getFullMessage(commitIndex)
-    if (message != null) return VcsCommitMetadataImpl(commit.id, commit.parents, commit.commitTime, commit.root, commit.subject,
-                                                      commit.author, message, commit.committer, commit.authorTime)
-    return null
-  }
-
-  private fun loadCommitData(project: Project, data: VcsLogData, commit: VcsShortCommitDetails): VcsCommitMetadata? {
-    var commitData: VcsCommitMetadata? = null
-    ProgressManager.getInstance().runProcessWithProgressSynchronously(
-      {
-        try {
-          commitData = VcsLogUtil.getDetails(data, commit.root, commit.id)
-        }
-        catch (e: VcsException) {
-          val error = GitBundle.message("rebase.log.reword.action.loading.commit.message.failed.message", commit.id.asString())
-          LOG.warn(error, e)
-          val notification = VcsNotifier.STANDARD_NOTIFICATION.createNotification(
-            "",
-            error,
-            NotificationType.ERROR,
-            null
-          )
-          VcsNotifier.getInstance(project).notify(notification)
-        }
-      }, GitBundle.getString("rebase.log.reword.action.progress.indicator.loading.commit.message.title"), true, project)
-    return commitData
-  }
 
   private fun rewordInBackground(project: Project, commit: VcsCommitMetadata, repository: GitRepository, newMessage: String) {
     object : Task.Backgroundable(project, GitBundle.getString("rebase.log.reword.action.progress.indicator.title")) {
