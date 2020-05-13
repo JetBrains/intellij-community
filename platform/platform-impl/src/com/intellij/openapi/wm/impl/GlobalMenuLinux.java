98f110ddec41bf7671a93e927c3f09c43bbd9f70
Konstantin Bulenkov <kb@jetbrains.com>
2020-05-13 11:46:43 +0200

    rollback proper JavaFX initialization
    GitOrigin-RevId: 644bec7e5e66d112116d4c157ac690c88b519bef

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/GlobalMenuLinux.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/GlobalMenuLinux.java
@@ -216,11 +216,19 @@ public final class GlobalMenuLinux implements LinuxGlobalMenuEventHandler, Dispo
         }
       };
 
-      final Thread glibMain = new Thread(
-              () -> ourLib.runMainLoop(ourGLogger,
-                      ourOnAppmenuServiceAppeared,
-                      ourOnAppmenuServiceVanished), "GlobalMenuLinux loop");
-      glibMain.start();
+      // NOTE: Linux implementation of JavaFX starts native main loop with GtkApplication._runLoop()
+      try {
+        Class<?> platformImpl = Class.forName("com.sun.javafx.application.PlatformImpl");
+        Method startup = platformImpl.getMethod("startup", Runnable.class);
+        Runnable r = () -> ourLib.startWatchDbus(ourGLogger, ourOnAppmenuServiceAppeared, ourOnAppmenuServiceVanished);
+        startup.invoke(null, r);
+      }
+      catch (Throwable e) {
+        LOG.info("can't start main loop via JavaFX (will run it manually): " + e.getMessage());
+        final Thread glibMain = new Thread(() -> ourLib.runMainLoop(ourGLogger, ourOnAppmenuServiceAppeared, ourOnAppmenuServiceVanished),
+                                           "GlobalMenuLinux loop");
+        glibMain.start();
+      }
     }
   }
 
