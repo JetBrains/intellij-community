5d6f1b674d92ee856bfebbaf947a31cd2f591bea
Marius Volkhart <marius@volkhart.com>
2020-05-13 16:39:59 +0200

    IG: out-of-the-box support for Log4J 2's LogManager factory (IDEA-240494)
    for "Logger initialized with foreign class" inspection
    
    GitOrigin-RevId: f7ef970f35e5aabf6af6c10b00cd097caf625405

--- plugins/InspectionGadgets/testsrc/com/siyeh/ig/logging/LoggerInitializedWithForeignClassInspectionTest.java
+++ plugins/InspectionGadgets/testsrc/com/siyeh/ig/logging/LoggerInitializedWithForeignClassInspectionTest.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.siyeh.ig.logging;
 
 import com.intellij.codeInspection.InspectionProfileEntry;
@@ -28,13 +14,15 @@ public class LoggerInitializedWithForeignClassInspectionTest extends LightJavaIn
     doTest();
   }
 
+  public void testLog4J2() {
+    doTest();
+    checkQuickFixAll();
+  }
+
   @Nullable
   @Override
   protected InspectionProfileEntry getInspection() {
-    final LoggerInitializedWithForeignClassInspection inspection = new LoggerInitializedWithForeignClassInspection();
-    inspection.loggerFactoryClassNames.add("java.util.logging.Logger");
-    inspection.loggerFactoryMethodNames.add("getLogger");
-    return inspection;
+    return new LoggerInitializedWithForeignClassInspection();
   }
 
   @Override
@@ -45,7 +33,18 @@ public class LoggerInitializedWithForeignClassInspectionTest extends LightJavaIn
       "  public static Logger getLogger(String name) {" +
       "    return null;" +
       "  }" +
-      "}"
+      "}",
+
+      "package org.apache.logging.log4j;" +
+      "public class LogManager {" +
+      "  public static Logger getLogger(Class<?> clazz) {" +
+      "    return null;" +
+      "  }" +
+      "  public static Logger getLogger(String name) {" +
+      "    return null;" +
+      "  }" +
+      "}" +
+      "public interface Logger {}"
     };
   }
 }
