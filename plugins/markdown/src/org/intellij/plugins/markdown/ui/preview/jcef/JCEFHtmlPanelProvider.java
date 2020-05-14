3a2ce802e5df429bb1c3117372138b351b3ca439
Anton Tarasov <anton.tarasov@jetbrains.com>
2020-05-14 22:11:08 +0300

    IDEA-240913 [jcef] ExceptionInInitializerError when opening md file with non-default jdk
    GitOrigin-RevId: 70e38f175faa08bb10382870a325b6b2cb9d381c

--- plugins/markdown/src/org/intellij/plugins/markdown/ui/preview/jcef/JCEFHtmlPanelProvider.java
+++ plugins/markdown/src/org/intellij/plugins/markdown/ui/preview/jcef/JCEFHtmlPanelProvider.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.intellij.plugins.markdown.ui.preview.jcef;
 
+import com.intellij.ui.jcef.JBCefApp;
 import org.intellij.plugins.markdown.ui.preview.MarkdownHtmlPanel;
 import org.intellij.plugins.markdown.ui.preview.MarkdownHtmlPanelProvider;
 import org.jetbrains.annotations.NotNull;
@@ -16,16 +17,7 @@ public class JCEFHtmlPanelProvider extends MarkdownHtmlPanelProvider {
   @NotNull
   @Override
   public AvailabilityInfo isAvailable() {
-    try {
-      // [tav] todo: we bundle jcef.jar with API in IDEA that anyway provides this class. So we should check for jcef module presence in JBR.
-      if (Class.forName("org.cef.browser.CefBrowser", false, getClass().getClassLoader()) != null) {
-        return AvailabilityInfo.AVAILABLE;
-      }
-    }
-    catch (ClassNotFoundException ignored) {
-    }
-
-    return AvailabilityInfo.UNAVAILABLE;
+    return JBCefApp.isSupported() ? AvailabilityInfo.AVAILABLE : AvailabilityInfo.UNAVAILABLE;
   }
 
   @NotNull
