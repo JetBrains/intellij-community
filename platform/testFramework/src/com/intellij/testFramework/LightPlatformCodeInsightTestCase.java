e986bf32658f77725b5b1f8a1cb6ffaca7985067
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2020-05-15 10:28:11 +0300

    IDEA-240930 refactored braces highlighting tests to be more friendly
    GitOrigin-RevId: d7d758b0c574aef1918591931ace1fa2239eb1fa

--- platform/testFramework/src/com/intellij/testFramework/LightPlatformCodeInsightTestCase.java
+++ platform/testFramework/src/com/intellij/testFramework/LightPlatformCodeInsightTestCase.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.testFramework;
 
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer;
@@ -106,6 +106,11 @@ public abstract class LightPlatformCodeInsightTestCase extends LightPlatformTest
     catch (IOException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  @NotNull
+  protected String getAnswerFilePath() {
+    return getTestDataPath() + myFileSuffix + ".txt";
   }
 
   private static void checkCaseSensitiveFS(@NotNull String fullOrRelativePath, @NotNull File ioFile) throws IOException {
