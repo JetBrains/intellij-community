0be35fa1bfe9f5663d00bc7ba5c67847096896a8
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2020-05-15 14:39:25 +0700

    Unused methods removed
    GitOrigin-RevId: 820c6726347c8fd73b6bd947426ce9bf6d5921da

--- platform/platform-impl/src/com/intellij/diagnostic/Diagnostic.java
+++ platform/platform-impl/src/com/intellij/diagnostic/Diagnostic.java
@@ -16,7 +16,6 @@
 package com.intellij.diagnostic;
 
 import com.intellij.openapi.diagnostic.Logger;
-import org.jetbrains.annotations.NonNls;
 
 public class Diagnostic {
   public static void trace(String category, String message) {
@@ -34,24 +33,5 @@ public class Diagnostic {
   public static boolean assertTrue(String category, String message, boolean condition) {
     if (condition) return true;
     return Logger.getInstance(category).assertTrue(condition, message);
-  }
-
-  public static boolean isAssertEnabled(String category) {
-    return true;
-  }
-
-  public static boolean isJavaAssertionsEnabled() {
-    boolean result = false;
-    assert result = true;
-    return result;
-  }
-
-  public static void methodNotImplemented(String category) {
-    methodNotImplemented(category, "");
-  }
-
-  public static void methodNotImplemented(String category, String message) {
-    @NonNls String message1 = "METHOD NOT IMPLEMENTED YET " + message;
-    Logger.getInstance(category).error(message1);
   }
 }
