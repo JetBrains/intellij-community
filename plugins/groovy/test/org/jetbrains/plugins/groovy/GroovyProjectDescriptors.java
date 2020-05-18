fbab07214b495ffd033bd72dd2d61e5447c47cf8
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2020-05-18 16:36:25 +0300

    [groovy] IDEA-209501 bump test project descriptor groovy to 2.5.11
    GitOrigin-RevId: 06cc33605093c532143ba7e76259ec9e464128c3

--- plugins/groovy/test/org/jetbrains/plugins/groovy/GroovyProjectDescriptors.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/GroovyProjectDescriptors.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy;
 
 import com.intellij.openapi.projectRoots.JavaSdk;
@@ -14,7 +14,7 @@ public interface GroovyProjectDescriptors {
   TestLibrary LIB_GROOVY_2_2 = new RepositoryTestLibrary("org.codehaus.groovy:groovy:2.2.0");
   TestLibrary LIB_GROOVY_2_3 = new RepositoryTestLibrary("org.codehaus.groovy:groovy:2.3.0");
   TestLibrary LIB_GROOVY_2_4 = new RepositoryTestLibrary("org.codehaus.groovy:groovy-all:2.4.17");
-  TestLibrary LIB_GROOVY_2_5 = new RepositoryTestLibrary("org.codehaus.groovy:groovy:2.5.6");
+  TestLibrary LIB_GROOVY_2_5 = new RepositoryTestLibrary("org.codehaus.groovy:groovy:2.5.11");
   TestLibrary LIB_GROOVY_3_0 = new RepositoryTestLibrary("org.codehaus.groovy:groovy:3.0.0-alpha-2");
 
   LightProjectDescriptor GROOVY_1_6 = new LibraryLightProjectDescriptor(LIB_GROOVY_1_6);
