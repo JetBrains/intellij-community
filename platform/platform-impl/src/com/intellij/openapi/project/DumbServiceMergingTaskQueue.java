e7b556708da120e5384145c4fff397e1399f6ebf
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2020-05-19 21:35:54 +0200

    DumbService: extract to-be-common tasks queue, add tests, ensure expected behaviour
    GitOrigin-RevId: 94f808c0b318a33bf8638cb691c14327a0efc15a

---
+++ platform/platform-impl/src/com/intellij/openapi/project/DumbServiceMergingTaskQueue.java
@@ -0,0 +1,120 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.project;
+
+import com.intellij.openapi.progress.util.ProgressIndicatorBase;
+import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.wm.ex.ProgressIndicatorEx;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.*;
+
+public class DumbServiceMergingTaskQueue {
+  private final Object myLock = new Object();
+  private final Map<Object, DumbModeTask> myTasksQueue = new LinkedHashMap<>();
+  private final Map<DumbModeTask, ProgressIndicatorBase> myProgresses = new HashMap<>();
+
+  /**
+   * Removes all tasks from Queue without disposing the tasks
+   */
+  void clearTasksQueue() {
+    //we use myProgresses to keep tasks for dispose
+    synchronized (myLock) {
+      myTasksQueue.clear();
+    }
+  }
+
+  /**
+   * Disposes tasks without removing them from queue,
+   * execution of such tasks would skip them later
+   */
+  void disposePendingTasks() {
+    Set<DumbModeTask> disposeQueue = new LinkedHashSet<>(1);
+    synchronized (myLock) {
+      disposeQueue.addAll(myTasksQueue.values());
+      disposeQueue.addAll(myProgresses.keySet());
+      myTasksQueue.clear();
+      myProgresses.clear();
+    }
+
+    for (DumbModeTask task : disposeQueue) {
+      Disposer.dispose(task);
+    }
+  }
+
+  void cancelAllTasks() {
+    List<ProgressIndicatorEx> tasks;
+    synchronized (myLock) {
+      tasks = new ArrayList<>(myProgresses.values());
+    }
+
+    for (ProgressIndicatorEx indicator : tasks) {
+      indicator.cancel();
+    }
+  }
+
+  public void cancelTask(@NotNull DumbModeTask task) {
+    ProgressIndicatorEx indicator;
+    synchronized (myLock) {
+      indicator = myProgresses.get(task);
+    }
+
+    if (indicator != null) {
+      indicator.cancel();
+    }
+  }
+
+  public void addTask(@NotNull DumbModeTask task) {
+    DumbModeTask olderTask;
+
+    synchronized (myLock) {
+      Object key = task.getEquivalenceObject();
+      //remove key to preserve FIFO order
+      olderTask = myTasksQueue.remove(key);
+
+      //register the new task last
+      myTasksQueue.put(key, task);
+
+      myProgresses.put(task, new ProgressIndicatorBase());
+      Disposer.register(task, () -> {
+        synchronized (myLock) {
+          myProgresses.remove(task);
+        }
+      });
+    }
+
+    if (olderTask != null) {
+      Disposer.dispose(olderTask);
+    }
+  }
+
+  @Nullable
+  public Pair<DumbModeTask, ProgressIndicatorEx> extractNextTask() {
+    List<DumbModeTask> disposeQueue = new ArrayList<>(1);
+
+    try {
+      synchronized (myLock) {
+        while (true) {
+          if (myTasksQueue.isEmpty()) return null;
+
+          Object key = myTasksQueue.keySet().iterator().next();
+          DumbModeTask task = myTasksQueue.remove(key);
+          if (task == null) continue;
+
+          ProgressIndicatorBase indicator = myProgresses.get(task);
+          if (indicator == null || indicator.isCanceled()) {
+            disposeQueue.add(task);
+            continue;
+          }
+
+          return Pair.create(task, indicator);
+        }
+      }
+    } finally {
+      for (DumbModeTask task : disposeQueue) {
+        Disposer.dispose(task);
+      }
+    }
+  }
+}
---
+++ platform/platform-tests/testSrc/com/intellij/openapi/project/DumbServiceMergingTaskQueueTest.java
@@ -0,0 +1,234 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.project;
+
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.wm.ex.ProgressIndicatorEx;
+import com.intellij.testFramework.fixtures.BasePlatformTestCase;
+import org.jetbrains.annotations.NotNull;
+import org.junit.Assert;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class DumbServiceMergingTaskQueueTest extends BasePlatformTestCase {
+  private final DumbServiceMergingTaskQueue myQueue = new DumbServiceMergingTaskQueue();
+
+  private void runAllTasks() {
+    while(true) {
+      Pair<DumbModeTask, ProgressIndicatorEx> nextTask = myQueue.extractNextTask();
+      if (nextTask == null) return;
+      try {
+        nextTask.first.performInDumbMode(nextTask.second);
+      } finally {
+        Disposer.dispose(nextTask.first);
+      }
+    }
+  }
+
+  public void testEquivalentTasksAreMerged() {
+    List<Integer> disposeLog = new ArrayList<>();
+    List<Integer> childLog = new ArrayList<>();
+    for (int i = 0; i < 100; i++) {
+      int taskId = i;
+      myQueue.addTask(new DumbModeTask("child") {
+        @Override
+        public void performInDumbMode(@NotNull ProgressIndicator indicator) {
+          childLog.add(taskId);
+        }
+
+        @Override
+        public void dispose() {
+          disposeLog.add(taskId);
+        }
+      });
+    }
+
+    runAllTasks();
+
+    Assert.assertEquals("Only one child task should run, but were: " + childLog, 1, childLog.size());
+    Assert.assertEquals("All tasks must be disposed, but were: " + disposeLog, 100, disposeLog.size());
+  }
+
+  public void testDifferentClassesWithSameEquivalentAreNotMerged() {
+    List<Integer> childLog = new ArrayList<>();
+    final String commonEquivalence = "child";
+    DumbModeTask taskA = new DumbModeTask(commonEquivalence) {
+      @Override
+      public void performInDumbMode(@NotNull ProgressIndicator indicator) {
+        childLog.add(1);
+      }
+    };
+
+    DumbModeTask taskB = new DumbModeTask(commonEquivalence) {
+      @Override
+      public void performInDumbMode(@NotNull ProgressIndicator indicator) {
+        childLog.add(-1);
+      }
+    };
+
+    //both taskA and taskB submits the same equality object, it must run both
+    myQueue.addTask(taskA);
+    myQueue.addTask(taskB);
+    runAllTasks();
+    Assert.assertEquals("All tasks should run, but were: " + childLog, 2, childLog.size());
+  }
+
+  public void testNonEquivalentTasksAreNotMerged() {
+    List<Integer> childLog = new ArrayList<>();
+    for (int i = 0; i < 100; i++) {
+      int taskId = i;
+      myQueue.addTask(new DumbModeTask("child" + i) {
+        @Override
+        public void performInDumbMode(@NotNull ProgressIndicator indicator) {
+          childLog.add(taskId);
+        }
+      });
+    }
+    runAllTasks();
+    Assert.assertEquals("Every child task are not unique, all must be executed: " + childLog, 100, childLog.size());
+  }
+
+  public void testNewTaskIsRunWhenMerged() {
+    List<Integer> disposeLog = new ArrayList<>();
+    List<Integer> childLog = new ArrayList<>();
+
+    for (int i = 0; i < 3; i++) {
+      int taskId = i;
+      myQueue.addTask(new DumbModeTask("child") {
+        @Override
+        public void performInDumbMode(@NotNull ProgressIndicator indicator) {
+          childLog.add(taskId);
+        }
+
+        @Override
+        public void dispose() {
+          disposeLog.add(taskId);
+        }
+      });
+    }
+
+    runAllTasks();
+    Assert.assertEquals("The last child task should run, but were: " + childLog, Collections.singletonList(2), childLog);
+    Assert.assertEquals("All tasks must be disposed, but were: " + disposeLog, 3, disposeLog.size());
+  }
+
+  public void testCancelledTask() {
+    List<Integer> disposeLog = new ArrayList<>();
+    List<Integer> childLog = new ArrayList<>();
+
+    DumbModeTask task = new DumbModeTask("child") {
+      @Override
+      public void performInDumbMode(@NotNull ProgressIndicator indicator) {
+        childLog.add(1);
+      }
+
+      @Override
+      public void dispose() {
+        disposeLog.add(1);
+      }
+    };
+
+    myQueue.addTask(task);
+    myQueue.cancelTask(task);
+
+    runAllTasks();
+    Assert.assertEquals("Cancelled task must not run " + childLog, Collections.emptyList(), childLog);
+    Assert.assertEquals("Cancelled task must dispose " + disposeLog, Collections.singletonList(1), disposeLog);
+  }
+
+  public void testMergedTaskShouldDispose() {
+    List<Integer> disposeLog = new ArrayList<>();
+    List<Integer> childLog = new ArrayList<>();
+
+    for (int i = 0; i < 3; i++) {
+      int taskId = i;
+      myQueue.addTask(new DumbModeTask("child") {
+        @Override
+        public void performInDumbMode(@NotNull ProgressIndicator indicator) {
+          childLog.add(taskId);
+        }
+
+        @Override
+        public void dispose() {
+          disposeLog.add(taskId);
+        }
+      });
+    }
+
+    Assert.assertEquals("No task should run by now " + childLog, Collections.emptyList(), childLog);
+    Assert.assertEquals("older task must be disposed" + disposeLog, Arrays.asList(0,1), disposeLog);
+
+    runAllTasks();
+
+    Assert.assertEquals("The last task should only run " + childLog, Collections.singletonList(2), childLog);
+    Assert.assertEquals("older task must be disposed" + disposeLog, Arrays.asList(0,1,2), disposeLog);
+  }
+
+  public void testTasksAreDisposed() {
+    List<Integer> disposeLog = new ArrayList<>();
+    List<Integer> childLog = new ArrayList<>();
+
+    DumbModeTask task = new DumbModeTask("child") {
+      @Override
+      public void performInDumbMode(@NotNull ProgressIndicator indicator) {
+        childLog.add(1);
+      }
+
+      @Override
+      public void dispose() {
+        disposeLog.add(1);
+      }
+    };
+
+    myQueue.addTask(task);
+    myQueue.disposePendingTasks();
+
+    Assert.assertEquals("The last task should only run " + childLog, Collections.emptyList(), childLog);
+    Assert.assertEquals("older task must be disposed" + disposeLog, Arrays.asList(1), disposeLog);
+
+    runAllTasks();
+    Assert.assertEquals("The last task should only run " + childLog, Collections.emptyList(), childLog);
+    Assert.assertEquals("older task must be disposed" + disposeLog, Arrays.asList(1), disposeLog);
+  }
+
+  public void testMergedTasksShouldNotReserveEarlierSlots() {
+    List<Integer> disposeLog = new ArrayList<>();
+    List<Integer> childLog = new ArrayList<>();
+
+    for (int i = 1; i <= 3; i++) {
+      int taskId = i;
+      myQueue.addTask(new DumbModeTask("child") {
+        @Override
+        public void performInDumbMode(@NotNull ProgressIndicator indicator) {
+          childLog.add(taskId);
+        }
+
+        @Override
+        public void dispose() {
+          disposeLog.add(taskId);
+        }
+      });
+
+      myQueue.addTask(new DumbModeTask("boss-" + i) {
+        @Override
+        public void performInDumbMode(@NotNull ProgressIndicator indicator) {
+          childLog.add(-taskId);
+        }
+
+        @Override
+        public void dispose() {
+          disposeLog.add(-taskId);
+        }
+      });
+    }
+
+    runAllTasks();
+    Assert.assertEquals("The last task should only run " + childLog, Arrays.asList(-1, -2, 3, -3), childLog);
+    Assert.assertEquals("older task must be disposed " + disposeLog, Arrays.asList(1, 2, -1, -2, 3, -3), disposeLog);
+  }
+
+}
