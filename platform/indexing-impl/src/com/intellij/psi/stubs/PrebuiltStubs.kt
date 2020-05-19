4a8f5feab1ff1fa19891a1a268b57f348996d806
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2020-05-19 21:16:45 +0300

    indexes: prebuilt stubs should tolerate PCE IDEA-241239
    GitOrigin-RevId: 8adbbdecf0227661b6f94976a08066480723dd9a

--- platform/indexing-impl/src/com/intellij/psi/stubs/PrebuiltStubs.kt
+++ platform/indexing-impl/src/com/intellij/psi/stubs/PrebuiltStubs.kt
@@ -7,6 +7,7 @@ import com.intellij.index.PrebuiltIndexProvider
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.fileTypes.FileTypeExtension
+import com.intellij.openapi.progress.ProcessCanceledException
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.util.text.StringUtilRt
 import com.intellij.util.indexing.FileContent
@@ -114,6 +115,9 @@ abstract class PrebuiltStubsProviderBase : PrebuiltIndexProvider<SerializedStubT
   override fun findStub(fileContent: FileContent): SerializedStubTree? {
     try {
       return get(fileContent)
+    }
+    catch (e: ProcessCanceledException) {
+      throw e
     }
     catch (e: Exception) {
       dispose()
