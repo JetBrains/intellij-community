ea6a1cfe1436de89f83339c9dcbe208ba829e31c
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2020-05-19 15:26:19 +0300

    IDEA-CR-62726: IDEA-234948: 'Unsplit All' action doesn't work
    (cherry picked from commit a035c36880b0c937c8d1571546ab12afa7b98fdd)
    
    GitOrigin-RevId: ea008fd0d43915dece0428387f1d551940897874

--- platform/platform-impl/src/com/intellij/ide/actions/SplitterAction.java
+++ platform/platform-impl/src/com/intellij/ide/actions/SplitterAction.java
@@ -1,11 +1,13 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.actions;
 
 import com.intellij.openapi.actionSystem.ActionPlaces;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx;
 import com.intellij.openapi.fileEditor.impl.EditorWindow;
 import com.intellij.openapi.project.DumbAwareAction;
+import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
 public abstract class SplitterAction extends DumbAwareAction {
@@ -26,11 +28,7 @@ public abstract class SplitterAction extends DumbAwareAction {
 
   boolean isEnabled(@NotNull AnActionEvent event) {
     EditorWindow window = event.getData(EditorWindow.DATA_KEY);
-    return window != null && isEnabled(event, window);
-  }
-
-  boolean isEnabled(@NotNull AnActionEvent event, @NotNull EditorWindow window) {
-    return window.inSplitter();
+    return window != null && window.inSplitter();
   }
 
 
@@ -78,13 +76,26 @@ public abstract class SplitterAction extends DumbAwareAction {
 
   public static final class UnsplitAll extends SplitterAction {
     @Override
+    public void actionPerformed(@NotNull AnActionEvent event) {
+      FileEditorManagerEx manager = getManager(event);
+      if (manager != null) manager.unsplitAllWindow();
+    }
+
+    @Override
     void actionPerformed(@NotNull EditorWindow window) {
       window.getManager().unsplitAllWindow();
     }
 
     @Override
-    boolean isEnabled(@NotNull AnActionEvent event, @NotNull EditorWindow window) {
-      return super.isEnabled(event, window) && window.getManager().getWindowSplitCount() > 2;
+    boolean isEnabled(@NotNull AnActionEvent event) {
+      if (ActionPlaces.isPopupPlace(event.getPlace())) return super.isEnabled(event);
+      FileEditorManagerEx manager = getManager(event);
+      return manager != null && manager.isInSplitter();
+    }
+
+    private static FileEditorManagerEx getManager(@NotNull AnActionEvent event) {
+      Project project = event.getProject();
+      return project == null ? null : FileEditorManagerEx.getInstanceEx(project);
     }
   }
 }
