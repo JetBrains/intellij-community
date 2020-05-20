434a6b17cc410330b3e7aef1f2dea92b5bd41713
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2020-05-20 18:22:48 +0300

    [project model] refactoring: extract superclass from ModuleLibraryTable to reuse in the new project model
    GitOrigin-RevId: 4e5651de661055c13c0e8e20608258530460c4a1

--- platform/projectModel-impl/src/com/intellij/openapi/roots/impl/ModuleLibraryTable.java
+++ platform/projectModel-impl/src/com/intellij/openapi/roots/impl/ModuleLibraryTable.java
@@ -16,84 +16,33 @@
 
 package com.intellij.openapi.roots.impl;
 
-import com.intellij.openapi.Disposable;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.roots.LibraryOrderEntry;
 import com.intellij.openapi.roots.OrderEntry;
 import com.intellij.openapi.roots.ProjectModelExternalSource;
-import com.intellij.openapi.roots.impl.libraries.LibraryTableImplUtil;
 import com.intellij.openapi.roots.libraries.Library;
-import com.intellij.openapi.roots.libraries.LibraryTable;
-import com.intellij.openapi.roots.libraries.LibraryTablePresentation;
 import com.intellij.openapi.roots.libraries.PersistentLibraryKind;
-import com.intellij.projectModel.ProjectModelBundle;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.containers.FilteringIterator;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 
 /**
  *  @author dsl
  */
 @ApiStatus.Internal
-public class ModuleLibraryTable implements LibraryTable, LibraryTable.ModifiableModel {
+public class ModuleLibraryTable extends ModuleLibraryTableBase {
   @NotNull
-  private final RootModelImpl myRootModel;
+  protected final RootModelImpl myRootModel;
   @NotNull
   private final ProjectRootManagerImpl myProjectRootManager;
-  public static final LibraryTablePresentation MODULE_LIBRARY_TABLE_PRESENTATION = new LibraryTablePresentation() {
-    @NotNull
-    @Override
-    public String getDisplayName(boolean plural) {
-      return ProjectModelBundle.message("module.library.display.name", plural ? 2 : 1);
-    }
-
-    @NotNull
-    @Override
-    public String getDescription() {
-      return ProjectModelBundle.message("libraries.node.text.module");
-    }
-
-    @NotNull
-    @Override
-    public String getLibraryTableEditorTitle() {
-      return ProjectModelBundle.message("library.configure.module.title");
-    }
-  };
 
   ModuleLibraryTable(@NotNull RootModelImpl rootModel, @NotNull ProjectRootManagerImpl projectRootManager) {
     myRootModel = rootModel;
     myProjectRootManager = projectRootManager;
-  }
-
-  @Override
-  public Library @NotNull [] getLibraries() {
-    final ArrayList<Library> result = new ArrayList<>();
-    final Iterator<Library> libraryIterator = getLibraryIterator();
-    ContainerUtil.addAll(result, libraryIterator);
-    return result.toArray(Library.EMPTY_ARRAY);
-  }
-
-  @NotNull
-  @Override
-  public Library createLibrary() {
-    return createLibrary(null);
-  }
-
-  @NotNull
-  @Override
-  public Library createLibrary(String name) {
-    return createLibrary(name, null);
-  }
-
-  @NotNull
-  @Override
-  public Library createLibrary(String name, @Nullable PersistentLibraryKind type) {
-    return createLibrary(name, type, null);
   }
 
   @NotNull
@@ -133,64 +82,12 @@ public class ModuleLibraryTable implements LibraryTable, LibraryTable.Modifiable
   }
 
   @NotNull
-  @Override
-  public String getTableLevel() {
-    return LibraryTableImplUtil.MODULE_LEVEL;
-  }
-
-  @NotNull
-  @Override
-  public LibraryTablePresentation getPresentation() {
-    return MODULE_LIBRARY_TABLE_PRESENTATION;
-  }
-
-  @Override
-  @Nullable
-  public Library getLibraryByName(@NotNull String name) {
-    final Iterator<Library> libraryIterator = getLibraryIterator();
-    while (libraryIterator.hasNext()) {
-      Library library = libraryIterator.next();
-      if (name.equals(library.getName())) return library;
-    }
-    return null;
-  }
-
-  @Override
-  public void addListener(@NotNull Listener listener) {
-    throw new UnsupportedOperationException();
-  }
-
-  @Override
-  public void addListener(@NotNull Listener listener, @NotNull Disposable parentDisposable) {
-    throw new UnsupportedOperationException("Method addListener is not yet implemented in " + getClass().getName());
-  }
-
-  @Override
-  public void removeListener(@NotNull Listener listener) {
-    throw new UnsupportedOperationException();
-  }
-
-  @NotNull
   public Module getModule() {
     return myRootModel.getModule();
   }
 
   @Override
-  public void commit() {
-  }
-
-  @Override
-  public void dispose() {
-  }
-
-  @Override
   public boolean isChanged() {
     return myRootModel.isChanged();
-  }
-
-  @NotNull
-  @Override
-  public ModifiableModel getModifiableModel() {
-    return this;
   }
 }
