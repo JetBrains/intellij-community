71e7f7cafced4ca2dabf37744e8f8419ddf87299
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2020-05-21 15:59:30 +0200

    WEB-42448 HTML Quote style - Enforce on reformat is not applied in Vue apps
    GitOrigin-RevId: 92ccc30545796abdafb7556f4db713601452de03

--- xml/impl/src/com/intellij/lang/html/HtmlQuotesFormatPreprocessor.java
+++ xml/impl/src/com/intellij/lang/html/HtmlQuotesFormatPreprocessor.java
@@ -43,7 +43,7 @@ public class HtmlQuotesFormatPreprocessor implements PreFormatProcessor {
     PsiElement psiElement = node.getPsi();
     if (psiElement != null &&
         psiElement.isValid() &&
-        psiElement.getLanguage().is(HTMLLanguage.INSTANCE)) {
+        psiElement.getLanguage().isKindOf(HTMLLanguage.INSTANCE)) {
       PsiFile file = psiElement.getContainingFile();
       PsiElement fileContext = file.getContext();
       String contextQuote = fileContext != null ? Character.toString(fileContext.getText().charAt(0)) : null;
