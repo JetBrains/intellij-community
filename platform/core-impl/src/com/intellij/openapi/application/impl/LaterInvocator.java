0f5cc099a2e6bdfeedd67598e6be8f6a986df5ad
Valentin Fondaratov <valentin.fondaratov@jetbrains.com>
2020-05-21 21:19:43 +0300

    IDEA-CR-62879: dry LaterInvocator a little to understand where `requestFlush` happens better
    Behavioural change: `purgeExpiredItems` leads to `requestFlush` as well now.
    
    (cherry picked from commit 3aec80e7c7b8005c3193db40902aa5bcd78122f7)
    
    GitOrigin-RevId: 996bc330b9c94711fdaea4a885aedc01f7dc2f3a

--- platform/core-impl/src/com/intellij/openapi/application/impl/LaterInvocator.java
+++ platform/core-impl/src/com/intellij/openapi/application/impl/LaterInvocator.java
@@ -107,8 +107,7 @@ public class LaterInvocator {
       return;
     }
     FlushQueue.RunnableInfo runnableInfo = new FlushQueue.RunnableInfo(runnable, modalityState, expired, callback);
-    getRunnableQueue(onEdt).push(runnableInfo);
-    requestFlush();
+    pushRunnableToQueue(onEdt, runnableInfo);
   }
 
   static void invokeAndWait(@NotNull final Runnable runnable, @NotNull ModalityState modalityState, boolean onEdt) {
@@ -181,8 +180,7 @@ public class LaterInvocator {
       guard.enteredModality(appendedState);
     }
 
-    reincludeSkippedItems();
-    requestFlush();
+    reincludeSkippedItemsAndRequestFlush();
   }
 
   public static void enterModal(Project project, Dialog dialog) {
@@ -216,8 +214,7 @@ public class LaterInvocator {
   @ApiStatus.Internal
   public static void markTransparent(@NotNull ModalityState state) {
     ((ModalityStateEx)state).markTransparent();
-    reincludeSkippedItems();
-    requestFlush();
+    reincludeSkippedItemsAndRequestFlush();
   }
 
   public static void leaveModal(Project project, Dialog dialog) {
@@ -245,8 +242,7 @@ public class LaterInvocator {
       }
     }
 
-    reincludeSkippedItems();
-    requestFlush();
+    reincludeSkippedItemsAndRequestFlush();
   }
 
   private static void removeModality(@NotNull Object modalEntity, int index) {
@@ -274,8 +270,7 @@ public class LaterInvocator {
     LOG.assertTrue(index >= 0);
     removeModality(modalEntity, index);
 
-    reincludeSkippedItems();
-    requestFlush();
+    reincludeSkippedItemsAndRequestFlush();
   }
 
   @TestOnly
@@ -284,8 +279,7 @@ public class LaterInvocator {
       leaveModal(ourModalEntities.get(ourModalEntities.size() - 1));
     }
     LOG.assertTrue(getCurrentModalityState() == ModalityState.NON_MODAL, getCurrentModalityState());
-    reincludeSkippedItems();
-    requestFlush();
+    reincludeSkippedItemsAndRequestFlush();
   }
 
   public static Object @NotNull [] getCurrentModalEntities() {
@@ -390,14 +384,21 @@ public class LaterInvocator {
     return ourWtQueue.getQueue();
   }
 
-  private static void reincludeSkippedItems() {
+  private static void pushRunnableToQueue(boolean onEdt, FlushQueue.RunnableInfo runnableInfo) {
+    getRunnableQueue(onEdt).push(runnableInfo);
+    requestFlush();
+  }
+
+  private static void reincludeSkippedItemsAndRequestFlush() {
     ourEdtQueue.reincludeSkippedItems();
     ourWtQueue.reincludeSkippedItems();
+    requestFlush();
   }
 
   public static void purgeExpiredItems() {
     ourEdtQueue.purgeExpiredItems();
     ourWtQueue.purgeExpiredItems();
+    requestFlush();
   }
 
   @TestOnly
