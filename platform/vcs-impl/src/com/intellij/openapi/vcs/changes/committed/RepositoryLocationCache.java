23d28dbe9588f27b96835f966471bd1747698987
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2020-05-22 23:40:08 +0300

    vcs: Refactor "RepositoryLocationCache" - remove redundant cache reset on mappings change
    "RepositoryLocationCache" will be still reset by listener in "CommittedChangesCache"
    
    GitOrigin-RevId: 8a6209355099f581c33460a6f198a41ae43c1718

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/committed/RepositoryLocationCache.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/committed/RepositoryLocationCache.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vcs.changes.committed;
 
 import com.intellij.openapi.application.ApplicationManager;
@@ -7,15 +7,12 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Couple;
 import com.intellij.openapi.util.ThrowableComputable;
 import com.intellij.openapi.vcs.*;
-import com.intellij.util.messages.MessageBusConnection;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.intellij.openapi.vcs.ProjectLevelVcsManager.VCS_CONFIGURATION_CHANGED;
-import static com.intellij.openapi.vcs.ProjectLevelVcsManager.VCS_CONFIGURATION_CHANGED_IN_PLUGIN;
 import static java.util.Collections.synchronizedMap;
 
 public class RepositoryLocationCache {
@@ -25,11 +22,6 @@ public class RepositoryLocationCache {
 
   public RepositoryLocationCache(@NotNull Project project) {
     myProject = project;
-
-    MessageBusConnection connection = myProject.getMessageBus().connect();
-    VcsListener listener = () -> reset();
-    connection.subscribe(VCS_CONFIGURATION_CHANGED, listener);
-    connection.subscribe(VCS_CONFIGURATION_CHANGED_IN_PLUGIN, listener);
   }
 
   @Nullable
