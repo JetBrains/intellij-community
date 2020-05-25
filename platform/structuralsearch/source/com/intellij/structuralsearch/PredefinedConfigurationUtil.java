76f4d19276df35d903d8c3f98bedfdb75689da3b
Bas Leijdekkers <basleijdekkers@gmail.com>
2020-05-25 15:52:26 +0200

    SSR: don't create java existing templates for sql profile
    GitOrigin-RevId: 49576ea3f7e04126a7950a89f4e20d7b11071be2

--- platform/structuralsearch/source/com/intellij/structuralsearch/PredefinedConfigurationUtil.java
+++ platform/structuralsearch/source/com/intellij/structuralsearch/PredefinedConfigurationUtil.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.structuralsearch;
 
 import com.intellij.openapi.fileTypes.LanguageFileType;
@@ -9,6 +9,12 @@ import org.jetbrains.annotations.NonNls;
 
 public class PredefinedConfigurationUtil {
 
+  /**
+   * @deprecated this creates a Java template, which is most likely not what you need. Use
+   * {@link #createSearchTemplateInfo(java.lang.String, java.lang.String, java.lang.String, com.intellij.openapi.fileTypes.LanguageFileType)}
+   * instead.
+   */
+  @Deprecated
   public static Configuration createSearchTemplateInfo(String name, @NonNls String criteria, String category) {
     return createSearchTemplateInfo(name, criteria, category, StdFileTypes.JAVA);
   }
@@ -34,6 +40,10 @@ public class PredefinedConfigurationUtil {
     return config;
   }
 
+  /**
+   * @deprecated this creates a Java template, which is most likely not what you need.
+   */
+  @Deprecated
   public static Configuration createSearchTemplateInfoSimple(String name, @NonNls String criteria, String category) {
     final Configuration info = createSearchTemplateInfo(name,criteria,category);
     info.getMatchOptions().setRecursiveSearch(false);
