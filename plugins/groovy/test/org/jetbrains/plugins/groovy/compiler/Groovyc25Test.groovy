b6c56ca0b3c83fb60754b01e7d1995be47144e29
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2020-05-25 19:13:17 +0300

    [groovy] fix @Bombed annotations in Groovyc25Test
    GitOrigin-RevId: 3348c7953b2df0489b9b2485351dc4bc006fa34d

--- plugins/groovy/test/org/jetbrains/plugins/groovy/compiler/Groovyc25Test.groovy
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/compiler/Groovyc25Test.groovy
@@ -14,49 +14,49 @@ class Groovyc25Test extends GroovycTestBase {
     GroovyProjectDescriptors.LIB_GROOVY_2_5
   }
 
-  @Bombed(year = 2029)
+  @Bombed(user = 'daniil', year = 2029, month = Calendar.JANUARY, day = 4)
   @Override
   void testClassLoadingDuringBytecodeGeneration() {
     super.testClassLoadingDuringBytecodeGeneration()
   }
 
-  @Bombed(year = 2029)
+  @Bombed(user = 'daniil', year = 2029, month = Calendar.JANUARY, day = 4)
   @Override
   void "test inner java class references with incremental recompilation"() {
     super.'test inner java class references with incremental recompilation'()
   }
 
-  @Bombed(year = 2029)
+  @Bombed(user = 'daniil', year = 2029, month = Calendar.JANUARY, day = 4)
   @Override
   void testTransitiveJavaDependencyThroughGroovy() {
     super.testTransitiveJavaDependencyThroughGroovy()
   }
 
-  @Bombed(year = 2029)
+  @Bombed(user = 'daniil', year = 2029, month = Calendar.JANUARY, day = 4)
   @Override
   void testMakeInDependentModuleAfterChunkRebuild() {
     super.testMakeInDependentModuleAfterChunkRebuild()
   }
 
-  @Bombed(year = 2029)
+  @Bombed(user = 'daniil', year = 2029, month = Calendar.JANUARY, day = 4)
   @Override
   void "test extend groovy classes with additional dependencies"() {
     super.'test extend groovy classes with additional dependencies'()
   }
 
-  @Bombed(year = 2029)
+  @Bombed(user = 'daniil', year = 2029, month = Calendar.JANUARY, day = 4)
   @Override
   void testStubForGroovyExtendingJava() {
     super.testStubForGroovyExtendingJava()
   }
 
-  @Bombed(year = 2029)
+  @Bombed(user = 'daniil', year = 2029, month = Calendar.JANUARY, day = 4)
   @Override
   void "test recompile one file that triggers chunk rebuild inside"() {
     super.'test recompile one file that triggers chunk rebuild inside'()
   }
 
-  @Bombed(year = 2029)
+  @Bombed(user = 'daniil', year = 2029, month = Calendar.JANUARY, day = 4)
   @Override
   void testPartialCrossRecompile() {
     super.testPartialCrossRecompile()
