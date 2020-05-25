6bd372c341c35e5e186e893d36aea51422cc6451
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2020-05-25 12:35:14 +0700

    TypeConstraints: presentation for anonymous and local classes
    GitOrigin-RevId: 36fc9a0b34ea1c213fe7f2d84a153e0cb41e54f0

--- java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/TypeConstraints.java
+++ java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/TypeConstraints.java
@@ -324,8 +324,15 @@ public class TypeConstraints {
     @NotNull
     @Override
     public String toString() {
-      // TODO: support anonymous classes
-      return String.valueOf(myClass.getQualifiedName());
+      String name = myClass.getQualifiedName();
+      if (name == null) {
+        name = myClass.getName();
+      }
+      if (name == null && myClass instanceof PsiAnonymousClass) {
+        PsiClassType baseClassType = ((PsiAnonymousClass)myClass).getBaseClassType();
+        name = "anonymous " + createExact(baseClassType);
+      }
+      return String.valueOf(name);
     }
 
     @Override
