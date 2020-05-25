ea39d94a5fe243c6d9a42da7f51de7649bd0bb18
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2020-05-25 21:13:44 +0300

    Implement base version of FeatureSuggestersManager that can catch line commenting.
    IJ-CR-14522
    
    GitOrigin-RevId: e7b76bd61583590c22b9791b35b38c52b7670138

--- plugins/ide-features-trainer/src/main/kotlin/org/jetbrains/plugins/feature/suggester/FeatureSuggestersManager.kt
+++ plugins/ide-features-trainer/src/main/kotlin/org/jetbrains/plugins/feature/suggester/FeatureSuggestersManager.kt
@@ -1,25 +1,77 @@
 package org.jetbrains.plugins.feature.suggester
 
+import com.intellij.openapi.fileEditor.FileEditor
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.fileEditor.FileEditorManagerListener
+import com.intellij.openapi.fileEditor.FileEditorProvider
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.project.impl.ProjectLifecycleListener
+import com.intellij.openapi.util.Pair
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiManager
 import com.intellij.psi.PsiTreeChangeAdapter
 import com.intellij.psi.PsiTreeChangeEvent
 import org.jetbrains.plugins.feature.suggester.cache.UserActionsCache
 import org.jetbrains.plugins.feature.suggester.cache.UserAnActionsCache
+import org.jetbrains.plugins.feature.suggester.changes.*
+import org.jetbrains.plugins.feature.suggester.settings.FeatureSuggesterSettings
 
-class FeatureSuggestersManager(val project: Project) : ProjectLifecycleListener {
-
+class FeatureSuggestersManager(val project: Project) : FileEditorManagerListener {
     private val MAX_ACTIONS_NUMBER: Int = 100
     private val actionsCache = UserActionsCache(MAX_ACTIONS_NUMBER)
     private val anActionsCache = UserAnActionsCache(MAX_ACTIONS_NUMBER)
 
+    private var psiListenersIsSet: Boolean = false
 
-    // TODO set PSI listeners in another phase of project lifecycle
-    override fun beforeProjectLoaded(project: Project) {
-        if (this.project.locationHash != project.locationHash) {
-            return
+    fun actionPerformed(action: UserAction) {
+        actionsCache.add(action)
+        for (suggester in FeatureSuggester.suggesters) {
+            if (!isEnabled(suggester)) continue
+            val suggestion = suggester.getSuggestion(actionsCache, anActionsCache)
+            if (suggestion is PopupSuggestion) {
+                println("Action performed: ${suggestion.message}")
+            }
         }
+    }
 
+    override fun fileOpenedSync(
+        source: FileEditorManager,
+        file: VirtualFile,
+        editors: Pair<Array<FileEditor>, Array<FileEditorProvider>>
+    ) {
+        if(project != source.project || psiListenersIsSet)
+            return
+
+        PsiManager.getInstance(project).addPsiTreeChangeListener(object : PsiTreeChangeAdapter() {
+            override fun propertyChanged(event: PsiTreeChangeEvent) {
+                actionPerformed(PropertyChangedAction(event.parent))
+            }
+
+            override fun childRemoved(event: PsiTreeChangeEvent) {
+                actionPerformed(ChildRemovedAction(event.parent, event.child))
+            }
+
+            override fun childReplaced(event: PsiTreeChangeEvent) {
+                actionPerformed(ChildReplacedAction(event.parent, event.newChild, event.oldChild))
+            }
+
+            override fun childAdded(event: PsiTreeChangeEvent) {
+                actionPerformed(ChildAddedAction(event.parent, event.newChild))
+            }
+
+            override fun childrenChanged(event: PsiTreeChangeEvent) {
+                actionPerformed(ChildrenChangedAction(event.parent))
+            }
+
+            override fun childMoved(event: PsiTreeChangeEvent) {
+                actionPerformed(ChildMovedAction(event.parent, event.child, event.oldParent))
+            }
+        })
+
+        psiListenersIsSet = true
+        println("PSI listeners have set")
+    }
+
+    private fun isEnabled(suggester: FeatureSuggester): Boolean {
+        return FeatureSuggesterSettings.isEnabled(suggester.getId())
     }
 }
