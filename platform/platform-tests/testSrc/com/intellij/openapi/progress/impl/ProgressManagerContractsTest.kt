388431a992fcd6002c4790b0ea855ba98efa761a
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2020-05-26 11:11:59 +0200

    ProgressManagerContractsTest - add more cases, simplify, IDEA-241785
    GitOrigin-RevId: 6ab566e4ecc4ee538dc0f495ab0588eb6a03f863

--- platform/platform-tests/testSrc/com/intellij/openapi/progress/impl/ProgressManagerContractsTest.kt
+++ platform/platform-tests/testSrc/com/intellij/openapi/progress/impl/ProgressManagerContractsTest.kt
@@ -1,11 +1,11 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.progress.impl
 
+import com.intellij.idea.TestFor
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.Task
 import com.intellij.testFramework.LightPlatformTestCase
-import com.intellij.testFramework.assertions.Assertions
 import com.intellij.testFramework.assertions.Assertions.assertThat
 import com.intellij.util.concurrency.AppExecutorUtil
 import com.intellij.util.ui.UIUtil
@@ -13,36 +13,49 @@ import java.util.concurrent.atomic.AtomicInteger
 
 class ProgressManagerContractsTest : LightPlatformTestCase() {
 
+  @TestFor(issues = ["IDEA-241785"])
+  fun `test backgroundable modality in GUI`() = runWithGuiTasksMode { `test backgroundable modality in tests`() }
+
+  @TestFor(issues = ["IDEA-241785"])
   fun `test backgroundable modality in tests`() = repeat(10) {
     require(ApplicationManager.getApplication().isDispatchThread)
-    val callbackResult = `task_backgroundable vs invokeLater`()
-    assertThat(callbackResult).isEqualTo("fromProgress.invokeLater.")
-  }
+    var callbackResult = ""
 
-  fun `test backgroundable modality in GUI`() = repeat(10) {
-    require(ApplicationManager.getApplication().isDispatchThread)
-    runWithGuiTasksMode {
-      val callbackResult = `task_backgroundable vs invokeLater`()
-      assertThat(callbackResult).isEqualTo("invokeLater.fromProgress.")
+    val taskCompleted = AtomicInteger(0)
+    ApplicationManager.getApplication().invokeLater {
+      callbackResult += "invokeLater."
+      taskCompleted.incrementAndGet()
     }
+
+    object : Task.Backgroundable(project, "mock", true) {
+      override fun run(indicator: ProgressIndicator) {
+        // we add the next callbackResult update operation.
+        ApplicationManager.getApplication().invokeAndWait {
+          callbackResult += "fromProgress."
+          taskCompleted.incrementAndGet()
+        }
+      }
+
+      override fun onFinished() {
+        super.onFinished()
+        taskCompleted.incrementAndGet()
+      }
+    }.queue()
+
+    flushEdtEvents { taskCompleted.get() < 3 }
+
+    assertThat(callbackResult).isEqualTo("invokeLater.fromProgress.")
   }
 
-  fun `test invoke later from pooled thread in tests`() = repeat(10) {
+  @TestFor(issues = ["IDEA-241785"])
+  fun `test invokeLater from pooled thread & modal in GUI`() = runWithGuiTasksMode {
+    `test invokeLater from pooled thread & modal in tests`()
+  }
+
+  @TestFor(issues = ["IDEA-241785"])
+  fun `test invokeLater from pooled thread & modal in tests`() = repeat(10) {
     require(ApplicationManager.getApplication().isDispatchThread)
 
-    val callbackResult = `task_modal vs pooled thread`()
-    assertThat(callbackResult).isEqualTo("fromProgress.1.fromProgress.2.fromPool.")
-  }
-
-  fun `test invoke later from pooled thread in GUI`() = repeat(10) {
-    require(ApplicationManager.getApplication().isDispatchThread)
-    runWithGuiTasksMode {
-      val callbackResult = `task_modal vs pooled thread`()
-      assertThat(callbackResult).isEqualTo("fromProgress.1.fromProgress.2.fromPool.")
-    }
-  }
-
-  private fun `task_modal vs pooled thread`() : String {
     var callbackResult = ""
     val taskCompleted = AtomicInteger(0)
 
@@ -54,7 +67,7 @@ class ProgressManagerContractsTest : LightPlatformTestCase() {
           taskCompleted.incrementAndGet()
         }
 
-        //this will post a task from a pooled thread to the EDT
+        //this will post a task from a pooled thread, which may not inherit the ModalityState to the EDT
         AppExecutorUtil.getAppExecutorService().submit {
           ApplicationManager.getApplication().invokeLater {
             callbackResult += "fromPool."
@@ -75,29 +88,40 @@ class ProgressManagerContractsTest : LightPlatformTestCase() {
     }.queue()
 
     flushEdtEvents { taskCompleted.get() < 3 }
-
-    return callbackResult
+    assertThat(callbackResult).isEqualTo("fromProgress.1.fromProgress.2.fromPool.")
   }
 
-  private fun `task_backgroundable vs invokeLater`(): String {
+  @TestFor(issues = ["IDEA-241785"])
+  fun `test invokeLater from pooled thread & background in GUI`() = runWithGuiTasksMode {
+    `test invokeLater from pooled thread & background in tests`()
+  }
+
+  @TestFor(issues = ["IDEA-241785"])
+  fun `test invokeLater from pooled thread & background in tests`() = repeat(10) {
+    require(ApplicationManager.getApplication().isDispatchThread)
+
     var callbackResult = ""
     val taskCompleted = AtomicInteger(0)
 
-    // 1. queue a default command via invokeLater
-    ApplicationManager.getApplication().invokeLater {
-      callbackResult += "invokeLater."
-      taskCompleted.incrementAndGet()
-    }
-
-    // 2. submit a backgroundable task (that may want to access the invokeLater side-effects
+    //we start a BACKGROUNDABLE task
     object : Task.Backgroundable(project, "mock", true) {
       override fun run(indicator: ProgressIndicator) {
-        // we add the next callbackResult update operation.
-        // now it depends from the context:
-        // - [for tests]. this will run first
-        // - [for gui]. this will run the second!
+        // ensure the messages queue is flushed
         ApplicationManager.getApplication().invokeAndWait {
-          callbackResult += "fromProgress."
+          callbackResult += "fromProgress.1."
+          taskCompleted.incrementAndGet()
+        }
+
+        //this will post a task from a pooled thread, which may not inherit the ModalityState to the EDT
+        AppExecutorUtil.getAppExecutorService().submit {
+          ApplicationManager.getApplication().invokeLater {
+            callbackResult += "fromPool."
+          }
+        }.get()
+
+        // flush messages to see if we have it here or not
+        ApplicationManager.getApplication().invokeAndWait {
+          callbackResult += "fromProgress.2."
           taskCompleted.incrementAndGet()
         }
       }
@@ -109,8 +133,7 @@ class ProgressManagerContractsTest : LightPlatformTestCase() {
     }.queue()
 
     flushEdtEvents { taskCompleted.get() < 3 }
-
-    return callbackResult
+    assertThat(callbackResult).isEqualTo("fromProgress.1.fromPool.fromProgress.2.")
   }
 
   private fun flushEdtEvents(predicate: () -> Boolean) {
@@ -124,7 +147,6 @@ class ProgressManagerContractsTest : LightPlatformTestCase() {
 
     UIUtil.dispatchAllInvocationEvents()
   }
-
 
   private inline fun <Y> runWithGuiTasksMode(action: () -> Y): Y {
     val key = "intellij.progress.task.ignoreHeadless"
