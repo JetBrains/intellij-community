0c57485914332c658f2bf072c46d54034bd1ca64
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2020-05-26 20:51:21 +0300

    [devkit] fix "Update IDE from Sources" action: add proper groovy version to classpath
    The action was broken after updating to Groovy 2.5 because paths to libraries were hardcoded. Now they are taken from the project configuration, and groovy-cli-commons is added to classpath (it contains groovy.util.CliBuilder which is used by Gant). (Dependency on groovy-cli-commons in intellij.idea.ultimate.build should be removed after we update jps-build-script-dependencies-bootstrap library.)
    
    GitOrigin-RevId: 728ca2e1a00f920d4d8203436d57c984d8daf57d

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/CommunityLibraryLicenses.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/CommunityLibraryLicenses.groovy
@@ -247,6 +247,8 @@ class CommunityLibraryLicenses {
                        license: "Apache 2.0", url: "https://groovy-lang.org/"),
     new LibraryLicense(name: "Groovy Ant", libraryName: "org.codehaus.groovy:groovy-ant",
                        license: "Apache 2.0", url: "https://groovy-lang.org/"),
+    new LibraryLicense(name: "Groovy CLI Commons", libraryName: "org.codehaus.groovy:groovy-cli-commons",
+                       license: "Apache 2.0", url: "https://groovy-lang.org/"),
     new LibraryLicense(name: "Groovy JSON", libraryName: "org.codehaus.groovy:groovy-json",
                        license: "Apache 2.0", url: "https://groovy-lang.org/"),
     new LibraryLicense(name: "Groovy JSR-223", libraryName: "org.codehaus.groovy:groovy-jsr223",
