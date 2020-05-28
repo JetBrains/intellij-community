90682b0de3cf7c4eb0069708c6cd2d8460743638
Nadya Zabrodina <Nadya.Zabrodina@jetbrains.com>
2020-05-28 14:10:08 +0300

    welcome screen: add errors notification panel
    GitOrigin-RevId: 7b6a28b9e598496059285153bf4eaeee2c64fa9e

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/ProjectsTabFactory.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/ProjectsTabFactory.java
@@ -6,12 +6,15 @@ import com.intellij.ide.IdeBundle;
 import com.intellij.ide.RecentProjectListActionProvider;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.actionSystem.ex.ActionButtonLook;
+import com.intellij.openapi.actionSystem.impl.ActionButton;
 import com.intellij.openapi.actionSystem.impl.ActionToolbarImpl;
 import com.intellij.openapi.wm.WelcomeScreenTab;
 import com.intellij.openapi.wm.WelcomeTabFactory;
 import com.intellij.ui.JBColor;
 import com.intellij.ui.SearchTextField;
 import com.intellij.ui.border.CustomLineBorder;
+import com.intellij.ui.components.panels.NonOpaquePanel;
 import com.intellij.util.ui.JBUI;
 import org.jetbrains.annotations.NotNull;
 
@@ -20,6 +23,8 @@ import java.awt.*;
 
 import static com.intellij.openapi.actionSystem.impl.ActionButton.HIDE_DROPDOWN_ICON;
 import static com.intellij.openapi.wm.impl.welcomeScreen.WelcomeScreenComponentFactory.ToolbarTextButtonWrapper.wrapAsTextButton;
+import static com.intellij.openapi.wm.impl.welcomeScreen.WelcomeScreenComponentFactory.createErrorsLink;
+import static com.intellij.openapi.wm.impl.welcomeScreen.WelcomeScreenUIManager.getProjectsBackground;
 
 public class ProjectsTabFactory implements WelcomeTabFactory {
 
@@ -34,37 +39,39 @@ public class ProjectsTabFactory implements WelcomeTabFactory {
         if (RecentProjectListActionProvider.getInstance().getActions(false, true).isEmpty()) {
           return new EmptyStateProjectsPanel();
         }
-        JPanel mainPanel = JBUI.Panels.simplePanel().withBorder(JBUI.Borders.empty(13, 12))
-          .withBackground(WelcomeScreenUIManager.getProjectsBackground());
-
+        JPanel mainPanel = JBUI.Panels.simplePanel().withBorder(JBUI.Borders.empty(13, 12)).withBackground(getProjectsBackground());
+        final SearchTextField projectSearch = createSearchProjectsField();
         NewRecentProjectPanel projectsPanel = new NewRecentProjectPanel(parentDisposable);
         projectsPanel.setBorder(JBUI.Borders.emptyTop(10));
 
-        JPanel northPanel = JBUI.Panels.simplePanel();
-        northPanel.setOpaque(false);
-        northPanel.setBorder(new CustomLineBorder(JBColor.border(), JBUI.insetsBottom(1)) {
-          @Override
-          public Insets getBorderInsets(Component c) {
-            return JBUI.insetsBottom(12);
-          }
-        });
+        JPanel northPanel =
+          JBUI.Panels.simplePanel().andTransparent().withBorder(new CustomLineBorder(JBColor.border(), JBUI.insetsBottom(1)) {
+            @Override
+            public Insets getBorderInsets(Component c) {
+              return JBUI.insetsBottom(12);
+            }
+          });
 
+        JComponent projectActionsPanel = createActionsToolbar().getComponent();
+
+        northPanel.add(projectSearch, BorderLayout.CENTER);
+        northPanel.add(projectActionsPanel, BorderLayout.EAST);
+        mainPanel.add(northPanel, BorderLayout.NORTH);
+        mainPanel.add(projectsPanel, BorderLayout.CENTER);
+        mainPanel.add(createNotificationsPanel(parentDisposable), BorderLayout.SOUTH);
+
+        return mainPanel;
+      }
+
+      @NotNull
+      private SearchTextField createSearchProjectsField() {
         SearchTextField projectSearch = new SearchTextField(false);
         projectSearch.setOpaque(false);
         projectSearch.setBorder(JBUI.Borders.empty());
         projectSearch.getTextEditor().setOpaque(false);
         projectSearch.getTextEditor().setBorder(JBUI.Borders.empty());
         projectSearch.getTextEditor().getEmptyText().setText(IdeBundle.message("welcome.screen.search.projects.empty.text"));
-        projectSearch.setMinimumSize(new Dimension(100, 20));
-
-        JComponent projectActionsPanel = createActionsToolbar().getComponent();
-
-        northPanel.add(projectSearch, BorderLayout.WEST);
-        northPanel.add(projectActionsPanel, BorderLayout.EAST);
-        mainPanel.add(northPanel, BorderLayout.NORTH);
-        mainPanel.add(projectsPanel, BorderLayout.CENTER);
-
-        return mainPanel;
+        return projectSearch;
       }
 
       @NotNull
@@ -93,6 +100,13 @@ public class ProjectsTabFactory implements WelcomeTabFactory {
         ActionToolbarImpl toolbar = new ActionToolbarImpl(ActionPlaces.WELCOME_SCREEN, toolbarActionGroup, true);
         toolbar.setOpaque(false);
         return toolbar;
+      }
+
+      private JPanel createNotificationsPanel(@NotNull Disposable parentDisposable) {
+        JPanel notificationsPanel = new NonOpaquePanel(new FlowLayout(FlowLayout.RIGHT));
+        notificationsPanel.setBorder(JBUI.Borders.emptyTop(10));
+        notificationsPanel.add(createErrorsLink(parentDisposable));
+        return notificationsPanel;
       }
     };
   }
