a7a33c79e124377e22dbb096fb6db98cc568af2d
Alex Plate <aleksei.plate@jetbrains.com>
2020-05-29 19:27:27 +0300

    [Workspace Model] Put proxy entities to proxy test
    GitOrigin-RevId: 1ffe23f4976cb31e8ba04ebca3df2e8c920be745

--- platform/workspaceModel-core-tests/test/com/intellij/workspace/api/EntityWithPersistentIdInProxyBasedStorageTest.kt
+++ platform/workspaceModel-core-tests/test/com/intellij/workspace/api/EntityWithPersistentIdInProxyBasedStorageTest.kt
@@ -1,21 +1,58 @@
 package com.intellij.workspace.api
 
-import com.intellij.workspace.api.pstorage.entities.ModifiableChildEntityWithPersistentId
-import com.intellij.workspace.api.pstorage.entities.ModifiableNamedSampleEntity
-import com.intellij.workspace.api.pstorage.entities.SampleEntityId
-import com.intellij.workspace.api.pstorage.entities.addNamedEntity
+import com.intellij.workspace.api.pstorage.entities.PSampleEntitySource
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNull
 import org.junit.Test
+
+internal interface ProxyNamedSampleEntity : TypedEntityWithPersistentId {
+  val name: String
+  val next: ProxySampleEntityId
+
+  @JvmDefault
+  override fun persistentId(): ProxySampleEntityId = ProxySampleEntityId(name)
+}
+
+internal data class ProxySampleEntityId(val name: String) : PersistentEntityId<ProxyNamedSampleEntity>() {
+  override val parentId: PersistentEntityId<*>?
+    get() = null
+  override val presentableName: String
+    get() = name
+}
+
+internal interface ModifiableProxyNamedSampleEntity : ModifiableTypedEntity<ProxyNamedSampleEntity>, ProxyNamedSampleEntity {
+  override var name: String
+  override var next: ProxySampleEntityId
+}
+
+internal data class ProxyChildEntityId(val childName: String,
+                                       override val parentId: ProxySampleEntityId) : PersistentEntityId<ModifiableChildEntityWithPersistentId>() {
+  override val presentableName: String
+    get() = childName
+}
+
+internal interface ModifiableChildEntityWithPersistentId : ModifiableTypedEntity<ModifiableChildEntityWithPersistentId>, TypedEntityWithPersistentId {
+  var parent: ProxyNamedSampleEntity
+  var childName: String
+
+  @JvmDefault
+  override fun persistentId(): PersistentEntityId<*> = ProxyChildEntityId(childName, parent.persistentId())
+}
+
+internal fun TypedEntityStorageBuilder.addProxyNamedEntity(name: String, next: ProxySampleEntityId) =
+  addEntity(ModifiableProxyNamedSampleEntity::class.java, PSampleEntitySource("test")) {
+    this.name = name
+    this.next = next
+  }
 
 class EntityWithPersistentIdInProxyBasedStorageTest {
   @Test
   fun `add remove entity`() {
     val builder = TypedEntityStorageBuilder.createProxy()
-    val foo = builder.addNamedEntity("foo", SampleEntityId("bar"))
+    val foo = builder.addProxyNamedEntity("foo", ProxySampleEntityId("bar"))
     builder.checkConsistency()
     assertNull(foo.next.resolve(builder))
-    val bar = builder.addNamedEntity("bar", SampleEntityId("baz"))
+    val bar = builder.addProxyNamedEntity("bar", ProxySampleEntityId("baz"))
     builder.checkConsistency()
     assertEquals(bar, foo.next.resolve(builder))
     builder.removeEntity(bar)
@@ -26,11 +63,11 @@ class EntityWithPersistentIdInProxyBasedStorageTest {
   @Test
   fun `change target entity name`() {
     val builder = TypedEntityStorageBuilder.createProxy()
-    val foo = builder.addNamedEntity("foo", SampleEntityId("bar"))
-    val bar = builder.addNamedEntity("bar", SampleEntityId("baz"))
+    val foo = builder.addProxyNamedEntity("foo", ProxySampleEntityId("bar"))
+    val bar = builder.addProxyNamedEntity("bar", ProxySampleEntityId("baz"))
     builder.checkConsistency()
     assertEquals(bar, foo.next.resolve(builder))
-    builder.modifyEntity(ModifiableNamedSampleEntity::class.java, bar) {
+    builder.modifyEntity(ModifiableProxyNamedSampleEntity::class.java, bar) {
       name = "baz"
     }
     builder.checkConsistency()
@@ -40,13 +77,13 @@ class EntityWithPersistentIdInProxyBasedStorageTest {
   @Test
   fun `change name in reference`() {
     val builder = TypedEntityStorageBuilder.createProxy()
-    val foo = builder.addNamedEntity("foo", SampleEntityId("bar"))
-    val bar = builder.addNamedEntity("bar", SampleEntityId("baz"))
-    val baz = builder.addNamedEntity("baz", SampleEntityId("foo"))
+    val foo = builder.addProxyNamedEntity("foo", ProxySampleEntityId("bar"))
+    val bar = builder.addProxyNamedEntity("bar", ProxySampleEntityId("baz"))
+    val baz = builder.addProxyNamedEntity("baz", ProxySampleEntityId("foo"))
     builder.checkConsistency()
     assertEquals(bar, foo.next.resolve(builder))
-    val newFoo = builder.modifyEntity(ModifiableNamedSampleEntity::class.java, foo) {
-      next = SampleEntityId("baz")
+    val newFoo = builder.modifyEntity(ModifiableProxyNamedSampleEntity::class.java, foo) {
+      next = ProxySampleEntityId("baz")
     }
     builder.checkConsistency()
     assertEquals(baz, newFoo.next.resolve(builder))
@@ -55,13 +92,14 @@ class EntityWithPersistentIdInProxyBasedStorageTest {
   @Test
   fun `remove child entity with parent entity`() {
     val builder = TypedEntityStorageBuilder.createProxy()
-    val parent = builder.addNamedEntity("parent", SampleEntityId("no"))
+    val parent = builder.addProxyNamedEntity("parent", ProxySampleEntityId("no"))
     builder.addEntity(ModifiableChildEntityWithPersistentId::class.java, SampleEntitySource("foo")) {
       this.childName = "child"
       this.parent = parent
     }
     builder.checkConsistency()
     builder.removeEntity(parent)
-    assertEquals(emptyList<ModifiableChildEntityWithPersistentId>(), builder.entities(ModifiableChildEntityWithPersistentId::class.java).toList())
+    assertEquals(emptyList<ModifiableChildEntityWithPersistentId>(),
+                 builder.entities(ModifiableChildEntityWithPersistentId::class.java).toList())
   }
 }
