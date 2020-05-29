c75b09ec12915dcb0409cb3194269c3bbaa694e9
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2020-05-29 17:30:40 +0300

    [workspace model] fire rootsChanged event only if configuration of modules or libraries was changed
    Do not fire it if only facets were changed, for example.
    
    GitOrigin-RevId: 8461dc121709336a40df8da3163b4fde63f45a2f

--- platform/workspaceModel-ide/src/com/intellij/workspace/legacyBridge/intellij/LegacyBridgeProjectRootsChangeListener.kt
+++ platform/workspaceModel-ide/src/com/intellij/workspace/legacyBridge/intellij/LegacyBridgeProjectRootsChangeListener.kt
@@ -7,17 +7,14 @@ import com.intellij.openapi.roots.LibraryOrderEntry
 import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.util.Disposer
-import com.intellij.workspace.api.EntityChange
-import com.intellij.workspace.api.EntityStoreChanged
-import com.intellij.workspace.api.LibraryEntity
-import com.intellij.workspace.ide.WorkspaceModelChangeListener
+import com.intellij.workspace.api.*
 
 internal class LegacyBridgeProjectRootsChangeListener(private val project: Project) {
   fun beforeChanged(event: EntityStoreChanged) {
     if (project.isDisposed || Disposer.isDisposing(project)) return
     val projectRootManager = ProjectRootManager.getInstance(project)
     if (projectRootManager !is LegacyBridgeProjectRootManager) return
-    val performUpdate = processChanges(event, project)
+    val performUpdate = shouldFireRootsChanged(event, project)
     if (performUpdate) projectRootManager.fireRootsChanged(true)
   }
 
@@ -25,30 +22,36 @@ internal class LegacyBridgeProjectRootsChangeListener(private val project: Proje
     if (project.isDisposed || Disposer.isDisposing(project)) return
     val projectRootManager = ProjectRootManager.getInstance(project)
     if (projectRootManager !is LegacyBridgeProjectRootManager) return
-    val performUpdate = processChanges(event, project)
+    val performUpdate = shouldFireRootsChanged(event, project)
     if (performUpdate) projectRootManager.fireRootsChanged(false)
   }
 
-  // Library changes should not fire any events if the library is not included in any of order entries
-  private fun processChanges(events: EntityStoreChanged, project: Project): Boolean {
-    val libraryChanges = events.getChanges(LibraryEntity::class.java)
-    return if (libraryChanges.count() == events.getAllChanges().count()) {
-      for (event in libraryChanges) {
-        val res = when (event) {
-          is EntityChange.Added -> libraryHasOrderEntry(event.entity.name, project)
-          is EntityChange.Removed -> libraryHasOrderEntry(event.entity.name, project)
-          is EntityChange.Replaced -> libraryHasOrderEntry(event.newEntity.name, project)
-        }
-        if (res) return true
+  private fun shouldFireRootsChanged(events: EntityStoreChanged, project: Project): Boolean {
+    return events.getAllChanges().any {
+      val entity = when (it) {
+        is EntityChange.Added -> it.entity
+        is EntityChange.Removed -> it.entity
+        is EntityChange.Replaced -> it.newEntity
       }
-      return false
+      when (entity) {
+        // Library changes should not fire any events if the library is not included in any of order entries
+        is LibraryEntity -> libraryHasOrderEntry(entity, project)
+        is LibraryPropertiesEntity -> libraryHasOrderEntry(entity.library, project)
+        is ModuleEntity, is JavaModuleSettingsEntity, is ModuleCustomImlDataEntity, is ModuleGroupPathEntity,
+          is SourceRootEntity, is JavaSourceRootEntity, is JavaResourceRootEntity, is CustomSourceRootPropertiesEntity,
+          is ContentRootEntity -> true
+        else -> false
+      }
     }
-    else true
   }
 
-  private fun libraryHasOrderEntry(name: String, project: Project): Boolean {
+  private fun libraryHasOrderEntry(library: LibraryEntity, project: Project): Boolean {
+    if (library.tableId is LibraryTableId.ModuleLibraryTableId) {
+      return true
+    }
+    val libraryName = library.name
     ModuleManager.getInstance(project).modules.forEach { module ->
-      val exists = ModuleRootManager.getInstance(module).orderEntries.any { it is LibraryOrderEntry && it.libraryName == name }
+      val exists = ModuleRootManager.getInstance(module).orderEntries.any { it is LibraryOrderEntry && it.libraryName == libraryName }
       if (exists) return true
     }
     return false
