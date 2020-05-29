f1b3ac014d793511e0ae4137d47e098c9483a8d0
Alex Plate <aleksei.plate@jetbrains.com>
2020-05-29 18:03:54 +0300

    [Workspace Model] Extract entities with soft links
    GitOrigin-RevId: a2d3f2669e7a9e008ebbe891c6d9102c20afd3d8

---
+++ platform/workspaceModel-core-tests/test/com/intellij/workspace/api/pstorage/entities/EntitiesWithSoftLinks.kt
@@ -0,0 +1,69 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+@file:Suppress("unused")
+
+package com.intellij.workspace.api.pstorage.entities
+
+import com.intellij.workspace.api.PersistentEntityId
+import com.intellij.workspace.api.TypedEntityStorage
+import com.intellij.workspace.api.TypedEntityWithPersistentId
+import com.intellij.workspace.api.pstorage.*
+
+// ------------------------------ Persistent Id ---------------
+
+internal data class NameId(private val name: String) : PersistentEntityId<NamedEntity>() {
+  override val parentId: PersistentEntityId<*>?
+    get() = null
+  override val presentableName: String
+    get() = name
+
+  override fun toString(): String = name
+}
+
+// ------------------------------ Entity With Persistent Id ------------------
+
+internal class NamedEntityData : PEntityData.WithCalculatablePersistentId<NamedEntity>() {
+
+  lateinit var name: String
+  override fun createEntity(snapshot: TypedEntityStorage): NamedEntity {
+    return NamedEntity(name).also { addMetaData(it, snapshot) }
+  }
+
+  override fun persistentId(): PersistentEntityId<*> = NameId(name)
+
+}
+
+internal class NamedEntity(val name: String) : PTypedEntity(), TypedEntityWithPersistentId {
+
+  override fun persistentId(): PersistentEntityId<*> = NameId(name)
+}
+
+internal class ModifiableNamedEntity : PModifiableTypedEntity<NamedEntity>() {
+  var name: String by EntityDataDelegation()
+}
+
+// ------------------------------ Entity with soft link --------------------
+
+internal class WithSoftLinkEntityData : PEntityData<WithSoftLinkEntity>(), PSoftLinkable {
+
+  lateinit var link: NameId
+
+  override fun createEntity(snapshot: TypedEntityStorage): WithSoftLinkEntity {
+    return WithSoftLinkEntity(link).also { addMetaData(it, snapshot) }
+  }
+
+  override fun getLinks(): List<PersistentEntityId<*>> = listOf(link)
+
+  override fun updateLink(oldLink: PersistentEntityId<*>,
+                          newLink: PersistentEntityId<*>,
+                          affectedIds: MutableList<Pair<PersistentEntityId<*>, PersistentEntityId<*>>>): Boolean {
+    this.link = newLink as NameId
+    return true
+  }
+
+}
+
+internal class WithSoftLinkEntity(val link: NameId) : PTypedEntity()
+
+internal class ModifiableWithSoftLinkEntity : PModifiableTypedEntity<WithSoftLinkEntity>() {
+  var link: NameId by EntityDataDelegation()
+}
