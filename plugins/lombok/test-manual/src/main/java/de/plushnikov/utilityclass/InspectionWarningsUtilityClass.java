b36ea806d36daafc70e19d7d509fea08e34bc9d6
Michail Plushnikov <mplushnikov@gmail.com>
2020-06-01 17:45:59 +0200

    Make inspection for "@UtilityClass already marks methods static" disabled by default fixed #800
    GitOrigin-RevId: 918f2280510045666e62a0cd1a831413a0e87ea4

---
+++ plugins/lombok/test-manual/src/main/java/de/plushnikov/utilityclass/InspectionWarningsUtilityClass.java
@@ -0,0 +1,11 @@
+package de.plushnikov.utilityclass;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class InspectionWarningsUtilityClass {
+
+  public static String someStringResult() {
+    return "someStringResult";
+  }
+}
