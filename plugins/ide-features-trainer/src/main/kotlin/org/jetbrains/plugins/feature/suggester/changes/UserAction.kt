96bc5c732be60f927af3bffc50f4790a41d6f9b3
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2020-06-01 16:01:54 +0300

    Mark UserActions classes as data classes
    IJ-CR-14522
    
    GitOrigin-RevId: 9fc31489b8a2ccaedb6a6ce560486decbe8e122a

--- plugins/ide-features-trainer/src/main/kotlin/org/jetbrains/plugins/feature/suggester/changes/UserAction.kt
+++ plugins/ide-features-trainer/src/main/kotlin/org/jetbrains/plugins/feature/suggester/changes/UserAction.kt
@@ -2,16 +2,16 @@ package org.jetbrains.plugins.feature.suggester.changes
 
 import com.intellij.psi.PsiElement
 
-sealed class UserAction(val parent: PsiElement?)
+sealed class UserAction(open val parent: PsiElement?)
 
-class ChildrenChangedAction(parent: PsiElement?) : UserAction(parent)
-class ChildAddedAction(parent: PsiElement?, val newChild: PsiElement?) : UserAction(parent)
-class ChildReplacedAction(parent: PsiElement?, val newChild: PsiElement?, val oldChild: PsiElement?) : UserAction(parent)
-class ChildRemovedAction(parent: PsiElement?, val child: PsiElement?) : UserAction(parent)
-class PropertyChangedAction(parent: PsiElement?) : UserAction(parent)
-class ChildMovedAction(parent: PsiElement?, val child: PsiElement?, val oldParent: PsiElement?) : UserAction(parent)
+data class ChildrenChangedAction(override val parent: PsiElement?) : UserAction(parent)
+data class ChildAddedAction(override val parent: PsiElement?, val newChild: PsiElement?) : UserAction(parent)
+data class ChildReplacedAction(override val parent: PsiElement?, val newChild: PsiElement?, val oldChild: PsiElement?) : UserAction(parent)
+data class ChildRemovedAction(override val parent: PsiElement?, val child: PsiElement?) : UserAction(parent)
+data class PropertyChangedAction(override val parent: PsiElement?) : UserAction(parent)
+data class ChildMovedAction(override val parent: PsiElement?, val child: PsiElement?, val oldParent: PsiElement?) : UserAction(parent)
 
 
-sealed class UserAnAction(val timestamp: Long)
+sealed class UserAnAction(open val timestamp: Long)
 
-class BackspaceAction(val selectedText: String, timestamp: Long) : UserAnAction(timestamp)
+data class BackspaceAction(val selectedText: String, override val timestamp: Long) : UserAnAction(timestamp)
