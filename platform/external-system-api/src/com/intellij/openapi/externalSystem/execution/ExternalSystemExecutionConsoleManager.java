09a95dfb8a066c2b7cbacc41c4062e70a48cb93d
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2020-06-02 11:03:40 +0300

    [Gradle] include exception and try suggestions into Build tw error message IDEA-226613
    GitOrigin-RevId: 6400dab2d419723168541d84e78128803a90fae2

--- platform/external-system-api/src/com/intellij/openapi/externalSystem/execution/ExternalSystemExecutionConsoleManager.java
+++ platform/external-system-api/src/com/intellij/openapi/externalSystem/execution/ExternalSystemExecutionConsoleManager.java
@@ -1,20 +1,7 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.externalSystem.execution;
 
+import com.intellij.execution.filters.Filter;
 import com.intellij.execution.process.ProcessHandler;
 import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.execution.ui.ExecutionConsole;
@@ -24,6 +11,7 @@ import com.intellij.openapi.externalSystem.model.ProjectSystemId;
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTask;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Key;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -53,4 +41,9 @@ public interface ExternalSystemExecutionConsoleManager<
   boolean isApplicableFor(@NotNull ExternalSystemTask task);
 
   AnAction[] getRestartActions(@NotNull ExternalSystemExecutionConsole consoleView);
+
+  @ApiStatus.Experimental
+  default Filter[] getCustomExecutionFilters(@NotNull Project project,
+                                             @NotNull ExternalSystemTask task,
+                                             @Nullable ExecutionEnvironment env) { return Filter.EMPTY_ARRAY; }
 }
