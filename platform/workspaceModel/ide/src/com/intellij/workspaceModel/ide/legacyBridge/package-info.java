45739d468e484f75114667dc0ecd6743362e0ea7
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2020-06-02 11:32:19 +0300

    [workspace model] rearrange classes in intellij.platform.workspaceModel.ide.* modules
    Change root package name to be consistent with module name, move impl classes to separate package, move legacyBridge implementations together.
    
    This is the first step, files are moved but package directives aren't updated.
    
    GitOrigin-RevId: bad7508720d7e60cae26e044434c819c2b08632d

---
+++ platform/workspaceModel/ide/src/com/intellij/workspaceModel/ide/legacyBridge/package-info.java
@@ -0,0 +1,14 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+/**
+ * Classes in this package provide base interfaces for implementations which store the old IDE project model data in
+ * {@link com.intellij.workspaceModel.storage.WorkspaceEntity} instances.
+ *
+ * <p>
+ * All classes in this package aren't supposed to be used in external code.
+ * </p>
+ */
+@ApiStatus.Internal
+package com.intellij.workspaceModel.ide.legacyBridge;
+
+import org.jetbrains.annotations.ApiStatus;
