ae98e89096c4367d25d19b278f9ef62bd472bb30
Nikita Eshkeev <nikita.eshkeev@jetbrains.com>
2020-06-03 13:23:41 +0300

    [codeInsight] IDEA-190678 Inspection to warn about unnecessary array creation in log call
    This patch removes RedundantObjectArrayCreationInLoggingInspection since it is more convenient to extend the existing inspection with just a simple `if` clause.
    
    Signed-off-by: Nikita Eshkeev <nikita.eshkeev@jetbrains.com>
    
    GitOrigin-RevId: edc983868bec4a3a0dcf7996c2bde7632ac61d81

--- java/java-impl/src/com/intellij/codeInspection/miscGenerics/RedundantArrayForVarargsCallInspection.java
+++ java/java-impl/src/com/intellij/codeInspection/miscGenerics/RedundantArrayForVarargsCallInspection.java
@@ -13,6 +13,7 @@ import com.intellij.psi.util.PsiUtil;
 import com.intellij.psi.util.TypeConversionUtil;
 import com.intellij.refactoring.util.InlineUtil;
 import com.intellij.util.IncorrectOperationException;
+import com.siyeh.ig.callMatcher.CallMatcher;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -20,6 +21,8 @@ import org.jetbrains.annotations.Nullable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import static com.siyeh.ig.callMatcher.CallMatcher.exactInstanceCall;
 
 /**
  * @author ven
@@ -48,126 +51,7 @@ public class RedundantArrayForVarargsCallInspection extends GenericsInspectionTo
                                              @NotNull final InspectionManager manager,
                                              final boolean isOnTheFly) {
     final List<ProblemDescriptor> problems = new ArrayList<>();
-    place.accept(new JavaRecursiveElementWalkingVisitor() {
-      @Override
-      public void visitCallExpression(PsiCallExpression expression) {
-        super.visitCallExpression(expression);
-        checkCall(expression);
-      }
-
-      @Override
-      public void visitEnumConstant(PsiEnumConstant enumConstant) {
-        super.visitEnumConstant(enumConstant);
-        checkCall(enumConstant);
-      }
-
-      @Override
-      public void visitClass(PsiClass aClass) {
-        //do not go inside to prevent multiple signals of the same problem
-      }
-
-      private void checkCall(PsiCall expression) {
-        final JavaResolveResult resolveResult = expression.resolveMethodGenerics();
-        PsiElement element = resolveResult.getElement();
-        final PsiSubstitutor substitutor = resolveResult.getSubstitutor();
-        if (!(element instanceof PsiMethod)) {
-          return;
-        }
-        PsiMethod method = (PsiMethod)element;
-        if (!method.isVarArgs() ||
-            AnnotationUtil.isAnnotated(method, CommonClassNames.JAVA_LANG_INVOKE_MH_POLYMORPHIC, 0)) {
-          return;
-        }
-        PsiParameter[] parameters = method.getParameterList().getParameters();
-        PsiExpressionList argumentList = expression.getArgumentList();
-        if (argumentList == null) {
-          return;
-        }
-        PsiExpression[] args = argumentList.getExpressions();
-        if (parameters.length != args.length) {
-          return;
-        }
-        PsiExpression lastArg = PsiUtil.skipParenthesizedExprDown(args[args.length - 1]);
-        PsiParameter lastParameter = parameters[args.length - 1];
-        if (!lastParameter.isVarArgs()) {
-          return;
-        }
-        PsiType lastParamType = lastParameter.getType();
-        LOG.assertTrue(lastParamType instanceof PsiEllipsisType, lastParamType);
-        if (!(lastArg instanceof PsiNewExpression)) {
-          return;
-        }
-        final PsiType substitutedLastParamType = substitutor.substitute(((PsiEllipsisType)lastParamType).toArrayType());
-        final PsiType lastArgType = lastArg.getType();
-        if (lastArgType == null || !lastArgType.equals(substitutedLastParamType) &&
-                                   !lastArgType.equals(TypeConversionUtil.erasure(substitutedLastParamType))) {
-          return;
-        }
-        PsiExpression[] initializers = getInitializers((PsiNewExpression)lastArg);
-        if (initializers == null) {
-          return;
-        }
-        if (Arrays.stream(initializers).anyMatch(expr -> expr instanceof PsiArrayInitializerExpression)) {
-          return;
-        }
-        if (!isSafeToFlatten(expression, method, initializers)) {
-          return;
-        }
-        String message = JavaBundle.message("inspection.redundant.array.creation.for.varargs.call.descriptor");
-        ProblemDescriptor descriptor = manager.createProblemDescriptor(lastArg, message, myQuickFixAction,
-                                                                       ProblemHighlightType.GENERIC_ERROR_OR_WARNING, isOnTheFly);
-        problems.add(descriptor);
-      }
-
-      private boolean isSafeToFlatten(@NotNull PsiCall callExpression, @NotNull PsiMethod oldRefMethod, PsiExpression @NotNull [] arrayElements) {
-        if (arrayElements.length == 1) {
-          PsiType type = arrayElements[0].getType();
-          // change foo(new Object[]{array}) to foo(array) is not safe
-          if (PsiType.NULL.equals(type) || type instanceof PsiArrayType) return false;
-        }
-        PsiCall copy = (PsiCall)callExpression.copy();
-        PsiExpressionList copyArgumentList = copy.getArgumentList();
-        LOG.assertTrue(copyArgumentList != null);
-        PsiExpression[] args = copyArgumentList.getExpressions();
-        try {
-          args[args.length - 1].delete();
-          if (arrayElements.length > 0) {
-            copyArgumentList.addRange(arrayElements[0], arrayElements[arrayElements.length - 1]);
-          }
-          final Project project = callExpression.getProject();
-          final JavaResolveResult resolveResult;
-          if (callExpression instanceof PsiEnumConstant) {
-            final PsiEnumConstant enumConstant = (PsiEnumConstant)callExpression;
-            final PsiClass containingClass = enumConstant.getContainingClass();
-            if (containingClass == null) return false;
-            final JavaPsiFacade facade = JavaPsiFacade.getInstance(project);
-            final PsiClassType classType = facade.getElementFactory().createType(containingClass);
-            resolveResult = facade.getResolveHelper().resolveConstructor(classType, copyArgumentList, enumConstant);
-            return resolveResult.isValidResult() && resolveResult.getElement() == oldRefMethod;
-          }
-          else {
-            resolveResult = copy.resolveMethodGenerics();
-            if (!resolveResult.isValidResult() || resolveResult.getElement() != oldRefMethod) {
-              return false;
-            }
-            if (callExpression.getParent() instanceof PsiExpressionStatement) return true;
-            final ExpectedTypeInfo[] expectedTypes = ExpectedTypesProvider.getExpectedTypes((PsiCallExpression)callExpression, false);
-            if (expectedTypes.length == 0) return false;
-            final PsiType expressionType = ((PsiCallExpression)copy).getType();
-            if (expressionType == null) return false;
-            for (ExpectedTypeInfo expectedType : expectedTypes) {
-              if (expectedType.getType().isAssignableFrom(expressionType)) {
-                return true;
-              }
-            }
-            return false;
-          }
-        }
-        catch (IncorrectOperationException e) {
-          return false;
-        }
-      }
-    });
+    place.accept(new RedundantArrayForVarargVisitor(manager, isOnTheFly, problems));
     if (problems.isEmpty()) return null;
     return problems.toArray(ProblemDescriptor.EMPTY_ARRAY);
   }
@@ -199,5 +83,146 @@ public class RedundantArrayForVarargsCallInspection extends GenericsInspectionTo
   @NonNls
   public String getShortName() {
     return "RedundantArrayCreation";
+  }
+
+  private static final class RedundantArrayForVarargVisitor extends JavaRecursiveElementWalkingVisitor {
+    private @NotNull final InspectionManager myManager;
+    private final boolean myIsOnTheFly;
+    private final List<ProblemDescriptor> myProblems;
+
+    private static final String[] METHOD_NAMES = new String[]{"debug", "error", "info", "trace", "warn"};
+
+    private static final CallMatcher LOGGER_MESSAGE_CALL = exactInstanceCall("org.slf4j.Logger", METHOD_NAMES)
+      .parameterTypes(String.class.getName(), "java.lang.Object...");
+
+    private RedundantArrayForVarargVisitor(@NotNull InspectionManager manager, boolean isOnTheFly, List<ProblemDescriptor> problems) {
+      myManager = manager;
+      myIsOnTheFly = isOnTheFly;
+      myProblems = problems;
+    }
+
+    @Override
+    public void visitCallExpression(PsiCallExpression expression) {
+      super.visitCallExpression(expression);
+      checkCall(expression);
+    }
+
+    @Override
+    public void visitEnumConstant(PsiEnumConstant enumConstant) {
+      super.visitEnumConstant(enumConstant);
+      checkCall(enumConstant);
+    }
+
+    @Override
+    public void visitClass(PsiClass aClass) {
+      //do not go inside to prevent multiple signals of the same problem
+    }
+
+    private void checkCall(PsiCall expression) {
+      final JavaResolveResult resolveResult = expression.resolveMethodGenerics();
+      PsiElement element = resolveResult.getElement();
+      final PsiSubstitutor substitutor = resolveResult.getSubstitutor();
+      if (!(element instanceof PsiMethod)) {
+        return;
+      }
+      PsiMethod method = (PsiMethod)element;
+      if (!method.isVarArgs() ||
+          AnnotationUtil.isAnnotated(method, CommonClassNames.JAVA_LANG_INVOKE_MH_POLYMORPHIC, 0)) {
+        return;
+      }
+      PsiParameter[] parameters = method.getParameterList().getParameters();
+      PsiExpressionList argumentList = expression.getArgumentList();
+      if (argumentList == null) {
+        return;
+      }
+      PsiExpression[] args = argumentList.getExpressions();
+      if (parameters.length != args.length) {
+        return;
+      }
+      PsiExpression lastArg = PsiUtil.skipParenthesizedExprDown(args[args.length - 1]);
+      PsiParameter lastParameter = parameters[args.length - 1];
+      if (!lastParameter.isVarArgs()) {
+        return;
+      }
+      PsiType lastParamType = lastParameter.getType();
+      LOG.assertTrue(lastParamType instanceof PsiEllipsisType, lastParamType);
+      if (!(lastArg instanceof PsiNewExpression)) {
+        return;
+      }
+      final PsiType substitutedLastParamType = substitutor.substitute(((PsiEllipsisType)lastParamType).toArrayType());
+      final PsiType lastArgType = lastArg.getType();
+      if (lastArgType == null || !lastArgType.equals(substitutedLastParamType) &&
+                                 !lastArgType.equals(TypeConversionUtil.erasure(substitutedLastParamType))) {
+        return;
+      }
+      PsiExpression[] initializers = getInitializers((PsiNewExpression)lastArg);
+      if (initializers == null) {
+        return;
+      }
+      if (Arrays.stream(initializers).anyMatch(expr -> expr instanceof PsiArrayInitializerExpression)) {
+        return;
+      }
+      if (!isSafeToFlatten(expression, method, initializers)) {
+        return;
+      }
+      String message = JavaBundle.message("inspection.redundant.array.creation.for.varargs.call.descriptor");
+      ProblemDescriptor descriptor = myManager.createProblemDescriptor(lastArg, message, myQuickFixAction,
+                                                                       ProblemHighlightType.GENERIC_ERROR_OR_WARNING, myIsOnTheFly);
+      myProblems.add(descriptor);
+    }
+
+    private static boolean isSafeToFlatten(@NotNull final PsiCall callExpression,
+                                           @NotNull final PsiMethod oldRefMethod,
+                                           @NotNull final PsiExpression @NotNull [] arrayElements) {
+      if (callExpression instanceof PsiExpression && LOGGER_MESSAGE_CALL.matches((PsiExpression)callExpression)) {
+        return true;
+      }
+      if (arrayElements.length == 1) {
+        PsiType type = arrayElements[0].getType();
+        // change foo(new Object[]{array}) to foo(array) is not safe
+        if (PsiType.NULL.equals(type) || type instanceof PsiArrayType) return false;
+      }
+      PsiCall copy = (PsiCall)callExpression.copy();
+      PsiExpressionList copyArgumentList = copy.getArgumentList();
+      LOG.assertTrue(copyArgumentList != null);
+      PsiExpression[] args = copyArgumentList.getExpressions();
+      try {
+        args[args.length - 1].delete();
+        if (arrayElements.length > 0) {
+          copyArgumentList.addRange(arrayElements[0], arrayElements[arrayElements.length - 1]);
+        }
+        final Project project = callExpression.getProject();
+        final JavaResolveResult resolveResult;
+        if (callExpression instanceof PsiEnumConstant) {
+          final PsiEnumConstant enumConstant = (PsiEnumConstant)callExpression;
+          final PsiClass containingClass = enumConstant.getContainingClass();
+          if (containingClass == null) return false;
+          final JavaPsiFacade facade = JavaPsiFacade.getInstance(project);
+          final PsiClassType classType = facade.getElementFactory().createType(containingClass);
+          resolveResult = facade.getResolveHelper().resolveConstructor(classType, copyArgumentList, enumConstant);
+          return resolveResult.isValidResult() && resolveResult.getElement() == oldRefMethod;
+        }
+        else {
+          resolveResult = copy.resolveMethodGenerics();
+          if (!resolveResult.isValidResult() || resolveResult.getElement() != oldRefMethod) {
+            return false;
+          }
+          if (callExpression.getParent() instanceof PsiExpressionStatement) return true;
+          final ExpectedTypeInfo[] expectedTypes = ExpectedTypesProvider.getExpectedTypes((PsiCallExpression)callExpression, false);
+          if (expectedTypes.length == 0) return false;
+          final PsiType expressionType = ((PsiCallExpression)copy).getType();
+          if (expressionType == null) return false;
+          for (ExpectedTypeInfo expectedType : expectedTypes) {
+            if (expectedType.getType().isAssignableFrom(expressionType)) {
+              return true;
+            }
+          }
+          return false;
+        }
+      }
+      catch (IncorrectOperationException e) {
+        return false;
+      }
+    }
   }
 }
