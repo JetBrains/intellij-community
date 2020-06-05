ba45659ecf8a056237f7af65c555b8e749d27559
Ivan Semenov <ivan.semenov@jetbrains.com>
2020-06-05 17:20:07 +0300

    [github] Better PR diff content titles
    GitOrigin-RevId: 46305dbb800187fed066cbc068378a2c1f029e85

--- plugins/github/src/org/jetbrains/plugins/github/pullrequest/action/GHPRShowDiffAction.kt
+++ plugins/github/src/org/jetbrains/plugins/github/pullrequest/action/GHPRShowDiffAction.kt
@@ -3,27 +3,12 @@ package org.jetbrains.plugins.github.pullrequest.action
 
 import com.intellij.diff.DiffDialogHints
 import com.intellij.diff.DiffManager
-import com.intellij.diff.util.DiffUserDataKeys
-import com.intellij.diff.util.DiffUserDataKeysEx
-import com.intellij.icons.AllIcons
-import com.intellij.ide.actions.NonEmptyActionGroup
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.diff.impl.GenericDataProvider
 import com.intellij.openapi.project.DumbAwareAction
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.Key
 import com.intellij.openapi.vcs.VcsDataKeys
-import com.intellij.openapi.vcs.changes.Change
-import com.intellij.openapi.vcs.changes.actions.diff.ChangeDiffRequestProducer
 import com.intellij.openapi.vcs.changes.ui.ChangeDiffRequestChain
-import org.jetbrains.plugins.github.i18n.GithubBundle
-import org.jetbrains.plugins.github.pullrequest.comment.GHPRDiffReviewSupport
-import org.jetbrains.plugins.github.pullrequest.comment.action.GHPRDiffReviewResolvedThreadsToggleAction
-import org.jetbrains.plugins.github.pullrequest.comment.action.GHPRDiffReviewThreadsReloadAction
-import org.jetbrains.plugins.github.pullrequest.comment.action.GHPRDiffReviewThreadsToggleAction
 import org.jetbrains.plugins.github.pullrequest.ui.changes.GHPRChangesDiffHelper
-import org.jetbrains.plugins.github.util.GHToolbarLabelAction
 
 class GHPRShowDiffAction : DumbAwareAction() {
 
@@ -41,44 +26,10 @@ class GHPRShowDiffAction : DumbAwareAction() {
     val diffHelper = e.getData(GHPRChangesDiffHelper.DATA_KEY)
     val actionDataContext = e.getData(GHPRActionKeys.ACTION_DATA_CONTEXT)
     val producers = selection.mapNotNull {
-      getDiffRequestProducer(project, it, diffHelper, actionDataContext)
+      GHPRChangesDiffHelper.getDiffRequestProducer(project, it, diffHelper, actionDataContext)
     }
     if (producers.isEmpty()) return
 
     DiffManager.getInstance().showDiff(project, ChangeDiffRequestChain(producers, 0), DiffDialogHints.DEFAULT)
-  }
-
-  private fun getDiffRequestProducer(project: Project, change: Change,
-                                     diffHelper: GHPRChangesDiffHelper?,
-                                     actionDataContext: GHPRActionDataContext?): ChangeDiffRequestProducer? {
-
-    val dataKeys: MutableMap<Key<out Any>, Any?> = mutableMapOf()
-
-    val diffComputer = diffHelper?.getDiffComputer(change)
-    if (diffComputer != null) {
-      dataKeys[DiffUserDataKeysEx.CUSTOM_DIFF_COMPUTER] = diffComputer
-    }
-
-    val reviewSupport = diffHelper?.getReviewSupport(change)
-    if (reviewSupport != null) {
-      dataKeys[GHPRDiffReviewSupport.KEY] = reviewSupport
-      dataKeys[DiffUserDataKeys.DATA_PROVIDER] = GenericDataProvider().apply {
-        putData(GHPRActionKeys.ACTION_DATA_CONTEXT, actionDataContext)
-        putData(GHPRDiffReviewSupport.DATA_KEY, reviewSupport)
-      }
-      val viewOptionsGroup = NonEmptyActionGroup().apply {
-        isPopup = true
-        templatePresentation.text = GithubBundle.message("pull.request.diff.view.options")
-        templatePresentation.icon = AllIcons.Actions.Show
-        add(GHPRDiffReviewThreadsToggleAction())
-        add(GHPRDiffReviewResolvedThreadsToggleAction())
-      }
-
-      dataKeys[DiffUserDataKeys.CONTEXT_ACTIONS] = listOf(GHToolbarLabelAction(GithubBundle.message("pull.request.diff.review.label")),
-                                                          viewOptionsGroup,
-                                                          GHPRDiffReviewThreadsReloadAction(),
-                                                          GHPRReviewSubmitAction())
-    }
-    return ChangeDiffRequestProducer.create(project, change, dataKeys)
   }
 }
--- plugins/github/src/org/jetbrains/plugins/github/pullrequest/ui/changes/GHPRChangesDiffHelper.kt
+++ plugins/github/src/org/jetbrains/plugins/github/pullrequest/ui/changes/GHPRChangesDiffHelper.kt
@@ -1,11 +1,27 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.github.pullrequest.ui.changes
 
+import com.intellij.diff.util.DiffUserDataKeys
 import com.intellij.diff.util.DiffUserDataKeysEx
+import com.intellij.icons.AllIcons
+import com.intellij.ide.actions.NonEmptyActionGroup
 import com.intellij.openapi.actionSystem.DataKey
+import com.intellij.openapi.diff.impl.GenericDataProvider
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Key
 import com.intellij.openapi.vcs.changes.Change
+import com.intellij.openapi.vcs.changes.actions.diff.ChangeDiffRequestProducer
+import com.intellij.openapi.vcs.history.VcsDiffUtil
+import org.jetbrains.plugins.github.i18n.GithubBundle
+import org.jetbrains.plugins.github.pullrequest.action.GHPRActionDataContext
+import org.jetbrains.plugins.github.pullrequest.action.GHPRActionKeys
+import org.jetbrains.plugins.github.pullrequest.action.GHPRReviewSubmitAction
 import org.jetbrains.plugins.github.pullrequest.comment.GHPRDiffReviewSupport
+import org.jetbrains.plugins.github.pullrequest.comment.action.GHPRDiffReviewResolvedThreadsToggleAction
+import org.jetbrains.plugins.github.pullrequest.comment.action.GHPRDiffReviewThreadsReloadAction
+import org.jetbrains.plugins.github.pullrequest.comment.action.GHPRDiffReviewThreadsToggleAction
 import org.jetbrains.plugins.github.pullrequest.data.GHPRChangesProvider
+import org.jetbrains.plugins.github.util.GHToolbarLabelAction
 
 interface GHPRChangesDiffHelper {
   fun setUp(changesProvider: GHPRChangesProvider)
@@ -16,5 +32,41 @@ interface GHPRChangesDiffHelper {
 
   companion object {
     val DATA_KEY = DataKey.create<GHPRChangesDiffHelper>("Github.PullRequest.Diff.Helper")
+
+    fun getDiffRequestProducer(project: Project, change: Change,
+                               diffHelper: GHPRChangesDiffHelper?,
+                               actionDataContext: GHPRActionDataContext?): ChangeDiffRequestProducer? {
+
+      val dataKeys: MutableMap<Key<out Any>, Any?> = mutableMapOf()
+
+      VcsDiffUtil.putFilePathsIntoChangeContext(change, dataKeys)
+
+      val diffComputer = diffHelper?.getDiffComputer(change)
+      if (diffComputer != null) {
+        dataKeys[DiffUserDataKeysEx.CUSTOM_DIFF_COMPUTER] = diffComputer
+      }
+
+      val reviewSupport = diffHelper?.getReviewSupport(change)
+      if (reviewSupport != null) {
+        dataKeys[GHPRDiffReviewSupport.KEY] = reviewSupport
+        dataKeys[DiffUserDataKeys.DATA_PROVIDER] = GenericDataProvider().apply {
+          putData(GHPRActionKeys.ACTION_DATA_CONTEXT, actionDataContext)
+          putData(GHPRDiffReviewSupport.DATA_KEY, reviewSupport)
+        }
+        val viewOptionsGroup = NonEmptyActionGroup().apply {
+          isPopup = true
+          templatePresentation.text = GithubBundle.message("pull.request.diff.view.options")
+          templatePresentation.icon = AllIcons.Actions.Show
+          add(GHPRDiffReviewThreadsToggleAction())
+          add(GHPRDiffReviewResolvedThreadsToggleAction())
+        }
+
+        dataKeys[DiffUserDataKeys.CONTEXT_ACTIONS] = listOf(GHToolbarLabelAction(GithubBundle.message("pull.request.diff.review.label")),
+                                                            viewOptionsGroup,
+                                                            GHPRDiffReviewThreadsReloadAction(),
+                                                            GHPRReviewSubmitAction())
+      }
+      return ChangeDiffRequestProducer.create(project, change, dataKeys)
+    }
   }
 }
