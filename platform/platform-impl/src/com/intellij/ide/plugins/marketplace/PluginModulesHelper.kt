eadca7f429b6509cbd7188e1ae553fb3e4f9c562
Alexander Prendota <Prendota@mail.ru>
2020-06-08 11:59:46 +0300

    IDEA-MR-1908: Implement cache for plugins modules
    (cherry picked from commit 5c1d3de4eaa03991e2cb5915668716c04478898d)
    
    GitOrigin-RevId: 81fa883cfda6866cc0c1d600dcef56faacf4b5eb

--- platform/platform-impl/src/com/intellij/ide/plugins/marketplace/PluginModulesHelper.kt
+++ platform/platform-impl/src/com/intellij/ide/plugins/marketplace/PluginModulesHelper.kt
@@ -1,9 +1,13 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.plugins.marketplace
 
+import com.google.common.cache.Cache
+import com.google.common.cache.CacheBuilder
 import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.openapi.components.service
 import com.intellij.openapi.extensions.PluginId
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 open class PluginModulesHelper {
   companion object {
@@ -11,20 +15,29 @@ open class PluginModulesHelper {
     fun getInstance(): PluginModulesHelper = service()
   }
 
+  private val pluginsModuleCache: Cache<PluginModule, Optional<PluginId>> = CacheBuilder
+    .newBuilder()
+    .expireAfterWrite(1, TimeUnit.HOURS)
+    .build()
+
   fun getMarketplacePluginIdByModule(depPluginId: PluginId): PluginId? {
     val installedPluginWithModule = PluginManagerCore.findPluginByModuleDependency(depPluginId)
-    return if (installedPluginWithModule != null) {
-      installedPluginWithModule.pluginId
-    }
-    else {
-      val updatesByModule: List<IdeCompatibleUpdate> = MarketplaceRequests.getInstance().getCompatibleUpdatesByModule(depPluginId.idString)
-      updatesByModule.firstOrNull()?.let { PluginId.getId(it.pluginId) }
-    }
+    if (installedPluginWithModule != null) return installedPluginWithModule.pluginId
+
+    val pluginModule = depPluginId.idString
+    val cachedModule = pluginsModuleCache.getIfPresent(pluginModule)?.orElse(null)
+    if (cachedModule != null) return cachedModule
+
+    val updatesByModule: List<IdeCompatibleUpdate> = MarketplaceRequests.getInstance().getCompatibleUpdatesByModule(pluginModule)
+    return updatesByModule.firstOrNull()
+      ?.let { PluginId.getId(it.pluginId) }
+      ?.also { pluginsModuleCache.put(pluginModule, Optional.ofNullable(it)) }
   }
 
   fun getInstalledPluginIdByModule(depPluginId: PluginId): PluginId? {
     return PluginManagerCore.findPluginByModuleDependency(depPluginId)?.pluginId
   }
 
-
 }
+
+private typealias PluginModule = String
