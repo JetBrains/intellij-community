d7ceab7d2d7711bd19d73d72a4bdbea7145c6d1a
Nikita Eshkeev <nikita.eshkeev@jetbrains.com>
2020-06-08 15:01:01 +0300

    [analysis] IDEA-241656 Provide a fix for "static member qualifying type may not be annotated"
    This patch fixes notes from the code review, it includes:
    
    1. moving MoveAnnotationOnStaticMemberQualifyingTypeFix to top level
    2. reusing MoveAnnotationOnStaticMemberQualifyingTypeFix#getFamilyName in MoveAnnotationOnStaticMemberQualifyingTypeFix#getName
    3. using PsiTreeUtil#findSiblingBackwards to find the rightmost dot token instead of custom code
    4. removing redundant test data
    
    Signed-off-by: Nikita Eshkeev <nikita.eshkeev@jetbrains.com>
    
    GitOrigin-RevId: a539ed96c3883947b621525fc4bd017eefd7b458

--- java/java-analysis-impl/src/com/intellij/codeInsight/daemon/impl/analysis/AnnotationsHighlightUtil.java
+++ java/java-analysis-impl/src/com/intellij/codeInsight/daemon/impl/analysis/AnnotationsHighlightUtil.java
@@ -10,12 +10,8 @@ import com.intellij.codeInsight.intention.IntentionAction;
 import com.intellij.codeInsight.intention.QuickFixFactory;
 import com.intellij.codeInsight.intention.impl.BaseIntentionAction;
 import com.intellij.codeInspection.LocalQuickFixAndIntentionActionOnPsiElement;
-import com.intellij.codeInspection.util.IntentionFamilyName;
-import com.intellij.codeInspection.util.IntentionName;
 import com.intellij.java.analysis.JavaAnalysisBundle;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Comparing;
 import com.intellij.patterns.ElementPattern;
 import com.intellij.pom.java.LanguageLevel;
@@ -29,7 +25,6 @@ import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.util.PsiUtil;
 import com.intellij.psi.util.TypeConversionUtil;
 import com.intellij.util.ObjectUtils;
-import com.siyeh.ig.psiutils.CommentTracker;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -769,71 +764,6 @@ public class AnnotationsHighlightUtil {
       }
 
       return classType.equalsToText(CommonClassNames.JAVA_LANG_CLASS) || classType.equalsToText(CommonClassNames.JAVA_LANG_STRING);
-    }
-  }
-
-  private static final class MoveAnnotationOnStaticMemberQualifyingTypeFix extends LocalQuickFixAndIntentionActionOnPsiElement {
-    private MoveAnnotationOnStaticMemberQualifyingTypeFix(@NotNull final PsiAnnotation annotation) {
-      super(annotation);
-    }
-
-    @Override
-    public @IntentionFamilyName @NotNull String getFamilyName() {
-      return JavaErrorBundle.message("annotation.on.static.member.qualifying.type.family.name");
-    }
-
-    @Override
-    public @IntentionName @NotNull String getText() {
-      return JavaErrorBundle.message("annotation.on.static.member.qualifying.type.family.name");
-    }
-
-    @Override
-    public void invoke(@NotNull final Project project,
-                       @NotNull final PsiFile file,
-                       @Nullable final Editor editor,
-                       @NotNull final PsiElement startElement,
-                       @NotNull final PsiElement endElement) {
-      final PsiTypeElement psiTypeElement = getTypeElement(startElement);
-      if (psiTypeElement == null) return;
-
-      final PsiJavaCodeReferenceElement innermostParent = psiTypeElement.getInnermostComponentReferenceElement();
-      if (innermostParent == null) return;
-
-      final PsiElement rightmostDot = getRightmostDot(innermostParent.getLastChild());
-      if (rightmostDot == null) return;
-
-      innermostParent.addAfter(startElement, rightmostDot);
-
-      final CommentTracker ct = new CommentTracker();
-      ct.markUnchanged(startElement);
-      ct.deleteAndRestoreComments(startElement);
-    }
-
-    @Nullable("The type does not have DOT tokens")
-    @Contract(value = "null -> null", pure = true)
-    private static PsiElement getRightmostDot(@Nullable final PsiElement element) {
-      if (element == null) return null;
-      if (element.getNode().getElementType().equals(JavaTokenType.DOT)) return element;
-
-      return getRightmostDot(element.getPrevSibling());
-    }
-
-    /**
-     * Returns the type element either from a type parameter or a variable declaration
-     *
-     * @param startElement element to find the type for
-     * @return the type element either from a type parameter or a variable declaration
-     */
-    @Nullable("No type element found")
-    @Contract(pure = true)
-    private static PsiTypeElement getTypeElement(@NotNull final PsiElement startElement) {
-      final PsiTypeElement psiTypeElement = PsiTreeUtil.getParentOfType(startElement, PsiTypeElement.class);
-      if (psiTypeElement != null) return psiTypeElement;
-
-      final PsiVariable variable = PsiTreeUtil.getParentOfType(startElement, PsiVariable.class);
-      if (variable == null) return null;
-
-      return variable.getTypeElement();
     }
   }
 }
