a028810f5565c98667c7d138d6606d2cc06af155
Fedor Kudasov <kudasov@google.com>
2020-06-08 14:35:25 +0100

    Rename layoutlib-coroutines to layoutlib-extensions
    Bug: 148260972
    Test: successful build
    Change-Id: Ia3787d44147856dcb9d9051ffead303ff247462a

--- build/groovy/org/jetbrains/intellij/build/AndroidStudioProperties.groovy
+++ build/groovy/org/jetbrains/intellij/build/AndroidStudioProperties.groovy
@@ -303,7 +303,7 @@ class AndroidStudioProperties extends BaseIdeaProperties {
       fileset(dir: "$root/prebuilts/studio/layoutlib") {
         include(name: "build.prop")
         include(name: "data/framework_res.jar")
-        include(name: "data/layoutlib-coroutines.jar")
+        include(name: "data/layoutlib-extensions.jar")
         include(name: "data/fonts/*")
         include(name: "data/fonts/native/fonts.xml")
         include(name: "data/fonts/standard/fonts.xml")
