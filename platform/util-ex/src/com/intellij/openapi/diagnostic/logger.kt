ce29b0856fc0e11f8822ab2096f7484c9b80ec69
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2020-06-10 00:50:39 +0200

    shared-indexes: undo global logger extension function
    GitOrigin-RevId: e8817d936ed0f34751a68eb7b334fa73a00ae347

--- platform/util-ex/src/com/intellij/openapi/diagnostic/logger.kt
+++ platform/util-ex/src/com/intellij/openapi/diagnostic/logger.kt
@@ -6,7 +6,6 @@ import com.intellij.openapi.progress.ProcessCanceledException
 import java.util.concurrent.CancellationException
 
 inline fun <reified T : Any> logger() = Logger.getInstance(T::class.java)
-inline fun <reified T : Any> T.logger() = Logger.getInstance(T::class.java)
 
 fun logger(category: String) = Logger.getInstance(category)
 
