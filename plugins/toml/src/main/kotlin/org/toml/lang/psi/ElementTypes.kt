d966f4d99690a8b9fea8de0e97ac55ce210e324b
Arseniy Pendryak <a.pendryak@yandex.ru>
2020-06-12 14:46:08 +0300

    TOML: improve toml file type detection
    Implement `FileTypeIdentifiableByVirtualFile` instead of `FileTypeRegistry.FileTypeDetector`
    to detect file type of `.cargo/config` file. We don't need to read file content so `FileTypeIdentifiableByVirtualFile` is more suitable in this case
    
    GitOrigin-RevId: f8a7c68e21b3d558a13cbe84a937369aa78dcfdd

--- plugins/toml/src/main/kotlin/org/toml/lang/psi/ElementTypes.kt
+++ plugins/toml/src/main/kotlin/org/toml/lang/psi/ElementTypes.kt
@@ -6,24 +6,28 @@
 package org.toml.lang.psi
 
 import com.intellij.openapi.fileTypes.LanguageFileType
+import com.intellij.openapi.fileTypes.ex.FileTypeIdentifiableByVirtualFile
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.tree.IElementType
 import com.intellij.psi.tree.TokenSet
 import org.toml.ide.icons.TomlIcons
 import org.toml.lang.TomlLanguage
 import org.toml.lang.psi.TomlElementTypes.*
+import javax.swing.Icon
 
 class TomlTokenType(debugName: String) : IElementType(debugName, TomlLanguage)
 class TomlCompositeType(debugName: String) : IElementType(debugName, TomlLanguage)
 
-object TomlFileType : LanguageFileType(TomlLanguage) {
-    override fun getName() = "TOML"
-    override fun getDescription() = "TOML file"
-    override fun getDefaultExtension() = "toml"
+object TomlFileType : LanguageFileType(TomlLanguage), FileTypeIdentifiableByVirtualFile {
+    override fun getName(): String = "TOML"
+    override fun getDescription(): String = "TOML file"
+    override fun getDefaultExtension(): String = "toml"
+    override fun getIcon(): Icon = TomlIcons.TOML_FILE
+    override fun getCharset(file: VirtualFile, content: ByteArray): String = "UTF-8"
 
-    override fun getIcon() = TomlIcons.TOML_FILE
-
-    override fun getCharset(file: VirtualFile, content: ByteArray) = "UTF-8"
+    override fun isMyFileType(file: VirtualFile): Boolean {
+        return file.name == "config" && file.parent?.name == ".cargo"
+    }
 }
 
 val TOML_COMMENTS = TokenSet.create(COMMENT)
