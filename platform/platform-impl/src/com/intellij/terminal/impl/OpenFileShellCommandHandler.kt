e4edbbb3d0301f0f5bb3f176cd18a195da15e2c1
Dmitry.Krasilschikov <kradima@jetbrains.com>
2020-06-14 17:18:28 +0300

    IDEA-240701 open editor with a file with a supported extension fetched from `open file.ext` terminal command
    GitOrigin-RevId: ea317c3ed634ff74d87cd53a899f2d6cb6051d4a

---
+++ platform/platform-impl/src/com/intellij/terminal/impl/OpenFileShellCommandHandler.kt
@@ -0,0 +1,41 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.terminal.impl
+
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.fileTypes.FileTypeRegistry
+import com.intellij.openapi.fileTypes.UnknownFileType
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.terminal.TerminalShellCommandHandler
+import java.io.File
+
+class OpenFileShellCommandHandler : TerminalShellCommandHandler {
+  override fun matches(project: Project, workingDirectory: String?, localSession: Boolean, command: String) =
+    handleCommand(command, localSession, workingDirectory) { file -> checkRegisteredFileType(file) }
+
+  override fun execute(project: Project, workingDirectory: String?, localSession: Boolean, command: String) =
+    handleCommand(command, localSession, workingDirectory) { file -> openFileEditor(project, file) }
+
+  private fun checkRegisteredFileType(file: VirtualFile?) =
+    file != null && FileTypeRegistry.getInstance().getFileTypeByFile(file) != UnknownFileType.INSTANCE
+
+  private fun openFileEditor(project: Project, file: VirtualFile?) =
+    file != null && FileEditorManager.getInstance(project).openFile(file, true).isNotEmpty()
+
+  private fun handleCommand(command: String, localSession: Boolean, workingDirectory: String?, block: (VirtualFile?) -> Boolean): Boolean {
+    val prefix = "open "
+    if (!command.startsWith(prefix)) return false
+
+    val path = command.substring(prefix.length)
+    if (!localSession) return false
+
+    val file = LocalFileSystem.getInstance().findFileByIoFile(File(path))
+    if (file != null && file.exists()) return block.invoke(file)
+
+    if (workingDirectory != null) return LocalFileSystem.getInstance().findFileByIoFile(
+      File(workingDirectory, path))?.takeIf { it.exists() }.let { block.invoke(it) }
+
+    return false
+  }
+}
