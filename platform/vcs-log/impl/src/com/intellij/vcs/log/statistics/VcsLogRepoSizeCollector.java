41afc79d502a8f256b128693bf257c140eaca6fb
Anastasia Ivanova <anastasia.ivanova@jetbrains.com>
2020-06-15 15:21:35 +0700

    IDEA-CR-63641: FUS-728 Change format of logging `vcs.log.data`
    GitOrigin-RevId: 62b782555c4ceee743ee2025b175fab66983e5a4

--- platform/vcs-log/impl/src/com/intellij/vcs/log/statistics/VcsLogRepoSizeCollector.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/statistics/VcsLogRepoSizeCollector.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.vcs.log.statistics;
 
 import com.intellij.internal.statistic.beans.MetricEvent;
@@ -7,6 +7,7 @@ import com.intellij.internal.statistic.eventLog.FeatureUsageData;
 import com.intellij.internal.statistic.service.fus.collectors.ProjectUsagesCollector;
 import com.intellij.internal.statistic.service.fus.collectors.UsageDescriptorKeyValidator;
 import com.intellij.internal.statistic.utils.PluginInfoDetectorKt;
+import com.intellij.internal.statistic.utils.StatisticsUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vcs.VcsKey;
@@ -16,7 +17,6 @@ import com.intellij.util.containers.MultiMap;
 import com.intellij.vcs.log.VcsLogProvider;
 import com.intellij.vcs.log.data.DataPack;
 import com.intellij.vcs.log.data.VcsLogData;
-import com.intellij.vcs.log.graph.PermanentGraph;
 import com.intellij.vcs.log.impl.VcsProjectLog;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
@@ -36,17 +36,18 @@ public class VcsLogRepoSizeCollector extends ProjectUsagesCollector {
     if (logData != null) {
       DataPack dataPack = logData.getDataPack();
       if (dataPack.isFull()) {
-        PermanentGraph<Integer> permanentGraph = dataPack.getPermanentGraph();
-
+        int commitCount = dataPack.getPermanentGraph().getAllCommits().size();
+        int branchesCount = dataPack.getRefsModel().getBranches().size();
+        int usersCount = logData.getAllUsers().size();
         Set<MetricEvent> usages = ContainerUtil.newHashSet(new MetricEvent("dataInitialized"));
-        usages.add(MetricEventFactoryKt.newCounterMetric("commit.count", permanentGraph.getAllCommits().size()));
-        usages.add(MetricEventFactoryKt.newCounterMetric("branches.count", dataPack.getRefsModel().getBranches().size()));
-        usages.add(MetricEventFactoryKt.newCounterMetric("users.count", logData.getAllUsers().size()));
-
+        usages.add(MetricEventFactoryKt.newCounterMetric("commit.count", StatisticsUtil.getNextPowerOfTwo(commitCount)));
+        usages.add(MetricEventFactoryKt.newCounterMetric("branches.count", StatisticsUtil.getNextPowerOfTwo(branchesCount)));
+        usages.add(MetricEventFactoryKt.newCounterMetric("users.count", StatisticsUtil.getNextPowerOfTwo(usersCount)));
         MultiMap<VcsKey, VirtualFile> groupedRoots = groupRootsByVcs(dataPack.getLogProviders());
         for (VcsKey vcs : groupedRoots.keySet()) {
           FeatureUsageData vcsData = new FeatureUsageData().addData("vcs", getVcsKeySafe(vcs));
-          usages.add(MetricEventFactoryKt.newCounterMetric("root.count", groupedRoots.get(vcs).size(), vcsData));
+          int rootCount = groupedRoots.get(vcs).size();
+          usages.add(MetricEventFactoryKt.newCounterMetric("root.count", StatisticsUtil.getNextPowerOfTwo(rootCount), vcsData));
         }
         return usages;
       }
