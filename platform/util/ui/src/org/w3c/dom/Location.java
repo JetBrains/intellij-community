add210d4e00ead149507926e88ced859c35d59a1
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2020-06-16 15:09:28 +0300

    [platform] migration to Java 11: remove classes from org.w3c.dom package (IDEA-241075)
    Because they conflicts with classes from JDK. This should also fix IDEA-235927.
    
    GitOrigin-RevId: 41a8cdd6c87b4032134f443f208a1eedd86d7693

--- platform/util/ui/src/org/w3c/dom/Location.java
+++
@@ -1,7 +0,0 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.w3c.dom;
-
-// Workaround for IDEA-193340
-@SuppressWarnings("unused")
-public interface Location extends org.apache.batik.w3c.dom.Location {
-}
