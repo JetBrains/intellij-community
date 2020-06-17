ca0a5eca8419095bed6db75a34528438a7b38aca
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2020-06-17 14:14:17 +0300

    IDEA-231995 Do not highlight properties in gradle-wrapper.properties
    GitOrigin-RevId: b9c62e3c7a6fb6c48e431fca23cb2f8ed7d82006

--- plugins/gradle/java/src/findUsages/GradleWrapperImplicitPropertyUsageProvider.kt
+++ plugins/gradle/java/src/findUsages/GradleWrapperImplicitPropertyUsageProvider.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.gradle.service.project
+package org.jetbrains.plugins.gradle.findUsages
 
 import com.intellij.codeInspection.unused.ImplicitPropertyUsageProvider
 import com.intellij.lang.properties.psi.Property
