66fdfa7f3717e7b640ce5d1e1659597d7248b7bf
Daniil Tsaryov <daniil.tsaryov@jetbrains.com>
2020-06-17 18:01:30 +0300

    [vcs] IDEA-243711 Fix incompatible options in git pull dialog
    GitOrigin-RevId: 0454785372d1e43d5576341db2ae8ecd07550f14

--- plugins/git4idea/src/git4idea/pull/PullOption.kt
+++ plugins/git4idea/src/git4idea/pull/PullOption.kt
@@ -9,19 +9,25 @@ enum class PullOption(@NonNls val option: String,
   REBASE("--rebase", "pull.option.rebase"),
   FF_ONLY("--ff-only", "pull.option.ff.only"),
   NO_FF("--no-ff", "pull.option.no.ff"),
-  SQUASH_COMMIT("--squash", "pull.option.squash.commit"),
+  SQUASH("--squash", "pull.option.squash.commit"),
   NO_COMMIT("--no-commit", "pull.option.no.commit"),
   NO_VERIFY("--no-verify", "merge.option.no.verify");
 
   fun isOptionSuitable(option: PullOption): Boolean {
-    if (this != REBASE && option == REBASE) return false
-
     return when (this) {
-      REBASE -> option == REBASE
-      FF_ONLY -> option != NO_FF
-      NO_FF -> option != FF_ONLY
-      SQUASH_COMMIT -> option == SQUASH_COMMIT || option == NO_COMMIT
+      REBASE -> option !in REBASE_INCOMPATIBLE
+      FF_ONLY -> option !in FF_ONLY_INCOMPATIBLE
+      NO_FF -> option !in NO_FF_INCOMPATIBLE
+      SQUASH -> option !in SQUASH_COMMIT_INCOMPATIBLE
+      NO_COMMIT -> option != REBASE
       else -> true
     }
+  }
+
+  companion object {
+    private val REBASE_INCOMPATIBLE = listOf(FF_ONLY, NO_FF, SQUASH, NO_COMMIT)
+    private val FF_ONLY_INCOMPATIBLE = listOf(NO_FF, SQUASH)
+    private val NO_FF_INCOMPATIBLE = listOf(FF_ONLY, SQUASH)
+    private val SQUASH_COMMIT_INCOMPATIBLE = listOf(NO_FF, FF_ONLY)
   }
 }
