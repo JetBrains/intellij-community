82c080d301ae2fcadd31fabd314c944c512f7e31
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2020-06-18 22:48:59 +0300

    [git-rebase] IDEA-243981: Allow to undo `Drop Commits` action result
    GitOrigin-RevId: 4378045e5cec9a368c2848e77e25f9f07f62917e

--- plugins/git4idea/tests/git4idea/rebase/log/drop/GitDropOperationTest.kt
+++ plugins/git4idea/tests/git4idea/rebase/log/drop/GitDropOperationTest.kt
@@ -1,6 +1,9 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package git4idea.rebase.log.drop
 
+import git4idea.rebase.log.GitMultipleCommitEditingOperationResult.Complete
+import git4idea.rebase.log.GitMultipleCommitEditingOperationResult.Complete.UndoPossibility
+import git4idea.rebase.log.GitMultipleCommitEditingOperationResult.Complete.UndoResult
 import git4idea.test.GitSingleRepoTest
 import git4idea.test.assertCommitted
 
@@ -58,6 +61,31 @@ class GitDropOperationTest : GitSingleRepoTest() {
     }
     repo.assertCommitted(2) {
       added("c")
+    }
+    repo.assertCommitted(3) {
+      added("a")
+    }
+  }
+
+  fun `test undo dropping of the last commit`() {
+    file("a").create().addCommit("Commit a").details()
+    file("b").create().addCommit("Commit b").details()
+    val commitToDrop = file("c").create().addCommit("Commit c").details()
+
+    refresh()
+    updateChangeListManager()
+
+    val operationResult = GitDropOperation(repo).execute(listOf(commitToDrop)) as Complete
+
+    assertTrue(operationResult.checkUndoPossibility() is UndoPossibility.Possible)
+    val undoResult = operationResult.undo()
+    assertTrue(undoResult is UndoResult.Success)
+
+    repo.assertCommitted(1) {
+      added("c")
+    }
+    repo.assertCommitted(2) {
+      added("b")
     }
     repo.assertCommitted(3) {
       added("a")
