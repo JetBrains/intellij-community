2e1a4551f38887b2382f47eb050864ab55c96179
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2020-06-19 21:38:30 +0300

    github: Convert "GithubRepositoryHostingService" to kotlin
    (cherry picked from commit 850043328775d86b6c59a54447f4de76cb146745)
    
    GitOrigin-RevId: dd48ead2239d8d987996ad274186aea2c11cc6b5

--- plugins/github/src/org/jetbrains/plugins/github/extensions/GithubRepositoryHostingService.kt
+++ plugins/github/src/org/jetbrains/plugins/github/extensions/GithubRepositoryHostingService.kt
@@ -1,156 +1,121 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.github.extensions;
+package org.jetbrains.plugins.github.extensions
 
-import com.intellij.dvcs.hosting.RepositoryListLoader;
-import com.intellij.dvcs.hosting.RepositoryListLoadingException;
-import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.project.Project;
-import git4idea.remote.GitHttpAuthDataProvider;
-import git4idea.remote.GitRepositoryHostingService;
-import git4idea.remote.InteractiveGitHttpAuthDataProvider;
-import one.util.streamex.StreamEx;
-import org.jetbrains.annotations.CalledInBackground;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.plugins.github.api.GithubApiRequestExecutor;
-import org.jetbrains.plugins.github.api.GithubApiRequestExecutorManager;
-import org.jetbrains.plugins.github.api.GithubApiRequests;
-import org.jetbrains.plugins.github.api.GithubServerPath;
-import org.jetbrains.plugins.github.api.data.GithubRepo;
-import org.jetbrains.plugins.github.api.util.GithubApiPagesLoader;
-import org.jetbrains.plugins.github.authentication.GithubAuthenticationManager;
-import org.jetbrains.plugins.github.authentication.accounts.GithubAccount;
-import org.jetbrains.plugins.github.exceptions.GithubAuthenticationException;
-import org.jetbrains.plugins.github.exceptions.GithubMissingTokenException;
-import org.jetbrains.plugins.github.exceptions.GithubStatusCodeException;
-import org.jetbrains.plugins.github.util.GithubAccountsMigrationHelper;
-import org.jetbrains.plugins.github.util.GithubGitHelper;
-import org.jetbrains.plugins.github.util.GithubUtil;
+import com.intellij.dvcs.hosting.RepositoryListLoader
+import com.intellij.dvcs.hosting.RepositoryListLoadingException
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.project.Project
+import git4idea.remote.GitHttpAuthDataProvider
+import git4idea.remote.GitRepositoryHostingService
+import git4idea.remote.InteractiveGitHttpAuthDataProvider
+import one.util.streamex.StreamEx
+import org.jetbrains.annotations.CalledInBackground
+import org.jetbrains.plugins.github.api.GithubApiRequestExecutor
+import org.jetbrains.plugins.github.api.GithubApiRequestExecutorManager
+import org.jetbrains.plugins.github.api.GithubApiRequests
+import org.jetbrains.plugins.github.api.GithubServerPath
+import org.jetbrains.plugins.github.api.data.GithubRepo
+import org.jetbrains.plugins.github.api.util.GithubApiPagesLoader.loadAll
+import org.jetbrains.plugins.github.authentication.GithubAuthenticationManager
+import org.jetbrains.plugins.github.authentication.accounts.GithubAccount
+import org.jetbrains.plugins.github.exceptions.GithubAuthenticationException
+import org.jetbrains.plugins.github.exceptions.GithubMissingTokenException
+import org.jetbrains.plugins.github.exceptions.GithubStatusCodeException
+import org.jetbrains.plugins.github.util.GithubAccountsMigrationHelper
+import org.jetbrains.plugins.github.util.GithubGitHelper
+import org.jetbrains.plugins.github.util.GithubUtil
+import java.awt.Component
+import java.util.*
+import java.util.function.Function
+import java.util.stream.Collectors
+import java.util.stream.Stream
 
-import java.awt.*;
-import java.util.List;
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+internal class GithubRepositoryHostingService : GitRepositoryHostingService() {
+  private val authenticationManager get() = GithubAuthenticationManager.getInstance()
+  private val executorManager get() = GithubApiRequestExecutorManager.getInstance()
+  private val gitHelper get() = GithubGitHelper.getInstance()
+  private val authDataProvider = GitHttpAuthDataProvider.EP_NAME.findExtensionOrFail(GithubHttpAuthDataProvider::class.java)
 
-final class GithubRepositoryHostingService extends GitRepositoryHostingService {
-  @NotNull private final GithubAuthenticationManager myAuthenticationManager;
-  @NotNull private final GithubApiRequestExecutorManager myExecutorManager;
-  @NotNull private final GithubGitHelper myGitHelper;
-  @NotNull private final GithubHttpAuthDataProvider myAuthDataProvider;
+  override fun getServiceDisplayName(): String = GithubUtil.SERVICE_DISPLAY_NAME
 
-  public GithubRepositoryHostingService() {
-    myAuthenticationManager = GithubAuthenticationManager.getInstance();
-    myExecutorManager = GithubApiRequestExecutorManager.getInstance();
-    myGitHelper = GithubGitHelper.getInstance();
-    myAuthDataProvider = GitHttpAuthDataProvider.EP_NAME.findExtensionOrFail(GithubHttpAuthDataProvider.class);
-  }
+  override fun getRepositoryListLoader(project: Project): RepositoryListLoader {
+    return object : RepositoryListLoader {
+      private val myExecutors = mutableMapOf<GithubAccount, GithubApiRequestExecutor>()
 
-  @NotNull
-  @Override
-  public String getServiceDisplayName() {
-    return GithubUtil.SERVICE_DISPLAY_NAME;
-  }
-
-  @Override
-  @NotNull
-  public RepositoryListLoader getRepositoryListLoader(@NotNull Project project) {
-    return new RepositoryListLoader() {
-      @NotNull private final Map<GithubAccount, GithubApiRequestExecutor> myExecutors = new HashMap<>();
-
-      @Override
-      public boolean isEnabled() {
-        for (GithubAccount account : myAuthenticationManager.getAccounts()) {
+      override fun isEnabled(): Boolean {
+        for (account in authenticationManager.getAccounts()) {
           try {
-            myExecutors.put(account, myExecutorManager.getExecutor(account));
+            myExecutors[account] = executorManager.getExecutor(account)
           }
-          catch (GithubMissingTokenException e) {
+          catch (e: GithubMissingTokenException) {
             // skip
           }
         }
-        return !myExecutors.isEmpty();
+        return !myExecutors.isEmpty()
       }
 
-      @Override
-      public boolean enable(@Nullable Component parentComponent) {
-        if (!GithubAccountsMigrationHelper.getInstance().migrate(project, parentComponent)) return false;
-        if (!myAuthenticationManager.ensureHasAccounts(project, parentComponent)) return false;
-        boolean atLeastOneHasToken = false;
-        for (GithubAccount account : myAuthenticationManager.getAccounts()) {
-          GithubApiRequestExecutor executor = myExecutorManager.getExecutor(account, project);
-          if (executor == null) continue;
-          myExecutors.put(account, executor);
-          atLeastOneHasToken = true;
+      override fun enable(parentComponent: Component?): Boolean {
+        if (!GithubAccountsMigrationHelper.getInstance().migrate(project, parentComponent)) return false
+        if (!authenticationManager.ensureHasAccounts(project, parentComponent)) return false
+        var atLeastOneHasToken = false
+        for (account in authenticationManager.getAccounts()) {
+          val executor = executorManager.getExecutor(account, project) ?: continue
+          myExecutors[account] = executor
+          atLeastOneHasToken = true
         }
-        return atLeastOneHasToken;
+        return atLeastOneHasToken
       }
 
-      @NotNull
-      @Override
-      public Result getAvailableRepositoriesFromMultipleSources(@NotNull ProgressIndicator progressIndicator) {
-        List<String> urls = new ArrayList<>();
-        List<RepositoryListLoadingException> exceptions = new ArrayList<>();
+      override fun getAvailableRepositoriesFromMultipleSources(progressIndicator: ProgressIndicator): RepositoryListLoader.Result {
+        val urls = mutableListOf<String>()
+        val exceptions = mutableListOf<RepositoryListLoadingException>()
 
-        for (Map.Entry<GithubAccount, GithubApiRequestExecutor> entry : myExecutors.entrySet()) {
-          GithubServerPath server = entry.getKey().getServer();
-          GithubApiRequestExecutor executor = entry.getValue();
+        for ((account, executor) in myExecutors) {
+          val server = account.server
           try {
-            Stream<GithubRepo> streamAssociated = GithubApiPagesLoader
-              .loadAll(executor, progressIndicator, GithubApiRequests.CurrentUser.Repos.pages(server)).stream();
-
-            Stream<GithubRepo> streamWatched = StreamEx.empty();
+            val streamAssociated = loadAll(executor, progressIndicator, GithubApiRequests.CurrentUser.Repos.pages(server)).stream()
+            var streamWatched: Stream<GithubRepo> = StreamEx.empty()
             try {
-              streamWatched = GithubApiPagesLoader
-                .loadAll(executor, progressIndicator, GithubApiRequests.CurrentUser.RepoSubs.pages(server)).stream();
+              streamWatched = loadAll(executor, progressIndicator, GithubApiRequests.CurrentUser.RepoSubs.pages(server)).stream()
             }
-            catch (GithubAuthenticationException | GithubStatusCodeException ignore) {
+            catch (ignore: GithubAuthenticationException) {
               // We already can return something useful from getUserRepos, so let's ignore errors.
               // One of this may not exist in GitHub enterprise
             }
+            catch (ignore: GithubStatusCodeException) {
+            }
             urls.addAll(
               Stream.concat(streamAssociated, streamWatched)
-                .sorted(Comparator.comparing(GithubRepo::getUserName).thenComparing(GithubRepo::getName))
-                .map(repo -> myGitHelper.getRemoteUrl(server, repo.getUserName(), repo.getName()))
+                .sorted(Comparator.comparing { repo: GithubRepo -> repo.userName }.thenComparing { repo: GithubRepo -> repo.name })
+                .map(Function { repo: GithubRepo -> gitHelper.getRemoteUrl(server, repo.userName, repo.name) })
                 .collect(Collectors.toList())
-            );
+            )
           }
-          catch (Exception e) {
-            exceptions.add(new RepositoryListLoadingException("Cannot load repositories from GitHub", e));
+          catch (e: Exception) {
+            exceptions.add(RepositoryListLoadingException("Cannot load repositories from GitHub", e))
           }
         }
-        return new Result(urls, exceptions);
+        return RepositoryListLoader.Result(urls, exceptions)
       }
-    };
+    }
   }
 
   @CalledInBackground
-  @Nullable
-  @Override
-  public InteractiveGitHttpAuthDataProvider getInteractiveAuthDataProvider(@NotNull Project project, @NotNull String url) {
-    return getProvider(project, url, null);
-  }
+  override fun getInteractiveAuthDataProvider(project: Project, url: String): InteractiveGitHttpAuthDataProvider? =
+    getProvider(project, url, null)
 
   @CalledInBackground
-  @Nullable
-  @Override
-  public InteractiveGitHttpAuthDataProvider getInteractiveAuthDataProvider(@NotNull Project project,
-                                                                           @NotNull String url,
-                                                                           @NotNull String login) {
-    return getProvider(project, url, login);
-  }
+  override fun getInteractiveAuthDataProvider(project: Project, url: String, login: String): InteractiveGitHttpAuthDataProvider? =
+    getProvider(project, url, login)
 
-  @Nullable
-  private InteractiveGitHttpAuthDataProvider getProvider(@NotNull Project project, @NotNull String url, @Nullable String login) {
-    Set<GithubAccount> potentialAccounts = myAuthDataProvider.getSuitableAccounts(project, url, login);
+  private fun getProvider(project: Project, url: String, login: String?): InteractiveGitHttpAuthDataProvider? {
+    val potentialAccounts = authDataProvider.getSuitableAccounts(project, url, login)
     if (!potentialAccounts.isEmpty()) {
-      return new InteractiveSelectGithubAccountHttpAuthDataProvider(project, potentialAccounts, myAuthenticationManager);
+      return InteractiveSelectGithubAccountHttpAuthDataProvider(project, potentialAccounts, authenticationManager)
     }
-
     if (GithubServerPath.DEFAULT_SERVER.matches(url)) {
-      return new InteractiveCreateGithubAccountHttpAuthDataProvider(project, myAuthenticationManager,
-                                                                    GithubServerPath.DEFAULT_SERVER, login);
+      return InteractiveCreateGithubAccountHttpAuthDataProvider(project, authenticationManager, GithubServerPath.DEFAULT_SERVER, login)
     }
-
-    return null;
+    return null
   }
 }
