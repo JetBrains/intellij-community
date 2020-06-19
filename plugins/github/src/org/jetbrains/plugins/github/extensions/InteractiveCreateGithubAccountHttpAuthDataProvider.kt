00a1b8fd970d2c27011e0a465f1e850170384e78
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2020-06-19 21:31:45 +0300

    github: Refactor "GithubHttpAuthDataProvider" - move nested class to top level
    (cherry picked from commit 76d1224db367cabb6532c506f8d4c54ef5a5881a)
    
    GitOrigin-RevId: f667e6d2a1d3e2d7e6ea0c03a0160fe562a915dd

--- plugins/github/src/org/jetbrains/plugins/github/extensions/InteractiveCreateGithubAccountHttpAuthDataProvider.kt
+++ plugins/github/src/org/jetbrains/plugins/github/extensions/InteractiveCreateGithubAccountHttpAuthDataProvider.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.github.extensions
 
 import com.intellij.openapi.project.Project
@@ -23,13 +23,13 @@ internal class InteractiveCreateGithubAccountHttpAuthDataProvider(private val pr
       val account = authenticationManager.requestNewAccountForServer(serverPath, project, parentComponent)
                     ?: return null
       val token = getToken(account, parentComponent) ?: return null
-      return GithubHttpAuthDataProvider.GithubAccountAuthData(account, GithubUtil.GIT_AUTH_PASSWORD_SUBSTITUTE, token)
+      return GHAccountAuthData(account, GithubUtil.GIT_AUTH_PASSWORD_SUBSTITUTE, token)
     }
     else {
       val account = authenticationManager.requestNewAccountForServer(serverPath, login, project, parentComponent)
                     ?: return null
       val token = getToken(account, parentComponent) ?: return null
-      return GithubHttpAuthDataProvider.GithubAccountAuthData(account, login, token)
+      return GHAccountAuthData(account, login, token)
     }
   }
 
