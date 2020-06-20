0bd45140796c52da4e67ea7f000b72d7d9aba6a9
Svetlana.Zemlyanskaya <Svetlana.Zemlyanskaya@jetbrains.com>
2020-06-20 15:29:36 +0200

    IDEA-CR-63978 FUS: record anonymized client id in event data if it's not default (IDEA-243603)
    (cherry picked from commit 14e25ab9c6b71c0c00a6e4b6c08b529cae93f23e)
    
    GitOrigin-RevId: 8d177319ac8694150c496cde322d86e44452d31b

--- platform/statistics/src/com/intellij/internal/statistic/eventLog/FeatureUsageData.kt
+++ platform/statistics/src/com/intellij/internal/statistic/eventLog/FeatureUsageData.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.internal.statistic.eventLog
 
+import com.intellij.codeWithMe.ClientId
 import com.intellij.internal.statistic.eventLog.StatisticsEventEscaper.escapeFieldName
 import com.intellij.internal.statistic.utils.PluginInfo
 import com.intellij.internal.statistic.utils.StatisticsUtil
@@ -43,10 +44,33 @@ private val LOG = logger<FeatureUsageData>()
 class FeatureUsageData {
   private var data: MutableMap<String, Any> = HashMap()
 
+  init {
+    val clientId = ClientId.currentOrNull
+    if (clientId != null && clientId != ClientId.defaultLocalId) {
+      addClientId(clientId.value)
+    }
+  }
+
   companion object {
     // don't list "version" as "platformDataKeys" because it format depends a lot on the tool
     val platformDataKeys: List<String> = listOf("plugin", "project", "os", "plugin_type", "lang", "current_file", "input_event", "place",
-                                                "file_path", "anonymous_id")
+                                                "file_path", "anonymous_id", "client_id")
+  }
+
+  fun addClientId(clientId: String?): FeatureUsageData {
+    clientId?.let {
+      val permanentClientId = parsePermanentClientId(clientId)
+      data["client_id"] = EventLogConfiguration.anonymize(permanentClientId)
+    }
+    return this
+  }
+
+  private fun parsePermanentClientId(clientId: String): String {
+    val separator = clientId.indexOf('-')
+    if (separator > 0) {
+      return clientId.substring(0, separator)
+    }
+    return clientId
   }
 
   /**
