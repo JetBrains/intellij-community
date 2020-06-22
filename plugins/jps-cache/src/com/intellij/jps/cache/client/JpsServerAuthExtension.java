da08243726a0ab1ceb9f899fb296cd8997e37c2c
Mikhail Mazurkevich <mikhail.mazurkevich@jetbrains.com>
2020-06-22 13:33:44 +0300

    [jps cache] Update plugin description, add the warning that auth plugin is required
    GitOrigin-RevId: db6061339b61efd0e448553b04c42fb2798cdc1e

--- plugins/jps-cache/src/com/intellij/jps/cache/client/JpsServerAuthExtension.java
+++ plugins/jps-cache/src/com/intellij/jps/cache/client/JpsServerAuthExtension.java
@@ -1,9 +1,11 @@
 package com.intellij.jps.cache.client;
 
 import com.intellij.openapi.Disposable;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.util.Disposer;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 
@@ -13,6 +15,7 @@ import static com.intellij.execution.process.ProcessIOExecutorService.INSTANCE;
  * Extension point which provides authentication data for requests to the JPS cache server
  */
 public interface JpsServerAuthExtension {
+  Logger LOG = Logger.getInstance("com.intellij.jps.cache.client.JpsServerAuthExtension");
   ExtensionPointName<JpsServerAuthExtension> EP_NAME = ExtensionPointName.create("com.intellij.jpsServerAuthExtension");
 
   /**
@@ -35,17 +38,24 @@ public interface JpsServerAuthExtension {
    */
   Map<String, String> getAuthHeader();
 
-  @NotNull
+  @Nullable
   static JpsServerAuthExtension getInstance() {
-    return EP_NAME.findExtensionOrFail(JpsServerAuthExtension.class);
+    return EP_NAME.extensions().findFirst().orElse(null);
   }
 
   static void checkAuthenticatedInBackgroundThread(@NotNull Disposable parentDisposable, @NotNull Runnable onAuthCompleted) {
     Disposable disposable = Disposer.newDisposable();
     Disposer.register(parentDisposable, disposable);
-    INSTANCE.execute(() -> getInstance().checkAuthenticated("Jps Caches Downloader", disposable, () -> {
-      Disposer.dispose(disposable);
-      onAuthCompleted.run();
-    }));
+    JpsServerAuthExtension authExtension = getInstance();
+    if (authExtension == null) {
+      LOG.warn("JetBrains Internal Authentication plugin is required for the correct work. Please enable it.");
+      return;
+    }
+    INSTANCE.execute(() -> {
+      authExtension.checkAuthenticated("Jps Caches Downloader", disposable, () -> {
+        Disposer.dispose(disposable);
+        onAuthCompleted.run();
+      });
+    });
   }
 }
