bb8a32f6b1cb0cba8392d412c6e74783880f7442
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2020-06-23 12:54:51 +0700

    IDEA-CR-1906: getTarget: disable type annotations for compiled elements (EA-232201)
    (cherry picked from commit 034d68de009381b7840018565d969cd3da4ce55e)
    
    GitOrigin-RevId: ce0c53e2e19a1a5277e7666d50ad7979202366e8

--- java/java-psi-api/src/com/intellij/codeInsight/AnnotationTargetUtil.java
+++ java/java-psi-api/src/com/intellij/codeInsight/AnnotationTargetUtil.java
@@ -233,7 +233,9 @@ public class AnnotationTargetUtil {
     if (list == null) return null;
     PsiClass annotationClass = JavaPsiFacade.getInstance(modifierListOwner.getProject())
       .findClass(annotation, modifierListOwner.getResolveScope());
-    if (annotationClass != null && findAnnotationTarget(annotationClass, TargetType.TYPE_USE) != null) {
+    if (annotationClass != null && findAnnotationTarget(annotationClass, TargetType.TYPE_USE) != null &&
+        // External annotations for types are not supported
+        !(modifierListOwner instanceof PsiCompiledElement)) {
       PsiElement parent = list.getParent();
       PsiTypeElement type = null;
       if (parent instanceof PsiMethod) {
