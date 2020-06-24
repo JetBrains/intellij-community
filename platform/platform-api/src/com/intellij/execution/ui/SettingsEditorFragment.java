6c1ce097ed7dee4119c8ca1d5746f5e4c20e4be6
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2020-06-24 14:50:37 +0300

    IDEA-243102 [new run config UI] Removing option doesn't disable the option
    environment parameters fixed
    
    (cherry picked from commit dda80ba962ec1434e22e30a7e3390d3ce558a985)
    
    GitOrigin-RevId: f586f03f58f345b5a3757f7d14973a555dd225f0

--- platform/platform-api/src/com/intellij/execution/ui/SettingsEditorFragment.java
+++ platform/platform-api/src/com/intellij/execution/ui/SettingsEditorFragment.java
@@ -62,24 +62,6 @@ public class SettingsEditorFragment<Settings, C extends JComponent> extends Sett
     this(id, name, group, component, 0, reset, apply, initialSelection);
   }
 
-  public static <S> SettingsEditorFragment<S, ?> create(String id,
-                                                        String name,
-                                                        String group,
-                                                        FragmentComponent<? super S> component) {
-    return createFromComponent(id, name, group, component, s -> false);
-  }
-
-  public static <S> SettingsEditorFragment<S, ?> createFromComponent(String id,
-                                                        String name,
-                                                        String group,
-                                                        FragmentComponent<? super S> component,
-                                                        Predicate<S> initialVisibility) {
-    return new SettingsEditorFragment<>(id, name, group, (JComponent)component,
-                                        (settings, c) -> component.reset(settings),
-                                        (settings, c) -> component.apply(settings),
-                                        initialVisibility);
-  }
-
   public static <S> SettingsEditorFragment<S, ?> createWrapper(String id, String name, String group, @NotNull SettingsEditor<S> inner) {
     JComponent component = inner.getComponent();
     SettingsEditorFragment<S, JComponent> fragment = new SettingsEditorFragment<>(id, name, group, component,
