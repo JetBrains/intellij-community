3b0a1d68e79aabfc5053eae7696412f827d079a0
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2020-06-24 10:33:24 +0300

    [vcs-log] move vcs.log.filter.all key to VcsLogBundle
    GitOrigin-RevId: 95978988fad6a339f380a88026c54cddabf02048

--- platform/vcs-log/impl/src/com/intellij/vcs/log/ui/filter/FilterPopupComponent.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/ui/filter/FilterPopupComponent.java
@@ -5,6 +5,7 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.vcs.VcsBundle;
+import com.intellij.vcs.log.VcsLogBundle;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -19,7 +20,7 @@ abstract class FilterPopupComponent<Filter, Model extends FilterModel<Filter>> e
   /**
    * Special value that indicates that no filtering is on.
    */
-  protected static final Supplier<@Nls String> ALL = VcsBundle.messagePointer("vcs.log.filter.all");
+  protected static final Supplier<@Nls String> ALL = VcsLogBundle.messagePointer("vcs.log.filter.all");
   @NotNull protected final Model myFilterModel;
 
   FilterPopupComponent(@NotNull Supplier<String> displayName, @NotNull Model filterModel) {
