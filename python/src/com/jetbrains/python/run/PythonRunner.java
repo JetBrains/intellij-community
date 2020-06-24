ded750f3940e54eaefc5106cb4939650fd712321
Vladimir Lagunov <vladimir.lagunov@jetbrains.com>
2020-06-24 17:31:46 +0700

    PythonRunner and child classes become asynchronous.
    Before this commit both PythonRunner and PythonRemoteDebugRunner started SSH processes synchronously
    that led to socket operations in EDT. Moreover, synchronous execution prevents to run subprocesses while
    starting remote SSH interpreter.
    
    GitOrigin-RevId: 72364b5faf9883436fb001cc209374ef802712d4

--- python/src/com/jetbrains/python/run/PythonRunner.java
+++ python/src/com/jetbrains/python/run/PythonRunner.java
@@ -2,20 +2,25 @@
 package com.jetbrains.python.run;
 
 import com.intellij.execution.ExecutionException;
-import com.intellij.execution.ExecutionManager;
 import com.intellij.execution.ExecutionResult;
 import com.intellij.execution.configurations.RunProfile;
 import com.intellij.execution.configurations.RunProfileState;
 import com.intellij.execution.configurations.RunnerSettings;
 import com.intellij.execution.executors.DefaultRunExecutor;
+import com.intellij.execution.runners.AsyncProgramRunner;
 import com.intellij.execution.runners.DefaultProgramRunnerKt;
 import com.intellij.execution.runners.ExecutionEnvironment;
-import com.intellij.execution.runners.ProgramRunner;
 import com.intellij.execution.ui.RunContentDescriptor;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.util.concurrency.AppExecutorUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.concurrency.AsyncPromise;
+import org.jetbrains.concurrency.Promise;
 
-public class PythonRunner implements ProgramRunner<RunnerSettings> {
+public class PythonRunner extends AsyncProgramRunner<RunnerSettings> {
   @Override
   @NotNull
   public String getRunnerId() {
@@ -23,28 +28,34 @@ public class PythonRunner implements ProgramRunner<RunnerSettings> {
   }
 
   @Override
-  public final void execute(@NotNull ExecutionEnvironment environment) throws ExecutionException {
-    ExecutionManager.getInstance(environment.getProject()).startRunProfile(environment, state -> {
-      return doExecute(state, environment);
-    });
-  }
-
-  @Override
   public boolean canRun(@NotNull String executorId, @NotNull RunProfile profile) {
     return executorId.equals(DefaultRunExecutor.EXECUTOR_ID) && profile instanceof AbstractPythonRunConfiguration;
   }
 
-  protected RunContentDescriptor doExecute(@NotNull RunProfileState state, @NotNull ExecutionEnvironment env) throws ExecutionException {
+  @NotNull
+  @Override
+  protected Promise<@Nullable RunContentDescriptor> execute(@NotNull ExecutionEnvironment env, @NotNull RunProfileState state) {
     FileDocumentManager.getInstance().saveAllDocuments();
 
-    ExecutionResult executionResult;
-    RunProfile profile = env.getRunProfile();
-    if (state instanceof PythonCommandLineState && profile instanceof CommandLinePatcher) {
-      executionResult = ((PythonCommandLineState)state).execute(env.getExecutor(), (CommandLinePatcher)profile);
-    }
-    else {
-      executionResult = state.execute(env.getExecutor(), this);
-    }
-    return DefaultProgramRunnerKt.showRunContent(executionResult, env);
+    AsyncPromise<RunContentDescriptor> promise = new AsyncPromise<>();
+    AppExecutorUtil.getAppExecutorService().execute(() -> {
+      try {
+        ExecutionResult executionResult;
+        RunProfile profile = env.getRunProfile();
+        if (state instanceof PythonCommandLineState && profile instanceof CommandLinePatcher) {
+          executionResult = ((PythonCommandLineState)state).execute(env.getExecutor(), (CommandLinePatcher)profile);
+        }
+        else {
+          executionResult = state.execute(env.getExecutor(), this);
+        }
+        ApplicationManager.getApplication().invokeLater(
+          () -> promise.setResult(DefaultProgramRunnerKt.showRunContent(executionResult, env)),
+          ModalityState.any());
+      }
+      catch (ExecutionException | RuntimeException err) {
+        promise.setError(err);
+      }
+    });
+    return promise;
   }
 }
