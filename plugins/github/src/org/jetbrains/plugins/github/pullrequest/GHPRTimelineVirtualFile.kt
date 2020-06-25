1b00fc4867c149af19fa1ca80e32c473d58bc16d
Ivan Semenov <ivan.semenov@jetbrains.com>
2020-06-25 20:15:55 +0300

    [github] Fix PR file presentation
    (cherry picked from commit 0939dedf344edb98c9684bcfdebab9469cf62616)
    
    GitOrigin-RevId: 00af03e5a5460b8b40e94777b4071f2bd1baa42d

--- plugins/github/src/org/jetbrains/plugins/github/pullrequest/GHPRTimelineVirtualFile.kt
+++ plugins/github/src/org/jetbrains/plugins/github/pullrequest/GHPRTimelineVirtualFile.kt
@@ -27,7 +27,7 @@ internal class GHPRTimelineVirtualFile(fileManagerId: String,
   }
 
   override fun getName() = "#${pullRequest.number}"
-  override fun getPresentableName() = details?.let { "$name ${it.title}" } ?: name
+  override fun getPresentableName() = details?.let { "${it.title} $name" } ?: name
 
   override fun getPath(): String = GHPRVirtualFileSystem.getPath(fileManagerId, project, repository, pullRequest)
   override fun getPresentablePath() = details?.url ?: "${repository.toUrl()}/pulls/${pullRequest.number}"
