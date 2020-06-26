8db2a28a99fe3697815fd3f4c1952a34977790fd
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-06-26 11:59:08 +0200

    fix ClassWithOnlyPrivateConstructorsInspectionTest
    GitOrigin-RevId: 0fac0b6c8aa2d3ab0098277d23941bba0572f1c0

--- plugins/InspectionGadgets/testsrc/com/siyeh/ig/classlayout/ClassWithOnlyPrivateConstructorsInspectionTest.java
+++ plugins/InspectionGadgets/testsrc/com/siyeh/ig/classlayout/ClassWithOnlyPrivateConstructorsInspectionTest.java
@@ -8,14 +8,14 @@ import org.jetbrains.annotations.Nullable;
 public class ClassWithOnlyPrivateConstructorsInspectionTest extends LightJavaInspectionTestCase {
 
   public void testSimple() {
-    doTest("final class /*Class 'X' with only 'private' constructors should be declared 'final'*/X/**/ {" +
+    doTest("class /*Class 'X' with only 'private' constructors should be declared 'final'*/X/**/ {" +
            "  private X() {}" +
            "  private X(int i) {}" +
            "}");
   }
 
   public void testExtendingInnerClass() {
-    doTest("final class X {\n" +
+    doTest("class X {\n" +
            "  private X() {}\n" +
            "  class Y {\n" +
            "    class Z extends X{}\n" +
@@ -34,7 +34,7 @@ public class ClassWithOnlyPrivateConstructorsInspectionTest extends LightJavaIns
   }
 
   public void testNoWarnOnAnonymInheritor() {
-    doTest("final class X {" +
+    doTest("class X {" +
            "  private X() {}" +
            "  static {new X() {};} " +
            "}");
@@ -57,7 +57,7 @@ public class ClassWithOnlyPrivateConstructorsInspectionTest extends LightJavaIns
 
   public void testSubclassInSameFile() {
     doTest("class Test {" +
-           "    private static final class Inner {" +
+           "    private static class Inner {" +
            "        private Inner() {}" +
            "    }" +
            "    private static class InnerSub extends Inner {}" +
