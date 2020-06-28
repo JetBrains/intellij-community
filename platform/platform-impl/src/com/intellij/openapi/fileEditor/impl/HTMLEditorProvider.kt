c2da087d2445fe13a4870cc8fabf6eb85a565ed2
Dmitry.Krasilschikov <kradima@jetbrains.com>
2020-06-28 18:43:17 +0300

    IDEA-239815 html editor: String timeout callback
    GitOrigin-RevId: 52a304376cb0572f43a322d372a4fa30a7f0d9f1

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/HTMLEditorProvider.kt
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/HTMLEditorProvider.kt
@@ -8,10 +8,8 @@ import com.intellij.openapi.fileEditor.FileEditorProvider
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Key
-import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.testFramework.LightVirtualFile
-import java.util.function.Supplier
 
 class HTMLEditorProvider : FileEditorProvider, DumbAware {
   override fun createEditor(project: Project, file: VirtualFile): FileEditor =
@@ -27,9 +25,9 @@ class HTMLEditorProvider : FileEditorProvider, DumbAware {
     private val URL_KEY: Key<String> = Key.create("URL_KEY")
     private val HTML_KEY: Key<String> = Key.create("HTML_KEY")
 
-    val TIMEOUT_CALLBACK: Key<Supplier<String>> = Key.create("TIMEOUT_CALLBACK")
+    val TIMEOUT_CALLBACK: Key<String> = Key.create("TIMEOUT_CALLBACK")
 
-    fun openEditor(project: Project, title: String, url: String? = null, html: String? = null, timeoutCallback: Supplier<String>? = null) {
+    fun openEditor(project: Project, title: String, url: String? = null, html: String? = null, timeoutCallback: String? = null) {
       val file = LightVirtualFile(title)
       if (url == null && html == null) return
       if (url != null) { file.putUserData(URL_KEY, url) }
