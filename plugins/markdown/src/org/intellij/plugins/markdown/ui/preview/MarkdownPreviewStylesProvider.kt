0152261ac0ba5326bb55d58928dbb64136d90a8b
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2020-06-28 18:32:41 +0300

    IDEA-244717 Swagger: provide CSS styles for Swagger Diff
    GitOrigin-RevId: db1d9774a4a908cf650fcc9e62a09a01c587727b

---
+++ plugins/markdown/src/org/intellij/plugins/markdown/ui/preview/MarkdownPreviewStylesProvider.kt
@@ -0,0 +1,18 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.intellij.plugins.markdown.ui.preview
+
+import com.intellij.openapi.extensions.ExtensionPointName
+import com.intellij.openapi.vfs.VirtualFile
+
+/**
+ * Enables plugins to provide custom CSS styles for markdown preview. Overrides user settings.
+ */
+interface MarkdownPreviewStylesProvider {
+  fun getStyles(file: VirtualFile): String?
+
+  companion object {
+    @get:JvmName("getExtensionPointName")
+    internal val EP_NAME: ExtensionPointName<MarkdownPreviewStylesProvider> =
+      ExtensionPointName.create("org.intellij.markdown.previewStylesProvider")
+  }
+}
