d8aa933c8f4e130541cf326a7c5edc9f9c633f4d
Ivan Semenov <ivan.semenov@jetbrains.com>
2020-06-29 14:27:41 +0300

    [github] Fix API exceptions handling
    Due to incorrect nullability annotations errors were not handled properly
    
    Fixes IDEA-244758
    
    GitOrigin-RevId: 23c607899814e5db571dfa3e33ebc358bd4f7517

--- plugins/github/src/org/jetbrains/plugins/github/api/data/graphql/GHGQLSyntaxError.kt
+++ plugins/github/src/org/jetbrains/plugins/github/api/data/graphql/GHGQLSyntaxError.kt
@@ -1,9 +1,7 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.github.api.data.graphql
 
-class GHGQLSyntaxError(val type: String?, val message: String, val locations: List<Location>) {
-  class Location(val line: Int, val column: Int)
-
+class GHGQLSyntaxError(val type: String?, val message: String) {
   override fun toString(): String = message
 }
 
