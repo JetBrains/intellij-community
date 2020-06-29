8185959f7b01283d9c94e80aab08dac15efbc778
Dmitry.Krasilschikov <kradima@jetbrains.com>
2020-06-29 17:26:43 +0300

    IJP-778 Reader mode: update command highlighting on option turning off/on
    GitOrigin-RevId: 55aa6821b748437c455a20a29b38de2acc681e1e

--- plugins/terminal/src/org/jetbrains/plugins/terminal/TerminalCommandHandlerCustomizer.kt
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/TerminalCommandHandlerCustomizer.kt
@@ -1,14 +1,16 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.terminal
 
-import com.intellij.ide.IdeBundle
 import com.intellij.ide.util.PropertiesComponent
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.options.BeanConfigurable
 import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.options.UnnamedConfigurable
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Getter
 import com.intellij.openapi.util.Setter
+import com.intellij.util.messages.Topic
+import java.util.*
 
 class TerminalCommandHandlerCustomizer : LocalTerminalCustomizer() {
   override fun getConfigurable(project: Project?): UnnamedConfigurable? {
@@ -23,7 +25,12 @@ class TerminalCommandHandlerCustomizer : LocalTerminalCustomizer() {
       get() = PropertiesComponent.getInstance().getBoolean(TERMINAL_CUSTOM_COMMAND_EXECUTION, true)
       set(value) {
         PropertiesComponent.getInstance().setValue(TERMINAL_CUSTOM_COMMAND_EXECUTION, value, true)
+        ApplicationManager.getApplication().messageBus.syncPublisher(TERMINAL_COMMAND_HANDLER_TOPIC).modeChanged()
       }
+  }
+
+  interface TerminalCommandHandlerListener {
+    fun modeChanged()
   }
 
   class TerminalCommandHandlerConfigurable(project: Project) :
@@ -41,5 +48,8 @@ class TerminalCommandHandlerCustomizer : LocalTerminalCustomizer() {
 
   companion object {
     const val TERMINAL_CUSTOM_COMMAND_EXECUTION = "terminalCustomCommandExecutionTurnOff"
+
+    @Topic.AppLevel
+    val TERMINAL_COMMAND_HANDLER_TOPIC = Topic(TerminalCommandHandlerListener::class.java)
   }
 }
