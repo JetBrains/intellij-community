4a5c051db9b95857d86a3b9e7097e55880d823eb
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2020-06-29 21:46:20 +0300

    Add AutoIndentLinesSuggester
    IJ-CR-14522
    
    GitOrigin-RevId: 4c46b44fecdce34f2e6a9f8ffab8af2c2a41bc6c

---
+++ plugins/ide-features-trainer/src/main/kotlin/org/jetbrains/plugins/feature/suggester/suggesters/AutoIndentLinesSuggester.kt
@@ -0,0 +1,40 @@
+package org.jetbrains.plugins.feature.suggester.suggesters
+
+import com.intellij.openapi.command.CommandProcessor
+import com.intellij.psi.PsiWhiteSpace
+import org.jetbrains.plugins.feature.suggester.FeatureSuggester
+import org.jetbrains.plugins.feature.suggester.NoSuggestion
+import org.jetbrains.plugins.feature.suggester.Suggestion
+import org.jetbrains.plugins.feature.suggester.cache.UserActionsCache
+import org.jetbrains.plugins.feature.suggester.cache.UserAnActionsCache
+import org.jetbrains.plugins.feature.suggester.changes.ChildReplacedAction
+
+class AutoIndentLinesSuggester : FeatureSuggester {
+
+    companion object {
+        const val POPUP_MESSAGE = "Why not use the Auto-Indent feature? (Ctrl + Shift + I)"
+    }
+
+    override fun getSuggestion(actions: UserActionsCache, anActions: UserAnActionsCache): Suggestion {
+        val command = CommandProcessor.getInstance().currentCommand
+        if (command != null) return NoSuggestion
+        val lastAction = actions.last()
+        if (lastAction is ChildReplacedAction) {
+            val newChild = lastAction.newChild
+            val oldChild = lastAction.oldChild
+            if (newChild is PsiWhiteSpace && oldChild is PsiWhiteSpace) {
+                val newChildText = newChild.text
+                val oldChildText = oldChild.text
+                if (newChildText.contains('\n')
+                    && newChildText.count { it == '\n' } == oldChildText.count { it == '\n' }
+                    && newChildText.takeLastWhile { it != '\n' }.length != oldChildText.takeLastWhile { it != '\n' }.length
+                    && newChild.nextSibling != null && newChild.nextSibling !is PsiWhiteSpace) {
+                    return createSuggestion(null, POPUP_MESSAGE)
+                }
+            }
+        }
+        return NoSuggestion
+    }
+
+    override fun getId(): String = "AutoIndent lines suggester"
+}
