d1399c8949f79878570802271d58edf168ffcd6d
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-06-29 14:50:36 +0200

    FileLoadingTracker - avoid trove dependency, use empty map if nothing requested to track
    GitOrigin-RevId: 0505c462a0c947a553db92ecd6c45181df5e6986

--- platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/impl/FileLoadingTracker.java
+++ platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/impl/FileLoadingTracker.java
@@ -2,16 +2,15 @@
 package com.intellij.openapi.vfs.newvfs.impl;
 
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.util.text.FilePathHashingStrategy;
-import gnu.trove.THashSet;
-import gnu.trove.TIntHashSet;
+import com.intellij.util.containers.CollectionFactory;
+import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
+import it.unimi.dsi.fastutil.ints.IntSet;
+import it.unimi.dsi.fastutil.ints.IntSets;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.MissingResourceException;
 import java.util.Set;
 
 /**
@@ -19,22 +18,27 @@ import java.util.Set;
  */
 final class FileLoadingTracker {
   private static final Logger LOG = Logger.getInstance(FileLoadingTracker.class);
-  private static final Set<String> ourPaths = new THashSet<>(getPathsToTrack(), FilePathHashingStrategy.create());
-  private static final TIntHashSet ourLeafNameIds = new TIntHashSet(ourPaths.stream().mapToInt(path -> FileNameCache.storeName(StringUtil.getShortName(path, '/'))).toArray());
+  private static final Set<String> ourPaths;
+  private static final IntSet ourLeafNameIds;
 
-  @NotNull
-  private static List<String> getPathsToTrack() {
-    try {
-      return StringUtil.split(Registry.stringValue("file.system.trace.loading"), ";");
+  static {
+    List<String> pathsToTrack = StringUtil.split(System.getProperty("file.system.trace.loading", ""), ";");
+    if (pathsToTrack.isEmpty()) {
+      ourPaths = Collections.emptySet();
+      ourLeafNameIds = IntSets.EMPTY_SET;
     }
-    catch (MissingResourceException e) {
-      return Collections.emptyList();
+    else {
+      ourPaths = CollectionFactory.createFilePathSet(pathsToTrack);
+      ourLeafNameIds = new IntOpenHashSet(ourPaths.size());
+      for (String path : ourPaths) {
+        ourLeafNameIds.add(FileNameCache.storeName(StringUtil.getShortName(path, '/')));
+      }
     }
   }
 
   static void fileLoaded(@NotNull VirtualDirectoryImpl parent, int nameId) {
     if (ourLeafNameIds.contains(nameId)) {
-      String path = parent.getPath() + "/" + FileNameCache.getVFileName(nameId).toString();
+      String path = parent.getPath() + '/' + FileNameCache.getVFileName(nameId);
       if (ourPaths.contains(path)) {
         LOG.info("Loading " + path, new Throwable());
       }
