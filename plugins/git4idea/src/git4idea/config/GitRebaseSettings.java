6032a966d3ec89a8ef03780466279fd4dd7131be
Daniil Tsaryov <daniil.tsaryov@jetbrains.com>
2020-06-29 09:34:42 +0300

    [vcs] IDEA-244365 Remember settings in git rebase dialog
    (cherry picked from commit 33db2328725b09a8120e6b2bc8c6839e6e4bb4b6)
    
    GitOrigin-RevId: b6cab0dd0e32fc04b848209c07fc134bef4b44f7

---
+++ plugins/git4idea/src/git4idea/config/GitRebaseSettings.java
@@ -0,0 +1,59 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package git4idea.config;
+
+import com.google.common.collect.ImmutableSet;
+import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.components.StoragePathMacros;
+import git4idea.rebase.RebaseOption;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.EnumSet;
+import java.util.Set;
+
+@State(name = "Git.Rebase.Settings", storages = @Storage(StoragePathMacros.WORKSPACE_FILE))
+public class GitRebaseSettings implements PersistentStateComponent<GitRebaseSettings.State> {
+
+  private final static Set<RebaseOption> DEFAULT_OPTIONS = EnumSet.of(RebaseOption.INTERACTIVE);
+  private final static Set<RebaseOption> NO_OPTIONS = EnumSet.noneOf(RebaseOption.class);
+
+  private State myState = new State();
+
+  public static class State {
+    public Set<RebaseOption> OPTIONS = DEFAULT_OPTIONS;
+    public String NEW_BASE = null;
+  }
+
+  @Nullable
+  @Override
+  public State getState() {
+    return myState;
+  }
+
+  @Override
+  public void loadState(@NotNull State state) {
+    myState = state;
+  }
+
+  @NotNull
+  public Set<RebaseOption> getOptions() {
+    return ImmutableSet.copyOf(myState.OPTIONS);
+  }
+
+  public void setOptions(@NotNull Set<RebaseOption> options) {
+    myState.OPTIONS = !options.isEmpty()
+                      ? EnumSet.copyOf(options)
+                      : NO_OPTIONS;
+  }
+
+  @Nullable
+  public String getNewBase() {
+    return myState.NEW_BASE;
+  }
+
+  public void setNewBase(@Nullable String newBase) {
+    myState.NEW_BASE = newBase;
+  }
+}
