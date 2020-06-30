71b4c19f9f7811b3d45408a92a90e7974eb91215
Alex Plate <aleksei.plate@jetbrains.com>
2020-06-30 17:39:29 +0300

    [Workspace Model] Add an indicator that shows if model is enabled
    GitOrigin-RevId: a8a61f3052480a423af2dbd0eb20c60e7e911e6b

---
+++ platform/workspaceModel/ide/src/com/intellij/workspaceModel/ide/Indicator.kt
@@ -0,0 +1,63 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.workspaceModel.ide
+
+import com.intellij.icons.AllIcons
+import com.intellij.icons.AllIcons.Debugger.Db_set_breakpoint
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.registry.Registry
+import com.intellij.openapi.wm.StatusBar
+import com.intellij.openapi.wm.StatusBarWidget
+import com.intellij.openapi.wm.StatusBarWidgetFactory
+import com.intellij.util.Consumer
+import com.intellij.workspaceModel.ide.impl.legacyBridge.LegacyBridgeProjectLifecycleListener
+import java.awt.event.MouseEvent
+import javax.swing.Icon
+
+class WorkspaceModelIndicator : StatusBarWidgetFactory {
+  override fun getId(): String = "WorkspaceModelIndicator"
+
+  override fun getDisplayName(): String {
+    return "Workspace model"
+  }
+
+  override fun isAvailable(project: Project): Boolean {
+    return Registry.`is`("ide.new.project.model.indicator")
+  }
+
+  override fun createWidget(project: Project): StatusBarWidget {
+    return object : StatusBarWidget.IconPresentation, StatusBarWidget {
+      override fun ID(): String {
+        return "Workspace Model widget indicator"
+      }
+
+      override fun install(statusBar: StatusBar) {
+      }
+
+      override fun dispose() {
+      }
+
+      override fun getTooltipText(): String? {
+        return if (LegacyBridgeProjectLifecycleListener.enabled(project)) "Enabled" else "disabled"
+      }
+
+      override fun getIcon(): Icon? {
+        return if (LegacyBridgeProjectLifecycleListener.enabled(project)) Db_set_breakpoint else AllIcons.Debugger.MuteBreakpoints
+      }
+
+      override fun getClickConsumer(): Consumer<MouseEvent>? {
+        return null
+      }
+
+      override fun getPresentation(): StatusBarWidget.WidgetPresentation? {
+        return this
+      }
+    }
+  }
+
+  override fun disposeWidget(widget: StatusBarWidget) {
+  }
+
+  override fun canBeEnabledOn(statusBar: StatusBar): Boolean {
+    return true
+  }
+}
