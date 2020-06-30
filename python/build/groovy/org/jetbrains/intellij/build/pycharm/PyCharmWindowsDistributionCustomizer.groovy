3b46e3c8d7dd55dc7de7d86bccff7f7cc370e68f
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2020-06-30 12:01:17 +0300

    IDEA-CR-64387: PY-43190 Implement user feedback survey to Windows Uninstaller in PyCharm
    (cherry picked from commit 023e2dd98419be6cd80e7fe107d3ce413b849f1e)
    
    GitOrigin-RevId: efc80768f59e2cbf54911b3eb8e3dac93520cb1f

--- python/build/groovy/org/jetbrains/intellij/build/pycharm/PyCharmWindowsDistributionCustomizer.groovy
+++ python/build/groovy/org/jetbrains/intellij/build/pycharm/PyCharmWindowsDistributionCustomizer.groovy
@@ -1,6 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.intellij.build.pycharm
 
+import org.jetbrains.intellij.build.ApplicationInfoProperties
 import org.jetbrains.intellij.build.BuildContext
 import org.jetbrains.intellij.build.WindowsDistributionCustomizer
 
@@ -14,5 +15,10 @@ class PyCharmWindowsDistributionCustomizer extends WindowsDistributionCustomizer
         include(name: "skeletons-win*.zip")
       }
     }
+  }
+
+  @Override
+  String getUninstallFeedbackPageUrl(ApplicationInfoProperties applicationInfo) {
+    "https://www.jetbrains.com/pycharm/uninstall/"
   }
 }
