1eedbe58b45341f0915ea1a60777b04a946519b3
Dmitriy.Panov <Dmitriy.Panov@jetbrains.com>
2020-07-01 21:03:31 +0300

    IJI-243 jps cache warmup moved out of CompilationContext constructor and is explicitly performed
    GitOrigin-RevId: ab53b12d5566c40a543ccd1f84cd63f11cffa33d

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/CompilationContextImpl.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/CompilationContextImpl.groovy
@@ -9,7 +9,6 @@ import groovy.transform.CompileDynamic
 import groovy.transform.CompileStatic
 import org.jetbrains.intellij.build.*
 import org.jetbrains.intellij.build.impl.compilation.CompilationPartsUtil
-import org.jetbrains.intellij.build.impl.compilation.PortableCompilationCache
 import org.jetbrains.intellij.build.impl.logging.BuildMessagesImpl
 import org.jetbrains.jps.model.JpsElementFactory
 import org.jetbrains.jps.model.JpsGlobal
@@ -80,8 +79,6 @@ class CompilationContextImpl implements CompilationContext {
                                              buildOutputRootEvaluator, options)
     context.prepareForBuild()
     messages.debugLogPath = "$context.paths.buildOutputRoot/log/debug.log"
-    def jpsCache = new PortableCompilationCache(context)
-    if (jpsCache.canBeUsed) jpsCache.warmUp()
     return context
   }
 
