61c49f0392d6272abecf4f766cd24817aa15919d
Bas Leijdekkers <basleijdekkers@gmail.com>
2020-07-01 17:19:47 +0200

    IG: use result of method call
    GitOrigin-RevId: 7f2470edfc0c0f66fed406e0c163d297687db8d2

--- plugins/InspectionGadgets/src/com/siyeh/ig/abstraction/TypeMayBeWeakenedInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/abstraction/TypeMayBeWeakenedInspection.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2006-2019 Bas Leijdekkers
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.siyeh.ig.abstraction;
 
 import com.intellij.codeInsight.daemon.impl.UnusedSymbolUtil;
@@ -101,7 +87,7 @@ public class TypeMayBeWeakenedInspection extends AbstractBaseJavaLocalInspection
     @Override
     public String getName() {
       if (myCandidates.size() == 1) {
-        InspectionGadgetsBundle.message("inspection.type.may.be.weakened.add.stopper.single", myCandidates.get(0));
+        return InspectionGadgetsBundle.message("inspection.type.may.be.weakened.add.stopper.single", myCandidates.get(0));
       }
       return InspectionGadgetsBundle.message("inspection.type.may.be.weakened.add.stopper");
     }
