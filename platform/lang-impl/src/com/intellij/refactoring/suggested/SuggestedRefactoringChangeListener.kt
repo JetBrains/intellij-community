4a2f9e326c9aa529a2263e7298917c9b5d158bec
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2020-07-01 15:02:05 +0200

    IDEA-CR-64447: [platform] ignoring document events from binary files in suggested refactoring listener (IDEA-238706)
    GitOrigin-RevId: 420d2f302137ba6df1db11a314d009004767afea

--- platform/lang-impl/src/com/intellij/refactoring/suggested/SuggestedRefactoringChangeListener.kt
+++ platform/lang-impl/src/com/intellij/refactoring/suggested/SuggestedRefactoringChangeListener.kt
@@ -180,7 +180,7 @@ class SuggestedRefactoringChangeListener(
     override fun beforeDocumentChange(event: DocumentEvent) {
       val document = event.document
       val psiFile = psiDocumentManager.getCachedPsiFile(document) ?: return
-      if (!psiFile.isPhysical || psiFile is PsiCodeFragment) return
+      if (!psiFile.isPhysical || psiFile.fileType.isBinary || psiFile is PsiCodeFragment) return
 
       val firstChangeInsideCommand = isFirstChangeInsideCommand
       isFirstChangeInsideCommand = false
@@ -220,7 +220,7 @@ class SuggestedRefactoringChangeListener(
 
       val document = event.document
       val psiFile = psiDocumentManager.getCachedPsiFile(document) ?: return
-      if (!psiFile.isPhysical || psiFile is PsiCodeFragment) return
+      if (!psiFile.isPhysical || psiFile.fileType.isBinary || psiFile is PsiCodeFragment) return
 
       newIdentifierWatcher.documentChanged(event, psiFile.language)
 
