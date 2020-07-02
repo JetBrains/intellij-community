d9b702b21ee9aac199b0e3f959ec10ce154aac23
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-07-02 15:39:01 +0200

    cleanup - inline XmlBasedSettingsImpl
    GitOrigin-RevId: 3ce5f9cb8379db7f510d82149d95dcf5f1435a36

--- platform/lang-impl/src/com/intellij/conversion/impl/ComponentManagerSettingsImpl.java
+++ platform/lang-impl/src/com/intellij/conversion/impl/ComponentManagerSettingsImpl.java
@@ -3,19 +3,35 @@ package com.intellij.conversion.impl;
 
 import com.intellij.conversion.CannotConvertException;
 import com.intellij.conversion.ComponentManagerSettings;
+import com.intellij.conversion.XmlBasedSettings;
 import org.jdom.Element;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
 import java.nio.file.Path;
 
-public class ComponentManagerSettingsImpl extends XmlBasedSettingsImpl implements ComponentManagerSettings {
+public class ComponentManagerSettingsImpl implements ComponentManagerSettings, XmlBasedSettings {
+  protected final SettingsXmlFile mySettingsFile;
+  protected final ConversionContextImpl myContext;
+
   protected ComponentManagerSettingsImpl(@NotNull Path file, @NotNull ConversionContextImpl context) throws CannotConvertException {
-    super(file, context);
+    myContext = context;
+    mySettingsFile = context.getOrCreateFile(file);
   }
 
   @Override
   public Element getComponentElement(@NotNull @NonNls String componentName) {
     return mySettingsFile.findComponent(componentName);
+  }
+
+  @Override
+  @NotNull
+  public Element getRootElement() {
+    return mySettingsFile.getRootElement();
+  }
+
+  @Override
+  public @NotNull Path getPath() {
+    return mySettingsFile.getFile();
   }
 }
