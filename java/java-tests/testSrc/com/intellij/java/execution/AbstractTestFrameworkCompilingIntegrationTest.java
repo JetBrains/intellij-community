e5578994512349102f9ff8c5cc5a52bb51fc775c
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-07-02 14:44:42 +0200

    fix TestNGModuleInfoIntegrationTest
    GitOrigin-RevId: 496f4ace75be8a1b567936f891783c6866d88ecf

--- java/java-tests/testSrc/com/intellij/java/execution/AbstractTestFrameworkCompilingIntegrationTest.java
+++ java/java-tests/testSrc/com/intellij/java/execution/AbstractTestFrameworkCompilingIntegrationTest.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.java.execution;
 
 import com.intellij.application.options.PathMacrosImpl;
@@ -17,6 +17,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public abstract class AbstractTestFrameworkCompilingIntegrationTest extends AbstractTestFrameworkIntegrationTest {
+  @Override
+  protected boolean isCreateProjectFileExplicitly() {
+    return false;
+  }
+
   private CompilerTester myCompilerTester;
 
   protected abstract String getTestContentRoot();
