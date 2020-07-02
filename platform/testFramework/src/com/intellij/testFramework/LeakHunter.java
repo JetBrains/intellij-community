026c4c4747b03de85a9c5ffffb73bd8452db3f53
Alexey Kudravtsev <cdr@intellij.com>
2020-07-02 13:55:44 +0300

    fix compilation
    GitOrigin-RevId: 530681abfa2f4c5e2f6d4e036ede0140ebf3a691

--- platform/testFramework/src/com/intellij/testFramework/LeakHunter.java
+++ platform/testFramework/src/com/intellij/testFramework/LeakHunter.java
@@ -69,10 +69,10 @@ public final class LeakHunter {
    * Checks if there is a memory leak if an object of type {@code suspectClass} is strongly accessible via references from the {@code root} object.
    */
   @TestOnly
-  static <T> void processLeaks(@NotNull Supplier<? extends Map<Object, String>> rootsSupplier,
-                               @NotNull Class<T> suspectClass,
-                               @Nullable final Predicate<? super T> isReallyLeak,
-                               @NotNull final PairProcessor<? super T, Object> processor) throws AssertionError {
+  public static <T> void processLeaks(@NotNull Supplier<? extends Map<Object, String>> rootsSupplier,
+                                      @NotNull Class<T> suspectClass,
+                                      @Nullable final Predicate<? super T> isReallyLeak,
+                                      @NotNull final PairProcessor<? super T, Object> processor) throws AssertionError {
     if (SwingUtilities.isEventDispatchThread()) {
       UIUtil.dispatchAllInvocationEvents();
     }
