a82b03d185b1375b68f1a66aed14de3a82829976
Roman.Ivanov <Roman.Ivanov@jetbrains.com>
2020-07-02 15:20:06 +0700

    Inlays: do not show providers with !isVisibleInSettings
    GitOrigin-RevId: 0d834b4354d26c879d2d095af6ec927486e9d1ca

--- java/java-impl/src/com/intellij/codeInsight/daemon/impl/JavaLensProvider.java
+++ java/java-impl/src/com/intellij/codeInsight/daemon/impl/JavaLensProvider.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.daemon.impl;
 
 import com.intellij.codeInsight.daemon.GutterIconNavigationHandler;
@@ -233,6 +233,6 @@ public class JavaLensProvider implements InlayHintsProvider<JavaLensSettings> {
 
   @Override
   public boolean isVisibleInSettings() {
-    return false;
+    return true;
   }
 }
