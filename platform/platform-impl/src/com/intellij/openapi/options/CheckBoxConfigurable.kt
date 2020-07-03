818199c3865d9914d2c4abbca565d6ba19f3e5af
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2020-07-04 02:05:28 +0300

    IDEA-142616: Redesign File Colors settings page
    GitOrigin-RevId: c03e7b73bd7589c11ea536c133db81c66ea0765a

---
+++ platform/platform-impl/src/com/intellij/openapi/options/CheckBoxConfigurable.kt
@@ -0,0 +1,37 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.options
+
+import javax.swing.JCheckBox
+
+abstract class CheckBoxConfigurable : UnnamedConfigurable {
+  protected abstract fun createCheckBox(): JCheckBox
+  protected abstract var selectedState: Boolean
+
+  var checkBox: JCheckBox? = null
+    private set
+
+
+  override fun createComponent(): JCheckBox {
+    val checkBox = createCheckBox()
+    this.checkBox = checkBox
+    return checkBox
+  }
+
+  override fun isModified(): Boolean {
+    val selected = checkBox?.isSelected ?: return false
+    return selected != selectedState
+  }
+
+  override fun apply() {
+    val selected = checkBox?.isSelected ?: return
+    selectedState = selected
+  }
+
+  override fun reset() {
+    checkBox?.isSelected = selectedState
+  }
+
+  override fun disposeUIResources() {
+    checkBox = null
+  }
+}
