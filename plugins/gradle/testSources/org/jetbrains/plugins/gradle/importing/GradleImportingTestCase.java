016e64929cee105e39935992b8bbab41fedeaf37
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2020-07-03 12:00:49 +0300

    [Gradle tests] do not run in dispatch thread importing tests
    GitOrigin-RevId: 53dce42da67211700a6ee230c2c6cc9982ae7a26

--- plugins/gradle/testSources/org/jetbrains/plugins/gradle/importing/GradleImportingTestCase.java
+++ plugins/gradle/testSources/org/jetbrains/plugins/gradle/importing/GradleImportingTestCase.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.gradle.importing;
 
 import com.intellij.execution.RunManagerEx;
@@ -119,6 +119,11 @@ public abstract class GradleImportingTestCase extends ExternalSystemImportingTes
     UnknownSdkResolver.EP_NAME.getPoint().registerExtension(TestUnknownSdkResolver.INSTANCE, getTestRootDisposable());
 
     TestUnknownSdkResolver.INSTANCE.setUnknownSdkFixMode(TestUnknownSdkResolver.TestUnknownSdkFixMode.REAL_LOCAL_FIX);
+  }
+
+  @Override
+  protected boolean runInDispatchThread() {
+    return false;
   }
 
   @NotNull
