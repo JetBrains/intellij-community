485d97dfd16956c6d209ff6e05d8781b08c6f2ea
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2020-07-06 18:42:36 +0300

    [workspace model] refactoring: move WorkspaceModelIndicator to impl package and use proper file name
    GitOrigin-RevId: 7dc9ac543287eaad69bd5483d190eaa0e3c6fceb

--- platform/workspaceModel/ide/src/com/intellij/workspaceModel/ide/impl/WorkspaceModelIndicator.kt
+++ platform/workspaceModel/ide/src/com/intellij/workspaceModel/ide/impl/WorkspaceModelIndicator.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.workspaceModel.ide
+package com.intellij.workspaceModel.ide.impl
 
 import com.intellij.icons.AllIcons
 import com.intellij.icons.AllIcons.Debugger.Db_set_breakpoint
