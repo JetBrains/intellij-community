48facaffd6b839c86ac42643f10fcbfbfd93c0c2
Egor Ushakov <egor.ushakov@jetbrains.com>
2020-07-06 18:34:07 +0300

    fixed flaky debugger test
    GitOrigin-RevId: 45fbd7aa4fa48853528b175a3541a9c697648b28

--- java/testFramework/src/com/intellij/debugger/ExecutionWithDebuggerToolsTestCase.java
+++ java/testFramework/src/com/intellij/debugger/ExecutionWithDebuggerToolsTestCase.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.debugger;
 
 import com.intellij.debugger.engine.*;
@@ -235,7 +235,7 @@ public abstract class ExecutionWithDebuggerToolsTestCase extends ExecutionTestCa
     println("frameProxy(" + frameIndex + ") = " + method, ProcessOutputTypes.SYSTEM);
   }
 
-  private static String toDisplayableString(SourcePosition sourcePosition) {
+  protected static String toDisplayableString(SourcePosition sourcePosition) {
     int line = sourcePosition.getLine();
     if (line >= 0) {
       line++;
