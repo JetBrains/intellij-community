45143a260b351dd08b0edc57e2dd1299b622e3db
Mikhail Sokolov <mikhail.sokolov@jetbrains.com>
2020-07-06 16:39:19 +0300

    _LastInSuiteTest.testProjectLeak fix
    GitOrigin-RevId: 4e3653ebc8a8ff4ed8a69a969c41023741ecb8bd

--- java/java-tests/testSrc/com/intellij/java/navigation/SearchEverywhereTest.groovy
+++ java/java-tests/testSrc/com/intellij/java/navigation/SearchEverywhereTest.groovy
@@ -164,9 +164,11 @@ class SearchEverywhereTest extends LightJavaCodeInsightFixtureTestCase {
       def file6 = myFixture.addFileToProject("SecondAppFile.txt", "")
       def wrongFile = myFixture.addFileToProject("wrong.txt", "")
 
+      def recentFilesContributor = new RecentFilesSEContributor(ChooseByNameTest.createEvent(project))
+      Disposer.register(project, recentFilesContributor)
       def ui = createTestUI([
         ChooseByNameTest.createFileContributor(project),
-        new RecentFilesSEContributor(ChooseByNameTest.createEvent(project))
+        recentFilesContributor
       ])
 
       def future = ui.findElementsForPattern("appfile")
