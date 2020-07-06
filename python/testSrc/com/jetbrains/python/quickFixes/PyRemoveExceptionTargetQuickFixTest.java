0d0afc705af279a32e0b991b0a6746a1f63d7da3
andrey.matveev <andrey.matveev@jetbrains.com>
2020-07-06 18:17:54 +0700

    [PY-20893] Fix removing full block by quick fix unused local variable for exception
    GitOrigin-RevId: 9b46b745f50882828c9ea8689f44ef1871f6f7b8

---
+++ python/testSrc/com/jetbrains/python/quickFixes/PyRemoveExceptionTargetQuickFixTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2000-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jetbrains.python.quickFixes;
+
+import com.intellij.testFramework.TestDataPath;
+import com.jetbrains.python.PyPsiBundle;
+import com.jetbrains.python.PyQuickFixTestCase;
+import com.jetbrains.python.inspections.unusedLocal.PyUnusedLocalInspection;
+import com.jetbrains.python.psi.LanguageLevel;
+
+@TestDataPath("$CONTENT_ROOT/../testData//quickFixes/PyRemoveTargetExpressionQuickFixTest/")
+public class PyRemoveExceptionTargetQuickFixTest extends PyQuickFixTestCase {
+  // PY-20893
+  public void testExcept() {
+    doQuickFixTest(PyUnusedLocalInspection.class, PyPsiBundle.message("QFIX.NAME.remove.target.expr"));
+  }
+
+  public void testExcept2() {
+    runWithLanguageLevel(LanguageLevel.PYTHON27,
+                         () -> doQuickFixTest(PyUnusedLocalInspection.class, PyPsiBundle.message("QFIX.NAME.remove.target.expr")));
+  }
+}
