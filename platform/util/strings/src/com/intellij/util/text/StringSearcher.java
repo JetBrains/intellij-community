f98c6909102ddd365ada8f8524e6ce22f94ddbe9
Alexander Zolotov <zolotov@jetbrains.com>
2020-07-07 13:42:09 +0300

    [platform] move StringSearcher to `strings` module
    GitOrigin-RevId: 335ea06cc4878bd08d937fafa94e0fe01fe53ca9

--- platform/util/strings/src/com/intellij/util/text/StringSearcher.java
+++ platform/util/strings/src/com/intellij/util/text/StringSearcher.java
@@ -1,21 +1,7 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.text;
 
-import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.util.text.Strings;
 import gnu.trove.TIntArrayList;
 import gnu.trove.TIntProcedure;
 import org.jetbrains.annotations.NotNull;
@@ -56,10 +42,10 @@ public class StringSearcher {
     myPattern = pattern;
     myCaseSensitive = caseSensitive;
     myForwardDirection = forwardDirection;
-    char[] chars = myCaseSensitive ? myPattern.toCharArray() : StringUtil.toLowerCase(myPattern).toCharArray();
+    char[] chars = myCaseSensitive ? myPattern.toCharArray() : Strings.toLowerCase(myPattern).toCharArray();
     if (chars.length != myPattern.length()) {
       myLowercaseTransform = false;
-      chars = StringUtil.toUpperCase(myPattern).toCharArray();
+      chars = Strings.toUpperCase(myPattern).toCharArray();
     } else {
       myLowercaseTransform = true;
     }
@@ -132,7 +118,7 @@ public class StringSearcher {
     if (myForwardDirection) {
       if (myPatternLength == 1) {
         // optimization
-        return StringUtil.indexOf(text, myPatternArray[0], _start, _end, myCaseSensitive);
+        return Strings.indexOf(text, myPatternArray[0], _start, _end, myCaseSensitive);
       }
       int start = _start;
       int end = _end - myPatternLength;
@@ -205,13 +191,13 @@ public class StringSearcher {
     if (myCaseSensitive) {
       return lastChar;
     }
-    return myLowercaseTransform ? StringUtil.toLowerCase(lastChar) : StringUtil.toUpperCase(lastChar);
+    return myLowercaseTransform ? Strings.toLowerCase(lastChar) : Strings.toUpperCase(lastChar);
   }
 
   private boolean isSameChar(char charInPattern, char charInText) {
     boolean sameChar = charInPattern == charInText;
     if (!sameChar && !myCaseSensitive) {
-      return StringUtil.charsEqualIgnoreCase(charInPattern, charInText);
+      return Strings.charsEqualIgnoreCase(charInPattern, charInText);
     }
     return sameChar;
   }
