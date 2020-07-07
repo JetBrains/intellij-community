992f88d6d406605efffbde89d4ce2ce81f0ddc79
Yann Cébron <yann.cebron@jetbrains.com>
2020-07-07 15:46:20 +0200

    [devkit] UsePrimitiveTypesInspection -> UAST (IDEA-189985)
    GitOrigin-RevId: fdad88f9eef8c14461be018adb3c99b4cdf766e1

--- plugins/devkit/devkit-core/src/inspections/internal/UsePrimitiveTypesInspection.java
+++ plugins/devkit/devkit-core/src/inspections/internal/UsePrimitiveTypesInspection.java
@@ -1,128 +1,74 @@
-/*
- * Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.devkit.inspections.internal;
 
-import com.intellij.codeInspection.LocalQuickFix;
-import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.codeInspection.ProblemsHolder;
-import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
-import com.intellij.psi.tree.IElementType;
-import com.intellij.psi.util.PsiUtil;
-import com.siyeh.IntentionPowerPackBundle;
-import com.siyeh.ig.PsiReplacementUtil;
-import com.siyeh.ig.psiutils.CommentTracker;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NonNls;
+import com.intellij.uast.UastHintedVisitorAdapter;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.idea.devkit.inspections.DevKitInspectionBase;
+import org.jetbrains.idea.devkit.inspections.DevKitUastInspectionBase;
+import org.jetbrains.uast.*;
+import org.jetbrains.uast.visitor.AbstractUastNonRecursiveVisitor;
 
-public class UsePrimitiveTypesInspection extends DevKitInspectionBase {
+public class UsePrimitiveTypesInspection extends DevKitUastInspectionBase {
+
   @Override
-  public PsiElementVisitor buildInternalVisitor(@NotNull final ProblemsHolder holder, final boolean isOnTheFly) {
-    return new JavaElementVisitor() {
+  public PsiElementVisitor buildInternalVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
+    if (JavaPsiFacade.getInstance(holder.getProject()).findClass(PsiType.class.getName(),
+                                                                 holder.getFile().getResolveScope()) == null) {
+      return PsiElementVisitor.EMPTY_VISITOR;
+    }
+
+    return UastHintedVisitorAdapter.create(holder.getFile().getLanguage(), new AbstractUastNonRecursiveVisitor() {
+
       @Override
-      public void visitBinaryExpression(PsiBinaryExpression expression) {
-        super.visitBinaryExpression(expression);
-        final IElementType tokenType = expression.getOperationTokenType();
-        if (tokenType.equals(JavaTokenType.EQEQ) || tokenType.equals(JavaTokenType.NE)) {
-          final PsiExpression lOperand = expression.getLOperand();
-          final PsiExpression rOperand = expression.getROperand();
-          if (rOperand != null && (isPrimitiveTypeRef(lOperand) || isPrimitiveTypeRef(rOperand))) {
-            final String name;
-            if (JavaTokenType.NE.equals(tokenType)) {
-              name = IntentionPowerPackBundle.message("replace.equality.with.not.equals.intention.name");
-            }
-            else {
-              name = IntentionPowerPackBundle.message("replace.equality.with.equals.intention.name");
-            }
-            holder.registerProblem(expression.getOperationSign(),
-                                   "Primitive types should be compared with .equals",
-                                   new ReplaceEqualityWithEqualsFix(name));
-          }
-        }
+      public boolean visitBinaryExpression(@NotNull UBinaryExpression node) {
+        inspectBinaryExpression(node, holder);
+
+        return true;
       }
-    };
+    }, new Class[]{UBinaryExpression.class});
   }
 
-  private static boolean isPrimitiveTypeRef(PsiExpression expression) {
-    if (expression instanceof PsiReferenceExpression) {
-      final PsiElement target = ((PsiReferenceExpression)expression).resolve();
-      if (target instanceof PsiField) {
-        final PsiField psiField = (PsiField)target;
-        if (!psiField.hasModifierProperty(PsiModifier.STATIC) ||
-            !psiField.hasModifierProperty(PsiModifier.FINAL)) {
-          return false;
-        }
+  private static void inspectBinaryExpression(UBinaryExpression uBinaryExpression, ProblemsHolder holder) {
+    final UastBinaryOperator operator = uBinaryExpression.getOperator();
+    if (operator != UastBinaryOperator.IDENTITY_EQUALS &&
+        operator != UastBinaryOperator.IDENTITY_NOT_EQUALS) {
+      return;
+    }
 
-        final PsiClass containingClass = psiField.getContainingClass();
-        return containingClass != null &&
-               PsiType.class.getName().equals(containingClass.getQualifiedName()) &&
-               !"NULL".equals(psiField.getName());
+    final UExpression lhs = uBinaryExpression.getLeftOperand();
+    final UExpression rhs = uBinaryExpression.getRightOperand();
+    if (lhs instanceof ULiteralExpression || rhs instanceof ULiteralExpression ||
+        lhs instanceof UThisExpression || rhs instanceof UThisExpression) {
+      return;
+    }
+
+    if (isPrimitivePsiTypeReference(lhs) || isPrimitivePsiTypeReference(rhs)) {
+      final PsiElement sourcePsi = uBinaryExpression.getSourcePsi();
+      if (sourcePsi == null) return;
+      holder.registerProblem(sourcePsi, "Primitive types should be compared by equals()");
+    }
+  }
+
+  private static boolean isPrimitivePsiTypeReference(UExpression uExpression) {
+    if (!(uExpression instanceof UReferenceExpression)) {
+      return false;
+    }
+    UReferenceExpression uReferenceExpression = (UReferenceExpression)uExpression;
+    final PsiElement resolve = uReferenceExpression.resolve();
+
+    if (resolve instanceof PsiField) {
+      final PsiField psiField = (PsiField)resolve;
+      if (!psiField.hasModifierProperty(PsiModifier.STATIC) ||
+          !psiField.hasModifierProperty(PsiModifier.FINAL)) {
+        return false;
       }
+
+      final PsiClass containingClass = psiField.getContainingClass();
+      return containingClass != null &&
+             PsiType.class.getName().equals(containingClass.getQualifiedName()) &&
+             !"NULL".equals(psiField.getName());
     }
     return false;
-  }
-
-  private static class ReplaceEqualityWithEqualsFix implements LocalQuickFix {
-    private final String myName;
-
-    ReplaceEqualityWithEqualsFix(String name) {
-      myName = name;
-    }
-
-    @Nls
-    @NotNull
-    @Override
-    public String getName() {
-      return myName;
-    }
-
-    @Nls
-    @NotNull
-    @Override
-    public String getFamilyName() {
-      return "Replace equality with .equals";
-    }
-
-    @Override
-    public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
-      final PsiElement psiElement = descriptor.getPsiElement();
-      if (psiElement instanceof PsiJavaToken) {
-
-        final IElementType tokenType = ((PsiJavaToken)psiElement).getTokenType();
-        final String prefix;
-        if (tokenType.equals(JavaTokenType.EQEQ)) {
-          prefix = "";
-        }
-        else if (tokenType.equals(JavaTokenType.NE)) {
-          prefix = "!";
-        }
-        else {
-          return;
-        }
-
-        final PsiElement parent = psiElement.getParent();
-        if (parent instanceof PsiBinaryExpression) {
-          final PsiExpression rOperand = ((PsiBinaryExpression)parent).getROperand();
-          final PsiExpression lOperand = ((PsiBinaryExpression)parent).getLOperand();
-          if (rOperand != null) {
-            final boolean flip = isPrimitiveTypeRef(rOperand);
-            if (flip || isPrimitiveTypeRef(lOperand)) {
-              CommentTracker commentTracker = new CommentTracker();
-              final String rText = commentTracker.text(PsiUtil.skipParenthesizedExprUp(rOperand));
-              final String lText = commentTracker.text(PsiUtil.skipParenthesizedExprUp(lOperand));
-
-              final String lhText = flip ? rText : lText;
-              final String rhText = flip ? lText : rText;
-
-              @NonNls final String expString = prefix + lhText + ".equals(" + rhText + ')';
-              PsiReplacementUtil.replaceExpression((PsiBinaryExpression)parent, expString, commentTracker);
-            }
-          }
-        }
-      }
-    }
   }
 }
