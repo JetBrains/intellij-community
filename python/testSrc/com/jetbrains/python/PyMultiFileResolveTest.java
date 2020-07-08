a8065ac5054ac3c2c109c1b840ff3d8f397f3608
Aleksei Kniazev <aleksei.kniazev@jetbrains.com>
2020-07-08 13:27:03 +0300

    IDEA-CR-64748: account for docstring when resolving namespace package, support try-except style declarations (PY-39748, PY-39512)
    (cherry picked from commit 94907cff458e56607c11163bb681988a44750bc0)
    
    GitOrigin-RevId: 807928430246635f795110e5197a35b3579009ac

--- python/testSrc/com/jetbrains/python/PyMultiFileResolveTest.java
+++ python/testSrc/com/jetbrains/python/PyMultiFileResolveTest.java
@@ -491,6 +491,16 @@ public class PyMultiFileResolveTest extends PyMultiFileResolveTestCase {
     doTestResolveInNamespacePackage(getTestName(true));
   }
 
+  // PY-39748
+  public void testPkgResourcesNamespaceWithDocstring() {
+    doTestResolveInNamespacePackage(getTestName(true));
+  }
+
+  // PY-39748
+  public void testTryExceptNamespace() {
+    doTestResolveInNamespacePackage(getTestName(true));
+  }
+
   private void doTestResolveInNamespacePackage(String namespace) {
     myFixture.copyDirectoryToProject(namespace, "");
     runWithSourceRoots(Lists.newArrayList(myFixture.findFileInTempDir("root1"), myFixture.findFileInTempDir("root2")), () -> {
