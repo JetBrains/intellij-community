4d1bd03325610e0407964dc59f97ab97365b04f4
Ilyas Selimov <ilyas.selimov@jetbrains.com>
2020-07-08 20:31:44 +0700

    IDEA-242390 - handled the simple normal constants in enums
    GitOrigin-RevId: 9ca524c24c94a3386d69131a4bab2c1038e4e70d

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/genericsHighlighting/Enum242390.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/genericsHighlighting/Enum242390.java
@@ -2,6 +2,10 @@ enum Enum242390 {
   TYPE1(<error descr="Illegal forward reference">Enum242390.TYPE2</error>),
   TYPE2(TYPE1);
 
+  static final String C1 = Enum242390.D;
+  static final String C2 = <error descr="Illegal forward reference">D</error>;
+  static final String D = "";
+
   private final Enum242390 next;
   Enum242390(Enum242390 next) {
     this.next = next;
