08c87824f3abcacf0fbf9793bda2ce16b6932c2e
Stanislav Utikeev <stanislav.utikeev@jetbrains.com>
2020-07-09 13:23:02 +0300

    LAB-31 Check that parameter list of signature help is closed before dropping the results
    GitOrigin-RevId: 7266d00843abbcfa6d01d0303a0317f37641f695

--- python/python-psi-impl/src/com/jetbrains/python/codeInsight/signatureHelp/PySignatureHelpProvider.java
+++ python/python-psi-impl/src/com/jetbrains/python/codeInsight/signatureHelp/PySignatureHelpProvider.java
@@ -24,7 +24,7 @@ public final class PySignatureHelpProvider implements SignatureHelpProvider {
   @Override
   public SignatureHelpResult getSignatureHelp(PsiFile file, int offset) {
     PyArgumentList argumentList = PyParameterInfoUtils.findArgumentList(file, offset, -1);
-    if (argumentList == null || !argumentList.getTextRange().contains(offset)) {
+    if (argumentList == null || (!argumentList.getTextRange().contains(offset) && argumentList.getText().endsWith(")"))) {
       return null;
     }
 
