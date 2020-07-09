65ab291490201e9f8c2c0f75ce65c9e9cd932816
Alexander Prendota <prendota@mail.ru>
2020-07-09 13:26:02 +0200

    IDEA-CR-2352: PluginNode: add setter to date with Long value
    (cherry picked from commit c24f0e1cbf0845a4b2ab08c21748e4f993ff5147)
    
    GitOrigin-RevId: c9b6805cfcf2f11656091e21cf85744158f2799c

--- platform/platform-impl/src/com/intellij/ide/plugins/marketplace/PluginRepositoryObjects.kt
+++ platform/platform-impl/src/com/intellij/ide/plugins/marketplace/PluginRepositoryObjects.kt
@@ -79,6 +79,7 @@ internal class MarketplaceSearchPluginData(
   var isPaid: Boolean = false,
   val rating: Double = 0.0,
   val name: String = "",
+  val cdate: Long? = null,
   val vendor: String = "",
   @get:JsonProperty("updateId")
   val externalUpdateId: String? = null,
