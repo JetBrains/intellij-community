21511e9ab3611f6d1a69f34f66aef2ca5d120da1
Daniil Tsaryov <daniil.tsaryov@jetbrains.com>
2020-07-09 16:19:51 +0300

    [vcs] Get rid of dimension service key in new git dialogs
    GitOrigin-RevId: be55634b78f33442489cddb335a45ef4bab75b77

--- plugins/git4idea/src/git4idea/merge/GitMergeDialog.kt
+++ plugins/git4idea/src/git4idea/merge/GitMergeDialog.kt
@@ -120,8 +120,6 @@ class GitMergeDialog(private val project: Project,
 
   override fun getHelpId() = "reference.VersionControl.Git.MergeBranches"
 
-  override fun getDimensionServiceKey(): String = GitMergeDialog::class.java.name
-
   override fun doOKAction() {
     try {
       saveSettings()
