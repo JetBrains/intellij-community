92f6e0435acbfc225a74285cb6888686da90f82c
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2020-07-10 15:13:08 +0300

    Refactor maven compatibility tests
    GitOrigin-RevId: d1d6e4389f769a0369746406f5c66c15b7056071

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/compatibility/MavenCompatibilityProjectImportingTest.java
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/compatibility/MavenCompatibilityProjectImportingTest.java
@@ -1,10 +1,84 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.maven.compatibility;
 
+import com.intellij.util.text.VersionComparatorUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.idea.maven.MavenImportingTestCase;
 import org.jetbrains.idea.maven.server.MavenServerManager;
+import org.junit.After;
+import org.junit.Assume;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-public class MavenCompatibilityProjectImportingTest extends MavenCompatibilityTest {
+import java.util.Arrays;
+import java.util.List;
+
+
+@RunWith(Parameterized.class)
+public class MavenCompatibilityProjectImportingTest extends MavenImportingTestCase {
+
+  @NotNull
+  protected MavenWrapperTestFixture myWrapperTestFixture;
+
+  @Parameterized.Parameter
+  public String myMavenVersion;
+
+  protected void assumeVersionMoreThan(String version) {
+    Assume.assumeTrue("Version should be more than " + version, VersionComparatorUtil.compare(myMavenVersion, version) > 0);
+  }
+
+  protected void assumeVersionLessThan(String version) {
+    Assume.assumeTrue("Version should be less than " + version, VersionComparatorUtil.compare(myMavenVersion, version) > 0);
+  }
+
+  protected void assumeVersionNot(String version) {
+    Assume.assumeTrue("Version " + version + " skipped", VersionComparatorUtil.compare(myMavenVersion, version) != 0);
+  }
+
+  @Before
+  public void before() throws Exception {
+    myWrapperTestFixture = new MavenWrapperTestFixture(myProject, myMavenVersion);
+    myWrapperTestFixture.setUp();
+  }
+
+  @After
+  public void after() throws Exception {
+    myWrapperTestFixture.tearDown();
+  }
+
+  @Parameterized.Parameters(name = "with Maven-{0}")
+  public static List<String[]> getMavenVersions() {
+    return Arrays.asList(
+      //new String[]{"3.7.0-SNAPSHOT"},
+      new String[]{"3.6.3"},
+      new String[]{"3.6.2"},
+      new String[]{"3.6.1"},
+      new String[]{"3.6.0"},
+      new String[]{"3.5.4"},
+      new String[]{"3.5.3"},
+      new String[]{"3.5.2"},
+      new String[]{"3.5.0"},
+      new String[]{"3.3.9"},
+      new String[]{"3.3.3"},
+      new String[]{"3.3.1"},
+      new String[]{"3.2.5"},
+      new String[]{"3.2.3"},
+      new String[]{"3.2.2"},
+      new String[]{"3.2.1"},
+      new String[]{"3.1.1"},
+      new String[]{"3.1.0"},
+      new String[]{"3.0.5"},
+      new String[]{"3.0.4"},
+      new String[]{"3.0.3"},
+      new String[]{"3.0.2"},
+      new String[]{"3.0.1"},
+      new String[]{"3.0"}
+    );
+  }
+
+
   @Test
   public void testSmokeImport() {
     assertCorrectVersion();
