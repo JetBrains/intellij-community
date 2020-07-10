ab33223ae7919cadffa6f3d502cf65f4bb2edeeb
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2020-07-10 11:43:34 +0300

    Fixed IDEA-245580 Light Edit Mode: getCurrentTextEditor and getCurrentFileEditor methods of StatusBarUtil return null
    GitOrigin-RevId: 39922610052a31c1a18dc1cee07068e39a1a76ce

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/status/StatusBarUtil.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/status/StatusBarUtil.java
@@ -1,6 +1,8 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.wm.impl.status;
 
+import com.intellij.ide.lightEdit.LightEdit;
+import com.intellij.ide.lightEdit.LightEditService;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
@@ -67,6 +69,10 @@ public final class StatusBarUtil {
     Project project = statusBar.getProject();
     if (project == null) {
       return null;
+    }
+
+    if (LightEdit.owns(project)) {
+      return LightEditService.getInstance().getSelectedFileEditor();
     }
 
     DockContainer c = DockManager.getInstance(project).getContainerFor(statusBar.getComponent());
