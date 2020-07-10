bd233fda5f7cab928666ef6f9f740c24ba9f6e4e
Vassiliy.Kudryashov <vassiliy.kudryashov@jetbrains.com>
2020-07-11 01:05:55 +0300

    IDEA-227970 Find/Replace toolbars incorrectly handle insufficient space
    GitOrigin-RevId: 7cb3ab9f4ed34359baafe6b0915d81723514bdc6

--- platform/lang-impl/src/com/intellij/find/SearchReplaceComponent.java
+++ platform/lang-impl/src/com/intellij/find/SearchReplaceComponent.java
@@ -196,6 +196,7 @@ public final class SearchReplaceComponent extends EditorHeaderComponent implemen
     splitter.setSecondComponent(myRightPanel);
     splitter.setHonorComponentsMinimumSize(true);
     splitter.setLackOfSpaceStrategy(Splitter.LackOfSpaceStrategy.HONOR_THE_SECOND_MIN_SIZE);
+    splitter.setDividerPositionStrategy(Splitter.DividerPositionStrategy.KEEP_FIRST_SIZE);
     splitter.setAndLoadSplitterProportionKey("FindSplitterProportion");
     splitter.setOpaque(false);
     splitter.getDivider().setOpaque(false);
