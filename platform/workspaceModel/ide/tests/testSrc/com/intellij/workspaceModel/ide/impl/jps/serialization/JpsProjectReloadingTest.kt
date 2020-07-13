fb25e6af19bd145d12f4fd242811c9c979cd3eff
Alex Plate <aleksei.plate@jetbrains.com>
2020-07-13 18:44:37 +0300

    [Workspace Model] Test refactoring: create a single event if all files in directory are removed
    If all files are removed in a directory, single event with removed directory is created instead of one event per file
    
    GitOrigin-RevId: bc3bb7c694d3b99141f15c24c6aee95eedc08fd6

--- platform/workspaceModel/ide/tests/testSrc/com/intellij/workspaceModel/ide/impl/jps/serialization/JpsProjectReloadingTest.kt
+++ platform/workspaceModel/ide/tests/testSrc/com/intellij/workspaceModel/ide/impl/jps/serialization/JpsProjectReloadingTest.kt
@@ -106,9 +106,9 @@ class JpsProjectReloadingTest : HeavyPlatformTestCase() {
   private fun reload(originalProjectDir: File, directoryName: String): ReloadedProjectData {
     return reload(originalProjectDir) { projectData ->
       val changedDir = PathManagerEx.findFileUnderCommunityHome("platform/workspaceModel/ide/tests/testData/serialization/reload/$directoryName")
-      val newUrls = collectFileUrls(changedDir, projectData.projectDirUrl) { it != "<delete/>"}
-      val urlsToDelete = collectFileUrls(changedDir, projectData.projectDirUrl) { it == "<delete/>"}
-      val oldUrls = collectFileUrls(projectData.projectDir, projectData.projectDirUrl) { true }
+      val newUrls = collectFileUrlsRec(changedDir, projectData.projectDirUrl, true) { it != "<delete/>"}
+      val urlsToDelete = collectFileUrlsRec(changedDir, projectData.projectDirUrl, true) { it == "<delete/>"}
+      val oldUrls = collectFileUrlsRec(projectData.projectDir, projectData.projectDirUrl, false) { true }
       FileUtil.copyDir(changedDir, projectData.projectDir)
       projectData.projectDir.walk().filter { it.isFile && it.readText().trim() == "<delete/>" }.forEach {
         FileUtil.delete(it)
@@ -119,10 +119,37 @@ class JpsProjectReloadingTest : HeavyPlatformTestCase() {
     }
   }
 
-  private fun collectFileUrls(dir: File, baseUrl: String, contentFilter: (String) -> Boolean): ArrayList<String> {
-    return dir.walkTopDown().filter { it.isFile && contentFilter(it.readText().trim()) }.mapTo(ArrayList()) {
-      baseUrl + "/${FileUtil.toSystemIndependentName(FileUtil.getRelativePath(dir, it)!!)}"
+  private fun collectFileUrlsRec(dir: File,
+                                 baseUrl: String,
+                                 replaceByParent: Boolean,
+                                 contentFilter: (String) -> Boolean): Set<String> {
+    val res = HashSet<String>()
+
+    dir.walkBottomUp().forEach { file ->
+      if (file.isFile && contentFilter(file.readText().trim())) {
+        res += toRelativeUrl(baseUrl, dir, file)
+      }
+      else if (file.isDirectory) {
+        // If all removed files relate to the one directory, the directory event is sent
+        val relativeFileUrl = FileUtil.getRelativePath(dir, file) ?: return@forEach
+        if ("." == relativeFileUrl) return@forEach
+        if (!relativeFileUrl.endsWith(".idea/libraries") && !relativeFileUrl.endsWith(".idea/artifacts")) return@forEach
+
+        // Existing files + files that will be copied
+        val children = (JpsPathUtil.urlToFile(toRelativeUrl(baseUrl, dir, file)).listFiles()?.map { JpsPathUtil.pathToUrl(it.absolutePath) } ?: emptyList()) +
+                       (file.listFiles()?.map { toRelativeUrl(baseUrl, dir, it) } ?: emptyList())
+        if (children.all { it in res }) {
+          if (replaceByParent) children.forEach { res.remove(it) }
+          res += toRelativeUrl(baseUrl, dir, file)
+        }
+      }
     }
+
+    return res
+  }
+
+  private fun toRelativeUrl(baseUrl: String, dir: File, file: File): String {
+    return "$baseUrl/${FileUtil.toSystemIndependentName(FileUtil.getRelativePath(dir, file)!!)}"
   }
 
   private data class ReloadedProjectData(val storage: WorkspaceEntityStorageBuilder, val projectDirUrl: String)
