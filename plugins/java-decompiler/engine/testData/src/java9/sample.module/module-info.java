d5c48afe81b0cae430eeaf645ab0b7093cd19966
leonardosnt <leonrdsnt@gmail.com>
2020-07-13 22:16:03 -0300

    Add a more complete module-info.class

--- plugins/java-decompiler/engine/testData/src/java9/sample.module/module-info.java
+++ plugins/java-decompiler/engine/testData/src/java9/sample.module/module-info.java
@@ -1,3 +1,15 @@
 module sample.module {
   requires java.base;
+
+  uses java.util.spi.ToolProvider;
+
+  provides test.TestService with test.TestServiceImpl;
+
+  exports test;
+
+  exports test2 to java.base;
+
+  opens test;
+
+  opens test2 to java.base;
 }
