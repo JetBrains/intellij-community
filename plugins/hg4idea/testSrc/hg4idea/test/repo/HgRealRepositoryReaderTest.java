e9e80625450330356427211803db93d2c18d1032
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-07-14 12:50:34 +0200

    cleanup
    GitOrigin-RevId: fe5316f7328790f633b36da8f01f86a65042841f

--- plugins/hg4idea/testSrc/hg4idea/test/repo/HgRealRepositoryReaderTest.java
+++ plugins/hg4idea/testSrc/hg4idea/test/repo/HgRealRepositoryReaderTest.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2013 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package hg4idea.test.repo;
 
 import com.intellij.dvcs.repo.Repository;
@@ -24,24 +10,21 @@ import org.zmlx.hg4idea.repo.HgRepositoryImpl;
 import org.zmlx.hg4idea.repo.HgRepositoryReader;
 import org.zmlx.hg4idea.util.HgUtil;
 
-import java.io.File;
 import java.util.Arrays;
+import java.util.Collections;
 
 import static com.intellij.openapi.vcs.Executor.*;
 import static hg4idea.test.HgExecutor.hg;
 import static hg4idea.test.HgExecutor.hgMergeWith;
 
 public class HgRealRepositoryReaderTest extends HgPlatformTest {
-
   @NotNull private HgRepositoryReader myRepositoryReader;
 
   @Override
   public void setUp() throws Exception {
     super.setUp();
-    File hgDir = new File(myRepository.getPath(), ".hg");
-    assertTrue(hgDir.exists());
     createBranchesAndTags();
-    myRepositoryReader = new HgRepositoryReader(myVcs, hgDir);
+    myRepositoryReader = new HgRepositoryReader(myVcs, myRepository.toNioPath().resolve(".hg").toFile());
   }
 
   public void testMergeState() {
@@ -80,7 +63,7 @@ public class HgRealRepositoryReaderTest extends HgPlatformTest {
 
   public void testLocalTags() {
     VcsTestUtil.assertEqualCollections(HgUtil.getNamesWithoutHashes(myRepositoryReader.readLocalTags()),
-                                       Arrays.asList("localTag"));
+                                       Collections.singletonList("localTag"));
   }
 
   public void testCurrentBookmark() {
