fdf741c5422ca745f472e926897a0f4e7f931caa
Vladislav Beskrovny <Vladislav.Beskrovny@jetbrains.com>
2020-07-14 14:57:31 +0300

    IDEA-CR-2264: Fix dynamic unloading of EP `fileTypeStatisticProvider`
    (cherry picked from commit d079c223124d8516c79e78491ed4343abf4abdee)
    
    GitOrigin-RevId: 8531033100b3f983a9908de636f9a57a0c3a0da5

--- platform/platform-impl/src/com/intellij/internal/statistic/fileTypes/FileTypeStatisticProvider.java
+++ platform/platform-impl/src/com/intellij/internal/statistic/fileTypes/FileTypeStatisticProvider.java
@@ -1,17 +1,22 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.internal.statistic.fileTypes;
 
+import com.intellij.openapi.Disposable;
 import com.intellij.openapi.editor.event.EditorFactoryEvent;
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.fileTypes.FileType;
 import org.jetbrains.annotations.NotNull;
 
-public interface FileTypeStatisticProvider {
+public interface FileTypeStatisticProvider extends Disposable {
   ExtensionPointName<FileTypeStatisticProvider> EP_NAME = ExtensionPointName.create("com.intellij.fileTypeStatisticProvider");
 
   @NotNull
   String getPluginId();
 
   boolean accept(@NotNull EditorFactoryEvent event, @NotNull FileType fileType);
+
+  @Override
+  default void dispose() {
+  }
 }
 
