cc39c35b7008c4abbff173e988247468c8c75955
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2020-07-14 21:08:58 +0300

    [project model tests] rewrite RepositoryLibrarySerializationTest
    Now the tests on loading repository libraries are included into LoadProjectTest. It works on higher level and load the whole project. This simplifies code and also ensures that test works under new workspace model as well (it doesn't support direct loading of project library table).
    
    GitOrigin-RevId: d11d742d271d779f18d7ad46dd9b051030cb4212

--- java/java-tests/testSrc/com/intellij/java/configurationStore/LoadProjectTest.kt
+++ java/java-tests/testSrc/com/intellij/java/configurationStore/LoadProjectTest.kt
@@ -6,6 +6,8 @@ import com.intellij.openapi.application.runWriteActionAndWait
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.OrderRootType
+import com.intellij.openapi.roots.impl.libraries.LibraryEx
+import com.intellij.openapi.roots.libraries.Library
 import com.intellij.openapi.roots.libraries.LibraryTablesRegistrar
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.vfs.VfsUtil
@@ -16,6 +18,7 @@ import com.intellij.testFramework.TemporaryDirectory
 import com.intellij.testFramework.createOrLoadProject
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.idea.maven.utils.library.RepositoryLibraryProperties
 import org.junit.ClassRule
 import org.junit.Rule
 import org.junit.Test
@@ -82,14 +85,47 @@ class LoadProjectTest {
     }
   }
 
+  private val Library.properties: RepositoryLibraryProperties
+    get() = (this as LibraryEx).properties as RepositoryLibraryProperties
+
+  @Test
+  fun `load repository libraries`() {
+    val projectPath = Paths.get(PathManagerEx.getCommunityHomePath()).resolve("jps/model-serialization/testData/repositoryLibraries")
+    loadProjectAndCheckResults(projectPath) { project ->
+      assertThat(ModuleManager.getInstance(project).modules).isEmpty()
+      val libraries = LibraryTablesRegistrar.getInstance().getLibraryTable(project).libraries.sortedBy { it.name }
+      assertThat(libraries).hasSize(3)
+      val (plain, withExcluded, withoutTransitive) = libraries
+
+      assertThat(plain.name).isEqualTo("plain")
+      assertThat(plain.properties.groupId).isEqualTo("junit")
+      assertThat(plain.properties.artifactId).isEqualTo("junit")
+      assertThat(plain.properties.version).isEqualTo("3.8.1")
+      assertThat(plain.properties.isIncludeTransitiveDependencies).isTrue()
+      assertThat(plain.properties.excludedDependencies).isEmpty()
+
+      assertThat(withExcluded.name).isEqualTo("with-excluded-dependencies")
+      assertThat(withExcluded.properties.isIncludeTransitiveDependencies).isTrue()
+      assertThat(withExcluded.properties.excludedDependencies).containsExactly("org.apache.httpcomponents:httpclient")
+
+      assertThat(withoutTransitive.name).isEqualTo("without-transitive-dependencies")
+      assertThat(withoutTransitive.properties.isIncludeTransitiveDependencies).isFalse()
+      assertThat(withoutTransitive.properties.excludedDependencies).isEmpty()
+    }
+  }
+
   private val testDataRoot
     get() = Paths.get(PathManagerEx.getCommunityHomePath()).resolve("java/java-tests/testData/configurationStore")
 
   private fun loadProjectAndCheckResults(testDataDirName: String, checkProject: (Project) -> Unit) {
+    return loadProjectAndCheckResults(testDataRoot.resolve(testDataDirName), checkProject)
+  }
+
+  private fun loadProjectAndCheckResults(projectPath: Path, checkProject: (Project) -> Unit) {
     @Suppress("RedundantSuspendModifier")
     suspend fun copyProjectFiles(dir: VirtualFile): Path {
       val projectDir = VfsUtil.virtualToIoFile(dir)
-      FileUtil.copyDir(testDataRoot.resolve(testDataDirName).toFile(), projectDir)
+      FileUtil.copyDir(projectPath.toFile(), projectDir)
       VfsUtil.markDirtyAndRefresh(false, true, true, dir)
       return projectDir.toPath()
     }
