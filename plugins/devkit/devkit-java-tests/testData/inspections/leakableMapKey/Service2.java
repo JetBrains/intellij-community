f0ce428844ff7107e2c5023efcb823b262ecb8c7
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2020-07-16 01:33:16 +0300

    IDEA-CR-2295: [dynamic plugins] an inspection to detect potentially leaking map keys (Language/FileType) #IDEA-245855 fixed
    (cherry picked from commit 424816555af618c0727a95dab1e5c154d056737b)
    
    GitOrigin-RevId: 7952518f44a66b29739214571a156fa906c82c43

---
+++ plugins/devkit/devkit-java-tests/testData/inspections/leakableMapKey/Service2.java
@@ -0,0 +1,9 @@
+import com.intellij.lang.Language;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+
+public class Service2 {
+
+  private final @NotNull HashMap<L<caret>anguage, Object> myLanguageMap = new HashMap<>();
+}
