e04af44a0cdfa8dee83fb0f8c623653ccd19cbd5
Mikhail Sokolov <mikhail.sokolov@jetbrains.com>
2020-07-16 18:38:30 +0300

    IDEA-230328 Search everywhere popup opens partially offscreen
    GitOrigin-RevId: c359d55fbe0baff17f97fb4e0d7a485e94514c4f

--- platform/platform-impl/src/com/intellij/ui/popup/AbstractPopup.java
+++ platform/platform-impl/src/com/intellij/ui/popup/AbstractPopup.java
@@ -864,6 +864,11 @@ public class AbstractPopup implements JBPopup, ScreenAreaConsumer {
     if (myLocateWithinScreen) {
       ScreenUtil.moveToFit(targetBounds, screen, null);
     }
+    else {
+      //even when LocateWithinScreen option is disabled, popup should not be shown in invisible area
+      fitToVisibleArea(targetBounds);
+    }
+
 
     if (myMouseOutCanceller != null) {
       myMouseOutCanceller.myEverEntered = targetBounds.equals(original);
@@ -1101,6 +1106,18 @@ public class AbstractPopup implements JBPopup, ScreenAreaConsumer {
     myState = State.SHOWN;
 
     afterShowSync();
+  }
+
+  private static void fitToVisibleArea(Rectangle targetBounds) {
+    Point topLeft = new Point(targetBounds.x, targetBounds.y);
+    Point bottomRight = new Point((int)targetBounds.getMaxX(), (int)targetBounds.getMaxY());
+    Rectangle topLeftScreen = ScreenUtil.getScreenRectangle(topLeft);
+    Rectangle bottomRightScreen = ScreenUtil.getScreenRectangle(bottomRight);
+    if (topLeft.x < topLeftScreen.x || topLeft.y < topLeftScreen.y
+        || bottomRight.x > bottomRightScreen.getMaxX() || bottomRight.y > bottomRightScreen.getMaxY()) {
+      Rectangle centerScreen = ScreenUtil.getScreenRectangle(new Point((int)targetBounds.getCenterX(), (int)targetBounds.getCenterY()));
+      ScreenUtil.moveToFit(targetBounds, centerScreen, null);
+    }
   }
 
   public void focusPreferredComponent() {
