34bd074371a655f9710baa385122f79abff390c1
Aleksey Pivovarov <AMPivovarov@gmail.com>
2020-07-17 15:05:58 +0300

    diff: remove dead code
    GitOrigin-RevId: 906c5959f12cf48f1984aef9724fdbf6f6c3894e

--- platform/platform-impl/src/com/intellij/openapi/editor/markup/MarkupEditorFilterFactory.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/markup/MarkupEditorFilterFactory.java
@@ -1,10 +1,10 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.editor.markup;
 
-import com.intellij.openapi.diff.DiffManager;
+import com.intellij.openapi.diff.impl.DiffUtil;
 
 public final class MarkupEditorFilterFactory {
-  private static final MarkupEditorFilter IS_DIFF_FILTER = DiffManager.getInstance().getDiffEditorFilter();
+  private static final MarkupEditorFilter IS_DIFF_FILTER = editor -> DiffUtil.isDiffEditor(editor);
   private static final MarkupEditorFilter NOT_DIFF_FILTER = createNotFilter(IS_DIFF_FILTER);
 
   public static MarkupEditorFilter createNotFilter(final MarkupEditorFilter filter) {
