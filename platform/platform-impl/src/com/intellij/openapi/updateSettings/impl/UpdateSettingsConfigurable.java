f5fc187643655c6c8ce719875adf4a7a88145d77
Raluca Sauciuc <ralucas@google.com>
2020-07-18 16:07:46 -0700

    Don't dereference field mySettings
    … in code added to createUIComponents by commit 63ca259.
    The form compiler generates a call to createUIComponents
    that happens before the field is set to a non-null value.
    
    Change-Id: Iab0bdbe589d82345f30fd76a2e54d9f66b53c4ea

--- platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/UpdateSettingsConfigurable.java
+++ platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/UpdateSettingsConfigurable.java
@@ -182,7 +182,7 @@ public class UpdateSettingsConfigurable implements SearchableConfigurable {
       });
       List<Pair<String, String>> extraStatuses = Lists.newArrayList();
 
-      for (ExternalComponentSource source : ExternalComponentManager.getInstance().getEnabledComponentSources(mySettings)) {
+      for (ExternalComponentSource source : ExternalComponentManager.getInstance().getEnabledComponentSources(UpdateSettings.getInstance())) {
         extraStatuses.addAll(source.getStatuses());
       }
       int row = 0;
