d32ebe5ff481629b1a93e23584767ddeb79a0abc
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2020-07-20 16:50:24 +0300

    FUS-756 Collect updates of ML completion ranking settings
    GitOrigin-RevId: 93690d11b2a55317b13b851565d14a239ed12aa2

--- plugins/stats-collector/src/com/intellij/completion/settings/CompletionMLRankingSettings.java
+++ plugins/stats-collector/src/com/intellij/completion/settings/CompletionMLRankingSettings.java
@@ -7,9 +7,7 @@ import com.intellij.stats.sender.SenderPreloadingActivityKt;
 import com.jetbrains.completion.ranker.WeakModelProvider;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @State(name = "CompletionMLRankingSettings", storages = @Storage(value = "completionMLRanking.xml", roamingType = RoamingType.DISABLED))
 public final class CompletionMLRankingSettings implements PersistentStateComponent<CompletionMLRankingSettings.State> {
@@ -37,7 +35,9 @@ public final class CompletionMLRankingSettings implements PersistentStateCompone
   }
 
   public void setRankingEnabled(boolean value) {
+    if (value == isRankingEnabled()) return;
     myState.rankingEnabled = value;
+    triggerSettingsChanged(value);
   }
 
   public boolean isCompletionLogsSendAllowed() {
@@ -45,23 +45,23 @@ public final class CompletionMLRankingSettings implements PersistentStateCompone
   }
 
   public boolean isLanguageEnabled(@NotNull String languageName) {
-    return myState.language2state.getOrDefault(languageName, enabledByDefault.contains(languageName));
+    return myState.language2state.getOrDefault(languageName, isEnabledByDefault(languageName));
   }
 
   public void setLanguageEnabled(@NotNull String languageName, boolean isEnabled) {
-    boolean defaultValue = enabledByDefault.contains(languageName);
-    if (defaultValue == isEnabled) {
-      myState.language2state.remove(languageName);
-    }
-    else {
-      myState.language2state.put(languageName, isEnabled);
-    }
-
+    if (isEnabled == isLanguageEnabled(languageName)) return;
+    setLanguageEnabledImpl(languageName, isEnabled);
     logCompletionState(languageName, isEnabled);
+    if (isRankingEnabled()) {
+      // log only if language ranking settings changes impact completion behavior
+      MLCompletionSettingsCollector.rankingSettingsChanged(languageName, isEnabled, isEnabledByDefault(languageName), true);
+    }
   }
 
   public void setShowDiffEnabled(boolean isEnabled) {
+    if (isEnabled == isShowDiffEnabled()) return;
     myState.showDiff = isEnabled;
+    MLCompletionSettingsCollector.decorationSettingChanged(isEnabled);
   }
 
   @Override
@@ -73,13 +73,47 @@ public final class CompletionMLRankingSettings implements PersistentStateCompone
   public void loadState(@NotNull State state) {
     myState.rankingEnabled = state.rankingEnabled;
     myState.showDiff = state.showDiff;
-    state.language2state.forEach((lang, enabled) -> setLanguageEnabled(lang, enabled));
+    state.language2state.forEach((lang, enabled) -> setLanguageEnabledImpl(lang, enabled));
   }
 
   private void logCompletionState(@NotNull String languageName, boolean isEnabled) {
     final boolean enabled = myState.rankingEnabled && isEnabled;
     final boolean showDiff = enabled && myState.showDiff;
     LOG.info("ML Completion " + (enabled ? "enabled" : "disabled") + " ,show diff " + (showDiff ? "on" : "off") + " for: " + languageName);
+  }
+
+  private boolean isEnabledByDefault(@NotNull String languageName) {
+    return enabledByDefault.contains(languageName);
+  }
+
+  private void setLanguageEnabledImpl(@NotNull String languageName, boolean isEnabled) {
+    if (isEnabledByDefault(languageName) == isEnabled) {
+      myState.language2state.remove(languageName);
+    }
+    else {
+      myState.language2state.put(languageName, isEnabled);
+    }
+  }
+
+  private void triggerSettingsChanged(boolean enabled) {
+    for (String ranker : getEnabledRankers()) {
+      MLCompletionSettingsCollector.rankingSettingsChanged(ranker, enabled, isEnabledByDefault(ranker), false);
+    }
+  }
+
+  private List<String> getEnabledRankers() {
+    List<String> enabledRankers = new ArrayList<>(enabledByDefault);
+    for (Map.Entry<String, Boolean> entry : myState.language2state.entrySet()) {
+      String rankerId = entry.getKey();
+      if (entry.getValue()) {
+        enabledRankers.add(rankerId);
+      }
+      else {
+        enabledRankers.remove(rankerId);
+      }
+    }
+
+    return enabledRankers;
   }
 
   public static final class State {
