328421fa7ddca633f4fb4ac7bb293e189d73f02f
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2020-07-21 18:16:30 +0300

    [gradle tests] added force save in tests where it expected
    GitOrigin-RevId: cd441817b8318cf269d75c67cf8daa8a4910bddd

--- platform/external-system-impl/testSrc/com/intellij/openapi/externalSystem/util/ExternalSystemTestUtil.kt
+++ platform/external-system-impl/testSrc/com/intellij/openapi/externalSystem/util/ExternalSystemTestUtil.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.externalSystem.util
 
+import com.intellij.configurationStore.StoreUtil
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ex.ProjectManagerEx
 import com.intellij.testFramework.runInEdtAndWait
@@ -14,11 +15,9 @@ fun Project.use(save: Boolean = false, action: (Project) -> Unit) {
     runInEdtAndWait {
       val projectManager = ProjectManagerEx.getInstanceEx()
       if (save) {
-        projectManager.closeAndDispose(project)
+        StoreUtil.saveSettings(project, forceSavingAllSettings = true)
       }
-      else {
-        projectManager.forceCloseProject(project)
-      }
+      projectManager.forceCloseProject(project)
     }
   }
 }
