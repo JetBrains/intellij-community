516c6578d94ee2b2aa7287d46c4bc99f12a13986
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2020-07-21 16:48:14 +0300

    Partial revert: return the old option name, remove new blank lines at the end option
    GitOrigin-RevId: 6152fc81d3a74dcb3e7946b2fd22371c07a6c595

--- platform/lang-impl/src/com/intellij/application/options/editor/EditorOptionsPanel.kt
+++ platform/lang-impl/src/com/intellij/application/options/editor/EditorOptionsPanel.kt
@@ -67,7 +67,7 @@ private val cdSmoothScrolling                                          get() = C
 private val cdUseSoftWrapsAtEditor                                     get() = CheckboxDescriptor(message("checkbox.use.soft.wraps.at.editor"), PropertyBinding(editorSettings::isUseSoftWraps, editorSettings::setUseSoftWraps))
 private val cdUseCustomSoftWrapIndent                                  get() = CheckboxDescriptor(message("checkbox.use.custom.soft.wraps.indent"), PropertyBinding(editorSettings::isUseCustomSoftWrapIndent, editorSettings::setUseCustomSoftWrapIndent))
 private val cdShowSoftWrapsOnlyOnCaretLine                             get() = CheckboxDescriptor(message("checkbox.show.softwraps.only.for.caret.line"), PropertyBinding({ !editorSettings.isAllSoftWrapsShown }, { editorSettings.setAllSoftwrapsShown(!it) }))
-private val cdTrimBlankLinesAtEOF                                      get() = CheckboxDescriptor(message("editor.options.trim.empty.lines.at.eof"), PropertyBinding(editorSettings::isTrimBlankLinesAtEOF, editorSettings::setTrimBlankLinesAtEOF))
+private val cdTrimBlankLinesAtEOF                                      get() = CheckboxDescriptor(message("editor.options.trim.blank.lines.at.eof"), PropertyBinding(editorSettings::isTrimBlankLinesAtEOF, editorSettings::setTrimBlankLinesAtEOF))
 private val cdEnsureBlankLineBeforeCheckBox                            get() = CheckboxDescriptor(message("editor.options.line.feed"), PropertyBinding(editorSettings::isEnsureNewLineAtEOF, editorSettings::setEnsureNewLineAtEOF))
 private val cdShowQuickDocOnMouseMove                                  get() = CheckboxDescriptor(message("editor.options.quick.doc.on.mouse.hover"), PropertyBinding(editorSettings::isShowQuickDocOnMouseOverElement, editorSettings::setShowQuickDocOnMouseOverElement))
 private val cdKeepTrailingSpacesOnCaretLine                            get() = CheckboxDescriptor(message("editor.settings.delete.trailing.spaces.on.caret.line"), PropertyBinding({ !editorSettings.isKeepTrailingSpacesOnCaretLine }, { editorSettings.isKeepTrailingSpacesOnCaretLine = !it }))
@@ -254,11 +254,7 @@ class EditorOptionsPanel : BoundCompositeConfigurable<UnnamedConfigurable>(messa
             largeGapAfter()
           }
         }
-        row {
-          label(message("editor.options.empty.lines.title"))
-          row { checkBox(cdEnsureBlankLineBeforeCheckBox) }
-          row { checkBox(cdTrimBlankLinesAtEOF) }
-        }
+        row { checkBox(cdEnsureBlankLineBeforeCheckBox) }
       }
       for (configurable in configurables) {
         appendDslConfigurableRow(configurable)
