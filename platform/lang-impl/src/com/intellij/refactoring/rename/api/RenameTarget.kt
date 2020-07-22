7b01316b4ee0b6813eabad83bd88f281cdf893b9
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2020-07-22 18:48:32 +0300

    IDEA-250626 rename api/impl: define Symbol -> RenameTarget and PsiSymbolReference -> RenameUsage
    GitOrigin-RevId: d34a4d09117af14b204dbdad063489956ec22eb2

--- platform/lang-impl/src/com/intellij/refactoring/rename/api/RenameTarget.kt
+++ platform/lang-impl/src/com/intellij/refactoring/rename/api/RenameTarget.kt
@@ -12,6 +12,7 @@ import com.intellij.psi.search.SearchScope
  * Visible in the UI.
  * Lifecycle: single read action.
  *
+ * @see com.intellij.refactoring.rename.symbol.SymbolRenameTarget
  * @see com.intellij.find.usages.SearchTarget
  */
 interface RenameTarget {
