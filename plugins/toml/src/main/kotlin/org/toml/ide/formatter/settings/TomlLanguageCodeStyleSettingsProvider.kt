10ed5877e8229c9c5e4aba6f8d031babfcbc2501
Arseniy Pendryak <a.pendryak@yandex.ru>
2020-07-26 23:05:23 +0300

    Simplify Rust and Toml code style settings
    Previously, each language provided two extensions: `codeStyleSettingsProvider` and `langCodeStyleSettingsProvider`. And it worked for some reason.
    But it doesn't make sense since `LanguageCodeStyleSettingsProvider` is inherent of `CodeStyleSettingsProvider` and it can provide all necessary things itself.
    
    So these changes merge both provides into single one. Also, `createSettingsPage` is replaced with `createConfigurable` because `createSettingsPage` is deprecated
    
    GitOrigin-RevId: bc145e440bb2a8c370d8b7e2661dda6b289817ae

--- plugins/toml/src/main/kotlin/org/toml/ide/formatter/settings/TomlLanguageCodeStyleSettingsProvider.kt
+++ plugins/toml/src/main/kotlin/org/toml/ide/formatter/settings/TomlLanguageCodeStyleSettingsProvider.kt
@@ -5,14 +5,33 @@
 
 package org.toml.ide.formatter.settings
 
+import com.intellij.application.options.CodeStyleAbstractConfigurable
+import com.intellij.application.options.CodeStyleAbstractPanel
 import com.intellij.application.options.IndentOptionsEditor
 import com.intellij.application.options.SmartIndentOptionsEditor
 import com.intellij.lang.Language
+import com.intellij.psi.codeStyle.CodeStyleConfigurable
+import com.intellij.psi.codeStyle.CodeStyleSettings
+import com.intellij.psi.codeStyle.CustomCodeStyleSettings
 import com.intellij.psi.codeStyle.LanguageCodeStyleSettingsProvider
 import org.toml.lang.TomlLanguage
 
 class TomlLanguageCodeStyleSettingsProvider : LanguageCodeStyleSettingsProvider() {
     override fun getLanguage(): Language = TomlLanguage
+
+    override fun createCustomSettings(settings: CodeStyleSettings): CustomCodeStyleSettings =
+        TomlCodeStyleSettings(settings)
+
+    override fun createConfigurable(
+        baseSettings: CodeStyleSettings,
+        modelSettings: CodeStyleSettings
+    ): CodeStyleConfigurable {
+        return object : CodeStyleAbstractConfigurable(baseSettings, modelSettings, configurableDisplayName) {
+            override fun createPanel(settings: CodeStyleSettings): CodeStyleAbstractPanel =
+                TomlCodeStyleMainPanel(currentSettings, settings)
+        }
+    }
+
     override fun getCodeSample(settingsType: SettingsType): String =
         when (settingsType) {
             SettingsType.INDENT_SETTINGS -> INDENT_SAMPLE
