8640c2a6e5f3fecd40f5895a3487f2b866220051
Vassiliy.Kudryashov <vassiliy.kudryashov@jetbrains.com>
2020-07-28 10:12:56 +0300

    IDEA-220087 "Open new tabs at the end" preference has no effect whatsoever
    GitOrigin-RevId: 24286bbfb5dd383bfc64bba3cd52a08e11ddae1c

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/EditorWindow.java
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/EditorWindow.java
@@ -175,6 +175,7 @@ public final class EditorWindow {
             myRemovedTabs.push(pair);
             myTabbedPane.removeTabAt(componentIndex, indexToSelect, transferFocus);
             editorManager.disposeComposite(editor);
+            file.putUserData(INITIAL_INDEX_KEY, null);
           }
         }
         else {
