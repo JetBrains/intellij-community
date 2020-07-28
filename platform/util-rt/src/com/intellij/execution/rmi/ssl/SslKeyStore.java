3bffdbf0272ee7ef31b54579ecd80cabc4678943
Alexander Kass <Alexander.Kass@jetbrains.com>
2020-07-28 12:50:41 +0300

    sflake keypair through SSL tab DBE-9112
    no password input yet
    
    GitOrigin-RevId: e2eab07fa1bc00281970510d0481852f770fe82a

--- platform/util-rt/src/com/intellij/execution/rmi/ssl/SslKeyStore.java
+++ platform/util-rt/src/com/intellij/execution/rmi/ssl/SslKeyStore.java
@@ -1,22 +1,31 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.rmi.ssl;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class SslKeyStore extends DelegateKeyStore {
+  public static final String SSL_DEFERRED_KEY_LOADING = "sslDeferredKeyLoading";
   private static final String NAME = "idea-key-store";
+  private static final Map<PrivateKey, X509Certificate> ourAutoAdded = new LinkedHashMap<PrivateKey, X509Certificate>();
   static {
     ourProvider.setProperty("KeyStore." + NAME, SslKeyStore.class.getName());
   }
 
   public SslKeyStore() {
     super("PKCS12");
+    loadUserCert();
   }
 
   public static void setDefault() {
@@ -26,12 +35,20 @@ public class SslKeyStore extends DelegateKeyStore {
     }
   }
 
-  private void appendUserCert() {
+  private static void loadUserCert() {
+    String certPath = System.getProperty(SslSocketFactory.SSL_CLIENT_CERT_PATH);
+    String keyPath = System.getProperty(SslSocketFactory.SSL_CLIENT_KEY_PATH);
+    if (certPath != null && keyPath != null) loadKey(certPath, keyPath, null);
+  }
+
+  public static void loadKey(@NotNull String clientCertPath,
+                             @NotNull String clientKeyPath,
+                             @Nullable char[] password) {
     try {
-      X509Certificate cert = SslSocketFactory.readCertificate(System.getProperty(SslSocketFactory.SSL_CLIENT_CERT_PATH));
-      //delegate.setCertificateEntry("user-provided-cert", cert);
-      PrivateKey key = SslSocketFactory.readPrivateKey(System.getProperty(SslSocketFactory.SSL_CLIENT_KEY_PATH));
-      delegate.setKeyEntry("user-provided-key", key, null, new Certificate[]{cert});
+      PrivateKey key = SslSocketFactory.readPrivateKey(clientKeyPath, password);
+      if (ourAutoAdded.containsKey(key)) return;
+      X509Certificate cert = SslSocketFactory.readCertificate(clientCertPath);
+      ourAutoAdded.put(key, cert);
     }
     catch (Exception e) {
       throw new IllegalStateException(e);
@@ -41,6 +58,15 @@ public class SslKeyStore extends DelegateKeyStore {
   @Override
   public void engineLoad(InputStream stream, char[] password) throws IOException, NoSuchAlgorithmException, CertificateException {
     delegate.load(null, null);
-    appendUserCert();
+    int i = 0;
+    for (Map.Entry<PrivateKey, X509Certificate> entry : ourAutoAdded.entrySet()) {
+      try {
+        delegate.setKeyEntry("user-provided-key" + (i == 0 ? "" : "#" + i), entry.getKey(), null, new Certificate[]{entry.getValue()});
+      }
+      catch (KeyStoreException e) {
+        throw new IllegalStateException();
+      }
+      ++i;
+    }
   }
 }
