c0981499a1c5dc8488dac428a715ed9ebd405578
Alexander Koshevoy <alexander.koshevoy@jetbrains.com>
2020-07-30 12:29:44 +0300

    Add @Nullable/@NotNull annotations to PySkeletonGenerator.getProcessOutput() method signature
    GitOrigin-RevId: ee07b42d6ab9c167b5ef5949822613be8713d965

--- python/python-sdk/src/com/jetbrains/python/sdk/skeletons/PySkeletonGenerator.java
+++ python/python-sdk/src/com/jetbrains/python/sdk/skeletons/PySkeletonGenerator.java
@@ -395,10 +395,11 @@ public class PySkeletonGenerator {
   public void prepare() {
   }
 
-  protected ProcessOutput getProcessOutput(String homePath,
+  @NotNull
+  protected ProcessOutput getProcessOutput(@Nullable String homePath,
                                            String @NotNull [] commandLine,
                                            @Nullable String stdin,
-                                           Map<String, String> extraEnv,
+                                           @Nullable Map<String, String> extraEnv,
                                            int timeout) throws InvalidSdkException {
     final byte[] bytes = stdin != null ? stdin.getBytes(StandardCharsets.UTF_8) : null;
     return PySdkUtil.getProcessOutput(homePath, commandLine, extraEnv, timeout, bytes, true);
