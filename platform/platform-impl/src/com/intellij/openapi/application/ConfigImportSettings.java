55c98a63133f3b18208b400db7cf4ba39f5d2ff4
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2020-07-31 22:36:40 +0300

    cwm: import configs from other IDE if there are no own configs: CWM-299
    At the first start of the thin client, if there are no own thin client configs to import from, try to import from existing configs of other IDEs (if they exist on the client's machine). That allows to reuse existing configs which the user got used to, instead of starting the thin client on clean default configs.
    
    If there are configs of multiple JetBrains IDEs existing on the client's machine, then those configs are preferred which correspond to the same IDE which is started as the host IDE for this client. Otherwise any other config is selected.
    
    (cherry picked from commit b13dd7e9681cdd833f903245b0d9018afdc02c37)
    
    GitOrigin-RevId: 01e5273898c90d1daa745ab78bd9e46c9f9f3263

--- platform/platform-impl/src/com/intellij/openapi/application/ConfigImportSettings.java
+++ platform/platform-impl/src/com/intellij/openapi/application/ConfigImportSettings.java
@@ -1,10 +1,25 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.application;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.nio.file.Path;
+import java.util.List;
 
 public interface ConfigImportSettings {
   void importFinished(@NotNull Path newConfigPath);
+
+  /**
+   * If there are no configs for previous versions of this product,
+   * then configs will be imported from the IDE path selector returned from this method, if they exist.
+   *
+   * @param programArgs arguments passed to the {@code main} method.
+   * @return the {@link PathManager#getPathsSelector() path selector} of an IDE to import configs from,
+   * or null if no import should happen if there are no configs for this product.
+   */
+  @Nullable
+  default String getProductToImportFrom(@NotNull List<String> programArgs) {
+    return null;
+  }
 }
