5814db0bf77dc158f65e17abad6ea0eb5c966cbf
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2020-07-31 12:00:26 +0300

    svn: Convert "HistoryConflictSide" to kotlin
    GitOrigin-RevId: e513aaa135ed3d20aeb0a48f58af0527dc08b9d8

--- plugins/svn4idea/src/org/jetbrains/idea/svn/treeConflict/HistoryConflictSide.kt
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/treeConflict/HistoryConflictSide.kt
@@ -1,101 +1,80 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.idea.svn.treeConflict;
+package org.jetbrains.idea.svn.treeConflict
 
-import com.intellij.openapi.util.Disposer;
-import com.intellij.openapi.vcs.FilePath;
-import com.intellij.openapi.vcs.VcsException;
-import com.intellij.openapi.vcs.history.*;
-import com.intellij.ui.JBColor;
-import gnu.trove.TLongArrayList;
-import org.jetbrains.idea.svn.SvnRevisionNumber;
-import org.jetbrains.idea.svn.SvnVcs;
-import org.jetbrains.idea.svn.api.Revision;
-import org.jetbrains.idea.svn.conflict.ConflictVersion;
-import org.jetbrains.idea.svn.history.SvnHistoryProvider;
+import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.vcs.VcsException
+import com.intellij.openapi.vcs.history.FileHistoryPanelImpl
+import com.intellij.openapi.vcs.history.FileHistoryRefresherI
+import com.intellij.openapi.vcs.history.VcsAppendableHistoryPartnerAdapter
+import com.intellij.openapi.vcs.history.VcsFileRevision
+import com.intellij.ui.JBColor
+import com.intellij.vcsUtil.VcsUtil.getFilePathOnNonLocal
+import gnu.trove.TLongArrayList
+import org.jetbrains.idea.svn.SvnRevisionNumber
+import org.jetbrains.idea.svn.SvnVcs
+import org.jetbrains.idea.svn.api.Revision
+import org.jetbrains.idea.svn.conflict.ConflictVersion
+import javax.swing.BorderFactory
+import javax.swing.JPanel
 
-import javax.swing.*;
-import java.util.List;
+private const val HISTORY_LIMIT = 10
 
-import static com.intellij.vcsUtil.VcsUtil.getFilePathOnNonLocal;
+internal class HistoryConflictSide(
+  private val vcs: SvnVcs,
+  private val version: ConflictVersion,
+  private val peg: Revision?
+) : ConflictSidePresentation {
 
-final class HistoryConflictSide implements ConflictSidePresentation {
-  public static final int LIMIT = 10;
+  private val sessionAdapter = VcsAppendableHistoryPartnerAdapter()
+  private val provider = vcs.vcsHistoryProvider
+  private val path = getFilePathOnNonLocal(version.repositoryRoot.appendPath(version.path, false).toDecodedString(), version.isDirectory)
+  private var fileHistoryPanel: FileHistoryPanelImpl? = null
 
-  private final VcsAppendableHistoryPartnerAdapter mySessionAdapter;
-  private final SvnHistoryProvider myProvider;
-  private final FilePath myPath;
-  private final SvnVcs myVcs;
-  private final ConflictVersion myVersion;
-  private final Revision myPeg;
-  private FileHistoryPanelImpl myFileHistoryPanel;
-  private TLongArrayList myListToReportLoaded;
+  var listToReportLoaded: TLongArrayList? = null
 
-  HistoryConflictSide(SvnVcs vcs, ConflictVersion version, final Revision peg) throws VcsException {
-    myVcs = vcs;
-    myVersion = version;
-    myPeg = peg;
-    myPath =
-      getFilePathOnNonLocal(version.getRepositoryRoot().appendPath(version.getPath(), false).toDecodedString(), version.isDirectory());
-
-    mySessionAdapter = new VcsAppendableHistoryPartnerAdapter();
-    myProvider = myVcs.getVcsHistoryProvider();
-  }
-
-  public void setListToReportLoaded(TLongArrayList listToReportLoaded) {
-    myListToReportLoaded = listToReportLoaded;
-  }
-
-  @Override
-  public void load() throws VcsException {
-    Revision from = Revision.of(myVersion.getPegRevision());
-    myProvider.reportAppendableHistory(myPath, mySessionAdapter, from, myPeg, myPeg == null ? LIMIT : 0, myPeg, true);
-    VcsAbstractHistorySession session = mySessionAdapter.getSession();
-    if (myListToReportLoaded != null && session != null) {
-      List<VcsFileRevision> list = session.getRevisionList();
-      for (VcsFileRevision revision : list) {
-        myListToReportLoaded.add(((SvnRevisionNumber)revision.getRevisionNumber()).getRevision().getNumber());
+  @Throws(VcsException::class)
+  override fun load() {
+    val from = Revision.of(version.pegRevision)
+    provider.reportAppendableHistory(path, sessionAdapter, from, peg, if (peg == null) HISTORY_LIMIT else 0, peg, true)
+    val session = sessionAdapter.session
+    if (listToReportLoaded != null && session != null) {
+      val list = session.revisionList
+      for (revision in list) {
+        listToReportLoaded!!.add((revision.revisionNumber as SvnRevisionNumber).revision.number)
       }
     }
   }
 
-  @Override
-  public void dispose() {
-    if (myFileHistoryPanel != null) {
-      Disposer.dispose(myFileHistoryPanel);
+  override fun dispose() {
+    if (fileHistoryPanel != null) {
+      Disposer.dispose(fileHistoryPanel!!)
     }
   }
 
-  @Override
-  public JPanel createPanel() {
-    VcsAbstractHistorySession session = mySessionAdapter.getSession();
-    if (session == null) return EmptyConflictSide.INSTANCE.createPanel();
-    List<VcsFileRevision> list = session.getRevisionList();
+  override fun createPanel(): JPanel? {
+    val session = sessionAdapter.session ?: return EmptyConflictSide.createPanel()
+    val list = session.revisionList
     if (list.isEmpty()) {
-      return EmptyConflictSide.INSTANCE.createPanel();
+      return EmptyConflictSide.createPanel()
     }
-    VcsFileRevision last = null;
-    if (myPeg == null && list.size() == LIMIT ||
-        myPeg != null && myPeg.getNumber() > 0 &&
-        myPeg.equals(((SvnRevisionNumber)list.get(list.size() - 1).getRevisionNumber()).getRevision())) {
-      last = list.remove(list.size() - 1);
+    var last: VcsFileRevision? = null
+    if (peg == null && list.size == HISTORY_LIMIT ||
+        peg != null && peg.number > 0 &&
+        peg == (list[list.size - 1].revisionNumber as SvnRevisionNumber).revision) {
+      last = list.removeAt(list.size - 1)
     }
-    myFileHistoryPanel = new FileHistoryPanelImpl(myVcs, myPath, session, myProvider, new FileHistoryRefresherI() {
-      @Override
-      public void refresh(boolean canUseCache) {
+    fileHistoryPanel = FileHistoryPanelImpl(vcs, path, session, provider, object : FileHistoryRefresherI {
+      override fun refresh(canUseCache: Boolean) {
         //we will not refresh
       }
 
-      @Override
-      public void selectContent() {
+      override fun selectContent() {}
+      override fun isInRefresh(): Boolean {
+        return false
       }
-
-      @Override
-      public boolean isInRefresh() {
-        return false;
-      }
-    }, true);
-    myFileHistoryPanel.setBottomRevisionForShowDiff(last);
-    myFileHistoryPanel.setBorder(BorderFactory.createLineBorder(JBColor.border()));
-    return myFileHistoryPanel;
+    }, true)
+    fileHistoryPanel!!.setBottomRevisionForShowDiff(last)
+    fileHistoryPanel!!.border = BorderFactory.createLineBorder(JBColor.border())
+    return fileHistoryPanel
   }
 }
