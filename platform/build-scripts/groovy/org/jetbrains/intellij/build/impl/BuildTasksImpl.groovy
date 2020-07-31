dd91c80a3130e3a86cd88e830b80f4a0b853e881
Alexander Kass <Alexander.Kass@jetbrains.com>
2020-07-31 16:47:26 +0300

    add explanation to manual dbus-java packaging
    GitOrigin-RevId: bafc38f9cfc264e3b0cbb5f729868341034d8cf2

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/BuildTasksImpl.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/BuildTasksImpl.groovy
@@ -484,6 +484,9 @@ idea.fatal.error.notification=disabled
     }
   }
 
+  //dbus-java is used only on linux for KWallet integration.
+  //It relies on native libraries, causing notarization issues on mac.
+  //So it is excluded from all distributions and manually re-included on linux.
   static def addDbusJava(BuildContext buildContext, String distDir) {
     def library = buildContext.findModule("intellij.platform.credentialStore").libraryCollection.findLibrary("dbus-java")
     library.getFiles(JpsOrderRootType.COMPILED).each { f ->
