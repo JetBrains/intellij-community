663ee0493529cf773824877e0c6e6b11053b22fa
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2020-08-04 20:34:43 +0300

    [git-index] split GitFileStatusNodeAction file and extract base action class
    GitOrigin-RevId: b19b23f73e46dc985fb23e87a4816eb104dcc8c8

---
+++ plugins/git4idea/src/git4idea/index/actions/StagingAreaOperation.kt
@@ -0,0 +1,89 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package git4idea.index.actions
+
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.MessageType
+import com.intellij.openapi.vcs.FilePath
+import com.intellij.openapi.vcs.VcsBundle
+import com.intellij.openapi.vcs.VcsException
+import com.intellij.openapi.vcs.ui.VcsBalloonProblemNotifier
+import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.xml.util.XmlStringUtil
+import git4idea.i18n.GitBundle
+import git4idea.index.ui.GitFileStatusNode
+import git4idea.index.ui.NodeKind
+import git4idea.util.GitFileUtils
+import java.util.function.Supplier
+import javax.swing.Icon
+
+interface StagingAreaOperation {
+  val actionText: Supplier<String>
+  val progressTitle: String
+  val icon: Icon?
+  fun matches(statusNode: GitFileStatusNode): Boolean
+
+  @Throws(VcsException::class)
+  fun processPaths(project: Project, root: VirtualFile, paths: List<FilePath>)
+  fun showErrorMessage(project: Project, exceptions: Collection<VcsException>)
+}
+
+class GitAddAction : StagingAreaOperationAction(GitAddOperation)
+class GitResetAction : StagingAreaOperationAction(GitResetOperation)
+class GitRevertAction : StagingAreaOperationAction(GitRevertOperation)
+
+object GitAddOperation : StagingAreaOperation {
+  override val actionText get() = GitBundle.messagePointer("add.action.name")
+  override val progressTitle get() = GitBundle.message("add.adding")
+  override val icon = AllIcons.General.Add
+
+  override fun matches(statusNode: GitFileStatusNode) = statusNode.kind == NodeKind.UNSTAGED || statusNode.kind == NodeKind.UNTRACKED
+
+  override fun processPaths(project: Project, root: VirtualFile, paths: List<FilePath>) {
+    GitFileUtils.addPaths(project, root, paths, false)
+  }
+
+  override fun showErrorMessage(project: Project, exceptions: Collection<VcsException>) {
+    showErrorMessage(project, VcsBundle.message("error.adding.files.title"), exceptions)
+  }
+}
+
+object GitResetOperation : StagingAreaOperation {
+  override val actionText get() = GitBundle.messagePointer("stage.reset.action.text")
+  override val progressTitle get() = GitBundle.message("stage.reset.process")
+  override val icon = AllIcons.General.Remove
+
+  override fun matches(statusNode: GitFileStatusNode) = statusNode.kind == NodeKind.STAGED
+
+  override fun processPaths(project: Project, root: VirtualFile, paths: List<FilePath>) {
+    GitFileUtils.resetPaths(project, root, paths)
+  }
+
+  override fun showErrorMessage(project: Project, exceptions: Collection<VcsException>) {
+    showErrorMessage(project, GitBundle.message("stage.reset.error.title"), exceptions)
+  }
+}
+
+object GitRevertOperation : StagingAreaOperation {
+  override val actionText get() = GitBundle.messagePointer("stage.revert.action.text")
+  override val progressTitle get() = GitBundle.message("stage.revert.process")
+  override val icon = AllIcons.Actions.Rollback
+
+  override fun matches(statusNode: GitFileStatusNode) = statusNode.kind == NodeKind.UNSTAGED
+
+  override fun processPaths(project: Project, root: VirtualFile, paths: List<FilePath>) {
+    GitFileUtils.revertUnstagedPaths(project, root, paths)
+    LocalFileSystem.getInstance().refreshFiles(paths.mapNotNull { it.virtualFile })
+  }
+
+  override fun showErrorMessage(project: Project, exceptions: Collection<VcsException>) {
+    showErrorMessage(project, GitBundle.message("stage.revert.error.title"), exceptions)
+  }
+}
+
+private fun showErrorMessage(project: Project, messageTitle: String, exceptions: Collection<Exception>) {
+  VcsBalloonProblemNotifier.showOverVersionControlView(project, XmlStringUtil.wrapInHtmlTag("$messageTitle:", "b")
+                                                                + "\n" + exceptions.joinToString("\n") { it.localizedMessage },
+                                                       MessageType.ERROR)
+}
