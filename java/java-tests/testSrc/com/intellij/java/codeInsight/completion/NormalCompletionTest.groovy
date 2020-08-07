553cf412e7e08904dbdbd3b735496f47b6caa8f8
Peter Gromov <peter@jetbrains.com>
2020-08-07 18:59:51 +0200

    java: speedup resolving of statically-imported members by honoring name hint (IDEA-247726)
    GitOrigin-RevId: 8cbdf1fca1afc4023fc95f1ed5e6da5fcc589000

--- java/java-tests/testSrc/com/intellij/java/codeInsight/completion/NormalCompletionTest.groovy
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/completion/NormalCompletionTest.groovy
@@ -2197,4 +2197,21 @@ class Abc {
       assert !lookup
     }.assertTiming()
   }
+
+  void "test performance with many matching statically-imported declarations"() {
+    def fieldCount = 7000
+
+    myFixture.addClass("interface Constants {" +
+            (0..<fieldCount).collect { "String field$it = \"x\";\n" } +
+    "}")
+    myFixture.configureByText("a.java", "import static Constants.*; class C { { field<caret>x } }")
+    PlatformTestUtil.startPerformanceTest(name, 10_000, {
+      assert myFixture.completeBasic().length > 100
+    }).setup {
+      lookup?.hideLookup(true)
+      myFixture.type("\bd")
+      psiManager.dropPsiCaches()
+      assert !lookup
+    }.assertTiming()
+  }
 }
