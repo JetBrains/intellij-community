e5d5ce0feed3327ed08e604d427955808b0b9bfe
Nikita Skvortsov <Nikita.Skvortsov@jetbrains.com>
2020-08-07 17:59:39 +0300

    [gradle] add missing package name
    GitOrigin-RevId: 9ef42e14177e0b079d2f04c7d666457a8e9d19d1

--- plugins/gradle/java/testSources/execution/test/GradleJavaTestEventsIntegrationTest.kt
+++ plugins/gradle/java/testSources/execution/test/GradleJavaTestEventsIntegrationTest.kt
@@ -1,3 +1,6 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.plugins.gradle.execution.test
+
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskId
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskNotificationListenerAdapter
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskType
