077310eb85874c44d77ba73e545e9d2a38e550cc
Dmitry.Krasilschikov <kradima@jetbrains.com>
2020-08-09 22:22:24 +0300

    IDEA-245885 textWithMnemonic: cleanup
    GitOrigin-RevId: 0a2f90f3ff603d71b9122d743a4e644a910edc50

--- platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/PresentationFactory.java
+++ platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/PresentationFactory.java
@@ -15,24 +15,16 @@
  */
 package com.intellij.openapi.actionSystem.impl;
 
-import com.intellij.DynamicBundle;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.util.NotNullLazyValue;
-import com.intellij.openapi.util.SystemInfo;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.containers.WeakList;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
 
-import static com.intellij.openapi.actionSystem.Presentation.STRIP_MNEMONIC;
-
 public class PresentationFactory {
-  private static final @NotNull NotNullLazyValue<Boolean> hasAnyLanguagePack =
-    NotNullLazyValue.createValue(DynamicBundle.LanguageBundleEP.EP_NAME::hasAnyExtensions);
-
   private final Map<AnAction,Presentation> myAction2Presentation = ContainerUtil.createWeakMap();
 
   private static final WeakList<PresentationFactory> ourAllFactories = new WeakList<>();
@@ -60,11 +52,7 @@ public class PresentationFactory {
     return presentation;
   }
 
-  protected void processPresentation(Presentation presentation) {
-    if (SystemInfo.isMac && hasAnyLanguagePack.getValue()) {
-      presentation.putClientProperty(STRIP_MNEMONIC, Boolean.TRUE);
-    }
-  }
+  protected void processPresentation(Presentation presentation) { }
 
   public void reset() {
     ApplicationManager.getApplication().assertIsDispatchThread();
