57ac9aadc9bd70bc56db4dc9ff9d5f64aa81a2a6
Bas Leijdekkers <basleijdekkers@gmail.com>
2020-08-10 16:47:30 +0200

    IG: copied object passed to super constructor should not prevent warning (IDEA-247907)
    GitOrigin-RevId: 2d41b2e365d397a17e9dd4382a3e75f33ec1a631

--- plugins/InspectionGadgets/testsrc/com/siyeh/ig/bugs/CopyConstructorMissesFieldInspectionTest.java
+++ plugins/InspectionGadgets/testsrc/com/siyeh/ig/bugs/CopyConstructorMissesFieldInspectionTest.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.siyeh.ig.bugs;
 
 import com.intellij.codeInspection.InspectionProfileEntry;
@@ -9,6 +9,10 @@ import org.jetbrains.annotations.Nullable;
  * @author Bas Leijdekkers
  */
 public class CopyConstructorMissesFieldInspectionTest extends LightJavaInspectionTestCase {
+
+  public void testCopyConstructorMissesField() {
+    doTest();
+  }
 
   public void testSimple() {
     doTest("class Simple {" +
