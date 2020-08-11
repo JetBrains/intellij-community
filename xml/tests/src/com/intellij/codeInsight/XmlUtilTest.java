e72a9e0cb13cca48fb8dc7c38a106a1ec390abf4
Alexey Kudravtsev <cdr@intellij.com>
2020-08-11 20:23:41 +0300

    remove spam from console
    GitOrigin-RevId: cc91aeda407d5932f89cde20caf19fe02a84ddd5

--- xml/tests/src/com/intellij/codeInsight/XmlUtilTest.java
+++ xml/tests/src/com/intellij/codeInsight/XmlUtilTest.java
@@ -4,21 +4,24 @@ package com.intellij.codeInsight;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.diagnostic.DefaultLogger;
 import com.intellij.openapi.util.Disposer;
-import com.intellij.testFramework.UsefulTestCase;
 import com.intellij.xml.util.XmlUtil;
 import junit.framework.TestCase;
 
 public class XmlUtilTest extends TestCase {
 
   public void testDecodeEntityRef() {
+    assertEquals('&', XmlUtil.getCharFromEntityRef("&amp;"));
     Disposable myRoot = Disposer.newDisposable();
     DefaultLogger.disableStderrDumping(myRoot);
-    assertEquals('&', XmlUtil.getCharFromEntityRef("&amp;"));
     try {
-      UsefulTestCase.assertThrows(AssertionError.class, () -> XmlUtil.getCharFromEntityRef("&unknown;"));
+      XmlUtil.getCharFromEntityRef("&unknown;");
+    }
+    catch (AssertionError ignore) {
+      return;
     }
     finally {
       Disposer.dispose(myRoot);
     }
+    fail("Exception should be thrown");
   }
 }
