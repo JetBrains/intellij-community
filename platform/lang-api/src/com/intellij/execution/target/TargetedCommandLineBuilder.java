6cb5764d47947824728a0f5c94858ae40432d8cd
Alexander Zolotov <zolotov@jetbrains.com>
2020-08-13 14:44:28 +0300

    [remote targets] allow null environment variable values (GO-9758)
    GitOrigin-RevId: d2203dc4d6833899914251414f24589302473203

--- platform/lang-api/src/com/intellij/execution/target/TargetedCommandLineBuilder.java
+++ platform/lang-api/src/com/intellij/execution/target/TargetedCommandLineBuilder.java
@@ -5,6 +5,7 @@ import com.intellij.execution.target.value.TargetValue;
 import com.intellij.openapi.util.UserDataHolderBase;
 import com.intellij.openapi.vfs.CharsetToolkit;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.nio.charset.Charset;
@@ -89,12 +90,21 @@ public class TargetedCommandLineBuilder extends UserDataHolderBase {
     myParameters.add(index, parameter);
   }
 
-  public void addEnvironmentVariable(String name, TargetValue<String> value) {
-    myEnvironment.put(name, value);
+  public void addEnvironmentVariable(@NotNull String name, @Nullable TargetValue<String> value) {
+    if (value != null) {
+      myEnvironment.put(name, value);
+    }
+    else {
+      myEnvironment.remove(name);
+    }
   }
 
-  public void addEnvironmentVariable(String name, String value) {
-    myEnvironment.put(name, TargetValue.fixed(value));
+  public void addEnvironmentVariable(@NotNull String name, @Nullable String value) {
+    addEnvironmentVariable(name, value != null ? TargetValue.fixed(value) : null);
+  }
+
+  public void removeEnvironmentVariable(@NotNull String name) {
+    myEnvironment.remove(name);
   }
 
   public void addFileToDeleteOnTermination(@NotNull File file) {
