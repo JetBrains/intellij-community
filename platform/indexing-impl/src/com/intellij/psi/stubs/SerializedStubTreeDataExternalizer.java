ca57c25342fc2d9ab2c7ae0cea28e13397b6211e
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2020-08-13 10:49:27 +0300

    stubs: inline logic for data externalization if compression is disabled
    GitOrigin-RevId: 8beca71d43308ae63b19e1246510336e3d8a1166

--- platform/indexing-impl/src/com/intellij/psi/stubs/SerializedStubTreeDataExternalizer.java
+++ platform/indexing-impl/src/com/intellij/psi/stubs/SerializedStubTreeDataExternalizer.java
@@ -1,10 +1,8 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.stubs;
 
-import com.intellij.util.CompressionUtil;
 import com.intellij.util.io.DataExternalizer;
 import com.intellij.util.io.DataInputOutputUtil;
-import com.intellij.util.io.PersistentHashMapValueStorage;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.DataInput;
@@ -23,22 +21,16 @@ public class SerializedStubTreeDataExternalizer implements DataExternalizer<Seri
 
   @Override
   public final void save(@NotNull final DataOutput out, @NotNull final SerializedStubTree tree) throws IOException {
-    if (PersistentHashMapValueStorage.COMPRESSION_ENABLED) {
-      DataInputOutputUtil.writeINT(out, tree.myTreeByteLength);
-      out.write(tree.myTreeBytes, 0, tree.myTreeByteLength);
-      DataInputOutputUtil.writeINT(out, tree.myIndexedStubByteLength);
-      out.write(tree.myIndexedStubBytes, 0, tree.myIndexedStubByteLength);
-    }
-    else {
-      CompressionUtil.writeCompressed(out, tree.myTreeBytes, 0, tree.myTreeByteLength);
-      CompressionUtil.writeCompressed(out, tree.myIndexedStubBytes, 0, tree.myIndexedStubByteLength);
-    }
+    DataInputOutputUtil.writeINT(out, tree.myTreeByteLength);
+    out.write(tree.myTreeBytes, 0, tree.myTreeByteLength);
+    DataInputOutputUtil.writeINT(out, tree.myIndexedStubByteLength);
+    out.write(tree.myIndexedStubBytes, 0, tree.myIndexedStubByteLength);
   }
 
   @NotNull
   @Override
   public final SerializedStubTree read(@NotNull final DataInput in) throws IOException {
-    if (PersistentHashMapValueStorage.COMPRESSION_ENABLED) {
+    {
       int serializedStubsLength = DataInputOutputUtil.readINT(in);
       byte[] bytes = new byte[serializedStubsLength];
       in.readFully(bytes);
@@ -48,12 +40,6 @@ public class SerializedStubTreeDataExternalizer implements DataExternalizer<Seri
       indexedStubBytes = new byte[indexedStubByteLength];
       in.readFully(indexedStubBytes);
       return new SerializedStubTree(bytes, bytes.length, indexedStubBytes, indexedStubByteLength,
-                                    null, myStubIndexesExternalizer, mySerializationManager);
-    }
-    else {
-      byte[] treeBytes = CompressionUtil.readCompressed(in);
-      byte[] indexedStubBytes = CompressionUtil.readCompressed(in);
-      return new SerializedStubTree(treeBytes, treeBytes.length, indexedStubBytes, indexedStubBytes.length,
                                     null, myStubIndexesExternalizer, mySerializationManager);
     }
   }
