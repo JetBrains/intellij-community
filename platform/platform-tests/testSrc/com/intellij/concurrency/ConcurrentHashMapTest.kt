961f17649849f4f368c39fe12406e48b0d0efde0
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2020-08-13 18:02:40 +0300

    [util] get rid of explicit reference to internal Unsafe class in ConcurrentHashMap
    Use method handle to call its methods instead. This is needed to be able to compile intellij.platform.core.impl module using JDK 11 (IDEA-248086).
    
    The change doesn't affect performance of ConcurrentHashMap much, and anyway we plan to create a variant of it which uses var handles (see IDEA-244473), so this implementation will be used only in places which stay on Java 8.
    
    GitOrigin-RevId: d379a7c747aa720566249d31f16d9d9e0fe13ea5

---
+++ platform/platform-tests/testSrc/com/intellij/concurrency/ConcurrentHashMapTest.kt
@@ -0,0 +1,117 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.concurrency
+
+import org.assertj.core.api.Assertions.*
+import org.junit.Test
+
+class ConcurrentHashMapTest {
+  @Test
+  fun `put and get`() {
+    val map = ConcurrentHashMap<Int, Int>()
+    assertThat(map.size).isEqualTo(0)
+    assertThat(map.isEmpty()).isTrue()
+    map[1] = 0
+    map[2] = 1
+    assertThat(map.size).isEqualTo(2)
+    assertThat(map.isEmpty()).isFalse()
+    assertThat(map[1]).isEqualTo(0)
+    assertThat(map[2]).isEqualTo(1)
+    assertThat(map.getOrDefault(2, 4)).isEqualTo(1)
+    assertThat(map[3]).isNull()
+    assertThat(map.getOrDefault(3, 4)).isEqualTo(4)
+    assertThat(map.containsKey(1)).isTrue()
+    assertThat(map.containsKey(3)).isFalse()
+    assertThat(map.containsValue(1)).isTrue()
+    assertThat(map.containsValue(2)).isFalse()
+    map[2] = 2
+    assertThat(map[2]).isEqualTo(2)
+    assertThat(map.size).isEqualTo(2)
+  }
+
+  @Test
+  fun remove() {
+    val map = ConcurrentHashMap(mapOf(1 to 2, 2 to 3, 3 to 4))
+    assertThat(map.size).isEqualTo(3)
+    assertThat(map.remove(4)).isNull()
+    assertThat(map.remove(3, 5)).isFalse()
+    assertThat(map.size).isEqualTo(3)
+
+    assertThat(map.remove(3)).isEqualTo(4)
+    assertThat(map.size).isEqualTo(2)
+    assertThat(map.remove(2, 3)).isTrue()
+    assertThat(map.size).isEqualTo(1)
+    map.clear()
+    assertThat(map.isEmpty()).isTrue()
+  }
+
+  @Test
+  fun entries() {
+    val map = ConcurrentHashMap(mapOf(1 to 2, 2 to 3, 3 to 4))
+    assertThat(map.entries.map { it.key to it.value }).containsExactlyInAnyOrder(1 to 2, 2 to 3, 3 to 4)
+    assertThat(map.keys).containsExactlyInAnyOrder(1, 2, 3)
+    assertThat(map.values).containsExactlyInAnyOrder(2, 3, 4)
+  }
+
+  @Test
+  fun `put if absent`() {
+    val map = ConcurrentHashMap(mapOf(1 to 2))
+    assertThat(map.putIfAbsent(1, 3)).isEqualTo(2)
+    assertThat(map[1]).isEqualTo(2)
+    assertThat(map.putIfAbsent(2, 3)).isNull()
+    assertThat(map[2]).isEqualTo(3)
+  }
+
+  @Test
+  fun `compute if absent`() {
+    val map = ConcurrentHashMap(mapOf(1 to 2))
+    assertThat(map.computeIfAbsent(1) { 3 }).isEqualTo(2)
+    assertThat(map[1]).isEqualTo(2)
+    assertThat(map.computeIfAbsent(2) { 3 }).isEqualTo(3)
+    assertThat(map[2]).isEqualTo(3)
+  }
+
+  @Test
+  fun `compute if present`() {
+    val map = ConcurrentHashMap(mapOf(1 to 2))
+    assertThat(map.computeIfPresent(2) { _, _ -> 3 }).isNull()
+    assertThat(map[1]).isEqualTo(2)
+    assertThat(map.computeIfPresent(1) { _, _ -> 3 }).isEqualTo(3)
+    assertThat(map[1]).isEqualTo(3)
+  }
+
+  @Test
+  fun compute() {
+    val map = ConcurrentHashMap(mapOf(1 to 2))
+    assertThat(map.compute(1) { _, _ -> 3 }).isEqualTo(3)
+    assertThat(map[1]).isEqualTo(3)
+    assertThat(map.compute(2) { _, _ -> 3 }).isEqualTo(3)
+    assertThat(map[2]).isEqualTo(3)
+  }
+
+  @Test
+  fun replace() {
+    val map = ConcurrentHashMap(mapOf(1 to 2, 2 to 3))
+    assertThat(map.replace(3, 4)).isNull()
+    assertThat(map.replace(2, 4)).isEqualTo(3)
+    assertThat(map[2]).isEqualTo(4)
+    assertThat(map.replace(1, 3, 4)).isFalse()
+    assertThat(map.replace(1, 2, 3)).isTrue()
+    assertThat(map[1]).isEqualTo(3)
+  }
+
+  @Test
+  fun `replace all`() {
+    val map = ConcurrentHashMap(mapOf(1 to 2, 2 to 3))
+    map.replaceAll { k, v -> k + v}
+    assertThat(map).isEqualTo(mapOf(1 to 3, 2 to 5))
+  }
+
+  @Test
+  fun merge() {
+    val map = ConcurrentHashMap(mapOf(1 to 2, 2 to 3))
+    assertThat(map.merge(3, 4) { _, _ -> 4 }).isEqualTo(4)
+    assertThat(map[3]).isEqualTo(4)
+    assertThat(map.merge(1, 3) { a, b -> a+b }).isEqualTo(5)
+    assertThat(map[1]).isEqualTo(5)
+  }
+}
