869a5db76d5351c3bf17d9403d122f73298e77bb
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2020-08-13 14:58:57 +0300

    [platform] Add `nobr` html tag to Event Log parsing process
    GitOrigin-RevId: d0ca2a9edbd2e5c5ebc94ea158d2847fc77d7376

--- platform/platform-impl/src/com/intellij/notification/EventLog.java
+++ platform/platform-impl/src/com/intellij/notification/EventLog.java
@@ -333,7 +333,7 @@ public final class EventLog {
       "blockquote", "body", "br", "button", "canvas", "caption", "center", "cite", "code", "col", "colgroup", "command", "datalist", "dd",
       "del", "details", "dfn", "dir", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "font", "footer", "form",
       "frame", "frameset", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hgroup", "hr", "html", "i", "iframe", "img", "input",
-      "ins", "kbd", "keygen", "label", "legend", "li", "link", "map", "mark", "menu", "meta", "meter", "nav", "noframes", "noscript",
+      "ins", "kbd", "keygen", "label", "legend", "li", "link", "map", "mark", "menu", "meta", "meter", "nav", "nobr", "noframes", "noscript",
       "object", "ol", "optgroup", "option", "output", "p", "param", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script",
       "section", "select", "small", "source", "span", "strike", "strong", "style", "sub", "summary", "sup", "table", "tbody", "td",
       "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "tt", "u", "ul", "var", "video", "wbr"};
