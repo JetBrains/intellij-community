68955e264c05ae206adf7c4b6f8002d637591dbd
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2020-08-17 20:01:44 +0300

    i18n: annotate PsiNamedElement#setName parameter with @NlsSafe
    GitOrigin-RevId: cb35ba08a6d7972a5fc631c1bb7dc95f8d65b6dc

--- platform/core-api/src/com/intellij/psi/PsiNamedElement.java
+++ platform/core-api/src/com/intellij/psi/PsiNamedElement.java
@@ -33,5 +33,5 @@ public interface PsiNamedElement extends PsiElement {
    * or a different element if the rename caused the element to be replaced).
    * @throws IncorrectOperationException if the modification is not supported or not possible for some reason.
    */
-  PsiElement setName(@NotNull String name) throws IncorrectOperationException;
+  PsiElement setName(@NlsSafe @NotNull String name) throws IncorrectOperationException;
 }
