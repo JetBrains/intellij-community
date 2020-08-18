89c188e76d89de3724825727f3e70d98edfdd351
Yann Cébron <yann.cebron@jetbrains.com>
2020-08-18 16:36:44 +0200

    [devkit] NewServiceActionBase: I18n (IDEA-247667)
    GitOrigin-RevId: 578aac7916e9d18d1e3d4dd378cc314086cda951

--- plugins/devkit/devkit-core/src/actions/service/NewServiceActionBase.java
+++ plugins/devkit/devkit-core/src/actions/service/NewServiceActionBase.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.devkit.actions.service;
 
 import com.intellij.ide.IdeView;
@@ -15,6 +15,8 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.ui.ValidationInfo;
+import com.intellij.openapi.util.NlsContexts;
+import com.intellij.openapi.util.NlsSafe;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiDirectory;
@@ -24,7 +26,6 @@ import com.intellij.psi.xml.XmlTag;
 import com.intellij.ui.DocumentAdapter;
 import com.intellij.util.IncorrectOperationException;
 import com.intellij.util.containers.ContainerUtil;
-import com.intellij.openapi.util.NlsContexts;
 import com.intellij.xml.util.IncludedXmlTag;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
@@ -44,7 +45,7 @@ import java.util.List;
 import java.util.concurrent.Callable;
 
 /**
- * An base class for actions generating service classes (implementation and optionally interface) and registering new service in {@code plugin.xml}.
+ * A base class for actions generating service classes (implementation and optionally interface) and registering new service in {@code plugin.xml}.
  */
 abstract class NewServiceActionBase extends CreateInDirectoryActionBase implements WriteActionAware {
   protected NewServiceActionBase() { }
@@ -365,7 +366,7 @@ abstract class NewServiceActionBase extends CreateInDirectoryActionBase implemen
 
     private void handleException(Throwable t) {
       LOG.info(t);
-      String errorMessage = ElementCreator.getErrorMessage(t);
+      @NlsSafe String errorMessage = ElementCreator.getErrorMessage(t);
       Messages.showMessageDialog(
         getProject(), errorMessage, DevKitBundle.message("error.cannot.create.service.class"), Messages.getErrorIcon());
     }
