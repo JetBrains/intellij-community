25b2e9a44eede24d387f9471b9f64daf844b7232
Daniil Tsaryov <daniil.tsaryov@jetbrains.com>
2020-08-19 15:34:22 +0300

    i18n [vcs] Suppress not relevant inspections in UIVcsUtil
    GitOrigin-RevId: fe6e99aec743d48c88773607afeeccdf68e6895c

--- platform/vcs-api/src/com/intellij/vcsUtil/UIVcsUtil.java
+++ platform/vcs-api/src/com/intellij/vcsUtil/UIVcsUtil.java
@@ -3,6 +3,7 @@ package com.intellij.vcsUtil;
 
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.ui.SimpleTextAttributes;
+import com.intellij.util.ui.JBUI;
 import com.intellij.util.ui.UIUtil;
 import com.intellij.xml.util.XmlStringUtil;
 import org.jetbrains.annotations.Nls;
@@ -20,23 +21,24 @@ public final class UIVcsUtil {
     final JLabel label = new JLabel(XmlStringUtil.wrapInHtml(escapeXmlAndAddBr(text)));
     label.setForeground(isError ? SimpleTextAttributes.ERROR_ATTRIBUTES.getFgColor() : UIUtil.getInactiveTextColor());
     final JPanel wrapper = new JPanel(new GridBagLayout());
-    wrapper.add(label, new GridBagConstraints(0,0,1,1,0,0,GridBagConstraints.CENTER, GridBagConstraints.NONE,
-                                                         new Insets(1,1,1,1), 0,0));
+    wrapper.add(label, new GridBagConstraints(0, 0, 1, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.NONE, JBUI.insets(1), 0, 0));
     return wrapper;
   }
 
-  private static String escapeXmlAndAddBr(@NotNull String text) {
-    String escaped = StringUtil.escapeXmlEntities(text);
-    escaped = StringUtil.replace(escaped, "\n", "<br/>");
-    return escaped;
-  }
-
-  public static JPanel infoPanel(@NotNull String header, @NotNull String text) {
-    final JLabel label = new JLabel(XmlStringUtil.wrapInHtml("<h4>" + StringUtil.escapeXmlEntities(header) +
-                                                             "</h4>" + escapeXmlAndAddBr(text)));
+  public static JPanel infoPanel(@NotNull String header, @Nls @NotNull String text) {
+    //noinspection HardCodedStringLiteral
+    final JLabel label = new JLabel(XmlStringUtil.wrapInHtml(
+      "<h4>" + StringUtil.escapeXmlEntities(header) + "</h4>" + escapeXmlAndAddBr(text)));
     final JPanel wrapper = new JPanel(new GridBagLayout());
-    wrapper.add(label, new GridBagConstraints(0,0,1,1,0,0,GridBagConstraints.CENTER, GridBagConstraints.NONE,
-                                                         new Insets(1,1,1,1), 0,0));
+    wrapper.add(label, new GridBagConstraints(0, 0, 1, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.NONE, JBUI.insets(1), 0, 0));
     return wrapper;
+  }
+
+  @Nls
+  private static String escapeXmlAndAddBr(@Nls @NotNull String text) {
+    String escaped = StringUtil.escapeXmlEntities(text);
+    //noinspection HardCodedStringLiteral
+    escaped = StringUtil.replace(escaped, "\n", UIUtil.BR);
+    return escaped;
   }
 }
