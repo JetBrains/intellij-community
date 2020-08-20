479f3275ba6e90a948fb91cfaef17a9bbaa6f854
Aleksey Pivovarov <AMPivovarov@gmail.com>
2020-08-20 15:46:00 +0300

    ui: i18n - mark DateFormatUtil as @NlsSafe
    GitOrigin-RevId: bc8492808a25e736c9276a39cc8268e92f3bdc52

--- platform/util/src/com/intellij/util/text/SyncDateFormat.java
+++ platform/util/src/com/intellij/util/text/SyncDateFormat.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.text;
 
+import com.intellij.openapi.util.NlsSafe;
 import org.jetbrains.annotations.NotNull;
 
 import java.text.DateFormat;
@@ -19,10 +20,12 @@ public class SyncDateFormat {
     return myDelegate.parse(s);
   }
 
+  @NlsSafe
   public synchronized String format(@NotNull Date date) {
     return myDelegate.format(date);
   }
 
+  @NlsSafe
   public synchronized String format(long time) {
     return myDelegate.format(time);
   }
