d65cb3d64b765a9cea20b8ac18e5f730695448b0
Yann Cébron <yann.cebron@jetbrains.com>
2020-08-20 14:41:42 +0200

    [devkit] PostfixTemplateDescriptionNotFoundInspection: I18n (IDEA-247667)
    GitOrigin-RevId: 85beb83f86aef5afbad852f40a1d5a87b5281400

--- plugins/devkit/devkit-core/src/inspections/PostfixTemplateDescriptionNotFoundInspection.java
+++ plugins/devkit/devkit-core/src/inspections/PostfixTemplateDescriptionNotFoundInspection.java
@@ -1,23 +1,10 @@
-/*
- * Copyright 2000-2019 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.devkit.inspections;
 
 import com.intellij.openapi.module.Module;
 import com.intellij.psi.PsiClass;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.idea.devkit.DevKitBundle;
 
 public class PostfixTemplateDescriptionNotFoundInspection extends DescriptionNotFoundInspectionBase {
 
@@ -33,12 +20,12 @@ public class PostfixTemplateDescriptionNotFoundInspection extends DescriptionNot
   @NotNull
   @Override
   protected String getHasNotDescriptionError(Module module, PsiClass psiClass) {
-    return "Postfix template does not have a description";
+    return DevKitBundle.message("inspections.postfix.description.not.found");
   }
 
   @NotNull
   @Override
   protected String getHasNotBeforeAfterError() {
-    return "Postfix template must have 'before.*.template' and 'after.*.template' beside 'description.html'";
+    return DevKitBundle.message("inspections.postfix.description.no.before.after.template");
   }
 }
