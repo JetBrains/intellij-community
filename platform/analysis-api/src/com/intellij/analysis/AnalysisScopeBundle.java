fdfa5ca36ebaf4b87734899b4be6b3523d72f24d
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2020-08-21 14:22:55 +0300

    [core] i18n: move properties from AnalysisScopeBundle to CoreDeprecatedMessagesBundle  (IDEA-CR-66213)
    GitOrigin-RevId: 7b660a1f3a71e92fae0724273f8c4b0545a95b05

--- platform/analysis-api/src/com/intellij/analysis/AnalysisScopeBundle.java
+++ platform/analysis-api/src/com/intellij/analysis/AnalysisScopeBundle.java
@@ -1,31 +1,20 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.analysis;
 
-import com.intellij.DynamicBundle;
+import com.intellij.core.CoreDeprecatedMessagesBundle;
 import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.PropertyKey;
-
-import java.util.function.Supplier;
 
 /**
  * @deprecated use other bundles ({@link AnalysisBundle}, {@link com.intellij.codeInsight.CodeInsightBundle}) instead
  */
 @Deprecated
-public final class AnalysisScopeBundle extends DynamicBundle {
-  @NonNls private static final String BUNDLE = "messages.AnalysisScopeBundle";
-  private static final AnalysisScopeBundle INSTANCE = new AnalysisScopeBundle();
-
-  private AnalysisScopeBundle() { super(BUNDLE); }
-
-  @NotNull
-  public static @Nls String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, Object @NotNull ... params) {
-    return INSTANCE.getMessage(key, params);
+public final class AnalysisScopeBundle {
+  private AnalysisScopeBundle() {
   }
 
   @NotNull
-  public static Supplier<@Nls String> messagePointer(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, Object @NotNull ... params) {
-    return INSTANCE.getLazyMessage(key, params);
+  public static @Nls String message(@NotNull String key, Object @NotNull ... params) {
+    return CoreDeprecatedMessagesBundle.message(key, params);
   }
 }
