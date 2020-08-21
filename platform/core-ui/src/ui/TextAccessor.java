d2ee8d10261736d4545f6b210ff170f5beb8cb11
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2020-08-21 20:40:40 +0200

    [platform] i18n: NLS-safe `TextFieldWithBrowseButton#setText`
    GitOrigin-RevId: 3a04ccbb58e7e09a2c421a941c0e2e3531f23c88

--- platform/core-ui/src/ui/TextAccessor.java
+++ platform/core-ui/src/ui/TextAccessor.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ui;
 
 import com.intellij.openapi.util.NlsSafe;
@@ -6,6 +6,5 @@ import com.intellij.openapi.util.NlsSafe;
 public interface TextAccessor {
   void setText(String text);
 
-  @NlsSafe
-  String getText();
+  @NlsSafe String getText();
 }
