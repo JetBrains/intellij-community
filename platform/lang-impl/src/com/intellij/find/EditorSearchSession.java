5eeeb4c233df124eae0df70559bfa41b9ef056d5
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2020-08-21 20:31:49 +0300

    IDEA-248371 [platform] i18n: EditorSearchSession
    GitOrigin-RevId: 362c472321cce06e8945f8aab48d632d8cf994ef

--- platform/lang-impl/src/com/intellij/find/EditorSearchSession.java
+++ platform/lang-impl/src/com/intellij/find/EditorSearchSession.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.find;
 
@@ -673,7 +673,7 @@ public class EditorSearchSession implements SearchSession,
 
   private class ReplaceAction extends ButtonAction implements LightEditCompatible {
     ReplaceAction() {
-      super("Replace", 'p');
+      super(ApplicationBundle.message("editorsearch.replace.action.text"), 'p');
     }
 
     @Override
@@ -689,7 +689,7 @@ public class EditorSearchSession implements SearchSession,
 
   private class ReplaceAllAction extends ButtonAction implements LightEditCompatible {
     ReplaceAllAction() {
-      super("Replace all", 'a');
+      super(ApplicationBundle.message("editorsearch.replace.all.action.text"), 'a');
     }
 
     @Override
