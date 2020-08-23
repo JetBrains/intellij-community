b94d83f1a5da1188aca623d04b588a0a32acbd2e
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2020-08-23 10:31:23 +0300

    fix tests on Windows
    GitOrigin-RevId: 765e57b2f423f9d6c38ae225035e057690f7a31e

--- json/tests/test/com/jetbrains/jsonSchema/remote/JsonSchemaCatalogManagerTest.java
+++ json/tests/test/com/jetbrains/jsonSchema/remote/JsonSchemaCatalogManagerTest.java
@@ -49,7 +49,6 @@ public class JsonSchemaCatalogManagerTest extends BasePlatformTestCase {
     doTest("jenkins-y.yml", null);
     doTest("my.schema.json", "https://jsonapi.org/schema");
     doTest("schema.json", null);
-    doTest("<hello>.json", null);
   }
 
   public void testPerformance() {
