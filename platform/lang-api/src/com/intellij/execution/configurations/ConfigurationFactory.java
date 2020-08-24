970e8e4e5abbf3cd4a2a28f59118d3a5083209d5
Max Medvedev <maxim.medvedev@jetbrains.com>
2020-08-24 15:06:43 +0300

    IDEA-247660 [appcode i18n] execution: fix XcodeSchemesManager
    GitOrigin-RevId: cd50af0b1a9537f1619daf6e6ed5f733e3f0a461

--- platform/lang-api/src/com/intellij/execution/configurations/ConfigurationFactory.java
+++ platform/lang-api/src/com/intellij/execution/configurations/ConfigurationFactory.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.configurations;
 
 import com.intellij.execution.BeforeRunTask;
@@ -6,6 +6,7 @@ import com.intellij.execution.RunManager;
 import com.intellij.openapi.components.BaseState;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Key;
+import com.intellij.openapi.util.NlsSafe;
 import com.intellij.util.DeprecatedMethodException;
 import com.intellij.util.IconUtil;
 import org.jetbrains.annotations.NonNls;
@@ -38,7 +39,7 @@ public abstract class ConfigurationFactory {
    * @return the new run configuration.
    */
   @NotNull
-  public RunConfiguration createConfiguration(@Nullable String name, @NotNull RunConfiguration template) {
+  public RunConfiguration createConfiguration(@NlsSafe @Nullable String name, @NotNull RunConfiguration template) {
     RunConfiguration newConfiguration = template.clone();
     newConfiguration.setName(name);
     return newConfiguration;
