89440a896d82b877505388399c208ffff1874d49
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2020-08-24 12:09:30 +0700

    i18n: Extract Java model name in settings into bundle (IDEA-246471)
    GitOrigin-RevId: d5a15c7db14e9a2b997c3a836cc5e7e76b0cdbaa

--- java/java-impl/src/com/intellij/codeInsight/completion/ml/JavaMLRankingProvider.kt
+++ java/java-impl/src/com/intellij/codeInsight/completion/ml/JavaMLRankingProvider.kt
@@ -4,10 +4,11 @@ package com.intellij.codeInsight.completion.ml
 import com.intellij.internal.ml.*
 import com.intellij.internal.ml.completion.CompletionRankingModelBase
 import com.intellij.internal.ml.completion.JarCompletionModelProvider
+import com.intellij.java.JavaBundle
 import com.intellij.lang.Language
 import com.jetbrains.completion.ranker.model.java.MLGlassBox
 
-class JavaMLRankingProvider : JarCompletionModelProvider("Java", "java_features") {
+class JavaMLRankingProvider : JarCompletionModelProvider(JavaBundle.message("settings.completion.ml.java.display.name"), "java_features") {
   override fun createModel(metadata: ModelMetadata): DecisionFunction {
     return object : CompletionRankingModelBase(metadata) {
       override fun predict(features: DoubleArray?): Double = MLGlassBox.makePredict(features)
