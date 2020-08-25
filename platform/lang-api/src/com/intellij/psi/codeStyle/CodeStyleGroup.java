3a109593e5412ebaf72567a6e90502c4d4821b3d
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2020-08-25 17:37:01 +0300

    IDEA-248371 [platform] i18n: CodeStyleGroup
    GitOrigin-RevId: 79e562ac4c8ec781f0d52277ffdf0d745c5834fc

--- platform/lang-api/src/com/intellij/psi/codeStyle/CodeStyleGroup.java
+++ platform/lang-api/src/com/intellij/psi/codeStyle/CodeStyleGroup.java
@@ -1,18 +1,21 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.codeStyle;
 
-
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import static org.jetbrains.annotations.Nls.Capitalization.Title;
 
 /**
  * Represents a group of code style configurables sharing the same {@code CodeStyleGroup} instance. The group is shown as a separate node
  * in settings UI with child configurables under it.
  */
 public class CodeStyleGroup {
-  private final String myDisplayName;
-  private final String myId;
-  private final String myHelpTopic;
+  private final @Nls(capitalization = Title) String myDisplayName;
+  private final @NonNls String myId;
+  private final @NonNls String myHelpTopic;
 
   /**
    * Constructor.
@@ -20,7 +23,7 @@ public class CodeStyleGroup {
    * @param groupId     The unique group ID.
    * @param displayName The display name as shown in settings UI.
    */
-  public CodeStyleGroup(@NotNull String groupId, @NotNull String displayName) {
+  public CodeStyleGroup(@NonNls @NotNull String groupId, @Nls(capitalization = Title) @NotNull String displayName) {
     this(groupId, displayName, null);
   }
 
@@ -31,7 +34,7 @@ public class CodeStyleGroup {
    * @param displayName The display name as shown in settings UI.
    * @param helpTopic   The help topic associated with the group.
    */
-  public CodeStyleGroup(@NotNull String groupId, @NotNull String displayName, @Nullable String helpTopic) {
+  public CodeStyleGroup(@NonNls @NotNull String groupId, @Nls(capitalization = Title) @NotNull String displayName, @NonNls @Nullable String helpTopic) {
     myId = groupId;
     myDisplayName = displayName;
     myHelpTopic = helpTopic;
@@ -41,19 +44,19 @@ public class CodeStyleGroup {
    * @return The group display name.
    */
   @NotNull
-  public String getDisplayName() {
+  public @Nls(capitalization = Title) String getDisplayName() {
     return myDisplayName;
   }
 
   /**
    * @return The group ID.
    */
-  public String getId() {
+  public @NonNls String getId() {
     return myId;
   }
 
   @Nullable
-  public String getHelpTopic() {
+  public @NonNls String getHelpTopic() {
     return myHelpTopic;
   }
 }
