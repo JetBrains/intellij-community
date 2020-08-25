608718b90757ea1724192b888380758ccd0596da
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2020-08-25 13:04:46 +0300

    [git] i18n: GitPushResultNotification
    GitOrigin-RevId: 03a93b33475802e39dfa7a6530846e4eb5aa7f03

--- plugins/git4idea/src/git4idea/push/GitPushRepoResult.java
+++ plugins/git4idea/src/git4idea/push/GitPushRepoResult.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package git4idea.push;
 
+import com.intellij.openapi.util.NlsSafe;
 import com.intellij.util.containers.ContainerUtil;
 import git4idea.GitLocalBranch;
 import git4idea.GitRemoteBranch;
@@ -120,16 +121,18 @@ public final class GitPushRepoResult {
     return myTargetBranch;
   }
 
+  @NlsSafe
   @Nullable
   String getError() {
     return myError;
   }
 
   @NotNull
-  List<String> getPushedTags() {
+  List<@NlsSafe String> getPushedTags() {
     return myPushedTags;
   }
 
+  @NlsSafe
   @NotNull
   public String getTargetRemote() {
     return myTargetRemote;
