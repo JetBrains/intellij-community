00d127e522043993d295ae37acbed552e4cfe227
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2020-08-25 21:07:22 +0300

    [groovy, i18n] IDEA-247643: NLS for GrCreateClassKind
    GitOrigin-RevId: 88356efb3ef16bb5bfc9b024437df8ac34fd259f

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/GrCreateClassKind.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/GrCreateClassKind.java
@@ -2,22 +2,28 @@
 package org.jetbrains.plugins.groovy.lang;
 
 import com.intellij.codeInsight.daemon.impl.quickfix.ClassKind;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.GroovyBundle;
+
+import java.util.function.Supplier;
 
 public enum GrCreateClassKind implements ClassKind {
-  CLASS     ("class"),
-  INTERFACE ("interface"),
-  TRAIT     ("trait"),
-  ENUM      ("enum"),
-  ANNOTATION("annotation");
+  CLASS     (GroovyBundle.messagePointer("groovy.term.class")),
+  INTERFACE (GroovyBundle.messagePointer("groovy.term.interface")),
+  TRAIT     (GroovyBundle.messagePointer("groovy.term.trait")),
+  ENUM      (GroovyBundle.messagePointer("groovy.term.enum")),
+  ANNOTATION(GroovyBundle.messagePointer("groovy.term.annotation"));
 
-  private final String myDescription;
+  private final Supplier<@Nls @NotNull String> myDescription;
 
-  GrCreateClassKind(final String description) {
+  GrCreateClassKind(final Supplier<String> description) {
     myDescription = description;
   }
 
   @Override
+  @Nls
   public String getDescription() {
-    return myDescription;
+    return myDescription.get();
   }
 }
