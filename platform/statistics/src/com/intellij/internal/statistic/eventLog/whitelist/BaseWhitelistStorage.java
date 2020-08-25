9aba74db7a25ecb678b5e94dc09649a19acf32d1
Anastasia Ivanova <anastasia.ivanova@jetbrains.com>
2020-08-25 16:52:57 +0200

    IDEA-CR-66197 cache global validation rules by references
    GitOrigin-RevId: 0dfb3e46a0880812742e6c15088faccc6c60957c

--- platform/statistics/src/com/intellij/internal/statistic/eventLog/whitelist/BaseWhitelistStorage.java
+++ platform/statistics/src/com/intellij/internal/statistic/eventLog/whitelist/BaseWhitelistStorage.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.internal.statistic.eventLog.whitelist;
 
 import com.intellij.internal.statistic.eventLog.EventLogBuild;
@@ -27,16 +27,11 @@ public abstract class BaseWhitelistStorage implements WhitelistGroupRulesStorage
 
   @NotNull
   protected Map<String, EventGroupRules> createValidators(@Nullable EventLogBuild build, @NotNull FUStatisticsWhiteListGroupsService.WLGroups groups) {
+    GlobalRulesHolder globalRulesHolder = new GlobalRulesHolder(groups.rules);
     return groups.groups.stream().
       filter(group -> StatisticsWhitelistGroupConditions.create(group).accepts(build)).
-      collect(Collectors.toMap(group -> group.id, group -> createRules(group, groups.rules)));
-  }
-
-  @NotNull
-  protected static EventGroupRules createRules(@NotNull FUStatisticsWhiteListGroupsService.WLGroup group,
-                                               @Nullable FUStatisticsWhiteListGroupsService.WLRule globalRules) {
-    return globalRules != null
-           ? EventGroupRules.create(group, globalRules.enums, globalRules.regexps)
-           : EventGroupRules.create(group, null, null);
+      collect(Collectors.toMap(group -> group.id, group -> {
+        return EventGroupRules.create(group, globalRulesHolder);
+      }));
   }
 }
