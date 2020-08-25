713b45c78b1d0ba57f66530c23f0aa8a46ef49f8
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2020-08-25 17:55:29 +0300

    IDEA-248865 [java] add registry key to allow turning off the behaviour introduced in IDEA-194817
    GitOrigin-RevId: b9bfaa4599c374abf57b6d881efa4c47ae381a7e

--- java/java-psi-impl/src/com/intellij/psi/impl/source/PsiEnumConstantImpl.java
+++ java/java-psi-impl/src/com/intellij/psi/impl/source/PsiEnumConstantImpl.java
@@ -6,6 +6,7 @@ import com.intellij.navigation.ItemPresentation;
 import com.intellij.navigation.ItemPresentationProviders;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.util.registry.Registry;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.ElementPresentationUtil;
 import com.intellij.psi.impl.PsiClassImplUtil;
@@ -219,7 +220,13 @@ public class PsiEnumConstantImpl extends JavaStubPsiElement<PsiFieldStub> implem
     @Override
     public TextRange getRangeInElement() {
       PsiIdentifier nameIdentifier = getNameIdentifier();
-      return TextRange.from(nameIdentifier.getStartOffsetInParent() + nameIdentifier.getTextLength(), 0);
+      int startOffsetInParent = nameIdentifier.getStartOffsetInParent();
+      if (Registry.is("java.empty.enum.constructor.ref")) {
+        return TextRange.from(startOffsetInParent + nameIdentifier.getTextLength(), 0);
+      }
+      else {
+        return TextRange.from(startOffsetInParent, nameIdentifier.getTextLength());
+      }
     }
 
     @Override
