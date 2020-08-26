9dc06d847564f064e4f7c8dc59c36c4d52802c30
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2020-08-26 15:29:40 +0300

    IDEA-247643 [groovy] i18n: get rid of GroovyTransformationsBundle
    GitOrigin-RevId: adc3f987ea49ff4b36c07aa665d577bf460e6748

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/transformations/singleton/SingletonConstructorInspection.kt
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/transformations/singleton/SingletonConstructorInspection.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package org.jetbrains.plugins.groovy.transformations.singleton
 
@@ -21,11 +7,11 @@ import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementVisitor
+import org.jetbrains.plugins.groovy.GroovyBundle
 import org.jetbrains.plugins.groovy.codeInspection.fixes.RemoveElementQuickFix
 import org.jetbrains.plugins.groovy.lang.lexer.GroovyTokenTypes
 import org.jetbrains.plugins.groovy.lang.psi.impl.booleanValue
 import org.jetbrains.plugins.groovy.lang.psi.impl.findDeclaredDetachedValue
-import org.jetbrains.plugins.groovy.transformations.GroovyTransformationsBundle.message
 
 class SingletonConstructorInspection : LocalInspectionTool() {
 
@@ -39,9 +25,9 @@ class SingletonConstructorInspection : LocalInspectionTool() {
 
       holder.registerProblem(
         element,
-        message("singleton.constructor.found"),
+        GroovyBundle.message("singleton.constructor.found"),
         ProblemHighlightType.GENERIC_ERROR_OR_WARNING,
-        RemoveElementQuickFix(message("singleton.constructor.remove")) { e -> e.parent },
+        RemoveElementQuickFix(GroovyBundle.message("singleton.constructor.remove")) { e -> e.parent },
         MakeNonStrictQuickFix()
       )
     }
