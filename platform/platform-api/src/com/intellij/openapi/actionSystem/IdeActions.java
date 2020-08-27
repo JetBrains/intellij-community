290ee6f3cb4711c9d7270d508f806719d474e378
Dmitry Osinovskiy <dmitry.osinovsky@jetbrains.com>
2020-08-28 01:04:12 +0200

    Expose IndentSelectionAction to rdclient
    GitOrigin-RevId: fba21830f14bb234633e089bac4c81212c19cab8

--- platform/platform-api/src/com/intellij/openapi/actionSystem/IdeActions.java
+++ platform/platform-api/src/com/intellij/openapi/actionSystem/IdeActions.java
@@ -51,6 +51,8 @@ public interface IdeActions {
   @NonNls String ACTION_EDITOR_NEXT_WORD_WITH_SELECTION = "EditorNextWordWithSelection";
   @NonNls String ACTION_EDITOR_PREVIOUS_WORD_WITH_SELECTION = "EditorPreviousWordWithSelection";
   @NonNls String ACTION_EDITOR_TAB = "EditorTab";
+  @NonNls String ACTION_EDITOR_INDENT_SELECTION = "EditorIndentSelection";
+  @NonNls String ACTION_EDITOR_UNINDENT_SELECTION = "EditorUnindentSelection";
   @NonNls String ACTION_EDITOR_EMACS_TAB = "EmacsStyleIndent";
   @NonNls String ACTION_EDITOR_ESCAPE = "EditorEscape";
   @NonNls String ACTION_EDITOR_JOIN_LINES = "EditorJoinLines";
