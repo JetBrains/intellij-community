8309d3e79a407b0a0312ed00b9c57f82f1352883
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2020-08-27 16:46:59 +0300

    Rewrite SurroundWithSuggester using more document actions than PSI actions
    IJ-CR-14522
    
    GitOrigin-RevId: e93eb271bf1ff1be1faf04b53249dc8bf85ab323

--- plugins/ide-features-trainer/src/main/kotlin/org/jetbrains/plugins/feature/suggester/suggesters/SurroundWithSuggester.kt
+++ plugins/ide-features-trainer/src/main/kotlin/org/jetbrains/plugins/feature/suggester/suggesters/SurroundWithSuggester.kt
@@ -1,14 +1,11 @@
 package org.jetbrains.plugins.feature.suggester.suggesters
 
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiErrorElement
+import com.intellij.psi.PsiFile
 import org.jetbrains.kotlin.psi.psiUtil.startOffset
 import org.jetbrains.plugins.feature.suggester.NoSuggestion
 import org.jetbrains.plugins.feature.suggester.Suggestion
-import org.jetbrains.plugins.feature.suggester.actions.BeforeChildReplacedAction
-import org.jetbrains.plugins.feature.suggester.actions.ChildAddedAction
-import org.jetbrains.plugins.feature.suggester.actions.ChildReplacedAction
-import org.jetbrains.plugins.feature.suggester.actions.PsiAction
+import org.jetbrains.plugins.feature.suggester.actions.*
 import org.jetbrains.plugins.feature.suggester.actionsLocalSummary
 import org.jetbrains.plugins.feature.suggester.createTipSuggestion
 import org.jetbrains.plugins.feature.suggester.history.UserActionsHistory
@@ -21,32 +18,54 @@ class SurroundWithSuggester : FeatureSuggester {
         const val POPUP_MESSAGE = "Why not to use Surround With action?"
         const val SUGGESTING_ACTION_ID = "SurroundWith"
         const val SUGGESTING_TIP_FILENAME = "neue-SurroundWith.html"
+        const val MAX_TIME_MILLIS_BETWEEN_ACTIONS: Long = 8000L
     }
 
-    private val actionsSummary = actionsLocalSummary()
-
-    private class SurroundingStatementData(var surroundingStatement: PsiElement) {
-        val startOffset: Int = surroundingStatement.startOffset
-        var expectedCloseBraceErrorElement: PsiErrorElement? = null
+    private object State {
+        var surroundingStatement: PsiElement? = null
+        var surroundingStatementStartOffset: Int = -1
         var firstStatementInBlockText: String = ""
-        var initialStatementsCount: Int = -1
+        var isLeftBraceAdded: Boolean = false
+        var isRightBraceAdded: Boolean = false
+        var lastChangeTimeMillis: Long = 0L
 
-        fun isValid(): Boolean {
-            return surroundingStatement.isValid
+        fun applySurroundingStatementAddition(statement: PsiElement, timeMillis: Long) {
+            reset()
+            surroundingStatement = statement
+            surroundingStatementStartOffset = statement.startOffset
+            lastChangeTimeMillis = timeMillis
+        }
+
+        fun applyBraceAddition(timeMillis: Long, braceType: String) {
+            lastChangeTimeMillis = timeMillis
+            if (braceType == "{") {
+                isLeftBraceAdded = true
+            } else {
+                isRightBraceAdded = true
+            }
+        }
+
+        fun isOutOfDate(newChangeTimeMillis: Long): Boolean {
+            return lastChangeTimeMillis != 0L && newChangeTimeMillis - lastChangeTimeMillis > MAX_TIME_MILLIS_BETWEEN_ACTIONS
+        }
+
+        fun reset() {
+            surroundingStatement = null
+            surroundingStatementStartOffset = -1
+            firstStatementInBlockText = ""
+            isLeftBraceAdded = false
+            isRightBraceAdded = false
+            lastChangeTimeMillis = 0L
         }
     }
 
+    private val actionsSummary = actionsLocalSummary()
     override lateinit var langSupport: LanguageSupport
-
-    private var surroundingStatementData: SurroundingStatementData? = null
 
     override fun getSuggestion(actions: UserActionsHistory): Suggestion {
         val action = actions.lastOrNull() ?: return NoSuggestion
-        if (surroundingStatementData != null
-            && action is PsiAction
-            && !surroundingStatementData!!.isValid()
-        ) {
-            updateStatementReference(action)
+        if (State.surroundingStatement?.isValid == false && action is PsiAction) {
+            State.tryToUpdateSurroundingStatement(action)
         }
         when (action) {
             is ChildReplacedAction -> {
@@ -54,40 +73,45 @@ class SurroundWithSuggester : FeatureSuggester {
                     || langSupport.isForStatement(action.newChild) && action.oldChild.text == "fo"
                     || langSupport.isWhileStatement(action.newChild) && action.oldChild.text == "whil"
                 ) {
-                    surroundingStatementData = SurroundingStatementData(action.newChild)
-                }
-            }
-            is BeforeChildReplacedAction -> {
-                if (surroundingStatementData == null) return NoSuggestion
-                with(action) {
-                    if (newChild is PsiErrorElement
-                        && newChild.isMissedCloseBrace()
-                    ) {
-                        surroundingStatementData!!.run {
-                            expectedCloseBraceErrorElement = newChild
-                            val statements = surroundingStatement.getStatements()
-                            initialStatementsCount = statements.size
-                            firstStatementInBlockText = statements.firstOrNull()?.text ?: ""
-                        }
-                    } else if (oldChild is PsiErrorElement
-                        && oldChild.isMissedCloseBrace()
-                        && surroundingStatementData!!.expectedCloseBraceErrorElement === oldChild
-                    ) {
-                        if (isStatementsSurrounded()) {
-                            surroundingStatementData = null
-                            return createTipSuggestion(
-                                createMessageWithShortcut(SUGGESTING_ACTION_ID, POPUP_MESSAGE),
-                                suggestingActionDisplayName,
-                                SUGGESTING_TIP_FILENAME
-                            )
-                        }
-                        surroundingStatementData = null
-                    }
+                    State.applySurroundingStatementAddition(action.newChild, action.timeMillis)
                 }
             }
             is ChildAddedAction -> {
                 if (action.newChild.isSurroundingStatement()) {
-                    surroundingStatementData = SurroundingStatementData(action.newChild)
+                    State.applySurroundingStatementAddition(action.newChild, action.timeMillis)
+                }
+            }
+            is ChildrenChangedAction -> {
+                if (State.surroundingStatement == null) return NoSuggestion
+                val textInsertedAction = actions.findLastTextInsertedAction() ?: return NoSuggestion
+                val text = textInsertedAction.text
+                if (text.contains("{") || text.contains("}")) {
+                    val psiFile = action.parent as? PsiFile ?: return NoSuggestion
+                    if (State.isOutOfDate(action.timeMillis) || text != "{" && text != "}") {
+                        State.reset()
+                    } else if (text == "{") {
+                        if (State.isLeftBraceAdded) {
+                            State.reset()
+                        } else if (State.isBraceAddedToStatement(psiFile, textInsertedAction.caretOffset)) {
+                            State.applyBraceAddition(action.timeMillis, "{")
+                            State.saveFirstStatementInBlock()
+                        }
+                    } else if (text == "}") {
+                        if (State.isLeftBraceAdded
+                            && State.isBraceAddedToStatement(psiFile, textInsertedAction.caretOffset)
+                        ) {
+                            State.applyBraceAddition(action.timeMillis, "}")
+                            if (State.isStatementsSurrounded()) {
+                                State.reset()
+                                return createTipSuggestion(
+                                    createMessageWithShortcut(SUGGESTING_ACTION_ID, POPUP_MESSAGE),
+                                    suggestingActionDisplayName,
+                                    SUGGESTING_TIP_FILENAME
+                                )
+                            }
+                        }
+                        State.reset()
+                    }
                 }
             }
             else -> NoSuggestion
@@ -103,24 +127,41 @@ class SurroundWithSuggester : FeatureSuggester {
         )
     }
 
-    private fun updateStatementReference(action: PsiAction) {
+    private fun State.tryToUpdateSurroundingStatement(action: PsiAction) {
         val psiFile = action.parent.containingFile ?: return
-        val element = psiFile.findElementAt(surroundingStatementData!!.startOffset) ?: return
+        val element = psiFile.findElementAt(surroundingStatementStartOffset) ?: return
         val parent = element.parent ?: return
         if (parent.isSurroundingStatement()) {
-            surroundingStatementData!!.surroundingStatement = parent
-        } else {
-            surroundingStatementData = null
+            surroundingStatement = parent
         }
     }
 
-    private fun isStatementsSurrounded(): Boolean {
-        if (surroundingStatementData == null) return false
-        with(surroundingStatementData!!) {
-            val statements = surroundingStatement.getStatements()
-            return statements.size in 1..initialStatementsCount
-                    && statements.first().text == firstStatementInBlockText
+    private fun UserActionsHistory.findLastTextInsertedAction(): EditorTextInsertedAction? {
+        return asIterable().findLast { it is EditorTextInsertedAction } as? EditorTextInsertedAction
+    }
+
+    private fun State.isBraceAddedToStatement(psiFile: PsiFile, offset: Int): Boolean {
+        val curElement = psiFile.findElementAt(offset) ?: return false
+        return curElement.parent === langSupport.getCodeBlock(surroundingStatement!!)
+    }
+
+    private fun State.saveFirstStatementInBlock() {
+        val statements = surroundingStatement!!.getStatements()
+        if (statements.isNotEmpty()) {
+            firstStatementInBlockText = statements.first().text
         }
+    }
+
+    private fun State.isStatementsSurrounded(): Boolean {
+        if (surroundingStatement?.isValid == false
+            || !isLeftBraceAdded
+            || !isRightBraceAdded
+        ) {
+            return false
+        }
+        val statements = surroundingStatement!!.getStatements()
+        return statements.isNotEmpty()
+                && statements.first().text == firstStatementInBlockText
     }
 
     private fun PsiElement.getStatements(): List<PsiElement> {
@@ -130,11 +171,6 @@ class SurroundWithSuggester : FeatureSuggester {
         } else {
             emptyList()
         }
-    }
-
-    private fun PsiErrorElement.isMissedCloseBrace(): Boolean {
-        return errorDescription == """'}' expected"""
-                || errorDescription == """Missing '}"""
     }
 
     private fun PsiElement.isSurroundingStatement(): Boolean {
