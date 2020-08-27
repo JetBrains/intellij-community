5c2cd4f45dd8532086c3825d5dfff104444579d0
Roman Ivanov <roman.ivanov@jetbrains.com>
2020-08-27 11:31:53 +0200

    RecordAugmentProvider: make it work only in smart mode (resolve required)
    GitOrigin-RevId: 7d0edef29b45464fce513090e6c8f394106ca368

--- java/java-psi-impl/src/com/intellij/psi/impl/RecordAugmentProvider.java
+++ java/java-psi-impl/src/com/intellij/psi/impl/RecordAugmentProvider.java
@@ -1,7 +1,6 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.impl;
 
-import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.*;
 import com.intellij.psi.augment.PsiAugmentProvider;
@@ -19,7 +18,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class RecordAugmentProvider extends PsiAugmentProvider implements DumbAware {
+public class RecordAugmentProvider extends PsiAugmentProvider {
   @Override
   protected @NotNull <Psi extends PsiElement> List<Psi> getAugments(@NotNull PsiElement element,
                                                                     @NotNull Class<Psi> type,
