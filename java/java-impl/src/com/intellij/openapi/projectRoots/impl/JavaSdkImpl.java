659a8943954a1b277377f00ba69bd843bf16b723
Anna Kozlova <anna.kozlova@jetbrains.com>
2020-08-28 20:32:02 +0200

    java sdk: remove not yet existing in 202 @NlsSafe
    GitOrigin-RevId: abdff5f1f3563fe5e013492173ef185521f339dd

--- java/java-impl/src/com/intellij/openapi/projectRoots/impl/JavaSdkImpl.java
+++ java/java-impl/src/com/intellij/openapi/projectRoots/impl/JavaSdkImpl.java
@@ -21,7 +21,6 @@ import com.intellij.openapi.roots.JavadocOrderRootType;
 import com.intellij.openapi.roots.OrderRootType;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.Comparing;
-import com.intellij.openapi.util.NlsSafe;
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.text.StringUtil;
@@ -333,7 +332,7 @@ public final class JavaSdkImpl extends JavaSdk {
     return false;
   }
 
-  private static void reportCorruptedJdkAnnotations(@NotNull VirtualFile root, @NotNull @NlsSafe String reason) {
+  private static void reportCorruptedJdkAnnotations(@NotNull VirtualFile root, @NotNull String reason) {
     LOG.warn("Internal jdk annotation root " + root + " seems corrupted: " + reason);
   }
 
