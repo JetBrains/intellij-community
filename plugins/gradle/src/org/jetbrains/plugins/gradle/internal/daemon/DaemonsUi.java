e8e652b526b975a749efdf969f84182ce4f45380
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2020-08-28 12:56:56 +0300

    IDEA-247653 - maven i18n IDEA-247670 - gradle i18n Fixes for unused properties
    GitOrigin-RevId: ddc793e5684c8745a76525264079e33b64ae765a

--- plugins/gradle/src/org/jetbrains/plugins/gradle/internal/daemon/DaemonsUi.java
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/internal/daemon/DaemonsUi.java
@@ -138,7 +138,7 @@ public class DaemonsUi implements Disposable {
   private static class DaemonsTable extends ListTableWithButtons<DaemonState> {
     @Override
     protected ListTableModel createListModel() {
-      final ColumnInfo pidColumn = new DaemonsTable.TableColumn(GradleBundle.message("column.name.daemon.status"), 80) {
+      final ColumnInfo pidColumn = new DaemonsTable.TableColumn(GradleBundle.message("column.name.daemon.PID"), 80) {
         @Nullable
         @Override
         public String valueOf(DaemonState daemonState) {
