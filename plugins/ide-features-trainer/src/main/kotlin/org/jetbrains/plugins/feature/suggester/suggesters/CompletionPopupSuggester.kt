404b963d2f41298e0e433e6205c1168bae1c3065
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2020-08-31 15:26:15 +0300

    Refactor CompletionPopupSuggester
    IJ-CR-14522
    
    GitOrigin-RevId: aa3dfc4dee63c5782e7cf6bea4bed336541de6fc

--- plugins/ide-features-trainer/src/main/kotlin/org/jetbrains/plugins/feature/suggester/suggesters/CompletionPopupSuggester.kt
+++ plugins/ide-features-trainer/src/main/kotlin/org/jetbrains/plugins/feature/suggester/suggesters/CompletionPopupSuggester.kt
@@ -1,8 +1,12 @@
 package org.jetbrains.plugins.feature.suggester.suggesters
 
 import com.intellij.psi.PsiComment
+import com.intellij.psi.PsiFile
 import org.jetbrains.plugins.feature.suggester.*
-import org.jetbrains.plugins.feature.suggester.actions.*
+import org.jetbrains.plugins.feature.suggester.actions.BeforeCompletionChooseItemAction
+import org.jetbrains.plugins.feature.suggester.actions.BeforeEditorTextInsertedAction
+import org.jetbrains.plugins.feature.suggester.actions.BeforeEditorTextRemovedAction
+import org.jetbrains.plugins.feature.suggester.actions.EditorEscapeAction
 import org.jetbrains.plugins.feature.suggester.history.UserActionsHistory
 import org.jetbrains.plugins.feature.suggester.suggesters.FeatureSuggester.Companion.createMessageWithShortcut
 import org.jetbrains.plugins.feature.suggester.suggesters.lang.LanguageSupport
@@ -13,49 +17,85 @@ class CompletionPopupSuggester : FeatureSuggester {
         const val POPUP_MESSAGE = "You may use shortcut to call completion popup."
         const val SUGGESTING_ACTION_ID = "CodeCompletion"
         const val SUGGESTING_TIP_FILENAME = "neue-CodeCompletion.html"
+        const val MAX_TIME_MILLIS_BETWEEN_ACTIONS: Long = 5000L
     }
 
-    private data class EditedStatementData(val dotOffset: Int) {
-        var completionStarted: Boolean = false
+    private object State {
+        var dotOffset: Int = -1
+            private set
+        var isCompletionStarted: Boolean = false
+            private set
+        var lastChangeTimeMillis: Long = 0L
+            private set
+
+        val isDotRemoved: Boolean
+            get() = dotOffset != -1
+
+        fun applyDotRemoving(offset: Int, timeMillis: Long) {
+            dotOffset = offset
+            lastChangeTimeMillis = timeMillis
+        }
+
+        fun applyCompletionStarting(timeMillis: Long) {
+            isCompletionStarted = true
+            lastChangeTimeMillis = timeMillis
+        }
 
         fun isAroundDot(offset: Int): Boolean {
             return offset in dotOffset..(dotOffset + 7)
+        }
+
+        fun isOutOfDate(newChangeTimeMillis: Long): Boolean {
+            return newChangeTimeMillis - lastChangeTimeMillis > MAX_TIME_MILLIS_BETWEEN_ACTIONS
+        }
+
+        fun reset() {
+            dotOffset = -1
+            isCompletionStarted = false
+            lastChangeTimeMillis = 0L
         }
     }
 
     private val actionsSummary = actionsLocalSummary()
     override lateinit var langSupport: LanguageSupport
 
-    private var editedStatementData: EditedStatementData? = null
-
     override fun getSuggestion(actions: UserActionsHistory): Suggestion {
         when (val action = actions.lastOrNull()) {
             is BeforeEditorTextRemovedAction -> {
                 if (action.textFragment.text == ".") {
-                    editedStatementData = createEditedStatementData(action, action.caretOffset)
+                    State.reset()
+                    val psiFile = action.psiFile ?: return NoSuggestion
+                    if (!isInsideCommentOrLiteral(psiFile, action.caretOffset)) {
+                        State.applyDotRemoving(action.caretOffset, action.timeMillis)
+                    }
                 }
             }
             is BeforeEditorTextInsertedAction -> {
-                if (editedStatementData != null &&
-                    action.text == "."
-                    && action.caretOffset == editedStatementData!!.dotOffset
-                ) {
-                    editedStatementData!!.completionStarted = true
+                if (action.text == ".") {
+                    if (State.isDotRemoved
+                        && action.caretOffset == State.dotOffset
+                        && !State.isOutOfDate(action.timeMillis)
+                    ) {
+                        State.applyCompletionStarting(action.timeMillis)
+                    } else {
+                        State.reset()
+                    }
                 }
             }
             is BeforeCompletionChooseItemAction -> {
-                if (editedStatementData?.isAroundDot(action.caretOffset) == true) {
-                    editedStatementData = null
-                    return createTipSuggestion(
-                        createMessageWithShortcut(SUGGESTING_ACTION_ID, POPUP_MESSAGE),
-                        suggestingActionDisplayName,
-                        SUGGESTING_TIP_FILENAME
-                    )
+                try {
+                    if (State.isCompletionStarted
+                        && State.isAroundDot(action.caretOffset)
+                        && !State.isOutOfDate(action.timeMillis)
+                    ) {
+                        return createSuggestion()
+                    }
+                } finally {
+                    State.reset()
                 }
-                editedStatementData = null
             }
             is EditorEscapeAction -> {
-                editedStatementData = null
+                State.reset()
             }
         }
 
@@ -71,16 +111,18 @@ class CompletionPopupSuggester : FeatureSuggester {
         )
     }
 
-    @Suppress("DuplicatedCode")
-    private fun createEditedStatementData(action: EditorAction, offset: Int): EditedStatementData? {
-        val curElement = action.psiFile?.findElementAt(offset) ?: return null
-        return if (curElement.getParentByPredicate(langSupport::isLiteralExpression) == null
-            && curElement.getParentOfType<PsiComment>() == null
-        ) {
-            EditedStatementData(offset)
-        } else {
-            null
-        }
+    private fun isInsideCommentOrLiteral(psiFile: PsiFile, offset: Int): Boolean {
+        val curElement = psiFile.findElementAt(offset) ?: return false
+        return curElement.getParentByPredicate(langSupport::isLiteralExpression) != null
+                || curElement.getParentOfType<PsiComment>() != null
+    }
+
+    private fun createSuggestion(): Suggestion {
+        return createTipSuggestion(
+            createMessageWithShortcut(SUGGESTING_ACTION_ID, POPUP_MESSAGE),
+            suggestingActionDisplayName,
+            SUGGESTING_TIP_FILENAME
+        )
     }
 
     override val id: String = "Completion"
