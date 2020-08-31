f8b3d071f1037fa304eb00416dde857e55fc53e3
Dmitry Jemerov <yole@jetbrains.com>
2020-08-31 18:58:04 +0200

    Specify possible values for updateChannelType (IDEA-CR-66444)
    GitOrigin-RevId: e6b5c0cc6cfb3cdcf4b5d019d500af7af1d13baf

--- platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/UpdateOptions.kt
+++ platform/platform-impl/src/com/intellij/openapi/updateSettings/impl/UpdateOptions.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.updateSettings.impl
 
 import com.intellij.openapi.components.BaseState
@@ -44,7 +44,7 @@ class UpdateOptions : BaseState() {
   var lastBuildChecked by string()
 
   @get:OptionTag("UPDATE_CHANNEL_TYPE")
-  @get:ReportValue
+  @get:ReportValue(possibleValues = ["eap", "milestone", "beta", "release"])
   var updateChannelType by string(ChannelStatus.RELEASE.code)
 
   @get:OptionTag("THIRD_PARTY_PLUGINS_ALLOWED")
