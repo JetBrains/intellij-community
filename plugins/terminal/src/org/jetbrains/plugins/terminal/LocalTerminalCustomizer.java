bf8d32e357c850f64e9fd1356fb085c2ad63479c
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2020-08-31 14:21:28 +0300

    Added javadocs and annotations to the LocalTerminalCustomizer
    GitOrigin-RevId: e946a654b4bf9a8f02d3e9f1785c19499efa4b2e

--- plugins/terminal/src/org/jetbrains/plugins/terminal/LocalTerminalCustomizer.java
+++ plugins/terminal/src/org/jetbrains/plugins/terminal/LocalTerminalCustomizer.java
@@ -1,42 +1,45 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.terminal;
 
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.options.UnnamedConfigurable;
 import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 
 public abstract class LocalTerminalCustomizer {
-  public static final ExtensionPointName<LocalTerminalCustomizer> EP_NAME = ExtensionPointName.create("org.jetbrains.plugins.terminal.localTerminalCustomizer");
+  public static final ExtensionPointName<LocalTerminalCustomizer> EP_NAME =
+    ExtensionPointName.create("org.jetbrains.plugins.terminal.localTerminalCustomizer");
 
-  public String[] customizeCommandAndEnvironment(Project project, String[] command, Map<String, String> envs) {
+  /**
+   * May alter the command to be run in terminal and/or adjust starting environment
+   *
+   * @param command original command to run
+   * @param envs    mutable map of environment variables
+   * @return new command to run. Original {@code command} should be returned if no alterations performed
+   * @apiNote terminal starting shell session with user-specified shell. Under the hood we are running shell with custom rcfile, e.g.:
+   * {@code /usr/bin/bash --rcfile PATH_TO/community/plugins/terminal/resources/jediterm-bash.in}. See the {@code jediterm-bash.in} script
+   * for more information on how to alter the execution process.
+   */
+  public @NotNull String[] customizeCommandAndEnvironment(@NotNull Project project,
+                                                          @NotNull String[] command,
+                                                          @NotNull Map<String, String> envs) {
     return command;
   }
 
-  @Nullable
-  public UnnamedConfigurable getConfigurable(Project project) {
+  /**
+   * @return configurable for customizer-specific options
+   */
+  public @Nullable UnnamedConfigurable getConfigurable(@NotNull Project project) {
     return null;
   }
 
-  @Nullable
-  protected String getDefaultFolder(Project project) {
+  /**
+   * @return path to the directory to run the terminal in or null if default directory should be used
+   */
+  protected @Nullable String getDefaultFolder(@NotNull Project project) {
     return null;
   }
-
 }
