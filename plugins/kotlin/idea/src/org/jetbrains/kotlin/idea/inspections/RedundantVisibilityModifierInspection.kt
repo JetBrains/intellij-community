981d27937b7f1704fbfb975fac2a86e0fc60462a
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-09-01 16:06:06 +0300

    Rename Visibility to DescriptorVisibility
    Original commit: d1fd1da56f222741c1626d4520128b2e30efc83f
    
    GitOrigin-RevId: 84ea80be6213e8bc008f454dc44112a2754bb8e9

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/inspections/RedundantVisibilityModifierInspection.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/inspections/RedundantVisibilityModifierInspection.kt
@@ -8,6 +8,10 @@ package org.jetbrains.kotlin.idea.inspections
 import com.intellij.codeInspection.*
 import com.intellij.psi.PsiElementVisitor
 import org.jetbrains.kotlin.config.AnalysisFlags
+import org.jetbrains.kotlin.descriptors.CallableMemberDescriptor
+import org.jetbrains.kotlin.descriptors.PropertyDescriptor
+import org.jetbrains.kotlin.descriptors.DescriptorVisibilities
+import org.jetbrains.kotlin.descriptors.DescriptorVisibility
 import org.jetbrains.kotlin.config.ExplicitApiMode
 import org.jetbrains.kotlin.descriptors.*
 import org.jetbrains.kotlin.idea.KotlinBundle
@@ -52,7 +56,7 @@ class RedundantVisibilityModifierInspection : AbstractKotlinInspection(), Cleanu
                 && declaration is KtProperty
                 && declaration.hasModifier(KtTokens.OVERRIDE_KEYWORD)
                 && declaration.isVar
-                && declaration.setterVisibility().let { it != null && it != Visibilities.PUBLIC }
+                && declaration.setterVisibility().let { it != null && it != DescriptorVisibilities.PUBLIC }
             ) return
 
             holder.registerProblem(
@@ -67,7 +71,7 @@ class RedundantVisibilityModifierInspection : AbstractKotlinInspection(), Cleanu
         })
     }
 
-    private fun KtProperty.setterVisibility(): Visibility? {
+    private fun KtProperty.setterVisibility(): DescriptorVisibility? {
         val descriptor = descriptor as? PropertyDescriptor ?: return null
         if (setter?.visibilityModifier() != null) {
             val visibility = descriptor.setter?.visibility
