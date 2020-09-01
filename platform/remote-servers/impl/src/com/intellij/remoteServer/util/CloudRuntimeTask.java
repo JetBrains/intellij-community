5ec80cc50b3b6526a23ac9f7765efef51251ff1c
Michael Golubev <michael.golubev@jetbrains.com>
2020-09-01 18:19:00 +0200

    IDEA-247683 - Clouds i18n : @NlsContexts.DialogTitle
    GitOrigin-RevId: e78a42d702f829f9906065a36d91dfab7e69b888

--- platform/remote-servers/impl/src/com/intellij/remoteServer/util/CloudRuntimeTask.java
+++ platform/remote-servers/impl/src/com/intellij/remoteServer/util/CloudRuntimeTask.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.remoteServer.util;
 
 import com.intellij.openapi.Disposable;
@@ -23,6 +9,7 @@ import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.util.NlsContexts;
 import com.intellij.remoteServer.configuration.deployment.DeploymentConfiguration;
 import com.intellij.util.concurrency.Semaphore;
 import com.intellij.util.ui.UIUtil;
@@ -44,7 +31,7 @@ public abstract class CloudRuntimeTask<
   private final AtomicReference<Boolean> mySuccess = new AtomicReference<>();
   private final AtomicReference<String> myErrorMessage = new AtomicReference<>();
 
-  public CloudRuntimeTask(Project project, String title) {
+  public CloudRuntimeTask(Project project, @NlsContexts.DialogTitle String title) {
     myProject = project;
     myTitle = title;
   }
