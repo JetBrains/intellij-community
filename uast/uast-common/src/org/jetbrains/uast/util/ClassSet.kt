aac2eac7616fbf21bff5d5a798387985948806b8
Alexander Bashkirov <alexander.bashkirov@jetbrains.com>
2020-09-01 22:59:02 +0300

    IJ-CR-2883 [uast] Removed useless optimization
    GitOrigin-RevId: a427f6482cac3e7e7e10be9f9e81739b8322e0a2

--- uast/uast-common/src/org/jetbrains/uast/util/ClassSet.kt
+++ uast/uast-common/src/org/jetbrains/uast/util/ClassSet.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.uast.util
 
-import com.intellij.util.SofterReference
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.ConcurrentMap
 
@@ -15,20 +14,15 @@ class ClassSetImpl<out T>(vararg val initialClasses: Class<out T>) : ClassSet<T>
 
   private val isSimple = initialClasses.size <= SIMPLE_CLASS_SET_LIMIT
 
-  private lateinit var _internalMapping: SofterReference<ConcurrentMap<Class<out T>, Boolean>>
-  private inline val internalMapping: ConcurrentMap<Class<out T>, Boolean>
-    get() = _internalMapping.get() ?: initMappings()
+  private lateinit var internalMapping: ConcurrentMap<Class<out T>, Boolean>
 
   init {
-    if (!isSimple) initMappings()
+    if (!isSimple)
+      internalMapping = ConcurrentHashMap<Class<out T>, Boolean>().apply {
+        for (initialClass in initialClasses)
+          this[initialClass] = true
+      }
   }
-
-  private fun initMappings() =
-    ConcurrentHashMap<Class<out T>, Boolean>().apply {
-      for (initialClass in initialClasses)
-        this[initialClass] = true
-      _internalMapping = SofterReference(this)
-    }
 
   override fun isEmpty(): Boolean = initialClasses.isEmpty()
 
