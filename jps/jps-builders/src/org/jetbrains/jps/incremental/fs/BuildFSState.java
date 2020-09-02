6be0ef0ef7d48e9c1537c5cd3aa880b6af4af1a9
Eugene Zhuravlev <jeka@intellij.com>
2020-09-02 14:38:03 +0200

    do not notify unprocessed change if file stamp is in future
    GitOrigin-RevId: 80e75663227a041d29e46d8645db507553681adb

--- jps/jps-builders/src/org/jetbrains/jps/incremental/fs/BuildFSState.java
+++ jps/jps-builders/src/org/jetbrains/jps/incremental/fs/BuildFSState.java
@@ -125,24 +125,28 @@ public final class BuildFSState {
     final BuildRootIndex rootIndex = context.getProjectDescriptor().getBuildRootIndex();
     try {
       delta.lockData();
+      final long now = System.currentTimeMillis();
       for (Set<File> files : delta.getSourcesToRecompile().values()) {
         files_loop:
         for (File file : files) {
-          if ((getEventRegistrationStamp(file) > targetBuildStart || FSOperations.lastModified(file) > targetBuildStart) && scope.isAffected(target, file)) {
-            for (BuildRootDescriptor rd : rootIndex.findAllParentDescriptors(file, context)) {
-              if (rd.isGenerated()) { // do not send notification for generated sources
-                continue files_loop;
+          final long fileStamp;
+          if (getEventRegistrationStamp(file) > targetBuildStart || (fileStamp = FSOperations.lastModified(file)) > targetBuildStart && fileStamp < now) {
+            if (scope.isAffected(target, file)) {
+              for (BuildRootDescriptor rd : rootIndex.findAllParentDescriptors(file, context)) {
+                if (rd.isGenerated()) { // do not send notification for generated sources
+                  continue files_loop;
+                }
               }
+              if (LOG.isDebugEnabled()) {
+                LOG.debug("Unprocessed changes detected for target " + target +
+                            "; file: " + file.getPath() +
+                            "; targetBuildStart=" + targetBuildStart +
+                            "; eventRegistrationStamp=" + getEventRegistrationStamp(file) +
+                            "; lastModified=" + FSOperations.lastModified(file)
+                );
+              }
+              return true;
             }
-            if (LOG.isDebugEnabled()) {
-              LOG.debug("Unprocessed changes detected for target " + target +
-                        "; file: " + file.getPath() +
-                        "; targetBuildStart=" + targetBuildStart +
-                        "; eventRegistrationStamp=" + getEventRegistrationStamp(file) +
-                        "; lastModified=" + FSOperations.lastModified(file)
-              );
-            }
-            return true;
           }
         }
       }
