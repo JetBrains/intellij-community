3c75a9545eeb9b4700f620436ddf024a99d248bf
Svetlana.Zemlyanskaya <Svetlana.Zemlyanskaya@jetbrains.com>
2020-09-03 14:22:21 +0200

    LAB-29: remove usage of deprecated method
    GitOrigin-RevId: 7ba1cd36049c840ffd9c5b430086567ef5aded18

--- plugins/filePrediction/src/com/intellij/filePrediction/FilePredictionNotifications.kt
+++ plugins/filePrediction/src/com/intellij/filePrediction/FilePredictionNotifications.kt
@@ -1,14 +1,13 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.filePrediction
 
-import com.intellij.notification.NotificationGroup
+import com.intellij.notification.NotificationGroupManager
 import com.intellij.notification.NotificationType
 import com.intellij.openapi.project.Project
 import org.jetbrains.annotations.Nls
 
 internal object FilePredictionNotifications {
-  private val NOTIFICATION_GROUP =
-    NotificationGroup.balloonGroup("NextFilePrediction", FilePredictionBundle.message("file.prediction.notification.group"))
+  private val NOTIFICATION_GROUP = NotificationGroupManager.getInstance().getNotificationGroup("NextFilePrediction")
 
   fun showWarning(project: Project?, @Nls message: String) {
     showNotification(project, NotificationType.WARNING, message)
