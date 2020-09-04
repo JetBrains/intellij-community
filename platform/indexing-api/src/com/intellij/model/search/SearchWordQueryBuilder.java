17ae77abed357fc61c440e653c97bad24169f687
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2020-09-04 14:05:01 +0300

    add SearchWordQueryBuilder#includeInjections
    GitOrigin-RevId: 68a1a764f28c38e4155d46a4dadf9b4fea696b9f

--- platform/indexing-api/src/com/intellij/model/search/SearchWordQueryBuilder.java
+++ platform/indexing-api/src/com/intellij/model/search/SearchWordQueryBuilder.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.model.search;
 
 import com.intellij.lang.Language;
@@ -73,6 +73,13 @@ public interface SearchWordQueryBuilder {
   @Contract(value = "_ -> new", pure = true)
   @NotNull
   SearchWordQueryBuilder inFilesWithLanguageOfKind(@NotNull Language language);
+
+  /**
+   * Orders to include occurrences in language injections of any language as well as occurrences in host files.
+   */
+  @Contract(value = "-> new", pure = true)
+  @NotNull
+  SearchWordQueryBuilder includeInjections();
 
   /**
    * Orders to search occurrences in language injections.
