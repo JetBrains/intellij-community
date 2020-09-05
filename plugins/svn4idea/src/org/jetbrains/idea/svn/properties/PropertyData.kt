2d5db1015f3f6d8eeb166f6fcf0b45df09a3adb3
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2020-09-05 23:20:02 +0300

    svn: Convert "PropertyData", "PropertyValue", "PropertiesMap" to kotlin
    GitOrigin-RevId: 4eb369ba933f6726360b1f8e28189995459a147d

--- plugins/svn4idea/src/org/jetbrains/idea/svn/properties/PropertyData.kt
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/properties/PropertyData.kt
@@ -1,42 +1,27 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.idea.svn.properties
+
+import org.jetbrains.annotations.Contract
+
+// TODO: Actually value is always not null for command line integration. But it is not clear enough if not null property value is always
+// TODO: provided by SVNKit. So currently value is @Nullable.
+class PropertyData(val name: String, val value: PropertyValue?)
+
+class PropertiesMap : HashMap<String, PropertyValue?>()
+
+/**
+ * TODO: Add correct support of binary properties - support in api, diff, etc.
  */
-package org.jetbrains.idea.svn.properties;
+data class PropertyValue(private val value: String) {
+  override fun toString(): String = value
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+  companion object {
+    @JvmStatic
+    @Contract(value = "null -> null; !null -> !null", pure = true)
+    fun create(propertyValue: String?): PropertyValue? = propertyValue?.let { PropertyValue(it) }
 
-public class PropertyData {
-
-  @Nullable private final PropertyValue myValue;
-  @NotNull private final String myName;
-
-  // TODO: Actually value is always not null for command line integration. But it is not clear enough if not null property value is always
-  // TODO: provided by SVNKit. So currently value is @Nullable.
-  public PropertyData(@NotNull String name, @Nullable PropertyValue value) {
-    myName = name;
-    myValue = value;
-  }
-
-  @NotNull
-  public String getName() {
-    return myName;
-  }
-
-  @Nullable
-  public PropertyValue getValue() {
-    return myValue;
+    @JvmStatic
+    @Contract(value = "null -> null; !null -> !null", pure = true)
+    fun toString(value: PropertyValue?): String? = value?.toString()
   }
 }
