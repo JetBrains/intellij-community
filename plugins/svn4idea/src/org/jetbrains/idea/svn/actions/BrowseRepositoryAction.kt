64c4442b706d1e1293314954f8e490a9f63527bc
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2020-09-05 19:57:56 +0300

    svn: Refactor "BrowseRepositoryAction" - remove deprecated api usages, cleanup
    GitOrigin-RevId: 8a6d6379c9811fa56bbe4f44ad896f6a042aa8de

--- plugins/svn4idea/src/org/jetbrains/idea/svn/actions/BrowseRepositoryAction.kt
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/actions/BrowseRepositoryAction.kt
@@ -2,56 +2,53 @@
 package org.jetbrains.idea.svn.actions
 
 import com.intellij.openapi.Disposable
-import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManager
-import com.intellij.openapi.util.Disposer
-import com.intellij.openapi.wm.ToolWindowAnchor
+import com.intellij.openapi.wm.RegisterToolWindowTask
+import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowManager
-import com.intellij.ui.content.ContentFactory
+import com.intellij.util.ui.components.BorderLayoutPanel
 import org.jetbrains.idea.svn.dialogs.RepositoryBrowserDialog
-import java.awt.BorderLayout
-import javax.swing.JPanel
 
-private const val REPOSITORY_BROWSER_TOOLWINDOW = "SVN Repositories"
+private const val REPOSITORY_BROWSER_TOOLWINDOW_ID = "SVN Repositories"
 
-class BrowseRepositoryAction : AnAction(), DumbAware {
+private fun getRepositoryBrowserToolWindow(project: Project): ToolWindow? =
+  ToolWindowManager.getInstance(project).getToolWindow(REPOSITORY_BROWSER_TOOLWINDOW_ID)
+
+class BrowseRepositoryAction : DumbAwareAction() {
   override fun actionPerformed(e: AnActionEvent) {
-    val project = e.getData(CommonDataKeys.PROJECT)
+    val project = e.project
+
     if (project == null) {
-      val dialog = RepositoryBrowserDialog(ProjectManager.getInstance().defaultProject)
-      dialog.show()
+      RepositoryBrowserDialog(ProjectManager.getInstance().defaultProject).show()
     }
     else {
-      val manager = ToolWindowManager.getInstance(project)
-      var w = manager.getToolWindow(REPOSITORY_BROWSER_TOOLWINDOW)
-      if (w == null) {
-        val component = RepositoryToolWindowPanel(project)
-        w = manager.registerToolWindow(REPOSITORY_BROWSER_TOOLWINDOW, true, ToolWindowAnchor.BOTTOM, project, true)
-        w.helpId = "reference.svn.repository"
-        val content = ContentFactory.SERVICE.getInstance().createContent(component, "", false)
-        Disposer.register(content, component)
-        w.contentManager.addContent(content)
-      }
-      w.show(null)
-      w.activate(null)
+      val toolWindow = getRepositoryBrowserToolWindow(project) ?: registerRepositoryBrowserToolWindow(project)
+      toolWindow.activate(null)
     }
   }
+
+  private fun registerRepositoryBrowserToolWindow(project: Project): ToolWindow =
+    ToolWindowManager.getInstance(project).registerToolWindow(RegisterToolWindowTask(
+      id = REPOSITORY_BROWSER_TOOLWINDOW_ID,
+      component = RepositoryToolWindowPanel(project)
+    )).apply {
+      helpId = "reference.svn.repository"
+    }
 }
 
-private class RepositoryToolWindowPanel(private val project: Project) : JPanel(BorderLayout()), Disposable {
+private class RepositoryToolWindowPanel(private val project: Project) : BorderLayoutPanel(), Disposable {
   private val dialog = RepositoryBrowserDialog(project)
 
   init {
-    add(dialog.createBrowserComponent(true), BorderLayout.CENTER)
-    add(dialog.createToolbar(false), BorderLayout.WEST)
+    addToCenter(dialog.createBrowserComponent(true))
+    addToLeft(dialog.createToolbar(false))
   }
 
   override fun dispose() {
     dialog.disposeRepositoryBrowser()
-    ToolWindowManager.getInstance(project).unregisterToolWindow(REPOSITORY_BROWSER_TOOLWINDOW)
+    getRepositoryBrowserToolWindow(project)?.remove()
   }
 }
