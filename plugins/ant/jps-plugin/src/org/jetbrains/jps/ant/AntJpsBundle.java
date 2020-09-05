d64152af7764b6937f699b4fd1876c4b0e8f9b02
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2020-09-05 11:01:47 +0700

    i18n: intellij.ant.jps (IDEA-247698)
    GitOrigin-RevId: 50043b9f74f76afeb18926d9fb69efce3a5d6362

---
+++ plugins/ant/jps-plugin/src/org/jetbrains/jps/ant/AntJpsBundle.java
@@ -0,0 +1,22 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.jps.ant;
+
+import com.intellij.DynamicBundle;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.PropertyKey;
+
+public final class AntJpsBundle extends DynamicBundle {
+  @NonNls private static final String BUNDLE = "messages.AntJpsBundle";
+  private static final AntJpsBundle INSTANCE = new AntJpsBundle();
+
+  private AntJpsBundle() {
+    super(BUNDLE);
+  }
+
+  @NotNull
+  public static @Nls String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, Object @NotNull ... params) {
+    return INSTANCE.getMessage(key, params);
+  }
+}
