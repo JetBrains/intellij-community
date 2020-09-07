121f3985696f1b5d9ce65dc93ea41089b31f5285
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2020-09-07 19:11:21 +0300

    [groovy] IDEA-235059: Refactor AffectedMembersCache
    GitOrigin-RevId: f8845d736806613a543fda95c9a438b7d57fc8b3

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/resolve/ast/GrGeneratedConstructorUtils.kt
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/resolve/ast/GrGeneratedConstructorUtils.kt
@@ -14,14 +14,14 @@ import org.jetbrains.plugins.groovy.lang.psi.api.auxiliary.modifiers.GrModifier
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrField
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.GrTypeDefinition
 import org.jetbrains.plugins.groovy.lang.psi.impl.GrAnnotationUtil
+import org.jetbrains.plugins.groovy.lang.psi.impl.GrAnnotationUtil.*
 import org.jetbrains.plugins.groovy.lang.psi.impl.getArrayValue
-import org.jetbrains.plugins.groovy.lang.psi.util.GroovyCommonClassNames
+import org.jetbrains.plugins.groovy.lang.psi.util.GroovyCommonClassNames.*
 import java.util.*
-import kotlin.collections.ArrayList
 
 val constructorGeneratingAnnotations: List<String> = listOf(
-  GroovyCommonClassNames.GROOVY_TRANSFORM_TUPLE_CONSTRUCTOR,
-  GroovyCommonClassNames.GROOVY_TRANSFORM_MAP_CONSTRUCTOR)
+  GROOVY_TRANSFORM_TUPLE_CONSTRUCTOR,
+  GROOVY_TRANSFORM_MAP_CONSTRUCTOR)
 
 object TupleConstructorAttributes {
   @NlsSafe const val EXCLUDES = "excludes"
@@ -41,7 +41,7 @@ object TupleConstructorAttributes {
 
 fun getIdentifierList(annotation: PsiAnnotation, @NlsSafe attributeName: String): List<String>? {
   annotation.takeIf { it.hasAttribute(attributeName) } ?: return null
-  val rawIdentifiers = GrAnnotationUtil.inferStringAttribute(annotation, attributeName)
+  val rawIdentifiers = inferStringAttribute(annotation, attributeName)
   return rawIdentifiers?.split(',')?.mapNotNull { it.trim().takeUnless(CharSequence::isBlank) }?.toList()
          ?: inferStringArrayValueShallow(annotation, attributeName)
 }
@@ -50,154 +50,23 @@ fun getIdentifierList(annotation: PsiAnnotation, @NlsSafe attributeName: String)
  * This function prevents evaluation of constant expressions inside attributes,
  * because this evaluation may trigger a recursion within the `TransformationContext`
  */
-private fun inferStringArrayValueShallow(anno: PsiAnnotation, attributeName: String) : List<String>? =
+private fun inferStringArrayValueShallow(anno: PsiAnnotation, attributeName: String): List<String>? =
   anno.findAttributeValue(attributeName)?.getArrayValue(GrAnnotationUtil::getString) ?: emptyList()
 
-fun getAffectedMembersCache(annotation: PsiAnnotation) : AffectedMembersCache = CachedValuesManager.getCachedValue(annotation) {
-  CachedValueProvider.Result(AffectedMembersCache(annotation), annotation)
-}
 
 /**
  * For specific annotation (@MapConstructor/@TupleConstructor) this class computes all actually affected members
  */
-class AffectedMembersCache internal constructor(anno: PsiAnnotation) {
-  private val order: List<PsiNamedElement>
-  private val hasPropertyOptions: Boolean
-  private val referencedFromExcludes: List<PsiNamedElement>
-
-  private class FilteringList<T>(private val predicate: (T) -> Boolean) : ArrayList<T>() {
-    override fun add(element: T): Boolean {
-      if (!predicate(element)) return false
-      return super.add(element)
-    }
-  }
-
-  init {
-    val owner = (anno.owner as? PsiElement)
-    val containingClass = owner?.parentOfType<GrTypeDefinition>()?.takeIf { it.modifierList === owner }
-    if (containingClass != null) {
-      val (nameFilter, excludesIdentifiers, ordering) = collectNamesOrderInformation(anno)
-      val acceptFields = GrAnnotationUtil.inferBooleanAttribute(anno, TupleConstructorAttributes.INCLUDE_FIELDS) ?: true
-      val acceptProperties = GrAnnotationUtil.inferBooleanAttribute(anno, TupleConstructorAttributes.INCLUDE_PROPERTIES) ?: true
-      val includeBeans = GrAnnotationUtil.inferBooleanAttribute(anno, TupleConstructorAttributes.ALL_PROPERTIES) ?: false
-      val acceptSuperProperties = GrAnnotationUtil.inferBooleanAttribute(anno, TupleConstructorAttributes.INCLUDE_SUPER_PROPERTIES) ?: false
-      val acceptSuperFields = GrAnnotationUtil.inferBooleanAttribute(anno, TupleConstructorAttributes.INCLUDE_SUPER_FIELDS) ?: false
-      val includeStatic = GrAnnotationUtil.inferBooleanAttribute(anno, "includeStatic") ?: false
-      val collector = FilteringList<PsiNamedElement> { it.name?.takeIf(nameFilter) != null }
-      val excludesCollector = FilteringList<PsiNamedElement> { it.name?.takeUnless(nameFilter) in excludesIdentifiers }
-      if (acceptSuperFields || acceptSuperProperties) {
-        val visited = mutableSetOf<PsiClass>()
-        val superClass = containingClass.getSupers(false)[0]
-        collectSupers(superClass, acceptSuperFields, acceptSuperProperties, collector, excludesCollector, visited)
-      }
-      referencedFromExcludes = excludesCollector
-      accept(containingClass, acceptProperties, includeBeans, acceptFields, includeStatic, excludesCollector, collector)
-      if (ordering != null) {
-        val includeComparator = Comparator.comparingInt { param: PsiNamedElement -> ordering.indexOf(param.name) }
-        collector.sortWith(includeComparator)
-      }
-      order = collector
-      hasPropertyOptions = containingClass.hasAnnotation(GroovyCommonClassNames.GROOVY_TRANSFORM_PROPERTY_OPTIONS)
-    }
-    else {
-      order = emptyList()
-      hasPropertyOptions = false
-      referencedFromExcludes = emptyList()
-    }
-
-  }
-
-  private fun collectSupers(owner: PsiClass?,
-                            acceptSuperFields: Boolean,
-                            acceptSuperProperties: Boolean,
-                            collector: MutableList<PsiNamedElement>,
-                            excludesCollector: MutableList<PsiNamedElement>,
-                            visited: MutableCollection<PsiClass>) {
-    if (owner == null || !visited.add(owner) || GroovyCommonClassNames.GROOVY_OBJECT_SUPPORT == owner.qualifiedName) {
-      return
-    }
-    collectSupers(owner.superClass, acceptSuperFields, acceptSuperProperties, collector, excludesCollector, visited)
-    accept(owner, acceptSuperProperties, false, acceptSuperFields, false, excludesCollector, collector)
-  }
+data class AffectedMembersCache internal constructor(private val order: List<PsiNamedElement>,
+                                                     private val hasPropertyOptions: Boolean,
+                                                     private val referencedFromExcludes: List<PsiNamedElement>) {
 
   companion object {
-
-    fun accept(clazz: PsiClass,
-               includeProperties: Boolean,
-               includeBeans: Boolean,
-               includeFields: Boolean,
-               includeStatic: Boolean,
-               excludesCollector: MutableList<PsiNamedElement>,
-               collector: MutableCollection<PsiNamedElement>) {
-      val (properties, setters, fields) = getGroupedClassMembers(clazz)
-
-      fun addParameter(origin: PsiField) {
-        if (!includeStatic && origin.hasModifierProperty(PsiModifier.STATIC)) return
-        excludesCollector.add(origin)
-        collector.add(origin)
-      }
-
-      if (includeProperties) {
-        for (property in properties) {
-          addParameter(property)
-        }
-      }
-
-      if (includeBeans) {
-        for (method in setters) {
-          if (!includeStatic && method.hasModifierProperty(PsiModifier.STATIC)) continue
-          excludesCollector.add(method)
-          collector.add(method)
-        }
-      }
-
-      if (includeFields) {
-        for (field in fields) {
-          if (field.hasModifierProperty(GrModifier.FINAL) && field.initializer != null) continue
-          addParameter(field)
-        }
-      }
-    }
-
-    private fun collectNamesOrderInformation(tupleConstructor: PsiAnnotation): Triple<(String) -> Boolean, List<String>, List<String>?> {
-
-      val excludes: List<String> = getIdentifierList(tupleConstructor, TupleConstructorAttributes.EXCLUDES) ?: emptyList()
-
-      val includes: List<String>? = getIdentifierList(tupleConstructor, TupleConstructorAttributes.INCLUDES)
-        ?.takeUnless { Undefined.isUndefined(it.singleOrNull()) }
-
-      val allowInternalNames = GrAnnotationUtil.inferBooleanAttribute(tupleConstructor, TupleConstructorAttributes.ALL_NAMES) ?: false
-
-      val filter: (String) -> Boolean = { name: String ->
-        val internalFilter = allowInternalNames || !isInternal(name)
-        val excludesFilter = !excludes.contains(name)
-        val includesFilter = includes == null || includes.contains(name)
-        internalFilter && excludesFilter && includesFilter
-      }
-      return Triple(filter, excludes, includes)
-    }
-
-    private fun getGroupedClassMembers(psiClass: PsiClass): Triple<List<PsiField>, List<PsiMethod>, List<PsiField>> {
-      val fields: MutableList<PsiField> = ArrayList()
-      val properties: MutableList<PsiField> = ArrayList()
-      val allFields = if (psiClass is GrTypeDefinition) psiClass.codeFields else psiClass.fields
-      for (field: PsiField in allFields) {
-        if (field is GrField && field.isProperty) properties.add(field) else fields.add(field)
-      }
-
-      val methods: Array<out PsiMethod> = if (psiClass is GrTypeDefinition) psiClass.codeMethods else psiClass.methods
-      val propertySetters = PropertyUtilBase.getAllProperties(true, false, methods)
-      val fieldNames = allFields.map(PsiField::getName)
-      val setters: List<PsiMethod> = propertySetters.filterKeys { !fieldNames.contains(it) }.values.toList()
-
-      return Triple(properties, setters, fields)
-    }
+    @JvmStatic
+    fun isInternal(name: String): Boolean = name.contains("$")
 
     @JvmStatic
-    fun isInternal(name : String): Boolean = name.contains("$")
-
-    @JvmStatic
-    fun getExternalName(namedElement: PsiNamedElement) : String? = when(namedElement) {
+    fun getExternalName(namedElement: PsiNamedElement): String? = when (namedElement) {
       is PsiField -> namedElement.name
       is PsiMethod -> PropertyUtilBase.getPropertyName(namedElement)
       else -> null
@@ -216,4 +85,133 @@ class AffectedMembersCache internal constructor(anno: PsiAnnotation) {
   fun getAllAffectedMembers(): List<PsiNamedElement> = order + referencedFromExcludes
 
   fun arePropertiesHandledByUser(): Boolean = hasPropertyOptions
+}
+
+fun getAffectedMembersCache(annotation: PsiAnnotation): AffectedMembersCache = CachedValuesManager.getCachedValue(annotation) {
+  val cache = computeAffectedMembersCache(annotation)
+  CachedValueProvider.Result(cache, annotation)
+}
+
+private fun computeAffectedMembersCache(annotation: PsiAnnotation): AffectedMembersCache {
+  val owner = annotation.owner as? PsiElement ?: return EmptyAffectedMembersCache
+  val containingClass = owner.parentOfType<GrTypeDefinition>()?.takeIf { it.modifierList === owner } ?: return EmptyAffectedMembersCache
+  val (nameFilter: (String) -> Boolean, excludedIdentifiers: List<String>, orderFromIncludes: List<String>?) =
+    collectNamesOrderInformation(annotation)
+
+  class FilteringList<T>(private val predicate: (T) -> Boolean) : ArrayList<T>() {
+    override fun add(element: T): Boolean = if (!predicate(element)) false else super.add(element)
+  }
+
+  val resultCollector = FilteringList<PsiNamedElement> { it.name?.takeIf(nameFilter) != null }
+  val excludesCollector = FilteringList<PsiNamedElement> { it.name?.takeUnless(nameFilter) in excludedIdentifiers }
+  val unifiedCollector = object : ArrayList<PsiNamedElement>() {
+    override fun add(element: PsiNamedElement): Boolean {
+      resultCollector.add(element)
+      excludesCollector.add(element)
+      return super.add(element)
+    }
+  }
+
+  processSuperClasses(containingClass, annotation, unifiedCollector)
+
+  processThisClass(containingClass, annotation, unifiedCollector)
+
+  if (orderFromIncludes != null) {
+    val includeComparator = Comparator.comparingInt { param: PsiNamedElement -> orderFromIncludes.indexOf(param.name) }
+    resultCollector.sortWith(includeComparator)
+  }
+  return AffectedMembersCache(resultCollector, containingClass.hasAnnotation(GROOVY_TRANSFORM_PROPERTY_OPTIONS), excludesCollector)
+}
+
+val EmptyAffectedMembersCache = AffectedMembersCache(emptyList(), false, emptyList())
+
+private fun processSuperClasses(containingClass: GrTypeDefinition, anno: PsiAnnotation, collector: MutableList<PsiNamedElement>) {
+  val acceptSuperProperties = inferBooleanAttribute(anno, TupleConstructorAttributes.INCLUDE_SUPER_PROPERTIES) ?: false
+  val acceptSuperFields = inferBooleanAttribute(anno, TupleConstructorAttributes.INCLUDE_SUPER_FIELDS) ?: false
+  if (!acceptSuperFields && !acceptSuperProperties) return
+
+  val visited = mutableSetOf<PsiClass>()
+  val superClass = containingClass.getSupers(false)[0]
+  fun collectSupers(owner: PsiClass?) {
+    if (owner == null || !visited.add(owner) || GROOVY_OBJECT_SUPPORT == owner.qualifiedName) return
+    collectSupers(owner.superClass)
+    acceptClass(owner, acceptSuperProperties, false, acceptSuperFields, false, collector)
+  }
+  collectSupers(superClass)
+}
+
+fun processThisClass(containingClass: PsiClass, anno: PsiAnnotation, collector: MutableList<PsiNamedElement>) {
+  val includeFields = inferBooleanAttribute(anno, TupleConstructorAttributes.INCLUDE_FIELDS) ?: true
+  val includeProperties = inferBooleanAttribute(anno, TupleConstructorAttributes.INCLUDE_PROPERTIES) ?: true
+  val includeBeans = inferBooleanAttribute(anno, TupleConstructorAttributes.ALL_PROPERTIES) ?: false
+  val includeStatic = inferBooleanAttribute(anno, "includeStatic") ?: false
+  acceptClass(containingClass, includeProperties, includeBeans, includeFields, includeStatic, collector)
+}
+
+private fun acceptClass(clazz: PsiClass,
+                        includeProperties: Boolean,
+                        includeBeans: Boolean,
+                        includeFields: Boolean,
+                        includeStatic: Boolean,
+                        collector: MutableCollection<PsiNamedElement>) {
+  val (properties, setters, fields) = getGroupedClassMembers(clazz)
+
+  fun addParameter(origin: PsiField) {
+    if (!includeStatic && origin.hasModifierProperty(PsiModifier.STATIC)) return
+    collector.add(origin)
+  }
+
+  if (includeProperties) {
+    for (property in properties) {
+      addParameter(property)
+    }
+  }
+
+  if (includeBeans) {
+    for (method in setters) {
+      if (!includeStatic && method.hasModifierProperty(PsiModifier.STATIC)) continue
+      collector.add(method)
+    }
+  }
+
+  if (includeFields) {
+    for (field in fields) {
+      if (field.hasModifierProperty(GrModifier.FINAL) && field.initializer != null) continue
+      addParameter(field)
+    }
+  }
+}
+
+private fun collectNamesOrderInformation(tupleConstructor: PsiAnnotation): Triple<(String) -> Boolean, List<String>, List<String>?> {
+
+  val excludes: List<String> = getIdentifierList(tupleConstructor, TupleConstructorAttributes.EXCLUDES) ?: emptyList()
+
+  val includes: List<String>? = getIdentifierList(tupleConstructor, TupleConstructorAttributes.INCLUDES)
+    ?.takeUnless { Undefined.isUndefined(it.singleOrNull()) }
+
+  val allowInternalNames = inferBooleanAttribute(tupleConstructor, TupleConstructorAttributes.ALL_NAMES) ?: false
+
+  val filter: (String) -> Boolean = { name: String ->
+    val internalFilter = allowInternalNames || !AffectedMembersCache.isInternal(name)
+    val excludesFilter = !excludes.contains(name)
+    val includesFilter = includes == null || includes.contains(name)
+    internalFilter && excludesFilter && includesFilter
+  }
+  return Triple(filter, excludes, includes)
+}
+
+fun getGroupedClassMembers(psiClass: PsiClass): Triple<List<PsiField>, List<PsiMethod>, List<PsiField>> {
+  val fields: MutableList<PsiField> = mutableListOf()
+  val properties: MutableList<PsiField> = mutableListOf()
+  val allFields = if (psiClass is GrTypeDefinition) psiClass.codeFields else psiClass.fields
+  for (field: PsiField in allFields) {
+    if (field is GrField && field.isProperty) properties.add(field) else fields.add(field)
+  }
+
+  val methods: Array<out PsiMethod> = if (psiClass is GrTypeDefinition) psiClass.codeMethods else psiClass.methods
+  val propertySetters = PropertyUtilBase.getAllProperties(true, false, methods)
+  val fieldNames = allFields.map(PsiField::getName)
+  val setters: List<PsiMethod> = propertySetters.filterKeys { !fieldNames.contains(it) }.values.toList()
+
+  return Triple(properties, setters, fields)
 }
