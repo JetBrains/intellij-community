739e672fff5dcaa9f08edd6450af3a4fc4ae4093
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2020-09-07 16:46:30 +0200

    accept nullable console as SMRootTestProxy constructor parameter
    GitOrigin-RevId: da5b88e7b5b52a24384b2b2c25740e6140229e36

--- platform/smRunner/src/com/intellij/execution/testframework/sm/runner/SMTestProxy.java
+++ platform/smRunner/src/com/intellij/execution/testframework/sm/runner/SMTestProxy.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.testframework.sm.runner;
 
 import com.intellij.execution.Location;
@@ -961,7 +961,7 @@ public class SMTestProxy extends AbstractTestProxy {
       this(false, null);
     }
 
-    public SMRootTestProxy(boolean preservePresentableName, @NotNull JComponent console) {
+    public SMRootTestProxy(boolean preservePresentableName, @Nullable JComponent console) {
       super("[root]", true, null, preservePresentableName);
       myConsole = console;
     }
