7c010a79789aded87911dc36fd17001a7ae3c266
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2020-09-07 18:36:52 +0300

    Index content diagnostics: refactoring: collect files in a builder.
    GitOrigin-RevId: 96ff014d32663437d5d3e56317a9a9f6b9aaa536

--- platform/lang-impl/src/com/intellij/util/indexing/diagnostic/dump/IndexContentDiagnosticDumper.kt
+++ platform/lang-impl/src/com/intellij/util/indexing/diagnostic/dump/IndexContentDiagnosticDumper.kt
@@ -1,8 +1,11 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.indexing.diagnostic.dump
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.containers.ConcurrentBitSet
 import com.intellij.util.indexing.FileBasedIndex
 import com.intellij.util.indexing.FileBasedIndexImpl
@@ -10,10 +13,42 @@ import com.intellij.util.indexing.IndexingBundle
 import com.intellij.util.indexing.diagnostic.dump.paths.IndexedFilePath
 import com.intellij.util.indexing.diagnostic.dump.paths.IndexedFilePaths
 import com.intellij.util.indexing.diagnostic.dump.paths.PortableFilePaths
+import java.nio.file.Path
+import java.util.*
+
+class IndexContentDiagnosticBuilder(private val project: Project) {
+
+  val allIndexedFilePaths = arrayListOf<IndexedFilePath>()
+  val filesFromUnsupportedFileSystems = arrayListOf<IndexedFilePath>()
+  val projectIndexedFileProviderDebugNameToFileIds = hashMapOf<String, MutableSet<Int>>()
+
+  fun addFile(file: VirtualFile, providerName: String) {
+    if (file.isDirectory) {
+      return
+    }
+    val indexedFilePath = IndexedFilePaths.createIndexedFilePath(file, project)
+    if (PortableFilePaths.isSupportedFileSystem(file)) {
+      allIndexedFilePaths += indexedFilePath
+      projectIndexedFileProviderDebugNameToFileIds.getOrPut(providerName) { TreeSet() } += indexedFilePath.originalFileSystemId
+    }
+    else {
+      // TODO: consider not excluding any file systems.
+      filesFromUnsupportedFileSystems += indexedFilePath
+    }
+  }
+
+  fun build(): IndexContentDiagnostic = IndexContentDiagnostic(
+    allIndexedFilePaths,
+    filesFromUnsupportedFileSystems,
+    projectIndexedFileProviderDebugNameToFileIds
+  )
+}
 
 object IndexContentDiagnosticDumper {
 
-  fun getIndexContentDiagnostic(project: Project, indicator: ProgressIndicator): IndexContentDiagnostic {
+  private val jacksonObjectMapper = jacksonObjectMapper()
+
+  fun getIndexContentDiagnosticForProject(project: Project, indicator: ProgressIndicator): IndexContentDiagnostic {
     val providers = (FileBasedIndex.getInstance() as FileBasedIndexImpl).getOrderedIndexableFilesProviders(project)
     val visitedFiles = ConcurrentBitSet()
 
@@ -21,36 +56,21 @@ object IndexContentDiagnosticDumper {
     indicator.isIndeterminate = false
     indicator.fraction = 0.0
 
-    val indexedFilePaths = arrayListOf<IndexedFilePath>()
-    val providerNameToOriginalFileIds = hashMapOf<String, MutableSet<Int>>()
-    val filesFromUnsupportedFileSystem = arrayListOf<IndexedFilePath>()
-
+    val builder = IndexContentDiagnosticBuilder(project)
     for ((index, provider) in providers.withIndex()) {
       indicator.text2 = provider.debugName
-      val providerFileIds = hashSetOf<Int>()
-      providerNameToOriginalFileIds[provider.debugName] = providerFileIds
       provider.iterateFiles(project, { fileOrDir ->
-        if (!fileOrDir.isDirectory) {
-          val indexedFilePath = IndexedFilePaths.createIndexedFilePath(fileOrDir, project)
-          val originalFileId = indexedFilePath.originalFileSystemId
-          if (PortableFilePaths.isSupportedFileSystem(fileOrDir)) {
-            indexedFilePaths += indexedFilePath
-            providerFileIds += originalFileId
-          }
-          else {
-            // TODO: consider not excluding any file systems.
-            filesFromUnsupportedFileSystem += indexedFilePath
-            return@iterateFiles true
-          }
-        }
+        builder.addFile(fileOrDir, provider.debugName)
         true
       }, visitedFiles)
       indicator.fraction = (index + 1).toDouble() / providers.size
     }
-    return IndexContentDiagnostic(
-      indexedFilePaths,
-      filesFromUnsupportedFileSystem,
-      providerNameToOriginalFileIds
-    )
+    return builder.build()
   }
+
+  fun writeTo(destination: Path, contentDiagnostic: IndexContentDiagnostic) {
+    jacksonObjectMapper.writerWithDefaultPrettyPrinter().writeValue(destination.toFile(), contentDiagnostic)
+  }
+
+  fun readFrom(file: Path): IndexContentDiagnostic = jacksonObjectMapper.readValue(file.toFile())
 }
