6e7bebaec1c7fcdf722fb090ebc03b9663dee92d
Dmitry Jemerov <yole@jetbrains.com>
2020-09-07 19:15:24 +0200

    Allow using ServiceManager.getServiceIfCreated() before application has been created
    GitOrigin-RevId: 8ba3164c9d20896d466a374a9256a89f1c7ddd4c

--- platform/core-api/src/com/intellij/openapi/components/ServiceManager.java
+++ platform/core-api/src/com/intellij/openapi/components/ServiceManager.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.components;
 
+import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.NotNullLazyKey;
@@ -37,7 +38,9 @@ public final class ServiceManager {
    */
   @Deprecated
   public static @Nullable <T> T getServiceIfCreated(@NotNull Class<T> serviceClass) {
-    return ApplicationManager.getApplication().getServiceIfCreated(serviceClass);
+    Application application = ApplicationManager.getApplication();
+    if (application == null) return null;
+    return application.getServiceIfCreated(serviceClass);
   }
 
   /**
