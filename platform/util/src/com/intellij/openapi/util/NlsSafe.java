a0e092df9922937f8998db82b7ef0704ad38c72e
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2020-09-10 10:28:41 +0700

    NlsSafe javadoc updated
    GitOrigin-RevId: cd420834a9465b8e19c5e865306d0ed74c03cfed

--- platform/util/src/com/intellij/openapi/util/NlsSafe.java
+++ platform/util/src/com/intellij/openapi/util/NlsSafe.java
@@ -2,11 +2,13 @@
 package com.intellij.openapi.util;
 
 import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NonNls;
 
 import java.lang.annotation.*;
 
 /**
  * Marker annotation for strings that don't require localization but still could be displayed in UI.
+ * <p>
  * Examples:
  * <ul>
  *   <li>File name or path</li>
@@ -14,6 +16,9 @@ import java.lang.annotation.*;
  *   <li>URL</li>
  *   <li>Programming language or framework name</li>
  * </ul>
+ * Avoid using NlsSafe just to suppress the "hardcoded string" inspection warning. Use @{@link NonNls} 
+ * if something is not intended to be displayed to the user: internal identifier, XML tag attribute, 
+ * substring to be searched in the external process output, etc.
  */
 @ApiStatus.Experimental
 @Retention(RetentionPolicy.CLASS)
