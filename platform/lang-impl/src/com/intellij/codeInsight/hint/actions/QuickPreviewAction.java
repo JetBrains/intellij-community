3933928dd897355dadf1c8542fb94072ae3e41f6
Konstantin Bulenkov <kb@jetbrains.com>
2020-09-11 14:36:04 +0200

    Add quick preview action in trees and lists
    GitOrigin-RevId: 95d27bb88538dc97fb7373120a3b9a5dc4dc72af

---
+++ platform/lang-impl/src/com/intellij/codeInsight/hint/actions/QuickPreviewAction.java
@@ -0,0 +1,47 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.codeInsight.hint.actions;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author Konstantin Bulenkov
+ */
+public class QuickPreviewAction extends ShowImplementationsAction {
+  @Override
+  protected void triggerFeatureUsed(@NotNull Project project) {
+    triggerFeatureUsed(project, "codeassist.quickpreview", "codeassist.quickpreview.lookup");
+  }
+
+  @Override
+  protected boolean couldPinPopup() {
+    return false;
+  }
+
+  @Override
+  public void update(@NotNull AnActionEvent e) {
+    if (isQuickPreviewAvailableFor(e)) {
+      super.update(e);
+    } else {
+      e.getPresentation().setEnabled(false);
+    }
+  }
+
+  protected boolean isQuickPreviewAvailableFor(AnActionEvent e) {
+    Component component = e.getData(PlatformDataKeys.CONTEXT_COMPONENT);
+    if (component instanceof JTree || component instanceof JList) {
+      VirtualFile virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE);
+      PsiElement psiElement = e.getData(CommonDataKeys.PSI_ELEMENT);
+      return virtualFile != null || psiElement != null;
+    }
+    return false;
+  }
+}
