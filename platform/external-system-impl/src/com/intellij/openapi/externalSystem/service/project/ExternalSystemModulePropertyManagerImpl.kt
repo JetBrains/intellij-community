0886b2be4178e75ae350e1f3e42450350c0dc9ac
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2020-09-14 19:03:52 +0300

    [external system] move ExternalStorageConfigurationManagerImpl to externalSystem.impl module
    GitOrigin-RevId: e0184e3defb69950ce21922539b883f2607436b6

--- platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/project/ExternalSystemModulePropertyManagerImpl.kt
+++ platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/project/ExternalSystemModulePropertyManagerImpl.kt
@@ -1,8 +1,9 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.openapi.externalSystem
+package com.intellij.openapi.externalSystem.service.project
 
 import com.intellij.openapi.components.PersistentStateComponent
 import com.intellij.openapi.components.State
+import com.intellij.openapi.externalSystem.ExternalSystemModulePropertyManager
 import com.intellij.openapi.externalSystem.model.ProjectSystemId
 import com.intellij.openapi.externalSystem.model.project.ModuleData
 import com.intellij.openapi.externalSystem.model.project.ProjectData
