54050dd02a0b812b29176c2295410f4163bdc1c2
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2020-09-15 11:21:31 +0200

    perf-tests: undo changes to intellij.idea.tools.launch module
    GitOrigin-RevId: 9aa83ae19be2720a00668fa628fddeac1e51840b

--- build/launch/src/com/intellij/tools/launch/ModulesProvider.kt
+++ build/launch/src/com/intellij/tools/launch/ModulesProvider.kt
@@ -2,7 +2,7 @@ package com.intellij.tools.launch
 
 interface ModulesProvider {
   val mainModule: String
-  val additionalModules: List<String> get() = listOf()
-  val excludedModules: List<String> get() = listOf()
+  val additionalModules: List<String>
+  val excludedModules: List<String>
   val includeTestDependencies: Boolean get() = false
 }
