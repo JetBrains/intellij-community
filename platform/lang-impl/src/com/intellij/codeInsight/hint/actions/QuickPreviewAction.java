41b19dc426d27588c66f15a4882407b8911ee2e3
Konstantin Bulenkov <kb@jetbrains.com>
2020-09-15 09:40:05 +0200

    QuickPreview conflicts with ColumnAction$Visibility. Add deeper checks
    GitOrigin-RevId: 5819d1d2d4c36f05b780cba82f19ac88916508ef

--- platform/lang-impl/src/com/intellij/codeInsight/hint/actions/QuickPreviewAction.java
+++ platform/lang-impl/src/com/intellij/codeInsight/hint/actions/QuickPreviewAction.java
@@ -1,8 +1,11 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.hint.actions;
 
+import com.intellij.codeInsight.hint.ImplementationViewSession;
+import com.intellij.codeInsight.hint.ImplementationViewSessionFactory;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -40,10 +43,21 @@ public class QuickPreviewAction extends ShowImplementationsAction {
     Component component = e.getData(PlatformDataKeys.CONTEXT_COMPONENT);
     if (component instanceof JTree || component instanceof JList) {
       SpeedSearchSupply supply = SpeedSearchSupply.getSupply((JComponent)component);
+
       if (supply == null) {
         VirtualFile virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE);
         PsiElement psiElement = e.getData(CommonDataKeys.PSI_ELEMENT);
-        return virtualFile != null || psiElement != null;
+        Project project = e.getProject();
+
+        if (project != null && (virtualFile != null || psiElement != null)) {
+          DataContext context = e.getDataContext();
+          for (ImplementationViewSessionFactory factory : getSessionFactories()) {
+            ImplementationViewSession session = factory.createSession(context, project, isSearchDeep(), isIncludeAlwaysSelf());
+            if (session != null) {
+              return true;
+            }
+          }
+        }
       }
     }
     return false;
