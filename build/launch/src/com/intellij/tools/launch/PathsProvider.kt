11ac23b2a940e0610274169133fbf4ab17f33f07
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2020-09-15 11:21:37 +0200

    Revert "perf-tests: allow running from sources (take 2 of N): extract common code from ClassPathBuilder.kt"
    This reverts commit 617192f87a562c17fd8adc47276ad74375440c45.
    
    GitOrigin-RevId: 467d53d7367318c0feb7c214873e92332a396c47

--- build/launch/src/com/intellij/tools/launch/PathsProvider.kt
+++ build/launch/src/com/intellij/tools/launch/PathsProvider.kt
@@ -4,17 +4,11 @@ import java.io.File
 import com.intellij.openapi.util.SystemInfo
 import com.intellij.util.SystemProperties
 
-interface IntelliJProjectPathsProvider {
-  val projectRootFolder: File
-  val communityRootFolder: File get() = File(projectRootFolder, "community")
-  val outputRootFolder: File get() = File(projectRootFolder, "out")
-}
-
-interface PathsProvider : IntelliJProjectPathsProvider {
+interface PathsProvider {
   val productId: String
-  override val projectRootFolder: File
-  override val communityRootFolder: File
-  override val outputRootFolder: File
+  val projectRootFolder: File
+  val communityRootFolder: File
+  val outputRootFolder: File
 
   val tempFolder: File
     get() = TeamCityHelper.tempDirectory ?: projectRootFolder.resolve("out").resolve("tmp")
--- build/launch/src/com/intellij/tools/launch/impl/ClassPathBuilder.kt
+++ build/launch/src/com/intellij/tools/launch/impl/ClassPathBuilder.kt
@@ -4,7 +4,6 @@ import com.intellij.execution.CommandLineWrapperUtil
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.tools.launch.ModulesProvider
 import com.intellij.tools.launch.PathsProvider
-import com.intellij.tools.launch.IntelliJProjectPathsProvider
 import org.jetbrains.jps.model.JpsElementFactory
 import org.jetbrains.jps.model.serialization.JpsModelSerializationDataService
 import org.jetbrains.jps.model.serialization.JpsProjectLoader
@@ -19,80 +18,65 @@ import java.nio.file.StandardCopyOption
 import java.util.*
 import java.util.jar.Manifest
 
-fun buildIntelliJClassPath(paths: IntelliJProjectPathsProvider,
-                           modules: ModulesProvider): List<File> {
-  val model = JpsElementFactory.getInstance().createModel() ?: throw Exception("Couldn't create JpsModel")
-
-  val pathVariablesConfiguration = JpsModelSerializationDataService.getOrCreatePathVariablesConfiguration(model.global)
-
-  val m2HomePath = File(SystemProperties.getUserHome())
-    .resolve(".m2")
-    .resolve("repository")
-  pathVariablesConfiguration.addPathVariable("MAVEN_REPOSITORY", m2HomePath.canonicalPath)
-
-  val kotlinPath = paths.communityRootFolder
-    .resolve("build")
-    .resolve("dependencies")
-    .resolve("build")
-    .resolve("kotlin")
-    .resolve("Kotlin")
-    .resolve("kotlinc")
-  pathVariablesConfiguration.addPathVariable("KOTLIN_BUNDLED", kotlinPath.canonicalPath)
-
-  val pathVariables = JpsModelSerializationDataService.computeAllPathVariables(model.global)
-  JpsProjectLoader.loadProject(model.project, pathVariables, paths.projectRootFolder.canonicalPath)
-
-  val productionOutput = paths.outputRootFolder.resolve("production")
-  if (!productionOutput.isDirectory) {
-    error("Production classes output directory is missing: $productionOutput")
-  }
-
-  JpsJavaExtensionService.getInstance().getProjectExtension(model.project)!!.outputUrl =
-    "file://${FileUtil.toSystemIndependentName(paths.outputRootFolder.path)}"
-
-  val modulesList = arrayListOf<String>()
-  modulesList.add("intellij.platform.boot")
-  modulesList.add(modules.mainModule)
-  modulesList.addAll(modules.additionalModules)
-  modulesList.add("intellij.configurationScript")
-
-  fun isModuleExcluded(module: JpsModule?): Boolean {
-    if (module == null) return true
-    return modules.excludedModules.contains(module.name)
-  }
-
-  fun getClasspathForModule(module: JpsModule): List<File> {
-    return JpsJavaExtensionService
-      .dependencies(module)
-      .recursively()
-      .satisfying { if (it is JpsModuleDependency) !isModuleExcluded(it.module) else true }
-      .includedIn(JpsJavaClasspathKind.runtime(modules.includeTestDependencies))
-      .classes().roots.filter { it.exists() }.map { it }.toList()
-  }
-
-  val classpath = mutableListOf<File>()
-  for (moduleName in modulesList) {
-    val module = model.project.modules.singleOrNull { it.name == moduleName }
-                 ?: throw Exception("Module $moduleName not found")
-    if (isModuleExcluded(module)) continue
-
-    classpath.addAll(getClasspathForModule(module))
-  }
-
-  /*
-      println("Created classpath:")
-      for (path in classpath.distinct().sorted()) {
-        println("  $path")
-      }
-      println("-- END")
-  */
-  return classpath.distinct()
-}
-
 internal class ClassPathBuilder(private val paths: PathsProvider, private val modules: ModulesProvider) {
+  fun buildClassPath(): List<File> {
+    val model = JpsElementFactory.getInstance().createModel() ?: throw Exception("Couldn't create JpsModel")
+
+    val pathVariablesConfiguration = JpsModelSerializationDataService.getOrCreatePathVariablesConfiguration(model.global)
+
+    val m2HomePath = File(SystemProperties.getUserHome())
+      .resolve(".m2")
+      .resolve("repository")
+    pathVariablesConfiguration.addPathVariable("MAVEN_REPOSITORY", m2HomePath.canonicalPath)
+
+    val kotlinPath = paths.communityRootFolder
+      .resolve("build")
+      .resolve("dependencies")
+      .resolve("build")
+      .resolve("kotlin")
+      .resolve("Kotlin")
+      .resolve("kotlinc")
+    pathVariablesConfiguration.addPathVariable("KOTLIN_BUNDLED", kotlinPath.canonicalPath)
+
+    val pathVariables = JpsModelSerializationDataService.computeAllPathVariables(model.global)
+    JpsProjectLoader.loadProject(model.project, pathVariables, paths.projectRootFolder.canonicalPath)
+
+    val productionOutput = paths.outputRootFolder.resolve("production")
+    if (!productionOutput.isDirectory) {
+      error("Production classes output directory is missing: $productionOutput")
+    }
+
+    JpsJavaExtensionService.getInstance().getProjectExtension(model.project)!!.outputUrl =
+      "file://${FileUtil.toSystemIndependentName(paths.outputRootFolder.path)}"
+
+    val modulesList = arrayListOf<String>()
+    modulesList.add("intellij.platform.boot")
+    modulesList.add(modules.mainModule)
+    modulesList.addAll(modules.additionalModules)
+    modulesList.add("intellij.configurationScript")
+
+    val classpath = mutableListOf<File>()
+    for (moduleName in modulesList) {
+      val module = model.project.modules.singleOrNull { it.name == moduleName }
+                   ?: throw Exception("Module $moduleName not found")
+      if (isModuleExcluded(module)) continue
+
+      classpath.addAll(getClasspathForModule(module))
+    }
+
+/*
+    println("Created classpath:")
+    for (path in classpath.distinct().sorted()) {
+      println("  $path")
+    }
+    println("-- END")
+*/
+    return classpath.distinct()
+  }
+
   fun build(): File {
-    val classpath = buildIntelliJClassPath(paths, modules).map { it.path }
-    val tempClasspathJarFile = CommandLineWrapperUtil.createClasspathJarFile(Manifest(), classpath.distinct())
+    val classpath = buildClassPath().map { it.path }
+    val tempClasspathJarFile = CommandLineWrapperUtil.createClasspathJarFile(Manifest(), classpath)
     val launcherFolder = paths.launcherFolder
     if (!launcherFolder.exists()) {
       launcherFolder.mkdirs()
@@ -103,5 +87,19 @@ internal class ClassPathBuilder(private val paths: PathsProvider, private val mo
     val launcherClasspathFile = launcherFolder.resolve("${launcherClasspathPrefix}Launcher${UUID.randomUUID()}.jar")
     Files.move(tempClasspathJarFile.toPath(), launcherClasspathFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
     return launcherClasspathFile
+  }
+
+  private fun getClasspathForModule(module: JpsModule): List<File> {
+    return JpsJavaExtensionService
+      .dependencies(module)
+      .recursively()
+      .satisfying { if (it is JpsModuleDependency) !isModuleExcluded(it.module) else true }
+      .includedIn(JpsJavaClasspathKind.runtime(modules.includeTestDependencies))
+      .classes().roots.filter { it.exists() }.map { it }.toList()
+  }
+
+  private fun isModuleExcluded(module: JpsModule?): Boolean {
+    if (module == null) return true
+    return modules.excludedModules.contains(module.name)
   }
 }
