8c5709ca985aee20d23db8acb8206673aacd145a
Dmitry Jemerov <yole@jetbrains.com>
2020-09-15 19:12:17 +0200

    Fix incorrect i18n for combo box close button position (IDEA-250690)
    GitOrigin-RevId: 13dcab4b89f92c46138c6e625f06b98b204d6f00

--- platform/platform-impl/src/com/intellij/application/options/editor/EditorCloseButtonPosition.kt
+++ platform/platform-impl/src/com/intellij/application/options/editor/EditorCloseButtonPosition.kt
@@ -6,32 +6,42 @@ import com.intellij.ide.ui.search.BooleanOptionDescription
 import com.intellij.openapi.application.ApplicationBundle.message
 import com.intellij.ui.CollectionComboBoxModel
 import com.intellij.ui.layout.*
+import org.jetbrains.annotations.Nls
+import org.jetbrains.annotations.NonNls
 
-private const val LEFT = "combobox.tab.placement.left"
-private const val RIGHT = "combobox.tab.placement.right"
-private const val NONE = "combobox.tab.placement.none"
+private const val LEFT = "Left"
+private const val RIGHT = "Right"
+private const val NONE = "None"
 
-private val items = arrayOf(LEFT, RIGHT, NONE)
+private val items = listOf(LEFT, RIGHT, NONE)
+
+@Nls
+private fun optionName(@NonNls option: String): String = when (option) {
+  LEFT -> message("combobox.tab.placement.left")
+  RIGHT -> message("combobox.tab.placement.right")
+  else -> message("combobox.tab.placement.none")
+}
 
 internal val CLOSE_BUTTON_POSITION = message("tabs.close.button.placement")
 
 internal fun Cell.closeButtonPositionComboBox() {
-  comboBox(CollectionComboBoxModel<String>(items.map { message(it) }),
+  comboBox(CollectionComboBoxModel<String>(items),
            { getCloseButtonPlacement() },
-           { set(it) }
+           { set(it) },
+           listCellRenderer { value, _, _ -> text = optionName(value) }
   )
 }
 
 internal fun closeButtonPlacementOptionDescription(): Collection<BooleanOptionDescription> = items.map { asOptionDescriptor(it) }
 
 private fun set(s: String?) {
-  ui.showCloseButton = s !== NONE
-  if (s !== NONE) {
+  ui.showCloseButton = s != NONE
+  if (s != NONE) {
     ui.closeTabButtonOnTheRight = s == RIGHT
   }
 }
 
-private fun asOptionDescriptor(s: String) = object : BooleanOptionDescription(CLOSE_BUTTON_POSITION + " | " + message(s), ID) {
+private fun asOptionDescriptor(s: String) = object : BooleanOptionDescription(CLOSE_BUTTON_POSITION + " | " + optionName(s), ID) {
   override fun isOptionEnabled() = getCloseButtonPlacement() === s
 
   override fun setOptionState(enabled: Boolean) {
