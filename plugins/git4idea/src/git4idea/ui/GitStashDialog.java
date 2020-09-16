69c836ca00331b91540bde612914e3e76148bbd9
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2020-09-16 17:29:34 +0300

    [git-index] add "Stash Silently" action to the "Staging Area" toolbar
    GitOrigin-RevId: 59a2959dacf48ff70003954a8543746f98b11454

--- plugins/git4idea/src/git4idea/ui/GitStashDialog.java
+++ plugins/git4idea/src/git4idea/ui/GitStashDialog.java
@@ -4,9 +4,9 @@ package git4idea.ui;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.vfs.VirtualFile;
-import git4idea.commands.GitCommand;
 import git4idea.commands.GitLineHandler;
 import git4idea.i18n.GitBundle;
+import git4idea.stash.GitStashUtils;
 import git4idea.util.GitUIUtil;
 
 import javax.swing.*;
@@ -40,16 +40,7 @@ public class GitStashDialog extends DialogWrapper {
   }
 
   public GitLineHandler handler() {
-    GitLineHandler handler = new GitLineHandler(myProject, getGitRoot(), GitCommand.STASH);
-    handler.addParameters("save");
-    if (myKeepIndexCheckBox.isSelected()) {
-      handler.addParameters("--keep-index");
-    }
-    final String msg = myMessageTextField.getText().trim();
-    if (msg.length() != 0) {
-      handler.addParameters(msg);
-    }
-    return handler;
+    return GitStashUtils.createStashHandler(myProject, getGitRoot(), myKeepIndexCheckBox.isSelected(), myMessageTextField.getText());
   }
 
   /**
