27853afdb060b318316eca676cffb30ff68d7d83
Anastasia Ivanova <anastasia.ivanova@jetbrains.com>
2020-09-16 18:18:02 +0200

    IDEA-CR-66818 move StatisticsCollectorType to inspection
    GitOrigin-RevId: cfa37825406af521ea1a08e8f50c3ddd931c3ab3

--- plugins/devkit/devkit-core/src/inspections/internal/StatisticsCollectorNotRegisteredInspection.java
+++ plugins/devkit/devkit-core/src/inspections/internal/StatisticsCollectorNotRegisteredInspection.java
@@ -21,7 +21,6 @@ import org.jetbrains.idea.devkit.inspections.DevKitJvmInspection;
 import org.jetbrains.idea.devkit.inspections.quickfix.RegisterExtensionFix;
 import org.jetbrains.idea.devkit.util.ExtensionPointCandidate;
 import org.jetbrains.idea.devkit.util.ExtensionPointLocator;
-import org.jetbrains.idea.devkit.util.StatisticsCollectorType;
 
 import java.util.Set;
 
@@ -93,6 +92,31 @@ public class StatisticsCollectorNotRegisteredInspection extends DevKitJvmInspect
     if (!candidateList.isEmpty()) {
       RegisterExtensionFix fix = new RegisterExtensionFix(checkedClass, candidateList);
       sink.highlight(DevKitBundle.message("inspections.statistics.collector.not.registered.message"), fix);
+    }
+  }
+
+  private enum StatisticsCollectorType {
+    COUNTER("com.intellij.internal.statistic.service.fus.collectors.CounterUsagesCollector",
+            "com.intellij.statistics.counterUsagesCollector"),
+    PROJECT("com.intellij.internal.statistic.service.fus.collectors.ProjectUsagesCollector",
+            "com.intellij.statistics.projectUsagesCollector"),
+    APPLICATION("com.intellij.internal.statistic.service.fus.collectors.ApplicationUsagesCollector",
+                "com.intellij.statistics.applicationUsagesCollector");
+
+    private final String myClassName;
+    private final String myExtensionPoint;
+
+    StatisticsCollectorType(String className, String extensionPoint) {
+      this.myClassName = className;
+      this.myExtensionPoint = extensionPoint;
+    }
+
+    public String getClassName() {
+      return myClassName;
+    }
+
+    public String getExtensionPoint() {
+      return myExtensionPoint;
     }
   }
 }
