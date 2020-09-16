91ed0c1d57ff313852fe6da458f8df6bea0e72e6
Kirill Glazyrin <kirill.glazyrin@jetbrains.com>
2020-09-16 14:20:26 +0300

    Add an utility method
    GitOrigin-RevId: a323cb918dab1902c1aa4c21381d0017c8454d8e

--- plugins/editorconfig/src/org/editorconfig/language/parser/EditorConfigParserUtil.kt
+++ plugins/editorconfig/src/org/editorconfig/language/parser/EditorConfigParserUtil.kt
@@ -3,9 +3,10 @@ package org.editorconfig.language.parser
 
 import com.intellij.lang.PsiBuilder
 import com.intellij.lang.parser.GeneratedParserUtilBase
+import org.editorconfig.language.psi.EditorConfigElementTypes.NEW_LINE
 
+@Suppress("UNUSED_PARAMETER")
 object EditorConfigParserUtil : GeneratedParserUtilBase() {
-  @Suppress("UNUSED_PARAMETER")
   @JvmStatic
   fun unbindComments(builder: PsiBuilder, level: Int): Boolean {
     val marker = builder.latestDoneMarker as? PsiBuilder.Marker
@@ -15,5 +16,22 @@ object EditorConfigParserUtil : GeneratedParserUtilBase() {
     )
     return true
   }
+
+  @JvmStatic
+  fun followedByNewLineOrEndOfFile(builder: PsiBuilder, level: Int) = when {
+    builder.eof() -> true
+    builder.tokenType == NEW_LINE -> true
+    else -> false
+  }
+  //
+  //@JvmStatic
+  //fun consumeNewLineOrEndOfFile(builder: PsiBuilder, level: Int): Boolean {
+  //  if (builder.eof()) return true
+  //  if (builder.tokenType == NEW_LINE) {
+  //    builder.advanceLexer()
+  //    return true
+  //  }
+  //  return false
+  //}
 }
 
