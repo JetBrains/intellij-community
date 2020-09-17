231b384c6548b259c51b8fb715fc43074a1e4ca9
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2020-09-17 14:33:02 +0300

    If copying a directory fails with an exception, mention the name of the file which actually failed to be copied
    This is to help in diagnostics of issues like IDEA-250355.
    
    GitOrigin-RevId: a76f6d0354f905667e8002e63113dff56ea24036

--- platform/util/src/com/intellij/openapi/util/io/FileUtil.java
+++ platform/util/src/com/intellij/openapi/util/io/FileUtil.java
@@ -395,10 +395,15 @@ public class FileUtil extends FileUtilRt {
   private static void performCopy(@NotNull File fromFile, @NotNull File toFile, final boolean syncTimestamp) throws IOException {
     if (filesEqual(fromFile, toFile)) return;
 
-    try (FileOutputStream fos = openOutputStream(toFile)) {
-      try (FileInputStream fis = new FileInputStream(fromFile)) {
-        copy(fis, fos);
+    try {
+      try (FileOutputStream fos = openOutputStream(toFile)) {
+        try (FileInputStream fis = new FileInputStream(fromFile)) {
+          copy(fis, fos);
+        }
       }
+    }
+    catch (IOException e) {
+      throw new IOException(String.format("Couldn't copy [%s] to [%s]", fromFile, toFile), e);
     }
 
     if (syncTimestamp) {
