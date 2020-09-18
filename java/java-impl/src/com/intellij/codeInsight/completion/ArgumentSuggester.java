a8053bccb9032d246f01b9a1b919193722302898
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2020-09-18 12:51:01 +0700

    [java-completion] ArgumentSuggester: fixes according to review IDEA-CR-66883
    1. Package-private class
    2. Honor codestyle
    3. Avoid search for PsiElement to shortenClassReferences
    4. Remove extra lambda
    5. Rename presentation -> argumentPresentation
    6. Use isAssignableFrom when filtering by expected type
    
    GitOrigin-RevId: 340c1c578b2a864d1f2dbff0c70e932fba819602

--- java/java-impl/src/com/intellij/codeInsight/completion/ArgumentSuggester.java
+++ java/java-impl/src/com/intellij/codeInsight/completion/ArgumentSuggester.java
@@ -1,69 +1,73 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.completion;
 
+import com.intellij.application.options.CodeStyle;
 import com.intellij.codeInsight.ExpectedTypeInfo;
 import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementPresentation;
 import com.intellij.codeInsight.lookup.TypedLookupItem;
 import com.intellij.psi.PsiExpression;
-import com.intellij.psi.PsiMethodCallExpression;
+import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiReferenceExpression;
 import com.intellij.psi.PsiType;
+import com.intellij.psi.codeStyle.CommonCodeStyleSettings;
 import com.intellij.psi.codeStyle.JavaCodeStyleManager;
-import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.util.PsiUtil;
 import com.intellij.util.PlatformIcons;
 import com.intellij.util.containers.ContainerUtil;
 import com.siyeh.ig.psiutils.TypeUtils;
 import one.util.streamex.StreamEx;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-public class ArgumentSuggester {
+import static com.intellij.codeInsight.completion.ReferenceExpressionCompletionContributor.getSpace;
+
+class ArgumentSuggester {
   static @NotNull Collection<LookupElement> suggestArgument(PsiReferenceExpression ref, List<ExpectedTypeInfo> expectedTypeInfos) {
     PsiExpression qualifier = ref.getQualifierExpression();
-    
+
     if (qualifier == null) return Collections.emptyList();
     PsiType type = qualifier.getType();
     if (TypeUtils.isJavaLangString(type)) {
       if (PsiUtil.isLanguageLevel7OrHigher(ref)) {
-        return StreamEx.of(new MethodWithArgument("toLowerCase", type, "java.util.Locale.ROOT", "Locale.ROOT"),
-                           new MethodWithArgument("toUpperCase", type, "java.util.Locale.ROOT", "Locale.ROOT"),
-                           new MethodWithArgument("getBytes", PsiType.BYTE.createArrayType(), "java.nio.charset.StandardCharsets.UTF_8", "StandardCharsets.UTF_8"))
-          .filter(expectedTypeInfos.isEmpty() ? 
-                  element -> true : 
-                  element -> ContainerUtil.exists(expectedTypeInfos, ti -> ti.getType().equals(element.getType())))
-          .map(element -> PrioritizedLookupElement.withPriority(element, 1)).toList();
+        StreamEx<MethodWithArgument> stream = StreamEx.of(
+          new MethodWithArgument("toLowerCase", type, "java.util.Locale.ROOT", "Locale.ROOT"),
+          new MethodWithArgument("toUpperCase", type, "java.util.Locale.ROOT", "Locale.ROOT"),
+          new MethodWithArgument("getBytes", PsiType.BYTE.createArrayType(), "java.nio.charset.StandardCharsets.UTF_8", "StandardCharsets.UTF_8"));
+        if (!expectedTypeInfos.isEmpty()) {
+          stream =
+            stream.filter(element -> ContainerUtil.exists(expectedTypeInfos, info -> info.getType().isAssignableFrom(element.getType())));
+        }
+        return stream.map(element -> PrioritizedLookupElement.withPriority(element, 1)).toList();
       }
     }
     return Collections.emptyList();
   }
 
   private static class MethodWithArgument extends LookupElement implements TypedLookupItem {
-    private final String myMethod;
-    private final PsiType myType;
-    private final String myArgument;
-    private final String myPresentation;
+    private final @NotNull String myMethod;
+    private final @NotNull PsiType myType;
+    private final @NotNull String myArgument;
+    private final @NotNull String myArgumentPresentation;
 
-    MethodWithArgument(String method, PsiType type, String argument, String presentation) {
+    MethodWithArgument(@NotNull String method, @NotNull PsiType type, @NotNull String argument, @NotNull String argumentPresentation) {
       myMethod = method;
       myType = type;
       myArgument = argument;
-      myPresentation = presentation;
+      myArgumentPresentation = argumentPresentation;
     }
 
     @Override
-    public @Nullable PsiType getType() {
+    public @NotNull PsiType getType() {
       return myType;
     }
 
     @Override
     public @NotNull String getLookupString() {
-      return myMethod + "(" + myPresentation + ")";
+      return myMethod + "(" + myArgumentPresentation + ")";
     }
 
     @Override
@@ -75,14 +79,15 @@ public class ArgumentSuggester {
 
     @Override
     public void handleInsert(@NotNull InsertionContext context) {
-      String insertString = myMethod + "(" + myArgument + ")";
-      context.getDocument().replaceString(context.getStartOffset(), context.getTailOffset(), insertString);
+      PsiFile file = context.getFile();
+      CommonCodeStyleSettings settings = CodeStyle.getLanguageSettings(file);
+      String callSpace = getSpace(settings.SPACE_WITHIN_METHOD_CALL_PARENTHESES);
+      String insertString = myMethod + "(" + callSpace + myArgument + callSpace + ")";
+      int offset = context.getStartOffset();
+      context.getDocument().replaceString(offset, context.getTailOffset(), insertString);
       context.commitDocument();
-      PsiMethodCallExpression call =
-        PsiTreeUtil.findElementOfClassAtOffset(context.getFile(), context.getStartOffset(), PsiMethodCallExpression.class, false);
-      if (call == null) return;
-
-      JavaCodeStyleManager.getInstance(context.getProject()).shortenClassReferences(call.getArgumentList());
+      JavaCodeStyleManager.getInstance(context.getProject())
+        .shortenClassReferences(file, offset, offset + insertString.length());
     }
   }
 }
