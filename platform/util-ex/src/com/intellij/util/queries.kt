b2a2e0ed9ec5959662682b9c0f63111d903ffb1c
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2020-09-18 13:00:10 +0300

    proper Query#mappingNotNull implementation
    GitOrigin-RevId: 9b901706a5209311fa02bd1e5ebe51e5dd4cbdb4

--- platform/util-ex/src/com/intellij/util/queries.kt
+++ platform/util-ex/src/com/intellij/util/queries.kt
@@ -1,6 +1,14 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util
 
-fun <T : Any, R : Any> Query<T>.mapNotNull(f: (T) -> R?): Query<R> {
-  return mapping(f)
+fun <T : Any, R : Any> Query<T>.mappingNotNull(f: (T) -> R?): Query<R> {
+  return transforming { t: T ->
+    val r: R? = f(t)
+    if (r == null) {
+      emptyList()
+    }
+    else {
+      listOf(r)
+    }
+  }
 }
