887cdfb22ff57f22bf1ecc1d9548c93c51c7e3a3
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2020-09-21 23:50:43 +0300

    IDEA-65915 (wip) added 'Store as file' checkbox for RunConfigurationFragmentedEditor when it's used for RC template
    GitOrigin-RevId: da8c937057e46c4638cf1e3f5c07c3c74fb864ba

--- platform/execution-impl/src/com/intellij/execution/ui/RunnerAndConfigurationSettingsEditor.java
+++ platform/execution-impl/src/com/intellij/execution/ui/RunnerAndConfigurationSettingsEditor.java
@@ -3,17 +3,24 @@ package com.intellij.execution.ui;
 
 import com.intellij.execution.RunnerAndConfigurationSettings;
 import com.intellij.execution.configurations.RunConfigurationBase;
+import com.intellij.execution.impl.RunConfigurationStorageUi;
+import com.intellij.execution.impl.RunManagerImpl;
 import com.intellij.execution.impl.RunnerAndConfigurationSettingsImpl;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.options.SettingsEditor;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Disposer;
+import com.intellij.util.ui.JBUI;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class RunnerAndConfigurationSettingsEditor extends SettingsEditor<RunnerAndConfigurationSettings> {
 
   private final RunConfigurationFragmentedEditor<RunConfigurationBase<?>> myConfigurationEditor;
+  private final @Nullable RunConfigurationStorageUi myRCStorageUi;
 
   public RunnerAndConfigurationSettingsEditor(RunnerAndConfigurationSettings settings,
                                               RunConfigurationFragmentedEditor<RunConfigurationBase<?>> configurationEditor) {
@@ -21,22 +28,60 @@ public class RunnerAndConfigurationSettingsEditor extends SettingsEditor<RunnerA
     myConfigurationEditor = configurationEditor;
     myConfigurationEditor.addSettingsEditorListener(editor -> fireEditorStateChanged());
     Disposer.register(this, myConfigurationEditor);
+
+    Project project = settings.getConfiguration().getProject();
+    // RunConfigurationStorageUi for non-template settings is managed by com.intellij.execution.impl.SingleConfigurationConfigurable
+    myRCStorageUi = !project.isDefault() && settings.isTemplate()
+                    ? new RunConfigurationStorageUi(project, () -> fireEditorStateChanged())
+                    : null;
   }
 
   @Override
   protected void resetEditorFrom(@NotNull RunnerAndConfigurationSettings s) {
     myConfigurationEditor.resetEditorFrom((RunnerAndConfigurationSettingsImpl)s);
     myConfigurationEditor.resetFrom((RunConfigurationBase<?>)s.getConfiguration());
+
+    if (myRCStorageUi != null) {
+      myRCStorageUi.reset(s);
+    }
   }
 
   @Override
   protected void applyEditorTo(@NotNull RunnerAndConfigurationSettings s) throws ConfigurationException {
     myConfigurationEditor.applyEditorTo((RunnerAndConfigurationSettingsImpl)s);
     myConfigurationEditor.applyTo((RunConfigurationBase<?>)s.getConfiguration());
+
+    if (myRCStorageUi != null) {
+      // editing a template run configuration
+      myRCStorageUi.apply(s);
+      RunManagerImpl.getInstanceImpl(s.getConfiguration().getProject()).addConfiguration(s);
+    }
   }
 
   @Override
   protected @NotNull JComponent createEditor() {
-    return myConfigurationEditor.getComponent();
+    if (myRCStorageUi == null) return myConfigurationEditor.getComponent();
+
+    JPanel panel = new JPanel(new GridBagLayout());
+    GridBagConstraints c = new GridBagConstraints();
+    c.gridx = 0;
+    c.gridy = 0;
+    c.anchor = GridBagConstraints.EAST;
+    c.insets = JBUI.insets(5, 0, 10, 0);
+    c.fill = GridBagConstraints.NONE;
+    c.weightx = 0;
+    c.weighty = 0;
+    panel.add(myRCStorageUi.createComponent(), c);
+
+    c.gridx = 0;
+    c.gridy = 1;
+    c.anchor = GridBagConstraints.NORTH;
+    c.insets = JBUI.emptyInsets();
+    c.fill = GridBagConstraints.BOTH;
+    c.weightx = 1;
+    c.weighty = 1;
+    panel.add(myConfigurationEditor.getComponent(), c);
+
+    return panel;
   }
 }
