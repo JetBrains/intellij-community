2ca6f25db00eb8e4d197681d12b94282bd6b0070
Artemiy Sartakov <artemiy.sartakov@jetbrains.com>
2020-09-23 17:44:01 +0700

    Related problems: recheck previous changes after refactoring (IDEA-245696)
    GitOrigin-RevId: 4a6bb158fb9aa051e8a58126b7c17af88fc63a60

--- java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/problems/ClassProblemsTest.kt
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/problems/ClassProblemsTest.kt
@@ -10,6 +10,7 @@ import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.psi.*
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.refactoring.BaseRefactoringProcessor
+import com.intellij.refactoring.RefactoringFactory
 import com.intellij.refactoring.move.moveInner.MoveInnerImpl
 import com.intellij.refactoring.openapi.impl.MoveInnerRefactoringImpl
 
@@ -281,6 +282,39 @@ internal class ClassProblemsTest : ProjectProblemsViewTest() {
         val typeParameterList = factory.createTypeParameterList()
         typeParameterList.add(factory.createTypeParameterFromText("T", psiClass))
         psiClass.typeParameterList?.replace(typeParameterList)
+      }
+
+      assertEmpty(ProjectProblemUtils.getReportedProblems(myFixture.editor).entries)
+    }
+  }
+
+  fun testRenameClassAndFixUsages() {
+    val targetClass = myFixture.addClass("""
+      class AClass {
+      }
+    """.trimIndent())
+
+    myFixture.addClass("""
+      class BClass {
+        AClass tmp;
+
+        public void method1(String arg) {
+            System.out.println(tmp.toString());
+        }
+      }
+    """.trimIndent())
+
+    doTest(targetClass) {
+      changeClass(targetClass) { psiClass, factory ->
+        psiClass.nameIdentifier?.replace(factory.createIdentifier("AClass1"))
+      }
+
+      assertSize(1, ProjectProblemUtils.getReportedProblems(myFixture.editor).entries)
+
+      changeClass(targetClass) { psiClass, _ ->
+        psiClass.setName("AClass")
+        val renameRefactoring = RefactoringFactory.getInstance(project).createRename(psiClass, "AClass1", true, false)
+        renameRefactoring.run()
       }
 
       assertEmpty(ProjectProblemUtils.getReportedProblems(myFixture.editor).entries)
