517732840b6f2de3d19a7791863be0d935cdc246
Eldar Abusalimov <Eldar.Abusalimov@jetbrains.com>
2020-09-23 16:09:12 +0300

    CPP-22344: elevation: Check-in gRPC-generated files
    GitOrigin-RevId: 8076e264b564cbc2a252b7b0bcb9cf6697d945ce

---
+++ platform/elevation/rpc/gen/com/intellij/execution/process/elevation/rpc/ElevatorProtoGrpcKt.kt
@@ -0,0 +1,93 @@
+package com.intellij.execution.process.elevation.rpc
+
+import com.intellij.execution.process.elevation.rpc.ElevatorGrpc.getServiceDescriptor
+import io.grpc.CallOptions
+import io.grpc.CallOptions.DEFAULT
+import io.grpc.Channel
+import io.grpc.Metadata
+import io.grpc.MethodDescriptor
+import io.grpc.ServerServiceDefinition
+import io.grpc.ServerServiceDefinition.builder
+import io.grpc.ServiceDescriptor
+import io.grpc.Status.UNIMPLEMENTED
+import io.grpc.StatusException
+import io.grpc.kotlin.AbstractCoroutineServerImpl
+import io.grpc.kotlin.AbstractCoroutineStub
+import io.grpc.kotlin.ClientCalls.unaryRpc
+import io.grpc.kotlin.ServerCalls.unaryServerMethodDefinition
+import io.grpc.kotlin.StubFor
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.jvm.JvmOverloads
+import kotlin.jvm.JvmStatic
+
+/**
+ * Holder for Kotlin coroutine-based client and server APIs for elevation.rpc.Elevator.
+ */
+object ElevatorGrpcKt {
+  @JvmStatic
+  val serviceDescriptor: ServiceDescriptor
+    get() = ElevatorGrpc.getServiceDescriptor()
+
+  val spawnMethod: MethodDescriptor<SpawnRequest, SpawnReply>
+    @JvmStatic
+    get() = ElevatorGrpc.getSpawnMethod()
+
+  /**
+   * A stub for issuing RPCs to a(n) elevation.rpc.Elevator service as suspending coroutines.
+   */
+  @StubFor(ElevatorGrpc::class)
+  class ElevatorCoroutineStub @JvmOverloads constructor(
+    channel: Channel,
+    callOptions: CallOptions = DEFAULT
+  ) : AbstractCoroutineStub<ElevatorCoroutineStub>(channel, callOptions) {
+    override fun build(channel: Channel, callOptions: CallOptions): ElevatorCoroutineStub =
+        ElevatorCoroutineStub(channel, callOptions)
+
+    /**
+     * Executes this RPC and returns the response message, suspending until the RPC completes
+     * with [`Status.OK`][io.grpc.Status].  If the RPC completes with another status, a
+     * corresponding
+     * [StatusException] is thrown.  If this coroutine is cancelled, the RPC is also cancelled
+     * with the corresponding exception as a cause.
+     *
+     * @param request The request message to send to the server.
+     *
+     * @return The single response from the server.
+     */
+    suspend fun spawn(request: SpawnRequest): SpawnReply = unaryRpc(
+      channel,
+      ElevatorGrpc.getSpawnMethod(),
+      request,
+      callOptions,
+      Metadata()
+    )}
+
+  /**
+   * Skeletal implementation of the elevation.rpc.Elevator service based on Kotlin coroutines.
+   */
+  abstract class ElevatorCoroutineImplBase(
+    coroutineContext: CoroutineContext = EmptyCoroutineContext
+  ) : AbstractCoroutineServerImpl(coroutineContext) {
+    /**
+     * Returns the response to an RPC for elevation.rpc.Elevator.Spawn.
+     *
+     * If this method fails with a [StatusException], the RPC will fail with the corresponding
+     * [io.grpc.Status].  If this method fails with a [java.util.concurrent.CancellationException],
+     * the RPC will fail
+     * with status `Status.CANCELLED`.  If this method fails for any other reason, the RPC will
+     * fail with `Status.UNKNOWN` with the exception as a cause.
+     *
+     * @param request The request from the client.
+     */
+    open suspend fun spawn(request: SpawnRequest): SpawnReply = throw
+        StatusException(UNIMPLEMENTED.withDescription("Method elevation.rpc.Elevator.Spawn is unimplemented"))
+
+    final override fun bindService(): ServerServiceDefinition = builder(getServiceDescriptor())
+      .addMethod(unaryServerMethodDefinition(
+      context = this.context,
+      descriptor = ElevatorGrpc.getSpawnMethod(),
+      implementation = ::spawn
+    )).build()
+  }
+}
