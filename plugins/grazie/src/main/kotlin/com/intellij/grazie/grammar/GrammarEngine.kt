54d58a22b2b32388e69ac0355df279d58a4723eb
Pavel Bakhvalov <pavel.bakhvalov@jetbrains.com>
2020-09-23 15:31:26 +0300

    IDEA-CR-67065: [Grazie] Fix IDEA-251286, switch context classloader to plugin classloader during lagnuagetool calls
    GitOrigin-RevId: 941f2e8d01ec13e798ab52a826394088095b3dba

--- plugins/grazie/src/main/kotlin/com/intellij/grazie/grammar/GrammarEngine.kt
+++ plugins/grazie/src/main/kotlin/com/intellij/grazie/grammar/GrammarEngine.kt
@@ -1,10 +1,12 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.grazie.grammar
 
+import com.intellij.grazie.GraziePlugin
 import com.intellij.grazie.detection.LangDetector
 import com.intellij.grazie.jlanguage.LangTool
 import com.intellij.grazie.utils.LinkedSet
 import com.intellij.openapi.progress.ProcessCanceledException
+import com.intellij.openapi.util.ClassLoaderUtil
 import com.intellij.util.ExceptionUtil
 import org.slf4j.LoggerFactory
 
@@ -23,11 +25,13 @@ object GrammarEngine {
     val lang = LangDetector.getLang(str) ?: return emptySet()
 
     return try {
-      LangTool.getTool(lang).check(str)
-        .asSequence()
-        .filterNotNull()
-        .map { Typo(it, lang, offset) }
-        .toCollection(LinkedSet())
+      ClassLoaderUtil.computeWithClassLoader<Set<Typo>, Throwable>(GraziePlugin.classLoader) {
+        LangTool.getTool(lang).check(str)
+          .asSequence()
+          .filterNotNull()
+          .map { Typo(it, lang, offset) }
+          .toCollection(LinkedSet())
+      }
     }
     catch (e: Throwable) {
       if (ExceptionUtil.causedBy(e, ProcessCanceledException::class.java)) {
