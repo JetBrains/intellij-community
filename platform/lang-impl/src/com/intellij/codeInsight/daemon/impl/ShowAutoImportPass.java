c96615b93da9de2ca84c2905083d5e3d68bd3dd0
Alexey Kudravtsev <cdr@intellij.com>
2020-09-24 12:33:25 +0200

    instantiate and run just GeneralHighlightingPass to determine the unresolved imports to speedup optimize imports and avoid nasty issues, e.g. running external annotators under read action
    GitOrigin-RevId: 96944d2da5dc05053b5c55ee0840a028f51cf797

--- platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/ShowAutoImportPass.java
+++ platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/ShowAutoImportPass.java
@@ -10,6 +10,7 @@ import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer;
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzerSettings;
 import com.intellij.codeInsight.intention.IntentionAction;
 import com.intellij.codeInspection.HintAction;
+import com.intellij.codeInspection.ex.GlobalInspectionContextBase;
 import com.intellij.injected.editor.EditorWindow;
 import com.intellij.lang.annotation.HighlightSeverity;
 import com.intellij.lang.injection.InjectedLanguageManager;
@@ -36,7 +37,7 @@ import com.intellij.openapi.util.registry.Registry;
 import com.intellij.psi.*;
 import com.intellij.psi.util.PsiUtilCore;
 import com.intellij.util.SmartList;
-import org.jetbrains.annotations.Nls;
+import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -205,14 +206,38 @@ public class ShowAutoImportPass extends TextEditorHighlightingPass {
     DaemonProgressIndicator progress = new DaemonProgressIndicator();
     AtomicReference<List<HighlightInfo>> infos = new AtomicReference<>(Collections.emptyList());
     ((ApplicationImpl)ApplicationManager.getApplication()).executeByImpatientReader(() ->
-      ProgressManager.getInstance().executeProcessUnderProgress(() ->
-        infos.set(DaemonCodeAnalyzerEx.getInstanceEx(project).runMainPasses(file, document, progress)), progress));
+      ProgressManager.getInstance().executeProcessUnderProgress(() -> infos.set(runGeneralHighlightingPass(file)), progress));
 
     List<HintAction> result = new ArrayList<>(infos.get().size());
     for (HighlightInfo info : infos.get()) {
       for (HintAction action : extractHints(info)) {
         if (action.isAvailable(project, null, file)) {
           result.add(action);
+        }
+      }
+    }
+    return result;
+  }
+
+  @NotNull
+  public static List<HighlightInfo> runGeneralHighlightingPass(@NotNull PsiFile file) {
+    Project project = file.getProject();
+    Document document = PsiDocumentManager.getInstance(project).getDocument(file);
+    if (document == null) return Collections.emptyList();
+    ProgressIndicator progress = ProgressManager.getGlobalProgressIndicator();
+    GlobalInspectionContextBase.assertUnderDaemonProgress();
+
+    TextEditorHighlightingPassRegistrarEx passRegistrarEx = TextEditorHighlightingPassRegistrarEx.getInstanceEx(project);
+    List<TextEditorHighlightingPass> passes = passRegistrarEx.instantiateMainPasses(file, document, HighlightInfoProcessor.getEmpty());
+    List<GeneralHighlightingPass> gpasses = ContainerUtil.filterIsInstance(passes, GeneralHighlightingPass.class);
+
+    List<HighlightInfo> result = new ArrayList<>();
+    for (TextEditorHighlightingPass pass : gpasses) {
+      pass.doCollectInformation(progress);
+      List<HighlightInfo> infos = pass.getInfos();
+      for (HighlightInfo info : infos) {
+        if (info != null && info.getSeverity().compareTo(HighlightSeverity.INFORMATION) > 0) {
+          result.add(info);
         }
       }
     }
