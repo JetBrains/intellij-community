fd64eeb9073efef1ebfbad26ae2df67bef803e93
Eldar Abusalimov <Eldar.Abusalimov@jetbrains.com>
2020-09-25 01:51:40 +0300

    CPP-22344: elevation: Implement daemon launcher and ElevatorClient stub
    (cherry picked from commit 8aab5e416d50e5a97637fe422abf59c8e534e67c)
    
    IJ-CR-3496
    
    GitOrigin-RevId: 12cbc6d3fd2835e3d48bc86cea926792c64537a5

---
+++ platform/elevation/src/com/intellij/execution/process/elevation/ElevationService.kt
@@ -0,0 +1,128 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.execution.process.elevation
+
+import com.intellij.execution.configurations.GeneralCommandLine
+import com.intellij.execution.process.OSProcessHandler
+import com.intellij.execution.process.ProcessAdapter
+import com.intellij.execution.process.ProcessEvent
+import com.intellij.execution.process.elevation.daemon.ElevatorDaemonRuntimeClasspath
+import com.intellij.execution.process.elevation.rpc.CommandLine
+import com.intellij.execution.process.elevation.rpc.ElevatorGrpcKt.ElevatorCoroutineStub
+import com.intellij.execution.process.elevation.rpc.SpawnRequest
+import com.intellij.openapi.application.PathManager
+import com.intellij.openapi.util.Key
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.util.SystemProperties
+import io.grpc.ManagedChannel
+import io.grpc.ManagedChannelBuilder
+import kotlinx.coroutines.runBlocking
+import java.io.Closeable
+import java.io.File
+import java.io.InputStream
+import java.io.OutputStream
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+private fun startElevatorDaemon(): ElevatorClient {
+  val host = if (java.lang.Boolean.getBoolean("java.net.preferIPv6Addresses")) "::1" else "127.0.0.1"
+  val port = 50051
+
+  val daemonCommandLine = createElevatorDaemonCommandLine(host, port.toString())
+  val daemonProcessHandler = OSProcessHandler.Silent(daemonCommandLine).apply {
+    addProcessListener(object : ProcessAdapter() {
+      override fun processTerminated(event: ProcessEvent) {
+        println("Daemon exited with code ${event.exitCode}")
+      }
+
+      override fun onTextAvailable(event: ProcessEvent, outputType: Key<*>) {
+        println("Daemon [$outputType]: ${event.text}")
+      }
+    })
+  }
+  daemonProcessHandler.startNotify()
+
+  val channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build()
+  return ElevatorClient(channel)
+}
+
+private fun createElevatorDaemonCommandLine(host: String, port: String): GeneralCommandLine {
+  val elevatorClass = ElevatorDaemonRuntimeClasspath.getMainClass().name
+  val classpathClasses = ElevatorDaemonRuntimeClasspath.getClasspathClasses()
+  val classpath = classpathClasses.mapNotNullTo(LinkedHashSet()) { it.getResourcePath() }.joinToString(File.pathSeparator)
+  val javaVmExecutablePath = SystemProperties.getJavaHome() + File.separator + "bin" + File.separator + "java"
+
+  return GeneralCommandLine(javaVmExecutablePath)
+    .withParameters("-cp", classpath)
+    .withParameters(elevatorClass)
+    .withParameters(host, port)
+}
+
+private fun Class<*>.getResourcePath(): String? {
+  return FileUtil.toCanonicalPath(PathManager.getResourceRoot(this, "/" + name.replace('.', '/') + ".class"))
+}
+
+private class ElevatedProcess private constructor(
+  private val elevatorClient: ElevatorClient,
+  private val pid: Int
+) : Process() {
+  companion object {
+    fun create(elevatorClient: ElevatorClient,
+               exePath: String,
+               arguments: List<String>): ElevatedProcess {
+      val pid = runBlocking {
+        elevatorClient.spawn(exePath,
+                             arguments)
+      }
+      return ElevatedProcess(elevatorClient, pid)
+    }
+  }
+
+  override fun pid(): Long {
+    return pid.toLong()
+  }
+
+  override fun getOutputStream(): OutputStream = OutputStream.nullOutputStream()
+  override fun getInputStream(): InputStream = InputStream.nullInputStream()
+  override fun getErrorStream(): InputStream = InputStream.nullInputStream()
+
+  override fun waitFor(): Int {
+    TODO("Not yet implemented")
+  }
+
+  override fun exitValue(): Int {
+    TODO("Not yet implemented")
+  }
+
+  override fun destroy() {
+    TODO("Not yet implemented")
+  }
+}
+
+private class ElevatorClient(private val channel: ManagedChannel) : Closeable {
+  private val stub: ElevatorCoroutineStub = ElevatorCoroutineStub(channel)
+
+  suspend fun spawn(exePath: String, arguments: Iterable<String>): Int {
+    val commandLine = CommandLine.newBuilder()
+      .setExePath(exePath)
+      .addAllArguments(arguments)
+      .build()
+    val request = SpawnRequest.newBuilder().setCommandLine(commandLine).build()
+    val response = stub.spawn(request)
+    return response.pid
+  }
+
+  override fun close() {
+    channel.shutdown().awaitTermination(5, TimeUnit.SECONDS)
+  }
+}
+
+fun main() {
+  org.apache.log4j.BasicConfigurator.configure()
+  val commandLine = GeneralCommandLine("/bin/echo", "hello")
+  startElevatorDaemon().use { elevatorClient ->
+    val elevatedProcess = ElevatedProcess.create(elevatorClient,
+                                                 commandLine.exePath,
+                                                 commandLine.parametersList.list)
+    println("pid: ${elevatedProcess.pid()}")
+  }
+}
