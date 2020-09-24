38928a2ddf8f7576cae6f983cd242b191a1182d8
Daniil Tsaryov <daniil.tsaryov@jetbrains.com>
2020-09-24 11:53:15 +0300

    IDEA-251349 [git] Do not pass upstream if --root option is selected in rebase dialog
    GitOrigin-RevId: 93804ac45a5fb87dbe6771999b1aa8fc7a2dcbfc

--- plugins/git4idea/src/git4idea/rebase/GitRebaseDialog.kt
+++ plugins/git4idea/src/git4idea/rebase/GitRebaseDialog.kt
@@ -144,7 +144,7 @@ internal class GitRebaseDialog(private val project: Project,
     val branch = branchField.item
 
     val newBase = if (GitRebaseOption.ONTO in selectedOptions) ontoField.getText() else null
-    val upstream = upstreamField.getText().orEmpty()
+    val upstream = if (GitRebaseOption.ROOT !in selectedOptions) upstreamField.getText().orEmpty() else null
 
     return GitRebaseParams(gitVersion, branch, newBase, upstream, selectedOptions intersect REBASE_FLAGS)
   }
