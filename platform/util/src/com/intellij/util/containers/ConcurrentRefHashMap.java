3696438271634a036694f1b5749bebb7a0d79ca0
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-09-24 21:46:54 +0200

    avoid dependency on ContainerUtil
    GitOrigin-RevId: 9ed74d72110c0feaedef7784c5511f3aca0a4ea7

--- platform/util/src/com/intellij/util/containers/ConcurrentRefHashMap.java
+++ platform/util/src/com/intellij/util/containers/ConcurrentRefHashMap.java
@@ -1,19 +1,4 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.containers;
 
 import gnu.trove.TObjectHashingStrategy;
@@ -22,6 +7,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.lang.ref.ReferenceQueue;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 /**
@@ -71,7 +57,7 @@ abstract class ConcurrentRefHashMap<K, V> extends AbstractMap<K, V> implements C
   }
 
   // returns true if some keys were processed
-  boolean processQueue() {
+  private boolean processQueue() {
     KeyReference<K> wk;
     boolean processed = false;
     //noinspection unchecked
@@ -89,7 +75,7 @@ abstract class ConcurrentRefHashMap<K, V> extends AbstractMap<K, V> implements C
     this(DEFAULT_CAPACITY);
   }
 
-  ConcurrentRefHashMap(int initialCapacity) {
+  private ConcurrentRefHashMap(int initialCapacity) {
     this(initialCapacity, LOAD_FACTOR);
   }
 
@@ -118,7 +104,7 @@ abstract class ConcurrentRefHashMap<K, V> extends AbstractMap<K, V> implements C
                        int concurrencyLevel,
                        @NotNull TObjectHashingStrategy<? super K> hashingStrategy) {
     myHashingStrategy = hashingStrategy == THIS ? this : hashingStrategy;
-    myMap = ContainerUtil.newConcurrentMap(initialCapacity, loadFactor, concurrencyLevel);
+    myMap = new ConcurrentHashMap<>(initialCapacity, loadFactor, concurrencyLevel);
   }
 
   @Override
