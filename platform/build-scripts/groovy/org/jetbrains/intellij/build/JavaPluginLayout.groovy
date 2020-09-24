943451f8037099c60ce47be8a7d4a8091193e53c
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2020-09-24 09:29:39 +0200

    [build scripts] fixing Java plugin layout after dependency host conversion
    GitOrigin-RevId: ef46187cdd80b10f5e06322f01ac6c0a3b326c8d

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/JavaPluginLayout.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/JavaPluginLayout.groovy
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.intellij.build
 
 import org.jetbrains.intellij.build.impl.PluginLayout
@@ -69,8 +69,9 @@ class JavaPluginLayout {
       withArtifact("debugger-agent-storage", "rt")
       withProjectLibrary("Eclipse")
       withProjectLibrary("jgoodies-common")
-      withProjectLibrary("debugger-memory-agent")//todo nik: convert to module-level library instead
       withProjectLibrary("jps-javac-extension")
+
+      withModuleLibrary("debugger-memory-agent", "intellij.java.debugger.memory.agent", "")
 
       withResourceArchive("../jdkAnnotations", "lib/jdkAnnotations.jar")
       addition.delegate = delegate
