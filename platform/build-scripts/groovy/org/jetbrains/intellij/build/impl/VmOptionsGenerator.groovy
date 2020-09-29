535ebb8f35a34d4fed1f615d4115aed651df8dca
Peter Gromov <peter@jetbrains.com>
2020-09-29 14:02:27 +0200

    increase the default code cache size to 512m to reduce the probability of its overflow
    GitOrigin-RevId: 55c263fbabdf031ec2f7e9749caefe50878d0970

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/VmOptionsGenerator.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/VmOptionsGenerator.groovy
@@ -21,6 +21,8 @@ class VmOptionsGenerator {
       '-Djdk.module.illegalAccess.silent=true',
     ]
 
+  static final String defaultCodeCacheSize = '512m'
+
   static List<String> computeVmOptions(JvmArchitecture arch, boolean isEAP, ProductProperties productProperties) {
     List<String> commonVmOptions
     if (isEAP) {
@@ -36,8 +38,8 @@ class VmOptionsGenerator {
   private static List<String> vmMemoryOptions(JvmArchitecture arch, ProductProperties productProperties) {
     switch (arch) {
       // NOTE: when changing, please review usages of ProductProperties.getCustomJvmMemoryOptionsX64 and synchronize if necessary  
-      case JvmArchitecture.x32: return ['-server', '-Xms128m', '-Xmx512m', '-XX:ReservedCodeCacheSize=240m']
-      case JvmArchitecture.x64: return productProperties.customJvmMemoryOptionsX64?.split(' ')?.toList() ?: ['-Xms128m', '-Xmx750m', '-XX:ReservedCodeCacheSize=240m']
+      case JvmArchitecture.x32: return ['-server', '-Xms128m', '-Xmx512m', '-XX:ReservedCodeCacheSize=' + defaultCodeCacheSize]
+      case JvmArchitecture.x64: return productProperties.customJvmMemoryOptionsX64?.split(' ')?.toList() ?: ['-Xms128m', '-Xmx750m', '-XX:ReservedCodeCacheSize=' + defaultCodeCacheSize]
     }
     throw new AssertionError(arch)
   }
