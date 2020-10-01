7dd8b837bd05094b9aa6d87e2169b96aa0a2ff6a
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2020-10-01 14:37:21 +0300

    [git-stash] manually refresh stashes after performing actions
    GitOrigin-RevId: 3e5b7426e3e861fddb61406558e3e5f6c3f42a5b

--- plugins/git4idea/src/git4idea/stash/ui/GitStashActions.kt
+++ plugins/git4idea/src/git4idea/stash/ui/GitStashActions.kt
@@ -3,15 +3,17 @@ package git4idea.stash.ui
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DataKey
+import com.intellij.openapi.components.serviceIfCreated
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.project.Project
 import git4idea.stash.GitStashOperations
+import git4idea.stash.GitStashTracker
 import git4idea.ui.StashInfo
 
 val STASH_INFO = DataKey.create<List<StashInfo>>("GitStashInfoList")
 
 abstract class GitSingleStashAction : DumbAwareAction() {
-  abstract fun perform(project: Project, stashInfo: StashInfo)
+  abstract fun perform(project: Project, stashInfo: StashInfo): Boolean
 
   override fun update(e: AnActionEvent) {
     e.presentation.isEnabled = e.project != null && e.getData(STASH_INFO)?.size == 1
@@ -19,24 +21,26 @@ abstract class GitSingleStashAction : DumbAwareAction() {
   }
 
   override fun actionPerformed(e: AnActionEvent) {
-    perform(e.project!!, e.getRequiredData(STASH_INFO).single())
+    if (perform(e.project!!, e.getRequiredData(STASH_INFO).single())) {
+      e.project!!.serviceIfCreated<GitStashTracker>()?.scheduleRefresh()
+    }
   }
 }
 
 class GitDropStashAction : GitSingleStashAction() {
-  override fun perform(project: Project, stashInfo: StashInfo) {
-    GitStashOperations.dropStashWithConfirmation(project, null, stashInfo)
+  override fun perform(project: Project, stashInfo: StashInfo): Boolean {
+    return GitStashOperations.dropStashWithConfirmation(project, null, stashInfo)
   }
 }
 
 class GitPopStashAction : GitSingleStashAction() {
-  override fun perform(project: Project, stashInfo: StashInfo) {
-    GitStashOperations.unstash(project, stashInfo, null, true, false)
+  override fun perform(project: Project, stashInfo: StashInfo): Boolean {
+    return GitStashOperations.unstash(project, stashInfo, null, true, false)
   }
 }
 
 class GitApplyStashAction : GitSingleStashAction() {
-  override fun perform(project: Project, stashInfo: StashInfo) {
-    GitStashOperations.unstash(project, stashInfo, null, false, false)
+  override fun perform(project: Project, stashInfo: StashInfo): Boolean {
+    return GitStashOperations.unstash(project, stashInfo, null, false, false)
   }
 }
