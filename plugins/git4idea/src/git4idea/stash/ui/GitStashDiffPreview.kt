7d4f19ce064445024bf5a5bb28ed86e3abbbeafd
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2020-10-06 01:07:57 +0300

    [git-stash] fix changes navigation in diff preview
    Change#equals does not compare revisions, so it was impossible to navigate to the correct change in the stash tree with several changes for the same file. This commit adds comparison by revision.
    
    IDEA-252215
    
    GitOrigin-RevId: 82f37909bd09a3a6bbf27076a6992c4475294849

--- plugins/git4idea/src/git4idea/stash/ui/GitStashDiffPreview.kt
+++ plugins/git4idea/src/git4idea/stash/ui/GitStashDiffPreview.kt
@@ -3,6 +3,7 @@ package git4idea.stash.ui
 
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Condition
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.vcs.changes.Change
 import com.intellij.openapi.vcs.changes.ChangeViewDiffRequestProcessor
@@ -13,6 +14,7 @@ import com.intellij.ui.SideBorder
 import com.intellij.util.ui.tree.TreeUtil
 import com.intellij.vcs.log.runInEdtAsync
 import git4idea.stash.ui.GitStashUi.Companion.GIT_STASH_UI_PLACE
+import java.util.*
 import java.util.stream.Stream
 
 class GitStashDiffPreview(project: Project, private val tree: ChangesTree, parentDisposable: Disposable) :
@@ -44,11 +46,44 @@ class GitStashDiffPreview(project: Project, private val tree: ChangesTree, paren
   }
 
   override fun selectChange(change: Wrapper) {
-    val node = TreeUtil.findNodeWithObject(tree.root, change.userObject) ?: return
+    val node = TreeUtil.findNode(tree.root, Condition { sameChange(change.userObject, it.userObject) }) ?: return
     TreeUtil.selectPath(tree, TreeUtil.getPathFromRoot(node), false)
   }
 
   private fun wrap(treeModelData: VcsTreeModelData): Stream<Wrapper> {
-    return treeModelData.userObjectsStream(Change::class.java).map { ChangeWrapper(it) }
+    return treeModelData.userObjectsStream(Change::class.java).map { MyChangeWrapper(it) }
+  }
+
+  companion object {
+    private fun sameChange(change1: Any, change2: Any): Boolean {
+      if (change1 !is Change) return false
+      if (change2 !is Change) return false
+      return sameChange(change1, change2)
+    }
+
+    private fun sameChange(change1: Change, change2: Change): Boolean {
+      if (change1.beforeRevision?.file != change2.beforeRevision?.file) return false
+      if (change1.beforeRevision?.revisionNumber != change2.beforeRevision?.revisionNumber) return false
+      if (change1.afterRevision?.file != change2.afterRevision?.file) return false
+      if (change1.afterRevision?.revisionNumber != change2.afterRevision?.revisionNumber) return false
+      return true
+    }
+  }
+
+  private class MyChangeWrapper(change: Change) : ChangeWrapper(change) {
+
+    override fun equals(other: Any?): Boolean {
+      if (this === other) return true
+      if (javaClass != other?.javaClass) return false
+
+      other as MyChangeWrapper
+
+      return sameChange(change, other.change)
+    }
+
+    override fun hashCode(): Int {
+      return Objects.hash(change.beforeRevision?.file, change.beforeRevision?.revisionNumber,
+                          change.afterRevision?.file, change.afterRevision?.revisionNumber)
+    }
   }
 }
