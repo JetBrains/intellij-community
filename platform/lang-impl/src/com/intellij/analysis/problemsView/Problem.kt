c4f8dd46093752c2dc004910b550fdfe265ed9f9
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2020-10-06 12:39:49 +0300

    Use correctly colorized icon by default
    GitOrigin-RevId: 6020580304864dde6960a35a4b454e2b318cf67b

--- platform/lang-impl/src/com/intellij/analysis/problemsView/Problem.kt
+++ platform/lang-impl/src/com/intellij/analysis/problemsView/Problem.kt
@@ -1,7 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.analysis.problemsView
 
-import com.intellij.icons.AllIcons
+import com.intellij.codeHighlighting.HighlightDisplayLevel.ERROR
 import javax.swing.Icon
 
 interface Problem {
@@ -25,5 +25,5 @@ interface Problem {
    * The problems icon.
    */
   val icon: Icon
-    get() = AllIcons.General.InspectionsError
+    get() = ERROR.icon
 }
