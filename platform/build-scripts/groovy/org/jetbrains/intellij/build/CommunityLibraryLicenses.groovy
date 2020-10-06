24f8374514e81bd409638fbe686fcc96489b6efd
Vadim Lomshakov <Vadim.Lomshakov@jetbrains.com>
2020-10-06 14:03:55 +0300

    fixed library licenses test
    (cherry picked from commit 79e0c1aa311fd412659689521f827acecf90e181)
    
    Change-Id: I79e0c1aa311fd412659689521f827acecf90e181

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/CommunityLibraryLicenses.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/CommunityLibraryLicenses.groovy
@@ -675,6 +675,7 @@ class CommunityLibraryLicenses {
     jetbrainsLibrary("completion-log-events"),
     jetbrainsLibrary("completion-ranking-java"),
     jetbrainsLibrary("completion-ranking-java-exp"),
+    jetbrainsLibrary("completion-ranking-java-exp2"),
     jetbrainsLibrary("completion-ranking-kotlin"),
     jetbrainsLibrary("completion-ranking-python"),
     jetbrainsLibrary("completion-ranking-python-exp"),
