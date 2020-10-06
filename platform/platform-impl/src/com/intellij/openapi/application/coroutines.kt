6155fd4ec03e225cbb39b335a284ef18dc027547
Alexander Zolotov <zolotov@jetbrains.com>
2020-10-06 14:28:10 +0200

    [ij coroutines] run smart action in the same coroutine as it was initiated
    GitOrigin-RevId: c7268907761af2fd414e648189b40b005a46ce05

--- platform/platform-impl/src/com/intellij/openapi/application/coroutines.kt
+++ platform/platform-impl/src/com/intellij/openapi/application/coroutines.kt
@@ -6,6 +6,7 @@ package com.intellij.openapi.application
 import com.intellij.openapi.application.constraints.ConstrainedExecution.ContextConstraint
 import com.intellij.openapi.application.ex.ApplicationEx
 import com.intellij.openapi.progress.util.ProgressIndicatorUtils.runActionAndCancelBeforeWrite
+import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
 import kotlinx.coroutines.*
 import org.jetbrains.annotations.ApiStatus
@@ -35,19 +36,6 @@ suspend fun <T> readAction(action: (ctx: CoroutineContext) -> T): T {
  */
 suspend fun <T> smartReadAction(project: Project, action: (ctx: CoroutineContext) -> T): T {
   return constrainedReadAction(ReadConstraints.inSmartMode(project), action)
-}
-
-/**
- * Suspends until dumb mode is over and runs [action] in Smart Mode on EDT
- */
-suspend fun <T> smartAction(project: Project, action: (ctx: CoroutineContext) -> T): T {
-  val unsatisfied = ReadConstraints.inSmartMode(project).findUnsatisfiedConstraint()
-  if (unsatisfied != null) {
-    yieldUntilRun(unsatisfied::schedule)
-    check(unsatisfied.isCorrectContext())
-  }
-  coroutineContext.ensureActive()
-  return action(coroutineContext)
 }
 
 /**
@@ -159,5 +147,36 @@ private class ResumeContinuationRunnable(continuation: CancellableContinuation<U
 
   override fun run() {
     myContinuation?.resume(Unit)
+  }
+}
+
+/**
+ * Suspends until dumb mode is over and runs [action] in Smart Mode on EDT
+ */
+suspend fun <T> smartAction(project: Project, action: (ctx: CoroutineContext) -> T): T {
+  return suspendCancellableCoroutine { continuation ->
+    DumbService.getInstance(project).runWhenSmart(SmartRunnable(action, continuation))
+  }
+}
+
+private class SmartRunnable<T>(action: (ctx: CoroutineContext) -> T, continuation: CancellableContinuation<T>) : Runnable {
+
+  @Volatile
+  private var myAction: ((ctx: CoroutineContext) -> T)? = action
+
+  @Volatile
+  private var myContinuation: CancellableContinuation<T>? = continuation
+
+  init {
+    continuation.invokeOnCancellation {
+      myAction = null
+      myContinuation = null // it's not possible to unschedule the runnable, so we make it do nothing instead
+    }
+  }
+
+  override fun run() {
+    val continuation = myContinuation ?: return
+    val action = myAction ?: return
+    continuation.resume(action.invoke(continuation.context))
   }
 }
