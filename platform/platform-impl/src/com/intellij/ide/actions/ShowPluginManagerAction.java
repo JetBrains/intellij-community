4329b81e69f6d2da88dd780fe97869e5cadbe223
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2020-10-07 16:20:17 +0300

    [dynamic plugins] ProjectUtil.currentOrDefaultProject extracted #RIDER-51964
    GitOrigin-RevId: f576091a84e9b943a642d2383400d65ab1d6e747

--- platform/platform-impl/src/com/intellij/ide/actions/ShowPluginManagerAction.java
+++ platform/platform-impl/src/com/intellij/ide/actions/ShowPluginManagerAction.java
@@ -10,16 +10,14 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.options.ShowSettingsUtil;
 import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.project.ProjectUtil;
 import org.jetbrains.annotations.NotNull;
 
 public class ShowPluginManagerAction extends AnAction implements DumbAware {
   @Override
   public void actionPerformed(@NotNull AnActionEvent e) {
-    Project actionProject = e.getProject();
     ShowSettingsUtil.getInstance().showSettingsDialog(
-      actionProject != null ? actionProject : ProjectManager.getInstance().getDefaultProject(),
+      ProjectUtil.currentOrDefaultProject(e.getProject()),
       PluginManagerConfigurable.class
     );
   }
