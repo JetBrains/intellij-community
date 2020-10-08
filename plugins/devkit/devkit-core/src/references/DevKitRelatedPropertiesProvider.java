9b5e551a5b73b863b080a3758454373f8dfcfbea
Yann Cébron <yann.cebron@jetbrains.com>
2020-10-08 17:09:47 +0200

    [devkit] DevKitRelatedPropertiesProvider: performance fix
    GitOrigin-RevId: a428a9b3e426be3fe19fc381ae10a25465250984

--- plugins/devkit/devkit-core/src/references/DevKitRelatedPropertiesProvider.java
+++ plugins/devkit/devkit-core/src/references/DevKitRelatedPropertiesProvider.java
@@ -7,14 +7,17 @@ import com.intellij.lang.properties.psi.PropertiesFile;
 import com.intellij.lang.properties.psi.Property;
 import com.intellij.navigation.GotoRelatedItem;
 import com.intellij.openapi.editor.markup.GutterIconRenderer;
+import com.intellij.openapi.util.NotNullLazyValue;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
-import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.LocalSearchScope;
 import com.intellij.psi.search.searches.ReferencesSearch;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.xml.XmlElement;
 import com.intellij.psi.xml.XmlToken;
 import com.intellij.psi.xml.XmlTokenType;
+import com.intellij.util.Query;
+import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.xml.DomElement;
 import com.intellij.util.xml.DomUtil;
 import com.intellij.util.xml.GenericDomValue;
@@ -29,7 +32,6 @@ import org.jetbrains.idea.devkit.util.DescriptorI18nUtil;
 import javax.swing.*;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.function.Function;
 
 /**
  * Provides gutter icon/goto related navigation for implicit message keys in {@code plugin.xml}.
@@ -94,17 +96,20 @@ public class DevKitRelatedPropertiesProvider extends DevkitRelatedLineMarkerProv
     PropertiesFile file = DescriptorI18nUtil.findBundlePropertiesFile(domElement);
     if (file == null) return;
 
-    Collection<PsiElement> properties =
-      ReferencesSearch.search(valueXmlElement, GlobalSearchScope.fileScope(file.getContainingFile()))
-        .mapping((Function<PsiReference, PsiElement>)reference -> PsiTreeUtil.getParentOfType(reference.getElement(), Property.class))
-        .findAll();
-    if (properties.isEmpty()) return;
+    final Query<PsiReference> query = ReferencesSearch.search(valueXmlElement, new LocalSearchScope(file.getContainingFile()));
+    if (query.findFirst() == null) return;
 
     result.add(
       NavigationGutterIconBuilder.create(DevkitIcons.Gutter.Properties,
                                          e -> Collections.singletonList(((PsiElement)e)),
                                          e -> Collections.singletonList(new GotoRelatedItem((PsiElement)e)))
-        .setTargets(properties)
+        .setTargets(new NotNullLazyValue<>() {
+          @Override
+          protected @NotNull Collection<PsiElement> compute() {
+            return ContainerUtil.map(query.findAll(),
+                                     reference -> PsiTreeUtil.getParentOfType(reference.getElement(), Property.class));
+          }
+        })
         .setPopupTitle(DevKitBundle.message("line.marker.related.property.popup.title"))
         .setTooltipText(DevKitBundle.message("line.marker.related.property.tooltip"))
         .setAlignment(GutterIconRenderer.Alignment.RIGHT)
