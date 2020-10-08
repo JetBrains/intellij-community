072a1f171f54fd93777b4f0315671e6e329176a6
Dmitry Matveev <dmitry.matveev@jetbrains.com>
2020-10-08 20:51:57 +0200

    Updated shortcuts. Fix tests
    GitOrigin-RevId: f31cac919144bd9f735fd1b0bb6a3ff4926fe799

--- platform/testFramework/extensions/src/com/intellij/keymap/KeymapsTestCase.java
+++ platform/testFramework/extensions/src/com/intellij/keymap/KeymapsTestCase.java
@@ -441,6 +441,7 @@ public abstract class KeymapsTestCase extends KeymapsTestCaseBase {
     "IDEtalk.SearchUserHistory", "IDEtalk.Rename", "CIDR.Lang.Cpp.GenerateDefinitions",
     "ActivateErrorsInSolutionToolWindow", "ActivateUnitTestsToolWindow", "ActivateInspectionResultsToolWindow",
     "ActivateDatabaseToolWindow", "ActivateBuildToolWindow", "ActivateNuGetToolWindow", "RiderBackendAction-EncapsulateField",
+    "CIDR.Lang.SwitchHeaderSource", "SwitchHeaderSource", "BuildProject", "RebuildProject", "BuildSolutionAction", "RebuildSolutionAction",
     ""
   );
 
