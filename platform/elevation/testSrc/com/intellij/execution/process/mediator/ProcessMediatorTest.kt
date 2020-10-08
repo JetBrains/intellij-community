a756ee2830c90efaedd75ee74c028f2b0e95a48b
Vasily Romanikhin <vasily.romanikhin@jetbrains.com>
2020-10-08 12:13:01 +0300

    CPP-22344: tests: destroy process
    GitOrigin-RevId: c61d39ef6cd1d8723f7ff9713b6854e0efdbe0eb

--- platform/elevation/testSrc/com/intellij/execution/process/mediator/ProcessMediatorTest.kt
+++ platform/elevation/testSrc/com/intellij/execution/process/mediator/ProcessMediatorTest.kt
@@ -5,8 +5,7 @@ import com.intellij.execution.process.mediator.daemon.ProcessMediatorDaemon
 import com.intellij.execution.process.mediator.rt.MediatedProcessTestMain
 import kotlinx.coroutines.CoroutineScope
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.util.concurrent.TimeUnit
@@ -51,6 +50,37 @@ internal class ProcessMediatorTest {
     val hasExited = process.waitFor(TIMEOUT_MS, TimeUnit.MILLISECONDS)
     assertTrue(hasExited)
     assertEquals(42, process.exitValue())
+  }
+
+  @Test
+  internal fun `destroy process with endless loop`() {
+    val process = createProcessBuilderForJavaClass(MediatedProcessTestMain.Loop::class)
+      .startMediatedProcess()
+    val pid = process.pid()
+    assertTrue(process.isAlive)
+    assertTrue(ProcessHandle.of(pid).isPresent)
+
+    process.destroy()
+
+    val hasExited = process.waitFor(TIMEOUT_MS, TimeUnit.MILLISECONDS)
+    assertTrue(hasExited)
+    assertTrue(ProcessHandle.of(pid).isEmpty)
+  }
+
+  @Test
+  internal fun `destroy exited process`() {
+    val process = createProcessBuilderForJavaClass(MediatedProcessTestMain.True::class)
+      .startMediatedProcess()
+
+    val hasExited = process.waitFor(TIMEOUT_MS, TimeUnit.MILLISECONDS)
+    assertTrue(hasExited)
+    assertTrue(ProcessHandle.of(process.pid()).isEmpty)
+    assertFalse(process.isAlive)
+
+    process.destroy()
+
+    assertTrue(process.waitFor(TIMEOUT_MS, TimeUnit.MILLISECONDS))
+    assertFalse(process.isAlive)
   }
 
   private fun ProcessBuilder.startMediatedProcess(): Process {
