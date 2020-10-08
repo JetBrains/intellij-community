cc57ea97665a4f1dfa4b5e254ce7bad76345c764
Vasily Romanikhin <vasily.romanikhin@jetbrains.com>
2020-10-08 16:42:22 +0300

    CPP-22344: tests: echo process
    (cherry picked from commit c77d93ba9cd987b4a4e693b2a0fd075afcf3ced0)
    
    IJ-CR-3496
    
    GitOrigin-RevId: 8496ddbcf256f533c9c8491e9d90c8cf6f1a0e1d

--- platform/elevation/testSrc/com/intellij/execution/process/mediator/rt/MediatedProcessTestMain.kt
+++ platform/elevation/testSrc/com/intellij/execution/process/mediator/rt/MediatedProcessTestMain.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.process.mediator.rt
 
+import java.util.*
 import kotlin.system.exitProcess
 
 class MediatedProcessTestMain {
@@ -23,6 +24,16 @@ class MediatedProcessTestMain {
     fun main(args: Array<String>) {
       while (true) {
         Thread.sleep(500)
+      }
+    }
+  }
+
+  object Echo {
+    @JvmStatic
+    fun main(args: Array<String>) {
+      val scanner = Scanner(System.`in`)
+      while (scanner.hasNextLine()) {
+        println(scanner.nextLine())
       }
     }
   }
