d1b1ed51fa14198902f800266ea801e6081bbc63
Maksim Pelevin <Maksim.Pelevin@jetbrains.com>
2020-10-09 16:44:22 +0300

    'Add icons for snapshots UX-1535' from 13a90d521 from git@github.com:JetBrains/IntelliJIcons.git
    GitOrigin-RevId: 24b3711b0ac3eb30d99d624ef4b51bd9ead6b2c5

--- platform/util/src/com/intellij/icons/AllIcons.java
+++ platform/util/src/com/intellij/icons/AllIcons.java
@@ -355,6 +355,7 @@ public class AllIcons {
     /** 16x16 */ public static final @NotNull Icon Custom = load("/fileTypes/custom.svg", -9085020248910575542L, 0);
     /** 16x16 */ public static final @NotNull Icon Diagram = load("/fileTypes/diagram.svg", 2878252828662988838L, 0);
     /** 16x16 */ public static final @NotNull Icon Dtd = load("/fileTypes/dtd.svg", 1512744705207344987L, 0);
+    /** 16x16 */ public static final @NotNull Icon Hprof = load("/fileTypes/hprof.svg", -917121733692257803L, 0);
     /** 16x16 */ public static final @NotNull Icon Htaccess = load("/fileTypes/htaccess.svg", 8172243446327886417L, 0);
     /** 16x16 */ public static final @NotNull Icon Html = load("/fileTypes/html.svg", -3878444809017733799L, 0);
     /** 16x16 */ public static final @NotNull Icon Idl = load("/fileTypes/idl.svg", -3673761919827134848L, 0);
@@ -362,6 +363,7 @@ public class AllIcons {
     /** 16x16 */ public static final @NotNull Icon JavaClass = load("/fileTypes/javaClass.svg", 1537034655787663158L, 0);
     /** 16x16 */ public static final @NotNull Icon JavaOutsideSource = load("/fileTypes/javaOutsideSource.svg", -1332262268381891224L, 0);
     /** 16x16 */ public static final @NotNull Icon JavaScript = load("/fileTypes/javaScript.svg", -4186188120721108429L, 0);
+    /** 16x16 */ public static final @NotNull Icon Jfr = load("/fileTypes/jfr.svg", 1504098668566506408L, 0);
     /** 16x16 */ public static final @NotNull Icon Json = load("/fileTypes/json.svg", 7047330769488036116L, 2);
     /** 16x16 */ public static final @NotNull Icon JsonSchema = load("/fileTypes/jsonSchema.svg", -637530520441570990L, 2);
     /** 16x16 */ public static final @NotNull Icon Jsp = load("/fileTypes/jsp.svg", 3865835302853651631L, 0);
