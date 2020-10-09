7639aa5fd34ed3ccad619fe3153f637d8d72d088
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2020-10-09 18:37:15 +0200

    Revert "Add ltedit.sh (.bat) script to IDE bin directory"
    This reverts commit 871c47f1 to build installers.
    
    GitOrigin-RevId: 99dde921988dad5be0a03a2d7fc5520b759ca07f

--- platform/build-scripts/resources/mac/scripts/ltedit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# ------------------------------------------------------
-# @@product_full@@ LightEdit mode script.
-# ------------------------------------------------------
-
-IDE_BIN_HOME="${0%/*}"
-exec "$IDE_BIN_HOME/../MacOS/@@script_name@@" -e "$@"
