6f9f3838f06cc8ed0a5d355d62085e8078e6a3a0
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2020-10-12 17:09:31 +0300

    test runner: get rid of Atomic* classes to make code clearer (IDEA-CR-67149)
    GitOrigin-RevId: 6dfff48eb6adabdd4c60d77ed562c9a9f32f70f2

--- platform/smRunner/src/com/intellij/execution/testframework/sm/ServiceMessageUtil.java
+++ platform/smRunner/src/com/intellij/execution/testframework/sm/ServiceMessageUtil.java
@@ -9,8 +9,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.text.ParseException;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class ServiceMessageUtil {
   private static final Logger LOG = Logger.getInstance(ServiceMessageUtil.class);
@@ -22,32 +20,43 @@ public class ServiceMessageUtil {
     if (text.startsWith(ServiceMessage.SERVICE_MESSAGE_START) && text.endsWith(ServiceMessage.SERVICE_MESSAGE_END)) {
       ServiceMessagesParser parser = new ServiceMessagesParser();
       parser.setValidateRequiredAttributes(validateRequiredAttributes);
-      AtomicReference<ServiceMessage> messageRef = new AtomicReference<>();
-      AtomicBoolean exceptionOccurred = new AtomicBoolean(false);
-      parser.parse(text, new ServiceMessageParserCallback() {
-        @Override
-        public void regularText(@NotNull String regularText) {
-          LOG.error("Regular text encountered when parsing service message", text, regularText);
-        }
-
-        @Override
-        public void serviceMessage(@NotNull ServiceMessage message) {
-          if (messageRef.get() != null) {
-            LOG.error("Another service message already parsed", text, messageRef.get().asString(), message.asString());
-          }
-          messageRef.set(message);
-        }
-
-        @Override
-        public void parseException(@NotNull ParseException parseException, @NotNull String text1) {
-          LOG.error("Failed to parse service message", parseException, text1);
-          exceptionOccurred.set(true);
-        }
-      });
-      if (!exceptionOccurred.get()) {
-        return messageRef.get();
-      }
+      MyServiceMessageParserCallback callback = new MyServiceMessageParserCallback(text);
+      parser.parse(text, callback);
+      return callback.getParsedMessage();
     }
     return null;
+  }
+
+  private static class MyServiceMessageParserCallback implements ServiceMessageParserCallback {
+    private @NotNull final String myText;
+    private ServiceMessage myMessage;
+    private boolean myParseFailed;
+
+    MyServiceMessageParserCallback(@NotNull String text) {
+      myText = text;
+    }
+
+    @Override
+    public void regularText(@NotNull String regularText) {
+      LOG.error("Regular text encountered when parsing service message", myText, regularText);
+    }
+
+    @Override
+    public void serviceMessage(@NotNull ServiceMessage message) {
+      if (myMessage != null) {
+        LOG.error("Another service message already parsed", myText, myMessage.asString(), message.asString());
+      }
+      myMessage = message;
+    }
+
+    @Override
+    public void parseException(@NotNull ParseException parseException, @NotNull String parseErrorText) {
+      LOG.error("Failed to parse service message", parseException, parseErrorText);
+      myParseFailed = true;
+    }
+
+    public @Nullable ServiceMessage getParsedMessage() {
+      return myParseFailed ? null : myMessage;
+    }
   }
 }
