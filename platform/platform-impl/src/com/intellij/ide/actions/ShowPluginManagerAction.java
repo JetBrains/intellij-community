1e875ce68555e847b49e140bcddf32a807af27f9
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2020-10-12 13:19:13 +0300

    [dynamic plugins] default project settings should be available at the welcome screen #RIDER-51964 fixed
    GitOrigin-RevId: d98a2529d7b7346651e63200149ac576f46915a1

--- platform/platform-impl/src/com/intellij/ide/actions/ShowPluginManagerAction.java
+++ platform/platform-impl/src/com/intellij/ide/actions/ShowPluginManagerAction.java
@@ -10,11 +10,17 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.options.ShowSettingsUtil;
 import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
 import org.jetbrains.annotations.NotNull;
 
 public class ShowPluginManagerAction extends AnAction implements DumbAware {
   @Override
   public void actionPerformed(@NotNull AnActionEvent e) {
-    ShowSettingsUtil.getInstance().showSettingsDialog(e.getProject(), PluginManagerConfigurable.class);
+    Project actionProject = e.getProject();
+    ShowSettingsUtil.getInstance().showSettingsDialog(
+      actionProject != null ? actionProject : ProjectManager.getInstance().getDefaultProject(),
+      PluginManagerConfigurable.class
+    );
   }
 }
