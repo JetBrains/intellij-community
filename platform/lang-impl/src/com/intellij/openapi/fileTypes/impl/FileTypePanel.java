96b90e36fa018e141bb4b8d57df06ea6cd9a557f
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2020-10-12 18:04:43 +0300

    OS file type association context help label [UX-1384]
    GitOrigin-RevId: a04acc0a8ac87d1c432dbbe4f74a4481ea60ac8e

--- platform/lang-impl/src/com/intellij/openapi/fileTypes/impl/FileTypePanel.java
+++ platform/lang-impl/src/com/intellij/openapi/fileTypes/impl/FileTypePanel.java
@@ -1,6 +1,9 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.fileTypes.impl;
 
+import com.intellij.openapi.application.ApplicationInfo;
+import com.intellij.openapi.fileTypes.FileTypesBundle;
+import com.intellij.ui.ContextHelpLabel;
 import com.intellij.ui.components.JBLabel;
 
 import javax.swing.*;
@@ -14,4 +17,11 @@ class FileTypePanel {
   JPanel myAssociatePanel;
   JPanel myRightPanel;
   JBLabel myAssociateMessageLabel;
+  @SuppressWarnings("unused") private JLabel myAssociateContextHelpLabel;
+
+  private void createUIComponents() {
+    myAssociateContextHelpLabel = ContextHelpLabel.create(
+      FileTypesBundle.message("filetype.associate.context.help.text", ApplicationInfo.getInstance().getFullApplicationName())
+    );
+  }
 }
