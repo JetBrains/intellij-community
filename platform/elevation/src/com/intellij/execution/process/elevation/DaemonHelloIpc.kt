7c3dcd1d49ebb1120df57b01497439ba63b757aa
Eldar Abusalimov <Eldar.Abusalimov@jetbrains.com>
2020-10-15 01:33:10 +0300

    CPP-22344: DaemonHello: Replace ad-hoc serializer/parser with protobuf
    (cherry picked from commit 9dbf952daeb0060a954a85eefcb24f644419b926)
    
    IJ-CR-3496
    
    GitOrigin-RevId: 7bd178cf310049ef7c8798c691aa4c6b7526ddd4

--- platform/elevation/src/com/intellij/execution/process/elevation/DaemonHelloIpc.kt
+++ platform/elevation/src/com/intellij/execution/process/elevation/DaemonHelloIpc.kt
@@ -1,23 +1,17 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.process.elevation
 
-import com.google.protobuf.ByteString
 import com.intellij.execution.configurations.GeneralCommandLine
-import com.intellij.execution.process.ProcessAdapter
-import com.intellij.execution.process.ProcessEvent
-import com.intellij.execution.process.ProcessListener
-import com.intellij.execution.process.ProcessOutputType
-import com.intellij.execution.process.mediator.ChannelInputStream
+import com.intellij.execution.process.*
+import com.intellij.execution.process.mediator.rpc.DaemonHello
 import com.intellij.execution.util.ExecUtil
 import com.intellij.openapi.util.Key
 import com.intellij.openapi.util.io.FileUtil
 import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.ClosedSendChannelException
-import kotlinx.coroutines.channels.sendBlocking
 import java.io.Closeable
 import java.io.IOException
 import java.io.InputStream
+import java.io.Reader
 import java.net.ServerSocket
 import java.nio.file.Files
 import java.nio.file.Path
@@ -25,46 +19,44 @@ import java.nio.file.StandardOpenOption.READ
 import java.nio.file.StandardOpenOption.WRITE
 import java.util.concurrent.CopyOnWriteArrayList
 
-data class DaemonHello(val port: Int) {
-  companion object {
-    fun parseLines(lineSequence: Sequence<String>): DaemonHello {
-      val lines = lineSequence.take(1).toList()
-      return parseLines(lines)
-    }
-
-    private fun parseLines(lines: List<String>): DaemonHello {
-      return try {
-        val port = lines.first().toInt()
-        DaemonHello(port)
-      }
-      catch (e: Exception) {
-        when (e) {
-          is NoSuchElementException,
-          is NumberFormatException -> throw IOException("Unable to read daemon hello", e)
-          else -> throw e
-        }
-      }
-    }
-  }
-}
-
-internal interface DaemonHelloIpc : ProcessListener, Closeable {
+internal interface DaemonHelloIpc : Closeable {
   fun patchDaemonCommandLine(daemonCommandLine: GeneralCommandLine): GeneralCommandLine
+  fun createDaemonProcessHandler(daemonCommandLine: GeneralCommandLine): BaseOSProcessHandler
   fun consumeDaemonHello(): DaemonHello
 }
 
-internal abstract class AbstractInputStreamDaemonHelloIpc(coroutineScope: CoroutineScope) : DaemonHelloIpc, ProcessAdapter(),
-                                                                                            CoroutineScope by coroutineScope.childScope() {
+internal abstract class AbstractInputStreamDaemonHelloIpc(
+  coroutineScope: CoroutineScope
+) : DaemonHelloIpc, ProcessAdapter(),
+    CoroutineScope by coroutineScope.childScope() {
+
   private val cleanupList = CopyOnWriteArrayList<Closeable>()
 
-  protected abstract fun openInputStream(): InputStream
+  init {
+    // TODO[eldar] revise the usage of invokeOnCompletion():
+    //   **Note**: Implementation of `CompletionHandler` must be fast, non-blocking, and thread-safe.
+    //   This handler can be invoked concurrently with the surrounding code.
+    //   There is no guarantee on the execution context in which the [handler] is invoked.
+    @Suppress("LeakingThis")
+    coroutineContext[Job]!!.invokeOnCompletion { close() }
+  }
+
+  final override fun createDaemonProcessHandler(daemonCommandLine: GeneralCommandLine): BaseOSProcessHandler {
+    return createProcessHandler(daemonCommandLine).also {
+      it.addProcessListener(this)
+    }
+  }
+
+  protected open fun createProcessHandler(daemonCommandLine: GeneralCommandLine): BaseOSProcessHandler {
+    return OSProcessHandler.Silent(daemonCommandLine)
+  }
 
   override fun consumeDaemonHello(): DaemonHello {
     return try {
       ensureActive()
-      openInputStream().reader(Charsets.UTF_8).buffered().use { reader ->
+      openInputStream().use { inputStream ->
         ensureActive()
-        DaemonHello.parseLines(reader.lineSequence())
+        DaemonHello.parseDelimitedFrom(inputStream)
       }
     }
     catch (e: CancellationException) {
@@ -72,8 +64,16 @@ internal abstract class AbstractInputStreamDaemonHelloIpc(coroutineScope: Corout
     }
   }
 
+  protected abstract fun openInputStream(): InputStream
+
+  override fun onTextAvailable(event: ProcessEvent, outputType: Key<*>) {
+    ElevationLogger.LOG.info("Daemon [$outputType]: ${event.text}")
+  }
+
   override fun processTerminated(event: ProcessEvent) {
-    cancel("Process terminated with exit code ${event.exitCode}")
+    val message = "Daemon process terminated with exit code ${event.exitCode}"
+    ElevationLogger.LOG.info(message)
+    cancel(message)
     close()
   }
 
@@ -103,41 +103,27 @@ internal abstract class AbstractInputStreamDaemonHelloIpc(coroutineScope: Corout
 
 internal open class ProcessOutputBasedDaemonHelloIpc(coroutineScope: CoroutineScope) : AbstractInputStreamDaemonHelloIpc(coroutineScope),
                                                                                        ProcessListener {
-  private val stdoutChannel = Channel<ByteString>(capacity = Channel.BUFFERED)
-
-  override fun openInputStream() = ChannelInputStream(stdoutChannel)
+  private lateinit var stdoutStream: InputStream
 
   override fun patchDaemonCommandLine(daemonCommandLine: GeneralCommandLine) = daemonCommandLine.apply {
     addParameters("--hello-file", "-")
   }
 
-  override fun onTextAvailable(event: ProcessEvent, outputType: Key<*>) {
-    if (outputType == ProcessOutputType.STDOUT) {
-      // Don't wanna rely on whether it is always complete lines or not, also it's easier to reuse ChannelInputStream
-      val byteString = ByteString.copyFrom(event.text, Charsets.UTF_8)
-      try {
-        stdoutChannel.sendBlocking(byteString)
+  override fun createProcessHandler(daemonCommandLine: GeneralCommandLine): BaseOSProcessHandler {
+    return object : OSProcessHandler.Silent(daemonCommandLine) {
+      override fun createProcessOutReader(): Reader {
+        return Reader.nullReader()
       }
-      catch (e: ClosedSendChannelException) {
-        event.processHandler.removeProcessListener(this)
-      }
+    }.also {
+      stdoutStream = it.process.inputStream
     }
   }
 
-  override fun startNotified(event: ProcessEvent) = Unit
-
-  override fun close() {
-    try {
-      stdoutChannel.close()
-    }
-    finally {
-      super.close()
-    }
-  }
+  override fun openInputStream() = stdoutStream
 }
 
 internal open class FileBasedDaemonHelloIpc(coroutineScope: CoroutineScope,
-                                            private val path: Path) : AbstractInputStreamDaemonHelloIpc(coroutineScope) {
+                                            protected val path: Path) : AbstractInputStreamDaemonHelloIpc(coroutineScope) {
   override fun patchDaemonCommandLine(daemonCommandLine: GeneralCommandLine) = daemonCommandLine.apply {
     addParameters("--hello-file", path.toAbsolutePath().toString())
   }
@@ -150,10 +136,10 @@ internal class UnixFifoDaemonHelloIpc private constructor(coroutineScope: Corout
   init {
     // We need to open the write end of the pipe to ensure opening the pipe for reading would not block indefinitely
     // in case the real daemon process doesn't open in for writing (for example, if it fails to start)
-    async(Dispatchers.IO) {
-      Files.newByteChannel(path, READ, WRITE).also(::registerCloseable)
-    }
+    Files.newByteChannel(path, READ, WRITE).also(::registerCloseable)
   }
+
+  override fun openInputStream(): InputStream = Files.newInputStream(path)
 
   companion object {
     fun create(coroutineScope: CoroutineScope): UnixFifoDaemonHelloIpc {
@@ -165,7 +151,11 @@ internal class UnixFifoDaemonHelloIpc private constructor(coroutineScope: Corout
         .checkSuccess(ElevationLogger.LOG).also { success ->
           if (!success) throw IOException("Unable to create fifo $path")
         }
-      return UnixFifoDaemonHelloIpc(coroutineScope, path)
+      return UnixFifoDaemonHelloIpc(coroutineScope, path).also {
+        it.registerCloseable {
+          FileUtil.delete(path)
+        }
+      }
     }
   }
 }
