19799a89bde5f1f8e09ea0b1bde0f8129d00388e
Dmitriy.Panov <Dmitriy.Panov@jetbrains.com>
2020-10-14 03:57:56 +0300

    IDEA-252826 donate notification dropped
    GitOrigin-RevId: 281d0f3bd48f248571170f24723fe1a3ce57a52f

--- plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/Version.java
+++ plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/Version.java
@@ -3,10 +3,6 @@ package de.plushnikov.intellij.plugin;
 public interface Version {
   String PLUGIN_NAME = "Lombok plugin";
   /**
-   * Current plugin version.
-   */
-  String PLUGIN_VERSION = "0.33";
-  /**
    * Current version of lombok plugin
    */
   String LAST_LOMBOK_VERSION = "1.18.12";
