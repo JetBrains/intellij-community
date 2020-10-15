ede0eb875f0a369a0135d1991ed105c801d09e50
Dmitry Jemerov <yole@jetbrains.com>
2020-10-15 12:02:16 +0200

    Empty state for Inspection Results toolwindow
    GitOrigin-RevId: 4ccd5dab3ec08524e0f3ae3700c8e5995b501e0c

---
+++ platform/lang-impl/src/com/intellij/codeInspection/ui/actions/ActivateInspectionResultsToolWindowAction.kt
@@ -0,0 +1,51 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.codeInspection.ui.actions
+
+import com.intellij.codeInspection.InspectionManager
+import com.intellij.codeInspection.InspectionsBundle
+import com.intellij.codeInspection.ex.InspectionManagerEx
+import com.intellij.icons.AllIcons
+import com.intellij.ide.DataManager
+import com.intellij.ide.IdeBundle
+import com.intellij.ide.actions.ToolWindowEmptyStateAction
+import com.intellij.idea.ActionsBundle
+import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.actionSystem.ActionPlaces
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.IdeActions
+import com.intellij.openapi.actionSystem.impl.SimpleDataContext
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.help.HelpManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindowId
+import com.intellij.ui.SimpleTextAttributes
+import com.intellij.util.ui.StatusText
+import org.jetbrains.annotations.Nls
+
+class ActivateInspectionResultsToolWindowAction : ToolWindowEmptyStateAction(ToolWindowId.INSPECTION,
+                                                                             AllIcons.Toolwindows.ToolWindowInspection) {
+  override fun setupEmptyText(project: Project, text: StatusText) {
+    text.appendText(InspectionsBundle.message("inspections.empty.state.1") + " ")
+    appendActionLink(text, project, IdeActions.ACTION_INSPECT_CODE,
+                     ActionsBundle.actionText(IdeActions.ACTION_INSPECT_CODE).replace("_", ""))
+    text.appendText(" " + InspectionsBundle.message("inspections.empty.state.2") + " ")
+    appendActionLink(text, project, "RunInspection", IdeBundle.message("goto.inspection.action.text").replace("&", ""))
+    text.appendLine("")
+    text.appendLine(AllIcons.General.ContextHelp, InspectionsBundle.message("inspections.empty.state.help"), SimpleTextAttributes.LINK_ATTRIBUTES) {
+      HelpManager.getInstance().invokeHelp("procedures.inspecting.running")
+    }
+  }
+
+  private fun appendActionLink(text: StatusText, project: Project, actionId: String, @Nls actionText: String) {
+    text.appendText(actionText, SimpleTextAttributes.LINK_ATTRIBUTES) {
+      val dataContext = FileEditorManager.getInstance(project).selectedEditor?.let { DataManager.getInstance().getDataContext(it.component) }
+                        ?: SimpleDataContext.getProjectContext(project)
+      val event = AnActionEvent.createFromDataContext(ActionPlaces.UNKNOWN, null, dataContext)
+      ActionManager.getInstance().getAction(actionId).actionPerformed(event)
+    }
+  }
+
+  override fun ensureToolWindowCreated(project: Project) {
+    (InspectionManager.getInstance(project) as InspectionManagerEx).contentManager.value
+  }
+}
