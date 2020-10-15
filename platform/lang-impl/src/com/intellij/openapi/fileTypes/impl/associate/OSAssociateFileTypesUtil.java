983d4c98ca7cf0e84c874c3f33ffa9775ec5dcac
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2020-10-15 13:28:56 +0300

    IDEA-247175 IDE does not remember file associations when updating to the next version
    GitOrigin-RevId: ebe497bc85440d23cf97c465b7985ba868e73005

--- platform/lang-impl/src/com/intellij/openapi/fileTypes/impl/associate/OSAssociateFileTypesUtil.java
+++ platform/lang-impl/src/com/intellij/openapi/fileTypes/impl/associate/OSAssociateFileTypesUtil.java
@@ -12,6 +12,7 @@ import com.intellij.openapi.util.NlsContexts;
 import com.intellij.openapi.util.NlsSafe;
 import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.util.ObjectUtils;
 import com.intellij.util.containers.ContainerUtil;
 import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.Nls;
@@ -29,6 +30,7 @@ public class OSAssociateFileTypesUtil {
   public final static String ENABLE_REG_KEY = "system.file.type.associations.enabled";
 
   private static final Logger LOG = Logger.getInstance(OSAssociateFileTypesUtil.class);
+  public static final String EXTENSION_SEPARATOR = "-x-";
 
   private OSAssociateFileTypesUtil() {
   }
@@ -39,24 +41,73 @@ public class OSAssociateFileTypesUtil {
       FileTypeAssociationDialog dialog = new FileTypeAssociationDialog();
       if (dialog.showAndGet()) {
         List<FileType> fileTypes = dialog.getSelectedFileTypes();
-        if (fileTypes.size() > 0) {
-          ApplicationManager.getApplication().executeOnPooledThread(
-            () -> {
-              try {
-                callback.beforeStart();
-                associator.associateFileTypes(fileTypes);
-                OSFileAssociationPreferences.getInstance().updateFileTypes(fileTypes);
-                OSFileAssociationFeatureUsagesUtil.logFilesAssociated(fileTypes);
-                callback.onSuccess(associator.isOsRestartRequired());
-              }
-              catch (OSFileAssociationException exception) {
-                callback.onFailure(exception.getMessage());
-                LOG.info(exception);
+        doAssociate(callback, associator, fileTypes);
+        OSFileAssociationPreferences.getInstance().updateFileTypes(fileTypes);
+      }
+    }
+  }
+
+  public static void restoreAssociations(@NotNull Callback callback) {
+    SystemFileTypeAssociator associator = SystemAssociatorFactory.getAssociator();
+    if (associator != null) {
+      List<FileType> savedFileTypes = restoreFileTypes();
+      if (!savedFileTypes.isEmpty()) {
+        doAssociate(callback, associator, savedFileTypes);
+      }
+    }
+  }
+
+  public static List<FileType> restoreFileTypes() {
+    List<FileType> fileTypes = new ArrayList<>();
+    OSFileAssociationPreferences.getInstance().fileTypeNames.forEach(
+      name -> {
+        if (name.contains(EXTENSION_SEPARATOR)) {
+          int extPos = name.indexOf(EXTENSION_SEPARATOR);
+          String originalName = name.substring(0, extPos);
+          String extension =  name.substring(extPos + EXTENSION_SEPARATOR.length());
+          FileType originalType = getFileTypeByName(originalName);
+          if (originalType != null) {
+            for (FileNameMatcher matcher : FileTypeManager.getInstance().getAssociations(originalType)) {
+              if (matcher instanceof ExtensionFileNameMatcher) {
+                if (extension.equals(((ExtensionFileNameMatcher)matcher).getExtension())) {
+                  fileTypes.add(new MyFileSubtype(originalType, matcher, name, matcher.getPresentableString()));
+                }
               }
             }
-          );
+          }
+        }
+        else {
+          ObjectUtils.consumeIfNotNull(getFileTypeByName(name), fileType -> fileTypes.add(fileType));
         }
       }
+    );
+    return fileTypes;
+  }
+
+  private static @Nullable FileType getFileTypeByName(@NotNull String name) {
+    for (FileType fileType : FileTypeManager.getInstance().getRegisteredFileTypes()) {
+      if (name.equals(fileType.getName())) return fileType;
+    }
+    LOG.warn(name + " file type not found.");
+    return null;
+  }
+
+  private static void doAssociate(@NotNull Callback callback, @NotNull SystemFileTypeAssociator associator, List<FileType> fileTypes) {
+    if (fileTypes.size() > 0) {
+      ApplicationManager.getApplication().executeOnPooledThread(
+        () -> {
+          try {
+            callback.beforeStart();
+            associator.associateFileTypes(fileTypes);
+            OSFileAssociationFeatureUsagesUtil.logFilesAssociated(fileTypes);
+            callback.onSuccess(associator.isOsRestartRequired());
+          }
+          catch (OSFileAssociationException exception) {
+            callback.onFailure(exception.getMessage());
+            LOG.info(exception);
+          }
+        }
+      );
     }
   }
 
@@ -108,8 +159,7 @@ public class OSAssociateFileTypesUtil {
 
   private static String getSubtypeName(@NotNull String baseName, @NotNull ExtensionFileNameMatcher matcher) {
     String ext = StringUtils.removeStart(matcher.getExtension(), ".");
-    ext = ext.replaceAll("\\+", "-plus");
-    return baseName + "-x-" + ext;
+    return baseName + EXTENSION_SEPARATOR + ext;
   }
 
   public static @Nullable FileNameMatcher getSubtypeMatcher(@NotNull FileType fileType) {
--- platform/lang-impl/src/com/intellij/openapi/fileTypes/impl/associate/OSFileAssociationPreferences.java
+++ platform/lang-impl/src/com/intellij/openapi/fileTypes/impl/associate/OSFileAssociationPreferences.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.fileTypes.impl.associate;
 
+import com.intellij.openapi.application.PathManager;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.State;
@@ -8,22 +9,30 @@ import com.intellij.openapi.components.Storage;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.xmlb.XmlSerializerUtil;
+import org.codehaus.groovy.tools.shell.util.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @State(name = "OSFileAssociationPreferences", storages =  @Storage("osFileIdePreferences.xml"))
 public final class OSFileAssociationPreferences implements PersistentStateComponent<OSFileAssociationPreferences> {
+  private final static Logger LOG = Logger.create(OSFileAssociationPreferences.class);
+
   public List<String> fileTypeNames = new ArrayList<>();
+  public String ideLocationHash;
 
   public static OSFileAssociationPreferences getInstance() {
     return ApplicationManager.getApplication().getService(OSFileAssociationPreferences.class);
   }
 
   @Override
-  public @Nullable OSFileAssociationPreferences getState() {
+  public @NotNull OSFileAssociationPreferences getState() {
     return this;
   }
 
@@ -35,6 +44,31 @@ public final class OSFileAssociationPreferences implements PersistentStateCompon
   public void updateFileTypes(List<FileType> fileTypes) {
     fileTypeNames.clear();
     fileTypeNames.addAll(ContainerUtil.map(fileTypes, fileType -> fileType.getName()));
+    updateIdeLocationHash();
+  }
+
+  boolean ideLocationChanged() {
+    return !Objects.equals(ideLocationHash, getIdeLocationHash());
+  }
+
+  void updateIdeLocationHash() {
+    ideLocationHash = getIdeLocationHash();
+  }
+
+  private static @Nullable String getIdeLocationHash() {
+    try {
+      MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+      messageDigest.update(PathManager.getHomePath().getBytes(StandardCharsets.UTF_8));
+      StringBuilder result = new StringBuilder();
+      for (byte b : messageDigest.digest()) {
+        result.append(String.format("%02X", b));
+      }
+      return result.toString();
+    }
+    catch (NoSuchAlgorithmException e) {
+      LOG.error(e);
+    }
+    return null;
   }
 
   public boolean contains(@NotNull FileType fileType) {
