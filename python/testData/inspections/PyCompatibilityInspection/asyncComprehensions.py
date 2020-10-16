00eb5054971510115bd12f40b6560b0ee5cebc6f
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2020-10-16 12:33:29 +0300

    Python 2.6 is no more supported in compatibility inspection
    Because 2.6 is not supported since 2019.1 in IDE.
    
    GitOrigin-RevId: 3dee3e941c921ad3a5dc6d72cca840eb5d811691

--- python/testData/inspections/PyCompatibilityInspection/asyncComprehensions.py
+++ python/testData/inspections/PyCompatibilityInspection/asyncComprehensions.py
@@ -1,6 +1,6 @@
-<warning descr="Python versions 2.6, 2.7 do not support this syntax">async</warning> def asyncgen():
+<warning descr="Python version 2.7 does not support this syntax">async</warning> def asyncgen():
     <warning descr="Python version 3.5 does not support 'yield' inside async functions">yield 10</warning>
-<warning descr="Python versions 2.6, 2.7 do not support this syntax">async</warning> def run():
+<warning descr="Python version 2.7 does not support this syntax">async</warning> def run():
     {i <warning descr="Python version 3.5 does not support 'async' inside comprehensions and generator expressions">async</warning> for i in asyncgen()}
     [i <warning descr="Python version 3.5 does not support 'async' inside comprehensions and generator expressions">async</warning> for i in asyncgen()]
     {i: i ** 2 <warning descr="Python version 3.5 does not support 'async' inside comprehensions and generator expressions">async</warning> for i in asyncgen()}
