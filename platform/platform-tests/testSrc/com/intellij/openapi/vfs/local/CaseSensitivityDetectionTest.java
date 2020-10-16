8282a03b171111d22454fe146320ced8d1ab0b47
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2020-10-16 14:49:57 +0200

    [test framework] extracting a couple of WSL- and 'fsutil'-related methods
    GitOrigin-RevId: 6424842c151dfd3ef0b55fc2bdb20b33d77b91ab

--- platform/platform-tests/testSrc/com/intellij/openapi/vfs/local/CaseSensitivityDetectionTest.java
+++ platform/platform-tests/testSrc/com/intellij/openapi/vfs/local/CaseSensitivityDetectionTest.java
@@ -1,33 +1,29 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vfs.local;
 
-import com.intellij.execution.configurations.GeneralCommandLine;
-import com.intellij.execution.configurations.PathEnvironmentVariableUtil;
-import com.intellij.execution.process.ProcessOutput;
-import com.intellij.execution.util.ExecUtil;
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.util.io.FileAttributes;
 import com.intellij.openapi.util.io.FileSystemUtil;
-import com.intellij.openapi.util.io.IoTestUtil;
-import com.intellij.openapi.util.text.Strings;
 import com.intellij.testFramework.rules.TempDirectory;
+import com.intellij.util.io.SuperUserStatus;
+import org.jetbrains.annotations.NotNull;
 import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.File;
 import java.util.List;
-import java.util.stream.Collectors;
 
+import static com.intellij.openapi.util.io.IoTestUtil.*;
 import static org.junit.Assert.*;
 import static org.junit.Assume.assumeTrue;
 
-/** test low-level functions for reading file case-sensitivity attributes in {@link FileSystemUtil} */
+/** Tests low-level functions for reading file case-sensitivity attributes in {@link FileSystemUtil} */
 public class CaseSensitivityDetectionTest {
   @Rule public TempDirectory myTempDir = new TempDirectory();
 
   @Test
   public void windowsFSRootsMustHaveDefaultSensitivity() {
-    IoTestUtil.assumeWindows();
+    assumeWindows();
 
     String systemDrive = System.getenv("SystemDrive");  // typically "C:"
     assertNotNull(systemDrive);
@@ -43,40 +39,35 @@ public class CaseSensitivityDetectionTest {
   }
 
   @Test
-  public void wslRootsMustBeCaseSensitive() throws Exception {
-    IoTestUtil.assumeWindows();
-    assumeTrue("'wsl.exe' not found in %Path%", PathEnvironmentVariableUtil.findInPath("wsl.exe") != null);
+  public void wslRootsMustBeCaseSensitive() {
+    assumeWindows();
 
-    ProcessOutput output = ExecUtil.execAndGetOutput(new GeneralCommandLine("wsl", "-l", "-v").withRedirectErrorStream(true), 30_000);
-    List<String> lines = output.getStdoutLines().stream().map(String::trim).filter(Strings::isNotEmpty).collect(Collectors.toList());
-    assumeTrue("Can't enumerate WSL distributions: " + output.getExitCode() + " stdout:\n" + output.getStdout().trim(),
-               output.getExitCode() == 0 && lines.size() > 1);
+    List<@NotNull String> distributions = enumerateWslDistributions();
+    assumeTrue("No WSL distributions found", !distributions.isEmpty());
 
-    for (int i = 1; i < lines.size(); i++) {
-      String name = lines.get(i).trim().split("\\s+")[1];
-      File root = new File("\\\\wsl$\\" + name);
-      assertEquals(FileAttributes.CaseSensitivity.SENSITIVE, FileSystemUtil.readParentCaseSensitivity(root));
+    for (String name : distributions) {
+      String root = "\\\\wsl$\\" + name;
+      assertEquals(root, FileAttributes.CaseSensitivity.SENSITIVE, FileSystemUtil.readParentCaseSensitivity(new File(root)));
     }
   }
 
   @Test
-  public void caseSensitivityChangesUnderWindowsMustBeReReadCorrectly() throws Exception {
-    IoTestUtil.assumeWindows();
-    assumeTrue("'wsl.exe' not found in %Path%", PathEnvironmentVariableUtil.findInPath("wsl.exe") != null);
+  public void caseSensitivityChangesUnderWindowsMustBeReReadCorrectly() {
+    assumeWindows();
+    assumeWslPresence();
+    assumeTrue("'fsutil.exe' needs elevated privileges to work", SuperUserStatus.isSuperUser());
 
-    File dir = myTempDir.newDirectory();
-    File anyChild = new File(dir, "child.txt");
-    assertTrue(anyChild.createNewFile());
-    assertEquals(FileAttributes.CaseSensitivity.INSENSITIVE, FileSystemUtil.readParentCaseSensitivity(anyChild));
-    WindowsCaseSensitivityTest.makeCaseSensitive(dir, true);
-    assertEquals(FileAttributes.CaseSensitivity.SENSITIVE, FileSystemUtil.readParentCaseSensitivity(anyChild));
-    WindowsCaseSensitivityTest.makeCaseSensitive(dir, false);
-    assertEquals(FileAttributes.CaseSensitivity.INSENSITIVE, FileSystemUtil.readParentCaseSensitivity(anyChild));
+    File file = myTempDir.newFile("dir/child.txt"), dir = file.getParentFile();
+    assertEquals(FileAttributes.CaseSensitivity.INSENSITIVE, FileSystemUtil.readParentCaseSensitivity(file));
+    setCaseSensitivity(dir, true);
+    assertEquals(FileAttributes.CaseSensitivity.SENSITIVE, FileSystemUtil.readParentCaseSensitivity(file));
+    setCaseSensitivity(dir, false);
+    assertEquals(FileAttributes.CaseSensitivity.INSENSITIVE, FileSystemUtil.readParentCaseSensitivity(file));
   }
 
   @Test
   public void macOsBasics() {
-    IoTestUtil.assumeMacOS();
+    assumeMacOS();
 
     File root = new File("/");
     FileAttributes.CaseSensitivity rootCs = FileSystemUtil.readParentCaseSensitivity(root);
