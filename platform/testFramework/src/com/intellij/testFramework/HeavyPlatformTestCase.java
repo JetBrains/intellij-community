10b1feb3e947fef166d62257d509473fbf8fe162
Dmitry Kozhevnikov <dmitry.kozhevnikov@jetbrains.com>
2020-10-18 17:54:54 +0300

    CPP-22223: get rid of SwiftTests fake application; make sure Swift plugin can be launched anywhere
    (given intellij.c plugin is present)
    
    GitOrigin-RevId: e7a3374fd9f68c0d4d07837fd354ca0289b2f307

--- platform/testFramework/src/com/intellij/testFramework/HeavyPlatformTestCase.java
+++ platform/testFramework/src/com/intellij/testFramework/HeavyPlatformTestCase.java
@@ -183,7 +183,7 @@ public abstract class HeavyPlatformTestCase extends UsefulTestCase implements Da
   private static final String[] PREFIX_CANDIDATES = {
     "Rider", "GoLand", "CLion", "MobileIDE",
     null,
-    "AppCode", "SwiftTests",
+    "AppCode",
     "DataGrip",
     "Python", "PyCharmCore",
     "Ruby",
