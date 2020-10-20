f4b6e2139d27a770991c42e9e3d3a480061b730e
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2020-10-20 10:18:19 +0300

    IDEA-252563 "Template. The values saved here..." banner isn't shown for template run configurations using the new UI
    (cherry picked from commit 1f49bd72684504f5bb7bf53467ca33c0b93809ab)
    
    GitOrigin-RevId: 6ee748760dfc7dbd854a631cdb3a3594aef7acdd

--- platform/execution-impl/src/com/intellij/execution/ui/RunnerAndConfigurationSettingsEditor.java
+++ platform/execution-impl/src/com/intellij/execution/ui/RunnerAndConfigurationSettingsEditor.java
@@ -1,30 +1,38 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.ui;
 
+import com.intellij.execution.ExecutionBundle;
 import com.intellij.execution.RunnerAndConfigurationSettings;
 import com.intellij.execution.configurations.RunConfigurationBase;
 import com.intellij.execution.impl.RunConfigurationStorageUi;
 import com.intellij.execution.impl.RunnerAndConfigurationSettingsImpl;
+import com.intellij.icons.AllIcons;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.options.SettingsEditor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Disposer;
+import com.intellij.ui.components.labels.LinkLabel;
+import com.intellij.ui.components.labels.LinkListener;
 import com.intellij.util.ui.JBUI;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.function.Consumer;
 
 public class RunnerAndConfigurationSettingsEditor extends SettingsEditor<RunnerAndConfigurationSettings> {
 
   private final RunConfigurationFragmentedEditor<RunConfigurationBase<?>> myConfigurationEditor;
+  private final Consumer<JComponent> myConfigurationCreator;
   private final @Nullable RunConfigurationStorageUi myRCStorageUi;
 
   public RunnerAndConfigurationSettingsEditor(RunnerAndConfigurationSettings settings,
-                                              RunConfigurationFragmentedEditor<RunConfigurationBase<?>> configurationEditor) {
+                                              RunConfigurationFragmentedEditor<RunConfigurationBase<?>> configurationEditor,
+                                              Consumer<JComponent> configurationCreator) {
     super(settings.createFactory());
     myConfigurationEditor = configurationEditor;
+    myConfigurationCreator = configurationCreator;
     myConfigurationEditor.addSettingsEditorListener(editor -> fireEditorStateChanged());
     Disposer.register(this, myConfigurationEditor);
 
@@ -68,15 +76,24 @@ public class RunnerAndConfigurationSettingsEditor extends SettingsEditor<RunnerA
     GridBagConstraints c = new GridBagConstraints();
     c.gridx = 0;
     c.gridy = 0;
+    c.anchor = GridBagConstraints.NORTH;
+    c.insets = JBUI.emptyInsets();
+    c.fill = GridBagConstraints.NONE;
+    c.weightx = 0;
+    c.weighty = 0;
+    panel.add(createDisclaimer(), c);
+
+    c.gridx = 0;
+    c.gridy = 1;
     c.anchor = GridBagConstraints.EAST;
-    c.insets = JBUI.insets(5, 0, 10, 0);
+    c.insets = JBUI.insets(5, 0, 5, 0);
     c.fill = GridBagConstraints.NONE;
     c.weightx = 0;
     c.weighty = 0;
     panel.add(myRCStorageUi.createComponent(), c);
 
     c.gridx = 0;
-    c.gridy = 1;
+    c.gridy = 2;
     c.anchor = GridBagConstraints.NORTH;
     c.insets = JBUI.emptyInsets();
     c.fill = GridBagConstraints.BOTH;
@@ -84,6 +101,21 @@ public class RunnerAndConfigurationSettingsEditor extends SettingsEditor<RunnerA
     c.weighty = 1;
     panel.add(myConfigurationEditor.getComponent(), c);
 
+    return panel;
+  }
+
+  private JComponent createDisclaimer() {
+    JPanel panel = new JPanel(new BorderLayout());
+    JLabel label = new JLabel(ExecutionBundle.message("template.disclaimer"), AllIcons.General.Warning, SwingConstants.LEADING);
+    label.setBorder(JBUI.Borders.emptyRight(JBUI.scale(10)));
+    panel.add(label, BorderLayout.WEST);
+    panel.add(new LinkLabel<>(ExecutionBundle.message("create.configuration"), null, new LinkListener<>() {
+      @Override
+      public void linkSelected(LinkLabel aSource, Object aLinkData) {
+        myConfigurationCreator.accept(panel);
+      }
+    }), BorderLayout.EAST);
+    panel.setBorder(JBUI.Borders.empty(JBUI.scale(3), 0, JBUI.scale(7), 0));
     return panel;
   }
 }
