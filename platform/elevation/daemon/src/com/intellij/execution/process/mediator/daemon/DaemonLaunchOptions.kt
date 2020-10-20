196088e9d67be6a6392dc85e5d05d4249df53a41
Eldar Abusalimov <Eldar.Abusalimov@jetbrains.com>
2020-10-21 02:38:46 +0300

    CPP-22344: DaemonTrampolineMain: Intermediate short-living launcher
    (cherry picked from commit 60feb19ff78da70a4da60c01613cf7dbbfb9d810)
    
    IJ-CR-4038
    
    GitOrigin-RevId: 55b607621c98e65fd9b345e36d436871cbd782d8

---
+++ platform/elevation/daemon/src/com/intellij/execution/process/mediator/daemon/DaemonLaunchOptions.kt
@@ -0,0 +1,115 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+@file:Suppress("EXPERIMENTAL_API_USAGE")
+
+package com.intellij.execution.process.mediator.daemon
+
+import com.intellij.execution.process.mediator.util.parseArgs
+import java.nio.file.Path
+import java.security.KeyFactory
+import java.security.PublicKey
+import java.security.spec.X509EncodedKeySpec
+import java.util.*
+import kotlin.system.exitProcess
+
+data class DaemonLaunchOptions(
+  val trampoline: Boolean = false,
+  val helloOption: HelloOption? = null,
+  val publicKeyOption: PublicKeyOption? = null,
+) {
+  interface CmdlineOption {
+    override fun toString(): String
+  }
+
+  sealed class HelloOption(private val arg: String) : CmdlineOption {
+    override fun toString(): String = arg
+
+    object Stdout : HelloOption("--hello-file=-")
+
+    class File(val path: Path) : HelloOption("--hello-file=$path") {
+      constructor(s: String) : this(Path.of(s))
+    }
+
+    class Port(val port: UShort) : HelloOption("--hello-port=$port") {
+      constructor(s: String) : this(s.toUShort())
+    }
+  }
+
+  class PublicKeyOption(val publicKey: PublicKey) {
+    constructor(s: String) : this(rsaPublicKeyFromBytes(Base64.getDecoder().decode(s)))
+
+    override fun toString(): String = Base64.getEncoder().encodeToString(publicKey.encoded)
+
+    companion object {
+      private fun rsaPublicKeyFromBytes(bytes: ByteArray) =
+        KeyFactory.getInstance("RSA").generatePublic(X509EncodedKeySpec(bytes))
+    }
+  }
+
+  fun asCmdlineArgs(): List<String> {
+    return listOf(
+      helloOption,
+      publicKeyOption,
+    ).mapNotNull { it?.toString() }
+  }
+
+  override fun toString(): String {
+    return asCmdlineArgs().joinToString(" ")
+  }
+
+  companion object {
+    private fun printUsage(programName: String) {
+      System.err.println("Usage: $programName [ --hello-file=file|- | --hello-port=port ] [ --token-encrypt-rsa=public-key ]")
+    }
+
+    fun parseFromArgsOrDie(programName: String, args: Array<String>): DaemonLaunchOptions {
+      return try {
+        parseFromArgs(args)
+      }
+      catch (e: Exception) {
+        System.err.println(e.message)
+        printUsage(programName)
+        exitProcess(1)
+      }
+    }
+
+    private fun parseFromArgs(args: Array<String>): DaemonLaunchOptions {
+      var helloOption: HelloOption? = null
+      var publicKeyOption: PublicKeyOption? = null
+
+      for ((option, value) in parseArgs(args)) {
+        requireNotNull(value) { "Missing '$option' value" }
+
+        when (option) {
+          "--hello-file" -> {
+            if (helloOption == null) {
+              helloOption =
+                if (value == "-") HelloOption.Stdout
+                else HelloOption.File(value)
+            }
+            else System.err.println("Ignoring '$option'")
+          }
+
+          "--hello-port" -> {
+            // handling multiple hello writers is too complicated w.r.t. resource management
+            if (helloOption == null) {
+              helloOption = HelloOption.Port(value)
+            }
+            else System.err.println("Ignoring '$option'")
+          }
+
+          "--token-encrypt-rsa" -> {
+            publicKeyOption = PublicKeyOption(value)
+          }
+
+          null -> throw IllegalArgumentException("Unrecognized positional argument '$value'")
+          else -> throw IllegalArgumentException("Unrecognized option '$option'")
+        }
+      }
+
+      return DaemonLaunchOptions(
+        helloOption = helloOption,
+        publicKeyOption = publicKeyOption,
+      )
+    }
+  }
+}
