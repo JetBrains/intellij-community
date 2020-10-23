b8937aced4a7f7f993e49ceb1927ecb6a886893d
Nikita Bobko <nikita.bobko@jetbrains.com>
2020-10-23 20:32:23 +0300

    Fix compilation error caused by migration to new compiler
    GitOrigin-RevId: dd17bc241c266a05f027b7d8a77ed1f5ec08494f

--- plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/caches/resolve/IDELightClassGenerationSupport.kt
+++ plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/caches/resolve/IDELightClassGenerationSupport.kt
@@ -222,7 +222,7 @@ class IDELightClassGenerationSupport(private val project: Project) : LightClassG
 
     override fun analyze(element: KtElement) = element.analyze(BodyResolveMode.PARTIAL)
 
-    fun analyzeAnnotation(element: KtAnnotationEntry): AnnotationDescriptor? = element.resolveToDescriptorIfAny()
+    override fun analyzeAnnotation(element: KtAnnotationEntry): AnnotationDescriptor? = element.resolveToDescriptorIfAny()
 
     override fun analyzeWithContent(element: KtClassOrObject) = element.analyzeWithContent()
 }
