9d7681517b35deba1de4d89ca12243f79423e5bc
Dmitry.Krasilschikov <kradima@jetbrains.com>
2020-10-25 17:27:11 +0200

    IDEA-251353 reader mode: disable warnings and errors in read only files
    GitOrigin-RevId: 285a9bda56bf4d95defcf282f90f7395e982e9f3

--- platform/lang-impl/src/com/intellij/codeInsight/actions/ReaderModeProviders.kt
+++ platform/lang-impl/src/com/intellij/codeInsight/actions/ReaderModeProviders.kt
@@ -31,7 +31,7 @@ class HighlightingReaderModeProvider : ReaderModeProvider {
     if (!fileIsOpenAlready) return
 
     val highlighting =
-      if (readerMode && !ReaderModeSettings.instance(project).showWarnings) FileHighlightingSetting.SKIP_INSPECTION
+      if (readerMode && !ReaderModeSettings.instance(project).showWarnings) FileHighlightingSetting.SKIP_HIGHLIGHTING
       else FileHighlightingSetting.FORCE_HIGHLIGHTING
 
     HighlightLevelUtil.forceRootHighlighting(PsiDocumentManager.getInstance(project).getPsiFile(editor.document) ?: return, highlighting)
@@ -43,7 +43,7 @@ class ReaderModeHighlightingSettingsProvider : DefaultHighlightingSettingProvide
     if (ReaderModeSettings.instance(project).enabled
         && !ReaderModeSettings.instance(project).showWarnings
         && ReaderModeSettings.matchMode(project, file)) {
-      return FileHighlightingSetting.SKIP_INSPECTION
+      return FileHighlightingSetting.SKIP_HIGHLIGHTING
     }
 
     return null
