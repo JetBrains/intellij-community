04e6bed3be7c7a4465ebbf9a2672bd6d601fc862
Konstantin Bulenkov <kb@jetbrains.com>
2020-10-26 19:42:32 +0100

    IDEA-242086 Add actions "close all projects" and "close all but this projects"
    GitOrigin-RevId: 52525dab831fee525b4f6828c77dff5509d296a9

---
+++ platform/platform-impl/src/com/intellij/ide/actions/CloseAllProjectsAction.kt
@@ -0,0 +1,11 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.ide.actions
+
+import com.intellij.openapi.project.Project
+
+/**
+ * @author Konstantin Bulenkov
+ */
+class CloseAllProjectsAction : CloseProjectsActionBase() {
+  override fun canClose(project: Project, currentProject: Project) = true
+}
