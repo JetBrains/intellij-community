fcf86a8b20770640ebbef33be104c2b4c6a6bb3f
Alexey Kalina <alexey.kalina@jetbrains.com>
2020-10-26 12:38:40 +0300

    [ml-completion] javascript: add catboost model to experiment
    (cherry picked from commit eb83fe837371014d065af79c432988061d3a0a7e)
    
    IJ-CR-3636
    
    GitOrigin-RevId: 134997d01f6479954ced439c6651dc76400150ed

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/CommunityLibraryLicenses.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/CommunityLibraryLicenses.groovy
@@ -678,6 +678,7 @@ class CommunityLibraryLicenses {
     jetbrainsLibrary("completion-ranking-go-exp"),
     jetbrainsLibrary("completion-ranking-java"),
     jetbrainsLibrary("completion-ranking-java-exp"),
+    jetbrainsLibrary("completion-ranking-js-exp"),
     jetbrainsLibrary("completion-ranking-kotlin-exp"),
     jetbrainsLibrary("completion-ranking-php-exp"),
     jetbrainsLibrary("completion-ranking-python"),
