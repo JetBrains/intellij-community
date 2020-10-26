5ef781e7fde7be576f2e00637a951d4d26db650e
Alexey Kalina <alexey.kalina@jetbrains.com>
2020-10-26 16:41:49 +0300

    [ml-completion] php: use catboost model in experiment
    (cherry picked from commit ac69d5398606c95d3aab13555dc501612c3246b6)
    
    IJ-CR-3644
    
    GitOrigin-RevId: 03199bb55046e21de30fff15bdfe861847ab1e22

--- plugins/completion-ml-ranking-models/src/com/jetbrains/completion/ml/ranker/ExperimentPhpMLRankingProvider.kt
+++ plugins/completion-ml-ranking-models/src/com/jetbrains/completion/ml/ranker/ExperimentPhpMLRankingProvider.kt
@@ -1,20 +1,11 @@
 package com.jetbrains.completion.ml.ranker
 
 import com.intellij.completion.ml.ranker.ExperimentModelProvider
-import com.intellij.internal.ml.DecisionFunction
-import com.intellij.internal.ml.ModelMetadata
-import com.intellij.internal.ml.completion.CompletionRankingModelBase
-import com.intellij.internal.ml.completion.JarCompletionModelProvider
+import com.intellij.internal.ml.catboost.CatBoostJarCompletionModelProvider
 import com.intellij.lang.Language
-import com.jetbrains.completion.ranker.model.php.exp.ExperimentalMLGlassBox
 
-class ExperimentPhpMLRankingProvider: JarCompletionModelProvider(CompletionRankingModelsBundle.message("ml.completion.experiment.model.php"), "php_features_exp"), ExperimentModelProvider {
-
-  override fun createModel(metadata: ModelMetadata): DecisionFunction {
-    return object : CompletionRankingModelBase(metadata) {
-      override fun predict(features: DoubleArray?): Double = ExperimentalMLGlassBox.makePredict(features)
-    }
-  }
+class ExperimentPhpMLRankingProvider: CatBoostJarCompletionModelProvider(
+  CompletionRankingModelsBundle.message("ml.completion.experiment.model.php"), "php_features_exp", "php_model_exp"), ExperimentModelProvider {
 
   override fun isLanguageSupported(language: Language): Boolean = language.id.compareTo("PHP", ignoreCase = true) == 0
 
