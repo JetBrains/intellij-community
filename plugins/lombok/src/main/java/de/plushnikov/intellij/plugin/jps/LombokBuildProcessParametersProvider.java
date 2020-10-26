899a7b2b40a85ca53742f129c1d079ce2d3faa3c
Anna Kozlova <anna.kozlova@jetbrains.com>
2020-10-26 10:41:33 +0100

    lombok: reorganize code (IDEA-CR-68004)
    IDEA-CR-68004
    
    GitOrigin-RevId: 76143db3bd286026db78575aa77cd4ba32685946

--- plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/jps/LombokBuildProcessParametersProvider.java
+++ plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/jps/LombokBuildProcessParametersProvider.java
@@ -2,18 +2,8 @@
 package de.plushnikov.intellij.plugin.jps;
 
 import com.intellij.compiler.server.BuildProcessParametersProvider;
-import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.OrderEntry;
-import com.intellij.openapi.roots.ProjectRootManager;
-import com.intellij.psi.JavaPsiFacade;
-import com.intellij.psi.PsiDirectory;
-import com.intellij.psi.PsiPackage;
-import com.intellij.psi.util.CachedValueProvider;
-import com.intellij.psi.util.CachedValuesManager;
-import de.plushnikov.intellij.plugin.Version;
 import de.plushnikov.intellij.plugin.activity.LombokProjectValidatorActivity;
-import de.plushnikov.intellij.plugin.settings.ProjectSettings;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collections;
@@ -29,34 +19,9 @@ public final class LombokBuildProcessParametersProvider extends BuildProcessPara
 
   @Override
   public @NotNull List<String> getVMArguments() {
-    if (ProjectSettings.isLombokEnabledInProject(myProject) && ReadAction.compute(() -> LombokProjectValidatorActivity.hasLombokLibrary(myProject))) {
-      return CachedValuesManager.getManager(myProject).getCachedValue(myProject,
-                                                               () -> {
-                                                                 List<String> disableOptions =
-                                                                   ReadAction.compute(() -> disableOptimisation())
-                                                                   ? Collections.singletonList("-Djps.track.ap.dependencies=false")
-                                                                   : Collections.emptyList();
-                                                                 return new CachedValueProvider.Result<>(disableOptions, ProjectRootManager.getInstance(myProject));
-                                                               });
-
+    if (LombokProjectValidatorActivity.isVersionLessThan1_18_16(myProject)) {
+      return Collections.singletonList("-Djps.track.ap.dependencies=false");
     }
     return super.getVMArguments();
-  }
-
-  private boolean disableOptimisation() {
-    PsiPackage aPackage = JavaPsiFacade.getInstance(myProject).findPackage("lombok.experimental");
-    if (aPackage != null) {
-      PsiDirectory[] directories = aPackage.getDirectories();
-      if (directories.length > 0) {
-        List<OrderEntry> entries = ProjectRootManager.getInstance(myProject).getFileIndex().getOrderEntriesForFile(directories[0].getVirtualFile());
-        if (!entries.isEmpty()) {
-          String lombokVersion = Version.parseLombokVersion(entries.get(0));
-          if (lombokVersion != null && Version.compareVersionString(lombokVersion, "1.18.16") < 0) {
-            return true;
-          }
-        }
-      }
-    }
-    return false;
   }
 }
