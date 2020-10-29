21b34c724aa0fa8105b9603de39f41a984175201
Alexander Koshevoy <Alexander.Koshevoy@jetbrains.com>
2020-10-29 21:21:24 +0300

    PY-29281 Use Compose "run" command instead of "up" for executing Django manage.py script
    These changes fixes also the inability to interact with manage.py Django script using stdin (PY-31678).
    
    (cherry picked from commit 554e003321bb3434c4c489efc43d38d4887bef6b)
    
    IJ-CR-3939
    
    GitOrigin-RevId: a23a955105a665d4e962387e1e5f89573d9204d2

--- python/src/com/jetbrains/python/run/PyRemoteProcessStarter.java
+++ python/src/com/jetbrains/python/run/PyRemoteProcessStarter.java
@@ -23,6 +23,15 @@ import org.jetbrains.annotations.Nullable;
 public class PyRemoteProcessStarter {
   public static final Key<Boolean> OPEN_FOR_INCOMING_CONNECTION = Key.create("OPEN_FOR_INCOMING_CONNECTION");
   public static final Key<HostAndPort> WEB_SERVER_HOST_AND_PORT = new Key<>("WEB_SERVER_HOST_AND_PORT");
+  /**
+   * This key is used to give the hint for the process starter that the
+   * process is auxiliary.
+   * <p>
+   * As for now this flag takes effect for Docker Compose process starters
+   * which uses {@code docker-compose run} command to the contrary of the usual
+   * process execution using {@code docker-compose up} command.
+   */
+  public static final Key<Boolean> RUN_AS_AUXILIARY_PROCESS = Key.create("RUN_AS_AUXILIARY_PROCESS");
 
   @NotNull
   public ProcessHandler startRemoteProcess(@NotNull Sdk sdk,
