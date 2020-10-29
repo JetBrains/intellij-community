1251259bcba2fe2ba5940b735e0e9e542383e4a4
Aleksey Pivovarov <AMPivovarov@gmail.com>
2020-10-29 21:03:49 +0300

    git-index: mark refresh action as DumbAware
    GitOrigin-RevId: ccbbcfe8a56ec6ca4d7b4dfc169126d2a8bfc62e

--- plugins/git4idea/src/git4idea/index/actions/GitRefreshStageAction.kt
+++ plugins/git4idea/src/git4idea/index/actions/GitRefreshStageAction.kt
@@ -1,13 +1,13 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package git4idea.index.actions
 
-import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.vcs.VcsBundle
 import git4idea.index.ui.GitStageDataKeys
 import git4idea.index.vfs.GitIndexFileSystemRefresher
 
-class GitRefreshStageAction : AnAction(VcsBundle.messagePointer("action.name.refresh")) {
+class GitRefreshStageAction : DumbAwareAction(VcsBundle.messagePointer("action.name.refresh")) {
 
   override fun update(e: AnActionEvent) {
     e.presentation.isEnabledAndVisible = e.getData(GitStageDataKeys.GIT_STAGE_TRACKER) != null && e.project != null
