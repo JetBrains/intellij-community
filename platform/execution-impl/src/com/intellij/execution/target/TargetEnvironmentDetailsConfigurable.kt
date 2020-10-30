f738c627ac832b90d1c850a9bb96f47b124d4c09
Michael Golubev <michael.golubev@jetbrains.com>
2020-10-30 03:28:48 +0100

    IDEA-251778: Add default path for project-dependent files to the target configuration
    - refactoring: extract UI for languages panel from target details configurable to reuse in wizard
    
    (cherry picked from commit 38b944b88e6f776122fac81a2c90ea14cd123ad9)
    
    IDEA-CR-68380
    
    GitOrigin-RevId: 508dd2f77241e79305c82aa078742fd64ca4ab74

--- platform/execution-impl/src/com/intellij/execution/target/TargetEnvironmentDetailsConfigurable.kt
+++ platform/execution-impl/src/com/intellij/execution/target/TargetEnvironmentDetailsConfigurable.kt
@@ -1,24 +1,15 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.target
 
-import com.intellij.execution.ExecutionBundle
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.NamedConfigurable
-import com.intellij.openapi.util.NlsActions
-import com.intellij.ui.TitledSeparator
-import com.intellij.ui.components.DropDownLink
 import com.intellij.ui.components.JBScrollPane
 import com.intellij.ui.components.panels.VerticalLayout
-import com.intellij.ui.layout.*
 import com.intellij.ui.scale.JBUIScale
 import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.UIUtil
-import java.util.function.Consumer
 import javax.swing.Icon
-import javax.swing.JButton
 import javax.swing.JComponent
 import javax.swing.JPanel
 
@@ -30,17 +21,22 @@ internal class TargetEnvironmentDetailsConfigurable(
 ) : NamedConfigurable<TargetEnvironmentConfiguration>(true, treeUpdate) {
 
   private val targetConfigurable: Configurable = config.getTargetType().createConfigurable(project, config, defaultLanguage)
-  private val runtimeConfigurables = mutableListOf<Configurable>()
+
+  private var languagesPanel: TargetEnvironmentLanguagesPanel? = null
 
   override fun getBannerSlogan(): String = config.displayName
 
   override fun getIcon(expanded: Boolean): Icon = config.getTargetType().icon
 
-  override fun isModified(): Boolean = allConfigurables().any { it.isModified }
+  override fun isModified(): Boolean =
+    targetConfigurable.isModified || languagesPanel?.isModified == true
 
   override fun getDisplayName(): String = config.displayName
 
-  override fun apply() = allConfigurables().forEach { it.apply() }
+  override fun apply() {
+    targetConfigurable.apply()
+    languagesPanel?.applyAll()
+  }
 
   override fun setDisplayName(name: String) {
     config.displayName = name
@@ -48,7 +44,8 @@ internal class TargetEnvironmentDetailsConfigurable(
 
   override fun disposeUIResources() {
     super.disposeUIResources()
-    allConfigurables().forEach { it.disposeUIResources() }
+    targetConfigurable.disposeUIResources()
+    languagesPanel?.disposeUIResources()
   }
 
   override fun getEditableObject() = config
@@ -59,81 +56,26 @@ internal class TargetEnvironmentDetailsConfigurable(
 
     panel.add(targetConfigurable.createComponent() ?: throw IllegalStateException())
 
-    config.runtimes.resolvedConfigs().forEach {
-      panel.add(createRuntimePanel(config, it))
+    languagesPanel = TargetEnvironmentLanguagesPanel(project, config) {
+      forceRefreshUI()
     }
-    panel.add(createAddRuntimeHyperlink())
+    panel.add(languagesPanel!!.component)
+
     return JBScrollPane(panel).also {
       it.border = JBUI.Borders.empty()
     }
   }
 
-  private fun createRuntimePanel(target: TargetEnvironmentConfiguration, runtime: LanguageRuntimeConfiguration): JPanel {
-    return panel {
-      row {
-        val separator = TitledSeparator(runtime.getRuntimeType().configurableDescription)
-        separator(CCFlags.growX, CCFlags.pushX)
-        gearButton(DuplicateRuntimeAction(runtime), RemoveRuntimeAction(target, runtime))
-      }
-      row {
-        val languageUI = runtime.getRuntimeType().createConfigurable(project, runtime, config)
-          .also { runtimeConfigurables.add(it) }
-          .let {
-            it.createComponent() ?: throw IllegalStateException("for runtime: $runtime")
-          }
-        languageUI(CCFlags.growX)
-      }
-    }
-  }
-
-  private fun createAddRuntimeHyperlink(): JButton {
-    class Item(val type: LanguageRuntimeType<*>?) {
-      override fun toString(): String {
-        return type?.displayName ?: "Add language runtime"
-      }
-    }
-
-    return DropDownLink(Item(null), LanguageRuntimeType.EXTENSION_NAME.extensionList.map { Item(it) }, Consumer {
-      val newRuntime = it.type?.createDefaultConfig() ?: return@Consumer
-      config.runtimes.addConfig(newRuntime)
-      forceRefreshUI()
-    })
-  }
-
-  private fun allConfigurables() = sequenceOf(targetConfigurable) + runtimeConfigurables.asSequence()
-
   override fun resetOptionsPanel() {
-    runtimeConfigurables.clear()
+    languagesPanel?.disposeUIResources()
+    languagesPanel = null
     super.resetOptionsPanel()
   }
 
   private fun forceRefreshUI() {
-    resetOptionsPanel()
-    createComponent()?.revalidate()
-  }
-
-  private abstract inner class ChangeRuntimeActionBase(protected val runtime: LanguageRuntimeConfiguration,
-                                                       @NlsActions.ActionText text: String) : AnAction(text)
-
-  private inner class DuplicateRuntimeAction(runtime: LanguageRuntimeConfiguration)
-    : ChangeRuntimeActionBase(runtime, ExecutionBundle.message("targets.details.action.duplicate.text")) {
-    override fun actionPerformed(e: AnActionEvent) {
-      val copy = runtime.getRuntimeType().duplicateConfig(runtime)
-      config.runtimes.addConfig(copy)
-      forceRefreshUI()
-    }
-  }
-
-  private inner class RemoveRuntimeAction(private val target: TargetEnvironmentConfiguration, runtime: LanguageRuntimeConfiguration)
-    : ChangeRuntimeActionBase(runtime, ExecutionBundle.message("targets.details.action.remove.text")) {
-    override fun actionPerformed(e: AnActionEvent) {
-      config.runtimes.removeConfig(runtime)
-      forceRefreshUI()
-    }
-
-    override fun update(e: AnActionEvent) {
-      val lastLanguage = target.runtimes.resolvedConfigs().filter { it != runtime }.isEmpty()
-      e.presentation.isEnabled = !lastLanguage
+    createComponent()?.let {
+      it.revalidate()
+      it.repaint()
     }
   }
 }
