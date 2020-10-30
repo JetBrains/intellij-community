422ddb5948f69f680d7c450e2c4745dfdc3cfd1f
Leonid Shalupov <leonid@shalupov.com>
2020-10-30 15:18:40 +0300

    cwm: correctly build CWM plugin for edu IDEs
    GitOrigin-RevId: b4624428d8e9456e18517cc39f02ba7077817ccb

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/CommunityRepositoryModules.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/CommunityRepositoryModules.groovy
@@ -273,9 +273,6 @@ class CommunityRepositoryModules {
     plugin("intellij.gauge"),
     plugin("intellij.lombok") {
       withModule("intellij.lombok.generated")
-    },
-    plugin("intellij.codeWithMe.plugin") {
-      withModule("intellij.codeWithMe.plugin.terminal")
     }
   ]
 
