089a28e68ca0161ddf16df23fdd717606cc61821
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2020-10-30 16:43:08 +0300

    [git-index] use renderer prefered height when tree row height is zero
    (cherry picked from commit f9d5c3db38ca535f828b537fac45dfd6102ce685)
    
    GitOrigin-RevId: 4337d0bd7eec56a3140b0890cf437398d2a256cb

--- plugins/git4idea/src/git4idea/index/ui/GitStageTree.kt
+++ plugins/git4idea/src/git4idea/index/ui/GitStageTree.kt
@@ -416,7 +416,8 @@ abstract class GitStageTree(project: Project, private val settings: GitStageUiSe
                        else IconLoader.getDisabledIcon(baseIcon, this)
 
       val componentWidth = tree.getComponentWidth(foreground)
-      val background = ColorIcon(componentWidth, tree.getRowHeight(), componentWidth, tree.getRowHeight(),
+      val componentHeight = tree.getRowHeight().takeIf { it != 0 } ?: this.preferredSize.height
+      val background = ColorIcon(componentWidth, componentHeight, componentWidth, componentHeight,
                                  tree.getBackground(row, selected), false)
 
       val icon = LayeredIcon(2).apply {
