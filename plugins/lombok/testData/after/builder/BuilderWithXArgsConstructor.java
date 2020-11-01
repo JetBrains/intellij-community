2e235259f66976a1b0152b19e784e24fcdcbab78
Michail Plushnikov <michail.plushnikov@jetbrains.com>
2020-11-01 10:47:02 +0100

    [lombok] IDEA-254198 added more tests
    GitOrigin-RevId: 6a842d278ae1ff28cdca504d82e299f0b68fd990

--- plugins/lombok/testData/after/builder/BuilderWithXArgsConstructor.java
+++ plugins/lombok/testData/after/builder/BuilderWithXArgsConstructor.java
@@ -39,3 +39,120 @@ public class BuilderWithXArgsConstructor {
         }
     }
 }
+class BuilderWithAllArgsConstructorPrivate {
+  private int someProperty;
+
+  @Generated
+  public static BuilderWithAllArgsConstructorPrivate.BuilderWithAllArgsConstructorPrivateBuilder builder() {
+    return new BuilderWithAllArgsConstructorPrivateBuilder();
+  }
+
+  @Generated
+  private BuilderWithAllArgsConstructorPrivate(int someProperty) {
+    this.someProperty = someProperty;
+  }
+
+  @Generated
+  public static class BuilderWithAllArgsConstructorPrivateBuilder {
+    @Generated
+    private int someProperty;
+
+    @Generated
+    BuilderWithAllArgsConstructorPrivateBuilder() {
+    }
+
+    @Generated
+    public BuilderWithAllArgsConstructorPrivate.BuilderWithAllArgsConstructorPrivateBuilder someProperty(int someProperty) {
+      this.someProperty = someProperty;
+      return this;
+    }
+
+    @Generated
+    public BuilderWithAllArgsConstructorPrivate build() {
+      return new BuilderWithAllArgsConstructorPrivate(someProperty);
+    }
+
+    @Generated
+    public String toString() {
+      return "BuilderWithAllArgsConstructorPrivate.BuilderWithAllArgsConstructorPrivateBuilder(someProperty=" + this.someProperty + ")";
+    }
+  }
+}
+class BuilderWithReqArgsConstructor {
+  private final int someProperty;
+
+  @Generated
+  public static BuilderWithReqArgsConstructor.BuilderWithReqArgsConstructorBuilder builder() {
+    return new BuilderWithReqArgsConstructorBuilder();
+  }
+
+  @Generated
+  public BuilderWithReqArgsConstructor(int someProperty) {
+    this.someProperty = someProperty;
+  }
+
+  @Generated
+  public static class BuilderWithReqArgsConstructorBuilder {
+    @Generated
+    private int someProperty;
+
+    @Generated
+    BuilderWithReqArgsConstructorBuilder() {
+    }
+
+    @Generated
+    public BuilderWithReqArgsConstructor.BuilderWithReqArgsConstructorBuilder someProperty(int someProperty) {
+      this.someProperty = someProperty;
+      return this;
+    }
+
+    @Generated
+    public BuilderWithReqArgsConstructor build() {
+      return new BuilderWithReqArgsConstructor(someProperty);
+    }
+
+    @Generated
+    public String toString() {
+      return "BuilderWithReqArgsConstructor.BuilderWithReqArgsConstructorBuilder(someProperty=" + this.someProperty + ")";
+    }
+  }
+}
+class BuilderWithReqArgsConstructorPrivate {
+  private final int someProperty;
+
+  @Generated
+  public static BuilderWithReqArgsConstructorPrivate.BuilderWithReqArgsConstructorPrivateBuilder builder() {
+    return new BuilderWithReqArgsConstructorPrivateBuilder();
+  }
+
+  @Generated
+  private BuilderWithReqArgsConstructorPrivate(int someProperty) {
+    this.someProperty = someProperty;
+  }
+
+  @Generated
+  public static class BuilderWithReqArgsConstructorPrivateBuilder {
+    @Generated
+    private int someProperty;
+
+    @Generated
+    BuilderWithReqArgsConstructorPrivateBuilder() {
+    }
+
+    @Generated
+    public BuilderWithReqArgsConstructorPrivate.BuilderWithReqArgsConstructorPrivateBuilder someProperty(int someProperty) {
+      this.someProperty = someProperty;
+      return this;
+    }
+
+    @Generated
+    public BuilderWithReqArgsConstructorPrivate build() {
+      return new BuilderWithReqArgsConstructorPrivate(someProperty);
+    }
+
+    @Generated
+    public String toString() {
+      return "BuilderWithReqArgsConstructorPrivate.BuilderWithReqArgsConstructorPrivateBuilder(someProperty=" + this.someProperty + ")";
+    }
+  }
+}
