06c05bb467f1628afc85683f58b11aab524c7a35
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-11-03 08:05:55 +0100

    cleanup
    GitOrigin-RevId: 7eba9e7b4a829f672c14588803d5cce9c55e6f6b

--- platform/lang-impl/src/com/intellij/packageDependencies/TestScopeProvider.java
+++ platform/lang-impl/src/com/intellij/packageDependencies/TestScopeProvider.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.packageDependencies;
 
 import com.intellij.openapi.project.Project;
@@ -11,13 +11,12 @@ import java.util.Collections;
 import java.util.List;
 
 public final class TestScopeProvider extends CustomScopesProviderEx {
-  public static TestScopeProvider getInstance(Project project) {
+  public static TestScopeProvider getInstance(@NotNull Project project) {
     return CUSTOM_SCOPES_PROVIDER.findExtension(TestScopeProvider.class, project);
   }
 
   @Override
-  @NotNull
-  public List<NamedScope> getCustomScopes() {
+  public @NotNull List<NamedScope> getCustomScopes() {
     return Collections.singletonList(TestsScope.INSTANCE);
   }
 }
