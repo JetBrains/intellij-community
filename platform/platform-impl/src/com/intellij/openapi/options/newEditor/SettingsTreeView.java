71e9afdfe6ffe0377ed99dc5ff11b630532525e5
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2020-11-03 16:50:41 +0100

    shared-indexes: IDEA-253972 - do not show shared index settings depends on a project
    (cherry picked from commit 49275245d9871a940a132fbded1e4c534d0bae82)
    
    IJ-CR-3828
    
    GitOrigin-RevId: 2fa9666ba255cf568fafa6da49a757e19ee27584

--- platform/platform-impl/src/com/intellij/openapi/options/newEditor/SettingsTreeView.java
+++ platform/platform-impl/src/com/intellij/openapi/options/newEditor/SettingsTreeView.java
@@ -636,7 +636,7 @@ public class SettingsTreeView extends JComponent implements Accessible, Disposab
       // configure project icon
       Project project = null;
       if (node != null) {
-        project = findConfigurableProject(node, false);
+        project = findConfigurableProject(node, true);
       }
       Configurable configurable = null;
       if (node != null) {
