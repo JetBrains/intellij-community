5d0910c461b206bf0e68ee2f596eaf06a5e4434e
Eldar Abusalimov <Eldar.Abusalimov@jetbrains.com>
2020-11-05 05:25:07 +0300

    CPP-22344: ElevationSettings: Get initial daemon quota from settings
    (cherry picked from commit 1cbf41c22bea8cd73b72ec8afe851c522c025c38)
    
    IJ-CR-4038
    
    GitOrigin-RevId: 3dda0d8c9000e5fc3d03e1fb2ceba698db819690

--- platform/elevation/src/com/intellij/execution/process/elevation/ProcessMediatorClientManager.kt
+++ platform/elevation/src/com/intellij/execution/process/elevation/ProcessMediatorClientManager.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.process.elevation
 
+import com.intellij.execution.process.elevation.settings.ElevationSettings
 import com.intellij.execution.process.mediator.ProcessMediatorClient
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.progress.ProgressManager
@@ -37,7 +38,7 @@ class ProcessMediatorClientManager : Disposable {
       ProcessMediatorDaemonLauncher.launchDaemon(sudo = true)
     }, ElevationBundle.message("progress.title.starting.elevation.daemon"), true, null)
     val channel = daemon.createChannel()
-    return ProcessMediatorClient(coroutineScope, channel)
+    return ProcessMediatorClient(coroutineScope, channel, ElevationSettings.getInstance().quotaOptions)
   }
 
   override fun dispose() {
