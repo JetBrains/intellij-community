48825fa98f9fe094c9924dd3fa046e94d1a8baac
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2020-11-05 08:53:18 +0300

    FIR IDE: introduce memory leak checking in symbols test
    Original commit: 15277c097492255d93a23247f12bae4e10d6cdc1
    
    GitOrigin-RevId: 0040988988c078395561c97cb4ec1289ec4095e1

--- plugins/kotlin/generators/test/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ plugins/kotlin/generators/test/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -955,6 +955,10 @@ private fun assembleWorkspace(): TWorkspace = workspace {
         testClass<AbstractSymbolFromLibraryPointerRestoreTest> {
             model("resoreSymbolFromLibrary", extension = "txt")
         }
+
+        testClass<AbstractMemoryLeakInSymbolsTest> {
+            model("symbolMemoryLeak")
+        }
     }
 
     testGroup("idea/idea-fir/tests", "compiler/testData") {
