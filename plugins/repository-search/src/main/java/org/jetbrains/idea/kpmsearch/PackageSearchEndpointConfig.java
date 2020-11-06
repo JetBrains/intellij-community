638e4609b83ca62ceaf6ae103f0106f51f0535a4
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2020-11-06 13:50:33 +0300

    Fix maven dependency completion
    (cherry picked from commit e854f520c2922c4b9487fef3ae796ad269f2e95e)
    
    IJ-CR-3915
    
    GitOrigin-RevId: bb5a8dec905c748e06c53b7e183c79dacfe63e16

---
+++ plugins/repository-search/src/main/java/org/jetbrains/idea/kpmsearch/PackageSearchEndpointConfig.java
@@ -0,0 +1,27 @@
+package org.jetbrains.idea.kpmsearch;
+
+import com.intellij.openapi.application.ApplicationInfo;
+import com.intellij.openapi.application.ApplicationNamesInfo;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public interface PackageSearchEndpointConfig {
+  @Nullable
+  String getFullTextUrl();
+
+  @Nullable
+  String getSuggestUrl();
+
+  @NotNull
+  default String getUserAgent() {
+    return ApplicationNamesInfo.getInstance().getProductName() + "/" + ApplicationInfo.getInstance().getFullVersion();
+  }
+
+  int getReadTimeout();
+
+  int getConnectTimeout();
+
+  default boolean forceHttps(){
+    return true;
+  }
+}
