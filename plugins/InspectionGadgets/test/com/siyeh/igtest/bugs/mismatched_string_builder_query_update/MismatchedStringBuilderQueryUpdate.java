06545a3021cf7e2132463c0093262fbdbd52a390
Bas Leijdekkers <basleijdekkers@gmail.com>
2020-11-06 13:40:36 +0100

    IG: improve StringBuilder query check (IDEA-254611)
    GitOrigin-RevId: 5242bed113cf4d45bfe4bb7d5daa78913712dbf6

--- plugins/InspectionGadgets/test/com/siyeh/igtest/bugs/mismatched_string_builder_query_update/MismatchedStringBuilderQueryUpdate.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/bugs/mismatched_string_builder_query_update/MismatchedStringBuilderQueryUpdate.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.siyeh.igtest.bugs.mismatched_string_builder_query_update;
 
 import java.util.*;
@@ -152,5 +137,17 @@ class EnumConstant {
 class Assert {
   static Object assertEquals(Object a, Object b) {
     return a;
+  }
+}
+class Main {
+
+  static String foo() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("txet");
+    return "some " + sb.reverse();
+  }
+
+  public static void main(String[] args) {
+    System.out.println(foo());
   }
 }
