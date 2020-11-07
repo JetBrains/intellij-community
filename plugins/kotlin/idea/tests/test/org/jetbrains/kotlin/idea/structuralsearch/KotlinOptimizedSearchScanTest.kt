cf3a1bbe52cb5ac6e25d26abf4971eede3427792
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2020-11-07 18:38:53 +0100

    [structuralsearch] Add copyright notice
    GitOrigin-RevId: dd46a261ff25e2834206fef9e7bfda15c15f8640

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/structuralsearch/KotlinOptimizedSearchScanTest.kt
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/structuralsearch/KotlinOptimizedSearchScanTest.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2000-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
 package org.jetbrains.kotlin.idea.structuralsearch
 
 import com.intellij.codeInsight.daemon.quickFix.LightQuickFixTestCase
