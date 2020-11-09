2f06ffc9e75bffa60fe82cd8dd79e46cf886b42b
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2020-11-09 09:52:57 +0100

    UnknownSdkTracker: EA-239206 - take 1 - move Tasks Queue to a dedicated class/file
    GitOrigin-RevId: d46a5b048d96ea82515f5be36756596d2e4e831a

---
+++ platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/UnknownSdkTrackerQueue.kt
@@ -0,0 +1,35 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.projectRoots.impl
+
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
+import com.intellij.util.ui.update.MergingUpdateQueue
+import com.intellij.util.ui.update.Update
+
+@Service // project
+class UnknownSdkTrackerQueue(private val project: Project) : Disposable {
+  companion object {
+    @JvmStatic
+    fun getInstance(project: Project) = project.service<UnknownSdkTrackerQueue>()
+  }
+
+  private val myUpdateQueue = MergingUpdateQueue(javaClass.simpleName,
+                                                 700,
+                                                 true,
+                                                 null,
+                                                 project,
+                                                 null,
+                                                 false).usePassThroughInUnitTestMode()
+
+  override fun dispose() = Unit
+
+  fun queue(task: Runnable) {
+    myUpdateQueue.queue(object : Update(this) {
+      override fun run() {
+        task.run()
+      }
+    })
+  }
+}
