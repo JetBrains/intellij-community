8c925cf1c2fc9e3a4b901efe00d04c42553f767e
Michail Plushnikov <michail.plushnikov@jetbrains.com>
2020-11-09 19:47:28 +0100

    [lombok] IDEA-254280 simplified code
    GitOrigin-RevId: 0c95f501183798b100180ad1eea91f2649ec71e8

--- plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/resolver/LombokExternalLibraryResolver.java
+++ plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/resolver/LombokExternalLibraryResolver.java
@@ -9,36 +9,24 @@ import de.plushnikov.intellij.plugin.Version;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import static de.plushnikov.intellij.plugin.LombokClassNames.*;
+import static de.plushnikov.intellij.plugin.LombokClassNames.MAIN_LOMBOK_CLASSES;
 
 public class LombokExternalLibraryResolver extends ExternalLibraryResolver {
 
-  private static final List<String> MAIN_LOMBOK_CLASSES =
-    Arrays.asList(ALL_ARGS_CONSTRUCTOR, REQUIRED_ARGS_CONSTRUCTOR, NO_ARGS_CONSTRUCTOR,
-                  DATA, GETTER, SETTER, EQUALS_AND_HASHCODE, TO_STRING,
-                  LOG_4_J, LOG_4_J_2, SLF_4_J, JAVA_LOG, JBOSS_LOG, FLOGGER, COMMONS_LOG, CUSTOM_LOG,
-                  VALUE, BUILDER, SUPER_BUILDER, FIELD_DEFAULTS,
-                  UTILITY_CLASS, WITH, WITHER, EXPERIMENTAL_DELEGATE,
-                  SNEAKY_THROWS, CLEANUP, SYNCHRONIZED);
-
-  private final Set<String> allLombokAnnotations;
   private final Set<String> allLombokPackages;
-  private final Map<String, String> simpleNameToPackageMap;
+  private final Map<String, String> simpleNameToFQNameMap;
 
   private static final ExternalLibraryDescriptor LOMBOK = new ExternalLibraryDescriptor("org.projectlombok", "lombok",
                                                                                         null, null, Version.LAST_LOMBOK_VERSION);
 
   public LombokExternalLibraryResolver() {
-    allLombokAnnotations = MAIN_LOMBOK_CLASSES.stream().map(StringUtil::getShortName).collect(Collectors.toUnmodifiableSet());
     allLombokPackages = MAIN_LOMBOK_CLASSES.stream().map(StringUtil::getPackageName).collect(Collectors.toUnmodifiableSet());
-    simpleNameToPackageMap = MAIN_LOMBOK_CLASSES.stream().collect(Collectors.toMap(StringUtil::getShortName, Function.identity()));
+    simpleNameToFQNameMap = MAIN_LOMBOK_CLASSES.stream().collect(Collectors.toMap(StringUtil::getShortName, Function.identity()));
   }
 
   @Nullable
@@ -46,8 +34,8 @@ public class LombokExternalLibraryResolver extends ExternalLibraryResolver {
   public ExternalClassResolveResult resolveClass(@NotNull String shortClassName,
                                                  @NotNull ThreeState isAnnotation,
                                                  @NotNull Module contextModule) {
-    if (isAnnotation == ThreeState.YES && allLombokAnnotations.contains(shortClassName)) {
-      return new ExternalClassResolveResult(simpleNameToPackageMap.get(shortClassName), LOMBOK);
+    if (isAnnotation == ThreeState.YES && simpleNameToFQNameMap.containsKey(shortClassName)) {
+      return new ExternalClassResolveResult(simpleNameToFQNameMap.get(shortClassName), LOMBOK);
     }
     return null;
   }
