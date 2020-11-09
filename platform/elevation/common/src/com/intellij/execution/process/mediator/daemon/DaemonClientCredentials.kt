c7abb6466b2730b529c1b6379d7af7e2c8c53d27
Eldar Abusalimov <Eldar.Abusalimov@jetbrains.com>
2020-11-09 20:20:39 +0300

    CPP-22344: DaemonClientCredentials: Defend against empty credentials
    Follow-up: IJ-CR-3747 CPP-22344: daemon: Generate credentials on hello, validate during calls
    
    GitOrigin-RevId: 124d15eaccc997b3f617d28897ce7b3f76e691fc

--- platform/elevation/common/src/com/intellij/execution/process/mediator/daemon/DaemonClientCredentials.kt
+++ platform/elevation/common/src/com/intellij/execution/process/mediator/daemon/DaemonClientCredentials.kt
@@ -2,29 +2,44 @@
 package com.intellij.execution.process.mediator.daemon
 
 import com.google.protobuf.ByteString
-import io.grpc.*
+import io.grpc.Metadata
 import java.security.SecureRandom
 
 
-data class DaemonClientCredentials(val token: ByteString) {
+class DaemonClientCredentials {
+  val token: ByteString
+
   constructor(token: ByteArray) : this(ByteString.copyFrom(token))
+
+  constructor(token: ByteString) {
+    require(!token.isEmpty) { "Token must not be empty" }
+    this.token = token
+  }
+
+  private constructor() {
+    this.token = ByteString.EMPTY
+  }
 
   fun asMetadata() = Metadata().apply {
     put(TOKEN_METADATA_KEY, token.toByteArray())
   }
 
+  override fun hashCode(): Int = token.hashCode()
+  override fun equals(other: Any?): Boolean = this === other || other is DaemonClientCredentials && token == other.token
+
   companion object {
-    private val EMPTY = DaemonClientCredentials(ByteString.EMPTY)
+    val EMPTY = DaemonClientCredentials()
 
     private val TOKEN_METADATA_KEY: Metadata.Key<ByteArray> = Metadata.Key.of("process-mediator-token" + Metadata.BINARY_HEADER_SUFFIX,
                                                                               Metadata.BINARY_BYTE_MARSHALLER)
 
     fun fromMetadata(headers: Metadata): DaemonClientCredentials {
-      val token = headers.get(TOKEN_METADATA_KEY) ?: return EMPTY
+      val token = headers.get(TOKEN_METADATA_KEY)?.takeUnless { it.isEmpty() } ?: return EMPTY
       return DaemonClientCredentials(token)
     }
 
     fun generate(length: Int = 64): DaemonClientCredentials {
+      require(length > 0) { "Token length must be positive" }
       val bytes = ByteArray(length).apply(SecureRandom()::nextBytes)
       return DaemonClientCredentials(bytes)
     }
