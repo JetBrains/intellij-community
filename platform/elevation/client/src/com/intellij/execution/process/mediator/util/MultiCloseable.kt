7a7978372076fbeac3650fecbb9b09e26d4b704c
Eldar Abusalimov <Eldar.Abusalimov@jetbrains.com>
2020-11-09 17:14:13 +0300

    CPP-22344: HandshakeTransport: Move to the 'client' module
    GitOrigin-RevId: 6371f08eba11d983db8fd59c9d7112ba8bf145ae

--- platform/elevation/client/src/com/intellij/execution/process/mediator/util/MultiCloseable.kt
+++ platform/elevation/client/src/com/intellij/execution/process/mediator/util/MultiCloseable.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.execution.process.elevation
+package com.intellij.execution.process.mediator.util
 
 import java.io.Closeable
 import java.util.concurrent.CopyOnWriteArrayList
