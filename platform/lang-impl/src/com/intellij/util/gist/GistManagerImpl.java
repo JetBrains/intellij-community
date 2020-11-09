12bf056f8f1428a6365bdae7ef9ddecca11bb753
Alexey Kudravtsev <cdr@intellij.com>
2020-11-09 15:54:05 +0300

    reduce spam in test logs
    GitOrigin-RevId: f525a8818d8afc1af8c533fda8632e34b01aba96

--- platform/lang-impl/src/com/intellij/util/gist/GistManagerImpl.java
+++ platform/lang-impl/src/com/intellij/util/gist/GistManagerImpl.java
@@ -19,10 +19,8 @@ import com.intellij.util.io.DataExternalizer;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.TestOnly;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public final class GistManagerImpl extends GistManager {
@@ -81,15 +79,15 @@ public final class GistManagerImpl extends GistManager {
 
   @Override
   public void invalidateData(@NotNull VirtualFile file) {
-    if (LOG.isDebugEnabled()) {
-      LOG.debug("Invalidating gist " + file);
+    if (LOG.isTraceEnabled()) {
+      LOG.trace("Invalidating gist " + file);
     }
     invalidateData(); // should be more granular in future
   }
 
   private void invalidateGists() {
-    if (LOG.isDebugEnabled()) {
-      LOG.debug("Invalidating gists", new Throwable());
+    if (LOG.isTraceEnabled()) {
+      LOG.trace(new Throwable("Invalidating gists"));
     }
     // Clear all cache at once to simplify and speedup this operation.
     // It can be made per-file if cache recalculation ever becomes an issue.
