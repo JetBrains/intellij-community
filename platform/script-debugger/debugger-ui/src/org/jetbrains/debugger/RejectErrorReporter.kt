eb65311156942762f9016d7b92e41897917b2796
Viktor Shatrov <Viktor.Shatrov@jetbrains.com>
2020-11-09 11:48:17 +0300

    i18n: add bundle for script-debugger module
    GitOrigin-RevId: 43e892fafecd76b47585c597dfb305abedfc6164

--- platform/script-debugger/debugger-ui/src/org/jetbrains/debugger/RejectErrorReporter.kt
+++ platform/script-debugger/debugger-ui/src/org/jetbrains/debugger/RejectErrorReporter.kt
@@ -1,12 +1,16 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.debugger
 
+import com.intellij.openapi.util.NlsContexts
 import com.intellij.xdebugger.XDebugSession
 import org.jetbrains.concurrency.errorIfNotMessage
 import org.jetbrains.rpc.LOG
 import java.util.function.Consumer
 
-class RejectErrorReporter @JvmOverloads constructor(private val session: XDebugSession, private val description: String? = null) : Consumer<Throwable> {
+class RejectErrorReporter @JvmOverloads constructor(
+  private val session: XDebugSession,
+  @NlsContexts.NotificationContent private val description: String? = null
+) : Consumer<Throwable> {
   override fun accept(error: Throwable) {
     if (LOG.errorIfNotMessage(error)) {
       session.reportError("${if (description == null) "" else "$description: "}${error.message}")
