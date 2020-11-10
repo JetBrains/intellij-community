3924c3139d8c5ec7ab60d3eb99d928e21a932920
Eldar Abusalimov <Eldar.Abusalimov@jetbrains.com>
2020-11-10 20:29:02 +0300

    CoroutineExceptionHandlerImpl: Use LOG.error() for uncaught exceptions
    Also fix PooledCoroutineContextTest logic.
    
    GitOrigin-RevId: 547d8d1a8ce7cae5cc0ee3fac222ab8ca5fd76a5

--- platform/platform-tests/testSrc/com/intellij/application/PooledCoroutineContextTest.kt
+++ platform/platform-tests/testSrc/com/intellij/application/PooledCoroutineContextTest.kt
@@ -1,23 +1,45 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.application
 
+import com.intellij.testFramework.LoggedErrorProcessor
+import com.intellij.testFramework.UsefulTestCase
 import com.intellij.testFramework.assertions.Assertions.assertThat
 import kotlinx.coroutines.*
+import org.apache.log4j.Logger
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import java.util.*
 
-class PooledCoroutineContextTest {
+@RunWith(JUnit4::class)
+class PooledCoroutineContextTest : UsefulTestCase() {
   @Test
   fun `log error`() = runBlocking<Unit> {
     val errorMessage = "don't swallow me"
     // cannot use assertThatThrownBy here, because AssertJ doesn't support Kotlin coroutines
-    try {
+    val loggedErrors = mutableListOf<Throwable>()
+    withLoggedErrorsRecorded(loggedErrors) {
       GlobalScope.launch(Dispatchers.ApplicationThreadPool) {
         throw RuntimeException(errorMessage)
       }.join()
     }
-    catch (e: AssertionError) {
-      // in tests logger throws AssertionError on log.error()
-      assertThat(e).hasMessage(errorMessage)
+    assertThat(loggedErrors).anyMatch { errorMessage in it.message.orEmpty() }
+  }
+
+  private suspend fun <T> withLoggedErrorsRecorded(loggedErrors: List<Throwable>,
+                                                   block: suspend () -> T): T {
+    val savedInstance = LoggedErrorProcessor.getInstance()
+    val synchronizedLoggedErrors = Collections.synchronizedList(loggedErrors)
+    LoggedErrorProcessor.setNewInstance(object : LoggedErrorProcessor() {
+      override fun processError(message: String, t: Throwable, details: Array<String>, logger: Logger) {
+        synchronizedLoggedErrors.add(t)
+      }
+    })
+    return try {
+      block()
+    }
+    finally {
+      LoggedErrorProcessor.setNewInstance(savedInstance)
     }
   }
 
