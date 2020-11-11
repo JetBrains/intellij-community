13ce746735d4bff95a4628c41f842f04b359d9e7
Yann Cébron <yann.cebron@jetbrains.com>
2020-11-11 15:07:48 +0100

    AbstractVcsAction: fix javadoc refs
    GitOrigin-RevId: 6a4ba48790865daaf25def626a70b32dcf25a49a

--- platform/vcs-impl/src/com/intellij/openapi/vcs/actions/AbstractVcsAction.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/actions/AbstractVcsAction.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vcs.actions;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
@@ -53,8 +39,7 @@ public abstract class AbstractVcsAction extends DumbAwareAction {
   protected abstract void actionPerformed(@NotNull VcsContext e);
 
   /**
-   * @deprecated Only sync update is currently supported by {@link AbstractVcsAction}. Use
-   * {@link com.intellij.openapi.actionSystem.AsyncUpdateAction} directly if async update is necessary.
+   * @deprecated Only sync update is currently supported by {@link AbstractVcsAction}.
    */
   @SuppressWarnings("unused") // Required for compatibility with external plugins.
   @Deprecated
