2badbaefba98a9c216d3018e07a6b14771712788
Yann Cébron <yann.cebron@jetbrains.com>
2020-11-11 12:45:18 +0100

    JBSlidingPanel: fix javadoc refs
    GitOrigin-RevId: 8a189749366689b75a668e7d0f6cf792a48b9cd3

--- platform/platform-api/src/com/intellij/ui/components/JBSlidingPanel.java
+++ platform/platform-api/src/com/intellij/ui/components/JBSlidingPanel.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2019 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ui.components;
 
 import com.intellij.openapi.util.ActionCallback;
@@ -77,7 +63,7 @@ public class JBSlidingPanel extends JBPanel {
   }
 
   /**
-   * @deprecated MUST use {@link #add((String, Component))}
+   * @deprecated MUST use {@link #add(String, Component)}
    */
   @Override
   @Deprecated
@@ -86,7 +72,7 @@ public class JBSlidingPanel extends JBPanel {
   }
 
   /**
-   * @deprecated MUST use {@link #add((String, Component))}
+   * @deprecated MUST use {@link #add(String, Component)}
    */
   @Override
   @Deprecated
@@ -95,7 +81,7 @@ public class JBSlidingPanel extends JBPanel {
   }
 
   /**
-   * @deprecated MUST use {@link #add((String, Component))}
+   * @deprecated MUST use {@link #add(String, Component)}
    */
   @Override
   @Deprecated
@@ -104,7 +90,7 @@ public class JBSlidingPanel extends JBPanel {
   }
 
   /**
-   * @deprecated MUST use {@link #add((String, Component))}
+   * @deprecated MUST use {@link #add(String, Component)}
    */
   @Override
   @Deprecated
