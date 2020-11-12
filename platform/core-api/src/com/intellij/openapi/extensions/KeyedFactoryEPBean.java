389d9c0615d6fd0883e9519cc1406b019a78f0b8
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-11-12 12:22:21 +0100

    extra KeyedFactoryEPBean constructor must be marked as non-injectable
    GitOrigin-RevId: 612a5d3475fb8a1e35d5ecaeb972136f14e48410

--- platform/core-api/src/com/intellij/openapi/extensions/KeyedFactoryEPBean.java
+++ platform/core-api/src/com/intellij/openapi/extensions/KeyedFactoryEPBean.java
@@ -1,19 +1,19 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.extensions;
 
+import com.intellij.serviceContainer.NonInjectable;
 import com.intellij.util.xmlb.annotations.Attribute;
 import com.intellij.util.xmlb.annotations.Transient;
 import org.jetbrains.annotations.NotNull;
 
-/**
- * @author yole
- */
 public final class KeyedFactoryEPBean implements PluginAware {
   private transient PluginDescriptor pluginDescriptor;
 
+  @SuppressWarnings("unused")
   private KeyedFactoryEPBean() {
   }
 
+  @NonInjectable
   public KeyedFactoryEPBean(@NotNull PluginDescriptor pluginDescriptor) {
     this.pluginDescriptor = pluginDescriptor;
   }
