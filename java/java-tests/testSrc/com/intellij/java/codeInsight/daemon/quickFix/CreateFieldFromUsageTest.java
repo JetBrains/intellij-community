c3d79c963a767b6f9482c6f18f2408941e48ec13
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2020-11-12 16:50:27 +0300

    IDEA-252714 [java] use anchor if request is coming from tree child of target class
    (cherry picked from commit 8248fb4f61867773b41c5274b52f2e73b3867972)
    
    IDEA-CR-68711
    
    GitOrigin-RevId: 5de6c179ae4b68176dcc0226bb0c87be45737381

--- java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/quickFix/CreateFieldFromUsageTest.java
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/quickFix/CreateFieldFromUsageTest.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.java.codeInsight.daemon.quickFix;
 
 import com.intellij.codeInsight.daemon.quickFix.ActionHint;
@@ -73,6 +73,8 @@ public class CreateFieldFromUsageTest extends LightQuickFixTestCase {
   public void testInnerGeneric() { doSingleTest(); }
 
   public void testInnerGenericArray() { doSingleTest(); }
+  
+  public void testFromUnfinishedAnonymousClass() { doSingleTest(); }
 
   protected void doSingleTest() {
     doSingleTest(getTestName(false) + ".java");
