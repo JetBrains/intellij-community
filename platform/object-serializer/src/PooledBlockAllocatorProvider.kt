4de583376ba7dd0c39a9ebcdb9f88a04078065f8
Nikita Skvortsov <Nikita.Skvortsov@jetbrains.com>
2020-11-13 11:24:06 +0300

    [core] fix caching of PooledBlockAllocators IDEA-253956
    Each each time an IonManagedBinaryWriter is created, two instances of BlockAllocator are required. But PooledBlockAllocatorProvider caches only one instance, so each time second instance is created and new memory block is allocated. This repeats for all objects without default constructor in deserialized object graph and may cause significant slowness
    
    (cherry picked from commit b0e935822d1c11a49a1f95242ff064be2fdb3384)
    
    Change-Id: I00fdfee2102412e784a76e214743f37242893133

--- platform/object-serializer/src/PooledBlockAllocatorProvider.kt
+++ platform/object-serializer/src/PooledBlockAllocatorProvider.kt
@@ -4,9 +4,10 @@ package com.intellij.serialization
 import com.amazon.ion.impl.bin.Block
 import com.amazon.ion.impl.bin.BlockAllocator
 import com.amazon.ion.impl.bin.BlockAllocatorProvider
-import com.intellij.util.containers.ContainerUtil
 import org.jetbrains.annotations.TestOnly
 import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentLinkedQueue
 import java.util.concurrent.atomic.AtomicInteger
 
 internal class PooledBlockAllocatorProvider : BlockAllocatorProvider() {
@@ -16,7 +17,7 @@ internal class PooledBlockAllocatorProvider : BlockAllocatorProvider() {
   }
 
   @Suppress("RemoveExplicitTypeArguments")
-  private val allocators = ContainerUtil.createConcurrentIntObjectMap<PooledBlockAllocator>()
+  private val allocators = ConcurrentHashMap<Int, Queue<PooledBlockAllocator>>()
 
   private inner class PooledBlockAllocator(private val blockSize: Int) : BlockAllocator() {
     private val freeBlocks = ArrayList<Block>()
@@ -45,17 +46,14 @@ internal class PooledBlockAllocatorProvider : BlockAllocatorProvider() {
 
     override fun close() {
       if ((blockSize * freeBlocks.size) > POOL_THRESHOLD) {
-        return
-      }
-
-      if (allocators.putIfAbsent(blockSize, this) == null) {
-        removeExcess()
-      }
-      else {
         // help GC - nullize
         freeBlocks.clear()
         blockCounter.set(0)
+        return
       }
+
+      allocators.getOrPut(blockSize, ::ConcurrentLinkedQueue).add(this)
+      removeExcess()
     }
   }
 
@@ -63,7 +61,7 @@ internal class PooledBlockAllocatorProvider : BlockAllocatorProvider() {
   val byteSize: Int
     get() {
       var totalByteSize = 0
-      for (allocator in allocators.elements()) {
+      for (allocator in allocators.values.flatten()) {
         totalByteSize += allocator.byteSize
       }
       return totalByteSize
@@ -74,9 +72,7 @@ internal class PooledBlockAllocatorProvider : BlockAllocatorProvider() {
       throw IllegalArgumentException("Invalid block size: $blockSize")
     }
 
-    // PooledBlockAllocator is not thread safe - do not put a new one to pool
-    val result = allocators.remove(blockSize) ?: PooledBlockAllocator(blockSize)
-
+    val result = allocators[blockSize]?.poll() ?: PooledBlockAllocator(blockSize)
     removeExcess()
 
     return result
@@ -84,19 +80,23 @@ internal class PooledBlockAllocatorProvider : BlockAllocatorProvider() {
 
   private fun removeExcess() {
     var totalByteSize = 0
-    val iterator = allocators.values().iterator()
+    val queuesIterator = allocators.values.iterator()
     var isExcess = false
-    while (iterator.hasNext()) {
-      val allocator = iterator.next()
-      if (isExcess) {
-        iterator.remove()
-        continue
-      }
+    while (queuesIterator.hasNext()) {
+      val nextQueue = queuesIterator.next()
+      val iterator = nextQueue.iterator()
+      while (iterator.hasNext()) {
+        val allocator = iterator.next()
+        if (isExcess) {
+          iterator.remove()
+          continue
+        }
 
-      totalByteSize += allocator.byteSize
-      if (totalByteSize > POOL_THRESHOLD) {
-        iterator.remove()
-        isExcess = true
+        totalByteSize += allocator.byteSize
+        if (totalByteSize > POOL_THRESHOLD) {
+          iterator.remove()
+          isExcess = true
+        }
       }
     }
   }
