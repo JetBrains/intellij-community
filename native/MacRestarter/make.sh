a3467ffc6ff673e51f9a076ee9431d4eed5cf3b6
Konstantin Bulenkov <kb@jetbrains.com>
2020-11-15 13:39:30 +0100

    Update make.sh fules for restarter and fsnotifier to generate binaries in universal format
    (cherry picked from commit 625da1c6c7d99973a8cb05a86a8bcccd47c7cc09)
    
    Change-Id: I9d6c355a9cfda554d5104980d35101a1ff68255e

--- native/MacRestarter/make.sh
+++ native/MacRestarter/make.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-clang -arch x86_64 -mmacosx-version-min=10.8 -framework AppKit -lobjc -o restarter restarter.m
+clang -arch x86_64 -mmacosx-version-min=10.8 -framework AppKit -lobjc -o restarter_x86_64 restarter.m
+clang -arch arm64 -mmacosx-version-min=10.8 -framework AppKit -lobjc -o restarter_arm64 restarter.m
+lipo -create restarter_x86_64 restarter_arm64 -o restarter
+rm restarter_arm64
+rm restarter_x86_64
