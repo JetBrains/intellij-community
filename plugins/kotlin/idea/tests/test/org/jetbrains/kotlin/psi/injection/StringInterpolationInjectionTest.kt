a79201e90aa576e5e752d772ff4a983345a34acd
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2020-11-16 16:54:16 +0700

    [tests] StringInterpolationInjectionTest: fix flaky
    GitOrigin-RevId: 545711cf67ca389d924768b5fe066adde89c6a96

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/psi/injection/StringInterpolationInjectionTest.kt
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/psi/injection/StringInterpolationInjectionTest.kt
@@ -7,10 +7,16 @@ package org.jetbrains.kotlin.psi.injection
 
 import com.intellij.lang.html.HTMLLanguage
 import com.intellij.lang.injection.InjectedLanguageManager
+import com.intellij.openapi.command.undo.UndoManager
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.fileEditor.impl.text.TextEditorProvider
+import com.intellij.openapi.ui.TestDialog
+import com.intellij.openapi.ui.TestDialogManager
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.injection.Injectable
 import com.intellij.testFramework.LightProjectDescriptor
+import com.intellij.util.ui.UIUtil
 import junit.framework.TestCase
 import org.intellij.plugins.intelliLang.inject.InjectLanguageAction
 import org.jetbrains.kotlin.idea.test.KotlinLightCodeInsightFixtureTestCase
@@ -63,6 +69,21 @@ class StringInterpolationInjectionTest : KotlinLightCodeInsightFixtureTestCase()
             "Shouldn't be FRANKENSTEIN",
             java.lang.Boolean.TRUE == containingFile.getUserData(InjectedLanguageManager.FRANKENSTEIN_INJECTION)
         )
+
+        undo(editor)
+    }
+
+    private fun undo(editor: Editor) {
+        UIUtil.invokeAndWaitIfNeeded(Runnable {
+            val oldTestDialog = TestDialogManager.setTestDialog(TestDialog.OK)
+            try {
+                val undoManager = UndoManager.getInstance(project)
+                val textEditor = TextEditorProvider.getInstance().getTextEditor(editor)
+                undoManager.undo(textEditor)
+            } finally {
+                TestDialogManager.setTestDialog(oldTestDialog)
+            }
+        })
     }
 
     private val injectedLanguageManager: InjectedLanguageManager
