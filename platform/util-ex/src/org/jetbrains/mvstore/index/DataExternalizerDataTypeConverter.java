f70af0d8cf7cfaf777245e38ba2e7509e6ca0385
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2020-11-16 17:49:31 +0300

    index: add ByteSequenceDataType to convert ByteSequenceDataExternalizer to mvstore data-type
    GitOrigin-RevId: 7cfed246d3c68cf4c00fb39d96d03a4b51325ce3

--- platform/util-ex/src/org/jetbrains/mvstore/index/DataExternalizerDataTypeConverter.java
+++ platform/util-ex/src/org/jetbrains/mvstore/index/DataExternalizerDataTypeConverter.java
@@ -1,15 +1,12 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.mvstore.index;
 
-import com.intellij.util.io.DataExternalizer;
-import com.intellij.util.io.EnumeratorStringDescriptor;
-import com.intellij.util.io.IntInlineKeyDescriptor;
-import com.intellij.util.io.KeyDescriptor;
+import com.intellij.openapi.util.io.ByteArraySequence;
+import com.intellij.util.io.*;
+import io.netty.buffer.ByteBuf;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.mvstore.type.DataType;
-import org.jetbrains.mvstore.type.IntDataType;
-import org.jetbrains.mvstore.type.KeyableDataType;
-import org.jetbrains.mvstore.type.StringDataType;
+import org.jetbrains.mvstore.DataUtil;
+import org.jetbrains.mvstore.type.*;
 
 class DataExternalizerDataTypeConverter {
   @SuppressWarnings("unchecked")
@@ -21,6 +18,9 @@ class DataExternalizerDataTypeConverter {
     if (externalizer instanceof EnumeratorStringDescriptor) {
       return (DataType<T>)StringDataType.INSTANCE;
     }
+    if (externalizer instanceof ByteSequenceDataExternalizer) {
+      return (DataType<T>)ByteSequenceDataType.INSTANCE;
+    }
     throw new IllegalArgumentException("unsupported externalizer");
   }
 
@@ -28,5 +28,33 @@ class DataExternalizerDataTypeConverter {
   static <T> KeyableDataType<T> convert(@NotNull KeyDescriptor<T> descriptor) {
     DataType<T> dataType = convert((DataExternalizer<T>)descriptor);
     return (KeyableDataType<T>)dataType;
+  }
+
+  private static final class ByteSequenceDataType implements DataType<ByteArraySequence> {
+    public static final ByteSequenceDataType INSTANCE = new ByteSequenceDataType();
+    @Override
+    public int getMemory(ByteArraySequence obj) {
+      return DataUtil.VAR_INT_MAX_SIZE + obj.getLength();
+    }
+
+    @Override
+    public int getFixedMemory() {
+      return -1;
+    }
+
+    @Override
+    public void write(ByteBuf buf, ByteArraySequence obj) {
+      ByteArrayDataType.INSTANCE.write(buf, obj.toBytes());
+    }
+
+    @Override
+    public ByteArraySequence read(ByteBuf buff) {
+      return new ByteArraySequence(ByteArrayDataType.INSTANCE.read(buff));
+    }
+
+    @Override
+    public ByteArraySequence[] createStorage(int size) {
+      return new ByteArraySequence[size];
+    }
   }
 }
