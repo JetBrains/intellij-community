125ddebfb53bec88d5878bcb592bcef5483029f4
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2020-11-16 22:00:28 +0100

    phm: fix API compatibility
    GitOrigin-RevId: eff635cd9ac9556760b1b2e8c108cc80460a8f63

--- platform/util/src/com/intellij/util/io/PersistentMap.java
+++ platform/util/src/com/intellij/util/io/PersistentMap.java
@@ -1,3 +1,4 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.io;
 
 import com.intellij.util.Processor;
@@ -27,5 +28,5 @@ public interface PersistentMap<K, V> extends KeyValueStore<K, V> {
 
   //TODO should not be a part of PersistentMap interface
   @ApiStatus.Internal
-  void deleteMap();
+  default void deleteMap() {}
 }
