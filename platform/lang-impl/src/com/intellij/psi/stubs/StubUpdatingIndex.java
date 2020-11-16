c75edb8031722366f3775356d521e4a20b1953e8
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2020-11-16 14:17:02 +0300

    Indexing: flush the SerializationManagerImpl in StubUpdatingIndex.MyIndex.doFlush.
    GitOrigin-RevId: 42b37cf9991f6844d7e07b3ce2622fdf086e0032

--- platform/lang-impl/src/com/intellij/psi/stubs/StubUpdatingIndex.java
+++ platform/lang-impl/src/com/intellij/psi/stubs/StubUpdatingIndex.java
@@ -406,7 +406,7 @@ public final class StubUpdatingIndex extends SingleEntryFileBasedIndexExtension<
       ? snapshotInputMappings.getForwardIndexAccessor()
       : stubForwardIndexAccessor;
 
-    return new MyIndex(extension, storage, forwardIndex, accessor, snapshotInputMappings);
+    return new MyIndex(extension, storage, forwardIndex, accessor, snapshotInputMappings, mySerializationManager);
   }
 
   private void checkNameStorage() throws StorageException {
@@ -421,13 +421,16 @@ public final class StubUpdatingIndex extends SingleEntryFileBasedIndexExtension<
     private StubIndexImpl myStubIndex;
     @Nullable
     private final CompositeBinaryBuilderMap myCompositeBinaryBuilderMap = FileBasedIndex.USE_IN_MEMORY_INDEX ? null : new CompositeBinaryBuilderMap();
+    private final @NotNull SerializationManagerEx mySerializationManager;
 
     MyIndex(@NotNull FileBasedIndexExtension<Integer, SerializedStubTree> extension,
             @NotNull IndexStorage<Integer, SerializedStubTree> storage,
             @Nullable ForwardIndex forwardIndex,
             @Nullable ForwardIndexAccessor<Integer, SerializedStubTree> forwardIndexAccessor,
-            @Nullable SnapshotInputMappings<Integer, SerializedStubTree> snapshotInputMappings) throws IOException {
+            @Nullable SnapshotInputMappings<Integer, SerializedStubTree> snapshotInputMappings,
+            @NotNull SerializationManagerEx serializationManager) throws IOException {
       super(extension, storage, forwardIndex, forwardIndexAccessor, snapshotInputMappings, null);
+      mySerializationManager = serializationManager;
     }
 
     @Override
@@ -435,6 +438,7 @@ public final class StubUpdatingIndex extends SingleEntryFileBasedIndexExtension<
       final StubIndexImpl stubIndex = getStubIndex();
       try {
         stubIndex.flush();
+        mySerializationManager.flushNameStorage();
       }
       finally {
         super.doFlush();
