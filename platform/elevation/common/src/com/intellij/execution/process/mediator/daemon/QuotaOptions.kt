66257bb48e5d3ab4eb96636983715d98bc265a5e
Eldar Abusalimov <Eldar.Abusalimov@jetbrains.com>
2020-11-16 15:20:50 +0300

    CPP-22344: QuotaOptions: Fix equals() & hashCode()
    GitOrigin-RevId: 839a0f1bed2f02a1c9b36f33e0f907e4de621643

--- platform/elevation/common/src/com/intellij/execution/process/mediator/daemon/QuotaOptions.kt
+++ platform/elevation/common/src/com/intellij/execution/process/mediator/daemon/QuotaOptions.kt
@@ -1,6 +1,8 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.process.mediator.daemon
 
+import java.util.*
+
 data class QuotaOptions(
   val timeLimitMs: Long = UNLIMITED_MS,
   val isRefreshable: Boolean = true,
@@ -8,6 +10,7 @@ data class QuotaOptions(
   init {
     require(isUnlimited || timeLimitMs >= 0) { "timeLimitMs must ge non-negative or UNLIMITED" }
   }
+
   val isUnlimited get() = this.timeLimitMs == UNLIMITED_MS
 
   /**
@@ -23,8 +26,13 @@ data class QuotaOptions(
       isRefreshable = other.isRefreshable && isRefreshable,
     )
 
-  override fun hashCode(): Int = if (isUnlimited) 0 else super.hashCode()
-  override fun equals(other: Any?): Boolean = other is QuotaOptions && isUnlimited && other.isUnlimited || super.equals(other)
+  override fun hashCode(): Int = if (isUnlimited) 0 else Objects.hash(timeLimitMs, isRefreshable)
+  override fun equals(other: Any?): Boolean {
+    if (other !is QuotaOptions) return false
+    return isUnlimited && other.isUnlimited ||
+           (timeLimitMs == other.timeLimitMs &&
+            isRefreshable == other.isRefreshable)
+  }
 
   companion object {
     const val UNLIMITED_MS: Long = -1L
