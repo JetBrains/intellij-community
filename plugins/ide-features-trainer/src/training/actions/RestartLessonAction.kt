cca7af2132f18cc1ffc352fcd41a0e207373806d
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2020-11-16 16:45:01 +0300

    [IFT] Replace reset icon
    GitOrigin-RevId: 7146adf9dcc400bf4be186552dd47129ef086985

--- plugins/ide-features-trainer/src/training/actions/RestartLessonAction.kt
+++ plugins/ide-features-trainer/src/training/actions/RestartLessonAction.kt
@@ -4,11 +4,12 @@ package training.actions
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import icons.FeaturesTrainerIcons
 import training.learn.CourseManager
 import training.learn.lesson.LessonManager
 import training.ui.LearningUiManager
 
-class RestartLessonAction : AnAction(AllIcons.General.Reset) {
+class RestartLessonAction : AnAction(FeaturesTrainerIcons.Img.ResetLesson) {
   override fun actionPerformed(e: AnActionEvent) {
     val activeToolWindow = LearningUiManager.activeToolWindow ?: return
     val lesson = LessonManager.instance.currentLesson ?: return
