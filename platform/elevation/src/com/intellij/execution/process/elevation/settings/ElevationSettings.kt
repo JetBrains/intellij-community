7e761a99461d808357df85f806dcbe4493020adc
Eldar Abusalimov <Eldar.Abusalimov@jetbrains.com>
2020-11-17 05:54:16 +0300

    CPP-22344: ElevationSettings: Suggest to update settings on first run
    (cherry picked from commit 7eccd9ec44e4fc48b4f2a92e62965e888aa855d4)
    
    IJ-CR-4090
    
    GitOrigin-RevId: 28f91b1e6f21019b46045778598672aeb7da48e5

--- platform/elevation/src/com/intellij/execution/process/elevation/settings/ElevationSettings.kt
+++ platform/elevation/src/com/intellij/execution/process/elevation/settings/ElevationSettings.kt
@@ -1,9 +1,18 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.process.elevation.settings
 
+import com.intellij.execution.process.elevation.ElevationBundle
 import com.intellij.execution.process.mediator.daemon.QuotaOptions
+import com.intellij.ide.nls.NlsMessages
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ApplicationNamesInfo
 import com.intellij.openapi.components.*
+import com.intellij.openapi.ui.MessageDialogBuilder
+import com.intellij.openapi.ui.Messages.CANCEL
+import com.intellij.openapi.ui.Messages.YES
+import com.intellij.openapi.util.SystemInfo
+import com.intellij.openapi.util.text.HtmlBuilder
+import com.intellij.openapi.util.text.HtmlChunk
 import com.intellij.util.messages.Topic
 import kotlin.time.ExperimentalTime
 import kotlin.time.minutes
@@ -14,6 +23,10 @@ class ElevationSettings : PersistentStateComponentWithModificationTracker<Elevat
   companion object {
     @JvmStatic
     fun getInstance() = service<ElevationSettings>()
+
+    @Suppress("EXPERIMENTAL_IS_NOT_ENABLED")
+    @OptIn(ExperimentalTime::class)
+    private val DEFAULT_GRACE_PERIOD_MS = 15.minutes.toLongMilliseconds()
   }
 
   private val options = ElevationOptions()
@@ -33,6 +46,8 @@ class ElevationSettings : PersistentStateComponentWithModificationTracker<Elevat
     get() = options.isKeepAuth
     set(value) = updateQuotaOptions {
       options.isKeepAuth = value
+    }.also {
+      if (value) isSettingsUpdateDone = true
     }
 
   var isRefreshable: Boolean
@@ -47,6 +62,11 @@ class ElevationSettings : PersistentStateComponentWithModificationTracker<Elevat
       options.gracePeriodMs = value
     }
 
+  private var isSettingsUpdateDone: Boolean
+    get() = options.isSettingsUpdateDone
+    set(value) {
+      options.isSettingsUpdateDone = value
+    }
 
   private fun updateQuotaOptions(block: () -> Unit) {
     val oldValue = quotaOptions
@@ -61,6 +81,38 @@ class ElevationSettings : PersistentStateComponentWithModificationTracker<Elevat
   override fun loadState(state: ElevationOptions) = options.copyFrom(state)
   override fun getStateModificationCount() = options.modificationCount
 
+  fun askEnableKeepAuthIfNeeded(): Boolean {
+    if (isSettingsUpdateDone) return true
+
+    val productName = ApplicationNamesInfo.getInstance().fullProductName
+    val firstSentence = ElevationBundle.message("text.you.are.about.to.run.privileges.process")
+    val explanatoryText = ElevationBundle.message("text.elevation.explanatory.comment", productName)
+    val warningHtml = ElevationBundle.message("text.elevation.explanatory.warning.html")
+
+    val title =
+      if (SystemInfo.isMac) ElevationBundle.message("update.elevation.preferences")
+      else ElevationBundle.message("update.elevation.settings")
+
+    val yesNoCancelResult = MessageDialogBuilder
+      .yesNoCancel(title,
+                   HtmlBuilder()
+                     .append(HtmlChunk.p().addText(firstSentence)).br()
+                     .append(HtmlChunk.p().addText(explanatoryText)).br()
+                     .append(HtmlChunk.p().addRaw(warningHtml))
+                     .wrapWithHtmlBody()
+                     .toString())
+      .yesText(ElevationBundle.message("keep.authorized.for.0", NlsMessages.formatDuration(DEFAULT_GRACE_PERIOD_MS)))
+      .noText(ElevationBundle.message("authorize.every.time"))
+      .guessWindowAndAsk()
+
+    if (yesNoCancelResult == CANCEL) return false
+
+    quotaTimeLimitMs = DEFAULT_GRACE_PERIOD_MS
+    isKeepAuth = (yesNoCancelResult == YES)
+    isSettingsUpdateDone = true
+    return true
+  }
+
   interface Listener {
     companion object {
       @JvmField
@@ -71,11 +123,10 @@ class ElevationSettings : PersistentStateComponentWithModificationTracker<Elevat
     fun onDaemonQuotaOptionsChanged(oldValue: QuotaOptions, newValue: QuotaOptions) = Unit
   }
 
-  @Suppress("EXPERIMENTAL_IS_NOT_ENABLED")
-  @OptIn(ExperimentalTime::class)
   class ElevationOptions : BaseState() {
+    var isSettingsUpdateDone by property(false)
     var isKeepAuth by property(false)
     var isRefreshable by property(true)
-    var gracePeriodMs by property(15.minutes.toLongMilliseconds())
+    var gracePeriodMs by property(DEFAULT_GRACE_PERIOD_MS)
   }
 }
