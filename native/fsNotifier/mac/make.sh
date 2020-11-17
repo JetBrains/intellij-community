71f5dd3bb9b3e52bc88b63be87e4e7d8d37ed4cb
Florian Kistner <florian.kistner@jetbrains.com>
2020-11-17 04:22:04 +0100

    Darwin arm64: Compile fat binaries for native helpers
    GitOrigin-RevId: a4dc15f515cf01219f79ddeeabfa3cd8217abd77

--- native/fsNotifier/mac/make.sh
+++ native/fsNotifier/mac/make.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-
-C_FLAGS="-std=c11 -O2 -Wall -Wextra -Wpedantic -Wno-newline-eof"
-
-clang -arch x86_64 -mmacosx-version-min=10.8 -framework CoreServices -std=c11 $C_FLAGS -o fsnotifier_x86_64 fsnotifier.c
-clang -arch arm64 -mmacosx-version-min=10.8 -framework CoreServices -std=c11 $C_FLAGS -o fsnotifier_arm64 fsnotifier.c
-lipo -create fsnotifier_x86_64 fsnotifier_arm64 -o fsnotifier
-rm fsnotifier_arm64
-rm fsnotifier_x86_64
+rm -rf build
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make install/strip VERBOSE=1
