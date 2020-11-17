40c727b003c39fd46e41a35509e9bbf4fdf1e765
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2020-11-17 12:41:50 +0300

    Indexing diagnostics: add missing read actions.
    GitOrigin-RevId: 553dac9151773303fb3a8a41a30e571b6ed10aee

--- platform/lang-impl/src/com/intellij/util/indexing/diagnostic/dump/paths/providers/LibraryRelativePortableFilePathProvider.kt
+++ platform/lang-impl/src/com/intellij/util/indexing/diagnostic/dump/paths/providers/LibraryRelativePortableFilePathProvider.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.indexing.diagnostic.dump.paths.providers
 
+import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.LibraryOrderEntry
 import com.intellij.openapi.roots.OrderRootType
@@ -13,7 +14,7 @@ import com.intellij.util.indexing.diagnostic.dump.paths.PortableFilePath
 
 object LibraryRelativePortableFilePathProvider : PortableFilePathProvider {
   override fun getRelativePortableFilePath(project: Project, virtualFile: VirtualFile): PortableFilePath.RelativePath? {
-    val orderEntry = LibraryUtil.findLibraryEntry(virtualFile, project)
+    val orderEntry = runReadAction { LibraryUtil.findLibraryEntry(virtualFile, project) }
     if (orderEntry !is LibraryOrderEntry) return null
     val libraryName = orderEntry.libraryName
     if (libraryName == null) return null
