8b8d68c2f4c7a34c42c49f7fc5a44b6f87ef5b3d
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-11-17 18:01:51 +0100

    don't use trove library
    GitOrigin-RevId: 3864e2bfd53d6bd663d9b1f399eca4947c75dbae

--- plugins/IntelliLang/java-support/org/intellij/plugins/intelliLang/inject/java/InjectionCache.java
+++ plugins/IntelliLang/java-support/org/intellij/plugins/intelliLang/inject/java/InjectionCache.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.intellij.plugins.intelliLang.inject.java;
 
 import com.intellij.ide.highlighter.JavaFileType;
@@ -15,19 +15,14 @@ import com.intellij.psi.util.CachedValuesManager;
 import com.intellij.psi.util.PsiModificationTracker;
 import com.intellij.util.PatternValuesIndex;
 import com.intellij.util.containers.ContainerUtil;
-import gnu.trove.THashMap;
-import gnu.trove.THashSet;
 import org.intellij.plugins.intelliLang.Configuration;
 import org.intellij.plugins.intelliLang.inject.config.BaseInjection;
 import org.intellij.plugins.intelliLang.inject.config.InjectionPlace;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
-public class InjectionCache {
+public final class InjectionCache {
   private final CachedValue<Set<String>> myAnnoIndex;
   private final CachedValue<Collection<String>> myXmlIndex;
   private final Project myProject;
@@ -36,7 +31,7 @@ public class InjectionCache {
     Configuration configuration = Configuration.getProjectInstance(project);
     myProject = project;
     myXmlIndex = CachedValuesManager.getManager(project).createCachedValue(() -> {
-      final Map<ElementPattern<?>, BaseInjection> map = new THashMap<>();
+      final Map<ElementPattern<?>, BaseInjection> map = new HashMap<>();
       for (BaseInjection injection : configuration.getInjections(JavaLanguageInjectionSupport.JAVA_SUPPORT_ID)) {
         for (InjectionPlace place : injection.getInjectionPlaces()) {
           if (!place.isEnabled() || place.getElementPattern() == null) continue;
@@ -61,7 +56,7 @@ public class InjectionCache {
     // todo use allScope once Kotlin support becomes fast enough (https://youtrack.jetbrains.com/issue/KT-13734)
     GlobalSearchScope usageScope = GlobalSearchScope.getScopeRestrictedByFileTypes(allScope, JavaFileType.INSTANCE);
 
-    Set<String> result = new THashSet<>();
+    Set<String> result = new HashSet<>();
     ArrayList<PsiClass> annoClasses = ContainerUtil.newArrayList(JavaPsiFacade.getInstance(myProject).findClasses(annotationClassName, allScope));
     for (int cursor = 0; cursor < annoClasses.size(); cursor++) {
       Parameters parameters = new Parameters(annoClasses.get(cursor), usageScope, true, PsiClass.class, PsiParameter.class, PsiMethod.class);
