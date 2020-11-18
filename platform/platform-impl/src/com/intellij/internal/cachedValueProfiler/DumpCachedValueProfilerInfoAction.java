4fd48279867ee1c95097143ac0d4c3a2311fe299
Gregory.Shrago <gregory.shrago@jetbrains.com>
2020-11-18 06:35:00 +0300

    cached value profiler: open snapshot from notification; cleanup
    GitOrigin-RevId: f9b23cde3b04f07d60dc2a1a64f8f7daf0bd1ad6

--- platform/platform-impl/src/com/intellij/internal/cachedValueProfiler/DumpCachedValueProfilerInfoAction.java
+++ platform/platform-impl/src/com/intellij/internal/cachedValueProfiler/DumpCachedValueProfilerInfoAction.java
@@ -2,24 +2,25 @@
 package com.intellij.internal.cachedValueProfiler;
 
 import com.intellij.ide.actions.RevealFileAction;
-import com.intellij.notification.NotificationDisplayType;
-import com.intellij.notification.NotificationGroup;
-import com.intellij.notification.NotificationType;
+import com.intellij.notification.*;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.PathManager;
+import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VfsUtilCore;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.util.io.URLUtil;
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.event.HyperlinkEvent;
 import java.io.File;
 import java.io.IOException;
 import java.text.MessageFormat;
 
 public class DumpCachedValueProfilerInfoAction extends DumbAwareAction {
-  private static class Holder {
-    private static final NotificationGroup GROUP = new NotificationGroup("Cached value profiling", NotificationDisplayType.BALLOON, false);
-  }
 
   @Override
   public void actionPerformed(@NotNull AnActionEvent e) {
@@ -30,15 +31,34 @@ public class DumpCachedValueProfilerInfoAction extends DumbAwareAction {
   }
 
   static void dumpResults(Project project) {
+    NotificationGroup group = NotificationGroupManager.getInstance().getNotificationGroup("Cached value profiling");
     try {
       File file = CachedValueProfilerDumper.dumpResults(new File(PathManager.getLogPath()));
       String url = FileUtil.getUrl(file);
-      String message = MessageFormat.format("Cached value snapshot ''{0}'' is captured. <a href=\"{1}\">Show in {2}</a>.",
-                                            file, url, RevealFileAction.getFileManagerName());
-      Holder.GROUP.createNotification("", message, NotificationType.INFORMATION, RevealFileAction.FILE_SELECTING_LISTENER).notify(project);
+      String message = MessageFormat.format("Cached values snapshot is captured to<br>" +
+                                            "{0}.<br>" +
+                                            "<a href=\"open:{1}\">Open in Editor</a><br/>" +
+                                            "<a href=\"{1}\">{2}</a>",
+                                            file.getPath(), url, RevealFileAction.getActionName());
+      group.createNotification("", message, NotificationType.INFORMATION, new NotificationListener.Adapter() {
+        @Override
+        protected void hyperlinkActivated(@NotNull Notification notification,
+                                          @NotNull HyperlinkEvent e) {
+          if (e.getDescription().startsWith("open:")) {
+            VirtualFile virtualFile = project.isDisposed() ? null : LocalFileSystem.getInstance().findFileByPath(
+              VfsUtilCore.urlToPath(VfsUtilCore.fixURLforIDEA(URLUtil.unescapePercentSequences(e.getDescription().substring(5)))));
+            if (virtualFile != null) {
+              new OpenFileDescriptor(project, virtualFile).navigate(true);
+            }
+          }
+          else {
+            RevealFileAction.FILE_SELECTING_LISTENER.hyperlinkUpdate(notification, e);
+          }
+        }
+      }).notify(project);
     }
     catch (IOException exception) {
-      Holder.GROUP.createNotification("Failed to capture snapshot: " + exception.getMessage(), NotificationType.ERROR).notify(project);
+      group.createNotification("Failed to capture snapshot: " + exception.getMessage(), NotificationType.ERROR).notify(project);
     }
   }
 }
