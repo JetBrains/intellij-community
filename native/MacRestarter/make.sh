db915532b856656fa6e9253aa4b603e762144795
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2020-11-19 21:32:01 +0100

    [build scripts] exit codes in macOS restarter build script
    (cherry picked from commit 816dc678446bd84ba22bb5e158bfa18f86ac7b23)
    
    Change-Id: Id8e6a66c5dc1128ad9a2080578ec48cc94e81a99

--- native/MacRestarter/make.sh
+++ native/MacRestarter/make.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
+
+# Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 rm -rf build
 mkdir build
-cd build
+cd build || exit 1
 
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make VERBOSE=1
+cmake -DCMAKE_BUILD_TYPE=Release .. || exit 2
+
+make VERBOSE=1 || exit 3
 
 if [ "$1" = "install" ]; then
-make install/strip VERBOSE=1
+  make install/strip VERBOSE=1
 fi
