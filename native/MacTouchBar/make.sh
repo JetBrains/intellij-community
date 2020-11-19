c2cd9bb0c37432bd1edb5a8f10832cb6ec8cb546
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2020-11-19 22:48:35 +0100

    [build scripts] exit codes in macOS file watcher and TouchBar support build scripts
    (cherry picked from commit 1c456f25eac7800eed11cbbcd308f09128bb76f2)
    
    GitOrigin-RevId: 7f4ad66267e05360a832fed605e7162b0fc2d85e

--- native/MacTouchBar/make.sh
+++ native/MacTouchBar/make.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
+
 rm -rf build
 mkdir build
-cd build
+cd build || exit 1
 
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make VERBOSE=1
+cmake -DCMAKE_BUILD_TYPE=Release .. || exit 2
+
+make VERBOSE=1 || exit 3
 
 if [ "$1" = "install" ]; then
-make install/strip VERBOSE=1
+  make install/strip VERBOSE=1 || exit 4
 fi
