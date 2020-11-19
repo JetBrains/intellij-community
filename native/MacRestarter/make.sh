648af21a056ee62d31e05d64d2d160b4edf827ef
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2020-11-19 21:33:52 +0100

    Cleanup (unsolicited copyright header)
    GitOrigin-RevId: f3ae1b4a48e9b3ebd41d9edb3cd0db8ac495f986

--- native/MacRestarter/make.sh
+++ native/MacRestarter/make.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 rm -rf build
 mkdir build
 cd build || exit 1
@@ -10,5 +9,5 @@ cmake -DCMAKE_BUILD_TYPE=Release .. || exit 2
 make VERBOSE=1 || exit 3
 
 if [ "$1" = "install" ]; then
-  make install/strip VERBOSE=1
+  make install/strip VERBOSE=1 || exit 4
 fi
