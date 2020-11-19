f2449a1b49b19fe84f77146a8a1a6e4dbaa1faad
Dmitry Jemerov <yole@jetbrains.com>
2020-11-19 14:27:35 +0100

    Code review feedback (IDEA-CR-68890)
    GitOrigin-RevId: 5b043ae236209e1bb2982c1fb2444168c37eae36

--- platform/lang-api/src/com/intellij/execution/runners/ExecutionEnvironment.java
+++ platform/lang-api/src/com/intellij/execution/runners/ExecutionEnvironment.java
@@ -2,10 +2,7 @@
 package com.intellij.execution.runners;
 
 import com.intellij.execution.*;
-import com.intellij.execution.configurations.ConfigurationPerRunnerSettings;
-import com.intellij.execution.configurations.RunProfile;
-import com.intellij.execution.configurations.RunProfileState;
-import com.intellij.execution.configurations.RunnerSettings;
+import com.intellij.execution.configurations.*;
 import com.intellij.execution.target.*;
 import com.intellij.execution.target.local.LocalTargetEnvironmentFactory;
 import com.intellij.execution.ui.RunContentDescriptor;
@@ -137,7 +134,8 @@ public final class ExecutionEnvironment extends UserDataHolderBase implements Di
                                                              TargetEnvironmentAwareRunProfileState.@NotNull TargetProgressIndicator targetProgressIndicator)
     throws ExecutionException {
     TargetEnvironmentFactory factory = null;
-    if (runProfileState instanceof TargetEnvironmentAwareRunProfileState) {
+    if (runProfileState instanceof TargetEnvironmentAwareRunProfileState &&
+        myRunProfile instanceof TargetEnvironmentAwareRunProfile && ((TargetEnvironmentAwareRunProfile) myRunProfile).getDefaultTargetName() == null) {
       factory = ((TargetEnvironmentAwareRunProfileState) runProfileState).createCustomTargetEnvironmentFactory();
     }
     if (factory == null) {
