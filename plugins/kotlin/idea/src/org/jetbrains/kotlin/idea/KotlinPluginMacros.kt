f591df51d9fb85fcd3627afadff81ccda9763feb
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-11-19 17:50:56 +0300

    Don't set KOTLIN_BUNDLED in unit tests
    Original commit: 1b559fe676103331ec00ad1a482f276ce6d60e03
    
    GitOrigin-RevId: bbcf728cdc44a6601836935a770a9b9c405c28c9

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/KotlinPluginMacros.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/KotlinPluginMacros.kt
@@ -7,6 +7,7 @@ package org.jetbrains.kotlin.idea
 
 import com.intellij.openapi.application.PathMacroContributor
 import org.jetbrains.kotlin.idea.artifacts.KotlinArtifacts
+import org.jetbrains.kotlin.idea.util.application.isUnitTestMode
 
 /**
  * Some actions have to be performed before loading and opening any project.
@@ -19,7 +20,9 @@ class KotlinPluginMacros : PathMacroContributor {
     }
 
     override fun forceRegisterPathMacros(macros: MutableMap<String, String>) {
-        macros[KOTLIN_BUNDLED_PATH_VARIABLE] = KotlinArtifacts.instance.kotlincDirectory.path
+        if (!isUnitTestMode()) {
+            macros[KOTLIN_BUNDLED_PATH_VARIABLE] = KotlinArtifacts.instance.kotlincDirectory.path
+        }
     }
 
     companion object {
