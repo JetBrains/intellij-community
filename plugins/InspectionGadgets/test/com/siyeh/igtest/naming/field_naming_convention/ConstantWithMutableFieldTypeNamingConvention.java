838558d8f10bb0544f391d4cc83a02ab8aec1601
Anna Kozlova <anna.kozlova@jetbrains.com>
2020-11-19 21:55:27 +0100

    mutable classes: allow classes with only final "immutable" fields (IDEA-221473)
    GitOrigin-RevId: b016645411df5f3af5ca06592d5a5fabca0cfa7f

--- plugins/InspectionGadgets/test/com/siyeh/igtest/naming/field_naming_convention/ConstantWithMutableFieldTypeNamingConvention.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/naming/field_naming_convention/ConstantWithMutableFieldTypeNamingConvention.java
@@ -4,4 +4,15 @@ import java.util.*;
 
 class ConstantNamingConvention {
   static final List<String> unmodifiableList = Collections.unmodifiableList(new ArrayList<>());
+  static final MyImmutable immutable = new MyImmutable(0, "name");
+}
+
+final class MyImmutable {
+  private final int idx;
+  private final String name;
+
+  MyImmutable(int idx, String name) {
+    this.idx = idx;
+    this.name = name;
+  }
 }
