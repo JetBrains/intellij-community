dbb236b1665f520cbe0ebfb12238361981d591bf
Bart van Helvert <b.a.j.v.helvert@student.tue.nl>
2020-11-23 16:22:08 +0100

    [UAST] IDEA-254704 Convert illegal package dependencies inspection to UAST
    Converts the illegal package dependencies inspection to an UAST inspection and adds tests for Kotlin and Java.
    
    GitOrigin-RevId: 81fa5e8c4928277f9fd62b57161d2437d2604991

---
+++ jvm/jvm-analysis-java-tests/testSrc/com/intellij/codeInspection/tests/java/JavaDependencyInspectionTest.kt
@@ -0,0 +1,42 @@
+package com.intellij.codeInspection.tests.java
+
+import com.intellij.codeInspection.tests.DependencyInspectionTestBase
+import com.intellij.packageDependencies.DependencyRule
+import com.intellij.packageDependencies.DependencyValidationManager
+import com.intellij.psi.search.scope.packageSet.NamedScope
+import com.intellij.psi.search.scope.packageSet.PackageSetFactory
+
+class JavaDependencyInspectionTest : DependencyInspectionTestBase() {
+  override fun setUp() {
+    super.setUp()
+    myFixture.addFileToProject("$clientFileName.java", """
+      package com.jetbrains.test.client;
+      
+      import <error descr="$errorMessage">com.jetbrains.test.api.Foo</error>;
+      
+      class Client {
+        public static void main(String[] args) {
+          new <error descr="$errorMessage">Foo</error>();
+        } 
+      }
+    """.trimIndent())
+    myFixture.addFileToProject("$apiFileName.java", """
+      package com.jetbrains.test.api;
+      
+      public class Foo() { };
+    """.trimIndent())
+  }
+
+  fun `test dependency`() {
+    val clientScope = NamedScope(clientFileName, PackageSetFactory.getInstance().compile("file:$clientFileName.java"))
+    val libraryScope = NamedScope(apiFileName, PackageSetFactory.getInstance().compile("file:$apiFileName.java"))
+    DependencyValidationManager.getInstance(project).addRule(DependencyRule(clientScope, libraryScope, true))
+    myFixture.testHighlighting("$clientFileName.java")
+  }
+
+  companion object {
+    const val clientFileName: String = "ClientFile"
+    const val apiFileName: String = "ApiFile"
+    const val errorMessage = "Dependency rule 'Deny usages of scope '$apiFileName' in scope '$clientFileName'.' is violated"
+  }
+}
---
+++ jvm/jvm-analysis-kotlin-tests/testSrc/com/intellij/codeInspection/KotlinDependencyInspectionTest.kt
@@ -0,0 +1,43 @@
+package com.intellij.codeInspection
+
+import com.intellij.codeInspection.tests.DependencyInspectionTestBase
+import com.intellij.jvm.analysis.JvmAnalysisKtTestsUtil
+import com.intellij.packageDependencies.DependencyRule
+import com.intellij.packageDependencies.DependencyValidationManager
+import com.intellij.psi.search.scope.packageSet.NamedScope
+import com.intellij.psi.search.scope.packageSet.PackageSetFactory
+
+class KotlinDependencyInspectionTest : DependencyInspectionTestBase() {
+  override fun getBasePath(): String = "${JvmAnalysisKtTestsUtil.TEST_DATA_PROJECT_RELATIVE_BASE_PATH}/codeInspection/dependency"
+
+  override fun setUp() {
+    super.setUp()
+    myFixture.addFileToProject("$clientFileName.kt", """
+      package com.jetbrains.test.client
+      
+      import com.jetbrains.test.api.<error descr="$errorMessage">Foo</error>
+      
+      fun main() {
+        <error descr="$errorMessage">Foo</error>()
+      }
+    """.trimIndent())
+    myFixture.addFileToProject("$apiFileName.kt", """
+      package com.jetbrains.test.api
+      
+      class Foo()
+    """.trimIndent())
+  }
+
+  fun `test dependency`() {
+    val clientScope = NamedScope(clientFileName, PackageSetFactory.getInstance().compile("file:$clientFileName.kt"))
+    val libraryScope = NamedScope(apiFileName, PackageSetFactory.getInstance().compile("file:$apiFileName.kt"))
+    DependencyValidationManager.getInstance(project).addRule(DependencyRule(clientScope, libraryScope, true))
+    myFixture.testHighlighting("$clientFileName.kt")
+  }
+
+  companion object {
+    const val clientFileName: String = "ClientFile"
+    const val apiFileName: String = "ApiFile"
+    const val errorMessage = "Dependency rule 'Deny usages of scope '$apiFileName' in scope '$clientFileName'.' is violated"
+  }
+}
