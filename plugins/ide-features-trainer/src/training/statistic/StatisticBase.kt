6efc18ad656975b434324030713324273114420c
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2020-11-23 22:18:56 +0300

    [IFT] Update statistics
    * Log progress separated from current lesson
    * Report course language instead of lesson language
    * Log expand course action
    * Log start of module from Welcome Screen
    
    IDEA-CR-69098
    
    (cherry picked from commit 210933347207e67b9c9fe67b52f69eef7bd665bc)
    (cherry picked from commit 35c6d84efc2660eee9683b90f23cf92821ec71c3)
    
    GitOrigin-RevId: 3861e9da9ccb523b223d91ccf49aaea789d81174

--- plugins/ide-features-trainer/src/training/statistic/StatisticBase.kt
+++ plugins/ide-features-trainer/src/training/statistic/StatisticBase.kt
@@ -6,19 +6,19 @@ import com.intellij.internal.statistic.service.fus.collectors.FUCounterUsageLogg
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.util.TimeoutUtil
+import training.lang.LangManager
+import training.learn.CourseManager
 import training.learn.interfaces.Lesson
 import training.learn.interfaces.Module
+import training.statistic.FeatureUsageStatisticConsts.COMPLETED_COUNT
+import training.statistic.FeatureUsageStatisticConsts.COURSE_SIZE
 import training.statistic.FeatureUsageStatisticConsts.DURATION
-import training.statistic.FeatureUsageStatisticConsts.GROUP_COLLAPSED
-import training.statistic.FeatureUsageStatisticConsts.GROUP_EVENT
-import training.statistic.FeatureUsageStatisticConsts.GROUP_EXPANDED
-import training.statistic.FeatureUsageStatisticConsts.GROUP_NAME
-import training.statistic.FeatureUsageStatisticConsts.GROUP_STATE
+import training.statistic.FeatureUsageStatisticConsts.EXPAND_WELCOME_PANEL
 import training.statistic.FeatureUsageStatisticConsts.LANGUAGE
 import training.statistic.FeatureUsageStatisticConsts.LESSON_ID
 import training.statistic.FeatureUsageStatisticConsts.MODULE_NAME
 import training.statistic.FeatureUsageStatisticConsts.PASSED
-import training.statistic.FeatureUsageStatisticConsts.PROGRESS_PERCENTAGE
+import training.statistic.FeatureUsageStatisticConsts.PROGRESS
 import training.statistic.FeatureUsageStatisticConsts.START
 import training.statistic.FeatureUsageStatisticConsts.START_MODULE_ACTION
 import java.util.concurrent.ConcurrentHashMap
@@ -39,8 +39,7 @@ class StatisticBase {
     sessionLessonTimestamp[lesson.id] = System.nanoTime()
     logEvent(START, FeatureUsageData()
       .addData(LESSON_ID, lesson.id)
-      .addData(PROGRESS_PERCENTAGE, calcProgressPercentage(lesson.module))
-      .addData(LANGUAGE, lesson.lang.toLowerCase()))
+      .addData(LANGUAGE, courseLanguage()))
   }
 
   fun onPassLesson(lesson: Lesson) {
@@ -52,44 +51,32 @@ class StatisticBase {
     val delta = TimeoutUtil.getDurationMillis(timestamp)
     logEvent(PASSED, FeatureUsageData()
       .addData(LESSON_ID, lesson.id)
-      .addData(LANGUAGE, lesson.lang.toLowerCase())
-      .addData(PROGRESS_PERCENTAGE, calcProgressPercentage(lesson.module))
+      .addData(LANGUAGE, courseLanguage())
       .addData(DURATION, delta))
+
+    logEvent(PROGRESS, FeatureUsageData()
+      .addData(LESSON_ID, lesson.id)
+      .addData(COMPLETED_COUNT, completedCount())
+      .addData(COURSE_SIZE, CourseManager.instance.lessonsForModules.size)
+      .addData(LANGUAGE, courseLanguage()))
   }
 
   private fun logEvent(event: String, featureUsageData: FeatureUsageData) {
     FUCounterUsageLogger.getInstance().logEvent(GROUP_ID, event, featureUsageData)
   }
 
-  /**
-   * @state if the state has changed to expanded = true
-   */
-  fun onGroupStateChanged(groupName: String, state: Boolean) {
-    val expandedCollapsed = if (state) GROUP_EXPANDED else GROUP_COLLAPSED
-    val whiteListedGroupName = try {
-      GroupNames.valueOf(groupName.toUpperCase())
-    }
-    catch (e: IllegalArgumentException) {
-      LOG.warn("Unable to parse group name for collision/expanding event.")
-      return
-    }
-    logEvent(GROUP_EVENT, FeatureUsageData()
-      .addData(GROUP_NAME, whiteListedGroupName.name)
-      .addData(GROUP_STATE, expandedCollapsed))
-  }
-
-  fun onStartModuleAction(module: Module, lesson: Lesson) {
+  fun onStartModuleAction(module: Module) {
     logEvent(START_MODULE_ACTION, FeatureUsageData()
       .addData(MODULE_NAME, module.name)
-      .addData(LESSON_ID, lesson.id)
-      .addData(PROGRESS_PERCENTAGE, calcProgressPercentage(module))
-      .addData(LANGUAGE, lesson.lang.toLowerCase()))
+      .addData(LANGUAGE, courseLanguage()))
   }
 
-  private fun calcProgressPercentage(module: Module): Int {
-    val passedLessons: Int = module.lessons.filter { it.passed }.count()
-    val totalLessons: Int = module.lessons.count()
-    return passedLessons * 100 / totalLessons
+  fun onExpandWelcomeScreenPanel() {
+    logEvent(EXPAND_WELCOME_PANEL, FeatureUsageData()
+      .addData(LANGUAGE, courseLanguage()))
   }
 
+  private fun courseLanguage() = LangManager.getInstance().getLangSupport()?.primaryLanguage?.toLowerCase() ?: ""
+
+  private fun completedCount(): Int = CourseManager.instance.lessonsForModules.count { it.passed }
 }
