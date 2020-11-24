2014756a022b3dc430b6111cbb4cf064da1f4ff2
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2020-11-24 11:03:40 +0100

    shared-indexes: prebuilt indexes generation should use license server, not a license.key (that points to the same license server)
    GitOrigin-RevId: bba6f8c76f09efdcc6c02ddca7573cf55185ea22

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/ProprietaryBuildTools.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/ProprietaryBuildTools.groovy
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.intellij.build
 
 import groovy.transform.Canonical
@@ -25,7 +11,7 @@ import org.jetbrains.intellij.build.fus.FeatureUsageStatisticsProperties
 @CompileStatic
 @Canonical
 class ProprietaryBuildTools {
-  public static final ProprietaryBuildTools DUMMY = new ProprietaryBuildTools(null, null, null, null, null)
+  public static final ProprietaryBuildTools DUMMY = new ProprietaryBuildTools(null, null, null, null, null, null)
 
   /**
    * This tool is required to sign *.exe files in Windows distribution. If it is {@code null} the files won't be signed and Windows may show
@@ -53,4 +39,10 @@ class ProprietaryBuildTools {
    * Properties required to bundle a default version of feature usage statistics white list into IDE
    */
   FeatureUsageStatisticsProperties featureUsageStatisticsProperties
+
+  /**
+   * Generation of shared indexes and other tasks may require a valid license to run,
+   * specify the license server URL to avoid hard-coding any license.
+   */
+  String licenseServerHost
 }
