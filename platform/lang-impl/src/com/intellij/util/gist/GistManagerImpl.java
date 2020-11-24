6be94b17af8336a9d2146cc687d980736a91b398
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2020-11-24 14:46:24 +0100

    IDEA-256068 clear gist ID on plugin unloading
    GistManagerImpl stores IDs of gists to track situations when the same ID is used for different gists. But it doesn't clear IDs on a dynamic plugin unloading. Now, in addition to an ID, a weak reference to a gist itself is stored. It's expected that when the plugin is unloaded, the gist is unloaded as well clearing the ID. Thus, when the plugin is loaded back, it can create the gist again using the same ID.
    
    GitOrigin-RevId: beab4a10c27b2ef8daffee1a6c4834ec871c156a

--- platform/lang-impl/src/com/intellij/util/gist/GistManagerImpl.java
+++ platform/lang-impl/src/com/intellij/util/gist/GistManagerImpl.java
@@ -20,12 +20,12 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.TestOnly;
 
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public final class GistManagerImpl extends GistManager {
   private static final Logger LOG = Logger.getInstance(GistManagerImpl.class);
-  private static final Set<String> ourKnownIds = ContainerUtil.newConcurrentSet();
+  private static final Map<String, VirtualFileGist<?>> ourGists = ContainerUtil.createConcurrentWeakValueMap();
   private static final String ourPropertyName = "file.gist.reindex.count";
   private final AtomicInteger myReindexCount = new AtomicInteger(PropertiesComponent.getInstance().getInt(ourPropertyName, 0));
 
@@ -51,11 +51,12 @@ public final class GistManagerImpl extends GistManager {
                                                          int version,
                                                          @NotNull DataExternalizer<Data> externalizer,
                                                          @NotNull VirtualFileGist.GistCalculator<Data> calcData) {
-    if (!ourKnownIds.add(id)) {
+    if (ourGists.get(id) != null) {
       throw new IllegalArgumentException("Gist '" + id + "' is already registered");
     }
 
-    return new VirtualFileGistImpl<>(id, version, externalizer, calcData);
+    //noinspection unchecked
+    return (VirtualFileGist<Data>)ourGists.computeIfAbsent(id, __ -> new VirtualFileGistImpl<>(id, version, externalizer, calcData));
   }
 
   @NotNull
