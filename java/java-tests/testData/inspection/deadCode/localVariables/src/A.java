12b7de5e77a7579cf32b9f286c19fa346f701cc0
Ilyas Selimov <ilyas.selimov@jetbrains.com>
2020-11-25 13:23:17 +0700

    IDEA-224056 - handle unused local declarations in lambda expressions
    GitOrigin-RevId: 5861b958e9ca05deec08ec9ae8f271b60a6b8a16

--- java/java-tests/testData/inspection/deadCode/localVariables/src/A.java
+++ java/java-tests/testData/inspection/deadCode/localVariables/src/A.java
@@ -1,7 +1,35 @@
+import java.util.*;
+
 class A {
   public static void main(String[] args) {
     int i = 0;
     @SuppressWarnings("unused") int j = 0;
+    int k;
+
+    initializeCons(value -> {
+        String unusedStr1 = "s";
+        @SuppressWarnings("unused") String unusedStr2 = "s";
+        {
+          String unusedStr3;
+        }
+        String unusedStr4 = "str";
+        initializeCons(innerValue1 -> {
+          {
+            String unusedStr5 = unusedStr4;
+          }
+          String unusedStr6 = "s";
+          String unusedStr7;
+          initializeSup(() -> unusedStr6);
+        });
+    });
+  }
+
+  private static void initializeCons(Consumer<String> consumer) {
+    consumer.accept("String to test");
+  }
+
+  private static void initializeSup(Supplier<String> supplier) {
+    supplier.get();
   }
 }
 
