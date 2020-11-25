514112ada9efea952c9f391165e62b2657212948
Sergey Karashevich <sergey.karashevich@jetbrains.com>
2020-11-25 21:40:02 +0100

    [IFT] fix font and styles initialization
    (cherry picked from commit d556c3c3afe1234e33fcde990107ddb404ce0e90)
    
    IDEA-CR-69220
    
    GitOrigin-RevId: efb566775c39c4e701fc92d3a1add0136fdfd5f9

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/learnIde/LearnIdeContentColorsAndFonts.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/learnIde/LearnIdeContentColorsAndFonts.kt
@@ -1,12 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.wm.impl.welcomeScreen.learnIde
 
-import com.intellij.ide.ui.UISettings
 import com.intellij.ui.JBColor
-import com.intellij.util.ui.JBFont
-import com.intellij.util.ui.UIUtil
-import java.awt.Font
-import javax.swing.JLabel
 import javax.swing.text.SimpleAttributeSet
 import javax.swing.text.StyleConstants
 
@@ -14,69 +9,23 @@ object LearnIdeContentColorsAndFonts {
 
   val ActiveInteractiveCoursesBorder = JBColor.namedColor("Component.focusedBorderColor", JBColor(0x87AFDA, 0x466d94))
   val InactiveInteractiveCoursesBorder = JBColor.namedColor("Component.borderColor", JBColor(0xC4C4C4, 0x5E6060))
-
-  private val settings: UISettings by lazy { UISettings.instance }
-  private val fontSize: Int by lazy { if (settings.overrideLafFonts) settings.fontSize else JBFont.label().size }
-  private val fontFace: String by lazy { settings.fontFace ?: JLabel().font.fontName }
-
-  val HeaderFont: Font by lazy { Font(fontFace, Font.BOLD, fontSize + 5) }
   val HeaderColor = JBColor.namedColor("ParameterInfo.foreground", JBColor(0x1D1D1D, 0xBBBBBB))
-
-
   val HoveredColor = JBColor.namedColor("Plugins.lightSelectionBackground", JBColor(0xEDF6FE, 0x464A4D))
-  val REGULAR = SimpleAttributeSet()
-
   val ModuleHeaderColor = JBColor.namedColor("link", JBColor(0x2470B3, 0x589DF6))
   val ModuleDescriptionColor = JBColor.namedColor("infoPanelForeground", JBColor(0x808080, 0x8C8C8C))
-  val MODULE_HEADER = SimpleAttributeSet()
-  val MODULE_DESCRIPTION = SimpleAttributeSet()
 
   val PARAGRAPH_STYLE = SimpleAttributeSet()
-  val HEADER = SimpleAttributeSet()
 
   init {
+    applyZeroParagraphStyle(PARAGRAPH_STYLE)
+  }
 
-    val labelFontName = UIUtil.getLabelFont().fontName
-
-    StyleConstants.setFontSize(REGULAR, UISettings.instance.fontSize - 1)
-    StyleConstants.setFontFamily(REGULAR, labelFontName)
-    StyleConstants.setForeground(REGULAR, HeaderColor)
-
-    StyleConstants.setLeftIndent(REGULAR, 0.0f)
-    StyleConstants.setRightIndent(REGULAR, 0f)
-    StyleConstants.setSpaceAbove(REGULAR, 0.0f)
-    StyleConstants.setSpaceBelow(REGULAR, 0.0f)
-    StyleConstants.setLineSpacing(REGULAR, 0.0f)
-
-    StyleConstants.setFontSize(HEADER, HeaderFont.size)
-    StyleConstants.setFontFamily(HEADER, labelFontName)
-    StyleConstants.setForeground(HEADER, HeaderColor)
-    StyleConstants.setBold(HEADER, true)
-
-    StyleConstants.setLeftIndent(PARAGRAPH_STYLE, 0.0f)
-    StyleConstants.setRightIndent(PARAGRAPH_STYLE, 0f)
-    StyleConstants.setSpaceAbove(PARAGRAPH_STYLE, 0.0f)
-    StyleConstants.setSpaceBelow(PARAGRAPH_STYLE, 0.0f)
-    StyleConstants.setLineSpacing(PARAGRAPH_STYLE, 0.0f)
-
-    StyleConstants.setFontFamily(MODULE_HEADER, labelFontName)
-    StyleConstants.setFontSize(MODULE_HEADER, UISettings.instance.fontSize - 1)
-    StyleConstants.setForeground(MODULE_HEADER, ModuleHeaderColor)
-    StyleConstants.setLeftIndent(MODULE_HEADER, 0.0f)
-    StyleConstants.setRightIndent(MODULE_HEADER, 0f)
-    StyleConstants.setSpaceAbove(MODULE_HEADER, 0.0f)
-    StyleConstants.setSpaceBelow(MODULE_HEADER, 0.0f)
-    StyleConstants.setLineSpacing(MODULE_HEADER, 0.0f)
-
-    StyleConstants.setFontFamily(MODULE_DESCRIPTION, labelFontName)
-    StyleConstants.setFontSize(MODULE_DESCRIPTION, UISettings.instance.fontSize - 2)
-    StyleConstants.setForeground(MODULE_DESCRIPTION, ModuleDescriptionColor)
-    StyleConstants.setLeftIndent(MODULE_DESCRIPTION, 0.0f)
-    StyleConstants.setRightIndent(MODULE_DESCRIPTION, 0f)
-    StyleConstants.setSpaceAbove(MODULE_DESCRIPTION, 0.0f)
-    StyleConstants.setSpaceBelow(MODULE_DESCRIPTION, 0.0f)
-    StyleConstants.setLineSpacing(MODULE_DESCRIPTION, 0.0f)
-
+  private fun applyZeroParagraphStyle(simpleAttributeSet: SimpleAttributeSet) {
+    StyleConstants.setLeftIndent(simpleAttributeSet, 0.0f)
+    StyleConstants.setRightIndent(simpleAttributeSet, 0f)
+    StyleConstants.setSpaceAbove(simpleAttributeSet, 0.0f)
+    StyleConstants.setSpaceBelow(simpleAttributeSet, 0.0f)
+    StyleConstants.setLineSpacing(simpleAttributeSet, 0.0f)
   }
 
 }
