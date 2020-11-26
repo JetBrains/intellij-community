e6c0f48346aedf9dd43686e739ecababd1d4572a
Viktor Shatrov <Viktor.Shatrov@jetbrains.com>
2020-11-26 13:09:19 +0300

    Inline Debugger: Hebrew/Arabic support in inline value tooltips (WEB-48092)
    (cherry picked from commit a50f4ba4de90f1f86ad8b84cb903290d85dcc3e4)
    
    IDEA-CR-69225
    
    GitOrigin-RevId: bae0bcc2a266a01fa146951fa8a43b38e9402681

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/inline/InlineDebugRenderer.java
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/inline/InlineDebugRenderer.java
@@ -7,11 +7,9 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.EditorCustomElementRenderer;
 import com.intellij.openapi.editor.Inlay;
 import com.intellij.openapi.editor.colors.EditorColorsScheme;
-import com.intellij.openapi.editor.colors.FontPreferences;
 import com.intellij.openapi.editor.colors.TextAttributesKey;
 import com.intellij.openapi.editor.event.EditorMouseEvent;
 import com.intellij.openapi.editor.ex.EditorEx;
-import com.intellij.openapi.editor.impl.ComplementaryFontsRegistry;
 import com.intellij.openapi.editor.impl.EditorImpl;
 import com.intellij.openapi.editor.impl.FontInfo;
 import com.intellij.openapi.editor.markup.EffectType;
@@ -23,6 +21,7 @@ import com.intellij.ui.SimpleTextAttributes;
 import com.intellij.ui.paint.EffectPainter;
 import com.intellij.util.Producer;
 import com.intellij.util.ui.GraphicsUtil;
+import com.intellij.util.ui.UIUtil;
 import com.intellij.xdebugger.impl.frame.XWatchesView;
 import com.intellij.xdebugger.impl.ui.XDebuggerUIConstants;
 import com.intellij.xdebugger.impl.ui.tree.nodes.XValueNodeImpl;
@@ -63,13 +62,11 @@ final class InlineDebugRenderer implements EditorCustomElementRenderer {
     myOnClick = onClick;
   }
 
-  private static FontInfo getFontInfo(@NotNull Editor editor) {
+  private static Font getFont(@NotNull Editor editor) {
     EditorColorsScheme colorsScheme = editor.getColorsScheme();
-    FontPreferences fontPreferences = colorsScheme.getFontPreferences();
     TextAttributes attributes = editor.getColorsScheme().getAttributes(DebuggerColors.INLINED_VALUES_EXECUTION_LINE);
     int fontStyle = attributes == null ? Font.PLAIN : attributes.getFontType();
-    return ComplementaryFontsRegistry.getFontAbleToDisplay('a', fontStyle, fontPreferences,
-                                                           FontInfo.getFontRenderContext(editor.getContentComponent()));
+    return UIUtil.getFontWithFallback(colorsScheme.getEditorFontName(), fontStyle, colorsScheme.getEditorFontSize());
   }
 
 
@@ -123,14 +120,19 @@ final class InlineDebugRenderer implements EditorCustomElementRenderer {
   }
 
   private int getInlayTextWidth(@NotNull Inlay inlay) {
-    FontInfo fontInfo = getFontInfo(inlay.getEditor());
+    Font font = getFont(inlay.getEditor());
     String text;
     if (isErrorMessage()) {
       text = myText.getTexts().get(0);
     } else {
       text = myText.toString() + NAME_VALUE_SEPARATION;
     }
-    return fontInfo.fontMetrics().stringWidth(text + INDENT);
+    return getFontMetrics(font, inlay.getEditor()).stringWidth(text + INDENT);
+  }
+
+  @NotNull
+  private static FontMetrics getFontMetrics(Font font, @NotNull Editor editor) {
+    return FontInfo.getFontMetrics(font, FontInfo.getFontRenderContext(editor.getContentComponent()));
   }
 
   private static final float BACKGROUND_ALPHA = 0.55f;
@@ -146,9 +148,9 @@ final class InlineDebugRenderer implements EditorCustomElementRenderer {
     TextAttributes inlineAttributes = getAttributes(editor);
     if (inlineAttributes == null || inlineAttributes.getForegroundColor() == null) return;
 
-    FontInfo fontInfo = getFontInfo(editor);
-    g.setFont(fontInfo.getFont());
-    FontMetrics metrics = fontInfo.fontMetrics();
+    Font font = getFont(editor);
+    g.setFont(font);
+    FontMetrics metrics = getFontMetrics(font, editor);
 
     int gap = 1;//(r.height < fontMetrics.lineHeight + 2) ? 1 : 2;
     int margin = metrics.charWidth(' ') / 4;
@@ -181,7 +183,7 @@ final class InlineDebugRenderer implements EditorCustomElementRenderer {
       Color fgColor = isHovered ? inlineAttributes.getForegroundColor() : attr.getFgColor();
       g.setColor(fgColor);
       g.drawString(curText, curX, r.y + inlay.getEditor().getAscent());
-      curX += fontInfo.fontMetrics().stringWidth(curText);
+      curX += metrics.stringWidth(curText);
       if (isErrorMessage()) {
         break;
       }
@@ -197,7 +199,7 @@ final class InlineDebugRenderer implements EditorCustomElementRenderer {
       icon.paintIcon(inlay.getEditor().getComponent(), g, curX, getIconY(icon, r));
     }
 
-    paintEffects(g, r, editor, inlineAttributes, fontInfo, metrics);
+    paintEffects(g, r, editor, inlineAttributes, font, metrics);
   }
 
   private boolean isErrorMessage() {
@@ -208,7 +210,7 @@ final class InlineDebugRenderer implements EditorCustomElementRenderer {
                                    @NotNull Rectangle r,
                                    EditorImpl editor,
                                    TextAttributes inlineAttributes,
-                                   FontInfo fontInfo,
+                                   Font font,
                                    FontMetrics metrics) {
     Color effectColor = inlineAttributes.getEffectColor();
     EffectType effectType = inlineAttributes.getEffectType();
@@ -219,24 +221,19 @@ final class InlineDebugRenderer implements EditorCustomElementRenderer {
       int xEnd = r.x + r.width;
       int y = r.y + metrics.getAscent();
       if (effectType == EffectType.LINE_UNDERSCORE) {
-        EffectPainter.LINE_UNDERSCORE.paint(g2d, xStart, y, xEnd - xStart, metrics.getDescent(), fontInfo
-          .getFont());
+        EffectPainter.LINE_UNDERSCORE.paint(g2d, xStart, y, xEnd - xStart, metrics.getDescent(), font);
       }
       else if (effectType == EffectType.BOLD_LINE_UNDERSCORE) {
-        EffectPainter.BOLD_LINE_UNDERSCORE.paint(g2d, xStart, y, xEnd - xStart, metrics
-          .getDescent(), fontInfo.getFont());
+        EffectPainter.BOLD_LINE_UNDERSCORE.paint(g2d, xStart, y, xEnd - xStart, metrics.getDescent(), font);
       }
       else if (effectType == EffectType.STRIKEOUT) {
-        EffectPainter.STRIKE_THROUGH.paint(g2d, xStart, y, xEnd - xStart, editor.getCharHeight(), fontInfo
-          .getFont());
+        EffectPainter.STRIKE_THROUGH.paint(g2d, xStart, y, xEnd - xStart, editor.getCharHeight(), font);
       }
       else if (effectType == EffectType.WAVE_UNDERSCORE) {
-        EffectPainter.WAVE_UNDERSCORE.paint(g2d, xStart, y, xEnd - xStart, metrics.getDescent(), fontInfo
-          .getFont());
+        EffectPainter.WAVE_UNDERSCORE.paint(g2d, xStart, y, xEnd - xStart, metrics.getDescent(), font);
       }
       else if (effectType == EffectType.BOLD_DOTTED_LINE) {
-        EffectPainter.BOLD_DOTTED_UNDERSCORE.paint(g2d, xStart, y, xEnd - xStart, metrics
-          .getDescent(), fontInfo.getFont());
+        EffectPainter.BOLD_DOTTED_UNDERSCORE.paint(g2d, xStart, y, xEnd - xStart, metrics.getDescent(), font);
       }
     }
   }
