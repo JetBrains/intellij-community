062fb5c284852f2eaecf4dd4afa2c330c0ebe38c
Roman Artemev <roman.artemev@jetbrains.com>
2020-11-27 12:12:04 +0300

    [IC] Add in-module tests for incremental compilation
    Two tests to check recompilation when parameter with default value was
    added or removed without changing use-site.
    
    Original commit: 96ba3b873e99392544219c3b8832c2115dc23dab
    
    GitOrigin-RevId: f994a339b7cbc5f46a6f5e8ac809614b1adfc6e5

--- plugins/kotlin/jps/jps-plugin/tests/test/org/jetbrains/kotlin/jps/build/IncrementalJvmJpsTestGenerated.java
+++ plugins/kotlin/jps/jps-plugin/tests/test/org/jetbrains/kotlin/jps/build/IncrementalJvmJpsTestGenerated.java
@@ -634,6 +634,16 @@ public abstract class IncrementalJvmJpsTestGenerated extends AbstractIncremental
             runTest("testData/incremental/pureKotlin/packageRemoved/");
         }
 
+        @TestMetadata("parameterWithDefaultValueAdded")
+        public void testParameterWithDefaultValueAdded() throws Exception {
+            runTest("jps-plugin/testData/incremental/pureKotlin/parameterWithDefaultValueAdded/");
+        }
+
+        @TestMetadata("parameterWithDefaultValueRemoved")
+        public void testParameterWithDefaultValueRemoved() throws Exception {
+            runTest("jps-plugin/testData/incremental/pureKotlin/parameterWithDefaultValueRemoved/");
+        }
+
         @TestMetadata("privateConstantsChanged")
         public void testPrivateConstantsChanged() throws Exception {
             runTest("testData/incremental/pureKotlin/privateConstantsChanged/");
