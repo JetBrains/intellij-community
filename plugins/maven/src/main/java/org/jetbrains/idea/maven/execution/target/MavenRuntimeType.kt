ecf3de3170d77899f704cffb23400f8c98d7b998
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2020-11-27 18:44:12 +0300

    IJ-CR-4262: refactoring
    (cherry picked from commit bb20024b7558c8b2a3d92e6fa18abdad8b563574)
    
    IJ-CR-4262
    
    GitOrigin-RevId: 307b974cac91d2e2c7e4388a38e67890876e76fc

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/execution/target/MavenRuntimeType.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/execution/target/MavenRuntimeType.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.maven.execution.target
 
-import com.intellij.execution.Platform
 import com.intellij.execution.RunnerAndConfigurationSettings
 import com.intellij.execution.target.LanguageRuntimeType
 import com.intellij.execution.target.TargetEnvironmentConfiguration
@@ -9,11 +8,8 @@ import com.intellij.openapi.components.PersistentStateComponent
 import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.NlsSafe
-import com.intellij.openapi.util.text.StringUtil
 import icons.MavenIcons
 import org.jetbrains.idea.maven.execution.RunnerBundle
-import java.util.concurrent.CompletableFuture
-import java.util.concurrent.CompletableFuture.*
 
 class MavenRuntimeType : LanguageRuntimeType<MavenRuntimeTargetConfiguration>(TYPE_ID) {
   override val icon = MavenIcons.ExecuteMavenGoal
@@ -43,47 +39,7 @@ class MavenRuntimeType : LanguageRuntimeType<MavenRuntimeTargetConfiguration>(TY
 
   override fun createIntrospector(config: MavenRuntimeTargetConfiguration): Introspector<MavenRuntimeTargetConfiguration>? {
     if (config.homePath.isNotBlank()) return null
-
-    return object : Introspector<MavenRuntimeTargetConfiguration> {
-      override fun introspect(subject: Introspectable): CompletableFuture<MavenRuntimeTargetConfiguration> {
-        var isWindows = false
-        val introspectionPromise = subject.promiseExecuteScript("ver")
-          .thenApply { isWindows = it?.contains("Microsoft Windows") ?: false }
-          .thenCompose { promiseMavenVersion(subject, "M2_HOME", isWindows) }
-          .thenCompose { it?.let { completedFuture(it) } ?: promiseMavenVersion(subject, "MAVEN_HOME", isWindows) }
-          .thenCompose { it?.let { completedFuture(it) } ?: promiseMavenVersion(subject, null, isWindows) }
-          .thenApply { (mavenHome, versionOutput) ->
-            if (versionOutput != null) {
-              val lines = StringUtil.splitByLines(versionOutput, true)
-              (mavenHome ?: lines.find { it.startsWith("Maven home: ") }?.substringAfter("Maven home: "))?.let {
-                config.homePath = it
-                config.versionString = lines.firstOrNull() ?: ""
-              }
-            }
-          }
-        return introspectionPromise.thenApply { config }
-      }
-    }
-  }
-
-  private fun promiseMavenVersion(subject: Introspectable,
-                                  mavenHomeEnvVariable: String?,
-                                  isWindows: Boolean): CompletableFuture<Pair<String?, String?>> {
-    if (mavenHomeEnvVariable == null) {
-      return subject.promiseExecuteScript("mvn -version").thenCompose { completedFuture(null to it) }
-    }
-
-    return subject.promiseEnvironmentVariable(mavenHomeEnvVariable).thenCompose { mavenHome ->
-      if (mavenHome == null) {
-        return@thenCompose completedFuture(null)
-      }
-      else {
-        val fileSeparator = if (isWindows) Platform.WINDOWS.fileSeparator else Platform.UNIX.fileSeparator
-        val mvnScriptPath = arrayOf(mavenHome, "bin", "mvn").joinToString(fileSeparator.toString())
-        return@thenCompose subject.promiseExecuteScript("$mvnScriptPath -version")
-          .thenCompose { completedFuture(mavenHome to it) }
-      }
-    }
+    return MavenTargetConfigurationIntrospector(config)
   }
 
   override fun duplicateConfig(config: MavenRuntimeTargetConfiguration): MavenRuntimeTargetConfiguration =
