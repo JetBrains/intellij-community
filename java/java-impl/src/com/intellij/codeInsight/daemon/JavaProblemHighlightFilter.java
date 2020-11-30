3f0806a4e0783144da0b69d1524f11facd4d8883
Bas Leijdekkers <basleijdekkers@gmail.com>
2020-11-30 18:31:15 +0100

    Faster check for Java hash bang script (IDEA-256481)
    GitOrigin-RevId: 0c492917a3f425753ae3a5b9196d7f7b42d8661b

--- java/java-impl/src/com/intellij/codeInsight/daemon/JavaProblemHighlightFilter.java
+++ java/java-impl/src/com/intellij/codeInsight/daemon/JavaProblemHighlightFilter.java
@@ -1,38 +1,33 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.daemon;
 
-import com.intellij.codeInsight.daemon.impl.analysis.HighlightClassUtil;
 import com.intellij.ide.highlighter.JavaFileType;
 import com.intellij.ide.scratch.ScratchUtil;
 import com.intellij.openapi.roots.JavaProjectRootsUtil;
 import com.intellij.openapi.roots.ProjectFileIndex;
 import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 
 
 public class JavaProblemHighlightFilter extends ProblemHighlightFilter {
   @Override
   public boolean shouldHighlight(@NotNull PsiFile psiFile) {
-    return psiFile.getFileType() != JavaFileType.INSTANCE ||
-           !JavaProjectRootsUtil.isOutsideJavaSourceRoot(psiFile) ||
-           ScratchUtil.isScratch(psiFile.getVirtualFile()) ||
-           HighlightClassUtil.isJavaHashBangScript(psiFile);
+    if (psiFile.getFileType() != JavaFileType.INSTANCE || !JavaProjectRootsUtil.isOutsideJavaSourceRoot(psiFile)) return true;
+    final VirtualFile vFile = psiFile.getVirtualFile();
+    if (ScratchUtil.isScratch(vFile)) return true;
+    try {
+      // Version of HighlightClassUtil.isJavaHashBangScript() that does not build PSI
+      // Time for scanning scope from 18 seconds to 8 seconds on IntelliJ project
+      return VfsUtilCore.loadText(vFile, 5).startsWith("#!");
+    }
+    catch (IOException e) {
+      return false;
+    }
   }
 
   @Override
