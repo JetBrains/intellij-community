9a99add7f3f4df4e840b75fc6cb7d8fe3b33298f
Boris Pristupa <boris.pristupa@jetbrains.com>
2020-11-30 14:55:28 +0300

    add number highlighting to markdown
    GitOrigin-RevId: 596210072d252a4983a2a22171b32a5f0a962e19

---
+++ plugins/markdown/src/org/intellij/plugins/markdown/highlighting/MarkdownNumberHighlightingAnnotator.kt
@@ -0,0 +1,35 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.intellij.plugins.markdown.highlighting
+
+import com.intellij.lang.annotation.AnnotationHolder
+import com.intellij.lang.annotation.Annotator
+import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
+import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiElement
+import com.intellij.psi.impl.source.tree.LeafPsiElement
+
+class MarkdownNumberHighlightingAnnotator : Annotator {
+
+  override fun annotate(element: PsiElement, holder: AnnotationHolder) {
+    if (element !is LeafPsiElement) {
+      return
+    }
+
+    val offset = element.startOffset
+
+    for (match in numberRegex.findAll(element.text)) {
+      holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+        .range(TextRange.create(offset + match.range.first, offset + match.range.last + 1))
+        .textAttributes(numberTextAttributesKey)
+        .create()
+    }
+  }
+
+  companion object {
+    private val numberTextAttributesKey =
+      TextAttributesKey.createTextAttributesKey("MARKDOWN_NUMBER", DefaultLanguageHighlighterColors.NUMBER)
+    private val numberRegex = Regex("""\d(\d|_)*([.,]\d(\d|_)*)?""")
+  }
+}
