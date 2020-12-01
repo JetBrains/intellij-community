ab3b050a6b11341ec74f3b0278b0646ec27f2958
Dmitry.Krasilschikov <kradima@jetbrains.com>
2020-12-01 20:02:27 +0200

    fix JavaWebReferenceTest test
    GitOrigin-RevId: a32dae47b98023deb7a559afe31ef9620c84d668

--- java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/JavaWebReferenceTest.java
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/JavaWebReferenceTest.java
@@ -3,6 +3,7 @@ package com.intellij.java.codeInsight.daemon;
 
 import com.intellij.ide.highlighter.JavaFileType;
 import com.intellij.model.psi.PsiSymbolReference;
+import com.intellij.openapi.util.SystemInfoRt;
 import com.intellij.openapi.util.registry.Registry;
 import com.intellij.psi.PsiFile;
 import com.intellij.testFramework.PlatformTestUtil;
@@ -25,7 +26,8 @@ public class JavaWebReferenceTest extends LightJavaCodeInsightFixtureTestCase {
   }
 
   public void testHighlighting() {
-    getReferences("class Hi { String url=\"<info descr=\"Open in browser (Ctrl+Click, Ctrl+B)\">http://foo</info>\"; }");
+    getReferences("class Hi { String url=\"<info descr=\"Open in browser (" +
+                  (SystemInfoRt.isMac ? "⌘click, ⌘B" : "Ctrl+Click, Ctrl+B") + ")\">http://foo</info>\"; }");
     myFixture.testHighlighting(true, true, true);
   }
 
