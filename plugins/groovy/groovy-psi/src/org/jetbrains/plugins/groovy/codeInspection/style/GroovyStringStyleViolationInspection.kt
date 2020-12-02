54b18eeb6dcb193bf8c37e952a62383f79bc07bc
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2020-12-02 16:49:53 +0300

    [groovy] IDEA-144807: Complete inspections for plain strings
    GitOrigin-RevId: 39d4e1bd8fdb457ab8747e9a6407bee5446d0358

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/codeInspection/style/GroovyStringStyleViolationInspection.kt
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/codeInspection/style/GroovyStringStyleViolationInspection.kt
@@ -9,6 +9,11 @@ import com.intellij.util.ui.JBUI
 import org.jetbrains.annotations.Nls
 import org.jetbrains.plugins.groovy.codeInspection.BaseInspection
 import org.jetbrains.plugins.groovy.codeInspection.BaseInspectionVisitor
+import org.jetbrains.plugins.groovy.codeInspection.style.GroovyStringStyleViolationInspection.ErrorKind.*
+import org.jetbrains.plugins.groovy.codeInspection.style.GroovyStringStyleViolationInspection.StringKind.*
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.literals.GrLiteral
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.literals.GrString
+import org.jetbrains.plugins.groovy.lang.psi.util.GrStringUtil
 import java.awt.BorderLayout
 import java.awt.GridBagConstraints
 import java.awt.GridBagLayout
@@ -22,7 +27,8 @@ class GroovyStringStyleViolationInspection : BaseInspection() {
     DOUBLE_QUOTED,
     SINGLE_QUOTED,
     SLASHY,
-    TRIPLE_QUOTED;
+    TRIPLE_QUOTED,
+    TRIPLE_DOUBLE_QUOTED;
 
     override fun toString(): String {
       return when (this) {
@@ -31,18 +37,22 @@ class GroovyStringStyleViolationInspection : BaseInspection() {
         SINGLE_QUOTED -> "Single-quoted string"
         SLASHY -> "Slashy string"
         TRIPLE_QUOTED -> "Triple-quoted string"
+        TRIPLE_DOUBLE_QUOTED -> "Triple-double-quoted string"
       }
     }
   }
 
   @Volatile
-  var currentVersion = StringKind.DOUBLE_QUOTED
+  var currentVersion = DOUBLE_QUOTED
 
   @Volatile
-  var escapeVersion = StringKind.UNDEFINED
+  var escapeVersion = UNDEFINED
 
   @Volatile
-  var interpolationVersion = StringKind.UNDEFINED
+  var interpolationVersion = UNDEFINED
+
+  @Volatile
+  var multilineVersion = TRIPLE_QUOTED
 
   @Volatile
   var inspectGradle: Boolean = true
@@ -54,7 +64,6 @@ class GroovyStringStyleViolationInspection : BaseInspection() {
   private fun JPanel.addStringKindComboBox(@Nls description: String,
                                            field: KMutableProperty<StringKind>,
                                            values: Array<StringKind>,
-                                           defaultValue: StringKind,
                                            id: Int,
                                            constraints: GridBagConstraints) {
     constraints.gridy = id
@@ -63,7 +72,7 @@ class GroovyStringStyleViolationInspection : BaseInspection() {
     val comboBox: JComboBox<StringKind> = ComboBox(values)
     comboBox.renderer = SimpleListCellRenderer.create("") { it.toString() }
 
-    comboBox.selectedItem = defaultValue
+    comboBox.selectedItem = field.getter.call()
 
     comboBox.addItemListener { e ->
       val selectedItem = e.item
@@ -75,15 +84,40 @@ class GroovyStringStyleViolationInspection : BaseInspection() {
     add(comboBox, constraints)
   }
 
-  private fun generateComboBoxes(panel: JPanel) = panel.apply {
+  private fun generateComboBoxes(): JPanel = JPanel(GridBagLayout()).apply {
     val constraints = GridBagConstraints().apply {
       weightx = 1.0; weighty = 1.0; fill = GridBagConstraints.HORIZONTAL; anchor = GridBagConstraints.WEST
     }
-    val activeStringKinds = arrayOf(StringKind.DOUBLE_QUOTED, StringKind.SINGLE_QUOTED, StringKind.SLASHY, StringKind.TRIPLE_QUOTED)
-    addStringKindComboBox("Default", ::currentVersion, activeStringKinds, StringKind.DOUBLE_QUOTED, 0, constraints)
-    addStringKindComboBox("Strings with escaping", ::escapeVersion, StringKind.values(), StringKind.UNDEFINED, 1, constraints)
-    addStringKindComboBox("Strings with interpolation", ::interpolationVersion, StringKind.values(), StringKind.UNDEFINED, 2, constraints)
+    val activeStringKinds = arrayOf(DOUBLE_QUOTED, SINGLE_QUOTED, SLASHY, TRIPLE_QUOTED)
+    addStringKindComboBox("Default", ::currentVersion, activeStringKinds, 0, constraints)
+    addStringKindComboBox("Strings with escaping", ::escapeVersion, StringKind.values(), 1, constraints)
+    addStringKindComboBox("Strings with interpolation", ::interpolationVersion, StringKind.values(), 2, constraints)
+    addStringKindComboBox("Multiline string", ::multilineVersion, arrayOf(UNDEFINED, TRIPLE_QUOTED, TRIPLE_DOUBLE_QUOTED), 3, constraints)
   }
+
+  private enum class ErrorKind {
+    PLAIN_STRING_SHOULD_BE_DOUBLE_QUOTED,
+    PLAIN_STRING_SHOULD_BE_SINGLE_QUOTED,
+    PLAIN_STRING_SHOULD_BE_SLASHY_QUOTED,
+    PLAIN_STRING_SHOULD_BE_TRIPLE_QUOTED,
+    PLAIN_STRING_SHOULD_BE_DOUBLY_TRIPLE_QUOTED,
+    MULTILINE_STRING_SHOULD_BE_TRIPLE_QUOTED,
+    MULTILINE_STRING_SHOULD_BE_DOUBLY_TRIPLE_QUOTED
+  }
+
+
+  override fun buildErrorString(vararg args: Any?): String {
+    return when (args[0]!! as ErrorKind) {
+      PLAIN_STRING_SHOULD_BE_DOUBLE_QUOTED -> "Plain string should be double-quoted"
+      PLAIN_STRING_SHOULD_BE_SINGLE_QUOTED -> "Plain string should be single-quoted"
+      PLAIN_STRING_SHOULD_BE_SLASHY_QUOTED -> "Plain string should be slashy-quoted"
+      PLAIN_STRING_SHOULD_BE_TRIPLE_QUOTED -> "Plain string should be quoted with '''"
+      PLAIN_STRING_SHOULD_BE_DOUBLY_TRIPLE_QUOTED -> "Plain string should quoted with \"\"\""
+      MULTILINE_STRING_SHOULD_BE_TRIPLE_QUOTED -> "Multiline string should be quoted with '''"
+      MULTILINE_STRING_SHOULD_BE_DOUBLY_TRIPLE_QUOTED -> "Multiline string should be quoted with \"\"\""
+    }
+  }
+
 
   override fun createOptionsPanel(): JComponent {
     val compressingPanel = JPanel(BorderLayout())
@@ -91,20 +125,54 @@ class GroovyStringStyleViolationInspection : BaseInspection() {
     containerPanel.border = JBUI.Borders.empty()
     containerPanel.layout = BoxLayout(containerPanel, BoxLayout.Y_AXIS)
     containerPanel.add(SeparatorFactory.createSeparator("Preferable kind", null))
-    val newPanel = JPanel(GridBagLayout())
-    containerPanel.add(newPanel, BorderLayout.NORTH)
-
-    generateComboBoxes(newPanel)
+    containerPanel.add(generateComboBoxes(), BorderLayout.NORTH)
 
     containerPanel.apply {
       add(SeparatorFactory.createSeparator("Domain of usage", null))
       add(SingleCheckboxOptionsPanel("Inspect Gradle files", this@GroovyStringStyleViolationInspection, "inspectGradle"))
       add(SingleCheckboxOptionsPanel("Inspect script files", this@GroovyStringStyleViolationInspection, "inspectScripts"))
     }
+
     compressingPanel.add(containerPanel, BorderLayout.NORTH)
     return compressingPanel
   }
 
   override fun buildVisitor(): BaseInspectionVisitor = object : BaseInspectionVisitor() {
+    override fun visitLiteralExpression(literal: GrLiteral) {
+      if (literal !is GrString) {
+        if (multilineVersion != UNDEFINED && GrStringUtil.isMultilineStringLiteral(literal)) {
+          checkInconsistency(multilineVersion, literal,
+                             mapOf(TRIPLE_QUOTED to MULTILINE_STRING_SHOULD_BE_TRIPLE_QUOTED,
+                                   TRIPLE_DOUBLE_QUOTED to MULTILINE_STRING_SHOULD_BE_DOUBLY_TRIPLE_QUOTED))
+        }
+        else {
+          checkInconsistency(currentVersion, literal,
+                             mapOf(DOUBLE_QUOTED to PLAIN_STRING_SHOULD_BE_DOUBLE_QUOTED,
+                                   SINGLE_QUOTED to PLAIN_STRING_SHOULD_BE_SINGLE_QUOTED,
+                                   SLASHY to PLAIN_STRING_SHOULD_BE_SLASHY_QUOTED,
+                                   TRIPLE_QUOTED to PLAIN_STRING_SHOULD_BE_TRIPLE_QUOTED,
+                                   TRIPLE_DOUBLE_QUOTED to PLAIN_STRING_SHOULD_BE_DOUBLY_TRIPLE_QUOTED))
+        }
+      }
+      super.visitLiteralExpression(literal)
+    }
+
+
+    private fun checkInconsistency(expected: StringKind, literal: GrLiteral, errorKinds: Map<StringKind, ErrorKind>) {
+      fun doCheck(kind: StringKind, predicate: (GrLiteral) -> Boolean) {
+        val errorKind = errorKinds[expected]
+        if (errorKind != null && expected == kind && !predicate(literal)) {
+          registerError(literal, errorKind)
+        }
+      }
+      doCheck(SINGLE_QUOTED, GrStringUtil::isSingleQuoteString)
+      doCheck(DOUBLE_QUOTED, GrStringUtil::isDoubleQuoteString)
+      doCheck(TRIPLE_QUOTED, GrStringUtil::isTripleQuoteString)
+      doCheck(TRIPLE_DOUBLE_QUOTED, GrStringUtil::isTripleDoubleQuoteString)
+      doCheck(SLASHY, GrStringUtil::isSlashyString)
+    }
+
   }
+
+
 }
