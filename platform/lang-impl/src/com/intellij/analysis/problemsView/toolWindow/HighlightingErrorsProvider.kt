d256e6e224eb9d99e6b621b99da8d87ad817fa8c
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2020-12-02 18:36:26 +0300

    IDEA-256555: Problems View: errors from library code are shown under "Project errors" tab
    GitOrigin-RevId: 3948fb0e7cd15e18bd5821422b965947a88588a3

--- platform/lang-impl/src/com/intellij/analysis/problemsView/toolWindow/HighlightingErrorsProvider.kt
+++ platform/lang-impl/src/com/intellij/analysis/problemsView/toolWindow/HighlightingErrorsProvider.kt
@@ -10,6 +10,7 @@ import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.problems.ProblemListener
 import com.intellij.problems.WolfTheProblemSolver
+import com.intellij.ui.tree.project.ProjectFileNode
 
 internal class HighlightingErrorsProvider(override val project: Project) : ProblemsProvider, ProblemListener {
   companion object {
@@ -28,6 +29,7 @@ internal class HighlightingErrorsProvider(override val project: Project) : Probl
   }
 
   override fun problemsAppeared(file: VirtualFile) {
+    ProjectFileNode.findArea(file, project) ?: return
     synchronized(watchers) {
       watchers.computeIfAbsent(file) { file ->
         HighlightingWatcher(this, ProblemsCollector.getInstance(project), file, ERROR.myVal).also { watcher ->
