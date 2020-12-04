95c503e5cd23345ae484d71aa87f767b07f23b62
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2020-12-04 16:09:41 +0300

    [space] Use DumbAwareAction instead of AnAction
    GitOrigin-RevId: dc9642c894dd58a7dd5d68a75480d71b407b8405

--- plugins/space/src/main/kotlin/com/intellij/space/auth/SpaceAuthNotifier.kt
+++ plugins/space/src/main/kotlin/com/intellij/space/auth/SpaceAuthNotifier.kt
@@ -1,8 +1,8 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.space.auth
 
-import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.space.messages.SpaceBundle
 import com.intellij.space.messages.SpaceBundleExtensions
 import com.intellij.space.settings.SpaceSettingsPanel
@@ -10,7 +10,7 @@ import com.intellij.space.utils.notify
 
 internal object SpaceAuthNotifier {
   fun authFailed() {
-    val tryAgainAction = object : AnAction(SpaceBundleExtensions.messagePointer("auth.notification.failed.login.again.action")) {
+    val tryAgainAction = object : DumbAwareAction(SpaceBundleExtensions.messagePointer("auth.notification.failed.login.again.action")) {
       override fun actionPerformed(e: AnActionEvent) {
         SpaceSettingsPanel.openSettings(null)
       }
