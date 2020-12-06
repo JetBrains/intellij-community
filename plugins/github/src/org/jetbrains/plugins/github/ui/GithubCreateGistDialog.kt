d6d8c8c48160a8019a65cfe848c5d9a68206581a
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2020-12-06 20:22:45 +0300

    github: Refactor "GithubCreateGistDialog" - cleanup
    GitOrigin-RevId: 3f00152452d21724f075d37f7c2e702020e31785

--- plugins/github/src/org/jetbrains/plugins/github/ui/GithubCreateGistDialog.kt
+++ plugins/github/src/org/jetbrains/plugins/github/ui/GithubCreateGistDialog.kt
@@ -12,7 +12,7 @@ import org.jetbrains.plugins.github.authentication.accounts.GithubAccount
 import org.jetbrains.plugins.github.authentication.ui.GHAccountsComboBoxModel
 import org.jetbrains.plugins.github.authentication.ui.GHAccountsComboBoxModel.Companion.accountSelector
 import org.jetbrains.plugins.github.authentication.ui.GHAccountsHost
-import org.jetbrains.plugins.github.i18n.GithubBundle
+import org.jetbrains.plugins.github.i18n.GithubBundle.message
 import javax.swing.JComponent
 import javax.swing.JTextArea
 
@@ -27,42 +27,33 @@ class GithubCreateGistDialog(
 ) : DialogWrapper(project, true),
     DataProvider {
 
-  private val fileNameField: JBTextField? = if (fileName != null) JBTextField(fileName) else null
-  private val descriptionField: JTextArea = JTextArea()
-  private val secretCheckBox: JBCheckBox = JBCheckBox(GithubBundle.message("create.gist.dialog.secret"), secret)
-  private val browserCheckBox: JBCheckBox = JBCheckBox(GithubBundle.message("create.gist.dialog.open.browser"), openInBrowser)
-  private val copyLinkCheckBox: JBCheckBox = JBCheckBox(GithubBundle.message("create.gist.dialog.copy.url"), copyLink)
-
-  val fileName: String?
-    get() = fileNameField?.text
-
-  val description: String
-    get() = descriptionField.text
-
-  val isSecret: Boolean
-    get() = secretCheckBox.isSelected
-
-  val isOpenInBrowser: Boolean
-    get() = browserCheckBox.isSelected
-
-  val isCopyURL: Boolean
-    get() = copyLinkCheckBox.isSelected
+  private val fileNameField = if (fileName != null) JBTextField(fileName) else null
+  private val descriptionField = JTextArea()
+  private val secretCheckBox = JBCheckBox(message("create.gist.dialog.secret"), secret)
+  private val browserCheckBox = JBCheckBox(message("create.gist.dialog.open.browser"), openInBrowser)
+  private val copyLinkCheckBox = JBCheckBox(message("create.gist.dialog.copy.url"), copyLink)
 
   private val accountsModel = GHAccountsComboBoxModel(accounts, defaultAccount ?: accounts.firstOrNull())
+
+  val fileName: String? get() = fileNameField?.text
+  val description: String get() = descriptionField.text
+  val isSecret: Boolean get() = secretCheckBox.isSelected
+  val isOpenInBrowser: Boolean get() = browserCheckBox.isSelected
+  val isCopyURL: Boolean get() = copyLinkCheckBox.isSelected
   val account: GithubAccount? get() = accountsModel.selected
 
   init {
-    title = GithubBundle.message("create.gist.dialog.title")
+    title = message("create.gist.dialog.title")
     init()
   }
 
   override fun createCenterPanel() = panel {
     fileNameField?.let {
-      row(GithubBundle.message("create.gist.dialog.filename.field")) {
+      row(message("create.gist.dialog.filename.field")) {
         it(pushX, growX)
       }
     }
-    row(GithubBundle.message("create.gist.dialog.description.field")) {
+    row(message("create.gist.dialog.description.field")) {
       scrollPane(descriptionField)
     }
     row("") {
@@ -73,23 +64,15 @@ class GithubCreateGistDialog(
       }
     }
     if (accountsModel.size != 1) {
-      row(GithubBundle.message("create.gist.dialog.create.for.field")) {
+      row(message("create.gist.dialog.create.for.field")) {
         accountSelector(accountsModel)
       }
     }
   }
 
-  override fun getHelpId(): String? {
-    return "github.create.gist.dialog"
-  }
-
-  override fun getDimensionServiceKey(): String? {
-    return "Github.CreateGistDialog"
-  }
-
-  override fun getPreferredFocusedComponent(): JComponent? {
-    return descriptionField
-  }
+  override fun getHelpId(): String = "github.create.gist.dialog"
+  override fun getDimensionServiceKey(): String = "Github.CreateGistDialog"
+  override fun getPreferredFocusedComponent(): JComponent = descriptionField
 
   override fun getData(dataId: String): Any? =
     if (GHAccountsHost.KEY.`is`(dataId)) accountsModel
