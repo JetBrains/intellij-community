0f18150b1798f182a85fa2e2c1fbea6f47ca99f3
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2020-12-08 17:53:36 +0300

    fix compilation
    GitOrigin-RevId: 14990c24920139cc118acb5acf650bef1a8aaf23

--- platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/persistent/FSRecords.java
+++ platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/persistent/FSRecords.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vfs.newvfs.persistent;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.PathManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.progress.ProcessCanceledException;
