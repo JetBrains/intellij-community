b16cb3ad1048cdbaac43122f02f01a8e5f68f45d
Kristoffer Andersen <kjaa@google.com>
2020-12-09 17:15:46 +0100

    [IR+Tests] Improve Local Function Debugging Experience
    This change improves the debugging experience around local functions
    on the IR backend. The changes include moving old
    checkLocalVariablesTable (cLVT) tests to the new stepping/local variable
    infrastructure in order to refine the tests and further define the
    behavior of the two JVM backends, and their differences.
    
    The primary ported test case is cLVT/localFun.kt that documents the
    discrepancy in implementation strategy for local functions on the two
    backends. The old backend implements local functions as lambdas
    assigned to a local variable while the IR backend lifts them out as
    static funtions on the surrounding class. The discrepancies and their
    consequences are documented in bytecodeListing, idea-stepping,
    localVariableTable and debugStepping tests.
    
    The only _code change_ is disabling the captured variable name
    mangling for captured variables on the IR backend. Captured variables
    are passed as arguments to the static function, so in the debugger,
    they really just are local variables. For them to show properly in the
    debugger and be detectable by evaluate expression, they simply need no
    mangling.
    
    Finally, this change cleans 3 redundant cLVT tests, copyFunction.kt
    and destructuringInlineLambda.kt and destructuringInFor.kt, that are
    all covered in the new suite. The stepping behavior needs to be made
    precise around for loops, but that is an entirely seperate issue.
    
    Original commit: 43b61a618d47d1c25c1b4f6ca66495ac02f62254
    
    GitOrigin-RevId: ccea9fd51330de12d83ca95b62f6d9123d41e37e

--- plugins/kotlin/jvm-debugger/test/test/org/jetbrains/kotlin/idea/debugger/test/IrKotlinSteppingTestGenerated.java
+++ plugins/kotlin/jvm-debugger/test/test/org/jetbrains/kotlin/idea/debugger/test/IrKotlinSteppingTestGenerated.java
@@ -477,6 +477,16 @@ public abstract class IrKotlinSteppingTestGenerated extends AbstractIrKotlinStep
             runTest("testData/stepping/stepOver/lambdaToInlineMapFiltersDisabled.kt");
         }
 
+        @TestMetadata("localFunction.kt")
+        public void testLocalFunction() throws Exception {
+            runTest("idea/jvm-debugger/jvm-debugger-test/testData/stepping/stepOver/localFunction.kt");
+        }
+
+        @TestMetadata("localFunctionWithSingleLineExpressionBody.kt")
+        public void testLocalFunctionWithSingleLineExpressionBody() throws Exception {
+            runTest("idea/jvm-debugger/jvm-debugger-test/testData/stepping/stepOver/localFunctionWithSingleLineExpressionBody.kt");
+        }
+
         @TestMetadata("noParameterExtensionLambdaArgumentCallInInline.kt")
         public void testNoParameterExtensionLambdaArgumentCallInInline() throws Exception {
             runTest("testData/stepping/stepOver/noParameterExtensionLambdaArgumentCallInInline.kt");
