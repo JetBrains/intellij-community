b0bd8e60be339094ab2c82947b117e080ea434b3
Alexander.Bashkirov <alexander.bashkirov@jetbrains.com>
2020-12-10 17:52:13 +0300

    [diagrams] (IDEA-251446) UML diagrams: initial support for Kotlin
    GitOrigin-RevId: 2a4bad3d3046c6d61b0ff5c81302cbb50b00f849

--- plugins/kotlin/uast/uast-kotlin/src/org/jetbrains/uast/kotlin/declarations/KotlinUFile.kt
+++ plugins/kotlin/uast/uast-kotlin/src/org/jetbrains/uast/kotlin/declarations/KotlinUFile.kt
@@ -16,10 +16,7 @@
 
 package org.jetbrains.uast.kotlin
 
-import com.intellij.psi.PsiClass
-import com.intellij.psi.PsiComment
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiRecursiveElementWalkingVisitor
+import com.intellij.psi.*
 import org.jetbrains.kotlin.asJava.findFacadeClass
 import org.jetbrains.kotlin.asJava.toLightClass
 import org.jetbrains.kotlin.psi.KtClassOrObject
@@ -38,7 +35,19 @@ class KotlinUFile(
     override val annotations: List<UAnnotation>
         get() = psi.annotationEntries.map { KotlinUAnnotation(it, this) }
 
-    override val javaPsi: PsiElement? = null
+    override val javaPsi: PsiClassOwner? by lz {
+        val lightClasses = this@KotlinUFile.classes.map { it.javaPsi }.toTypedArray()
+        val lightFile = lightClasses.asSequence()
+            .mapNotNull { it.containingFile as? PsiClassOwner }
+            .firstOrNull()
+
+        if (lightFile == null) null
+        else
+            object : PsiClassOwner by lightFile {
+                override fun getClasses() = lightClasses
+                override fun setPackageName(packageName: String?) = error("Incorrect operation for non-physical Java PSI")
+            }
+    }
 
     override val sourcePsi: KtFile = psi
 
