a2bf37066390d67e6af8f42ec7b02b9bf7e2cde0
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2020-12-10 12:39:19 +0300

    git-index: Allow configuring if editor diff preview or file itself should be opened on double-click
    (cherry picked from commit f1b20f0ca3959317b06d5e96c0260799e00d042e)
    
    GitOrigin-RevId: a04308468e03969bdb5a436bcdf2b6431e0331b1

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/ShowEditorPreviewOnDoubleClickToggleAction.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/ShowEditorPreviewOnDoubleClickToggleAction.kt
@@ -4,6 +4,7 @@ package com.intellij.openapi.vcs.changes.actions
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareToggleAction
 import com.intellij.openapi.vcs.VcsApplicationSettings
+import com.intellij.openapi.vcs.changes.ChangeListManager
 import com.intellij.openapi.vcs.changes.ChangesViewManager
 
 private class ShowEditorPreviewOnDoubleClickToggleAction : DumbAwareToggleAction() {
@@ -11,7 +12,10 @@ private class ShowEditorPreviewOnDoubleClickToggleAction : DumbAwareToggleAction
     super.update(e)
 
     val changesViewManager = e.project?.let { ChangesViewManager.getInstance(it) as? ChangesViewManager }
-    e.presentation.isEnabledAndVisible = changesViewManager?.isEditorPreview == true
+    val changeListManager = e.project?.let { ChangeListManager.getInstance(it) }
+
+    e.presentation.isEnabledAndVisible =
+      changesViewManager?.isEditorPreview == true || changeListManager?.areChangeListsEnabled() == false
   }
 
   override fun isSelected(e: AnActionEvent): Boolean = VcsApplicationSettings.getInstance().SHOW_EDITOR_PREVIEW_ON_DOUBLE_CLICK
