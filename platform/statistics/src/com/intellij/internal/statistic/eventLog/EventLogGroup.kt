4781b5e14e99d651db77d410dda624350316c7d8
Svetlana.Zemlyanskaya <Svetlana.Zemlyanskaya@jetbrains.com>
2020-12-11 21:13:34 +0100

    FUS: add links between register event and vararg event methods
    GitOrigin-RevId: c13a6115a0f5673c82ac72fa87c574062051705a

--- platform/statistics/src/com/intellij/internal/statistic/eventLog/EventLogGroup.kt
+++ platform/statistics/src/com/intellij/internal/statistic/eventLog/EventLogGroup.kt
@@ -22,6 +22,8 @@ class EventLogGroup(val id: String, val version: Int) {
   /**
    * New style API to record IDE events (e.g. invoked action, opened dialog) or state.
    *
+   * For events with more than 3 fields use EventLogGroup.registerVarargEvent
+   *
    * To implement a new collector:
    * - Record events according to a "fus-collectors.md" dev guide and register it in plugin.xml
    * - Implement custom validation rules if necessary (see SensitiveDataValidator)
@@ -31,6 +33,8 @@ class EventLogGroup(val id: String, val version: Int) {
    * To test collector:
    * - If group is not registered on the server, add it to events test scheme with "Add Group to Events Test Scheme" action.
    *   (com.intellij.internal.statistic.actions.scheme.AddGroupToTestSchemeAction)
+   *
+   * @see registerVarargEvent
    */
   fun registerEvent(eventId: String): EventId {
     return EventId(this, eventId).also { addToRegisteredEvents(it) }
@@ -38,6 +42,9 @@ class EventLogGroup(val id: String, val version: Int) {
 
   /**
    * See docs for com.intellij.internal.statistic.eventLog.EventLogGroup.registerEvent(java.lang.String)
+   *
+   * @see registerEvent
+   * @see registerVarargEvent
    */
   fun <T1> registerEvent(eventId: String, eventField1: EventField<T1>): EventId1<T1> {
     return EventId1(this, eventId, eventField1).also { addToRegisteredEvents(it) }
@@ -45,6 +52,9 @@ class EventLogGroup(val id: String, val version: Int) {
 
   /**
    * See docs for com.intellij.internal.statistic.eventLog.EventLogGroup.registerEvent(java.lang.String)
+   *
+   * @see registerEvent
+   * @see registerVarargEvent
    */
   fun <T1, T2> registerEvent(eventId: String, eventField1: EventField<T1>, eventField2: EventField<T2>): EventId2<T1, T2> {
     return EventId2(this, eventId, eventField1, eventField2).also { addToRegisteredEvents(it) }
@@ -52,6 +62,9 @@ class EventLogGroup(val id: String, val version: Int) {
 
   /**
    * See docs for com.intellij.internal.statistic.eventLog.EventLogGroup.registerEvent(java.lang.String)
+   *
+   * @see registerEvent
+   * @see registerVarargEvent
    */
   fun <T1, T2, T3> registerEvent(eventId: String, eventField1: EventField<T1>, eventField2: EventField<T2>, eventField3: EventField<T3>): EventId3<T1, T2, T3> {
     return EventId3(this, eventId, eventField1, eventField2, eventField3).also { addToRegisteredEvents(it) }
@@ -59,6 +72,8 @@ class EventLogGroup(val id: String, val version: Int) {
 
   /**
    * See docs for com.intellij.internal.statistic.eventLog.EventLogGroup.registerEvent(java.lang.String)
+   *
+   * @see registerEvent
    */
   fun registerVarargEvent(eventId: String, vararg fields: EventField<*>): VarargEventId {
     return VarargEventId(this, eventId, *fields).also { addToRegisteredEvents(it) }
