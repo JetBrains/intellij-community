0f293e82b20ac4e3f292e8f75e7db7ffe7a69227
Ivan Donchevskii <ivan.donchevskii@jetbrains.com>
2020-12-11 14:49:29 +0100

    CPP-28: Add Mac OS X version of QtCreator keymap
    GitOrigin-RevId: 2d72d162d7cdb158db57ea6a1533022e81f4013c

--- platform/platform-impl/src/com/intellij/openapi/keymap/impl/DefaultKeymap.kt
+++ platform/platform-impl/src/com/intellij/openapi/keymap/impl/DefaultKeymap.kt
@@ -151,6 +151,6 @@ private fun isKnownLinuxKeymap(keymapName: String?) = when (keymapName) {
 private fun isKnownMacOSKeymap(keymapName: String?) = when (keymapName) {
   KeymapManager.MAC_OS_X_KEYMAP, KeymapManager.MAC_OS_X_10_5_PLUS_KEYMAP, "macOS System Shortcuts",
   "Eclipse (Mac OS X)", "Sublime Text (Mac OS X)", "Xcode", "ReSharper OSX",
-  "Visual Studio OSX", "Visual Assist OSX", "Visual Studio for Mac", "VSCode OSX" -> true
+  "Visual Studio OSX", "Visual Assist OSX", "Visual Studio for Mac", "VSCode OSX", "QtCreator (Mac OS X)" -> true
   else -> false
 }
