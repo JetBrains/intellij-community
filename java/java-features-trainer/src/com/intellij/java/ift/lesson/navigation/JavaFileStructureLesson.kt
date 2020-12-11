e4af561bcb4f5bfb3819c86f449a8eee5ff8cd0e
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2020-12-11 18:43:29 +0300

    [IFT] Edit navigate task in FileStructureLesson and refactor
    GitOrigin-RevId: d24371ac5f86592bc04c84132222cafa6c402eb1

--- java/java-features-trainer/src/com/intellij/java/ift/lesson/navigation/JavaFileStructureLesson.kt
+++ java/java-features-trainer/src/com/intellij/java/ift/lesson/navigation/JavaFileStructureLesson.kt
@@ -1,12 +1,11 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.java.ift.lesson.navigation
 
+import com.intellij.openapi.editor.LogicalPosition
 import training.learn.interfaces.Module
 import training.learn.lesson.general.navigation.FileStructureLesson
 
 class JavaFileStructureLesson(module: Module) : FileStructureLesson(module, "JAVA") {
-  override val searchSubstring: String = "hosa"
-  override val firstWord: String = "homo"
-  override val secondWord: String = "sapience"
   override val existedFile: String = "src/FileStructureDemo.java"
+  override val methodToFindPosition: LogicalPosition = LogicalPosition(74, 16)
 }
