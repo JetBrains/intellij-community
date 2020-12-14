710bdbf8a62e8f35f572b8560d88809ad91b72c8
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2020-12-14 13:38:29 +0100

    jdk-downloader - IDEA-257556 - show target platform of JDKs in the downloader
    (cherry picked from commit 82478b47fa808d3fe38d569215ced59578aafba1)
    
    IJ-MR-4619
    
    GitOrigin-RevId: d2e6c4abc79c572767252797fa63c8e74ea00a21

--- platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/jdkDownloader/JdkDownloadDialog.kt
+++ platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/jdkDownloader/JdkDownloadDialog.kt
@@ -30,7 +30,8 @@ class JdkDownloaderModel(
   val versionGroups: List<JdkVersionItem>,
   val defaultItem: JdkItem,
   val defaultVersion: JdkVersionItem,
-  val defaultVersionVendor: JdkVersionVendorItem
+  val defaultVersionVendor: JdkVersionVendorItem,
+  val hasMultipleArchs: Boolean
 )
 
 class JdkVersionItem(
@@ -71,7 +72,7 @@ class JdkVersionVendorItem(
   val canBeSelected: Boolean get() = parent == null
 }
 
-private class JdkVersionVendorCombobox: ComboBox<JdkVersionVendorElement>() {
+private class JdkVersionVendorCombobox(private val hasMultipleArchs : Boolean): ComboBox<JdkVersionVendorElement>() {
   private val myActionItemSelectedListeners = mutableListOf<(item: JdkVersionVendorItem) -> Unit>()
 
   override fun setSelectedItem(anObject: Any?) {
@@ -124,6 +125,9 @@ private class JdkVersionVendorCombobox: ComboBox<JdkVersionVendorElement>() {
         val jdkVersion = value.item.jdkVersion
         if (jdkVersion != value.parent?.jdkVersion) {
           append(" $jdkVersion", SimpleTextAttributes.GRAYED_ATTRIBUTES, false)
+          if (hasMultipleArchs) {
+            append(" ${value.item.arch}", SimpleTextAttributes.GRAYED_ATTRIBUTES, false)
+          }
         }
       }
     }
@@ -193,7 +197,8 @@ fun buildJdkDownloaderModel(allItems: List<JdkItem>): JdkDownloaderModel {
     versionGroups = versionItems,
     defaultItem = defaultItem,
     defaultVersion = defaultJdkVersionItem,
-    defaultVersionVendor = defaultVersionVendor
+    defaultVersionVendor = defaultVersionVendor,
+    hasMultipleArchs = allItems.mapTo(HashSet()) { it.arch }.size > 1
   )
 }
 
@@ -230,7 +235,7 @@ internal class JdkDownloadDialog(
     versionComboBox.renderer = jdkVersionItemRenderer
     versionComboBox.isSwingPopup = false
 
-    vendorComboBox = JdkVersionVendorCombobox()
+    vendorComboBox = JdkVersionVendorCombobox(model.hasMultipleArchs)
 
     installDirTextField = textFieldWithBrowseButton(
       project = project,
