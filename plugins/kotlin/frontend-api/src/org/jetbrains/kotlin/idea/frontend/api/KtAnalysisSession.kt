5ee025113af663357250cf71a30ca1ad9cdb0560
Roman Golyshev <roman.golyshev@jetbrains.com>
2020-12-21 15:22:12 +0300

    FIR IDE: Add reference shortening service which works over FIR
    Original commit: c1130f20106a0684f84973b3af8ee4494801b7f1
    
    GitOrigin-RevId: 5ea55fb9d351e2b19648e5c93a7612a80ffb6855

--- plugins/kotlin/frontend-api/src/org/jetbrains/kotlin/idea/frontend/api/KtAnalysisSession.kt
+++ plugins/kotlin/frontend-api/src/org/jetbrains/kotlin/idea/frontend/api/KtAnalysisSession.kt
@@ -46,6 +46,7 @@ abstract class KtAnalysisSession(final override val token: ValidityToken) : Vali
     protected abstract val callResolver: KtCallResolver
     protected abstract val completionCandidateChecker: KtCompletionCandidateChecker
     protected abstract val symbolDeclarationOverridesProvider: KtSymbolDeclarationOverridesProvider
+    protected abstract val referenceShortener: KtReferenceShortener
 
     @Suppress("LeakingThis")
 
@@ -179,4 +180,6 @@ abstract class KtAnalysisSession(final override val token: ValidityToken) : Vali
 
     fun KtReturnExpression.getReturnTargetSymbol(): KtCallableSymbol? =
         expressionHandlingComponent.getReturnExpressionTargetSymbol(this)
+
+    fun collectPossibleReferenceShortenings(file: KtFile, from: Int, to: Int) = referenceShortener.collectShortenings(file, from, to)
 }
