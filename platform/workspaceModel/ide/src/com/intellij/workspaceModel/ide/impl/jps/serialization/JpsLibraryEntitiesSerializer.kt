813126ea8500efc095f9cc387f6c5129a0cf2b4a
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2020-12-21 20:07:27 +0300

    [workspace model] speed up loading modules which contains many libraries (IDEA-257596)
    If a module has N unnamed module-level libraries, the previous version will generate artifical #xxx names for them in O(N^3). Now it works in O(N) by using HashSet instead of ArrayList and by remembering the last used index.
    
    (cherry picked from commit 73d3a556e52a24a753de3732c7a1df9137354451)
    
    IDEA-CR-69916
    
    GitOrigin-RevId: 9efeac1a81ae2f46ddd5b6bfa5c42188ebbf340c

--- platform/workspaceModel/ide/src/com/intellij/workspaceModel/ide/impl/jps/serialization/JpsLibraryEntitiesSerializer.kt
+++ platform/workspaceModel/ide/src/com/intellij/workspaceModel/ide/impl/jps/serialization/JpsLibraryEntitiesSerializer.kt
@@ -205,7 +205,7 @@ internal fun saveLibrary(library: LibraryEntity, externalSystemId: String?): Ele
 }
 
 private val ROOT_TYPES_TO_WRITE_EMPTY_TAG = listOf("CLASSES", "SOURCES", "JAVADOC").map { libraryRootTypes[it]!! }
-private const val UNNAMED_LIBRARY_NAME_PREFIX = "#"
+internal const val UNNAMED_LIBRARY_NAME_PREFIX = "#"
 private const val UNIQUE_INDEX_LIBRARY_NAME_SUFFIX = "-d1a6f608-UNIQUE-INDEX-f29c-4df6-"
 
 fun getLegacyLibraryName(libraryId: LibraryId): String? {
@@ -216,8 +216,6 @@ fun getLegacyLibraryName(libraryId: LibraryId): String? {
 
 fun generateLibraryEntityName(legacyLibraryName: String?, exists: (String) -> Boolean): String {
   if (legacyLibraryName == null) {
-    // TODO Make it O(1) if required
-
     var index = 1
     while (true) {
       val candidate = "$UNNAMED_LIBRARY_NAME_PREFIX$index"
@@ -227,16 +225,19 @@ fun generateLibraryEntityName(legacyLibraryName: String?, exists: (String) -> Bo
 
       index++
     }
-
     @Suppress("UNREACHABLE_CODE")
     error("Unable to suggest unique name for unnamed module library")
   }
 
-  if (!exists(legacyLibraryName)) return legacyLibraryName
+  return generateUniqueLibraryName(legacyLibraryName, exists)
+}
+
+internal fun generateUniqueLibraryName(name: String, exists: (String) -> Boolean): String {
+  if (!exists(name)) return name
 
   var index = 1
   while (true) {
-    val candidate = "$legacyLibraryName$UNIQUE_INDEX_LIBRARY_NAME_SUFFIX$index"
+    val candidate = "$name$UNIQUE_INDEX_LIBRARY_NAME_SUFFIX$index"
     if (!exists(candidate)) {
       return candidate
     }
