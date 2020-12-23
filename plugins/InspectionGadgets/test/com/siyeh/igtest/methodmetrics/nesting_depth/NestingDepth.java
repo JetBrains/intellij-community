8ceae4cc1cfb6cfd0bfa665128fa7edf027588af
Bas Leijdekkers <basleijdekkers@gmail.com>
2020-12-23 15:06:06 +0100

    IG: calculate nesting depth for for-eaches, lambdas and switch expressions (IDEA-258369)
    GitOrigin-RevId: 619ff8ebc220359f07c22b8568069c6f7a7b59c2

--- plugins/InspectionGadgets/test/com/siyeh/igtest/methodmetrics/nesting_depth/NestingDepth.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/methodmetrics/nesting_depth/NestingDepth.java
@@ -30,4 +30,21 @@ public class NestingDepth
         return true;
     }
 
+    void <warning descr="'lambdas' is overly nested (maximum nesting depth = 6)">lambdas</warning>() {
+      Runnable r = () -> {
+        Runnable s = () -> {
+          Runnable t = () -> {
+            Runnable u = () -> {
+              for (String str : new String[] {""}) {
+                str = switch (1) {
+                  case 0 -> "zero";
+                  case 1 -> "one";
+                  default -> "many";
+                };
+              }
+            };
+          };
+        };
+      };
+    }
 }
