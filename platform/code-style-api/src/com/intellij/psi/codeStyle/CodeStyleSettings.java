acde2c08372032275cf81b099b2226a04ae15b4b
Raluca Sauciuc <ralucas@google.com>
2020-12-23 20:32:43 -0800

    Preserve code style defaults
    This is a last resort solution to keep Android defaults. The patch
    from commit 698d76f no longer applies cleanly, because
    PredefinedCodeStyle has been pushed into an upstream module.
    
    Bug: 175998141
    Change-Id: I996b8e8810a5bb7c7970f5ef38268a75284baecc

--- platform/code-style-api/src/com/intellij/psi/codeStyle/CodeStyleSettings.java
+++ platform/code-style-api/src/com/intellij/psi/codeStyle/CodeStyleSettings.java
@@ -98,10 +98,18 @@ public class CodeStyleSettings extends LegacyCodeStyleSettings implements Clonea
 
     if (loadExtensions) {
       for (final CustomCodeStyleSettingsFactory factory : CodeStyleSettingsService.getInstance().getCustomCodeStyleSettingsFactories()) {
+        if (!factory.getClass().getSimpleName().startsWith("AndroidStudio")) { // Android Studio: b/175998141
         myCustomCodeStyleSettingsManager.addCustomSettings(factory.createCustomSettings(this));
+        }
       }
       for (CustomCodeStyleSettingsFactory factory : CodeStyleSettingsService.getInstance().getSettingsPagesProviders()) {
         myCustomCodeStyleSettingsManager.addCustomSettings(factory.createCustomSettings(this));
+      }
+      // Android Studio: also apply our predefined Android code styles (b/175998141).
+      for (final CustomCodeStyleSettingsFactory factory : CodeStyleSettingsService.getInstance().getCustomCodeStyleSettingsFactories()) {
+        if (factory.getClass().getSimpleName().startsWith("AndroidStudio")) {
+          myCustomCodeStyleSettingsManager.addCustomSettings(factory.createCustomSettings(this));
+        }
       }
     }
 
