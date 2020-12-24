87f2e93f9a275da7531606e822f46e96015cc7c1
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2020-12-25 00:49:35 +0300

    wsl target: simplify code by migrating Ref<WSLDistribution?> to WSLDistribution?
    GitOrigin-RevId: 517c504f0b367b0168a90086d97345071a6d6577

--- platform/platform-impl/src/com/intellij/execution/wsl/target/WslTargetConfigurable.kt
+++ platform/platform-impl/src/com/intellij/execution/wsl/target/WslTargetConfigurable.kt
@@ -10,7 +10,6 @@ import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.options.BoundConfigurable
 import com.intellij.openapi.ui.ComboBox
 import com.intellij.openapi.ui.DialogPanel
-import com.intellij.openapi.util.Ref
 import com.intellij.ui.CollectionComboBoxModel
 import com.intellij.ui.ColoredListCellRenderer
 import com.intellij.ui.layout.*
@@ -21,12 +20,10 @@ class WslTargetConfigurable(val config: WslTargetEnvironmentConfiguration) :
 
   override fun createPanel(): DialogPanel = panel {
     row(label = IdeBundle.message("wsl.linux.distribution.label")) {
-      val comboBox: ComboBox<Ref<WSLDistribution?>> = createComboBox()
+      val comboBox: ComboBox<WSLDistribution?> = createComboBox()
       comboBox().withBinding(
-        { c ->
-          (c.selectedItem as? Ref<*>)?.get() as? WSLDistribution
-        },
-        { c, v -> c.selectedItem = Ref.create(v) },
+        { c -> c.selectedItem as? WSLDistribution },
+        { c, v -> c.selectedItem = v },
         PropertyBinding(
           { config.distribution },
           { config.distribution = it }
@@ -35,15 +32,22 @@ class WslTargetConfigurable(val config: WslTargetEnvironmentConfiguration) :
     }
   }
 
-  private fun createComboBox(): ComboBox<Ref<WSLDistribution?>> {
-    val selectedDistribution = config.distribution
-    val selectedItem = Ref.create(selectedDistribution)
-    val model = CollectionComboBoxModel(mutableListOf(selectedItem))
-    val comboBox: ComboBox<Ref<WSLDistribution?>> = ComboBox()
+  private fun createComboBox(): ComboBox<WSLDistribution?> {
+    val model = CollectionComboBoxModel<WSLDistribution?>()
+    val comboBox: ComboBox<WSLDistribution?> = ComboBox()
     comboBox.model = model
     comboBox.renderer = WslDistributionRenderer()
-    comboBox.selectedItem = selectedItem
-    comboBox.prototypeDisplayValue = Ref.create()
+
+    val selectedDistribution: WSLDistribution? = config.distribution
+    if (selectedDistribution != null) {
+      model.add(selectedDistribution)
+    }
+    else {
+      // show "No available distributions" message fully
+      model.add(null)
+      comboBox.preferredSize = comboBox.preferredSize
+      model.removeAll()
+    }
 
     WslDistributionManager.getInstance().installedDistributionsFuture.thenAccept { distributions ->
       var result = distributions
@@ -52,23 +56,19 @@ class WslTargetConfigurable(val config: WslTargetEnvironmentConfiguration) :
       }
       ApplicationManager.getApplication().invokeLater(Runnable {
         model.removeAll()
-        val data = result.map { Ref.create(it) }
-        model.addAll(0, data)
-        if (selectedDistribution != null) {
-          comboBox.selectedItem = data.find { it.get().msId.equals(selectedDistribution.msId, ignoreCase = true) }
-        }
+        model.addAll(0, result)
       }, ModalityState.any())
     }
     return comboBox
   }
 
-  private class WslDistributionRenderer : ColoredListCellRenderer<Ref<WSLDistribution?>>() {
-    override fun customizeCellRenderer(list: JList<out Ref<WSLDistribution?>>,
-                                       value: Ref<WSLDistribution?>?,
+  private class WslDistributionRenderer : ColoredListCellRenderer<WSLDistribution?>() {
+    override fun customizeCellRenderer(list: JList<out WSLDistribution?>,
+                                       value: WSLDistribution?,
                                        index: Int,
                                        selected: Boolean,
                                        hasFocus: Boolean) {
-      append(value?.get()?.presentableName ?: IdeBundle.message("wsl.no.available.distributions"))
+      append(value?.presentableName ?: IdeBundle.message("wsl.no.available.distributions"))
     }
   }
 }
