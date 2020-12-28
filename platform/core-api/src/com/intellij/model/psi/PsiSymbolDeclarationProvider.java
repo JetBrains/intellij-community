bd1dd76c65a834539f38dc34130ba97105afcb55
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2020-12-28 13:16:44 +0100

    document `offsetInElement` in `PsiSymbolDeclarationProvider`
    GitOrigin-RevId: 11b2e77dc2b8e39b7181ff52c11388a46365f68f

--- platform/core-api/src/com/intellij/model/psi/PsiSymbolDeclarationProvider.java
+++ platform/core-api/src/com/intellij/model/psi/PsiSymbolDeclarationProvider.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.model.psi;
 
 import com.intellij.psi.PsiElement;
@@ -15,7 +15,12 @@ import java.util.Collection;
 public interface PsiSymbolDeclarationProvider {
 
   /**
-   * @param element PsiElement in the code which might represent some declaration
+   * @param element         PsiElement in the code which might represent some declaration
+   * @param offsetInElement offset relative to the {@code element}, which should be inside
+   *                        {@linkplain PsiSymbolDeclaration#getDeclarationRange ranges of returned declarations},
+   *                        or {@code -1} if all declarations are requested.
+   *                        The offset serves as a hint to avoid computing declarations, which cannot contain the offset.
+   *                        The platform filters returned declarations by the offset later
    */
   @NotNull
   Collection<? extends PsiSymbolDeclaration> getDeclarations(@NotNull PsiElement element, int offsetInElement);
