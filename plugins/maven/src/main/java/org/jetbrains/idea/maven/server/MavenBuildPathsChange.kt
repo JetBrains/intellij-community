135ebea867a5b98b9c4106746dd705b360daef50
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2020-12-28 13:46:07 +0300

    IDEA-256251 - wsl dependencyTree mapping
    GitOrigin-RevId: 5595a69ee17056bf7764cb6b1f093a004a5ae3fc

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/server/MavenBuildPathsChange.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/server/MavenBuildPathsChange.kt
@@ -2,10 +2,7 @@
 package org.jetbrains.idea.maven.server
 
 import com.intellij.util.containers.ContainerUtil
-import org.jetbrains.idea.maven.model.MavenArtifact
-import org.jetbrains.idea.maven.model.MavenBuild
-import org.jetbrains.idea.maven.model.MavenModel
-import org.jetbrains.idea.maven.model.MavenResource
+import org.jetbrains.idea.maven.model.*
 import java.io.File
 
 class MavenBuildPathsChange(private val transformer: (String) -> String) {
@@ -13,6 +10,7 @@ class MavenBuildPathsChange(private val transformer: (String) -> String) {
 
   private fun MavenModel.transformPaths() {
     dependencies = dependencies.map { it.transformPaths() }
+    dependencyTree = dependencyTree.map { it.transformPaths(null) }
     build.transformPaths()
   }
 
@@ -38,4 +36,21 @@ class MavenBuildPathsChange(private val transformer: (String) -> String) {
     File(transformer(file.path)),
     null
   )
+
+  private fun MavenArtifactNode.transformPaths(parent: MavenArtifactNode?): MavenArtifactNode {
+    val result = MavenArtifactNode(
+      parent,
+      artifact.transformPaths(),
+      state,
+      relatedArtifact?.transformPaths(),
+      originalScope,
+      premanagedVersion,
+      premanagedScope
+    )
+    result.dependencies = dependencies.map { it.transformPaths(result) }
+    return result
+  }
 }
+
+
+
