1f1ed03249cc479070e9f06339add40552e9bdc4
Ivan Chirkov <Ivan.Chirkov@jetbrains.com>
2020-12-29 15:30:05 +0100

    PluginFeatureService: update code to new version of zip-signer lib
    GitOrigin-RevId: c93d33ae66e64f7de7202a642a4885e815605d4e

--- platform/platform-impl/src/com/intellij/ide/plugins/marketplace/PluginSignatureChecker.kt
+++ platform/platform-impl/src/com/intellij/ide/plugins/marketplace/PluginSignatureChecker.kt
@@ -2,12 +2,13 @@
 package com.intellij.ide.plugins.marketplace
 
 import com.intellij.ide.IdeBundle
-import com.intellij.ide.plugins.IdeaPluginDescriptor
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.ui.Messages
-import org.jetbrains.zip.signer.exceptions.ZipVerificationException
+import org.jetbrains.zip.signer.verifier.InvalidSignatureResult
+import org.jetbrains.zip.signer.verifier.MissingSignatureResult
+import org.jetbrains.zip.signer.verifier.SuccessfulVerificationResult
 import org.jetbrains.zip.signer.verifier.ZipVerifier
 import java.io.File
 
@@ -15,10 +16,6 @@ import java.security.KeyStore
 import java.security.cert.X509Certificate
 import javax.net.ssl.TrustManagerFactory
 import javax.net.ssl.X509TrustManager
-
-sealed class PluginsSignatureVerificationResult
-data class PluginsSignatureVerificationError(val errorMessage: String?) : PluginsSignatureVerificationResult()
-object PluginsSignatureVerificationSuccess : PluginsSignatureVerificationResult()
 
 object PluginSignatureChecker {
 
@@ -28,10 +25,23 @@ object PluginSignatureChecker {
     indicator.text2 = IdeBundle.message("plugin.signature.checker.progress", pluginName)
     indicator.isIndeterminate = true
 
-    val verificationResult = verify(pluginFile)
-    if (verificationResult is PluginsSignatureVerificationError) {
+    val errorMessage = when (val verificationResult = ZipVerifier.verify(pluginFile)) {
+      is InvalidSignatureResult ->
+        verificationResult.errorMessage
+      is MissingSignatureResult ->
+        "Plugin file is not signed"
+      is SuccessfulVerificationResult ->
+        if (!verificationResult.isSignedBy(getCertificate())) {
+          "Plugin file is not signed by JetBrains"
+        }
+        else {
+          null
+        }
+    }
+
+    if (errorMessage != null) {
       val title = IdeBundle.message("plugin.signature.checker.title")
-      val message = IdeBundle.message("plugin.signature.checker.untrusted.message", pluginName, verificationResult.errorMessage)
+      val message = IdeBundle.message("plugin.signature.checker.untrusted.message", pluginName, errorMessage)
       val yesText = IdeBundle.message("plugin.signature.checker.yes")
       val noText = IdeBundle.message("plugin.signature.checker.no")
       var result: Int = -1
@@ -44,20 +54,12 @@ object PluginSignatureChecker {
     return true
   }
 
-  private fun verify(pluginFile: File): PluginsSignatureVerificationResult {
-    try {
-      ZipVerifier.verify(pluginFile, getCertificates().last())
-      return PluginsSignatureVerificationSuccess
-    } catch (e: ZipVerificationException) {
-      return PluginsSignatureVerificationError(e.message)
-    }
-  }
-
-  private fun getCertificates(): List<X509Certificate> {
+  //TODO: this method should actually return JetBrains CA
+  private fun getCertificate(): X509Certificate {
     val trustManagerFactory: TrustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
     trustManagerFactory.init(null as KeyStore?)
     return trustManagerFactory.trustManagers.filterIsInstance<X509TrustManager>().flatMap {
       it.acceptedIssuers.toList()
-    }
+    }.last()
   }
 }
--- platform/platform-impl/src/com/intellij/ide/plugins/marketplace/RepositoryContentHandler.java
+++ platform/platform-impl/src/com/intellij/ide/plugins/marketplace/RepositoryContentHandler.java
@@ -8,6 +8,7 @@ import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.util.containers.Stack;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.zip.signer.verifier.ZipVerifier;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
