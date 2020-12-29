4e0d03336e02ae822ac4b4c039352ef23786d8c1
Alex Plate <aleksei.plate@jetbrains.com>
2020-12-30 01:46:28 +0300

    [Workspace Model] new added modules should be marked as new
    Issue: CPP-23528
    
    GitOrigin-RevId: cd87f29a9a769432bce050f849ba3bf44d6624ff

--- platform/workspaceModel/ide/tests/testSrc/com/intellij/workspaceModel/ide/ModuleBridgesTest.kt
+++ platform/workspaceModel/ide/tests/testSrc/com/intellij/workspaceModel/ide/ModuleBridgesTest.kt
@@ -7,6 +7,7 @@ import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.invokeAndWaitIfNeeded
 import com.intellij.openapi.application.runWriteActionAndWait
 import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.module.*
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.rootManager
@@ -46,7 +47,9 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import java.io.File
+import java.io.PrintStream
 import java.nio.file.Files
+import kotlin.random.Random
 
 class ModuleBridgesTest {
   @Rule
@@ -231,6 +234,7 @@ class ModuleBridgesTest {
   @Test
   fun `test remove and add module with the same name`() =
     WriteCommandAction.runWriteCommandAction(project) {
+      startLog()
       val moduleManager = ModuleManager.getInstance(project)
       for (module in moduleManager.modules) {
         moduleManager.disposeModule(module)
@@ -253,6 +257,9 @@ class ModuleBridgesTest {
 
       assertEquals(1, moduleManager.modules.size)
       assertNotNull(moduleManager.findModuleByName("name"))
+      val caughtLogs = catchLog()
+      // Trying to assert that a particular warning isn't presented in logs
+      assertFalse("name.iml does not exist" in caughtLogs)
     }
 
   @Test
@@ -599,6 +606,7 @@ class ModuleBridgesTest {
 
   @Test
   fun `test remove module removes source roots`() = WriteCommandAction.runWriteCommandAction(project) {
+    startLog()
     val moduleName = "build"
     val antLibraryFolder = "ant-lib"
 
@@ -624,6 +632,10 @@ class ModuleBridgesTest {
     ModuleManager.getInstance(project).disposeModule(module)
     assertEmpty(entityStore.current.entities(ContentRootEntity::class.java).toList())
     assertEmpty(entityStore.current.entities(JavaSourceRootEntity::class.java).toList())
+
+    val caughtLogs = catchLog()
+    // Trying to assert that a particular warning isn't presented in logs
+    assertFalse("name.iml does not exist" in caughtLogs)
   }
 
   @Test
@@ -765,6 +777,36 @@ class ModuleBridgesTest {
     val updatedStore = WorkspaceModel.getInstance(project).entityStorage.current
     assertEmpty(updatedStore.entities(ModuleEntity::class.java).toList())
     assertEmpty(updatedStore.entities(LibraryEntity::class.java).toList())
+  }
+
+  class OutCatcher(printStream: PrintStream) : PrintStream(printStream) {
+    var catcher = ""
+    override fun println(x: String?) {
+      catcher += "$x\n"
+    }
+  }
+
+  companion object {
+    val log = logger<ModuleBridgesTest>()
+    var logLine = ""
+    val rand = Random(0)
+
+    fun startLog() {
+      logLine = "Start logging " + rand.nextInt()
+      log.info(logLine)
+    }
+
+    fun catchLog(): String {
+      return try {
+        val outCatcher = OutCatcher(System.out)
+        System.setOut(outCatcher)
+        TestLoggerFactory.dumpLogToStdout(logLine)
+        outCatcher.catcher
+      }
+      finally {
+        System.setOut(System.out)
+      }
+    }
   }
 }
 
