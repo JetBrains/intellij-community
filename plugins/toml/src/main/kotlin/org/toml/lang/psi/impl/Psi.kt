3fa0e4ec23fbfcc3d7cb4275fe0d96a94cc96e08
Jakub Beránek <berykubik@gmail.com>
2020-12-29 19:39:55 +0100

    TOML: add TomlVisitor
    GitOrigin-RevId: c917b7d8b5f846b9d7f1cb63f95c0e8cb1c556eb

--- plugins/toml/src/main/kotlin/org/toml/lang/psi/impl/Psi.kt
+++ plugins/toml/src/main/kotlin/org/toml/lang/psi/impl/Psi.kt
@@ -9,10 +9,7 @@ import com.intellij.ide.projectView.PresentationData
 import com.intellij.lang.ASTFactory
 import com.intellij.lang.psi.SimpleMultiLineTextEscaper
 import com.intellij.navigation.ItemPresentation
-import com.intellij.psi.LiteralTextEscaper
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiLanguageInjectionHost
-import com.intellij.psi.PsiReference
+import com.intellij.psi.*
 import com.intellij.psi.impl.source.resolve.reference.ReferenceProvidersRegistry
 import com.intellij.psi.impl.source.tree.CompositeElement
 import com.intellij.psi.impl.source.tree.CompositePsiElement
@@ -27,11 +24,18 @@ class TomlKeyValueImpl(type: IElementType) : CompositePsiElement(type), TomlKeyV
     override val key: TomlKey get() = childOfTypeNotNull()
     override val value: TomlValue? get() = childOfTypeNullable()
     override fun toString(): String = "TomlKeyValue"
+
+    override fun accept(visitor: PsiElementVisitor) {
+        if (visitor is TomlVisitor) {
+            visitor.visitKeyValue(this)
+        } else {
+            super.accept(visitor)
+        }
+    }
 }
 
 class TomlKeyImpl(type: IElementType) : CompositePsiElement(type), TomlKey {
-    override fun getReferences(): Array<PsiReference>
-        = ReferenceProvidersRegistry.getReferencesFromProviders(this)
+    override fun getReferences(): Array<PsiReference> = ReferenceProvidersRegistry.getReferencesFromProviders(this)
 
     override fun getName(): String = text
 
@@ -42,11 +46,18 @@ class TomlKeyImpl(type: IElementType) : CompositePsiElement(type), TomlKey {
     override fun getPresentation(): ItemPresentation = PresentationData(name, null, null, null)
 
     override fun toString(): String = "TomlKey"
+
+    override fun accept(visitor: PsiElementVisitor) {
+        if (visitor is TomlVisitor) {
+            visitor.visitKey(this)
+        } else {
+            super.accept(visitor)
+        }
+    }
 }
 
 class TomlLiteralImpl(type: IElementType) : CompositePsiElement(type), TomlLiteral {
-    override fun getReferences(): Array<PsiReference>
-        = ReferenceProvidersRegistry.getReferencesFromProviders(this)
+    override fun getReferences(): Array<PsiReference> = ReferenceProvidersRegistry.getReferencesFromProviders(this)
 
     override fun toString(): String = "TomlLiteral"
 
@@ -64,34 +75,82 @@ class TomlLiteralImpl(type: IElementType) : CompositePsiElement(type), TomlLiter
         val tokenType = node.findChildByType(TOML_STRING_LITERALS)?.elementType ?: error("$text is not string literal")
         return if (tokenType in TOML_BASIC_STRINGS) TomlLiteralTextEscaper(this) else SimpleMultiLineTextEscaper(this)
     }
+
+    override fun accept(visitor: PsiElementVisitor) {
+        if (visitor is TomlVisitor) {
+            visitor.visitLiteral(this)
+        } else {
+            super.accept(visitor)
+        }
+    }
 }
 
 class TomlArrayImpl(type: IElementType) : CompositePsiElement(type), TomlArray {
     override val elements: List<TomlValue> get() = childrenOfType()
     override fun toString(): String = "TomlArray"
+
+    override fun accept(visitor: PsiElementVisitor) {
+        if (visitor is TomlVisitor) {
+            visitor.visitArray(this)
+        } else {
+            super.accept(visitor)
+        }
+    }
 }
 
 class TomlTableImpl(type: IElementType) : CompositePsiElement(type), TomlTable {
     override val header: TomlTableHeader get() = childOfTypeNotNull()
     override val entries: List<TomlKeyValue> get() = childrenOfType()
     override fun toString(): String = "TomlTable"
+
+    override fun accept(visitor: PsiElementVisitor) {
+        if (visitor is TomlVisitor) {
+            visitor.visitTable(this)
+        } else {
+            super.accept(visitor)
+        }
+    }
 }
 
 class TomlTableHeaderImpl(type: IElementType) : CompositePsiElement(type), TomlTableHeader {
     override val names: List<TomlKey> get() = childrenOfType()
     override fun toString(): String = "TomlTableHeader"
+
+    override fun accept(visitor: PsiElementVisitor) {
+        if (visitor is TomlVisitor) {
+            visitor.visitTableHeader(this)
+        } else {
+            super.accept(visitor)
+        }
+    }
 }
 
 class TomlInlineTableImpl(type: IElementType) : CompositePsiElement(type), TomlInlineTable {
     override val entries: List<TomlKeyValue> get() = childrenOfType()
 
     override fun toString(): String = "TomlInlineTable"
+
+    override fun accept(visitor: PsiElementVisitor) {
+        if (visitor is TomlVisitor) {
+            visitor.visitInlineTable(this)
+        } else {
+            super.accept(visitor)
+        }
+    }
 }
 
 class TomlArrayTableImpl(type: IElementType) : CompositePsiElement(type), TomlArrayTable {
     override val header: TomlTableHeader get() = childOfTypeNotNull()
     override val entries: List<TomlKeyValue> get() = childrenOfType()
     override fun toString(): String = "TomlArrayTable"
+
+    override fun accept(visitor: PsiElementVisitor) {
+        if (visitor is TomlVisitor) {
+            visitor.visitArrayTable(this)
+        } else {
+            super.accept(visitor)
+        }
+    }
 }
 
 class TomlASTFactory : ASTFactory() {
