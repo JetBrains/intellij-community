09c1f7085601a55703bec659a22ba2d1ffe71359
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2020-12-30 17:20:22 +0300

    Don't consider imported element as private if it is presented in dunder all (PY-46169)
    GitOrigin-RevId: b9f22ea3c7619f9db5b06e321ed3fe46cbaaea60

--- python/testData/pyi/inspections/hiddenPyiImports/HiddenPyiImports.py
+++ python/testData/pyi/inspections/hiddenPyiImports/HiddenPyiImports.py
@@ -1,7 +1,8 @@
 from m1 import <error descr="Cannot find reference 'foo' in 'm1.pyi'">foo</error>
 from m1 import <error descr="Cannot find reference 'bar' in 'm1.pyi'">bar</error>
 from m1 import bar_imported
+from m1 import baz
 from m1 import <error descr="Cannot find reference 'm2' in 'm1.pyi'">m2</error>
 from m1 import m2_imported
 
-print(foo, bar, bar_imported, m2, m2_imported)
+print(foo, bar, bar_imported, baz, m2, m2_imported)
