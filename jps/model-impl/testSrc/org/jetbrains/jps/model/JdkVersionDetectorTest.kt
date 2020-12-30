b0c805104ef8427015bdcd5339a8ecd6af41f163
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2020-12-30 23:04:16 +0100

    [tests] more accurate CPU arch detection (JPS)
    GitOrigin-RevId: ba8f1a7b1addc67e660b1c9beb3631073fc41f5b

--- jps/model-impl/testSrc/org/jetbrains/jps/model/JdkVersionDetectorTest.kt
+++ jps/model-impl/testSrc/org/jetbrains/jps/model/JdkVersionDetectorTest.kt
@@ -1,22 +1,20 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.jps.model
 
 import com.intellij.openapi.util.Bitness
-import com.intellij.openapi.util.SystemInfo
+import com.intellij.util.SystemProperties
 import com.intellij.util.lang.JavaVersion
+import com.intellij.util.system.CpuArch
 import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.jps.model.java.JdkVersionDetector
 import org.junit.Test
 
 class JdkVersionDetectorTest {
   @Test fun detectJdkVersion() {
-    val jdkHome = System.getProperty("java.home")
-    val jdkVersion = JdkVersionDetector.getInstance().detectJdkVersionInfo(jdkHome)
+    val jdkVersion = JdkVersionDetector.getInstance().detectJdkVersionInfo(SystemProperties.getJavaHome())
     assertThat(jdkVersion?.version?.feature).isEqualTo(JavaVersion.current().feature)
     assertThat(jdkVersion?.version?.minor).isEqualTo(JavaVersion.current().minor)
     assertThat(jdkVersion?.version?.update).isEqualTo(JavaVersion.current().update)
-    assertThat(jdkVersion?.bitness).isEqualTo(if (SystemInfo.is64Bit) Bitness.x64 else Bitness.x32)
+    assertThat(jdkVersion?.bitness).isEqualTo(if (CpuArch.is32Bit()) Bitness.x32 else Bitness.x64)
   }
 }
