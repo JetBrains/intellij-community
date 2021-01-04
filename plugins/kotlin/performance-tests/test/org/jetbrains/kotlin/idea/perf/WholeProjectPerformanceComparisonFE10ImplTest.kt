8d5b79e83c60d0b0c2312ecdc0a152ff003e019f
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-01-04 19:44:20 +0100

    Add perf the same project perf tests used for FIR for fe10 plugin
    Original commit: a4cf21adef48b4a29174c074f95b9689c2f4d0dd
    
    GitOrigin-RevId: f21dce2fa1032053734e461d3cc73fc5a9aa6286

---
+++ plugins/kotlin/performance-tests/test/org/jetbrains/kotlin/idea/perf/WholeProjectPerformanceComparisonFE10ImplTest.kt
@@ -0,0 +1,19 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package org.jetbrains.kotlin.idea.perf
+
+import org.jetbrains.kotlin.idea.perf.common.AbstractWholeProjectPerformanceComparisonTest
+
+class WholeProjectPerformanceComparisonFE10ImplTest : AbstractWholeProjectPerformanceComparisonTest() {
+    override val testPrefix: String = "FE10"
+    override fun getWarmUpProject(): WarmUpProject = warmUpProject
+
+    fun testRustPlugin() {
+        doTestRustPlugin()
+    }
+
+    companion object {
+        private val hwStats: Stats = Stats("FE10 warmup project")
+        private val warmUpProject = WarmUpProject(hwStats)
+    }
+}
