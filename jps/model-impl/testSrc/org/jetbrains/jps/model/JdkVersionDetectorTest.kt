96ca1cdb3a4290589aae57c46d1b652e7098fd92
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2021-01-04 14:18:28 +0100

    [tests] fixing JDK version detector behavior on incomplete macOS paths
    GitOrigin-RevId: c9c931a08dc467d59cc74e8ec4d184e033a1020c

--- jps/model-impl/testSrc/org/jetbrains/jps/model/JdkVersionDetectorTest.kt
+++ jps/model-impl/testSrc/org/jetbrains/jps/model/JdkVersionDetectorTest.kt
@@ -2,6 +2,7 @@
 package org.jetbrains.jps.model
 
 import com.intellij.openapi.util.Bitness
+import com.intellij.openapi.util.SystemInfo
 import com.intellij.util.SystemProperties
 import com.intellij.util.lang.JavaVersion
 import com.intellij.util.system.CpuArch
@@ -11,10 +12,17 @@ import org.junit.Test
 
 class JdkVersionDetectorTest {
   @Test fun detectJdkVersion() {
-    val jdkVersion = JdkVersionDetector.getInstance().detectJdkVersionInfo(SystemProperties.getJavaHome())
-    assertThat(jdkVersion?.version?.feature).isEqualTo(JavaVersion.current().feature)
-    assertThat(jdkVersion?.version?.minor).isEqualTo(JavaVersion.current().minor)
-    assertThat(jdkVersion?.version?.update).isEqualTo(JavaVersion.current().update)
+    val jdkHome = SystemProperties.getJavaHome()
+    val jdkVersion = JdkVersionDetector.getInstance().detectJdkVersionInfo(jdkHome)
+    assertThat(jdkVersion?.version?.trim()).isEqualTo(JavaVersion.current().trim())
     assertThat(jdkVersion?.bitness).isEqualTo(if (CpuArch.is32Bit()) Bitness.x32 else Bitness.x64)
+
+    if (SystemInfo.isMac && jdkHome.endsWith("/Contents/Home")) {
+      val altHome = jdkHome.substring(0, jdkHome.length - "/Contents/Home".length)
+      val altVersion = JdkVersionDetector.getInstance().detectJdkVersionInfo(altHome)
+      assertThat(altVersion?.version).isNull()
+    }
   }
+
+  private fun JavaVersion.trim(): JavaVersion = JavaVersion.compose(feature, minor, update, 0, false)
 }
