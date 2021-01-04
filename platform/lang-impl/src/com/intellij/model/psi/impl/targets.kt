e8641b1be5eb259ef5d6967518e1ad1b6c140f9d
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-01-04 15:49:16 +0100

    clean up: reformat targets.kt
    GitOrigin-RevId: 44b1585accdb575cf1aabae8dce5e61632d4c292

--- platform/lang-impl/src/com/intellij/model/psi/impl/targets.kt
+++ platform/lang-impl/src/com/intellij/model/psi/impl/targets.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.model.psi.impl
 
 import com.intellij.codeInsight.TargetElementUtil
@@ -45,7 +45,9 @@ internal fun declaredReferencedData(file: PsiFile, offset: Int): DeclaredReferen
     return emptyData
   }
 
-  val withMinimalRanges: Collection<DeclarationOrReference> = chooseByRange(allDeclarationsOrReferences, offset, DeclarationOrReference::rangeWithOffset)
+  val withMinimalRanges: Collection<DeclarationOrReference> = chooseByRange(
+    allDeclarationsOrReferences, offset, DeclarationOrReference::rangeWithOffset
+  )
 
   var declaration: PsiSymbolDeclaration? = null
   val references: MutableList<PsiSymbolReference> = ArrayList()
