a59d794f41fcdd90aef78c164d9b13b68246c432
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-01-05 13:15:15 +0100

    RunAll: add ability to run collections of tests
    GitOrigin-RevId: 4eadb7ff76c48a573bfa9ade5be7738b01d0e343

--- platform/testFramework/src/com/intellij/testFramework/RunAll.java
+++ platform/testFramework/src/com/intellij/testFramework/RunAll.java
@@ -1,15 +1,16 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.testFramework;
 
-import com.intellij.util.ExceptionUtil;
-import com.intellij.util.SmartList;
-import com.intellij.util.ThrowableRunnable;
+import com.intellij.util.*;
+import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.lang.CompoundRuntimeException;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Runs all given runnables and throws all the caught exceptions at the end.
@@ -17,9 +18,20 @@ import java.util.List;
  * @author peter
  */
 public final class RunAll implements Runnable {
+
   @SafeVarargs
   public static void runAll(ThrowableRunnable<Throwable> @NotNull ... actions) {
     new RunAll(actions).run();
+  }
+
+  public static <T> void runAll(@NotNull Collection<? extends T> input,
+                                @NotNull ThrowableConsumer<? super T, Throwable> action) {
+    new RunAll(ContainerUtil.map(input, it -> () -> action.consume(it))).run();
+  }
+
+  public static <K, V> void runAll(@NotNull Map<? extends K, ? extends V> input,
+                                   @NotNull ThrowablePairConsumer<? super K, ? super V, Throwable> action) {
+    runAll(input.entrySet(), e -> action.consume(e.getKey(), e.getValue()));
   }
 
   private final List<? extends ThrowableRunnable<?>> myActions;
