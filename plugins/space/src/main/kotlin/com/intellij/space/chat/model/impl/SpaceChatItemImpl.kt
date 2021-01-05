17cac70ec79c31a2d9473e637d8c9238091f13cf
Joffrey.Bion <joffrey.bion@jetbrains.com>
2021-01-05 17:15:26 +0100

    [space] Upgrade space-idea-sdk to 1.1.62795
    IJ-MR-5005
    
    GitOrigin-RevId: 995e35f73b1596ca828ee29c0b5deaabc9baf349

--- plugins/space/src/main/kotlin/com/intellij/space/chat/model/impl/SpaceChatItemImpl.kt
+++ plugins/space/src/main/kotlin/com/intellij/space/chat/model/impl/SpaceChatItemImpl.kt
@@ -50,7 +50,7 @@ internal class SpaceChatItemImpl private constructor(
   }
 
   override suspend fun delete() {
-    messageVm.delete()
+    chat.deleteMessage(id)
   }
 
   override fun equals(other: Any?): Boolean {
