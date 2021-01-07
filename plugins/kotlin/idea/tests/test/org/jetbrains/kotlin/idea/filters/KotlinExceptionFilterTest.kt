dd820954c9c73d2309582e3e93e079df00b658ff
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2021-01-08 01:40:39 +0900

    Fix KotlinExceptionFilterTest
    GitOrigin-RevId: f38d9f850babecf23c18d309fb1430cb51371180

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/filters/KotlinExceptionFilterTest.kt
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/filters/KotlinExceptionFilterTest.kt
@@ -7,12 +7,11 @@ package org.jetbrains.kotlin.idea.filters
 
 import com.intellij.execution.filters.FileHyperlinkInfo
 import com.intellij.openapi.fileEditor.FileDocumentManager
-import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.testFramework.LightProjectDescriptor
 import org.jetbrains.kotlin.idea.core.util.toVirtualFile
 import org.jetbrains.kotlin.idea.test.KotlinLightCodeInsightFixtureTestCase
-import org.junit.Assert
+import org.jetbrains.kotlin.test.KotlinRoot
 import org.junit.internal.runners.JUnit38ClassRunner
 import org.junit.runner.RunWith
 import java.io.File
@@ -21,41 +20,25 @@ private data class SuffixOption(val suffix: String, val expectedLine: Int, val e
 
 @RunWith(JUnit38ClassRunner::class)
 class KotlinExceptionFilterTest : KotlinLightCodeInsightFixtureTestCase() {
-    private val mySuffixPlug = "<suffix>"
-    private val myPathPlug = "<path>"
+    private val suffixPlug = "<suffix>"
+    private val pathPlug = "<path>"
 
-    private val myTemplates = listOf(
-        "\tat 1   main.kexe\t\t 0x000000010d7cdb4c kfun:package.function(kotlin.Int) + 108 ($myPathPlug:$mySuffixPlug)\n"
+    private val templates = listOf(
+        "\tat 1   main.kexe\t\t 0x000000010d7cdb4c kfun:package.function(kotlin.Int) + 108 ($pathPlug:$suffixPlug)\n"
     )
 
-    private var myFiles = HashMap<String, VirtualFile>()
-    private var myExceptionLine: String = ""
-
-    private val mySuffixOptions = listOf(
+    private val suffixOptions = listOf(
         SuffixOption("10:1", 9, 1),
         SuffixOption("14:11", 13, 11),
         SuffixOption("<unknown>", 0, 0)
     )
 
-    private fun errorMessage(detail: String) = "Failed to parse Kotlin Native exception '$myExceptionLine': $detail"
-
-    override fun getProjectDescriptor() = LightProjectDescriptor.EMPTY_PROJECT_DESCRIPTOR
-
-    override fun setUp() {
-        super.setUp()
-        val rootDir = File("idea/testData/debugger/nativeExceptions/")
-        rootDir.listFiles().forEach {
-            val virtualFile = it.toVirtualFile()
-            if (virtualFile != null) {
-                myFiles[it.absolutePath] = virtualFile
-            }
-        }
-    }
+    override fun getProjectDescriptor(): LightProjectDescriptor = LightProjectDescriptor.EMPTY_PROJECT_DESCRIPTOR
 
     fun testDifferentLocations() {
-        for (template in myTemplates) {
-            for (suffixOption in mySuffixOptions) {
-                val templateWithSuffix = template.replace(mySuffixPlug, suffixOption.suffix)
+        for (template in templates) {
+            for (suffixOption in suffixOptions) {
+                val templateWithSuffix = template.replace(suffixPlug, suffixOption.suffix)
                 doTest(templateWithSuffix, suffixOption.expectedLine, suffixOption.expectedColumn)
             }
         }
@@ -64,21 +47,28 @@ class KotlinExceptionFilterTest : KotlinLightCodeInsightFixtureTestCase() {
     fun doTest(template: String, expectedLine: Int, expectedColumn: Int) {
         val filter = KotlinExceptionFilterFactory().create(GlobalSearchScope.allScope(project))
 
-        for ((absolutePath, virtualFile) in myFiles) {
-            myExceptionLine = template.replace(myPathPlug, absolutePath)
+        val rootDir = File(KotlinRoot.DIR, "idea/testData/debugger/nativeExceptions")
 
-            val filterResult = filter.applyFilter(myExceptionLine, myExceptionLine.length)
-            Assert.assertNotNull(errorMessage("filename is not found by parser"), filterResult)
+        for (ioFile in rootDir.listFiles()!!) {
+            val absolutePath = ioFile.absolutePath
+            val virtualFile = ioFile.toVirtualFile() ?: continue
+
+            val exceptionLine = template.replace(pathPlug, absolutePath)
+
+            fun errorMessage(detail: String) = "Failed to parse Kotlin Native exception '$exceptionLine': $detail"
+
+            val filterResult = filter.applyFilter(exceptionLine, exceptionLine.length)
+            assertNotNull(errorMessage("filename is not found by parser"), filterResult)
             val fileHyperlinkInfo = filterResult?.firstHyperlinkInfo as FileHyperlinkInfo
 
             val document = FileDocumentManager.getInstance().getDocument(virtualFile)
-            Assert.assertNotNull(errorMessage("test file $absolutePath could not be found in repository"), document)
+            assertNotNull(errorMessage("test file $absolutePath could not be found in repository"), document)
             val expectedOffset = document!!.getLineStartOffset(expectedLine) + expectedColumn
 
             val descriptor = fileHyperlinkInfo.descriptor
-            Assert.assertNotNull(errorMessage("found file hyperlink with null descriptor"), descriptor)
-            Assert.assertEquals(errorMessage("different filename parsed"), virtualFile.canonicalPath, descriptor?.file?.canonicalPath)
-            Assert.assertEquals(errorMessage("different offset parsed"), expectedOffset, descriptor?.offset)
+            assertNotNull(errorMessage("found file hyperlink with null descriptor"), descriptor)
+            assertEquals(errorMessage("different filename parsed"), virtualFile.canonicalPath, descriptor?.file?.canonicalPath)
+            assertEquals(errorMessage("different offset parsed"), expectedOffset, descriptor?.offset)
         }
     }
 }
