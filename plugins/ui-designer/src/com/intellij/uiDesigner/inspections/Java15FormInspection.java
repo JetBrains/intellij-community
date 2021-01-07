33b2e79e14fdd122e4925021e74e69d1c43f973a
Yann Cébron <yann.cebron@jetbrains.com>
2021-01-07 12:05:40 +0100

    [ui-designer] Java15FormInspection: suppress false positive "missing inspection description" warning
    GitOrigin-RevId: ed7cb2b0d3d53b2a9383075d17106a427874438f

--- plugins/ui-designer/src/com/intellij/uiDesigner/inspections/Java15FormInspection.java
+++ plugins/ui-designer/src/com/intellij/uiDesigner/inspections/Java15FormInspection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2017 JetBrains s.r.o.
+ * Copyright 2000-2021 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -42,6 +42,7 @@ import java.util.Collections;
 /**
  * @author yole
  */
+@SuppressWarnings("InspectionDescriptionNotFoundInspection")
 public class Java15FormInspection extends BaseFormInspection {
   public Java15FormInspection() {
     super("Since15");
