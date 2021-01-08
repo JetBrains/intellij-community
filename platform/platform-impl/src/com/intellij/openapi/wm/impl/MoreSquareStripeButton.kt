cf86cfc644f789fe1d914c94d4fb6a2430b587d8
Konstantin Bulenkov <kb@jetbrains.com>
2021-01-08 14:58:54 +0100

    [New UI] rotate kebab icon, show light-weight popup for TW navigation
    GitOrigin-RevId: 2c7d66f802e7ce23bd2f38b7e00571f502f75e25

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/MoreSquareStripeButton.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/MoreSquareStripeButton.kt
@@ -2,29 +2,36 @@
 package com.intellij.openapi.wm.impl
 
 import com.intellij.icons.AllIcons
+import com.intellij.ide.actions.ToolwindowSwitcher
 import com.intellij.openapi.actionSystem.ActionPlaces
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.Presentation
 import com.intellij.openapi.actionSystem.impl.ActionButton
 import com.intellij.openapi.project.DumbAware
 import com.intellij.ui.ToggleActionButton
+import com.intellij.ui.awt.RelativePoint
 import java.awt.Dimension
+import java.awt.Point
 
 class MoreSquareStripeButton(toolwindowSideBar: IdeLeftToolbar) :
-  ActionButton(createAction(toolwindowSideBar), createPresentation(), ActionPlaces.TOOLWINDOW_SIDE_BAR, Dimension(40, 40)) {
+  ActionButton(createAction(toolwindowSideBar), createPresentation(), ActionPlaces.TOOLWINDOW_SIDE_BAR, Dimension(40, 24)) {
 
   companion object {
     fun createPresentation(): Presentation {
       return Presentation().apply {
-        icon = AllIcons.Actions.More
+        icon = AllIcons.Actions.MoreHorizontal
         isEnabledAndVisible = true
       }
     }
 
     fun createAction(toolwindowSideBar: IdeLeftToolbar): ToggleActionButton =
       object : ToggleActionButton(Presentation.NULL_STRING, null), DumbAware {
-        override fun isSelected(e: AnActionEvent?) = toolwindowSideBar.isExtendedToolwindowPaneShown()
-        override fun setSelected(e: AnActionEvent?, state: Boolean) = toolwindowSideBar.openExtendedToolwindowPane(state)
+        override fun isSelected(e: AnActionEvent?) = false
+        override fun setSelected(e: AnActionEvent?, state: Boolean) {
+          if (e != null) {
+            ToolwindowSwitcher.invokePopup(e.project!!, RelativePoint(toolwindowSideBar, Point(toolwindowSideBar.width, 0)))
+          }
+        }
       }
   }
 }
