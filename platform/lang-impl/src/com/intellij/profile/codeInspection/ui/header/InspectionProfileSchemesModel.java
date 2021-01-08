959e74f6beaf80a80c83eea184b64f1747566896
Bas Leijdekkers <basleijdekkers@gmail.com>
2021-01-08 12:58:07 +0100

    keep inspection settings profile combo box sorted
    GitOrigin-RevId: c171acf5dc59560f0604665d52da9d3952510850

--- platform/lang-impl/src/com/intellij/profile/codeInspection/ui/header/InspectionProfileSchemesModel.java
+++ platform/lang-impl/src/com/intellij/profile/codeInspection/ui/header/InspectionProfileSchemesModel.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.profile.codeInspection.ui.header;
 
 import com.intellij.application.options.schemes.SchemesModel;
@@ -130,7 +130,7 @@ public abstract class InspectionProfileSchemesModel implements SchemesModel<Insp
         }
       })
       .filter(Objects::nonNull)
-      .forEach(this::addProfile);
+      .forEachOrdered(this::addProfile);
   }
 
   void disposeUI() {
