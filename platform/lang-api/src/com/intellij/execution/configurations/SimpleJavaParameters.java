c6bd2d31db3d9a02baa5b5351905e0af9c7ff9a4
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-01-11 17:52:21 +0100

    rename `SimpleJavaParameters#useDynamicClasspathUnconditionally` -> `#useDynamicClasspathDefinedByJdkLevel`
    - fix javadocs: don't mention properties;
    - also rename `ShortenCommandLine#getDefaultMethodForJdk` -> `#getDefaultMethodForJdkLevel` for consistency.
    
    GitOrigin-RevId: 287ffe9433098cf8a14d6e51da8b55853bb13aa5

--- platform/lang-api/src/com/intellij/execution/configurations/SimpleJavaParameters.java
+++ platform/lang-api/src/com/intellij/execution/configurations/SimpleJavaParameters.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.configurations;
 
 import com.intellij.execution.CantRunException;
@@ -90,11 +90,11 @@ public class SimpleJavaParameters extends SimpleProgramParameters {
 
   /**
    * Enables command line shortening considering current {@link #getJdk JDK}
-   * and ignoring {@link JdkUtil#useDynamicClasspath project and application properties}.
+   * and ignoring whether dynamic classpath is {@linkplain JdkUtil#useDynamicClasspath enabled on project or application level}.
    */
-  public void useDynamicClasspathUnconditionally() {
+  public void useDynamicClasspathDefinedByJdkLevel() {
     Sdk jdk = getJdk();
-    setShortenCommandLine(ShortenCommandLine.getDefaultMethodForJdk(jdk != null ? jdk.getHomePath() : null));
+    setShortenCommandLine(ShortenCommandLine.getDefaultMethodForJdkLevel(jdk != null ? jdk.getHomePath() : null));
   }
 
   public void setUseDynamicClasspath(boolean useDynamicClasspath) {
