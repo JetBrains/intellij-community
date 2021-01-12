e0344f1cc640cb383a41a2f89a743759374e0448
Yann Cébron <yann.cebron@jetbrains.com>
2021-01-12 22:50:51 +0100

    InspectionElementsMerger: javadoc fix
    GitOrigin-RevId: 055e70802bee89ee3d6e1b8ec4616c4cf8671b02

--- platform/analysis-api/src/com/intellij/codeInspection/ex/InspectionElementsMerger.java
+++ platform/analysis-api/src/com/intellij/codeInspection/ex/InspectionElementsMerger.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInspection.ex;
 
 import com.intellij.openapi.extensions.ExtensionPointName;
@@ -15,11 +15,11 @@ import java.util.concurrent.ConcurrentMap;
  * Merges multiple inspections settings {@link #getSourceToolNames()} into another one {@link #getMergedToolName()}.
  *
  * Used to preserve backward compatibility when merging several inspections into one, or replacing an inspection with a different
- * inspection. An inspection merger keeps existing @SuppressWarnings annotations working. It can also avoid the need to modify user
+ * inspection. An inspection merger keeps existing {@code @SuppressWarnings} annotations working. It can also avoid the need to modify user
  * inspection profiles, because a new inspection can take one or more old inspection's settings, without the user needing to configure
  * it again.
  *
- * {@see com.intellij.codeInspection.ex.InspectionElementsMergerBase} to provide more fine control over xml
+ * @see com.intellij.codeInspection.ex.InspectionElementsMergerBase to provide more fine control over XML
  */
 public abstract class InspectionElementsMerger {
   public static final ExtensionPointName<InspectionElementsMerger> EP_NAME = new ExtensionPointName<>("com.intellij.inspectionElementsMerger");
