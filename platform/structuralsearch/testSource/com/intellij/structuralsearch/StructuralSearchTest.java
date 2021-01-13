9a8eddc0543e5e1f699df8d01c0420711cbcb7e8
Bas Leijdekkers <basleijdekkers@gmail.com>
2021-01-13 15:56:33 +0100

    SSR: add more cases to test (IJ-CR-5043)
    GitOrigin-RevId: 1541ae249687112b387afd96aee5a3b3e2d66539

--- platform/structuralsearch/testSource/com/intellij/structuralsearch/StructuralSearchTest.java
+++ platform/structuralsearch/testSource/com/intellij/structuralsearch/StructuralSearchTest.java
@@ -2108,17 +2108,23 @@ public class StructuralSearchTest extends StructuralSearchTestCase {
     assertEquals(0, findMatchesCount(s1,s2));
   }
 
-  public void testSuperExpressionShouldNotMatch() {
+  public void testFindRecursiveCall() {
     String source = "class X {\n" +
                     "  void x() {\n" +
                     "     y();\n" +
                     "  }\n" +
-                    "  void y() {}\n" +
+                    "  void y() {}" +
+                    "  void z() {" +
+                    "    z();" +
+                    "  }" +
+                    "  void a() {" +
+                    "    a();" +
+                    "  }\n" +
                     "}";
     String pattern = "void '_a() {" +
                      "  '_a();" +
                      "}";
-    assertEquals(0, findMatchesCount(source, pattern));
+    assertEquals(2, findMatchesCount(source, pattern));
   }
 
   public void testDownUpMatch() {
