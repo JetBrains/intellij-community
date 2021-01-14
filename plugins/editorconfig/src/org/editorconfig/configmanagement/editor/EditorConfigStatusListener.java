0d4bf5938aa97ac95ccd7d5ee27aac3b65102029
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2021-01-14 13:35:01 +0300

    Check "charset" properties in .editorconfig to reload files
    GitOrigin-RevId: 2a080dd189fc60e6350ebf74c507722afb941f59

--- plugins/editorconfig/src/org/editorconfig/configmanagement/editor/EditorConfigStatusListener.java
+++ plugins/editorconfig/src/org/editorconfig/configmanagement/editor/EditorConfigStatusListener.java
@@ -2,6 +2,7 @@
 package org.editorconfig.configmanagement.editor;
 
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer;
+import com.intellij.navigation.NavigationItem;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
@@ -14,26 +15,29 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileVisitor;
-import com.intellij.openapi.vfs.encoding.EncodingProjectManager;
-import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.*;
 import com.intellij.psi.codeStyle.CodeStyleSettingsChangeEvent;
 import com.intellij.psi.codeStyle.CodeStyleSettingsListener;
 import com.intellij.psi.codeStyle.CodeStyleSettingsManager;
 import com.intellij.ui.EditorNotifications;
 import com.intellij.util.ObjectUtils;
 import org.editorconfig.Utils;
+import org.editorconfig.configmanagement.ConfigEncodingManager;
 import org.editorconfig.language.messages.EditorConfigBundle;
+import org.editorconfig.language.psi.EditorConfigOption;
+import org.editorconfig.language.psi.EditorConfigSection;
 import org.jetbrains.annotations.NotNull;
 
-import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class EditorConfigStatusListener implements CodeStyleSettingsListener, Disposable {
   private       boolean     myEnabledStatus;
   private final VirtualFile myVirtualFile;
   private final Project     myProject;
-  private       Charset     myEncoding;
+  private       Set<String> myEncodings;
 
   private MyReloadTask myReloadTask;
 
@@ -41,7 +45,7 @@ public class EditorConfigStatusListener implements CodeStyleSettingsListener, Di
     myProject = project;
     myEnabledStatus = Utils.isEnabled(project);
     myVirtualFile = virtualFile;
-    myEncoding = EncodingProjectManager.getInstance(project).getEncoding(virtualFile, true);
+    myEncodings = extractEncodings();
     CodeStyleSettingsManager.getInstance(project).addListener(this);
   }
 
@@ -52,10 +56,12 @@ public class EditorConfigStatusListener implements CodeStyleSettingsListener, Di
       myEnabledStatus = newEnabledStatus;
       onEditorConfigEnabled(newEnabledStatus);
     }
-    Charset newEncoding = EncodingProjectManager.getInstance(myProject).getEncoding(myVirtualFile, true);
-    if (!myEncoding.equals(newEncoding)) {
-      onEncodingChanged();
-      myEncoding = newEncoding;
+    Set<String> newEncodings = extractEncodings();
+    if (!myEncodings.equals(newEncodings)) {
+      if (containsValidEncodings(newEncodings)) {
+        onEncodingChanged();
+      }
+      myEncodings = newEncodings;
     }
   }
 
@@ -85,6 +91,10 @@ public class EditorConfigStatusListener implements CodeStyleSettingsListener, Di
     myReloadTask = reloadTask;
   }
 
+  private static boolean containsValidEncodings(@NotNull Set<String> encodings) {
+    return encodings.stream().allMatch(encoding->ConfigEncodingManager.encodingMap.get(encoding) != null);
+  }
+
   private class MyReloadTask extends Task.Backgroundable {
     private volatile boolean myInterrupted;
 
@@ -103,7 +113,9 @@ public class EditorConfigStatusListener implements CodeStyleSettingsListener, Di
           @Override
           public boolean visitFile(@NotNull VirtualFile file) {
             if (myInterrupted) throw new ProcessCanceledException();
-            if (!file.isDirectory() && fileDocumentManager.getCachedDocument(file) != null) {
+            if (!file.isDirectory() &&
+                !Utils.EDITOR_CONFIG_FILE_NAME.equals(file.getName()) &&
+                fileDocumentManager.getCachedDocument(file) != null) {
               filesToReload.add(file);
             }
             return true;
@@ -118,5 +130,32 @@ public class EditorConfigStatusListener implements CodeStyleSettingsListener, Di
     void interrupt() {
       myInterrupted = true;
     }
+  }
+
+  @NotNull
+  private Set<String> extractEncodings() {
+    final Set<String> charsets = new HashSet<>();
+    PsiFile psiFile = PsiManager.getInstance(myProject).findFile(myVirtualFile);
+    if (psiFile != null) {
+      PsiRecursiveElementVisitor visitor = new PsiRecursiveElementVisitor() {
+        @Override
+        public void visitElement(@NotNull PsiElement element) {
+          if (element instanceof PsiFile || element instanceof EditorConfigSection) {
+            super.visitElement(element);
+          }
+          else if (element instanceof EditorConfigOption) {
+            String keyName = ObjectUtils.doIfNotNull(((EditorConfigOption)element).getFlatOptionKey(), NavigationItem::getName);
+            if (ConfigEncodingManager.charsetKey.equals(keyName)) {
+              String charsetStr = ObjectUtils.doIfNotNull(((EditorConfigOption)element).getOptionValueIdentifier(), NavigationItem::getName);
+              if (charsetStr != null) {
+                charsets.add(charsetStr);
+              }
+            }
+          }
+        }
+      };
+      psiFile.accept(visitor);
+    }
+    return charsets;
   }
 }
