f9a113a1c4c4bceb77c334aa17839e2b93e5643b
Ivan Semenov <ivan.semenov@jetbrains.com>
2021-01-14 18:18:08 +0300

    [github] fix server matching
    GitOrigin-RevId: 3b97367e1e0ece752cfb752be583710cc8e988c6

--- plugins/github/src/org/jetbrains/plugins/github/AbstractAuthenticatingGithubUrlGroupingAction.kt
+++ plugins/github/src/org/jetbrains/plugins/github/AbstractAuthenticatingGithubUrlGroupingAction.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.github
 
 import com.intellij.openapi.actionSystem.AnActionEvent
@@ -31,7 +31,7 @@ abstract class AbstractAuthenticatingGithubUrlGroupingAction(dynamicText: Suppli
 
   private fun getAccount(project: Project, repository: GHGitRepositoryMapping): GithubAccount? {
     val authenticationManager = service<GithubAuthenticationManager>()
-    val accounts = authenticationManager.getAccounts().filter { it.server == repository.repository.serverPath }
+    val accounts = authenticationManager.getAccounts().filter { it.server.equals(repository.repository.serverPath, true) }
     if (accounts.isEmpty()) {
       return authenticationManager.requestNewAccountForServer(repository.repository.serverPath, project)
     }
