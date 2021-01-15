93fd12175f45601a32808f2096fb364f4420c40f
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-01-15 22:15:51 +0100

    [groovy] clean up GroovyPsiManager
    GitOrigin-RevId: 209e8b3a375bb36c9c78800c14946a530ce18831

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/GroovyPsiManager.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/impl/GroovyPsiManager.java
@@ -1,7 +1,6 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.lang.psi.impl;
 
-import com.intellij.openapi.Disposable;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
@@ -9,15 +8,11 @@ import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.util.RecursionGuard;
 import com.intellij.openapi.util.RecursionManager;
 import com.intellij.psi.*;
-import com.intellij.psi.impl.AnyPsiChangeListener;
 import com.intellij.psi.search.GlobalSearchScope;
-import com.intellij.util.ConcurrencyUtil;
 import com.intellij.util.IncorrectOperationException;
-import com.intellij.util.containers.CollectionFactory;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.plugins.groovy.lang.psi.GroovyPsiElement;
 import org.jetbrains.plugins.groovy.lang.psi.GroovyPsiElementFactory;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.GrTypeDefinition;
 import org.jetbrains.plugins.groovy.lang.psi.util.GroovyCommonClassNames;
@@ -26,12 +21,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ConcurrentMap;
-import java.util.function.Function;
 
-import static com.intellij.psi.impl.PsiManagerImpl.ANY_PSI_CHANGE_TOPIC;
+public final class GroovyPsiManager {
 
-public final class GroovyPsiManager implements Disposable {
   private static final Logger LOG = Logger.getInstance(GroovyPsiManager.class);
 
   private static final Set<String> ourPopularClasses = ContainerUtil.newHashSet(GroovyCommonClassNames.GROOVY_LANG_CLOSURE,
@@ -41,34 +33,21 @@ public final class GroovyPsiManager implements Disposable {
                                                                                 CommonClassNames.JAVA_UTIL_LIST,
                                                                                 CommonClassNames.JAVA_UTIL_COLLECTION,
                                                                                 CommonClassNames.JAVA_LANG_STRING);
-  private final Project myProject;
+  private final @NotNull Project myProject;
 
   private final Map<String, GrTypeDefinition> myArrayClass = new HashMap<>();
 
-  private final ConcurrentMap<GroovyPsiElement, PsiType> myCalculatedTypes = CollectionFactory.createConcurrentWeakMap();
-
   private static final RecursionGuard<PsiElement> ourGuard = RecursionManager.createGuard("groovyPsiManager");
 
-  public GroovyPsiManager(Project project) {
+  public GroovyPsiManager(@NotNull Project project) {
     myProject = project;
-    myProject.getMessageBus().connect(this).subscribe(ANY_PSI_CHANGE_TOPIC, new AnyPsiChangeListener() {
-      @Override
-      public void beforePsiChanged(boolean isPhysical) {
-        dropTypesCache();
-      }
-    });
-  }
-
-  public void dropTypesCache() {
-    myCalculatedTypes.clear();
   }
 
   public static GroovyPsiManager getInstance(Project project) {
     return ServiceManager.getService(project, GroovyPsiManager.class);
   }
 
-  @NotNull
-  public PsiClassType createTypeByFQClassName(@NotNull String fqName, @NotNull GlobalSearchScope resolveScope) {
+  public @NotNull PsiClassType createTypeByFQClassName(@NotNull String fqName, @NotNull GlobalSearchScope resolveScope) {
     if (ourPopularClasses.contains(fqName)) {
       PsiClass result = JavaPsiFacade.getInstance(myProject).findClass(fqName, resolveScope);
       if (result != null) {
@@ -79,48 +58,14 @@ public final class GroovyPsiManager implements Disposable {
     return JavaPsiFacade.getElementFactory(myProject).createTypeByFQClassName(fqName, resolveScope);
   }
 
-  private static final PsiType UNKNOWN_TYPE = new GrPsiTypeStub();
-
-  @Nullable
-  public <T extends GroovyPsiElement> PsiType getType(@NotNull T element, @NotNull Function<? super T, ? extends PsiType> calculator) {
-    return getTypeWithCaching(element, myCalculatedTypes, calculator);
-  }
-
-  @Nullable
-  private static <K extends GroovyPsiElement> PsiType getTypeWithCaching(@NotNull K key, @NotNull ConcurrentMap<? super K, PsiType> map, @NotNull Function<? super K, ? extends PsiType> calculator) {
-    PsiType type = map.get(key);
-    if (type == null) {
-      RecursionGuard.StackStamp stamp = RecursionManager.markStack();
-      type = calculator.apply(key);
-      if (type == null) {
-        type = UNKNOWN_TYPE;
-      }
-      if (stamp.mayCacheNow()) {
-        type = ConcurrencyUtil.cacheOrGet(map, key, type);
-      } else {
-        final PsiType alreadyInferred = map.get(key);
-        if (alreadyInferred != null) {
-          type = alreadyInferred;
-        }
-      }
-    }
-    if (!type.isValid()) {
-      error(key, type);
-    }
-    return UNKNOWN_TYPE == type ? null : type;
-  }
-
-  private static void error(PsiElement element, PsiType type) {
-    LOG.error("Type is invalid: " + type + "; element: " + element + " of class " + element.getClass());
-  }
-
-  @Nullable
-  public GrTypeDefinition getArrayClass(@NotNull PsiType type) {
+  public @Nullable GrTypeDefinition getArrayClass(@NotNull PsiType type) {
     final String typeText = type.getCanonicalText();
     GrTypeDefinition definition = myArrayClass.get(typeText);
     if (definition == null) {
       try {
-        definition = GroovyPsiElementFactory.getInstance(myProject).createTypeDefinition("class __ARRAY__ { public int length; public " + typeText + "[] clone(){} }");
+        definition = GroovyPsiElementFactory.getInstance(myProject).createTypeDefinition(
+          "class __ARRAY__ { public int length; public " + typeText + "[] clone(){} }"
+        );
         myArrayClass.put(typeText, definition);
       }
       catch (IncorrectOperationException e) {
@@ -132,8 +77,7 @@ public final class GroovyPsiManager implements Disposable {
     return definition;
   }
 
-  @Nullable
-  public static PsiType inferType(@NotNull PsiElement element, @NotNull Computable<? extends PsiType> computable) {
+  public static @Nullable PsiType inferType(@NotNull PsiElement element, @NotNull Computable<? extends PsiType> computable) {
     List<? extends PsiElement> stack = ourGuard.currentStack();
     if (stack.size() > 7) { //don't end up walking the whole project PSI
       ourGuard.prohibitResultCaching(stack.get(0));
@@ -141,10 +85,5 @@ public final class GroovyPsiManager implements Disposable {
     }
 
     return ourGuard.doPreventingRecursion(element, true, computable);
-  }
-
-  @Override
-  public void dispose() {
-
   }
 }
