16d9064c76a6d6f12e10f3fb6df731ce737d76ed
Gregory.Shrago <gregory.shrago@jetbrains.com>
2021-01-15 21:20:33 +0300

    introduce SimpleDataContext#builder
    GitOrigin-RevId: 98ce453f07b5d4bd233578ef05cc5e1d3f4fa38e

--- platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/SimpleDataContext.java
+++ platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/SimpleDataContext.java
@@ -6,11 +6,15 @@ import com.intellij.ide.impl.DataManagerImpl;
 import com.intellij.ide.impl.dataRules.GetDataRule;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.actionSystem.DataKey;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.IdeFocusManager;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import javax.swing.*;
+import java.util.HashMap;
 import java.util.Map;
 
 public final class SimpleDataContext implements DataContext {
@@ -45,9 +49,16 @@ public final class SimpleDataContext implements DataContext {
            myParent == null ? null : myParent.getData(dataId);
   }
 
+  /** @deprecated use {@link SimpleDataContext#getSimpleContext(DataKey, Object, DataContext)} instead. */
+  @Deprecated
   @NotNull
   public static DataContext getSimpleContext(@NotNull String dataId, @NotNull Object data, DataContext parent) {
     return new SimpleDataContext(Map.of(dataId, data), parent);
+  }
+
+  @NotNull
+  public static <T> DataContext getSimpleContext(@NotNull DataKey<? super T> dataKey, @NotNull T data, DataContext parent) {
+    return new SimpleDataContext(Map.of(dataKey.getName(), data), parent);
   }
 
   @NotNull
@@ -55,13 +66,58 @@ public final class SimpleDataContext implements DataContext {
     return new SimpleDataContext(dataId2data, parent);
   }
 
+  /** @deprecated use {@link SimpleDataContext#getSimpleContext(DataKey, Object)} instead. */
+  @Deprecated
   @NotNull
   public static DataContext getSimpleContext(@NotNull String dataId, @NotNull Object data) {
     return getSimpleContext(dataId, data, null);
   }
 
   @NotNull
+  public static <T> DataContext getSimpleContext(@NotNull DataKey<? super T> dataKey, @NotNull T data) {
+    return getSimpleContext(dataKey, data, null);
+  }
+
+  @NotNull
   public static DataContext getProjectContext(@NotNull Project project) {
     return getSimpleContext(CommonDataKeys.PROJECT.getName(), project);
+  }
+
+  @NotNull
+  public static Builder builder() {
+    return new Builder(null);
+  }
+
+  @NotNull
+  public static Builder builder(@Nullable DataContext parent) {
+    return new Builder(parent);
+  }
+
+  public final static class Builder {
+    private final DataContext myParent;
+    private Map<String, Object> myMap;
+
+    Builder(DataContext parent) {
+      myParent = parent;
+    }
+
+    @NotNull
+    public <T> Builder add(@NotNull DataKey<? super T> dataKey, @Nullable T value) {
+      if (value != null) {
+        if (myMap == null) myMap = new HashMap<>();
+        myMap.put(dataKey.getName(), value);
+      }
+      return this;
+    }
+
+    @NotNull
+    public Builder noUI() {
+      return add(PlatformDataKeys.CONTEXT_COMPONENT, new JLabel());
+    }
+
+    @NotNull
+    public SimpleDataContext build() {
+      return new SimpleDataContext(myMap, myParent);
+    }
   }
 }
