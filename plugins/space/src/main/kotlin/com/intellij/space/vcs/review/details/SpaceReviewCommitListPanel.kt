3ead56fcfb5d312ef6a74fcc8fdd0f861db189bc
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2021-01-15 22:46:30 +0300

    [space] Change splitters behaviour in review commits tab
    There should be two splitter parts: commit list with details and changes browser
    
    GitOrigin-RevId: ff5f918c099f8f757ef93eee99ca139be166cbb5

--- plugins/space/src/main/kotlin/com/intellij/space/vcs/review/details/SpaceReviewCommitListPanel.kt
+++ plugins/space/src/main/kotlin/com/intellij/space/vcs/review/details/SpaceReviewCommitListPanel.kt
@@ -1,10 +1,11 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.space.vcs.review.details
 
 import circlet.code.api.CodeReviewRecord
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.project.Project
 import com.intellij.space.vcs.review.details.diff.SpaceDiffFile
+import com.intellij.space.vcs.review.details.diff.SpaceDiffVm
 import com.intellij.testFramework.LightVirtualFile
 import com.intellij.ui.IdeBorderFactory
 import com.intellij.ui.OnePixelSplitter
@@ -17,58 +18,47 @@ import com.intellij.vcs.log.impl.VcsUserImpl
 import com.intellij.vcs.log.ui.details.CommitDetailsListPanel
 import com.intellij.vcs.log.ui.details.commit.CommitDetailsPanel
 import libraries.coroutines.extra.Lifetime
-import runtime.reactive.Property
 import javax.swing.JComponent
 
-internal class SpaceReviewCommitListPanel(parentDisposable: Disposable,
-                                          reviewDetailsVm: SpaceReviewDetailsVm<out CodeReviewRecord>) : BorderLayoutPanel() {
+internal class SpaceReviewCommitListPanel(
+  private val parentDisposable: Disposable,
+  private val reviewDetailsVm: SpaceReviewDetailsVm<out CodeReviewRecord>
+) : BorderLayoutPanel() {
   init {
-    val commitsBrowser = OnePixelSplitter(true, "space.review.commit.list", 0.4f).apply {
+    val reviewCommitsList = OnePixelSplitter(true, "space.review.commit.list.v2", 0.6f).apply {
       val changesVm = reviewDetailsVm.commitChangesVm
-      val diffVm = reviewDetailsVm.spaceDiffVm
-
-      val selectedCommitDetails = OnePixelSplitter(true, "space.review.commit.list.details", 0.3f).apply {
-        val commitDetailsPanel = createCommitsDetailsPanel(parentDisposable, reviewDetailsVm.ideaProject, changesVm)
-
-        val tree = SpaceReviewChangesTreeFactory.create(
-          reviewDetailsVm.ideaProject,
-          this,
-          changesVm,
-          object : SpaceDiffFileProvider {
-            override fun getSpaceDiffFile(): SpaceDiffFile {
-              return SpaceDiffFile(reviewDetailsVm.selectedChangesVm, diffVm)
-            }
-          }
-        ).apply {
-          border = IdeBorderFactory.createBorder(SideBorder.TOP)
-        }
-        val treeActionsToolbarPanel = createChangesBrowserToolbar(tree)
-
-        firstComponent = commitDetailsPanel
-        secondComponent = BorderLayoutPanel()
-          .addToTop(treeActionsToolbarPanel)
-          .addToCenter(tree)
-      }
-
-      firstComponent = SpaceReviewCommitListFactory.createCommitList(
-        object : SpaceReviewCommitListVm {
-          override val commits: Property<List<SpaceReviewCommitListItem>> = reviewDetailsVm.commits
-          override val lifetime: Lifetime = reviewDetailsVm.lifetime
-        },
-        object : SpaceReviewCommitListController {
-          override fun setSelectedCommitsIndices(indices: List<Int>) {
-            reviewDetailsVm.commitChangesVm.selectedCommitIndices.value = indices
-          }
-        }
-      )
-      secondComponent = selectedCommitDetails
+      firstComponent = createCommitListWithDetails(reviewDetailsVm.lifetime, changesVm)
+      secondComponent = createChangesBrowserPanel(reviewDetailsVm.spaceDiffVm, changesVm)
     }
-    addToCenter(commitsBrowser)
+    addToCenter(reviewCommitsList)
   }
 
-  private fun createCommitsDetailsPanel(parentDisposable: Disposable, project: Project, changesVm: SpaceReviewChangesVm): JComponent {
+  private fun createCommitListWithDetails(
+    lifetime: Lifetime,
+    changesVm: SpaceReviewChangesVm
+  ): JComponent {
+    val commitList = SpaceReviewCommitListFactory.createCommitList(
+      object : SpaceReviewCommitListVm {
+        override val commits = reviewDetailsVm.commits
+        override val lifetime = lifetime
+      },
+      object : SpaceReviewCommitListController {
+        override fun setSelectedCommitsIndices(indices: List<Int>) {
+          changesVm.selectedCommitIndices.value = indices
+        }
+      }
+    )
+
+    val commitDetails = createCommitsDetailsPanel(reviewDetailsVm.ideaProject, changesVm)
+    return OnePixelSplitter(true, "space.review.commit.list.details.v2", 0.6f).apply {
+      firstComponent = commitList
+      secondComponent = commitDetails
+    }
+  }
+
+  private fun createCommitsDetailsPanel(project: Project, changesVm: SpaceReviewChangesVm): JComponent {
     val lifetime = changesVm.lifetime
-    val commitListPanel: CommitDetailsListPanel<CommitDetailsPanel> = object : CommitDetailsListPanel<CommitDetailsPanel>(parentDisposable) {
+    val commitListPanel = object : CommitDetailsListPanel<CommitDetailsPanel>(parentDisposable) {
       init {
         border = JBUI.Borders.empty()
       }
@@ -94,6 +84,28 @@ internal class SpaceReviewCommitListPanel(parentDisposable: Disposable,
     }
 
     return commitListPanel
+  }
+
+  private fun createChangesBrowserPanel(
+    diffVm: SpaceDiffVm,
+    changesVm: SpaceReviewChangesVm
+  ): JComponent {
+    val tree = SpaceReviewChangesTreeFactory.create(
+      reviewDetailsVm.ideaProject,
+      this,
+      changesVm,
+      object : SpaceDiffFileProvider {
+        override fun getSpaceDiffFile(): SpaceDiffFile {
+          return SpaceDiffFile(reviewDetailsVm.selectedChangesVm, diffVm)
+        }
+      }
+    ).apply {
+      border = IdeBorderFactory.createBorder(SideBorder.TOP)
+    }
+
+    return BorderLayoutPanel()
+      .addToTop(createChangesBrowserToolbar(tree))
+      .addToCenter(tree)
   }
 }
 
