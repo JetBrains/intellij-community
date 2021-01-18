37ff34473e89a1a305c6b6806f2041b8a340124d
Boris Pristupa <boris.pristupa@jetbrains.com>
2021-01-18 18:21:09 +0300

    [Markdown] add option for (un)selecting all entity highlighting
    GitOrigin-RevId: 7493f8a28413f7b9567cfa838f68401e0bfc30dc

---
+++ plugins/markdown/src/org/intellij/plugins/markdown/ui/actions/styling/highlighting/HighlightAllEntitiesAction.kt
@@ -0,0 +1,17 @@
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.intellij.plugins.markdown.ui.actions.styling.highlighting
+
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.ToggleAction
+import org.intellij.plugins.markdown.highlighting.ner.HighlightedEntityType
+
+class HighlightAllEntitiesAction : ToggleAction() {
+  override fun isSelected(e: AnActionEvent): Boolean = HighlightedEntityType.values().all(HighlightedEntityType::isEnabled)
+
+  override fun setSelected(e: AnActionEvent, state: Boolean) {
+    val allSelected = isSelected(e)
+    for (entity in HighlightedEntityType.values()) {
+      entity.highlightingManager.setHighlightingEnabled(!allSelected, e)
+    }
+  }
+}
