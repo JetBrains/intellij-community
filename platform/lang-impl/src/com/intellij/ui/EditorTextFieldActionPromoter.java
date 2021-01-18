ed7b17316c451fdb06d0497854fda712e4f4f5e0
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2021-01-18 14:33:47 +0300

    Fix "EditorTextFieldActionPromoter" - only promote "text processing"-oriented editor actions (IDEA-259561)
    GitOrigin-RevId: 3698276c79c862407572f26bfdbea97b8f529752

--- platform/lang-impl/src/com/intellij/ui/EditorTextFieldActionPromoter.java
+++ platform/lang-impl/src/com/intellij/ui/EditorTextFieldActionPromoter.java
@@ -4,41 +4,26 @@ package com.intellij.ui;
 import com.intellij.openapi.actionSystem.ActionPromoter;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.DataContext;
-import com.intellij.openapi.editor.actionSystem.EditorAction;
 import com.intellij.openapi.editor.actions.TextComponentEditorAction;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
-import static java.util.Comparator.comparingInt;
+import static com.intellij.util.containers.ContainerUtil.filterIsInstance;
 
 /**
+ * Encapsulates sorting rule that defines what editor actions have precedence to non-editor actions.
+ * Current approach is that we want to process text processing-oriented editor actions with higher priority.
+ * <p/>
+ * Rationale: {@code 'Ctrl+Shift+Right/Left Arrow'} shortcut is bound to
+ * {@code 'expand/reduce selection by word'} editor action and {@code 'change dialog width'} non-editor action
+ * and we want to use the first one.
+ *
  * @author Konstantin Bulenkov
  */
 public class EditorTextFieldActionPromoter implements ActionPromoter {
-  /**
-   * Encapsulates sorting rule that defines what editor actions have precedence to non-editor actions. Current approach is that
-   * we want to process text processing-oriented editor actions with higher priority than non-editor actions and all
-   * other editor actions with lower priority.
-   * <p/>
-   * Rationale: there is at least one commit-specific action that is mapped to the editor action by default
-   * ({@code 'show commit messages history'} vs {@code 'scroll to center'}). We want to process the former on target
-   * short key triggering. Another example is that {@code 'Ctrl+Shift+Right/Left Arrow'} shortcut is bound to
-   * {@code 'expand/reduce selection by word'} editor action and {@code 'change dialog width'} non-editor action
-   * and we want to use the first one.
-   */
-  private static final Comparator<AnAction> ACTIONS_COMPARATOR = comparingInt(action -> {
-    if (action instanceof TextComponentEditorAction) return -1;
-    if (action instanceof EditorAction) return 1;
-    return 0;
-  });
-
   @Override
   public List<AnAction> promote(@NotNull List<AnAction> actions, @NotNull DataContext context) {
-    ArrayList<AnAction> result = new ArrayList<>(actions);
-    result.sort(ACTIONS_COMPARATOR);
-    return result;
+    return filterIsInstance(actions, TextComponentEditorAction.class);
   }
 }
