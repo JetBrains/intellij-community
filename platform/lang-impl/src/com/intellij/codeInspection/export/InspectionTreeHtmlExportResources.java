bdee656f5d4b595fa7f7bf56c34cf101a55f4fa7
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2021-01-19 08:58:25 +0100

    IDEA-259617 Can't export inspection results to HTML
    GitOrigin-RevId: a10bec5f53a314400a57d9f5ab369388baf41711

--- platform/lang-impl/src/com/intellij/codeInspection/export/InspectionTreeHtmlExportResources.java
+++ platform/lang-impl/src/com/intellij/codeInspection/export/InspectionTreeHtmlExportResources.java
@@ -1,15 +1,15 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInspection.export;
 
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.util.io.FileUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
 
 /**
  * @author Dmitry Batkovich
@@ -22,20 +22,17 @@ final class InspectionTreeHtmlExportResources {
     copyInspectionReportResource("script.js", targetDirectory);
   }
 
-  private static void copyInspectionReportResource(final String resourceName, @NotNull Path targetDirectory) {
+  private static void copyInspectionReportResource(@NotNull String resourceName, @NotNull Path targetDirectory) {
     Path resourceTargetFile = targetDirectory.resolve(resourceName);
-
     try {
       Files.createDirectories(resourceTargetFile.getParent());
     }
     catch (IOException e) {
-      LOG.error("Can't create file: " + resourceTargetFile.toString());
+      LOG.error("Can't create file: " + resourceTargetFile);
     }
 
-    try (InputStream input = InspectionTreeHtmlExportResources.class.getClassLoader().getResourceAsStream("/inspectionReport/" + resourceName)) {
-      try (OutputStream f = Files.newOutputStream(resourceTargetFile)) {
-        FileUtil.copy(input, f);
-      }
+    try (InputStream input = InspectionTreeHtmlExportResources.class.getClassLoader().getResourceAsStream("inspectionReport/" + resourceName)) {
+      Files.copy(Objects.requireNonNull(input), resourceTargetFile, StandardCopyOption.REPLACE_EXISTING);
     }
     catch (IOException e) {
       LOG.error(e);
