8c5690b8a928e2406b1152a6cd116a81397a2ea2
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2021-01-19 18:18:01 +0700

    [java-refactoring] Testdata fixed
    GitOrigin-RevId: d9e82cd43846bcf8078bac1d6a10be19efe3a9f6

--- java/java-tests/testData/refactoring/inplaceIntroduceVariable/brokenFormattingWithInValidation_after.java
+++ java/java-tests/testData/refactoring/inplaceIntroduceVariable/brokenFormattingWithInValidation_after.java
@@ -4,7 +4,7 @@ class C {
         for (int i = 0; i < array.length; i++) {
             j = 0;
             boolean bool = array[j] > array[i];
-            while (j<i)&&(array[j] > array[i])
+            while (j<i)&&(array[j]>array[i])
 
         }
     }
