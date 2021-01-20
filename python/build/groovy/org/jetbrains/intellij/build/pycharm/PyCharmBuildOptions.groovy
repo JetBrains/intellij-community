9fde7cfa95b9393804739c1c640bdee4b2395a86
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2021-01-20 18:25:14 +0300

    PyCharm: remove Anaconda edition property files
    GitOrigin-RevId: 52d81716985689d31b7f7a83b946da5ca65691ac

--- python/build/groovy/org/jetbrains/intellij/build/pycharm/PyCharmBuildOptions.groovy
+++ python/build/groovy/org/jetbrains/intellij/build/pycharm/PyCharmBuildOptions.groovy
@@ -35,11 +35,6 @@ final class PyCharmBuildOptions {
   static final String prebuiltStubsArchive = System.getProperty("intellij.build.pycharm.prebuilt.stubs.archive")
 
   /**
-   * Build PyCharm with Anaconda plugin distributions together with regular PyCharm
-   */
-  static final boolean buildWithAnacondaEdition = SystemProperties.getBooleanProperty("intellij.build.pycharm.anaconda.edition", false)
-
-  /**
    * Miniconda installer folder name
    * The same folder should be specified at com.jetbrains.python.conda.PythonMinicondaLocator#getMinicondaInstallerFolder()
    * */
