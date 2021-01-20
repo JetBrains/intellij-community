16621fd43d4f92b391c8014dcf468131d45d28d8
KotlinIsland <kotlinisland@users.noreply.github.com>
2021-01-20 21:21:53 +0300

    PY-40863 Highlight "{{", "}}" as escape sequence in fstrings
    Update locateToken to consider double curly braces and override getEscapeSequenceType accordingly to return StringEscapesTokenTypes.VALID_STRING_ESCAPE_TOKEN for such tokens.
    
    close #1448
    
    GitOrigin-RevId: b30aa4bfc15e2a83f5b33006d3057f056e2013ae

--- python/python-psi-impl/src/com/jetbrains/python/lexer/PyFStringLiteralLexer.kt
+++ python/python-psi-impl/src/com/jetbrains/python/lexer/PyFStringLiteralLexer.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.python.lexer
 
+import com.intellij.psi.StringEscapesTokenTypes
 import com.intellij.psi.tree.IElementType
 import com.intellij.util.text.CharArrayUtil
 import com.jetbrains.python.PyTokenTypes
@@ -10,17 +11,15 @@ class PyFStringLiteralLexer(fStringTextToken: IElementType) : PyStringLiteralLex
     assert(PyTokenTypes.FSTRING_TEXT_TOKENS.contains(fStringTextToken))
   }
 
-  override fun locateToken(start: Int): Int {
-    if (start >= myBufferEnd) {
-      return myBufferEnd
-    }
-    
-    if (myBuffer[start] == '\\') {
-      return locateEscapeSequence(start)
-    }
-    else {
+  override fun locateToken(start: Int): Int = when {
+    start >= myBufferEnd -> myBufferEnd
+    myBuffer[start] == '\\' -> locateEscapeSequence(start)
+    isDoubleCurley() -> start + 2
+    else -> {
       val nextBackslashOffset = CharArrayUtil.indexOf(myBuffer, "\\", start + 1, myBufferEnd)
-      return if (nextBackslashOffset >= 0) nextBackslashOffset else myBufferEnd
+      val nextLCurleyOffset = CharArrayUtil.indexOf(myBuffer, "{{", start + 1, myBufferEnd)
+      val nextRCurleyOffset = CharArrayUtil.indexOf(myBuffer, "}}", start + 1, myBufferEnd)
+      setOf(nextBackslashOffset, nextLCurleyOffset, nextRCurleyOffset).filter { it >= 0 }.minOrNull() ?: myBufferEnd
     }
   }
 
@@ -29,4 +28,13 @@ class PyFStringLiteralLexer(fStringTextToken: IElementType) : PyStringLiteralLex
   override fun isUnicodeMode(): Boolean = true
 
   override fun getState(): Int = myBaseLexerState
+
+  override fun isEscape() = isDoubleCurley() || super.isEscape()
+
+  override fun getEscapeSequenceType(): IElementType =
+    if (isDoubleCurley()) StringEscapesTokenTypes.VALID_STRING_ESCAPE_TOKEN else super.getEscapeSequenceType()
+
+  private fun isDoubleCurley() =
+    myBufferEnd > myStart + 1 &&
+    ((myBuffer[myStart] == '{' && myBuffer[myStart + 1] == '{') || (myBuffer[myStart] == '}' && myBuffer[myStart + 1] == '}'))
 }
