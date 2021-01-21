5eef6f9ceb4f4c7118a0eba46a3220ddb75a0ccd
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2021-01-21 13:22:12 +0100

    ignore IllegalAccessException as well
    GitOrigin-RevId: cb9a2cb59c392a84cc65693877d612367b8952f2

--- platform/core-impl/src/com/intellij/ide/plugins/PluginManagerCore.java
+++ platform/core-impl/src/com/intellij/ide/plugins/PluginManagerCore.java
@@ -385,7 +385,7 @@ public final class PluginManagerCore {
       return (boolean)MethodHandles.publicLookup().findVirtual(aClass, "hasLoadedClass", HAS_LOADED_CLASS_METHOD_TYPE)
         .invoke(loader, className);
     }
-    catch (NoSuchMethodError | IllegalAccessError ignore) {
+    catch (NoSuchMethodError | IllegalAccessError | IllegalAccessException ignore) {
     }
     catch (Throwable e) {
       getLogger().error(e);
