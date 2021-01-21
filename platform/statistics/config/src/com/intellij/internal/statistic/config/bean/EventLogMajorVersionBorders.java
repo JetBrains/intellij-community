f007639081a3d4f410debeafae6cbfbde608152c
Anastasia Ivanova <anastasia.ivanova@jetbrains.com>
2021-01-21 20:46:01 +0100

    IDEA-259507: remove dependency of config module on validator module
    GitOrigin-RevId: 4f9eeb1fdc6bd1922588d091a2b3aa851a743379

--- platform/statistics/config/src/com/intellij/internal/statistic/config/bean/EventLogMajorVersionBorders.java
+++ platform/statistics/config/src/com/intellij/internal/statistic/config/bean/EventLogMajorVersionBorders.java
@@ -1,24 +1,24 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.internal.statistic.config.bean;
 
-import com.intellij.internal.statistic.eventLog.EventLogBuild;
+import com.intellij.internal.statistic.eventLog.EventLogMajorVersion;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class EventLogMajorVersion {
+public class EventLogMajorVersionBorders {
   @Nullable
   public String from;
   @Nullable
   public String to;
 
   public boolean accept(@NotNull String current) {
-    EventLogBuild build = EventLogBuild.fromString(current);
+    EventLogMajorVersion build = EventLogMajorVersion.fromString(current);
     if (!isValidMajorVersion(build)) {
       return false;
     }
 
-    EventLogBuild toVersion = EventLogBuild.fromString(to);
-    EventLogBuild fromVersion = EventLogBuild.fromString(from);
+    EventLogMajorVersion toVersion = EventLogMajorVersion.fromString(to);
+    EventLogMajorVersion fromVersion = EventLogMajorVersion.fromString(from);
     if (!isValidMajorVersion(toVersion) && !isValidMajorVersion(fromVersion)) {
       return false;
     }
@@ -27,7 +27,7 @@ public class EventLogMajorVersion {
            (toVersion == null || toVersion.compareTo(build) > 0);
   }
 
-  private static boolean isValidMajorVersion(@Nullable EventLogBuild build) {
+  private static boolean isValidMajorVersion(@Nullable EventLogMajorVersion build) {
     if (build == null) return false;
 
     int[] components = build.getComponents();
