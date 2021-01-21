ecf77bf38ca06585040d1b1424ef64d933af40e3
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-01-21 11:57:16 +0100

    @NotNull parameter of `UsageTypeProvider#getUsageType`
    GitOrigin-RevId: 87bf3eb9e68e247b560cf09c63ba82a53233147a

--- python/src/com/jetbrains/python/findUsages/PyUsageTypeProvider.java
+++ python/src/com/jetbrains/python/findUsages/PyUsageTypeProvider.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.python.findUsages;
 
 import com.intellij.psi.PsiElement;
@@ -25,7 +25,7 @@ public class PyUsageTypeProvider implements UsageTypeProviderEx {
   private static final UsageType USAGE_IN_SUPERCLASS = new UsageType(PyPsiBundle.messagePointer("python.find.usages.usage.in.superclass.list"));
 
   @Override
-  public UsageType getUsageType(PsiElement element) {
+  public UsageType getUsageType(@NotNull PsiElement element) {
     return getUsageType(element, UsageTarget.EMPTY_ARRAY);
   }
 
--- xml/dom-impl/src/com/intellij/util/xml/DomUsageTypeProvider.java
+++ xml/dom-impl/src/com/intellij/util/xml/DomUsageTypeProvider.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.util.xml;
 
@@ -8,15 +8,17 @@ import com.intellij.psi.PsiFile;
 import com.intellij.psi.xml.XmlFile;
 import com.intellij.usages.impl.rules.UsageType;
 import com.intellij.usages.impl.rules.UsageTypeProvider;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
  * @author Gregory.Shrago
  */
 public class DomUsageTypeProvider implements UsageTypeProvider {
+
   @Override
   @Nullable
-  public UsageType getUsageType(PsiElement element) {
+  public UsageType getUsageType(@NotNull PsiElement element) {
     final PsiFile psiFile = element.getContainingFile();
     if (psiFile != null && XMLLanguage.INSTANCE.equals(psiFile.getLanguage()) &&
         DomManager.getDomManager(element.getProject()).getFileElement((XmlFile)psiFile, DomElement.class) != null) {
