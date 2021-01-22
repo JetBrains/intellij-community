7b591e8ef935d67513a2cc94ed47c9bdecbab520
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-01-22 15:30:20 +0100

    IDEA-260122 [groovy] index groovy parameter annotations
    GitOrigin-RevId: c8e7e8b7f51d256ea7be12ae644feca80d86f41e

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/stubs/elements/GrStubFileElementType.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/stubs/elements/GrStubFileElementType.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.lang.psi.stubs.elements;
 
 import com.intellij.lang.ASTNode;
@@ -29,7 +29,7 @@ import java.io.IOException;
  */
 public class GrStubFileElementType extends IStubFileElementType<GrFileStub> {
 
-  public static final int STUB_VERSION = 50;
+  public static final int STUB_VERSION = 51;
 
   public GrStubFileElementType(Language language) {
     super(language);
