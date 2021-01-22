d3e6d5ccd746fae4f866f7014c14bb974210acf8
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2021-01-22 12:43:15 +0300

    [Gradle] fix for Gradle 6.8 update IDEA-247529 IDEA-256250
    GitOrigin-RevId: 0877d2d9ba7bdf44dce1a5018e714c0978719b0a

--- plugins/gradle/tooling-proxy/src/org/jetbrains/plugins/gradle/tooling/proxy/TargetIncomingConnectionHandler.kt
+++ plugins/gradle/tooling-proxy/src/org/jetbrains/plugins/gradle/tooling/proxy/TargetIncomingConnectionHandler.kt
@@ -1,14 +1,14 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.gradle.tooling.proxy
 
-import org.gradle.internal.remote.internal.RemoteConnection
 import org.gradle.launcher.daemon.protocol.BuildEvent
 import org.gradle.launcher.daemon.protocol.Message
 import org.gradle.launcher.daemon.server.IncomingConnectionHandler
+import org.gradle.launcher.daemon.server.SynchronizedDispatchConnection
 import org.slf4j.LoggerFactory
 
 class TargetIncomingConnectionHandler : IncomingConnectionHandler {
-  private lateinit var connection: RemoteConnection<Message>
+  private lateinit var connection: SynchronizedDispatchConnection<Message>
   private lateinit var buildParameters: TargetBuildParameters
 
   fun isConnected() = ::connection.isInitialized
@@ -16,17 +16,14 @@ class TargetIncomingConnectionHandler : IncomingConnectionHandler {
 
   fun targetBuildParameters() = buildParameters
 
-  override fun handle(connection: RemoteConnection<Message>) {
+  override fun handle(connection: SynchronizedDispatchConnection<Message>) {
     LOG.debug("connection got $connection")
     this.connection = connection
     val message = connection.receive() as BuildEvent
     this.buildParameters = message.payload as TargetBuildParameters
   }
 
-  fun dispatch(message: Message) = connection.run {
-    dispatch(message)
-    flush()
-  }
+  fun dispatch(message: Message) = connection.dispatchAndFlush(message)
 
   companion object {
     private val LOG = LoggerFactory.getLogger(TargetIncomingConnectionHandler::class.java)
