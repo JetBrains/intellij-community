7de13c812247fce3fb23a9541748f8865d90a7e7
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2021-01-24 22:54:34 +0300

    wsl: extract WslDistributionComboBox
    GitOrigin-RevId: e2ab66fb438430fd5e7855a9ac020dad7bf412c0

--- platform/platform-impl/src/com/intellij/execution/wsl/target/WslTargetConfigurable.kt
+++ platform/platform-impl/src/com/intellij/execution/wsl/target/WslTargetConfigurable.kt
@@ -2,73 +2,26 @@
 package com.intellij.execution.wsl.target
 
 import com.intellij.execution.target.getTargetType
-import com.intellij.execution.wsl.WSLDistribution
-import com.intellij.execution.wsl.WslDistributionManager
+import com.intellij.execution.wsl.ui.WslDistributionComboBox
 import com.intellij.ide.IdeBundle
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.options.BoundConfigurable
-import com.intellij.openapi.ui.ComboBox
 import com.intellij.openapi.ui.DialogPanel
-import com.intellij.ui.CollectionComboBoxModel
-import com.intellij.ui.ColoredListCellRenderer
 import com.intellij.ui.layout.*
-import javax.swing.JList
 
 class WslTargetConfigurable(val config: WslTargetEnvironmentConfiguration) :
   BoundConfigurable(config.displayName, config.getTargetType().helpTopic) {
 
   override fun createPanel(): DialogPanel = panel {
     row(label = IdeBundle.message("wsl.linux.distribution.label")) {
-      val comboBox: ComboBox<WSLDistribution?> = createComboBox()
+      val comboBox = WslDistributionComboBox(null, true)
       comboBox().withBinding(
-        { c -> c.selectedItem as? WSLDistribution },
-        { c, v -> c.selectedItem = v },
+        { c -> c.selected },
+        { c, v -> c.selected = v },
         PropertyBinding(
           { config.distribution },
           { config.distribution = it }
         )
       )
-    }
-  }
-
-  private fun createComboBox(): ComboBox<WSLDistribution?> {
-    val model = CollectionComboBoxModel<WSLDistribution?>()
-    val comboBox: ComboBox<WSLDistribution?> = ComboBox()
-    comboBox.model = model
-    comboBox.renderer = WslDistributionRenderer()
-
-    val selectedDistribution: WSLDistribution? = config.distribution
-    if (selectedDistribution != null) {
-      model.add(selectedDistribution)
-    }
-    else {
-      // show "No available distributions" message fully
-      model.add(null)
-      comboBox.preferredSize = comboBox.preferredSize
-      model.removeAll()
-    }
-
-    WslDistributionManager.getInstance().installedDistributionsFuture.thenAccept { distributions ->
-      var result = distributions
-      if (selectedDistribution != null && distributions.find { it.msId.equals(selectedDistribution.msId, ignoreCase = true) } == null) {
-        result = distributions + selectedDistribution
-      }
-      ApplicationManager.getApplication().invokeLater(Runnable {
-        model.removeAll()
-        model.addAll(0, result)
-      }, ModalityState.any())
-    }
-    return comboBox
-  }
-
-  private class WslDistributionRenderer : ColoredListCellRenderer<WSLDistribution?>() {
-    override fun customizeCellRenderer(list: JList<out WSLDistribution?>,
-                                       value: WSLDistribution?,
-                                       index: Int,
-                                       selected: Boolean,
-                                       hasFocus: Boolean) {
-      append(value?.presentableName ?: IdeBundle.message("wsl.no.available.distributions"))
     }
   }
 }
