2bff999a1e597430d5ed287decb33c987b2305bb
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2021-01-25 18:15:07 +0700

    [java-inspections] Better comments placement on quick-fix (IDEA-259626)
    GitOrigin-RevId: ca8f1b545c9625011890bc22ed4d584cc740b400

--- java/java-tests/testData/inspection/switchExpressionMigration/afterSwitchStatements.java
+++ java/java-tests/testData/inspection/switchExpressionMigration/afterSwitchStatements.java
@@ -4,8 +4,8 @@ import java.util.*;
 class SwitchExpressionMigration {
   private static String m(int n) {
       switch (n) {
-          /*1*/
-          case 1 -> System.out.println("a"/*2*/); /*3*/
+          case 1 ->/*1*/
+                  System.out.println("a"/*2*/); /*3*/
           case 2 -> System.out.println("b");
       }
   }
