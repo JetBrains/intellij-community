c5d69b39735abf7abb6633869f6d7e1b91088252
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2021-01-25 15:52:30 +0100

    [dynamic plugins] statistics merged #IDEA-246104
    GitOrigin-RevId: 80afd9c47e2cb022e98a16a414bc9f6bc6dcf884

--- platform/platform-impl/src/com/intellij/internal/statistic/collectors/fus/plugins/PluginsUsagesCollector.java
+++ platform/platform-impl/src/com/intellij/internal/statistic/collectors/fus/plugins/PluginsUsagesCollector.java
@@ -1,9 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.internal.statistic.collectors.fus.plugins;
 
-import com.intellij.ide.plugins.DisabledPluginsState;
-import com.intellij.ide.plugins.IdeaPluginDescriptor;
-import com.intellij.ide.plugins.PluginManagerCore;
+import com.intellij.ide.plugins.*;
 import com.intellij.internal.statistic.beans.MetricEvent;
 import com.intellij.internal.statistic.eventLog.EventLogGroup;
 import com.intellij.internal.statistic.eventLog.events.EventFields;
@@ -15,14 +13,20 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 final class PluginsUsagesCollector extends ApplicationUsagesCollector {
-  private static final EventLogGroup GROUP = new EventLogGroup("plugins", 3);
+  private static final EventLogGroup GROUP = new EventLogGroup("plugins",
+                                                               4);
   private static final EventId1<PluginInfo> DISABLED_PLUGIN = GROUP.registerEvent("disabled.plugin",
                                                                                   EventFields.PluginInfo);
   private static final EventId1<PluginInfo> ENABLED_NOT_BUNDLED_PLUGIN = GROUP.registerEvent("enabled.not.bundled.plugin",
                                                                                              EventFields.PluginInfo);
+  private static final EventId1<Integer> PER_PROJECT_ENABLED = GROUP.registerEvent("per.project.enabled",
+                                                                                   EventFields.Count);
+  private static final EventId1<Integer> PER_PROJECT_DISABLED = GROUP.registerEvent("per.project.disabled",
+                                                                                    EventFields.Count);
 
   @Override
   public EventLogGroup getGroup() {
@@ -35,6 +39,10 @@ final class PluginsUsagesCollector extends ApplicationUsagesCollector {
     Set<MetricEvent> result = new HashSet<>();
     result.addAll(getDisabledPlugins());
     result.addAll(getEnabledNonBundledPlugins());
+    result.addAll(getPerProjectPlugins(PER_PROJECT_ENABLED,
+                                       state -> state.getEnabledPlugins$intellij_platform_ide_impl()));
+    result.addAll(getPerProjectPlugins(PER_PROJECT_DISABLED,
+                                       state -> state.getDisabledPlugins$intellij_platform_ide_impl()));
     return result;
   }
 
@@ -55,6 +63,20 @@ final class PluginsUsagesCollector extends ApplicationUsagesCollector {
       .filter(descriptor -> !descriptor.isBundled())
       .map(PluginInfoDetectorKt::getPluginInfoByDescriptor)
       .map(ENABLED_NOT_BUNDLED_PLUGIN::metric)
+      .collect(Collectors.toUnmodifiableSet());
+  }
+
+  private static @NotNull Set<MetricEvent> getPerProjectPlugins(@NotNull EventId1<Integer> eventId,
+                                                                @NotNull Function<ProjectPluginTracker.Companion.@NotNull ProjectPluginTrackerState, @NotNull Set<String>> countProducer) {
+    return ProjectPluginTrackerManager
+      .getInstance()
+      .getStatesByProject$intellij_platform_ide_impl()
+      .values()
+      .stream()
+      .map(countProducer)
+      .filter(set -> !set.isEmpty())
+      .mapToInt(Set::size)
+      .mapToObj(eventId::metric)
       .collect(Collectors.toUnmodifiableSet());
   }
 }
