98f2cd578ac1d62f6c994c7e124f1e32cfc82fa1
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-01-26 19:31:34 +0100

    IDEA-260227 add test for ctrl+mouse documentation on a lambda parameter
    GitOrigin-RevId: 65b32ec19f159956adc298eba0d02aa05d4c2c0d

---
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/javadoc/JavaCtrlMouseTest.kt
@@ -0,0 +1,25 @@
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.java.codeInsight.javadoc
+
+import com.intellij.JavaTestUtil
+import com.intellij.codeInsight.navigation.actions.GotoDeclarationAction
+import com.intellij.java.codeInsight.javadoc.JavaDocInfoGeneratorTest.assertEqualsFileText
+import com.intellij.testFramework.RunsInEdt
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase4
+import org.junit.Test
+
+@RunsInEdt
+class JavaCtrlMouseTest : LightJavaCodeInsightFixtureTestCase4(
+  testDataPath = "${JavaTestUtil.getJavaTestDataPath()}/codeInsight/ctrlMouse/"
+) {
+
+  private fun doTest() {
+    val testName = testName
+    fixture.configureByFile("$testName.java")
+    val actual = GotoDeclarationAction().getCtrlMouseInfo(fixture.editor, fixture.file, fixture.caretOffset)?.docInfo?.text!!
+    assertEqualsFileText("$testDataPath$testName.html", actual)
+  }
+
+  @Test
+  fun `lambda parameter`(): Unit = doTest()
+}
