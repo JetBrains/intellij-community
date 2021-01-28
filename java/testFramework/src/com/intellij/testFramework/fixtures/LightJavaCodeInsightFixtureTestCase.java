372b8038b830af0930e7a61ca5c3785bbad035b5
Yann Cébron <yann.cebron@jetbrains.com>
2021-01-28 16:57:33 +0100

    LightJavaCodeInsightFixtureTestCase: note junit4 variant
    GitOrigin-RevId: dd4419bb78fb113799a40c2e35fa36109bc72d70

--- java/testFramework/src/com/intellij/testFramework/fixtures/LightJavaCodeInsightFixtureTestCase.java
+++ java/testFramework/src/com/intellij/testFramework/fixtures/LightJavaCodeInsightFixtureTestCase.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.testFramework.fixtures;
 
 import com.intellij.lang.Language;
@@ -21,6 +21,7 @@ import org.jetbrains.annotations.NotNull;
 import java.io.File;
 
 /**
+ * @see LightJavaCodeInsightFixtureTestCase4 for JUnit4 variant
  * @author peter
  */
 public abstract class LightJavaCodeInsightFixtureTestCase extends UsefulTestCase implements TestIndexingModeSupporter {
