79f10007e8a8b5f899c6c02140951f724612e8ac
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2021-01-28 13:08:05 +0300

    IDEA-224228: consume processed key event
    GitOrigin-RevId: d5d985a0055dca6e74e053c9af2c275e92919866

--- platform/platform-impl/src/com/intellij/ide/actions/Switcher.java
+++ platform/platform-impl/src/com/intellij/ide/actions/Switcher.java
@@ -131,11 +131,13 @@ public final class Switcher extends AnAction implements DumbAware {
         if (SWITCHER != null && event instanceof KeyEvent && !SWITCHER.isPinnedMode()) {
           final KeyEvent keyEvent = (KeyEvent)event;
           if (event.getID() == KEY_RELEASED && keyEvent.getKeyCode() == CTRL_KEY) {
+            keyEvent.consume();
             ApplicationManager.getApplication().invokeLater(CHECKER, ModalityState.current());
             return true; // because the key event is actually processed
           }
           else if (event.getID() == KEY_PRESSED && event != INIT_EVENT
                    && (tw = SWITCHER.twShortcuts.get(String.valueOf((char)keyEvent.getKeyCode()))) != null) {
+            keyEvent.consume();
             SWITCHER.myPopup.closeOk(null);
             tw.activate(null, true, true);
             return true; // because the key event is actually processed
