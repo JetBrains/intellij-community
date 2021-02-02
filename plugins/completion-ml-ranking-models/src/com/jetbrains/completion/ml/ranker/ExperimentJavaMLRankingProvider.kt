715cb61a48ea4c16b2d1519ac68e60f0f318b0f7
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2021-02-02 08:52:19 +0300

    [ml-completion] Add new Java experimental model: v0.4.0
    GitOrigin-RevId: db1b76788ae7d53a5ad4cb93d86f7d29acaaad2f

--- plugins/completion-ml-ranking-models/src/com/jetbrains/completion/ml/ranker/ExperimentJavaMLRankingProvider.kt
+++ plugins/completion-ml-ranking-models/src/com/jetbrains/completion/ml/ranker/ExperimentJavaMLRankingProvider.kt
@@ -1,20 +1,12 @@
 package com.jetbrains.completion.ml.ranker
 
 import com.intellij.completion.ml.ranker.ExperimentModelProvider
-import com.intellij.internal.ml.DecisionFunction
-import com.intellij.internal.ml.ModelMetadata
-import com.intellij.internal.ml.completion.CompletionRankingModelBase
-import com.intellij.internal.ml.completion.JarCompletionModelProvider
+import com.intellij.internal.ml.catboost.CatBoostJarCompletionModelProvider
 import com.intellij.lang.Language
-import com.jetbrains.completion.ranker.model.java.MLGlassBox
 
-class ExperimentJavaMLRankingProvider: JarCompletionModelProvider(
-  CompletionRankingModelsBundle.message("ml.completion.experiment.model.java"), "java_features"), ExperimentModelProvider {
-  override fun createModel(metadata: ModelMetadata): DecisionFunction {
-    return object : CompletionRankingModelBase(metadata) {
-      override fun predict(features: DoubleArray?): Double = MLGlassBox.makePredict(features)
-    }
-  }
+class ExperimentJavaMLRankingProvider : CatBoostJarCompletionModelProvider(
+  CompletionRankingModelsBundle.message("ml.completion.experiment.model.java"), "java_features_exp",
+  "java_model_exp"), ExperimentModelProvider {
 
   override fun isLanguageSupported(language: Language): Boolean = language.id.compareTo("java", ignoreCase = true) == 0
 
