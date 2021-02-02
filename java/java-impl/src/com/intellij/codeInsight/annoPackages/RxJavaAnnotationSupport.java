9ce45b5182c7874e770f04a13c864a2504ec4d69
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2021-02-02 13:36:29 +0700

    [java-impl] RxJavaAnnotationSupport: List.of used
    GitOrigin-RevId: 3879a5314f5680c1563e8587bc36ed770c99f36d

--- java/java-impl/src/com/intellij/codeInsight/annoPackages/RxJavaAnnotationSupport.java
+++ java/java-impl/src/com/intellij/codeInsight/annoPackages/RxJavaAnnotationSupport.java
@@ -2,7 +2,6 @@
 package com.intellij.codeInsight.annoPackages;
 
 import com.intellij.codeInsight.Nullability;
-import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collections;
@@ -14,8 +13,8 @@ class RxJavaAnnotationSupport implements AnnotationPackageSupport {
   private final static String NOTNULL_RXJAVA_3 = "io.reactivex.rxjava3.annotations.NonNull";
   private final static String NULLABLE_RXJAVA_3 = "io.reactivex.rxjava3.annotations.Nullable";
 
-  private static final List<String> NOT_NULLS = ContainerUtil.immutableList(NOTNULL_RXJAVA_2, NOTNULL_RXJAVA_3);
-  private static final List<String> NULLABLES = ContainerUtil.immutableList(NULLABLE_RXJAVA_2, NULLABLE_RXJAVA_3);
+  private static final List<String> NOT_NULLS = List.of(NOTNULL_RXJAVA_2, NOTNULL_RXJAVA_3);
+  private static final List<String> NULLABLES = List.of(NULLABLE_RXJAVA_2, NULLABLE_RXJAVA_3);
 
   @Override
   public @NotNull List<String> getNullabilityAnnotations(@NotNull Nullability nullability) {
