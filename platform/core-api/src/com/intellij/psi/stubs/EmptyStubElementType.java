da2416c95d0c28b52f61751450a92c744a72f30f
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2021-02-03 15:10:04 +0100

    fix compilation
    GitOrigin-RevId: c8fd2dddaae7f868a5e1ed45c7b8b3a653692ad9

--- platform/core-api/src/com/intellij/psi/stubs/EmptyStubElementType.java
+++ platform/core-api/src/com/intellij/psi/stubs/EmptyStubElementType.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.stubs;
 
 import com.intellij.lang.Language;
@@ -7,37 +7,31 @@ import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-/**
- * @author peter
- */
 public abstract class EmptyStubElementType<T extends PsiElement> extends IStubElementType<EmptyStub<?>, T> {
   protected EmptyStubElementType(@NotNull @NonNls String debugName, @Nullable Language language) {
     super(debugName, language);
   }
 
-  @NotNull
   @Override
-  public final EmptyStub<?> createStub(@NotNull T psi, StubElement<?> parentStub) {
+  public final @NotNull EmptyStub<?> createStub(@NotNull T psi, StubElement<? extends PsiElement> parentStub) {
     return createStub(parentStub);
   }
 
-  protected EmptyStub<?> createStub(StubElement parentStub) {
-    return new EmptyStub(parentStub, this);
+  protected EmptyStub<?> createStub(StubElement<?> parentStub) {
+    return new EmptyStub<>(parentStub, this);
   }
 
-  @NotNull
   @Override
-  public String getExternalId() {
-    return getLanguage().getID() + toString();
+  public @NotNull String getExternalId() {
+    return getLanguage().getID() + this;
   }
 
   @Override
   public final void serialize(@NotNull EmptyStub stub, @NotNull StubOutputStream dataStream) {
   }
 
-  @NotNull
   @Override
-  public final EmptyStub deserialize(@NotNull StubInputStream dataStream, StubElement parentStub) {
+  public final @NotNull EmptyStub<?> deserialize(@NotNull StubInputStream dataStream, StubElement parentStub) {
     return createStub(parentStub);
   }
 
