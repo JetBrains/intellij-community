5c907fbad6d8a4b15471de7374d2ff7d472fbdda
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2021-02-03 09:44:17 +0100

    [platform] incorrect custom VM options file name (IDEA-260798)
    GitOrigin-RevId: 5169fe85ff40ea9f369719b6abe2a727e37c93f5

--- platform/platform-impl/src/com/intellij/diagnostic/VMOptions.java
+++ platform/platform-impl/src/com/intellij/diagnostic/VMOptions.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.diagnostic;
 
 import com.intellij.openapi.application.ApplicationNamesInfo;
@@ -215,7 +215,7 @@ public final class VMOptions {
   @NotNull
   public static String getCustomVMOptionsFileName() {
     String fileName = ApplicationNamesInfo.getInstance().getScriptName();
-    if (!SystemInfo.isWindows && CpuArch.isIntel64() || SystemInfo.isXWindow && !CpuArch.is32Bit()) fileName += "64";
+    if (!SystemInfo.isMac && CpuArch.isIntel64()) fileName += "64";
     if (SystemInfo.isWindows) fileName += ".exe";
     fileName += ".vmoptions";
     return fileName;
