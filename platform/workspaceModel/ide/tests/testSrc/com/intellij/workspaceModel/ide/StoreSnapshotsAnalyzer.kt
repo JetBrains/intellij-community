902669cdf961868ed7f0dbf78f2c57affbe92aac
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2021-02-03 13:58:24 +0300

    [workspace model] move StoreSnapshotsAnalyzer to tests
    There is no need to have it in production sources.
    
    GitOrigin-RevId: 7bc89dec9c818c30ed652e0230e0992f1dbffdbb

--- platform/workspaceModel/ide/tests/testSrc/com/intellij/workspaceModel/ide/StoreSnapshotsAnalyzer.kt
+++ platform/workspaceModel/ide/tests/testSrc/com/intellij/workspaceModel/ide/StoreSnapshotsAnalyzer.kt
@@ -1,5 +1,5 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.workspaceModel.ide.impl
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.workspaceModel.ide
 
 import com.intellij.workspaceModel.storage.EntitySource
 import com.intellij.workspaceModel.storage.impl.EntityStorageSerializerImpl
