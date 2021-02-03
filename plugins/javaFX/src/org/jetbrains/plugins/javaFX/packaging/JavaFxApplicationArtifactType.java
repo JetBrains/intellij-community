77d51b9f6e38f74e777bdc258ef3dc606baa6996
Bas Leijdekkers <basleijdekkers@gmail.com>
2021-02-03 17:57:10 +0100

    javafx: warn in project settings when fx:deploy is not available
    GitOrigin-RevId: 1697bd494def322f08fc9b6c00afec831a42fbb8

--- plugins/javaFX/src/org/jetbrains/plugins/javaFX/packaging/JavaFxApplicationArtifactType.java
+++ plugins/javaFX/src/org/jetbrains/plugins/javaFX/packaging/JavaFxApplicationArtifactType.java
@@ -1,40 +1,29 @@
-/*
- * Copyright 2000-2013 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.javaFX.packaging;
 
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.module.JavaModuleType;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleType;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.roots.ui.configuration.ChooseModulesDialog;
 import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.packaging.artifacts.Artifact;
 import com.intellij.packaging.artifacts.ArtifactTemplate;
 import com.intellij.packaging.artifacts.ArtifactType;
-import com.intellij.packaging.elements.CompositePackagingElement;
-import com.intellij.packaging.elements.PackagingElement;
-import com.intellij.packaging.elements.PackagingElementFactory;
-import com.intellij.packaging.elements.PackagingElementOutputKind;
-import com.intellij.packaging.elements.PackagingElementResolvingContext;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import javax.swing.*;
+import com.intellij.packaging.elements.*;
+import com.intellij.packaging.ui.ArtifactProblemsHolder;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.javaFX.JavaFXBundle;
+import org.jetbrains.plugins.javaFX.JavaFXCommonBundle;
+
+import javax.swing.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class JavaFxApplicationArtifactType extends ArtifactType {
   public static JavaFxApplicationArtifactType getInstance() {
@@ -61,6 +50,17 @@ public class JavaFxApplicationArtifactType extends ArtifactType {
   @Override
   public CompositePackagingElement<?> createRootElement(@NotNull String artifactName) {
     return PackagingElementFactory.getInstance().createArtifactRootElement();
+  }
+
+  @Override
+  public void checkRootElement(@NotNull CompositePackagingElement<?> rootElement,
+                               @NotNull Artifact artifact,
+                               @NotNull ArtifactProblemsHolder manager) {
+    final Project project = manager.getContext().getProject();
+    final Sdk sdk = JavaFxArtifactProperties.getFxCompatibleSdk(artifact, project);
+    if (sdk != null && !new File(sdk.getHomePath() + "/lib/ant-javafx.jar").exists()) {
+      manager.registerError(JavaFXCommonBundle.message("cant.build.artifact.fx.deploy.is.not.available.in.this.jdk"), "no-fx:deploy");
+    }
   }
 
   @NotNull
