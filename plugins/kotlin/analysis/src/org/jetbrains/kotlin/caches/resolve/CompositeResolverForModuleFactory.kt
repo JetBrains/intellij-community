225ab35f2c911d50400299814c308585d9f03b85
Victor Petukhov <victor.petukhov@jetbrains.com>
2021-02-04 11:42:52 +0300

    Add reporting of the warnings based on Java annotations for expanded type aliases
    Before that, such warnings weren't reported as the corresponding errors were reported during type inference (only original types took part there)
    
    Original commit: 13a97b56c105b86016d25a6023d554c5db7bdbad
    
    GitOrigin-RevId: 28b0ad223117f454b6a745c3c1114a47c423e498

--- plugins/kotlin/analysis/src/org/jetbrains/kotlin/caches/resolve/CompositeResolverForModuleFactory.kt
+++ plugins/kotlin/analysis/src/org/jetbrains/kotlin/caches/resolve/CompositeResolverForModuleFactory.kt
@@ -221,7 +221,7 @@ class CompositeResolverForModuleFactory(
 }
 
 class CompositeAnalyzerServices(val services: List<PlatformDependentAnalyzerServices>) : PlatformDependentAnalyzerServices() {
-    override val platformConfigurator: PlatformConfigurator = CompositePlatformConigurator(services.map { it.platformConfigurator })
+    override val platformConfigurator: PlatformConfigurator = CompositePlatformConfigurator(services.map { it.platformConfigurator })
 
     override fun computePlatformSpecificDefaultImports(storageManager: StorageManager, result: MutableList<ImportPath>) {
         val intersectionOfDefaultImports = services.map { service ->
@@ -244,7 +244,7 @@ class CompositeAnalyzerServices(val services: List<PlatformDependentAnalyzerServ
         if (isEmpty()) emptyList() else reduce { first, second -> first.intersect(second) }.toList()
 }
 
-class CompositePlatformConigurator(private val componentConfigurators: List<PlatformConfigurator>) : PlatformConfigurator {
+class CompositePlatformConfigurator(private val componentConfigurators: List<PlatformConfigurator>) : PlatformConfigurator {
     override val platformSpecificContainer: StorageComponentContainer
         get() = composeContainer(this::class.java.simpleName) {
             configureDefaultCheckers()
@@ -253,8 +253,8 @@ class CompositePlatformConigurator(private val componentConfigurators: List<Plat
             }
         }
 
-    override fun configureModuleComponents(container: StorageComponentContainer) {
-        componentConfigurators.forEach { it.configureModuleComponents(container) }
+    override fun configureModuleComponents(container: StorageComponentContainer, languageVersionSettings: LanguageVersionSettings) {
+        componentConfigurators.forEach { it.configureModuleComponents(container, languageVersionSettings) }
     }
 
     override fun configureModuleDependentCheckers(container: StorageComponentContainer) {
@@ -263,6 +263,5 @@ class CompositePlatformConigurator(private val componentConfigurators: List<Plat
         // Unfortunately, it is declared in base class, so repeating call to 'configureModuleDependentCheckers' will lead
         // to multiple registrrations.
         container.useImpl<ExperimentalMarkerDeclarationAnnotationChecker>()
-        container.useImpl<UpperBoundChecker>()
     }
 }
