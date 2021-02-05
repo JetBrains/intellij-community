6c25fe1f25390f4fbea82bb5f638cdca0609f656
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-02-05 15:09:52 +0100

    require `equals` and `hashCode` in `Symbol` and `SymbolResolveResult`
    GitOrigin-RevId: 610326b9731bd3421b063ae0575789b6d43e45cf

--- platform/core-api/src/com/intellij/model/SymbolResolveResult.java
+++ platform/core-api/src/com/intellij/model/SymbolResolveResult.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.model;
 
 import org.jetbrains.annotations.NotNull;
@@ -11,11 +11,13 @@ public interface SymbolResolveResult {
   /**
    * @return referenced Symbol
    */
-  @NotNull
-  Symbol getTarget();
+  @NotNull Symbol getTarget();
 
-  @NotNull
-  static SymbolResolveResult fromSymbol(@NotNull Symbol symbol) {
-    return () -> symbol;
+  boolean equals(Object obj);
+
+  int hashCode();
+
+  static @NotNull SymbolResolveResult fromSymbol(@NotNull Symbol symbol) {
+    return new SymbolResolveResultImpl(symbol);
   }
 }
