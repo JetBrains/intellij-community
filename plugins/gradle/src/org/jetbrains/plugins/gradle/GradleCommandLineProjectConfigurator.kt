81cda258070d7cc6bcb0b68db85ffe3190c19ce4
alexey.afanasiev <alexey.afanasiev@jetbrains.com>
2021-02-08 01:33:32 +0300

    Qodana inspections: refactor gradle logging
    GitOrigin-RevId: 13c781a271248ff37b17284392fa823a4ec3ba2d

--- plugins/gradle/src/org/jetbrains/plugins/gradle/GradleCommandLineProjectConfigurator.kt
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/GradleCommandLineProjectConfigurator.kt
@@ -1,8 +1,9 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.gradle
 
 import com.intellij.ide.CommandLineInspectionProjectConfigurator
 import com.intellij.ide.CommandLineInspectionProjectConfigurator.ConfiguratorContext
+import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.externalSystem.importing.ImportSpecBuilder
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskId
@@ -20,12 +21,14 @@ import org.jetbrains.plugins.gradle.settings.GradleImportHintService
 import org.jetbrains.plugins.gradle.settings.GradleSettings
 import org.jetbrains.plugins.gradle.util.GradleBundle
 import org.jetbrains.plugins.gradle.util.GradleConstants
+import java.io.BufferedWriter
 import java.io.File
-import java.lang.Exception
+import java.io.FileWriter
+import java.util.concurrent.atomic.AtomicReference
 
 private val LOG = Logger.getInstance(GradleCommandLineProjectConfigurator::class.java)
 
-private val GRADLE_OUTPUT_LOG = Logger.getInstance("GradleOutput")
+private val gradleLogWriter = BufferedWriter(FileWriter(PathManager.getLogPath() + "/gradle-import.log"))
 
 class GradleCommandLineProjectConfigurator : CommandLineInspectionProjectConfigurator {
   override fun getName() = "gradle"
@@ -41,8 +44,9 @@ class GradleCommandLineProjectConfigurator : CommandLineInspectionProjectConfigu
     val state = GradleImportHintService.getInstance(project).state
 
     if (state.skip) return
+    val finishStatus = AtomicReference(FinishStatus.INITIAL)
     val progressManager = ExternalSystemProgressNotificationManager.getInstance()
-    progressManager.addNotificationListener(TaskNotificationListener())
+    progressManager.addNotificationListener(TaskNotificationListener(finishStatus))
 
     if (state.projectsToImport.isNotEmpty()) {
       for (projectPath in state.projectsToImport) {
@@ -68,39 +72,58 @@ class GradleCommandLineProjectConfigurator : CommandLineInspectionProjectConfigu
     if (FileUtil.findFirstThatExist(gradleGroovyDslFile, kotlinDslGradleFile) == null) return
 
     refreshProject(basePath, getImportSpecBuilder(project))
+    val status = finishStatus.get()
+    if (status != FinishStatus.SUCCESS && status != FinishStatus.INITIAL) {
+      throw IllegalStateException("Gradle project import failure. Project import status: $status")
+    }
   }
 
   private fun getImportSpecBuilder(project: Project): ImportSpecBuilder =
     ImportSpecBuilder(project, GradleConstants.SYSTEM_ID).use(MODAL_SYNC)
 
-  class TaskNotificationListener : ExternalSystemTaskNotificationListener {
+  class TaskNotificationListener(private val finishStatus: AtomicReference<FinishStatus>) : ExternalSystemTaskNotificationListener {
     override fun onSuccess(id: ExternalSystemTaskId) {
+      finishStatus.set(FinishStatus.SUCCESS)
+      gradleLogWriter.close()
       LOG.info("Gradle import success")
     }
 
     override fun onFailure(id: ExternalSystemTaskId, e: Exception) {
+      finishStatus.set(FinishStatus.FAILURE)
+      gradleLogWriter.close()
       LOG.error("Gradle import failure", e)
     }
 
     override fun onTaskOutput(id: ExternalSystemTaskId, text: String, stdOut: Boolean) {
-      GRADLE_OUTPUT_LOG.debug("[Gradle ${if (stdOut) "STDOUT" else "STDERR" } ] $text")
+      gradleLogWriter.write((if (stdOut) "" else "STDERR: ") + text)
     }
 
     override fun onStatusChange(event: ExternalSystemTaskNotificationEvent) {
     }
 
     override fun onCancel(id: ExternalSystemTaskId) {
-      LOG.warn("Gradle import canceled")
+      finishStatus.set(FinishStatus.CANCELLED)
+      LOG.error("Gradle import canceled")
     }
 
     override fun onEnd(id: ExternalSystemTaskId) {
+      gradleLogWriter.close()
     }
 
     override fun beforeCancel(id: ExternalSystemTaskId) {
     }
 
     override fun onStart(id: ExternalSystemTaskId) {
+      finishStatus.set(FinishStatus.STARTED)
       LOG.info("Gradle import started")
     }
+  }
+
+  enum class FinishStatus {
+    INITIAL,
+    STARTED,
+    CANCELLED,
+    FAILURE,
+    SUCCESS
   }
 }
