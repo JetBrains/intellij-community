edd6d9b1977b4291319aaf59f5f7ac81102603da
Roman Ivanov <roman.ivanov@jetbrains.com>
2021-02-07 19:59:20 +0100

    Inlay hints: add default colors for inlays: IDEA-242414
    GitOrigin-RevId: 1ebe11818469b02982b4e476d7d6274920c73fe6

--- platform/editor-ui-api/src/com/intellij/openapi/editor/DefaultLanguageHighlighterColors.java
+++ platform/editor-ui-api/src/com/intellij/openapi/editor/DefaultLanguageHighlighterColors.java
@@ -63,6 +63,8 @@ public final class DefaultLanguageHighlighterColors {
   public static final TextAttributesKey INLINE_PARAMETER_HINT = TextAttributesKey.createTextAttributesKey("INLINE_PARAMETER_HINT");
   public static final TextAttributesKey INLINE_PARAMETER_HINT_HIGHLIGHTED = TextAttributesKey.createTextAttributesKey("INLINE_PARAMETER_HINT_HIGHLIGHTED");
   public static final TextAttributesKey INLINE_PARAMETER_HINT_CURRENT = TextAttributesKey.createTextAttributesKey("INLINE_PARAMETER_HINT_CURRENT");
+  public static final TextAttributesKey INLAY_DEFAULT = TextAttributesKey.createTextAttributesKey("INLAY_DEFAULT");
+  public static final TextAttributesKey INLAY_TEXT_WITHOUT_BACKGROUND = TextAttributesKey.createTextAttributesKey("INLAY_TEXT_WITHOUT_BACKGROUND");
   public static final ColorKey INLINE_REFACTORING_SETTINGS_DEFAULT = ColorKey.createColorKey("INLINE_REFACTORING_SETTINGS_DEFAULT");
   public static final ColorKey INLINE_REFACTORING_SETTINGS_FOCUSED = ColorKey.createColorKey("INLINE_REFACTORING_SETTINGS_FOCUSED");
   public static final ColorKey INLINE_REFACTORING_SETTINGS_HOVERED = ColorKey.createColorKey("INLINE_REFACTORING_SETTINGS_HOVERED");
