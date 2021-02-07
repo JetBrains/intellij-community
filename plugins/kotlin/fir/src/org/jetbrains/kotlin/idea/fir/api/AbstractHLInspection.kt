5da597a842cf4021001c2420033efc2c20db2eeb
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-02-07 18:34:52 +0100

    FIR IDE: add more KDoc to HLApplicator stuff
    Original commit: cf3defbc9c5e202a714bc048fd2fad0a6f5bb992
    
    GitOrigin-RevId: ec15a90321f4ec771d5677d7d79bfb884ce27774

--- plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/fir/api/AbstractHLInspection.kt
+++ plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/fir/api/AbstractHLInspection.kt
@@ -81,6 +81,7 @@ abstract class AbstractHLInspection<PSI : KtElement, INPUT : HLApplicatorInput>(
         }
     }
 
+
     abstract val presentation: HLPresentation<PSI>
     abstract val applicabilityRange: HLApplicabilityRange<PSI>
     abstract val inputProvider: HLApplicatorInputProvider<PSI, INPUT>
@@ -93,7 +94,6 @@ private fun <PSI : PsiElement, INPUT : HLApplicatorInput> HLApplicator<PSI, INPU
 ): LocalQuickFix = object : LocalQuickFix {
     override fun startInWriteAction() = false
 
-    @OptIn(HackToForceAllowRunningAnalyzeOnEDT::class)
     override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
         @Suppress("UNCHECKED_CAST")
         val element = descriptor.psiElement as PSI
