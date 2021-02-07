b3d370dee2c48b54296b81456bb711a283db7cdf
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-02-07 12:39:11 +0100

    FIR IDE: introduce infrastructure for HL based inspections & intentions
    Original commit: a6f76399e24c27785df3b963d73e8fa3186f90ca
    
    GitOrigin-RevId: 75d774073253a94796c848ce032b1751010b8add

--- plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/fir/api/fixes/KtQuickFixService.kt
+++ plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/fir/api/fixes/KtQuickFixService.kt
@@ -1,15 +1,11 @@
-/*
- * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
- * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
- */
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
-package org.jetbrains.kotlin.idea.quickfix
+package org.jetbrains.kotlin.idea.fir.api.fixes
 
 import com.intellij.codeInsight.intention.IntentionAction
 import com.intellij.openapi.extensions.ExtensionPointName
 import org.jetbrains.kotlin.idea.frontend.api.KtAnalysisSession
 import org.jetbrains.kotlin.idea.frontend.api.diagnostics.KtDiagnosticWithPsi
-import org.jetbrains.kotlin.idea.frontend.api.fir.diagnostics.KtFirDiagnostic
 
 class KtQuickFixService {
     private val list = KtQuickFixesList.createCombined(KtQuickFixRegistrar.allQuickFixesList())
