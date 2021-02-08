0fa4300072356b1a037b9c1a839dee8a91fbf897
Evgeniy.Stepanov <Evgeniy.Stepanov@JetBrains.com>
2021-02-08 16:34:59 +0100

    Fix project leak in title providers
    (cherry picked from commit fce89d4d326cbcf843656938a8c4bf456df88091)
    
    IJ-MR-5715
    
    GitOrigin-RevId: 7526871a102bf63191c5244cbd5ac96cb1174877

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/TitleInfoProvider.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/TitleInfoProvider.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.wm.impl
 
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.project.Project
@@ -39,5 +40,5 @@ interface TitleInfoProvider {
   val borderlessSuffix: String
   val borderlessPrefix: String
 
-  fun addUpdateListener(project: Project, value: (provider: TitleInfoProvider) -> Unit)
+  fun addUpdateListener(project: Project, disp: Disposable, value: (provider: TitleInfoProvider) -> Unit)
 }
