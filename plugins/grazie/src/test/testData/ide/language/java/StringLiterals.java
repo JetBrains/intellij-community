38a5e117337887a8a0bce4fb2673db3ead4bdca9
Pavel Bakhvalov <pavel.bakhvalov@jetbrains.com>
2021-02-09 17:58:04 +0300

    [Grazie] Disable MISSING_ARTICLE rule
    (cherry picked from commit c580a910dd7eed4e9ac10ab936daf9e29b51f37f)
    
    IJ-CR-5752
    
    GitOrigin-RevId: 5551954d6a44de85d96c76621e2b6db4e9e6bb8a

--- plugins/grazie/src/test/testData/ide/language/java/StringLiterals.java
+++ plugins/grazie/src/test/testData/ide/language/java/StringLiterals.java
@@ -1,15 +1,15 @@
 class Main {
     public static void main(String[] args) {
-        String oneTypo = "It is <warning descr="ARTICLE_MISSING">friend</warning> of human";
+        String oneTypo = "It is <warning descr="BEEN_PART_AGREEMENT">friend</warning> of human";
         String oneSpellcheckTypo = "It is <TYPO descr="Typo: In word 'frend'">frend</TYPO> of human";
         String fewTypos = "It <warning descr="IT_VBZ">are</warning> working for <warning descr="MUCH_COUNTABLE">much</warning> warnings";
         String ignoreTemplate = "It is ${1} friend";
-        String notIgnoreOtherMistakes = "It is <warning descr="ARTICLE_MISSING">friend</warning>. <warning descr="And">But</warning> I have a ${1} here";
+        String notIgnoreOtherMistakes = "It is friend. <warning descr="And">But</warning> I have a ${1} here";
 
-        System.out.println("It is <warning descr="ARTICLE_MISSING">friend</warning> of human");
+        System.out.println("It is <warning descr="BEEN_PART_AGREEMENT">friend</warning> of human");
         System.out.println("It is <TYPO descr="Typo: In word 'frend'">frend</TYPO> of human");
         System.out.println("It <warning descr="IT_VBZ">are</warning> working for <warning descr="MUCH_COUNTABLE">much</warning> warnings");
         System.out.println("It is ${1} friend");
-        System.out.println("It is <warning descr="ARTICLE_MISSING">friend</warning>. <warning descr="And">But</warning> I have a ${1} here");
+        System.out.println("It is friend. <warning descr="And">But</warning> I have a ${1} here");
     }
 }
