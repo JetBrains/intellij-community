204671ce5ce160e5d01e87c3804efa0679aaaa8c
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2021-02-09 16:35:24 +0300

    read bytes to detect file types directly if called on edt
    GitOrigin-RevId: 47b2f7a9b6a549da5dcffbb99a7da59e21afbe80

--- platform/platform-impl/src/com/intellij/openapi/fileTypes/impl/FileTypeDetectionService.java
+++ platform/platform-impl/src/com/intellij/openapi/fileTypes/impl/FileTypeDetectionService.java
@@ -591,7 +591,7 @@ final class FileTypeDetectionService implements Disposable {
     if (content == null) {
       int bufferLength = getDetectFileBufferSize(file);
       try {
-        return ProgressManager.getInstance().isInNonCancelableSection()
+        return ProgressManager.getInstance().isInNonCancelableSection() && ApplicationManager.getApplication().isWriteThread()
                ? readFirstBytesFromFile(file, bufferLength)
                : myReadFirstBytesFromFileRelay.accessDiskWithCheckCanceled(Pair.create(file, bufferLength));
       }
