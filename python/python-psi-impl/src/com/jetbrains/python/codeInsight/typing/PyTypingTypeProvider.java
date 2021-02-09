12bedabe965e7e42e68abd9b143d727c23f03c1e
Danny Weinberg <fuegofro@gmail.com>
2021-02-09 15:50:25 +0700

    [PATCH] PY-19917 Don't annotate type: ignore[error-code] syntax
    From: Danny Weinberg <FuegoFro@gmail.com>
    Date: Sun, 7 Feb 2021 10:52:42 -0800
    
    close #1510
    
    GitOrigin-RevId: 509846789ffe75479d9e9e7b7dd326d743436269

--- python/python-psi-impl/src/com/jetbrains/python/codeInsight/typing/PyTypingTypeProvider.java
+++ python/python-psi-impl/src/com/jetbrains/python/codeInsight/typing/PyTypingTypeProvider.java
@@ -101,7 +101,7 @@ public class PyTypingTypeProvider extends PyTypeProviderBase {
   private static final String PY3_TEXT_FILE_TYPE = "typing.TextIO";
 
   private static final Pattern TYPE_COMMENT_PATTERN = Pattern.compile("# *type: *([^#]+) *(#.*)?");
-  public static final String IGNORE = "ignore";
+  public static final Pattern IGNORE_PATTERN = Pattern.compile("^ignore(\\[ *[^ ,\\]]+ *(, *[^ ,\\]]+ *)*\\])?$");
 
   public static final ImmutableMap<String, String> BUILTIN_COLLECTION_CLASSES = ImmutableMap.<String, String>builder()
     .put(LIST, "list")
