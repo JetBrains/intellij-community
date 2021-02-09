53b03572fd7f33f8532a5d241f0b0a54d793aa94
Anastasia Ivanova <anastasia.ivanova@jetbrains.com>
2021-02-09 15:00:18 +0100

    FUS: delete validator module
    GitOrigin-RevId: f090b51df8a3190d6371b9eb3b02e684ce277e1c

--- platform/statistics/validator/src/com/intellij/internal/statistic/eventLog/validator/rules/FUSRegexpAwareRule.java
+++
@@ -1,9 +0,0 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.internal.statistic.eventLog.validator.rules;
-
-import org.jetbrains.annotations.NotNull;
-
-public interface FUSRegexpAwareRule extends FUSRule {
-    @NotNull
-    String asRegexp();
-}
