25062aff34f387663cff5bd8a9134aa872813c46
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2021-02-09 21:42:10 +0300

    Do not replace mnemonic ampersand if mnemonic character is already added
    GitOrigin-RevId: 8ae27163d906c1beb95d383ccec2ab0c6732e262

--- platform/util/src/com/intellij/BundleBase.java
+++ platform/util/src/com/intellij/BundleBase.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij;
 
 import com.intellij.openapi.diagnostic.Logger;
@@ -192,7 +192,7 @@ public abstract class BundleBase {
 
   @Contract("null -> null; !null -> !null")
   public static @Nls String replaceMnemonicAmpersand(@Nullable @Nls String value) {
-    if (value == null || value.indexOf('&') < 0) {
+    if (value == null || value.indexOf('&') < 0 || value.indexOf(MNEMONIC) >= 0) {
       return value;
     }
 
