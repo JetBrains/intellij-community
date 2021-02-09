c1353fac3f823bcd949a8b25948f4b37455f0842
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2021-02-09 19:11:17 +0300

    [wizard, Kotlin/JS] Fix of test data for common css support
    ^KT-44285 fixed
    ^KT-44848 fixed
    
    GitOrigin-RevId: 3a45699eab3c9d1b28cd8fdf518cc7712782b54c

--- plugins/kotlin/project-wizard/cli/testData/projectTemplatesBuildFileGeneration/fullStackWebApplication/build.gradle
+++ plugins/kotlin/project-wizard/cli/testData/projectTemplatesBuildFileGeneration/fullStackWebApplication/build.gradle
@@ -27,19 +27,10 @@ kotlin {
         withJava()
     }
     js(LEGACY) {
+        binaries.executable()
         browser {
-            binaries.executable()
-            webpackTask {
+            commonWebpackConfig {
                 cssSupport.enabled = true
-            }
-            runTask {
-                cssSupport.enabled = true
-            }
-            testTask {
-                useKarma {
-                    useChromeHeadless()
-                    webpackConfig.cssSupport.enabled = true
-                }
             }
         }
     }
