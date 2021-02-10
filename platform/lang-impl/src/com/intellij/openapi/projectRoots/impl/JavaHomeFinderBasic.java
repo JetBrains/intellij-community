e70a9b6e6dbec89dd934b61878f90cb7f6d29846
Carlos Ramirez <crramirez@gmail.com>
2021-02-10 17:34:32 +0100

    If we are looking for Java Home in WSL, look for .sdkman/candidates inside the distribution
    ... even if we are running the IDE in Windows
    
    GitOrigin-RevId: 76661c08b1e2a266ee1f0388258b31bb59cc71a6

--- platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/JavaHomeFinderBasic.java
+++ platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/JavaHomeFinderBasic.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.projectRoots.impl;
 
 import com.intellij.openapi.application.ApplicationManager;
@@ -230,7 +230,7 @@ public class JavaHomeFinderBasic {
     }
 
     // finally, try the usual location in UNIX
-    if (!SystemInfo.isWindows) {
+    if (!SystemInfo.isWindows || this instanceof JavaHomeFinderWsl) {
       Path candidates = getPathInUserHome(".sdkman/candidates");
       if (candidates != null && isDirectory(candidates)) {
         return candidates;
