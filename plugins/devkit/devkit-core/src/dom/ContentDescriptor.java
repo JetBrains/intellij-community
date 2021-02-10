88a93ddd0280a76e268093a887f69cd5c9ab86d3
Yann Cébron <yann.cebron@jetbrains.com>
2021-02-10 17:07:15 +0100

    IDEA-261707 Devkit: plugin.xml code insight/highlighting for <content> and <dependencies> - extract DOM
    GitOrigin-RevId: 90ab75fb2b1be29438d529f76ad53aba0a78a696

---
+++ plugins/devkit/devkit-core/src/dom/ContentDescriptor.java
@@ -0,0 +1,38 @@
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.idea.devkit.dom;
+
+import com.intellij.ide.presentation.Presentation;
+import com.intellij.psi.PsiPackage;
+import com.intellij.util.xml.*;
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+@ApiStatus.Experimental
+public interface ContentDescriptor extends DomElement {
+
+  @NotNull
+  @Stubbed
+  @SubTagList("module")
+  List<DependencyDescriptor.ModuleDescriptor> getModuleEntry();
+
+  @SubTagList("module")
+  DependencyDescriptor.ModuleDescriptor addModuleEntry();
+
+  @Presentation(icon = "AllIcons.Nodes.Module")
+  interface ModuleDescriptor extends DomElement {
+    @NotNull
+    @Required
+    @Stubbed
+    @NameValue
+    GenericAttributeValue<String> getName();
+
+    @NotNull
+    @Required
+    @Stubbed
+    @NameValue
+    @Convert(PsiPackageConverter.class)
+    GenericAttributeValue<PsiPackage> getPackage();
+  }
+}
