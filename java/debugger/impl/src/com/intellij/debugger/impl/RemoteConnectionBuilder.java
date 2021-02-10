4ea323de5866ff130167eb2bfde38e1e8e064d0c
Egor Ushakov <egor.ushakov@jetbrains.com>
2021-02-10 21:03:10 +0300

    [debugger] use project for memory agent
    GitOrigin-RevId: c7d308c80690e8360b4d7dc1b0c62fa9b1597495

--- java/debugger/impl/src/com/intellij/debugger/impl/RemoteConnectionBuilder.java
+++ java/debugger/impl/src/com/intellij/debugger/impl/RemoteConnectionBuilder.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.debugger.impl;
 
 import com.intellij.debugger.JavaDebuggerBundle;
@@ -136,7 +136,7 @@ public class RemoteConnectionBuilder {
       }
 
       if (myMemoryAgent) {
-        MemoryAgentUtil.addMemoryAgent(parameters);
+        MemoryAgentUtil.addMemoryAgent(parameters, myProject);
       }
 
       final Sdk jdk = parameters.getJdk();
