e44490f1392fae80f9bf530f44be33962f1a2027
Konstantin Bulenkov <kb@jetbrains.com>
2021-02-11 12:02:25 +0100

    IDEA-260690 Built-in html preview: Add icon to Preview tab
    (cherry picked from commit e9b79eb44565b0d5e4587c339e8274e5f7422e9a)
    
    GitOrigin-RevId: 12ad10bf51451a5ee0eacf19ecd07678dd385ce9

--- platform/platform-impl/src/com/intellij/ide/browsers/actions/WebPreviewFileType.java
+++ platform/platform-impl/src/com/intellij/ide/browsers/actions/WebPreviewFileType.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.browsers.actions;
 
+import com.intellij.icons.AllIcons;
 import com.intellij.ide.IdeBundle;
 import com.intellij.openapi.fileTypes.ex.FakeFileType;
 import com.intellij.openapi.util.NlsContexts;
@@ -8,6 +9,8 @@ import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
 
 /**
  * @author Konstantin Bulenkov
@@ -36,5 +39,10 @@ public class WebPreviewFileType extends FakeFileType {
   @Override
   public boolean isMyFileType(@NotNull VirtualFile file) {
     return file instanceof WebPreviewVirtualFile;
+  }
+
+  @Override
+  public Icon getIcon() {
+    return AllIcons.General.Web;
   }
 }
