c5bcd0d1172b4f3396bc36a6bc7cabbf9684dbca
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2021-02-11 14:25:04 +0300

    mark new project as trusted after project initialization
    No services should be requested before initProject (otherwise "com.intellij.openapi.components.PathMacroManager is registered as application service, but requested as project one" will happen)
    
    Follow-up: febf529f, 3cea8db2
    
    GitOrigin-RevId: 3a9cd91018c9861aa826ce3e4e24abe025a150bc

--- platform/platform-impl/src/com/intellij/openapi/project/impl/ProjectManagerExImpl.kt
+++ platform/platform-impl/src/com/intellij/openapi/project/impl/ProjectManagerExImpl.kt
@@ -194,6 +194,7 @@ open class ProjectManagerExImpl : ProjectManagerImpl() {
       val template = if (options.useDefaultProjectAsTemplate) defaultProject else null
       initProject(projectFile, project, options.isRefreshVfsNeeded, options.preloadServices, template,
                   ProgressManager.getInstance().progressIndicator)
+      project.setTrusted(true)
       return project
     }
     catch (t: Throwable) {
