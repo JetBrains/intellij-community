16dd128d9f2d1476e3a801d0b2ce42dc4c5665ab
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-02-11 21:17:07 +0100

    IDEA-261695 set accessible name for left-right renderer
    GitOrigin-RevId: d9d5f2bef84781d2c5d123d7ce9ac978555b1e81

--- platform/platform-api/src/com/intellij/ui/list/LeftRightRenderer.kt
+++ platform/platform-api/src/com/intellij/ui/list/LeftRightRenderer.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ui.list
 
 import com.intellij.util.ui.JBUI
@@ -42,6 +42,10 @@ abstract class LeftRightRenderer<T> : ListCellRenderer<T> {
       add(mainComponent, BorderLayout.WEST)
       add(spacer, BorderLayout.CENTER)
       add(rightComponent, BorderLayout.EAST)
+      accessibleContext.accessibleName = listOfNotNull(
+        mainComponent.accessibleContext?.accessibleName,
+        rightComponent.accessibleContext?.accessibleName
+      ).joinToString(separator = " ")
     }
     return component
   }
