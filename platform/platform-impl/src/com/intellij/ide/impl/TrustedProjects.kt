97c4c4e4748302a62e0e35683b305653861c8430
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2021-02-11 15:35:41 +0300

    Use a better storage format for the "project is trusted" property
    Follow-up: 3cea8db2
    (cherry picked from commit 9aee06e530d67b5909531ecd9ffb309280b3b999)
    
    GitOrigin-RevId: fb78a091a13635e1b95b9a3a7a98c732542d29fc

--- platform/platform-impl/src/com/intellij/ide/impl/TrustedProjects.kt
+++ platform/platform-impl/src/com/intellij/ide/impl/TrustedProjects.kt
@@ -11,7 +11,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.MessageDialogBuilder
 import com.intellij.openapi.ui.Messages
 import com.intellij.util.ThreeState
-import com.intellij.util.xmlb.annotations.OptionTag
+import com.intellij.util.xmlb.annotations.Attribute
 import org.jetbrains.annotations.Nls
 
 fun confirmOpeningUntrustedProject(name: @Nls String): OpenUntrustedProjectChoice {
@@ -69,13 +69,13 @@ fun Project.setTrusted(value: Boolean) {
 class TrustedProjectSettings : SimplePersistentStateComponent<TrustedProjectSettings.State>(State()) {
 
   class State : BaseState() {
-    @get:OptionTag("TRUSTED")
-    var trustedState by enum(ThreeState.UNSURE)
+    @get:Attribute
+    var isTrusted by enum(ThreeState.UNSURE)
   }
 
   var trustedState: ThreeState
-    get() = state.trustedState
+    get() = state.isTrusted
     set(value) {
-      state.trustedState = value
+      state.isTrusted = value
     }
 }
