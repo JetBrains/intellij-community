d8e57214a98c485a491e123e61157343c89662a1
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2021-02-11 11:02:43 +0700

    [java-dfa] Add test from IDEA-254146
    GitOrigin-RevId: c84a1ee4e1c41b5c1ea9e37c539704b65308835f

--- java/java-tests/testData/inspection/dataFlow/fixture/DontWidenPlusInLoop.java
+++ java/java-tests/testData/inspection/dataFlow/fixture/DontWidenPlusInLoop.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class DontWidenPlusInLoop {
   public static int parse(String value, int i) {
     do {
@@ -11,5 +13,17 @@ public class DontWidenPlusInLoop {
     }
     while (<warning descr="Condition 'i != -1' is always 'true'">i != -1</warning>);
     return i;
+  }
+
+  void test(List<String> list) {
+    List<String> result = null;
+    int count = 0;
+    for (String s : list) {
+      count++;
+      if (count == 1) {
+        result = new ArrayList<>();
+      }
+      result.add(s); // No possible NPE!
+    }
   }
 }
