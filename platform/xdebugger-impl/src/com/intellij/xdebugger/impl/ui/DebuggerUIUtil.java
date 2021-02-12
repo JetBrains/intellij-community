ed512a1c37212903a0ef274b26ca5cd85d68a0fc
Egor Ushakov <egor.ushakov@jetbrains.com>
2021-02-12 11:59:06 +0300

    EA-250607 - REWA: ApplicationImpl.throwThreadAccessException
    GitOrigin-RevId: cce3141f8ea6b1e4f2eead278be23bb1b12cf7d5

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/ui/DebuggerUIUtil.java
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/ui/DebuggerUIUtil.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.xdebugger.impl.ui;
 
 import com.intellij.codeInsight.hint.HintUtil;
@@ -459,9 +459,11 @@ public final class DebuggerUIUtil {
     modifier.setValue(text, new XValueModifier.XModificationCallback() {
       @Override
       public void valueModified() {
-        if (tree.isDetached()) {
-          AppUIUtil.invokeOnEdt(() -> tree.rebuildAndRestore(treeState));
-        }
+        AppUIUtil.invokeOnEdt(() -> {
+          if (tree.isDetached()) {
+            tree.rebuildAndRestore(treeState);
+          }
+        });
         XDebuggerUtilImpl.rebuildAllSessionsViews(project);
       }
 
