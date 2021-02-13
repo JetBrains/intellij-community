16c52e8087654c76295dee02a4bed8ec5874e617
Michail Plushnikov <michail.plushnikov@jetbrains.com>
2021-02-13 11:59:34 +0100

    [lombok] Fixed ignoring @Tolerate methods for @With processing
    merged PR(#1001) and corresponding changes from github
    updated Readme.md
    
    GitOrigin-RevId: 69e65839eb6f83becc8a6d05a02b7350353b7a6b

--- plugins/lombok/src/test/java/de/plushnikov/intellij/plugin/processor/TolerateTest.java
+++ plugins/lombok/src/test/java/de/plushnikov/intellij/plugin/processor/TolerateTest.java
@@ -2,14 +2,16 @@ package de.plushnikov.intellij.plugin.processor;
 
 import de.plushnikov.intellij.plugin.AbstractLombokParsingTestCase;
 
-import java.io.IOException;
-
 /**
  * Unit tests for IntelliJPlugin for Lombok, based on lombok test classes
  */
 public class TolerateTest extends AbstractLombokParsingTestCase {
 
-  public void testTolerateTest() throws IOException {
-    doTest();
+  public void testTolerate$TolerateTest() {
+    doTest(true);
+  }
+
+  public void testTolerate$WitherTolerateTest() {
+    doTest(true);
   }
 }
