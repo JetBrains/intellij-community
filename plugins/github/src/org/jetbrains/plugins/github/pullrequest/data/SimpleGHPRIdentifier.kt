09e280d77981ab3559c0266c02829197904ec088
Ivan Semenov <ivan.semenov@jetbrains.com>
2021-02-14 21:13:41 +0300

    [github] fix PR identifier comparison
    GitOrigin-RevId: b52ee302f07304b5007c80cb5206f2f78418353e

--- plugins/github/src/org/jetbrains/plugins/github/pullrequest/data/SimpleGHPRIdentifier.kt
+++ plugins/github/src/org/jetbrains/plugins/github/pullrequest/data/SimpleGHPRIdentifier.kt
@@ -1,6 +1,8 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.github.pullrequest.data
 
-data class SimpleGHPRIdentifier(override val id: String, override val number: Long) : GHPRIdentifier {
+import org.jetbrains.plugins.github.api.data.GHNode
+
+class SimpleGHPRIdentifier(id: String, override val number: Long) : GHNode(id), GHPRIdentifier {
   constructor(id: GHPRIdentifier) : this(id.id, id.number)
 }
