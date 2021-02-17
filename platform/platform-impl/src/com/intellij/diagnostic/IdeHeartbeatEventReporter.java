93b9a0e54acee7621e3ec7fdd9d6a8383441f4c5
Svetlana.Zemlyanskaya <Svetlana.Zemlyanskaya@jetbrains.com>
2021-02-17 17:32:57 +0100

    FUS: record heartbeat events from regular thread (IDEA-259780)
    (cherry picked from commit 5489a326fca769650b79c17add68b54850cb70f7)
    
    IJ-CR-6048
    
    GitOrigin-RevId: ca7960ab0def71e0bd87075a4d1ea880648dd63b

--- platform/platform-impl/src/com/intellij/diagnostic/IdeHeartbeatEventReporter.java
+++ platform/platform-impl/src/com/intellij/diagnostic/IdeHeartbeatEventReporter.java
@@ -5,17 +5,25 @@ import com.intellij.featureStatistics.fusCollectors.LifecycleUsageTriggerCollect
 import com.intellij.internal.DebugAttachDetector;
 import com.intellij.internal.statistic.eventLog.EventLogGroup;
 import com.intellij.internal.statistic.eventLog.events.EventFields;
+import com.intellij.internal.statistic.eventLog.events.EventId;
 import com.intellij.internal.statistic.eventLog.events.EventId1;
 import com.intellij.internal.statistic.service.fus.collectors.CounterUsagesCollector;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.util.concurrency.AppExecutorUtil;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 final class IdeHeartbeatEventReporter implements Disposable {
   private static final int UI_RESPONSE_LOGGING_INTERVAL_MS = 100_000;
   private static final int TOLERABLE_UI_LATENCY = 100;
+
+  private final ScheduledExecutorService myExecutor = AppExecutorUtil.createBoundedScheduledExecutorService("IDE Heartbeat", 1);
+  private final ScheduledFuture<?> myThread;
 
   private volatile long myPreviousLoggedUIResponse = 0;
 
@@ -42,17 +50,29 @@ final class IdeHeartbeatEventReporter implements Disposable {
           }
         }
       });
+
+    myThread = myExecutor.scheduleWithFixedDelay(
+      IdeHeartbeatEventReporter::recordHeartbeat,
+      UI_RESPONSE_LOGGING_INTERVAL_MS, UI_RESPONSE_LOGGING_INTERVAL_MS, TimeUnit.MILLISECONDS
+    );
   }
 
+  private static void recordHeartbeat() {
+    UILatencyLogger.HEARTBEAT.log();
+  }
 
   @Override
   public void dispose() {
-
+    if (myThread != null) {
+      myThread.cancel(true);
+    }
+    myExecutor.shutdownNow();
   }
 
   public static final class UILatencyLogger extends CounterUsagesCollector {
-    private static final EventLogGroup GROUP = new EventLogGroup("performance", 57);
+    private static final EventLogGroup GROUP = new EventLogGroup("performance", 58);
 
+    private static final EventId HEARTBEAT = GROUP.registerEvent("heartbeat");
     private static final EventId1<Long> LATENCY = GROUP.registerEvent("ui.latency", EventFields.DurationMs);
     private static final EventId1<Long> LAGGING = GROUP.registerEvent("ui.lagging", EventFields.DurationMs);
 
