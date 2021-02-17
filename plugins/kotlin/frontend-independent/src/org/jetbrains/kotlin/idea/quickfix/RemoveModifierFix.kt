5ee3659878421bdee6b05328471a0a58d98dac2a
Tianyu Geng <tgeng@google.com>
2021-02-17 15:29:57 -0800

    FIR IDE: Simplify registerPsiQuickFixes
    This change makes it possible to register multiple fixes for a
    diagnostic. Also, the previous registerPsiQuickFix that relies compiler
    to infer the KtFirDiagnostic type parameter is dangerous since it
    can silently register fixes on interface `KtDiagnosticWithPsi` if caller
    doesn't specify it explicitly.
    
    Original commit: 51da54ce669ae5ad0024ffc4ec329888dd8ff23b
    
    GitOrigin-RevId: 68c219c7b5a36d1d34620d63063dc66d417df69f

--- plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/quickfix/RemoveModifierFix.kt
+++ plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/quickfix/RemoveModifierFix.kt
@@ -53,6 +53,9 @@ class RemoveModifierFix(
     }
 
     companion object {
+        val removeRedundantModifier = createRemoveModifierFactory(isRedundant = true)
+        val removeNonRedundantModifier = createRemoveModifierFactory(isRedundant = false)
+
         @Deprecated(
             "For binary compatibility",
             replaceWith = ReplaceWith("createRemoveModifierFromListOwnerPsiBasedFactory(modifier, isRedundant)")
@@ -81,7 +84,7 @@ class RemoveModifierFix(
             listOf(RemoveModifierFix(it, modifier, isRedundant))
         }
 
-        fun createRemoveModifierFactory(isRedundant: Boolean = false): QuickFixesPsiBasedFactory<PsiElement> {
+        private fun createRemoveModifierFactory(isRedundant: Boolean = false): QuickFixesPsiBasedFactory<PsiElement> {
             return quickFixesPsiBasedFactory { psiElement: PsiElement ->
                 val elementType = psiElement.node.elementType as? KtModifierKeywordToken ?: return@quickFixesPsiBasedFactory emptyList()
                 val modifierListOwner = psiElement.getStrictParentOfType<KtModifierListOwner>()
