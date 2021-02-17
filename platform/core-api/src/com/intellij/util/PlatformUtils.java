23d90b4a3763745d65690e44b6d4b7318a869f06
Leonid Shalupov <leonid@shalupov.com>
2021-02-17 21:14:22 +0300

    cwm: rename most of product info and icons: intellij client -> cwm guest
    (cherry picked from commit 3fda02d5c523fda6c64d137f038c90a4a9285f75)
    
    GitOrigin-RevId: dcfb07720ca3464ebb6eb8d7a8a04734437f854d

--- platform/core-api/src/com/intellij/util/PlatformUtils.java
+++ platform/core-api/src/com/intellij/util/PlatformUtils.java
@@ -40,7 +40,7 @@ public final class PlatformUtils {
   public static final @NonNls String RIDER_PREFIX = "Rider";
   public static final String GOIDE_PREFIX = "GoLand";
   public static final String FLEET_PREFIX = "FleetBackend";
-  public static final String INTELLIJ_CLIENT_PREFIX = "IntelliJClient";
+  public static final String CWM_GUEST_PREFIX = "CodeWithMeGuest";
 
   @SuppressWarnings("SSBasedInspection") private static final Set<String> COMMERCIAL_EDITIONS = new HashSet<>(Arrays.asList(
     IDEA_PREFIX, APPCODE_PREFIX, CLION_PREFIX, MOBILE_IDE_PREFIX, PYCHARM_PREFIX, RUBY_PREFIX, PHP_PREFIX, WEB_PREFIX, DBE_PREFIX, RIDER_PREFIX, GOIDE_PREFIX));
@@ -141,7 +141,7 @@ public final class PlatformUtils {
   }
 
   public static boolean isIntelliJClient() {
-    return is(INTELLIJ_CLIENT_PREFIX);
+    return is(CWM_GUEST_PREFIX);
   }
 
   public static boolean isCommunityEdition() {
