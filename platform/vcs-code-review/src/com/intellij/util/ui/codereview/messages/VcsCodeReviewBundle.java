742446c2388eb349d7d46fd0b1d317159baad9d2
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2021-02-18 12:50:30 +0300

    [vcs-review] IDEA-233137, IDEA-261821: Add tooltip for add comment icon
    GitOrigin-RevId: a1a3038f0f027acf5883a873ce4f07f4efed3a19

---
+++ platform/vcs-code-review/src/com/intellij/util/ui/codereview/messages/VcsCodeReviewBundle.java
@@ -0,0 +1,30 @@
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.util.ui.codereview.messages;
+
+import com.intellij.DynamicBundle;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.PropertyKey;
+
+import java.util.function.Supplier;
+
+public class VcsCodeReviewBundle extends DynamicBundle {
+  @NonNls private static final String BUNDLE = "messages.VcsCodeReviewBundle";
+  private static final VcsCodeReviewBundle INSTANCE = new VcsCodeReviewBundle();
+
+  private VcsCodeReviewBundle() {
+    super(BUNDLE);
+  }
+
+  @NotNull
+  public static @Nls String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, Object @NotNull ... params) {
+    return INSTANCE.getMessage(key, params);
+  }
+
+  @NotNull
+  public static Supplier<@Nls String> messagePointer(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key,
+                                                     Object @NotNull ... params) {
+    return INSTANCE.getLazyMessage(key, params);
+  }
+}
