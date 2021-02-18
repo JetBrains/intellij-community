e1df699b5437cb634b80da649f5d8fe83341073c
Sergey.Anchipolevsky <sergey.anchipolevsky@jetbrains.com>
2021-02-19 01:36:54 +0300

    [kubernetes] IDEA-188834 kubernetes plugin does not permit "kind: List"
    (cherry picked from commit f06c103b1e9c5126831a3483adb990aa49a81c5e)
    
    IJ-CR-6065
    
    GitOrigin-RevId: 2e1646e0822ae15362cf8bbfdd94578c7d8e8792

--- plugins/yaml/src/org/jetbrains/yaml/meta/model/Field.java
+++ plugins/yaml/src/org/jetbrains/yaml/meta/model/Field.java
@@ -9,6 +9,7 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
 import org.jetbrains.annotations.*;
 import org.jetbrains.yaml.psi.YAMLKeyValue;
+import org.jetbrains.yaml.psi.YAMLValue;
 
 import javax.swing.*;
 import java.util.Collections;
@@ -48,6 +49,10 @@ public class Field {
    */
   public interface MetaTypeSupplier {
     @NotNull YamlMetaType getMainType();
+
+    default @Nullable YamlMetaType getSpecializedType(@SuppressWarnings("unused") @NotNull YAMLValue element) {
+      return null;
+    }
   }
 
   public Field(@NonNls @NotNull String name, @NotNull YamlMetaType mainType) {
@@ -253,7 +258,39 @@ public class Field {
   public Icon getLookupIcon() {
     return myIsMany ? AllIcons.Json.Array : getMainType().getIcon();
   }
-  
+
+  @NotNull
+  public Field resolveToSpecializedField(@NotNull YAMLValue element) {
+    if(myMetaTypeSupplier == null)
+      return this;
+
+    YamlMetaType specializedType = myMetaTypeSupplier.getSpecializedType(element);
+    if(specializedType == null)
+      return this;
+
+    return cloneWithNewType(specializedType);
+  }
+
+  private Field cloneWithNewType(@NotNull YamlMetaType newType) {
+    var result = newField(newType);
+
+    result.myIsRequired = myIsRequired;
+    result.myEditable = myEditable;
+    result.myDeprecated = myDeprecated;
+    result.myAnyNameAllowed = myAnyNameAllowed;
+    result.myEmptyValueAllowed = myEmptyValueAllowed;
+    result.myIsMany = myIsMany;
+    result.myOverriddenDefaultRelation = myOverriddenDefaultRelation;
+    result.myPerRelationTypes.putAll(myPerRelationTypes);
+
+    return result;
+  }
+
+  @NotNull
+  protected Field newField(@NotNull YamlMetaType type) {
+    return new Field(this.myName, type);
+  }
+
   @NotNull
   private YamlMetaType getMainType() {
     if(myMainType != null)
