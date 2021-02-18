c08c06d8a582c93c4848ae31d9ac097b4df042f8
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-02-18 12:29:33 +0700

    [inspections] AbstractPrimitiveRangeToInspection: cleanup code
    GitOrigin-RevId: 16b42cf04c6801426752419b4d079f8640950ee1

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/inspections/AbstractPrimitiveRangeToInspection.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/inspections/AbstractPrimitiveRangeToInspection.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2018 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2000-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -47,5 +47,3 @@ abstract class AbstractPrimitiveRangeToInspection : AbstractKotlinInspection() {
         }
     }
 }
-
-var q = 12
