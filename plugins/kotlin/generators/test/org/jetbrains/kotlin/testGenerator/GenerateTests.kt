0426331726d391427b0116e283c50737ee9cf7ac
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-02-18 20:27:39 +0100

    FIR IDE: specify behaviour of HL API getOverriddenSymbols
    - Split it into two functions getAllOverriddenSymbols and getDirectlyOverriddenSymbols
    - Implement tests for getOverriddenSymbols
    - temporary mute inheritance.kt light classes test
    
    Original commit: b08eb6cf4c7b74343659f8c04d269ee1bbf36a9e
    
    GitOrigin-RevId: ec6ee8691a0998e3fe5da097cd020c20b88116b3

--- plugins/kotlin/generators/test/org/jetbrains/kotlin/testGenerator/GenerateTests.kt
+++ plugins/kotlin/generators/test/org/jetbrains/kotlin/testGenerator/GenerateTests.kt
@@ -65,6 +65,7 @@ import org.jetbrains.kotlin.idea.editor.backspaceHandler.AbstractBackspaceHandle
 import org.jetbrains.kotlin.idea.editor.quickDoc.AbstractQuickDocProviderTest
 import org.jetbrains.kotlin.idea.externalAnnotations.AbstractExternalAnnotationTest
 import org.jetbrains.kotlin.idea.folding.AbstractKotlinFoldingTest
+import org.jetbrains.kotlin.idea.frontend.api.components.AbstractOverriddenDeclarationProviderTest
 import org.jetbrains.kotlin.idea.hierarchy.AbstractHierarchyTest
 import org.jetbrains.kotlin.idea.hierarchy.AbstractHierarchyWithLibTest
 import org.jetbrains.kotlin.idea.highlighter.*
@@ -1035,6 +1036,10 @@ private fun assembleWorkspace(): TWorkspace = workspace {
 
         testClass<AbstractExpectedExpressionTypeTest> {
             model("components/expectedExpressionType")
+        }
+
+        testClass<AbstractOverriddenDeclarationProviderTest> {
+            model("components/overridenDeclarations")
         }
     }
 
