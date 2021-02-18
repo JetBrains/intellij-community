39ac4d49572b8a611ac9f1e0a27c00bef69afa68
Tianyu Geng <tgeng@google.com>
2021-02-18 14:00:13 -0800

    FIR IDE: Add quickfix for PRIVATE_SETTER_FOR_(OPEN|ABSTRACT)_PROPERTY
    Original commit: bca393bd5c26a5166fe704adb63853eaed42dfeb
    
    GitOrigin-RevId: ea3e5e7999a3ff2d8e506e9b930bfc311fa050c3

--- plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/quickfix/AddModifierFix.kt
+++ plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/quickfix/AddModifierFix.kt
@@ -108,6 +108,8 @@ open class AddModifierFix(
             AddModifierFix.createFactory(KtTokens.ABSTRACT_KEYWORD, KtClassOrObject::class.java)
         val addOpenToContainingClass: QuickFixesPsiBasedFactory<PsiElement> =
             AddModifierFix.createFactory(KtTokens.OPEN_KEYWORD, KtClassOrObject::class.java)
+        val addFinalToProperty: QuickFixesPsiBasedFactory<PsiElement> =
+            AddModifierFix.createFactory(KtTokens.FINAL_KEYWORD, KtProperty::class.java)
 
         val modifiersWithWarning = setOf(KtTokens.ABSTRACT_KEYWORD, KtTokens.FINAL_KEYWORD)
 
