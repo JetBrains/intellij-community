697775c28041fb326bfb35026351e5ce469fd52a
Dmitry Jemerov <yole@jetbrains.com>
2021-02-19 16:43:33 +0100

    Consistent tooltip capitalization
    GitOrigin-RevId: 0b8243c22ae1850dff6da24a3d0e27ae5f28514e

--- platform/execution-impl/src/com/intellij/execution/impl/RunConfigurable.kt
+++ platform/execution-impl/src/com/intellij/execution/impl/RunConfigurable.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.impl
 
 import com.intellij.execution.*
@@ -1296,7 +1296,7 @@ open class RunConfigurable @JvmOverloads constructor(protected val project: Proj
         }
       }
       e.presentation.text = if (toMove) ExecutionBundle.message("run.configuration.create.folder.description.move")
-                            else ExecutionBundle.message("run.configuration.create.folder.description")
+                            else ExecutionBundle.message("run.configuration.create.folder.text")
       e.presentation.isEnabled = isEnabled
     }
   }
