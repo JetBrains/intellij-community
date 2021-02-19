d62da9f9cd2743afbe611d300a01bae4e85ca085
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2021-02-19 13:53:47 +0300

    diagnostics for EA-238161/EA-238149
    GitOrigin-RevId: e1afd9b13c03f8f02b28005f96b089afb4dfe7aa

--- platform/platform-impl/src/com/intellij/openapi/editor/impl/ErrorStripeMarkerImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/editor/impl/ErrorStripeMarkerImpl.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.editor.impl;
 
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.event.DocumentEvent;
 import com.intellij.openapi.editor.ex.DocumentEx;
 import com.intellij.openapi.editor.ex.RangeHighlighterEx;
@@ -8,6 +9,7 @@ import com.intellij.openapi.editor.markup.HighlighterTargetArea;
 import org.jetbrains.annotations.NotNull;
 
 class ErrorStripeMarkerImpl extends RangeMarkerImpl {
+  private static final Logger LOG = Logger.getInstance(ErrorStripeMarkerImpl.class);
 
   private final RangeHighlighterEx myHighlighter;
   private int myLine;
@@ -32,6 +34,28 @@ class ErrorStripeMarkerImpl extends RangeMarkerImpl {
     }
     else {
       super.changedUpdateImpl(e);
+    }
+
+    validateState(e);
+  }
+
+  private void validateState(DocumentEvent e) {
+    if (myHighlighter.isValid()) {
+      if (!isValid()) {
+        LOG.error("Base highlighter " + myHighlighter + " is valid, mirror " + this + " is invalid after " + e);
+      }
+      else if (intervalStart() != myHighlighter.getStartOffset() || intervalEnd() != myHighlighter.getEndOffset()) {
+        LOG.error("Mirror highlighter " + this + " diverged from base one " + myHighlighter + " after " + e);
+        setIntervalStart(myHighlighter.getStartOffset());
+        setIntervalEnd(myHighlighter.getEndOffset());
+        if (myHighlighter.isPersistent()) {
+          myLine = getDocument().getLineNumber(intervalStart());
+        }
+      }
+    }
+    else if (isValid()) {
+      LOG.error("Base highlighter " + myHighlighter + " is invalid, mirror " + this + " is valid after " + e);
+      invalidate(e);
     }
   }
 }
