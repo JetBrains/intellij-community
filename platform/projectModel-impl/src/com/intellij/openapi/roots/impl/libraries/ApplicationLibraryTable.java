090c8ed1f53c944e48ae19fa17b74b898e0a0524
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2021-02-19 13:52:41 +0300

    [project model] ensure that VirtualFilePointerManager is disposed after global and custom libraries
    ...to fix "Not disposed pointer" errors in log during application disposal.
    
    GitOrigin-RevId: b3ba08285cf649f274eb358b30f06fc806a238d6

--- platform/projectModel-impl/src/com/intellij/openapi/roots/impl/libraries/ApplicationLibraryTable.java
+++ platform/projectModel-impl/src/com/intellij/openapi/roots/impl/libraries/ApplicationLibraryTable.java
@@ -5,6 +5,7 @@ package com.intellij.openapi.roots.impl.libraries;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.roots.libraries.LibraryTablePresentation;
 import com.intellij.openapi.roots.libraries.LibraryTablesRegistrar;
+import com.intellij.openapi.vfs.pointers.VirtualFilePointerManager;
 import com.intellij.projectModel.ProjectModelBundle;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
@@ -36,6 +37,12 @@ public class ApplicationLibraryTable extends LibraryTableBase {
 
   public static ApplicationLibraryTable getApplicationTable() {
     return ApplicationManager.getApplication().getService(ApplicationLibraryTable.class);
+  }
+
+  public ApplicationLibraryTable() {
+    //this is needed to ensure that VirtualFilePointerManager is initialized before ApplicationLibraryTable and therefore disposed after it;
+    //otherwise VirtualFilePointerManagerImpl.dispose will report non-disposed pointers from global libraries
+    VirtualFilePointerManager.getInstance();
   }
 
   @NotNull
