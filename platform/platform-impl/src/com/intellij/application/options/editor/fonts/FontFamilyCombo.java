d4ca89377bc884cf1f270e40cf0953840dec5f8a
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2021-02-20 18:03:32 +0300

    Fixed IDEA-260496 Font Settings: dropdown uses wrong font when Monospaced selected
    GitOrigin-RevId: f48afd5e39a9e21e4494a86be8f94c41e3c5850c

--- platform/platform-impl/src/com/intellij/application/options/editor/fonts/FontFamilyCombo.java
+++ platform/platform-impl/src/com/intellij/application/options/editor/fonts/FontFamilyCombo.java
@@ -90,6 +90,10 @@ public class FontFamilyCombo extends AbstractFontCombo<FontFamilyCombo.MyFontIte
     public @NotNull String getFamilyName() {
       return myFamilyName;
     }
+
+    public boolean isSelectable() {
+      return true;
+    }
   }
 
   private static class MyNoFontItem extends MyFontItem {
@@ -103,6 +107,11 @@ public class FontFamilyCombo extends AbstractFontCombo<FontFamilyCombo.MyFontIte
 
     private MySeparatorItem(@NotNull String title, boolean isMonospaced) {
       super(title, isMonospaced);
+    }
+
+    @Override
+    public boolean isSelectable() {
+      return false;
     }
   }
 
@@ -160,7 +169,7 @@ public class FontFamilyCombo extends AbstractFontCombo<FontFamilyCombo.MyFontIte
         mySelectedItem = myNoFontItem;
       }
       else if (anItem instanceof String) {
-        mySelectedItem = ContainerUtil.find(myItems, item -> item.myFamilyName.equals(anItem));
+        mySelectedItem = ContainerUtil.find(myItems, item -> item.isSelectable() && item.myFamilyName.equals(anItem));
       }
       else if (anItem instanceof MySeparatorItem) {
         return;
