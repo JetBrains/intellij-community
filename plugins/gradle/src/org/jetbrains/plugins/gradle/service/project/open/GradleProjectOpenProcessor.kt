758c714f484390b9bad5984162012ac6560b2bdc
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2021-02-20 16:35:35 +0300

    Remove unused ProjectOpenProcessor.executesUnverifiedCode, and fix javadoc
    The method is not used anymore since 4f64cceb.
    
    IDEA-260911
    
    GitOrigin-RevId: 1b974422eb2155a0a44c3faa6853e568ab0ce5fd

--- plugins/gradle/src/org/jetbrains/plugins/gradle/service/project/open/GradleProjectOpenProcessor.kt
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/service/project/open/GradleProjectOpenProcessor.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.gradle.service.project.open
 
 import com.intellij.openapi.project.Project
@@ -25,6 +25,4 @@ class GradleProjectOpenProcessor : ProjectOpenProcessor() {
   override fun importProjectAfterwards(project: Project, file: VirtualFile) {
     linkAndRefreshGradleProject(file.path, project)
   }
-
-  override fun executesUnverifiedCode(): Boolean = true
 }
