278bee35bea7c57ffcacd82d8f87c5d834d87a0b
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2021-02-20 12:07:08 +0300

    Revert "Disable one more trusted projects check for headless mode and for integration performance tests"
    This reverts commit 5818fce3bd35c1b8e7bbb54412d21d7d635dc358.
    
    GitOrigin-RevId: 272817d113cb6c93108ef3baab7ff1b17d3a204f

--- platform/platform-impl/src/com/intellij/ide/impl/ProjectUtil.java
+++ platform/platform-impl/src/com/intellij/ide/impl/ProjectUtil.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.impl;
 
 import com.intellij.CommonBundle;
@@ -380,7 +380,7 @@ public final class ProjectUtil {
                                                    @NotNull VirtualFile projectFile,
                                                    @Nullable Project projectToClose,
                                                    boolean forceOpenInNewFrame) {
-    if (processor.executesUnverifiedCode() && !TrustedProjects.isTrustedCheckDisabled()) {
+    if (processor.executesUnverifiedCode()) {
       OpenUntrustedProjectChoice choice = TrustedProjects.confirmOpeningUntrustedProject(processor.getName());
       switch (choice) {
         case IMPORT:
