ff3f6b98562e44618a3a9b8d7d517390c5a91b59
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2021-02-21 01:00:07 +0300

    wsl target: more verbose logging in wsl2_proxy.py to investigate no connection received by SimpleProxy
    GitOrigin-RevId: 00c67f0bac94fb1bb89c3a1ded67277d8e037614

--- platform/execution-impl/src/com/intellij/execution/wsl/target/proxy/wsl2_proxy.py
+++ platform/execution-impl/src/com/intellij/execution/wsl/target/proxy/wsl2_proxy.py
@@ -15,17 +15,22 @@ def read_write(src, dest, name):
     dest.close()
 
 
-def process(client_socket):
+def process(client_socket, client_address):
     remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    remote_address = (remoteHost, remotePort)
     try:
-        remote_socket.connect((remoteHost, remotePort))
+        remote_socket.connect(remote_address)
+        print("{0}: connected to {1}".format(client_address, remote_address), flush=True)
         t1 = threading.Thread(target=read_write, args=(client_socket, remote_socket, "client->remote"))
         t1.start()
         t2 = threading.Thread(target=read_write, args=(remote_socket, client_socket, "remote->client"))
         t2.start()
         t1.join()
         t2.join()
+    except Exception as err:
+        print("failed to proxy {0}<->{1}, caused by {2}".format(client_address, remote_address, err), flush=True)
     finally:
+        print("{0}: disconnected from {1}".format(client_address, remote_address), flush=True)
         remote_socket.close()
 
 
@@ -41,9 +46,10 @@ if __name__ == '__main__':
     serverSocket.listen(10)
 
     while True:
-        (_client_socket, client_address) = serverSocket.accept()
+        (_client_socket, _client_address) = serverSocket.accept()
         try:
-            print("client connected from {0}".format(client_address), flush=True)
-            process(_client_socket)
+            print("client connected from {0}".format(_client_address), flush=True)
+            process(_client_socket, _client_address)
         finally:
+            print("client from {0} is disconnected".format(_client_address), flush=True)
             _client_socket.close()
