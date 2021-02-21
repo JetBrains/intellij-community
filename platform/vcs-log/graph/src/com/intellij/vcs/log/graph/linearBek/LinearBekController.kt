b214b21d793c1f3fb66dc746cc41ae1e98690410
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2021-02-21 19:34:15 +0300

    [vcs-log-graph] convert LinearBekController to kotlin
    GitOrigin-RevId: e78b1a9deef6eae5237372008e98b16720a96678

--- platform/vcs-log/graph/src/com/intellij/vcs/log/graph/linearBek/LinearBekController.kt
+++ platform/vcs-log/graph/src/com/intellij/vcs/log/graph/linearBek/LinearBekController.kt
@@ -1,238 +1,185 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.vcs.log.graph.linearBek;
+package com.intellij.vcs.log.graph.linearBek
 
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.util.containers.ContainerUtil;
-import com.intellij.vcs.log.graph.actions.GraphAction;
-import com.intellij.vcs.log.graph.api.EdgeFilter;
-import com.intellij.vcs.log.graph.api.GraphLayout;
-import com.intellij.vcs.log.graph.api.LinearGraph;
-import com.intellij.vcs.log.graph.api.elements.GraphEdge;
-import com.intellij.vcs.log.graph.api.elements.GraphEdgeType;
-import com.intellij.vcs.log.graph.api.elements.GraphElement;
-import com.intellij.vcs.log.graph.api.elements.GraphNode;
-import com.intellij.vcs.log.graph.api.permanent.PermanentGraphInfo;
-import com.intellij.vcs.log.graph.impl.facade.BekBaseController;
-import com.intellij.vcs.log.graph.impl.facade.CascadeController;
-import com.intellij.vcs.log.graph.impl.facade.GraphChangesUtil;
-import com.intellij.vcs.log.graph.impl.facade.bek.BekIntMap;
-import com.intellij.vcs.log.graph.utils.LinearGraphUtils;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.vcs.log.graph.actions.GraphAction
+import com.intellij.vcs.log.graph.api.EdgeFilter
+import com.intellij.vcs.log.graph.api.GraphLayout
+import com.intellij.vcs.log.graph.api.LinearGraph
+import com.intellij.vcs.log.graph.api.elements.GraphEdge
+import com.intellij.vcs.log.graph.api.elements.GraphEdgeType
+import com.intellij.vcs.log.graph.api.elements.GraphNode
+import com.intellij.vcs.log.graph.api.permanent.PermanentGraphInfo
+import com.intellij.vcs.log.graph.impl.facade.BekBaseController
+import com.intellij.vcs.log.graph.impl.facade.CascadeController
+import com.intellij.vcs.log.graph.impl.facade.GraphChangesUtil
+import com.intellij.vcs.log.graph.impl.facade.LinearGraphController.LinearGraphAction
+import com.intellij.vcs.log.graph.impl.facade.LinearGraphController.LinearGraphAnswer
+import com.intellij.vcs.log.graph.impl.facade.bek.BekIntMap
+import com.intellij.vcs.log.graph.linearBek.LinearBekGraph.WorkingLinearBekGraph
+import com.intellij.vcs.log.graph.linearBek.LinearBekGraphBuilder.MergeFragment
+import com.intellij.vcs.log.graph.utils.LinearGraphUtils
+import java.util.*
 
-import java.util.*;
+class LinearBekController(controller: BekBaseController, permanentGraphInfo: PermanentGraphInfo<*>) : CascadeController(controller,
+                                                                                                                        permanentGraphInfo) {
+  private val delegateGraph: LinearGraph
+    get() = delegateController.compiledGraph
 
-public class LinearBekController extends CascadeController {
-  private static final Logger LOG = Logger.getInstance(LinearBekController.class);
-  @NotNull private final LinearBekGraph myCompiledGraph;
-  private final LinearBekGraphBuilder myLinearBekGraphBuilder;
-  private final BekGraphLayout myBekGraphLayout;
+  private val compiledGraph: LinearBekGraph = LinearBekGraph(delegateGraph)
+  private val bekGraphLayout: BekGraphLayout = BekGraphLayout(permanentGraphInfo.permanentGraphLayout, controller.bekIntMap)
+  private val linearBekGraphBuilder: LinearBekGraphBuilder = LinearBekGraphBuilder(compiledGraph, bekGraphLayout)
 
-  public LinearBekController(@NotNull BekBaseController controller, @NotNull PermanentGraphInfo permanentGraphInfo) {
-    super(controller, permanentGraphInfo);
-    myCompiledGraph = new LinearBekGraph(getDelegateGraph());
-    myBekGraphLayout = new BekGraphLayout(permanentGraphInfo.getPermanentGraphLayout(), controller.getBekIntMap());
-    myLinearBekGraphBuilder = new LinearBekGraphBuilder(myCompiledGraph, myBekGraphLayout);
-
-    long start = System.currentTimeMillis();
-    myLinearBekGraphBuilder.collapseAll();
-    LOG.debug("Linear bek took " + (System.currentTimeMillis() - start) / 1000.0 + " sec");
+  init {
+    val start = System.currentTimeMillis()
+    linearBekGraphBuilder.collapseAll()
+    LOG.debug("Linear bek took " + (System.currentTimeMillis() - start) / 1000.0 + " sec")
   }
 
-  @NotNull
-  @Override
-  protected LinearGraphAnswer delegateGraphChanged(@NotNull LinearGraphAnswer delegateAnswer) {
-    return delegateAnswer;
+  override fun delegateGraphChanged(delegateAnswer: LinearGraphAnswer): LinearGraphAnswer {
+    return delegateAnswer
   }
 
-  @Nullable
-  @Override
-  protected LinearGraphAnswer performAction(@NotNull LinearGraphAction action) {
-    if (action.getAffectedElement() != null) {
-      if (action.getType() == GraphAction.Type.MOUSE_CLICK) {
-        GraphElement graphElement = action.getAffectedElement().getGraphElement();
-        if (graphElement instanceof GraphNode) {
-          LinearGraphAnswer answer = collapseNode((GraphNode)graphElement);
-          if (answer != null) return answer;
-          for (GraphEdge dottedEdge : getAllAdjacentDottedEdges((GraphNode)graphElement)) {
-            LinearGraphAnswer expandedAnswer = expandEdge(dottedEdge);
-            if (expandedAnswer != null) return expandedAnswer;
+  override fun performAction(action: LinearGraphAction): LinearGraphAnswer? {
+    if (action.affectedElement != null) {
+      if (action.type == GraphAction.Type.MOUSE_CLICK) {
+        val graphElement = action.affectedElement!!.graphElement
+        if (graphElement is GraphNode) {
+          val answer = collapseNode(graphElement)
+          if (answer != null) return answer
+          for (dottedEdge in getAllAdjacentDottedEdges(graphElement)) {
+            val expandedAnswer = expandEdge(dottedEdge)
+            if (expandedAnswer != null) return expandedAnswer
           }
         }
-        else if (graphElement instanceof GraphEdge) {
-          return expandEdge((GraphEdge)graphElement);
+        else if (graphElement is GraphEdge) {
+          return expandEdge(graphElement)
         }
       }
-      else if (action.getType() == GraphAction.Type.MOUSE_OVER) {
-        GraphElement graphElement = action.getAffectedElement().getGraphElement();
-        if (graphElement instanceof GraphNode) {
-          LinearGraphAnswer answer = highlightNode((GraphNode)graphElement);
-          if (answer != null) return answer;
-          for (GraphEdge dottedEdge : getAllAdjacentDottedEdges((GraphNode)graphElement)) {
-            LinearGraphAnswer highlightAnswer = highlightEdge(dottedEdge);
-            if (highlightAnswer != null) return highlightAnswer;
+      else if (action.type == GraphAction.Type.MOUSE_OVER) {
+        val graphElement = action.affectedElement!!.graphElement
+        if (graphElement is GraphNode) {
+          val answer = highlightNode(graphElement)
+          if (answer != null) return answer
+          for (dottedEdge in getAllAdjacentDottedEdges(graphElement)) {
+            val highlightAnswer = highlightEdge(dottedEdge)
+            if (highlightAnswer != null) return highlightAnswer
           }
         }
-        else if (graphElement instanceof GraphEdge) {
-          return highlightEdge((GraphEdge)graphElement);
+        else if (graphElement is GraphEdge) {
+          return highlightEdge(graphElement)
         }
       }
     }
-    else if (action.getType() == GraphAction.Type.BUTTON_COLLAPSE) {
-      return collapseAll();
+    else if (action.type == GraphAction.Type.BUTTON_COLLAPSE) {
+      return collapseAll()
     }
-    else if (action.getType() == GraphAction.Type.BUTTON_EXPAND) {
-      return expandAll();
+    else if (action.type == GraphAction.Type.BUTTON_EXPAND) {
+      return expandAll()
     }
-    return null;
+    return null
   }
 
-  @NotNull
-  private List<GraphEdge> getAllAdjacentDottedEdges(GraphNode graphElement) {
-    return ContainerUtil.filter(myCompiledGraph.getAdjacentEdges(graphElement.getNodeIndex(), EdgeFilter.ALL),
-                                graphEdge -> graphEdge.getType() == GraphEdgeType.DOTTED);
+  private fun getAllAdjacentDottedEdges(graphElement: GraphNode): List<GraphEdge> {
+    return compiledGraph.getAdjacentEdges(graphElement.nodeIndex, EdgeFilter.ALL).filter { graphEdge: GraphEdge ->
+      graphEdge.type == GraphEdgeType.DOTTED
+    }
   }
 
-  @NotNull
-  private LinearGraphAnswer expandAll() {
-    return new LinearGraphAnswer(GraphChangesUtil.SOME_CHANGES) {
-      @Override
-      public @NotNull Runnable getGraphUpdater() {
-        return () -> {
-          myCompiledGraph.myDottedEdges.removeAll();
-          myCompiledGraph.myHiddenEdges.removeAll();
-        };
+  private fun expandAll(): LinearGraphAnswer {
+    return object : LinearGraphAnswer(GraphChangesUtil.SOME_CHANGES) {
+      override fun getGraphUpdater(): Runnable {
+        return Runnable {
+          compiledGraph.myDottedEdges.removeAll()
+          compiledGraph.myHiddenEdges.removeAll()
+        }
       }
-    };
+    }
   }
 
-  @NotNull
-  private LinearGraphAnswer collapseAll() {
-    final LinearBekGraph.WorkingLinearBekGraph workingGraph = new LinearBekGraph.WorkingLinearBekGraph(myCompiledGraph);
-    new LinearBekGraphBuilder(workingGraph, myBekGraphLayout).collapseAll();
-    return new LinearGraphAnswer(
-      GraphChangesUtil.edgesReplaced(workingGraph.getRemovedEdges(), workingGraph.getAddedEdges(), getDelegateGraph())) {
-      @Override
-      public @NotNull Runnable getGraphUpdater() {
-        return () -> workingGraph.applyChanges();
+  private fun collapseAll(): LinearGraphAnswer {
+    val workingGraph = WorkingLinearBekGraph(compiledGraph)
+    LinearBekGraphBuilder(workingGraph, bekGraphLayout).collapseAll()
+    return object : LinearGraphAnswer(
+      GraphChangesUtil.edgesReplaced(workingGraph.removedEdges, workingGraph.addedEdges, delegateGraph)) {
+      override fun getGraphUpdater(): Runnable {
+        return Runnable { workingGraph.applyChanges() }
       }
-    };
+    }
   }
 
-  @Nullable
-  private LinearGraphAnswer highlightNode(GraphNode node) {
-    Set<LinearBekGraphBuilder.MergeFragment> toCollapse = collectFragmentsToCollapse(node);
-    if (toCollapse.isEmpty()) return null;
-
-    Set<Integer> toHighlight = new HashSet<>();
-    for (LinearBekGraphBuilder.MergeFragment fragment : toCollapse) {
-      toHighlight.addAll(fragment.getAllNodes());
+  private fun highlightNode(node: GraphNode): LinearGraphAnswer? {
+    val toCollapse = collectFragmentsToCollapse(node)
+    if (toCollapse.isEmpty()) return null
+    val toHighlight: MutableSet<Int> = HashSet()
+    for (fragment in toCollapse) {
+      toHighlight.addAll(fragment.allNodes)
     }
-
-    return LinearGraphUtils.createSelectedAnswer(myCompiledGraph, toHighlight);
+    return LinearGraphUtils.createSelectedAnswer(compiledGraph, toHighlight)
   }
 
-  @Nullable
-  private LinearGraphAnswer highlightEdge(GraphEdge edge) {
-    if (edge.getType() == GraphEdgeType.DOTTED) {
-      return LinearGraphUtils.createSelectedAnswer(myCompiledGraph, ContainerUtil.set(edge.getUpNodeIndex(), edge.getDownNodeIndex()));
-    }
-    return null;
+  private fun highlightEdge(edge: GraphEdge): LinearGraphAnswer? {
+    if (edge.type != GraphEdgeType.DOTTED) return null
+    return LinearGraphUtils.createSelectedAnswer(compiledGraph, setOf(edge.upNodeIndex, edge.downNodeIndex))
   }
 
-  @Nullable
-  private LinearGraphAnswer collapseNode(GraphNode node) {
-    SortedSet<Integer> toCollapse = collectNodesToCollapse(node);
-
-    if (toCollapse.isEmpty()) return null;
-
-    for (Integer i : toCollapse) {
-      myLinearBekGraphBuilder.collapseFragment(i);
+  private fun collapseNode(node: GraphNode): LinearGraphAnswer? {
+    val toCollapse = collectNodesToCollapse(node)
+    if (toCollapse.isEmpty()) return null
+    for (i in toCollapse) {
+      linearBekGraphBuilder.collapseFragment(i)
     }
-    return new LinearGraphAnswer(GraphChangesUtil.SOME_CHANGES);
+    return LinearGraphAnswer(GraphChangesUtil.SOME_CHANGES)
   }
 
-  private SortedSet<Integer> collectNodesToCollapse(GraphNode node) {
-    SortedSet<Integer> toCollapse = new TreeSet<>(Comparator.reverseOrder());
-    for (LinearBekGraphBuilder.MergeFragment f : collectFragmentsToCollapse(node)) {
-      toCollapse.add(f.getParent());
-      toCollapse.addAll(f.getTailsAndBody());
+  private fun collectNodesToCollapse(node: GraphNode): SortedSet<Int> {
+    val toCollapse: SortedSet<Int> = TreeSet(Comparator.reverseOrder())
+    for (f in collectFragmentsToCollapse(node)) {
+      toCollapse.add(f.parent)
+      toCollapse.addAll(f.tailsAndBody)
     }
-    return toCollapse;
+    return toCollapse
   }
 
-  @NotNull
-  private Set<LinearBekGraphBuilder.MergeFragment> collectFragmentsToCollapse(GraphNode node) {
-    Set<LinearBekGraphBuilder.MergeFragment> result = new HashSet<>();
-
-    int mergesCount = 0;
-
-    LinkedHashSet<Integer> toProcess = new LinkedHashSet<>();
-    toProcess.add(node.getNodeIndex());
-    while (!toProcess.isEmpty()) {
-      Integer i = ContainerUtil.getFirstItem(toProcess);
-      toProcess.remove(i);
-
-      LinearBekGraphBuilder.MergeFragment fragment = myLinearBekGraphBuilder.getFragment(i);
-      if (fragment == null) continue;
-
-      result.add(fragment);
-      toProcess.addAll(fragment.getTailsAndBody());
-
-      mergesCount++;
-      if (mergesCount > 10) break;
+  private fun collectFragmentsToCollapse(node: GraphNode): Set<MergeFragment> {
+    val result: MutableSet<MergeFragment> = HashSet()
+    var mergesCount = 0
+    val toProcess = LinkedHashSet<Int>()
+    toProcess.add(node.nodeIndex)
+    while (toProcess.isNotEmpty()) {
+      val i = toProcess.first()
+      toProcess.remove(i)
+      val fragment = linearBekGraphBuilder.getFragment(i) ?: continue
+      result.add(fragment)
+      toProcess.addAll(fragment.tailsAndBody)
+      mergesCount++
+      if (mergesCount > 10) break
     }
-    return result;
+    return result
   }
 
-  @Nullable
-  private LinearGraphAnswer expandEdge(GraphEdge edge) {
-    if (edge.getType() == GraphEdgeType.DOTTED) {
-      return new LinearGraphAnswer(
-        GraphChangesUtil.edgesReplaced(Collections.singleton(edge), myCompiledGraph.expandEdge(edge), getDelegateGraph()));
-    }
-    return null;
+  private fun expandEdge(edge: GraphEdge): LinearGraphAnswer? {
+    if (edge.type != GraphEdgeType.DOTTED) return null
+    return LinearGraphAnswer(GraphChangesUtil.edgesReplaced(setOf(edge), compiledGraph.expandEdge(edge), delegateGraph))
   }
 
-  @NotNull
-  private LinearGraph getDelegateGraph() {
-    return getDelegateController().getCompiledGraph();
+  override fun getCompiledGraph(): LinearGraph = compiledGraph
+
+  private class BekGraphLayout(private val graphLayout: GraphLayout,
+                               private val bekIntMap: BekIntMap) : GraphLayout {
+    override fun getLayoutIndex(nodeIndex: Int): Int {
+      return graphLayout.getLayoutIndex(bekIntMap.getUsualIndex(nodeIndex))
+    }
+
+    override fun getOneOfHeadNodeIndex(nodeIndex: Int): Int {
+      val usualIndex = graphLayout.getOneOfHeadNodeIndex(bekIntMap.getUsualIndex(nodeIndex))
+      return bekIntMap.getBekIndex(usualIndex)
+    }
+
+    override fun getHeadNodeIndex(): List<Int> = graphLayout.headNodeIndex.map { bekIntMap.getBekIndex(it) }
   }
 
-  @NotNull
-  @Override
-  public LinearGraph getCompiledGraph() {
-    return myCompiledGraph;
-  }
-
-  private static class BekGraphLayout implements GraphLayout {
-    private final GraphLayout myGraphLayout;
-    private final BekIntMap myBekIntMap;
-
-    BekGraphLayout(GraphLayout graphLayout, BekIntMap bekIntMap) {
-      myGraphLayout = graphLayout;
-      myBekIntMap = bekIntMap;
-    }
-
-    @Override
-    public int getLayoutIndex(int nodeIndex) {
-      return myGraphLayout.getLayoutIndex(myBekIntMap.getUsualIndex(nodeIndex));
-    }
-
-    @Override
-    public int getOneOfHeadNodeIndex(int nodeIndex) {
-      int usualIndex = myGraphLayout.getOneOfHeadNodeIndex(myBekIntMap.getUsualIndex(nodeIndex));
-      return myBekIntMap.getBekIndex(usualIndex);
-    }
-
-    @NotNull
-    @Override
-    public List<Integer> getHeadNodeIndex() {
-      List<Integer> bekIndexes = new ArrayList<>();
-      for (int head : myGraphLayout.getHeadNodeIndex()) {
-        bekIndexes.add(myBekIntMap.getBekIndex(head));
-      }
-      return bekIndexes;
-    }
+  companion object {
+    private val LOG = Logger.getInstance(LinearBekController::class.java)
   }
 }
