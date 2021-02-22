59f41dd4252ba68934d5762df30363855da69ee0
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2021-02-22 13:40:07 +0100

    [updater] handling file-to-link transitions (IDEA-262659)
    GitOrigin-RevId: 2c648841ddf9767e66afc437c8c68e0e66834295

--- updater/testSrc/com/intellij/updater/PatchApplyingRevertingTest.java
+++ updater/testSrc/com/intellij/updater/PatchApplyingRevertingTest.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.updater;
 
 import com.intellij.openapi.util.io.FileUtil;
@@ -593,6 +593,17 @@ public abstract class PatchApplyingRevertingTest extends PatchTestCase {
     resetNewerDir();
     Utils.setExecutable(new File(myNewerDir, "bin/to_become_plain"), false);
     Utils.setExecutable(new File(myNewerDir, "bin/to_become_executable"), true);
+
+    assertAppliedAndReverted();
+  }
+
+  @Test
+  public void fileToSymlinks() throws Exception {
+    assumeNioSymLinkCreationIsSupported();
+
+    resetNewerDir();
+    Files.move(new File(myNewerDir, "Readme.txt").toPath(), new File(myNewerDir, "Readme.md").toPath());
+    Files.createSymbolicLink(new File(myNewerDir, "Readme.txt").toPath(), Paths.get("Readme.md"));
 
     assertAppliedAndReverted();
   }
