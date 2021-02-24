08e51f11cd562f5a94338c95e3d3e1ef5610b778
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2021-02-24 16:40:16 +0300

    Ask for confirmation before configuring inner venv as a project interpreter (PY-41524)
    (cherry picked from commit 4b9cdcb0545044342fb02670a416d3cee7d24ff9)
    
    GitOrigin-RevId: 6d833d5b765350f9aed09ca28929ee2bfeec4ea2

--- python/src/com/jetbrains/python/sdk/PySdkExt.kt
+++ python/src/com/jetbrains/python/sdk/PySdkExt.kt
@@ -40,6 +40,7 @@ import com.intellij.openapi.vfs.StandardFileSystems
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.PathUtil
+import com.intellij.util.ThreeState
 import com.intellij.webcore.packaging.PackagesNotificationPanel
 import com.jetbrains.python.PyBundle
 import com.jetbrains.python.packaging.ui.PyPackageManagementService
@@ -122,6 +123,20 @@ fun detectAssociatedEnvironments(module: Module, existingSdks: List<Sdk>, contex
   val virtualEnvs = detectVirtualEnvs(module, existingSdks, context).filter { it.isAssociatedWithModule(module) }
   val condaEnvs = detectCondaEnvs(module, existingSdks, context).filter { it.isAssociatedWithModule(module) }
   return virtualEnvs + condaEnvs
+}
+
+fun chooseEnvironmentToSuggest(module: Module, environments: List<PyDetectedSdk>, trustedState: ThreeState): Pair<PyDetectedSdk, Boolean>? {
+  return if (trustedState == ThreeState.YES) {
+    environments.firstOrNull()?.let { it to false }
+  }
+  else {
+    val (detectedInnerEnvs, detectedOuterEnvs) = environments.partition { it.isLocatedInsideModule(module) }
+
+    when {
+      detectedInnerEnvs.isEmpty() || trustedState == ThreeState.NO -> detectedOuterEnvs.firstOrNull()?.let { it to false }
+      else -> detectedInnerEnvs.firstOrNull()?.let { it to true }
+    }
+  }
 }
 
 fun createSdkByGenerateTask(generateSdkHomePath: Task.WithResult<String, ExecutionException>,
