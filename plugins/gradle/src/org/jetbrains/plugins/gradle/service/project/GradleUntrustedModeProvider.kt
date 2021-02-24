313b64c52398a1e58f9350f562c9155c01ec5a27
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2021-02-24 21:46:47 +0300

    IDEA-243002 moved untrusted project notification provider into external system
    (cherry picked from commit c7529521bc5b677a82acbb371ea10f6af54e40aa)
    (cherry picked from commit f64317ff8991e199aa1a10748fb3c30e160491ed)
    
    IJ-CR-6280
    
    GitOrigin-RevId: ab9186b6fd8f7ed17340d842e7c43c5f1a3d3d50

--- plugins/gradle/src/org/jetbrains/plugins/gradle/service/project/GradleUntrustedModeProvider.kt
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/service/project/GradleUntrustedModeProvider.kt
@@ -1,7 +1,7 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.gradle.service.project
 
-import com.intellij.ide.impl.UntrustedProjectModeProvider
+import com.intellij.openapi.externalSystem.service.project.UntrustedProjectModeProvider
 import com.intellij.ide.impl.isTrusted
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
 import com.intellij.openapi.project.Project
