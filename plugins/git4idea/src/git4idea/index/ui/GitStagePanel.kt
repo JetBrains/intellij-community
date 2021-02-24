d3a31701745baa53449348302f0ba0f19bbe85ce
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2021-02-24 19:24:41 +0300

    [git-index] implement new inclusion model for roots
    GitOrigin-RevId: 0e6791191999462dbbc37eae423113ac62bc8df9

--- plugins/git4idea/src/git4idea/index/ui/GitStagePanel.kt
+++ plugins/git4idea/src/git4idea/index/ui/GitStagePanel.kt
@@ -232,6 +232,7 @@ internal class GitStagePanel(private val tracker: GitStageTracker,
     private val includedRootsListeners = EventDispatcher.create(IncludedRootsListener::class.java)
 
     init {
+      setInclusionModel(GitStageRootInclusionModel(project))
       groupingSupport.addPropertyChangeListener(PropertyChangeListener {
         includedRootsListeners.multicaster.includedRootsChanged()
       })
