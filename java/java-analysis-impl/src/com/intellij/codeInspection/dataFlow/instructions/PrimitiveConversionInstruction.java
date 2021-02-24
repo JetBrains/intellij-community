567f58b7c715706702ee5743916925540e744dbf
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2021-02-24 12:19:51 +0700

    [java-dfa] PrimitiveConversionInstruction: NotNull target type; display target type in toString
    GitOrigin-RevId: bfa959e977a164cf496d4b15b2dff246a11bf687

--- java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/instructions/PrimitiveConversionInstruction.java
+++ java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/instructions/PrimitiveConversionInstruction.java
@@ -19,9 +19,9 @@ import org.jetbrains.annotations.Nullable;
  * A unary instruction that converts a primitive value from the stack to the desired type
  */
 public class PrimitiveConversionInstruction extends EvalInstruction {
-  @Nullable private final PsiPrimitiveType myTargetType;
+  @NotNull private final PsiPrimitiveType myTargetType;
 
-  public PrimitiveConversionInstruction(@Nullable PsiPrimitiveType targetType, @Nullable PsiExpression expression) {
+  public PrimitiveConversionInstruction(@NotNull PsiPrimitiveType targetType, @Nullable PsiExpression expression) {
     super(expression, 1);
     myTargetType = targetType;
   }
@@ -35,7 +35,7 @@ public class PrimitiveConversionInstruction extends EvalInstruction {
     if (value instanceof DfaBinOpValue) {
       value = ((DfaBinOpValue)value).tryReduceOnCast(state, type);
     }
-    if (value instanceof DfaVariableValue && type != null &&
+    if (value instanceof DfaVariableValue &&
         (type.equals(value.getType()) ||
          TypeConversionUtil.isSafeConversion(type, value.getType()) && 
          TypeConversionUtil.isSafeConversion(PsiType.INT, type))) {
@@ -43,7 +43,7 @@ public class PrimitiveConversionInstruction extends EvalInstruction {
     }
 
     DfType dfType = state.getDfType(value);
-    if (dfType instanceof DfPrimitiveType && type != null) {
+    if (dfType instanceof DfPrimitiveType) {
       return factory.fromDfType(((DfPrimitiveType)dfType).castTo(type));
     }
     return factory.getUnknown();
@@ -51,6 +51,6 @@ public class PrimitiveConversionInstruction extends EvalInstruction {
 
   @Override
   public String toString() {
-    return "CONVERT_PRIMITIVE";
+    return "CONVERT_PRIMITIVE " + myTargetType.getPresentableText();
   }
 }
