836d694ee914287d96d4e57310938d678053cd7a
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2021-02-24 19:41:10 +0300

    Consider `Trusted Hosts` settings while configuring inner venv (PY-41524)
    GitOrigin-RevId: 67a97e6312341241704f2d6d80b88900127ec200

--- python/src/com/jetbrains/python/inspections/PyInterpreterInspection.java
+++ python/src/com/jetbrains/python/inspections/PyInterpreterInspection.java
@@ -167,7 +167,7 @@ public final class PyInterpreterInspection extends PyInspection {
       final UserDataHolderBase context = new UserDataHolderBase();
 
       final var detectedAssociatedEnvironments = PySdkExtKt.detectAssociatedEnvironments(module, existingSdks, context);
-      final var trustedState = TrustedProjects.getTrustedState(module.getProject());
+      final var trustedState = TrustedProjects.getExplicitTrustedStateOrByHostAndLocation(module.getProject());
       final var detectedEnv = PySdkExtKt.chooseEnvironmentToSuggest(module, detectedAssociatedEnvironments, trustedState);
       if (detectedEnv != null) {
         return new UseDetectedInterpreterFix(detectedEnv.getFirst(), existingSdks, true, module, detectedEnv.getSecond());
@@ -492,7 +492,10 @@ public final class PyInterpreterInspection extends PyInspection {
             CommonBundle.message("button.without.mnemonic.yes"),
             CommonBundle.message("button.without.mnemonic.no")
           )
-        ).asWarning().ask(project);
+        )
+          .asWarning()
+          .doNotAsk(TrustedProjects.createDoNotAskOptionForHost(project))
+          .ask(project);
 
         TrustedProjects.setTrusted(project, confirmation);
 
