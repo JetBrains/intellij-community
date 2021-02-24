6bd9cc59cf5e4ccedec4d20133e929517bbfd87f
Alexander Udalov <alexander.udalov@jetbrains.com>
2021-02-24 21:24:10 +0100

    Fix warnings in Java code in tests-common
    Original commit: 5d6aa01086f9885151ce697f6892d5fcf346ad17
    
    GitOrigin-RevId: ef16de58fe076b0333f0abdff6c7422d730eb8a4

--- plugins/kotlin/tests-common/test/org/jetbrains/kotlin/test/testFramework/KtUsefulTestCase.java
+++ plugins/kotlin/tests-common/test/org/jetbrains/kotlin/test/testFramework/KtUsefulTestCase.java
@@ -4,15 +4,14 @@ package org.jetbrains.kotlin.test.testFramework;
 
 import com.intellij.codeInsight.CodeInsightSettings;
 import com.intellij.diagnostic.PerformanceWatcher;
+import com.intellij.ide.highlighter.JavaFileType;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.PathManager;
 import com.intellij.openapi.application.ex.ApplicationManagerEx;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.fileTypes.StdFileTypes;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.IconLoader;
 import com.intellij.openapi.util.JDOMUtil;
@@ -333,7 +332,7 @@ public abstract class KtUsefulTestCase extends TestCase {
                     }
                 },
                 () -> {
-                    currentCodeStyleSettings.getIndentOptions(StdFileTypes.JAVA);
+                    currentCodeStyleSettings.getIndentOptions(JavaFileType.INSTANCE);
                     try {
                         checkCodeStyleSettingsEqual(oldCodeStyleSettings, currentCodeStyleSettings);
                     }
@@ -903,7 +902,7 @@ public abstract class KtUsefulTestCase extends TestCase {
         }
         String expected = StringUtil.convertLineSeparators(trimBeforeComparing ? fileText.trim() : fileText);
         String actual = StringUtil.convertLineSeparators(trimBeforeComparing ? actualText.trim() : actualText);
-        if (!Comparing.equal(expected, actual)) {
+        if (!Objects.equals(expected, actual)) {
             throw new FileComparisonFailure(messageProducer == null ? null : messageProducer.get(), expected, actual, filePath);
         }
     }
