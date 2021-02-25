1677298a0bb2dae7bb0560513d09cbfa7ab84e19
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2021-02-26 00:22:31 +0100

    [ui] opening all non-navigational "what's new" links in external browser
    (cherry picked from commit a573003f3a328e04abfb6614a7ab4dd2742db82d)
    
    IJ-CR-6312
    
    GitOrigin-RevId: 278f41b776234ea0d48cecfb7b0e916e973f32c3

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/HTMLFileEditor.kt
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/HTMLFileEditor.kt
@@ -23,6 +23,8 @@ import org.cef.browser.CefBrowser
 import org.cef.browser.CefFrame
 import org.cef.handler.CefLifeSpanHandlerAdapter
 import org.cef.handler.CefLoadHandlerAdapter
+import org.cef.handler.CefRequestHandlerAdapter
+import org.cef.network.CefRequest
 import java.awt.BorderLayout
 import java.beans.PropertyChangeListener
 import javax.swing.JComponent
@@ -62,6 +64,12 @@ internal class HTMLFileEditor(private val file: LightVirtualFile, private val ur
           }
         }
       }
+    }, contentPanel.cefBrowser)
+
+    contentPanel.jbCefClient.addRequestHandler(object : CefRequestHandlerAdapter() {
+      override fun onBeforeBrowse(browser: CefBrowser, frame: CefFrame, request: CefRequest, userGesture: Boolean, isRedirect: Boolean): Boolean =
+        if (userGesture) { BrowserUtil.browse(request.url); true }
+        else false
     }, contentPanel.cefBrowser)
 
     contentPanel.jbCefClient.addLifeSpanHandler(object : CefLifeSpanHandlerAdapter() {
