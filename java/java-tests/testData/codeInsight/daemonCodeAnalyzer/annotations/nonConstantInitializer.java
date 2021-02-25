7c9f25434060a3d6359df00c929417cc19359c47
Anna Kozlova <anna.kozlova@jetbrains.com>
2021-02-25 12:06:48 +0100

    java: reject non-TypeName qualifiers before constant field refs (IDEA-262804)
    GitOrigin-RevId: 1aa7a1dbc1c24ad5d4af5c271f454aaa92cefcde

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/annotations/nonConstantInitializer.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/annotations/nonConstantInitializer.java
@@ -6,7 +6,10 @@
 
 class D {
     int field;
+    final int field1 = 1;
     @Ann(i=<error descr="Attribute value must be constant">field</error>) void foo () {}
+    @Ann(i=<error descr="Attribute value must be constant">this.field1</error>) void foo1 () {}
+    @Ann(i=field1, j = {}) void foo2 () {}
 
     @Ann(j={<error descr="Attribute value must be constant">null</error>}) void bar() {}
 }
