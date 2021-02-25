5f5a6c6c52b9dc4751b66838aafb60afdc54efd2
Alexey Kalina <alexey.kalina@jetbrains.com>
2021-02-25 16:09:54 +0300

    [ml-local-models] process files in silent mode in single thread
    GitOrigin-RevId: 8f33fc3b32c2b810eac52321c6acdf3450479891

--- plugins/ml-local-models/src/com/intellij/ml/local/models/LocalModelsTraining.kt
+++ plugins/ml-local-models/src/com/intellij/ml/local/models/LocalModelsTraining.kt
@@ -1,34 +1,24 @@
 package com.intellij.ml.local.models
 
 import com.intellij.lang.Language
-import com.intellij.ml.local.MlLocalModelsBundle
 import com.intellij.ml.local.models.api.LocalModelBuilder
 import com.intellij.ml.local.models.api.LocalModelFactory
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.fileTypes.FileType
-import com.intellij.openapi.progress.ProgressIndicator
-import com.intellij.openapi.progress.ProgressManager
-import com.intellij.openapi.progress.Task
-import com.intellij.openapi.progress.impl.BackgroundableProcessIndicator
-import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.roots.ContentIterator
+import com.intellij.openapi.roots.ProjectFileIndex
+import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.openapi.vfs.VirtualFileWithId
 import com.intellij.psi.PsiManager
 import com.intellij.psi.impl.source.resolve.ResolveCache
-import com.intellij.psi.search.FileTypeIndex
-import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.util.indexing.FileBasedIndex
-import java.lang.IllegalArgumentException
-import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
-import java.util.concurrent.atomic.AtomicInteger
+import kotlin.system.measureTimeMillis
 
 
 object LocalModelsTraining {
   private val LOG = logger<LocalModelsTraining>()
-  private const val FILES_CHUNK_SIZE = 100
 
   @Volatile private var isTraining = false
 
@@ -42,80 +32,57 @@ object LocalModelsTraining {
     }
     isTraining = true
     val modelsManager = LocalModelsManager.getInstance(project)
-    val task = object : Task.Backgroundable(project, MlLocalModelsBundle.message("ml.local.models.training.title"), true) {
-      override fun run(indicator: ProgressIndicator) {
-        val files = getFiles(project, fileType)
-        val factories = LocalModelFactory.forLanguage(language)
-        val id2builder = factories.associate { it.id to it.modelBuilder(project, language) }
-        id2builder.forEach { it.value.onStarted() }
-        val ids = id2builder.keys.joinToString(", ")
-        LOG.info("Local models training started for language ${language.id} and models: $ids")
-        processFiles(files, id2builder, project, indicator)
-        id2builder.forEach { it.value.onFinished() }
-        LOG.info("Local models training finished for language ${language.id} and models: $ids")
-        id2builder.forEach {
-          it.value.build()?.let { model ->
-            modelsManager.registerModel(language, model)
-          }
-        }
-      }
-
-      override fun onFinished() {
-        isTraining = false
+    val files = getFiles(project, fileType)
+    val factories = LocalModelFactory.forLanguage(language)
+    val id2builder = factories.associate { it.id to it.modelBuilder(project, language) }
+    id2builder.forEach { it.value.onStarted() }
+    val ids = id2builder.keys.joinToString(", ")
+    LOG.info("Local models training started for language ${language.id} and models: $ids")
+    val ms = measureTimeMillis {
+      processFiles(files, id2builder, project)
+    }
+    id2builder.forEach { it.value.onFinished() }
+    LOG.info("Local models training finished for language ${language.id} and models: $ids. Duration: $ms ms")
+    id2builder.forEach {
+      it.value.build()?.let { model ->
+        modelsManager.registerModel(language, model)
       }
     }
-    ProgressManager.getInstance().runProcessWithProgressAsynchronously(task, BackgroundableProcessIndicator(task))
+    isTraining = false
   }
 
-  private fun processFiles(files: List<VirtualFile>, modelBuilders: Map<String, LocalModelBuilder>, project: Project,
-                           indicator: ProgressIndicator) {
-    val dumbService = DumbService.getInstance(project)
+  private fun processFiles(files: Set<Int>, modelBuilders: Map<String, LocalModelBuilder>, project: Project) {
     val psiManager = PsiManager.getInstance(project)
     val resolveCache = ResolveCache.getInstance(project)
-    val executorService = Executors.newFixedThreadPool((Runtime.getRuntime().availableProcessors() - 1).coerceAtLeast(1))
-    indicator.isIndeterminate = false
-    indicator.text = MlLocalModelsBundle.message("ml.local.models.training.files.processing")
-    indicator.fraction = 0.0
-    val processed = AtomicInteger(0)
-
-    for (filesChunk in files.chunked(FILES_CHUNK_SIZE)) {
-      executorService.submit {
-        dumbService.runReadActionInSmartMode {
-          for (file in filesChunk) {
-            psiManager.findFile(file)?.let { psiFile ->
-              for (id2builder in modelBuilders) {
-                try {
-                  psiFile.accept(id2builder.value.fileVisitor())
-                }
-                catch (e: Throwable) {
-                  LOG.error("Local model training error. Model: ${id2builder.key}. File: ${file.path}.", e)
-                }
-              }
-              resolveCache.clearCache(true)
+    for (fileId in files) {
+      runReadAction {
+        val file = VirtualFileManager.getInstance().findFileById(fileId)
+        psiManager.findFile(file)?.let { psiFile ->
+          for (id2builder in modelBuilders) {
+            try {
+              psiFile.accept(id2builder.value.fileVisitor())
             }
-            indicator.fraction = processed.incrementAndGet().toDouble() / files.size
+            catch (e: Throwable) {
+              LOG.warn("Local model training error. Model: ${id2builder.key}. File: ${file.path}.", e)
+            }
           }
+          resolveCache.clearCache(true)
         }
       }
-    }
-    executorService.shutdown()
-
-    try {
-      while (true) {
-        if (executorService.awaitTermination(1, TimeUnit.SECONDS)) {
-          break
-        }
-        ProgressManager.checkCanceled()
-      }
-    }
-    finally {
-      executorService.shutdownNow()
     }
   }
 
-  fun getFiles(project: Project, fileType: FileType): List<VirtualFile> = runReadAction {
-    FileBasedIndex.getInstance().getContainingFiles(FileTypeIndex.NAME,
-                                                    fileType,
-                                                    GlobalSearchScope.projectScope(project)).toList()
+  private fun getFiles(project: Project, fileType: FileType): Set<Int> {
+    val index = ProjectFileIndex.getInstance(project)
+    val fileIds = mutableSetOf<Int>()
+    runReadAction {
+      index.iterateContent(ContentIterator { file ->
+        if (file is VirtualFileWithId && file.fileType == fileType) {
+          fileIds.add(file.id)
+        }
+        true
+      })
+    }
+    return fileIds
   }
 }
