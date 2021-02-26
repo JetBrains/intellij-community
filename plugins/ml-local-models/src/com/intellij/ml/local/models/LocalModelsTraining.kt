eb96aa5b23be187377e28a8193d218c2913d7e6a
Alexey Kalina <alexey.kalina@jetbrains.com>
2021-02-26 15:15:25 +0300

    [ml-local-models] rollback to using progress indicator during processing files
    GitOrigin-RevId: be2243764c8f1d9b27508588b07e23cf175894c8

--- plugins/ml-local-models/src/com/intellij/ml/local/models/LocalModelsTraining.kt
+++ plugins/ml-local-models/src/com/intellij/ml/local/models/LocalModelsTraining.kt
@@ -1,12 +1,17 @@
 package com.intellij.ml.local.models
 
 import com.intellij.lang.Language
+import com.intellij.ml.local.MlLocalModelsBundle
 import com.intellij.ml.local.models.api.LocalModelBuilder
 import com.intellij.ml.local.models.api.LocalModelFactory
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.fileTypes.FileType
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.Task
+import com.intellij.openapi.progress.impl.BackgroundableProcessIndicator
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ContentIterator
 import com.intellij.openapi.roots.ProjectFileIndex
@@ -14,6 +19,8 @@ import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.openapi.vfs.VirtualFileWithId
 import com.intellij.psi.PsiManager
 import com.intellij.psi.impl.source.resolve.ResolveCache
+import java.lang.IllegalArgumentException
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.system.measureTimeMillis
 
 
@@ -32,28 +39,41 @@ object LocalModelsTraining {
     }
     isTraining = true
     val modelsManager = LocalModelsManager.getInstance(project)
-    val files = getFiles(project, fileType)
-    val factories = LocalModelFactory.forLanguage(language)
-    val id2builder = factories.associate { it.id to it.modelBuilder(project, language) }
-    id2builder.forEach { it.value.onStarted() }
-    val ids = id2builder.keys.joinToString(", ")
-    LOG.info("Local models training started for language ${language.id} and models: $ids")
-    val ms = measureTimeMillis {
-      processFiles(files, id2builder, project)
-    }
-    id2builder.forEach { it.value.onFinished() }
-    LOG.info("Local models training finished for language ${language.id} and models: $ids. Duration: $ms ms")
-    id2builder.forEach {
-      it.value.build()?.let { model ->
-        modelsManager.registerModel(language, model)
+    val task = object : Task.Backgroundable(project, MlLocalModelsBundle.message("ml.local.models.training.title"), true) {
+      override fun run(indicator: ProgressIndicator) {
+        val files = getFiles(project, fileType)
+        val factories = LocalModelFactory.forLanguage(language)
+        val id2builder = factories.associate { it.id to it.modelBuilder(project, language) }
+        id2builder.forEach { it.value.onStarted() }
+        val ids = id2builder.keys.joinToString(", ")
+        LOG.info("Local models training started for language ${language.id} and models: $ids")
+        val ms = measureTimeMillis {
+          processFiles(files, id2builder, project, indicator)
+        }
+        id2builder.forEach { it.value.onFinished() }
+        LOG.info("Local models training finished for language ${language.id} and models: $ids. Duration: $ms ms")
+        id2builder.forEach {
+          it.value.build()?.let { model ->
+            modelsManager.registerModel(language, model)
+          }
+        }
+      }
+
+      override fun onFinished() {
+        isTraining = false
       }
     }
-    isTraining = false
+    ProgressManager.getInstance().runProcessWithProgressAsynchronously(task, BackgroundableProcessIndicator(task))
   }
 
-  private fun processFiles(files: Set<Int>, modelBuilders: Map<String, LocalModelBuilder>, project: Project) {
+  private fun processFiles(files: Set<Int>, modelBuilders: Map<String, LocalModelBuilder>, project: Project,
+                           indicator: ProgressIndicator) {
     val psiManager = PsiManager.getInstance(project)
     val resolveCache = ResolveCache.getInstance(project)
+    indicator.isIndeterminate = false
+    indicator.text = MlLocalModelsBundle.message("ml.local.models.training.files.processing")
+    indicator.fraction = 0.0
+    val processed = AtomicInteger(0)
     for (fileId in files) {
       runReadAction {
         val file = VirtualFileManager.getInstance().findFileById(fileId)
@@ -69,6 +89,7 @@ object LocalModelsTraining {
           resolveCache.clearCache(true)
         }
       }
+      indicator.fraction = processed.incrementAndGet().toDouble() / files.size
     }
   }
 
