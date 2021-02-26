83faed1d0b875c19b8c14020f578f54e8d2bc5b7
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2021-02-26 13:38:27 +0300

    Execution environment provider refactoring
    (cherry picked from commit 21e25f649830b192261e6a90ffa9898eba3de990)
    
    IJ-CR-6657
    
    (cherry picked from commit 830a63274843f6541ba721a84912327c0c727a8a)
    
    IJ-CR-6682
    
    GitOrigin-RevId: 93389bb4a5bc08f4c070219997eef2742977f2b2

--- platform/lang-impl/src/com/intellij/task/impl/ExecutionEnvironmentProviderImpl.kt
+++ platform/lang-impl/src/com/intellij/task/impl/ExecutionEnvironmentProviderImpl.kt
@@ -1,82 +1,75 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.task.impl;
+package com.intellij.task.impl
 
-import com.intellij.execution.ExecutionTarget;
-import com.intellij.execution.Executor;
-import com.intellij.execution.RunnerAndConfigurationSettings;
-import com.intellij.execution.configurations.*;
-import com.intellij.execution.impl.ExecutionManagerImpl;
-import com.intellij.execution.runners.ExecutionEnvironment;
-import com.intellij.execution.runners.ExecutionEnvironmentProvider;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.progress.ProcessCanceledException;
-import com.intellij.openapi.project.Project;
-import com.intellij.task.ExecuteRunConfigurationTask;
-import com.intellij.task.ProjectTaskRunner;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.execution.ExecutionTarget
+import com.intellij.execution.Executor
+import com.intellij.execution.RunnerAndConfigurationSettings
+import com.intellij.execution.configurations.*
+import com.intellij.execution.impl.ExecutionManagerImpl.Companion.setDelegatedRunProfile
+import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.execution.runners.ExecutionEnvironmentProvider
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.progress.ProcessCanceledException
+import com.intellij.openapi.project.Project
+import com.intellij.task.ExecuteRunConfigurationTask
+import com.intellij.task.ProjectTaskRunner
 
 /**
  * @author Vladislav.Soroka
  */
-public final class ExecutionEnvironmentProviderImpl implements ExecutionEnvironmentProvider {
-  private static final Logger LOG = Logger.getInstance(ExecutionEnvironmentProvider.class);
-
-  @Nullable
-  @Override
-  public ExecutionEnvironment createExecutionEnvironment(@NotNull Project project,
-                                                         @NotNull RunProfile runProfile,
-                                                         @NotNull Executor executor,
-                                                         @NotNull ExecutionTarget target,
-                                                         @Nullable RunnerSettings runnerSettings,
-                                                         @Nullable ConfigurationPerRunnerSettings configurationSettings,
-                                                         @Nullable RunnerAndConfigurationSettings settings) {
-    ExecuteRunConfigurationTask
-      runTask = new ExecuteRunConfigurationTaskImpl(runProfile, target, runnerSettings, configurationSettings, settings);
-    ExecutionEnvironment environment = ProjectTaskRunner.EP_NAME.computeSafeIfAny(projectTaskRunner -> {
+class ExecutionEnvironmentProviderImpl : ExecutionEnvironmentProvider {
+  override fun createExecutionEnvironment(project: Project,
+                                          runProfile: RunProfile,
+                                          executor: Executor,
+                                          target: ExecutionTarget,
+                                          runnerSettings: RunnerSettings?,
+                                          configurationSettings: ConfigurationPerRunnerSettings?,
+                                          settings: RunnerAndConfigurationSettings?): ExecutionEnvironment? {
+    val runTask: ExecuteRunConfigurationTask =
+      ExecuteRunConfigurationTaskImpl(runProfile, target, runnerSettings, configurationSettings, settings)
+    val environment = ProjectTaskRunner.EP_NAME.computeSafeIfAny {
       try {
-        if (projectTaskRunner.canRun(project, runTask)) {
-          return projectTaskRunner.createExecutionEnvironment(project, runTask, executor);
+        if (it.canRun(project, runTask)) {
+          return@computeSafeIfAny it.createExecutionEnvironment(project, runTask, executor)
         }
       }
-      catch (ProcessCanceledException e) {
-        throw e;
+      catch (e: ProcessCanceledException) {
+        throw e
       }
-      catch (Exception e) {
-        LOG.error("Broken project task runner: " + projectTaskRunner.getClass().getName(), e);
+      catch (e: Exception) {
+        LOG.error("Broken project task runner: " + it.javaClass.name, e)
       }
-
-      return null;
-    });
-    if (environment != null) {
-      RunProfile environmentRunProfile = environment.getRunProfile();
-      ExecutionManagerImpl.setDelegatedRunProfile(environmentRunProfile, runProfile);
-      copySettings(settings, environment);
-      copyCommonRunProfileOptions(runProfile, environmentRunProfile);
-    }
-    return environment;
+      null
+    } ?: return null
+    val environmentRunProfile = environment.runProfile
+    setDelegatedRunProfile(environmentRunProfile, runProfile)
+    copySettings(settings, environment)
+    copyCommonRunProfileOptions(runProfile, environmentRunProfile)
+    return environment
   }
 
-  private static void copySettings(@Nullable RunnerAndConfigurationSettings settings, @NotNull ExecutionEnvironment environment) {
-    if (settings != null) {
-      RunnerAndConfigurationSettings environmentSettings = environment.getRunnerAndConfigurationSettings();
-      if (environmentSettings != null && environmentSettings != settings) {
-        environmentSettings.setActivateToolWindowBeforeRun(settings.isActivateToolWindowBeforeRun());
-        environmentSettings.setEditBeforeRun(settings.isEditBeforeRun());
+  companion object {
+    private val LOG = logger<ExecutionEnvironmentProvider>()
+
+    private fun copySettings(settings: RunnerAndConfigurationSettings?, environment: ExecutionEnvironment) {
+      if (settings == null) return
+      val environmentSettings = environment.runnerAndConfigurationSettings
+      if (environmentSettings != null && environmentSettings !== settings) {
+        environmentSettings.isActivateToolWindowBeforeRun = settings.isActivateToolWindowBeforeRun
+        environmentSettings.isEditBeforeRun = settings.isEditBeforeRun
       }
     }
-  }
 
-  private static void copyCommonRunProfileOptions(@NotNull RunProfile runProfile, @NotNull RunProfile environmentRunProfile) {
-    if (environmentRunProfile instanceof RunConfiguration && runProfile instanceof RunConfiguration) {
-      ((RunConfiguration)environmentRunProfile).setAllowRunningInParallel(((RunConfiguration)runProfile).isAllowRunningInParallel());
-    }
-
-    if (environmentRunProfile instanceof RunConfigurationBase && runProfile instanceof RunConfigurationBase) {
-      for (LogFileOptions logFile : ((RunConfigurationBase<?>)runProfile).getLogFiles()) {
-        if (!((RunConfigurationBase<?>)environmentRunProfile).getLogFiles().contains(logFile)) {
-          ((RunConfigurationBase<?>)environmentRunProfile).addLogFile(logFile.getPathPattern(), logFile.getName(), logFile.isEnabled(),
-                                                                      logFile.isSkipContent(), logFile.isShowAll());
+    private fun copyCommonRunProfileOptions(runProfile: RunProfile, environmentRunProfile: RunProfile) {
+      if (environmentRunProfile is RunConfiguration && runProfile is RunConfiguration) {
+        environmentRunProfile.isAllowRunningInParallel = runProfile.isAllowRunningInParallel
+      }
+      if (environmentRunProfile is RunConfigurationBase<*> && runProfile is RunConfigurationBase<*>) {
+        for (logFile in runProfile.logFiles) {
+          if (!environmentRunProfile.logFiles.contains(logFile)) {
+            environmentRunProfile.addLogFile(logFile.pathPattern, logFile.name, logFile.isEnabled,
+                                             logFile.isSkipContent, logFile.isShowAll)
+          }
         }
       }
     }
