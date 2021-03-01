0f3f5992ea2a04ba572977ea46c5ba85daea7fd2
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2021-03-01 12:32:09 +0300

    Revert "Treat project as trusted if it has settings in the APP_CONFIG/workspace: IDEA-262365"
    Reason: IDEA-262917 Untrusted project gets trusted after reopening (unless explicitly set to NO).
    
    This reverts commit 2397ae0c
    
    GitOrigin-RevId: bad91a51e362653d497f0f2cd1473173e510ed5c

--- platform/configuration-store-impl/src/ProjectStoreBase.kt
+++ platform/configuration-store-impl/src/ProjectStoreBase.kt
@@ -3,8 +3,6 @@ package com.intellij.configurationStore
 
 import com.intellij.ide.highlighter.ProjectFileType
 import com.intellij.ide.highlighter.WorkspaceFileType
-import com.intellij.ide.impl.getTrustedState
-import com.intellij.ide.impl.setTrusted
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.application.appSystemDir
@@ -19,7 +17,6 @@ import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.SmartList
-import com.intellij.util.ThreeState
 import com.intellij.util.containers.isNullOrEmpty
 import com.intellij.util.io.Ksuid
 import com.intellij.util.io.exists
@@ -162,15 +159,8 @@ abstract class ProjectStoreBase(final override val project: Project) : Component
       projectIdManager.state.id = projectId
     }
 
-    val productWorkspaceFile = productSpecificWorkspaceParentDir.resolve("$projectId.xml")
-    macros.add(Macro(StoragePathMacros.PRODUCT_WORKSPACE_FILE, productWorkspaceFile))
+    macros.add(Macro(StoragePathMacros.PRODUCT_WORKSPACE_FILE, productSpecificWorkspaceParentDir.resolve("$projectId.xml")))
     storageManager.setMacros(macros)
-
-    val state = project.getTrustedState()
-    if (state == ThreeState.UNSURE && productWorkspaceFile.exists()) {
-      LOG.info("Marked the project as trusted because there are settings in $productWorkspaceFile")
-      project.setTrusted(true)
-    }
   }
 
   override fun <T> getStorageSpecs(component: PersistentStateComponent<T>, stateSpec: State, operation: StateStorageOperation): List<Storage> {
