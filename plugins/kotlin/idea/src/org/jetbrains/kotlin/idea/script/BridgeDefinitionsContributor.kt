cc27a2e133f6e77d16f4aa1968530ad9b68bad64
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-03-01 08:55:26 +0700

    BridgeDefinitionsContributor: remove deprecated warning about Extensions.getArea()
    GitOrigin-RevId: 5f950fa812adb7a40e2b006334b517e027e91ddc

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/script/BridgeDefinitionsContributor.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/script/BridgeDefinitionsContributor.kt
@@ -1,12 +1,11 @@
 /*
- * Copyright 2010-2018 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.idea.script
 
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.extensions.Extensions
 import com.intellij.openapi.project.Project
 import org.jetbrains.kotlin.idea.core.script.ScriptDefinitionSourceAsContributor
 import org.jetbrains.kotlin.idea.core.script.loadDefinitionsFromTemplates
@@ -16,39 +15,41 @@ import kotlin.script.experimental.api.ScriptDiagnostic
 import kotlin.script.experimental.api.ScriptEvaluationConfiguration
 import kotlin.script.experimental.intellij.ScriptDefinitionsProvider
 import kotlin.script.experimental.jvm.defaultJvmScriptingHostConfiguration
+import kotlin.streams.asSequence
 
 class BridgeScriptDefinitionsContributor(private val project: Project) : ScriptDefinitionSourceAsContributor {
     override val id: String = "BridgeScriptDefinitionsContributor"
 
     override val definitions: Sequence<ScriptDefinition>
-        get() {
-            val extensions = Extensions.getArea(project).getExtensionPoint(ScriptDefinitionsProvider.EP_NAME).extensions
-            return extensions.asSequence().flatMap { provider ->
-                val explicitClasses = provider.getDefinitionClasses().toList()
-                val classPath = provider.getDefinitionsClassPath().toList()
-                val baseHostConfiguration = defaultJvmScriptingHostConfiguration
-                // TODO: rewrite load and discovery to return kotlin.script.experimental.host.ScriptDefinition to avoid unnecessary conversions
-                val explicitDefinitions =
-                    if (explicitClasses.isEmpty()) emptySequence()
-                    else loadDefinitionsFromTemplates(explicitClasses, classPath, baseHostConfiguration).asSequence()
-                val discoveredDefinitions =
-                    if (provider.useDiscovery())
-                        ScriptDefinitionsFromClasspathDiscoverySource(
-                            classPath,
-                            baseHostConfiguration,
-                            ::loggingReporter
-                        ).definitions
-                    else emptySequence()
-                val loadedDefinitions = (explicitDefinitions + discoveredDefinitions).map {
-                    kotlin.script.experimental.host.ScriptDefinition(
-                        it.compilationConfiguration,
-                        it.evaluationConfiguration ?: ScriptEvaluationConfiguration.Default
-                    )
-                }.toList()
-                provider.provideDefinitions(baseHostConfiguration, loadedDefinitions).map {
-                    ScriptDefinition.FromNewDefinition(baseHostConfiguration, it)
-                }.asSequence()
-            }
+        get() = ScriptDefinitionsProvider.EP_NAME.extensions(project).asSequence().flatMap { provider ->
+            val explicitClasses = provider.getDefinitionClasses().toList()
+            val classPath = provider.getDefinitionsClassPath().toList()
+            val baseHostConfiguration = defaultJvmScriptingHostConfiguration
+            // TODO: rewrite load and discovery to return kotlin.script.experimental.host.ScriptDefinition to avoid unnecessary conversions
+            val explicitDefinitions = if (explicitClasses.isEmpty())
+                emptySequence()
+            else
+                loadDefinitionsFromTemplates(explicitClasses, classPath, baseHostConfiguration).asSequence()
+
+            val discoveredDefinitions = if (provider.useDiscovery())
+                ScriptDefinitionsFromClasspathDiscoverySource(
+                    classPath,
+                    baseHostConfiguration,
+                    ::loggingReporter
+                ).definitions
+            else
+                emptySequence()
+
+            val loadedDefinitions = (explicitDefinitions + discoveredDefinitions).map {
+                kotlin.script.experimental.host.ScriptDefinition(
+                    it.compilationConfiguration,
+                    it.evaluationConfiguration ?: ScriptEvaluationConfiguration.Default,
+                )
+            }.toList()
+
+            provider.provideDefinitions(baseHostConfiguration, loadedDefinitions).map {
+                ScriptDefinition.FromNewDefinition(baseHostConfiguration, it)
+            }.asSequence()
         }
 }
 
