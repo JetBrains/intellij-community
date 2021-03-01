faf8a9ad7fa4d5d2e81e7a41986617dd5e492b17
Vladislav Ertel <Vladislav.Ertel@jetbrains.com>
2021-03-02 02:19:04 +0300

    Move runtime dialog to RuntimeChooserUtil.kt
    GitOrigin-RevId: ee9fe0f6a10efe34d082616f4fc8bd0ae53976a8

--- platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/jdkDownloader/RuntimeChooser.kt
+++ platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/jdkDownloader/RuntimeChooser.kt
@@ -3,24 +3,10 @@ package com.intellij.openapi.projectRoots.impl.jdkDownloader
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.components.service
 
 
 class RuntimeChooserAction : AnAction() {
   override fun actionPerformed(e: AnActionEvent) {
-    val model = RuntimeChooserModel()
-
-    model.fetchAvailableJbrs()
-    model.fetchCurrentRuntime()
-
-    val result = RuntimeChooserDialog(null, model).showDialogAndGetResult()
-
-    @Suppress("MoveVariableDeclarationIntoWhen")
-    return when(result) {
-      is RuntimeChooserDialogResult.Cancel -> Unit
-      is RuntimeChooserDialogResult.UseDefault -> service<RuntimeChooserPaths>().resetCustomJdk()
-      is RuntimeChooserDialogResult.UseCustomJdk -> service<RuntimeChooserPaths>().installCustomJdk(result.name, result.path)
-      is RuntimeChooserDialogResult.DownloadAndUse -> service<RuntimeChooserDownloader>().downloadAndUse(result.item, result.path)
-    }
+    RuntimeChooserUtil.showRuntimeChooserPopup()
   }
 }
