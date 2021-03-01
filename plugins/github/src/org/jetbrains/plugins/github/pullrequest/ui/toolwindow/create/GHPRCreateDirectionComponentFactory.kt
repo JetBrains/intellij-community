f149fd35cef746fd06e3accd7970c55cc63aad19
Ivan Semenov <ivan.semenov@jetbrains.com>
2021-03-01 14:25:53 +0300

    [github] use natural sorting for branches
    Fixes IDEA-262615
    
    GitOrigin-RevId: 171f015eca6f8fb67fad9248279f92a0ac5cf471

--- plugins/github/src/org/jetbrains/plugins/github/pullrequest/ui/toolwindow/create/GHPRCreateDirectionComponentFactory.kt
+++ plugins/github/src/org/jetbrains/plugins/github/pullrequest/ui/toolwindow/create/GHPRCreateDirectionComponentFactory.kt
@@ -7,6 +7,7 @@ import com.intellij.openapi.ui.ComboBox
 import com.intellij.openapi.ui.popup.JBPopup
 import com.intellij.openapi.ui.popup.JBPopupFactory
 import com.intellij.openapi.util.NlsSafe
+import com.intellij.openapi.util.text.StringUtil
 import com.intellij.ui.CollectionComboBoxModel
 import com.intellij.ui.ComboboxSpeedSearch
 import com.intellij.ui.MutableCollectionComboBoxModel
@@ -113,7 +114,7 @@ class GHPRCreateDirectionComponentFactory(private val repositoriesManager: GHPro
       val branches = currentRepo.gitRemote.repository.branches.remoteBranches.filter {
         it.remote == remote
       }
-      replaceAll(branches.toList())
+      replaceAll(branches.sortedWith(BRANCHES_COMPARATOR))
       selectedItem = currentBranch.takeIf { it != null && branches.contains(it) }
     }
 
@@ -227,12 +228,13 @@ class GHPRCreateDirectionComponentFactory(private val repositoriesManager: GHPro
       it.remote == remote
     }
 
-    val branches = repo.branches.localBranches.sorted() + remoteBranches.sorted()
+    val branches = repo.branches.localBranches.sortedWith(BRANCHES_COMPARATOR) + remoteBranches.sortedWith(BRANCHES_COMPARATOR)
     branchModel.replaceAll(branches)
     branchModel.selectedItem = repo.currentBranch
   }
 
   companion object {
+    private val BRANCHES_COMPARATOR = Comparator<GitBranch> { b1, b2 -> StringUtil.naturalCompare(b1.name, b2.name) }
 
     @NlsSafe
     private fun getRepoText(repo: GHRepositoryPath?, showOwner: Boolean, branch: GitBranch?): String {
