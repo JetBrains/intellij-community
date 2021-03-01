22938829a1d5199f414bcf8416ffbe832ab8fa76
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2021-03-01 19:30:13 +0100

    NationalKeyboardSupport - do not write defaults
    GitOrigin-RevId: 8cf8e6c8437f2b41bee81773281f8a09079fed05

--- platform/platform-api/src/com/intellij/openapi/keymap/NationalKeyboardSupport.java
+++ platform/platform-api/src/com/intellij/openapi/keymap/NationalKeyboardSupport.java
@@ -1,11 +1,11 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.keymap;
 
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
-import com.intellij.openapi.util.SystemInfo;
+import com.intellij.openapi.util.SystemInfoRt;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -13,23 +13,28 @@ import java.awt.*;
 import java.awt.im.InputContext;
 import java.util.Locale;
 
-@State(name = "KeyboardSettings", storages = @Storage("keyboard.xml"))
+@State(name = "KeyboardSettings", storages = {
+  @Storage("ui.lnf.xml"),
+  @Storage(value = "keyboard.xml", deprecated = true)
+})
 public final class NationalKeyboardSupport implements PersistentStateComponent<NationalKeyboardSupport.OptionSet> {
   private static final String[] MAC_SUPPORTED_LOCALES = {"de", "fr", "it", "no", "sk"};
   private static final String[] WIN_SUPPORTED_LOCALES = {"be", "ru", "uk", "bg", "sr"};
 
   public static final String VMOption = getVMOption();
 
+  private boolean isEnabled = false;
+
   public static boolean isSupportedKeyboardLayout(@NotNull Component component) {
-    if (SystemInfo.isLinux) {
+    if (SystemInfoRt.isLinux) {
       return true;
     }
 
     String[] supportedNonEnglishLanguages;
-    if (SystemInfo.isMac) {
+    if (SystemInfoRt.isMac) {
       supportedNonEnglishLanguages = MAC_SUPPORTED_LOCALES;
     }
-    else if (SystemInfo.isWindows) {
+    else if (SystemInfoRt.isWindows) {
       supportedNonEnglishLanguages = WIN_SUPPORTED_LOCALES;
     }
     else {
@@ -46,14 +51,14 @@ public final class NationalKeyboardSupport implements PersistentStateComponent<N
   }
 
   public static @NotNull String getVMOption() {
-    if (SystemInfo.isMac || SystemInfo.isLinux) {
+    if (SystemInfoRt.isMac || SystemInfoRt.isLinux) {
       return "com.sun.awt.use.national.layouts";
     }
     return "com.sun.awt.useLatinNonAlphaNumKeycodes";
   }
 
   public static @NotNull String getKeymapBundleKey() {
-    if (SystemInfo.isMac || SystemInfo.isLinux) {
+    if (SystemInfoRt.isMac || SystemInfoRt.isLinux) {
       return "use.national.layouts.for.shortcuts";
     }
     return "use.us.non.alpha.num.keys";
@@ -67,13 +72,13 @@ public final class NationalKeyboardSupport implements PersistentStateComponent<N
   }
 
   @Nullable
-  protected static Locale getLocaleForComponent(@NotNull Component component) {
+  private static Locale getLocaleForComponent(@NotNull Component component) {
     final InputContext context = component.getInputContext();
     return context == null ? null : context.getLocale();
   }
 
   public static final class OptionSet {
-    public boolean enabled = "true".equals(System.getProperty(VMOption));
+    public boolean enabled;
   }
 
   private OptionSet myOptions = new OptionSet();
@@ -95,25 +100,22 @@ public final class NationalKeyboardSupport implements PersistentStateComponent<N
 
   @Override
   public void loadState(@NotNull OptionSet state) {
-    state.enabled
-      = state.enabled || "true".equals(System.getProperty(VMOption));
-
+    isEnabled = state.enabled || "true".equals(System.getProperty(VMOption));
     myOptions = state;
   }
 
   @Override
   public void noStateLoaded() {
     // on MacOS national keymap support is turned on by default
-    if (SystemInfo.isMac) {
-      myOptions.enabled = "true".equals(System.getProperty(VMOption, "true"));
-    }
+    isEnabled = SystemInfoRt.isMac && "true".equals(System.getProperty(VMOption, "true"));
   }
 
   public boolean getEnabled() {
-    return myOptions.enabled;
+    return isEnabled;
   }
 
   public void setEnabled(boolean enabled) {
+    isEnabled = enabled;
     myOptions.enabled = enabled;
   }
 }
