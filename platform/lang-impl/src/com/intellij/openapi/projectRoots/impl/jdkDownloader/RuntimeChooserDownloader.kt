f642497eeb196d3bf07f5fc8ed411ce7e1f53e79
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2021-03-02 11:55:08 +0100

    runtime-chooser: IDEA-261869 - simplify notifications flow, use backgroundable progress & modal messages
    (cherry picked from commit 34d49281de239c3919a6746850a4dd4c613aaf26)
    
    IJ-CR-6490
    
    GitOrigin-RevId: bd5ba14b326f189694718c14ef8df51f60853619

--- platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/jdkDownloader/RuntimeChooserDownloader.kt
+++ platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/jdkDownloader/RuntimeChooserDownloader.kt
@@ -5,6 +5,7 @@ import com.intellij.lang.LangBundle
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.util.SystemInfo
@@ -14,26 +15,20 @@ import java.nio.file.Paths
 
 @Service(Service.Level.APP)
 class RuntimeChooserDownloader {
-  companion object {
-    private val LOG = logger<RuntimeChooserDownloader>()
-  }
-
-  fun downloadAndUse(jdk: JdkItem, targetDir: Path) {
-    val title = LangBundle.message("progress.title.choose.ide.runtime.downloading.jdk", jdk.fullPresentationText)
-    object: Task.Backgroundable(null, title, true, ALWAYS_BACKGROUND) {
-      override fun run(indicator: ProgressIndicator) {
-        try {
-          val installer = service<RuntimeChooserJbrInstaller>()
-          val request = installer.prepareJdkInstallation(jdk, targetDir)
-          installer.installJdk(request, indicator, null)
-
-          service<RuntimeChooserPaths>().installCustomJdk(request.item.fullPresentationText, request.installDir)
-        } catch (t: Throwable) {
-          LOG.warn("Failed to download boot runtime from $jdk. ${t.message}")
-          service<RuntimeChooserNotifications>().notifyDownloadFailed(jdk, t.localizedMessage)
-        }
-      }
-    }.queue()
+  fun downloadAndUse(indicator: ProgressIndicator, jdk: JdkItem, targetDir: Path): Path? {
+    try {
+      val installer = service<RuntimeChooserJbrInstaller>()
+      val request = installer.prepareJdkInstallation(jdk, targetDir)
+      installer.installJdk(request, indicator, null)
+      return request.installDir
+    }
+    catch (t: Throwable) {
+      thisLogger().warn("Failed to download boot runtime from $jdk. ${t.message}")
+      RuntimeChooserMessages.showErrorMessage(
+        LangBundle.message("dialog.message.choose.ide.runtime.download.error", jdk.fullPresentationText, targetDir.toString())
+      )
+      return null
+    }
   }
 }
 
--- platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/jdkDownloader/RuntimeChooserPaths.kt
+++ platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/jdkDownloader/RuntimeChooserPaths.kt
@@ -1,22 +1,17 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.projectRoots.impl.jdkDownloader
 
-import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.lang.LangBundle
 import com.intellij.openapi.application.ApplicationNamesInfo
 import com.intellij.openapi.application.PathManager
-import com.intellij.openapi.application.invokeLater
 import com.intellij.openapi.components.Service
-import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.Task
-import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.util.NlsSafe
 import com.intellij.openapi.util.SystemInfo
 import com.intellij.util.io.createDirectories
 import com.intellij.util.io.delete
-import com.intellij.util.io.exists
 import com.intellij.util.io.write
 import com.intellij.util.system.CpuArch
 import java.nio.file.Path
@@ -33,63 +28,58 @@ class RuntimeChooserPaths {
     return Paths.get(PathManager.getConfigPath(), configName)
   }
 
-  fun installCustomJdk(@NlsSafe name: String, suggestedHome: Path) = runWithProgress {
+  fun installCustomJdk(@NlsSafe jdkName: String,
+                       resolveSuggestedHome: (ProgressIndicator) -> Path?
+  ) = runWithProgress { indicator, jdkFile ->
+    val sdkHome = try {
+      resolveSuggestedHome(indicator)
+    }
+    catch (t: Throwable) {
+      LOG.warn("resolve failed. ${t.message}", t)
+      return@runWithProgress null
+    }
+
     val home = RuntimeChooserJreValidator.testNewJdkUnderProgress(
-      computeHomePath = { suggestedHome.toAbsolutePath().toString() },
+      computeHomePath = { sdkHome?.toAbsolutePath()?.toString() },
       callback = object : RuntimeChooserJreValidatorCallback<Path?> {
         override fun onSdkResolved(versionString: String, sdkHome: Path) = sdkHome
         override fun onError(message: String): Path? {
-          invokeLater { Messages.showErrorDialog(message, LangBundle.message("dialog.title.choose.ide.runtime")) }
+          RuntimeChooserMessages.showErrorMessage(message)
           return null
         }
-      }) ?: return@runWithProgress
+      }) ?: return@runWithProgress null
 
-    var jdkFileShadow : Path? = null
-    try {
-      val jdkFile = computeJdkFilePath()
-      jdkFileShadow = jdkFile
-
-      assertNotRunningFromSources()
-      jdkFile.parent?.createDirectories()
-      jdkFile.write(home.toAbsolutePath().toString())
-      LOG.warn("Set custom boot runtime to: $home in the $jdkFile. On errors, please remove the .jdk file")
-      service<RuntimeChooserNotifications>().notifyRuntimeChangeToCustomAndRestart(name)
-    } catch (t: Throwable) {
-      LOG.warn("Failed to set boot runtime to $home for $name in $jdkFileShadow. ${t.message}", t)
-      service<RuntimeChooserNotifications>().notifySettingBootJdkFailed(home, jdkFileShadow)
-    }
+    jdkFile.parent?.createDirectories()
+    jdkFile.write(home.toAbsolutePath().toString())
+    LOG.warn("Set custom boot runtime to: $home in the $jdkFile. On errors, please remove the .jdk file")
+    jdkName
   }
 
-  fun resetCustomJdk() = runWithProgress {
-    var jdkFileShadow : Path? = null
-    try {
-      val jdkFile = computeJdkFilePath()
-      jdkFileShadow = jdkFile
-      if (!jdkFile.exists()) return@runWithProgress
-
-      assertNotRunningFromSources()
-      jdkFile.delete()
-      LOG.warn("Removed custom boot runtime from the $jdkFile. Bundled runtime will be used")
-
-      service<RuntimeChooserNotifications>().notifyRuntimeChangeToDefaultAndRestart()
-    } catch (t: Throwable) {
-      LOG.warn("Failed to reset boot runtime to default in $jdkFileShadow. ${t.message}", t)
-      service<RuntimeChooserNotifications>().notifySettingDefaultBootJdkFailed(jdkFileShadow)
-    }
+  fun resetCustomJdk() = runWithProgress { _, jdkFile ->
+    jdkFile.delete()
+    LOG.warn("Removed custom boot runtime from the $jdkFile. Bundled runtime will be used")
+    LangBundle.message("dialog.message.choose.ide.runtime.is.set.to.param.default")
   }
 
-  private fun runWithProgress(action: (indicator: ProgressIndicator) -> Unit) {
+  private fun runWithProgress(action: (indicator: ProgressIndicator, jdkFile: Path) -> /*runtime name*/ @NlsSafe String?) {
     val title = LangBundle.message("progress.title.choose.ide.runtime.set.jdk")
     object : Task.Backgroundable(null, title, true, DEAF) {
       override fun run(indicator: ProgressIndicator) {
-          action(indicator)
+        var jdkFileShadow: Path? = null
+        try {
+          val jdkFile = computeJdkFilePath()
+          jdkFileShadow = jdkFile
+
+          @NlsSafe val runtimeName = action(indicator, jdkFile)
+          if (runtimeName != null) {
+            RuntimeChooserMessages.showRestartMessage(runtimeName)
+          }
+        }
+        catch (t: Throwable) {
+          LOG.warn("Failed to change boot runtime in $jdkFileShadow. ${t.message}", t)
+          RuntimeChooserMessages.showErrorMessage(LangBundle.message("dialog.message.choose.ide.runtime.unknown.error", t.localizedMessage))
+        }
       }
     }.queue()
-  }
-
-  private fun assertNotRunningFromSources() {
-    if (PluginManagerCore.isRunningFromSources()) {
-      throw RuntimeException("IntelliJ is running from sources")
-    }
   }
 }
