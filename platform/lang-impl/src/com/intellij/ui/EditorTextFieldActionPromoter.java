1ba4cd19b34e3691cc2a1eac3eb255d52e523301
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2021-03-03 15:00:58 +0300

    Revert "EditorTextFieldActionPromoter" sorting changes to fix regressions
    IDEA-260241, IDEA-260463, IDEA-261792
    
    3698276c79c862407572f26bfdbea97b8f529752, d8ff6f692fedd4200af5a992a9726390f8622dbc follow-up
    
    GitOrigin-RevId: e44282ae3c94f0f86287d5a6cb1dff61ecbd24cf

--- platform/lang-impl/src/com/intellij/ui/EditorTextFieldActionPromoter.java
+++ platform/lang-impl/src/com/intellij/ui/EditorTextFieldActionPromoter.java
@@ -4,26 +4,42 @@ package com.intellij.ui;
 import com.intellij.openapi.actionSystem.ActionPromoter;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.editor.actionSystem.EditorAction;
 import com.intellij.openapi.editor.actions.TextComponentEditorAction;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
-import static com.intellij.util.containers.ContainerUtil.filterIsInstance;
-
 /**
- * Encapsulates sorting rule that defines what editor actions have precedence to non-editor actions.
- * Current approach is that we want to process text processing-oriented editor actions with higher priority.
+ * Encapsulates sorting rule that defines what editor actions have precedence to non-editor actions. Current approach is that
+ * we want to process text processing-oriented editor actions with higher priority than non-editor actions and all
+ * other editor actions with lower priority.
  * <p/>
- * Rationale: {@code 'Ctrl+Shift+Right/Left Arrow'} shortcut is bound to
+ * Rationale: there is at least one commit-specific action that is mapped to the editor action by default
+ * ({@code 'show commit messages history'} vs {@code 'scroll to center'}). We want to process the former on target
+ * short key triggering. Another example is that {@code 'Ctrl+Shift+Right/Left Arrow'} shortcut is bound to
  * {@code 'expand/reduce selection by word'} editor action and {@code 'change dialog width'} non-editor action
  * and we want to use the first one.
  *
  * @author Konstantin Bulenkov
  */
 public class EditorTextFieldActionPromoter implements ActionPromoter {
+  private static final Comparator<AnAction> ACTIONS_COMPARATOR = (o1, o2) -> {
+    boolean textFieldAction1 = o1 instanceof TextComponentEditorAction;
+    boolean textFieldAction2 = o2 instanceof TextComponentEditorAction;
+    boolean plainEditorAction1 = o1 instanceof EditorAction && !textFieldAction1;
+    boolean plainEditorAction2 = o2 instanceof EditorAction && !textFieldAction2;
+    if (textFieldAction1 && plainEditorAction2) return -1;
+    if (textFieldAction2 && plainEditorAction1) return 1;
+    return 0;
+  };
+
   @Override
   public List<AnAction> promote(@NotNull List<? extends AnAction> actions, @NotNull DataContext context) {
-    return filterIsInstance(actions, TextComponentEditorAction.class);
+    ArrayList<AnAction> result = new ArrayList<>(actions);
+    result.sort(ACTIONS_COMPARATOR);
+    return result;
   }
 }
