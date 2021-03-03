29d2118f5cc62671254b010f77c11e8257bb1c7d
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2021-03-03 11:31:48 +0300

    index: initial VFS traversing should be done with IndexableFileScanner applied to exclude node_modules (part of IDEA-253188)
    Previously, PushedFilePropertiesUpdater.pushAllPropertiesNow was performed first. Since it doesn't apply IndexableFileScanner, node_modules folders were fully traversed. Some node_modules may contain exponential amount of file due to symlinks.
    
    (cherry picked from commit 0bef4c8a2335f54ca49f3f9cdf256c601e0e9069)
    
    IJ-CR-6545
    
    GitOrigin-RevId: d66509e5c7c24928595bc65cf52b5f9106d0bf5d

--- platform/lang-impl/src/com/intellij/util/indexing/UnindexedFilesUpdater.java
+++ platform/lang-impl/src/com/intellij/util/indexing/UnindexedFilesUpdater.java
@@ -133,12 +133,6 @@ public final class UnindexedFilesUpdater extends DumbModeTask {
     indicator.setText(IndexingBundle.message("progress.indexing.scanning"));
 
     PerformanceWatcher.Snapshot snapshot = PerformanceWatcher.takeSnapshot();
-    Instant pushPropertiesStart = Instant.now();
-    try {
-      myPusher.pushAllPropertiesNow();
-    } finally {
-      projectIndexingHistory.getTimes().setPushPropertiesDuration(Duration.between(pushPropertiesStart, Instant.now()));
-    }
 
     LOG.info(snapshot.getLogResponsivenessSinceCreationMessage("Pushing properties"));
 
@@ -156,6 +150,14 @@ public final class UnindexedFilesUpdater extends DumbModeTask {
     } finally {
       projectIndexingHistory.getTimes().setScanFilesDuration(Duration.between(scanFilesStart, Instant.now()));
     }
+
+    Instant pushPropertiesStart = Instant.now();
+    try {
+      myPusher.pushAllPropertiesNow();
+    } finally {
+      projectIndexingHistory.getTimes().setPushPropertiesDuration(Duration.between(pushPropertiesStart, Instant.now()));
+    }
+
     String scanningCompletedMessage = getLogScanningCompletedStageMessage(projectIndexingHistory);
     LOG.info(snapshot.getLogResponsivenessSinceCreationMessage(scanningCompletedMessage));
 
