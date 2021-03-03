e2ebdd8ab9826cc5d4ca1d0f0a9ad69dfe3aef77
Alexey Kudravtsev <cdr@intellij.com>
2021-03-03 03:37:28 +0100

    remove poison factory as unnecessary
    GitOrigin-RevId: c24bfe9bb2677b9cd1cda4ff977e2924866c2f96

--- platform/testFramework/src/com/intellij/testFramework/fixtures/impl/BaseFixture.java
+++ platform/testFramework/src/com/intellij/testFramework/fixtures/impl/BaseFixture.java
@@ -18,10 +18,6 @@ import java.util.concurrent.TimeUnit;
 import static org.junit.Assert.assertFalse;
 
 public class BaseFixture implements IdeaTestFixture {
-  static {
-    IdeaForkJoinWorkerThreadFactory.setupPoisonFactory();
-  }
-
   private boolean myInitialized;
   private boolean myDisposed;
   private @Nullable List<Throwable> mySuppressedExceptions;
