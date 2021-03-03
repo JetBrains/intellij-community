db3b47c651803a50ae0b3375e87fc8a7625f5047
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2021-03-04 01:45:16 +0900

    Revert "Support Android single-variant-sync for KAPT"
    This reverts commit bee2b5d9cbfa795386501d618a00412b883f0a27.
    
    GitOrigin-RevId: b8e4ba0dd19225e992d0fccb7cd5e1d34a74e476

--- plugins/kotlin/compiler-plugins/kapt/src/org/jetbrains/kotlin/kapt/idea/KaptProjectResolverExtension.kt
+++ plugins/kotlin/compiler-plugins/kapt/src/org/jetbrains/kotlin/kapt/idea/KaptProjectResolverExtension.kt
@@ -6,7 +6,6 @@ import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.externalSystem.model.DataNode
 import com.intellij.openapi.externalSystem.model.ProjectKeys
 import com.intellij.openapi.externalSystem.model.project.*
-import com.intellij.openapi.util.Key
 import org.gradle.tooling.model.idea.IdeaModule
 import org.jetbrains.kotlin.gradle.AndroidAwareGradleModelProvider
 import org.jetbrains.kotlin.idea.framework.GRADLE_SYSTEM_ID
@@ -20,17 +19,7 @@ class KaptProjectResolverExtension : AbstractProjectResolverExtension() {
         private val LOG = Logger.getInstance(KaptProjectResolverExtension::class.java)
     }
 
-    override fun getExtraProjectModelClasses(): Set<Class<KaptGradleModel>> {
-        error("getModelProvider() is overridden instead")
-    }
-
-    override fun getModelProvider(): ProjectImportModelProvider? {
-        val isAndroidPluginRequestingKaptGradleModelKey = Key.findKeyByName("IS_ANDROID_PLUGIN_REQUESTING_KAPT_GRADLE_MODEL_KEY")
-        val isAndroidPluginRequestingKaptGradleModel =
-            isAndroidPluginRequestingKaptGradleModelKey != null && resolverCtx.getUserData(isAndroidPluginRequestingKaptGradleModelKey) != null
-        return AndroidAwareGradleModelProvider(KaptGradleModel::class.java, isAndroidPluginRequestingKaptGradleModel)
-    }
-
+    override fun getExtraProjectModelClasses() = setOf(KaptGradleModel::class.java)
     override fun getToolingExtensionsClasses() = setOf(KaptModelBuilderService::class.java, Unit::class.java)
 
     override fun populateModuleExtraModels(gradleModule: IdeaModule, ideModule: DataNode<ModuleData>) {
