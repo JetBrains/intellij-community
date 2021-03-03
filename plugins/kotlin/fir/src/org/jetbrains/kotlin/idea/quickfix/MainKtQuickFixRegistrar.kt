20156f5ca836ba9beee7c0fb9b84fa8b6bec2ebf
Tianyu Geng <tgeng@google.com>
2021-03-03 13:43:04 -0800

    FIR IDE: add quickfix for NESTED_CLASS_NOT_ALLOWED
    Original commit: 8158a07063242c50ddaaadfd87b3f9561af2ace6
    
    GitOrigin-RevId: c3b6996acadbaa6e76a32ee73de6cdf09097861e

--- plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/quickfix/MainKtQuickFixRegistrar.kt
+++ plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/quickfix/MainKtQuickFixRegistrar.kt
@@ -41,6 +41,10 @@ class MainKtQuickFixRegistrar : KtQuickFixRegistrar() {
             KtFirDiagnostic.PrivateSetterForAbstractProperty::class,
             RemoveModifierFix.removePrivateModifier
         )
+        registerPsiQuickFixes(
+            KtFirDiagnostic.NestedClassNotAllowed::class,
+            AddModifierFix.addInnerModifier
+        )
     }
 
     private val overrides = KtQuickFixesListBuilder.registerPsiQuickFix {
