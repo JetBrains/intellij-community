68a7f752f70be1d4868e6837401bd60aa5e61d2a
Alexandr Suhinin <alexandr.suhinin@jetbrains.com>
2021-03-03 12:47:19 +0200

    in-place extract method: fix missed static modifier
    GitOrigin-RevId: 6d6456b14dab43f70687b7787c1d462bc98120e8

---
+++ java/java-tests/testData/refactoring/extractMethodInplace/StaticMustBePlaced_after.java
@@ -0,0 +1,11 @@
+class Test {
+    static void test() {
+        int x = getX();
+        System.out.println(x);
+    }
+
+    private static int getX() {
+        int x = 42;
+        return x;
+    }
+}
