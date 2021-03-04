34ef48d02bfea7ad5ed73ebfc56226c16b072b02
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2021-03-04 22:37:27 +0100

    Revert "runtime-chooser: IDEA-261869 - really simplified approach for the Runtime Chooser"
    This reverts commit c477706bd509a2f3c8ac92b48c094e3dc93aff6f.
    
    (cherry picked from commit e59045f8e2a7262ea0e5a184523964fec9ecf935)
    
    IJ-CR-6490
    
    GitOrigin-RevId: d20788e0ec673692e5a6bd83a2a1840a5181223e

--- platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/jdkDownloader/RuntimeChooserCustom.kt
+++ platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/jdkDownloader/RuntimeChooserCustom.kt
@@ -2,14 +2,26 @@
 package com.intellij.openapi.projectRoots.impl.jdkDownloader
 
 import com.intellij.lang.LangBundle
+import com.intellij.openapi.actionSystem.DataProvider
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.application.invokeAndWaitIfNeeded
 import com.intellij.openapi.application.invokeLater
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.Task
+import com.intellij.openapi.progress.util.ProgressIndicatorUtils
+import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.openapi.projectRoots.SdkType
+import com.intellij.openapi.projectRoots.SdkTypeId
 import com.intellij.openapi.projectRoots.SimpleJavaSdkType
-import com.intellij.openapi.projectRoots.impl.SdkConfigurationUtil
+import com.intellij.openapi.projectRoots.impl.jdkDownloader.RuntimeChooserJreValidator.isSupportedSdkItem
+import com.intellij.openapi.roots.ui.configuration.SdkPopup
+import com.intellij.openapi.roots.ui.configuration.SdkPopupFactory
+import com.intellij.openapi.roots.ui.configuration.projectRoot.SdkDownloadTracker
 import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.util.Disposer
+import com.intellij.util.Consumer
 import java.nio.file.Path
+import java.util.concurrent.CountDownLatch
 import javax.swing.JComponent
 
 data class RuntimeChooserCustomItem(
@@ -20,7 +32,7 @@ data class RuntimeChooserCustomItem(
 object RuntimeChooserAddCustomItem : RuntimeChooserItem()
 
 object RuntimeChooserCustom {
-  val sdkType: SdkType?
+  val sdkType
     get() = SdkType
       .getAllTypes()
       .singleOrNull(SimpleJavaSdkType.notSimpleJavaSdkTypeIfAlternativeExistsAndNotDependentSdkType()::value)
@@ -28,22 +40,43 @@ object RuntimeChooserCustom {
   val isActionAvailable
     get() = sdkType != null
 
-  fun showAddCustomJdkPopup(parent: JComponent, model: RuntimeChooserModel) {
-    val type = sdkType
-    if (type == null) return
-
-    SdkConfigurationUtil.selectSdkHome(type, parent) { home ->
-      if (home != null) {
-        importNewItem(parent, home, model)
-      }
+  val jdkDownloaderExtensionProvider = DataProvider { dataId ->
+    when {
+      JDK_DOWNLOADER_EXT.`is`(dataId) -> jdkDownloaderExtension
+      else -> null
     }
   }
 
-  private fun importNewItem(parent: JComponent, homePath: String, model: RuntimeChooserModel) {
-    object : Task.Modal(null, LangBundle.message("progress.title.choose.ide.runtime.scanning.jdk"), false) {
+  private val jdkDownloaderExtension = object : JdkDownloaderDialogHostExtension {
+    override fun allowWsl(): Boolean = false
+
+    override fun shouldIncludeItem(sdkType: SdkTypeId, item: JdkItem): Boolean {
+      return sdkType == this@RuntimeChooserCustom.sdkType && isSupportedSdkItem(item)
+    }
+  }
+
+  fun createSdkChooserPopup(parent: JComponent, model: RuntimeChooserModel): SdkPopup? {
+    return SdkPopupFactory
+      .newBuilder()
+      .withSdkType(sdkType ?: return null)
+      .withSdkFilter { it != null && isSupportedSdkItem(it) }
+      .withSuggestedSdkFilter { it != null && isSupportedSdkItem(it) }
+      .onSdkSelected { sdk -> importNewItem(parent, sdk, model) }
+      .buildPopup()
+  }
+
+  private fun importNewItem(parent: JComponent, sdk: Sdk?, model: RuntimeChooserModel) {
+    if (sdk == null) return
+
+    val task = object : Task.Modal(null, LangBundle.message("progress.title.choose.ide.runtime.scanning.jdk"), false) {
       override fun run(indicator: ProgressIndicator) {
+        //a downloading JDK may be returned, we need to wait for it's download
+        waitForDownloadingSdk(parent, indicator, sdk)
+
+        indicator.checkCanceled()
+
         RuntimeChooserJreValidator.testNewJdkUnderProgress(
-          computeHomePath = { homePath },
+          computeHomePath = { sdk.homePath },
           callback = object : RuntimeChooserJreValidatorCallback<Unit> {
             override fun onSdkResolved(versionString: String, sdkHome: Path) {
               val newItem = RuntimeChooserCustomItem(versionString, sdkHome.toString())
@@ -59,6 +92,48 @@ object RuntimeChooserCustom {
             }
           })
       }
-    }.queue()
+    }
+
+    //we make sure we execute the task with the current parent and modality
+    invokeLater(ModalityState.stateForComponent(parent)) {
+      task.queue()
+    }
+  }
+
+  private fun waitForDownloadingSdk(parent: JComponent, indicator: ProgressIndicator, sdk: Sdk) {
+    if (!SdkDownloadTracker.getInstance().isDownloading(sdk)) return
+
+    val lifetime = Disposer.newDisposable("Choose Runtime")
+    try {
+      var downloadSucceeed = false
+      val downloadCompletion = CountDownLatch(1)
+      val downloadIsRunning = invokeAndWaitIfNeeded {
+        SdkDownloadTracker
+          .getInstance()
+          .tryRegisterDownloadingListener(
+            sdk,
+            lifetime,
+            indicator,
+            Consumer {
+              downloadSucceeed = it
+              downloadCompletion.countDown()
+            })
+      }
+
+      if (downloadIsRunning) {
+        ProgressIndicatorUtils.awaitWithCheckCanceled(downloadCompletion)
+        if (!downloadSucceeed) {
+          invokeLater {
+            Messages.showErrorDialog(
+              parent,
+              LangBundle.message("dialog.message.choose.ide.runtime.sdk.download.error", sdk.name),
+              LangBundle.message("dialog.title.choose.ide.runtime"))
+          }
+        }
+      }
+    }
+    finally {
+      Disposer.dispose(lifetime)
+    }
   }
 }
