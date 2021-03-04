2f1f9352492914d092ebb6039adb39f0c1be6a7e
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2021-03-04 22:37:45 +0100

    Revert "runtime-chooser: IDEA-261869 - it is not possible to download an SDK under a modal progress (failed)"
    This reverts commit ae551c3a14161de4c6312adfe7961d155e1ca5ea.
    
    GitOrigin-RevId: 0754b3f52cdf7051611548f6f5a29aea5653e860

--- platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/jdkDownloader/RuntimeChooserCustom.kt
+++ platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/jdkDownloader/RuntimeChooserCustom.kt
@@ -3,12 +3,9 @@ package com.intellij.openapi.projectRoots.impl.jdkDownloader
 
 import com.intellij.lang.LangBundle
 import com.intellij.openapi.actionSystem.DataProvider
-import com.intellij.openapi.application.ModalityState
-import com.intellij.openapi.application.invokeAndWaitIfNeeded
 import com.intellij.openapi.application.invokeLater
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.Task
-import com.intellij.openapi.progress.util.ProgressIndicatorUtils
 import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.openapi.projectRoots.SdkType
 import com.intellij.openapi.projectRoots.SdkTypeId
@@ -16,12 +13,9 @@ import com.intellij.openapi.projectRoots.SimpleJavaSdkType
 import com.intellij.openapi.projectRoots.impl.jdkDownloader.RuntimeChooserJreValidator.isSupportedSdkItem
 import com.intellij.openapi.roots.ui.configuration.SdkPopup
 import com.intellij.openapi.roots.ui.configuration.SdkPopupFactory
-import com.intellij.openapi.roots.ui.configuration.projectRoot.SdkDownloadTracker
 import com.intellij.openapi.ui.Messages
-import com.intellij.openapi.util.Disposer
-import com.intellij.util.Consumer
+import com.intellij.openapi.util.SystemInfo
 import java.nio.file.Path
-import java.util.concurrent.CountDownLatch
 import javax.swing.JComponent
 
 data class RuntimeChooserCustomItem(
@@ -68,13 +62,8 @@ object RuntimeChooserCustom {
   private fun importNewItem(parent: JComponent, sdk: Sdk?, model: RuntimeChooserModel) {
     if (sdk == null) return
 
-    val task = object : Task.Modal(null, LangBundle.message("progress.title.choose.ide.runtime.scanning.jdk"), false) {
+    object : Task.Modal(null, LangBundle.message("progress.title.choose.ide.runtime.scanning.jdk"), false) {
       override fun run(indicator: ProgressIndicator) {
-        //a downloading JDK may be returned, we need to wait for it's download
-        waitForDownloadingSdk(parent, indicator, sdk)
-
-        indicator.checkCanceled()
-
         RuntimeChooserJreValidator.testNewJdkUnderProgress(
           computeHomePath = { sdk.homePath },
           callback = object : RuntimeChooserJreValidatorCallback<Unit> {
@@ -92,48 +81,6 @@ object RuntimeChooserCustom {
             }
           })
       }
-    }
-
-    //we make sure we execute the task with the current parent and modality
-    invokeLater(ModalityState.stateForComponent(parent)) {
-      task.queue()
-    }
-  }
-
-  private fun waitForDownloadingSdk(parent: JComponent, indicator: ProgressIndicator, sdk: Sdk) {
-    if (!SdkDownloadTracker.getInstance().isDownloading(sdk)) return
-
-    val lifetime = Disposer.newDisposable("Choose Runtime")
-    try {
-      var downloadSucceeed = false
-      val downloadCompletion = CountDownLatch(1)
-      val downloadIsRunning = invokeAndWaitIfNeeded {
-        SdkDownloadTracker
-          .getInstance()
-          .tryRegisterDownloadingListener(
-            sdk,
-            lifetime,
-            indicator,
-            Consumer {
-              downloadSucceeed = it
-              downloadCompletion.countDown()
-            })
-      }
-
-      if (downloadIsRunning) {
-        ProgressIndicatorUtils.awaitWithCheckCanceled(downloadCompletion)
-        if (!downloadSucceeed) {
-          invokeLater {
-            Messages.showErrorDialog(
-              parent,
-              LangBundle.message("dialog.message.choose.ide.runtime.sdk.download.error", sdk.name),
-              LangBundle.message("dialog.title.choose.ide.runtime"))
-          }
-        }
-      }
-    }
-    finally {
-      Disposer.dispose(lifetime)
-    }
+    }.queue()
   }
 }
