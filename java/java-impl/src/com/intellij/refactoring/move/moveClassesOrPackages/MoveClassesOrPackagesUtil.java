000785c72725c423054b55e7940de32c3efac2fd
Anna Kozlova <anna.kozlova@jetbrains.com>
2021-03-05 14:09:48 +0100

    move: show chooser if not possible to leave in the same root (IDEA-263671)
    GitOrigin-RevId: 8b1ff0be9fa148f36b7411d9d59697a597e425ed

--- java/java-impl/src/com/intellij/refactoring/move/moveClassesOrPackages/MoveClassesOrPackagesUtil.java
+++ java/java-impl/src/com/intellij/refactoring/move/moveClassesOrPackages/MoveClassesOrPackagesUtil.java
@@ -311,16 +311,19 @@ public final class MoveClassesOrPackagesUtil {
     PsiDirectory[] directories = aPackage != null ? aPackage.getDirectories() : null;
     final ProjectFileIndex fileIndex = ProjectRootManager.getInstance(project).getFileIndex();
     final VirtualFile baseDirVirtualFile = baseDir != null ? baseDir.getVirtualFile() : null;
-    final boolean isBaseDirInTestSources = baseDirVirtualFile != null && fileIndex.isInTestSourceContent(baseDirVirtualFile);
-    if (directories != null && directories.length == 1 && (baseDirVirtualFile == null ||
-                                                           fileIndex.isInTestSourceContent(directories[0].getVirtualFile()) == isBaseDirInTestSources)) {
+    final VirtualFile baseSourceRoot = baseDirVirtualFile != null ? fileIndex.getSourceRootForFile(baseDirVirtualFile) : null;
+    if (directories != null && 
+        directories.length == 1 && 
+        baseSourceRoot != null && 
+        baseSourceRoot.equals(fileIndex.getSourceRootForFile(directories[0].getVirtualFile()))) {
       directory = directories[0];
     }
     else {
       final List<VirtualFile> contentSourceRoots = JavaProjectRootsUtil.getSuitableDestinationSourceRoots(project);
-      if (contentSourceRoots.size() == 1 && (baseDirVirtualFile == null || fileIndex.isInTestSourceContent(contentSourceRoots.get(0)) == isBaseDirInTestSources)) {
-        directory = WriteAction
-          .compute(() -> RefactoringUtil.createPackageDirectoryInSourceRoot(packageWrapper, contentSourceRoots.get(0)));
+      if (contentSourceRoots.size() == 1 && 
+          baseSourceRoot != null && 
+          baseSourceRoot.equals(contentSourceRoots.get(0))) {
+        directory = WriteAction.compute(() -> RefactoringUtil.createPackageDirectoryInSourceRoot(packageWrapper, contentSourceRoots.get(0)));
       }
       else {
         final VirtualFile sourceRootForFile = chooseSourceRoot(packageWrapper, contentSourceRoots, baseDir);
