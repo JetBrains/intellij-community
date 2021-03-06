ae0690ec6853cf0cce5dcdf8400377642478f37e
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2021-03-06 23:20:04 +0300

    [git-index] run postponed GitStagedPanel updates after commit is finished
    follow-up: 14658a87c33f2a3b8f3c6ca15ff65fba939ba7af
    
    GitOrigin-RevId: 50f69b264d5ab87dd773c13baf9046757271626b

--- plugins/git4idea/src/git4idea/index/ui/GitStagePanel.kt
+++ plugins/git4idea/src/git4idea/index/ui/GitStagePanel.kt
@@ -22,6 +22,7 @@ import com.intellij.openapi.vcs.changes.EditorTabPreview
 import com.intellij.openapi.vcs.changes.InclusionListener
 import com.intellij.openapi.vcs.changes.ui.*
 import com.intellij.openapi.vcs.changes.ui.ChangesGroupingSupport.Companion.REPOSITORY_GROUPING
+import com.intellij.openapi.vcs.checkin.CheckinHandler
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.wm.IdeFocusManager
 import com.intellij.ui.OnePixelSplitter
@@ -41,6 +42,7 @@ import com.intellij.util.ui.JBUI.Panels.simplePanel
 import com.intellij.util.ui.ThreeStateCheckBox
 import com.intellij.util.ui.tree.TreeUtil
 import com.intellij.vcs.commit.CommitStatusPanel
+import com.intellij.vcs.commit.CommitWorkflowListener
 import com.intellij.vcs.commit.EditedCommitNode
 import com.intellij.vcs.log.runInEdt
 import com.intellij.vcs.log.runInEdtAsync
@@ -155,6 +157,7 @@ internal class GitStagePanel(private val tracker: GitStageTracker,
     val busConnection = project.messageBus.connect(this)
     busConnection.subscribe(GitChangeProvider.TOPIC, MyGitChangeProviderListener())
     busConnection.subscribe(ChangeListListener.TOPIC, MyChangeListListener())
+    commitWorkflowHandler.workflow.addListener(MyCommitWorkflowListener(), this)
 
     if (GitVcs.getInstance(project).changeProvider?.isRefreshInProgress == true) {
       tree.setEmptyText(message("stage.loading.status"))
@@ -390,6 +393,20 @@ internal class GitStagePanel(private val tracker: GitStageTracker,
         updateChangesStatusPanel()
       }
     }
+  }
+
+  private inner class MyCommitWorkflowListener: CommitWorkflowListener {
+    override fun executionEnded() {
+      if (hasPendingUpdates) {
+        hasPendingUpdates = false
+        update()
+      }
+    }
+
+    override fun vcsesChanged() = Unit
+    override fun executionStarted() = Unit
+    override fun beforeCommitChecksStarted() = Unit
+    override fun beforeCommitChecksEnded(isDefaultCommit: Boolean, result: CheckinHandler.ReturnResult) = Unit
   }
 
   companion object {
