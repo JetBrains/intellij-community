b857b50877a183700b0173034e4fb2350f269dd5
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2021-03-06 17:27:21 +0300

    Add "Show in Git Log" action to the list of commits in Space reviews: IDEA-263767
    GitOrigin-RevId: cf9bcfdc7f552262d5f7bfec3cd582f693ef0900

--- plugins/space/src/main/kotlin/com/intellij/space/vcs/review/details/SpaceReviewCommitListFactory.kt
+++ plugins/space/src/main/kotlin/com/intellij/space/vcs/review/details/SpaceReviewCommitListFactory.kt
@@ -1,14 +1,22 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.space.vcs.review.details
 
+import com.intellij.openapi.actionSystem.ActionGroup
+import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.actionSystem.ActionPlaces
+import com.intellij.openapi.vcs.VcsDataKeys
 import com.intellij.space.stats.SpaceStatsCounterCollector
 import com.intellij.ui.CollectionListModel
 import com.intellij.ui.ListSpeedSearch
+import com.intellij.ui.PopupHandler.installPopupHandler
 import com.intellij.ui.ScrollPaneFactory
 import com.intellij.ui.ScrollingUtil
 import com.intellij.ui.components.JBList
 import com.intellij.util.ui.ListUiUtil
 import com.intellij.util.ui.UIUtil
+import com.intellij.vcs.log.impl.HashImpl
+import com.intellij.vcs.log.util.VcsLogUtil
+import git4idea.GitVcs
 import javax.swing.JComponent
 import javax.swing.ListSelectionModel
 import javax.swing.ScrollPaneConstants
@@ -31,6 +39,17 @@ object SpaceReviewCommitListFactory {
     }.also {
       ScrollingUtil.installActions(it)
       ListUiUtil.Selection.installSelectionOnRightClick(it)
+      installPopupHandler(it, ActionManager.getInstance().getAction("space.review.commits.popup") as ActionGroup, ActionPlaces.POPUP)
+      it.setDataProvider { key ->
+        when {
+          VcsDataKeys.VCS_REVISION_NUMBER.`is`(key) -> it.selectedValue?.let { selectedValue ->
+            val hash = selectedValue.commitWithGraph.commit.id
+            VcsLogUtil.convertToRevisionNumber(HashImpl.build(hash))
+          }
+          VcsDataKeys.VCS.`is`(key) -> GitVcs.getKey()
+          else -> null
+        }
+      }
 
       ListSpeedSearch(it) { commit -> commit.commitWithGraph.commit.message }
     }
