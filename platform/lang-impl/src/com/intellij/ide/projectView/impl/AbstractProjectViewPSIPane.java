3645c9374b848da832e812db73a2a161399fd912
Konstantin Bulenkov <kb@jetbrains.com>
2021-03-07 00:30:21 +0100

    IDEA-162454 Project View. Count selected items.
    GitOrigin-RevId: aa6be492d7bb0976a2600d0dd3298717c9cd846c

--- platform/lang-impl/src/com/intellij/ide/projectView/impl/AbstractProjectViewPSIPane.java
+++ platform/lang-impl/src/com/intellij/ide/projectView/impl/AbstractProjectViewPSIPane.java
@@ -6,9 +6,11 @@ import com.intellij.ide.projectView.BaseProjectTreeBuilder;
 import com.intellij.ide.projectView.impl.nodes.PsiDirectoryNode;
 import com.intellij.ide.ui.customization.CustomizationUtil;
 import com.intellij.ide.util.treeView.*;
+import com.intellij.lang.LangBundle;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.actionSystem.ActionPlaces;
 import com.intellij.openapi.actionSystem.IdeActions;
+import com.intellij.openapi.actionSystem.impl.ActionMenu;
 import com.intellij.openapi.editor.colors.EditorColorsManager;
 import com.intellij.openapi.editor.colors.TextAttributesKey;
 import com.intellij.openapi.editor.markup.TextAttributes;
@@ -33,6 +35,8 @@ import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreePath;
@@ -130,6 +134,7 @@ public abstract class AbstractProjectViewPSIPane extends AbstractProjectViewPane
 
   private void initTree() {
     myTree.getSelectionModel().setSelectionMode(TreeSelectionModel.DISCONTIGUOUS_TREE_SELECTION);
+    myTree.getSelectionModel().addTreeSelectionListener(e -> onSelectionChanged(e));
     myTree.setRootVisible(false);
     myTree.setShowsRootHandles(true);
     myTree.expandPath(new TreePath(myTree.getModel().getRoot()));
@@ -144,6 +149,12 @@ public abstract class AbstractProjectViewPSIPane extends AbstractProjectViewPane
 
     myTree.addKeyListener(new PsiCopyPasteManager.EscapeHandler());
     CustomizationUtil.installPopupHandler(myTree, IdeActions.GROUP_PROJECT_VIEW_POPUP, ActionPlaces.PROJECT_VIEW_POPUP);
+  }
+
+  protected void onSelectionChanged(@SuppressWarnings("unused") TreeSelectionEvent e) {
+    int count = myTree.getSelectionModel().getSelectionCount();
+    String description = count > 1 ? LangBundle.message("project.view.elements.selected", count) : null;
+    ActionMenu.showDescriptionInStatusBar(true, myTree, description);
   }
 
   @NotNull
