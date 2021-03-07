1225b870eb172efcc0db59d2bf4ffeccc42ed45e
Konstantin Bulenkov <kb@jetbrains.com>
2021-03-07 22:03:56 +0100

    IDEA-261155 Built-in HTML tab is opened as Preview tab if the *.html file is opened as Preview tab
    GitOrigin-RevId: ef88f593f755bed189c86e1b09c2ed0eb73545df

--- platform/platform-impl/src/com/intellij/ide/browsers/actions/WebPreviewVirtualFile.java
+++ platform/platform-impl/src/com/intellij/ide/browsers/actions/WebPreviewVirtualFile.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.browsers.actions;
 
+import com.intellij.openapi.fileEditor.impl.NotSuitableForPreviewTab;
 import com.intellij.openapi.util.NlsSafe;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.testFramework.LightVirtualFile;
@@ -10,7 +11,7 @@ import org.jetbrains.annotations.NotNull;
 /**
  * @author Konstantin Bulenkov
  */
-public class WebPreviewVirtualFile extends LightVirtualFile {
+public class WebPreviewVirtualFile extends LightVirtualFile implements NotSuitableForPreviewTab {
   private final VirtualFile myFile;
   private final Url myPreviewUrl;
 
