c2a980ef0e40d9523938e7de74ee0b30e7f4bd5e
Alexey Kudravtsev <cdr@intellij.com>
2021-03-08 14:03:54 +0100

    add assertion: @Service class must be final and must not contain constructor with injectable parameters
    GitOrigin-RevId: ad82c62aaf06079799da65ebaeec18fc11eb592c

--- platform/service-container/src/com/intellij/serviceContainer/ComponentManagerImpl.kt
+++ platform/service-container/src/com/intellij/serviceContainer/ComponentManagerImpl.kt
@@ -68,8 +68,23 @@ abstract class ComponentManagerImpl @JvmOverloads constructor(internal val paren
 
     // not as file level function to avoid scope cluttering
     @Internal
-    fun <T> isLightService(serviceClass: Class<T>): Boolean {
-      return Modifier.isFinal(serviceClass.modifiers) && serviceClass.isAnnotationPresent(Service::class.java)
+    fun isLightService(serviceClass: Class<*>): Boolean {
+      if (!serviceClass.isAnnotationPresent(Service::class.java)) {
+        return false
+      }
+      if (!Modifier.isFinal(serviceClass.modifiers)) {
+        throw IllegalStateException("$serviceClass must be final because it's marked as @Service")
+      }
+      val acceptableCtr: Constructor<*>? = serviceClass
+        .declaredConstructors
+        .filter { c -> c.parameterCount == 0 ||
+                  // the only "Project" parameter is ok
+                  c.parameterCount == 1 && c.parameterTypes[0] == com.intellij.openapi.project.Project::class.java }
+        .firstOrNull()
+      if (acceptableCtr == null) {
+        throw IllegalStateException("@Service $serviceClass must contain default constructor or constructor(Project) but got: ${serviceClass.declaredConstructors.asList()}")
+      }
+      return true
     }
 
     @ApiStatus.Internal
