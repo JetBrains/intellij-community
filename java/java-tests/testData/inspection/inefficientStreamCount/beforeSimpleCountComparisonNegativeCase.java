fc88ac89196542aa54a496e0c4a30f2267cb33b3
Andrey.Cherkasov <andrey.cherkasov@jetbrains.com>
2021-03-09 14:47:25 +0300

    [java-inspections] IDEA-207061 Fix testdata
    GitOrigin-RevId: 60d26794920c6583219598591f033915f68bf9e9

--- java/java-tests/testData/inspection/inefficientStreamCount/beforeSimpleCountComparisonNegativeCase.java
+++ java/java-tests/testData/inspection/inefficientStreamCount/beforeSimpleCountComparisonNegativeCase.java
@@ -3,7 +3,7 @@
 import java.util.Arrays;
 
 class Test {
-  long cnt() {
-    return Arrays.asList('d', 'e', 'f')./*stream*/stream()./*count*/c<caret>ount()/*after*/ > 0;
+  boolean isPresent() {
+    return Arrays.asList('d', 'e', 'f').stream().c<caret>ount() > 0;
   }
 }
