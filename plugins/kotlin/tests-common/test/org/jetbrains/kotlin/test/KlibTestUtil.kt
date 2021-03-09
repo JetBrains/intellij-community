19ce0b7e69d104369ce45d293c24c9a26247e10c
Vladimir Ivanov <knebekaizer@users.noreply.github.com>
2021-03-09 16:50:23 +0300

    [Native] Export KDoc into generated native header
    Original commit: fd02802028b3fb577263a959daa7603969869195
    
    GitOrigin-RevId: abfda6b3b99f21cf11e085c07068f4a36195ae58

--- plugins/kotlin/tests-common/test/org/jetbrains/kotlin/test/KlibTestUtil.kt
+++ plugins/kotlin/tests-common/test/org/jetbrains/kotlin/test/KlibTestUtil.kt
@@ -75,6 +75,7 @@ object KlibTestUtil {
         val serializer = KlibMetadataMonolithicSerializer(
             languageVersionSettings = LanguageVersionSettingsImpl.DEFAULT,
             metadataVersion = KlibMetadataVersion.INSTANCE,
+            exportKDoc = false,
             skipExpects = false,
             project = null
         )
