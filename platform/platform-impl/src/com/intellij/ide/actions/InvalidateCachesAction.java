ce9f0a99f4593c97140ae454707c2eea3b61d79d
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2021-03-10 16:19:24 +0100

    invalidate-caches: IDEA-263504, IDEA-263504 - fix messages, improve dialog layout
    (cherry picked from commit 116fba7c59390f7e928c419884a3f254d958da4d)
    
    IJ-CR-6948
    
    GitOrigin-RevId: 562793ef9d687e94bfc5f8f159a933f694b66477

--- platform/platform-impl/src/com/intellij/ide/actions/InvalidateCachesAction.java
+++ platform/platform-impl/src/com/intellij/ide/actions/InvalidateCachesAction.java
@@ -2,7 +2,6 @@
 package com.intellij.ide.actions;
 
 import com.intellij.ide.caches.CachesInvalidator;
-import com.intellij.idea.ActionsBundle;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
@@ -10,18 +9,10 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ex.ApplicationEx;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.DumbAware;
-import com.intellij.util.gist.GistManager;
-import com.intellij.util.indexing.FileBasedIndex;
 import org.jetbrains.annotations.NotNull;
 
 final class InvalidateCachesAction extends AnAction implements DumbAware {
   private static final Logger LOG = Logger.getInstance(InvalidateCachesAction.class);
-
-  InvalidateCachesAction() {
-    String text = ApplicationManager.getApplication().isRestartCapable() ? ActionsBundle.message("action.InvalidateCachesRestart.text")
-                                                                         : ActionsBundle.message("action.InvalidateCaches.text");
-    getTemplatePresentation().setText(text);
-  }
 
   @Override
   public void actionPerformed(@NotNull AnActionEvent e) {
--- platform/platform-impl/src/com/intellij/ide/actions/InvalidateCachesDialog.kt
+++ platform/platform-impl/src/com/intellij/ide/actions/InvalidateCachesDialog.kt
@@ -6,13 +6,19 @@ import com.intellij.ide.IdeBundle
 import com.intellij.ide.caches.CachesInvalidator
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
-import com.intellij.openapi.ui.Messages
-import com.intellij.openapi.ui.panel.ComponentPanelBuilder
-import com.intellij.ui.components.JBLabel
+import com.intellij.openapi.util.text.HtmlChunk
+import com.intellij.ui.components.Link
+import com.intellij.ui.components.panels.NonOpaquePanel
 import com.intellij.ui.layout.*
+import com.intellij.util.ui.JBUI
+import java.awt.BorderLayout
 import javax.swing.Action
 import javax.swing.JCheckBox
-import javax.swing.SwingConstants
+import javax.swing.JPanel
+
+private var Action.text
+  get() = getValue(Action.NAME)
+  set(value) = putValue(Action.NAME, value)
 
 class InvalidateCachesDialog(
   project: Project?,
@@ -35,41 +41,39 @@ class InvalidateCachesDialog(
     title = IdeBundle.message("dialog.title.invalidate.caches")
     setResizable(false)
     init()
-  }
 
-  fun isRestartOnly() = canRestart && exitCode == JUST_RESTART_CODE
-
-  private var Action.text
-    get() = getValue(Action.NAME)
-    set(value) = putValue(Action.NAME, value)
-
-
-  override fun createActions(): Array<Action> {
     okAction.text = when {
       canRestart -> IdeBundle.message("button.invalidate.and.restart")
       else -> IdeBundle.message("button.invalidate.and.exit")
     }
 
     cancelAction.text = IdeBundle.message("button.cancel.without.mnemonic")
+  }
 
-    val justRestartAction = if (canRestart) {
-      DialogWrapperExitAction(IdeBundle.message("button.just.restart"), JUST_RESTART_CODE)
-    } else null
+  fun isRestartOnly() = canRestart && exitCode == JUST_RESTART_CODE
 
-    return listOfNotNull(okAction, cancelAction, justRestartAction).toTypedArray()
+  override fun createSouthAdditionalPanel(): JPanel? {
+    if (!canRestart) return null
+
+    val link = Link(IdeBundle.message("link.just.restart")) {
+      close(JUST_RESTART_CODE)
+    }
+
+    val panel = NonOpaquePanel(BorderLayout())
+    panel.border = JBUI.Borders.emptyLeft(10)
+    panel.add(link)
+    return panel
   }
 
   override fun createCenterPanel() = panel {
-    val commentComponent = ComponentPanelBuilder.createCommentComponent(
-      IdeBundle.message("dialog.message.caches.will.be.invalidated", ""),
-      true,
-      -1,
-      true
-    )
-    commentComponent.icon = Messages.getWarningIcon()
-
-    val component = JBLabel(IdeBundle.message("dialog.message.caches.will.be.invalidated"), Messages.getWarningIcon(), SwingConstants.LEFT)
-    createNoteOrCommentRow(component)
+    row {
+      label(
+        HtmlChunk
+          .html()
+          .addText(IdeBundle.message("dialog.message.caches.will.be.invalidated"))
+          .toString()
+      ).growPolicy(GrowPolicy.MEDIUM_TEXT).constraints(CCFlags.growY)
+    }
 
     if (invalidatorsWithDescriptor.isNotEmpty()) {
       row {
