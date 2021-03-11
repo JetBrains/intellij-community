1a17d198d1398690fb93e4e077b1b1b5a07203f8
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2021-03-11 17:14:09 +0100

    invalidate-caches: IDEA-263504 - check "ask before exit" in the restart action
    (cherry picked from commit d3e7266140a69c8d6d85a1c41837f2165da51ef7)
    
    IJ-CR-6948
    
    GitOrigin-RevId: b97853fb20e90aac3829d6da7de1b6ed257c5810

--- platform/platform-impl/src/com/intellij/ide/actions/RestartIdeAction.java
+++ platform/platform-impl/src/com/intellij/ide/actions/RestartIdeAction.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.actions;
 
+import com.intellij.ide.GeneralSettings;
 import com.intellij.ide.IdeBundle;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
@@ -13,13 +14,19 @@ import org.jetbrains.annotations.NotNull;
 public class RestartIdeAction extends AnAction implements DumbAware {
   @Override
   public void actionPerformed(@NotNull AnActionEvent e) {
-    var result = Messages.showYesNoDialog(
-      IdeBundle.message("dialog.message.restart.ide"),
-      IdeBundle.message("dialog.title.restart.ide"),
-      IdeBundle.message("dialog.action.restart.yes"),
-      IdeBundle.message("dialog.action.restart.cancel"),
-      Messages.getWarningIcon()
-    ) == Messages.YES;
+
+    boolean result;
+    if (GeneralSettings.getInstance().isConfirmExit()) {
+      result = Messages.showYesNoDialog(
+        IdeBundle.message("dialog.message.restart.ide"),
+        IdeBundle.message("dialog.title.restart.ide"),
+        IdeBundle.message("dialog.action.restart.yes"),
+        IdeBundle.message("dialog.action.restart.cancel"),
+        Messages.getWarningIcon()
+      ) == Messages.YES;
+    } else {
+      result = true;
+    }
 
     if (result) {
       final ApplicationEx app = (ApplicationEx)ApplicationManager.getApplication();
