a4d31b4054e98bb5238ba6c15ba8e9c9385b318f
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2021-03-12 19:55:32 +0900

    Update generated tests
    GitOrigin-RevId: d6f2d0482a12b3d03f886bde6da9c8c5922d79e4

--- plugins/kotlin/fir/test/org/jetbrains/kotlin/idea/resolve/FirReferenceResolveTestGenerated.java
+++ plugins/kotlin/fir/test/org/jetbrains/kotlin/idea/resolve/FirReferenceResolveTestGenerated.java
@@ -218,7 +218,7 @@ public class FirReferenceResolveTestGenerated extends AbstractFirReferenceResolv
 
     @TestMetadata("InMethodVarargParameter.kt")
     public void testInMethodVarargParameter() throws Exception {
-        runTest("idea/testData/resolve/references/InMethodVarargParameter.kt");
+        runTest("../idea/testData/resolve/references/InMethodVarargParameter.kt");
     }
 
     @TestMetadata("InObjectClassObject.kt")
@@ -233,12 +233,12 @@ public class FirReferenceResolveTestGenerated extends AbstractFirReferenceResolv
 
     @TestMetadata("InVaragReferenceInFunctionBody.kt")
     public void testInVaragReferenceInFunctionBody() throws Exception {
-        runTest("idea/testData/resolve/references/InVaragReferenceInFunctionBody.kt");
+        runTest("../idea/testData/resolve/references/InVaragReferenceInFunctionBody.kt");
     }
 
     @TestMetadata("InVaragReferenceInNamedParameter.kt")
     public void testInVaragReferenceInNamedParameter() throws Exception {
-        runTest("idea/testData/resolve/references/InVaragReferenceInNamedParameter.kt");
+        runTest("../idea/testData/resolve/references/InVaragReferenceInNamedParameter.kt");
     }
 
     @TestMetadata("JavaAnnotationParameter.kt")
