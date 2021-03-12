90b65e4856dacc8b9350cb5b350aa00f460c1697
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2021-03-12 17:46:49 +0300

    Pass the project instance to statistics events: IDEA-263381
    GitOrigin-RevId: db72a98237f7925b588689394374b9c1459f615e

--- platform/platform-impl/src/com/intellij/ide/impl/UntrustedProjectEditorNotificationPanel.kt
+++ platform/platform-impl/src/com/intellij/ide/impl/UntrustedProjectEditorNotificationPanel.kt
@@ -4,18 +4,20 @@ package com.intellij.ide.impl
 import com.intellij.ide.IdeBundle
 import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.help.HelpManager
+import com.intellij.openapi.project.Project
 import com.intellij.ui.EditorNotificationPanel
 
-class UntrustedProjectEditorNotificationPanel(fileEditor: FileEditor,
+class UntrustedProjectEditorNotificationPanel(project: Project,
+                                              fileEditor: FileEditor,
                                               onTrustProjectLinkClicked: () -> Unit) : EditorNotificationPanel(fileEditor) {
   init {
     text = IdeBundle.message("untrusted.project.notification.description")
     createActionLabel(IdeBundle.message("untrusted.project.notification.trust.link"), {
-      TrustedProjectsStatistics.TRUST_PROJECT_FROM_BANNER.log()
+      TrustedProjectsStatistics.TRUST_PROJECT_FROM_BANNER.log(project)
       onTrustProjectLinkClicked()
     }, false)
     createActionLabel(IdeBundle.message("untrusted.project.notification.read.more.link"), {
-      TrustedProjectsStatistics.READ_MORE_FROM_BANNER.log()
+      TrustedProjectsStatistics.READ_MORE_FROM_BANNER.log(project)
       HelpManager.getInstance().invokeHelp(TRUSTED_PROJECTS_HELP_TOPIC)
     }, false)
   }
