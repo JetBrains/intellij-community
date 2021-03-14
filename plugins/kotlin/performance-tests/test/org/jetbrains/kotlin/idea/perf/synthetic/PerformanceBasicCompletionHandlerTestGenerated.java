b79caed5dfe7ed1582473cceada86919c2a048ea
Roman Golyshev <roman.golyshev@jetbrains.com>
2021-03-15 02:26:17 +0300

    FIR IDE: Refactor functions insertion handler
    Original commit: fe921b98ce59d997fb4dc7a4c49fd394d6619ca0
    
    GitOrigin-RevId: 972ba40b6979233a132ee41b52a2e16b3632cc6c

--- plugins/kotlin/performance-tests/test/org/jetbrains/kotlin/idea/perf/synthetic/PerformanceBasicCompletionHandlerTestGenerated.java
+++ plugins/kotlin/performance-tests/test/org/jetbrains/kotlin/idea/perf/synthetic/PerformanceBasicCompletionHandlerTestGenerated.java
@@ -148,6 +148,11 @@ public class PerformanceBasicCompletionHandlerTestGenerated extends AbstractPerf
         runTest("../completion/tests/testData/handlers/basic/KT36306.kt");
     }
 
+    @TestMetadata("LocalFunction.kt")
+    public void testLocalFunction() throws Exception {
+        runTest("idea/idea-completion/testData/handlers/basic/LocalFunction.kt");
+    }
+
     @TestMetadata("NestedTypeArg.kt")
     public void testNestedTypeArg() throws Exception {
         runTest("../completion/tests/testData/handlers/basic/NestedTypeArg.kt");
@@ -445,6 +450,11 @@ public class PerformanceBasicCompletionHandlerTestGenerated extends AbstractPerf
         @TestMetadata("HigherOrderSuspendFunctionWithArgs.kt")
         public void testHigherOrderSuspendFunctionWithArgs() throws Exception {
             runTest("../completion/tests/testData/handlers/basic/highOrderFunctions/HigherOrderSuspendFunctionWithArgs.kt");
+        }
+
+        @TestMetadata("InsertFunctionLiteralForLocalFunction.kt")
+        public void testInsertFunctionLiteralForLocalFunction() throws Exception {
+            runTest("idea/idea-completion/testData/handlers/basic/highOrderFunctions/InsertFunctionLiteralForLocalFunction.kt");
         }
 
         @TestMetadata("InsertFunctionWithSingleParameterWithBrace.kt")
