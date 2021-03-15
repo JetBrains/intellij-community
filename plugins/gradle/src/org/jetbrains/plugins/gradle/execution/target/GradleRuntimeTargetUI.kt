922f718b2dab4f5a4172625f9e716c614f80ff4b
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2021-03-15 16:17:50 +0300

    [Gradle] Run Targets: SSH allow to select the Gradle home using the path chooser IDEA-262430
    GitOrigin-RevId: 57fc5c5b3993ef127fb4e4dc174f7e6410968149

--- plugins/gradle/src/org/jetbrains/plugins/gradle/execution/target/GradleRuntimeTargetUI.kt
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/execution/target/GradleRuntimeTargetUI.kt
@@ -1,19 +1,34 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.gradle.execution.target
 
-import com.intellij.execution.target.getRuntimeType
+import com.intellij.execution.target.*
 import com.intellij.openapi.options.BoundConfigurable
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.ui.layout.*
+import org.jetbrains.plugins.gradle.util.GradleBundle.message
+import java.util.function.Supplier
 
-class GradleRuntimeTargetUI(private val config: GradleRuntimeTargetConfiguration) :
+class GradleRuntimeTargetUI<C : TargetEnvironmentConfiguration>(private val config: GradleRuntimeTargetConfiguration,
+                                                                private val targetType: TargetEnvironmentType<C>,
+                                                                private val targetSupplier: Supplier<TargetEnvironmentConfiguration>,
+                                                                private val project: Project) :
   BoundConfigurable(config.displayName, config.getRuntimeType().helpTopic) {
 
   override fun createPanel(): DialogPanel {
     return panel {
-      row("Gradle home path:") {
-        textField(config::homePath)
-          .comment("The path to the Gradle on the target")
+      row(message("gradle.target.configurable.home.path.label")) {
+        val cellBuilder: CellBuilder<*>
+        if (targetType is BrowsableTargetEnvironmentType) {
+          cellBuilder = TargetUIUtil.textFieldWithBrowseButton(this, targetType, targetSupplier,
+                                                               project,
+                                                               message("gradle.target.configurable.home.path.title"),
+                                                               config::homePath.toBinding())
+        }
+        else {
+          cellBuilder = textField(config::homePath)
+        }
+        cellBuilder.comment(message("gradle.target.configurable.home.path.comment"))
       }
     }
   }
