a4b98111b19869dc5e9d2a1e0576a54ac12146c2
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2021-03-15 13:00:39 +0700

    [java-inspections] ForwardCompatibilityInspection updated
    1. Report 'record' class name.
    2. Wording changed for 'yield' warnings
    
    GitOrigin-RevId: ed85451ff87f83622d2842857fe94a7693b32e1e

--- plugins/InspectionGadgets/src/com/siyeh/ig/jdk/ForwardCompatibilityInspection.java
+++ plugins/InspectionGadgets/src/com/siyeh/ig/jdk/ForwardCompatibilityInspection.java
@@ -58,12 +58,17 @@ public class ForwardCompatibilityInspection extends AbstractBaseJavaLocalInspect
             break;
           case PsiKeyword.VAR:
             if (languageLevel.isLessThan(LanguageLevel.JDK_10) && parent instanceof PsiClass) {
-              return JavaErrorBundle.message("var.identifier.warn");
+              return JavaErrorBundle.message("restricted.identifier.warn", PsiKeyword.VAR, 10);
             }
             break;
           case PsiKeyword.YIELD:
             if (languageLevel.isLessThan(LanguageLevel.JDK_14) && parent instanceof PsiClass) {
-              return JavaErrorBundle.message("yield.identifier.warn");
+              return JavaErrorBundle.message("restricted.identifier.warn", PsiKeyword.YIELD, 14);
+            }
+            break;
+          case PsiKeyword.RECORD:
+            if (languageLevel.isLessThan(LanguageLevel.JDK_16) && parent instanceof PsiClass) {
+              return JavaErrorBundle.message("restricted.identifier.warn", PsiKeyword.RECORD, 16);
             }
             break;
         }
--- plugins/InspectionGadgets/test/com/siyeh/igtest/jdk/forward_compatibility/UnqualifiedYield.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/jdk/forward_compatibility/UnqualifiedYield.java
@@ -1,14 +1,14 @@
 class Yield {
   class X {
     void test() {
-      <warning descr="Unqualified call to 'yield' method might not be supported in releases since Java 14">yield</warning>("x");
-      <warning descr="Unqualified call to 'yield' method might not be supported in releases since Java 14">yield</warning>(1);
+      <warning descr="Unqualified call to 'yield' method is not supported in releases since Java 14">yield</warning>("x");
+      <warning descr="Unqualified call to 'yield' method is not supported in releases since Java 14">yield</warning>(1);
     }
   }
 
   void test() {
-    <warning descr="Unqualified call to 'yield' method might not be supported in releases since Java 14">yield</warning>("x");
-    <warning descr="Unqualified call to 'yield' method might not be supported in releases since Java 14">yield</warning>(1);
+    <warning descr="Unqualified call to 'yield' method is not supported in releases since Java 14">yield</warning>("x");
+    <warning descr="Unqualified call to 'yield' method is not supported in releases since Java 14">yield</warning>(1);
   }
   
   void varYield() {
@@ -22,7 +22,7 @@ class Yield {
   static void yield(String x) {
   }
   
-  class <warning descr="Use of 'yield' as class name might not be supported in releases since Java 14">yield</warning> {}
-  class foo<<warning descr="Use of 'yield' as class name might not be supported in releases since Java 14">yield</warning>> {}
-  static <<warning descr="Use of 'yield' as class name might not be supported in releases since Java 14">yield</warning>> void foo() {}
+  class <warning descr="Use of 'yield' as a class name is not supported in releases since Java 14">yield</warning> {}
+  class foo<<warning descr="Use of 'yield' as a class name is not supported in releases since Java 14">yield</warning>> {}
+  static <<warning descr="Use of 'yield' as a class name is not supported in releases since Java 14">yield</warning>> void foo() {}
 }
