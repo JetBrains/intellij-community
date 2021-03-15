9ae42b1cfd37b8600975f061a45301f04d230fff
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2021-03-15 20:48:43 +0100

    IDEA-262420 Search everywhere throws exception: com.intellij.diagnostic.PluginException: Created extension classloader is not equal to plugin's one.
    (cherry picked from commit 18594868d2b43bc1eb46d8d4439afbc39ac96c1f)
    
    GitOrigin-RevId: b79578bdefbe07ae11ba3badb784fb441690c7b9

--- platform/lang-impl/src/com/intellij/ide/util/DefaultModuleRendererFactory.java
+++ platform/lang-impl/src/com/intellij/ide/util/DefaultModuleRendererFactory.java
@@ -1,11 +1,11 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.util;
 
+import com.intellij.openapi.extensions.InternalIgnoreDependencyViolation;
+
 import javax.swing.*;
 
-/**
- * @author yole
- */
+@InternalIgnoreDependencyViolation
 final class DefaultModuleRendererFactory extends ModuleRendererFactory {
   @Override
   public DefaultListCellRenderer getModuleRenderer() {
