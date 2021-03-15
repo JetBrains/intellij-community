cd940da9a2db4d7878b9ce8722f31a6f964df727
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2021-03-15 22:48:31 +0300

    Move notification colors to JBUI.CurrentTheme
    GitOrigin-RevId: 12d2390964a21f8f3bfb8d84c296eb41b55cef6a

--- platform/platform-impl/src/com/intellij/diagnostic/IdeMessagePanel.java
+++ platform/platform-impl/src/com/intellij/diagnostic/IdeMessagePanel.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.diagnostic;
 
 import com.intellij.icons.AllIcons;
@@ -19,9 +19,9 @@ import com.intellij.openapi.wm.impl.ProjectFrameHelper;
 import com.intellij.ui.BalloonLayout;
 import com.intellij.ui.BalloonLayoutData;
 import com.intellij.ui.ClickListener;
-import com.intellij.ui.JBColor;
 import com.intellij.ui.components.panels.NonOpaquePanel;
 import com.intellij.util.concurrency.EdtExecutorService;
+import com.intellij.util.ui.JBUI;
 import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
@@ -196,10 +196,6 @@ public final class IdeMessagePanel extends NonOpaquePanel implements MessagePool
     return frame instanceof Window && ((Window)frame).isActive();
   }
 
-  private static final Color TEXT_COLOR = JBColor.namedColor("Notification.errorForeground", NotificationsManagerImpl.DEFAULT_TEXT_COLOR);
-  private static final Color FILL_COLOR = JBColor.namedColor("Notification.errorBackground", new JBColor(0XF5E6E7, 0X593D41));
-  private static final Color BORDER_COLOR = JBColor.namedColor("Notification.errorBorderColor", new JBColor(0XE0A8A9, 0X73454B));
-
   private void showErrorNotification(@NotNull Project project) {
     String title = DiagnosticBundle.message("error.new.notification.title");
     String linkText = DiagnosticBundle.message("error.new.notification.link");
@@ -218,9 +214,9 @@ public final class IdeMessagePanel extends NonOpaquePanel implements MessagePool
 
     BalloonLayoutData layoutData = BalloonLayoutData.createEmpty();
     layoutData.fadeoutTime = 5000;
-    layoutData.textColor = TEXT_COLOR;
-    layoutData.fillColor = FILL_COLOR;
-    layoutData.borderColor = BORDER_COLOR;
+    layoutData.textColor = JBUI.CurrentTheme.Notification.Error.FOREGROUND;
+    layoutData.fillColor = JBUI.CurrentTheme.Notification.Error.BACKGROUND;
+    layoutData.borderColor = JBUI.CurrentTheme.Notification.Error.BORDER_COLOR;
 
     assert myBalloon == null;
     myBalloon = NotificationsManagerImpl.createBalloon(myFrame, notification, false, false, new Ref<>(layoutData), project);
