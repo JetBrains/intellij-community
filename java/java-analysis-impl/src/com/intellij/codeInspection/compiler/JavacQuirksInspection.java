64c30d9923356aa5843761c70ee7e28f55190d36
Ilyas Selimov <ilyas.selimov@jetbrains.com>
2021-03-16 12:52:32 +0700

    JavacQuirks - revert occasional fix
    GitOrigin-RevId: 777bcca61b4515d9126631edbceda3dce093bc1d

--- java/java-analysis-impl/src/com/intellij/codeInspection/compiler/JavacQuirksInspection.java
+++ java/java-analysis-impl/src/com/intellij/codeInspection/compiler/JavacQuirksInspection.java
@@ -8,75 +8,7 @@ import com.intellij.psi.PsiElementVisitor;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class JavacQuirksInspection extends AbstractBaseJavaLocalInspectionTool {
-  {
-    //noinspection RedundantTypeArguments (explicit type arguments speedup compilation and analysis time)
-    Arrays.<List<String>>asList(
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b"),
-      Arrays.asList("a", "b")
-    );
-  }
   @Override
   public boolean isEnabledByDefault() {
     return true;
