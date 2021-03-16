23103c4883c9f2037d4f36c30c4392a90f5be00e
Anna Kozlova <anna.kozlova@jetbrains.com>
2021-03-16 14:26:10 +0100

    EA-259614 - REWA: ApplicationImpl.startWrite
    GitOrigin-RevId: 7b8a8f69ff92deb9afa5a694935024e0b891a23e

--- java/java-impl/src/com/intellij/refactoring/migration/MigrationProcessor.java
+++ java/java-impl/src/com/intellij/refactoring/migration/MigrationProcessor.java
@@ -56,7 +56,7 @@ public class MigrationProcessor extends BaseRefactoringProcessor {
     super(project);
     myMigrationMap = migrationMap;
     mySearchScope = scope;
-    myPsiMigration = startMigration(project);
+    
   }
 
   @Override
@@ -86,6 +86,12 @@ public class MigrationProcessor extends BaseRefactoringProcessor {
   @Override
   protected void refreshElements(PsiElement @NotNull [] elements) {
     myPsiMigration = startMigration(myProject);
+  }
+
+  @Override
+  protected void doRun() {
+    myPsiMigration = startMigration(myProject);
+    super.doRun();
   }
 
   @Override
