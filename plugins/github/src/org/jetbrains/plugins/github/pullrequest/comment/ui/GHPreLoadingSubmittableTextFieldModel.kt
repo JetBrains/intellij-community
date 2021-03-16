4b49f688ff1bbd942028ce51630f287f215f7aea
Ivan Semenov <ivan.semenov@jetbrains.com>
2021-03-16 17:33:58 +0300

    [github] allow `\r` in PR texts
    Fixes IDEA-263510
    
    GitOrigin-RevId: 8392a97b72b6d78c762f50f66c162458c007c48d

--- plugins/github/src/org/jetbrains/plugins/github/pullrequest/comment/ui/GHPreLoadingSubmittableTextFieldModel.kt
+++ plugins/github/src/org/jetbrains/plugins/github/pullrequest/comment/ui/GHPreLoadingSubmittableTextFieldModel.kt
@@ -1,7 +1,8 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.github.pullrequest.comment.ui
 
 import com.intellij.openapi.application.runWriteAction
+import com.intellij.openapi.editor.impl.DocumentImpl
 import org.jetbrains.plugins.github.util.completionOnEdt
 import org.jetbrains.plugins.github.util.errorOnEdt
 import org.jetbrains.plugins.github.util.successOnEdt
@@ -13,6 +14,8 @@ class GHPreLoadingSubmittableTextFieldModel(initialText: String,
   : GHSubmittableTextFieldModel(initialText, submitter) {
 
   init {
+    document as DocumentImpl
+    document.setAcceptSlashR(true)
     document.setReadOnly(true)
     isBusy = true
     preLoader.successOnEdt {
