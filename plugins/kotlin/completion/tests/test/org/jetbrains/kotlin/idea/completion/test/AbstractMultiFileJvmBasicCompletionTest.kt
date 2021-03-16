2d1bf159a36737fea39f795d266de0310da13d79
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-03-16 16:55:39 +0700

    [tests] completion: remove sdk from basic completion
    MultiFileJvmBasicCompletionTestGenerated.testFileRefInStringLiteralNoPrefix: fixed
    
    GitOrigin-RevId: 8d38e0cce6ee6b429463ff1eb906e6ac2da772f7

--- plugins/kotlin/completion/tests/test/org/jetbrains/kotlin/idea/completion/test/AbstractMultiFileJvmBasicCompletionTest.kt
+++ plugins/kotlin/completion/tests/test/org/jetbrains/kotlin/idea/completion/test/AbstractMultiFileJvmBasicCompletionTest.kt
@@ -6,12 +6,15 @@
 package org.jetbrains.kotlin.idea.completion.test
 
 import com.intellij.codeInsight.completion.CompletionType
+import com.intellij.openapi.projectRoots.Sdk
 import org.jetbrains.kotlin.idea.test.AstAccessControl
 import org.jetbrains.kotlin.platform.jvm.JvmPlatforms
 import org.jetbrains.kotlin.test.KotlinTestUtils
 import java.io.File
 
 abstract class AbstractMultiFileJvmBasicCompletionTest : KotlinCompletionTestCase() {
+    override fun getTestProjectJdk(): Sdk? = null
+
     protected fun doTest(testPath: String) {
         configureByFile(getTestName(false) + ".kt", "")
         val shouldFail = testPath.contains("NoSpecifiedType")
