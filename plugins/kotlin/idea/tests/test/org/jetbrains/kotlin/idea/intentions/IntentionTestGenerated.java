6dc353ef51d3b67d2a082466645bdf00a0c8fabf
Toshiaki Kameyama <kmymtsak@gmail.com>
2021-03-17 20:20:30 +0900

    RemoveBracesIntention: don't suggest when block has a assignment statement
    #KTIJ-5639 Fixed
    
    GitOrigin-RevId: a03232733b10135c519544576a330fe82eaa8d9a

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/intentions/IntentionTestGenerated.java
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/intentions/IntentionTestGenerated.java
@@ -13528,6 +13528,11 @@ public abstract class IntentionTestGenerated extends AbstractIntentionTest {
             runTest("testData/intentions/removeBraces/doWhile.kt");
         }
 
+        @TestMetadata("doWhileWithLocalVariable.kt")
+        public void testDoWhileWithLocalVariable() throws Exception {
+            runTest("testData/intentions/removeBraces/doWhileWithLocalVariable.kt");
+        }
+
         @TestMetadata("doWhileWithTwoStatements.kt")
         public void testDoWhileWithTwoStatements() throws Exception {
             runTest("testData/intentions/removeBraces/doWhileWithTwoStatements.kt");
@@ -13633,6 +13638,11 @@ public abstract class IntentionTestGenerated extends AbstractIntentionTest {
             runTest("testData/intentions/removeBraces/ifWithComment.kt");
         }
 
+        @TestMetadata("ifWithLocalVariable.kt")
+        public void testIfWithLocalVariable() throws Exception {
+            runTest("testData/intentions/removeBraces/ifWithLocalVariable.kt");
+        }
+
         @TestMetadata("ifWithNoStatement.kt")
         public void testIfWithNoStatement() throws Exception {
             runTest("testData/intentions/removeBraces/ifWithNoStatement.kt");
@@ -13711,6 +13721,11 @@ public abstract class IntentionTestGenerated extends AbstractIntentionTest {
         @TestMetadata("whileOutsideBlock.kt")
         public void testWhileOutsideBlock() throws Exception {
             runTest("testData/intentions/removeBraces/whileOutsideBlock.kt");
+        }
+
+        @TestMetadata("whileWithLocalVariable.kt")
+        public void testWhileWithLocalVariable() throws Exception {
+            runTest("testData/intentions/removeBraces/whileWithLocalVariable.kt");
         }
 
         @TestMetadata("whileWithTwoStatements.kt")
