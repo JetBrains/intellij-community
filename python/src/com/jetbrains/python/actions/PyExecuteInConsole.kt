f5e6c0db2ab38abc9460ff9a272d08f57ee7d6d9
Elizaveta Shashkova <elizaveta.shashkova@jetbrains.com>
2021-03-17 18:12:46 +0300

    IJ-CR-6110 Fixes after review
    GitOrigin-RevId: 760a61a6a28b0ca8acb0aedbc7bc3f1d8f25d9eb

--- python/src/com/jetbrains/python/actions/PyExecuteInConsole.kt
+++ python/src/com/jetbrains/python/actions/PyExecuteInConsole.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.python.actions
 
-import com.google.common.collect.Iterables
 import com.intellij.execution.ui.RunContentDescriptor
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.impl.EditorImpl
