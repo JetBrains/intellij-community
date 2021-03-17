dccb0b3f17228cd14d15839357ee72bab3df8c17
Alexey Kalina <alexey.kalina@jetbrains.com>
2021-03-17 12:31:11 +0300

    [ml-completion] EA-254313: fix freezing in vcs features calculation
    (cherry picked from commit 022a8284dd8ff2bd29b9fd63c26fb50689df7a3e)
    
    IJ-CR-7180
    
    GitOrigin-RevId: 8b3fbc830c6931ac94470c58f95c90c77435e96f

--- platform/vcs-impl/src/com/intellij/codeInsight/completion/ml/VcsFeatureProvider.kt
+++ platform/vcs-impl/src/com/intellij/codeInsight/completion/ml/VcsFeatureProvider.kt
@@ -1,14 +1,22 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.completion.ml
 
-import com.intellij.codeInsight.actions.FormatChangedTextUtil
+import com.intellij.codeInsight.actions.VcsFacade
 import com.intellij.codeInsight.completion.CompletionLocation
 import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.vcs.changes.Change
 import com.intellij.openapi.vcs.changes.ChangeListManager
 import com.intellij.psi.PsiNameIdentifierOwner
+import com.intellij.util.ConcurrencyUtil
+import java.util.concurrent.Callable
+import java.util.concurrent.FutureTask
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
 
 class VcsFeatureProvider : ElementFeatureProvider {
+  private val executor = ConcurrencyUtil.newSingleThreadExecutor("ML completion. VCS feature calculation")
+
   override fun getName(): String = "vcs"
 
   override fun calculateFeatures(element: LookupElement,
@@ -24,13 +32,29 @@ class VcsFeatureProvider : ElementFeatureProvider {
         features["file_state"] = MLFeatureValue.categorical(change.type) // NON-NLS
 
         if (change.type == Change.Type.MODIFICATION && psi is PsiNameIdentifierOwner) {
-          val changedRanges = FormatChangedTextUtil.getInstance().getChangedTextRanges(location.project, psiFile)
-          if (changedRanges.any { psi.textRange?.intersects(it) == true }) {
-            features["declaration_is_changed"] = MLFeatureValue.binary(true) // NON-NLS
+          calculateWithTimeout(100, TimeUnit.MILLISECONDS) {
+            runReadAction {
+              val changedRanges = VcsFacade.getInstance().getChangedTextRanges(location.project, psiFile)
+              if (changedRanges.any { psi.textRange?.intersects(it) == true })
+                MLFeatureValue.binary(true) else null
+            }
+          }?.let {
+            features["declaration_is_changed"] = it // NON-NLS
           }
         }
       }
     }
     return features
+  }
+
+  private fun calculateWithTimeout(timeout: Long, timeUnit: TimeUnit, callable: Callable<MLFeatureValue?>): MLFeatureValue? {
+    val future = FutureTask(callable)
+    executor.execute(future)
+    var result: MLFeatureValue? = null
+    try {
+      result = future.get(timeout, timeUnit)
+    }
+    catch (ignored: TimeoutException) { }
+    return result
   }
 }
