e85b4c0145bdb8596551c68fe7d8a4981dacc34e
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2021-03-17 23:11:21 +0300

    suppress warning
    GitOrigin-RevId: d0953f447f96d9249cf80c5b82708c14be760c18

--- platform/lang-impl/src/com/intellij/util/indexing/impl/storage/SwitchFileBasedIndexStorageAction.kt
+++ platform/lang-impl/src/com/intellij/util/indexing/impl/storage/SwitchFileBasedIndexStorageAction.kt
@@ -69,6 +69,7 @@ private class IndexStorageDescriptorPopupContext(private val project: Project,
     return model
   }
 
+  @Suppress("HardCodedStringLiteral")
   override fun getRenderer(): ListCellRenderer<IndexStorageDescriptor> {
     return SimpleListCellRenderer.create { label, value, _ -> label.text = value.presentableName }
   }
