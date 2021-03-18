37f5fb561c2c5098dde3770c5b3f280c4260f88d
Sergey Shanshin <sergey.shanshin@jetbrains.com>
2021-03-18 05:44:03 +0300

    Add inspections and quickfixes of redundant Json format instantiation
    Resolves KT-45075
    
    Original commit: cfca7183e5831d258bc2fa8ea40700cd16d3b753
    
    GitOrigin-RevId: e9b514d0b3bc2d726ce5c0c0e8ecbd689a4434bd

---
+++ plugins/kotlin/compiler-plugins/kotlinx-serialization/common/src/org/jetbrains/kotlin/idea/compilerPlugin/kotlinxSerialization/KotlinSerializationBundle.kt
@@ -0,0 +1,19 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package org.jetbrains.kotlinx.serialization.idea
+
+import org.jetbrains.annotations.NonNls
+import org.jetbrains.annotations.PropertyKey
+import org.jetbrains.kotlin.util.AbstractKotlinBundle
+
+@NonNls
+private const val BUNDLE = "messages.KotlinSerializationBundle"
+
+object KotlinSerializationBundle : AbstractKotlinBundle(BUNDLE) {
+    @JvmStatic
+    fun message(@NonNls @PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String = getMessage(key, *params)
+
+    @JvmStatic
+    fun htmlMessage(@NonNls @PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String =
+        getMessage(key, *params).withHtml()
+}
---
+++ plugins/kotlin/compiler-plugins/kotlinx-serialization/common/test/org/jetbrains/kotlin/idea/compilerPlugin/kotlinxSerialization/AbstractSerializationPluginIdeDiagnosticTest.kt
@@ -0,0 +1,23 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package org.jetbrains.kotlinx.serialization.idea
+
+import org.jetbrains.kotlin.ObsoleteTestInfrastructure
+import org.jetbrains.kotlin.checkers.AbstractDiagnosticsTest
+import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.jetbrains.kotlin.cli.jvm.config.JvmClasspathRoot
+import org.jetbrains.kotlin.extensions.StorageComponentContainerContributor
+import org.jetbrains.kotlinx.serialization.compiler.extensions.SerializationIDEContainerContributor
+
+@OptIn(ObsoleteTestInfrastructure::class)
+abstract class AbstractSerializationPluginIdeDiagnosticTest : AbstractDiagnosticsTest() {
+    private val coreLibraryPath = getSerializationCoreLibraryJar()!!
+    private val jsonLibraryPath = getSerializationJsonLibraryJar()!!
+
+    override fun setupEnvironment(environment: KotlinCoreEnvironment) {
+        if (!StorageComponentContainerContributor.getInstances(project).any { it is SerializationIDEContainerContributor }) {
+            StorageComponentContainerContributor.registerExtension(project, SerializationIDEContainerContributor())
+        }
+        environment.updateClasspath(listOf(JvmClasspathRoot(coreLibraryPath), JvmClasspathRoot(jsonLibraryPath)))
+    }
+}
