27df069eb5e845e975078475bd942b2d5f458583
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2021-03-18 13:08:10 +0300

    Use compiler arguments for Java against Kotlin highlighting tests
     This is needed for the next commit to check jvm-default options
    
     #KT-41130 In Progress
    
    Original commit: 13df529a5aea522ec776964e53dc1d62e99a0548
    
    GitOrigin-RevId: e3e187e85eaf925f3326a55acd010676022c3c49

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/checkers/AbstractJavaAgainstKotlinCheckerTest.kt
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/checkers/AbstractJavaAgainstKotlinCheckerTest.kt
@@ -4,11 +4,23 @@ package org.jetbrains.kotlin.checkers
 
 import org.jetbrains.kotlin.idea.perf.forceUsingOldLightClassesForTest
 import java.io.File
+import org.jetbrains.kotlin.idea.test.withCustomCompilerOptions
 
 abstract class AbstractJavaAgainstKotlinSourceCheckerTest : AbstractJavaAgainstKotlinCheckerTest() {
     fun doTest(path: String) {
         val relativePath = File(path).toRelativeString(File(testDataPath))
-        doTest(true, true, relativePath.replace(".kt", ".java"), relativePath)
+        fun doTest() {
+            doTest(true, true, relativePath.replace(".kt", ".java"), relativePath)
+        }
+
+        val configFile = configFileText
+        if (configFile != null) {
+            withCustomCompilerOptions(configFile, project, module) {
+                doTest()
+            }
+        } else {
+            doTest()
+        }
     }
 }
 
