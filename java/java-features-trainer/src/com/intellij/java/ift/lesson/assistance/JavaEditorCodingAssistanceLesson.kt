e32773646c4b2d8fb8daf18c92faf7571f80c6d0
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2021-03-18 19:02:53 +0300

    IFT-181 Improve Editor coding assistance lesson
    IDEA-CR-70982
    
    GitOrigin-RevId: 1f78e7471f3391dbbbe9281d7db10766ea1c548f

--- java/java-features-trainer/src/com/intellij/java/ift/lesson/assistance/JavaEditorCodingAssistanceLesson.kt
+++ java/java-features-trainer/src/com/intellij/java/ift/lesson/assistance/JavaEditorCodingAssistanceLesson.kt
@@ -2,8 +2,12 @@
 package com.intellij.java.ift.lesson.assistance
 
 import com.intellij.codeInsight.daemon.QuickFixBundle
+import com.intellij.codeInsight.daemon.impl.runActionCustomShortcutSet
+import com.intellij.java.ift.JavaLessonsBundle
+import com.intellij.openapi.actionSystem.KeyboardShortcut
 import com.siyeh.InspectionGadgetsBundle
 import training.dsl.LessonSample
+import training.dsl.LessonUtil
 import training.learn.lesson.general.assistance.EditorCodingAssistanceLesson
 
 class JavaEditorCodingAssistanceLesson(sample: LessonSample) :
@@ -17,4 +21,9 @@ class JavaEditorCodingAssistanceLesson(sample: LessonSample) :
   override val warningFixedText: String = "new String[0]"
 
   override val variableNameToHighlight: String = "lines"
+
+  override fun getFixWarningText(): String {
+    val shortcut = runActionCustomShortcutSet.shortcuts.first() as KeyboardShortcut
+    return JavaLessonsBundle.message("java.editor.coding.assistance.press.to.fix", LessonUtil.rawKeyStroke(shortcut.firstKeyStroke))
+  }
 }
