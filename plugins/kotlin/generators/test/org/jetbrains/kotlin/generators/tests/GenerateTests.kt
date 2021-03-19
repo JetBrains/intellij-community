8fae376d83b59937a1f1fb0c53ab24cdeee4427e
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-03-19 15:58:15 +0100

    FIR IDE: introduce AbstractSymbolByReferencePointerRestoreTest
    Original commit: 0bb37db4bfe9b490845b8574767e53490695aacc
    
    GitOrigin-RevId: 2d00e7a2e5ce83582f8a1504a17dfe6877fe7e5f

--- plugins/kotlin/generators/test/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ plugins/kotlin/generators/test/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -1010,6 +1010,10 @@ private fun assembleWorkspace(): TWorkspace = workspace {
             model("resoreSymbolFromLibrary", pattern = TXT)
         }
 
+        testClass<AbstractSymbolByReferencePointerRestoreTest> {
+            model("symbolByReference")
+        }
+
         testClass<AbstractMemoryLeakInSymbolsTest> {
             model("symbolMemoryLeak")
         }
