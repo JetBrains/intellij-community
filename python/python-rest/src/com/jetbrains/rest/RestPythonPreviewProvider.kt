a0fdf44e3a9b84d1a93fd8c4d382c0edc3b9c69a
Andrey Lisin <andrey.lisin@jetbrains.com>
2021-03-19 13:42:10 +0300

    PY-31110 Display unknown reStructuredText directives verbatim
    (cherry picked from commit d85b7c8b514342d5cab1fe200bd08477f5081566)
    
    IDEA-CR-70988
    
    GitOrigin-RevId: 1dfc4cbe4edaa8395da7b34699a4f2550b1b2b59

--- python/python-rest/src/com/jetbrains/rest/RestPythonPreviewProvider.kt
+++ python/python-rest/src/com/jetbrains/rest/RestPythonPreviewProvider.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.rest
 
-import com.google.common.collect.Lists
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.vfs.VirtualFile
@@ -17,11 +16,15 @@ class RestPythonPreviewProvider : RestPreviewProvider() {
     val module = ProjectFileIndex.SERVICE.getInstance(project).getModuleForFile(virtualFile) ?: return null
 
     val sdk = PythonSdkUtil.findPythonSdk(module) ?: return Pair("", PyBundle.message("python.sdk.no.interpreter.configured.warning"))
-    val commandLine = REST_RUNNER.newCommandLine(sdk, Lists.newArrayList("rst2html"))
+    val commandLine = REST_RUNNER.newCommandLine(sdk, listOf("rst2html"))
     val output = PySdkUtil.getProcessOutput(commandLine, virtualFile.parent.path, null, 5000,
                                             text.toByteArray(Charsets.UTF_8), false)
     return if (output.isCancelled || output.isTimeout)
       Pair("", PythonRestBundle.message("python.rest.failed.to.generate.html"))
-    else Pair(output.stdout, "<h3>" + PythonRestBundle.message("python.rest.error.output") + "</h3>" + output.stderr)
+    else {
+      RestOutputHandler.EP_NAME.extensions.fold(output.stdout) { acc, handler -> handler.apply(acc) }.let {
+        return Pair(it, "<h3>" + PythonRestBundle.message("python.rest.error.output") + "</h3>" + output.stderr)
+      }
+    }
   }
 }
