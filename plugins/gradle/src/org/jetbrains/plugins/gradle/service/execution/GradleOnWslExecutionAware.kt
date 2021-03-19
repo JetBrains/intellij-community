d44b213bff6918bec0cbb2e64e62a1deff792dd7
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2021-03-19 18:55:26 +0300

    [Gradle] validate Gradle JDK configuration when running on WSL IDEA-264439
    GitOrigin-RevId: b6dbcff9650990db6134bec27a4c3128806d2e07

--- plugins/gradle/src/org/jetbrains/plugins/gradle/service/execution/GradleOnWslExecutionAware.kt
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/service/execution/GradleOnWslExecutionAware.kt
@@ -1,20 +1,28 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.gradle.service.execution
 
+import com.intellij.build.issue.BuildIssue
+import com.intellij.build.issue.BuildIssueQuickFix
 import com.intellij.execution.target.HostPort
 import com.intellij.execution.target.TargetEnvironmentConfiguration
 import com.intellij.execution.wsl.WSLDistribution
 import com.intellij.execution.wsl.WslDistributionManager
 import com.intellij.execution.wsl.WslPath
 import com.intellij.execution.wsl.target.WslTargetEnvironmentConfiguration
+import com.intellij.openapi.externalSystem.issue.BuildIssueException
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTask
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskNotificationListener
 import com.intellij.openapi.externalSystem.service.execution.ExternalSystemExecutionAware
 import com.intellij.openapi.externalSystem.service.execution.ExternalSystemRunConfiguration
 import com.intellij.openapi.externalSystem.service.execution.TargetEnvironmentConfigurationProvider
+import com.intellij.openapi.externalSystem.service.internal.ExternalSystemResolveProjectTask
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ui.configuration.SdkLookupProvider.SdkInfo.Resolved
+import com.intellij.pom.Navigatable
 import com.intellij.util.PathMapper
 import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.plugins.gradle.issue.quickfix.GradleSettingsQuickFix
+import org.jetbrains.plugins.gradle.util.GradleBundle.message
 
 @ApiStatus.Internal
 class GradleOnWslExecutionAware : ExternalSystemExecutionAware {
@@ -25,6 +33,17 @@ class GradleOnWslExecutionAware : ExternalSystemExecutionAware {
     taskNotificationListener: ExternalSystemTaskNotificationListener,
     project: Project
   ) {
+    if (isPreviewMode) return
+    val wslDistribution = resolveWslDistribution(externalProjectPath) ?: return
+    // wait for JVM resolution and run common JVM checks
+    val sdkInfo = GradleExecutionAware().prepareJvmForExecution(task, externalProjectPath, taskNotificationListener, project)
+    if (sdkInfo !is Resolved) return
+    val homePath = sdkInfo.homePath ?: return
+    val jdkWslDistribution = WslPath.getDistributionByWindowsUncPath(homePath)
+    if (wslDistribution.id != jdkWslDistribution?.id) {
+      val isProjectResolveProjectTask = task is ExternalSystemResolveProjectTask
+      throw BuildIssueException(IncorrectGradleJdkIssue(externalProjectPath, wslDistribution, homePath, isProjectResolveProjectTask))
+    }
   }
 
   override fun getEnvironmentConfigurationProvider(runConfiguration: ExternalSystemRunConfiguration,
@@ -70,5 +89,28 @@ class GradleOnWslExecutionAware : ExternalSystemExecutionAware {
   private fun resolveWslDistribution(path: String): WSLDistribution? {
     if (!WslDistributionManager.isWslPath(path)) return null
     return WslPath.getDistributionByWindowsUncPath(path)
+  }
+
+  @ApiStatus.Internal
+  class IncorrectGradleJdkIssue(projectPath: String,
+                                distribution: WSLDistribution,
+                                jdkHomePath: String,
+                                isProjectResolveProjectTask: Boolean) : BuildIssue {
+    override val title: String = message("gradle.incorrect.jvm.issue.title")
+    override val description: String
+    override val quickFixes: List<BuildIssueQuickFix>
+    override fun getNavigatable(project: Project): Navigatable? = null
+
+    init {
+      val gradleSettingsFix = GradleSettingsQuickFix(projectPath, isProjectResolveProjectTask,
+                                                     { oldSettings, currentSettings -> oldSettings.gradleJvm != currentSettings.gradleJvm },
+                                                     message("gradle.settings.text.jvm.path"))
+      quickFixes = listOf(gradleSettingsFix)
+      val distributionPath = distribution.uncRootPath.toString()
+      val distributionName = distribution.presentableName
+      val message = message("gradle.incorrect.jvm.wsl.issue.description", jdkHomePath, distributionName, distributionPath)
+      val fixLink = "<a href=\"${gradleSettingsFix.id}\">${message("gradle.open.gradle.settings")}</a>"
+      description = "$message\n$fixLink\n"
+    }
   }
 }
