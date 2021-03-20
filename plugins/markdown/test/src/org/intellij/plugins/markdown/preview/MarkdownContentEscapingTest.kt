3b65c50101692454c554ed05350c2c290cadf45a
Ivan Posti <ivan.posti@jetbrains.com>
2021-03-20 04:11:59 +0300

    [Markdown] Add escaping tests with the actual JCEF preview
    Should be run with `intellij.test.standalone=true`.
    
    (cherry picked from commit ecd6c6065594d737c7d83d5b20d61b7eb3e9254f)
    
    GitOrigin-RevId: cf111a327043d748afec267347d0156f529fe9ef

---
+++ plugins/markdown/test/src/org/intellij/plugins/markdown/preview/MarkdownContentEscapingTest.kt
@@ -0,0 +1,80 @@
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.intellij.plugins.markdown.preview
+
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.testFramework.ApplicationRule
+import com.intellij.ui.scale.TestScaleHelper
+import org.intellij.plugins.markdown.MarkdownTestingUtil
+import org.intellij.plugins.markdown.preview.MarkdownJCEFPreviewTestUtil.collectPageSource
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
+import org.junit.AfterClass
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.ClassRule
+import org.junit.Test
+import java.io.File
+
+/**
+ * Run with *intellij.test.standalone=true*
+ *
+ * Basically all these tests are checking if the passed to the preview HTML
+ * stays the same after conversion to incremental DOM.
+ */
+class MarkdownContentEscapingTest {
+  @Test
+  fun `applied patch sanity`() = doTest("appliedPatchSanity")
+
+  @Test
+  fun `backtick in code fence`() = doTest("backtickInCodeFence")
+
+  @Test
+  fun `newline in link title`() = doTest("newlineInLinkTitle")
+
+  @Test
+  fun `backtick in link title`() = doTest("backtickInLinkTitle")
+
+  @Test
+  fun `code fence with backslashes`() = doTest("codeFenceWithBackslashes")
+
+  @Test
+  fun `interesting symbols`() = doTest("interestingSymbols")
+
+  private fun doTest(name: String) {
+    TestScaleHelper.assumeStandalone()
+    val content = File(testPath, "$name.html").readText()
+    val panel = MarkdownJCEFPreviewTestUtil.setupPreviewPanel(content)
+    val expected = parseContentBody(content)
+    val got = parseContentBody(panel.collectPageSource()!!)
+    assertTrue(got.children().isNotEmpty())
+    assertEquals(expected.html(), got.child(0).html())
+  }
+
+  private fun parseContentBody(html: String): Element {
+    val dom = Jsoup.parse(html)
+    dom.outputSettings()
+      .outline(true)
+      .indentAmount(0)
+    return dom.body()
+  }
+
+  companion object {
+    init {
+      TestScaleHelper.setSystemProperty("java.awt.headless", "false")
+      //TestScaleHelper.setRegistryProperty("ide.browser.jcef.headless.enabled", "true")
+      TestScaleHelper.setRegistryProperty("ide.browser.jcef.testMode.enabled", "true")
+    }
+
+    private val testPath = FileUtil.join(MarkdownTestingUtil.TEST_DATA_PATH!!, "preview", "escaping")
+
+    @ClassRule
+    @JvmField
+    val appRule = ApplicationRule()
+
+    @AfterClass
+    @JvmStatic
+    fun after() {
+      TestScaleHelper.restoreProperties()
+    }
+  }
+}
