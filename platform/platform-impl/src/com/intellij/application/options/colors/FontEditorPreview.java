552f30c0a7b10aba87147d63965530923dd7125c
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2021-03-22 22:17:14 +0300

    IDEA-260495 Font Settings: add sample of bold font to preview
    GitOrigin-RevId: 5bcf812c7652ebcb5ef8f315ebdb6f9c592e6c4d

--- platform/platform-impl/src/com/intellij/application/options/colors/FontEditorPreview.java
+++ platform/platform-impl/src/com/intellij/application/options/colors/FontEditorPreview.java
@@ -27,18 +27,27 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.EditorFactory;
 import com.intellij.openapi.editor.EditorSettings;
 import com.intellij.openapi.editor.colors.EditorColorsScheme;
+import com.intellij.openapi.editor.event.DocumentEvent;
+import com.intellij.openapi.editor.event.DocumentListener;
 import com.intellij.openapi.editor.ex.EditorEx;
 import com.intellij.openapi.editor.ex.EditorGutterComponentEx;
 import com.intellij.openapi.editor.ex.EditorMarkupModel;
+import com.intellij.openapi.editor.highlighter.EditorHighlighter;
+import com.intellij.openapi.editor.highlighter.HighlighterClient;
+import com.intellij.openapi.editor.highlighter.HighlighterIterator;
 import com.intellij.openapi.editor.impl.ContextMenuPopupHandler;
 import com.intellij.openapi.editor.markup.AnalyzerStatus;
 import com.intellij.openapi.editor.markup.ErrorStripeRenderer;
+import com.intellij.openapi.editor.markup.TextAttributes;
 import com.intellij.openapi.fileEditor.impl.FileDocumentManagerBase;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.util.Key;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.tree.IElementType;
 import com.intellij.testFramework.LightVirtualFile;
 import com.intellij.ui.JBColor;
 import com.intellij.util.EventDispatcher;
+import com.intellij.util.ObjectUtils;
 import com.intellij.util.ui.JBUI;
 import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.Nls;
@@ -47,13 +56,20 @@ import org.jetbrains.annotations.NotNull;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.function.Supplier;
 
 public class FontEditorPreview implements PreviewPanel{
   private static final String PREVIEW_TEXT_KEY = "FontPreviewText";
 
+  private static final Key<PreviewTextModel> TEXT_MODEL_KEY = Key.create("RestorePreviewTextAction.textModel");
+
   private final EditorEx myEditor;
   private final JPanel myTopPanel;
+
+  private final PreviewTextModel myTextModel;
 
   private final Supplier<? extends EditorColorsScheme> mySchemeSupplier;
 
@@ -64,8 +80,12 @@ public class FontEditorPreview implements PreviewPanel{
 
     @Nls String text = PropertiesComponent.getInstance().getValue(PREVIEW_TEXT_KEY, getIDEDemoText());
 
-    myEditor = (EditorEx)createPreviewEditor(text, mySchemeSupplier.get(), editable);
+    myTextModel = new PreviewTextModel(text);
+    myEditor = (EditorEx)createPreviewEditor(myTextModel.getText(), mySchemeSupplier.get(), editable);
     myEditor.setBorder(JBUI.Borders.empty());
+    myEditor.setHighlighter(new PreviewHighlighter(myTextModel, myEditor.getDocument()));
+    myEditor.getDocument().addDocumentListener(myTextModel);
+
     myTopPanel = new JPanel(new BorderLayout());
     myTopPanel.add(myEditor.getComponent(), BorderLayout.CENTER);
 
@@ -86,8 +106,8 @@ public class FontEditorPreview implements PreviewPanel{
     return JBUI.Borders.customLine(JBColor.border());
   }
 
-  private static void registerRestoreAction(EditorEx editor) {
-    editor.putUserData(RestorePreviewTextAction.OUR_EDITOR, Boolean.TRUE);
+  private void registerRestoreAction(EditorEx editor) {
+    editor.putUserData(TEXT_MODEL_KEY, myTextModel);
     AnAction restoreAction = ActionManager.getInstance().getAction(IdeActions.ACTION_RESTORE_FONT_PREVIEW_TEXT);
     if (restoreAction != null) {
       String originalGroupId = editor.getContextMenuGroupId();
@@ -104,19 +124,26 @@ public class FontEditorPreview implements PreviewPanel{
   private static String getIDEDemoText() {
     return
       ApplicationNamesInfo.getInstance().getFullProductName() +
-      " is an Integrated \n" +
-      "Development Environment (IDE) designed\n" +
+      " is an <bold>Integrated \n" +
+      "Development Environment (IDE)</bold> designed\n" +
       "to maximize productivity. It provides\n" +
-      "clever code completion, static code\n" +
-      "analysis, and refactorings, and lets\n" +
+      "<bold>clever code completion, static code\n" +
+      "analysis, and refactorings,</bold> and lets\n" +
       "you focus on the bright side of\n" +
       "software development making\n" +
       "it an enjoyable experience.\n" +
       "\n" +
+      "Default:\n" +
       "abcdefghijklmnopqrstuvwxyz\n" +
       "ABCDEFGHIJKLMNOPQRSTUVWXYZ\n" +
       " 0123456789 (){}[]\n" +
       " +-*/= .,;:!? #&$%@|^\n" +
+      "\n" +
+      "<bold>Bold:\n" +
+      "abcdefghijklmnopqrstuvwxyz\n" +
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZ\n" +
+      " 0123456789 (){}[]\n" +
+      " +-*/= .,;:!? #&$%@|^</bold>\n" +
       "\n" +
       "<!-- -- != := === >= >- >=> |-> -> <$>\n" +
       "</> #[ |||> |= ~@\n" +
@@ -180,12 +207,11 @@ public class FontEditorPreview implements PreviewPanel{
 
   @Override
   public void disposeUIResources() {
-    String previewText = myEditor.getDocument().getText();
-    if (previewText.equals(getIDEDemoText())) {
+    if (myTextModel.isDefault()) {
       PropertiesComponent.getInstance().unsetValue(PREVIEW_TEXT_KEY);
     }
     else {
-      PropertiesComponent.getInstance().setValue(PREVIEW_TEXT_KEY, previewText);
+      PropertiesComponent.getInstance().setValue(PREVIEW_TEXT_KEY, myTextModel.getRawText());
     }
     EditorFactory.getInstance().releaseEditor(myEditor);
   }
@@ -198,24 +224,271 @@ public class FontEditorPreview implements PreviewPanel{
   }
 
   public static class RestorePreviewTextAction extends DumbAwareAction {
-    private static final Key<Boolean> OUR_EDITOR = Key.create("RestorePreviewTextAction.editor");
 
     @Override
     public void update(@NotNull AnActionEvent e) {
       Editor editor = e.getData(CommonDataKeys.EDITOR);
+      PreviewTextModel textModel = ObjectUtils.doIfNotNull(editor, it->it.getUserData(TEXT_MODEL_KEY));
       e.getPresentation().setEnabledAndVisible(editor != null &&
-                                               editor.getUserData(OUR_EDITOR) != null &&
-                                               !editor.getDocument().getText().equals(getIDEDemoText()));
+                                               textModel != null &&
+                                               !textModel.isDefault());
     }
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
       Editor editor = e.getData(CommonDataKeys.EDITOR);
       if (editor != null) {
-        WriteCommandAction.runWriteCommandAction(editor.getProject(), null, null, () -> {
-          editor.getDocument().setText(getIDEDemoText());
-        });
+        PreviewTextModel textModel = editor.getUserData(TEXT_MODEL_KEY);
+        if (textModel != null) {
+          textModel.resetToDefault();
+          WriteCommandAction.runWriteCommandAction(editor.getProject(), null, null, () -> {
+            editor.getDocument().setText(textModel.getText());
+          });
+        }
       }
+    }
+  }
+
+  private static class PreviewTextModel implements DocumentListener {
+    private final static String BOLD_START_MARKER = "<bold>";
+    private final static String BOLD_END_MARKER = "</bold>";
+
+    private final static RangeHighlightingData EMPTY_RANGE = new RangeHighlightingData(0, 0, false);
+
+    private String myText;
+    private final List<RangeHighlightingData> myRanges = new ArrayList<>();
+
+    private PreviewTextModel(String rawPreviewText) {
+      extractMarkersAndText(rawPreviewText);
+    }
+
+    private void extractMarkersAndText(@NotNull String rawPreviewText) {
+      myRanges.clear();
+      StringBuilder output = new StringBuilder();
+      int shift = 0;
+      int searchOffset = 0;
+      while (searchOffset < rawPreviewText.length()) {
+        int rawBoldStart = rawPreviewText.indexOf(BOLD_START_MARKER, searchOffset);
+        if (rawBoldStart >= 0) {
+          output.append(rawPreviewText, searchOffset, rawBoldStart);
+          int boldStart = rawBoldStart - shift;
+          myRanges.add(new RangeHighlightingData(searchOffset - shift, boldStart, false));
+          searchOffset = rawBoldStart + BOLD_START_MARKER.length();
+          shift += BOLD_START_MARKER.length();
+
+          int rawBoldEnd = rawPreviewText.indexOf(BOLD_END_MARKER, searchOffset);
+          if (rawBoldEnd < 0) rawBoldEnd = rawPreviewText.length();
+          output.append(rawPreviewText, searchOffset, rawBoldEnd);
+          int boldEnd = rawBoldEnd - shift;
+          searchOffset = rawBoldEnd + BOLD_END_MARKER.length();
+          shift += BOLD_END_MARKER.length();
+          myRanges.add(new RangeHighlightingData(boldStart, boldEnd, true));
+        }
+        else {
+          myRanges.add(new RangeHighlightingData(searchOffset - shift, rawPreviewText.length() - shift, false));
+          output.append(rawPreviewText, searchOffset, rawPreviewText.length());
+          break;
+        }
+      }
+      myText = output.toString();
+    }
+
+    String getText() {
+      return myText;
+    }
+
+    String getRawText() {
+      StringBuilder builder = new StringBuilder();
+      for (RangeHighlightingData data : myRanges) {
+        if (data.isBold) {
+          builder.append(BOLD_START_MARKER);
+        }
+        builder.append(myText, data.textRange.getStartOffset(), data.textRange.getEndOffset());
+        if (data.isBold) {
+          builder.append(BOLD_END_MARKER);
+        }
+      }
+      return builder.toString();
+    }
+
+    int getRangeCount() {
+      return myRanges.size();
+    }
+
+    RangeHighlightingData getRangeDataAt(int index) {
+      return myRanges.isEmpty() ? EMPTY_RANGE :  myRanges.get(index);
+    }
+
+    boolean isDefault() {
+      return getIDEDemoText().equals(getRawText());
+    }
+
+    void resetToDefault() {
+      extractMarkersAndText(getIDEDemoText());
+    }
+
+    @Override
+    public void documentChanged(@NotNull DocumentEvent event) {
+      String docText = event.getDocument().getText();
+      if (myText.equals(docText)) return;
+      if (event.isWholeTextReplaced()) {
+        myRanges.clear();
+        myRanges.add(new RangeHighlightingData(0, docText.length(), false));
+      }
+      else {
+        int offset = event.getOffset();
+        if (event.getNewLength() >= event.getOldLength()) {
+          if (myRanges.isEmpty()) {
+            myRanges.add(EMPTY_RANGE);
+          }
+          int insertedLen = event.getNewLength() - event.getOldLength();
+          for (RangeHighlightingData data : myRanges) {
+            if (data.textRange.contains(offset) || data == myRanges.get(myRanges.size() - 1) && data.textRange.getEndOffset() <= offset) {
+              data.updateRange(data.textRange.grown(insertedLen));
+            }
+            else if (data.textRange.getStartOffset() > offset) {
+              data.updateRange(data.textRange.shiftRight(insertedLen));
+            }
+          }
+        }
+        else {
+          int deletedLen = event.getOldLength() - event.getNewLength();
+          TextRange deletedRange = new TextRange(offset, offset + deletedLen);
+          int delta = 0;
+          Iterator<RangeHighlightingData> rangeIterator = myRanges.iterator();
+          while(rangeIterator.hasNext()) {
+            RangeHighlightingData data =rangeIterator.next();
+            int cutoutStart = Math.max(deletedRange.getStartOffset(), data.textRange.getStartOffset());
+            int cutoutEnd = Math.min(deletedRange.getEndOffset(), data.textRange.getEndOffset());
+            if (cutoutStart < cutoutEnd) {
+              int shrinkSize = cutoutEnd - cutoutStart;
+              if (shrinkSize == data.textRange.getLength()) {
+                rangeIterator.remove();
+              }
+              else {
+                data.updateRange(data.textRange.grown(-shrinkSize));
+              }
+              data.updateRange(data.textRange.shiftLeft(delta));
+              delta += shrinkSize;
+            }
+            else {
+              data.updateRange(data.textRange.shiftLeft(delta));
+            }
+          }
+        }
+      }
+      myText = docText;
+    }
+
+    int getIndexAtOffset(int offset) {
+      for (int i = 0; i < myRanges.size(); i ++) {
+        if (myRanges.get(i).textRange.contains(offset)) return i;
+      }
+      return -1;
+    }
+  }
+
+  private static class RangeHighlightingData {
+    private TextRange textRange;
+    private final boolean isBold;
+
+    private RangeHighlightingData(int startOffset, int endOffset, boolean isBold) {
+      textRange = TextRange.create(startOffset, endOffset);
+      this.isBold = isBold;
+    }
+
+    void updateRange(@NotNull TextRange newRange) {
+      this.textRange = newRange;
+    }
+
+    @Override
+    public String toString() {
+      return "RangeHighlightingData{" +
+             "textRange=" + textRange +
+             ", isBold=" + isBold +
+             '}';
+    }
+  }
+
+  private static class PreviewHighlighter implements EditorHighlighter {
+    private final PreviewTextModel myTextModelModel;
+    private final Document myDocument;
+
+    private PreviewHighlighter(@NotNull PreviewTextModel model, @NotNull Document document) {
+      myTextModelModel = model;
+      myDocument = document;
+    }
+
+    @Override
+    public @NotNull HighlighterIterator createIterator(int startOffset) {
+      return new PreviewHighlighterIterator(myTextModelModel, myDocument, startOffset);
+    }
+
+    @Override
+    public void setEditor(@NotNull HighlighterClient editor) {
+    }
+  }
+
+  private static class PreviewHighlighterIterator implements HighlighterIterator {
+    private final PreviewTextModel myTextModel;
+    private final Document myDocument;
+
+    private final static TextAttributes PLAIN_ATTRIBUTES =
+      new TextAttributes(null, null, null, null, Font.PLAIN);
+    private final static TextAttributes BOLD_ATTRIBUTES =
+      new TextAttributes(null, null, null, null, Font.BOLD);
+
+    private int myCurrIndex;
+
+    private PreviewHighlighterIterator(@NotNull PreviewTextModel model, @NotNull Document document, int startOffset) {
+      myTextModel = model;
+      myDocument = document;
+      myCurrIndex = Math.max(myTextModel.getIndexAtOffset(startOffset), 0);
+    }
+
+    @NotNull
+    private RangeHighlightingData getData() {
+      return myTextModel.getRangeDataAt(myCurrIndex);
+    }
+
+    @Override
+    public TextAttributes getTextAttributes() {
+      return getData().isBold ? BOLD_ATTRIBUTES : PLAIN_ATTRIBUTES;
+    }
+
+    @Override
+    public int getStart() {
+      return getData().textRange.getStartOffset();
+    }
+
+    @Override
+    public int getEnd() {
+      return getData().textRange.getEndOffset();
+    }
+
+    @Override
+    public IElementType getTokenType() {
+      return null;
+    }
+
+    @Override
+    public void advance() {
+      if (myCurrIndex < myTextModel.getRangeCount() - 1) myCurrIndex++;
+    }
+
+    @Override
+    public void retreat() {
+      if (myCurrIndex > 0) myCurrIndex--;
+    }
+
+    @Override
+    public boolean atEnd() {
+      return myCurrIndex >= myTextModel.getRangeCount() - 1;
+    }
+
+    @Override
+    public Document getDocument() {
+      return myDocument;
     }
   }
 }
