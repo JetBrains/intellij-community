081d6aae3e6e6669550ca9366bf73f96062a8d3e
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2021-03-22 23:46:52 +0300

    [space] Extract Space plugin sources from Community
    e872d745c76e5ebcb66cdd9ac0cf228bdead4998
    
    GitOrigin-RevId: 8d2d06ccf5ec7323858722f857284f4d08110dc8

--- plugins/space/src/main/kotlin/com/intellij/space/tools/Utilities.kt
+++
@@ -1,9 +0,0 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.space.tools
-
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.wm.ToolWindowManager
-import com.intellij.openapi.wm.ex.ToolWindowManagerEx
-
-val Project.toolWindowManager: ToolWindowManager get() = ToolWindowManager.getInstance(this)
-val Project.toolWindowManagerEx: ToolWindowManagerEx get() = ToolWindowManagerEx.getInstanceEx(this)
