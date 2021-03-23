877e289cd83af6c4faf0a37263a1e15cdbbd082b
Tianyu Geng <tgeng@google.com>
2021-03-23 14:08:19 -0700

    FIR IDE: register quickfix to add `lateinit` modifier
    Original commit: 6d69959bfdde89f406eb70cacaf35373909a4b32
    
    GitOrigin-RevId: 253fd69be20f44f4b9363ebc97da74aa61c938bd

---
+++ plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/quickfix/fixes/AddLateInitFactory.kt
@@ -0,0 +1,25 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package org.jetbrains.kotlin.idea.quickfix.fixes
+
+import org.jetbrains.kotlin.idea.fir.api.fixes.diagnosticFixFactory
+import org.jetbrains.kotlin.idea.frontend.api.fir.diagnostics.KtFirDiagnostic
+import org.jetbrains.kotlin.idea.frontend.api.types.isPrimitive
+import org.jetbrains.kotlin.idea.quickfix.AddModifierFix
+import org.jetbrains.kotlin.lexer.KtTokens
+import org.jetbrains.kotlin.psi.KtProperty
+
+object AddLateInitFactory {
+
+    @OptIn(ExperimentalStdlibApi::class)
+    val addLateInitFactory = diagnosticFixFactory<KtFirDiagnostic.MustBeInitializedOrBeAbstract> { diagnostic ->
+        val property: KtProperty = diagnostic.psi
+        if (!property.isVar) return@diagnosticFixFactory emptyList()
+
+        val type = property.getReturnKtType()
+
+        if (type.isPrimitive || type.canBeNull) return@diagnosticFixFactory emptyList()
+
+        listOf(AddModifierFix(property, KtTokens.LATEINIT_KEYWORD))
+    }
+}
