33584b681512217de5fb499b8b370a23099581ab
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2021-03-23 18:28:23 +0700

    [java] Improve parentheses fixer in complete statement action
    1. Do not add parentheses left to caret
    2. Use LongRangeSet to add parentheses at valid overload positions
    3. Support vararg methods
    4. Filter by outer method call arguments range
    5. Do not try to fix anything in already valid inner calls
    
    Fixes IDEA-264821 Complete current statement: wrong choice for missing bracket
    
    GitOrigin-RevId: d637703725e9e67ddb83e67a1e05d95d7fc4c1fe

---
+++ java/java-tests/testData/codeInsight/completeStatement/OverloadedMethodNoCaretHint.java
@@ -0,0 +1,11 @@
+public class Foo {
+  public Foo(String a) {
+  }
+
+  public Foo(String a, String b) {
+  }
+
+  public static void main(String[] args) {
+    System.out.println(new Foo(<caret>"a", "b";
+  }
+}
