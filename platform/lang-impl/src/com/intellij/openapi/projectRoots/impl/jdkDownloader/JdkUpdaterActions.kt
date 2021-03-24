c83f86faf7d9decff607f9ebbeb9b5886a2c32e6
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2021-03-24 14:53:59 +0100

    jdk-updates: IDEA-261052
    GitOrigin-RevId: 0e61362a53a83fbcc877b4de251511347f9484f5

---
+++ platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/jdkDownloader/JdkUpdaterActions.kt
@@ -0,0 +1,96 @@
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.projectRoots.impl.jdkDownloader
+
+import com.intellij.ide.actions.SettingsEntryPointAction
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.application.invokeLater
+import com.intellij.openapi.components.*
+import com.intellij.openapi.projectRoots.Sdk
+import com.intellij.util.Alarm
+import com.intellij.util.ui.update.MergingUpdateQueue
+import com.intellij.util.ui.update.Update
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
+
+
+class JdkSettingsActionRegistryState: BaseState() {
+  val knownActions by list<String>()
+}
+
+@Service(Service.Level.APP)
+@State(name = "jdk-update-state", storages = [Storage(StoragePathMacros.CACHE_FILE)], allowLoadInTests = true)
+class JdkUpdaterNotifications : SimplePersistentStateComponent<JdkSettingsActionRegistryState>(JdkSettingsActionRegistryState()), Disposable {
+  private val lock = ReentrantLock()
+  private val pendingNotifications = HashMap<Sdk, JdkUpdateNotification>()
+  private var pendingActionsCopy = listOf<JdkUpdateNotification.JdkUpdateSuggestionAction>()
+
+  private val alarm = MergingUpdateQueue("jdk-update-actions", 500, true, null, this, null, Alarm.ThreadToUse.POOLED_THREAD).usePassThroughInUnitTestMode()
+  override fun dispose() = Unit
+
+  private fun scheduleUpdate() {
+    alarm.queue(object: Update(this) {
+      override fun run() = lock.withLock {
+        //we would not like it to overflow
+        if (state.knownActions.size > 300) {
+          val tail = state.knownActions.toList().takeLast(30).toHashSet()
+          state.knownActions.clear()
+          state.knownActions.addAll(tail)
+          state.intIncrementModificationCount()
+        }
+
+        val ids = pendingNotifications.values.map { it.persistentId }.toSortedSet()
+        val iconState = if (!state.knownActions.containsAll(ids)) {
+          state.knownActions.addAll(ids)
+          state.intIncrementModificationCount()
+
+          SettingsEntryPointAction.IconState.ApplicationUpdate
+        } else {
+          SettingsEntryPointAction.IconState.Current
+        }
+
+        invokeLater {
+          pendingActionsCopy = pendingNotifications.values.sortedBy { it.persistentId }.map { it.updateAction }
+          SettingsEntryPointAction.updateState(iconState)
+        }
+      }
+    })
+  }
+
+  fun hideNotification(jdk: Sdk) {
+    lock.withLock {
+      pendingNotifications[jdk]?.tryReplaceWithNewerNotification()
+    }
+    scheduleUpdate()
+  }
+
+  fun showNotification(jdk: Sdk, actualItem: JdkItem, newItem: JdkItem): JdkUpdateNotification? {
+    val newNotification = lock.withLock {
+      val newNotification = JdkUpdateNotification(
+        jdk = jdk,
+        oldItem = actualItem,
+        newItem = newItem,
+        whenComplete = {
+          lock.withLock {
+            pendingNotifications.remove(jdk, it)
+          }
+          scheduleUpdate()
+        }
+      )
+
+      val currentNotification = pendingNotifications[jdk]
+      if (currentNotification != null && !currentNotification.tryReplaceWithNewerNotification(newNotification)) return null
+      pendingNotifications[jdk] = newNotification
+      newNotification
+    }
+
+    scheduleUpdate()
+    return newNotification
+  }
+
+  fun getActions() : List<JdkUpdateNotification.JdkUpdateSuggestionAction> = pendingActionsCopy
+}
+
+class JdkSettingsActionRegistryActionProvider : SettingsEntryPointAction.ActionProvider {
+  override fun getUpdateActions(context: DataContext) = service<JdkUpdaterNotifications>().getActions()
+}
