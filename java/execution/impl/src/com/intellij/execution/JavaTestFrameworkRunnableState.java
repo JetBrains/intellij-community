6b10799ff400907a6d5aa33ccb302c08935c76f1
Alexander Koshevoy <Alexander.Koshevoy@jetbrains.com>
2021-03-24 13:38:48 +0300

    Run JavaTestFrameworkRunnableState.spansMultipleModules(...) method under ReadAction
    GitOrigin-RevId: f3fbe684421105c8155e4828737a18b87c680465

--- java/execution/impl/src/com/intellij/execution/JavaTestFrameworkRunnableState.java
+++ java/execution/impl/src/com/intellij/execution/JavaTestFrameworkRunnableState.java
@@ -28,6 +28,7 @@ import com.intellij.execution.util.ProgramParametersConfigurator;
 import com.intellij.execution.util.ProgramParametersUtil;
 import com.intellij.execution.wsl.target.WslTargetEnvironmentConfiguration;
 import com.intellij.execution.wsl.target.WslTargetEnvironmentFactory;
+import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.compiler.JavaCompilerBundle;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.extensions.ExtensionPointName;
@@ -641,7 +642,7 @@ public abstract class JavaTestFrameworkRunnableState<T extends
   protected boolean forkPerModule() {
     return getScope() != TestSearchScope.SINGLE_MODULE &&
            toChangeWorkingDirectory(getConfiguration().getWorkingDirectory()) &&
-           spansMultipleModules(getConfiguration().getPackage());
+           ReadAction.compute(() -> spansMultipleModules(getConfiguration().getPackage()));
   }
 
   private static boolean toChangeWorkingDirectory(final String workingDirectory) {
