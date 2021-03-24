f260de94848e8290e8d9a0f4bd2112c5fd9b53d6
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2021-03-24 18:08:13 +0300

    settings: log the fact that all config directories were considered as too old to import from
    GitOrigin-RevId: b3e97bef141e371b2fdccd44c8731a2f5e0dfcc8

--- platform/platform-impl/src/com/intellij/openapi/application/ConfigImportHelper.java
+++ platform/platform-impl/src/com/intellij/openapi/application/ConfigImportHelper.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.application;
 
 import com.intellij.diagnostic.VMOptions;
@@ -162,6 +162,7 @@ public final class ConfigImportHelper {
       else {
         Pair<Path, FileTime> bestConfigGuess = guessedOldConfigDirs.getFirstItem();
         if (isConfigOld(bestConfigGuess.second)) {
+          log.info("The best config guess [" + bestConfigGuess.first + "] is too old, it won't be used for importing.");
           oldConfigDirAndOldIdePath = showDialogAndGetOldConfigPath(guessedOldConfigDirs.getPaths());
           importScenarioStatistics = SHOW_DIALOG_CONFIGS_ARE_TOO_OLD;
         }
