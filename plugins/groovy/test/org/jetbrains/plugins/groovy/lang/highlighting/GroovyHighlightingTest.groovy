60ac5d7b9816def6a33abd23fd9f8e03a2c31ee2
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2021-03-24 19:32:33 +0300

    [groovy] IDEA-260855: Add support for instance parameters to method pointers
    GitOrigin-RevId: 499177601c685355bfae1bc3463dea45e312c1ea

--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GroovyHighlightingTest.groovy
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/highlighting/GroovyHighlightingTest.groovy
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.lang.highlighting
 
 import com.siyeh.ig.junit.AbstractTestClassNamingConvention
@@ -2120,5 +2120,21 @@ A a = [<warning>s</warning> : "asd"]
 
   void testIllegalMethodName() {
     doTest()
+  }
+
+  void 'test method reference with just type argument'() {
+    testHighlighting """
+@groovy.transform.CompileStatic
+class A {
+  String key = ""
+  String foo(int x) {return key}
+  
+  public static void main() {
+    def ref = A.&foo
+    A a = new A()
+    ref(a, 1)
+  }
+}
+"""
   }
 }
