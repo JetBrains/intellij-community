5e6bca7f85ca2004523f9dd10c5be34bf82ea9f2
alexey.afanasiev <alexey.afanasiev@jetbrains.com>
2021-03-25 22:10:19 +0300

    Qodana: add git origin parameter to JBProtocol.
    GitOrigin-RevId: e3690bc13843528ca8dc79ecdfd8eb1befb7fe16

--- platform/platform-impl/src/com/intellij/ide/ReopenProjectAction.java
+++ platform/platform-impl/src/com/intellij/ide/ReopenProjectAction.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide;
 
 import com.intellij.CommonBundle;
@@ -83,6 +83,11 @@ public class ReopenProjectAction extends AnAction implements DumbAware, LightEdi
       return ((RecentProjectsManagerBase)mgr).getProjectName(myProjectPath);
     }
     return myProjectName;
+  }
+
+  @NlsSafe
+  public String getProjectOrigin() {
+    return "";
   }
 
   @NlsActions.ActionText
