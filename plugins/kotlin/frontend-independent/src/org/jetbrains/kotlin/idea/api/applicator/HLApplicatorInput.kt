e25297ce1f68d2aa2fcdb3592d76a9aa8940a01a
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-03-26 18:54:56 +0100

    Move HLApplicator from fir ide module to frontend-independent
    Original commit: 18ab9c7b25975882a67bbd0c9fefa609120f13b2
    
    GitOrigin-RevId: 08c22f55332874493a1842150172267a7563e2f2

--- plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/api/applicator/HLApplicatorInput.kt
+++ plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/api/applicator/HLApplicatorInput.kt
@@ -1,9 +1,6 @@
-/*
- * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
- * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
- */
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
-package org.jetbrains.kotlin.idea.fir.api.applicator
+package org.jetbrains.kotlin.idea.api.applicator
 
 import com.intellij.psi.PsiElement
 
