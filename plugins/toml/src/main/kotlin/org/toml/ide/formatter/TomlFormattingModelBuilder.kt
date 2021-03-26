c53507c552a23fcff017815c36425c5a27a359a1
Arseniy Pendryak <a.pendryak@yandex.ru>
2021-03-26 09:36:39 +0300

    Fix or suppress warning from `UnstableApiUsage` inspection
    GitOrigin-RevId: c2c0e5a94154a45601a72c889d615450a7578934

--- plugins/toml/src/main/kotlin/org/toml/ide/formatter/TomlFormattingModelBuilder.kt
+++ plugins/toml/src/main/kotlin/org/toml/ide/formatter/TomlFormattingModelBuilder.kt
@@ -8,14 +8,14 @@ package org.toml.ide.formatter
 import com.intellij.formatting.*
 import com.intellij.lang.ASTNode
 import com.intellij.openapi.util.TextRange
-import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
-import com.intellij.psi.codeStyle.CodeStyleSettings
 
 class TomlFormattingModelBuilder : FormattingModelBuilder {
     override fun getRangeAffectingIndent(file: PsiFile?, offset: Int, elementAtOffset: ASTNode?): TextRange? = null
 
-    override fun createModel(element: PsiElement, settings: CodeStyleSettings): FormattingModel {
+    override fun createModel(formattingContext: FormattingContext): FormattingModel {
+        val settings = formattingContext.codeStyleSettings
+        val element = formattingContext.psiElement
         val ctx = TomlFmtContext.create(settings)
         val block = createBlock(element.node, null, Indent.getNoneIndent(), null, ctx)
         return FormattingModelProvider.createFormattingModelForPsiFile(element.containingFile, block, settings)
