0ed34a5c1580a8d760e477e3dc552e77f2377a8c
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2021-03-26 22:29:35 +0300

    [vcs-log] warn when index storage gets cleared
    IDEA-265042
    
    GitOrigin-RevId: 766f637707bfc81c26cd10ddc3b3797c7fa2c9e6

--- platform/vcs-log/impl/src/com/intellij/vcs/log/data/index/VcsLogFullDetailsIndex.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/data/index/VcsLogFullDetailsIndex.java
@@ -2,6 +2,7 @@
 package com.intellij.vcs.log.data.index;
 
 import com.intellij.openapi.Disposable;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.util.Disposer;
@@ -29,6 +30,7 @@ import java.util.function.IntConsumer;
 import java.util.function.ObjIntConsumer;
 
 public class VcsLogFullDetailsIndex<T, D> implements Disposable {
+  private static final Logger LOG = Logger.getInstance(VcsLogFullDetailsIndex.class);
   @NonNls protected static final String INDEX = "index";
   @NotNull private final MyMapReduceIndex myMapReduceIndex;
   @NotNull protected final StorageId myStorageId;
@@ -160,14 +162,23 @@ public class VcsLogFullDetailsIndex<T, D> implements Disposable {
   }
 
   private static class MyMapIndexStorage<T> extends MapIndexStorage<Integer, T> {
+    private @NotNull final String myName;
+
     MyMapIndexStorage(@NotNull String name, @NotNull StorageId storageId, @NotNull DataExternalizer<T> externalizer)
       throws IOException {
       super(storageId.getStorageFile(name, true), EnumeratorIntegerDescriptor.INSTANCE, externalizer, 5000, false);
+      myName = name;
     }
 
     @Override
     protected void checkCanceled() {
       ProgressManager.checkCanceled();
+    }
+
+    @Override
+    public void clear() throws StorageException {
+      LOG.warn("Clearing '" + myName + "' map index storage", new RuntimeException());
+      super.clear();
     }
   }
 
