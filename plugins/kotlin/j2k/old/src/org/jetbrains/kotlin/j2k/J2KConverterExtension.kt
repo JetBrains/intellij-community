7a49c90eac1df38792b4f869c688d3d57616c703
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-03-28 17:04:44 +0700

    remove AbstractExtensionPointBean usage from J2kConverterExtension
    GitOrigin-RevId: d48157297533ebf73743bb09fb5134556718a302

--- plugins/kotlin/j2k/old/src/org/jetbrains/kotlin/j2k/J2KConverterExtension.kt
+++ plugins/kotlin/j2k/old/src/org/jetbrains/kotlin/j2k/J2KConverterExtension.kt
@@ -1,14 +1,28 @@
+/*
+ * Copyright 2010-2021 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.jetbrains.kotlin.j2k
 
-import com.intellij.ide.util.PropertiesComponent
-import com.intellij.openapi.extensions.AbstractExtensionPointBean
 import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiJavaFile
 
-abstract class J2kConverterExtension : AbstractExtensionPointBean() {
+abstract class J2kConverterExtension {
     abstract val isNewJ2k: Boolean
 
     abstract fun createJavaToKotlinConverter(
@@ -30,10 +44,8 @@ abstract class J2kConverterExtension : AbstractExtensionPointBean() {
     ): WithProgressProcessor
 
     companion object {
-        val EP_NAME = ExtensionPointName.create<J2kConverterExtension>("org.jetbrains.kotlin.j2kConverterExtension")
+        val EP_NAME = ExtensionPointName<J2kConverterExtension>("org.jetbrains.kotlin.j2kConverterExtension")
 
-        fun extension(useNewJ2k: Boolean): J2kConverterExtension =
-            EP_NAME.extensions.first { it.isNewJ2k == useNewJ2k }
+        fun extension(useNewJ2k: Boolean): J2kConverterExtension = EP_NAME.extensionList.first { it.isNewJ2k == useNewJ2k }
     }
 }
-
