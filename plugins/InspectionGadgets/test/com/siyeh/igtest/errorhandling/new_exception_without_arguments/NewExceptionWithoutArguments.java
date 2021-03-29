a543a119815a520828aee90d7c473646ca4a9d4d
Bas Leijdekkers <basleijdekkers@gmail.com>
2021-03-29 17:03:07 +0200

    IG: don't ignore method references in "Exception constructor called without arguments" inspection (IDEA-262199)
    GitOrigin-RevId: b0ff50e2ca05a62cc41a713272417f0b6c7acac5

--- plugins/InspectionGadgets/test/com/siyeh/igtest/errorhandling/new_exception_without_arguments/NewExceptionWithoutArguments.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/errorhandling/new_exception_without_arguments/NewExceptionWithoutArguments.java
@@ -1,5 +1,8 @@
 package com.siyeh.igtest.errorhandling.new_exception_without_arguments;
 
+import java.util.Optional;
+import java.util.function.Function;
+
 class NewExceptionWithoutArguments {
 
   void foo() {
@@ -8,6 +11,12 @@ class NewExceptionWithoutArguments {
 
   void bar() {
     throw new MyException();
+  }
+
+  void x() {
+    Optional.ofNullable(null).orElseThrow(<warning descr="'new RuntimeException()' without arguments">RuntimeException</warning>::new);
+    Optional.ofNullable(null).orElseThrow(() -> new <warning descr="'new RuntimeException()' without arguments">RuntimeException</warning>());
+    Function<String, Exception> f = Exception::new;
   }
 
 }
