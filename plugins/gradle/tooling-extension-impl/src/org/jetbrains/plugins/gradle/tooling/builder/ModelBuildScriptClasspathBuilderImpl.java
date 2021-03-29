9f9e9cc369c6ebaac1c1429d6d76edbbe962c973
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2021-03-29 14:01:15 +0300

    [Gradle] remove needless collections creation IDEA-263670
    (cherry picked from commit 72bc0c9e9a1d47d855e3698fe5ac69d2bb2df176)
    
    IJ-CR-7735
    
    GitOrigin-RevId: 6c67551c9a443d15ef77290929b0eac32aa700d2

--- plugins/gradle/tooling-extension-impl/src/org/jetbrains/plugins/gradle/tooling/builder/ModelBuildScriptClasspathBuilderImpl.java
+++ plugins/gradle/tooling-extension-impl/src/org/jetbrains/plugins/gradle/tooling/builder/ModelBuildScriptClasspathBuilderImpl.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.gradle.tooling.builder;
 
 import org.gradle.api.Project;
@@ -93,33 +79,33 @@ public class ModelBuildScriptClasspathBuilderImpl extends AbstractModelBuilderSe
         Collection<File> projectDependencyArtifacts = projectDependency.getProjectDependencyArtifacts();
         Collection<File> projectDependencyArtifactsSources = projectDependency.getProjectDependencyArtifactsSources();
         buildScriptClasspath.add(new ClasspathEntryModelImpl(
-          pathSet(projectDependencyArtifacts),
-          pathSet(projectDependencyArtifactsSources),
-          Collections.<String>emptySet()
+          projectDependencyArtifacts,
+          projectDependencyArtifactsSources,
+          Collections.<File>emptySet()
         ));
       }
       else if (dependency instanceof ExternalLibraryDependency) {
         final ExternalLibraryDependency libraryDep = (ExternalLibraryDependency)dependency;
         buildScriptClasspath.add(new ClasspathEntryModelImpl(
-          pathSet(libraryDep.getFile()),
-          pathSet(libraryDep.getSource()),
-          pathSet(libraryDep.getJavadoc())
+          Collections.singletonList(libraryDep.getFile()),
+          Collections.singletonList(libraryDep.getSource()),
+          Collections.singletonList(libraryDep.getJavadoc())
         ));
       }
       else if (dependency instanceof ExternalMultiLibraryDependency) {
         ExternalMultiLibraryDependency multiLibraryDependency = (ExternalMultiLibraryDependency)dependency;
         buildScriptClasspath.add(new ClasspathEntryModelImpl(
-          pathSet(multiLibraryDependency.getFiles()),
-          pathSet(multiLibraryDependency.getSources()),
-          pathSet(multiLibraryDependency.getJavadoc())
+          multiLibraryDependency.getFiles(),
+          multiLibraryDependency.getSources(),
+          multiLibraryDependency.getJavadoc()
         ));
       }
       else if (dependency instanceof FileCollectionDependency) {
         FileCollectionDependency fileCollectionDependency = (FileCollectionDependency)dependency;
         buildScriptClasspath.add(new ClasspathEntryModelImpl(
-          pathSet(fileCollectionDependency.getFiles()),
-          Collections.<String>emptySet(),
-          Collections.<String>emptySet()
+          fileCollectionDependency.getFiles(),
+          Collections.<File>emptySet(),
+          Collections.<File>emptySet()
         ));
       }
     }
