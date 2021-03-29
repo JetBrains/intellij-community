7fec652a92292c8c9b0e84b60f18b22957fa620f
Anton Yalyshev <anton.yalyshev@jetbrains.com>
2021-03-29 18:32:52 +0300

    Make possible FUS logging in Android Studio
    GitOrigin-RevId: 63e5fb7e6bf8a233378620d0545c40578bf441aa

--- plugins/kotlin/core/src/org/jetbrains/kotlin/idea/statistics/KotlinFUSLogger.kt
+++ plugins/kotlin/core/src/org/jetbrains/kotlin/idea/statistics/KotlinFUSLogger.kt
@@ -18,16 +18,10 @@ open class KotlinFUSLogger {
         private val logger = FUCounterUsageLogger.getInstance()
 
         fun log(group: FUSEventGroups, event: String) {
-            if (PlatformVersion.isAndroidStudio()) {
-                return
-            }
             logger.logEvent(group.GROUP_ID, event, context)
         }
 
         fun log(group: FUSEventGroups, event: String, eventData: Map<String, String>) {
-            if (PlatformVersion.isAndroidStudio()) {
-                return
-            }
             val localContext = context.copy()
             for (entry in eventData) {
                 localContext.addData(entry.key, entry.value)
--- plugins/kotlin/core/src/org/jetbrains/kotlin/idea/statistics/ProjectConfigurationCollector.kt
+++ plugins/kotlin/core/src/org/jetbrains/kotlin/idea/statistics/ProjectConfigurationCollector.kt
@@ -32,9 +32,6 @@ class ProjectConfigurationCollector : ProjectUsagesCollector() {
     override fun getGroup() = GROUP
 
     override fun getMetrics(project: Project): Set<MetricEvent> {
-        if (PlatformVersion.isAndroidStudio()) {
-            return emptySet()
-        }
         val metrics = mutableSetOf<MetricEvent>()
         val modulesWithFacet = ProjectFacetManager.getInstance(project).getModulesWithFacet(KotlinFacetType.TYPE_ID)
         val pluginInfo = getPluginInfoById(KotlinPluginUtil.KOTLIN_PLUGIN_ID)
