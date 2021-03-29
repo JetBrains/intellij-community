371a249ab92b5dd4f34f74cb6f8eebe572836e39
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2021-03-29 19:28:01 +0300

    fixup! Drop Bintray from wizards (KTI-528)
    Original commit: c868116535628b7807b5fbe546ed03d84407f8e1
    
    (cherry picked from commit 6966b8f16c2b3882b0bc4f5be9374879ea474da4)
    (cherry picked from commit 790541b820228babd1745fc927f5a0aed3d4d097)
    
    GitOrigin-RevId: 7cee841b78ad9cac45f0e0af75251b5b9ab0a10c

--- plugins/kotlin/project-wizard/core/src/org/jetbrains/kotlin/tools/projectWizard/templates/SimpleJsClientTemplate.kt
+++ plugins/kotlin/project-wizard/core/src/org/jetbrains/kotlin/tools/projectWizard/templates/SimpleJsClientTemplate.kt
@@ -41,9 +41,9 @@ class SimpleJsClientTemplate : JsClientTemplate() {
         buildList {
             if (useKotlinxHtml.reference.settingValue()) {
                 +ArtifactBasedLibraryDependencyIR(
-                  MavenArtifact(Repositories.KOTLINX_HTML, "org.jetbrains.kotlinx", "kotlinx-html"),
-                  Versions.KOTLINX.KOTLINX_HTML,
-                  DependencyType.MAIN
+                    MavenArtifact(Repositories.KOTLINX_HTML, "org.jetbrains.kotlinx", "kotlinx-html"),
+                    Versions.KOTLINX.KOTLINX_HTML,
+                    DependencyType.MAIN
                 )
             }
         }
