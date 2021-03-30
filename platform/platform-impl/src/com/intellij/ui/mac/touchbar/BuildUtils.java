5373bdf149052712371fcfc81f6bc3a11ac27cc8
Gregory.Shrago <gregory.shrago@jetbrains.com>
2021-03-30 23:47:14 +0300

    EA-263901 - T: BuildUtils.addActionGroupButtons
    GitOrigin-RevId: 38be6f911294116df5b719d25d627d7c05ea5756

--- platform/platform-impl/src/com/intellij/ui/mac/touchbar/BuildUtils.java
+++ platform/platform-impl/src/com/intellij/ui/mac/touchbar/BuildUtils.java
@@ -117,7 +117,7 @@ final class BuildUtils {
                                     @Nullable Customizer customizer) {
     GroupVisitor visitor = new GroupVisitor(out, filterGroupPrefix, customizer, out.getStats(), false);
 
-    final DataContext dctx = DataManager.getInstance().getDataContext(getCurrentFocusComponent());
+    DataContext dctx = Utils.wrapDataContext(DataManager.getInstance().getDataContext(getCurrentFocusComponent()));
     Utils.expandActionGroup(false, actionGroup, out.getFactory(), dctx, ActionPlaces.TOUCHBAR_GENERAL, false, visitor);
     if (customizer != null) {
       customizer.finish();
