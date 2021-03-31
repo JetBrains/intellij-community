1ef2b6380bbc738db5d24206e2250e121e37e37f
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2021-03-31 18:33:05 +0300

    Enable the tests for the new YT connector in "Integration Tests" configuration
    The package determines whether the test gets included in TASKS_INTEGRATION_TESTS
    group in testGroups.properties.
    
    GitOrigin-RevId: 5dc3dee403a34776f07a6f6e90658fa63678f15e

--- plugins/tasks/tasks-tests/test/com/intellij/tasks/integration/YouTrackIntegrationTest.java
+++ plugins/tasks/tasks-tests/test/com/intellij/tasks/integration/YouTrackIntegrationTest.java
@@ -1,5 +1,5 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.tasks.live;
+package com.intellij.tasks.integration;
 
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
