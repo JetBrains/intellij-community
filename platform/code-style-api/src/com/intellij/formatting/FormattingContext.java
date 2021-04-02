aa1c04e29db58b44d791d10db9dee793921bd9bb
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2021-04-02 11:05:12 +0300

    Add getVirtualFile() method
    GitOrigin-RevId: ca8849db8598a7cbe72f0170bdca3a21ffc6a60e

--- platform/code-style-api/src/com/intellij/formatting/FormattingContext.java
+++ platform/code-style-api/src/com/intellij/formatting/FormattingContext.java
@@ -4,10 +4,12 @@ package com.intellij.formatting;
 import com.intellij.lang.ASTNode;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Objects;
 
@@ -73,6 +75,10 @@ public class FormattingContext {
    */
   public @NotNull FormattingMode getFormattingMode() {
     return myFormattingMode;
+  }
+
+  public @Nullable VirtualFile getVirtualFile() {
+    return getContainingFile().getVirtualFile();
   }
 
   @Override
