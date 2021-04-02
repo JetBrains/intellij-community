8ea40e53c2a6d5142c360b62b5b6133b06d6bf8f
Ilyas Selimov <ilyas.selimov@jetbrains.com>
2021-04-02 12:05:33 +0700

    IDEA-161641 - reverted previous fix
    GitOrigin-RevId: 66d7b417b56a09b7ad0af70293f137f2559346a8

--- java/java-impl/src/com/intellij/packaging/jlink/JLinkArtifactPropertiesEditor.java
+++ java/java-impl/src/com/intellij/packaging/jlink/JLinkArtifactPropertiesEditor.java
@@ -4,19 +4,19 @@ package com.intellij.packaging.jlink;
 import com.intellij.java.JavaBundle;
 import com.intellij.openapi.ui.ComboBox;
 import com.intellij.packaging.ui.ArtifactPropertiesEditor;
+import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.ui.FormBuilder;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
-import java.util.*;
+import java.util.Optional;
 
 import static com.intellij.packaging.jlink.JLinkArtifactProperties.CompressionLevel;
 
 final class JLinkArtifactPropertiesEditor extends ArtifactPropertiesEditor {
   private final JLinkArtifactProperties myProperties;
-  private final CompressionLevelRenderer myCompressionLevelRenderer = new CompressionLevelRenderer();
 
   private ComboBox<String> myCompressionLevel;
   private JCheckBox myVerbose;
@@ -35,8 +35,12 @@ final class JLinkArtifactPropertiesEditor extends ArtifactPropertiesEditor {
   public @Nullable JComponent createComponent() {
     final FormBuilder builder = new FormBuilder();
 
-    myCompressionLevel = new ComboBox<>(myCompressionLevelRenderer.getLevels().toArray(String[]::new));
-    myCompressionLevel.setItem(myCompressionLevelRenderer.getTextByLevel(myProperties.compressionLevel));
+    myCompressionLevel = new ComboBox<>(ContainerUtil.map(CompressionLevel.values(), t -> t.myText).toArray(String[]::new));
+    CompressionLevel compressionLevel = myProperties.compressionLevel;
+    if (compressionLevel == null) {
+      compressionLevel = CompressionLevel.ZERO;
+    }
+    myCompressionLevel.setItem(compressionLevel.myText);
     builder.addLabeledComponent(JavaBundle.message("packaging.jlink.compression.level"), myCompressionLevel);
 
     myVerbose = new JCheckBox(JavaBundle.message("packaging.jlink.verbose.tracing"), myProperties.verbose);
@@ -47,7 +51,7 @@ final class JLinkArtifactPropertiesEditor extends ArtifactPropertiesEditor {
 
   @Override
   public boolean isModified() {
-    if (myProperties.compressionLevel != myCompressionLevelRenderer.getLevelByText(myCompressionLevel.getItem())) return true;
+    if (myProperties.compressionLevel != CompressionLevel.getLevelByText(myCompressionLevel.getItem())) return true;
     if (myProperties.verbose != myVerbose.isSelected()) return true;
     return false;
   }
@@ -55,44 +59,8 @@ final class JLinkArtifactPropertiesEditor extends ArtifactPropertiesEditor {
   @Override
   public void apply() {
     myProperties.compressionLevel = Optional.ofNullable(myCompressionLevel.getItem())
-      .map(i -> myCompressionLevelRenderer.getLevelByText(i))
+      .map(i -> CompressionLevel.getLevelByText(i))
       .orElse(CompressionLevel.ZERO);
     myProperties.verbose = myVerbose.isSelected();
-  }
-
-  private static class CompressionLevelRenderer {
-    private final EnumMap<CompressionLevel, @Nls String> myTextByLevel = new EnumMap<>(CompressionLevel.class);
-    private final Map<String, CompressionLevel> myLevelByText = new HashMap<>();
-
-    private CompressionLevelRenderer() {
-      String zeroLevel = JavaBundle.message("packaging.jlink.compression.zero.level");
-      myTextByLevel.put(CompressionLevel.ZERO, zeroLevel);
-      myLevelByText.put(zeroLevel, CompressionLevel.ZERO);
-
-      String firstLevel = JavaBundle.message("packaging.jlink.compression.first.level");
-      myTextByLevel.put(CompressionLevel.FIRST, firstLevel);
-      myLevelByText.put(firstLevel, CompressionLevel.FIRST);
-
-      String secondLevel = JavaBundle.message("packaging.jlink.compression.second.level");
-      myTextByLevel.put(CompressionLevel.SECOND, secondLevel);
-      myLevelByText.put(secondLevel, CompressionLevel.SECOND);
-    }
-
-    @NotNull
-    List<String> getLevels() {
-      return List.copyOf(myTextByLevel.values());
-    }
-
-    @NotNull
-    @Nls
-    String getTextByLevel(@Nullable CompressionLevel level) {
-      if (level == null) return myTextByLevel.get(CompressionLevel.ZERO);
-      return myTextByLevel.getOrDefault(level, myTextByLevel.get(CompressionLevel.ZERO));
-    }
-
-    @Nullable
-    CompressionLevel getLevelByText(@NotNull String text) {
-      return myLevelByText.get(text);
-    }
   }
 }
