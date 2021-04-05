50b4b94aa30970dd4f5a489e62e873d5aab17697
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2021-04-06 04:24:52 +0900

    Fix test generation DSL
    GitOrigin-RevId: cf65033ba34309be0b29381862085b40483a20b0

--- plugins/kotlin/generators/test/org/jetbrains/kotlin/testGenerator/GenerateTests.kt
+++ plugins/kotlin/generators/test/org/jetbrains/kotlin/testGenerator/GenerateTests.kt
@@ -40,9 +40,13 @@ import org.jetbrains.kotlin.idea.completion.test.handlers.AbstractCompletionChar
 import org.jetbrains.kotlin.idea.completion.test.handlers.AbstractKeywordCompletionHandlerTest
 import org.jetbrains.kotlin.idea.completion.test.handlers.AbstractSmartCompletionHandlerTest
 import org.jetbrains.kotlin.idea.completion.test.weighers.AbstractBasicCompletionWeigherTest
+import org.jetbrains.kotlin.idea.completion.test.weighers.AbstractSmartCompletionWeigherTest
+import org.jetbrains.kotlin.idea.completion.wheigher.AbstractHighLevelWeigherTest
 import org.jetbrains.kotlin.idea.configuration.AbstractGradleConfigureProjectByChangingFileTest
+import org.jetbrains.kotlin.idea.conversion.copy.AbstractJavaToKotlinCopyPasteConversionTest
 import org.jetbrains.kotlin.idea.conversion.copy.AbstractLiteralKotlinToKotlinCopyPasteTest
 import org.jetbrains.kotlin.idea.conversion.copy.AbstractLiteralTextToKotlinCopyPasteTest
+import org.jetbrains.kotlin.idea.conversion.copy.AbstractTextJavaToKotlinCopyPasteConversionTest
 import org.jetbrains.kotlin.idea.coverage.AbstractKotlinCoverageOutputFilesTest
 import org.jetbrains.kotlin.idea.debugger.evaluate.AbstractCodeFragmentAutoImportTest
 import org.jetbrains.kotlin.idea.debugger.evaluate.AbstractCodeFragmentCompletionHandlerTest
@@ -136,7 +140,6 @@ import org.jetbrains.kotlin.idea.stubs.AbstractResolveByStubTest
 import org.jetbrains.kotlin.idea.stubs.AbstractStubBuilderTest
 import org.jetbrains.kotlin.j2k.AbstractJavaToKotlinConverterMultiFileTest
 import org.jetbrains.kotlin.j2k.AbstractJavaToKotlinConverterSingleFileTest
-import org.jetbrains.kotlin.incremental.*
 import org.jetbrains.kotlin.jps.build.*
 import org.jetbrains.kotlin.jps.incremental.AbstractJsProtoComparisonTest
 import org.jetbrains.kotlin.jps.incremental.AbstractJvmProtoComparisonTest
@@ -162,6 +165,7 @@ import org.jetbrains.kotlin.testGenerator.model.Patterns.KT_OR_KTS
 import org.jetbrains.kotlin.testGenerator.model.Patterns.KT_OR_KTS_WITHOUT_DOTS
 import org.jetbrains.kotlin.testGenerator.model.Patterns.KT_WITHOUT_DOTS
 import org.jetbrains.kotlin.testGenerator.model.Patterns.TEST
+import org.jetbrains.kotlin.testGenerator.model.Patterns.TXT
 import org.jetbrains.kotlin.testGenerator.model.Patterns.WS_KTS
 import org.jetbrains.kotlin.tools.projectWizard.cli.AbstractProjectTemplateBuildFileGenerationTest
 import org.jetbrains.kotlin.tools.projectWizard.cli.AbstractYamlBuildFileGenerationTest
@@ -709,7 +713,6 @@ private fun assembleWorkspace(): TWorkspace = workspace {
             model("dataFlowValueRendering")
         }
 
-
         testClass<AbstractLiteralTextToKotlinCopyPasteTest> {
             model("copyPaste/plainTextLiteral", pattern = Patterns.forRegex("""^([^.]+)\.txt$"""))
         }
@@ -989,7 +992,7 @@ private fun assembleWorkspace(): TWorkspace = workspace {
         }
 
         testClass<AbstractFileScopeTest> {
-            model("fileScopeTest", pattern = KT)
+            model("fileScopeTest")
         }
 
         testClass<AbstractSymbolByPsiTest> {
@@ -1140,23 +1143,23 @@ private fun assembleWorkspace(): TWorkspace = workspace {
     }
 
     testGroup("fir", testDataPath = "../completion/testData") {
-            testClass<AbstractHighLevelJvmBasicCompletionTest> {
-                model("basic/common")
-                model("basic/java")
-            }
-
-            testClass<AbstractHighLevelBasicCompletionHandlerTest> {
-                model("handlers/basic", pattern = KT_WITHOUT_DOTS)
-            }
-
-            testClass<AbstractHighLevelWeigherTest> {
-                model("weighers/basic", pattern = KT_OR_KTS_WITHOUT_DOTS_IN_NAME)
-            }
-
-            testClass<AbstractHighLevelMultiFileJvmBasicCompletionTest> {
-                model("basic/multifile", extension = null, recursive = false)
-            }
+        testClass<AbstractHighLevelJvmBasicCompletionTest> {
+            model("basic/common")
+            model("basic/java")
         }
+
+        testClass<AbstractHighLevelBasicCompletionHandlerTest> {
+            model("handlers/basic", pattern = KT_WITHOUT_DOTS)
+        }
+
+        testClass<AbstractHighLevelWeigherTest> {
+            model("weighers/basic", pattern = KT_OR_KTS_WITHOUT_DOTS_IN_NAME)
+        }
+
+        testClass<AbstractHighLevelMultiFileJvmBasicCompletionTest> {
+            model("basic/multifile", pattern = DIRECTORY, extension = null, recursive = false)
+        }
+    }
 
     testGroup("fir", testDataPath = "../idea/testData/findUsages") {
         testClass<AbstractFindUsagesFirTest> {
@@ -1328,11 +1331,11 @@ private fun assembleWorkspace(): TWorkspace = workspace {
         }
 
         testClass<AbstractMultiFileJvmBasicCompletionTest>("MultiFilePrimitiveJvmBasicCompletionTestGenerated") {
-            model("basic/multifilePrimitive", extension = null, recursive = false)
+            model("basic/multifilePrimitive", pattern = DIRECTORY, extension = null, recursive = false)
         }
 
         testClass<AbstractMultiFileSmartCompletionTest> {
-            model("smartMultiFile", extension = null, recursive = false)
+            model("smartMultiFile", pattern = DIRECTORY, extension = null, recursive = false)
         }
 
         testClass<AbstractJvmBasicCompletionTest>("org.jetbrains.kotlin.idea.completion.test.KDocCompletionTestGenerated") {
@@ -1394,7 +1397,6 @@ private fun assembleWorkspace(): TWorkspace = workspace {
             model("basic/codeFragments", pattern = KT)
         }
     }
-
 
     testGroup("j2k/new/tests") {
         testClass<AbstractNewJavaToKotlinConverterSingleFileTest> {
@@ -1507,16 +1509,6 @@ private fun assembleWorkspace(): TWorkspace = workspace {
         }
     }
 
-    testGroup("plugins/parcelize/parcelize-ide/tests", "plugins/parcelize/parcelize-ide/testData") {
-        testClass<AbstractParcelizeQuickFixTest> {
-            model("quickfix", pattern = Patterns.forRegex("^([\\w\\-_]+)\\.kt$"), filenameStartsLowerCase = true)
-        }
-
-        testClass<AbstractParcelizeCheckerTest> {
-            model("checker", extension = "kt")
-        }
-    }
-
         testGroup(
             "plugins/kotlin-serialization/kotlin-serialization-ide/test",
             "plugins/kotlin-serialization/kotlin-serialization-ide/testData"
@@ -1529,7 +1521,7 @@ private fun assembleWorkspace(): TWorkspace = workspace {
             }
         }
 
-        testGroup("idea/performanceTests/test", "idea/tests/testData") {
+        testGroup("idea/performanceTests/test", "../idea/tests/testData") {
             testClass<AbstractPerformanceJavaToKotlinCopyPasteConversionTest> {
                 model("copyPaste/conversion", testMethod = "doPerfTest", pattern = Patterns.forRegex("""^([^.]+)\.java$"""))
             }
