31713e3c58b0f0cc016ded391e3fe65f1bfd36b2
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-04-05 14:52:32 +0700

    update testData
    ^KTIJ-5639
    
    GitOrigin-RevId: 25b7f019ff4012ed67b37df58f76163faa69058a

--- plugins/kotlin/idea/tests/testData/quickfix/addExclExclCall/nullExpression.kt
+++ plugins/kotlin/idea/tests/testData/quickfix/addExclExclCall/nullExpression.kt
@@ -1,7 +1,6 @@
 // "Add non-null asserted (!!) call" "false"
 // ACTION: Add 'toString()' call
 // ACTION: Change type of 'x' to 'String?'
-// ACTION: Remove braces from 'if' statement
 // ERROR: Type mismatch: inferred type is String? but String was expected
 
 fun foo(arg: String?) {
