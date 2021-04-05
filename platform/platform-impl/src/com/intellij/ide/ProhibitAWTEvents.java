d27f0e136022de40e0e92222a8cd80e75bf47dde
Gregory.Shrago <gregory.shrago@jetbrains.com>
2021-04-05 20:13:30 +0300

    introduce the ability to choose events to prohibit
    GitOrigin-RevId: 97e2bcff0b9c2fe50217c7a9ee61789a5b8e23ff

--- platform/platform-impl/src/com/intellij/ide/ProhibitAWTEvents.java
+++ platform/platform-impl/src/com/intellij/ide/ProhibitAWTEvents.java
@@ -5,10 +5,11 @@ import com.intellij.openapi.application.AccessToken;
 import com.intellij.openapi.diagnostic.Logger;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.function.Supplier;
+import java.util.function.Function;
 
 /**
  * Use to assert that no AWT events are pumped during some activity (e.g. action update, write operations, etc)
@@ -21,27 +22,42 @@ public final class ProhibitAWTEvents implements IdeEventQueue.EventDispatcher {
   private static long ourUseCount;
 
   private final String myActivityName;
+  private final Function<? super AWTEvent, String> myErrorFunction;
   private boolean myReported;
 
-  private ProhibitAWTEvents(@NotNull String activityName) {
+  private ProhibitAWTEvents(@NotNull String activityName, @Nullable Function<? super AWTEvent, String> errorFunction) {
     myActivityName = activityName;
+    myErrorFunction = errorFunction;
   }
 
   @Override
   public boolean dispatch(@NotNull AWTEvent e) {
-    if (!myReported) {
+    String message = myReported ? null :
+                     myErrorFunction == null ? "AWT events are prohibited inside " + myActivityName + "; got " + e :
+                     myErrorFunction.apply(e);
+    if (message != null) {
       myReported = true;
-      LOG.error("AWT events are prohibited inside " + myActivityName + "; got " + e);
+      LOG.error(message);
     }
     return true;
   }
 
   @NotNull
   public static AccessToken start(@NotNull @NonNls String activityName) {
+    return doStart(activityName, null);
+  }
+
+  @NotNull
+  public static AccessToken startFiltered(@NotNull @NonNls String activityName, @NotNull Function<? super AWTEvent, String> errorFunction) {
+    return doStart(activityName, errorFunction);
+  }
+
+  @NotNull
+  private static AccessToken doStart(@NotNull @NonNls String activityName, @Nullable Function<? super AWTEvent, String> errorFunction) {
     if (!SwingUtilities.isEventDispatchThread()) {
       return AccessToken.EMPTY_ACCESS_TOKEN;
     }
-    ProhibitAWTEvents dispatcher = new ProhibitAWTEvents(activityName);
+    ProhibitAWTEvents dispatcher = new ProhibitAWTEvents(activityName, errorFunction);
     IdeEventQueue.getInstance().addPostprocessor(dispatcher, null);
     ourUseCount ++;
     return new AccessToken() {
@@ -52,22 +68,6 @@ public final class ProhibitAWTEvents implements IdeEventQueue.EventDispatcher {
         IdeEventQueue.getInstance().removePostprocessor(dispatcher);
       }
     };
-  }
-
-  public static <T> T prohibitEventsInside(@NonNls @NotNull String activityName, @NotNull Supplier<? extends T> supplier) {
-    if (!SwingUtilities.isEventDispatchThread()) {
-      return supplier.get();
-    }
-    ProhibitAWTEvents dispatcher = new ProhibitAWTEvents(activityName);
-    IdeEventQueue.getInstance().addPostprocessor(dispatcher, null);
-    try {
-      ourUseCount++;
-      return supplier.get();
-    }
-    finally {
-      ourUseCount--;
-      IdeEventQueue.getInstance().removePostprocessor(dispatcher);
-    }
   }
 
   public static boolean areEventsProhibited() {
