80b2fec08682982eee59b431cacaad953a659f4f
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2021-04-05 16:21:22 +0300

    JavaDoc fix
    GitOrigin-RevId: 3b8941143486720ca14979e8e3618e6bbc6918a8

--- platform/lang-impl/src/com/intellij/util/indexing/FileBasedIndexProjectHandler.java
+++ platform/lang-impl/src/com/intellij/util/indexing/FileBasedIndexProjectHandler.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.indexing;
 
 import com.intellij.diagnostic.PerformanceWatcher;
@@ -36,7 +36,7 @@ public final class FileBasedIndexProjectHandler {
   private static final int ourMinFilesSizeToStartDumbMode = Registry.intValue("ide.dumb.mode.minFilesSizeToStart", 1048576);
 
   /**
-   * @deprecated Use {@see scheduleReindexingInDumbMode()} instead.
+   * @deprecated Use {@link #scheduleReindexingInDumbMode(Project)} instead.
    */
   @SuppressWarnings("DeprecatedIsStillUsed")
   @Deprecated
