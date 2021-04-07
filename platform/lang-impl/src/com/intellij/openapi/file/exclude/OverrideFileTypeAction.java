0ffc38f35581e7fd48a226250d011a93bb797394
Alexey Kudravtsev <cdr@intellij.com>
2021-04-07 14:30:40 +0200

    augment file types with identical getDisplayName() with the corresponding plugin name, to make it more distinguishable (part of IDEA-265112 "Override file type" action shows duplicated and irrelevant file types)
    GitOrigin-RevId: 372a773bb9be7d484129b17aca0fb10b0bdcb76e

--- platform/lang-impl/src/com/intellij/openapi/file/exclude/OverrideFileTypeAction.java
+++ platform/lang-impl/src/com/intellij/openapi/file/exclude/OverrideFileTypeAction.java
@@ -6,14 +6,21 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.openapi.extensions.PluginDescriptor;
 import com.intellij.openapi.fileTypes.*;
 import com.intellij.openapi.fileTypes.ex.FakeFileType;
+import com.intellij.openapi.fileTypes.impl.FileTypeManagerImpl;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 class OverrideFileTypeAction extends AnAction {
   @Override
@@ -27,17 +34,31 @@ class OverrideFileTypeAction extends AnAction {
     VirtualFile file = e.getData(CommonDataKeys.VIRTUAL_FILE);
     if (file == null) return;
     DefaultActionGroup group = new DefaultActionGroup();
+    // although well-behaved types have unique names, file types coming from plugins can be wild
+    Map<String, List<String>> duplicates = Arrays.stream(FileTypeManager.getInstance().getRegisteredFileTypes())
+      .map(t -> t.getDisplayName())
+      .collect(Collectors.groupingBy(Function.identity()));
+
     for (FileType type : ContainerUtil.sorted(Arrays.asList(FileTypeManager.getInstance().getRegisteredFileTypes()),
                                               (f1,f2)->f1.getDisplayName().compareToIgnoreCase(f2.getDisplayName()))) {
       if (type instanceof InternalFileType) continue;
       if (type instanceof DirectoryFileType) continue;
       if (type instanceof UnknownFileType) continue;
       if (type instanceof FakeFileType) continue;
-      group.add(new ChangeToThisFileTypeAction(file, type));
+      boolean hasDuplicate = duplicates.get(type.getDisplayName()).size() > 1;
+      String dupHint = null;
+      if (hasDuplicate) {
+        PluginDescriptor descriptor = ((FileTypeManagerImpl)FileTypeManager.getInstance()).findPluginDescriptor(type);
+        dupHint = descriptor == null ? null :
+                  " (" + (descriptor.isBundled() ? ActionsBundle.message("group.OverrideFileTypeAction.bundledPlugin") :
+                          ActionsBundle.message("group.OverrideFileTypeAction.fromNamedPlugin", descriptor.getName()))
+                  + ")";
+      }
+      group.add(new ChangeToThisFileTypeAction(file, type, dupHint));
     }
     JBPopupFactory.getInstance()
-      .createActionGroupPopup(ActionsBundle.message("group.OverrideFileTypeAction.title")
-        , group, e.getDataContext(), false, null, -1)
+      .createActionGroupPopup(ActionsBundle.message("group.OverrideFileTypeAction.title"),
+                              group, e.getDataContext(), false, null, -1)
       .showInBestPositionFor(e.getDataContext());
   }
 
@@ -45,8 +66,9 @@ class OverrideFileTypeAction extends AnAction {
     private final VirtualFile myFile;
     private final FileType myType;
 
-    ChangeToThisFileTypeAction(@NotNull VirtualFile file, @NotNull FileType type) {
-      super(type.getDisplayName(), ActionsBundle.message("action.ChangeToThisFileTypeAction.description", file.getName(), type.getDisplayName()), type.getIcon());
+    ChangeToThisFileTypeAction(@NotNull VirtualFile file, @NotNull FileType type, String dupHint) {
+      super(type.getDisplayName() + StringUtil.notNullize(dupHint),
+            ActionsBundle.message("action.ChangeToThisFileTypeAction.description", file.getName(), type.getDescription()), type.getIcon());
       myFile = file;
       myType = type;
     }
