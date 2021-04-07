ffac1b35168936bfa1cc7c6b80e298793c5114bc
Dmitry Zhuravlev <dmitry.zhuravlev@jetbrains.com>
2021-04-07 13:04:15 +0300

    vcs: move diff preview actions should check the actual place where particular file was opened
    GitOrigin-RevId: 8767d350f88c66f01a8af45207f4aee6c5d6dfd1

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/diff/MoveDiffPreviewAction.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/diff/MoveDiffPreviewAction.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vcs.changes.actions.diff
 
 import com.intellij.diff.editor.DiffContentVirtualFile
@@ -7,12 +7,14 @@ import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.VcsDataKeys.VIRTUAL_FILES
 import com.intellij.openapi.vcs.changes.VcsEditorTabFilesManager
+import com.intellij.openapi.vcs.changes.VcsEditorTabFilesManager.Companion.OPENED_IN_NEW_WINDOW
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.containers.JBIterable
 import com.intellij.vcsUtil.VcsUtil
 
 internal abstract class MoveDiffPreviewAction(private val openInNewWindow: Boolean) : DumbAwareAction() {
 
-  abstract fun isEnabledAndVisible(project: Project): Boolean
+  abstract fun isEnabledAndVisible(project: Project, file: VirtualFile): Boolean
 
   override fun update(e: AnActionEvent) {
     val project = e.project
@@ -20,7 +22,7 @@ internal abstract class MoveDiffPreviewAction(private val openInNewWindow: Boole
     e.presentation.isEnabledAndVisible = project != null
                                          && file != null
                                          && file is DiffContentVirtualFile
-                                         && isEnabledAndVisible(project)
+                                         && isEnabledAndVisible(project, file)
   }
 
 
@@ -33,10 +35,12 @@ internal abstract class MoveDiffPreviewAction(private val openInNewWindow: Boole
   }
 }
 
+private fun VirtualFile.openedInNewWindow() = OPENED_IN_NEW_WINDOW.get(this, false)
+
 internal class MoveDiffPreviewToEditorAction : MoveDiffPreviewAction(false) {
-  override fun isEnabledAndVisible(project: Project): Boolean = VcsEditorTabFilesManager.getInstance().shouldOpenInNewWindow
+  override fun isEnabledAndVisible(project: Project, file: VirtualFile): Boolean = file.openedInNewWindow()
 }
 
 internal class MoveDiffPreviewToNewWindowAction : MoveDiffPreviewAction(true) {
-  override fun isEnabledAndVisible(project: Project): Boolean = !VcsEditorTabFilesManager.getInstance().shouldOpenInNewWindow
+  override fun isEnabledAndVisible(project: Project, file: VirtualFile): Boolean = !file.openedInNewWindow()
 }
