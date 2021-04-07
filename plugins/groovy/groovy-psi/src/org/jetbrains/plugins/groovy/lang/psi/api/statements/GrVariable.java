c15d4aacc56ac3fa2139ce9c54aa63efee95eb56
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-04-07 20:00:22 +0200

    [groovy] GrVariable: add javadocs; clean up
    GitOrigin-RevId: 36545cd321e86983e61165b9f4353a6de20f2b0f

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/api/statements/GrVariable.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/api/statements/GrVariable.java
@@ -1,5 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.lang.psi.api.statements;
 
 import com.intellij.openapi.util.NlsSafe;
@@ -11,47 +10,57 @@ import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.groovy.lang.psi.GrNamedElement;
-import org.jetbrains.plugins.groovy.lang.psi.api.auxiliary.modifiers.GrModifier;
+import org.jetbrains.plugins.groovy.lang.psi.api.auxiliary.modifiers.GrModifier.GrModifierConstant;
 import org.jetbrains.plugins.groovy.lang.psi.api.auxiliary.modifiers.GrModifierList;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrExpression;
 import org.jetbrains.plugins.groovy.lang.psi.api.types.GrTypeElement;
 
-/**
- * @author: Dmitry.Krasilschikov
- * @date: 11.04.2007
- */
 public interface GrVariable extends PsiVariable, GrNamedElement {
+
   GrVariable[] EMPTY_ARRAY = new GrVariable[0];
   ArrayFactory<GrVariable> ARRAY_FACTORY = count -> count == 0 ? EMPTY_ARRAY : new GrVariable[count];
 
   @Override
   @NlsSafe @NotNull String getName();
 
+  /**
+   * Returns corresponding type element, or {@code null} if type is not declared explicitly.
+   * May return same element for different variables in tuple variable declaration.
+   */
+  @Nullable GrTypeElement getTypeElementGroovy();
+
+  /**
+   * Returns statically known type, or {@code null} if type is unknown.
+   * May return non null value for variables without {@link #getTypeElementGroovy()}, e.g., vararg parameters or enum fields.
+   */
+  default @Nullable PsiType getDeclaredType() {
+    GrTypeElement typeElement = getTypeElementGroovy();
+    return typeElement != null ? typeElement.getType() : null;
+  }
+
+  /**
+   * Returns initializer of this variable, or {@code null} if there is no explicit initializer.
+   */
+  @Nullable GrExpression getInitializerGroovy();
+
+  /**
+   * Returns type which initializes this variable.
+   * May return non null value for variables without {@link #getInitializerGroovy()}, e.g., tuple variables.
+   */
   default @Nullable PsiType getInitializerType() {
     GrExpression initializer = getInitializerGroovy();
     return initializer == null ? null : initializer.getType();
   }
 
-  @Nullable
-  GrExpression getInitializerGroovy();
+  @Nullable PsiType getTypeGroovy();
+
+  @Override
+  @Nullable GrModifierList getModifierList();
+
+  @Override
+  boolean hasModifierProperty(@GrModifierConstant @NonNls @NotNull String name);
 
   void setType(@Nullable PsiType type) throws IncorrectOperationException;
 
-  @Nullable
-  GrTypeElement getTypeElementGroovy();
-
-  @Nullable
-  PsiType getTypeGroovy();
-
-  @Nullable
-  PsiType getDeclaredType();
-
-  @Override
-  @Nullable
-  GrModifierList getModifierList();
-
-  void setInitializerGroovy(@Nullable GrExpression initializer);
-
-  @Override
-  boolean hasModifierProperty(@GrModifier.GrModifierConstant @NonNls @NotNull String name);
+  void setInitializerGroovy(@Nullable GrExpression initializer) throws IncorrectOperationException;
 }
