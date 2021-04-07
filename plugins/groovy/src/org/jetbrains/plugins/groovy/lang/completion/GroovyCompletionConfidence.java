1fd57c09c0ef9992f705bd3aae22bbc4a5af8309
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-04-07 12:08:54 +0200

    IDEA-196389 EA-260123 remove `GroovyShellActionBase`
    GitOrigin-RevId: 7b2447c2a3bdaac51b0a2515d196ec2cfeca9c2e

--- plugins/groovy/src/org/jetbrains/plugins/groovy/lang/completion/GroovyCompletionConfidence.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/lang/completion/GroovyCompletionConfidence.java
@@ -1,7 +1,6 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.lang.completion;
 
-import com.intellij.codeInsight.CodeInsightSettings;
 import com.intellij.codeInsight.completion.CompletionConfidence;
 import com.intellij.patterns.PsiJavaPatterns;
 import com.intellij.psi.PsiElement;
@@ -12,8 +11,6 @@ import com.intellij.util.ThreeState;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.groovy.lang.lexer.TokenSets;
 
-import static org.jetbrains.plugins.groovy.shell.GroovyShellRunnerImpl.GROOVY_SHELL_FILE;
-
 /**
  * @author peter
  */
@@ -22,10 +19,6 @@ public class GroovyCompletionConfidence extends CompletionConfidence {
   @NotNull
   @Override
   public ThreeState shouldSkipAutopopup(@NotNull PsiElement contextElement, @NotNull PsiFile psiFile, int offset) {
-    if (CodeInsightSettings.getInstance().isSelectAutopopupSuggestionsByChars() && psiFile.getUserData(GROOVY_SHELL_FILE) == Boolean.TRUE) {
-      return ThreeState.YES;
-    }
-
     if (PsiImplUtil.isLeafElementOfType(contextElement, TokenSets.STRING_LITERALS)) {
       PsiElement parent = contextElement.getParent();
       if (parent != null) {
