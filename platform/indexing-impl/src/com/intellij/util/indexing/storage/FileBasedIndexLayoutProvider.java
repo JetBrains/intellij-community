97e8a33dad485470ccf86a3367014a166c53eeac
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2021-04-07 13:37:38 +0300

    mark as internal
    GitOrigin-RevId: fd7664407d9a52e13f9bc4e53eb02727dac8a534

--- platform/indexing-impl/src/com/intellij/util/indexing/storage/FileBasedIndexLayoutProvider.java
+++ platform/indexing-impl/src/com/intellij/util/indexing/storage/FileBasedIndexLayoutProvider.java
@@ -3,12 +3,14 @@ package com.intellij.util.indexing.storage;
 
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.util.indexing.FileBasedIndexExtension;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * A main interface to override index storages.
  * Use {@link FileBasedIndexLayoutProviderBean} to register a plugin which could provide custom index storage.
  */
+@ApiStatus.Internal
 public interface FileBasedIndexLayoutProvider {
   ExtensionPointName<FileBasedIndexLayoutProviderBean> STORAGE_LAYOUT_EP_NAME
     = ExtensionPointName.create("com.intellij.fileBasedIndexLayout");
