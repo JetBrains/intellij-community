b48ef8f7763748739602247b8b5cce19a2fe5e8e
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2021-04-07 15:51:12 +0700

    Anonymous classes -> lambdas
    GitOrigin-RevId: a6e92b334c6e9faf887386defd8d286c8b349c27

--- plugins/ui-designer/src/com/intellij/uiDesigner/inspections/StringDescriptorInspection.java
+++ plugins/ui-designer/src/com/intellij/uiDesigner/inspections/StringDescriptorInspection.java
@@ -8,18 +8,11 @@ import com.intellij.uiDesigner.propertyInspector.properties.BorderProperty;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.function.Supplier;
-
 /**
  * @author yole
  */
 public abstract class StringDescriptorInspection extends BaseFormInspection {
-  private static final NotNullLazyValue<BorderProperty> myBorderProperty = NotNullLazyValue.lazy(new Supplier<>() {
-    @Override
-    public BorderProperty get() {
-      return new BorderProperty(null);
-    }
-  });
+  private static final NotNullLazyValue<BorderProperty> myBorderProperty = NotNullLazyValue.lazy(() -> new BorderProperty(null));
 
   public StringDescriptorInspection(@NonNls String inspectionKey) {
     super(inspectionKey);
