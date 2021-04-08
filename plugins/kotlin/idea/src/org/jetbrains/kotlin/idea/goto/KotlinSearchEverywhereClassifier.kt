f474133351bb5617c0dad52eaa3a6b9c12b73ca9
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-04-08 17:19:02 +0700

    [goto] replace old copy-pasted SearchEverywherePsiRenderer with inheritance
    GitOrigin-RevId: b1b528ed887a756390c51ca6239350ccb60eb45c

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/goto/KotlinSearchEverywhereClassifier.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/goto/KotlinSearchEverywhereClassifier.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -8,26 +8,30 @@ package org.jetbrains.kotlin.idea.goto
 import com.intellij.ide.actions.SearchEverywhereClassifier
 import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.asJava.unwrapped
+import org.jetbrains.kotlin.idea.core.KotlinPluginDisposable
 import org.jetbrains.kotlin.psi.KtClassOrObject
 import org.jetbrains.kotlin.psi.KtNamedDeclaration
+import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 import java.awt.Component
 import javax.swing.JList
 
 class KotlinSearchEverywhereClassifier : SearchEverywhereClassifier {
     override fun isClass(o: Any?) = o is KtClassOrObject
-
     override fun isSymbol(o: Any?) = o is KtNamedDeclaration
-
-    override fun getVirtualFile(o: Any) = (o as? PsiElement)?.containingFile?.virtualFile
-
+    override fun getVirtualFile(o: Any) = o.safeAs<PsiElement>()?.containingFile?.virtualFile
     override fun getListCellRendererComponent(
         list: JList<*>,
         value: Any?,
         index: Int,
         isSelected: Boolean,
         cellHasFocus: Boolean
-    ): Component? {
-        val declaration = (value as? PsiElement)?.unwrapped as? KtNamedDeclaration ?: return null
-        return KotlinSearchEverywherePsiRenderer(list).getListCellRendererComponent(list, declaration, index, isSelected, isSelected)
+    ): Component? = value.safeAs<PsiElement>()?.unwrapped?.safeAs<KtNamedDeclaration>()?.let {
+        KotlinSearchEverywherePsiRenderer.getInstance(it.project).getListCellRendererComponent(
+            list,
+            it,
+            index,
+            isSelected,
+            isSelected
+        )
     }
 }
