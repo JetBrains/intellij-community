3fb175da3a32c80de156a192d7d8e61427a2408c
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2021-04-08 20:26:58 +0200

    Revert "[ml-completion] fix mapping of compound proximity value (don't cast it to string). For example, UndefinedMapper gets string "null" and returns 0, but actually feature value is absence."
    This reverts commit 0537bea0
    
    GitOrigin-RevId: 18f2da2891c1d6b9f564d447734d31266767600f

--- plugins/completion-ml-ranking/src/com/intellij/completion/ml/util/RelevanceUtil.kt
+++ plugins/completion-ml-ranking/src/com/intellij/completion/ml/util/RelevanceUtil.kt
@@ -3,14 +3,10 @@ package com.intellij.completion.ml.util
 import com.intellij.completion.ml.sorting.FeatureUtils
 import com.intellij.internal.statistic.utils.PluginType
 import com.intellij.internal.statistic.utils.getPluginInfo
-import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.util.Pair
 import com.intellij.openapi.util.text.StringUtil
-import com.intellij.psi.WeighingComparable
 
 object RelevanceUtil {
-  private val LOG = logger<RelevanceUtil>()
-
   private val IGNORED_FACTORS = setOf("kotlin.byNameAlphabetical",
                                       "scalaMethodCompletionWeigher",
                                       "unresolvedOnTop",
@@ -37,11 +33,11 @@ object RelevanceUtil {
       val value = pair.second
       if (name in IGNORED_FACTORS || value == null) continue
       when (name) {
-        "proximity" -> relevanceMap.addProximityValues("prox", value)
-        "kotlin.proximity" -> relevanceMap.addProximityValues("kt_prox", value)
+        "proximity" -> relevanceMap.addCompoundValues("prox", value.toString())
+        "kotlin.proximity" -> relevanceMap.addCompoundValues("kt_prox", value.toString())
         "swiftSymbolProximity" -> {
           relevanceMap[name] = value // while this feature is used in actual models
-          relevanceMap.addProximityValues("swift_prox", value)
+          relevanceMap.addCompoundValues("swift_prox", value.toString())
         }
         "kotlin.callableWeight" -> relevanceMap.addDataClassValues("kotlin.callableWeight", value.toString())
         "ml_weigh" -> additionalMap.addCompoundValues("ml", value.toString())
@@ -72,17 +68,6 @@ object RelevanceUtil {
     val items = proximity.replace("[", "").replace("]", "").split(",")
 
     this.addProperties(prefix, items)
-  }
-
-  private fun MutableMap<String, Any>.addProximityValues(prefix: String, proximity: Any) {
-    if (proximity !is WeighingComparable<*, *>) {
-      LOG.error("Unexpected value type of `$prefix`: ${proximity.javaClass.simpleName}")
-      return
-    }
-
-    for ((name, weight) in proximity.weights) {
-      this["${prefix}_$name"] = weight
-    }
   }
 
   private fun MutableMap<String, Any>.addDataClassValues(featureName: String, dataClassString: String) {
