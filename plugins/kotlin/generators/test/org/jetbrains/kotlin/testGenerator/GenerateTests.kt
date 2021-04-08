364b81e525465a25a2dcb95ad50fa33518ddeba4
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-04-08 11:08:46 +0200

    FIR IDE: introduce tests for checking diagnostic visitor context collection
    Original commit: 32c11019594dcca569db6359afd615a41569c1e8
    
    GitOrigin-RevId: 058b64cd592b9a8d90705717ef00281de6be4d0e

--- plugins/kotlin/generators/test/org/jetbrains/kotlin/testGenerator/GenerateTests.kt
+++ plugins/kotlin/generators/test/org/jetbrains/kotlin/testGenerator/GenerateTests.kt
@@ -69,6 +69,8 @@ import org.jetbrains.kotlin.idea.editor.backspaceHandler.AbstractBackspaceHandle
 import org.jetbrains.kotlin.idea.editor.quickDoc.AbstractQuickDocProviderTest
 import org.jetbrains.kotlin.idea.externalAnnotations.AbstractExternalAnnotationTest
 import org.jetbrains.kotlin.idea.fir.low.level.api.diagnostic.AbstractDiagnosticTraversalCounterTest
+import org.jetbrains.kotlin.idea.fir.low.level.api.diagnostic.AbstractFirContextCollectionTest
+import org.jetbrains.kotlin.idea.fir.low.level.api.resolve.AbstractInnerDeclarationsResolvePhaseTest
 import org.jetbrains.kotlin.idea.folding.AbstractKotlinFoldingTest
 import org.jetbrains.kotlin.idea.frontend.api.components.AbstractExpectedExpressionTypeTest
 import org.jetbrains.kotlin.idea.frontend.api.components.AbstractHLExpressionTypeTest
@@ -1061,6 +1063,9 @@ private fun assembleWorkspace(): TWorkspace = workspace {
         testClass<AbstractFirMultiModuleLazyResolveTest> {
             model("multiModuleLazyResolve", pattern = DIRECTORY, isRecursive = false)
         }
+        testClass<AbstractFirSealedInheritorsTest> {
+            model("resolveSealed", pattern = DIRECTORY, isRecursive = false)
+        }
         testClass<AbstractFirLazyDeclarationResolveTest> {
             model("lazyResolve")
         }
@@ -1073,11 +1078,17 @@ private fun assembleWorkspace(): TWorkspace = workspace {
         testClass<AbstractFileStructureTest> {
             model("fileStructure")
         }
+        testClass<AbstractFirContextCollectionTest> {
+            model("fileStructure")
+        }
         testClass<AbstractDiagnosticTraversalCounterTest> {
             model("diagnosticTraversalCounter")
         }
         testClass<AbstractSessionsInvalidationTest> {
-            model("sessionInvalidation", isRecursive = false, pattern = DIRECTORY)
+            model("sessionInvalidation", pattern = DIRECTORY, isRecursive = false)
+        }
+        testClass<AbstractInnerDeclarationsResolvePhaseTest> {
+            model("innerDeclarationsResolve")
         }
     }
 
