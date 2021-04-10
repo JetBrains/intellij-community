2563ea492ba34f4c7946e302744cfa944ebf758b
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-04-10 15:19:18 +0200

    IDEA-265498 Fix 'KotlinRoot' after Kotlin Plugin merge into monorepo
    GitOrigin-RevId: 3178c1716575d08ef18eef647590c42f5b0b2070

--- plugins/kotlin/common/test/org/jetbrains/kotlin/test/KotlinRoot.kt
+++ plugins/kotlin/common/test/org/jetbrains/kotlin/test/KotlinRoot.kt
@@ -5,45 +5,9 @@ import java.io.File
 
 object KotlinRoot {
     @JvmField
-    val REPO: File
+    val REPO: File = File(PathManager.getHomePath())
 
     @JvmField
-    val DIR: File
-
-    init {
-        var current = File(".").canonicalFile
-
-        var kind = current.getKotlinRootKind()
-        while (kind == null) {
-            current = current.parentFile ?: break
-            kind = current.getKotlinRootKind()
-        }
-        if (kind != null) {
-            REPO = current
-
-            DIR = when (kind) {
-                KotlinRootKind.ULTIMATE -> current.resolve("kotlin")
-                KotlinRootKind.COMMUNITY -> current
-            }
-        } else {
-            REPO = File(PathManager.getHomePath())
-            DIR = File(PathManager.getHomePath())
-        }
-    }
-}
-
-private enum class KotlinRootKind {
-    ULTIMATE, COMMUNITY
-}
-
-private fun File.getKotlinRootKind(): KotlinRootKind? {
-    if (resolve("kotlin.kotlin-ide.iml").isFile && resolve("intellij").isDirectory && resolve("kotlin/idea/kotlin.idea.iml").isFile) {
-        return KotlinRootKind.ULTIMATE
-    }
-
-    if (resolve("kotlin.intellij-kotlin.iml").isFile && resolve("intellij").isDirectory && resolve("idea/kotlin.idea.iml").isFile) {
-        return KotlinRootKind.COMMUNITY
-    }
-
-    return null
+    val DIR: File = REPO.resolve("community/plugins/kotlin").takeIf { it.exists() }
+        ?: File(PathManager.getCommunityHomePath()).resolve("plugins/kotlin")
 }
