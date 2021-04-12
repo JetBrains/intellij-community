20ac2761dbb9b6fa4925e180b0c5b6a9469617d5
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-04-12 12:57:28 +0200

    FIR IDE: add member type-alias test for FileStructureTest
    Original commit: ddf451739b6eb0535359dd1e7e41937c0e7bef22
    
    GitOrigin-RevId: 6ea3f47617c039a9b67cb0591cb84194b4564b50

--- plugins/kotlin/fir-low-level-api/test/org/jetbrains/kotlin/idea/fir/low/level/api/diagnostic/FirContextCollectionTestGenerated.java
+++ plugins/kotlin/fir-low-level-api/test/org/jetbrains/kotlin/idea/fir/low/level/api/diagnostic/FirContextCollectionTestGenerated.java
@@ -71,6 +71,11 @@ public class FirContextCollectionTestGenerated extends AbstractFirContextCollect
         runTest("testdata/fileStructure/localProperty.kt");
     }
 
+    @TestMetadata("memberTypeAlias.kt")
+    public void testMemberTypeAlias() throws Exception {
+        runTest("idea/idea-frontend-fir/idea-fir-low-level-api/testdata/fileStructure/memberTypeAlias.kt");
+    }
+
     @TestMetadata("nestedClasses.kt")
     public void testNestedClasses() throws Exception {
         runTest("testdata/fileStructure/nestedClasses.kt");
