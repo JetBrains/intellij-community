e8968897878de15ebebd7c54ee3c80f539f7518e
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2021-04-12 19:04:29 +0300

    [groovy] IDEA-258067: Avoid querying PsiManager for each instruction in flow
    GitOrigin-RevId: 233f8d6d04d4129fea1fa7ce95ace67ef29bf94f

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/dataFlow/types/TypeDfaInstanceUtil.kt
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/dataFlow/types/TypeDfaInstanceUtil.kt
@@ -8,6 +8,7 @@ import org.jetbrains.plugins.groovy.lang.psi.api.GrFunctionalExpression
 import org.jetbrains.plugins.groovy.lang.psi.controlFlow.Instruction
 import org.jetbrains.plugins.groovy.lang.psi.controlFlow.ReadWriteVariableInstruction
 import org.jetbrains.plugins.groovy.lang.psi.controlFlow.VariableDescriptor
+import org.jetbrains.plugins.groovy.lang.psi.controlFlow.impl.ArgumentsInstruction
 import org.jetbrains.plugins.groovy.lang.psi.controlFlow.impl.getControlFlowOwner
 import org.jetbrains.plugins.groovy.lang.psi.dataFlow.DFAType
 import org.jetbrains.plugins.groovy.lang.psi.impl.statements.expressions.TypesUtil
@@ -16,25 +17,26 @@ fun getLeastUpperBoundByAllWrites(block: GrControlFlowOwner,
                                   initialTypes: Map<VariableDescriptor, DFAType>,
                                   descriptor: VariableDescriptor): PsiType {
   val flow: Array<Instruction> = block.controlFlow
-  var resultType: PsiType = PsiType.NULL
+  val types = mutableSetOf<PsiType>()
   val cache = TypeInferenceHelper.getInferenceCache(block)
   for (instruction: Instruction in flow) {
-    val inferred: PsiType = if (instruction is ReadWriteVariableInstruction && instruction.isWrite && instruction.descriptor == descriptor) {
-      cache.getInferredType(instruction.descriptor, instruction, false, initialTypes) ?: PsiType.NULL
+    if (instruction is ReadWriteVariableInstruction && instruction.isWrite && instruction.descriptor == descriptor) {
+      val inferredType = cache.getInferredType(instruction.descriptor, instruction, false, initialTypes) ?: continue
+      types.add(inferredType)
     }
-    else if (instruction.element is GrFunctionalExpression) {
+    else if (instruction !is ReadWriteVariableInstruction &&
+             instruction !is ArgumentsInstruction &&
+             instruction.element is GrFunctionalExpression) {
       val nestedFlowOwner: GrControlFlowOwner? = (instruction.element as GrFunctionalExpression).getControlFlowOwner()
       if (nestedFlowOwner != null && ControlFlowUtils.getOverwrittenForeignVariableDescriptors(nestedFlowOwner).contains(descriptor)) {
-        getLeastUpperBoundByAllWrites(nestedFlowOwner, initialTypes, descriptor)
+        types.add(getLeastUpperBoundByAllWrites(nestedFlowOwner, initialTypes, descriptor))
       }
-      else {
-        PsiType.NULL
-      }
-    }
-    else PsiType.NULL
-    if (inferred != PsiType.NULL) {
-      resultType = TypesUtil.getLeastUpperBound(resultType, inferred, block.manager) ?: PsiType.NULL
     }
   }
-  return resultType
+  return if (types.isEmpty()) {
+    PsiType.NULL
+  }
+  else {
+    TypesUtil.getLeastUpperBound(types.toTypedArray(), block.manager)
+  }
 }
