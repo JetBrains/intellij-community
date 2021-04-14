7c9d573605f0f7d1e6dcf065433883d1284e4ecc
Louis Vignier <louis.vignier@jetbrains.com>
2021-04-14 17:51:38 +0200

    [java] Update inspection descriptions
    GitOrigin-RevId: f6f817f2fe7bb58f603324b9d0f7da648bae5893

--- plugins/InspectionGadgets/test/com/siyeh/igtest/style/constant_on_wrong_side_of_comparison/ConstantOnRHS.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/style/constant_on_wrong_side_of_comparison/ConstantOnRHS.java
@@ -3,11 +3,11 @@ package com.siyeh.igtest.style.constant_on_rhs;
 public class ConstantOnRHS
 {
     private int m_bar = 4;
-    private boolean m_foo = (m_bar == <warning descr="Constant '3' on right side of comparison">3</warning>);
+    private boolean m_foo = (m_bar == <warning descr="Constant '3' on the right side of the comparison">3</warning>);
 
     public void foo()
     {
-        if(m_bar == <warning descr="Constant '3' on right side of comparison">3</warning>)
+        if(m_bar == <warning descr="Constant '3' on the right side of the comparison">3</warning>)
         {
 
         }
