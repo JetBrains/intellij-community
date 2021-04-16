5267a89af9b190135d1e0d6e9f618b4f84e94cb6
Bas Leijdekkers <basleijdekkers@gmail.com>
2021-04-16 11:39:30 +0200

    IG: fix some false positives and fix test (partially fixes IDEA-266717)
    GitOrigin-RevId: dab7c8fa0c740ae5e255b614729b489176b14352

---
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/junit/parameterized/Wrongsignature3.java
@@ -0,0 +1,7 @@
+@org.junit.runner.RunWith(org.junit.runners.Parameterized.class)
+public class Wrongsignature3 {
+   @org.junit.runners.Parameterized.Parameters
+   public static Integer[] <warning descr="Method 'regExValues()' should have return type Iterable or Object[]">regExValues</warning>() {
+        return null;
+    }
+}
