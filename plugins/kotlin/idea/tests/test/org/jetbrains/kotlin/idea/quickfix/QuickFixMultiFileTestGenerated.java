ded5e5f7dbeab011617e43963d5aa6cab5fc4132
Roman Golyshev <roman.golyshev@jetbrains.com>
2021-04-16 13:26:52 +0500

    FIR IDE: Add `Add Import` quickfix
    This quickfix can import unresolved types and callables
    
    It currently does not support Java types and is not as advanced as in
    the old plugin
    
    Also, enable tests that now pass
    
    Original commit: 22db894076e6ea2442eaacd60bea77422229565b
    
    GitOrigin-RevId: d85e0b8d91f775680705cce037391803aaf5f1af

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/quickfix/QuickFixMultiFileTestGenerated.java
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/quickfix/QuickFixMultiFileTestGenerated.java
@@ -424,6 +424,11 @@ public abstract class QuickFixMultiFileTestGenerated extends AbstractQuickFixMul
                 runTest("testData/quickfix/autoImports/importKotlinStaticPropertyOverloadedSetterFromJava.test");
             }
 
+            @TestMetadata("importNullableTraitWithGenerics.before.Main.kt")
+            public void testImportNullableTraitWithGenerics() throws Exception {
+                runTest("idea/testData/quickfix/autoImports/importNullableTraitWithGenerics.before.Main.kt");
+            }
+
             @TestMetadata("ImportOperatorInvokeWithConvention.before.Main.kt")
             public void testImportOperatorInvokeWithConvention() throws Exception {
                 runTest("testData/quickfix/autoImports/ImportOperatorInvokeWithConvention.before.Main.kt");
@@ -612,6 +617,11 @@ public abstract class QuickFixMultiFileTestGenerated extends AbstractQuickFixMul
             @TestMetadata("noImportForPrivateClass.before.Main.kt")
             public void testNoImportForPrivateClass() throws Exception {
                 runTest("testData/quickfix/autoImports/noImportForPrivateClass.before.Main.kt");
+            }
+
+            @TestMetadata("noImportForPrivateFunction.before.Main.kt")
+            public void testNoImportForPrivateFunction() throws Exception {
+                runTest("idea/testData/quickfix/autoImports/noImportForPrivateFunction.before.Main.kt");
             }
 
             @TestMetadata("noImportInImports.before.Main.kt")
