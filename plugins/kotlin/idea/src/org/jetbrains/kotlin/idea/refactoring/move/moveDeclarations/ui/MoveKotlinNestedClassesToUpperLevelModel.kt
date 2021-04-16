c89e55bf25034925cf85bb0a914c4b03356a3065
Andrei Klunnyi <andrei.klunnyi@jetbrains.com>
2021-04-16 18:17:39 +0200

    KT-44841 Move sealed: no warning on splitting hierarchy (JPS)
    To provide similar behavior for both Gradle and JPS projects target and
    source directories are now taken into account.
    
    Prior to this commit AbstractMoveTest.kt contained a hack allowing not
    to create empty target directories. It breaks updated logic of the move
    check and therefore is removed.
    To commit empty target directories under 'before' to git one needs to
    put something in them (git ignores dirs otherwise). Hence .gitignore-s.
    
    ^KT-44841 Fixed
    
    Original commit: 89801a1ac7b0944cfeca532c42bb54e2ab6c8d08
    
    GitOrigin-RevId: 096b2cdccd194f3937ad0930c2bc1350fd7402f9

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/refactoring/move/moveDeclarations/ui/MoveKotlinNestedClassesToUpperLevelModel.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/refactoring/move/moveDeclarations/ui/MoveKotlinNestedClassesToUpperLevelModel.kt
@@ -173,7 +173,7 @@ internal abstract class MoveKotlinNestedClassesToUpperLevelModel(
 
             val target = KotlinMoveTargetForDeferredFile(
                 targetPackageFqName,
-                target.virtualFile
+                target
             ) { createKotlinFile(targetFileName, target, targetPackageFqName.asString()) }
 
             target to MoveRefactoringDestination.FILE
