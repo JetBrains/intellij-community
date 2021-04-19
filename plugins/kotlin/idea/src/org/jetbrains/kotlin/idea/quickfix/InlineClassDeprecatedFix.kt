fe4b05ae527434e20bf8734b2d04370647bca579
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2021-04-19 12:14:53 +0200

    Add @JvmInline annotation in common code if JVM target is present
     #KTIJ-8863 Fixed
     #KT-46088 Fixed
    
    Original commit: f5379c5a0496cb259763bfc8273c4bb20c22e714
    
    GitOrigin-RevId: ab896521a8072eca5fc554952c9f482351433940

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/InlineClassDeprecatedFix.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/InlineClassDeprecatedFix.kt
@@ -8,14 +8,14 @@ package org.jetbrains.kotlin.idea.quickfix
 import com.intellij.codeInsight.intention.IntentionAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
-import org.jetbrains.annotations.Nls
 import org.jetbrains.kotlin.diagnostics.Diagnostic
 import org.jetbrains.kotlin.diagnostics.Errors
 import org.jetbrains.kotlin.idea.KotlinBundle
 import org.jetbrains.kotlin.idea.project.TargetPlatformDetector
 import org.jetbrains.kotlin.idea.util.addAnnotation
 import org.jetbrains.kotlin.lexer.KtTokens
-import org.jetbrains.kotlin.platform.jvm.isJvm
+import org.jetbrains.kotlin.platform.has
+import org.jetbrains.kotlin.platform.jvm.JvmPlatform
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtModifierListOwner
 import org.jetbrains.kotlin.psi.psiUtil.getParentOfType
@@ -25,20 +25,19 @@ class InlineClassDeprecatedFix(
     element: KtModifierListOwner
 ) : KotlinQuickFixAction<KtModifierListOwner>(element), CleanupFix {
 
-    override fun getText() =
-        element?.let {
-            KotlinBundle.message(
-                "replace.with.0",
-                (if (TargetPlatformDetector.getPlatform(it.containingKtFile).isJvm()) "@JvmInline " else "") + "value"
-            )
-        } ?: ""
+    private val text = KotlinBundle.message(
+        "replace.with.0",
+        (if (element.containingKtFile.hasJvmTarget()) "@JvmInline " else "") + "value"
+    )
+
+    override fun getText() = text
 
     override fun getFamilyName() = KotlinBundle.message("replace.modifier")
 
     override fun invoke(project: Project, editor: Editor?, file: KtFile) {
         element?.removeModifier(KtTokens.INLINE_KEYWORD)
         element?.addModifier(KtTokens.VALUE_KEYWORD)
-        if (TargetPlatformDetector.getPlatform(file).isJvm()) {
+        if (file.hasJvmTarget()) {
             element?.addAnnotation(JVM_INLINE_ANNOTATION_FQ_NAME)
         }
     }
@@ -50,4 +49,7 @@ class InlineClassDeprecatedFix(
             return if (deprecatedModifier != null) InlineClassDeprecatedFix(modifierListOwner) else null
         }
     }
+
+    private fun KtFile.hasJvmTarget(): Boolean =
+        TargetPlatformDetector.getPlatform(this).has<JvmPlatform>()
 }
