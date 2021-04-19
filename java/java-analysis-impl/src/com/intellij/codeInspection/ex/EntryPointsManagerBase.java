c550716f18fdd604b43e136376fb8e4900759962
Ilyas Selimov <ilyas.selimov@jetbrains.com>
2021-04-19 14:59:29 +0700

    IDEA-265399 - fixed collector to recognize annotation patterns as well
    (cherry picked from commit 7480998fa1b4668a74c57a3a4c39faafd3728e12)
    
    GitOrigin-RevId: ab3ce57c3cfcff582de0e7af8739077016ce3241

--- java/java-analysis-impl/src/com/intellij/codeInspection/ex/EntryPointsManagerBase.java
+++ java/java-analysis-impl/src/com/intellij/codeInspection/ex/EntryPointsManagerBase.java
@@ -541,6 +541,10 @@ public abstract class EntryPointsManagerBase extends EntryPointsManager implemen
     return false;
   }
 
+  public List<String> getCustomAdditionalAnnotations() {
+    return List.copyOf(ADDITIONAL_ANNOTATIONS);
+  }
+
   public List<String> getWriteAnnotations() {
     return List.copyOf(myWriteAnnotations);
   }
