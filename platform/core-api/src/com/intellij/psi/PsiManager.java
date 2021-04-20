eb3fa01d892e6c4b0c5947bb9d91dcec36807903
Dmitry Jemerov <yole@jetbrains.com>
2021-04-20 17:08:14 +0200

    Delete ages-old TODO
    GitOrigin-RevId: 225377e92c610aa1df95213de67b5a1f97fca960

--- platform/core-api/src/com/intellij/psi/PsiManager.java
+++ platform/core-api/src/com/intellij/psi/PsiManager.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi;
 
 import com.intellij.openapi.Disposable;
@@ -76,7 +76,7 @@ public abstract class PsiManager extends UserDataHolderBase {
    *
    * @param file the PSI file to reload.
    */
-  public abstract void reloadFromDisk(@NotNull PsiFile file);   //todo: move to FileDocumentManager
+  public abstract void reloadFromDisk(@NotNull PsiFile file);
 
   /**
    * Adds a listener for receiving notifications about all changes in the PSI tree of the project.
