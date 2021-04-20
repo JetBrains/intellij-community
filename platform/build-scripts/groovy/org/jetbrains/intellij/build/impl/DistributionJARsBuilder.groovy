4b2a11d66ed05ad2da44e05756722956d246d7bf
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2021-04-20 08:39:42 +0200

    exclude intellij-test-discovery from jar merging
    GitOrigin-RevId: 89d689c064640553a755b48076f7727f427a027f

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/DistributionJARsBuilder.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/DistributionJARsBuilder.groovy
@@ -1147,7 +1147,7 @@ final class DistributionJARsBuilder {
   private static Set<String> excludedFromMergeLibs = Set.of(
     "JDOM", "jna", "Log4J", "sqlite", "Slf4j", "Trove4j", "async-profiler", "precompiled_jshell-frontend",
     "dexlib2", // android-only lib
-    "intellij-coverage", // used as agent
+    "intellij-coverage", "intellij-test-discovery", // used as agent
     "winp", "junixsocket-core", "pty4j" // contains native library
   )
 
