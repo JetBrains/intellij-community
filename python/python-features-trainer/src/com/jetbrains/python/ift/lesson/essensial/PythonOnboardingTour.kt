1121d02e50a2cda82b5353d80cb1b8f024e38b4c
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2021-04-20 11:53:14 +0300

    [IFT] Return focus to editor to prevent double right-click
    (cherry picked from commit 160e31fcab70b3fec48208bc2a2edf85b3504e9a)
    
    IDEA-CR-71309
    
    GitOrigin-RevId: a0c8b1e304f5b84ab3af49ef6d9a3ad8d7350225

--- python/python-features-trainer/src/com/jetbrains/python/ift/lesson/essensial/PythonOnboardingTour.kt
+++ python/python-features-trainer/src/com/jetbrains/python/ift/lesson/essensial/PythonOnboardingTour.kt
@@ -342,6 +342,7 @@ class PythonOnboardingTour :
 
     prepareRuntimeTask {
       LearningUiHighlightingManager.clearHighlights()
+      FocusManagerImpl.getInstance(project).requestFocus(editor.contentComponent, false)
     }
   }
 
