0ed118536bcc86d5dc891cc7f85d9517ec9abdcb
Bas Leijdekkers <basleijdekkers@gmail.com>
2021-04-20 15:18:11 +0200

    IG: don't warn about "Comparable implemented but 'equals()' not overridden" in records (IDEA-267452)
    GitOrigin-RevId: 4c761ff0607cc6f34b00897ea1cbf0490c82412b

--- plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/bugs/ComparableImplementedButEqualsNotOverriddenInspection.java
+++ plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/bugs/ComparableImplementedButEqualsNotOverriddenInspection.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2006-2018 Bas Leijdekkers
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.siyeh.ig.bugs;
 
 import com.google.common.annotations.VisibleForTesting;
@@ -143,8 +129,10 @@ public class ComparableImplementedButEqualsNotOverriddenInspection extends BaseI
     @Override
     public void visitClass(PsiClass aClass) {
       super.visitClass(aClass);
-      if (aClass.isInterface()) {
+      if (aClass.isInterface() || aClass.isRecord() || aClass.isEnum()) {
         // the problem can't be fixed for an interface, so let's not report it
+        // a record has an equals method implemented by default
+        // an enum has a default final compareTo() method, no need to check
         return;
       }
       final PsiClass comparableClass =
