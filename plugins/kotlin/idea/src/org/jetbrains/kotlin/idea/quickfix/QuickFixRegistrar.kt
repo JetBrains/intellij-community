46bc707d41ca613fde42a2bf7879efbaf508ae72
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2021-04-21 11:08:39 +0200

    Disable inline class -> @JvmInline value class quick-fix
    Original commit: 64d415de8d7a3ad98340255c7633a13e77ee0119
    
    GitOrigin-RevId: 9c15425ffb024c2481d386c0451150647f66e30b

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/QuickFixRegistrar.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/QuickFixRegistrar.kt
@@ -664,7 +664,8 @@ class QuickFixRegistrar : QuickFixContributor {
         COMPATIBILITY_WARNING.registerFactory(UseFullyQualifiedCallFix)
 
         INLINE_CLASS_CONSTRUCTOR_NOT_FINAL_READ_ONLY_PARAMETER.registerFactory(InlineClassConstructorNotValParameterFactory)
-        INLINE_CLASS_DEPRECATED.registerFactory(InlineClassDeprecatedFix)
+
+//        INLINE_CLASS_DEPRECATED.registerFactory(InlineClassDeprecatedFix)
 
         SUBCLASS_CANT_CALL_COMPANION_PROTECTED_NON_STATIC.registerFactory(AddJvmStaticAnnotationFix)
 
