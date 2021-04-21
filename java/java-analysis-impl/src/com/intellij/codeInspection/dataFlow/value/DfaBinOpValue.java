ceebc0dcb3581ca8005ee1559f872df938af7bb0
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2021-04-21 14:23:11 +0700

    [java-dfa] DfaBinOpValue: avoid dependency on DfTypes
    GitOrigin-RevId: 02bee64425cd14d69111efa9d9f7ab1658a9b018

--- java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/value/DfaBinOpValue.java
+++ java/java-analysis-impl/src/com/intellij/codeInspection/dataFlow/value/DfaBinOpValue.java
@@ -7,7 +7,6 @@ import com.intellij.codeInspection.dataFlow.rangeSet.LongRangeSet;
 import com.intellij.codeInspection.dataFlow.types.DfConstantType;
 import com.intellij.codeInspection.dataFlow.types.DfIntegralType;
 import com.intellij.codeInspection.dataFlow.types.DfType;
-import com.intellij.codeInspection.dataFlow.types.DfTypes;
 import com.intellij.openapi.util.Trinity;
 import com.intellij.util.ObjectUtils;
 import org.jetbrains.annotations.NotNull;
@@ -157,7 +156,7 @@ public final class DfaBinOpValue extends DfaValue {
         right = right.getFactory().fromDfType(rightDfType);
       }
       if (op == LongRangeBinOp.MINUS && state.areEqual(left, right)) {
-        return myFactory.fromDfType(DfTypes.intValue(0));
+        return myFactory.fromDfType(resultType.meetRange(LongRangeSet.point(0)));
       }
       if (op == LongRangeBinOp.MOD) {
         if (leftDfType instanceof DfIntegralType && rightDfType instanceof DfIntegralType) {
