5ff1909effa038d0b13c0d8213a2b62735eca61b
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2021-04-21 12:11:35 +0300

    [build scripts] add required libraries to standalone JPS distribution
    (cherry picked from commit 0862c8c9688f55210aa882600320337167d58de7)
    
    IDEA-CR-71318
    
    GitOrigin-RevId: f63a71ed78762cae59a8a45035ed22fff6baceda

--- build/groovy/org/jetbrains/intellij/build/CommunityStandaloneJpsBuilder.groovy
+++ build/groovy/org/jetbrains/intellij/build/CommunityStandaloneJpsBuilder.groovy
@@ -82,7 +82,8 @@ final class CommunityStandaloneJpsBuilder {
         for (String name in List.of(
           "JDOM", "jna", "OroMatcher", "Trove4j", "ASM", "NanoXML", "protobuf", "cli-parser", "Log4J", "jgoodies-forms", "Eclipse",
           "netty-codec-http", "lz4-java", "commons-codec", "commons-logging", "http-client", "Slf4j", "Guava", "plexus-utils",
-          "jetbrains-annotations-java5", "qdox-java-parser", "gson", "jps-javac-extension", "fastutil-min", "kotlin-stdlib-jdk8"
+          "jetbrains-annotations-java5", "qdox-java-parser", "gson", "jps-javac-extension", "fastutil-min", "kotlin-stdlib-jdk8",
+          "commons-lang3", "maven-resolver-provider", "netty-buffer"
         )) {
           projectLibrary(name)
         }
