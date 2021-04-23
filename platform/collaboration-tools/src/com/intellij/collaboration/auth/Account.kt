900dfeed1c8d7b6ee2bc4aca5435a004b4ebb6da
Ivan Semenov <ivan.semenov@jetbrains.com>
2021-04-23 17:25:44 +0300

    [github] convert account to kotlin
    GitOrigin-RevId: afc09c0da3c3b1a59dc28ee86c65cbe105d430c8

--- platform/collaboration-tools/src/com/intellij/collaboration/auth/Account.kt
+++ platform/collaboration-tools/src/com/intellij/collaboration/auth/Account.kt
@@ -1,6 +1,23 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.collaboration.auth
 
-interface Account {
-  val id: String
+import com.intellij.util.xmlb.annotations.Attribute
+import com.intellij.util.xmlb.annotations.Tag
+import java.util.*
+
+@Tag("account")
+abstract class Account(@Attribute("id") val id: String = UUID.randomUUID().toString()) {
+
+  final override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (other !is Account) return false
+
+    if (id != other.id) return false
+
+    return true
+  }
+
+  final override fun hashCode(): Int {
+    return id.hashCode()
+  }
 }
