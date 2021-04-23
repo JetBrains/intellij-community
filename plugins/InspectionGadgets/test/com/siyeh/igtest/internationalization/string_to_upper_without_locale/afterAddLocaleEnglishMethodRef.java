cb78cdf494932b7f4cf304d79575c91613bd4d6d
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2021-04-23 16:06:42 +0700

    [java-inspections] IDEA-267805 Use "Locale.ROOT" instead of "Locale.ENGLISH" in toUpperCase() without Locale inspection quick-fix
    GitOrigin-RevId: d1da1df7f1e81c8b310e4fad68473b2ce760880e

--- plugins/InspectionGadgets/test/com/siyeh/igtest/internationalization/string_to_upper_without_locale/afterAddLocaleEnglishMethodRef.java
+++ plugins/InspectionGadgets/test/com/siyeh/igtest/internationalization/string_to_upper_without_locale/afterAddLocaleEnglishMethodRef.java
@@ -1,9 +1,9 @@
-// "Add 'Locale.ENGLISH' argument" "true"
+// "Add 'Locale.ROOT' argument" "true"
 import java.util.Locale;
 import java.util.Optional;
 
 class X {
   void test(Optional<String> opt) {
-    String s = opt.map(s1 -> s1.toUpperCase(Locale.ENGLISH)).orElse(null);
+    String s = opt.map(s1 -> s1.toUpperCase(Locale.ROOT)).orElse(null);
   }
 }
