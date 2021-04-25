0c473a36997235e04d97a8a53f0cd7d28e8f91b1
Mark Punzalan <markpun@google.com>
2021-04-25 21:39:25 +0000

    FIR IDE: Enable AddExclExclCallFix for UNSAFE_CALL, UNSAFE_OPERATOR_CALL, UNSAFE_INFIX_CALL, ITERATOR_ON_NULLABLE, ARGUMENT_TYPE_MISMATCH, RETURN_TYPE_MISMATCH.
    TODO: Don't offer fix when target is known to be null (from data flow
    analysis).
    
    Original commit: db82797f58fbe039ec9e8363c75021d1daef16f9
    
    GitOrigin-RevId: 281f66b7202d192c1dbf54b45fe4c45c98766620

--- plugins/kotlin/fir/test/org/jetbrains/kotlin/idea/quickfix/HighLevelQuickFixTestGenerated.java
+++ plugins/kotlin/fir/test/org/jetbrains/kotlin/idea/quickfix/HighLevelQuickFixTestGenerated.java
@@ -261,6 +261,11 @@ public abstract class HighLevelQuickFixTestGenerated extends AbstractHighLevelQu
             runTest("idea/testData/quickfix/addExclExclCall/implicitFunctionCall.kt");
         }
 
+        @TestMetadata("infixCall.kt")
+        public void testInfixCall() throws Exception {
+            runTest("idea/testData/quickfix/addExclExclCall/infixCall.kt");
+        }
+
         @TestMetadata("iterable.kt")
         public void testIterable() throws Exception {
             runTest("idea/testData/quickfix/addExclExclCall/iterable.kt");
@@ -296,6 +301,16 @@ public abstract class HighLevelQuickFixTestGenerated extends AbstractHighLevelQu
             runTest("idea/testData/quickfix/addExclExclCall/operationIn.kt");
         }
 
+        @TestMetadata("operationPlus.kt")
+        public void testOperationPlus() throws Exception {
+            runTest("idea/testData/quickfix/addExclExclCall/operationPlus.kt");
+        }
+
+        @TestMetadata("operationUnary.kt")
+        public void testOperationUnary() throws Exception {
+            runTest("idea/testData/quickfix/addExclExclCall/operationUnary.kt");
+        }
+
         @TestMetadata("idea/testData/quickfix/addExclExclCall/typeMismatch")
         @TestDataPath("$PROJECT_ROOT")
         @RunWith(JUnit3RunnerWithInners.class)
@@ -306,6 +321,11 @@ public abstract class HighLevelQuickFixTestGenerated extends AbstractHighLevelQu
 
             public void testAllFilesPresentInTypeMismatch() throws Exception {
                 KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("idea/testData/quickfix/addExclExclCall/typeMismatch"), Pattern.compile("^([\\w\\-_]+)\\.kt$"), null, true);
+            }
+
+            @TestMetadata("argumentAfterNullCheck.kt")
+            public void testArgumentAfterNullCheck() throws Exception {
+                runTest("idea/testData/quickfix/addExclExclCall/typeMismatch/argumentAfterNullCheck.kt");
             }
 
             @TestMetadata("assignmentRValue.kt")
@@ -321,6 +341,11 @@ public abstract class HighLevelQuickFixTestGenerated extends AbstractHighLevelQu
             @TestMetadata("memberAccessInExtensionAsAssignmentRValue.kt")
             public void testMemberAccessInExtensionAsAssignmentRValue() throws Exception {
                 runTest("idea/testData/quickfix/addExclExclCall/typeMismatch/memberAccessInExtensionAsAssignmentRValue.kt");
+            }
+
+            @TestMetadata("nullArgument.kt")
+            public void testNullArgument() throws Exception {
+                runTest("idea/testData/quickfix/addExclExclCall/typeMismatch/nullArgument.kt");
             }
 
             @TestMetadata("qualifiedArgument.kt")
