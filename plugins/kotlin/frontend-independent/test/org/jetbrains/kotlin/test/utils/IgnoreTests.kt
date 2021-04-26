0c8248cd6c763da4ef6611847691bcb81a24e5d4
Tianyu Geng <tgeng@google.com>
2021-04-26 11:42:10 -0700

    FIR IDE: fix typo in package name
    Original commit: e56b39bbd843fbf08f1265a0bf93cee7a04091db
    
    GitOrigin-RevId: 09af316cf71f3f60a374c994ff94b7ec6fe0a4cc

--- plugins/kotlin/frontend-independent/test/org/jetbrains/kotlin/test/utils/IgnoreTests.kt
+++ plugins/kotlin/frontend-independent/test/org/jetbrains/kotlin/test/utils/IgnoreTests.kt
@@ -1,6 +1,6 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
-package org.jetbrains.kotlin.test.util
+package org.jetbrains.kotlin.test.utils
 
 import com.intellij.openapi.util.io.FileUtil
 import org.junit.Assert
