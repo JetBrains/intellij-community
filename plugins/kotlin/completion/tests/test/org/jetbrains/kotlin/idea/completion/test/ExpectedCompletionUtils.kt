6e94bb06841b2a475c8c58d52c3e2a8b101b5ea2
Tianyu Geng <tgeng@google.com>
2021-04-26 11:42:10 -0700

    FIR IDE: fix typo in package name
    Original commit: e56b39bbd843fbf08f1265a0bf93cee7a04091db
    
    GitOrigin-RevId: caef15e9d55bf0b823ac3c04ab1364ab184772af

--- plugins/kotlin/completion/tests/test/org/jetbrains/kotlin/idea/completion/test/ExpectedCompletionUtils.kt
+++ plugins/kotlin/completion/tests/test/org/jetbrains/kotlin/idea/completion/test/ExpectedCompletionUtils.kt
@@ -23,7 +23,7 @@ import org.jetbrains.kotlin.platform.js.isJs
 import org.jetbrains.kotlin.platform.jvm.JvmPlatforms
 import org.jetbrains.kotlin.platform.jvm.isJvm
 import org.jetbrains.kotlin.idea.test.InTextDirectivesUtils
-import org.jetbrains.kotlin.test.util.IgnoreTests
+import org.jetbrains.kotlin.test.utils.IgnoreTests
 import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 import org.junit.Assert
 
