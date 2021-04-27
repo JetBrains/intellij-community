f44507d71291a562114e9e0b5dccb61bae3d9ba2
Alexey Kalina <alexey.kalina@jetbrains.com>
2021-04-27 13:23:34 +0300

    [ml-completion] IDEA-267985: calculate vcs_declaration_changed only for cached documents and changes
    (cherry picked from commit 302dd979f8a78b7ef06f915ba2b3557cefc307e9)
    
    IJ-CR-8658
    
    GitOrigin-RevId: 8dd29bcb72b99b6d52c240427022084a58950105

--- platform/vcs-impl/src/com/intellij/codeInsight/completion/ml/VcsFeatureProvider.kt
+++ platform/vcs-impl/src/com/intellij/codeInsight/completion/ml/VcsFeatureProvider.kt
@@ -1,67 +1,39 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.completion.ml
 
-import com.intellij.codeInsight.actions.VcsFacade
 import com.intellij.codeInsight.completion.CompletionLocation
 import com.intellij.codeInsight.lookup.LookupElement
-import com.intellij.openapi.application.runReadAction
-import com.intellij.openapi.vcs.changes.Change
 import com.intellij.openapi.vcs.changes.ChangeListManager
-import com.intellij.psi.PsiNameIdentifierOwner
-import com.intellij.util.concurrency.AppExecutorUtil
-import com.intellij.vcsUtil.VcsUtil
-import java.util.concurrent.Callable
-import java.util.concurrent.FutureTask
-import java.util.concurrent.TimeUnit
-import java.util.concurrent.TimeoutException
+import com.intellij.openapi.vcs.impl.LineStatusTrackerManager
+import com.intellij.psi.PsiDocumentManager
 
 class VcsFeatureProvider : ElementFeatureProvider {
-  companion object {
-    private const val GIT_VCS_NAME = "Git"
-  }
-  private val executor = AppExecutorUtil.createBoundedApplicationPoolExecutor("ML completion. VCS feature calculation", 1)
-
   override fun getName(): String = "vcs"
 
   override fun calculateFeatures(element: LookupElement,
                                  location: CompletionLocation,
                                  contextFeatures: ContextFeatures): Map<String, MLFeatureValue> {
     val features = mutableMapOf<String, MLFeatureValue>()
+    val project = location.project
     val psi = element.psiElement
     val psiFile = psi?.containingFile
 
     psiFile?.viewProvider?.virtualFile?.let { file ->
-      val changeListManager = ChangeListManager.getInstance(location.project)
+      val changeListManager = ChangeListManager.getInstance(project)
       changeListManager.getChange(file)?.let { change ->
         features["file_state"] = MLFeatureValue.categorical(change.type) // NON-NLS
-
-        val vcs = VcsUtil.getVcsFor(location.project, file)
-        if (vcs?.name == GIT_VCS_NAME && change.type == Change.Type.MODIFICATION && psi is PsiNameIdentifierOwner) {
-          calculateWithTimeout {
-            runReadAction {
-              val changedRanges = VcsFacade.getInstance().getChangedTextRanges(location.project, psiFile)
-              if (changedRanges.any { psi.textRange?.intersects(it) == true })
-                MLFeatureValue.binary(true) else null
+        val document = PsiDocumentManager.getInstance(project).getCachedDocument(psiFile)
+        val range = psi.textRange
+        if (document != null && range != null) {
+          val lineStatusTracker = LineStatusTrackerManager.getInstance(project).getLineStatusTracker(document)
+          if (lineStatusTracker != null && lineStatusTracker.isValid()) {
+            if (lineStatusTracker.isRangeModified(document.getLineNumber(range.startOffset), document.getLineNumber(range.endOffset))) {
+              features["declaration_is_changed"] = MLFeatureValue.binary(true) // NON-NLS
             }
-          }?.let {
-            features["declaration_is_changed"] = it // NON-NLS
           }
         }
       }
     }
     return features
-  }
-
-  private fun calculateWithTimeout(timeout: Long = 100,
-                                   timeUnit: TimeUnit = TimeUnit.MILLISECONDS,
-                                   callable: Callable<MLFeatureValue?>): MLFeatureValue? {
-    val future = FutureTask(callable)
-    executor.execute(future)
-    var result: MLFeatureValue? = null
-    try {
-      result = future.get(timeout, timeUnit)
-    }
-    catch (ignored: TimeoutException) { }
-    return result
   }
 }
