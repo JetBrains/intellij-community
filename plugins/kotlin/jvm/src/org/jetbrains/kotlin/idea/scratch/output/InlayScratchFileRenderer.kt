64dc1c431a1342429c14dfedfa660919c9b6f40d
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2021-04-27 16:45:14 +0900

    Revert "Migrate deprecations in core, compiler, idea, tests"
    This reverts commit effa2c49
    
    GitOrigin-RevId: 50b639013721033bc50c7f68cb291b74bc740a76

--- plugins/kotlin/jvm/src/org/jetbrains/kotlin/idea/scratch/output/InlayScratchFileRenderer.kt
+++ plugins/kotlin/jvm/src/org/jetbrains/kotlin/idea/scratch/output/InlayScratchFileRenderer.kt
@@ -18,7 +18,7 @@ class InlayScratchFileRenderer(val text: String, private val outputType: Scratch
         val attributes = getAttributesForOutputType(outputType)
         val fontStyle = attributes.fontType
         return ComplementaryFontsRegistry.getFontAbleToDisplay(
-            'a'.code, fontStyle, fontPreferences, FontInfo.getFontRenderContext(editor.contentComponent)
+            'a'.toInt(), fontStyle, fontPreferences, FontInfo.getFontRenderContext(editor.contentComponent)
         )
     }
 
