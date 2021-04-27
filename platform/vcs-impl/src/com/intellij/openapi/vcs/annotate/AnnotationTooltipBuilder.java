62bc38e13761a65e0a07fa554137715b167dfa6e
Aleksey Pivovarov <AMPivovarov@gmail.com>
2021-04-27 21:53:39 +0300

    vcs: fix package of AnnotationTooltipBuilder
    GitOrigin-RevId: a1ac3e37780335580b531e96822338798b9aeb04

--- platform/vcs-impl/src/com/intellij/openapi/vcs/annotate/AnnotationTooltipBuilder.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/annotate/AnnotationTooltipBuilder.java
@@ -1,5 +1,5 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package git4idea.annotate;
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.vcs.annotate;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.NlsSafe;
