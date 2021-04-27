00d0af81f157a8f3e9c47d7e0c2d48bf4b5d4886
Alexey Kalina <alexey.kalina@jetbrains.com>
2021-04-27 14:49:42 +0300

    [ml-completion] IDEA-267985: check that change is modification and element is identifier
    (cherry picked from commit d518f1d02ca0a871bba54909e0b7005b071f4be7)
    
    IJ-CR-8658
    
    GitOrigin-RevId: 4d9b8aaf80c95141f11e450f730fc2a4584e4f58

--- platform/vcs-impl/src/com/intellij/codeInsight/completion/ml/VcsFeatureProvider.kt
+++ platform/vcs-impl/src/com/intellij/codeInsight/completion/ml/VcsFeatureProvider.kt
@@ -3,9 +3,11 @@ package com.intellij.codeInsight.completion.ml
 
 import com.intellij.codeInsight.completion.CompletionLocation
 import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.openapi.vcs.changes.Change
 import com.intellij.openapi.vcs.changes.ChangeListManager
 import com.intellij.openapi.vcs.impl.LineStatusTrackerManager
 import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiNameIdentifierOwner
 
 class VcsFeatureProvider : ElementFeatureProvider {
   override fun getName(): String = "vcs"
@@ -22,13 +24,15 @@ class VcsFeatureProvider : ElementFeatureProvider {
       val changeListManager = ChangeListManager.getInstance(project)
       changeListManager.getChange(file)?.let { change ->
         features["file_state"] = MLFeatureValue.categorical(change.type) // NON-NLS
-        val document = PsiDocumentManager.getInstance(project).getCachedDocument(psiFile)
-        val range = psi.textRange
-        if (document != null && range != null) {
-          val lineStatusTracker = LineStatusTrackerManager.getInstance(project).getLineStatusTracker(document)
-          if (lineStatusTracker != null && lineStatusTracker.isValid()) {
-            if (lineStatusTracker.isRangeModified(document.getLineNumber(range.startOffset), document.getLineNumber(range.endOffset))) {
-              features["declaration_is_changed"] = MLFeatureValue.binary(true) // NON-NLS
+        if (change.type == Change.Type.MODIFICATION && psi is PsiNameIdentifierOwner) {
+          val document = PsiDocumentManager.getInstance(project).getCachedDocument(psiFile)
+          val range = psi.textRange
+          if (document != null && range != null) {
+            val lineStatusTracker = LineStatusTrackerManager.getInstance(project).getLineStatusTracker(document)
+            if (lineStatusTracker != null && lineStatusTracker.isValid()) {
+              if (lineStatusTracker.isRangeModified(document.getLineNumber(range.startOffset), document.getLineNumber(range.endOffset))) {
+                features["declaration_is_changed"] = MLFeatureValue.binary(true) // NON-NLS
+              }
             }
           }
         }
