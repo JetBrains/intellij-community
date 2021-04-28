ff84812ac487e89e53ac2c7cd42ec6ba27e0847e
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2021-04-28 23:17:58 +0200

    database-dialects: don't depend on platform-ide (LAB-77)
    GitOrigin-RevId: 3d9e2230a0a096592f3177463750f015fbe5584f

---
+++ platform/analysis-impl/src/com/intellij/ide/PlatformIdeService.java
@@ -0,0 +1,22 @@
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.ide;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.util.NlsContexts;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public class PlatformIdeService {
+  public static PlatformIdeService getInstance() {
+    return ApplicationManager.getApplication().getService(PlatformIdeService.class);
+  }
+
+  public void warningNotification(@NotNull @NonNls String groupId,
+                                  @Nullable Icon icon,
+                                  @Nullable @NlsContexts.NotificationTitle String title,
+                                  @Nullable @NlsContexts.NotificationSubtitle String subtitle,
+                                  @Nullable @NlsContexts.NotificationContent String content) {}
+}
---
+++ platform/platform-impl/src/com/intellij/ide/PlatformIdeServiceImpl.java
@@ -0,0 +1,32 @@
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.ide;
+
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
+import com.intellij.openapi.util.NlsContexts;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public class PlatformIdeServiceImpl extends PlatformIdeService {
+  @Override
+  public void warningNotification(@NotNull @NonNls String groupId,
+                                  @Nullable Icon icon,
+                                  @Nullable @NlsContexts.NotificationTitle String title,
+                                  @Nullable @NlsContexts.NotificationSubtitle String subtitle,
+                                  @Nullable @NlsContexts.NotificationContent String content) {
+    notification(groupId, icon, title, subtitle, content, NotificationType.WARNING);
+  }
+
+  private static void notification(@NonNls @NotNull String groupId,
+                                   @Nullable Icon icon,
+                                   @Nullable @NlsContexts.NotificationTitle String title,
+                                   @Nullable @NlsContexts.NotificationSubtitle String subtitle,
+                                   @Nullable @NlsContexts.NotificationContent String content, @NotNull NotificationType notificationType) {
+    Notification notification = new Notification(groupId, icon, title, subtitle, content, notificationType, null);
+    Notifications.Bus.notify(notification);
+  }
+}
