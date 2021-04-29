e430d038d6b8133315c1895bf19d006fc9e5fb66
Ivan Semenov <ivan.semenov@jetbrains.com>
2021-04-29 18:51:11 +0300

    [collab] move class
    GitOrigin-RevId: 4a8ca031411313c0bbf27a31d9cd9a6ad475e82b

--- platform/collaboration-tools/src/com/intellij/collaboration/util/ProgressIndicatorsProvider.kt
+++ platform/collaboration-tools/src/com/intellij/collaboration/util/ProgressIndicatorsProvider.kt
@@ -1,5 +1,5 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.plugins.github.util
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.collaboration.util
 
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.progress.EmptyProgressIndicator
