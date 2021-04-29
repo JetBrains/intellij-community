c9785b81a25d32c8b5890e07029fba9243197e08
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2021-04-29 11:21:32 +0200

    Update copyright
    GitOrigin-RevId: 4118ebbc95431c108f96c15a485ba96f097b9d3f

--- platform/core-api/src/com/intellij/openapi/fileTypes/ex/FakeFileType.java
+++ platform/core-api/src/com/intellij/openapi/fileTypes/ex/FakeFileType.java
@@ -1,8 +1,7 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.openapi.fileTypes.ex;
 
-import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
