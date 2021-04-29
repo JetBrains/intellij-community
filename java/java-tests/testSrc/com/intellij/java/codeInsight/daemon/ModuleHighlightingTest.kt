53535f83767bfb168ba6dc9ac2238f1056af8dec
Sebastiano Poggi <poggos@gmail.com>
2021-04-29 10:05:08 +0200

    PKGS Fix ModuleHighlightingTest again
    Thanks to Roman Shevchenko for the help
    
    GitOrigin-RevId: f602ef34b10576925910b7c87f9567e9ec0abd49

--- java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/ModuleHighlightingTest.kt
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/ModuleHighlightingTest.kt
@@ -224,8 +224,7 @@ class ModuleHighlightingTest : LightJava9ModulesCodeInsightFixtureTestCase() {
     fixes("module M { exports <caret>pkg.missing; }", arrayOf("CreateClassInPackageInModuleFix"))
     fixes("module M { exports <caret>pkg.m3; }", arrayOf())
     fixes("module M { uses pkg.m3.<caret>C3; }", arrayOf("AddModuleDependencyFix"))
-    fixes("pkg/main/C.java", "package pkg.main;\nimport <caret>pkg.m2.C2;",
-          arrayOf("AddRequiresDirectiveFix", "PackageSearchUnresolvedReferenceQuickFix"))
+    fixes("pkg/main/C.java", "package pkg.main;\nimport <caret>pkg.m2.C2;", arrayOf("AddRequiresDirectiveFix"))
 
     addFile("module-info.java", "module M { requires M6; }")
     addFile("pkg/main/Util.java", "package pkg.main;\nclass Util {\n static <T> void sink(T t) { }\n}")
@@ -234,8 +233,7 @@ class ModuleHighlightingTest : LightJava9ModulesCodeInsightFixtureTestCase() {
 
     addFile("module-info.java", "module M2 { }", M2)
     fixes("module M { requires M2; uses <caret>pkg.m2.C2; }", arrayOf("AddExportsDirectiveFix"))
-    fixes("pkg/main/C.java", "package pkg.main;\nimport <caret>pkg.m2.C2;",
-          arrayOf("AddExportsDirectiveFix", "PackageSearchUnresolvedReferenceQuickFix"))
+    fixes("pkg/main/C.java", "package pkg.main;\nimport <caret>pkg.m2.C2;", arrayOf("AddExportsDirectiveFix"))
 
     addFile("pkg/main/S.java", "package pkg.main;\npublic class S { }")
     fixes("module M { provides pkg.main.<caret>S with pkg.main.S; }", arrayOf("AddExportsDirectiveFix", "AddUsesDirectiveFix"))
@@ -495,8 +493,9 @@ class ModuleHighlightingTest : LightJava9ModulesCodeInsightFixtureTestCase() {
   private fun fixes(path: String, text: String, fixes: Array<String>) {
     myFixture.configureFromExistingVirtualFile(addFile(path, text))
     val available = myFixture.availableIntentions
-      .map { IntentionActionDelegate.unwrap(it)::class.java.simpleName }
-      .filter { it != "GutterIntentionAction" }
+      .map { IntentionActionDelegate.unwrap(it)::class.java }
+      .filter { it.name.startsWith("com.intellij.codeInsight.") }
+      .map { it.simpleName }
     assertThat(available).containsExactlyInAnyOrder(*fixes)
   }
   //</editor-fold>
