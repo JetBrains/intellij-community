0f25124e7f806199b6a0392e6be596b5656bde79
Yann Cébron <yann.cebron@jetbrains.com>
2021-05-03 19:52:18 +0200

    [devkit] idea-plugin@package: resolve in module dependencies if current XML has id/name (IDEA-268294)
    GitOrigin-RevId: f81b60ae7949b61747d453d9669938d5aa1b4852

--- plugins/devkit/devkit-java-tests/testSrc/org/jetbrains/idea/devkit/codeInsight/PluginXmlContentDependencyDescriptorTest.java
+++ plugins/devkit/devkit-java-tests/testSrc/org/jetbrains/idea/devkit/codeInsight/PluginXmlContentDependencyDescriptorTest.java
@@ -75,6 +75,10 @@ public class PluginXmlContentDependencyDescriptorTest extends JavaCodeInsightFix
     doHighlightingTest();
   }
 
+  public void testContentDescriptorNoPackageInMainHighlighting() {
+    doHighlightingTest();
+  }
+
   private void doHighlightingTest() {
     doHighlightingTest("anotherModule");
   }
