99f21d5417bcb230321a23663406316877e76171
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2021-05-04 19:32:08 +0300

    [project settings] extract module action: add option to move extracted classes to a separate root (IDEA-268316)
    GitOrigin-RevId: c379f4563a7e8f23ac3d26d128f09eb291b00ae8

--- java/idea-ui/src/com/intellij/ide/projectView/actions/ExtractModuleFromPackageAction.kt
+++ java/idea-ui/src/com/intellij/ide/projectView/actions/ExtractModuleFromPackageAction.kt
@@ -1,15 +1,18 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.projectView.actions
 
+import com.intellij.CommonBundle
 import com.intellij.analysis.AnalysisScope
 import com.intellij.ide.JavaUiBundle
-import com.intellij.ide.projectView.impl.ModuleNameValidator
+import com.intellij.ide.SaveAndSyncHandler
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.application.runWriteAction
+import com.intellij.openapi.diagnostic.ControlFlowException
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.module.ModuleTypeId
@@ -19,12 +22,14 @@ import com.intellij.openapi.roots.*
 import com.intellij.openapi.roots.libraries.Library
 import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.util.NlsSafe
-import com.intellij.openapi.util.TextRange
+import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.packageDependencies.ForwardDependenciesBuilder
 import com.intellij.psi.JavaDirectoryService
 import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiManager
+import com.intellij.refactoring.move.moveClassesOrPackages.MoveClassesOrPackagesUtil
 import org.jetbrains.annotations.TestOnly
+import java.nio.file.Path
 
 class ExtractModuleFromPackageAction : AnAction() {
   override fun actionPerformed(e: AnActionEvent) {
@@ -33,13 +38,11 @@ class ExtractModuleFromPackageAction : AnAction() {
     val module = ProjectFileIndex.getInstance(project).getModuleForFile(virtualFile) ?: return
     val directory = PsiManager.getInstance(module.project).findDirectory(virtualFile) ?: return
     val suggestedModuleName = "${module.name}.${directory.name}"
-    val initialSelection = TextRange(suggestedModuleName.length - directory.name.length, suggestedModuleName.length)
-    val moduleName = Messages.showInputDialog(project, JavaUiBundle.message("dialog.message.module.name"),
-                                              JavaUiBundle.message("dialog.title.extract.module.from.package"), null,
-                                              suggestedModuleName, ModuleNameValidator(project), initialSelection)
-    if (moduleName == null) return
+    val parentContentRoot = ModuleRootManager.getInstance(module).contentRoots.first()
+    val dialog = ExtractModuleFromPackageDialog(project, suggestedModuleName, Path.of(parentContentRoot.path, directory.name, "src").toString())
+    if (!dialog.showAndGet()) return
 
-    analyzeDependenciesAndCreateModule(directory, module, moduleName)
+    analyzeDependenciesAndCreateModule(directory, module, dialog.moduleName, dialog.targetSourceRootPath)
   }
 
   override fun update(e: AnActionEvent) {
@@ -50,7 +53,12 @@ class ExtractModuleFromPackageAction : AnAction() {
   }
 
   companion object {
-    private fun analyzeDependenciesAndCreateModule(directory: PsiDirectory, module: Module, moduleName: @NlsSafe String) {
+    private val LOG = logger<ExtractModuleFromPackageAction>()
+
+    private fun analyzeDependenciesAndCreateModule(directory: PsiDirectory,
+                                                   module: Module,
+                                                   moduleName: @NlsSafe String,
+                                                   targetSourceRootPath: String?) {
       val dependenciesBuilder = ForwardDependenciesBuilder(module.project, AnalysisScope(directory))
       object : Task.Backgroundable(module.project,
                                    JavaUiBundle.message("progress.title.extract.module.analyzing.dependencies", directory.name)) {
@@ -79,8 +87,16 @@ class ExtractModuleFromPackageAction : AnAction() {
             }
           }
           ApplicationManager.getApplication().invokeLater {
-            runWriteAction {
-              extractModule(directory, module, moduleName, usedModules, usedLibraries)
+            try {
+              runWriteAction {
+                extractModule(directory, module, moduleName, usedModules, usedLibraries, targetSourceRootPath)
+              }
+            }
+            catch (e: Throwable) {
+              if (e !is ControlFlowException) {
+                LOG.info(e)
+                Messages.showErrorDialog(project, JavaUiBundle.message("dialog.message.failed.to.extract.module", e), CommonBundle.getErrorTitle())
+              }
             }
           }
         }
@@ -88,10 +104,11 @@ class ExtractModuleFromPackageAction : AnAction() {
     }
 
     private fun extractModule(directory: PsiDirectory, module: Module, moduleName: @NlsSafe String,
-                              usedModules: Set<Module>, usedLibraries: Set<Library>) {
+                              usedModules: Set<Module>, usedLibraries: Set<Library>, targetSourceRootPath: String?) {
       val newModule = ModuleManager.getInstance(module.project).newModule(module.moduleNioFile.parent.resolve("$moduleName.iml"),
                                                                           ModuleTypeId.JAVA_MODULE)
       val packagePrefix = JavaDirectoryService.getInstance().getPackage(directory)?.qualifiedName ?: ""
+      val targetSourceRoot = targetSourceRootPath?.let { VfsUtil.createDirectories(it) }
       ModuleRootModificationUtil.updateModel(newModule) { model ->
         if (ModuleRootManager.getInstance(module).isSdkInherited) {
           model.inheritSdk()
@@ -99,7 +116,14 @@ class ExtractModuleFromPackageAction : AnAction() {
         else {
           model.sdk = ModuleRootManager.getInstance(module).sdk
         }
-        model.addContentEntry(directory.virtualFile).addSourceFolder(directory.virtualFile, false, packagePrefix)
+        if (targetSourceRoot != null) {
+          val parent = targetSourceRoot.parent
+          val contentRoot = if (parent in ModuleRootManager.getInstance(module).contentRoots) targetSourceRoot else parent
+          model.addContentEntry(contentRoot).addSourceFolder(targetSourceRoot, false)
+        }
+        else {
+          model.addContentEntry(directory.virtualFile).addSourceFolder(directory.virtualFile, false, packagePrefix)
+        }
         val moduleDependencies = JavaProjectDependenciesAnalyzer.removeDuplicatingDependencies(usedModules)
         moduleDependencies.forEach { model.addModuleOrderEntry(it) }
         val exportedLibraries = HashSet<Library>()
@@ -110,11 +134,16 @@ class ExtractModuleFromPackageAction : AnAction() {
         }
         (usedLibraries - exportedLibraries).forEach { model.addLibraryEntry(it) }
       }
+      if (targetSourceRoot != null) {
+        val targetDirectory = VfsUtil.createDirectoryIfMissing(targetSourceRoot, packagePrefix.replace('.', '/'))
+        MoveClassesOrPackagesUtil.moveDirectoryRecursively(directory, PsiManager.getInstance(module.project).findDirectory(targetDirectory.parent))
+      }
+      SaveAndSyncHandler.getInstance().scheduleProjectSave(module.project)
     }
 
     @TestOnly
     fun extractModuleFromDirectory(directory: PsiDirectory, module: Module, moduleName: @NlsSafe String) {
-      analyzeDependenciesAndCreateModule(directory, module, moduleName)
+      analyzeDependenciesAndCreateModule(directory, module, moduleName, null)
     }
   }
 }
