cf188d14da3cab2d99ac29a97b35640fc708c2c5
Roman Golyshev <roman.golyshev@jetbrains.com>
2021-05-05 16:49:51 +0300

    Modify `KotlinGrammarCheckingStrategy`
    - Treats close single-line comments as one text
    - Implement `getIgnoredRuleGroup`
    - Implement `isAbsorb` so it correctly handles tags like `@param`, `@author` etc.
    
    GitOrigin-RevId: a5d64bb6af5621eeb1d46a749c00bdd41725a763

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/grazie/KotlinGrammarCheckingStrategy.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/grazie/KotlinGrammarCheckingStrategy.kt
@@ -3,13 +3,18 @@ package org.jetbrains.kotlin.idea.grazie
 import com.intellij.grazie.grammar.strategy.BaseGrammarCheckingStrategy
 import com.intellij.grazie.grammar.strategy.GrammarCheckingStrategy.TextDomain
 import com.intellij.grazie.grammar.strategy.StrategyUtils
+import com.intellij.grazie.grammar.strategy.impl.RuleGroup
 import com.intellij.grazie.utils.LinkedSet
 import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiElement
+import com.intellij.psi.impl.source.tree.LeafPsiElement
 import com.intellij.psi.tree.IElementType
 import com.intellij.psi.util.elementType
 import org.jetbrains.kotlin.kdoc.lexer.KDocTokens
 import org.jetbrains.kotlin.kdoc.psi.api.KDoc
+import org.jetbrains.kotlin.kdoc.psi.impl.KDocSection
+import org.jetbrains.kotlin.kdoc.psi.impl.KDocTag
+import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.KtStringTemplateExpression
 
 internal class KotlinGrammarCheckingStrategy : BaseGrammarCheckingStrategy {
@@ -29,11 +34,34 @@ internal class KotlinGrammarCheckingStrategy : BaseGrammarCheckingStrategy {
         KDocTokens.LEADING_ASTERISK,
         KDocTokens.START,
         KDocTokens.END,
-        KDocTokens.TAG_NAME,
     )
 
     override fun isStealth(element: PsiElement): Boolean =
         element.elementType in stealthTokens
+
+    /**
+     * Checks that [PsiElement] is a direct child of a [KDocTag] section.
+     *
+     * Checking with `is KtDocTag` does not work, because [KDocSection] is a subclass of [KDocTag] too, and we don't want it.
+     */
+    private val PsiElement.isInDocTag: Boolean
+        get() = parent?.let { it::class } == KDocTag::class
+
+    private val PsiElement.isDocText: Boolean
+        get() = this is LeafPsiElement && elementType == KDocTokens.TEXT
+
+    /**
+     * We want to drop everything except the regular text from the tags.
+     */
+    override fun isAbsorb(element: PsiElement): Boolean =
+        element.isInDocTag && !element.isDocText
+
+    override fun getIgnoredRuleGroup(root: PsiElement, child: PsiElement): RuleGroup? =
+        when {
+            root is KtStringTemplateExpression -> RuleGroup.LITERALS
+            child.isInDocTag -> RuleGroup.CASING + RuleGroup.PUNCTUATION
+            else -> null
+        }
 
     /**
      * For multiline strings, we want to treat `'|'` as an indentation because it is commonly used with [String.trimMargin].
@@ -47,5 +75,15 @@ internal class KotlinGrammarCheckingStrategy : BaseGrammarCheckingStrategy {
             is KtStringTemplateExpression -> StrategyUtils.indentIndexes(text, stringIndentations)
             is PsiComment -> StrategyUtils.indentIndexes(text, commentIndentations)
             else -> super.getStealthyRanges(root, text)
+        }
+
+    private val PsiElement?.isSingleLineComment: Boolean
+        get() = this.elementType == KtTokens.EOL_COMMENT
+
+    override fun getRootsChain(root: PsiElement): List<PsiElement> =
+        if (root.isSingleLineComment) {
+            StrategyUtils.getNotSoDistantSimilarSiblings(this, root) { it.isSingleLineComment }.toList()
+        } else {
+            super.getRootsChain(root)
         }
 }
