5f6b68df520bc761758dfdee5321e7ed1d447b96
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-05-05 15:55:33 +0200

    make `CoroutineSuspenderTest#test resume paused coroutines` linear
    GitOrigin-RevId: 4d633c4facbbe417f2c73a97f9e17fe5d41182fe

--- platform/platform-tests/testSrc/com/intellij/openapi/progress/impl/CoroutineSuspenderTest.kt
+++ platform/platform-tests/testSrc/com/intellij/openapi/progress/impl/CoroutineSuspenderTest.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.progress.impl
 
 import com.intellij.openapi.progress.checkCanceled
@@ -31,24 +31,23 @@ class CoroutineSuspenderTest : LightPlatformTestCase() {
   fun `test resume paused coroutines`(): Unit = runBlocking {
     val count = 10
     val started = Semaphore(count)
-    val stop = Semaphore(1)
+    val paused = Semaphore(1)
     val suspender = coroutineSuspender()
     val result = async(Dispatchers.Default + suspender) {
       (1..count).map {
         async { // coroutine context (including CoroutineSuspender) is inherited
+          checkCanceled() // won't suspend
           started.up()
-          while (!stop.waitFor(1)) {
-            checkCanceled()
-            yield()
-          }
+          assertTrue(paused.waitFor(1000))
+          checkCanceled() // should suspend here
           it
         }
       }.awaitAll().sum()
     }
     assertTrue(started.waitFor(1000))
     suspender.pause()
+    paused.up()
     delay(10) // letBackgroundThreadsSuspend
-    stop.up()
     val children = result.children.toList()
     assertSize(count, children)
     assertFalse(children.any { it.isCompleted })
