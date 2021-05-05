97c785c0de18c994118d7c36bc2d7837ca12fa85
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-05-05 14:31:13 +0200

    FIR IDE: reuse keyword completion handlers which do not require resovle
    Original commit: 36068d0b94a671fa9ac4ab073c11d76f3d3942fa
    
    GitOrigin-RevId: c0a7164c9a86bfc9ca4deab2309a7d38f7059c3e

--- plugins/kotlin/completion/src/org/jetbrains/kotlin/idea/completion/BasicCompletionSession.kt
+++ plugins/kotlin/completion/src/org/jetbrains/kotlin/idea/completion/BasicCompletionSession.kt
@@ -17,12 +17,10 @@ import com.intellij.psi.*
 import com.intellij.psi.impl.source.tree.LeafPsiElement
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.util.ProcessingContext
-import org.jetbrains.kotlin.config.LanguageVersionSettings
 import org.jetbrains.kotlin.descriptors.*
 import org.jetbrains.kotlin.idea.analysis.analyzeInContext
 import org.jetbrains.kotlin.idea.caches.resolve.util.resolveToDescriptor
 import org.jetbrains.kotlin.idea.codeInsight.ReferenceVariantsHelper
-import org.jetbrains.kotlin.idea.completion.handlers.createKeywordConstructLookupElement
 import org.jetbrains.kotlin.idea.completion.smart.ExpectedInfoMatch
 import org.jetbrains.kotlin.idea.completion.smart.SMART_COMPLETION_ITEM_PRIORITY_KEY
 import org.jetbrains.kotlin.idea.completion.smart.SmartCompletion
@@ -604,11 +602,18 @@ class BasicCompletionSession(
                 isJvmModule
             )
 
-            val isUseSiteAnnotationTarget = position.prevLeaf()?.node?.elementType == KtTokens.AT
             keywordCompletion.complete(expression ?: position, resultSet.prefixMatcher, isJvmModule) { lookupElement ->
-                when (val keyword = lookupElement.lookupString) {
-                    in keywordsToSkip -> return@complete
+                val keyword = lookupElement.lookupString
+                if (keyword in keywordsToSkip) return@complete
+                
+                val completionKeywordHandler = DefaultCompletionKeywordHandlers.defaultHandlers.getHandlerForKeyword(keyword)
+                if (completionKeywordHandler != null) {
+                    val lookups = completionKeywordHandler.createLookups(parameters, expression, lookupElement, project)
+                    collector.addElements(lookups)
+                    return@complete
+                }
 
+                when (keyword) {
                     // if "this" is parsed correctly in the current context - insert it and all this@xxx items
                     "this" -> {
                         if (expression != null) {
@@ -632,12 +637,6 @@ class BasicCompletionSession(
                         }
                     }
 
-                    "break", "continue" -> {
-                        if (expression != null) {
-                            collector.addElements(breakOrContinueExpressionItems(expression, keyword))
-                        }
-                    }
-
                     "override" -> {
                         collector.addElement(lookupElement)
 
@@ -648,41 +647,6 @@ class BasicCompletionSession(
                         if (callTypeAndReceiver !is CallTypeAndReceiver.CALLABLE_REFERENCE) { // otherwise it should be handled by KeywordValues
                             collector.addElement(lookupElement)
                         }
-                    }
-
-                    "get" -> {
-                        collector.addElement(lookupElement)
-
-                        if (!isUseSiteAnnotationTarget) {
-                            collector.addElement(createKeywordConstructLookupElement(project, keyword, "val v:Int get()=caret"))
-                            collector.addElement(
-                                createKeywordConstructLookupElement(
-                                    project,
-                                    keyword,
-                                    "val v:Int get(){caret}",
-                                    trimSpacesAroundCaret = true
-                                )
-                            )
-                        }
-                    }
-
-                    "set" -> {
-                        collector.addElement(lookupElement)
-
-                        if (!isUseSiteAnnotationTarget) {
-                            collector.addElement(createKeywordConstructLookupElement(project, keyword, "var v:Int set(value)=caret"))
-                            collector.addElement(
-                                createKeywordConstructLookupElement(
-                                    project,
-                                    keyword,
-                                    "var v:Int set(value){caret}",
-                                    trimSpacesAroundCaret = true
-                                )
-                            )
-                        }
-                    }
-
-                    "contract" -> {
                     }
 
                     else -> collector.addElement(lookupElement)
