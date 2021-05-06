16caa8fbdc2506a4e031fa9b512bca1230d4273d
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2021-05-06 12:41:01 +0300

    EA-261834 more information in exception
    GitOrigin-RevId: 830e7e53c227cc90722193cb4b944e654e2f7c35

--- platform/platform-impl/src/com/intellij/execution/wsl/WSLDistribution.java
+++ platform/platform-impl/src/com/intellij/execution/wsl/WSLDistribution.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.wsl;
 
 import com.google.common.net.InetAddresses;
@@ -467,7 +467,8 @@ public class WSLDistribution {
 
       String distName = windowsPath.substring(0, index);
       if (!distName.equalsIgnoreCase(myDescriptor.getMsId())) {
-        throw new IllegalArgumentException("Trying to get WSL path from a different WSL distribution");
+        throw new IllegalArgumentException(
+          "Trying to get WSL path from a different WSL distribution: in path: " + distName + "; mine is: " + myDescriptor.getMsId());
       }
       return FileUtil.toSystemIndependentName(windowsPath.substring(index));
     }
