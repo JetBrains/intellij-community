79dfedae3f8aa59fe9cb28edab8358391ca5ea87
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2021-05-06 14:08:38 +0200

    [FIR IDE] Add OnAir resolve tests
    Original commit: 0569f810cf05decf83ba871afa73b8012c9a8682
    
    GitOrigin-RevId: f873bb6ab42e74828a46d362e890c6a64e59923d

--- plugins/kotlin/generators/test/org/jetbrains/kotlin/testGenerator/GenerateTests.kt
+++ plugins/kotlin/generators/test/org/jetbrains/kotlin/testGenerator/GenerateTests.kt
@@ -1065,6 +1065,9 @@ private fun assembleWorkspace(): TWorkspace = workspace {
         testClass<AbstractFirSealedInheritorsTest> {
             model("resolveSealed", pattern = DIRECTORY, isRecursive = false)
         }
+        testClass<AbstractFirOnAirResolveTest> {
+            model("onAirResolve")
+        }
         testClass<AbstractFirLazyDeclarationResolveTest> {
             model("lazyResolve")
         }
