7d41ad6fc2024744f9465e026ae14c5922eeb6d0
Dmitry Jemerov <yole@jetbrains.com>
2021-05-06 13:09:52 +0200

    Use a better API for removing ellipsis suffix
    GitOrigin-RevId: fbdf4bc767dcfc2814cd8364c93a54ab1c68ba43

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/MoveChangesToAnotherListAction.kt
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/actions/MoveChangesToAnotherListAction.kt
@@ -6,6 +6,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.MessageType
+import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vcs.*
 import com.intellij.openapi.vcs.changes.*
 import com.intellij.openapi.vcs.changes.ui.ChangeListChooser
@@ -136,7 +137,7 @@ class MoveChangesToAnotherListAction : AbstractChangeListAction() {
         .ifEmpty { nonAffectedLists }
       val defaultSelection = guessPreferredList(preferredList)
 
-      val chooser = ChangeListChooser(project, suggestedLists, defaultSelection, title.removeSuffix("..."), null)
+      val chooser = ChangeListChooser(project, suggestedLists, defaultSelection, StringUtil.removeEllipsisSuffix(title), null)
       chooser.show()
       return chooser.selectedList
     }
