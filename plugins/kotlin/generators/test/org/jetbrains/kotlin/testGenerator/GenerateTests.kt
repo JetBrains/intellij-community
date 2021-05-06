17417893af8c93026d66109bf1f5b85da59dddd2
Tianyu Geng <tgeng@google.com>
2021-05-06 09:43:04 -0700

    FIR IDE: add quickfix to change function return type
    Original commit: 71c5c9f6c52f815f27a01cbe967b647b327ea47e
    
    GitOrigin-RevId: e9ecee110ef1556710906b1f11e074dec1970333

--- plugins/kotlin/generators/test/org/jetbrains/kotlin/testGenerator/GenerateTests.kt
+++ plugins/kotlin/generators/test/org/jetbrains/kotlin/testGenerator/GenerateTests.kt
@@ -1168,6 +1168,8 @@ private fun assembleWorkspace(): TWorkspace = workspace {
             model("quickfix/variables/changeMutability", pattern = pattern, isRecursive = false)
             model("quickfix/when", pattern = pattern)
             model("quickfix/wrapWithSafeLetCall", pattern = pattern)
+            model("quickfix/typeMismatch/componentFunctionReturnTypeMismatch", pattern = pattern, filenameStartsLowerCase = true)
+            model("quickfix/typeMismatch/typeMismatchOnReturnedExpression", pattern = pattern, filenameStartsLowerCase = true)
         }
 
         testClass<AbstractHighLevelQuickFixMultiFileTest> {
