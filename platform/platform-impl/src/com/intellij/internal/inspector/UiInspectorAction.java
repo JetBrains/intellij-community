e04256a833e62196bcdfe2cfa51981a10d5a8b2f
Konstantin Bulenkov <kb@jetbrains.com>
2021-05-07 11:20:08 +0200

    make it possible to click on some elements of the editor: like toolbars
    GitOrigin-RevId: c4e03ed3db002cff3a15ff09aab1f2393520485a

--- platform/platform-impl/src/com/intellij/internal/inspector/UiInspectorAction.java
+++ platform/platform-impl/src/com/intellij/internal/inspector/UiInspectorAction.java
@@ -26,6 +26,7 @@ import com.intellij.openapi.actionSystem.impl.ActionMenuItem;
 import com.intellij.openapi.actionSystem.impl.ActionToolbarImpl;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.ex.EditorGutterComponentEx;
+import com.intellij.openapi.editor.impl.EditorComponentImpl;
 import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.keymap.Keymap;
 import com.intellij.openapi.keymap.KeymapManagerListener;
@@ -169,9 +170,12 @@ public class UiInspectorAction extends DumbAwareAction implements LightEditCompa
   @Override
   public List<AnAction> promote(@NotNull List<? extends AnAction> actions,
                                 @NotNull DataContext context) {
+
     ArrayList<AnAction> sorted = new ArrayList<>(actions);
-    sorted.remove(this);
-    sorted.add(this);
+    if (context.getData(PlatformDataKeys.CONTEXT_COMPONENT) instanceof EditorComponentImpl) {
+      sorted.remove(this);
+      sorted.add(this);
+    }
     return sorted;
   }
 
