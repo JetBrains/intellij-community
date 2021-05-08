859b21ec8f030e2daba291b4b2ae6d68ef45bc42
Jinseong Jeon <jsjeon@google.com>
2021-05-07 23:34:25 -0700

    FIR/UAST: commonize KotlinUFile
    We can avoid explicit creation of UAnnotation and UImportStatement if we
    use languagePlugin as a UAST element factory.
    
    Original commit: e5f3091f2c13b7367eb1d988195f1b3b0c665e3a
    
    GitOrigin-RevId: 8cdac557adf82195083b289856edbecd944148f4

--- plugins/kotlin/uast/uast-kotlin/src/org/jetbrains/uast/kotlin/KotlinUastLanguagePlugin.kt
+++ plugins/kotlin/uast/uast-kotlin/src/org/jetbrains/uast/kotlin/KotlinUastLanguagePlugin.kt
@@ -586,7 +586,7 @@ object KotlinConverter {
                 is KtParameter -> convertParameter(original, givenParent, this).firstOrNull()
 
                 is KtFile -> convertKtFile(original, givenParent, this).firstOrNull()
-                is FakeFileForLightClass -> el<UFile> { KotlinUFile(original.navigationElement) }
+                is FakeFileForLightClass -> el<UFile> { KotlinUFile(original.navigationElement, kotlinUastPlugin) }
                 is KtAnnotationEntry -> el<UAnnotation>(build(::KotlinUAnnotation))
                 is KtCallExpression ->
                     if (expectedTypes.isAssignableFrom(KotlinUNestedAnnotation::class.java) && !expectedTypes.isAssignableFrom(UCallExpression::class.java)) {
@@ -691,7 +691,7 @@ object KotlinConverter {
         givenParent: UElement?,
         requiredTypes: Array<out Class<out UElement>>
     ): Sequence<UElement> = requiredTypes.accommodate(
-        alternative { KotlinUFile(element) },
+        alternative { KotlinUFile(element, kotlinUastPlugin) },
         alternative { element.findFacadeClass()?.let { KotlinUClass.create(it, givenParent) } }
     )
 
