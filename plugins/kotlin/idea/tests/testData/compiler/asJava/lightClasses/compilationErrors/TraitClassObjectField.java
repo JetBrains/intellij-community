90e6b93d7ccf812341c1e5cd508c8d9a4d306e53
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-05-10 18:49:32 +0200

    Sync Kotlin compiler testData
    See 'sync-kotlin-compiler-testdata.bash' script
    
    GitOrigin-RevId: fcb881e874d64c9aaca4e0840581917465b37a14

--- plugins/kotlin/idea/tests/testData/compiler/asJava/lightClasses/compilationErrors/TraitClassObjectField.java
+++ plugins/kotlin/idea/tests/testData/compiler/asJava/lightClasses/compilationErrors/TraitClassObjectField.java
@@ -8,10 +8,9 @@ public abstract interface TraitClassObjectField /* TraitClassObjectField*/ {
 
 public static final class Companion /* TraitClassObjectField.Companion*/ {
   @org.jetbrains.annotations.Nullable()
-  private static final java.lang.String y;
-
-  @org.jetbrains.annotations.Nullable()
   public static final java.lang.String x = "" /* initializer type: java.lang.String */ /* constant value  */;
+
+  private static final java.lang.String y;
 
   private  Companion();//  .ctor()
 
