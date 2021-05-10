7b90a2c7ef61b2b07d7a75b38264fe21997580c5
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-05-10 15:59:31 +0200

    Use 'TargetBackend' from Kotlin compiler in order to fix compilation problems
    GitOrigin-RevId: e5b036a4f2ff1ef8ac5fa999c3af8856fc78d461

--- plugins/kotlin/tests-common/test/org/jetbrains/kotlin/idea/codegen/CodegenTestCase.java
+++ plugins/kotlin/tests-common/test/org/jetbrains/kotlin/idea/codegen/CodegenTestCase.java
@@ -21,7 +21,7 @@ import org.jetbrains.kotlin.idea.test.Directives;
 import org.jetbrains.kotlin.idea.artifacts.KotlinArtifacts;
 import org.jetbrains.kotlin.idea.test.ConfigurationKind;
 import org.jetbrains.kotlin.idea.test.KotlinTestUtils;
-import org.jetbrains.kotlin.idea.test.TargetBackend;
+import org.jetbrains.kotlin.test.TargetBackend;
 import org.jetbrains.kotlin.test.TestJdkKind;
 import org.jetbrains.kotlin.psi.KtFile;
 import org.jetbrains.kotlin.test.*;
