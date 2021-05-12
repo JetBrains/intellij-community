a19b644686020a8a84f5edd7095eb7bdbff23c04
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-05-12 18:55:27 +0200

    use Kotlin `Semaphore` in `CoroutineSuspenderTest`
    JB semaphore is blocking, the test was failing on TC because all threads were blocked by semaphore,
    the test passed locally because number of threads is greater that number of tested coroutines.
    
    GitOrigin-RevId: c4715fe37bebd9bdba79deb3f1b54788c248f5fb

--- platform/platform-tests/testSrc/com/intellij/openapi/progress/impl/CoroutineSuspenderTest.kt
+++ platform/platform-tests/testSrc/com/intellij/openapi/progress/impl/CoroutineSuspenderTest.kt
@@ -4,25 +4,29 @@ package com.intellij.openapi.progress.impl
 import com.intellij.openapi.progress.checkCanceled
 import com.intellij.openapi.progress.coroutineSuspender
 import com.intellij.testFramework.LightPlatformTestCase
-import com.intellij.util.concurrency.Semaphore
+import com.intellij.util.ConcurrencyUtil
 import kotlinx.coroutines.*
+import kotlinx.coroutines.sync.Semaphore
 
 class CoroutineSuspenderTest : LightPlatformTestCase() {
 
   fun `test cancel paused coroutines`(): Unit = runBlocking {
     val count = 10
-    val started = Semaphore(count)
+    val started = Semaphore(count, count)
     val suspender = coroutineSuspender(false)
     val job = launch(Dispatchers.Default + suspender) {
       repeat(count) {
         launch {
-          started.up()
+          started.release()
           checkCanceled()
           fail("must not be called")
         }
       }
     }
-    assertTrue(started.waitFor(1000))
+    withTimeout(1000) {
+      started.acquire() // all coroutines are started
+    }
+    letBackgroundThreadsSuspend()
     withTimeout(1000) {
       job.cancelAndJoin()
     }
@@ -30,24 +34,28 @@ class CoroutineSuspenderTest : LightPlatformTestCase() {
 
   fun `test resume paused coroutines`(): Unit = runBlocking {
     val count = 10
-    val started = Semaphore(count)
-    val paused = Semaphore(1)
+    val started = Semaphore(count, count)
+    val paused = Semaphore(count, count)
     val suspender = coroutineSuspender()
     val result = async(Dispatchers.Default + suspender) {
       (1..count).map {
         async { // coroutine context (including CoroutineSuspender) is inherited
           checkCanceled() // won't suspend
-          started.up()
-          assertTrue(paused.waitFor(1000))
+          started.release()
+          paused.acquire()
           checkCanceled() // should suspend here
           it
         }
       }.awaitAll().sum()
     }
-    assertTrue(started.waitFor(1000))
-    suspender.pause()
-    paused.up()
-    delay(10) // letBackgroundThreadsSuspend
+    withTimeout(1000) {
+      started.acquire() // all coroutines are started
+    }
+    suspender.pause() // pause suspender before next checkCanceled
+    repeat(count) {
+      paused.release() // let coroutines pause in next checkCanceled
+    }
+    letBackgroundThreadsSuspend()
     val children = result.children.toList()
     assertSize(count, children)
     assertFalse(children.any { it.isCompleted })
@@ -56,4 +64,6 @@ class CoroutineSuspenderTest : LightPlatformTestCase() {
       assertEquals(55, result.await())
     }
   }
+
+  private suspend fun letBackgroundThreadsSuspend(): Unit = delay(ConcurrencyUtil.DEFAULT_TIMEOUT_MS)
 }
