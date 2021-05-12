5c4f82b6e362de9d904c28a195b1fba6e6beca74
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2021-05-12 19:46:44 +0300

    [test framework] use proper service provider to fix presentation of UserDataHolderBase in assertj messages
    assertj prints a warning if Representation is extended directly.
    
    GitOrigin-RevId: 04b73eb9e0fa8a371559c41c8d51ed47d33fa07d

--- platform/testFramework/extensions/src/com/intellij/testFramework/assertj/PatchedConfiguration.kt
+++ platform/testFramework/extensions/src/com/intellij/testFramework/assertj/PatchedConfiguration.kt
@@ -2,17 +2,27 @@
 package com.intellij.testFramework.assertj
 
 import com.intellij.openapi.util.UserDataHolderBase
+import org.assertj.core.configuration.Configuration
+import org.assertj.core.presentation.Representation
 import org.assertj.core.presentation.StandardRepresentation
 
 /**
  * This is a workaround for [#2192](https://github.com/assertj/assertj-core/issues/2192). Without it instances of classes inheriting from
  * `UserDataHolderBase` will be shown as `AtomicReference[...]`.
  */
-class PatchedStandardRepresentation : StandardRepresentation() {
-  override fun toStringOf(o: Any?): String? {
-    if (o is UserDataHolderBase) {
-      return o.toString()
+class PatchedConfiguration : Configuration() {
+  override fun representation(): Representation {
+    return object : StandardRepresentation() {
+      override fun toStringOf(o: Any?): String? {
+        if (o is UserDataHolderBase) {
+          return o.toString()
+        }
+        return super.toStringOf(o)
+      }
     }
-    return super.toStringOf(o)
+  }
+
+  override fun applyAndDisplay() {
+    apply()
   }
 }
