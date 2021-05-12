22ee50e9bfc16520f90e379a6339e3d93624f3f9
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2021-05-12 16:59:33 +0300

    [IFT] Remove possibility to close learning popups
    GitOrigin-RevId: 6d31da77e7dbda12407a4cbecd799c8e480c5d82

--- plugins/ide-features-trainer/src/training/dsl/impl/LessonExecutorUtil.kt
+++ plugins/ide-features-trainer/src/training/dsl/impl/LessonExecutorUtil.kt
@@ -3,7 +3,6 @@ package training.dsl.impl
 
 import com.intellij.openapi.application.invokeLater
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.ui.popup.Balloon
 import com.intellij.openapi.ui.popup.JBPopupFactory
 import com.intellij.openapi.ui.popup.JBPopupListener
@@ -11,13 +10,10 @@ import com.intellij.openapi.ui.popup.LightweightWindowEvent
 import com.intellij.ui.UIBundle
 import com.intellij.ui.awt.RelativePoint
 import com.intellij.util.Alarm
-import icons.FeaturesTrainerIcons
 import training.dsl.LearningBalloonConfig
 import training.dsl.TaskContext
 import training.dsl.TaskRuntimeContext
 import training.learn.ActionsRecorder
-import training.learn.LearnBundle
-import training.learn.lesson.LessonManager
 import training.ui.LearningUiHighlightingManager
 import training.ui.LessonMessagePane
 import training.ui.MessageFactory
@@ -56,7 +52,6 @@ internal object LessonExecutorUtil {
 
     val preferredSize = messagesPane.preferredSize
 
-    messagesPane.toolTipText = LearnBundle.message("learn.stop.hint")
     val balloonPanel = JPanel()
     balloonPanel.border = EmptyBorder(8, 8, 8, 8)
     balloonPanel.isOpaque = false
@@ -91,7 +86,6 @@ internal object LessonExecutorUtil {
       .setBorderColor(UISettings.instance.activeTaskBorder)
       .setHideOnCloseClick(false)
       .setDisposable(actionsRecorder)
-      .setCloseButtonEnabled(true)
       .createBalloon()
 
 
@@ -99,17 +93,8 @@ internal object LessonExecutorUtil {
       override fun onClosed(event: LightweightWindowEvent) {
         val checkStopLesson = {
           invokeLater {
-            if (actionsRecorder.disposed) return@invokeLater
-            val yesNo = Messages.showYesNoDialog(project, LearnBundle.message("learn.stop.lesson.question"), LearnBundle.message("learn.stop.lesson"), FeaturesTrainerIcons.Img.PluginIcon)
-            if (yesNo == Messages.YES) {
-              invokeLater {
-                LessonManager.instance.stopLesson()
-              }
-            }
-            else {
-              if (actionsRecorder.disposed) return@invokeLater
+            if (!actionsRecorder.disposed)
               showBalloonMessage(text, ui, balloonConfig, actionsRecorder, project)
-            }
           }
         }
         Alarm().addRequest(checkStopLesson, 500) // it is a hacky a little bit
