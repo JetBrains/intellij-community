4875d8acfd4e56aa35d72d3a42d6d128c4ad78e8
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2021-05-13 14:24:09 +0300

    IDEA-269232 JavaFXApplication template should not be available in modules where there is no JavaFX library
    GitOrigin-RevId: 81d4bce6de21984e43c5456efe12d9a77c5ca0ac

--- plugins/javaFX/src/org/jetbrains/plugins/javaFX/actions/JavaFxCreateFromTemplateHandler.java
+++ plugins/javaFX/src/org/jetbrains/plugins/javaFX/actions/JavaFxCreateFromTemplateHandler.java
@@ -3,12 +3,18 @@ package org.jetbrains.plugins.javaFX.actions;
 
 import com.intellij.ide.fileTemplates.FileTemplate;
 import com.intellij.ide.fileTemplates.JavaCreateFromTemplateHandler;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleUtilCore;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.search.GlobalSearchScope;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.javaFX.fxml.JavaFxCommonNames;
 
-public class JavaFxCreateFromTemplateHandler extends JavaCreateFromTemplateHandler {
+import static com.intellij.psi.search.GlobalSearchScope.moduleWithDependenciesAndLibrariesScope;
+
+final class JavaFxCreateFromTemplateHandler extends JavaCreateFromTemplateHandler {
   @Override
   public boolean handlesTemplate(@NotNull FileTemplate template) {
     return "JavaFXApplication".equals(template.getName());
@@ -18,10 +24,14 @@ public class JavaFxCreateFromTemplateHandler extends JavaCreateFromTemplateHandl
   public boolean canCreate(PsiDirectory @NotNull [] dirs) {
     if (dirs.length > 0) {
       Project project = dirs[0].getProject();
-      if (JavaPsiFacade.getInstance(project).findPackage("javafx") == null) {
-        return false;
-      }
+      Module module = ModuleUtilCore.findModuleForFile(dirs[0].getVirtualFile(), project);
+      return module != null && hasJavaFxDependency(module);
     }
     return super.canCreate(dirs);
+  }
+
+  private static boolean hasJavaFxDependency(Module module) {
+    GlobalSearchScope scope = moduleWithDependenciesAndLibrariesScope(module);
+    return JavaPsiFacade.getInstance(module.getProject()).findClass(JavaFxCommonNames.JAVAFX_APPLICATION_APPLICATION, scope) != null;
   }
 }
