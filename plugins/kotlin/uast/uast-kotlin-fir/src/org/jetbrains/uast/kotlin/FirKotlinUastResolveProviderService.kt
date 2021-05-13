b4f392ff1a56cc81e366389db7aab4d57437b1e8
Jinseong Jeon <jsjeon@google.com>
2021-05-13 15:20:37 -0700

    FIR/UAST: commonize UImportStatement
    Original commit: 15a46496753e96608f44c7ee2e061120cf3abcc3
    
    GitOrigin-RevId: 0719456ea7a872cdeca8b752034af495e21f8e8d

---
+++ plugins/kotlin/uast/uast-kotlin-fir/src/org/jetbrains/uast/kotlin/FirKotlinUastResolveProviderService.kt
@@ -0,0 +1,28 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package org.jetbrains.uast.kotlin
+
+import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.idea.frontend.api.analyse
+import org.jetbrains.kotlin.idea.references.mainReference
+import org.jetbrains.kotlin.psi.KtExpression
+import org.jetbrains.kotlin.psi.KtReferenceExpression
+import org.jetbrains.uast.UElement
+
+interface FirKotlinUastResolveProviderService : BaseKotlinUastResolveProviderService {
+    override fun convertParent(uElement: UElement): UElement? {
+        TODO("Not yet implemented")
+    }
+
+    override fun resolveToDeclaration(ktExpression: KtExpression): PsiElement? {
+        when (ktExpression) {
+            is KtReferenceExpression -> {
+                analyse(ktExpression) {
+                    return ktExpression.mainReference.resolve()
+                }
+            }
+            else ->
+                return null
+        }
+    }
+}
