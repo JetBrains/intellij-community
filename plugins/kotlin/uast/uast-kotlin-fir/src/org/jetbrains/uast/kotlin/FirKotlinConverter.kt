2f21e5861078c1a12e053ea6d4a5050d523c85b3
Jinseong Jeon <jsjeon@google.com>
2021-05-14 11:56:31 -0700

    FIR/UAST: commonize UnknownKotlinExpression
    Original commit: 741fdee59e123b092e080815e60d05230b8a2538
    
    GitOrigin-RevId: 6ba3d431c4fb7a0d06a95d1b4aee36d0323ef818

--- plugins/kotlin/uast/uast-kotlin-fir/src/org/jetbrains/uast/kotlin/FirKotlinConverter.kt
+++ plugins/kotlin/uast/uast-kotlin-fir/src/org/jetbrains/uast/kotlin/FirKotlinConverter.kt
@@ -209,7 +209,7 @@ internal object FirKotlinConverter {
 
         return with(requiredTypes) {
             when (expression) {
-                else -> expr<UExpression>(build(::FirUnknownKotlinExpression))
+                else -> expr<UExpression>(build(::UnknownKotlinExpression))
             }
         }
     }
