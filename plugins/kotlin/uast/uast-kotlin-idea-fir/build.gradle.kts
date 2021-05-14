79c98c3c1ada3428dc283ec4502ad35f2fdcb974
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-05-14 14:16:48 +0200

    Separate uast-idea modules into fir and fe10 implementations
    Original commit: 8b722cfc47ca57f7a124c9852fc77cbddb101fd2
    
    GitOrigin-RevId: f626e4861cf16f0536c500d93c8c3ef63811aaf4

---
+++ plugins/kotlin/uast/uast-kotlin-idea-fir/build.gradle.kts
@@ -0,0 +1,24 @@
+
+plugins {
+    kotlin("jvm")
+    id("jps-compatible")
+}
+
+dependencies {
+    compile(kotlinStdlib())
+    compile(project(":core:util.runtime"))
+    compile(project(":compiler:backend"))
+    compile(project(":compiler:frontend.java"))
+    compile(project(":idea:ide-common"))
+    compile(project(":idea:idea-core"))
+    compile(project(":plugins:uast-kotlin-idea-base"))
+    compile(project(":plugins:uast-kotlin-fir"))
+    compileOnly(intellijDep())
+    compileOnly(intellijPluginDep("java"))
+}
+
+sourceSets {
+    "main" { projectDefault() }
+    "test" {}
+}
+
