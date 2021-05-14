4b3fcd2a5cb39dc1ad4c65e01cca18824b08b05b
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2021-05-14 13:18:51 +0200

    Revert "[go] move GOIcons to com.goide"
    This reverts commit 2f8521d4bd972906488fb8ab35f20ac60e33f16a.
    
    GitOrigin-RevId: c702ab2af15523061bff00886f6225a4e0735d04

--- platform/build-scripts/icons/src/org/jetbrains/intellij/build/images/IntellijIconClassGeneratorConfig.kt
+++ platform/build-scripts/icons/src/org/jetbrains/intellij/build/images/IntellijIconClassGeneratorConfig.kt
@@ -25,8 +25,6 @@ class IntellijIconClassGeneratorConfig : IconsClasses() {
       "intellij.platform.icons" -> IntellijIconClassGeneratorModuleConfig(className = "AllIcons", packageName = "com.intellij.icons")
       "intellij.properties.psi" -> IntellijIconClassGeneratorModuleConfig(className = "PropertiesIcons", packageName = "com.intellij.lang.properties",
                                                                           iconDirectory = "icons")
-      // default name 'com.goide.GOIcons' clashes with existing 'com.goide.GoIcons'
-      "intellij.go" -> IntellijIconClassGeneratorModuleConfig(className = "GoGeneratedIcons")
       else -> super.getConfigForModule(moduleName)
     }
   }
