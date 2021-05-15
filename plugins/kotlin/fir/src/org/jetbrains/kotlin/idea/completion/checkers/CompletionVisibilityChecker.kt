a5665f342ec18b4f9167050869df75e048542e6f
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-05-15 17:57:55 +0200

    FIR IDE: move CompletionVisibilityChecker/ExtensionApplicabilityChecker to dedicated package
    Original commit: 8dda232ae3dee4413595dbd56bb9687896327e3d
    
    GitOrigin-RevId: 2461d8b5a4796271e0fc2398cfc545d54bcc007c

---
+++ plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/completion/checkers/CompletionVisibilityChecker.kt
@@ -0,0 +1,20 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package org.jetbrains.kotlin.idea.completion.checkers
+
+import org.jetbrains.kotlin.idea.frontend.api.KtAnalysisSession
+import org.jetbrains.kotlin.idea.frontend.api.symbols.KtCallableSymbol
+import org.jetbrains.kotlin.idea.frontend.api.symbols.KtClassifierSymbol
+import org.jetbrains.kotlin.idea.frontend.api.symbols.markers.KtSymbolWithVisibility
+
+internal fun interface CompletionVisibilityChecker {
+    fun KtAnalysisSession.isVisible(symbol: KtSymbolWithVisibility): Boolean
+
+    fun KtAnalysisSession.isVisible(symbol: KtCallableSymbol): Boolean {
+        return symbol !is KtSymbolWithVisibility || isVisible(symbol as KtSymbolWithVisibility)
+    }
+
+    fun KtAnalysisSession.isVisible(symbol: KtClassifierSymbol): Boolean {
+        return symbol !is KtSymbolWithVisibility || isVisible(symbol as KtSymbolWithVisibility)
+    }
+}
