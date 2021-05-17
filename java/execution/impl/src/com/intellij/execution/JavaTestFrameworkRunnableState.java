c23dfcbe97a4835be647a36b0053ce473c8c3ba4
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2021-05-17 18:23:40 +0300

    [platform] advance version where API is scheduled for removal for some APIs (IDEA-262168)
    These APIs become deprecated in 2021.1 so let's postpone their removal.
    
    GitOrigin-RevId: 12cc81f1dedac0c0676b4272c703b70ac5f5007a

--- java/execution/impl/src/com/intellij/execution/JavaTestFrameworkRunnableState.java
+++ java/execution/impl/src/com/intellij/execution/JavaTestFrameworkRunnableState.java
@@ -194,7 +194,7 @@ public abstract class JavaTestFrameworkRunnableState<T extends
    * @deprecated Use {@link #createSearchingForTestsTask(TargetEnvironment)} instead
    */
   @Deprecated
-  @ApiStatus.ScheduledForRemoval(inVersion = "2021.2")
+  @ApiStatus.ScheduledForRemoval(inVersion = "2021.3")
   public @Nullable SearchForTestsTask createSearchingForTestsTask() throws ExecutionException {
     return null;
   }
