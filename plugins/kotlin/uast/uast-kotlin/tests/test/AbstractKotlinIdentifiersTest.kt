8a0e6f9777f8014b249a90e3cf01981224e22afb
Jinseong Jeon <jsjeon@google.com>
2021-05-17 00:15:31 -0700

    UAST: utilize identifier test base
    Original commit: c37123603c152208e6c931b79fda06e4d54b40e0
    
    GitOrigin-RevId: a1ed43b53d6b38697008ac2726f0ee4fe4fb9bde

--- plugins/kotlin/uast/uast-kotlin/tests/test/AbstractKotlinIdentifiersTest.kt
+++ plugins/kotlin/uast/uast-kotlin/tests/test/AbstractKotlinIdentifiersTest.kt
@@ -1,15 +1,11 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.uast.test.kotlin
 
-import com.intellij.psi.PsiElement
-import org.jetbrains.uast.*
-import org.jetbrains.uast.test.common.UElementToParentMap
+import org.jetbrains.uast.UFile
 import org.jetbrains.uast.test.common.kotlin.IdentifiersTestBase
-import org.jetbrains.uast.test.common.visitUFileAndGetResult
+import org.jetbrains.uast.test.common.kotlin.asRefNames
 import com.intellij.testFramework.assertEqualsToFile
 import java.io.File
-import kotlin.test.assertNotNull
-
 
 abstract class AbstractKotlinIdentifiersTest : AbstractKotlinUastTest(), IdentifiersTestBase {
 
@@ -23,16 +19,3 @@ abstract class AbstractKotlinIdentifiersTest : AbstractKotlinUastTest(), Identif
         assertEqualsToFile("refNames", getTestFile(testName, "refNames.txt"), file.asRefNames())
     }
 }
-
-private fun refNameRetriever(psiElement: PsiElement): UElement? =
-    when (val uElement = psiElement.toUElementOfExpectedTypes(UCallExpression::class.java, UReferenceExpression::class.java)) {
-        is UReferenceExpression -> uElement.referenceNameElement
-        is UCallExpression -> uElement.classReference?.referenceNameElement
-        else -> null
-    }?.also {
-        assertNotNull(it.sourcePsi, "referenceNameElement should have physical source, origin = $psiElement")
-    }
-
-fun UFile.asRefNames() = object : UElementToParentMap(::refNameRetriever) {
-    override fun renderSource(element: PsiElement): String = element.javaClass.simpleName
-}.visitUFileAndGetResult(this)
