21cb97a4fdbcc0e4ff551a329e84907edc830cdb
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-05-18 14:35:44 +0200

    FIR IDE: fix completion of type arguments without closing >
    Original commit: 0739f6974fd2a9442a020c2157b99c171b9af05d
    
    GitOrigin-RevId: 020a36cb7dc1e4401b9dbb285b4b6be6f0d9c89c

---
+++ plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/completion/FirCompletionDummyIdentifierProviderService.kt
@@ -0,0 +1,24 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package org.jetbrains.kotlin.idea.completion
+
+import org.jetbrains.kotlin.idea.frontend.api.analyse
+import org.jetbrains.kotlin.idea.frontend.api.symbols.KtClassKind
+import org.jetbrains.kotlin.idea.frontend.api.symbols.KtClassOrObjectSymbol
+import org.jetbrains.kotlin.idea.frontend.api.symbols.KtFunctionSymbol
+import org.jetbrains.kotlin.idea.references.mainReference
+import org.jetbrains.kotlin.psi.KtNameReferenceExpression
+
+class FirCompletionDummyIdentifierProviderService : CompletionDummyIdentifierProviderService() {
+    override fun allTargetsAreFunctionsOrClasses(nameReferenceExpression: KtNameReferenceExpression): Boolean {
+        return true
+        // TODO fir cannot handle invalid code and handles listOf< as binary expression
+//        return analyse(nameReferenceExpression) {
+//            val reference = nameReferenceExpression.mainReference
+//            val targets = reference.resolveToSymbols()
+//            targets.isNotEmpty() && targets.all { target ->
+//                target is KtFunctionSymbol || target is KtClassOrObjectSymbol && target.classKind == KtClassKind.CLASS
+//            }
+//        }
+    }
+}
