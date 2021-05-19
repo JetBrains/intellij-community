254b741bd72c73e32ff117d44e68d8b4c03c0157
Ivan Semenov <ivan.semenov@jetbrains.com>
2021-05-19 14:13:25 +0300

    [github] extract graphql DTOs to a common module
    GitOrigin-RevId: b65693882cb1f8a797ca1566c375aee6383ecdc5

---
+++ platform/collaboration-tools/src/com/intellij/collaboration/api/dto/GraphQLCursorPageInfoDTO.kt
@@ -0,0 +1,5 @@
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.collaboration.api.dto
+
+class GraphQLCursorPageInfoDTO(val startCursor: String?, val hasPreviousPage: Boolean,
+                               val endCursor: String?, val hasNextPage: Boolean)
