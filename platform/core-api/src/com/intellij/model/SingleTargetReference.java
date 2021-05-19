0ed73873717510fc8318b600f2a4e0766e79716e
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-05-19 19:49:54 +0200

    remove unneeded SingleResultReference
    GitOrigin-RevId: bf9bde8ded81475573ac5811aface8e3ecbd7165

--- platform/core-api/src/com/intellij/model/SingleTargetReference.java
+++ platform/core-api/src/com/intellij/model/SingleTargetReference.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.model;
 
 import org.jetbrains.annotations.NotNull;
@@ -9,8 +9,6 @@ import java.util.Collections;
 
 /**
  * Convenience base class for references that resolve to a single target without additional data.
- *
- * @see SingleResultReference
  */
 public abstract class SingleTargetReference implements SymbolReference {
 
