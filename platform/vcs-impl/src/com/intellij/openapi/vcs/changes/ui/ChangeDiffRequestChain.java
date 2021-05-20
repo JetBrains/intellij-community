6d26f0a2752c3684fa8cf991d6e47e75f0812334
Dmitry Zhuravlev <dmitry.zhuravlev@jetbrains.com>
2021-05-20 12:23:55 +0300

    diff: refactor: extract common "Go to Change" action
    GitOrigin-RevId: 858ce6bd59cc81b45fb61908f27080530645e444

--- platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/ChangeDiffRequestChain.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/changes/ui/ChangeDiffRequestChain.java
@@ -2,31 +2,26 @@
 package com.intellij.openapi.vcs.changes.ui;
 
 import com.intellij.diff.actions.impl.GoToChangePopupBuilder;
-import com.intellij.diff.chains.*;
+import com.intellij.diff.chains.AsyncDiffRequestChain;
+import com.intellij.diff.chains.DiffRequestChainBase;
+import com.intellij.diff.chains.DiffRequestProducer;
+import com.intellij.diff.chains.DiffRequestProducerException;
 import com.intellij.openapi.ListSelection;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Condition;
-import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vcs.FilePath;
 import com.intellij.openapi.vcs.FileStatus;
 import com.intellij.openapi.vcs.changes.actions.diff.ChangeGoToChangePopupAction;
 import com.intellij.util.Consumer;
 import com.intellij.util.ObjectUtils;
 import com.intellij.util.containers.ContainerUtil;
-import com.intellij.util.containers.MultiMap;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.DefaultTreeModel;
-import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
-
-import static com.intellij.util.containers.ContainerUtil.sorted;
-import static java.util.Comparator.comparing;
 
 public class ChangeDiffRequestChain extends DiffRequestChainBase implements GoToChangePopupBuilder.Chain {
   private static final Logger LOG = Logger.getInstance(ChangeDiffRequestChain.class);
@@ -50,44 +45,17 @@ public class ChangeDiffRequestChain extends DiffRequestChainBase implements GoTo
   @NotNull
   @Override
   public AnAction createGoToChangeAction(@NotNull Consumer<? super Integer> onSelected, int defaultSelection) {
-    return createGoToChangeAction(this, onSelected, defaultSelection);
+    return createGoToChangeAction(getRequests(), onSelected, defaultSelection);
   }
 
   /**
    * NB: {@code chain.getRequests()} MUST return instances of {@link Producer}
    */
   @NotNull
-  private static ChangeGoToChangePopupAction createGoToChangeAction(@NotNull DiffRequestChain chain,
+  private static ChangeGoToChangePopupAction createGoToChangeAction(@NotNull List<? extends DiffRequestProducer> producers,
                                                                     @NotNull Consumer<? super Integer> onSelected,
                                                                     int defaultSelection) {
-    return new ChangeGoToChangePopupAction() {
-      @Override
-      protected boolean canNavigate() {
-        return chain.getRequests().size() > 1;
-      }
-
-      @NotNull
-      @Override
-      protected DefaultTreeModel buildTreeModel(@NotNull Project project, @NotNull ChangesGroupingPolicyFactory grouping) {
-        MultiMap<ChangesBrowserNode.Tag, GenericChangesBrowserNode> groups = new MultiMap<>();
-        List<? extends DiffRequestProducer> producers = chain.getRequests();
-        for (int i = 0; i < producers.size(); i++) {
-          Producer producer = ObjectUtils.tryCast(producers.get(i), Producer.class);
-          if (producer == null) continue;
-
-          FilePath filePath = producer.getFilePath();
-          FileStatus fileStatus = producer.getFileStatus();
-          ChangesBrowserNode.Tag tag = producer.getTag();
-          groups.putValue(tag, new GenericChangesBrowserNode(filePath, fileStatus, i));
-        }
-
-        MyTreeModelBuilder builder = new MyTreeModelBuilder(project, grouping);
-        for (ChangesBrowserNode.Tag tag : groups.keySet()) {
-          builder.setGenericNodes(groups.get(tag), tag);
-        }
-        return builder.build();
-      }
-
+    return new ChangeGoToChangePopupAction(producers) {
       @Override
       protected void onSelected(@Nullable ChangesBrowserNode object) {
         GenericChangesBrowserNode node = ObjectUtils.tryCast(object, GenericChangesBrowserNode.class);
@@ -124,78 +92,6 @@ public class ChangeDiffRequestChain extends DiffRequestChainBase implements GoTo
     }
   }
 
-  private static class MyTreeModelBuilder extends TreeModelBuilder {
-    MyTreeModelBuilder(@NotNull Project project, @NotNull ChangesGroupingPolicyFactory grouping) {
-      super(project, grouping);
-    }
-
-    public void setGenericNodes(@NotNull Collection<GenericChangesBrowserNode> nodes, @Nullable ChangesBrowserNode.Tag tag) {
-      ChangesBrowserNode<?> parentNode = createTagNode(tag);
-
-      for (GenericChangesBrowserNode node : sorted(nodes, comparing(data -> data.myFilePath, PATH_COMPARATOR))) {
-        insertChangeNode(node.myFilePath, parentNode, node);
-      }
-    }
-  }
-
-  private static class GenericChangesBrowserNode extends ChangesBrowserNode<FilePath> implements Comparable<GenericChangesBrowserNode> {
-    @NotNull private final FilePath myFilePath;
-    @NotNull private final FileStatus myFileStatus;
-    private final int myIndex;
-
-    protected GenericChangesBrowserNode(@NotNull FilePath filePath, @NotNull FileStatus fileStatus, int index) {
-      super(filePath);
-      myFilePath = filePath;
-      myFileStatus = fileStatus;
-      myIndex = index;
-    }
-
-    private int getIndex() {
-      return myIndex;
-    }
-
-    @Override
-    protected boolean isFile() {
-      return !isDirectory();
-    }
-
-    @Override
-    protected boolean isDirectory() {
-      return myFilePath.isDirectory();
-    }
-
-    @Override
-    public void render(@NotNull ChangesBrowserNodeRenderer renderer, boolean selected, boolean expanded, boolean hasFocus) {
-      renderer.appendFileName(myFilePath.getVirtualFile(), myFilePath.getName(), myFileStatus.getColor());
-
-      if (renderer.isShowFlatten()) {
-        appendParentPath(renderer, myFilePath.getParentPath());
-      }
-
-      if (!renderer.isShowFlatten() && getFileCount() != 1 || getDirectoryCount() != 0) {
-        appendCount(renderer);
-      }
-
-      renderer.setIcon(myFilePath, myFilePath.isDirectory() || !isLeaf());
-    }
-
-    @Override
-    public String getTextPresentation() {
-      return myFilePath.getName();
-    }
-
-    @Override
-    public String toString() {
-      return FileUtil.toSystemDependentName(myFilePath.getPath());
-    }
-
-    @Override
-    public int compareTo(@NotNull GenericChangesBrowserNode o) {
-      return compareFilePaths(myFilePath, o.myFilePath);
-    }
-  }
-
-
   public static abstract class Async extends AsyncDiffRequestChain implements GoToChangePopupBuilder.Chain {
     @NotNull
     @Override
@@ -204,7 +100,7 @@ public class ChangeDiffRequestChain extends DiffRequestChainBase implements GoTo
     @Nullable
     @Override
     public AnAction createGoToChangeAction(@NotNull Consumer<? super Integer> onSelected, int defaultSelection) {
-      return ChangeDiffRequestChain.createGoToChangeAction(this, onSelected, defaultSelection);
+      return ChangeDiffRequestChain.createGoToChangeAction(getRequests(), onSelected, defaultSelection);
     }
   }
 }
