a18a5b513ff6838318a53e2a0a66fe1fbf1ff401
Mark Punzalan <markpun@google.com>
2021-05-21 21:32:17 +0000

    IDE: Remove duplicate quickfix message in resources.
    Original commit: 06adb405e1828c672fb4fd42d45dc60f25ea0cf7
    
    GitOrigin-RevId: 88db6e6b6dccecfd8728cebeb34cd397f3925dd0

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/ReplaceInfixOrOperatorCallFix.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/ReplaceInfixOrOperatorCallFix.kt
@@ -27,7 +27,7 @@ class ReplaceInfixOrOperatorCallFix(
     private val notNullNeeded: Boolean
 ) : KotlinQuickFixAction<KtExpression>(element) {
 
-    override fun getText() = KotlinBundle.message("replace.with.safe.call1")
+    override fun getText() = KotlinBundle.message("replace.with.safe.call")
 
     override fun getFamilyName() = text
 
