daff11db9897468d7b3f07288a5c089a4031c607
Mark Punzalan <markpun@google.com>
2021-05-22 07:13:57 +0000

    IDE: Move ReplaceInfixOrOperatorCallFix to idea-frontend-independent.
    Original commit: afeeec30915cfecd9125c8c0b9160bc61eed9cf8
    
    GitOrigin-RevId: b843d256f3f02db4b0847cda59e62c20d4ef6735

--- plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/quickfix/ReplaceInfixOrOperatorCallFix.kt
+++ plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/quickfix/ReplaceInfixOrOperatorCallFix.kt
@@ -2,31 +2,24 @@
 
 package org.jetbrains.kotlin.idea.quickfix
 
-import com.intellij.codeInsight.intention.IntentionAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiWhiteSpace
-import org.jetbrains.kotlin.diagnostics.Diagnostic
-import org.jetbrains.kotlin.diagnostics.Errors
 import org.jetbrains.kotlin.idea.KotlinBundle
-import org.jetbrains.kotlin.idea.caches.resolve.resolveToCall
-import org.jetbrains.kotlin.idea.intentions.canBeReplacedWithInvokeCall
 import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.getAssignmentByLHS
 import org.jetbrains.kotlin.psi.psiUtil.siblings
-import org.jetbrains.kotlin.resolve.calls.resolvedCallUtil.getImplicitReceiverValue
-import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
 import org.jetbrains.kotlin.types.expressions.OperatorConventions
 
 class ReplaceInfixOrOperatorCallFix(
     element: KtExpression,
     private val notNullNeeded: Boolean,
     private val binaryOperatorName: String = ""
-) : KotlinQuickFixAction<KtExpression>(element) {
+) : KotlinPsiOnlyQuickFixAction<KtExpression>(element) {
 
     override fun getText() = KotlinBundle.message("replace.with.safe.call")
 
@@ -96,46 +89,6 @@ class ReplaceInfixOrOperatorCallFix(
         }
         if (elvis.isNotEmpty()) {
             replacement?.moveCaretToEnd(editor, project)
-        }
-    }
-
-    override fun startInWriteAction() = true
-
-    companion object : KotlinSingleIntentionActionFactory() {
-        override fun createAction(diagnostic: Diagnostic): IntentionAction? {
-            val expression = diagnostic.psiElement
-            if (expression is KtArrayAccessExpression && diagnostic.factory != Errors.UNSAFE_IMPLICIT_INVOKE_CALL) {
-                if (expression.arrayExpression == null) return null
-                return ReplaceInfixOrOperatorCallFix(expression, expression.shouldHaveNotNullType())
-            }
-
-            when (val parent = expression.parent) {
-                is KtBinaryExpression -> {
-                    return when {
-                        parent.left == null || parent.right == null -> null
-                        parent.operationToken == KtTokens.EQ -> null
-                        parent.operationToken in OperatorConventions.COMPARISON_OPERATIONS -> null
-                        else -> {
-                            val binaryOperatorName = if (parent.operationToken == KtTokens.IDENTIFIER) {
-                                // Get name of infix function call
-                                parent.operationReference.text
-                            } else {
-                                parent.resolveToCall(BodyResolveMode.FULL)?.candidateDescriptor?.name?.asString()
-                            }
-                            binaryOperatorName?.let {
-                                ReplaceInfixOrOperatorCallFix(parent, parent.shouldHaveNotNullType(), binaryOperatorName)
-                            }
-                        }
-                    }
-                }
-                is KtCallExpression -> {
-                    if (parent.calleeExpression == null || parent.valueArgumentList == null) return null
-                    val resolvedCall = parent.resolveToCall(BodyResolveMode.FULL) ?: return null
-                    if (!resolvedCall.canBeReplacedWithInvokeCall() || resolvedCall.getImplicitReceiverValue() != null) return null
-                    return ReplaceInfixOrOperatorCallFix(parent, parent.shouldHaveNotNullType())
-                }
-                else -> return null
-            }
         }
     }
 }
