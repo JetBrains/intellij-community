4e4c6ddeefe695a00b86127cc41a6d5897c53d2b
Nikita Skvortsov <nikita.skvortsov@jetbrains.com>
2021-05-24 13:52:27 +0300

    [npw] gradle support stub
    GitOrigin-RevId: caa3370604d5218bda8075c6fdd4733051312978

--- java/idea-ui/src/com/intellij/ide/projectWizard/generators/JavaBuildSystemType.kt
+++ java/idea-ui/src/com/intellij/ide/projectWizard/generators/JavaBuildSystemType.kt
@@ -1,23 +1,13 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.projectWizard.generators
 
-import com.intellij.ide.LabelAndComponent
 import com.intellij.ide.wizard.BuildSystemType
 import com.intellij.openapi.extensions.ExtensionPointName
-import javax.swing.JLabel
 
 abstract class JavaBuildSystemType(override val name: String) : BuildSystemType<JavaSettings>(name) {
   companion object{
     var EP_NAME = ExtensionPointName<JavaBuildSystemType>("com.intellij.newProjectWizard.buildSystem.java")
   }
-}
-
-class GradleJavaBuildSystemType : JavaBuildSystemType("Gradle") {
-  override fun setupProject(settings: JavaSettings) {
-    TODO("Not yet implemented")
-  }
-
-  override val advancedSettings: List<LabelAndComponent> = listOf(LabelAndComponent(component = JLabel("Gradle Advanced Settings")))
 }
 
 class MavenJavaBuildSystemType : JavaBuildSystemType("Maven") {
