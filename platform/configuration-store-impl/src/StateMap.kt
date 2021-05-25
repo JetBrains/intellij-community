d08e94b0f4eb8117df224770cd7e8a9ca18c2b41
Dmitry Jemerov <yole@jetbrains.com>
2021-05-25 19:49:01 +0200

    Correctly unarchive plugin state (IDEA-262732)
    GitOrigin-RevId: e5d8ba6c92ee6458f2a244d726937e2689529ae9

--- platform/configuration-store-impl/src/StateMap.kt
+++ platform/configuration-store-impl/src/StateMap.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.configurationStore
 
 import com.intellij.openapi.application.ApplicationManager
@@ -149,9 +149,11 @@ class StateMap private constructor(private val names: Array<String>, private val
     val prev = states.getAndUpdate(index) { state ->
       when {
         archive && state is Element -> archiveState(state).toByteArray()
-        !archive && state is ByteArray -> unarchiveState(state)
         else -> state
       }
+    }
+    if (!archive && prev is ByteArray) {
+      return unarchiveState(prev)
     }
     return prev as? Element
   }
