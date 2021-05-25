47a738a4f84ec1c49ecb5df607a8f422ad2de87d
Jinseong Jeon <jsjeon@google.com>
2021-05-25 00:03:41 -0700

    FIR/UAST: commonize KotlinEvaluatableUElement
    Original commit: 6b5bddeed7c6546e42febca8637e3fb892451fd6
    
    GitOrigin-RevId: b25f29e33337e77408420244aef0b062e3226db9

--- plugins/kotlin/uast/uast-kotlin-base/src/org/jetbrains/uast/kotlin/BaseKotlinUastResolveProviderService.kt
+++ plugins/kotlin/uast/uast-kotlin-base/src/org/jetbrains/uast/kotlin/BaseKotlinUastResolveProviderService.kt
@@ -16,4 +16,6 @@ interface BaseKotlinUastResolveProviderService {
     fun resolveToDeclaration(ktExpression: KtExpression): PsiElement?
 
     fun getExpressionType(uExpression: UExpression): PsiType?
+
+    fun evaluate(uExpression: UExpression): Any?
 }
