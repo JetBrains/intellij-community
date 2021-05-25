e484ac934f3a6001ad626f742f9015695dddf5fe
Dmitry Jemerov <yole@jetbrains.com>
2021-05-25 18:24:59 +0200

    If the same disabled plugin is present in bundled and user plugin directory, use newer version as plugin version (IDEA-267878)
    (cherry picked from commit 34a89e9c7db2ec4d9758274f5a04b4535c43e991)
    
    GitOrigin-RevId: 7cf68571c0fe01db10e25647682b57eab9cd9dfc

--- platform/core-impl/src/com/intellij/ide/plugins/PluginLoadingResult.java
+++ platform/core-impl/src/com/intellij/ide/plugins/PluginLoadingResult.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.plugins;
 
 import com.intellij.core.CoreBundle;
@@ -92,7 +92,10 @@ final class PluginLoadingResult {
 
   void addIncompletePlugin(@NotNull IdeaPluginDescriptorImpl plugin, @Nullable PluginLoadingError error) {
     if (!idMap.containsKey(plugin.getPluginId())) {
-      incompletePlugins.put(plugin.getPluginId(), plugin);
+      IdeaPluginDescriptorImpl existingIncompletePlugin = incompletePlugins.get(plugin.getPluginId());
+      if (existingIncompletePlugin == null || VersionComparatorUtil.compare(plugin.getVersion(), existingIncompletePlugin.getVersion()) > 0) {
+        incompletePlugins.put(plugin.getPluginId(), plugin);
+      }
     }
     if (error != null) {
       pluginErrors.put(plugin.getPluginId(), error);
