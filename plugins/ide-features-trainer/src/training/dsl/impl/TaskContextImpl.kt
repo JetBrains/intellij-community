1f03890baf1922eb302f6b3c287db5c85c2cdb93
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2021-05-25 12:32:04 +0300

    [IFT] Introduce transparent restore API
    Also fix autorun test for Java extract method lesson
    
    GitOrigin-RevId: 10f6a766679a1da569b5c7142eaf84cbaa76fe9c

--- plugins/ide-features-trainer/src/training/dsl/impl/TaskContextImpl.kt
+++ plugins/ide-features-trainer/src/training/dsl/impl/TaskContextImpl.kt
@@ -34,6 +34,12 @@ internal class TaskContextImpl(private val lessonExecutor: LessonExecutor,
 
   override val taskId = TaskId(taskIndex)
 
+  override var transparentRestore: Boolean?
+    get() = data.transparentRestore
+    set(value) {
+      data.transparentRestore = value
+    }
+
   private val runtimeContext = TaskRuntimeContext(lessonExecutor,
                                                   recorder,
                                                   { lessonExecutor.applyRestore(this) },
@@ -48,9 +54,10 @@ internal class TaskContextImpl(private val lessonExecutor: LessonExecutor,
   }
 
   override fun restoreState(restoreId: TaskId?, delayMillis: Int, restoreRequired: TaskRuntimeContext.() -> Boolean) {
+    assert(lessonExecutor.currentTaskIndex == taskIndex)
     data.delayMillis = delayMillis
     val previous = data.shouldRestoreToTask
-    val actualId = restoreId ?: TaskId(taskIndex - 1)
+    val actualId = restoreId ?: TaskId(lessonExecutor.calculateRestoreIndex())
     data.shouldRestoreToTask = { previous?.let { it() } ?: if (restoreRequired(runtimeContext)) actualId else null }
   }
 
