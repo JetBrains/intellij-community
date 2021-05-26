5eecd7cf3b8657e1c5b48f0a8901858e035b54ab
Nicolay Mitropolsky <nicolay.mitropolsky@jetbrains.com>
2021-05-26 13:29:05 +0300

    FlakyTestLogger updates
    follow up the 1130715be3508cbaab8579dbbc8193e5713dfa9d
    
    GitOrigin-RevId: d9e4cd35e5dc70beb146b6f566a6300df4fc1ac7

--- platform/platform-api/src/com/intellij/testFramework/FlakyTestLogger.kt
+++ platform/platform-api/src/com/intellij/testFramework/FlakyTestLogger.kt
@@ -1,6 +1,7 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.testFramework
 
+import com.intellij.concurrency.ConcurrentCollectionFactory
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.util.Key
@@ -11,22 +12,31 @@ import java.io.StringWriter
 @Deprecated("this class is here only for debugging a flaky test, it will be deleted as soon problem will be resolved")
 class FlakyTestLogger(val enabled: Boolean) : Disposable {
 
-  private val lines = ArrayList<String>()
+
+  data class Entry(val time: Long, val text: String)
+
+  private val lists = ConcurrentCollectionFactory.createConcurrentIdentitySet<List<Entry>>()
+
+  private val lines = ThreadLocal.withInitial { ArrayList<Entry>().also { lists.add(it) } }
+
+  private fun add(str: String) {
+    lines.get().add(Entry(System.currentTimeMillis(), """${System.currentTimeMillis()} ${Thread.currentThread().name} $str"""))
+  }
 
   override fun dispose() {
-    lines.forEach(System.out::println)
+    lists.flatten().sortedBy { it.time }.forEach { println(it.text) }
   }
 
   fun append(line: String) {
     if (!enabled) return
-    lines.add(line)
+    add(line)
   }
 
   fun append(exception: Exception) {
     if (!enabled) return
     val stringWriter = StringWriter()
     exception.printStackTrace(PrintWriter(stringWriter))
-    lines.add(stringWriter.toString())
+    add(stringWriter.toString())
   }
 
   companion object {
