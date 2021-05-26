7416ab899b04550951469a5ded836f0cf5ad7cbe
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-05-26 17:00:56 +0200

    FIR IDE: refactor: separate lookup element factories into different files
    Original commit: aef071691fe792795bb5d6f4633539127bee7e99
    
    GitOrigin-RevId: ca981ec7a581db5fa02d9c63231c8d179fa3a78b

---
+++ plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/completion/lookups/factories/TypeParameterLookupElementFactory.kt
@@ -0,0 +1,16 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package org.jetbrains.kotlin.idea.completion.lookups.factories
+
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import org.jetbrains.kotlin.idea.completion.lookups.UniqueLookupObject
+import org.jetbrains.kotlin.idea.completion.lookups.withSymbolInfo
+import org.jetbrains.kotlin.idea.frontend.api.KtAnalysisSession
+import org.jetbrains.kotlin.idea.frontend.api.symbols.KtTypeParameterSymbol
+
+internal class TypeParameterLookupElementFactory {
+    fun KtAnalysisSession.createLookup(symbol: KtTypeParameterSymbol): LookupElementBuilder {
+        return LookupElementBuilder.create(UniqueLookupObject(), symbol.name.asString())
+            .let { withSymbolInfo(symbol, it) }
+    }
+}
