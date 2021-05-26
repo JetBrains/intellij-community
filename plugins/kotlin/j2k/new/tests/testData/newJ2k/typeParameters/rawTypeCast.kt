26aa1796520d9a4cc57d7757afe6ee47047cd4ca
Victor Petukhov <victor.petukhov@jetbrains.com>
2021-05-26 17:54:28 +0300

    Fix builder inference tests
    Original commit: bd7fb56a240a7b179be7aeaa95c8f9687e88d1e7
    
    GitOrigin-RevId: 93c1f6e7bdc533ca3d7f82db6f5e6014391f6a18

--- plugins/kotlin/j2k/new/tests/testData/newJ2k/typeParameters/rawTypeCast.kt
+++ plugins/kotlin/j2k/new/tests/testData/newJ2k/typeParameters/rawTypeCast.kt
@@ -1,10 +1,7 @@
 // ERROR: Type mismatch: inferred type is Any? but String? was expected
 // ERROR: Type mismatch: inferred type is (Any?..Any?) but String? was expected
-// ERROR: Type mismatch: inferred type is Any? but String? was expected
-// ERROR: Type mismatch: inferred type is (Any?..Any?) but String? was expected
-// ERROR: Type mismatch: inferred type is Any? but String? was expected
-// ERROR: Type mismatch: inferred type is (Any?..Any?) but String? was expected
 // ERROR: Type mismatch: inferred type is kotlin.collections.HashMap<Any?, Any?> /* = java.util.HashMap<Any?, Any?> */ but Map<String?, String?> was expected
+import java.util.HashMap
 import java.util.Properties
 
 internal object A {
