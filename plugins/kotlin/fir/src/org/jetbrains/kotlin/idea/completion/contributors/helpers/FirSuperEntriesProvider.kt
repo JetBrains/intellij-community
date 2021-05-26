4faabe167e749a0707be6397d8f7aba60de82b52
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-05-26 20:04:05 +0200

    FIR IDE: implement completion in where clause
    Original commit: 0f13359229ad904df1f60a72ff8ea51ad50f0120
    
    GitOrigin-RevId: e25132b24e8776c8268477b8e3dccc0451e4ffb3

--- plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/completion/contributors/helpers/FirSuperEntriesProvider.kt
+++ plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/completion/contributors/helpers/FirSuperEntriesProvider.kt
@@ -31,7 +31,7 @@ internal object SuperCallInsertionHandler : InsertHandler<LookupElement> {
         val lookupObject = item.`object` as SuperCallLookupObject
 
         replaceWithClassIdAndShorten(lookupObject, context)
-        context.addDotAndInvokeCompletion()
+        context.addSymbolAndInvokeCompletion(symbol = ".")
     }
 
     private fun replaceWithClassIdAndShorten(
