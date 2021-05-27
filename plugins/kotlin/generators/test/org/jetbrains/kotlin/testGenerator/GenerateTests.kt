eecf9bccfd7f4ecea6a06b64797c00c8ef3d53d8
Tianyu Geng <tgeng@google.com>
2021-05-27 15:26:27 -0700

    FIR IDE: Add ImportAllMembersIntention
    Original commit: 66d44f24717cde27a9e497c8d6866e44104cb89d
    
    GitOrigin-RevId: 56b403614eec3a6e919c363d4d9009aab099e56b

--- plugins/kotlin/generators/test/org/jetbrains/kotlin/testGenerator/GenerateTests.kt
+++ plugins/kotlin/generators/test/org/jetbrains/kotlin/testGenerator/GenerateTests.kt
@@ -1115,6 +1115,7 @@ private fun assembleWorkspace(): TWorkspace = workspace {
             val pattern = Patterns.forRegex("^([\\w\\-_]+)\\.(kt|kts)$")
             model("intentions/addPropertyAccessors", pattern = pattern)
             model("intentions/specifyTypeExplicitly", pattern = pattern)
+            model("intentions/importAllMembers", pattern = pattern)
         }
 
         testClass<AbstractFirShortenRefsTest> {
