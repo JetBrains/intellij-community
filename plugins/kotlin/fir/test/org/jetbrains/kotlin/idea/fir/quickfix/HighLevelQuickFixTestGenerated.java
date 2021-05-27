2b15b2336958d76ecfd2ca35430ff86593783a6c
Mark Punzalan <markpun@google.com>
2021-05-27 22:20:34 +0000

    FIR IDE: Enable AddExclExclCallFix for ASSIGNMENT_TYPE_MISMATCH and INITIALIZER_TYPE_MISMATCH.
    Original commit: c6427d57f13cf11f1fcf9ce186334945e12908ed
    
    GitOrigin-RevId: 07baa0367d3f243d2620eb3fb1943f7b7b0559a4

--- plugins/kotlin/fir/test/org/jetbrains/kotlin/idea/fir/quickfix/HighLevelQuickFixTestGenerated.java
+++ plugins/kotlin/fir/test/org/jetbrains/kotlin/idea/fir/quickfix/HighLevelQuickFixTestGenerated.java
@@ -345,6 +345,11 @@ public abstract class HighLevelQuickFixTestGenerated extends AbstractHighLevelQu
                 runTest("../idea/tests/testData/quickfix/addExclExclCall/typeMismatch/assignmentRValue.kt");
             }
 
+            @TestMetadata("initializer.kt")
+            public void testInitializer() throws Exception {
+                runTest("idea/testData/quickfix/addExclExclCall/typeMismatch/initializer.kt");
+            }
+
             @TestMetadata("memberAccessInExtension.kt")
             public void testMemberAccessInExtension() throws Exception {
                 runTest("../idea/tests/testData/quickfix/addExclExclCall/typeMismatch/memberAccessInExtension.kt");
@@ -353,6 +358,11 @@ public abstract class HighLevelQuickFixTestGenerated extends AbstractHighLevelQu
             @TestMetadata("memberAccessInExtensionAsAssignmentRValue.kt")
             public void testMemberAccessInExtensionAsAssignmentRValue() throws Exception {
                 runTest("../idea/tests/testData/quickfix/addExclExclCall/typeMismatch/memberAccessInExtensionAsAssignmentRValue.kt");
+            }
+
+            @TestMetadata("memberAccessInExtensionAsInitializer.kt")
+            public void testMemberAccessInExtensionAsInitializer() throws Exception {
+                runTest("idea/testData/quickfix/addExclExclCall/typeMismatch/memberAccessInExtensionAsInitializer.kt");
             }
 
             @TestMetadata("nullArgument.kt")
