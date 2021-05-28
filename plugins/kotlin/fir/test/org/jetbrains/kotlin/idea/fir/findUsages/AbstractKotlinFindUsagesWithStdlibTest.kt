c19b076607d29436a71946e55e41b124a0c340a1
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-05-28 17:38:37 +0200

    'idea-fir' move fir tests into subpackage 'org.jetbrains.kotlin.idea.fir'
    In intellij infrastructure we run tests by matching package pattern.
    So in order to be able run FIR tests we need to place them into unique package
    
    Original commit: 1ba6845b591269f7dde82912887fb614fcd861a1
    
    GitOrigin-RevId: dca10a049f00890d591665849596ceb544bc7685

---
+++ plugins/kotlin/fir/test/org/jetbrains/kotlin/idea/fir/findUsages/AbstractKotlinFindUsagesWithStdlibTest.kt
@@ -0,0 +1,21 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package org.jetbrains.kotlin.idea.fir.findUsages
+
+import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.findUsages.AbstractKotlinFindUsagesWithStdlibTest
+import org.jetbrains.kotlin.idea.invalidateCaches
+import org.jetbrains.kotlin.psi.KtFile
+
+abstract class AbstractKotlinFindUsagesWithStdlibFirTest : AbstractKotlinFindUsagesWithStdlibTest() {
+    override fun isFirPlugin(): Boolean = true
+
+    override fun tearDown() {
+        project.invalidateCaches(file as? KtFile)
+        super.tearDown()
+    }
+
+    override fun <T : PsiElement> doTest(path: String) = doTestWithFIRFlagsByPath(path) {
+        super.doTest<T>(path)
+    }
+}
