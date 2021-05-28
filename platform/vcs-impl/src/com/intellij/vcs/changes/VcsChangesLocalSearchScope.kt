538e4458e14ddd1d9e85fa86aa47691ce8e8f657
Dmitry Osinovskiy <dmitry.osinovsky@jetbrains.com>
2021-05-28 19:25:43 +0200

    VcsChangesLocalSearchScope: cleanup, deal with ReadAction
    (cherry picked from commit 26f41882f548b050db20088c98f4cb11722c76a3)
    
    IJ-CR-9580
    
    GitOrigin-RevId: c7a19044d81a6d8de26bbf14f3a1c86e632033ab

--- platform/vcs-impl/src/com/intellij/vcs/changes/VcsChangesLocalSearchScope.kt
+++ platform/vcs-impl/src/com/intellij/vcs/changes/VcsChangesLocalSearchScope.kt
@@ -21,47 +21,50 @@ class VcsChangesLocalSearchScope(private val myProject: Project,
                                  private val myGivenVirtualFiles: Array<VirtualFile>?,
                                  ignoreInjectedPsi: Boolean) : RangeBasedLocalSearchScope(displayName, ignoreInjectedPsi), WeighedItem {
 
-  private val rangeMap: HashMap<PsiFile, List<TextRange>> by lazy {
-    val changeListManager = ChangeListManager.getInstance(myProject)
-    val vcsFacade = VcsFacade.getInstance()
-    val psiManager = PsiManager.getInstance(myProject)
-    val psiFiles =
-      (myGivenVirtualFiles ?: changeListManager.affectedFiles.toTypedArray())
-        .mapNotNull { psiManager.findFile(it) }.toMutableList()
+  private val rangeMap: HashMap<VirtualFile, List<TextRange>> by lazy {
+    ReadAction.compute<HashMap<VirtualFile, List<TextRange>>, RuntimeException> {
+      val changeListManager = ChangeListManager.getInstance(myProject)
+      val vcsFacade = VcsFacade.getInstance()
+      val psiManager = PsiManager.getInstance(myProject)
+      val psiFiles =
+        (myGivenVirtualFiles ?: changeListManager.affectedFiles.toTypedArray())
+          .mapNotNull { psiManager.findFile(it) }.toMutableList()
 
-    val result = HashMap<PsiFile, List<TextRange>>()
-    for (file in psiFiles) {
-      val info = vcsFacade.getChangedRangesInfo(file)
-      if (info != null) {
-        val document = file.viewProvider.document
-        val ranges = ArrayList<TextRange>()
+      val result = HashMap<VirtualFile, List<TextRange>>()
+      for (file in psiFiles) {
+        val info = vcsFacade.getChangedRangesInfo(file)
+        if (info != null) {
+          val document = file.viewProvider.document
+          val ranges = ArrayList<TextRange>()
 
-        for (range in info.allChangedRanges) {
-          val startLine = document.getLineNumber(range.startOffset)
-          val endLine = document.getLineNumber(range.endOffset)
-          var startOffset = document.getLineStartOffset(startLine)
-          var endOffset = document.getLineEndOffset(endLine)
-          startOffset = CharArrayUtil.shiftForward(document.charsSequence, startOffset, endOffset, " /t")
-          if (startOffset == endOffset) continue
-          endOffset = CharArrayUtil.shiftBackward(document.charsSequence, startOffset, endOffset, " /t")
-          val lineRange = TextRange(startOffset, endOffset)
-          ranges.add(lineRange)
+          for (range in info.allChangedRanges) {
+            val startLine = document.getLineNumber(range.startOffset)
+            val endLine = document.getLineNumber(range.endOffset)
+            var startOffset = document.getLineStartOffset(startLine)
+            var endOffset = document.getLineEndOffset(endLine)
+            startOffset = CharArrayUtil.shiftForward(document.charsSequence, startOffset, endOffset, " /t")
+            if (startOffset == endOffset) continue
+            endOffset = CharArrayUtil.shiftBackward(document.charsSequence, startOffset, endOffset, " /t")
+            val lineRange = TextRange(startOffset, endOffset)
+            ranges.add(lineRange)
+          }
+
+          result[file.virtualFile] = ranges
         }
-
-        result[file] = ranges
       }
+      result
     }
-    result
   }
 
-  override fun getVirtualFiles(): Array<VirtualFile> = rangeMap.keys.map { it.virtualFile }.toTypedArray()
+  override fun getVirtualFiles(): Array<VirtualFile> = rangeMap.keys.toTypedArray()
 
   private val psiElementsLazy: Array<PsiElement> by lazy {
     ReadAction.compute<Array<PsiElement>, RuntimeException> {
       val elements: List<PsiElement> = ArrayList()
-
-      this.rangeMap.forEach { (psiFile, ranges) ->
-        ranges.forEach { CollectPsiElementsAtRange(psiFile, elements, it.startOffset, it.endOffset) }
+      val psiManager = PsiManager.getInstance(myProject)
+      this.rangeMap.forEach { (virtualFile, ranges) ->
+        val psiFile = psiManager.findFile(virtualFile)
+        ranges.forEach { collectPsiElementsAtRange(psiFile, elements, it.startOffset, it.endOffset) }
       }
 
       elements.toTypedArray()
@@ -70,53 +73,25 @@ class VcsChangesLocalSearchScope(private val myProject: Project,
 
   override fun getPsiElements(): Array<PsiElement> = psiElementsLazy
 
-  override fun equals(other: Any?): Boolean {
-    if (this === other) return true
-    if (other !is VcsChangesLocalSearchScope) return false
-    return myProject === other.myProject && myIgnoreInjectedPsi == other.myIgnoreInjectedPsi &&
-           Arrays.equals(myGivenVirtualFiles, other.myGivenVirtualFiles)
-  }
+  override fun equals(other: Any?): Boolean
+    = this === other ||
+      other is VcsChangesLocalSearchScope &&
+      myProject === other.myProject && myIgnoreInjectedPsi == other.myIgnoreInjectedPsi &&
+      Arrays.equals(myGivenVirtualFiles, other.myGivenVirtualFiles)
 
-  override fun toString(): String {
-    val builder = StringBuilder()
-    builder.append("Vcs Changes Local Search Scope")
-    if (myGivenVirtualFiles != null) {
-      builder.append("{")
-      var first = true
-      for (file in myGivenVirtualFiles) {
-        if (first) {
-          builder.append(",")
-          first = false
-        }
-        builder.append(file)
-      }
-    }
-    return builder.toString()
-  }
+  override fun toString()
+    = StringBuilder()
+      .append("Vcs Changes Local Search Scope")
+      .apply { myGivenVirtualFiles?.joinTo(this, prefix = "{", postfix = "}") { it.presentableName } }
+      .toString()
 
   override fun getWeight(): Int = 2
 
-  override fun calcHashCode(): Int {
-    var result = if (myIgnoreInjectedPsi) 27644437 else 1100101
-    if (myGivenVirtualFiles != null) {
-      for (file in myGivenVirtualFiles) {
-        result = result * 397 xor file.hashCode()
-      }
-    }
-    return result
-  }
+  override fun calcHashCode(): Int = Objects.hash(myIgnoreInjectedPsi, Arrays.hashCode(myGivenVirtualFiles))
 
-  override fun containsRange(file: PsiFile, range: TextRange): Boolean {
-    return getRanges(file).any { it.contains(range) }
-  }
+  override fun containsRange(file: PsiFile, range: TextRange): Boolean
+    = getRanges(file.virtualFile).any { it.contains(range) }
 
-  override fun getRanges(file: VirtualFile): Array<TextRange> {
-    return ReadAction.compute<Array<TextRange>, RuntimeException> {
-      PsiManager.getInstance(myProject).findFile(file)?.let { getRanges(it) } ?: TextRange.EMPTY_ARRAY
-    }
-  }
-
-  private fun getRanges(psiFile: PsiFile): Array<TextRange> {
-    return rangeMap[psiFile]?.toTypedArray() ?: TextRange.EMPTY_ARRAY
-  }
+  override fun getRanges(file: VirtualFile): Array<TextRange>
+    = rangeMap[file]?.toTypedArray() ?: TextRange.EMPTY_ARRAY
 }
