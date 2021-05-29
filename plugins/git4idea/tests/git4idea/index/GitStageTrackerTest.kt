48dcae7534021c41a645ab0094a6e4cd72a6b27b
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2021-05-30 00:45:08 +0300

    [git-index] turn off tests for another two and a half weeks
    GitOrigin-RevId: eb633b551e9a06b0c0908040f0f49e2f3c8abc82

--- plugins/git4idea/tests/git4idea/index/GitStageTrackerTest.kt
+++ plugins/git4idea/tests/git4idea/index/GitStageTrackerTest.kt
@@ -22,7 +22,7 @@ import java.util.*
 import java.util.concurrent.Future
 import java.util.concurrent.TimeUnit
 
-@Bombed(year = 2021, month = Calendar.MAY, day = 30, user = "Julia Beliaeva")
+@Bombed(year = 2021, month = Calendar.JUNE, day = 16, user = "Julia Beliaeva")
 class GitStageTrackerTest : GitSingleRepoTest() {
   private var _tracker: GitStageTracker? = null
 
