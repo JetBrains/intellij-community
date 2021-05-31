fc2662e89a9fcdc54b90a76d6a80753749b39213
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-05-31 18:02:03 +0700

    [kotlin] extract variable: don't suggest expression from parenthesis if caret after parenthesis
    ^KTIJ-11638 Fixed
    
    GitOrigin-RevId: 7189a814a553a84fe5a832ecc346df0e3d4d9d21

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/refactoring/elementSelectionUtils.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/refactoring/elementSelectionUtils.kt
@@ -83,17 +83,18 @@ fun selectElement(
 fun getSmartSelectSuggestions(
     file: PsiFile,
     offset: Int,
-    elementKind: CodeInsightUtils.ElementKind
+    elementKind: CodeInsightUtils.ElementKind,
+    isOriginalOffset: Boolean = true,
 ): List<KtElement> {
     if (offset < 0) return emptyList()
 
     var element: PsiElement? = file.findElementAt(offset) ?: return emptyList()
 
     if (element is PsiWhiteSpace
-        || element?.node?.elementType == KtTokens.RPAR
+        || isOriginalOffset && element?.node?.elementType == KtTokens.RPAR
         || element is PsiComment
         || element?.getStrictParentOfType<KDoc>() != null
-    ) return getSmartSelectSuggestions(file, offset - 1, elementKind)
+    ) return getSmartSelectSuggestions(file, offset - 1, elementKind, isOriginalOffset = false)
 
     val elements = ArrayList<KtElement>()
     while (element != null && !(element is KtBlockExpression && element.parent !is KtFunctionLiteral) &&
