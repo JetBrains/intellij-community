16b7cdee091e05a8e7a4d5435471a87ac0b140b0
Bas Leijdekkers <basleijdekkers@gmail.com>
2021-06-01 14:49:31 +0200

    Move two general intentions from JavaPlugin to LangExtensions
    GitOrigin-RevId: af760dffec5f6c0e0ec11d26121d6aae4132803b

--- platform/lang-impl/src/com/intellij/codeInsight/intention/impl/ConvertAbsolutePathToRelativeIntentionAction.java
+++ platform/lang-impl/src/com/intellij/codeInsight/intention/impl/ConvertAbsolutePathToRelativeIntentionAction.java
@@ -1,21 +1,7 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.intention.impl;
 
-import com.intellij.java.JavaBundle;
+import com.intellij.codeInsight.CodeInsightBundle;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
@@ -62,7 +48,7 @@ public class ConvertAbsolutePathToRelativeIntentionAction extends BaseIntentionA
   @Override
   @NotNull
   public String getFamilyName() {
-    return JavaBundle
+    return CodeInsightBundle
       .message("intention.family.convert.0.path.to.1", isConvertToRelative() ? "absolute" : "relative", isConvertToRelative() ? "relative" : "absolute");
   }
 
@@ -79,6 +65,6 @@ public class ConvertAbsolutePathToRelativeIntentionAction extends BaseIntentionA
   @NotNull
   @Override
   public String getText() {
-    return JavaBundle.message("intention.text.convert.path.to.0", isConvertToRelative() ? "relative" : "absolute");
+    return CodeInsightBundle.message("intention.text.convert.path.to.0", isConvertToRelative() ? "relative" : "absolute");
   }
 }
---
+++ platform/lang-impl/src/com/intellij/codeInsight/intention/impl/ConvertRelativePathToAbsoluteIntentionAction.java
@@ -0,0 +1,13 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.codeInsight.intention.impl;
+
+/**
+ * @author spleaner
+ */
+public class ConvertRelativePathToAbsoluteIntentionAction extends ConvertAbsolutePathToRelativeIntentionAction {
+
+  @Override
+  protected boolean isConvertToRelative() {
+    return false;
+  }
+}
