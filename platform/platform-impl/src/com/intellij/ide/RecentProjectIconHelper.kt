19a0ee4b1fa2da44aafdf53c9e6341f4f5a5a235
Konstantin Bulenkov <kb@jetbrains.com>
2021-06-01 23:09:46 +0200

    IDEA-270568 set project icons auto updatable
    GitOrigin-RevId: f79b98e289f2b7300b909357058e438c92a2468c

--- platform/platform-impl/src/com/intellij/ide/RecentProjectIconHelper.kt
+++ platform/platform-impl/src/com/intellij/ide/RecentProjectIconHelper.kt
@@ -83,7 +83,7 @@ internal class RecentProjectIconHelper {
     if (!RecentProjectsManagerBase.isFileSystemPath(path)) {
       return EmptyIcon.create(projectIconSize())
     }
-    return IconDeferrer.getInstance().defer(EmptyIcon.create(projectIconSize()), Pair(path, isDark)) {
+    return IconDeferrer.getInstance().deferAutoUpdatable(EmptyIcon.create(projectIconSize()), Pair(path, isDark)) {
       val calculateIcon = calculateIcon(path = it.first, isDark = it.second)
       if (calculateIcon == null && generateFromName) {
         val name = RecentProjectsManagerBase.instanceEx.getProjectName(path)
