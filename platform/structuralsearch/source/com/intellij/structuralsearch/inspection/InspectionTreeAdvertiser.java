2eeda65d5fd32eb93c4220ee35ca93962c966224
Louis Vignier <louis.vignier@jetbrains.com>
2021-06-01 15:25:39 +0200

    IJ-CR-9539 [structuralsearch] Extract createAndFocusInspection
    GitOrigin-RevId: 97ec2937701a41ad06d14d77895b46be89ffc290

--- platform/structuralsearch/source/com/intellij/structuralsearch/inspection/InspectionTreeAdvertiser.java
+++ platform/structuralsearch/source/com/intellij/structuralsearch/inspection/InspectionTreeAdvertiser.java
@@ -1,14 +1,12 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.structuralsearch.inspection;
 
-import com.intellij.codeInspection.ex.InspectionProfileModifiableModel;
 import com.intellij.openapi.project.Project;
 import com.intellij.profile.codeInspection.ui.EmptyInspectionTreeLinkProvider;
 import com.intellij.profile.codeInspection.ui.SingleInspectionProfilePanel;
 import com.intellij.structuralsearch.SSRBundle;
-import com.intellij.structuralsearch.plugin.ui.Configuration;
+import com.intellij.structuralsearch.inspection.StructuralSearchProfileActionProvider.AddInspectionAction;
 import com.intellij.structuralsearch.plugin.ui.SearchContext;
-import com.intellij.structuralsearch.plugin.ui.StructuralSearchDialog;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 
@@ -30,13 +28,7 @@ public class InspectionTreeAdvertiser extends EmptyInspectionTreeLinkProvider {
       @Override
       public void actionPerformed(ActionEvent e) {
         final Project project = panel.getProject();
-        final InspectionProfileModifiableModel profile = panel.getProfile();
-
-        final StructuralSearchDialog dialog = new StructuralSearchDialog(new SearchContext(project), false, true);
-        if (!dialog.showAndGet()) return;
-        final Configuration configuration = dialog.getConfiguration();
-        if (!StructuralSearchProfileActionProvider.createNewInspection(configuration, project, profile)) return;
-        panel.selectInspectionTool(configuration.getUuid().toString());
+        AddInspectionAction.createAndFocusInspection(panel, false, new SearchContext(project), project);
       }
     };
   }
