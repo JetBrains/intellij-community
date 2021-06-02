96bfde1344f5c0cd87e7bc1b12301bf735688ea8
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-06-02 14:08:51 +0200

    [kotlin] Download compiler testdata for tests which requires it
    GitOrigin-RevId: 1c77b781a6e21ab180131ea44bcf03f7b6e5f519

--- plugins/kotlin/common/test/org/jetbrains/kotlin/idea/artifacts/AdditionalKotlinArtifacts.kt
+++ plugins/kotlin/common/test/org/jetbrains/kotlin/idea/artifacts/AdditionalKotlinArtifacts.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.kotlin.idea.artifacts
 
+import com.intellij.openapi.application.PathManager
 import java.io.File
 
 object AdditionalKotlinArtifacts {
@@ -30,5 +31,25 @@ object AdditionalKotlinArtifacts {
 
     val androidExtensionsRuntime by lazy {
         KotlinArtifacts.instance.kotlincDirectory.resolve("lib/android-extensions-runtime.jar").also { check(it.exists()) }
+    }
+
+    @JvmStatic
+    val compilerTestDataDir = run {
+        val testDataJar = findLibrary(
+            RepoLocation.MAVEN_REPOSITORY,
+            "kotlinc_kotlin_compiler_testdata.xml",
+            "org.jetbrains.kotlin",
+            "kotlin-compiler-testdata-for-ide"
+        )
+        lazyUnpackJar(
+            testDataJar,
+            File(PathManager.getHomePath(), "out").resolve("kotlinc-testdata"),
+            "testData"
+        )
+    }
+
+    @JvmStatic
+    fun compilerTestData(compilerTestDataPath: String): String {
+        return compilerTestDataDir.resolve(compilerTestDataPath).canonicalPath
     }
 }
--- plugins/kotlin/generators/test/org/jetbrains/kotlin/testGenerator/model/TGroup.kt
+++ plugins/kotlin/generators/test/org/jetbrains/kotlin/testGenerator/model/TGroup.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.kotlin.testGenerator.model
 
+import org.jetbrains.kotlin.idea.artifacts.AdditionalKotlinArtifacts
 import org.jetbrains.kotlin.test.KotlinRoot
 import java.io.File
 
@@ -16,17 +17,24 @@ interface TGroup {
     val testDataRoot: File
 
     val suites: List<TSuite>
+
+    val isCompilerTestData: Boolean
 }
 
 interface MutableTGroup : TGroup {
     override val suites: MutableList<TSuite>
 }
 
-class TGroupImpl(override val modulePath: String, override val testSourcesPath: String, override val testDataPath: String) : MutableTGroup {
+class TGroupImpl(
+    override val modulePath: String,
+    override val testSourcesPath: String,
+    override val testDataPath: String,
+    override val isCompilerTestData: Boolean,
+) : MutableTGroup {
     override val kotlinRoot = KotlinRoot.DIR
     override val moduleRoot = File(kotlinRoot, modulePath)
     override val testSourcesRoot = File(moduleRoot, testSourcesPath)
-    override val testDataRoot = File(moduleRoot, testDataPath)
+    override val testDataRoot = if (testDataPath.startsWith("/")) File(testDataPath) else File(moduleRoot, testDataPath)
     override val suites = mutableListOf<TSuite>()
 }
 
@@ -36,5 +44,10 @@ fun MutableTWorkspace.testGroup(
     testDataPath: String = "testData",
     block: MutableTGroup.() -> Unit
 ) {
-    groups += TGroupImpl(modulePath, testSourcesPath, testDataPath).apply(block)
+    groups += TGroupImpl(
+        modulePath,
+        testSourcesPath,
+        testDataPath,
+        isCompilerTestData = testDataPath.startsWith(AdditionalKotlinArtifacts.compilerTestDataDir.canonicalPath)
+    ).apply(block)
 }
