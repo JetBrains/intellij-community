b652ca43d963732bc3e4095141c7a27b07024ea4
Alexandr Suhinin <alexandr.suhinin@jetbrains.com>
2021-06-02 08:26:57 +0300

    [extract method] add initial duplicates finder
    GitOrigin-RevId: 6d7073d87e50a6b4aec14a7f49a5ae5d19e0b16e

---
+++ java/java-impl/src/com/intellij/refactoring/extractMethod/newImpl/JavaDuplicatesFinder.kt
@@ -0,0 +1,120 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.refactoring.extractMethod.newImpl
+
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.*
+import com.intellij.psi.util.PsiTreeUtil
+
+data class ChangedExpression(val pattern: PsiElement, val candidate: PsiElement)
+
+class Duplicate(val pattern: List<PsiElement>, val candidate: List<PsiElement>, val changedExpressions: List<ChangedExpression>)
+
+class JavaDuplicatesFinder(pattern: List<PsiElement>) {
+
+  companion object {
+    fun textRangeOf(range: List<PsiElement>) = TextRange(range.first().textRange.startOffset, range.last().textRange.endOffset)
+  }
+
+  private val pattern: List<PsiElement> = copyOf(pattern).filterNot(::isNoise)
+
+  fun copyOf(elements: List<PsiElement>): List<PsiElement> {
+    val project = elements.firstOrNull()?.project ?: throw IllegalArgumentException()
+    val block = PsiElementFactory.getInstance(project).createCodeBlockFromText("{}", elements.first().context)
+    val first = block.addRange(elements.first(), elements.last())
+    val last = block.lastBodyElement!!
+    return PsiTreeUtil.getElementsOfRange(first, last)
+  }
+
+  fun findDuplicates(file: PsiFile): List<Duplicate> {
+    val duplicates = mutableListOf<Duplicate>()
+    val visitor = object: JavaRecursiveElementWalkingVisitor() {
+      override fun visitStatement(statement: PsiStatement) {
+        super.visitStatement(statement)
+        if (statement.textOffset in textRangeOf(pattern)) return
+        val siblings = siblingsOf(statement).take(pattern.size).toList()
+        val duplicate = createDuplicate(pattern, siblings)
+        if (duplicate != null) {
+          duplicates += duplicate
+        }
+      }
+    }
+    file.accept(visitor)
+
+    return duplicates
+  }
+
+  private fun isNoise(it: PsiElement) = it is PsiWhiteSpace || it is PsiComment || it is PsiEmptyStatement
+
+  private fun siblingsOf(element: PsiElement?): Sequence<PsiElement> {
+    return if (element != null) {
+      generateSequence(element) { it.nextSibling }.filterNot(::isNoise)
+    } else {
+      emptySequence()
+    }
+  }
+
+  private fun childrenOf(element: PsiElement?): List<PsiElement> {
+    return siblingsOf(element?.firstChild).toList()
+  }
+
+  fun createDuplicate(pattern: List<PsiElement>, candidate: List<PsiElement>): Duplicate? {
+    val changedExpressions = ArrayList<ChangedExpression>()
+    if ( !canBeDuplicate(pattern, candidate, changedExpressions)) return null
+    val patternDeclarations = pattern.flatMap { PsiTreeUtil.findChildrenOfType(it, PsiVariable::class.java) }
+    val candidateDeclarations = candidate.flatMap { PsiTreeUtil.findChildrenOfType(it, PsiVariable::class.java) }
+    val declarationsMapping = patternDeclarations.zip(candidateDeclarations).toMap()
+
+    val result = ArrayList<ChangedExpression>()
+    changedExpressions.forEach { (pattern, candidate) ->
+      if (pattern is PsiReferenceExpression && candidate is PsiReferenceExpression){
+        val variable = pattern.resolve()
+        val pairedVariable = declarationsMapping[variable]
+        if (pairedVariable == null) {
+          result += ChangedExpression(pattern, candidate)
+        } else if (pairedVariable != candidate.resolve()) {
+          return null
+        }
+      } else {
+        result += ChangedExpression(pattern, candidate)
+      }
+    }
+
+    return Duplicate(pattern, candidate, result)
+  }
+
+
+  fun canBeDuplicate(pattern: List<PsiElement>, candidate: List<PsiElement>, changedExpressions: MutableList<ChangedExpression>): Boolean {
+    if (candidate.size != pattern.size) return false
+    val notEqualElements = pattern.zip(candidate).filterNot { (pattern, candidate) ->
+      isEquivalent(pattern, candidate) && canBeDuplicate(childrenOf(pattern), childrenOf(candidate), changedExpressions)
+    }
+    changedExpressions += notEqualElements.map { (pattern, candidate) -> ChangedExpression(pattern, candidate) }
+    return notEqualElements.all { (pattern, candidate) -> canBeReplaced(pattern, candidate) }
+  }
+
+  private fun isEquivalent(pattern: PsiElement, candidate: PsiElement): Boolean {
+    return when {
+      pattern is PsiTypeElement && candidate is PsiTypeElement -> pattern.type.isAssignableFrom(candidate.type)
+      pattern is PsiReferenceExpression && candidate is PsiReferenceExpression -> pattern.resolve() == candidate.resolve()
+      pattern is PsiLiteralExpression && candidate is PsiLiteralExpression -> pattern.text == candidate.text
+      pattern.node?.elementType == candidate.node?.elementType -> true
+      else -> false
+    }
+  }
+
+  private fun canBeReplaced(pattern: PsiElement, candidate: PsiElement): Boolean {
+    return when {
+      pattern !is PsiExpression || candidate !is PsiExpression -> false
+      pattern.parent is PsiExpressionStatement -> false
+      pattern is PsiReferenceExpression && pattern.parent?.parent is PsiExpressionStatement -> false
+      pattern is PsiReferenceExpression && pattern.resolve() is PsiMethod && candidate is PsiReferenceExpression -> pattern.resolve() == candidate.resolve()
+      else -> pattern.type.isAssignableFrom(candidate.type)
+    }
+  }
+
+  private fun PsiType?.isAssignableFrom(candidate: PsiType?): Boolean {
+    if (this == null || candidate == null) return false
+    return isAssignableFrom(candidate)
+  }
+
+}
