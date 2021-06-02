d8d52e0b4a15714116c56176c2d554b0d943e09b
Peter Gromov <peter@jetbrains.com>
2021-06-02 14:39:39 +0200

    grazie: let there be text content in todo comments
    for Grazie Pro completion to work, and to avoid expensive TODO index access on each text content query
    
    GitOrigin-RevId: 728a91937978aac88de6c794c853165bfb32f6be

--- plugins/grazie/src/main/kotlin/com/intellij/grazie/text/CommentProblemFilter.kt
+++ plugins/grazie/src/main/kotlin/com/intellij/grazie/text/CommentProblemFilter.kt
@@ -1,13 +1,28 @@
 package com.intellij.grazie.text
 
+import com.intellij.grazie.text.TextContent.TextDomain.COMMENTS
+import com.intellij.grazie.text.TextContent.TextDomain.DOCUMENTATION
 import com.intellij.grazie.utils.Text
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiFile
+import com.intellij.psi.search.PsiTodoSearchHelper
 
 internal class CommentProblemFilter : ProblemFilter() {
 
   override fun shouldIgnore(problem: TextProblem): Boolean {
-    return problem.text.domain == TextContent.TextDomain.COMMENTS &&
-           Text.isSingleSentence(problem.text) &&
-           problem.fitsGroup(RuleGroup.UNDECORATED_SINGLE_SENTENCE)
+    val domain = problem.text.domain
+    if (domain == COMMENTS || domain == DOCUMENTATION) {
+      val parent = problem.text.commonParent
+      if (isTodoComment(parent.containingFile, parent.textRange)) {
+        return true
+      }
+    }
+
+    return domain == COMMENTS && Text.isSingleSentence(problem.text) && problem.fitsGroup(RuleGroup.UNDECORATED_SINGLE_SENTENCE)
   }
+
+  // the _todo_ word spoils the grammar of what follows
+  private fun isTodoComment(file: PsiFile, range: TextRange) =
+    PsiTodoSearchHelper.SERVICE.getInstance(file.project).findTodoItems(file, range.startOffset, range.endOffset).isNotEmpty()
 
 }
--- plugins/grazie/src/main/kotlin/com/intellij/grazie/text/TextExtractor.java
+++ plugins/grazie/src/main/kotlin/com/intellij/grazie/text/TextExtractor.java
@@ -93,18 +93,9 @@ public abstract class TextExtractor {
   }
 
   private static boolean isSuitable(TextContent content, PsiElement psi) {
-    TextRange range = psi.getTextRange();
-    PsiFile file = psi.getContainingFile();
-    return content.intersectsRange(range) &&
-           !hasIntersectingInjection(content, file) &&
-           !isSuppressionComment(content) &&
-           !isTodoComment(range, file);
-  }
-
-  // the _todo_ word spoils the grammar of what follows
-  private static boolean isTodoComment(TextRange range, PsiFile file) {
-    return PsiTodoSearchHelper.SERVICE.getInstance(file.getProject())
-             .findTodoItems(file, range.getStartOffset(), range.getEndOffset()).length != 0;
+    return content.intersectsRange(psi.getTextRange()) &&
+           !hasIntersectingInjection(content, psi.getContainingFile()) &&
+           !isSuppressionComment(content);
   }
 
   private static boolean isSuppressionComment(TextContent content) {
