14652598c60526a74b762aafafff23d192dc8086
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-06-03 14:14:52 +0200

    IDEA-261054 add `FilePresentationService` as an entry point for file colors
    `FileColorManager` is responsible only for user configuration.
    `FilePresentationService` uses `FileColorManager` under the hood.
    
    GitOrigin-RevId: ebc7510f2d2a653e30767ef10b597f38ac67d872

--- platform/lang-impl/src/com/intellij/ide/projectView/impl/ProjectViewTree.java
+++ platform/lang-impl/src/com/intellij/ide/projectView/impl/ProjectViewTree.java
@@ -1,5 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.projectView.impl;
 
 import com.intellij.ide.dnd.aware.DnDAwareTree;
@@ -8,11 +7,8 @@ import com.intellij.ide.util.treeView.AbstractTreeNode;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.vfs.newvfs.VfsPresentationUtil;
-import com.intellij.psi.PsiDirectory;
-import com.intellij.psi.PsiDirectoryContainer;
+import com.intellij.presentation.FilePresentationService;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.util.PsiUtilCore;
 import com.intellij.ui.DirtyUI;
 import com.intellij.ui.popup.HintUpdateSupply;
 import com.intellij.ui.tabs.FileColorManagerImpl;
@@ -116,7 +112,7 @@ public class ProjectViewTree extends DnDAwareTree {
       if (file != null) {
         Project project = node.getProject();
         if (project != null && !project.isDisposed()) {
-          Color color = VfsPresentationUtil.getFileBackgroundColor(project, file);
+          Color color = FilePresentationService.getInstance(project).getFileBackgroundColor(file);
           if (color != null) return color;
         }
       }
@@ -126,33 +122,10 @@ public class ProjectViewTree extends DnDAwareTree {
 
   @Nullable
   public static Color getColorForElement(@Nullable PsiElement psi) {
-    Color color = null;
-    if (psi != null) {
-      if (!psi.isValid()) return null;
-
-      Project project = psi.getProject();
-      final VirtualFile file = PsiUtilCore.getVirtualFile(psi);
-
-      if (file != null) {
-        color = VfsPresentationUtil.getFileBackgroundColor(project, file);
-      }
-      else if (psi instanceof PsiDirectory) {
-        color = VfsPresentationUtil.getFileBackgroundColor(project, ((PsiDirectory)psi).getVirtualFile());
-      }
-      else if (psi instanceof PsiDirectoryContainer) {
-        final PsiDirectory[] dirs = ((PsiDirectoryContainer)psi).getDirectories();
-        for (PsiDirectory dir : dirs) {
-          Color c = VfsPresentationUtil.getFileBackgroundColor(project, dir.getVirtualFile());
-          if (c != null && color == null) {
-            color = c;
-          }
-          else if (c != null) {
-            color = null;
-            break;
-          }
-        }
-      }
+    if (psi == null || !psi.isValid()) {
+      return null;
     }
-    return color;
+    Project project = psi.getProject();
+    return FilePresentationService.getInstance(project).getFileBackgroundColor(psi);
   }
 }
