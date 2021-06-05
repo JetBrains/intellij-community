b5f635bebab0d9d7c424bc89e7cb172012cf13ff
Jinseong Jeon <jsjeon@google.com>
2021-06-04 22:37:03 -0700

    FIR IDE/UAST: introduce UAST-specific version of analyse
    Original commit: 0db510ad8bfe89991a2447552e75c3b957899955
    
    GitOrigin-RevId: b1f6bae6435abdf7d6095ae25b4278d8304e169d

--- plugins/kotlin/uast/uast-kotlin-fir/src/org/jetbrains/uast/kotlin/FirKotlinUastResolveProviderService.kt
+++ plugins/kotlin/uast/uast-kotlin-fir/src/org/jetbrains/uast/kotlin/FirKotlinUastResolveProviderService.kt
@@ -4,8 +4,7 @@ package org.jetbrains.uast.kotlin
 
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiType
-import org.jetbrains.kotlin.idea.frontend.api.analyseWithCustomToken
-import org.jetbrains.kotlin.idea.frontend.api.tokens.AlwaysAccessibleValidityTokenFactory
+import org.jetbrains.kotlin.idea.frontend.api.analyseForUast
 import org.jetbrains.kotlin.idea.references.mainReference
 import org.jetbrains.kotlin.load.kotlin.TypeMappingMode
 import org.jetbrains.kotlin.psi.KtExpression
@@ -25,7 +24,7 @@ interface FirKotlinUastResolveProviderService : BaseKotlinUastResolveProviderSer
     override fun resolveToDeclaration(ktExpression: KtExpression): PsiElement? {
         when (ktExpression) {
             is KtReferenceExpression -> {
-                analyseWithCustomToken(ktExpression, AlwaysAccessibleValidityTokenFactory) {
+                analyseForUast(ktExpression) {
                     return ktExpression.mainReference.resolve()
                 }
             }
@@ -35,14 +34,14 @@ interface FirKotlinUastResolveProviderService : BaseKotlinUastResolveProviderSer
     }
 
     override fun resolveToType(ktTypeReference: KtTypeReference, source: UElement): PsiType? {
-        analyseWithCustomToken(ktTypeReference, AlwaysAccessibleValidityTokenFactory) {
+        analyseForUast(ktTypeReference) {
             return ktTypeReference.getPsiType(TypeMappingMode.DEFAULT_UAST)
         }
     }
 
     override fun getExpressionType(uExpression: UExpression): PsiType? {
         val ktExpression = uExpression.sourcePsi as? KtExpression ?: return null
-        analyseWithCustomToken(ktExpression, AlwaysAccessibleValidityTokenFactory) {
+        analyseForUast(ktExpression) {
             return ktExpression.getPsiType(TypeMappingMode.DEFAULT_UAST)
         }
     }
