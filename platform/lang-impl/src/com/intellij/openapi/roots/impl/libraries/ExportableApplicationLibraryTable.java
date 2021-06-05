ef46a735ca209fd569c0132654e176ea2d58031e
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2021-06-05 14:23:57 +0300

    Don't share paths defined in application libraries
    Paths are workstation-specific and shouldn't be shared. It is quite probable that a user sets up a similar environment on both computers, so sharing would be useful for them. But at the same time if the users doesn't have a common environment, then unexpected synchronization would be very annoying (even if it can be turned off).
    
    IDEA-152940
    
    GitOrigin-RevId: ccc6d511dae74c684355bb295df0bd76b4ecc0df

--- platform/lang-impl/src/com/intellij/openapi/roots/impl/libraries/ExportableApplicationLibraryTable.java
+++ platform/lang-impl/src/com/intellij/openapi/roots/impl/libraries/ExportableApplicationLibraryTable.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.roots.impl.libraries;
 
 import com.intellij.openapi.components.RoamingType;
@@ -8,7 +8,7 @@ import com.intellij.openapi.components.Storage;
 
 @State(
   name = "libraryTable",
-  storages = @Storage(value = "applicationLibraries.xml", roamingType = RoamingType.PER_OS)
+  storages = @Storage(value = "applicationLibraries.xml", roamingType = RoamingType.DISABLED)
 )
 public class ExportableApplicationLibraryTable extends ApplicationLibraryTable {
 }
