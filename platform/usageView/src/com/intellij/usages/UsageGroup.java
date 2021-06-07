d752baa06e9211c064f83688db7afd3c99b6b721
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-06-07 16:59:15 +0200

    `UsageGroup`: clean up
    GitOrigin-RevId: d593ea4d2ce4bde42acde80f0373c19c7ae7e692

--- platform/usageView/src/com/intellij/usages/UsageGroup.java
+++ platform/usageView/src/com/intellij/usages/UsageGroup.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.usages;
 
 import com.intellij.openapi.util.NlsContexts;
@@ -10,14 +10,14 @@ import org.jetbrains.annotations.Nullable;
 import javax.swing.*;
 
 public interface UsageGroup extends Comparable<UsageGroup>, Navigatable {
-  @Nullable
-  Icon getIcon(boolean isOpen);
+
+  @Nullable Icon getIcon(boolean isOpen);
 
   @NlsContexts.ListItem @NotNull String getText(@Nullable UsageView view);
 
-  @Nullable
-  FileStatus getFileStatus();
+  @Nullable FileStatus getFileStatus();
 
   boolean isValid();
+
   void update();
 }
