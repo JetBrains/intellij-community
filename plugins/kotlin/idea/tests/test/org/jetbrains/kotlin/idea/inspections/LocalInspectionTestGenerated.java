8a176c6deff4bc8295abea6222d8d9b5877cd25a
Toshiaki Kameyama <kmymtsak@gmail.com>
2021-06-09 00:09:30 +0900

    [kotlin] Range could be replaced with '.indices' call: don't report when collection is Map
    #KTIJ-11575 Fixed

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/inspections/LocalInspectionTestGenerated.java
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/inspections/LocalInspectionTestGenerated.java
@@ -11529,6 +11529,11 @@ public abstract class LocalInspectionTestGenerated extends AbstractLocalInspecti
             runTest("testData/inspectionsLocal/replaceManualRangeWithIndicesCalls/indexInLvalue.kt");
         }
 
+        @TestMetadata("map.kt")
+        public void testMap() throws Exception {
+            runTest("testData/inspectionsLocal/replaceManualRangeWithIndicesCalls/map.kt");
+        }
+
         @TestMetadata("multidimensionalArray.kt")
         public void testMultidimensionalArray() throws Exception {
             runTest("testData/inspectionsLocal/replaceManualRangeWithIndicesCalls/multidimensionalArray.kt");
