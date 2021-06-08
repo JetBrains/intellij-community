955f01f90a0600df05452806250b3a107e707c66
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-06-08 11:32:41 +0700

    [kotlin] compiler index: add advanced setting
    ^KTIJ-16709
    
    GitOrigin-RevId: 0509f9b9abe3a94ec58bc677571ab0ff709ac9a8

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/search/refIndex/KotlinCompilerReferenceIndexService.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/search/refIndex/KotlinCompilerReferenceIndexService.kt
@@ -1,9 +1,9 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.kotlin.idea.search.refIndex
 
-import com.intellij.compiler.CompilerReferenceService
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
+import com.intellij.openapi.options.advanced.AdvancedSettings
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.psi.search.GlobalSearchScope
@@ -15,6 +15,6 @@ class KotlinCompilerReferenceIndexService(project: Project) {
     companion object {
         operator fun get(project: Project): KotlinCompilerReferenceIndexService = project.service()
         fun getInstanceIfEnable(project: Project): KotlinCompilerReferenceIndexService? = if (isEnabled) get(project) else null
-        val isEnabled: Boolean get() = CompilerReferenceService.isEnabled()
+        val isEnabled: Boolean get() = AdvancedSettings.getBoolean("kotlin.compiler.ref.index")
     }
 }
