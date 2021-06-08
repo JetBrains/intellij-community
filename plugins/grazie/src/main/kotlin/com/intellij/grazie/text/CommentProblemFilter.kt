d5a21a55acc6bd5e294e96545a2d1078a4c79746
Peter Gromov <peter@jetbrains.com>
2021-06-08 10:36:30 +0200

    [grazie] ignore problems in todo comments only, don't affect neighbouring comments
    GitOrigin-RevId: 07d02c09b8ed879462c7c31a225d11f86745e53d

--- plugins/grazie/src/main/kotlin/com/intellij/grazie/text/CommentProblemFilter.kt
+++ plugins/grazie/src/main/kotlin/com/intellij/grazie/text/CommentProblemFilter.kt
@@ -3,26 +3,23 @@ package com.intellij.grazie.text
 import com.intellij.grazie.text.TextContent.TextDomain.COMMENTS
 import com.intellij.grazie.text.TextContent.TextDomain.DOCUMENTATION
 import com.intellij.grazie.utils.Text
-import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiFile
 import com.intellij.psi.search.PsiTodoSearchHelper
 
 internal class CommentProblemFilter : ProblemFilter() {
 
   override fun shouldIgnore(problem: TextProblem): Boolean {
-    val domain = problem.text.domain
-    if (domain == COMMENTS || domain == DOCUMENTATION) {
-      val parent = problem.text.commonParent
-      if (isTodoComment(parent.containingFile, parent.textRange)) {
-        return true
-      }
+    val text = problem.text
+    val domain = text.domain
+    if ((domain == COMMENTS || domain == DOCUMENTATION) && isTodoComment(text.commonParent.containingFile, text)) {
+      return true
     }
 
-    return domain == COMMENTS && Text.isSingleSentence(problem.text) && problem.fitsGroup(RuleGroup.UNDECORATED_SINGLE_SENTENCE)
+    return domain == COMMENTS && Text.isSingleSentence(text) && problem.fitsGroup(RuleGroup.UNDECORATED_SINGLE_SENTENCE)
   }
 
   // the _todo_ word spoils the grammar of what follows
-  private fun isTodoComment(file: PsiFile, range: TextRange) =
-    PsiTodoSearchHelper.SERVICE.getInstance(file.project).findTodoItems(file, range.startOffset, range.endOffset).isNotEmpty()
+  private fun isTodoComment(file: PsiFile, text: TextContent) =
+    PsiTodoSearchHelper.SERVICE.getInstance(file.project).findTodoItems(file).any { text.intersectsRange(it.textRange) }
 
 }
