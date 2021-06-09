45afa390d7d5e20aec8fa5e20c676558ed2fc1db
Elizaveta Shashkova <elizaveta.shashkova@jetbrains.com>
2021-06-09 12:40:16 +0300

    PY-40000 Revert finals
    (cherry picked from commit e6d4743f7f32cf01324594644e8d14e18d0f77a7)
    
    IJ-CR-9902
    
    GitOrigin-RevId: 281db18576513efc744da78927560de510c54dd7

--- python/src/com/jetbrains/python/console/PyConsoleProcessHandler.java
+++ python/src/com/jetbrains/python/console/PyConsoleProcessHandler.java
@@ -35,7 +35,7 @@ public class PyConsoleProcessHandler extends PythonProcessHandler {
   }
 
   @Override
-  public void coloredTextAvailable(@NotNull String text, @NotNull Key attributes) {
+  public void coloredTextAvailable(@NotNull final String text, @NotNull final Key attributes) {
     String string = PyConsoleUtil.processPrompts(myConsoleView, text);
     super.coloredTextAvailable(string, attributes);
   }
