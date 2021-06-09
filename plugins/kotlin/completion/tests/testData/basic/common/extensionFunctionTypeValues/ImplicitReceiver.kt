288dae7959f7f07e5fad1db6f614796c02e79b2e
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-06-09 14:35:19 +0200

    FIR IDE: implement completion for variables with functional types with receivers
    Original commit: 915c8b79962edc93f4cfb17c3241892530ca0026
    
    GitOrigin-RevId: 0f52039890a466c20569fae94253a24d4c961c76

--- plugins/kotlin/completion/tests/testData/basic/common/extensionFunctionTypeValues/ImplicitReceiver.kt
+++ plugins/kotlin/completion/tests/testData/basic/common/extensionFunctionTypeValues/ImplicitReceiver.kt
@@ -6,4 +6,3 @@ fun C.test(foo: C.() -> Unit) {
 }
 
 // EXIST: { lookupString: "foo", itemText: "foo", tailText: null, typeText: "C.() -> Unit", icon: "nodes/parameter.svg"}
-// ABSENT: { itemText: "foo", typeText: "Unit" }
