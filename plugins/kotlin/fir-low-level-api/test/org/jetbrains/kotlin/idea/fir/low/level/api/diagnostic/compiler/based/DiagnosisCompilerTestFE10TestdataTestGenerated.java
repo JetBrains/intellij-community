32c06f31558a7ad9a6740717c65166722dbdc471
Tianyu Geng <tgeng@google.com>
2021-06-09 15:25:16 -0700

    FIR: handle synthetic properties with unstable smartcast
    Synthetic properties from Java getter/setters need to be specially
    handled so that candidates from such symbols are marked with unstable.
    
    Original commit: 3c8693758b7641a98c903dadece333a3a18b95ad
    
    GitOrigin-RevId: 1ea159d7dcad585214d4a8e51a5a7cf931fa61dd

--- plugins/kotlin/fir-low-level-api/test/org/jetbrains/kotlin/idea/fir/low/level/api/diagnostic/compiler/based/DiagnosisCompilerTestFE10TestdataTestGenerated.java
+++ plugins/kotlin/fir-low-level-api/test/org/jetbrains/kotlin/idea/fir/low/level/api/diagnostic/compiler/based/DiagnosisCompilerTestFE10TestdataTestGenerated.java
@@ -22137,6 +22137,11 @@ public abstract class DiagnosisCompilerTestFE10TestdataTestGenerated extends Abs
                     runTest(compilerTestData("compiler/testData/diagnostics/tests/smartCasts/inference/stabilityOfSmartcastsAgainstGenericFunctions.kt"));
                 }
 
+                @TestMetadata("syntheticPropertyOnUnstableSmartcast.kt")
+                public void testSyntheticPropertyOnUnstableSmartcast() throws Exception {
+                    runTest("compiler/testData/diagnostics/tests/smartCasts/inference/syntheticPropertyOnUnstableSmartcast.kt");
+                }
+
                 @TestMetadata("unneededUnstableSmartcast.kt")
                 public void testUnneededUnstableSmartcast() throws Exception {
                     runTest("compiler/testData/diagnostics/tests/smartCasts/inference/unneededUnstableSmartcast.kt");
