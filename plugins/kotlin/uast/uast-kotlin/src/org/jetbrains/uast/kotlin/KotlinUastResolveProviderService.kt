9b1c373faeda6ad9a18b5861c813de7b07836f73
Jinseong Jeon <jsjeon@google.com>
2021-06-08 18:17:37 -0700

    FIR/UAST: move multi resolve utils/APIs to the base module/service
    Original commit: 77e8aed995fdc7b10c02a2442da3941d8631247b
    
    GitOrigin-RevId: b7f3b936f1bb60d903ad46af903c011a280829ee

--- plugins/kotlin/uast/uast-kotlin/src/org/jetbrains/uast/kotlin/KotlinUastResolveProviderService.kt
+++ plugins/kotlin/uast/uast-kotlin/src/org/jetbrains/uast/kotlin/KotlinUastResolveProviderService.kt
@@ -6,7 +6,6 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiType
 import org.jetbrains.kotlin.codegen.state.KotlinTypeMapper
 import org.jetbrains.kotlin.config.LanguageVersionSettings
-import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
 import org.jetbrains.kotlin.psi.KtElement
 import org.jetbrains.kotlin.psi.KtExpression
 import org.jetbrains.kotlin.psi.KtTypeReference
@@ -20,7 +19,6 @@ interface KotlinUastResolveProviderService : BaseKotlinUastResolveProviderServic
     fun getBindingContext(element: KtElement): BindingContext
     fun getTypeMapper(element: KtElement): KotlinTypeMapper?
     fun getLanguageVersionSettings(element: KtElement): LanguageVersionSettings
-    fun getReferenceVariants(ktElement: KtElement, nameHint: String): Sequence<DeclarationDescriptor>
 
     override val baseKotlinConverter: BaseKotlinConverter
         get() = KotlinConverter
