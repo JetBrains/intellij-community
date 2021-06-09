7e84048ed617cbe60c9f62dde27724453b356333
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-06-09 19:20:05 +0200

    FIR IDE: add ::class/::class.java to callable references completion
    Original commit: a5edbd531b597369cdc1357b599c5caa84656c7e
    
    GitOrigin-RevId: 5814581da4ca80b95ca894ade1cc9337e6be98ba

---
+++ plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/completion/contributors/FirClassReferenceCompletionContributor.kt
@@ -0,0 +1,21 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package org.jetbrains.kotlin.idea.completion.contributors
+
+import org.jetbrains.kotlin.idea.completion.context.FirBasicCompletionContext
+import org.jetbrains.kotlin.idea.completion.context.FirCallableReferencePositionContext
+import org.jetbrains.kotlin.idea.completion.createKeywordElement
+import org.jetbrains.kotlin.idea.frontend.api.KtAnalysisSession
+import org.jetbrains.kotlin.platform.jvm.isJvm
+
+internal class FirClassReferenceCompletionContributor(
+    basicContext: FirBasicCompletionContext
+) : FirCompletionContributorBase<FirCallableReferencePositionContext>(basicContext) {
+    override fun KtAnalysisSession.complete(positionContext: FirCallableReferencePositionContext) {
+        if (positionContext.explicitReceiver == null) return
+        sink.addElement(createKeywordElement("class"))
+        if (targetPlatform.isJvm()) {
+            sink.addElement(createKeywordElement("class", tail = ".java"))
+        }
+    }
+}
