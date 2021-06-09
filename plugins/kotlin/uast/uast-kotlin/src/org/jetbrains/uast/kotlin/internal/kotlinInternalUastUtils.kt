5234f9578c453ef58e66a90e14cdc5d482e845ed
Jinseong Jeon <jsjeon@google.com>
2021-06-08 18:17:37 -0700

    FIR/UAST: move multi resolve utils/APIs to the base module/service
    Original commit: 77e8aed995fdc7b10c02a2442da3941d8631247b
    
    GitOrigin-RevId: ff5e8d0ce6de08bbdabeac5689490449db442130

--- plugins/kotlin/uast/uast-kotlin/src/org/jetbrains/uast/kotlin/internal/kotlinInternalUastUtils.kt
+++ plugins/kotlin/uast/uast-kotlin/src/org/jetbrains/uast/kotlin/internal/kotlinInternalUastUtils.kt
@@ -353,7 +353,7 @@ internal fun resolveToDeclarationImpl(sourcePsi: KtExpression): PsiElement? =
                 ?.let { descriptor -> resolveToDeclarationImpl(sourcePsi, descriptor) }
     }
 
-internal fun resolveToDeclarationImpl(sourcePsi: KtExpression, declarationDescriptor: DeclarationDescriptor): PsiElement? {
+fun resolveToDeclarationImpl(sourcePsi: KtExpression, declarationDescriptor: DeclarationDescriptor): PsiElement? {
     declarationDescriptor.toSource()?.getMaybeLightElement(sourcePsi)?.let { return it }
 
     @Suppress("NAME_SHADOWING")
