ca00b477dcc01d4a5b7fa35daec30c7e6c70052d
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-06-09 14:35:19 +0200

    FIR IDE: implement completion for variables with functional types with receivers
    Original commit: 915c8b79962edc93f4cfb17c3241892530ca0026
    
    GitOrigin-RevId: 9cc486eb3e37577c8f93f79aed5cb44335e5da44

---
+++ plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/completion/lookups/CallableInsertionOptions.kt
@@ -0,0 +1,26 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package org.jetbrains.kotlin.idea.completion.lookups
+
+import org.jetbrains.kotlin.idea.frontend.api.KtAnalysisSession
+import org.jetbrains.kotlin.idea.frontend.api.symbols.KtCallableSymbol
+import org.jetbrains.kotlin.idea.frontend.api.symbols.KtFunctionSymbol
+
+internal data class CallableInsertionOptions(
+    val importingStrategy: ImportStrategy,
+    val insertionStrategy: CallableInsertionStrategy,
+) {
+    fun withImportingStrategy(newImportStrategy: ImportStrategy): CallableInsertionOptions =
+        copy(importingStrategy = newImportStrategy)
+
+    fun withInsertionStrategy(newInsertionStrategy: CallableInsertionStrategy): CallableInsertionOptions =
+        copy(insertionStrategy = newInsertionStrategy)
+}
+
+internal fun KtAnalysisSession.detectCallableOptions(symbol: KtCallableSymbol): CallableInsertionOptions = CallableInsertionOptions(
+    importingStrategy = detectImportStrategy(symbol),
+    insertionStrategy = when (symbol) {
+        is KtFunctionSymbol -> CallableInsertionStrategy.AsCall
+        else -> CallableInsertionStrategy.AsIdentifier
+    }
+)
