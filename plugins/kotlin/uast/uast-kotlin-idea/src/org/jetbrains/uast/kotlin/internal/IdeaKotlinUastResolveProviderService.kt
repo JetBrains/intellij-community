cce87bf76c4e6824368883e061c1ba3fa67b299b
Jinseong Jeon <jsjeon@google.com>
2021-06-08 18:17:37 -0700

    FIR/UAST: move multi resolve utils/APIs to the base module/service
    Original commit: 77e8aed995fdc7b10c02a2442da3941d8631247b
    
    GitOrigin-RevId: 86a0bb5cc3b0e021e712422a3be459cee7852c9c

--- plugins/kotlin/uast/uast-kotlin-idea/src/org/jetbrains/uast/kotlin/internal/IdeaKotlinUastResolveProviderService.kt
+++ plugins/kotlin/uast/uast-kotlin-idea/src/org/jetbrains/uast/kotlin/internal/IdeaKotlinUastResolveProviderService.kt
@@ -6,7 +6,6 @@ import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.codegen.ClassBuilderMode
 import org.jetbrains.kotlin.codegen.state.KotlinTypeMapper
 import org.jetbrains.kotlin.config.LanguageVersionSettings
-import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
 import org.jetbrains.kotlin.idea.caches.resolve.analyze
 import org.jetbrains.kotlin.idea.caches.resolve.getResolutionFacade
 import org.jetbrains.kotlin.idea.core.resolveCandidates
@@ -15,10 +14,11 @@ import org.jetbrains.kotlin.idea.util.actionUnderSafeAnalyzeBlock
 import org.jetbrains.kotlin.idea.util.safeAnalyzeNonSourceRootCode
 import org.jetbrains.kotlin.metadata.jvm.deserialization.JvmProtoBufUtil
 import org.jetbrains.kotlin.psi.KtElement
-import org.jetbrains.kotlin.resolve.BindingContext
+import org.jetbrains.kotlin.psi.KtExpression
 import org.jetbrains.kotlin.resolve.calls.callUtil.getCall
 import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
 import org.jetbrains.uast.kotlin.KotlinUastResolveProviderService
+import org.jetbrains.uast.kotlin.resolveToDeclarationImpl
 
 class IdeaKotlinUastResolveProviderService : KotlinUastResolveProviderService {
     override fun getBindingContext(element: KtElement) = element.analyze(BodyResolveMode.PARTIAL_WITH_CFA)
@@ -36,10 +36,12 @@ class IdeaKotlinUastResolveProviderService : KotlinUastResolveProviderService {
 
     override fun getLanguageVersionSettings(element: KtElement): LanguageVersionSettings = element.languageVersionSettings
 
-    override fun getReferenceVariants(ktElement: KtElement, nameHint: String): Sequence<DeclarationDescriptor> {
-        val resolutionFacade = ktElement.getResolutionFacade()
-        val bindingContext = ktElement.safeAnalyzeNonSourceRootCode(resolutionFacade)
-        val call = ktElement.getCall(bindingContext) ?: return emptySequence()
-        return call.resolveCandidates(bindingContext, resolutionFacade).map { it.candidateDescriptor }.asSequence()
+    override fun getReferenceVariants(ktExpression: KtExpression, nameHint: String): Sequence<PsiElement> {
+        val resolutionFacade = ktExpression.getResolutionFacade()
+        val bindingContext = ktExpression.safeAnalyzeNonSourceRootCode(resolutionFacade)
+        val call = ktExpression.getCall(bindingContext) ?: return emptySequence()
+        return call.resolveCandidates(bindingContext, resolutionFacade)
+            .mapNotNull { resolveToDeclarationImpl(ktExpression, it.candidateDescriptor) }
+            .asSequence()
     }
 }
