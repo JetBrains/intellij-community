3067105f4df3e22bed5629a0f48ca160fb82de71
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2021-06-10 18:02:18 +0300

    git: Add "gpg signature" icons (IDEA-269495)
    GitOrigin-RevId: 4ae904e38a332757747eb575bc1a93c7a0380925

---
+++ plugins/git4idea/gen/git4idea/GitIcons.java
@@ -0,0 +1,19 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package git4idea;
+
+import com.intellij.ui.IconManager;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+/**
+ * NOTE THIS FILE IS AUTO-GENERATED
+ * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
+ */
+public final class GitIcons {
+  private static @NotNull Icon load(@NotNull String path, long cacheKey, int flags) {
+    return IconManager.getInstance().loadRasterizedIcon(path, GitIcons.class.getClassLoader(), cacheKey, flags);
+  }
+  /** 16x16 */ public static final @NotNull Icon Signed = load("icons/signed.svg", 8080827721044471571L, 1);
+  /** 16x16 */ public static final @NotNull Icon Verified = load("icons/verified.svg", -4213912419341159251L, 2);
+}
