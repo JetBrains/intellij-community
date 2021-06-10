3f37329e77a191494ffe4e82af6b0f7f24c8b841
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2021-06-10 14:10:20 +0300

    [IFT] Edit remote project creation logic
    - Extract to GitProjectUtil
    - Invoke in background
    
    GitOrigin-RevId: df040091f47ede07544d1349886d471d5c7aa457

--- plugins/git-features-trainer/src/git4idea/ift/GitProjectUtil.kt
+++ plugins/git-features-trainer/src/git4idea/ift/GitProjectUtil.kt
@@ -5,16 +5,20 @@ import com.intellij.openapi.application.invokeAndWaitIfNeeded
 import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.vfs.VirtualFile
 import git4idea.actions.GitInit
+import git4idea.commands.Git
+import git4idea.index.actions.runProcess
+import git4idea.repo.GitRepositoryManager
 import training.project.FileUtils
 import training.project.ProjectUtils
 import java.io.File
 
 object GitProjectUtil {
+  private const val remoteProjectName = "RemoteLearningProject"
+
   fun restoreGitLessonsFiles(project: Project) {
-    val learningProjectPath = ProjectUtils.getProjectRoot(project).toNioPath()
-    val learningProjectRoot = LocalFileSystem.getInstance().refreshAndFindFileByNioFile(learningProjectPath)
-                              ?: error("Learning project not found")
+    val learningProjectRoot = refreshAndGetProjectRoot(project)
     val gitProjectRoot = invokeAndWaitIfNeeded {
       runWriteAction {
         learningProjectRoot.findChild("git")?.apply {
@@ -31,9 +35,45 @@ object GitProjectUtil {
     }
   }
 
-  fun copyGitProject(destination: File): Boolean {
+  fun createRemoteProject(remoteName: String, project: Project): File {
+    val root = reCreateRemoteProjectDir()
+    if (copyGitProject(root)) {
+      configureRemote(remoteName, root, project)
+      return root
+    }
+    error("Failed to create remote project at path: ${root.path}")
+  }
+
+  private fun reCreateRemoteProjectDir(): File {
+    val projectsRoot = ProjectUtils.learningProjectsPath.toFile()
+    val remoteProjectRoot = projectsRoot.listFiles()?.find { it.name == remoteProjectName }.let {
+      it?.apply { deleteRecursively() } ?: File(projectsRoot.absolutePath + File.separator + remoteProjectName)
+    }
+    remoteProjectRoot.mkdir()
+    return remoteProjectRoot
+  }
+
+  private fun copyGitProject(destination: File): Boolean {
     // Classloader of Git IFT plugin is required here
     val gitProjectURL = this.javaClass.classLoader.getResource("learnProjects/GitProject") ?: error("GitProject not found")
     return FileUtils.copyResourcesRecursively(gitProjectURL, destination)
+  }
+
+  private fun configureRemote(remoteName: String, remoteProjectRoot: File, project: Project) {
+    runProcess(project, "", false) {
+      val git = Git.getInstance()
+      val repository = GitRepositoryManager.getInstance(project).repositories.first()
+      git.addRemote(repository, remoteName, remoteProjectRoot.path).throwOnError()
+      repository.update()
+      git.fetch(repository, repository.remotes.first(), emptyList()).throwOnError()
+      git.setUpstream(repository, "$remoteName/main", "main").throwOnError()
+      repository.update()
+    }
+  }
+
+  private fun refreshAndGetProjectRoot(project: Project): VirtualFile {
+    val learningProjectPath = ProjectUtils.getProjectRoot(project).toNioPath()
+    return LocalFileSystem.getInstance().refreshAndFindFileByNioFile(learningProjectPath)
+           ?: error("Learning project not found")
   }
 }
