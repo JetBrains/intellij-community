141c64fe94d4c68f43782aa0b1c09d82fd1747c8
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-06-11 11:39:06 +0200

    FIR IDE: simplify KtType hierarchy
    - get rid of Kt(Non)DenotatbleType super classes as unused
    - make KtErrorType to inherit KtClassType to be consistent with FIR
    
    Original commit: 0a6c96492a53d8d827584ac8a60e0e0097265b83
    
    GitOrigin-RevId: b78f0e4c8e41584c2c7efce13d050c3bb277766b

--- plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/completion/contributors/FirWhenWithSubjectConditionContributor.kt
+++ plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/completion/contributors/FirWhenWithSubjectConditionContributor.kt
@@ -69,7 +69,7 @@ internal class FirWhenWithSubjectConditionContributor(
     }
 
     private fun KtAnalysisSession.getClassSymbol(subjectType: KtType): KtNamedClassOrObjectSymbol? {
-        val classType = subjectType as? KtClassType
+        val classType = subjectType as? KtNonErrorClassType
         return classType?.classSymbol as? KtNamedClassOrObjectSymbol
     }
 
@@ -104,7 +104,7 @@ internal class FirWhenWithSubjectConditionContributor(
         return when (classifier) {
             is KtAnonymousObjectSymbol -> return false
             is KtNamedClassOrObjectSymbol -> !classifier.classKind.isObject
-            is KtTypeAliasSymbol -> (classifier.expandedType as? KtClassType)?.classSymbol?.let { isPrefixNeeded(it) } == true
+            is KtTypeAliasSymbol -> (classifier.expandedType as? KtNonErrorClassType)?.classSymbol?.let { isPrefixNeeded(it) } == true
             is KtTypeParameterSymbol -> true
         }
     }
