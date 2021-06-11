ba0a350cbac5a810db6f472505472ab2b8c4627a
Aleksei Kniazev <aleksei.kniazev@jetbrains.com>
2021-06-11 20:31:40 +0300

    fixed python package ranking resource loading (PY-47449)
    (cherry picked from commit af892cd23b5312ddd58825baacdd90ce5f04d6c7)
    
    IJ-CR-9914
    
    GitOrigin-RevId: a6a6e2a567e5f895bc34b2d25d808db91df7f328

--- python/src/com/jetbrains/python/packaging/PyPIPackageRanking.kt
+++ python/src/com/jetbrains/python/packaging/PyPIPackageRanking.kt
@@ -2,10 +2,9 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.python.packaging
 
+import com.google.common.io.Resources
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
-import java.nio.file.Paths
-import kotlin.io.path.bufferedReader
 
 object PyPIPackageRanking {
 
@@ -14,9 +13,8 @@ object PyPIPackageRanking {
   fun reload() {
     packageRank.clear()
     val gson = Gson()
-    val path = Paths.get(PyPIPackageRanking::class.java.getResource("/packaging/pypi-ranking.json")!!.path)
-
-    val array = path.bufferedReader().use {
+    val resource = PyPIPackageRanking::class.java.getResource("/packaging/pypi-ranking.json") ?: error("Python package ranking not found")
+    val array = Resources.asCharSource(resource, Charsets.UTF_8).openBufferedStream().use {
       gson.fromJson(it, Array<PyPackageRankingEntry>::class.java)
     }
     array.forEach {
