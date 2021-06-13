75f7a1ef766b131104dc3f3f80d6aaeac307ffaa
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2021-06-13 22:12:34 +0300

    IJP-630 (wip) ActionsOnSaveFileDocumentManagerListener should not process files not related to the processed project.
    GitOrigin-RevId: 648dff425fa0b212c86c781d52f3831888f09f88

--- platform/platform-impl/src/com/intellij/ide/actionsOnSave/impl/ActionsOnSaveFileDocumentManagerListener.java
+++ platform/platform-impl/src/com/intellij/ide/actionsOnSave/impl/ActionsOnSaveFileDocumentManagerListener.java
@@ -9,10 +9,14 @@ import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.fileEditor.FileDocumentManagerListener;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.roots.ProjectFileIndex;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public final class ActionsOnSaveFileDocumentManagerListener implements FileDocumentManagerListener {
@@ -20,15 +24,15 @@ public final class ActionsOnSaveFileDocumentManagerListener implements FileDocum
 
   public abstract static class ActionOnSave {
     /**
-     * Invoked in EDT. Should be fast. It's ok to return <code>true</code> and then do nothing in {@link #processDocuments}
+     * Invoked in EDT, maybe inside write action. Should be fast. It's ok to return <code>true</code> and then do nothing in {@link #processDocuments}
      *
      * @param project it's initialized, open, not disposed, and not the default one; no need to double-check in implementations
      */
     public boolean isEnabledForProject(@NotNull Project project) { return false; }
 
     /**
-     * Invoked in EDT, not under write action. Potentially long implementations should run with modal progress synchronously.
-     * Implementations don't have to save modified documents. Note that the passed documents may be unsaved if already modified by some other save action.
+     * Invoked in EDT, not inside write action. Potentially long implementations should run with modal progress synchronously.
+     * Implementations don't need to save modified documents. Note that the passed documents may be unsaved if already modified by some other save action.
      */
     public void processDocuments(@NotNull Project project, @NotNull Document @NotNull [] documents) { }
   }
@@ -40,10 +44,15 @@ public final class ActionsOnSaveFileDocumentManagerListener implements FileDocum
 
   @Override
   public void beforeAllDocumentsSaving() {
+    Document[] documents = FileDocumentManager.getInstance().getUnsavedDocuments();
+    if (documents.length == 0) {
+      return;
+    }
+
     for (Project project : ProjectManager.getInstance().getOpenProjects()) {
       for (ActionOnSave saveAction : EP_NAME.getExtensionList()) {
         if (saveAction.isEnabledForProject(project)) {
-          scheduleDocumentsProcessing(FileDocumentManager.getInstance().getUnsavedDocuments());
+          scheduleDocumentsProcessing(documents);
           return;
         }
       }
@@ -65,18 +74,29 @@ public final class ActionsOnSaveFileDocumentManagerListener implements FileDocum
     myDocumentsToProcess.clear();
 
     // Although invokeLater() is called with ModalityState.NON_MODAL argument, somehow this might be called in modal context (for example on Commit File action)
-    // It's quite weird if save action progress appears or documents are changed is modal context, let's ignore the request.
+    // It's quite weird if save action progress appears or documents get changed in modal context, let's ignore the request.
     if (ModalityState.current() != ModalityState.NON_MODAL) return;
 
+    FileDocumentManager manager = FileDocumentManager.getInstance();
+
     for (Project project : ProjectManager.getInstance().getOpenProjects()) {
+      ProjectFileIndex index = ProjectFileIndex.getInstance(project);
+      List<Document> projectDocuments = ContainerUtil.filter(documents, document -> {
+        VirtualFile file = manager.getFile(document);
+        return file != null && index.isInContent(file);
+      });
+
+      if (projectDocuments.isEmpty()) {
+        continue;
+      }
+
       for (ActionOnSave saveAction : EP_NAME.getExtensionList()) {
         if (saveAction.isEnabledForProject(project)) {
-          saveAction.processDocuments(project, documents);
+          saveAction.processDocuments(project, projectDocuments.toArray(Document.EMPTY_ARRAY));
         }
       }
     }
 
-    FileDocumentManager manager = FileDocumentManager.getInstance();
     for (Document document : documents) {
       manager.saveDocument(document);
     }
