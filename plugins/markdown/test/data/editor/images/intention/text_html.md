edf2d634ec78ebc72637196e57995a23fbbf9dec
Ivan Posti <ivan.posti@jetbrains.com>
2021-06-13 17:21:34 +0300

    [Markdown] Update Configure Image intention tests
    (cherry picked from commit 4fd5c5174b4ed5667e7fbbba55e5278bcbbbd178)
    
    IJ-CR-10327
    
    GitOrigin-RevId: 05604845dbdf3742b41550d25be799ae876ab7bf

--- plugins/markdown/test/data/editor/images/intention/text_html.md
+++
@@ -1,7 +0,0 @@
-Some paragraph <caret><img<caret> src="im<caret>age.png">
-
-Some paragraph <caret><img<caret> src="i<caret>mage.png"> suffix
-
-Some paragraph <div><caret><i<caret>mg src="i<caret>mage.png">
-
-Some paragraph <div><caret><<caret>img src="<caret>image.png"></div>
--- plugins/markdown/test/src/org/intellij/plugins/markdown/editor/images/MarkdownConfigureImageIntentionTest.kt
+++ plugins/markdown/test/src/org/intellij/plugins/markdown/editor/images/MarkdownConfigureImageIntentionTest.kt
@@ -7,9 +7,27 @@ import org.intellij.plugins.markdown.MarkdownBundle
 import org.intellij.plugins.markdown.MarkdownTestingUtil
 
 class MarkdownConfigureImageIntentionTest: BasePlatformTestCase() {
-  fun `test markdown image`() = doTest()
+  fun `test markdown image1`() = doTest()
 
-  fun `test markdown image inside text html`() = doTest()
+  fun `test markdown image2`() = doTest()
+
+  fun `test markdown image3`() = doTest()
+
+  fun `test markdown image4`() = doTest()
+
+  fun `test markdown image inside text html1`() {
+    doContentTest("Prefix paragraph text <div><caret>!<caret>[descri<caret>ption]<caret>(ima<caret>ge.png)</div>")
+  }
+
+  fun `test markdown image inside text html2`() {
+    doContentTest("Prefix paragraph text <div><caret>!<caret>[descri<caret>ption]<caret>(ima<caret>ge.png)</div> suffix text")
+  }
+
+  fun `test markdown image inside text html3`() {
+    doContentTest("Prefix paragraph text <div><div><caret>!<caret>[descri<caret>ption]<caret>(ima<caret>ge.png)</div></div>")
+  }
+
+  fun `test markdown image inside text html4`() = doTest()
 
   fun `test markdown image inside reference`() = doTest()
 
@@ -21,7 +39,19 @@ class MarkdownConfigureImageIntentionTest: BasePlatformTestCase() {
 
   fun `test no markdown image inside html block3`() = doTest(shouldHaveIntentions = false)
 
-  fun `test html block`() = doTest()
+  fun `test html block1`() = doTest()
+
+  fun `test html block2`() = doTest()
+
+  fun `test html block3`() = doContentTest("""<caret><<caret>img<caret> src="s<caret>ome.png">""")
+
+  fun `test html block4`() = doContentTest("""<caret><<caret>img<caret> src="s<caret>ome.png"/>""")
+
+  fun `test html block5`() = doContentTest("""<caret><<caret>img<caret> src="s<caret>ome.png"></img>""")
+
+  fun `test html block6`() = doContentTest("""<div><caret><<caret>img<caret> src="s<caret>ome.png"></div>""")
+
+  fun `test html block7`() = doContentTest("""<div><div><caret><<caret>img<caret> src="s<caret>ome.png"></div></div>""")
 
   fun `test no around html image1`() = doTest(shouldHaveIntentions = false)
 
@@ -29,10 +59,25 @@ class MarkdownConfigureImageIntentionTest: BasePlatformTestCase() {
 
   fun `test no around html image3`() = doTest(shouldHaveIntentions = false)
 
-  fun `test text html`() = doTest()
+  fun `test text html1`() = doContentTest("""Some paragraph <div><caret><<caret>img src="<caret>image.png"></div>""")
 
-  private fun doTest(shouldHaveIntentions: Boolean = true) {
-    myFixture.configureByFile(getTestFileName())
+  fun `test text html2`() = doContentTest("""Some paragraph <caret><img<caret> src="i<caret>mage.png"> suffix""")
+
+  fun `test text html3`() = doContentTest("""Some paragraph <div><caret><i<caret>mg src="i<caret>mage.png">""")
+
+  fun `test text html4`() = doContentTest("""Some paragraph <div><caret><<caret>img src="<caret>image.png"></div>""")
+
+  private fun doContentTest(fileContent: String, shouldHaveIntentions: Boolean = true) {
+    myFixture.configureByText(getTestFileName(), fileContent)
+    doActualTest(shouldHaveIntentions)
+  }
+
+  private fun doTest(fileName: String = getTestFileName(), shouldHaveIntentions: Boolean = true) {
+    myFixture.configureByFile(fileName)
+    doActualTest(shouldHaveIntentions)
+  }
+
+  private fun doActualTest(shouldHaveIntentions: Boolean) {
     // Workaround for org.intellij.plugins.markdown.injection.MarkdownCodeFenceErrorHighlightingIntention.SettingsListener
     (myFixture as CodeInsightTestFixtureImpl).canChangeDocumentDuringHighlighting(true)
     val intentions = myFixture.filterAvailableIntentions(MarkdownBundle.message("markdown.configure.image.intention.name"))
