c4945cbf6b41e700a6dfdd650328f0e0fde47ad3
Lamberto Basti <lamberto.basti@jetbrains.com>
2021-06-14 18:59:47 +0200

    PKGS fixing PKGS-748
    (cherry picked from commit 718f71a44bdcf4a728843e98e60ed5f1d6ff98eb)
    
    GitOrigin-RevId: de8cc0488d04508335591367eb20b6026dc01c35

--- plugins/package-search/src/com/jetbrains/packagesearch/intellij/plugin/ui/toolwindow/PackageSearchToolWindowFactory.kt
+++ plugins/package-search/src/com/jetbrains/packagesearch/intellij/plugin/ui/toolwindow/PackageSearchToolWindowFactory.kt
@@ -1,26 +1,47 @@
-@file:Suppress("EXPERIMENTAL_IS_NOT_ENABLED", "EXPERIMENTAL_API_USAGE")
-
 package com.jetbrains.packagesearch.intellij.plugin.ui.toolwindow
 
-import com.intellij.openapi.components.service
+import com.intellij.openapi.actionSystem.ActionGroup
+import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.RegisterToolWindowTask
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.openapi.wm.ToolWindowManager
+import com.intellij.openapi.wm.ex.ToolWindowEx
+import com.intellij.ui.content.ContentFactory
+import com.intellij.ui.content.ContentManager
+import com.intellij.util.castSafelyTo
 import com.jetbrains.packagesearch.PackageSearchIcons
 import com.jetbrains.packagesearch.intellij.plugin.PackageSearchBundle
+import com.jetbrains.packagesearch.intellij.plugin.ui.toolwindow.panels.HasToolWindowActions
+import com.jetbrains.packagesearch.intellij.plugin.ui.toolwindow.panels.PackageSearchPanelBase
+import com.jetbrains.packagesearch.intellij.plugin.ui.toolwindow.panels.SimpleToolWindowWithToolWindowActionsPanel
+import com.jetbrains.packagesearch.intellij.plugin.ui.toolwindow.panels.SimpleToolWindowWithTwoToolbarsPanel
+import com.jetbrains.packagesearch.intellij.plugin.ui.toolwindow.panels.management.PackageManagementPanel
+import com.jetbrains.packagesearch.intellij.plugin.ui.toolwindow.panels.repositories.RepositoryManagementPanel
+import com.jetbrains.packagesearch.intellij.plugin.ui.updateAndRepaint
 import com.jetbrains.packagesearch.intellij.plugin.util.AppUI
+import com.jetbrains.packagesearch.intellij.plugin.util.FeatureFlags
+import com.jetbrains.packagesearch.intellij.plugin.util.addSelectionChangedListener
 import com.jetbrains.packagesearch.intellij.plugin.util.lifecycleScope
+import com.jetbrains.packagesearch.intellij.plugin.util.logInfo
+import com.jetbrains.packagesearch.intellij.plugin.util.lookAndFeelFlow
+import com.jetbrains.packagesearch.intellij.plugin.util.map
+import com.jetbrains.packagesearch.intellij.plugin.util.onEach
+import com.jetbrains.packagesearch.intellij.plugin.util.packageSearchDataService
 import com.jetbrains.packagesearch.intellij.plugin.util.packageSearchModulesChangesFlow
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.annotations.Nls
+import javax.swing.JComponent
 
 class PackageSearchToolWindowFactory : ToolWindowFactory, DumbAware {
 
@@ -30,51 +51,139 @@ class PackageSearchToolWindowFactory : ToolWindowFactory, DumbAware {
 
         private fun getToolWindow(project: Project) = ToolWindowManager.getInstance(project).getToolWindow(ToolWindowId)
 
-        fun activateToolWindow(project: Project) {
-            getToolWindow(project)?.activate {}
-        }
-
         fun activateToolWindow(project: Project, action: () -> Unit) {
             getToolWindow(project)?.activate(action, true, true)
         }
-
-        fun toggleToolWindow(project: Project) {
-            getToolWindow(project)?.let {
-                if (it.isVisible) {
-                    it.hide { }
-                } else {
-                    it.activate(null, true, true)
-                }
-            }
-        }
     }
 
-    /**
-     * Used as entrypoint for Package Search Plugin. It registers the listeners in the
-     * plugin lifecycle to create the toolbar if needed. Since `isApplicable` is called only once at plugin
-     * initialization, it cannot be used to create the tool window later when needed.
-     */
-    override fun isApplicable(project: Project): Boolean {
-        project.packageSearchModulesChangesFlow
+    override fun isApplicable(project: Project) = runBlocking {
+        val isAvailable = project.packageSearchModulesChangesFlow.first().isNotEmpty()
+
+        if (!isAvailable) project.packageSearchModulesChangesFlow
             .filter { it.isNotEmpty() }
             .take(1)
-            .flowOn(Dispatchers.Default)
-            .map {
+            .map(Dispatchers.AppUI) {
                 RegisterToolWindowTask.closable(
                     ToolWindowId,
-                     PackageSearchBundle.messagePointer("toolwindow.stripe.Packages"),
+                    PackageSearchBundle.messagePointer("toolwindow.stripe.Packages"),
                     PackageSearchIcons.ArtifactSmall
                 )
             }
-            .map { toolWindowTask -> ToolWindowManager.getInstance(project).registerToolWindow(toolWindowTask) }
-            .onEach { toolWindow -> project.service<PackageSearchToolWindowService>().initialize(toolWindow) }
-            .flowOn(Dispatchers.AppUI)
+            .map(Dispatchers.AppUI) { toolWindowTask -> ToolWindowManager.getInstance(project).registerToolWindow(toolWindowTask) }
+            .onEach(Dispatchers.AppUI) { toolWindow -> initialize(toolWindow, project) }
             .launchIn(project.lifecycleScope)
 
-        return false
+        isAvailable
     }
 
-    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        // Intentionally left blank. Read `isApplicable` KDoc above.
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) = initialize(toolWindow, project)
+
+    private fun initialize(
+        toolWindow: ToolWindow,
+        project: Project
+    ): Unit = with(toolWindow) {
+        title = PackageSearchBundle.message("toolwindow.stripe.Packages")
+
+        contentManager.addSelectionChangedListener { event ->
+            if (this is ToolWindowEx) {
+                setAdditionalGearActions(null)
+                event.content.component.castSafelyTo<HasToolWindowActions>()
+                    ?.also { setAdditionalGearActions(it.gearActions) }
+            }
+            setTitleActions(emptyList())
+            event.content.component.castSafelyTo<HasToolWindowActions>()
+                ?.titleActions
+                ?.also { setTitleActions(it.toList()) }
+        }
+
+        contentManager.removeAllContents(true)
+
+        val panels = buildList {
+            add(
+                PackageManagementPanel(
+                    rootDataModelProvider = project.packageSearchDataService,
+                    selectedPackageSetter = project.packageSearchDataService,
+                    targetModuleSetter = project.packageSearchDataService,
+                    searchClient = project.packageSearchDataService,
+                    operationExecutor = project.packageSearchDataService
+                )
+            )
+            if (FeatureFlags.showRepositoriesTab) {
+                add(RepositoryManagementPanel(rootDataModelProvider = project.packageSearchDataService))
+            }
+        }
+
+        val contentFactory = ContentFactory.SERVICE.getInstance()
+
+        for (panel in panels) {
+            panel.initialize(contentManager, contentFactory)
+        }
+
+        isAvailable = false
+
+        project.packageSearchModulesChangesFlow
+            .map { it.isNotEmpty() }
+            .onEach { logInfo("PackageSearchToolWindowFactory#packageSearchModulesChangesFlow") { "Setting toolWindow.isAvailable = $it" } }
+            .onEach(Dispatchers.AppUI) { isAvailable = it }
+            .launchIn(project.lifecycleScope)
+
+        combine(project.lookAndFeelFlow, project.packageSearchModulesChangesFlow.filter { it.isNotEmpty() }) { _, _ -> }
+            .onEach(Dispatchers.AppUI) { contentManager.component.updateAndRepaint() }
+            .launchIn(project.lifecycleScope)
+
+    }
+
+    private fun PackageSearchPanelBase.initialize(
+        contentManager: ContentManager,
+        contentFactory: ContentFactory,
+    ) {
+        val panelContent = content // should be executed before toolbars
+        val toolbar = toolbar
+        val topToolbar = topToolbar
+        val gearActions = gearActions
+        val titleActions = titleActions
+
+        if (topToolbar == null) {
+            contentManager.addTab(title, panelContent, toolbar, gearActions, titleActions, contentFactory)
+        } else {
+            val content = contentFactory.createContent(
+                toolbar?.let {
+                    SimpleToolWindowWithTwoToolbarsPanel(
+                        it,
+                        topToolbar,
+                        gearActions,
+                        titleActions,
+                        panelContent
+                    )
+                },
+                title,
+                false
+            )
+
+            content.isCloseable = false
+            contentManager.addContent(content)
+            content.component.updateAndRepaint()
+        }
+    }
+
+    private fun ContentManager.addTab(
+        @Nls title: String,
+        content: JComponent,
+        toolbar: JComponent?,
+        gearActions: ActionGroup?,
+        titleActions: Array<AnAction>?,
+        contentFactory: ContentFactory
+    ) {
+        addContent(
+            contentFactory.createContent(null, title, false).apply {
+                component = SimpleToolWindowWithToolWindowActionsPanel(gearActions, titleActions, false).apply {
+                    setProvideQuickActions(true)
+                    setContent(content)
+                    toolbar?.let { setToolbar(it) }
+
+                    isCloseable = false
+                }
+            }
+        )
     }
 }
