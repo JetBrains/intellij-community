d64f96ba8eae2ce38a9f9be40b97a6271b6a2ab9
Anna Kozlova <anna.kozlova@jetbrains.com>
2021-06-14 21:36:15 +0200

    [java] prune `import ` from package configurations (IDEA-271345)
    .* is pruned, `import ` makes here no sense as well
    
    GitOrigin-RevId: cb4220daf85e56dbba2911a90581ea2eb3419b98

--- platform/code-style-api/src/com/intellij/psi/codeStyle/PackageEntry.java
+++ platform/code-style-api/src/com/intellij/psi/codeStyle/PackageEntry.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.codeStyle;
 
 import com.intellij.openapi.util.NlsSafe;
@@ -16,7 +16,7 @@ public class PackageEntry {
 
   public PackageEntry(boolean isStatic, @NotNull @NonNls String packageName, boolean withSubpackages) {
     this.isStatic = isStatic;
-    myPackageName = StringUtil.trimEnd(packageName, ".*");
+    myPackageName = StringUtil.trimStart(StringUtil.trimEnd(packageName, ".*"), "import ");
     myWithSubpackages = withSubpackages;
   }
 
