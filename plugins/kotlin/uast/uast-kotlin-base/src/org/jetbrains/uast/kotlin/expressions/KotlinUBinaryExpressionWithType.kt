794c2418d3fa6c5670ce43f13e65e1c7482e0850
Jinseong Jeon <jsjeon@google.com>
2021-06-15 12:20:25 -0700

    FIR/UAST: commonize binary expression
    Original commit: 6c3d349461aed310b545fa104480956ab5b7fd0f
    
    GitOrigin-RevId: 9f2d4d44effc462048ed79644f48b192de343313

--- plugins/kotlin/uast/uast-kotlin-base/src/org/jetbrains/uast/kotlin/expressions/KotlinUBinaryExpressionWithType.kt
+++ plugins/kotlin/uast/uast-kotlin-base/src/org/jetbrains/uast/kotlin/expressions/KotlinUBinaryExpressionWithType.kt
@@ -1,7 +1,4 @@
-/*
- * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
- * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
- */
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package org.jetbrains.uast.kotlin
 
