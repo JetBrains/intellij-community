64c33d4a343fe2386a059dba8cecc6b1a8fd8364
Anna Kozlova <anna.kozlova@jetbrains.com>
2021-06-15 08:56:32 +0200

    suppress strong warnings in injection
    (cherry picked from commit eb3e74d33bc03051bbd662506ef7ba2769ebabdf)
    
    IJ-CR-10078
    
    GitOrigin-RevId: d02e26ecf4e1e1723dd963ca4af69fbf5559f5ed

--- plugins/InspectionGadgets/testsrc/com/siyeh/ig/migration/BigDecimalLegacyMethodInspectionTest.java
+++ plugins/InspectionGadgets/testsrc/com/siyeh/ig/migration/BigDecimalLegacyMethodInspectionTest.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.siyeh.ig.migration;
 
 import com.intellij.codeInspection.InspectionProfileEntry;
@@ -49,7 +35,7 @@ public class BigDecimalLegacyMethodInspectionTest extends LightJavaInspectionTes
   public void testSimple() {
     doTest(
       "import java.math.*;" +
-      "class X {" +
+      " /** @noinspection BigDecimalLegacyMethod*/ class X {" +
       "  void m(BigDecimal value) {" +
       "    value./*Call to 'BigDecimal.divide()' can use 'RoundingMode' enum constant*/divide/**/(value, BigDecimal.ROUND_HALF_DOWN);" +
       "    value./*Call to 'BigDecimal.divide()' can use 'RoundingMode' enum constant*/divide/**/(value, 2, 7);" +
