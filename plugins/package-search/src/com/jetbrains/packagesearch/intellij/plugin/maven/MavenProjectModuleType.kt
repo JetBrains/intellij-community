05d4f10fdb44bab5f7499759484e9c4a6267107e
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2021-06-15 21:15:08 +0200

    [packageSearch] cleanup for #IDEA-267229
    (cherry picked from commit 04bb97b4c8c8df6768e077d9f1708ae5cf0d86eb)
    
    IJ-CR-10116
    
    GitOrigin-RevId: f966829ef5453c0aaceeb3f05ca33f62aafa8933

--- plugins/package-search/src/com/jetbrains/packagesearch/intellij/plugin/maven/MavenProjectModuleType.kt
+++ plugins/package-search/src/com/jetbrains/packagesearch/intellij/plugin/maven/MavenProjectModuleType.kt
@@ -1,20 +1,12 @@
 package com.jetbrains.packagesearch.intellij.plugin.maven
 
-import com.intellij.buildsystem.model.unified.UnifiedDependencyRepository
 import com.intellij.openapi.project.Project
 import com.jetbrains.packagesearch.intellij.plugin.PackageSearchBundle
-import com.jetbrains.packagesearch.intellij.plugin.extensibility.ProjectModule
 import com.jetbrains.packagesearch.intellij.plugin.extensibility.ProjectModuleType
 import com.jetbrains.packagesearch.intellij.plugin.extensibility.ProjectModuleTypeTerm
 import com.jetbrains.packagesearch.intellij.plugin.maven.configuration.packageSearchMavenConfigurationForProject
 import icons.OpenapiIcons
 import javax.swing.Icon
-
-private val MAVEN_CENTRAL_UNIFIED_REPOSITORY = UnifiedDependencyRepository(
-    "central",
-    "Central Repository",
-    "https://repo.maven.apache.org/maven2",
-)
 
 internal object MavenProjectModuleType : ProjectModuleType {
 
@@ -32,12 +24,4 @@ internal object MavenProjectModuleType : ProjectModuleType {
 
     override fun scopes(project: Project): List<String> =
         packageSearchMavenConfigurationForProject(project).getMavenScopes()
-
-    override fun declaredRepositories(module: ProjectModule): List<UnifiedDependencyRepository> {
-        val declaredRepositories = super.declaredRepositories(module)
-        return if (declaredRepositories.none { it.id == "central" })
-            declaredRepositories + MAVEN_CENTRAL_UNIFIED_REPOSITORY
-        else
-            declaredRepositories
-    }
 }
