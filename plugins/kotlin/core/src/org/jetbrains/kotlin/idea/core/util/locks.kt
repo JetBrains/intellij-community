a1a94bb2ef430ac33d39dc6805a9304b1555e2eb
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2021-06-15 17:29:21 +0200

    Added CheckCanceledLock to be able to cancel acquire lock
    (cherry picked from commit b3dbbf3a511eab9c601c7858bcc5cb265439ab1e)
    
    IJ-CR-10081
    
    GitOrigin-RevId: dbedcdeffebf2de51a41ed875917a59add10330e

--- plugins/kotlin/core/src/org/jetbrains/kotlin/idea/core/util/locks.kt
+++ plugins/kotlin/core/src/org/jetbrains/kotlin/idea/core/util/locks.kt
@@ -3,7 +3,7 @@ package org.jetbrains.kotlin.idea.core.util
 
 import com.intellij.openapi.progress.ProgressManager
 import java.util.concurrent.TimeUnit
-import java.util.concurrent.locks.Lock
+import java.util.concurrent.locks.ReentrantLock
 import java.util.concurrent.locks.ReentrantReadWriteLock
 
 internal inline fun <T> ReentrantReadWriteLock.writeWithCheckCanceled(action: () -> T): T {
@@ -24,13 +24,23 @@ internal inline fun <T> ReentrantReadWriteLock.writeWithCheckCanceled(action: ()
     }
 }
 
-internal inline fun <T> Lock.withCheckCanceledLock(action: () -> T): T {
-    while (!tryLock(100, TimeUnit.MILLISECONDS)) {
-        ProgressManager.checkCanceled()
-    }
-    try {
-        return action()
-    } finally {
-        unlock()
+/**
+ * It is preferable to use [CheckCanceledLock] instead of [ReentrantLock] in case of mix of
+ * [IntelliJ Read-Write locks](https://plugins.jetbrains.com/docs/intellij/general-threading-rules.html) and regular locks.
+ *
+ * To acquire lock has to be cancellable action as read actions.
+ */
+class CheckCanceledLock {
+    private val lock = ReentrantLock()
+
+    internal inline fun <T> withLock(action: () -> T): T {
+        while (!lock.tryLock(100, TimeUnit.MILLISECONDS)) {
+            ProgressManager.checkCanceled()
+        }
+        try {
+            return action()
+        } finally {
+            lock.unlock()
+        }
     }
 }
