eb5361582911aa5939315725d7b685e4b0141f30
Anna Kozlova <anna.kozlova@jetbrains.com>
2021-06-15 15:48:51 +0200

    [execution] add bg progress for starting tasks
    EA-285575 (plugin) - ISE: ApplicationImpl.invokeAndWait
    
    GitOrigin-RevId: f8b2d88bbfb1067da28057403299003c6f4f7623

--- platform/lang-api/src/com/intellij/execution/target/TargetEnvironmentAwareRunProfileState.java
+++ platform/lang-api/src/com/intellij/execution/target/TargetEnvironmentAwareRunProfileState.java
@@ -1,15 +1,20 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.target;
 
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.ExecutionManager;
+import com.intellij.execution.configurations.RunProfile;
 import com.intellij.execution.configurations.RunProfileState;
 import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.lang.LangBundle;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.progress.ProcessCanceledException;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.ThrowableComputable;
 import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.util.concurrency.AppExecutorUtil;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
@@ -36,8 +41,10 @@ public interface TargetEnvironmentAwareRunProfileState extends RunProfileState {
                                                          @NotNull ThrowableComputable<? extends T, ? extends ExecutionException> afterPreparation)
     throws ExecutionException {
     Promise<Object> preparationTasks;
-    if (((TargetEnvironmentAwareRunProfile)env.getRunProfile()).needPrepareTarget()) {
-      preparationTasks = ExecutionManager.getInstance(env.getProject()).executePreparationTasks(env, this);
+    final Project project = env.getProject();
+    RunProfile runProfile = env.getRunProfile();
+    if (((TargetEnvironmentAwareRunProfile)runProfile).needPrepareTarget()) {
+      preparationTasks = ExecutionManager.getInstance(project).executePreparationTasks(env, this);
     }
     else {
       preparationTasks = Promises.resolvedPromise();
@@ -45,20 +52,23 @@ public interface TargetEnvironmentAwareRunProfileState extends RunProfileState {
 
     return preparationTasks.thenAsync((Object o) -> {
       AsyncPromise<@Nullable T> promise = new AsyncPromise<>();
-      AppExecutorUtil.getAppExecutorService().execute(() -> {
-        try {
-          promise.setResult(afterPreparation.compute());
-        }
-        catch (ProcessCanceledException e) {
-          promise.setError(StringUtil.notNullize(e.getLocalizedMessage()));
-        }
-        catch (ExecutionException t) {
-          logger.warn(logFailureMessage, t);
-          promise.setError(t);
-        }
-        catch (Throwable t) {
-          logger.error(logFailureMessage, t);
-          promise.setError(t);
+      ProgressManager.getInstance().run(new Task.Backgroundable(project, LangBundle.message("progress.title.starting.run.configuration", runProfile.getName())) {
+        @Override
+        public void run(@NotNull ProgressIndicator indicator) {
+          try {
+            promise.setResult(afterPreparation.compute());
+          }
+          catch (ProcessCanceledException e) {
+            promise.setError(StringUtil.notNullize(e.getLocalizedMessage()));
+          }
+          catch (ExecutionException t) {
+            logger.warn(logFailureMessage, t);
+            promise.setError(t);
+          }
+          catch (Throwable t) {
+            logger.error(logFailureMessage, t);
+            promise.setError(t);
+          }
         }
       });
       return promise;
