c78ff8d3efccc5978a574667e14313360508ffc9
Peter Gromov <peter@jetbrains.com>
2021-06-15 15:45:14 +0200

    [grazie] improve .properties text extraction
    support MessageFormat peculiarities, exclude HTML
    
    GitOrigin-RevId: 2a1eabeef3f29b260b849338462d8665d68bfb14

--- plugins/grazie/properties/src/main/kotlin/com/intellij/grazie/ide/language/properties/PropertyTextExtractor.java
+++ plugins/grazie/properties/src/main/kotlin/com/intellij/grazie/ide/language/properties/PropertyTextExtractor.java
@@ -3,7 +3,9 @@ package com.intellij.grazie.ide.language.properties;
 import com.intellij.grazie.text.TextContent;
 import com.intellij.grazie.text.TextContentBuilder;
 import com.intellij.grazie.text.TextExtractor;
+import com.intellij.grazie.utils.HtmlUtilsKt;
 import com.intellij.lang.properties.parsing.PropertiesTokenTypes;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiComment;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.PsiUtilCore;
@@ -11,8 +13,12 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PropertyTextExtractor extends TextExtractor {
+  private static final Pattern FORMAT_ELEMENT = Pattern.compile("\\{.+?}");
+
   @Override
   public @Nullable TextContent buildTextContent(@NotNull PsiElement root,
                                                 @NotNull Set<TextContent.TextDomain> allowedDomains) {
@@ -20,7 +26,21 @@ public class PropertyTextExtractor extends TextExtractor {
       return TextContentBuilder.FromPsi.removingIndents(" \t#").build(root, TextContent.TextDomain.COMMENTS);
     }
     if (PsiUtilCore.getElementType(root) == PropertiesTokenTypes.VALUE_CHARACTERS) {
-      return TextContent.psiFragment(TextContent.TextDomain.PLAIN_TEXT, root);
+      TextContent content = TextContent.psiFragment(TextContent.TextDomain.PLAIN_TEXT, root);
+      while (true) {
+        int apostrophes = content.toString().indexOf("''");
+        if (apostrophes < 0) break;
+
+        content = content.excludeRange(TextRange.from(apostrophes, 1));
+      }
+
+      while (true) {
+        Matcher matcher = FORMAT_ELEMENT.matcher(content.toString());
+        if (!matcher.find()) break;
+
+        content = content.markUnknown(new TextRange(matcher.start(), matcher.end()));
+      }
+      return HtmlUtilsKt.removeHtml(content);
     }
     return null;
   }
