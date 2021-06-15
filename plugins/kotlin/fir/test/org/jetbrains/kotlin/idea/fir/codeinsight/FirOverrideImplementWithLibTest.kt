6deb5d9cbd65a3f8df23114761999fbfcc933792
Roman Golyshev <roman.golyshev@jetbrains.com>
2021-06-15 17:00:13 +0300

    Fix test metadata for `FirOverrideImplementWithLibTest`
    GitOrigin-RevId: 67a6fbdf13065ac34b1463e0b67649cf254fc22f

--- plugins/kotlin/fir/test/org/jetbrains/kotlin/idea/fir/codeinsight/FirOverrideImplementWithLibTest.kt
+++ plugins/kotlin/fir/test/org/jetbrains/kotlin/idea/fir/codeinsight/FirOverrideImplementWithLibTest.kt
@@ -5,10 +5,14 @@ package org.jetbrains.kotlin.idea.fir.codeinsight
 import org.jetbrains.kotlin.idea.codeInsight.OverrideImplementWithLibTest
 import org.jetbrains.kotlin.idea.core.overrideImplement.KtClassMember
 import org.jetbrains.kotlin.idea.fir.invalidateCaches
+import org.jetbrains.kotlin.idea.test.TestRoot
 import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.test.TestMetadata
 import org.junit.internal.runners.JUnit38ClassRunner
 import org.junit.runner.RunWith
 
+@TestRoot("idea/tests")
+@TestMetadata("testData/codeInsight/overrideImplement/withLib")
 @RunWith(JUnit38ClassRunner::class)
 internal class FirOverrideImplementWithLibTest : OverrideImplementWithLibTest<KtClassMember>(), FirOverrideImplementTestMixIn {
     override fun isFirPlugin(): Boolean = true
