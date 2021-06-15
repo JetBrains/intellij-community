4be8dcf679e59d79faa0d344045d146f3ee9e28f
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2021-06-15 10:10:03 +0300

    move indexed data calculation outside non-cancellable action WI-60935
    GitOrigin-RevId: 5a630c98f50a344c6e1f6bf6f759af1273becbf9

--- platform/lang-impl/src/com/intellij/util/indexing/FileBasedIndexImpl.java
+++ platform/lang-impl/src/com/intellij/util/indexing/FileBasedIndexImpl.java
@@ -1078,9 +1078,8 @@ public final class FileBasedIndexImpl extends FileBasedIndexEx {
 
           markFileIndexed(vFile, newFc);
           try {
-            ProgressManager.getInstance().executeNonCancelableSection(() -> {
-              getIndex(requestedIndexId).mapInputAndPrepareUpdate(inputId, newFc).compute();
-            });
+            Computable<Boolean> update = getIndex(requestedIndexId).mapInputAndPrepareUpdate(inputId, newFc);
+            ProgressManager.getInstance().executeNonCancelableSection(update::compute);
           }
           finally {
             unmarkBeingIndexed();
@@ -1088,9 +1087,8 @@ public final class FileBasedIndexImpl extends FileBasedIndexEx {
           }
         }
         else { // effectively wipe the data from the indices
-          ProgressManager.getInstance().executeNonCancelableSection(() -> {
-            getIndex(requestedIndexId).mapInputAndPrepareUpdate(inputId, null).compute();
-          });
+          Computable<Boolean> update = getIndex(requestedIndexId).mapInputAndPrepareUpdate(inputId, null);
+          ProgressManager.getInstance().executeNonCancelableSection(update::compute);
         }
       }
 
