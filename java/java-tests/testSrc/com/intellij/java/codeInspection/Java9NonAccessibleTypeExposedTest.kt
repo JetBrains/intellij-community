871fa0d674a5c78f8fc92f088b3a48aa62a9cf57
Anna Kozlova <anna.kozlova@jetbrains.com>
2021-06-15 08:56:32 +0200

    suppress strong warnings in injection
    GitOrigin-RevId: eb3e74d33bc03051bbd662506ef7ba2769ebabdf

--- java/java-tests/testSrc/com/intellij/java/codeInspection/Java9NonAccessibleTypeExposedTest.kt
+++ java/java-tests/testSrc/com/intellij/java/codeInspection/Java9NonAccessibleTypeExposedTest.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.java.codeInspection
 
 import com.intellij.java.testFramework.fixtures.LightJava9ModulesCodeInsightFixtureTestCase
@@ -26,6 +12,7 @@ import org.jetbrains.annotations.NotNull
 /**
  * @author Pavel.Dolgov
  */
+@Suppress("ProtectedMemberInFinalClass")
 class Java9NonAccessibleTypeExposedTest : LightJava9ModulesCodeInsightFixtureTestCase() {
   override fun setUp() {
     super.setUp()
@@ -294,7 +281,7 @@ public class Highlighted {
     highlight("""package apiPkg;
 import otherPkg.*;
 import implPkg.*;
-public class Highlighted {
+@SuppressWarnings("ALL") public class Highlighted {
   public void throwsPublic() throws PublicException {}
   public void throwsPackageLocal() throws <warning descr="Class 'PackageLocalException' is not exported from module 'MAIN'">PackageLocalException</warning> {}
   public void throwsOther() throws OtherException {}
