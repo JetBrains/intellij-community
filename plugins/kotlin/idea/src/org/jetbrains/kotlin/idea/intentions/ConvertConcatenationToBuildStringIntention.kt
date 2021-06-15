33330b7fb14caaa085ae46172ca905791d4fa799
Toshiaki Kameyama <kmymtsak@gmail.com>
2021-06-15 13:46:59 +0900

    Do not inherit ConvertToStringTemplateIntention

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/intentions/ConvertConcatenationToBuildStringIntention.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/intentions/ConvertConcatenationToBuildStringIntention.kt
@@ -3,14 +3,24 @@
 package org.jetbrains.kotlin.idea.intentions
 
 import com.intellij.openapi.editor.Editor
+import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.builtins.KotlinBuiltIns
 import org.jetbrains.kotlin.idea.KotlinBundle
+import org.jetbrains.kotlin.idea.caches.resolve.analyze
 import org.jetbrains.kotlin.idea.core.ShortenReferences
 import org.jetbrains.kotlin.idea.core.replaced
+import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.resolve.calls.callUtil.getType
+import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
 
-class ConvertConcatenationToBuildStringIntention : ConvertToStringTemplateIntention() {
-    init {
-        setTextGetter(KotlinBundle.lazyMessage("convert.concatenation.to.build.string"))
+class ConvertConcatenationToBuildStringIntention : SelfTargetingIntention<KtBinaryExpression>(
+    KtBinaryExpression::class.java,
+    KotlinBundle.lazyMessage("convert.concatenation.to.build.string")
+) {
+
+    override fun isApplicableTo(element: KtBinaryExpression, caretOffset: Int): Boolean {
+        return element.isConcatenation() && !element.parent.isConcatenation()
     }
 
     override fun applyTo(element: KtBinaryExpression, editor: Editor?) {
@@ -34,6 +44,13 @@ class ConvertConcatenationToBuildStringIntention : ConvertToStringTemplateIntent
         }
         val replaced = element.replaced(buildStringCall)
         ShortenReferences.DEFAULT.process(replaced)
+    }
+
+    private fun PsiElement.isConcatenation(): Boolean {
+        if (this !is KtBinaryExpression) return false
+        if (operationToken != KtTokens.PLUS) return false
+        val type = getType(analyze(BodyResolveMode.PARTIAL)) ?: return false
+        return KotlinBuiltIns.isString(type)
     }
 
     private fun KtBinaryExpression.allExpressions(): List<KtExpression> {
--- plugins/kotlin/idea/tests/testData/intentions/convertConcatenationToBuildString/basic.kt
+++ plugins/kotlin/idea/tests/testData/intentions/convertConcatenationToBuildString/basic.kt
@@ -1,9 +1,9 @@
 // WITH_RUNTIME
 fun test(foo: String, bar: Int, baz: Int) {
-    val s = <caret>"foo = $foo" +
+    val s = "foo = $foo" +
             """foo = $foo""" +
             "$bar" +
-            "${baz}" +
+            "${baz}" +<caret>
             """$bar""" +
             """${baz + baz}""" +
             foo.length +
