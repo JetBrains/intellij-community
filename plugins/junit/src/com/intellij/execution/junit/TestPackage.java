e73023874e12d8e8180130a543dd9b560b4a4b54
Anna Kozlova <anna.kozlova@jetbrains.com>
2021-06-17 14:58:20 +0200

    EA-295369 (plugin) - NPE: TestPackage.collectClassesRecursively
    GitOrigin-RevId: f594e43dff91bed4b29e3b808ad5c70b2e090a10

--- plugins/junit/src/com/intellij/execution/junit/TestPackage.java
+++ plugins/junit/src/com/intellij/execution/junit/TestPackage.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.junit;
 
 import com.intellij.execution.*;
@@ -87,7 +87,7 @@ public class TestPackage extends TestObject {
             if (JUnitStarter.JUNIT5_PARAMETER.equals(getRunner())) {
               searchTests5(module, classFilter, myClasses);
             }
-            else {
+            else if (classFilter != null) {
               searchTests(module, classFilter, myClasses);
             }
           }
