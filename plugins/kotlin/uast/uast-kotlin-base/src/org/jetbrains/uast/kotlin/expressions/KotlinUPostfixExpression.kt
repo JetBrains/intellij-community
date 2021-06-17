e88545c2bfee6ef1bbe92fab126aeac13d656512
Jinseong Jeon <jsjeon@google.com>
2021-06-17 11:12:38 -0700

    FIR/UAST: commonize postfix expression
    Original commit: 6cb0a1a72865152457f1f75f700e30b1656d0c3a
    
    GitOrigin-RevId: d21b4b1ac8d86eefa2a34f7ce807b5a4ee8b5344

--- plugins/kotlin/uast/uast-kotlin-base/src/org/jetbrains/uast/kotlin/expressions/KotlinUPostfixExpression.kt
+++ plugins/kotlin/uast/uast-kotlin-base/src/org/jetbrains/uast/kotlin/expressions/KotlinUPostfixExpression.kt
@@ -9,11 +9,13 @@ import org.jetbrains.uast.*
 import org.jetbrains.uast.kotlin.internal.DelegatedMultiResolve
 
 class KotlinUPostfixExpression(
-        override val sourcePsi: KtPostfixExpression,
-        givenParent: UElement?
+    override val sourcePsi: KtPostfixExpression,
+    givenParent: UElement?
 ) : KotlinAbstractUExpression(givenParent), UPostfixExpression, KotlinUElementWithType, KotlinEvaluatableUElement,
     UResolvable, DelegatedMultiResolve {
-    override val operand by lz { KotlinConverter.convertOrEmpty(sourcePsi.baseExpression, this) }
+    override val operand by lz {
+        baseResolveProviderService.baseKotlinConverter.convertOrEmpty(sourcePsi.baseExpression, this)
+    }
 
     override val operator = when (sourcePsi.operationToken) {
         KtTokens.PLUSPLUS -> UastPostfixOperator.INC
@@ -22,10 +24,11 @@ class KotlinUPostfixExpression(
         else -> UastPostfixOperator.UNKNOWN
     }
 
-    override val operatorIdentifier: UIdentifier?
+    override val operatorIdentifier: UIdentifier
         get() = KotlinUIdentifier(sourcePsi.operationReference, this)
 
-    override fun resolveOperator(): PsiMethod? = resolveToPsiMethod(sourcePsi)
+    override fun resolveOperator(): PsiMethod? =
+        baseResolveProviderService.resolveCall(sourcePsi)
 
     override fun resolve(): PsiMethod? = when (sourcePsi.operationToken) {
         KtTokens.EXCLEXCL -> operand.tryResolve() as? PsiMethod
