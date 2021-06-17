7e08a6269908fcdd946e123121325a3ff9d49fc9
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2021-06-17 17:44:45 +0300

    [groovy] IDEA-270272: Refactor GroovyNewProjectWizard
    GitOrigin-RevId: 1d9145287613e98c7c44aa89f15ca8a67088d403

--- plugins/groovy/src/org/jetbrains/plugins/groovy/config/GroovyNewProjectWizard.kt
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/config/GroovyNewProjectWizard.kt
@@ -7,6 +7,7 @@ import com.intellij.framework.library.FrameworkLibraryVersionFilter
 import com.intellij.ide.*
 import com.intellij.ide.NewModuleStep.Companion.twoColumnRow
 import com.intellij.ide.util.projectWizard.ModuleBuilder
+import com.intellij.ide.util.projectWizard.ModuleBuilder.ModuleConfigurationUpdater
 import com.intellij.ide.util.projectWizard.WizardContext
 import com.intellij.openapi.fileChooser.FileChooserFactory
 import com.intellij.openapi.module.Module
@@ -17,22 +18,21 @@ import com.intellij.openapi.roots.ModifiableRootModel
 import com.intellij.openapi.roots.libraries.LibraryType
 import com.intellij.openapi.roots.ui.configuration.JdkComboBox
 import com.intellij.openapi.roots.ui.configuration.libraryEditor.NewLibraryEditor
+import com.intellij.openapi.roots.ui.configuration.projectRoot.LibrariesContainer
 import com.intellij.openapi.roots.ui.configuration.projectRoot.LibrariesContainerFactory
 import com.intellij.openapi.roots.ui.configuration.projectRoot.ProjectSdksModel
-import com.intellij.openapi.ui.ComponentWithBrowseButton
+import com.intellij.openapi.ui.ComponentWithBrowseButton.BrowseFolderActionListener
 import com.intellij.openapi.ui.TextComponentAccessor
 import com.intellij.openapi.ui.TextFieldWithBrowseButton
 import com.intellij.openapi.vfs.VfsUtil
-import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.ui.SimpleListCellRenderer
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBRadioButton
 import com.intellij.ui.layout.*
-import com.intellij.util.download.DownloadableFileSetVersions
+import com.intellij.util.download.DownloadableFileSetVersions.FileSetVersionsCallback
 import org.jetbrains.plugins.groovy.GroovyBundle
 import java.awt.Dimension
-import java.awt.KeyboardFocusManager
-import java.awt.event.ItemListener
+import java.awt.KeyboardFocusManager.getCurrentKeyboardFocusManager
 import java.nio.file.Path
 import javax.swing.DefaultComboBoxModel
 import javax.swing.JTextField
@@ -45,93 +45,121 @@ class GroovyNewProjectWizard : NewProjectWizard<GroovyModuleSettings> {
   override fun settingsList(settings: GroovyModuleSettings): List<SettingsComponent> {
     val sdkCombo = JdkComboBox(null, ProjectSdksModel(), { it is JavaSdk }, null, null, null)
       .apply { minimumSize = Dimension(0, 0) }
-      .also { combo -> combo.addItemListener(ItemListener { settings.javaSdk = combo.selectedJdk }) }
+      .also { combo -> combo.addItemListener { settings.javaSdk = combo.selectedJdk } }
     val panel = panel {
       row {
-        lateinit var fromUrlCheckbox: CellBuilder<JBRadioButton>
-        lateinit var fromFilesystemCheckbox: CellBuilder<JBRadioButton>
-        twoColumnRow(
-          { fromUrlCheckbox = radioButton(GroovyBundle.message("radio.use.version.from.maven"), settings::useMavenLibrary) },
-          {
-            val downloadableType = LibraryType.EP_NAME.findExtension(GroovyDownloadableLibraryType::class.java)!!
-            val groovyLibraryDescription = downloadableType.libraryDescription
-            comboBox(DefaultComboBoxModel(emptyArray()),
-                     { settings.mavenVersion }, { settings.mavenVersion = it },
-                     SimpleListCellRenderer.create("") { it?.versionString ?: "<unknown>" })
-              .applyToComponent {
-                groovyLibraryDescription.fetchVersions(object : DownloadableFileSetVersions.FileSetVersionsCallback<FrameworkLibraryVersion>() {
-                  override fun onSuccess(versions: List<FrameworkLibraryVersion>) = SwingUtilities.invokeLater {
-                    for (item in versions) {
-                      addItem(item)
-                    }
-                  }
-                })
-              }.enableIf(fromUrlCheckbox.selected)
-          })
-        twoColumnRow(
-          { fromFilesystemCheckbox = radioButton(GroovyBundle.message("radio.use.sdk.from.disk"), settings::useLocalLibrary) },
-          {
-            // todo: color text field in red if selected path does not correspond to a groovy sdk home
-            val groovyLibraryDescription = GroovyLibraryDescription()
-            val fileChooserDescriptor = groovyLibraryDescription.createFileChooserDescriptor()
-            val textWithBrowse = TextFieldWithBrowseButton()
-            val pathToGroovyHome = groovyLibraryDescription.findPathToGroovyHome()
-            textWithBrowse.setText(pathToGroovyHome?.path)
-            textWithBrowse.addActionListener(object : ComponentWithBrowseButton.BrowseFolderActionListener<JTextField>(
-              GroovyBundle.message("dialog.title.select.groovy.sdk"), null, textWithBrowse, null, fileChooserDescriptor,
-              TextComponentAccessor.TEXT_FIELD_WHOLE_TEXT) {
-              override fun getInitialFile(): VirtualFile? {
-                return pathToGroovyHome
-              }
-            })
-            FileChooserFactory.getInstance().installFileCompletion(textWithBrowse.textField, fileChooserDescriptor, true, null)
-            val wrappedComponent = component(textWithBrowse)
-            wrappedComponent.constraints(growX)
-              .withBinding(TextFieldWithBrowseButton::getText, TextFieldWithBrowseButton::setText, settings::sdkPath.toBinding())
-              .enableIf(fromFilesystemCheckbox.selected)
-          }
-        )
-        fromFilesystemCheckbox.applyToComponent { addChangeListener { if (fromFilesystemCheckbox.selected()) fromUrlCheckbox.applyToComponent { isSelected = false } } }
-        fromUrlCheckbox.applyToComponent { addChangeListener { if (fromUrlCheckbox.selected()) fromFilesystemCheckbox.applyToComponent { isSelected = false } } }
+        val fromUrlCheckbox: JBRadioButton = createDownloadableLibraryPanel(settings)
+        val fromFilesystemCheckbox: JBRadioButton = createFileSystemLibraryPanel(settings)
+
+        fromFilesystemCheckbox.addChangeListener {
+          if (fromFilesystemCheckbox.selected()) fromUrlCheckbox.isSelected = false
+        }
+        fromUrlCheckbox.addChangeListener {
+          if (fromUrlCheckbox.selected()) fromFilesystemCheckbox.isSelected = false
+        }
       }
     }
-    return listOf(LabelAndComponent(JBLabel(JavaUiBundle.message("label.project.wizard.new.project.jdk")), sdkCombo), LabelAndComponent(JBLabel(GroovyBundle.message("label.groovy.sdk")), panel {}), JustComponent(panel))
+    return listOf(LabelAndComponent(JBLabel(JavaUiBundle.message("label.project.wizard.new.project.jdk")), sdkCombo),
+                  LabelAndComponent(JBLabel(GroovyBundle.message("label.groovy.sdk")), panel {}),
+                  JustComponent(panel))
+  }
+
+  private fun Row.createDownloadableLibraryPanel(settings: GroovyModuleSettings): JBRadioButton {
+    lateinit var checkbox: JBRadioButton
+    twoColumnRow(
+      { checkbox = radioButton(GroovyBundle.message("radio.use.version.from.maven"), settings::useMavenLibrary).component },
+      {
+        val groovyLibraryType = LibraryType.EP_NAME.findExtensionOrFail(GroovyDownloadableLibraryType::class.java)
+        val downloadableLibraryDescription = groovyLibraryType.libraryDescription
+        comboBox(
+          DefaultComboBoxModel(emptyArray()),
+          { settings.mavenVersion }, { settings.mavenVersion = it },
+          SimpleListCellRenderer.create("") { it?.versionString ?: "<unknown>" }
+        ).applyToComponent {
+          downloadableLibraryDescription.fetchVersions(object : FileSetVersionsCallback<FrameworkLibraryVersion>() {
+            override fun onSuccess(versions: List<FrameworkLibraryVersion>) = SwingUtilities.invokeLater { versions.forEach(::addItem) }
+          })
+        }.enableIf(checkbox.selected)
+      })
+    return checkbox
+  }
+
+  private fun Row.createFileSystemLibraryPanel(settings: GroovyModuleSettings): JBRadioButton {
+    lateinit var checkbox: JBRadioButton
+    twoColumnRow(
+      { checkbox = radioButton(GroovyBundle.message("radio.use.sdk.from.disk"), settings::useLocalLibrary).component },
+      {
+        // todo: color text field in red if selected path does not correspond to a groovy sdk home
+        val groovyLibraryDescription = GroovyLibraryDescription()
+        val fileChooserDescriptor = groovyLibraryDescription.createFileChooserDescriptor()
+        val pathToGroovyHome = groovyLibraryDescription.findPathToGroovyHome()
+        val textWithBrowse = TextFieldWithBrowseButton()
+          .apply {
+            setText(pathToGroovyHome?.path)
+            addActionListener(object : BrowseFolderActionListener<JTextField>(
+              GroovyBundle.message("dialog.title.select.groovy.sdk"),
+              null,
+              this,
+              null,
+              fileChooserDescriptor,
+              TextComponentAccessor.TEXT_FIELD_WHOLE_TEXT) {
+              override fun getInitialFile() = pathToGroovyHome
+            })
+            FileChooserFactory.getInstance().installFileCompletion(textField, fileChooserDescriptor, true, null)
+          }
+          .let(::component)
+        textWithBrowse
+          .constraints(growX)
+          .withBinding(TextFieldWithBrowseButton::getText, TextFieldWithBrowseButton::setText, settings::sdkPath.toBinding())
+          .enableIf(checkbox.selected)
+      }
+    )
+    return checkbox
   }
 
   override fun setupProject(project: Project, settings: GroovyModuleSettings, context: WizardContext) {
     val builder = context.projectBuilder as? ModuleBuilder ?: return
-    builder.contentEntryPath = project.basePath
-    builder.name = project.name
-    val groovyModuleBuilder = GroovyAwareModuleBuilder()
-    val libraryDescription = GroovyLibraryDescription()
-    val compositionSettings = LibraryCompositionSettings(libraryDescription, { project.basePath ?: "./" },
-                                                         FrameworkLibraryVersionFilter.ALL, listOf(settings.mavenVersion))
-    builder.moduleJdk = settings.javaSdk
-    groovyModuleBuilder.updateFrom(builder)
-    groovyModuleBuilder.moduleJdk = settings.javaSdk
-    println(settings.javaSdk)
-    val librariesContainer = if (context.modulesProvider == null)
-      LibrariesContainerFactory.createContainer(context.project)
-    else LibrariesContainerFactory.createContainer(context, context.modulesProvider)
-    if (settings.useMavenLibrary && settings.mavenVersion != null) {
-      compositionSettings.setDownloadLibraries(true)
-      compositionSettings.downloadFiles(context.wizard.contentComponent)
+    val groovyModuleBuilder = GroovyAwareModuleBuilder().apply {
+      contentEntryPath = project.basePath
+      name = project.name
+      moduleJdk = settings.javaSdk
     }
-    else if (settings.useLocalLibrary) {
-      val virtualFile = VfsUtil.findFile(Path.of(settings.sdkPath), false) ?: return
-      val newLibraryConfiguration = libraryDescription.createLibraryConfiguration(KeyboardFocusManager.getCurrentKeyboardFocusManager().activeWindow, virtualFile) ?: return
-      val libraryEditor = NewLibraryEditor(newLibraryConfiguration.libraryType, newLibraryConfiguration.properties)
-      libraryEditor.name = librariesContainer.suggestUniqueLibraryName(newLibraryConfiguration.defaultLibraryName)
-      newLibraryConfiguration.addRoots(libraryEditor)
-      compositionSettings.setNewLibraryEditor(libraryEditor)
-    }
-    builder.addModuleConfigurationUpdater(object : ModuleBuilder.ModuleConfigurationUpdater() {
+
+    val librariesContainer = LibrariesContainerFactory.createContainer(context.project)
+
+    val compositionSettings = generateCompositionSettings(settings, project, context, librariesContainer)
+
+    builder.addModuleConfigurationUpdater(object : ModuleConfigurationUpdater() {
       override fun update(module: Module, rootModel: ModifiableRootModel) {
         groovyModuleBuilder.setupRootModel(rootModel)
         compositionSettings.addLibraries(rootModel, mutableListOf(), librariesContainer)
       }
     })
   }
+}
+
+private fun generateCompositionSettings(settings: GroovyModuleSettings,
+                                        project: Project,
+                                        context: WizardContext,
+                                        container: LibrariesContainer): LibraryCompositionSettings {
+  val libraryDescription = GroovyLibraryDescription()
+  val compositionSettings = LibraryCompositionSettings(libraryDescription, { project.basePath ?: "./" },
+                                                       FrameworkLibraryVersionFilter.ALL, listOf(settings.mavenVersion))
+  if (settings.useMavenLibrary && settings.mavenVersion != null) {
+    compositionSettings.setDownloadLibraries(true)
+    compositionSettings.downloadFiles(context.wizard.contentComponent)
+  }
+  else if (settings.useLocalLibrary) {
+    val virtualFile = VfsUtil.findFile(Path.of(settings.sdkPath), false) ?: return compositionSettings
+    val newLibraryConfiguration =
+      libraryDescription.createLibraryConfiguration(getCurrentKeyboardFocusManager().activeWindow, virtualFile)
+      ?: return compositionSettings
+    val libraryEditor = NewLibraryEditor(newLibraryConfiguration.libraryType, newLibraryConfiguration.properties)
+    libraryEditor.name = container.suggestUniqueLibraryName(newLibraryConfiguration.defaultLibraryName)
+    newLibraryConfiguration.addRoots(libraryEditor)
+    compositionSettings.setNewLibraryEditor(libraryEditor)
+  }
+  return compositionSettings
 }
 
 class GroovyModuleSettings {
