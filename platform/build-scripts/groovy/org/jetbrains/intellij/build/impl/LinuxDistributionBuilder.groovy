dcb7b173499e9780959f787e02b27b2d0237cbe9
Sergey Dubov <sergey.dubov@jetbrains.com>
2021-06-17 23:56:24 +0300

    [CodeWithMe] Enable Remote Development from Idea distribution by default
    - Add a build flag for adding Remote Development libraries into Idea distribution
    - Set default value for the flag to 'true'
    
    IJ-CR-10312
    
    GitOrigin-RevId: 4299cb486dd4fd16a429a5672e624469e649881d

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/LinuxDistributionBuilder.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/LinuxDistributionBuilder.groovy
@@ -49,8 +49,10 @@ final class LinuxDistributionBuilder extends OsSpecificDistributionBuilder {
     BuildTasksImpl.generateBuildTxt(buildContext, unixDistPath)
     BuildTasksImpl.copyDistFiles(buildContext, unixDistPath)
     BuildTasksImpl.addDbusJava(buildContext, unixDistPath)
-    BuildTasksImpl.addProjectorServer(buildContext, unixDistPath)
-    prepareSelfContainedRemoteDevelopmentFiles(buildContext, unixDistPath)
+    if (buildContext.productProperties.addRemoteDevelopmentLibraries) {
+      BuildTasksImpl.addProjectorServer(buildContext, unixDistPath)
+      prepareSelfContainedRemoteDevelopmentFiles(buildContext, unixDistPath)
+    }
     Files.copy(ideaProperties, distBinDir.resolve(ideaProperties.fileName), StandardCopyOption.REPLACE_EXISTING)
     //todo[nik] converting line separators to unix-style make sense only when building Linux distributions under Windows on a local machine;
     // for real installers we need to checkout all text files with 'lf' separators anyway
