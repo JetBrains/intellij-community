33c7e8bcdf3278365e6309fdf05e28811497f3ce
Yann Cébron <yann.cebron@jetbrains.com>
2021-06-17 15:15:21 +0200

    ExperimentalFeatureImpl: note registry EP as replacement
    (cherry picked from commit 126934d3a61316e9e149341eda8db589c818e5aa)
    
    IJ-CR-10280
    
    GitOrigin-RevId: b20aedd06d052c1dde8f4b9d1548ec01fcbf096b

--- platform/platform-impl/src/com/intellij/openapi/application/ExperimentalFeatureImpl.java
+++ platform/platform-impl/src/com/intellij/openapi/application/ExperimentalFeatureImpl.java
@@ -5,7 +5,7 @@ import com.intellij.openapi.diagnostic.Logger;
 
 /**
  * @author Konstantin Bulenkov
- * @deprecated Please use the registry to enable/disable experimental features
+ * @deprecated Please use the registry to enable/disable experimental features, see {@code com.intellij.registryKey} extension point
  */
 @Deprecated
 public final class ExperimentalFeatureImpl extends ExperimentalFeature{
