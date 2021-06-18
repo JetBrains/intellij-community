434ccaa60bae0328b742a50839ac9f338148589c
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-06-18 17:09:54 +0200

    `SlowOperations`: remove `KotlinAddImportAction` from misbehaving frames, it seems to be fixed
    GitOrigin-RevId: 08ac8516dca1a6ae74e9d8d3cddfafc2bbac6537

--- platform/core-api/src/com/intellij/util/SlowOperations.java
+++ platform/core-api/src/com/intellij/util/SlowOperations.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util;
 
 import com.intellij.diagnostic.LoadingState;
@@ -30,7 +30,6 @@ public final class SlowOperations {
   private static final Set<String> ourReportedTraces = new HashSet<>();
   private static final String[] misbehavingFrames = {
     "org.jetbrains.kotlin.idea.refactoring.introduce.introduceVariable.KotlinIntroduceVariableHandler",
-    "org.jetbrains.kotlin.idea.actions.KotlinAddImportAction",
     "org.jetbrains.kotlin.idea.codeInsight.KotlinCopyPasteReferenceProcessor",
     "com.intellij.apiwatcher.plugin.presentation.bytecode.UsageHighlighter",
   };
