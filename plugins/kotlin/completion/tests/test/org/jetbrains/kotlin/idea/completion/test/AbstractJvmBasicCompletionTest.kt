64d3c92b1f8eb81a0fd234d8c749ad1669c3473b
Roman Golyshev <roman.golyshev@jetbrains.com>
2021-06-18 15:51:12 +0300

    Add missing mockJDK classes to the fixture
    Those classes are referenced by the Kotlin SDK via typealiases. When
    completion tries to unwrap typealiases, it fails and gives a
    'missing class' error
    
    We add shallow copies of those classes to the fixture by hand,
    so the compiler can see them and avoid failures
    
    GitOrigin-RevId: 8afac5fedd7119d315dfe6482916ccbd124ccca9

--- plugins/kotlin/completion/tests/test/org/jetbrains/kotlin/idea/completion/test/AbstractJvmBasicCompletionTest.kt
+++ plugins/kotlin/completion/tests/test/org/jetbrains/kotlin/idea/completion/test/AbstractJvmBasicCompletionTest.kt
@@ -19,4 +19,78 @@ abstract class AbstractJvmBasicCompletionTest : KotlinFixtureCompletionBaseTestC
 
     override fun getPlatform() = JvmPlatforms.jvm16
     override fun defaultCompletionType() = CompletionType.BASIC
+
+    override fun configureFixture(testPath: String) {
+        // Kotlin SDK references many JDK classes via typealiases.
+        // Some of them are missing in the mockJDKs of intellij repository,
+        // so we have to add them to the fixture by hand
+        addCharacterCodingException()
+        addAppendable()
+        addHashSet()
+        addLinkedHashSet()
+
+        super.configureFixture(testPath)
+    }
+
+    private fun addCharacterCodingException() {
+        myFixture.addClass(
+            """
+            package java.nio.charset;
+                                    
+            import java.io.IOException;            
+            
+            public class CharacterCodingException extends IOException {}
+            """.trimIndent()
+        )
+    }
+
+    private fun addAppendable() {
+        myFixture.addClass(
+            """
+            package java.lang;
+            
+            import java.io.IOException;
+            
+            public interface Appendable {
+                Appendable append(CharSequence csq) throws IOException;
+                Appendable append(CharSequence csq, int start, int end) throws IOException;
+                Appendable append(char c) throws IOException;
+            }
+            """.trimIndent()
+        )
+    }
+
+    private fun addHashSet() {
+        myFixture.addClass(
+            """
+            package java.util;
+            
+            import java.io.Serializable;
+            
+            public class HashSet<E> extends AbstractSet<E> implements Set<E>, Cloneable, Serializable {
+                @Override
+                public Iterator<E> iterator() {
+                    return null;
+                }
+                        
+                @Override
+                public int size() {
+                    return 0;
+                }
+            }
+            """.trimIndent()
+        )
+    }
+
+    private fun addLinkedHashSet() {
+        myFixture.addClass(
+            """
+            package java.util;
+            
+            import java.io.Serializable;
+                        
+            public class LinkedHashSet<E> extends HashSet<E> implements Set<E>, Cloneable, Serializable {}
+            """.trimIndent()
+        )
+    }
 }
