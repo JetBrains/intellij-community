8ea36e32ac58a577a6d4ec1b205ba4aca375b85f
Roman Ivanov <roman.ivanov@jetbrains.com>
2021-06-18 17:58:11 +0200

    [java] do not suggest to replace switch statement with label
    IDEA-226169, IDEA-271705
    
    (cherry picked from commit 194aa32a4d824970b8823b30c5f32a6c22b8f867)
    
    GitOrigin-RevId: edb933957c9271e55982eab5d081d592aaf29e86

--- java/java-impl/src/com/intellij/codeInspection/EnhancedSwitchMigrationInspection.java
+++ java/java-impl/src/com/intellij/codeInspection/EnhancedSwitchMigrationInspection.java
@@ -91,7 +91,8 @@ public class EnhancedSwitchMigrationInspection extends AbstractBaseJavaLocalInsp
   }
 
   @Nullable
-  private static List<OldSwitchStatementBranch> extractBranches(@NotNull PsiCodeBlock body) {
+  private static List<OldSwitchStatementBranch> extractBranches(@NotNull PsiCodeBlock body,
+                                                                PsiSwitchStatement switchStatement) {
     List<OldSwitchStatementBranch> branches = new ArrayList<>();
     PsiStatement[] statements = body.getStatements();
     int unmatchedCaseIndex = -1;
@@ -108,6 +109,7 @@ public class EnhancedSwitchMigrationInspection extends AbstractBaseJavaLocalInsp
         unmatchedCaseIndex = i;
       }
       else if (current instanceof PsiBreakStatement) {
+        if (((PsiBreakStatement)current).findExitedStatement() != switchStatement) return null;
         if (unmatchedCaseIndex == -1) return null;
         OldSwitchStatementBranch newBranch = addBranch(branches, statements, unmatchedCaseIndex, i, false, (PsiBreakStatement)current);
         newBranch.myPreviousSwitchBranch = previousBranch;
@@ -136,7 +138,7 @@ public class EnhancedSwitchMigrationInspection extends AbstractBaseJavaLocalInsp
     if (expression == null) return null;
     PsiCodeBlock body = switchStatement.getBody();
     if (body == null) return null;
-    List<OldSwitchStatementBranch> branches = extractBranches(body);
+    List<OldSwitchStatementBranch> branches = extractBranches(body, switchStatement);
     if (branches == null || branches.isEmpty()) return null;
     boolean isExhaustive = isExhaustiveSwitch(branches, expression);
     return runInspections(switchStatement, isExhaustive, branches);
