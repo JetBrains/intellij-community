67e05c09cd2191875bec2517478e450ecbc9ade3
Dmitry Zhuravlev <dmitry.zhuravlev@jetbrains.com>
2021-06-18 18:49:41 +0300

    diff: extract "diff_opened_in_new_window" key to diff related listener
    GitOrigin-RevId: 1acdf12fab5a04b6b3a85e55a154ce0e10c6f044

--- platform/diff-impl/src/com/intellij/diff/editor/DiffEditorTabFilesManager.kt
+++ platform/diff-impl/src/com/intellij/diff/editor/DiffEditorTabFilesManager.kt
@@ -6,6 +6,8 @@ import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.options.advanced.AdvancedSettings
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Key
+import com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Internal
@@ -19,9 +21,15 @@ interface DiffEditorTabFilesManager {
     const val SHOW_DIFF_IN_EDITOR_SETTING = "show.diff.as.editor.tab"
 
     @JvmStatic
+    fun isDiffOpenedInNewWindow(file: VirtualFile): Boolean = DIFF_OPENED_IN_NEW_WINDOW.get(file, false)
+
+    @JvmStatic
     fun getInstance(project: Project): DiffEditorTabFilesManager = project.service()
   }
 }
+
+@JvmField
+val DIFF_OPENED_IN_NEW_WINDOW = Key<Boolean>("DIFF_OPENED_IN_NEW_WINDOW")
 
 class DefaultDiffTabFilesManager(private val project: Project) : DiffEditorTabFilesManager {
 
