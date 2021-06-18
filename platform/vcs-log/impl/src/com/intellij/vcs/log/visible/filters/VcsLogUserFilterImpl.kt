7e928a7fa465c2682eb4b2783f2bda4e5b4fb31c
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2021-06-18 15:05:26 +0300

    [vcs-log] move VcsLogUserResolver to the data package
    GitOrigin-RevId: 2995907bd5f69af8de40c9e2fc2154ee7e884c69

--- platform/vcs-log/impl/src/com/intellij/vcs/log/visible/filters/VcsLogUserFilterImpl.kt
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/visible/filters/VcsLogUserFilterImpl.kt
@@ -2,15 +2,15 @@
 package com.intellij.vcs.log.visible.filters
 
 import com.intellij.openapi.diagnostic.logger
-import com.intellij.openapi.util.ClearableLazyValue
 import com.intellij.openapi.util.Comparing
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.util.containers.MultiMap
 import com.intellij.vcs.log.VcsCommitMetadata
 import com.intellij.vcs.log.VcsLogBundle
 import com.intellij.vcs.log.VcsLogUserFilter
 import com.intellij.vcs.log.VcsUser
+import com.intellij.vcs.log.data.VcsLogUserResolver
+import com.intellij.vcs.log.data.VcsLogUserResolverBase
 import com.intellij.vcs.log.util.VcsUserUtil
 import org.jetbrains.annotations.ApiStatus
 
@@ -78,70 +78,6 @@ internal class VcsLogUserFilterImpl(private val userNames: Collection<String>,
 
   companion object {
     private val LOG = logger<VcsLogUserFilterImpl>()
-  }
-}
-
-interface VcsLogUserResolver {
-  fun resolveUserName(name: String): Set<VcsUser>
-  fun resolveCurrentUser(root: VirtualFile): Set<VcsUser>
-}
-
-abstract class VcsLogUserResolverBase : VcsLogUserResolver {
-  abstract val currentUsers: Map<VirtualFile, VcsUser>
-  abstract val allUsers: Set<VcsUser>
-
-  private val cachedUsers = ClearableLazyValue.createAtomic {
-    val usersByNames: MultiMap<String, VcsUser> = MultiMap.create()
-    val usersByEmails: MultiMap<String, VcsUser> = MultiMap.create()
-
-    for (user in allUsers) {
-      val name = user.name
-      if (name.isNotEmpty()) {
-        usersByNames.putValue(VcsUserUtil.getNameInStandardForm(name), user)
-      }
-      val email = user.email
-      val nameFromEmail = VcsUserUtil.getNameFromEmail(email)
-      if (nameFromEmail != null) {
-        usersByEmails.putValue(VcsUserUtil.getNameInStandardForm(nameFromEmail), user)
-      }
-    }
-
-    return@createAtomic Pair(usersByNames, usersByEmails)
-  }
-
-  private val allUsersByNames: MultiMap<String, VcsUser> get() = cachedUsers.value.first
-  private val allUsersByEmails: MultiMap<String, VcsUser> get() = cachedUsers.value.second
-
-  protected fun buildCache() = cachedUsers.value
-
-  override fun resolveUserName(name: String): Set<VcsUser> {
-    val standardName = VcsUserUtil.getNameInStandardForm(name)
-    return allUsersByNames[standardName].union(allUsersByEmails[standardName])
-  }
-
-  override fun resolveCurrentUser(root: VirtualFile): Set<VcsUser> {
-    val vcsUser = currentUsers[root]
-    if (vcsUser == null) {
-      LOG.warn("Can not resolve user name for root $root")
-      return emptySet()
-    }
-
-    val usersByName = resolveUserName(vcsUser.name)
-    val emailNamePart = VcsUserUtil.getNameFromEmail(vcsUser.email) ?: return usersByName
-
-    val emails = usersByName.map { user -> VcsUserUtil.emailToLowerCase(user.email) }.toSet()
-    val usersByEmail = resolveUserName(emailNamePart).filter { candidateUser ->
-      /*
-      ivan@ivanov.com and ivan@petrov.com have the same emailNamePart but they are different people
-      resolveUserName("ivan") will find both of them, so we filter results here
-      */
-      emails.contains(VcsUserUtil.emailToLowerCase(candidateUser.email))
-    }
-    return usersByName + usersByEmail
-  }
-
-  companion object {
-    private val LOG = logger<VcsLogUserResolverBase>()
   }
 }
 
