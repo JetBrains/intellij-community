d797a4ebee342aed15cbf406e2df5ba3f8a32df0
Aleksandr Krasilnikov <aleksandr.krasilnikov@jetbrains.com>
2021-06-18 15:37:04 +0300

    git: cleanup
    GitOrigin-RevId: f08b52b213c95791384cc3b4aabd4dfe27e7d4a5

--- plugins/git4idea/src/git4idea/util/GitFindPushTarget.kt
+++
@@ -1,9 +0,0 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package git4idea.util
-
-import git4idea.GitLocalBranch
-import git4idea.branch.GitBranchUtil
-import git4idea.push.GitPushTarget
-import git4idea.repo.GitRemote
-import git4idea.repo.GitRepository
-
