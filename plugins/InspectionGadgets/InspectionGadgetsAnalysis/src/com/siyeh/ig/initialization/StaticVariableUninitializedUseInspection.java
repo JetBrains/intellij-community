25d0503723393df825750d175468ed72df036667
Bas Leijdekkers <basleijdekkers@gmail.com>
2021-06-21 11:49:02 +0200

    IG: don't report element if it is not physical (EA-263167)
    GitOrigin-RevId: ac6fa3963a3b97068fb97122a8178a1107eb4ac9

--- plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/initialization/StaticVariableUninitializedUseInspection.java
+++ plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/initialization/StaticVariableUninitializedUseInspection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2017 Dave Griffith, Bas Leijdekkers
+ * Copyright 2003-2021 Dave Griffith, Bas Leijdekkers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -102,7 +102,7 @@ public class StaticVariableUninitializedUseInspection extends BaseInspection {
         }
         final PsiExpression[] moreBadReads = uninitializedReadCollector.getUninitializedReads();
         for (PsiExpression badRead : moreBadReads) {
-          registerError(badRead);
+          if (badRead.isPhysical()) registerError(badRead); // EA-263167
         }
       }
     }
