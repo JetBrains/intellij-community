a6826086c85218f45507cb3dc166334927d33631
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2021-06-23 01:23:12 +0300

    OC-21807: add trace logging to catch who closes a popup
    idea.log will contain the stack trace with `#com.intellij.ui.popup.AbstractPopup:trace`:
    2021-06-23 01:13:50,194 [18543812]  TRACE - ntellij.ui.popup.AbstractPopup -
    java.lang.Exception: cancel popup stack trace
    	at com.intellij.ui.popup.AbstractPopup.cancel(AbstractPopup.java:745)
    	at com.intellij.ui.popup.AbstractPopup.cancel(AbstractPopup.java:727)
    	at com.intellij.ui.LightweightHint.hide(LightweightHint.java:376)
    	at com.intellij.ui.LightweightHint.hide(LightweightHint.java:366)
    	at com.intellij.codeInsight.lookup.impl.LookupImpl.doHide(LookupImpl.java:1110)
    	at com.intellij.codeInsight.lookup.impl.LookupImpl.hideLookup(LookupImpl.java:1096)
    	at com.intellij.codeInsight.lookup.impl.LookupImpl.hide(LookupImpl.java:1087)
    	at com.intellij.codeInsight.hint.HintManagerImpl.hideHints(HintManagerImpl.java:1001)
    	at com.intellij.codeInsight.hint.EscapeHandler.doExecute(EscapeHandler.java:33)
    
    GitOrigin-RevId: 41f904bc79a2e4e4362463c2112435f13fb7e5ac

--- platform/platform-impl/src/com/intellij/ui/popup/AbstractPopup.java
+++ platform/platform-impl/src/com/intellij/ui/popup/AbstractPopup.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ui.popup;
 
 import com.intellij.codeInsight.hint.HintUtil;
@@ -741,6 +741,8 @@ public class AbstractPopup implements JBPopup, ScreenAreaConsumer {
     myState = State.CANCEL;
 
     if (isDisposed()) return;
+
+    if (LOG.isTraceEnabled()) LOG.trace(new Exception("cancel popup stack trace"));
 
     if (myPopup != null) {
       if (!canClose()) {
