6c9e148d09b279bfdc8a2f94130800c0c5fdc7f5
Aleksey Pivovarov <AMPivovarov@gmail.com>
2021-06-22 21:59:09 +0300

    IDEA-269501 git: show documentation link if git failed to sign commit
    GitOrigin-RevId: 76c7bc7cc1baf1915b6583cd87c4588a109005f6

--- plugins/git4idea/src/git4idea/checkin/GitRepositoryCommitter.kt
+++ plugins/git4idea/src/git4idea/checkin/GitRepositoryCommitter.kt
@@ -1,10 +1,15 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package git4idea.checkin
 
+import com.intellij.execution.process.ProcessOutputTypes
+import com.intellij.ide.BrowserUtil
+import com.intellij.notification.NotificationAction
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Key
 import com.intellij.openapi.vcs.VcsException
 import com.intellij.openapi.vcs.changes.CommitContext
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.vcs.commit.CommitExceptionWithActions
 import com.intellij.vcs.commit.isAmendCommitMode
 import com.intellij.vcs.commit.isCleanupCommitMessage
 import com.intellij.vcs.log.VcsUser
@@ -13,6 +18,8 @@ import git4idea.checkin.GitCheckinEnvironment.runWithMessageFile
 import git4idea.commands.Git
 import git4idea.commands.GitCommand
 import git4idea.commands.GitLineHandler
+import git4idea.commands.GitLineHandlerListener
+import git4idea.i18n.GitBundle
 import git4idea.repo.GitRepository
 import java.io.File
 import java.util.*
@@ -45,14 +52,28 @@ internal class GitRepositoryCommitter(val repository: GitRepository, val commitO
 
   @Throws(VcsException::class)
   fun commitStaged(messageFile: File) {
+    val gpgProblemDetector = GitGpgProblemDetector()
     val handler = GitLineHandler(project, root, GitCommand.COMMIT)
     handler.setStdoutSuppressed(false)
+    handler.addLineListener(gpgProblemDetector)
 
     handler.setCommitMessage(messageFile)
     handler.setCommitOptions(commitOptions)
     handler.endOptions()
 
-    Git.getInstance().runCommand(handler).throwOnError()
+    val command = Git.getInstance().runCommand(handler)
+
+    try {
+      command.throwOnError()
+    }
+    catch (e: VcsException) {
+      if (gpgProblemDetector.isDetected) {
+        throw GitGpgCommitException(e)
+      }
+      else {
+        throw e
+      }
+    }
   }
 }
 
@@ -69,4 +90,26 @@ private fun GitLineHandler.setCommitOptions(options: GitCommitOptions) {
 private fun GitLineHandler.setCommitMessage(messageFile: File) {
   addParameters("-F")
   addAbsoluteFile(messageFile)
+}
+
+private class GitGpgProblemDetector : GitLineHandlerListener {
+  var isDetected = false
+    private set
+
+  override fun onLineAvailable(line: String, outputType: Key<*>) {
+    if (outputType === ProcessOutputTypes.STDERR && line.contains(PATTERN)) {
+      isDetected = true
+    }
+  }
+
+  companion object {
+    private const val PATTERN = "gpg failed to sign the data"
+  }
+}
+
+private class GitGpgCommitException(cause: VcsException) : VcsException(cause), CommitExceptionWithActions {
+  override val actions: List<NotificationAction>
+    get() = listOf(NotificationAction.createSimple(GitBundle.message("gpg.error.see.documentation.link.text")) {
+      BrowserUtil.browse(GitBundle.message("gpg.jb.manual.link"))
+    })
 }
