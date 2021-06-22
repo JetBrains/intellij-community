b0c6eaa2ba82d371c7815bcdb506d34263ce8d12
Anna Kozlova <anna.kozlova@jetbrains.com>
2021-06-22 20:03:22 +0200

    [global inspections] log file name in headless mode
    GitOrigin-RevId: e08b5dedffb2970f563a4eff0f210059cdd10e43

--- platform/analysis-impl/src/com/intellij/codeInspection/reference/RefManagerImpl.java
+++ platform/analysis-impl/src/com/intellij/codeInspection/reference/RefManagerImpl.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.codeInspection.reference;
 
@@ -527,7 +527,12 @@ public class RefManagerImpl extends RefManager {
           throw e;
         }
         catch (Throwable e) {
-          LOG.error(new RuntimeExceptionWithAttachments(e, new Attachment("diagnostics.txt", file.getName())));
+          if (ApplicationManager.getApplication().isHeadlessEnvironment()) {
+            LOG.error(file.getName(), e);
+          }
+          else {
+            LOG.error(new RuntimeExceptionWithAttachments(e, new Attachment("diagnostics.txt", file.getName())));
+          }
         }
       }
       myPsiManager.dropResolveCaches();
