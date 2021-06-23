4234f5f3a2bf8f09c7e3049d30241e67a8b89279
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-06-23 17:38:57 +0200

    register `ImportUsageFilteringRuleProvider` in `LangExtensions.xml` once
    GitOrigin-RevId: 7891d77f5867d852112cbbe366a63d8efa9e4929

--- platform/usageView/src/com/intellij/usages/impl/ImportUsageFilteringRuleProvider.java
+++ platform/usageView/src/com/intellij/usages/impl/ImportUsageFilteringRuleProvider.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.usages.impl;
 
 import com.intellij.icons.AllIcons;
@@ -32,7 +32,7 @@ public final class ImportUsageFilteringRuleProvider implements UsageFilteringRul
 
   @Override
   public AnAction @NotNull [] createFilteringActions(@NotNull UsageView view) {
-    if (view.getPresentation().isCodeUsages()) {
+    if (view.getPresentation().isCodeUsages() && ImportFilteringRule.EP_NAME.hasAnyExtensions()) {
       JComponent component = view.getComponent();
       UsageViewImpl impl = (UsageViewImpl)view;
       ShowImportsAction showImportsAction = new ShowImportsAction();
