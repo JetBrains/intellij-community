d343503ce39fea3579af5329af99c58809c07baa
Mikhail Mazurkevich <mikhail.mazurkevich@jetbrains.com>
2021-06-23 12:46:12 +0300

    [workspace model] IDEA-271843 Fix registration of `ArtifactManagerBridge`
    GitOrigin-RevId: a2ee259554545d641d83ad5ee45db0ddf2cc6042

--- java/compiler/impl/src/com/intellij/packaging/impl/artifacts/workspacemodel/LegacyBridgeArtifactManagerRedefiner.kt
+++ java/compiler/impl/src/com/intellij/packaging/impl/artifacts/workspacemodel/LegacyBridgeArtifactManagerRedefiner.kt
@@ -1,20 +1,20 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.workspaceModel.ide.impl.legacyBridge
+package com.intellij.packaging.impl.artifacts.workspacemodel
 
-import com.intellij.ide.plugins.IdeaPluginDescriptor
 import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.openapi.components.ServiceDescriptor
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.impl.ProjectServiceContainerCustomizer
+import com.intellij.packaging.artifacts.ArtifactManager
 import com.intellij.serviceContainer.ComponentManagerImpl
 import com.intellij.workspaceModel.ide.WorkspaceModel
 import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Internal
-class LegacyBridgeProjectLifecycleListener : ProjectServiceContainerCustomizer {
+class LegacyBridgeArtifactManagerRedefiner : ProjectServiceContainerCustomizer {
   companion object {
-    private val LOG = logger<LegacyBridgeProjectLifecycleListener>()
+    private val LOG = logger<LegacyBridgeArtifactManagerRedefiner>()
   }
 
   override fun serviceRegistered(project: Project) {
@@ -25,19 +25,8 @@ class LegacyBridgeProjectLifecycleListener : ProjectServiceContainerCustomizer {
 
     if (WorkspaceModel.enabledForArtifacts) {
       val container = project as ComponentManagerImpl
-      registerArtifactManager(container, pluginDescriptor)
-    }
-  }
-
-  private fun registerArtifactManager(container: ComponentManagerImpl, pluginDescriptor: IdeaPluginDescriptor) {
-    try { //todo improve
-      val apiClass = Class.forName("com.intellij.packaging.artifacts.ArtifactManager", true, javaClass.classLoader)
-      val implClass = Class.forName("com.intellij.packaging.impl.artifacts.workspacemodel.ArtifactManagerBridge", true,
-                                    javaClass.classLoader)
-      container.registerService(apiClass, implClass, pluginDescriptor, override = true, preloadMode = ServiceDescriptor.PreloadMode.AWAIT)
-    }
-    catch (ignored: Throwable) {
-      LOG.warn(ignored)
+      container.registerService(ArtifactManager::class.java, ArtifactManagerBridge::class.java, pluginDescriptor, override = true,
+                                preloadMode = ServiceDescriptor.PreloadMode.AWAIT)
     }
   }
 }
