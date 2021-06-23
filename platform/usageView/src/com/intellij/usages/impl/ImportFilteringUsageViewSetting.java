5e87e36e23572c80b50735c2cad295be500a983d
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-06-23 17:30:36 +0200

    convert `ImportFilteringUsageViewSetting` to a light service
    GitOrigin-RevId: 9a6d220106e1c1d07e792c566c7956413df386c3

--- platform/usageView/src/com/intellij/usages/impl/ImportFilteringUsageViewSetting.java
+++ platform/usageView/src/com/intellij/usages/impl/ImportFilteringUsageViewSetting.java
@@ -1,15 +1,18 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.usages.impl;
 
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.Service;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
 import com.intellij.util.xmlb.XmlSerializerUtil;
 import org.jetbrains.annotations.NotNull;
 
+@Service
 @State(name = "ImportFilteringUsageViewSetting", storages = @Storage("usageView.xml"))
-public class ImportFilteringUsageViewSetting implements PersistentStateComponent<ImportFilteringUsageViewSetting> {
+public final class ImportFilteringUsageViewSetting implements PersistentStateComponent<ImportFilteringUsageViewSetting> {
+
   public static ImportFilteringUsageViewSetting getInstance() {
     return ApplicationManager.getApplication().getService(ImportFilteringUsageViewSetting.class);
   }
