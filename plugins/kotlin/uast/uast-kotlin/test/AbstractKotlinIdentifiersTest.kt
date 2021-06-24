243cac6769b690d489ff9a173d519225ba0c3365
Alexander Dudinsky <Alexander.Dudinsky@jetbrains.com>
2021-06-24 10:32:50 +0300

    Fix `KotlinUastIdentifiersTest`
    GitOrigin-RevId: 6c9886cafa6611ec0d2238dffe3ebca628fd0653

--- plugins/kotlin/uast/uast-kotlin/test/AbstractKotlinIdentifiersTest.kt
+++ plugins/kotlin/uast/uast-kotlin/test/AbstractKotlinIdentifiersTest.kt
@@ -1,11 +1,14 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.uast.test.kotlin
 
-import org.jetbrains.uast.UFile
+import com.intellij.psi.PsiElement
 import org.jetbrains.uast.test.common.kotlin.IdentifiersTestBase
 import com.intellij.testFramework.assertEqualsToFile
-import org.jetbrains.uast.test.common.asRefNames
+import org.jetbrains.uast.*
+import org.jetbrains.uast.test.common.UElementToParentMap
+import org.jetbrains.uast.test.common.visitUFileAndGetResult
 import java.io.File
+import kotlin.test.assertNotNull
 
 abstract class AbstractKotlinIdentifiersTest : AbstractKotlinUastTest(), IdentifiersTestBase {
 
@@ -19,3 +22,16 @@ abstract class AbstractKotlinIdentifiersTest : AbstractKotlinUastTest(), Identif
         assertEqualsToFile("refNames", getTestFile(testName, "refNames.txt"), file.asRefNames())
     }
 }
+
+private fun refNameRetriever(psiElement: PsiElement): UElement? =
+    when (val uElement = psiElement.toUElementOfExpectedTypes(UCallExpression::class.java, UReferenceExpression::class.java)) {
+        is UReferenceExpression -> uElement.referenceNameElement
+        is UCallExpression -> uElement.classReference?.referenceNameElement
+        else -> null
+    }?.also {
+        assertNotNull(it.sourcePsi, "referenceNameElement should have physical source, origin = $psiElement")
+    }
+
+fun UFile.asRefNames() = object : UElementToParentMap(::refNameRetriever) {
+    override fun renderSource(element: PsiElement): String = element.javaClass.simpleName
+}.visitUFileAndGetResult(this)
