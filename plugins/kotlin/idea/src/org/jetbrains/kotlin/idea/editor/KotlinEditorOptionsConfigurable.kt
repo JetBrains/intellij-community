329de088d7dc918f136e1b8a13a992de57a99b7c
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2021-06-24 17:25:18 +0200

    Provide search.topHitProvider for Kotlin editor options
    Relates to #KTIJ-18945
    
    GitOrigin-RevId: b95e738ad8b09109f1ca8f863293b7cf153ef4a0

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/editor/KotlinEditorOptionsConfigurable.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/editor/KotlinEditorOptionsConfigurable.kt
@@ -1,33 +1,71 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.kotlin.idea.editor
 
-package org.jetbrains.kotlin.idea.editor;
+import com.intellij.application.options.editor.*
+import com.intellij.openapi.options.BeanConfigurable
+import com.intellij.openapi.options.UnnamedConfigurable
+import com.intellij.ui.IdeBorderFactory
+import com.intellij.ui.layout.*
+import org.jetbrains.annotations.NonNls
+import org.jetbrains.kotlin.idea.KotlinBundle.message
+import javax.swing.JComponent
 
-import com.intellij.openapi.options.BeanConfigurable;
-import com.intellij.openapi.options.UnnamedConfigurable;
-import com.intellij.ui.IdeBorderFactory;
-import org.jetbrains.kotlin.idea.KotlinBundle;
+private val editorOptions = KotlinEditorOptions.getInstance()
 
-import javax.swing.*;
+private val cbConvertPastedJavaToKotlin
+    get() = CheckboxDescriptor(
+        message("editor.checkbox.title.convert.pasted.java.code.to.kotlin"),
+        PropertyBinding(
+            editorOptions::isEnableJavaToKotlinConversion,
+            editorOptions::setEnableJavaToKotlinConversion
+        )
+    )
 
-public class KotlinEditorOptionsConfigurable extends BeanConfigurable<KotlinEditorOptions> implements UnnamedConfigurable {
-    public KotlinEditorOptionsConfigurable() {
-        super(KotlinEditorOptions.getInstance());
-        KotlinEditorOptions instance = KotlinEditorOptions.getInstance();
-        checkBox(KotlinBundle.message("editor.checkbox.title.convert.pasted.java.code.to.kotlin"),
-                 instance::isEnableJavaToKotlinConversion,
-                 instance::setEnableJavaToKotlinConversion);
-        checkBox(KotlinBundle.message("editor.checkbox.title.don.t.show.java.to.kotlin.conversion.dialog.on.paste"),
-                 instance::isDonTShowConversionDialog,
-                 instance::setDonTShowConversionDialog);
-        checkBox(KotlinBundle.message("editor.checkbox.title.auto.add.val.keyword.to.data.inline.class.constructor.parameters"),
-                 instance::isAutoAddValKeywordToDataClassParameters,
-                 instance::setAutoAddValKeywordToDataClassParameters);
+private val cbDontShowJavaToKotlinConversionDialog
+    get() = CheckboxDescriptor(
+        message("editor.checkbox.title.don.t.show.java.to.kotlin.conversion.dialog.on.paste"),
+        PropertyBinding(
+            editorOptions::isDonTShowConversionDialog,
+            editorOptions::setDonTShowConversionDialog
+        )
+    )
+
+private val cbAutoAddValKeywordToCtorParameters
+    get() = CheckboxDescriptor(
+        message("editor.checkbox.title.auto.add.val.keyword.to.data.inline.class.constructor.parameters"),
+        PropertyBinding(
+            editorOptions::isAutoAddValKeywordToDataClassParameters,
+            editorOptions::setAutoAddValKeywordToDataClassParameters
+        )
+    )
+
+@NonNls
+const val ID = "editor.title.kotlin"
+
+internal val kotlinEditorOptionsDescriptors
+    get() = sequenceOf(
+        cbConvertPastedJavaToKotlin,
+        cbDontShowJavaToKotlinConversionDialog,
+        cbAutoAddValKeywordToCtorParameters
+    ).map(CheckboxDescriptor::asUiOptionDescriptor)
+
+class KotlinEditorOptionsConfigurable : BeanConfigurable<KotlinEditorOptions?>(KotlinEditorOptions.getInstance()),
+                                        UnnamedConfigurable {
+    override fun createComponent(): JComponent {
+        val component = super.createComponent()!!
+        component.border = IdeBorderFactory.createTitledBorder(message(ID))
+        return component
     }
 
-    @Override
-    public JComponent createComponent() {
-        JComponent component = super.createComponent();
-        component.setBorder(IdeBorderFactory.createTitledBorder(KotlinBundle.message("editor.title.kotlin")));
-        return component;
+    init {
+        checkBox(cbConvertPastedJavaToKotlin)
+        checkBox(cbDontShowJavaToKotlinConversionDialog)
+        checkBox(cbAutoAddValKeywordToCtorParameters)
+    }
+
+    private fun checkBox(checkboxDescriptor: CheckboxDescriptor) {
+        checkBox(checkboxDescriptor.name, { checkboxDescriptor.binding.get() }) {
+            checkboxDescriptor.binding.set(it)
+        }
     }
 }
