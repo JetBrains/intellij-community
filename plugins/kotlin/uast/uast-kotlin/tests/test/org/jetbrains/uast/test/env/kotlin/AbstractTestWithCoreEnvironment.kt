b4770a0c4e78db2b6adbbdc7ff8bdb13e3d6ceb7
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-06-24 15:14:05 +0200

    FIR IDE: fix uast test failing because of unregistered service
    GitOrigin-RevId: 8d448b98f5c7021f3147537647e02e6a04aade3b

--- plugins/kotlin/uast/uast-kotlin/tests/test/org/jetbrains/uast/test/env/kotlin/AbstractTestWithCoreEnvironment.kt
+++ plugins/kotlin/uast/uast-kotlin/tests/test/org/jetbrains/uast/test/env/kotlin/AbstractTestWithCoreEnvironment.kt
@@ -12,6 +12,7 @@ import com.intellij.psi.PsiNameHelper
 import com.intellij.psi.impl.PsiNameHelperImpl
 import com.intellij.rt.execution.junit.FileComparisonFailure
 import junit.framework.TestCase
+import org.jetbrains.uast.UastContext
 import org.jetbrains.uast.UastLanguagePlugin
 import org.jetbrains.uast.evaluation.UEvaluatorExtension
 import org.jetbrains.uast.java.JavaUastLanguagePlugin
@@ -57,6 +58,7 @@ abstract class AbstractTestWithCoreEnvironment : TestCase() {
         )
 
         project.registerService(PsiNameHelper::class.java, PsiNameHelperImpl(project))
+        project.registerService(UastContext::class.java)
         registerUastLanguagePlugins()
     }
 
