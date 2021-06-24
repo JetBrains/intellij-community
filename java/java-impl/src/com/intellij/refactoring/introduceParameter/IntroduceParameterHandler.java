e8235d71b22d16dc7a60193c8e57c91eaa4c515c
Anna Kozlova <anna.kozlova@jetbrains.com>
2021-06-24 14:26:25 +0200

    [java introduce parameter] don't force final for sub expressions (IDEA-272361)
    GitOrigin-RevId: a428e90939fc1a7c40e5557782d55c11d0bc329d

--- java/java-impl/src/com/intellij/refactoring/introduceParameter/IntroduceParameterHandler.java
+++ java/java-impl/src/com/intellij/refactoring/introduceParameter/IntroduceParameterHandler.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.refactoring.introduceParameter;
 
 import com.google.common.annotations.VisibleForTesting;
@@ -406,7 +406,7 @@ public class IntroduceParameterHandler extends IntroduceHandlerBase {
         mustBeFinal = parent != null && PsiTreeUtil.getParentOfType(parent, PsiClass.class, PsiMethod.class) != method;
       }
       for (PsiExpression occurrence : occurrences) {
-        if (PsiTreeUtil.getParentOfType(occurrence, PsiClass.class, PsiMethod.class) != method) {
+        if (occurrence.isPhysical() && PsiTreeUtil.getParentOfType(occurrence, PsiClass.class, PsiMethod.class) != method) {
           mustBeFinal = true;
           break;
         }
