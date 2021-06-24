05234f994e933bdcc26b9072e4244ee78453f945
Alexander Dudinsky <Alexander.Dudinsky@jetbrains.com>
2021-06-24 11:21:07 +0300

    Fix IntentionTestGenerated `Several .intention files are available for ... Please remove some of them`
    GitOrigin-RevId: 0d1b38726ba5cc370a317cf24e466da0e7e442ec

--- plugins/kotlin/idea/tests/testData/intentions/addLabeledReturnInLambda/convertToIndexedFunctionCall/basic/mapNotNull.kt
+++
@@ -1,7 +0,0 @@
-// INTENTION_TEXT: "Convert to 'mapIndexedNotNull'"
-// WITH_RUNTIME
-fun test(list: List<String?>) {
-    list.<caret>mapNotNull { s ->
-        s
-    }
-}
--- plugins/kotlin/idea/tests/testData/intentions/addLabeledReturnInLambda/convertToIndexedFunctionCall/basic/reduceOrNull.kt
+++
@@ -1,8 +0,0 @@
-// INTENTION_TEXT: "Convert to 'reduceIndexedOrNull'"
-// WITH_RUNTIME
-@OptIn(ExperimentalStdlibApi::class)
-fun test(list: List<String>) {
-    list.<caret>reduceOrNull { acc, s ->
-        acc + s
-    }
-}
