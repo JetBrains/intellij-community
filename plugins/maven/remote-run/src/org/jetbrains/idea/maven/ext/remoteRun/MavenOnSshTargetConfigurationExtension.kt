99a77b6dcef0609164e77a30abcae9095bc7184d
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2021-06-24 16:18:35 +0200

    extract intellij.maven.ext.remoteRun.xml
    GitOrigin-RevId: 1810ebcd5892be11f12c69c7b98056c2c6160ddf

--- plugins/maven/remote-run/src/org/jetbrains/idea/maven/ext/remoteRun/MavenOnSshTargetConfigurationExtension.kt
+++ plugins/maven/remote-run/src/org/jetbrains/idea/maven/ext/remoteRun/MavenOnSshTargetConfigurationExtension.kt
@@ -1,21 +1,21 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.idea.maven.execution.target
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.idea.maven.ext.remoteRun
 
-import com.intellij.execution.target.LanguageRuntimeType.VolumeDescriptor
+import com.intellij.execution.target.LanguageRuntimeType
 import com.intellij.execution.target.TargetEnvironment
-import com.intellij.execution.target.TargetEnvironment.UploadRoot
-import com.intellij.execution.target.TargetEnvironmentConfiguration
 import com.intellij.execution.target.TargetEnvironmentRequest
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.util.io.DigestUtil
 import com.jetbrains.plugins.remotesdk.target.ssh.target.SshTargetEnvironmentConfiguration
+import org.jetbrains.idea.maven.execution.target.MavenRuntimeTargetConfiguration
+import org.jetbrains.idea.maven.execution.target.TargetConfigurationMavenExtension
 import java.nio.file.Path
 
-class MavenOnSshTargetConfigurationExtension : TargetConfigurationMavenExtension {
+internal class MavenOnSshTargetConfigurationExtension : TargetConfigurationMavenExtension {
   override fun createUploadRoot(mavenRuntimeConfiguration: MavenRuntimeTargetConfiguration?,
                                 targetEnvironmentRequest: TargetEnvironmentRequest,
-                                volumeDescriptor: VolumeDescriptor,
-                                localRootPath: Path): UploadRoot? {
+                                volumeDescriptor: LanguageRuntimeType.VolumeDescriptor,
+                                localRootPath: Path): TargetEnvironment.UploadRoot? {
     val targetEnvironmentConfiguration = targetEnvironmentRequest.configuration
     if (targetEnvironmentConfiguration !is SshTargetEnvironmentConfiguration) return null
     if (!targetEnvironmentConfiguration.useRsync) return null
@@ -27,7 +27,7 @@ class MavenOnSshTargetConfigurationExtension : TargetConfigurationMavenExtension
     val targetRootParentDirName = localRootPath.sha256().take(16)
     val targetRootParentDir = applicationDir + fileSeparator + targetRootParentDirName
     val targetRootPath = TargetEnvironment.TargetPath.Temporary(volumeDescriptor.type.id, targetRootParentDir)
-    return UploadRoot(localRootPath, targetRootPath).also {
+    return TargetEnvironment.UploadRoot(localRootPath, targetRootPath).also {
       it.volumeData = mavenRuntimeConfiguration?.getTargetSpecificData(volumeDescriptor)
     }
   }
