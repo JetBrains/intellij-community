e27a053b825da7ca1bb64f8517f507a202edef4d
Alexander Dudinsky <Alexander.Dudinsky@jetbrains.com>
2021-06-24 14:32:06 +0300

    Fix LocalInspectionTestGenerated `Several .inspection files are available for ... Please remove some of them`
    GitOrigin-RevId: 7d4de52376021ac8fd5025c61c0f1a93a6400226

--- plugins/kotlin/idea/tests/testData/inspectionsLocal/prohibitUseSiteTargetAnnotationsOnSuperTypesMigration/replaceIsEmptyWithIfEmpty/intArray/isEmpty.kt
+++
@@ -1,9 +0,0 @@
-// PROBLEM: none
-// WITH_RUNTIME
-fun test(intArr: IntArray): IntArray {
-    return if (intArr.isEmpty<caret>()) {
-        intArrayOf(1)
-    } else {
-        intArr
-    }
-}
--- plugins/kotlin/idea/tests/testData/inspectionsLocal/prohibitUseSiteTargetAnnotationsOnSuperTypesMigration/replaceIsEmptyWithIfEmpty/str/isNotBlank.kt
+++
@@ -1,9 +0,0 @@
-// WITH_RUNTIME
-class Api(val name: String)
-
-fun test(api: Api) {
-    val name = if (api.name.isNotBlank<caret>())
-        api.name
-    else
-        "John"
-}
