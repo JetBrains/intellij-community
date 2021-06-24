d366328363aafbf95f702a5063452207228d36bb
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2021-06-24 19:45:05 +0200

    WIP plugins: use vendor and organization for filtering (ListPluginComponent misses the vendor field)
    GitOrigin-RevId: 3b5b925fa59ae7d6d2188a1fecdabfd2f1bca1f9

--- platform/platform-impl/src/com/intellij/ide/plugins/org/PluginManagerFilters.kt
+++ platform/platform-impl/src/com/intellij/ide/plugins/org/PluginManagerFilters.kt
@@ -85,7 +85,7 @@ private fun PluginManagerConfigurableForOrgStateRule.matches(descriptor: IdeaPlu
   }
 
   vendorRegex?.let {
-    if (!safeMatch(descriptor.vendor, it)) return false
+    if (!safeMatch(descriptor.vendor ?: descriptor.organization, it)) return false
   }
 
   versionRegex?.let {
