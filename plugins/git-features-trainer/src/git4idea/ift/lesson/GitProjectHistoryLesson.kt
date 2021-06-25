1a17dbb5e9bafcab46b576020147baf151e25664
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2021-06-25 16:29:30 +0300

    [IFT] Adapt Git lessons to editor and separate window Diff
    IDEA-CR-71690
    
    GitOrigin-RevId: b47379232365b0529ebfdd05c9088da4b6d90c81

--- plugins/git-features-trainer/src/git4idea/ift/lesson/GitProjectHistoryLesson.kt
+++ plugins/git-features-trainer/src/git4idea/ift/lesson/GitProjectHistoryLesson.kt
@@ -2,6 +2,7 @@
 package git4idea.ift.lesson
 
 import com.intellij.diff.tools.util.SimpleDiffPanel
+import com.intellij.openapi.vcs.changes.VcsEditorTabFilesManager
 import com.intellij.openapi.wm.ToolWindowId
 import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.ui.JBColor
@@ -131,6 +132,13 @@ class GitProjectHistoryLesson : GitLesson("Git.ProjectHistory", GitLessonsBundle
       }
       triggerByUiComponentAndHighlight(false, false) { _: SimpleDiffPanel -> true }
       showWarningIfGitWindowClosed()
+    }
+
+    if (VcsEditorTabFilesManager.getInstance().shouldOpenInNewWindow) {
+      task("EditorEscape") {
+        text(GitLessonsBundle.message("git.project.history.close.diff", action(it)))
+        stateCheck { previous.ui?.isShowing != true }
+      }
     }
 
     text(GitLessonsBundle.message("git.project.history.invitation.to.commit.lesson"))
