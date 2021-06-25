3101622e8070a4688959993f60c8594e56f2b0c1
Alexander Dudinsky <Alexander.Dudinsky@jetbrains.com>
2021-06-25 18:00:40 +0300

    Exclude ``*.fir.kt` files from `AbstractKeywordCompletionTest`. Regenerate tests
    GitOrigin-RevId: e9b5c37a9a5b299081776ea8762723b3a9a1a478

--- plugins/kotlin/generators/test/org/jetbrains/kotlin/testGenerator/GenerateTests.kt
+++ plugins/kotlin/generators/test/org/jetbrains/kotlin/testGenerator/GenerateTests.kt
@@ -1325,7 +1325,7 @@ private fun assembleWorkspace(): TWorkspace = workspace {
         }
 
         testClass<AbstractKeywordCompletionTest> {
-            model("keywords", isRecursive = false)
+            model("keywords", isRecursive = false, pattern = KT.withPrecondition(excludedFirPrecondition))
         }
 
         testClass<AbstractJvmWithLibBasicCompletionTest> {
