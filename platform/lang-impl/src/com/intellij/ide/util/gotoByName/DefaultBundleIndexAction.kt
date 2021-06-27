bad49c3ce722c47e7b15c112004afbf0b8021474
Dmitry.Krasilschikov <kradima@jetbrains.com>
2021-06-27 14:10:17 +0000

    IDEA-250323 - search actions by English pattern;
    don't repeat actions already extracted into bundles
    
    GitOrigin-RevId: db3f0f42ebfe2c0f343f05ff0b67ebdfab2cbf74

--- platform/lang-impl/src/com/intellij/ide/util/gotoByName/DefaultBundleIndexAction.kt
+++ platform/lang-impl/src/com/intellij/ide/util/gotoByName/DefaultBundleIndexAction.kt
@@ -1,11 +1,15 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.util.gotoByName
 
+import com.intellij.DynamicBundle
+import com.intellij.ide.plugins.PluginManagerCore
+import com.intellij.idea.ActionsBundle
 import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.actionSystem.ex.ActionUtil
 import com.intellij.openapi.actionSystem.impl.ActionManagerImpl
 import com.intellij.openapi.util.io.FileUtil
 import java.io.File
+import java.util.*
 
 class DefaultBundleIndexAction : AnAction() {
   override fun actionPerformed(e: AnActionEvent) {
@@ -21,16 +25,27 @@ class DefaultBundleIndexAction : AnAction() {
         ActionUtil.lastUpdateAndCheckDumb(it, event, false)
 
         val id = actionManager.getId(it)
-        val text = presentation.text
-        val description = presentation.description
         if (id.contains(":")) return@forEach
 
-        if (!text.isNullOrBlank()) {
-          file.appendText("action.$id.text=$text\n")
-        }
-        if (!description.isNullOrBlank()) {
-          file.appendText("action.$id.description=$description\n")
+        val pluginDescriptor = PluginManagerCore.getPluginDescriptorOrPlatformByClassName(it.javaClass.name)
+        val path = pluginDescriptor?.resourceBundleBaseName ?: ActionsBundle.IDEA_ACTIONS_BUNDLE
+        val bundle = DynamicBundle.INSTANCE.getResourceBundle(path, pluginDescriptor?.pluginClassLoader ?: it.javaClass.classLoader)
+
+        if (it !is ActionGroup) {
+          appendKey(bundle, file, "action.$id.text", presentation.text)
+          appendKey(bundle, file, "action.$id.description", presentation.description)
+        } else {
+          appendKey(bundle, file, "group.$id.text", presentation.text)
+          appendKey(bundle, file, "group.$id.description", presentation.description)
         }
       }
+  }
+
+  companion object {
+    private fun appendKey(bundle: ResourceBundle, file: File, key: String, string: String?) {
+      if (!bundle.containsKey(key) && !string.isNullOrBlank()) {
+        file.appendText("$key=$string\n")
+      }
+    }
   }
 }
