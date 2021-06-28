05a9cb41c515b2c5a0a3099ed97b54e4868ab3e4
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2021-06-28 13:52:14 +0000

    [kotlin] Fix parse short kotlin version
    Relates to #KTIJ-18938
    
    GitOrigin-RevId: 25f082f1f8218f8909d2479eaef6abc29f10e125

--- plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/compiler/configuration/Version.kt
+++ plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/compiler/configuration/Version.kt
@@ -4,6 +4,16 @@ package org.jetbrains.kotlin.idea.compiler.configuration
 import org.jetbrains.kotlin.config.KotlinCompilerVersion
 import org.jetbrains.kotlin.config.LanguageVersion
 
+fun LanguageVersion.coerceAtMostVersion(version: Version): LanguageVersion {
+    // 1.4.30+ and 1.5.30+ have full support of next language version
+    val languageVersion = if (version.major == 1 && (version.minor == 4 || version.minor == 5) && version.patch >= 30) {
+        Version.lookup(version.major, version.minor + 1)
+    } else {
+        version.languageVersion
+    }
+    return this.coerceAtMost(languageVersion)
+}
+
 class Version(val major: Int, val minor: Int, val patch: Int) {
     val languageVersion: LanguageVersion
         get() = lookup(major, minor)
@@ -21,17 +31,17 @@ class Version(val major: Int, val minor: Int, val patch: Int) {
     }
 
     companion object {
-        private val VERSION_REGEX = Regex("(\\d+)\\.(\\d+)\\.(\\d+).*")
+        private val VERSION_REGEX = Regex("(\\d+)\\.(\\d+)(\\.(\\d+).*)?")
         val CURRENT_VERSION = parse(KotlinCompilerVersion.VERSION)
 
-        private fun lookup(major: Int, minor: Int) =
+        internal fun lookup(major: Int, minor: Int) =
             LanguageVersion.values().firstOrNull { it.major == major && it.minor == minor } ?: LanguageVersion.LATEST_STABLE
 
         fun parse(version: String?): Version {
             version ?: return CURRENT_VERSION
             val matchEntire = VERSION_REGEX.matchEntire(version) ?: return CURRENT_VERSION
             val values = matchEntire.groupValues
-            return  Version(values[1].toInt(), values[2].toInt(), values[3].toInt())
+            return  Version(values[1].toInt(), values[2].toInt(), values[4].takeIf { it.isNotEmpty() }?.toInt() ?: 0)
         }
     }
 }
