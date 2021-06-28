31fb1d4e4916eae421ed17cec7d7157476294dfa
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-06-28 20:26:13 +0700

    KtArrayAccessReference: remove redundant functions
    GitOrigin-RevId: f84f476b4591b9c519c46b32e5c94feb43b3aa2e

--- plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/references/KtArrayAccessReferenceDescriptorsImpl.kt
+++ plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/references/KtArrayAccessReferenceDescriptorsImpl.kt
@@ -7,8 +7,6 @@ import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
 import org.jetbrains.kotlin.idea.core.canMoveLambdaOutsideParentheses
 import org.jetbrains.kotlin.idea.core.moveFunctionLiteralOutsideParentheses
 import org.jetbrains.kotlin.psi.KtArrayAccessExpression
-import org.jetbrains.kotlin.psi.KtCallExpression
-import org.jetbrains.kotlin.psi.KtExpression
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.BindingContext.INDEXED_LVALUE_GET
 import org.jetbrains.kotlin.resolve.BindingContext.INDEXED_LVALUE_SET
@@ -18,9 +16,7 @@ internal class KtArrayAccessReferenceDescriptorsImpl(
 ) : KtArrayAccessReference(expression), KtDescriptorsBasedReference {
     override fun isReferenceTo(element: PsiElement): Boolean = super<KtDescriptorsBasedReference>.isReferenceTo(element)
 
-    override fun handleElementRename(newElementName: String): PsiElement = doRenameImplicitConventionalCall(newElementName)
-
-    override fun doRenameImplicitConventionalCall(newName: String?): KtExpression = renameImplicitConventionalCall(newName)
+    override fun handleElementRename(newElementName: String): PsiElement = renameImplicitConventionalCall(newElementName)
 
     override fun getTargetDescriptors(context: BindingContext): Collection<DeclarationDescriptor> {
         val getFunctionDescriptor = context[INDEXED_LVALUE_GET, expression]?.candidateDescriptor
