7edf8d76f56081cbae13eaad477875b90ede3277
Aleksandr Krasilnikov <aleksandr.krasilnikov@jetbrains.com>
2021-06-28 12:53:33 +0000

    [github] Fix remote branch detection when creating PR
    fix for [IDEA-272485]
    introduced in [IDEA-267608] 62da51c72
    
    (cherry picked from commit b622718ab5425e9fef58c1df4b58b6839dca04ea)
    
    IJ-CR-10831
    
    GitOrigin-RevId: 8ad7cf68510165be13d0a67e71d488c7d1551dee

--- plugins/git4idea/src/git4idea/GitPushUtil.kt
+++ plugins/git4idea/src/git4idea/GitPushUtil.kt
@@ -11,6 +11,7 @@ import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.messages.MessagesService
 import com.intellij.openapi.util.NlsContexts
+import git4idea.branch.GitBranchUtil
 import git4idea.i18n.GitBundle
 import git4idea.push.GitPushOperation
 import git4idea.push.GitPushSource
@@ -95,7 +96,13 @@ object GitPushUtil {
   )
 
   @JvmStatic
-  fun findPushTarget(repository: GitRepository, remote: GitRemote, branch: GitLocalBranch): GitPushTarget? =
-    GitPushTarget.getFromPushSpec(repository, remote, branch)
+  fun findPushTarget(repository: GitRepository, remote: GitRemote, branch: GitLocalBranch): GitPushTarget? {
+    return GitPushTarget.getFromPushSpec(repository, remote, branch)
+           ?: GitBranchUtil.getTrackInfoForBranch(repository, branch)
+             ?.takeIf { it.remote == remote }
+             // use tracking information only for branches with the same local and remote name
+             ?.takeIf { it.remoteBranch.nameForRemoteOperations == branch.name }
+             ?.let { GitPushTarget(it.remoteBranch, false) }
+  }
 }
 
