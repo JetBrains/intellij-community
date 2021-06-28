8428c7d8668ed11fb3262311ead6f92f14b16aca
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2021-06-28 23:21:12 +0200

    [kotlin] Wrap containsKotlinFile with read action in smart mode
    #EA-267209 Fixed
    
    GitOrigin-RevId: 364a7f3c4b499ec634a661e77110fdbc848febfd

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/formatter/KotlinFormatterUsageCollector.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/formatter/KotlinFormatterUsageCollector.kt
@@ -15,6 +15,7 @@ import org.jetbrains.kotlin.idea.KotlinPluginUtil
 import org.jetbrains.kotlin.idea.PlatformVersion
 import org.jetbrains.kotlin.idea.formatter.KotlinFormatterUsageCollector.KotlinFormatterKind.*
 import org.jetbrains.kotlin.idea.search.containsKotlinFile
+import org.jetbrains.kotlin.idea.util.runReadActionInSmartMode
 
 class KotlinFormatterUsageCollector : ProjectUsagesCollector() {
     override fun requiresReadAccess() = true
@@ -22,7 +23,7 @@ class KotlinFormatterUsageCollector : ProjectUsagesCollector() {
     override fun getGroup(): EventLogGroup = GROUP
 
     override fun getMetrics(project: Project): Set<MetricEvent> {
-        if (PlatformVersion.isAndroidStudio() || !project.containsKotlinFile()) {
+        if (PlatformVersion.isAndroidStudio() || project.runReadActionInSmartMode { !project.containsKotlinFile() }) {
             return emptySet()
         }
 
