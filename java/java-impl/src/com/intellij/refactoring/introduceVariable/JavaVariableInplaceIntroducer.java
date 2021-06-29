bfd19b610a132cc86e1a2c5e42dafd679b3ad5ce
Anna Kozlova <anna.kozlova@jetbrains.com>
2021-06-29 12:58:49 +0200

    move slow operations away from EDT
    GitOrigin-RevId: 4b804222554580ffc1dcdf1b0572df2f9bbb89c8

--- java/java-impl/src/com/intellij/refactoring/introduceVariable/JavaVariableInplaceIntroducer.java
+++ java/java-impl/src/com/intellij/refactoring/introduceVariable/JavaVariableInplaceIntroducer.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.refactoring.introduceVariable;
 
 import com.intellij.codeInsight.intention.impl.TypeExpression;
@@ -21,6 +7,8 @@ import com.intellij.java.JavaBundle;
 import com.intellij.java.refactoring.JavaRefactoringBundle;
 import com.intellij.openapi.actionSystem.Shortcut;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ModalityState;
+import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Document;
@@ -50,6 +38,7 @@ import com.intellij.refactoring.rename.inplace.VariableInplaceRenamer;
 import com.intellij.refactoring.ui.TypeSelectorManagerImpl;
 import com.intellij.refactoring.util.RefactoringUtil;
 import com.intellij.ui.NonFocusableCheckBox;
+import com.intellij.util.concurrency.NonUrgentExecutor;
 import com.intellij.util.ui.JBUI;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -491,7 +480,13 @@ public class JavaVariableInplaceIntroducer extends AbstractJavaInplaceIntroducer
         SmartPsiElementPointer<PsiDeclarationStatement> pointer = smartPointerManager.createSmartPsiElementPointer(declarationStatement);
         myPointer = pointer;
         myEditor.putUserData(ReassignVariableUtil.DECLARATION_KEY, pointer);
-        setAdvertisementText(getAdvertisementText(declarationStatement, variable.getType(), myHasTypeSuggestion));
+        PsiType variableType = variable.getType();
+        ReadAction.nonBlocking(() -> {
+            PsiDeclarationStatement element = pointer.getElement();
+            return element != null ? getAdvertisementText(element, variableType, myHasTypeSuggestion) : null;
+          })
+          .finishOnUiThread(ModalityState.NON_MODAL, text -> setAdvertisementText(text))
+          .submit(NonUrgentExecutor.getInstance());
       }
     }
 
