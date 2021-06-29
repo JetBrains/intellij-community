77981e5976f506d40286f82252f4a6b881caecb3
Peter Gromov <peter@jetbrains.com>
2021-06-29 13:45:36 +0000

    [grazie+properties] join comment texts
    GitOrigin-RevId: f19ccad740d840df5372efeb712a01e795aac6fd

--- plugins/grazie/properties/src/main/kotlin/com/intellij/grazie/ide/language/properties/PropertyTextExtractor.java
+++ plugins/grazie/properties/src/main/kotlin/com/intellij/grazie/ide/language/properties/PropertyTextExtractor.java
@@ -4,24 +4,30 @@ import com.intellij.grazie.text.TextContent;
 import com.intellij.grazie.text.TextContentBuilder;
 import com.intellij.grazie.text.TextExtractor;
 import com.intellij.grazie.utils.HtmlUtilsKt;
+import com.intellij.grazie.utils.PsiUtilsKt;
 import com.intellij.lang.properties.parsing.PropertiesTokenTypes;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiComment;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.PsiUtilCore;
+import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
+import static com.intellij.grazie.text.TextContent.TextDomain.COMMENTS;
 
 public class PropertyTextExtractor extends TextExtractor {
   @Override
   public @Nullable TextContent buildTextContent(@NotNull PsiElement root,
                                                 @NotNull Set<TextContent.TextDomain> allowedDomains) {
     if (root instanceof PsiComment) {
-      return TextContentBuilder.FromPsi.removingIndents(" \t#").build(root, TextContent.TextDomain.COMMENTS);
+      List<PsiElement> roots = PsiUtilsKt.getNotSoDistantSimilarSiblings(root, PropertiesTokenTypes.WHITESPACES, e ->
+        PropertiesTokenTypes.COMMENTS.contains(PsiUtilCore.getElementType(e)));
+      return TextContent.joinWithWhitespace(ContainerUtil.mapNotNull(roots, c ->
+        TextContentBuilder.FromPsi.removingIndents(" \t#!").build(c, COMMENTS)));
     }
     if (PsiUtilCore.getElementType(root) == PropertiesTokenTypes.VALUE_CHARACTERS) {
       TextContent content = TextContent.psiFragment(TextContent.TextDomain.PLAIN_TEXT, root);
