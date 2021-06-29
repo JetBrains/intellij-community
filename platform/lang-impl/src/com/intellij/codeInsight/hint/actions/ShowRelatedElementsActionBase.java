09b80045282a37c90b2a80b44fe64b1f23847e54
Vassiliy.Kudryashov <vassiliy.kudryashov@jetbrains.com>
2021-06-29 17:48:03 +0000

    Replace anonymous class with lambda
    GitOrigin-RevId: 90775d7e48afd7ad1be92d2b54f044e0f9d57ec3

--- platform/lang-impl/src/com/intellij/codeInsight/hint/actions/ShowRelatedElementsActionBase.java
+++ platform/lang-impl/src/com/intellij/codeInsight/hint/actions/ShowRelatedElementsActionBase.java
@@ -11,7 +11,6 @@ import com.intellij.codeInsight.navigation.BackgroundUpdaterTaskBase;
 import com.intellij.codeInsight.navigation.ImplementationSearcher;
 import com.intellij.featureStatistics.FeatureUsageTracker;
 import com.intellij.ide.DataManager;
-import com.intellij.openapi.Disposable;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.DataContext;
@@ -190,12 +189,7 @@ public abstract class ShowRelatedElementsActionBase extends DumbAwareAction impl
         });
       WindowMoveListener listener = new WindowMoveListener();
       listener.installTo(component);
-      Disposer.register(session, new Disposable() {
-        @Override
-        public void dispose() {
-          listener.uninstallFrom(component);
-        }
-      });
+      Disposer.register(session, () -> listener.uninstallFrom(component));
       popup = popupBuilder.createPopup();
 
       updateInBackground(session, component, (AbstractPopup)popup, usageView);
