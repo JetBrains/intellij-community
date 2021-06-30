7606013b47167d5dac829ab4e810cc592a752da4
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2021-06-30 13:18:40 +0300

    IDEA-272610 Apply is enabled on open Run/Debug dialog with Application run configuration
    comparing to snapshot restored
    
    GitOrigin-RevId: 9547859db7492a660432f375947caa4f1d86264a

--- platform/execution-impl/src/com/intellij/execution/impl/BaseRCSettingsConfigurable.java
+++ platform/execution-impl/src/com/intellij/execution/impl/BaseRCSettingsConfigurable.java
@@ -1,10 +1,12 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.impl;
 
+import com.intellij.configurationStore.SerializableScheme;
 import com.intellij.execution.RunnerAndConfigurationSettings;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.options.SettingsEditor;
 import com.intellij.openapi.options.SettingsEditorConfigurable;
+import com.intellij.openapi.util.JDOMUtil;
 import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.scale.JBUIScale;
 import com.intellij.util.ui.JBUI;
@@ -36,12 +38,11 @@ abstract class BaseRCSettingsConfigurable extends SettingsEditorConfigurable<Run
         return false;
       }
 
-      RunnerAndConfigurationSettings snapshot = getEditor().getSnapshot();
-      if (isSpecificallyModified() ||
-          !RunManagerImplKt.doGetBeforeRunTasks(original.getConfiguration())
-            .equals(RunManagerImplKt.doGetBeforeRunTasks(snapshot.getConfiguration()))) {
+      if (isSpecificallyModified()) {
         return true;
       }
+      RunnerAndConfigurationSettings snapshot = getEditor().getSnapshot();
+      return !JDOMUtil.areElementsEqual(((SerializableScheme)original).writeScheme(), ((SerializableScheme)snapshot).writeScheme());
     }
     catch (ConfigurationException e) {
       //ignore
