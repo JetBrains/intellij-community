6fd6c158d62e436df54fdec5168f124709fc16fe
Dmitry.Krasilschikov <kradima@jetbrains.com>
2021-06-30 20:27:59 +0300

    load inspection descriptions from language plugins
    GitOrigin-RevId: 648bb64d52af992267d83c79ac015eceb1d1ac7b

--- platform/lang-impl/src/com/intellij/codeInsight/intention/impl/config/ResourceTextDescriptor.java
+++ platform/lang-impl/src/com/intellij/codeInsight/intention/impl/config/ResourceTextDescriptor.java
@@ -1,13 +1,18 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.intention.impl.config;
 
+import com.intellij.DynamicBundle;
+import com.intellij.openapi.extensions.PluginDescriptor;
 import com.intellij.openapi.util.text.Strings;
 import com.intellij.util.ResourceUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Objects;
+
+import static com.intellij.DynamicBundle.findLanguageBundle;
 
 final class ResourceTextDescriptor implements TextDescriptor {
   private final ClassLoader myLoader;
@@ -35,11 +40,26 @@ final class ResourceTextDescriptor implements TextDescriptor {
   @NotNull
   @Override
   public String getText() throws IOException {
-    InputStream stream = myLoader.getResourceAsStream(myResourcePath);
+    InputStream stream = getStream();
+
     if (stream == null) {
       throw new IOException("Resource not found: " + myResourcePath);
     }
     return ResourceUtil.loadText(stream); //NON-NLS
+  }
+
+  @Nullable
+  private InputStream getStream() {
+    InputStream stream = myLoader.getResourceAsStream(myResourcePath);
+
+    DynamicBundle.LanguageBundleEP langBundle = findLanguageBundle();
+    if (langBundle == null) return stream;
+
+    PluginDescriptor descriptor = langBundle.pluginDescriptor;
+    if (descriptor == null) return stream;
+
+    ClassLoader classLoader = descriptor.getPluginClassLoader();
+    return classLoader != null ? classLoader.getResourceAsStream(myResourcePath) : stream;
   }
 
   @NotNull
