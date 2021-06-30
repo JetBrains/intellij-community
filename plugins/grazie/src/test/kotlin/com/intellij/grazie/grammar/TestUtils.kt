557cb99f775ade0630054937a10b06249196aaf1
Peter Gromov <peter@jetbrains.com>
2021-06-30 09:43:00 +0200

    [grazie] improve test failure messages
    GitOrigin-RevId: 58ee442d09f2a995a3faa7a31668c1fb0f346876

--- plugins/grazie/src/test/kotlin/com/intellij/grazie/grammar/TestUtils.kt
+++ plugins/grazie/src/test/kotlin/com/intellij/grazie/grammar/TestUtils.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.grazie.grammar
 
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 fun assertIsEmpty(collection: Collection<*>) {
@@ -8,7 +9,7 @@ fun assertIsEmpty(collection: Collection<*>) {
 }
 
 internal fun LanguageToolChecker.Problem.assertTypoIs(range: IntRange, fixes: List<String> = emptyList()) {
-  assertTrue { highlightRange.equalsToRange(range.first, range.last + 1) }
+  assertEquals(range, highlightRange.startOffset until highlightRange.endOffset)
   assertTrue { corrections.containsAll(fixes) }
 }
 
