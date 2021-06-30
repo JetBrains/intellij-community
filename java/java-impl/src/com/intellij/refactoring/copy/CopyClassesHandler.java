41519cecf8a6d499fa8d4d6932317bb21341cda7
Anna Kozlova <anna.kozlova@jetbrains.com>
2021-06-30 17:32:10 +0000

    EA-323494 (plugin) - NPE: CopyClassesHandler.getNewFileName
    GitOrigin-RevId: 006cfc131221d0ebd2742772dad9d2b945e1990e

--- java/java-impl/src/com/intellij/refactoring/copy/CopyClassesHandler.java
+++ java/java-impl/src/com/intellij/refactoring/copy/CopyClassesHandler.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.refactoring.copy;
 
 import com.intellij.codeInsight.actions.OptimizeImportsProcessor;
@@ -448,7 +448,7 @@ public class CopyClassesHandler extends CopyHandlerDelegateBase {
   private static String getNewFileName(PsiFile file, String name) {
     if (name != null) {
       String fileExtension = getFileExtension(file);
-      return fileExtension.isEmpty() ? name : StringUtil.getQualifiedName(name, fileExtension);
+      return StringUtil.isEmpty(fileExtension) ? name : StringUtil.getQualifiedName(name, fileExtension);
     }
     return file.getName();
   }
