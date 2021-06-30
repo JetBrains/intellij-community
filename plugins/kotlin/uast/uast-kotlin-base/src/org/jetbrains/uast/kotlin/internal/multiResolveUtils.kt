e6325c7d75cfd63d98a3a00f8a3ab8bdf9b29f04
Jinseong Jeon <jsjeon@google.com>
2021-06-30 00:19:16 -0700

    FIR/UAST: merge multi resolve utils
    GitOrigin-RevId: 9cfb9c21ed7527b622d0af1b9f41e6bddf4326fc

--- plugins/kotlin/uast/uast-kotlin-base/src/org/jetbrains/uast/kotlin/internal/multiResolveUtils.kt
+++ plugins/kotlin/uast/uast-kotlin-base/src/org/jetbrains/uast/kotlin/internal/multiResolveUtils.kt
@@ -2,9 +2,11 @@
 
 package org.jetbrains.uast.kotlin.internal
 
+import com.intellij.psi.PsiPolyVariantReference
 import com.intellij.psi.PsiSubstitutor
 import com.intellij.psi.ResolveResult
 import com.intellij.psi.infos.CandidateInfo
+import org.jetbrains.kotlin.psi.KtElement
 import org.jetbrains.kotlin.psi.KtExpression
 import org.jetbrains.uast.kotlin.BaseKotlinUastResolveProviderService
 
@@ -19,3 +21,10 @@ fun getResolveResultVariants(
     return referenceVariants.mapNotNull { CandidateInfo(it, PsiSubstitutor.EMPTY) }.asIterable()
 }
 
+fun KtElement.multiResolveResults(): Sequence<ResolveResult> =
+    references.asSequence().flatMap { ref ->
+        when (ref) {
+            is PsiPolyVariantReference -> ref.multiResolve(false).asSequence()
+            else -> (ref.resolve()?.let { sequenceOf(CandidateInfo(it, PsiSubstitutor.EMPTY)) }).orEmpty()
+        }
+    }
