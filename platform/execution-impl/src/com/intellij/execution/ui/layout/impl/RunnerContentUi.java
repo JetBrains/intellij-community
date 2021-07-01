461b6b4444fad1be30e8d58a916c736452517ad4
maksim.pelevin <maksim.pelevin@jetbrains.com>
2021-07-01 20:19:55 +0000

    [debugger] Restore removeNotify logic and add a flag to prevent messy remove (IDEA-272108)
    GitOrigin-RevId: 26a429aae5c00d63a00778169c68a4ec73753510

--- platform/execution-impl/src/com/intellij/execution/ui/layout/impl/RunnerContentUi.java
+++ platform/execution-impl/src/com/intellij/execution/ui/layout/impl/RunnerContentUi.java
@@ -1655,6 +1655,20 @@ public final class RunnerContentUi implements ContentUI, Disposable, CellTransfo
         }));
       }
     }
+
+    @Override
+    public void removeNotify() {
+      super.removeNotify();
+      if (!ScreenUtil.isStandardAddRemoveNotify(this)) {
+        return;
+      }
+
+      if (Disposer.isDisposed(RunnerContentUi.this)) return;
+
+      if (myWasEverAdded) {
+        saveUiState();
+      }
+    }
   }
 
   @SuppressWarnings("SSBasedInspection")
