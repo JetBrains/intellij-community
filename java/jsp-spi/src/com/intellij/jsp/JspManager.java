7d190a3e686dd124e8e0a64ae95b0481d783465f
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2021-07-02 08:32:33 +0200

    move JspManager and JspContextManager from com.intellij.psi.impl.source.jsp to com.intellij.jsp Vladimir Krivosheev Today 08:32
    GitOrigin-RevId: 026cc4b03b3fb977feb38b85775e938070bbcb51

--- java/jsp-spi/src/com/intellij/jsp/JspManager.java
+++ java/jsp-spi/src/com/intellij/jsp/JspManager.java
@@ -1,5 +1,5 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.psi.impl.source.jsp;
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.jsp;
 
 import com.intellij.lang.jsp.IBaseJspManager;
 import com.intellij.lang.jsp.JspVersion;
