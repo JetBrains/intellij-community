4ef4e27ab57a48b502e8a161f6da72a5c4e12f7e
Nicolay Mitropolsky <nicolay.mitropolsky@jetbrains.com>
2021-07-02 15:37:25 +0000

    [kubernetes] KubernetesLanguageInjector: use same injection approach as YamlLanguageInjectionPerformer
    GitOrigin-RevId: 4c514129e5b025ec9f2bdfb5609c88c0a8e0c89a

--- plugins/yaml/src/org/jetbrains/yaml/YamlLanguageInjectionPerformer.kt
+++ plugins/yaml/src/org/jetbrains/yaml/YamlLanguageInjectionPerformer.kt
@@ -2,9 +2,11 @@
 package org.jetbrains.yaml
 
 import com.intellij.injected.editor.InjectionMeta
+import com.intellij.lang.Language
 import com.intellij.lang.injection.MultiHostRegistrar
 import com.intellij.lang.injection.general.Injection
 import com.intellij.lang.injection.general.LanguageInjectionPerformer
+import com.intellij.openapi.util.TextRange
 import com.intellij.psi.ElementManipulators
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiLanguageInjectionHost
@@ -22,36 +24,39 @@ class YamlLanguageInjectionPerformer : LanguageInjectionPerformer {
                                 context: PsiElement): Boolean {
     if (context !is YAMLScalar) return false
     val language = injection.injectedLanguage ?: return false
-    registrar.startInjecting(language)
-    if (context is YAMLScalarImpl) {
-
-      val contentRanges = context.contentRanges
-
-      if(context is YAMLBlockScalarImpl){
-        context.putUserData(InjectionMeta.INJECTION_INDENT, " ".repeat(context.locateIndent()))
-        context.putUserData(InjectionMeta.SUPPRESS_COPY_PASTE_HANDLER_IN_FE, true)
-      }
-      
-      if (contentRanges.isEmpty()) {
-        // do nothing
-      }
-      else if (contentRanges.size == 1) {
-        registrar.addPlace(injection.prefix, injection.suffix, context, contentRanges.single())
-      }
-      else {
-        registrar.addPlace(injection.prefix, null, context, contentRanges.first())
-        for (textRange in contentRanges.subList(1, contentRanges.size - 1)) {
-          registrar.addPlace(null, null, context, textRange)
-        }
-        registrar.addPlace(null, injection.suffix, context, contentRanges.last())
-      }
-
-    }
-    else {
-      val textRange = ElementManipulators.getValueTextRange(context)
-      registrar.addPlace(injection.prefix, injection.suffix, (context as PsiLanguageInjectionHost), textRange)
-    }
-    registrar.doneInjecting()
+    injectIntoYamlMultiRanges(registrar, context, getYamlLiteralRanges(context), language, injection.prefix, injection.suffix)
     return true
   }
+
 }
+
+fun getYamlLiteralRanges(context: PsiElement) = 
+  (context as? YAMLScalarImpl)?.contentRanges ?: listOf(ElementManipulators.getValueTextRange(context))
+
+fun injectIntoYamlMultiRanges(registrar: MultiHostRegistrar,
+                              context: PsiLanguageInjectionHost,
+                              ranges: List<TextRange>,
+                              language: Language,
+                              prefix: String?,
+                              suffix: String?) {
+  registrar.startInjecting(language)
+  if (context is YAMLBlockScalarImpl) {
+    context.putUserData(InjectionMeta.INJECTION_INDENT, " ".repeat(context.locateIndent()))
+    context.putUserData(InjectionMeta.SUPPRESS_COPY_PASTE_HANDLER_IN_FE, true)
+  }
+  if (ranges.isEmpty()) {
+    // do nothing
+  }
+  else if (ranges.size == 1) {
+    registrar.addPlace(prefix, suffix, context, ranges.single())
+  }
+  else {
+    registrar.addPlace(prefix, null, context, ranges.first())
+    for (textRange in ranges.subList(1, ranges.size - 1)) {
+      registrar.addPlace(null, null, context, textRange)
+    }
+    registrar.addPlace(null, suffix, context, ranges.last())
+  }
+  registrar.doneInjecting()
+}
+
