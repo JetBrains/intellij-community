ca334cd354e012073d27bd264b01519d966ee9bb
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2021-07-02 09:00:14 +0000

    EA-285780 RUBY-28389 defer VirtualFile computation on OpenFileDescriptor creation
    (cherry picked from commit c556af337fc766a0f4c10694f3bfdeda49f96aea)
    
    GitOrigin-RevId: 8a3a1593cecd7b06f2e03c1b1927fe810f391c40

--- platform/smRunner/src/com/intellij/execution/testframework/sm/runner/SMTRunnerConsoleProperties.java
+++ platform/smRunner/src/com/intellij/execution/testframework/sm/runner/SMTRunnerConsoleProperties.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.testframework.sm.runner;
 
 import com.intellij.execution.Executor;
@@ -19,6 +19,7 @@ import com.intellij.execution.testframework.sm.runner.history.actions.ImportTest
 import com.intellij.execution.ui.ConsoleView;
 import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.NlsSafe;
 import com.intellij.openapi.util.text.StringUtil;
@@ -125,7 +126,31 @@ public class SMTRunnerConsoleProperties extends TestConsoleProperties implements
 
         // covers 99% use existing cases
         if (info instanceof FileHyperlinkInfo) {
-          return ((FileHyperlinkInfo)info).getDescriptor();
+          return new Navigatable() {
+            private OpenFileDescriptor myFileDescriptor;
+
+            private OpenFileDescriptor getOpenFileDescriptor() {
+              if (myFileDescriptor == null) {
+                myFileDescriptor = ((FileHyperlinkInfo)info).getDescriptor();
+              }
+              return myFileDescriptor;
+            }
+
+            @Override
+            public void navigate(boolean requestFocus) {
+              getOpenFileDescriptor().navigate(requestFocus);
+            }
+
+            @Override
+            public boolean canNavigate() {
+              return getOpenFileDescriptor().canNavigate();
+            }
+
+            @Override
+            public boolean canNavigateToSource() {
+              return getOpenFileDescriptor().canNavigateToSource();
+            }
+          };
         }
 
         // otherwise
