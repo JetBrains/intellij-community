66fd1a66d70fe59355ac45a9f8304bf2f94a189d
Egor Ushakov <egor.ushakov@jetbrains.com>
2021-07-02 11:20:59 +0000

    removed obsolete inlay settings
    GitOrigin-RevId: 12a4a8a66ff21987718c886bee20da344963e0e6

--- platform/analysis-impl/src/com/intellij/codeInsight/CodeInsightSettings.java
+++ platform/analysis-impl/src/com/intellij/codeInsight/CodeInsightSettings.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight;
 
 import com.intellij.codeInsight.editorActions.SmartBackspaceMode;
@@ -54,9 +54,6 @@ public class CodeInsightSettings implements PersistentStateComponent<Element>, C
       return null;
     }
   }
-
-  public boolean SHOW_EXTERNAL_ANNOTATIONS_INLINE = true;
-  public boolean SHOW_INFERRED_ANNOTATIONS_INLINE;
 
   public boolean SHOW_PARAMETER_NAME_HINTS_ON_COMPLETION;
   public boolean AUTO_POPUP_PARAMETER_INFO = true;
