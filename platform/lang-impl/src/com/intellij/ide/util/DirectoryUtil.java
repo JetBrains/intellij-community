c4cb04d887417a8df3fcb2732d5ea27158174ad7
Anna Kozlova <anna.kozlova@jetbrains.com>
2021-07-05 19:40:46 +0000

    EA-265153 - SIOOBE: DirectoryUtil.mkdirs
    compare paths, according to system case sensitivity
    
    GitOrigin-RevId: 62e21c0cae95ac39554d7b7f7681ca4648c0cfca

--- platform/lang-impl/src/com/intellij/ide/util/DirectoryUtil.java
+++ platform/lang-impl/src/com/intellij/ide/util/DirectoryUtil.java
@@ -1,8 +1,9 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.ide.util;
 
 import com.intellij.openapi.util.SystemInfo;
+import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VfsUtil;
@@ -39,7 +40,7 @@ public final class DirectoryUtil {
       return null;
     }
     String existingPath = directory.getVirtualFile().getPath();
-    if (existingPath.equals(path)) {
+    if (FileUtil.comparePaths(existingPath, path) == 0) {
       return directory;
     }
 
