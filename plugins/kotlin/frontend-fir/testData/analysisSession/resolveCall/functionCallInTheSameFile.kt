3928062596fd2dcaf5b22fc9a5de399823f9375b
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-07-05 19:23:59 +0200

    FIR IDE: remove unused modules
    They live in kotlin repository now
    
    GitOrigin-RevId: 90f3705cd367a0242c0860aef65f5cb36129e503

--- plugins/kotlin/frontend-fir/testData/analysisSession/resolveCall/functionCallInTheSameFile.kt
+++
@@ -1,7 +0,0 @@
-fun function(a: Int) {}
-
-fun call() {
-    <expr>function(1)</expr>
-}
-
-// CALL: KtFunctionCall: targetFunction = /function(a: kotlin.Int): kotlin.Unit
--- plugins/kotlin/frontend-fir/testData/analysisSession/resolveCall/simpleCallWithNonMatchingArgs.kt
+++
@@ -1,7 +0,0 @@
-fun x() {
-    <expr>foo(1)</expr>
-}
-
-fun foo(){}
-
-// CALL: KtFunctionCall: targetFunction = ERR<Too many arguments for public final fun /foo(): R|kotlin/Unit|, [/foo(): kotlin.Unit]>
