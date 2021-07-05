4cf74413a9a2fbb6c0920706e2f0614e6df86517
Gregory.Shrago <gregory.shrago@jetbrains.com>
2021-07-05 10:12:30 +0000

    IDEA-149044 Actions to show popup scratch list 2
    (cherry picked from commit 3087ff902a1cbf321df6ec25710d34301098a49c)
    
    IJ-CR-10421
    
    GitOrigin-RevId: 2aaa6b7352c921c3f3c3c2c629517a628952fc4b

--- platform/lang-impl/src/com/intellij/ide/scratch/ScratchFileActions.java
+++ platform/lang-impl/src/com/intellij/ide/scratch/ScratchFileActions.java
@@ -4,7 +4,9 @@ package com.intellij.ide.scratch;
 import com.intellij.featureStatistics.FeatureUsageTracker;
 import com.intellij.icons.AllIcons;
 import com.intellij.ide.actions.NewActionGroup;
+import com.intellij.ide.actions.RecentLocationsAction;
 import com.intellij.ide.scratch.ScratchImplUtil.LanguageItem;
+import com.intellij.ide.util.DeleteHandler;
 import com.intellij.ide.util.PsiNavigationSupport;
 import com.intellij.idea.ActionsBundle;
 import com.intellij.lang.*;
@@ -14,22 +16,32 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.command.undo.UnexpectedUndoException;
 import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.RangeMarker;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.fileEditor.ex.IdeDocumentHistory;
+import com.intellij.openapi.fileEditor.impl.IdeDocumentHistoryImpl;
+import com.intellij.openapi.fileEditor.impl.text.TextEditorState;
 import com.intellij.openapi.fileTypes.*;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Condition;
+import com.intellij.openapi.util.Conditions;
 import com.intellij.openapi.util.NlsContexts;
 import com.intellij.openapi.util.NotNullLazyValue;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileVisitor;
 import com.intellij.psi.*;
 import com.intellij.psi.util.PsiUtilCore;
 import com.intellij.ui.LayeredIcon;
 import com.intellij.util.*;
+import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.containers.JBIterable;
 import com.intellij.util.ui.EmptyIcon;
 import org.jetbrains.annotations.ApiStatus;
@@ -38,9 +50,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 import static com.intellij.openapi.util.Conditions.not;
 
@@ -57,7 +67,7 @@ public final class ScratchFileActions {
   }
 
 
-  public static class NewFileAction extends DumbAwareAction {
+  public static class NewFileAction extends DumbAwareAction implements UpdateInBackground {
     private static final Icon ICON = LayeredIcon.create(AllIcons.FileTypes.Text, AllIcons.Actions.Scratch);
 
     private static final String ACTION_ID = "NewScratchFile";
@@ -132,7 +142,7 @@ public final class ScratchFileActions {
     }
   }
 
-  public static class NewBufferAction extends DumbAwareAction {
+  public static class NewBufferAction extends DumbAwareAction implements UpdateInBackground {
 
     @Override
     public void update(@NotNull AnActionEvent e) {
@@ -256,7 +266,7 @@ public final class ScratchFileActions {
     checkLanguageAndTryToFixText(project, context, dataContext);
   }
 
-  public static class LanguageAction extends DumbAwareAction {
+  public static class ChangeLanguageAction extends DumbAwareAction implements UpdateInBackground {
     @Override
     public void update(@NotNull AnActionEvent e) {
       Project project = e.getProject();
@@ -343,6 +353,59 @@ public final class ScratchFileActions {
         })
         .buildPopup()
         .showCenteredInCurrentWindow(project);
+    }
+  }
+
+  public static class ShowFilesPopupAction extends DumbAwareAction implements UpdateInBackground {
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+      e.getPresentation().setEnabledAndVisible(e.getProject() != null);
+    }
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+      Project project = e.getProject();
+      if (project == null) return;
+      RecentLocationsAction.showPopup(
+        project, false, LangBundle.message("scratch.file.popup.title"),
+        LangBundle.message("scratch.file.popup.changed.title"),
+        LangBundle.message("scratch.file.popup.title.empty.text"),
+        changed -> getPlaces(project, changed),
+        toRemove -> removePlaces(project, toRemove));
+    }
+
+    private static @NotNull List<IdeDocumentHistoryImpl.PlaceInfo> getPlaces(@NotNull Project project, boolean changed) {
+      String path = ScratchFileService.getInstance().getRootPath(ScratchRootType.getInstance());
+      VirtualFile rootDir = LocalFileSystem.getInstance().findFileByPath(path);
+      if (rootDir == null || !rootDir.exists() || !rootDir.isDirectory()) return Collections.emptyList();
+      Condition<? super VirtualFile> condition;
+      if (!changed) {
+        condition = Conditions.alwaysTrue();
+      }
+      else {
+        Set<VirtualFile> files = JBIterable.from(IdeDocumentHistory.getInstance(project).getChangePlaces())
+          .map(o -> o.getFile()).toSet();
+        condition = files::contains;
+      }
+      List<IdeDocumentHistoryImpl.PlaceInfo> result = new ArrayList<>();
+      VfsUtilCore.visitChildrenRecursively(rootDir, new VirtualFileVisitor<>(VirtualFileVisitor.SKIP_ROOT) {
+        @Override
+        public boolean visitFile(@NotNull VirtualFile file) {
+          if (file.isDirectory() || !file.isValid() || !condition.value(file)) return true;
+          Document document = FileDocumentManager.getInstance().getDocument(file);
+          if (document == null) return true;
+          RangeMarker caret = document.createRangeMarker(0, 0);
+          result.add(new IdeDocumentHistoryImpl.PlaceInfo(file, new TextEditorState(), "text-editor", null, caret));
+          return result.size() < 1000;
+        }
+      });
+      return result;
+    }
+
+    private static void removePlaces(@NotNull Project project, @NotNull List<IdeDocumentHistoryImpl.PlaceInfo> toRemove) {
+      PsiManager psiManager = PsiManager.getInstance(project);
+      List<PsiFile> files = ContainerUtil.mapNotNull(toRemove, o -> psiManager.findFile(o.getFile()));
+      DeleteHandler.deletePsiElement(files.toArray(PsiElement.EMPTY_ARRAY), project, false);
     }
   }
 }
