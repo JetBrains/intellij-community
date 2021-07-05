53fb4fa63a67d5d969314012bda598c22f0b0bd5
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-07-05 19:23:59 +0200

    FIR IDE: remove unused modules
    They live in kotlin repository now
    
    GitOrigin-RevId: 5f42807abf66b1901a1e0182c5e4c880c5a6c4cc

--- plugins/kotlin/fir-low-level-api/testdata/diagnosticTraversalCounter/lambda.kt
+++
@@ -1,7 +0,0 @@
-// WITH_RUNTIME
-
-val x = run {
-    val inLambda = 10
-    println(inLambda)
-    inLambda
-}
--- plugins/kotlin/frontend-fir/testData/analysisSession/resolveCall/functionWithReceiverCall.kt
+++
@@ -1,7 +0,0 @@
-fun String.function(a: Int) {}
-
-fun call() {
-   "str".<expr>function(1)</expr>
-}
-
-// CALL: KtFunctionCall: targetFunction = /function(<receiver>: kotlin.String, a: kotlin.Int): kotlin.Unit
