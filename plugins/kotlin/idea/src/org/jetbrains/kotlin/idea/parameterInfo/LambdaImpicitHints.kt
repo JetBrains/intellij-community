dc86412b517d747c5c0f069accc5be2a3a657105
Vladimir Dolzhenko <Vladimir.Dolzhenko@jetbrains.com>
2021-07-06 11:40:47 +0000

    [kotlin] Make types in inlay hints clickable
    (cherry picked from commit 8d62a13404c4e78d78ae7fe19d49e20913fc17a9)
    
    IJ-MR-11217
    
    GitOrigin-RevId: 2fabba79d449f248f97281f30d72c5d336aa8c6c

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/parameterInfo/LambdaImpicitHints.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/parameterInfo/LambdaImpicitHints.kt
@@ -7,13 +7,14 @@ import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiComment
 import com.intellij.psi.TokenType
 import org.jetbrains.kotlin.idea.caches.resolve.analyze
+import org.jetbrains.kotlin.idea.codeInsight.hints.HintType
 import org.jetbrains.kotlin.psi.KtLambdaExpression
 import org.jetbrains.kotlin.psi.psiUtil.siblings
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
 import org.jetbrains.kotlin.types.typeUtil.isUnit
 
-fun provideLambdaImplicitHints(lambda: KtLambdaExpression): List<InlayInfo> {
+fun provideLambdaImplicitHints(lambda: KtLambdaExpression): List<HintType.InlayInfoDetails> {
     val lbrace = lambda.leftCurlyBrace
     if (!lbrace.isFollowedByNewLine()) {
         return emptyList()
@@ -21,26 +22,27 @@ fun provideLambdaImplicitHints(lambda: KtLambdaExpression): List<InlayInfo> {
     val bindingContext = lambda.analyze(BodyResolveMode.PARTIAL)
     val functionDescriptor = bindingContext[BindingContext.FUNCTION, lambda.functionLiteral] ?: return emptyList()
 
-    val implicitReceiver = functionDescriptor.extensionReceiverParameter
-    if (implicitReceiver != null) {
+    functionDescriptor.extensionReceiverParameter?.let { implicitReceiver ->
+        val type = implicitReceiver.type
+        val renderType = getInlayHintsTypeRenderer(bindingContext, lambda).renderType(type)
         val text = buildString {
-            append(TYPE_INFO_PREFIX)
             append("this: ")
-            append(getInlayHintsTypeRenderer(bindingContext, lambda).renderType(implicitReceiver.type))
+            append(renderType)
         }
-        return listOf(InlayInfo(text, lbrace.textRange.endOffset))
+        val inlayInfo = InlayInfo(text, lbrace.textRange.endOffset)
+        return listOf(HintType.TypedInlayInfoDetails(inlayInfo, type))
     }
 
     val singleParameter = functionDescriptor.valueParameters.singleOrNull()
     if (singleParameter != null && bindingContext[BindingContext.AUTO_CREATED_IT, singleParameter] == true) {
         val type = singleParameter.type
         if (type.isUnit()) return emptyList()
+        val renderType = getInlayHintsTypeRenderer(bindingContext, lambda).renderType(type)
         val text = buildString {
-            append(TYPE_INFO_PREFIX)
             append("it: ")
-            append(getInlayHintsTypeRenderer(bindingContext, lambda).renderType(type))
+            append(renderType)
         }
-        return listOf(InlayInfo(text, lbrace.textRange.endOffset))
+        return listOf(HintType.TypedInlayInfoDetails(InlayInfo(text, lbrace.textRange.endOffset), type))
     }
     return emptyList()
 }
