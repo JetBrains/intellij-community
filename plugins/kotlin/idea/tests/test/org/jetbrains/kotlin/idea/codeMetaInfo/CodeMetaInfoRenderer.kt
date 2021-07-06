2cb05ac937c1e06ff226872cc33178322f960c24
Roman Golyshev <roman.golyshev@jetbrains.com>
2021-07-06 23:13:59 +0300

    Correct package declaration in CodeMetaInfoRenderer.kt
    GitOrigin-RevId: 5be25aa56d00777f3237b339bd6154cae7cdd362

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/codeMetaInfo/CodeMetaInfoRenderer.kt
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/codeMetaInfo/CodeMetaInfoRenderer.kt
@@ -1,6 +1,6 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
-package org.jetbrains.kotlin.codeMetaInfo
+package org.jetbrains.kotlin.idea.codeMetaInfo
 
 import com.intellij.util.containers.Stack
 import org.jetbrains.kotlin.codeMetaInfo.model.CodeMetaInfo
