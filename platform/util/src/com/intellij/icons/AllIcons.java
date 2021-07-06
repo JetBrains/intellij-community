ac1060e0d665e758a5e7173cf3a3cca7201eedea
Vladislav Ertel <Vladislav.Ertel@jetbrains.com>
2021-07-06 13:26:42 +0000

    [cwm] telephony: add screen sharing icons
    GitOrigin-RevId: a2488b45a25da4ffd46b59d09747634ff7882269

--- platform/util/src/com/intellij/icons/AllIcons.java
+++ platform/util/src/com/intellij/icons/AllIcons.java
@@ -250,6 +250,9 @@ public class AllIcons {
     /** 16x16 */ public static final @NotNull Icon CwmPermissionEdit = load("codeWithMe/cwmPermissionEdit.svg", 5905686597847340239L, 2);
     /** 16x16 */ public static final @NotNull Icon CwmPermissions = load("codeWithMe/cwmPermissions.svg", -2006616855776238912L, 2);
     /** 16x16 */ public static final @NotNull Icon CwmPermissionView = load("codeWithMe/cwmPermissionView.svg", 9138074844400158078L, 2);
+    /** 16x16 */ public static final @NotNull Icon CwmScreenOff = load("codeWithMe/cwmScreenOff.svg", -476175803014967160L, 2);
+    /** 16x16 */ public static final @NotNull Icon CwmScreenOn = load("codeWithMe/cwmScreenOn.svg", -3537464376874600357L, 2);
+    /** 16x16 */ public static final @NotNull Icon CwmShared = load("codeWithMe/cwmShared.svg", 548821871765212397L, 2);
     /** 16x16 */ public static final @NotNull Icon CwmTerminate = load("codeWithMe/cwmTerminate.svg", 2019277118291534216L, 2);
     /** 16x16 */ public static final @NotNull Icon Users = load("codeWithMe/users.svg", 679237194319153354L, 2);
   }
