a2af131ab20c62e5db6ecc4b8fb5568a90f7b9b8
Alexey Kalina <alexey.kalina@jetbrains.com>
2021-07-06 11:23:54 +0300

    [stats-collector] EA-288324: don't use lookup element as key
    GitOrigin-RevId: c60c8400d36efde7f347fd91da0af71e903bd536

--- plugins/stats-collector/src/com/intellij/stats/completion/tracker/LookupStateManager.kt
+++ plugins/stats-collector/src/com/intellij/stats/completion/tracker/LookupStateManager.kt
@@ -12,10 +12,10 @@ import com.intellij.stats.completion.LookupState
 import com.intellij.util.SlowOperations
 
 class LookupStateManager {
-    private val elementToId = mutableMapOf<String, Int>()
-    private val idToEntryInfo = mutableMapOf<Int, LookupEntryInfo>()
-    private val lookupStringToHash = mutableMapOf<String, Int>()
-    private var currentSessionFactors: Map<String, String> = emptyMap()
+  private val elementToId = mutableMapOf<String, Int>()
+  private val idToEntryInfo = mutableMapOf<Int, LookupEntryInfo>()
+  private val lookupStringToHash = mutableMapOf<String, Int>()
+  private var currentSessionFactors: Map<String, String> = emptyMap()
 
   fun update(lookup: LookupImpl, factorsUpdated: Boolean): LookupState {
     return SlowOperations.allowSlowOperations<LookupState, Throwable> {
@@ -24,106 +24,108 @@ class LookupStateManager {
   }
 
   private fun doUpdate(lookup: LookupImpl, factorsUpdated: Boolean): LookupState {
-        val ids = mutableListOf<Int>()
-        val newIds = mutableSetOf<Int>()
+    val ids = mutableListOf<Int>()
+    val newIds = mutableSetOf<Int>()
 
-        val items = lookup.items
+    val items = lookup.items
 
-        val currentPosition = items.indexOf(lookup.currentItem)
+    val currentPosition = items.indexOf(lookup.currentItem)
 
-        val elementToId = mutableMapOf<LookupElement, Int>()
-        for (item in items) {
-            var id = getElementId(item)
-            if (id == null) {
-                id = registerElement(item)
-                newIds.add(id)
-            }
-            elementToId[item] = id
-            ids.add(id)
-        }
-
-        val storage = LookupStorage.get(lookup)
-        val commonSessionFactors = storage?.sessionFactors?.getLastUsedCommonFactors() ?: emptyMap()
-        val sessionFactorsToLog = computeSessionFactorsToLog(commonSessionFactors)
-
-        if (factorsUpdated) {
-            val infos = items.toLookupInfos(lookup, elementToId)
-            val newInfos = infos.filter { it.id in newIds }
-
-            val itemsDiff = infos.mapNotNull { idToEntryInfo[it.id]?.calculateDiff(it) }
-            infos.forEach { idToEntryInfo[it.id] = it }
-            return LookupState(ids, newInfos, itemsDiff, currentPosition, sessionFactorsToLog)
-        }
-        else {
-            val newItems = items.filter { getElementId(it) in newIds }.toLookupInfos(lookup, elementToId)
-            newItems.forEach { idToEntryInfo[it.id] = it }
-            return LookupState(ids, newItems, emptyList(), currentPosition, sessionFactorsToLog)
-        }
+    val elementToId = mutableMapOf<String, Int>()
+    for (item in items) {
+      var id = getElementId(item)
+      if (id == null) {
+        id = registerElement(item)
+        newIds.add(id)
+      }
+      elementToId[item.idString()] = id
+      ids.add(id)
     }
 
-    fun getElementId(item: LookupElement): Int? {
-        val itemString = item.idString()
-        return elementToId[itemString]
+    val storage = LookupStorage.get(lookup)
+    val commonSessionFactors = storage?.sessionFactors?.getLastUsedCommonFactors() ?: emptyMap()
+    val sessionFactorsToLog = computeSessionFactorsToLog(commonSessionFactors)
+
+    if (factorsUpdated) {
+      val infos = items.toLookupInfos(lookup, elementToId)
+      val newInfos = infos.filter { it.id in newIds }
+
+      val itemsDiff = infos.mapNotNull { idToEntryInfo[it.id]?.calculateDiff(it) }
+      infos.forEach { idToEntryInfo[it.id] = it }
+      return LookupState(ids, newInfos, itemsDiff, currentPosition, sessionFactorsToLog)
+    }
+    else {
+      val newItems = items.filter { getElementId(it) in newIds }.toLookupInfos(lookup, elementToId)
+      newItems.forEach { idToEntryInfo[it.id] = it }
+      return LookupState(ids, newItems, emptyList(), currentPosition, sessionFactorsToLog)
+    }
+  }
+
+  fun getElementId(item: LookupElement): Int? {
+    val itemString = item.idString()
+    return elementToId[itemString]
+  }
+
+  private fun computeSessionFactorsToLog(factors: Map<String, String>): Map<String, String> {
+    if (factors == currentSessionFactors) return emptyMap()
+    currentSessionFactors = factors
+    return factors
+  }
+
+  private fun registerElement(item: LookupElement): Int {
+    val itemString = item.idString()
+    val newId = elementToId.size
+    elementToId[itemString] = newId
+    return newId
+  }
+
+  private fun List<LookupElement>.toLookupInfos(lookup: LookupImpl, elementToId: Map<String, Int>): List<LookupEntryInfo> {
+    val item2relevance = calculateRelevance(lookup, this)
+    return this.map { lookupElement ->
+      val id = lookupElement.idString()
+      val lookupString = lookupElement.lookupString
+      val itemHash = getLookupStringHash(lookupString)
+      LookupEntryInfo(elementToId.getValue(id), lookupString.length, itemHash, item2relevance.getValue(id))
+    }
+  }
+
+  private fun calculateRelevance(lookup: LookupImpl, items: List<LookupElement>): Map<String, Map<String, String>> {
+    val lookupStorage = LookupStorage.get(lookup)
+    if (lookupStorage?.shouldComputeFeatures() == false) {
+      return items.associateBy({ it.idString() }, { emptyMap() })
     }
 
-    private fun computeSessionFactorsToLog(factors: Map<String, String>): Map<String, String> {
-        if (factors == currentSessionFactors) return emptyMap()
-        currentSessionFactors = factors
-        return factors
-    }
-
-    private fun registerElement(item: LookupElement): Int {
-        val itemString = item.idString()
-        val newId = elementToId.size
-        elementToId[itemString] = newId
-        return newId
-    }
-
-    private fun List<LookupElement>.toLookupInfos(lookup: LookupImpl, elementToId: Map<LookupElement, Int>): List<LookupEntryInfo> {
-        val item2relevance = calculateRelevance(lookup, this)
-        return this.map { lookupElement ->
-            val lookupString = lookupElement.lookupString
-            val itemHash = getLookupStringHash(lookupString)
-            LookupEntryInfo(elementToId.getValue(lookupElement), lookupString.length, itemHash, item2relevance.getValue(lookupElement))
+    val result = mutableMapOf<String, Map<String, String>>()
+    if (lookupStorage != null) {
+      for (item in items) {
+        val id = item.idString()
+        val factors = lookupStorage.getItemStorage(id).getLastUsedFactors()?.mapValues { it.value.toString() }
+        if (factors != null) {
+          result[id] = factors
         }
+      }
     }
 
-    private fun calculateRelevance(lookup: LookupImpl, items: List<LookupElement>): Map<LookupElement, Map<String, String>> {
-        val lookupStorage = LookupStorage.get(lookup)
-        if (lookupStorage?.shouldComputeFeatures() == false) {
-            return items.associateBy({ it }, { emptyMap() })
-        }
-
-        val result = mutableMapOf<LookupElement, Map<String, String>>()
-        if (lookupStorage != null) {
-            for (item in items) {
-                val factors = lookupStorage.getItemStorage(item.idString()).getLastUsedFactors()?.mapValues { it.value.toString() }
-                if (factors != null) {
-                    result[item] = factors
-                }
-            }
-        }
-
-        // fallback (get factors from the relevance objects)
-        val rest = items.filter { it !in result }
-        if (rest.isNotEmpty()) {
-            val relevanceObjects = lookup.getRelevanceObjects(rest, false)
-            for (item in rest) {
-                val relevanceMap: Map<String, String> = relevanceObjects[item]?.let { objects ->
-                    val (relevanceMap, additionalMap) = RelevanceUtil.asRelevanceMaps(objects)
-                    val features = mutableMapOf<String, String>()
-                    relevanceMap.forEach { features[it.key] = it.value.toString() }
-                    additionalMap.forEach { features[it.key] = it.value.toString() }
-                    return@let features
-                } ?: emptyMap()
-                result[item] = relevanceMap
-            }
-        }
-
-        return result
+    // fallback (get factors from the relevance objects)
+    val rest = items.filter { it.idString() !in result }
+    if (rest.isNotEmpty()) {
+      val relevanceObjects = lookup.getRelevanceObjects(rest, false)
+      for (item in rest) {
+        val relevanceMap: Map<String, String> = relevanceObjects[item]?.let { objects ->
+          val (relevanceMap, additionalMap) = RelevanceUtil.asRelevanceMaps(objects)
+          val features = mutableMapOf<String, String>()
+          relevanceMap.forEach { features[it.key] = it.value.toString() }
+          additionalMap.forEach { features[it.key] = it.value.toString() }
+          return@let features
+        } ?: emptyMap()
+        result[item.idString()] = relevanceMap
+      }
     }
 
-    private fun getLookupStringHash(lookupString: String): Int {
-        return lookupStringToHash.computeIfAbsent(lookupString) { lookupStringToHash.size }
-    }
+    return result
+  }
+
+  private fun getLookupStringHash(lookupString: String): Int {
+    return lookupStringToHash.computeIfAbsent(lookupString) { lookupStringToHash.size }
+  }
 }
