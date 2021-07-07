fa532148faf32e0bd55d7e224e9c9d22301cf0d2
Alexander Dudinsky <Alexander.Dudinsky@jetbrains.com>
2021-07-07 12:54:21 +0000

    Update tesData for `testDuplicateExpectsImplicit`
    (cherry picked from commit d96f3c57f5a781b34e0490d05cd1df4772b59966)
    
    GitOrigin-RevId: 13fe6e8850f5918bdfd0213056aa254915ba321c

--- plugins/kotlin/idea/tests/testData/multiplatform/duplicateExpectsImplicit/bottom/bottom.kt
+++ plugins/kotlin/idea/tests/testData/multiplatform/duplicateExpectsImplicit/bottom/bottom.kt
@@ -1,3 +1,1 @@
-// Note that here we have no ambiguity, becuase we don't consider
-// declarations without 'expect' as potential weakly-compatible 'expect'-counterpart
-actual class <!LINE_MARKER("descr='Has declaration in common module'"), LINE_MARKER("descr='Has declaration in common module'")!>A<!>
+actual class <!LINE_MARKER("descr='Has declaration in common module'")!>A<!>
