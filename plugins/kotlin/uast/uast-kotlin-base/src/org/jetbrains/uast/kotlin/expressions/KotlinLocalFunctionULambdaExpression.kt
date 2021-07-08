c929f84bdef8d2eb87bf47b97d5e848685e9c11f
Jinseong Jeon <jsjeon@google.com>
2021-07-07 17:29:08 -0700

    FIR/UAST: commonize local function conversion
    GitOrigin-RevId: 57d3e1055df07f2bccfeb3d753a6bdd5d4a4801c

--- plugins/kotlin/uast/uast-kotlin-base/src/org/jetbrains/uast/kotlin/expressions/KotlinLocalFunctionULambdaExpression.kt
+++ plugins/kotlin/uast/uast-kotlin-base/src/org/jetbrains/uast/kotlin/expressions/KotlinLocalFunctionULambdaExpression.kt
@@ -1,12 +1,36 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.uast.kotlin.expressions
+package org.jetbrains.uast.kotlin
 
+import com.intellij.psi.PsiType
 import org.jetbrains.kotlin.psi.KtFunction
-import org.jetbrains.uast.UElement
-import org.jetbrains.uast.ULambdaExpression
-import org.jetbrains.uast.kotlin.KotlinAbstractUExpression
+import org.jetbrains.uast.*
+import org.jetbrains.uast.kotlin.psi.UastKotlinPsiParameter
 
-abstract class KotlinLocalFunctionULambdaExpression(
+class KotlinLocalFunctionULambdaExpression(
     override val sourcePsi: KtFunction,
     givenParent: UElement?
-) : KotlinAbstractUExpression(givenParent), ULambdaExpression
+) : KotlinAbstractUExpression(givenParent), ULambdaExpression {
+    override val functionalInterfaceType: PsiType? = null
+
+    override val body by lz {
+        sourcePsi.bodyExpression?.let { wrapExpressionBody(this, it) } ?: UastEmptyExpression(this)
+    }
+
+    override val valueParameters by lz {
+        sourcePsi.valueParameters.mapIndexed { i, p ->
+            KotlinUParameter(UastKotlinPsiParameter.create(baseResolveProviderService, p, sourcePsi, this, i), p, this)
+        }
+    }
+
+    override fun asRenderString(): String {
+        val renderedValueParameters = valueParameters.joinToString(
+            prefix = "(",
+            postfix = ")",
+            transform = KotlinUParameter::asRenderString
+        )
+        val expressions = (body as? UBlockExpression)?.expressions?.joinToString("\n") {
+            it.asRenderString().withMargin
+        } ?: body.asRenderString()
+        return "fun $renderedValueParameters {\n${expressions.withMargin}\n}"
+    }
+}
