8d1047cbe984e76d2cc4f51a039c1b2f3e37b013
Jinseong Jeon <jsjeon@google.com>
2021-07-07 17:44:09 -0700

    FIR/UAST: commonize UBlockExpression
    GitOrigin-RevId: 1a5c9c22d93030bbbcb2467f8fbb060114c2202f

--- plugins/kotlin/uast/uast-kotlin-fir/src/org/jetbrains/uast/kotlin/FirKotlinConverter.kt
+++ plugins/kotlin/uast/uast-kotlin-fir/src/org/jetbrains/uast/kotlin/FirKotlinConverter.kt
@@ -21,7 +21,6 @@ import org.jetbrains.uast.*
 import org.jetbrains.uast.expressions.UInjectionHost
 import org.jetbrains.uast.kotlin.expressions.FirKotlinUArrayAccessExpression
 import org.jetbrains.uast.kotlin.expressions.FirKotlinUBinaryExpression
-import org.jetbrains.uast.kotlin.expressions.FirKotlinUBlockExpression
 import org.jetbrains.uast.kotlin.expressions.FirKotlinUSimpleReferenceExpression
 import org.jetbrains.uast.kotlin.internal.firKotlinUastPlugin
 import org.jetbrains.uast.kotlin.psi.UastKotlinPsiParameter
@@ -389,7 +388,7 @@ internal object FirKotlinConverter : BaseKotlinConverter {
 
                 is KtBlockExpression -> expr<UBlockExpression> {
                     // TODO: differentiate lambda
-                    FirKotlinUBlockExpression(expression, givenParent)
+                    KotlinUBlockExpression(expression, givenParent)
                 }
                 is KtReturnExpression -> expr<UReturnExpression>(build(::KotlinUReturnExpression))
                 is KtThrowExpression -> expr<UThrowExpression>(build(::KotlinUThrowExpression))
