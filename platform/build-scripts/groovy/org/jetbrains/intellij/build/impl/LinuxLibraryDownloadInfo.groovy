fb1bae6423526b18a0ec27d2471f0e598410e329
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2021-07-08 09:59:14 +0000

    Add `@CompileStatic` to `LinuxLibraryDownloadInfo` groovy class
    GitOrigin-RevId: 06f57128cfe61eccac99e6d4e8dfb4b440851247

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/LinuxLibraryDownloadInfo.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/LinuxLibraryDownloadInfo.groovy
@@ -1,12 +1,15 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.intellij.build.impl
 
+import groovy.transform.CompileStatic
+
 import java.nio.file.Files
 import java.nio.file.Path
 
 /**
  * Define an information about Linux RPM package to be downloaded
  */
+@CompileStatic
 class LinuxLibraryDownloadInfo {
 
   public String libraryName
