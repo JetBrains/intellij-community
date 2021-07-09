7e1ab3078672969e0565ec7f5e413e4ba96a8b1a
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-07-09 23:26:03 +0200

    Cover Java's MethodChainHints implementation with more tests
    I am going to change the implementation so let's add more tests
    
    relates to KTIJ-5665
    
    GitOrigin-RevId: 56a1c7e2359194b4e4fd4f75ba05486e527c341f

--- java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/inlays/MethodChainHintsTest.kt
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/inlays/MethodChainHintsTest.kt
@@ -5,7 +5,7 @@ import com.intellij.codeInsight.hints.LinearOrderInlayRenderer
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
 import org.intellij.lang.annotations.Language
 
-class MethodChainHintsTest: LightJavaCodeInsightFixtureTestCase() {
+class MethodChainHintsTest : LightJavaCodeInsightFixtureTestCase() {
   fun check(@Language("Java") text: String) {
     myFixture.configureByText("A.java", text)
     myFixture.testInlays({ (it.renderer as LinearOrderInlayRenderer<*>).toString() }, { it.renderer is LinearOrderInlayRenderer<*> })
@@ -96,6 +96,109 @@ public class Chains {
      .a()<hint text="[[temp:///src/A.java:0]Chains . [temp:///src/A.java:24]A]"/>
      .c()<hint text="[[temp:///src/A.java:0]Chains . [temp:///src/A.java:172]C]"/>
      .b();
+  }
+}
+""")
+  }
+
+  fun `test comments`() {
+    check("""public class Chains {
+  static class A {
+    B b() {return null;}
+    C c() {return null;}
+  }
+
+  static class B {
+    A a() {return null;}
+    C c() {return null;}
+  }
+
+  static class C {
+    B b() {return null;}
+    A a() {return null;}
+  }
+
+  public static void main(String[] args) {
+    A a = new A();
+    a.b().c() // comment
+     .a()
+     .b()
+     .a() // comment
+     .c()
+     .b();
+  }
+}
+""")
+  }
+
+  fun `test several call chains`() {
+    check("""public class Chains {
+  static class A {
+    B b() {return null;}
+    C c() {return null;}
+  }
+
+  static class B {
+    A a() {return null;}
+    C c() {return null;}
+  }
+
+  static class C {
+    B b() {return null;}
+    A a() {return null;}
+  }
+
+  public static void main(String[] args) {
+    new A()
+      .b()<hint text="[[temp:///src/A.java:0]Chains . [temp:///src/A.java:98]B]"/>
+                .c()<hint text="[[temp:///src/A.java:0]Chains . [temp:///src/A.java:172]C]"/>
+                .a()<hint text="[[temp:///src/A.java:0]Chains . [temp:///src/A.java:24]A]"/>
+                .c();
+
+    new A()
+      .b()<hint text="[[temp:///src/A.java:0]Chains . [temp:///src/A.java:98]B]"/>
+                .c()<hint text="[[temp:///src/A.java:0]Chains . [temp:///src/A.java:172]C]"/>
+                .a()<hint text="[[temp:///src/A.java:0]Chains . [temp:///src/A.java:24]A]"/>
+                .c();
+  }
+}
+""")
+  }
+
+  fun `test nested call chains`() {
+    check("""
+public class Chains {
+  static interface Callable {
+    void call();
+  }
+
+  static class A {
+    B b(Callable callable) {return null;}
+    C c() {return null;}
+  }
+
+  static class B {
+    A a() {return null;}
+    C c() {return null;}
+  }
+
+  static class C {
+    B b() {return null;}
+    A a() {return null;}
+  }
+
+  public static void main(String[] args) {
+    new A()
+      .b(() -> {
+        new B()
+          .a()<hint text="[[temp:///src/A.java:1]Chains . [temp:///src/A.java:77]A]"/>
+          .c()<hint text="[[temp:///src/A.java:1]Chains . [temp:///src/A.java:242]C]"/>
+          .b()<hint text="[[temp:///src/A.java:1]Chains . [temp:///src/A.java:168]B]"/>
+          .a();
+      })<hint text="[[temp:///src/A.java:1]Chains . [temp:///src/A.java:168]B]"/>
+      .c()<hint text="[[temp:///src/A.java:1]Chains . [temp:///src/A.java:242]C]"/>
+      .a()<hint text="[[temp:///src/A.java:1]Chains . [temp:///src/A.java:77]A]"/>
+      .c();
   }
 }
 """)
