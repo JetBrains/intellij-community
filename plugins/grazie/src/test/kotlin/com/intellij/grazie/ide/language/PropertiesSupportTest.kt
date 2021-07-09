a131f5b490abca295fc1f61b77393446838e3eb6
Peter Gromov <peter@jetbrains.com>
2021-07-09 14:23:46 +0000

    disable grazie checks for Properties/XML for now, as they're not tested well enough (XTEST-787)
    (cherry picked from commit 0507e9e3dd8d09a1e0fd0d840cfa7de915ed013d)
    
    IJ-CR-11616
    
    GitOrigin-RevId: d3f9c51f42e339be62e3913875e47c5509891ec6

--- plugins/grazie/src/test/kotlin/com/intellij/grazie/ide/language/PropertiesSupportTest.kt
+++ plugins/grazie/src/test/kotlin/com/intellij/grazie/ide/language/PropertiesSupportTest.kt
@@ -2,9 +2,12 @@
 package com.intellij.grazie.ide.language
 
 import com.intellij.grazie.GrazieTestBase
+import com.intellij.lang.properties.PropertiesLanguage
 
 
 class PropertiesSupportTest : GrazieTestBase() {
+  override val additionalEnabledContextLanguages = setOf(PropertiesLanguage.INSTANCE)
+
   fun `test grammar check in file`() {
     runHighlightTestForFile("ide/language/properties/Example.properties")
   }
