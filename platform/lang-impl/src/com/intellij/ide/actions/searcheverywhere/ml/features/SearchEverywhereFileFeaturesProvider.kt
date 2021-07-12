661c5398d121f63c4b5c798da458adfe9e31c539
adam.malek <adam.malek@jetbrains.com>
2021-07-12 11:37:57 +0100

    ML in SE: Add two more file features - is it opened and is it in favorites list (IDEA-274480)
    GitOrigin-RevId: fbf276495e72a5c9bceae0c4fac09a9ad6d2d993

--- platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/ml/features/SearchEverywhereFileFeaturesProvider.kt
+++ platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/ml/features/SearchEverywhereFileFeaturesProvider.kt
@@ -1,7 +1,12 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.actions.searcheverywhere.ml.features
 
+import com.intellij.ide.actions.searcheverywhere.PSIPresentationBgRendererWrapper
+import com.intellij.ide.favoritesTreeView.FavoritesManager
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.changes.ChangeListManager
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 
 internal class SearchEverywhereFileFeaturesProvider : SearchEverywhereElementFeaturesProvider() {
@@ -9,7 +14,8 @@ internal class SearchEverywhereFileFeaturesProvider : SearchEverywhereElementFea
     private const val FILETYPE_DATA_KEY = "filetype"
     private const val IS_IGNORED_DATA_KEY = "isIgnored"
     private const val IS_CHANGED_DATA_KEY = "isChanged"
-
+    private const val IS_FAVORITE_DATA_KEY = "isFavorite"
+    private const val IS_OPENED_DATA_KEY = "isOpened"
     private const val TIME_SINCE_LAST_MODIFICATION_DATA_KEY = "timeSinceLastModification"
   }
 
@@ -19,13 +25,19 @@ internal class SearchEverywhereFileFeaturesProvider : SearchEverywhereElementFea
     if (element !is PSIPresentationBgRendererWrapper.PsiItemWithPresentation)
       return emptyMap()
 
-    return getPsiElementFeatures(element, currentTime)
+    return getPsiElementFeatures(element.item, currentTime)
   }
 
   private fun getPsiElementFeatures(element: PsiElement, currentTime: Long): Map<String, Any> {
+    val psiFile = element.containingFile
+    val virtualFile = psiFile.virtualFile
+    val project = psiFile.project
+
     val data = hashMapOf(
-      IS_IGNORED_DATA_KEY to isIgnoredFile(element),
-      IS_CHANGED_DATA_KEY to isChangedFile(element),
+      IS_IGNORED_DATA_KEY to isIgnoredFile(virtualFile, project),
+      IS_CHANGED_DATA_KEY to isChangedFile(virtualFile, project),
+      IS_FAVORITE_DATA_KEY to isFavorite(virtualFile, project),
+      IS_OPENED_DATA_KEY to isOpened(virtualFile, project),
       FILETYPE_DATA_KEY to element.containingFile.fileType,
       TIME_SINCE_LAST_MODIFICATION_DATA_KEY to currentTime - element.containingFile.virtualFile.timeStamp,
     )
@@ -33,21 +45,23 @@ internal class SearchEverywhereFileFeaturesProvider : SearchEverywhereElementFea
     return data
   }
 
-  private fun isIgnoredFile(element: PsiElement): Boolean {
-    val psiFile = element.containingFile
-    val virtualFile = psiFile.virtualFile
-    val project = element.containingFile.project
-
+  private fun isIgnoredFile(virtualFile: VirtualFile, project: Project): Boolean {
     val changeListManager = ChangeListManager.getInstance(project)
     return changeListManager.isIgnoredFile(virtualFile)
   }
 
-  private fun isChangedFile(element: PsiElement): Boolean {
-    val psiFile = element.containingFile
-    val virtualFile = psiFile.virtualFile
-    val project = element.containingFile.project
-
+  private fun isChangedFile(virtualFile: VirtualFile, project: Project): Boolean {
     val changeListManager = ChangeListManager.getInstance(project)
     return changeListManager.isFileAffected(virtualFile)
+  }
+
+  private fun isFavorite(virtualFile: VirtualFile, project: Project): Boolean {
+    val favoritesManager = FavoritesManager.getInstance(project)
+    return favoritesManager.getFavoriteListName(null, virtualFile) != null
+  }
+
+  private fun isOpened(virtualFile: VirtualFile, project: Project): Boolean {
+    val openedFiles = FileEditorManager.getInstance(project).openFiles
+    return virtualFile in openedFiles
   }
 }
