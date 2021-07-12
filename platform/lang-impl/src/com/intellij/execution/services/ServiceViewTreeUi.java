0ef8ed95cb0f2c2b66a5f33d19aebc3b2d7beaf0
Konstantin Aleev <konstantin.aleev@jetbrains.com>
2021-07-12 18:50:09 +0300

    CWM-3018 cwm: create nested lifetime for all dynamic containers
    (cherry picked from commit 16b99c39b938f2c1be5deeb39881192cfd79d955)
    
    IJ-CR-11678
    
    GitOrigin-RevId: d2d9a835cb8889cd4b3917f355f81752937f7d58

--- platform/lang-impl/src/com/intellij/execution/services/ServiceViewTreeUi.java
+++ platform/lang-impl/src/com/intellij/execution/services/ServiceViewTreeUi.java
@@ -49,7 +49,6 @@ class ServiceViewTreeUi implements ServiceViewUi {
     mySplitter.setFirstComponent(myMasterPanel);
 
     myDetailsPanel = new JPanel(new BorderLayout());
-    UIUtil.putClientProperty(myDetailsPanel, UIUtil.CLEARABLE_COMPONENT_KEY, true);
     myMessagePanel.setFocusable(true);
     myDetailsPanel.add(myMessagePanel, BorderLayout.CENTER);
     mySplitter.setSecondComponent(myDetailsPanel);
@@ -80,7 +79,6 @@ class ServiceViewTreeUi implements ServiceViewUi {
   @Override
   public void setServiceToolbar(@NotNull ServiceViewActionProvider actionProvider) {
     myServiceActionToolbar = actionProvider.createServiceToolbar(myMainPanel);
-    UIUtil.putClientProperty(myServiceActionToolbar.getComponent(), UIUtil.CLEARABLE_COMPONENT_KEY, true);
     myContentPanel.setToolbar(actionProvider.wrapServiceToolbar(myServiceActionToolbar));
   }
 
