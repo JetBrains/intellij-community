cb9ee326400b235539c8363fc44a7caeb9a50755
Jinseong Jeon <jsjeon@google.com>
2021-07-13 12:28:01 -0700

    FIR UAST: fix muting failing tests
    GitOrigin-RevId: 8e472c3138feece85de4b01049b9b4bdad22cbcd

--- plugins/kotlin/uast/uast-kotlin-fir/test/org/jetbrains/kotlin/idea/fir/uast/AbstractFirLegacyUastIdentifiersTest.kt
+++ plugins/kotlin/uast/uast-kotlin-fir/test/org/jetbrains/kotlin/idea/fir/uast/AbstractFirLegacyUastIdentifiersTest.kt
@@ -4,18 +4,26 @@ package org.jetbrains.kotlin.idea.fir.uast
 
 import org.jetbrains.uast.UFile
 import org.jetbrains.uast.test.common.kotlin.LegacyUastIdentifiersTestBase
+import java.nio.file.Paths
+import kotlin.io.path.ExperimentalPathApi
+import kotlin.io.path.absolute
 
 abstract class AbstractFirLegacyUastIdentifiersTest : AbstractFirUastIdentifiersTest(), LegacyUastIdentifiersTestBase {
+    @OptIn(ExperimentalPathApi::class)
+    private val whitelist : Set<String> = setOf(
+        // TODO: need a special handling of members in enum entries. See testEnumValueMembers in FirLegacyUastIdentifiersTestGenerated
+        //   Seems related to KT-45115: in FIR, enum entry init is an anonymous object, and thus members in it are regarded as _local_.
+        //   whereas FIR UAST conversion took a path to look up non-local FIR declaration for now.
+        "uast-kotlin/testData/EnumValueMembers.kt"
+    ).mapTo(mutableSetOf()) { Paths.get("..").resolve(it).absolute().normalize().toString() }
+
     override fun isExpectedToFail(filePath: String): Boolean {
         // TODO: Implement parent conversion in FIR UAST
         return true
     }
 
     override fun check(filePath: String, file: UFile) {
-        if (filePath == "plugins/uast-kotlin/testData/EnumValueMembers.kt") {
-            // TODO: need a special handling of members in enum entries. See testEnumValueMembers in FirLegacyUastIdentifiersTestGenerated
-            //   Seems related to KT-45115: in FIR, enum entry init is an anonymous object, and thus members in it are regarded as _local_.
-            //   whereas FIR UAST conversion took a path to look up non-local FIR declaration for now.
+        if (filePath in whitelist) {
             return
         }
         super<LegacyUastIdentifiersTestBase>.check(filePath, file)
