20f260b358305bf90ee1f00d12f9b2c6e69d0e35
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2021-07-14 17:45:37 +0900

    [kotlin] Move Gradle-dependent components of scripting support in IDE to the appropriate package
    GitOrigin-RevId: 16c06de502438a80e9c55ef4caad005463319c89

--- plugins/kotlin/compiler-plugins/scripting/src/org/jetbrains/kotlin/gradle/scripting/idea/plugin/ScriptingGradleProjectImportHandler.kt
+++ plugins/kotlin/compiler-plugins/scripting/src/org/jetbrains/kotlin/gradle/scripting/idea/plugin/ScriptingGradleProjectImportHandler.kt
@@ -1,6 +1,6 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
-package org.jetbrains.kotlin.scripting.idea.plugin
+package org.jetbrains.kotlin.gradle.scripting.idea.plugin
 
 import org.jetbrains.kotlin.cli.common.arguments.CommonCompilerArguments
 import org.jetbrains.kotlin.idea.configuration.GradleProjectImportHandler
