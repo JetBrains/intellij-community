1d135a3b4a398d8df2388625c4e5c4c32a9646b4
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2021-07-15 01:19:55 +0900

    [kotlin] Move Gradle-dependent classes of the Kotlin IDE plugin's 'gradle-native' module to the appropriate package, also move component registration to correct files
    GitOrigin-RevId: fa7e7e0b2b8cb5eef1744f01d30dcb5c94f6def0

--- plugins/kotlin/gradle/gradle-native/src/org/jetbrains/kotlin/idea/gradle/konan/KotlinNativeABICompatibilityChecker.kt
+++ plugins/kotlin/gradle/gradle-native/src/org/jetbrains/kotlin/idea/gradle/konan/KotlinNativeABICompatibilityChecker.kt
@@ -1,6 +1,6 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
-package org.jetbrains.kotlin.ide.konan
+package org.jetbrains.kotlin.idea.gradle.konan
 
 import com.intellij.ProjectTopics
 import com.intellij.notification.Notification
@@ -19,6 +19,7 @@ import com.intellij.openapi.util.text.StringUtilRt
 import com.intellij.util.PathUtilRt
 import com.intellij.util.concurrency.AppExecutorUtil
 import org.jetbrains.concurrency.CancellablePromise
+import org.jetbrains.kotlin.ide.konan.NativeKlibLibraryInfo
 import org.jetbrains.kotlin.idea.caches.project.getModuleInfosFromIdeaModel
 import org.jetbrains.kotlin.idea.gradle.configuration.klib.KotlinNativeLibraryNameUtil.isGradleLibraryName
 import org.jetbrains.kotlin.idea.gradle.configuration.klib.KotlinNativeLibraryNameUtil.parseIDELibraryName
--- plugins/kotlin/gradle/gradle-native/src/org/jetbrains/kotlin/idea/gradle/konan/KotlinNativeGradleConfigurator.kt
+++ plugins/kotlin/gradle/gradle-native/src/org/jetbrains/kotlin/idea/gradle/konan/KotlinNativeGradleConfigurator.kt
@@ -1,8 +1,7 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
-package org.jetbrains.kotlin.ide.konan.gradle
+package org.jetbrains.kotlin.idea.gradle.konan
 
-import org.jetbrains.kotlin.ide.konan.KotlinGradleNativeBundle
 import org.jetbrains.kotlin.ide.konan.hasKotlinNativeRuntimeInScope
 import org.jetbrains.kotlin.idea.configuration.ConfigureKotlinStatus
 import org.jetbrains.kotlin.idea.gradle.configuration.KotlinWithGradleConfigurator
