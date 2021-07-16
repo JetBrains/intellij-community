0eded5dacca1f36868ae0de3a7f32539abb7e0cb
adam.malek <adam.malek@jetbrains.com>
2021-07-16 13:25:07 +0100

    ML in SE: Add boolean features to time-related stats (IDEA-274480)
    Add features such as was the file modified in the
    last minute/hour/day/month for both file type usage
    and modification time.
    
    GitOrigin-RevId: 1c7073cc4dc81531bf0674f1c38912c2f2747acc

--- platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/ml/features/SearchEverywhereFileFeaturesProvider.kt
+++ platform/lang-impl/src/com/intellij/ide/actions/searcheverywhere/ml/features/SearchEverywhereFileFeaturesProvider.kt
@@ -11,16 +11,29 @@ import com.intellij.openapi.fileEditor.impl.EditorHistoryManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFileSystemItem
+import com.intellij.util.Time.*
 
 internal class SearchEverywhereFileFeaturesProvider : SearchEverywhereElementFeaturesProvider() {
   companion object {
+    private const val IS_DIRECTORY_DATA_KEY = "isDirectory"
     private const val FILETYPE_DATA_KEY = "fileType"
     private const val IS_FAVORITE_DATA_KEY = "isFavorite"
     private const val IS_OPENED_DATA_KEY = "isOpened"
     private const val RECENT_INDEX_DATA_KEY = "recentFilesIndex"
-    private const val TIME_SINCE_LAST_MODIFICATION_DATA_KEY = "timeSinceLastModification"
+
     private const val FILETYPE_USAGE_RATIO_DATA_KEY = "fileTypeUsageRatio"
-    private const val FILETYPE_LAST_USAGE_DATA_KEY = "fileTypeLastUsage"
+    private const val TIME_SINCE_LAST_FILETYPE_USAGE = "timeSinceLastFileTypeUsage"
+    private const val FILETYPE_USED_IN_LAST_MINUTE_DATA_KEY = "fileTypeUsedInLastMinute"
+    private const val FILETYPE_USED_IN_LAST_HOUR_DATA_KEY = "fileTypeUsedInLastHour"
+    private const val FILETYPE_USED_IN_LAST_DAY_DATA_KEY = "fileTypeUsedInLastDay"
+    private const val FILETYPE_USED_IN_LAST_MONTH_DATA_KEY = "fileTypeUsedInLastMonth"
+
+    private const val TIME_SINCE_LAST_MODIFICATION_DATA_KEY = "timeSinceLastModification"
+    private const val WAS_MODIFIED_IN_LAST_MINUTE_DATA_KEY = "wasModifiedInLastMinute"
+    private const val WAS_MODIFIED_IN_LAST_HOUR_DATA_KEY = "wasModifiedInLastHour"
+    private const val WAS_MODIFIED_IN_LAST_DAY_DATA_KEY = "wasModifiedInLastDay"
+    private const val WAS_MODIFIED_IN_LAST_MONTH_DATA_KEY = "wasModifiedInLastMonth"
   }
 
   override fun getElementFeatures(element: Any,
@@ -36,13 +49,23 @@ internal class SearchEverywhereFileFeaturesProvider : SearchEverywhereElementFea
     val virtualFile = (element as PsiFileSystemItem).virtualFile
     val project = element.project
 
-    return hashMapOf(
+    val data = hashMapOf<String, Any>(
       IS_FAVORITE_DATA_KEY to isFavorite(virtualFile, project),
-      IS_OPENED_DATA_KEY to isOpened(virtualFile, project),
-      FILETYPE_DATA_KEY to element.containingFile.fileType,
-      RECENT_INDEX_DATA_KEY to getRecentFilesIndex(virtualFile, project),
-      TIME_SINCE_LAST_MODIFICATION_DATA_KEY to currentTime - element.containingFile.virtualFile.timeStamp,
-    ) + getFileTypeStats(virtualFile)
+      IS_DIRECTORY_DATA_KEY to virtualFile.isDirectory
+    )
+
+    if (virtualFile.isDirectory) {
+      // Rest of the features are only applicable to files, not directories
+      return data
+    }
+
+    data[IS_OPENED_DATA_KEY] = isOpened(virtualFile, project)
+    data[FILETYPE_DATA_KEY] = virtualFile.fileType
+    data[RECENT_INDEX_DATA_KEY] = getRecentFilesIndex(virtualFile, project)
+    data.putAll(getModificationTimeStats(virtualFile, currentTime))
+    data.putAll(getFileTypeStats(virtualFile, currentTime))
+
+    return data
   }
 
   private fun isFavorite(virtualFile: VirtualFile, project: Project): Boolean {
@@ -68,7 +91,19 @@ internal class SearchEverywhereFileFeaturesProvider : SearchEverywhereElementFea
     return recentFilesList.size - fileIndex
   }
 
-  private fun getFileTypeStats(virtualFile: VirtualFile): Map<String, Any> {
+  private fun getModificationTimeStats(virtualFile: VirtualFile, currentTime: Long): Map<String, Any> {
+    val timeSinceLastMod = currentTime - virtualFile.timeStamp
+
+    return hashMapOf(
+      TIME_SINCE_LAST_MODIFICATION_DATA_KEY to timeSinceLastMod,
+      WAS_MODIFIED_IN_LAST_MINUTE_DATA_KEY to (timeSinceLastMod <= MINUTE),
+      WAS_MODIFIED_IN_LAST_HOUR_DATA_KEY to (timeSinceLastMod <= HOUR),
+      WAS_MODIFIED_IN_LAST_DAY_DATA_KEY to (timeSinceLastMod <= DAY),
+      WAS_MODIFIED_IN_LAST_MONTH_DATA_KEY to (timeSinceLastMod <= (4 * WEEK.toLong()))
+    )
+  }
+
+  private fun getFileTypeStats(virtualFile: VirtualFile, currentTime: Long): Map<String, Any> {
     val fileTypeName = virtualFile.fileType.name
     val localSummary = service<FileTypeUsageLocalSummary>()
     val allFileTypesStats = localSummary.getFileTypeStats()
@@ -76,17 +111,17 @@ internal class SearchEverywhereFileFeaturesProvider : SearchEverywhereElementFea
     val totalUsage = allFileTypesStats.values.sumBy { it.usageCount }
     val stats = allFileTypesStats[fileTypeName]
 
-    return if (stats == null) {
-      hashMapOf(
-        FILETYPE_USAGE_RATIO_DATA_KEY to 0.0,
-        FILETYPE_LAST_USAGE_DATA_KEY to 0.0,
-      )
-    }
-    else {
-      hashMapOf(
-        FILETYPE_USAGE_RATIO_DATA_KEY to stats.usageCount.toDouble() / totalUsage,
-        FILETYPE_LAST_USAGE_DATA_KEY to stats.lastUsed
-      )
-    }
+    val timeSinceLastUsage = if (stats == null) Long.MAX_VALUE else currentTime - stats.lastUsed
+    val usageRatio = if (stats == null) 0 else stats.usageCount.toDouble() / totalUsage
+
+    return hashMapOf(
+      FILETYPE_USAGE_RATIO_DATA_KEY to usageRatio,
+
+      TIME_SINCE_LAST_FILETYPE_USAGE to timeSinceLastUsage,
+      FILETYPE_USED_IN_LAST_MINUTE_DATA_KEY to (timeSinceLastUsage <= MINUTE),
+      FILETYPE_USED_IN_LAST_HOUR_DATA_KEY to (timeSinceLastUsage <= HOUR),
+      FILETYPE_USED_IN_LAST_DAY_DATA_KEY to (timeSinceLastUsage <= DAY),
+      FILETYPE_USED_IN_LAST_MONTH_DATA_KEY to (timeSinceLastUsage <= (4 * WEEK.toLong()))
+    )
   }
 }
