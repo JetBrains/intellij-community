424e8e0b26a58d9818e0aca20c96c78f37cafec4
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2021-07-19 23:09:00 +0300

    PY-48100, IDEA-272966:
    Use ```\\wsl$`` prefix instead of ``wsl://``.
    Latter one was filtered out by WslFilter
    
    (cherry picked from commit 0d0542b6b3be3e484b9eab7753c0508e6d69b83c)
    
    GitOrigin-RevId: 704ebceab110f50ecacbeecddc7bbccdf1e63360

--- platform/platform-impl/src/com/intellij/remote/ext/CredentialsManager.java
+++ platform/platform-impl/src/com/intellij/remote/ext/CredentialsManager.java
@@ -1,14 +1,18 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.remote.ext;
 
+import com.intellij.execution.wsl.WSLDistribution;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.projectRoots.SdkAdditionalData;
+import com.intellij.openapi.projectRoots.impl.ProjectJdkImpl;
 import com.intellij.remote.CredentialsType;
 import com.intellij.remote.OutdatedCredentialsType;
 import com.intellij.remote.RemoteSdkAdditionalData;
 import kotlin.Pair;
 import org.jdom.Element;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -16,12 +20,29 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public abstract class CredentialsManager {
+  private final static String WSL_PATH_TO_REMOVE = "wsl://";
 
   public static CredentialsManager getInstance() {
     return ApplicationManager.getApplication().getService(CredentialsManager.class);
   }
 
   public abstract List<CredentialsType<?>> getAllTypes();
+
+  /**
+   * Change old (wsl://) prefix to the new one (\\wsl$\)
+   *
+   * @deprecated remove after everyone migrates to the new prefix
+   */
+  @ApiStatus.ScheduledForRemoval(inVersion = "2022.1")
+  @Deprecated
+  public static void fixWslPrefix(@NotNull Sdk sdk) {
+    if (sdk instanceof ProjectJdkImpl) {
+      var path = sdk.getHomePath();
+      if (path != null && path.startsWith(WSL_PATH_TO_REMOVE)) {
+        ((ProjectJdkImpl)sdk).setHomePath(WSLDistribution.UNC_PREFIX + path.substring(WSL_PATH_TO_REMOVE.length()));
+      }
+    }
+  }
 
   public abstract void loadCredentials(String interpreterPath,
                                        @Nullable Element element,
