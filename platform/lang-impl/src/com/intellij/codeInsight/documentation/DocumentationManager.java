54b7ebf6de859e0856b4d1ded9895ccb20e648a6
Mikhail Sokolov <mikhail.sokolov@jetbrains.com>
2021-07-19 12:44:40 +0300

    FUS-1095 Change group `searchEverywhere`
    (cherry picked from commit 55c8c7edd0543a401ee04b3891458c826b98e009)
    
    IJ-CR-12136
    
    GitOrigin-RevId: 3a6fbe249b44064f1da1ea195b23cf8eecd46532

--- platform/lang-impl/src/com/intellij/codeInsight/documentation/DocumentationManager.java
+++ platform/lang-impl/src/com/intellij/codeInsight/documentation/DocumentationManager.java
@@ -15,6 +15,7 @@ import com.intellij.ide.BrowserUtil;
 import com.intellij.ide.IdeEventQueue;
 import com.intellij.ide.actions.BaseNavigateToSourceAction;
 import com.intellij.ide.actions.WindowAction;
+import com.intellij.ide.actions.searcheverywhere.PSIPresentationBgRendererWrapper;
 import com.intellij.ide.highlighter.ArchiveFileType;
 import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.ide.util.gotoByName.ChooseByNameBase;
@@ -530,7 +531,7 @@ public class DocumentationManager extends DockablePopupManager<DocumentationComp
     PopupUpdateProcessor updateProcessor = new PopupUpdateProcessor(element.getProject()) {
       @Override
       public void updatePopup(Object lookupItemObject) {
-        PsiElement psiElement = toPsi(lookupItemObject);
+        PsiElement psiElement = PSIPresentationBgRendererWrapper.toPsi(lookupItemObject);
         if (psiElement != null) {
           doShowJavaDocInfo(psiElement, requestFocus, this, original, null, null,
                             useStoredPopupSize, onAutoUpdate);
@@ -582,7 +583,7 @@ public class DocumentationManager extends DockablePopupManager<DocumentationComp
                             true, onAutoUpdate);
           return;
         }
-        PsiElement psiElement = toPsi(lookupItemObject);
+        PsiElement psiElement = PSIPresentationBgRendererWrapper.toPsi(lookupItemObject);
         if (psiElement != null) {
           doShowJavaDocInfo(psiElement, false, this, originalElement, closeCallback,
                             null, true, onAutoUpdate);
