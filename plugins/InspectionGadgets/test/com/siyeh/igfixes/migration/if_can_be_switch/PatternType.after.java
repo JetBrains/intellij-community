4e3e88273608912eb5c3dd5132cffb3a3b85086d
Alexandr Suhinin <alexandr.suhinin@jetbrains.com>
2021-07-19 18:01:40 +0300

    IDEA-273242 replace switch with enhanced switch: suggest with empty default branch
    GitOrigin-RevId: ec8d6e47f488d009a94092fe02bb5cc214a1f0dc

--- plugins/InspectionGadgets/test/com/siyeh/igfixes/migration/if_can_be_switch/PatternType.after.java
+++ plugins/InspectionGadgets/test/com/siyeh/igfixes/migration/if_can_be_switch/PatternType.after.java
@@ -3,14 +3,10 @@ import org.jetbrains.annotations.NotNull;
 class Test {
     void test(@NotNull Object o) {
         switch (o) {
-            case String s:
-                System.out.println();
-                break;
-            case Integer i:
-                System.out.println();
-                break;
-            default:
-                break;
+            case String s -> System.out.println();
+            case Integer i -> System.out.println();
+            default -> {
+            }
         }
     }
 }
