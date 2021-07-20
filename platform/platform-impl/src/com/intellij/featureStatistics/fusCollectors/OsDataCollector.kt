d11eddd8a3fc733404d4a3c58c8b4daa4bd9918e
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2021-07-20 22:19:16 +0200

    [platform] OS statistics: restoring timezone format, restricting locales (IJ-CR-11963)
    GitOrigin-RevId: 4b4208a243d0747b591203bc813ddea403a9c316

--- platform/platform-impl/src/com/intellij/featureStatistics/fusCollectors/OsDataCollector.kt
+++ platform/platform-impl/src/com/intellij/featureStatistics/fusCollectors/OsDataCollector.kt
@@ -3,11 +3,10 @@ package com.intellij.featureStatistics.fusCollectors
 
 import com.intellij.internal.statistic.beans.MetricEvent
 import com.intellij.internal.statistic.eventLog.EventLogGroup
-import com.intellij.internal.statistic.eventLog.events.EventFields.Int
 import com.intellij.internal.statistic.eventLog.events.EventFields.String
-import com.intellij.internal.statistic.eventLog.events.EventFields.StringValidatedByInlineRegexp
 import com.intellij.internal.statistic.eventLog.events.EventFields.StringValidatedByRegexp
 import com.intellij.internal.statistic.eventLog.events.EventFields.Version
+import com.intellij.internal.statistic.eventLog.events.StringEventField
 import com.intellij.internal.statistic.service.fus.collectors.ApplicationUsagesCollector
 import com.intellij.openapi.util.SystemInfo
 import java.io.IOException
@@ -15,11 +14,15 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.time.OffsetDateTime
 import java.util.*
-import kotlin.math.roundToInt
 import kotlin.streams.asSequence
 
 internal class OsDataCollector : ApplicationUsagesCollector() {
   private val OS_NAMES = listOf("Windows", "Mac", "Linux", "FreeBSD", "Solaris", "Other")
+
+  private val LOCALES = listOf(
+    "am", "ar", "as", "az", "bn", "cs", "da", "de", "el", "en", "es", "fa", "fr", "gu", "ha", "hi", "hu", "ig", "in", "it", "ja", "kk",
+    "kn", "ko", "ml", "mr", "my", "nb", "ne", "nl", "nn", "no", "or", "pa", "pl", "pt", "ro", "ru", "rw", "sd", "si", "so", "sv", "ta",
+    "te", "th", "tr", "uk", "ur", "uz", "vi", "yo", "zh", "zu")
 
   @Suppress("SpellCheckingInspection")
   private val DISTROS = listOf(
@@ -28,9 +31,14 @@ internal class OsDataCollector : ApplicationUsagesCollector() {
     "parrot", "pop", "pureos", "raspbian", "rhel", "rocky", "rosa", "sabayon", "slackware", "solus", "ubuntu", "void", "zorin",
     "other", "unknown")
 
+  private data class TimezoneField(override val name: String) : StringEventField(name) {
+    override val validationRule: List<String>
+      get() = listOf("{regexp#time_zone}", "{enum:Z}")
+  }
+
   private val GROUP = EventLogGroup("system.os", 9)
-  private val NAME = GROUP.registerEvent("os.name", String("name", OS_NAMES), Version, StringValidatedByInlineRegexp("locale", "[a-z]{2}"))
-  private val TIMEZONE = GROUP.registerEvent("os.timezone", Int("value"))
+  private val NAME = GROUP.registerEvent("os.name", String("name", OS_NAMES), Version, String("locale", LOCALES))
+  private val TIMEZONE = GROUP.registerEvent("os.timezone", TimezoneField("value"))
   private val LINUX = GROUP.registerEvent("linux", String("distro", DISTROS), StringValidatedByRegexp("release", "version"))
 
   override fun getGroup(): EventLogGroup = GROUP
@@ -38,7 +46,7 @@ internal class OsDataCollector : ApplicationUsagesCollector() {
   override fun getMetrics(): Set<MetricEvent> {
     val metrics = mutableSetOf(
       NAME.metric(getOSName(), SystemInfo.OS_VERSION, Locale.getDefault().language),
-      TIMEZONE.metric((OffsetDateTime.now().offset.totalSeconds / 3600.0).roundToInt()))
+      TIMEZONE.metric((OffsetDateTime.now().offset.toString())))
     if (SystemInfo.isLinux) {
       val (distro, release) = getReleaseData()
       metrics += LINUX.metric(distro, release)
