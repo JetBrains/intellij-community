5d931979e5cc685c64f297c7accef6f6878387ae
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2021-07-20 21:53:30 +0200

    [ui] dropping UTM parameters from initial "what's new" request (IDEA-274113)
    (cherry picked from commit acce1b383cf7281aae840002ac3253f55dd1f781)
    
    IJ-CR-12100
    
    GitOrigin-RevId: d8a8eace7d85e25f13cb065ff1c0dbf1d8e1ca7b

--- platform/platform-impl/src/com/intellij/ide/actions/WhatsNewAction.java
+++ platform/platform-impl/src/com/intellij/ide/actions/WhatsNewAction.java
@@ -19,7 +19,6 @@ import com.intellij.openapi.updateSettings.impl.UpdateChecker;
 import com.intellij.openapi.util.NlsContexts.DetailedDescription;
 import com.intellij.openapi.util.io.StreamUtil;
 import com.intellij.ui.jcef.JBCefApp;
-import com.intellij.util.Url;
 import com.intellij.util.Urls;
 import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.ApiStatus;
@@ -87,11 +86,8 @@ public class WhatsNewAction extends AnAction implements DumbAware {
     else if (url != null) {
       boolean darkTheme = UIUtil.isUnderDarcula();
 
-      Url embeddedUrl = Urls.newFromEncoded(url).addParameters(Map.of("var", "embed"));
-      if (darkTheme) {
-        embeddedUrl = embeddedUrl.addParameters(Map.of("theme", "dark"));
-      }
-      String finalUrl = IdeUrlTrackingParametersProvider.getInstance().augmentUrl(embeddedUrl.toExternalForm());
+      Map<String, String> parameters = darkTheme ? Map.of("var", "embed", "theme", "dark") : Map.of("var", "embed");
+      String embeddedUrl = Urls.newFromEncoded(url).addParameters(parameters).toExternalForm();
 
       String timeoutContent = null;
       try (InputStream html = WhatsNewAction.class.getResourceAsStream("whatsNewTimeoutText.html")) {
@@ -108,7 +104,7 @@ public class WhatsNewAction extends AnAction implements DumbAware {
         Logger.getInstance(WhatsNewAction.class).error(e);
       }
 
-      HTMLEditorProvider.openEditor(project, title, finalUrl, timeoutContent);
+      HTMLEditorProvider.openEditor(project, title, embeddedUrl, timeoutContent);
     }
     else {
       HTMLEditorProvider.openEditor(project, title, content);
