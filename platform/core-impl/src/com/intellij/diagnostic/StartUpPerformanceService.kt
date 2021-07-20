d84c17bcf6587e02ebbdb374b6768219d7a884f1
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2021-07-20 09:08:31 +0200

    cleanup
    GitOrigin-RevId: 8d97445daf7511b11a9b0a8dbd4b5e9c4e22c06c

--- platform/core-impl/src/com/intellij/diagnostic/StartUpPerformanceService.kt
+++ platform/core-impl/src/com/intellij/diagnostic/StartUpPerformanceService.kt
@@ -1,41 +1,33 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.diagnostic;
+package com.intellij.diagnostic
 
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.extensions.impl.ExtensionPointImpl;
-import com.intellij.openapi.project.Project;
-import com.intellij.util.ThreeState;
-import it.unimi.dsi.fastutil.objects.Object2IntMap;
-import it.unimi.dsi.fastutil.objects.Object2LongMap;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.nio.ByteBuffer;
-import java.util.Map;
-import java.util.Objects;
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.extensions.impl.ExtensionPointImpl
+import com.intellij.openapi.project.Project
+import com.intellij.util.ThreeState
+import it.unimi.dsi.fastutil.objects.Object2IntMap
+import it.unimi.dsi.fastutil.objects.Object2LongMap
+import org.jetbrains.annotations.ApiStatus
+import java.nio.ByteBuffer
 
 @ApiStatus.Internal
-public interface StartUpPerformanceService {
-  void reportStatistics(@NotNull Project project);
-
-  @NotNull Map<String, Object2LongMap<String>> getPluginCostMap();
-
-  @Nullable Object2IntMap<String> getMetrics();
-
-  @Nullable ByteBuffer getLastReport();
-
-  @NotNull
-  static StartUpPerformanceService getInstance() {
-    try {
-      @SuppressWarnings("unchecked")
-      Class<StartUpPerformanceService> aClass = (Class<StartUpPerformanceService>)StartUpPerformanceService.class.getClassLoader()
-        .loadClass("com.intellij.diagnostic.startUpPerformanceReporter.StartUpPerformanceReporter");
-      ExtensionPointImpl<Object> ep = (ExtensionPointImpl<Object>)ApplicationManager.getApplication().getExtensionArea().getExtensionPoint("com.intellij.startupActivity");
-      return Objects.requireNonNull(ep.findExtension(aClass, true, ThreeState.YES));
-    }
-    catch (ClassNotFoundException e) {
-      throw new RuntimeException(e);
+interface StartUpPerformanceService {
+  companion object {
+    @JvmStatic
+    fun getInstance(): StartUpPerformanceService {
+      @Suppress("UNCHECKED_CAST") val aClass = StartUpPerformanceService::class.java.classLoader
+        .loadClass("com.intellij.diagnostic.startUpPerformanceReporter.StartUpPerformanceReporter") as Class<StartUpPerformanceService>
+      val ep = ApplicationManager.getApplication().extensionArea
+        .getExtensionPoint<Any>("com.intellij.startupActivity") as ExtensionPointImpl<Any>
+      return ep.findExtension(aClass, true, ThreeState.YES)!!
     }
   }
+
+  fun reportStatistics(project: Project)
+
+  fun getPluginCostMap(): Map<String, Object2LongMap<String>>
+
+  fun getMetrics(): Object2IntMap<String>?
+
+  fun getLastReport(): ByteBuffer?
 }
