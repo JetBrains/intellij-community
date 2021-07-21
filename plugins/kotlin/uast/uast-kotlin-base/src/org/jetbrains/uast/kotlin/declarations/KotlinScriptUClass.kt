1ef1e8c14bdf72e17f5ad40567030b75c297e397
Jinseong Jeon <jsjeon@google.com>
2021-07-20 22:50:20 -0700

    FIR/UAST: commonize UClass (entirely)
    After commonizing UFunctionCallExpression, which forces commonzing
    UMethod for constructors, UClass can be fully commonized now.
    
    GitOrigin-RevId: 0414167ab6c89ebf65ce34b753600b6e5eae83eb

--- plugins/kotlin/uast/uast-kotlin-base/src/org/jetbrains/uast/kotlin/declarations/KotlinScriptUClass.kt
+++ plugins/kotlin/uast/uast-kotlin-base/src/org/jetbrains/uast/kotlin/declarations/KotlinScriptUClass.kt
@@ -1,42 +1,24 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-
+/*
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
 package org.jetbrains.uast.kotlin
 
 import com.intellij.psi.*
-import org.jetbrains.kotlin.asJava.classes.KtLightClass
 import org.jetbrains.kotlin.asJava.classes.KtLightClassForScript
 import org.jetbrains.kotlin.asJava.elements.KtLightMethod
-import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.KtClassOrObject
+import org.jetbrains.kotlin.psi.KtScript
+import org.jetbrains.kotlin.psi.KtScriptInitializer
 import org.jetbrains.uast.*
 
-class KotlinUClass(
-    psi: KtLightClass,
-    givenParent: UElement?
-) : BaseKotlinUClass(psi, givenParent) {
-    override fun buildPrimaryConstructorUMethod(ktClass: KtClassOrObject?, psi: KtLightMethod, givenParent: UElement?): UMethod {
-        return KotlinConstructorUMethod(ktClass, psi, givenParent)
-    }
-
-    override fun buildSecondaryConstructorUMethod(ktClass: KtClassOrObject?, psi: KtLightMethod, givenParent: UElement?): UMethod {
-        return KotlinSecondaryConstructorWithInitializersUMethod(ktClass, psi, givenParent)
-    }
-
-    companion object {
-        fun create(psi: KtLightClass, containingElement: UElement?): UClass = when (psi) {
-            is PsiAnonymousClass -> KotlinUAnonymousClass(psi, containingElement)
-            is KtLightClassForScript -> KotlinScriptUClass(psi, containingElement)
-            else -> KotlinUClass(psi, containingElement)
-        }
-    }
-}
-
 class KotlinScriptUClass(
-        psi: KtLightClassForScript,
-        givenParent: UElement?
+    psi: KtLightClassForScript,
+    givenParent: UElement?
 ) : AbstractKotlinUClass(givenParent), PsiClass by psi {
     override fun getContainingFile(): PsiFile = unwrapFakeFileForLightClass(psi.containingFile)
 
-    override fun getNameIdentifier(): PsiIdentifier? = UastLightIdentifier(psi, psi.kotlinOrigin)
+    override fun getNameIdentifier(): PsiIdentifier = UastLightIdentifier(psi, psi.kotlinOrigin)
 
     override val uastAnchor by lazy { KotlinUIdentifier(nameIdentifier, sourcePsi?.nameIdentifier, this) }
 
@@ -53,7 +35,7 @@ class KotlinScriptUClass(
     override fun getInitializers(): Array<UClassInitializer> = super.getInitializers()
 
     override fun getInnerClasses(): Array<UClass> =
-            psi.innerClasses.mapNotNull { getLanguagePlugin().convertOpt<UClass>(it, this) }.toTypedArray()
+        psi.innerClasses.mapNotNull { getLanguagePlugin().convertOpt<UClass>(it, this) }.toTypedArray()
 
     override fun getMethods(): Array<UMethod> = psi.methods.map(this::createUMethod).toTypedArray()
 
@@ -62,7 +44,7 @@ class KotlinScriptUClass(
             KotlinScriptConstructorUMethod(psi.script, method as KtLightMethod, this)
         }
         else {
-            getLanguagePlugin().convertOpt(method, this) ?: reportConvertFailure(method)
+            languagePlugin?.convertOpt(method, this) ?: reportConvertFailure(method)
         }
     }
 
@@ -72,7 +54,7 @@ class KotlinScriptUClass(
         script: KtScript,
         override val psi: KtLightMethod,
         givenParent: UElement?
-    ) : KotlinUMethod(psi, psi.kotlinOrigin, givenParent) {
+    ) : BaseKotlinUMethod(psi, psi.kotlinOrigin, givenParent) {
         override val uastBody: UExpression? by lz {
             val initializers = script.declarations.filterIsInstance<KtScriptInitializer>()
             KotlinLazyUBlockExpression.create(initializers, this)
