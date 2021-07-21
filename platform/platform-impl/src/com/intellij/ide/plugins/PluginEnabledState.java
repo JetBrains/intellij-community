c0a49c64c95b85aafb4c1130c43c8d795b20afae
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2021-07-21 15:16:17 +0200

    [plugins] labels simplified if per project plugins are disabled
    PluginEnabler moved to `platform-impl` #IDEA-274358 fixed
    
    (cherry-picked from commits 5efca3d6dafafe34784bfec92d44a30a4001003b, db869a19100c329a52b3498b4b1649f020d89e59)
    
    IJ-CR-12202
    
    GitOrigin-RevId: 93b9e57ffccaef7417c195cfb6ff37699339f393

---
+++ platform/platform-impl/src/com/intellij/ide/plugins/PluginEnabledState.java
@@ -0,0 +1,65 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.ide.plugins;
+
+import com.intellij.ide.IdeBundle;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.PropertyKey;
+
+public enum PluginEnabledState {
+
+  ENABLED {
+    @Override
+    protected @NotNull @PropertyKey(resourceBundle = IdeBundle.BUNDLE) String getPropertyKey() {
+      return PluginEnabler.isPerProjectEnabled() ? "plugins.configurable.enabled.for.all.projects" : "plugins.configurable.enabled";
+    }
+  },
+  ENABLED_FOR_PROJECT {
+    @Override
+    protected @NotNull @PropertyKey(resourceBundle = IdeBundle.BUNDLE) String getPropertyKey() {
+      return "plugins.configurable.enabled.for.current.project";
+    }
+  },
+  DISABLED {
+    @Override
+    protected @NotNull @PropertyKey(resourceBundle = IdeBundle.BUNDLE) String getPropertyKey() {
+      return PluginEnabler.isPerProjectEnabled() ? "plugins.configurable.disabled.for.all.projects" : "plugins.configurable.disabled";
+    }
+  },
+  DISABLED_FOR_PROJECT {
+    @Override
+    protected @NotNull @PropertyKey(resourceBundle = IdeBundle.BUNDLE) String getPropertyKey() {
+      return "plugins.configurable.disabled.for.current.project";
+    }
+  };
+
+  protected abstract @NotNull @PropertyKey(resourceBundle = IdeBundle.BUNDLE) String getPropertyKey();
+
+  public @NotNull @Nls String getPresentableText() {
+    return IdeBundle.message(getPropertyKey());
+  }
+
+  public boolean isEnabled() {
+    return this == ENABLED || this == ENABLED_FOR_PROJECT;
+  }
+
+  public boolean isDisabled() {
+    return this == DISABLED || this == DISABLED_FOR_PROJECT;
+  }
+
+  public boolean isPerProject() {
+    return this == ENABLED_FOR_PROJECT || this == DISABLED_FOR_PROJECT;
+  }
+
+  public @NotNull PluginEnabledState getInverted() {
+    return getState(isDisabled(), isPerProject());
+  }
+
+  public static @NotNull PluginEnabledState getState(boolean enabled, boolean perProject) {
+    return perProject ? enabled ? ENABLED_FOR_PROJECT : DISABLED_FOR_PROJECT : globally(enabled);
+  }
+
+  public static @NotNull PluginEnabledState globally(boolean enabled) {
+    return enabled ? ENABLED : DISABLED;
+  }
+}
--- platform/platform-impl/src/com/intellij/ide/plugins/PluginEnabler.java
+++ platform/platform-impl/src/com/intellij/ide/plugins/PluginEnabler.java
@@ -1,13 +1,19 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.plugins;
 
 import com.intellij.openapi.extensions.PluginId;
+import com.intellij.openapi.util.registry.Registry;
+import com.intellij.openapi.util.registry.RegistryValue;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
 import java.util.LinkedHashSet;
 
 public interface PluginEnabler {
+
+  @ApiStatus.Internal
+  @NotNull RegistryValue ourIsPerProjectEnabled = Registry.get("ide.plugins.per.project");
 
   void setEnabledState(@NotNull Collection<? extends IdeaPluginDescriptor> descriptors,
                        @NotNull PluginEnableDisableAction action);
@@ -22,6 +28,10 @@ public interface PluginEnabler {
     setEnabledState(descriptors, PluginEnableDisableAction.DISABLE_GLOBALLY);
   }
 
+  static boolean isPerProjectEnabled() {
+    return ourIsPerProjectEnabled.asBoolean();
+  }
+
   PluginEnabler HEADLESS = new PluginEnabler() {
 
     @Override
@@ -31,11 +41,7 @@ public interface PluginEnabler {
       LinkedHashSet<PluginId> pluginIds = new LinkedHashSet<>(descriptors.size());
       for (IdeaPluginDescriptor descriptor : descriptors) {
         descriptor.setEnabled(enabled);
-
-        PluginId pluginId = descriptor.getPluginId();
-        if (pluginId != null) {
-          pluginIds.add(pluginId);
-        }
+        pluginIds.add(descriptor.getPluginId());
       }
 
       DisabledPluginsState.setEnabledState(pluginIds, enabled);
