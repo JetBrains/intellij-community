84ed9c3b0c3e2a8f2f2891f7ef55d82b7fda26e7
Yuriy Solodkyy <solodkyy@google.com>
2021-07-22 19:46:56 +0100

    Increase idle Gradle timeout in tests to 100 seconds
    The default 10 second value results in the daemon being shut down before
    the IDE completes indexing after several initial project syncs.

--- plugins/gradle/src/org/jetbrains/plugins/gradle/GradleConnectorService.kt
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/GradleConnectorService.kt
@@ -245,7 +245,7 @@ internal class GradleConnectorService(@Suppress("UNUSED_PARAMETER") project: Pro
       }
       // do not spawn gradle daemons during test execution
       val app = ApplicationManager.getApplication()
-      val ttl = if (app != null && app.isUnitTestMode) 10000 else connectorParams.ttlMs ?: -1
+      val ttl = if (app != null && app.isUnitTestMode) 100_000 else connectorParams.ttlMs ?: -1
       if (ttl > 0 && connector is DefaultGradleConnector) {
         connector.daemonMaxIdleTime(ttl, TimeUnit.MILLISECONDS)
       }
