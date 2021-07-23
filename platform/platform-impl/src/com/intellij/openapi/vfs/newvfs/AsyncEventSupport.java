21e9271b7f409f874211d7844f5919437899a30e
Anna Kozlova <anna.kozlova@jetbrains.com>
2021-07-23 10:57:08 +0200

    do not log PCE
    GitOrigin-RevId: 394ab79f03ab528befab52425fbd8e021ce3ecb5

--- platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/AsyncEventSupport.java
+++ platform/platform-impl/src/com/intellij/openapi/vfs/newvfs/AsyncEventSupport.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vfs.newvfs;
 
 import com.intellij.openapi.application.Application;
@@ -135,6 +135,9 @@ public final class AsyncEventSupport {
       PingProgress.interactWithEdtProgress();
       try {
         applier.afterVfsChange();
+      }
+      catch (ProcessCanceledException e) {
+        throw e;
       }
       catch (Throwable e) {
         LOG.error(e);
