5cb9ecb60ba3b27543facb4d1805e6bcc7e8443d
Vyacheslav Karpukhin <Vyacheslav.Karpukhin@jetbrains.com>
2021-07-26 23:21:04 +0200

    KMA-260 Incremented kotlin.ide.new.project version
    (cherry picked from commit d4a711233347167360414883af4ff296388b08cd)
    (cherry picked from commit 06b130aebcc74578909d86e899a16ceeae2c415f)
    
    GitOrigin-RevId: d5cad786e588af525018a4aa8509b14ca6ecc463

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/projectWizard/WizardStatsService.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/projectWizard/WizardStatsService.kt
@@ -23,7 +23,7 @@ class WizardStatsService : CounterUsagesCollector() {
     companion object {
 
         // Collector ID
-        private val GROUP = EventLogGroup("kotlin.ide.new.project", 7)
+        private val GROUP = EventLogGroup("kotlin.ide.new.project", 8)
 
         // Whitelisted values for the events fields
         private val allowedProjectTemplates = listOf( // Modules
