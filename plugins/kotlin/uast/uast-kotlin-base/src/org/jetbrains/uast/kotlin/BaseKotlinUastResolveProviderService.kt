fc9deca02cb106b61c2b243bf833c1261e937781
Jinseong Jeon <jsjeon@google.com>
2021-07-26 12:18:29 -0700

    FIR/UAST: resolve KtAnnotationEntry to annotation class
    GitOrigin-RevId: 7882eaa643e645a1282a0ea23bc931e3c391949c

--- plugins/kotlin/uast/uast-kotlin-base/src/org/jetbrains/uast/kotlin/BaseKotlinUastResolveProviderService.kt
+++ plugins/kotlin/uast/uast-kotlin-base/src/org/jetbrains/uast/kotlin/BaseKotlinUastResolveProviderService.kt
@@ -2,6 +2,7 @@
 
 package org.jetbrains.uast.kotlin
 
+import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiMethod
 import com.intellij.psi.PsiType
@@ -51,7 +52,9 @@ interface BaseKotlinUastResolveProviderService {
 
     fun callKind(ktCallElement: KtCallElement): UastCallKind
 
-    fun resolveToClassIfConstructorCall(ktCallElement: KtCallElement, source: UElement): PsiElement?
+    fun resolveToClassIfConstructorCall(ktCallElement: KtCallElement, source: UElement): PsiClass?
+
+    fun resolveToClass(ktAnnotationEntry: KtAnnotationEntry): PsiClass?
 
     fun resolveToDeclaration(ktExpression: KtExpression): PsiElement?
 
