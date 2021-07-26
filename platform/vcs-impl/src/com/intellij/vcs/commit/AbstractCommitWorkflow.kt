367bdd845fe9b5c3d23dd4dc3779f61bc356b6bb
Aleksey Pivovarov <AMPivovarov@gmail.com>
2021-07-26 18:36:30 +0300

    vcs: deprecate unused Refreshable.refresh method
    Only actual call was in p4.RevertAllUnchangedFilesAction, replace it with restoreState.
    
    GitOrigin-RevId: 4ba0705dab59f11d6a0b0e711da066d9a5b72de4

--- platform/vcs-impl/src/com/intellij/vcs/commit/AbstractCommitWorkflow.kt
+++ platform/vcs-impl/src/com/intellij/vcs/commit/AbstractCommitWorkflow.kt
@@ -38,7 +38,6 @@ private val LOG = logger<AbstractCommitWorkflow>()
 
 internal fun CommitOptions.saveState() = allOptions.forEach { it.saveState() }
 internal fun CommitOptions.restoreState() = allOptions.forEach { it.restoreState() }
-internal fun CommitOptions.refresh() = allOptions.forEach { it.refresh() }
 
 private class CommitProperty<T>(private val key: Key<T>, private val defaultValue: T) : ReadWriteProperty<CommitContext, T> {
   override fun getValue(thisRef: CommitContext, property: KProperty<*>): T = thisRef.getUserData(key) ?: defaultValue
