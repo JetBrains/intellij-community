26d023c93a1ba8940cae43f73264203930e3e339
Jinseong Jeon <jsjeon@google.com>
2021-07-27 23:10:11 +0900

    FIR/UAST: commonize API tests
    GitOrigin-RevId: 8493d169a312168d669e34b700c430e2f8cfc022

--- plugins/kotlin/uast/uast-kotlin-base/test/org/jetbrains/uast/test/common/kotlin/utils.kt
+++ plugins/kotlin/uast/uast-kotlin-base/test/org/jetbrains/uast/test/common/kotlin/utils.kt
@@ -2,10 +2,33 @@
 
 package org.jetbrains.uast.test.common.kotlin
 
+import com.intellij.util.PairProcessor
+import com.intellij.util.ref.DebugReflectionUtil
+import junit.framework.TestCase
 import org.jetbrains.kotlin.asJava.classes.KtLightClassForFacade
+import org.jetbrains.kotlin.cli.jvm.compiler.CliTraceHolder
+import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
+import org.jetbrains.kotlin.descriptors.annotations.AnnotationDescriptor
 import org.jetbrains.uast.UClass
+import org.jetbrains.uast.UElement
 import org.jetbrains.uast.UFile
 
 internal fun UFile.findFacade(): UClass? {
     return classes.find { it.psi is KtLightClassForFacade }
+}
+
+private val descriptorsClasses = listOf(AnnotationDescriptor::class, DeclarationDescriptor::class)
+
+fun checkDescriptorsLeak(node: UElement) {
+    DebugReflectionUtil.walkObjects(
+        10,
+        mapOf(node to node.javaClass.name),
+        Any::class.java,
+        { it !is CliTraceHolder },
+        PairProcessor { value, backLink ->
+            descriptorsClasses.find { it.isInstance(value) }?.let {
+                TestCase.fail("""Leaked descriptor ${it.qualifiedName} in ${node.javaClass.name}\n$backLink""")
+                false
+            } ?: true
+        })
 }
