d5b3a6149661c770d1d23f77a436b4ae589d7bc7
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2021-07-27 15:33:59 +0200

    Cleanup (unsolicited copyright)
    GitOrigin-RevId: 74d61a6122d5b8ef2a5108620e7b677db42a5b69

--- native/fsNotifier/linux/make.sh
+++ native/fsNotifier/linux/make.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 VER=$(date "+%Y%m%d.%H%M")
 sed -i.bak "s/#define VERSION .*/#define VERSION \"${VER}\"/" fsnotifier.h && rm fsnotifier.h.bak
 
