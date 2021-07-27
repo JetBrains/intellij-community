b4e1693fb3b5ac1423026cd30a8ccb46fd1fd729
Alexey Kudravtsev <cdr@intellij.com>
2021-07-27 22:54:06 +0200

    remove trove4j from OpenAPI module dependencies
    GitOrigin-RevId: 1232755f070379667d8d50abef2a39e5a94fe710

--- plugins/gradle/tooling-extension-api/src/org/jetbrains/plugins/gradle/tooling/util/IntObjectMap.java
+++ plugins/gradle/tooling-extension-api/src/org/jetbrains/plugins/gradle/tooling/util/IntObjectMap.java
@@ -1,17 +1,16 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.gradle.tooling.util;
 
-import gnu.trove.TIntObjectHashMap;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Vladislav.Soroka
  */
 public class IntObjectMap<T> {
-  private final TIntObjectHashMap<T> myObjectsMap = new TIntObjectHashMap<T>();
+  private final IntObjectHashMap myObjectsMap = new IntObjectHashMap();
 
   public T computeIfAbsent(int objectID, @NotNull ObjectFactory<T> objectFactory) {
-    T object = myObjectsMap.get(objectID);
+    T object = (T)myObjectsMap.get(objectID);
     if (object == null) {
       object = objectFactory.newInstance();
       myObjectsMap.put(objectID, object);
@@ -37,4 +36,77 @@ public class IntObjectMap<T> {
     @Override
     public void fill(T object) { }
   }
-}
+
+
+/**
+ * Specialized memory saving map implementation to avoid extra dependencies.
+ */
+static final class IntObjectHashMap {
+  private int size;
+  private int[] keys;
+  private Object[] values;
+  private Object specialZeroValue;
+  private boolean hasZeroValue;
+
+  IntObjectHashMap() {
+    keys = new int[4];
+    values = new Object[keys.length];
+  }
+
+  void put(int key, @NotNull Object value) {
+    if (key == 0) {
+      specialZeroValue = value;
+      hasZeroValue = true;
+      return;
+    }
+
+    if (size >= (2 * values.length) / 3) {
+      rehash();
+    }
+    Object previousValue = doPut(keys, values, key, value);
+    if (previousValue == null) {
+      ++size;
+    }
+  }
+
+  private static Object doPut(@NotNull int[] keys, @NotNull Object[] values, int key, @NotNull Object value) {
+    int index = hashIndex(keys, key);
+    Object obj = values[index];
+    values[index] = value;
+    if (keys[index] == 0) keys[index] = key;
+    return obj;
+  }
+
+  private static int hashIndex(@NotNull int[] keys, int key) {
+    int hash = (int)((key * 0x9E3779B9L) & 0x7fffffff);
+    int index = hash & (keys.length - 1);
+
+    int candidate;
+    while ((candidate = keys[index]) != 0) {
+      if (candidate == key) return index;
+      if (index == 0) index = keys.length;
+      index--;
+    }
+
+    return index;
+  }
+
+  private void rehash() {
+    int[] newKeys = new int[keys.length << 1];
+    Object[] newValues = new Object[newKeys.length];
+
+    for (int i = keys.length - 1; i >= 0; i--) {
+      int key = keys[i];
+      if (key != 0) {
+        doPut(newKeys, newValues, key, values[i]);
+      }
+    }
+
+    keys = newKeys;
+    values = newValues;
+  }
+
+  public Object get(int key) {
+    return key == 0 ? specialZeroValue : values[hashIndex(keys, key)];
+  }
+}}
