f6213fb42dc2c40a1e2840fa778eeedb342c9ae0
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2021-07-28 21:53:53 +0900

    [kotlin] Migrate kotlin.compiler-plugins.parcelize.gradle to the new plugin model
    GitOrigin-RevId: 592afade6e9ad7095ca848622e38b57a8fa009d1

--- plugins/kotlin/compiler-plugins/parcelize/gradle/src/org/jetbrains/kotlin/idea/compilerPlugin/parcelize/gradleJava/GradleParcelizeAvailabilityProvider.kt
+++ plugins/kotlin/compiler-plugins/parcelize/gradle/src/org/jetbrains/kotlin/idea/compilerPlugin/parcelize/gradleJava/GradleParcelizeAvailabilityProvider.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.kotlin.idea.gradleJava.compilerPlugin.parcelize
+package org.jetbrains.kotlin.idea.compilerPlugin.parcelize.gradleJava
 
 import org.jetbrains.kotlin.idea.compilerPlugin.parcelize.ParcelizeAvailabilityProvider
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
