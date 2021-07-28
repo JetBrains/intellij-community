74eee9f0d8b32934a431e54603ad4dfd83916c05
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-07-28 15:05:57 +0200

    HighLevelCompletionMultifileHandlerTest: Fix compilation
    GitOrigin-RevId: 6dff78880f3ddb7d58fbd770a019e6381555b2e3

--- plugins/kotlin/fir/test/org/jetbrains/kotlin/idea/fir/completion/test/handlers/HighLevelCompletionMultifileHandlerTest.kt
+++ plugins/kotlin/fir/test/org/jetbrains/kotlin/idea/fir/completion/test/handlers/HighLevelCompletionMultifileHandlerTest.kt
@@ -5,21 +5,21 @@ package org.jetbrains.kotlin.idea.completion.test.handlers
 import com.intellij.testFramework.LightProjectDescriptor
 import org.jetbrains.kotlin.idea.test.KotlinWithJdkAndRuntimeLightProjectDescriptor
 import org.junit.internal.runners.JUnit38ClassRunner
-import org.jetbrains.kotlin.test.uitls.IgnoreTests
+import org.jetbrains.kotlin.test.utils.IgnoreTests
 import org.junit.runner.RunWith
 import java.nio.file.Paths
 
-@RunWith(JUnit3WithIdeaConfigurationRunner::class)
+@RunWith(JUnit38ClassRunner::class)
 class HighLevelCompletionMultifileHandlerTest : CompletionMultiFileHandlerTest() {
 
     /**
      * This is a temporary solution! This test should be rewritten to be generated!
      */
-    override fun doTest(fileName: String, completionChar: Char, extraFileNames: Array<out String>, tailText: String?) {
-        val testFile = Paths.get(testDataPath, "$fileName-1.kt")
+    override fun doTest(completionChar: Char, vararg extraFileNames: String, tailText: String?) {
+        val testFile = Paths.get(testDataPath, "${getTestName(false)}-1.kt")
 
         IgnoreTests.runTestIfEnabledByFileDirective(testFile, IgnoreTests.DIRECTIVES.FIR_COMPARISON) {
-            super.doTest(fileName, completionChar, extraFileNames, tailText)
+            super.doTest(completionChar, *extraFileNames, tailText = tailText)
         }
     }
 
