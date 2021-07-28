8ac68ef76cf05aa9003649154f5b785e0b144e9c
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2021-07-28 20:49:25 +0300

    IDEA-265161 fixed sizes of RC command line completion dialog tables
    GitOrigin-RevId: 7aef752c2d46b913b22d2d9000f709e98a1a6749

--- platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/ui/command/line/CommandLineDialog.kt
+++ platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/ui/command/line/CommandLineDialog.kt
@@ -12,6 +12,7 @@ import com.intellij.ui.ColoredTableCellRenderer
 import com.intellij.ui.SimpleTextAttributes
 import com.intellij.ui.TableSpeedSearch
 import com.intellij.ui.layout.*
+import com.intellij.ui.scale.JBUIScale
 import com.intellij.ui.table.JBTable
 import com.intellij.util.ui.ColumnInfo
 import com.intellij.util.ui.ListTableModel
@@ -125,11 +126,16 @@ class CommandLineDialog(
     }
 
     init {
-      val search = TableSpeedSearch(this)
       val nameColumn = columnModel.getColumn(0)
-      nameColumn.cellRenderer = Renderer(search, tableInfo.dataColumnIcon)
       val descriptionColumn = columnModel.getColumn(1)
+
+      val search = TableSpeedSearch(this)
+      nameColumn.cellRenderer = Renderer(search, tableInfo.dataColumnIcon)
       descriptionColumn.cellRenderer = Renderer(search, tableInfo.descriptionColumnIcon)
+
+      visibleRowCount = 8
+      nameColumn.preferredWidth = JBUIScale.scale(250)
+      descriptionColumn.preferredWidth = JBUIScale.scale(500)
     }
   }
 
