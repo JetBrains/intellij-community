74728c07d4593eee87243965e47c668b472b2901
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2021-07-28 21:34:59 +0900

    [kotlin] Migrate kotlin.gradle.gradle-native to the new plugin model
    GitOrigin-RevId: f620ed0896fe78440dcdbeedb7a61ef302b45d42

--- plugins/kotlin/gradle/gradle-native/src/org/jetbrains/kotlin/idea/gradle/native/KotlinNativeGradleConfigurator.kt
+++ plugins/kotlin/gradle/gradle-native/src/org/jetbrains/kotlin/idea/gradle/native/KotlinNativeGradleConfigurator.kt
@@ -1,6 +1,6 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
-package org.jetbrains.kotlin.idea.gradle.konan
+package org.jetbrains.kotlin.idea.gradle.native
 
 import org.jetbrains.kotlin.ide.konan.hasKotlinNativeRuntimeInScope
 import org.jetbrains.kotlin.idea.configuration.ConfigureKotlinStatus
