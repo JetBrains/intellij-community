002621368bff85b7fcf5d078b048ea106dcc863d
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-07-28 12:46:54 +0200

    FIR IDE: fix package name for tests
    GitOrigin-RevId: 9a5aa9b8c0ecc94052e531046f0145d414df976a

--- plugins/kotlin/fir/test/org/jetbrains/kotlin/idea/fir/codeInsight/FirOverrideImplementTest.kt
+++ plugins/kotlin/fir/test/org/jetbrains/kotlin/idea/fir/codeInsight/FirOverrideImplementTest.kt
@@ -1,9 +1,9 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
-package org.jetbrains.kotlin.idea.fir.codeinsight
+package org.jetbrains.kotlin.idea.fir.codeInsight
 
 import com.intellij.util.ThrowableRunnable
-import org.jetbrains.kotlin.idea.codeInsight.Java8OverrideImplementTest
+import org.jetbrains.kotlin.idea.codeInsight.OverrideImplementTest
 import org.jetbrains.kotlin.idea.core.overrideImplement.KtClassMember
 import org.jetbrains.kotlin.idea.fir.invalidateCaches
 import org.jetbrains.kotlin.idea.test.runAll
@@ -12,7 +12,7 @@ import org.junit.internal.runners.JUnit38ClassRunner
 import org.junit.runner.RunWith
 
 @RunWith(JUnit38ClassRunner::class)
-internal class FirJava8OverrideImplementTest : Java8OverrideImplementTest<KtClassMember>(), FirOverrideImplementTestMixIn {
+internal class FirOverrideImplementTest : OverrideImplementTest<KtClassMember>(), FirOverrideImplementTestMixIn {
     override fun isFirPlugin(): Boolean = true
 
     override fun tearDown() {
