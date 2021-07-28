d25eb593afeb04bd3251921c340c741234296c74
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2021-07-28 22:36:32 +0900

    [kotlin] Migrate kotlin.project-wizard.maven to the new plugin model
    GitOrigin-RevId: 8bc07a81723de9cbc87b59abd0cc1489aa1900c1

--- plugins/kotlin/project-wizard/maven/src/org/jetbrains/kotlin/idea/projectWizard/maven/MavenKotlinBuildSystemType.kt
+++ plugins/kotlin/project-wizard/maven/src/org/jetbrains/kotlin/idea/projectWizard/maven/MavenKotlinBuildSystemType.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.kotlin.idea.maven.projectWizard
+package org.jetbrains.kotlin.idea.projectWizard.maven
 
 import com.intellij.ide.util.projectWizard.WizardContext
 import com.intellij.ide.wizard.NewProjectWizardStep
