4b1e8eec6e5ffa253d2f3c8679350daef6738d0b
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2021-07-28 13:02:19 +0200

    IDEA-274625 Debug UI scaling breaks the UI
    GitOrigin-RevId: 79711678815da6deb8c70d1c22b16357b4a7d082

--- platform/util/ui/src/com/intellij/ui/scale/JBUIScale.java
+++ platform/util/ui/src/com/intellij/ui/scale/JBUIScale.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ui.scale;
 
 import com.intellij.openapi.diagnostic.Logger;
@@ -181,7 +181,7 @@ public final class JBUIScale {
 
     Float debug = DEBUG_USER_SCALE_FACTOR.get();
     if (debug != null) {
-      return result;
+      return debug;
     }
 
     synchronized (lock) {
