c3a39c8524dc51fdb8b91033b3a253e2ed5bde18
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-07-29 13:15:46 +0200

    add `CoroutineScope.progress`
    GitOrigin-RevId: e968162571a97b49e76e7a8895ec28551d1dad35

--- platform/util-ex/src/com/intellij/openapi/progress/coroutines.kt
+++ platform/util-ex/src/com/intellij/openapi/progress/coroutines.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 @file:ApiStatus.Experimental
 
 package com.intellij.openapi.progress
@@ -124,3 +124,5 @@ internal fun <T> runUnderIndicator(job: Job, progressSink: ProgressSink?, action
     throw job.getCancellationException()
   }
 }
+
+fun CoroutineScope.progress(): Progress = JobProgress(coroutineContext.job)
