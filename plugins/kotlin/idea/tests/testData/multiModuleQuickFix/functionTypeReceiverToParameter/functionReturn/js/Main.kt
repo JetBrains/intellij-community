7a3a25f81aa5c1d4bc59947feb34635454379f63
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-07-30 17:34:55 +0700

    [kotlin] add quickfix for `ErrorsJs.EXTENSION_FUNCTION_IN_EXTERNAL_DECLARATION`
    ^KTIJ-1759 Fixed
    
    (cherry-picked from intellij)
    
    GitOrigin-RevId: 052ce0cd08e1809c59524db703b4cd11458a0052

---
+++ plugins/kotlin/idea/tests/testData/multiModuleQuickFix/functionTypeReceiverToParameter/functionReturn/js/Main.kt
@@ -0,0 +1,4 @@
+// "Convert 'String.() -> Unit' to '(String) -> Unit'" "true"
+external fun boo(p: String.() -> Unit): Str<caret>ing.() -> Unit
+
+// ERROR: Function types with receiver are prohibited in external declarations
