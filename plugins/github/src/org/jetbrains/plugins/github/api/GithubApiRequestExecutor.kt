2b879d458e792ca4b6cabfb4da12041ad2ff4216
Ivan Semenov <ivan.semenov@jetbrains.com>
2021-08-02 16:03:22 +0300

    [github] always use Bearer authorization type for API calls
    (cherry picked from commit 68104af1f2f0262726f08f34ff89a9fcc638d9b6)
    
    IJ-CR-12536
    
    GitOrigin-RevId: c2dc221bc228e25c24308c54863bbdb9c070b628

--- plugins/github/src/org/jetbrains/plugins/github/api/GithubApiRequestExecutor.kt
+++ plugins/github/src/org/jetbrains/plugins/github/api/GithubApiRequestExecutor.kt
@@ -71,7 +71,7 @@ sealed class GithubApiRequestExecutor {
       return createRequestBuilder(request)
         .tuner { connection ->
           request.additionalHeaders.forEach(connection::addRequestProperty)
-          connection.addRequestProperty(HttpSecurityUtil.AUTHORIZATION_HEADER_NAME, "${request.tokenHeaderType} $token")
+          connection.addRequestProperty(HttpSecurityUtil.AUTHORIZATION_HEADER_NAME, "Bearer $token")
         }
         .useProxy(useProxy)
         .execute(request, indicator)
@@ -273,9 +273,5 @@ sealed class GithubApiRequestExecutor {
 
   interface AuthDataChangeListener : EventListener {
     fun authDataChanged()
-  }
-
-  enum class TokenHeaderType {
-    TOKEN, BEARER
   }
 }
