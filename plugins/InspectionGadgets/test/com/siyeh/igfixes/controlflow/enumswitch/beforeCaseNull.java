eef3a894a5c46b26a15917bac9fc749031f498c5
Andrey.Cherkasov <andrey.cherkasov@jetbrains.com>
2021-08-02 04:29:01 +0300

    [java-inspections] EnumSwitchStatementWhichMissesCasesInspection: support PsiDefaultCaseLabelElement and matching null
    GitOrigin-RevId: 8798cc450a09008b64874cb16ed0f2b958c64554

---
+++ plugins/InspectionGadgets/test/com/siyeh/igfixes/controlflow/enumswitch/beforeCaseNull.java
@@ -0,0 +1,13 @@
+// "Create missing branches: 'B', and 'D'" "true"
+enum Foo { A, B, C, D, E }
+
+class Test {
+    void testI(Foo foo) {
+        switch (foo<caret>) {
+            case A -> System.out.println(1);
+            case C -> System.out.println(2);
+            case E -> System.out.println(3);
+            case null -> System.out.println(4);
+        }
+    }
+}
