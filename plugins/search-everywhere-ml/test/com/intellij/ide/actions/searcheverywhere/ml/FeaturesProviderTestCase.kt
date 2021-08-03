44a374b402cd1b0585415f8a76bcd75b69b1878b
adam.malek <adam.malek@jetbrains.com>
2021-08-03 10:07:46 +0100

    ML in SE: Create tests for file features provider (IDEA-274480)
    GitOrigin-RevId: 083e1b5685d092c9b0fb096a68afdedfee620cb0

---
+++ plugins/search-everywhere-ml/test/com/intellij/ide/actions/searcheverywhere/ml/FeaturesProviderTestCase.kt
@@ -0,0 +1,169 @@
+package com.intellij.ide.actions.searcheverywhere.ml
+
+import com.intellij.ide.actions.searcheverywhere.ml.FeaturesProviderTestCase.AssertionElementSelector.AssertionSpecifier
+import com.intellij.ide.actions.searcheverywhere.ml.features.SearchEverywhereElementFeaturesProvider
+import com.intellij.openapi.application.PluginPathManager
+import com.intellij.openapi.fileEditor.impl.EditorHistoryManager
+import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiFile
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
+
+internal abstract class FeaturesProviderTestCase<T : SearchEverywhereElementFeaturesProvider>(private val providerClass: Class<T>)
+  : LightJavaCodeInsightFixtureTestCase() {
+  protected val provider: SearchEverywhereElementFeaturesProvider by lazy {
+    SearchEverywhereElementFeaturesProvider.EP_NAME.findExtensionOrFail(providerClass)
+  }
+
+  override fun setUp() {
+    super.setUp()
+    provider.init(project)
+  }
+
+  override fun tearDown() {
+    super.tearDown()
+    EditorHistoryManager.getInstance(project).removeAllFiles()
+  }
+
+  override fun getTestDataPath(): String {
+    return PluginPathManager.getPluginHomePath("search-everywhere-ml").plus("/testData")
+  }
+
+  protected fun getFileFromTestData(name: String): PsiFile {
+    val vf = getFileFromPath(name)
+    return psiManager.findFile(vf)!!
+  }
+
+  protected fun getDirectoryFromTestData(name: String): PsiDirectory {
+    val vf = getFileFromPath(name)
+    return psiManager.findDirectory(vf)!!
+  }
+
+  private fun getFileFromPath(name: String) = LocalFileSystem.getInstance().findFileByPath("$testDataPath/$name")!!
+
+  fun checkThatFeature(feature: String) = AssertionElementSelector(feature)
+
+  fun checkThatFeatures() = AssertionElementSelector(null)
+
+  /**
+   * The [AssertionElementSelector] and its inner classes allow for writing concise
+   * tests with methods chaining, that takes care of checking a feature
+   *   - has a certain value (through [AssertionSpecifier.isEqualTo]), or
+   *   - its value changes (through [AssertionSpecifier.changes]) upon some operation
+   *
+   * To use it, use either [checkThatFeature] to perform assertion on a single feature,
+   * or [checkThatFeatures] to perform assertion on multiple features, passed as a
+   * Map<String, *> to either [AssertionSpecifier.isEqualTo] or [AssertionSpecifier.changes]
+   *
+   * By default, all other arguments passed to [SearchEverywhereElementFeaturesProvider.getElementFeatures]
+   * are equal to 0, apart from the element. To change that and pass a different value for testing, use:
+   *   - [AssertionSpecifier.withCurrentTime]
+   *   - [AssertionSpecifier.withQueryLength]
+   *   - [AssertionSpecifier.withPriority]
+   *
+   * Additionally, if the feature provider needs to be reinitialized, for instance, to obtain
+   * some copy of data as a cache, use [AssertionSpecifier.withProviderReloaded].
+   */
+  inner class AssertionElementSelector(private val feature: String?) {
+    fun <E : Any> ofElement(element: E) = AssertionSpecifier(element)
+
+    inner class AssertionSpecifier<E : Any>(private val element: E) {
+      private var features: Map<String, Any> = emptyMap()
+      private var currentTime = 0L
+      private var queryLength = 0
+      private var elementPriority = 0
+
+      /**
+       * Specifies the current time / session start time that will be passed when obtaining the features,
+       * see [SearchEverywhereElementFeaturesProvider.getElementFeatures]
+       */
+      fun withCurrentTime(time: Long): AssertionSpecifier<E> {
+        currentTime = time
+        return this
+      }
+
+      /**
+       * Specifies the query length start time that will be passed when obtaining the features,
+       * see [SearchEverywhereElementFeaturesProvider.getElementFeatures]
+       */
+      fun withQueryLength(length: Int): AssertionSpecifier<E> {
+        queryLength = length
+        return this
+      }
+
+      /**
+       * Specifies the priority of the element that will be passed when obtaining the features,
+       * see [SearchEverywhereElementFeaturesProvider.getElementFeatures]
+       */
+      fun withPriority(priority: Int): AssertionSpecifier<E> {
+        elementPriority = priority
+        return this
+      }
+
+      /**
+       * Reloads the feature provider
+       */
+      fun withProviderReloaded(): AssertionSpecifier<E> {
+        provider.init(project)
+        return this
+      }
+
+      /**
+       * Checks if the specified [feature] of the element has the [expectedValue]
+       */
+      fun isEqualTo(expectedValue: Any?) = assert(expectedValue)
+
+      /**
+       * Checks if the value of the [feature] will change from [from] to [to] after the specified operation is performed.
+       *
+       * @see ChangeOperation.after
+       */
+      fun changes(from: Any?, to: Any?) = ChangeOperation(from, to)
+
+      private fun assert(expectedValue: Any?) {
+        features = provider.getElementFeatures(element, currentTime, queryLength, elementPriority)
+
+        if (features.isEmpty()) {
+          fail("The provider ${providerClass.simpleName} has returned empty map of features. Maybe element type is incorrect?")
+          return
+        }
+
+        when (feature) {
+          null -> assertMultipleFeatures(expectedValue)
+          else -> assertSingleFeature(feature, expectedValue)
+        }
+      }
+
+      private fun assertSingleFeature(key: String, expectedValue: Any?) {
+        val actualValue = features[key]
+        assertEquals("Assertion of the feature $key has failed", expectedValue, actualValue)
+      }
+
+      private fun assertMultipleFeatures(expectedValue: Any?) {
+        if (expectedValue == null) {
+          throw IllegalArgumentException("The expected value for checkThatFeatures() must be of type Map<String, *>, not null")
+        }
+        else if (expectedValue !is Map<*, *>) {
+          throw IllegalArgumentException("The expected value for checkThatFeatures() must be of type Map<String, *>")
+        }
+
+        expectedValue.forEach { (key, value) ->
+          key as String
+          assertSingleFeature(key, value)
+        }
+      }
+
+      inner class ChangeOperation(private val oldValue: Any?, private val newValue: Any?) {
+        /**
+         * Checks if the value of the feature will change after [operation] is performed.
+         */
+        fun after(operation: (E) -> Unit) {
+          assert(oldValue)
+          operation(element)
+          assert(newValue)
+        }
+      }
+    }
+  }
+}
+
---
+++ plugins/search-everywhere-ml/test/com/intellij/ide/actions/searcheverywhere/ml/SearchEverywhereFileFeaturesProviderTest.kt
@@ -0,0 +1,383 @@
+package com.intellij.ide.actions.searcheverywhere.ml
+
+import com.intellij.ide.actions.searcheverywhere.ml.features.SearchEverywhereFileFeaturesProvider
+import com.intellij.ide.actions.searcheverywhere.ml.features.SearchEverywhereFileFeaturesProvider.Companion.FILETYPE_DATA_KEY
+import com.intellij.ide.actions.searcheverywhere.ml.features.SearchEverywhereFileFeaturesProvider.Companion.FILETYPE_USAGE_RATIO_DATA_KEY
+import com.intellij.ide.actions.searcheverywhere.ml.features.SearchEverywhereFileFeaturesProvider.Companion.FILETYPE_USED_IN_LAST_DAY_DATA_KEY
+import com.intellij.ide.actions.searcheverywhere.ml.features.SearchEverywhereFileFeaturesProvider.Companion.FILETYPE_USED_IN_LAST_HOUR_DATA_KEY
+import com.intellij.ide.actions.searcheverywhere.ml.features.SearchEverywhereFileFeaturesProvider.Companion.FILETYPE_USED_IN_LAST_MINUTE_DATA_KEY
+import com.intellij.ide.actions.searcheverywhere.ml.features.SearchEverywhereFileFeaturesProvider.Companion.FILETYPE_USED_IN_LAST_MONTH_DATA_KEY
+import com.intellij.ide.actions.searcheverywhere.ml.features.SearchEverywhereFileFeaturesProvider.Companion.IS_DIRECTORY_DATA_KEY
+import com.intellij.ide.actions.searcheverywhere.ml.features.SearchEverywhereFileFeaturesProvider.Companion.IS_FAVORITE_DATA_KEY
+import com.intellij.ide.actions.searcheverywhere.ml.features.SearchEverywhereFileFeaturesProvider.Companion.PRIORITY_DATA_KEY
+import com.intellij.ide.actions.searcheverywhere.ml.features.SearchEverywhereFileFeaturesProvider.Companion.RECENT_INDEX_DATA_KEY
+import com.intellij.ide.actions.searcheverywhere.ml.features.SearchEverywhereFileFeaturesProvider.Companion.TIME_SINCE_LAST_FILETYPE_USAGE
+import com.intellij.ide.actions.searcheverywhere.ml.features.SearchEverywhereFileFeaturesProvider.Companion.TIME_SINCE_LAST_MODIFICATION_DATA_KEY
+import com.intellij.ide.actions.searcheverywhere.ml.features.SearchEverywhereFileFeaturesProvider.Companion.WAS_MODIFIED_IN_LAST_DAY_DATA_KEY
+import com.intellij.ide.actions.searcheverywhere.ml.features.SearchEverywhereFileFeaturesProvider.Companion.WAS_MODIFIED_IN_LAST_HOUR_DATA_KEY
+import com.intellij.ide.actions.searcheverywhere.ml.features.SearchEverywhereFileFeaturesProvider.Companion.WAS_MODIFIED_IN_LAST_MINUTE_DATA_KEY
+import com.intellij.ide.actions.searcheverywhere.ml.features.SearchEverywhereFileFeaturesProvider.Companion.WAS_MODIFIED_IN_LAST_MONTH_DATA_KEY
+import com.intellij.ide.favoritesTreeView.FavoritesManager
+import com.intellij.internal.statistic.local.FileTypeUsageSummary
+import com.intellij.internal.statistic.local.FileTypeUsageSummaryProvider
+import com.intellij.internal.statistic.local.TestFileTypeUsageSummaryProvider
+import com.intellij.mock.MockPsiDirectory
+import com.intellij.mock.MockPsiFile
+import com.intellij.mock.MockVirtualFile
+import com.intellij.openapi.components.service
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.fileEditor.impl.EditorHistoryManager
+import com.intellij.psi.PsiFileSystemItem
+import com.intellij.util.Time
+import java.time.LocalDateTime
+import java.time.ZoneId
+
+
+internal class SearchEverywhereFileFeaturesProviderTest
+  : FeaturesProviderTestCase<SearchEverywhereFileFeaturesProvider>(SearchEverywhereFileFeaturesProvider::class.java) {
+
+  private val testFile: PsiFileSystemItem by lazy {
+    getFileFromTestData("file.java")
+  }
+
+  private val mockedFileStatsProvider
+    get() = project.service<FileTypeUsageSummaryProvider>() as TestFileTypeUsageSummaryProvider
+
+  private val currentTime: Long
+  private val lastMinute: Long
+  private val lastHour: Long
+  private val lastDay: Long
+  private val lastMonth: Long
+
+  init {
+    val localDt = LocalDateTime.of(2020, 12, 31, 23, 59)
+    localDt.atZone(ZoneId.systemDefault()).let {
+      currentTime = it.toInstant().toEpochMilli()
+      lastMinute = it.minusMinutes(1).toInstant().toEpochMilli()
+      lastHour = it.minusHours(1).toInstant().toEpochMilli()
+      lastDay = it.minusDays(1).toInstant().toEpochMilli()
+      lastMonth = it.minusWeeks(4).toInstant().toEpochMilli()
+    }
+  }
+
+  fun testIsDirectory() {
+    val directory = MockPsiDirectory(project, project)
+
+    checkThatFeature(IS_DIRECTORY_DATA_KEY)
+      .ofElement(directory)
+      .isEqualTo(true)
+  }
+
+  fun testFileIsNotDirectory() {
+    checkThatFeature(IS_DIRECTORY_DATA_KEY)
+      .ofElement(testFile)
+      .isEqualTo(false)
+  }
+
+  fun testFileType() {
+    checkThatFeature(FILETYPE_DATA_KEY)
+      .ofElement(testFile)
+      .isEqualTo(testFile.virtualFile.fileType.name)
+  }
+
+  fun testIsInFavorites() {
+    val addFileToFavorites = { file: PsiFileSystemItem ->
+      FavoritesManager.getInstance(project).also {
+        it.createNewList("xxx")
+        if (!it.addRoots("xxx", module, file.virtualFile)) {
+          fail("Failed to add the file to the favorites list")
+        }
+      }
+    }
+
+    checkThatFeature(IS_FAVORITE_DATA_KEY)
+      .ofElement(testFile)
+      .changes(false, true)
+      .after { addFileToFavorites(it) }
+  }
+
+  fun testIsOpened() {
+    checkThatFeature(SearchEverywhereFileFeaturesProvider.IS_OPENED_DATA_KEY)
+      .ofElement(testFile)
+      .changes(false, true)
+      .after { FileEditorManager.getInstance(project).openFile(it.virtualFile, false) }
+  }
+
+  fun testRecentIndexOfNeverOpenedFile() {
+    prepareForRecentIndexTest()
+
+    checkThatFeature(RECENT_INDEX_DATA_KEY)
+      .ofElement(testFile)
+      .isEqualTo(-1)
+  }
+
+  fun testMostRecentFileIndex() {
+    val openedFiles = prepareForRecentIndexTest()
+
+    checkThatFeature(RECENT_INDEX_DATA_KEY)
+      .ofElement(openedFiles.last()) // Last opened file (i.e. the most recent)
+      .isEqualTo(1)
+  }
+
+  fun testOldestFileIndex() {
+    val openedFiles = prepareForRecentIndexTest()
+    val expectedIndex = openedFiles.size
+
+    checkThatFeature(RECENT_INDEX_DATA_KEY)
+      .ofElement(openedFiles.first()) // First opened file (i.e. the oldest)
+      .isEqualTo(expectedIndex)
+  }
+
+  fun testPriority() {
+    val priority = 10101
+
+    checkThatFeature(PRIORITY_DATA_KEY)
+      .ofElement(testFile)
+      .withPriority(priority)
+      .isEqualTo(priority)
+  }
+
+  fun testFileTypeUsageRatio() {
+    mockedFileStatsProvider
+      .setStats(testFile.virtualFile.fileType.name, FileTypeUsageSummary(7, lastDay))
+      .setStats("Python", FileTypeUsageSummary(2, lastDay))
+      .setStats("XML", FileTypeUsageSummary(1, lastMinute))
+
+    checkThatFeature(FILETYPE_USAGE_RATIO_DATA_KEY)
+      .ofElement(testFile)
+      .withProviderReloaded()
+      .isEqualTo(0.7)
+  }
+
+  fun testFileTypeUsedInLastMinute() {
+    mockedFileStatsProvider.setStats(testFile.virtualFile.fileType.name, FileTypeUsageSummary(1, lastMinute))
+
+    val expectedValues = mapOf(
+      FILETYPE_USAGE_RATIO_DATA_KEY to 1.0,
+      TIME_SINCE_LAST_FILETYPE_USAGE to currentTime - lastMinute,
+
+      FILETYPE_USED_IN_LAST_MINUTE_DATA_KEY to true,
+      FILETYPE_USED_IN_LAST_HOUR_DATA_KEY to true,
+      FILETYPE_USED_IN_LAST_DAY_DATA_KEY to true,
+      FILETYPE_USED_IN_LAST_MONTH_DATA_KEY to true,
+    )
+
+    checkThatFeatures()
+      .ofElement(testFile)
+      .withCurrentTime(currentTime)
+      .withProviderReloaded()
+      .isEqualTo(expectedValues)
+  }
+
+  fun testFileTypeUsedInLastHour() {
+    mockedFileStatsProvider.setStats(testFile.virtualFile.fileType.name, FileTypeUsageSummary(1, lastHour))
+
+    val expectedValues = mapOf(
+      FILETYPE_USAGE_RATIO_DATA_KEY to 1.0,
+      TIME_SINCE_LAST_FILETYPE_USAGE to currentTime - lastHour,
+
+      FILETYPE_USED_IN_LAST_MINUTE_DATA_KEY to false,
+      FILETYPE_USED_IN_LAST_HOUR_DATA_KEY to true,
+      FILETYPE_USED_IN_LAST_DAY_DATA_KEY to true,
+      FILETYPE_USED_IN_LAST_MONTH_DATA_KEY to true,
+    )
+
+    checkThatFeatures()
+      .ofElement(testFile)
+      .withCurrentTime(currentTime)
+      .withProviderReloaded()
+      .isEqualTo(expectedValues)
+  }
+
+  fun testFileTypeUsedInLastDay() {
+    mockedFileStatsProvider.setStats(testFile.virtualFile.fileType.name, FileTypeUsageSummary(1, lastDay))
+
+    val expectedValues = mapOf(
+      FILETYPE_USAGE_RATIO_DATA_KEY to 1.0,
+      TIME_SINCE_LAST_FILETYPE_USAGE to currentTime - lastDay,
+
+      FILETYPE_USED_IN_LAST_MINUTE_DATA_KEY to false,
+      FILETYPE_USED_IN_LAST_HOUR_DATA_KEY to false,
+      FILETYPE_USED_IN_LAST_DAY_DATA_KEY to true,
+      FILETYPE_USED_IN_LAST_MONTH_DATA_KEY to true,
+    )
+
+    checkThatFeatures()
+      .ofElement(testFile)
+      .withCurrentTime(currentTime)
+      .withProviderReloaded()
+      .isEqualTo(expectedValues)
+  }
+
+  fun testFileTypeUsedInLastMonth() {
+    mockedFileStatsProvider.setStats(testFile.virtualFile.fileType.name, FileTypeUsageSummary(1, lastMonth))
+
+    val expectedValues = mapOf(
+      FILETYPE_USAGE_RATIO_DATA_KEY to 1.0,
+      TIME_SINCE_LAST_FILETYPE_USAGE to currentTime - lastMonth,
+
+      FILETYPE_USED_IN_LAST_MINUTE_DATA_KEY to false,
+      FILETYPE_USED_IN_LAST_HOUR_DATA_KEY to false,
+      FILETYPE_USED_IN_LAST_DAY_DATA_KEY to false,
+      FILETYPE_USED_IN_LAST_MONTH_DATA_KEY to true,
+    )
+
+    checkThatFeatures()
+      .ofElement(testFile)
+      .withCurrentTime(currentTime)
+      .withProviderReloaded()
+      .isEqualTo(expectedValues)
+  }
+
+  fun testFileTypeNeverUsed() {
+    mockedFileStatsProvider.clearStats()
+
+    val expectedValues = mapOf(
+      FILETYPE_USAGE_RATIO_DATA_KEY to 0.0,
+      TIME_SINCE_LAST_FILETYPE_USAGE to Long.MAX_VALUE,
+
+      FILETYPE_USED_IN_LAST_MINUTE_DATA_KEY to false,
+      FILETYPE_USED_IN_LAST_HOUR_DATA_KEY to false,
+      FILETYPE_USED_IN_LAST_DAY_DATA_KEY to false,
+      FILETYPE_USED_IN_LAST_MONTH_DATA_KEY to false,
+    )
+
+    checkThatFeatures()
+      .ofElement(testFile)
+      .withCurrentTime(currentTime)
+      .withProviderReloaded()
+      .isEqualTo(expectedValues)
+  }
+
+  fun testModifiedInLastMinute() {
+    val file = createFileWithModTimestamp(lastMinute)
+
+    val expectedValues = mapOf(
+      TIME_SINCE_LAST_MODIFICATION_DATA_KEY to currentTime - lastMinute,
+
+      WAS_MODIFIED_IN_LAST_MINUTE_DATA_KEY to true,
+      WAS_MODIFIED_IN_LAST_HOUR_DATA_KEY to true,
+      WAS_MODIFIED_IN_LAST_DAY_DATA_KEY to true,
+      WAS_MODIFIED_IN_LAST_MONTH_DATA_KEY to true,
+    )
+
+    checkThatFeatures()
+      .ofElement(file)
+      .withCurrentTime(currentTime)
+      .isEqualTo(expectedValues)
+  }
+
+  fun testModifiedInLastHour() {
+    val file = createFileWithModTimestamp(lastHour)
+
+    val expectedValues = mapOf(
+      TIME_SINCE_LAST_MODIFICATION_DATA_KEY to currentTime - lastHour,
+
+      WAS_MODIFIED_IN_LAST_MINUTE_DATA_KEY to false,
+      WAS_MODIFIED_IN_LAST_HOUR_DATA_KEY to true,
+      WAS_MODIFIED_IN_LAST_DAY_DATA_KEY to true,
+      WAS_MODIFIED_IN_LAST_MONTH_DATA_KEY to true,
+    )
+
+    checkThatFeatures()
+      .ofElement(file)
+      .withCurrentTime(currentTime)
+      .isEqualTo(expectedValues)
+  }
+
+  fun testModifiedInLastDay() {
+    val file = createFileWithModTimestamp(lastDay)
+
+    val expectedValues = mapOf(
+      TIME_SINCE_LAST_MODIFICATION_DATA_KEY to currentTime - lastDay,
+
+      WAS_MODIFIED_IN_LAST_MINUTE_DATA_KEY to false,
+      WAS_MODIFIED_IN_LAST_HOUR_DATA_KEY to false,
+      WAS_MODIFIED_IN_LAST_DAY_DATA_KEY to true,
+      WAS_MODIFIED_IN_LAST_MONTH_DATA_KEY to true,
+    )
+
+    checkThatFeatures()
+      .ofElement(file)
+      .withCurrentTime(currentTime)
+      .isEqualTo(expectedValues)
+  }
+
+  fun testModifiedInLastMonth() {
+    val file = createFileWithModTimestamp(lastMonth)
+
+    val expectedValues = mapOf(
+      TIME_SINCE_LAST_MODIFICATION_DATA_KEY to currentTime - lastMonth,
+
+      WAS_MODIFIED_IN_LAST_MINUTE_DATA_KEY to false,
+      WAS_MODIFIED_IN_LAST_HOUR_DATA_KEY to false,
+      WAS_MODIFIED_IN_LAST_DAY_DATA_KEY to false,
+      WAS_MODIFIED_IN_LAST_MONTH_DATA_KEY to true,
+    )
+
+    checkThatFeatures()
+      .ofElement(file)
+      .withCurrentTime(currentTime)
+      .isEqualTo(expectedValues)
+  }
+
+  fun testModifiedLaterThanMonth() {
+    val modTime = lastMonth - Time.DAY
+    val file = createFileWithModTimestamp(modTime)
+
+    val expectedValues = mapOf(
+      TIME_SINCE_LAST_MODIFICATION_DATA_KEY to currentTime - modTime,
+
+      WAS_MODIFIED_IN_LAST_MINUTE_DATA_KEY to false,
+      WAS_MODIFIED_IN_LAST_HOUR_DATA_KEY to false,
+      WAS_MODIFIED_IN_LAST_DAY_DATA_KEY to false,
+      WAS_MODIFIED_IN_LAST_MONTH_DATA_KEY to false,
+    )
+
+    checkThatFeatures()
+      .ofElement(file)
+      .withCurrentTime(currentTime)
+      .isEqualTo(expectedValues)
+  }
+
+  private fun createFileWithModTimestamp(modificationTimestamp: Long): PsiFileSystemItem {
+    val mockVirtualFile = object : MockVirtualFile("file.java") {
+      override fun getTimeStamp(): Long {
+        return modificationTimestamp
+      }
+    }
+
+    return MockPsiFile(mockVirtualFile, psiManager)
+  }
+
+  private fun prepareForRecentIndexTest(): List<PsiFileSystemItem> {
+    closeAllOpenedFiles()
+    EditorHistoryManager.getInstance(project).removeAllFiles()
+    return openAllFilesFromRecentFilesTestDir()
+  }
+
+  private fun closeAllOpenedFiles() {
+    val editor = FileEditorManager.getInstance(project)
+    editor.openFiles.forEach { editor.closeFile(it) }
+  }
+
+  /**
+   * Opens all files in testData/recentFilesTest
+   * @return List of the opened files in the order they were opened
+   */
+  private fun openAllFilesFromRecentFilesTestDir(): List<PsiFileSystemItem> {
+    val files = getDirectoryFromTestData("recentFilesTest").files.toList()
+
+    files.forEach {
+      FileEditorManager.getInstance(project).openFile(it.virtualFile, true)
+    }
+
+    return files
+  }
+
+
+  override fun tearDown() {
+    super.tearDown()
+    mockedFileStatsProvider.clearStats()
+  }
+}
