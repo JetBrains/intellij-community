f065a46a8bf71c03458b11212fbd7e4b21fc4853
Dmitry.Krasilschikov <kradima@jetbrains.com>
2021-08-03 15:50:36 +0300

    Add search to project type list
    ListWithFilter : revert the latest changes
    
    GitOrigin-RevId: bee508d113bbcaf1b3ca60f6a6ece06bd23c33df

--- java/idea-ui/src/com/intellij/ide/projectWizard/ProjectTypeStep.java
+++ java/idea-ui/src/com/intellij/ide/projectWizard/ProjectTypeStep.java
@@ -46,7 +46,6 @@ import com.intellij.ui.components.JBLabel;
 import com.intellij.ui.components.JBList;
 import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.popup.list.GroupedItemsListRenderer;
-import com.intellij.ui.speedSearch.ListWithFilter;
 import com.intellij.uiDesigner.core.GridLayoutManager;
 import com.intellij.util.Function;
 import com.intellij.util.PlatformUtils;
@@ -118,85 +117,22 @@ public final class ProjectTypeStep extends ModuleWizardStep implements SettingsS
     myProjectTypeList.setModel(new CollectionListModel<>(groups));
     myProjectTypeList.setSelectionModel(new SingleSelectionModel());
     myProjectTypeList.addListSelectionListener(__ -> updateSelection());
+    myProjectTypeList.setCellRenderer(new ProjectTypeListRenderer(groups));
+
     if (isNewWizard()) {
       GridLayoutManager layout = (GridLayoutManager)myPanel.getLayout();
       layout.setHGap(0);
       myPanel.setLayout(layout);
 
-      ListWithFilter<TemplatesGroup> listWithFilter = (ListWithFilter<TemplatesGroup>)ListWithFilter.wrap(
-        myProjectTypeList, new JBScrollPane(myProjectTypeList), group -> group.getName());
-      listWithFilter.setSearchAlwaysVisible(true);
+      ProjectTypeListWithSearch<TemplatesGroup>
+        listWithFilter  = new ProjectTypeListWithSearch<>(myProjectTypeList, new JBScrollPane(myProjectTypeList), group -> group.getName());
       listWithFilter.setMinimumSize(JBUI.size(40, -1));
       myProjectTypePanel.add(listWithFilter);
 
       mySettingsPanel.setBorder(JBUI.Borders.customLine(JBColor.border(), 0, 0, 1, 0));
+    } else {
+      myProjectTypePanel.add(BorderLayout.CENTER, new JBScrollPane(myProjectTypeList));
     }
-    myProjectTypeList.setCellRenderer(new GroupedItemsListRenderer<>(new ListItemDescriptorAdapter<TemplatesGroup>() {
-      @Nullable
-      @Override
-      public String getTextFor(TemplatesGroup value) {
-        return value.getName();
-      }
-
-      @Nullable
-      @Override
-      public String getTooltipFor(TemplatesGroup value) {
-        return value.getDescription();
-      }
-
-      @Nullable
-      @Override
-      public Icon getIconFor(TemplatesGroup value) {
-        return value.getIcon();
-      }
-
-      @Override
-      public String getCaptionAboveOf(TemplatesGroup value) {
-        return isNewWizard() ? UIBundle.message("list.caption.group.generators") : super.getCaptionAboveOf(value);
-      }
-
-      @Override
-      public boolean hasSeparatorAboveOf(TemplatesGroup value) {
-        int index = groups.indexOf(value);
-        if (index < 1) return false;
-        TemplatesGroup upper = groups.get(index - 1);
-        if (isNewWizard()) {
-          return upper.getModuleBuilder() instanceof NewProjectModuleBuilder;
-        }
-
-        if (upper.getParentGroup() == null && value.getParentGroup() == null) return true;
-        return !Objects.equals(upper.getParentGroup(), value.getParentGroup()) &&
-               !Objects.equals(upper.getName(), value.getParentGroup());
-      }
-    }) {
-      @Override
-      protected JComponent createItemComponent() {
-        JComponent component = super.createItemComponent();
-        myTextLabel.setBorder(!isNewWizard() ? JBUI.Borders.empty(3) : JBUI.Borders.empty(5, 0));
-        return component;
-      }
-
-      @Override
-      protected SeparatorWithText createSeparator() {
-        if (!isNewWizard()) {
-          return super.createSeparator();
-        }
-        SeparatorWithText separator = createSeparatorComponent();
-        separator.setBorder(JBUI.Borders.empty(20, 8, 5, 0));
-        separator.setCaptionCentered(false);
-        separator.setFont(JBUI.Fonts.smallFont());
-        return separator;
-      }
-
-      @NotNull
-      private SeparatorWithText createSeparatorComponent() {
-        return new SeparatorWithText() {
-          @Override
-          protected void paintLinePart(Graphics g, int xMin, int xMax, int hGap, int y) { }
-        };
-      }
-    });
-    myProjectTypePanel.add(BorderLayout.CENTER, new JBScrollPane(myProjectTypeList));
 
     myModulesProvider = modulesProvider;
     Project project = context.getProject();
@@ -833,5 +769,75 @@ public final class ProjectTypeStep extends ModuleWizardStep implements SettingsS
     }
 
     FUCounterUsageLogger.getInstance().logEvent("new.project.wizard", eventId, data);
+  }
+
+  private static class ProjectTypeListRenderer extends GroupedItemsListRenderer<TemplatesGroup> {
+    ProjectTypeListRenderer(java.util.List<TemplatesGroup> groups) {
+      super(new ListItemDescriptorAdapter<>() {
+        @Nullable
+        @Override
+        public String getTextFor(TemplatesGroup value) {
+          return value.getName();
+        }
+
+        @Nullable
+        @Override
+        public String getTooltipFor(TemplatesGroup value) {
+          return value.getDescription();
+        }
+
+        @Nullable
+        @Override
+        public Icon getIconFor(TemplatesGroup value) {
+          return value.getIcon();
+        }
+
+        @Override
+        public String getCaptionAboveOf(TemplatesGroup value) {
+          return isNewWizard() ? UIBundle.message("list.caption.group.generators") : super.getCaptionAboveOf(value);
+        }
+
+        @Override
+        public boolean hasSeparatorAboveOf(TemplatesGroup value) {
+          int index = groups.indexOf(value);
+          if (index < 1) return false;
+          TemplatesGroup upper = groups.get(index - 1);
+          if (isNewWizard()) {
+            return upper.getModuleBuilder() instanceof NewProjectModuleBuilder;
+          }
+
+          if (upper.getParentGroup() == null && value.getParentGroup() == null) return true;
+          return !Objects.equals(upper.getParentGroup(), value.getParentGroup()) &&
+                 !Objects.equals(upper.getName(), value.getParentGroup());
+        }
+      });
+    }
+
+    @Override
+    protected JComponent createItemComponent() {
+      JComponent component = super.createItemComponent();
+      myTextLabel.setBorder(!isNewWizard() ? JBUI.Borders.empty(3) : JBUI.Borders.empty(5, 0));
+      return component;
+    }
+
+    @Override
+    protected SeparatorWithText createSeparator() {
+      if (!isNewWizard()) {
+        return super.createSeparator();
+      }
+      SeparatorWithText separator = createSeparatorComponent();
+      separator.setBorder(JBUI.Borders.empty(20, 8, 5, 0));
+      separator.setCaptionCentered(false);
+      separator.setFont(JBUI.Fonts.smallFont());
+      return separator;
+    }
+
+    @NotNull
+    private static SeparatorWithText createSeparatorComponent() {
+      return new SeparatorWithText() {
+        @Override
+        protected void paintLinePart(Graphics g, int xMin, int xMax, int hGap, int y) { }
+      };
+    }
   }
 }
