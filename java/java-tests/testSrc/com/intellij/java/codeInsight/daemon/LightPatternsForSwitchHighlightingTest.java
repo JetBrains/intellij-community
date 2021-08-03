53f0b04f589d92320cf09694de079fbb49342ac5
Ilyas Selimov <ilyas.selimov@jetbrains.com>
2021-08-03 11:40:33 +0700

    IDEA-274612 - ignore highlighting pattern variable as unused inside switch blocks
    (cherry picked from commit ac2c1b1588830df61be6592373b1663ee6bd0fea)
    
    IJ-CR-12555
    
    GitOrigin-RevId: cb8b3c761231bb8adcec8c2c00608dff1303af9a

--- java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/LightPatternsForSwitchHighlightingTest.java
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/LightPatternsForSwitchHighlightingTest.java
@@ -4,6 +4,7 @@ package com.intellij.java.codeInsight.daemon;
 import com.intellij.JavaTestUtil;
 import com.intellij.codeInsight.daemon.impl.IdentifierHighlighterPass;
 import com.intellij.codeInsight.intention.IntentionAction;
+import com.intellij.codeInspection.deadCode.UnusedDeclarationInspection;
 import com.intellij.pom.java.LanguageLevel;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
@@ -89,6 +90,11 @@ public class LightPatternsForSwitchHighlightingTest extends LightJavaCodeInsight
   }
 
   public void testBreakAndOtherStopWords() {
+    doTest();
+  }
+
+  public void testUnusedPatternVariable() {
+    myFixture.enableInspections(new UnusedDeclarationInspection());
     doTest();
   }
 
