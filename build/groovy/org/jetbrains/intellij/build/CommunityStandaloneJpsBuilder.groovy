436b79d6770e2485f4fe45c261ed8b72a1d8a173
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2021-08-03 14:22:27 +0300

    [space] IDEA-264046: Allow building intellij with private Space packages
    GitOrigin-RevId: fe8a758c1bbd2a6fb4d4e75f8821f5dfa99e6475

--- build/groovy/org/jetbrains/intellij/build/CommunityStandaloneJpsBuilder.groovy
+++ build/groovy/org/jetbrains/intellij/build/CommunityStandaloneJpsBuilder.groovy
@@ -80,6 +80,8 @@ final class CommunityStandaloneJpsBuilder {
         jar("devkit-jps.jar") { module("intellij.devkit.jps") }
         jar("java-langInjection-jps.jar") { module("intellij.java.langInjection.jps") }
 
+        jar("space-java-jps.jar") { module("intellij.space.java.jps") }
+
         for (String name in List.of(
           "JDOM", "jna", "OroMatcher", "Trove4j", "ASM", "NanoXML", "protobuf", "cli-parser", "Log4J", "jgoodies-forms", "Eclipse",
           "netty-codec-http", "lz4-java", "commons-codec", "commons-logging", "http-client", "Slf4j", "Guava", "plexus-utils",
