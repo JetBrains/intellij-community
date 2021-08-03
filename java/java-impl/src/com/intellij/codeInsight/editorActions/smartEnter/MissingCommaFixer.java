ec2238e7c8be657f878d353e786093594ad4b098
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2021-08-03 16:29:11 +0700

    [java-completion] Avoid fixing missing comma for methods without parameters
    Fixes IDEA-274311 Applying the completion adds a redundant comma on the next line
    
    GitOrigin-RevId: 3f4a55aff4a640e9e40e7a9c242611f0bb95475d

--- java/java-impl/src/com/intellij/codeInsight/editorActions/smartEnter/MissingCommaFixer.java
+++ java/java-impl/src/com/intellij/codeInsight/editorActions/smartEnter/MissingCommaFixer.java
@@ -3,26 +3,30 @@ package com.intellij.codeInsight.editorActions.smartEnter;
 
 import com.intellij.core.JavaPsiBundle;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiErrorElement;
-import com.intellij.psi.PsiExpression;
-import com.intellij.psi.PsiExpressionList;
+import com.intellij.psi.*;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.IncorrectOperationException;
 
 public class MissingCommaFixer implements Fixer {
   @Override
   public void apply(Editor editor, JavaSmartEnterProcessor processor, PsiElement psiElement) throws IncorrectOperationException {
-    if (psiElement instanceof PsiErrorElement) {
-      if (((PsiErrorElement)psiElement).getErrorDescription().equals(JavaPsiBundle.message("expected.comma.or.rparen"))) {
-        PsiElement parent = psiElement.getParent();
-        if (parent instanceof PsiExpressionList) {
-          PsiElement next = PsiTreeUtil.skipWhitespacesAndCommentsForward(psiElement);
-          if (next instanceof PsiExpression) {
-            editor.getDocument().insertString(psiElement.getTextOffset(), ",");
-          }
-        }
+    if (!(psiElement instanceof PsiErrorElement) ||
+        !((PsiErrorElement)psiElement).getErrorDescription().equals(JavaPsiBundle.message("expected.comma.or.rparen"))) {
+      return;
+    }
+    PsiElement parent = psiElement.getParent();
+    if (!(parent instanceof PsiExpressionList)) return;
+    PsiElement next = PsiTreeUtil.skipWhitespacesAndCommentsForward(psiElement);
+    if (!(next instanceof PsiExpression)) return;
+    PsiElement call = parent.getParent();
+    if (call instanceof PsiCall) {
+      PsiMethod method = ((PsiCall)call).resolveMethod();
+      if (method != null) {
+        PsiParameterList list = method.getParameterList();
+        int count = list.getParametersCount();
+        if (count == 0 || count == 1 && !method.isVarArgs()) return;
       }
     }
+    editor.getDocument().insertString(psiElement.getTextOffset(), ",");
   }
 }
