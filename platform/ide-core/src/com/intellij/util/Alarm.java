129ec249589a746c81ace17a62be0f87bfd811b7
Alexey Kudravtsev <cdr@intellij.com>
2021-08-04 13:30:38 +0200

    tighten access which was made public incorrectly
    GitOrigin-RevId: f966878d359b9c6abaed575727c61193db1c8482

--- platform/ide-core/src/com/intellij/util/Alarm.java
+++ platform/ide-core/src/com/intellij/util/Alarm.java
@@ -207,7 +207,7 @@ public class Alarm implements Disposable {
     }
   }
 
-  public void _addRequest(@NotNull Runnable request, long delayMillis, @Nullable ModalityState modalityState) {
+  void _addRequest(@NotNull Runnable request, long delayMillis, @Nullable ModalityState modalityState) {
     Request requestToSchedule = new Request(request, modalityState, delayMillis);
     synchronized (LOCK) {
       checkDisposed();
