915e454074a57e0d761779872f3332167268e001
Roman Golyshev <roman.golyshev@jetbrains.com>
2021-08-04 18:32:13 +0300

    Add KotlinFirMainRunLineMarkerContributor implementation to FIR plugin
    Now in FIR plugin we would have Run gutters on the main methods
    
    It was hard to make platform checks in the
    `KotlinRunLineMarkerContributor` common to both plugins,
    so I've decided to extract a base class with an abstract
    filtering method
    
    GitOrigin-RevId: e6a217f5c49b6e124b2e0c9317aeee9010b6c6d8

--- plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/highlighter/AbstractKotlinMainRunLineMarkerContributor.kt
+++ plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/highlighter/AbstractKotlinMainRunLineMarkerContributor.kt
@@ -6,15 +6,17 @@ import com.intellij.execution.lineMarker.ExecutorAction
 import com.intellij.execution.lineMarker.RunLineMarkerContributor
 import com.intellij.icons.AllIcons
 import com.intellij.psi.PsiElement
-import org.jetbrains.kotlin.idea.platform.tooling
-import org.jetbrains.kotlin.idea.project.platform
 import org.jetbrains.kotlin.idea.run.KotlinMainFunctionLocatingService
-import org.jetbrains.kotlin.idea.util.module
-import org.jetbrains.kotlin.platform.idePlatformKind
-import org.jetbrains.kotlin.platform.isCommon
 import org.jetbrains.kotlin.psi.KtNamedFunction
 
-class KotlinRunLineMarkerContributor : RunLineMarkerContributor() {
+abstract class AbstractKotlinMainRunLineMarkerContributor : RunLineMarkerContributor() {
+    /**
+     * Additional condition on main function in case that it should not be possible to run.
+     *
+     * Note that [function] is already checked to have correct signature and name.
+     */
+    protected abstract fun acceptEntryPoint(function: KtNamedFunction): Boolean
+
     override fun getInfo(element: PsiElement): Info? {
         val function = element.parent as? KtNamedFunction ?: return null
 
@@ -23,8 +25,7 @@ class KotlinRunLineMarkerContributor : RunLineMarkerContributor() {
         val mainLocatingService = KotlinMainFunctionLocatingService.getInstance()
         if (!mainLocatingService.isMain(function)) return null
 
-        val platform = function.containingKtFile.module?.platform ?: return null
-        if (platform.isCommon() || !platform.idePlatformKind.tooling.acceptsAsEntryPoint(function)) return null
+        if (!acceptEntryPoint(function)) return null
 
         return Info(AllIcons.RunConfigurations.TestState.Run, null, *ExecutorAction.getActions(Int.MAX_VALUE))
     }
