f8eaf26eb849eafdccdd8bc680952cc9a75de271
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2021-08-04 14:46:16 +0300

    [groovy] IDEA-275032: Properly mark constructor calls as @Deprecated
    Underlying CodeReferenceElement resolves to class and not to constructor, so it spoiled behavior of our highlighter
    
    GitOrigin-RevId: a9bcbc1dcddf8adea0fec6a08f66bf49a75dfccf

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/codeInspection/confusing/GrDeprecatedAPIUsageInspection.java
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/codeInspection/confusing/GrDeprecatedAPIUsageInspection.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.codeInspection.confusing;
 
 import com.intellij.codeInspection.LocalQuickFix;
@@ -11,7 +11,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.groovy.GroovyBundle;
 import org.jetbrains.plugins.groovy.codeInspection.BaseInspection;
 import org.jetbrains.plugins.groovy.codeInspection.BaseInspectionVisitor;
-import org.jetbrains.plugins.groovy.lang.psi.GrReferenceElement;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrNewExpression;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrReferenceExpression;
 import org.jetbrains.plugins.groovy.lang.psi.api.types.GrCodeReferenceElement;
 
@@ -27,30 +27,37 @@ public class GrDeprecatedAPIUsageInspection extends BaseInspection {
       @Override
       public void visitReferenceExpression(@NotNull GrReferenceExpression ref) {
         super.visitReferenceExpression(ref);
-        checkRef(ref);
+        checkRef(ref.resolve(), ref.getReferenceNameElement(), ref.getReferenceName());
       }
 
       @Override
       public void visitCodeReferenceElement(@NotNull GrCodeReferenceElement ref) {
         super.visitCodeReferenceElement(ref);
-        checkRef(ref);
+        checkRef(ref.resolve(), ref.getReferenceNameElement(), ref.getReferenceName());
       }
 
-      private void checkRef(GrReferenceElement ref) {
-        PsiElement resolved = ref.resolve();
-        if (isDeprecated(resolved)) {
-          PsiElement toHighlight = getElementToHighlight(ref);
-          registerError(toHighlight, GroovyBundle.message("0.is.deprecated", ref.getReferenceName()), LocalQuickFix.EMPTY_ARRAY,
-                        ProblemHighlightType.LIKE_DEPRECATED);
+      @Override
+      public void visitNewExpression(@NotNull GrNewExpression ref) {
+        super.visitNewExpression(ref);
+        var resolvedCall = ref.resolveMethod();
+        if (resolvedCall == null || isDeprecated(resolvedCall.getContainingClass())) {
+          return;
+        }
+        var referenceElement = ref.getReferenceElement();
+        if (referenceElement != null) {
+          checkRef(resolvedCall, ref.getReferenceElement(), referenceElement.getReferenceName());
+        }
+        else {
+          checkRef(resolvedCall, ref, resolvedCall.getName());
         }
       }
 
-      @NotNull
-      public PsiElement getElementToHighlight(@NotNull GrReferenceElement refElement) {
-        final PsiElement refNameElement = refElement.getReferenceNameElement();
-        return refNameElement != null ? refNameElement : refElement;
+      private void checkRef(PsiElement resolved, PsiElement elementToHighlight, String elementName) {
+        if (isDeprecated(resolved)) {
+          registerError(elementToHighlight, GroovyBundle.message("0.is.deprecated", elementName), LocalQuickFix.EMPTY_ARRAY,
+                        ProblemHighlightType.LIKE_DEPRECATED);
+        }
       }
-
 
       private boolean isDeprecated(PsiElement resolved) {
         if (resolved instanceof PsiDocCommentOwner) {
