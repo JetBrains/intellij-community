c00ebd4c1e595b306b86bfed8082157ecc8dd7c7
Bas Leijdekkers <basleijdekkers@gmail.com>
2021-08-05 12:53:38 +0200

    Change condition to use more common construct (taken from LossyEncodingInspection)
    GitOrigin-RevId: fd060721e536f9d2f7bb4fcc809f69205a1fe0b7

--- platform/lang-impl/src/com/intellij/codeInspection/ProblematicWhitespaceInspection.java
+++ platform/lang-impl/src/com/intellij/codeInspection/ProblematicWhitespaceInspection.java
@@ -93,7 +93,8 @@ public class ProblematicWhitespaceInspection extends LocalInspectionTool {
       if (!(fileType instanceof LanguageFileType)) {
         return;
       }
-      if (file != file.getViewProvider().getAllFiles().get(0)) {
+      if (file.getViewProvider().getBaseLanguage() != file.getLanguage()) {
+        // don't warn multiple times on files which have multiple views like PHP and JSP
         return;
       }
       final InjectedLanguageManager injectedLanguageManager = InjectedLanguageManager.getInstance(file.getProject());
