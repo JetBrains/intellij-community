d972fe9d4b9b38fea1bd6167eda0ab8fa0d7d71e
Bas Leijdekkers <basleijdekkers@gmail.com>
2021-08-05 12:55:05 +0200

    Don't warn multiple times on files with multiple view
    for "Non-ASCII characters" inspection
    
    GitOrigin-RevId: d913408a5acefc15fd1ba597ba5d71e4267f6f80

--- platform/lang-impl/src/com/intellij/codeInspection/NonAsciiCharactersInspection.java
+++ platform/lang-impl/src/com/intellij/codeInspection/NonAsciiCharactersInspection.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.codeInspection;
 
@@ -109,6 +95,10 @@ public class NonAsciiCharactersInspection extends LocalInspectionTool {
       public void visitFile(@NotNull PsiFile file) {
         super.visitFile(file);
         if (CHECK_FOR_FILES_CONTAINING_BOM) {
+          if (file.getViewProvider().getBaseLanguage() != file.getLanguage()) {
+            // don't warn multiple times on files which have multiple views like PHP and JSP
+            return;
+          }
           VirtualFile virtualFile = file.getVirtualFile();
           byte[] bom = virtualFile == null ? null : virtualFile.getBOM();
           if (bom != null) {
