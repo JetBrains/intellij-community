9a9f03ace3d856b07c1fc6a67a4cb85f1590cd26
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2021-08-09 21:46:35 +0300

    Return PLUGINS category for plugin component
    GitOrigin-RevId: a77541bcae6344b45d8ada40edb01e473e55e0bf

--- platform/projectModel-impl/src/com/intellij/openapi/components/ComponentCategorizer.java
+++ platform/projectModel-impl/src/com/intellij/openapi/components/ComponentCategorizer.java
@@ -1,15 +1,25 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.components;
 
+import com.intellij.ide.plugins.PluginManager;
+import com.intellij.openapi.extensions.PluginId;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class ComponentCategorizer {
   private ComponentCategorizer() {
   }
 
-  @Nullable
-  public static ComponentCategory getCategory(PersistentStateComponent<?> component) {
+  public static @Nullable ComponentCategory getCategory(@NotNull PersistentStateComponent<?> component) {
+    PluginId pluginId = getPluginId(component);
+    if (pluginId != null) {
+      return ComponentCategory.PLUGINS;
+    }
     State state = component.getClass().getAnnotation(State.class);
     return state != null ? state.category() : ComponentCategory.OTHER;
+  }
+
+  public static @Nullable PluginId getPluginId(@NotNull PersistentStateComponent<?> component) {
+    return PluginManager.getPluginByClassName(component.getClass().getName());
   }
 }
