5392a7de952235c922802afccd9078fb0c93b67b
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2021-08-09 22:22:06 +0200

    Cleanup (warnings; typos)
    GitOrigin-RevId: ee5158df43cf80d676bfc223aee4d1c422cc5c17

--- platform/testFramework/src/com/intellij/testFramework/rules/TempDirectory.kt
+++ platform/testFramework/src/com/intellij/testFramework/rules/TempDirectory.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.testFramework.rules
 
 import com.intellij.openapi.util.io.FileUtil
@@ -12,7 +12,6 @@ import com.intellij.testFramework.RunAll
 import com.intellij.testFramework.UsefulTestCase
 import com.intellij.testFramework.VfsTestUtil
 import com.intellij.util.io.zipFile
-import org.jetbrains.annotations.ApiStatus
 import org.junit.rules.ExternalResource
 import org.junit.runner.Description
 import org.junit.runners.model.Statement
@@ -74,7 +73,7 @@ class TempDirectory : ExternalResource() {
   }
 
   /**
-   * Creates a new directory with random name under the root temp directory.
+   * Creates a new directory with a random name under the root temp directory.
    */
   fun newDirectory(): File = newDirectory("dir" + myNextDirNameSuffix.incrementAndGet())
 
