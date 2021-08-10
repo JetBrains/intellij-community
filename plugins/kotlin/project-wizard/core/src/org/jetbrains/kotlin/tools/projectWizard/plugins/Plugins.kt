842d453b0ee25a3ca372d05d3105b7d76c233338
Andrei Klunnyi <andrei.klunnyi@jetbrains.com>
2021-08-10 15:59:12 +0200

    KTIJ-19374 NPW: performance improvement
    Wizard plugins irrelevant due to IDE plugins absence
    are now also disabled.
    
    ^KTIJ-19374 Fixed
    
    (cherry-picked from commit 45eda3641527317bf4d0fe1d54e193bab993ca05)
    
    GitOrigin-RevId: e30ae56500f9fb92689fbbfa90f44d4b6b53024c

--- plugins/kotlin/project-wizard/core/src/org/jetbrains/kotlin/tools/projectWizard/plugins/Plugins.kt
+++ plugins/kotlin/project-wizard/core/src/org/jetbrains/kotlin/tools/projectWizard/plugins/Plugins.kt
@@ -1,8 +1,11 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.kotlin.tools.projectWizard.plugins
 
-import kotlinx.collections.immutable.persistentListOf
 import org.jetbrains.kotlin.tools.projectWizard.core.Context
+import org.jetbrains.kotlin.tools.projectWizard.core.Plugin
+import org.jetbrains.kotlin.tools.projectWizard.core.buildPersistenceList
+import org.jetbrains.kotlin.tools.projectWizard.core.service.BuildSystemAvailabilityWizardService
+import org.jetbrains.kotlin.tools.projectWizard.plugins.buildSystem.BuildSystemType
 import org.jetbrains.kotlin.tools.projectWizard.plugins.buildSystem.JpsPlugin
 import org.jetbrains.kotlin.tools.projectWizard.plugins.buildSystem.MavenPlugin
 import org.jetbrains.kotlin.tools.projectWizard.plugins.buildSystem.gradle.GroovyDslPlugin
@@ -13,29 +16,38 @@ import org.jetbrains.kotlin.tools.projectWizard.plugins.templates.*
 
 object Plugins {
     val allPlugins = { context: Context ->
-        persistentListOf(
-            StructurePlugin(context),
+        val buildSystemService = context.read {
+            service<BuildSystemAvailabilityWizardService>()
+        }
 
-            GroovyDslPlugin(context),
-            KotlinDslPlugin(context),
-            JpsPlugin(context),
-            MavenPlugin(context),
+        buildPersistenceList<Plugin> {
+            +StructurePlugin(context)
+            if (buildSystemService.isAvailable(BuildSystemType.GradleGroovyDsl))
+                +GroovyDslPlugin(context)
 
-            KotlinPlugin(context),
-            TemplatesPlugin(context),
-            ProjectTemplatesPlugin(context),
-            RunConfigurationsPlugin(context),
-            AndroidPlugin(context),
+            if (buildSystemService.isAvailable(BuildSystemType.GradleKotlinDsl))
+                +KotlinDslPlugin(context)
+
+            +JpsPlugin(context)
+
+            if (buildSystemService.isAvailable(BuildSystemType.Maven))
+                +MavenPlugin(context)
+
+            +KotlinPlugin(context)
+            +TemplatesPlugin(context)
+            +ProjectTemplatesPlugin(context)
+            +RunConfigurationsPlugin(context)
+            +AndroidPlugin(context)
 
             // templates
-            ConsoleJvmApplicationTemplatePlugin(context),
-            KtorTemplatesPlugin(context),
-            JsTemplatesPlugin(context),
-            ReactJsTemplatesPlugin(context),
-            SimpleNodeJsTemplatesPlugin(context),
-            NativeConsoleApplicationTemplatePlugin(context),
-            ComposeTemplatesPlugin(context),
-            MobileMppTemplatePlugin(context),
-        )
+            +ConsoleJvmApplicationTemplatePlugin(context)
+            +KtorTemplatesPlugin(context)
+            +JsTemplatesPlugin(context)
+            +ReactJsTemplatesPlugin(context)
+            +SimpleNodeJsTemplatesPlugin(context)
+            +NativeConsoleApplicationTemplatePlugin(context)
+            +ComposeTemplatesPlugin(context)
+            +MobileMppTemplatePlugin(context)
+        }
     }
 }
