2837215a59d6abd954ec8aec6e3a39a48625bc9d
Anastasia Ivanova <anastasia.ivanova@jetbrains.com>
2021-08-10 09:30:23 +0200

    FUS: migrate "build.maven.actions" to new API (IDEA-268269)
    GitOrigin-RevId: ffacbd301a593372ba8cadd6cd13917da498bef4

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/server/ui/ConnectorTable.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/server/ui/ConnectorTable.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.maven.server.ui
 
 import com.intellij.execution.util.ListTableWithButtons
@@ -18,7 +18,7 @@ class ConnectorTable : ListTableWithButtons<MavenServerConnector>() {
 
   val stop = object : AnActionButton(MavenConfigurableBundle.message("connector.ui.stop"), AllIcons.Debugger.KillProcess) {
     override fun actionPerformed(e: AnActionEvent) {
-      MavenActionsUsagesCollector.trigger(e.project, MavenActionsUsagesCollector.ActionID.KillMavenConnector)
+      MavenActionsUsagesCollector.trigger(e.project, MavenActionsUsagesCollector.KILL_MAVEN_CONNECTOR)
       val connector = tableView.selectedObject ?: return;
       connector.shutdown(true)
       this@ConnectorTable.refreshValues()
--- plugins/maven/src/main/java/org/jetbrains/idea/maven/server/wsl/WslMavenRemoteProcessSupportFactory.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/server/wsl/WslMavenRemoteProcessSupportFactory.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.maven.server.wsl
 
 import com.intellij.execution.wsl.WSLDistribution
@@ -21,7 +21,7 @@ class WslMavenRemoteProcessSupportFactory : MavenRemoteProcessSupportFactory {
     val wslDistribution = project.basePath?.let { WslPath.getDistributionByWindowsUncPath(it) }
                           ?: throw IllegalArgumentException("Project $project is not WSL based!")
     MavenLog.LOG.info("Use WSL maven distribution at ${mavenDistribution}")
-    trigger(project, MavenActionsUsagesCollector.ActionID.StartWslMavenServer)
+    trigger(project, MavenActionsUsagesCollector.START_WSL_MAVEN_SERVER)
     val wslMavenDistribution = toWslMavenDistribution(mavenDistribution, wslDistribution)
     return WslMavenServerRemoteProcessSupport(wslDistribution, jdk, vmOptions, wslMavenDistribution, project, debugPort)
   }
