00b121a46b97ed4e6dcf605f7d883b537be2db54
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2021-08-11 18:58:51 +0300

    EA-344855 Check that Git is installed before lesson start
    And do not start lesson if Git is not installed or version is invalid
    
    IJ-CR-12951
    
    GitOrigin-RevId: 6be5192c1200b189e07267b2c6af8238867d0002

--- plugins/git-features-trainer/src/git4idea/ift/lesson/GitLesson.kt
+++ plugins/git-features-trainer/src/git4idea/ift/lesson/GitLesson.kt
@@ -2,16 +2,21 @@
 package git4idea.ift.lesson
 
 import com.intellij.openapi.project.Project
+import git4idea.config.GitExecutableManager
 import git4idea.ift.GitProjectUtil
 import org.jetbrains.annotations.Nls
 import org.jetbrains.annotations.NonNls
 import training.learn.course.KLesson
 import training.learn.course.LessonProperties
+import training.learn.exceptons.LessonPreparationException
 
 abstract class GitLesson(@NonNls id: String, @Nls name: String) : KLesson(id, name) {
   override val properties = LessonProperties(availableSince = "212")
 
   override fun prepare(project: Project) {
-    GitProjectUtil.restoreGitLessonsFiles(project)
+    if (GitExecutableManager.getInstance().testGitExecutableVersionValid(project)) {
+      GitProjectUtil.restoreGitLessonsFiles(project)
+    }
+    else throw LessonPreparationException("Git is not installed or version is invalid")
   }
 }
