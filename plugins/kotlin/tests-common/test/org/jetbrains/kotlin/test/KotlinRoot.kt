26bac9a6300cccf798a5041d4d5e52ba41ffddaa
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-08-11 15:13:01 +0200

    FIR IDE: fix uast test failing on TC because of invalid paths
    GitOrigin-RevId: c6dc8a9e57a05cac5726062edf14a9b6c262a626

--- plugins/kotlin/tests-common/test/org/jetbrains/kotlin/test/KotlinRoot.kt
+++ plugins/kotlin/tests-common/test/org/jetbrains/kotlin/test/KotlinRoot.kt
@@ -1,8 +1,12 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+/*
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
 package org.jetbrains.kotlin.test
 
 import com.intellij.openapi.application.PathManager
 import java.io.File
+import java.nio.file.Path
 
 object KotlinRoot {
     @JvmField
@@ -11,4 +15,7 @@ object KotlinRoot {
     @JvmField
     val DIR: File = REPO.resolve("community/plugins/kotlin").takeIf { it.exists() }
         ?: File(PathManager.getCommunityHomePath()).resolve("plugins/kotlin")
+
+    @JvmField
+    val DIR_PATH: Path = DIR.toPath()
 }
