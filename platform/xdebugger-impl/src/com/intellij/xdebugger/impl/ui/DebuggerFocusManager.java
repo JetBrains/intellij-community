1703b4af36cebc6b787fe1170f17eceeab46e59d
Egor Ushakov <egor.ushakov@jetbrains.com>
2021-08-12 19:35:05 +0300

    EA-267579 - NPE: DebuggerFocusManager.onDebugFinished - do not rely on local counters
    GitOrigin-RevId: ca619dab6151871c67fe3ed6b1cde57148504c1f

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/ui/DebuggerFocusManager.java
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/ui/DebuggerFocusManager.java
@@ -2,10 +2,9 @@
 package com.intellij.xdebugger.impl.ui;
 
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.project.ProjectManagerListener;
-import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.util.registry.RegistryValue;
 import com.intellij.openapi.util.registry.RegistryValueListener;
@@ -15,51 +14,33 @@ import com.intellij.xdebugger.XDebuggerManager;
 import com.intellij.xdebugger.XDebuggerManagerListener;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.Arrays;
 
 /**
  * This class mutes focus stealing prevention mechanism on Windows, while at least one debug session is active, to make 'Focus application
  * on breakpoint' setting work as expected.
  */
 public class DebuggerFocusManager implements XDebuggerManagerListener, RegistryValueListener, ProjectManagerListener {
-  private static final Logger LOG = Logger.getInstance(DebuggerFocusManager.class);
-  private static final Key<List<XDebugProcess>> ACTIVE_EXECUTIONS = Key.create("DebuggerFocusManager.active.executions");
   private final RegistryValue mySetting = Registry.get("debugger.mayBringFrameToFrontOnBreakpoint");
-  private int mySessionCount;
   private boolean myFocusStealingEnabled;
 
   private DebuggerFocusManager() {
     mySetting.addListener(this, ApplicationManager.getApplication());
   }
 
-    @Override
+  @Override
   public void processStarted(@NotNull XDebugProcess debugProcess) {
-    Project project = debugProcess.getSession().getProject();
-    List<XDebugProcess> executions = project.getUserData(ACTIVE_EXECUTIONS);
-    if (executions == null) {
-      project.putUserData(ACTIVE_EXECUTIONS, executions = new ArrayList<>());
-    }
-    executions.add(debugProcess);
-    update(1);
+    update(true);
   }
 
   @Override
   public void processStopped(@NotNull XDebugProcess debugProcess) {
-    Project project = debugProcess.getSession().getProject();
-    List<XDebugProcess> executions = Objects.requireNonNull(project.getUserData(ACTIVE_EXECUTIONS));
-    if (executions.remove(debugProcess)) {
-      update(-1);
-    }
-    else {
-      LOG.error("Unexpected event for " + debugProcess);
-    }
+    update(getDebugSessionsCount() > 1); // stopped session is still counted
   }
 
   @Override
   public void afterValueChanged(@NotNull RegistryValue value) {
-    update(0);
+    update(getDebugSessionsCount() > 0);
   }
 
   @Override
@@ -69,17 +50,18 @@ public class DebuggerFocusManager implements XDebuggerManagerListener, RegistryV
 
   @Override
   public void projectClosed(@NotNull Project project) {
-    // verify that everything is closed
-    List<XDebugProcess> executions = project.getUserData(ACTIVE_EXECUTIONS);
-    if (executions != null) {
-      LOG.assertTrue(executions.isEmpty());
-      project.putUserData(ACTIVE_EXECUTIONS, null);
-    }
+    update(getDebugSessionsCount() > 0);
   }
 
-  private synchronized void update(int sessionCountDelta) {
-    mySessionCount += sessionCountDelta;
-    boolean shouldBeEnabled = mySessionCount > 0 && mySetting.asBoolean();
+  private static long getDebugSessionsCount() {
+    return Arrays.stream(ProjectManager.getInstance().getOpenProjects())
+      .filter(p -> !p.isDisposed())
+      .mapToInt(p -> XDebuggerManager.getInstance(p).getDebugSessions().length)
+      .sum();
+  }
+
+  private synchronized void update(boolean enable) {
+    boolean shouldBeEnabled = enable && mySetting.asBoolean();
     if (shouldBeEnabled != myFocusStealingEnabled) {
       WinFocusStealer.setFocusStealingEnabled(myFocusStealingEnabled = shouldBeEnabled);
     }
