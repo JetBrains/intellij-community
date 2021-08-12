9eb6fb0bd0f2e4efb71730b51c7d873863e17ee4
Nikita Skvortsov <Nikita.Skvortsov@jetbrains.com>
2021-08-12 11:06:25 +0300

    [gradle] refactor GradleAutoLinkTest
    GitOrigin-RevId: c7b45298f5ddcc63fbc176d6acd440b4659a18ce

--- plugins/gradle/java/testSources/importing/GradleAutoLinkTestCase.kt
+++ plugins/gradle/java/testSources/importing/GradleAutoLinkTestCase.kt
@@ -1,9 +1,9 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.gradle.importing
 
-import com.intellij.openapi.externalSystem.test.ExternalSystemTestCase
+import com.intellij.openapi.externalSystem.autolink.AutoLinkTestCase
 
-abstract class GradleAutoLinkTestCase : ExternalSystemTestCase() {
+abstract class GradleAutoLinkTestCase : AutoLinkTestCase() {
 
   override fun getTestsTempDir() = "tmp${System.currentTimeMillis()}"
 
