4f2703ce821539ccae46cb978d5cd87841650b5b
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-08-12 13:34:45 +0200

    Initial impelementation of project-model-updater
    (cherry picked from commit 07a1779203267c79a48fa71421281aa7951b719e)
    
    GitOrigin-RevId: 9a788c0e108c739c3e92cabb791eea9697d03585

---
+++ plugins/kotlin/util/project-model-updater/src/org/jetbrains/tools/model/updater/Main.kt
@@ -0,0 +1,71 @@
+package org.jetbrains.tools.model.updater
+
+import org.jetbrains.tools.model.updater.impl.Args
+import org.jetbrains.tools.model.updater.impl.JpsPath
+import org.jetbrains.tools.model.updater.impl.KotlincArtifactsMode
+import org.jetbrains.tools.model.updater.impl.escapeForRegex
+import java.io.File
+import java.util.*
+
+fun main(args: Array<String>) {
+    val manualArgs = args.flatMap { it.split(" ") }.toMapOfArgs()
+    val defaultArgs = File(object {}::class.java.getResource("/model.properties")!!.toURI()).inputStream().use { stream ->
+        Properties().apply { load(stream) }.map { (key, value) -> key as String to value as String }.toMap()
+    }
+    val communityRoot = generateSequence(File(".").canonicalFile) { it.parentFile }.first {
+        it.resolve(".idea").isDirectory && !it.resolve("community").isDirectory
+    }
+    val monorepoRoot = communityRoot.resolve("..").takeIf { it.resolve(".idea").isDirectory }
+
+    for ((root, isCommunity) in listOf(monorepoRoot to false, communityRoot to true)) {
+        if (root == null) continue
+        val parsedArgs = Args(defaultArgs + manualArgs)
+        generateProjectModelFiles(root.resolve(".idea"), parsedArgs, isCommunity)
+        patchProjectModelFiles(root.resolve(".idea"), parsedArgs, isCommunity)
+    }
+}
+
+private fun generateProjectModelFiles(dotIdea: File, args: Args, isCommunity: Boolean) {
+    generateKotlincLibraries(args.kotlincArtifactsMode, args.kotlincVersion, isCommunity).forEach {
+        val libXmlName = it.name.replace(".", "_").replace("-", "_") + ".xml"
+        dotIdea.resolve("libraries").resolve(libXmlName).writeText(it.generateXml())
+    }
+}
+
+private fun patchProjectModelFiles(dotIdea: File, args: Args, isCommunity: Boolean) {
+    val kotlincMvnLibs = generateKotlincLibraries(KotlincArtifactsMode.MAVEN, args.kotlincVersion, isCommunity)
+
+    val artifactIds = kotlincMvnLibs.flatMap { lib -> lib.classes.map { (it.jpsPath as JpsPath.MavenRepository).mavenId.artifactId } }
+    val replacements = generateKotlincLibraries(args.kotlincArtifactsMode, args.kotlincVersion, isCommunity)
+        .flatMap { lib -> lib.classes.map { it.jpsPath.generateXml() } }
+
+    val stubVersion = "STUB_VERSION"
+    val regexes = KotlincArtifactsMode.values().flatMap { generateKotlincLibraries(it, stubVersion, isCommunity) }
+        .flatMap { it.classes }
+        .map {
+            it.jpsPath.generateXml().escapeForRegex().replace(stubVersion, """\d+.\d+.\d+(-SNAPSHOT|-M1|-M2|-RC|-release)?(-\d+)?""")
+        }
+        .distinct()
+        .map { Regex(it) }
+        .toList()
+
+    for (artifactXmlFile in dotIdea.resolve("artifacts").listFiles()!!) {
+        for (artifactId in artifactIds) {
+            val allMatchingRegexes = regexes.filter { it.toString().contains("\\/$artifactId\\/") }
+            check(allMatchingRegexes.size == KotlincArtifactsMode.values().size) {
+                "There should be only ${KotlincArtifactsMode.values().size} matching regexes"
+            }
+            val replacement = replacements.single { it.contains("/$artifactId/") }
+            val newText = allMatchingRegexes.fold(artifactXmlFile.readText()) { text, regex ->
+                regex.replace(text, Regex.escapeReplacement(replacement))
+            }
+            artifactXmlFile.writeText(newText)
+        }
+    }
+}
+
+private fun Iterable<String>.toMapOfArgs(): Map<String, String> = associate { arg ->
+    arg.split("=").also {
+        check(it.size == 2) { "All arguments are expected to be key value pairs in 'key=value' format but got '$arg'" }
+    }.let { it[0] to it[1] }
+}
