b811d4f4e01c3b8d956a9c03d8dce2c53a080466
Tianyu Geng <tgeng@google.com>
2021-08-12 20:24:33 -0700

    FIR IDE: add KindWeigher

--- plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/completion/completionUtilsNoResolve.kt
+++ plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/completion/completionUtilsNoResolve.kt
@@ -6,15 +6,15 @@ import com.intellij.codeInsight.completion.InsertionContext
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.codeInsight.lookup.LookupElementDecorator
+import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.idea.completion.handlers.WithTailInsertHandler
+import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.name.Name
-import org.jetbrains.kotlin.psi.KtDeclarationWithBody
-import org.jetbrains.kotlin.psi.KtElement
-import org.jetbrains.kotlin.psi.KtLabeledExpression
-import org.jetbrains.kotlin.psi.KtLoopExpression
+import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.psiUtil.getNonStrictParentOfType
+import org.jetbrains.kotlin.psi.psiUtil.isAncestor
 import org.jetbrains.kotlin.psi.psiUtil.parentsWithSelf
 import org.jetbrains.kotlin.renderer.render
-import java.util.ArrayList
 
 fun createKeywordElement(
     keyword: String,
@@ -73,4 +73,70 @@ fun createKeywordElementWithSpace(
 
 fun Name?.labelNameToTail(): String = if (this != null) "@" + render() else ""
 
+
+/**
+ *  Checks whether user likely expects completion at the given position to offer a return statement.
+ */
+fun isLikelyInPositionForReturn(position: KtElement, parent: KtDeclarationWithBody, parentReturnUnit: Boolean): Boolean {
+    val isInTopLevelInUnitFunction = parentReturnUnit && position.parent?.parent === parent
+
+    val isInsideLambda = position.getNonStrictParentOfType<KtFunctionLiteral>()?.let { parent.isAncestor(it) } == true
+    return when {
+        isInsideLambda -> false // for now we do not want to alter completion inside lambda bodies
+        position.inReturnExpression() -> false
+        position.isRightOperandInElvis() -> true
+        position.isLastOrSingleStatement() && !position.isDirectlyInLoopBody() && !isInTopLevelInUnitFunction -> true
+        else -> false
+    }
+}
+
+private fun KtElement.inReturnExpression(): Boolean = findReturnExpression(this) != null
+
+fun KtElement.isRightOperandInElvis(): Boolean {
+    val elvisParent = parent as? KtBinaryExpression ?: return false
+    return elvisParent.operationToken == KtTokens.ELVIS && elvisParent.right === this
+}
+
+/**
+ * Checks if expression is either last expression in a block, or a single expression in position where single
+ * expressions are allowed (`when` entries, `for` and `while` loops, and `if`s).
+ */
+private fun PsiElement.isLastOrSingleStatement(): Boolean =
+    when (val containingExpression = parent) {
+        is KtBlockExpression -> containingExpression.statements.lastOrNull() === this
+        is KtWhenEntry, is KtContainerNodeForControlStructureBody -> true
+        else -> false
+    }
+
+private fun KtElement.isDirectlyInLoopBody(): Boolean {
+    val loopContainer = when (val blockOrContainer = parent) {
+        is KtBlockExpression -> blockOrContainer.parent as? KtContainerNodeForControlStructureBody
+        is KtContainerNodeForControlStructureBody -> blockOrContainer
+        else -> null
+    }
+
+    return loopContainer?.parent is KtLoopExpression
+}
+
+/**
+ * If [expression] is directly relates to the return expression already, this return expression will be found.
+ *
+ * Examples:
+ *
+ * ```kotlin
+ * return 10                                        // 10 is in return
+ * return if (true) 10 else 20                      // 10 and 20 are in return
+ * return 10 ?: 20                                  // 10 and 20 are in return
+ * return when { true -> 10 ; else -> { 20; 30 } }  // 10 and 30 are in return, but 20 is not
+ * ```
+ */
+private tailrec fun findReturnExpression(expression: PsiElement?): KtReturnExpression? =
+    when (val parent = expression?.parent) {
+        is KtReturnExpression -> parent
+        is KtBinaryExpression -> if (parent.operationToken == KtTokens.ELVIS) findReturnExpression(parent) else null
+        is KtContainerNodeForControlStructureBody, is KtIfExpression -> findReturnExpression(parent)
+        is KtBlockExpression -> if (expression.isLastOrSingleStatement()) findReturnExpression(parent) else null
+        is KtWhenEntry -> findReturnExpression(parent.parent)
+        else -> null
+    }
 
