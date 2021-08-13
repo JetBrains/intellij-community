c62ec25b24dcb2933394f90a36f352a4a69622ff
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2021-08-13 13:03:04 +0200

    [kotlin] i18n kotlin jps
    GitOrigin-RevId: 7aad0a02ca6511dbbe659c2933acf535a587c268

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/KotlinJpsClasspathProviderTest.kt
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/KotlinJpsClasspathProviderTest.kt
@@ -14,8 +14,9 @@ class KotlinJpsClasspathProviderTest : LightPlatformTestCase() {
 
         val jars = inst.classPath
 
-        assertEquals(2, jars.size)
+        assertEquals(3, jars.size)
         assertTrue(jars[0], File(jars[0]!!).name.startsWith("kotlin-reflect"))
         assertTrue(jars[1], File(jars[1]!!).name.startsWith("kotlin-compiler-for-ide") || File(jars[1]!!).name == "kotlin-plugin.jar")
+        assertTrue(jars[2], File(jars[2]!!).name == "intellij.platform.core")
     }
 }
