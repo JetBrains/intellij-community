df858ec003ca51f16a151f093f6ce6cc30bcad6c
Alexey Kalina <alexey.kalina@jetbrains.com>
2021-08-13 11:49:05 +0300

    [ml-completion] add seed to a/b config and shuffle buckets
    GitOrigin-RevId: 3ce99ced983c319c36a87ff775e73b3537416473

--- plugins/completion-ml-ranking/src/com/intellij/completion/ml/experiment/ExperimentConfig.kt
+++ plugins/completion-ml-ranking/src/com/intellij/completion/ml/experiment/ExperimentConfig.kt
@@ -1,14 +1,15 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.completion.ml.experiment
 
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class ExperimentConfig(val version: Int,
+                            val seed: Long?,
                             val groups: List<ExperimentGroupConfig>,
                             val languages: List<ExperimentLanguageConfig>) {
   companion object {
-    private val DISABLED_EXPERIMENT = ExperimentConfig(version = 2, groups = emptyList(), languages = emptyList())
+    private val DISABLED_EXPERIMENT = ExperimentConfig(version = 2, seed = null, groups = emptyList(), languages = emptyList())
     fun disabledExperiment(): ExperimentConfig = DISABLED_EXPERIMENT
   }
 }
