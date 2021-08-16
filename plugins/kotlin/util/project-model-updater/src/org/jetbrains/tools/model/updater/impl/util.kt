4f2bc2041da949061349b77e647fd4496e0812e1
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-08-16 19:28:33 +0200

    [project-model-updater] Generate & patch run configurations
    GitOrigin-RevId: 5961dada7445ce36c32518ccc17598ab2945941a

--- plugins/kotlin/util/project-model-updater/src/org/jetbrains/tools/model/updater/impl/util.kt
+++ plugins/kotlin/util/project-model-updater/src/org/jetbrains/tools/model/updater/impl/util.kt
@@ -1,5 +1,12 @@
 package org.jetbrains.tools.model.updater.impl
 
+import org.jdom.Element
+import org.jdom.input.SAXBuilder
+import org.jdom.output.Format
+import org.jdom.output.XMLOutputter
+import java.io.ByteArrayOutputStream
+import java.io.File
+
 fun String.trimMarginWithInterpolations(): String {
     val regex = Regex("""^(\s*\|)(\s*).*$""")
     var prevIndent = ""
@@ -35,3 +42,40 @@ fun singleJarMvnLib(
 fun String.escapeForRegex() = replace("/", "\\/")
     .replace("$", "\\$")
     .replace(".", "\\.")
+
+fun File.readXml(): Element {
+    return inputStream().use { SAXBuilder().build(it).rootElement }
+}
+
+fun String.readXml(): Element {
+    return byteInputStream().use { SAXBuilder().build(it).rootElement }
+}
+
+fun String.jpsEntityNameToFilename(): String = replace(".", "_").replace("-", "_").replace(" ", "_")
+
+fun Element.writeXml(): String {
+    val bos = ByteArrayOutputStream()
+
+    bos.writer().use { writer ->
+        val format = Format.getCompactFormat()
+            .setIndent("  ")
+            .setTextMode(Format.TextMode.TRIM)
+            .setEncoding("UTF-8")
+            .setOmitEncoding(false)
+            .setOmitDeclaration(false)
+            .setLineSeparator("\n")
+
+        val outputter = XMLOutputter(format)
+        outputter.output(this, writer)
+    }
+
+    return bos.toString()
+}
+
+private suspend fun SequenceScope<Element>.visit(element: Element) {
+    element.children.forEach { visit(it) }
+    yield(element)
+}
+fun Element.traverseChildren(): Sequence<Element> {
+    return sequence { visit(this@traverseChildren) }
+}
