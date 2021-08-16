450635f5cceadce614a8872f654b8d9566f39c05
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-08-16 12:49:05 +0200

    [project-model-updater] Check for unknown args
    GitOrigin-RevId: 7ad1a1a835f37efff027a2729a1e5b803e085942

--- plugins/kotlin/util/project-model-updater/src/org/jetbrains/tools/model/updater/impl/Args.kt
+++ plugins/kotlin/util/project-model-updater/src/org/jetbrains/tools/model/updater/impl/Args.kt
@@ -1,14 +1,33 @@
 package org.jetbrains.tools.model.updater.impl
 
 import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+import kotlin.reflect.KVisibility
 
-data class Args(val args: Map<String, String>) {
-    val kotlincVersion: String by args
-    val kotlincArtifactsMode: KotlincArtifactsMode by mapDelegate(args, KotlincArtifactsMode::valueOf)
+class Args(args: Map<String, String>) {
+    private val mutableArgs: MutableMap<String, String> = HashMap(args)
 
-    private fun <T> mapDelegate(map: Map<String, String>, transform: (String) -> T) =
-        ReadOnlyProperty<Any?, T> { thisRef, property ->
-            map[property.name]?.let(transform) ?: error("Cannot find ${property.name} in $map}")
+    val kotlincVersion: String by mapDelegate(mutableArgs)
+    val kotlincArtifactsMode: KotlincArtifactsMode by mapDelegate(mutableArgs, KotlincArtifactsMode::valueOf)
+
+    init {
+        this::class.members.filterIsInstance<KProperty<*>>()
+            .filter { it.visibility == KVisibility.PUBLIC }
+            .forEach { it.getter.call(this) } // Initialize all the values
+        check(mutableArgs.isEmpty()) {
+            "Unknown args: " + mutableArgs.map { (key, value) -> "$key=$value" }.joinToString()
+        }
+    }
+
+    private fun mapDelegate(map: MutableMap<String, String>) = mapDelegate(map) { it }
+    private fun <T> mapDelegate(map: MutableMap<String, String>, transform: (String) -> T) =
+        object : ReadOnlyProperty<Any?, T> {
+            private var value: T? = null
+
+            override fun getValue(thisRef: Any?, property: KProperty<*>): T {
+                return value ?: map.remove(property.name)?.let(transform).also { value = it }
+                    ?: error("Cannot find ${property.name} in $map}")
+            }
         }
 }
 
