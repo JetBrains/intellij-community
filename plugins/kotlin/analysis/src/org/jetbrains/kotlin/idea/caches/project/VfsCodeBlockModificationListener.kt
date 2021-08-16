40f98cc83114142bbb6beb3d375f36daddafc8b9
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2021-08-16 12:02:06 +0200

    [kotlin] handle vfs events asynchronously to not block EDT
    (cherry picked from commit 76b0733c4c50af97d1567ff5fd66806d7612f39f)
    
    IJ-CR-13617
    
    GitOrigin-RevId: 871d009bc7329ee0581fcf9721c2d17cfd81bede

--- plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/caches/project/VfsCodeBlockModificationListener.kt
+++ plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/caches/project/VfsCodeBlockModificationListener.kt
@@ -3,9 +3,9 @@ package org.jetbrains.kotlin.idea.caches.project
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
-import com.intellij.openapi.vfs.VirtualFileManager
-import com.intellij.openapi.vfs.newvfs.BulkFileListener
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent
+import com.intellij.vfs.AsyncVfsEventsListener
+import com.intellij.vfs.AsyncVfsEventsPostProcessor
 import org.jetbrains.kotlin.idea.caches.trackers.KotlinCodeBlockModificationListener
 import org.jetbrains.kotlin.idea.core.KotlinPluginDisposable
 import org.jetbrains.kotlin.idea.util.ProjectRootsUtil
@@ -14,17 +14,14 @@ class VfsCodeBlockModificationListener: StartupActivity.Background {
     override fun runActivity(project: Project) {
         val disposable = KotlinPluginDisposable.getInstance(project)
         val kotlinOCBModificationListener = KotlinCodeBlockModificationListener.getInstance(project)
-        val connection = project.messageBus.connect(disposable)
-        connection.subscribe(VirtualFileManager.VFS_CHANGES, object : BulkFileListener {
-            override fun after(events: List<VFileEvent>) {
-                val projectRelatedVfsFileChange = events.any { event ->
-                    val file = event.takeIf { it.isFromRefresh }?.file ?: return@any false
-                    ProjectRootsUtil.isProjectSourceFile(project, file)
-                }
-                if (projectRelatedVfsFileChange) {
-                    kotlinOCBModificationListener.incModificationCount()
-                }
+        AsyncVfsEventsPostProcessor.getInstance().addListener(AsyncVfsEventsListener { events: List<VFileEvent> ->
+            val projectRelatedVfsFileChange = events.any { event ->
+                val file = event.takeIf { it.isFromRefresh }?.file ?: return@any false
+                ProjectRootsUtil.isProjectSourceFile(project, file)
             }
-        })
+            if (projectRelatedVfsFileChange) {
+                kotlinOCBModificationListener.incModificationCount()
+            }
+        }, disposable)
     }
 }
