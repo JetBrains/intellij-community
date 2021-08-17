dd14ee98b6c5deaca5b1a645db9d29ed92fd1a25
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2021-08-17 22:58:41 +0200

    [kssr] Add language annotation to replace test
    GitOrigin-RevId: 8a7ea2cd7cfe919b2cfacb5b4110a4ff9c41468b

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/structuralsearch/KotlinSSRReplaceTest.kt
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/structuralsearch/KotlinSSRReplaceTest.kt
@@ -14,6 +14,7 @@ import com.intellij.structuralsearch.plugin.util.CollectingMatchResultSink
 import com.intellij.testFramework.LightProjectDescriptor
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.SmartList
+import org.intellij.lang.annotations.Language
 import org.jetbrains.kotlin.idea.KotlinFileType
 import org.jetbrains.kotlin.idea.util.application.executeWriteCommand
 
@@ -28,8 +29,8 @@ abstract class KotlinSSRReplaceTest : BasePlatformTestCase() {
     protected fun doTest(
         searchPattern: String,
         replacePattern: String,
-        match: String,
-        result: String,
+        @Language("kotlin") match: String,
+        @Language("kotlin") result: String,
         reformat: Boolean = false,
         shortenFqNames: Boolean = false,
         context: PatternContext = KotlinStructuralSearchProfile.DEFAULT_CONTEXT
