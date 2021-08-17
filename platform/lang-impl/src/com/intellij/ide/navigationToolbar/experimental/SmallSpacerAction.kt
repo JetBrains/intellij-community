8385ff1f17dc5d1344ba33320c3740c92a52c8fe
Elena.Korotkova <elena.korotkova@jetbrains.com>
2021-08-17 16:18:05 +0300

    Fix new toolbar borders
    GitOrigin-RevId: eb06e987ed60d78b8cb84d8e959a6661be76129b

--- platform/lang-impl/src/com/intellij/ide/navigationToolbar/experimental/SmallSpacerAction.kt
+++ platform/lang-impl/src/com/intellij/ide/navigationToolbar/experimental/SmallSpacerAction.kt
@@ -5,21 +5,26 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.Presentation
 import com.intellij.openapi.actionSystem.ex.CustomComponentAction
 import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.util.ui.JBUI
 import java.awt.Dimension
 import javax.swing.BorderFactory
 import javax.swing.JComponent
 import javax.swing.JPanel
 
-class SmallSpacerAction : DumbAwareAction(), CustomComponentAction {
+open class SpacerAction(val space: Int) : DumbAwareAction(), CustomComponentAction {
   override fun createCustomComponent(presentation: Presentation, place: String): JComponent {
     return JPanel().apply {
       border = BorderFactory.createEmptyBorder(0, 0, 0, 0)
-      minimumSize = Dimension(6, 6)
-      preferredSize = Dimension(6, 6)
-      maximumSize = Dimension(6, 6)
+      minimumSize = Dimension(space, space)
+      preferredSize = Dimension(space, space)
+      maximumSize = Dimension(space, space)
     }
   }
 
   override fun actionPerformed(e: AnActionEvent) {
   }
 }
+
+class TinySpacerAction : SpacerAction(JBUI.scale(4))
+class SmallSpacerAction : SpacerAction(JBUI.scale(6))
+class BigSpacerAction : SpacerAction(JBUI.scale(8))
