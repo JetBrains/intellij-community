31cd1b0b8ff421d0272ac72a3641c6be5b61791f
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-08-17 17:49:11 +0200

    IDEA-275480 unconditionally turn off all grouping rules except for file grouping rule in `ShowUsagesSettings`
    The bug was caused by 86eb345cb97f85f822169d112608656c93aba466. The groups were expected to be not even created, instead
    they were instantiated but not rendered. That commit started rendering all groups. They cannot be turned on or off in
    the UI, but the settings (restored from broken `usageView.xml`) turn them on causing the behaviour described in the
    issue. This commit turns off the grouping, so that the groups are not even instantiated.
    
    GitOrigin-RevId: b19d7fd615491d58147fa8e0c2777c97ae06aad2

--- platform/usageView/src/com/intellij/usages/ShowUsagesSettings.kt
+++ platform/usageView/src/com/intellij/usages/ShowUsagesSettings.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.usages
 
 import com.intellij.openapi.application.ApplicationManager
@@ -27,4 +27,26 @@ class ShowUsagesSettings : PersistentStateComponent<ShowUsageViewSettings> {
   }
 }
 
-class ShowUsageViewSettings : UsageViewSettings(false, false, false, false, false)
+@Suppress("UNUSED_PARAMETER")
+class ShowUsageViewSettings : UsageViewSettings(false, false, false, false, false) {
+
+  override var isGroupByUsageType: Boolean
+    get() = false
+    set(value) {}
+
+  override var isGroupByModule: Boolean
+    get() = false
+    set(value) {}
+
+  override var isGroupByPackage: Boolean
+    get() = false
+    set(value) {}
+
+  override var isGroupByDirectoryStructure: Boolean
+    get() = false
+    set(value) {}
+
+  override var isGroupByScope: Boolean
+    get() = false
+    set(value) {}
+}
--- platform/usageView/src/com/intellij/usages/UsageViewSettings.kt
+++ platform/usageView/src/com/intellij/usages/UsageViewSettings.kt
@@ -1,8 +1,11 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.usages
 
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.components.*
+import com.intellij.openapi.components.BaseState
+import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
 import com.intellij.util.PathUtil
 import com.intellij.util.xmlb.annotations.OptionTag
 import com.intellij.util.xmlb.annotations.Transient
@@ -80,22 +83,22 @@ open class UsageViewSettings(
   var previewUsagesSplitterProportion: Float by property(0.5f)
 
   @get:OptionTag("GROUP_BY_USAGE_TYPE")
-  var isGroupByUsageType by property(isGroupByUsageType)
+  open var isGroupByUsageType by property(isGroupByUsageType)
 
   @get:OptionTag("GROUP_BY_MODULE")
-  var isGroupByModule by property(isGroupByModule)
+  open var isGroupByModule by property(isGroupByModule)
 
   @get:OptionTag("FLATTEN_MODULES")
   var isFlattenModules by property(true)
 
   @get:OptionTag("GROUP_BY_PACKAGE")
-  var isGroupByPackage by property(isGroupByPackage)
+  open var isGroupByPackage by property(isGroupByPackage)
 
   @get:OptionTag("GROUP_BY_FILE_STRUCTURE")
   var isGroupByFileStructure by property(isGroupByFileStructure)
 
   @get:OptionTag("GROUP_BY_DIRECTORY_STRUCTURE")
-  var isGroupByDirectoryStructure: Boolean by property(isGroupByDirectoryStructure)
+  open var isGroupByDirectoryStructure: Boolean by property(isGroupByDirectoryStructure)
 
   /**
    * Compact middle directories option for directories usage grouping
@@ -104,7 +107,7 @@ open class UsageViewSettings(
   var isCompactMiddleDirectories by property(false)
 
   @get:OptionTag("GROUP_BY_SCOPE")
-  var isGroupByScope by property(isGroupByScope)
+  open var isGroupByScope by property(isGroupByScope)
 
   var exportFileName: String?
     @Transient
