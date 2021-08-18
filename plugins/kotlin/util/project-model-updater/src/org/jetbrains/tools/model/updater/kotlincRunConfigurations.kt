1bb73167735b76c7972e0f060f4a728786e1474c
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-08-18 13:37:11 +0200

    Disable automatic kotlinc artifacts republishing
    It does more harm than good
    
    GitOrigin-RevId: 002a5b5b878629a1874d145d057ab96d3e5e0e61

--- plugins/kotlin/util/project-model-updater/src/org/jetbrains/tools/model/updater/kotlincRunConfigurations.kt
+++ plugins/kotlin/util/project-model-updater/src/org/jetbrains/tools/model/updater/kotlincRunConfigurations.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.tools.model.updater
 
-import org.jdom.Text
 import org.jetbrains.tools.model.updater.impl.*
 import java.io.File
 
@@ -43,25 +42,4 @@ fun generateRunConfigurations(dotIdea: File, kotlincArtifactsMode: KotlincArtifa
             )
         }
     }
-}
-
-fun patchRunConfigurations(dotIdea: File, kotlincArtifactsMode: KotlincArtifactsMode) {
-    listOf("runConfigurations/IDEA.xml", "runConfigurations/IDEA_Community.xml", "IDEA_Community_FIR.xml")
-        .map { dotIdea.resolve(it) }
-        .filter { it.exists() }
-        .forEach { runConfigurationFile ->
-            val root = runConfigurationFile.readXml()
-            root.traverseChildren().filter {
-                it.name == "option" && it.getAttributeValue("run_configuration_name") == PUBLISH_COMPILER_GRADLE_TASK_NAME
-            }.single().detach()
-            if (kotlincArtifactsMode == KotlincArtifactsMode.BOOTSTRAP) {
-                root.traverseChildren().filter { it.name == "method" && it.getAttributeValue("v") == "2" }.single().addContent(
-                    0,
-                    ("""<option name="RunConfigurationTask" enabled="true" """ +
-                            """run_configuration_name="$PUBLISH_COMPILER_GRADLE_TASK_NAME" """ +
-                            """run_configuration_type="GradleRunConfiguration" />""").readXml().detach()
-                )
-                runConfigurationFile.writeText(root.writeXml())
-            }
-        }
 }
