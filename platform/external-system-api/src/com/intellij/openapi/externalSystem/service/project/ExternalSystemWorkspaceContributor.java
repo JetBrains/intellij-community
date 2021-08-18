b5819443571a81f4780cd9ed0ad18be6bf1bbfc4
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2021-08-18 16:01:28 +0300

    [java doc] added docs for com.intellij.externalSystemWorkspaceContributor
    GitOrigin-RevId: ca1cccd79cddfbac49da603a936b382847d10df2

--- platform/external-system-api/src/com/intellij/openapi/externalSystem/service/project/ExternalSystemWorkspaceContributor.java
+++ platform/external-system-api/src/com/intellij/openapi/externalSystem/service/project/ExternalSystemWorkspaceContributor.java
@@ -1,4 +1,3 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.externalSystem.service.project;
 
 import com.intellij.openapi.externalSystem.model.project.ProjectCoordinate;
@@ -6,7 +5,17 @@ import com.intellij.openapi.module.Module;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * Extracts common data from external system data that is specifically smeared across workspace model.
+ */
 @ApiStatus.Experimental
 public interface ExternalSystemWorkspaceContributor {
+  /**
+   * Finds project coordinates for external project that corresponding to given module.
+   *
+   * @param module         is a module to find project coordinates of corresponding external project.
+   * @param modelsProvider is a modifiable model of current modification session.
+   * @return found external project coordinates.
+   */
   @Nullable ProjectCoordinate findProjectId(Module module, IdeModifiableModelsProvider modelsProvider);
 }
