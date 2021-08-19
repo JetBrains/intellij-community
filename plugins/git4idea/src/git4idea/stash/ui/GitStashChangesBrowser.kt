515a42bb645e498f209c5d915ec64c80b99e2118
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2021-08-20 00:55:49 +0300

    [git-stash] show index and untracked changes in the stash changes browser
    GitOrigin-RevId: 7a68ff14ff771fdaae5ec7424d3771bdd38fe309

--- plugins/git4idea/src/git4idea/stash/ui/GitStashChangesBrowser.kt
+++ plugins/git4idea/src/git4idea/stash/ui/GitStashChangesBrowser.kt
@@ -5,23 +5,36 @@ import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vcs.changes.Change
-import com.intellij.openapi.vcs.changes.ui.SimpleChangesBrowser
+import com.intellij.openapi.vcs.changes.ui.ChangesBrowserBase
+import com.intellij.openapi.vcs.changes.ui.ChangesBrowserNode
+import com.intellij.openapi.vcs.changes.ui.TreeModelBuilder
 import com.intellij.ui.SimpleTextAttributes
 import com.intellij.util.concurrency.EdtExecutorService
 import com.intellij.util.ui.StatusText
+import com.intellij.vcs.log.Hash
+import git4idea.GitCommit
 import git4idea.i18n.GitBundle
 import git4idea.stash.GitStashCache
 import git4idea.ui.StashInfo
+import org.jetbrains.annotations.Nls
 import java.util.concurrent.CompletableFuture
+import javax.swing.tree.DefaultTreeModel
 
-class GitStashChangesBrowser(project: Project) : SimpleChangesBrowser(project, false, false) {
+class GitStashChangesBrowser(project: Project) : ChangesBrowserBase(project, false, false) {
   private val stashCache: GitStashCache get() = myProject.service()
+
+  private var stashedChanges: Collection<Change> = emptyList()
+  private var otherChanges: Map<ChangesBrowserNode.Tag, Set<Change>> = emptyMap()
+
+  val allChanges get() = stashedChanges
 
   private var currentStash: StashInfo? = null
   private var currentChangesFuture: CompletableFuture<GitStashCache.StashData>? = null
 
   init {
+    init()
     viewer.emptyText.text = GitBundle.message("stash.changes.empty")
   }
 
@@ -45,7 +58,7 @@ class GitStashChangesBrowser(project: Project) : SimpleChangesBrowser(project, f
 
       when (val stashData = currentChangesFuture?.get()) {
         is GitStashCache.StashData.Changes -> {
-          setData(stashData.changes)
+          setData(stashData.changes, stashData.parentCommits)
         }
         is GitStashCache.StashData.Error -> {
           setEmpty { statusText -> statusText.setText(stashData.error.localizedMessage, SimpleTextAttributes.ERROR_ATTRIBUTES) }
@@ -59,14 +72,33 @@ class GitStashChangesBrowser(project: Project) : SimpleChangesBrowser(project, f
     return super.createPopupMenuActions() + ActionManager.getInstance().getAction("Git.Stash.ChangesBrowser.ContextMenu")
   }
 
-  private fun setEmpty(updateEmptyText: (StatusText) -> Unit) = setData(emptyList(), updateEmptyText)
-
-  private fun setData(changes: Collection<Change>) {
-    setData(changes) { statusText -> statusText.text = "" }
+  override fun buildTreeModel(): DefaultTreeModel {
+    val builder = TreeModelBuilder(myProject, grouping)
+    builder.setChanges(stashedChanges, null)
+    for ((tag, changes) in otherChanges) {
+      if (changes.isEmpty()) continue
+      builder.insertChanges(changes, builder.createTagNode(tag, SimpleTextAttributes.REGULAR_ATTRIBUTES, false))
+    }
+    return builder.build()
   }
 
-  private fun setData(changes: Collection<Change>, updateEmptyText: (StatusText) -> Unit) {
-    setChangesToDisplay(changes)
+  private fun setEmpty(updateEmptyText: (StatusText) -> Unit) = setData(emptyList(), emptyList(), updateEmptyText)
+
+  private fun setData(stash: Collection<Change>, parents: Collection<GitCommit>) {
+    setData(stash, parents) { statusText -> statusText.text = "" }
+  }
+
+  private fun setData(stash: Collection<Change>, parents: Collection<GitCommit>, updateEmptyText: (StatusText) -> Unit) {
+    stashedChanges = stash
+    otherChanges = parents.associate { parent ->
+      val tag = MyTag(StringUtil.capitalize(parent.subject.substringBefore(":")), parent.id)
+      Pair(tag, parent.changes.toSet())
+    }
     updateEmptyText(viewer.emptyText)
+    viewer.rebuildTree()
+  }
+
+  private data class MyTag(@Nls private val text: String, private val hash: Hash): ChangesBrowserNode.Tag {
+    override fun toString(): String = text
   }
 }
