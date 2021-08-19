6d736cf8c87eea9a38d8a1df6959475904101c4f
Alexey Kudravtsev <cdr@intellij.com>
2021-08-19 17:42:44 +0200

    Revert exact detection of called methods (IDEA-270674), because it confuses users too much. Besides, that's how our Find Usages works. To fix IDEA-275145 result of call hierarchy is not correct
    (cherry picked from commit 950c50e9f5daae8bbffc648080ee469fa26b5b09)
    
    IDEA-CR-71902
    
    GitOrigin-RevId: 0d7fe3ccb3e3732d68c0c2a67f47ecc06467192d

--- java/java-tests/testSrc/com/intellij/java/ide/hierarchy/JavaCallHierarchyTest.java
+++ java/java-tests/testSrc/com/intellij/java/ide/hierarchy/JavaCallHierarchyTest.java
@@ -122,4 +122,7 @@ public class JavaCallHierarchyTest extends HierarchyViewTestBase {
   public void testCallersOfBaseMethod() throws Exception {
     doJavaCallerTypeHierarchyTest("p.BaseClass", "method", "X.java");
   }
+  public void testCallersOfSubMethod() throws Exception {
+    doJavaCallerTypeHierarchyTest("p.BaseClass", "method", "X.java");
+  }
 }
