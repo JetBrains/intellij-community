32d8cf9553f59a32e3efe71a58feb8cf46a48886
elena.korotkova <elena.korotkova@jetbrains.com>
2021-08-19 12:47:13 +0300

    Fix ActionsTreeTest.testPresentation
    GitOrigin-RevId: a4b7eed93dd19ab3a05a3801785abe2a041aa36f

--- platform/lang-impl/src/com/intellij/ide/navigationToolbar/experimental/SpacerAction.kt
+++ platform/lang-impl/src/com/intellij/ide/navigationToolbar/experimental/SpacerAction.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.navigationToolbar.experimental
 
+import com.intellij.idea.ActionsBundle
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.Presentation
 import com.intellij.openapi.actionSystem.ex.CustomComponentAction
@@ -11,7 +12,13 @@ import javax.swing.BorderFactory
 import javax.swing.JComponent
 import javax.swing.JPanel
 
-open class SpacerAction(val space: Int) : DumbAwareAction(), CustomComponentAction {
+private val BIG_SPACER_INTERNAL = ActionsBundle.message("big.spacer")
+private val SMALL_SPACER_INTERNAL = ActionsBundle.message("small.spacer")
+private val TINY_SPACER_INTERNAL = ActionsBundle.message("tiny.spacer")
+
+@Suppress("HardCodedStringLiteral")
+open class SpacerAction(val id: String, val space: Int) : DumbAwareAction(id), CustomComponentAction {
+
   override fun createCustomComponent(presentation: Presentation, place: String): JComponent {
     return JPanel().apply {
       border = BorderFactory.createEmptyBorder(0, 0, 0, 0)
@@ -25,6 +32,6 @@ open class SpacerAction(val space: Int) : DumbAwareAction(), CustomComponentActi
   }
 }
 
-class TinySpacerAction : SpacerAction(JBUI.scale(4))
-class SmallSpacerAction : SpacerAction(JBUI.scale(6))
-class BigSpacerAction : SpacerAction(JBUI.scale(8))
+class TinySpacerAction : SpacerAction(TINY_SPACER_INTERNAL, JBUI.scale(4))
+class SmallSpacerAction : SpacerAction(SMALL_SPACER_INTERNAL, JBUI.scale(6))
+class BigSpacerAction : SpacerAction(BIG_SPACER_INTERNAL, JBUI.scale(8))
