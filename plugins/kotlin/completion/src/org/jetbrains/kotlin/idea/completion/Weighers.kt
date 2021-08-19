c5f584e95dce6b7aca3ec7da07521dd15c49b1e2
Tianyu Geng <tgeng@google.com>
2021-08-19 16:51:00 -0700

    FIR IDE: move ImportableFqNameClassifier to common
    GitOrigin-RevId: 342f85ee9a327b51d85e5f0bbbee0413849743c0

--- plugins/kotlin/completion/src/org/jetbrains/kotlin/idea/completion/Weighers.kt
+++ plugins/kotlin/completion/src/org/jetbrains/kotlin/idea/completion/Weighers.kt
@@ -12,7 +12,6 @@ import com.intellij.psi.util.proximity.PsiProximityComparator
 import org.jetbrains.kotlin.descriptors.*
 import org.jetbrains.kotlin.idea.completion.smart.*
 import org.jetbrains.kotlin.idea.core.ExpectedInfo
-import org.jetbrains.kotlin.idea.core.ImportableFqNameClassifier
 import org.jetbrains.kotlin.idea.core.completion.DeclarationLookupObject
 import org.jetbrains.kotlin.idea.core.completion.PackageLookupObject
 import org.jetbrains.kotlin.idea.resolve.ResolutionFacade
