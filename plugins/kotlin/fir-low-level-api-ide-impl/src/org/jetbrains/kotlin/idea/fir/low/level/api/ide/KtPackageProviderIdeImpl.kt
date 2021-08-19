790d30bdd0a6d7dca55c607160b8c81d1907817e
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2021-08-19 17:31:43 +0200

    [FIR IDE] Cache package existence index in provider
    GitOrigin-RevId: 0ccbdf53d3a3b58dbe771ff9f51412a4efa1af82

--- plugins/kotlin/fir-low-level-api-ide-impl/src/org/jetbrains/kotlin/idea/fir/low/level/api/ide/KtPackageProviderIdeImpl.kt
+++ plugins/kotlin/fir-low-level-api-ide-impl/src/org/jetbrains/kotlin/idea/fir/low/level/api/ide/KtPackageProviderIdeImpl.kt
@@ -5,19 +5,28 @@ import com.intellij.openapi.project.Project
 import com.intellij.psi.search.GlobalSearchScope
 import org.jetbrains.kotlin.idea.fir.low.level.api.KtPackageProvider
 import org.jetbrains.kotlin.idea.fir.low.level.api.KtPackageProviderFactory
+import org.jetbrains.kotlin.idea.fir.low.level.api.api.createProjectWideOutOfBlockModificationTracker
 import org.jetbrains.kotlin.idea.stubindex.PackageIndexUtil
+import org.jetbrains.kotlin.idea.util.*
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
-import org.jetbrains.kotlin.platform.TargetPlatform
-import org.jetbrains.kotlin.idea.util.module
+import java.util.concurrent.ConcurrentHashMap
 
 internal class KtPackageProviderIdeImpl(
     private val project: Project,
     private val searchScope: GlobalSearchScope,
 ) : KtPackageProvider() {
 
-    override fun isPackageExists(packageFqName: FqName): Boolean =
-        PackageIndexUtil.packageExists(packageFqName, searchScope, project)
+    private val cache by cachedValue(
+        project,
+        project.createProjectWideOutOfBlockModificationTracker()
+    ) { ConcurrentHashMap<FqName, Boolean>() }
+
+    override fun isPackageExists(packageFqName: FqName): Boolean {
+        return cache[packageFqName] ?: PackageIndexUtil.packageExists(packageFqName, searchScope, project).also {
+            cache.putIfAbsent(packageFqName, it)
+        }
+    }
 
     override fun getKotlinSubPackageFqNames(packageFqName: FqName): Set<Name> {
         return PackageIndexUtil
