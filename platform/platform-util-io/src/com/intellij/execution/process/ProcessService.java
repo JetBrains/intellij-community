38df7706258920de71847a9cc9a8a1c51d738071
Alexander Koshevoy <Alexander.Koshevoy@jetbrains.com>
2021-08-20 16:30:50 +0300

    Add @NotNull/@Nullable annotations to ProcessService
    GitOrigin-RevId: f6e7d18972c4653c2976b0bf0f0a7d085c2a23ac

--- platform/platform-util-io/src/com/intellij/execution/process/ProcessService.java
+++ platform/platform-util-io/src/com/intellij/execution/process/ProcessService.java
@@ -3,30 +3,34 @@ package com.intellij.execution.process;
 
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 
 public interface ProcessService {
-  Process startPtyProcess(String[] command,
-                          String directory,
-                          Map<String, String> env,
-                          PtyCommandLineOptions options,
-                          Application app,
+  @NotNull
+  Process startPtyProcess(String @NotNull [] command,
+                          @Nullable String directory,
+                          @NotNull Map<String, String> env,
+                          @NotNull PtyCommandLineOptions options,
+                          @Nullable Application app,
                           boolean redirectErrorStream,
                           boolean windowsAnsiColorEnabled,
                           boolean unixOpenTtyToPreserveOutputAfterTermination);
 
-  boolean sendWinProcessCtrlC(Process process);
+  boolean sendWinProcessCtrlC(@NotNull Process process);
 
   boolean sendWinProcessCtrlC(int pid);
 
-  void killWinProcessRecursively(Process process);
+  void killWinProcessRecursively(@NotNull Process process);
 
-  boolean isWinPty(Process process);
+  boolean isWinPty(@NotNull Process process);
 
-  Integer winPtyChildProcessId(Process process);
+  @Nullable
+  Integer winPtyChildProcessId(@NotNull Process process);
 
-  static ProcessService getInstance() {
+  static @NotNull ProcessService getInstance() {
     return ApplicationManager.getApplication().getService(ProcessService.class);
   }
 
