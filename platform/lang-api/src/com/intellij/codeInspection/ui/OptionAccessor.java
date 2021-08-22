752cba8abc004c207ae061c460731ba64deb2cea
Bas Leijdekkers <basleijdekkers@gmail.com>
2021-08-22 18:54:14 +0200

    annotate
    GitOrigin-RevId: 5c431910a75e43a52a1f3816415cc2434fdedf4a

--- platform/lang-api/src/com/intellij/codeInspection/ui/OptionAccessor.java
+++ platform/lang-api/src/com/intellij/codeInspection/ui/OptionAccessor.java
@@ -1,8 +1,9 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInspection.ui;
 
 import com.intellij.codeInspection.InspectionProfileEntry;
 import com.intellij.util.ReflectionUtil;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Dmitry Batkovich
@@ -16,12 +17,12 @@ public interface OptionAccessor {
   class Default implements OptionAccessor {
     private final InspectionProfileEntry myInspection;
 
-    public Default(final InspectionProfileEntry inspection) {
+    public Default(@NotNull InspectionProfileEntry inspection) {
       myInspection = inspection;
     }
 
     @Override
-    public boolean getOption(final String optionName) {
+    public boolean getOption(String optionName) {
       return ReflectionUtil.getField(myInspection.getClass(), myInspection, boolean.class, optionName);
     }
 
