d5e360eea868b556505ffefdfc07971335300316
adam.malek <adam.malek@jetbrains.com>
2021-08-23 13:35:56 +0100

    ML in SE: Add binary features for time since last usage (IDEA-274480)
    Add "used in last minute/hour/day/month" for time since last usage of an action
    
    GitOrigin-RevId: e9265d2e0c4b7d825a982a6a54ff8fbd4346e360

--- plugins/search-everywhere-ml/src/com/intellij/ide/actions/searcheverywhere/ml/features/SearchEverywhereActionFeaturesProvider.kt
+++ plugins/search-everywhere-ml/src/com/intellij/ide/actions/searcheverywhere/ml/features/SearchEverywhereActionFeaturesProvider.kt
@@ -6,8 +6,10 @@ import com.intellij.ide.util.gotoByName.GotoActionModel
 import com.intellij.internal.statistic.local.ActionsGlobalSummaryManager
 import com.intellij.internal.statistic.local.ActionsLocalSummary
 import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.ToggleAction
-import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.components.service
+import com.intellij.util.Time.*
 
 internal class SearchEverywhereActionFeaturesProvider : SearchEverywhereElementFeaturesProvider() {
   companion object {
@@ -24,6 +26,11 @@ internal class SearchEverywhereActionFeaturesProvider : SearchEverywhereElementF
     private const val WEIGHT_KEY = "weight"
 
     private const val TIME_SINCE_LAST_USAGE_DATA_KEY = "timeSinceLastUsage"
+    private const val WAS_USED_IN_LAST_MINUTE_DATA_KEY = "wasUsedInLastMinute"
+    private const val WAS_USED_IN_LAST_HOUR_DATA_KEY = "wasUsedInLastHour"
+    private const val WAS_USED_IN_LAST_DAY_DATA_KEY = "wasUsedInLastDay"
+    private const val WAS_USED_IN_LAST_MONTH_DATA_KEY = "wasUsedInLastMonth"
+
     private const val LOCAL_USAGE_COUNT_DATA_KEY = "usage"
     private const val GLOBAL_USAGE_COUNT_KEY = "globalUsage"
     private const val USERS_RATIO_DATA_KEY = "usersRatio"
@@ -51,7 +58,7 @@ internal class SearchEverywhereActionFeaturesProvider : SearchEverywhereElementF
                                           matchedValue: GotoActionModel.MatchedValue,
                                           queryLength: Int): Map<String, Any> {
     val wrapper = matchedValue.value as? GotoActionModel.ActionWrapper
-    val data = hashMapOf(
+    val data = hashMapOf<String, Any>(
       TOTAL_SYMBOLS_AMOUNT_DATA_KEY to queryLength,
       IS_ABBREVIATION_DATA_KEY to matchedValue.isAbbreviation,
       IS_ACTION_DATA_KEY to (wrapper != null),
@@ -80,19 +87,34 @@ internal class SearchEverywhereActionFeaturesProvider : SearchEverywhereElementF
     data[IS_ENABLED_KEY] = presentation.isEnabled
     data[WEIGHT_KEY] = presentation.weight
 
-    val actionId = ActionManager.getInstance().getId(action) ?: action.javaClass.name
-    val localSummary = ApplicationManager.getApplication().getService(ActionsLocalSummary::class.java)
-    localSummary?.getActionStatsById(actionId)?.let {
-      data[TIME_SINCE_LAST_USAGE_DATA_KEY] = currentTime - it.lastUsedTimestamp
-      data[LOCAL_USAGE_COUNT_DATA_KEY] = it.usageCount
-    }
+    data.putAll(getLocalUsageStatistics(action, currentTime))
 
-    val globalSummary = ApplicationManager.getApplication().getService(ActionsGlobalSummaryManager::class.java)
-    globalSummary?.getActionStatistics(actionId)?.let {
+    val actionId = ActionManager.getInstance().getId(action) ?: action.javaClass.name
+    val globalSummary = service<ActionsGlobalSummaryManager>()
+    globalSummary.getActionStatistics(actionId)?.let {
       data[GLOBAL_USAGE_COUNT_KEY] = it.usagesCount
       data[USERS_RATIO_DATA_KEY] = roundDouble(it.usersRatio)
       data[USAGES_PER_USER_RATIO_DATA_KEY] = roundDouble(it.usagesPerUserRatio)
     }
     return data
+  }
+
+  private fun getLocalUsageStatistics(action: AnAction,
+                                      currentTime: Long): Map<String, Any> {
+    val actionId = ActionManager.getInstance().getId(action) ?: action.javaClass.name
+    val localSummary = service<ActionsLocalSummary>()
+    val actionSummary = localSummary.getActionStatsById(actionId) ?: return emptyMap()
+
+    val timeSinceLastUsage = currentTime - actionSummary.lastUsedTimestamp
+
+    return hashMapOf(
+      LOCAL_USAGE_COUNT_DATA_KEY to actionSummary.usageCount,
+
+      TIME_SINCE_LAST_USAGE_DATA_KEY to timeSinceLastUsage,
+      WAS_USED_IN_LAST_MINUTE_DATA_KEY to (timeSinceLastUsage <= MINUTE),
+      WAS_USED_IN_LAST_HOUR_DATA_KEY to (timeSinceLastUsage <= HOUR),
+      WAS_USED_IN_LAST_DAY_DATA_KEY to (timeSinceLastUsage <= DAY),
+      WAS_USED_IN_LAST_MONTH_DATA_KEY to (timeSinceLastUsage <= (4 * WEEK.toLong()))
+    )
   }
 }
