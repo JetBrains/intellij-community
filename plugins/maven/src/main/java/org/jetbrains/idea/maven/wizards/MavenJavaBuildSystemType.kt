5bd8ff64f63c5c6b49cff5d9879cd19c949fdaf4
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2021-08-23 17:37:37 +0300

    IDEA-275056 added MVP of NPW for Java Maven projects
    GitOrigin-RevId: 0e89f91f61f1ec91bfcd2e0828f6b2fb34551d39

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/wizards/MavenJavaBuildSystemType.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/wizards/MavenJavaBuildSystemType.kt
@@ -2,12 +2,14 @@
 package org.jetbrains.idea.maven.wizards
 
 import com.intellij.ide.projectWizard.generators.JavaBuildSystemType
+import com.intellij.ide.projectWizard.generators.JavaSettings
 import com.intellij.ide.util.projectWizard.WizardContext
-import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
+import com.intellij.openapi.externalSystem.service.project.manage.ExternalProjectsManagerImpl
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.openapi.util.Key
 import com.intellij.ui.layout.*
+import org.jetbrains.idea.maven.model.MavenId
 
 class MavenJavaBuildSystemType : JavaBuildSystemType<MavenJavaBuildSystemSettings>("Maven") {
   override val settingsKey = MavenJavaBuildSystemSettings.KEY
@@ -15,34 +17,33 @@ class MavenJavaBuildSystemType : JavaBuildSystemType<MavenJavaBuildSystemSetting
   override fun createSettings() = MavenJavaBuildSystemSettings()
 
   override fun setupProject(project: Project, settings: MavenJavaBuildSystemSettings, context: WizardContext) {
-    TODO("Not yet implemented")
+    val languageSettings = JavaSettings.KEY.get(context)
+
+    val builder = InternalMavenModuleBuilder().apply {
+      moduleJdk = languageSettings.sdk
+
+      parentProject = null
+      aggregatorProject = null
+      projectId = MavenId(settings.groupId, settings.artifactId, settings.version)
+      isInheritGroupId = false
+      isInheritVersion = false
+    }
+
+    ExternalProjectsManagerImpl.setupCreatedProject(project)
+    builder.commit(project)
   }
 
   override fun advancedSettings(settings: MavenJavaBuildSystemSettings, context: WizardContext): DialogPanel =
     panel {
       hideableRow(MavenWizardBundle.message("label.project.wizard.new.project.advanced.settings.title")) {
-        row {
-          cell { label(MavenWizardBundle.message("label.project.wizard.new.project.group.id")) }
-          cell {
-            textField(settings::groupId)
-          }
+        row(MavenWizardBundle.message("label.project.wizard.new.project.group.id")) {
+          textField(settings::groupId)
         }
-
-        row {
-          cell { label(MavenWizardBundle.message("label.project.wizard.new.project.artifact.id")) }
-          cell {
-            textFieldWithBrowseButton(settings::artifactId, MavenWizardBundle.message("label.project.wizard.new.project.artifact.id"), null,
-                                      FileChooserDescriptorFactory.createSingleFolderDescriptor())
-          }
+        row(MavenWizardBundle.message("label.project.wizard.new.project.artifact.id")) {
+          textField(settings::artifactId)
         }
-
-        row {
-          cell { label(MavenWizardBundle.message("label.project.wizard.new.project.version")) }
-          cell {
-            textFieldWithBrowseButton(settings::version,
-                                      MavenWizardBundle.message("label.project.wizard.new.project.version"), null,
-                                      FileChooserDescriptorFactory.createSingleFolderDescriptor())
-          }
+        row(MavenWizardBundle.message("label.project.wizard.new.project.version")) {
+          textField(settings::version)
         }
       }
     }
