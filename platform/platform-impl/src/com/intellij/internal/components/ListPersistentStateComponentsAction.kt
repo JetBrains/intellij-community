1801d097102745c9bf68a64d82cc056bfc1cf8db
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2021-08-24 12:36:22 +0300

    Show components with storage in workspace file as disabled
    GitOrigin-RevId: 204cd55a1808cc8be147d6ac6002159541270bc2

--- platform/platform-impl/src/com/intellij/internal/components/ListPersistentStateComponentsAction.kt
+++ platform/platform-impl/src/com/intellij/internal/components/ListPersistentStateComponentsAction.kt
@@ -87,7 +87,9 @@ class ListPersistentStateComponentsAction : AnAction() {
           state.storages.forEach {
             if (!it.deprecated) {
               val storageRoamingType =
-                if (it.value == StoragePathMacros.NON_ROAMABLE_FILE || it.value == StoragePathMacros.CACHE_FILE) "DISABLED"
+                if (it.value == StoragePathMacros.NON_ROAMABLE_FILE ||
+                    it.value == StoragePathMacros.CACHE_FILE ||
+                    it.value == StoragePathMacros.WORKSPACE_FILE) "DISABLED"
                 else it.roamingType.toString()
               if (roamingType == null) {
                 roamingType = storageRoamingType
