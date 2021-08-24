49df19e2e38244b4adf364ec8ce52fec26c76d29
akurasov <alexander.kurasov@jetbrains.com>
2021-08-24 13:06:33 +0300

    [Kotlin-wizard]
    Backport of Compose Web wizard to 212
    
    Backport of Compose MPP wizard fixes to 212
    
    Update compose version to 1.0.0-alpha3
    
    Fixing compose multiplatform template
    
    Fixes https://youtrack.jetbrains.com/issue/KTIJ-19331
    
    Co-authored-by: Igor Demin <igordmn@gmail.com>, Alexander Kurasov
    
    (#cherry picked from commit 30d1c2df759252a3f5eb006bf1066c4e7f40e792)
    
    GitOrigin-RevId: c8e41e7564dbcda023df56e9d8570ecfcc1c5256

--- plugins/kotlin/project-wizard/cli/testData/projectTemplatesBuildFileGeneration/composeDesktopApplication/build.gradle.kts
+++ plugins/kotlin/project-wizard/cli/testData/projectTemplatesBuildFileGeneration/composeDesktopApplication/build.gradle.kts
@@ -3,15 +3,15 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.10"
-    id("org.jetbrains.compose") version "0.4.0"
+    kotlin("jvm") version "1.5.21"
+    id("org.jetbrains.compose") version "1.0.0-alpha3"
 }
 
 group = "me.user"
 version = "1.0"
 
 repositories {
-    jcenter()
+    google()
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
