266d272f665011661b85bbd9c753f4731c9d1a7c
Egor Ushakov <egor.ushakov@jetbrains.com>
2021-08-24 17:25:44 +0300

    [debugger] removed JavaDebuggerSupport
    GitOrigin-RevId: ce60622a5aa210523d06947e3c5b30e868ca0eb6

--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/breakpoints/ui/BreakpointPanelProvider.java
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/breakpoints/ui/BreakpointPanelProvider.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.xdebugger.impl.breakpoints.ui;
 
 import com.intellij.openapi.Disposable;
@@ -13,6 +13,10 @@ import java.util.Collection;
 
 public abstract class BreakpointPanelProvider<B> {
 
+  /**
+   * @deprecated instead register at {@link com.intellij.xdebugger.breakpoints.ui.XBreakpointGroupingRule#EP}
+   */
+  @Deprecated
   public abstract void createBreakpointsGroupingRules(Collection<XBreakpointGroupingRule> rules);
 
   public interface BreakpointsListener {
--- platform/xdebugger-impl/src/com/intellij/xdebugger/impl/breakpoints/ui/BreakpointsDialog.java
+++ platform/xdebugger-impl/src/com/intellij/xdebugger/impl/breakpoints/ui/BreakpointsDialog.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.xdebugger.impl.breakpoints.ui;
 
 import com.intellij.icons.AllIcons;
@@ -348,6 +348,7 @@ public class BreakpointsDialog extends DialogWrapper {
   }
 
   private void collectGroupingRules() {
+    myRulesAvailable.addAll(XBreakpointGroupingRule.EP.getExtensionList());
     for (BreakpointPanelProvider provider : myBreakpointsPanelProviders) {
       provider.createBreakpointsGroupingRules(myRulesAvailable);
     }
