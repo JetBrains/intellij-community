14cba0e7d48c41be87c32d9edddf2170045be5c6
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2021-08-25 16:26:24 +0300

    [Gradle] Add dependency owner information to UnresolvedDependencyIssue for all gradle projects IJ-MR-13390
    GitOrigin-RevId: 68f12addcba1944d1301bdbc5b09d3592e71a9f0

--- plugins/gradle/src/org/jetbrains/plugins/gradle/service/project/CommonGradleProjectResolverExtension.java
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/service/project/CommonGradleProjectResolverExtension.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.gradle.service.project;
 
 import com.intellij.build.events.MessageEvent;
@@ -1007,7 +1007,8 @@ public final class CommonGradleProjectResolverExtension extends AbstractProjectR
     else {
       boolean isOfflineWork = resolverCtx.getSettings() != null && resolverCtx.getSettings().isOfflineWork();
       String message = String.format("Could not resolve %s.", libraryName);
-      BuildIssue buildIssue = new UnresolvedDependencySyncIssue(libraryName, message, resolverCtx.getProjectPath(), isOfflineWork);
+      BuildIssue buildIssue = new UnresolvedDependencySyncIssue(
+        libraryName, message, resolverCtx.getProjectPath(), isOfflineWork, ownerModule.getData().getId());
       resolverCtx.report(MessageEvent.Kind.ERROR, buildIssue);
     }
 
