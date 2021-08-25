062c1051c0bd357d77c83bcdd26240c764203959
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2021-08-25 13:53:29 +0300

    IDEA-274334 Can't revert back "Do not show this dialog in the future" for process run in parallel
    migration
    
    (cherry picked from commit ad4cbd3f7deee296f1a861a7b1e303fd782f3ca5)
    
    IJ-CR-13391
    
    GitOrigin-RevId: 13413ade80c9881fd8617523ec7ffce710095643

--- platform/execution-impl/src/com/intellij/execution/RunManagerConfig.java
+++ platform/execution-impl/src/com/intellij/execution/RunManagerConfig.java
@@ -14,6 +14,7 @@ public final class RunManagerConfig {
   private final PropertiesComponent myPropertiesComponent;
 
   @NonNls private static final String RECENTS_LIMIT = "recentsLimit";
+  @NonNls private static final String RESTART_REQUIRES_CONFIRMATION = "restartRequiresConfirmation";
   @NonNls private static final String DELETION_FROM_POPUP_REQUIRES_CONFIRMATION = "deletionFromPopupRequiresConfirmation";
   @NonNls private static final String STOP_INCOMPATIBLE_REQUIRES_CONFIRMATION = "stopIncompatibleRequiresConfirmation";
 
@@ -32,11 +33,14 @@ public final class RunManagerConfig {
     AdvancedSettings.setInt(RECENTS_LIMIT_KEY, Math.max(MIN_RECENT_LIMIT, recentsLimit));
   }
 
-  public void migrateToRegistry() {
-    String value = myPropertiesComponent.getValue(RECENTS_LIMIT);
-    if (value != null) {
-      setRecentsLimit(Math.max(MIN_RECENT_LIMIT, StringUtil.parseInt(value, DEFAULT_RECENT_LIMIT)));
-      myPropertiesComponent.setValue(RECENTS_LIMIT, null);
+  public void migrateToAdvancedSettings() {
+    if (myPropertiesComponent.isValueSet(RECENTS_LIMIT)) {
+      setRecentsLimit(Math.max(MIN_RECENT_LIMIT, StringUtil.parseInt(myPropertiesComponent.getValue(RECENTS_LIMIT), DEFAULT_RECENT_LIMIT)));
+      myPropertiesComponent.unsetValue(RECENTS_LIMIT);
+    }
+    if (myPropertiesComponent.isValueSet(RESTART_REQUIRES_CONFIRMATION)) {
+      setRestartRequiresConfirmation(myPropertiesComponent.getBoolean(RESTART_REQUIRES_CONFIRMATION));
+      myPropertiesComponent.unsetValue(RESTART_REQUIRES_CONFIRMATION);
     }
   }
 
--- platform/execution-impl/src/com/intellij/execution/impl/RunManagerImpl.kt
+++ platform/execution-impl/src/com/intellij/execution/impl/RunManagerImpl.kt
@@ -747,7 +747,7 @@ open class RunManagerImpl @JvmOverloads constructor(val project: Project, shared
   }
 
   override fun loadState(parentNode: Element) {
-    config.migrateToRegistry()
+    config.migrateToAdvancedSettings()
     val oldSelectedConfigurationId: String?
     val isFirstLoadState = isFirstLoadState.compareAndSet(true, false)
     if (isFirstLoadState) {
