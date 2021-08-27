f35bc8fe40afdda5223a98ac23124d3377a7838b
Anna Kozlova <anna.kozlova@jetbrains.com>
2021-08-27 21:53:44 +0200

    [java] do not prioritize replace constructor with refactoring fixes
    GitOrigin-RevId: ce82d010dcd436da2048feda7c04abdfb0ff6a30

--- java/java-impl/src/com/intellij/refactoring/replaceConstructorWithBuilder/ReplaceConstructorWithBuilderAction.java
+++ java/java-impl/src/com/intellij/refactoring/replaceConstructorWithBuilder/ReplaceConstructorWithBuilderAction.java
@@ -1,21 +1,24 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.refactoring.replaceConstructorWithBuilder;
 
+import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction;
+import com.intellij.icons.AllIcons;
 import com.intellij.java.refactoring.JavaRefactoringBundle;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Iconable;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiMethod;
-import com.intellij.refactoring.BaseRefactoringIntentionAction;
 import com.intellij.refactoring.actions.RefactoringActionContextUtil;
 import com.intellij.util.IncorrectOperationException;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import javax.swing.*;
 import java.util.Objects;
 
-public class ReplaceConstructorWithBuilderAction extends BaseRefactoringIntentionAction {
+public class ReplaceConstructorWithBuilderAction extends PsiElementBaseIntentionAction implements Iconable {
 
   @NotNull
   @Override
@@ -44,7 +47,12 @@ public class ReplaceConstructorWithBuilderAction extends BaseRefactoringIntentio
     PsiClass aClass = constructor.getContainingClass();
     new ReplaceConstructorWithBuilderDialog(project, Objects.requireNonNull(aClass).getConstructors()).show();
   }
-  
+
+  @Override
+  public Icon getIcon(int flags) {
+    return AllIcons.Actions.RefactoringBulb;
+  }
+
   private static boolean isNotEnumClass(@Nullable PsiClass psiClass) {
     return psiClass != null && !psiClass.isEnum();
   }
