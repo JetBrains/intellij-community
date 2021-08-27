f9f3f2a26ca8f6e1abccc0dbfd70938f6a883da4
Igor Slobodskov <igor.slobodskov@jetbrains.com>
2021-08-27 18:02:03 +0300

    [DS-1599] change Datalore lexer to NonIncrementalCellLines
    GitOrigin-RevId: 5195520da1de0b2ce99871a899ea746de4e421c2

--- notebooks/visualization/src/org/jetbrains/plugins/notebooks/editor/NotebookCellLinesLexer.kt
+++ notebooks/visualization/src/org/jetbrains/plugins/notebooks/editor/NotebookCellLinesLexer.kt
@@ -1,7 +1,9 @@
 package org.jetbrains.plugins.notebooks.editor
 
 import com.intellij.lexer.Lexer
+import com.intellij.openapi.editor.Document
 import com.intellij.psi.tree.IElementType
+import kotlin.math.max
 
 interface NotebookCellLinesLexer {
   fun shouldParseWholeFile(): Boolean = false
@@ -13,7 +15,7 @@ interface NotebookCellLinesLexer {
                               tokenToCellType: (IElementType) -> NotebookCellLines.CellType?,
                               chars: CharSequence,
                               ordinalIncrement: Int,
-                              offsetIncrement: Int): Sequence<NotebookCellLines.Marker> = sequence{
+                              offsetIncrement: Int): Sequence<NotebookCellLines.Marker> = sequence {
       val lexer = underlyingLexerFactory()
       lexer.start(chars, 0, chars.length)
       var ordinal = 0
@@ -31,5 +33,35 @@ interface NotebookCellLinesLexer {
         lexer.advance()
       }
     }
+
+    fun defaultIntervals(document: Document, markers: List<NotebookCellLines.Marker>): List<NotebookCellLines.Interval> {
+      val lineMarkers = toLineMarkers(document, markers)
+
+      val result = mutableListOf<NotebookCellLines.Interval>()
+      for (i in 0 until (lineMarkers.size - 1)) {
+        result += NotebookCellLines.Interval(ordinal = i, type = lineMarkers[i].type,
+          lines = lineMarkers[i].startLine until lineMarkers[i + 1].startLine)
+      }
+      return result
+    }
   }
 }
+
+private fun toLineMarkers(document: Document, markers: List<NotebookCellLines.Marker>): List<LineMarker> {
+  val m = mutableListOf<LineMarker>()
+
+  // add first if necessary
+  if (markers.isEmpty() || document.getLineNumber(markers.first().offset) != 0) {
+    m += LineMarker(0, NotebookCellLines.CellType.RAW)
+  }
+
+  for (marker in markers) {
+    m += LineMarker(document.getLineNumber(marker.offset), marker.type)
+  }
+
+  // marker for the end
+  m += LineMarker(max(document.lineCount, 1), NotebookCellLines.CellType.RAW)
+  return m
+}
+
+private data class LineMarker(val startLine: Int, val type: NotebookCellLines.CellType)
