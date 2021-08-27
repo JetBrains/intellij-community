7db35c2b59000c50004d42f13c818292e9cf8187
Tianyu Geng <tgeng@google.com>
2021-08-26 17:25:01 -0700

    FIR IDE: populate completion entries with substituted types
    GitOrigin-RevId: 99495ff7bf2ed4915fae1ce5d8603d334d943964

--- plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/completion/lookups/CompletionShortNamesRenderer.kt
+++ plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/completion/lookups/CompletionShortNamesRenderer.kt
@@ -8,25 +8,28 @@ import org.jetbrains.kotlin.analysis.api.symbols.KtCallableSymbol
 import org.jetbrains.kotlin.analysis.api.symbols.KtFunctionSymbol
 import org.jetbrains.kotlin.analysis.api.symbols.KtValueParameterSymbol
 import org.jetbrains.kotlin.analysis.api.symbols.KtVariableLikeSymbol
+import org.jetbrains.kotlin.analysis.api.types.KtSubstitutor
 
 internal object CompletionShortNamesRenderer {
-    fun KtAnalysisSession.renderFunctionParameters(function: KtFunctionSymbol): String {
-        val receiver = renderReceiver(function)
-        val parameters = function.valueParameters.joinToString(", ", "(", ")") { renderFunctionParameter(it) }
+    fun KtAnalysisSession.renderFunctionParameters(function: KtFunctionSymbol, substitutor: KtSubstitutor): String {
+        val receiver = renderReceiver(function, substitutor)
+        val parameters = function.valueParameters.joinToString(", ", "(", ")") { renderFunctionParameter(it, substitutor) }
         return receiver + parameters
     }
 
-    fun KtAnalysisSession.renderVariable(function: KtVariableLikeSymbol): String {
-        return renderReceiver(function)
+    fun KtAnalysisSession.renderVariable(function: KtVariableLikeSymbol, substitutor: KtSubstitutor): String {
+        return renderReceiver(function, substitutor)
     }
-    
-    private fun KtAnalysisSession.renderReceiver(symbol: KtCallableSymbol): String {
-        val receiverType = symbol.receiverType?.type ?: return ""
+
+    private fun KtAnalysisSession.renderReceiver(symbol: KtCallableSymbol, substitutor: KtSubstitutor): String {
+        val receiverType = symbol.receiverType?.type?.let { substitutor.substituteOrSelf(it) } ?: return ""
         return receiverType.render(TYPE_RENDERING_OPTIONS) + "."
     }
 
-    private fun KtAnalysisSession.renderFunctionParameter(param: KtValueParameterSymbol): String =
-        "${if (param.isVararg) "vararg " else ""}${param.name.asString()}: ${param.annotatedType.type.render(TYPE_RENDERING_OPTIONS)}"
+    private fun KtAnalysisSession.renderFunctionParameter(param: KtValueParameterSymbol, substitutor: KtSubstitutor): String =
+        "${if (param.isVararg) "vararg " else ""}${param.name.asString()}: ${
+            substitutor.substituteOrSelf(param.annotatedType.type).render(TYPE_RENDERING_OPTIONS)
+        }"
 
     val TYPE_RENDERING_OPTIONS = KtTypeRendererOptions.SHORT_NAMES
 }
