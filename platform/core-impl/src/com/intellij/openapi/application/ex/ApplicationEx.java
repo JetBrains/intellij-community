73cbb7610f62a839b21c795731f11403bc5a2922
Alexey Kudravtsev <cdr@intellij.com>
2021-08-27 16:58:43 +0200

    IDEA-276965 Ability to cancel the wait for the Write Action lock
    GitOrigin-RevId: ad1b30dedc6dd247ae468706715c021ea6bab62f

--- platform/core-impl/src/com/intellij/openapi/application/ex/ApplicationEx.java
+++ platform/core-impl/src/com/intellij/openapi/application/ex/ApplicationEx.java
@@ -139,11 +139,19 @@ public interface ApplicationEx extends Application {
   void assertTimeConsuming();
 
   /**
-   * Tries to acquire the read lock and run the {@code action}
+   * Tries to acquire the read lock and run the {@code action}.
    *
    * @return true if action was run while holding the lock, false if was unable to get the lock and action was not run
    */
   boolean tryRunReadAction(@NotNull Runnable action);
+
+  /**
+   * Tries to acquire the write lock and run the {@code action}
+   * Must be called from {@link #isWriteThread()} only.
+   *
+   * @return true if action was run while holding the lock, false if was unable to get the lock and action was not run
+   */
+  boolean tryRunWriteAction(@NotNull Runnable action);
 
   /** DO NOT USE */
   @ApiStatus.Internal
