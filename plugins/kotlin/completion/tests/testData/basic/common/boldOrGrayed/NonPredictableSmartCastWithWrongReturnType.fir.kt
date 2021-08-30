6c2377435cece0a2db08ba28c7658e65c370e618
Tianyu Geng <tgeng@google.com>
2021-08-30 14:00:18 -0700

    FIR IDE: fix test since FIR does not suffer KT-47225

---
+++ plugins/kotlin/completion/tests/testData/basic/common/boldOrGrayed/NonPredictableSmartCastWithWrongReturnType.fir.kt
@@ -0,0 +1,17 @@
+// FIR_COMPARISON
+// COMPILER_ARGUMENTS: -XXLanguage:-NewInference
+
+interface A {
+    fun foo(): Any
+    fun bar()
+}
+interface B: A {
+    override fun foo(): String
+}
+
+fun f(pair: Pair<out A, out Any>) {
+    if (pair.first !is B) return
+    pair.first.<caret>
+}
+// EXIST: { lookupString: "foo", "typeText":"Any", attributes: "bold" }
+// EXIST: { lookupString: "bar", attributes: "bold" }
