ef03094cabfb18f205da961c6777de41e6dd1a2d
Tianyu Geng <tgeng@google.com>
2021-08-30 14:00:18 -0700

    FIR IDE: fix test since FIR does not suffer KT-47225
    GitOrigin-RevId: dcfa75b07d22445c57bf8b26bcb947215d7778b3

---
+++ plugins/kotlin/completion/tests/testData/basic/common/boldOrGrayed/NonPredictableSmartCast.fir.kt
@@ -0,0 +1,17 @@
+// FIR_COMPARISON
+// COMPILER_ARGUMENTS: -XXLanguage:-NewInference
+
+fun String.forString(){}
+fun Any.forAny(){}
+
+fun <T> T.forT() {}
+
+fun f(pair: Pair<out Any, out Any>) {
+    if (pair.first !is String) return
+    pair.first.<caret>
+}
+
+// EXIST: { lookupString: "hashCode", attributes: "bold" }
+// EXIST: { lookupString: "forAny", attributes: "bold" }
+
+// EXIST: { lookupString: "forT", attributes: "" }
