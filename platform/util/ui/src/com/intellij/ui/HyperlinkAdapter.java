3108b78f3a4c86a54c9c631999e180c235c202df
Pavel Porvatov <Pavel.Porvatov@jetbrains.com>
2021-08-31 20:57:12 +0300

    IDEA-277188 Port AppEditorFontOptionsPanel to new UI DSL framework
    GitOrigin-RevId: 9b1c5ec9b303786d91b7ec5dd696769a8416aaa7

--- platform/util/ui/src/com/intellij/ui/HyperlinkAdapter.java
+++ platform/util/ui/src/com/intellij/ui/HyperlinkAdapter.java
@@ -1,8 +1,6 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ui;
 
-import org.jetbrains.annotations.NotNull;
-
 import javax.swing.event.HyperlinkEvent;
 import javax.swing.event.HyperlinkListener;
 
@@ -14,5 +12,5 @@ public abstract class HyperlinkAdapter implements HyperlinkListener {
     }
   }
 
-  protected abstract void hyperlinkActivated(@NotNull HyperlinkEvent e);
+  protected abstract void hyperlinkActivated(HyperlinkEvent e);
 }
