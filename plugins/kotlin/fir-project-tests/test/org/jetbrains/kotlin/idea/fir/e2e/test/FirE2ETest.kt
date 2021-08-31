a3edc04e4e8b7a54177e48a93b7c6640843465ba
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-08-31 13:18:35 +0300

    FIR IDE: implement benchmarks
    GitOrigin-RevId: f850fdd7b655fe907101032c3fffebeab36ebe43

--- plugins/kotlin/fir-project-tests/test/org/jetbrains/kotlin/idea/fir/e2e/test/FirE2ETest.kt
+++ plugins/kotlin/fir-project-tests/test/org/jetbrains/kotlin/idea/fir/e2e/test/FirE2ETest.kt
@@ -1,71 +1,30 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.kotlin.idea.fir.e2e.test
 
+import com.intellij.util.io.isDirectory
 import org.jetbrains.kotlin.idea.fir.project.test.base.AbstractFirProjectBasedTests
+import org.jetbrains.kotlin.idea.fir.project.test.base.RustProject
 import org.jetbrains.kotlin.idea.perf.common.HighlightFile
 import org.jetbrains.kotlin.idea.perf.common.ProjectAction
 import org.jetbrains.kotlin.idea.perf.common.ProjectData
 import org.jetbrains.kotlin.idea.perf.common.TypeAndAutocompleteInFile
 import org.jetbrains.kotlin.idea.perf.live.PerformanceTestProfile
 import org.jetbrains.kotlin.idea.testFramework.ProjectOpenAction
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.stream.Collectors
+
 
 class FirE2ETest : AbstractFirProjectBasedTests() {
     override val testPrefix: String = "FIR e2e"
     override val warmUpOnHelloWorldProject: Boolean = false
 
     fun testRustPlugin() {
-        /*
-        The fork of intellij-rust plugin is considered here https://github.com/darthorimar/intellij-rust
-        It has required code pregenerated manually
-        Also, it is always the same version of rust plugin codebase to test against
-         */
-        val project = ProjectData(
-            name = "intellijRustPlugin",
-            path = "../intellij-rust",
-            openAction = ProjectOpenAction.GRADLE_PROJECT,
-        )
-
         val profile = PerformanceTestProfile(
             warmUpIterations = 0,
             iterations = 1,
         )
 
-        val basePath = "src/main/kotlin/org/rust"
-
-        val actions: List<ProjectAction> = listOf(
-            HighlightFile("$basePath/ide/inspections/RsExternalLinterInspection.kt"),
-            HighlightFile("$basePath/ide/injected/RsDoctestLanguageInjector.kt"),
-            HighlightFile("$basePath/cargo/runconfig/filters/RegexpFileLinkFilter.kt"),
-            HighlightFile("$basePath/cargo/util/CargoOptions.kt"),
-            HighlightFile("$basePath/lang/core/macros/MacroExpansionManager.kt"),
-            HighlightFile("$basePath/lang/core/resolve/NameResolution.kt"),
-
-            TypeAndAutocompleteInFile(
-                filePath = "$basePath/cargo/runconfig/filters/RegexpFileLinkFilter.kt",
-                typeAfter = "fun applyFilter(line: String, entireLength: Int): Filter.Result? {",
-                textToType = "val a = l",
-                expectedLookupElements = listOf("line"),
-                note = "in-method completion",
-            ),
-
-             // TODO fix, fails in UAST type mapping
-             /* TypeAndAutocompleteInFile(
-                  filePath =  "$basePath/lang/core/resolve/NameResolution.kt",
-                  typeAfter = "private data class ImplicitStdlibCrate(val name: String, val crateRoot: RsFile)",
-                  textToType = "\nval a = ",
-                  expectedLookupElements = listOf("processAssocTypeVariants"),
-                  note = "top-level completion",
-              ),
-
-              TypeAndAutocompleteInFile(
-                  filePath =  "$basePath/lang/core/resolve/NameResolution.kt",
-                  typeAfter = "testAssert { cameFrom.context == scope }",
-                  textToType = "\nval a = s",
-                  expectedLookupElements = listOf("scope"),
-                  note = "in big method in big file completion",
-              ),*/
-        )
-
-        test("Rust Plugin", project, actions, profile)
+        test("Rust Plugin", RustProject.project, RustProject.actions, profile)
     }
 }
