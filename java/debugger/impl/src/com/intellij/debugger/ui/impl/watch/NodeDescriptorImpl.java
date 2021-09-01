d9c8947360e7f3bfbc41e8702954d8705e35a914
Aleksey Tomin <aleksey.tomin@jetbrains.com>
2021-09-01 19:02:49 +0400

    IDEA-277284 Fix NPE on error logging
    GitOrigin-RevId: 0e19028528ac68cda552a19c2e4e194086cd91a7

--- java/debugger/impl/src/com/intellij/debugger/ui/impl/watch/NodeDescriptorImpl.java
+++ java/debugger/impl/src/com/intellij/debugger/ui/impl/watch/NodeDescriptorImpl.java
@@ -84,7 +84,7 @@ public abstract class NodeDescriptorImpl implements NodeDescriptor {
         if (e.getCause() instanceof InterruptedException) {
           throw e;
         }
-        if (context.getDebugProcess().getVirtualMachineProxy().canBeModified()) { // do not care in read only vms
+        if (context != null && context.getDebugProcess().getVirtualMachineProxy().canBeModified()) { // do not care in read only vms
           LOG.debug(e);
         }
         else {
