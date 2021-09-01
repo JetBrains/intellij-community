ee2e91beb4c14c6bc95a366907068994b736770c
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2021-09-01 15:28:00 +0300

    [kotlin] Adjust testdata after changes in expect/actual matching
    Consequence of
    https://github.com/JetBrains/kotlin/commit/748a2d2e7ca952f064403e66512acdc3b16ac07c
    
    GitOrigin-RevId: 59f1d0b4e8d8dcced64328887df355e8b7b9f719

--- plugins/kotlin/idea/tests/testData/multiplatform/duplicateActualsImplicit/middle/middle.kt
+++ plugins/kotlin/idea/tests/testData/multiplatform/duplicateActualsImplicit/middle/middle.kt
@@ -1,6 +1,6 @@
 package foo
 
 actual class <!LINE_MARKER("descr='Has declaration in common module'")!>ActualInMiddleCompatibleInBottom<!>
-class <!ACTUAL_MISSING!>CompatibleInMiddleActualInBottom<!>
+class CompatibleInMiddleActualInBottom
 
-class <!ACTUAL_MISSING!>CompatibleInMiddleAndBottom<!>
+class CompatibleInMiddleAndBottom
