4dc58eb850cd2721ffe896d87c5c53e53ace6c4b
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2021-09-01 15:20:21 +0300

    [kotlin] Adjust testdata after several changes in rendering of linemarkers
    1. Now text of line markers contains "Click or press ... to navigate"
    Most of places fixed in
    https://jetbrains.team/p/ij/repositories/intellij/revision/7ca52de274750b60e8ea2e1c00eca230c0517c0d
    
    2. Now line markers are sorted by their 'asString' to make their order
    stable when they start at the same offset. The commit introduced it in
    https://jetbrains.team/p/ij/repositories/intellij/revision/a0de815b8d5652bafe2ccb48c1af2ad75f0ddd30
    
    GitOrigin-RevId: d8be6ea41e3b0638859a90141face5134587281e

--- plugins/kotlin/idea/tests/testData/multiplatform/sealedInheritorsInComplexModuleStructure2/common/common.kt
+++ plugins/kotlin/idea/tests/testData/multiplatform/sealedInheritorsInComplexModuleStructure2/common/common.kt
@@ -10,7 +10,7 @@ package foo
  *   - Derived3 [main]
  */
 expect sealed class <!LINE_MARKER("descr='Has subclasses'")!>Sealed1<!>()
-expect sealed class <!LINE_MARKER("descr='Is subclassed by Derived11 Derived12 Derived13Error'"), LINE_MARKER("descr='Has actuals in JVM'")!>Sealed2<!>() : Sealed1
+expect sealed class <!LINE_MARKER("descr='Has actuals in JVM'"), LINE_MARKER("descr='Is subclassed by Derived11 Derived12 Derived13Error  Click or press ... to navigate'")!>Sealed2<!>() : Sealed1
 
 class Derived1 : Sealed1()
 class Derived11 : Sealed2()
