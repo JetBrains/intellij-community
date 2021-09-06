93a36f6e6447f554fe177fe009a5f16740921406
Yann Cébron <yann.cebron@jetbrains.com>
2021-09-06 16:43:38 +0200

    IDEA-276530 CompositePsiElement does not implement NavigatablePsiElement
    GitOrigin-RevId: b337f2b668036c80d73adab6a2009333a4707db1

--- platform/core-impl/src/com/intellij/psi/impl/source/tree/CompositePsiElement.java
+++ platform/core-impl/src/com/intellij/psi/impl/source/tree/CompositePsiElement.java
@@ -1,25 +1,10 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.impl.source.tree;
 
 import com.intellij.ide.util.PsiNavigationSupport;
 import com.intellij.lang.ASTNode;
 import com.intellij.lang.Language;
 import com.intellij.navigation.ItemPresentation;
-import com.intellij.navigation.NavigationItem;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectCoreUtil;
@@ -38,7 +23,7 @@ import com.intellij.psi.tree.TokenSet;
 import com.intellij.util.IncorrectOperationException;
 import org.jetbrains.annotations.NotNull;
 
-public abstract class CompositePsiElement extends CompositeElement implements PsiElement, NavigationItem {
+public abstract class CompositePsiElement extends CompositeElement implements NavigatablePsiElement {
   private static final Logger LOG = Logger.getInstance(CompositePsiElement.class);
 
   protected static int ourHC;
