99799ef51516b4e8c145527adef23ad17115841f
Bas Leijdekkers <basleijdekkers@gmail.com>
2021-09-06 16:34:31 +0200

    IG: "Package with classes in multiple modules" inspection without reference graph
    GitOrigin-RevId: bf2407c0225f842e7a9ce2c0284aa518c8e0b6b7

--- plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/packaging/PackageInMultipleModulesInspection.java
+++ plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/packaging/PackageInMultipleModulesInspection.java
@@ -19,10 +19,15 @@ import com.intellij.analysis.AnalysisScope;
 import com.intellij.codeInspection.CommonProblemDescriptor;
 import com.intellij.codeInspection.GlobalInspectionContext;
 import com.intellij.codeInspection.InspectionManager;
-import com.intellij.codeInspection.reference.RefClass;
-import com.intellij.codeInspection.reference.RefEntity;
-import com.intellij.codeInspection.reference.RefModule;
+import com.intellij.codeInspection.ProblemDescriptionsProcessor;
+import com.intellij.codeInspection.reference.RefJavaManager;
 import com.intellij.codeInspection.reference.RefPackage;
+import com.intellij.openapi.application.ReadAction;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ProjectFileIndex;
+import com.intellij.psi.*;
+import com.intellij.psi.search.GlobalSearchScope;
 import com.siyeh.InspectionGadgetsBundle;
 import com.siyeh.ig.BaseGlobalInspection;
 import org.jetbrains.annotations.NotNull;
@@ -36,41 +41,67 @@ import java.util.Set;
 public class PackageInMultipleModulesInspection extends BaseGlobalInspection {
 
   @Override
-  public CommonProblemDescriptor @Nullable [] checkElement(
-    @NotNull RefEntity refEntity, @NotNull AnalysisScope analysisScope,
-    @NotNull InspectionManager inspectionManager,
-    @NotNull GlobalInspectionContext globalInspectionContext) {
-    if (!(refEntity instanceof RefPackage)) {
-      return null;
-    }
-    final List<RefEntity> children = refEntity.getChildren();
-    final Set<RefModule> modules = new HashSet<>();
-    for (RefEntity child : children) {
-      if (!(child instanceof RefClass)) {
-        continue;
+  public boolean isGraphNeeded() {
+    return false;
+  }
+
+  @Override
+  public void runInspection(@NotNull AnalysisScope scope,
+                            @NotNull InspectionManager manager,
+                            @NotNull GlobalInspectionContext globalContext,
+                            @NotNull ProblemDescriptionsProcessor problemDescriptionsProcessor) {
+    final Set<String> packages = new HashSet<>();
+    scope.accept(file -> {
+      if (file.isDirectory() || !scope.contains(file)) return true;
+      final String packageName = ReadAction.compute(() -> {
+        final PsiFile element = PsiManager.getInstance(scope.getProject()).findFile(file);
+        if (!(element instanceof PsiClassOwner)) return null;
+        final PsiClassOwner classOwner = (PsiClassOwner)element;
+        return classOwner.getPackageName();
+      });
+      if (packageName == null || !packages.add(packageName)) return true;
+      final RefPackage aPackage = globalContext.getRefManager().getExtension(RefJavaManager.MANAGER).getPackage(packageName);
+      if (aPackage == null) return true;
+      CommonProblemDescriptor[] descriptors = checkPackage(aPackage, scope, manager, globalContext);
+      if (descriptors != null) {
+        problemDescriptionsProcessor.addProblemElement(aPackage, descriptors);
       }
-      final RefClass refClass = (RefClass)child;
-      final RefModule module = refClass.getModule();
+      return true;
+    });
+  }
+
+  public CommonProblemDescriptor @Nullable [] checkPackage(@NotNull RefPackage aPackage,
+                                                           @NotNull AnalysisScope analysisScope,
+                                                           @NotNull InspectionManager inspectionManager,
+                                                           @NotNull GlobalInspectionContext globalInspectionContext) {
+    final Project project = inspectionManager.getProject();
+    final PsiPackage psiPackage = JavaPsiFacade.getInstance(project).findPackage(aPackage.getQualifiedName());
+    if (psiPackage == null)  return null;
+    final PsiFile @NotNull [] files = ReadAction.compute(() -> psiPackage.getFiles(GlobalSearchScope.projectScope(project)));
+    final Set<Module> modules = new HashSet<>();
+    final ProjectFileIndex index = ProjectFileIndex.getInstance(project);
+    for (PsiFile file : files) {
+      final Module module = index.getModuleForFile(file.getVirtualFile());
       modules.add(module);
     }
     final int moduleCount = modules.size();
     if (moduleCount <= 1) {
       return null;
     }
-    final List<RefModule> moduleList = new ArrayList<>(modules);
+    final List<Module> moduleList = new ArrayList<>(modules);
     final String errorString;
     if (moduleCount == 2) {
       errorString = InspectionGadgetsBundle.message(
-        "package.in.multiple.modules.problem.descriptor2", refEntity.getQualifiedName(), moduleList.get(0), moduleList.get(1));
+        "package.in.multiple.modules.problem.descriptor2", aPackage.getQualifiedName(), moduleList.get(0), moduleList.get(1));
     }
     else if (moduleCount == 3) {
       errorString = InspectionGadgetsBundle.message(
-        "package.in.multiple.modules.problem.descriptor3", refEntity.getQualifiedName(),
+        "package.in.multiple.modules.problem.descriptor3", aPackage.getQualifiedName(),
         moduleList.get(0), moduleList.get(1), moduleList.get(2));
     }
     else {
       errorString = InspectionGadgetsBundle.message(
-        "package.in.multiple.modules.problem.descriptor.many", refEntity.getQualifiedName(),
+        "package.in.multiple.modules.problem.descriptor.many", aPackage.getQualifiedName(),
         moduleList.get(0), moduleList.get(1), moduleCount - 2);
     }
 
