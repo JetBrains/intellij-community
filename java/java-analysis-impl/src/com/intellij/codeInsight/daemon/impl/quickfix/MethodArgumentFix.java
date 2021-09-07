44f751215e4927a8bad1615b098ab9ac5d798ca5
Bas Leijdekkers <basleijdekkers@gmail.com>
2021-09-07 17:45:07 +0200

    java: cast quick-fix cosmetics (IDEA-277682)
    GitOrigin-RevId: 2e9ca6999d9003e1d0d7392de6713593722238f2

--- java/java-analysis-impl/src/com/intellij/codeInsight/daemon/impl/quickfix/MethodArgumentFix.java
+++ java/java-analysis-impl/src/com/intellij/codeInsight/daemon/impl/quickfix/MethodArgumentFix.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.daemon.impl.quickfix;
 
 import com.intellij.codeInsight.daemon.QuickFixBundle;
@@ -21,6 +7,7 @@ import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
+import com.intellij.psi.codeStyle.JavaCodeStyleManager;
 import com.intellij.psi.util.PsiTypesUtil;
 import org.jetbrains.annotations.NotNull;
 
@@ -64,7 +51,8 @@ public abstract class MethodArgumentFix implements IntentionAction {
     LOG.assertTrue(expression != null && expression.isValid());
     PsiExpression modified = myArgumentFixerActionFactory.getModifiedArgument(expression, myToType);
     LOG.assertTrue(modified != null, myArgumentFixerActionFactory);
-    expression.replace(modified);
+    PsiElement newElement = expression.replace(modified);
+    JavaCodeStyleManager.getInstance(project).shortenClassReferences(newElement);
   }
 
   @Override
