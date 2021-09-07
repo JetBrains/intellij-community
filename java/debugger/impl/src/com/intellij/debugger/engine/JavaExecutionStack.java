749e8d29c36337eb03a0cd7960ae5c92d63b0452
Nikita Nazarov <nikita.nazarov@jetbrains.com>
2021-09-07 14:00:01 +0300

    [debugger] Deprecate com.intellij.debugger.engine.JavaExecutionStack.createStackFrame
    (cherry picked from commit afaac45f674c3baef8f9e09214d172bdce3a340b)
    
    IJ-MR-13900
    
    GitOrigin-RevId: e10a87dd74ce1ca16ebfac588fb1f2b8051a2908

--- java/debugger/impl/src/com/intellij/debugger/engine/JavaExecutionStack.java
+++ java/debugger/impl/src/com/intellij/debugger/engine/JavaExecutionStack.java
@@ -141,6 +141,15 @@ public class JavaExecutionStack extends XExecutionStack {
     }
   }
 
+  /**
+   * @deprecated Use {@link #createStackFrames(StackFrameProxyImpl)} instead.
+   */
+  @NotNull
+  @Deprecated(forRemoval = true)
+  public XStackFrame createStackFrame(@NotNull StackFrameProxyImpl stackFrameProxy) {
+    return createStackFrames(stackFrameProxy).get(0);
+  }
+
   @NotNull
   public List<XStackFrame> createStackFrames(@NotNull StackFrameProxyImpl stackFrameProxy) {
     StackFrameDescriptorImpl descriptor = new StackFrameDescriptorImpl(stackFrameProxy, myTracker);
