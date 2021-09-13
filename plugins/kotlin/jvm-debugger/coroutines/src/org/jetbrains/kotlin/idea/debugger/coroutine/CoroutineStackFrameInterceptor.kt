0e40380e423faf4b6b5517873da452dcd271546e
Nikita Nazarov <nikita.nazarov@jetbrains.com>
2021-09-13 18:30:16 +0300

    [kotlin] Automatically show the coroutine panel when a stack trace contains coroutine stack frames
    (cherry picked from commit 0209b2e56f430358b5cc5065be9fa89ad4a232b0)
    
    IJ-MR-14123
    
    GitOrigin-RevId: 8177a0d67fdedc77ce9aa2854efc9f5999b24b8e

--- plugins/kotlin/jvm-debugger/coroutines/src/org/jetbrains/kotlin/idea/debugger/coroutine/CoroutineStackFrameInterceptor.kt
+++ plugins/kotlin/jvm-debugger/coroutines/src/org/jetbrains/kotlin/idea/debugger/coroutine/CoroutineStackFrameInterceptor.kt
@@ -5,6 +5,9 @@ package org.jetbrains.kotlin.idea.debugger.coroutine
 import com.intellij.debugger.actions.AsyncStacksToggleAction
 import com.intellij.debugger.engine.DebugProcessImpl
 import com.intellij.debugger.jdi.StackFrameProxyImpl
+import com.intellij.execution.ui.layout.impl.RunnerContentUi
+import com.intellij.execution.ui.layout.impl.RunnerLayoutUiImpl
+import com.intellij.openapi.application.runInEdt
 import com.intellij.openapi.project.Project
 import com.intellij.xdebugger.frame.XStackFrame
 import com.intellij.xdebugger.impl.XDebugSessionImpl
@@ -13,21 +16,35 @@ import org.jetbrains.kotlin.idea.debugger.StackFrameInterceptor
 import org.jetbrains.kotlin.idea.debugger.coroutine.proxy.SkipCoroutineStackFrameProxyImpl
 import org.jetbrains.kotlin.idea.debugger.coroutine.util.CoroutineFrameBuilder
 import org.jetbrains.kotlin.idea.util.application.isUnitTestMode
+import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 
 class CoroutineStackFrameInterceptor(val project: Project) : StackFrameInterceptor {
     override fun createStackFrame(frame: StackFrameProxyImpl, debugProcess: DebugProcessImpl, location: Location): XStackFrame? {
-        return if (debugProcess.xdebugProcess?.session is XDebugSessionImpl
+        if (debugProcess.xdebugProcess?.session is XDebugSessionImpl
             && frame !is SkipCoroutineStackFrameProxyImpl
-            && AsyncStacksToggleAction.isAsyncStacksEnabled(debugProcess.xdebugProcess?.session as XDebugSessionImpl)
-        ) {
+            && AsyncStacksToggleAction.isAsyncStacksEnabled(debugProcess.xdebugProcess?.session as XDebugSessionImpl)) {
             val suspendContextImpl = when {
                 isUnitTestMode() -> debugProcess.suspendManager.pausedContext
                 else -> debugProcess.debuggerContext.suspendContext
             }
-            suspendContextImpl?.let {
+            val stackFrame = suspendContextImpl?.let {
                 CoroutineFrameBuilder.coroutineExitFrame(frame, it)
             }
-        } else
-            null
+
+            if (stackFrame != null) {
+                showCoroutinePanel(debugProcess)
+            }
+
+            return stackFrame
+        }
+        return null
+    }
+
+    private fun showCoroutinePanel(debugProcess: DebugProcessImpl) {
+        val ui = debugProcess.session.xDebugSession?.ui.safeAs<RunnerLayoutUiImpl>() ?: return
+        val runnerContentUi = RunnerContentUi.KEY.getData(ui) ?: return
+        runInEdt {
+            runnerContentUi.findOrRestoreContentIfNeeded(CoroutineDebuggerContentInfo.XCOROUTINE_THREADS_CONTENT)
+        }
     }
 }
