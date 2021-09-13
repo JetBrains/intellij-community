f693b9a5477d5f29c7395560f3b0a91ba7885c60
Louis Vignier <louis.vignier@jetbrains.com>
2021-09-13 12:12:01 +0200

    [java] Use new UI DSL in the project structure dialog
    GitOrigin-RevId: d4598b596b49879f888df4b69c9045b364a2016e

--- java/idea-ui/src/com/intellij/openapi/roots/ui/configuration/ProjectConfigurableUi.kt
+++ java/idea-ui/src/com/intellij/openapi/roots/ui/configuration/ProjectConfigurableUi.kt
@@ -3,29 +3,39 @@ package com.intellij.openapi.roots.ui.configuration
 
 import com.intellij.core.JavaPsiBundle
 import com.intellij.ide.JavaUiBundle
+import com.intellij.ide.wizard.getPresentablePath
+import com.intellij.openapi.observable.properties.GraphPropertyImpl.Companion.graphProperty
+import com.intellij.openapi.observable.properties.PropertyGraph
+import com.intellij.openapi.observable.properties.map
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.projectRoots.JavaSdk
 import com.intellij.openapi.roots.impl.LanguageLevelProjectExtensionImpl
 import com.intellij.openapi.roots.ui.configuration.projectRoot.ProjectSdksModel
-import com.intellij.openapi.ui.TextFieldWithBrowseButton
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.pom.java.LanguageLevel
 import com.intellij.project.isDirectoryBased
-import com.intellij.ui.DocumentAdapter
-import com.intellij.ui.layout.*
+import com.intellij.ui.dsl.builder.BottomGap
+import com.intellij.ui.dsl.builder.bindText
+import com.intellij.ui.dsl.builder.columns
+import com.intellij.ui.dsl.builder.panel
+import com.intellij.ui.dsl.gridLayout.HorizontalAlign
 import com.intellij.util.ui.JBUI
 import javax.swing.JPanel
-import javax.swing.JTextField
-import javax.swing.event.DocumentEvent
 
 internal class ProjectConfigurableUi(private val myProjectConfigurable: ProjectConfigurable,
                                      private val myProject: Project) {
 
-  private val myProjectName: JTextField = JTextField(28)
+  private val propertyGraph = PropertyGraph()
+
+  private val nameProperty = propertyGraph.graphProperty { myProject.name }
+  private val compilerOutputProperty = propertyGraph.graphProperty { "" }.map(::getPresentablePath)
+
+  var projectName by nameProperty
+  var projectCompilerOutput by compilerOutputProperty
+
   private lateinit var myProjectJdkConfigurable: ProjectJdkConfigurable
   private lateinit var myLanguageLevelCombo: LanguageLevelCombo
-  private lateinit var myProjectCompilerOutput: TextFieldWithBrowseButton
   private lateinit var myPanel: JPanel
 
   fun initComponents(modulesConfigurator: ModulesConfigurator, model: ProjectSdksModel) {
@@ -37,14 +47,6 @@ internal class ProjectConfigurableUi(private val myProjectConfigurable: ProjectC
         val version = JavaSdk.getInstance().getVersion(sdk)
         return version?.maxLanguageLevel
       }
-    }
-    myProjectCompilerOutput = TextFieldWithBrowseButton().apply {
-      textField.document.addDocumentListener(object : DocumentAdapter() {
-        override fun textChanged(e: DocumentEvent) {
-          if (myProjectConfigurable.isFrozen) return
-          LanguageLevelProjectExtensionImpl.getInstanceImpl(myProject).currentLevel = myLanguageLevelCombo.selectedLevel
-        }
-      })
     }
 
     buildPanel()
@@ -61,53 +63,51 @@ internal class ProjectConfigurableUi(private val myProjectConfigurable: ProjectC
     myPanel = panel {
 
       if (myProject.isDirectoryBased) {
-        blockRow {
-          nestedPanel {
-            row {
-              label(JavaUiBundle.message("project.structure.title"), bold = true).comment(JavaUiBundle.message("project.structure.comment"),
-                120)
+        row {
+          label(JavaUiBundle.message("project.structure.title"))
+            .bold()
+            .comment(JavaUiBundle.message("project.structure.comment"),
+              120)
+        }
+          .bottomGap(BottomGap.SMALL)
+
+        row {
+          label(JavaUiBundle.message("project.structure.name"))
+          textField()
+            .bindText(nameProperty)
+            .columns(28)
+        }
+      }
+
+      group(JavaUiBundle.message("project.structure.sdks")) {
+        row(JavaUiBundle.message("project.structure.java.sdk")) {
+          cell(myProjectJdkConfigurable.createComponent())
+        }
+      }
+
+      group(JavaUiBundle.message("project.structure.java")) {
+        row(JavaUiBundle.message("module.module.language.level")) {
+          cell(myLanguageLevelCombo)
+        }
+        row(JavaUiBundle.message("project.structure.compiler.output")) {
+          textFieldWithBrowseButton()
+            .bindText(compilerOutputProperty)
+            .onIsModified {
+              if (!myProjectConfigurable.isFrozen)
+                LanguageLevelProjectExtensionImpl.getInstanceImpl(myProject).currentLevel = myLanguageLevelCombo.selectedLevel
+              return@onIsModified true
             }
-          }
-        }
-
-        nestedPanel {
-          blockRow {
-            cell {
-              label(JavaUiBundle.message("project.structure.name"))
-              myProjectName()
-            }
-          }
+            .horizontalAlign(HorizontalAlign.FILL)
+            .comment(JavaUiBundle.message("project.structure.compiler.output.comment"), 84)
         }
       }
-
-      nestedPanel {
-        titledRow(JavaUiBundle.message("project.structure.sdks")) {
-          row(JavaUiBundle.message("project.structure.java.sdk")) {
-            myProjectJdkConfigurable.createComponent()()
-          }
-        }
-      }
-
-      nestedPanel {
-        titledRow(JavaUiBundle.message("project.structure.java")) {
-          row(JavaUiBundle.message("module.module.language.level")) {
-            myLanguageLevelCombo()
-          }
-          row(JavaUiBundle.message("project.structure.compiler.output")) {
-            myProjectCompilerOutput().comment(JavaUiBundle.message("project.structure.compiler.output.comment"), 84)
-          }
-        }
-      }
+    }.apply {
+      withBorder(JBUI.Borders.empty(20, 20))
     }
-
-    myPanel.preferredSize = JBUI.size(700, 500)
-    myPanel.border = JBUI.Borders.empty(20)
   }
 
   fun getPanel(): JPanel = myPanel
 
-  fun getProjectName(): String = myProjectName.text
-  fun getProjectCompilerOutput(): String = myProjectCompilerOutput.text
   fun getLanguageLevel(): LanguageLevel? = myLanguageLevelCombo.selectedLevel
   fun isDefaultLanguageLevel(): Boolean = myLanguageLevelCombo.isDefault
   fun isProjectJdkConfigurableModified(): Boolean = myProjectJdkConfigurable.isModified
@@ -124,9 +124,9 @@ internal class ProjectConfigurableUi(private val myProjectConfigurable: ProjectC
   fun reset(compilerOutput: String?) {
     myProjectJdkConfigurable.reset()
     if (compilerOutput != null) {
-      myProjectCompilerOutput.text = FileUtil.toSystemDependentName(VfsUtilCore.urlToPath(compilerOutput))
+      projectCompilerOutput = FileUtil.toSystemDependentName(VfsUtilCore.urlToPath(compilerOutput))
     }
     myLanguageLevelCombo.reset(myProject)
-    myProjectName.text = myProject.name
+    projectName = myProject.name
   }
 }
