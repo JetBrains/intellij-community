d4571638e6ee4ca8a161d5916035ed1dbda6b5ea
sergey.vasiliev <sergey.vasiliev@jetbrains.com>
2021-09-13 12:45:34 +0200

    IDEA-154812 Exception at BaseRefactoringProcessor (AliasingPsiTarget)
    GitOrigin-RevId: aa36563a4b1e1b99ca98f0934fcd6b8f847f2995

--- java/java-impl/src/com/intellij/refactoring/rename/RenameAliasingPomTargetProcessor.java
+++ java/java-impl/src/com/intellij/refactoring/rename/RenameAliasingPomTargetProcessor.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.refactoring.rename;
 
 import com.intellij.openapi.diagnostic.Logger;
@@ -6,6 +6,7 @@ import com.intellij.pom.PomTarget;
 import com.intellij.pom.PomTargetPsiElement;
 import com.intellij.pom.references.PomService;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiNamedElement;
 import com.intellij.psi.targets.AliasingPsiTarget;
 import com.intellij.psi.targets.AliasingPsiTargetMapper;
 import org.jetbrains.annotations.NotNull;
@@ -31,22 +32,38 @@ public class RenameAliasingPomTargetProcessor extends RenamePsiElementProcessor 
     }
 
     if (target != null) {
-      for (AliasingPsiTargetMapper mapper : AliasingPsiTargetMapper.EP_NAME.getExtensionList()) {
-        for (AliasingPsiTarget psiTarget : mapper.getTargets(target)) {
-          PsiElement psiElement = PomService.convertToPsi(psiTarget);
-          String name = psiTarget.getNameAlias(newName);
+      if (target instanceof AliasingPsiTarget) {
+        prepareAliasingPsiTargetRenaming((AliasingPsiTarget)target, newName, allRenames);
+      } else {
+        for (AliasingPsiTargetMapper mapper : AliasingPsiTargetMapper.EP_NAME.getExtensionList()) {
+          for (AliasingPsiTarget psiTarget : mapper.getTargets(target)) {
+            PsiElement psiElement = PomService.convertToPsi(psiTarget);
+            String name = psiTarget.getNameAlias(newName);
 
-          String definedName = allRenames.put(psiElement, name);
-          if (definedName != null) {
-            LOG.assertTrue(definedName.equals(name), "target: " + psiTarget + "; " +
-                                                     "defined name: " + definedName + "; " +
-                                                     "name: " + name + "; " +
-                                                     "mapper: " + mapper);
-          }
-          else {
-            prepareRenaming(psiElement, name, allRenames);
+            String definedName = allRenames.put(psiElement, name);
+            if (definedName != null) {
+              LOG.assertTrue(definedName.equals(name), "target: " + psiTarget + "; " +
+                                                       "defined name: " + definedName + "; " +
+                                                       "name: " + name + "; " +
+                                                       "mapper: " + mapper);
+            }
+            else {
+              if (name != null) prepareRenaming(psiElement, name, allRenames);
+            }
           }
         }
+      }
+    }
+  }
+
+  private static void prepareAliasingPsiTargetRenaming(@NotNull AliasingPsiTarget aliasingPsiTarget,
+                                                       @NotNull String newName,
+                                                       @NotNull Map<PsiElement, String> allRenames) {
+    PsiElement psiElement = aliasingPsiTarget.getNavigationElement();
+    if (psiElement instanceof PsiNamedElement) {
+      String newTargetName = aliasingPsiTarget.getTargetName(newName);
+      if (!newTargetName.equals(((PsiNamedElement)psiElement).getName())) {
+        allRenames.put(psiElement, newTargetName);
       }
     }
   }
