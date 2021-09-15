504f1d8f2f3ccb882ab73b42b6970735727cbe18
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2021-09-15 16:10:27 +0700

    [uast-kotlin] Avoid NPE when originalElement is null
    GitOrigin-RevId: ba02f47b4f2d7d0dcfceefb32a00a3e004eadd29

--- plugins/kotlin/uast/uast-kotlin/src/org/jetbrains/uast/kotlin/kotlinPsiElementMapping.kt
+++ plugins/kotlin/uast/uast-kotlin/src/org/jetbrains/uast/kotlin/kotlinPsiElementMapping.kt
@@ -22,9 +22,9 @@ private val checkCanConvert = Registry.`is`("kotlin.uast.use.psi.type.precheck",
 
 internal fun canConvert(element: PsiElement, targets: Array<out Class<out UElement>>): Boolean {
     if (!checkCanConvert) return true
-    val originalCls = element.originalElement.javaClass
+    val originalCls = element.originalElement?.javaClass
 
-    if (targets.any { getPossibleSourceTypes(it).let { originalCls in it } })
+    if (originalCls != null && targets.any { getPossibleSourceTypes(it).let { originalCls in it } })
         return true
 
     val ktOriginalCls = (element as? KtLightElementBase)?.kotlinOrigin?.javaClass ?: return false
