3ee5948cd7d752e25955fae394bb250d36f11d9d
Jinseong Jeon <jsjeon@google.com>
2021-09-15 16:14:59 -0700

    FIR/UAST: minor improvement in annotation's javaPsi
    GitOrigin-RevId: b44be4543a9744f6bbcdacbe6d828281c64095f9

--- plugins/kotlin/uast/uast-kotlin-base/src/org/jetbrains/uast/kotlin/declarations/KotlinUAnnotation.kt
+++ plugins/kotlin/uast/uast-kotlin-base/src/org/jetbrains/uast/kotlin/declarations/KotlinUAnnotation.kt
@@ -75,8 +75,9 @@ class KotlinUAnnotation(
     givenParent: UElement?
 ) : KotlinUAnnotationBase<KtAnnotationEntry>(annotationEntry, givenParent), UAnnotation {
 
-    override val javaPsi: PsiAnnotation? =
+    override val javaPsi by lz {
         annotationEntry.actionUnderSafeAnalyzeBlock({ annotationEntry.toLightAnnotation() }, { null })
+    }
 
     override fun annotationUseSiteTarget() = sourcePsi.useSiteTarget?.getAnnotationUseSiteTarget()
 
