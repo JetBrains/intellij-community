72f45a3a74e36919021a262e6ed381b4f8865adc
Dmitry Timofeev <dmitry.timofeev@jetbrains.com>
2021-09-17 16:06:51 +0300

    [kotlin] plugin compatibility fix for addAnnotation overload
    GitOrigin-RevId: 1ad7c50949dce0658e91d9de430683e346be488c

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/ReplaceAnnotationFix.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/ReplaceAnnotationFix.kt
@@ -9,6 +9,7 @@ import org.jetbrains.kotlin.descriptors.annotations.AnnotationUseSiteTarget
 import org.jetbrains.kotlin.idea.KotlinBundle
 import org.jetbrains.kotlin.idea.core.ShortenReferences
 import org.jetbrains.kotlin.idea.util.addAnnotation
+import org.jetbrains.kotlin.idea.util.addAnnotationWithUseSiteTarget
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.KtAnnotationEntry
 import org.jetbrains.kotlin.psi.KtFile
@@ -58,7 +59,10 @@ open class ReplaceAnnotationFix(
                 ?: existingEntry.addAfter(psiFactory.createCallArguments("($annotationInnerText)"), existingEntry.lastChild)
             ShortenReferences.DEFAULT.process(existingEntry)
         } else {
-            owner.addAnnotation(annotationFqName, annotationInnerText, useSiteTarget = useSiteTarget)
+            if (useSiteTarget != null)
+                owner.addAnnotationWithUseSiteTarget(annotationFqName, useSiteTarget, annotationInnerText)
+            else
+                owner.addAnnotation(annotationFqName, annotationInnerText)
         }
     }
 }
