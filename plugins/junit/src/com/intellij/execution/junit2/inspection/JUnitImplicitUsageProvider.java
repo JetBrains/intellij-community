7ab10868ca6c697bd040ccb446dd53e103373383
Bas Leijdekkers <basleijdekkers@gmail.com>
2021-09-17 23:47:35 +0200

    JUnit: implicit usage provider optimization
    GitOrigin-RevId: ea5f4aa3948e425fc9606e7c7d8144b55b282967

--- plugins/junit/src/com/intellij/execution/junit2/inspection/JUnitImplicitUsageProvider.java
+++ plugins/junit/src/com/intellij/execution/junit2/inspection/JUnitImplicitUsageProvider.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.junit2.inspection;
 
 import com.intellij.codeInsight.AnnotationUtil;
@@ -37,6 +37,7 @@ public class JUnitImplicitUsageProvider implements ImplicitUsageProvider {
     if (!(element instanceof PsiMethod)) return false;
     PsiMethod method = (PsiMethod) element;
     if (method.getAnnotation(ORG_JUNIT_JUPITER_PARAMS_PROVIDER_METHOD_SOURCE) != null) return false;
+    if (method.getParameterList().getParametersCount() != 0) return false;
     PsiClass psiClass = method.getContainingClass();
     if (psiClass == null) return false;
     String methodName = method.getName();
