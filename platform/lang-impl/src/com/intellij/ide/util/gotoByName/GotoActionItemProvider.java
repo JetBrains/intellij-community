f2688ebdd21ec50cac4dd2725501275273df43b3
Anna Kozlova <anna.kozlova@jetbrains.com>
2021-09-17 09:47:04 +0200

    [SE] prevent intention computation from freezing
    GitOrigin-RevId: 1fc54128921df62de48254eac38a9fca206a81ca

--- platform/lang-impl/src/com/intellij/ide/util/gotoByName/GotoActionItemProvider.java
+++ platform/lang-impl/src/com/intellij/ide/util/gotoByName/GotoActionItemProvider.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.util.gotoByName;
 
 import com.intellij.ide.SearchTopHitProvider;
@@ -55,7 +55,7 @@ public final class GotoActionItemProvider implements ChooseByNameWeightedItemPro
 
   public GotoActionItemProvider(GotoActionModel model) {
     myModel = model;
-    myIntentions = ClearableLazyValue.create(() -> ReadAction.compute(() -> myModel.getAvailableIntentions()));
+    myIntentions = ClearableLazyValue.create(() -> ReadAction.nonBlocking(() -> myModel.getAvailableIntentions()).executeSynchronously());
   }
 
   @NotNull
