c5a7af222a52b32aa2628648faacb512da6c462b
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2021-09-19 00:50:00 +0300

    [groovy] IDEA-275560: Fix statements mover
    GitOrigin-RevId: 186aad87f74e63a7fc61751ac7b1746735922d1d

--- plugins/groovy/src/org/jetbrains/plugins/groovy/editor/actions/GroovyStatementMover.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/editor/actions/GroovyStatementMover.java
@@ -21,11 +21,9 @@ import org.jetbrains.plugins.groovy.lang.groovydoc.psi.api.GrDocComment;
 import org.jetbrains.plugins.groovy.lang.groovydoc.psi.api.GrDocCommentOwner;
 import org.jetbrains.plugins.groovy.lang.psi.GroovyFileBase;
 import org.jetbrains.plugins.groovy.lang.psi.GroovyPsiElement;
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrField;
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrStatement;
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrVariable;
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrVariableDeclaration;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.*;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.blocks.GrCodeBlock;
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.blocks.GrOpenBlock;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.clauses.GrCaseSection;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.literals.GrLiteral;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.GrTypeDefinition;
@@ -123,13 +121,11 @@ public class GroovyStatementMover extends StatementUpDownMover {
         if (stmtLevel && element instanceof GrCodeBlock) {
           final PsiElement lBrace = ((GrCodeBlock)element).getLBrace();
           if (nlsAfter(lBrace)) {
-            assert lBrace != null;
             addRange(new LineRange(lBrace).endLine);
           }
           addChildRanges(((GrCodeBlock)element).getStatements());
           final PsiElement rBrace = ((GrCodeBlock)element).getRBrace();
           if (nlsAfter(rBrace)) {
-            assert rBrace != null;
             final int endLine = new LineRange(rBrace).endLine;
             if (lastStart >= 0) {
               for (int i = lastStart + 1; i < endLine; i++) {
@@ -140,11 +136,21 @@ public class GroovyStatementMover extends StatementUpDownMover {
         }
         else if (stmtLevel && element instanceof GrCaseSection) {
           PsiElement delimiter = ((GrCaseSection)element).getColon();
-          if (delimiter == null) delimiter = ((GrCaseSection)element).getArrow();
-          if (delimiter != null && nlsAfter(delimiter)) {
-            addRange(new LineRange(element.getFirstChild(), delimiter).endLine);
+          if (delimiter != null) {
+            if (nlsAfter(delimiter)) {
+              addRange(new LineRange(element.getFirstChild(), delimiter).endLine);
+            }
+            addChildRanges(((GrCaseSection)element).getStatements());
+          } else {
+            var statements = ((GrCaseSection)element).getStatements();
+            if (statements.length == 1 && statements[0] instanceof GrBlockStatement) {
+              GrOpenBlock block = ((GrBlockStatement)statements[0]).getBlock();
+              if (nlsAfter(block.getLBrace())) {
+                addRange(new LineRange(element.getFirstChild(), block.getLBrace()).endLine);
+              }
+              addChildRanges(block.getStatements());
+            }
           }
-          addChildRanges(((GrCaseSection)element).getStatements());
         }
         else if (element instanceof GroovyFileBase) {
           addChildRanges(((GroovyFileBase)element).getTopStatements());
--- plugins/groovy/test/org/jetbrains/plugins/groovy/lang/actions/updown/GroovyMoveStatementTest.java
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/lang/actions/updown/GroovyMoveStatementTest.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package org.jetbrains.plugins.groovy.lang.actions.updown;
 
@@ -67,7 +53,7 @@ public class GroovyMoveStatementTest extends GroovyEditorActionTestBase {
   public void testLeaveGroovydocWithMethod() { bothTest(); }
   public void testMoveMethodWithGroovydoc() { bothTest(); }
   public void testMoveGroovyDocWithField() { bothTest(); }
-  
+
   public void testMoveSecondFieldUp() { bothTest(); }
   public void testMoveFirstFieldDown() { bothTest(); }
 
@@ -75,7 +61,7 @@ public class GroovyMoveStatementTest extends GroovyEditorActionTestBase {
   public void testVariableOverClassInScript() { bothTest(); }
 
   public void testUpFromLastOffset() { bothTest(); }
-  
+
   public void testClosureWithPrequel() { bothTest(); }
 
   public void testMultiLineVariable() { bothTest(); }
@@ -90,6 +76,8 @@ public class GroovyMoveStatementTest extends GroovyEditorActionTestBase {
   public void testInSwitchCaseUp3() { bothTest(); }
   public void testInSwitchCaseUp4() { bothTest(); }
   public void testInSwitchCaseUp5() { bothTest(); }
+
+  public void testInSwitchExprUp1() { bothTest(); }
 
   public void testTwoStatements() { bothTest(); }
 
