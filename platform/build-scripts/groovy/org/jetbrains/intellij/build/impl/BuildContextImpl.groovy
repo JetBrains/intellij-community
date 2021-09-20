2587e23fdb276627f959d957796d38b94e3d4607
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2021-09-20 21:31:51 +0200

    [build scripts] moving critical VM options to launchers (JBR-2729)
    GitOrigin-RevId: 767fe8c40b6b1a7483dbc07cba0637a92cda813d

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/BuildContextImpl.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/BuildContextImpl.groovy
@@ -356,6 +356,29 @@ final class BuildContextImpl extends BuildContext {
       //noinspection SpellCheckingInspection
       jvmArgs.add('-Dsplash=true')
     }
+
+    if (options.bundledJreVersion >= 17) {
+      jvmArgs.addAll([
+        '--add-opens=java.base/java.lang=ALL-UNNAMED',
+        '--add-opens=java.base/java.util=ALL-UNNAMED',
+        '--add-opens=java.base/jdk.internal.vm=ALL-UNNAMED',
+        '--add-opens=java.base/sun.nio.ch=ALL-UNNAMED',
+        '--add-opens=java.desktop/java.awt=ALL-UNNAMED',
+        '--add-opens=java.desktop/java.awt.event=ALL-UNNAMED',
+        '--add-opens=java.desktop/javax.swing=ALL-UNNAMED',
+        '--add-opens=java.desktop/javax.swing.plaf.basic=ALL-UNNAMED',
+        '--add-opens=java.desktop/javax.swing.text.html=ALL-UNNAMED',
+        '--add-opens=java.desktop/sun.awt=ALL-UNNAMED',
+        '--add-opens=java.desktop/sun.awt.image=ALL-UNNAMED',
+        '--add-opens=java.desktop/sun.awt.windows=ALL-UNNAMED',
+        '--add-opens=java.desktop/sun.font=ALL-UNNAMED',
+        '--add-opens=java.desktop/sun.java2d=ALL-UNNAMED',
+        '--add-opens=java.desktop/sun.swing=ALL-UNNAMED',
+        '--add-opens=java.desktop/com.apple.eawt=ALL-UNNAMED',
+        '--add-opens=java.desktop/com.apple.eawt.event=ALL-UNNAMED',
+        '--add-opens=java.desktop/com.apple.laf=ALL-UNNAMED'])
+    }
+
     return jvmArgs
   }
 }
--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/VmOptionsGenerator.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/VmOptionsGenerator.groovy
@@ -20,25 +20,6 @@ final class VmOptionsGenerator {
     '-Djdk.module.illegalAccess.silent=true',
     '-Dkotlinx.coroutines.debug=off',
     '-XX:+IgnoreUnrecognizedVMOptions', //todo[kb] remove when we find a way to remove outdated options like -XX:+UseConcMarkSweepGC
-    '--add-opens=java.base/java.lang=ALL-UNNAMED',
-    '--add-opens=java.base/java.util=ALL-UNNAMED',
-    '--add-opens=java.base/jdk.internal.vm=ALL-UNNAMED',
-    '--add-opens=java.base/sun.nio.ch=ALL-UNNAMED',
-    '--add-opens=java.desktop/java.awt=ALL-UNNAMED',
-    '--add-opens=java.desktop/java.awt.event=ALL-UNNAMED',
-    '--add-opens=java.desktop/java.awt.peer=ALL-UNNAMED',
-    '--add-opens=java.desktop/javax.swing=ALL-UNNAMED',
-    '--add-opens=java.desktop/javax.swing.plaf.basic=ALL-UNNAMED',
-    '--add-opens=java.desktop/javax.swing.text.html=ALL-UNNAMED',
-    '--add-opens=java.desktop/sun.awt=ALL-UNNAMED',
-    '--add-opens=java.desktop/sun.awt.image=ALL-UNNAMED',
-    '--add-opens=java.desktop/sun.awt.windows=ALL-UNNAMED',
-    '--add-opens=java.desktop/sun.font=ALL-UNNAMED',
-    '--add-opens=java.desktop/sun.java2d=ALL-UNNAMED',
-    '--add-opens=java.desktop/sun.swing=ALL-UNNAMED',
-    '--add-opens=java.desktop/com.apple.eawt=ALL-UNNAMED',
-    '--add-opens=java.desktop/com.apple.eawt.event=ALL-UNNAMED',
-    '--add-opens=java.desktop/com.apple.laf=ALL-UNNAMED'
     )
 
   static final Map<String, String> MEMORY_OPTIONS = Map.of(
