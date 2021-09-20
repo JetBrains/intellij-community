c766d3deb3af3cbdf58fc4f5933112ab891ff34b
Aleksey Pivovarov <AMPivovarov@gmail.com>
2021-09-20 23:19:23 +0300

    diff: remove code duplication
    GitOrigin-RevId: 06810a91a6266cb7edd6ffec39559f4e290cd2a5

--- plugins/git4idea/src/git4idea/stash/ui/GitStashEditorDiffPreview.kt
+++ plugins/git4idea/src/git4idea/stash/ui/GitStashEditorDiffPreview.kt
@@ -1,40 +1,21 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package git4idea.stash.ui
 
-import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.vcs.changes.EditorTabPreview
 import com.intellij.openapi.vcs.changes.ui.ChangesTree
 import com.intellij.openapi.vcs.changes.ui.ChangesViewContentManager
-import com.intellij.ui.ExpandableItemsHandler
-import com.intellij.util.ui.UIUtil
+import com.intellij.openapi.vcs.changes.ui.SimpleTreeEditorDiffPreview
 import git4idea.i18n.GitBundle
 import javax.swing.JComponent
 
-class GitStashEditorDiffPreview(diffProcessor: GitStashDiffPreview, tree: ChangesTree, targetComponent: JComponent) : EditorTabPreview(diffProcessor) {
+class GitStashEditorDiffPreview(diffProcessor: GitStashDiffPreview, tree: ChangesTree, targetComponent: JComponent)
+  : SimpleTreeEditorDiffPreview(diffProcessor, tree, targetComponent, false) {
 
-  private val stashDiffPreview: GitStashDiffPreview get() = diffProcessor as GitStashDiffPreview
-
-  init {
-    escapeHandler = Runnable {
-      closePreview()
-      ChangesViewContentManager.getToolWindowFor(project, GitStashContentProvider.TAB_NAME)?.activate(null)
-    }
-    installListeners(tree, false)
-    installNextDiffActionOn(targetComponent)
-    UIUtil.putClientProperty(tree, ExpandableItemsHandler.IGNORE_ITEM_SELECTION, true)
-  }
-
-  override fun updateAvailability(event: AnActionEvent) {
-    event.presentation.isVisible = event.isFromActionToolbar || event.presentation.isEnabled
+  override fun returnFocusToTree() {
+    ChangesViewContentManager.getToolWindowFor(project, GitStashContentProvider.TAB_NAME)?.activate(null)
   }
 
   override fun getCurrentName(): String {
-    return stashDiffPreview
-             .currentChangeName?.let { changeName -> GitBundle.message("stash.editor.diff.preview.title", changeName) }
+    return changeViewProcessor.currentChangeName?.let { changeName -> GitBundle.message("stash.editor.diff.preview.title", changeName) }
            ?: GitBundle.message("stash.editor.diff.preview.empty.title")
-  }
-
-  override fun hasContent(): Boolean {
-    return stashDiffPreview.currentChangeName != null
   }
 }
