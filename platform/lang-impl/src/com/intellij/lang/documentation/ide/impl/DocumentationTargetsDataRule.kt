0c3ff0d0d5db7925e9dbd7b4ea01d0ff3131d631
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-09-20 18:21:36 +0200

    IDEA-277609 [documentation] move `DocumentationTargetsDataRule` to proper package
    GitOrigin-RevId: 930908dbe65a984873ec1af2f5e8331cc220856f

--- platform/lang-impl/src/com/intellij/lang/documentation/ide/impl/DocumentationTargetsDataRule.kt
+++ platform/lang-impl/src/com/intellij/lang/documentation/ide/impl/DocumentationTargetsDataRule.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.lang.documentation.ide.actions
+package com.intellij.lang.documentation.ide.impl
 
 import com.intellij.ide.impl.dataRules.GetDataRule
 import com.intellij.lang.documentation.DocumentationTarget
