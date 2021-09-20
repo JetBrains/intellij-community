b7810682d725538859ebca1c2ab71141e3eb8dde
Anton Bragin <Anton.Bragin@jetbrains.com>
2021-09-20 19:29:59 +0300

    DS-1356 Only part of image is used to guess whether the inversion should be applied
    GitOrigin-RevId: 4c0e85344da0e6f592256553c16347cbaf97fb18

--- notebooks/visualization/src/org/intellij/datavis/r/inlays/components/ImageInverter.kt
+++ notebooks/visualization/src/org/intellij/datavis/r/inlays/components/ImageInverter.kt
@@ -11,6 +11,8 @@ import java.awt.image.IndexColorModel
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import javax.imageio.ImageIO
+import kotlin.math.max
+import kotlin.math.min
 
 class ImageInverter(foreground: Color, background: Color) {
   private val rgb = FloatArray(3)
@@ -29,35 +31,75 @@ class ImageInverter(foreground: Color, background: Color) {
    * Check if [image] should be inverted in dark themes.
    *
    * @param brightnessThreshold images with average brightness exceeding the threshold will be recommended for inversion
-   * @param numberOfColorsThreshold images with number of colors below the threshold will be recommended for inversion
    *
    * @return true if it's recommended to invert the image
    */
-  fun shouldInvert(image: BufferedImage, brightnessThreshold: Double = 0.7, numberOfColorsThreshold: Int = 5000): Boolean {
-    val numberOfPixels = image.height * image.width
-    val colors = IntArray(numberOfPixels)
-    image.getRGB(0, 0, image.width, image.height, colors, 0, image.width)
-
-    val averageBrightness = colors.map { getBrightness(it, image) }.sum() / numberOfPixels
+  fun shouldInvert(image: BufferedImage, brightnessThreshold: Double = 0.7): Boolean {
+    val colors = getImageSample(image)
+    val numberOfColorsInComplexImage = 5000
+    val numberOfPixels = colors.size
+    val numberOfColorsThreshold = min(numberOfPixels / 3, numberOfColorsInComplexImage)
+    val hasAlpha = image.colorModel.hasAlpha()
+    
+    val averageBrightness = colors.map { getBrightness(it, hasAlpha) }.sum() / numberOfPixels
     val numberOfColors = colors.toSet()
     
     return (averageBrightness > brightnessThreshold && numberOfColors.size < numberOfColorsThreshold) ||
-           hasLightBackground(colors, image, brightnessThreshold) == true
+           hasLightBackground(colors, hasAlpha, brightnessThreshold) == true
   }
 
-  private fun getBrightness(argb: Int, image: BufferedImage): Float {
-    val color = Color(argb, image.colorModel.hasAlpha())
+  /**
+   * Get part of image for color analysis.
+   * 
+   * For narrow/low images all image pixels are returned.
+   * For regular images the result is a concatenation of areas in image corners and at the central area.
+   */
+  private fun getImageSample(image: BufferedImage): IntArray {
+    if (image.height < 10 || image.width < 10) {
+      val colors = IntArray(image.height * image.width)
+      image.getRGB(0, 0, image.width, image.height, colors, 0, image.width)
+      return colors
+    }
+    
+    val defaultSpotSize = min(max(image.height / 10, image.width / 10), min(image.height, image.width))
+    val spotHeight = min(image.height, defaultSpotSize)
+    val spotWidth = min(image.width, defaultSpotSize)
+
+    val spotSize = spotHeight * spotWidth
+    val colors = IntArray(spotSize * 5)
+
+    image.getRGB(0, 0, spotWidth, spotHeight, colors, 0, spotWidth)
+    image.getRGB(image.width - spotWidth, 0, spotWidth, spotHeight, colors, spotSize, spotWidth)
+    image.getRGB(0, image.height - spotHeight, spotWidth, spotHeight, colors, 2 * spotSize, spotWidth)
+    image.getRGB(image.width - spotWidth, image.height - spotHeight, spotWidth, spotHeight, colors, 3 * spotSize, spotWidth)
+    
+    // We operate on integers so dividing and multiplication with the same number is not trivial operation 
+    val centralSpotX = image.width / spotWidth / 2 * spotWidth
+    val centralSpotY = image.height / spotHeight / 2 * spotHeight
+    
+    image.getRGB(centralSpotX, centralSpotY, spotWidth, spotHeight, colors, 4 * spotSize, spotWidth)
+    
+    return colors
+  }
+  
+  private fun getBrightness(argb: Int, hasAlpha: Boolean): Float {
+    val color = Color(argb, hasAlpha)
     val hsb = FloatArray(3)
     Color.RGBtoHSB(color.red, color.green, color.blue, hsb)
     return hsb[2]
   }
-  
-  private fun hasLightBackground(colors: IntArray, image: BufferedImage, brightnessThreshold: Double): Boolean? {
+
+  /**
+   * Try to guess whether the image has light background.
+   * 
+   * The background is defined as a large fraction of pixels with the same color.
+   */
+  private fun hasLightBackground(colors: IntArray, hasAlpha: Boolean, brightnessThreshold: Double): Boolean? {
     val dominantColorPair = colors.groupBy { it }.maxByOrNull { it.value.size } ?: return null
     val dominantColor = dominantColorPair.key
     val dominantPixels = dominantColorPair.value
     
-    return dominantPixels.size.toDouble() / colors.size > 0.5 && getBrightness(dominantColor, image) > brightnessThreshold
+    return dominantPixels.size.toDouble() / colors.size > 0.5 && getBrightness(dominantColor, hasAlpha) > brightnessThreshold
   }
   
   fun invert(color: Color): Color {
