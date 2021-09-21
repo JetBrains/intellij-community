5431d2c4120971bf46bf301dfa0ffe7beeb0be8a
Nikita Skvortsov <Nikita.Skvortsov@jetbrains.com>
2021-09-21 09:36:52 +0300

    [gradle] trigger multi-threaded configuration resolution before iterating over resolution results IDEA-275594
    GitOrigin-RevId: 6beb55181c1e40529489340cc44f8790df775f29

--- plugins/gradle/tooling-extension-impl/src/org/jetbrains/plugins/gradle/tooling/util/resolve/DependencyResolverImpl.java
+++ plugins/gradle/tooling-extension-impl/src/org/jetbrains/plugins/gradle/tooling/util/resolve/DependencyResolverImpl.java
@@ -181,6 +181,16 @@ public final class DependencyResolverImpl implements DependencyResolver {
     if (configuration == null) {
       return emptySet();
     }
+
+    // following statement should trigger parallel resolution of configurations artifacts
+    // all subsequent iteration are expected to use cached results.
+    configuration.getIncoming().artifactView(new Action<ArtifactView.ViewConfiguration>() {
+      @Override
+      public void execute(@NotNull ArtifactView.ViewConfiguration configuration) {
+        configuration.setLenient(true);
+      }
+    }).getArtifacts().getArtifacts();
+
     LenientConfiguration lenientConfiguration = configuration.getResolvedConfiguration().getLenientConfiguration();
     ResolutionResult resolutionResult = configuration.getIncoming().getResolutionResult();
     List<ComponentIdentifier> components = new ArrayList<ComponentIdentifier>();
