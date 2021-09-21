ff3e79c9eca7dbda47ecd7617569bfebbc553315
Margarita Bobova <Margarita.Bobova@jetbrains.com>
2021-09-21 14:54:10 +0300

    Update versions of wrappers
    GitOrigin-RevId: 0fe4dec82a9d9acba87268d2f23682e678959f85

--- plugins/kotlin/project-wizard/core/src/org/jetbrains/kotlin/tools/projectWizard/Versions.kt
+++ plugins/kotlin/project-wizard/core/src/org/jetbrains/kotlin/tools/projectWizard/Versions.kt
@@ -11,7 +11,7 @@ import org.jetbrains.kotlin.tools.projectWizard.settings.version.Version
 object Versions {
     val KOTLIN = version("1.5.0") // used as fallback version
     val GRADLE = version("7.0.2")
-    val KTOR = version("1.6.0")
+    val KTOR = version("1.6.3")
     val JUNIT = version("4.13")
     val JUNIT5 = version("5.6.0")
     val JETBRAINS_COMPOSE = version("1.0.0")
@@ -38,13 +38,13 @@ object Versions {
     object JS_WRAPPERS {
         val KOTLIN_REACT = wrapperVersion("17.0.2")
         val KOTLIN_REACT_DOM = KOTLIN_REACT
-        val KOTLIN_STYLED = wrapperVersion("5.3.0")
+        val KOTLIN_STYLED = wrapperVersion("5.3.1")
         val KOTLIN_REACT_ROUTER_DOM = wrapperVersion("5.2.0")
-        val KOTLIN_REDUX = wrapperVersion("4.0.5")
-        val KOTLIN_REACT_REDUX = wrapperVersion("7.2.3")
+        val KOTLIN_REDUX = wrapperVersion("4.1.0")
+        val KOTLIN_REACT_REDUX = wrapperVersion("7.2.4")
 
         private fun wrapperVersion(version: String): Version =
-            version("$version-pre.206-kotlin-1.5.10")
+            version("$version-pre.240-kotlin-1.5.30")
     }
 
     object GRADLE_PLUGINS {
