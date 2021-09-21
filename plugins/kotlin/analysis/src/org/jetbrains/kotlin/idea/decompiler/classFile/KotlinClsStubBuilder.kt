b098668423f5a6f5c2a18f4cc070d4e248dcb37e
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-09-21 22:12:40 +0700

    [decompiler] replace copy-pasted code with method from base class
    ^KTIJ-19676
    
    GitOrigin-RevId: c68b9396ba988247f44664aa4b5719fd54490e90

--- plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/decompiler/classFile/KotlinClsStubBuilder.kt
+++ plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/decompiler/classFile/KotlinClsStubBuilder.kt
@@ -12,7 +12,6 @@ import org.jetbrains.kotlin.SpecialJvmAnnotations
 import org.jetbrains.kotlin.descriptors.SourceElement
 import org.jetbrains.kotlin.idea.caches.IDEKotlinBinaryClassCache
 import org.jetbrains.kotlin.idea.decompiler.stubBuilder.*
-import org.jetbrains.kotlin.load.java.JvmAbi
 import org.jetbrains.kotlin.load.kotlin.*
 import org.jetbrains.kotlin.load.kotlin.header.KotlinClassHeader
 import org.jetbrains.kotlin.metadata.ProtoBuf
@@ -155,7 +154,7 @@ class AnnotationLoaderForClassFileStubBuilder(
             }
 
             override fun visitEnd() {
-                if (!isRepeatableWithImplicitContainer(arguments)) {
+                if (!isRepeatableWithImplicitContainer(annotationClassId, arguments)) {
                     result.add(annotationClassId)
                 }
             }
@@ -165,15 +164,5 @@ class AnnotationLoaderForClassFileStubBuilder(
             override fun visitArray(name: Name): KotlinJvmBinaryClass.AnnotationArrayArgumentVisitor? = null
             override fun visitAnnotation(name: Name, classId: ClassId): KotlinJvmBinaryClass.AnnotationArgumentVisitor? = null
         }
-    }
-
-    private fun isRepeatableWithImplicitContainer(arguments: Map<Name, ConstantValue<*>>): Boolean {
-        val containerKClassValue = arguments[Name.identifier("value")] as? KClassValue ?: return false
-        val normalClass = containerKClassValue.value as? KClassValue.Value.NormalClass ?: return false
-        val classId = normalClass.classId
-        if (classId.outerClassId == null || classId.shortClassName.asString() != JvmAbi.REPEATABLE_ANNOTATION_CONTAINER_NAME) return false
-
-        val klass = kotlinClassFinder.findKotlinClass(classId)
-        return klass != null && SpecialJvmAnnotations.isAnnotatedWithContainerMetaAnnotation(klass)
     }
 }
