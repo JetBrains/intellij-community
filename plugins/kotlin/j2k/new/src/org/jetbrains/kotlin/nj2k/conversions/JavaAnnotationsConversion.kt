1ac0cd676ca76d3f9a1266e7ced1dfe1b2b57b14
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-09-22 14:12:16 +0700

    [j2k] convert `java.lang.annotation.Repeatable` to `kotlin.jvm.JvmRepeatable` if possible
    ^KTIJ-19713 Fixed
    
    GitOrigin-RevId: 3f88cd26fa518db636d54b66ce0344302eded56f

--- plugins/kotlin/j2k/new/src/org/jetbrains/kotlin/nj2k/conversions/JavaAnnotationsConversion.kt
+++ plugins/kotlin/j2k/new/src/org/jetbrains/kotlin/nj2k/conversions/JavaAnnotationsConversion.kt
@@ -2,10 +2,15 @@
 
 package org.jetbrains.kotlin.nj2k.conversions
 
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.search.ProjectScope
+import org.jetbrains.kotlin.config.ApiVersion
+import org.jetbrains.kotlin.idea.stubindex.KotlinTopLevelTypeAliasFqNameIndex
 import org.jetbrains.kotlin.load.java.JvmAnnotationNames
 import org.jetbrains.kotlin.nj2k.NewJ2kConverterContext
 import org.jetbrains.kotlin.nj2k.tree.*
 import org.jetbrains.kotlin.utils.addToStdlib.safeAs
+import org.jetbrains.kotlin.utils.ifEmpty
 
 class JavaAnnotationsConversion(context: NewJ2kConverterContext) : RecursiveApplicableConversionBase(context) {
     override fun applyToElement(element: JKTreeElement): JKTreeElement {
@@ -23,8 +28,9 @@ class JavaAnnotationsConversion(context: NewJ2kConverterContext) : RecursiveAppl
     }
 
     private fun processAnnotation(annotation: JKAnnotation) {
-        annotation.tryConvertDeprecatedAnnotation() ||
-                annotation.tryConvertTargetAnnotation()
+        with(annotation) {
+            tryConvertDeprecatedAnnotation() || tryConvertTargetAnnotation() || tryConvertRepeatableAnnotation()
+        }
     }
 
     private fun JKAnnotation.tryConvertDeprecatedAnnotation(): Boolean {
@@ -59,6 +65,25 @@ class JavaAnnotationsConversion(context: NewJ2kConverterContext) : RecursiveAppl
                     arguments = it
                 }
 
+            return true
+        }
+
+        return false
+    }
+
+    private fun JKAnnotation.tryConvertRepeatableAnnotation(): Boolean {
+        if (classSymbol.fqName == JvmAnnotationNames.REPEATABLE_ANNOTATION.asString() && moduleApiVersion >= ApiVersion.KOTLIN_1_6) {
+            val jvmRepeatable = "kotlin.jvm.JvmRepeatable"
+            KotlinTopLevelTypeAliasFqNameIndex.getInstance()[
+                    jvmRepeatable,
+                    context.project,
+                    context.converter.targetModule?.let { GlobalSearchScope.moduleWithLibrariesScope(it) }
+                        ?: ProjectScope.getLibrariesScope(context.project)
+            ].ifEmpty {
+                return false
+            }
+
+            classSymbol = symbolProvider.provideClassSymbol(jvmRepeatable)
             return true
         }
 
