c5f5555de9a8b6c1329ebe0738180b6a0e39aa5f
Roman Ivanov <roman.ivanov@jetbrains.com>
2021-09-22 15:10:35 +0200

    JDParser use @Contract instead of @Nullable annotations to avoid FP
    GitOrigin-RevId: cdf2fc41d65a85cf121d70d186099af0faf916a6

--- java/java-impl/src/com/intellij/psi/impl/source/codeStyle/javadoc/JDParser.java
+++ java/java-impl/src/com/intellij/psi/impl/source/codeStyle/javadoc/JDParser.java
@@ -12,6 +12,7 @@ import com.intellij.psi.codeStyle.JavaCodeStyleSettings;
 import com.intellij.psi.javadoc.PsiDocComment;
 import com.intellij.psi.search.TodoPattern;
 import com.intellij.util.text.CharArrayUtil;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -438,7 +439,7 @@ public class JDParser {
    * @param width width of the wrapped text
    * @return array of strings (lines)
    */
-  @Nullable
+  @Contract("null, _ -> null")
   private List<String> toArrayWrapping(@Nullable String s, int width) {
     List<String> list = new ArrayList<>();
     List<Pair<String, Boolean>> pairs = splitToParagraphs(s);
@@ -498,7 +499,7 @@ public class JDParser {
    * @param s   string to process
    * @return    processing result
    */
-  @Nullable
+  @Contract("null -> null")
   private List<Pair<String, Boolean>> splitToParagraphs(@Nullable String s) {
     if (s == null) return null;
     s = s.trim();
