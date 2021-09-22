1d2269786899b6efd2b7ea89b9f52893359a6207
Nikita Skvortsov <Nikita.Skvortsov@jetbrains.com>
2021-09-22 17:14:52 +0300

    [gradle] do not fail the build on dependency resolution failure IDEA-278790
    GitOrigin-RevId: fe6dc97dfcd44d5f566ac6ac49bef056da6d6acf

--- plugins/gradle/tooling-extension-impl/src/org/jetbrains/plugins/gradle/tooling/util/resolve/DependencyResolverImpl.java
+++ plugins/gradle/tooling-extension-impl/src/org/jetbrains/plugins/gradle/tooling/util/resolve/DependencyResolverImpl.java
@@ -184,12 +184,14 @@ public final class DependencyResolverImpl implements DependencyResolver {
 
     // following statement should trigger parallel resolution of configurations artifacts
     // all subsequent iteration are expected to use cached results.
-    configuration.getIncoming().artifactView(new Action<ArtifactView.ViewConfiguration>() {
-      @Override
-      public void execute(@NotNull ArtifactView.ViewConfiguration configuration) {
-        configuration.setLenient(true);
-      }
-    }).getArtifacts().getArtifacts();
+    try {
+      configuration.getIncoming().artifactView(new Action<ArtifactView.ViewConfiguration>() {
+        @Override
+        public void execute(@NotNull ArtifactView.ViewConfiguration configuration) {
+          configuration.setLenient(true);
+        }
+      }).getArtifacts().getArtifacts();
+    } catch (Exception ignore) {}
 
     LenientConfiguration lenientConfiguration = configuration.getResolvedConfiguration().getLenientConfiguration();
     ResolutionResult resolutionResult = configuration.getIncoming().getResolutionResult();
