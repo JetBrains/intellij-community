5473a5a3a3d2d0335c63e66c1dce185d4ae9e90c
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2021-09-26 22:07:56 +0200

    [kotlin] Update Kotlin icons
    Relates to #KTIJ-19184
    
    GitOrigin-RevId: 5b048e38e9eb79d6d7e10e30a8e4798e306c1da9

--- plugins/kotlin/frontend-independent/src/icons/KotlinFirFrontendIndependentIcons.java
+++ plugins/kotlin/frontend-independent/src/icons/KotlinFirFrontendIndependentIcons.java
@@ -14,31 +14,31 @@ public final class KotlinFirFrontendIndependentIcons {
     return IconManager.getInstance().loadRasterizedIcon(path, KotlinFirFrontendIndependentIcons.class.getClassLoader(), cacheKey, flags);
   }
   /** 16x16 */ public static final @NotNull Icon Abstract_extension_function = load("org/jetbrains/kotlin/idea/icons/abstract_extension_function.svg", 7417679411263450358L, 0);
-  /** 16x16 */ public static final @NotNull Icon AbstractClassKotlin = load("org/jetbrains/kotlin/idea/icons/abstractClassKotlin.svg", 6118422021574552028L, 0);
+  /** 16x16 */ public static final @NotNull Icon AbstractClassKotlin = load("org/jetbrains/kotlin/idea/icons/abstractClassKotlin.svg", -227958960148310810L, 0);
   /** 16x16 */ public static final @NotNull Icon Actual = load("org/jetbrains/kotlin/idea/icons/actual.svg", -4727863439746678606L, 0);
-  /** 16x16 */ public static final @NotNull Icon AnnotationKotlin = load("org/jetbrains/kotlin/idea/icons/annotationKotlin.svg", 1981410558175520747L, 0);
-  /** 16x16 */ public static final @NotNull Icon ClassInitializerKotlin = load("org/jetbrains/kotlin/idea/icons/classInitializerKotlin.svg", 428407677003035493L, 0);
-  /** 16x16 */ public static final @NotNull Icon ClassKotlin = load("org/jetbrains/kotlin/idea/icons/classKotlin.svg", -4723898843334724017L, 0);
+  /** 16x16 */ public static final @NotNull Icon AnnotationKotlin = load("org/jetbrains/kotlin/idea/icons/annotationKotlin.svg", -542938248217452804L, 0);
+  /** 16x16 */ public static final @NotNull Icon ClassInitializerKotlin = load("org/jetbrains/kotlin/idea/icons/classInitializerKotlin.svg", -819443966118624997L, 0);
+  /** 16x16 */ public static final @NotNull Icon ClassKotlin = load("org/jetbrains/kotlin/idea/icons/classKotlin.svg", 3432369378109023470L, 0);
   /** 12x12 */ public static final @NotNull Icon DslMarkerAnnotation = load("org/jetbrains/kotlin/idea/icons/dslMarkerAnnotation.svg", 5574692736776531477L, 2);
-  /** 16x16 */ public static final @NotNull Icon EnumKotlin = load("org/jetbrains/kotlin/idea/icons/enumKotlin.svg", -8462447318313336813L, 0);
+  /** 16x16 */ public static final @NotNull Icon EnumKotlin = load("org/jetbrains/kotlin/idea/icons/enumKotlin.svg", -3107262642245201801L, 0);
   /** 16x16 */ public static final @NotNull Icon Expect = load("org/jetbrains/kotlin/idea/icons/expect.svg", -2910408808136595896L, 1);
   /** 16x16 */ public static final @NotNull Icon Field_value = load("org/jetbrains/kotlin/idea/icons/field_value.svg", 173519203061099646L, 0);
   /** 16x16 */ public static final @NotNull Icon Field_variable = load("org/jetbrains/kotlin/idea/icons/field_variable.svg", -4943884320640938494L, 0);
-  /** 16x16 */ public static final @NotNull Icon InterfaceKotlin = load("org/jetbrains/kotlin/idea/icons/interfaceKotlin.svg", 6105411896669082060L, 1);
-  /** 16x16 */ public static final @NotNull Icon Kotlin = load("org/jetbrains/kotlin/idea/icons/kotlin.svg", -6159597857264791813L, 0);
-  /** 13x13 */ public static final @NotNull Icon Kotlin13 = load("org/jetbrains/kotlin/idea/icons/kotlin13.svg", -5550034022007323579L, 0);
-  /** 16x16 */ public static final @NotNull Icon Kotlin_file = load("org/jetbrains/kotlin/idea/icons/kotlin_file.svg", 410462991849735236L, 0);
-  /** 16x16 */ public static final @NotNull Icon Kotlin_gradle_script = load("org/jetbrains/kotlin/idea/icons/kotlin_gradle_script.svg", 407924732847883692L, 0);
-  /** 16x16 */ public static final @NotNull Icon Kotlin_js = load("org/jetbrains/kotlin/idea/icons/kotlin_js.svg", -4797950250597216272L, 0);
-  /** 16x16 */ public static final @NotNull Icon Kotlin_launch_configuration = load("org/jetbrains/kotlin/idea/icons/kotlin_launch_configuration.svg", 8617317266006756955L, 0);
-  /** 16x16 */ public static final @NotNull Icon Kotlin_multiplatform_project = load("org/jetbrains/kotlin/idea/icons/kotlin_multiplatform_project.svg", 1800183257649593188L, 0);
-  /** 16x16 */ public static final @NotNull Icon Kotlin_native = load("org/jetbrains/kotlin/idea/icons/kotlin_native.svg", 4139485729290770650L, 0);
-  /** 16x16 */ public static final @NotNull Icon Kotlin_script = load("org/jetbrains/kotlin/idea/icons/kotlin_script.svg", 8910176401343092076L, 0);
+  /** 16x16 */ public static final @NotNull Icon InterfaceKotlin = load("org/jetbrains/kotlin/idea/icons/interfaceKotlin.svg", -5828088447818827807L, 1);
+  /** 16x16 */ public static final @NotNull Icon Kotlin = load("org/jetbrains/kotlin/idea/icons/kotlin.svg", 6883890252837590649L, 0);
+  /** 16x16 */ public static final @NotNull Icon Kotlin13 = load("org/jetbrains/kotlin/idea/icons/kotlin13.svg", 1473694336482961138L, 0);
+  /** 16x16 */ public static final @NotNull Icon Kotlin_file = load("org/jetbrains/kotlin/idea/icons/kotlin_file.svg", -1478619433488751770L, 0);
+  /** 16x16 */ public static final @NotNull Icon Kotlin_gradle_script = load("org/jetbrains/kotlin/idea/icons/kotlin_gradle_script.svg", -3186769780781836726L, 0);
+  /** 16x16 */ public static final @NotNull Icon Kotlin_js = load("org/jetbrains/kotlin/idea/icons/kotlin_js.svg", 8152888755935746231L, 0);
+  /** 16x16 */ public static final @NotNull Icon Kotlin_launch_configuration = load("org/jetbrains/kotlin/idea/icons/kotlin_launch_configuration.svg", 5820812500559770008L, 0);
+  /** 16x16 */ public static final @NotNull Icon Kotlin_multiplatform_project = load("org/jetbrains/kotlin/idea/icons/kotlin_multiplatform_project.svg", 4087514012143852704L, 0);
+  /** 16x16 */ public static final @NotNull Icon Kotlin_native = load("org/jetbrains/kotlin/idea/icons/kotlin_native.svg", 3295068484043525404L, 0);
+  /** 16x16 */ public static final @NotNull Icon Kotlin_script = load("org/jetbrains/kotlin/idea/icons/kotlin_script.svg", 5367077549717436975L, 0);
   /** 16x16 */ public static final @NotNull Icon Lambda = load("org/jetbrains/kotlin/idea/icons/lambda.svg", -395028811269734745L, 0);
   /** 16x16 */ public static final @NotNull Icon LoadScriptConfiguration = load("org/jetbrains/kotlin/idea/icons/loadScriptConfiguration.svg", 4218830826949357848L, 2);
-  /** 16x16 */ public static final @NotNull Icon ObjectKotlin = load("org/jetbrains/kotlin/idea/icons/objectKotlin.svg", 6837434796032324128L, 0);
+  /** 16x16 */ public static final @NotNull Icon ObjectKotlin = load("org/jetbrains/kotlin/idea/icons/objectKotlin.svg", -3912856265615102368L, 0);
   /** 12x12 */ public static final @NotNull Icon SuspendCall = load("org/jetbrains/kotlin/idea/icons/suspendCall.svg", -6406590614097998571L, 2);
-  /** 16x16 */ public static final @NotNull Icon TypeAlias = load("org/jetbrains/kotlin/idea/icons/typeAlias.svg", -272854517691002937L, 0);
+  /** 16x16 */ public static final @NotNull Icon TypeAlias = load("org/jetbrains/kotlin/idea/icons/typeAlias.svg", -6590065222255029733L, 0);
   /** 16x16 */ public static final @NotNull Icon Value = load("org/jetbrains/kotlin/idea/icons/value.svg", 5883345383289852521L, 0);
 
   public static final class Wizard {
