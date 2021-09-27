451cf0fd508e06890bb7f16fdb08d6b526c720d1
Peter Gromov <peter@jetbrains.com>
2021-09-27 08:19:56 +0200

    [grazie] process TextChecker results as soon as they're ready, to speed up the highlighting
    (cherry picked from commit 539cba75535874a090b92c91f43232c79dfba26f)
    
    GitOrigin-RevId: 78adcd70ea312a4d43f8e93b6322f803761740c2

--- plugins/grazie/src/main/kotlin/com/intellij/grazie/text/CheckerRunner.kt
+++ plugins/grazie/src/main/kotlin/com/intellij/grazie/text/CheckerRunner.kt
@@ -9,8 +9,8 @@ import com.intellij.codeInspection.ProblemDescriptorBase
 import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.grazie.ide.fus.GrazieFUSCounter
 import com.intellij.grazie.ide.inspection.grammar.quickfix.GrazieAddExceptionQuickFix
-import com.intellij.grazie.ide.inspection.grammar.quickfix.GrazieRuleSettingsAction
 import com.intellij.grazie.ide.inspection.grammar.quickfix.GrazieReplaceTypoQuickFix
+import com.intellij.grazie.ide.inspection.grammar.quickfix.GrazieRuleSettingsAction
 import com.intellij.grazie.ide.language.LanguageGrammarChecking
 import com.intellij.grazie.utils.toLinkedSet
 import com.intellij.lang.LanguageExtension
@@ -22,55 +22,64 @@ import com.intellij.psi.PsiFile
 import com.intellij.psi.SmartPointerManager
 import com.intellij.psi.util.parents
 import com.intellij.refactoring.suggested.startOffset
-import kotlinx.coroutines.CoroutineStart
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.*
 
 internal class CheckerRunner(val text: TextContent) {
   private val sentences by lazy { SRXSentenceTokenizer.tokenize(text.toString()) }
 
-  fun run(checkers: List<TextChecker>): List<TextProblem> {
-    val problems = runSuspendingAction {
+  fun run(checkers: List<TextChecker>, consumer: (TextProblem) -> Unit) {
+    runSuspendingAction {
       val deferred: List<Deferred<Collection<TextProblem>>> = checkers.map { checker ->
         when (checker) {
           is ExternalTextChecker -> async { checker.checkExternally(text) }
           else -> async(start = CoroutineStart.LAZY) { checker.check(text) }
         }
       }
-      deferred.awaitAll().flatten()
-    }
-
-    val filtered = ArrayList<TextProblem>()
-    for (problem in problems) {
-      require(problem.text == text)
-
-      if (isSuppressed(problem) ||
-          hasIgnoredCategory(problem) ||
-          isIgnoredByStrategies(problem) ||
-          isIgnoredByFilters(problem)) {
-        continue
+      launch {
+        for (job in deferred) {
+          yield() // allow the main coroutine to process the available results as soon as possible
+          job.start()
+        }
       }
 
-      if (filtered.none { it.highlightRange.intersects(problem.highlightRange) }) {
-        filtered.add(problem)
+      val filtered = ArrayList<TextProblem>()
+      for (job in deferred) {
+        val problems = job.await()
+        for (problem in problems) {
+          if (processProblem(problem, filtered)) {
+            consumer(problem)
+          }
+        }
       }
     }
-
-    return filtered
   }
 
-  fun toProblemDescriptors(problems: List<TextProblem>, isOnTheFly: Boolean): List<ProblemDescriptor> {
+  private fun processProblem(problem: TextProblem, filtered: MutableList<TextProblem>): Boolean {
+    require(problem.text == text)
+
+    if (isSuppressed(problem) ||
+        hasIgnoredCategory(problem) ||
+        isIgnoredByStrategies(problem) ||
+        isIgnoredByFilters(problem)) {
+      return false
+    }
+
+    if (filtered.none { it.highlightRange.intersects(problem.highlightRange) }) {
+      filtered.add(problem)
+      return true
+    }
+    return false
+  }
+
+  fun toProblemDescriptors(problem: TextProblem, isOnTheFly: Boolean): List<ProblemDescriptor> {
     val parent = text.commonParent
-    return problems.flatMap { problem ->
-      fileHighlightRanges(problem).map { range ->
-        ProblemDescriptorBase(
-          parent, parent, problem.getDescriptionTemplate(isOnTheFly),
-          if (isOnTheFly) toFixes(problem) else LocalQuickFix.EMPTY_ARRAY,
-          ProblemHighlightType.GENERIC_ERROR_OR_WARNING, false,
-          range.shiftLeft(parent.startOffset),
-          true, isOnTheFly)
-      }
+    return fileHighlightRanges(problem).map { range ->
+      ProblemDescriptorBase(
+        parent, parent, problem.getDescriptionTemplate(isOnTheFly),
+        if (isOnTheFly) toFixes(problem) else LocalQuickFix.EMPTY_ARRAY,
+        ProblemHighlightType.GENERIC_ERROR_OR_WARNING, false,
+        range.shiftLeft(parent.startOffset),
+        true, isOnTheFly)
     }
   }
 
