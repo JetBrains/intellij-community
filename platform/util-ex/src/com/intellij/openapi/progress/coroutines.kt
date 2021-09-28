b97612c1a5d02bca55eb8c1b6724b61876b4c3e3
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-09-28 14:23:17 +0200

    make `runSuspendingAction` accept not-null `ProgressIndicator`
    This change makes this particular overload handle case wil indicator only.
    The generic overload now handles the case with presence or absence of the indicator.
    
    GitOrigin-RevId: b038aa92227e821a24a76ac4807d75c29e281fb3

--- platform/util-ex/src/com/intellij/openapi/progress/coroutines.kt
+++ platform/util-ex/src/com/intellij/openapi/progress/coroutines.kt
@@ -43,14 +43,14 @@ suspend fun checkCanceled() {
  */
 fun <T> runSuspendingAction(action: suspend CoroutineScope.() -> T): T {
   val indicator = ProgressManager.getGlobalProgressIndicator()
-  return runSuspendingAction(indicator, action)
-}
-
-fun <T> runSuspendingAction(indicator: ProgressIndicator?, action: suspend CoroutineScope.() -> T): T {
   if (indicator == null) {
     // we are not under indicator => just run the action, since nobody will cancel it anyway
     return runBlocking(block = action)
   }
+  return runSuspendingAction(indicator, action)
+}
+
+fun <T> runSuspendingAction(indicator: ProgressIndicator, action: suspend CoroutineScope.() -> T): T {
   // we are under indicator => the Job must be canceled when indicator is canceled
   return runBlocking(progressSinkElement(ProgressIndicatorSink(indicator)) + CoroutineName("indicator run blocking")) {
     val indicatorWatchJob = launch(Dispatchers.Default + CoroutineName("indicator watcher")) {
