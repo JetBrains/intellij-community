e8b9fd391f5398249a73c9325fa8097a756f18ed
Mikhail Shagvaliev <Mikhail.Shagvaliev@jetbrains.com>
2021-09-29 18:38:53 +0300

    [qodana] QD-1313 QD-1273 Moved logic of navigating with "navigate" protocol from JBProtocolNavigateCommandBase to JBProtocolNavigateCommand
    - made JBProtocolNavigateCommandBase interpret line:column as 1-based values of line number and character number in line
    - made JBProtocolNavigateCommand implement JBProtocolCommand
    - add deprecation annotation to JBProtocolNavigateCommandBase
    
    GitOrigin-RevId: a19aa42b5ef0342cc9d882d7978355cf1e80db04

--- platform/lang-impl/src/com/intellij/navigation/JBProtocolNavigateCommand.kt
+++ platform/lang-impl/src/com/intellij/navigation/JBProtocolNavigateCommand.kt
@@ -1,22 +1,51 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.navigation
 
+import com.intellij.openapi.application.JBProtocolCommand
 import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.LogicalPosition
+import com.intellij.openapi.project.Project
 
 private val LOG = logger<JBProtocolNavigateCommand>()
 
-open class JBProtocolNavigateCommand: JBProtocolNavigateCommandBase(NAVIGATE_COMMAND) {
+open class JBProtocolNavigateCommand: JBProtocolCommand(NAVIGATE_COMMAND) {
   companion object {
     const val NAVIGATE_COMMAND = "navigate"
   }
+  /**
+   * The handler parses the following "navigate" command parameters:
+   *
+   * \\?project=(?<project>[\\w]+)
+   *   (&fqn[\\d]*=(?<fqn>[\\w.\\-#]+))*
+   *   (&path[\\d]*=(?<path>[\\w-_/\\\\.]+)
+   *     (:(?<lineNumber>[\\d]+))?
+   *     (:(?<columnNumber>[\\d]+))?)*
+   *   (&selection[\\d]*=
+   *     (?<line1>[\\d]+):(?<column1>[\\d]+)
+   *    -(?<line2>[\\d]+):(?<column2>[\\d]+))*
+   */
 
   override fun perform(target: String, parameters: Map<String, String>) {
     if (target != REFERENCE_TARGET) {
       LOG.warn("JB navigate action supports only reference target, got $target")
       return
     }
-    openProject(parameters) {
+    openProjectWithAction(parameters) {
       findAndNavigateToReference(it, parameters)
+    }
+  }
+
+  private fun findAndNavigateToReference(project: Project, parameters: Map<String, String>) {
+    fun locationToOffset(locationInFile: LocationInFile, editor: Editor) =
+      editor.logicalPositionToOffset(LogicalPosition(locationInFile.line, locationInFile.column))
+
+    val keyPrefixToNavigator = mapOf(
+      (FQN_KEY to ::navigateByFqn),
+      (PATH_KEY to ::navigateByPath)
+    )
+    keyPrefixToNavigator.forEach { (keyPrefix, navigator) ->
+      parameters.filterKeys { it.startsWith(keyPrefix) }.values.forEach { navigator.invoke(project, parameters, it, ::locationToOffset) }
     }
   }
 }
--- platform/lang-impl/src/com/intellij/navigation/JBProtocolNavigateCommandBase.kt
+++ platform/lang-impl/src/com/intellij/navigation/JBProtocolNavigateCommandBase.kt
@@ -5,38 +5,24 @@ import com.intellij.openapi.application.JBProtocolCommand
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.LogicalPosition
 import com.intellij.openapi.project.Project
+import com.intellij.util.containers.ComparatorUtil
 
-
-
+@Deprecated(message = "implement JBProtocolCommand instead. For navigation you can use functions from JBprotocolNavigation")
 abstract class JBProtocolNavigateCommandBase(command: String): JBProtocolCommand(command) {
-  /**
-   * The handler parses the following command parameters:
-   *
-   * \\?project=(?<project>[\\w]+)
-   *   (&fqn[\\d]*=(?<fqn>[\\w.\\-#]+))*
-   *   (&path[\\d]*=(?<path>[\\w-_/\\\\.]+)
-   *     (:(?<lineNumber>[\\d]+))?
-   *     (:(?<columnNumber>[\\d]+))?)*
-   *   (&selection[\\d]*=
-   *     (?<line1>[\\d]+):(?<column1>[\\d]+)
-   *    -(?<line2>[\\d]+):(?<column2>[\\d]+))*
-   */
 
   fun openProject(parameters: Map<String, String>, action: (Project) -> Unit) = openProjectWithAction(parameters, action)
 
   fun findAndNavigateToReference(project: Project, parameters: Map<String, String>) {
-    fun locationToOffset(locationInFile: LocationInFile, editor: Editor) =
-      editor.logicalPositionToOffset(LogicalPosition(locationInFile.line, locationInFile.column))
+    fun locationToOffset(locationInFile: LocationInFile, editor: Editor): Int {
+      val offsetOfLine = editor.logicalPositionToOffset(LogicalPosition(ComparatorUtil.max(locationInFile.line - 1, 0), 0))
+      val offsetInLine = locationInFile.column - 1
+      return ComparatorUtil.max(offsetOfLine + offsetInLine, 0)
+    }
 
-    val keyPrefixToNavigator = mapOf(
-      (FQN_KEY to ::navigateByFqn),
-      (PATH_KEY to ::navigateByPath)
-    )
-    keyPrefixToNavigator.forEach { (keyPrefix, navigator) ->
-      parameters.filterKeys { it.startsWith(keyPrefix) }.values.forEach { navigator.invoke(project, parameters, it, ::locationToOffset) }
+    parameters.filterKeys { it.startsWith(PATH_KEY) }.values.forEach {
+      navigateByPath(project, parameters, it, ::locationToOffset)
     }
   }
 }
-
 
 fun parseNavigatePath(pathText: String) = parseNavigationPath(pathText)
