b498df7716b605308c0ae7e7a33de7187cfa8dc5
Toshiaki Kameyama <kmymtsak@gmail.com>
2021-09-29 12:56:06 +0900

    Remove unnecessary 'if' expression

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/createFromUsage/createCallable/CreateCallableFromCallActionFactory.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/createFromUsage/createCallable/CreateCallableFromCallActionFactory.kt
@@ -140,8 +140,6 @@ sealed class CreateCallableFromCallActionFactory<E : KtExpression>(
     }
 
     protected fun getAbstractCallableInfo(mainCallable: CallableInfo, originalExpression: KtExpression): CallableInfo? {
-        if (mainCallable.isForCompanion) return null
-
         val receiverTypeInfo: TypeInfo
         val receiverType: KotlinType
 
