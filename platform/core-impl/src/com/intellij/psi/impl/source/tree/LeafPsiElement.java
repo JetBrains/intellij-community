3e5bab3b79a1fa9c8c13abe5ac505e1621ba3b7e
Yann Cébron <yann.cebron@jetbrains.com>
2021-09-30 08:42:35 +0200

    Revert "IDEA-279199 LeafPsiElement does not implement NavigatablePsiElement"
    This reverts commit aebb3cef6169195a74ee8da504dcfe53bbdbf2ee.
    
    GitOrigin-RevId: a674bdc2801cc782dc3131b29c7aa437cffe7454

--- platform/core-impl/src/com/intellij/psi/impl/source/tree/LeafPsiElement.java
+++ platform/core-impl/src/com/intellij/psi/impl/source/tree/LeafPsiElement.java
@@ -1,10 +1,26 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+/*
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.intellij.psi.impl.source.tree;
 
 import com.intellij.ide.util.PsiNavigationSupport;
 import com.intellij.lang.ASTNode;
 import com.intellij.lang.Language;
 import com.intellij.navigation.ItemPresentation;
+import com.intellij.navigation.NavigationItem;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.progress.ProgressIndicatorProvider;
 import com.intellij.openapi.project.Project;
@@ -23,7 +39,7 @@ import com.intellij.util.IncorrectOperationException;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
-public class LeafPsiElement extends LeafElement implements NavigatablePsiElement {
+public class LeafPsiElement extends LeafElement implements PsiElement, NavigationItem {
   private static final Logger LOG = Logger.getInstance(LeafPsiElement.class);
 
   public LeafPsiElement(@NotNull IElementType type, CharSequence text) {
