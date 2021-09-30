fefc0c6c5e39322a7d9ceadcd36cf367c3150296
Ivan Semenov <ivan.semenov@jetbrains.com>
2021-09-30 12:27:33 +0300

    [vcs-log] convert containing branches getter to kotlin
    GitOrigin-RevId: 53a2581ac31d07f14146c460af9539e447cc9f6d

--- platform/vcs-log/impl/src/com/intellij/vcs/log/data/ContainingBranchesGetter.kt
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/data/ContainingBranchesGetter.kt
@@ -1,82 +1,70 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.vcs.log.data;
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.vcs.log.data
 
-import com.intellij.openapi.Disposable;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.progress.util.BackgroundTaskUtil;
-import com.intellij.openapi.util.Condition;
-import com.intellij.openapi.vcs.VcsException;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.containers.ContainerUtil;
-import com.intellij.util.containers.SLRUMap;
-import com.intellij.vcs.log.*;
-import com.intellij.vcs.log.graph.PermanentGraph;
-import com.intellij.vcs.log.graph.api.permanent.PermanentGraphInfo;
-import com.intellij.vcs.log.util.SequentialLimitedLifoExecutor;
-import com.intellij.vcs.log.util.StopWatch;
-import org.jetbrains.annotations.CalledInAny;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.awt.*;
-import java.util.List;
-import java.util.*;
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.progress.util.BackgroundTaskUtil
+import com.intellij.openapi.vcs.VcsException
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.util.containers.SLRUMap
+import com.intellij.vcs.log.*
+import com.intellij.vcs.log.graph.impl.facade.PermanentGraphImpl
+import com.intellij.vcs.log.util.SequentialLimitedLifoExecutor
+import com.intellij.vcs.log.util.StopWatch
+import org.jetbrains.annotations.CalledInAny
+import java.awt.EventQueue
 
 /**
  * Provides capabilities to asynchronously calculate "contained in branches" information.
  */
-public class ContainingBranchesGetter {
-  private static final Logger LOG = Logger.getInstance(ContainingBranchesGetter.class);
-
-  @NotNull private final SequentialLimitedLifoExecutor<CachingTask> myTaskExecutor;
-  @NotNull private final VcsLogData myLogData;
+class ContainingBranchesGetter internal constructor(private val logData: VcsLogData, parentDisposable: Disposable) {
+  private val taskExecutor: SequentialLimitedLifoExecutor<CachingTask>
 
   // other fields accessed only from EDT
-  @NotNull private final List<Runnable> myLoadingFinishedListeners = new ArrayList<>();
-  @NotNull private SLRUMap<CommitId, List<String>> myCache = createCache();
-  @NotNull private final CurrentBranchConditionCache myConditionsCache;
-  private int myCurrentBranchesChecksum;
+  private val loadingFinishedListeners: MutableList<Runnable> = ArrayList()
+  private var cache = createCache()
+  private val conditionsCache: CurrentBranchConditionCache
+  private var currentBranchesChecksum = 0
 
-  ContainingBranchesGetter(@NotNull VcsLogData logData, @NotNull Disposable parentDisposable) {
-    myLogData = logData;
-    myConditionsCache = new CurrentBranchConditionCache(logData, parentDisposable);
-    myTaskExecutor = new SequentialLimitedLifoExecutor<>(parentDisposable, 10, CachingTask::run);
-    myLogData.addDataPackChangeListener(dataPack -> {
-      Collection<VcsRef> currentBranches = dataPack.getRefsModel().getBranches();
-      int checksum = currentBranches.hashCode();
-      if (myCurrentBranchesChecksum != 0 && myCurrentBranchesChecksum != checksum) { // clear cache if branches set changed after refresh
-        clearCache();
+  init {
+    conditionsCache = CurrentBranchConditionCache(logData, parentDisposable)
+    taskExecutor = SequentialLimitedLifoExecutor(parentDisposable, 10, CachingTask::run)
+    logData.addDataPackChangeListener { dataPack: DataPack ->
+      val currentBranches = dataPack.refsModel.branches
+      val checksum = currentBranches.hashCode()
+      if (currentBranchesChecksum != 0 && currentBranchesChecksum != checksum) { // clear cache if branches set changed after refresh
+        clearCache()
       }
-      myCurrentBranchesChecksum = checksum;
-    });
+      currentBranchesChecksum = checksum
+    }
   }
 
-  private void clearCache() {
-    myCache = createCache();
-    myTaskExecutor.clear();
-    myConditionsCache.clear();
+  private fun clearCache() {
+    cache = createCache()
+    taskExecutor.clear()
+    conditionsCache.clear()
     // re-request containing branches information for the commit user (possibly) currently stays on
-    ApplicationManager.getApplication().invokeLater(this::notifyListeners);
+    ApplicationManager.getApplication().invokeLater { notifyListeners() }
   }
 
   /**
    * This task will be executed each time the calculating process completes.
    */
-  public void addTaskCompletedListener(@NotNull Runnable runnable) {
-    LOG.assertTrue(EventQueue.isDispatchThread());
-    myLoadingFinishedListeners.add(runnable);
+  fun addTaskCompletedListener(runnable: Runnable) {
+    LOG.assertTrue(EventQueue.isDispatchThread())
+    loadingFinishedListeners.add(runnable)
   }
 
-  public void removeTaskCompletedListener(@NotNull Runnable runnable) {
-    LOG.assertTrue(EventQueue.isDispatchThread());
-    myLoadingFinishedListeners.remove(runnable);
+  fun removeTaskCompletedListener(runnable: Runnable) {
+    LOG.assertTrue(EventQueue.isDispatchThread())
+    loadingFinishedListeners.remove(runnable)
   }
 
-  private void notifyListeners() {
-    LOG.assertTrue(EventQueue.isDispatchThread());
-    for (Runnable listener : myLoadingFinishedListeners) {
-      listener.run();
+  private fun notifyListeners() {
+    LOG.assertTrue(EventQueue.isDispatchThread())
+    for (listener in loadingFinishedListeners) {
+      listener.run()
     }
   }
 
@@ -84,166 +72,124 @@ public class ContainingBranchesGetter {
    * Returns the alphabetically sorted list of branches containing the specified node, if this information is ready;
    * if it is not available, starts calculating in the background and returns null.
    */
-  @Nullable
-  public List<String> requestContainingBranches(@NotNull VirtualFile root, @NotNull Hash hash) {
-    LOG.assertTrue(EventQueue.isDispatchThread());
-    List<String> refs = getContainingBranchesFromCache(root, hash);
+  fun requestContainingBranches(root: VirtualFile, hash: Hash): List<String>? {
+    LOG.assertTrue(EventQueue.isDispatchThread())
+    val refs = getContainingBranchesFromCache(root, hash)
     if (refs == null) {
-      myTaskExecutor.queue(new CachingTask(createTask(root, hash, myLogData.getDataPack()), myCache));
+      taskExecutor.queue(CachingTask(createTask(root, hash, logData.dataPack), cache))
     }
-    return refs;
+    return refs
   }
 
-  @Nullable
-  public List<String> getContainingBranchesFromCache(@NotNull VirtualFile root, @NotNull Hash hash) {
-    LOG.assertTrue(EventQueue.isDispatchThread());
-    return myCache.get(new CommitId(hash, root));
+  fun getContainingBranchesFromCache(root: VirtualFile, hash: Hash): List<String>? {
+    LOG.assertTrue(EventQueue.isDispatchThread())
+    return cache[CommitId(hash, root)]
   }
 
-  @Nullable
-  public List<String> getContainingBranchesQuickly(@NotNull VirtualFile root, @NotNull Hash hash) {
-    LOG.assertTrue(EventQueue.isDispatchThread());
-    CommitId commitId = new CommitId(hash, root);
-    List<String> branches = myCache.get(commitId);
+  fun getContainingBranchesQuickly(root: VirtualFile, hash: Hash): List<String>? {
+    LOG.assertTrue(EventQueue.isDispatchThread())
+    val commitId = CommitId(hash, root)
+    var branches = cache[commitId]
     if (branches == null) {
-      int index = myLogData.getCommitIndex(hash, root);
-      PermanentGraph<Integer> pg = myLogData.getDataPack().getPermanentGraph();
-      if (pg instanceof PermanentGraphInfo) {
-        //noinspection unchecked
-        int nodeId = ((PermanentGraphInfo)pg).getPermanentCommitsInfo().getNodeId(index);
-        if (nodeId < 10000 && canUseGraphForComputation(myLogData.getLogProvider(root))) {
-          branches = getContainingBranchesSynchronously(root, hash);
+      val index = logData.getCommitIndex(hash, root)
+      val pg = logData.dataPack.permanentGraph
+      if (pg is PermanentGraphImpl<Int>) {
+        val nodeId = pg.permanentCommitsInfo.getNodeId(index)
+        branches = if (nodeId < 10000 && canUseGraphForComputation(logData.getLogProvider(root))) {
+          getContainingBranchesSynchronously(root, hash)
         }
         else {
-          branches = BackgroundTaskUtil.tryComputeFast(indicator -> getContainingBranchesSynchronously(root, hash), 100);
+          BackgroundTaskUtil.tryComputeFast({ getContainingBranchesSynchronously(root, hash) }, 100)
         }
-        if (branches != null) myCache.put(commitId, branches);
+        if (branches != null) cache.put(commitId, branches)
       }
     }
-    return branches;
+    return branches
   }
 
   @CalledInAny
-  @NotNull
-  public Condition<Integer> getContainedInCurrentBranchCondition(@NotNull VirtualFile root) {
-    return myConditionsCache.getContainedInCurrentBranchCondition(root);
-  }
-
-  @NotNull
-  private static SLRUMap<CommitId, List<String>> createCache() {
-    return new SLRUMap<>(1000, 1000);
-  }
+  fun getContainedInCurrentBranchCondition(root: VirtualFile) =
+    conditionsCache.getContainedInCurrentBranchCondition(root)
 
   @CalledInAny
-  @NotNull
-  public List<String> getContainingBranchesSynchronously(@NotNull VirtualFile root, @NotNull Hash hash) {
-    return createTask(root, hash, myLogData.getDataPack()).getContainingBranches();
-  }
+  fun getContainingBranchesSynchronously(root: VirtualFile, hash: Hash) =
+    createTask(root, hash, logData.dataPack).getContainingBranches()
 
-  @NotNull
-  private ContainingBranchesGetter.Task createTask(@NotNull VirtualFile root, @NotNull Hash hash, @NotNull DataPack dataPack) {
-    VcsLogProvider provider = myLogData.getLogProvider(root);
-    if (canUseGraphForComputation(provider)) {
-      return new GraphTask(provider, root, hash, dataPack);
+  private fun createTask(root: VirtualFile, hash: Hash, dataPack: DataPack): Task {
+    val provider = logData.getLogProvider(root)
+    return if (canUseGraphForComputation(provider)) {
+      GraphTask(provider, root, hash, dataPack)
     }
-    return new ProviderTask(provider, root, hash);
+    else ProviderTask(provider, root, hash)
   }
 
-  private static boolean canUseGraphForComputation(@NotNull VcsLogProvider logProvider) {
-    return VcsLogProperties.LIGHTWEIGHT_BRANCHES.getOrDefault(logProvider);
-  }
+  private abstract class Task(private val myProvider: VcsLogProvider, val myRoot: VirtualFile, val myHash: Hash) {
 
-  private abstract static class Task {
-    @NotNull private final VcsLogProvider myProvider;
-    @NotNull private final VirtualFile myRoot;
-    @NotNull private final Hash myHash;
-
-    Task(@NotNull VcsLogProvider provider, @NotNull VirtualFile root, @NotNull Hash hash) {
-      myProvider = provider;
-      myRoot = root;
-      myHash = hash;
-    }
-
-    @NotNull
-    public List<String> getContainingBranches() {
-      StopWatch sw = StopWatch.start("get containing branches");
-      try {
-        return getContainingBranches(myProvider, myRoot, myHash);
+    @Throws(VcsException::class)
+    fun getContainingBranches(): List<String> {
+      val sw = StopWatch.start("get containing branches")
+      return try {
+        getContainingBranches(myProvider, myRoot, myHash)
       }
-      catch (VcsException e) {
-        LOG.warn(e);
-        return Collections.emptyList();
+      catch (e: VcsException) {
+        LOG.warn(e)
+        emptyList()
       }
       finally {
-        sw.report();
+        sw.report()
       }
     }
 
-
-    protected abstract @NotNull List<String> getContainingBranches(@NotNull VcsLogProvider provider,
-                                                                   @NotNull VirtualFile root, @NotNull Hash hash) throws VcsException;
+    @Throws(VcsException::class)
+    protected abstract fun getContainingBranches(provider: VcsLogProvider,
+                                                 root: VirtualFile, hash: Hash): List<String>
   }
 
-  private class GraphTask extends Task {
-    @NotNull private final RefsModel myRefs;
-    @NotNull private final PermanentGraph<Integer> myGraph;
+  private inner class GraphTask constructor(provider: VcsLogProvider, root: VirtualFile, hash: Hash, dataPack: DataPack) :
+    Task(provider, root, hash) {
 
-    GraphTask(@NotNull VcsLogProvider provider, @NotNull VirtualFile root, @NotNull Hash hash, @NotNull DataPack dataPack) {
-      super(provider, root, hash);
-      myGraph = dataPack.getPermanentGraph();
-      myRefs = dataPack.getRefsModel();
+    private val graph = dataPack.permanentGraph
+    private val refs = dataPack.refsModel
+
+    override fun getContainingBranches(provider: VcsLogProvider, root: VirtualFile, hash: Hash): List<String> {
+      val commitIndex = logData.getCommitIndex(hash, root)
+      return graph.getContainingBranches(commitIndex)
+        .map(::getBranchesRefs)
+        .flatten()
+        .sortedWith(provider.referenceManager.labelsOrderComparator)
+        .map(VcsRef::getName)
     }
 
-    @Override
-    protected @NotNull List<String> getContainingBranches(@NotNull VcsLogProvider provider, @NotNull VirtualFile root, @NotNull Hash hash) {
-      Set<Integer> branchesIndexes = myGraph.getContainingBranches(myLogData.getCommitIndex(hash, root));
-
-      Collection<VcsRef> branchesRefs = new HashSet<>();
-      for (Integer index : branchesIndexes) {
-        myRefs.refsToCommit(index).stream().filter(ref -> ref.getType().isBranch()).forEach(branchesRefs::add);
-      }
-      branchesRefs = ContainerUtil.sorted(branchesRefs, provider.getReferenceManager().getLabelsOrderComparator());
-
-      ArrayList<String> branchesList = new ArrayList<>();
-      for (VcsRef ref : branchesRefs) {
-        branchesList.add(ref.getName());
-      }
-      return branchesList;
-    }
+    private fun getBranchesRefs(branchIndex: Int) =
+      refs.refsToCommit(branchIndex).filter { it.type.isBranch }
   }
 
-  private static class ProviderTask extends Task {
-
-    ProviderTask(@NotNull VcsLogProvider provider, @NotNull VirtualFile root, @NotNull Hash hash) {
-      super(provider, root, hash);
-    }
-
-    @Override
-    public @NotNull List<String> getContainingBranches(@NotNull VcsLogProvider provider,
-                                                       @NotNull VirtualFile root, @NotNull Hash hash) throws VcsException {
-      List<String> branches = new ArrayList<>(provider.getContainingBranches(root, hash));
-      Collections.sort(branches);
-      return branches;
-    }
+  private class ProviderTask(provider: VcsLogProvider, root: VirtualFile, hash: Hash) : Task(provider, root, hash) {
+    @Throws(VcsException::class)
+    override fun getContainingBranches(provider: VcsLogProvider,
+                                       root: VirtualFile, hash: Hash) =
+      provider.getContainingBranches(root, hash).sorted()
   }
 
-  private class CachingTask {
-    @NotNull private final Task myTask;
-    @NotNull private final SLRUMap<CommitId, List<String>> myCache;
+  private inner class CachingTask(private val delegate: Task, private val cache: SLRUMap<CommitId, List<String>>) {
+    fun run() {
+      val branches = delegate.getContainingBranches()
+      ApplicationManager.getApplication().invokeLater {
 
-    CachingTask(@NotNull Task task, @NotNull SLRUMap<CommitId, List<String>> cache) {
-      myTask = task;
-      myCache = cache;
-    }
-
-    public void run() {
-      List<String> branches = myTask.getContainingBranches();
-      ApplicationManager.getApplication().invokeLater(() -> {
         // if cache is cleared (because of log refresh) during this task execution,
         // this will put obsolete value into the old instance we don't care anymore
-        myCache.put(new CommitId(myTask.myHash, myTask.myRoot), branches);
-        notifyListeners();
-      });
+        cache.put(CommitId(delegate.myHash, delegate.myRoot), branches)
+        notifyListeners()
+      }
     }
+  }
+
+  companion object {
+    private val LOG = Logger.getInstance(ContainingBranchesGetter::class.java)
+
+    private fun createCache(): SLRUMap<CommitId, List<String>> = SLRUMap(1000, 1000)
+
+    private fun canUseGraphForComputation(logProvider: VcsLogProvider) =
+      VcsLogProperties.LIGHTWEIGHT_BRANCHES.getOrDefault(logProvider)
   }
 }
