522dbd9395dfc8931571842d0a5baf9c271b0023
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2021-10-01 17:17:09 +0200

    [packageSearch] optional dependencies should not be used from the main descriptor #PKGS-846 fixed
    (cherry picked from commit 695ac433d99b157e1b59f6a19a6c7b6b2eb6f1a4)
    
    GitOrigin-RevId: 8407cdad94f467dd106de3e54dc7cfa4dace8197

--- plugins/package-search/src/com/jetbrains/packagesearch/intellij/plugin/gradle/configuration/PackageSearchGradleConfiguration.kt
+++ plugins/package-search/src/com/jetbrains/packagesearch/intellij/plugin/gradle/configuration/PackageSearchGradleConfiguration.kt
@@ -4,14 +4,22 @@ import com.intellij.openapi.components.BaseState
 import com.intellij.openapi.components.PersistentStateComponent
 import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
 import com.intellij.util.xmlb.annotations.OptionTag
 import com.jetbrains.packagesearch.intellij.plugin.configuration.PackageSearchGeneralConfiguration
 
 @State(
     name = "PackageSearchGradleConfiguration",
-    storages = [(Storage(PackageSearchGeneralConfiguration.StorageFileName))]
+    storages = [(Storage(PackageSearchGeneralConfiguration.StorageFileName))],
 )
 internal class PackageSearchGradleConfiguration : BaseState(), PersistentStateComponent<PackageSearchGradleConfiguration> {
+
+    companion object {
+
+        @JvmStatic
+        fun getInstance(project: Project) = project.service<PackageSearchGradleConfiguration>()
+    }
 
     override fun getState(): PackageSearchGradleConfiguration = this
 
