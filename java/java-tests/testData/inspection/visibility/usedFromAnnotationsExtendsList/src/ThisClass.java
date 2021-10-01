de9873d15da46a0bad59c08c51ff8138857e7c20
Anna Kozlova <anna.kozlova@jetbrains.com>
2021-10-01 13:00:06 +0200

    [java] visibility inspection: fix class references in annotation list (IDEA-279225)
    GitOrigin-RevId: cb8bebcfdc0bde638a70ee8b67de29893af63033

--- java/java-tests/testData/inspection/visibility/usedFromAnnotationsExtendsList/src/ThisClass.java
+++ java/java-tests/testData/inspection/visibility/usedFromAnnotationsExtendsList/src/ThisClass.java
@@ -1,9 +1,14 @@
 import java.util.ArrayList;
 @SuppressWarnings(ThisClass.PUBLICFINALNAME)
+@Anno(ThisClass.FFF)
 public class ThisClass extends ArrayList<ThisClass.FF> {
     public static final String PUBLICFINALNAME = "stuff";
     public static class FF {}
+    static class FF {}
 
     public static void main(String[] args) {
     }
+}
+@interface Anno {
+  Class<?> value() default String.class; 
 }
