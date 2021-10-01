18a4d0f0added3d22023399119668c5598441857
Lamberto Basti <lamberto.basti@jetbrains.com>
2021-10-01 10:03:47 +0200

    Workaround for PKGS-846
    GitOrigin-RevId: d16946bd8d5482caa08181e21461b40520a604f4

--- plugins/package-search/src/com/jetbrains/packagesearch/intellij/plugin/maven/configuration/PackageSearchMavenConfigurationDefaults.kt
+++ plugins/package-search/src/com/jetbrains/packagesearch/intellij/plugin/maven/configuration/PackageSearchMavenConfigurationDefaults.kt
@@ -2,6 +2,8 @@ package com.jetbrains.packagesearch.intellij.plugin.maven.configuration
 
 internal object PackageSearchMavenConfigurationDefaults {
 
-    const val MavenScopes = "compile,provided,runtime,test,system,import"
+    @JvmField
+    val MavenScopes = listOf("compile", "provided", "runtime", "test", "system", "import")
+
     const val MavenDefaultScope = "compile"
 }
