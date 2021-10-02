dfda62d033c3d2fa764e2c49570be2f617868379
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2021-10-02 22:09:48 +0200

    [testdata] Temporary mute QuickFixMultiModuleTestGenerated $Other.testAddActualTo[Class|TopLevelMember]
    There is no simple fix for KTIJ-19789 so tests temporarily muted
    #KTIJ-19789
    
    GitOrigin-RevId: 35616cdf5b0b143b13c047a3a9bc7864e6c28459

--- plugins/kotlin/idea/tests/testData/multiModuleQuickFix/other/addActualToClass/jvm/jvm.kt
+++ plugins/kotlin/idea/tests/testData/multiModuleQuickFix/other/addActualToClass/jvm/jvm.kt
@@ -1,3 +1,7 @@
-// "Add 'actual' modifier" "true"
+// "Add 'actual' modifier" "false"
+// ACTION: Create test
+// ACTION: Rename file to Foo.kt
+
+// KTIJ-19789 No diagnostic "actual is missing" on top-level class/function/property in IDE
 
 class <caret>Foo
