a981bb2b0964762b71f10d65d218cd80f51f4ee8
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2021-10-03 08:50:13 +0200

    [kotlin] Mitigate invalid module issue
    Mitigate invalid module descriptor / Module <production sources for module light_idea_test_case>is not contained in his own dependencies
    Proper fix requires Kotlin compiler change, coming in 1.6.0
    
    Relates to #KT-48977 #EA-244353
    
    GitOrigin-RevId: 1eef48030f58ad57d4e8b870c621d186354393ed

--- plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/caches/project/VfsCodeBlockModificationListener.kt
+++ plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/caches/project/VfsCodeBlockModificationListener.kt
@@ -1,8 +1,12 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.kotlin.idea.caches.project
 
+import com.intellij.openapi.application.invokeLater
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.openapi.vfs.newvfs.BulkFileListener
+import com.intellij.openapi.vfs.newvfs.events.VFileContentChangeEvent
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent
 import com.intellij.vfs.AsyncVfsEventsListener
 import com.intellij.vfs.AsyncVfsEventsPostProcessor
@@ -10,21 +14,42 @@ import org.jetbrains.kotlin.idea.caches.trackers.KotlinCodeBlockModificationList
 import org.jetbrains.kotlin.idea.core.KotlinPluginDisposable
 import org.jetbrains.kotlin.idea.core.util.runInReadActionWithWriteActionPriority
 import org.jetbrains.kotlin.idea.util.ProjectRootsUtil
+import org.jetbrains.kotlin.idea.util.application.isDispatchThread
+import org.jetbrains.kotlin.idea.util.application.isUnitTestMode
 
 class VfsCodeBlockModificationListener: StartupActivity.Background {
     override fun runActivity(project: Project) {
         val disposable = KotlinPluginDisposable.getInstance(project)
         val kotlinOCBModificationListener = KotlinCodeBlockModificationListener.getInstance(project)
-        AsyncVfsEventsPostProcessor.getInstance().addListener(AsyncVfsEventsListener { events: List<VFileEvent> ->
+        val vfsEventsListener = AsyncVfsEventsListener { events: List<VFileEvent> ->
             val projectRelatedVfsFileChange = events.any { event ->
-                val file = event.takeIf { it.isFromRefresh }?.file ?: return@any false
+                val file = event.takeIf { it.isFromRefresh || it is VFileContentChangeEvent }?.file ?: return@any false
                 runInReadActionWithWriteActionPriority {
                     ProjectRootsUtil.isProjectSourceFile(project, file)
                 } ?: true
             }
             if (projectRelatedVfsFileChange) {
-                kotlinOCBModificationListener.incModificationCount()
+                if (isDispatchThread()) {
+                    kotlinOCBModificationListener.incModificationCount()
+                } else {
+                    // it is not fully correct - OCB has to be triggered ASAP as we detect VFS-change out of IJ that
+                    // can affect Kotlin resolve, we can't do that properly before Kotlin 1.6.0
+                    // see [org.jetbrains.kotlin.descriptors.InvalidModuleNotifier]
+                    invokeLater {
+                        kotlinOCBModificationListener.incModificationCount()
+                    }
+                }
             }
-        }, disposable)
+        }
+        if (isUnitTestMode()) {
+            val connection = project.messageBus.connect(disposable)
+            connection.subscribe(VirtualFileManager.VFS_CHANGES, object : BulkFileListener {
+                override fun after(events: List<VFileEvent>) {
+                    vfsEventsListener.filesChanged(events)
+                }
+            })
+        } else {
+            AsyncVfsEventsPostProcessor.getInstance().addListener(vfsEventsListener, disposable)
+        }
     }
 }
