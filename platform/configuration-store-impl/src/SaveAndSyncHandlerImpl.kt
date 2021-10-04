3da77959e4d49192cbbb15323023ed5dface90db
Maria Filipanova <maria.filipanova@jetbrains.com>
2021-10-04 16:55:21 +0300

    GTW-196: add SaveAndSyncListener.TOPIC and register listeners in xml
    GitOrigin-RevId: 6b825b4f988376f129db455192a20aaed38b9e81

--- platform/configuration-store-impl/src/SaveAndSyncHandlerImpl.kt
+++ platform/configuration-store-impl/src/SaveAndSyncHandlerImpl.kt
@@ -3,10 +3,7 @@ package com.intellij.configurationStore
 
 import com.intellij.CommonBundle
 import com.intellij.conversion.ConversionService
-import com.intellij.ide.FrameStateListener
-import com.intellij.ide.GeneralSettings
-import com.intellij.ide.IdeEventQueue
-import com.intellij.ide.SaveAndSyncHandler
+import com.intellij.ide.*
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.AccessToken
 import com.intellij.openapi.application.Application
@@ -33,6 +30,7 @@ import com.intellij.openapi.vfs.newvfs.NewVirtualFile
 import com.intellij.openapi.vfs.newvfs.RefreshQueue
 import com.intellij.project.stateStore
 import com.intellij.util.SingleAlarm
+import com.intellij.util.application
 import com.intellij.util.concurrency.EdtScheduledExecutorService
 import com.intellij.util.concurrency.annotations.RequiresEdt
 import kotlinx.coroutines.*
@@ -54,6 +52,8 @@ internal class SaveAndSyncHandlerImpl : SaveAndSyncHandler(), Disposable {
   private val saveQueue = ArrayDeque<SaveTask>()
 
   private val currentJob = AtomicReference<Job?>()
+
+  private val eventPublisher = application.messageBus.syncPublisher(SaveAndSyncHandlerListener.TOPIC)
 
   init {
     // add listeners after some delay - doesn't make sense to listen earlier
@@ -104,7 +104,7 @@ internal class SaveAndSyncHandlerImpl : SaveAndSyncHandler(), Disposable {
       }
 
       LOG.runAndLogException {
-        myDispatcher.multicaster.beforeSave(task, forceExecuteImmediately)
+        eventPublisher.beforeSave(task, forceExecuteImmediately)
         saveProjectsAndApp(forceSavingAllSettings = task.forceSavingAllSettings, onlyProject = task.project)
       }
     }
@@ -257,7 +257,7 @@ internal class SaveAndSyncHandlerImpl : SaveAndSyncHandler(), Disposable {
   }
 
   private fun doScheduledRefresh() {
-    myDispatcher.multicaster.beforeRefresh()
+    eventPublisher.beforeRefresh()
     if (canSyncOrSave()) {
       refreshOpenFiles()
     }
