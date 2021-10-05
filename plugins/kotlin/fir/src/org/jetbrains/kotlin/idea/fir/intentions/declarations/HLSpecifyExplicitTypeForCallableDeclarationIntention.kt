bf9a3570858c8c11083cc370de85d33c302bde59
Tianyu Geng <tgeng@google.com>
2021-10-04 19:41:07 -0700

    FIR IDE: improve HLSpecifyExplicitTypeForCallableDeclarationIntention
    * align applicability range with FE1.0
    * align action name with FE1.0
    * shorten fully qualified references
    
    GitOrigin-RevId: 7f8e9eefb72556aaf9b4e3edbc8a74d61d916352

--- plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/fir/intentions/declarations/HLSpecifyExplicitTypeForCallableDeclarationIntention.kt
+++ plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/fir/intentions/declarations/HLSpecifyExplicitTypeForCallableDeclarationIntention.kt
@@ -4,7 +4,7 @@ package org.jetbrains.kotlin.idea.fir.intentions.declarations
 
 import org.jetbrains.kotlin.idea.KotlinBundle
 import org.jetbrains.kotlin.idea.api.applicator.with
-import org.jetbrains.kotlin.idea.fir.api.*
+import org.jetbrains.kotlin.idea.fir.api.AbstractHLIntention
 import org.jetbrains.kotlin.idea.fir.api.applicator.HLApplicabilityRange
 import org.jetbrains.kotlin.idea.fir.api.applicator.inputProvider
 import org.jetbrains.kotlin.idea.fir.applicators.ApplicabilityRanges
@@ -13,11 +13,11 @@ import org.jetbrains.kotlin.psi.*
 
 class HLSpecifyExplicitTypeForCallableDeclarationIntention :
     AbstractHLIntention<KtCallableDeclaration, CallableReturnTypeUpdaterApplicator.Type>(KtCallableDeclaration::class, applicator) {
-    override val applicabilityRange: HLApplicabilityRange<KtCallableDeclaration> = ApplicabilityRanges.SELF
+    override val applicabilityRange: HLApplicabilityRange<KtCallableDeclaration> = ApplicabilityRanges.DECLARATION_WITHOUT_INITIALIZER
 
     override val inputProvider = inputProvider<KtCallableDeclaration, CallableReturnTypeUpdaterApplicator.Type> { declaration ->
         val returnType = declaration.getReturnKtType()
-        val denotableType = returnType.approximateToSuperPublicDenotable() ?: returnType
+        val denotableType = returnType.approximateToSuperPublicDenotableOrSelf()
         with(CallableReturnTypeUpdaterApplicator.Type) { createByKtType(denotableType) }
     }
 
@@ -27,7 +27,13 @@ class HLSpecifyExplicitTypeForCallableDeclarationIntention :
                 if (declaration is KtConstructor<*> || declaration is KtFunctionLiteral) return@isApplicableByPsi false
                 declaration.typeReference == null && (declaration as? KtNamedFunction)?.hasBlockBody() != true
             }
-            familyAndActionName(KotlinBundle.lazyMessage("specify.return.type.explicitly"))
+            familyName(KotlinBundle.lazyMessage("specify.type.explicitly"))
+            actionName { declaration, _ ->
+                when (declaration) {
+                    is KtFunction -> KotlinBundle.message("specify.return.type.explicitly")
+                    else -> KotlinBundle.message("specify.type.explicitly")
+                }
+            }
         }
     }
 }
