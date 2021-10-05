2039bd9daff8697e1a821fd213ebe8087b3e7259
Artemiy Sartakov <artemiy.sartakov@jetbrains.com>
2021-10-05 17:58:36 +0700

    Sealed classes: report lambda conversion to functional interface in case when interface is sealed (IDEA-266915)
    GitOrigin-RevId: ba43f101b672654155b0d7be4f801ea12b79e28c

--- java/java-tests/testData/codeInsight/daemonCodeAnalyzer/advHighlightingSealedTypes/SealedFunctionalInterface.java
+++ java/java-tests/testData/codeInsight/daemonCodeAnalyzer/advHighlightingSealedTypes/SealedFunctionalInterface.java
@@ -2,3 +2,23 @@
 sealed interface I {
   void m();
 }
+sealed interface Fn {
+  int get();
+}
+
+final class MyFn implements Fn {
+  @Override
+  public int get() {
+    return 0;
+  }
+}
+
+class Test {
+
+  void test() {
+    Fn fn = <error descr="Sealed class can not be used as functional interface">() -> 1</error>;
+    foo(<error descr="Sealed class can not be used as functional interface">() -> 1</error>);
+  }
+
+  void foo(Fn fn) { }
+}
