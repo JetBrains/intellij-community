2c9ff0f085184839fcec1b6c80aa534b75fc2d16
Tianyu Geng <tgeng@google.com>
2021-10-04 19:41:07 -0700

    FIR IDE: improve HLSpecifyExplicitTypeForCallableDeclarationIntention
    * align applicability range with FE1.0
    * align action name with FE1.0
    * shorten fully qualified references
    
    GitOrigin-RevId: ce68a66c2423fc8c318a25191d6425f0a6ff39cc

--- plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/fir/applicators/CallableReturnTypeUpdaterApplicator.kt
+++ plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/fir/applicators/CallableReturnTypeUpdaterApplicator.kt
@@ -3,12 +3,13 @@
 package org.jetbrains.kotlin.idea.fir.applicators
 
 import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.analysis.api.KtAnalysisSession
+import org.jetbrains.kotlin.analysis.api.components.KtReferenceShortenerMixIn
+import org.jetbrains.kotlin.analysis.api.components.KtTypeRendererOptions
+import org.jetbrains.kotlin.analysis.api.types.KtType
 import org.jetbrains.kotlin.idea.KotlinBundle
 import org.jetbrains.kotlin.idea.api.applicator.HLApplicatorInput
 import org.jetbrains.kotlin.idea.api.applicator.applicator
-import org.jetbrains.kotlin.analysis.api.KtAnalysisSession
-import org.jetbrains.kotlin.analysis.api.components.KtTypeRendererOptions
-import org.jetbrains.kotlin.analysis.api.types.KtType
 import org.jetbrains.kotlin.idea.util.application.runWriteAction
 import org.jetbrains.kotlin.psi.KtCallableDeclaration
 import org.jetbrains.kotlin.psi.KtFunction
@@ -19,12 +20,14 @@ object CallableReturnTypeUpdaterApplicator {
         familyAndActionName(KotlinBundle.lazyMessage("fix.change.return.type.family"))
 
         applyTo { declaration, type, project ->
-            val newTypeRef = if (!declaration.isProcedure(type)) {
-                // TODO use longTypeRepresentation and then shorten
-                KtPsiFactory(project).createType(type.shortTypeRepresentation)
-            } else null
+            val newTypeRef = if (declaration.isProcedure(type)) {
+                null
+            } else {
+                KtPsiFactory(project).createType(type.longTypeRepresentation)
+            }
             runWriteAction {
                 declaration.typeReference = newTypeRef
+                declaration.typeReference?.let { KtReferenceShortenerMixIn.shortenReferences(it) }
             }
         }
     }
