59b8e2033505d10f4f1cd947eef12b002a5a4be8
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2021-10-05 21:29:43 +0200

    [plugins] refactoring after #IDEA-279171
    GitOrigin-RevId: 4a42a26818a941e82c24b0c4ed24b8672aab2abc

--- platform/platform-impl/src/com/intellij/ide/ui/PluginOptionsTopHitProvider.java
+++ platform/platform-impl/src/com/intellij/ide/ui/PluginOptionsTopHitProvider.java
@@ -1,17 +1,12 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.ui;
 
-import com.intellij.ide.plugins.IdeaPluginDescriptorImpl;
-import com.intellij.ide.plugins.PluginManager;
-import com.intellij.ide.plugins.PluginManagerCore;
+import com.intellij.ide.plugins.PluginManagerConfigurable;
 import com.intellij.ide.ui.search.OptionDescription;
-import com.intellij.openapi.application.ex.ApplicationInfoEx;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Konstantin Bulenkov
@@ -20,22 +15,9 @@ final class PluginOptionsTopHitProvider implements OptionsTopHitProvider.Applica
 
   @Override
   public @NotNull Collection<OptionDescription> getOptions() {
-    ApplicationInfoEx applicationInfo = ApplicationInfoEx.getInstanceEx();
-    List<IdeaPluginDescriptorImpl> descriptors = PluginManagerCore.getPluginSet().allPlugins;
-    List<PluginBooleanOptionDescriptor> options = new ArrayList<>(descriptors.size());
-    boolean hideImplDetails = PluginManager.getInstance().hideImplementationDetails();
-
-    for (IdeaPluginDescriptorImpl descriptor : descriptors) {
-      if (applicationInfo.isEssentialPlugin(descriptor.getPluginId())) {
-        continue;
-      }
-      if (hideImplDetails && descriptor.isImplementationDetail()) {
-        continue;
-      }
-
-      options.add(new PluginBooleanOptionDescriptor(descriptor));
-    }
-    return Collections.unmodifiableList(options);
+    return PluginManagerConfigurable.getVisiblePlugins()
+      .map(PluginBooleanOptionDescriptor::new)
+      .collect(Collectors.toUnmodifiableList());
   }
 
   @Override
