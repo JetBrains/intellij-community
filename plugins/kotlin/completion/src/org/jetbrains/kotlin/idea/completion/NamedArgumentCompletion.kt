60f447b49b55db32e235b1e7b7fc002d45c4928a
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-10-05 12:57:17 +0200

    Named argument completion: don't add trailing comma in case when user tries to manually add name for existing argument
    ^KTIJ-19819 Fixed
    
    GitOrigin-RevId: 151af766beb69aeb86263267d8c7f60a97a519e5

--- plugins/kotlin/completion/src/org/jetbrains/kotlin/idea/completion/NamedArgumentCompletion.kt
+++ plugins/kotlin/completion/src/org/jetbrains/kotlin/idea/completion/NamedArgumentCompletion.kt
@@ -6,11 +6,15 @@ import com.intellij.codeInsight.completion.InsertHandler
 import com.intellij.codeInsight.completion.InsertionContext
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiWhiteSpace
+import com.intellij.psi.util.PsiUtil
+import com.intellij.psi.util.parentOfType
 import org.jetbrains.kotlin.config.LanguageFeature
 import org.jetbrains.kotlin.descriptors.CallableDescriptor
 import org.jetbrains.kotlin.idea.KotlinIcons
 import org.jetbrains.kotlin.idea.caches.resolve.resolveToCall
+import org.jetbrains.kotlin.idea.completion.handlers.WithTailInsertHandler
 import org.jetbrains.kotlin.idea.core.ArgumentPositionData
 import org.jetbrains.kotlin.idea.core.ExpectedInfo
 import org.jetbrains.kotlin.idea.project.languageVersionSettings
@@ -19,12 +23,15 @@ import org.jetbrains.kotlin.idea.util.CallType
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.getStrictParentOfType
+import org.jetbrains.kotlin.psi.psiUtil.parentsWithSelf
 import org.jetbrains.kotlin.psi.psiUtil.siblings
 import org.jetbrains.kotlin.renderer.render
 import org.jetbrains.kotlin.resolve.calls.callUtil.getParameterForArgument
 import org.jetbrains.kotlin.resolve.calls.components.isVararg
 import org.jetbrains.kotlin.resolve.calls.model.ResolvedCall
 import org.jetbrains.kotlin.types.KotlinType
+import org.jetbrains.kotlin.utils.addToStdlib.firstIsInstance
+import org.jetbrains.kotlin.utils.addToStdlib.firstIsInstanceOrNull
 
 object NamedArgumentCompletion {
     fun isOnlyNamedArgumentExpected(nameExpression: KtSimpleNameExpression, resolutionFacade: ResolutionFacade): Boolean {
@@ -63,22 +70,27 @@ object NamedArgumentCompletion {
     private class NamedArgumentInsertHandler(private val parameterName: Name) : InsertHandler<LookupElement> {
         override fun handleInsert(context: InsertionContext, item: LookupElement) {
             val editor = context.editor
+
             val (textAfterCompletionArea, doNeedTrailingSpace) = context.file.findElementAt(context.tailOffset).let { psi ->
                 psi?.siblings()?.firstOrNull { it !is PsiWhiteSpace }?.text to (psi !is PsiWhiteSpace)
             }
 
             var text: String
             var caretOffset: Int
-            when (textAfterCompletionArea) {
-                "=" -> {
-                    text = parameterName.render()
-                    caretOffset = text.length
-                }
-                ")" -> {
-                    text = "${parameterName.render()} = "
-                    caretOffset = text.length
-                }
-                else -> {
+            if (textAfterCompletionArea == "=") {
+                // User tries to manually rename existing named argument. We shouldn't add trailing `=` in such case
+                text = parameterName.render()
+                caretOffset = text.length
+            } else {
+                // For complicated cases let's try to normalize the document firstly in order to avoid parsing errors due to incomplete code
+                editor.document.replaceString(context.startOffset, context.tailOffset, "")
+                PsiDocumentManager.getInstance(context.project).commitDocument(editor.document)
+
+                val nextArgument = context.file.findElementAt(context.startOffset)?.siblings()
+                    ?.firstOrNull { it !is PsiWhiteSpace }?.parentsWithSelf?.takeWhile { it !is KtValueArgumentList }
+                    ?.firstIsInstanceOrNull<KtValueArgument>()
+
+                if (nextArgument?.isNamed() == true) {
                     if (doNeedTrailingSpace) {
                         text = "${parameterName.render()} = , "
                         caretOffset = text.length - 2
@@ -86,6 +98,9 @@ object NamedArgumentCompletion {
                         text = "${parameterName.render()} = ,"
                         caretOffset = text.length - 1
                     }
+                } else {
+                    text = "${parameterName.render()} = "
+                    caretOffset = text.length
                 }
             }
 
