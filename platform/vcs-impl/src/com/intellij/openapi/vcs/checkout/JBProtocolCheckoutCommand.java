151d8267ded9e635f087dedf68cd12feed1d744e
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2021-10-06 15:42:20 +0200

    [vcs] migrating JB protocol 'checkout' command to the new API
    GitOrigin-RevId: 95ee4ad5c4ed327d27f03ee19601a96ac0d0fb9d

--- platform/vcs-impl/src/com/intellij/openapi/vcs/checkout/JBProtocolCheckoutCommand.java
+++ platform/vcs-impl/src/com/intellij/openapi/vcs/checkout/JBProtocolCheckoutCommand.java
@@ -1,17 +1,22 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vcs.checkout;
 
+import com.intellij.ide.IdeBundle;
 import com.intellij.openapi.application.JBProtocolCommand;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
-import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.util.NlsContexts.DialogMessage;
 import com.intellij.openapi.vcs.CheckoutProvider;
 import com.intellij.openapi.vcs.CheckoutProviderEx;
 import com.intellij.openapi.vcs.ProjectLevelVcsManager;
+import com.intellij.openapi.vcs.VcsBundle;
 import com.intellij.ui.AppIcon;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
 
 /**
  * @author Konstantin Bulenkov
@@ -25,21 +30,19 @@ final class JBProtocolCheckoutCommand extends JBProtocolCommand {
 
 
   @Override
-  public void perform(String vcsId, @NotNull Map<String, String> parameters) {
+  public @NotNull Future<@Nullable @DialogMessage String> perform(@Nullable String target, @NotNull Map<String, String> parameters, @Nullable String fragment) {
     String repository = parameters.get(REPOSITORY_NAME_KEY);
+    if (repository == null || repository.isBlank()) return CompletableFuture.completedFuture(IdeBundle.message("ide.protocol.parameter.missing", REPOSITORY_NAME_KEY));
 
-    if (StringUtil.isEmpty(repository)) {
-      return;
-    }
+    CheckoutProviderEx provider = (CheckoutProviderEx)CheckoutProvider.EXTENSION_POINT_NAME.findFirstSafe(
+      it -> it instanceof CheckoutProviderEx && ((CheckoutProviderEx)it).getVcsId().equals(target));
+    if (provider == null) return CompletableFuture.completedFuture(VcsBundle.message("jb.protocol.no.provider", target));
 
-    CheckoutProviderEx provider = (CheckoutProviderEx)CheckoutProvider.EXTENSION_POINT_NAME.findFirstSafe(it -> {
-      return it instanceof CheckoutProviderEx && ((CheckoutProviderEx)it).getVcsId().equals(vcsId);
-    });
-    if (provider != null) {
-      Project project = ProjectManager.getInstance().getDefaultProject();
-      CheckoutProvider.Listener listener = ProjectLevelVcsManager.getInstance(project).getCompositeCheckoutListener();
-      AppIcon.getInstance().requestAttention(null, true);
-      provider.doCheckout(project, listener, repository);
-    }
+    Project project = ProjectManager.getInstance().getDefaultProject();
+    CheckoutProvider.Listener listener = ProjectLevelVcsManager.getInstance(project).getCompositeCheckoutListener();
+    AppIcon.getInstance().requestAttention(null, true);
+    provider.doCheckout(project, listener, repository);
+
+    return CompletableFuture.completedFuture(null);
   }
 }
