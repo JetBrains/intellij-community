5760f42fbae585beef5b842df19522e8a5da52bd
Alexey Kudravtsev <cdr@intellij.com>
2021-10-07 18:00:43 +0200

    remove wrong Nullable
    GitOrigin-RevId: 9f2a5919fdb3d613d3965a966d1c9b3ff1cbec66

--- platform/editor-ui-api/src/com/intellij/ide/util/treeView/NodeDescriptor.java
+++ platform/editor-ui-api/src/com/intellij/ide/util/treeView/NodeDescriptor.java
@@ -3,7 +3,6 @@ package com.intellij.ide.util.treeView;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.NlsSafe;
-import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -65,7 +64,6 @@ public abstract class NodeDescriptor<E> {
     return myName;
   }
 
-  @Nullable
   public final Icon getIcon() {
     return myClosedIcon;
   }
@@ -74,7 +72,6 @@ public abstract class NodeDescriptor<E> {
     return myColor;
   }
 
-  @Nullable
   public final Project getProject() {
     return myProject;
   }
