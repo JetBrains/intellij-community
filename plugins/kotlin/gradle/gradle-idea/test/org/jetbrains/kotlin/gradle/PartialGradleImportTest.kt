4c6965799be193c4dfa33a9058519d3d23195600
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2021-10-07 15:03:08 +0200

    [Kotlin] PartialGradleImportTest: Fixup: Support IDEA 212
    ^KTIJ-19823
    
    GitOrigin-RevId: b07ddcfdb94f894aaba79c65d31ed0ee8f91ca21

--- plugins/kotlin/gradle/gradle-idea/test/org/jetbrains/kotlin/gradle/PartialGradleImportTest.kt
+++ plugins/kotlin/gradle/gradle-idea/test/org/jetbrains/kotlin/gradle/PartialGradleImportTest.kt
@@ -3,13 +3,12 @@ package org.jetbrains.kotlin.gradle
 
 import org.jetbrains.kotlin.idea.codeInsight.gradle.MultiplePluginVersionGradleImportingTestCase
 import org.jetbrains.kotlin.idea.completion.test.assertInstanceOf
-import org.jetbrains.kotlin.idea.gradleJava.scripting.LoadConfigurationAction
-import org.jetbrains.kotlin.idea.gradleJava.scripting.importing.KotlinDslScriptModel
-import org.jetbrains.kotlin.idea.gradleJava.scripting.roots.GradleBuildRoot
-import org.jetbrains.kotlin.idea.gradleJava.scripting.roots.GradleBuildRootsManager
-import org.jetbrains.kotlin.idea.gradleJava.scripting.roots.Imported
-import org.jetbrains.kotlin.idea.gradleJava.scripting.roots.New
-import org.jetbrains.kotlin.idea.gradleJava.scripting.runPartialGradleImport
+import org.jetbrains.kotlin.idea.scripting.gradle.importing.KotlinDslScriptModel
+import org.jetbrains.kotlin.idea.scripting.gradle.roots.GradleBuildRoot
+import org.jetbrains.kotlin.idea.scripting.gradle.roots.GradleBuildRootsManager
+import org.jetbrains.kotlin.idea.scripting.gradle.roots.Imported
+import org.jetbrains.kotlin.idea.scripting.gradle.roots.New
+import org.jetbrains.kotlin.idea.scripting.gradle.runPartialGradleImport
 import org.jetbrains.plugins.gradle.tooling.annotation.PluginTargetVersions
 import org.junit.Test
 import kotlin.test.assertEquals
@@ -21,7 +20,7 @@ class PartialGradleImportTest : MultiplePluginVersionGradleImportingTestCase() {
 
     /**
      * Regular Gradle sync / import should not run in 'classpath' mode to prevent swallowing issues described in:
-     * Only when running the special [LoadConfigurationAction] this special mode shall be used.
+     * Only when running the special [runPartialGradleImport] this special mode shall be used.
      * https://youtrack.jetbrains.com/issue/KT-48823
      * https://youtrack.jetbrains.com/issue/KTIJ-19823
      */
@@ -99,7 +98,7 @@ class PartialGradleImportTest : MultiplePluginVersionGradleImportingTestCase() {
         the Kotlin Gradle plugin was applied
          */
         assertTrue(
-            imported.data.models.flatMap { it.classPath }.any { classPath -> "kotlin" in classPath.lowercase() },
+            imported.data.models.flatMap { it.classPath }.any { classPath -> "kotlin" in classPath.toLowerCase() },
             "Expected 'kotlin' mentioned in imported classPath. Found ${imported.data.models.flatMap { it.classPath }.toSet()}"
         )
     }
