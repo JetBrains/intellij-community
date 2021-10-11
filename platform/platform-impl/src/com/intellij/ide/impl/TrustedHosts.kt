158553c103616cfdde0a72b883740f5bd4507372
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2021-10-11 15:33:11 +0300

    trusted-projects: extract class to a separate file
    (cherry picked from commit 74cb5805df96995c56321246e908ace5db72cb07)
    
    GitOrigin-RevId: dfa7bb5e4c94af5e65a14078acdfff96d7b065fc

--- platform/platform-impl/src/com/intellij/ide/impl/TrustedHosts.kt
+++ platform/platform-impl/src/com/intellij/ide/impl/TrustedHosts.kt
@@ -1,19 +1,13 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.impl
 
-import com.intellij.openapi.components.*
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.util.NlsSafe
 import com.intellij.util.io.URLUtil
-import com.intellij.util.io.URLUtil.SCHEME_SEPARATOR
-import com.intellij.util.io.isAncestor
-import com.intellij.util.xmlb.annotations.OptionTag
 import org.jetbrains.annotations.VisibleForTesting
 import java.net.URI
 import java.nio.file.Path
-import java.nio.file.Paths
-import java.util.*
 
 @NlsSafe
 fun getProjectOriginUrl(projectDir: Path?): String? {
@@ -74,30 +68,6 @@ fun getOriginFromUrl(url: String): Origin? {
     LOG.warn("Couldn't parse URL $url", e)
   }
   return null
-}
-
-@State(name = "Trusted.Paths.Settings", storages = [Storage(value = "trusted-paths.xml", roamingType = RoamingType.DISABLED)])
-@Service(Service.Level.APP)
-internal class TrustedPathsSettings : SimplePersistentStateComponent<TrustedPathsSettings.State>(State()) {
-
-  class State : BaseState() {
-    @get:OptionTag("TRUSTED_PATHS")
-    var trustedPaths by list<String>()
-  }
-
-  fun isPathTrusted(path: Path): Boolean {
-    return state.trustedPaths.map { Paths.get(it) }.any { it.isAncestor(path) }
-  }
-
-  fun getTrustedPaths(): List<String> = Collections.unmodifiableList(state.trustedPaths)
-
-  fun setTrustedPaths(paths: List<String>) {
-    state.trustedPaths = ArrayList<String>(paths)
-  }
-
-  fun addTrustedPath(path: String) {
-    state.trustedPaths.add(path)
-  }
 }
 
 private val LOG = Logger.getInstance("com.intellij.ide.impl.TrustedHosts")
