e3ab0cc6aa02248f9a74341e8f24375c87fa5157
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2021-10-11 15:37:20 +0300

    trusted-projects: cleanup: rename class to identify its purpose more clearly
    Since we don't have the "trusted hosts" conception anymore.
    
    (cherry picked from commit f98314845b719d83f07ec8c64c85a02212816479)
    
    GitOrigin-RevId: 3cb4851e28ac488763070c6f47d2f93be146d554

--- platform/platform-tests/testSrc/com/intellij/ide/impl/ProjectOriginTest.kt
+++ platform/platform-tests/testSrc/com/intellij/ide/impl/ProjectOriginTest.kt
@@ -1,10 +1,10 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.impl
 
 import org.junit.Assert.*
 import org.junit.Test
 
-class TrustedProjectsTest {
+class ProjectOriginTest {
 
   @Test
   fun `origin from url`() {
