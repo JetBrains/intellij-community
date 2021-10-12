0f55007a4bb86568ce4b9b5ad1936438da94d654
Elena.Korotkova <elena.korotkova@jetbrains.com>
2021-10-12 18:23:57 +0300

    New toolbar: fix menu items fix RIDER-68486
    GitOrigin-RevId: e7ab80a675ba1d869967d575c003fa7225115303

--- platform/platform-impl/src/com/intellij/ide/actions/toolbar/experimental/ViewObsoleteNavBarAction.java
+++ platform/platform-impl/src/com/intellij/ide/actions/toolbar/experimental/ViewObsoleteNavBarAction.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.actions.toolbar.experimental;
 
+import com.intellij.ide.ui.ToolbarSettings;
 import com.intellij.ide.ui.UISettings;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.ToggleAction;
@@ -19,5 +20,11 @@ final class ViewObsoleteNavBarAction extends ToggleAction implements DumbAware {
     UISettings uiSettings = UISettings.getInstance();
     uiSettings.setShowNavigationBar(state);
     uiSettings.fireUISettingsChanged();
+  }
+
+  @Override
+  public void update(@NotNull AnActionEvent e) {
+    super.update(e);
+    e.getPresentation().setEnabledAndVisible(ToolbarSettings.getInstance().isEnabled());
   }
 }
