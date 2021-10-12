e071160648fcd9597686a289c493170b7a8f5f71
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2021-10-12 22:03:29 +0300

    IDEA-176014/#29/Ilya: actions to choose a mnemonic and to remove it
    GitOrigin-RevId: 551123a0c77dfd75cec73b5dc5ef3eb840383d5c

--- platform/lang-impl/src/com/intellij/ide/bookmark/actions/NodeChooseTypeAction.kt
+++ platform/lang-impl/src/com/intellij/ide/bookmark/actions/NodeChooseTypeAction.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.bookmark.actions
 
+import com.intellij.ide.bookmark.Bookmark
 import com.intellij.ide.bookmark.BookmarkBundle
 import com.intellij.ide.bookmark.BookmarkType
 import com.intellij.openapi.actionSystem.AnActionEvent
@@ -8,36 +9,30 @@ import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.ui.popup.JBPopupFactory
 import com.intellij.ui.popup.PopupState
 
-internal class ChooseBookmarkTypeAction : DumbAwareAction(BookmarkBundle.messagePointer("mnemonic.chooser.mnemonic.toggle.action.text")) {
+internal class NodeChooseTypeAction : DumbAwareAction(BookmarkBundle.messagePointer("mnemonic.chooser.mnemonic.change.action.text")) {
   private val popupState = PopupState.forPopup()
 
   override fun update(event: AnActionEvent) {
-    val manager = event.bookmarksManager
-    val bookmark = event.contextBookmark
-    val type = bookmark?.let { manager?.getType(it) }
-    event.presentation.apply {
-      isVisible = bookmark != null
-      isEnabled = bookmark != null && popupState.isHidden
-      text = when (type) {
-        BookmarkType.DEFAULT -> BookmarkBundle.message("mnemonic.chooser.mnemonic.assign.action.text")
-        null -> BookmarkBundle.message("mnemonic.chooser.bookmark.create.action.text")
-        else -> BookmarkBundle.message("mnemonic.chooser.mnemonic.change.action.text")
-      }
-    }
+    event.presentation.isEnabledAndVisible = false
+    if (popupState.isShowing) return
+    val manager = event.bookmarksManager ?: return
+    val bookmark = event.bookmarksViewFromToolWindow?.selectedNode?.value as? Bookmark ?: return
+    val type = manager.getType(bookmark) ?: return
+    if (type == BookmarkType.DEFAULT) event.presentation.text = BookmarkBundle.message("mnemonic.chooser.mnemonic.assign.action.text")
+    event.presentation.isEnabledAndVisible = true
   }
 
   override fun actionPerformed(event: AnActionEvent) {
     if (popupState.isRecentlyHidden) return
     val manager = event.bookmarksManager ?: return
-    val bookmark = event.contextBookmark ?: return
-    val type = manager.getType(bookmark)
+    val bookmark = event.bookmarksViewFromToolWindow?.selectedNode?.value as? Bookmark ?: return
+    val type = manager.getType(bookmark) ?: return
     val chooser = BookmarkTypeChooser(type, manager.assignedTypes) {
       popupState.hidePopup()
-      if (it != type) manager.toggle(bookmark, it)
+      manager.setType(bookmark, it)
     }
     val title = when (type) {
       BookmarkType.DEFAULT -> BookmarkBundle.message("mnemonic.chooser.mnemonic.assign.popup.title")
-      null -> BookmarkBundle.message("mnemonic.chooser.bookmark.create.popup.title")
       else -> BookmarkBundle.message("mnemonic.chooser.mnemonic.change.popup.title")
     }
     JBPopupFactory.getInstance().createComponentPopupBuilder(chooser, chooser.buttons().first())
