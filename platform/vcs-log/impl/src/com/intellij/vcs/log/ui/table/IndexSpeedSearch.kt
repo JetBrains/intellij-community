49af497e2ddd3b0d7303292c9e8a2e99829c9cb4
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2021-10-13 16:45:23 +0300

    [vcs-log] convert IndexSpeedSearch to kotlin
    GitOrigin-RevId: a201b86cc2637d763cd9ce243dde469350738a9d

--- platform/vcs-log/impl/src/com/intellij/vcs/log/ui/table/IndexSpeedSearch.kt
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/ui/table/IndexSpeedSearch.kt
@@ -1,125 +1,75 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.vcs.log.ui.table;
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.vcs.log.ui.table
 
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.containers.ContainerUtil;
-import com.intellij.vcs.log.VcsCommitMetadata;
-import com.intellij.vcs.log.VcsLogUserFilter;
-import com.intellij.vcs.log.VcsUser;
-import com.intellij.vcs.log.VcsUserRegistry;
-import com.intellij.vcs.log.data.index.IndexDataGetter;
-import com.intellij.vcs.log.data.index.IndexedDetails;
-import com.intellij.vcs.log.data.index.VcsLogIndex;
-import com.intellij.vcs.log.util.VcsLogUtil;
-import com.intellij.vcs.log.util.VcsUserUtil;
-import com.intellij.vcs.log.visible.VisiblePack;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.vcs.log.*
+import com.intellij.vcs.log.data.index.IndexedDetails.Companion.getSubject
+import com.intellij.vcs.log.data.index.VcsLogIndex
+import com.intellij.vcs.log.util.VcsLogUtil
+import com.intellij.vcs.log.util.VcsUserUtil
+import java.beans.PropertyChangeEvent
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Set;
+open class IndexSpeedSearch(project: Project, private val index: VcsLogIndex, component: VcsLogGraphTable) :
+  VcsLogSpeedSearch(component) {
 
-public class IndexSpeedSearch extends VcsLogSpeedSearch {
-  @NotNull private final VcsLogIndex myIndex;
-  @NotNull private final VcsUserRegistry myUserRegistry;
+  private val userRegistry: VcsUserRegistry
+  private var matchedByUserCommits: Set<Int>? = null
+  private var matchedUsers: Collection<VcsUser>? = null
 
-  @Nullable private Set<Integer> myMatchedByUserCommits;
-  @Nullable private Collection<VcsUser> myMatchedUsers;
-
-  public IndexSpeedSearch(@NotNull Project project, @NotNull VcsLogIndex index, @NotNull VcsLogGraphTable component) {
-    super(component);
-    myIndex = index;
-    myUserRegistry = project.getService(VcsUserRegistry.class);
-
-    addChangeListener(evt -> {
-      if (evt.getPropertyName().equals(ENTERED_PREFIX_PROPERTY_NAME)) {
-        String newValue = (String)evt.getNewValue();
-        IndexDataGetter dataGetter = myIndex.getDataGetter();
+  init {
+    userRegistry = project.getService(VcsUserRegistry::class.java)
+    addChangeListener { evt: PropertyChangeEvent ->
+      if (evt.propertyName == ENTERED_PREFIX_PROPERTY_NAME) {
+        val newValue = evt.newValue as? String
+        val dataGetter = index.dataGetter
         if (newValue != null && dataGetter != null) {
-          String oldValue = (String)evt.getOldValue();
-          Collection<VcsUser> usersToExamine = myUserRegistry.getUsers();
-          if (oldValue != null && myMatchedUsers != null && newValue.contains(oldValue)) {
-            if (myMatchedUsers.isEmpty()) return;
-            usersToExamine = myMatchedUsers;
+          val oldValue = evt.oldValue as? String
+          var usersToExamine: Collection<VcsUser> = userRegistry.users
+          if (oldValue != null && matchedUsers != null && newValue.contains(oldValue)) {
+            if (matchedUsers!!.isEmpty()) return@addChangeListener
+            usersToExamine = matchedUsers!!
           }
-          myMatchedUsers = ContainerUtil.filter(usersToExamine,
-                                                user -> compare(VcsUserUtil.getShortPresentation(user), newValue));
-          myMatchedByUserCommits = dataGetter.filter(Collections.singletonList(new SimpleVcsLogUserFilter(myMatchedUsers)));
+          matchedUsers = usersToExamine.filter { user: VcsUser -> compare(VcsUserUtil.getShortPresentation(user), newValue) }
+          matchedByUserCommits = dataGetter.filter(listOf<VcsLogDetailsFilter>(SimpleVcsLogUserFilter(matchedUsers!!)))
         }
         else {
-          myMatchedByUserCommits = null;
-          myMatchedUsers = null;
+          matchedByUserCommits = null
+          matchedUsers = null
         }
       }
-    });
+    }
   }
 
-  @Override
-  protected boolean isSpeedSearchEnabled() {
+  override fun isSpeedSearchEnabled(): Boolean {
     if (super.isSpeedSearchEnabled()) {
-      VisiblePack visiblePack = myComponent.getModel().getVisiblePack();
-      Set<VirtualFile> roots = visiblePack.getLogProviders().keySet();
-      Set<VirtualFile> visibleRoots = VcsLogUtil.getAllVisibleRoots(roots, visiblePack.getFilters());
-      for (VirtualFile root : visibleRoots) {
-        if (!myIndex.isIndexed(root)) return false;
-      }
-      return true;
+      val visiblePack = myComponent.model.visiblePack
+      return VcsLogUtil.getAllVisibleRoots(visiblePack.logProviders.keys, visiblePack.filters).all { index.isIndexed(it) }
     }
-    return false;
+    return false
   }
 
-  @Nullable
-  @Override
-  protected String getElementText(@NotNull Object row) {
-    throw new UnsupportedOperationException(
-      "Getting row text in a Log is unsupported since we match commit subject and author separately.");
+  override fun getElementText(row: Any): String? {
+    throw UnsupportedOperationException("Getting row text in a Log is unsupported since we match commit subject and author separately.")
   }
 
-  @Nullable
-  private String getCommitSubject(@NotNull Integer row) {
-    IndexDataGetter dataGetter = myIndex.getDataGetter();
-    if (dataGetter != null) {
-      Integer id = myComponent.getModel().getIdAtRow(row);
-      String message = dataGetter.getFullMessage(id);
-      if (message != null) return IndexedDetails.getSubject(message);
-    }
-    return super.getElementText(row);
+  private fun getCommitSubject(row: Int): String? {
+    val dataGetter = index.dataGetter ?: return super.getElementText(row)
+    val id = myComponent.model.getIdAtRow(row)
+    val message = dataGetter.getFullMessage(id) ?: return super.getElementText(row)
+    return getSubject(message)
   }
 
-  @Override
-  protected boolean isMatchingElement(Object row, String pattern) {
-    String str = getCommitSubject((Integer)row);
-    return (str != null && compare(str, pattern)) ||
-           (myMatchedByUserCommits != null &&
-            !myMatchedByUserCommits.isEmpty() &&
-            // getting id from row takes time, so optimizing a little here
-            myMatchedByUserCommits.contains(myComponent.getModel().getIdAtRow((Integer)row)));
+  override fun isMatchingElement(row: Any, pattern: String): Boolean {
+    val str = getCommitSubject(row as Int)
+    return str != null && compare(str, pattern) ||
+           matchedByUserCommits?.isNotEmpty() == true &&  // getting id from row takes time, so optimizing a little here
+           matchedByUserCommits!!.contains(myComponent.model.getIdAtRow(row))
   }
 
-  private static class SimpleVcsLogUserFilter implements VcsLogUserFilter {
-    @NotNull private final Collection<VcsUser> myMatchedUsers;
-
-    SimpleVcsLogUserFilter(@NotNull Collection<VcsUser> matchedUsers) {
-      myMatchedUsers = matchedUsers;
-    }
-
-    @NotNull
-    @Override
-    public Collection<VcsUser> getUsers(@NotNull VirtualFile root) {
-      return myMatchedUsers;
-    }
-
-    @NotNull
-    @Override
-    public Collection<String> getValuesAsText() {
-      return ContainerUtil.map(myMatchedUsers, user -> VcsUserUtil.toExactString(user));
-    }
-
-    @Override
-    public boolean matches(@NotNull VcsCommitMetadata details) {
-      return myMatchedUsers.contains(details.getAuthor());
-    }
+  private class SimpleVcsLogUserFilter(private val users: Collection<VcsUser>) : VcsLogUserFilter {
+    override fun getUsers(root: VirtualFile): Collection<VcsUser> = users
+    override fun getValuesAsText(): Collection<String> = users.map { VcsUserUtil.toExactString(it) }
+    override fun matches(details: VcsCommitMetadata): Boolean = users.contains(details.author)
   }
 }
