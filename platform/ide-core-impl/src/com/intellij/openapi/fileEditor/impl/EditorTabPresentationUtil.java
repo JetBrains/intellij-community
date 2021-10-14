e90b60a7c79e7979b85607882b9b90acd79b1f13
Egor Zhdan <egor.zhdan@jetbrains.com>
2021-10-14 12:21:23 +0300

    OC-21975 Cleanup: remove unused imports
    GitOrigin-RevId: 41fbebdc492bd047cfa0876dd8ee2cce07fd0075

--- platform/ide-core-impl/src/com/intellij/openapi/fileEditor/impl/EditorTabPresentationUtil.java
+++ platform/ide-core-impl/src/com/intellij/openapi/fileEditor/impl/EditorTabPresentationUtil.java
@@ -1,14 +1,12 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.fileEditor.impl;
 
-import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.fileEditor.UniqueVFilePathBuilder;
 import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.NlsContexts;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
