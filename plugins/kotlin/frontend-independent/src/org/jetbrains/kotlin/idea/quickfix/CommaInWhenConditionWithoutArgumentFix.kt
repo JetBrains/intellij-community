81d788bbbce3614bb31a9d2d224ca635bc978661
Tianyu Geng <tgeng@google.com>
2021-10-13 17:58:08 -0700

    FIR IDE: enable CommaInWhenConditionWithoutArgumentFix for FIR
    Also do some minor clean up and change the fix to not to touch a
    condition if it doesn't contain any condition with expression.
    
    GitOrigin-RevId: 201e5991b544a3fa218dee69354fd028ef2d2cdf

--- plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/quickfix/CommaInWhenConditionWithoutArgumentFix.kt
+++ plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/quickfix/CommaInWhenConditionWithoutArgumentFix.kt
@@ -1,4 +1,7 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+/*
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
 
 package org.jetbrains.kotlin.idea.quickfix
 
@@ -6,28 +9,28 @@ import com.intellij.codeInsight.intention.IntentionAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
-import org.jetbrains.kotlin.diagnostics.Diagnostic
+import com.intellij.util.containers.addIfNotNull
 import org.jetbrains.kotlin.idea.KotlinBundle
-import org.jetbrains.kotlin.idea.intentions.branchedTransformations.combineWhenConditions
 import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.*
 
-
-class CommaInWhenConditionWithoutArgumentFix(element: PsiElement) : KotlinQuickFixAction<PsiElement>(element), CleanupFix {
+class CommaInWhenConditionWithoutArgumentFix(element: KtWhenExpression) : KotlinPsiOnlyQuickFixAction<KtWhenExpression>(element),
+                                                                          CleanupFix {
     override fun getFamilyName(): String = text
     override fun getText(): String = KotlinBundle.message("replace.with.in.when")
 
     override fun invoke(project: Project, editor: Editor?, file: KtFile) {
-        val whenExpression = element as? KtWhenExpression ?: return
+        val whenExpression = element ?: return
         replaceCommasWithOrsInWhenExpression(whenExpression)
     }
 
-    companion object : KotlinSingleIntentionActionFactory() {
-        override fun createAction(diagnostic: Diagnostic): IntentionAction? =
-            diagnostic.psiElement.parent?.parent?.let(::CommaInWhenConditionWithoutArgumentFix)
+    companion object : QuickFixesPsiBasedFactory<PsiElement>(PsiElement::class, PsiElementSuitabilityCheckers.ALWAYS_SUITABLE) {
+        override fun doCreateQuickFix(psiElement: PsiElement): List<IntentionAction> {
+            return listOfNotNull((psiElement.parent?.parent as? KtWhenExpression)?.let(::CommaInWhenConditionWithoutArgumentFix))
+        }
 
         private class WhenEntryConditionsData(
-            val conditions: Array<KtWhenCondition>,
+            val conditions: List<KtExpression>,
             val first: PsiElement,
             val last: PsiElement,
             val arrow: PsiElement
@@ -37,7 +40,11 @@ class CommaInWhenConditionWithoutArgumentFix(element: PsiElement) : KotlinQuickF
             for (whenEntry in whenExpression.entries) {
                 if (whenEntry.conditions.size > 1) {
                     val conditionsData = getConditionsDataOrNull(whenEntry) ?: return
-                    val replacement = KtPsiFactory(whenEntry).combineWhenConditions(conditionsData.conditions, null) ?: return
+                    // Leave branch untouched if there are no valid conditions
+                    if (conditionsData.conditions.isEmpty()) continue
+                    val replacement = KtPsiFactory(whenEntry).buildExpression {
+                        appendExpressions(conditionsData.conditions, separator = "||")
+                    }
                     whenEntry.deleteChildRange(conditionsData.first, conditionsData.last)
                     whenEntry.addBefore(replacement, conditionsData.arrow)
                 }
@@ -45,7 +52,7 @@ class CommaInWhenConditionWithoutArgumentFix(element: PsiElement) : KotlinQuickF
         }
 
         private fun getConditionsDataOrNull(whenEntry: KtWhenEntry): WhenEntryConditionsData? {
-            val conditions = ArrayList<KtWhenCondition>()
+            val conditions = mutableListOf<KtExpression>()
 
             var arrow: PsiElement? = null
 
@@ -53,7 +60,7 @@ class CommaInWhenConditionWithoutArgumentFix(element: PsiElement) : KotlinQuickF
             whenEntryChildren@ while (child != null) {
                 when {
                     child is KtWhenConditionWithExpression -> {
-                        conditions.add(child)
+                        conditions.addIfNotNull(child.expression)
                     }
                     child.node.elementType == KtTokens.ARROW -> {
                         arrow = child
@@ -66,11 +73,9 @@ class CommaInWhenConditionWithoutArgumentFix(element: PsiElement) : KotlinQuickF
             val last = child?.prevSibling
 
             return if (arrow != null && last != null)
-                WhenEntryConditionsData(conditions.toTypedArray(), whenEntry.firstChild, last, arrow)
+                WhenEntryConditionsData(conditions, whenEntry.firstChild, last, arrow)
             else
                 null
         }
-
     }
-
 }
