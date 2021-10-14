a7e5536d0910edc3c261eed8f536142ea69b0549
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2021-10-14 22:26:18 +0200

    [kotlin] Address the problem of referencing private kotlin classes from java code
    Relates to #KTIJ-8710
    
    GitOrigin-RevId: f7211553d5ec729e8040d3c6c1f8bf65c0461088

--- plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/search/ideaExtensions/KotlinDeclarationScopeOptimizer.kt
+++ plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/search/ideaExtensions/KotlinDeclarationScopeOptimizer.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.kotlin.idea.search.ideaExtensions
 
+import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.psi.PsiElement
 import com.intellij.psi.search.*
 import com.intellij.psi.util.PsiTreeUtil
@@ -17,9 +18,12 @@ class KotlinDeclarationScopeOptimizer: ScopeOptimizer {
         val ktClassOrObject =
             PsiTreeUtil.getParentOfType(declaration, KtClassOrObject::class.java)?.takeIf { it.isPrivate() } ?: return null
         val containingFile = ktClassOrObject.containingKtFile
-        val packageScope = GlobalSearchScopesCore.directoryScope(element.project, containingFile.virtualFile.parent, false)
-            .excludeKotlinSources()
 
-        return containingFile.fileScope().union(packageScope)
+        val fileScope = containingFile.fileScope()
+
+        val projectFileIndex = ProjectFileIndex.SERVICE.getInstance(element.project)
+        // it is possible to create new kotlin private class from java - so have to look up in the same module as well
+        val moduleScope = projectFileIndex.getModuleForFile(containingFile.virtualFile)?.moduleScope?.excludeKotlinSources()
+        return moduleScope?.let { fileScope.union(moduleScope) } ?: fileScope
     }
 }
