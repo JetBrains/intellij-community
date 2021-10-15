d97349cd5548686f6a3d47b8312a873dac3317a5
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2021-10-15 11:44:43 +0300

    Add JavaDoc
    GitOrigin-RevId: 17d6fbcebaba8e197fb5d51ef0bd6cf473e4c7e8

--- platform/code-style-api/src/com/intellij/psi/impl/source/codeStyle/PostFormatProcessor.java
+++ platform/code-style-api/src/com/intellij/psi/impl/source/codeStyle/PostFormatProcessor.java
@@ -8,12 +8,36 @@ import com.intellij.psi.PsiFile;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * The processor is run after core formatter primarily to handle non-whitespace changes, for example to add code block braces and other
+ * elements. If only whitespace changes are allowed, for example when ad hoc formatting of changed PSI elements is performed, the
+ * processor is not called.
+ */
 public interface PostFormatProcessor {
   ExtensionPointName<PostFormatProcessor> EP_NAME = ExtensionPointName.create("com.intellij.postFormatProcessor");
 
+  /**
+   * Process the given source element and returns its formatted equivalent.
+   *
+   * @param source   The source element to format.
+   * @param settings The root code style settings to use.
+   *
+   * @return The resulting element containing necessary changes. Note: the element must be valid! Thus, if there are PSI modifications
+   * which do not invalidate the source element, the same source element can be returned. But in other cases it should be a valid
+   * element equivalent from the updated PSI tree.
+   */
   @NotNull
   PsiElement processElement(@NotNull PsiElement source, @NotNull CodeStyleSettings settings);
 
+  /**
+   * Process the source PSI file within the given text range.
+   *
+   * @param source          The source PSI file.
+   * @param rangeToReformat The range within which the changes can be made.
+   * @param settings        The root code style settings to use.
+   *
+   * @return The updated text range after the changes.
+   */
   @NotNull
   TextRange processText(@NotNull PsiFile source, @NotNull TextRange rangeToReformat, @NotNull CodeStyleSettings settings);
 }
