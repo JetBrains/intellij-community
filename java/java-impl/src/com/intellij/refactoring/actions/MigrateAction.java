04c86b281a38d37ab438879bfaac6f876500c44b
Louis Vignier <louis.vignier@jetbrains.com>
2021-10-15 17:11:03 +0200

    IDEA-271404 [java] Revamp migrations UI
    - Add 'Create New Migration…' action
    - Migrate MigrationDialog & EditMigrationDialog UI to the new UI DSL
    - Add migration maps as menu items
    
    #IDEA-271404 Fixed
    
    GitOrigin-RevId: 26f93c412fcd31171f174cf3a3c6cb6d33500e8d

--- java/java-impl/src/com/intellij/refactoring/actions/MigrateAction.java
+++ java/java-impl/src/com/intellij/refactoring/actions/MigrateAction.java
@@ -1,24 +1,28 @@
-
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.refactoring.actions;
 
 import com.intellij.openapi.actionSystem.ActionPlaces;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.refactoring.RefactoringManager;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+
 public class MigrateAction extends AnAction {
+
   @Override
   public void actionPerformed(@NotNull AnActionEvent e) {
     Project project = e.getProject();
-    RefactoringManager.getInstance(project).getMigrateManager().showMigrationDialog();
+    if (project == null) return;
+    RefactoringManager.getInstance(project).getMigrateManager().createNewMigration();
   }
 
   @Override
-  public void update(@NotNull AnActionEvent event){
+  public void update(@NotNull AnActionEvent event) {
     Presentation presentation = event.getPresentation();
     Project project = event.getProject();
     presentation.setEnabled(project != null);
