b4dc4c92186c820761850a642215f348779ca29c
Andrey Sokolov <andrey.sokolov@jetbrains.com>
2021-10-18 19:27:49 +0300

    IDEA-280606 Find usages statistics: provide a session id to have an ability to analyze the session in statistics
    usage.view increase collector version
    
    IJ-CR-15403
    
    (cherry picked from commit 1df995a061f9a3750a422a15854dd9e66ed42894)
    
    GitOrigin-RevId: 37d41cfd13e9849f64721d9f9ff8c29ea0521a51

--- platform/usageView/src/com/intellij/usages/impl/UsageViewStatisticsCollector.kt
+++ platform/usageView/src/com/intellij/usages/impl/UsageViewStatisticsCollector.kt
@@ -33,7 +33,7 @@ class UsageViewStatisticsCollector : CounterUsagesCollector() {
   override fun getGroup() = GROUP
 
   companion object {
-    val GROUP = EventLogGroup("usage.view", 2)
+    val GROUP = EventLogGroup("usage.view", 3)
     private var sessionId = AtomicInteger(0)
     private val SESSION_ID = EventFields.Int("id")
     private val REFERENCE_CLASS = EventFields.Class("reference_class")
