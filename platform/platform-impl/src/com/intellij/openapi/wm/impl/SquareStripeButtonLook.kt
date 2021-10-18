b919d0bc0d4d31eb221647f1abd45ebf120caa31
Konstantin Bulenkov <kb@jetbrains.com>
2021-10-18 11:41:42 +0300

    ExpUI: new square button look for tool windows
    GitOrigin-RevId: 66404a846684c698020c2b3e1a322282057c0be7

---
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/SquareStripeButtonLook.kt
@@ -0,0 +1,26 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.wm.impl
+
+import com.intellij.openapi.actionSystem.impl.IdeaActionButtonLook
+import java.awt.Color
+import java.awt.Graphics
+import java.awt.Rectangle
+
+/**
+ * @author Konstantin Bulenkov
+ */
+class SquareStripeButtonLook(button: SquareStripeButton) : IdeaActionButtonLook() {
+  override fun paintLookBackground(g: Graphics, rect: Rectangle, color: Color) {
+    super.paintLookBackground(g, toSquareButtonRect(rect), color)
+  }
+
+  private fun toSquareButtonRect(rect: Rectangle): Rectangle {
+    val off = 5
+    return Rectangle(rect.x + 5, rect.y + 5, rect.width - 2 * off, rect.height - 2 * off)
+  }
+
+  override fun paintLookBorder(g: Graphics, rect: Rectangle, color: Color) {
+    super.paintLookBorder(g, toSquareButtonRect(rect), color)
+  }
+}
