322aa580391df9cb246d16426ad392c705335675
Tianyu Geng <tgeng@google.com>
2021-10-19 14:52:06 -0700

    FE independent: move RemovePsiElementSimpleFix to fe-independent
    GitOrigin-RevId: e83f0741d4b2fa8d26ce16b4614f26b27401d4ae

--- plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/util/CommentSaver.kt
+++ plugins/kotlin/frontend-independent/src/org/jetbrains/kotlin/idea/util/CommentSaver.kt
@@ -1,4 +1,7 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+/*
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
 
 package org.jetbrains.kotlin.idea.util
 
