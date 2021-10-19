968d4501f8d1c9587cf019cfa91bc71b9e0fe3e8
Konstantin Kolosovsky <konstantin.kolosovsky@jetbrains.com>
2021-10-19 14:57:13 +0300

    [github] Mark pull request files as viewed on diff opening (IDEA-242846)
    GitOrigin-RevId: 67e162d1aa060b5fd826eca88e7f4a103ce437b8

--- plugins/github/src/org/jetbrains/plugins/github/pullrequest/ui/changes/GHPRDiffRequestChainProducer.kt
+++ plugins/github/src/org/jetbrains/plugins/github/pullrequest/ui/changes/GHPRDiffRequestChainProducer.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.github.pullrequest.ui.changes
 
 import com.intellij.diff.chains.AsyncDiffRequestChain
@@ -38,11 +38,12 @@ import org.jetbrains.plugins.github.util.DiffRequestChainProducer
 import org.jetbrains.plugins.github.util.GHToolbarLabelAction
 import java.util.concurrent.CompletableFuture
 
-class GHPRDiffRequestChainProducer(private val project: Project,
-                                   private val dataProvider: GHPRDataProvider,
-                                   private val avatarIconsProvider: GHAvatarIconsProvider,
-                                   private val currentUser: GHUser)
-  : DiffRequestChainProducer {
+open class GHPRDiffRequestChainProducer(
+  private val project: Project,
+  private val dataProvider: GHPRDataProvider,
+  private val avatarIconsProvider: GHAvatarIconsProvider,
+  private val currentUser: GHUser
+) : DiffRequestChainProducer {
 
   override fun getRequestChain(changes: ListSelection<Change>): DiffRequestChain {
     val changesData = dataProvider.changesData
@@ -53,13 +54,17 @@ class GHPRDiffRequestChainProducer(private val project: Project,
     return object : AsyncDiffRequestChain() {
       override fun loadRequestProducers(): ListSelection<out DiffRequestProducer> {
         return changes.map { change ->
-          val changeDataKeys = loadRequestDataKeys(ProgressManager.getInstance().progressIndicator, change,
-                                                   changesProviderFuture, fetchFuture)
-          ChangeDiffRequestProducer.create(project, change, changeDataKeys)
+          val indicator = ProgressManager.getInstance().progressIndicator
+          val changeDataKeys = loadRequestDataKeys(indicator, change, changesProviderFuture, fetchFuture)
+          val customDataKeys = createCustomContext(change)
+
+          ChangeDiffRequestProducer.create(project, change, changeDataKeys + customDataKeys)
         }
       }
     }
   }
+
+  protected open fun createCustomContext(change: Change): Map<Key<*>, Any> = emptyMap()
 
   private fun loadRequestDataKeys(indicator: ProgressIndicator,
                                   change: Change,
