466046eab4b22f4ef331e67612b05d6034468b94
Tianyu Geng <tgeng@google.com>
2021-10-18 17:20:06 -0700

    FIR IDE: test ref shortener for annotations on property accessor
    GitOrigin-RevId: c2be8c10b46095231cea5708b7302bdd9b684e1b

---
+++ plugins/kotlin/idea/tests/testData/shortenRefsFir/calls/propertiesDeclaredInPrimaryConstructor_accessorAnnotation.kt
@@ -0,0 +1,6 @@
+// FIR_COMPARISON
+package some
+
+annotation class SomeAnnotation
+
+class Complete(<selection>@set:some.SomeAnnotation var field1: Int, @set:some.SomeAnnotation var field2: Int</selection>)
