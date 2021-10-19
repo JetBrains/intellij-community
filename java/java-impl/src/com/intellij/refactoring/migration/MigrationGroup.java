c6ca38b2db82ffbe63271e20ec0fe435d5e52a70
Louis Vignier <louis.vignier@jetbrains.com>
2021-10-19 10:45:47 +0200

    IJ-CR-15365 [java] Fix migration actions
    GitOrigin-RevId: aa680e5972bea0096e995e4bf2e759397933f752

--- java/java-impl/src/com/intellij/refactoring/migration/MigrationGroup.java
+++ java/java-impl/src/com/intellij/refactoring/migration/MigrationGroup.java
@@ -1,9 +1,7 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.refactoring.migration;
 
-import com.intellij.openapi.actionSystem.ActionGroup;
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.project.Project;
 import com.intellij.refactoring.RefactoringManager;
 import org.jetbrains.annotations.NotNull;
@@ -29,5 +27,13 @@ public class MigrationGroup extends ActionGroup {
     }
 
     return availableMigrations.toArray(AnAction[]::new);
+  }
+
+  @Override
+  public void update(@NotNull AnActionEvent event) {
+    Presentation presentation = event.getPresentation();
+    Project project = event.getProject();
+    presentation.setEnabled(project != null);
+    presentation.setVisible(!ActionPlaces.isPopupPlace(event.getPlace()));
   }
 }
