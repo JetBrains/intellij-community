b4c350ee0471d2012208768739fa34485e7309f6
Roman Ivanov <roman.ivanov@jetbrains.com>
2021-10-20 16:38:53 +0200

    [java] docs for CustomExceptionHandler
    (cherry picked from commit 2c72dbb3def475cd43ab91fe88f2ec5d95a0cf47)
    
    IJ-CR-15987
    
    GitOrigin-RevId: 20238372317960500c6ad0958f9c7598bdb553ec

--- java/java-psi-impl/src/com/intellij/codeInsight/CustomExceptionHandler.java
+++ java/java-psi-impl/src/com/intellij/codeInsight/CustomExceptionHandler.java
@@ -6,8 +6,19 @@ import com.intellij.psi.PsiElement;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * Allow to specify that a given element handles given exception.
+ *
+ * Such elements won't be highlighted as unhandled.
+ */
 public abstract class CustomExceptionHandler {
   public static final ExtensionPointName<CustomExceptionHandler> KEY = ExtensionPointName.create("com.intellij.custom.exception.handler");
 
+  /**
+   * Checks if the exception produced by element
+   * @param element place which produces exception (for example {@link com.intellij.psi.PsiCall})
+   * @param exceptionType type of produced exception
+   * @param topElement element at which exception should be handled
+   */
   public abstract boolean isHandled(@Nullable PsiElement element, @NotNull PsiClassType exceptionType, PsiElement topElement);
 }
