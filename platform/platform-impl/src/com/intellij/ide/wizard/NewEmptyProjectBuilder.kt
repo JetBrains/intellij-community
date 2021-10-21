1daa834a751a93b6878a29f8d97f9d186fbb94d2
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2021-10-21 20:55:10 +0300

    using GeneralModuleType in new wizard
    GitOrigin-RevId: 3e6c956ad1124038b308ab97e4b805a8c1b78264

--- platform/platform-impl/src/com/intellij/ide/wizard/NewEmptyProjectBuilder.kt
+++ platform/platform-impl/src/com/intellij/ide/wizard/NewEmptyProjectBuilder.kt
@@ -2,7 +2,10 @@
 package com.intellij.ide.wizard
 
 import com.intellij.ide.util.projectWizard.WizardContext
+import com.intellij.openapi.module.GeneralModuleType
+import com.intellij.openapi.project.Project
 import com.intellij.ui.UIBundle
+import com.intellij.ui.dsl.builder.Panel
 import com.intellij.util.ui.EmptyIcon
 import javax.swing.Icon
 
@@ -11,5 +14,15 @@ class NewEmptyProjectBuilder : AbstractNewProjectWizardBuilder() {
   override fun getDescription() = UIBundle.message("label.project.wizard.empty.project.generator.description")
   override fun getNodeIcon(): Icon = EmptyIcon.ICON_0
 
-  override fun createStep(context: WizardContext) = NewProjectWizardBaseStep(context)
+  override fun createStep(context: WizardContext): NewProjectWizardStep {
+    return NewProjectWizardBaseStep(context).chain {
+      object : AbstractNewProjectWizardStep(it) {
+        override fun setupUI(builder: Panel) {}
+
+        override fun setupProject(project: Project) {
+          GeneralModuleType.INSTANCE.createModuleBuilder().commit(project)
+        }
+      }
+    }
+  }
 }
