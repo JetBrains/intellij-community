b2e68e9c3f25ac6376b56115a5917421fa1ae7e0
Bas Leijdekkers <basleijdekkers@gmail.com>
2021-10-21 16:44:20 +0200

    [global inspections] fix possible race condition in "Declaration can have final modifier" inspection
    (cherry picked from commit ebdb415792e56604ecca93a9030da4dbedd936f1)
    
    IJ-CR-16417
    
    GitOrigin-RevId: 639b68ac80fc8156d172a45833de35637b9afd03

--- java/java-analysis-impl/src/com/intellij/codeInspection/canBeFinal/CanBeFinalAnnotator.java
+++ java/java-analysis-impl/src/com/intellij/codeInspection/canBeFinal/CanBeFinalAnnotator.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInspection.canBeFinal;
 
 import com.intellij.codeInsight.daemon.impl.analysis.JavaHighlightUtil;
@@ -45,6 +31,9 @@ class CanBeFinalAnnotator extends RefGraphAnnotatorEx {
   @Override
   public void onInitialize(RefElement refElement) {
     ((RefElementImpl)refElement).setFlag(true, CAN_BE_FINAL_MASK);
+  }
+
+  public void mark(RefElement refElement) {
     if (refElement instanceof RefClass) {
       final RefClass refClass = (RefClass)refElement;
       final UClass psiClass = refClass.getUastElement();
@@ -126,6 +115,7 @@ class CanBeFinalAnnotator extends RefGraphAnnotatorEx {
 
   @Override
   public void onReferencesBuild(RefElement refElement) {
+    mark(refElement);
     if (refElement instanceof RefClass) {
       final PsiClass psiClass = ObjectUtils.tryCast(refElement.getPsiElement(), PsiClass.class);
       if (psiClass != null) {
