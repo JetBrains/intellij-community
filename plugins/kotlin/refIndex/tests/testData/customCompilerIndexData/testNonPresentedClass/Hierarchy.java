98b1604874a3b3963bcfd42734c628ed93bd7ab5
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-10-25 16:12:08 +0700

    [kotlin] cri: remove redundant comments from testData
    ^KTIJ-16709
    
    GitOrigin-RevId: 2ab315f2225767ee0f5a31e7c3febca0b3023bc2

--- plugins/kotlin/refIndex/tests/testData/customCompilerIndexData/testNonPresentedClass/Hierarchy.java
+++ plugins/kotlin/refIndex/tests/testData/customCompilerIndexData/testNonPresentedClass/Hierarchy.java
@@ -22,39 +22,4 @@ public class Hierarchy extends Proxy {
 
     this.extension("awd");
   }
-  
-  //void companionMembers() {
-  //  Companion.getCompanionproperty();
-  //
-  //  int companionfieldProperty1 = companionfieldProperty;
-  //
-  //  getCompanionstaticProperty();
-  //
-  //  int companionconstProperty1 = companionconstProperty;
-  //
-  //  Companion.getCompanionvariable();
-  //  Companion.setCompanionvariable(4);
-  //
-  //  int companionfieldVariable1 = companionfieldVariable;
-  //  companionfieldVariable = 4;
-  //
-  //  getCompanionstaticVariable();
-  //  setCompanionstaticVariable(4);
-  //
-  //  Custom companionlateinitVariable = KotlinOnlyClass.companionlateinitVariable;
-  //  Companion.getCompanionlateinitVariable();
-  //  Companion.setCompanionlateinitVariable(companionlateinitVariable);
-  //
-  //  Custom companionlateinitStaticVariable = KotlinOnlyClass.companionlateinitStaticVariable;
-  //  getCompanionlateinitStaticVariable();
-  //  setCompanionlateinitStaticVariable(companionlateinitStaticVariable);
-  //
-  //  Companion.companionsimpleFunction(4);
-  //
-  //  companionstaticFunction(4);
-  //
-  //  Companion.companionextension("");
-  //
-  //  companionstaticExtension("42");
-  //}
 }
