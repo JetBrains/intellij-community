163f6c582928661609c678fe3804681d6d2c8d5b
Jinseong Jeon <jsjeon@google.com>
2021-10-25 23:33:30 +0200

    UAST: adjust test data for bitwise operators
    so that variables `one` and `zero` literally have values 1 and 0.
    This way, we can avoid div-by-zero error during expression evaluation.
    
    (cherry picked from commit 47513b5ab73ffaacbb2a9b35d758845472064608)
    
    GitOrigin-RevId: 612873657025ae5fdf967bcf227039b399e106b4

--- plugins/kotlin/uast/uast-kotlin/tests/test/org/jetbrains/uast/test/kotlin/comparison/AbstractFE1LegacyUastValuesTest.kt
+++ plugins/kotlin/uast/uast-kotlin/tests/test/org/jetbrains/uast/test/kotlin/comparison/AbstractFE1LegacyUastValuesTest.kt
@@ -3,19 +3,6 @@
 package org.jetbrains.uast.test.kotlin.comparison
 
 import org.jetbrains.uast.test.common.kotlin.LegacyUastValuesTestBase
-import java.nio.file.Paths
-import kotlin.io.path.ExperimentalPathApi
-import kotlin.io.path.absolute
 
-abstract class AbstractFE1LegacyUastValuesTest : AbstractFE1UastValuesTest(), LegacyUastValuesTestBase {
-    // TODO: better not to see exceptions from legacy UAST
-    @OptIn(ExperimentalPathApi::class)
-    private val whitelist: Set<String> = listOf(
-        // TODO: div-by-zero error!
-        "uast-kotlin/tests/testData/Bitwise.kt",
-    ).mapTo(mutableSetOf()) { Paths.get("..").resolve(it).absolute().normalize().toString() }
 
-    override fun isExpectedToFail(filePath: String): Boolean {
-        return filePath in whitelist
-    }
-}
+abstract class AbstractFE1LegacyUastValuesTest : AbstractFE1UastValuesTest(), LegacyUastValuesTestBase
