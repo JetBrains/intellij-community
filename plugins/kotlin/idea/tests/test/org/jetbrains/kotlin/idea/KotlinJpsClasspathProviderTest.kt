ec32cc439bf1d1e27b863be0a614d8411be36d68
Aleksei.Cherepanov <aleksei.cherepanov@jetbrains.com>
2021-10-26 16:48:38 +0300

    Fix KotlinJpsClasspathProviderTest after splitting kotlin-compiler-for-ide into 3 parts
    GitOrigin-RevId: 15f4e35a42a446a946c39961bd492a48a590ae42

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/KotlinJpsClasspathProviderTest.kt
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/KotlinJpsClasspathProviderTest.kt
@@ -8,15 +8,25 @@ import java.io.File
 
 @RunWith(JUnit38ClassRunner::class)
 class KotlinJpsClasspathProviderTest : LightPlatformTestCase() {
+    private val KOTLIN_REFLECT_PREFIX = "kotlin-reflect"
+    private val KOTLIN_COMPILER_COMMON_FOR_IDE_PREFIX = "kotlin-compiler-common-for-ide"
+    private val KOTLIN_COMPILER_FE10_FOR_IDE_PREFIX = "kotlin-compiler-fe10-for-ide"
+    private val KOTLIN_COMPILER_IR_FOR_IDE_PREFIX = "kotlin-compiler-ir-for-ide"
 
     fun testGetClassPath() {
         val inst = KotlinJpsClasspathProvider()
-
         val jars = inst.classPath
 
-        assertEquals(3, jars.size)
-        assertTrue(jars[0], File(jars[0]!!).name.startsWith("kotlin-reflect"))
-        assertTrue(jars[1], File(jars[1]!!).name.startsWith("kotlin-compiler-for-ide") || File(jars[1]!!).name == "kotlin-plugin.jar")
-        assertTrue(jars[2], File(jars[2]!!).name == "intellij.platform.core")
+        assertEquals(4, jars.size)
+        assertJar(jars[0], KOTLIN_REFLECT_PREFIX)
+        assertJar(jars[1], KOTLIN_COMPILER_COMMON_FOR_IDE_PREFIX)
+        assertJar(jars[2], KOTLIN_COMPILER_FE10_FOR_IDE_PREFIX)
+        assertJar(jars[3], KOTLIN_COMPILER_IR_FOR_IDE_PREFIX)
+    }
+
+    private fun assertJar(jar: String?, prefix: String) {
+        val jarName = File(jar!!).name
+        assertTrue("Expected jar with prefix: $prefix, but $jarName found.",
+                   jarName.startsWith(prefix))
     }
 }
