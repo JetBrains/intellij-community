911e2a2ac75f26855270b901657cb25c1a743b0d
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-10-27 18:46:09 +0200

    IDEA-276596 [documentation] inline `documentation.v2.turn.off.preview.by.action` registry key
    GitOrigin-RevId: 3d0802e452f0543f78df4ab0392406eed0d9b660

--- platform/lang-impl/src/com/intellij/lang/documentation/ide/actions/TurnOffAutoUpdateAction.kt
+++ platform/lang-impl/src/com/intellij/lang/documentation/ide/actions/TurnOffAutoUpdateAction.kt
@@ -4,7 +4,6 @@ package com.intellij.lang.documentation.ide.actions
 import com.intellij.lang.documentation.ide.impl.DocumentationToolWindowManager
 import com.intellij.lang.documentation.ide.ui.toolWindowUI
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowContextMenuActionBase
 import com.intellij.ui.content.Content
@@ -14,7 +13,6 @@ internal class TurnOffAutoUpdateAction : ToolWindowContextMenuActionBase() {
   override fun update(e: AnActionEvent, toolWindow: ToolWindow, content: Content?) {
     e.presentation.isEnabledAndVisible = toolWindow.id == DocumentationToolWindowManager.TOOL_WINDOW_ID
                                          && content != null && content.toolWindowUI.isPreview
-                                         && Registry.`is`("documentation.v2.turn.off.preview.by.action")
   }
 
   override fun actionPerformed(e: AnActionEvent, toolWindow: ToolWindow, content: Content?) {
