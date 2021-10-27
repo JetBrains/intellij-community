78a5157fab431b2918cb6dbedc1971003a7fbe03
Andrei Klunnyi <andrei.klunnyi@jetbrains.com>
2021-10-27 18:44:33 +0200

    KTIJ-18464 [MPP]: running tests for module/package
    This commit introduces MPP specific gradle-tasks
    producers allowing launching tests for an entire
    module or a package.
    
    So far, gradle plugin fulfilled this duty [1][2].
    But MPP details (including gradle task names) are stored
    in Kotlin facet available only from Kotlin IDE plugin.
    
    [1]: AllInDirectoryGradleConfigurationProducer
    [2]: AllInPackageGradleConfigurationProducer
    
    ^KTIJ-18464 fixed
    
    GitOrigin-RevId: 893559f1b866bd03551a226e71bb4c741677ebf8

---
+++ plugins/kotlin/gradle/gradle-java/src/org/jetbrains/kotlin/idea/gradleJava/testing/KotlinMultiplatformAllInDirectoryConfigurationProducer.kt
@@ -0,0 +1,40 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.kotlin.idea.gradleJava.testing
+
+import com.intellij.execution.actions.ConfigurationContext
+import com.intellij.execution.actions.ConfigurationFromContext
+import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.idea.gradleJava.run.MultiplatformTestTasksChooser
+import org.jetbrains.plugins.gradle.execution.test.runner.AllInDirectoryGradleConfigurationProducer
+import org.jetbrains.plugins.gradle.util.createTestWildcardFilter
+
+class KotlinMultiplatformAllInDirectoryConfigurationProducer
+    : AllInDirectoryGradleConfigurationProducer() {
+
+    private val mppTestTasksChooser = MultiplatformTestTasksChooser()
+
+
+    override fun isPreferredConfiguration(self: ConfigurationFromContext, other: ConfigurationFromContext): Boolean {
+        return !other.isProducedBy(KotlinMultiplatformAllInPackageConfigurationProducer::class.java) && super.isPreferredConfiguration(self, other)
+    }
+
+    override fun shouldReplace(self: ConfigurationFromContext, other: ConfigurationFromContext): Boolean {
+        return !other.isProducedBy(KotlinMultiplatformAllInPackageConfigurationProducer::class.java) && super.shouldReplace(self, other)
+    }
+
+    override fun getAllTestsTaskToRun(
+        context: ConfigurationContext,
+        element: PsiElement,
+        chosenElements: List<PsiElement>
+    ): List<TestTasksToRun> {
+        var result: List<TestTasksToRun> = emptyList()
+        mppTestTasksChooser.multiplatformChooseTasks(context.project, context.dataContext, listOf(element)) { tasks ->
+            val wildcardFilter = createTestWildcardFilter()
+            val tasksToRuns = tasks
+                .flatMap { it.values }
+                .map { TestTasksToRun(it, wildcardFilter) }
+            result = tasksToRuns
+        }
+        return result
+    }
+}
