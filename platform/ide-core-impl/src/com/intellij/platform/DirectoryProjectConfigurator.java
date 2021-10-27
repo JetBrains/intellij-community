94811e3f42afc871475fb83ba40408757691c265
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2021-10-27 16:10:31 +0300

    Document EP
    GitOrigin-RevId: 4a6050b793e5452764d4d1f51e4e34a0f3c66c2f

--- platform/ide-core-impl/src/com/intellij/platform/DirectoryProjectConfigurator.java
+++ platform/ide-core-impl/src/com/intellij/platform/DirectoryProjectConfigurator.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.platform;
 
 import com.intellij.openapi.module.Module;
@@ -9,6 +9,12 @@ import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 
+/**
+ * Configures various subsystems (facets etc) when user opens folder with code but without of ".idea" folder.
+ * <br/>
+ * Example: to support some framework you need to enable and configure facet. User opens folder with code for the first time.
+ * This class scans code and detects framework heuristically. It then configures facet without user action.
+ */
 public interface DirectoryProjectConfigurator {
   /**
    * @deprecated Use {@link #configureProject(Project, VirtualFile, Ref, boolean)}
@@ -17,7 +23,10 @@ public interface DirectoryProjectConfigurator {
   @Deprecated
   default void configureProject(@NotNull Project project, @NotNull VirtualFile baseDir, @NotNull Ref<Module> moduleRef) {
   }
-
+  /**
+   * @return if code must be called or EDT or not.
+   * If {@link #configureProject(Project, VirtualFile, Ref, boolean)} is slow (heavy computations, network access etc) return "false" here.
+   */
   default boolean isEdtRequired() {
     return true;
   }
