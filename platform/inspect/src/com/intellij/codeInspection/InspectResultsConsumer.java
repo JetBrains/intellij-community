fe78e8eb79afca010470d08861c3d4579a20f6f5
alexey.afanasiev <alexey.afanasiev@jetbrains.com>
2021-10-28 01:09:31 +0300

    [qodana] Add @ApiStatus.Internal to InspectResultsConsumer and InspectionApplicationFactory.
    GitOrigin-RevId: 418a6865a3b5587fa7217f51681f7a6d840da754

--- platform/inspect/src/com/intellij/codeInspection/InspectResultsConsumer.java
+++ platform/inspect/src/com/intellij/codeInspection/InspectResultsConsumer.java
@@ -1,15 +1,17 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInspection;
 
 import com.intellij.codeInspection.ex.Tools;
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.util.List;
 import java.util.Map;
 
+@ApiStatus.Internal
 public interface InspectResultsConsumer {
   ExtensionPointName<InspectResultsConsumer> EP_NAME =
     ExtensionPointName.create("com.intellij.inspectResultsConsumer");
