e148a4aea707a1fc4c3b6eeaf47541ed48cbef0e
Dmitry Timofeev <dmitry.timofeev@jetbrains.com>
2021-10-27 08:07:16 +0300

    [kotlin]  @Experimental -> @RequiresOptIn migration: style fixes
    ^KTIJ-15780 In progress
    
    GitOrigin-RevId: 0f91071f0e01f356760d90eef38068a446d95617

--- plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/util/modifierListModifactor.kt
+++ plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/util/modifierListModifactor.kt
@@ -44,15 +44,15 @@ fun KtModifierListOwner.addAnnotation(
 /**
  * Add a new annotation to the declaration or expression, or modify an existing annotation.
  *
- * Note: if the search for and existing annotation is enabled (this is default for compatibility), the function will
- * resolve annotation names. This should be avoided is the function is called from an event dispatcher thread (EDT),
+ * Note: if the search for an existing annotation is enabled (this is default for compatibility), the function will
+ * resolve annotation names. This should be avoided if the function is called from an event dispatcher thread (EDT),
  * e.g., if it is a part of a write action such as the `invoke()` function of a quick fix.
  *
  * @receiver the annotation owner to modify
  * @param annotationFqName fully qualified name of the annotation to add
  * @param annotationInnerText the inner text (rendered arguments) of the annotation, or null if there is no inner text (default)
  * @param useSiteTarget the use site target of the annotation, or null if no explicit use site target is provided
- * @param searchForExistingEntry `true` iff the function should search for an existing annotation and update it instead of creating a new entry
+ * @param searchForExistingEntry `true` if the function should search for an existing annotation and update it instead of creating a new entry
  * @param whiteSpaceText the whitespace separator that should be inserted between annotation entries (newline by default)
  * @param addToExistingAnnotation a lambda expression to run on an existing annotation if it has been found
  * @return `true` if an annotation has been added or modified, `false` otherwise
@@ -60,7 +60,7 @@ fun KtModifierListOwner.addAnnotation(
 fun KtModifierListOwner.addAnnotation(
     annotationFqName: FqName,
     annotationInnerText: String? = null,
-    useSiteTarget: AnnotationUseSiteTarget?,
+    useSiteTarget: AnnotationUseSiteTarget? = null,
     searchForExistingEntry: Boolean = true,
     whiteSpaceText: String = "\n",
     addToExistingAnnotation: ((KtAnnotationEntry) -> Boolean)? = null
