ede3933d5b98a6e02f7008cf27d491785c7e740e
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2021-10-29 00:10:38 +0300

    [vcs-log] convert MiniDetailsGetter to kotlin
    GitOrigin-RevId: 8c66dbeffd93741a7af2000c374eb2c9111b5bfe

--- platform/vcs-log/impl/src/com/intellij/vcs/log/data/MiniDetailsGetter.kt
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/data/MiniDetailsGetter.kt
@@ -1,105 +1,86 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.vcs.log.data;
+package com.intellij.vcs.log.data
 
-import com.intellij.openapi.Disposable;
-import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.VcsException;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.Consumer;
-import com.intellij.util.concurrency.annotations.RequiresBackgroundThread;
-import com.intellij.vcs.log.VcsCommitMetadata;
-import com.intellij.vcs.log.VcsLogObjectsFactory;
-import com.intellij.vcs.log.VcsLogProvider;
-import com.intellij.vcs.log.data.index.IndexDataGetter;
-import com.intellij.vcs.log.data.index.IndexedDetails;
-import com.intellij.vcs.log.data.index.VcsLogIndex;
-import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
-import it.unimi.dsi.fastutil.ints.IntSet;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vcs.VcsException
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.util.Consumer
+import com.intellij.util.concurrency.annotations.RequiresBackgroundThread
+import com.intellij.vcs.log.VcsCommitMetadata
+import com.intellij.vcs.log.VcsLogObjectsFactory
+import com.intellij.vcs.log.VcsLogProvider
+import com.intellij.vcs.log.data.index.IndexedDetails.Companion.createMetadata
+import com.intellij.vcs.log.data.index.VcsLogIndex
+import it.unimi.dsi.fastutil.ints.IntConsumer
+import it.unimi.dsi.fastutil.ints.IntOpenHashSet
+import it.unimi.dsi.fastutil.ints.IntSet
 
-import java.util.List;
-import java.util.Map;
+class MiniDetailsGetter internal constructor(project: Project,
+                                             storage: VcsLogStorage,
+                                             logProviders: Map<VirtualFile, VcsLogProvider>,
+                                             private val topCommitsDetailsCache: TopCommitsCache,
+                                             index: VcsLogIndex,
+                                             parentDisposable: Disposable) :
+  AbstractDataGetter<VcsCommitMetadata>(storage, logProviders, index, parentDisposable) {
 
-public class MiniDetailsGetter extends AbstractDataGetter<VcsCommitMetadata> {
-
-  @NotNull private final TopCommitsCache myTopCommitsDetailsCache;
-  @NotNull private final VcsLogObjectsFactory myFactory;
-
-  MiniDetailsGetter(@NotNull Project project,
-                    @NotNull VcsLogStorage storage,
-                    @NotNull Map<VirtualFile, VcsLogProvider> logProviders,
-                    @NotNull TopCommitsCache topCommitsDetailsCache,
-                    @NotNull VcsLogIndex index,
-                    @NotNull Disposable parentDisposable) {
-    super(storage, logProviders, index, parentDisposable);
-    myTopCommitsDetailsCache = topCommitsDetailsCache;
-    myFactory = project.getService(VcsLogObjectsFactory.class);
-  }
+  private val factory = project.getService(VcsLogObjectsFactory::class.java)
 
   @RequiresBackgroundThread
-  public void loadCommitsData(@NotNull Iterable<Integer> hashes,
-                              @NotNull Consumer<? super VcsCommitMetadata> consumer,
-                              @NotNull ProgressIndicator indicator) throws VcsException {
-    final IntSet toLoad = new IntOpenHashSet();
-
-    for (int id : hashes) {
-      VcsCommitMetadata details = getFromCache(id);
-      if (details == null || details instanceof LoadingDetails) {
-        toLoad.add(id);
+  @Throws(VcsException::class)
+  fun loadCommitsData(hashes: Iterable<Int>,
+                      consumer: Consumer<in VcsCommitMetadata>,
+                      indicator: ProgressIndicator) {
+    val toLoad = IntOpenHashSet()
+    for (id in hashes) {
+      val details = getFromCache(id)
+      if (details == null || details is LoadingDetails) {
+        toLoad.add(id)
       }
       else {
-        consumer.consume(details);
+        consumer.consume(details)
       }
     }
-
     if (!toLoad.isEmpty()) {
-      indicator.checkCanceled();
-      preLoadCommitData(toLoad, consumer);
-      notifyLoaded();
+      indicator.checkCanceled()
+      preLoadCommitData(toLoad, consumer)
+      notifyLoaded()
     }
   }
 
-  @Nullable
-  @Override
-  protected VcsCommitMetadata getFromAdditionalCache(int commitId) {
-    return myTopCommitsDetailsCache.get(commitId);
+  override fun getFromAdditionalCache(commitId: Int): VcsCommitMetadata? {
+    return topCommitsDetailsCache[commitId]
   }
 
-  @Override
-  protected void readDetails(@NotNull VcsLogProvider logProvider,
-                             @NotNull VirtualFile root,
-                             @NotNull List<String> hashes,
-                             @NotNull Consumer<? super VcsCommitMetadata> consumer) throws VcsException {
-    logProvider.readMetadata(root, hashes, consumer);
+  @Throws(VcsException::class)
+  override fun readDetails(logProvider: VcsLogProvider,
+                           root: VirtualFile,
+                           hashes: List<String>,
+                           consumer: Consumer<in VcsCommitMetadata>) {
+    logProvider.readMetadata(root, hashes, consumer)
   }
 
-  @Override
-  protected void preLoadCommitData(@NotNull IntSet commits, @NotNull Consumer<? super VcsCommitMetadata> consumer)
-    throws VcsException {
-
-    IndexDataGetter dataGetter = getIndex().getDataGetter();
+  @Throws(VcsException::class)
+  override fun preLoadCommitData(commits: IntSet, consumer: Consumer<in VcsCommitMetadata>) {
+    val dataGetter = index.dataGetter
     if (dataGetter == null) {
-      super.preLoadCommitData(commits, consumer);
-      return;
+      super.preLoadCommitData(commits, consumer)
+      return
     }
-
-    IntSet notIndexed = new IntOpenHashSet();
-
-    commits.forEach(commit -> {
-      VcsCommitMetadata metadata = IndexedDetails.createMetadata(commit, dataGetter, getStorage(), myFactory);
+    val notIndexed = IntOpenHashSet()
+    commits.forEach(IntConsumer { commit: Int ->
+      val metadata = createMetadata(commit, dataGetter, storage, factory)
       if (metadata == null) {
-        notIndexed.add(commit);
+        notIndexed.add(commit)
       }
       else {
-        saveInCache(commit, metadata);
-        consumer.consume(metadata);
+        saveInCache(commit, metadata)
+        consumer.consume(metadata)
       }
-    });
-
+    })
     if (!notIndexed.isEmpty()) {
-      super.preLoadCommitData(notIndexed, consumer);
+      super.preLoadCommitData(notIndexed, consumer)
     }
   }
 }
