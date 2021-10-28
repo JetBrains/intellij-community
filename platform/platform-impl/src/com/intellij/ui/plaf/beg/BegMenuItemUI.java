4942aa53e437aab33ff9ab6393b5372780072901
Gabriel Pizarro <gpizarro@javaman.net>
2021-10-28 11:40:42 -0700

    Allow customizing ActionMenu UI

--- platform/platform-impl/src/com/intellij/ui/plaf/beg/BegMenuItemUI.java
+++ platform/platform-impl/src/com/intellij/ui/plaf/beg/BegMenuItemUI.java
@@ -1,5 +1,4 @@
-
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ui.plaf.beg;
 
 import com.intellij.ide.ui.UISettings;
@@ -42,8 +41,9 @@ import java.lang.reflect.Method;
 /**
  * @author Eugene Belyaev
  * @author Vladimir Kondratyev
+ * @author Gabriel Pizarro
  */
-public final class BegMenuItemUI extends BasicMenuItemUI {
+public class BegMenuItemUI extends BasicMenuItemUI {
   private static final Rectangle b = new Rectangle(0, 0, 0, 0);
   private static final Rectangle j = new Rectangle();
   private static final Rectangle d = new Rectangle();
