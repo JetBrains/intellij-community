8918355b957467cb56833556381f9b142fff1b2f
Jinseong Jeon <jsjeon@google.com>
2021-10-28 16:11:20 +0900

    FIR/UAST: commonize Fixture-based resolve API tests
    GitOrigin-RevId: 28307c3bcb536155b3d558b02a7c897855e4749a

---
+++ plugins/kotlin/uast/uast-kotlin/tests/test/org/jetbrains/uast/test/kotlin/comparison/FE1UastResolveApiFixtureTest.kt
@@ -0,0 +1,65 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.uast.test.kotlin.comparison
+
+import com.intellij.testFramework.LightProjectDescriptor
+import org.jetbrains.kotlin.idea.test.KotlinLightCodeInsightFixtureTestCase
+import org.jetbrains.kotlin.idea.test.KotlinWithJdkAndRuntimeLightProjectDescriptor
+import org.jetbrains.uast.test.common.kotlin.UastResolveApiFixtureTestBase
+import org.junit.internal.runners.JUnit38ClassRunner
+import org.junit.runner.RunWith
+
+@RunWith(JUnit38ClassRunner::class)
+class FE1UastResolveApiFixtureTest : KotlinLightCodeInsightFixtureTestCase(), UastResolveApiFixtureTestBase {
+    override val isFirUastPlugin: Boolean = false
+
+    override fun getProjectDescriptor(): LightProjectDescriptor =
+        KotlinWithJdkAndRuntimeLightProjectDescriptor.INSTANCE
+
+    fun testResolveStringFromUast() {
+        checkResolveStringFromUast(myFixture, project)
+    }
+
+    fun testMultiResolve() {
+        checkMultiResolve(myFixture)
+    }
+
+    fun testMultiResolveJava() {
+        checkMultiResolveJava(myFixture)
+    }
+
+    fun testMultiResolveJavaAmbiguous() {
+        checkMultiResolveJavaAmbiguous(myFixture)
+    }
+
+    fun testResolveFromBaseJava() {
+        checkResolveFromBaseJava(myFixture)
+    }
+
+    fun testMultiResolveInClass() {
+        checkMultiResolveInClass(myFixture)
+    }
+
+    fun testMultiConstructorResolve() {
+        checkMultiConstructorResolve(myFixture, project)
+    }
+
+    fun testMultiInvokableObjectResolve() {
+        checkMultiInvokableObjectResolve(myFixture)
+    }
+
+    fun testMultiResolveJvmOverloads() {
+        checkMultiResolveJvmOverloads(myFixture)
+    }
+
+    fun testLocalResolve() {
+        checkLocalResolve(myFixture)
+    }
+
+    fun testResolveCompiledAnnotation() {
+        checkResolveCompiledAnnotation(myFixture)
+    }
+
+    fun testAssigningArrayElementType() {
+        checkAssigningArrayElementType(myFixture)
+    }
+}
