cd6523e72085d24366c818cc4a1fd7224b297891
Peter Gromov <peter@jetbrains.com>
2021-10-28 14:29:59 +0200

    [grazie] improve whitespace presentation in the quick fix names
    (cherry picked from commit 06cbc2f5a158aac42486c21c583ce26e0e533e4c)
    
    IJ-CR-16108
    
    GitOrigin-RevId: 706a5e06fe675d773cb954f30cb04f1d0bdf8ecf

--- plugins/grazie/src/test/kotlin/com/intellij/grazie/ide/language/ReportingTest.kt
+++ plugins/grazie/src/test/kotlin/com/intellij/grazie/ide/language/ReportingTest.kt
@@ -1,6 +1,12 @@
 package com.intellij.grazie.ide.language
 
 import com.intellij.grazie.ide.inspection.grammar.GrazieInspection
+import com.intellij.grazie.ide.inspection.grammar.quickfix.GrazieReplaceTypoQuickFix
+import com.intellij.grazie.text.Rule
+import com.intellij.grazie.text.TextContent
+import com.intellij.grazie.text.TextExtractor
+import com.intellij.grazie.text.TextProblem
+import com.intellij.openapi.util.TextRange
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 
 class ReportingTest : BasePlatformTestCase() {
@@ -20,5 +26,26 @@ class ReportingTest : BasePlatformTestCase() {
     val message = "Use a instead of 'an' if the following word doesn't start with a vowel sound, e.g. 'a sentence', 'a university'."
     assertEquals(info.description, message)
     assertTrue(info.toolTip, info.toolTip!!.matches(Regex(".*" + Regex.escape(message) + ".*Incorrect:.*Correct:.*")))
+  }
+
+  fun `test quick fix presentation`() {
+    myFixture.configureByText("a.txt", "Some text")
+    val text = TextExtractor.findTextAt(myFixture.file, 0, TextContent.TextDomain.ALL)!!
+    val range = TextRange(0, 1)
+    val rule = object : Rule("something.something", "something", "something") {
+      override fun getDescription() = "something"
+    }
+    val problem = object : TextProblem(rule, text, range) {
+      override fun getShortMessage() = "this problem"
+      override fun getDescriptionTemplate(isOnTheFly: Boolean) = "something"
+      override fun getReplacementRange() = highlightRange
+      override fun getCorrections() = listOf(
+        " ", // should be visible
+        "another suggestion", // plain normal suggestion
+        "" // should not be rendered as an empty string
+      )
+    }
+    assertEquals(listOf("Fix 'this problem'", "' '", "another suggestion", "Remove"),
+                 GrazieReplaceTypoQuickFix.getReplacementFixes(problem, emptyList()).map { it.name })
   }
 }
