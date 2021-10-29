c87d8643eddca086a58174da64863be8263fa835
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2021-10-29 10:09:28 +0200

    [kotlin] Collect all keys  and all into a single collection
    GitOrigin-RevId: d03f4d0d5a4ba72015dc0d21e3b856915db3c882

--- plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/stubindex/AbstractStringStubIndexExtension.kt
+++ plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/stubindex/AbstractStringStubIndexExtension.kt
@@ -5,28 +5,42 @@ import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.stubs.StringStubIndexExtension
+import com.intellij.psi.stubs.StubIndex
 import com.intellij.util.Processor
+import com.intellij.util.Processors
 
 abstract class AbstractStringStubIndexExtension<Psi: PsiElement>(protected val valueClass: Class<Psi>): StringStubIndexExtension<Psi>() {
 
-    fun processAllElements(
+    /**
+     * Note: [processor] should not invoke any indices as it could lead to deadlock. Nested index access is forbidden.
+     */
+    fun processElements(
+        s: String,
         project: Project,
         scope: GlobalSearchScope,
         processor: Processor<in Psi>
     ) {
-        processAllElements(project, scope, { true }, processor)
+        StubIndex.getInstance().processElements(key, s, project, scope, valueClass, processor)
     }
 
     fun processAllElements(
         project: Project,
         scope: GlobalSearchScope,
-        filter: (String) -> Boolean,
+        filter: (String) -> Boolean = { true },
         processor: Processor<in Psi>
     ) {
+        val values = mutableSetOf<Psi>()
+        val collectProcessor = Processors.cancelableCollectProcessor(values)
+        val stubIndex = StubIndex.getInstance()
+        val indexKey = key
+
+        // collect all keys, collect all values those fulfill filter into a single collection, process values after that
         getAllKeys(project).forEach { s ->
             if (filter(s)) {
-                get(s, project, scope).forEach { if (!processor.process(it)) return }
+                if (!stubIndex.processElements(indexKey, s, project, scope, valueClass, collectProcessor)) return
             }
         }
+        // process until the 1st negative result of processor
+        values.all(processor::process)
     }
 }
