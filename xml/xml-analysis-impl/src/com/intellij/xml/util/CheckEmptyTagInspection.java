2b7cafbab4e01c12cde1dfeb19ba8fbbad8ec75d
Alexey Kudravtsev <cdr@intellij.com>
2021-10-31 12:38:39 +0100

    more correct "do not show INFORMATION in batch mode" fix (IDEA-CR-71979 followup)
    GitOrigin-RevId: a8ce7d36e30fe7a7d350d261f27c0a8d449007f6

--- xml/xml-analysis-impl/src/com/intellij/xml/util/CheckEmptyTagInspection.java
+++ xml/xml-analysis-impl/src/com/intellij/xml/util/CheckEmptyTagInspection.java
@@ -39,10 +39,6 @@ public class CheckEmptyTagInspection extends XmlSuppressableInspectionTool {
   @Override
   @NotNull
   public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
-    if (!isOnTheFly) {
-      // should not report INFORMATION in batch mode
-      return PsiElementVisitor.EMPTY_VISITOR;
-    }
     return new XmlElementVisitor() {
       @Override public void visitXmlTag(final XmlTag tag) {
         if (XmlExtension.shouldIgnoreSelfClosingTag(tag) || !isTagWithEmptyEndNotAllowed(tag)) {
@@ -53,14 +49,16 @@ public class CheckEmptyTagInspection extends XmlSuppressableInspectionTool {
           return;
         }
 
-        final LocalQuickFix fix = new MyLocalQuickFix();
-
-        holder.registerProblem(tag,
-                               XmlAnalysisBundle.message("html.inspections.check.empty.script.message"),
-                               tag.getContainingFile().getContext() != null ?
-                               ProblemHighlightType.INFORMATION:
-                               ProblemHighlightType.GENERIC_ERROR_OR_WARNING,
-                               fix);
+        ProblemHighlightType type = tag.getContainingFile().getContext() != null ?
+                                    ProblemHighlightType.INFORMATION :
+                                    ProblemHighlightType.GENERIC_ERROR_OR_WARNING;
+        // should not report INFORMATION in batch mode
+        if (isOnTheFly || type != ProblemHighlightType.INFORMATION) {
+          holder.registerProblem(tag,
+                                 XmlAnalysisBundle.message("html.inspections.check.empty.script.message"),
+                                 type,
+                                 new MyLocalQuickFix());
+        }
       }
     };
   }
