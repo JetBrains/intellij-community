7f5df7c4373d3cbeb470176fd7f5ee8e397b6f18
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2021-11-01 20:00:16 +0300

    Extract TestFixture as JUnit 4 test rule.
    Migrate to the modern JUnit 4 API to get rid of redundant inheritance and simplify code reuse
    
    GitOrigin-RevId: 191fba135b413da0280e4328449b1353ee515e65

---
+++ platform/platform-tests/testSrc/com/intellij/execution/wsl/WslTestBase.kt
@@ -0,0 +1,25 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.execution.wsl
+
+import com.intellij.testFramework.fixtures.BareTestFixture
+import com.intellij.testFramework.fixtures.TestFixtureRule
+import org.junit.Rule
+import org.junit.rules.RuleChain
+
+
+open class WslTestBase {
+  private val testFixtureRule = TestFixtureRule()
+  private val wslRule = WslRule()
+
+  protected val wsl: WSLDistribution get() = wslRule.wsl
+  protected val testFixture: BareTestFixture get() = testFixtureRule.fixture
+
+
+  /**
+   * [WslRule] depends on [TestFixtureRule]
+   */
+  @JvmField
+  @Rule
+  val ruleChain: RuleChain = RuleChain.outerRule(testFixtureRule).around(wslRule)
+}
+
--- platform/testFramework/src/com/intellij/testFramework/fixtures/BareTestFixtureTestCase.java
+++ platform/testFramework/src/com/intellij/testFramework/fixtures/BareTestFixtureTestCase.java
@@ -1,56 +1,30 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.testFramework.fixtures;
 
 import com.intellij.openapi.Disposable;
-import com.intellij.openapi.application.ex.ApplicationManagerEx;
-import com.intellij.openapi.util.Disposer;
-import com.intellij.testFramework.*;
+import com.intellij.testFramework.PlatformTestUtil;
+import com.intellij.testFramework.TestLoggerFactory;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.TestOnly;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.rules.TestName;
 import org.junit.rules.TestRule;
 
-import static com.intellij.testFramework.TestFrameworkUtil.SKIP_HEADLESS;
-import static com.intellij.testFramework.TestFrameworkUtil.SKIP_SLOW;
-import static org.junit.Assume.assumeFalse;
-
 @TestOnly
 public abstract class BareTestFixtureTestCase {
-  @Rule public final TestName testName = new TestName();
-  @Rule public final TestRule testLoggerWatcher = TestLoggerFactory.createTestWatcher();
+  @Rule
+  public final TestFixtureRule testFixtureRule = new TestFixtureRule();
+  @Rule
+  public final TestName testName = new TestName();
+  @Rule
+  public final TestRule testLoggerWatcher = TestLoggerFactory.createTestWatcher();
 
-  private BareTestFixture myFixture;
-
-  @Before
-  public final void setupFixture() throws Exception {
-    ApplicationManagerEx.setInStressTest(TestFrameworkUtil.isPerformanceTest(null, getClass().getName()));
-
-    boolean headless = SKIP_HEADLESS && getClass().getAnnotation(SkipInHeadlessEnvironment.class) != null;
-    assumeFalse("Class '" + getClass().getName() + "' is skipped because it requires working UI environment", headless);
-    boolean slow = SKIP_SLOW && getClass().getAnnotation(SkipSlowTestLocally.class) != null;
-    assumeFalse("Class '" + getClass().getName() + "' is skipped because it is dog slow", slow);
-
-    myFixture = IdeaTestFixtureFactory.getFixtureFactory().createBareFixture();
-    myFixture.setUp();
-    Disposer.register(getTestRootDisposable(), () -> ApplicationManagerEx.setInStressTest(false));
-  }
-
-  @After
-  public final void tearDownFixture() throws Exception {
-    if (myFixture != null) {
-      myFixture.tearDown();
-      myFixture = null;
-    }
-  }
 
   protected final @NotNull String getTestName(boolean lowercaseFirstLetter) {
     return PlatformTestUtil.getTestName(testName.getMethodName(), lowercaseFirstLetter);
   }
 
   protected final @NotNull Disposable getTestRootDisposable() {
-    return myFixture.getTestRootDisposable();
+    return testFixtureRule.getFixture().getTestRootDisposable();
   }
 }
