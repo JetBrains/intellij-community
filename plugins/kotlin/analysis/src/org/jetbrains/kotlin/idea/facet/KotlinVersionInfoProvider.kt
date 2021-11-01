ecb48ac2fe677da4cc4252704cf15aec0d1b8eae
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2021-11-01 20:01:55 +0100

    [kotlin] Add mapping for 1.7 and 1.8 language version
    KotlinVersionInfoProviderTest Fixed
    
    GitOrigin-RevId: 03e00cb4c2d4399ab078045fd814785e3952bd28

--- plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/facet/KotlinVersionInfoProvider.kt
+++ plugins/kotlin/analysis/src/org/jetbrains/kotlin/idea/facet/KotlinVersionInfoProvider.kt
@@ -78,6 +78,8 @@ fun getDefaultLanguageLevel(
 
 fun String?.toLanguageVersion(): LanguageVersion = when {
     this == null -> VersionView.RELEASED_VERSION
+    startsWith("1.8") -> LanguageVersion.KOTLIN_1_8
+    startsWith("1.7") -> LanguageVersion.KOTLIN_1_7
     startsWith("1.6") -> LanguageVersion.KOTLIN_1_6
     startsWith("1.5") -> LanguageVersion.KOTLIN_1_5
     startsWith("1.4") -> LanguageVersion.KOTLIN_1_4
