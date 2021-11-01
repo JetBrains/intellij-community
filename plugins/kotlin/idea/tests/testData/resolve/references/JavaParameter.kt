de41b8b642ac09e5b7453a2deb2d95201334c56b
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2021-11-01 17:16:53 +0100

    [kotlin:testdata] ReferenceResolveTestGenerated.testJavaParameter
    Quite similar to 5b8dd08a1518eb57ff653a14cb243cfbf8004bab, also in master we have the same testdata.
    Seems like the source of the change is 23f66b088657a454328abe70a6cea5de6755ea12
    
    GitOrigin-RevId: adb23244ba74c4db0bdaad31a816d6449138a999

--- plugins/kotlin/idea/tests/testData/resolve/references/JavaParameter.kt
+++ plugins/kotlin/idea/tests/testData/resolve/references/JavaParameter.kt
@@ -1,6 +1,6 @@
 lateinit var x: java.lang.Readable
 lateinit var y: java.nio.CharBuffer
 
-val h = x.read(c<caret>b = y)
+val h = x.read(p<caret>0 = y)
 
-// REF: CharBuffer cb
+// REF: CharBuffer var1
