3d6d9809b6a1d388efd1c5a71a8c454743e78c7c
Elena.Korotkova <elena.korotkova@jetbrains.com>
2021-11-01 21:53:44 +0300

    gateway: fix back button when connection is created from the recent connections panel
    IJ-CR-16332
    
    GitOrigin-RevId: 2581ae0ba280a1f483f6f682836355e4562b5cd1

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/TabbedWelcomeScreen.java
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/welcomeScreen/TabbedWelcomeScreen.java
@@ -126,7 +126,7 @@ public final class TabbedWelcomeScreen extends AbstractWelcomeScreen {
     tree.setSelectionRow(tabIndex);
     var selectedTab = getSelectedTab();
     if(selectedTab == null) return;
-    if(selectedTab.myAssociatedComponent.getComponentCount() > 0) return;
+    if(selectedTab.myAssociatedComponent.getComponentCount() == 0) return;
 
     var panel = selectedTab.myAssociatedComponent.getComponent(0);
     ((JComponent)panel).removeAll();
