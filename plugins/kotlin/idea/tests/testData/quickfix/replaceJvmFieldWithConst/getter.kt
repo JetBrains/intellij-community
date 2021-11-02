06d3a363f0c3041ecb873ed73969a340c3c39074
Toshiaki Kameyama <kmymtsak@gmail.com>
2021-11-02 19:12:09 +0900

    [kotlin] Add quickfix for WRONG_ANNOTATION_TARGET to add use-site target
    #KTIJ-20037 Fixed

--- plugins/kotlin/idea/tests/testData/quickfix/replaceJvmFieldWithConst/getter.kt
+++ plugins/kotlin/idea/tests/testData/quickfix/replaceJvmFieldWithConst/getter.kt
@@ -3,6 +3,5 @@
 // ERROR: This annotation is not applicable to target 'top level property without backing field or delegate'
 // ACTION: Make internal
 // ACTION: Remove explicit type specification
-// ACTION: Add use-site target 'property'
 <caret>@JvmField val number: Int
     get() = 42
