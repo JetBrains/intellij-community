4e011ba367e009087bb851541e2e60f59ddd4a06
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2021-11-02 22:54:43 +0300

    trusted-projects: make new project automatically trusted
    (cherry picked from commit 4c2949fb46dcc31b6824449fd05544ba0ea20306)
    
    GitOrigin-RevId: 9dd65036780e9481e82dd2de795bb40922684e72

--- java/idea-ui/src/com/intellij/ide/impl/NewProjectUtil.java
+++ java/idea-ui/src/com/intellij/ide/impl/NewProjectUtil.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.impl;
 
 import com.intellij.ide.JavaUiBundle;
@@ -177,6 +177,7 @@ public final class NewProjectUtil {
         if (fileName != null) {
           options = options.withProjectName(fileName.toString());
         }
+        TrustedPaths.getInstance().setProjectPathTrusted(projectDir, true);
         ProjectManagerEx.getInstanceEx().openProject(projectDir, options);
       }
 
