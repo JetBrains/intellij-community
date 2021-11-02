ddb7a16a8e67ba5b799f54bc1028ead810d1e66e
Roman Golyshev <roman.golyshev@jetbrains.com>
2021-11-02 16:55:12 +0300

    KTIJ-8148 Filter inapplicable keywords in type reference contexts
    In places like `listOf<` we want to see only types (and maybe a `suspend`
    keyword); instead we were seeing all possible keywords proposed
    because `KeywordCompletion::buildFilter` travelled from the
    completion position to the outer body, and used it as a filtering
    context. In such context, almost all keywords are proposed as
    available
    
    This commit changes that by providing a special behaviour
    for completion inside `KtTypeReference`
    
    N.B. KTIJ-8148 has a second part to it, namely KTIJ-20081
    
    ^KTIJ-8148 Fixed
    
    GitOrigin-RevId: c2b8e65f707f3404f9d2aad3dc1f9aa02c3d1993

--- plugins/kotlin/completion/tests/testData/keywords/SuspendInsideTypeArguments.kt
+++ plugins/kotlin/completion/tests/testData/keywords/SuspendInsideTypeArguments.kt
@@ -7,28 +7,6 @@ class Su() {
 
 // EXIST: suspend
 
-/* TODO: items below are not valid here */
-// EXIST: class
-// EXIST: do
-// EXIST: false
-// EXIST: for
-// EXIST: fun
-// EXIST: if
-// EXIST: interface
-// EXIST: null
-// EXIST: object
-// EXIST: return
-// EXIST: super
-// EXIST: this
-// EXIST: throw
-// EXIST: true
-// EXIST: try
-// EXIST: typealias
-// EXIST: val
-// EXIST: var
-// EXIST: when
-// EXIST: while
-
 // NOTHING_ELSE
 
 
