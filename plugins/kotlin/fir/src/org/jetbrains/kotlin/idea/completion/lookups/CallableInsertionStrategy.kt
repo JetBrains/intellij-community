61d71dfe2540c6a7ccba8396ce113c9f7b4ecc0d
Tianyu Geng <tgeng@google.com>
2021-11-04 11:27:27 -0700

    FIR Completion: Add `WithSuperDisambiguation` insertion strategy
    This strategy is used if receiver is `super` and the inserted element
    needs disambguation on the receiver type. For example,
    
    public interface A {
        public fun foo(i: Int) {}
    }
    
    public interface B {
        public fun foo(i: Int) {}
    }
    
    class C : A, B {
        override fun foo(j: Int) {
            super.<caret>
        }
    }
    
    `super.foo` would be a compilation error because it's ambiguous. This
    change allows the IDE to insert `super<A>.foo` or `super<B>.foo`
    instead.
    
    Note that the next commit actually make the change to use this
    insertion strategy
    
    GitOrigin-RevId: 57393d12a3086994f7a71415b96445143c78d888

--- plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/completion/lookups/CallableInsertionStrategy.kt
+++ plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/completion/lookups/CallableInsertionStrategy.kt
@@ -3,10 +3,18 @@
 package org.jetbrains.kotlin.idea.completion.lookups
 
 import com.intellij.codeInsight.completion.InsertionContext
+import com.intellij.psi.SmartPsiElementPointer
+import org.jetbrains.kotlin.name.ClassId
+import org.jetbrains.kotlin.psi.KtSuperExpression
 
 internal sealed class CallableInsertionStrategy {
     object AsCall : CallableInsertionStrategy()
     object AsIdentifier : CallableInsertionStrategy()
     class AsIdentifierCustom(val insertionHandlerAction: InsertionContext.() -> Unit) : CallableInsertionStrategy()
-    class WithCallArgs(val args: List<String>): CallableInsertionStrategy()
+    class WithCallArgs(val args: List<String>) : CallableInsertionStrategy()
+    class WithSuperDisambiguation(
+        val superExpressionPointer: SmartPsiElementPointer<KtSuperExpression>,
+        val superClassId: ClassId,
+        val subStrategy: CallableInsertionStrategy
+    ) : CallableInsertionStrategy()
 }
--- plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/completion/lookups/utils.kt
+++ plugins/kotlin/fir/src/org/jetbrains/kotlin/idea/completion/lookups/utils.kt
@@ -2,15 +2,21 @@
 
 package org.jetbrains.kotlin.idea.completion.lookups
 
+import com.intellij.codeInsight.completion.InsertionContext
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.openapi.util.TextRange
-import org.jetbrains.kotlin.idea.completion.KotlinFirIconProvider
+import com.intellij.refactoring.suggested.endOffset
 import org.jetbrains.kotlin.analysis.api.KtAnalysisSession
 import org.jetbrains.kotlin.analysis.api.analyse
 import org.jetbrains.kotlin.analysis.api.symbols.KtSymbol
 import org.jetbrains.kotlin.analysis.api.tokens.HackToForceAllowRunningAnalyzeOnEDT
 import org.jetbrains.kotlin.analysis.api.tokens.hackyAllowRunningOnEdt
+import org.jetbrains.kotlin.idea.completion.KotlinFirIconProvider
+import org.jetbrains.kotlin.idea.completion.contributors.helpers.insertSymbol
+import org.jetbrains.kotlin.idea.util.shortenReferencesInRange
+import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtSuperExpression
 
 internal fun KtAnalysisSession.withSymbolInfo(
     symbol: KtSymbol,
@@ -45,5 +51,28 @@ internal fun shortenReferencesForFirCompletion(targetFile: KtFile, textRange: Te
         }
     }
     shortenings.invokeShortening()
+}
+
+
+internal fun updateLookupElementBuilderToInsertTypeQualifierOnSuper(
+    builder: LookupElementBuilder,
+    insertionStrategy: CallableInsertionStrategy.WithSuperDisambiguation
+) =
+    builder.withInsertHandler { context, item ->
+        builder.insertHandler?.handleInsert(context, item)
+        val superExpression = insertionStrategy.superExpressionPointer.element ?: return@withInsertHandler
+        superExpression.setSuperTypeQualifier(context, insertionStrategy.superClassId)
+    }.appendTailText(" for ${insertionStrategy.superClassId.relativeClassName}", true)
+
+
+private fun KtSuperExpression.setSuperTypeQualifier(
+    context: InsertionContext,
+    superClassId: ClassId
+) {
+    superTypeQualifier?.delete()
+    val typeQualifier = "<${superClassId.asFqNameString()}>"
+    context.insertSymbol(typeQualifier, instanceReference.endOffset, moveCaretToEnd = false)
+    context.commitDocument()
+    shortenReferencesInRange(context.file as KtFile, TextRange(this.endOffset, this.endOffset + typeQualifier.length))
 }
 
