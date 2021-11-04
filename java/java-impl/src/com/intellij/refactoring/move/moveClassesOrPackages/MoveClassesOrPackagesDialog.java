cbe06f677e87a6a32ca5661172f2c8001e5fb1bc
Anna Kozlova <anna.kozlova@jetbrains.com>
2021-11-04 14:13:48 +0100

    EA-389104 - T: FileBasedIndexImpl.ensureUpToDate
    GitOrigin-RevId: c9d8880ad54baf8682e892a99eaf89d14e0a49aa

--- java/java-impl/src/com/intellij/refactoring/move/moveClassesOrPackages/MoveClassesOrPackagesDialog.java
+++ java/java-impl/src/com/intellij/refactoring/move/moveClassesOrPackages/MoveClassesOrPackagesDialog.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.refactoring.move.moveClassesOrPackages;
 
 import com.intellij.CommonBundle;
@@ -324,6 +324,11 @@ public class MoveClassesOrPackagesDialog extends MoveDialogBase {
       final String validationError = verifyInnerClassDestination();
       if (validationError != null) throw new ConfigurationException(validationError);
     }
+  }
+
+  @Override
+  protected void validateButtons() {
+    validateButtonsAsync();
   }
 
   @Nullable
