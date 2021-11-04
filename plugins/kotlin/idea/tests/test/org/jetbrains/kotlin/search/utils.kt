2aaa96dcdc204825b5c46f2202e121b26c7c529e
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-11-04 17:45:50 +0700

    [kotlin] search: optimize scope for private light class declarations
    ^KTIJ-20077 Fixed
    
    GitOrigin-RevId: 6b04088a26221a30f0dbb25d90752333c4b2aa3f

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/search/utils.kt
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/search/utils.kt
@@ -1,8 +1,15 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.kotlin.search
 
+import com.intellij.ide.highlighter.JavaFileType
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiNamedElement
+import com.intellij.psi.search.FileTypeIndex
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.search.LocalSearchScope
 import com.intellij.psi.search.SearchScope
 import junit.framework.TestCase
+import org.jetbrains.kotlin.idea.KotlinFileType
 import org.jetbrains.kotlin.idea.search.toHumanReadableString
 
 fun assertWithExpectedScope(actual: SearchScope, expected: String) {
@@ -26,3 +33,28 @@ fun assertWithExpectedScope(actual: SearchScope, expected: String) {
 }
 
 private val stablePartOfLineRegex = Regex("[^\\[]*")
+
+fun SearchScope.findFiles(vFileRenderer: (VirtualFile) -> String): String {
+    if (this is GlobalSearchScope) {
+        val files = FileTypeIndex.getFiles(KotlinFileType.INSTANCE, this)
+            .plus(FileTypeIndex.getFiles(JavaFileType.INSTANCE, this))
+            .vFilesToString(vFileRenderer)
+
+        return "global:$files"
+    }
+
+    this as LocalSearchScope
+
+    val files = virtualFiles.toList().vFilesToString(vFileRenderer)
+    val elements = scope.map { if (it is PsiNamedElement) it.name.toString() else it.toString() }.sortAndJoinToString()
+
+    return "local:\nfiles:$files\nelements:$elements"
+}
+
+private fun Iterable<VirtualFile>.vFilesToString(vFileRenderer: (VirtualFile) -> String): String = map(vFileRenderer).sortAndJoinToString()
+
+private fun <T : Comparable<T>> Iterable<T>.sortAndJoinToString(): String = sorted().joinToString(
+    prefix = "\n",
+    separator = ",\n",
+    postfix = ",",
+)
