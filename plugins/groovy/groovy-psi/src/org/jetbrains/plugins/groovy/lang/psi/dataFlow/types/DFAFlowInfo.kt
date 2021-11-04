b3a7d7f43253f0c55ed441d32a1a397d8a0b2ca1
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2021-11-04 12:37:40 +0300

    [groovy] IDEA-221054: Generalize type
    GitOrigin-RevId: 14bbceab314056b607bc4ec947bebe5d48543786

--- plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/dataFlow/types/DFAFlowInfo.kt
+++ plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/psi/dataFlow/types/DFAFlowInfo.kt
@@ -1,13 +1,13 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.lang.psi.dataFlow.types
 
-import it.unimi.dsi.fastutil.objects.Object2IntMap
 import org.jetbrains.plugins.groovy.lang.psi.controlFlow.Instruction
 import org.jetbrains.plugins.groovy.lang.psi.controlFlow.VariableDescriptor
 
-internal data class DFAFlowInfo(val interestingInstructions: Set<Instruction>,
-                                val acyclicInstructions: Set<Instruction>,
-                                val interestingDescriptors: Set<VariableDescriptor>,
-                                val dependentOnSharedVariables: Set<Instruction>,
-                                val varIndexes : Object2IntMap<VariableDescriptor>,
-                               )
+internal data class DFAFlowInfo(
+  val interestingInstructions: Set<Instruction>,
+  val acyclicInstructions: Set<Instruction>,
+  val interestingDescriptors: Set<VariableDescriptor>,
+  val dependentOnSharedVariables: Set<Instruction>,
+  val varIndexes: Map<VariableDescriptor, Int>,
+)
