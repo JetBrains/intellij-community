9afbf0589574f0348415f639aa45827f5b73ce47
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2021-11-05 19:25:03 +0100

    [kotlin] Regenerate tests
    GitOrigin-RevId: 4d12eecd081b8ff72fbda626264bc62d75d88399

--- plugins/kotlin/fir/test/org/jetbrains/kotlin/idea/fir/parameterInfo/FirParameterInfoTestGenerated.java
+++ plugins/kotlin/fir/test/org/jetbrains/kotlin/idea/fir/parameterInfo/FirParameterInfoTestGenerated.java
@@ -1,7 +1,4 @@
-/*
- * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
- * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
- */
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package org.jetbrains.kotlin.idea.fir.parameterInfo;
 
@@ -21,7 +18,7 @@ import org.junit.runner.RunWith;
 @TestDataPath("$CONTENT_ROOT")
 @RunWith(JUnit3RunnerWithInners.class)
 @TestMetadata("../idea/tests/testData/parameterInfo")
-public class FirParameterInfoTestGenerated extends AbstractFirParameterInfoTest {
+public abstract class FirParameterInfoTestGenerated extends AbstractFirParameterInfoTest {
     @RunWith(JUnit3RunnerWithInners.class)
     @TestMetadata("../idea/tests/testData/parameterInfo/annotations")
     public static class Annotations extends AbstractFirParameterInfoTest {
