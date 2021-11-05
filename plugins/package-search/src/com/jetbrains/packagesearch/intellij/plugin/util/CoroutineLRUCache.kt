6aaf72073f13e3f99d9ec5db37f04230c0742b4e
Lamberto Basti <lamberto.basti@jetbrains.com>
2021-11-05 14:44:37 +0100

    PKGS perf improvements
    - CoroutineLRUCache synchronization fixes
    
    GitOrigin-RevId: 64d952f02cbc890d22497adabf9394600e3b1ec1

--- plugins/package-search/src/com/jetbrains/packagesearch/intellij/plugin/util/CoroutineLRUCache.kt
+++ plugins/package-search/src/com/jetbrains/packagesearch/intellij/plugin/util/CoroutineLRUCache.kt
@@ -1,7 +1,5 @@
 package com.jetbrains.packagesearch.intellij.plugin.util
 
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
@@ -60,49 +58,34 @@ class CoroutineLRUCache<K : Any, V>(val maxSize: Int, initialValues: Map<K, V> =
 
     private val cache = LRUMap(maxSize).apply { putAll(initialValues) }
     private val syncMutex = Mutex()
-    private val mutexMap = mutableMapOf<K, Mutex>()
 
-    private suspend inline fun <R> withLock(key: K, action: () -> R): R {
-        val mutex = syncMutex.withLock { mutexMap.getOrPut(key) { Mutex() }.apply { lock() } }
-        val result = action()
-        mutex.unlock()
-        return result
-    }
-
-    suspend fun get(key: K): V? = withLock(key) { cache[key] as? V }
+    suspend fun get(key: K): V? = syncMutex.withLock { cache[key] as? V }
 
     suspend fun getValue(key: K): V = checkNotNull(get(key)) { "Key $key not available" }
 
     suspend fun put(key: K, value: V) {
-        withLock(key) { cache.put(key, value) }
+        syncMutex.withLock { cache.put(key, value) }
     }
 
-    suspend fun getOrElse(key: K, default: suspend () -> V) = withLock(key) {
+    suspend fun getOrElse(key: K, default: suspend () -> V) = syncMutex.withLock {
         val value = cache[key] as? V
         value ?: default()
     }
 
-    suspend fun getOrPut(key: K, default: suspend () -> V) = withLock(key) {
+    suspend fun getOrPut(key: K, default: suspend () -> V) = syncMutex.withLock {
         val value = cache[key] as? V
         value ?: default().also { cache[key] = it }
     }
 
-    suspend fun getOrTryPutDefault(key: K, default: suspend () -> V) = withLock(key) {
+    suspend fun getOrTryPutDefault(key: K, default: suspend () -> V) = syncMutex.withLock {
         val value = cache[key] as? V
-
-        value ?: runCatching { default() }.getOrNull()?.also { cache[key] = it }
+        value ?: runCatching { default() }.getOrNull()?.also<V> { cache[key] = it }
     }
 
     suspend fun clear() {
-        syncMutex.withLock {
-            coroutineScope {
-                launch { mutexMap.clear() }
-                launch { cache.clear() }
-            }
-        }
+        syncMutex.withLock { cache.clear() }
     }
 
     suspend fun cachedElements() =
         syncMutex.withLock { cache.map { (k, v) -> k as K to v as V }.toMap() }
-
 }
