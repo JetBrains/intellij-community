04f40c38711a97a7fa2ee26d60190117b31a234a
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-11-06 20:04:33 +0300

    [documentation] make sure `ProgressManager.checkCanceled()` works inside non-suspending lambdas
    (cherry picked from commit 742cacfe44cecd179d10fbd7061ea5079c439fb8)
    
    IJ-CR-16613
    
    GitOrigin-RevId: ec024e33eac04322da993d4bd6d377bd59ebfbb8

--- platform/lang-impl/src/com/intellij/lang/documentation/classes.kt
+++ platform/lang-impl/src/com/intellij/lang/documentation/classes.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.lang.documentation
 
+import com.intellij.openapi.progress.withJob
 import com.intellij.util.AsyncSupplier
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -21,6 +22,8 @@ internal class AsyncDocumentation(
 
 internal fun <X> Supplier<X>.asAsyncSupplier(): AsyncSupplier<X> = {
   withContext(Dispatchers.IO) {
-    this@asAsyncSupplier.get()
+    withJob {
+      this@asAsyncSupplier.get()
+    }
   }
 }
