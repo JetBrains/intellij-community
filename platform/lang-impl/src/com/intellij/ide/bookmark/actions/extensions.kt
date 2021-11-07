c76b382aafebd99b989ffc1c179eef945bd37ee8
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2021-11-08 00:05:18 +0300

    IDEA-280568: Bookmarks window: go to source shortcut not supported
    GitOrigin-RevId: 9cf6e44e92ec371b6c835910c7eb099c123c6d48

--- platform/lang-impl/src/com/intellij/ide/bookmark/actions/extensions.kt
+++ platform/lang-impl/src/com/intellij/ide/bookmark/actions/extensions.kt
@@ -2,20 +2,27 @@
 package com.intellij.ide.bookmark.actions
 
 import com.intellij.ide.bookmark.Bookmark
+import com.intellij.ide.bookmark.BookmarkType
 import com.intellij.ide.bookmark.BookmarksManager
 import com.intellij.ide.bookmark.providers.LineBookmarkProvider
 import com.intellij.ide.bookmark.ui.BookmarksView
 import com.intellij.ide.bookmark.ui.BookmarksViewState
 import com.intellij.ide.bookmark.ui.tree.GroupNode
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.actionSystem.CommonShortcuts
+import com.intellij.openapi.actionSystem.CustomShortcutSet
 import com.intellij.openapi.actionSystem.PlatformDataKeys
 import com.intellij.openapi.editor.ex.EditorGutterComponentEx
+import com.intellij.openapi.project.LightEditActionFactory
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowId
 import com.intellij.openapi.wm.ToolWindowManager
+import com.intellij.util.OpenSourceUtil
 import com.intellij.util.ui.tree.TreeUtil
 import java.awt.Component
+import javax.swing.JComponent
 import javax.swing.JTree
 
 
@@ -67,3 +74,18 @@ internal val Bookmark.bookmarksManager
 
 internal val Bookmark.firstGroupWithDescription
   get() = bookmarksManager?.getGroups(this)?.firstOrNull { it.getDescription(this).isNullOrBlank().not() }
+
+
+/**
+ * Creates an action that navigates to a bookmark by a digit or a letter.
+ */
+internal fun JComponent.registerBookmarkTypeAction(parent: Disposable, type: BookmarkType) = LightEditActionFactory
+  .create { it.bookmarksManager?.getBookmark(type)?.run { OpenSourceUtil.navigate(this) } }
+  .registerCustomShortcutSet(CustomShortcutSet.fromString(type.mnemonic.toString()), this, parent)
+
+/**
+ * Creates an action that navigates to a selected bookmark by the EditSource shortcut.
+ */
+internal fun JComponent.registerEditSourceAction(parent: Disposable) = LightEditActionFactory
+  .create { OpenSourceUtil.navigate(*it.getData(CommonDataKeys.NAVIGATABLE_ARRAY)) }
+  .registerCustomShortcutSet(CommonShortcuts.getEditSource(), this, parent)
