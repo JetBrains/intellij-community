0997e9658a1b93a5d5869fc5e404b40413af2083
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2021-11-07 22:03:21 +0300

    Hide all corresponding actions if Favorites is disabled
    GitOrigin-RevId: 8f83e87402f6dff1fc2b7f9dabbc03e9c73df5e9

--- platform/lang-impl/src/com/intellij/ide/favoritesTreeView/actions/AddToFavoritesAction.java
+++ platform/lang-impl/src/com/intellij/ide/favoritesTreeView/actions/AddToFavoritesAction.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.ide.favoritesTreeView.actions;
 
@@ -18,6 +18,7 @@ import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.registry.Registry;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
@@ -81,6 +82,10 @@ public class AddToFavoritesAction extends AnAction implements DumbAware {
 
   @Override
   public void update(@NotNull AnActionEvent e) {
+    if (!Registry.is("ide.favorites.tool.window.applicable", false)) {
+      e.getPresentation().setEnabledAndVisible(false);
+      return;
+    }
     e.getPresentation().setEnabled(canCreateNodes(e, myFavoritesListName));
   }
 
--- platform/lang-impl/src/com/intellij/ide/favoritesTreeView/actions/SendToFavoritesGroup.java
+++ platform/lang-impl/src/com/intellij/ide/favoritesTreeView/actions/SendToFavoritesGroup.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.ide.favoritesTreeView.actions;
 
@@ -9,6 +9,7 @@ import com.intellij.ide.favoritesTreeView.FavoritesTreeViewPanel;
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.registry.Registry;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -40,6 +41,10 @@ public class SendToFavoritesGroup extends ActionGroup implements DumbAware {
 
   @Override
   public void update(@NotNull AnActionEvent e) {
+    if (!Registry.is("ide.favorites.tool.window.applicable", false)) {
+      e.getPresentation().setEnabledAndVisible(false);
+      return;
+    }
     super.update(e);
     e.getPresentation().setVisible(SendToFavoritesAction.isEnabled(e)
                                    && e.getData(FavoritesTreeViewPanel.FAVORITES_LIST_NAME_DATA_KEY) != null);
