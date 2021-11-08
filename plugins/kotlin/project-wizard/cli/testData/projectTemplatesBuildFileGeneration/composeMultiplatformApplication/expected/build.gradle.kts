dc468025e87e1cd145da30c16e8de648251140ca
akurasov <alexander.kurasov@jetbrains.com>
2021-11-08 14:57:20 +0300

    Switching Compose version to Beta in Compose Project Wizard
    (cherry picked from commit 8998115c1fdda9c7a7cc4b05e9c543409f643300)
    
    GitOrigin-RevId: 14a6a31a3a7006c6caccbab480f91bb1ec6fa834

--- plugins/kotlin/project-wizard/cli/testData/projectTemplatesBuildFileGeneration/composeMultiplatformApplication/expected/build.gradle.kts
+++ plugins/kotlin/project-wizard/cli/testData/projectTemplatesBuildFileGeneration/composeMultiplatformApplication/expected/build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
         classpath("com.android.tools.build:gradle:4.1.2")
     }
 }
