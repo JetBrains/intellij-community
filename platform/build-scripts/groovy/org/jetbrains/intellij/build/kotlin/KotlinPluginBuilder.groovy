6b12fac84eae6dfa8842261095265686d963220c
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2021-11-09 16:42:28 +0100

    [kotlin] Correct plugin version kind
    (cherry picked from commit 532326b8bbd25b7f5076fab9d23f87105984a7f1)
    
    GitOrigin-RevId: 51a186bb7e81850ae81f625cd69aeb80e7f81b49

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/kotlin/KotlinPluginBuilder.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/kotlin/KotlinPluginBuilder.groovy
@@ -280,12 +280,27 @@ class KotlinPluginBuilder {
               throw new IllegalStateException("Can't determine Kotlin compiler version")
             }
             String version = "${major}-${kotlinVersion}-${kind}${minor}"
-            context.messages.info("version: $version [kotlin.plugin.kind: ${System.getProperty("kotlin.plugin.kind")}]")
+            context.messages.info("version: $version")
             return version
           } else {
+            // 221-1.5.10-release-IJ916
+            Matcher kotlinPluginIJBuildNumber = Pattern.compile("^(\\d+)-([.\\d]+)-(\\w+)-([A-Z]+)(\\d+)\$").matcher(buildNumber)
+
+            if (kotlinPluginIJBuildNumber.matches()) {
+              String major = kotlinPluginIJBuildNumber.group(1)
+              String kotlinVersion = kotlinPluginIJBuildNumber.group(2)
+              String type = kotlinPluginIJBuildNumber.group(3)
+              String buildKind = kotlinPluginIJBuildNumber.group(4)
+              String minor = kotlinPluginIJBuildNumber.group(5)
+
+              String version = "${major}-${kotlinVersion}-${type}-${kind}${minor}"
+              context.messages.info("Kotlin plugin IJ version: $version")
+              return version
+            }
+
             // Build number isn't recognized as IJ build number then it means build
             // number must be plain Kotlin plugin version which we can use directly
-            context.messages.info("buildNumber version: $buildNumber [kotlin.plugin.kind: ${System.getProperty("kotlin.plugin.kind")}]")
+            context.messages.info("buildNumber version: $buildNumber")
             return buildNumber
           }
         }
