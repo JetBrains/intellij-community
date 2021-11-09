cad35e18fd2c3f1a2aa7f4746980807779519db3
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2021-11-09 15:52:16 +0100

    [integration-tests] Temporary fix for Maven import
    GitOrigin-RevId: 52da06a15f5ed1c01d409c7a580cf2444a7217b6

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/wizards/MavenProjectBuilder.java
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/wizards/MavenProjectBuilder.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.idea.maven.wizards;
 
 import com.intellij.openapi.application.ApplicationManager;
@@ -223,10 +223,11 @@ public final class MavenProjectBuilder extends ProjectImportBuilder<MavenProject
     }
 
     manager.waitForReadingCompletion();
+    //noinspection UnresolvedPluginConfigReference
     if (ApplicationManager.getApplication().isHeadlessEnvironment() &&
         !CoreProgressManager.shouldKeepTasksAsynchronousInHeadlessMode() &&
-        !MavenUtil.isMavenUnitTestModeEnabled()) {
-
+        (!MavenUtil.isMavenUnitTestModeEnabled() || Registry.is("ide.force.maven.import", false)) // workaround for inspection integration test
+    ) {
       Promise<List<Module>> promise = manager.scheduleImportAndResolve();
       manager.waitForResolvingCompletion();
       try {
