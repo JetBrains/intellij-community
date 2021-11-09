2583ccb86456bf65cc669383896e7ea85ab8f183
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2021-11-09 15:39:31 +0300

    Fix IDEA-281204 Ctrl+F1 doesn't work in EditorConfig
    Show element name in the doc tab when possible
    
    (cherry picked from commit cf6f197e614b13bd85ff11c6a730176e0119389a)
    
    IJ-CR-16834
    
    GitOrigin-RevId: a8b27115d0290f13930cf6e479796d01526642df

--- plugins/editorconfig/test/org/editorconfig/language/documentation/EditorConfigDocumentationTest.kt
+++ plugins/editorconfig/test/org/editorconfig/language/documentation/EditorConfigDocumentationTest.kt
@@ -2,6 +2,8 @@
 package org.editorconfig.language.documentation
 
 import com.intellij.codeInsight.documentation.DocumentationManager
+import com.intellij.lang.documentation.ide.IdeDocumentationTargetProvider
+import com.intellij.testFramework.UsefulTestCase
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import org.editorconfig.language.codeinsight.documentation.EditorConfigDocumentationProvider
 
@@ -14,8 +16,14 @@ class EditorConfigDocumentationTest : BasePlatformTestCase() {
         indent_<caret>size = 2
       """.trimIndent()
     )
-    val originalElement = myFixture.file.findElementAt(myFixture.editor.caretModel.offset)
+    val offset = myFixture.editor.caretModel.offset
+    val originalElement = myFixture.file.findElementAt(offset)
     val element = DocumentationManager.getInstance(project).findTargetElement(myFixture.editor, myFixture.file)
+    val targets = IdeDocumentationTargetProvider.getInstance(project).documentationTargets(myFixture.editor, myFixture.file, offset)
+    UsefulTestCase.assertSize(1, targets)
+    val target = targets[0]
+    val text = target.presentation.presentableText
+    assertEquals("indent_size", text)
     val doc = EditorConfigDocumentationProvider().generateDoc(element, originalElement)
     val expected = "number of whitespace symbols used for indents"
     assertEquals(expected, doc)
