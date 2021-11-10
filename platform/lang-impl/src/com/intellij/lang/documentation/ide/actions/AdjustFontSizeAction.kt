a0b76885b893bf9e11371ee6be9e573a18b55093
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-11-10 16:55:20 +0100

    IDEA-281582 [documentation] extract `documentationToolWindowUI` from `AdjustFontSizeAction`
    (cherry picked from commit 86847796284c81b2001bea9ebb6acc3f44de79c9)
    
    IJ-CR-16855
    
    GitOrigin-RevId: b2a5856359be0de86eae9b4af23fbee03e99301f

--- platform/lang-impl/src/com/intellij/lang/documentation/ide/actions/AdjustFontSizeAction.kt
+++ platform/lang-impl/src/com/intellij/lang/documentation/ide/actions/AdjustFontSizeAction.kt
@@ -5,31 +5,17 @@ import com.intellij.codeInsight.CodeInsightBundle
 import com.intellij.codeInsight.documentation.DocFontSizePopup
 import com.intellij.codeInsight.documentation.DocumentationEditorPane
 import com.intellij.codeInsight.hint.HintManagerImpl.ActionToIgnore
-import com.intellij.lang.documentation.ide.impl.DocumentationToolWindowManager
-import com.intellij.lang.documentation.ide.ui.toolWindowUI
-import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.actionSystem.PlatformCoreDataKeys
 import com.intellij.openapi.project.DumbAware
-import com.intellij.openapi.wm.impl.content.BaseLabel
 
 internal class AdjustFontSizeAction : AnAction(CodeInsightBundle.message("javadoc.adjust.font.size")), ActionToIgnore, DumbAware {
 
   private fun editorPane(dc: DataContext): DocumentationEditorPane? {
-    val component = dc.getData(PlatformCoreDataKeys.CONTEXT_COMPONENT)
-    if (component is DocumentationEditorPane) {
-      return component
-    }
-    val toolWindow = dc.getData(PlatformDataKeys.TOOL_WINDOW)
-                     ?: return null
-    if (toolWindow.id != DocumentationToolWindowManager.TOOL_WINDOW_ID) {
-      return null
-    }
-    val content = if (component is BaseLabel) {
-      component.content
-    }
-    else {
-      toolWindow.contentManager.selectedContent
-    }
-    return content?.toolWindowUI?.editorPane
+    return dc.getData(PlatformCoreDataKeys.CONTEXT_COMPONENT) as? DocumentationEditorPane
+           ?: documentationToolWindowUI(dc)?.editorPane
   }
 
   override fun update(e: AnActionEvent) {
