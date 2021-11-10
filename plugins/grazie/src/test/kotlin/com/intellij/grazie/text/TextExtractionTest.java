1ac170d3191a37a98687f851941d85918b763abf
Peter Gromov <peter@jetbrains.com>
2021-11-10 13:43:12 +0100

    [grazie] don't query TextExtractor multiple times for the same PSI (IDEA-282252)
    When starting from a leaf that's excluded, we found nothing suitable in the cache and queried the TextExtractor again and again. Now we cache the query results separately.
    
    In addition, we now avoid doing expensive injection/todo checks on each query, performing them only once.
    
    (cherry picked from commit 03c7598aee94e98234fecc0f9f09afd64334c48c)
    
    IJ-CR-16848
    
    GitOrigin-RevId: 642b6fa44f47365eed67a99845a8f9791158ec21

--- plugins/grazie/src/test/kotlin/com/intellij/grazie/text/TextExtractionTest.java
+++ plugins/grazie/src/test/kotlin/com/intellij/grazie/text/TextExtractionTest.java
@@ -19,9 +19,13 @@ import com.intellij.psi.xml.XmlTag;
 import com.intellij.testFramework.PlatformTestUtil;
 import com.intellij.testFramework.fixtures.BasePlatformTestCase;
 import org.intellij.lang.regexp.RegExpLanguage;
+import org.intellij.plugins.markdown.lang.MarkdownFileType;
+import org.intellij.plugins.markdown.lang.MarkdownLanguage;
+import org.intellij.plugins.markdown.lang.psi.impl.MarkdownParagraphImpl;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.Set;
 
 import static com.intellij.grazie.text.TextContentTest.unknownOffsets;
 
@@ -236,6 +240,33 @@ public class TextExtractionTest extends BasePlatformTestCase {
     PlatformTestUtil.startPerformanceTest("TextContent building from a long text fragment", 200, () -> {
       assertEquals(expected, extractor.buildTextContent(literal, TextContent.TextDomain.ALL).toString());
     }).assertTiming();
+  }
+
+  public void testCachingWorks() {
+    TextExtractor delegate = TextExtractor.EP.forLanguage(MarkdownLanguage.INSTANCE);
+    var countingExtractor = new TextExtractor() {
+      int count = 0;
+
+      @Override
+      protected @NotNull List<TextContent> buildTextContents(@NotNull PsiElement element,
+                                                             @NotNull Set<TextContent.TextDomain> allowedDomains) {
+        if (element instanceof MarkdownParagraphImpl) {
+          count++;
+        }
+        return delegate.buildTextContents(element, allowedDomains);
+      }
+    };
+    TextExtractor.EP.addExplicitExtension(MarkdownLanguage.INSTANCE, countingExtractor);
+    disposeOnTearDown(() -> TextExtractor.EP.removeExplicitExtension(MarkdownLanguage.INSTANCE, countingExtractor));
+
+    PsiFile file = PsiFileFactory.getInstance(getProject()).createFileFromText(
+      "a.md", MarkdownFileType.INSTANCE,
+      "[Before ![AltText](http://www.google.com.au/images/nav_logo7.png) after](http://google.com.au/)");
+    for (int i = 0; i <= file.getTextLength(); i++) {
+      TextExtractor.findTextAt(file, i, TextContent.TextDomain.ALL);
+    }
+    // should be invoked once, but allow some repetitions in case GC kicks in
+    assertTrue(String.valueOf(countingExtractor.count), countingExtractor.count < 3);
   }
 
   private TextContent extractText(String fileName, String fileText, int offset) {
