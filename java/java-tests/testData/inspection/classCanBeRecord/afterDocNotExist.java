88faadb23a0ea1bd84f380653ffac3f3dc4b8148
Ilyas Selimov <ilyas.selimov@jetbrains.com>
2021-11-10 20:24:54 +0700

    IDEA-279215 - preserving fields' comments during class-ro-record conversion
    GitOrigin-RevId: 762bb177f566fc8f75d155d41e8feceae8008df6

---
+++ java/java-tests/testData/inspection/classCanBeRecord/afterDocNotExist.java
@@ -0,0 +1,11 @@
+// "Convert to a record" "true"
+
+/**
+ * @param x head doc for x
+ *          another head doc for xtail doc for x
+ * @param y head doc for y
+ *          internal doc for y
+ *          tail doc for y
+ */
+record Foo(int x, int y, int z) {
+}
