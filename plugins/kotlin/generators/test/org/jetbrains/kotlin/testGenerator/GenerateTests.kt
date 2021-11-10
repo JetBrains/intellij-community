d2228e6cb6eae8ea2582843f930ce27b911d92a7
Jinseong Jeon <jsjeon@google.com>
2021-11-09 22:00:11 -0800

    FIR LL: restore AbstractFirLibraryModuleDeclarationResolveTest
    GitOrigin-RevId: eeb7521ace43d1830f9205abaea23bbadf95c316

--- plugins/kotlin/generators/test/org/jetbrains/kotlin/testGenerator/GenerateTests.kt
+++ plugins/kotlin/generators/test/org/jetbrains/kotlin/testGenerator/GenerateTests.kt
@@ -92,6 +92,7 @@ import org.jetbrains.kotlin.idea.folding.AbstractKotlinFoldingTest
 //import org.jetbrains.kotlin.idea.fir.inspections.AbstractHLInspectionTest
 //import org.jetbrains.kotlin.idea.fir.inspections.AbstractHLLocalInspectionTest
 //import org.jetbrains.kotlin.idea.fir.intentions.AbstractHLIntentionTest
+//import org.jetbrains.kotlin.idea.fir.low.level.api.AbstractFirLibraryModuleDeclarationResolveTest
 //import org.jetbrains.kotlin.idea.fir.parameterInfo.AbstractFirParameterInfoTest
 //import org.jetbrains.kotlin.idea.fir.quickfix.AbstractHighLevelQuickFixMultiFileTest
 //import org.jetbrains.kotlin.idea.fir.quickfix.AbstractHighLevelQuickFixTest
@@ -1032,6 +1033,11 @@ private fun assembleWorkspace(): TWorkspace = workspace {
         }
     }*/
 
+    testGroup("fir-low-level-api-ide-impl") {
+        testClass<AbstractFirLibraryModuleDeclarationResolveTest> {
+            model("libraryModuleResolve", isRecursive = false)
+        }
+    }
 
     /*testGroup("idea/idea-fir-performance-tests/tests", "idea") {
          testClass<AbstractFirHighlightingPerformanceTest> {
