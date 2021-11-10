c2f6e209bb6a4edfa4126590a47acdbcc5956285
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-11-10 15:36:19 +0100

    IDEA-281582 [documentation] teach `AdjustFontSizeAction` to work in the tool window gear
    (cherry picked from commit 83a9372d094a3bba562d4be5fcfabd09516fae77)
    
    IJ-CR-16855
    
    GitOrigin-RevId: f4671b6f21aff1309ad451b7e4bf2420962e2514

--- platform/lang-impl/src/com/intellij/lang/documentation/ide/actions/AdjustFontSizeAction.kt
+++ platform/lang-impl/src/com/intellij/lang/documentation/ide/actions/AdjustFontSizeAction.kt
@@ -5,19 +5,39 @@ import com.intellij.codeInsight.CodeInsightBundle
 import com.intellij.codeInsight.documentation.DocFontSizePopup
 import com.intellij.codeInsight.documentation.DocumentationEditorPane
 import com.intellij.codeInsight.hint.HintManagerImpl.ActionToIgnore
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.PlatformCoreDataKeys
+import com.intellij.lang.documentation.ide.impl.DocumentationToolWindowManager
+import com.intellij.lang.documentation.ide.ui.toolWindowUI
+import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.wm.impl.content.BaseLabel
 
 internal class AdjustFontSizeAction : AnAction(CodeInsightBundle.message("javadoc.adjust.font.size")), ActionToIgnore, DumbAware {
 
+  private fun editorPane(dc: DataContext): DocumentationEditorPane? {
+    val component = dc.getData(PlatformCoreDataKeys.CONTEXT_COMPONENT)
+    if (component is DocumentationEditorPane) {
+      return component
+    }
+    val toolWindow = dc.getData(PlatformDataKeys.TOOL_WINDOW)
+                     ?: return null
+    if (toolWindow.id != DocumentationToolWindowManager.TOOL_WINDOW_ID) {
+      return null
+    }
+    val content = if (component is BaseLabel) {
+      component.content
+    }
+    else {
+      toolWindow.contentManager.selectedContent
+    }
+    return content?.toolWindowUI?.editorPane
+  }
+
   override fun update(e: AnActionEvent) {
-    e.presentation.isEnabledAndVisible = e.dataContext.getData(PlatformCoreDataKeys.CONTEXT_COMPONENT) is DocumentationEditorPane
+    e.presentation.isEnabledAndVisible = editorPane(e.dataContext) != null
   }
 
   override fun actionPerformed(e: AnActionEvent) {
-    val editorPane = e.dataContext.getData(PlatformCoreDataKeys.CONTEXT_COMPONENT) as DocumentationEditorPane
+    val editorPane = requireNotNull(editorPane(e.dataContext))
     DocFontSizePopup.show(editorPane, editorPane::applyFontProps)
   }
 }
