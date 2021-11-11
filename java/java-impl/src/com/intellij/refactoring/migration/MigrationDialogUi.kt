0474967aac5ca859c4856aa58fc62bdd3bbff5dc
Louis Vignier <louis.vignier@jetbrains.com>
2021-11-11 18:19:20 +0100

    [java] Fix too wide combo in the migration dialog
    (cherry picked from commit 4be45fdf65870d3977299c2186fb9d47da271bf6)
    
    GitOrigin-RevId: 807fb34e9fe0c49e12c2f022d8e8fcb4cee0c81a

--- java/java-impl/src/com/intellij/refactoring/migration/MigrationDialogUi.kt
+++ java/java-impl/src/com/intellij/refactoring/migration/MigrationDialogUi.kt
@@ -5,9 +5,9 @@ import com.intellij.application.options.ModulesComboBox
 import com.intellij.java.refactoring.JavaRefactoringBundle
 import com.intellij.ui.components.ActionLink
 import com.intellij.ui.dsl.builder.BottomGap
-import com.intellij.ui.dsl.builder.RowLayout
 import com.intellij.ui.dsl.builder.panel
 import com.intellij.ui.dsl.gridLayout.HorizontalAlign
+import com.intellij.util.ui.JBUI
 import javax.swing.JComponent
 import javax.swing.JEditorPane
 import javax.swing.JLabel
@@ -36,6 +36,7 @@ class MigrationDialogUi(map: MigrationMap?) {
 
     row(JavaRefactoringBundle.message("migration.dialog.scope.label")) {
       modulesCombo = cell(ModulesComboBox())
+        .applyToComponent { setMinimumAndPreferredWidth(JBUI.scale(380)) }
         .horizontalAlign(HorizontalAlign.FILL)
         .component
       bottomGap(BottomGap.SMALL)
@@ -45,7 +46,6 @@ class MigrationDialogUi(map: MigrationMap?) {
       descriptionLabel = text(map?.description ?: "", 68)
         .component
     }
-      .layout(RowLayout.INDEPENDENT)
   }
 
   fun preferredFocusedComponent(): JComponent = editLink
