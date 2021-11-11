af2b65dc1c2e41d7828b9430744b4f22c00c71bb
Mikhail Filippov <mikhail@filippov.me>
2021-11-11 10:35:16 +0300

    Support macOS in remote-dev-server.sh
    GitOrigin-RevId: 2addedf7f06641df55c82bb045b168a47a68647c

--- platform/build-scripts/resources/linux/scripts/remote-dev-server.sh
+++ platform/build-scripts/resources/linux/scripts/remote-dev-server.sh
@@ -2,9 +2,9 @@
 # Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 ### See plugins/remote-dev-server/bin/launcher.sh for actual launcher code
+set -e -u -f
 
-SCRIPT_PATH="$(realpath "$0")"
-IDE_BIN_DIR="$(dirname "$SCRIPT_PATH")"
+IDE_BIN_DIR="$(cd "$(dirname "$0")" && pwd)"
 IDE_HOME="$(dirname "$IDE_BIN_DIR")"
 REMOTE_DEV_SERVER_DIR="$IDE_HOME/plugins/remote-dev-server/bin"
 REMOTE_DEV_SERVER_LAUNCHER_PATH="$REMOTE_DEV_SERVER_DIR/launcher.sh"
