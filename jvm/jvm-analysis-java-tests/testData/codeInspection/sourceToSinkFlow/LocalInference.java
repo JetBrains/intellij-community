d42856d2a61d44ca59d704960bf53113fbd6e91c
Artemiy Sartakov <artemiy.sartakov@jetbrains.com>
2021-11-12 16:53:26 +0700

    SourceToSinkFlowInspection: better message when unsafe/unknown string is assigned to safe local var (IDEA-281280)
    GitOrigin-RevId: 9761dc6d69ecaece443a7e72e40bb8d2155ef893

--- jvm/jvm-analysis-java-tests/testData/codeInspection/sourceToSinkFlow/LocalInference.java
+++ jvm/jvm-analysis-java-tests/testData/codeInspection/sourceToSinkFlow/LocalInference.java
@@ -5,32 +5,32 @@ public class LocalInference {
     void simpleInit() {
       String s1 = source();
       String s = s1;
-      sink(<warning descr="Unsafe string is passed to safe method">s</warning>);
+      sink(<warning descr="Unsafe string is used as safe parameter">s</warning>);
     }
 
     void concatInit() {
       String s1 = "foo" + source() + "bar";
       String s = "foo" + s1 + "bar";
-      sink(<warning descr="Unsafe string is passed to safe method">s</warning>);
+      sink(<warning descr="Unsafe string is used as safe parameter">s</warning>);
     }
 
     void recursive() {
       String s1 = source();
       String s = s1;
       s1 = s;
-      sink(<warning descr="Unsafe string is passed to safe method">s</warning>);
+      sink(<warning descr="Unsafe string is used as safe parameter">s</warning>);
     }
 
     void ternary(boolean b) {
       String s1 = b ? source() : "";
       String s = s1;
-      sink(<warning descr="Unsafe string is passed to safe method">s</warning>);
+      sink(<warning descr="Unsafe string is used as safe parameter">s</warning>);
     }
 
     void nestedTernary(boolean b, boolean c) {
       String s1 = b ? c ? "" : (source()) : "";
       String s = s1;
-      sink(<warning descr="Unsafe string is passed to safe method">s</warning>);
+      sink(<warning descr="Unsafe string is used as safe parameter">s</warning>);
     }
 
     void blocks() {
@@ -39,7 +39,7 @@ public class LocalInference {
         s1 = source();
         {
           String s = s1;
-          sink(<warning descr="Unsafe string is passed to safe method">s</warning>);
+          sink(<warning descr="Unsafe string is used as safe parameter">s</warning>);
         }
       }
     }
@@ -51,7 +51,7 @@ public class LocalInference {
       }
       String s1 = ((s2) + (safe()));
       String s = s1;
-      sink(<warning descr="Unsafe string is passed to safe method">s</warning>);
+      sink(<warning descr="Unsafe string is used as safe parameter">s</warning>);
     }
 
     void transitiveRecursive(boolean b) {
@@ -60,7 +60,7 @@ public class LocalInference {
       String s2 = s1 + foo();
       String s3 = s2;
       if (b) s = s3;
-      sink(<warning descr="Unknown string is passed to safe method">s</warning>);
+      sink(<warning descr="Unknown string is used as safe parameter">s</warning>);
     }
 
     String foo() {
