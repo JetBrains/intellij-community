fb3547560e9e8f153a65e0291d1090e78ffa0c01
Yaroslav.Chernyshev <Yaroslav.Chernyshev@jetbrains.com>
2021-11-12 22:13:20 +0300

    [kpm] Introduce basic tooling entities for KPM: KotlinModule, KotlinFragment, KotlinVariant
    GitOrigin-RevId: b97083d2e3a3b00ec57ab9113d05347eb24f9cc1

---
+++ plugins/kotlin/project-model/src/org/jetbrains/kotlin/idea/projectModel/KotlinFragment.kt
@@ -0,0 +1,12 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.kotlin.idea.projectModel
+
+import java.io.File
+
+interface KotlinFragment : KotlinComponent {
+    val moduleIdentifier: KotlinModuleIdentifier
+    val languageSettings: KotlinLanguageSettings?
+    val directRefinesFragments: Collection<KotlinFragment>
+    val sourceDirs: Set<File>
+    val resourceDirs: Set<File>
+}
