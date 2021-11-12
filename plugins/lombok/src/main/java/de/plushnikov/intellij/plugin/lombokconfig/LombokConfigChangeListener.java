be07287976266c1b9957e562a36267007a4f16b9
Michail Plushnikov <michail.plushnikov@jetbrains.com>
2021-11-12 19:42:22 +0100

    [lombok] IDEA-282112 added additional quick filename-pre-check
    GitOrigin-RevId: c2c205713464a2dbf71e7e969758010675913adf

--- plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/lombokconfig/LombokConfigChangeListener.java
+++ plugins/lombok/src/main/java/de/plushnikov/intellij/plugin/lombokconfig/LombokConfigChangeListener.java
@@ -3,6 +3,7 @@ package de.plushnikov.intellij.plugin.lombokconfig;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.fileTypes.FileTypeRegistry;
 import com.intellij.openapi.util.ModificationTracker;
+import com.intellij.openapi.util.text.Strings;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.newvfs.BulkFileListener;
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
@@ -22,11 +23,13 @@ public class LombokConfigChangeListener implements BulkFileListener {
       VirtualFile eventFile = event.getFile();
       if (null != eventFile) {
         final CharSequence nameSequence = eventFile.getNameSequence();
-        final FileType fileType = FileTypeRegistry.getInstance().getFileTypeByFileName(nameSequence);
+        if (Strings.endsWith(nameSequence, "lombok.config")) {
+          final FileType fileType = FileTypeRegistry.getInstance().getFileTypeByFileName(nameSequence);
 
-        if (LombokConfigFileType.INSTANCE.equals(fileType)) {
-          CONFIG_CHANGE_COUNTER.incrementAndGet();
-          break;
+          if (LombokConfigFileType.INSTANCE.equals(fileType)) {
+            CONFIG_CHANGE_COUNTER.incrementAndGet();
+            break;
+          }
         }
       }
     }
