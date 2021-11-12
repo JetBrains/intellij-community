b49d6770718998f93c9584fe80e5035c3566ed3f
Konstantin Kolosovsky <Konstantin.Kolosovsky@jetbrains.com>
2021-11-12 21:08:46 +0000

    vcs: Update documentation for "add checkin handler" api
    GitOrigin-RevId: 4e3cc39ab65e4951dd8d5885c4ed2ef694e28182

--- platform/vcs-api/vcs-api-core/src/com/intellij/openapi/vcs/changes/CommitContext.kt
+++ platform/vcs-api/vcs-api-core/src/com/intellij/openapi/vcs/changes/CommitContext.kt
@@ -1,10 +1,20 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.vcs.changes
 
 import com.intellij.openapi.util.UserDataHolderBase
 import com.intellij.util.NullableFunction
 import com.intellij.util.PairConsumer
 
+/**
+ * Contains specific commit data used in the commit flow. For example:
+ * - if "amend commit" should be performed instead of usual commit
+ * - if "commit and push" was invoked and so push should start right after commit
+ *
+ * @see com.intellij.openapi.vcs.checkin.CheckinHandler
+ * @see com.intellij.openapi.vcs.changes.CommitExecutor
+ * @see com.intellij.openapi.vcs.changes.CommitSession
+ * @see com.intellij.openapi.vcs.checkin.CheckinEnvironment
+ */
 class CommitContext : UserDataHolderBase() {
   private val _additionalData = HashMap<Any, Any>()
 
