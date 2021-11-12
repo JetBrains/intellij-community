9cbcb9ff63e42d13bbb9f9e62bd322e5a9a684a4
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2021-11-12 10:11:22 +0700

    [property-tests] KotlinIntentionPolicy: ignore "Convert to enum class" (KTIJ-20104)
    GitOrigin-RevId: f7548a1e7d1f22ebf81a94d37f18a2808e62b09a

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/propertyBased/KotlinIntentionPolicy.kt
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/propertyBased/KotlinIntentionPolicy.kt
@@ -13,7 +13,10 @@ internal class KotlinIntentionPolicy : IntentionPolicy() {
     override fun shouldSkipIntention(actionText: String): Boolean =
         // These intentions don't modify code (probably should not start in write-action?)
         actionText == "Enable a trailing comma by default in the formatter" ||
-        actionText == "Disable a trailing comma by default in the formatter"
+        actionText == "Disable a trailing comma by default in the formatter" ||
+        // May produce error hint instead of actual action which results in RefactoringErrorHintException
+        // TODO: ignore only exceptional case but proceed with normal one
+        actionText == "Convert to enum class"
 
     override fun mayBreakCode(action: IntentionAction, editor: Editor, file: PsiFile): Boolean = false
 
