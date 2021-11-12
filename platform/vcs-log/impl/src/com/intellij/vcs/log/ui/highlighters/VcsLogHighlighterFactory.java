8c33a66b42b6f011b36f7bb0e6f8ffcebfd54854
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2021-11-12 20:41:52 +0300

    [vcs-log] add javadoc to VcsLogHighlighterFactory
    GitOrigin-RevId: 079b3b021d11c1101d745ca100767c9399740fb3

--- platform/vcs-log/impl/src/com/intellij/vcs/log/ui/highlighters/VcsLogHighlighterFactory.java
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/ui/highlighters/VcsLogHighlighterFactory.java
@@ -7,16 +7,49 @@ import com.intellij.vcs.log.data.VcsLogData;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * A factory for creating {@link VcsLogHighlighter} instances to customize commit presentation (such as color and text style)
+ * in the Vcs Log table.
+ *
+ * @see VcsLogHighlighter
+ * @see VcsLogHighlighter.VcsCommitStyle
+ */
 public interface VcsLogHighlighterFactory {
+  /**
+   * Creates a new {@link VcsLogHighlighter} instance for the specified {@link VcsLogData} and {@link VcsLogUi}.
+   *
+   * @param logData a {@link VcsLogData} instance
+   * @param logUi   a {@link VcsLogUi} instance
+   * @return a new highlighter instance
+   * @see VcsLogHighlighter
+   */
   @NotNull
   VcsLogHighlighter createHighlighter(@NotNull VcsLogData logData, @NotNull VcsLogUi logUi);
 
+  /**
+   * A unique id of this factory.
+   *
+   * @return id string
+   */
   @NotNull
   String getId();
 
+  /**
+   * A name for this highlighter. This name is used for showing a toggle action to enable or disable the highlighter
+   * from the Vcs Log presentation settings.
+   *
+   * @return a name string
+   * @see VcsLogHighlighterFactory#showMenuItem()
+   */
   @NotNull
   @Nls(capitalization = Nls.Capitalization.Title)
   String getTitle();
 
+  /**
+   * Return true to show a toggle action to enable or disable this highlighter in the Vcs Log presentation settings.
+   *
+   * @return true to display a toggle action for the highlighter, false otherwise
+   * @see VcsLogHighlighterFactory#getTitle()
+   */
   boolean showMenuItem();
 }
