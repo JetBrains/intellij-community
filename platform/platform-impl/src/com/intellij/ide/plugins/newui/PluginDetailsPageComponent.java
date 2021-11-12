79f1e425e73c0bcbf418c13b42b2fc39154f6262
Alexander Lobas <Alexander.Lobas@jetbrains.com>
2021-11-12 18:16:32 +0300

    IDEA-259584 Font of plugin's version breaks when IDE UI font is changed
    GitOrigin-RevId: e09fbd7daf6bf5f00faf7c31bced8bf0e65a350b

--- platform/platform-impl/src/com/intellij/ide/plugins/newui/PluginDetailsPageComponent.java
+++ platform/platform-impl/src/com/intellij/ide/plugins/newui/PluginDetailsPageComponent.java
@@ -292,7 +292,23 @@ public class PluginDetailsPageComponent extends MultiPanel {
 
   private void createMetricsPanel(@NotNull JPanel centerPanel) {
     // text field without horizontal margins
-    myVersion = new JTextField();
+    myVersion = new JTextField() {
+      @Override
+      public void setBorder(Border border) {
+        super.setBorder(null);
+      }
+
+      @Override
+      public void updateUI() {
+        super.updateUI();
+        if (myVersion != null) {
+          PluginDetailsPageComponent.setFont(myVersion);
+        }
+        if (myVersionSize != null) {
+          PluginDetailsPageComponent.setFont(myVersionSize);
+        }
+      }
+    };
     myVersion.putClientProperty("TextFieldWithoutMargins", Boolean.TRUE);
     myVersion.setEditable(false);
     setFont(myVersion);
