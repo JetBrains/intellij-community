e395b1958eec1de5572c84729fcc86e7852ed83c
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-11-12 17:35:57 +0100

    [cwm] add ability to pass around `ClientId` and set it in coroutines
    GitOrigin-RevId: 03bf1ae4cd161f0d4893ddc1258622bb7fcdf259

--- platform/core-api/src/com/intellij/codeWithMe/ClientId.kt
+++ platform/core-api/src/com/intellij/codeWithMe/ClientId.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeWithMe
 
 import com.intellij.openapi.Disposable
@@ -7,10 +7,13 @@ import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.diagnostic.trace
 import com.intellij.openapi.util.Disposer
 import com.intellij.util.Processor
+import kotlinx.coroutines.ThreadContextElement
 import org.jetbrains.annotations.ApiStatus
 import java.util.concurrent.Callable
 import java.util.function.BiConsumer
 import java.util.function.Function
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
 /**
  * ClientId is a global context class that is used to distinguish the originator of an action in multi-client systems
@@ -273,6 +276,8 @@ data class ClientId(val value: String) {
       val currentId = currentOrNull
       return Processor { withClientId(currentId) { processor.process(it) } }
     }
+
+    fun coroutineContext(): CoroutineContext = currentOrNull?.asContextElement() ?: EmptyCoroutineContext
   }
 }
 
@@ -282,4 +287,21 @@ fun isForeignClientOnServer(): Boolean {
 
 fun isOnGuest(): Boolean {
   return ClientId.localId != ClientId.defaultLocalId
+}
+
+fun ClientId.asContextElement(): CoroutineContext.Element = ClientIdElement(this)
+
+private object ClientIdElementKey : CoroutineContext.Key<ClientIdElement>
+
+private class ClientIdElement(private val clientId: ClientId) : ThreadContextElement<AccessToken> {
+
+  override val key: CoroutineContext.Key<*> get() = ClientIdElementKey
+
+  override fun updateThreadContext(context: CoroutineContext): AccessToken {
+    return ClientId.withClientId(clientId)
+  }
+
+  override fun restoreThreadContext(context: CoroutineContext, oldState: AccessToken): Unit {
+    oldState.finish()
+  }
 }
