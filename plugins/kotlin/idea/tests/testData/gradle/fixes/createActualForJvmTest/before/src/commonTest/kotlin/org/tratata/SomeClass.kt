73321ab4a9d8e2a59ce34ecd890aca26010f7bba
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-11-12 20:11:41 +0700

    [kotlin] mpp: add heuristic to better guess path for new source root
    ^KTIJ-20149 Fixed
    
    (cherry picked from commit 370f03b41fee1b51ee3177a0161ec75be8505938)
    
    IJ-CR-17172
    
    GitOrigin-RevId: f81c18b8ad13ddb0e7edd377f0c2f0e830f03473

---
+++ plugins/kotlin/idea/tests/testData/gradle/fixes/createActualForJvmTest/before/src/commonTest/kotlin/org/tratata/SomeClass.kt
@@ -0,0 +1,8 @@
+// "Create actual class for module fullStackWebApplication.jvmTest (JVM)" "true"
+// ACTION: Create actual class for module fullStackWebApplication.jsTest (JS)
+// ACTION: Rename class to SomeClass
+// ACTION: Rename file to Aasdawdaw.kt
+
+package org.tratata
+
+expect class Aasd<caret>awdaw
