17b0cc72a35c2b4715d37ceae076111309de0e23
Konstantin Kolosovsky <Konstantin.Kolosovsky@jetbrains.com>
2021-11-12 21:00:54 +0000

    vcs: Update "BranchStateProvider" documentation
    GitOrigin-RevId: 7560839c848cdd33f36ed76e42a6fdb9e4dcef4b

--- platform/vcs-api/src/com/intellij/vcs/branch/BranchData.java
+++ platform/vcs-api/src/com/intellij/vcs/branch/BranchData.java
@@ -1,11 +1,11 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.vcs.branch;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * Represents information about single branch in certain vcs root.
+ * Represents information about single branch in certain VCS root.
  */
 public interface BranchData {
   @NotNull
