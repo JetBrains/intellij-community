84d2c0594b1dc93eb3e25853d9ff98336968180c
marcelhillesheim <contact@marcelhillesheim.com>
2021-11-13 19:29:28 +0100

    Quickfix for both quote types based on usage ()
    GitOrigin-RevId: 0c124f2275ac9a1a14e261132eaa95fca1a0575b

--- plugins/env-files-support/src/main/java/ru/adelf/idea/dotenv/inspections/SpaceInsideNonQuotedInspection.java
+++ plugins/env-files-support/src/main/java/ru/adelf/idea/dotenv/inspections/SpaceInsideNonQuotedInspection.java
@@ -5,13 +5,17 @@ import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.impl.source.tree.LeafPsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.IncorrectOperationException;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import ru.adelf.idea.dotenv.DotEnvFactory;
-import ru.adelf.idea.dotenv.psi.*;
+import ru.adelf.idea.dotenv.psi.DotEnvFile;
+import ru.adelf.idea.dotenv.psi.DotEnvTypes;
+import ru.adelf.idea.dotenv.psi.DotEnvValue;
+
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public class SpaceInsideNonQuotedInspection extends LocalInspectionTool {
     // Change the display name within the plugin.xml
@@ -44,7 +48,8 @@ public class SpaceInsideNonQuotedInspection extends LocalInspectionTool {
         ProblemsHolder problemsHolder = new ProblemsHolder(manager, file, isOnTheFly);
 
         PsiTreeUtil.findChildrenOfType(file, DotEnvValue.class).forEach(dotEnvValue -> {
-            // first child valuechars -> non quoted value
+            // first child VALUE_CHARS -> non quoted value
+            // first child QUOTE -> quoted value
             if(dotEnvValue.getFirstChild().getNode().getElementType() == DotEnvTypes.VALUE_CHARS) {
                 if (dotEnvValue.getText().trim().contains(" ")) {
                     problemsHolder.registerProblem(dotEnvValue, "Space inside allowed only for quoted values", addQuotesQuickFix);
@@ -70,10 +75,25 @@ public class SpaceInsideNonQuotedInspection extends LocalInspectionTool {
          * @param descriptor A problem found by this inspection.
          */
         public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
+
+            // counting each quote type " AND '. The quickfix will use the most common quote type.
+            String quote;
+            Supplier<Stream<DotEnvValue>> supplier = () -> PsiTreeUtil.findChildrenOfType(descriptor.getPsiElement().getContainingFile(), DotEnvValue.class)
+                    .stream()
+                    .filter(dotEnvValue -> dotEnvValue.getFirstChild().getNode().getElementType() == DotEnvTypes.QUOTE);
+            long total = supplier.get().count();
+            long doubleQuoted = supplier.get().filter(dotEnvValue -> dotEnvValue.getFirstChild().getText().contains("\"")).count();
+            long singleQuoted = total - doubleQuoted;
+            if (doubleQuoted > singleQuoted) {
+                quote = "\"";
+            } else {
+                quote = "'";
+            }
+
             try {
                 DotEnvValue valueElement = (DotEnvValue) descriptor.getPsiElement();
 
-                PsiElement newValueElement = DotEnvFactory.createFromText(project, DotEnvTypes.VALUE, "DUMMY=\"" + valueElement.getText() + "\"");
+                PsiElement newValueElement = DotEnvFactory.createFromText(project, DotEnvTypes.VALUE, "DUMMY=" + quote + valueElement.getText() + quote);
 
                 valueElement.getNode().getTreeParent().replaceChild(valueElement.getNode(), newValueElement.getNode());
             } catch (IncorrectOperationException e) {
