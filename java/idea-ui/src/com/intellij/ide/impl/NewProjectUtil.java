316666046710c0d74b53960aee360f364b56f188
Dmitry Pogrebnoy <dmitry.pogrebnoy@jetbrains.com>
2021-11-13 19:13:12 +0300

    [Feedback] Filter collected data
    (cherry picked from commit 8995b430562421480c6d84b67773180e8018f80d)
    
    IJ-CR-18392
    
    GitOrigin-RevId: c5723a45e257f686f490673d665b27b71f1c18a4

--- java/idea-ui/src/com/intellij/ide/impl/NewProjectUtil.java
+++ java/idea-ui/src/com/intellij/ide/impl/NewProjectUtil.java
@@ -10,6 +10,8 @@ import com.intellij.ide.util.newProjectWizard.AbstractProjectWizard;
 import com.intellij.ide.util.projectWizard.AbstractModuleBuilder;
 import com.intellij.ide.util.projectWizard.ProjectBuilder;
 import com.intellij.internal.statistic.service.fus.collectors.FUCounterUsageLogger;
+import com.intellij.internal.statistic.utils.PluginInfo;
+import com.intellij.internal.statistic.utils.PluginInfoDetectorKt;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.command.CommandProcessor;
@@ -88,10 +90,13 @@ public final class NewProjectUtil {
       final NewProjectStatisticService newProjectStatisticService = NewProjectStatisticService.getInstance();
       final ProjectBuilder projectBuilder = wizard.getWizardContext().getProjectBuilder();
       if (projectBuilder instanceof AbstractModuleBuilder) {
-        final String builderId = ((AbstractModuleBuilder)projectBuilder).getBuilderId();
-        if (builderId != null) {
-          final NewProjectInfoState newProjectInfoState = newProjectStatisticService.getState();
-          newProjectInfoState.getCreatedProjectInfo().add(NewProjectInfoEntry.createNewProjectInfoEntry(builderId));
+        final PluginInfo pluginInfo = PluginInfoDetectorKt.getPluginInfo(projectBuilder.getClass());
+        if (pluginInfo.isSafeToReport()) {
+          final String builderId = ((AbstractModuleBuilder)projectBuilder).getBuilderId();
+          if (builderId != null) {
+            final NewProjectInfoState newProjectInfoState = newProjectStatisticService.getState();
+            newProjectInfoState.getCreatedProjectInfo().add(NewProjectInfoEntry.createNewProjectInfoEntry(builderId));
+          }
         }
       }
     }
