19dde7b50b4df05ed1b6d2c93305e7d74625305b
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-11-13 13:23:30 +0700

    [external system] introduce api to find content root paths by module
    ^KTIJ-16294
    
    GitOrigin-RevId: 4a2f664e77fcdcec889dfb53a97e49a0aacd4917

--- plugins/gradle/src/org/jetbrains/plugins/gradle/GradleDirectoryCompletionContributor.kt
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/GradleDirectoryCompletionContributor.kt
@@ -3,21 +3,15 @@ package org.jetbrains.plugins.gradle
 
 import com.intellij.ide.actions.CreateDirectoryCompletionContributor
 import com.intellij.openapi.externalSystem.ExternalSystemModulePropertyManager
-import com.intellij.openapi.externalSystem.model.DataNode
-import com.intellij.openapi.externalSystem.model.ProjectKeys
 import com.intellij.openapi.externalSystem.model.project.ContentRootData
 import com.intellij.openapi.externalSystem.model.project.ExternalSystemSourceType
-import com.intellij.openapi.externalSystem.model.project.ModuleData
-import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
 import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.psi.PsiDirectory
 import org.jetbrains.jps.model.java.JavaResourceRootType
 import org.jetbrains.jps.model.java.JavaSourceRootType
 import org.jetbrains.jps.model.module.JpsModuleSourceRootType
-import org.jetbrains.plugins.gradle.model.data.GradleSourceSetData
 import org.jetbrains.plugins.gradle.util.GradleBundle
 import org.jetbrains.plugins.gradle.util.GradleConstants
-import org.jetbrains.plugins.gradle.util.GradleUtil.findGradleModuleData
 
 class GradleDirectoryCompletionContributor : CreateDirectoryCompletionContributor {
   override fun getDescription(): String = GradleBundle.message("gradle.tasks.directory.completion.description");
@@ -31,26 +25,17 @@ class GradleDirectoryCompletionContributor : CreateDirectoryCompletionContributo
     if (GradleConstants.SYSTEM_ID.id != moduleProperties.getExternalSystemId()) return emptyList()
 
     val result = mutableListOf<CreateDirectoryCompletionContributor.Variant>()
-
     fun addAll(data: ContentRootData, type: ExternalSystemSourceType, rootType: JpsModuleSourceRootType<*>) {
-      result.addAll(data.getPaths(type).map { CreateDirectoryCompletionContributor.Variant(it.path, rootType) })
+      data.getPaths(type).mapTo(result) { CreateDirectoryCompletionContributor.Variant(it.path, rootType) }
     }
 
-    fun addAll(moduleData: DataNode<out ModuleData>) {
-      for (eachContentRootNode in ExternalSystemApiUtil.findAll(moduleData, ProjectKeys.CONTENT_ROOT)) {
-        addAll(eachContentRootNode.data, ExternalSystemSourceType.SOURCE, JavaSourceRootType.SOURCE)
-        addAll(eachContentRootNode.data, ExternalSystemSourceType.TEST, JavaSourceRootType.TEST_SOURCE)
-        addAll(eachContentRootNode.data, ExternalSystemSourceType.RESOURCE, JavaResourceRootType.RESOURCE)
-        addAll(eachContentRootNode.data, ExternalSystemSourceType.TEST_RESOURCE, JavaResourceRootType.TEST_RESOURCE)
-      }
+    GradleContentRootContributor.processContentRoots(module) { rootData ->
+      addAll(rootData, ExternalSystemSourceType.SOURCE, JavaSourceRootType.SOURCE)
+      addAll(rootData, ExternalSystemSourceType.TEST, JavaSourceRootType.TEST_SOURCE)
+      addAll(rootData, ExternalSystemSourceType.RESOURCE, JavaResourceRootType.RESOURCE)
+      addAll(rootData, ExternalSystemSourceType.TEST_RESOURCE, JavaResourceRootType.TEST_RESOURCE)
     }
 
-    val moduleData = findGradleModuleData(module) ?: return emptyList()
-    addAll(moduleData)
-    for (eachSourceSetNode in ExternalSystemApiUtil.getChildren(moduleData, GradleSourceSetData.KEY)) {
-      addAll(eachSourceSetNode)
-    }
-
-    return result;
+    return result.toList()
   }
 }
