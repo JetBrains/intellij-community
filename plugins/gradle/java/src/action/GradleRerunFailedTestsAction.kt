cf388c2496dbaf43d776b23b21618ffe78fd4485
Andrei Klunnyi <andrei.klunnyi@jetbrains.com>
2021-11-17 12:10:34 +0100

    KTIJ-13654 MPP: rerunning failed tests
    With this commit rerunning failed tests for MPP project
    becomes possible. More details are below.
    
    What happened. Gradle command line used for rerun looked
    twice corrupted. It contained wrong task names (different
    from those used for initial run) and filter contained
    strangely formatted test names.
    
    Why it happened. Gradle-Kotlin-plugin reports tests execution status sending
    messages to IDEA. Test-method names for MPP contain target related data [1].
    IDEA treats such format as parametrized tests [2][3] and tries to
    launch them accordingly. Here is why gradle filter looked the way it did.
    Rerun [4] as a functionality is a part of IDEA-Gradle plugin obviously
    having no idea about MPP and its specific tasks. Given this limitation
    names were recalculated incorrectly.
    
    Solution. Gradle-Kotlin-plugin is aware of two names: system and display
    ones. Messages though included only the latter [5] complicating client-side
    parser. So the format was extended to report both names, parser and further
    steps adjusted accordingly. Tasks recalculation turned out to be needless,
    the ones used and known from initial launch are reused.
    
    [1]: kotlin: TCServiceMessagesClient.beginTest()
    [2]: BeforeTestEvent.doProcess(), findLocationUrl() inside.
    [3]: GradleExecutionSettingsUtil.createTestFilterFrom(Location<?>, PsiClass, PsiMethod)
    [4]: GradleRerunFailedTestsAction
    [5]: KotlinMppTestLogger.groovy, IJTestLogger.groovy
    
    ^KTIJ-13654 fixed
    
    (cherry picked from commit 471e63b52f54512f45c96084f4f140a0b0355528)
    (cherry picked from commit f0fbcc32091ca6be89a4154aab0fde532460afa4)
    
    GitOrigin-RevId: f944a6347d961feb2f570d1e330a07e2c5a37aee

--- plugins/gradle/java/src/action/GradleRerunFailedTestsAction.kt
+++ plugins/gradle/java/src/action/GradleRerunFailedTestsAction.kt
@@ -30,11 +30,9 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiMethod
 import com.intellij.psi.search.GlobalSearchScope
 import org.jetbrains.plugins.gradle.execution.test.runner.GradleSMTestProxy
-import org.jetbrains.plugins.gradle.execution.test.runner.GradleTestRunConfigurationProducer.findAllTestsTaskToRun
 import org.jetbrains.plugins.gradle.execution.test.runner.GradleTestsExecutionConsole
 import org.jetbrains.plugins.gradle.execution.test.runner.applyTestConfiguration
 import org.jetbrains.plugins.gradle.execution.test.runner.getSourceFile
-import org.jetbrains.plugins.gradle.util.containsTasks
 import org.jetbrains.plugins.gradle.util.createTestFilterFrom
 
 class GradleRerunFailedTestsAction(
@@ -61,12 +59,13 @@ class GradleRerunFailedTestsAction(
     val failedTests = getFailedTests(project).filterIsInstance<GradleSMTestProxy>().mapNotNull { getTestLocationInfo(project, it) }
     val findTestSource = { it: TestLocationInfo -> getSourceFile(it.element) }
     val createFiler = { it: TestLocationInfo -> createTestFilterFrom(it.location, it.psiClass, it.psiMethod) }
-    val getTestsTaskToRun = { source: VirtualFile ->
-      val foundTasksToRun = findAllTestsTaskToRun(source, project)
-      foundTasksToRun
-        .filter { tasks -> containsTasks(tasks) }
-        .ifEmpty { listOfNotNull(foundTasksToRun.firstOrNull()) }
-    }
+
+    val tasksToRun = taskNames
+      .takeWhile { !it.startsWith("--") } // gradle filter is out of interest, created from scratch
+      .flatMap { it.split(" ") }
+    val formattedTasksToRun = listOf(tasksToRun)
+    val getTestsTaskToRun = { _: VirtualFile -> formattedTasksToRun }
+
     if (!applyTestConfiguration(externalProjectPath, failedTests, findTestSource, createFiler, getTestsTaskToRun)) {
       LOG.warn("Cannot apply test configuration, uses previous run configuration")
     }
