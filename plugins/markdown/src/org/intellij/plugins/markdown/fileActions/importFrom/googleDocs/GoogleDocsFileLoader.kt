a51dda461348db71283a6d880721eddd45a2dc06
Ivan Posti <ivan.posti@jetbrains.com>
2021-11-17 11:16:43 +0300

    [Markdown] Update com.google.apis:google-api-services-drive to 1.32.1
    GitOrigin-RevId: c3cf8cb338476a047fd532e2354b379d41c964b5

--- plugins/markdown/src/org/intellij/plugins/markdown/fileActions/importFrom/googleDocs/GoogleDocsFileLoader.kt
+++ plugins/markdown/src/org/intellij/plugins/markdown/fileActions/importFrom/googleDocs/GoogleDocsFileLoader.kt
@@ -5,7 +5,7 @@ import com.google.api.client.auth.oauth2.Credential
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
 import com.google.api.client.googleapis.json.GoogleJsonResponseException
 import com.google.api.client.json.JsonFactory
-import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.client.json.gson.GsonFactory
 import com.google.api.services.drive.Drive
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.logger
@@ -104,7 +104,7 @@ class GoogleDocsFileLoader {
   companion object {
     private const val mimeType = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
 
-    private val jsonFactory: JsonFactory get() = JacksonFactory.getDefaultInstance()
+    private val jsonFactory: JsonFactory get() = GsonFactory.getDefaultInstance()
     private val LOG = logger<GoogleDocsFileLoader>()
   }
 }
