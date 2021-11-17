2d19324846ca9469c7f830ef10a7a7e4c52bbc34
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2021-11-17 17:31:31 +0700

    [java-completion] MissingArrayInitializerBraceFixer: use EnterAfterUnmatchedBraceHandler (IDEA-282469)
    GitOrigin-RevId: 29a477bf9a7b726ef2d56c7e0ad7f82c8621d8b7

--- java/java-impl/src/com/intellij/codeInsight/editorActions/smartEnter/MissingArrayInitializerBraceFixer.java
+++ java/java-impl/src/com/intellij/codeInsight/editorActions/smartEnter/MissingArrayInitializerBraceFixer.java
@@ -15,6 +15,7 @@
  */
 package com.intellij.codeInsight.editorActions.smartEnter;
 
+import com.intellij.codeInsight.editorActions.enter.EnterAfterUnmatchedBraceHandler;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.*;
 import com.intellij.psi.util.PsiTreeUtil;
@@ -26,10 +27,21 @@ public class MissingArrayInitializerBraceFixer implements Fixer {
   @Override
   public void apply(Editor editor, JavaSmartEnterProcessor processor, PsiElement psiElement) throws IncorrectOperationException {
     if (!(psiElement instanceof PsiArrayInitializerExpression || psiElement instanceof PsiArrayInitializerMemberValue)) return;
-    PsiElement deepestVisibleLast = PsiTreeUtil.getDeepestVisibleLast(psiElement);
-    if (PsiUtil.isJavaToken(deepestVisibleLast, JavaTokenType.RBRACE) && deepestVisibleLast.getParent() == psiElement) return;
-    PsiErrorElement err = ContainerUtil.findInstance(psiElement.getChildren(), PsiErrorElement.class);
-    int endOffset = (err != null ? err : psiElement).getTextRange().getEndOffset();
+    PsiElement child = psiElement.getFirstChild();
+    if (!PsiUtil.isJavaToken(child, JavaTokenType.LBRACE)) return;
+    if (!EnterAfterUnmatchedBraceHandler.isAfterUnmatchedLBrace(editor, child.getTextRange().getEndOffset(),
+                                                                psiElement.getContainingFile().getFileType())) return;
+    PsiElement anchor = ContainerUtil.findInstance(psiElement.getChildren(), PsiErrorElement.class);
+    if (anchor == null) {
+      PsiElement last = PsiTreeUtil.getDeepestVisibleLast(psiElement);
+      while (PsiUtil.isJavaToken(last, JavaTokenType.RBRACE)) {
+        last = PsiTreeUtil.prevCodeLeaf(last);
+      }
+      if (last != null && PsiTreeUtil.isAncestor(psiElement, last, true)) {
+        anchor = last;
+      }
+    }
+    int endOffset = (anchor != null ? anchor : psiElement).getTextRange().getEndOffset();
     editor.getDocument().insertString(endOffset, "}");
   }
 }
