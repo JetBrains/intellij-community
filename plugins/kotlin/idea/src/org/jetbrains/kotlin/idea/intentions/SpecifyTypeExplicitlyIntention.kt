918b447efea2045b5357c28d035887e84882796e
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2021-11-18 13:50:56 +0700

    [kotlin-intentions] SpecifyTypeExplicitlyIntention: preview-friendly
    GitOrigin-RevId: 083a4e6d9a741ca501fe8ff8eb140b692bc47921

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/intentions/SpecifyTypeExplicitlyIntention.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/intentions/SpecifyTypeExplicitlyIntention.kt
@@ -23,7 +23,7 @@ import org.jetbrains.kotlin.idea.core.unquote
 import org.jetbrains.kotlin.idea.project.languageVersionSettings
 import org.jetbrains.kotlin.idea.util.IdeDescriptorRenderers
 import org.jetbrains.kotlin.idea.util.application.isUnitTestMode
-import org.jetbrains.kotlin.idea.util.application.runWriteAction
+import org.jetbrains.kotlin.idea.util.application.runWriteActionIfPhysical
 import org.jetbrains.kotlin.idea.util.getResolutionScope
 import org.jetbrains.kotlin.idea.util.getResolvableApproximations
 import org.jetbrains.kotlin.psi.*
@@ -210,7 +210,7 @@ class SpecifyTypeExplicitlyIntention : SelfTargetingRangeIntention<KtCallableDec
             override fun templateFinished(template: Template, brokenOff: Boolean) {
                 val typeRef = declaration.typeReference
                 if (typeRef != null && typeRef.isValid) {
-                    runWriteAction {
+                    runWriteActionIfPhysical(typeRef) {
                         ShortenReferences.DEFAULT.process(typeRef)
                         if (iterator != null && editor != null) addTypeAnnotationWithTemplate(editor, iterator)
                     }
