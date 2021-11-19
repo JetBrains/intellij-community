b93bc1eeccc767fcd91e951e93a57f99401ac53a
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-11-19 14:48:16 +0100

    revert a73f84acb721ff91a86d0e94b67136f68a2ee202: add `PROJECT` data to fix `DocumentationTargetsDataRule`
    (cherry picked from commit a62ac77e62c86fb556afe92670525e8462eed32a)
    
    IJ-CR-17439
    
    GitOrigin-RevId: bb5a46bf69cfcd571fd5eb64963f8f15f493e057

--- platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/text/TextEditorComponent.java
+++ platform/platform-impl/src/com/intellij/openapi/fileEditor/impl/text/TextEditorComponent.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.fileEditor.impl.text;
 
 import com.intellij.openapi.Disposable;
@@ -209,9 +209,6 @@ class TextEditorComponent extends JBLoadingPanel implements DataProvider, Dispos
 
       if (CommonDataKeys.EDITOR.is(dataId)) {
         return myEditor;
-      }
-      if (CommonDataKeys.PROJECT.is(dataId)) {
-        return myProject;
       }
       if (CommonDataKeys.VIRTUAL_FILE.is(dataId)) {
         return myFile.isValid() ? myFile : null;  // fix for SCR 40329
