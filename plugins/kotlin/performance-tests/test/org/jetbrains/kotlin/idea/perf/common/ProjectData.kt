07bbd0c5c3c12afe6b4e509fe6d51150216d33ef
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-11-22 22:48:43 +0100

    FIR IDE: use environment variable to get perf testdata dir
    GitOrigin-RevId: 90b1ef9af552428a0adae2438bb41c24a4801c73

--- plugins/kotlin/performance-tests/test/org/jetbrains/kotlin/idea/perf/common/ProjectData.kt
+++ plugins/kotlin/performance-tests/test/org/jetbrains/kotlin/idea/perf/common/ProjectData.kt
@@ -2,9 +2,17 @@
 package org.jetbrains.kotlin.idea.perf.common
 
 import org.jetbrains.kotlin.idea.testFramework.ProjectOpenAction
+import java.nio.file.Path
+import java.nio.file.Paths
 
 data class ProjectData(
     val name: String,
-    val path: String,
+    val path: Path,
     val openAction: ProjectOpenAction,
 )
+
+fun perfTestProjectPath(relativePath: String): Path {
+    val dir = System.getenv("PERF_TESTDATA_DIR")
+        ?: error("Please specify environment variable `PERF_TESTDATA_DIR` pointing to the directory with performance testdata")
+    return Paths.get(dir).resolve(relativePath)
+}
