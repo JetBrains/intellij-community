4809e44504af9588ec1023f4a27633b1f992146d
Nicolay Mitropolsky <nicolay.mitropolsky@jetbrains.com>
2021-11-22 13:01:44 +0300

    IntelliLang: language comments folding
    GitOrigin-RevId: 516d9e36407d8e88369d334a14109117b1c28c9d

--- plugins/IntelliLang/IntelliLang-tests/test/org/intellij/plugins/intelliLang/LanguageCommentTest.kt
+++ plugins/IntelliLang/IntelliLang-tests/test/org/intellij/plugins/intelliLang/LanguageCommentTest.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.intellij.plugins.intelliLang
 
+import com.intellij.codeInsight.assertFolded
 import com.intellij.codeInsight.completion.CompletionType
 import com.intellij.openapi.util.registry.Registry
 
@@ -36,6 +37,8 @@ class LanguageCommentTest : AbstractLanguageInjectionTestCase() {
     """)
 
     injectionTestFixture.assertInjectedContent("function foo(){}")
+    myFixture.doHighlighting()
+    myFixture.editor.assertFolded("// language=jshelllanguage", "JShellLanguage")
   }
   
   fun testJavaLanguageWordCompletion() {
--- plugins/IntelliLang/src/org/intellij/plugins/intelliLang/references/LanguageReferenceContributor.kt
+++ plugins/IntelliLang/src/org/intellij/plugins/intelliLang/references/LanguageReferenceContributor.kt
@@ -4,30 +4,33 @@ package org.intellij.plugins.intelliLang.references
 import com.intellij.codeInsight.completion.*
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.lang.ASTNode
 import com.intellij.lang.LanguageCommenters
+import com.intellij.lang.folding.FoldingBuilderEx
+import com.intellij.lang.folding.FoldingDescriptor
 import com.intellij.model.Pointer
 import com.intellij.model.Symbol
 import com.intellij.model.psi.*
 import com.intellij.model.search.SearchRequest
+import com.intellij.openapi.editor.Document
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.TextRange
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiRecursiveElementVisitor
 import com.intellij.util.ProcessingContext
+import com.intellij.util.SmartList
 import com.intellij.util.castSafelyTo
 import com.intellij.util.text.findTextRange
 import org.intellij.plugins.intelliLang.inject.InjectLanguageAction
+import org.intellij.plugins.intelliLang.inject.InjectorUtils
 
 class LanguageReferenceContributor : PsiSymbolReferenceProvider {
   override fun getReferences(element: PsiExternalReferenceHost, hints: PsiSymbolReferenceHints): Collection<PsiSymbolReference> {
     val psiComment = element.castSafelyTo<PsiComment>() ?: return emptyList()
-    val commentBody = psiComment.commentBody.trim()
-    if (!commentBody.startsWith(LANGUAGE_PREFIX)) return emptyList()
-
-    val startOffset = psiComment.text.findTextRange(LANGUAGE_PREFIX)!!.endOffset
-    val endOffset = psiComment.text.findTextRange(commentBody)!!.endOffset
+    val languageRange = getLanguageRange(psiComment) ?: return emptyList()
     return listOf(object : PsiCompletableReference {
 
       override fun getCompletionVariants(): Collection<LookupElement> =
@@ -37,7 +40,7 @@ class LanguageReferenceContributor : PsiSymbolReferenceProvider {
 
       override fun getElement(): PsiElement = element
 
-      override fun getRangeInElement(): TextRange = TextRange(startOffset, endOffset)
+      override fun getRangeInElement(): TextRange = languageRange
 
       override fun resolveReference(): Collection<Symbol> = listOf(LanguageSymbol(rangeInElement.substring(this.element.text)))
     })
@@ -46,6 +49,15 @@ class LanguageReferenceContributor : PsiSymbolReferenceProvider {
   override fun getSearchRequests(project: Project, target: Symbol): Collection<SearchRequest> =
     listOfNotNull(target.castSafelyTo<LanguageSymbol>()?.let { SearchRequest.of(it.name) })
 
+}
+
+private fun getLanguageRange(psiComment: PsiComment): TextRange? {
+  val commentBody = psiComment.commentBody.trim()
+  if (!commentBody.startsWith(LANGUAGE_PREFIX)) return null
+
+  val startOffset = psiComment.text.findTextRange(LANGUAGE_PREFIX)!!.endOffset
+  val endOffset = psiComment.text.findTextRange(commentBody)!!.endOffset
+  return TextRange(startOffset, endOffset)
 }
 
 private data class LanguageSymbol(val name: String) : Symbol, Pointer<LanguageSymbol> {
@@ -68,6 +80,31 @@ class LanguageWordInCommentCompletionContributor : CompletionContributor() {
       }
     })
   }
+}
+
+class LanguageCommentFolding : FoldingBuilderEx() {
+  override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> {
+    return SmartList<FoldingDescriptor>().also { result ->
+      root.accept(object : PsiRecursiveElementVisitor() {
+        override fun visitComment(comment: PsiComment) {
+          super.visitComment(comment)
+          if (getLanguageRange(comment) != null) {
+            result.add(FoldingDescriptor(comment, comment.textRange))
+          }
+        }
+      })
+    }.toArray(FoldingDescriptor.EMPTY)
+  }
+
+  override fun getPlaceholderText(node: ASTNode): String? {
+    val psiComment = node.psi.castSafelyTo<PsiComment>() ?: return null
+    val languageRange = getLanguageRange(psiComment) ?: return null
+    val writtenText = languageRange.substring(psiComment.text)
+    return InjectorUtils.getLanguageByString(writtenText)?.id ?: writtenText
+  }
+
+  override fun isCollapsedByDefault(node: ASTNode): Boolean = true
+
 }
 
 private const val LANGUAGE_PREFIX = "language="
