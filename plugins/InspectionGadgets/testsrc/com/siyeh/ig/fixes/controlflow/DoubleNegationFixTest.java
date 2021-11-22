383785be7f325c574409da7cd87d9801e12d7de4
Fabrice Tiercelin <fabrice.tiercelin@yahoo.fr>
2021-11-22 16:00:19 +0100

    IG: Enable double negation as cleanup tool (IDEA-279961)
    GitOrigin-RevId: 7f36696ba0f49badd5f4030c5fde1897fadb57d7

--- plugins/InspectionGadgets/testsrc/com/siyeh/ig/fixes/controlflow/DoubleNegationFixTest.java
+++ plugins/InspectionGadgets/testsrc/com/siyeh/ig/fixes/controlflow/DoubleNegationFixTest.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.siyeh.ig.fixes.controlflow;
 
 import com.siyeh.InspectionGadgetsBundle;
@@ -32,6 +18,22 @@ public class DoubleNegationFixTest extends IGQuickFixesTestCase {
     myDefaultHint = InspectionGadgetsBundle.message("double.negation.quickfix");
   }
 
+  public void testDoubleNegation() {
+    doExpressionTest(myDefaultHint,
+                     "!/**/!(System.currentTimeMillis() > 1_000_000)",
+                     "System.currentTimeMillis() > 1_000_000");
+  }
+
   public void testDoubleDoubleNegation() { doTest(); }
+
   public void testPolyadicParentheses() { doTest(); }
+
+  public void testDoNotCleanupWithSingleNegation() {
+    assertQuickfixNotAvailable(myDefaultHint,
+                               "class X {\n" +
+                               "  boolean test(boolean isValid) {\n" +
+                               "    return !/**/isValid;\n" +
+                               "  }\n" +
+                               "}\n");
+  }
 }
