c548897c2a26f88e665079c586f1ae8c846cd7e9
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-11-23 17:06:20 +0700

    [kotlin] GradleQuickFixTest: update testData for testCreateActualForJvm
    (cherry picked from commit 132cd84c076dd4dce108900dd10839895f244ecf)
    
    IJ-CR-17172
    
    GitOrigin-RevId: 123f622b5315e73a51fc6d1bd640867ad2e046fe

--- plugins/kotlin/idea/tests/testData/gradle/fixes/createActualForJvm/after/src/commonMain/kotlin/org/tratata/SomeClass.kt
+++
@@ -1,7 +0,0 @@
-// "Create actual class for module fullStackWebApplication.jvmMain (JVM)" "true"
-// ACTION: Rename class to SomeClass
-// ACTION: Rename file to Aasdawdaw.kt
-
-package org.tratata
-
-expect class Aasdawdaw
