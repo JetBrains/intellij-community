37a3cd00e78415e8e4adb604ca27cf4a75d6c3d7
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-11-23 16:58:45 +0700

    [kotlin] GradleQuickFixTest: update testData for testCreateActualForJsTest
    (cherry picked from commit 649d446f8f4badaee9b37b9efbabebefb045a697)
    
    IJ-CR-17172
    
    GitOrigin-RevId: 0fc3ff0c807ee6c33e5f9c3f087f9cad7a8c944f

---
+++ plugins/kotlin/idea/tests/testData/gradle/fixes/createActualForJsTest/before/src/commonTest/kotlin/my.project.pack/JsUtils.kt
@@ -0,0 +1,6 @@
+// "Create actual function for module fullStackWebApplication.jsTest (JS)" "true"
+// ACTION: Convert function to property
+// ACTION: Create actual function for module fullStackWebApplication.jvmTest (JVM)
+package my.project.pack
+
+expect fun i<caret>sJs(): Boolean
