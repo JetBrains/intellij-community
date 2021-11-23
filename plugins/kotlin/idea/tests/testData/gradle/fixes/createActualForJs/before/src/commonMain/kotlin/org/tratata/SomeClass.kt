1d77bf80f5976531906e288533b5e6f718d8182f
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-11-23 16:56:20 +0700

    [kotlin] GradleQuickFixTest: update testData for testCreateActualForJs
    GitOrigin-RevId: 4340d07f39e07ea088a5da1c4efebbd9a4805418

--- plugins/kotlin/idea/tests/testData/gradle/fixes/createActualForJs/before/src/commonMain/kotlin/org/tratata/SomeClass.kt
+++
@@ -1,8 +0,0 @@
-// "Create actual class for module fullStackWebApplication.jsMain (JS)" "true"
-// ACTION: Create actual class for module fullStackWebApplication.jvmMain (JVM)
-// ACTION: Rename class to SomeClass
-// ACTION: Rename file to Aasdawdaw.kt
-
-package org.tratata
-
-expect class Aasd<caret>awdaw
