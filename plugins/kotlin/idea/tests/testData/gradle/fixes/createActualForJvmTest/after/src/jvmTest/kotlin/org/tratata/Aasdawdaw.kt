34570bd419f75ab71834094f543a6c016761b4b2
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-11-23 21:20:56 +0700

    [kotlin] GradleQuickFixTest: update testData for testCreateActualForJvmTest
    (cherry picked from commit ab39e734bcda2e8092d2564aac2b84d312f3b0cc)
    
    IJ-CR-17172
    
    GitOrigin-RevId: bb5bb89ce6ccab9ada9f580eb1a70a80cdb801aa

--- plugins/kotlin/idea/tests/testData/gradle/fixes/createActualForJvmTest/after/src/jvmTest/kotlin/org/tratata/Aasdawdaw.kt
+++
@@ -1,3 +0,0 @@
-package org.tratata
-
-actual class Aasdawdaw
--- plugins/kotlin/idea/tests/testData/gradle/fixes/createActualForJvmTest/before/settings.gradle.kts
+++ plugins/kotlin/idea/tests/testData/gradle/fixes/createActualForJvmTest/before/settings.gradle.kts
@@ -1,2 +1,12 @@
-rootProject.name = "fullStackWebApplication"
+pluginManagement {
+    repositories {
+        {{kts_kotlin_plugin_repositories}}
+    }
+
+    plugins {
+        kotlin("multiplatform") version "{{kotlin_plugin_version}}"
+    }
+}
+
+rootProject.name = "testProject"
 
