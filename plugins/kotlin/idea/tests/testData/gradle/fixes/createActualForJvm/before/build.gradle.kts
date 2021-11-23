9863a29d635074b4a253a1ca1b70aadd0104030c
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-11-23 17:06:20 +0700

    [kotlin] GradleQuickFixTest: update testData for testCreateActualForJvm
    GitOrigin-RevId: 132cd84c076dd4dce108900dd10839895f244ecf

--- plugins/kotlin/idea/tests/testData/gradle/fixes/createActualForJvm/before/build.gradle.kts
+++ plugins/kotlin/idea/tests/testData/gradle/fixes/createActualForJvm/before/build.gradle.kts
@@ -1,35 +1,17 @@
 plugins {
-    kotlin("multiplatform") version "1.4.20"
+    kotlin("multiplatform") version "{{kotlin_plugin_version}}"
 }
 
-group = "me.one.twp"
+group = "me.one.two"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+    {{kts_kotlin_plugin_repositories}}
 }
 
 kotlin {
     jvm {
-        compilations.all {
-            kotlinOptions.jvmTarget = "1.8"
-        }
         withJava()
     }
-    js(LEGACY) {
-        binaries.executable()
-        browser {
-            commonWebpackConfig {
-                cssSupport.enabled = true
-            }
-        }
-    }
-    sourceSets {
-        val commonMain by getting
-        val commonTest by getting
-        val jvmMain by getting
-        val jvmTest by getting
-        val jsMain by getting
-        val jsTest by getting
-    }
+    js {}
 }
