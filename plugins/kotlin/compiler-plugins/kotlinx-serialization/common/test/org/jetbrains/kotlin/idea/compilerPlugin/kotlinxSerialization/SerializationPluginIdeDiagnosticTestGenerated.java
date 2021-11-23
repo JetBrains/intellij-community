e5d1da555f1d5abcaa1cdf929602c279f93905dd
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2021-11-23 19:59:11 +0900

    Fix serialization test compilation
    AbstractSerializationPluginIdeDiagnosticTest is rewritten with the
    IDE infrastructure.
    
    GitOrigin-RevId: cb7220bd60b44fda1be923ad1e02626b9a394a5b

--- plugins/kotlin/compiler-plugins/kotlinx-serialization/common/test/org/jetbrains/kotlin/idea/compilerPlugin/kotlinxSerialization/SerializationPluginIdeDiagnosticTestGenerated.java
+++ plugins/kotlin/compiler-plugins/kotlinx-serialization/common/test/org/jetbrains/kotlin/idea/compilerPlugin/kotlinxSerialization/SerializationPluginIdeDiagnosticTestGenerated.java
@@ -1,6 +1,6 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
-package org.jetbrains.kotlinx.serialization.idea;
+package org.jetbrains.kotlin.idea.compilerPlugin.kotlinxSerialization;
 
 import com.intellij.testFramework.TestDataPath;
 import org.jetbrains.kotlin.test.JUnit3RunnerWithInners;
