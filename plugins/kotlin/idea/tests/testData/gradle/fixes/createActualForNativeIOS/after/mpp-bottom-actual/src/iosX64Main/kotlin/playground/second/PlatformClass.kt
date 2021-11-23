67c56d20e09e616bd548c603b2a6cac728cdf1ed
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-11-23 22:59:54 +0700

    [kotlin] GradleQuickFixTest: update testData for testCreateActualForNativeIOS
    (cherry picked from commit 326970001f926237b33562b583cbd9927c3445b7)
    
    IJ-CR-17172
    
    (cherry picked from commit 6d7ee98ea404038f42ad8b7cfa491cd92cb3f57b)
    
    GitOrigin-RevId: 3427d3f368bb86d32e706c52e82447f7490eaa3f

--- plugins/kotlin/idea/tests/testData/gradle/fixes/createActualForNativeIOS/after/mpp-bottom-actual/src/iosX64Main/kotlin/playground/second/PlatformClass.kt
+++ plugins/kotlin/idea/tests/testData/gradle/fixes/createActualForNativeIOS/after/mpp-bottom-actual/src/iosX64Main/kotlin/playground/second/PlatformClass.kt
@@ -1,6 +1,6 @@
 package playground.second
 
-actual open class FromCommonMainAExp actual constructor() {
+actual open class PlatformClass actual constructor() {
     actual open fun c(a: Int, b: String) {
     }
 }
