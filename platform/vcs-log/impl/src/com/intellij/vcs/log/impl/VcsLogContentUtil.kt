323b3cc2010d1d451e1a5a3892e9cd20212dcda3
Aleksey Pivovarov <AMPivovarov@gmail.com>
2021-11-24 16:43:41 +0300

    vcs: convert VcsLogContentUtil to Kotlin
    GitOrigin-RevId: 6ec9a1dfafb0e130b066c3ba3ecd8c84e5b2bd14

--- platform/vcs-log/impl/src/com/intellij/vcs/log/impl/VcsLogContentUtil.kt
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/impl/VcsLogContentUtil.kt
@@ -1,180 +1,159 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.vcs.log.impl;
+package com.intellij.vcs.log.impl
 
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.MessageType;
-import com.intellij.openapi.util.Condition;
-import com.intellij.openapi.util.NlsContexts;
-import com.intellij.openapi.vcs.changes.ui.ChangesViewContentManager;
-import com.intellij.openapi.vcs.ui.VcsBalloonProblemNotifier;
-import com.intellij.openapi.wm.ToolWindow;
-import com.intellij.openapi.wm.ToolWindowManager;
-import com.intellij.ui.content.*;
-import com.intellij.util.Consumer;
-import com.intellij.util.ContentUtilEx;
-import com.intellij.util.concurrency.annotations.RequiresBackgroundThread;
-import com.intellij.util.concurrency.annotations.RequiresEdt;
-import com.intellij.util.containers.ContainerUtil;
-import com.intellij.vcs.log.VcsLogBundle;
-import com.intellij.vcs.log.VcsLogUi;
-import com.intellij.vcs.log.ui.MainVcsLogUi;
-import com.intellij.vcs.log.ui.VcsLogPanel;
-import com.intellij.vcs.log.ui.VcsLogUiEx;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import javax.swing.*;
-import java.util.List;
-import java.util.Objects;
-import java.util.function.Function;
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.MessageType
+import com.intellij.openapi.util.Condition
+import com.intellij.openapi.util.NlsContexts
+import com.intellij.openapi.vcs.changes.ui.ChangesViewContentManager
+import com.intellij.openapi.vcs.ui.VcsBalloonProblemNotifier
+import com.intellij.openapi.wm.ToolWindowManager
+import com.intellij.ui.content.Content
+import com.intellij.ui.content.ContentManager
+import com.intellij.ui.content.TabDescriptor
+import com.intellij.ui.content.TabGroupId
+import com.intellij.util.Consumer
+import com.intellij.util.ContentUtilEx
+import com.intellij.util.concurrency.annotations.RequiresBackgroundThread
+import com.intellij.util.concurrency.annotations.RequiresEdt
+import com.intellij.vcs.log.VcsLogBundle
+import com.intellij.vcs.log.VcsLogUi
+import com.intellij.vcs.log.impl.VcsLogManager.VcsLogUiFactory
+import com.intellij.vcs.log.ui.MainVcsLogUi
+import com.intellij.vcs.log.ui.VcsLogPanel
+import com.intellij.vcs.log.ui.VcsLogUiEx
+import org.jetbrains.annotations.ApiStatus
+import java.util.function.Function
+import java.util.function.Supplier
+import javax.swing.JComponent
 
 /**
- * Utility methods to operate VCS Log tabs as {@link Content}s of the {@link ContentManager} of the VCS toolwindow.
+ * Utility methods to operate VCS Log tabs as [Content]s of the [ContentManager] of the VCS toolwindow.
  */
-public final class VcsLogContentUtil {
-
-  private static @Nullable VcsLogUiEx getLogUi(@NotNull JComponent c) {
-    List<VcsLogUiEx> uis = VcsLogPanel.getLogUis(c);
-    if (uis.size() > 1) throw new IllegalArgumentException("Component " + c + " has more than one log ui: " + uis);
-    return ContainerUtil.getOnlyItem(uis);
+object VcsLogContentUtil {
+  private fun getLogUi(c: JComponent): VcsLogUiEx? {
+    val uis = VcsLogPanel.getLogUis(c)
+    require(uis.size <= 1) { "Component $c has more than one log ui: $uis" }
+    return uis.singleOrNull()
   }
 
-  @Nullable
-  public static <U extends VcsLogUiEx> U findAndSelect(@NotNull Project project,
-                                                       @NotNull Class<U> clazz,
-                                                       @NotNull Condition<? super U> condition) {
-    return find(project, clazz, true, condition);
+  fun <U : VcsLogUiEx> findAndSelect(project: Project,
+                                     clazz: Class<U>,
+                                     condition: Condition<in U>): U? {
+    return find(project, clazz, true, condition)
   }
 
-  @Nullable
-  public static <U extends VcsLogUiEx> U find(@NotNull Project project,
-                                              @NotNull Class<U> clazz, boolean select,
-                                              @NotNull Condition<? super U> condition) {
-    ToolWindow toolWindow = ToolWindowManager.getInstance(project).getToolWindow(ChangesViewContentManager.TOOLWINDOW_ID);
-    if (toolWindow == null) {
-      return null;
-    }
-
-    ContentManager manager = toolWindow.getContentManager();
-    JComponent component = ContentUtilEx.findContentComponent(manager, c -> {
-      VcsLogUiEx ui = getLogUi(c);
-      if (ui != null) {
-        //noinspection unchecked
-        return clazz.isInstance(ui) && condition.value((U)ui);
-      }
-      return false;
-    });
-    if (component == null) {
-      return null;
-    }
+  fun <U : VcsLogUiEx> find(project: Project,
+                            clazz: Class<U>,
+                            select: Boolean,
+                            condition: Condition<in U>): U? {
+    val toolWindow = ToolWindowManager.getInstance(project).getToolWindow(ChangesViewContentManager.TOOLWINDOW_ID) ?: return null
+    val manager = toolWindow.contentManager
+    val component = ContentUtilEx.findContentComponent(manager) { c: JComponent ->
+      getLogUi(c).safeCastTo(clazz)?.let { condition.value(it) } ?: false
+    } ?: return null
 
     if (select) {
-      if (!toolWindow.isVisible()) {
-        toolWindow.activate(null);
+      if (!toolWindow.isVisible) {
+        toolWindow.activate(null)
       }
       if (!ContentUtilEx.selectContent(manager, component, true)) {
-        return null;
+        return null
       }
     }
-    //noinspection unchecked
-    return (U)getLogUi(component);
+
+    @Suppress("UNCHECKED_CAST")
+    return getLogUi(component) as U
   }
 
-  @Nullable
-  public static String getId(@NotNull Content content) {
-    VcsLogUiEx ui = getLogUi(content.getComponent());
-    if (ui == null) return null;
-    return ui.getId();
+  fun getId(content: Content): String? {
+    return getLogUi(content.component)?.id
   }
 
-  public static <U extends VcsLogUiEx> U openLogTab(@NotNull Project project,
-                                                    @NotNull VcsLogManager logManager,
-                                                    @NotNull TabGroupId tabGroupId,
-                                                    @NotNull Function<U, @NlsContexts.TabTitle String> tabDisplayName,
-                                                    @NotNull VcsLogManager.VcsLogUiFactory<? extends U> factory,
-                                                    boolean focus) {
-    U logUi = logManager.createLogUi(factory, VcsLogManager.LogWindowKind.TOOL_WINDOW);
-
-    ToolWindow toolWindow = ToolWindowManager.getInstance(project).getToolWindow(ChangesViewContentManager.TOOLWINDOW_ID);
-    if (toolWindow == null) throw new IllegalStateException("Could not find tool window for id " + ChangesViewContentManager.TOOLWINDOW_ID);
-    ContentUtilEx.addTabbedContent(toolWindow.getContentManager(), tabGroupId,
-                                   new TabDescriptor(new VcsLogPanel(logManager, logUi), () -> tabDisplayName.apply(logUi), logUi), focus);
+  @JvmStatic
+  fun <U : VcsLogUiEx> openLogTab(project: Project,
+                                  logManager: VcsLogManager,
+                                  tabGroupId: TabGroupId,
+                                  tabDisplayName: Function<U, @NlsContexts.TabTitle String>,
+                                  factory: VcsLogUiFactory<out U>,
+                                  focus: Boolean): U {
+    val logUi = logManager.createLogUi(factory, VcsLogManager.LogWindowKind.TOOL_WINDOW)
+    val toolWindow = ToolWindowManager.getInstance(project).getToolWindow(ChangesViewContentManager.TOOLWINDOW_ID)
+                     ?: throw IllegalStateException("Could not find tool window for id ${ChangesViewContentManager.TOOLWINDOW_ID}")
+    ContentUtilEx.addTabbedContent(toolWindow.contentManager, tabGroupId,
+                                   TabDescriptor(VcsLogPanel(logManager, logUi), Supplier { tabDisplayName.apply(logUi) }, logUi), focus)
     if (focus) {
-      toolWindow.activate(null);
+      toolWindow.activate(null)
     }
-    return logUi;
+    return logUi
   }
 
-  public static boolean closeLogTab(@NotNull ContentManager manager, @NotNull String tabId) {
-    return ContentUtilEx.closeContentTab(manager, c -> {
-      VcsLogUiEx ui = getLogUi(c);
-      if (ui != null) {
-        return ui.getId().equals(tabId);
-      }
-      return false;
-    });
+  fun closeLogTab(manager: ContentManager, tabId: String): Boolean {
+    return ContentUtilEx.closeContentTab(manager) { c: JComponent ->
+      getLogUi(c)?.id == tabId
+    }
   }
 
-  public static void runInMainLog(@NotNull Project project, @NotNull Consumer<? super MainVcsLogUi> consumer) {
-    ToolWindow window = ToolWindowManager.getInstance(project).getToolWindow(ChangesViewContentManager.TOOLWINDOW_ID);
-    if (window == null || !selectMainLog(window.getContentManager())) {
-      showLogIsNotAvailableMessage(project);
-      return;
+  @JvmStatic
+  fun runInMainLog(project: Project, consumer: Consumer<in MainVcsLogUi>) {
+    val window = ToolWindowManager.getInstance(project).getToolWindow(ChangesViewContentManager.TOOLWINDOW_ID)
+    if (window == null || !selectMainLog(window.contentManager)) {
+      showLogIsNotAvailableMessage(project)
+      return
     }
 
-    Runnable runConsumer = () -> Objects.requireNonNull(VcsLogContentProvider.getInstance(project)).executeOnMainUiCreated(consumer);
-    if (!window.isVisible()) {
-      window.activate(runConsumer);
+    val runConsumer = Runnable { VcsLogContentProvider.getInstance(project)!!.executeOnMainUiCreated(consumer) }
+    if (!window.isVisible) {
+      window.activate(runConsumer)
     }
     else {
-      runConsumer.run();
+      runConsumer.run()
     }
   }
 
   @RequiresEdt
-  public static void showLogIsNotAvailableMessage(@NotNull Project project) {
-    VcsBalloonProblemNotifier.showOverChangesView(project, VcsLogBundle.message("vcs.log.is.not.available"), MessageType.WARNING);
+  fun showLogIsNotAvailableMessage(project: Project) {
+    VcsBalloonProblemNotifier.showOverChangesView(project, VcsLogBundle.message("vcs.log.is.not.available"), MessageType.WARNING)
   }
 
-  private static boolean selectMainLog(@NotNull ContentManager cm) {
-    Content[] contents = cm.getContents();
-    for (Content content : contents) {
+  private fun selectMainLog(cm: ContentManager): Boolean {
+    val contents = cm.contents
+    for (content in contents) {
       // here tab name is used instead of log ui id to select the correct tab
       // it's done this way since main log ui may not be created when this method is called
-      if (VcsLogContentProvider.TAB_NAME.equals(content.getTabName())) {
-        cm.setSelectedContent(content);
-        return true;
+      if (VcsLogContentProvider.TAB_NAME == content.tabName) {
+        cm.setSelectedContent(content)
+        return true
       }
     }
-    return false;
+    return false
   }
 
-  public static boolean selectMainLog(@NotNull Project project) {
-    ToolWindow toolWindow = ToolWindowManager.getInstance(project).getToolWindow(ChangesViewContentManager.TOOLWINDOW_ID);
-    if (toolWindow == null) return false;
-    return selectMainLog(toolWindow.getContentManager());
+  fun selectMainLog(project: Project): Boolean {
+    val toolWindow = ToolWindowManager.getInstance(project).getToolWindow(ChangesViewContentManager.TOOLWINDOW_ID) ?: return false
+    return selectMainLog(toolWindow.contentManager)
   }
 
-  public static void updateLogUiName(@NotNull Project project, @NotNull VcsLogUi ui) {
-    ToolWindow toolWindow = ToolWindowManager.getInstance(project).getToolWindow(ChangesViewContentManager.TOOLWINDOW_ID);
-    if (toolWindow == null) return;
-
-    ContentManager manager = toolWindow.getContentManager();
-    JComponent component = ContentUtilEx.findContentComponent(manager, c -> ui == getLogUi(c));
-    if (component == null) return;
-    ContentUtilEx.updateTabbedContentDisplayName(manager, component);
+  @JvmStatic
+  fun updateLogUiName(project: Project, ui: VcsLogUi) {
+    val toolWindow = ToolWindowManager.getInstance(project).getToolWindow(ChangesViewContentManager.TOOLWINDOW_ID) ?: return
+    val manager = toolWindow.contentManager
+    val component = ContentUtilEx.findContentComponent(manager) { c: JComponent -> ui === getLogUi(c) } ?: return
+    ContentUtilEx.updateTabbedContentDisplayName(manager, component)
   }
 
-  /**
-   * @deprecated use {@link VcsProjectLog#runWhenLogIsReady(Project, Consumer)} instead.
-   */
-  @Deprecated
   @ApiStatus.ScheduledForRemoval(inVersion = "2021.1")
+  @Deprecated("use VcsProjectLog#runWhenLogIsReady(Project, Consumer) instead.")
+  @JvmStatic
   @RequiresBackgroundThread
-  @Nullable
-  public static VcsLogManager getOrCreateLog(@NotNull Project project) {
-    VcsProjectLog.ensureLogCreated(project);
-    return VcsProjectLog.getInstance(project).getLogManager();
+  fun getOrCreateLog(project: Project): VcsLogManager? {
+    VcsProjectLog.ensureLogCreated(project)
+    return VcsProjectLog.getInstance(project).logManager
+  }
+
+  private fun <U> Any?.safeCastTo(clazz: Class<U>): U? {
+    @Suppress("UNCHECKED_CAST")
+    if (this != null && clazz.isInstance(this)) return this as U
+    return null
   }
 }
