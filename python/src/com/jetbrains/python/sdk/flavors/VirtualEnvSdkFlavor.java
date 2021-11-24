a5a8c1b1928f36ff00e8a4f9630bbb9599912548
Semyon Proshev <Semyon.Proshev@jetbrains.com>
2021-11-24 17:52:26 +0300

    Don't use python binary regex for venvs, assume there is always `python/python.exe` binary inside (PY-51527)
    GitOrigin-RevId: 03e5f4ee83428399cbe3e63931a528ce1a12ec38

--- python/src/com/jetbrains/python/sdk/flavors/VirtualEnvSdkFlavor.java
+++ python/src/com/jetbrains/python/sdk/flavors/VirtualEnvSdkFlavor.java
@@ -24,6 +24,7 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 /**
  * User : catherine
@@ -31,7 +32,7 @@ import java.util.List;
 public final class VirtualEnvSdkFlavor extends CPythonSdkFlavor {
   private VirtualEnvSdkFlavor() {
   }
-  private final static String[] NAMES = new String[]{"jython", "pypy", "python.exe", "jython.bat", "pypy.exe"};
+  private final static Set<String> NAMES = Set.of("jython", "pypy", "python", "jython.bat", "pypy.exe", "python.exe");
 
   public static VirtualEnvSdkFlavor getInstance() {
     return PythonSdkFlavor.EP_NAME.findExtension(VirtualEnvSdkFlavor.class);
@@ -121,11 +122,11 @@ public final class VirtualEnvSdkFlavor extends CPythonSdkFlavor {
     if (rootDir != null && rootDir.isDirectory()) {
       final VirtualFile bin = rootDir.findChild("bin");
       final VirtualFile scripts = rootDir.findChild("Scripts");
-      if (bin != null) {
+      if (bin != null && bin.isDirectory()) {
         final String interpreter = findInterpreter(bin);
         if (interpreter != null) candidates.add(interpreter);
       }
-      if (scripts != null) {
+      if (scripts != null && scripts.isDirectory()) {
         final String interpreter = findInterpreter(scripts);
         if (interpreter != null) candidates.add(interpreter);
       }
@@ -142,19 +143,9 @@ public final class VirtualEnvSdkFlavor extends CPythonSdkFlavor {
     for (VirtualFile child : dir.getChildren()) {
       if (!child.isDirectory()) {
         final String childName = StringUtil.toLowerCase(child.getName());
-        for (String name : NAMES) {
-          if (SystemInfo.isWindows) {
-            if (childName.equals(name)) {
-              return FileUtil.toSystemDependentName(child.getPath());
-            }
-          }
-          else {
-            if (childName.startsWith(name) || PYTHON_RE.matcher(childName).matches()) {
-              if (!childName.endsWith("-config")) {
-                return child.getPath();
-              }
-            }
-          }
+        if (NAMES.contains(childName)) {
+          final String childPath = child.getPath();
+          return SystemInfo.isWindows ? FileUtil.toSystemDependentName(childPath) : childPath;
         }
       }
     }
