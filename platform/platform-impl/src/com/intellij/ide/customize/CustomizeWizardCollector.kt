952ce70dcbe5ba41d75d08dac3ef1765d2128a2b
Liubov Afanaseva <liubov.afanaseva@jetbrains.com>
2021-11-25 17:22:15 +0300

    IJ-CR-17423 FUS: "customize.wizard" rewrite after review (IDEA-268269)
    GitOrigin-RevId: bb64fb58acd03262567ce3a6d9a298a0c316e4f1

--- platform/platform-impl/src/com/intellij/ide/customize/CustomizeWizardCollector.kt
+++ platform/platform-impl/src/com/intellij/ide/customize/CustomizeWizardCollector.kt
@@ -4,9 +4,6 @@ import com.intellij.ide.customize.CustomizeIDEWizardInteractionType.*
 import com.intellij.internal.statistic.collectors.fus.actions.persistence.ActionsEventLogGroup
 import com.intellij.internal.statistic.eventLog.EventLogGroup
 import com.intellij.internal.statistic.eventLog.events.EventFields
-import com.intellij.internal.statistic.eventLog.events.EventFields.Int
-import com.intellij.internal.statistic.eventLog.events.EventFields.Long
-import com.intellij.internal.statistic.eventLog.events.EventFields.String
 import com.intellij.internal.statistic.eventLog.events.EventPair
 import com.intellij.internal.statistic.eventLog.events.VarargEventId
 import com.intellij.internal.statistic.service.fus.collectors.CounterUsagesCollector
@@ -18,23 +15,23 @@ class CustomizeWizardCollector : CounterUsagesCollector() {
 
   companion object {
     private val GROUP = EventLogGroup("customize.wizard", 2)
-    private val PAGE_FIELD = Int("page")
-    private val TIMESTAMP_FIELD = Long("timestamp")
-    private val GROUP_FIELD = String("group", listOf("Java_Frameworks", "Web_Development", "Version_Controls", "Test_Tools",
-                                                     "Application_Servers", "Clouds", "Swing", "Android", "Database_Tools",
-                                                     "Other_Tools", "Plugin_Development", "Build_Tools"))
+    private val PAGE_FIELD = EventFields.Int("page")
+    private val TIMESTAMP_FIELD = EventFields.Long("timestamp")
+    private val GROUP_FIELD = EventFields.String("group", listOf("Java_Frameworks", "Web_Development", "Version_Controls", "Test_Tools",
+                                                                 "Application_Servers", "Clouds", "Swing", "Android", "Database_Tools",
+                                                                 "Other_Tools", "Plugin_Development", "Build_Tools"))
     private val REMAINING_PAGES_SKIPPED_EVENT = GROUP.registerEvent("remaining.pages.skipped", PAGE_FIELD)
 
-    private val WIZARD_DISPLAYED_EVENT = getEvent(WizardDisplayed.toString())
-    private val BUNDLED_PLUGIN_GROUP_ENABLED_EVENT = getEvent(BundledPluginGroupEnabled.toString())
-    private val BUNDLED_PLUGIN_GROUP_CUSTOMIZED_EVENT = getEvent(BundledPluginGroupCustomized.toString())
-    private val BUNDLED_PLUGIN_GROUP_DISABLED_EVENT = getEvent(BundledPluginGroupDisabled.toString())
-    private val DESKTOP_ENTRY_CREATED_EVENT = getEvent(DesktopEntryCreated.toString())
-    private val LAUNCHER_SCRIPT_CREATED_EVENT = getEvent(LauncherScriptCreated.toString())
-    private val FEATURED_PLUGIN_INSTALLED_EVENT = getEvent(FeaturedPluginInstalled.toString())
-    private val UI_THEME_CHANGED_EVENT = getEvent(UIThemeChanged.toString())
+    private val WIZARD_DISPLAYED_EVENT = registerActionEvent(WizardDisplayed.toString())
+    private val BUNDLED_PLUGIN_GROUP_ENABLED_EVENT = registerActionEvent(BundledPluginGroupEnabled.toString())
+    private val BUNDLED_PLUGIN_GROUP_CUSTOMIZED_EVENT = registerActionEvent(BundledPluginGroupCustomized.toString())
+    private val BUNDLED_PLUGIN_GROUP_DISABLED_EVENT = registerActionEvent(BundledPluginGroupDisabled.toString())
+    private val DESKTOP_ENTRY_CREATED_EVENT = registerActionEvent(DesktopEntryCreated.toString())
+    private val LAUNCHER_SCRIPT_CREATED_EVENT = registerActionEvent(LauncherScriptCreated.toString())
+    private val FEATURED_PLUGIN_INSTALLED_EVENT = registerActionEvent(FeaturedPluginInstalled.toString())
+    private val UI_THEME_CHANGED_EVENT = registerActionEvent(UIThemeChanged.toString())
 
-    fun getEvent(eventId: String): VarargEventId {
+    fun registerActionEvent(eventId: String): VarargEventId {
       return ActionsEventLogGroup.registerActionEvent(GROUP, eventId, TIMESTAMP_FIELD, EventFields.PluginInfo, GROUP_FIELD)
     }
 
@@ -43,10 +40,11 @@ class CustomizeWizardCollector : CounterUsagesCollector() {
     }
 
     fun logEvent(eventId: CustomizeIDEWizardInteractionType, timestamp: Long, pluginInfo: PluginInfo?, group: String?) {
-      val data: MutableList<EventPair<*>> = ArrayList()
+      val data = mutableListOf<EventPair<*>>()
       data.add(TIMESTAMP_FIELD.with(timestamp))
       if (pluginInfo != null) {
-        data.add(EventFields.PluginInfo.with(pluginInfo))
+        data.add(EventFields.PluginInfo.with
+        (pluginInfo))
       }
       if (group != null) {
         data.add(GROUP_FIELD.with(group))
