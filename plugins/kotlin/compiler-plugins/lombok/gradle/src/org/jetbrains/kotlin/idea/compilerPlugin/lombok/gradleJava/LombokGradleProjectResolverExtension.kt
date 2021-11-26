a15482d54a396d18d231b62fa59487b78ccb988b
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2021-11-26 20:31:40 +0900

    Fix Lombok IDE support compilation
    Adapt the Lombok IDE support module to the new plugin model,
    separating modules for Maven and Gradle support.
    
    GitOrigin-RevId: d9e71801be47d99f3a3848cd4fdb2443a73a282a

--- plugins/kotlin/compiler-plugins/lombok/gradle/src/org/jetbrains/kotlin/idea/compilerPlugin/lombok/gradleJava/LombokGradleProjectResolverExtension.kt
+++ plugins/kotlin/compiler-plugins/lombok/gradle/src/org/jetbrains/kotlin/idea/compilerPlugin/lombok/gradleJava/LombokGradleProjectResolverExtension.kt
@@ -1,11 +1,11 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-
-package org.jetbrains.kotlin.lombok.ide
+package org.jetbrains.kotlin.idea.compilerPlugin.lombok.gradleJava
 
 import com.intellij.openapi.externalSystem.model.DataNode
 import com.intellij.openapi.externalSystem.model.project.ModuleData
 import com.intellij.openapi.util.Key
 import org.gradle.tooling.model.idea.IdeaModule
+import org.jetbrains.kotlin.idea.gradleTooling.model.lombok.LombokModel
 import org.jetbrains.plugins.gradle.service.project.AbstractProjectResolverExtension
 
 class LombokGradleProjectResolverExtension : AbstractProjectResolverExtension() {
