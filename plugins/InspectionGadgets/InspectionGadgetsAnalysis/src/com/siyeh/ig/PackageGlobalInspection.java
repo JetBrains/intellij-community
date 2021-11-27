47a1baf6dcc7bf8868d996bfa0268d6531f224d7
Bas Leijdekkers <basleijdekkers@gmail.com>
2021-11-27 13:54:53 +0100

    use job descriptor so progress is displayed while analyzing
    GitOrigin-RevId: 577aee51d3314f33a982fad14a8817420ffe25b7

--- plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/PackageGlobalInspection.java
+++ plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/PackageGlobalInspection.java
@@ -6,15 +6,18 @@ import com.intellij.codeInspection.CommonProblemDescriptor;
 import com.intellij.codeInspection.GlobalInspectionContext;
 import com.intellij.codeInspection.InspectionManager;
 import com.intellij.codeInspection.ProblemDescriptionsProcessor;
+import com.intellij.codeInspection.ex.JobDescriptor;
 import com.intellij.codeInspection.reference.RefEntity;
 import com.intellij.codeInspection.reference.RefJavaManager;
 import com.intellij.codeInspection.reference.RefManagerImpl;
 import com.intellij.codeInspection.reference.RefPackage;
 import com.intellij.openapi.application.ReadAction;
+import com.intellij.openapi.util.IntRef;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiClassOwner;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
+import com.siyeh.InspectionGadgetsBundle;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -29,6 +32,9 @@ import java.util.Set;
  * @author Bas Leijdekkers
  */
 public abstract class PackageGlobalInspection extends BaseGlobalInspection {
+
+  private final JobDescriptor myJobDescriptor = new JobDescriptor("");
+
   @Override
   public boolean isGraphNeeded() {
     return false;
@@ -44,15 +50,20 @@ public abstract class PackageGlobalInspection extends BaseGlobalInspection {
       super.runInspection(scope, manager, globalContext, problemDescriptionsProcessor);
     }
     else {
+      final AnalysisScope analysisScope = globalContext.getRefManager().getScope();
+      assert analysisScope != null;
+      myJobDescriptor.setTotalAmount(analysisScope.getFileCount());
       final Set<String> packages = new HashSet<>();
-      scope.accept(file -> {
-        if (file.isDirectory()) return true;
-        String packageName = ReadAction.compute(() -> {
+      final IntRef current = new IntRef();
+      analysisScope.accept(file -> {
+        current.inc();
+        if (file.isDirectory() || !scope.contains(file)) return true;
+        String packageName = ReadAction.nonBlocking(() -> {
           final PsiFile element = PsiManager.getInstance(scope.getProject()).findFile(file);
           if (!(element instanceof PsiClassOwner)) return null;
           final PsiClassOwner classOwner = (PsiClassOwner)element;
           return classOwner.getPackageName();
-        });
+        }).executeSynchronously();
         if (packageName == null) {
           return true;
         }
@@ -60,6 +71,9 @@ public abstract class PackageGlobalInspection extends BaseGlobalInspection {
           if (!packages.add(packageName)) {
             return true;
           }
+          myJobDescriptor.setDoneAmount(current.get());
+          globalContext.incrementJobDoneAmount(myJobDescriptor,
+                                               InspectionGadgetsBundle.message("progress.text.analyzing.package.0", packageName));
           final RefPackage aPackage = (RefPackage)globalContext.getRefManager().getReference(RefJavaManager.PACKAGE, packageName);
           if (aPackage == null) return true;
           CommonProblemDescriptor[] descriptors = checkPackage(aPackage, scope, manager, globalContext);
@@ -72,6 +86,11 @@ public abstract class PackageGlobalInspection extends BaseGlobalInspection {
         return true;
       });
     }
+  }
+
+  @Override
+  public JobDescriptor @Nullable [] getAdditionalJobs(@NotNull GlobalInspectionContext context) {
+    return new JobDescriptor[] { myJobDescriptor };
   }
 
   @Override
