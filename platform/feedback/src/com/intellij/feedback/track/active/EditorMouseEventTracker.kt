68200bd089ac51c69ee30b22cd1136eb6b7ff24c
Dmitry Pogrebnoy <dmitry.pogrebnoy@jetbrains.com>
2021-11-28 12:26:12 +0300

    [Feedback] Rework and generalize the process of showing feedback
    IJ-CR-17867
    
    GitOrigin-RevId: d5e4260c680b16df454d0bb6ab94844c326d9b24

--- platform/feedback/src/com/intellij/feedback/track/active/EditorMouseEventTracker.kt
+++ platform/feedback/src/com/intellij/feedback/track/active/EditorMouseEventTracker.kt
@@ -1,7 +1,7 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.feedback.track.active
 
-import com.intellij.feedback.state.active.LastActive.trackActive
+import com.intellij.feedback.FeedbackTypeResolver
 import com.intellij.openapi.editor.event.EditorMouseEvent
 import com.intellij.openapi.editor.event.EditorMouseListener
 import com.intellij.openapi.editor.event.EditorMouseMotionListener
@@ -12,10 +12,10 @@ import com.intellij.openapi.editor.event.EditorMouseMotionListener
 
 class EditorMouseEventTracker : EditorMouseListener, EditorMouseMotionListener {
   override fun mouseClicked(event: EditorMouseEvent) {
-    trackActive(event.editor.project ?: return)
+    FeedbackTypeResolver.checkActivity(event.editor.project)
   }
 
   override fun mouseMoved(event: EditorMouseEvent) {
-    trackActive(event.editor.project ?: return)
+    FeedbackTypeResolver.checkActivity(event.editor.project)
   }
 }
