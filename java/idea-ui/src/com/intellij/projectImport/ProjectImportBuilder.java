4b30f36cfd19bc0e1adf057c76a6f5b17a596a16
Louis Vignier <louis.vignier@jetbrains.com>
2021-11-29 18:08:04 +0100

    IJ-CR-16241 Document EP-s
    GitOrigin-RevId: 5a8701d8129dc2f225cec124dca7bc66391c8dc0

--- java/idea-ui/src/com/intellij/projectImport/ProjectImportBuilder.java
+++ java/idea-ui/src/com/intellij/projectImport/ProjectImportBuilder.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.projectImport;
 
 import com.intellij.ide.DataManager;
@@ -21,6 +21,8 @@ import javax.swing.*;
 import java.util.List;
 
 /**
+ * An extension point for importing modules from a given file
+ * (see {@link com.intellij.ide.util.newProjectWizard.AddModuleWizard#initModuleWizard}).
  * @author Vladislav.Kaznacheev
  */
 public abstract class ProjectImportBuilder<T> extends ProjectBuilder {
--- platform/analysis-api/src/com/intellij/codeInspection/lang/InspectionExtensionsFactory.java
+++ platform/analysis-api/src/com/intellij/codeInspection/lang/InspectionExtensionsFactory.java
@@ -1,19 +1,4 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInspection.lang;
 
 import com.intellij.codeInspection.HTMLComposer;
@@ -26,13 +11,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * Provides implementations for the following interfaces:
- *
- * <ul>
- *   <li>{@link GlobalInspectionContextExtension} to perform activities before and after running inspections (see {@link com.intellij.codeInspection.GlobalInspectionContext});</li>
- *   <li>{@link RefManagerExtension} to manage the reference graph (see {@link RefManager});</li>
- *   <li>{@link HTMLComposerExtension} to output HTML (see {@link HTMLComposer});</li>
- * </ul>
+ * An extension point to extend the framework under global inspections.
  */
 public abstract class InspectionExtensionsFactory {
 
