97d95ea58e4624b59b5d89d40d85d8f5643af7b4
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2021-11-29 10:35:03 -0800

    [lang-impl] fix typo: ReverteOverrideFileTypeAction -> RevertOverrideFileTypeAction
    GitOrigin-RevId: 478d7c9e145d1c1056561642fac6f955bed3ae7c

--- platform/lang-impl/src/com/intellij/openapi/file/exclude/RevertOverrideFileTypeAction.java
+++ platform/lang-impl/src/com/intellij/openapi/file/exclude/RevertOverrideFileTypeAction.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.file.exclude;
 
 import com.intellij.idea.ActionsBundle;
@@ -9,15 +9,15 @@ import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 
 /** removes destruction caused by {@link OverrideFileTypeAction} and restores the original file type */
-class ReverteOverrideFileTypeAction extends AnAction {
+class RevertOverrideFileTypeAction extends AnAction {
   @Override
   public void update(@NotNull AnActionEvent e) {
     VirtualFile[] files = OverrideFileTypeAction.getContextFiles(e, file -> OverrideFileTypeManager.getInstance().getFileValue(file) != null);
     Presentation presentation = e.getPresentation();
     boolean enabled = files.length != 0;
     presentation.setDescription(enabled
-                                ? ActionsBundle.message("action.ReverteOverrideFileTypeAction.verbose.description", files[0].getName(), files.length - 1)
-                                : ActionsBundle.message("action.ReverteOverrideFileTypeAction.description"));
+                                ? ActionsBundle.message("action.RevertOverrideFileTypeAction.verbose.description", files[0].getName(), files.length - 1)
+                                : ActionsBundle.message("action.RevertOverrideFileTypeAction.description"));
     presentation.setEnabledAndVisible(enabled);
   }
 
