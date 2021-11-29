6fbe7d86bd98003916fd475905d7a1c108fd55aa
Petr Golubev <petr.golubev@jetbrains.com>
2021-11-29 20:15:36 +0300

    deleted unused method CodeStyleSettingsService.getLanguageCodeStyleProvider(Language)
    GitOrigin-RevId: 6366e1007f0d00d8caff32245084e9b60f1a4762

--- platform/code-style-api/src/com/intellij/psi/codeStyle/CodeStyleSettingsService.java
+++ platform/code-style-api/src/com/intellij/psi/codeStyle/CodeStyleSettingsService.java
@@ -1,7 +1,6 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.codeStyle;
 
-import com.intellij.lang.Language;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ApplicationManager;
 import org.jetbrains.annotations.ApiStatus;
@@ -15,15 +14,6 @@ import java.util.Set;
 public interface CodeStyleSettingsService {
   static CodeStyleSettingsService getInstance() {
     return ApplicationManager.getApplication().getService(CodeStyleSettingsService.class);
-  }
-
-  /**
-   * @deprecated Use {@link LanguageCodeStyleProvider#forLanguage(Language)}.
-   */
-  @Deprecated
-  @Nullable
-  static LanguageCodeStyleProvider getLanguageCodeStyleProvider(Language language) {
-    return LanguageCodeStyleProvider.forLanguage(language);
   }
 
   void addListener(@NotNull CodeStyleSettingsServiceListener listener, @Nullable Disposable disposable);
