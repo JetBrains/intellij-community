e3068165830c8e443730f159800ff47c841b4b6d
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2021-11-29 18:51:36 +0300

    fixes in hierarchical hover after review
    GitOrigin-RevId: 5883d9ce2bf18181686136e7b84d0f6f17872116

--- platform/platform-impl/src/com/intellij/ide/HoverService.java
+++ platform/platform-impl/src/com/intellij/ide/HoverService.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide;
 
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.ui.hover.HoverListener;
 import com.intellij.util.SmartList;
 import org.jetbrains.annotations.NotNull;
@@ -12,8 +13,10 @@ import java.awt.Point;
 import java.awt.Window;
 import java.awt.event.MouseEvent;
 import java.lang.ref.WeakReference;
+import java.util.function.Consumer;
 
 final class HoverService {
+  private static final Logger LOG = Logger.getInstance(HoverService.class);
   private final SmartList<ComponentPoint> hierarchy = new SmartList<>();
 
   void process(@NotNull AWTEvent event) {
@@ -40,25 +43,34 @@ final class HoverService {
     while (index < hierarchySize && index < componentsCount && components.get(index) == hierarchy.get(index).reference.get()) index++;
     while (index < hierarchySize) hierarchy.remove(--hierarchySize).mouseExited();
     if (index == componentsCount) {
+      // notify only the deepest component in the hover hierarchy
       if (index > 0) hierarchy.get(index - 1).mouseMoved(event);
     }
     else {
-      while (index < componentsCount) hierarchy.add(new ComponentPoint(event, components.get(index++)));
+      while (index < componentsCount) {
+        ComponentPoint point = new ComponentPoint(components.get(index++));
+        hierarchy.add(point);
+        point.mouseEntered(event);
+      }
     }
   }
 
 
   private static final class ComponentPoint {
     private final WeakReference<Component> reference;
-    private final Point point;
+    private final Point point = new Point();
 
-    private ComponentPoint(@NotNull MouseEvent event, @NotNull Component component) {
+    private ComponentPoint(@NotNull Component component) {
       reference = new WeakReference<>(component);
-      point = new Point(event.getXOnScreen(), event.getYOnScreen());
+    }
+
+    private void mouseEntered(@NotNull MouseEvent event) {
+      Component component = reference.get();
+      if (component == null) return; // component is already collected
+      point.setLocation(event.getXOnScreen(), event.getYOnScreen());
       SwingUtilities.convertPointFromScreen(point, component);
-      for (HoverListener listener : HoverListener.getAll(component)) {
-        listener.mouseEntered(component, point.x, point.y);
-      }
+      if (LOG.isDebugEnabled()) LOG.debug("mouse entered into " + component);
+      notifySafely(component, listener -> listener.mouseEntered(component, point.x, point.y));
     }
 
     private void mouseMoved(@NotNull MouseEvent event) {
@@ -69,16 +81,25 @@ final class HoverService {
       point.setLocation(event.getXOnScreen(), event.getYOnScreen());
       SwingUtilities.convertPointFromScreen(point, component);
       if (point.x == x && point.y == y) return; // mouse location is not changed
-      for (HoverListener listener : HoverListener.getAll(component)) {
-        listener.mouseMoved(component, point.x, point.y);
-      }
+      if (LOG.isTraceEnabled()) LOG.trace("mouse moved in " + component);
+      notifySafely(component, listener -> listener.mouseMoved(component, point.x, point.y));
     }
 
     private void mouseExited() {
       Component component = reference.get();
       if (component == null) return; // component is already collected
+      if (LOG.isDebugEnabled()) LOG.debug("mouse exited from " + component);
+      notifySafely(component, listener -> listener.mouseExited(component));
+    }
+
+    private static void notifySafely(@NotNull Component component, @NotNull Consumer<HoverListener> notify) {
       for (HoverListener listener : HoverListener.getAll(component)) {
-        listener.mouseExited(component);
+        try {
+          notify.accept(listener);
+        }
+        catch (Exception exception) {
+          LOG.error(exception);
+        }
       }
     }
   }
