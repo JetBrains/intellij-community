6076ba5ab9070b8db9db814adc4c781a3218ec96
Elena.Korotkova <elena.korotkova@jetbrains.com>
2021-11-30 01:56:02 +0300

    new toolbar: fix widgets overlap on resize
    (cherry picked from commit dfbc3cc48e7b0f14e70d321bfbf1afec370b21f8)
    
    IJ-MR-17927
    
    GitOrigin-RevId: bdf88baf5118cd8aa1cc2548cecc8051ba818739

--- platform/lang-impl/src/com/intellij/ide/navigationToolbar/experimental/SearchEverywhereNewToolbarAction.kt
+++ platform/lang-impl/src/com/intellij/ide/navigationToolbar/experimental/SearchEverywhereNewToolbarAction.kt
@@ -120,6 +120,9 @@ class SearchEverywhereNewToolbarAction : SearchEverywhereAction(), AnActionListe
       }
 
       override fun paint(g: Graphics?) {
+        if (parent.bounds.width < parent.preferredSize.width) {
+          return
+        }
         foreground = DISABLED_TEXT_COLOR
         background = searchFieldBackground()
         super.paint(g)
@@ -130,6 +133,9 @@ class SearchEverywhereNewToolbarAction : SearchEverywhereAction(), AnActionListe
       }
 
       override fun paintComponent(g: Graphics) {
+        if (parent.bounds.width < parent.preferredSize.width) {
+          return
+        }
         setupAntialiasing(g)
 
         val fm = getFontMetrics(font)
