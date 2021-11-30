2de7bef7bcf13ff82064001a82f156f0ca8b984d
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2021-11-30 19:06:55 +0900

    Add missing scripting libraries to the Kotlin plugin artifact
    GitOrigin-RevId: 8a5e2d03700d41e05c3e956f2426bef537353f66

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/kotlin/KotlinPluginBuilder.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/kotlin/KotlinPluginBuilder.groovy
@@ -117,6 +117,9 @@ final class KotlinPluginBuilder {
     "kotlinc.kotlin-script-util",
     "kotlin-script-runtime",
     "kotlinc.kotlin-scripting-compiler",
+    "kotlinc.kotlin-scripting-compiler-impl",
+    "kotlinc.kotlin-scripting-common",
+    "kotlinc.kotlin-scripting-jvm",
     "kotlinc.kotlin-gradle-statistics",
     )
 
