bf4b20ffb9025b9bcad7d45133183ac2f9097501
Andrei Klunnyi <andrei.klunnyi@jetbrains.com>
2021-11-30 11:04:48 +0100

    KTIJ-18464 [MPP]: tests
    This commit introduces tests checking gradle-test-run
    configurations for MPP project [1].
    
    During tests execution gradle- and kotlin-plugins are loaded
    by separate class loaders. Protected methods of a base class
    loaded by a different class loader are effectively private.
    To overcome this limitation visibility of some base classes
    methods was extended [2][3].
    
    [1]: GradleMppRunConfigurationProducersTest
    [2]: GradleTestRunConfigurationProducer
    [3]: GradleTestRunConfigurationProducerTestCase
    
    GitOrigin-RevId: beb9417ce522e6e39dd242a319b0fff837dfe906

--- plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/idea/codeInsight/gradle/KotlinVersionUtilsTest.kt
+++ plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/idea/codeInsight/gradle/KotlinVersionUtilsTest.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.kotlin.util
+package org.jetbrains.kotlin.idea.codeInsight.gradle
 
 import org.junit.Test
 import kotlin.test.assertFalse
