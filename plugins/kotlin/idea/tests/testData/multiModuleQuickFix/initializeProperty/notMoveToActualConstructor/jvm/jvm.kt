2a4edc98880d5dc84ff9e4ddfec28d923fa381cc
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-11-30 08:36:27 +0100

    [kotlin] mpp: do not suggest to move/initialize property to/in `actual` constructor
    InitializePropertyQuickFix shouldn't suggest such refactorings if the target constructor is `actual`
    
    ^KTIJ-13227 Fixed
    
    (cherry picked from commit 01e81eea679625d05314744cecc39315ef9c8d46)
    
    IJ-CR-17953
    
    (cherry picked from commit 73f745acdacfae90565e0f73043bf67bd2485f7a)
    
    GitOrigin-RevId: 3f6d5e867c7becb37c2b15876e14aa2825fa219e

---
+++ plugins/kotlin/idea/tests/testData/multiModuleQuickFix/initializeProperty/notMoveToActualConstructor/jvm/jvm.kt
@@ -0,0 +1,15 @@
+// "Move to constructor parameters" "false"
+// ACTION: Add getter
+// ACTION: Add initializer
+// ACTION: Apply all 'Add modifier' fixes in file
+// ACTION: Convert member to extension
+// ACTION: Convert property to function
+// ACTION: Introduce backing property
+// ACTION: Make 'b' 'abstract'
+// ACTION: Move to companion object
+// ACTION: Move to constructor
+// ERROR: Property must be initialized or be abstract
+
+actual class SimpleWConstructor actual constructor(i: Int) {
+    actual val <caret>b: String
+}
