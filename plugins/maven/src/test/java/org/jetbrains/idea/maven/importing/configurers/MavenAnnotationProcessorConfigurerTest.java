0af9b2a0a78cf5e696a149b28fcdb66df59e4673
grigoriy.myasoedov <grigoriy.myasoedov@jetbrains.com>
2021-11-30 15:38:58 +0300

    [maven] IDEA-169048 Maven integration enables Annotation processors
    no remove manually disabled profiles (created profile after import - enabled by default)
    
    GitOrigin-RevId: 8acd06d6bbbdd22153c3821fffda4e546e2f8348

--- plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/configurers/MavenAnnotationProcessorConfigurerTest.java
+++ plugins/maven/src/test/java/org/jetbrains/idea/maven/importing/configurers/MavenAnnotationProcessorConfigurerTest.java
@@ -36,4 +36,45 @@ public class MavenAnnotationProcessorConfigurerTest extends MavenImportingTestCa
     Assert.assertNotNull(profile);
     Assert.assertFalse(profile.isEnabled());
   }
+
+  public void testDisabledAnnotationProcessorWithModule() {
+    createProjectPom("<groupId>test</groupId>" +
+                     "<artifactId>project</artifactId>" +
+                     "<packaging>pom</packaging>" +
+                     "<version>1</version>" +
+
+                     "<modules>" +
+                     "  <module>m1</module>" +
+                     "</modules>");
+
+    createModulePom("m1", "<groupId>test</groupId>" +
+                          "<artifactId>m1</artifactId>" +
+                          "<version>1</version>");
+
+    importProject();
+
+    CompilerConfigurationImpl compilerConfiguration = (CompilerConfigurationImpl)CompilerConfiguration.getInstance(myProject);
+    ProcessorConfigProfile profile = compilerConfiguration.findModuleProcessorProfile(MAVEN_DEFAULT_ANNOTATION_PROFILE);
+    Assert.assertNotNull(profile);
+    Assert.assertTrue(profile.isEnabled());
+
+    WriteAction.runAndWait(() -> {
+      ProcessorConfigProfile p = compilerConfiguration.findModuleProcessorProfile(MAVEN_DEFAULT_ANNOTATION_PROFILE);
+      p.setEnabled(false);
+      p.removeModuleName("m1");
+
+      ProcessorConfigProfile defaultProfile = compilerConfiguration.getDefaultProcessorProfile();
+      defaultProfile.addModuleName("m1");
+    });
+
+    profile = compilerConfiguration.findModuleProcessorProfile(MAVEN_DEFAULT_ANNOTATION_PROFILE);
+    Assert.assertNotNull(profile);
+    Assert.assertFalse(profile.isEnabled());
+
+    importProject();
+    profile = compilerConfiguration.findModuleProcessorProfile(MAVEN_DEFAULT_ANNOTATION_PROFILE);
+    Assert.assertNotNull(profile);
+    Assert.assertTrue(profile.getModuleNames().isEmpty());
+    Assert.assertFalse(profile.isEnabled());
+  }
 }
