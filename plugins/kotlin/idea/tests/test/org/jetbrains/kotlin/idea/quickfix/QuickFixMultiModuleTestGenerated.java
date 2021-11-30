64cb5254b829cfebac7d2058aba1e7bb9fa935b4
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-11-30 15:06:19 +0700

    [kotlin] mpp: do not suggest "Move to constructor" if `actual` constructor exists
    ^KTIJ-20288 Fixed
    
    (cherry picked from commit 3be80ba53cf12bb8a7ad65156460638ebbe5fd7f)
    
    IJ-CR-17953
    
    (cherry picked from commit 5e2bff7603775d422f2014d15acbf1df46c073d3)
    
    GitOrigin-RevId: 14f267a4c951c59b270d52cbe6aa50fb057e407d

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/quickfix/QuickFixMultiModuleTestGenerated.java
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/quickfix/QuickFixMultiModuleTestGenerated.java
@@ -1255,6 +1255,16 @@ public abstract class QuickFixMultiModuleTestGenerated extends AbstractQuickFixM
             runTest("testData/multiModuleQuickFix/other/memberValToExtensionByImpl/");
         }
 
+        @TestMetadata("moveActualPropertyToExistentConstructor")
+        public void testMoveActualPropertyToExistentConstructor() throws Exception {
+            runTest("testData/multiModuleQuickFix/other/moveActualPropertyToExistentConstructor/");
+        }
+
+        @TestMetadata("movePropertyToConstructor")
+        public void testMovePropertyToConstructor() throws Exception {
+            runTest("testData/multiModuleQuickFix/other/movePropertyToConstructor/");
+        }
+
         @TestMetadata("orderHeader")
         public void testOrderHeader() throws Exception {
             runTest("testData/multiModuleQuickFix/other/orderHeader/");
