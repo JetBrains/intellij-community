cc0f301b1bfd68f5e6189ee84c3fa723aeb93aba
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2021-12-02 15:43:01 +0100

    [built-in server] show product icon as a default favicon (WEB-53858)
    GitOrigin-RevId: 764e1956bc57cb2cd8540758bdc4b72e15aa9ec4

--- platform/built-in-server/src/org/jetbrains/builtInWebServer/FavIconHttpRequestHandler.kt
+++ platform/built-in-server/src/org/jetbrains/builtInWebServer/FavIconHttpRequestHandler.kt
@@ -1,5 +1,5 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.ide
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.builtInWebServer
 
 import com.intellij.ide.ui.ProductIcons
 import com.intellij.util.ui.ImageUtil
@@ -16,17 +16,16 @@ import org.jetbrains.io.response
 import org.jetbrains.io.send
 import java.awt.image.BufferedImage
 
-internal class FavIconHttpRequestHandler : HttpRequestHandler() {
-  override fun process(urlDecoder: QueryStringDecoder, request: FullHttpRequest, context: ChannelHandlerContext): Boolean {
-    if (urlDecoder.path() != "/favicon.ico") {
+internal object FavIconHttpRequestHandler {
+  fun sendDefaultFavIcon(rawPath: String, request: FullHttpRequest, context: ChannelHandlerContext): Boolean {
+    if (rawPath != "/favicon.ico") {
       return false
     }
-
     val icon = ProductIcons.getInstance().productIcon
     val image = ImageUtil.createImage(icon.iconWidth, icon.iconHeight, BufferedImage.TYPE_INT_ARGB)
     icon.paintIcon(null, image.graphics, 0, 0)
     val icoBytes = Imaging.writeImageToBytes(image, ImageFormats.ICO, null)
-    response(FileResponses.getContentType(urlDecoder.path()), Unpooled.wrappedBuffer(icoBytes))
+    response(FileResponses.getContentType(rawPath), Unpooled.wrappedBuffer(icoBytes))
       .addNoCache()
       .send(context.channel(), request)
     return true
