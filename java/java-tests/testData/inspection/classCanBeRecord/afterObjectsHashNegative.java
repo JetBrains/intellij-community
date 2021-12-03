d8a3f626b673ac8990edb76d1cbb5bee87a4581c
Ilyas Selimov <ilyas.selimov@jetbrains.com>
2021-12-03 13:55:33 +0700

    IDEA-251722 - remove plain hashCode method during class-to-record conversion
    GitOrigin-RevId: 1db63c1bdeec14f3ec6ef2aced3e089572633dcc

---
+++ java/java-tests/testData/inspection/classCanBeRecord/afterObjectsHashNegative.java
@@ -0,0 +1,10 @@
+// "Convert to a record" "true"
+import java.util.Objects;
+
+record Test(boolean booleanValue, char charValue, String stringValue, long longValue, float floatValue,
+            double doubleValue, double[] arrayValue) {
+    @Override
+    public int hashCode() {
+        return Objects.hash(booleanValue, stringValue, longValue, floatValue, doubleValue, arrayValue);
+    }
+}
