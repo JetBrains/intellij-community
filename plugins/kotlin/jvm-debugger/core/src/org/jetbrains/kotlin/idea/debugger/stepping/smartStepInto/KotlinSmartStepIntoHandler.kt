b62d795695326e9967424e476347fc367e819085
Nikita Nazarov <nikita.nazarov@jetbrains.com>
2021-12-06 10:51:29 +0300

    [kotlin] implement asynchronous smart step target fetching
    ^KTIJ-19896 fixed
    ^KTIJ-20281 fixed
    ^KTIJ-19441 fixed
    
    (cherry picked from commit c006fdb152440b56456e943501b1ff4171721a70)
    
    IJ-CR-18230
    
    GitOrigin-RevId: 7600be3a22eee016195f69bbdaf506c4849b5e08

--- plugins/kotlin/jvm-debugger/core/src/org/jetbrains/kotlin/idea/debugger/stepping/smartStepInto/KotlinSmartStepIntoHandler.kt
+++ plugins/kotlin/jvm-debugger/core/src/org/jetbrains/kotlin/idea/debugger/stepping/smartStepInto/KotlinSmartStepIntoHandler.kt
@@ -6,13 +6,19 @@ import com.intellij.debugger.SourcePosition
 import com.intellij.debugger.actions.JvmSmartStepIntoHandler
 import com.intellij.debugger.actions.SmartStepTarget
 import com.intellij.debugger.engine.MethodFilter
+import com.intellij.debugger.engine.SuspendContextImpl
+import com.intellij.debugger.engine.events.DebuggerContextCommandImpl
 import com.intellij.debugger.impl.DebuggerSession
+import com.intellij.debugger.impl.PrioritizedTask
+import com.intellij.openapi.application.ReadAction
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.util.Range
 import com.intellij.util.containers.OrderedSet
+import org.jetbrains.concurrency.AsyncPromise
+import org.jetbrains.concurrency.Promise
+import org.jetbrains.concurrency.compute
 import org.jetbrains.kotlin.idea.core.util.CodeInsightUtils.getTopmostElementAtOffset
 import org.jetbrains.kotlin.idea.debugger.KotlinDebuggerSettings
-import org.jetbrains.kotlin.idea.util.application.runReadAction
 import org.jetbrains.kotlin.psi.KtElement
 import org.jetbrains.kotlin.psi.KtFile
 
@@ -26,21 +32,25 @@ class KotlinSmartStepIntoHandler : JvmSmartStepIntoHandler() {
             super.findStepIntoTargets(position, session)
         }
 
-    override fun findSmartStepTargets(position: SourcePosition): List<SmartStepTarget> {
-        val file = position.file
-        val element = position.elementAt ?: return emptyList()
-        val ktElement = getTopmostElementAtOffset(element, element.textRange.startOffset) as? KtElement
-        val elementTextRange = ktElement?.textRange ?: return emptyList()
-        val document = PsiDocumentManager.getInstance(file.project).getDocument(file) ?: return emptyList()
-        val lines = Range(document.getLineNumber(elementTextRange.startOffset), document.getLineNumber(elementTextRange.endOffset))
+    override fun findSmartStepTargetsAsync(position: SourcePosition, session: DebuggerSession): Promise<List<SmartStepTarget>> {
+        val result = AsyncPromise<List<SmartStepTarget>>()
+        session.process.managerThread.schedule(
+            object : DebuggerContextCommandImpl(session.contextManager.context) {
+                override fun threadAction(suspendContext: SuspendContextImpl) =
+                    result.compute { findSmartStepTargetsInReadAction(position) }
 
-        return runReadAction {
-            val consumer = OrderedSet<SmartStepTarget>()
-            val visitor = SmartStepTargetVisitor(ktElement, lines, consumer)
-            ktElement.accept(visitor, null)
-            return@runReadAction consumer
-        }
+                override fun commandCancelled() {
+                    result.setError("Cancelled")
+                }
+
+                override fun getPriority() =
+                    PrioritizedTask.Priority.NORMAL
+            })
+        return result
     }
+
+    override fun findSmartStepTargets(position: SourcePosition): List<SmartStepTarget> =
+        findSmartStepTargetsInReadAction(position)
 
     override fun createMethodFilter(stepTarget: SmartStepTarget?): MethodFilter? {
         return when (stepTarget) {
@@ -48,4 +58,22 @@ class KotlinSmartStepIntoHandler : JvmSmartStepIntoHandler() {
             else -> super.createMethodFilter(stepTarget)
         }
     }
+}
+
+private fun findSmartStepTargetsInReadAction(position: SourcePosition) =
+    ReadAction.nonBlocking<List<SmartStepTarget>> {
+        findSmartStepTargets(position)
+    }.executeSynchronously()
+
+private fun findSmartStepTargets(position: SourcePosition): List<SmartStepTarget> {
+    val element = position.elementAt ?: return emptyList()
+    val ktElement = getTopmostElementAtOffset(element, element.textRange.startOffset) as? KtElement
+    val elementTextRange = ktElement?.textRange ?: return emptyList()
+    val file = position.file
+    val document = PsiDocumentManager.getInstance(file.project).getDocument(file) ?: return emptyList()
+    val lines = Range(document.getLineNumber(elementTextRange.startOffset), document.getLineNumber(elementTextRange.endOffset))
+    val consumer = OrderedSet<SmartStepTarget>()
+    val visitor = SmartStepTargetVisitor(ktElement, lines, consumer)
+    ktElement.accept(visitor, null)
+    return consumer
 }
