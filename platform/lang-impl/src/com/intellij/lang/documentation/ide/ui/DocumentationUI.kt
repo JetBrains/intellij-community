93d231d5253111a9ca800504fb751b2540595384
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-12-07 00:10:29 +0100

    IDEA-277609 [documentation] support content update on link activation
    GitOrigin-RevId: 99c01db0b41166281685f6c0b5b8411fbb68c5e2

--- platform/lang-impl/src/com/intellij/lang/documentation/ide/ui/DocumentationUI.kt
+++ platform/lang-impl/src/com/intellij/lang/documentation/ide/ui/DocumentationUI.kt
@@ -180,7 +180,7 @@ internal class DocumentationUI(
     update(element.toString(), ScrollingPosition.Reset)
   }
 
-  private fun update(text: @Nls String, scrollingPosition: ScrollingPosition) {
+  fun update(text: @Nls String, scrollingPosition: ScrollingPosition) {
     EDT.assertIsEdt()
     editorPane.text = text
     when (scrollingPosition) {
