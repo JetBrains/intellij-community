544b32d5473d09ca19d7459a339b2b1d5a23ad34
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2021-12-07 19:35:06 +0300

    [vcs-log] convert SimpleRefGroup to kotlin
    GitOrigin-RevId: 2ce5321bde652ac9a3836f8b333bb7b7ed6d38f8

--- platform/vcs-log/impl/src/com/intellij/vcs/log/impl/SimpleRefGroup.kt
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/impl/SimpleRefGroup.kt
@@ -1,105 +1,66 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.vcs.log.impl;
+package com.intellij.vcs.log.impl
 
-import com.intellij.util.containers.ContainerUtil;
-import com.intellij.util.containers.MultiMap;
-import com.intellij.vcs.log.RefGroup;
-import com.intellij.vcs.log.VcsRef;
-import com.intellij.vcs.log.VcsRefType;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.util.containers.MultiMap
+import com.intellij.vcs.log.RefGroup
+import com.intellij.vcs.log.VcsRef
+import com.intellij.vcs.log.VcsRefType
+import org.jetbrains.annotations.Nls
+import java.awt.Color
 
-import java.awt.*;
-import java.util.List;
-import java.util.*;
+class SimpleRefGroup @JvmOverloads constructor(private val name: @Nls String,
+                                               private val refs: MutableList<VcsRef>,
+                                               private val isExpanded: Boolean = false) : RefGroup {
+  override fun isExpanded(): Boolean = isExpanded
+  override fun getName(): String = name
+  override fun getRefs(): MutableList<VcsRef> = refs
 
-public class SimpleRefGroup implements RefGroup {
-  @NotNull private final @Nls String myName;
-  @NotNull private final List<VcsRef> myRefs;
-  private final boolean myExpanded;
-
-  public SimpleRefGroup(@NotNull @Nls String name, @NotNull List<VcsRef> refs) {
-    this(name, refs, false);
+  override fun getColors(): List<Color> {
+    return getColors(refs)
   }
 
-  public SimpleRefGroup(@NotNull @Nls String name, @NotNull List<VcsRef> refs, boolean expanded) {
-    myName = name;
-    myRefs = refs;
-    myExpanded = expanded;
-  }
-
-  @Override
-  public boolean isExpanded() {
-    return myExpanded;
-  }
-
-  @NotNull
-  @Override
-  public String getName() {
-    return myName;
-  }
-
-  @NotNull
-  @Override
-  public List<VcsRef> getRefs() {
-    return myRefs;
-  }
-
-  @NotNull
-  @Override
-  public List<Color> getColors() {
-    return getColors(myRefs);
-  }
-
-  @NotNull
-  public static List<Color> getColors(@NotNull Collection<? extends VcsRef> refs) {
-    MultiMap<VcsRefType, VcsRef> referencesByType = ContainerUtil.groupBy(refs, VcsRef::getType);
-    if (referencesByType.size() == 1) {
-      Map.Entry<VcsRefType, Collection<VcsRef>> firstItem =
-        Objects.requireNonNull(ContainerUtil.getFirstItem(referencesByType.entrySet()));
-      boolean multiple = firstItem.getValue().size() > 1;
-      Color color = firstItem.getKey().getBackgroundColor();
-      return multiple ? Arrays.asList(color, color) : Collections.singletonList(color);
-    }
-    else {
-      List<Color> colorsList = new ArrayList<>();
-      for (VcsRefType type : referencesByType.keySet()) {
-        if (referencesByType.get(type).size() > 1) {
-          colorsList.add(type.getBackgroundColor());
+  companion object {
+    @JvmStatic
+    fun getColors(refs: Collection<VcsRef>): List<Color> {
+      val referencesByType = refs.groupBy(VcsRef::getType)
+      if (referencesByType.size == 1) {
+        val color = referencesByType.keys.single().backgroundColor
+        return if (refs.size > 1) listOf(color, color) else listOf(color)
+      }
+      val colorsList = ArrayList<Color>()
+      for (type in referencesByType.keys) {
+        if (referencesByType[type]!!.size > 1) {
+          colorsList.add(type.backgroundColor)
         }
-        colorsList.add(type.getBackgroundColor());
+        colorsList.add(type.backgroundColor)
       }
-      return colorsList;
+      return colorsList
     }
-  }
 
-  public static void buildGroups(@NotNull MultiMap<VcsRefType, VcsRef> groupedRefs,
-                                 boolean compact,
-                                 boolean showTagNames,
-                                 @NotNull List<RefGroup> result) {
-    if (groupedRefs.isEmpty()) return;
-
-    if (compact) {
-      VcsRef firstRef = Objects.requireNonNull(ContainerUtil.getFirstItem(groupedRefs.values()));
-      RefGroup group = ContainerUtil.getFirstItem(result);
-      if (group == null) {
-        result.add(new SimpleRefGroup(firstRef.getType().isBranch() || showTagNames ? firstRef.getName() : "",
-                                      new ArrayList<>(groupedRefs.values())));
-      }
-      else {
-        group.getRefs().addAll(groupedRefs.values());
-      }
-    }
-    else {
-      for (Map.Entry<VcsRefType, Collection<VcsRef>> entry : groupedRefs.entrySet()) {
-        if (entry.getKey().isBranch()) {
-          for (VcsRef ref : entry.getValue()) {
-            result.add(new SimpleRefGroup(ref.getName(), ContainerUtil.newArrayList(ref)));
-          }
+    @JvmStatic
+    fun buildGroups(groupedRefs: MultiMap<VcsRefType, VcsRef>,
+                    compact: Boolean,
+                    showTagNames: Boolean,
+                    result: MutableList<RefGroup>) {
+      if (groupedRefs.isEmpty) return
+      if (compact) {
+        if (result.isEmpty()) {
+          val firstRef = groupedRefs.values().first()
+          result.add(SimpleRefGroup(if (firstRef.type.isBranch || showTagNames) firstRef.name else "",
+                                    groupedRefs.values().toMutableList()))
         }
         else {
-          result.add(new SimpleRefGroup(showTagNames ? Objects.requireNonNull(ContainerUtil.getFirstItem(entry.getValue())).getName() : "",
-                                        new ArrayList<>(entry.getValue())));
+          result.first().refs.addAll(groupedRefs.values())
+        }
+      }
+      else {
+        for ((refType, refs) in groupedRefs.entrySet()) {
+          if (refType.isBranch) {
+            result.addAll(refs.map { ref -> SimpleRefGroup(ref.name, arrayListOf(ref)) })
+          }
+          else {
+            result.add(SimpleRefGroup(if (showTagNames) refs.first().name else "", refs.toMutableList()))
+          }
         }
       }
     }
