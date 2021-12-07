432410f45da4dc71ac5d3ececcd15002aa3f5198
Anton Tarasov <anton.tarasov@jetbrains.com>
2021-12-07 18:03:03 +0300

    IDEA-283733 [followup] Icons in main menu submenus are low-resolution
    cmake -G "Unix Makefiles" does not honor CMAKE_OSX_ARCHITECTURES and does not produce universal binary. Changed to "Xcode".
    
    (cherry picked from commit cc86b53eb342a60c55d4247485d53c1339f4a53d)
    
    GitOrigin-RevId: fff157b37cf804b8d6506ca6519bbad949bde15a

--- native/MacScreenMenu/make.sh
+++ native/MacScreenMenu/make.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
+MY_LIB_NAME=macscreenmenu64
+
 rm -rf build
 mkdir build
 cd build || exit 1
 
-cmake -DCMAKE_BUILD_TYPE=Release .. || exit 2
-
-make VERBOSE=1 || exit 3
+# -G "Unix Makefiles" does not build a universal binary
+cmake -DMY_LIB_NAME=${MY_LIB_NAME} -G "Xcode" .. || exit 2
 
 if [ "$1" = "install" ]; then
-  make install/strip VERBOSE=1 || exit 4
+  MY_TARGET="install"
+  MY_STRIP="YES"
+else
+  MY_TARGET=${MY_LIB_NAME}
+  MY_STRIP="NO"
 fi
+
+STRIP_INSTALLED_PRODUCT = YES
+STRIP_STYLE = non-global
+
+xcodebuild -target ${MY_TARGET} -configuration Release STRIP_INSTALLED_PRODUCT=${MY_STRIP} STRIP_STYLE=non-global || exit 3
