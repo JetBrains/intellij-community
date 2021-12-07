6fe4154f18cfe7a05a6eb50f807cd0b9b74c5d99
Aleksandr Krasilnikov <aleksandr.krasilnikov@jetbrains.com>
2021-12-07 14:10:27 +0300

    FUS: Add the ability to log duration of stages in StructuredIdeActivity
    IDEA-283241
    
    (cherry picked from commit 5c5bd0a58fc063195141ec65abbf7acb495b177e)
    
    IJ-CR-18698
    
    (cherry picked from commit 0b6af9b431e1bdcd3120242eec4c4af7ce3e53ed)
    
    GitOrigin-RevId: 07e386b1ba2061595fa023b6d596602109ef0f7a

--- platform/statistics/src/com/intellij/internal/statistic/StructuredIdeActivity.kt
+++ platform/statistics/src/com/intellij/internal/statistic/StructuredIdeActivity.kt
@@ -9,8 +9,10 @@ import com.intellij.internal.statistic.eventLog.events.VarargEventId
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.util.TimeoutUtil
+import com.intellij.util.containers.SmartHashSet
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.concurrency.Promise
+import java.util.*
 import java.util.concurrent.atomic.AtomicInteger
 
 private val LOG = Logger.getInstance(StructuredIdeActivity::class.java)
@@ -29,16 +31,22 @@ private val LOG = Logger.getInstance(StructuredIdeActivity::class.java)
  */
 @ApiStatus.Internal
 class StructuredIdeActivity internal constructor(private val projectOrNullForApplication: Project?,
-                                                 private val ideActivityDefinition: IdeActivityDefinition) {
-  private val id = counter.incrementAndGet()
+                                                 private val ideActivityDefinition: IdeActivityDefinition,
+                                                 private val parentActivity: StructuredIdeActivity? = null) {
+  private val id: Int = parentActivity?.id ?: counter.incrementAndGet()
 
   private var state = IdeActivityState.NOT_STARTED
   private var startedTimestamp = 0L
 
+  private val innerActivities: MutableSet<StructuredIdeActivity> = Collections.synchronizedSet(SmartHashSet())
+
   @JvmOverloads
   fun started(dataSupplier: (() -> List<EventPair<*>>)? = null): StructuredIdeActivity {
+    if (parentNotStarted(parentActivity)) return this
     if (!LOG.assertTrue(state == IdeActivityState.NOT_STARTED, state.name)) return this
+
     state = IdeActivityState.STARTED
+    parentActivity?.addInnerActivity(this)
 
     val data: MutableList<EventPair<*>> = mutableListOf(IdeActivityDefinition.activityId.with(id))
     if (dataSupplier != null) {
@@ -51,8 +59,11 @@ class StructuredIdeActivity internal constructor(private val projectOrNullForApp
   }
 
   fun startedAsync(dataSupplier: () -> Promise<List<EventPair<*>>>): StructuredIdeActivity {
+    if (parentNotStarted(parentActivity)) return this
     if (!LOG.assertTrue(state == IdeActivityState.NOT_STARTED, state.name)) return this
+
     state = IdeActivityState.STARTED
+    parentActivity?.addInnerActivity(this)
     startedTimestamp = System.nanoTime()
 
     val data: MutableList<EventPair<*>> = mutableListOf(IdeActivityDefinition.activityId.with(id))
@@ -67,6 +78,7 @@ class StructuredIdeActivity internal constructor(private val projectOrNullForApp
 
   @JvmOverloads
   fun stageStarted(stage: VarargEventId, dataSupplier: (() -> List<EventPair<*>>)? = null): StructuredIdeActivity {
+    if (parentNotStarted(parentActivity)) return this
     if (!LOG.assertTrue(state == IdeActivityState.STARTED, state.name)) return this
 
     val data: MutableList<EventPair<*>> = mutableListOf(IdeActivityDefinition.activityId.with(id))
@@ -80,8 +92,12 @@ class StructuredIdeActivity internal constructor(private val projectOrNullForApp
 
   @JvmOverloads
   fun finished(dataSupplier: (() -> List<EventPair<*>>)? = null): StructuredIdeActivity {
+    if (parentNotStarted(parentActivity)) return this
     if (!LOG.assertTrue(state == IdeActivityState.STARTED, state.name)) return this
+
     state = IdeActivityState.FINISHED
+    parentActivity?.removeInnerActivity(this)
+    innerActivities.toList().forEach { it.finished() }
 
     val data: MutableList<EventPair<*>> = mutableListOf(IdeActivityDefinition.activityId.with(id))
     if (dataSupplier != null) {
@@ -93,13 +109,29 @@ class StructuredIdeActivity internal constructor(private val projectOrNullForApp
     return this
   }
 
+  private fun addInnerActivity(inner: StructuredIdeActivity) {
+    innerActivities.add(inner)
+  }
+
+  private fun removeInnerActivity(inner: StructuredIdeActivity) {
+    innerActivities.remove(inner)
+  }
+
+  private fun parentNotStarted(parentActivity: StructuredIdeActivity?): Boolean {
+    if (parentActivity != null) {
+      if (!LOG.assertTrue(parentActivity.state == IdeActivityState.STARTED, parentActivity.state.name)) return true
+    }
+    return false
+  }
+
   companion object {
     private val counter = AtomicInteger(0)
   }
 }
 
 class IdeActivityDefinition internal constructor(val group: EventLogGroup,
-                                                 val activityName: String?,
+                                                 private val parentActivityDefinition: IdeActivityDefinition? = null,
+                                                 internal var activityName: String?,
                                                  startEventAdditionalFields: Array<EventField<*>> = emptyArray(),
                                                  finishEventAdditionalFields: Array<EventField<*>> = emptyArray()) {
   val started = group.registerVarargEvent(appendActivityName(activityName, "started"), activityId, *startEventAdditionalFields)
@@ -108,16 +140,43 @@ class IdeActivityDefinition internal constructor(val group: EventLogGroup,
 
   @JvmOverloads
   fun started(project: Project?, dataSupplier: (() -> List<EventPair<*>>)? = null): StructuredIdeActivity {
+    logIfShouldBeStartedWithParent()
     return StructuredIdeActivity(project, this).started(dataSupplier)
   }
 
+  @JvmOverloads
+  fun startedWithParent(project: Project?,
+                        parentActivity: StructuredIdeActivity,
+                        dataSupplier: (() -> List<EventPair<*>>)? = null): StructuredIdeActivity {
+    logIfParentNotRegistered()
+    return StructuredIdeActivity(project, this, parentActivity).started(dataSupplier)
+  }
+
   fun startedAsync(project: Project?, dataSupplier: () -> Promise<List<EventPair<*>>>): StructuredIdeActivity {
+    logIfShouldBeStartedWithParent()
     return StructuredIdeActivity(project, this).startedAsync(dataSupplier)
+  }
+
+  @JvmOverloads
+  fun startedAsyncWithParent(project: Project?,
+                             parentActivity: StructuredIdeActivity,
+                             dataSupplier: (() -> List<EventPair<*>>)? = null): StructuredIdeActivity {
+    logIfParentNotRegistered()
+    return StructuredIdeActivity(project, this, parentActivity).started(dataSupplier)
+  }
+
+  private fun logIfShouldBeStartedWithParent() {
+    LOG.assertTrue(parentActivityDefinition == null, "Use startedWithParent/startedAsyncWithParent")
+  }
+
+  private fun logIfParentNotRegistered() {
+    LOG.assertTrue(parentActivityDefinition != null, "Use started/startedAsync or register parent activity")
   }
 
   private fun appendActivityName(activityName: String?, state: String): String {
     if (activityName == null) return state
-    return "$activityName.$state"
+    val name = if (parentActivityDefinition == null) activityName else "${parentActivityDefinition.activityName}.$activityName"
+    return "$name.$state"
   }
 
   @JvmOverloads
