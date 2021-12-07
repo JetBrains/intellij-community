de9da01569c4c97db6ceaabe07ab89e89741b64b
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2021-12-07 16:44:06 +0300

    allow to force m2 home without any check (needed for debug/tests)
    GitOrigin-RevId: ddff0820d2cfe49666ac88ffd46e58bea0c6b2c1

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/utils/MavenUtil.java
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/utils/MavenUtil.java
@@ -122,6 +122,7 @@ public class MavenUtil {
   public static final String LIB_DIR = "lib";
   public static final String CLIENT_ARTIFACT_SUFFIX = "-client";
   public static final String CLIENT_EXPLODED_ARTIFACT_SUFFIX = CLIENT_ARTIFACT_SUFFIX + " exploded";
+  private static final String PROP_FORCED_M2_HOME = "idea.force.m2.home";
 
 
   @SuppressWarnings("unchecked")
@@ -805,6 +806,10 @@ public class MavenUtil {
   public static File resolveLocalRepository(@Nullable String overriddenLocalRepository,
                                             @Nullable String overriddenMavenHome,
                                             @Nullable String overriddenUserSettingsFile) {
+    String forcedM2Home = System.getProperty(PROP_FORCED_M2_HOME);
+    if (forcedM2Home != null) {
+      return new File(forcedM2Home);
+    }
     File result = null;
     if (!isEmptyOrSpaces(overriddenLocalRepository)) result = new File(overriddenLocalRepository);
     if (result == null) {
