0194425a4de9e570b489decb2403d5a99b7f7640
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-12-08 14:03:56 +0700

    [kotlin] split KotlinIDERenderLogTest class into several to parallelize tests
    GitOrigin-RevId: a0505427c14c52f4394c6f8c04cba4af7a33bb8c

--- plugins/kotlin/uast/uast-kotlin/tests/test/KotlinIDERenderLogTest.kt
+++ plugins/kotlin/uast/uast-kotlin/tests/test/KotlinIDERenderLogTest.kt
@@ -6,7 +6,7 @@ import org.junit.Ignore
 import org.junit.Test
 import java.io.File
 
-class KotlinIDERenderLogTest : AbstractKotlinUastLightCodeInsightFixtureTest(), AbstractKotlinRenderLogTest {
+abstract class KotlinIDERenderLogTest : AbstractKotlinUastLightCodeInsightFixtureTest(), AbstractKotlinRenderLogTest {
 
     override fun getTestFile(testName: String, ext: String): File {
         if (ext.endsWith(".txt")) {
@@ -18,156 +18,251 @@ class KotlinIDERenderLogTest : AbstractKotlinUastLightCodeInsightFixtureTest(), 
 
     override fun check(testName: String, file: UFile) = super.check(testName, file)
 
-    @Test
-    fun testLocalDeclarations() = doTest("LocalDeclarations")
-
-    @Test
-    fun testSimple() = doTest("Simple")
-
-    @Test
-    fun testWhenIs() = doTest("WhenIs")
-
-    @Test
-    fun testDefaultImpls() = doTest("DefaultImpls")
-
-    @Test
-    fun testBitwise() = doTest("Bitwise")
-
-    @Test
-    fun testElvis() = doTest("Elvis")
-
-    @Test
-    fun testPropertyAccessors() = doTest("PropertyAccessors")
-
-    @Test
-    fun testPropertyInitializer() = doTest("PropertyInitializer")
-
-    @Test
-    fun testPropertyInitializerWithoutSetter() = doTest("PropertyInitializerWithoutSetter")
-
-    @Test
-    fun testAnnotationParameters() = doTest("AnnotationParameters")
-
-    @Test
-    fun testEnumValueMembers() = doTest("EnumValueMembers")
-
-    @Test
-    fun testEnumValuesConstructors() = doTest("EnumValuesConstructors")
-
-    @Test
-    fun testStringTemplate() = doTest("StringTemplate")
-
-    @Test
-    fun testStringTemplateComplex() = doTest("StringTemplateComplex")
-
-    @Test
-    fun testStringTemplateComplexForUInjectionHost() = withForceUInjectionHostValue {
-        doTest("StringTemplateComplexForUInjectionHost")
+    class LocalDeclarations : KotlinIDERenderLogTest() {
+        @Test
+        fun testLocalDeclarations() = doTest("LocalDeclarations")
     }
 
-    @Test
-    fun testQualifiedConstructorCall() = doTest("QualifiedConstructorCall")
-
-    @Test
-    fun testPropertyDelegate() = doTest("PropertyDelegate")
-
-    @Test
-    fun testLocalVariableWithAnnotation() = doTest("LocalVariableWithAnnotation")
-
-    @Test
-    fun testPropertyWithAnnotation() = doTest("PropertyWithAnnotation")
-
-    @Test
-    fun testIfStatement() = doTest("IfStatement")
-
-    @Test
-    fun testInnerClasses() = doTest("InnerClasses")
-
-    @Test
-    @Ignore // there is a descriptor leak probably, unignore when fixed
-    fun ingoretestSimpleScript() = doTest("SimpleScript") { testName, file -> check(testName, file, false) }
-
-    @Test
-    fun testDestructuringDeclaration() = doTest("DestructuringDeclaration")
-
-    @Test
-    fun testDefaultParameterValues() = doTest("DefaultParameterValues")
-
-    @Test
-    fun testParameterPropertyWithAnnotation() = doTest("ParameterPropertyWithAnnotation")
-
-    @Test
-    fun testParametersWithDefaultValues() = doTest("ParametersWithDefaultValues")
-
-    @Test
-    fun testUnexpectedContainer() = doTest("UnexpectedContainerException")
-
-    @Test
-    fun testWhenStringLiteral() = doTest("WhenStringLiteral")
-
-    @Test
-    fun testWhenAndDestructing() = doTest("WhenAndDestructing") { testName, file -> check(testName, file, false) }
-
-    @Test
-    fun testSuperCalls() = doTest("SuperCalls")
-
-    @Test
-    fun testConstructors() = doTest("Constructors")
-
-    @Test
-    fun testClassAnnotation() = doTest("ClassAnnotation")
-
-    @Test
-    fun testReceiverFun() = doTest("ReceiverFun")
-
-    @Test
-    fun testAnonymous() = doTest("Anonymous")
-
-    @Test
-    fun testAnnotationComplex() = doTest("AnnotationComplex")
-
-    @Test
-    fun testParametersDisorder() = doTest("ParametersDisorder") { testName, file ->
-        // disabled due to inconsistent parents for 2-receivers call (KT-22344)
-        check(testName, file, false)
+    class Simple : KotlinIDERenderLogTest() {
+        @Test
+        fun testSimple() = doTest("Simple")
     }
 
-    @Test
-    fun testLambdas() = doTest("Lambdas")
-
-    @Test
-    fun testTypeReferences() = doTest("TypeReferences")
-
-    @Test
-    fun testDelegate() = doTest("Delegate")
-
-    @Test
-    fun testConstructorDelegate() = doTest("ConstructorDelegate") { testName, file ->
-        // Igor Yakovlev told that delegation is a little bit broken in ULC and not expected to be fixed
-        check(testName, file, false)
+    class WhenIs : KotlinIDERenderLogTest() {
+        @Test
+        fun testWhenIs() = doTest("WhenIs")
     }
 
-    @Test
-    fun testLambdaReturn() = doTest("LambdaReturn")
+    class DefaultImpls : KotlinIDERenderLogTest() {
+        @Test
+        fun testDefaultImpls() = doTest("DefaultImpls")
+    }
 
-    @Test
-    fun testReified() = doTest("Reified")
+    class Bitwise : KotlinIDERenderLogTest() {
+        @Test
+        fun testBitwise() = doTest("Bitwise")
+    }
 
-    @Test
-    fun testReifiedReturnType() = doTest("ReifiedReturnType")
+    class Elvis : KotlinIDERenderLogTest() {
+        @Test
+        fun testElvis() = doTest("Elvis")
+    }
 
-    @Test
-    fun testReifiedParameters() = doTest("ReifiedParameters")
+    class PropertyAccessors : KotlinIDERenderLogTest() {
+        @Test
+        fun testPropertyAccessors() = doTest("PropertyAccessors")
+    }
 
-    @Test
-    fun testSuspend() = doTest("Suspend")
+    class PropertyInitializer : KotlinIDERenderLogTest() {
+        @Test
+        fun testPropertyInitializer() = doTest("PropertyInitializer")
+    }
 
-    @Test
-    fun testDeprecatedHidden() = doTest("DeprecatedHidden")
+    class PropertyInitializerWithoutSetter : KotlinIDERenderLogTest() {
+        @Test
+        fun testPropertyInitializerWithoutSetter() = doTest("PropertyInitializerWithoutSetter")
+    }
 
-    @Test
-    fun testTryCatch() = doTest("TryCatch")
+    class AnnotationParameters : KotlinIDERenderLogTest() {
+        @Test
+        fun testAnnotationParameters() = doTest("AnnotationParameters")
+    }
 
-    @Test
-    fun testComments() = doTest("Comments")
+    class EnumValueMembers : KotlinIDERenderLogTest() {
+        @Test
+        fun testEnumValueMembers() = doTest("EnumValueMembers")
+    }
+
+    class EnumValuesConstructors : KotlinIDERenderLogTest() {
+        @Test
+        fun testEnumValuesConstructors() = doTest("EnumValuesConstructors")
+    }
+
+    class StringTemplate : KotlinIDERenderLogTest() {
+        @Test
+        fun testStringTemplate() = doTest("StringTemplate")
+    }
+
+    class StringTemplateComplex : KotlinIDERenderLogTest() {
+        @Test
+        fun testStringTemplateComplex() = doTest("StringTemplateComplex")
+    }
+
+    class StringTemplateComplexForUInjectionHost : KotlinIDERenderLogTest() {
+        @Test
+        fun testStringTemplateComplexForUInjectionHost() = withForceUInjectionHostValue {
+            doTest("StringTemplateComplexForUInjectionHost")
+        }
+    }
+
+    class QualifiedConstructorCall : KotlinIDERenderLogTest() {
+        @Test
+        fun testQualifiedConstructorCall() = doTest("QualifiedConstructorCall")
+    }
+
+    class PropertyDelegate : KotlinIDERenderLogTest() {
+        @Test
+        fun testPropertyDelegate() = doTest("PropertyDelegate")
+    }
+
+    class LocalVariableWithAnnotation : KotlinIDERenderLogTest() {
+        @Test
+        fun testLocalVariableWithAnnotation() = doTest("LocalVariableWithAnnotation")
+    }
+
+    class PropertyWithAnnotation : KotlinIDERenderLogTest() {
+        @Test
+        fun testPropertyWithAnnotation() = doTest("PropertyWithAnnotation")
+    }
+
+    class IfStatement : KotlinIDERenderLogTest() {
+        @Test
+        fun testIfStatement() = doTest("IfStatement")
+    }
+
+    class InnerClasses : KotlinIDERenderLogTest() {
+        @Test
+        fun testInnerClasses() = doTest("InnerClasses")
+    }
+
+    //class SimpleScript : KotlinIDERenderLogTest() {
+    //    @Test
+    //    fun testSimpleScript() = doTest("SimpleScript") { testName, file -> check(testName, file, false) }
+    //}
+
+    class DestructuringDeclaration : KotlinIDERenderLogTest() {
+        @Test
+        fun testDestructuringDeclaration() = doTest("DestructuringDeclaration")
+    }
+
+    class DefaultParameterValues : KotlinIDERenderLogTest() {
+        @Test
+        fun testDefaultParameterValues() = doTest("DefaultParameterValues")
+    }
+
+    class ParameterPropertyWithAnnotation : KotlinIDERenderLogTest() {
+        @Test
+        fun testParameterPropertyWithAnnotation() = doTest("ParameterPropertyWithAnnotation")
+    }
+
+    class ParametersWithDefaultValues : KotlinIDERenderLogTest() {
+        @Test
+        fun testParametersWithDefaultValues() = doTest("ParametersWithDefaultValues")
+    }
+
+    class UnexpectedContainer : KotlinIDERenderLogTest() {
+        @Test
+        fun testUnexpectedContainer() = doTest("UnexpectedContainerException")
+    }
+
+    class WhenStringLiteral : KotlinIDERenderLogTest() {
+        @Test
+        fun testWhenStringLiteral() = doTest("WhenStringLiteral")
+    }
+
+    class WhenAndDestructing : KotlinIDERenderLogTest() {
+        @Test
+        fun testWhenAndDestructing() = doTest("WhenAndDestructing") { testName, file -> check(testName, file, false) }
+    }
+
+    class SuperCalls : KotlinIDERenderLogTest() {
+        @Test
+        fun testSuperCalls() = doTest("SuperCalls")
+    }
+
+    class Constructors : KotlinIDERenderLogTest() {
+        @Test
+        fun testConstructors() = doTest("Constructors")
+    }
+
+    class ClassAnnotation : KotlinIDERenderLogTest() {
+        @Test
+        fun testClassAnnotation() = doTest("ClassAnnotation")
+    }
+
+    class ReceiverFun : KotlinIDERenderLogTest() {
+        @Test
+        fun testReceiverFun() = doTest("ReceiverFun")
+    }
+
+    class Anonymous : KotlinIDERenderLogTest() {
+        @Test
+        fun testAnonymous() = doTest("Anonymous")
+    }
+
+    class AnnotationComplex : KotlinIDERenderLogTest() {
+        @Test
+        fun testAnnotationComplex() = doTest("AnnotationComplex")
+    }
+
+    class ParametersDisorder : KotlinIDERenderLogTest() {
+        @Test
+        fun testParametersDisorder() = doTest("ParametersDisorder") { testName, file ->
+            // disabled due to inconsistent parents for 2-receivers call (KT-22344)
+            check(testName, file, false)
+        }
+    }
+
+    class Lambdas : KotlinIDERenderLogTest() {
+        @Test
+        fun testLambdas() = doTest("Lambdas")
+    }
+
+    class TypeReferences : KotlinIDERenderLogTest() {
+        @Test
+        fun testTypeReferences() = doTest("TypeReferences")
+    }
+
+    class Delegate : KotlinIDERenderLogTest() {
+        @Test
+        fun testDelegate() = doTest("Delegate")
+    }
+
+    class ConstructorDelegate : KotlinIDERenderLogTest() {
+        @Test
+        fun testConstructorDelegate() = doTest("ConstructorDelegate") { testName, file ->
+            // Igor Yakovlev told that delegation is a little bit broken in ULC and not expected to be fixed
+            check(testName, file, false)
+        }
+    }
+
+    class LambdaReturn : KotlinIDERenderLogTest() {
+        @Test
+        fun testLambdaReturn() = doTest("LambdaReturn")
+    }
+
+    class Reified : KotlinIDERenderLogTest() {
+        @Test
+        fun testReified() = doTest("Reified")
+    }
+
+    class ReifiedReturnType : KotlinIDERenderLogTest() {
+        @Test
+        fun testReifiedReturnType() = doTest("ReifiedReturnType")
+    }
+
+    class ReifiedParameters : KotlinIDERenderLogTest() {
+        @Test
+        fun testReifiedParameters() = doTest("ReifiedParameters")
+    }
+
+    class Suspend : KotlinIDERenderLogTest() {
+        @Test
+        fun testSuspend() = doTest("Suspend")
+    }
+
+    class DeprecatedHidden : KotlinIDERenderLogTest() {
+        @Test
+        fun testDeprecatedHidden() = doTest("DeprecatedHidden")
+    }
+
+    class TryCatch : KotlinIDERenderLogTest() {
+        @Test
+        fun testTryCatch() = doTest("TryCatch")
+    }
+
+    class Comments : KotlinIDERenderLogTest() {
+        @Test
+        fun testComments() = doTest("Comments")
+    }
 }
