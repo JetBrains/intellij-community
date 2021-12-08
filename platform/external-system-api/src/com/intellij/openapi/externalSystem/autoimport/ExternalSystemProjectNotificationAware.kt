fd886d055479d1092bf0b1dea5c46c996a204649
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2021-12-08 20:47:55 +0300

    [auto-reload] cleanup ExternalSystemProjectNotificationAware
    removed redundant hideNotification function
    added documentation
    added topic instead direct updating editor floating toolbar
    
    GitOrigin-RevId: fe6766868c8f5a650de91b7fddc12f752cc2d945

--- platform/external-system-api/src/com/intellij/openapi/externalSystem/autoimport/ExternalSystemProjectNotificationAware.kt
+++ platform/external-system-api/src/com/intellij/openapi/externalSystem/autoimport/ExternalSystemProjectNotificationAware.kt
@@ -3,19 +3,57 @@ package com.intellij.openapi.externalSystem.autoimport
 
 import com.intellij.openapi.externalSystem.model.ProjectSystemId
 import com.intellij.openapi.project.Project
-import org.jetbrains.annotations.ApiStatus
+import com.intellij.util.messages.Topic
 
-@ApiStatus.Experimental
+/**
+ * Bridge between auto-reload backend and notification view about that project is needed to reload.
+ * Notifications can be shown in editor floating toolbar, editor banner, etc.
+ */
 interface ExternalSystemProjectNotificationAware {
+
+  /**
+   * Requests to show notifications for reload project that defined by [projectAware]
+   */
   fun notificationNotify(projectAware: ExternalSystemProjectAware)
+
+  /**
+   * Requests to hide all notifications for all projects.
+   */
   fun notificationExpire()
+
+  /**
+   * Requests to hide all notifications for project that defined by [projectId]
+   * @see ExternalSystemProjectAware.projectId
+   */
   fun notificationExpire(projectId: ExternalSystemProjectId)
-  fun hideNotification()
+
+  /**
+   * Checks that notifications should be shown.
+   */
   fun isNotificationVisible(): Boolean
+
+  /**
+   * Gets list of project ids which should be reloaded.
+   */
   fun getSystemIds(): Set<ProjectSystemId>
 
+  interface Listener {
+
+    /**
+     * Happens when notification should be shown or hidden.
+     */
+    @JvmDefault
+    fun onNotificationChanged(project: Project) {
+    }
+  }
+
   companion object {
+    @JvmField
+    @Topic.AppLevel
+    val TOPIC = Topic.create("ExternalSystemProjectNotificationAware", Listener::class.java)
+
     @JvmStatic
-    fun getInstance(project: Project): ExternalSystemProjectNotificationAware = project.getService(ExternalSystemProjectNotificationAware::class.java)
+    fun getInstance(project: Project): ExternalSystemProjectNotificationAware =
+      project.getService(ExternalSystemProjectNotificationAware::class.java)
   }
 }
