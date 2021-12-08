03709e0c0555c297f2be86094b8f078d712c2e2e
Mikhail Mazurkevich <mikhail.mazurkevich@jetbrains.com>
2021-12-08 22:33:31 +0300

    [intellij sh] IDEA-283506 Properly handle setting env variables for scripts at run configuration
    GitOrigin-RevId: e8f86160b04612c49e165d8c2a0f52414f783f25

--- plugins/sh/src/com/intellij/sh/run/ShRunConfigurationProfileState.java
+++ plugins/sh/src/com/intellij/sh/run/ShRunConfigurationProfileState.java
@@ -209,9 +209,12 @@ final class ShRunConfigurationProfileState implements RunProfileState {
         String escapedValue = StringUtil.escapeQuotes(value);
         quotedString = StringUtil.containsWhitespaces(value) ? StringUtil.QUOTER.apply(escapedValue) : escapedValue;
       }
-      if (endWithSemicolon && index == envs.size() - 1) {
-        commandLine.add(key + "=" + quotedString + ";");
-      } else {
+      if (endWithSemicolon) {
+        String semicolon = "";
+        if (index == envs.size() - 1) semicolon = ";";
+        commandLine.add("export " + key + "=" + quotedString + semicolon);
+      }
+      else {
         commandLine.add(key + "=" + quotedString);
       }
       index++;
