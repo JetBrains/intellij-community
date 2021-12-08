84cdb659fbcec5624095c7d07f9e6bb9aae68778
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2021-12-08 19:56:20 +0100

    [kotlin:testdata] Use KotlinRoot.DIR for tests path location
    Tests are still ignored, but at least it works for any run configuration
    
    GitOrigin-RevId: 26bb204a79349cc78996312ad72d843b2d5ce811

--- plugins/kotlin/uast/uast-kotlin/tests/test/org/jetbrains/uast/test/kotlin/comparison/AbstractFE1LegacyUastIdentifiersTest.kt
+++ plugins/kotlin/uast/uast-kotlin/tests/test/org/jetbrains/uast/test/kotlin/comparison/AbstractFE1LegacyUastIdentifiersTest.kt
@@ -2,19 +2,18 @@
 
 package org.jetbrains.uast.test.kotlin.comparison
 
+import org.jetbrains.kotlin.test.KotlinRoot
 import org.jetbrains.uast.test.common.kotlin.LegacyUastIdentifiersTestBase
-import java.nio.file.Paths
 import kotlin.io.path.ExperimentalPathApi
-import kotlin.io.path.absolute
 
 abstract class AbstractFE1LegacyUastIdentifiersTest : AbstractFE1UastIdentifiersTest(), LegacyUastIdentifiersTestBase {
     // TODO: better not to see exceptions from FE1.0 UAST
     @OptIn(ExperimentalPathApi::class)
     private val whitelist : Set<String> = setOf(
-        "uast-kotlin/tests/testData/DestructuringDeclaration.kt",
-        "uast-kotlin/tests/testData/LambdaReturn.kt",
-        "uast-kotlin/tests/testData/WhenAndDestructing.kt",
-    ).mapTo(mutableSetOf()) { Paths.get("..").resolve(it).absolute().normalize().toString() }
+        "uast/uast-kotlin/tests/testData/DestructuringDeclaration.kt",
+        "uast/uast-kotlin/tests/testData/LambdaReturn.kt",
+        "uast/uast-kotlin/tests/testData/WhenAndDestructing.kt",
+    ).mapTo(mutableSetOf()) { KotlinRoot.DIR.resolve(it).absolutePath }
 
     override fun isExpectedToFail(filePath: String): Boolean {
         return filePath in whitelist
