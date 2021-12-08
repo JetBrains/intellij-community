dc1232ac2dab0e4e623bac7b822626a7aae4a4df
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2021-12-08 11:10:38 +0300

    IDEA-283733 [build scripts] fix scripts for MacScreenMenu build, ensure modern CMake is used for that
    (cherry picked from commit e19c6013a4c5945788192a679815058188a4fbb4)
    
    GitOrigin-RevId: ddef13534f6e3e6faee605fa63777fe679a229b2

--- native/MacScreenMenu/make.sh
+++ native/MacScreenMenu/make.sh
@@ -1,23 +1,13 @@
 #!/bin/sh
 
-MY_LIB_NAME=macscreenmenu64
-
 rm -rf build
 mkdir build
 cd build || exit 1
 
-# -G "Unix Makefiles" does not build a universal binary
-cmake -DMY_LIB_NAME=${MY_LIB_NAME} -G "Xcode" .. || exit 2
+cmake -DCMAKE_BUILD_TYPE=Release .. || exit 2
+
+make VERBOSE=1 || exit 3
 
 if [ "$1" = "install" ]; then
-  MY_TARGET="install"
-  MY_STRIP="YES"
-else
-  MY_TARGET=${MY_LIB_NAME}
-  MY_STRIP="NO"
+  make install/strip VERBOSE=1 || exit 4
 fi
-
-STRIP_INSTALLED_PRODUCT = YES
-STRIP_STYLE = non-global
-
-xcodebuild -target ${MY_TARGET} -configuration Release STRIP_INSTALLED_PRODUCT=${MY_STRIP} STRIP_STYLE=non-global || exit 3
