c26344a9c1f4a4a4a26b6cb089c692fdde1fa20a
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-12-08 13:35:25 +0700

    [kotlin] split SimpleKotlinRenderLogTest class into several to parallelize tests
    GitOrigin-RevId: 9077d9ef42cbad0056a980519568c51f237284f2

--- plugins/kotlin/uast/uast-kotlin/tests/test/SimpleKotlinRenderLogTest.kt
+++ plugins/kotlin/uast/uast-kotlin/tests/test/SimpleKotlinRenderLogTest.kt
@@ -5,161 +5,273 @@ import org.jetbrains.uast.UFile
 import org.jetbrains.uast.kotlin.KotlinConverter
 import org.junit.Test
 
-class SimpleKotlinRenderLogTest : AbstractKotlinUastTest(), AbstractKotlinRenderLogTest {
-
+abstract class SimpleKotlinRenderLogTest : AbstractKotlinUastTest(), AbstractKotlinRenderLogTest {
     override fun check(testName: String, file: UFile) = super.check(testName, file)
 
-    @Test
-    fun testLocalDeclarations() = doTest("LocalDeclarations")
-
-    @Test
-    fun testSimple() = doTest("Simple")
-
-    @Test
-    fun testWhenIs() = doTest("WhenIs")
-
-    @Test
-    fun testDefaultImpls() = doTest("DefaultImpls")
-
-    @Test
-    fun testBitwise() = doTest("Bitwise")
-
-    @Test
-    fun testElvis() = doTest("Elvis")
-
-    @Test
-    fun testPropertyAccessors() = doTest("PropertyAccessors")
-
-    @Test
-    fun testPropertyInitializer() = doTest("PropertyInitializer")
-
-    @Test
-    fun testPropertyInitializerWithoutSetter() = doTest("PropertyInitializerWithoutSetter")
-
-    @Test
-    fun testAnnotationParameters() = doTest("AnnotationParameters")
-
-    @Test
-    fun testEnumValueMembers() = doTest("EnumValueMembers")
-
-    @Test
-    fun testEnumValuesConstructors() = doTest("EnumValuesConstructors")
-
-    @Test
-    fun testStringTemplate() = doTest("StringTemplate")
-
-    @Test
-    fun testStringTemplateComplex() = doTest("StringTemplateComplex")
-
-    @Test
-    fun testStringTemplateComplexForUInjectionHost() = withForceUInjectionHostValue {
-        doTest("StringTemplateComplexForUInjectionHost")
+    class LocalDeclarations : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testLocalDeclarations() = doTest("LocalDeclarations")
     }
 
-    @Test
-    fun testQualifiedConstructorCall() = doTest("QualifiedConstructorCall")
-
-    @Test
-    fun testPropertyDelegate() = doTest("PropertyDelegate")
-
-    @Test fun testLocalVariableWithAnnotation() = doTest("LocalVariableWithAnnotation")
-
-    @Test fun testPropertyWithAnnotation() = doTest("PropertyWithAnnotation")
-
-    @Test fun testIfStatement() = doTest("IfStatement")
-
-    @Test fun testInnerClasses() = doTest("InnerClasses")
-
-    @Test fun testSimpleScript() = doTest("SimpleScript") { testName, file -> check(testName, file, false) }
-
-    @Test fun testDestructuringDeclaration() = doTest("DestructuringDeclaration")
-
-    @Test fun testDefaultParameterValues() = doTest("DefaultParameterValues")
-
-    @Test fun testParameterPropertyWithAnnotation() = doTest("ParameterPropertyWithAnnotation")
-
-    @Test fun testParametersWithDefaultValues() = doTest("ParametersWithDefaultValues")
-
-    @Test
-    fun testUnexpectedContainer() = doTest("UnexpectedContainerException")
-
-    @Test
-    fun testWhenStringLiteral() = doTest("WhenStringLiteral")
-
-    @Test
-    fun testWhenAndDestructing() = doTest("WhenAndDestructing") { testName, file -> check(testName, file, false) }
-
-    @Test
-    fun testSuperCalls() = doTest("SuperCalls")
-
-    @Test
-    fun testConstructors() = doTest("Constructors")
-
-    @Test
-    fun testClassAnnotation() = doTest("ClassAnnotation")
-
-    @Test
-    fun testReceiverFun() = doTest("ReceiverFun")
-
-    @Test
-    fun testAnonymous() = doTest("Anonymous")
-
-    @Test
-    fun testAnnotationComplex() = doTest("AnnotationComplex")
-
-    @Test
-    fun testParametersDisorder() = doTest("ParametersDisorder") { testName, file ->
-        // disabled due to inconsistent parents for 2-receivers call (KT-22344)
-        check(testName, file, false)
+    class Simple : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testSimple() = doTest("Simple")
     }
 
-    @Test
-    fun testLambdas() = doTest("Lambdas")
+    class WhenIs : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testWhenIs() = doTest("WhenIs")
+    }
 
-    @Test
-    fun testTypeReferences() = doTest("TypeReferences")
+    class DefaultImpls : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testDefaultImpls() = doTest("DefaultImpls")
+    }
 
-    @Test
-    fun testDelegate() = doTest("Delegate")
+    class Bitwise : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testBitwise() = doTest("Bitwise")
+    }
 
-    @Test
-    fun testConstructorDelegate() = doTest("ConstructorDelegate")
+    class Elvis : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testElvis() = doTest("Elvis")
+    }
 
-    @Test
-    fun testLambdaReturn() = doTest("LambdaReturn")
+    class PropertyAccessors : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testPropertyAccessors() = doTest("PropertyAccessors")
+    }
 
-    @Test
-    fun testReified() = doTest("Reified")
+    class PropertyInitializer : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testPropertyInitializer() = doTest("PropertyInitializer")
+    }
 
-    @Test
-    fun testReifiedReturnType() = doTest("ReifiedReturnType")
+    class PropertyInitializerWithoutSetter : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testPropertyInitializerWithoutSetter() = doTest("PropertyInitializerWithoutSetter")
+    }
 
-    @Test
-    fun testReifiedParameters() = doTest("ReifiedParameters")
+    class AnnotationParameters : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testAnnotationParameters() = doTest("AnnotationParameters")
+    }
 
-    @Test
-    fun testSuspend() = doTest("Suspend")
+    class EnumValueMembers : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testEnumValueMembers() = doTest("EnumValueMembers")
+    }
 
-    @Test
-    fun testDeprecatedHidden() = doTest("DeprecatedHidden")
+    class EnumValuesConstructors : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testEnumValuesConstructors() = doTest("EnumValuesConstructors")
+    }
 
-    @Test
-    fun testTryCatch() = doTest("TryCatch")
+    class StringTemplate : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testStringTemplate() = doTest("StringTemplate")
+    }
 
-    @Test
-    fun testAnnotatedExpressions() = doTest("AnnotatedExpressions")
+    class StringTemplateComplex : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testStringTemplateComplex() = doTest("StringTemplateComplex")
+    }
 
-    @Test
-    fun testNonTrivialIdentifiers() = doTest("NonTrivialIdentifiers")
+    class StringTemplateComplexForUInjectionHost : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testStringTemplateComplexForUInjectionHost() = withForceUInjectionHostValue {
+            doTest("StringTemplateComplexForUInjectionHost")
+        }
+    }
 
-    @Test
-    fun testTypeAliasExpansionWithOtherAliasInArgument() = doTest("TypeAliasExpansionWithOtherAliasInArgument")
+    class QualifiedConstructorCall : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testQualifiedConstructorCall() = doTest("QualifiedConstructorCall")
+    }
 
-    @Test
-    fun testComments() = doTest("Comments")
+    class PropertyDelegate : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testPropertyDelegate() = doTest("PropertyDelegate")
+    }
 
-    @Test
-    fun testBrokenDataClass() = doTest("BrokenDataClass")
+    class LocalVariableWithAnnotation : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testLocalVariableWithAnnotation() = doTest("LocalVariableWithAnnotation")
+    }
+
+    class PropertyWithAnnotation : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testPropertyWithAnnotation() = doTest("PropertyWithAnnotation")
+    }
+
+    class IfStatement : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testIfStatement() = doTest("IfStatement")
+    }
+
+    class InnerClasses : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testInnerClasses() = doTest("InnerClasses")
+    }
+
+    class SimpleScript : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testSimpleScript() = doTest("SimpleScript") { testName, file -> check(testName, file, false) }
+    }
+
+    class DestructuringDeclaration : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testDestructuringDeclaration() = doTest("DestructuringDeclaration")
+    }
+
+    class DefaultParameterValues : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testDefaultParameterValues() = doTest("DefaultParameterValues")
+    }
+
+    class ParameterPropertyWithAnnotation : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testParameterPropertyWithAnnotation() = doTest("ParameterPropertyWithAnnotation")
+    }
+
+    class ParametersWithDefaultValues : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testParametersWithDefaultValues() = doTest("ParametersWithDefaultValues")
+    }
+
+    class UnexpectedContainer : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testUnexpectedContainer() = doTest("UnexpectedContainerException")
+    }
+
+    class WhenStringLiteral : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testWhenStringLiteral() = doTest("WhenStringLiteral")
+    }
+
+    class WhenAndDestructing : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testWhenAndDestructing() = doTest("WhenAndDestructing") { testName, file -> check(testName, file, false) }
+    }
+
+    class SuperCalls : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testSuperCalls() = doTest("SuperCalls")
+    }
+
+    class Constructors : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testConstructors() = doTest("Constructors")
+    }
+
+    class ClassAnnotation : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testClassAnnotation() = doTest("ClassAnnotation")
+    }
+
+    class ReceiverFun : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testReceiverFun() = doTest("ReceiverFun")
+    }
+
+    class Anonymous : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testAnonymous() = doTest("Anonymous")
+    }
+
+    class AnnotationComplex : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testAnnotationComplex() = doTest("AnnotationComplex")
+    }
+
+    class ParametersDisorder : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testParametersDisorder() = doTest("ParametersDisorder") { testName, file ->
+            // disabled due to inconsistent parents for 2-receivers call (KT-22344)
+            check(testName, file, false)
+        }
+    }
+
+    class Lambdas : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testLambdas() = doTest("Lambdas")
+    }
+
+    class TypeReferences : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testTypeReferences() = doTest("TypeReferences")
+    }
+
+    class Delegate : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testDelegate() = doTest("Delegate")
+    }
+
+    class ConstructorDelegate : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testConstructorDelegate() = doTest("ConstructorDelegate")
+    }
+
+    class LambdaReturn : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testLambdaReturn() = doTest("LambdaReturn")
+    }
+
+    class Reified : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testReified() = doTest("Reified")
+    }
+
+    class ReifiedReturnType : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testReifiedReturnType() = doTest("ReifiedReturnType")
+    }
+
+    class ReifiedParameters : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testReifiedParameters() = doTest("ReifiedParameters")
+    }
+
+    class Suspend : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testSuspend() = doTest("Suspend")
+    }
+
+    class DeprecatedHidden : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testDeprecatedHidden() = doTest("DeprecatedHidden")
+    }
+
+    class TryCatch : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testTryCatch() = doTest("TryCatch")
+    }
+
+    class AnnotatedExpressions : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testAnnotatedExpressions() = doTest("AnnotatedExpressions")
+    }
+
+    class NonTrivialIdentifiers : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testNonTrivialIdentifiers() = doTest("NonTrivialIdentifiers")
+    }
+
+    class TypeAliasExpansionWithOtherAliasInArgument : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testTypeAliasExpansionWithOtherAliasInArgument() = doTest("TypeAliasExpansionWithOtherAliasInArgument")
+    }
+
+    class Comments : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testComments() = doTest("Comments")
+    }
+
+    class BrokenDataClass : SimpleKotlinRenderLogTest() {
+        @Test
+        fun testBrokenDataClass() = doTest("BrokenDataClass")
+    }
 }
 
 fun withForceUInjectionHostValue(call: () -> Unit) {
