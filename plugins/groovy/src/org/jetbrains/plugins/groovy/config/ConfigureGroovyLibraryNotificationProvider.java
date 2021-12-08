bc8bdb3dd951046b546bae9a12f44a8ebe25b6ab
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2021-12-08 22:38:02 +0300

    [groovy] Prevent SlowOperations assertion in editor notification
    GitOrigin-RevId: f9207864dde6f8e692762696ee81219bc6fc2692

--- plugins/groovy/src/org/jetbrains/plugins/groovy/config/ConfigureGroovyLibraryNotificationProvider.java
+++ plugins/groovy/src/org/jetbrains/plugins/groovy/config/ConfigureGroovyLibraryNotificationProvider.java
@@ -1,8 +1,7 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.groovy.config;
 
 import com.intellij.openapi.compiler.CompilerManager;
-import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtilCore;
 import com.intellij.openapi.progress.ProcessCanceledException;
@@ -13,7 +12,7 @@ import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.EditorNotificationPanel;
-import com.intellij.ui.EditorNotifications;
+import com.intellij.ui.EditorNotificationProvider;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.groovy.GroovyFileType;
 import org.jetbrains.plugins.groovy.annotator.GroovyFrameworkConfigNotification;
@@ -21,7 +20,7 @@ import org.jetbrains.plugins.groovy.annotator.GroovyFrameworkConfigNotification;
 /**
  * @author Maxim.Medvedev
  */
-public final class ConfigureGroovyLibraryNotificationProvider extends EditorNotifications.Provider<EditorNotificationPanel> {
+public final class ConfigureGroovyLibraryNotificationProvider implements EditorNotificationProvider<EditorNotificationPanel> {
   private static final Key<EditorNotificationPanel> KEY = Key.create("configure.groovy.library");
 
   @NotNull
@@ -31,27 +30,26 @@ public final class ConfigureGroovyLibraryNotificationProvider extends EditorNoti
   }
 
   @Override
-  public EditorNotificationPanel createNotificationPanel(@NotNull VirtualFile file,
-                                                         @NotNull FileEditor fileEditor,
-                                                         @NotNull Project project) {
+  public @NotNull ComponentProvider<EditorNotificationPanel> collectNotificationData(@NotNull Project project,
+                                                                                     @NotNull VirtualFile file) {
     try {
-      if (!file.getFileType().equals(GroovyFileType.GROOVY_FILE_TYPE)) return null;
+      if (!file.getFileType().equals(GroovyFileType.GROOVY_FILE_TYPE)) return ComponentProvider.getDummy();
       // do not show the panel for Gradle build scripts
       // expecting groovy library to always be available at the gradle distribution
-      if (StringUtil.endsWith(file.getName(), ".gradle")) return null;
-      if (CompilerManager.getInstance(project).isExcludedFromCompilation(file)) return null;
+      if (StringUtil.endsWith(file.getName(), ".gradle")) return ComponentProvider.getDummy();
+      if (CompilerManager.getInstance(project).isExcludedFromCompilation(file)) return ComponentProvider.getDummy();
 
       final Module module = ModuleUtilCore.findModuleForFile(file, project);
-      if (module == null) return null;
+      if (module == null) return ComponentProvider.getDummy();
 
-      if (isMavenModule(module)) return null;
+      if (isMavenModule(module)) return ComponentProvider.getDummy();
 
       for (GroovyFrameworkConfigNotification configNotification : GroovyFrameworkConfigNotification.EP_NAME.getExtensions()) {
         if (configNotification.hasFrameworkStructure(module)) {
           if (!configNotification.hasFrameworkLibrary(module)) {
-            return (EditorNotificationPanel)configNotification.createConfigureNotificationPanel(module, fileEditor);
+            return fileEditor -> (EditorNotificationPanel)configNotification.createConfigureNotificationPanel(module, fileEditor);
           }
-          return null;
+          return ComponentProvider.getDummy();
         }
       }
     }
@@ -59,7 +57,7 @@ public final class ConfigureGroovyLibraryNotificationProvider extends EditorNoti
 
     }
 
-    return null;
+    return ComponentProvider.getDummy();
   }
 
   private static boolean isMavenModule(@NotNull Module module) {
