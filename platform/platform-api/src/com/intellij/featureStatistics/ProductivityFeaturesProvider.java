0f156b27f439d4a1f9cc02fa87c9df0bc1712f41
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2021-12-09 07:46:38 +0100

    use getResourceAsBytes API
    GitOrigin-RevId: 0603d6b86a3cb2fce087837a001aab0f0fc6dc37

--- platform/platform-api/src/com/intellij/featureStatistics/ProductivityFeaturesProvider.java
+++ platform/platform-api/src/com/intellij/featureStatistics/ProductivityFeaturesProvider.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.featureStatistics;
 
 import com.intellij.openapi.extensions.ExtensionPointName;
@@ -22,7 +8,7 @@ import java.util.Collection;
 import java.util.Collections;
 
 public abstract class ProductivityFeaturesProvider {
-  public static final ExtensionPointName<ProductivityFeaturesProvider> EP_NAME = ExtensionPointName.create("com.intellij.productivityFeaturesProvider");
+  public static final ExtensionPointName<ProductivityFeaturesProvider> EP_NAME = new ExtensionPointName<>("com.intellij.productivityFeaturesProvider");
   
   public abstract FeatureDescriptor[] getFeatureDescriptors();
 
@@ -33,8 +19,7 @@ public abstract class ProductivityFeaturesProvider {
   /**
    * @return list of xml file urls with features configurations
    */
-  @NotNull
-  public Collection<String> getXmlFilesUrls() {
+  public @NotNull Collection<String> getXmlFilesUrls() {
     return Collections.emptyList();
   }
 }
