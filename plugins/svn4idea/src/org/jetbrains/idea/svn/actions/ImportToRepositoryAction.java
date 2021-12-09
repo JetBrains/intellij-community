cdc4f1ffc1d1cefd775c9cde96e62a3ca2618117
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2021-12-09 13:12:40 +0300

    vcs: hide git/hg/svn init actions in the safe mode
    GitOrigin-RevId: 8ac1e322d7e35e81882590e50a5716b7fb2a44af

--- plugins/svn4idea/src/org/jetbrains/idea/svn/actions/ImportToRepositoryAction.java
+++ plugins/svn4idea/src/org/jetbrains/idea/svn/actions/ImportToRepositoryAction.java
@@ -1,10 +1,10 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.svn.actions;
 
+import com.intellij.ide.impl.TrustedProjects;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
@@ -12,16 +12,14 @@ import com.intellij.openapi.vcs.ProjectLevelVcsManager;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.idea.svn.dialogs.ImportDialog;
 
-/**
- * @author alex
- */
 public class ImportToRepositoryAction extends AnAction implements DumbAware {
   @Override
-  public void update(@NotNull final AnActionEvent e) {
-    final Project project = e.getData(CommonDataKeys.PROJECT);
-    final Presentation presentation = e.getPresentation();
-    presentation.setEnabled(presentation.isEnabled() &&
-      (project == null || (! ProjectLevelVcsManager.getInstance(project).isBackgroundVcsOperationRunning())));
+  public void update(@NotNull AnActionEvent e) {
+    Project project = e.getData(CommonDataKeys.PROJECT);
+    e.getPresentation().setEnabled(
+      project == null ||
+      TrustedProjects.isTrusted(project) && !ProjectLevelVcsManager.getInstance(project).isBackgroundVcsOperationRunning()
+    );
   }
 
   @Override
