604c499299ebf9fb18531545a3eb1edab135ae3b
Viktor.Noskin <viktor.noskin@jetbrains.com>
2021-12-10 18:22:24 +0300

    [gradle] use EventDispatcher for ExternalAnnotationsProgressNotificationManagerImpl and refactor api
    GitOrigin-RevId: 61a7c43e9e66f549ef925a97c87296e2e4d7b069

--- plugins/gradle/src/org/jetbrains/plugins/gradle/service/notification/ExternalAnnotationsProgressNotificationManagerImpl.kt
+++ plugins/gradle/src/org/jetbrains/plugins/gradle/service/notification/ExternalAnnotationsProgressNotificationManagerImpl.kt
@@ -1,25 +1,43 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.gradle.service.notification
 
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.util.EventDispatcher
 
 class ExternalAnnotationsProgressNotificationManagerImpl : ExternalAnnotationsProgressNotificationManager {
-  private val listeners: MutableList<ExternalAnnotationsProgressNotificationListener> = mutableListOf()
-  override fun addNotificationListener(listener: ExternalAnnotationsProgressNotificationListener) {
-    listeners += listener
+  private val dispatcher = EventDispatcher.create(ExternalAnnotationsProgressNotificationListener::class.java)
+
+  override fun addNotificationListener(listener: ExternalAnnotationsProgressNotificationListener): Boolean {
+    return addListener(listener)
+  }
+
+  override fun addNotificationListener(listener: ExternalAnnotationsProgressNotificationListener, parentDisposable: Disposable): Boolean {
+    return addNotificationListener(listener, parentDisposable)
   }
 
   override fun removeNotificationListener(listener: ExternalAnnotationsProgressNotificationListener) {
-    listeners -= listener
+    dispatcher.listeners.remove(listener)
   }
 
   override fun onStartResolve(id: ExternalAnnotationsTaskId) {
-    listeners.forEach { it.onStartResolve(id) }
+    dispatcher.listeners.forEach { it.onStartResolve(id) }
   }
 
-  override fun onCancelResolve(id: ExternalAnnotationsTaskId) {
-    listeners.forEach { it.onCancelResolve(id) }
+  override fun onFinishResolve(id: ExternalAnnotationsTaskId) {
+    dispatcher.listeners.forEach { it.onFinishResolve(id) }
   }
+
+  private fun addListener(listener: ExternalAnnotationsProgressNotificationListener, parentDisposable: Disposable? = null): Boolean {
+    if(dispatcher.listeners.contains(listener)) return false
+    if(parentDisposable == null) {
+      dispatcher.addListener(listener)
+    } else {
+      dispatcher.addListener(listener, parentDisposable)
+    }
+    return true
+  }
+
   companion object {
     fun getInstanceImpl(): ExternalAnnotationsProgressNotificationManagerImpl {
       return ApplicationManager.getApplication()
