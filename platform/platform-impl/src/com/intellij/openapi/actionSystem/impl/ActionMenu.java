36d3ee7ece4feaf41415df5673ec58f0fcea487e
Artem Bochkarev <artem.bochkarev@jetbrains.com>
2021-12-10 16:39:06 +0700

    IDEA-284421: set icon for submenu
    fixed IDEA-284421 No icons in menu for items with submenu
    
    GitOrigin-RevId: abbe164ee219ff0723ae2065010922e3a6edbfc3

--- platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/ActionMenu.java
+++ platform/platform-impl/src/com/intellij/openapi/actionSystem/impl/ActionMenu.java
@@ -201,6 +201,7 @@ public final class ActionMenu extends JBMenu {
         else {
           setDisabledIcon(icon == null ? null : IconLoader.getDisabledIcon(icon));
         }
+        if (myScreenMenuPeer != null) myScreenMenuPeer.setIcon(icon);
       }
     }
   }
