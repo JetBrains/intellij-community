06462ed71093781c31a1d26cdebacb7edf50c0f9
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2021-12-11 04:24:06 +0300

    PY-51679: support several connections to wslproxy.
    Ingress port (127.0.0.1:some-port) or WSL must accept several connections.
    All of them are forwarded to application port.
    
    Django debug doesn't work without it because it uses several processes, each connected to port on WSL.
    
    See wslproxy.svg and wslproxy.c doc
    
    GitOrigin-RevId: 4623aef93409c1ea8f282908ae0ba8a810652413

--- native/WslTools/README.txt
+++ native/WslTools/README.txt
@@ -1,9 +1,10 @@
 This project creates two tools:
 
-wslproxy
+wslproxy (see wslproxy.svg)
 To avoid connecting from WSL to Windows (such connections may be blocked by firewall) we connect from Windows to WSL instead.
-This proxy accepts two clients: one for external (eth0) and one for local (loopback). It then passes data between them with two threads.
-Client disconnection, signal or any byte written to the stdin kills process.
+This proxy accepts two clients: one for egress (eth0) and one for ingress (loopback). It then passes data between them with two threads.
+It then reports IP and port via stdout
+EOF (close stream) written to the stdin kills process.
 
 To build tool use Makefile. We link it statically because WSL may lack glibc. Kernel ABI is backward compatible, so use some old Linux
 
--- native/WslTools/wslproxy.c
+++ native/WslTools/wslproxy.c
@@ -4,16 +4,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <stdbool.h>
 #include <pthread.h>
 #include <strings.h>
-#include <sys/select.h>
+#include <errno.h>
 
-// Output: [linux-ip] [external-port] [internal-port]
-// Connect to the external from Windows and to the internal from Linux
-// Sockets are connected from that moment until byte written to the stdin or SIGINT/SIGHUP sent
 
-#define JB_WSL_INTERFACE "eth0"
+// See svg file and wslproxy_test_client.py
+
+// When started, prints egress (eth0) IP addr as 4 bytes. Then, 2 bytes of ingress (loopback) port.
+// App running on WSL connects to this port.
+// Tool then opens egress (eth0) port and prints it as 2 bytes.
+// Windows client connects to it and talks to WSL app connected to the ingress port.
+// When stdout is closed or SIGINT sent, process stops.
+
+// Threads are unbound, but it should not be a problem unless you create lots of connections
+
+
+// Will listen egress in this port
+#define JB_EGRESS_INTERFACE "eth0"
+
+// Egress IP
+static in_addr_t g_egress_ip;
 
 // Bind to eth0 only
 static in_addr_t jb_get_wsl_public_ip() {
@@ -23,9 +34,9 @@ static in_addr_t jb_get_wsl_public_ip() {
         exit(-1);
     }
     for (struct ifaddrs *ifaddrs_p = ifaddrs_p_base; ifaddrs_p != NULL; ifaddrs_p = ifaddrs_p->ifa_next) {
-        // Search for inteface and ipv4
+        // Search for interface and ipv4
         if ((ifaddrs_p->ifa_addr->sa_family != AF_INET) ||
-            ((strcmp(ifaddrs_p->ifa_name, JB_WSL_INTERFACE) != 0))) {
+            ((strcmp(ifaddrs_p->ifa_name, JB_EGRESS_INTERFACE) != 0))) {
             continue;
         }
         const struct sockaddr_in *in_addr = (struct sockaddr_in *) ifaddrs_p->ifa_addr;
@@ -34,12 +45,12 @@ static in_addr_t jb_get_wsl_public_ip() {
         return result;
     }
     freeifaddrs(ifaddrs_p_base);
-    fprintf(stderr, "No interface %s found", JB_WSL_INTERFACE);
+    fprintf(stderr, "No interface %s found", JB_EGRESS_INTERFACE);
     exit(-1);
 }
 
-// Open internal or external sock, print addr if print_addr
-static int jb_open_socket(const in_addr_t listen_to, const bool print_addr) {
+// Creates server socket, returns its descriptor and opened port via pointer
+static int jb_create_srv_socket(const in_addr_t listen_to, uint16_t *port) {
     const int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         perror("can't open socket");
@@ -62,103 +73,120 @@ static int jb_open_socket(const in_addr_t listen_to, const bool print_addr) {
         perror("getsockname failed");
         exit(-1);
     }
-
-    if (print_addr) {
-        printf("%s ", inet_ntoa(addr_p.sin_addr));
-    }
-    printf("%d ", ntohs(addr_p.sin_port));
-
+    *port = ntohs(addr_p.sin_port);
     return sock;
 }
 
-struct jb_socket_info {
-    int server_sock_fd; // Listening server socket
-    int client_sock_fd; // Connected (accepted) client
-};
 
-// public (external) socket and local (internal)
-static struct jb_socket_info jb_public = {0}, jb_local = {0};
+// runs command in separate thread
+// thread is detached so it just dies when command finishes: no need to join it
+static void jb_launch_in_thread(void *(command)(void *), void *argument) {
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
 
-// marks socket for the next "select" call if client hasn't been connected yet
-static void jb_set_select_socket(const struct jb_socket_info *sock, fd_set *sock_set, int *max_sock) {
-    if (sock->client_sock_fd != 0) { // Client already connected
-        return;
-    }
-    FD_SET(sock->server_sock_fd, sock_set); // select socket
-    if (sock->server_sock_fd > *max_sock) {
-        *max_sock = sock->server_sock_fd;
-    }
-}
-
-static void jb_set_client_socket(const fd_set *sock_set, struct jb_socket_info *sock) {
-    if (sock->client_sock_fd == 0 && FD_ISSET(sock->server_sock_fd, sock_set)) {
-        if ((sock->client_sock_fd = accept(sock->server_sock_fd, NULL, NULL)) < 0) {
-            perror("accept call failed");
-            exit(-1);
-        }
-        close(sock->server_sock_fd); // Client connected, close listening socket
-        sock->server_sock_fd = 0;
-    }
-}
-
-// Waits for both clients to connect
-static void jb_connect_accept_clients() {
-    fd_set sock_set;
-    struct timeval time = {.tv_sec = 120, .tv_usec = 0};
-
-    const int stdin_fd = fileno(stdin);
-    while (jb_public.client_sock_fd == 0 || jb_local.client_sock_fd == 0) {
-        FD_ZERO(&sock_set);
-        FD_SET(stdin_fd, &sock_set);
-        int max_sock = stdin_fd;
-        jb_set_select_socket(&jb_local, &sock_set, &max_sock);
-        jb_set_select_socket(&jb_public, &sock_set, &max_sock);
-        if (select(max_sock + 1, &sock_set, NULL, NULL, &time) < 0) {
-            perror("select call failed");
-            exit(-1);
-        }
-        if (FD_ISSET(stdin_fd, &sock_set)) {
-            exit(0);
-        }
-        jb_set_client_socket(&sock_set, &jb_local);
-        jb_set_client_socket(&sock_set, &jb_public);
-    }
-}
-
-// Connect source (sock) and destination (another socket)
-static void *jb_connect(const struct jb_socket_info *sock) {
-    const int source = sock->client_sock_fd;
-    const int dest = (sock == &jb_local ? jb_public : jb_local).client_sock_fd;
-    char buf[4096];
-    size_t bytes;
-    while ((bytes = read(source, buf, sizeof(buf))) > 0) {
-        write(dest, buf, bytes);
-    }
-    exit(0);
-}
-
-static void jb_create_thread(struct jb_socket_info *sock) {
     pthread_t thread;
-    const int result = pthread_create(&thread, NULL, (void *(*)(void *)) &jb_connect, sock);
+    const int result = pthread_create(&thread, &attr, (void *(*)(void *)) command, argument);
     if (result != 0) {
         fprintf(stderr, "pthread_create failed: %d", result);
         exit(result);
     }
 }
 
+// Pair of socket to connect
+typedef struct {
+    int src_socket_fd; // read from here
+    int dest_socket_fd; // write here
+} jb_sockpair;
+
+
+// connects two sockets: read from src, write to dst.
+// struct must live in heap: it will be freed at the end
+// it closes only source, but since you have two pairs each source will be closed by appropriate function
+static void *jb_connect_pair(jb_sockpair *sockpair) {
+    const int source = sockpair->src_socket_fd;
+    const int dest = sockpair->dest_socket_fd;
+    free(sockpair);
+    char buf[64800]; //MTU 1500 + 60 bytes TCP+IP header * 45 (because can't be greater than TCP max window size)
+    ssize_t bytes;
+    while ((bytes = read(source, buf, sizeof(buf))) > 0) {
+        ssize_t sent = 0;
+        ssize_t write_result;
+        while (sent < bytes) {
+            if ((write_result = write(dest, buf + sent, bytes - sent)) < 0) {
+                if (errno != EINTR || errno != EAGAIN) {
+                    break; //socket closed
+                }
+            }
+            sent += write_result;
+        }
+    }
+    close(source);
+    return NULL;
+}
+
+// accepts server socket, returns client socket
+static int jb_accept(const int srv_sock_fd) {
+    const int client_sock_fd = accept(srv_sock_fd, NULL, NULL);
+    if (client_sock_fd < 0) {
+        perror("Can't accept connection");
+        return -1;
+    }
+    return client_sock_fd;
+}
+
+
+// creates structure for pair of sockets in heap
+static jb_sockpair *jb_create_sockpair(const int src_fd, const int dst_fd) {
+    jb_sockpair *pair = malloc(sizeof(jb_sockpair));
+    pair->src_socket_fd = src_fd;
+    pair->dest_socket_fd = dst_fd;
+    return pair;
+}
+
+
+// Listens for ingress server socket
+// on each connection creates egress server socket and connects it with client socket
+// As detached thread may run forever
+_Noreturn static void *jb_listen_ingress(const int *p_ingress_srv_sock_fd) {
+    while (1) {
+        const int ingress_client_sock_fd = jb_accept(*p_ingress_srv_sock_fd);
+        if (ingress_client_sock_fd < 0) {
+            continue; // Error logged by jb_accept
+        }
+
+        uint16_t egress_port;
+        const int egress_srv_sock_fd = jb_create_srv_socket(g_egress_ip, &egress_port);
+        write(STDOUT_FILENO, &egress_port, sizeof egress_port);
+
+        const int egress_client_sock_fd = jb_accept(egress_srv_sock_fd);
+        close(egress_srv_sock_fd);
+        if (egress_srv_sock_fd < 0) {
+            continue; // Error logged by jb_accept
+        }
+        jb_sockpair *egress_to_ingress = jb_create_sockpair(egress_client_sock_fd, ingress_client_sock_fd);
+        jb_sockpair *ingress_to_egress = jb_create_sockpair(ingress_client_sock_fd, egress_client_sock_fd);
+
+        jb_launch_in_thread((void *(*)(void *)) &jb_connect_pair, egress_to_ingress);
+        jb_launch_in_thread((void *(*)(void *)) &jb_connect_pair, ingress_to_egress);
+    }
+}
+
+static int g_ingress_srv_sock_fd;
+
 int main() {
-    jb_public.server_sock_fd = jb_open_socket(jb_get_wsl_public_ip(), true);
-    jb_local.server_sock_fd = jb_open_socket(htonl(INADDR_LOOPBACK), false);
+    g_egress_ip = jb_get_wsl_public_ip();
 
-    puts("\n");
-    fflush(stdout);
+    // IP address
+    write(STDOUT_FILENO, &g_egress_ip, sizeof g_egress_ip);
 
-    jb_connect_accept_clients();
+    // Open ingress port and report to Intellij
+    uint16_t ingress_port;
+    g_ingress_srv_sock_fd = jb_create_srv_socket(htonl(INADDR_LOOPBACK), &ingress_port);
+    // Ingress server port
+    write(STDOUT_FILENO, &ingress_port, sizeof ingress_port);
+    jb_launch_in_thread((void *(*)(void *)) &jb_listen_ingress, &g_ingress_srv_sock_fd);
 
-
-    jb_create_thread(&jb_public);
-    jb_create_thread(&jb_local);
-
-    getchar();
+    while (getchar() != EOF);
     return 0;
 }
