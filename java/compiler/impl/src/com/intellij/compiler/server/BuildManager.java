df23c55a2f2138c9bc0134260698ea8f22190d95
Eugene Zhuravlev <jeka@jetbrains.com>
2021-12-13 08:36:25 +0100

    reset suspend_tasks counter on explicit build to compensate unbalanced BatchFileChangeListener API calls (IDEA-284089)
    (cherry picked from commit 7f266140663c9c7cd5ed65ebf9669a8187ba868a)
    
    IJ-CR-18565
    
    GitOrigin-RevId: e409fdf0e8ddd1e8ac7135ab2e52d58cf01a8002

--- java/compiler/impl/src/com/intellij/compiler/server/BuildManager.java
+++ java/compiler/impl/src/com/intellij/compiler/server/BuildManager.java
@@ -378,7 +378,7 @@ public final class BuildManager implements Disposable {
 
       @Override
       public void batchChangeCompleted(@NotNull Project project) {
-        allowBackgroundTasks();
+        allowBackgroundTasks(false);
       }
     });
 
@@ -429,8 +429,15 @@ public final class BuildManager implements Disposable {
     mySuspendBackgroundTasksCounter.incrementAndGet();
   }
 
-  public void allowBackgroundTasks() {
-    mySuspendBackgroundTasksCounter.decrementAndGet();
+  public void allowBackgroundTasks(boolean resetState) {
+    if (resetState) {
+      mySuspendBackgroundTasksCounter.set(0);
+    }
+    else {
+      if (mySuspendBackgroundTasksCounter.decrementAndGet() < 0) {
+        mySuspendBackgroundTasksCounter.incrementAndGet();
+      }
+    }
   }
 
   private static @NotNull String getFallbackSdkHome() {
