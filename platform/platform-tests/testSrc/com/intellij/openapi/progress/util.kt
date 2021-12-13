2662e6935435740d5ad5c42241e6a59fb379b96b
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-12-13 17:33:02 +0100

    tests: fix typo in `Job.waitJoin`
    GitOrigin-RevId: 61bfb9fc49cc34dde18bfe8889154b89678f69b2

--- platform/platform-tests/testSrc/com/intellij/openapi/progress/util.kt
+++ platform/platform-tests/testSrc/com/intellij/openapi/progress/util.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.progress
 
 import com.intellij.testFramework.LoggedErrorProcessor
@@ -67,8 +67,8 @@ fun waitAssertCompletedWithCancellation(future: Future<*>) {
 }
 
 fun Job.waitJoin(): Unit = runBlocking {
-  join()
   withTimeout(TIMEOUT_MS) {
+    join()
   }
 }
 
