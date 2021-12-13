45b782a03729d42d47b7a6cd03d8fe47f398fbcb
Jinseong Jeon <jsjeon@google.com>
2021-12-14 07:46:48 +0900

    FIR/UAST: reproduce various issues on fake light methods
    ^KTIJ-18039 In Progress
    ^KTIJ-18716 In Progress
    ^KTIJ-18720 In Progress
    
    GitOrigin-RevId: 7d215a5c52bc8a97353a296178f38a6762c0a0c0

--- plugins/kotlin/uast/uast-kotlin-fir/test/org/jetbrains/kotlin/idea/fir/uast/FirUastResolveApiFixtureTest.kt
+++ plugins/kotlin/uast/uast-kotlin-fir/test/org/jetbrains/kotlin/idea/fir/uast/FirUastResolveApiFixtureTest.kt
@@ -122,4 +122,8 @@ class FirUastResolveApiFixtureTest : KotlinLightCodeInsightFixtureTestCase(), Ua
     fun testDivByZero() {
         doCheck("DivByZero", ::checkDivByZero)
     }
+
+    fun testDetailsOfDeprecatedHidden() {
+        doCheck("DetailsOfDeprecatedHidden", ::checkDetailsOfDeprecatedHidden)
+    }
 }
