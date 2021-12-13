d529f06db7f6dc0f8658782008a20b35e62fc2a7
Liubov Afanaseva <liubov.afanaseva@jetbrains.com>
2021-12-13 15:35:22 +0300

    FUS: "indexing" and "run.configuration.exec" groups: add TERMINATED/FINISHED finish_type to finished event (IDEA-282296)
    GitOrigin-RevId: 015e69b9b39dc1ee8943ad8a17572dd34f709b4e

--- platform/platform-impl/src/com/intellij/openapi/project/IndexingStatisticsCollector.kt
+++ platform/platform-impl/src/com/intellij/openapi/project/IndexingStatisticsCollector.kt
@@ -1,19 +1,29 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.project
 
+import com.intellij.internal.statistic.StructuredIdeActivity
 import com.intellij.internal.statistic.eventLog.EventLogGroup
 import com.intellij.internal.statistic.eventLog.events.EventFields
+import com.intellij.internal.statistic.eventLog.events.EventPair
 import com.intellij.internal.statistic.service.fus.collectors.CounterUsagesCollector
 
 class IndexingStatisticsCollector : CounterUsagesCollector() {
   companion object {
-    val GROUP = EventLogGroup("indexing", 5)
+    @JvmStatic
+    fun logProcessFinished(activity: StructuredIdeActivity?,
+                           finishType: IndexingFinishType) {
+      activity?.finished { listOf(EventPair(FINISH_TYPE, finishType)) }
+    }
+
+    val GROUP = EventLogGroup("indexing", 6)
 
     @JvmField
     val STAGE_CLASS = EventFields.Class("stage_class")
 
     @JvmField
-    val INDEXING_ACTIVITY = GROUP.registerIdeActivity(null)
+    val FINISH_TYPE = EventFields.Enum("finish_type", IndexingFinishType::class.java)
+    @JvmField
+    val INDEXING_ACTIVITY = GROUP.registerIdeActivity(null, emptyArray(), arrayOf(FINISH_TYPE))
 
     @JvmField
     val INDEXING_STAGE = INDEXING_ACTIVITY.registerStage("stage", arrayOf(STAGE_CLASS, EventFields.PluginInfo))
@@ -21,5 +31,9 @@ class IndexingStatisticsCollector : CounterUsagesCollector() {
 
   override fun getGroup(): EventLogGroup {
     return GROUP
+  }
+
+  enum class IndexingFinishType {
+    TERMINATED, FINISHED
   }
 }
