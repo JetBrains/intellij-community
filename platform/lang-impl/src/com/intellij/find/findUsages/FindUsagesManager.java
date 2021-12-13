925bfb0b6b3ec08d023e520fc66da33a5875e453
Vadim Makarov <vadim.makarov@jetbrains.com>
2021-12-13 18:24:10 +0100

    [navigation][fleet][FLEET-T-2062] move supplemental `createUsageSearcher` into Fleet backend world.
    GitOrigin-RevId: a8633c2e1824cb5bd4802369069800bab7abed88

--- platform/lang-impl/src/com/intellij/find/findUsages/FindUsagesManager.java
+++ platform/lang-impl/src/com/intellij/find/findUsages/FindUsagesManager.java
@@ -51,7 +51,6 @@ import com.intellij.util.ArrayUtil;
 import com.intellij.util.CommonProcessors;
 import com.intellij.util.Processor;
 import com.intellij.util.containers.ContainerUtil;
-import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -59,8 +58,6 @@ import org.jetbrains.annotations.Nullable;
 import javax.swing.*;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -290,7 +287,6 @@ public final class FindUsagesManager {
     startProcessUsages(indicator, handler.getProject(), createUsageSearcher(handler, primaryElements, secondaryElements, findUsagesOptions), processor, onComplete);
   }
 
-  @ApiStatus.Internal
   public static UsageSearcher createUsageSearcher(@NotNull FindUsagesHandlerBase handler,
                                                   PsiElement @NotNull [] primaryElements,
                                                   PsiElement @NotNull [] secondaryElements, @NotNull FindUsagesOptions findUsagesOptions) {
@@ -299,42 +295,6 @@ public final class FindUsagesManager {
       PsiElement2UsageTargetAdapter[] secondaryTargets = PsiElement2UsageTargetAdapter.convert(secondaryElements, false);
       return createUsageSearcher(primaryTargets, secondaryTargets, handler, findUsagesOptions, null);
     });
-  }
-
-  public @Nullable UsageSearcher createUsageSearcher(FindUsagesHandler handler) {
-
-    PsiElement[] primaryElements;
-    PsiElement[] secondaryElements;
-    LOG.assertTrue(handler.getPsiElement().isValid());
-
-    try {
-      final CompletableFuture<PsiElement[]> primaryElementsFuture = new CompletableFuture<>();
-      final CompletableFuture<PsiElement[]> secondaryElementsFuture = new CompletableFuture<>();
-      ApplicationManager.getApplication().invokeAndWait(() -> {
-        // This is ugly, but some implementations of FindUsagesHandler do require a DispatcherThread for these operations.
-        // for example: com.intellij.ide.util.SuperMethodWarningUtil.getTargetMethodCandidates
-        // called from com.intellij.find.findUsages.JavaFindUsagesHandler.getPrimaryElements
-        primaryElementsFuture.complete(handler.getPrimaryElements());
-        secondaryElementsFuture.complete(handler.getSecondaryElements());
-      });
-      primaryElements = primaryElementsFuture.get();
-      secondaryElements = secondaryElementsFuture.get();
-    }
-    catch (InterruptedException e) {
-      LOG.warn("primaryElements or secondaryElements calculation got interrupted! ", e);
-      return null;
-    }
-    catch (ExecutionException e) {
-      LOG.warn("primaryElements or secondaryElements is completed with exception", e);
-      return null;
-    }
-
-
-    LOG.assertTrue(handler.getPsiElement().isValid());
-    checkNotNull(primaryElements, handler, "getPrimaryElements()");
-    checkNotNull(secondaryElements, handler, "getSecondaryElements()");
-
-    return createUsageSearcher(handler, primaryElements, secondaryElements, handler.getFindUsagesOptions());
   }
 
   public static void startProcessUsages(@NotNull ProgressIndicator indicator,
