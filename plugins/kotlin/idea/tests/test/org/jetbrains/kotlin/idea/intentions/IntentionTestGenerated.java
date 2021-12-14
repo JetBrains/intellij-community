ae37394f4e4d4c47d8863710e038c6d0c5addb26
Toshiaki Kameyama <kmymtsak@gmail.com>
2021-12-14 12:57:53 +0900

    [kotlin] Convert template to concatenated string: fix it works correctly when string has plus sign
    ^KTIJ-20414 Fixed
    
    closes https://github.com/JetBrains/intellij-community/pull/1873
    
    GitOrigin-RevId: 07602f84396bb87e79a84b6ed84eb1a9198f33dc

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/intentions/IntentionTestGenerated.java
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/intentions/IntentionTestGenerated.java
@@ -8170,6 +8170,16 @@ public abstract class IntentionTestGenerated extends AbstractIntentionTest {
             runTest("testData/intentions/convertToConcatenatedString/handlesEscapeString.kt");
         }
 
+        @TestMetadata("hasPlusSign.kt")
+        public void testHasPlusSign() throws Exception {
+            runTest("testData/intentions/convertToConcatenatedString/hasPlusSign.kt");
+        }
+
+        @TestMetadata("hasPlusSignInRawString.kt")
+        public void testHasPlusSignInRawString() throws Exception {
+            runTest("testData/intentions/convertToConcatenatedString/hasPlusSignInRawString.kt");
+        }
+
         @TestMetadata("interpolate2StringConstants.kt")
         public void testInterpolate2StringConstants() throws Exception {
             runTest("testData/intentions/convertToConcatenatedString/interpolate2StringConstants.kt");
