f395c6357e0fc15e4cd65bfb4c85ea7b7d98d9a7
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2021-12-15 14:45:26 +0300

    [groovy] IDEA-122730: Add possibility of testing postfix templates
    GitOrigin-RevId: d4b538e366724d5ee121536f86b4508394a04354

---
+++ plugins/groovy/test/org/jetbrains/plugins/groovy/codeInsight/GroovyPostfixTemplatesTest.groovy
@@ -0,0 +1,57 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.plugins.groovy.codeInsight
+
+import com.intellij.codeInsight.lookup.Lookup
+import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.codeInsight.template.impl.LiveTemplateCompletionContributor
+import com.intellij.codeInsight.template.postfix.completion.PostfixTemplateLookupElement
+import com.intellij.codeInsight.template.postfix.settings.PostfixTemplatesSettings
+import com.intellij.codeInsight.template.postfix.templates.PostfixTemplate
+import org.jetbrains.annotations.NotNull
+import org.jetbrains.annotations.Nullable
+import org.jetbrains.plugins.groovy.codeInsight.template.postfix.GrParenthesizedExpressionPostfixTemplate
+import org.jetbrains.plugins.groovy.completion.GroovyAutoPopupTest
+
+class GroovyPostfixTemplatesTest extends GroovyAutoPopupTest {
+
+  @Override
+  void setUp() throws Exception {
+    super.setUp();
+    LiveTemplateCompletionContributor.setShowTemplatesInTests(false, myFixture.getTestRootDisposable())
+  }
+
+  @Override
+  void tearDown() throws Exception {
+    PostfixTemplatesSettings settings = PostfixTemplatesSettings.getInstance()
+    assertNotNull(settings)
+    settings.setProviderToDisabledTemplates(new HashMap<>())
+    settings.setPostfixTemplatesEnabled(true)
+    settings.setTemplatesCompletionEnabled(true)
+    super.tearDown()
+  }
+
+  private void doAutoPopupTest(@NotNull String baseText,
+                               @NotNull String textToType,
+                               @NotNull String result,
+                               @Nullable Class<? extends PostfixTemplate> expectedClass) {
+    myFixture.configureByText "_.groovy", baseText
+    type textToType
+    Lookup lookup = myFixture.getLookup()
+    if (expectedClass != null) {
+      assertNotNull(lookup)
+      LookupElement item = lookup.getCurrentItem()
+      assertNotNull item
+      assertInstanceOf item, PostfixTemplateLookupElement.class
+      assertInstanceOf((item as PostfixTemplateLookupElement).getPostfixTemplate(), expectedClass)
+      type ' '
+      myFixture.checkResult result
+    }
+    else {
+      assertNull(lookup);
+    }
+  }
+
+  void testPar() {
+    doAutoPopupTest "1.<caret>", "par", "(1)", GrParenthesizedExpressionPostfixTemplate
+  }
+}
