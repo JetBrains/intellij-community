27d7589591ef753495171ddb959f3d133652ff95
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2021-12-15 17:44:20 +0300

    [git-stash] hide extra changes browser borders
    GitOrigin-RevId: b7a9db86a7dd866c30f1c610daa2040b2870734c

--- plugins/git4idea/src/git4idea/stash/ui/GitStashChangesBrowser.kt
+++ plugins/git4idea/src/git4idea/stash/ui/GitStashChangesBrowser.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package git4idea.stash.ui
 
 import com.intellij.openapi.Disposable
@@ -49,6 +49,7 @@ class GitStashChangesBrowser(project: Project, parentDisposable: Disposable) : C
   init {
     init()
     viewer.emptyText.text = GitBundle.message("stash.changes.empty")
+    hideViewerBorder()
 
     Disposer.register(parentDisposable, this)
   }
