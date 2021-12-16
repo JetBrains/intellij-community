dfba4ccd36a3fe7f058679a922e22d1a79eb1d4b
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2021-12-16 17:45:27 +0300

    [kotlin, mpp] Add test on declaring kotlin-test via newer DSL
    It is possible to use 'kotlin("test")' in DSL. Not only it is more
    concise, but it also substitutes the correct module (with Gradle
    module metadata, which in this case is a 'kotlin-test' module)
    
    Prior tests used to add modules directly by their coordinates and were
    not using Gradle Module Metadata-aware module (e.g.
    'testImportIntermediateModules' declared specifically
    'kotlin-test-common' for 'commonTest', 'kotlin-test-js' for 'jsTest',
    etc.)
    
    GitOrigin-RevId: dc43c6bc1fe9b7adac876c633599a508f261b39d

---
+++ plugins/kotlin/idea/tests/testData/gradle/hierarchicalMultiplatformImport/dependencyOnKotlinTestFromPlatformSourceSets/build.gradle.kts
@@ -0,0 +1,30 @@
+buildscript {
+    repositories {
+        {{kts_kotlin_plugin_repositories}}
+    }
+}
+
+repositories {
+    {{kts_kotlin_plugin_repositories}}
+}
+
+plugins {
+    kotlin("multiplatform").version("{{kotlin_plugin_version}}")
+}
+
+group = "project"
+version = "1.0"
+
+kotlin {
+    jvm() 
+    js()
+    linuxX64()
+
+    sourceSets {
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+    }
+}
