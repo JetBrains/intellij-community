a59f879e850758046df45f3b1b4cba0fc116032c
Anton Lakotka <anton.lakotka@jetbrains.com>
2021-12-16 11:56:45 +0100

    [Kotlin] Fix "Bad Dependency" tests
    Chery-picked from: 4fb5d88701f62c17988df1d721e41d085b03864a
    
    After enabling HMPP by default MPP projects with Android
    can't be configured via gradle pluginManagement
    in settings.gradle.kts
    
    This needs detailed investigation and proper fix.
    So currently it is a workaround via buildscript dependencies.
    
    GitOrigin-RevId: cf76b3a9322b3704fc0e2ed2a537df26b6adf63e

--- plugins/kotlin/idea/tests/testData/gradle/newMultiplatformImport/kT42381BadDependencyOnArtifactInsteadOfSources/build.gradle.kts
+++ plugins/kotlin/idea/tests/testData/gradle/newMultiplatformImport/kT42381BadDependencyOnArtifactInsteadOfSources/build.gradle.kts
@@ -1,3 +1,16 @@
+plugins {
+    kotlin("multiplatform") version "{{kotlin_plugin_version}}" apply false
+}
+
+buildscript {
+    repositories {
+        {{kts_kotlin_plugin_repositories}}
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.6.4")
+    }
+}
+
 allprojects {
     repositories {
         {{kts_kotlin_plugin_repositories}}
