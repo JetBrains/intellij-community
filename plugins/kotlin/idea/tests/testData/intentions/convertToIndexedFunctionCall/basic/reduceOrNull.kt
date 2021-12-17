4e32cbd90a71b996f6d9113a2876efbc3cc1611e
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2021-12-17 12:23:05 +0300

    [kotlin] fix ConvertToIndexedFunctionCall$Basic tests
    GitOrigin-RevId: d672115976fb282862f073e5346dd46a7b8b6677

--- plugins/kotlin/idea/tests/testData/intentions/convertToIndexedFunctionCall/basic/reduceOrNull.kt
+++ plugins/kotlin/idea/tests/testData/intentions/convertToIndexedFunctionCall/basic/reduceOrNull.kt
@@ -1,6 +1,6 @@
 // INTENTION_TEXT: "Convert to 'reduceIndexedOrNull'"
 // WITH_RUNTIME
-// AFTER-WARNING: This class can only be used with the compiler argument '-opt-in=kotlin.RequiresOptIn'
+// AFTER-WARNING: This annotation should be used with the compiler argument '-opt-in=kotlin.RequiresOptIn'
 // TODO: fix warning?
 // AFTER-WARNING: Parameter 'index' is never used, could be renamed to _
 @OptIn(ExperimentalStdlibApi::class)
