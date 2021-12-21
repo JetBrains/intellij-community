20ef354555bf47cfd3a8f13a7d146247962e0c08
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2021-12-21 13:07:38 +0100

    [Kotlin][MPP] Fixup newMultiplatformHighlighting after expect/actual marker changes
    ^KT-50120
    
    GitOrigin-RevId: c46aab020421dec418604e9bec8e222839d4b503

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/highlighter/markers/HasExpectActualMarkerUtil.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/highlighter/markers/HasExpectActualMarkerUtil.kt
@@ -2,19 +2,32 @@
 package org.jetbrains.kotlin.idea.highlighter.markers
 
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
+import org.jetbrains.kotlin.descriptors.MemberDescriptor
 import org.jetbrains.kotlin.descriptors.ModuleDescriptor
 import org.jetbrains.kotlin.resolve.descriptorUtil.module
 
 internal fun getModulesStringForExpectActualMarkerTooltip(
     descriptors: List<DeclarationDescriptor>
-): String?  {
-    if(descriptors.isEmpty()) return null
+): String? {
+    if (descriptors.isEmpty()) return null
+    val isExpectDescriptors = descriptors.all { it is MemberDescriptor && it.isExpect }
 
     fun ModuleDescriptor.nameForTooltip(): String {
-        return stableName?.asStringStripSpecialMarkers() ?: "N/A"
+        stableName?.asStringStripSpecialMarkers()?.let { return it }
+
+        /* Handle non-hmpp modes where no stableName might be present: */
+
+        // There is no 'easy' way to figure out the exact common module in this case
+        if (isExpectDescriptors) return "?common?"
+
+        // We want to represent actual descriptors, so let's represent them by platform
+        this.platform?.componentPlatforms?.joinToString(", ", "{", "}") { it.platformName }?.let { return it }
+
+        // We did our best, what can you do, this is life for you!
+        return "N/A"
     }
 
-    return  when (descriptors.size) {
+    return when (descriptors.size) {
         1 -> descriptors.single().module.nameForTooltip()
         else -> descriptors.joinToString(", ", "[", "]") { it.module.nameForTooltip() }
     }
