91ca299961ad96090a0e2b165dfcc7dbb93f080d
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2021-12-21 17:30:34 +0300

    [Kotlin][MPP] Fixup GradleMultiplatformHighlightingTest after expect/actual marker changes
    ^KT-50120
    
    GitOrigin-RevId: d98c0adee77316c3f3300ff96a7ede6a716b8a12

--- plugins/kotlin/idea/tests/testData/gradle/testRunConfigurations/expectClassWithTests/src/jvmTest/kotlin/sample/SampleTestsJVM.kt
+++ plugins/kotlin/idea/tests/testData/gradle/testRunConfigurations/expectClassWithTests/src/jvmTest/kotlin/sample/SampleTestsJVM.kt
@@ -4,8 +4,8 @@ import kotlin.test.Test
 import kotlin.test.assertTrue
 
 // JVM
-actual class <lineMarker descr="Run Test" settings=":cleanJvmTest :jvmTest --tests \"sample.SampleTests\""><lineMarker descr="Has declaration in common module">SampleTests</lineMarker></lineMarker> {
+actual class <lineMarker descr="Run Test" settings=":cleanJvmTest :jvmTest --tests \"sample.SampleTests\""><lineMarker descr="Has expects in ?common? module">SampleTests</lineMarker></lineMarker> {
     @Test
-    actual fun <lineMarker descr="Run Test" settings=":cleanJvmTest :jvmTest --tests \"sample.SampleTests.testMe\""><lineMarker descr="Has declaration in common module">testMe</lineMarker></lineMarker>() {
+    actual fun <lineMarker descr="Run Test" settings=":cleanJvmTest :jvmTest --tests \"sample.SampleTests.testMe\""><lineMarker descr="Has expects in ?common? module">testMe</lineMarker></lineMarker>() {
     }
 }
