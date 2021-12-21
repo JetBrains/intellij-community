0edbba6597da2c2ea73f6f03a64273f7ccaaddcf
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2021-12-21 09:35:28 +0100

    [Kotlin][MPP] Fixup MultiModuleLineMarkerTestGenerated after expect/actual marker changes
    ^KT-50120
    
    GitOrigin-RevId: bb7a8ef4bca6fdf83c2460a62a64ca58385a13c6

--- plugins/kotlin/idea/tests/testData/multiModuleLineMarker/actualEnumEntries/jvm/jvm.kt
+++ plugins/kotlin/idea/tests/testData/multiModuleLineMarker/actualEnumEntries/jvm/jvm.kt
@@ -1,8 +1,8 @@
 package test
 
-actual enum class <!LINE_MARKER("descr='Has declaration in common module'")!>Enum<!> {
-    <!LINE_MARKER("descr='Has declaration in common module'")!>A<!>,
-    <!LINE_MARKER("descr='Has declaration in common module'")!>B<!>,
-    <!LINE_MARKER("descr='Has declaration in common module'")!>C<!>,
+actual enum class <!LINE_MARKER("descr='Has expects in common module'")!>Enum<!> {
+    <!LINE_MARKER("descr='Has expects in common module'")!>A<!>,
+    <!LINE_MARKER("descr='Has expects in common module'")!>B<!>,
+    <!LINE_MARKER("descr='Has expects in common module'")!>C<!>,
     D
 }
--- plugins/kotlin/idea/tests/testData/multiModuleLineMarker/fromCommonToJvmImpl/jvm/jvm.kt
+++ plugins/kotlin/idea/tests/testData/multiModuleLineMarker/fromCommonToJvmImpl/jvm/jvm.kt
@@ -1,11 +1,9 @@
-actual class <!LINE_MARKER("descr='Has declaration in common module'")!>Header<!> {
-    actual fun <!LINE_MARKER("descr='Has declaration in common module'")!>foo<!>() = 42
+actual class <!LINE_MARKER("descr='Has expects in common module'")!>Header<!> {
+    actual fun <!LINE_MARKER("descr='Has expects in common module'")!>foo<!>() = 42
 }
 
-actual class <!LINE_MARKER("descr='Has declaration in common module'")!>Incomplete<!>
+actual class <!LINE_MARKER("descr='Has expects in common module'")!>Incomplete<!>
 
-actual fun <!LINE_MARKER("descr='Has declaration in common module'")!>foo<!>(arg: Int) = arg.toString()
+actual fun <!LINE_MARKER("descr='Has expects in common module'")!>foo<!>(arg: Int) = arg.toString()
 
-actual val <!LINE_MARKER("descr='Has declaration in common module'")!>flag<!> = true
-
-
+actual val <!LINE_MARKER("descr='Has expects in common module'")!>flag<!> = true
