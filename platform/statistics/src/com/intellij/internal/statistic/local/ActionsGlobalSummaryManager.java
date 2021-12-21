cd17cfc0e23e7aaa9e096232114c2b9ea4aca223
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2021-12-21 17:19:05 +0100

    no need to duplicate ActionsGlobalSummaryManager registration as a service - it is already a light service
    GitOrigin-RevId: 50f4bb73f11d016912d54c744aa9b9193ef47a87

--- platform/statistics/src/com/intellij/internal/statistic/local/ActionsGlobalSummaryManager.java
+++ platform/statistics/src/com/intellij/internal/statistic/local/ActionsGlobalSummaryManager.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.internal.statistic.local;
 
 import com.intellij.openapi.components.Service;
@@ -16,9 +16,8 @@ import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
-@Service
+@Service(Service.Level.APP)
 public final class ActionsGlobalSummaryManager {
-
   private static final Logger LOG = Logger.getInstance(ActionsGlobalSummaryManager.class);
   private static final @NotNull CharFilter QUOTE_FILTER = ch -> ch != '"';
 
