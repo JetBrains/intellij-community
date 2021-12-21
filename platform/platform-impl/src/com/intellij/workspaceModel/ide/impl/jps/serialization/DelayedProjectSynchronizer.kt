7245a922c012dfca564f22001df08d8a9243245f
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2021-12-21 17:22:38 +0100

    cleanup
    GitOrigin-RevId: eca6f9a688d4b2dde00cfdf83fe33b57c22552e1

--- platform/platform-impl/src/com/intellij/workspaceModel/ide/impl/jps/serialization/DelayedProjectSynchronizer.kt
+++ platform/platform-impl/src/com/intellij/workspaceModel/ide/impl/jps/serialization/DelayedProjectSynchronizer.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.workspaceModel.ide.impl.jps.serialization
 
 import com.intellij.openapi.diagnostic.logger
@@ -15,7 +15,7 @@ import kotlin.system.measureTimeMillis
  * Initially IJ loads the state of workspace model from the cache. In this startup activity it synchronizes the state
  * of workspace model with project model files (iml/xml).
  */
-class DelayedProjectSynchronizer : StartupActivity.DumbAware {
+internal class DelayedProjectSynchronizer : StartupActivity.DumbAware {
   override fun runActivity(project: Project) {
     val projectModelSynchronizer = JpsProjectModelSynchronizer.getInstance(project)
     if (projectModelSynchronizer != null && (WorkspaceModel.getInstance(project) as WorkspaceModelImpl).loadedFromCache) {
@@ -23,8 +23,8 @@ class DelayedProjectSynchronizer : StartupActivity.DumbAware {
         projectModelSynchronizer.loadProject(project)
         project.messageBus.syncPublisher(JpsProjectLoadedListener.LOADED).loaded()
       }
-      log.info(
-        "Workspace model loaded from cache. Syncing real project state into workspace model in $loadingTime ms. ${Thread.currentThread()}")
+      log.info("Workspace model loaded from cache. " +
+               "Syncing real project state into workspace model in $loadingTime ms. ${Thread.currentThread()}")
     }
   }
 
