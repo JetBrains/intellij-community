598b2605a898309ef3a5b839f3414b9b42e81648
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2021-12-21 15:44:11 +0100

    cleanup
    GitOrigin-RevId: 58bc6599e9bd9fad74bb5ec93a9526f7bfb2d67a

--- platform/platform-impl/src/com/intellij/ide/ui/ProjectTopHitCache.kt
+++ platform/platform-impl/src/com/intellij/ide/ui/ProjectTopHitCache.kt
@@ -1,25 +1,25 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.intellij.ide.ui;
+package com.intellij.ide.ui
 
-import com.intellij.openapi.components.Service;
-import com.intellij.openapi.extensions.ExtensionPointListener;
-import com.intellij.openapi.extensions.PluginDescriptor;
-import com.intellij.openapi.project.Project;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.ide.ui.OptionsSearchTopHitProvider.ProjectLevelProvider
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
+import com.intellij.openapi.extensions.ExtensionPointListener
+import com.intellij.openapi.extensions.PluginDescriptor
+import com.intellij.openapi.project.Project
 
 @Service(Service.Level.PROJECT)
-public final class ProjectTopHitCache extends TopHitCache {
-  public static TopHitCache getInstance(@NotNull Project project) {
-    return project.getService(ProjectTopHitCache.class);
+class ProjectTopHitCache(project: Project) : TopHitCache() {
+  companion object {
+    @JvmStatic
+    fun getInstance(project: Project): TopHitCache = project.service<ProjectTopHitCache>()
   }
 
-  public ProjectTopHitCache(@NotNull Project project) {
-    OptionsTopHitProvider.PROJECT_LEVEL_EP.addExtensionPointListener(new ExtensionPointListener<>() {
-      @Override
-      public void extensionRemoved(@NotNull OptionsSearchTopHitProvider.ProjectLevelProvider extension,
-                                   @NotNull PluginDescriptor pluginDescriptor) {
-        invalidateCachedOptions(extension.getClass());
+  init {
+    OptionsTopHitProvider.PROJECT_LEVEL_EP.addExtensionPointListener(object : ExtensionPointListener<ProjectLevelProvider?> {
+      override fun extensionRemoved(extension: ProjectLevelProvider, pluginDescriptor: PluginDescriptor) {
+        invalidateCachedOptions(extension.javaClass)
       }
-    }, project);
+    }, project)
   }
 }
