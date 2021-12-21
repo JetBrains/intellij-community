908d36862fa94f3137c5f13bc762b82463b33998
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2021-12-21 11:35:28 +0300

    [Kotlin][MPP] Fixup MultiModuleLineMarkerTestGenerated after expect/actual marker changes
    ^KT-50120
    
    GitOrigin-RevId: 3a34f8c6ad63bf4c19af0c42ac9ebcbbfd780187

--- plugins/kotlin/idea/tests/testData/multiModuleLineMarker/fromExpectedSealedClass/common/common.kt
+++ plugins/kotlin/idea/tests/testData/multiModuleLineMarker/fromExpectedSealedClass/common/common.kt
@@ -1,9 +1,9 @@
-expect sealed class <!LINE_MARKER("descr='Has actuals in JVM'"), LINE_MARKER("descr='Is subclassed by Sealed1 in Sealed [jvm] Sealed1 [common] Sealed2 in Sealed [jvm] Sealed2 [common]  Click or press ... to navigate'")!>Sealed<!> {
+expect sealed class <!LINE_MARKER("descr='Has actuals in jvm module'"), LINE_MARKER("descr='Is subclassed by Sealed1 in Sealed [jvm] Sealed1 [common] Sealed2 in Sealed [jvm] Sealed2 [common]  Click or press ... to navigate'")!>Sealed<!> {
 
-    object <!LINE_MARKER("descr='Has actuals in JVM'")!>Sealed1<!> : Sealed
+    object <!LINE_MARKER("descr='Has actuals in jvm module'")!>Sealed1<!> : Sealed
 
-    class <!LINE_MARKER("descr='Has actuals in JVM'")!>Sealed2<!> : Sealed {
-        val <!LINE_MARKER("descr='Has actuals in JVM'")!>x<!>: Int
-        fun <!LINE_MARKER("descr='Has actuals in JVM'")!>foo<!>(): String
+    class <!LINE_MARKER("descr='Has actuals in jvm module'")!>Sealed2<!> : Sealed {
+        val <!LINE_MARKER("descr='Has actuals in jvm module'")!>x<!>: Int
+        fun <!LINE_MARKER("descr='Has actuals in jvm module'")!>foo<!>(): String
     }
 }
