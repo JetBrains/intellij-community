16b04a4726329c8eddff64879163e501f13e27a9
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2021-12-21 08:37:39 +0100

    android - reduce JAR file count, no need to use pack into separate JARs
    Change-Id: I70b77e36e1e918aecd159d46ffcf45fc3f1e04f9

--- build/groovy/org/jetbrains/intellij/build/AndroidStudioProperties.groovy
+++ build/groovy/org/jetbrains/intellij/build/AndroidStudioProperties.groovy
@@ -102,7 +102,7 @@ class AndroidStudioProperties extends BaseIdeaProperties {
     productLayout.prepareCustomPluginRepositoryForPublishedPlugins = false
     productLayout.buildAllCompatiblePlugins = false
 
-    List<PluginLayout> inheritedPluginLayouts = CommunityRepositoryModules.COMMUNITY_REPOSITORY_PLUGINS
+    List<PluginLayout> inheritedPluginLayouts = new ArrayList<>(CommunityRepositoryModules.COMMUNITY_REPOSITORY_PLUGINS)
     // Remove plugin layouts that reference modules that do not exist in our fork.
     inheritedPluginLayouts.removeAll {
       it.mainModule in EXCLUDED_PLUGINS || it.mainModule == "intellij.python.community.plugin"
