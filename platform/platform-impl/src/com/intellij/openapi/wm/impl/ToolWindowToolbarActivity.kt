d2f9bf931a58f73cad7eea83578317665eb5ff9b
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2021-12-21 16:54:26 +0100

    get rid of ToolwindowToolbarListener start-up activity
    GitOrigin-RevId: bd546f71ec6107bf462b111739eee74e563ae1af

--- platform/platform-impl/src/com/intellij/openapi/wm/impl/ToolWindowToolbarActivity.kt
+++ platform/platform-impl/src/com/intellij/openapi/wm/impl/ToolWindowToolbarActivity.kt
@@ -1,33 +1,25 @@
-// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.openapi.wm.impl
 
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.startup.StartupActivity
-import com.intellij.ui.ExperimentalUI
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.openapi.wm.ex.ToolWindowManagerListener
+import com.intellij.ui.ExperimentalUI
 
-class ToolWindowToolbarActivity : StartupActivity {
-  override fun runActivity(project: Project) {
-    project.messageBus.connect(project).subscribe(ToolWindowManagerListener.TOPIC, ToolwindowToolbarListener(project))
-  }
-}
-
-class ToolwindowToolbarListener(val project: Project) : ToolWindowManagerListener {
-
+private class ToolwindowToolbarListener(val project: Project) : ToolWindowManagerListener {
   override fun stateChanged(toolWindowManager: ToolWindowManager) {
-    if (!ExperimentalUI.isNewToolWindowsStripes()) return
-
-    if (toolWindowManager is ToolWindowManagerImpl) {
-      toolWindowManager.updateSquareButtons()
+    if (!ExperimentalUI.isNewToolWindowsStripes() || toolWindowManager !is ToolWindowManagerImpl) {
+      return
     }
+    toolWindowManager.updateSquareButtons()
   }
 
   override fun toolWindowShown(toolWindow: ToolWindow) {
-    if (!ExperimentalUI.isNewToolWindowsStripes()) return
+    if (!ExperimentalUI.isNewToolWindowsStripes() || toolWindow.isVisibleOnLargeStripe) {
+      return
+    }
 
-    if (toolWindow.isVisibleOnLargeStripe) return
     toolWindow.isVisibleOnLargeStripe = true
     toolWindow.largeStripeAnchor = toolWindow.anchor
   }
