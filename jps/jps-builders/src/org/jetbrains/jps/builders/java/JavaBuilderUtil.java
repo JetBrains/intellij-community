d7856c49bb9c45ce133568308ed205a2558cc28b
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-12-23 19:14:39 +0100

    Deprecate `org.jetbrains.jps.cmdline.BuildRunner#runBuild` parameter
    Since correct API haven't been existing we have to postpone deprecation for other
    ConstantAffectionResolver usages.
    
    GitOrigin-RevId: 3720dcda987bf2feea92ff4700a4b06c4816afd5

--- jps/jps-builders/src/org/jetbrains/jps/builders/java/JavaBuilderUtil.java
+++ jps/jps-builders/src/org/jetbrains/jps/builders/java/JavaBuilderUtil.java
@@ -46,7 +46,7 @@ public final class JavaBuilderUtil {
    * @deprecated This functionality is obsolete and is not used by dependency analysis anymore. To be removed in future releases
    */
   @Deprecated
-  @ApiStatus.ScheduledForRemoval(inVersion = "2021.3")
+  @ApiStatus.ScheduledForRemoval(inVersion = "2022.3")
   public static final Key<Callbacks.ConstantAffectionResolver> CONSTANT_SEARCH_SERVICE = Key.create("_constant_search_service_");
 
   private static final Logger LOG = Logger.getInstance(Builder.class);
