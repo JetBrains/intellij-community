af18300a525f88998d004e0771847a0eb9a4570b
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2021-12-23 17:33:32 +0300

    [plugins] MemoryFileSystem leak fixed
    verification rule added
    
    (cherry picked from commit 2b14b66e37b12646d9a3def9886f113812d7f7c7)
    
    IJ-CR-19056
    
    GitOrigin-RevId: 06138acecb4d7733deaa32b9a0e05937590094bc

--- platform/platform-tests/testSrc/com/intellij/ide/plugins/PerProjectPluginsTest.kt
+++ platform/platform-tests/testSrc/com/intellij/ide/plugins/PerProjectPluginsTest.kt
@@ -1,28 +1,44 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.plugins
 
+import com.intellij.openapi.project.ex.ProjectEx
 import com.intellij.testFramework.EdtRule
 import com.intellij.testFramework.ProjectRule
 import com.intellij.testFramework.RuleChain
 import com.intellij.testFramework.RunsInEdt
-import com.intellij.testFramework.assertions.Assertions.assertThat
 import com.intellij.testFramework.rules.InMemoryFsRule
 import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.Verifier
 import kotlin.test.assertFalse
+import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
 @RunsInEdt
 class PerProjectPluginsTest {
 
-  private val inMemoryFsRule = InMemoryFsRule()
   private val projectRule = ProjectRule()
+  private val project: ProjectEx
+    get() = projectRule.project
+
+  private val verifierRule = object : Verifier() {
+    override fun verify() {
+      val pluginTracker = DynamicPluginEnabler.findPluginTracker(project)
+      assertNotNull(pluginTracker)
+
+      assertTrue(pluginTracker.disabledPluginsIds.isEmpty())
+      assertTrue(pluginTracker.enabledPluginsIds.isEmpty())
+    }
+  }
+
+  private val inMemoryFsRule = InMemoryFsRule()
 
   @Rule
   @JvmField
   val chain = RuleChain(
-    inMemoryFsRule,
     projectRule,
+    verifierRule,
+    inMemoryFsRule,
     EdtRule(),
   )
 
@@ -34,10 +50,11 @@ class PerProjectPluginsTest {
       .build(path)
 
     val descriptor = loadDescriptorInTest(path)
-    assertThat(descriptor).isNotNull
+    assertNotNull(descriptor)
+    val pluginId = descriptor.pluginId
 
-    val project = projectRule.project
-    val pluginEnabler = PluginEnabler.getInstance() as DynamicPluginEnabler
+    val pluginEnabler = PluginEnabler.getInstance() as? DynamicPluginEnabler
+    assertNotNull(pluginEnabler)
 
     val loaded = pluginEnabler.updatePluginsState(
       listOf(descriptor),
@@ -46,7 +63,7 @@ class PerProjectPluginsTest {
     )
     assertTrue(loaded)
     assertRestartIsNotRequired()
-    assertFalse(PluginManagerCore.isDisabled(descriptor.pluginId))
+    assertFalse(PluginManagerCore.isDisabled(pluginId))
     assertTrue(PluginManagerCore.getLoadedPlugins().contains(descriptor))
 
     val unloaded = pluginEnabler.updatePluginsState(
@@ -56,8 +73,11 @@ class PerProjectPluginsTest {
     )
     assertTrue(unloaded)
     assertRestartIsNotRequired()
-    assertFalse(PluginManagerCore.isDisabled(descriptor.pluginId))
+    assertFalse(PluginManagerCore.isDisabled(pluginId))
     assertFalse(PluginManagerCore.getLoadedPlugins().contains(descriptor))
+
+    pluginEnabler.getPluginTracker(project)
+      .stopTracking(listOf(pluginId))
   }
 
   private fun assertRestartIsNotRequired() {
