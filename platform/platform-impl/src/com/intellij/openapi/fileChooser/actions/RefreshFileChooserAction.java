7e5dc7b9cbb8856c8e606af0a195bce85102e91a
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2021-12-24 00:27:18 +0100

    [ui] adapting "refresh" and "show hidden" actions to the new file chooser (IDEA-101218)
    GitOrigin-RevId: 59d1602f964b5077bf4f1ca8e3831737eacddbf4

--- platform/platform-impl/src/com/intellij/openapi/fileChooser/actions/RefreshFileChooserAction.java
+++ platform/platform-impl/src/com/intellij/openapi/fileChooser/actions/RefreshFileChooserAction.java
@@ -4,12 +4,21 @@ package com.intellij.openapi.fileChooser.actions;
 import com.intellij.ide.lightEdit.LightEditCompatible;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ModalityState;
+import com.intellij.openapi.fileChooser.FileChooserPanel;
 import com.intellij.openapi.fileChooser.FileSystemTree;
 import com.intellij.openapi.vfs.newvfs.ManagingFS;
 import com.intellij.openapi.vfs.newvfs.RefreshQueue;
 import org.jetbrains.annotations.NotNull;
 
 public class RefreshFileChooserAction extends FileChooserAction implements LightEditCompatible {
+  @Override
+  protected void update(@NotNull FileChooserPanel panel, @NotNull AnActionEvent e) { }
+
+  @Override
+  protected void actionPerformed(@NotNull FileChooserPanel panel, @NotNull AnActionEvent e) {
+    panel.reload();
+  }
+
   @Override
   protected void update(@NotNull FileSystemTree fileChooser, @NotNull AnActionEvent e) { }
 
--- platform/platform-impl/src/com/intellij/openapi/fileChooser/actions/ToggleVisibilityAction.java
+++ platform/platform-impl/src/com/intellij/openapi/fileChooser/actions/ToggleVisibilityAction.java
@@ -3,10 +3,23 @@ package com.intellij.openapi.fileChooser.actions;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.Toggleable;
+import com.intellij.openapi.fileChooser.FileChooserPanel;
 import com.intellij.openapi.fileChooser.FileSystemTree;
 import org.jetbrains.annotations.NotNull;
 
 public final class ToggleVisibilityAction extends FileChooserAction implements Toggleable {
+  @Override
+  protected void update(@NotNull FileChooserPanel panel, @NotNull AnActionEvent e) {
+    Toggleable.setSelected(e.getPresentation(), panel.showHiddenFiles());
+  }
+
+  @Override
+  protected void actionPerformed(@NotNull FileChooserPanel panel, @NotNull AnActionEvent e) {
+    boolean state = !panel.showHiddenFiles();
+    panel.showHiddenFiles(state);
+    Toggleable.setSelected(e.getPresentation(), state);
+  }
+
   @Override
   protected void update(@NotNull FileSystemTree fileChooser, @NotNull AnActionEvent e) {
     Toggleable.setSelected(e.getPresentation(), fileChooser.areHiddensShown());
