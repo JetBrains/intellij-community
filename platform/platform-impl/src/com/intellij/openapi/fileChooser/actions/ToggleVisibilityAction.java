347f5f46433e28a39ee4cd31f24284f67f27a366
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2021-12-23 15:08:06 +0100

    [ui] reimplementing "show hidden files" chooser action on top of the common base class
    GitOrigin-RevId: ecff7faff697c525acfa62b82674666a850dd959

---
+++ platform/platform-impl/src/com/intellij/openapi/fileChooser/actions/ToggleVisibilityAction.java
@@ -0,0 +1,21 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.openapi.fileChooser.actions;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.Toggleable;
+import com.intellij.openapi.fileChooser.FileSystemTree;
+import org.jetbrains.annotations.NotNull;
+
+public final class ToggleVisibilityAction extends FileChooserAction implements Toggleable {
+  @Override
+  protected void update(@NotNull FileSystemTree fileChooser, @NotNull AnActionEvent e) {
+    Toggleable.setSelected(e.getPresentation(), fileChooser.areHiddensShown());
+  }
+
+  @Override
+  protected void actionPerformed(@NotNull FileSystemTree fileChooser, @NotNull AnActionEvent e) {
+    boolean state = !fileChooser.areHiddensShown();
+    fileChooser.showHiddens(state);
+    Toggleable.setSelected(e.getPresentation(), state);
+  }
+}
