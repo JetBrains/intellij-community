f445757e7690b74d218f99954bc97d615e65a3e3
Leonid Shalupov <leonid@shalupov.com>
2021-12-25 22:03:59 +0100

    buildscripts: download Android Studio for android tests without Gradle
    GitOrigin-RevId: 07ef487a24f627acb6b0285db16378391de68c71

---
+++ platform/build-scripts/downloader/groovy/org/jetbrains/intellij/build/dependencies/BuildDependenciesConstants.java
@@ -0,0 +1,6 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.intellij.build.dependencies;
+
+public interface BuildDependenciesConstants {
+  String INTELLIJ_DEPENDENCIES_URL = "https://cache-redirector.jetbrains.com/intellij-dependencies";
+}
