b27935c747899ee7d7f5dbccf0b6909d1ac4b374
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2021-12-27 16:42:45 +0700

    [java-inspections] IDEA-285742 New inspection: replacement method has no effect
    GitOrigin-RevId: 708af15324387475e434e2e0c87fe2d26ceac1ef

---
+++ java/java-tests/testSrc/com/intellij/java/codeInsight/daemon/quickFix/ReplaceOnLiteralHasNoEffectInspectionTest.java
@@ -0,0 +1,27 @@
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.java.codeInsight.daemon.quickFix;
+
+import com.intellij.JavaTestUtil;
+import com.intellij.codeInspection.InspectionProfileEntry;
+import com.intellij.testFramework.LightProjectDescriptor;
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
+import com.siyeh.ig.LightJavaInspectionTestCase;
+import com.siyeh.ig.redundancy.RedundantStringOperationInspection;
+import com.siyeh.ig.redundancy.ReplaceOnLiteralHasNoEffectInspection;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class ReplaceOnLiteralHasNoEffectInspectionTest extends LightJavaInspectionTestCase {
+  @Nullable
+  @Override
+  protected InspectionProfileEntry getInspection() {
+    return new ReplaceOnLiteralHasNoEffectInspection();
+  }
+
+  public void testReplaceOnLiteral() {doTest();}
+
+  @Override
+  protected String getBasePath() {
+    return JavaTestUtil.getRelativeJavaTestDataPath() + "/inspection/replaceOnLiteral/";
+  }
+}
