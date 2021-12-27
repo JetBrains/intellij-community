f2bde7c0e3b4a57a5e27179e3d4f5bdbf45b0474
Ivan Posti <ivan.posti@jetbrains.com>
2021-12-27 11:35:40 +0300

    [Markdown] Add notification after cleaning up extensions external files
    GitOrigin-RevId: 4d090501da1e04ce3b21ebca84f8e77c7b9ac3be

--- plugins/markdown/core/src/org/intellij/plugins/markdown/extensions/CleanupExtensionsExternalFilesAction.kt
+++ plugins/markdown/core/src/org/intellij/plugins/markdown/extensions/CleanupExtensionsExternalFilesAction.kt
@@ -1,17 +1,29 @@
 package org.intellij.plugins.markdown.extensions
 
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationType
+import com.intellij.notification.Notifications
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.progress.runModalTask
+import org.intellij.plugins.markdown.MarkdownBundle
 
 internal class CleanupExtensionsExternalFilesAction: AnAction() {
   override fun actionPerformed(event: AnActionEvent) {
-    runModalTask("Removing Extensions External Files", event.project, cancellable = false) {
+    runModalTask(MarkdownBundle.message("Markdown.Extensions.CleanupExternalFiles.task.title"), event.project, cancellable = false) {
       val extensions = MarkdownExtensionsUtil.collectExtensionsWithExternalFiles()
       val pathManager = ExtensionsExternalFilesPathManager.getInstance()
       for (extension in extensions) {
         pathManager.cleanupExternalFiles(extension)
       }
+      Notifications.Bus.notify(
+        Notification(
+          "Markdown",
+          MarkdownBundle.message("Markdown.Extensions.CleanupExternalFiles.notification.title"),
+          MarkdownBundle.message("Markdown.Extensions.CleanupExternalFiles.notification.text"),
+          NotificationType.INFORMATION
+        )
+      )
     }
   }
 }
