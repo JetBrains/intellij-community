df7824b4de24ca5c86c2d5b216d953b47a9142af
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-12-29 14:22:01 +0700

    [kotlin] move NewCodeStyleNotification to suitable package
    ^KTIJ-20169
    
    (cherry picked from commit ac50d94ff90b901c8784726b9cf566e6a46597da)
    (cherry picked from commit 65386872fb41c79d9aa99574c729075afae0b264)
    
    GitOrigin-RevId: 3c85d2138ab0503fa1a21f01eb95846d407c88a5

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/configuration/notifications/newCodeStyleNotification.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/configuration/notifications/newCodeStyleNotification.kt
@@ -1,6 +1,6 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
-package org.jetbrains.kotlin.idea.configuration.ui.notifications
+package org.jetbrains.kotlin.idea.configuration.notifications
 
 import com.intellij.application.options.CodeStyle
 import com.intellij.notification.*
