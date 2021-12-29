c02cdf4404bb5b12c87042ae5aadbabd9f983f00
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-12-29 15:12:37 +0700

    [kotlin] move migrationNotification.kt to suitable package
    ^KTIJ-20169
    
    (cherry picked from commit 6618ce5a3632e3a5880d4f182725640f2d6103a9)
    (cherry picked from commit 255084bf031e9c3921274305a24e8a3a18554fe1)
    
    GitOrigin-RevId: 6a73411a52ccad8103e8d49fc005844570fb8246

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/configuration/notifications/migrationNotification.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/configuration/notifications/migrationNotification.kt
@@ -1,6 +1,6 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
-package org.jetbrains.kotlin.idea.configuration.ui
+package org.jetbrains.kotlin.idea.configuration.notifications
 
 import com.intellij.notification.*
 import com.intellij.openapi.actionSystem.ActionManager
