036f6b5f6b94728eab3c613dbd42fdb4327fd184
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-12-30 15:26:16 +0700

    [kotlin] newCodeStyleNotification: migrate to new notification api
    * add Kotlin logo
    
    ^KTIJ-20630 Fixed
    
    GitOrigin-RevId: c645aa9e08c30b128abb6ef64b9cd7ea72adee65

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/configuration/notifications/newCodeStyleNotification.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/configuration/notifications/newCodeStyleNotification.kt
@@ -3,16 +3,18 @@
 package org.jetbrains.kotlin.idea.configuration.notifications
 
 import com.intellij.application.options.CodeStyle
-import com.intellij.notification.*
+import com.intellij.ide.IdeCoreBundle
+import com.intellij.notification.NotificationAction
+import com.intellij.notification.NotificationGroupManager
+import com.intellij.notification.NotificationType
 import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.components.*
 import com.intellij.openapi.project.Project
 import org.jetbrains.kotlin.idea.KotlinBundle
+import org.jetbrains.kotlin.idea.KotlinIcons
 import org.jetbrains.kotlin.idea.formatter.KotlinStyleGuideCodeStyle
 import org.jetbrains.kotlin.idea.formatter.ProjectCodeStyleImporter
 import org.jetbrains.kotlin.idea.formatter.kotlinCodeStyleDefaults
-
-private const val KOTLIN_UPDATE_CODE_STYLE_GROUP_ID = "Update Kotlin code style"
 
 internal fun notifyKotlinStyleUpdateIfNeeded(project: Project) {
     if (CodeStyle.getSettings(project).kotlinCodeStyleDefaults() == KotlinStyleGuideCodeStyle.CODE_STYLE_ID) return
@@ -20,38 +22,39 @@ internal fun notifyKotlinStyleUpdateIfNeeded(project: Project) {
         return
     }
 
-    val notification = createNotification()
-    NotificationsConfiguration.getNotificationsConfiguration().register(
-        KOTLIN_UPDATE_CODE_STYLE_GROUP_ID,
-        NotificationDisplayType.STICKY_BALLOON,
-        true
-    )
-
-    notification.notify(project)
+    NotificationGroupManager.getInstance()
+        .getNotificationGroup("Update Kotlin code style")
+        .createNotification(
+            KotlinBundle.message("configuration.kotlin.code.style"),
+            KotlinBundle.htmlMessage("configuration.notification.update.code.style.to.official"),
+            NotificationType.WARNING,
+        )
+        .addAction(applyCodeStyleAction())
+        .addAction(dontAskAgainAction())
+        .setImportant(true)
+        .setIcon(KotlinIcons.SMALL_LOGO)
+        .notify(project)
 }
 
-private fun createNotification(): Notification =
-    Notification(
-        KOTLIN_UPDATE_CODE_STYLE_GROUP_ID,
-        KotlinBundle.message("configuration.kotlin.code.style"),
-        KotlinBundle.htmlMessage("configuration.notification.update.code.style.to.official"),
-        NotificationType.WARNING
-    )
-        .addAction(NotificationAction.createExpiring(KotlinBundle.message("configuration.apply.new.code.style")) { e, _ ->
-            e.project?.takeIf { !it.isDisposed }?.let { project ->
-                runWriteAction {
-                    ProjectCodeStyleImporter.apply(project, KotlinStyleGuideCodeStyle.INSTANCE)
-                }
-            }
-        })
-        .addAction(NotificationAction.createExpiring(KotlinBundle.message("configuration.do.not.suggest.new.code.style")) { e, _ ->
-            e.project?.takeIf { !it.isDisposed }?.let { project ->
-                runWriteAction {
-                    SuppressKotlinCodeStyleComponent.getInstance(project).state.disableForAll = true
-                }
-            }
-        })
-        .setImportant(true)
+private fun dontAskAgainAction() = NotificationAction.createExpiring(
+    IdeCoreBundle.message("dialog.options.do.not.ask")
+) { e, _ ->
+    e.project?.takeIf { !it.isDisposed }?.let { project ->
+        runWriteAction {
+            SuppressKotlinCodeStyleComponent.getInstance(project).state.disableForAll = true
+        }
+    }
+}
+
+private fun applyCodeStyleAction() = NotificationAction.createExpiring(
+    KotlinBundle.message("configuration.apply.new.code.style")
+) { e, _ ->
+    e.project?.takeIf { !it.isDisposed }?.let { project ->
+        runWriteAction {
+            ProjectCodeStyleImporter.apply(project, KotlinStyleGuideCodeStyle.INSTANCE)
+        }
+    }
+}
 
 class SuppressKotlinCodeStyleState : BaseState() {
     var disableForAll by property(false)
