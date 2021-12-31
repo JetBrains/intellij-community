d531ec971c72823c5457a9ae5228de2018646699
George Leung <phisgr@gmail.com>
2021-12-31 16:49:02 -0500

    space between string and lambda

--- plugins/kotlin/idea/tests/testData/formatter/SpaceBeforeFunctionLiteral.kt
+++ plugins/kotlin/idea/tests/testData/formatter/SpaceBeforeFunctionLiteral.kt
@@ -1,6 +1,12 @@
 fun test(some : (Int) -> Int) {
 }
 
+fun String.invoke(action: (String) -> Unit) {
+    action(this)
+}
+
 fun foo() {
-    test() { it }
+    test(){ it }
+    "foo"{ println(it) }
+    "bar"  { println(it) }
 }
