1546cd41fd065535bc6e6f9b1ef7edf3e5052882
George Leung <phisgr@gmail.com>
2021-12-31 17:04:24 -0500

    test multiline lambda

--- plugins/kotlin/idea/tests/testData/formatter/SpaceBeforeFunctionLiteral.after.kt
+++ plugins/kotlin/idea/tests/testData/formatter/SpaceBeforeFunctionLiteral.after.kt
@@ -1,12 +1,14 @@
 fun test(some: (Int) -> Int) {
 }
 
-fun String.invoke(action: (String) -> Unit) {
+operator fun String.invoke(action: (String) -> Unit) {
     action(this)
 }
 
 fun foo() {
     test() { it }
     "foo" { println(it) }
-    "bar" { println(it) }
+    "bar" {
+        println(it)
+    }
 }
