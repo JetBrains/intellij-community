8a64253e8dc9ad3c09bb1b50b5c231a18a3c5d69
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2021-12-31 14:15:36 +0100

    IDEA-285792 implement main coroutine dispatcher
    - subclass `MainCoroutineDispatcher` in `EdtCoroutineDispatcher`;
    - register `EdtCoroutineDispatcher` as main via `MainDispatcherFactory`.
    
    GitOrigin-RevId: 18101ad18c424bb51733fe78350eb7dba2c33d54

--- platform/core-impl/src/com/intellij/openapi/application/impl/EdtCoroutineDispatcher.kt
+++ platform/core-impl/src/com/intellij/openapi/application/impl/EdtCoroutineDispatcher.kt
@@ -3,11 +3,15 @@ package com.intellij.openapi.application.impl
 
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.ModalityState
-import kotlinx.coroutines.CoroutineDispatcher
+import com.intellij.util.ui.EDT
+import kotlinx.coroutines.MainCoroutineDispatcher
+import kotlinx.coroutines.Runnable
 import kotlinx.coroutines.job
 import kotlin.coroutines.CoroutineContext
 
-internal object EdtCoroutineDispatcher : CoroutineDispatcher() {
+internal sealed class EdtCoroutineDispatcher : MainCoroutineDispatcher() {
+
+  override val immediate: MainCoroutineDispatcher get() = Immediate
 
   override fun dispatch(context: CoroutineContext, block: Runnable) {
     val state = context[ModalityStateElement]?.modalityState
@@ -19,5 +23,25 @@ internal object EdtCoroutineDispatcher : CoroutineDispatcher() {
       DispatchedRunnable(context.job, block)
     }
     ApplicationManager.getApplication().invokeLater(runnable, state)
+  }
+
+  companion object : EdtCoroutineDispatcher() {
+
+    override fun toString() = "EDT"
+  }
+
+  object Immediate : EdtCoroutineDispatcher() {
+
+    override fun isDispatchNeeded(context: CoroutineContext): Boolean {
+      if (!EDT.isCurrentThreadEdt()) {
+        return true
+      }
+      // The current coroutine is executed with the correct modality state
+      // (the execution would be postponed otherwise)
+      // => there is no need to check modality state here.
+      return false
+    }
+
+    override fun toString() = "EDT.immediate"
   }
 }
