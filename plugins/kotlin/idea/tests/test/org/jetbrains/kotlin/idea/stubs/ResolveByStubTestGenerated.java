4e48fc94d306997ab5ad805b2a0fef604978a3ec
Jinseong Jeon <jsjeon@google.com>
2022-01-04 14:28:48 -0800

    Regenerate Kotlin tests
    GitOrigin-RevId: 48afb6d1593b6e92e3ae4cc1cc495171e8c1bfaf

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/stubs/ResolveByStubTestGenerated.java
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/stubs/ResolveByStubTestGenerated.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package org.jetbrains.kotlin.idea.stubs;
 
@@ -2560,6 +2560,11 @@ public abstract class ResolveByStubTestGenerated extends AbstractResolveByStubTe
         @TestMetadata("ExtVarl.kt")
         public void testExtVarl() throws Exception {
             runTest(compilerTestData("compiler/testData/loadJava/compiledKotlin/prop/ExtVarl.kt"));
+        }
+
+        @TestMetadata("nonConstValWithConstantValueAttribute.kt")
+        public void testNonConstValWithConstantValueAttribute() throws Exception {
+            runTest(compilerTestData("compiler/testData/loadJava/compiledKotlin/prop/nonConstValWithConstantValueAttribute.kt"));
         }
 
         @TestMetadata("NsVal.kt")
