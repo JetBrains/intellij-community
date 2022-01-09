348ef79f7b53e93277af5c1a93c43c86634b3dc1
Leonid Shalupov <leonid@shalupov.com>
2022-01-09 19:57:59 +0300

    build-scripts: provide gzipped metadata.json for inc. compilation results
    it's 2x smaller than full .json
    
    GitOrigin-RevId: 98663c95684ad3fb4c8209502d69c1d8aebce1a8

--- platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/compilation/CompilationPartsUtil.groovy
+++ platform/build-scripts/groovy/org/jetbrains/intellij/build/impl/compilation/CompilationPartsUtil.groovy
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.intellij.build.impl.compilation
 
 import com.google.gson.Gson
@@ -45,6 +45,7 @@ import java.util.concurrent.atomic.AtomicInteger
 import java.util.concurrent.atomic.AtomicLong
 import java.util.function.Consumer
 import java.util.function.Predicate
+import java.util.zip.GZIPOutputStream
 
 @CompileStatic
 class CompilationPartsUtil {
@@ -279,12 +280,18 @@ class CompilationPartsUtil {
 
     executor.reportErrors(messages)
 
-
     // Save and publish metadata file
     def metadataFile = new File("$zipsLocation/metadata.json")
     FileUtil.writeToFile(metadataFile, metadataJson)
-
     messages.artifactBuilt(metadataFile.absolutePath)
+
+    def gzippedMetadataFile = new File(zipsLocation, "metadata.json.gz")
+    new GZIPOutputStream(gzippedMetadataFile.newOutputStream()).withCloseable { OutputStream outputStream ->
+      metadataFile.newInputStream().withCloseable { InputStream inputStream ->
+        FileUtil.copy(inputStream, outputStream)
+      }
+    }
+    messages.artifactBuilt(gzippedMetadataFile.absolutePath)
   }
 
   static void fetchAndUnpackCompiledClasses(BuildMessages messages, File classesOutput, BuildOptions options) {
