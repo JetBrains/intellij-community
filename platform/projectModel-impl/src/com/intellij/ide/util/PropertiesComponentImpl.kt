26af8c0c5ceb2a14be9b81d409f1c93986658ca3
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-01-09 10:41:16 +0100

    cleanup
    GitOrigin-RevId: b2baf7c467f7b2ea54e65f20211403433ff78514

--- platform/projectModel-impl/src/com/intellij/ide/util/PropertiesComponentImpl.kt
+++ platform/projectModel-impl/src/com/intellij/ide/util/PropertiesComponentImpl.kt
@@ -1,153 +1,129 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.ide.util;
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+@file:Suppress("ReplacePutWithAssignment", "ReplaceGetOrSet")
 
-import com.intellij.openapi.components.PersistentStateComponent;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.util.text.StringUtil;
-import org.jdom.Element;
-import org.jdom.Verifier;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+package com.intellij.ide.util
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
+import com.intellij.openapi.components.*
+import com.intellij.openapi.diagnostic.logger
+import org.jdom.Element
+import org.jdom.Verifier
+import org.jetbrains.annotations.NonNls
+import java.util.concurrent.ConcurrentHashMap
 
-public class PropertiesComponentImpl extends PropertiesComponent implements PersistentStateComponent<Element> {
-  private static final Logger LOG = Logger.getInstance(PropertiesComponentImpl.class);
+private val LOG = logger<PropertiesComponentImpl>()
 
-  private final Map<@NonNls String, @NonNls String> myMap = new ConcurrentHashMap<>();
+private const val ELEMENT_PROPERTY = "property"
+private const val ATTRIBUTE_NAME = "name"
+private const val ATTRIBUTE_VALUE = "value"
 
-  @NonNls private static final String ELEMENT_PROPERTY = "property";
-  @NonNls private static final String ATTRIBUTE_NAME = "name";
-  @NonNls private static final String ATTRIBUTE_VALUE = "value";
+open class PropertiesComponentImpl internal constructor() : PropertiesComponent(), PersistentStateComponent<Element?> {
+  private val keyToValue = ConcurrentHashMap<String, String>()
 
-  PropertiesComponentImpl() {
-  }
+  val keys: Set<String>
+    get() = keyToValue.keys
 
-  private void doPut(@NotNull String key, @NotNull String value) {
-    String reason = Verifier.checkCharacterData(key);
-    if (reason != null) {
-      LOG.error(reason);
+  private fun doPut(key: String, value: String) {
+    Verifier.checkCharacterData(key)?.let(LOG::error)
+    if (keyToValue.put(key, value) !== value) {
+      incModificationCount()
     }
-    myMap.put(key, value);
-    incModificationCount();
   }
 
-  @Override
-  public Element getState() {
-    Element parentNode = new Element("state");
-    List<String> keys = new ArrayList<>(myMap.keySet());
-    keys.sort(null);
-    for (final String key : keys) {
-      String value = myMap.get(key);
+  override fun getState(): Element? {
+    val parentNode = Element("state")
+    val keys = ArrayList<String>(keyToValue.keys)
+    keys.sort()
+    for (key in keys) {
+      val value = keyToValue.get(key)
       if (value != null) {
-        Element element = new Element(ELEMENT_PROPERTY);
-        element.setAttribute(ATTRIBUTE_NAME, key);
-        element.setAttribute(ATTRIBUTE_VALUE, value);
-        parentNode.addContent(element);
+        val element = Element(ELEMENT_PROPERTY)
+        element.setAttribute(ATTRIBUTE_NAME, key)
+        element.setAttribute(ATTRIBUTE_VALUE, value)
+        parentNode.addContent(element)
       }
     }
-    return parentNode;
+    return parentNode
   }
 
-  @Override
-  public void loadState(final @NotNull Element parentNode) {
-    myMap.clear();
-    for (Element e : parentNode.getChildren(ELEMENT_PROPERTY)) {
-      String name = e.getAttributeValue(ATTRIBUTE_NAME);
-      if (name != null) {
-        myMap.put(name, e.getAttributeValue(ATTRIBUTE_VALUE));
-      }
+  override fun loadState(parentNode: Element) {
+    keyToValue.clear()
+    for (e in parentNode.getChildren(ELEMENT_PROPERTY)) {
+      val name = e.getAttributeValue(ATTRIBUTE_NAME) ?: continue
+      keyToValue.put(name, e.getAttributeValue(ATTRIBUTE_VALUE) ?: continue)
     }
   }
 
-  @Override
-  public String getValue(@NotNull String name) {
-    return myMap.get(name);
-  }
+  override fun getValue(name: String): String? = keyToValue.get(name)
 
-  @Override
-  public void setValue(@NotNull String name, @Nullable String value) {
+  override fun setValue(name: String, value: String?) {
     if (value == null) {
-      unsetValue(name);
+      unsetValue(name)
     }
     else {
-      doPut(name, value);
+      doPut(name, value)
     }
   }
 
-  @Override
-  public void setValue(@NotNull String name, @Nullable String value, @Nullable String defaultValue) {
-    if (value == null || value.equals(defaultValue)) {
-      unsetValue(name);
+  override fun setValue(name: String, value: String?, defaultValue: String?) {
+    if (value == null || value == defaultValue) {
+      unsetValue(name)
     }
     else {
-      doPut(name, value);
+      doPut(name, value)
     }
   }
 
-  @Override
-  public void setValue(@NotNull String name, float value, float defaultValue) {
+  override fun setValue(name: String, value: Float, defaultValue: Float) {
     if (value == defaultValue) {
-      unsetValue(name);
+      unsetValue(name)
     }
     else {
-      doPut(name, String.valueOf(value));
+      doPut(name, value.toString())
     }
   }
 
-  @Override
-  public void setValue(@NotNull String name, int value, int defaultValue) {
+  override fun setValue(name: String, value: Int, defaultValue: Int) {
     if (value == defaultValue) {
-      unsetValue(name);
+      unsetValue(name)
     }
     else {
-      doPut(name, String.valueOf(value));
+      doPut(name, value.toString())
     }
   }
 
-  @Override
-  public void setValue(@NotNull String name, boolean value, boolean defaultValue) {
+  override fun setValue(name: String, value: Boolean, defaultValue: Boolean) {
     if (value == defaultValue) {
-      unsetValue(name);
+      unsetValue(name)
     }
     else {
-      setValue(name, String.valueOf(value));
+      setValue(name, value.toString())
     }
   }
 
-  @Override
-  public void unsetValue(@NotNull String name) {
-    myMap.remove(name);
-    incModificationCount();
+  override fun unsetValue(name: String) {
+    if (keyToValue.remove(name) != null) {
+      incModificationCount()
+    }
   }
 
-  @Override
-  public boolean isValueSet(@NotNull String name) {
-    return myMap.containsKey(name);
+  override fun isValueSet(name: String) = keyToValue.containsKey(name)
+
+  override fun getValues(name: @NonNls String): Array<String>? {
+    return getValue(name)?.split("\n")?.dropLastWhile { it.isEmpty() }?.toTypedArray()
   }
 
-  @Override
-  public String @Nullable [] getValues(@NotNull @NonNls String name) {
-    final String value = getValue(name);
-    return value != null ? value.split("\n") : null;
-  }
-
-  @Override
-  public void setValues(@NotNull @NonNls String name, String[] values) {
+  override fun setValues(name: @NonNls String, values: Array<String>?) {
     if (values == null) {
-      setValue(name, null);
+      setValue(name, null)
     }
     else {
-      setValue(name, StringUtil.join(values, "\n"));
+      setValue(name, values.joinToString(separator = "\n"))
     }
-  }
-
-  public Set<String> getKeys() {
-    return myMap.keySet();
   }
 }
+
+@State(name = "PropertiesComponent", storages = [Storage(StoragePathMacros.WORKSPACE_FILE)])
+internal class ProjectPropertiesComponentImpl : PropertiesComponentImpl()
+
+@State(name = "PropertiesComponent", storages = [Storage(value = StoragePathMacros.NON_ROAMABLE_FILE, roamingType = RoamingType.DISABLED)])
+internal class AppPropertiesComponentImpl : PropertiesComponentImpl()
