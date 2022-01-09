ef29dbd18139be06b2c4758fe51b8e03d0211c09
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2022-01-09 14:19:49 +0300

    [vcs-log] cleanup DefaultColorGenerator
    GitOrigin-RevId: cb2f78fba95aa5f56aebc911355ff7c3eb3ae18e

--- platform/vcs-log/impl/src/com/intellij/vcs/log/graph/DefaultColorGenerator.kt
+++ platform/vcs-log/impl/src/com/intellij/vcs/log/graph/DefaultColorGenerator.kt
@@ -1,46 +1,39 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.vcs.log.graph
 
 import com.intellij.ui.JBColor
 import com.intellij.vcs.log.paint.ColorGenerator
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap
 import java.awt.Color
+import java.util.function.IntFunction
 import kotlin.math.abs
 
 /**
  * @author erokhins
  */
 class DefaultColorGenerator : ColorGenerator {
-  override fun getColor(branchNumber: Int): JBColor {
-    var color = ourColorMap[branchNumber]
-    if (color == null) {
-      color = calcColor(branchNumber)
-      ourColorMap[branchNumber] = color
-    }
-    return color
+  override fun getColor(colorId: Int): JBColor {
+    return ourColorMap.computeIfAbsent(colorId, IntFunction { calcColor(it) })
   }
 
   companion object {
-    private val ourColorMap: MutableMap<Int, JBColor> = HashMap()
-
-    init {
-      ourColorMap[GraphColorManagerImpl.DEFAULT_COLOR] = JBColor.BLACK
+    private val ourColorMap = Int2ObjectOpenHashMap<JBColor>().apply {
+      put(GraphColorManagerImpl.DEFAULT_COLOR, JBColor.BLACK)
     }
 
-    private fun rangeFix(n: Int): Int {
-      return abs(n % 100) + 70
-    }
-
-    private fun calcColor(indexColor: Int): JBColor {
-      val r = indexColor * 200 + 30
-      val g = indexColor * 130 + 50
-      val b = indexColor * 90 + 100
+    private fun calcColor(colorId: Int): JBColor {
+      val r = colorId * 200 + 30
+      val g = colorId * 130 + 50
+      val b = colorId * 90 + 100
       return try {
         val color = Color(rangeFix(r), rangeFix(g), rangeFix(b))
         JBColor(color, color)
       }
       catch (a: IllegalArgumentException) {
-        throw IllegalArgumentException("indexColor: $indexColor ${r % 256} ${g % 256} ${b % 256}")
+        throw IllegalArgumentException("Color: $colorId ${r % 256} ${g % 256} ${b % 256}")
       }
     }
+
+    private fun rangeFix(n: Int) = abs(n % 100) + 70
   }
 }
