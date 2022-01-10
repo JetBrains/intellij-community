fbe28ebc282418fca112e752057f9194737c5d6c
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2022-01-10 12:32:38 +0700

    [kotlin] GradleConfiguratorTest: use missed `myDisposable`
    ^KTIJ-20169
    
    GitOrigin-RevId: 5ddb3c76bf2f65381530c7f7f9e5277f7011947c

--- plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/idea/codeInsight/gradle/GradleConfiguratorTest.kt
+++ plugins/kotlin/gradle/gradle-java/tests/test/org/jetbrains/kotlin/idea/codeInsight/gradle/GradleConfiguratorTest.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package org.jetbrains.kotlin.idea.codeInsight.gradle
 
@@ -54,7 +54,7 @@ class GradleConfiguratorTest : KotlinGradleImportingTestCase() {
         val counter = AtomicInteger(0)
         val myDisposable = Disposer.newDisposable()
         try {
-            val connection = myProject.messageBus.connect(testRootDisposable)
+            val connection = myProject.messageBus.connect(myDisposable)
             connection.subscribe(Notifications.TOPIC, object : Notifications {
                 override fun notify(notification: Notification) {
                     counter.incrementAndGet()
