a5422e11b37d08be0b80be0eee4dd9966ce90911
Kirill Likhodedov <Kirill.Likhodedov@jetbrains.com>
2022-01-10 15:40:52 +0300

    settings-sync: implement equals/hashCode for the data class containing byte[]
    GitOrigin-RevId: 0c1751fcf28ce1e3ec2869d8f5ef8ab730684084

--- plugins/settings-sync/src/com/intellij/settingsSync/SettingsChangeListener.kt
+++ plugins/settings-sync/src/com/intellij/settingsSync/SettingsChangeListener.kt
@@ -18,7 +18,26 @@ internal data class SettingsSnapshot(val fileStates: Set<FileState>) {
   fun isEmpty(): Boolean = fileStates.isEmpty()
 }
 
-// todo use ByteArrayWrapper
 internal data class FileState(val file: String, val content: ByteArray, val size: Int) {
   override fun toString(): String = "file='$file', content:\n${String(content, UTF_8)}"
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (javaClass != other?.javaClass) return false
+
+    other as FileState
+
+    if (file != other.file) return false
+    if (!content.contentEquals(other.content)) return false
+    if (size != other.size) return false
+
+    return true
+  }
+
+  override fun hashCode(): Int {
+    var result = file.hashCode()
+    result = 31 * result + content.contentHashCode()
+    result = 31 * result + size
+    return result
+  }
 }
