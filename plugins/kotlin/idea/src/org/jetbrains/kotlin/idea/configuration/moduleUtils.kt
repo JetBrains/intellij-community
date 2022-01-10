95916c476bb047f3c8dc44db0392606d0892f9bf
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2022-01-10 06:57:10 +0100

    [kotlin] replace redundant KotlinExternalCompilerVersionService with util extension
    ^KTIJ-20169
    
    (cherry picked from commit 774952cb7b184d1764b6e2cfd198abcb08ea2f62)
    (cherry picked from commit cb16f22c664deda7a830e985db23179979edc785)
    
    GitOrigin-RevId: b21ab5f58e5bf582877439f67b47b3763f07a978

--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/configuration/moduleUtils.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/configuration/moduleUtils.kt
@@ -1,8 +1,20 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.idea.configuration
 
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.util.Key
+import org.jetbrains.kotlin.idea.KotlinVersionVerbose
+import org.jetbrains.kotlin.idea.util.buildNumber
 import org.jetbrains.kotlin.psi.UserDataProperty
 
 var Module.externalCompilerVersion: String? by UserDataProperty(Key.create("EXTERNAL_COMPILER_VERSION"))
+
+fun Module.findExternalKotlinCompilerVersion(): KotlinVersionVerbose? {
+    val externalCompilerVersion = (if (getBuildSystemType() == BuildSystemType.JPS) {
+        buildNumber
+    } else {
+        this.externalCompilerVersion
+    }) ?: return null
+
+    return KotlinVersionVerbose.parse(externalCompilerVersion)
+}
--- plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/configuration/projectUtils.kt
+++ plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/configuration/projectUtils.kt
@@ -1,10 +1,14 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 package org.jetbrains.kotlin.idea.configuration
 
 import com.intellij.openapi.module.Module
+import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.project.Project
 import com.intellij.psi.search.FileTypeIndex
 import org.jetbrains.kotlin.idea.KotlinFileType
+import org.jetbrains.kotlin.idea.KotlinVersionVerbose
+import org.jetbrains.kotlin.util.firstNotNullResult
 
 fun hasKotlinFilesOnlyInTests(module: Module): Boolean {
     return !hasKotlinFilesInSources(module) && FileTypeIndex.containsFileOfType(KotlinFileType.INSTANCE, module.getModuleScope(true))
@@ -13,3 +17,6 @@ fun hasKotlinFilesOnlyInTests(module: Module): Boolean {
 fun hasKotlinFilesInSources(module: Module): Boolean {
     return FileTypeIndex.containsFileOfType(KotlinFileType.INSTANCE, module.getModuleScope(false))
 }
+
+fun Project.findAnyExternalKotlinCompilerVersion(): KotlinVersionVerbose? =
+    ModuleManager.getInstance(this).modules.firstNotNullResult { it.findExternalKotlinCompilerVersion() }
