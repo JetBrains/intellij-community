469a2a6af54a3cb335fefac1fb282f45252f5e05
grigoriy.myasoedov <grigoriy.myasoedov@jetbrains.com>
2022-01-11 16:05:29 +0300

    [maven] IDEA-283167 Implement Maven dependency analyzer
    MavenDependencyAnalyzerContributor
    
    GitOrigin-RevId: 5e81cd086330c0bade621cbd7ebf8278e7cfa114

--- plugins/maven/src/main/java/org/jetbrains/idea/maven/project/MavenDependencyAnalyzerContributor.kt
+++ plugins/maven/src/main/java/org/jetbrains/idea/maven/project/MavenDependencyAnalyzerContributor.kt
@@ -2,11 +2,96 @@
 package org.jetbrains.idea.maven.project
 
 import com.intellij.openapi.Disposable
-import com.intellij.openapi.externalSystem.dependency.analyzer.DummyDependencyAnalyzerContributor
+import com.intellij.openapi.externalSystem.dependency.analyzer.DependencyAnalyzerContributor
+import com.intellij.openapi.externalSystem.util.ExternalSystemBundle.message
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.openapi.vfs.LocalFileSystem
+import org.jetbrains.idea.maven.model.MavenArtifactNode
+import org.jetbrains.idea.maven.model.MavenArtifactState
+import org.jetbrains.idea.maven.model.MavenConstants
 
-class MavenDependencyAnalyzerContributor(project: Project): DummyDependencyAnalyzerContributor(project) {
-  override fun whenDataChanged(listener: () -> Unit, parentDisposable: Disposable) {
 
+class MavenDependencyAnalyzerContributor(private val project: Project) : DependencyAnalyzerContributor {
+
+  override fun whenDataChanged(listener: () -> Unit, parentDisposable: Disposable) {}
+
+  override fun getExternalProjects(): List<DependencyAnalyzerContributor.ExternalProject> {
+    return MavenProjectsManager.getInstance(project)
+      .projects
+      .map { DependencyAnalyzerContributor.ExternalProject(it.directory, it.displayName) }
   }
+
+  override fun getDependencyScopes(externalProjectPath: String): List<DependencyAnalyzerContributor.Scope> {
+    return listOf(scope(MavenConstants.SCOPE_COMPILE),
+                  scope(MavenConstants.SCOPE_PROVIDED),
+                  scope(MavenConstants.SCOPE_RUNTIME),
+                  scope(MavenConstants.SCOPE_SYSTEM),
+                  scope(MavenConstants.SCOPE_IMPORT),
+                  scope(MavenConstants.SCOPE_TEST));
+  }
+
+  override fun getDependencies(externalProjectPath: String): List<DependencyAnalyzerContributor.Dependency> {
+    return LocalFileSystem.getInstance().findFileByPath(externalProjectPath)
+             ?.let { MavenProjectsManager.getInstance(project).findContainingProject(it) }
+             ?.let { createDependencyList(it) }
+           ?: emptyList()
+  }
+
+  private fun createDependencyList(mavenProject: MavenProject): List<DependencyAnalyzerContributor.Dependency> {
+    val root = DependencyAnalyzerContributor.Dependency.Data.Module(mavenProject.displayName)
+    val rootDependency = DependencyAnalyzerContributor.Dependency(root, scope("compile"), null, emptyList())
+    val result = mutableListOf<DependencyAnalyzerContributor.Dependency>()
+    collectDependency(mavenProject.dependencyTree, rootDependency, result)
+    return result;
+  }
+
+  private fun collectDependency(nodes: List<MavenArtifactNode>,
+                                rootDependency: DependencyAnalyzerContributor.Dependency,
+                                result: MutableList<DependencyAnalyzerContributor.Dependency>) {
+    for (mavenArtifactNode in nodes) {
+      val dependency = DependencyAnalyzerContributor.Dependency(
+        getDependencyData(mavenArtifactNode),
+        scope(mavenArtifactNode.originalScope ?: "compile"), rootDependency,
+        getStatus(mavenArtifactNode))
+      result.add(dependency)
+      if (mavenArtifactNode.dependencies != null) {
+        collectDependency(mavenArtifactNode.dependencies, dependency, result)
+      }
+    }
+  }
+
+  private fun getDependencyData(mavenArtifactNode: MavenArtifactNode): DependencyAnalyzerContributor.Dependency.Data {
+    val mavenProject = MavenProjectsManager.getInstance(project).findProject(mavenArtifactNode.artifact)
+    if (mavenProject != null) {
+      return DependencyAnalyzerContributor.Dependency.Data.Module(mavenProject.displayName)
+    }
+    else {
+      return DependencyAnalyzerContributor.Dependency.Data.Artifact(
+        mavenArtifactNode.artifact.groupId, mavenArtifactNode.artifact.artifactId, mavenArtifactNode.artifact.version
+      )
+    }
+  }
+
+  private fun getStatus(mavenArtifactNode: MavenArtifactNode): List<DependencyAnalyzerContributor.Status> {
+    val status = mutableListOf<DependencyAnalyzerContributor.Status>()
+    if (mavenArtifactNode.getState() == MavenArtifactState.CONFLICT) {
+      status.add(DependencyAnalyzerContributor.Status.Omitted)
+      mavenArtifactNode.relatedArtifact?.version?.also {
+        val message = message("external.system.dependency.analyzer.warning.version.conflict", it)
+        status.add(DependencyAnalyzerContributor.Status.Warning(message))
+      }
+    }
+    else if (mavenArtifactNode.getState() == MavenArtifactState.DUPLICATE) {
+      status.add(DependencyAnalyzerContributor.Status.Omitted)
+    }
+    if (!mavenArtifactNode.artifact.isResolvedArtifact) {
+      status.add(DependencyAnalyzerContributor.Status.Warning(message("external.system.dependency.analyzer.warning.unresolved")))
+    }
+    return status;
+  }
+
+  @Suppress("HardCodedStringLiteral")
+  private fun scope(id: String) =
+    DependencyAnalyzerContributor.Scope(id, StringUtil.toLowerCase(id), StringUtil.toTitleCase(id))
 }
