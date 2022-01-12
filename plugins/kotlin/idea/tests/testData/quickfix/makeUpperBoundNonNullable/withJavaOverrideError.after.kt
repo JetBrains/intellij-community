5f4639adf1361cd1338c559191e1c96b43796cbb
Denis.Zharkov <denis.zharkov@jetbrains.com>
2022-01-12 12:45:40 +0300

    [kotlin] Adapt MakeUpperBoundNonNullableFix to the recent changes in the compiler
    GitOrigin-RevId: 61e8e1ae4540a4ff885c9f942b81561720758f78

--- plugins/kotlin/idea/tests/testData/quickfix/makeUpperBoundNonNullable/withJavaOverrideError.after.kt
+++ plugins/kotlin/idea/tests/testData/quickfix/makeUpperBoundNonNullable/withJavaOverrideError.after.kt
@@ -1,6 +1,6 @@
 // "Add 'Any' as upper bound for T to make it non-nullable" "true"
 // ERROR: 'foo' overrides nothing
-// LANGUAGE_VERSION: 1.7
+// LANGUAGE_VERSION: 1.8
 
 class FooChild<T : Any> : Foo<T>() {
     override fun foo(x: T) {}
