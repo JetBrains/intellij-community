a3629bf69da043a6b61f7f3699be98afd5a7c01a
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-01-12 08:06:05 +0100

    lazy creation of ReadMeCreator (LangSupport is used for checking toolwindow applicability)
    GitOrigin-RevId: bf1017cad3ec23e5c49b75a9c81417d243646634

--- java/java-features-trainer/src/com/intellij/java/ift/JavaLangSupport.kt
+++ java/java-features-trainer/src/com/intellij/java/ift/JavaLangSupport.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.java.ift
 
 import com.intellij.openapi.project.Project
@@ -6,7 +6,7 @@ import com.intellij.openapi.vfs.VirtualFile
 import training.project.ReadMeCreator
 import training.util.getFeedbackLink
 
-class JavaLangSupport : JavaBasedLangSupport() {
+internal class JavaLangSupport : JavaBasedLangSupport() {
   override val contentRootDirectoryName = "IdeaLearningProject"
   override val projectResourcePath = "learnProjects/java/LearnProject"
 
@@ -18,9 +18,10 @@ class JavaLangSupport : JavaBasedLangSupport() {
 
   override val projectSandboxRelativePath: String = "Sample.java"
 
-  override val langCourseFeedback get() = getFeedbackLink(this, false)
+  override val langCourseFeedback
+    get() = getFeedbackLink(this, false)
 
-  override val readMeCreator = ReadMeCreator()
+  override val readMeCreator by lazy { ReadMeCreator() }
 
   override fun blockProjectFileModification(project: Project, file: VirtualFile): Boolean {
     return file.name != projectSandboxRelativePath
