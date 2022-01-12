c3c0c901d4a92f8c95056ba9f9b1ff120cc2aa8b
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2022-01-12 18:31:26 +0300

    Kotlin: regenerate tests
    GitOrigin-RevId: 8b3fee99e17529b8c96c498033004e9997703c8a

--- plugins/kotlin/uast/uast-kotlin/tests/test/org/jetbrains/uast/test/kotlin/comparison/FE1UastDeclarationTestGenerated.java
+++ plugins/kotlin/uast/uast-kotlin/tests/test/org/jetbrains/uast/test/kotlin/comparison/FE1UastDeclarationTestGenerated.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package org.jetbrains.uast.test.kotlin.comparison;
 
@@ -26,6 +26,11 @@ public class FE1UastDeclarationTestGenerated extends AbstractFE1UastDeclarationT
     @TestMetadata("annotationOnElvis.kt")
     public void testAnnotationOnElvis() throws Exception {
         runTest("../../uast-kotlin-fir/testData/declaration/annotationOnElvis.kt");
+    }
+
+    @TestMetadata("commentsOnProperties.kt")
+    public void testCommentsOnProperties() throws Exception {
+        runTest("../../uast-kotlin-fir/testData/declaration/commentsOnProperties.kt");
     }
 
     @TestMetadata("doWhile.kt")
--- plugins/kotlin/uast/uast-kotlin/tests/test/org/jetbrains/uast/test/kotlin/comparison/FE1UastTypesTestGenerated.java
+++ plugins/kotlin/uast/uast-kotlin/tests/test/org/jetbrains/uast/test/kotlin/comparison/FE1UastTypesTestGenerated.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package org.jetbrains.uast.test.kotlin.comparison;
 
@@ -41,6 +41,11 @@ public class FE1UastTypesTestGenerated extends AbstractFE1UastTypesTest {
     @TestMetadata("classLiteral.kt")
     public void testClassLiteral() throws Exception {
         runTest("../../uast-kotlin-fir/testData/type/classLiteral.kt");
+    }
+
+    @TestMetadata("threadSafe.kt")
+    public void testThreadSafe() throws Exception {
+        runTest("../../uast-kotlin-fir/testData/type/threadSafe.kt");
     }
 
     @TestMetadata("typeCast.kt")
