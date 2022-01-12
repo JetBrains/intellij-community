d798b4aac99a0c627692003864a7b2a697506d3b
Victor Matchenko <Victor.Matchenko@jetbrains.com>
2022-01-12 14:02:35 +0100

    FUS: migrate "project.view.pane.changes" to the new API (IDEA-268269)
    GitOrigin-RevId: 854c0b7534caec10a8edb8d98b4f366dbe874efe

--- platform/lang-impl/src/com/intellij/ide/projectView/impl/ProjectViewImpl.java
+++ platform/lang-impl/src/com/intellij/ide/projectView/impl/ProjectViewImpl.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.ide.projectView.impl;
 
 import com.intellij.application.options.OptionsApplicabilityFilter;
@@ -15,8 +15,12 @@ import com.intellij.ide.ui.UISettings;
 import com.intellij.ide.util.treeView.AbstractTreeBuilder;
 import com.intellij.ide.util.treeView.NodeDescriptor;
 import com.intellij.idea.ActionsBundle;
-import com.intellij.internal.statistic.eventLog.FeatureUsageData;
-import com.intellij.internal.statistic.service.fus.collectors.FUCounterUsageLogger;
+import com.intellij.internal.statistic.eventLog.EventLogGroup;
+import com.intellij.internal.statistic.eventLog.events.ClassEventField;
+import com.intellij.internal.statistic.eventLog.events.EventFields;
+import com.intellij.internal.statistic.eventLog.events.EventPair;
+import com.intellij.internal.statistic.eventLog.events.VarargEventId;
+import com.intellij.internal.statistic.service.fus.collectors.CounterUsagesCollector;
 import com.intellij.lang.LangBundle;
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.application.ApplicationManager;
@@ -837,26 +841,28 @@ public class ProjectViewImpl extends ProjectView implements PersistentStateCompo
     }
     myProject.getMessageBus().syncPublisher(ProjectViewListener.TOPIC).paneShown(newPane, currentPane);
 
-    logProjectViewPaneChangedEvent(currentPane, newPane);
+    logProjectViewPaneChangedEvent(myProject, currentPane, newPane);
   }
 
-  private static void logProjectViewPaneChangedEvent(@Nullable AbstractProjectViewPane currentPane,
+  private static void logProjectViewPaneChangedEvent(@NotNull Project project,
+                                                     @Nullable AbstractProjectViewPane currentPane,
                                                      @NotNull AbstractProjectViewPane newPane) {
-    FeatureUsageData data = new FeatureUsageData()
-      .addData("to_class_name", newPane.getClass().getName());
+    List<EventPair<?>> events = new ArrayList<>(4);
+
+    events.add(ProjectViewPaneChangesCollector.TO_PROJECT_VIEW.with(newPane.getClass()));
     NamedScope selectedScope = newPane instanceof ScopeViewPane ? ((ScopeViewPane)newPane).getSelectedScope() : null;
     if (selectedScope != null) {
-      data.addData("to_scope_class_name", selectedScope.getClass().getName());
+      events.add(ProjectViewPaneChangesCollector.TO_SCOPE.with(selectedScope.getClass()));
     }
     if (currentPane != null) {
-      data.addData("from_class_name", currentPane.getClass().getName());
+      events.add(ProjectViewPaneChangesCollector.FROM_PROJECT_VIEW.with(currentPane.getClass()));
       selectedScope = currentPane instanceof ScopeViewPane ? ((ScopeViewPane)currentPane).getSelectedScope() : null;
       if (selectedScope != null) {
-        data.addData("from_scope_class_name", selectedScope.getClass().getName());
+        events.add(ProjectViewPaneChangesCollector.FROM_SCOPE.with(selectedScope.getClass()));
       }
     }
 
-    FUCounterUsageLogger.getInstance().logEvent("project.view.pane.changes", "changed", data);
+    ProjectViewPaneChangesCollector.CHANGED.log(project, events);
   }
 
   // public for tests
@@ -1965,6 +1971,21 @@ public class ProjectViewImpl extends ProjectView implements PersistentStateCompo
       SortByType() {
         super(view -> view.mySortByType);
       }
+    }
+  }
+
+  private static final class ProjectViewPaneChangesCollector extends CounterUsagesCollector {
+    private static final EventLogGroup GROUP = new EventLogGroup("project.view.pane.changes", 2);
+    private static final ClassEventField TO_PROJECT_VIEW = EventFields.Class("to_class_name");
+    private static final ClassEventField TO_SCOPE = EventFields.Class("to_scope_class_name");
+    private static final ClassEventField FROM_PROJECT_VIEW = EventFields.Class("from_class_name");
+    private static final ClassEventField FROM_SCOPE = EventFields.Class("from_scope_class_name");
+    private static final VarargEventId CHANGED =
+      GROUP.registerVarargEvent("changed", TO_PROJECT_VIEW, TO_SCOPE, FROM_PROJECT_VIEW, FROM_SCOPE);
+
+    @Override
+    public EventLogGroup getGroup() {
+      return GROUP;
     }
   }
 }
