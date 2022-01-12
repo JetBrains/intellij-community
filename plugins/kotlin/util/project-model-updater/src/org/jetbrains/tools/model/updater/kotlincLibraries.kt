3304d8c8a037020c7badaaeb633b2b70ab7fc374
Nikita Bobko <nikita.bobko@jetbrains.com>
2022-01-12 14:51:55 +0100

    Add 'kotlinc.kotlin-jps-common' to project model updater
    GitOrigin-RevId: 15322d8a3c7e80c4dcf19214edc920b8432d1d29

--- plugins/kotlin/util/project-model-updater/src/org/jetbrains/tools/model/updater/kotlincLibraries.kt
+++ plugins/kotlin/util/project-model-updater/src/org/jetbrains/tools/model/updater/kotlincLibraries.kt
@@ -35,6 +35,7 @@ fun generateKotlincLibraries(kotlincArtifactsMode: KotlincArtifactsMode, version
         kotlincForIdeWithStandardNaming("kotlinc.noarg-compiler-plugin", version),
         kotlincForIdeWithStandardNaming("kotlinc.parcelize-compiler-plugin", version),
         kotlincForIdeWithStandardNaming("kotlinc.sam-with-receiver-compiler-plugin", version),
+        kotlincForIdeWithStandardNaming("kotlinc.kotlin-jps-common", version),
         singleJarMvnLib("kotlinc.compiler-components-for-jps", "$ktGroup:compiler-components-for-jps:$version", transitive = false),
         singleJarMvnLib("kotlinc.kotlin-scripting-common", "$ktGroup:kotlin-scripting-common:$version", transitive = false),
         singleJarMvnLib("kotlinc.kotlin-scripting-compiler-impl", "$ktGroup:kotlin-scripting-compiler-impl:$version", transitive = false),
