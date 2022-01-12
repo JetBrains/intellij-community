f1999805912dcd10122de8f8815131f583344aef
Bart van Helvert <bart.vanhelvert@jetbrains.com>
2022-01-12 13:29:14 +0100

    [java-decompiler] IDEA-246757 Add type annotation test sources
    GitOrigin-RevId: 7a066131ee0893c98074f4e18c89f2cd577be533

--- plugins/java-decompiler/engine/testData/src/typeAnnotations/ArrayTypeAnnotations.java
+++ plugins/java-decompiler/engine/testData/src/typeAnnotations/ArrayTypeAnnotations.java
@@ -2,10 +2,10 @@ package typeAnnotations;
 
 public class ArrayTypeAnnotations implements ParentInterface {
     @A String[] s1 = new String[0];
-
     String @B [] s2 = new String[0];
-
     String @C [][] s3 = new String[0][0];
-
     String [] @D [] s4 = new String[0][0];
+    @A String[] s5() { return null; }
+    String @B [] s6() { return null; }
+    @A String @B [] @C [] @D [] s7() { return null; }
 }
