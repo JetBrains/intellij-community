1d421294318b7fc0d89e0ed3bdb05d8783dff34d
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2022-01-12 20:09:16 +0300

    [git-stash] remove unused property
    follow-up: 22fec22559316f21345e05776697707d4dfe87e4
    
    GitOrigin-RevId: 8c5a41912f0bfa0a301b689978bcbe6f79fe8a95

--- plugins/git4idea/src/git4idea/stash/ui/GitStashDiffPreview.kt
+++ plugins/git4idea/src/git4idea/stash/ui/GitStashDiffPreview.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package git4idea.stash.ui
 
 import com.intellij.diff.FrameDiffTool
@@ -27,8 +27,6 @@ abstract class GitStashDiffPreview(project: Project,
                                    parentDisposable: Disposable)
   : ChangeViewDiffRequestProcessor(project, GIT_STASH_UI_PLACE) {
   private val disposableFlag = Disposer.newCheckedDisposable()
-
-  val toolbarWrapper get() = myToolbarWrapper
 
   init {
     tree.addSelectionListener(Runnable {
