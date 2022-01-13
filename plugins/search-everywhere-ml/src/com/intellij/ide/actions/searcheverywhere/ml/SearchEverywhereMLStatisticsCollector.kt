ac140d0b15f59d50b80d6ce907c3ad7c798ff4e7
Svetlana.Zemlyanskaya <Svetlana.Zemlyanskaya@jetbrains.com>
2022-01-13 20:23:51 +0100

    ML in SE: update ml model for Files tab
    GitOrigin-RevId: 884b596886d92c46cdd76bb4de046e235c5949b9

--- plugins/search-everywhere-ml/src/com/intellij/ide/actions/searcheverywhere/ml/SearchEverywhereMLStatisticsCollector.kt
+++ plugins/search-everywhere-ml/src/com/intellij/ide/actions/searcheverywhere/ml/SearchEverywhereMLStatisticsCollector.kt
@@ -224,7 +224,7 @@ internal class SearchEverywhereMLStatisticsCollector {
   }
 
   companion object {
-    private val GROUP = EventLogGroup("mlse.log", 15)
+    private val GROUP = EventLogGroup("mlse.log", 16)
     private val EMPTY_ARRAY = IntArray(0)
     private const val REPORTED_ITEMS_LIMIT = 100
 
--- plugins/search-everywhere-ml/src/com/intellij/ide/actions/searcheverywhere/ml/model/SearchEverywhereFilesRankingModelProvider.kt
+++ plugins/search-everywhere-ml/src/com/intellij/ide/actions/searcheverywhere/ml/model/SearchEverywhereFilesRankingModelProvider.kt
@@ -2,19 +2,14 @@ package com.intellij.ide.actions.searcheverywhere.ml.model
 
 import com.intellij.ide.actions.searcheverywhere.FileSearchEverywhereContributor
 import com.intellij.internal.ml.DecisionFunction
-import com.intellij.internal.ml.FeaturesInfo
-import com.intellij.internal.ml.ResourcesModelMetadataReader
-import com.intellij.searchEverywhere.model.files.PredictionModel
 
 internal class SearchEverywhereFilesRankingModelProvider : SearchEverywhereMLRankingModelProvider() {
-  private val resourceDirectory = "file-features"
+  private val resourceDirectory = "files_features"
+  private val modelDirectory = "files_model"
 
   override val supportedContributor = FileSearchEverywhereContributor::class.java
 
   override fun getBundledModel(): DecisionFunction {
-    val metadata = FeaturesInfo.buildInfo(ResourcesModelMetadataReader(this::class.java, resourceDirectory))
-    return object : SearchEverywhereMLRankingDecisionFunction(metadata) {
-      override fun predict(features: DoubleArray?): Double = PredictionModel.makePredict(features)
-    }
+    return getCatBoostModel(resourceDirectory, modelDirectory)
   }
 }
