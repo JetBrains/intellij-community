d25fd6a9c3c69d22adf36b2c5c46b4f31c5ad3b5
Ivan Chirkov <Ivan.Chirkov@jetbrains.com>
2022-01-13 22:56:22 +0100

    PluginManager: appropriate buy message for freemium plugins
    GitOrigin-RevId: 2a211eca5501e01ef50a43d4856c708553807328

--- platform/platform-impl/src/com/intellij/ide/plugins/newui/LicensePanel.java
+++ platform/platform-impl/src/com/intellij/ide/plugins/newui/LicensePanel.java
@@ -6,6 +6,7 @@ import com.intellij.ide.BrowserUtil;
 import com.intellij.ide.IdeBundle;
 import com.intellij.ide.plugins.IdeaPluginDescriptor;
 import com.intellij.ide.plugins.PluginManagerConfigurable;
+import com.intellij.ide.plugins.PluginNode;
 import com.intellij.openapi.application.IdeUrlTrackingParametersProvider;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.util.NlsSafe;
@@ -24,6 +25,7 @@ import org.jetbrains.annotations.Nullable;
 import javax.swing.*;
 import java.awt.*;
 import java.util.Date;
+import java.util.List;
 import java.util.function.Supplier;
 
 /**
@@ -155,11 +157,18 @@ public class LicensePanel extends NonOpaquePanel {
     setLink(IdeBundle.message("plugins.configurable.buy.the.plugin"), () ->
       BrowserUtil.browse(IdeUrlTrackingParametersProvider.getInstance().augmentUrl("https://plugins.jetbrains.com/purchase-link/" + plugin.getProductCode())), true);
 
-    PluginPriceService.getPrice(plugin, price -> updateLink(IdeBundle.message("plugins.configurable.buy.the.plugin.from.0", price), false), price -> {
-      if (plugin == getPlugin.get()) {
-        updateLink(IdeBundle.message("plugins.configurable.buy.the.plugin.from.0", price), true);
+    if (plugin instanceof PluginNode) {
+      List<String> tags = ((PluginNode)plugin).getTags();
+      if (tags.contains(Tags.Freemium.name())) {
+        updateLink(IdeBundle.message("plugins.configurable.activate.trial.for.full.access"), false);
       }
-    });
+    } else {
+      PluginPriceService.getPrice(plugin, price -> updateLink(IdeBundle.message("plugins.configurable.buy.the.plugin.from.0", price), false), price -> {
+        if (plugin == getPlugin.get()) {
+          updateLink(IdeBundle.message("plugins.configurable.buy.the.plugin.from.0", price), true);
+        }
+      });
+    }
   }
 
   public static boolean isEA2Product(@Nullable String productCodeOrPluginId) {
