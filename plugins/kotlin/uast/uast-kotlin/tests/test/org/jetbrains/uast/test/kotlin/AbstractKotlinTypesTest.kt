62a291616a5eb52576914378f8400259b9c9802f
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2022-01-14 17:18:51 +0100

    [kt-*-only] Fixed test KotlinUastTypesTest#testAnnotatedTypes
    There is 2 changes. In testdata -- it is because of f08b2c8e28
    And the second one. The story behind that is strange.
    Some time ago there were 2 copy of TypesTestBase, and in
    e430a1ea14d5a90 it was fixed. But in parallel branch there was
    commit e8f294316c5d97cf6 and that change was lost.
    
    But because now the code of TypesTestBase is in IDEA platform, we
    cannot change it in kt-* branch.
    
    Please do deduplication again in IDEA master :)
    
    GitOrigin-RevId: 3ef6b71aa3e5cb653e4cf53695074c5da228dbf4

--- plugins/kotlin/uast/uast-kotlin/tests/test/org/jetbrains/uast/test/kotlin/AbstractKotlinTypesTest.kt
+++ plugins/kotlin/uast/uast-kotlin/tests/test/org/jetbrains/uast/test/kotlin/AbstractKotlinTypesTest.kt
@@ -1,10 +1,10 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.uast.test.kotlin
 
-import org.jetbrains.uast.test.common.TypesTestBase
+import org.jetbrains.uast.test.kotlin.org.jetbrains.uast.test.kotlin.common.TypesTestBaseDuplicate
 import java.io.File
 
-abstract class AbstractKotlinTypesTest : AbstractKotlinUastTest(), TypesTestBase {
+abstract class AbstractKotlinTypesTest : AbstractKotlinUastTest(), TypesTestBaseDuplicate {
     private fun getTestFile(testName: String, ext: String) =
             File(File(TEST_KOTLIN_MODEL_DIR, testName).canonicalPath + '.' + ext)
 
