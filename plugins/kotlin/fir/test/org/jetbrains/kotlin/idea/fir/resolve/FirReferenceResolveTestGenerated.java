ac18b93bfd80f8bc44a8e3ca92d95aec3e9e2f28
Mads Ager <ager@google.com>
2022-01-14 13:36:45 +0100

    Test updates for analysis resolution behavior for super type calls.
    The super type calls now resolve to the constructor they are
    calling instead of the class.
    
    GitOrigin-RevId: a9e3c518b051b598ba589a3aa0acc8ad41adb50a

--- plugins/kotlin/fir/test/org/jetbrains/kotlin/idea/fir/resolve/FirReferenceResolveTestGenerated.java
+++ plugins/kotlin/fir/test/org/jetbrains/kotlin/idea/fir/resolve/FirReferenceResolveTestGenerated.java
@@ -373,6 +373,16 @@ public class FirReferenceResolveTestGenerated extends AbstractFirReferenceResolv
         runTest("../idea/tests/testData/resolve/references/SeveralOverrides.kt");
     }
 
+    @TestMetadata("SuperTypePrimaryConstructor.kt")
+    public void testSuperTypePrimaryConstructor() throws Exception {
+        runTest("../idea/tests/testData/resolve/references/SuperTypePrimaryConstructor.kt");
+    }
+
+    @TestMetadata("SuperTypeSecondaryConstructor.kt")
+    public void testSuperTypeSecondaryConstructor() throws Exception {
+        runTest("../idea/tests/testData/resolve/references/SuperTypeSecondaryConstructor.kt");
+    }
+
     @TestMetadata("SyntheticProperty.kt")
     public void testSyntheticProperty() throws Exception {
         runTest("../idea/tests/testData/resolve/references/SyntheticProperty.kt");
