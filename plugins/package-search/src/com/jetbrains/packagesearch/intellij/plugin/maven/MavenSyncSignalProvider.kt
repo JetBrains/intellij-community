ff2006a9c3dfc40d008dda48779725256de413c0
Lamberto Basti <lamberto.basti@jetbrains.com>
2022-01-14 15:59:15 +0100

    [PKGS] using VFS instead of polling for checking ancillary files.
    GitOrigin-RevId: 85d4512cdecde4f607fdfb77f65bf25ecc805210

--- plugins/package-search/src/com/jetbrains/packagesearch/intellij/plugin/maven/MavenSyncSignalProvider.kt
+++ plugins/package-search/src/com/jetbrains/packagesearch/intellij/plugin/maven/MavenSyncSignalProvider.kt
@@ -1,25 +1,13 @@
 package com.jetbrains.packagesearch.intellij.plugin.maven
 
 import com.intellij.openapi.project.Project
-import com.intellij.util.io.exists
 import com.intellij.util.messages.SimpleMessageBusConnection
 import com.jetbrains.packagesearch.intellij.plugin.extensibility.DumbAwareMessageBusModuleChangesSignalProvider
-import com.jetbrains.packagesearch.intellij.plugin.extensibility.FlowModuleChangesSignalProvider
+import com.jetbrains.packagesearch.intellij.plugin.extensibility.FileWatcherSignalProvider
 import com.jetbrains.packagesearch.intellij.plugin.extensibility.invoke
-import com.jetbrains.packagesearch.intellij.plugin.util.lifecycleScope
 import com.jetbrains.packagesearch.intellij.plugin.util.logDebug
-import com.jetbrains.packagesearch.intellij.plugin.util.timer
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.drop
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
 import org.jetbrains.idea.maven.project.MavenImportListener
-import java.nio.file.Path
 import java.nio.file.Paths
-import kotlin.io.path.getLastModifiedTime
-import kotlin.time.DurationUnit
-import kotlin.time.toDuration
 
 internal class MavenSyncSignalProvider : DumbAwareMessageBusModuleChangesSignalProvider() {
 
@@ -34,18 +22,5 @@ internal class MavenSyncSignalProvider : DumbAwareMessageBusModuleChangesSignalP
     }
 }
 
-internal class GlobalMavenSettingsChangedSignalProvider : FlowModuleChangesSignalProvider {
-
-    override fun registerModuleChangesListener(project: Project): Flow<Unit> {
-
-        val path: Path = Paths.get(System.getProperty("user.home"), ".m2", "settings.xml")
-
-        val getTime = { if (path.exists()) path.getLastModifiedTime().toMillis() else 0 }
-
-        return timer(1.toDuration(DurationUnit.SECONDS))
-            .map { getTime() }
-            .stateIn(project.lifecycleScope, SharingStarted.Lazily, getTime())
-            .drop(1)
-            .map { }
-    }
-}
+internal class GlobalMavenSettingsChangedSignalProvider
+    : FileWatcherSignalProvider(Paths.get(System.getProperty("user.home"), ".m2", "settings.xml"))
