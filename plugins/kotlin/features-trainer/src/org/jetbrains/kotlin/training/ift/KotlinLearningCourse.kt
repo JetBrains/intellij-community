3d21817d6744e8a5207a30d0be3abad3ae809b68
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2022-01-14 13:18:45 +0300

    [IFT] Add Essential module for Kotlin
    IJ-CR-19709
    
    GitOrigin-RevId: cb292c82f28c2d7ac9eb41f24100ac953025a98b

--- plugins/kotlin/features-trainer/src/org/jetbrains/kotlin/training/ift/KotlinLearningCourse.kt
+++ plugins/kotlin/features-trainer/src/org/jetbrains/kotlin/training/ift/KotlinLearningCourse.kt
@@ -2,17 +2,35 @@
 package org.jetbrains.kotlin.training.ift
 
 import org.jetbrains.kotlin.idea.KotlinLanguage
+import org.jetbrains.kotlin.training.ift.lesson.basic.KotlinContextActionsLesson
 import org.jetbrains.kotlin.training.ift.lesson.basic.KotlinSelectLesson
 import org.jetbrains.kotlin.training.ift.lesson.navigation.KotlinFileStructureLesson
+import org.jetbrains.kotlin.training.ift.lesson.navigation.KotlinSearchEverywhereLesson
+import training.dsl.LessonUtil
 import training.learn.LessonsBundle
 import training.learn.course.LearningCourseBase
 import training.learn.course.LearningModule
 import training.learn.course.LessonType
+import training.learn.lesson.general.GotoActionLesson
 
 class KotlinLearningCourse : LearningCourseBase(KotlinLanguage.INSTANCE.id) {
     override fun modules() = stableModules()
 
     private fun stableModules() = listOf(
+        LearningModule(
+            id = "Kotlin.Essential",
+            name = LessonsBundle.message("essential.module.name"),
+            description = LessonsBundle.message("essential.module.description", LessonUtil.productName),
+            primaryLanguage = langSupport,
+            moduleType = LessonType.SINGLE_EDITOR  // todo: change to SCRATCH when KTIJ-20742 will be resolved
+        ) {
+            fun ls(sampleName: String) = loadSample("EditorBasics/$sampleName")
+            listOf(
+                KotlinContextActionsLesson(),
+                GotoActionLesson(ls("Actions.kt.sample"), firstLesson = false),
+                KotlinSearchEverywhereLesson(),
+            )
+        },
         LearningModule(
             id = "Kotlin.EditorBasics",
             name = LessonsBundle.message("editor.basics.module.name"),
@@ -29,7 +47,10 @@ class KotlinLearningCourse : LearningCourseBase(KotlinLanguage.INSTANCE.id) {
             primaryLanguage = langSupport,
             moduleType = LessonType.PROJECT
         ) {
-            listOf(KotlinFileStructureLesson())
+            listOf(
+                KotlinSearchEverywhereLesson(),
+                KotlinFileStructureLesson(),
+            )
         }
     )
 }
